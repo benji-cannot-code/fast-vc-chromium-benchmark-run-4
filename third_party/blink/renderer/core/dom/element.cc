d386347999b303f05328524b709f14d2bd115be4
Albert J. Wong <ajwong@chromium.org>
2020-07-15 08:08:46 +0000

    Avoid unnecessary lowercasing and string creation in DOM Lookup APIs.
    Some of the DOM Attribute APIs require lower casing of the ASCII
    characters. Historically, Blink code called
    Element::LowercaseIfNecessary() anytime such a comparison was needed.
    This caused 2-3 string traversals in most lookup APIs, one
    for verifying the string was lowercase, another for lowercasing the
    string if necessary, and a third for doing a string lookup in the
    AtomicStringTable.
    
    Furthermore, in the cases of very short strings on platforms with
    V8 Pointer Compression (eg x64), there can often be 2 more string
    traversals: The first happens when V8::String object is copied into
    a Blink StringImpl, and the second happens when that StringImpl is
    added to the AtomicStringTable. This lookup is NOT cached via the
    externalization process when V8 Pointer compression is on.
    
    This CL:
      * Removes unnecessary creation of a ASCII lower-cased AtomicString
        if it is only used for checking existence in a collection.
      * Removes multiple calls to lowercasing functions within one
        DOM API function call chain.
    
    This is done by:
      * Normalizing the DOM APIs to take a StringView instead of an
        AtomicString as a parameter.
      * Using the newly added WeakFind() API in AtomicStringTable and
        "Hinted" APIs in AttributeCollection.
    
    The change also avoids unnecessary AddRef/Release pairs in many
    API call chains and will remove much of the performance hit when
    StringImpl is made threadsafe.
    
    Performance on microbenchmarks is preserved or slightly improved on
    desktop and mobile platforms.
    
    Note that performance analysis on this code is difficult. arm32
    does not have v8 pointer compression so the most critical speed-up
    is ensuring externalization can happen. However, this means
    adding more conditionals to all APIs that take a StringView to
    check if that view is backed by an AtomicString and then
    fast pathing. Accidentally inhibiting externalization will reduce
    benchmark performance by greater than 50%. Also many of these codepaths
    are sensitive to -Oz versus -O2 changes.
    
    In contrast, on x64, externalization does not affect most of
    these microbenchmarks thus the priority is removing branches
    and copies. Unilaterally removing any Blink String creation yields
    a 20% speed-up in the benchmarks, but will hurt code that has larger
    strings which would get externalized (this is not represented in
    the blink_perf benchmarks). Adding the conditionals in to support
    this case will remove some of this 20% speed-up.
    
    Doing performance optimization on this code requires balancing
    all these different use cases. The current implementation reflects
    a best attempt to address each case satisfactorily.
    
    Note: This CL regresses Android get-attribute perf by 15%.
      https://pinpoint-dot-chromeperf.appspot.com/job/1141cbd7120000
    But the final CL with all other changes comes down to only 5% regression
      https://pinpoint-dot-chromeperf.appspot.com/job/111168fe120000
    Committing as is rather than debugging perf on each step.
    
    
    This is partially broken out of
      https://chromium-review.googlesource.com/c/chromium/src/+/2204539
    
    which in turn is broken out of
    https://chromium-review.googlesource.com/c/chromium/src/+/1557854
    
    See this design doc for more details on the larger motivation for
    this change:
    https://docs.google.com/document/d/1hp7h6zfD5S6mnMI4cHe1PpacRnb2rNpRRkn1gBBJHuQ/edit#heading=h.erv1bv18t616
    
    Bug: 1083392
    Change-Id: I502040666fdaec3008bb2d59ca845b14a637f951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2263652
    Commit-Queue: Albert J. Wong <ajwong@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788522}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -972,13 +972,17 @@ bool Element::hasAttribute(const QualifiedName& name) const {
 }
 
 bool Element::HasAttributeIgnoringNamespace(
-    const AtomicString& local_name) const {
+    const StringView& local_name) const {
   if (!GetElementData())
     return false;
-  SynchronizeAttribute(local_name);
-  AtomicString name = LowercaseIfNecessary(local_name);
+  WTF::AtomicStringTable::WeakResult hint =
+      WeakLowercaseIfNecessary(local_name);
+  SynchronizeAttributeHinted(local_name, hint);
+  if (hint.IsNull()) {
+    return false;
+  }
   for (const Attribute& attribute : GetElementData()->Attributes()) {
-    if (attribute.LocalName() == name)
+    if (hint == attribute.LocalName())
       return true;
   }
   return false;
@@ -2056,21 +2060,22 @@ bool Element::toggleAttribute(const AtomicString& qualified_name,
   // 2. If the context object is in the HTML namespace and its node document is
   // an HTML document, then set qualifiedName to qualifiedName in ASCII
   // lowercase.
-  AtomicString lower_case_name = LowercaseIfNecessary(qualified_name);
+  AtomicString lowercase_name = LowercaseIfNecessary(qualified_name);
+  WTF::AtomicStringTable::WeakResult hint(lowercase_name.Impl());
   // 3. Let attribute be the first attribute in the context object’s attribute
   // list whose qualified name is qualifiedName, and null otherwise.
   // 4. If attribute is null, then
-  if (!getAttribute(lower_case_name)) {
+  if (!GetAttributeHinted(lowercase_name, hint)) {
     // 4. 1. If force is not given or is true, create an attribute whose local
-    // name is qualifiedName, value is the empty string, and node document is
+    // name is qualified_name, value is the empty string, and node document is
     // the context object’s node document, then append this attribute to the
     // context object, and then return true.
-    setAttribute(lower_case_name, g_empty_atom);
+    SetAttributeHinted(lowercase_name, hint, g_empty_atom);
     return true;
   }
   // 5. Otherwise, if force is not given or is false, remove an attribute given
   // qualifiedName and the context object, and then return false.
-  removeAttribute(lower_case_name);
+  RemoveAttributeHinted(lowercase_name, hint);
   return false;
 }
 
@@ -2089,17 +2094,18 @@ bool Element::toggleAttribute(const AtomicString& qualified_name,
   // 2. If the context object is in the HTML namespace and its node document is
   // an HTML document, then set qualifiedName to qualifiedName in ASCII
   // lowercase.
-  AtomicString lower_case_name = LowercaseIfNecessary(qualified_name);
+  AtomicString lowercase_name = LowercaseIfNecessary(qualified_name);
+  WTF::AtomicStringTable::WeakResult hint(lowercase_name.Impl());
   // 3. Let attribute be the first attribute in the context object’s attribute
   // list whose qualified name is qualifiedName, and null otherwise.
   // 4. If attribute is null, then
-  if (!getAttribute(lower_case_name)) {
+  if (!GetAttributeHinted(lowercase_name, hint)) {
     // 4. 1. If force is not given or is true, create an attribute whose local
-    // name is qualifiedName, value is the empty string, and node document is
+    // name is qualified_name, value is the empty string, and node document is
     // the context object’s node document, then append this attribute to the
     // context object, and then return true.
     if (force) {
-      setAttribute(lower_case_name, g_empty_atom);
+      SetAttributeHinted(lowercase_name, hint, g_empty_atom);
       return true;
     }
     // 4. 2. Return false.
@@ -2108,7 +2114,7 @@ bool Element::toggleAttribute(const AtomicString& qualified_name,
   // 5. Otherwise, if force is not given or is false, remove an attribute given
   // qualifiedName and the context object, and then return false.
   if (!force) {
-    removeAttribute(lower_case_name);
+    RemoveAttributeHinted(lowercase_name, hint);
     return false;
   }
   // 6. Return true.
@@ -3804,12 +3810,14 @@ void Element::removeAttributeNS(const AtomicString& namespace_uri,
   removeAttribute(QualifiedName(g_null_atom, local_name, namespace_uri));
 }
 
-Attr* Element::getAttributeNode(const AtomicString& local_name) {
+Attr* Element::getAttributeNode(const StringView& local_name) {
   if (!GetElementData())
     return nullptr;
-  SynchronizeAttribute(local_name);
+  WTF::AtomicStringTable::WeakResult hint =
+      WeakLowercaseIfNecessary(local_name);
+  SynchronizeAttributeHinted(local_name, hint);
   const Attribute* attribute =
-      GetElementData()->Attributes().Find(LowercaseIfNecessary(local_name));
+      GetElementData()->Attributes().FindHinted(local_name, hint);
   if (!attribute)
     return nullptr;
   return EnsureAttr(attribute->GetName());
@@ -3827,12 +3835,14 @@ Attr* Element::getAttributeNodeNS(const AtomicString& namespace_uri,
   return EnsureAttr(attribute->GetName());
 }
 
-bool Element::hasAttribute(const AtomicString& local_name) const {
+bool Element::hasAttribute(const StringView& local_name) const {
   if (!GetElementData())
     return false;
-  SynchronizeAttribute(local_name);
-  return GetElementData()->Attributes().FindIndex(
-             LowercaseIfNecessary(local_name)) != kNotFound;
+  WTF::AtomicStringTable::WeakResult hint =
+      WeakLowercaseIfNecessary(local_name);
+  SynchronizeAttributeHinted(local_name, hint);
+  return GetElementData()->Attributes().FindIndexHinted(local_name, hint) !=
+         kNotFound;
 }
 
 bool Element::hasAttributeNS(const AtomicString& namespace_uri,
