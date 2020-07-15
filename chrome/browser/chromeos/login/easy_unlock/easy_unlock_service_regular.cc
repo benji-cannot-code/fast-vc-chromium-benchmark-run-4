5631b5026095171d18fe9cbb6e784d0d804a9bc8
Anand K Mistry <amistry@chromium.org>
2020-07-15 02:49:32 +0000

    Replace RemoveWithoutPathExpansion(.*, nullptr) with Value::RemoveKey()
    DictionaryValue::RemoveWithoutPathExpansion() is deprecated. For the
    case where nullptr is passed for the output argument, Value::RemoveKey()
    is a trivial replacement.
    
    Mostly done with the following command line, plus a bit of finesse:
    % git grep -l RemoveWithoutPathExpansion | \
      xargs sed -E -i 's/RemoveWithoutPathExpansion\((.*), nullptr/RemoveKey\(\1/g'
    
    Bug: 646113
    
    This CL was uploaded by git cl split.
    
    R=khorimoto@chromium.org
    
    Change-Id: I9471132239f2ff79dfb6c60ddd31389fbba2aef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2298840
    Auto-Submit: Anand K Mistry <amistry@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788466}

--- chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_regular.cc
+++ chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_regular.cc
@@ -266,7 +266,7 @@ void EasyUnlockServiceRegular::SetStoredRemoteDevices(
   DictionaryPrefUpdate pairing_update(profile()->GetPrefs(),
                                       prefs::kEasyUnlockPairing);
   if (devices.empty())
-    pairing_update->RemoveWithoutPathExpansion(kKeyDevices, NULL);
+    pairing_update->RemoveKey(kKeyDevices);
   else
     pairing_update->SetKey(kKeyDevices, devices.Clone());
 
