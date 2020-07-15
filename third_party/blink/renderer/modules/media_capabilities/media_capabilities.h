38963399f1f1a212203f3ed224597eed30a4eef1
Chris Cunningham <chcunningham@chromium.org>
2020-07-15 00:49:01 +0000

    MediaCapabilities: check gpu video acceleration for powerEfficient
    Feature disabled by default. Enable with command line:
    --enable-features=MediaCapabilitiesQueryGpuFactories
    
    Historically info.powerEfficient was determined by the
    VideoDecodePerfHistory DB. This patch instead queries the GpuFactories
    (proxy for hw decode) for to ask the question directly.
    
    The critical difference is that the DB requires some past data to
    be correct, while GpuFactories will know the answer without ever having
    played a video.
    
    Change-Id: I402bc03d008c73fa1bccf61beb61609efaf42ddf
    Bug: 1105258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2295684
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788421}

--- third_party/blink/renderer/modules/media_capabilities/media_capabilities.h
+++ third_party/blink/renderer/modules/media_capabilities/media_capabilities.h
@@ -61,6 +61,7 @@ class MODULES_EXPORT MediaCapabilities final : public ScriptWrappable {
     base::Optional<bool> is_nnr_prediction_smooth;
     base::Optional<bool> db_is_smooth;
     base::Optional<bool> db_is_power_efficient;
+    base::Optional<bool> is_gpu_factories_supported;
     base::TimeTicks request_time;
   };
 
@@ -75,6 +76,7 @@ class MODULES_EXPORT MediaCapabilities final : public ScriptWrappable {
   ScriptPromise GetEmeSupport(ScriptState*,
                               media::VideoCodec,
                               media::VideoCodecProfile,
+                              media::VideoColorSpace,
                               const MediaDecodingConfiguration*,
                               const base::TimeTicks& request_time,
                               ExceptionState&);
@@ -82,7 +84,8 @@ class MODULES_EXPORT MediaCapabilities final : public ScriptWrappable {
   // parallel request to GetPerfInfo_ML() when learning experiment is enabled.
   void GetPerfInfo(media::VideoCodec,
                    media::VideoCodecProfile,
-                   const VideoConfiguration*,
+                   media::VideoColorSpace,
+                   const MediaDecodingConfiguration*,
                    const base::TimeTicks& request_time,
                    ScriptPromiseResolver*,
                    MediaKeySystemAccess*);
@@ -94,6 +97,15 @@ class MODULES_EXPORT MediaCapabilities final : public ScriptWrappable {
                       media::VideoCodecProfile video_profile,
                       int width,
                       double framerate);
+
+  // Query media::GpuVideoAcceleratorFactories for support of hardware
+  // accelerate decode. Only called when |UseGpuFactoriesForPowerEfficient()|
+  // is true.
+  void GetGpuFactoriesSupport(int callback_id,
+                              media::VideoCodec video_codec,
+                              media::VideoCodecProfile video_profile,
+                              media::VideoColorSpace,
+                              const MediaDecodingConfiguration*);
 
   // Callback for perf info from the VideoDecodePerfHistory service.
   void OnPerfHistoryInfo(int callback_id,
@@ -109,6 +121,9 @@ class MODULES_EXPORT MediaCapabilities final : public ScriptWrappable {
   void OnNnrPrediction(
       int callback_id,
       const base::Optional<::media::learning::TargetHistogram>& histogram);
+
+  // Callback for GetGpuFactoriesSupport().
+  void OnGpuFactoriesSupport(int callback_id, bool is_supported);
 
   // Resolves the callback with associated |callback_id| and removes it from the
   // |pending_callback_map_|.
