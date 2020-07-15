eb96b4fb0ba99c1d88e182f80d82f7e325909b61
Steven Bennetts <stevenjb@chromium.org>
2020-07-15 16:30:10 +0000

    Cleanup NetworkConfigurationHandler::SetManagerProperty
    When this was added, there was discussion around providing policy
    controls around the AlwaysOnVpnLockdown network property, which was
    why we added a wrapper in MNCH.
    
    Since that never materialized, and since the one caller does not
    implement either callback in release, this CL removes the MNCH
    wrapper and removes the callbacks from the API to simplify it.
    
    For DVLOG removal in components/arc/net:
    TBR=hugobenichi@chromium.org
    
    Bug: 1054441
    Change-Id: Ib9581f22d4ed7f028694ef02db08294cf5ef014a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287388
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Philipp Weiß <phweiss@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Auto-Submit: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788671}

--- chromeos/network/managed_network_configuration_handler.h
+++ chromeos/network/managed_network_configuration_handler.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 class DictionaryValue;
 class ListValue;
-class Value;
 }  // namespace base
 
 namespace chromeos {
@@ -94,12 +93,6 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) ManagedNetworkConfigurationHandler {
   virtual void SetProperties(
       const std::string& service_path,
       const base::DictionaryValue& user_settings,
-      const base::Closure& callback,
-      const network_handler::ErrorCallback& error_callback) = 0;
-
-  virtual void SetManagerProperty(
-      const std::string& property_name,
-      const base::Value& value,
       const base::Closure& callback,
       const network_handler::ErrorCallback& error_callback) = 0;
 
