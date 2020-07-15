1f52dbb6ff6df2de7b0b16a9f63d34529a0702bb
Peter Kotwicz <pkotwicz@chromium.org>
2020-07-15 15:43:48 +0000

    [Android] Re-enable custom errorprone plugins
    The custom Chromium errorprone plugins were unintentionally disabled
    as a result of
    https://chromium-review.googlesource.com/c/chromium/src/+/2133870
    
    This CL re-enables the errorprone plugins by using the
    auto_service_processor in the javac compile of the
    errorprone_plugin java_binary. This preserves the META-INF service
    provider configuration generated by auto_service_processor.
    
    This CL:
    - Adds the 'processor_generates_service_provider_configuration'
    parameter to the java_annotation_processor() template to indicate
    whether the annotation processor generates the META-INF service provider
    configuration files. If a library depends on an annotation processor
    which has the parameter set, the annotation processor will be
    passed in the --processorpath and --processor parameters in the javac
    compile in addition to the turbine compile.
    - Adds the 'errorprone_stderr_path' to the java_library() template to
    save the stderr output of the errorprone compile to a file.
    
    BUG=1104558
    TEST=errorprone_plugin:tests
    
    Change-Id: If224e9a2cb62b3fae72ffb6664aa54770530672d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291983
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788647}

---
+++ tools/android/errorprone_plugin/test/src/org/chromium/tools/errorprone/plugin/TestClassNameCheckTesting.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.tools.errorprone.plugin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.BaseJUnit4ClassRunner;
+
+/**
+ * Should cause 'TestClassNameCheck' errorprone warning.
+ */
+@RunWith(BaseJUnit4ClassRunner.class)
+public class TestClassNameCheckTesting {
+    @Test
+    public void testRandom() {}
+}
