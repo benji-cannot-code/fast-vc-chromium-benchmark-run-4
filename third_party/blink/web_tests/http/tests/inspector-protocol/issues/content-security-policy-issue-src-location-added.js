12b3a5668be8edfa5aa388cb2a92502807e7fd4a
Kateryna Prokopenko <kprokopenko@google.com>
2020-07-15 15:47:15 +0000

    [issues] Add reporting of source location of url violations to CSP issues.
    Change-Id: I561d5893976fac92acf02329bf13b7422a9a2e91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279211
    Commit-Queue: Kateryna Prokopenko <kprokopenko@google.com>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788652}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/content-security-policy-issue-src-location-added.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+    const {page, session, dp} = await testRunner.startBlank(
+      `Verifies that CSP issue contains source location.\n`);
+
+    await dp.Network.enable();
+    await dp.Audits.enable();
+    page.navigate('https://devtools.test:8443/inspector-protocol/resources/content-security-policy-issue-with-src-location.html');
+    const issue = await dp.Audits.onceIssueAdded();
+
+    testRunner.log(issue.params, "Inspector issue: ");
+    testRunner.completeTest();
+  })
