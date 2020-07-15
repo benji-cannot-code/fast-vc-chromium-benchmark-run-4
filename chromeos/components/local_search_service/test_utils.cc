8fd0c88bb97fe971e9b0ebb0ffa105a169149c86
Maksim Ivanov <emaxx@chromium.org>
2020-07-15 01:00:20 +0000

    IWYU in local_search_service/test_utils.cc
    The <cmath> header is needed for the log() function.
    
    Without this, the build fails in some configurations (e.g., when
    building inside Chrome OS SDK).
    
    Bug: none
    Change-Id: I425913a03173936a1949c022679852cc5242618d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2297509
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788429}

--- chromeos/components/local_search_service/test_utils.cc
+++ chromeos/components/local_search_service/test_utils.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/components/local_search_service/test_utils.h"
 
+#include <cmath>
 #include <map>
 #include <string>
 #include <utility>
