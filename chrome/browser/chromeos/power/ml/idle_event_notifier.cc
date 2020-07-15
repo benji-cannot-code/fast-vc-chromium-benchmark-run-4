d429720f6c4916bfb37603ea59850db9345f3196
Peter Kasting <pkasting@chromium.org>
2020-07-15 17:48:37 +0000

    Trivial formatting/cleanup changes.
    * Improve comments
    * Use const and constexpr more
    * EXPECT -> static_assert where possible
    * kCamelCase for constant names
    * Spaces around operators
    * Prefer prefix operators
    * Initialize members in declarations
    * Use =default
    * ASSERT_TRUE -> ASSERT_GE,LT, etc.
    * No return after else
    
    Bug: none
    Change-Id: If2f609d5ec45b950a081eca25f7339f757bdd93a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2294024
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788707}

--- chrome/browser/chromeos/power/ml/idle_event_notifier.cc
+++ chrome/browser/chromeos/power/ml/idle_event_notifier.cc
@@ -20,6 +20,7 @@ namespace ml {
 
 using TimeSinceBoot = base::TimeDelta;
 
+// static
 constexpr base::TimeDelta IdleEventNotifier::kIdleDelay;
 constexpr base::TimeDelta IdleEventNotifier::kUserInputEventsDuration;
 constexpr int IdleEventNotifier::kNumUserInputEventsBuckets;
