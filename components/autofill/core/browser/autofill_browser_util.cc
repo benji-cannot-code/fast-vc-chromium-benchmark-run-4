db2b1546ffe2848868d40a231b92d305970a5fd7
Carlos IL <carlosil@chromium.org>
2020-07-15 02:30:27 +0000

    Initial implementation of on-form mixed content warnings.
    Implements on-form warning bubbles, and disables autofill for mixed
    content forms (forms on HTTPS sites that submit over HTTP).
    
    Pending work after this CL:
    -If we decide to do the same for password fields, a similar
    implementation for password manager.
    -The warning gets re-created with each typed character, we should not
    do this since it looks weird.
    -Login fields currently show the warning if there are no saved logins,
    this should be removed if we don't do this for login forms.
    
    Strings are not final and will likely be changed after UX review.
    
    Bug: 1093103
    Change-Id: If77c59f652aac17c44bcdd0558b91fc6199025d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292868
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788459}

--- components/autofill/core/browser/autofill_browser_util.cc
+++ components/autofill/core/browser/autofill_browser_util.cc
@@ -6,12 +6,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_browser_util.h"
 
 #include "components/autofill/core/browser/autofill_client.h"
+#include "services/network/public/cpp/is_potentially_trustworthy.h"
+
+namespace {
+// Matches the blink check for mixed content.
+bool IsInsecureFormAction(const GURL& action_url) {
+  if (action_url.SchemeIs(url::kBlobScheme) ||
+      action_url.SchemeIs(url::kFileSystemScheme))
+    return false;
+  return !network::IsOriginPotentiallyTrustworthy(
+      url::Origin::Create(action_url));
+}
+}  // namespace
 
 namespace autofill {
 
 bool IsFormOrClientNonSecure(AutofillClient* client, const FormData& form) {
   return !client->IsContextSecure() ||
          (form.action.is_valid() && form.action.SchemeIs("http"));
+}
+
+bool IsFormMixedContent(AutofillClient* client, const FormData& form) {
+  return client->IsContextSecure() &&
+         (form.action.is_valid() && IsInsecureFormAction(form.action));
 }
 
 bool ShouldAllowCreditCardFallbacks(AutofillClient* client,
