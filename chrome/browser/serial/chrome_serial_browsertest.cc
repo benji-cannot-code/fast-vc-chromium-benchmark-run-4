403bb687f81f8a5990ea760ff4583e58ad06d1f8
Kristi Park <kristipark@chromium.org>
2020-07-15 23:23:41 +0000

    [Sheriff] Disable flaky SerialTest.NavigateWithChooserCrossOrigin
    Multiple flakes in linux-chromeos-chrome (e.g. https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome/7058)
    
    TBR=reillyg@chromium.org
    
    Bug: 1069695
    Change-Id: Ib783ef93bbb2d005aa6f49446343f6786bd89a12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300663
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788785}

--- chrome/browser/serial/chrome_serial_browsertest.cc
+++ chrome/browser/serial/chrome_serial_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/command_line.h"
+#include "build/build_config.h"
 #include "chrome/browser/serial/serial_chooser_context.h"
 #include "chrome/browser/serial/serial_chooser_context_factory.h"
 #include "chrome/browser/ui/browser.h"
@@ -52,7 +53,14 @@ class SerialTest : public InProcessBrowserTest {
   SerialChooserContext* context_;
 };
 
-IN_PROC_BROWSER_TEST_F(SerialTest, NavigateWithChooserCrossOrigin) {
+// TODO(crbug/1069695): Flaky on linux-chromeos-chrome.
+#if defined(OS_LINUX)
+#define MAYBE_NavigateWithChooserCrossOrigin \
+  DISABLED_NavigateWithChooserCrossOrigin
+#else
+#define MAYBE_NavigateWithChooserCrossOrigin NavigateWithChooserCrossOrigin
+#endif
+IN_PROC_BROWSER_TEST_F(SerialTest, MAYBE_NavigateWithChooserCrossOrigin) {
   content::WebContents* web_contents =
       browser()->tab_strip_model()->GetActiveWebContents();
 
