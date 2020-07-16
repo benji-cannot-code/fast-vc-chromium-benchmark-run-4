f41e6f5266b41f601a58e7208e0abae7c0840152
Kent Tamura <tkent@chromium.org>
2020-07-16 09:39:06 +0000

    Remove ToLayotuButton*() macros
    They are unnecessary.  LayoutBox methods are enough.
    This CL has no behavior changes.
    
    Bug: 1040826
    Change-Id: Ic9458dfd0da5d6b70f318687f39ea52a17f6d023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2301689
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788983}

--- third_party/blink/renderer/core/paint/file_upload_control_painter.cc
+++ third_party/blink/renderer/core/paint/file_upload_control_painter.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/file_upload_control_painter.h"
 
 #include "base/optional.h"
-#include "third_party/blink/renderer/core/layout/layout_button.h"
+#include "third_party/blink/renderer/core/html/forms/html_input_element.h"
 #include "third_party/blink/renderer/core/layout/layout_file_upload_control.h"
 #include "third_party/blink/renderer/core/layout/text_run_constructor.h"
 #include "third_party/blink/renderer/core/paint/paint_info.h"
@@ -58,8 +58,7 @@ void FileUploadControlPainter::PaintObject(const PaintInfo& paint_info,
     LayoutUnit text_y;
     // We want to match the button's baseline
     // FIXME: Make this work with transforms.
-    if (LayoutButton* button_layout_object =
-            ToLayoutButton(button->GetLayoutObject()))
+    if (LayoutBox* button_layout_object = button->GetLayoutBox())
       text_y = paint_offset.top + layout_file_upload_control_.BorderTop() +
                layout_file_upload_control_.PaddingTop() +
                button_layout_object->BaselinePosition(
