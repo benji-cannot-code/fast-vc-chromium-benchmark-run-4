d0807da689f341ef0202b3c357b992963dd00f5c
Viviane Yang <viviy@google.com>
2020-07-16 07:51:51 +0000

    [Push] Handle inputs in StartPushSubsciprionChangeEvent
    Conversion from mojom::blink::PushSubscriptionPtr to
    blink::PushSubscription*.
    
    
    In a bigger picture, this change is for implementing
    the pushsubscriptionchange Event. Until now, only nullptr where passed
    since DispatchPushSubscriptionChangeEvent is never called.
    
    Change-Id: I976e436cc50340ac7bcac9e1f8b0d7e1c62b98e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2277888
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Viviane Yang <viviy@google.com>
    Cr-Commit-Position: refs/heads/master@{#788957}

--- third_party/blink/renderer/modules/service_worker/service_worker_global_scope.cc
+++ third_party/blink/renderer/modules/service_worker/service_worker_global_scope.cc
@@ -97,7 +97,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/payments/payment_request_respond_with_observer.h"
 #include "third_party/blink/renderer/modules/push_messaging/push_event.h"
 #include "third_party/blink/renderer/modules/push_messaging/push_message_data.h"
-#include "third_party/blink/renderer/modules/push_messaging/push_messaging_type_converter.h"
 #include "third_party/blink/renderer/modules/push_messaging/push_subscription_change_event.h"
 #include "third_party/blink/renderer/modules/service_worker/cross_origin_resource_policy_checker.h"
 #include "third_party/blink/renderer/modules/service_worker/extendable_event.h"
@@ -2064,8 +2063,9 @@ void ServiceWorkerGlobalScope::StartPushSubscriptionChangeEvent(
       this, WaitUntilObserver::kPushSubscriptionChange, event_id);
   Event* event = PushSubscriptionChangeEvent::Create(
       event_type_names::kPushsubscriptionchange,
-      new_subscription.To<blink::PushSubscription*>(),
-      old_subscription.To<blink::PushSubscription*>(), observer);
+      PushSubscription::Create(std::move(new_subscription), registration_),
+      PushSubscription::Create(std::move(old_subscription), registration_),
+      observer);
   DispatchExtendableEvent(event, observer);
 }
 
