6af3868ca63a4819f361b72fb6f73fda9d58c96c
Xinan Cheng <xinan@google.com>
2020-07-16 22:10:07 +0000

    [Fuchsia] Accept InterfaceHandle in FuchsiaAudioRenderer constructor.
    Previously FuchsiaAudioRenderer dependent on FuchsiaMediaResourceProvider,
    but not all clients will use it.
    
    Bug: b/149863069
    Change-Id: I7a8c46c7acdfa6a90674d23b25fb50fe5d8a7437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2293467
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Xinan Cheng <xinan@google.com>
    Cr-Commit-Position: refs/heads/master@{#789223}

--- media/fuchsia/audio/fuchsia_audio_renderer.cc
+++ media/fuchsia/audio/fuchsia_audio_renderer.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/sequenced_task_runner.h"
 #include "base/threading/sequenced_task_runner_handle.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "media/base/decoder_buffer.h"
 #include "media/base/renderer_client.h"
 #include "media/filters/decrypting_demuxer_stream.h"
@@ -79,17 +80,12 @@ constexpr size_t kNumBuffers = 16;
 
 FuchsiaAudioRenderer::FuchsiaAudioRenderer(
     MediaLog* media_log,
-    mojo::PendingRemote<media::mojom::FuchsiaMediaResourceProvider>
-        pending_media_resource_provider)
-    : media_log_(media_log) {
+    fidl::InterfaceHandle<fuchsia::media::AudioConsumer> audio_consumer_handle)
+    : media_log_(media_log),
+      audio_consumer_handle_(std::move(audio_consumer_handle)) {
   DETACH_FROM_THREAD(thread_checker_);
-
-  mojo::Remote<media::mojom::FuchsiaMediaResourceProvider>
-      media_resource_provider;
-  media_resource_provider.Bind(std::move(pending_media_resource_provider));
-  media_resource_provider->CreateAudioConsumer(
-      audio_consumer_handle_.NewRequest());
 }
+
 
 FuchsiaAudioRenderer::~FuchsiaAudioRenderer() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
