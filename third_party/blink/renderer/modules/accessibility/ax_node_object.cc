b9985cc07cecb9b728ac6a0c4f994b8983f4576b
Aaron Leventhal <aleventhal@chromium.org>
2020-07-16 19:46:19 +0000

    Expose autocompletes and autofills with haspopup state
    Users need to be informed when pressing down arrow would allow
    perusing options for filling in a textfield. This does not work in
    JAWS for autocomplete, but does for autofill. The difference is
    whether we expose STATE_SYSTEM_HASPOPUP.
    
    This CL exposes the haspopup state across all platforms, for both
    autofill and autocomplete. From the user's point of view they are
    choosing options from a list either way.
    
    R=akihiroota@chromium.org,dmazzoni@chromium.org
    
    AX-Relnotes: more consistent announcements for autocomplete textfields
    Bug: 1106044
    Change-Id: I4f0b291c3d3478a3980f73e83474d022f541df7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2299851
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789161}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -2588,6 +2588,11 @@ ax::mojom::blink::HasPopup AXNodeObject::HasPopup() const {
       RoleValue() == ax::mojom::blink::Role::kTextFieldWithComboBox)
     return ax::mojom::blink::HasPopup::kListbox;
 
+  if (AXObjectCache().GetAutofillState(AXObjectID()) !=
+      WebAXAutofillState::kNoSuggestions) {
+    return ax::mojom::blink::HasPopup::kMenu;
+  }
+
   return AXObject::HasPopup();
 }
 
