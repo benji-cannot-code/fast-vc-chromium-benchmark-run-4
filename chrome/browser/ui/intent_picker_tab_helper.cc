ec8cbf41eedee2b53c1393942fff1f6f16c7b04a
nancylingwang <nancylingwang@chromium.org>
2020-07-16 00:32:01 +0000

    Modify intent picker to show the standard icon.
    Similar to the CL:2271958, modify the load icon interface for the intent
    picker, to load the new standard icons.
    
    Design doc: go/appservice-adaptive-icon
    
    BUG=1083331
    
    Change-Id: Ie6b047961cd0a402595d30d734c33325a1e6f9c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2299684
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788811}

--- chrome/browser/ui/intent_picker_tab_helper.cc
+++ chrome/browser/ui/intent_picker_tab_helper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/common/chrome_features.h"
 #include "ui/gfx/favicon_size.h"
 #include "ui/gfx/image/image.h"
 
@@ -109,8 +110,12 @@ void IntentPickerTabHelper::LoadAppIcon(
   }
 
   constexpr bool allow_placeholder_icon = false;
-  proxy->LoadIcon(app_type, app_id, apps::mojom::IconType::kUncompressed,
-                  gfx::kFaviconSize, allow_placeholder_icon,
+  auto icon_type =
+      (base::FeatureList::IsEnabled(features::kAppServiceAdaptiveIcon))
+          ? apps::mojom::IconType::kStandard
+          : apps::mojom::IconType::kUncompressed;
+  proxy->LoadIcon(app_type, app_id, icon_type, gfx::kFaviconSize,
+                  allow_placeholder_icon,
                   base::BindOnce(&IntentPickerTabHelper::OnAppIconLoaded,
                                  weak_factory_.GetWeakPtr(), std::move(apps),
                                  std::move(callback), index));
