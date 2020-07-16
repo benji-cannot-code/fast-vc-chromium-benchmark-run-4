4e9e50b502af8af4dcbf5b43c87cd22fac8dac70
Wenhan (Han) Zhang <zwenhan@google.com>
2020-07-16 15:59:26 +0000

    [Reland] Pretty-print histograms and enums xml with model representation
    Removed imports and references related to histograms_print_style.py
    which is now replaced by histogram_configuration_model.py.
    
    Original description below:
    
    Now enums and histograms pretty-printed using model representation,
    like how ukm and rappor currently are.
    
    Change-Id: I3b3a29333b9d4fa2418ee4faa64cf14c59d6341f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300324
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Wenhan (Han) Zhang <zwenhan@google.com>
    Cr-Commit-Position: refs/heads/master@{#789061}

--- tools/metrics/histograms/merge_xml.py
+++ tools/metrics/histograms/merge_xml.py
@@ -13,7 +13,7 @@ import xml.dom.minidom
 
 import expand_owners
 import extract_histograms
-import histograms_print_style
+import histogram_configuration_model
 import populate_enums
 
 
@@ -107,7 +107,7 @@ def MergeFiles(filenames=[], files=[]):
 
 
 def PrettyPrintMergedFiles(filenames=[], files=[]):
-  return histograms_print_style.GetPrintStyle().PrettyPrintXml(
+  return histogram_configuration_model.PrettifyTree(
       MergeFiles(filenames=filenames, files=files))
 
 
--- tools/metrics/histograms/pretty_print_test.py
+++ tools/metrics/histograms/pretty_print_test.py
@@ -22,7 +22,6 @@ ORIGINAL_XML = """
 
         It has multiple paragraphs.
    </summary>
-   Mixed content.
    <obsolete>
        Removed 1/2019.
    </obsolete>
@@ -33,8 +32,6 @@ ORIGINAL_XML = """
   <obsolete>Obsolete 1</obsolete>
   <obsolete>Obsolete 2</obsolete>
   <enums>This shouldn't be here</enums>
-  <component>Component</component>
-  <component>Other&gt;Component</component>
  </histogram>
 
  <histogram_suffixes name="Test.HistogramSuffixes" separator=".">
@@ -48,9 +45,9 @@ ORIGINAL_XML = """
 
 <histogram name="Test.MisplacedHistogram" units="us">
    <owner>person@chromium.org</owner>
+   Misplaced content.
    <summary>A misplaced histogram
    </summary>
-   Misplaced content.
  </histogram>
 
 </histogram_suffixes_list>
@@ -74,8 +71,6 @@ PRETTY_XML = """
     Obsolete 1
   </obsolete>
   <summary>Foo</summary>
-  <component>Component</component>
-  <component>Other&gt;Component</component>
 </histogram>
 
 <histogram name="Test.Histogram" units="microseconds">
@@ -89,13 +84,11 @@ PRETTY_XML = """
 
     It has multiple paragraphs.
   </summary>
-  Mixed content.
 </histogram>
 
 <histogram name="Test.MisplacedHistogram" units="microseconds">
   <owner>person@chromium.org</owner>
   <summary>A misplaced histogram</summary>
-  Misplaced content.
 </histogram>
 
 </histograms>
