076d4677065d9ce6948fd5abfddec60de384e39d
Aaron Leventhal <aleventhal@google.com>
2020-07-16 15:04:54 +0000

    Allow disabled elements to report selected state
    A disabled element can have the selected state, but it is not
    selectable. In other words, the current selection state should be
    exposed, but it is not changeable.
    
    This change allows the currently selected item in a disabled
    container widget to be announced by a screen reader.
    
    AX-Relnotes: fix for some Google Form results not being read.
    Bug: 1027275
    Change-Id: I885e4b9e9aad928caf458b6bc7880743b1795452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290890
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789048}

--- ui/accessibility/platform/ax_platform_node_win.cc
+++ ui/accessibility/platform/ax_platform_node_win.cc
@@ -2077,11 +2077,8 @@ IFACEMETHODIMP AXPlatformNodeWin::get_VerticalViewSize(double* result) {
 HRESULT AXPlatformNodeWin::ISelectionItemProviderSetSelected(
     bool selected) const {
   UIA_VALIDATE_CALL();
-  int restriction;
-  if (GetIntAttribute(ax::mojom::IntAttribute::kRestriction, &restriction)) {
-    if (restriction == static_cast<int>(ax::mojom::Restriction::kDisabled))
-      return UIA_E_ELEMENTNOTENABLED;
-  }
+  if (GetData().GetRestriction() == ax::mojom::Restriction::kDisabled)
+    return UIA_E_ELEMENTNOTENABLED;
 
   // The platform implements selection follows focus for single-selection
   // container elements. Focus action can change a node's accessibility selected
@@ -7391,7 +7388,7 @@ int AXPlatformNodeWin::MSAAState() const {
   // TODO(dougt) unhandled ux::ax::mojom::State::kRequired
   // TODO(dougt) unhandled ux::ax::mojom::State::kRichlyEditable
 
-  if (data.HasBoolAttribute(ax::mojom::BoolAttribute::kSelected))
+  if (data.IsSelectable())
     msaa_state |= STATE_SYSTEM_SELECTABLE;
 
   if (data.GetBoolAttribute(ax::mojom::BoolAttribute::kSelected))
