f1cd1cdc31f3863189ae61b1634a3788dcfa89f6
Haiyang Pan <hypan@google.com>
2020-07-16 16:45:35 +0000

    Revert "fido: make AuthenticatorTestBase set the MOCK_TIME TaskEnvironment trait"
    This reverts commit f55cd37e3222c274447a6af8059b0f7cdc00b568.
    
    Reason for revert: The tests testAuthenticatorImplGetAssertionBridge_resultCanceled and testAuthenticatorImplGetAssertionBridgeWithUvmRequestedWithUvmResponded_success in org.chromium.chrome.browser.webauth.Fido2CredentialRequestTest consistently fail on android-pie-x86-rel since https://ci.chromium.org/p/chromium/builders/ci/android-pie-x86-rel/1498.
    This CL is the only one that touches authenticator_impl around that time. Revert it to see if it can fix the failure
    
    Original change's description:
    > fido: make AuthenticatorTestBase set the MOCK_TIME TaskEnvironment trait
    > 
    > This allows us to get rid of base::Timer injection into
    > AuthenticatorCommon, which tests used to wait for requests to time out.
    > 
    > Also get remove a number of superfluous RunUntilIdle() calls, and of
    > calls to OverrideLastCommittedOrigin() where SimulateNavigation() would
    > suffice.
    > 
    > Change-Id: I8cb4980d51efe43f59d828cde5f34ce67d38d928
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2299630
    > Auto-Submit: Martin Kreichgauer <martinkr@google.com>
    > Reviewed-by: Jared Saul <jsaul@google.com>
    > Reviewed-by: Adam Langley <agl@chromium.org>
    > Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    > Cr-Commit-Position: refs/heads/master@{#788885}
    
    TBR=agl@chromium.org,jsaul@google.com,martinkr@google.com
    
    Change-Id: I47c8796582f40f1946811b5eca4ae3cdfe5aac0f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303030
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/master@{#789079}

--- content/browser/webauth/authenticator_impl.cc
+++ content/browser/webauth/authenticator_impl.cc
@@ -18,9 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 AuthenticatorImpl::AuthenticatorImpl(RenderFrameHost* render_frame_host)
-    : AuthenticatorImpl(
-          render_frame_host,
-          std::make_unique<AuthenticatorCommon>(render_frame_host)) {}
+    : AuthenticatorImpl(render_frame_host,
+                        std::make_unique<AuthenticatorCommon>(
+                            render_frame_host,
+                            std::make_unique<base::OneShotTimer>())) {}
 
 AuthenticatorImpl::AuthenticatorImpl(
     RenderFrameHost* render_frame_host,
