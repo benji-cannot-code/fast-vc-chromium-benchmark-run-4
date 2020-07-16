9593e962800ebc5c68d8c0479a20dbc9c3c76565
Xianzhu Wang <wangxianzhu@chromium.org>
2020-07-16 23:06:05 +0000

    Use display item visual rect for SVG
    This is a part of the work of removing from
    DisplayItemClient::VisualRect(). Now we calculate visual rect for
    display items during paint.
    
    For most SVG objects, the visual rect is simply
    EnclosingIntRect(object.VisualRectInLocalSVGCoordinates()) (not
    including outlines).
    
    Bug: 1104064
    Change-Id: If72e66238585c17c48fda21a80060c24029b25f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2301206
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789249}

--- third_party/blink/renderer/core/paint/svg_root_inline_box_painter.cc
+++ third_party/blink/renderer/core/paint/svg_root_inline_box_painter.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/scoped_svg_paint_state.h"
 #include "third_party/blink/renderer/core/paint/svg_inline_flow_box_painter.h"
 #include "third_party/blink/renderer/core/paint/svg_inline_text_box_painter.h"
+#include "third_party/blink/renderer/core/paint/svg_model_object_painter.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
 
 namespace blink {
@@ -25,15 +26,13 @@ void SVGRootInlineBoxPainter::Paint(const PaintInfo& paint_info,
   bool has_selection =
       !paint_info.IsPrinting() && svg_root_inline_box_.IsSelected();
 
-  if (has_selection && !DrawingRecorder::UseCachedDrawingIfPossible(
-                           paint_info.context,
-                           *LineLayoutAPIShim::ConstLayoutObjectFrom(
-                               svg_root_inline_box_.GetLineLayoutItem()),
-                           paint_info.phase)) {
-    DrawingRecorder recorder(paint_info.context,
-                             *LineLayoutAPIShim::ConstLayoutObjectFrom(
-                                 svg_root_inline_box_.GetLineLayoutItem()),
-                             paint_info.phase);
+  const auto& layout_object = *LineLayoutAPIShim::ConstLayoutObjectFrom(
+      svg_root_inline_box_.GetLineLayoutItem());
+  if (has_selection &&
+      !DrawingRecorder::UseCachedDrawingIfPossible(
+          paint_info.context, layout_object, paint_info.phase)) {
+    SVGDrawingRecorder recorder(paint_info.context, layout_object,
+                                paint_info.phase);
     for (InlineBox* child = svg_root_inline_box_.FirstChild(); child;
          child = child->NextOnLine()) {
       if (auto* svg_inline_text_box = DynamicTo<SVGInlineTextBox>(child)) {
