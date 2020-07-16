f1f50c1c34627d18983da835ec73ba37008f57e1
Marijn Kruisselbrink <mek@chromium.org>
2020-07-16 18:51:27 +0000

    Make sure all methods have a correct name.
    Adds an assertion to idlharness to check the name attribute of
    methods, and fixes the bindings code to make sure we actually set the
    name correctly.
    
    Bug: 1106097, 1097814
    Change-Id: I73dea7b96a94a9b2925c23156e6ef8146291cbc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300824
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789135}

--- third_party/blink/web_tests/external/wpt/resources/idlharness.js
+++ third_party/blink/web_tests/external/wpt/resources/idlharness.js
@@ -2535,6 +2535,10 @@ IdlInterface.prototype.do_member_operation_asserts = function(memberHolderObject
         memberHolderObject[member.name].length,
         minOverloadLength(ctors),
         "property has wrong .length");
+    assert_equals(
+        memberHolderObject[member.name].name,
+        member.name,
+        "property has wrong .name");
 
     // Make some suitable arguments
     var args = member.arguments.map(function(arg) {
