e90e5eb4be26f09269726e2737a439f731d73242
Etienne Pierre-doray <etiennep@chromium.org>
2020-07-16 14:24:10 +0000

    [ThreadPool]: Remove NumWorkers and NumActiveWorkers histograms.
    Not used in active investigations and expiring soon.
    
    Bug: 1105064, 1105067
    Change-Id: I587e2df361526494c0564ab18470d4d3e131e92b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300264
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789041}

--- base/task/thread_pool/thread_group_impl.h
+++ base/task/thread_pool/thread_group_impl.h
@@ -94,7 +94,6 @@ class BASE_EXPORT ThreadGroupImpl : public ThreadGroup {
   // ThreadGroup:
   void JoinForTesting() override;
   size_t GetMaxConcurrentNonBlockedTasksDeprecated() const override;
-  void ReportHeartbeatMetrics() const override;
   void DidUpdateCanRunPolicy() override;
 
   const HistogramBase* num_tasks_before_detach_histogram() const {
@@ -352,14 +351,6 @@ class BASE_EXPORT ThreadGroupImpl : public ThreadGroup {
   // ThreadPool.NumTasksBeforeDetach.[thread group name] histogram.
   // Intentionally leaked.
   HistogramBase* const num_tasks_before_detach_histogram_;
-
-  // ThreadPool.NumWorkers.[thread group name] histogram.
-  // Intentionally leaked.
-  HistogramBase* const num_workers_histogram_;
-
-  // ThreadPool.NumActiveWorkers.[thread group name] histogram.
-  // Intentionally leaked.
-  HistogramBase* const num_active_workers_histogram_;
 
   // Ensures recently cleaned up workers (ref.
   // WorkerThreadDelegateImpl::CleanupLockRequired()) had time to exit as
--- base/task/thread_pool/thread_group_impl_unittest.cc
+++ base/task/thread_pool/thread_group_impl_unittest.cc
@@ -1838,15 +1838,5 @@ TEST_F(ThreadGroupImplImplStartInBodyTest, RacyCleanup) {
   thread_group_.reset();
 }
 
-TEST_P(ThreadGroupImplImplTestParam, ReportHeartbeatMetrics) {
-  HistogramTester tester;
-  thread_group_->ReportHeartbeatMetrics();
-  EXPECT_FALSE(
-      tester.GetAllSamples("ThreadPool.NumWorkers.TestThreadGroup").empty());
-  EXPECT_FALSE(
-      tester.GetAllSamples("ThreadPool.NumActiveWorkers.TestThreadGroup")
-          .empty());
-}
-
 }  // namespace internal
 }  // namespace base
