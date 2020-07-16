56432f50252f341733cd7af88867c55f59b4788b
Vladimir Levin <vmpstr@chromium.org>
2020-07-16 19:29:17 +0000

    viz: Refactor RemapPassId into a separate class.
    This patch refactors SurfaceAggregator::RemapPassId into a separate
    class. The intent of this is to clean up the code a bit, and to allow
    the remapper to be extended in the future by allowing persistent ids
    for certain render passes.
    
    R=kylechar@chromium.org, weiliangc@chromium.org
    Bug: 1106418
    
    Change-Id: I4ffffd15dd4fcd6a51df88f4d14666a5ff320d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2280262
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789148}

--- components/viz/service/display/surface_aggregator.h
+++ components/viz/service/display/surface_aggregator.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/resources/transferable_resource.h"
 #include "components/viz/common/surfaces/surface_id.h"
 #include "components/viz/common/surfaces/surface_range.h"
+#include "components/viz/service/display/render_pass_id_remapper.h"
 #include "components/viz/service/viz_service_export.h"
 #include "ui/gfx/display_color_spaces.h"
 #include "ui/gfx/overlay_transform.h"
@@ -93,13 +94,6 @@ class VIZ_SERVICE_EXPORT SurfaceAggregator {
   struct ChildSurfaceInfo;
   struct RenderPassMapEntry;
 
-  struct RenderPassInfo {
-    // This is the id the pass is mapped to.
-    int id;
-    // This is true if the pass was used in the last aggregated frame.
-    bool in_use = true;
-  };
-
   // Helper function that gets a list of render passes and returns a map from
   // render pass ids to render passes.
   static base::flat_map<RenderPassId, RenderPassMapEntry> GenerateRenderPassMap(
@@ -109,9 +103,6 @@ class VIZ_SERVICE_EXPORT SurfaceAggregator {
   ClipData CalculateClipRect(const ClipData& surface_clip,
                              const ClipData& quad_clip,
                              const gfx::Transform& target_transform);
-
-  RenderPassId RemapPassId(RenderPassId surface_local_pass_id,
-                           const SurfaceId& surface_id);
 
   void HandleSurfaceQuad(const SurfaceDrawQuad* surface_quad,
                          float parent_device_scale_factor,
@@ -323,13 +314,6 @@ class VIZ_SERVICE_EXPORT SurfaceAggregator {
   SurfaceManager* manager_;
   DisplayResourceProvider* provider_;
 
-  // Every Surface has its own RenderPass ID namespace. This structure maps
-  // each source (SurfaceId, RenderPass id) to a unified ID namespace that's
-  // used in the aggregated frame. An entry is removed from the map if it's not
-  // used for one output frame.
-  base::flat_map<std::pair<SurfaceId, RenderPassId>, RenderPassInfo>
-      render_pass_allocator_map_;
-  RenderPassId next_render_pass_id_;
   const bool aggregate_only_damaged_;
   bool output_is_secure_;
 
@@ -451,6 +435,10 @@ class VIZ_SERVICE_EXPORT SurfaceAggregator {
   // a surface contains delegated ink metadata on its frame, and it is cleared
   // after it is placed on the final aggregated frame during aggregation.
   std::unique_ptr<DelegatedInkMetadata> delegated_ink_metadata_;
+
+  // A helper class used to remap render pass IDs from the surface namespace to
+  // a common space, to avoid collisions.
+  RenderPassIdRemapper pass_id_remapper_;
 
   base::WeakPtrFactory<SurfaceAggregator> weak_factory_{this};
 
