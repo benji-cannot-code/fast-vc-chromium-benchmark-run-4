99f7503a2ae6a087bb37175209b5c7e56787c675
Johann <johannkoenig@google.com>
2020-07-16 01:49:21 +0000

    BluetoothRemoteGATTServer: remove context observer and prefinalizer
    With the default HeapMojoRemote setting there was a bad
    interaction between service_ and the GATTServer cleanup. service_
    would get cleaned up in its own OnContextDestroyed method but
    then the GattServer would try to access it in Dispose. This
    was worked around by using kForceWithoutContextObserver.
    
    In BluetoothGATTServer there should not be any need for manual
    cleanup: service_ can use the OnContextDestroyed method and the
    other object being cleaned up, active_algorithms_, is a HeapHashSet
    with its own associated garbage collection.
    
    Bug: 1049056
    Change-Id: Ia81e99743c1f8bdccf37b54d0761258571a3ad4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2275515
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#788852}

--- third_party/blink/renderer/modules/bluetooth/bluetooth_remote_gatt_server.h
+++ third_party/blink/renderer/modules/bluetooth/bluetooth_remote_gatt_server.h
@@ -28,22 +28,15 @@ class ScriptState;
 // bluetooth peripheral.
 class BluetoothRemoteGATTServer
     : public ScriptWrappable,
-      public ExecutionContextLifecycleObserver,
       public mojom::blink::WebBluetoothServerClient {
-  USING_PRE_FINALIZER(BluetoothRemoteGATTServer, Dispose);
   DEFINE_WRAPPERTYPEINFO();
   USING_GARBAGE_COLLECTED_MIXIN(BluetoothRemoteGATTServer);
 
  public:
   BluetoothRemoteGATTServer(ExecutionContext*, BluetoothDevice*);
 
-  // ExecutionContextLifecycleObserver:
-  void ContextDestroyed() override;
-
   // mojom::blink::WebBluetoothServerClient:
   void GATTServerDisconnected() override;
-
-  void SetConnected(bool connected) { connected_ = connected; }
 
   // The Active Algorithms set is maintained so that disconnection, i.e.
   // disconnect() method or the device disconnecting by itself, can be detected
@@ -56,8 +49,6 @@ class BluetoothRemoteGATTServer
   // Removes |resolver| from the set of Active Algorithms if it was in the set
   // and returns true, false otherwise.
   bool RemoveFromActiveAlgorithms(ScriptPromiseResolver*);
-  // Removes all ScriptPromiseResolvers from the set of Active Algorithms.
-  void ClearActiveAlgorithms() { active_algorithms_.clear(); }
 
   // If gatt is connected then sets gatt.connected to false and disconnects.
   // This function only performs the necessary steps to ensure a device
@@ -70,10 +61,6 @@ class BluetoothRemoteGATTServer
   void CleanupDisconnectedDeviceAndFireEvent();
 
   void DispatchDisconnected();
-
-  // USING_PRE_FINALIZER interface.
-  // Called before the object gets garbage collected.
-  void Dispose();
 
   // Interface required by Garbage Collectoin:
   void Trace(Visitor*) const override;
@@ -112,9 +99,9 @@ class BluetoothRemoteGATTServer
   // using this server’s connection.
   HeapHashSet<Member<ScriptPromiseResolver>> active_algorithms_;
 
+  const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   HeapMojoAssociatedReceiverSet<mojom::blink::WebBluetoothServerClient,
-                                BluetoothRemoteGATTServer,
-                                HeapMojoWrapperMode::kWithoutContextObserver>
+                                BluetoothRemoteGATTServer>
       client_receivers_;
 
   Member<BluetoothDevice> device_;
