52d092c8f8008be30567b361fc3dbd1507c12f1c
Charlie Hu <chenleihu@google.com>
2020-07-16 16:02:36 +0000

    Add DocumentPolicyNegotiation flag
    This CL split the DocumentPolicy negotiation mechanism out from
    DocumentPolicy runtime flag so that DocumentPolicy can be shipped
    without the negotiation mechanism, which includes:
    
    - Request HTTP header: Sec-Required-Document-Policy
    - Response HTTP header: Required-Document-Policy
    - Iframe attribute: policy
    
    Change-Id: I789fdeab9232ff7d69814af0ea478cc4d5f128a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287814
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Cr-Commit-Position: refs/heads/master@{#789064}

--- third_party/blink/renderer/core/html/html_frame_owner_element.cc
+++ third_party/blink/renderer/core/html/html_frame_owner_element.cc
@@ -324,6 +324,10 @@ void HTMLFrameOwnerElement::UpdateContainerPolicy() {
 }
 
 void HTMLFrameOwnerElement::UpdateRequiredPolicy() {
+  if (!RuntimeEnabledFeatures::DocumentPolicyNegotiationEnabled(
+          GetExecutionContext()))
+    return;
+
   auto* frame = GetDocument().GetFrame();
   DocumentPolicy::FeatureState new_required_policy =
       frame
