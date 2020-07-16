ed7c87c2393e116998a7f323c06e360bf01c590f
Mohamed Amir Yosef <mamir@chromium.org>
2020-07-16 11:46:21 +0000

    [Passwords] Circle Favicon badge in the move bubble banner
    This CL brings the move banner one step closer to the mocks.
    Follow up CLs will polish it more.
    
    Screenshots:
    Example 1:https://screenshot.googleplex.com/HJEjgB1jNSt.png
    Example 2:https://screenshot.googleplex.com/zR8GEYpfCvX.png
    
    Bug: 1100814
    
    Change-Id: I17d7234c3da059508ff40d02abe33d83036026af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2301772
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789011}

--- chrome/browser/ui/views/passwords/move_to_account_store_bubble_view.cc
+++ chrome/browser/ui/views/passwords/move_to_account_store_bubble_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/ui/passwords/bubble_controllers/move_to_account_store_bubble_controller.h"
 #include "chrome/browser/ui/passwords/passwords_model_delegate.h"
 #include "chrome/browser/ui/views/accessibility/non_accessible_image_view.h"
@@ -38,8 +39,7 @@ namespace {
 constexpr int kBadgeSpacing = 4;
 constexpr int kImageSize = BadgedProfilePhoto::kImageSize;
 // Width and Height of the badged icon.
-constexpr int kBadgedProfilePhotoWidth = kImageSize + kBadgeSpacing;
-constexpr int kBadgedProfilePhotoHeight = kImageSize;
+constexpr int kBadgedProfilePhotoSize = kImageSize + kBadgeSpacing;
 
 // An image view that shows a vector icon and tracks changes in the theme.
 class VectorIconView : public views::ImageView {
@@ -103,18 +103,20 @@ void ImageWithBadge::AddDefaultBadge() {
   // Use a Globe icon as the default badge.
   auto badge_view =
       std::make_unique<VectorIconView>(kGlobeIcon, kBadgeIconSize);
-  badge_view->SetPosition(
-      gfx::Point(kBadgedProfilePhotoWidth - kBadgeIconSize,
-                 kBadgedProfilePhotoHeight - kBadgeIconSize));
+  badge_view->SetPosition(gfx::Point(kBadgedProfilePhotoSize - kBadgeIconSize,
+                                     kBadgedProfilePhotoSize - kBadgeIconSize));
   badge_view->SizeToPreferredSize();
   badge_view_ = AddChildView(std::move(badge_view));
 
-  SetPreferredSize(
-      gfx::Size(kBadgedProfilePhotoWidth, kBadgedProfilePhotoHeight));
+  SetPreferredSize(gfx::Size(kBadgedProfilePhotoSize, kBadgedProfilePhotoSize));
 }
 
 void ImageWithBadge::UpdateBadge(const gfx::ImageSkia& badge_image) {
-  badge_view_->SetImage(badge_image);
+  gfx::Image rounded_badge = profiles::GetSizedAvatarIcon(
+      gfx::Image(badge_image),
+      /*is_rectangle=*/true, /*width=*/gfx::kFaviconSize,
+      /*height=*/gfx::kFaviconSize, profiles::SHAPE_CIRCLE);
+  badge_view_->SetImage(rounded_badge.ToImageSkia());
   badge_view_->SizeToPreferredSize();
 }
 
