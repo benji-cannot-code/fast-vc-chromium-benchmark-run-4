e3361b9ae0c33a64fe24e7029191eba4c751e447
ckitagawa <ckitagawa@chromium.org>
2020-07-16 13:14:40 +0000

    [Paint Preview] Disable PaintPreviewPlayerTests temporarily
    These tests are blocking a Skia update without a good reason. For now
    they will be disabled.
    
    Swapping out the SkPicture directly is non-trivial due to how we used
    googlestorage and need to update the stored copy when the test isn't
    live to avoid issues with bots having out of sync data compared to the
    test. For now I'm just going to disable all the affected tests.
    
    Bug: 1106035
    Change-Id: I3db25f37c311e951500ce60abe5d61e0fe0b5ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300277
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789021}

--- components/paint_preview/player/android/javatests/src/org/chromium/components/paintpreview/player/PaintPreviewPlayerTest.java
+++ components/paint_preview/player/android/javatests/src/org/chromium/components/paintpreview/player/PaintPreviewPlayerTest.java
@@ -22,6 +22,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.CallbackHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.ScalableTimeout;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
@@ -85,6 +86,7 @@ public class PaintPreviewPlayerTest extends DummyUiActivityTestCase {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1106035")
     public void singleFrameDisplayTest() {
         initPlayerManager();
         final View playerHostView = mPlayerManager.getView();
@@ -106,6 +108,7 @@ public class PaintPreviewPlayerTest extends DummyUiActivityTestCase {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1106035")
     public void linkClickTest() {
         initPlayerManager();
         final View playerHostView = mPlayerManager.getView();
@@ -130,6 +133,7 @@ public class PaintPreviewPlayerTest extends DummyUiActivityTestCase {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1106035")
     public void overscrollRefreshTest() throws Exception {
         initPlayerManager();
         UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
