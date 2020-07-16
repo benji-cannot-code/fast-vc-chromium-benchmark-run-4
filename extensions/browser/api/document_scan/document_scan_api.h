20716813aefac76161a5e1691f3f4cda0bd61115
Jesse Schettler <jschettler@chromium.org>
2020-07-16 00:58:10 +0000

    document_scan: Refactor the Document Scan API
    The Document Scan API is only supported on Chrome OS, so ensure it is
    only built on Chrome OS. Additionally, remove the unnecessary
    DocumentScanInterface layer now that there doesn't have to be interface
    implementations for Chrome OS and non-Chrome OS builds.
    
    Bug: None
    Test: Perform a scan using the Scan extension
    Change-Id: Ie18d15cac752e9166be6ee538cae3a7e3c8fe4bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2296642
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788820}

--- extensions/browser/api/document_scan/document_scan_api.h
+++ extensions/browser/api/document_scan/document_scan_api.h
@@ -8,13 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
-#include <vector>
 
-#include "extensions/browser/api/document_scan/document_scan_interface.h"
+#include "base/optional.h"
+#include "chromeos/dbus/lorgnette/lorgnette_service.pb.h"
 #include "extensions/browser/extension_function.h"
 #include "extensions/common/api/document_scan.h"
 
 namespace extensions {
+
 namespace api {
 
 class DocumentScanScanFunction : public ExtensionFunction {
@@ -34,18 +35,14 @@ class DocumentScanScanFunction : public ExtensionFunction {
   friend class DocumentScanScanFunctionTest;
 
   void OnScannerListReceived(
-      const std::vector<DocumentScanInterface::ScannerDescription>&
-          scanner_descriptions,
-      const std::string& error);
-  void OnResultsReceived(const std::string& scanned_image,
-                         const std::string& mime_type,
-                         const std::string& error);
+      base::Optional<lorgnette::ListScannersResponse> response);
+  void OnResultsReceived(base::Optional<std::string> scanned_image);
 
   std::unique_ptr<document_scan::Scan::Params> params_;
-  std::unique_ptr<DocumentScanInterface> document_scan_interface_;
 };
 
 }  // namespace api
+
 }  // namespace extensions
 
 #endif  // EXTENSIONS_BROWSER_API_DOCUMENT_SCAN_DOCUMENT_SCAN_API_H_
