0b414182b3e5bbf09ee33ebfa08ab41a9c63e8b7
Sophey Dong <sophey@chromium.org>
2020-07-17 17:55:12 +0000

    [QRCode Desktop] Disable Download button upon error.
    Bug: 1105139
    Change-Id: Idf56cc5a8fdc30490c750f22e3e6341100fa0370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303462
    Reviewed-by: Travis Skare <skare@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789537}

--- chrome/browser/ui/views/qrcode_generator/qrcode_generator_bubble.cc
+++ chrome/browser/ui/views/qrcode_generator/qrcode_generator_bubble.cc
@@ -151,6 +151,7 @@ void QRCodeGeneratorBubble::OnCodeGeneratorResponse(
 
   ShrinkAndHideDisplay(center_error_label_);
   bottom_error_label_->SetVisible(false);
+  download_button_->SetEnabled(true);
   gfx::ImageSkia image = gfx::ImageSkia::CreateFrom1xBitmap(response->bitmap);
   UpdateQRImage(image);
 }
@@ -167,6 +168,7 @@ void QRCodeGeneratorBubble::DisplayPlaceholderImage() {
 }
 
 void QRCodeGeneratorBubble::DisplayError(mojom::QRCodeGeneratorError error) {
+  download_button_->SetEnabled(false);
   if (error == mojom::QRCodeGeneratorError::INPUT_TOO_LONG) {
     ShrinkAndHideDisplay(center_error_label_);
     DisplayPlaceholderImage();
