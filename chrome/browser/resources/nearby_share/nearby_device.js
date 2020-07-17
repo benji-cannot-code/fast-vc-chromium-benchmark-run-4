092305c985d70244e17a85bff4c1021b6d8f1c77
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2020-07-17 13:07:04 +0000

    [Nearby] Add default device icon and gradient.
    Screenshot: https://screenshot.googleplex.com/QRFx2waLZvJ.png
    
    Bug: 1103190
    Change-Id: I0f8eb5e3135db9e1ebd762ede8ce80765b2d69ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2302615
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789456}

--- chrome/browser/resources/nearby_share/nearby_device.js
+++ chrome/browser/resources/nearby_share/nearby_device.js
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview The 'nearby-device' component shows details of a remote device.
  */
 
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import './icons.js';
+
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 Polymer({
