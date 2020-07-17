1ce3279575e410675f2f869265886e7276ece4bc
Robert Ogden <robertogden@chromium.org>
2020-07-17 18:44:42 +0000

    Remove Loading Predictor preconnect for Prerenders
    This has the potential to leak user information during an isolated
    prerender and isn't otherwise beneficial in the context of a prerender.
    
    Bug: 1103392
    Change-Id: Ic1806bcdf3476bc2c864f91028125d380b0b1be1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300805
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789562}

--- chrome/browser/predictors/loading_predictor_tab_helper.cc
+++ chrome/browser/predictors/loading_predictor_tab_helper.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/predictors/loading_predictor_factory.h"
 #include "chrome/browser/predictors/predictors_enums.h"
 #include "chrome/browser/predictors/predictors_features.h"
+#include "chrome/browser/prerender/prerender_manager.h"
+#include "chrome/browser/prerender/prerender_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/optimization_guide/optimization_guide_decider.h"
 #include "components/optimization_guide/proto/hints.pb.h"
@@ -68,6 +70,16 @@ net::RequestPriority GetRequestPriority(
 }
 
 bool IsHandledNavigation(content::NavigationHandle* navigation_handle) {
+  content::WebContents* web_contents = navigation_handle->GetWebContents();
+
+  prerender::PrerenderManager* prerender_manager =
+      prerender::PrerenderManagerFactory::GetForBrowserContext(
+          web_contents->GetBrowserContext());
+  if (prerender_manager &&
+      prerender_manager->IsWebContentsPrerendering(web_contents, nullptr)) {
+    return false;
+  }
+
   return navigation_handle->IsInMainFrame() &&
          !navigation_handle->IsSameDocument() &&
          navigation_handle->GetURL().SchemeIsHTTPOrHTTPS();
