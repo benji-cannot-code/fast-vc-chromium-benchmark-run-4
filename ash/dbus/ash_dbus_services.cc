5a806643d4cb6a30a9534283577b7c0cb6250c99
Yicheng Li <yichengli@chromium.org>
2020-07-17 02:35:00 +0000

    ash: Add UserAuthenticationServiceProvider
    This adds a DBus service provider to enable ChromeOS daemons to call
    into Ash to start the in-session user authentication dialog. The dialog
    will be implemented in Ash.
    
    Currently the only expected client is u2fd, which needs this UI flow
    in WebAuthn.
    
    TEST=Used u2fd to call UserAuthenticationServiceProvider and verified
         it received the call by logging.
    
    Cq-Depend: chromium:2291256
    Bug: b:144861739
    Change-Id: I47c6b88f2eb4484cef197a009e6ad5bbc4b3e5c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291258
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yicheng Li <yichengli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789336}

--- ash/dbus/ash_dbus_services.cc
+++ ash/dbus/ash_dbus_services.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/dbus/gesture_properties_service_provider.h"
 #include "ash/dbus/liveness_service_provider.h"
 #include "ash/dbus/url_handler_service_provider.h"
+#include "ash/dbus/user_authentication_service_provider.h"
 #include "base/feature_list.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/dbus/services/cros_dbus_service.h"
@@ -41,6 +42,11 @@ AshDBusServices::AshDBusServices(dbus::Bus* system_bus) {
       dbus::ObjectPath(chromeos::kUrlHandlerServicePath),
       chromeos::CrosDBusService::CreateServiceProviderList(
           std::make_unique<UrlHandlerServiceProvider>()));
+  user_authentication_service_ = chromeos::CrosDBusService::Create(
+      system_bus, chromeos::kUserAuthenticationServiceName,
+      dbus::ObjectPath(chromeos::kUserAuthenticationServicePath),
+      chromeos::CrosDBusService::CreateServiceProviderList(
+          std::make_unique<UserAuthenticationServiceProvider>()));
 }
 
 AshDBusServices::~AshDBusServices() {
@@ -48,6 +54,7 @@ AshDBusServices::~AshDBusServices() {
   gesture_properties_service_.reset();
   liveness_service_.reset();
   url_handler_service_.reset();
+  user_authentication_service_.reset();
 }
 
 }  // namespace ash
