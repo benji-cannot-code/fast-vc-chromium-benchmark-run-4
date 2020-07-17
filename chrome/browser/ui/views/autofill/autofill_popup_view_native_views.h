3a964857ddb898c213a06fccb1c208c648a02d14
Aaron Leventhal <aleventhal@chromium.org>
2020-07-17 17:26:26 +0000

    Correct a11y events and focus handling in suggested password popup
    AX-Relnotes: screen reader support for Use Suggested Password popup
    Bug: 786147
    Change-Id: Ic59b7c0d01da7c7258ada0761dfdbb4204bed962
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300184
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789529}

--- chrome/browser/ui/views/autofill/autofill_popup_view_native_views.h
+++ chrome/browser/ui/views/autofill/autofill_popup_view_native_views.h
@@ -84,7 +84,6 @@ class AutofillPopupViewNativeViews : public AutofillPopupBaseView,
   // views::View:
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
   void OnThemeChanged() override;
-  void VisibilityChanged(View* starting_from, bool is_visible) override;
 
   // AutofillPopupView:
   void Show() override;
@@ -120,8 +119,6 @@ class AutofillPopupViewNativeViews : public AutofillPopupBaseView,
   views::ScrollView* scroll_view_ = nullptr;
   views::View* body_container_ = nullptr;
   views::View* footer_container_ = nullptr;
-
-  bool is_ax_menu_start_event_fired_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(AutofillPopupViewNativeViews);
 };
