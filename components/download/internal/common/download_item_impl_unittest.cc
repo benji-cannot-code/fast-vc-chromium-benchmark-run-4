381a57af5e6664b03487d7e3e2167d5aa2853ed1
Xing Liu <xingliu@chromium.org>
2020-07-17 18:16:42 +0000

    Download later: Started download should not be marked as scheduled.
    If the user scheduled a download that starts right away, or the
    network condition met right away. Download schedule struct should be
    cleared, or the download will be shown in the "scheduled for later"
    section, but it's actually completed.
    
    Bug: 1106208,1078454
    Change-Id: I70526221839d16c96a31b1409c28e5227cc9daa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2301379
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789549}

--- components/download/internal/common/download_item_impl_unittest.cc
+++ components/download/internal/common/download_item_impl_unittest.cc
@@ -2700,6 +2700,11 @@ TEST_P(DownloadLaterTest, TestDownloadScheduleAfterTargetDetermined) {
     ASSERT_EQ(DOWNLOAD_INTERRUPT_REASON_CRASH, item->GetLastReason());
   }
 
+  if (param.state == DownloadItem::IN_PROGRESS) {
+    EXPECT_FALSE(item->GetDownloadSchedule().has_value())
+        << "Download schedule should be cleared before completion.";
+  }
+
   CleanupItem(item, download_file, param.state);
 }
 
