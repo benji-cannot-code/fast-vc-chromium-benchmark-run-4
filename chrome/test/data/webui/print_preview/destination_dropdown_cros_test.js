e9983e484269d187044ca5b4e3ec070cbb913c97
Gavin Williams <gavinwill@chromium.org>
2020-07-17 15:14:17 +0000

    Use index to determine highlighted dropdown item
    Instead of stamping 'highlighted_' HTML attribute to determine
    which of one of the dropdown items should be highlighted, use an index
    instead. Allows for simpler tracking of which item to highlight.
    
    Bug: 1059607
    Change-Id: I1c60419c8c38862e33434e0f2ed05523cfb74b44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300668
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789486}

--- chrome/test/data/webui/print_preview/destination_dropdown_cros_test.js
+++ chrome/test/data/webui/print_preview/destination_dropdown_cros_test.js
@@ -20,7 +20,6 @@ destination_dropdown_cros_test.suiteName =
 /** @enum {string} */
 destination_dropdown_cros_test.TestNames = {
   CorrectListItems: 'correct list items',
-  ClickRemovesHighlight: 'click removes highlight',
   ClickCloses: 'click closes dropdown',
   TabCloses: 'tab closes dropdown',
   HighlightedAfterUpDown: 'highlighted after keyboard press up and down',
@@ -87,7 +86,7 @@ suite(destination_dropdown_cros_test.suiteName, function() {
 
   /** @return {?Element} */
   function getHighlightedElement() {
-    return dropdown.$$('[highlighted_]');
+    return dropdown.$$('.highlighted');
   }
 
   /** @return {string} */
@@ -133,20 +132,6 @@ suite(destination_dropdown_cros_test.suiteName, function() {
         assertEquals('One', itemList[0].textContent.trim());
         assertEquals('Two', itemList[1].textContent.trim());
         assertEquals('Three', itemList[2].textContent.trim());
-      });
-
-  test(
-      assert(destination_dropdown_cros_test.TestNames.ClickRemovesHighlight),
-      function() {
-        const destinationOne = createDestination('One', DestinationOrigin.CROS);
-        setItemList([destinationOne]);
-        dropdown.value = destinationOne;
-
-        getList()[0].toggleAttribute('highlighted_', true);
-        assertTrue(getList()[0].hasAttribute('highlighted_'));
-
-        getList()[0].click();
-        assertFalse(getList()[0].hasAttribute('highlighted_'));
       });
 
   test(
