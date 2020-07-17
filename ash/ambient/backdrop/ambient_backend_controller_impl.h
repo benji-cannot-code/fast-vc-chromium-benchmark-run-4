0bcf7832b3f4e3187388bb0f031be58cf92e7748
wutao <wutao@chromium.org>
2020-07-17 00:13:00 +0000

    ambient: Add API to fetch preview images of live album
    Bug: b/160753530
    Test: manual
    Change-Id: I4edae4b329f17919ad25aca3800adf79861e6f1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287273
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789276}

--- ash/ambient/backdrop/ambient_backend_controller_impl.h
+++ ash/ambient/backdrop/ambient_backend_controller_impl.h
@@ -32,6 +32,9 @@ class AmbientBackendControllerImpl : public AmbientBackendController {
   void GetSettings(GetSettingsCallback callback) override;
   void UpdateSettings(const AmbientSettings& settings,
                       UpdateSettingsCallback callback) override;
+  void FetchSettingPreview(int preview_width,
+                           int preview_height,
+                           OnSettingPreviewFetchedCallback callback) override;
   void FetchPersonalAlbums(int banner_width,
                            int banner_height,
                            int num_albums,
@@ -69,6 +72,17 @@ class AmbientBackendControllerImpl : public AmbientBackendController {
   void OnUpdateSettings(UpdateSettingsCallback callback,
                         std::unique_ptr<BackdropURLLoader> backdrop_url_loader,
                         std::unique_ptr<std::string> response);
+
+  void FetchSettingPreviewInternal(int preview_width,
+                                   int preview_height,
+                                   OnSettingPreviewFetchedCallback callback,
+                                   const std::string& gaia_id,
+                                   const std::string& access_token);
+
+  void OnSettingPreviewFetched(
+      OnSettingPreviewFetchedCallback callback,
+      std::unique_ptr<BackdropURLLoader> backdrop_url_loader,
+      std::unique_ptr<std::string> response);
 
   void FetchPersonalAlbumsInternal(int banner_width,
                                    int banner_height,
