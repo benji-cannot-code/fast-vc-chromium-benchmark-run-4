e5ceb2bc9c99e6ee7e705b98cd016aa045ade1ee
Anders Hartvoll Ruud <andruud@chromium.org>
2020-07-17 01:12:24 +0000

    Verify that vars() are resolved in ApplyProperty with CSSCascade enabled
    There is code to resolve variables in this function, but we should
    not reach it with CSSCascade enabled.
    
    Change-Id: I39580db63b1f48114210b2ac59d3d29253a8eb1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2302056
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789309}

--- third_party/blink/renderer/core/css/resolver/style_builder.cc
+++ third_party/blink/renderer/core/css/resolver/style_builder.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/resolver/style_builder.h"
 #include "third_party/blink/renderer/core/css/resolver/style_resolver_state.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -71,19 +72,27 @@ void StyleBuilder::ApplyProperty(const CSSProperty& property,
 
   CSSPropertyID id = property.PropertyID();
   bool is_inherited = property.IsInherited();
-  if (id != CSSPropertyID::kVariable && (value.IsVariableReferenceValue() ||
-                                         value.IsPendingSubstitutionValue())) {
-    bool omit_animation_tainted =
-        CSSAnimations::IsAnimationAffectingProperty(property);
-    const CSSValue* resolved_value =
-        CSSVariableResolver(state).ResolveVariableReferences(
-            id, value, omit_animation_tainted);
-    ApplyProperty(property, state, *resolved_value);
 
-    if (!state.Style()->HasVariableReferenceFromNonInheritedProperty() &&
-        !is_inherited)
-      state.Style()->SetHasVariableReferenceFromNonInheritedProperty();
-    return;
+  if (RuntimeEnabledFeatures::CSSCascadeEnabled()) {
+    // These values must be resolved by StyleCascade before application:
+    DCHECK(!value.IsVariableReferenceValue());
+    DCHECK(!value.IsPendingSubstitutionValue());
+  } else {
+    if (id != CSSPropertyID::kVariable &&
+        (value.IsVariableReferenceValue() ||
+         value.IsPendingSubstitutionValue())) {
+      bool omit_animation_tainted =
+          CSSAnimations::IsAnimationAffectingProperty(property);
+      const CSSValue* resolved_value =
+          CSSVariableResolver(state).ResolveVariableReferences(
+              id, value, omit_animation_tainted);
+      ApplyProperty(property, state, *resolved_value);
+
+      if (!state.Style()->HasVariableReferenceFromNonInheritedProperty() &&
+          !is_inherited)
+        state.Style()->SetHasVariableReferenceFromNonInheritedProperty();
+      return;
+    }
   }
 
   DCHECK(!property.IsShorthand())
