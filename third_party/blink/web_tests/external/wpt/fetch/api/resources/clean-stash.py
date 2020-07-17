4bd08a821750914f8054b1e5ef246bf2c66fa31c
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-07-17 20:28:51 +0000

    Import wpt@78ffebc0791d71df085ccaa72c9a1594037942a1
    Using wpt-import in Chromium 727fdb5bd29e7d43d2721e7144610e28d470e999.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, philipp.hancke@googlemail.com:
      external/wpt/webrtc
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=foolip@google.com
    
    No-Export: true
    Change-Id: Ic965601d615bf3e84fb364a2b496fdbc2aa7c438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2302814
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#789624}

--- third_party/blink/web_tests/external/wpt/fetch/api/resources/clean-stash.py
+++ third_party/blink/web_tests/external/wpt/fetch/api/resources/clean-stash.py
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
-    token = request.GET.first("token")
+    token = request.GET.first(b"token")
     if request.server.stash.take(token) is not None:
-        return "1"
+        return b"1"
     else:
-        return "0"
+        return b"0"
--- third_party/blink/web_tests/external/wpt/fetch/content-encoding/resources/bad-gzip-body.py
+++ third_party/blink/web_tests/external/wpt/fetch/content-encoding/resources/bad-gzip-body.py
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
-    headers = [("Content-Encoding", "gzip")]
-    return headers, "not actually gzip"
+    headers = [(b"Content-Encoding", b"gzip")]
+    return headers, b"not actually gzip"
