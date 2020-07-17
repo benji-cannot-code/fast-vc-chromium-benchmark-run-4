b927aac7f5ad3025ceac0c0adf4ec571ad42883e
Charlie Hu <chenleihu@google.com>
2020-07-17 15:27:24 +0000

    Generate GetDefaultFeatureList from feature_policy_features.json5
    Previously, adding a new feature to feature policy needs to modify
    3 files:
    - feature_policy_feature.mojom
    - feature_policy_features.json5
    - feature_policy.cc
    
    This CL generates |FeaturePolicy::GetDefaultFeatureList| from json5
    config, which eliminates the need to modify feature_policy.cc when
    adding a new feature.
    
    Change-Id: I368215bc66fa8b568e20203f7e9810ddd13f683f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212794
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Cr-Commit-Position: refs/heads/master@{#789494}

---
+++ third_party/blink/renderer/build/scripts/make_feature_policy_features.py
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import json5_generator
+import template_expander
+
+
+class FeaturePolicyFeatureWriter(json5_generator.Writer):
+    file_basename = 'feature_policy_features'
+
+    def __init__(self, json5_file_path, output_dir):
+        super(FeaturePolicyFeatureWriter,
+              self).__init__(json5_file_path, output_dir)
+
+        @template_expander.use_jinja('templates/' + self.file_basename +
+                                     '.cc.tmpl')
+        def generate_implementation():
+            return {
+                'header_guard':
+                self.make_header_guard(self._relative_output_dir +
+                                       self.file_basename + '.h'),
+                'input_files':
+                self._input_files,
+                'features':
+                self.json5_file.name_dictionaries
+            }
+
+        self._outputs = {
+            self.file_basename + '.cc': generate_implementation,
+        }
+
+
+if __name__ == '__main__':
+    json5_generator.Maker(FeaturePolicyFeatureWriter).main()
