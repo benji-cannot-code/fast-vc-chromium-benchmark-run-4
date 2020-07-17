caf273561fa759d748291fdd5c7624fc6f89a8fe
Daniel Rubery <drubery@chromium.org>
2020-07-17 21:29:29 +0000

    Respect DelayDeliveryUntilVerdict policy on chrome://downloads
    This CL makes the presence of the "Open Now" button during deep scanning
    dependent on whether policy has allowed pending scans to be opened.
    
    Fixed: 1068649
    Change-Id: I5d640f45d0c8d5275fd75f135312de4667d61061
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2297979
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789649}

--- chrome/browser/resources/downloads/item.js
+++ chrome/browser/resources/downloads/item.js
@@ -499,7 +499,8 @@ Polymer({
      * @private
      */
     computeShowOpenNow_() {
-      return this.data.state === States.ASYNC_SCANNING;
+      const allowOpenNow = loadTimeData.getBoolean('allowOpenNow');
+      return this.data.state === States.ASYNC_SCANNING && allowOpenNow;
     },
 
     /**
