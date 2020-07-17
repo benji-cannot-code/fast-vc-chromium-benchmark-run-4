19802d86be023ef2784646d0efec930ec9355460
Maksim Ivanov <emaxx@chromium.org>
2020-07-17 21:44:00 +0000

    Error log from PathService::CheckedGet
    Log the error details, including the key, when crashing in
    base::PathService::CheckedGet().
    
    Bug: none
    Change-Id: I69386d92cc335c82012a247c9cef255d8fefea57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291150
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789657}

--- base/path_service.cc
+++ base/path_service.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/logging.h"
 #include "base/synchronization/lock.h"
 #include "build/build_config.h"
 
@@ -232,7 +233,7 @@ bool PathService::Get(int key, FilePath* result) {
 
 FilePath PathService::CheckedGet(int key) {
   FilePath path;
-  CHECK(Get(key, &path));
+  LOG_IF(FATAL, !Get(key, &path)) << "Failed to get the path for " << key;
   return path;
 }
 
