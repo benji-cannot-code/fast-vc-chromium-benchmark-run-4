168ab0095f7d68db2695028cf4d44392632db0c4
Jonathan Mengedoht <mengedoht@google.com>
2020-07-17 11:26:33 +0000

    Add ChangePasswordUrlService
    Bug: 1086141
    Change-Id: I6681eebcc8b14c649dac7622bf9c32c3e7b2eb98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283595
    Commit-Queue: Jonathan Mengedoht <mengedoht@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789432}

---
+++ components/password_manager/core/browser/change_password_url_service.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_CHANGE_PASSWORD_URL_SERVICE_H_
+#define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_CHANGE_PASSWORD_URL_SERVICE_H_
+
+#include "base/callback_forward.h"
+#include "components/keyed_service/core/keyed_service.h"
+
+class GURL;
+
+namespace url {
+class Origin;
+}
+
+namespace password_manager {
+
+class ChangePasswordUrlService : public KeyedService {
+ public:
+  using UrlCallback = base::OnceCallback<void(GURL)>;
+  // Initializes the service.
+  virtual void Initialize() = 0;
+  // Returns the change password URL for `origin` via `callback`.
+  virtual void GetChangePasswordUrl(const url::Origin& origin,
+                                    UrlCallback callback) = 0;
+};
+
+}  // namespace password_manager
+
+#endif  // COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_CHANGE_PASSWORD_URL_SERVICE_H_
