7b25c4d9bfc75e79d7738628166931f0cb274bc6
Wez <wez@chromium.org>
2020-07-17 17:19:56 +0000

    [blink] Fix MessageReceiver::OnMessage() to default to failing.
    WebMessagePort::MessageReceiver::OnMessage() is documented as returning
    false if passed a message that it cannot handle (e.g. because it is
    malformed or unrecognised).  The default OnMessage() implementation was
    instead returning true.
    
    Also tidy up the comment for OnMessage() to document only the return-
    false-on-failure path, since the converse (true-on-success) is implicit.
    
    Change-Id: If5f72913465aff40360d3dcc9a88fb8c10fbe237
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300386
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789527}

--- third_party/blink/common/messaging/web_message_port.cc
+++ third_party/blink/common/messaging/web_message_port.cc
@@ -41,8 +41,9 @@ WebMessagePort::Message::Message(const base::string16& data,
 
 WebMessagePort::MessageReceiver::MessageReceiver() = default;
 WebMessagePort::MessageReceiver::~MessageReceiver() = default;
+
 bool WebMessagePort::MessageReceiver::OnMessage(Message) {
-  return true;
+  return false;
 }
 
 WebMessagePort::WebMessagePort() = default;
