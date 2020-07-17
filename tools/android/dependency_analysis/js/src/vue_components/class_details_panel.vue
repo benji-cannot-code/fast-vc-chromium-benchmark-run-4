3abd20ae9d98b009168da9a389645dc39cbe8ebf
James Long <yjlong@google.com>
2020-07-17 19:36:58 +0000

    Display buildtargets in dependency visualization class details panel
    A new component has been made for class details, similar to how the
    package graph has a panel for package details. This component contains
    the link to the class' package and a list of the class' build targets.
    
    Bug: 1106496
    Change-Id: I71b34fa973256cf43984b351769ca95b2fb6bb93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303985
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: James Long <yjlong@google.com>
    Cr-Commit-Position: refs/heads/master@{#789597}

---
+++ tools/android/dependency_analysis/js/src/vue_components/class_details_panel.vue
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<!-- Copyright 2020 The Chromium Authors. All rights reserved.
+     Use of this source code is governed by a BSD-style license that can be
+     found in the LICENSE file. -->
+
+<template>
+  <div
+      v-if="selectedClass !== null"
+      class="class-details-panel">
+    <LinkToGraph
+        :filter="[selectedClass.packageName]"
+        :graph-type="PagePathName.PACKAGE"
+        :text="'View ' + selectedClass.packageName"/>
+    <div v-if="selectedClass.buildTargets.length > 0">
+      <div>Class buildtarget(s):</div>
+      <ul>
+        <li
+            v-for="buildTarget in selectedClass.buildTargets"
+            :key="buildTarget">
+          {{ buildTarget }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import {PagePathName} from '../url_processor.js';
+
+import LinkToGraph from './link_to_graph.vue';
+
+const ClassDetailsPanel = {
+  components: {
+    LinkToGraph,
+  },
+  props: {
+    selectedClass: Object,
+  },
+  computed: {
+    PagePathName: () => PagePathName,
+  },
+};
+
+export default ClassDetailsPanel;
+</script>
