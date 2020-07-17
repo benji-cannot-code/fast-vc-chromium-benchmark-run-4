201f507e39af8d3ce0ec391b3a228bef5ce2421a
ckitagawa <ckitagawa@chromium.org>
2020-07-17 16:11:57 +0000

    [Paint Preview] Cleanup capture experiment
    This CL deprecates the capture experiment for paint previews.
    
    Bug: 1105936
    Change-Id: I1bb63bc213e1c16804cf187c1a754f8a5f5c608a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2299184
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789504}

--- components/paint_preview/features/features.cc
+++ components/paint_preview/features/features.cc
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace paint_preview {
 
-const base::Feature kPaintPreviewCaptureExperiment{
-    "PaintPreviewCaptureExperiment", base::FEATURE_DISABLED_BY_DEFAULT};
-
 const base::Feature kPaintPreviewDemo{"PaintPreviewDemo",
                                       base::FEATURE_DISABLED_BY_DEFAULT};
 
