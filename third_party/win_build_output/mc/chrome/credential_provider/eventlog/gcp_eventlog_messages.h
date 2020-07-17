1847974623bb130acc8572f9fd55ffac1313aefc
Yusuf Sengul <yusufsn@google.com>
2020-07-17 17:43:09 +0000

    Add packaging and installation of gcpw extension
    Adding an empty process that is built and installed along with GCPW.
    gcpw_extension.exe process is meant to be a service process that is
    installed by gcp_setup.exe.
    
    In this change;
    
    An empty process with basic initialization is created
    Self extracting module script is modified to include gcpw_extension.exe
    Adds another event category called "GCPW Extension Events"
    
    Bug: 1101590
    Change-Id: I39f0e73d266c5328b4c18d348003a9109a55fe78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278588
    Reviewed-by: Rakesh Soma <rakeshsoma@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Yusuf Sengul <yusufsn@google.com>
    Cr-Commit-Position: refs/heads/master@{#789533}

--- third_party/win_build_output/mc/chrome/credential_provider/eventlog/gcp_eventlog_messages.h
+++ third_party/win_build_output/mc/chrome/credential_provider/eventlog/gcp_eventlog_messages.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -55,6 +55,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // GCPW Events
 //
 #define GCPW_CATEGORY                    ((WORD)0x00000001L)
+
+//
+// MessageId: GCPW_EXTENSION_CATEGORY
+//
+// MessageText:
+//
+// GCPW Extension Events
+//
+#define GCPW_EXTENSION_CATEGORY          ((WORD)0x00000002L)
 
 //
 // MessageId: MSG_LOG_MESSAGE
