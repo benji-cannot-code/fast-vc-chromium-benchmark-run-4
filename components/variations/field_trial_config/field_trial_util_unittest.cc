0859c2b8aa6b36b1baa82e8dee96e90ea3cf0e1a
Weilun Shi <sweilun@chromium.org>
2020-07-17 23:31:00 +0000

    Move UnescapeValue to base/metrics/field_trial_params
    After moving some functions from net// to base//, the UnescapeValue no
    longer needs to depend on net// so it can be moved to
    base/metrics/field_trial_params where it gets used. Also replace the
    temporary NoOpDecodeFunc with the UnescapeValue as it's available now.
    
    Bug: 1068052
    Change-Id: I1b1790a3e1491a834822d017a5f2f4bfa34ffbec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2298942
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789696}

--- components/variations/field_trial_config/field_trial_util_unittest.cc
+++ components/variations/field_trial_config/field_trial_util_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/macros.h"
 #include "base/metrics/field_trial.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/optional.h"
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -904,6 +905,7 @@ TEST_F(FieldTrialUtilTest, TestEscapeValue) {
   EXPECT_EQ(escaped_str.find(','), std::string::npos);
   EXPECT_EQ(escaped_str.find('*'), std::string::npos);
 
-  EXPECT_EQ(str, UnescapeValue(escaped_str));
+  // Make sure the EscapeValue function is the inverse of base::UnescapeValue.
+  EXPECT_EQ(str, base::UnescapeValue(escaped_str));
 }
 }  // namespace variations
