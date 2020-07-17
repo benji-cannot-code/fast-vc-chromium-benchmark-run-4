4a18ae4740b094ea4dae02d0cd77184032885379
Viktor Semeniuk <vsemeniuk@google.com>
2020-07-17 08:14:32 +0000

    [iOS][Password Check] Number of compromised credentials
    This change displays number of compromised credentials in the Passwords
    screen using plural string.
    
    Bug: 1075494
    Change-Id: Ic155353280f2f40834fef78e26a1d4b12a6477a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2301142
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#789397}

--- ios/chrome/browser/ui/settings/password/passwords_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/passwords_table_view_controller.mm
@@ -76,6 +76,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/grit/ios_chromium_strings.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "net/base/mac/url_conversions.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 #include "url/gurl.h"
 
@@ -973,7 +974,9 @@ std::vector<std::unique_ptr<autofill::PasswordForm>> CopyOf(
     }
     case PasswordCheckStateUnSafe: {
       _passwordProblemsItem.detailText =
-          l10n_util::GetNSString(IDS_IOS_CHECK_PASSWORDS_UNSAFE_STATE);
+          base::SysUTF16ToNSString(l10n_util::GetPluralStringFUTF16(
+              IDS_IOS_CHECK_PASSWORDS_COMPROMISED_COUNT,
+              _passwordCheck->GetCompromisedCredentials().size()));
       UIImage* unSafeIconImage = [[UIImage imageNamed:@"settings_unsafe_state"]
           imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
       _passwordProblemsItem.trailingImage = unSafeIconImage;
@@ -984,10 +987,12 @@ std::vector<std::unique_ptr<autofill::PasswordForm>> CopyOf(
       break;
     }
     case PasswordCheckStateSafe: {
+      DCHECK(_passwordCheck->GetCompromisedCredentials().empty());
       UIImage* safeIconImage = [[UIImage imageNamed:@"settings_safe_state"]
           imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
       _passwordProblemsItem.detailText =
-          l10n_util::GetNSString(IDS_IOS_CHECK_PASSWORDS_SAFE_STATE);
+          base::SysUTF16ToNSString(l10n_util::GetPluralStringFUTF16(
+              IDS_IOS_CHECK_PASSWORDS_COMPROMISED_COUNT, 0));
       _passwordProblemsItem.trailingImage = safeIconImage;
       _passwordProblemsItem.trailingImageTintColor =
           [UIColor colorNamed:kGreenColor];
