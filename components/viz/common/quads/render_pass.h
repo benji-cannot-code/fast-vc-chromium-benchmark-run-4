93200622b2d34c217f9ddb5729af5bb9728f3826
Vladimir Levin <vmpstr@chromium.org>
2020-07-17 20:23:04 +0000

    viz: Separate QuadList into a separate file.
    This is just a mechanical patch to split a fairly non-trivial
    class (QuadList) into its own separate file. There should be no
    behavior changes.
    
    R=weiliangc@chromium.org
    
    Bug: 1106418
    Change-Id: I485c314060b93c572a2d55b3d5220726b2d5f150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2304152
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789622}

--- components/viz/common/quads/render_pass.h
+++ components/viz/common/quads/render_pass.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <memory>
 #include <utility>
 #include <vector>
 
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/filter_operations.h"
 #include "components/viz/common/quads/draw_quad.h"
 #include "components/viz/common/quads/largest_draw_quad.h"
+#include "components/viz/common/quads/quad_list.h"
 #include "components/viz/common/viz_common_export.h"
 #include "ui/gfx/display_color_spaces.h"
 #include "ui/gfx/geometry/rect.h"
@@ -36,30 +38,7 @@ class DrawQuad;
 class RenderPassDrawQuad;
 class SharedQuadState;
 
-// A list of DrawQuad objects, sorted internally in front-to-back order. To
-// add a new quad drawn behind another quad, it must be placed after the other
-// quad.
-class VIZ_COMMON_EXPORT QuadList : public cc::ListContainer<DrawQuad> {
- public:
-  QuadList();
-  explicit QuadList(size_t default_size_to_reserve);
-
-  typedef QuadList::ReverseIterator BackToFrontIterator;
-  typedef QuadList::ConstReverseIterator ConstBackToFrontIterator;
-
-  inline BackToFrontIterator BackToFrontBegin() { return rbegin(); }
-  inline BackToFrontIterator BackToFrontEnd() { return rend(); }
-  inline ConstBackToFrontIterator BackToFrontBegin() const { return rbegin(); }
-  inline ConstBackToFrontIterator BackToFrontEnd() const { return rend(); }
-
-  // This function is used by overlay algorithm to fill the backbuffer with
-  // transparent black.
-  void ReplaceExistingQuadWithOpaqueTransparentSolidColor(Iterator at);
-  Iterator InsertCopyBeforeDrawQuad(Iterator at, size_t count);
-};
-
 using SharedQuadStateList = cc::ListContainer<SharedQuadState>;
-
 using RenderPassId = uint64_t;
 
 class VIZ_COMMON_EXPORT RenderPass {
