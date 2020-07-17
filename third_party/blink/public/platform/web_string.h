9be74ed320cfb2043edb2783db786ed320e0f950
Jose Dapena Paz <jdapena@igalia.com>
2020-07-17 18:05:27 +0000

    IWYU: numeric_limits usage in web_string.h requires <limits>
    Bug: 819294
    Change-Id: I79cb83515d197106c70345526a2a17177e8ca9e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303437
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#789543}

--- third_party/blink/public/platform/web_string.h
+++ third_party/blink/public/platform/web_string.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_WEB_STRING_H_
 
 #include <cstring>
+#include <limits>
 #include <string>
 
 #include "base/memory/scoped_refptr.h"
