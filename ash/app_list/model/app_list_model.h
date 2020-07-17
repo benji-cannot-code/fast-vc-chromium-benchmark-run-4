31be4fe7114764d058591f2e4d2cca836b402b5b
Wen-Chien Wang <wcwang@google.com>
2020-07-17 18:18:40 +0000

    Fixed the app position bug in a folder
    Fixed the bug that when user changes the position of an app in folder,
    the change won't update the syncable service so that the user will lose
    their changes if they logout.
    
    R=tbarzic@chromium.org
    
    Bug: 1041633
    Change-Id: I14be6e26f3fc7b4532aad2f4b17199d7bbaabce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2293470
    Commit-Queue: Wen-Chien Wang <wcwang@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789551}

--- ash/app_list/model/app_list_model.h
+++ ash/app_list/model/app_list_model.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/app_list_types.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
+#include "base/scoped_observer.h"
 
 namespace ash {
 
@@ -165,7 +166,8 @@ class APP_LIST_MODEL_EXPORT AppListModel : public AppListItemListObserver {
   // The AppListView state. Controlled by the AppListView.
   AppListViewState state_fullscreen_ = AppListViewState::kClosed;
   base::ObserverList<AppListModelObserver, true>::Unchecked observers_;
-
+  ScopedObserver<AppListItemList, AppListItemListObserver>
+      item_list_scoped_observer_{this};
   DISALLOW_COPY_AND_ASSIGN(AppListModel);
 };
 
