07595b70db814d74df71ed3251e42be6478903d7
Reilly Grant <reillyg@chromium.org>
2020-07-18 02:34:08 +0000

    serial: Add manual WPT test using a loopback device
    This manual test architecture allows us to build integration tests. A
    connected serial device with its transmit and receive pins connected
    together is assumed.
    
    Bug: 884928
    Change-Id: I960faa894776723fd5a858ff7f35a1999afa2955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2277582
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789737}

--- third_party/blink/web_tests/wpt_internal/serial/serial_getPorts.https.window.js
+++ third_party/blink/web_tests/wpt_internal/serial/serial_getPorts.https.window.js
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/gen/layout_test_data/mojo/public/js/mojo_bindings.js
 // META: script=/gen/mojo/public/mojom/base/unguessable_token.mojom.js
 // META: script=/gen/third_party/blink/public/mojom/serial/serial.mojom.js
-// META: script=resources/serial-test-utils.js
+// META: script=resources/common.js
+// META: script=resources/automation.js
 
 promise_test(async () => {
   let interceptor =
