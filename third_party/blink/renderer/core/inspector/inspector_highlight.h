5677fb035317da7ab67ce3b4e0ee089dc1871d01
Patrick Brosset <patrick.brosset@microsoft.com>
2020-07-18 10:01:15 +0000

    DevTools: protocol and backend support for line names in overlay
    This introduces a new option (showLineNames) to the
    gridHighlightConfig option, passed to overlays.
    The option defaults to false. When set to true, it tells the
    backend implementation of the overlay domain to
    also generate offset information for all of the named lines.
    
    The corresponding frontend CL is
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2292205
    
    Because the frontend CL also adds a new setting that needs
    telemetry support, the corresponding enum value is added here.
    
    Bug: 1100385
    Change-Id: I0487ae7c5d6aad21c8d6b16bcb3be46a08e0ee07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291435
    Commit-Queue: Patrick Brosset <patrick.brosset@microsoft.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Brandon Goddard <brgoddar@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#789773}

--- third_party/blink/renderer/core/inspector/inspector_highlight.h
+++ third_party/blink/renderer/core/inspector/inspector_highlight.h
@@ -39,6 +39,7 @@ struct CORE_EXPORT InspectorGridHighlightConfig {
   bool show_positive_line_numbers;
   bool show_negative_line_numbers;
   bool show_area_names;
+  bool show_line_names;
 };
 
 struct CORE_EXPORT InspectorHighlightConfig {
