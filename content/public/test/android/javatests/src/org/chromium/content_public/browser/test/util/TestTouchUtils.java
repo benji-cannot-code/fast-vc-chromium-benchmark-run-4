b5bf0fda01f23d1117b7c2718fb40633eb156e92
Haiyang Pan <hypan@google.com>
2020-07-19 20:31:24 +0000

    Revert "Reland "Reland "[Omnibox] Use ActivityState to check if we are in browser mode"""
    This reverts commit 5d52e576217af20c3b3641b42f38f99054998369.
    
    Reason for revert: org.chromium.chrome.browser.omnibox.suggestions.SwitchToTabTest#testSwitchToTabSuggestion flakily fails with java.lang.NullPointerException
    For example:
    https://ci.chromium.org/p/chromium/builders/ci/android-pie-x86-rel/1524
    https://ci.chromium.org/p/chromium/builders/ci/android-pie-x86-rel/1525
    https://ci.chromium.org/p/chromium/builders/ci/android-marshmallow-x86-fyi-rel/1272
    
    Also, it is flaky in android-pie-arm64-rel. See https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyhAELEgVGbGFrZSJ5Y2hyb21pdW1AY2hyb21lX3B1YmxpY190ZXN0X2Fwa0BvcmcuY2hyb21pdW0uY2hyb21lLmJyb3dzZXIub21uaWJveC5zdWdnZXN0aW9ucy5Td2l0Y2hUb1RhYlRlc3QjdGVzdFN3aXRjaFRvVGFiU3VnZ2VzdGlvbgw
    
    Original change's description:
    > Reland "Reland "[Omnibox] Use ActivityState to check if we are in browser mode""
    > 
    > This is a reland of 5ae112d147943083696c294b1909ab30f10d9a10
    > 
    > Original change's description:
    > > Reland "[Omnibox] Use ActivityState to check if we are in browser mode"
    > > 
    > > This is a reland of be1e96360a10671da872dabe9c717608cb561ba3
    > > 
    > > Update the test to run on UI thread.
    > > 
    > > Original change's description:
    > > > [Omnibox] Use ActivityState to check if we are in browser mode
    > > >
    > > >
    > > > In multi windows mode, check if we are in browser mode by WindowAndroid
    > > > is not working, should check ActivityState instead.
    > > >
    > > > Bug: 1102640
    > > > Change-Id: I35a720a2aa6f337cc9f9ae43e39d83c9368fcb5a
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2286478
    > > > Commit-Queue: Gang Wu <gangwu@chromium.org>
    > > > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#786636}
    > > 
    > > Bug: 1102640
    > > Change-Id: Iefba5a5d0c181535bd7fb86cf0e073be2ebdbcdb
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2293074
    > > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > > Commit-Queue: Gang Wu <gangwu@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#788816}
    > 
    > Bug: 1102640
    > Change-Id: I6da3c7ad8402cef83083a53f7ffc517f975720f8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2301285
    > Commit-Queue: Gang Wu <gangwu@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#789727}
    
    TBR=tedchoc@chromium.org,gangwu@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1102640
    Change-Id: I2227da6e687932a6a90c3eb73f2a5386ba2c6d9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2306749
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/master@{#789802}

--- content/public/test/android/javatests/src/org/chromium/content_public/browser/test/util/TestTouchUtils.java
+++ content/public/test/android/javatests/src/org/chromium/content_public/browser/test/util/TestTouchUtils.java
@@ -204,7 +204,7 @@ public class TestTouchUtils {
      * @param v The view to call performClick on.
      */
     public static void performClickOnMainSync(Instrumentation instrumentation, final View v) {
-        TestThreadUtils.runOnUiThreadBlocking(() -> { v.performClick(); });
+        instrumentation.runOnMainSync(() -> v.performClick());
     }
 
     /**
