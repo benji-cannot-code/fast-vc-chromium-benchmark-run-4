55962ab237c8d2221774e96a248699eadd1d0667
Yuwei Huang <yuweih@chromium.org>
2020-07-20 21:12:41 +0000

    [remoting] Add stream request support to ProtobufHttpStatus
    * Add stream request support to ProtobufHttpStatus, which includes some
      refactoring in the code.
    * Make ProtobufHttpClient directly own the requests, to make lifetime
      managing less painful.
    
    Bug: 1103416
    Change-Id: I9c1f62acb4e02cd968387bc4cc34db8dd6268c25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2299536
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790112}

---
+++ remoting/base/protobuf_http_request_config.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/base/protobuf_http_request_config.h"
+
+#include "third_party/protobuf/src/google/protobuf/message_lite.h"
+
+namespace remoting {
+
+ProtobufHttpRequestConfig::ProtobufHttpRequestConfig(
+    const net::NetworkTrafficAnnotationTag& traffic_annotation)
+    : traffic_annotation(traffic_annotation) {}
+
+ProtobufHttpRequestConfig::~ProtobufHttpRequestConfig() = default;
+
+void ProtobufHttpRequestConfig::Validate() const {
+  DCHECK(request_message);
+  DCHECK(!path.empty());
+}
+
+}  // namespace remoting
