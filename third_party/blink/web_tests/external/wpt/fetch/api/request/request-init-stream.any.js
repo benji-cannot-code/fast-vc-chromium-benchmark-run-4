635edf178b5586c540995771ef94319e06775c6c
Yoichi Osato <yoichio@chromium.org>
2020-07-20 07:36:33 +0000

    [fetch] Expose Request.body property.
    This CL implements https://fetch.spec.whatwg.org/#body-mixin:
    """
    Request includes Body;
    
    The body attribute’s getter must return null if body is null and body’s stream otherwise.
    """
    Since this feature is required for fetch upload streaming, this CL makes
    it under the flag FetchUploadStreaming.
    
    Fixed: 1105704
    Change-Id: I334d9e1a3494ff4ba31efc9dc8b319294d2e19d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2297035
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789852}

--- third_party/blink/web_tests/external/wpt/fetch/api/request/request-init-stream.any.js
+++ third_party/blink/web_tests/external/wpt/fetch/api/request/request-init-stream.any.js
@@ -3,6 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 "use strict";
 
+test(() => {
+  const stream = new ReadableStream();
+  const request = new Request("...", { method:"POST", body: stream });
+  assert_equals(request.body, stream);
+}, "Constructing a Request with a stream holds the original object.");
+
 async function assert_request(test, input, init) {
   assert_throws_js(TypeError, () => new Request(input, init), "new Request()");
   await promise_rejects_js(test, TypeError, fetch(input, init), "fetch()");
