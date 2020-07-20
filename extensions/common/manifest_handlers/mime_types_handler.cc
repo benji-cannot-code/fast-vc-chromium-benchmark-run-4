9e2c84c7f6ccf3fbfc474647ff6eff6dbdfd9559
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-07-20 20:04:04 +0000

    [Extensions] Make MimeTypesHandler::GetMIMETypeAllowlist() faster
    Rather than constructing a new vector of strings on each invocation of
    MimeTypesHandler::GetMIMETypeAllowlist(), use base::NoDestructor<> to
    cache the result and return a reference. Since this method is called
    multiple times during hot paths, this helps (a bit).
    
    Bug: None
    Change-Id: Ib3598ee7b0580a266d497ef57177cf0964101f76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2305005
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790068}

--- extensions/common/manifest_handlers/mime_types_handler.cc
+++ extensions/common/manifest_handlers/mime_types_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/metrics/histogram_functions.h"
+#include "base/no_destructor.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -26,7 +27,7 @@ namespace {
 // This has to by in sync with MimeHandlerType enum.
 // Note that if multiple versions of quickoffice are installed, the
 // higher-indexed entry will clobber earlier entries.
-const char* const kMIMETypeHandlersAllowlist[] = {
+constexpr const char* const kMIMETypeHandlersAllowlist[] = {
     extension_misc::kPdfExtensionId,
     extension_misc::kQuickOfficeComponentExtensionId,
     extension_misc::kQuickOfficeInternalExtensionId,
@@ -69,9 +70,11 @@ MimeTypesHandlerInfo::~MimeTypesHandlerInfo() = default;
 }  // namespace
 
 // static
-std::vector<std::string> MimeTypesHandler::GetMIMETypeAllowlist() {
-  return {std::begin(kMIMETypeHandlersAllowlist),
-          std::end(kMIMETypeHandlersAllowlist)};
+const std::vector<std::string>& MimeTypesHandler::GetMIMETypeAllowlist() {
+  static base::NoDestructor<std::vector<std::string>> allowlist_vector{
+      std::begin(kMIMETypeHandlersAllowlist),
+      std::end(kMIMETypeHandlersAllowlist)};
+  return *allowlist_vector;
 }
 
 // static
--- extensions/common/manifest_handlers/mime_types_handler.h
+++ extensions/common/manifest_handlers/mime_types_handler.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class MimeTypesHandler {
  public:
   // Returns list of extensions' ids that are allowed to use MIME type filters.
-  static std::vector<std::string> GetMIMETypeAllowlist();
+  static const std::vector<std::string>& GetMIMETypeAllowlist();
 
   static MimeTypesHandler* GetHandler(const extensions::Extension* extension);
 
