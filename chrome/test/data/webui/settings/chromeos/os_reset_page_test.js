e950635807c7db8fdada144d87049d95d0abaef9
Daniel Classon <dclasson@google.com>
2020-07-20 20:39:25 +0000

    Reland "[OsSettingsDeepLinking] Add DeepLinkingBehavior & Deep link ..."
    Fix: Add missing import to OsResetPageTest that caused a failure in
    Polymer3. Polymer3 tests failed in post-submit but weren't run in pre
    submit, leading to the error going unnoticed and forcing revert.
    
    Reverted CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/2306122
    Reason for revert: "Newly added test is failing:
    https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-dbg/19641
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8874475110456145024/+/steps/browser_tests/0/logs/Deterministic_failure:_OSSettingsResetPageV3Test.All__status_FAILURE_/0
    
    Original CL:
    parameter from the URL and then focus on elements containing that
    settingId. Imported in the OS Reset Page."
    
    https: //chromium-review.googlesource.com/c/chromium/src/+/2296008/21
    Description: "Added the Polymer Behavior to fetch the settingId
    Bug: 1084154
    Change-Id: Ida343c79467d22571fe2a218c39c7e405f399800
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2306556
    Commit-Queue: Daniel Classon <dclasson@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790087}

--- chrome/test/data/webui/settings/chromeos/os_reset_page_test.js
+++ chrome/test/data/webui/settings/chromeos/os_reset_page_test.js
@@ -3,17 +3,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
 // #import {TestLifetimeBrowserProxy} from './test_os_lifetime_browser_proxy.m.js';
-// #import {LifetimeBrowserProxy, LifetimeBrowserProxyImpl, OsResetBrowserProxyImpl} from 'chrome://os-settings/chromeos/os_settings.js';
+// #import {LifetimeBrowserProxy, LifetimeBrowserProxyImpl, OsResetBrowserProxyImpl, Router, routes} from 'chrome://os-settings/chromeos/os_settings.js';
 // #import {TestOsResetBrowserProxy} from './test_os_reset_browser_proxy.m.js';
 // #import {assertEquals, assertFalse, assertNotEquals, assertTrue} from '../../chai_assert.js';
 // #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// #import {getDeepActiveElement} from 'chrome://resources/js/util.m.js';
+// #import {waitAfterNextRender} from 'chrome://test/test_util.m.js';
+// clang-format on
 
 cr.define('settings_reset_page', function() {
   /** @enum {string} */
   const TestNames = {
     PowerwashDialogAction: 'PowerwashDialogAction',
     PowerwashDialogOpenClose: 'PowerwashDialogOpenClose',
+    PowerwashFocusDeepLink: 'PowerwashFocusDeepLink',
+    PowerwashFocusDeepLinkNoFlag: 'PowerwashFocusDeepLinkNoFlag',
+    PowerwashFocusDeepLinkWrongId: 'PowerwashFocusDeepLinkWrongId',
   };
 
   suite('DialogTests', function() {
@@ -39,6 +46,7 @@ cr.define('settings_reset_page', function() {
     });
 
     teardown(function() {
+      settings.Router.getInstance().resetRouteForTesting();
       resetPage.remove();
     });
 
@@ -70,6 +78,23 @@ cr.define('settings_reset_page', function() {
       ]);
     }
 
+    /**
+     * Navigates to the deep link provided by |settingId| and returns true if
+     * the focused element is |deepLinkElement|.
+     * @param {!Element} deepLinkElement
+     * @param {!string} settingId
+     * @returns {!boolean}
+     */
+    async function isDeepLinkFocusedForSettingId(deepLinkElement, settingId) {
+      const params = new URLSearchParams;
+      params.append('settingId', settingId);
+      settings.Router.getInstance().navigateTo(
+          settings.routes.OS_RESET, params);
+
+      await test_util.waitAfterNextRender(deepLinkElement);
+      return deepLinkElement === getDeepActiveElement();
+    }
+
     // Tests that the powerwash dialog opens and closes correctly, and
     // that chrome.send calls are propagated as expected.
     test(TestNames.PowerwashDialogOpenClose, function() {
@@ -91,6 +116,36 @@ cr.define('settings_reset_page', function() {
       const requestTpmFirmwareUpdate =
           await lifetimeBrowserProxy.whenCalled('factoryReset');
       assertFalse(requestTpmFirmwareUpdate);
+    });
+
+    // Tests that when the route changes to one containing a deep link to
+    // powerwash, powerwash is focused.
+    test(TestNames.PowerwashFocusDeepLink, async () => {
+      loadTimeData.overrideValues({isDeepLinkingEnabled: true});
+      assertTrue(loadTimeData.getBoolean('isDeepLinkingEnabled'));
+      assertTrue(
+          await isDeepLinkFocusedForSettingId(resetPage.$.powerwash, '1600'),
+          'Powerwash should be focused for settingId=1600.');
+    });
+
+    // Tests that when the deep linking flag is disabled, no focusing of deep
+    // links occurs.
+    test(TestNames.PowerwashFocusDeepLinkNoFlag, async () => {
+      loadTimeData.overrideValues({isDeepLinkingEnabled: false});
+      assertFalse(loadTimeData.getBoolean('isDeepLinkingEnabled'));
+      assertFalse(
+          await isDeepLinkFocusedForSettingId(resetPage.$.powerwash, '1600'),
+          'Powerwash should not be focused with flag disabled.');
+    });
+
+    // Tests that when the route changes to one containing a deep link not equal
+    // to powerwash, no focusing of powerwash occurs.
+    test(TestNames.PowerwashFocusDeepLinkWrongId, async () => {
+      loadTimeData.overrideValues({isDeepLinkingEnabled: true});
+      assertTrue(loadTimeData.getBoolean('isDeepLinkingEnabled'));
+      assertFalse(
+          await isDeepLinkFocusedForSettingId(resetPage.$.powerwash, '1234'),
+          'Powerwash should not be focused for settingId=1234.');
     });
   });
 
