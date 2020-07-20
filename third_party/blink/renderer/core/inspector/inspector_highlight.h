40b2c70a736df8f1f23f4ffb07b694bee41358a7
Alex Rudenko <alexrudenko@chromium.org>
2020-07-20 09:55:20 +0000

    DevTools: add showTrackSizes parameter to grid overlay
    Adds a new option to GridHighlightConfig to display grid track sizes.
    
    Screenshot: https://i.imgur.com/JjSr5v2.png
    Frontend CL: https://crrev.com/c/2300099
    
    Change-Id: Ic2a105128bce8c67ee41c3a153808ac171e5ea98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2299343
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Brandon Goddard <brgoddar@microsoft.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789873}

--- third_party/blink/renderer/core/inspector/inspector_highlight.h
+++ third_party/blink/renderer/core/inspector/inspector_highlight.h
@@ -40,6 +40,7 @@ struct CORE_EXPORT InspectorGridHighlightConfig {
   bool show_negative_line_numbers;
   bool show_area_names;
   bool show_line_names;
+  bool show_track_sizes;
 };
 
 struct CORE_EXPORT InspectorHighlightConfig {
