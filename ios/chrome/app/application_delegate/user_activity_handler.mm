d0c530a42d2c054d38a5f6f1c516d2e55c222eec
Andy Lu <andyhylu@google.com>
2020-07-20 18:50:33 +0000

    Search in Chrome Shortcut now accepts input
    Make search in chrome accept a string input that will be searched in any search engine of user's choice
    
    Bug: 1106922
    
    Change-Id: I4bb79e8f0d77b04d1cf79cda83cbe87fe00eabb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2297731
    Commit-Queue: Andy Lu <andyhylu@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#790037}

--- ios/chrome/app/application_delegate/user_activity_handler.mm
+++ ios/chrome/app/application_delegate/user_activity_handler.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/application_delegate/tab_opening.h"
 #include "ios/chrome/app/application_mode.h"
 #import "ios/chrome/app/intents/OpenInChromeIntent.h"
+#import "ios/chrome/app/intents/SearchInChromeIntent.h"
 #import "ios/chrome/app/spotlight/actions_spotlight_manager.h"
 #import "ios/chrome/app/spotlight/spotlight_util.h"
 #include "ios/chrome/app/startup/chrome_app_startup_parameters.h"
@@ -145,10 +146,23 @@ NSString* const kShortcutQRScanner = @"OpenQRScanner";
   } else if ([userActivity.activityType
                  isEqualToString:@"SearchInChromeIntent"]) {
     base::RecordAction(UserMetricsAction("IOSLaunchedBySearchInChromeIntent"));
+
     AppStartupParameters* startupParams = [[AppStartupParameters alloc]
         initWithExternalURL:GURL(kChromeUINewTabURL)
                 completeURL:GURL(kChromeUINewTabURL)];
-    [startupParams setPostOpeningAction:FOCUS_OMNIBOX];
+
+    SearchInChromeIntent* intent =
+        base::mac::ObjCCastStrict<SearchInChromeIntent>(
+            userActivity.interaction.intent);
+
+    NSString* searchPhrase = intent.searchPhrase;
+
+    if ([searchPhrase length]) {
+      startupParams.textQuery = searchPhrase;
+    } else {
+      startupParams.postOpeningAction = FOCUS_OMNIBOX;
+    }
+
     [connectionInformation setStartupParameters:startupParams];
     webpageURL =
         [NSURL URLWithString:base::SysUTF8ToNSString(kChromeUINewTabURL)];
