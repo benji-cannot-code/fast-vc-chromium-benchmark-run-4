41cb9a34da919cb210b5458fb6437c4129e8ea39
Kunihiko Sakamoto <ksakamoto@chromium.org>
2020-07-20 06:00:19 +0000

    Use CrossVariantMojoReceiver in CreateWebBundleSubresourceLoaderFactory
    It's safer, and easier to understand.
    
    Bug: 1082020
    Change-Id: I8bd3de0692f66ce53897bd86587f86800394650f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2305819
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789809}

--- third_party/blink/renderer/platform/loader/fetch/url_loader/web_bundle_subresource_loader.h
+++ third_party/blink/renderer/platform/loader/fetch/url_loader/web_bundle_subresource_loader.h
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_LOADER_FETCH_URL_LOADER_WEB_BUNDLE_SUBRESOURCE_LOADER_H_
 
 #include "mojo/public/cpp/bindings/pending_receiver.h"
-#include "services/network/public/mojom/url_loader_factory.mojom-forward.h"
+#include "services/network/public/mojom/url_loader_factory.mojom-shared.h"
+#include "third_party/blink/public/platform/cross_variant_mojo_util.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 
 namespace blink {
@@ -15,7 +16,8 @@ namespace blink {
 // Creates a network::mojom::URLLoaderFactory that can load resources from a
 // WebBundle, and binds it to |factory_receiver|.
 PLATFORM_EXPORT void CreateWebBundleSubresourceLoaderFactory(
-    mojo::PendingReceiver<network::mojom::URLLoaderFactory> factory_receiver,
+    CrossVariantMojoReceiver<network::mojom::URLLoaderFactoryInterfaceBase>
+        factory_receiver,
     mojo::ScopedDataPipeConsumerHandle bundle_body);
 
 }  // namespace blink
