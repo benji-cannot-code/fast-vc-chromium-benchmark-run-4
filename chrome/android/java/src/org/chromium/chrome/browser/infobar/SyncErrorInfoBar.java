5c8d86fecae8ac32b467f1c75e5864e885815200
Tanmoy Mollik <triploblastic@google.com>
2020-07-20 11:21:41 +0000

    [Android][Settings] Add new icons in the Main settings page on Android.
    This cl adds new icons to show in the main settings page. The icons are
    used when MOBILE_IDENTITY_CONSISTENCY flag is on.
    
    Screenshot: https://crbug.com/1092857#c1
    
    Bug: 1092857
    Change-Id: Ibb56b4f4586d2ef9dcd29a198fdb9a50049140f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236406
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789888}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/SyncErrorInfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/SyncErrorInfoBar.java
@@ -107,7 +107,7 @@ public class SyncErrorInfoBar
 
     private SyncErrorInfoBar(@SyncErrorInfoBarType int type, String title, String detailsMessage,
             String primaryButtonText) {
-        super(R.drawable.ic_sync_error_40dp, R.color.default_red, null, title, null,
+        super(R.drawable.ic_sync_error_legacy_40dp, R.color.default_red, null, title, null,
                 primaryButtonText, null);
         mType = type;
         mDetailsMessage = detailsMessage;
