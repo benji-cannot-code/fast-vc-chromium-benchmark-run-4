de972c35cfa84934bd9b4e50058339ab9420f33b
Megan Jablonski <megjablon@chromium.org>
2020-07-20 20:29:37 +0000

    Fix Translate Desktop UI Return action
    Ensure that the focus is traversing on the language in which the page is
    translated.
    
    Bug: 1105807
    Change-Id: I14a2b7a957c5ad8b581ca25a0d89a406ba3bfc52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300321
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Commit-Queue: Megan Jablonski <megjablon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790082}

--- chrome/browser/ui/views/translate/translate_bubble_view.cc
+++ chrome/browser/ui/views/translate/translate_bubble_view.cc
@@ -380,7 +380,6 @@ bool TranslateBubbleView::AcceleratorPressed(
       break;
     }
     case TranslateBubbleModel::VIEW_STATE_TRANSLATING:
-      break;
     case TranslateBubbleModel::VIEW_STATE_AFTER_TRANSLATE: {
       if (accelerator.key_code() == ui::VKEY_RETURN) {
         ShowOriginal();
@@ -592,12 +591,14 @@ views::View* TranslateBubbleView::GetCurrentView() const {
 void TranslateBubbleView::Translate() {
   model_->Translate();
   model_->SetViewState(TranslateBubbleModel::VIEW_STATE_TRANSLATING);
+  SwitchView(TranslateBubbleModel::VIEW_STATE_TRANSLATING);
   translate::ReportUiAction(translate::TRANSLATE_BUTTON_CLICKED);
 }
 
 void TranslateBubbleView::ShowOriginal() {
   model_->RevertTranslation();
   model_->SetViewState(TranslateBubbleModel::VIEW_STATE_BEFORE_TRANSLATE);
+  SwitchView(TranslateBubbleModel::VIEW_STATE_BEFORE_TRANSLATE);
   translate::ReportUiAction(translate::SHOW_ORIGINAL_BUTTON_CLICKED);
 }
 
