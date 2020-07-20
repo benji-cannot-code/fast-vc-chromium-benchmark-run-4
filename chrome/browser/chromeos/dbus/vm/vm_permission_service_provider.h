8ee36f59abaf2066500da2c7c70b5353f4f4e3d8
Dmitry Torokhov <dtor@chromium.org>
2020-07-20 06:01:23 +0000

    Send not granted VM permissions in GetPermissions response
    Instead of sending only permissions that have been granted, send also
    the ones that have not been granted, but can potentially be granted.
    Only suppress permissions completely disabled via either a Chrome
    feature flag, or enterprise policy.
    
    Bug: 1071872
    Change-Id: I00a9a11b00c908753e9d21d26b646ff204a5f958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2304990
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Dmitry Torokhov <dtor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789811}

--- chrome/browser/chromeos/dbus/vm/vm_permission_service_provider.h
+++ chrome/browser/chromeos/dbus/vm/vm_permission_service_provider.h
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_DBUS_VM_VM_PERMISSION_SERVICE_PROVIDER_H_
 
 #include <memory>
-#include <set>
 #include <string>
 #include <unordered_map>
 #include <utility>
 
+#include "base/containers/flat_map.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/unguessable_token.h"
@@ -112,7 +112,7 @@ class VmPermissionServiceProvider
     const std::string name_;
     const VmType type_;
 
-    std::set<PermissionType> permissions_;
+    base::flat_map<PermissionType, bool> permissions_;
 
     VmInfo(std::string owner_id, std::string name, VmType type);
     ~VmInfo();
