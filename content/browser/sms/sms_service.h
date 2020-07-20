98e0140db8d55dc008a285507f0a8a735e0e77aa
Majid Valipour <majidvp@chromium.org>
2020-07-20 16:59:43 +0000

    [WebOTP] Refactor sms service tests to not require a particular backend
    Currently the sms_service_unittests only exercises the verification
    backend which is not actually active by default since we are
    currently using User Consent backend.
    
    More than half the tests (10 out of 10) seems to apply to both backends.
    This patch refactors the logic so that the backend specific part of
    sms service are well encapsulated.
    
    It then updates the test so these common tests are exercised with
    both backends.
    
    Changes:
     - Introduce UserConsentHandler with two specific implementation
        1. Noop: which does not show any UI to user and is used with user
           consent backend.
        2. PromptBased: which prompts user to give consent and is used with
           verification backend. This encapsulates previous OpenInfoBar,
           OnCancel, OnConfirm methods from sms service.
     - The consent handler is created on construction of the service and is
       used for its lifetime. This makes it possible to easily inject mock
       consent handlers for testing.
     - Update mock Service to use a simple Noop consent handler for common
       tests which don't involve consent.
     - Introduce a more specialized ServiceWithPrompt that uses a prompt
       based consent handler for tests specific to prompt based logic.
     - Add unit tests for consent handlers.
     - Minor renaming, comments to clarify the code further.
    
    
    There is not expected to be any functional changes in this CL.
    
    
    Change-Id: I411bf9d12cb7a2051e545ce6dd8a50fe37204326
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254447
    Reviewed-by: Sam Goto <goto@chromium.org>
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789970}

--- content/browser/sms/sms_service.h
+++ content/browser/sms/sms_service.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "content/browser/sms/sms_queue.h"
+#include "content/browser/sms/user_consent_handler.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/frame_service_base.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -45,6 +46,7 @@ class CONTENT_EXPORT SmsService
              RenderFrameHost*,
              mojo::PendingReceiver<blink::mojom::SmsReceiver>);
   SmsService(SmsFetcher*,
+             std::unique_ptr<UserConsentHandler> consent_handler,
              const url::Origin&,
              RenderFrameHost*,
              mojo::PendingReceiver<blink::mojom::SmsReceiver>);
@@ -57,31 +59,27 @@ class CONTENT_EXPORT SmsService
   // content::SmsQueue::Subscriber
   void OnReceive(const std::string& one_time_code) override;
 
+  // Completes the in-flight sms otp code request. Invokes the receive callback,
+  // if one is available, with the provided status code and the existing one
+  // time code.
+  void CompleteRequest(blink::mojom::SmsStatus);
+
  protected:
   // content::WebContentsObserver:
   void NavigationEntryCommitted(
       const content::LoadCommittedDetails& load_details) override;
 
  private:
-  void OpenInfoBar(const std::string& one_time_code);
-  void Process(blink::mojom::SmsStatus,
-               base::Optional<std::string> one_time_code);
   void CleanUp();
 
-  // Called when the user manually clicks the 'Enter code' button.
-  void OnConfirm();
-  // Called when the user manually dismisses the infobar.
-  void OnCancel();
 
   // |fetcher_| is safe because all instances of SmsFetcher are owned
   // by the browser context, which transitively (through RenderFrameHost) owns
   // and outlives this class.
   SmsFetcher* fetcher_;
+  std::unique_ptr<UserConsentHandler> consent_handler_;
 
   const url::Origin origin_;
-
-  bool prompt_open_ = false;
-
   ReceiveCallback callback_;
   base::Optional<std::string> one_time_code_;
   base::TimeTicks start_time_;
