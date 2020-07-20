a8158dfd4899ea7e760198bbff6ef253aa48d806
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-07-20 17:49:09 +0000

    [CrOS settings] Add OS Settings Polymer3 feature flag
    Bug: 1045266
    Change-Id: Ife06c03e0de7ee59cd74c1d65c67e4465928b85c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303876
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#789995}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2311,6 +2311,9 @@ extern const char kMediaSessionNotificationsDescription[];
 extern const char kMeteredShowToggleName[];
 extern const char kMeteredShowToggleDescription[];
 
+extern const char kOsSettingsPolymer3Name[];
+extern const char kOsSettingsPolymer3Description[];
+
 extern const char kPrintServerUiName[];
 extern const char kPrintServerUiDescription[];
 
--- chromeos/constants/chromeos_features.h
+++ chromeos/constants/chromeos_features.h
@@ -142,6 +142,8 @@ extern const base::Feature kOobeScreensPriority;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kOsSettingsDeepLinking;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
+extern const base::Feature kOsSettingsPolymer3;
+COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kParentalControlsSettings;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kPluginVmShowCameraPermissions;
