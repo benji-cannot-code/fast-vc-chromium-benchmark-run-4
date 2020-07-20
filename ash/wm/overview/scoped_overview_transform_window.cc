a365ddff5f8c02b69903a5e73744966edbd741f6
Sammie Quon <sammiequon@chromium.org>
2020-07-20 18:13:53 +0000

    overview: Fix bug with notches in the transformed window.
    Think it has something to do with subpixel rendering, and/or the damage
    on the bookmark bar is incorrect. It seems like a harmless and logical
    enough fix I did not investigate further.
    
    This can happen to any transformed browser window for instance the drag
    from shelf feature. I didn't fix that one because its harder to notice
    during a drag, and rounding during a drag can cause other visuals that
    are less desirable.
    
    Test: manual
    Change-Id: Icc680063c3aed19e7811c918b622fb0da5a20d57
    Fixed: 1035223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2307615
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790012}

--- ash/wm/overview/scoped_overview_transform_window.cc
+++ ash/wm/overview/scoped_overview_transform_window.cc
@@ -33,12 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/scoped_window_event_targeting_blocker.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/layer.h"
-#include "ui/compositor/layer_animation_element.h"
 #include "ui/compositor/layer_animation_observer.h"
-#include "ui/compositor/layer_animation_sequence.h"
 #include "ui/compositor/layer_animator.h"
 #include "ui/compositor/layer_observer.h"
 #include "ui/gfx/geometry/insets.h"
+#include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/gfx/geometry/vector2d_f.h"
 #include "ui/gfx/transform_util.h"
 #include "ui/views/layout/layout_provider.h"
@@ -440,7 +439,11 @@ gfx::RectF ScopedOverviewTransformWindow::ShrinkRectToFitPreservingAspectRatio(
       break;
   }
 
-  return new_bounds;
+  // If we do not use whole numbers, there may be some artifacts drawn (i.e.
+  // shadows, notches). This may be an effect of subpixel rendering. It's ok to
+  // round it here since this is the last calculation (we don't have to worry
+  // about roundoff error).
+  return gfx::RectF(gfx::ToRoundedRect(new_bounds));
 }
 
 aura::Window* ScopedOverviewTransformWindow::GetOverviewWindow() const {
