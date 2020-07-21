e5562fd99ae13ee0c7e720ec314717855e32f7e6
Jing Wang <jiwan@chromium.org>
2020-07-21 07:09:02 +0000

    Refactor emoji suggester and some minor behaviour change.
    1. When emoji suggestion pop up, pressing "up" won't enter browsing mode,
    this is now the same as personal info suggester.
    2. Allow number input after entering browsing mode, pressing "down" is
    not required as the previous action
    3. Unify some naming fashion in two suggesters.
    4. In emoji suggester, change the way of managing highlight buttons, now
    it's the same as in personal info suggester
    5. Removed some unnecessary functions.
    6. Updated tests.
    
    Test: tested with Chrome on Linux.
    Bug: NOBUG=refactor
    Change-Id: I91828526f0ecaaac3380764ab430e2c6f07d0417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303931
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: My Nguyen <myy@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790276}

--- chrome/browser/chromeos/input_method/personal_info_suggester.h
+++ chrome/browser/chromeos/input_method/personal_info_suggester.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/optional.h"
 #include "chrome/browser/chromeos/input_method/input_method_engine_base.h"
 #include "chrome/browser/chromeos/input_method/suggester.h"
 #include "chrome/browser/chromeos/input_method/suggestion_enums.h"
@@ -129,7 +130,7 @@ class PersonalInfoSuggester : public Suggester {
   std::vector<ui::ime::AssistiveWindowButton> buttons_;
   int highlighted_index_;
   ui::ime::AssistiveWindowButton suggestion_button_;
-  ui::ime::AssistiveWindowButton link_button_;
+  ui::ime::AssistiveWindowButton settings_button_;
 };
 
 }  // namespace chromeos
