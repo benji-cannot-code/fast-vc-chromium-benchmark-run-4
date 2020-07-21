ed6f26b9a06f3bd1436b36522260c73d2cdad9f9
Greg Thompson <grt@chromium.org>
2020-07-21 21:08:52 +0000

    Introduce MiniFile, a simple wrapper around a file handle.
    Resource extraction and file expansion in the mini_installer now use
    the MiniFile class to manage output files while they are open. It takes
    care of handle lifetime management and provides a robust way to delete
    an open file.
    
    This change also fixes two potential file leaks. Specifically, a
    destination file created by either PEResource::WriteToDisk or
    mini_installer::Expand is now deleted in case of error.
    
    BUG=516207
    
    Change-Id: I2ee894d4e6f2d04fbb6cf3355638ee50e8bb1bf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303400
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790547}

---
+++ chrome/installer/mini_installer/path_string.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_INSTALLER_MINI_INSTALLER_PATH_STRING_H_
+#define CHROME_INSTALLER_MINI_INSTALLER_PATH_STRING_H_
+
+#include <windows.h>
+
+#include "chrome/installer/mini_installer/mini_string.h"
+
+namespace mini_installer {
+
+// A string sufficiently large to hold a Windows file path. Note that starting
+// with Windows 10 1607, applications may opt-in to supporting long paths via
+// the longPathAware element in the app's manifest. Chrome does not do this. See
+// https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file#maximum-path-length-limitation
+// for details.
+using PathString = StackString<MAX_PATH>;
+
+}  // namespace mini_installer
+
+#endif  // CHROME_INSTALLER_MINI_INSTALLER_PATH_STRING_H_
