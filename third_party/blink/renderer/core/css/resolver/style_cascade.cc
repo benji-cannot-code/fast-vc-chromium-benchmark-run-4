eea02f855791b47122ff68e40a88c2e816ccbf44
Anders Hartvoll Ruud <andruud@chromium.org>
2020-07-21 15:06:39 +0000

    Count kSummaryElementWithDisplayBlockAuthorRule in StyleCascade
    This eliminates the final call-site for MatchResult::AuthorRules, which
    means we can (in a future CL) remove MatchResult::UaRules, ::UserRules
    and ::AuthorRules.
    
    Also establish a common pattern for use-counting in StyleCascade,
    isolating as much use counting "noise" as possible into separate
    functions.
    
    Finally, this CL adds a unit test for kSummaryElementWithDisplayBlock-
    AuthorRule, since we didn't have one already.
    
    Change-Id: I4693fd8ee538da88fea0648eda8ccfac31818dbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2307238
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790388}

--- third_party/blink/renderer/core/css/resolver/style_cascade.cc
+++ third_party/blink/renderer/core/css/resolver/style_cascade.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
+#include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/style_property_shorthand.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
@@ -228,6 +229,8 @@ void StyleCascade::AnalyzeMatchResult() {
       map_.Add(property.GetCSSPropertyName(), e.Priority());
     }
   }
+
+  MaybeUseCountSummaryDisplayBlock();
 }
 
 void StyleCascade::AnalyzeInterpolations() {
@@ -699,11 +702,7 @@ const CSSValue* StyleCascade::ResolveRevert(const CSSProperty& property,
                                             const CSSValue& value,
                                             CascadeOrigin origin,
                                             CascadeResolver& resolver) {
-  // In forced colors mode, we can behave like 'revert' any value [1], but we
-  // should only use-count the true uses of 'revert'.
-  // [1] https://drafts.csswg.org/css-color-adjust-1/#forced-colors-properties
-  if (IsRevert(value))
-    GetDocument().CountUse(WebFeature::kCSSKeywordRevert);
+  MaybeUseCountRevert(value);
 
   CascadeOrigin target_origin = TargetOriginForRevert(origin);
 
@@ -928,6 +927,32 @@ const CSSProperty& StyleCascade::ResolveSurrogate(const CSSProperty& property) {
       state_.Style()->Direction(), state_.Style()->GetWritingMode());
   DCHECK(original);
   return *original;
+}
+
+void StyleCascade::CountUse(WebFeature feature) {
+  GetDocument().CountUse(feature);
+}
+
+void StyleCascade::MaybeUseCountRevert(const CSSValue& value) {
+  // In forced colors mode, any value can behave like 'revert' [1], but we
+  // should only use-count the true uses of 'revert'.
+  // [1] https://drafts.csswg.org/css-color-adjust-1/#forced-colors-properties
+  if (IsRevert(value))
+    CountUse(WebFeature::kCSSKeywordRevert);
+}
+
+// TODO(crbug.com/590014): Remove this when display type of <summary> is fixed
+void StyleCascade::MaybeUseCountSummaryDisplayBlock() {
+  if (!state_.GetElement().HasTagName(html_names::kSummaryTag))
+    return;
+  CascadePriority priority = map_.At(CSSPropertyName(CSSPropertyID::kDisplay));
+  if (priority.GetOrigin() == CascadeOrigin::kUserAgent)
+    return;
+  const CSSValue* value = ValueAt(match_result_, priority.GetPosition());
+  if (auto* identifier = DynamicTo<CSSIdentifierValue>(value)) {
+    if (identifier->GetValueID() == CSSValueID::kBlock)
+      CountUse(WebFeature::kSummaryElementWithDisplayBlockAuthorRule);
+  }
 }
 
 }  // namespace blink
--- third_party/blink/renderer/core/css/resolver/style_cascade.h
+++ third_party/blink/renderer/core/css/resolver/style_cascade.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/resolver/cascade_origin.h"
 #include "third_party/blink/renderer/core/css/resolver/cascade_priority.h"
 #include "third_party/blink/renderer/core/css/resolver/match_result.h"
+#include "third_party/blink/renderer/core/frame/web_feature_forward.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/wtf/text/text_encoding.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -324,6 +325,10 @@ class CORE_EXPORT StyleCascade {
   const CSSProperty& ResolveSurrogate(const CSSProperty& surrogate);
 
   bool ShouldRevert(const CSSProperty&, const CSSValue&, CascadeOrigin);
+
+  void CountUse(WebFeature);
+  void MaybeUseCountRevert(const CSSValue&);
+  void MaybeUseCountSummaryDisplayBlock();
 
   StyleResolverState& state_;
   MatchResult match_result_;
