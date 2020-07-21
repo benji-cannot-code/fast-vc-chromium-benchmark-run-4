8afea0ec09e3496bd68faa1b12dc217c0b2a66de
David Roger <droger@chromium.org>
2020-07-21 15:12:19 +0000

    [signin] Improve style for dice signin interception bubble
    Screenshot with this CL:
    https://drive.google.com/file/d/1b-x0NrGnycEfV24EQ9pb_vKBHfPc2X60/view
    
    Mock:
    https://drive.google.com/file/d/1ckOFYd_eFQ1UK5byLkeXOuNqo0Fskxj2/view?usp=sharing
    
    Bug: 1076880
    Change-Id: I5d0b4ab2194899039e97f86a90b1aa3d578bd78d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292300
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790393}

--- chrome/browser/resources/signin/dice_web_signin_intercept/dice_web_signin_intercept_app.js
+++ chrome/browser/resources/signin/dice_web_signin_intercept/dice_web_signin_intercept_app.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import './strings.m.js';
+import './signin_shared_css.js';
 
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
--- chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
+++ chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 
 namespace {
-constexpr int kInterceptionBubbleHeight = 362;
+constexpr int kInterceptionBubbleHeight = 342;
 constexpr int kInterceptionBubbleWidth = 290;
 }  // namespace
 
