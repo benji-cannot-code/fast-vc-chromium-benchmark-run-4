b6e2af2fdc16a897c8b689979b4e5d58f55d9751
Katie Dektar <katie@chromium.org>
2020-07-21 18:17:08 +0000

    Revert "[COOP] access reporting [5/N] Plumb report type."
    This reverts commit 6e52cf215e6406f19c38227899f5cd392998c0e0.
    
    Reason for revert: Speculative revert: A related test has begun
    failing since this landed.
    
    Failing test:
    external/wpt/html/cross-origin-opener-policy/reporting/access-reporting/property.https.htm
    
    First failing build (with this change in blamelist): 
    https://ci.chromium.org/p/chromium/builders/ci/WebKit%20Linux%20Leak/16841
    
    Original change's description:
    > [COOP] access reporting [5/N] Plumb report type.
    > 
    > All of this is put behind a flag disabled by default.
    > This is mostly based on the initial prototype:
    > https://chromium-review.googlesource.com/c/chromium/src/+/2223934/24
    > 
    > Define, plumb and use the "report-type" for COOP access reporting.
    > 
    > COOP access reporting:
    > [1/N] https://chromium-review.googlesource.com/c/chromium/src/+/2264294
    > [2/N] https://chromium-review.googlesource.com/c/chromium/src/+/2270185
    > [3/N] https://chromium-review.googlesource.com/c/chromium/src/+/2270472
    > [4/N] https://chromium-review.googlesource.com/c/chromium/src/+/2273120
    > [5/N] this patch.
    > 
    > Bug: chromium:1090273
    > Change-Id: I5e51b3da6a20c85c073ea1fd84ea74557760a42f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2309433
    > Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Reviewed-by: Camille Lamy <clamy@chromium.org>
    > Reviewed-by: Pâris Meuleman <pmeuleman@chromium.org>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#790420}
    
    TBR=clamy@chromium.org,mkwst@chromium.org,arthursonzogni@chromium.org,pmeuleman@chromium.org
    
    Change-Id: I88a01f1dc601205b2344c4251747cf75ef5a84d0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:1090273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310769
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790466}

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -2381,7 +2381,6 @@ void LocalFrame::GetStringForRange(const gfx::Range& range,
 #endif
 
 void LocalFrame::InstallCoopAccessMonitor(
-    network::mojom::blink::CoopAccessReportType report_type,
     const base::UnguessableToken& accessed_window,
     mojo::PendingRemote<network::mojom::blink::CrossOriginOpenerPolicyReporter>
         reporter) {
@@ -2390,7 +2389,7 @@ void LocalFrame::InstallCoopAccessMonitor(
   if (!accessed_frame)
     return;
 
-  accessed_frame->DomWindow()->InstallCoopAccessMonitor(report_type, this,
+  accessed_frame->DomWindow()->InstallCoopAccessMonitor(this,
                                                         std::move(reporter));
 }
 
