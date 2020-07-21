918c25f265347ab7eff78a3fef4e9d47d4ebae8e
Toby Huang <tobyhuang@chromium.org>
2020-07-21 20:23:02 +0000

    Add UMA metric to track state of extension permissions toggle
    When the parent disables the "Permissions for sites, apps and
    extensions" toggle, supervised users can't install new extensions and
    can't approve permission updates on existing extensions by themselves.
    Otherwise, supervised users can install new extensions with parent/
    custodian approval and approve permission updates on existing
    extensions by themselves. Therefore, this toggle is very important for
    controlling extensions behavior for supervised users.
    
    Since this toggle is enabled by default, we want to track how often
    the toggle is disabled for supervised users by adding a new UMA
    metric.
    
    Bug: 1096763
    Change-Id: Ie1506fccc14e81a4fc2711335ede3457cb4813b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2296823
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790528}

--- chrome/browser/supervised_user/supervised_user_pref_store.cc
+++ chrome/browser/supervised_user/supervised_user_pref_store.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "chrome/browser/chrome_notification_types.h"
@@ -168,6 +169,9 @@ void SupervisedUserPrefStore::OnNewSettingsAvailable(
                            &permissions_disallowed);
       prefs_->SetBoolean(prefs::kSupervisedUserExtensionsMayRequestPermissions,
                          !permissions_disallowed);
+      base::UmaHistogramBoolean(
+          "SupervisedUsers.ExtensionsMayRequestPermissions",
+          !permissions_disallowed);
     }
 #endif
   }
