b686ab971e8c7d2994b9736566726a38f007ba55
Mila Green <milagreen@chromium.org>
2020-07-21 21:30:42 +0000

    Reland "AppWake starts the server instead of doing an update check." Updater: Fix ASAN test failure. Reland "Updater: AppWake starts the server instead of doing an update check."
    This is a reland of 4d324410999591ecfc2ab5dd1ff0a7c76ed8b2b6
    
    Original change's description:
    > Updater: AppWake starts the server instead of doing an update check.
    >
    > Bug: 1105589
    >
    > Change-Id: I6d7e8b0b7858a4f1a538684758644f134318c0a4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2298045
    > Commit-Queue: Mila Green <milagreen@chromium.org>
    > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#790139}
    
    Bug: 1105589
    Change-Id: I7946629e862fbd9d3d42113de16f194be7a56df7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2309225
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Mila Green <milagreen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790561}

--- chrome/updater/app/server/mac/service_delegate.h
+++ chrome/updater/app/server/mac/service_delegate.h
@@ -11,11 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 
 namespace updater {
+class ControlService;
 class UpdateService;
 class AppServerMac;
 }
 
-@interface CRUUpdateCheckXPCServiceDelegate : NSObject <NSXPCListenerDelegate>
+@interface CRUUpdateCheckServiceXPCDelegate : NSObject <NSXPCListenerDelegate>
 
 - (instancetype)init NS_UNAVAILABLE;
 
@@ -27,15 +28,14 @@ class AppServerMac;
 
 @end
 
-@interface CRUAdministrationXPCServiceDelegate
-    : NSObject <NSXPCListenerDelegate>
+@interface CRUControlServiceXPCDelegate : NSObject <NSXPCListenerDelegate>
 
 - (instancetype)init NS_UNAVAILABLE;
 
 // Designated initializer.
 - (instancetype)
-    initWithUpdateService:(scoped_refptr<updater::UpdateService>)service
-                appServer:(scoped_refptr<updater::AppServerMac>)appServer
+    initWithControlService:(scoped_refptr<updater::ControlService>)service
+                 appServer:(scoped_refptr<updater::AppServerMac>)appServer
     NS_DESIGNATED_INITIALIZER;
 
 @end
