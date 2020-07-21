4fe43525c25dfb6c3fabeec9769a558e1c155f09
Matthias Körber <koerber@google.com>
2020-07-21 16:12:44 +0000

    [Autofill][Slimshady] Added a provider for enumerated regular expressions.
    This utility class lazily builds the patterns, compiles and caches
    enumerated regular expressions that are used for parsing and matching.
    
    The justification for this is that it becomes unnecessary to
    repeatedly rebuild patterns and pass them around the code.
    Further, a future integration of a component updater
    to change the patterns behind on of the expressions becomes minimally
    invasive.
    
    Change-Id: Icb7082e504603b43358a6d84f853e7b5336c3cad
    Bug: 1099202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2299632
    Commit-Queue: Matthias Körber <koerber@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790415}

---
+++ components/autofill/core/browser/data_model/autofill_structured_address_constants.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_DATA_MODEL_AUTOFILL_STRUCTURED_ADDRESS_CONSTANTS_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_DATA_MODEL_AUTOFILL_STRUCTURED_ADDRESS_CONSTANTS_H_
+
+namespace autofill {
+namespace structured_address {
+
+// Regular expression pattern to match a single word.
+extern const char kSingleWordRe[];
+
+}  // namespace structured_address
+}  // namespace autofill
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_DATA_MODEL_AUTOFILL_STRUCTURED_ADDRESS_CONSTANTS_H_
