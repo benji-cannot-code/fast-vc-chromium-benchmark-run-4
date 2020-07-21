05f671475c9d7b14935c867bab6f5b33437a0bcc
Xianzhu Wang <wangxianzhu@chromium.org>
2020-07-21 21:02:17 +0000

    Avoid LayoutObject::FragmentsVisualRectBoundingBox() in paint timing
    We are removing the function to remove obstacle to integrate paint
    with LayoutNGBlockFragments.
    
    Now always pass image border size to painting timing detectors. This
    is also more accurate than the visual rect used previously. Visual rect
    includes visual overflows, borders, etc. which are not part of the
    image.
    
    For the text in FileUploadControl, now report the bounds of the text
    instead of the control's visual rect which also covers the button and
    visual overflows.
    
    Bug: 1104064
    Change-Id: I9974ee67c5f0a0bdc84e0d0530c12c4843b577ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2309801
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790544}

--- third_party/blink/renderer/core/paint/image_element_timing.h
+++ third_party/blink/renderer/core/paint/image_element_timing.h
@@ -53,7 +53,8 @@ class CORE_EXPORT ImageElementTiming final
   void NotifyImagePainted(
       const LayoutObject*,
       const ImageResourceContent* cached_image,
-      const PropertyTreeStateOrAlias& current_paint_chunk_properties);
+      const PropertyTreeStateOrAlias& current_paint_chunk_properties,
+      const IntRect& image_border);
 
   void NotifyBackgroundImagePainted(
       Node*,
@@ -75,7 +76,7 @@ class CORE_EXPORT ImageElementTiming final
       const ImageResourceContent& cached_image,
       const PropertyTreeStateOrAlias& current_paint_chunk_properties,
       base::TimeTicks load_time,
-      const IntRect* image_border);
+      const IntRect& image_border);
 
   // Callback for the swap promise. Reports paint timestamps.
   void ReportImagePaintSwapTime(WebSwapResult, base::TimeTicks timestamp);
--- third_party/blink/renderer/core/paint/image_paint_timing_detector.cc
+++ third_party/blink/renderer/core/paint/image_paint_timing_detector.cc
@@ -219,7 +219,7 @@ void ImagePaintTimingDetector::RecordImage(
     const ImageResourceContent& cached_image,
     const PropertyTreeStateOrAlias& current_paint_chunk_properties,
     const StyleFetchedImage* style_image,
-    const IntRect* image_border) {
+    const IntRect& image_border) {
   Node* node = object.GetNode();
   if (!node)
     return;
@@ -238,9 +238,7 @@ void ImagePaintTimingDetector::RecordImage(
             frame_view_->GetPaintTimingDetector().Visualizer()) {
       FloatRect mapped_visual_rect =
           frame_view_->GetPaintTimingDetector().CalculateVisualRect(
-              image_border ? *image_border
-                           : object.FragmentsVisualRectBoundingBox(),
-              current_paint_chunk_properties);
+              image_border, current_paint_chunk_properties);
       visualizer->DumpImageDebuggingRect(object, mapped_visual_rect,
                                          cached_image);
     }
@@ -249,15 +247,14 @@ void ImagePaintTimingDetector::RecordImage(
 
   if (is_recored_visible_image || !is_recording_)
     return;
-  IntRect visual_rect =
-      image_border ? *image_border : object.FragmentsVisualRectBoundingBox();
+
   // Before the image resource starts loading, <img> has no size info. We wait
   // until the size is known.
-  if (visual_rect.IsEmpty())
+  if (image_border.IsEmpty())
     return;
   FloatRect mapped_visual_rect =
       frame_view_->GetPaintTimingDetector().CalculateVisualRect(
-          visual_rect, current_paint_chunk_properties);
+          image_border, current_paint_chunk_properties);
   if (base::Optional<PaintTimingVisualizer>& visualizer =
           frame_view_->GetPaintTimingDetector().Visualizer()) {
     visualizer->DumpImageDebuggingRect(object, mapped_visual_rect,
@@ -265,7 +262,7 @@ void ImagePaintTimingDetector::RecordImage(
   }
   uint64_t rect_size = mapped_visual_rect.Size().Area();
   // Transform visual rect to window before calling downscale.
-  WebFloatRect float_visual_rect = FloatRect(visual_rect);
+  WebFloatRect float_visual_rect = FloatRect(image_border);
   frame_view_->GetPaintTimingDetector().ConvertViewportToWindow(
       &float_visual_rect);
   rect_size = DownScaleIfIntrinsicSizeIsSmaller(
