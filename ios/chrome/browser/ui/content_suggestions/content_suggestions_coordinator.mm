28f3b464e3f065ecacdb31500d86bfe61757e4ef
edchin <edchin@chromium.org>
2020-07-21 19:33:13 +0000

    [ios] Use UrlLoadingBrowserAgent in NTP
    UrlLoadingBrowserAgent is the proper way to load a URL
    in the current tab.
    
    This CL also deprecates use of DropAndNavigate in favor
    of the new URLDragDropHandler.
    
    This CL should have no visible user change.
    
    Test: Enable DragAndDrop flag. In iPad multitasking, drop a URL onto the NTP.
    Change-Id: If593598f7ab6da4f54078ccd4f7765134aa32234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2288738
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790494}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -17,8 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/drag_and_drop/drag_and_drop_flag.h"
-#import "ios/chrome/browser/drag_and_drop/drop_and_navigate_delegate.h"
-#import "ios/chrome/browser/drag_and_drop/drop_and_navigate_interaction.h"
+#import "ios/chrome/browser/drag_and_drop/url_drag_drop_handler.h"
 #include "ios/chrome/browser/favicon/ios_chrome_large_icon_cache_factory.h"
 #include "ios/chrome/browser/favicon/ios_chrome_large_icon_service_factory.h"
 #include "ios/chrome/browser/favicon/large_icon_cache.h"
@@ -50,11 +49,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/utils/pref_backed_boolean.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/url_loading/url_loading_browser_agent.h"
+#import "ios/chrome/browser/url_loading/url_loading_params.h"
 #import "ios/chrome/browser/voice/voice_search_availability.h"
 #import "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/discover_feed/discover_feed_provider.h"
-#import "ios/web/public/navigation/navigation_manager.h"
-#import "ios/web/public/web_state.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -62,8 +60,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface ContentSuggestionsCoordinator () <
     ContentSuggestionsViewControllerAudience,
-    DropAndNavigateDelegate,
-    OverscrollActionsControllerDelegate> {
+    OverscrollActionsControllerDelegate,
+    URLDropDelegate> {
   // Helper object managing the availability of the voice search feature.
   VoiceSearchAvailability _voiceSearchAvailability;
 }
@@ -77,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong) ContentSuggestionsMetricsRecorder* metricsRecorder;
 @property(nonatomic, strong) NTPHomeMediator* NTPMediator;
 @property(nonatomic, strong) UIViewController* discoverFeedViewController;
+@property(nonatomic, strong) URLDragDropHandler* dragDropHandler;
 
 // Redefined as readwrite.
 @property(nonatomic, strong, readwrite)
@@ -240,9 +239,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       self.headerCollectionInteractionHandler;
 
   if (DragAndDropIsEnabled()) {
+    self.dragDropHandler = [[URLDragDropHandler alloc] init];
+    self.dragDropHandler.dropDelegate = self;
     [self.suggestionsViewController.collectionView
-        addInteraction:[[DropAndNavigateInteraction alloc]
-                           initWithDelegate:self]];
+        addInteraction:[[UIDropInteraction alloc]
+                           initWithDelegate:self.dragDropHandler]];
   }
 }
 
@@ -328,12 +329,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return nullptr;
 }
 
-#pragma mark - DropAndNavigateDelegate
+#pragma mark - URLDropDelegate
 
-- (void)URLWasDropped:(GURL const&)URL {
-  web::NavigationManager::WebLoadParams params(URL);
-  params.transition_type = ui::PAGE_TRANSITION_TYPED;
-  self.webState->GetNavigationManager()->LoadURLWithParams(params);
+- (BOOL)canHandleURLDropInView:(UIView*)view {
+  return YES;
+}
+
+- (void)view:(UIView*)view didDropURL:(const GURL&)URL atPoint:(CGPoint)point {
+  UrlLoadingBrowserAgent::FromBrowser(self.browser)
+      ->Load(UrlLoadParams::InCurrentTab(URL));
 }
 
 #pragma mark - Public methods
