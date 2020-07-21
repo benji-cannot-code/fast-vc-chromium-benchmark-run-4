060f9915024ea378ce12040df75b4b9ebed3bd35
Will Harris <wfh@chromium.org>
2020-07-21 20:20:57 +0000

    Allow CFG guards to be generated with gn config.
    This CL adds a gn flag "win_enable_cfg_guards" to allow CFG guard
    dispatch calls to be generated by the compiler.
    
    This also adds a test to verify that a guarded indirect call to
    an invalid location results in a 0xC0000409 fast fail.
    
    It also turns on the CFI linker for base_unittests and
    sbox_integration_tests.
    
    BUG=584575
    
    Cq-Include-Trybots: luci.chromium.try:win7-rel
    Change-Id: I446d3a28733a46fa4d8f96f28cdfc7b132b10772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300634
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790525}

--- base/debug/invalid_access_win.cc
+++ base/debug/invalid_access_win.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/invalid_access_win.h"
 
+#include <intrin.h>
 #include <stdlib.h>
 #include <windows.h>
 
@@ -16,6 +17,22 @@ namespace debug {
 namespace win {
 
 namespace {
+
+// Function that can be jumped midway into safely.
+__attribute__((naked)) int nop_sled() {
+  __asm {
+     nop
+     nop
+     ret
+  }
+}
+
+using FuncType = decltype(&nop_sled);
+
+void IndirectCall(FuncType* func) {
+  // This code always generates CFG guards.
+  (*func)();
+}
 
 void CreateSyntheticHeapCorruption() {
   EXCEPTION_RECORD record = {};
@@ -49,6 +66,21 @@ void TerminateWithHeapCorruption() {
     CHECK(false);
   }
   // Should never reach here.
+  abort();
+}
+
+void TerminateWithControlFlowViolation() {
+  // Call into the middle of the NOP sled.
+  FuncType func =
+      reinterpret_cast<FuncType>((reinterpret_cast<uintptr_t>(nop_sled)) + 0x1);
+  __try {
+    // Generates a STATUS_STACK_BUFFER_OVERRUN exception if CFG triggers.
+    IndirectCall(&func);
+  } __except (EXCEPTION_EXECUTE_HANDLER) {
+    // CFG fast fail should never be caught.
+    CHECK(false);
+  }
+  // Should only reach here if CFG is disabled.
   abort();
 }
 
--- base/debug/invalid_access_win.h
+++ base/debug/invalid_access_win.h
@@ -16,6 +16,9 @@ namespace win {
 // terminate immediately with a fast fail exception.
 [[noreturn]] BASE_EXPORT void TerminateWithHeapCorruption();
 
+// Creates a CFG violation.
+[[noreturn]] BASE_EXPORT void TerminateWithControlFlowViolation();
+
 }  // namespace win
 }  // namespace debug
 }  // namespace base
