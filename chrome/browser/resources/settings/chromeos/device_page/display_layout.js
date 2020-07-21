cabb05b35156d77a2a73845e94dffd34a0af7959
Bailey Berro <baileyberro@chromium.org>
2020-07-21 19:19:25 +0000

    Only show display highlight while dragging
    This change makes it so that a highlight around the currently selected
    display is only shown once dragging of the display in the Display Layout
    section begins. Previously, whenever Display settings was open and
    multiple displays were attached to the device, whichever display was
    currently selected in settings would be highlighted at all times.
    
    Bug: 1070122
    Change-Id: I2ab461dc15db4b63b98a54383ca8966e5083ffdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290353
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790490}

--- chrome/browser/resources/settings/chromeos/device_page/display_layout.js
+++ chrome/browser/resources/settings/chromeos/device_page/display_layout.js
@@ -71,6 +71,9 @@ Polymer({
   allowDisplayAlignmentApi_:
       loadTimeData.getBoolean('allowDisplayAlignmentApi'),
 
+  /** @private {string} */
+  invalidDisplayId_: loadTimeData.getString('invalidDisplayId'),
+
   /** @override */
   created() {
     if (this.allowDisplayAlignmentApi_) {
@@ -273,7 +276,14 @@ Polymer({
       this.finishUpdateDisplayBounds(id);
       newBounds = this.getCalculatedDisplayBounds(id);
       this.lastDragCoordinates_ = null;
+      if (this.allowDisplayIdentificationApi_) {
+        // When the drag stops, remove the highlight around the display.
+        this.browserProxy_.highlightDisplay(this.invalidDisplayId_);
+      }
     } else {
+      if (this.allowDisplayIdentificationApi_) {
+        this.browserProxy_.highlightDisplay(id);
+      }
       // Make sure the dragged display is also selected.
       if (id != this.selectedDisplay.id) {
         this.fire('select-display', id);
