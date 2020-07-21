02b28ad4d15fde7d0f619396d638898f168f66eb
Tomasz Wiszkowski <ender@google.com>
2020-07-21 02:11:17 +0000

    Ensure the keyboard is always shown when there are no suggestions.
    This change addresses the case where an empty suggestions list is
    received, following a very rich suggestions list from previous
    Omnibox interaction.
    
    Mediator's internal logic clears up the locally stored
    AutocompleteResult as user completes interaction with the Omnibox,
    making the subsequently received empty list detected as a no-op.
    
    Bug: 1107562
    Change-Id: Ifa50c9aea78e730a0424af411ea5d08c7dd5b015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2308134
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790218}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListBuilder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListBuilder.java
@@ -207,6 +207,7 @@ class DropdownItemViewInfoListBuilder {
             mImageFetcher.clear();
         }
 
+        mBuiltListHasFullyConcealedElements = false;
         mHeaderProcessor.onUrlFocusChange(hasFocus);
         for (int index = 0; index < mPriorityOrderedSuggestionProcessors.size(); index++) {
             mPriorityOrderedSuggestionProcessors.get(index).onUrlFocusChange(hasFocus);
