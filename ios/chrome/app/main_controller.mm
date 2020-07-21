53edc31eba7c58a1cbfc2f2ae43625611b975ac4
Stepan Khapugin <stkhapugin@chromium.org>
2020-07-21 14:59:47 +0000

    [multiball] Remove main controller's FRE UI blocker.
    Remove this UI blocker because there's now one owned by the scene
    that's showing the FRE, so this one is unnecessary.
    
    Bug: none
    Change-Id: I742f0e432fdec3e33fd4434bcbba7c3440eba07d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2289858
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790386}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -263,9 +263,6 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
 
   // Hander for the startup tasks, deferred or not.
   StartupTasks* _startupTasks;
-
-  // UI blocker used during first run in multiwindow.
-  std::unique_ptr<ScopedUIBlocker> _firstRunUIBlocker;
 }
 
 // The ChromeBrowserState associated with the main (non-OTR) browsing mode.
@@ -608,10 +605,6 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
               object:nil];
 
   [self markEulaAsAccepted];
-
-  if (IsMultiwindowSupported()) {
-    _firstRunUIBlocker.reset();
-  }
 }
 
 - (void)handleFirstRunUIDidFinish {
@@ -1086,11 +1079,6 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
          selector:@selector(handleFirstRunUIDidFinish)
              name:kChromeFirstRunUIDidFinishNotification
            object:nil];
-
-  if (IsMultiwindowSupported()) {
-    // Update the AppState.
-    _firstRunUIBlocker = std::make_unique<ScopedUIBlocker>(presentingScene);
-  }
 }
 
 - (void)crashIfRequested {
