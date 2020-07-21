32641acfa8add33e8bd45d984ff7a096bfe06976
Pavel Feldman <pfeldman@chromium.org>
2020-07-21 20:01:15 +0000

    CDP: report binary post data in Network and Fetch domains
    This change introduces a binary post data field in the Network.Request
    type. New field contains entries that correspond to the http body data
    entries. New PostDataEntry type supports text/bytes data only, but
    allows progressive enhancement for other types such as files and blobs.
    
    Bug: 1107728
    Change-Id: I205dd7fc93d627a14c111cc2a05a63ce962bc449
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2309660
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790513}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/fetch/fetch-post-data.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      `Test post data interception`);
+
+  await dp.Network.enable();
+  await dp.Fetch.enable();
+
+  const [requestWillBeSent, requestPaused] = await Promise.all([
+    dp.Network.onceRequestWillBeSent(),
+    dp.Fetch.onceRequestPaused(),
+    session.evaluate(`fetch('${testRunner.url('./resources/hello-world.txt')}', {
+                          method: 'post',
+                          body: new Uint8Array([1, 2, 3, 4, 5])
+                      })`)
+  ]);
+  testRunner.log(requestWillBeSent.params.request.postDataEntries);
+  testRunner.log(requestPaused.params.request.postDataEntries);
+  testRunner.completeTest();
+})
