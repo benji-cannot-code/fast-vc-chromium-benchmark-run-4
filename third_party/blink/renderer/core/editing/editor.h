08cc7fd48e703b660bf586462864231843c3406f
Abhijeet Kandalkar <abhijeet@igalia.com>
2020-07-21 09:31:07 +0000

    Merge blink::WritingDirection to mojom::TextDirection
    In this CL, WritingDirection {kNatural, kLeftToRight, kRightToLeft} is
    replaced by TextDirection {UNKNOWN_DIRECTION, RIGHT_TO_LEFT,
    LEFT_TO_RIGHT} and dependant files are ported to use |TextDirection|
    enum.
    
    Bug: 1088377
    Change-Id: Iac185f2d34eddd78127c3f59b7dc90fe380b5f34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2306127
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#790314}

--- third_party/blink/renderer/core/editing/editor.h
+++ third_party/blink/renderer/core/editing/editor.h
@@ -60,7 +60,6 @@ enum class InsertMode { kSimple, kSmart };
 enum class DragSourceType { kHTMLSource, kPlainTextSource };
 enum class EditorParagraphSeparator { kIsDiv, kIsP };
 enum class EditorCommandSource { kMenuOrKeyBinding, kDOM };
-enum class WritingDirection;
 
 class CORE_EXPORT Editor final : public GarbageCollected<Editor> {
  public:
@@ -134,7 +133,7 @@ class CORE_EXPORT Editor final : public GarbageCollected<Editor> {
   // Supposed to be used as |const UndoStack&|.
   UndoStack& GetUndoStack() const { return *undo_stack_; }
 
-  void SetBaseWritingDirection(WritingDirection);
+  void SetBaseWritingDirection(mojo_base::mojom::blink::TextDirection);
 
   // smartInsertDeleteEnabled and selectTrailingWhitespaceEnabled are
   // mutually exclusive, meaning that enabling one will disable the other.
