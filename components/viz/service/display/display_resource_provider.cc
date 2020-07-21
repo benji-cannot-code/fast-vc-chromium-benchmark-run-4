4d3108b7957361cd68fe92e70cc956a5db91d585
Sunny Sachanandani <sunnyps@chromium.org>
2020-07-21 22:46:04 +0000

    viz: Do not apply white level scaling for RGBA fp16 HDR video
    Since SDR white level was baked into the display color space, HDR video
    YUV quads with PQ color space had white level adjusted to skip white
    level scaling.  However, that workaround didn't apply to RGBA HDR video
    texture quads with scRGB linear color space.
    
    This CL applies the white level adjustment to that case.  Texture quads
    containing video are marked so that the white level adjustment applies
    only to video and not webgl or canvas.  The default white level for
    scRGB linear is set to 80 nits so that it matches the SRGB white level.
    
    Bug: 1084689
    Change-Id: I59c50bb2fe0f702c5ea48ed5d2b71b69572f0f8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2296006
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790596}

--- components/viz/service/display/display_resource_provider.cc
+++ components/viz/service/display/display_resource_provider.cc
@@ -981,7 +981,7 @@ DisplayResourceProvider::LockSetForExternalUse::~LockSetForExternalUse() {
 ExternalUseClient::ImageContext*
 DisplayResourceProvider::LockSetForExternalUse::LockResource(
     ResourceId id,
-    bool is_video_plane) {
+    bool use_skia_color_conversion) {
   auto it = resource_provider_->resources_.find(id);
   DCHECK(it != resource_provider_->resources_.end());
 
@@ -994,9 +994,10 @@ DisplayResourceProvider::LockSetForExternalUse::LockResource(
 
     if (!resource.image_context) {
       sk_sp<SkColorSpace> image_color_space;
-      // Video color conversion is handled externally in SkiaRenderer using a
-      // special color filter.
-      if (!is_video_plane)
+      // Video (YUV with PQ or half float RGBA with linear HDR) color conversion
+      // is handled externally in SkiaRenderer using a special color filter, and
+      // |use_skia_color_conversion| is false in that case.
+      if (use_skia_color_conversion)
         image_color_space = resource.transferable.color_space.ToSkColorSpace();
       resource.image_context =
           resource_provider_->external_use_client_->CreateImageContext(
