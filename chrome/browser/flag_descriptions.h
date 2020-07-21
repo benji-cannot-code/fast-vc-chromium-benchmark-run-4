14e3751e31cea04ed4b5d0004808fec307c3ed20
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-07-21 21:10:19 +0000

    [CrOS settings] Add phone hub feature flag
    Bug: 1106937
    Change-Id: Idd4931433c8421187510df98b200b93e51da508c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2309757
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790548}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2317,6 +2317,9 @@ extern const char kMeteredShowToggleDescription[];
 extern const char kOsSettingsPolymer3Name[];
 extern const char kOsSettingsPolymer3Description[];
 
+extern const char kPhoneHubName[];
+extern const char kPhoneHubDescription[];
+
 extern const char kPrintServerUiName[];
 extern const char kPrintServerUiDescription[];
 
