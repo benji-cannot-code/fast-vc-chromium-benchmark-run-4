cc8500375d58f8a658ea3fac09127c24d72f87fe
Alice Wang <aliceywang@chromium.org>
2020-07-21 16:08:52 +0000

    [Signin][Android] Adjust naming style in account picker bottom sheet
    This CL renames some account picker bottom sheet tests to make all
    test names consistent and put the if branch in the account picker
    view binder in to the nest to make the view binding if branches
    more consistent.
    
    Bug: 1106442
    Change-Id: I33b84a7bca0513ce443b63c3bcd2895031de003b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2308418
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790413}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/AccountPickerBottomSheetTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/AccountPickerBottomSheetTest.java
@@ -167,7 +167,7 @@ public class AccountPickerBottomSheetTest {
 
     @Test
     @MediumTest
-    public void testAccountDisappearedInCollapsedSheet() {
+    public void testAccountDisappearedOnCollapsedSheet() {
         buildAndShowCollapsedBottomSheet();
         mAccountManagerTestRule.removeAccountAndWaitForSeeding(PROFILE_DATA1.getAccountName());
         mAccountManagerTestRule.removeAccountAndWaitForSeeding(PROFILE_DATA2.getAccountName());
@@ -177,7 +177,7 @@ public class AccountPickerBottomSheetTest {
 
     @Test
     @MediumTest
-    public void testAccountDisappearedInExpandedSheet() {
+    public void testAccountDisappearedOnExpandedSheet() {
         buildAndShowExpandedBottomSheet();
         mAccountManagerTestRule.removeAccountAndWaitForSeeding(PROFILE_DATA1.getAccountName());
         mAccountManagerTestRule.removeAccountAndWaitForSeeding(PROFILE_DATA2.getAccountName());
@@ -187,7 +187,7 @@ public class AccountPickerBottomSheetTest {
 
     @Test
     @MediumTest
-    public void testAccountReappearedInCollapsedSheet() {
+    public void testAccountReappearedOnCollapsedSheet() {
         mAccountManagerTestRule.removeAccountAndWaitForSeeding(PROFILE_DATA1.getAccountName());
         mAccountManagerTestRule.removeAccountAndWaitForSeeding(PROFILE_DATA2.getAccountName());
         buildAndShowCollapsedBottomSheet();
@@ -200,7 +200,7 @@ public class AccountPickerBottomSheetTest {
 
     @Test
     @MediumTest
-    public void testOtherAccountsChangeInCollapsedSheet() {
+    public void testOtherAccountsChangeOnCollapsedSheet() {
         buildAndShowCollapsedBottomSheet();
         checkCollapsedAccountList(PROFILE_DATA1);
         mAccountManagerTestRule.removeAccountAndWaitForSeeding(PROFILE_DATA2.getAccountName());
@@ -210,7 +210,7 @@ public class AccountPickerBottomSheetTest {
 
     @Test
     @MediumTest
-    public void testSelectedAccountChangeInCollapsedSheet() {
+    public void testSelectedAccountChangeOnCollapsedSheet() {
         buildAndShowCollapsedBottomSheet();
         mAccountManagerTestRule.removeAccountAndWaitForSeeding(PROFILE_DATA1.getAccountName());
         InstrumentationRegistry.getInstrumentation().waitForIdleSync();
@@ -219,7 +219,7 @@ public class AccountPickerBottomSheetTest {
 
     @Test
     @MediumTest
-    public void testProfileDataUpdateInExpandedSheet() {
+    public void testProfileDataUpdateOnExpandedSheet() {
         buildAndShowExpandedBottomSheet();
         String newFullName = "New Full Name1";
         String newGivenName = "New Given Name1";
