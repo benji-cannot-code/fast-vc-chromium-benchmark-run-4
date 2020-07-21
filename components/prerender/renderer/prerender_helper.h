e0d62741b73d4a14997e38eea3b31b8a3a76e35b
Mugdha Lakhani <nator@chromium.org>
2020-07-21 14:46:48 +0000

    [WebLayer] Componentize PrerenderHelper.
    This is so WebLayer can use it to support NoStatePrefetch.
    
    Bug: 1096088
    Change-Id: Iedae9f5248d37da9aacd9c739719c9889c1949bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236482
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790380}

--- components/prerender/renderer/prerender_helper.h
+++ components/prerender/renderer/prerender_helper.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_RENDERER_PRERENDER_PRERENDER_HELPER_H_
-#define CHROME_RENDERER_PRERENDER_PRERENDER_HELPER_H_
+#ifndef COMPONENTS_PRERENDER_RENDERER_PRERENDER_HELPER_H_
+#define COMPONENTS_PRERENDER_RENDERER_PRERENDER_HELPER_H_
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
@@ -79,4 +79,4 @@ class PrerenderHelper
 
 }  // namespace prerender
 
-#endif  // CHROME_RENDERER_PRERENDER_PRERENDER_HELPER_H_
+#endif  // COMPONENTS_PRERENDER_RENDERER_PRERENDER_HELPER_H_
