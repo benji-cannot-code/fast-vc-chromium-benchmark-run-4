ac8ec13baf63aeb093b79c6cb8e989b107ad9ecc
Sophie Chang <sophiechang@chromium.org>
2020-07-21 02:21:50 +0000

    Make sure we still have adequate opt guide prediction logging for preconnect even if prefetch is on
    This will allow us to continue to evaluate preconnect accuracy even if
    the prefetch experiment is on.
    
    Change-Id: I1a0d0d2f10203413fc31b6241b0c9b8bbd49af28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2308856
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790220}

--- chrome/browser/predictors/loading_stats_collector_unittest.cc
+++ chrome/browser/predictors/loading_stats_collector_unittest.cc
@@ -309,7 +309,7 @@ TEST_F(LoadingStatsCollectorTest,
       entry,
       ukm::builders::LoadingPredictor::
           kOptimizationGuidePredictionCorrectlyPredictedOriginsName,
-      2);
+      1);
   ukm_recorder_->ExpectEntryMetric(
       entry,
       ukm::builders::LoadingPredictor::
