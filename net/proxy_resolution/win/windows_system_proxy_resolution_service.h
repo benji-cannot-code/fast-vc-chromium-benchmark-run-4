42e8e3e47090e7a87d785c887cb4e219720781d3
Nicolas Arciniega <niarci@microsoft.com>
2020-07-21 19:32:44 +0000

    Implement the WindowsSystemProxyResolutionService/Request
    This change creates a functional proxy resolver for Windows that relies
    on WinHttp APIs. This is not in its final state, though. This CL
    represents the simplest, least-work way to implement this proxy
    resolution service. Work still to come includes:
    - Complete NetLogs
    - Better error reporting from WinHttp APIs
    - Proxy Delegate
    - Proxy retry info
    - Assurance that the proxy configs we're receiving are from the system
    
    The WindowsSystemProxyResolutionService is the object that external
    callers use to resolve a proxy. These callers can keep track of the
    status of this proxy resolution via a ProxyResolutionRequest,
    implemented here as a WindowsSystemProxyResolutionRequest. The request
    object is mainly intended to kick off a specific proxy resolution and
    report a result via a caller-provided callback. Both of these objects
    deal with a WindowsSystemProxyResolver.
    
    The WindowsSystemProxyResolver is a new class that does all the required
    interfacing with WinHttp APIs. It is a reference counted object created
    by the WindowsSystemProxyResolutionService. Once created, it is used for
    the lifetime of the WindowsSystemProxyResolutionService. Throughout that
    time, the Resolver indirectly keeps a WinHttp session handle open. Each
    WindowsSystemProxyResolutionRequest is given a reference to the new
    resolver. When the Request object attempts to resolve a proxy for a
    given URL, it'll call into the Resolver object. Under the hood, the
    Resolver object creates a handle for the proxy resolution and runs the
    async call to WinHttp. At any time, the Request object can choose to
    ignore a pending result from the Resolver (ex: shutdown). The Resolver
    object must be reference counted because async WinHttp calls call back
    directly into the Resolver object, so it needs to stay alive as long as
    we're waiting for an async response from WinHttp.
    
    The only other new object is the WinHttpAPIWrapper, which is just a thin
    wrapper over WinHttp APIs which we use to enable easier testing and
    to simplify some interactions with WinHttp. A WinHttpAPIWrapper is owned
    by a WindowsSystemProxyResolver. The Resolver object is the only object
    that should interact with the Wrapper object in any meaningful way.
    Internally, the WinHttpAPIWrapper keeps track of the opened WinHttp
    session handle that we're using for the lifetime of the Resolver object.
    
    This change includes tests for the WindowsSystemProxyResolver layer (by
    mocking out the WinHttpAPIWrapper) and the
    WindowsSystemProxyResolutionService (by mocking out the
    WindowsSystemProxyResolver).
    
    Bug: 1032820
    Change-Id: Ic1c60033ff148e6e8f3708e37a2af366613fefac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2159128
    Commit-Queue: Nicolas Arciniega <niarci@microsoft.com>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790493}

--- net/proxy_resolution/win/windows_system_proxy_resolution_service.h
+++ net/proxy_resolution/win/windows_system_proxy_resolution_service.h
@@ -8,11 +8,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/proxy_resolution/proxy_resolution_service.h"
 
+#include <memory>
+#include <set>
 #include <string>
 
+#include "base/compiler_specific.h"
+#include "base/memory/ref_counted.h"
+#include "base/sequence_checker.h"
 #include "net/base/net_export.h"
 
 namespace net {
+
+class NetLog;
+class WindowsSystemProxyResolutionRequest;
+class WindowsSystemProxyResolver;
+
+using CreateWindowsSystemProxyResolverFunctionForTesting =
+    scoped_refptr<WindowsSystemProxyResolver> (*)();
 
 // This class decides which proxy server(s) to use for a particular URL request.
 // It does NOT support passing in fetched proxy configurations. Instead, it
@@ -21,7 +33,7 @@ namespace net {
 class NET_EXPORT WindowsSystemProxyResolutionService
     : public ProxyResolutionService {
  public:
-  WindowsSystemProxyResolutionService();
+  explicit WindowsSystemProxyResolutionService(NetLog* net_log);
 
   WindowsSystemProxyResolutionService(
       const WindowsSystemProxyResolutionService&) = delete;
@@ -53,9 +65,57 @@ class NET_EXPORT WindowsSystemProxyResolutionService
       ConfiguredProxyResolutionService** configured_proxy_resolution_service)
       override WARN_UNUSED_RESULT;
 
+  // Used in tests to provide a fake |windows_system_proxy_resolver_|.
+  void SetCreateWindowsSystemProxyResolverFunctionForTesting(
+      CreateWindowsSystemProxyResolverFunctionForTesting function);
+  void SetWindowsSystemProxyResolverForTesting(
+      scoped_refptr<WindowsSystemProxyResolver> windows_system_proxy_resolver);
+
  private:
+  friend class WindowsSystemProxyResolutionRequest;
+  friend class WindowsSystemProxyResolutionServiceTest;
+
+  typedef std::set<WindowsSystemProxyResolutionRequest*> PendingRequests;
+
+  bool ContainsPendingRequest(WindowsSystemProxyResolutionRequest* req)
+      WARN_UNUSED_RESULT;
+  void RemovePendingRequest(WindowsSystemProxyResolutionRequest* req);
+
+  // Lazily creates |windows_system_proxy_resolver_|.
+  bool CreateWindowsSystemProxyResolverIfNeeded() WARN_UNUSED_RESULT;
+
+  size_t PendingRequestSizeForTesting() const {
+    return pending_requests_.size();
+  }
+
+  // Called when proxy resolution has completed (either synchronously or
+  // asynchronously). Handles logging the result, and cleaning out
+  // bad entries from the results list.
+  int DidFinishResolvingProxy(const GURL& url,
+                              const std::string& method,
+                              ProxyInfo* result,
+                              int result_code,
+                              const NetLogWithSource& net_log);
+
+  CreateWindowsSystemProxyResolverFunctionForTesting
+      create_proxy_resolver_function_for_testing_;
+
   // Map of the known bad proxies and the information about the retry time.
   ProxyRetryInfoMap proxy_retry_info_;
+
+  // Set of pending/in-progress requests.
+  PendingRequests pending_requests_;
+
+  // This is the log for any generated events.
+  NetLog* net_log_;
+
+  // This object encapsulates all WinHttp logic in Chromium-friendly terms. It
+  // manages the lifetime of the WinHttp session (which is
+  // per-resolution-service). This will get handed off to individual resolution
+  // requests so that they can query/cancel proxy resolution as needed.
+  scoped_refptr<WindowsSystemProxyResolver> windows_system_proxy_resolver_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
 };
 
 }  // namespace net
