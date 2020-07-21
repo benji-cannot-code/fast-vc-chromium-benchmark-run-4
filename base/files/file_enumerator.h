f2c611eba47b2211ff7483c0d5f73c4daf7e455b
Ian Barkley-Yeung <iby@chromium.org>
2020-07-21 13:25:43 +0000

    base: Add unit tests for FileEnumerator::GetInfo
    Add unit tests for FileEnumerator::GetInfo(), since it didn't have any.
    
    Change-Id: Ia15f59c89349943b4f44ef7eeb7ba0d561ace45c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2297816
    Auto-Submit: Ian Barkley-Yeung <iby@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790352}

--- base/files/file_enumerator.h
+++ base/files/file_enumerator.h
@@ -58,6 +58,8 @@ class BASE_EXPORT FileEnumerator {
     FilePath GetName() const;
 
     int64_t GetSize() const;
+
+    // On POSIX systems, this is rounded down to the second.
     Time GetLastModifiedTime() const;
 
 #if defined(OS_WIN)
