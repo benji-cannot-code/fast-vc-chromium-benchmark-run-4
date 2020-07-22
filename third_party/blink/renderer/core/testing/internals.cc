0d54ca08e546e4fb212ab35aab099dc5daff56f2
Haiyang Pan <hypan@google.com>
2020-07-22 21:27:40 +0000

    Revert "Rewrite hr-timestamp/input-events.html to a browser test"
    This reverts commit 4f6da930b4cda797fe68d99482840c298ea6d22b.
    
    Reason for revert: InputEventBrowserTest.TouchStartEventTimeStamp is flaky in android pie CQ builder: https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyVwsSBUZsYWtlIkxjaHJvbWl1bUBjb250ZW50X2Jyb3dzZXJ0ZXN0c0BJbnB1dEV2ZW50QnJvd3NlclRlc3QuVG91Y2hTdGFydEV2ZW50VGltZVN0YW1wDA
    
    It is flaky in several waterfall builders as well, starting at
    https://ci.chromium.org/p/chromium/builders/ci/android-pie-x86-rel/1565
    https://ci.chromium.org/p/chromium/builders/ci/android-pie-arm64-rel/5952
    https://ci.chromium.org/p/chromium/builders/ci/android-asan/7107
    
    Original change's description:
    > Rewrite hr-timestamp/input-events.html to a browser test
    > 
    > hr-timestamp/input-events.htmlEvent timestamp tests that the received
    > events on the page should be equal to the timestamp provided by the
    > eventSender's recorded last event. But we should deprecate eventSender in
    > the future, and GpuBenchmarking sends events based on the current system
    > time, which cannot be equal to the received events' timestamp.
    > 
    > 
    > Bug: 1047176
    > Change-Id: I225dec07b1928ae254d61e8a8384ebfc7dab75e6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173421
    > Commit-Queue: Lan Wei <lanwei@chromium.org>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Reviewed-by: Majid Valipour <majidvp@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#790868}
    
    TBR=bokan@chromium.org,lanwei@chromium.org,majidvp@chromium.org
    
    Change-Id: Ifd5238546ed74824628a5d126b69a9fa37958ee3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1047176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2314806
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/master@{#791011}

--- third_party/blink/renderer/core/testing/internals.cc
+++ third_party/blink/renderer/core/testing/internals.cc
@@ -3318,11 +3318,6 @@ double Internals::monotonicTimeToZeroBasedDocumentTime(
       .InSecondsF();
 }
 
-int64_t Internals::zeroBasedDocumentTimeToMonotonicTime(double dom_event_time) {
-  return document_->Loader()->GetTiming().ZeroBasedDocumentTimeToMonotonicTime(
-      dom_event_time);
-}
-
 int64_t Internals::currentTimeTicks() {
   return base::TimeTicks::Now().since_origin().InMicroseconds();
 }
