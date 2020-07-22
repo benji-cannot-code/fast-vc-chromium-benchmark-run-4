490f583af57c246931442469eea71fdba76fee8a
Yuchen Liu <yucliu@chromium.org>
2020-07-22 19:56:56 +0000

    [Chromecast] Flag to use the same AudioManager as Chrome on Android
    Use AudioManagerAndroid, instead of CastAudioManagerAndroid on ATV.
    
    The flag should only be used for dev and testing.
    
    Bug: internal b/160753974
    Test: Turn on the flag and verify in the log
    Change-Id: Ie205d3f48d40d85b67272eeb143dbbc99d22bfe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311677
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790968}

--- chromecast/base/chromecast_switches.h
+++ chromecast/base/chromecast_switches.h
@@ -102,6 +102,8 @@ extern const char kForceUpdateRemoteUrl[];
 
 extern const char kSysInfoFilePath[];
 
+extern const char kEnableChromeAudioManagerAndroid[];
+
 }  // namespace switches
 
 namespace chromecast {
