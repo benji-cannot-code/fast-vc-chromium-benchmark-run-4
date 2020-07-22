222d2ee2a3c3c7910bd8f5091cb3ed67e93eace8
Antonio Sartori <antoniosartori@chromium.org>
2020-07-22 10:42:40 +0000

    Add missing directives to network CSP parser
    I recently (https://crrev.com/c/2220053) extended the network CSP
    parser adding support for almost all missing directives so that it can
    be used for CSP: Embedded Enforcement (https://crbug.com/1094909). I
    did not add a few directives which are ignored for CSPEE at the
    moment. However, with https://crrev.com/c/2279976 they will generate a
    warning, which we do not want.
    
    This CL adds support for the missing directives in the network CSP
    parser. Although the values of the directives are not being parsed,
    they are recognized as being valid directives ad will not generate any
    warning.
    
    Bug: 1094909
    Change-Id: I2c4b74ede0ca56b4b7582db9cc3eb809b36ce19d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300300
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790791}

--- content/public/browser/url_data_source.cc
+++ content/public/browser/url_data_source.cc
@@ -84,6 +84,7 @@ std::string URLDataSource::GetContentSecurityPolicy(
     case network::mojom::CSPDirectiveName::FrameAncestors:
       return "frame-ancestors 'none';";
     case network::mojom::CSPDirectiveName::BaseURI:
+    case network::mojom::CSPDirectiveName::BlockAllMixedContent:
     case network::mojom::CSPDirectiveName::ConnectSrc:
     case network::mojom::CSPDirectiveName::FrameSrc:
     case network::mojom::CSPDirectiveName::FontSrc:
@@ -93,6 +94,7 @@ std::string URLDataSource::GetContentSecurityPolicy(
     case network::mojom::CSPDirectiveName::MediaSrc:
     case network::mojom::CSPDirectiveName::PrefetchSrc:
     case network::mojom::CSPDirectiveName::ReportURI:
+    case network::mojom::CSPDirectiveName::RequireTrustedTypesFor:
     case network::mojom::CSPDirectiveName::Sandbox:
     case network::mojom::CSPDirectiveName::ScriptSrcAttr:
     case network::mojom::CSPDirectiveName::ScriptSrcElem:
@@ -101,6 +103,7 @@ std::string URLDataSource::GetContentSecurityPolicy(
     case network::mojom::CSPDirectiveName::StyleSrcElem:
     case network::mojom::CSPDirectiveName::UpgradeInsecureRequests:
     case network::mojom::CSPDirectiveName::TreatAsPublicAddress:
+    case network::mojom::CSPDirectiveName::TrustedTypes:
     case network::mojom::CSPDirectiveName::WorkerSrc:
     case network::mojom::CSPDirectiveName::ReportTo:
     case network::mojom::CSPDirectiveName::NavigateTo:
