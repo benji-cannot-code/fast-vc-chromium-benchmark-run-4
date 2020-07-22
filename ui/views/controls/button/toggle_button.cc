3661e2b5c69c4e7aab80f143d6ef11bfec43eb89
Sammie Quon <sammiequon@chromium.org>
2020-07-22 20:21:22 +0000

    views: Remove ink drop masks for views code.
    Makes use of the clipping api for ink drop highlights/ripples.
    
    ToggleButton: Ink-drop layers were always forwarded to the thumb view,
    so the InkDropMask was unnecessary before.
    
    FrameButton: InkDropMask relies a bit on the frame button internals, so
    create a custom highlight path generator.
    
    MediaButton: Used CircularMask which can be converted to a CircularPath.
    
    Test: manual
    Bug: 1056490
    Change-Id: I16e4b853158136eee2ffba1a2de2b495437896f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310875
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790984}

--- ui/views/controls/button/toggle_button.cc
+++ ui/views/controls/button/toggle_button.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/shadow_value.h"
 #include "ui/gfx/skia_paint_util.h"
 #include "ui/views/animation/ink_drop_impl.h"
-#include "ui/views/animation/ink_drop_mask.h"
 #include "ui/views/animation/ink_drop_ripple.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/highlight_path_generator.h"
@@ -329,10 +328,6 @@ std::unique_ptr<InkDrop> ToggleButton::CreateInkDrop() {
   ink_drop->SetAutoHighlightMode(
       InkDropImpl::AutoHighlightMode::HIDE_ON_RIPPLE);
   return std::move(ink_drop);
-}
-
-std::unique_ptr<InkDropMask> ToggleButton::CreateInkDropMask() const {
-  return nullptr;
 }
 
 std::unique_ptr<InkDropRipple> ToggleButton::CreateInkDropRipple() const {
--- ui/views/window/frame_caption_button.h
+++ ui/views/window/frame_caption_button.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/views/controls/button/button.h"
+#include "ui/views/controls/focus_ring.h"
 #include "ui/views/views_export.h"
 #include "ui/views/window/caption_button_types.h"
 
@@ -74,6 +75,7 @@ class VIEWS_EXPORT FrameCaptionButton : public views::Button {
   void set_ink_drop_corner_radius(int ink_drop_corner_radius) {
     ink_drop_corner_radius_ = ink_drop_corner_radius;
   }
+  int ink_drop_corner_radius() const { return ink_drop_corner_radius_; }
 
   CaptionButtonIcon icon() const { return icon_; }
 
@@ -88,6 +90,8 @@ class VIEWS_EXPORT FrameCaptionButton : public views::Button {
   void PaintButtonContents(gfx::Canvas* canvas) override;
 
  private:
+  class HighlightPathGenerator;
+
   // Determines what alpha to use for the icon based on animation and
   // active state.
   int GetAlphaForIcon(int base_alpha) const;
