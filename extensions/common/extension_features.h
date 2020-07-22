17dea686bdbad15fbcee88e25d012c16f3ca1a19
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-07-22 23:10:37 +0000

    [Extensions] Add support for matching data URLs in manifest parsing
    Add support for a "match_data_urls" field in content scripts specified
    in an extension's manifest. This CL adds the parsing logic and related
    unittests, behind a base::Feature. Future CLs will add support for
    the actual injection of scripts into these data URLs, as well as
    expanding the support to include dynamic script injections.
    
    Bug: 55084
    
    Change-Id: I288c66dfb901e9cb77d09562cf943a223adcb8d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292697
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791068}

--- extensions/common/extension_features.h
+++ extensions/common/extension_features.h
@@ -26,6 +26,8 @@ extern const base::Feature kForceWebRequestProxyForTest;
 
 extern const base::Feature kAllowWithholdingExtensionPermissionsOnInstall;
 
+extern const base::Feature kContentScriptsOnDataUrls;
+
 extern const base::Feature kReportKeepaliveUkm;
 
 }  // namespace extensions_features
--- extensions/common/manifest_handlers/content_scripts_handler.cc
+++ extensions/common/manifest_handlers/content_scripts_handler.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/url_constants.h"
 #include "extensions/common/error_utils.h"
 #include "extensions/common/extension.h"
+#include "extensions/common/extension_features.h"
 #include "extensions/common/extension_resource.h"
 #include "extensions/common/host_id.h"
 #include "extensions/common/manifest_constants.h"
@@ -125,6 +126,22 @@ std::unique_ptr<UserScript> LoadUserScriptFromDictionary(
       return nullptr;
     }
     result->set_match_about_blank(match_about_blank->GetBool());
+  }
+
+  // match data urls
+  if (base::FeatureList::IsEnabled(
+          extensions_features::kContentScriptsOnDataUrls)) {
+    const base::Value* match_data_urls =
+        content_script.FindKey(keys::kMatchDataUrls);
+    if (match_data_urls) {
+      if (!match_data_urls->is_bool()) {
+        *error = ErrorUtils::FormatErrorMessageUTF16(
+            errors::kInvalidMatchDataUrls,
+            base::NumberToString(definition_index));
+        return nullptr;
+      }
+      result->set_match_data_urls(match_data_urls->GetBool());
+    }
   }
 
   // matches (required)
