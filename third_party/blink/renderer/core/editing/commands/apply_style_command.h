807583eff1e7fbbca4f1cec49845635d8c84ca00
Kent Tamura <tkent@chromium.org>
2020-07-22 05:36:28 +0000

    Reland "Reduce the size of frame.h"
    This is a reland of ad354acebfba9a185bcf27758bb887d0b8541096
    
    Differences from the original CL:
     - Add text_direction.mojom-blink-forward.h to apply_style_command.h
      and style_command.h to fix build.
    
    Original change's description:
    > Reduce the size of frame.h
    >
    > This CL reduces the estimated pre-processed size of frame.h by 1.33MB.
    > This CL has no behavior changes.
    >
    > Bug: 242216
    > Change-Id: Ibf3551b924b070664120bb04fce9706b0415975a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2309370
    > Auto-Submit: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#790698}
    
    Tbr: tikuta@chromium.org
    Bug: 242216
    Change-Id: I8b18fd1715e58a7b0db1db5a750a10051091f289
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310222
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790720}

--- third_party/blink/renderer/core/editing/commands/apply_style_command.h
+++ third_party/blink/renderer/core/editing/commands/apply_style_command.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_EDITING_COMMANDS_APPLY_STYLE_COMMAND_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_EDITING_COMMANDS_APPLY_STYLE_COMMAND_H_
 
+#include "mojo/public/mojom/base/text_direction.mojom-blink-forward.h"
 #include "third_party/blink/renderer/core/editing/commands/composite_edit_command.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 
--- third_party/blink/renderer/core/events/pointer_event.h
+++ third_party/blink/renderer/core/events/pointer_event.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_EVENTS_POINTER_EVENT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_EVENTS_POINTER_EVENT_H_
 
+#include "third_party/blink/public/common/input/pointer_id.h"
 #include "third_party/blink/renderer/core/events/mouse_event.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
 
