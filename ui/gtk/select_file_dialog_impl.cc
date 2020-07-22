55f2b5380e3fd37183661d9b27b0cf1987a5bf9d
Marijn Kruisselbrink <mek@chromium.org>
2020-07-22 00:12:53 +0000

    Improve integration with kdialog.
    Modern kdialog versions support passing in filters in the format
    SelectFileDialog has them, avoiding the need for a lossy conversion
    via mime types. Detect the kdialog version and make use of this.
    
    A downside of this is that is now much more obvious that always passing
    1 as "selected filter index" to select file callers is wrong. But
    unfortunately kdialog does not provide a way to figure out which filter
    was selected when multiple filters could match the returned file. At
    least this isn't more broken than it was before.
    
    Bug: 1108060
    Change-Id: Ie39baf0dfb0664e366d7ae1574303a66ab8e2de9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311056
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790610}

--- ui/gtk/select_file_dialog_impl.cc
+++ ui/gtk/select_file_dialog_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/environment.h"
 #include "base/files/file_util.h"
 #include "base/nix/xdg_util.h"
+#include "base/no_destructor.h"
 #include "base/threading/thread_restrictions.h"
 
 namespace {
@@ -17,6 +18,10 @@ namespace {
 enum UseKdeFileDialogStatus { UNKNOWN, NO_KDE, YES_KDE };
 
 UseKdeFileDialogStatus use_kde_ = UNKNOWN;
+std::string& KDialogVersion() {
+  static base::NoDestructor<std::string> version;
+  return *version;
+}
 
 }  // namespace
 
@@ -43,7 +48,8 @@ ui::SelectFileDialog* SelectFileDialogImpl::Create(
       // Check to see if the user dislikes the KDE file dialog.
       if (!env->HasVar("NO_CHROME_KDE_FILE_DIALOG")) {
         // Check to see if the KDE dialog works.
-        if (SelectFileDialogImpl::CheckKDEDialogWorksOnUIThread()) {
+        if (SelectFileDialogImpl::CheckKDEDialogWorksOnUIThread(
+                KDialogVersion())) {
           use_kde_ = YES_KDE;
         }
       }
@@ -59,7 +65,7 @@ ui::SelectFileDialog* SelectFileDialogImpl::Create(
   base::nix::DesktopEnvironment desktop =
       base::nix::GetDesktopEnvironment(env.get());
   return SelectFileDialogImpl::NewSelectFileDialogImplKDE(
-      listener, std::move(policy), desktop);
+      listener, std::move(policy), desktop, KDialogVersion());
 }
 
 SelectFileDialogImpl::SelectFileDialogImpl(
