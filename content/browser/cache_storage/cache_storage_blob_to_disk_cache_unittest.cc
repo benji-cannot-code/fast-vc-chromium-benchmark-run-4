60e47ce3e59fa943b1c85284080682017a47e93d
Victor Costan <pwnall@chromium.org>
2020-07-22 18:13:48 +0000

    Cache Storage: clang-tidy fixes to tests.
    Change-Id: Ifaa1c3dfac42adfde6869b77595f70248c81b54a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311047
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790899}

--- content/browser/cache_storage/cache_storage_blob_to_disk_cache_unittest.cc
+++ content/browser/cache_storage/cache_storage_blob_to_disk_cache_unittest.cc
@@ -76,10 +76,8 @@ class CacheStorageBlobToDiskCacheTest : public testing::Test {
  protected:
   CacheStorageBlobToDiskCacheTest()
       : task_environment_(BrowserTaskEnvironment::IO_MAINLOOP),
-        browser_context_(new TestBrowserContext()),
-        data_(kTestData),
-        callback_success_(false),
-        callback_called_(false) {}
+        browser_context_(std::make_unique<TestBrowserContext>()),
+        data_(kTestData) {}
 
   void SetUp() override {
     InitBlobStorage();
@@ -194,8 +192,8 @@ class CacheStorageBlobToDiskCacheTest : public testing::Test {
       cache_storage_blob_to_disk_cache_;
   std::string data_;
 
-  bool callback_success_;
-  bool callback_called_;
+  bool callback_success_ = false;
+  bool callback_called_ = false;
 
  private:
   scoped_refptr<storage::QuotaManager> quota_manager_;
