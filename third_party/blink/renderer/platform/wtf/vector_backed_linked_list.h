716af392f4a870ef1a2065f63f00b772d57be6b8
Jose Dapena Paz <jdapena@igalia.com>
2020-07-22 07:57:33 +0000

    GCC: avoid WTF vector backed linked list template specialization
    GCC refuses to compile the recently added ConstructAndNotifyElementImpl
    Construct method, that gets a template specialization inside the struct
    namespace. This is not working in GCC.
    
    To avoid that, we avoid the template specialization using a new struct
    for when type is garbage collected, and select the struct with
    std::conditional.
    
    Bug: 819294
    Change-Id: Ibd88999c2667b1130c0132864ec692b1e0a8fbc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2304575
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#790746}

--- third_party/blink/renderer/platform/wtf/vector_backed_linked_list.h
+++ third_party/blink/renderer/platform/wtf/vector_backed_linked_list.h
@@ -108,7 +108,7 @@ class ConstructTraits<VectorBackedLinkedListNode<ValueType, Allocator>,
 
   template <typename... Args>
   static Node* ConstructAndNotifyElement(void* location, Args&&... args) {
-    Node* object = ConstructAndNotifyElementImpl<>::Construct(
+    Node* object = ConstructAndNotifyElementImpl::Construct(
         location, std::forward<Args>(args)...);
     NotifyNewElement(object);
     return object;
@@ -119,8 +119,7 @@ class ConstructTraits<VectorBackedLinkedListNode<ValueType, Allocator>,
   }
 
  private:
-  template <bool = Allocator::kIsGarbageCollected>
-  struct ConstructAndNotifyElementImpl {
+  struct ConstructAndNotifyElementImplNotGarbageCollected {
     template <typename... Args>
     static Node* Construct(void* location, Args&&... args) {
       return ConstructTraits<Node, Traits, Allocator>::Construct(
@@ -128,8 +127,7 @@ class ConstructTraits<VectorBackedLinkedListNode<ValueType, Allocator>,
     }
   };
 
-  template <>
-  struct ConstructAndNotifyElementImpl<true> {
+  struct ConstructAndNotifyElementImplGarbageCollected {
     static Node* Construct(void* location, Node&& element) {
       // ConstructAndNotifyElement updates an existing node which might
       // also be concurrently traced while we update it. The regular ctors
@@ -141,6 +139,11 @@ class ConstructTraits<VectorBackedLinkedListNode<ValueType, Allocator>,
       return reinterpret_cast<Node*>(location);
     }
   };
+
+  using ConstructAndNotifyElementImpl = typename std::conditional<
+      Allocator::kIsGarbageCollected,
+      ConstructAndNotifyElementImplGarbageCollected,
+      ConstructAndNotifyElementImplNotGarbageCollected>::type;
 };
 
 // VectorBackedLinkedList maintains a linked list through its contents such that
