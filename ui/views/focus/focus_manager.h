52a3196615e538c819a06944b8f0455aa2bf8204
Olesia Marukhno <olesiamarukhno@google.com>
2020-07-22 12:32:59 +0000

    Fix bubbles not being closed after processing accelerators on Linux
    Bug: 1098660
    
    Change-Id: I3036489ce3b939d9668c6fa374c465caf8f98a2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292290
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790808}

--- ui/views/focus/focus_manager.h
+++ ui/views/focus/focus_manager.h
@@ -337,9 +337,6 @@ class VIEWS_EXPORT FocusManager : public ViewObserver {
   bool RedirectAcceleratorToBubbleAnchorWidget(
       const ui::Accelerator& accelerator);
 
-  // Returns bubble's anchor widget.
-  Widget* GetBubbleAnchorWidget();
-
   // Whether arrow key traversal is enabled globally.
   static bool arrow_key_traversal_enabled_;
 
