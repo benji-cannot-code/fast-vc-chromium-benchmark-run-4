f9c7159ae1e40a60c3f25eaab362d24635fb7ae9
arthursonzogni <arthursonzogni@chromium.org>
2020-07-22 16:06:11 +0000

    [COOP] Access reporting [9/N] Filter by virtual BCG
    This patch fixes a TODO. A COOP access report is sent when two windows from
    different virtual browsing context groups tries to access each other.
    
    COOP access reporting:
    [1/N] https://chromium-review.googlesource.com/c/chromium/src/+/2264294
    [2/N] https://chromium-review.googlesource.com/c/chromium/src/+/2270185
    [3/N] https://chromium-review.googlesource.com/c/chromium/src/+/2270472
    [4/N] https://chromium-review.googlesource.com/c/chromium/src/+/2273120
    [5/N] https://chromium-review.googlesource.com/c/chromium/src/+/2309433
    [6/N] https://chromium-review.googlesource.com/c/chromium/src/+/2308715
    [7/N] https://chromium-review.googlesource.com/c/chromium/src/+/2309697
    [8/N] https://chromium-review.googlesource.com/c/chromium/src/+/2275889
    [9/N] this patch.
    
    Bug: 1090273
    Change-Id: I249183839ee8a5b93b6e8d35a033a99a4942b62b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310530
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Pâris Meuleman <pmeuleman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790867}

--- content/browser/net/cross_origin_opener_policy_reporter.cc
+++ content/browser/net/cross_origin_opener_policy_reporter.cc
@@ -84,6 +84,8 @@ std::vector<FrameTreeNode*> CollectOtherWindowForCoopAccess(
     FrameTreeNode* frame) {
   DCHECK(frame->IsMainFrame());
   SiteInstance* site_instance = frame->current_frame_host()->GetSiteInstance();
+  int virtual_browsing_context_group =
+      frame->current_frame_host()->virtual_browsing_context_group();
 
   std::vector<FrameTreeNode*> out;
   for (WebContentsImpl* wc : WebContentsImpl::GetAllWebContents()) {
@@ -93,13 +95,11 @@ std::vector<FrameTreeNode*> CollectOtherWindowForCoopAccess(
     if (!rfh->GetSiteInstance()->IsRelatedSiteInstance(site_instance))
       continue;
 
-    // TODO(arthursonzogni): Filter out window from the same virtual browsing
-    // context group.
-    FrameTreeNode* ftn = rfh->frame_tree_node();
-    if (ftn == frame)
+    // Filter out windows from the same virtual browsing context group.
+    if (rfh->virtual_browsing_context_group() == virtual_browsing_context_group)
       continue;
 
-    out.push_back(ftn);
+    out.push_back(rfh->frame_tree_node());
   }
   return out;
 }
