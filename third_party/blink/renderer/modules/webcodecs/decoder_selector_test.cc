a02d73921084a769230a0c20f1e34b22d78f4d80
Jose Dapena Paz <jdapena@igalia.com>
2020-07-22 07:22:25 +0000

    GCC: avoid naming a type with same name of an existing template in decoder selector test.
    In decoder selector test, we alias DecoderSelector to the instantiated template
    (i.e. DecoderSelector<media::DemuxerStream::AUDIO>). GCC does not allow that as
    the alias name clashes with template name.
    
    To solve it we rename the alias to be MockDecoderSelector.
    
    Bug: 819294
    Change-Id: I8ca0664df9b874fd5bd0c1db3397cbd17d14e084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310492
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#790738}

--- third_party/blink/renderer/modules/webcodecs/decoder_selector_test.cc
+++ third_party/blink/renderer/modules/webcodecs/decoder_selector_test.cc
@@ -42,7 +42,7 @@ class AudioDecoderSelectorTestParam {
   static constexpr media::DemuxerStream::Type kStreamType =
       media::DemuxerStream::AUDIO;
 
-  using DecoderSelector = DecoderSelector<media::DemuxerStream::AUDIO>;
+  using MockDecoderSelector = DecoderSelector<media::DemuxerStream::AUDIO>;
   using MockDecoder = media::MockAudioDecoder;
   using Output = media::AudioBuffer;
 
@@ -80,7 +80,7 @@ class VideoDecoderSelectorTestParam {
   static constexpr media::DemuxerStream::Type kStreamType =
       media::DemuxerStream::VIDEO;
 
-  using DecoderSelector = DecoderSelector<media::DemuxerStream::VIDEO>;
+  using MockDecoderSelector = DecoderSelector<media::DemuxerStream::VIDEO>;
   using MockDecoder = media::MockVideoDecoder;
   using Output = media::VideoFrame;
 
@@ -126,8 +126,8 @@ class WebCodecsDecoderSelectorTest : public ::testing::Test {
  public:
   // Convenience aliases.
   using Self = WebCodecsDecoderSelectorTest<TypeParam>;
-  using Decoder = typename TypeParam::DecoderSelector::Decoder;
-  using DecoderConfig = typename TypeParam::DecoderSelector::DecoderConfig;
+  using Decoder = typename TypeParam::MockDecoderSelector::Decoder;
+  using DecoderConfig = typename TypeParam::MockDecoderSelector::DecoderConfig;
   using MockDecoder = typename TypeParam::MockDecoder;
   using Output = typename TypeParam::Output;
 
