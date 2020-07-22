748036faef18f3981260eee1ad1712dfe8487170
Meredith Lane <meredithl@chromium.org>
2020-07-22 23:40:41 +0000

    Revert "Update core items for macOS Big Sur."
    This reverts commit 780e443e5650e8e8037009358720279c5e355b18.
    
    Reason for revert: Suspected Culprit for tree closure: https://ci.chromium.org/p/chromium/builders/ci/mac-arm64/2964?
    
    Original change's description:
    > Update core items for macOS Big Sur.
    > 
    > - Update the base::mac::IsOS*() functions. Introduce a parallel set
    >   of IsOS#() functions to match the IsOS10_#() functions. Update all
    >   tests to match, and update callers where appropriate.
    > - Turn on the CFAllocator OOM killer for Big Sur, as the
    >   internals of CFAllocator have not changed.
    > 
    > BUG=45650, 1101439
    > TEST=base_unittests
    > 
    > Change-Id: I57438eb025316e3cb899a0fa5aef35f4d297855f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2285930
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Commit-Queue: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#791051}
    
    TBR=avi@chromium.org,mark@chromium.org,kelvinjiang@chromium.org
    
    Change-Id: I86460b6921c2b141d69d44351c6c2702e85dff67
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 45650
    Bug: 1101439
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315357
    Reviewed-by: Meredith Lane <meredithl@chromium.org>
    Commit-Queue: Meredith Lane <meredithl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791072}

--- base/system/sys_info_mac.mm
+++ base/system/sys_info_mac.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/system/sys_info.h"
 
+#include <ApplicationServices/ApplicationServices.h>
+#include <CoreServices/CoreServices.h>
 #import <Foundation/Foundation.h>
 #include <mach/mach_host.h>
 #include <mach/mach_init.h>
