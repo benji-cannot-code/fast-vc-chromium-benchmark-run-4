68491c7fc8892ef75326da48deb36c707317af6c
Peter Kotwicz <pkotwicz@chromium.org>
2020-07-22 18:58:21 +0000

    Add console message to notify about prefer_related_applications support
    This CL adds a Developer Tools console message to notify that
    prefer_related_applications is only supported on Beta & Stable channels
    (Beta support was added recently).
    
    BUG=964314
    
    Change-Id: I9b21ad24760a2804ce1afc9fa2a28b0e26096e9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2308864
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790934}

--- chrome/android/java/src/org/chromium/chrome/browser/banners/AppBannerManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/banners/AppBannerManager.java
@@ -142,6 +142,12 @@ public class AppBannerManager {
         sIsSupported = state;
     }
 
+    /** Sets the app-banner-showing logic to ignore the Chrome channel. */
+    @VisibleForTesting
+    public static void ignoreChromeChannelForTesting() {
+        AppBannerManagerJni.get().ignoreChromeChannelForTesting();
+    }
+
     /** Returns whether the native AppBannerManager is working. */
     @VisibleForTesting
     public boolean isRunningForTesting() {
@@ -178,6 +184,7 @@ public class AppBannerManager {
         boolean onAppDetailsRetrieved(long nativeAppBannerManagerAndroid, AppBannerManager caller,
                 AppData data, String title, String packageName, String imageUrl);
         // Testing methods.
+        void ignoreChromeChannelForTesting();
         boolean isRunningForTesting(long nativeAppBannerManagerAndroid, AppBannerManager caller);
         void setDaysAfterDismissAndIgnoreToTrigger(int dismissDays, int ignoreDays);
         void setTimeDeltaForTesting(int days);
--- chrome/android/javatests/src/org/chromium/chrome/browser/banners/AppBannerManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/banners/AppBannerManagerTest.java
@@ -201,6 +201,7 @@ public class AppBannerManagerTest {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> { AppBannerManager.setAppDetailsDelegate(mDetailsDelegate); });
 
+        AppBannerManager.ignoreChromeChannelForTesting();
         AppBannerManager.setTotalEngagementForTesting(10);
         mTestServer = EmbeddedTestServer.createAndStartServer(InstrumentationRegistry.getContext());
         mUiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
