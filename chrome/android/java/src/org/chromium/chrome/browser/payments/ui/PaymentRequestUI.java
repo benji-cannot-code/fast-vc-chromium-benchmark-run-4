096a641251ec35707e4ffde426c2487a0b1be264
Side Yilmaz <sideyilmaz@chromium.org>
2020-07-22 12:38:00 +0000

    Update deprecated IdentityServicesProvider function in PaymentRequestUI.
    This CL replaces deprecated functions of IdentityServicesProvider with
    the newer service getters with profile parameter. |mProfile| instance
    variable stores the profile coming from constructor and this CL
    passes |mProfile| to IdentityServicesProvider class. Since
    IdentityServicesProvider return null for off-the-record profile,
    there is an additional null check for identity manager.
    
    Bug: 1041781, 1075562
    Change-Id: I9966e7177e81d2e0bb45b5b8c93e29e8715b7747
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2307235
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790809}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
@@ -31,6 +31,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.IntDef;
+import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.core.view.ViewCompat;
 
@@ -56,6 +57,7 @@ import org.chromium.components.payments.PaymentApp;
 import org.chromium.components.payments.PaymentFeatureList;
 import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.identitymanager.ConsentLevel;
+import org.chromium.components.signin.identitymanager.IdentityManager;
 import org.chromium.ui.text.NoUnderlineClickableSpan;
 import org.chromium.ui.text.SpanApplier;
 import org.chromium.ui.text.SpanApplier.SpanInfo;
@@ -288,6 +290,7 @@ public class PaymentRequestUI implements DimmingDialog.OnDismissListener, View.O
     private final Client mClient;
     private final boolean mShowDataSource;
     private final PaymentUisShowStateReconciler mPaymentUisShowStateReconciler;
+    private final Profile mProfile;
 
     /**
      * The top level container of this UI. When needing to call show() or hide(), use {@link
@@ -365,6 +368,7 @@ public class PaymentRequestUI implements DimmingDialog.OnDismissListener, View.O
         mShowDataSource = showDataSource;
         mAnimatorTranslation = mContext.getResources().getDimensionPixelSize(
                 R.dimen.payments_ui_translation);
+        mProfile = profile;
 
         mReadyToPayNotifierForTest = new NotifierForTest(new Runnable() {
             @Override
@@ -1123,12 +1127,10 @@ public class PaymentRequestUI implements DimmingDialog.OnDismissListener, View.O
         if (!mShowDataSource) {
             message = mContext.getString(R.string.payments_card_and_address_settings);
         } else {
-            CoreAccountInfo coreAccountInfo =
-                    IdentityServicesProvider.get().getIdentityManager().getPrimaryAccountInfo(
-                            ConsentLevel.SYNC);
-            if (coreAccountInfo != null) {
-                message = mContext.getString(R.string.payments_card_and_address_settings_signed_in,
-                        coreAccountInfo.getEmail());
+            String email = getEmail();
+            if (email != null) {
+                message = mContext.getString(
+                        R.string.payments_card_and_address_settings_signed_in, email);
             } else {
                 message =
                         mContext.getString(R.string.payments_card_and_address_settings_signed_out);
@@ -1151,6 +1153,17 @@ public class PaymentRequestUI implements DimmingDialog.OnDismissListener, View.O
                 R.dimen.editor_dialog_section_large_spacing);
         ViewCompat.setPaddingRelative(view, paddingSize, paddingSize, paddingSize, paddingSize);
         parent.addView(view);
+    }
+
+    /** @return The email of signed in user or null. */
+    @Nullable
+    private String getEmail() {
+        IdentityManager identityManager =
+                IdentityServicesProvider.get().getIdentityManager(mProfile);
+        if (identityManager == null) return null;
+        CoreAccountInfo info = identityManager.getPrimaryAccountInfo(ConsentLevel.SYNC);
+        if (info == null) return null;
+        return info.getEmail();
     }
 
     private Callback<SectionInformation> createUpdateSectionCallback(@DataType final int type) {
