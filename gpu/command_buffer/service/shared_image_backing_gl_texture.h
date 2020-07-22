112e42a6b8562eb80d9daea2544b4c4e3314acbf
Christopher Cameron <ccameron@chromium.org>
2020-07-22 19:10:39 +0000

    SharedImageBackingFactoryGLTexture: Split sources
    TBR=geofflang
    
    Bug: 1092155
    Change-Id: Iea8f93286198faa695893684f650710d35318a7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2313044
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790939}

--- gpu/command_buffer/service/shared_image_backing_gl_texture.h
+++ gpu/command_buffer/service/shared_image_backing_gl_texture.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_COMMAND_BUFFER_SERVICE_SHARED_IMAGE_BACKING_GL_TEXTURE_H_
 #define GPU_COMMAND_BUFFER_SERVICE_SHARED_IMAGE_BACKING_GL_TEXTURE_H_
 
-#include "gpu/command_buffer/service/shared_image_backing.h"
+#include "gpu/command_buffer/service/shared_image_backing_gl_common.h"
 
 namespace gpu {
 
