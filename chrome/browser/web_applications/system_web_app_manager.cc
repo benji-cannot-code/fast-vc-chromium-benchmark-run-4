d126871c5bea5d8f97ed6cdf3037f7f9f649aa86
Trent Apted <tapted@chromium.org>
2020-07-22 04:09:59 +0000

    Move sample_system_web_app deps out of official builds.
    Since r790679 their presence triggers a gn assert failure on official bots.
    
    Tbr: dominicschulz@google.com
    Bug: 1108151
    Change-Id: I55cb7dd479f7e9e83fce590811f707d43c0bbdbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310018
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790707}

--- chrome/browser/web_applications/system_web_app_manager.cc
+++ chrome/browser/web_applications/system_web_app_manager.cc
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chromeos/policy/system_features_disable_list_policy_handler.h"
 #include "chrome/browser/chromeos/web_applications/default_web_app_ids.h"
-#include "chrome/browser/chromeos/web_applications/sample_system_web_app_info.h"
 #include "chrome/browser/chromeos/web_applications/terminal_source.h"
 #include "chromeos/components/help_app_ui/url_constants.h"
 #include "chromeos/components/media_app_ui/url_constants.h"
@@ -55,6 +54,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/strings/grit/chromeos_strings.h"
 #include "components/policy/core/common/policy_pref_names.h"
 #include "extensions/common/constants.h"
+
+#if !defined(OFFICIAL_BUILD)
+#include "chrome/browser/chromeos/web_applications/sample_system_web_app_info.h"
+#endif  // !defined(OFFICIAL_BUILD)
+
 #endif  // defined(OS_CHROMEOS)
 
 namespace web_app {
