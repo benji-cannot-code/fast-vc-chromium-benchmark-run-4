c50689ce5e2d5a076c7acbb9fa411299387031aa
Victor Costan <pwnall@chromium.org>
2020-07-22 18:20:20 +0000

    IndexedDB: clang-tidy fixes to tests.
    Change-Id: I9fc513f40e02ff304bad940bf413543dc594dfce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311267
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790910}

--- content/browser/indexed_db/indexed_db_unittest.cc
+++ content/browser/indexed_db/indexed_db_unittest.cc
@@ -50,7 +50,7 @@ void CreateAndBindTransactionPlaceholder(
 
 class LevelDBLock {
  public:
-  LevelDBLock() : env_(nullptr), lock_(nullptr) {}
+  LevelDBLock() = default;
   LevelDBLock(leveldb::Env* env, leveldb::FileLock* lock)
       : env_(env), lock_(lock) {}
   ~LevelDBLock() {
@@ -59,8 +59,8 @@ class LevelDBLock {
   }
 
  private:
-  leveldb::Env* env_;
-  leveldb::FileLock* lock_;
+  leveldb::Env* env_ = nullptr;
+  leveldb::FileLock* lock_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(LevelDBLock);
 };
@@ -229,7 +229,7 @@ class ForceCloseDBCallbacks : public IndexedDBCallbacks {
   IndexedDBConnection* connection() { return connection_.get(); }
 
  protected:
-  ~ForceCloseDBCallbacks() override {}
+  ~ForceCloseDBCallbacks() override = default;
 
  private:
   scoped_refptr<IndexedDBContextImpl> idb_context_;
