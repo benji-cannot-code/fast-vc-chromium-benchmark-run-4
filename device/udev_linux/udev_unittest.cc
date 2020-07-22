a446cc6a401cabdb3e1bdf813b841a399f27cd49
Reilly Grant <reillyg@chromium.org>
2020-07-22 18:28:14 +0000

    serial: Fix USB product ID enumeration and add tests
    This change fixes an error introduced in r774216 when the ID_MODEL_ID
    property name was accidentally changed to ID_PRODUCT_ID. To prevent
    mistakes like this from happening in the future this change uses the
    FakeUdevLoader infrastructure (with some improvements) to implement unit
    tests for this platform specific code.
    
    Bug: 1108115
    Change-Id: I2dd42e282cc47f1af42ca3b482c4cba5a782d33b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311418
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#790917}

--- device/udev_linux/udev_unittest.cc
+++ device/udev_linux/udev_unittest.cc
@@ -29,7 +29,10 @@ TEST(UdevTest, Loader) {
 
 TEST(UdevTest, GetPropertyWithNone) {
   testing::FakeUdevLoader fake_udev;
-  udev_device* device = fake_udev.AddFakeDevice("Foo", "/device/foo", {}, {});
+  udev_device* device =
+      fake_udev.AddFakeDevice(/*name=*/"Foo", /*syspath=*/"/device/foo",
+                              /*subsystem=*/"", /*sysattrs=*/{},
+                              /*properties=*/{});
 
   const std::string attr_value = UdevDeviceGetPropertyValue(device, "prop");
   EXPECT_TRUE(attr_value.empty());
@@ -39,8 +42,9 @@ TEST(UdevTest, GetSysPropSimple) {
   testing::FakeUdevLoader fake_udev;
   std::map<std::string, std::string> props;
   props.emplace("prop", "prop value");
-  udev_device* device =
-      fake_udev.AddFakeDevice("Foo", "/device/foo", {}, std::move(props));
+  udev_device* device = fake_udev.AddFakeDevice(
+      /*name=*/"Foo", /*syspath=*/"/device/foo",
+      /*subsystem=*/"", /*sysattrs=*/{}, std::move(props));
 
   std::string attr_value = UdevDeviceGetPropertyValue(device, "prop");
   EXPECT_EQ("prop value", attr_value);
@@ -51,7 +55,9 @@ TEST(UdevTest, GetSysPropSimple) {
 
 TEST(UdevTest, GetSysAttrNoAttrs) {
   testing::FakeUdevLoader fake_udev;
-  udev_device* device = fake_udev.AddFakeDevice("Foo", "/device/foo", {}, {});
+  udev_device* device = fake_udev.AddFakeDevice(
+      /*name=*/"Foo", /*syspath=*/"/device/foo",
+      /*subsystem=*/"", /*sysattrs=*/{}, /*properties=*/{});
 
   const std::string attr_value = UdevDeviceGetSysattrValue(device, "attr");
   EXPECT_TRUE(attr_value.empty());
@@ -61,8 +67,9 @@ TEST(UdevTest, GetSysAttrSimple) {
   testing::FakeUdevLoader fake_udev;
   std::map<std::string, std::string> attrs;
   attrs.emplace("attr", "attr value");
-  udev_device* device =
-      fake_udev.AddFakeDevice("Foo", "/device/foo", std::move(attrs), {});
+  udev_device* device = fake_udev.AddFakeDevice(
+      /*name=*/"Foo", /*syspath=*/"/device/foo",
+      /*subsystem=*/"", std::move(attrs), /*properties=*/{});
 
   std::string attr_value = UdevDeviceGetSysattrValue(device, "attr");
   EXPECT_EQ("attr value", attr_value);
@@ -74,9 +81,12 @@ TEST(UdevTest, GetSysAttrSimple) {
 TEST(UdevTest, GetParent) {
   testing::FakeUdevLoader fake_udev;
   std::map<std::string, std::string> attrs;
-  udev_device* parent = fake_udev.AddFakeDevice("Foo", "/device/foo", {}, {});
-  udev_device* device =
-      fake_udev.AddFakeDevice("Foo", "/device/foo/bar", {}, {});
+  udev_device* parent = fake_udev.AddFakeDevice(
+      /*name=*/"Foo", /*syspath=*/"/device/foo",
+      /*subsystem=*/"", /*sysattrs=*/{}, /*properties=*/{});
+  udev_device* device = fake_udev.AddFakeDevice(
+      /*name=*/"Foo", /*syspath=*/"/device/foo/bar",
+      /*subsystem=*/"", /*sysattrs=*/{}, /*properties=*/{});
 
   EXPECT_EQ(parent, udev_device_get_parent(device));
   EXPECT_EQ(nullptr, udev_device_get_parent(parent));
@@ -86,9 +96,13 @@ TEST(UdevTest, GetSysAttrRecursiveOneLevel) {
   testing::FakeUdevLoader fake_udev;
   std::map<std::string, std::string> attrs;
   attrs.emplace("attr", "attr value");
-  fake_udev.AddFakeDevice("Foo", "/device/foo", std::move(attrs), {});
+  fake_udev.AddFakeDevice(/*name=*/"Foo", /*syspath=*/"/device/foo",
+                          /*subsystem=*/"", std::move(attrs),
+                          /*properties=*/{});
   udev_device* device =
-      fake_udev.AddFakeDevice("Foo", "/device/foo/bar", {}, {});
+      fake_udev.AddFakeDevice(/*name=*/"Foo", /*syspath=*/"/device/foo/bar",
+                              /*subsystem=*/"",
+                              /*sysattrs=*/{}, /*properties=*/{});
 
   // Don't find the attr on the current device.
   std::string attr_value = UdevDeviceGetSysattrValue(device, "attr");
--- services/device/serial/serial_device_enumerator_linux.h
+++ services/device/serial/serial_device_enumerator_linux.h
@@ -20,7 +20,10 @@ namespace device {
 class SerialDeviceEnumeratorLinux : public SerialDeviceEnumerator,
                                     public UdevWatcher::Observer {
  public:
-  SerialDeviceEnumeratorLinux();
+  static std::unique_ptr<SerialDeviceEnumeratorLinux> Create();
+
+  explicit SerialDeviceEnumeratorLinux(
+      const base::FilePath& tty_driver_info_path);
   ~SerialDeviceEnumeratorLinux() override;
 
   // UdevWatcher::Observer
@@ -32,6 +35,7 @@ class SerialDeviceEnumeratorLinux : public SerialDeviceEnumerator,
   void CreatePort(ScopedUdevDevicePtr device, const std::string& syspath);
 
   std::unique_ptr<UdevWatcher> watcher_;
+  const base::FilePath tty_driver_info_path_;
   std::map<std::string, base::UnguessableToken> paths_;
 
   DISALLOW_COPY_AND_ASSIGN(SerialDeviceEnumeratorLinux);
