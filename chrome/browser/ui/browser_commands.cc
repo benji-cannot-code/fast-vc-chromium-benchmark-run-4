61fd46a8dad7b99150dcf8f520fcea4bd3be471f
Scott Violet <sky@chromium.org>
2020-07-22 23:11:31 +0000

    chrome: moves SaveFaviconEvenIfInIncognito() into chrome
    This function is only used in chrome, and should be moved there.
    
    BUG=1076463
    TEST=covered by tests
    
    Change-Id: I5916d68e5761635839e6ce1db680d45116ea0331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311520
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791069}

--- chrome/browser/ui/browser_commands.cc
+++ chrome/browser/ui/browser_commands.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/devtools/devtools_window.h"
 #include "chrome/browser/dom_distiller/tab_utils.h"
+#include "chrome/browser/favicon/favicon_utils.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/media/router/media_router_dialog_controller.h"  // nogncheck
 #include "chrome/browser/media/router/media_router_feature.h"
@@ -972,8 +973,7 @@ void BookmarkCurrentTab(Browser* browser) {
       web_contents->GetBrowserContext()->IsOffTheRecord()) {
     // If we're incognito the favicon may not have been saved. Save it now
     // so that bookmarks have an icon for the page.
-    favicon::ContentFaviconDriver::FromWebContents(web_contents)
-        ->SaveFaviconEvenIfInIncognito();
+    favicon::SaveFaviconEvenIfInIncognito(web_contents);
   }
   bool was_bookmarked_by_user = bookmarks::IsBookmarkedByUser(model, url);
   bookmarks::AddIfNotBookmarked(model, url, title);
