414f9ad2be35fa9c2a9ffe16020ead2dd2b093bb
Tanmoy Mollik <triploblastic@google.com>
2020-07-23 15:54:47 +0000

    [Android] Move ClearDataProgressDialog class to a separate file
    This class will be used by ManageSyncSettings as well during sign out
    when MOBILE_IDENTITY_CONSISTENCEY is on.
    
    Bug: 1092410
    Change-Id: Iefbdac89c38599411d400db32046e658c1cf51b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315022
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791244}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/settings/ClearDataProgressDialog.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.sync.settings;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+/**
+ * This class is used by {@link AccountManagementFragment} to show the clear data progress dialog
+ * during sign out.
+ */
+public class ClearDataProgressDialog extends DialogFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Don't allow the dialog to be recreated by Android, since it wouldn't ever be
+        // dismissed after recreation.
+        if (savedInstanceState != null) dismiss();
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        setCancelable(false);
+        ProgressDialog dialog = new ProgressDialog(getActivity());
+        dialog.setTitle(getString(org.chromium.chrome.R.string.wiping_profile_data_title));
+        dialog.setMessage(getString(org.chromium.chrome.R.string.wiping_profile_data_message));
+        dialog.setIndeterminate(true);
+        return dialog;
+    }
+}
