066831283fb0d632c080365d62c89007a921b0d5
Jose Dapena Paz <jdapena@igalia.com>
2020-07-23 17:54:41 +0000

    GCC: to be able to pass through cross thread copier webRTC mocks they cannot be abstract
    MockWebRtcVideoTrack and MockWebRtcVideoTrackSource are abstract because they do
    not implement the rtc::RefCounted interface. To fix hte problem, solution is
    inheriting from rtc::RefCountedObject.
    
    Bug: 819294
    Change-Id: Ia0975711cff5755f0608e801a5cb60d175bd9b42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310414
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#791288}

--- third_party/blink/renderer/modules/peerconnection/mock_peer_connection_dependency_factory.h
+++ third_party/blink/renderer/modules/peerconnection/mock_peer_connection_dependency_factory.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "third_party/blink/renderer/modules/peerconnection/peer_connection_dependency_factory.h"
 #include "third_party/webrtc/api/media_stream_interface.h"
+#include "third_party/webrtc/rtc_base/ref_counted_object.h"
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -66,7 +67,8 @@ class MockWebRtcAudioTrack : public webrtc::AudioTrackInterface {
   ObserverSet observers_;
 };
 
-class MockWebRtcVideoTrackSource : public webrtc::VideoTrackSourceInterface {
+class MockWebRtcVideoTrackSource
+    : public rtc::RefCountedObject<webrtc::VideoTrackSourceInterface> {
  public:
   static scoped_refptr<MockWebRtcVideoTrackSource> Create(
       bool supports_encoded_output);
@@ -92,7 +94,8 @@ class MockWebRtcVideoTrackSource : public webrtc::VideoTrackSourceInterface {
   bool supports_encoded_output_;
 };
 
-class MockWebRtcVideoTrack : public webrtc::VideoTrackInterface {
+class MockWebRtcVideoTrack
+    : public rtc::RefCountedObject<webrtc::VideoTrackInterface> {
  public:
   static scoped_refptr<MockWebRtcVideoTrack> Create(
       const std::string& id,
