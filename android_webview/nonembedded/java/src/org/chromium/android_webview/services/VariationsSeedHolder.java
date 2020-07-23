ebcc2ff771324af7cc687212da5be0f0ad77b554
Nate Fischer <ntfschr@chromium.org>
2020-07-23 15:10:07 +0000

    AW: fix lint issues in WebView services
    No change to logic. This exposes some @VisibleForTesting methods which
    WebView services need to access.
    
    Bug: 1107950
    Test: autoninja -C out/Default monochrome_public_apk
    Change-Id: I8f6d3c6d0b091db79c3b531e87fd7b642a06399e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2313836
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791240}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/services/VariationsSeedHolder.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/services/VariationsSeedHolder.java
@@ -27,7 +27,6 @@ import java.io.FileOutputStream;
  * concurrent access to the seed by serializing all operations onto mSeedThread.
  * VariationsSeedHolder is not meant to be used outside the variations service.
  */
-@VisibleForTesting
 public class VariationsSeedHolder {
     private static final String TAG = "VariationsSeedHolder";
 
@@ -137,12 +136,10 @@ public class VariationsSeedHolder {
         return sInstance;
     }
 
-    @VisibleForTesting
     public void writeSeedIfNewer(ParcelFileDescriptor destination, long date) {
         mSeedHandler.post(new SeedWriter(destination, date));
     }
 
-    @VisibleForTesting
     public void updateSeed(SeedInfo newSeed, Runnable onFinished) {
         mSeedHandler.post(new SeedUpdater(newSeed, onFinished));
     }
