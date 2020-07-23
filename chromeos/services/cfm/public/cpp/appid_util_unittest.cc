057004cf5a30e89f7db58c7a31e7a88cee0499ba
Kyle Williams <kdgwill@google.com>
2020-07-23 05:42:46 +0000

    cfm: Add App ID for intended Packaged App
    Creates a helper function to determine if the value passed by
    extensions::Extension::id(), which returns extensions::ExtensionsID,
    is a valid app id for a Packaged Application supported by the
    Chromebox For Meeting platform.
    
    BUG=chromium:1105567
    TEST='autoninja -C out/Default chromeos:chromeos_unittests &&
    ./out/Default/chromeos_unittests --dbus-stub --single-process-tests
    --gtest_filter=CfmAppIdUtilTest.*'
    
    Change-Id: I84e85563119afcb564407f88c214aa649ad79626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2304334
    Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791148}

---
+++ chromeos/services/cfm/public/cpp/appid_util_unittest.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/cfm/public/cpp/appid_util.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+namespace cfm {
+namespace {
+
+using CfmAppIdUtilTest = testing::Test;
+
+TEST_F(CfmAppIdUtilTest, AppIdIsTrue) {
+  std::string app_id = "hkamnlhnogggfddmjomgbdokdkgfelgg";
+  ASSERT_TRUE(IsChromeboxForMeetingsAppId(app_id));
+}
+
+TEST_F(CfmAppIdUtilTest, AppIdIsFalse) {
+  std::string app_id = "FAKE_APP_ID";
+  ASSERT_FALSE(IsChromeboxForMeetingsAppId(app_id));
+}
+
+}  // namespace
+}  // namespace cfm
+}  // namespace chromeos
