87abe32e14b08420285c2b082304d6d1d3b50e34
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-07-23 05:35:43 +0000

    Import wpt@0cc1eb61d81b71834c47960d5b6cc51494f92696
    Using wpt-import in Chromium 75d163e933ef9d7332a154333111e55ddbc56720.
    With Chromium commits locally applied on WPT:
    a02f2df5db "Implement Storage Access commands for TestDriver"
    9c2ef27118 "[COOP report] Add support for report-only"
    48a79144f1 "[NativeFS] Keep directory iterator alive while it has pending promises."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    rego@igalia.com:
      external/wpt/css/css-grid
    
    NOAUTOREVERT=true
    TBR=robertma@google.com
    
    No-Export: true
    Change-Id: Idae60f308ac32f1a1fcbac995af9584037672c32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315299
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#791147}

---
+++ third_party/blink/web_tests/external/wpt/webxr/dom-overlay/idlharness.https.window.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// https://immersive-web.github.io/dom-overlays/
+
+idl_test(
+  ['dom-overlays'],
+  ['webxr', 'html', 'dom'],
+  async idl_array => {
+    idl_array.add_objects({
+      // TODO: Add object instances
+    });
+  }
+);
