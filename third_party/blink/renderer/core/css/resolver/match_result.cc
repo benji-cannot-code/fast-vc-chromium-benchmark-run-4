85e8495edb10bab56e18177bd4b0831e04c4955e
Anders Hartvoll Ruud <andruud@chromium.org>
2020-07-23 07:47:10 +0000

    Increment MatchResult::current_tree_order_ by one for each tree scope
    This is effectively what we're doing already, since the size of the
    author_range_ends_ vector just increased by one (and the size of that
    vector never decreases).
    
    Setting the current_tree_order_ when finishing user rules is not
    necessary. I don't know why we did this. Possibly it's a remnant of
    a previous solution.
    
    Overall, what matters is that current_tree_order_ is monotonically
    increasing for each call to FinishAddingAuthorRulesForTreeScope.
    
    This prepares the ua/user/author ranges for removal in a subsequent CL.
    
    Change-Id: I99947e5bfdf727ba772d296ecab65b424625dd91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310495
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791167}

--- third_party/blink/renderer/core/css/resolver/match_result.cc
+++ third_party/blink/renderer/core/css/resolver/match_result.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <type_traits>
 
+#include "base/numerics/clamped_math.h"
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
 #include "third_party/blink/renderer/core/css/style_rule.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
@@ -80,11 +81,11 @@ void MatchResult::FinishAddingUserRules() {
       user_range_ends_.back() == matched_properties_.size())
     return;
   user_range_ends_.push_back(matched_properties_.size());
-  current_tree_order_ = clampTo<uint16_t>(user_range_ends_.size());
 }
 
 void MatchResult::FinishAddingAuthorRulesForTreeScope() {
   DCHECK_EQ(current_origin_, CascadeOrigin::kAuthor);
+  current_tree_order_ = base::ClampAdd(current_tree_order_, 1);
   // Don't add empty ranges.
   if (author_range_ends_.IsEmpty() && user_range_ends_.IsEmpty() &&
       ua_range_end_ == matched_properties_.size())
@@ -96,7 +97,6 @@ void MatchResult::FinishAddingAuthorRulesForTreeScope() {
       author_range_ends_.back() == matched_properties_.size())
     return;
   author_range_ends_.push_back(matched_properties_.size());
-  current_tree_order_ = clampTo<uint16_t>(author_range_ends_.size());
 }
 
 MatchedExpansionsRange MatchResult::Expansions(const Document& document,
