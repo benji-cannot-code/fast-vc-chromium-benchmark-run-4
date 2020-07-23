a447f04ef9e611d9d28114ac65d01c133e69454a
Marc Treib <treib@chromium.org>
2020-07-23 12:31:35 +0000

    Delete passwords account store on startup if the feature is disabled
    Bug: 1079297
    Change-Id: I1cb15bd3e3b9cdafcc46bb8954f731fa8b35242b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300439
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791208}

--- chrome/browser/password_manager/account_storage/account_password_store_factory.cc
+++ chrome/browser/password_manager/account_storage/account_password_store_factory.cc
@@ -123,6 +123,13 @@ scoped_refptr<PasswordStore> AccountPasswordStoreFactory::GetForProfile(
     ServiceAccessType access_type) {
   if (!base::FeatureList::IsEnabled(
           password_manager::features::kEnablePasswordsAccountStorage)) {
+    if (!GetInstance()->account_store_deleted_.contains(profile)) {
+      // TODO(crbug.com/1108738): Remove this logic once
+      // kEnablePasswordsAccountStorage is launched.
+      GetInstance()->account_store_deleted_.insert(profile);
+      password_manager::DeleteLoginDatabaseForAccountStorageFiles(
+          profile->GetPath());
+    }
     return nullptr;
   }
   // |profile| gets always redirected to a non-Incognito profile below, so
@@ -148,7 +155,10 @@ AccountPasswordStoreFactory::AccountPasswordStoreFactory()
   DependsOn(WebDataServiceFactory::GetInstance());
 }
 
-AccountPasswordStoreFactory::~AccountPasswordStoreFactory() = default;
+AccountPasswordStoreFactory::~AccountPasswordStoreFactory() {
+  // All entries should have been removed in BrowserContextShutdown().
+  DCHECK(account_store_deleted_.empty());
+}
 
 scoped_refptr<RefcountedKeyedService>
 AccountPasswordStoreFactory::BuildServiceInstanceFor(
@@ -198,4 +208,10 @@ content::BrowserContext* AccountPasswordStoreFactory::GetBrowserContextToUse(
 
 bool AccountPasswordStoreFactory::ServiceIsNULLWhileTesting() const {
   return true;
+}
+
+void AccountPasswordStoreFactory::BrowserContextShutdown(
+    content::BrowserContext* context) {
+  account_store_deleted_.erase(context);
+  RefcountedBrowserContextKeyedServiceFactory::BrowserContextShutdown(context);
 }
