822105c187f43b7a2503b7a372a8d9e05f3c9696
Tomek Ponitka <tommek@google.com>
2020-07-23 13:32:06 +0000

    Adding GPUQueue.writeTexture in WebGPU.
    Added a writeTexture function.
    
    Bug: dawn:483
    Change-Id: I2276553daa0c8c1491c7aebfcc63b068bdc0e93b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303394
    Commit-Queue: Tomek Ponitka <tommek@google.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791219}

--- third_party/blink/renderer/modules/webgpu/dawn_conversions.h
+++ third_party/blink/renderer/modules/webgpu/dawn_conversions.h
@@ -24,6 +24,7 @@ class DoubleSequenceOrGPUColorDict;
 class GPUColorDict;
 class GPUProgrammableStageDescriptor;
 class GPUTextureCopyView;
+class GPUTextureDataLayout;
 class UnsignedLongEnforceRangeSequenceOrGPUExtent3DDict;
 class UnsignedLongEnforceRangeSequenceOrGPUOrigin3DDict;
 
@@ -49,6 +50,7 @@ WGPUOrigin3D AsDawnType(
     const UnsignedLongEnforceRangeSequenceOrGPUOrigin3DDict*);
 WGPUTextureCopyView AsDawnType(const GPUTextureCopyView* webgpu_view,
                                GPUDevice* device);
+WGPUTextureDataLayout AsDawnType(const GPUTextureDataLayout* webgpu_layout);
 using OwnedProgrammableStageDescriptor =
     std::tuple<WGPUProgrammableStageDescriptor, std::unique_ptr<char[]>>;
 OwnedProgrammableStageDescriptor AsDawnType(
