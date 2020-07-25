1b8ca4aac6b5e198d1c46b23a818d8ae14f8f846
Erik Chen <erikchen@chromium.org>
2020-07-25 06:58:20 +0000

    Fix lacros on chromeos compile.
    The function CreateGlobalMenuBar() is only used in a block guarded by
    defined(USE_DBUS_MENU), so the function definition should be guarded by
    the same flag.
    
    Change-Id: I4df2786b0e3dacd29bf4869575c5caf6bea01cc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316128
    Auto-Submit: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791369}

--- chrome/browser/ui/views/frame/browser_desktop_window_tree_host_linux.cc
+++ chrome/browser/ui/views/frame/browser_desktop_window_tree_host_linux.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+#if defined(USE_DBUS_MENU)
 bool CreateGlobalMenuBar() {
 #if defined(USE_OZONE)
   return ui::OzonePlatform::GetInstance()
@@ -29,6 +30,7 @@ bool CreateGlobalMenuBar() {
   return true;
 #endif
 }
+#endif
 
 }  // namespace
 
