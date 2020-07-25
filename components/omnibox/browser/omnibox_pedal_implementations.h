daa1a38e02b2b7a38f26be657229d119080e4018
Orin Jaworski <orinj@chromium.org>
2020-07-25 14:33:38 +0000

    [omnibox] Delete CHANGE_SEARCH_ENGINE and CHANGE_HOME_PAGE Pedals
    This CL fully deletes two Pedals that won't be launched, including
    all data and related unit tests. This is done to simplify upcoming
    changes to the full set of remaining Pedals (no sense maintaining
    code that isn't going to fly).
    
    Bug: 893183
    Change-Id: I1687774557efe416f17b1da852e0e6e0da6e88d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316950
    Auto-Submit: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791455}

--- components/omnibox/browser/omnibox_pedal_implementations.h
+++ components/omnibox/browser/omnibox_pedal_implementations.h
@@ -13,14 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/omnibox_pedal.h"
 #include "components/omnibox/browser/omnibox_pedal_concepts.h"
 
-class OmniboxPedalClearBrowsingData : public OmniboxPedal {
- public:
-  OmniboxPedalClearBrowsingData();
-#if (!defined(OS_ANDROID) || BUILDFLAG(ENABLE_VR)) && !defined(OS_IOS)
-  const gfx::VectorIcon& GetVectorIcon() const override;
-#endif
-};
-
 // Returns the full set of encapsulated OmniboxPedal implementations.
 std::unordered_map<OmniboxPedalId, std::unique_ptr<OmniboxPedal>>
 GetPedalImplementations();
