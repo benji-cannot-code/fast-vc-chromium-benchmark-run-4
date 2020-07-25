5f5eeff0aa636349b2a2bfc320e8925091821fd6
Yusuf Sengul <yusufsn@google.com>
2020-07-25 19:05:05 +0000

    Add common source files for gcpw extension
    This change adds abstraction for the Windows APIs that
    are used create and manage GCPW extension.
    
    Bug: 1101590
    Change-Id: I5fb88a6f73cc4c85f42728b9dee487d1067b8bc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278651
    Commit-Queue: Yusuf Sengul <yusufsn@google.com>
    Reviewed-by: Rakesh Soma <rakeshsoma@google.com>
    Cr-Commit-Position: refs/heads/master@{#791507}

---
+++ chrome/credential_provider/extension/extension_strings.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/credential_provider/extension/extension_strings.h"
+
+namespace credential_provider {
+namespace extension {
+
+const wchar_t kGCPWExtensionServiceName[] = L"GCPWExtension";
+
+const wchar_t kGCPWExtensionServiceDescription[] = L"GCPW extension service";
+
+}  // namespace extension
+}  // namespace credential_provider
