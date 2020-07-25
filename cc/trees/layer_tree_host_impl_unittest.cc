washezium/chromium

commit 917ce177502eb0e3fd5ca37aefc31ecafa35d719

Decouple AverageLagTracker and LatencyInfo.
Moving the event collection to LayerTreeHostImpl is an initial step
towards a new version of the AverageLag metrics using
PresentationFeedback times instead of GpuSwap times, which are available
through LayerTreeHostImpl.
Moving them from LatencyTracker is also interesting as it aggregates
events across all viz clients at the end of the GPU pipeline, which can
potentially mix up events from multiple sources.
Tracking events by viz client (through LayerTreeHostImpl) avoids it.

As AverageLagTracker should be dedicated to Telemetry, it is interesting
to remove all LatencyInfo preprocessing from it, as LatencyInfo stores
a lot of information that is irrelevant to AverageLagTracker.

This CL proposes to move the scroll events collection from LatencyTracker
to LayerTreeHostImpl. It also proposes an AverageLagTrackingManager,
an intermediary class for using AverageLagTracker that preprocesses
LatencyInfo objects so AverageLagTracker can be independent from it.

As the event collection is moved to LayerTreeHostImpl, the
AverageLagTrackingManager also serves to encapsulate any preprocessing
from LayerTreeHostImpl and AverageLagTrakcer.


Bug: 1079024, 989207
Change-Id: I2e73e244eee9565239a638f00dfbf19d5658eadb
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2189633
Commit-Queue: João Victor Almeida de Aguiar <joalmei@microsoft.com>
Reviewed-by: Steven Holte <holte@chromium.org>
Reviewed-by: Robert Flack <flackr@chromium.org>
Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
Reviewed-by: Xida Chen <xidachen@chromium.org>
Reviewed-by: Daniel Libby <dlibby@microsoft.com>
Cr-Commit-Position: refs/heads/master@{#785975}

cc/metrics/average_lag_tracking_manager.cc
@@ -1,0 +1,96 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cc/metrics/average_lag_tracking_manager.h"
+
+#include <algorithm>
+
+#include "components/viz/common/frame_timing_details.h"
+#include "components/viz/common/quads/compositor_frame_metadata.h"
+#include "ui/latency/latency_info.h"
+
+namespace cc {
+
+AverageLagTrackingManager::AverageLagTrackingManager() = default;
+
+AverageLagTrackingManager::~AverageLagTrackingManager() {
+  // The map must contain only frames that haven't been presented (i.e. did not
+  // get a presentation feedback yet). Thus, at a given point in time, more than
+  // a handful (actually around 2) of frames without feedback is unexpected.
+  DCHECK_LE(frame_token_to_info_.size(), 20u);
+}
+
+void AverageLagTrackingManager::CollectScrollEventsFromFrame(
+    uint32_t frame_token,
+    const std::vector<ui::LatencyInfo>& latency_infos) {
+  std::vector<AverageLagTracker::EventInfo> event_infos;
+
+  for (ui::LatencyInfo latency_info : latency_infos) {
+    if (latency_info.source_event_type() != ui::SourceEventType::TOUCH)
+      continue;
+
+    bool found_scroll_begin = latency_info.FindLatency(
+        ui::INPUT_EVENT_LATENCY_FIRST_SCROLL_UPDATE_ORIGINAL_COMPONENT,
+        nullptr);
+    bool found_scroll_update = latency_info.FindLatency(
+        ui::INPUT_EVENT_LATENCY_SCROLL_UPDATE_ORIGINAL_COMPONENT, nullptr);
+
+    if (!found_scroll_begin && !found_scroll_update)
+      continue;
+
+    base::TimeTicks event_timestamp;
+    bool found_event = latency_info.FindLatency(
+        ui::INPUT_EVENT_LATENCY_SCROLL_UPDATE_LAST_EVENT_COMPONENT,
+        &event_timestamp);
+    DCHECK(found_event);
+
+    AverageLagTracker::EventInfo event_info(
+        latency_info.trace_id(), latency_info.scroll_update_delta(),
+        latency_info.predicted_scroll_update_delta(), event_timestamp,
+        found_scroll_begin == true
+            ? AverageLagTracker::EventType::ScrollBegin
+            : AverageLagTracker::EventType::ScrollUpdate);
+
+    event_infos.push_back(event_info);
+  }
+
+  frame_token_to_info_.push_back(
+      std::make_pair(frame_token, std::move(event_infos)));
+}
+
+void AverageLagTrackingManager::DidPresentCompositorFrame(
+    uint32_t frame_token,
+    const viz::FrameTimingDetails& frame_details) {
+  // Erase all previous frames that haven't received a feedback and get the
+  // current |frame_token| list of events.
+  std::vector<AverageLagTracker::EventInfo> infos;
+  while (!frame_token_to_info_.empty() &&
+         !viz::FrameTokenGT(frame_token_to_info_.front().first, frame_token)) {
+    if (frame_token_to_info_.front().first == frame_token)
+      infos = std::move(frame_token_to_info_.front().second);
+
+    frame_token_to_info_.pop_front();
+  }
+
+  if (infos.size() == 0)
+    return;
+
+  if (!frame_details.presentation_feedback.failed()) {
+    DCHECK(!frame_details.swap_timings.is_null());
+
+    // Sorts data by trace_id because |infos| can be in non-ascending order
+    // (ascending order of trace_id/time is required by AverageLagTracker).
+    std::sort(infos.begin(), infos.end(),
+              [](const AverageLagTracker::EventInfo& a,
+                 const AverageLagTracker::EventInfo& b) {
+                return a.trace_id < b.trace_id;
+              });
+
+    for (AverageLagTracker::EventInfo info : infos) {
+      info.finish_timestamp = frame_details.swap_timings.swap_start;
+      lag_tracker_.AddScrollEventInFrame(info);
+    }
+  }
+}
+}  // namespace cc

cc/metrics/average_lag_tracking_manager.h
@@ -1,0 +1,62 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CC_METRICS_AVERAGE_LAG_TRACKING_MANAGER_H_
+#define CC_METRICS_AVERAGE_LAG_TRACKING_MANAGER_H_
+
+#include <utility>
+#include <vector>
+
+#include "base/containers/circular_deque.h"
+#include "cc/cc_export.h"
+#include "cc/metrics/average_lag_tracker.h"
+
+namespace ui {
+class LatencyInfo;
+}  // namespace ui
+
+namespace viz {
+struct FrameTimingDetails;
+}  // namespace viz
+
+namespace cc {
+
+// A helper to decouple the LatencyInfos and the AverageLagTracker
+class CC_EXPORT AverageLagTrackingManager {
+ public:
+  AverageLagTrackingManager();
+  ~AverageLagTrackingManager();
+
+  // Disallow copy and assign.
+  AverageLagTrackingManager(const AverageLagTrackingManager&) = delete;
+  AverageLagTrackingManager& operator=(AverageLagTrackingManager const&) =
+      delete;
+
+  // Adds all the eligible events in the collection |infos| to the |frame_token|
+  // wait list.
+  void CollectScrollEventsFromFrame(uint32_t frame_token,
+                                    const std::vector<ui::LatencyInfo>& infos);
+
+  // Sends all pending events in the |frame_token| list to the
+  // AverageLagTracker, given its |frame_details|.
+  void DidPresentCompositorFrame(uint32_t frame_token,
+                                 const viz::FrameTimingDetails& frame_details);
+
+ private:
+  // Adds an eligible event |info| to the |frame_token| wait list.
+  void CollectScrollEventFromFrame(uint32_t frame_token,
+                                   const ui::LatencyInfo& info);
+
+  AverageLagTracker lag_tracker_;
+
+  // List of events (vector) per frame (uint32_t |frame_token|) to submit to the
+  // lag trackers when DidPresentCompositorFrame is called for a |frame_token|.
+  base::circular_deque<
+      std::pair<uint32_t, std::vector<AverageLagTracker::EventInfo>>>
+      frame_token_to_info_;
+};
+
+}  // namespace cc
+
+#endif  // CC_METRICS_AVERAGE_LAG_TRACKING_MANAGER_H_

cc/trees/layer_tree_host_impl.cc
@@ -2080,6 +2080,21 @@ void LayerTreeHostImpl::DidPresentCompositorFrame(
     client_->NotifyThroughputTrackerResults(
         std::move(throughput_tracker_results));
   }
+
+  // Send all pending lag events waiting on the frame pointed by |frame_token|.
+  // It is posted as a task because LayerTreeHostImpl::DidPresentCompositorFrame
+  // is in the rendering critical path (it is called by AsyncLayerTreeFrameSink
+  // ::OnBeginFrame).
+  GetTaskRunner()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&LayerTreeHostImpl::LogAverageLagEvents,
+                     weak_factory_.GetWeakPtr(), frame_token, details));
+}
+
+void LayerTreeHostImpl::LogAverageLagEvents(
+    uint32_t frame_token,
+    const viz::FrameTimingDetails& details) {
+  lag_tracking_manager_.DidPresentCompositorFrame(frame_token, details);
 }
 
 void LayerTreeHostImpl::DidNotNeedBeginFrame() {
@@ -2364,6 +2379,10 @@ bool LayerTreeHostImpl::DrawLayers(FrameData* frame) {
 
   auto compositor_frame = GenerateCompositorFrame(frame);
   frame->frame_token = compositor_frame.metadata.frame_token;
+
+  // Collect |latency_info| information for tracking
+  lag_tracking_manager_.CollectScrollEventsFromFrame(
+      frame->frame_token, compositor_frame.metadata.latency_info);
   layer_tree_frame_sink_->SubmitCompositorFrame(
       std::move(compositor_frame),
       /*hit_test_data_changed=*/false, debug_state_.show_hit_test_borders);

cc/BUILD.gn
@@ -149,6 +149,10 @@ cc_component("cc") {
     "layers/video_layer_impl.h",
     "layers/viewport.cc",
     "layers/viewport.h",
+    "metrics/average_lag_tracker.cc",
+    "metrics/average_lag_tracker.h",
+    "metrics/average_lag_tracking_manager.cc",
+    "metrics/average_lag_tracking_manager.h",
     "metrics/begin_main_frame_metrics.cc",
     "metrics/begin_main_frame_metrics.h",
     "metrics/compositor_frame_reporter.cc",
@@ -660,6 +664,8 @@ cc_test("cc_unittests") {
     "layers/video_frame_provider_client_impl_unittest.cc",
     "layers/video_layer_impl_unittest.cc",
     "layers/viewport_unittest.cc",
+    "metrics/average_lag_tracker_unittest.cc",
+    "metrics/average_lag_tracking_manager_unittest.cc",
     "metrics/compositor_frame_reporter_unittest.cc",
     "metrics/compositor_frame_reporting_controller_unittest.cc",
     "metrics/compositor_timing_history_unittest.cc",

cc/metrics/average_lag_tracker.cc
@@ -1,0 +1,211 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cc/metrics/average_lag_tracker.h"
+
+#include <algorithm>
+
+#include "base/metrics/histogram_functions.h"
+
+namespace cc {
+
+AverageLagTracker::AverageLagTracker() = default;
+AverageLagTracker::~AverageLagTracker() = default;
+
+void AverageLagTracker::AddScrollEventInFrame(const EventInfo& event_info) {
+  if (event_info.event_type == EventType::ScrollBegin) {
+    AddScrollBeginInFrame(event_info);
+  } else if (!last_event_timestamp_.is_null()) {
+    AddScrollUpdateInFrame(event_info);
+  }
+
+  last_event_timestamp_ = event_info.event_timestamp;
+  last_event_accumulated_delta_ += event_info.event_scroll_delta;
+  last_rendered_accumulated_delta_ += event_info.predicted_scroll_delta;
+}
+
+void AverageLagTracker::AddScrollBeginInFrame(const EventInfo& event_info) {
+  DCHECK_EQ(event_info.event_type, EventType::ScrollBegin);
+
+  // Flush all unfinished frames.
+  while (!frame_lag_infos_.empty()) {
+    frame_lag_infos_.front().lag_area += LagForUnfinishedFrame(
+        frame_lag_infos_.front().rendered_accumulated_delta);
+    frame_lag_infos_.front().lag_area_no_prediction += LagForUnfinishedFrame(
+        frame_lag_infos_.front().rendered_accumulated_delta_no_prediction);
+
+    // Record UMA when it's the last item in queue.
+    CalculateAndReportAverageLagUma(frame_lag_infos_.size() == 1);
+  }
+  // |accumulated_lag_| should be cleared/reset.
+  DCHECK_EQ(accumulated_lag_, 0);
+
+  // Create ScrollBegin report, with report time equals to the frame
+  // timestamp.
+  LagAreaInFrame first_frame(event_info.finish_timestamp);
+  frame_lag_infos_.push_back(first_frame);
+
+  // Reset fields.
+  last_reported_time_ = event_info.event_timestamp;
+  last_finished_frame_time_ = event_info.event_timestamp;
+  last_event_accumulated_delta_ = 0;
+  last_rendered_accumulated_delta_ = 0;
+  is_begin_ = true;
+}
+
+void AverageLagTracker::AddScrollUpdateInFrame(const EventInfo& event_info) {
+  DCHECK_EQ(event_info.event_type, EventType::ScrollUpdate);
+
+  // Only accept events in nondecreasing order.
+  if ((event_info.event_timestamp - last_event_timestamp_).InMilliseconds() < 0)
+    return;
+
+  // Pop all frames where frame_time <= event_timestamp.
+  while (!frame_lag_infos_.empty() &&
+         frame_lag_infos_.front().frame_time <= event_info.event_timestamp) {
+    base::TimeTicks front_time =
+        std::max(last_event_timestamp_, last_finished_frame_time_);
+    base::TimeTicks back_time = frame_lag_infos_.front().frame_time;
+    frame_lag_infos_.front().lag_area +=
+        LagBetween(front_time, back_time, event_info.event_scroll_delta,
+                   event_info.event_timestamp,
+                   frame_lag_infos_.front().rendered_accumulated_delta);
+    frame_lag_infos_.front().lag_area_no_prediction += LagBetween(
+        front_time, back_time, event_info.event_scroll_delta,
+        event_info.event_timestamp,
+        frame_lag_infos_.front().rendered_accumulated_delta_no_prediction);
+
+    CalculateAndReportAverageLagUma();
+  }
+
+  // Initialize a new LagAreaInFrame when current_frame_time > frame_time.
+  if (frame_lag_infos_.empty() ||
+      event_info.finish_timestamp > frame_lag_infos_.back().frame_time) {
+    LagAreaInFrame new_frame(event_info.finish_timestamp,
+                             last_rendered_accumulated_delta_,
+                             last_event_accumulated_delta_);
+    frame_lag_infos_.push_back(new_frame);
+  }
+
+  // last_frame_time <= event_timestamp < frame_time
+  if (!frame_lag_infos_.empty()) {
+    // The front element in queue (if any) must satisfy frame_time >
+    // event_timestamp, otherwise it would be popped in the while loop.
+    DCHECK_LE(last_finished_frame_time_, event_info.event_timestamp);
+    DCHECK_LE(event_info.event_timestamp, frame_lag_infos_.front().frame_time);
+    base::TimeTicks front_time =
+        std::max(last_finished_frame_time_, last_event_timestamp_);
+    base::TimeTicks back_time = event_info.event_timestamp;
+
+    frame_lag_infos_.front().lag_area +=
+        LagBetween(front_time, back_time, event_info.event_scroll_delta,
+                   event_info.event_timestamp,
+                   frame_lag_infos_.front().rendered_accumulated_delta);
+
+    frame_lag_infos_.front().lag_area_no_prediction += LagBetween(
+        front_time, back_time, event_info.event_scroll_delta,
+        event_info.event_timestamp,
+        frame_lag_infos_.front().rendered_accumulated_delta_no_prediction);
+  }
+}
+
+float AverageLagTracker::LagBetween(base::TimeTicks front_time,
+                                    base::TimeTicks back_time,
+                                    const float scroll_delta,
+                                    base::TimeTicks event_timestamp,
+                                    float rendered_accumulated_delta) {
+  // In some tests, we use const event time. return 0 to avoid divided by 0.
+  if (event_timestamp == last_event_timestamp_)
+    return 0;
+
+  float front_delta =
+      (last_event_accumulated_delta_ +
+       (scroll_delta *
+        ((front_time - last_event_timestamp_).InMillisecondsF() /
+         (event_timestamp - last_event_timestamp_).InMillisecondsF()))) -
+      rendered_accumulated_delta;
+
+  float back_delta =
+      (last_event_accumulated_delta_ +
+       scroll_delta *
+           ((back_time - last_event_timestamp_).InMillisecondsF() /
+            (event_timestamp - last_event_timestamp_).InMillisecondsF())) -
+      rendered_accumulated_delta;
+
+  // Calculate the trapezoid area.
+  if (front_delta * back_delta >= 0) {
+    return 0.5f * std::abs(front_delta + back_delta) *
+           (back_time - front_time).InMillisecondsF();
+  }
+
+  // Corner case that rendered_accumulated_delta is in between of front_pos
+  // and back_pos.
+  return 0.5f *
+         std::abs((front_delta * front_delta + back_delta * back_delta) /
+                  (back_delta - front_delta)) *
+         (back_time - front_time).InMillisecondsF();
+}
+
+float AverageLagTracker::LagForUnfinishedFrame(
+    float rendered_accumulated_delta) {
+  base::TimeTicks last_time =
+      std::max(last_event_timestamp_, last_finished_frame_time_);
+  return std::abs(last_event_accumulated_delta_ - rendered_accumulated_delta) *
+         (frame_lag_infos_.front().frame_time - last_time).InMillisecondsF();
+}
+
+void AverageLagTracker::CalculateAndReportAverageLagUma(bool send_anyway) {
+  DCHECK(!frame_lag_infos_.empty());
+  const LagAreaInFrame& frame_lag = frame_lag_infos_.front();
+
+  DCHECK_GE(frame_lag.lag_area, 0.f);
+  DCHECK_GE(frame_lag.lag_area_no_prediction, 0.f);
+  accumulated_lag_ += frame_lag.lag_area;
+  accumulated_lag_no_prediction_ += frame_lag.lag_area_no_prediction;
+
+  if (is_begin_) {
+    DCHECK_EQ(accumulated_lag_, accumulated_lag_no_prediction_);
+  }
+
+  // |send_anyway| is true when we are flush all remaining frames on next
+  // |ScrollBegin|. Otherwise record UMA when it's ScrollBegin, or when
+  // reaching the 1 second gap.
+  if (send_anyway || is_begin_ ||
+      (frame_lag.frame_time - last_reported_time_).InSecondsF() >= 1.0f) {
+    std::string scroll_name = is_begin_ ? "ScrollBegin" : "ScrollUpdate";
+    const float time_delta =
+        (frame_lag.frame_time - last_reported_time_).InMillisecondsF();
+    const float scaled_lag = accumulated_lag_ / time_delta;
+    base::UmaHistogramCounts1000(
+        "Event.Latency." + scroll_name + ".Touch.AverageLag", scaled_lag);
+
+    const float prediction_effect =
+        (accumulated_lag_no_prediction_ - accumulated_lag_) / time_delta;
+    // Log positive and negative prediction effects. ScrollBegin currently
+    // doesn't take prediction into account so don't log for it.
+    // Positive effect means that the prediction reduced the perceived lag,
+    // where negative means prediction made lag worse (most likely due to
+    // misprediction).
+    if (!is_begin_) {
+      if (prediction_effect >= 0.f) {
+        base::UmaHistogramCounts1000(
+            "Event.Latency.ScrollUpdate.Touch.AverageLag.PredictionPositive",
+            prediction_effect);
+      } else {
+        base::UmaHistogramCounts1000(
+            "Event.Latency.ScrollUpdate.Touch.AverageLag.PredictionNegative",
+            -prediction_effect);
+      }
+    }
+    accumulated_lag_ = 0;
+    accumulated_lag_no_prediction_ = 0;
+    last_reported_time_ = frame_lag.frame_time;
+    is_begin_ = false;
+  }
+
+  last_finished_frame_time_ = frame_lag.frame_time;
+  frame_lag_infos_.pop_front();
+}
+
+}  // namespace cc

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit d125f44040be3065c6a16bb8e6a0abda4ef12a9f

Fix flaky bugs introduced by AverageLagTrackingManager
After commit crrev.com/917ce177502eb0e3fd5ca37aefc31ecafa35d719
introduced the AverageLagTrackingManager some cc_unittests started
failing.
These test tested scenarios where frames were Drawn but didn't receive
a PresentationFeedback on LayerTreeHostImpl. They were failing due to a
DCHECK at  AverageLagTrackingManager that checked the amount of pending
frames without feedback before destruction.

These frames never received a PresentationFeedback because
LayerTreeFrameSink was purposefully losing its context
(LayerTreeFrameSink::OnContextLost).
This prevents DisplayScheduler to post the tasks that will ultimately
call LayerTreeHostImpl::DidPresentCompositorFrame.

In this CL the list in AverageLagTrackingManager is cleaned whenever
LayerTreeFrameSink loses its context, as it is a sign that the current
pending frames will never receive a presentation feedback.
Also, frames that don't have relevant latency_info aren't added to the
list anymore as this was one of the reasons why the tests where flaky
too.

Bug: 1103289, 1104841, 1106020, 1104202
Change-Id: I9b74ac22adb6db6af7c8e727304dc2e0b02e8899
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303363
Reviewed-by: Xida Chen <xidachen@chromium.org>
Reviewed-by: Robert Flack <flackr@chromium.org>
Commit-Queue: João Victor Almeida de Aguiar <joalmei@microsoft.com>
Cr-Commit-Position: refs/heads/master@{#791413}

cc/metrics/average_lag_tracking_manager.cc
@@ -56,7 +56,8 @@ void AverageLagTrackingManager::CollectScrollEventsFromFrame(
     event_infos.push_back(event_info);
   }
 
-  frame_token_to_info_.push_back(std::make_pair(frame_token, event_infos));
+  if (event_infos.size() > 0)
+    frame_token_to_info_.push_back(std::make_pair(frame_token, event_infos));
 }
 
 void AverageLagTrackingManager::DidPresentCompositorFrame(
@@ -95,5 +96,9 @@ void AverageLagTrackingManager::DidPresentCompositorFrame(
       lag_tracker_presentation_.AddScrollEventInFrame(info);
     }
   }
+}
+
+void AverageLagTrackingManager::Clear() {
+  frame_token_to_info_.clear();
 }
 }  // namespace cc

cc/metrics/average_lag_tracking_manager.h
@@ -44,6 +44,10 @@ class CC_EXPORT AverageLagTrackingManager {
   void DidPresentCompositorFrame(uint32_t frame_token,
                                  const viz::FrameTimingDetails& frame_details);
 
+  // Clears the list of events |frame_token_to_info_| if the current frames wont
+  // receive a presentation feedback (e.g. LayerTreeFrameSink loses context)
+  void Clear();
+
  private:
   // TODO(https://crbug.com/1101005): Remove GpuSwap implementation after M86.
   // Tracker for the AverageLag metrics that uses the gpu swap begin timing as

cc/trees/layer_tree_host_impl.cc
@@ -3048,6 +3048,7 @@ void LayerTreeHostImpl::DidLoseLayerTreeFrameSink() {
     return;
   has_valid_layer_tree_frame_sink_ = false;
   client_->DidLoseLayerTreeFrameSinkOnImplThread();
+  lag_tracking_manager_.Clear();
 }
 
 bool LayerTreeHostImpl::ShouldPinTopControlsToContentTop() const {

cc/trees/layer_tree_host_impl_unittest.cc
@@ -13750,6 +13750,55 @@ TEST_P(ScrollUnifiedLayerTreeHostImplTest,
   host_impl_ = nullptr;
 }
 
+// Test if the AverageLagTrackingManager's pending frames list is cleared when
+// the LayerTreeFrameSink loses context. It is necessary since the frames won't
+// receive a presentation feedback if the context is lost, and the pending
+// frames will never be removed from the list otherwise.
+TEST_F(LayerTreeHostImplTest,
+       ClearTrackingManagerOnLayerTreeFrameSinkLoseContext) {
+  const gfx::Size content_size(1000, 10000);
+  const gfx::Size viewport_size(500, 500);
+  SetupViewportLayersOuterScrolls(viewport_size, content_size);
+  DrawFrame();
+
+  host_impl_->ScrollBegin(BeginState(gfx::Point(250, 250), gfx::Vector2dF(),
+                                     ui::ScrollInputType::kTouchscreen)
+                              .get(),
+                          ui::ScrollInputType::kTouchscreen);
+
+  // Draw 30 frames, each with 1 LatencyInfo object that will be added to the
+  // AverageLagTrackingManager.
+  for (int i = 0; i < 30; i++) {
+    // Makes a scroll update so the next frame is set to be processed
+    // (to force frame->has_no_damage = false).
+    host_impl_->ScrollUpdate(UpdateState(gfx::Point(), gfx::Vector2d(0, 10),
+                                         ui::ScrollInputType::kTouchscreen)
+                                 .get());
+
+    // Add a LatencyInfo object that will be accepted by
+    // AverageLagTrackingManager::CollectScrollEventsFromFrame.
+    ui::LatencyInfo latency_info;
+    latency_info.set_source_event_type(ui::SourceEventType::TOUCH);
+    latency_info.AddLatencyNumber(
+        ui::INPUT_EVENT_LATENCY_SCROLL_UPDATE_ORIGINAL_COMPONENT);
+    latency_info.AddLatencyNumberWithTimestamp(
+        ui::INPUT_EVENT_LATENCY_SCROLL_UPDATE_LAST_EVENT_COMPONENT,
+        base::TimeTicks::Now());
+    std::unique_ptr<SwapPromise> swap_promise(
+        new LatencyInfoSwapPromise(latency_info));
+    host_impl_->active_tree()->QueuePinnedSwapPromise(std::move(swap_promise));
+
+    DrawFrame();
+  }
+
+  // Make LayerTreeFrameSink lose context. It should clear
+  // |lag_tracking_manager_|.
+  host_impl_->DidLoseLayerTreeFrameSink();
+
+  // Finish the test. |lag_tracking_manager_| will check in its destructor if
+  // there is less than 20 frames in its pending frames list.
+}
+
 // Tests that the scheduled autoscroll task aborts if a 2nd mousedown occurs in
 // the same frame.
 TEST_F(LayerTreeHostImplTest, AutoscrollTaskAbort) {