2cb38843f6ff2c1301f869483f1ec98196cac0fb
Kalvin Lee <kdlee@chromium.org>
2020-07-25 21:53:26 +0000

    PpdProvider v3: prefer to post tasks
    This change modifies the PrinterConfigCache and FakePrinterConfigCache
    classes to post callback tasks rather than Run()ning them synchronously.
    
    Bug: chromium:888189
    Test: chromeos_unittests --gtest_filter='PrinterConfigCacheTest.*'
    Test: chromeos_unittests --gtest_filter='PpdMetadataManagerTest.*'
    Change-Id: I4a42c855eb49b5eb58f0831141ddfbeae373d676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303860
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791539}

--- chromeos/printing/fake_printer_config_cache.cc
+++ chromeos/printing/fake_printer_config_cache.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/strings/string_piece.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/clock.h"
 #include "base/time/time.h"
 #include "chromeos/printing/printer_config_cache.h"
@@ -38,15 +39,19 @@ void FakePrinterConfigCache::Fetch(const std::string& key,
                                    base::TimeDelta unused_expiration,
                                    PrinterConfigCache::FetchCallback cb) {
   if (contents_.contains(key)) {
-    std::move(cb).Run(PrinterConfigCache::FetchResult::Success(
-        key, contents_.at(key), base::Time()));
+    base::SequencedTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE, base::BindOnce(std::move(cb),
+                                  PrinterConfigCache::FetchResult::Success(
+                                      key, contents_.at(key), base::Time())));
     return;
   } else if (fetch_requests_to_ignore_.contains(key)) {
     // Caller has directed us, by way of DiscardFetchRequestFor(), to
     // _not_ respond to this Fetch().
     return;
   }
-  std::move(cb).Run(PrinterConfigCache::FetchResult::Failure(key));
+  base::SequencedTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(std::move(cb),
+                                PrinterConfigCache::FetchResult::Failure(key)));
 }
 
 void FakePrinterConfigCache::Drop(const std::string& key) {
--- chromeos/printing/printer_config_cache.cc
+++ chromeos/printing/printer_config_cache.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string_piece.h"
 #include "base/task/post_task.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/clock.h"
 #include "base/time/time.h"
 #include "net/base/load_flags.h"
@@ -97,8 +98,10 @@ class PrinterConfigCacheImpl : public PrinterConfigCache {
     if (finding != cache_.end()) {
       const Entry& entry = finding->second;
       if (entry.time_of_fetch + expiration > clock_->Now()) {
-        std::move(cb).Run(
-            FetchResult::Success(key, entry.contents, entry.time_of_fetch));
+        base::SequencedTaskRunnerHandle::Get()->PostTask(
+            FROM_HERE, base::BindOnce(std::move(cb), FetchResult::Success(
+                                                         key, entry.contents,
+                                                         entry.time_of_fetch)));
         return;
       }
     }
@@ -178,11 +181,15 @@ class PrinterConfigCacheImpl : public PrinterConfigCache {
       // (if extant) or retain no entry at all (if not).
       const Entry newly_inserted = Entry(*contents, clock_->Now());
       cache_.insert_or_assign(context->key, newly_inserted);
-      std::move(context->cb)
-          .Run(FetchResult::Success(context->key, newly_inserted.contents,
-                                    newly_inserted.time_of_fetch));
+      base::SequencedTaskRunnerHandle::Get()->PostTask(
+          FROM_HERE, base::BindOnce(std::move(context->cb),
+                                    FetchResult::Success(
+                                        context->key, newly_inserted.contents,
+                                        newly_inserted.time_of_fetch)));
     } else {
-      std::move(context->cb).Run(FetchResult::Failure(context->key));
+      base::SequencedTaskRunnerHandle::Get()->PostTask(
+          FROM_HERE, base::BindOnce(std::move(context->cb),
+                                    FetchResult::Failure(context->key)));
     }
 
     fetcher_.reset();
