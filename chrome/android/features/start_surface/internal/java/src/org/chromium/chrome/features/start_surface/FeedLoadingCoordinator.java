f8ac7ab190ce1c20aab82192febcdf59126aa810
spdonghao <spdonghao@chromium.org>
2020-07-25 16:01:27 +0000

    [Instant Start] Update Feed Placeholder layout.
    Update Feed placeholder layout for a lighter border and
    use the screen width to assign the width of placeholders.
    
    Bug: 1076139
    Change-Id: I6080cbf2362c701578004c091439be05c565420c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311424
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791471}

--- chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/FeedLoadingCoordinator.java
+++ chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/FeedLoadingCoordinator.java
@@ -9,7 +9,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.VisibleForTesting;
@@ -44,7 +43,5 @@ public class FeedLoadingCoordinator {
 
         StartSurfaceConfiguration.recordHistogram(FEEDS_LOADING_PLACEHOLDER_SHOWN_TIME_UMA,
                 layoutInflationCompleteMs - activityCreationTimeMs, true);
-
-        mFeedLoadingView.setVisibility(View.GONE);
     }
 }
