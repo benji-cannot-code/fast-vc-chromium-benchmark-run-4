111ab271eedc07fe3a961882ca68e0333f1c2140
Keren Zhu <kerenzhu@chromium.org>
2020-07-25 16:57:31 +0000

    views: Remove improper use of GridLayout from examples
    Replace GridLayout with BoxLayout and FlexLayout in cases where
    GridLayout is not used as a true grid, but only for alignment.
    
    This CL is part of the changes to apply documented best practice to
    examples.
    
    Bug: 897377
    Change-Id: I789da2ebaa083f149079c36134dc8a1f48dd093a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316455
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791482}

--- ui/views/examples/radio_button_example.cc
+++ ui/views/examples/radio_button_example.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/radio_button.h"
 #include "ui/views/examples/examples_window.h"
 #include "ui/views/examples/grit/views_examples_resources.h"
-#include "ui/views/layout/grid_layout.h"
+#include "ui/views/layout/box_layout.h"
 #include "ui/views/view.h"
 
 using l10n_util::GetStringUTF16;
@@ -40,25 +40,21 @@ RadioButtonExample::RadioButtonExample()
 RadioButtonExample::~RadioButtonExample() = default;
 
 void RadioButtonExample::CreateExampleView(View* container) {
-  GridLayout* layout =
-      container->SetLayoutManager(std::make_unique<views::GridLayout>());
-  ColumnSet* column_set = layout->AddColumnSet(0);
-  column_set->AddColumn(GridLayout::FILL, GridLayout::FILL, 1.0f,
-                        GridLayout::ColumnSize::kUsePreferred, 0, 0);
+  container->SetLayoutManager(
+      std::make_unique<views::BoxLayout>(BoxLayout::Orientation::kVertical));
+
   const int group = 1;
   for (size_t i = 0; i < 3; ++i) {
-    layout->StartRow(0, 0);
-    radio_buttons_.push_back(layout->AddView(std::make_unique<RadioButton>(
-        base::UTF8ToUTF16(base::StringPrintf("Radio %d in group %d",
-                                             static_cast<int>(i) + 1, group)),
-        group)));
+    radio_buttons_.push_back(
+        container->AddChildView(std::make_unique<RadioButton>(
+            base::UTF8ToUTF16(base::StringPrintf(
+                "Radio %d in group %d", static_cast<int>(i) + 1, group)),
+            group)));
   }
 
-  layout->StartRow(0, 0);
-  select_ = layout->AddView(std::make_unique<LabelButton>(
+  select_ = container->AddChildView(std::make_unique<LabelButton>(
       this, GetStringUTF16(IDS_RADIO_BUTTON_SELECT_BUTTON_LABEL)));
-  layout->StartRow(0, 0);
-  status_ = layout->AddView(std::make_unique<LabelButton>(
+  status_ = container->AddChildView(std::make_unique<LabelButton>(
       this, GetStringUTF16(IDS_RADIO_BUTTON_STATUS_LABEL)));
 }
 
