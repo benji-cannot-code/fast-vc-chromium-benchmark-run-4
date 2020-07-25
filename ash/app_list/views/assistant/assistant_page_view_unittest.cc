7e7db60c26e8643cd6b156a7c99dee287cd1dc97
Yue Li <updowndota@google.com>
2020-07-25 15:51:17 +0000

    Replace 'sanity check' keyword in Assistant files
    See go/inclusive-codebase.
    
    Bug: b/161801556
    Test: Local compile
    Change-Id: I6f489e910c4050dab71b2d2bc30fdfb6679fb6bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316450
    Auto-Submit: Yue Li <updowndota@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791469}

--- ash/app_list/views/assistant/assistant_page_view_unittest.cc
+++ ash/app_list/views/assistant/assistant_page_view_unittest.cc
@@ -175,7 +175,7 @@ class AssistantPageViewTest : public AssistantAshTestBase {
   gfx::Point GetPointInAppListOutsideAssistantUi() {
     gfx::Point result = GetPointOutside(page_view());
 
-    // Sanity check
+    // Validity check
     EXPECT_TRUE(app_list_view()->bounds().Contains(result));
     EXPECT_FALSE(page_view()->bounds().Contains(result));
 
@@ -373,7 +373,7 @@ TEST_F(AssistantPageViewTest, FocusShouldRemainInAssistantViewWhenPressingTab) {
         << "Focus advanced to view '" << focused_view->GetClassName()
         << "' which is not a part of the Assistant UI";
 
-    // Sanity check to ensure we do not loop forever
+    // Validity check to ensure we do not loop forever
     num_views++;
     ASSERT_LT(num_views, kMaxIterations);
   } while (focused_view != initial_focused_view);
@@ -403,7 +403,7 @@ TEST_F(AssistantPageViewTest,
   int num_iterations = 0;
   while (GetFocusedView() != onboarding_suggestions.at(0)) {
     PressKeyAndWait(ui::VKEY_TAB);
-    ASSERT_LE(++num_iterations, kMaxIterations);  // Sanity check.
+    ASSERT_LE(++num_iterations, kMaxIterations);  // Validity check.
   }
 
   // Verify we can cycle through them.
@@ -416,7 +416,7 @@ TEST_F(AssistantPageViewTest,
   num_iterations = 0;
   while (GetFocusedView() != first_focused_view) {
     PressKeyAndWait(ui::VKEY_TAB);
-    ASSERT_LE(++num_iterations, kMaxIterations);  // Sanity check.
+    ASSERT_LE(++num_iterations, kMaxIterations);  // Validity check.
   }
 }
 
