207b4a510185fbdab436b0ae95524d3363fb2387
Mounir Lamouri <mlamouri@chromium.org>
2020-07-25 18:34:30 +0000

    Picture-in-Picture: remove lint suppressions on Android.
    Bug: 1108415
    Change-Id: I6fe8888a8c70ca4217a9738fecd82b46d188c485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2314817
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Auto-Submit: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791501}

--- chrome/android/java/src/org/chromium/chrome/browser/media/PictureInPictureActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/PictureInPictureActivity.java
@@ -139,6 +139,7 @@ public class PictureInPictureActivity extends AsyncInitializationActivity {
     }
 
     @Override
+    @SuppressLint("NewAPI") // Picture-in-Picture API will not be enabled for oldver versions.
     public void onStart() {
         super.onStart();
 
