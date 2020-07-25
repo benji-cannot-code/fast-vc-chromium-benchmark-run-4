61d3ec324bbd56ac5c0fbf6f9426d95fc6033499
David Black <dmblack@google.com>
2020-07-25 23:45:56 +0000

    Rough in custom data support.
    After this change, clipboard history will support custom data.
    Follow up changes will make it look pretty.
    
    Bug: 1104352
    Change-Id: I018494a117f0d4d909500fd0be12e1ceb803ebb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315922
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791560}

--- ash/clipboard/clipboard_history.cc
+++ ash/clipboard/clipboard_history.cc
@@ -115,7 +115,6 @@ void ClipboardHistory::OnClipboardDataChanged() {
     } else if (type == ui::ClipboardFormatType::GetBitmapType().GetName()) {
       contains_bitmap = true;
     }
-    // TODO(newcomer): Handle custom data, which could be files.
   }
 
   if (contains_bitmap) {
@@ -126,6 +125,13 @@ void ClipboardHistory::OnClipboardDataChanged() {
                        std::move(new_data)));
     return;
   }
+
+  std::string custom_data;
+  const auto& custom_format = ui::ClipboardFormatType::GetWebCustomDataType();
+  clipboard->ReadData(custom_format, /* data_dst = */ nullptr, &custom_data);
+  if (!custom_data.empty())
+    new_data.SetCustomData(custom_format.GetName(), custom_data);
+
   CommitData(GetActiveAccountId(), std::move(new_data));
 }
 
