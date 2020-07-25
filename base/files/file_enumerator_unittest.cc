2a31f4a212c3ab98ddadd579ecf9e8c98049b3e9
Ian Barkley-Yeung <iby@chromium.org>
2020-07-25 07:03:26 +0000

    Fix unit test on Windows
    Stop unit test FileEnumerator::GetInfoDotDot from failing on Windows.
    The unit test is showing a real bug in Windows, but it's not one we can
    fix, so skip the check on Windows.
    
    Fixed: chromium:1108304
    
    Change-Id: Ibf0efa1a18fbd6c9a8008425eb7192f6a5e665a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2314816
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Ian Barkley-Yeung <iby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791370}

--- base/files/file_enumerator_unittest.cc
+++ base/files/file_enumerator_unittest.cc
@@ -552,6 +552,11 @@ TEST(FileEnumerator, GetInfoRecursive) {
 #if defined(OS_FUCHSIA)
 // FileEnumerator::GetInfo does not work correctly with INCLUDE_DOT_DOT.
 // https://crbug.com/1106172
+#elif defined(OS_WIN)
+// Windows has a bug in their handling of ".."; they always report the file
+// modification time of the current directory, not the parent directory. This is
+// a bug in Windows, not us -- you can see it with the "dir" command (notice
+// that the time of . and .. always match). Skip this test.
 #else
 // Tests that FileEnumerator::GetInfo() returns the correct info for the ..
 // directory.
@@ -604,6 +609,6 @@ TEST(FileEnumerator, GetInfoDotDot) {
         << "File " << file.path.value() << " was not returned";
   }
 }
-#endif  // !defined(OS_FUCHSIA)
+#endif  // !defined(OS_FUCHSIA) && !defined(OS_WIN)
 
 }  // namespace base
