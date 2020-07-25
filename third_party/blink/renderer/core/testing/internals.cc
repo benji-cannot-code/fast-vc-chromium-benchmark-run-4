2aa9aa9ab665308cafe47880a01be3ec6597b765
Lan Wei <lanwei@chromium.org>
2020-07-25 15:36:02 +0000

    Reland "Rewrite hr-timestamp/input-events.html to a browser test"
    hr-timestamp/input-events.htmlEvent timestamp tests that the received
    events on the page should be equal to the timestamp provided by the
    eventSender's recorded last event. But we should deprecate eventSender in
    the future, and GpuBenchmarking sends events based on the current system
    time, which cannot be equal to the received events' timestamp.
    
    Some Android bots such as android-pie-x86-rel have 1 ms difference, so
    I add 1 ms error.
    
    TBR=bokan@chromium.org，majidvp@chromium.org
    
    Bug: 1047176
    Change-Id: Ie2e52d0f097aa399823208cbb5ff2df24a23c56a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2314687
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791466}

--- third_party/blink/renderer/core/testing/internals.cc
+++ third_party/blink/renderer/core/testing/internals.cc
@@ -3318,6 +3318,11 @@ double Internals::monotonicTimeToZeroBasedDocumentTime(
       .InSecondsF();
 }
 
+int64_t Internals::zeroBasedDocumentTimeToMonotonicTime(double dom_event_time) {
+  return document_->Loader()->GetTiming().ZeroBasedDocumentTimeToMonotonicTime(
+      dom_event_time);
+}
+
 int64_t Internals::currentTimeTicks() {
   return base::TimeTicks::Now().since_origin().InMicroseconds();
 }
