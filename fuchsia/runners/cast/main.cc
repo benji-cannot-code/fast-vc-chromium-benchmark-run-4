78c49292c31183543f69b9656ecfa7998b7b5e3d
David Dorwin <ddorwin@chromium.org>
2020-07-25 08:15:15 +0000

    [fuchsia] Register crash product names for components
    Crashes in WebEngine are reported as FuchsiaWebEngine, and crashes in
    CastRunner are reported as FuchsiaCastRunner.
    The version is also reported. The channel is the empty string for now.
    
    Bug: 1050339
    Test: While the components are running, run `fx bugreport`. Then verify the mapping is registered in the bugreport's inspect.json at crash_reports.cmx > fidl > crash_register > mappings.
    Change-Id: I86ffc4c0c1af19f9f34a462ee7fce77068cbded1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315796
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791383}

--- fuchsia/runners/cast/main.cc
+++ fuchsia/runners/cast/main.cc
@@ -14,12 +14,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_executor.h"
 #include "base/values.h"
 #include "fuchsia/base/config_reader.h"
+#include "fuchsia/base/feedback_registration.h"
 #include "fuchsia/base/fuchsia_dir_scheme.h"
 #include "fuchsia/base/init_logging.h"
 #include "fuchsia/base/inspect.h"
 #include "fuchsia/runners/cast/cast_runner.h"
 
 namespace {
+
+constexpr char kCrashProductName[] = "FuchsiaCastRunner";
+// TODO(https://fxbug.dev/51490): Use a programmatic mechanism to obtain this.
+constexpr char kComponentUrl[] =
+    "fuchsia-pkg://fuchsia.com/cast_runner#meta/cast_runner.cmx";
 
 bool IsHeadless() {
   constexpr char kHeadlessConfigKey[] = "headless";
@@ -38,6 +44,8 @@ bool IsHeadless() {
 int main(int argc, char** argv) {
   base::SingleThreadTaskExecutor io_task_executor(base::MessagePumpType::IO);
   base::RunLoop run_loop;
+
+  cr_fuchsia::RegisterCrashReportingFields(kComponentUrl, kCrashProductName);
 
   base::CommandLine::Init(argc, argv);
   CHECK(cr_fuchsia::InitLoggingFromCommandLine(
