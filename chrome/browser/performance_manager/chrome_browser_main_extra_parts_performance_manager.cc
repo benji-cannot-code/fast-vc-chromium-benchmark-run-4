bbb005c48c326e6ac13a597eb90677fe8764f6c2
Brian Geffon <bgeffon@chromium.org>
2020-07-26 04:53:37 +0000

    CrOS: userspace swap: introduce the userspace swap policy.
    This CL introduces the userspace swap policy as described in
    go/userspace-swap. The policy is only concerned with the "when"
    of userspace swap and not the actual userspace swap implementation
    itself. The swapping part will be handled in a later CL which will
    contain the "mechanism" part of userspace swapping.
    
    This CL uses information about renderers to determine if they are
    eligible to be swapped and if so it will trigger the userspace
    swap process to the mechanism.
    
    BUG=chromium:1067833
    
    Change-Id: I9a1f635c96401701f4c1cad44ba403582f2af828
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2295926
    Commit-Queue: Brian Geffon <bgeffon@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791615}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -36,10 +36,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "base/allocator/buildflags.h"
+#include "chrome/browser/performance_manager/policies/userspace_swap_policy_chromeos.h"
+
 #if BUILDFLAG(USE_TCMALLOC)
 #include "chrome/browser/performance_manager/policies/dynamic_tcmalloc_policy_chromeos.h"
 #include "chrome/common/performance_manager/mojom/tcmalloc.mojom.h"
 #endif  // BUILDFLAG(USE_TCMALLOC)
+
 #endif  // defined(OS_CHROMEOS)
 
 #if !defined(OS_ANDROID)
@@ -92,6 +95,12 @@ void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
   }
 
 #if defined(OS_CHROMEOS)
+  if (performance_manager::policies::UserspaceSwapPolicy::
+          UserspaceSwapSupportedAndEnabled()) {
+    graph->PassToGraph(
+        std::make_unique<performance_manager::policies::UserspaceSwapPolicy>());
+  }
+
 #if BUILDFLAG(USE_TCMALLOC)
   if (base::FeatureList::IsEnabled(
           performance_manager::features::kDynamicTcmallocTuning)) {
