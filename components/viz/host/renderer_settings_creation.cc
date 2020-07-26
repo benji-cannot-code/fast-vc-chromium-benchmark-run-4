2320d7a57efcc09c4a37566b6b8496865eb309ed
Alexander Alekseev <alemate@chromium.org>
2020-07-26 06:24:44 +0000

    [viz + Ash] Make --show-aggregated-damage dynamic and add it to HUD overlay.
    This Cl moves show_aggregated_damage from RendererSettings to
    DebugRendererSettings (thus making it dynamic) and adds checkbox for it
    in Ash HUD settings.
    
    Bug: 1106855
    Change-Id: Id42dcd54476eac3d4b2c4339f4e6c7b4c38709f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315437
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791627}

--- components/viz/host/renderer_settings_creation.cc
+++ components/viz/host/renderer_settings_creation.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/viz/host/renderer_settings_creation.h"
 
+#include <string>
+
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
@@ -60,8 +62,6 @@ RendererSettings CreateRendererSettings() {
 #elif defined(OS_CHROMEOS)
   renderer_settings.auto_resize_output_surface = false;
 #endif
-  renderer_settings.show_aggregated_damage =
-      command_line->HasSwitch(switches::kShowAggregatedDamage);
   renderer_settings.allow_antialiasing =
       !command_line->HasSwitch(switches::kDisableCompositedAntialiasing);
   renderer_settings.use_skia_renderer = features::IsUsingSkiaRenderer();
@@ -110,6 +110,8 @@ DebugRendererSettings CreateDefaultDebugRendererSettings() {
       command_line->HasSwitch(switches::kShowOverdrawFeedback);
   result.show_dc_layer_debug_borders =
       command_line->HasSwitch(switches::kShowDCLayerDebugBorders);
+  result.show_aggregated_damage =
+      command_line->HasSwitch(switches::kShowAggregatedDamage);
   return result;
 }
 
