40eccb7770ea3ab9d38869356788dcde34d6b16b
Xing Liu <xingliu@chromium.org>
2020-07-26 06:45:19 +0000

    Download later: Polish the state when selecting download later.
    When selecting download later in the download later dialog, checkbox
    is now disabled, and the positive button text is changed to "Next".
    
    Bug: 1107620
    Change-Id: Idf08c8675b81b6eb67affbe0ebd231ded621fbf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316447
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791630}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/dialogs/DownloadLaterDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/dialogs/DownloadLaterDialogTest.java
@@ -7,7 +7,9 @@ package org.chromium.chrome.browser.download.dialogs;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -17,6 +19,9 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import android.view.View;
+
+import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.filters.MediumTest;
 
 import org.junit.Assert;
@@ -161,6 +166,11 @@ public class DownloadLaterDialogTest {
             Assert.assertNotNull(downloadLaterButton);
             downloadLaterButton.setChecked(true);
             getDownloadLaterDialogView().onCheckedChanged(null, -1);
+        });
+
+        onView(withId(org.chromium.chrome.R.id.positive_button)).check(matches(withText("Next")));
+        onView(withId(R.id.show_again_checkbox)).check((View view, NoMatchingViewException e) -> {
+            Assert.assertFalse(view.isEnabled());
         });
 
         clickPositiveButton();
