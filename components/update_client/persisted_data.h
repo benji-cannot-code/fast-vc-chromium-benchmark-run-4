b100ca3b5f916924d42496cf2036b1742fc6342b
Joshua Pawlicki <waffles@chromium.org>
2020-07-27 23:20:28 +0000

    update_client: Adjust how Prefs data is stored to allow for ids containing "."s.
    Bug: 1092503
    Change-Id: Ia4fffa66a320904b2ac6985d82416cbba5483a19
    Fixed: 1092503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316668
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792058}

--- components/update_client/persisted_data.h
+++ components/update_client/persisted_data.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
-#include "base/threading/thread_checker.h"
+#include "base/sequence_checker.h"
 #include "base/values.h"
 
 class PrefRegistrySimple;
@@ -114,13 +114,23 @@ class PersistedData {
   void SetFingerprint(const std::string& id, const std::string& fingerprint);
 
  private:
+  // Returns nullptr if the app key does not exist.
+  const base::Value* GetAppKey(const std::string& id) const;
+
+  // Returns an existing or newly created app key under a root pref.
+  base::Value* GetOrCreateAppKey(const std::string& id, base::Value* root);
+
+  // Returns fallback if the key does not exist.
   int GetInt(const std::string& id, const std::string& key, int fallback) const;
+
+  // Returns the empty string if the key does not exist.
   std::string GetString(const std::string& id, const std::string& key) const;
+
   void SetString(const std::string& id,
                  const std::string& key,
                  const std::string& value);
 
-  base::ThreadChecker thread_checker_;
+  SEQUENCE_CHECKER(sequence_checker_);
   PrefService* pref_service_;
   ActivityDataService* activity_data_service_;
 
