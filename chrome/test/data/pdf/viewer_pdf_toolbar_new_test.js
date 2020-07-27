8db39be17ed079178d29727846f21dae4b99007e
John Lee <johntlee@chromium.org>
2020-07-27 18:52:00 +0000

    PDF Viewer: Hook in single page view vs. two-page view buttons
    Bug: 1084620
    Change-Id: Idde2fe2bc1ce4f387a20a00d01aa7c8ba13354a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311039
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791914}

--- chrome/test/data/pdf/viewer_pdf_toolbar_new_test.js
+++ chrome/test/data/pdf/viewer_pdf_toolbar_new_test.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {FittingType} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/constants.js';
+import {FittingType, TwoUpViewAction} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/constants.js';
 import {ViewerPdfToolbarNewElement} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/elements/viewer-pdf-toolbar-new.js';
 
 /** @return {!ViewerPdfToolbarNewElement} */
@@ -146,7 +146,43 @@ const tests = [
     });
     zoomField.value = '110%';
     zoomField.dispatchEvent(new CustomEvent('input'));
-  }
+  },
+
+  function testSinglePageView() {
+    const toolbar = createToolbar();
+    const singlePageViewButton =
+        toolbar.shadowRoot.querySelector('#single-page-view-button');
+    const twoPageViewButton =
+        toolbar.shadowRoot.querySelector('#two-page-view-button');
+
+    toolbar.addEventListener('two-up-view-changed', function(e) {
+      chrome.test.assertEq(TwoUpViewAction.TWO_UP_VIEW_DISABLE, e.detail);
+      chrome.test.assertFalse(
+          singlePageViewButton.querySelector('iron-icon').hidden);
+      chrome.test.assertTrue(
+          twoPageViewButton.querySelector('iron-icon').hidden);
+      chrome.test.succeed();
+    });
+    singlePageViewButton.click();
+  },
+
+  function testTwoPageView() {
+    const toolbar = createToolbar();
+    const singlePageViewButton =
+        toolbar.shadowRoot.querySelector('#single-page-view-button');
+    const twoPageViewButton =
+        toolbar.shadowRoot.querySelector('#two-page-view-button');
+
+    toolbar.addEventListener('two-up-view-changed', function(e) {
+      chrome.test.assertEq(TwoUpViewAction.TWO_UP_VIEW_ENABLE, e.detail);
+      chrome.test.assertFalse(
+          twoPageViewButton.querySelector('iron-icon').hidden);
+      chrome.test.assertTrue(
+          singlePageViewButton.querySelector('iron-icon').hidden);
+      chrome.test.succeed();
+    });
+    twoPageViewButton.click();
+  },
 ];
 
 chrome.test.runTests(tests);
