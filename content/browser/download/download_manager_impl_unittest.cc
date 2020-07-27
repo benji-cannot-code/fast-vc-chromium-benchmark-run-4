71994f2e2dd4b29c9e708011ad7084b727dfa13b
Maksim Sisov <msisov@igalia.com>
2020-07-27 12:03:09 +0000

    X11 and Ozone: cleanup //content for use_x11 && use_ozone build.
    Disables/enables tests for Ozone/Linux or non-Ozone/X11
    
    -----
    
    PS: Please note that this is a temp solution that will help to choose
    between ozone and non-ozone X11 build. The switch that will be used
    to choose the path is --enable-features=UseOzonePlatform. Once
    non-Ozone X11 path is removed (hopefully by Q1 2021 depending on how
    th finch trial goes), the wrapper will be removed.
    
    Please also note that it's impossible to build use_x11 && use_ozone
    without some hacks in PlatformCursor code. The changes to that are
    on their way to upstream.
    
    ----
    
    Bug: 1085700
    Change-Id: I48797083cecf61929e10381d740590fc4584137d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315027
    Commit-Queue: Maksim Sisov (GMT+3) <msisov@igalia.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791772}

--- content/browser/download/download_manager_impl_unittest.cc
+++ content/browser/download/download_manager_impl_unittest.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_browser_context.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/ui_base_features.h"
 #include "url/origin.h"
 
 using base::test::RunOnceCallback;
@@ -608,10 +609,16 @@ TEST_F(DownloadManagerTest, StartDownload) {
   EXPECT_CALL(GetMockDownloadManagerDelegate(), GetNextId_(_))
       .WillOnce(RunOnceCallback<0>(local_id));
 
-#if !defined(USE_X11)
-  // Doing nothing will set the default download directory to null.
-  EXPECT_CALL(GetMockDownloadManagerDelegate(), GetSaveDir(_, _, _));
+  // TODO(https://crbug.com/1109690): figure out what to do for Ozone/Linux.
+  // Probably, this can be removed.
+  bool should_call_get_save_dir = true;
+#if defined(USE_X11)
+  should_call_get_save_dir = features::IsUsingOzonePlatform();
 #endif
+  if (should_call_get_save_dir) {
+    // Doing nothing will set the default download directory to null.
+    EXPECT_CALL(GetMockDownloadManagerDelegate(), GetSaveDir(_, _, _));
+  }
   EXPECT_CALL(GetMockDownloadManagerDelegate(),
               ApplicationClientIdForFileScanning())
       .WillRepeatedly(Return("client-id"));
@@ -644,10 +651,16 @@ TEST_F(DownloadManagerTest, StartDownloadWithoutHistoryDB) {
   EXPECT_CALL(GetMockDownloadManagerDelegate(), GetNextId_(_))
       .WillOnce(RunOnceCallback<0>(download::DownloadItem::kInvalidId));
 
-#if !defined(USE_X11)
-  // Doing nothing will set the default download directory to null.
-  EXPECT_CALL(GetMockDownloadManagerDelegate(), GetSaveDir(_, _, _));
+  // TODO(https://crbug.com/1109690): figure out what to do for Ozone/Linux.
+  // Probably, this can be removed.
+  bool should_call_get_save_dir = true;
+#if defined(USE_X11)
+  should_call_get_save_dir = features::IsUsingOzonePlatform();
 #endif
+  if (should_call_get_save_dir) {
+    // Doing nothing will set the default download directory to null.
+    EXPECT_CALL(GetMockDownloadManagerDelegate(), GetSaveDir(_, _, _));
+  }
   EXPECT_CALL(GetMockDownloadManagerDelegate(),
               ApplicationClientIdForFileScanning())
       .WillRepeatedly(Return("client-id"));
