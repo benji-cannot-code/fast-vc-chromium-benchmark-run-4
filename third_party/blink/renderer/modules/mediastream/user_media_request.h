ae4e279f4c7c04949cba0ef757f07e6e50ce9cd1
Guido Urdaneta <guidou@chromium.org>
2020-07-27 13:46:08 +0000

    Reland "Do not expose media devices before a successful getUserMedia()"
    This reverts commit 69a6001995c51516b403dbde99a2db4c5054ce55.
    
    chrome-prefixed URLs are exempted from the getUserMedia() requirement.
    
    Original change's description:
    > Revert "Do not expose media devices before a successful getUserMedia()"
    >
    > This reverts commit d64e736aa8c7976b12b33f92472f7950bf0215b9.
    >
    > Reason for revert: Caused chrome_all_tast_tests / camera.CCAUISanity.fake failures; e.g. https://ci.chromium.org/p/chrome/builders/ci/chromeos-betty-pi-arc-chrome/7680
    >
    > Original change's description:
    > > Do not expose media devices before a successful getUserMedia()
    > >
    > > This is a spec-compliance change.
    > >
    > > This CL also adds some improvements to the blink MediaDevices unit test.
    > >
    > > Bug: 1101860
    > > Change-Id: If2e7824df3b3dbd3cdb80f4bcf6b5c417d6f0ef2
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279808
    > > Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    > > Reviewed-by: Harald Alvestrand <hta@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#786253}
    >
    > TBR=hta@chromium.org,guidou@chromium.org
    >
    > Change-Id: I41b676c7d3e437c06d23f2b46270453f1cc81670
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1101860
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2288282
    > Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    > Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#786615}
    
    TBR=mohsen@chromium.org,hta@chromium.org,guidou@chromium.org
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1101860
    Change-Id: I3fb6d9a620291cda57eb5fe48ce330b45425a5bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2289833
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791797}

--- third_party/blink/renderer/modules/mediastream/user_media_request.h
+++ third_party/blink/renderer/modules/mediastream/user_media_request.h
@@ -95,12 +95,14 @@ class MODULES_EXPORT UserMediaRequest final
                                   MediaType media_type,
                                   const MediaStreamConstraints* options,
                                   Callbacks*,
+                                  base::OnceClosure success_update_callback,
                                   MediaErrorState&);
   static UserMediaRequest* Create(ExecutionContext*,
                                   UserMediaController*,
                                   const MediaStreamConstraints* options,
                                   V8NavigatorUserMediaSuccessCallback*,
                                   V8NavigatorUserMediaErrorCallback*,
+                                  base::OnceClosure success_update_callback,
                                   MediaErrorState&);
   static UserMediaRequest* CreateForTesting(const MediaConstraints& audio,
                                             const MediaConstraints& video);
@@ -110,7 +112,8 @@ class MODULES_EXPORT UserMediaRequest final
                    MediaType media_type,
                    MediaConstraints audio,
                    MediaConstraints video,
-                   Callbacks*);
+                   Callbacks*,
+                   base::OnceClosure success_update_callback);
   virtual ~UserMediaRequest();
 
   LocalDOMWindow* GetWindow();
@@ -162,6 +165,7 @@ class MODULES_EXPORT UserMediaRequest final
   Member<UserMediaController> controller_;
 
   Member<Callbacks> callbacks_;
+  base::OnceClosure success_update_callback_;
   bool is_resolved_ = false;
 };
 
