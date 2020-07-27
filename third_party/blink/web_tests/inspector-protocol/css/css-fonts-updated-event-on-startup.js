0a2d277d0ba4da64f9d237af0e8792b5a08c02f3
Alex Rudenko <alexrudenko@chromium.org>
2020-07-27 07:16:35 +0000

    Use Amstelvar font in inspector-protocol tests
    Amstelvar has many font variation axes so it's better to use it in a
    test.
    
    Bug: 1101236
    Change-Id: Ic15f80b9adcee826b7f0fb30904f3a4f8bfbb2cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2319986
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791726}

--- third_party/blink/web_tests/inspector-protocol/css/css-fonts-updated-event-on-startup.js
+++ third_party/blink/web_tests/inspector-protocol/css/css-fonts-updated-event-on-startup.js
@@ -1,13 +1,21 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
-  const {session, dp} = await testRunner.startHTML(
-    `<link rel="stylesheet" href="${testRunner.url('./resources/noto-mono.css')}">
+  const {dp} = await testRunner.startHTML(
+    `<style>
+      @font-face {
+        font-family: Amstelvar;
+        src: url('../../third_party/Amstelvar/Amstelvar.ttf');
+      }
+      body {
+        font-family: Amstelvar;
+      }
+    </style>
     some text`,
     'Verifies that CSS.fontsUpdated events are sent after CSS domain is enabled'
   );
   const eventPromise = dp.CSS.onceFontsUpdated(
     event => typeof event.params.font !== 'undefined' &&
-    event.params.font.fontFamily === 'Noto Mono');
+    event.params.font.fontFamily === 'Amstelvar');
   await dp.DOM.enable();
   await dp.CSS.enable();
   const event = await eventPromise;
