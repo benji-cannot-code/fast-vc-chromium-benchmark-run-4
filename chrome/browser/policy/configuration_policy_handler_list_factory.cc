c359baad215397e4d17fded14db3d9b6830c37da
Yann Dago <ydago@chromium.org>
2020-07-27 20:11:55 +0000

    Deprecate AuthServerWhitelist in favor of AuthServerAllowlist
    Bug: 1095307
    Change-Id: Ia2f45e05154e0cbd129c6aac1284c76aa23a00ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316337
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791952}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -320,9 +320,6 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kEnableAuthNegotiatePort,
     prefs::kEnableAuthNegotiatePort,
     base::Value::Type::BOOLEAN },
-  { key::kAuthServerWhitelist,
-    prefs::kAuthServerWhitelist,
-    base::Value::Type::STRING },
   { key::kGSSAPILibraryName,
     prefs::kGSSAPILibraryName,
     base::Value::Type::STRING },
@@ -1438,6 +1435,13 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
   handlers->AddHandler(std::make_unique<RestoreOnStartupPolicyHandler>());
   handlers->AddHandler(
       std::make_unique<safe_browsing::SafeBrowsingPolicyHandler>());
+  handlers->AddHandler(std::make_unique<SimpleDeprecatingPolicyHandler>(
+      std::make_unique<SimplePolicyHandler>(key::kAuthServerWhitelist,
+                                            prefs::kAuthServerAllowlist,
+                                            base::Value::Type::STRING),
+      std::make_unique<SimplePolicyHandler>(key::kAuthServerAllowlist,
+                                            prefs::kAuthServerAllowlist,
+                                            base::Value::Type::STRING)));
   handlers->AddHandler(std::make_unique<SimpleDeprecatingPolicyHandler>(
       std::make_unique<SimplePolicyHandler>(
           key::kAuthNegotiateDelegateWhitelist,
--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -767,7 +767,7 @@ extern const char kTotalMemoryLimitMb[];
 extern const char kAuthSchemes[];
 extern const char kDisableAuthNegotiateCnameLookup[];
 extern const char kEnableAuthNegotiatePort[];
-extern const char kAuthServerWhitelist[];
+extern const char kAuthServerAllowlist[];
 extern const char kAuthNegotiateDelegateAllowlist[];
 extern const char kGSSAPILibraryName[];
 extern const char kAuthAndroidNegotiateAccountType[];
