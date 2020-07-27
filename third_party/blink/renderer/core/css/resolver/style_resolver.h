e2fc816a493f38410a5fe2703d42b92cb3500581
Anders Hartvoll Ruud <andruud@chromium.org>
2020-07-27 21:38:24 +0000

    Eliminate PseudoStyleForElementInternal
    This CL merges the function PseudoStyleForElementInternal with its
    non-internal wrapper PseudoStyleForElement.
    
    This is a step towards unifying the two very similar codepaths of
    StyleForElement and PseudoStyleForElement.
    
    There should be no behavior change.
    
    Change-Id: Iad0dfd59c8081de5ec68bbc5120e4cf28eee7954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2317960
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#792003}

--- third_party/blink/renderer/core/css/resolver/style_resolver.h
+++ third_party/blink/renderer/core/css/resolver/style_resolver.h
@@ -244,10 +244,6 @@ class CORE_EXPORT StyleResolver final : public GarbageCollected<StyleResolver> {
 
   void ApplyCallbackSelectors(StyleResolverState&);
 
-  bool PseudoStyleForElementInternal(Element&,
-                                     const PseudoElementStyleRequest&,
-                                     StyleResolverState&);
-
   bool HasAuthorBorder(const StyleResolverState&);
   Document& GetDocument() const { return *document_; }
   bool WasViewportResized() const { return was_viewport_resized_; }
