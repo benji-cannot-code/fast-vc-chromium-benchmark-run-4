d53c93b47227b7492d20ca41f15a6335b0d6daca
Rainhard Findling <rainhard@chromium.org>
2020-07-27 10:13:58 +0000

    Safety check: update Chrome cleaner safe states
    * This CL aligns the Chrome cleaner child element behavior in the
      'safe' states with the latest mocks.
    
    Bug: 1087263
    Change-Id: Ie4c36cf6d45c80f421b191758aeadadab8d04280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2282401
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791752}

--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -1371,6 +1371,8 @@ void AddPrivacyStrings(content::WebUIDataSource* html_source,
        IDS_SETTINGS_SAFETY_CHECK_CHROME_CLEANER_PRIMARY_LABEL},
       {"safetyCheckChromeCleanerButtonAriaLabel",
        IDS_SETTINGS_SAFETY_CHECK_CHROME_CLEANER_BUTTON_ARIA_LABEL},
+      {"safetyCheckChromeCleanerMoreButtonAriaLabel",
+       IDS_SETTINGS_SAFETY_CHECK_CHROME_CLEANER_MORE_BUTTON_ARIA_LABEL},
   };
   AddLocalizedStringsBulk(html_source, kLocalizedStrings);
 
