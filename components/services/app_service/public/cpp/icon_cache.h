a96ecce14c10707ec97f16f98bef0e9e974037d2
nancylingwang <nancylingwang@chromium.org>
2020-07-27 19:00:06 +0000

    Add RemoveIcon to IconCache to cache icons for the search result.
    When clicks the launcher button, AppServiceAppResult is created for each
    app, and call LoadIcon, which could genearte mojom calls to load the
    icon.
    
    Remove SweepReleasedIcons when the view is closed to keep the icon
    cache in IconCache. When the app is removed or the icon is updated, call
    RemoveIcon to clear the map in IconCache.
    
    BUG=1104650
    
    Change-Id: I6f5bfb44c9892187976f89ecb3993430fb565f2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2318486
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791922}

--- components/services/app_service/public/cpp/icon_cache.h
+++ components/services/app_service/public/cpp/icon_cache.h
@@ -85,6 +85,8 @@ class IconCache : public IconLoader {
   // actively held.
   void SweepReleasedIcons();
 
+  void RemoveIcon(apps::mojom::AppType app_type, const std::string& app_id);
+
  private:
   class Value {
    public:
