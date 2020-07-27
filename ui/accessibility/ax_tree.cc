540bb33825d802ac1b12d8be8b5ed9ec6e38dc58
Mario Sanchez Prada <mario@igalia.com>
2020-07-27 05:41:30 +0000

    Re-enable flaky TranslateIframe test and add extra debug information
    This test has been disabled in https://crrev.com/c/2303433 due to it
    being flaky. However its stack trace matches the one observed recently
    in production code (crbug.com/1107103) so we're re-enabling it now and
    adding some extra debugging information in order to help us fix the
    issue, as it hasn't been possible to reproduce it locally so far.
    
    Bug: 1107103
    Change-Id: Ifeee8483c83e72b52a232b71ef2437ecab6bf305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316101
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Auto-Submit: Mario Sanchez Prada <mario@igalia.com>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791714}

--- ui/accessibility/ax_tree.cc
+++ ui/accessibility/ax_tree.cc
@@ -1354,8 +1354,10 @@ bool AXTree::ComputePendingChangesToNode(const AXNodeData& new_data,
       if (!update_state->IncrementPendingCreateNodeCount(child_id,
                                                          new_data.id)) {
         error_ = base::StringPrintf(
-            "Node %d is already pending for creation, cannot be a new child",
-            child_id);
+            "Node %d from |new_child_id_set| is already pending for "
+            "creation, cannot be a new child (of parent %d)\n"
+            "Parent Node info:\n%s",
+            child_id, new_data.id, new_data.ToString().c_str());
         return false;
       }
     }
@@ -1407,8 +1409,10 @@ bool AXTree::ComputePendingChangesToNode(const AXNodeData& new_data,
       if (!update_state->IncrementPendingCreateNodeCount(child_id,
                                                          new_data.id)) {
         error_ = base::StringPrintf(
-            "Node %d is already pending for creation, cannot be a new child",
-            child_id);
+            "Node %d from |create_or_destroy_ids| is already pending for "
+            "creation, cannot be a new child (of parent %d)\n"
+            "Parent Node info:\n%s",
+            child_id, new_data.id, new_data.ToString().c_str());
         return false;
       }
     } else {
