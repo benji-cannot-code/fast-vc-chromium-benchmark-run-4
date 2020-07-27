b522dfb88531a86e835692c254a6614724ae1f3a
Henrique Nakashima <hnakashima@chromium.org>
2020-07-27 19:36:18 +0000

    Remove incorrect @VisibleForTesting from FeatureList methods
    This fixes some lint warnings, so we can remove the suppressions.
    
    Bug: 1107950
    Change-Id: Idbe53038d4595fb571d374f04665671dd68e4d93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310789
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791931}

--- base/android/java/src/org/chromium/base/FeatureList.java
+++ base/android/java/src/org/chromium/base/FeatureList.java
@@ -84,7 +84,6 @@ public class FeatureList {
     /**
      * @return Whether test feature values have been configured.
      */
-    @VisibleForTesting
     public static boolean hasTestFeatures() {
         return sTestFeatures != null;
     }
@@ -96,7 +95,6 @@ public class FeatureList {
      * @return The test value set for the feature, or null if no test value has been set.
      * @throws IllegalArgumentException if no test value was set and default values aren't allowed.
      */
-    @VisibleForTesting
     public static Boolean getTestValueForFeature(String featureName) {
         if (hasTestFeatures()) {
             if (sTestFeatures.containsKey(featureName)) {
