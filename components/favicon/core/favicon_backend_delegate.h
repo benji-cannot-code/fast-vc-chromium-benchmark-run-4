574bd629b3e74d298e18df768eeab3861e26b699
Scott Violet <sky@chromium.org>
2020-07-27 21:43:31 +0000

    history/favicons: creates FaviconBackend with favicon logic
    This moves all the favicon related functionality out of
    HistoryBackend into FaviconBackend. FaviconBackend is owned
    by HistoryBackend. FaviconBackend indirectly talks to
    HistoryBackend by way FaviconBackendDelegate.
    
    BUG=1076463
    TEST=covered by tests
    
    Change-Id: If9927019b4539174d36daaf02597fcf1f1ad5c35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2314990
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792007}

---
+++ components/favicon/core/favicon_backend_delegate.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_FAVICON_CORE_FAVICON_BACKEND_DELEGATE_H_
+#define COMPONENTS_FAVICON_CORE_FAVICON_BACKEND_DELEGATE_H_
+
+#include <vector>
+
+class GURL;
+
+namespace favicon {
+
+class FaviconBackendDelegate {
+ public:
+  FaviconBackendDelegate() = default;
+
+  // The delegate should schedule an asynchronous commit.
+  virtual void ScheduleCommitForFavicons() = 0;
+
+  // Returns the redirects for |page_url|. This should always return a
+  // vector with at least one element (|page_url|).
+  virtual std::vector<GURL> GetCachedRecentRedirectsForPage(
+      const GURL& page_url) = 0;
+
+  // Called when the favicon for a particular page changes.
+  virtual void OnFaviconChangedForPageAndRedirects(const GURL& page_url) = 0;
+
+ protected:
+  virtual ~FaviconBackendDelegate() = default;
+};
+
+}  // namespace favicon
+
+#endif  // COMPONENTS_FAVICON_CORE_FAVICON_BACKEND_DELEGATE_H_
