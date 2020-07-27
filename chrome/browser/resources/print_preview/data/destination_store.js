a8eb60ef9625ff022ed40e5d07f58395efcebe1c
rbpotter <rbpotter@chromium.org>
2020-07-27 18:11:18 +0000

    Print Preview: closure compile more tests
    Add closure compilation for:
    - duplex_settings_test.js
    - dpi_settings_test.js
    - destination_store_test.js
    
    Bug: 1000989
    Change-Id: I799f0006f738c8f6650d5af2a8e87a6efe25adb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2314666
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791875}

--- chrome/browser/resources/print_preview/data/destination_store.js
+++ chrome/browser/resources/print_preview/data/destination_store.js
@@ -1136,7 +1136,7 @@ export class DestinationStore extends EventTarget {
     clearTimeout(this.autoSelectTimeout_);
     this.autoSelectTimeout_ = setTimeout(
         this.selectDefaultDestination.bind(this),
-        DestinationStore.AUTO_SELECT_TIMEOUT_);
+        DestinationStore.AUTO_SELECT_TIMEOUT);
   }
 
   /**
@@ -1371,10 +1371,10 @@ DestinationStore.EventType = {
  * Delay in milliseconds before the destination store ignores the initial
  * destination ID and just selects any printer (since the initial destination
  * was not found).
- * @private {number}
- * @const
+ * Public and non-const so that it can be overridden in tests.
+ * @type {number}
  */
-DestinationStore.AUTO_SELECT_TIMEOUT_ = 15000;
+DestinationStore.AUTO_SELECT_TIMEOUT = 15000;
 
 /**
  * Maximum amount of time spent searching for extension destinations, in
