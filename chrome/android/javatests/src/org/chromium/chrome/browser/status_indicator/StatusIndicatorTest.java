4b034572ccced10b54d8d093e65b5581b0aa7a8a
Theresa Wellington <twellington@chromium.org>
2020-07-27 19:42:48 +0000

    🤠 Disable flaky StatusIndicatorTest's
    TBR=sinansahin@google.com
    BUG=1109965
    
    Change-Id: I157f44d8557a457390321194e9e81be53fe2ea07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2319344
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791937}

--- chrome/android/javatests/src/org/chromium/chrome/browser/status_indicator/StatusIndicatorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/status_indicator/StatusIndicatorTest.java
@@ -34,6 +34,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
@@ -167,6 +168,7 @@ public class StatusIndicatorTest {
     @CommandLineFlags.Add({"enable-features=" + ChromeFeatureList.START_SURFACE_ANDROID + "<Study",
             "force-fieldtrials=Study/Group",
             "force-fieldtrial-params=Study.Group:start_surface_variation/single"})
+    @DisabledTest(message = "https://crbug.com/1109965")
     public void testShowAndHideOnStartSurface() {
         // clang-format on
         TabUiTestHelper.enterTabSwitcher(mActivityTestRule.getActivity());
@@ -223,6 +225,7 @@ public class StatusIndicatorTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1109965")
     public void testShowAndHideOnNTP() {
         mActivityTestRule.loadUrl(UrlConstants.NTP_URL);
         Tab tab = mActivityTestRule.getActivity().getActivityTab();
@@ -265,6 +268,7 @@ public class StatusIndicatorTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1109965")
     public void testShowAndHideOnRecentTabsPage() {
         mActivityTestRule.loadUrl(UrlConstants.RECENT_TABS_URL);
         final Tab tab = mActivityTestRule.getActivity().getActivityTab();
