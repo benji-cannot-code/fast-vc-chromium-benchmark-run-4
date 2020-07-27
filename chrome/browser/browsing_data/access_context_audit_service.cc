67dfd44da297b7ffae3f49266362da85ff01167d
sauski <sauski@google.com>
2020-07-27 08:08:34 +0000

    Access Context Auditing: Respect time range history deletions
    Extends Access Context service and database to delete access records
    with timestamps that fall within an observed history deletion time
    range.
    
    Bug: 1101675
    Change-Id: I49e0fdfc4d638c6d065d63a188da57ae5ab9ffca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2312699
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791736}

--- chrome/browser/browsing_data/access_context_audit_service.cc
+++ chrome/browser/browsing_data/access_context_audit_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
+#include "base/time/default_clock.h"
 #include "base/time/time.h"
 #include "chrome/browser/browsing_data/access_context_audit_database.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
@@ -17,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 
 AccessContextAuditService::AccessContextAuditService(Profile* profile)
-    : profile_(profile) {}
+    : clock_(base::DefaultClock::GetInstance()), profile_(profile) {}
 AccessContextAuditService::~AccessContextAuditService() = default;
 
 bool AccessContextAuditService::Init(
@@ -50,7 +51,7 @@ bool AccessContextAuditService::Init(
 void AccessContextAuditService::RecordCookieAccess(
     const net::CookieList& accessed_cookies,
     const url::Origin& top_frame_origin) {
-  auto now = base::Time::Now();
+  auto now = clock_->Now();
   std::vector<AccessContextAuditDatabase::AccessRecord> access_records;
   for (const auto& cookie : accessed_cookies) {
     // Do not record accesses to already expired cookies. This service is
@@ -72,8 +73,8 @@ void AccessContextAuditService::RecordStorageAPIAccess(
     AccessContextAuditDatabase::StorageAPIType type,
     const url::Origin& top_frame_origin) {
   std::vector<AccessContextAuditDatabase::AccessRecord> access_record = {
-      AccessContextAuditDatabase::AccessRecord(
-          top_frame_origin, type, storage_origin, base::Time::Now())};
+      AccessContextAuditDatabase::AccessRecord(top_frame_origin, type,
+                                               storage_origin, clock_->Now())};
   database_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&AccessContextAuditDatabase::AddRecords,
                                 database_, std::move(access_record)));
@@ -123,6 +124,20 @@ void AccessContextAuditService::OnURLsDeleted(
     return;
   }
 
+  if (deletion_info.time_range().IsValid()) {
+    // If a time range is specified, a time based deletion is performed as a
+    // first pass before origins without history entries are removed. A second
+    // pass based on origins is required as access record timestamps are not
+    // directly comparable to history timestamps. Only deleting based on
+    // timestamp may persist origins on disk for which no other trace exists.
+    database_task_runner_->PostTask(
+        FROM_HERE,
+        base::BindOnce(
+            &AccessContextAuditDatabase::RemoveAllRecordsForTimeRange,
+            database_, deletion_info.time_range().begin(),
+            deletion_info.time_range().end()));
+  }
+
   std::vector<url::Origin> deleted_origins;
   // Map is of type {Origin -> {Count, LastVisitTime}}.
   for (const auto& origin_urls_remaining :
@@ -140,6 +155,10 @@ void AccessContextAuditService::OnURLsDeleted(
             &AccessContextAuditDatabase::RemoveAllRecordsForTopFrameOrigins,
             database_, std::move(deleted_origins)));
   }
+}
+
+void AccessContextAuditService::SetClockForTesting(base::Clock* clock) {
+  clock_ = clock;
 }
 
 void AccessContextAuditService::SetTaskRunnerForTesting(
