bbbc917462d29be80d4377d6c209d7776b7200c5
James Hollyer <jameshollyer@google.com>
2020-07-27 20:58:33 +0000

    Use kUserInteraction task source for Pointer Lock events
    kUserInteraction task source will align the pointer lock events with the
    mouse events. It will also help spec consistency.
    
    Bug: 1109007
    Change-Id: I6426874161fc772a27640831617e70f941737b2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316808
    Reviewed-by: James Hollyer <jameshollyer@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: James Hollyer <jameshollyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791978}

--- third_party/blink/renderer/core/page/pointer_lock_controller.cc
+++ third_party/blink/renderer/core/page/pointer_lock_controller.cc
@@ -329,7 +329,7 @@ void PointerLockController::EnqueueEvent(const AtomicString& type,
                                          Document* document) {
   if (document && document->domWindow()) {
     document->domWindow()->EnqueueDocumentEvent(*Event::Create(type),
-                                                TaskType::kMiscPlatformAPI);
+                                                TaskType::kUserInteraction);
   }
 }
 
