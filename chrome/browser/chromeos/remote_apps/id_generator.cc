9cf4c0065b6710c97dc428fe6382b8ee62bc154c
Jit Yao Yap <jityao@google.com>
2020-07-27 13:04:33 +0000

    Add RemoteApps
    This CL implements the RemoteApps PublisherBase and RemoteAppsManager
    which are used to manage Remote Apps. Remote Apps are items which are
    added to the launcher but are not backed by an app/extension/web app.
    Instead, the item is managed by an extension via an API. The extension
    is able to add multiple items and folders to the launcher and observe
    when the items are launched. The extension can then decide on how to
    handle the launch event. Currently RemoteApps are only available to
    Managed Guest Sessions.
    
    RemoteAppsManager is the KeyedService which handles the adding and
    deleting of Remote Apps and folders. RemoteAppsManager also implements
    RemoteApps::Delegate and is used by RemoteApps to handle the LaunchApp(),
    GetIcon() and GetMenuModel() methods.
    
    RemoteAppsManager is not initialized as a KeyedService so this CL does
    not change any existing behavior.
    
    Bug: 1101208
    Change-Id: I664469fa0284c6e65288e59ba7a5d1271b081a04
    DD: go/cros-remote-apps
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2299226
    Commit-Queue: Jit Yao Yap <jityao@google.com>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791788}

---
+++ chrome/browser/chromeos/remote_apps/id_generator.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/remote_apps/id_generator.h"
+
+#include "base/guid.h"
+
+namespace chromeos {
+
+std::string GuidIdGenerator::GenerateId() {
+  return base::GenerateGUID();
+}
+
+FakeIdGenerator::FakeIdGenerator(const std::vector<std::string>& ids)
+    : ids_(ids) {}
+
+FakeIdGenerator::~FakeIdGenerator() = default;
+
+std::string FakeIdGenerator::GenerateId() {
+  std::string id = ids_[index_];
+  ++index_;
+  return id;
+}
+
+}  // namespace chromeos
