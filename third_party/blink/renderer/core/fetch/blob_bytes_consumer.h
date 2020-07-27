39f81405e1516f0db45658cc510b845a0bc059a3
Ben Kelly <wanderview@chromium.org>
2020-07-27 18:31:48 +0000

    Fetch: Make BlobBytesConsumer use DataPipeBytesConsumer directly.
    Previously BlobBytesConsumer would read the BlobData object by first
    create a DataPipeGetter and then passing the resulting pipe to a
    FormDataBytesConsumer.  The FormDataBytesConsumer would then internally
    use a DataPipeBytesConsumer to read the pipe.
    
    This CL attempts to simply things by having BlobBytesConsumer use a
    DataPipeBytesConsumer directly.  In addition, it uses
    BlobData->ReadAll() avoiding the need to create a DataPipeGetter.  This
    removes one mojo call.
    
    Bug: 1076081
    Change-Id: I0f270cd99c9808c85d43523ac700aafe75d49c7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2171017
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791906}

--- third_party/blink/renderer/core/fetch/blob_bytes_consumer.h
+++ third_party/blink/renderer/core/fetch/blob_bytes_consumer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include "base/memory/scoped_refptr.h"
+#include "third_party/blink/public/mojom/blob/blob.mojom-blink-forward.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/loader/fetch/bytes_consumer.h"
@@ -41,8 +42,10 @@ class CORE_EXPORT BlobBytesConsumer final : public BytesConsumer {
   void Trace(Visitor*) const override;
 
  private:
+  class BlobClient;
   Member<ExecutionContext> execution_context_;
   scoped_refptr<BlobDataHandle> blob_data_handle_;
+  Member<BlobBytesConsumer::BlobClient> blob_client_;
   Member<BytesConsumer> nested_consumer_;
   Member<BytesConsumer::Client> client_;
 };
