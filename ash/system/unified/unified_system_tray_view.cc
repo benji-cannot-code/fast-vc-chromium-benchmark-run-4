e9d1e92b683200fe0c2bb7ece16d2474f9bcab11
Ahmed Mehfooz <amehfooz@chromium.org>
2020-07-27 21:14:12 +0000

    Remove redundant layer transforms code from system tray
    The implementation to use layer transforms to resize the bubble
    is no longer used due to recent optimizations. Removing this code
    and the extra view containers that were required for it.
    
    Bug: 1024530
    Change-Id: I71e5cecbc51011b16c0b7d0da09464ea4562eacd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2318693
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#791989}

--- ash/system/unified/unified_system_tray_view.cc
+++ ash/system/unified/unified_system_tray_view.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/border.h"
 #include "ui/views/focus/focus_search.h"
 #include "ui/views/layout/box_layout.h"
-#include "ui/views/layout/fill_layout.h"
 #include "ui/views/painter.h"
 
 namespace ash {
@@ -220,7 +219,6 @@ UnifiedSystemTrayView::UnifiedSystemTrayView(
           std::make_unique<InteractedByTapRecorder>(this)) {
   DCHECK(controller_);
 
-  SetLayoutManager(std::make_unique<views::FillLayout>());
   auto add_layered_child = [](views::View* parent, views::View* child) {
     parent->AddChildView(child);
     child->SetPaintToLayer();
@@ -334,18 +332,9 @@ void UnifiedSystemTrayView::SetExpandedAmount(double expanded_amount) {
   page_indicator_view_->SetExpandedAmount(expanded_amount);
   sliders_container_->SetExpandedAmount(expanded_amount);
 
-  if (!IsTransformEnabled()) {
-    PreferredSizeChanged();
-    // It is possible that the ratio between |message_center_view_| and others
-    // can change while the bubble size remain unchanged.
-    Layout();
-    return;
-  }
-
-  // Note: currently transforms are only enabled when there are no
-  // notifications, so we can consider only the system tray height.
-  if (height() != GetExpandedSystemTrayHeight())
-    PreferredSizeChanged();
+  PreferredSizeChanged();
+  // It is possible that the ratio between |message_center_view_| and others
+  // can change while the bubble size remain unchanged.
   Layout();
 }
 
@@ -380,14 +369,6 @@ int UnifiedSystemTrayView::GetCollapsedSystemTrayHeight() const {
 int UnifiedSystemTrayView::GetCurrentHeight() const {
   return GetPreferredSize().height();
 }
-
-bool UnifiedSystemTrayView::IsTransformEnabled() const {
-  // TODO(amehfooz): Remove transform code completely, the code does not work
-  // and isn't needed after Oshima's performance improvement changes for the
-  // tray.
-  return false;
-}
-
 
 int UnifiedSystemTrayView::GetVisibleFeaturePodCount() const {
   return feature_pods_container_->GetVisibleCount();
@@ -462,10 +443,11 @@ void UnifiedSystemTrayView::OnGestureEvent(ui::GestureEvent* event) {
   }
 }
 
-void UnifiedSystemTrayView::ChildPreferredSizeChanged(views::View* child) {
-  // The size change is not caused by SetExpandedAmount(), because they don't
-  // trigger PreferredSizeChanged().
-  PreferredSizeChanged();
+void UnifiedSystemTrayView::Layout() {
+  if (system_tray_container_->GetVisible())
+    system_tray_container_->SetBoundsRect(GetContentsBounds());
+  else if (detailed_view_container_->GetVisible())
+    detailed_view_container_->SetBoundsRect(GetContentsBounds());
 }
 
 const char* UnifiedSystemTrayView::GetClassName() const {
