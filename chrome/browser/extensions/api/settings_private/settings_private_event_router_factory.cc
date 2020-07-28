179909fb412804b277c8fa45abf89a8905ddb121
sauski <sauski@google.com>
2020-07-28 16:32:54 +0000

    Add GeneratedPrefs as service dependency for SettingsPrivateEventRouter
    The SettingsPrivateEventRouter is written such that it expects to be
    shutdown before the GeneratedPrefs service, this CL makes this
    expectation explicit by declaring a dependency in the service factory.
    
    Bug: 1110305
    Change-Id: Ica1844daa151d1f1fd161e155a313c66e066f325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2323370
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#792277}

--- chrome/browser/extensions/api/settings_private/settings_private_event_router_factory.cc
+++ chrome/browser/extensions/api/settings_private/settings_private_event_router_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/api/settings_private/settings_private_event_router_factory.h"
 
+#include "chrome/browser/extensions/api/settings_private/generated_prefs_factory.h"
 #include "chrome/browser/extensions/api/settings_private/settings_private_delegate_factory.h"
 #include "chrome/browser/extensions/api/settings_private/settings_private_event_router.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
@@ -34,6 +35,7 @@ SettingsPrivateEventRouterFactory::SettingsPrivateEventRouterFactory()
           BrowserContextDependencyManager::GetInstance()) {
   DependsOn(ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
   DependsOn(EventRouterFactory::GetInstance());
+  DependsOn(settings_private::GeneratedPrefsFactory::GetInstance());
   DependsOn(SettingsPrivateDelegateFactory::GetInstance());
 }
 
