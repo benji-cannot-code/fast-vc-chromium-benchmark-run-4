ae71bb3c50e5771ed163a809dd78c0e36df3af78
David Maunder <davidjm@chromium.org>
2020-07-28 20:22:01 +0000

    Disable Server fetcher for incognito tab model
    We should not be enabling the server fetcher for an incognito tab
    model due to privacy constraints.
    
    Bug: 1108571
    Change-Id: I30b99d0fff8b85e3333f8c50f5bd8157f957e2c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315241
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792395}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/suggestions/TabSuggestionsServerFetcher.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/suggestions/TabSuggestionsServerFetcher.java
@@ -64,6 +64,13 @@ public class TabSuggestionsServerFetcher implements TabSuggestionsFetcher {
     @Override
     public void fetch(TabContext tabContext, Callback<TabSuggestionsFetcherResults> callback) {
         try {
+            for (TabContext.TabInfo tabInfo : tabContext.getUngroupedTabs()) {
+                if (tabInfo.isIncognito) {
+                    callback.onResult(
+                            new TabSuggestionsFetcherResults(Collections.emptyList(), tabContext));
+                    return;
+                }
+            }
             EndpointFetcher.fetchUsingChromeAPIKey(res
                     -> { fetchCallback(res, callback, tabContext); },
                     mProfileForTesting == null ? Profile.getLastUsedRegularProfile()
