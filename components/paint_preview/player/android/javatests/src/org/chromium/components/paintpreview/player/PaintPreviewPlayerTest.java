9e7bd0b9c43e8774cb5bbb922b3344ecd7edaebb
ckitagawa <ckitagawa@chromium.org>
2020-07-28 19:58:16 +0000

    [Paint Preview] Enable overscroll test
    The overscollRefreshTest stopped crashing for a while, but then
    returned. I suspect a stale Assert.fail callback was to blame. This
    harness has been largely refactored so we will get a much clearer stack
    trace if there is actually a problem and there are no longer Assert.fail
    callbacks.
    
    Bug: 1093083
    Change-Id: Icb804887c5ba77113fc7d0efaa479e807ccc84ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2324362
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792385}

--- components/paint_preview/player/android/javatests/src/org/chromium/components/paintpreview/player/PaintPreviewPlayerTest.java
+++ components/paint_preview/player/android/javatests/src/org/chromium/components/paintpreview/player/PaintPreviewPlayerTest.java
@@ -26,7 +26,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.CallbackHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.ScalableTimeout;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
 import org.chromium.content_public.browser.test.util.Criteria;
@@ -145,7 +144,6 @@ public class PaintPreviewPlayerTest extends DummyUiActivityTestCase {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1093083")
     public void overscrollRefreshTest() throws Exception {
         initPlayerManager(true);
         UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
