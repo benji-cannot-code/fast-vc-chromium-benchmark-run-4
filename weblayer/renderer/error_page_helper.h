4e90e62fb7e9cfccd36784ad13d85f9b4bc11080
Alex Ilin <alexilin@chromium.org>
2020-07-28 13:00:19 +0000

    Revert "[WebLayer] Use NetErrorAutoReloader"
    This reverts commit 596311e06503a537ac62a79ea493adfd5ea20a41.
    
    Reason for revert: causes flakiness in
    ErrorPageReloadBrowserTest.ReloadOnNetworkChanged test, 
    https://crbug.com/1110192
    
    Original change's description:
    > [WebLayer] Use NetErrorAutoReloader
    > 
    > Navigations which fail with network errors may attempt to
    > auto-reload in some cases, in order to more seamlessly recover
    > from transient failures.
    > 
    > This behavior was previously implemented renderer-side and copied
    > verbatim from Chrome. The Chrome implementation has been moved
    > browser-side (see bug for why) and also made into a reusable
    > component.
    > 
    > This changes WebLayer to use the same component implementation
    > and deletes all the corresponding renderer logic.
    > 
    > Fixed: 1098578
    > Change-Id: I10c337abcae36f64be47f31b89e36c7393989077
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311371
    > Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: Clark DuVall <cduvall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#792028}
    
    TBR=jam@chromium.org,rockot@google.com,cduvall@chromium.org
    
    Bug: 1098578, 1110192
    Change-Id: I7b05267e5bc9c13528607bebfa35beb485b0c965
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2323150
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792215}

--- weblayer/renderer/error_page_helper.h
+++ weblayer/renderer/error_page_helper.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_interstitials/core/common/mojom/interstitial_commands.mojom.h"
 #include "content/public/renderer/render_frame_observer.h"
 #include "content/public/renderer/render_frame_observer_tracker.h"
+#include "content/public/renderer/render_thread_observer.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 
 namespace error_page {
@@ -27,6 +28,7 @@ namespace weblayer {
 class ErrorPageHelper
     : public content::RenderFrameObserver,
       public content::RenderFrameObserverTracker<ErrorPageHelper>,
+      public content::RenderThreadObserver,
       public security_interstitials::SecurityInterstitialPageController::
           Delegate {
  public:
@@ -39,12 +41,25 @@ class ErrorPageHelper
   static ErrorPageHelper* GetForFrame(content::RenderFrame* render_frame);
 
   // Called when the current navigation results in an error.
-  void PrepareErrorPage(const error_page::Error& error);
+  void PrepareErrorPage(const error_page::Error& error, bool was_failed_post);
+
+  // Returns whether a load for the frame the ErrorPageHelper is attached to
+  // should have its error page suppressed.
+  bool ShouldSuppressErrorPage(int error_code);
 
   // content::RenderFrameObserver:
+  void DidStartNavigation(
+      const GURL& url,
+      base::Optional<blink::WebNavigationType> navigation_type) override;
   void DidCommitProvisionalLoad(ui::PageTransition transition) override;
   void DidFinishLoad() override;
+  void OnStop() override;
+  void WasShown() override;
+  void WasHidden() override;
   void OnDestruct() override;
+
+  // content::RenderThreadObserver:
+  void NetworkStateChanged(bool online) override;
 
   // security_interstitials::SecurityInterstitialPageController::Delegate:
   void SendCommand(
@@ -60,6 +75,11 @@ class ErrorPageHelper
   ~ErrorPageHelper() override;
 
   void Reload();
+  void MaybeStartAutoReloadTimer();
+  void StartAutoReloadTimer();
+  void AutoReloadTimerFired();
+  void PauseAutoReloadTimer();
+  void CancelPendingReload();
 
   // Information for the provisional / "pre-provisional" error page. Null when
   // there's no page pending, or the pending page is not an error page.
@@ -68,6 +88,30 @@ class ErrorPageHelper
   // Information for the committed error page. Null when the committed page is
   // not an error page.
   std::unique_ptr<ErrorPageInfo> committed_error_page_info_;
+
+  // Timer used to wait for auto-reload attempts.
+  base::OneShotTimer auto_reload_timer_;
+
+  // True if the auto-reload timer would be running but is waiting for an
+  // offline->online network transition.
+  bool auto_reload_paused_ = false;
+
+  // Whether an auto-reload-initiated Reload() attempt is in flight.
+  bool auto_reload_in_flight_ = false;
+
+  // True if there is an uncommitted-but-started load, error page or not. This
+  // is used to inhibit starting auto-reload when an error page finishes, in
+  // case this happens:
+  //   Error page starts
+  //   Error page commits
+  //   Non-error page starts
+  //   Error page finishes
+  bool uncommitted_load_started_ = false;
+
+  // Is the browser online?
+  bool online_ = false;
+
+  int auto_reload_count_ = 0;
 
   base::WeakPtrFactory<ErrorPageHelper> weak_factory_{this};
 
