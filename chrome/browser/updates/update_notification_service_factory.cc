1c77eae801a276595edd0f8a5e14cec9a682056e
Hesen Zhang <hesen@google.com>
2020-07-28 22:13:29 +0000

    [Update notification service]: Refactor to adopt new scheduler features.
    - Use built-in last shown notification timestamp to check whether to
    show next one.
    - Use built-in stats like number of throttle events to extend the
    throttle interval.
    - Remove all bridge/SharePref getter/setter methods that no longer needed.
    - Add a max_interval as a ceiling to the throttle interval to avoid
    unlimitted extension.
    
    Change-Id: I9eaca883dac245d953351652d6782a4c13404fce
    Bug: 1013685, 1043250
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300632
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792441}

--- chrome/browser/updates/update_notification_service_factory.cc
+++ chrome/browser/updates/update_notification_service_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/singleton.h"
+#include "base/time/default_clock.h"
 #include "chrome/browser/notifications/scheduler/notification_schedule_service_factory.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile_key.h"
@@ -50,7 +51,8 @@ UpdateNotificationServiceFactory::BuildServiceInstanceFor(
   auto bridge =
       std::make_unique<updates::UpdateNotificationServiceBridgeAndroid>();
   return std::make_unique<updates::UpdateNotificationServiceImpl>(
-      schedule_service, std::move(config), std::move(bridge));
+      schedule_service, std::move(config), std::move(bridge),
+      base::DefaultClock::GetInstance());
 }
 
 SimpleFactoryKey* UpdateNotificationServiceFactory::GetKeyToUse(
