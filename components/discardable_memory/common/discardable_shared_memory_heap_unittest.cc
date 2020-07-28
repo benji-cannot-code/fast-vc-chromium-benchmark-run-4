c58d5c3e920b84e3c50686717cfe7794ba52b466
Thiabaud Engelbrecht <thiabaud@google.com>
2020-07-28 00:44:02 +0000

    Adding tracing of freelist size to ClientDiscardableSharedMemoryManager
    Bug: 1106364
    Change-Id: If62ae6d1efdd76fd12f7550c5828c61798a61914
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310899
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/master@{#792098}

--- components/discardable_memory/common/discardable_shared_memory_heap_unittest.cc
+++ components/discardable_memory/common/discardable_shared_memory_heap_unittest.cc
@@ -5,12 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/discardable_memory/common/discardable_shared_memory_heap.h"
 
+#include <inttypes.h>
 #include <stddef.h>
 #include <utility>
 
 #include "base/bind.h"
 #include "base/memory/discardable_shared_memory.h"
 #include "base/process/process_metrics.h"
+#include "base/strings/stringprintf.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace discardable_memory {
@@ -20,7 +23,7 @@ void NullTask() {}
 
 TEST(DiscardableSharedMemoryHeapTest, Basic) {
   size_t block_size = base::GetPageSize();
-  DiscardableSharedMemoryHeap heap(block_size);
+  DiscardableSharedMemoryHeap heap;
 
   // Initial size should be 0.
   EXPECT_EQ(0u, heap.GetSize());
@@ -76,7 +79,7 @@ TEST(DiscardableSharedMemoryHeapTest, Basic) {
 
 TEST(DiscardableSharedMemoryHeapTest, SplitAndMerge) {
   size_t block_size = base::GetPageSize();
-  DiscardableSharedMemoryHeap heap(block_size);
+  DiscardableSharedMemoryHeap heap;
 
   const size_t kBlocks = 6;
   size_t memory_size = block_size * kBlocks;
@@ -133,7 +136,7 @@ TEST(DiscardableSharedMemoryHeapTest, SplitAndMerge) {
 
 TEST(DiscardableSharedMemoryHeapTest, MergeSingleBlockSpan) {
   size_t block_size = base::GetPageSize();
-  DiscardableSharedMemoryHeap heap(block_size);
+  DiscardableSharedMemoryHeap heap;
 
   const size_t kBlocks = 6;
   size_t memory_size = block_size * kBlocks;
@@ -160,7 +163,8 @@ TEST(DiscardableSharedMemoryHeapTest, MergeSingleBlockSpan) {
 
 TEST(DiscardableSharedMemoryHeapTest, Grow) {
   size_t block_size = base::GetPageSize();
-  DiscardableSharedMemoryHeap heap(block_size);
+  DiscardableSharedMemoryHeap heap;
+
   int next_discardable_shared_memory_id = 0;
 
   std::unique_ptr<base::DiscardableSharedMemory> memory1(
@@ -198,7 +202,8 @@ TEST(DiscardableSharedMemoryHeapTest, Grow) {
 
 TEST(DiscardableSharedMemoryHeapTest, ReleaseFreeMemory) {
   size_t block_size = base::GetPageSize();
-  DiscardableSharedMemoryHeap heap(block_size);
+  DiscardableSharedMemoryHeap heap;
+
   int next_discardable_shared_memory_id = 0;
 
   std::unique_ptr<base::DiscardableSharedMemory> memory(
@@ -226,7 +231,8 @@ TEST(DiscardableSharedMemoryHeapTest, ReleaseFreeMemory) {
 
 TEST(DiscardableSharedMemoryHeapTest, ReleasePurgedMemory) {
   size_t block_size = base::GetPageSize();
-  DiscardableSharedMemoryHeap heap(block_size);
+  DiscardableSharedMemoryHeap heap;
+
   int next_discardable_shared_memory_id = 0;
 
   std::unique_ptr<base::DiscardableSharedMemory> memory(
@@ -253,7 +259,7 @@ TEST(DiscardableSharedMemoryHeapTest, ReleasePurgedMemory) {
 
 TEST(DiscardableSharedMemoryHeapTest, Slack) {
   size_t block_size = base::GetPageSize();
-  DiscardableSharedMemoryHeap heap(block_size);
+  DiscardableSharedMemoryHeap heap;
 
   const size_t kBlocks = 6;
   size_t memory_size = block_size * kBlocks;
@@ -288,7 +294,8 @@ void OnDeleted(bool* deleted) {
 
 TEST(DiscardableSharedMemoryHeapTest, DeletedCallback) {
   size_t block_size = base::GetPageSize();
-  DiscardableSharedMemoryHeap heap(block_size);
+  DiscardableSharedMemoryHeap heap;
+
   int next_discardable_shared_memory_id = 0;
 
   std::unique_ptr<base::DiscardableSharedMemory> memory(
@@ -307,7 +314,8 @@ TEST(DiscardableSharedMemoryHeapTest, DeletedCallback) {
 
 TEST(DiscardableSharedMemoryHeapTest, CreateMemoryAllocatorDumpTest) {
   size_t block_size = base::GetPageSize();
-  DiscardableSharedMemoryHeap heap(block_size);
+  DiscardableSharedMemoryHeap heap;
+
   int next_discardable_shared_memory_id = 0;
 
   std::unique_ptr<base::DiscardableSharedMemory> memory(
@@ -335,5 +343,48 @@ TEST(DiscardableSharedMemoryHeapTest, CreateMemoryAllocatorDumpTest) {
                                              pmd.get()));
 }
 
+TEST(DiscardableSharedMemoryHeapTest, OnMemoryDumpTest) {
+  size_t block_size = base::GetPageSize();
+  using testing::ByRef;
+  using testing::Contains;
+  using testing::Eq;
+  DiscardableSharedMemoryHeap heap;
+
+  int next_discardable_shared_memory_id = 0;
+
+  base::trace_event::MemoryDumpArgs args = {
+      base::trace_event::MemoryDumpLevelOfDetail::BACKGROUND};
+  {
+    base::trace_event::ProcessMemoryDump pmd(args);
+    heap.OnMemoryDump(args, &pmd);
+    auto* dump = pmd.GetAllocatorDump(base::StringPrintf(
+        "discardable/child_0x%" PRIXPTR, reinterpret_cast<uintptr_t>(&heap)));
+    ASSERT_NE(nullptr, dump);
+
+    base::trace_event::MemoryAllocatorDump::Entry freelist("freelist_size",
+                                                           "bytes", 0);
+    EXPECT_THAT(dump->entries(), Contains(Eq(ByRef(freelist))));
+  }
+
+  {
+    std::unique_ptr<base::DiscardableSharedMemory> memory(
+        new base::DiscardableSharedMemory);
+    ASSERT_TRUE(memory->CreateAndMap(block_size));
+    std::unique_ptr<DiscardableSharedMemoryHeap::Span> span = heap.Grow(
+        std::move(memory), block_size, next_discardable_shared_memory_id++,
+        base::BindOnce(NullTask));
+    heap.MergeIntoFreeLists(std::move(span));
+    base::trace_event::ProcessMemoryDump pmd(args);
+    heap.OnMemoryDump(args, &pmd);
+    auto* dump = pmd.GetAllocatorDump(base::StringPrintf(
+        "discardable/child_0x%" PRIXPTR, reinterpret_cast<uintptr_t>(&heap)));
+    ASSERT_NE(nullptr, dump);
+
+    base::trace_event::MemoryAllocatorDump::Entry freelist("freelist_size",
+                                                           "bytes", block_size);
+    EXPECT_THAT(dump->entries(), Contains(Eq(ByRef(freelist))));
+  }
+}
+
 }  // namespace
-}  // namespace content
+}  // namespace discardable_memory
