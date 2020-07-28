f61d8144eef8fd76163dec32cc8ca17ec2bc7590
Dave Tapuska <dtapuska@chromium.org>
2020-07-28 22:31:09 +0000

    IME events were not dispatched properly to Pepper.
    A stub implementation for ShouldDispatchImeEventsToPepper was placed
    on FrameWidget but was never implemented anywhere. Some methods called
    the proper Client()->ShouldDispatchImeEventsToPepper code path, but
    others ended up using the stub. Remove the dead code, call the right
    method.
    
    Fallout from https://crrev.com/23748d4a10c7a3cefb87851045250b8b86887938
    
    BUG=1110131
    
    Change-Id: I04884f4d86b1a07b56b65bb49711f3b9ca060b69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2324444
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792444}

--- third_party/blink/renderer/platform/widget/widget_base.cc
+++ third_party/blink/renderer/platform/widget/widget_base.cc
@@ -745,7 +745,7 @@ void WidgetBase::ImeSetComposition(
   FrameWidget* frame_widget = client_->FrameWidget();
   if (!frame_widget)
     return;
-  if (frame_widget->ShouldDispatchImeEventsToPepper()) {
+  if (frame_widget->Client()->ShouldDispatchImeEventsToPepper()) {
     frame_widget->Client()->ImeSetCompositionForPepper(
         text,
         std::vector<ui::ImeTextSpan>(ime_text_spans.begin(),
@@ -778,7 +778,7 @@ void WidgetBase::ImeCommitText(const String& text,
   FrameWidget* frame_widget = client_->FrameWidget();
   if (!frame_widget)
     return;
-  if (frame_widget->ShouldDispatchImeEventsToPepper()) {
+  if (frame_widget->Client()->ShouldDispatchImeEventsToPepper()) {
     frame_widget->Client()->ImeCommitTextForPepper(
         text,
         std::vector<ui::ImeTextSpan>(ime_text_spans.begin(),
@@ -802,7 +802,7 @@ void WidgetBase::ImeFinishComposingText(bool keep_selection) {
   FrameWidget* frame_widget = client_->FrameWidget();
   if (!frame_widget)
     return;
-  if (frame_widget->ShouldDispatchImeEventsToPepper()) {
+  if (frame_widget->Client()->ShouldDispatchImeEventsToPepper()) {
     frame_widget->Client()->ImeFinishComposingTextForPepper(keep_selection);
     return;
   }
