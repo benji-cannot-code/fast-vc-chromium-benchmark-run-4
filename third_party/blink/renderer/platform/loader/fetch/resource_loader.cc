fc28fc6481206788e25609011773735a5f04aac3
Dominic Farolino <dom@chromium.org>
2020-07-28 02:58:38 +0000

    Fix ResourceLoader include style
    As per typical Chromium style, we separate system includes from
    intra-project includes. This CL fixes that for resource_loader.cc in
    Blink.
    
    Bug: N/A
    Change-Id: I6a64b76862df1c41b0e4f9be5ebabd3660c86a85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2321250
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792129}

--- third_party/blink/renderer/platform/loader/fetch/resource_loader.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_loader.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <utility>
+
 #include "base/bind.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
