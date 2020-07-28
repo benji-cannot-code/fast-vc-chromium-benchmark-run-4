307c9a17901e6d9d1368f2a61d25ddb8c54a0a7b
Shik Chen <shik@chromium.org>
2020-07-28 05:34:12 +0000

    CCA: Run getUserMedia() once before enumerateDevices()
    Recently the spec is updated to require a successful getUserMedia()
    before running enumerateDevices(). Otherwise the deviceId and label
    fields would be empty. See https://crbug.com/1101860 for more details.
    
    Bug: 1103303, 1101860
    Test: tast run <nocturne> camera.CCAUI*
    
    Change-Id: I61b4278d20720bf6424dada24a1d4e4b73f8c5d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2321907
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792156}

--- chromeos/components/camera_app_ui/resources/src/js/browser_proxy/browser_proxy_interface.js
+++ chromeos/components/camera_app_ui/resources/src/js/browser_proxy/browser_proxy_interface.js
@@ -15,6 +15,12 @@ import {
  */
 export class BrowserProxy {
   /**
+   * @return {!Promise<boolean>}
+   * @abstract
+   */
+  async requestEnumerateDevicesPermission() {}
+
+  /**
    * @return {!Promise<?AbstractDirectoryEntry>}
    * @abstract
    */
