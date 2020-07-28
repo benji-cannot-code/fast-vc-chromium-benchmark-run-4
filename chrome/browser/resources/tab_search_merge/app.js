911f39bb0187cf25ebf7f21bab9b38ea3d6af910
Roman Arora <romanarora@chromium.org>
2020-07-27 17:11:04 -0700

    Tab Search WebUI: components and styling changes
    Tab Search UI related changes:
    - Custom tab-search-item component that represents each tab item
    - Show only hostname of tab url on tab-search-item
    - Close tab icon for certain list-item states
    - Keyboard navigation for tab-search-items
    
    Bug: 1099917
    Change-Id: Idae91276a66b5b8e2efd7914266ea73107c58d34
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/chrome/browser/resources/tab_search/+/3154044
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819570}

--- chrome/browser/resources/tab_search_merge/app.js
+++ chrome/browser/resources/tab_search_merge/app.js
@@ -4,7 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/icons.m.js';
+import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import 'chrome://resources/polymer/v3_0/iron-selector/iron-selector.js';
+
+import './tab_search_item.js';
+
+import {assert} from 'chrome://resources/js/assert.m.js';
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {TabSearchApiProxy, TabSearchApiProxyImpl} from './tab_search_api_proxy.js';
@@ -35,6 +41,12 @@ export class TabSearchAppElement extends PolymerElement {
         value: '',
       },
 
+      /**
+       * The seleted item's index, or -1 if no item selected.
+       * @private {number}
+       */
+      selectedIndex_: {type: Number, value: -1},
+
       /** @private {?Array<!tabSearch.mojom.WindowTabs>} */
       openTabs_: Array,
 
@@ -48,6 +60,7 @@ export class TabSearchAppElement extends PolymerElement {
 
   constructor() {
     super();
+
     /** @private {!TabSearchApiProxy} */
     this.apiProxy_ = TabSearchApiProxyImpl.getInstance();
   }
@@ -63,21 +76,9 @@ export class TabSearchAppElement extends PolymerElement {
     });
   }
 
-  /**
-   * @param {!Event} e
-   * @private
-   */
-  onSearchInput_(e) {
-    this.searchText_ = e.target.value;
-  }
-
-  /**
-   * @param {!Event} e
-   * @private
-   */
-  onItemClick_(e) {
-    const tabId = parseInt(e.currentTarget.id, 10);
-    this.apiProxy_.switchToTab({tabId});
+  /** @return {number} */
+  getSelectedIndex() {
+    return this.selectedIndex_;
   }
 
   /**
@@ -93,7 +94,89 @@ export class TabSearchAppElement extends PolymerElement {
         result.push(...window.tabs.filter(filterFunc.bind(null, searchText)));
       });
     }
+
+    this.selectedIndex_ = result.length > 0 ? 0 : -1;
     return result;
+  }
+
+  /**
+   * @param {!Event} e
+   * @private
+   */
+  onItemClick_(e) {
+    const tabId = Number.parseInt(e.currentTarget.id, 10);
+    this.apiProxy_.switchToTab({tabId});
+  }
+
+  /**
+   * @param {number} index A valid index for an element present in the
+   *     filteredOpenTabs_ array.
+   * @return {?HTMLElement}
+   * @private
+   */
+  getTabSearchItem_(index) {
+    const tabItemId = assert(this.filteredOpenTabs_[index]).tabId;
+    return this.shadowRoot.getElementById(tabItemId.toString());
+  }
+
+  /**
+   * @param {!KeyboardEvent} e
+   * @private
+   */
+  onKeyDown_(e) {
+    if (this.selectedIndex_ !== -1) {
+      switch (e.key) {
+        case 'ArrowUp':
+          this.selectItem_(-1);
+          break;
+        case 'ArrowDown':
+          this.selectItem_(1);
+          break;
+        case 'Home':
+          this.selectItem_(-this.selectedIndex_);
+          break;
+        case 'End':
+          this.selectItem_(
+              this.filteredOpenTabs_.length - 1 - this.selectedIndex_);
+          break;
+        case 'Enter':
+          const selectedItem = this.filteredOpenTabs_[this.selectedIndex_];
+          this.apiProxy_.switchToTab({tabId: selectedItem.tabId});
+          break;
+      }
+    }
+
+    e.stopPropagation();
+  }
+
+  /**
+   * @param {!Event} e
+   * @private
+   */
+  onSearchInput_(e) {
+    this.searchText_ = e.target.value;
+  }
+
+  /**
+   * @param {number} offset Distance from the desired item to select and the
+   *     currently selected item.
+   * @private
+   */
+  selectItem_(offset) {
+    const length = assert(this.filteredOpenTabs_.length);
+    this.selectedIndex_ = (this.selectedIndex_ + offset + length) % length;
+
+    // Ensure the scroll view can fully display a preceding or following tab
+    // item if existing. Use Math.sign to identify any such preceding or
+    // following item.
+    const scrollToIndex =
+        (this.selectedIndex_ === 0 ||
+         this.selectedIndex_ === this.filteredOpenTabs_.length - 1) ?
+        this.selectedIndex_ :
+        this.selectedIndex_ + Math.sign(offset);
+
+    this.getTabSearchItem_(scrollToIndex)
+        .scrollIntoView({behavior: 'smooth', block: 'nearest'});
   }
 
   /**
