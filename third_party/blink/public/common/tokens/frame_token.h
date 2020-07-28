27397cef5326ff34173189e989d1531a79db431e
Chris Hamilton <chrisha@chromium.org>
2020-07-28 17:27:30 +0000

    Create FrameToken typemaps.
    This makes FrameTokens consistent with WorkerTokens. A further CL will
    migrate usage of base::UnguessableToken to FrameToken.
    
    BUG=1096617
    
    Change-Id: If29b4e50b17b4ead953b378339df214afd619ddd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2314276
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792301}

---
+++ third_party/blink/public/common/tokens/frame_token.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_PUBLIC_COMMON_TOKENS_FRAME_TOKEN_H_
+#define THIRD_PARTY_BLINK_PUBLIC_COMMON_TOKENS_FRAME_TOKEN_H_
+
+#include "base/util/type_safety/token_type.h"
+
+namespace blink {
+
+// Frame token type. Note that this token is type-mapped to its mojom equivalent
+// (defined in third_party/blink/public/mojom/tokens/frame_token.mojom). See
+// frame_token_mojom_traits.h for the StructTraits.
+using FrameToken = util::TokenType<class FrameTokenTypeMarker>;
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_PUBLIC_COMMON_TOKENS_FRAME_TOKEN_H_
