d9cb8c9defb2c2884c1936918e8104c2722be09a
Regan Hsu <hsuregan@google.com>
2020-07-28 00:39:24 +0000

    [CrOS Cellular] Create radio button element for psim/esim setup flow.
    Screenshot: https://screenshot.googleplex.com/XvkUmxNGiM7
    Mock: https://screenshot.googleplex.com/33XZNfWX4ms
    Spec: https://carbon.googleplex.com/cros-ux/pages/lte-setup/settings-approach?q=
    
    * Caller formatted graphics and text are added into the
      slotted-content by the caller.
    * Formatting the position and alignment of cr-card-radio-button
      relative to the cr-radio-group is also up to discretion of caller
      (e.g. caller may want to have cards aligned horizontally or
      vertically).
    
    Bug: 1093185
    Change-Id: I4a0fab53f7bb34e0140b851e896175aa89340254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310878
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792096}

--- ui/webui/resources/cr_elements/cr_radio_group/cr_radio_group.js
+++ ui/webui/resources/cr_elements/cr_radio_group/cr_radio_group.js
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
       selectableElements: {
         type: String,
-        value: 'cr-radio-button, controlled-radio-button',
+        value: 'cr-radio-button, cr-card-radio-button, controlled-radio-button',
       },
 
       /**
