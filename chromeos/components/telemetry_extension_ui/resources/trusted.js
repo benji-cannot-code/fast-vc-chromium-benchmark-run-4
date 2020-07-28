50e3a49e2b9d0a16711cb287011228c8b7186114
Oleh Lamzin <lamzin@google.com>
2020-07-28 16:05:04 +0000

    [Telemetry SWX] Use strict closure flags
    Use strict flags for closure compilation of telemetry extension sources.
    
    Change-Id: Ia5ba8ec67fe16ef3dd8aed68c2d9b74ff198fd7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2320588
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792263}

--- chromeos/components/telemetry_extension_ui/resources/trusted.js
+++ chromeos/components/telemetry_extension_ui/resources/trusted.js
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/**
+ * Pointer to remote implementation of probe service.
+ * @type {!chromeos.health.mojom.ProbeServiceRemote}
+ */
 const probeService = chromeos.health.mojom.ProbeService.getRemote();
 
 const untrustedMessagePipe =
