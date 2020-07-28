e2270f2ac007aee5f47709bbe80fc0b6895b2c17
Hitoshi Yoshida <peria@chromium.org>
2020-07-28 03:00:52 +0000

    Revert "Reland "Add pixel browser tests for focus rings""
    This reverts commit 7ec7300ae47252ff4f1690a8d80f3b209ab5e393.
    
    Reason for revert: This CL still breaks interactive_ui_tests.
    See crbug.com/1110181 for the details.
    
    Bug: 1110181
    
    Original change's description:
    > Reland "Add pixel browser tests for focus rings"
    > 
    > Original Patch: https://chromium-review.googlesource.com/c/chromium/src/+/1986972
    > Revert: https://chromium-review.googlesource.com/c/chromium/src/+/2148472
    > 
    > The original patch was reverted for three reasons:
    > 1. android-pie-x86-rel had slightly different pixel output. I am
    >    addressing this with a fuzzy pixel comparator, and the bot passes in
    >    "Choose Tryjobs".
    > 2. android-arm64-proguard-rel started failing, and surprisingly it
    >    showed that it was using the linux baseline as the expected output.
    >    This must have happened because I left in the logic for separate
    >    marshmallow baselines but didn't provide a separate marshmallow
    >    baseline. This shouldn't happen again now that I removed the check
    >    for a separate marshmallow baseline. Unfortunately this bot does not
    >    appear in "Choose Tryjobs," but I am confident that at the very least
    >    the bot will not fail again for the same reason.
    > 3. Focus rings on different macos versions have different colors. I
    >    fixed this in http://crrev.com/786769 and verified that it works by
    >    running tests on the various macos bots available in "Choose Tryjobs"
    > 
    > This patch also implements the suggestion crbug.com/1091661 and
    > re-enables the tests disabled in that bug.
    > 
    > In the original patch, I made a new file called screenshot_test_utils,
    > but I found another function in browser_test_utils which does the same
    > thing, so this patch doesn't have screenshot_test_utils anymore.
    > 
    > Bug: 1038425, 1022122, 1039866, 1091661
    > Change-Id: Ic1642179bd3ca659f3b4b1f3a25c86a719518ede
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2150765
    > Commit-Queue: Joey Arhar <jarhar@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Mason Freed <masonfreed@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#792092}
    
    TBR=avi@chromium.org,sky@chromium.org,vmpstr@chromium.org,masonfreed@chromium.org,jarhar@chromium.org
    
    Change-Id: I33f8898677410e7674d2f396c5c29e507c52d0a4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1038425
    Bug: 1022122
    Bug: 1039866
    Bug: 1091661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2322006
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792130}

--- content/public/test/browser_test_utils.cc
+++ content/public/test/browser_test_utils.cc
@@ -3614,11 +3614,9 @@ void ProxyDSFObserver::OnCreation(RenderFrameProxyHost* rfph) {
     runner_->Quit();
 }
 
-bool CompareWebContentsOutputToReference(
-    WebContents* web_contents,
-    const base::FilePath& expected_path,
-    const gfx::Size& snapshot_size,
-    const cc::PixelComparator& comparator) {
+bool CompareWebContentsOutputToReference(WebContents* web_contents,
+                                         const base::FilePath& expected_path,
+                                         const gfx::Size& snapshot_size) {
   // Produce a frame of output first to ensure the system is in a consistent,
   // known state.
   {
@@ -3654,7 +3652,8 @@ bool CompareWebContentsOutputToReference(
               SkIRect::MakeWH(snapshot_size.width(), snapshot_size.height()));
 
           snapshot_matches =
-              cc::MatchesPNGFile(clipped_bitmap, expected_path, comparator);
+              cc::MatchesPNGFile(clipped_bitmap, expected_path,
+                                 cc::ManhattanDistancePixelComparator());
 
           // When rebaselining the pixel test, the test may fail. However, the
           // reference file will still be overwritten.
