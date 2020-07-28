19ad3f262d537fab33373d2848e3028000d059ba
Jeff Yoon <jeffyoon@google.com>
2020-07-28 00:11:54 +0000

    [ios] refactor xcode calls, add logging
    Move xcode calls into an xcode util, and add logging for cmd being run.
    Print the test runner class being used for the run in logs so
    we can trace which one's being used without walking through.
    
    TBR=lindsayw@chromium.org
    
    Bug: 1110060
    Change-Id: I1134cfb39a18b05d3072a468d59503924fd264d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2317772
    Commit-Queue: Jeff Yoon <jeffyoon@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792082}

---
+++ ios/build/bots/scripts/xcode_util.py
@@ -0,0 +1,82 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import subprocess
+
+LOGGER = logging.getLogger(__name__)
+
+
+def select(xcode_app_path):
+  """Invoke sudo xcode-select -s {xcode_app_path}
+
+  Raises:
+    subprocess.CalledProcessError on exit codes non zero
+  """
+  cmd = [
+      'sudo',
+      'xcode-select',
+      '-s',
+      xcode_app_path,
+  ]
+  LOGGER.debug("Selecting XCode with command: %s" % cmd)
+
+  output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+  return output
+
+
+def install(mac_toolchain, xcode_build_version, xcode_path):
+  """Invoke mactoolchain to install the given xcode version.
+
+  Args:
+    xcode_build_version: (string) Xcode build version to install.
+    mac_toolchain: (string) Path to mac_toolchain command to install Xcode
+    See https://chromium.googlesource.com/infra/infra/+/master/go/src/infra/cmd/mac_toolchain/
+    xcode_path: (string) Path to install the contents of Xcode.app.
+
+  Raises:
+    subprocess.CalledProcessError on exit codes non zero
+  """
+  cmd = [
+      mac_toolchain,
+      'install',
+      '-kind',
+      'ios',
+      '-xcode-version',
+      xcode_build_version.lower(),
+      '-output-dir',
+      xcode_path,
+  ]
+  LOGGER.debug("Installing xcode with command: %s" % cmd)
+  output = subprocess.check_call(cmd, stderr=subprocess.STDOUT)
+  return output
+
+
+def version():
+  """Invoke xcodebuild -version
+
+  Raises:
+    subprocess.CalledProcessError on exit codes non zero
+
+  Returns:
+    version (12.0), build_version (12a6163b)
+  """
+  cmd = [
+      'xcodebuild',
+      '-version',
+  ]
+  LOGGER.debug("Checking XCode version with command: %s" % cmd)
+
+  output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+  output = output.splitlines()
+  # output sample:
+  # Xcode 12.0
+  # Build version 12A6159
+  logging.info(output)
+
+  version = output[0].decode('UTF-8').split(' ')[1]
+  build_version = output[1].decode('UTF-8').split(' ')[2].lower()
+
+  return version, build_version
