69881303dca01add3bd49c84ddf31d4331fd2c82
Joey Arhar <jarhar@chromium.org>
2020-07-28 00:31:32 +0000

    Reland "Add pixel browser tests for focus rings"
    Original Patch: https://chromium-review.googlesource.com/c/chromium/src/+/1986972
    Revert: https://chromium-review.googlesource.com/c/chromium/src/+/2148472
    
    The original patch was reverted for three reasons:
    1. android-pie-x86-rel had slightly different pixel output. I am
       addressing this with a fuzzy pixel comparator, and the bot passes in
       "Choose Tryjobs".
    2. android-arm64-proguard-rel started failing, and surprisingly it
       showed that it was using the linux baseline as the expected output.
       This must have happened because I left in the logic for separate
       marshmallow baselines but didn't provide a separate marshmallow
       baseline. This shouldn't happen again now that I removed the check
       for a separate marshmallow baseline. Unfortunately this bot does not
       appear in "Choose Tryjobs," but I am confident that at the very least
       the bot will not fail again for the same reason.
    3. Focus rings on different macos versions have different colors. I
       fixed this in http://crrev.com/786769 and verified that it works by
       running tests on the various macos bots available in "Choose Tryjobs"
    
    This patch also implements the suggestion crbug.com/1091661 and
    re-enables the tests disabled in that bug.
    
    In the original patch, I made a new file called screenshot_test_utils,
    but I found another function in browser_test_utils which does the same
    thing, so this patch doesn't have screenshot_test_utils anymore.
    
    Bug: 1038425, 1022122, 1039866, 1091661
    Change-Id: Ic1642179bd3ca659f3b4b1f3a25c86a719518ede
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2150765
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792092}

--- content/public/test/browser_test_utils.h
+++ content/public/test/browser_test_utils.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "build/build_config.h"
+#include "cc/test/pixel_test_utils.h"
 #include "components/viz/common/quads/compositor_frame.h"
 #include "content/public/browser/browser_message_filter.h"
 #include "content/public/browser/notification_observer.h"
@@ -1912,9 +1913,12 @@ class ProxyDSFObserver {
 // ManhattanDistancePixelComparator which allows some small differences.  If
 // the flag switches::kRebaselinePixelTests (--rebaseline-pixel-tests) is set,
 // this function will (over)write the reference file with the produced output.
-bool CompareWebContentsOutputToReference(WebContents* web_contents,
-                                         const base::FilePath& expected_path,
-                                         const gfx::Size& snapshot_size);
+bool CompareWebContentsOutputToReference(
+    WebContents* web_contents,
+    const base::FilePath& expected_path,
+    const gfx::Size& snapshot_size,
+    const cc::PixelComparator& comparator =
+        cc::ManhattanDistancePixelComparator());
 
 }  // namespace content
 
