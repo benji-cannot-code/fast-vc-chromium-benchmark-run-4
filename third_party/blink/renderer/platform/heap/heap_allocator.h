ecb447e54967113adbe01375e4e654b6e4499dd8
Omer Katz <omerkatz@chromium.org>
2020-07-28 17:27:24 +0000

    heap: Refine bailout for concurrent marking
    In practice the bailout is need just for when we trace a backing store.
    A lot of the bailout calls are no longer needed.
    
    Bailout calls remaining after this CL are only for HeapHashTableBacking,
    HeapVectorBacking and tracing inline buffers of vectors.
    
    Bug: 986235
    Change-Id: I7909dd01559ca7bda3dea43e8f05ab8cabae7912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2317190
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792300}

--- third_party/blink/renderer/platform/heap/heap_allocator.h
+++ third_party/blink/renderer/platform/heap/heap_allocator.h
@@ -752,6 +752,8 @@ struct VectorTraits<blink::WeakMember<T>>
   static const bool kCanClearUnusedSlotsWithMemset = true;
   static const bool kCanCopyWithMemcpy = true;
   static const bool kCanMoveWithMemcpy = true;
+
+  static constexpr bool kCanTraceConcurrently = true;
 };
 
 template <typename T>
@@ -762,6 +764,8 @@ struct VectorTraits<blink::UntracedMember<T>>
   static const bool kCanInitializeWithMemset = true;
   static const bool kCanClearUnusedSlotsWithMemset = true;
   static const bool kCanMoveWithMemcpy = true;
+
+  static constexpr bool kCanTraceConcurrently = true;
 };
 
 template <typename T>
