70839c7468de9a74edb734f1d0a82ff40403a9d2
Andrey Kosyakov <caseq@chromium.org>
2020-07-28 00:07:09 +0000

    Revert "Do not create proto database provider for incognito profile."
    This reverts commit e69e60b13e3dc42c89bb4dc4a68c3863258f81aa.
    
    Reason for revert: browser crash, see crbug.com/1110091 for details.
    
    TBR=ssid, nyquist
    BUG=1110091, 1109349
    
    Original change's description:
    > Do not create proto database provider for incognito profile.
    >
    > This is to verify that we currently do not create any database in
    > incognito profile. Support for in-memory database for incognito will be
    > added in future.
    >
    > BUG= 1109349
    >
    > Change-Id: I7dbda9226c207315a4b6b807fac2791f45b89725
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303436
    > Reviewed-by: Bo <boliu@chromium.org>
    > Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    > Commit-Queue: ssid <ssid@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#791419}
    
    TBR=nyquist@chromium.org,boliu@chromium.org,ssid@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1109349
    Change-Id: If66c18de62a5e983ed08040d990cd5f5dd77ac45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2321144
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: ssid <ssid@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792080}

--- content/browser/storage_partition_impl.cc
+++ content/browser/storage_partition_impl.cc
@@ -1641,8 +1641,6 @@ NativeIOContext* StoragePartitionImpl::GetNativeIOContext() {
 
 leveldb_proto::ProtoDatabaseProvider*
 StoragePartitionImpl::GetProtoDatabaseProvider() {
-  if (is_in_memory_)
-    return nullptr;
   if (!proto_database_provider_) {
     proto_database_provider_ =
         std::make_unique<leveldb_proto::ProtoDatabaseProvider>(partition_path_);
