7f4a315714b4c8d4682dc49f626eb2095662ab7f
Jonathan Mengedoht <mengedoht@google.com>
2020-07-28 14:10:16 +0000

    Add WellKnownChangePasswordNavigationThrottle to android
    The NavigationThrottle validates if a site supports
    .well-known/change-password urls and redirects to the origin if not to
    not show the user a 404 page.
    The NavigationThrottle is guarded by the WellKnownChangePassword feature
    flag.
    
    Bug: 927473
    Change-Id: I4c9bf4bc61878096fbfec06cc922d1852a71fb8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316164
    Commit-Queue: Jonathan Mengedoht <mengedoht@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792230}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -151,6 +151,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/login/login_handler.h"
 #include "chrome/browser/ui/login/login_navigation_throttle.h"
 #include "chrome/browser/ui/login/login_tab_helper.h"
+#include "chrome/browser/ui/passwords/well_known_change_password_navigation_throttle.h"
 #include "chrome/browser/ui/prefs/pref_watcher.h"
 #include "chrome/browser/ui/sync/sync_promo_ui.h"
 #include "chrome/browser/ui/tab_contents/chrome_web_contents_view_delegate.h"
@@ -466,7 +467,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/passwords/google_password_manager_navigation_throttle.h"
-#include "chrome/browser/ui/passwords/well_known_change_password_navigation_throttle.h"
 #include "chrome/browser/ui/search/new_tab_page_navigation_throttle.h"
 #include "chrome/browser/webauthn/authenticator_request_scheduler.h"
 #include "chrome/browser/webauthn/chrome_authenticator_request_delegate.h"
@@ -4046,6 +4046,10 @@ ChromeContentBrowserClient::CreateThrottlesForNavigation(
 
   MaybeAddThrottle(TabUnderNavigationThrottle::MaybeCreate(handle), &throttles);
 
+  MaybeAddThrottle(
+      WellKnownChangePasswordNavigationThrottle::MaybeCreateThrottleFor(handle),
+      &throttles);
+
   throttles.push_back(std::make_unique<PolicyBlocklistNavigationThrottle>(
       handle, handle->GetWebContents()->GetBrowserContext()));
 
@@ -4071,9 +4075,6 @@ ChromeContentBrowserClient::CreateThrottlesForNavigation(
       GooglePasswordManagerNavigationThrottle::MaybeCreateThrottleFor(handle),
       &throttles);
 
-  MaybeAddThrottle(
-      WellKnownChangePasswordNavigationThrottle::MaybeCreateThrottleFor(handle),
-      &throttles);
 #endif
 
   throttles.push_back(
