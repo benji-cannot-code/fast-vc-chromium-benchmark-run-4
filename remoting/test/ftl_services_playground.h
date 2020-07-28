16683ea27efdaa7d2b4d7487068e3fe4ba388283
Yuwei Huang <yuweih@chromium.org>
2020-07-28 00:39:03 +0000

    [remoting] Delete GetIceServer
    We never use FTL's PeerToPeerService, so this CL simply removes it from
    FtlServicesPlayground, so that we have one less thing to migrate.
    
    Bug: 1103416
    Change-Id: I48bbea1399c447e681ab5624c7d9db3221bb527d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2321533
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792095}

--- remoting/test/ftl_services_playground.h
+++ remoting/test/ftl_services_playground.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "remoting/base/grpc_support/grpc_authenticated_executor.h"
 #include "remoting/base/oauth_token_getter.h"
 #include "remoting/signaling/ftl_messaging_client.h"
 #include "remoting/signaling/ftl_registration_manager.h"
@@ -41,18 +40,11 @@ class FtlServicesPlayground {
   void StartAndAuthenticate();
 
  private:
-  using PeerToPeer =
-      google::internal::communications::instantmessaging::v1::PeerToPeer;
   using Registration =
       google::internal::communications::instantmessaging::v1::Registration;
 
   void StartLoop();
   void ResetServices(base::OnceClosure on_done);
-
-  void GetIceServer(base::OnceClosure on_done);
-  void OnGetIceServerResponse(base::OnceClosure on_done,
-                              const grpc::Status& status,
-                              const ftl::GetICEServerResponse& response);
 
   void SignInGaia(base::OnceClosure on_done);
   void OnSignInGaiaResponse(base::OnceClosure on_done,
@@ -81,7 +73,6 @@ class FtlServicesPlayground {
 
   std::unique_ptr<test::TestTokenStorage> storage_;
   std::unique_ptr<test::TestOAuthTokenGetter> token_getter_;
-  std::unique_ptr<GrpcAuthenticatedExecutor> executor_;
   std::unique_ptr<network::TransitionalURLLoaderFactoryOwner>
       url_loader_factory_owner_;
 
@@ -91,8 +82,6 @@ class FtlServicesPlayground {
   std::unique_ptr<FtlMessagingClient> messaging_client_;
   std::unique_ptr<FtlMessagingClient::MessageCallbackSubscription>
       message_subscription_;
-
-  std::unique_ptr<PeerToPeer::Stub> peer_to_peer_stub_;
 
   base::OnceClosure receive_messages_done_callback_;
 
