6c85f21951edf4039e0cdc1e64ecec6e54975314
dpapad <dpapad@chromium.org>
2020-07-28 16:20:43 +0000

    PDF Viewer: JS type-checking for more tests.
    Specifically:
     - bookmarks_test
     - material_elements_test
     - params_parser_test
    
    Bug: 1000989
    Change-Id: Ife9caa747482afe05eed471f4a70f85fa77e7cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2321243
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792268}

--- chrome/browser/resources/pdf/elements/viewer-page-selector.js
+++ chrome/browser/resources/pdf/elements/viewer-page-selector.js
@@ -27,9 +27,9 @@ Polymer({
     },
   },
 
-  /** @return {!CrInputElement} */
+  /** @return {!HTMLInputElement} */
   get pageSelector() {
-    return /** @type {!CrInputElement} */ (this.$.pageselector);
+    return /** @type {!HTMLInputElement} */ (this.$.pageselector);
   },
 
   pageNoCommitted() {
