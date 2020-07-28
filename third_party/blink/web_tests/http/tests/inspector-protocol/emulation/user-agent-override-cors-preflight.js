b24f58155c9d2b8e6793dce14c7ba5aab0a8187d
Andrey Kosyakov <caseq@chromium.org>
2020-07-28 19:28:28 +0000

    DevTools: apply User-Agent override when performing a CORS preflight
    Change-Id: Iab48be6f9588bd1ce118892823323dfeae67abb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2320996
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792364}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/emulation/user-agent-override-cors-preflight.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {session, dp} = await testRunner.startBlank('Tests emulation of the user agent for CORS preflight requests.');
+
+  await dp.Network.enable();
+  await dp.Emulation.setUserAgentOverride({userAgent: 'Nutri-Matic Drinks Dispenser'});
+  testRunner.log('navigator.userAgent: ' + await session.evaluate('navigator.userAgent'));
+
+  const url = 'http://localhost:8000/inspector-protocol/network/resources/cors-return-post.php';
+  session.evaluate(`
+      fetch("${url}", {method: 'POST', headers: {'X-DevTools-Test': 'foo'}, body: 'test'})
+  `);
+  const {headers} = (await dp.Network.onceRequestWillBeSentExtraInfo()).params;
+  const headers_to_dump = [
+    'Access-Control-Request-Headers',
+    'User-Agent'
+  ];
+  for (const h of headers_to_dump)
+    testRunner.log(`${h}: ${headers[h]}`);
+
+  testRunner.completeTest();
+})
