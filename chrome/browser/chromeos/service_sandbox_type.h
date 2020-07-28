f5694f56720e8473894dd57ed49702ac51702947
Leo Zhang <googleo@chromium.org>
2020-07-28 13:58:03 +0000

    Correct the sandbox mapping for IME service.
    When WithSandboxType is deprecated, a wrong Mojo interface was set for
    ImeService launching, which invalidates the "ime" sandbox. And
    ImeService's sandbox is set to "utility" by default always.
    
    
    BUG=b:161958233
    TEST="--service-sandbox-type=ime" was set for the ime process.
    
    Change-Id: I14d0d4141fe8fe684935153ca05dee6cdd86fe26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2320448
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792227}

--- chrome/browser/chromeos/service_sandbox_type.h
+++ chrome/browser/chromeos/service_sandbox_type.h
@@ -14,18 +14,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // require a non-utility sandbox can be added here.  See
 // ServiceProcessHost::Launch() for how these templates are consumed.
 
-// chromeos::ime::mojom::InputEngineManager
+// chromeos::ime::mojom::ImeService
 namespace chromeos {
 namespace ime {
 namespace mojom {
-class InputEngineManager;
+class ImeService;
 }  // namespace mojom
 }  // namespace ime
 }  // namespace chromeos
 
 template <>
 inline sandbox::policy::SandboxType
-content::GetServiceSandboxType<chromeos::ime::mojom::InputEngineManager>() {
+content::GetServiceSandboxType<chromeos::ime::mojom::ImeService>() {
   if (chromeos::features::IsImeSandboxEnabled())
     return sandbox::policy::SandboxType::kIme;
 
