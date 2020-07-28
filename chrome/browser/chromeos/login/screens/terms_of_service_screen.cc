ac5bb6ff7b3202f1c22d070ffa0a014e1280f2ab
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-07-28 11:56:03 +0000

    [MGS] Refine Terms of Service Screen
    Previouisly, in case of failure to download the terms of service, we
    were saying "Something went wrong" without a chance for user to
    redownload the ToS in case they are under flaky network.
    This cl makes the text on that screen more verbose and adds "Retry"
    button.
    
    Bug: b/156695308
    Change-Id: Idd934bcd7572732d2b5b03f8b43c8fd937121ba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316202
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792207}

--- chrome/browser/chromeos/login/screens/terms_of_service_screen.cc
+++ chrome/browser/chromeos/login/screens/terms_of_service_screen.cc
@@ -34,6 +34,7 @@ namespace {
 
 constexpr const char kAccept[] = "accept";
 constexpr const char kBack[] = "back";
+constexpr const char kRetry[] = "retry";
 
 }  // namespace
 
@@ -82,6 +83,18 @@ void TermsOfServiceScreen::OnAccept() {
   OnDecline();
 }
 
+void TermsOfServiceScreen::OnRetry() {
+  // If the Terms of Service have been successfully downloaded or are still
+  // being downloaded, this button should not be accessible. If the user managed
+  // to activate it somehow anyway, do not do anything.
+  if (view_ && view_->AreTermsLoaded())
+    return;
+  if (terms_of_service_loader_)
+    return;
+
+  StartDownload();
+}
+
 void TermsOfServiceScreen::OnViewDestroyed(TermsOfServiceScreenView* view) {
   if (view_ == view)
     view_ = nullptr;
@@ -127,6 +140,8 @@ void TermsOfServiceScreen::OnUserAction(const std::string& action_id) {
     OnDecline();
   else if (action_id == kAccept)
     OnAccept();
+  else if (action_id == kRetry)
+    OnRetry();
   else
     BaseScreen::OnUserAction(action_id);
 }
