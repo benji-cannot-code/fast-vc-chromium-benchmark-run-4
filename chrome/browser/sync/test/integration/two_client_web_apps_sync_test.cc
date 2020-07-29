8e5000c3723135e8a7fe6a7f1582c623bb4b2222
Glen Robertson <glenrob@chromium.org>
2020-07-29 08:33:08 +0000

    Add a 'purpose' field to icon_info and propagate through sync.
    Syncing through new BMO system only, not legacy extensions system.
    
    Bug: 1102701
    Change-Id: Ib1eab404518148c7873fa9fb75d1e51e51f06fc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2286111
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792613}

--- chrome/browser/sync/test/integration/two_client_web_apps_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_web_apps_sync_test.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_utils.h"
+#include "third_party/blink/public/common/manifest/manifest.h"
 
 namespace web_app {
 
@@ -414,6 +415,7 @@ IN_PROC_BROWSER_TEST_P(TwoClientWebAppsSyncTest, SyncUsingIconUrlFallback) {
   WebApplicationIconInfo icon_info;
   icon_info.square_size_px = 192;
   icon_info.url = embedded_test_server()->GetURL("/web_apps/blue-192.png");
+  icon_info.purpose = blink::Manifest::ImageResource::Purpose::ANY;
   info.icon_infos.push_back(icon_info);
   AppId app_id = InstallApp(info, GetProfile(0));
   EXPECT_EQ(GetRegistrar(source_profile).GetAppShortName(app_id), "Blue icon");
