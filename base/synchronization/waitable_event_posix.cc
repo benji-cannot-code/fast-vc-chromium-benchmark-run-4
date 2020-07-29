190348d002494d892150b3ffc484ba91b7874f44
Gabriel Charette <gab@chromium.org>
2020-07-29 07:41:29 +0000

    Revert "Add flow trace event to WaitableEvent"
    This reverts commit 6fc1a00ba3230da0ab7c36824ab8dd11a7a34c7b.
    
    Reason for revert: crbug.com/1110742
    
    Original change's description:
    > Add flow trace event to WaitableEvent
    > 
    > Provide more information by adding a trace event to WaitableEvent::Wait.
    > Connect WaitableEvent::Signal with the end of WaitableEvent::TimedWait by a flow event.
    > 
    > Make the implementation of WaitableEvent::Wait cross-platform and move
    > it in waitable_event.cc.
    > 
    > Bug: 1043616
    > Change-Id: I07d13d43e951d6fa223202ccd5f6a076559b416f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2267160
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    > Commit-Queue: Karel Král <karelkral@google.com>
    > Cr-Commit-Position: refs/heads/master@{#791831}
    
    TBR=ajwong@chromium.org,dcheng@chromium.org,gab@chromium.org,altimin@chromium.org,karelkral@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1043616, 1110742
    Change-Id: Ie72c2bef941d9a215d809e553a4b063a320428b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2325379
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792594}

--- base/synchronization/waitable_event_posix.cc
+++ base/synchronization/waitable_event_posix.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "base/time/time_override.h"
-#include "base/trace_event/base_tracing.h"
 
 // -----------------------------------------------------------------------------
 // A WaitableEvent on POSIX is implemented as a wait-list. Currently we don't
@@ -60,8 +59,6 @@ void WaitableEvent::Reset() {
 
 void WaitableEvent::Signal() {
   base::AutoLock locked(kernel_->lock_);
-  TRACE_EVENT_WITH_FLOW0("base", "WaitableEvent::Signal", this,
-                         TRACE_EVENT_FLAG_FLOW_OUT);
 
   if (kernel_->signaled_)
     return;
@@ -157,7 +154,12 @@ class SyncWaiter : public WaitableEvent::Waiter {
   base::ConditionVariable cv_;
 };
 
-bool WaitableEvent::TimedWaitImpl(const TimeDelta& wait_delta) {
+void WaitableEvent::Wait() {
+  bool result = TimedWait(TimeDelta::Max());
+  DCHECK(result) << "TimedWait() should never fail with infinite timeout";
+}
+
+bool WaitableEvent::TimedWait(const TimeDelta& wait_delta) {
   if (wait_delta <= TimeDelta())
     return IsSignaled();
 
@@ -299,7 +301,7 @@ size_t WaitableEvent::WaitMany(WaitableEvent** raw_waitables,
   sw.lock()->Release();
 
   // The address of the WaitableEvent which fired is stored in the SyncWaiter.
-  WaitableEvent* const signaled_event = sw.signaling_event();
+  WaitableEvent *const signaled_event = sw.signaling_event();
   // This will store the index of the raw_waitables which fired.
   size_t signaled_index = 0;
 
--- base/synchronization/waitable_event_win.cc
+++ base/synchronization/waitable_event_win.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "base/time/time_override.h"
-#include "base/trace_event/base_tracing.h"
 
 namespace base {
 
@@ -46,8 +45,6 @@ void WaitableEvent::Reset() {
 }
 
 void WaitableEvent::Signal() {
-  TRACE_EVENT_WITH_FLOW0("base", "WaitableEvent::Signal", this,
-                         TRACE_EVENT_FLAG_FLOW_OUT);
   SetEvent(handle_.Get());
 }
 
@@ -58,7 +55,26 @@ bool WaitableEvent::IsSignaled() {
   return result == WAIT_OBJECT_0;
 }
 
-bool WaitableEvent::TimedWaitImpl(const TimeDelta& wait_delta) {
+void WaitableEvent::Wait() {
+  // Record the event that this thread is blocking upon (for hang diagnosis) and
+  // consider it blocked for scheduling purposes. Ignore this for non-blocking
+  // WaitableEvents.
+  Optional<debug::ScopedEventWaitActivity> event_activity;
+  Optional<internal::ScopedBlockingCallWithBaseSyncPrimitives>
+      scoped_blocking_call;
+  if (waiting_is_blocking_) {
+    event_activity.emplace(this);
+    scoped_blocking_call.emplace(FROM_HERE, BlockingType::MAY_BLOCK);
+  }
+
+  DWORD result = WaitForSingleObject(handle_.Get(), INFINITE);
+  // It is most unexpected that this should ever fail.  Help consumers learn
+  // about it if it should ever fail.
+  DPCHECK(result != WAIT_FAILED);
+  DCHECK_EQ(WAIT_OBJECT_0, result);
+}
+
+bool WaitableEvent::TimedWait(const TimeDelta& wait_delta) {
   if (wait_delta <= TimeDelta())
     return IsSignaled();
 
