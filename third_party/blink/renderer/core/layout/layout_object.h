83c57ac4df31150634959e0e370985731c366db0
Xianzhu Wang <wangxianzhu@chromium.org>
2020-07-29 23:44:33 +0000

    Simplify partial rect invalidation
    After crrev.com/c/2319540, partial invalidation is for LayoutBoxes
    only, and no longer needs to combine with selection invalidation.
    
    Change-Id: I427797f85976f24b3c4c8238437ca1e49a21d840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2321051
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792951}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -283,19 +283,9 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
   using DisplayItemClient::IsValid;
   using DisplayItemClient::GetPaintInvalidationReason;
 
-  void ClearPartialInvalidationVisualRect() const final {
-    return GetMutableForPainting()
-        .FirstFragment()
-        .SetPartialInvalidationVisualRect(IntRect());
-  }
-
   DOMNodeId OwnerNodeId() const final;
 
  public:
-  IntRect PartialInvalidationVisualRect() const final {
-    return FirstFragment().PartialInvalidationVisualRect();
-  }
-
   String DebugName() const final;
 
   // End of DisplayItemClient methods.
@@ -1899,11 +1889,6 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
 
   bool CanBeCompositedForDirectReasons() const;
 
-  // Invalidate the raster of a specific sub-rectangle within the object. The
-  // rect is in the object's local coordinate space. This is useful e.g. when
-  // a small region of a canvas changes.
-  void InvalidatePaintRectangle(const PhysicalRect&);
-
   // Returns the rect that should have raster invalidated whenever this object
   // changes. The rect is in the coordinate space of the document's scrolling
   // contents. This method deals with outlines and overflow.
@@ -2407,12 +2392,6 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
       layout_object_.AddSubtreePaintPropertyUpdateReason(reason);
     }
 
-    void SetPartialInvalidationVisualRect(const IntRect& r) {
-      DCHECK_EQ(layout_object_.GetDocument().Lifecycle().GetState(),
-                DocumentLifecycle::kInPrePaint);
-      FirstFragment().SetPartialInvalidationVisualRect(r);
-    }
-
     void InvalidateClipPathCache() { layout_object_.InvalidateClipPathCache(); }
 
     void UpdateInsideBlockingTouchEventHandler(bool inside) {
@@ -2530,10 +2509,6 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
     SetShouldDoFullPaintInvalidationWithoutGeometryChange(
         PaintInvalidationReason::kBackground);
     bitfields_.SetBackgroundNeedsFullPaintInvalidation(true);
-  }
-
-  PhysicalRect PartialInvalidationLocalRect() const {
-    return fragment_.PartialInvalidationLocalRect();
   }
 
   void InvalidateIfControlStateChanged(ControlState);
