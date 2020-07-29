754630f6d4b2da5beddf48d16af4212f2ba6d34d
Brian Malcolm <bmalcolm@chromium.org>
2020-07-29 23:44:53 +0000

    Add new ExternalPrintServersAllowlist policy
    This is intended to replace the ExternalPrintServersAllowlist policy. The
    old policy will still be used in the absence of the new one for at least
    a few releases of Chrome.
    
    BUG=chromium:1098046
    
    Change-Id: I3e656555cd373a98d0dc0197fea327826720d7b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2325027
    Commit-Queue: Brian Malcolm <bmalcolm@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792952}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -442,7 +442,7 @@ extern const char kUserDataSnapshotRetentionLimit[];
 #endif
 
 #if defined(OS_CHROMEOS)
-extern const char kExternalPrintServersWhitelist[];
+extern const char kExternalPrintServersAllowlist[];
 extern const char kRecommendedNativePrinters[];
 extern const char kRecommendedNativePrintersAccessMode[];
 extern const char kRecommendedNativePrintersBlacklist[];
