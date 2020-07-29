32df6eabf3ca7c997c288175fc65db27d07c0500
Xinghui Lu <xinghuilu@chromium.org>
2020-07-29 22:52:35 +0000

    Add preferences under Safe Browsing standard protection page.
    Add extended reporting and password leak detection toggles under the
    standard protection page. They are clickable only if Chrome is currently
    under standard protection mode. If Chrome is under enhanced protection
    mode, they are forced enabled; If Chrome is under no protection mode,
    they are forced disabled.
    
    If leak detection should be enabled but there is currently no account
    signed in, the toggle will be shown as disabled with an additional
    explanation under the toggle: http://screen/ROmpLDkUNAP
    
    before: http://screen/KdwYdsgcUf5
    after: http://screen/4BXF3e8ZRSg
    
    Bug: 1097310
    Change-Id: I25b60626f4c8b3622e87fe5c98baff22f0c3eea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311030
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792921}

--- chrome/browser/safe_browsing/android/java/src/org/chromium/chrome/browser/safe_browsing/SafeBrowsingBridge.java
+++ chrome/browser/safe_browsing/android/java/src/org/chromium/chrome/browser/safe_browsing/SafeBrowsingBridge.java
@@ -60,6 +60,13 @@ public final class SafeBrowsingBridge {
         SafeBrowsingBridgeJni.get().setSafeBrowsingState(state);
     }
 
+    /**
+     * @return Whether there is a Google account to use for the leak detection check.
+     */
+    public static boolean hasAccountForLeakCheckRequest() {
+        return SafeBrowsingBridgeJni.get().hasAccountForLeakCheckRequest();
+    }
+
     @NativeMethods
     interface Natives {
         int umaValueForFile(String path);
@@ -69,5 +76,6 @@ public final class SafeBrowsingBridge {
         @SafeBrowsingState
         int getSafeBrowsingState();
         void setSafeBrowsingState(@SafeBrowsingState int state);
+        boolean hasAccountForLeakCheckRequest();
     }
 }
--- chrome/browser/safe_browsing/android/safe_browsing_bridge.cc
+++ chrome/browser/safe_browsing/android/safe_browsing_bridge.cc
@@ -7,9 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/safe_browsing/android/jni_headers/SafeBrowsingBridge_jni.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
+#include "components/password_manager/core/browser/leak_detection/authenticated_leak_check.h"
+#include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/safe_browsing/core/file_type_policies.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
 
 using base::android::JavaParamRef;
 
@@ -61,6 +65,15 @@ static void JNI_SafeBrowsingBridge_SetSafeBrowsingState(JNIEnv* env,
                                                         jint state) {
   return safe_browsing::SetSafeBrowsingState(
       GetPrefService(), static_cast<SafeBrowsingState>(state));
+}
+
+static jboolean JNI_SafeBrowsingBridge_HasAccountForLeakCheckRequest(
+    JNIEnv* env) {
+  signin::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForProfile(
+          ProfileManager::GetLastUsedProfile());
+  return password_manager::AuthenticatedLeakCheck::HasAccountForRequest(
+      identity_manager);
 }
 
 }  // namespace safe_browsing
