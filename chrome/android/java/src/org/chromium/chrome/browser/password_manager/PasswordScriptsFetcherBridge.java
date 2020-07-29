11de57f6adf8411375d73c4e9d755af09a993e48
Maxim Kolosovskiy <kolos@chromium.org>
2020-07-29 15:55:13 +0000

    [Password Manager] Prefetch the list of password scripts
    In this CL, the list of scripts is prefetched when leak dialog is shown
    or password settings is open.
    
    Bug: 1095627
    Change-Id: I35f28ec325b5925651f774fc28efd6931d301edf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2325611
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Maxim Kolosovskiy  <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792714}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/PasswordScriptsFetcherBridge.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.password_manager;
+
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.components.embedder_support.browser_context.BrowserContextHandle;
+
+/**
+ * Android bridge to |PasswordScriptsFetcher|.
+ */
+@JNINamespace("password_manager")
+public class PasswordScriptsFetcherBridge {
+    public static void prewarmCache(Profile profile) {
+        PasswordScriptsFetcherBridgeJni.get().prewarmCache(profile);
+    }
+
+    @NativeMethods
+    interface Natives {
+        void prewarmCache(BrowserContextHandle browserContext);
+    }
+}
