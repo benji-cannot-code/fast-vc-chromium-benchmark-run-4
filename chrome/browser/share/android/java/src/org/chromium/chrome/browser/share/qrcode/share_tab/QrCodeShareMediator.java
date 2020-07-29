52382bc370ae683e5bd3145a5651f7c4a984b0ad
Travis Skare <skare@chromium.org>
2020-07-29 03:44:12 +0000

    [Sharing Hub] Don't call QR service with empty URL.
    Bug: 1109649
    Change-Id: I319cebee36eee81087bf8b19f2f04bc0bac8190a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2321766
    Commit-Queue: Travis Skare <skare@chromium.org>
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792518}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/share_tab/QrCodeShareMediator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/share_tab/QrCodeShareMediator.java
@@ -15,6 +15,7 @@ import android.os.Process;
 import android.text.DynamicLayout;
 import android.text.Layout.Alignment;
 import android.text.TextPaint;
+import android.text.TextUtils;
 import android.text.TextUtils.TruncateAt;
 import android.view.View;
 
@@ -71,6 +72,12 @@ class QrCodeShareMediator {
      * @param data The data to encode.
      */
     protected void refreshQrCode(String data) {
+        if (TextUtils.isEmpty(data)) {
+            mPropertyModel.set(QrCodeShareViewProperties.ERROR_STRING,
+                    mContext.getResources().getString(R.string.qr_code_error_unknown));
+            return;
+        }
+
         QRCodeGenerationRequest.QRCodeServiceCallback callback =
                 new QRCodeGenerationRequest.QRCodeServiceCallback() {
                     @Override
@@ -83,7 +90,7 @@ class QrCodeShareMediator {
                                 ChromeFeatureList.CHROME_SHARE_QRCODE, "max_url_length",
                                 /*defaultValue=*/122);
                         String errorMessage;
-                        if (mUrl.length() > maxUrlLength) {
+                        if (data != null && data.length() > maxUrlLength) {
                             errorMessage = mContext.getResources().getString(
                                     R.string.qr_code_error_too_long, maxUrlLength);
                         } else {
