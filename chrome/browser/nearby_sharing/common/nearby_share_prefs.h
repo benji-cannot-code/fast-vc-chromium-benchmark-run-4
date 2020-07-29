50903fae089786ab99a5d1b506fb12d4e17a7996
James Vecore <vecore@google.com>
2020-07-29 22:08:19 +0000

    [Nearby] RELAND Add NearbyShareSettings mojo for use in settings and...
    This is a reland of https://crrev.com/c/2317166 which fixes an issue
    with the post CQ builder: linux-blink-cors-rel. Fixed by adding
    'network::features::kOutOfBlinkCors' to the enabled list in featureList.
    This also fixes an issue in the WATCHLISTS.
    
    Revert is here: https://crrev.com/c/2325433
    
    Original message:
    
    > [Nearby] Add NearbyShareSettings mojo for use in settings and webui
    
    > |NearbyShareSettings| provides a strongly typed wrapper over the raw
    > user prefs and allows clients to observe individual changes. This class
    > also implements a mojo interface to allow multiple WebUIs to access
    > settings (OSSettingsUI, NearbyShareDialog, and eventually SettingsUI).
    >
    > This CL achieves sharing of mojos and the components that depend on them
    > outside of chrome://resources by having the shared resources hosted in
    > chrome://nearby and re-registering them in chrome://os-settings. This
    > gets around having to include explicit references to the mojo generated
    > resources in //content which required a DEPS exception that was not
    > acceptable to //content/OWNERS.
    >
    > Many other options were considered to solve this shared resource
    > problem. Please see the dicussion thread for more context:
    > http://g/chrome-webui/Q8WGiGO7h-E
    >
    > Change-Id: I831044176ed71b15fb7df0be8f1c393e574254f1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2317166
    > Commit-Queue: James Vecore <vecore@google.com>
    > Reviewed-by: James Vecore <vecore@google.com>
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Reviewed-by: Josh Nohle <nohle@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Richard Knoll <knollr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#792503}
    
    Change-Id: I433a4a6292525d50eb7df12d20a8abe81d2949db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2327010
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#792900}

--- chrome/browser/nearby_sharing/common/nearby_share_prefs.h
+++ chrome/browser/nearby_sharing/common/nearby_share_prefs.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NEARBY_SHARING_COMMON_NEARBY_SHARE_PREFS_H_
 #define CHROME_BROWSER_NEARBY_SHARING_COMMON_NEARBY_SHARE_PREFS_H_
 
-namespace user_prefs {
-class PrefRegistrySyncable;
-}  // namespace user_prefs
 
 class PrefRegistrySimple;
 
@@ -19,13 +16,14 @@ extern const char kNearbySharingBackgroundVisibilityName[];
 extern const char kNearbySharingDataUsageName[];
 extern const char kNearbySharingDeviceIdPrefName[];
 extern const char kNearbySharingDeviceNamePrefName[];
+extern const char kNearbySharingAllowedContactsPrefName[];
 extern const char kNearbySharingFullNamePrefName[];
 extern const char kNearbySharingIconUrlPrefName[];
 extern const char kNearbySharingSchedulerDownloadDeviceDataPrefName[];
 extern const char kNearbySharingSchedulerUploadDeviceNamePrefName[];
 }  // namespace prefs
 
-void RegisterNearbySharingPrefs(user_prefs::PrefRegistrySyncable* registry);
+void RegisterNearbySharingPrefs(PrefRegistrySimple* registry);
 
 void RegisterNearbySharingLocalPrefs(PrefRegistrySimple* local_state);
 
