53cd6b9e2473741edb9dd571ceda1d7847e439c4
Roman Sorokin <rsorokin@chromium.org>
2020-07-29 12:12:31 +0000

    cros login: Do not trigger online signin on login and in the session.
    This CL fixes the issue when one invalid "password changed" token
    triggers online reauth two times - on the login screen and in the
    session. Check in the session happens before the new token is backfilled
    with the online login triggered on the login screen.
    So this CL adds backoff time between two consecutive checks.
    Also moved more relative logic into TokenHandleUtil.
    
    Fixed: 1094030
    Change-Id: Ica0a9c547f14e8f0bc743fdab87fff7bf49c56c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300441
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792655}

--- chrome/browser/chromeos/login/signin/signin_error_notifier_ash.h
+++ chrome/browser/chromeos/login/signin/signin_error_notifier_ash.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/auto_reset.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
@@ -31,6 +32,8 @@ class SigninErrorNotifier : public SigninErrorController::Observer,
  public:
   SigninErrorNotifier(SigninErrorController* controller, Profile* profile);
   ~SigninErrorNotifier() override;
+
+  static std::unique_ptr<base::AutoReset<bool>> IgnoreSyncErrorsForTesting();
 
   // KeyedService:
   void Shutdown() override;
