a761d73e197c7042f8ae90773a81b51b7a868f59
Tao Bai <michaelbai@chromium.org>
2020-07-29 21:50:48 +0000

    Autofill: send the visibility to Autofill service.
    Directly use the result of FormFieldData.IsVisible(), since it is
    hard to detect if a field visible to the end user or not, the
    result could be false positive, but once it is set to INVISIBLE,
    it isn't visible to the user.
    
    Bug: 1110041
    Change-Id: Ie745ababf98ce0e195c7cd603fa16665656d5918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2324996
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792889}

--- components/autofill/android/provider/java/src/org/chromium/components/autofill/FormFieldData.java
+++ components/autofill/android/provider/java/src/org/chromium/components/autofill/FormFieldData.java
@@ -50,6 +50,7 @@ public class FormFieldData {
     public final String mHeuristicType;
     public final String[] mDatalistValues;
     public final String[] mDatalistLabels;
+    public final boolean mVisible;
 
     // The bounds in the viewport's coordinates
     private final RectF mBounds;
@@ -67,7 +68,7 @@ public class FormFieldData {
             boolean shouldAutocomplete, String placeholder, String type, String id,
             String[] optionValues, String[] optionContents, boolean isCheckField, boolean isChecked,
             int maxLength, String heuristicType, float left, float top, float right, float bottom,
-            String[] datalistValues, String[] datalistLabels) {
+            String[] datalistValues, String[] datalistLabels, boolean visible) {
         mName = name;
         mLabel = label;
         mValue = value;
@@ -93,6 +94,7 @@ public class FormFieldData {
         mMaxLength = maxLength;
         mHeuristicType = heuristicType;
         mBounds = new RectF(left, top, right, bottom);
+        mVisible = visible;
     }
 
     public @ControlType int getControlType() {
@@ -155,9 +157,11 @@ public class FormFieldData {
             String autocompleteAttr, boolean shouldAutocomplete, String placeholder, String type,
             String id, String[] optionValues, String[] optionContents, boolean isCheckField,
             boolean isChecked, int maxLength, String heuristicType, float left, float top,
-            float right, float bottom, String[] datalistValues, String[] datalistLabels) {
+            float right, float bottom, String[] datalistValues, String[] datalistLabels,
+            boolean visible) {
         return new FormFieldData(name, label, value, autocompleteAttr, shouldAutocomplete,
                 placeholder, type, id, optionValues, optionContents, isCheckField, isChecked,
-                maxLength, heuristicType, left, top, right, bottom, datalistValues, datalistLabels);
+                maxLength, heuristicType, left, top, right, bottom, datalistValues, datalistLabels,
+                visible);
     }
 }
