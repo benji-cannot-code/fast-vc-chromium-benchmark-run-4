0663b8aaf45fc162dde35b097f7c019f22364796
Nicholas Verne <nverne@chromium.org>
2020-07-29 01:53:40 +0000

    Crostin upgrade: re-enable cancel button
    Bug: 1103505
    Change-Id: I162fa82ecd13a43117ce6be1e098591bb3518674
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2320710
    Commit-Queue: David Munro <davidmunro@google.com>
    Reviewed-by: David Munro <davidmunro@google.com>
    Auto-Submit: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792501}

--- chrome/browser/resources/chromeos/crostini_upgrader/app.js
+++ chrome/browser/resources/chromeos/crostini_upgrader/app.js
@@ -342,7 +342,6 @@ Polymer({
    */
   canCancel_(state) {
     switch (state) {
-      case State.UPGRADING:  // TODO(nverne): remove once we have OK from UX.
       case State.BACKUP:
       case State.RESTORE:
       case State.BACKUP_SUCCEEDED:
