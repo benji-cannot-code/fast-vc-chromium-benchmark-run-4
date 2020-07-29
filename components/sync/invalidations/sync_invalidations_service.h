fb2089d67f9b7463aaad54d82ceac989b1729ff3
Rushan Suleymanov <rushans@google.com>
2020-07-29 09:45:42 +0000

    [Sync] Add sync invalidations service
    This patch introduces a dedicated sync invalidations service. It is not
    wired yet and there is no any factory using or creating it.
    
    The service provides a handler which is used to retrieve an FCM token
    and process incoming messages. A listener to pass all received messages
    will be added in a separate CL.
    
    Bug: 1082122
    Change-Id: If998ed53f411b87182b47bed38dcd8dcf463d29b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212275
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792625}

---
+++ components/sync/invalidations/sync_invalidations_service.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SYNC_INVALIDATIONS_SYNC_INVALIDATIONS_SERVICE_H_
+#define COMPONENTS_SYNC_INVALIDATIONS_SYNC_INVALIDATIONS_SERVICE_H_
+
+#include <memory>
+#include <string>
+
+#include "components/keyed_service/core/keyed_service.h"
+
+namespace gcm {
+class GCMDriver;
+}
+
+namespace instance_id {
+class InstanceIDDriver;
+}
+
+namespace syncer {
+class FCMHandler;
+
+// Service which is used to register with FCM. It is used to obtain an FCM token
+// which is used to send invalidations from the server. The service also
+// provides incoming invalidations handling and an interface to subscribe to
+// invalidations.
+class SyncInvalidationsService : public KeyedService {
+ public:
+  SyncInvalidationsService(gcm::GCMDriver* gcm_driver,
+                           instance_id::InstanceIDDriver* instance_id_driver,
+                           const std::string& sender_id,
+                           const std::string& app_id);
+  ~SyncInvalidationsService() override;
+
+  void Shutdown() override;
+
+ private:
+  std::unique_ptr<FCMHandler> fcm_handler_;
+};
+
+}  // namespace syncer
+
+#endif  // COMPONENTS_SYNC_INVALIDATIONS_SYNC_INVALIDATIONS_SERVICE_H_
