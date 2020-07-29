93b90f21abfd65b399a48022f91ee0e4d1283139
Claude van der Merwe <cvandermerwe@google.com>
2020-07-29 05:02:53 +0000

    Add localization for BSSID in avanced network properties rollout
    BSSID was displayed as lower case because of missing localization
    in chromeos_strings.grdp
    
    Bug: 1041699
    Change-Id: Ic16c307d639559a645f84d49dba5d3360b700b6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2324171
    Commit-Queue: Claude van der Merwe <cvandermerwe@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792545}

--- ui/webui/resources/cr_components/chromeos/network/network_property_list_mojo.js
+++ ui/webui/resources/cr_components/chromeos/network/network_property_list_mojo.js
@@ -118,6 +118,8 @@ Polymer({
         result += 'OTP';
       } else if (subKey === 'ssid') {
         result += 'SSID';
+      } else if (subKey === 'bssid') {
+        result += 'BSSID';
       } else if (subKey === 'serverCa') {
         result += 'ServerCA';
       } else if (subKey === 'vpn') {
