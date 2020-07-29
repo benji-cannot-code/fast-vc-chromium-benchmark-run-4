0aaf585a7fb77dbeb91b37591df0ea63211f24d1
Vikas Soni <vikassoni@chromium.org>
2020-07-29 11:45:39 +0000

    Add new VideoFrameMetata CopyMode.
    Remove |copy_required| and replace it with an enum |copy_mode|.
    
    This is done since in future texture copy will not be always required
    for webview and hence |copy_required| will not always be true.
    When AImageReader is enabled, texture copy can be skipped
    and resource sharing across different context can be achieved via
    AHardwareBuffer.
    
    TBR=dcheng@chromium.org
    
    Bug: 1091945
    Change-Id: Ie482546cc2f28128b487a55205a3c7411bc2a86f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2306670
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792652}

--- media/mojo/mojom/media_types_enum_mojom_traits.h
+++ media/mojo/mojom/media_types_enum_mojom_traits.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MEDIA_MOJO_MOJOM_MEDIA_TYPES_ENUM_MOJOM_TRAITS_H_
 
 #include "base/notreached.h"
+#include "media/base/video_frame_metadata.h"
 #include "media/base/video_transformation.h"
 #include "media/mojo/mojom/media_types.mojom-shared.h"
 
@@ -56,6 +57,41 @@ struct EnumTraits<media::mojom::VideoRotation, ::media::VideoRotation> {
     NOTREACHED();
     *output = static_cast<::media::VideoRotation>(input);
     return true;
+  }
+};
+
+template <>
+struct EnumTraits<media::mojom::CopyMode,
+                  ::media::VideoFrameMetadata::CopyMode> {
+  static media::mojom::CopyMode ToMojom(
+      ::media::VideoFrameMetadata::CopyMode input) {
+    switch (input) {
+      case ::media::VideoFrameMetadata::CopyMode::kCopyToNewTexture:
+        return media::mojom::CopyMode::kCopyToNewTexture;
+      case ::media::VideoFrameMetadata::CopyMode::kCopyMailboxesOnly:
+        return media::mojom::CopyMode::kCopyMailboxesOnly;
+    }
+
+    NOTREACHED();
+    return static_cast<media::mojom::CopyMode>(input);
+  }
+
+  // Returning false results in deserialization failure and causes the
+  // message pipe receiving it to be disconnected.
+  static bool FromMojom(media::mojom::CopyMode input,
+                        media::VideoFrameMetadata::CopyMode* output) {
+    switch (input) {
+      case media::mojom::CopyMode::kCopyToNewTexture:
+        *output = ::media::VideoFrameMetadata::CopyMode::kCopyToNewTexture;
+        return true;
+      case media::mojom::CopyMode::kCopyMailboxesOnly:
+        *output = ::media::VideoFrameMetadata::CopyMode::kCopyMailboxesOnly;
+        return true;
+    }
+
+    NOTREACHED();
+    *output = static_cast<::media::VideoFrameMetadata::CopyMode>(input);
+    return false;
   }
 };
 
--- media/mojo/mojom/video_frame_metadata_mojom_traits.h
+++ media/mojo/mojom/video_frame_metadata_mojom_traits.h
@@ -34,10 +34,6 @@ struct StructTraits<media::mojom::VideoFrameMetadataDataView,
     return input.allow_overlay;
   }
 
-  static bool copy_required(const media::VideoFrameMetadata& input) {
-    return input.copy_required;
-  }
-
   static bool end_of_stream(const media::VideoFrameMetadata& input) {
     return input.end_of_stream;
   }
@@ -71,6 +67,11 @@ struct StructTraits<media::mojom::VideoFrameMetadataDataView,
   }
 
   GENERATE_OPT_SERIALIZATION(int, capture_counter, 0)
+
+  GENERATE_OPT_SERIALIZATION(
+      media::VideoFrameMetadata::CopyMode,
+      copy_mode,
+      media::VideoFrameMetadata::CopyMode::kCopyToNewTexture)
 
   GENERATE_OPT_SERIALIZATION(media::VideoRotation,
                              rotation,
