4789c19193cf78fd3ab32d42369b8bfb59e59efd
Brian Sheedy <bsheedy@chromium.org>
2020-07-29 04:25:49 +0000

    Disable minidump symbolization tests on Mac
    Disables the Telemetry minidump symbolization tests on Mac, as they are
    currently failing on some 10.15.5 machines due to Xcode not being set up
    properly for Telemetry tests, and newly reinstalled machines do not have
    a system-wide installation anymore.
    
    TBR=johnchen@chromium.org
    
    Bug: 1110441
    Change-Id: I2e9f3d33a1086db24c07f167f5ab376a2a564146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2324606
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792534}

--- tools/perf/core/minidump_unittest.py
+++ tools/perf/core/minidump_unittest.py
@@ -36,6 +36,7 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
       'chromeos-local',
+      'mac',  #https://crbug.com/1110441
       'win'  # https://crbug.com/1084931
   )
   def testSymbolizeMinidump(self):
@@ -81,6 +82,7 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
       'chromeos-local',
+      'mac',  # https://crbug.com/1110441
       'win'  # https://crbug.com/1084931
   )
   def testMultipleCrashMinidumps(self):
