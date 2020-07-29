90d78abfda44fe0e5ee58454c07babe738e90e60
Robert Ma <robertma@chromium.org>
2020-07-29 18:35:28 +0000

    [WPT] Properly ignore DIR_METADATA and OWNERS in lint
    `wpt lint --ignore-glob` uses fnmatch to match patterns, which means
    1. Patterns must match the full filename (we need a wildcard at the
       beginning).
    2. Path separators are not treated specially (i.e. we need to use
       platform-dependent path separators).
    
    Furthermore, we need to ignore OWNERS files, too, which was forgotten in
    an earlier change.
    
    Add tests to prevent regressions.
    
    Fixed: 1103374
    Change-Id: I62e6ea5ebef900c60b10a325f01683fa5ed836c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2326110
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Auto-Submit: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792792}

--- third_party/blink/web_tests/external/PRESUBMIT_test.py
+++ third_party/blink/web_tests/external/PRESUBMIT_test.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import os
+import shutil
 import subprocess
 import sys
 import unittest
@@ -77,9 +78,13 @@ class MockFile(object):
 class LintWPTTest(unittest.TestCase):
     def setUp(self):
         self._test_file = os.path.join(os.path.dirname(__file__), 'wpt', '_DO_NOT_SUBMIT_.html')
+        self._ignored_directory = os.path.join(os.path.dirname(__file__), 'wpt', 'css', '_DNS_')
 
     def tearDown(self):
-        os.remove(self._test_file)
+        if os.path.exists(self._test_file):
+            os.remove(self._test_file)
+        if os.path.exists(self._ignored_directory):
+            shutil.rmtree(self._ignored_directory)
 
     def testWPTLintSuccess(self):
         with open(self._test_file, 'w') as f:
@@ -99,6 +104,19 @@ class LintWPTTest(unittest.TestCase):
         mock_input.affected_paths = [os.path.abspath(self._test_file)]
         errors = PRESUBMIT._LintWPT(mock_input, mock_output)
         self.assertEqual(len(errors), 1)
+
+    def testWPTLintIgnore(self):
+        os.mkdir(self._ignored_directory)
+        files = []
+        for f in ['DIR_METADATA', 'OWNERS', 'test-expected.txt']:
+            path = os.path.abspath(os.path.join(self._ignored_directory, f))
+            files.append(path)
+            open(path, 'w').close()
+        mock_input = MockInputApi()
+        mock_output = MockOutputApi()
+        mock_input.affected_paths = files
+        errors = PRESUBMIT._LintWPT(mock_input, mock_output)
+        self.assertEqual(len(errors), 0)
 
 
 class DontModifyIDLFilesTest(unittest.TestCase):
