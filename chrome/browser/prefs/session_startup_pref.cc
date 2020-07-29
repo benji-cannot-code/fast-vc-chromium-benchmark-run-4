22785376464984efeffa8b6e3fd898e693adf403
James Cook <jamescook@chromium.org>
2020-07-29 04:16:14 +0000

    lacros: Restore tabs on startup by default
    The Chrome OS default for "On Startup" is "Continue where I left off".
    Use the same default for Lacros.
    
    In theory we would get this for free when Lacros starts using the
    OS_CHROMEOS macro. However, I think the macro conversion process is
    going to start with a bulk conversion of UI-layer Chrome OS code to
    IS_ASH, so I suspect we'll need the IS_LACROS check anyway.
    
    Bug: 1109050
    Test: with a new profile, "Continue where I left off" is the default
    Change-Id: I5015ea9bfcaaa4108a147c0cb3c0347fe0acdabf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316804
    Reviewed-by: Darin Fisher <darin@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792529}

--- chrome/browser/prefs/session_startup_pref.cc
+++ chrome/browser/prefs/session_startup_pref.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/values.h"
 #include "build/build_config.h"
+#include "build/lacros_buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
@@ -58,7 +59,7 @@ void SessionStartupPref::RegisterProfilePrefs(
 
 // static
 SessionStartupPref::Type SessionStartupPref::GetDefaultStartupType() {
-#if defined(OS_CHROMEOS)
+#if defined(OS_CHROMEOS) || BUILDFLAG(IS_LACROS)
   return SessionStartupPref::LAST;
 #else
   return SessionStartupPref::DEFAULT;
