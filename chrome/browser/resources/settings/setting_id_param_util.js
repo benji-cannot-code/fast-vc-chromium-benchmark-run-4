a4ab79ab7623846d31ef4a24471982b8cc219da8
Daniel Classon <dclasson@google.com>
2020-07-29 23:01:19 +0000

    [OsSettingsDeepLinking] Add getSettingIdParameter util function
    Abstracted the getSettingIdParameter from DeepLinkingBehavior so
    that it can be used in both settings/ and settings/chromeos/.
    Refactor DeepLinkingBehavior to use this function.
    
    Bug: 1084154
    Change-Id: I2553d724d421ad34279aade5ce1964ebe9a8f578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2327179
    Commit-Queue: Daniel Classon <dclasson@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792926}

---
+++ chrome/browser/resources/settings/setting_id_param_util.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Utility function for retrieving the deep linked setting ID
+ * from the Url parameter.
+ */
+
+// #import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+// #import {Router} from './router.m.js';
+
+/** @type {string} */
+const SETTING_ID_URL_PARAM_NAME = 'settingId';
+
+/**
+ * Retrieves the setting ID saved in the URL's query parameter. Returns null if
+ * setting ID is unavailable.
+ * @return {?string}
+ */
+/* #export */ function getSettingIdParameter() {
+  // This flag must be enabled for the setting ID to be available.
+  if (!loadTimeData.valueExists('isDeepLinkingEnabled') ||
+      !loadTimeData.getBoolean('isDeepLinkingEnabled')) {
+    return null;
+  }
+
+  return settings.Router.getInstance().getQueryParameters().get(
+      SETTING_ID_URL_PARAM_NAME);
+}
