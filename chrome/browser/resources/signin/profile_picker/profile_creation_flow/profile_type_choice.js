c78ed442daf7135f3aa579f2bf7beca426fb22e3
dizg <dizg@google.com>
2020-07-29 11:31:00 +0000

    [signin] Add title text for profile creation signin promo
    Screenshot: https://drive.google.com/file/d/1z0MLt2zTldb5ZQIzoUc06XTQjS5ID5kx/view?usp=sharing
    
    Bug: 1105865
    Change-Id: Ifccc725b28a2cc1e3d13532e61730633e0c0981c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2320673
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Diana Zagidullina <dizg@google.com>
    Cr-Commit-Position: refs/heads/master@{#792648}

--- chrome/browser/resources/signin/profile_picker/profile_creation_flow/profile_type_choice.js
+++ chrome/browser/resources/signin/profile_picker/profile_creation_flow/profile_type_choice.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
 import 'chrome://resources/cr_elements/cr_icons_css.m.js';
+import '../profile_picker_shared_css.js';
 
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
