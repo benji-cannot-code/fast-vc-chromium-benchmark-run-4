b998b86e5b19341707dcd130287fcce8e4321b64
Takashi Sakamoto <tasak@google.com>
2020-07-29 12:11:24 +0000

    CanvasMemoryDumpProvider should be thread-safe.
    RegisterClient and UnregisterClient methods will be invoked in different threads at the same time. This causes ASAN crashes.
    
    Bug: 1092385
    Change-Id: I67ed7fd1925bde72770c8c5b4f737258e9564de5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2324689
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792654}

--- third_party/blink/renderer/platform/instrumentation/canvas_memory_dump_provider.h
+++ third_party/blink/renderer/platform/instrumentation/canvas_memory_dump_provider.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_INSTRUMENTATION_CANVAS_MEMORY_DUMP_PROVIDER_H_
 
 #include "base/macros.h"
+#include "base/synchronization/lock.h"
 #include "base/trace_event/memory_dump_provider.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -40,7 +41,8 @@ class PLATFORM_EXPORT CanvasMemoryDumpProvider final
  private:
   CanvasMemoryDumpProvider() = default;
 
-  WTF::HashSet<CanvasMemoryDumpClient*> clients_;
+  base::Lock lock_;
+  WTF::HashSet<CanvasMemoryDumpClient*> clients_ GUARDED_BY(lock_);
 
   DISALLOW_COPY_AND_ASSIGN(CanvasMemoryDumpProvider);
 };
