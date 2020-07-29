35460be43aaa065bd37938349cd447d129ae458c
Brandon Wylie <wylieb@chromium.org>
2020-07-29 23:38:56 +0000

    Disabled failing PasswordCheck test
    Bug: 1110965
    Change-Id: I54982da6c008bff04f49a742b6c3fa8eef6fe6d7
    
    TBR=chouinard@chromium.org
    
    Change-Id: I54982da6c008bff04f49a742b6c3fa8eef6fe6d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2327823
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792947}

--- chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
@@ -26,6 +26,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.about_settings.AboutChromeSettings;
@@ -377,6 +378,7 @@ public class MainSettingsFragmentTest {
     @Test
     @SmallTest
     @EnableFeatures({ChromeFeatureList.PASSWORD_CHECK})
+    @DisabledTest(message = "crbug.com/1110965")
     public void testDestroysPasswordCheck() {
         launchSettingsActivity();
         Activity activity = mMainSettings.getActivity();
