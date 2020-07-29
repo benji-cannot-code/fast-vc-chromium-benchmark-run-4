e96337cf6cc327de6a9d272da8f84f8745c47fca
Noah Rose Ledesma <noahrose@google.com>
2020-07-29 23:19:09 +0000

    GMC: Add IPC for switching the audio output device
    This change adds methods used by the global media controls ui for
    switching the audio output device used by a page.
    
    MediaController::SetAudioSinkId was created allowing the
    MediaNotificationService to route the audio output of a
    MediaNotificationSession to a specified audio device.
    
    Mojo interfaces were expanded to allow for this IPC between the
    controller and session.
    
    Bug: 1096243
    Change-Id: Icd0ad17d6ef88ec297444b8b2cbc7ddda6c2c4c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2295799
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/master@{#792935}

--- chrome/browser/ui/global_media_controls/media_notification_service.cc
+++ chrome/browser/ui/global_media_controls/media_notification_service.cc
@@ -222,7 +222,9 @@ bool MediaNotificationService::Session::IsPlaying() {
   return is_playing_;
 }
 
-void MediaNotificationService::Session::SetAudioSinkId(const std::string& id) {}
+void MediaNotificationService::Session::SetAudioSinkId(const std::string& id) {
+  controller_->SetAudioSinkId(id);
+}
 
 // static
 void MediaNotificationService::Session::RecordDismissReason(
