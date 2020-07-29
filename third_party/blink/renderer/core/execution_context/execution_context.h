bf3411cd33e7ef8eae04aa4caab245cc1f2109f3
Nate Chapin <japhet@chromium.org>
2020-07-29 21:19:54 +0000

    Fix window agent SECURITY_CHECK failures in android webview
    WebView has a special mode where the initial empty document of the main
    frame can be reused. This is accomplished by giving the initial empty
    document's security origin universal access. This causes us to reuse
    the WindowAgent created for the initial empty document, but if we
    conduct a security check against that agent, we trigger a SECURITY_CHECK
    in binding_security.cc. We therefore need to ensure that we do not
    reuse the WindowAgent in this special WebView-only case.
    
    Bug: 1006684, 1096140
    
    Change-Id: I00fb74e46a417e36dcc963df5dc2fa2c60932a25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2321707
    Reviewed-by: Andrew Luo <aluo@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792872}

--- third_party/blink/renderer/core/execution_context/execution_context.h
+++ third_party/blink/renderer/core/execution_context/execution_context.h
@@ -378,6 +378,10 @@ class CORE_EXPORT ExecutionContext : public Supplementable<ExecutionContext>,
   explicit ExecutionContext(v8::Isolate* isolate, Agent*);
   ~ExecutionContext() override;
 
+  // Resetting the Agent is only necessary for a special case related to the
+  // GetShouldReuseGlobalForUnownedMainFrame() Setting.
+  void ResetAgent(Agent* agent) { agent_ = agent; }
+
  private:
   // ConsoleLogger implementation.
   void AddConsoleMessageImpl(mojom::ConsoleMessageSource,
@@ -396,7 +400,7 @@ class CORE_EXPORT ExecutionContext : public Supplementable<ExecutionContext>,
 
   SecurityContext security_context_;
 
-  const Member<Agent> agent_;
+  Member<Agent> agent_;
 
   bool DispatchErrorEventInternal(ErrorEvent*, SanitizeScriptErrors);
 
