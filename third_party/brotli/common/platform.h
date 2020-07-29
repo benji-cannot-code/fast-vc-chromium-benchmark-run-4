9cc93891db2335d619c41556d36b3f4f9e1d1514
Avi Drissman <avi@chromium.org>
2020-07-29 06:47:06 +0000

    Migrate to OS_MAC and OS_APPLE in third_party
    Bug: 1105907
    Change-Id: I1784cf675b6bc2bcf7921e4b7535c9ea9cd9e2d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2324897
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#792579}

--- third_party/brotli/common/platform.h
+++ third_party/brotli/common/platform.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <endian.h>
 #elif defined(OS_FREEBSD)
 #include <machine/endian.h>
-#elif defined(OS_MACOSX)
+#elif defined(OS_APPLE)
 #include <machine/endian.h>
 /* Let's try and follow the Linux convention */
 #define BROTLI_X_BYTE_ORDER BYTE_ORDER
