62ab1914491c12da8285953d8d778a51fdb96d19
Joel Hockey <joelhockey@chromium.org>
2020-07-30 05:23:39 +0000

    Register Terminal app by merging prefs with app values
    Previous crrev.com/c/2318945 changed Terminal to have its own
    registration, but it did not correctly merge any existing pref
    values such as last_launch_time.
    
    Changed Registration to always move base::Value in for prefs_.
    
    Change-Id: I0774c07d0e5b9d3bdb4d5057a1996912339cf87e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2326552
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793030}

--- chrome/browser/chromeos/guest_os/guest_os_registry_service.h
+++ chrome/browser/chromeos/guest_os/guest_os_registry_service.h
@@ -72,7 +72,7 @@ class GuestOsRegistryService : public KeyedService {
 
   class Registration {
    public:
-    Registration(const std::string app_id, const base::Value* pref);
+    Registration(const std::string app_id, const base::Value pref);
     Registration(Registration&& registration) = default;
     Registration& operator=(Registration&& registration) = default;
     ~Registration();
