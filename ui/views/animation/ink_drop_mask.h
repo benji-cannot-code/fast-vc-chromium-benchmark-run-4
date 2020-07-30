d49234940afa58d314e3ba9c5b3e952d260fd65e
Sammie Quon <sammiequon@chromium.org>
2020-07-30 21:25:42 +0000

    inkdrop: Remove circle and round rect ink drop masks.
    The last usage is FrameCaptionButton and it was a no-op because there
    was a highlight path generator installed in the constructor.
    
    PathInkDropMask is still used as the default for buttons that do not
    have a mask override or a highlight path generator installed.
    
    The base InkDropMask is still overridden by PathInkDropMask and the
    browser app menu button's PulsingInkDropMask.
    
    Test: existing tests
    Bug: 1056490
    Change-Id: I279d9c4edbe1258bdf414dfb04175f8d7fc4edd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2327957
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793352}

--- ui/views/animation/ink_drop_mask.h
+++ ui/views/animation/ink_drop_mask.h
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_delegate.h"
-#include "ui/gfx/geometry/insets_f.h"
-#include "ui/gfx/geometry/point.h"
-#include "ui/gfx/geometry/rect.h"
 #include "ui/views/views_export.h"
 
 class SkPath;
@@ -40,44 +37,6 @@ class VIEWS_EXPORT InkDropMask : public ui::LayerDelegate {
   ui::Layer layer_;
 
   DISALLOW_COPY_AND_ASSIGN(InkDropMask);
-};
-
-// A rectangular ink drop mask with rounded corners.
-class VIEWS_EXPORT RoundRectInkDropMask : public InkDropMask {
- public:
-  RoundRectInkDropMask(const gfx::Size& layer_size,
-                       const gfx::InsetsF& mask_insets,
-                       float corner_radius);
-
- private:
-  FRIEND_TEST_ALL_PREFIXES(InkDropMaskTest, RoundRectInkDropMaskPaintsRect);
-
-  // InkDropMask:
-  void OnPaintLayer(const ui::PaintContext& context) override;
-
-  gfx::InsetsF mask_insets_;
-  float corner_radius_;
-
-  DISALLOW_COPY_AND_ASSIGN(RoundRectInkDropMask);
-};
-
-// A circular ink drop mask.
-class VIEWS_EXPORT CircleInkDropMask : public InkDropMask {
- public:
-  CircleInkDropMask(const gfx::Size& layer_size,
-                    const gfx::Point& mask_center,
-                    int mask_radius);
-
- private:
-  FRIEND_TEST_ALL_PREFIXES(InkDropMaskTest, CircleInkDropMaskPaintsCircle);
-
-  // InkDropMask:
-  void OnPaintLayer(const ui::PaintContext& context) override;
-
-  gfx::Point mask_center_;
-  int mask_radius_;
-
-  DISALLOW_COPY_AND_ASSIGN(CircleInkDropMask);
 };
 
 // An ink-drop mask that paints a specified path.
