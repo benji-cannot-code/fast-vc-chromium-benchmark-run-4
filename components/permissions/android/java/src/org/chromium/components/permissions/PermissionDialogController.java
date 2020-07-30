13c54e3923f310154c3dce6f26f8337388db6bc7
Clark DuVall <cduvall@chromium.org>
2020-07-30 15:58:30 +0000

    [WebLayer] Fix crash when showing blocked permission interaction dialog
    WebLayer wraps the Activity in a ContextWrapper that allows accessing
    chromium resources. When accessing resources that context needs to be
    used instead of the activity.
    
    Bug: 1111109
    Change-Id: Id2e465264e5dc90591d4a1a8c74b7e786aeba512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2327958
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793197}

--- components/permissions/android/java/src/org/chromium/components/permissions/PermissionDialogController.java
+++ components/permissions/android/java/src/org/chromium/components/permissions/PermissionDialogController.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.permissions;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
@@ -16,6 +15,7 @@ import androidx.annotation.IntDef;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.BuildInfo;
+import org.chromium.base.ContextUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
@@ -145,12 +145,14 @@ public class PermissionDialogController
         assert mState == State.NOT_SHOWING;
 
         mDialogDelegate = mRequestQueue.remove(0);
-        Activity activity = mDialogDelegate.getWindow().getActivity().get();
+        // Use the context to access resources instead of the activity because the activity may not
+        // have the correct resources in some cases (e.g. WebLayer).
+        Context context = mDialogDelegate.getWindow().getContext().get();
 
         // It's possible for the activity to be null if we reach here just after the user
         // backgrounds the browser and cleanup has happened. In that case, we can't show a prompt,
         // so act as though the user dismissed it.
-        if (activity == null) {
+        if (ContextUtils.activityFromContext(context) == null) {
             // TODO(timloh): This probably doesn't work, as this happens synchronously when creating
             // the PermissionPromptAndroid, so the PermissionRequestManager won't be ready yet.
             mDialogDelegate.onDismiss();
@@ -168,7 +170,7 @@ public class PermissionDialogController
         mModalDialogManager = mDialogDelegate.getWindow().getModalDialogManager();
 
         mDialogModel = PermissionDialogModel.getModel(
-                this, mDialogDelegate, () -> showFilteredTouchEventDialog(activity));
+                this, mDialogDelegate, () -> showFilteredTouchEventDialog(context));
         mModalDialogManager.showDialog(mDialogModel, ModalDialogManager.ModalDialogType.TAB);
         mState = State.PROMPT_OPEN;
     }
