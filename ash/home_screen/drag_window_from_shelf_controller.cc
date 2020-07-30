7dfbff9af46285f3176b5cabc8bfc9e75c3f8707
Toshiki Kikuchi <toshikikikuchi@google.com>
2020-07-30 02:23:05 +0000

    splitview: Avoid using animation disabled key to skip auto snapping
    This CL allows AutoSnapController to check if
    DragWindowFromShelfController is restoring windows after dragging.
    AutoSnapController uses kAnimationsDisabledKey to skip auto snapping
    after transient hide & show operations by DragWindowFromShelfController.
    But kAnimationsDisabledKey may be used in other locations so we should
    not rely on it.
    This CL also cleans up auto snapping trigger conditions.
    
    BUG=chromium:1107306
    TEST=DragWindowFromShelfControllerTest.HideWindowDuringWindowDragging*
    TEST=SplitViewControllerTest.AutoSnapFromMinimizedState
    
    Change-Id: Ibc4f72558db3cf0efa070a77e44d3247b79d9ee7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303337
    Commit-Queue: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793002}

--- ash/home_screen/drag_window_from_shelf_controller.cc
+++ ash/home_screen/drag_window_from_shelf_controller.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/splitview/split_view_controller.h"
 #include "ash/wm/splitview/split_view_drag_indicators.h"
 #include "ash/wm/splitview/split_view_utils.h"
+#include "ash/wm/window_properties.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_transient_descendant_iterator.h"
 #include "ash/wm/window_util.h"
@@ -99,13 +100,16 @@ class DragWindowFromShelfController::WindowsHider
 
       hidden_windows_.push_back(window);
       window->AddObserver(this);
+      window->SetProperty(kHideDuringWindowDragging, true);
     }
     window_util::MinimizeAndHideWithoutAnimation(hidden_windows_);
   }
 
   ~WindowsHider() override {
-    for (auto* window : hidden_windows_)
+    for (auto* window : hidden_windows_) {
       window->RemoveObserver(this);
+      window->ClearProperty(kHideDuringWindowDragging);
+    }
     hidden_windows_.clear();
   }
 
@@ -114,6 +118,7 @@ class DragWindowFromShelfController::WindowsHider
       window->RemoveObserver(this);
       ScopedAnimationDisabler disabler(window);
       window->Show();
+      window->ClearProperty(kHideDuringWindowDragging);
     }
     hidden_windows_.clear();
   }
