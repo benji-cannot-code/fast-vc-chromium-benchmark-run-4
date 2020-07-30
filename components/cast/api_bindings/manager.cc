e686c5827fca1c6d1fc8cd12252d480c20062e40
Kevin Marshall <kmarshall@chromium.org>
2020-07-30 23:38:06 +0000

    [chromecast] Relocate BindingsManager, add ScopedApiBinding class.
    Defines a class "ScopedApi" provides RAII-based management of
    injected Chromecast bindings and their MessageChannels.
    
    Move BindingsManager to a component, rename it as
    cast_api_bindings::Manager. An alias subclass is added at its old
    location to allow the new name to be transitioned in a followup CL.
    
    Bug: 1104369
    Change-Id: I1ecaff11dae85a3f62f75056a5c1d6005c3652c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303563
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793429}

--- components/cast/api_bindings/manager.cc
+++ components/cast/api_bindings/manager.cc
@@ -3,42 +3,44 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chromecast/bindings/bindings_manager.h"
+#include "components/cast/api_bindings/manager.h"
 
 #include <utility>
 
 #include "base/logging.h"
 
-namespace chromecast {
-namespace bindings {
+namespace cast_api_bindings {
 
-BindingsManager::BindingsManager() = default;
+Manager::Manager() = default;
 
-BindingsManager::~BindingsManager() {
+Manager::~Manager() {
   DCHECK(port_handlers_.empty());
 }
 
-void BindingsManager::RegisterPortHandler(base::StringPiece port_name,
-                                          MessagePortConnectedHandler handler) {
+void Manager::RegisterPortHandler(base::StringPiece port_name,
+                                  MessagePortConnectedHandler handler) {
   auto result = port_handlers_.try_emplace(port_name, std::move(handler));
   DCHECK(result.second);
 }
 
-void BindingsManager::UnregisterPortHandler(base::StringPiece port_name) {
+void Manager::UnregisterPortHandler(base::StringPiece port_name) {
   size_t deleted = port_handlers_.erase(port_name);
   DCHECK_EQ(deleted, 1u);
 }
 
-void BindingsManager::OnPortConnected(base::StringPiece port_name,
-                                      blink::WebMessagePort port) {
+bool Manager::OnPortConnected(base::StringPiece port_name,
+                              blink::WebMessagePort port) {
+  if (!port.IsValid())
+    return false;
+
   auto handler = port_handlers_.find(port_name);
   if (handler == port_handlers_.end()) {
     LOG(ERROR) << "No handler found for port " << port_name << ".";
-    return;
+    return false;
   }
 
   handler->second.Run(std::move(port));
+  return true;
 }
 
-}  // namespace bindings
-}  // namespace chromecast
+}  // namespace cast_api_bindings
---
+++ components/cast/cast_component_export.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_CAST_CAST_COMPONENT_EXPORT_H_
+#define COMPONENTS_CAST_CAST_COMPONENT_EXPORT_H_
+
+#if defined(COMPONENT_BUILD) && defined(CAST_COMPONENT_IMPLEMENTATION)
+#define CAST_COMPONENT_EXPORT __attribute__((visibility("default")))
+#else  // !defined(COMPONENT_BUILD) ||
+       // !defined(CAST_COMPONENT_EXPORT)
+#define CAST_COMPONENT_EXPORT
+#endif
+
+#endif  // COMPONENTS_CAST_CAST_COMPONENT_EXPORT_H_
