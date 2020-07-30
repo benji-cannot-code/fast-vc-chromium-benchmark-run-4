0254025f8e62345741851c99a4cb2ab56a549d34
Bence Béky <bnc@chromium.org>
2020-07-30 14:33:34 +0000

    Roll src/net/third_party/quiche/src/ e5c11c069..d782fd9e3 (8 commits)
    https://quiche.googlesource.com/quiche.git/+log/e5c11c069f53..d782fd9e320e
    
    $ git log e5c11c069..d782fd9e3 --date=short --no-merges --format='%ad %ae %s'
    2020-07-30 vasilvv Fix a typo in CertificateView that prevented P-256 from working.
    2020-07-29 dschinazi Cleanup uses of QUIC version
    2020-07-29 renjietang Replace gfe2_reloadable_flag_quic_enable_zero_rtt_for_tls with gfe2_restart_flag_quic_enable_zero_rtt_for_tls_v2
    2020-07-29 vasilvv Add support for legacy ECDSA private keys to certificate_view.h
    2020-07-29 fayang QUIC client do not send new request if now is close to idle timeout. Only affecting client, not protected.
    2020-07-29 fayang In QUIC, add a copt to enable overshooting detection. Protected by gfe2_reloadable_flag_quic_enable_overshooting_detection.
    2020-07-29 fayang Make sure MaybeCoalescePacketOfHigherSpace is not re-entrant to avoid infinite loop. Protected by gfe2_reloadable_flag_quic_coalesced_packet_of_higher_space2 which replaces gfe2_reloadable_flag_quic_coalesced_packet_of_higher_space.
    2020-07-29 fayang In quic, respect enable_half_rtt_tail_loss_probe_ when using pto. protected by gfe2_reloadable_flag_quic_use_half_rtt_as_first_pto.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: I2e4ce8ba9b1fc158528ae44d2cdf52b9560a57ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2329246
    Commit-Queue: Renjie Tang <renjietang@chromium.org>
    Reviewed-by: Renjie Tang <renjietang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793155}

--- net/quic/quic_stream_factory.cc
+++ net/quic/quic_stream_factory.cc
@@ -1117,7 +1117,7 @@ QuicStreamFactory::QuicStreamFactory(
   DCHECK(transport_security_state_);
   DCHECK(http_server_properties_);
   if (params_.disable_tls_zero_rtt)
-    SetQuicReloadableFlag(quic_enable_zero_rtt_for_tls, false);
+    SetQuicRestartFlag(quic_enable_zero_rtt_for_tls_v2, false);
   InitializeMigrationOptions();
 }
 
