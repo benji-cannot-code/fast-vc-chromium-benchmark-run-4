2a3a4ebac0f38e9a36e51638cb063816a39e4cac
Josh Nohle <nohle@google.com>
2020-07-30 23:30:54 +0000

    [CryptAuth v2] Register with GCM and fetch metadata during service init
    Register with GCM and fetch ClientAppMetadata as part of the DeviceSync
    service initialization flow. Previously, GCM registration was performed
    by the enrollment manager, and ClientAppMetadata was fetched on demand
    by any class that needed it.
    
    Note: Currently, the GCM registration ID is persisted across restarts,
    and the ClientAppMetadata is lazily created and stored in memory by its
    provider class; so there is already an assumption that the GCM
    registration ID will never change and that the ClientAppMetadata will
    not change during the user's session.
    
    By fetching the ClientAppMetadata every time the service starts, we will
    be able to detect if it has changes since the last successful
    enrollment. For instance, a feature bit in the ClientAppMetadata might
    be newly marked as "supported" behind a feature flag.
    
    In a follow-up CL [3/4], we will directly pass the ClientAppMetadata to
    other classes instead of passing the ClientAppMetadata provider. This
    will remove a lot of technical complexity in place because of the
    ClientAppMetadata provider's asynchronous API.
    
    ---
    
    This is ClientAppMetadata change-detection CL [2/4]:
    
    This chain of CLs relates to the end goal of re-enrolling if
    ClientAppMetadata has changed since the last successful enrollment. For
    example, when Phone Hub become supported, we will want to re-enroll.
    
    We were previously only fetching ClientAppMetadata on demand, for
    instance, right before a periodic enrollment. Now, we need to fetch the
    data every time the service starts up to see if it changed. We continue
    to make the assumption that the data will not change during a user
    session.
    
    Bug: 1105703
    Change-Id: Id629244053b7cf557d01502d1a2ea32aa8029e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2319328
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793422}

--- chromeos/services/device_sync/device_sync_impl.h
+++ chromeos/services/device_sync/device_sync_impl.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/device_sync/device_sync_base.h"
 #include "chromeos/services/device_sync/feature_status_change.h"
 #include "chromeos/services/device_sync/network_request_error.h"
+#include "chromeos/services/device_sync/proto/cryptauth_client_app_metadata.pb.h"
 #include "chromeos/services/device_sync/proto/cryptauth_common.pb.h"
 #include "chromeos/services/device_sync/public/mojom/device_sync.mojom.h"
 #include "chromeos/services/device_sync/remote_device_provider.h"
@@ -62,11 +63,14 @@ class SoftwareFeatureManager;
 // starts an initialization flow with the following steps:
 // (1) Check if the primary user is logged in with a valid account ID.
 // (2) If not, wait for IdentityManager to provide the primary account ID.
-// (3) Instantiate classes which communicate with the CryptAuth back-end.
-// (4) Check enrollment state; if not yet enrolled, enroll the device.
-// (5) When enrollment is valid, listen for device sync updates.
+// (3) Register with GCM.
+// (4) Fetch ClientAppMetadata (CryptAuth v2 only).
+// (5) Instantiate classes which communicate with the CryptAuth back-end.
+// (6) Check enrollment state; if not yet enrolled, enroll the device.
+// (7) When enrollment is valid, listen for device sync updates.
 class DeviceSyncImpl : public DeviceSyncBase,
                        public signin::IdentityManager::Observer,
+                       public CryptAuthGCMManager::Observer,
                        public CryptAuthEnrollmentManager::Observer,
                        public RemoteDeviceProvider::Observer {
  public:
@@ -129,6 +133,9 @@ class DeviceSyncImpl : public DeviceSyncBase,
       GetDevicesActivityStatusCallback callback) override;
   void GetDebugInfo(GetDebugInfoCallback callback) override;
 
+  // CryptAuthGcmManager::Observer:
+  void OnGCMRegistrationResult(bool success) override;
+
   // CryptAuthEnrollmentManager::Observer:
   void OnEnrollmentFinished(bool success) override;
 
@@ -138,7 +145,14 @@ class DeviceSyncImpl : public DeviceSyncBase,
  private:
   friend class DeviceSyncServiceTest;
 
-  enum class Status { FETCHING_ACCOUNT_INFO, WAITING_FOR_ENROLLMENT, READY };
+  enum class InitializationStatus {
+    kNotStarted,
+    kFetchingAccountInfo,
+    kWaitingForGcmRegistration,
+    kWaitingForClientAppMetadata,
+    kWaitingForEnrollment,
+    kReady
+  };
 
   class PendingSetSoftwareFeatureRequest {
    public:
@@ -211,7 +225,18 @@ class DeviceSyncImpl : public DeviceSyncBase,
   void OnUnconsentedPrimaryAccountChanged(
       const CoreAccountInfo& primary_account_info) override;
 
+  // Initialization functions.
+  void RunNextInitializationStep();
+  void FetchAccountInfo();
   void ProcessPrimaryAccountInfo(const CoreAccountInfo& primary_account_info);
+  void RegisterWithGcm();
+  void FetchClientAppMetadata();
+  void OnClientAppMetadataFetchTimeout();
+  void OnClientAppMetadataFetched(
+      const base::Optional<cryptauthv2::ClientAppMetadata>&
+          client_app_metadata);
+  void OnClientAppMetadataFetchFailure();
+  void WaitForValidEnrollment();
   void InitializeCryptAuthManagementObjects();
   void CompleteInitializationAfterSuccessfulEnrollment();
 
@@ -258,9 +283,9 @@ class DeviceSyncImpl : public DeviceSyncBase,
   ClientAppMetadataProvider* client_app_metadata_provider_;
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
   base::Clock* clock_;
-  std::unique_ptr<base::OneShotTimer> set_software_feature_timer_;
+  std::unique_ptr<base::OneShotTimer> timer_;
 
-  Status status_;
+  InitializationStatus status_ = InitializationStatus::kNotStarted;
   CoreAccountInfo primary_account_info_;
   base::flat_map<base::UnguessableToken,
                  std::unique_ptr<PendingSetSoftwareFeatureRequest>>
@@ -272,6 +297,13 @@ class DeviceSyncImpl : public DeviceSyncBase,
       pending_notify_devices_callbacks_;
   base::flat_map<base::UnguessableToken, GetDevicesActivityStatusCallback>
       get_devices_activity_status_callbacks_;
+
+  base::Optional<cryptauthv2::ClientAppMetadata> client_app_metadata_;
+  size_t num_gcm_registration_failures_ = 0;
+  size_t num_client_app_metadata_fetch_failures_ = 0;
+  base::TimeTicks initialization_start_timestamp_;
+  base::TimeTicks gcm_registration_start_timestamp_;
+  base::TimeTicks client_app_metadata_fetch_start_timestamp_;
 
   std::unique_ptr<CryptAuthGCMManager> cryptauth_gcm_manager_;
   std::unique_ptr<CryptAuthClientFactory> cryptauth_client_factory_;
