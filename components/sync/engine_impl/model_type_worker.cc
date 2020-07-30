67cce60df64195353fc8e88a83464d04b068c588
Victor Hugo Vianna Silva <victorvianna@google.com>
2020-07-30 14:55:19 +0000

    Add metrics to evaluate number of undecryptable sync entities
    This CL adds a histogram that records, per data type, what was the
    number of sync entities that remained undecrypted when it was disabled.
    
    Bug: 1109221
    Change-Id: I4b97f99286a85f3f5bd4f3ee4cf8de206ff93d69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2326710
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793168}

--- components/sync/engine_impl/model_type_worker.cc
+++ components/sync/engine_impl/model_type_worker.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/format_macros.h"
 #include "base/guid.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_restrictions.h"
@@ -109,6 +110,10 @@ ModelTypeWorker::ModelTypeWorker(
 }
 
 ModelTypeWorker::~ModelTypeWorker() {
+  base::UmaHistogramCounts1000(
+      std::string("Sync.UndecryptedEntitiesOnDataTypeDisabled.") +
+          ModelTypeToHistogramSuffix(type_),
+      entries_pending_decryption_.size());
   model_type_processor_->DisconnectSync();
 }
 
