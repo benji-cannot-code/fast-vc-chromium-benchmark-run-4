33c4fb1d4374b02944410e7c9539d27b84314e17
Xianzhu Wang <wangxianzhu@chromium.org>
2020-07-30 20:42:15 +0000

    Try to fix paint performance regression after crrev.com/792951
    The CL caused LayoutBox::PartialInvalidationVisualRect to return a
    IntRect with size 1x1 when there was no partial invalidation and
    the paint offset is not integral.
    
    Check for empty rect to avoid the problem.
    
    Bug: 1111222
    Change-Id: Id57607ab8dee0bf0822c5e6f950a326eadd4730c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2330001
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793329}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -2216,6 +2216,8 @@ IntRect LayoutBox::PartialInvalidationVisualRect() const {
   if (!rare_data_)
     return IntRect();
   PhysicalRect rect = rare_data_->partial_invalidation_rect_;
+  if (rect.IsEmpty())
+    return IntRect();
   rect.Move(FirstFragment().PaintOffset());
   return EnclosingIntRect(rect);
 }
