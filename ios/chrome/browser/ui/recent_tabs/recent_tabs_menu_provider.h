9e93673ad08067246fdfc71c1ca0020e2823cc5f
Ewann <ewannpv@google.com>
2020-07-30 14:38:20 +0000

    [iOS] Basic RecentTabs Entry Context Menu
     - Adds a new Context Menu with a "Copy" action for tabs item.
     - Adds a new empty Context Menu for section header.
     - Wraps the old implementation with a feature flag check.
     - Adds metrics for the new context menu and its action.
    
    Bug: 1093302
    Change-Id: I53d698f5480e201423cf5f28add4c16c2b35b9c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2325730
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793158}

---
+++ ios/chrome/browser/ui/recent_tabs/recent_tabs_menu_provider.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_RECENT_TABS_RECENT_TABS_MENU_PROVIDER_H_
+#define IOS_CHROME_BROWSER_UI_RECENT_TABS_RECENT_TABS_MENU_PROVIDER_H_
+
+@class TableViewURLItem;
+
+// Protocol for instances that will provide menus to RecentTabs components.
+@protocol RecentTabsMenuProvider
+
+// Creates a context menu configuration instance for the given |item|.
+- (UIContextMenuConfiguration*)contextMenuConfigurationForItem:
+    (TableViewURLItem*)item API_AVAILABLE(ios(13.0));
+
+// Creates a context menu configuration instance for the header of the given
+// |sectionIdentifier|.
+- (UIContextMenuConfiguration*)
+    contextMenuConfigurationForHeaderWithSectionIdentifier:
+        (NSInteger)sectionIdentifier API_AVAILABLE(ios(13.0));
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_RECENT_TABS_RECENT_TABS_MENU_PROVIDER_H_
