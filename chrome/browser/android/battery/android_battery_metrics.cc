db12c5f91af98608698e8aed258f605e37c80d93
Sami Kyostila <skyostil@chromium.org>
2020-07-30 12:25:40 +0000

    AndroidBatteryMetrics: Add exclusive battery drain metric
    This patch extends the Android battery drain metric to record a parallel
    discharge rate which is only reported for time intervals where Chrome
    was the only foreground app. This avoids accidentally attributing power
    draw from other apps that were running before the user switched to
    Chrome.
    
    We also add a metric to track the time durations between successive
    detected battery capacity drops to get a better idea of what the
    granularity of this data is.
    
    Bug: b/161693081
    Change-Id: Ic5e71799333c4db188ab43a1f23200e84a027884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2322986
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793120}

--- chrome/browser/android/battery/android_battery_metrics.cc
+++ chrome/browser/android/battery/android_battery_metrics.cc
@@ -12,14 +12,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-void Report30SecondDrain(int capacity_consumed) {
+void Report30SecondDrain(int capacity_consumed, bool is_exclusive_measurement) {
   // Drain over the last 30 seconds in uAh. We assume a max current of 10A which
   // translates to a little under 100mAh capacity drain over 30 seconds.
   UMA_HISTOGRAM_COUNTS_100000("Power.ForegroundBatteryDrain.30Seconds",
                               capacity_consumed);
+
+  // Record a separate metric for power drain that was completely observed while
+  // we were the foreground app. This avoids attributing power draw from other
+  // apps to us.
+  if (is_exclusive_measurement) {
+    UMA_HISTOGRAM_COUNTS_100000(
+        "Power.ForegroundBatteryDrain.30Seconds.Exclusive", capacity_consumed);
+  }
 }
 
-void ReportAveragedDrain(int capacity_consumed, int num_sampling_periods) {
+void ReportAveragedDrain(int capacity_consumed,
+                         bool is_exclusive_measurement,
+                         int num_sampling_periods) {
   // Averaged drain over 30 second intervals in uAh. We assume a max current of
   // 10A which translates to a little under 100mAh capacity drain over 30
   // seconds.
@@ -30,6 +40,24 @@ void ReportAveragedDrain(int capacity_consumed, int num_sampling_periods) {
       base::Histogram::FactoryGet(
           kName, /*min_value=*/1, /*max_value=*/100000, /*bucket_count=*/50,
           base::HistogramBase::kUmaTargetedHistogramFlag));
+
+  if (is_exclusive_measurement) {
+    static const char kExclusiveName[] =
+        "Power.ForegroundBatteryDrain.30SecondsAvg.Exclusive";
+    STATIC_HISTOGRAM_POINTER_BLOCK(
+        kExclusiveName,
+        AddCount(capacity_consumed / num_sampling_periods,
+                 num_sampling_periods),
+        base::Histogram::FactoryGet(
+            kName, /*min_value=*/1, /*max_value=*/100000, /*bucket_count=*/50,
+            base::HistogramBase::kUmaTargetedHistogramFlag));
+  }
+
+  // Also report the time it took for us to detect this drop to see what the
+  // overall metric sensitivity is.
+  UMA_HISTOGRAM_LONG_TIMES_100(
+      "Power.ForegroundBatteryDrain.TimeBetweenEvents",
+      base::TimeDelta::FromSeconds(30 * num_sampling_periods));
 }
 
 }  // namespace
@@ -76,6 +104,7 @@ void AndroidBatteryMetrics::UpdateDrainMetricsEnabled() {
     last_remaining_capacity_uah_ =
         base::PowerMonitor::GetRemainingBatteryCapacity();
     skipped_timers_ = 0;
+    observed_capacity_drops_ = 0;
 
     drain_metrics_timer_.Start(FROM_HERE, kDrainMetricsInterval, this,
                                &AndroidBatteryMetrics::CaptureAndReportDrain);
@@ -96,20 +125,22 @@ void AndroidBatteryMetrics::CaptureAndReportDrain() {
     // device reports bogus values. We don't change last_remaining_capacity_uah_
     // here to avoid overreporting in case of fluctuating values.
     skipped_timers_++;
-    Report30SecondDrain(0);
+    Report30SecondDrain(0, IsMeasuringDrainExclusively());
     return;
   }
+  observed_capacity_drops_++;
 
   // Report the consumed capacity delta over the last 30 seconds.
   int capacity_consumed = last_remaining_capacity_uah_ - remaining_capacity_uah;
-  Report30SecondDrain(capacity_consumed);
+  Report30SecondDrain(capacity_consumed, IsMeasuringDrainExclusively());
 
   // Also record drain over 30 second intervals, but averaged since the last
   // time we recorded an increase (or started recording samples). Because the
   // underlying battery capacity counter is often low-resolution (usually
   // between .5 and 50 mAh), it may only increment after multiple sampling
   // points.
-  ReportAveragedDrain(capacity_consumed, skipped_timers_ + 1);
+  ReportAveragedDrain(capacity_consumed, IsMeasuringDrainExclusively(),
+                      skipped_timers_ + 1);
 
   // Also track the total capacity consumed in a single-bucket-histogram,
   // emitting one sample for every 100 uAh drained.
@@ -117,7 +148,16 @@ void AndroidBatteryMetrics::CaptureAndReportDrain() {
   UMA_HISTOGRAM_SCALED_EXACT_LINEAR("Power.ForegroundBatteryDrain",
                                     /*sample=*/1, capacity_consumed,
                                     /*sample_max=*/1, kSampleFactor);
+  if (IsMeasuringDrainExclusively()) {
+    UMA_HISTOGRAM_SCALED_EXACT_LINEAR("Power.ForegroundBatteryDrain.Exclusive",
+                                      /*sample=*/1, capacity_consumed,
+                                      /*sample_max=*/1, kSampleFactor);
+  }
 
   last_remaining_capacity_uah_ = remaining_capacity_uah;
   skipped_timers_ = 0;
+}
+
+bool AndroidBatteryMetrics::IsMeasuringDrainExclusively() const {
+  return observed_capacity_drops_ >= 2;
 }
