7ad500fc5aeb260255be743b3a1698d92e2daf94
Reilly Grant <reillyg@chromium.org>
2020-07-30 20:44:05 +0000

    serial: Add policy control for the guard content setting
    This change adds basic support for controlling access to the Serial API
    through enterprise policy. Administrators can disable the API by default
    on some or all origins and enable it again for a select set of origins.
    
    Bug: 1001242
    Change-Id: Ide33c5f2b749121ee7039ce2b2e0c4e33c4c527a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2321002
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793331}

--- components/content_settings/core/common/pref_names.h
+++ components/content_settings/core/common/pref_names.h
@@ -35,6 +35,7 @@ extern const char kManagedDefaultMediaStreamSetting[];
 extern const char kManagedDefaultWebBluetoothGuardSetting[];
 extern const char kManagedDefaultWebUsbGuardSetting[];
 extern const char kManagedDefaultLegacyCookieAccessSetting[];
+extern const char kManagedDefaultSerialGuardSetting[];
 
 extern const char kManagedCookiesAllowedForUrls[];
 extern const char kManagedCookiesBlockedForUrls[];
@@ -56,6 +57,8 @@ extern const char kManagedWebUsbAllowDevicesForUrls[];
 extern const char kManagedWebUsbAskForUrls[];
 extern const char kManagedWebUsbBlockedForUrls[];
 extern const char kManagedLegacyCookieAccessAllowedForDomains[];
+extern const char kManagedSerialAskForUrls[];
+extern const char kManagedSerialBlockedForUrls[];
 
 extern const char kEnableQuietNotificationPermissionUi[];
 
