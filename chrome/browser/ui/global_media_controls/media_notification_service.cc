227a3b962c585fceb184714f916a50baf2fea6c5
Toni Baržić <tbarzic@chromium.org>
2020-07-30 02:50:46 +0000

    Revert "GMC: Add IPC for switching the audio output device"
    This reverts commit f300ac056d303b94ed2c5dd216de14587959ad67.
    
    Reason for revert: Breaking chrome OS build:
    https://crbug.com/1111185
    
    BUG=1111185
    
    Original change's description:
    > GMC: Add IPC for switching the audio output device
    > 
    > This change adds methods used by the global media controls ui for
    > switching the audio output device used by a page.
    > 
    > MediaController::SetAudioSinkId was created allowing the
    > MediaNotificationService to route the audio output of a
    > MediaNotificationSession to a specified audio device.
    > 
    > Mojo interfaces were expanded to allow for this IPC between the
    > controller and session.
    > 
    > Bug: 1096243
    > Change-Id: Icd0ad17d6ef88ec297444b8b2cbc7ddda6c2c4c9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2295799
    > Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Reviewed-by: Sean Topping <seantopping@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    > Cr-Commit-Position: refs/heads/master@{#792935}
    
    TBR=dcheng@chromium.org,rijubrata.bhaumik@intel.com,sergeyu@chromium.org,seantopping@chromium.org,beccahughes@chromium.org,steimel@chromium.org,noahrose@google.com
    
    Change-Id: I526e44f992450f4bfff9b7eebd85e6b8380df812
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1096243
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2327805
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793005}

--- chrome/browser/ui/global_media_controls/media_notification_service.cc
+++ chrome/browser/ui/global_media_controls/media_notification_service.cc
@@ -222,9 +222,7 @@ bool MediaNotificationService::Session::IsPlaying() {
   return is_playing_;
 }
 
-void MediaNotificationService::Session::SetAudioSinkId(const std::string& id) {
-  controller_->SetAudioSinkId(id);
-}
+void MediaNotificationService::Session::SetAudioSinkId(const std::string& id) {}
 
 // static
 void MediaNotificationService::Session::RecordDismissReason(
