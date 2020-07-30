7e5dddce912f913739f647f8387e5e281f6e5d1d
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2020-07-30 15:31:02 +0000

    Update JavaDoc about DisableIf and Restriction annotations
    Bug: None
    Change-Id: Ic31d39ee8be716b4829049db7770ffc1fd1c3273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2325428
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793184}

--- ui/android/javatests/src/org/chromium/ui/test/util/UiRestriction.java
+++ ui/android/javatests/src/org/chromium/ui/test/util/UiRestriction.java
@@ -5,10 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.ui.test.util;
 
+import org.chromium.base.test.util.Restriction;
+
 /**
  * UiRestrictions list form factor restrictions, that are usable with
- * the @Restriction enum in layers depending on //ui.
- * E.g. @Restriction({UiRestriction.RESTRICTION_TYPE_PHONE})
+ * the {@link Restriction} annotation in layers depending on //ui.
+ * E.g. <code>
+ *   \@Restriction({UiRestriction.RESTRICTION_TYPE_PHONE})
+ * </code>
  */
 
 public final class UiRestriction {
