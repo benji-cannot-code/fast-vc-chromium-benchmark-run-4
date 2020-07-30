1eba01aaf186fb3481078ecb994534b9175deff5
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-07-30 05:50:27 +0000

    [base] Ranges: Improve begin() and end()
    This change improves util::ranges::begin() and end() by adding constexpr
    support and making them prefer a member function over a free standing
    function. This serves to close the gap to std::ranges::begin() and
    std::ranges::end() as specified by the C++20 standard.
    
    Furthermore, special constexpr support is added for std::array, which
    otherwise only has constexpr support in C++17.
    
    Lastly, it moves the functions from the iterator.h to the ranges.h
    header, since in C++20 these functions are present in <ranges>, not
    <iterator>.
    
    Bug: 1071094
    Change-Id: Id5f3b7ff83b3fb336286f4ed2d4fe8a6cfcf0b9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2325171
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793034}

--- base/util/ranges/algorithm.h
+++ base/util/ranges/algorithm.h
@@ -14,20 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/util/ranges/functional.h"
 #include "base/util/ranges/iterator.h"
 #include "base/util/ranges/ranges.h"
+#include "base/util/ranges/ranges_internal.h"
 
 namespace util {
+
 namespace ranges {
 
 namespace internal {
-
-// Helper to express preferences in an overload set. If more than one overload
-// are available for a given set of parameters the overload with the higher
-// priority will be chosen.
-template <size_t I>
-struct priority_tag : priority_tag<I - 1> {};
-
-template <>
-struct priority_tag<0> {};
 
 // Returns a transformed version of the unary predicate `pred` applying `proj`
 // to its argument before invoking `pred` on it.
