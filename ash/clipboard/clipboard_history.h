495eda7515aaf417ad10108b5026942e6f84fd4e
David Black <dmblack@google.com>
2020-07-30 18:31:33 +0000

    Debounce clipboard copies that occur in the same task sequence.
    Problem:
    Sometimes multiple copies to the clipboard occur in the same task
    sequence. The user is likely only aware of the most recent copy, so
    seeing previous copies that occurred would be strange in history.
    
    Example:
    User copies the URL from the address bar in the browser.
    First the short form of the URL is copied to the clipboard.
    Then the long form of the URL is copied to the clipboard.
    The user probably only expects to see the most recent copy in history.
    
    Solution:
    Debounce our history commits by queuing them to the end of the task
    sequence. This should filter out programmatic copies that happen in
    rapid succession.
    
    Bug: 1109367
    Change-Id: Ibef538cd15793702ea11e5ff8bbf398cbc62e4a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2317754
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793258}

--- ash/clipboard/clipboard_history.h
+++ ash/clipboard/clipboard_history.h
@@ -65,7 +65,8 @@ class ASH_EXPORT ClipboardHistory : public ui::ClipboardObserver,
   void OnClipboardDataChanged() override;
 
  private:
-  void CommitData(ui::ClipboardData data);
+  // Adds |data| to the clipboard history associated with |account_id|.
+  void CommitData(const AccountId& account_id, ui::ClipboardData data);
   void Pause();
   void Resume();
 
@@ -81,6 +82,9 @@ class ASH_EXPORT ClipboardHistory : public ui::ClipboardObserver,
   // Clipboard history is mapped by account ID to store different items per
   // account when multiprofile is used. Lists of items are sorted by recency.
   std::map<AccountId, std::list<ui::ClipboardData>> items_by_account_id_;
+
+  // Factory to create WeakPtrs used to debounce calls to CommitData().
+  base::WeakPtrFactory<ClipboardHistory> commit_data_weak_factory_{this};
 };
 
 }  // namespace ash
