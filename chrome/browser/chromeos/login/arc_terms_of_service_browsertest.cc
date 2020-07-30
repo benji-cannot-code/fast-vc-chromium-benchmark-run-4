0dde057860d1d05c23a9bae0d2ced9aeba3cf2eb
Balazs Engedy <engedy@chromium.org>
2020-07-30 12:43:34 +0000

    Revert "MultiDeviceSetupScreen: Skip screen properly."
    This reverts commit 6549ee059a00ac7c0e103de089326edf6b996db2.
    
    Reason for revert: Likely culprit for crash/timeout on:
    
    MultiDeviceSetupScreenTest.Accepted
    MultiDeviceSetupScreenTest.Declined
    MultiDeviceSetupScreenTest.Skipped
    
    https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome/7383
    
    Original change's description:
    > MultiDeviceSetupScreen: Skip screen properly.
    > 
    > Use new MaybeSkip method for skipping screen. Add tests for UMA stats.
    > 
    > Bug: 1064561, 1095062
    > Change-Id: I9ba91dd8b132367a160b975f724b5e1692987832
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166263
    > Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    > Cr-Commit-Position: refs/heads/master@{#793103}
    
    TBR=rsorokin@chromium.org,raleksandrov@google.com
    
    Change-Id: Ic190563d2e266731d1fab4684b23adae5f487163
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1064561
    Bug: 1095062
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2329424
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793124}

--- chrome/browser/chromeos/login/arc_terms_of_service_browsertest.cc
+++ chrome/browser/chromeos/login/arc_terms_of_service_browsertest.cc
@@ -725,6 +725,7 @@ class PublicAccountArcTermsOfServiceScreenTest
 IN_PROC_BROWSER_TEST_F(PublicAccountArcTermsOfServiceScreenTest,
                        SkippedForPublicAccount) {
   StartPublicSession();
+  ShowArcTosScreen();
 
   chromeos::test::WaitForPrimaryUserSessionStart();
   histogram_tester_.ExpectTotalCount(
--- chrome/browser/chromeos/login/wizard_controller.h
+++ chrome/browser/chromeos/login/wizard_controller.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/screens/hid_detection_screen.h"
 #include "chrome/browser/chromeos/login/screens/kiosk_autolaunch_screen.h"
 #include "chrome/browser/chromeos/login/screens/marketing_opt_in_screen.h"
-#include "chrome/browser/chromeos/login/screens/multidevice_setup_screen.h"
 #include "chrome/browser/chromeos/login/screens/network_screen.h"
 #include "chrome/browser/chromeos/login/screens/packaged_license_screen.h"
 #include "chrome/browser/chromeos/login/screens/recommend_apps_screen.h"
@@ -269,7 +268,7 @@ class WizardController {
   void OnRecommendAppsScreenExit(RecommendAppsScreen::Result result);
   void OnAppDownloadingScreenExit();
   void OnAssistantOptInFlowScreenExit(AssistantOptInFlowScreen::Result result);
-  void OnMultiDeviceSetupScreenExit(MultiDeviceSetupScreen::Result result);
+  void OnMultiDeviceSetupScreenExit();
   void OnGestureNavigationScreenExit(GestureNavigationScreen::Result result);
   void OnMarketingOptInScreenExit(MarketingOptInScreen::Result result);
   void OnResetScreenExit();
