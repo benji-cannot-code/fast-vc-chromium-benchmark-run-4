e0a816db422b6a0bc6b1c11841f2143c3504a392
Albert Chaulk <achaulk@chromium.org>
2020-07-30 22:11:42 +0000

    Change IS_CHROMECAST to BUILDFLAG(IS_CHROMECAST)
    Bug: b/158233592
    Test: internal build
    Change-Id: I5550f397cc5c73025f071bf33e44813b75aa9198
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2324554
    Commit-Queue: Albert Chaulk <achaulk@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Auto-Submit: Albert Chaulk <achaulk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793379}

--- gpu/command_buffer/service/feature_info.cc
+++ gpu/command_buffer/service/feature_info.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "build/build_config.h"
+#include "build/chromecast_buildflags.h"
 #include "gpu/command_buffer/service/gpu_switches.h"
 #include "gpu/command_buffer/service/texture_definition.h"
 #include "gpu/config/gpu_switches.h"
@@ -194,7 +195,7 @@ FeatureInfo::FeatureInfo(
           .status_values[GPU_FEATURE_TYPE_ANDROID_SURFACE_CONTROL] ==
       gpu::kGpuFeatureStatusEnabled;
 
-#if defined(OS_CHROMEOS) || defined(IS_CHROMECAST)
+#if defined(OS_CHROMEOS) || BUILDFLAG(IS_CHROMECAST)
   feature_flags_.chromium_image_ycbcr_420v = base::Contains(
       gpu_feature_info.supported_buffer_formats_for_allocation_and_texturing,
       gfx::BufferFormat::YUV_420_BIPLANAR);
