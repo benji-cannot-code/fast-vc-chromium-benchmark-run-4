05f7f6ca27061ab95bfb7ee3ecdcd9da899d3c6f
Yuwei Huang <yuweih@chromium.org>
2020-07-30 21:06:40 +0000

    [remoting] Migrate TelemetryLogWriter to Protobuf over HTTP
    This CL makes the TelemetryLogWriter (used by mobile clients) to send
    logs using Protobuf over HTTP.
    
    Bug: 1103416
    Change-Id: I7a99cd1b94d1d7cac50ff88407b0632525309502
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2328350
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793345}

--- remoting/base/protobuf_http_test_responder.h
+++ remoting/base/protobuf_http_test_responder.h
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_BASE_PROTOBUF_HTTP_TEST_RESPONDER_H_
 #define REMOTING_BASE_PROTOBUF_HTTP_TEST_RESPONDER_H_
 
+#include <memory>
 #include <string>
 #include <vector>
 
 #include "base/memory/scoped_refptr.h"
+#include "base/test/mock_callback.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
 
@@ -26,6 +28,9 @@ class ProtobufHttpStatus;
 // Helper class to send responses to Protobuf HTTP requests.
 class ProtobufHttpTestResponder final {
  public:
+  using MockInterceptor =
+      base::MockCallback<network::TestURLLoaderFactory::Interceptor>;
+
   ProtobufHttpTestResponder();
   ~ProtobufHttpTestResponder();
 
@@ -80,8 +85,19 @@ class ProtobufHttpTestResponder final {
 
   std::string GetMostRecentRequestUrl();
 
+  // Installs (if called for the first time) and returns a mock interceptor
+  // callback that you can add expectations:
+  //
+  //   EXPECT_CALL(test_responder_.GetMockInterceptor(), Run(_))
+  //       .WillOnce([](const ResourceRequest& request) {...});
+  //
+  // Note that if you call this without passing it to EXPECT_CALL() or ON_CALL()
+  // then the test will fail immediately as long as any request is sent.
+  MockInterceptor& GetMockInterceptor();
+
  private:
   network::TestURLLoaderFactory test_url_loader_factory_;
+  std::unique_ptr<MockInterceptor> mock_interceptor_;
 };
 
 }  // namespace remoting
