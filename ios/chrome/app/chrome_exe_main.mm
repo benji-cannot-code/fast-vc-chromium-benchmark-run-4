7957086b45905b9ea308c55272aea4e18bca46fb
Olivier Robin <olivierrobin@google.com>
2020-07-30 06:21:52 +0000

    Allow dumping Chrome sandboxes.
    It can be very useful when developing features to access the sandboxes.
    This CL adds the possibility to dump the sandboxes to the Document
    directory, which is accessible via the finder.
    
    Change-Id: I0f760fdf59350dc85744048f374b8585392cc670
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316365
    Reviewed-by: Satoru Takabayashi <satorux@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793043}

--- ios/chrome/app/chrome_exe_main.mm
+++ ios/chrome/app/chrome_exe_main.mm
@@ -10,9 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "components/crash/core/common/crash_keys.h"
 #include "ios/chrome/app/startup/ios_chrome_main.h"
+#include "ios/chrome/app/startup/ios_enable_sandbox_dump_buildflags.h"
 #include "ios/chrome/browser/crash_report/breakpad_helper.h"
 #include "ios/chrome/common/channel_info.h"
 #include "ios/testing/perf/startupLoggers.h"
+
+#if BUILDFLAG(IOS_ENABLE_SANDBOX_DUMP)
+#include "ios/chrome/app/startup/sandbox_dump.h"  // nogncheck
+#endif  // BUILDFLAG(IOS_ENABLE_SANDBOX_DUMP)
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -73,6 +78,13 @@ int RunUIApplicationMain(int argc, char* argv[]) {
 int main(int argc, char* argv[]) {
   IOSChromeMain::InitStartTime();
   startup_loggers::RegisterAppStartTime();
+
+#if BUILDFLAG(IOS_ENABLE_SANDBOX_DUMP)
+  // Dumps the sandbox if needed. This must be called as soon as possible,
+  // before actions are done on the sandbox.
+  // This is a blocking call.
+  DumpSandboxIfRequested();
+#endif  // BUILDFLAG(IOS_ENABLE_SANDBOX_DUMP)
 
   // Set NSUserDefaults keys to force pseudo-RTL if needed.
   SetTextDirectionIfPseudoRTLEnabled();
---
+++ ios/chrome/app/startup/sandbox_dump.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_APP_STARTUP_SANDBOX_DUMP_H_
+#define IOS_CHROME_APP_STARTUP_SANDBOX_DUMP_H_
+
+// Dumps the sandboxed directory accessible by Chrome to the document directory.
+// The dump is then accessible using finder.
+// The document directory is not copied as it is already accessible.
+// Some files or directories cannot be copied, so they are silently skipped.
+// This function makes file operation on main thread and can block.
+void DumpSandboxIfRequested();
+
+#endif  // IOS_CHROME_APP_STARTUP_SANDBOX_DUMP_H_
