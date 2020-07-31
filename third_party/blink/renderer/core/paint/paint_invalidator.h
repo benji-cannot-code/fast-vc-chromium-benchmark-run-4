c59113f41ef355ac947ba4e0905288cda4e72bdb
Xianzhu Wang <wangxianzhu@chromium.org>
2020-07-31 23:19:25 +0000

    Use border box or layout overflow rect for layout shift tracking
    This CL addresses the following issues of layout shift tracking:
    - Changed visual overflows triggered layout shift report
      (originally filed as crbug.com/1090763).
    - Descendant layout overflows were not included in ancestor's layout
      shift report.
    - Unnecessary descendant layout shift report.
    - Slow visual rect calculation for LayoutInline and LayoutText.
    
    It includes the following changes
    - Track LayoutBoxes and LayoutTexts only.
    - Track both old and new starting points and visual representations
      instead of using location of visual rects as starting points.
    - For a LayoutBox, the local visual representation is the layout
      overflow rect if the LayoutBox has layout overflow and doesn't clip
      overflow. Otherwise use the border box rect.
    - For a LayoutText, suppose its shift causes shift of all contents
      in containing block logically below the LayoutText, and use that
      part of visual representation of the containing block as the visual
      representation of the shift.
    - Map both local starting points and local visual representations to
      the initial containing block and viewport.
    
    This has the following benefits:
    - More accurate starting points and visual representations, conforming
      to the specs;
    - Better performance by
      * avoiding unnecessary layout shift reports of descendants
      * avoiding slow LayoutInline and LayoutText visual rect calculation.
    - sizeof(LayoutObject) is reduced by 16 bytes because now we use the
      previous geometry data in LayoutBoxes for paint invalidation.
    
    Bug: 1108622
    Change-Id: I7ebb4e250e7ca2a03b4dadea50034e300e55ad84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2319538
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793806}

--- third_party/blink/renderer/core/paint/paint_invalidator.h
+++ third_party/blink/renderer/core/paint/paint_invalidator.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_PAINT_INVALIDATOR_H_
 
 #include "third_party/blink/renderer/core/layout/layout_object.h"
+#include "third_party/blink/renderer/core/layout/layout_shift_tracker.h"
 #include "third_party/blink/renderer/core/paint/paint_property_tree_builder.h"
 #include "third_party/blink/renderer/platform/geometry/layout_rect.h"
 #include "third_party/blink/renderer/platform/graphics/paint_invalidation_reason.h"
@@ -104,6 +105,8 @@ struct CORE_EXPORT PaintInvalidatorContext {
  private:
   friend class PaintInvalidator;
 
+  base::Optional<LayoutShiftTracker::ContainingBlockScope>
+      containing_block_scope_;
   const PaintPropertyTreeBuilderFragmentContext* tree_builder_context_ =
       nullptr;
 };
@@ -138,6 +141,9 @@ class PaintInvalidator {
   ALWAYS_INLINE void UpdateForPaintOffsetChange(const LayoutObject&,
                                                 FragmentData&,
                                                 PaintInvalidatorContext&);
+  ALWAYS_INLINE void UpdateForLayoutShiftTracking(const LayoutObject&,
+                                                  FragmentData&,
+                                                  PaintInvalidatorContext&);
 
   Vector<const LayoutObject*> pending_delayed_paint_invalidations_;
 };
