3825eb12fd548551ff4e1784e34cc0c0c9205a61
Maxim Kolosovskiy <kolos@chromium.org>
2020-07-31 09:21:43 +0000

    [Password Manager] Change password with script button
    This CL adds a new credential layout corresponding to a compromised
    credential for a website where the password can be changed via a script.
    This new layout replaces the "Change password" button with 2 buttons:
    one for automatic change and one for manual.
    
    UI mock: https://docs.google.com/presentation/d/1jsRsqvW8ZasT2iJGu-_8ebY3bPDe6ic8x8touqeNjuo/edit#slide=id.g8e7b42d005_0_45
    
    This CL
    - introduces |hasScript| field in |CompromisedCredential|
    - introduces new |PasswordCheckProperties.ItemType| -
    COMPROMISED_CREDENTIAL_WITH_SCRIPT
    - adds layout for the credentials of the new item type (the new layout
    contains additional button)
    - adds strings for the new button's caption and its explanation
    - tests whether the new layout is inflated.
    
    TODOs:
    - add methods to CredentialsEventHandler to handle clicks on buttons
    - propagate the data about whether a given domain has script or not.
    
    
    Bug: 1086114, 1092444
    Change-Id: I9aae4772e4e529d5510bf0ecb0db3d7127f62b72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2326570
    Commit-Queue: Maxim Kolosovskiy  <kolos@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Auto-Submit: Maxim Kolosovskiy  <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793550}

--- chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckProperties.java
+++ chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckProperties.java
@@ -56,7 +56,8 @@ class PasswordCheckProperties {
         private HeaderProperties() {}
     }
 
-    @IntDef({ItemType.HEADER, ItemType.COMPROMISED_CREDENTIAL})
+    @IntDef({ItemType.HEADER, ItemType.COMPROMISED_CREDENTIAL,
+            ItemType.COMPROMISED_CREDENTIAL_WITH_SCRIPT})
     @Retention(RetentionPolicy.SOURCE)
     @interface ItemType {
         /**
@@ -68,6 +69,12 @@ class PasswordCheckProperties {
          * A section containing a user's name and password.
          */
         int COMPROMISED_CREDENTIAL = 2;
+
+        /**
+         * A section containing a user's name and password for a domain where a password change
+         * script is available.
+         */
+        int COMPROMISED_CREDENTIAL_WITH_SCRIPT = 3;
     }
 
     /**
