d4d2582ec704643595e78ac77bf09fabbceab997
Peng Huang <penghuang@chromium.org>
2020-07-31 12:14:11 +0000

    Reland "ExternalVkImageBacking: reuse more VkSemaphores."
    This is a reland of 0b8256601986b62998e48d2396131994984df010
    
    Original change's description:
    > ExternalVkImageBacking: reuse more VkSemaphores.
    > 
    > This CL change SharedImageRepresentationSkia::BeginScoped*Access()'s
    > definition to not pass semaphores's ownership to caller. So the caller
    > will not release any semaphores, so shared image backings can reuse or
    > release them.
    > 
    > Bug: 1004772
    > Change-Id: Idfbcf61571a56be6acc8c80ee838ff0e119ba8bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2323562
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Commit-Queue: Peng Huang <penghuang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#793261}
    
    Bug: 1004772
    Change-Id: I2e3d41a29f2f3f45daf0d37cd09df2884a1b9189
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2331293
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793567}

--- components/viz/service/display_embedder/output_presenter.cc
+++ components/viz/service/display_embedder/output_presenter.cc
@@ -65,8 +65,10 @@ void OutputPresenter::Image::BeginWriteSkia() {
       gpu::SharedImageRepresentation::AllowUnclearedAccess::kYes);
   DCHECK(scoped_skia_write_access_);
   if (!begin_semaphores.empty()) {
-    scoped_skia_write_access_->surface()->wait(begin_semaphores.size(),
-                                               begin_semaphores.data());
+    scoped_skia_write_access_->surface()->wait(
+        begin_semaphores.size(),
+        begin_semaphores.data(),
+        /*deleteSemaphoresAfterWait=*/false);
   }
 }
 
