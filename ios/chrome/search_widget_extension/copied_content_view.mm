dcc426f0d02bb0d5c4cf7ba8a6cc9874c67db5b4
Robbie Gibson <rkgibson@google.com>
2020-07-31 09:12:05 +0000

    [iOS] Use new clipboard recent content async apis in Today widget
    The new async APIs don't get the actual clipboard content when checking
    if the clipboard contains a url/image. This prevents them from
    triggering the new iOS 14 pasteboard access notification at bad times.
    
    Bug: 1110845
    Change-Id: I8aa3989dae03999a319dc937f23432cff1f9ad81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2325173
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#793547}

--- ios/chrome/search_widget_extension/copied_content_view.mm
+++ ios/chrome/search_widget_extension/copied_content_view.mm
@@ -25,8 +25,6 @@ const CGFloat kURLButtonMargin = 10;
 
 // The type of the copied content
 @property(nonatomic) CopiedContentType type;
-// The copied text to be displayed if the type supports showing the string.
-@property(nonatomic, copy) NSString* copiedText;
 // The copied URL label containing the URL or a placeholder text.
 @property(nonatomic, strong) UILabel* copiedContentLabel;
 // The copied URL title label containing the title of the copied URL button.
@@ -39,7 +37,7 @@ const CGFloat kURLButtonMargin = 10;
 @property(nonatomic, strong) UIVisualEffectView* primaryEffectView;
 @property(nonatomic, strong) UIVisualEffectView* secondaryEffectView;
 
-// Updates the view to show the currently set |type| and |copiedText|.
+// Updates the view to show the currently set |type|.
 - (void)updateUI;
 
 @end
@@ -174,7 +172,7 @@ const CGFloat kURLButtonMargin = 10;
       [_copiedContentLabel.trailingAnchor
           constraintEqualToAnchor:_openCopiedContentTitleLabel.trailingAnchor],
     ]];
-    [self setCopiedContentType:CopiedContentTypeNone copiedText:nil];
+    [self setCopiedContentType:CopiedContentTypeNone];
     self.highlightableViews = @[
       _hairlineView, _copiedButtonView, _openCopiedContentTitleLabel,
       _copiedContentLabel
@@ -183,10 +181,8 @@ const CGFloat kURLButtonMargin = 10;
   return self;
 }
 
-- (void)setCopiedContentType:(CopiedContentType)type
-                  copiedText:(NSString*)copiedText {
+- (void)setCopiedContentType:(CopiedContentType)type {
   self.type = type;
-  self.copiedText = copiedText;
   [self updateUI];
 }
 
@@ -225,12 +221,10 @@ const CGFloat kURLButtonMargin = 10;
     }
     case CopiedContentTypeURL: {
       titleText = NSLocalizedString(@"IDS_IOS_OPEN_COPIED_LINK", nil);
-      contentText = self.copiedText;
       break;
     }
     case CopiedContentTypeString: {
       titleText = NSLocalizedString(@"IDS_IOS_OPEN_COPIED_TEXT", nil);
-      contentText = self.copiedText;
       break;
     }
     case CopiedContentTypeImage: {
