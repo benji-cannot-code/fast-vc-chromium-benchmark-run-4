a9b711a7343393d0625045856af9e46ef9f4e8d9
Sergey Ulanov <sergeyu@chromium.org>
2020-07-31 21:59:00 +0000

    WebMediaPlayerMS: Copy output frames only when it necessary
    When paused WebMediaPlayerMS copies current video frame to a new buffer.
    Previously it was doing it ever for frames that were already copied in
    GpuMemoryBufferVideoFramePool. That's not really necessary.
    Updated WebMediaPlayerMSCompositor to keep track of the frames that have
    been copied by GpuMemoryBufferVideoFramePool This allows to avoid
    copying frames in WebMediaPlayerMS when it isn't necessary.
    
    Also update the class to use WTF::Vector instead of std::map.
    
    Bug: 1110218
    Change-Id: I21d4269c74fec8b5e67329331611f4ae9993d1e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2331003
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793768}

--- third_party/blink/renderer/modules/mediastream/webmediaplayer_ms.cc
+++ third_party/blink/renderer/modules/mediastream/webmediaplayer_ms.cc
@@ -144,8 +144,8 @@ const gfx::Size WebMediaPlayerMS::kUseGpuMemoryBufferVideoFramesMinResolution =
 // should be destructed on the IO thread.
 class WebMediaPlayerMS::FrameDeliverer {
  public:
-  using RepaintCB =
-      WTF::CrossThreadRepeatingFunction<void(scoped_refptr<media::VideoFrame>)>;
+  using RepaintCB = WTF::CrossThreadRepeatingFunction<
+      void(scoped_refptr<media::VideoFrame> frame, bool is_copy)>;
   FrameDeliverer(const base::WeakPtr<WebMediaPlayerMS>& player,
                  RepaintCB enqueue_frame_cb,
                  scoped_refptr<base::SingleThreadTaskRunner> media_task_runner,
@@ -183,7 +183,7 @@ class WebMediaPlayerMS::FrameDeliverer {
 #endif  // defined(OS_ANDROID)
 
     if (!gpu_memory_buffer_pool_) {
-      EnqueueFrame(std::move(frame));
+      EnqueueFrame(frame, frame);
       return;
     }
 
@@ -202,7 +202,7 @@ class WebMediaPlayerMS::FrameDeliverer {
     // frames is unnecessary, because the frames are not going to be shown for
     // the time period.
     if (render_frame_suspended_ || skip_creating_gpu_memory_buffer) {
-      EnqueueFrame(std::move(frame));
+      EnqueueFrame(frame, frame);
       // If there are any existing MaybeCreateHardwareFrame() calls, we do not
       // want those frames to be placed after the current one, so just drop
       // them.
@@ -220,10 +220,10 @@ class WebMediaPlayerMS::FrameDeliverer {
         FROM_HERE,
         base::BindOnce(
             &media::GpuMemoryBufferVideoFramePool::MaybeCreateHardwareFrame,
-            base::Unretained(gpu_memory_buffer_pool_.get()), std::move(frame),
+            base::Unretained(gpu_memory_buffer_pool_.get()), frame,
             media::BindToCurrentLoop(
                 base::BindOnce(&FrameDeliverer::EnqueueFrame,
-                               weak_factory_for_pool_.GetWeakPtr()))));
+                               weak_factory_for_pool_.GetWeakPtr(), frame))));
   }
 
   void SetRenderFrameSuspended(bool render_frame_suspended) {
@@ -231,7 +231,9 @@ class WebMediaPlayerMS::FrameDeliverer {
     render_frame_suspended_ = render_frame_suspended;
   }
 
-  RepaintCB GetRepaintCallback() {
+  WTF::CrossThreadRepeatingFunction<
+      void(scoped_refptr<media::VideoFrame> frame)>
+  GetRepaintCallback() {
     return CrossThreadBindRepeating(&FrameDeliverer::OnVideoFrame,
                                     weak_factory_.GetWeakPtr());
   }
@@ -239,7 +241,8 @@ class WebMediaPlayerMS::FrameDeliverer {
  private:
   friend class WebMediaPlayerMS;
 
-  void EnqueueFrame(scoped_refptr<media::VideoFrame> frame) {
+  void EnqueueFrame(scoped_refptr<media::VideoFrame> original_frame,
+                    scoped_refptr<media::VideoFrame> frame) {
     DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
 
     {
@@ -255,7 +258,8 @@ class WebMediaPlayerMS::FrameDeliverer {
       }
     }
 
-    enqueue_frame_cb_.Run(std::move(frame));
+    bool is_copy = original_frame != frame;
+    enqueue_frame_cb_.Run(std::move(frame), is_copy);
   }
 
   void DropCurrentPoolTasks() {
