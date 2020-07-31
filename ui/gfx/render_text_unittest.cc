5fad20d237357ad363183096e3c3bccdf06d6451
Etienne Bergeron <etienneb@chromium.org>
2020-07-31 02:23:45 +0000

    Fix typo in test name
    There is no behavior change.
    
    R=msw@chromium.org
    
    Bug: 1025561
    Change-Id: I2165cb218cb0fcee4286becc52c95b57f317b904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2331331
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793481}

--- ui/gfx/render_text_unittest.cc
+++ ui/gfx/render_text_unittest.cc
@@ -4741,7 +4741,7 @@ TEST_F(RenderTextTest, GetTextOffsetHorizontalDefaultInRTL) {
   SetRTL(was_rtl);
 }
 
-TEST_F(RenderTextTest, GetTextOffsetVerticalAignment) {
+TEST_F(RenderTextTest, GetTextOffsetVerticalAlignment) {
   RenderText* render_text = GetRenderText();
   render_text->SetText(ASCIIToUTF16("abcdefg"));
   render_text->SetFontList(FontList("Arial, 13px"));
@@ -4777,7 +4777,7 @@ TEST_F(RenderTextTest, GetTextOffsetVerticalAignment) {
   EXPECT_EQ(kEnlargementY, offset.y());
 }
 
-TEST_F(RenderTextTest, GetTextOffsetVerticalAignment_Multiline) {
+TEST_F(RenderTextTest, GetTextOffsetVerticalAlignment_Multiline) {
   RenderText* render_text = GetRenderText();
   render_text->SetMultiline(true);
   render_text->SetMaxLines(2);
