b175c0961a80cc4b052f38f7f219e155e7420064
Leonard Grey <lgrey@chromium.org>
2020-07-31 15:59:52 +0000

    Commander: introduce SimpleCommandSource
    This is a first draft of a command source that handles basic
    Chromium commands independent of actual content (like tabs, windows,
    bookmarks, etc.)
    
    The purpose for now is just to provide something that can be interacted
    with when the view layer is added. Accordingly, commands have  been
    chosen on the basis of: it's something we would like to implement
    and it has a cross-platform translation that works for this context.
    
    (NB: I didn't try particularly hard to find all of these)
    
    There's also a stubbed out fuzzy finder which is not fuzzy in the least
    at the moment.
    
    Bug: 1014639
    Change-Id: I08282aed82ba9fb1c62e592088e774a02479142e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2327820
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793635}

---
+++ chrome/browser/ui/commander/fuzzy_finder.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/commander/fuzzy_finder.h"
+
+#include "base/i18n/case_conversion.h"
+
+namespace commander {
+
+double FuzzyFind(const base::string16& needle,
+                 const base::string16& haystack,
+                 std::vector<gfx::Range>* matched_ranges) {
+  DCHECK(needle == base::i18n::FoldCase(needle));
+  matched_ranges->clear();
+  const base::string16& folded = base::i18n::FoldCase(haystack);
+  if (folded == needle) {
+    matched_ranges->emplace_back(0, needle.length());
+    return 1.0;
+  }
+  size_t substring_position = folded.find(needle);
+  if (substring_position == std::string::npos)
+    return 0;
+  matched_ranges->emplace_back(substring_position, needle.length());
+  if (substring_position == 0)
+    return .99;
+  return std::min(1 - static_cast<double>(substring_position) / folded.length(),
+                  0.01);
+}
+
+}  // namespace commander
