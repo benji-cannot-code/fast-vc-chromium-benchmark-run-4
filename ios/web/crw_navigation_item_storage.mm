106c67a7d2023f7667de55a09dec8a44d22befc1
Eugene But <eugenebut@chromium.org>
2020-07-31 18:27:44 +0000

    [ios] Do not store POST data for NavigationItem
    According to Session.WebStates.NavigationItem.SerializedPostDataSize
    histogram stored POST data can be quite large, which can have negative
    impact on stability.
    
    slim-nav based navigation can not restore POST data, so there is no
    point in storing this data. This also apply for Preload feature,
    which relies on session restoration and looses POST data.
    
    Bug: 1082279
    Change-Id: Iedfdc247be8b8fd5e7872351f36529d4d10ce66a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2325208
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793701}

--- ios/web/crw_navigation_item_storage.mm
+++ ios/web/crw_navigation_item_storage.mm
@@ -26,7 +26,6 @@ NSString* const kNavigationItemStorageReferrerPolicyKey = @"referrerPolicy";
 NSString* const kNavigationItemStorageTimestampKey = @"timestamp";
 NSString* const kNavigationItemStorageTitleKey = @"title";
 NSString* const kNavigationItemStoragePageDisplayStateKey = @"state";
-NSString* const kNavigationItemStoragePOSTDataKey = @"POSTData";
 NSString* const kNavigationItemStorageHTTPRequestHeadersKey = @"httpHeaders";
 NSString* const kNavigationItemStorageSkipRepostFormConfirmationKey =
     @"skipResubmitDataConfirmation";
@@ -44,8 +43,6 @@ const char kNavigationItemSerializedTitleSizeHistogram[] =
     "Session.WebStates.NavigationItem.SerializedTitleSize";
 const char kNavigationItemSerializedDisplayStateSizeHistogram[] =
     "Session.WebStates.NavigationItem.SerializedDisplayStateSize";
-const char kNavigationItemSerializedPostDataSizeHistogram[] =
-    "Session.WebStates.NavigationItem.SerializedPostDataSize";
 const char kNavigationItemSerializedRequestHeadersSizeHistogram[] =
     "Session.WebStates.NavigationItem.SerializedRequestHeadersSize";
 
@@ -70,7 +67,6 @@ const char kNavigationItemSerializedRequestHeadersSizeHistogram[] =
   [description
       appendFormat:@"userAgentType : %s, ",
                    web::GetUserAgentTypeDescription(_userAgentType).c_str()];
-  [description appendFormat:@"POSTData : %@, ", _POSTData];
   [description appendFormat:@"HTTPRequestHeaders : %@", _HTTPRequestHeaders];
   return description;
 }
@@ -146,8 +142,6 @@ const char kNavigationItemSerializedRequestHeadersSizeHistogram[] =
     _shouldSkipRepostFormConfirmation =
         [aDecoder decodeBoolForKey:
                       web::kNavigationItemStorageSkipRepostFormConfirmationKey];
-    _POSTData =
-        [aDecoder decodeObjectForKey:web::kNavigationItemStoragePOSTDataKey];
     _HTTPRequestHeaders = [aDecoder
         decodeObjectForKey:web::kNavigationItemStorageHTTPRequestHeadersKey];
   }
@@ -223,13 +217,6 @@ const char kNavigationItemSerializedRequestHeadersSizeHistogram[] =
       aCoder, web::kNavigationItemStorageUserAgentTypeKey, userAgent);
   serializedSizeInBytes += userAgent.size();
   // No need to log the user agent type size, because it's a set of constants.
-
-  [aCoder encodeObject:_POSTData forKey:web::kNavigationItemStoragePOSTDataKey];
-  int serializedPostDataSizeInBytes = _POSTData.length;
-  serializedSizeInBytes += serializedPostDataSizeInBytes;
-  base::UmaHistogramMemoryKB(
-      web::kNavigationItemSerializedPostDataSizeHistogram,
-      serializedPostDataSizeInBytes / 1024);
 
   [aCoder encodeObject:_HTTPRequestHeaders
                 forKey:web::kNavigationItemStorageHTTPRequestHeadersKey];
