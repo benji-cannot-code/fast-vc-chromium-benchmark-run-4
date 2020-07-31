aa0b66157bddeb938a642c8b4004fb47518fad6f
Michael Crouse <mcrouse@chromium.org>
2020-07-31 21:11:55 +0000

    Reland "[LiteVideo] Observe MediaPlayer rebuffer events to stop throttling."
    This is a reland of 57fd8da7a27bbaf615106aa354ea8b09c6c1cf96
    
    The browsertest is really flaky because of the timeout. The test is
    not there to verify the code being added so I'm going to decouple
    adding these tests. I removed or disabled the tests causing failures
    so hopefully this resolves it.
    
    Original change's description:
    > [LiteVideo] Observe MediaPlayer rebuffer events to stop throttling.
    >
    > This change adds plumbing to allow the LiteVideo optimization to
    > observe when rebuffer events occur so that all throttling can be
    > stopped.
    >
    > A future change will record the even in the user blocklist and
    > record the event in UKM.
    >
    > Bug: 1082554
    > Change-Id: I9109fe221800b355e46c78e80254f75f433f9448
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316849
    > Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: rajendrant <rajendrant@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#793232}
    
    TBR=tbansal@chromium.org,kinuko@chromium.org,dalecurtis@chromium.org
    
    Bug: 1082554
    Change-Id: I5b11eb841a62b1c90f465c84eb68df6fcd0e573d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332684
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793751}

--- chrome/browser/lite_video/lite_video_observer.h
+++ chrome/browser/lite_video/lite_video_observer.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "chrome/browser/lite_video/lite_video_navigation_metrics.h"
 #include "chrome/browser/lite_video/lite_video_user_blocklist.h"
+#include "content/public/browser/media_player_id.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
@@ -39,6 +40,7 @@ class LiteVideoObserver
   // content::WebContentsObserver.
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
+  void MediaBufferUnderflow(const content::MediaPlayerId& id) override;
 
   // Determines the LiteVideoDecision based on |hint| and the coinflip
   // holdback state.
