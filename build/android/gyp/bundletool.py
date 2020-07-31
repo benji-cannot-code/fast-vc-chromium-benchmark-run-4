8b7bed5f345730155456475e8f851d3d4bcc4e2b
Nate Fischer <ntfschr@chromium.org>
2020-07-31 01:30:02 +0000

    Permit bundletool to emit stderr
    https://crrev.com/c/2309813 recently changed fail_on_output to default
    to True, but this breaks the internal official build installer script
    (see bug for details). This sets this back to False at the call site to
    get the script working again.
    
    Fixed: 1111563
    Test: install_webview.py -M 86 --bundle
    Change-Id: If5bc1646bcf6d4ac7e40ee224033cf678b073cd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2330813
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793462}

--- build/android/gyp/bundletool.py
+++ build/android/gyp/bundletool.py
@@ -31,6 +31,7 @@ def RunBundleTool(args):
   return build_utils.CheckOutput(
       args,
       print_stderr=True,
+      fail_on_output=False,
       stderr_filter=build_utils.FilterReflectiveAccessJavaWarnings)
 
 
