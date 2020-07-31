9d14251deea866f1a9bda5459370936603df4854
Danil Chapovalov <danilchap@chromium.org>
2020-07-31 16:29:07 +0000

    Enable absl variant and optional tests
    To be more sure it is safe to use absl::variant in chromium
    
    Abseil already uses EXPECT_DEATH_IF_SUPPORTED instead of EXPECT_DEATH
    thus the only blocker for these test was already resolved.
    
    Bug: 1073011
    Change-Id: I3103fd558253aa80d6e770bd2cbf29840a5f768c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332439
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793649}

--- third_party/abseil-cpp/absl_hardening_test.cc
+++ third_party/abseil-cpp/absl_hardening_test.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/strings/string_view.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/abseil-cpp/absl/types/span.h"
+#include "third_party/abseil-cpp/absl/types/variant.h"
 
 namespace {
 
@@ -69,6 +70,12 @@ TEST(AbslHardeningTest, Span) {
   std::vector<int> v1 = {1, 2, 3, 4};
   EXPECT_DEATH_IF_SUPPORTED(absl::MakeSpan(&v1[2], &v[0]), "");
   EXPECT_DEATH_IF_SUPPORTED(absl::MakeConstSpan(&v1[2], &v[0]), "");
+}
+
+TEST(AbslHardeningTest, Variant) {
+  absl::variant<int, std::string> variant = 5;
+  EXPECT_DEATH_IF_SUPPORTED(absl::get<std::string>(variant), "");
+  EXPECT_DEATH_IF_SUPPORTED(absl::get<1>(variant), "");
 }
 
 }  // namespace
