595d598bd82a175e1afcc43a8074b35464cc847e
Jongmok Kim <jongmok.kim@navercorp.com>
2020-07-31 01:30:13 +0000

    Use ui::ImageModel instead of ImageSkia in ToolbarActionView related classes
    Bug: 1100034
    Change-Id: Id638fd2fe0f554d7c2c7f440dd27e26954fbec58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2325457
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Jongmok Kim <jongmok.kim@navercorp.com>
    Cr-Commit-Position: refs/heads/master@{#793463}

--- chrome/browser/ui/views/toolbar/toolbar_action_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_action_view.cc
@@ -172,7 +172,8 @@ void ToolbarActionView::UpdateState() {
           .AsImageSkia());
 
   if (!icon.isNull())
-    SetImage(views::Button::STATE_NORMAL, icon);
+    SetImageModel(views::Button::STATE_NORMAL,
+                  ui::ImageModel::FromImageSkia(icon));
 
   SetTooltipText(view_controller_->GetTooltip(web_contents));
 
