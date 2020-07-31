def1c6284ba5dc3f4bd408b0f30d9151aae4976f
Swapnil <swapnilgupta@google.com>
2020-07-31 17:56:26 +0000

    Add downloading stage event for force installed extensions
    We have several downloading stages that the force installed extension
    must go through. We want to add downloading stage event to the
    existing events for the event based reporting for force installed
    extensions.
    
    Bug: 1108257
    Change-Id: I0dacd8f319c1d9eed186fc290d20659c2bbfd223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2310539
    Commit-Queue: Swapnil Gupta <swapnilgupta@google.com>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793685}

--- chrome/browser/extensions/forced_extensions/install_stage_tracker.h
+++ chrome/browser/extensions/forced_extensions/install_stage_tracker.h
@@ -334,6 +334,11 @@ class InstallStageTracker : public KeyedService {
     // Called when installation stage of extension is updated.
     virtual void OnExtensionInstallationStageChanged(const ExtensionId& id,
                                                      Stage stage) {}
+
+    // Called when downloading stage of extension is updated.
+    virtual void OnExtensionDownloadingStageChanged(
+        const ExtensionId& id,
+        ExtensionDownloaderDelegate::Stage stage) {}
   };
 
   explicit InstallStageTracker(const content::BrowserContext* context);
