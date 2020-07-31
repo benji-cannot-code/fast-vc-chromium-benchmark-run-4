3756db5f17019b2ad5d0dbc5e96f73b216c0b58e
dpapad <dpapad@chromium.org>
2020-07-31 17:45:31 +0000

    PDF Viewer update: Use localStorage API to remember sidenav status.
    Bug: 1110015
    Change-Id: I7978308c2f402f0c221f96131a440c3375aa5726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2326830
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793681}

---
+++ chrome/browser/resources/pdf/local_storage_proxy.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {addSingletonGetter} from 'chrome://resources/js/cr.m.js';
+
+/** @interface */
+class LocalStorageProxy {
+  /**
+   * @param {string} key
+   * @return {?string}
+   */
+  getItem(key) {}
+
+  /**
+   * @param {string} key
+   * @param {string} value
+   */
+  setItem(key, value) {}
+}
+
+/** @implements {LocalStorageProxy} */
+export class LocalStorageProxyImpl {
+  /** @override */
+  getItem(key) {
+    return window.localStorage.getItem(key);
+  }
+
+  /** @override */
+  setItem(key, value) {
+    window.localStorage.setItem(key, value);
+  }
+}
+
+addSingletonGetter(LocalStorageProxyImpl);
