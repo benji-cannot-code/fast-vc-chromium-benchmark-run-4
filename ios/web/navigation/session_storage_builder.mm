ecb65d79050fa2bbc905423c15fb4f7775e0cd3e
Gauthier Ambard <gambard@chromium.org>
2020-07-31 14:17:40 +0000

    [iOS] Automatic UA is restored when flag is enabled
    This CL makes sure that the AUTOMATIC UserAgentType is restored in the
    WebState when the flag to have the AUTOMATIC UserAgent is enabled.
    In some cases (for example after updating the flag), the session is
    restored with the AUTOMATIC UserAgent, leading to a DCHECK.
    
    Bug: None
    Change-Id: Iceb9e8b2af25db987e80f07e9fbb2911d0c4f0c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2329709
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793604}

--- ios/web/navigation/session_storage_builder.mm
+++ ios/web/navigation/session_storage_builder.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/mac/foundation_util.h"
+#include "ios/web/common/features.h"
 #import "ios/web/navigation/navigation_item_impl.h"
 #import "ios/web/navigation/navigation_item_storage_builder.h"
 #include "ios/web/navigation/navigation_manager_impl.h"
@@ -114,7 +115,12 @@ void SessionStorageBuilder::ExtractSessionState(
   web_state->certificate_policy_cache_ = std::move(cert_policy_cache);
   web::SerializableUserDataManager::FromWebState(web_state)
       ->AddSerializableUserData(storage.userData);
-  web_state->SetUserAgent(storage.userAgentType);
+  UserAgentType user_agent_type = storage.userAgentType;
+  if (user_agent_type == UserAgentType::AUTOMATIC &&
+      !features::UseWebClientDefaultUserAgent()) {
+    user_agent_type = UserAgentType::MOBILE;
+  }
+  web_state->SetUserAgent(user_agent_type);
 }
 
 }  // namespace web
