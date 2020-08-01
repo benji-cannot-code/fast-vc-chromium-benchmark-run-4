f6db34c33cff57e29e4add519c88ccfd50c0ed16
Mugdha Lakhani <nator@chromium.org>
2020-08-01 06:53:14 +0000

    [Prerender] Componentize prerender_util.
    This is used to support NoStatePrefetch for WebLayer.
    
    Bug: 1096088
    Change-Id: I9b2b5e81f58118e7d8167e1b1de19f0b3a18662a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2329852
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793882}

--- components/prerender/browser/prerender_util.cc
+++ components/prerender/browser/prerender_util.cc
@@ -3,18 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/prerender/prerender_util.h"
+#include "components/prerender/browser/prerender_util.h"
 
 #include "base/metrics/histogram_macros.h"
 #include "components/google/core/common/google_util.h"
-#include "content/public/common/url_constants.h"
-#include "extensions/buildflags/buildflags.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
-
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "extensions/common/constants.h"
-#endif
 
 namespace prerender {
 
--- components/prerender/browser/prerender_util.h
+++ components/prerender/browser/prerender_util.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PRERENDER_PRERENDER_UTIL_H_
-#define CHROME_BROWSER_PRERENDER_PRERENDER_UTIL_H_
+#ifndef COMPONENTS_PRERENDER_BROWSER_PRERENDER_UTIL_H_
+#define COMPONENTS_PRERENDER_BROWSER_PRERENDER_UTIL_H_
 
 class GURL;
 
@@ -21,4 +21,4 @@ void ReportUnsupportedPrerenderScheme(const GURL& url);
 
 }  // namespace prerender
 
-#endif  // CHROME_BROWSER_PRERENDER_PRERENDER_UTIL_H_
+#endif  // COMPONENTS_PRERENDER_BROWSER_PRERENDER_UTIL_H_
