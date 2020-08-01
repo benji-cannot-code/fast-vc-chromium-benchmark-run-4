db332aa5523647930a6e02af7c30503fb3f9570a
David Van Cleve <davidvc@chromium.org>
2020-08-01 00:53:27 +0000

    Trust Tokens: Add more browser tests covering issuance
    This change expands the Trust Tokens browser tests to cover additional
    issuance cases:
    
    issuance to issuer with no keys -> expect error
    issuance, server-side error -> expect error
    cross-origin issuance -> expect success
    cross-site issuance -> expect success
    issuance with number of associated origins at cap
      -> expect success iff the issuer is associated
    issuance after keys expire -> should fail
    cors mode issuance to origin A, redirect to origin B
      -> obtain origin B token
    no-cors mode issuance to origin A, redirect to origin B
      -> obtain origin A token
    
    Subsequent changes will add more redemption and signing tests.
    
    Bug: 1071293
    Change-Id: Id5c0e86fc889551f366db0555d25a6f253f902c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2308860
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793837}

--- services/network/trust_tokens/test/test_server_handler_registration.cc
+++ services/network/trust_tokens/test/test_server_handler_registration.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_request_headers.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
+#include "services/network/trust_tokens/suitable_trust_token_origin.h"
 #include "services/network/trust_tokens/test/trust_token_request_handler.h"
 
 namespace network {
@@ -32,7 +33,9 @@ std::unique_ptr<net::test_server::HttpResponse>
 MakeTrustTokenFailureResponse() {
   // No need to report a failure HTTP code here: returning a vanilla OK should
   // fail the Trust Tokens operation client-side.
-  return std::make_unique<net::test_server::BasicHttpResponse>();
+  auto ret = std::make_unique<net::test_server::BasicHttpResponse>();
+  ret->AddCustomHeader("Access-Control-Allow-Origin", "*");
+  return ret;
 }
 
 // Constructs and returns an HTTP response bearing the given base64-encoded
@@ -46,6 +49,7 @@ std::unique_ptr<net::test_server::HttpResponse> MakeTrustTokenResponse(
 
   auto ret = std::make_unique<net::test_server::BasicHttpResponse>();
   ret->AddCustomHeader("Sec-Trust-Token", std::string(contents));
+  ret->AddCustomHeader("Access-Control-Allow-Origin", "*");
   return ret;
 }
 
@@ -116,7 +120,9 @@ void RegisterTrustTokenTestHandlers(net::EmbeddedTestServer* test_server,
 
         // Unlike issuance and redemption, there's no special state to return
         // on success for signing.
-        return std::make_unique<net::test_server::BasicHttpResponse>();
+        auto ret = std::make_unique<net::test_server::BasicHttpResponse>();
+        ret->AddCustomHeader("Access-Control-Allow-Origin", "*");
+        return ret;
       }));
 }
 
