192003d2515d3085cb035fd4201e9b0023698fec
Koji Ishii <kojii@chromium.org>
2020-08-03 00:31:33 +0000

    Add EXPECT_EQ support to NGTextOffset
    Bug: 982194
    Change-Id: Iabe6ead5ae8f98ae203353f7911a6b91973041a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2331992
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#793977}

---
+++ third_party/blink/renderer/core/layout/ng/inline/ng_text_offset.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_text_offset.h"
+
+#include <ostream>
+
+namespace blink {
+
+std::ostream& operator<<(std::ostream& ostream, const NGTextOffset& offset) {
+  return ostream << "{" << offset.start << ", " << offset.end << "}";
+}
+
+}  // namespace blink
