ad37bd3267ba3e2a0749ed1df27d96d7b734ff74
Luca Hunkeler <hluca@google.com>
2020-08-03 12:52:39 +0000

    [Autofill Assistant] Revert temporary solution for back button in browse
    This solution doesn't work anymore since we recently added some
    restrictions for navigating while in RUNNING state. Morover, even
    besides that we would need to add a whitelist to avoid allowint the user
    to leave the website without closing AA.
    This needs a more complex solution, tracked by: b/161520124.
    
    Change-Id: I82f13737cfbeb533f91b53841dc5ba01a243e122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2323334
    Reviewed-by: Marian Fechete <marianfe@google.com>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/master@{#794082}

--- components/autofill_assistant/browser/actions/action_delegate.h
+++ components/autofill_assistant/browser/actions/action_delegate.h
@@ -405,7 +405,8 @@ class ActionDelegate {
   virtual void ClearGenericUi() = 0;
 
   // Sets the OverlayBehavior.
-  virtual void SetOverlayBehavior(ConfigureUiStateProto::OverlayBehavior) = 0;
+  virtual void SetOverlayBehavior(
+      ConfigureUiStateProto::OverlayBehavior overlay_behavior) = 0;
 
  protected:
   ActionDelegate() = default;
