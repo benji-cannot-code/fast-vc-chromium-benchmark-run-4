5b77cc4072b82537dde1a714d8495b70346d18cd
Milica Selakovic <selakovic@google.com>
2020-08-03 13:10:52 +0000

    [Autofill assistant] Introduce PresaveGeneratedPasswordAction
    Extract presaving from generate password action.
    
    Bug: 1110942
    Change-Id: I2ec6e261fca1d56b518077b9c06ca6e618c75698
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2326670
    Commit-Queue: Milica Selakovic <selakovic@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#794087}

--- components/autofill_assistant/browser/protocol_utils.cc
+++ components/autofill_assistant/browser/protocol_utils.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/actions/highlight_element_action.h"
 #include "components/autofill_assistant/browser/actions/navigate_action.h"
 #include "components/autofill_assistant/browser/actions/popup_message_action.h"
+#include "components/autofill_assistant/browser/actions/presave_generated_password_action.h"
 #include "components/autofill_assistant/browser/actions/prompt_action.h"
 #include "components/autofill_assistant/browser/actions/save_generated_password_action.h"
 #include "components/autofill_assistant/browser/actions/select_option_action.h"
@@ -226,6 +227,8 @@ std::unique_ptr<Action> ProtocolUtils::CreateAction(ActionDelegate* delegate,
       return std::make_unique<SaveGeneratedPasswordAction>(delegate, action);
     case ActionProto::ActionInfoCase::kConfigureUiState:
       return std::make_unique<ConfigureUiStateAction>(delegate, action);
+    case ActionProto::ActionInfoCase::kPresaveGeneratedPassword:
+      return std::make_unique<PresaveGeneratedPasswordAction>(delegate, action);
     case ActionProto::ActionInfoCase::ACTION_INFO_NOT_SET: {
       VLOG(1) << "Encountered action with ACTION_INFO_NOT_SET";
       return std::make_unique<UnsupportedAction>(delegate, action);
