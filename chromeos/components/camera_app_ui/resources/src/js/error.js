f0b2fafcfab0f24fb3856ca682bbfe2e47f7bf3f
Shik Chen <shik@chromium.org>
2020-08-03 08:01:20 +0000

    CCA: Check the generated thumbnail content
    Bug: 1083830, 1092806
    Test: Check that CCAUIVideoRecord would fail as expected on devices
    affected by https://crbug.com/1092806.
    
    Change-Id: I4cf1145076445fbb4890c59ea0a1dca870f1f270
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2330873
    Commit-Queue: Shik Chen <shik@chromium.org>
    Reviewed-by: Kuo Jen Wei <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794032}

--- chromeos/components/camera_app_ui/resources/src/js/error.js
+++ chromeos/components/camera_app_ui/resources/src/js/error.js
@@ -11,6 +11,7 @@ import * as metrics from './metrics.js';
  * @enum {string}
  */
 export const ErrorType = {
+  BROKEN_THUMBNAIL: 'broken-thumbnail',
   UNCAUGHT_PROMISE: 'uncaught-promise',
 };
 
@@ -157,6 +158,14 @@ const triggeredErrorSet = new Set();
  * @param {!Error} error
  */
 export function reportError(type, level, error) {
+  // uncaught promise is already logged in console
+  if (type !== ErrorType.UNCAUGHT_PROMISE) {
+    if (level === ErrorLevel.ERROR) {
+      console.error(type, error);
+    } else {
+      console.warn(type, error);
+    }
+  }
   const time = Date.now();
   const frames = getStackFrames(error);
   const errorName = error.name;
