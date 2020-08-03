0c7f1ff4b976343837917d90a003e25f37da0922
David Trainor <dtrainor@chromium.org>
2020-08-03 18:45:15 +0000

    Add support for reengagement notification
    This patch adds the functionality required for an IPH system to trigger
    one of three re-engagement notifications.  The IPH changes are -
    
    - Starting ChromeTabbedActivity will be flagged as engagement.
    - Starting a CCT will check criteria and decide whether or not to notify
    the user about Chrome.
    
    The feature is integrated into Chrome through the two main
    RootUiCoordinators and is guarded by an overall feature flag (disabling
    all added code) in addition to the IPH feature configurations.
    
    This CL also adds a OneShotCallback, which makes it easier to get a
    single value back from an ObservableSupplier.
    
    Bug: 1108002
    Change-Id: Ia23827337b325550ff16259d97d81bf223de3566
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2311827
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794174}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/app/reengagement/ReengagementActivity.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.app.reengagement;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import org.chromium.base.StrictModeContext;
+import org.chromium.chrome.browser.IntentHandler;
+import org.chromium.chrome.browser.reengagement.ReengagementNotificationController;
+
+/** Trampoline activity to start the NTP from the reengagement notification. */
+public class ReengagementActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String action = getIntent().getAction();
+        if (ReengagementNotificationController.LAUNCH_NTP_ACTION.equals(action)) {
+            Intent intent =
+                    IntentHandler.createTrustedOpenNewTabIntent(this, /* incognito = */ false);
+            try (StrictModeContext ignored = StrictModeContext.allowDiskWrites()) {
+                startActivity(intent);
+            }
+        }
+        finish();
+    }
+}
