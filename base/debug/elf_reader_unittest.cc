52effa393acdca6cda0b208a313d4206de798f4a
Mike Wittman <wittman@chromium.org>
2020-08-04 18:35:44 +0000

    [base] Add ElfReader tests for relocatable ELF files with load bias
    Updates the ELF image builder to support building relocatable ELF images
    with virtual address mapping subject to load bias. Parameterizes tests
    based on mapping state. Fixes ReadElfLibraryName() for the load bias case.
    
    Bug: 1105170
    Change-Id: I74c37342e50636fb875c2e0a463a987d7312adc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2305569
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794616}

--- base/debug/elf_reader_unittest.cc
+++ base/debug/elf_reader_unittest.cc
@@ -25,11 +25,26 @@ namespace {
 constexpr uint8_t kBuildIdBytes[] = {0xab, 0xcd, 0x12, 0x34};
 constexpr const char kBuildIdHexString[] = "ABCD1234";
 constexpr const char kBuildIdHexStringLower[] = "ABCD1234";
+
+std::string ParamInfoToString(
+    const ::testing::TestParamInfo<base::TestElfImageBuilder::MappingType>&
+        param_info) {
+  switch (param_info.param) {
+    case TestElfImageBuilder::RELOCATABLE:
+      return "Relocatable";
+
+    case TestElfImageBuilder::RELOCATABLE_WITH_BIAS:
+      return "RelocatableWithBias";
+  }
+}
 }  // namespace
 
-TEST(ElfReaderTest, ReadElfBuildIdUppercase) {
+using ElfReaderTest =
+    ::testing::TestWithParam<TestElfImageBuilder::MappingType>;
+
+TEST_P(ElfReaderTest, ReadElfBuildIdUppercase) {
   TestElfImage image =
-      TestElfImageBuilder()
+      TestElfImageBuilder(GetParam())
           .AddLoadSegment(PF_R | PF_X, /* size = */ 2000)
           .AddNoteSegment(NT_GNU_BUILD_ID, "GNU", kBuildIdBytes)
           .Build();
@@ -40,9 +55,9 @@ TEST(ElfReaderTest, ReadElfBuildIdUppercase) {
   EXPECT_EQ(kBuildIdHexString, StringPiece(&build_id[0], build_id_size));
 }
 
-TEST(ElfReaderTest, ReadElfBuildIdLowercase) {
+TEST_P(ElfReaderTest, ReadElfBuildIdLowercase) {
   TestElfImage image =
-      TestElfImageBuilder()
+      TestElfImageBuilder(GetParam())
           .AddLoadSegment(PF_R | PF_X, /* size = */ 2000)
           .AddNoteSegment(NT_GNU_BUILD_ID, "GNU", kBuildIdBytes)
           .Build();
@@ -54,11 +69,11 @@ TEST(ElfReaderTest, ReadElfBuildIdLowercase) {
             StringPiece(&build_id[0], build_id_size));
 }
 
-TEST(ElfReaderTest, ReadElfBuildIdMultipleNotes) {
+TEST_P(ElfReaderTest, ReadElfBuildIdMultipleNotes) {
   constexpr uint8_t kOtherNoteBytes[] = {0xef, 0x56};
 
   TestElfImage image =
-      TestElfImageBuilder()
+      TestElfImageBuilder(GetParam())
           .AddLoadSegment(PF_R | PF_X, /* size = */ 2000)
           .AddNoteSegment(NT_GNU_BUILD_ID + 1, "ABC", kOtherNoteBytes)
           .AddNoteSegment(NT_GNU_BUILD_ID, "GNU", kBuildIdBytes)
@@ -70,9 +85,9 @@ TEST(ElfReaderTest, ReadElfBuildIdMultipleNotes) {
   EXPECT_EQ(kBuildIdHexString, StringPiece(&build_id[0], build_id_size));
 }
 
-TEST(ElfReaderTest, ReadElfBuildIdWrongName) {
+TEST_P(ElfReaderTest, ReadElfBuildIdWrongName) {
   TestElfImage image =
-      TestElfImageBuilder()
+      TestElfImageBuilder(GetParam())
           .AddLoadSegment(PF_R | PF_X, /* size = */ 2000)
           .AddNoteSegment(NT_GNU_BUILD_ID, "ABC", kBuildIdBytes)
           .Build();
@@ -82,9 +97,9 @@ TEST(ElfReaderTest, ReadElfBuildIdWrongName) {
   EXPECT_EQ(0u, build_id_size);
 }
 
-TEST(ElfReaderTest, ReadElfBuildIdWrongType) {
+TEST_P(ElfReaderTest, ReadElfBuildIdWrongType) {
   TestElfImage image =
-      TestElfImageBuilder()
+      TestElfImageBuilder(GetParam())
           .AddLoadSegment(PF_R | PF_X, /* size = */ 2000)
           .AddNoteSegment(NT_GNU_BUILD_ID + 1, "GNU", kBuildIdBytes)
           .Build();
@@ -94,8 +109,8 @@ TEST(ElfReaderTest, ReadElfBuildIdWrongType) {
   EXPECT_EQ(0u, build_id_size);
 }
 
-TEST(ElfReaderTest, ReadElfBuildIdNoBuildId) {
-  TestElfImage image = TestElfImageBuilder()
+TEST_P(ElfReaderTest, ReadElfBuildIdNoBuildId) {
+  TestElfImage image = TestElfImageBuilder(GetParam())
                            .AddLoadSegment(PF_R | PF_X, /* size = */ 2000)
                            .Build();
 
@@ -104,7 +119,34 @@ TEST(ElfReaderTest, ReadElfBuildIdNoBuildId) {
   EXPECT_EQ(0u, build_id_size);
 }
 
-TEST(ElfReaderTest, ReadElfBuildIdForCurrentElfImage) {
+TEST_P(ElfReaderTest, ReadElfLibraryName) {
+  TestElfImage image = TestElfImageBuilder(GetParam())
+                           .AddLoadSegment(PF_R | PF_X, /* size = */ 2000)
+                           .AddSoName("mysoname")
+                           .Build();
+
+  Optional<StringPiece> library_name = ReadElfLibraryName(image.elf_start());
+  ASSERT_NE(nullopt, library_name);
+  EXPECT_EQ("mysoname", *library_name);
+}
+
+TEST_P(ElfReaderTest, ReadElfLibraryNameNoSoName) {
+  TestElfImage image = TestElfImageBuilder(GetParam())
+                           .AddLoadSegment(PF_R | PF_X, /* size = */ 2000)
+                           .Build();
+
+  Optional<StringPiece> library_name = ReadElfLibraryName(image.elf_start());
+  EXPECT_EQ(nullopt, library_name);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    MappingTypes,
+    ElfReaderTest,
+    ::testing::Values(TestElfImageBuilder::RELOCATABLE,
+                      TestElfImageBuilder::RELOCATABLE_WITH_BIAS),
+    &ParamInfoToString);
+
+TEST(ElfReaderTestWithCurrentElfImage, ReadElfBuildId) {
   ElfBuildIdBuffer build_id;
   size_t build_id_size = ReadElfBuildId(&__executable_start, true, build_id);
   ASSERT_NE(build_id_size, 0u);
@@ -123,27 +165,7 @@ TEST(ElfReaderTest, ReadElfBuildIdForCurrentElfImage) {
   }
 }
 
-TEST(ElfReaderTest, ReadElfLibraryName) {
-  TestElfImage image = TestElfImageBuilder()
-                           .AddLoadSegment(PF_R | PF_X, /* size = */ 2000)
-                           .AddSoName("mysoname")
-                           .Build();
-
-  Optional<StringPiece> library_name = ReadElfLibraryName(image.elf_start());
-  ASSERT_NE(nullopt, library_name);
-  EXPECT_EQ("mysoname", *library_name);
-}
-
-TEST(ElfReaderTest, ReadElfLibraryNameNoSoName) {
-  TestElfImage image = TestElfImageBuilder()
-                           .AddLoadSegment(PF_R | PF_X, /* size = */ 2000)
-                           .Build();
-
-  Optional<StringPiece> library_name = ReadElfLibraryName(image.elf_start());
-  EXPECT_EQ(nullopt, library_name);
-}
-
-TEST(ElfReaderTest, ReadElfLibraryNameForCurrentElfImage) {
+TEST(ElfReaderTestWithCurrentImage, ReadElfBuildId) {
 #if defined(OS_ANDROID)
   // On Android the library loader memory maps the full so file.
   const char kLibraryName[] = "libbase_unittests__library";
