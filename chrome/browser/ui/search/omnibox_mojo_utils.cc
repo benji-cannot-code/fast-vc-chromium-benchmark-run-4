9997851c2e961b300069a85f4da080e6551b14ff
Tommy Li <tommycli@chromium.org>
2020-08-04 15:36:25 +0000

    [omnibox] Update UI to use AutocompleteResult::IsSuggestionGroupIdHidden
    This CL updates all the UI callsites to use the new
    AutocompleteResult::IsSuggestionGroupIdHidden() method, which respects
    the server-provided hint as to which groups should default-hidden.
    
    This CL also deletes the deprecated omnibox::IsSuggestionGroupIdHidden()
    method, which is no longer used after this CL.
    
    This CL also deletes the deprecated
    omnibox::ToggleSuggestionGroupIdVisibility() method, and migrates all
    callsites to the omnibox::SetSuggestionGroupVisbility() method.
    
    Bug: 1106096
    Change-Id: I00a6248a65a4b2567fae204438534d1735020fd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2330320
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794546}

--- chrome/browser/ui/search/omnibox_mojo_utils.cc
+++ chrome/browser/ui/search/omnibox_mojo_utils.cc
@@ -23,6 +23,7 @@ namespace {
 
 base::flat_map<int32_t, search::mojom::SuggestionGroupPtr>
 CreateSuggestionGroupsMap(
+    const AutocompleteResult& result,
     PrefService* prefs,
     const SearchSuggestionParser::HeadersMap& headers_map) {
   base::flat_map<int32_t, search::mojom::SuggestionGroupPtr> result_map;
@@ -31,7 +32,7 @@ CreateSuggestionGroupsMap(
         search::mojom::SuggestionGroup::New();
     suggestion_group->header = pair.second;
     suggestion_group->hidden =
-        omnibox::IsSuggestionGroupIdHidden(prefs, pair.first);
+        result.IsSuggestionGroupIdHidden(prefs, pair.first);
     result_map.emplace(pair.first, std::move(suggestion_group));
   }
   return result_map;
@@ -144,7 +145,7 @@ search::mojom::AutocompleteResultPtr CreateAutocompleteResult(
     const AutocompleteResult& result,
     PrefService* prefs) {
   return search::mojom::AutocompleteResult::New(
-      input, CreateSuggestionGroupsMap(prefs, result.headers_map()),
+      input, CreateSuggestionGroupsMap(result, prefs, result.headers_map()),
       CreateAutocompleteMatches(result));
 }
 
--- chrome/browser/ui/views/omnibox/omnibox_row_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_row_view.cc
@@ -83,8 +83,9 @@ class OmniboxRowView::HeaderView : public views::View,
     header_label_->SetText(base::i18n::ToUpper(header_text_));
 
     if (row_view_->pref_service_) {
-      suggestion_group_hidden_ = omnibox::IsSuggestionGroupIdHidden(
-          row_view_->pref_service_, suggestion_group_id_);
+      suggestion_group_hidden_ =
+          row_view_->popup_model_->result().IsSuggestionGroupIdHidden(
+              row_view_->pref_service_, suggestion_group_id_);
 
       header_toggle_button_->SetToggled(suggestion_group_hidden_);
     }
@@ -193,8 +194,9 @@ class OmniboxRowView::HeaderView : public views::View,
   void OnPrefChanged() {
     DCHECK(row_view_->pref_service_);
     bool was_hidden = suggestion_group_hidden_;
-    suggestion_group_hidden_ = omnibox::IsSuggestionGroupIdHidden(
-        row_view_->pref_service_, suggestion_group_id_);
+    suggestion_group_hidden_ =
+        row_view_->popup_model_->result().IsSuggestionGroupIdHidden(
+            row_view_->pref_service_, suggestion_group_id_);
 
     if (was_hidden != suggestion_group_hidden_) {
       NotifyAccessibilityEvent(ax::mojom::Event::kExpandedChanged, true);
