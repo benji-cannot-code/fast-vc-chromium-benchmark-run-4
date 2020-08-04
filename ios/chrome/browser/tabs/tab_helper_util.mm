1809ba6ae5eb34e9316adf83a6ecf3809dd701ea
Jonathan Mengedoht <mengedoht@google.com>
2020-08-04 20:39:13 +0000

    Add WellKnownChangePasswordTabHelper
    Implements the behaviour for .well-known/change-password urls,
    analogous to the WellKnownChangePasswordNavigationThrottle
    
    Bug: 927473
    Change-Id: Ib3eed87306ed6b3b5bc1f49f4aa8b91369c614dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2317952
    Commit-Queue: Jonathan Mengedoht <mengedoht@google.com>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794708}

--- ios/chrome/browser/tabs/tab_helper_util.mm
+++ ios/chrome/browser/tabs/tab_helper_util.mm
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/open_in/open_in_tab_helper.h"
 #import "ios/chrome/browser/overscroll_actions/overscroll_actions_tab_helper.h"
 #import "ios/chrome/browser/passwords/password_tab_helper.h"
+#import "ios/chrome/browser/passwords/well_known_change_password_tab_helper.h"
 #import "ios/chrome/browser/policy/policy_features.h"
 #import "ios/chrome/browser/policy_url_blocking/policy_url_blocking_tab_helper.h"
 #include "ios/chrome/browser/reading_list/reading_list_model_factory.h"
@@ -115,6 +116,8 @@ void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
   IOSTaskTabHelper::CreateForWebState(web_state);
   AppLauncherTabHelper::CreateForWebState(web_state);
   security_interstitials::IOSBlockingPageTabHelper::CreateForWebState(
+      web_state);
+  password_manager::WellKnownChangePasswordTabHelper::CreateForWebState(
       web_state);
 
   if (base::FeatureList::IsEnabled(web::features::kUseJSForErrorPage)) {
