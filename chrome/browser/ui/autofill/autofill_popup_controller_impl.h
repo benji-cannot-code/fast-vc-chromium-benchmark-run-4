b6a8cec63f1004be09393ec6d682be9eb5d751bd
Mohamed Amir Yosef <mamir@chromium.org>
2020-08-04 19:15:49 +0000

    [Autofill] Introduce AutofillPopupViewDelegate::GetWebContents()
    Follow up CL will make use of this API.
    
    Bug: 1108181
    Change-Id: I931e2aac81ff5164b129c8556b0bb27d1e15fc6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2333835
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794660}

--- chrome/browser/ui/autofill/autofill_popup_controller_impl.h
+++ chrome/browser/ui/autofill/autofill_popup_controller_impl.h
@@ -93,6 +93,7 @@ class AutofillPopupControllerImpl : public AutofillPopupController {
 
   void SelectionCleared() override;
   gfx::NativeView container_view() const override;
+  content::WebContents* GetWebContents() const override;
   const gfx::RectF& element_bounds() const override;
   void SetElementBounds(const gfx::RectF& bounds);
   bool IsRTL() const override;
