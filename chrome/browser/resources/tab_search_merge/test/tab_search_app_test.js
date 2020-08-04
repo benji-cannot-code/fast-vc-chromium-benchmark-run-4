ec18d4a0f814cb34a02455af38cfa498ab12031f
Tom Lukaszewicz <tluk@chromium.org>
2020-08-04 09:57:35 -0700

    Tab Search: Fix closure_compile for TabSearchSearchField
    Updated JSDoc information to support using the CrSearchFieldBehavior
    with with the non-legacy class based Polymer3 syntax.
    
    Related CL: https://crrev.com/c/2333208
    
    Bug: 1099917
    Change-Id: Id514a5b20e19700a3f7b2948f6a2c7f36caf8d58
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/chrome/browser/resources/tab_search/+/3194288
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819574}

--- chrome/browser/resources/tab_search_merge/test/tab_search_app_test.js
+++ chrome/browser/resources/tab_search_merge/test/tab_search_app_test.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {keyDownOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
 import {TabSearchAppElement} from 'chrome://tab-search/app.js';
+import {TabSearchSearchField} from 'chrome://tab-search/tab_search_search_field.js';
 import {TabSearchApiProxy, TabSearchApiProxyImpl} from 'chrome://tab-search/tab_search_api_proxy.js'
 
 import {assertEquals, assertTrue} from '../../chai_assert.js';
@@ -117,7 +118,7 @@ suite('TabSearchAppTest', () => {
 
   test('return filtered tabs', async () => {
     await setupTest(sampleData());
-    const searchField = /** @type {!TabSearchSearchFieldElement} */
+    const searchField = /** @type {!TabSearchSearchField} */
       (tabSearchApp.shadowRoot.querySelector("#searchField"));
     searchField.setValue('bing');
     await flushTasks();
@@ -134,7 +135,7 @@ suite('TabSearchAppTest', () => {
   test('Keyboard navigation on an empty list', async () => {
     await setupTest({windows: [{active: true, tabs: []}]});
 
-    const searchField = /** @type {!TabSearchSearchFieldElement} */
+    const searchField = /** @type {!TabSearchSearchField} */
         (tabSearchApp.shadowRoot.querySelector("#searchField"));
 
     keyDownOn(searchField, 0, [], 'ArrowUp');
@@ -155,7 +156,7 @@ suite('TabSearchAppTest', () => {
 
     const numTabs =
         sampleData().windows.reduce((total, w) => total + w.tabs.length, 0);
-    const searchField = /** @type {!TabSearchSearchFieldElement} */
+    const searchField = /** @type {!TabSearchSearchField} */
         (tabSearchApp.shadowRoot.querySelector("#searchField"));
 
     keyDownOn(searchField, 0, [], 'ArrowUp');
