07ff4d22eb9d53edcf35dc12278e1eb212686b77
Shengfa Lin <shengfa@google.com>
2020-08-04 04:12:56 +0000

    [chromedriver] Full page screenshot support
    1. Use Chrome DevTools Protocol Page.getLayoutMetrics,
    Emulation.setDeviceMetricsOverride and Emulation.clearDeviceMetricsOverride
    to implement full page screenshot.
    2. If there is existing mobile emulation device override metrics in use,
    do not clear device metrics override and restore to the one in use instead.
    3. Use mobile device metrics device scale factor and mobile
    for set device metrics override when present
    4. Include unit test and python test for screenshot and
    full page screenshot
    5. Disabled newly added failed python tests on android
    
    Bug: chromedriver:294
    Change-Id: Id329055a932c2d4f3b2d7eef2e65d832ce86db90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300980
    Commit-Queue: Shengfa Lin <shengfa@google.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794380}

--- chrome/test/chromedriver/window_commands.h
+++ chrome/test/chromedriver/window_commands.h
@@ -292,6 +292,12 @@ Status ExecuteScreenshot(Session* session,
                          std::unique_ptr<base::Value>* value,
                          Timeout* timeout);
 
+Status ExecuteFullPageScreenshot(Session* session,
+                                 WebView* web_view,
+                                 const base::DictionaryValue& params,
+                                 std::unique_ptr<base::Value>* value,
+                                 Timeout* timeout);
+
 Status ExecutePrint(Session* session,
                     WebView* web_view,
                     const base::DictionaryValue& params,
