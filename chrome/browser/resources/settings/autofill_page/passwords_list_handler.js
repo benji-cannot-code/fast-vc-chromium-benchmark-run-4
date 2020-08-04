f0db80f431cc83702190940558c7e8624ba2463f
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-08-04 11:48:46 +0000

    [Passwords] Add password property to MultiStorePasswordUiEntry
    This change adds a password property to MultiStorePasswordUiEntry.
    This simplifies code and prevents bugs. Furthermore, this change
    performs some other small clean-ups.
    
    Bug: 1049141
    Change-Id: Iacca8005536e3bd9f36cb2af0335e7d84a779311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2329534
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#794486}

--- chrome/browser/resources/settings/autofill_page/passwords_list_handler.js
+++ chrome/browser/resources/settings/autofill_page/passwords_list_handler.js
@@ -180,6 +180,7 @@ Polymer({
     e.preventDefault();
     this.$.menu.close();
     this.showPasswordEditDialog_ = true;
+    this.activePassword.hide();
   },
 
   /**
@@ -196,6 +197,7 @@ Polymer({
     this.showPasswordEditDialog_ = false;
     focusWithoutInk(assert(this.activeDialogAnchor_));
     this.activeDialogAnchor_ = null;
+    this.activePassword.hide();
     this.activePassword = null;
   },
 
