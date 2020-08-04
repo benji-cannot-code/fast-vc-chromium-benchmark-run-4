fc6ee444dbb6abefa6a9d5ac1dbab607baa3b94c
Rakina Zata Amni <rakina@chromium.org>
2020-08-04 01:41:34 +0000

    Add specific BackForwardCache disabling reason to NotRestoredReason
    It's now possible to disable bfcache through an enterprise policy (and
    command line) as of crrev.com/c/2329107. We should track this on
    bfcache's NotRestoredReason metrics. We're also adding another
    NotRestoredReason for disabling due to low memory device.
    
    Bug: 511340
    Change-Id: Iee9023549d6e57966362beebc8ccdfc1a2fba6dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2331994
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794343}

--- content/common/content_navigation_policy.h
+++ content/common/content_navigation_policy.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 CONTENT_EXPORT bool IsBackForwardCacheEnabled();
+CONTENT_EXPORT bool IsBackForwardCacheDisabledByCommandLine();
 CONTENT_EXPORT bool DeviceHasEnoughMemoryForBackForwardCache();
 
 // Whether proactive BrowsingInstance swap can happen on cross-site navigations.
