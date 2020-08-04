f3823f62a9405d4983f108dc87d9376607272622
Reilly Grant <reillyg@chromium.org>
2020-08-04 02:36:35 +0000

    usb: Fix loop to close handles on disconnect
    When platform-specific logic from UsbDeviceImpl and UsbDeviceLinux was
    marged into UsbDevice the version from UsbDeviceLinux, which incorrectly
    iterated over the swapped list of open handles, was used. This change
    fixes that typo so that handles are once again closed when the device is
    disconnected.
    
    Bug: 1106051
    Change-Id: I53eabd25b5799d32d2db7da5665b481abade2d11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300880
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: James Hollyer <jameshollyer@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794359}

--- services/device/usb/usb_device.cc
+++ services/device/usb/usb_device.cc
@@ -119,7 +119,7 @@ void UsbDevice::OnDisconnect() {
   // Swap out the handle list as HandleClosed() will try to modify it.
   std::list<UsbDeviceHandle*> handles;
   handles.swap(handles_);
-  for (auto* handle : handles_)
+  for (auto* handle : handles)
     handle->Close();
 }
 
