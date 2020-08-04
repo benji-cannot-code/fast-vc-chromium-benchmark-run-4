e10aa91db86b1866b052b406592f10d76d484429
Nicolás Peña Moreno <npm@chromium.org>
2020-08-04 19:59:59 +0000

    [LCP] Ignore paints with opacity 0
    This changes the opacity 0 paints that are ignored by the LCP algorithm.
    Before, paints that would later be composited were not ignored, which
    resulted in an inaccurate metric due to detecting LCP too early. After
    this change, even will-change opacity paints are ignored, which could
    result in elements not becoming candidates because they are never
    repainted. In the special case where documentElement changes its
    opacity, we consider the largest content that becomes visible as a valid
    LCP candidate.
    
    In other words this CL does the following 3 changes:
    1) Ignores all LCP signals under style.opacity: 0, as before styles with
    'will-change: opacity' were not ignored.
    2) Saves off the LCP for ignored LCP signals if they are under
    non-nested style.opacity = 0.
    3.) If the document opacity style changes from opacity: 0 to non-zero,
    we record the saved off LCP signal.
    
    Bug: 1092473
    Change-Id: I2a970a34fdb1db6c7c746060c286077553b816a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316788
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794689}

--- third_party/blink/renderer/core/paint/paint_timing_detector.h
+++ third_party/blink/renderer/core/paint/paint_timing_detector.h
@@ -206,6 +206,10 @@ class CORE_EXPORT PaintTimingDetector
 
   void UpdateLargestContentfulPaintCandidate();
 
+  // Reports the largest image and text candidates painted under non-nested 0
+  // opacity layer.
+  void ReportIgnoredContent();
+
   base::Optional<PaintTimingVisualizer>& Visualizer() { return visualizer_; }
   void Trace(Visitor* visitor) const;
 
@@ -309,7 +313,7 @@ class ScopedPaintTimingDetectorBlockPaintHook {
 // static
 inline void PaintTimingDetector::NotifyTextPaint(
     const IntRect& text_visual_rect) {
-  if (IgnorePaintTimingScope::ShouldIgnore())
+  if (IgnorePaintTimingScope::IgnoreDepth() > 1)
     return;
   ScopedPaintTimingDetectorBlockPaintHook::AggregateTextPaint(text_visual_rect);
 }
--- third_party/blink/renderer/platform/graphics/paint/ignore_paint_timing_scope.cc
+++ third_party/blink/renderer/platform/graphics/paint/ignore_paint_timing_scope.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-bool IgnorePaintTimingScope::should_ignore_ = false;
+int IgnorePaintTimingScope::ignore_depth_ = 0;
+bool IgnorePaintTimingScope::is_document_element_invisible_ = false;
 
 }  // namespace blink
