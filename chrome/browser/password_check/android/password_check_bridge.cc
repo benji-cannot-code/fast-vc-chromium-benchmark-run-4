ba504e4764796cb785e3c3ba7bc91540e84da8e2
Ioana Pandele <ioanap@chromium.org>
2020-08-04 10:15:25 +0000

    [PwdCheckAndroid] Move the C++ bridge in chrome/browser
    All the C++ files for this feature will need to be able to depend
    on chrome/browser, because they require a Profile and a suite of
    factories that reside in chrome/browser.
    
    Due to the C++ part being instantiated from java, the necessary objects
    cannot be injected.
    
    Bug: 1102025, 1092444
    Change-Id: Iebd6c283c0ba3305225ddefb1b0a12530cadd9a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332658
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794454}

--- chrome/browser/password_check/android/password_check_bridge.cc
+++ chrome/browser/password_check/android/password_check_bridge.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/password_check/android/internal/password_check_bridge.h"
+#include "chrome/browser/password_check/android/password_check_bridge.h"
 
 #include <jni.h>
 
