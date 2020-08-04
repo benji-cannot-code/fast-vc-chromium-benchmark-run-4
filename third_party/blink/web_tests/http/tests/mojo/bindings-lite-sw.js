c4be652b17b45fc98258bdc8b93b8d3b9556aa31
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2020-08-04 03:02:06 +0000

    mojo-js: Fix encoding and decoding of optional unions
    Previously we assumed nullable unions were always encoded as/decoded
    from pointers, but unions are only encoded as/decoded from pointers
    when they are nested in another union.
    
    This CL changes encodeUnion/decodeUnion to use encodeUnionAsPointer()
    and decodeUnionFromPointer() when a union field is another union.
    
    Fixed: 1101849
    Change-Id: I7615207fe4052eadb544749fe89225c37d52fa58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2325458
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794363}

--- third_party/blink/web_tests/http/tests/mojo/bindings-lite-sw.js
+++ third_party/blink/web_tests/http/tests/mojo/bindings-lite-sw.js
@@ -3,4 +3,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importScripts('/resources/testharness.js');
 importScripts('/gen/layout_test_data/mojo/public/js/mojo_bindings_lite.js');
 importScripts('/gen/content/test/data/lite_js_test.mojom-lite.js');
+importScripts('/gen/content/shell/common/web_test/mojo_echo.mojom-lite.js');
 importScripts('./bindings-lite-tests.js');
