2b6d61c74e9db6549d8ce917002d18aa6e116bef
David Roger <droger@chromium.org>
2020-08-04 15:12:59 +0000

    [signin] Use profile colors in profile menu and interception bubble
    Screenshot:
    https://screenshot.googleplex.com/UaY0XVo8r6B.png
    With dark background:
    https://screenshot.googleplex.com/zkCm5YkgnpS.png
    
    Bug: 1076880, 1099286
    Change-Id: I5255ce9f25d88c19b7d24cb684f574cc544cb63f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2328854
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794539}

--- chrome/browser/ui/views/profiles/profile_menu_view.cc
+++ chrome/browser/ui/views/profiles/profile_menu_view.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/passwords/manage_passwords_view_utils.h"
+#include "chrome/browser/ui/signin/profile_colors_util.h"
 #include "chrome/browser/ui/sync/sync_promo_ui.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/accessibility/non_accessible_image_view.h"
@@ -457,9 +458,11 @@ void ProfileMenuView::BuildIdentity() {
   }
 #endif
 
+  SkColor background_color =
+      GetThemeColorsForProfile(profile).profile_highlight_color;
   if (account_info.has_value()) {
     SetProfileIdentityInfo(
-        profile_name, edit_button_params,
+        profile_name, background_color, edit_button_params,
         ui::ImageModel::FromImage(account_info.value().account_image),
         base::UTF8ToUTF16(account_info.value().full_name),
         IsSyncPaused(profile)
@@ -467,7 +470,7 @@ void ProfileMenuView::BuildIdentity() {
             : base::UTF8ToUTF16(account_info.value().email));
   } else {
     SetProfileIdentityInfo(
-        profile_name, edit_button_params,
+        profile_name, background_color, edit_button_params,
         ui::ImageModel::FromImage(profile_attributes->GetAvatarIcon()),
         /*title=*/base::string16(),
         l10n_util::GetStringUTF16(IDS_PROFILES_LOCAL_PROFILE_STATE));
@@ -476,6 +479,7 @@ void ProfileMenuView::BuildIdentity() {
 
 void ProfileMenuView::BuildGuestIdentity() {
   SetProfileIdentityInfo(/*profile_name=*/base::string16(),
+                         /*background_color=*/SK_ColorTRANSPARENT,
                          /*edit_button=*/base::nullopt,
                          profiles::GetGuestAvatar(),
                          l10n_util::GetStringUTF16(IDS_GUEST_PROFILE_NAME));
