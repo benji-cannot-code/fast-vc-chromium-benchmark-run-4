a993c1390f6a1262e73861b3619634e741a6e64a
Rebekah Potter <rbpotter@chromium.org>
2020-08-04 20:07:02 +0000

    Revert "service worker: Use mojo version of GetUserDataForAllRegistrations"
    This reverts commit 853e0dd9d54bdab27fa032f88f8904dd5626ce60.
    
    Reason for revert: Change seems to have caused consistent unit_tests
    failures on the TSAN bot, see
    https://bugs.chromium.org/p/chromium/issues/detail?id=1112825
    
    Original change's description:
    > service worker: Use mojo version of GetUserDataForAllRegistrations
    >
    > This CL starts using mojo version of
    > GetUserDataForAllRegistrations{,ByKeyPrefix}().
    >
    > The type of the return value of these methods is also changed from
    > map<registration_id, value> to array<ServiceWorkerUserData>,
    > and ServiceWorkerUserData now has registration_id.
    > These changes are required to handle a situation where a
    > registration_id is associated with multiple values. Some features like
    > payments could associate multiple values for a registration_id.
    >
    > Bug: 1055677
    > Change-Id: I29e9ae7c1722316b2ac2f163cfc5b50fe7590d54
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2331376
    > Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#794447}
    
    TBR=falken@chromium.org,kinuko@chromium.org,bashi@chromium.org,nhiroki@chromium.org
    
    Change-Id: If41653b493ad11a6af679ee000a207be5b65f73b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1055677, 1112825
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2337273
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794695}

--- content/browser/service_worker/service_worker_storage.cc
+++ content/browser/service_worker/service_worker_storage.cc
@@ -769,10 +769,9 @@ void ServiceWorkerStorage::GetUserDataForAllRegistrations(
   switch (state_) {
     case STORAGE_STATE_DISABLED:
       RunSoon(FROM_HERE,
-              base::BindOnce(
-                  std::move(callback),
-                  ServiceWorkerDatabase::Status::kErrorDisabled,
-                  std::vector<storage::mojom::ServiceWorkerUserDataPtr>()));
+              base::BindOnce(std::move(callback),
+                             std::vector<std::pair<int64_t, std::string>>(),
+                             ServiceWorkerDatabase::Status::kErrorDisabled));
       return;
     case STORAGE_STATE_INITIALIZING:  // Fall-through.
     case STORAGE_STATE_UNINITIALIZED:
@@ -801,10 +800,9 @@ void ServiceWorkerStorage::GetUserDataForAllRegistrationsByKeyPrefix(
   switch (state_) {
     case STORAGE_STATE_DISABLED:
       RunSoon(FROM_HERE,
-              base::BindOnce(
-                  std::move(callback),
-                  ServiceWorkerDatabase::Status::kErrorDisabled,
-                  std::vector<storage::mojom::ServiceWorkerUserDataPtr>()));
+              base::BindOnce(std::move(callback),
+                             std::vector<std::pair<int64_t, std::string>>(),
+                             ServiceWorkerDatabase::Status::kErrorDisabled));
       return;
     case STORAGE_STATE_INITIALIZING:  // Fall-through.
     case STORAGE_STATE_UNINITIALIZED:
@@ -1559,12 +1557,11 @@ void ServiceWorkerStorage::GetUserDataForAllRegistrationsInDB(
     scoped_refptr<base::SequencedTaskRunner> original_task_runner,
     const std::string& key,
     GetUserDataForAllRegistrationsInDBCallback callback) {
-  std::vector<storage::mojom::ServiceWorkerUserDataPtr> user_data;
+  std::vector<std::pair<int64_t, std::string>> user_data;
   ServiceWorkerDatabase::Status status =
       database->ReadUserDataForAllRegistrations(key, &user_data);
   original_task_runner->PostTask(
-      FROM_HERE,
-      base::BindOnce(std::move(callback), status, std::move(user_data)));
+      FROM_HERE, base::BindOnce(std::move(callback), user_data, status));
 }
 
 void ServiceWorkerStorage::GetUserDataForAllRegistrationsByKeyPrefixInDB(
@@ -1572,13 +1569,12 @@ void ServiceWorkerStorage::GetUserDataForAllRegistrationsByKeyPrefixInDB(
     scoped_refptr<base::SequencedTaskRunner> original_task_runner,
     const std::string& key_prefix,
     GetUserDataForAllRegistrationsInDBCallback callback) {
-  std::vector<storage::mojom::ServiceWorkerUserDataPtr> user_data;
+  std::vector<std::pair<int64_t, std::string>> user_data;
   ServiceWorkerDatabase::Status status =
       database->ReadUserDataForAllRegistrationsByKeyPrefix(key_prefix,
                                                            &user_data);
   original_task_runner->PostTask(
-      FROM_HERE,
-      base::BindOnce(std::move(callback), status, std::move(user_data)));
+      FROM_HERE, base::BindOnce(std::move(callback), user_data, status));
 }
 
 void ServiceWorkerStorage::DeleteAllDataForOriginsFromDB(
