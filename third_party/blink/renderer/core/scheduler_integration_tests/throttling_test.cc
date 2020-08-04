db69a8590257534bccc4d0544b6cafb481b941f3
Peter Kasting <pkasting@chromium.org>
2020-08-04 03:34:57 +0000

    Change TimeDelta::IntDiv() to have the same restrictions as FltDiv().
    This requires adding a check against divide-by-zero in some net/ code.
    
    This also switches a few places from IntDiv() to FltDiv() where the
    result is obviously an int (e.g. FromSeconds(15) / FromSeconds(1)),
    which looks odd for now but will look less so as "/".  This will allow
    the next CL in this chain to be trivially mechanical.
    
    Bug: 1104532
    Change-Id: I0b867cba4498eed8c69bea2d7bf15679823ec61f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2318483
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794371}

--- third_party/blink/renderer/core/scheduler_integration_tests/throttling_test.cc
+++ third_party/blink/renderer/core/scheduler_integration_tests/throttling_test.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code if governed by a BSD-style license that can be
 // found in LICENSE file.
 
+#include "base/numerics/safe_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
@@ -272,7 +273,8 @@ class IntensiveWakeUpThrottlingTest : public ThrottlingTestBase {
     // Tasks are not throttled beyond the default background throttling behavior
     // nor do they get to run more often.
     Vector<String> expected_ouput(
-        kTimeUntilNextCheck.IntDiv(kDefaultThrottledWakeUpInterval),
+        base::ClampFloor<wtf_size_t>(
+            kTimeUntilNextCheck.FltDiv(kDefaultThrottledWakeUpInterval)),
         "called onTimer");
     EXPECT_THAT(ConsoleMessages(), expected_ouput);
   }
