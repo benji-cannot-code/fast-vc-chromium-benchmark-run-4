7d4aafd6b543c0a0e88f3e092019503a62644e9c
Kevin Babbitt <kbabbitt@microsoft.com>
2020-08-04 17:10:20 +0000

    Add a UseCounter for CSS system colors computing to themselves
    Check for cases where a system color keyword is used on an element
    whose color-scheme differs from its parent.
    
    Refactored color-scheme methods on ComputedStyle slightly so that we
    can observe the computed value for color-scheme without needing to
    enable the CSSColorSchemeUARendering flag.
    
    Verified using http://wpt.live/css/css-color/system-color-compute.html
    that the UseCounter is recording on live sites.
    
    Bug: 1081945
    Change-Id: Ifde1fcddd8bb230a9e2a52cfb4a32818d44b567a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2330438
    Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794594}

--- third_party/blink/renderer/core/css/resolver/style_builder_converter.cc
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.cc
@@ -1530,6 +1530,20 @@ scoped_refptr<SVGDashArray> StyleBuilderConverter::ConvertStrokeDasharray(
   return array;
 }
 
+void StyleBuilderConverter::CountSystemColorComputeToSelfUsage(
+    const StyleResolverState& state) {
+  // Count cases where a system color keyword is used on an element whose
+  // color-scheme is different from its parent.
+  // This is a superset of when the feature will change the resolved color
+  // (inheriting the keyword is also required) but it should be a reasonable
+  // approximation for use counting purposes.
+  if (state.Style()->ComputedColorScheme() !=
+      state.ParentStyle()->ComputedColorScheme()) {
+    UseCounter::Count(state.GetDocument(),
+                      WebFeature::kCSSSystemColorComputeToSelf);
+  }
+}
+
 StyleColor StyleBuilderConverter::ConvertStyleColor(StyleResolverState& state,
                                                     const CSSValue& value,
                                                     bool for_visited_link) {
@@ -1538,9 +1552,10 @@ StyleColor StyleBuilderConverter::ConvertStyleColor(StyleResolverState& state,
     CSSValueID value_id = identifier_value->GetValueID();
     if (value_id == CSSValueID::kCurrentcolor)
       return StyleColor::CurrentColor();
-    if (StyleColor::IsSystemColor(value_id) &&
-        RuntimeEnabledFeatures::CSSSystemColorComputeToSelfEnabled()) {
-      return StyleColor(value_id);
+    if (StyleColor::IsSystemColor(value_id)) {
+      CountSystemColorComputeToSelfUsage(state);
+      if (RuntimeEnabledFeatures::CSSSystemColorComputeToSelfEnabled())
+        return StyleColor(value_id);
     }
   }
   return StyleColor(state.GetDocument().GetTextLinkColors().ColorFromCSSValue(
@@ -1564,9 +1579,10 @@ StyleAutoColor StyleBuilderConverter::ConvertStyleAutoColor(
       return StyleAutoColor::CurrentColor();
     if (value_id == CSSValueID::kAuto)
       return StyleAutoColor::AutoColor();
-    if (StyleColor::IsSystemColor(value_id) &&
-        RuntimeEnabledFeatures::CSSSystemColorComputeToSelfEnabled()) {
-      return StyleAutoColor(value_id);
+    if (StyleColor::IsSystemColor(value_id)) {
+      CountSystemColorComputeToSelfUsage(state);
+      if (RuntimeEnabledFeatures::CSSSystemColorComputeToSelfEnabled())
+        return StyleAutoColor(value_id);
     }
   }
   return StyleAutoColor(
--- third_party/blink/renderer/core/css/resolver/style_builder_converter.h
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.h
@@ -294,6 +294,9 @@ class StyleBuilderConverter {
 
   static ScrollbarGutter ConvertScrollbarGutter(StyleResolverState& state,
                                                 const CSSValue& value);
+
+  static void CountSystemColorComputeToSelfUsage(
+      const StyleResolverState& state);
 };
 
 template <typename T>
