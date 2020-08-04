0468ea028ffb2991ddde12cb931e16ed1dd58e63
Mugdha Lakhani <nator@chromium.org>
2020-08-04 10:52:35 +0000

    [Prerender] Remove Profile dependency from PrerenderManager.
    Bug: 1096088
    Change-Id: Ibbcf9b9bb35bc165c67b1947400a02523b9d251e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332706
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794465}

--- chrome/browser/prerender/prerender_manager.h
+++ chrome/browser/prerender/prerender_manager.h
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 #include "url/origin.h"
 
-class Profile;
-
 namespace base {
 class DictionaryValue;
 class ListValue;
@@ -44,6 +42,7 @@ enum class NetworkPredictionStatus;
 
 namespace content {
 class WebContents;
+class BrowserContext;
 }
 
 namespace gfx {
@@ -93,8 +92,8 @@ class PrerenderManager : public content::RenderProcessHostObserver,
     CLEAR_MAX = 0x1 << 2
   };
 
-  // Owned by a Profile object for the lifetime of the profile.
-  PrerenderManager(Profile* profile,
+  // Owned by a BrowserContext object for the lifetime of the browser_context.
+  PrerenderManager(content::BrowserContext* browser_context,
                    std::unique_ptr<PrerenderManagerDelegate> delegate);
   ~PrerenderManager() override;
 
@@ -244,8 +243,6 @@ class PrerenderManager : public content::RenderProcessHostObserver,
   // Records that some visible tab navigated (or was redirected) to the
   // provided URL.
   void RecordNavigation(const GURL& url);
-
-  Profile* profile() const { return profile_; }
 
   // Return current time and ticks with ability to mock the clock out for
   // testing.
@@ -500,8 +497,8 @@ class PrerenderManager : public content::RenderProcessHostObserver,
   // The configuration.
   Config config_;
 
-  // The profile that owns this PrerenderManager.
-  Profile* profile_;
+  // The browser_context that owns this PrerenderManager.
+  content::BrowserContext* browser_context_;
 
   // The delegate that allows content embedder to override the logic in this
   // class.
@@ -513,7 +510,7 @@ class PrerenderManager : public content::RenderProcessHostObserver,
   // Prerenders awaiting deletion.
   PrerenderDataVector to_delete_prerenders_;
 
-  // List of recent navigations in this profile, sorted by ascending
+  // List of recent navigations in this browser_context, sorted by ascending
   // |navigate_time_|.
   std::vector<NavigationRecord> navigations_;
 
@@ -540,12 +537,12 @@ class PrerenderManager : public content::RenderProcessHostObserver,
 
   const std::unique_ptr<PrerenderHistograms> histograms_;
 
-  // The number of bytes transferred over the network for the profile this
-  // PrerenderManager is attached to.
-  int64_t profile_network_bytes_ = 0;
+  // The number of bytes transferred over the network for the browser_context
+  // this PrerenderManager is attached to.
+  int64_t browser_context_network_bytes_ = 0;
 
-  // The value of profile_network_bytes_ that was last recorded.
-  int64_t last_recorded_profile_network_bytes_ = 0;
+  // The value of browser_context_network_bytes_ that was last recorded.
+  int64_t last_recorded_browser_context_network_bytes_ = 0;
 
   // Set of process hosts being prerendered.
   using PrerenderProcessSet = std::set<content::RenderProcessHost*>;
