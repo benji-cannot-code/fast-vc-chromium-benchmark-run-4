10dfd73b4060064b511881f0f1f2131117907880
harrisonsean <harrisonsean@chromium.org>
2020-08-04 11:18:35 +0000

    [iOS][Safety Check] Start adding Password support and UI changes
    Add the ability to start/cancel a check and alter UI accordingly.
    
    Start adding support for password check in safety check.
    
    Some style refactoring to avoid naming collisions.
    
    Bug: 1078782
    Change-Id: I4ba23877b926d038b2af606b7e47c244926f4e32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315864
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794475}

--- ios/chrome/browser/ui/settings/safety_check/safety_check_consumer.h
+++ ios/chrome/browser/ui/settings/safety_check/safety_check_consumer.h
@@ -8,10 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#import "ios/chrome/browser/ui/table_view/chrome_table_view_consumer.h"
 #import "ios/chrome/browser/ui/table_view/table_view_model.h"
 
 // Consumer protocol for safety check.
-@protocol SafetyCheckConsumer <NSObject>
+@protocol SafetyCheckConsumer <ChromeTableViewConsumer>
 
 // Initializes the check types section with |items|.
 - (void)setCheckItems:(NSArray<TableViewItem*>*)items;
--- ios/chrome/browser/ui/settings/safety_check/safety_check_service_delegate.h
+++ ios/chrome/browser/ui/settings/safety_check/safety_check_service_delegate.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_SETTINGS_SAFETY_CHECK_SAFETY_CHECK_SERVICE_DELEGATE_H_
 #define IOS_CHROME_BROWSER_UI_SETTINGS_SAFETY_CHECK_SAFETY_CHECK_SERVICE_DELEGATE_H_
 
+#import <UIKit/UIKit.h>
+
 @class TableViewItem;
 
 // Protocol to handle user actions from the safety check view.
