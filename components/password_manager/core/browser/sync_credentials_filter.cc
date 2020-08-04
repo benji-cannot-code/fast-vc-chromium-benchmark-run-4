d140078a870471985666a65bffac1f4ad2ff4cb6
Marc Treib <treib@chromium.org>
2020-08-04 18:38:33 +0000

    Password manager: Allow updating the Sync account password
    Bug: 1110878
    Change-Id: I92b45b1a8b97e4e00814886a66e646fb727681dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2333837
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794621}

--- components/password_manager/core/browser/sync_credentials_filter.cc
+++ components/password_manager/core/browser/sync_credentials_filter.cc
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 
-using autofill::PasswordForm;
-
 namespace password_manager {
 
 SyncCredentialsFilter::SyncCredentialsFilter(
