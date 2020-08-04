0fe62e9425033641437022b4125bb9e365f55c9a
Joe DeBlasio <jdeblasio@chromium.org>
2020-08-04 19:32:48 +0000

    [Safety Tips] Don't go to NTP when Safety Tip shown in CCT.
    The New Tab Page in CCTs doesn't work quite like you'd expect (since
    they don't happen in organically), so this CL prevents using the NTP
    as the default "safe page" for Safety Tips when a better
    recommendation isn't available.
    
    Instead, this CL either navigates back (if possible), or closes the CCT
    (if not possible). This isn't an ideal solution either, but there
    probably isn't a magical option.
    
    Bug: 1007029
    Change-Id: I63f53e361d734f4b26480467f74b182612dd210b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2334591
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794672}

--- chrome/browser/reputation/safety_tip_ui_helper.cc
+++ chrome/browser/reputation/safety_tip_ui_helper.cc
@@ -15,7 +15,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
 
+#if defined(OS_ANDROID)
+#include "chrome/browser/android/tab_android.h"
+#include "chrome/browser/ui/android/tab_model/tab_model.h"
+#include "chrome/browser/ui/android/tab_model/tab_model_list.h"
+#include "content/public/browser/navigation_controller.h"
+#endif
+
+namespace {
+
+// URL that the "leave site" button aborts to by default.
 const char kSafetyTipLeaveSiteUrl[] = "chrome://newtab";
+
+}  // namespace
 
 void RecordSafetyTipInteractionHistogram(content::WebContents* web_contents,
                                          SafetyTipInteraction interaction) {
@@ -33,8 +45,32 @@ void LeaveSiteFromSafetyTip(content::WebContents* web_contents,
                             const GURL& safe_url) {
   RecordSafetyTipInteractionHistogram(web_contents,
                                       SafetyTipInteraction::kLeaveSite);
+  auto navigated_to = safe_url;
+  if (navigated_to.is_empty()) {
+    navigated_to = GURL(kSafetyTipLeaveSiteUrl);
+
+#if defined(OS_ANDROID)
+    if (TabAndroid::FromWebContents(web_contents)->IsCustomTab()) {
+      auto& controller = web_contents->GetController();
+      // For CCTs, just go back if we can...
+      if (controller.CanGoBack()) {
+        controller.GoBack();
+        return;
+      }
+      // ... or close the CCT otherwise.
+      auto* tab_model = TabModelList::GetTabModelForWebContents(web_contents);
+      if (tab_model) {
+        tab_model->CloseTabAt(tab_model->GetActiveIndex());
+        return;
+      }
+      // (And if we don't have a tab model for some reason, just navigate away
+      //  someplace at least slightly. To my knowledge, this shouldn't happen.)
+    }
+#endif
+  }
+
   content::OpenURLParams params(
-      safe_url, content::Referrer(), WindowOpenDisposition::CURRENT_TAB,
+      navigated_to, content::Referrer(), WindowOpenDisposition::CURRENT_TAB,
       ui::PAGE_TRANSITION_AUTO_TOPLEVEL, false /* is_renderer_initiated */);
   params.should_replace_current_entry = true;
   web_contents->OpenURL(params);
