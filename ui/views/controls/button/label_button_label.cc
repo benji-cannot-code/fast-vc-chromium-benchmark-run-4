46b9623464677c68f73978d05d7ceed1c8627543
Wei Li <weili@chromium.org>
2020-08-04 01:13:52 +0000

    Move LabelButtonLabel internal
    LabelButtonLabel class is only used by LabelButton class. To be used in
    tests, it is still exported. This makes it clearer that this class
    should not be used externally.
    
    Bug: none
    
    Change-Id: I3ccf4adb118075dc57db3041f6de7018f5534c0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2324208
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794340}

--- ui/views/controls/button/label_button_label.cc
+++ ui/views/controls/button/label_button_label.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
+namespace internal {
+
 LabelButtonLabel::LabelButtonLabel(const base::string16& text, int text_context)
     : Label(text, text_context, style::STYLE_PRIMARY) {}
 
@@ -42,5 +44,7 @@ void LabelButtonLabel::SetColorForEnableState() {
     Label::SetEnabledColor(style::GetColor(*this, GetTextContext(), style));
   }
 }
+
+}  // namespace internal
 
 }  // namespace views
--- ui/views/controls/button/label_button_label.h
+++ ui/views/controls/button/label_button_label.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
+namespace internal {
+
 // A Label subclass that can be disabled. This is only used internally for
 // views::LabelButton.
 class VIEWS_EXPORT LabelButtonLabel : public Label {
@@ -48,6 +50,8 @@ class VIEWS_EXPORT LabelButtonLabel : public Label {
 
   DISALLOW_COPY_AND_ASSIGN(LabelButtonLabel);
 };
+
+}  // namespace internal
 
 }  // namespace views
 
