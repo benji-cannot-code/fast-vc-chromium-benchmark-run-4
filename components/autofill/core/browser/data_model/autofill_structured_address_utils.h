c0689eee693ad58182fa54a50f9efc26c776e6f5
Matthias Körber <koerber@google.com>
2020-08-04 12:44:43 +0000

    [Autofill][Slimshady] Merging of structured components.
    The merge logic supports combining observations in the scenario that
    both root nodes have token equivalent values.
    
    There are some other edge cases that we could support for v1 without
    the need for additional UI, but this covers the scope we discussed in
    the feature sync.
    
    Change-Id: I6c773eeda6be328e50f1c7818b46585281f72d79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316200
    Commit-Queue: Matthias Körber <koerber@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794499}

--- components/autofill/core/browser/data_model/autofill_structured_address_utils.h
+++ components/autofill/core/browser/data_model/autofill_structured_address_utils.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/synchronization/lock.h"
 #include "components/autofill/core/browser/autofill_type.h"
+#include "components/autofill/core/browser/data_model/autofill_structured_address_constants.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "third_party/re2/src/re2/re2.h"
 
@@ -146,6 +147,19 @@ std::string CaptureTypeWithPattern(const ServerFieldType& type,
 // options.
 std::string CaptureTypeWithPattern(const ServerFieldType& type,
                                    const std::string& pattern);
+
+// Collapses white spaces and line breaks, converts the string to lower case and
+// removes diacritics.
+base::string16 NormalizeValue(const base::string16& value);
+
+// Returns true of both vectors contain the same tokens in the same order.
+bool AreSortedTokensEqual(const std::vector<base::string16>& first,
+                          const std::vector<base::string16>& second);
+
+// Returns a sorted vector containing the tokens of |value| after |value| was
+// canonicalized. |value| is tokenized by splitting it by white spaces and
+// commas.
+std::vector<base::string16> TokenizeValue(const base::string16 value);
 
 }  // namespace structured_address
 
