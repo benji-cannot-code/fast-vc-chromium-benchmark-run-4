c2c6bfc23b582d25bcd41036ff3fef8accaadefc
Xiaoling Bao <xiaolingbao@google.com>
2020-08-04 19:04:28 +0000

    Add policy fetch support.
    Bug: 1068797
    Change-Id: Iaf56825d95fdb1ba502dfb2d8538f288192e3c28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332572
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794656}

--- chrome/updater/device_management/dm_policy_builder_for_testing.cc
+++ chrome/updater/device_management/dm_policy_builder_for_testing.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "chrome/updater/device_management/dm_cached_policy_info.h"
 #include "chrome/updater/protos/omaha_settings.pb.h"
+#include "chrome/updater/unittest_util.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #include "crypto/rsa_private_key.h"
 #include "crypto/signature_creator.h"
@@ -137,6 +138,8 @@ const uint8_t kSigningKey2Signature[] = {
     0xDD, 0x6F, 0x80, 0xC3,
 };
 
+}  // namespace
+
 std::unique_ptr<DMSigningKeyForTesting> GetTestKey1() {
   constexpr int kFakeKeyVersion = 5;
   return std::make_unique<DMSigningKeyForTesting>(
@@ -150,8 +153,6 @@ std::unique_ptr<DMSigningKeyForTesting> GetTestKey2() {
       kSigningKey2, sizeof(kSigningKey2), kSigningKey2Signature,
       sizeof(kSigningKey2Signature), kFakeKeyVersion, "example.com");
 }
-
-}  // namespace
 
 std::unique_ptr<
     ::wireless_android_enterprise_devicemanagement::OmahaSettingsClientProto>
@@ -173,7 +174,7 @@ GetDefaultTestingOmahaPolicyProto() {
       ::wireless_android_enterprise_devicemanagement::MANUAL_UPDATES_ONLY);
 
   ::wireless_android_enterprise_devicemanagement::ApplicationSettings app;
-  app.set_app_guid("{8A69D345-D564-463C-AFF1-A69D9E530F96}");
+  app.set_app_guid(kChromeAppId);
   app.set_install(
       ::wireless_android_enterprise_devicemanagement::INSTALL_DISABLED);
   app.set_update(
--- chrome/updater/unittest_util.cc
+++ chrome/updater/unittest_util.cc
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/tag.h"
 
 namespace updater {
+
+const char kChromeAppId[] = "{8A69D345-D564-463C-AFF1-A69D9E530F96}";
+
 namespace tagging {
 
 std::ostream& operator<<(std::ostream& os, const ErrorCode& error_code) {
