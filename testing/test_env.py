18e9ba42c235e94c3f7f4669f0d59a6e097799c6
Takuto Ikuta <tikuta@chromium.org>
2020-08-05 16:27:30 +0000

    testing: revert whitespace change
    This is revert of https://crrev.com/c/2337833 , but remove unused
    import too.
    
    Bug: 1112996
    Change-Id: Id0370a6e321b861a27e6415702934eb5f24d1ff5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2336323
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795053}

--- testing/test_env.py
+++ testing/test_env.py
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-# Whitespace change for crbug.com/1112996. TODO: Delete me.
 
 """Sets environment variables needed to run a chromium unit test."""
 
@@ -11,7 +10,6 @@ from __future__ import print_function
 import io
 import os
 import signal
-import stat
 import subprocess
 import sys
 import time
