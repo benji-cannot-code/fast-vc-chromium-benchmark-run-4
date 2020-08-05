2165482765c6b22cbc45d91457f3043618262080
Maksim Sadym <sadym@chromium.org>
2020-08-05 22:00:35 +0000

    Fix IdleDetector crash on headless mode
    Bug: 1113202
    Change-Id: I63ad3e451b5421b2d4398731405d6b151e558506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339354
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Maksim Sadym <sadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795213}

--- ui/base/idle/screensaver_window_finder_x11.cc
+++ ui/base/idle/screensaver_window_finder_x11.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/idle/screensaver_window_finder_x11.h"
 
+#include "base/command_line.h"
 #include "ui/base/x/x11_util.h"
+#include "ui/gfx/switches.h"
 #include "ui/gfx/x/connection.h"
 #include "ui/gfx/x/screensaver.h"
 #include "ui/gfx/x/x11.h"
@@ -17,6 +19,10 @@ namespace ui {
 ScreensaverWindowFinder::ScreensaverWindowFinder() : exists_(false) {}
 
 bool ScreensaverWindowFinder::ScreensaverWindowExists() {
+  // Avoid calling into potentially missing X11 APIs in headless mode.
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(switches::kHeadless))
+    return false;
+
   auto* connection = x11::Connection::Get();
 
   // Let the server know the client version before making any requests.
