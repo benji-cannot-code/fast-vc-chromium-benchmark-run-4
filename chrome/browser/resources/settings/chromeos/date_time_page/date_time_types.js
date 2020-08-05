93963ffa5c9bbd4573b553d814020a08310e7c25
James Hawkins <jhawkins@chromium.org>
2020-08-05 01:45:53 +0000

    OS Settings: Remove unused type TimeZoneAutoDetectPolicyRestriction.
    R=khorimoto@chromium.org
    
    Bug: none
    Test: none
    Change-Id: I52646b33d0757c0fb2169da209a00648315616cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2337574
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794808}

--- chrome/browser/resources/settings/chromeos/date_time_page/date_time_types.js
+++ chrome/browser/resources/settings/chromeos/date_time_page/date_time_types.js
@@ -5,17 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 cr.define('settings', function() {
   /**
-   * Describes the effective policy restriction on time zone automatic
-   * detection.
-   * @enum {number}
-   */
-  const TimeZoneAutoDetectPolicyRestriction = {
-    NONE: 0,
-    FORCED_ON: 1,
-    FORCED_OFF: 2,
-  };
-
-  /**
    * Describes values of
    * prefs.generated.resolve_timezone_by_geolocation_method_short. Must be kept
    * in sync with TimeZoneResolverManager::TimeZoneResolveMethod enum.
@@ -29,5 +18,5 @@ cr.define('settings', function() {
   };
 
   // #cr_define_end
-  return {TimeZoneAutoDetectPolicyRestriction, TimeZoneAutoDetectMethod};
+  return {TimeZoneAutoDetectMethod};
 });
