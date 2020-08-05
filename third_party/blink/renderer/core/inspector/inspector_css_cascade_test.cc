e65cd1c72706b4aee21825de795d439b9742af4b
Anders Hartvoll Ruud <andruud@chromium.org>
2020-08-05 22:07:54 +0000

    [scroll-animations] Make Scroll/DocumentTimeline decision earlier
    Instead of storing a ScrollTimeline that may be nullptr, store an
    AnimationTimeline that's already been determined to either be a
    ScrollTimeline or a DocumentTimeline.
    
    There should be no behavior change in this CL. It's a preparation that
    will make subsequent work on dynamically updatable @scroll-timeline
    rules a bit smoother.
    
    It's also needed in the future to differentiate between animation-
    timeline:auto and animation-timeline:none.
    
    Note: Apparently inspector_css_cascade_test.cc previously depended on
    some header transitively included via css_animation_update.h.
    
    Bug: 1074052
    Change-Id: Ic9704f3a08859244163fd911b65bef98bc0b2d74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332618
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795218}

--- third_party/blink/renderer/core/inspector/inspector_css_cascade_test.cc
+++ third_party/blink/renderer/core/inspector/inspector_css_cascade_test.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/css/css_value.h"
+#include "third_party/blink/renderer/core/css/css_value_list.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/style/computed_style_constants.h"
