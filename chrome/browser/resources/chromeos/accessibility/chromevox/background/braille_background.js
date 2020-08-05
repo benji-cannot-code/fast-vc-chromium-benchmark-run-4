8972a06a45826bfaa84d7b07b39f279caab1f06b
Abigail Klein <abigailbklein@google.com>
2020-08-05 19:12:24 +0000

    Add left and right panning icons and make the Chromevox panel communicate with the braille background page.
    Screenshot: https://screenshot.googleplex.com/cN7RSTWBv08
    
    ChromeVox.
    
    AX-Relnotes: added virtual panning keys to the braille caption panel in
    Change-Id: Ice94af92fd84be29f14d828d3e2561c093635c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2331913
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#795131}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/braille_background.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/braille_background.js
@@ -117,6 +117,16 @@ BrailleBackground = class {
     return this.translatorManager_;
   }
 
+  /** @override */
+  panLeft() {
+    this.displayManager_.panLeft();
+  }
+
+  /** @override */
+  panRight() {
+    this.displayManager_.panRight();
+  }
+
   /**
    * @param {!NavBraille} newContent
    * @param {?string} newContentId
