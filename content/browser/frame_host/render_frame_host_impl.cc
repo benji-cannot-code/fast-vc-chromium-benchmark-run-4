e9e626a5512576e687f6cb032e2dce427df032cd
Adithya Srinivasan <adithyas@chromium.org>
2020-08-05 22:43:44 +0000

    Portals: Move some IPCs to blink.mojom.LocalMainFrame
    This CL moves some portal related IPCs that were defined in
    content.mojom.FrameNavigationControl to blink.mojom.LocalMainFrame. This
    allows us to handle the IPCs directly in blink and remove some layering
    in public/web. There should be no behaviour change caused by this patch.
    
    Change-Id: I4dc41ca52cffa75590b889930aafeac06a211e6e
    Bug: 1110329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2314420
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795231}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -1196,7 +1196,7 @@ void RenderFrameHostImpl::OnPortalActivated(
     base::OnceCallback<void(blink::mojom::PortalActivateResult)> callback) {
   auto it = portals_.insert(std::move(predecessor)).first;
 
-  GetNavigationControl()->OnPortalActivated(
+  GetAssociatedLocalMainFrame()->OnPortalActivated(
       (*it)->portal_token(), std::move(pending_portal),
       std::move(client_receiver), std::move(data),
       base::BindOnce(
@@ -1260,8 +1260,8 @@ void RenderFrameHostImpl::ForwardMessageFromHost(
     if (target_origin != GetLastCommittedOrigin())
       return;
   }
-  GetNavigationControl()->ForwardMessageFromHost(std::move(message),
-                                                 source_origin, target_origin);
+  GetAssociatedLocalMainFrame()->ForwardMessageFromHost(
+      std::move(message), source_origin, target_origin);
 }
 
 SiteInstanceImpl* RenderFrameHostImpl::GetSiteInstance() {
@@ -6546,12 +6546,12 @@ RenderFrameHostImpl::GetAssociatedLocalFrame() {
   return local_frame_;
 }
 
-const mojo::AssociatedRemote<blink::mojom::LocalMainFrame>&
+blink::mojom::LocalMainFrame*
 RenderFrameHostImpl::GetAssociatedLocalMainFrame() {
   DCHECK(frame_tree_node_->IsMainFrame());
   if (!local_main_frame_)
     GetRemoteAssociatedInterfaces()->GetInterface(&local_main_frame_);
-  return local_main_frame_;
+  return local_main_frame_.get();
 }
 
 const mojo::Remote<blink::mojom::HighPriorityLocalFrame>&
