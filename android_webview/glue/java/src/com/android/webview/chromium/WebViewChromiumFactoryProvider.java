7a4fd895854303d6542232e6780c39f19a8ed4be
Clark DuVall <cduvall@chromium.org>
2020-08-05 02:10:06 +0000

    Reland "Move getPacProcessor() to GlueApiHelperForR"
    This is a reland of 02b24a7661b4a7f97f9c95789c7e8f22cfb8bc9b
    
    Forgot to add the line in proguard.py to make VerifiesOnR work
    correctly.
    
    Original change's description:
    > Move getPacProcessor() to GlueApiHelperForR
    >
    > Follow up after feedback on http://crrev.com/c/2328302.
    >
    > Bug: 1111909
    > Change-Id: If3107a6bc513d2aad898e8bc87c8436392ee057d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2335385
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Reviewed-by: Richard Coles <torne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#794655}
    
    TBR=torne@chromium.org
    
    Bug: 1111909
    Change-Id: I64eafe0fb9093907a5510142d73455aee3af2c67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2337385
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794814}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -18,6 +18,7 @@ import android.provider.Settings;
 import android.view.ViewGroup;
 import android.webkit.CookieManager;
 import android.webkit.GeolocationPermissions;
+import android.webkit.PacProcessor;
 import android.webkit.ServiceWorkerController;
 import android.webkit.TokenBindingService;
 import android.webkit.TracingController;
@@ -715,5 +716,10 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
         getSingleton().addTask(() -> {
             getSingleton().getBrowserContextOnUiThread().setWebLayerRunningInSameProcess();
         });
+    }
+
+    @Override
+    public PacProcessor getPacProcessor() {
+        return GlueApiHelperForR.getPacProcessor();
     }
 }
