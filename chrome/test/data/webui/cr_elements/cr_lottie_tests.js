8f6069ea36721ced6c91d3f68e1120e0d886f0b5
Nina Satragno <nsatragno@chromium.org>
2020-08-05 08:44:49 +0000

    [webui] Polish up lottie tests
    This patch follows-up on crrev.com/c/2320075 to clean up lottie tests:
    * Enable closure compilation.
    * Re-enable a test that was missed in the previous CL.
    * Delete a redundant test that was verifying private behaviour.
    
    Bug: 1000989, 1103865
    Change-Id: I3754eb1ebb060179e6a652028751110f9da792f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2317854
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794928}

--- chrome/test/data/webui/cr_elements/cr_lottie_tests.js
+++ chrome/test/data/webui/cr_elements/cr_lottie_tests.js
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 // #import {eventToPromise} from '../test_util.m.js';
 // #import {MockController, MockMethod} from '../mock_controller.m.js';
+// #import {assertDeepEquals, assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
 // clang-format on
 
 /** @fileoverview Suite of tests for cr-lottie. */
@@ -40,13 +41,13 @@ suite('cr_lottie_test', function() {
 
   /**
    * A green pixel as returned by samplePixel.
-   * @type {!Array<number>>}
+   * @type {!Array<number>}
    */
   const GREEN_PIXEL = [0, 255, 0, 255];
 
   /**
    * A blue pixel as returned by samplePixel.
-   * @type {!Array<number>>}
+   * @type {!Array<number>}
    */
   const BLUE_PIXEL = [0, 0, 255, 255];
 
@@ -62,7 +63,7 @@ suite('cr_lottie_test', function() {
   /** @type {?HTMLCanvasElement} */
   let canvas = null;
 
-  /** @type {?blob} */
+  /** @type {?Blob} */
   let lottieWorkerJs = null;
 
   /** @type {Promise} */
@@ -70,9 +71,6 @@ suite('cr_lottie_test', function() {
 
   /** @type {Promise} */
   let waitForPlayingEvent;
-
-  /** @type {Promise} */
-  let waitForResizeEvent;
 
   setup(function(done) {
     mockController = new MockController();
@@ -84,7 +82,7 @@ suite('cr_lottie_test', function() {
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4) {
         assertEquals(200, xhr.status);
-        lottieWorkerJs = xhr.response;
+        lottieWorkerJs = /** @type {Blob} */ (xhr.response);
         done();
       }
     };
@@ -95,8 +93,9 @@ suite('cr_lottie_test', function() {
   });
 
   function createLottieElement() {
-    PolymerTest.clearBody();
-    crLottieElement = document.createElement('cr-lottie');
+    document.body.innerHTML = '';
+    crLottieElement =
+        /** @type {!CrLottieElement} */ (document.createElement('cr-lottie'));
     crLottieElement.animationUrl = SAMPLE_LOTTIE_GREEN;
     crLottieElement.autoplay = true;
 
@@ -104,16 +103,14 @@ suite('cr_lottie_test', function() {
         test_util.eventToPromise('cr-lottie-initialized', crLottieElement);
     waitForPlayingEvent =
         test_util.eventToPromise('cr-lottie-playing', crLottieElement);
-    waitForResizeEvent =
-        test_util.eventToPromise('cr-lottie-resized', crLottieElement);
 
-    container = document.createElement('div');
+    container = /** @type {!HTMLDivElement} */ (document.createElement('div'));
     container.style.width = '300px';
     container.style.height = '200px';
     document.body.appendChild(container);
     container.appendChild(crLottieElement);
 
-    canvas = crLottieElement.offscreenCanvas_;
+    canvas = /** @type {!HTMLCanvasElement} */ (crLottieElement.$$('canvas'));
 
     Polymer.dom.flush();
   }
@@ -146,34 +143,24 @@ suite('cr_lottie_test', function() {
         context.getImageData(canvas.width / 2, canvas.height / 2, 1, 1).data);
   }
 
-  test('TestInitializeAnimationAndAutoPlay', async () => {
-    createLottieElement();
-    assertFalse(crLottieElement.isAnimationLoaded_);
-    await waitForInitializeEvent;
-    assertTrue(crLottieElement.isAnimationLoaded_);
-    await waitForPlayingEvent;
-  });
-
-  // TODO(crbug.com/1021474): flaky.
-  test.skip('TestResize', async () => {
+  test('TestResize', async () => {
     createLottieElement();
     await waitForInitializeEvent;
     await waitForPlayingEvent;
-    await waitForResizeEvent;
 
     const newHeight = 300;
     const newWidth = 400;
-    waitForResizeEvent =
-        test_util.eventToPromise('cr-lottie-resized', crLottieElement)
-            .then(function(e) {
-              assertEquals(e.detail.height, newHeight);
-              assertEquals(e.detail.width, newWidth);
-            });
+    const waitForResizeEvent =
+        /** @type {!Promise<!CustomEvent<{width: number, height: number}>>} */ (
+            test_util.eventToPromise('cr-lottie-resized', crLottieElement));
 
     // Update size of parent div container to see if the canvas is resized.
     container.style.width = newWidth + 'px';
     container.style.height = newHeight + 'px';
-    await waitForResizeEvent;
+    const resizeEvent = await waitForResizeEvent;
+
+    assertEquals(resizeEvent.detail.height, newHeight);
+    assertEquals(resizeEvent.detail.width, newWidth);
   });
 
   test('TestPlayPause', async () => {
@@ -267,13 +254,15 @@ suite('cr_lottie_test', function() {
   test('TestHidden', async () => {
     await waitForPlayingEvent;
 
-    assertFalse(crLottieElement.$$('canvas').hidden);
+    assertFalse(canvas.hidden);
     crLottieElement.hidden = true;
-    assertTrue(crLottieElement.$$('canvas').hidden);
+    assertTrue(canvas.hidden);
   });
 
   test('TestDetachBeforeImageLoaded', async () => {
-    const mockXhr = {};
+    const mockXhr = {
+      onreadystatechange: () => {},
+    };
     mockXhr.open = mockController.createFunctionMock(mockXhr, 'open');
     mockXhr.send = mockController.createFunctionMock(mockXhr, 'send');
     mockXhr.abort = mockController.createFunctionMock(mockXhr, 'abort');
@@ -310,7 +299,9 @@ suite('cr_lottie_test', function() {
   });
 
   test('TestLoadNewImageWhileOldImageIsStillLoading', async () => {
-    const mockXhr = {};
+    const mockXhr = {
+      onreadystatechange: () => {},
+    };
     mockXhr.open = mockController.createFunctionMock(mockXhr, 'open');
     mockXhr.send = mockController.createFunctionMock(mockXhr, 'send');
     mockXhr.abort = mockController.createFunctionMock(mockXhr, 'abort');
