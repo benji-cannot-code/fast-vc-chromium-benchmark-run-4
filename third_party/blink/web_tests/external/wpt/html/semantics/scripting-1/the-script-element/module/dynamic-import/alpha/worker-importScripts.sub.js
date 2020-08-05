d15aa46db2146c7981b80078ff7651116bbbe4f2
Hiroshige Hayashizaki <hiroshige@chromium.org>
2020-08-05 04:22:29 +0000

    [WPT] Reland with fix: Test script's base URL used in dynamic imports
    This CL checks base URL used in resolving relative URL-like
    specifiers in dynamic imports() from
    
    - `<script>`s,
    - worker top-level scripts, and
    - worker scripts imported by `importScripts()`.
    
    This is for https://github.com/whatwg/html/issues/5751.
    
    This CL relands
    https://chromium-review.googlesource.com/c/chromium/src/+/2198835/
    with a fix:
    
    `base-url-worker.sub.html` was flaky due to
    race between two `fetch_tests_from_worker()`s.
    In this reland, a `fetch_tests_from_worker()` is
    moved to a new file `base-url-worker-importScripts.html`,
    having at most one `fetch_tests_from_worker()` per file.
    
    Bug: 1082086, 1112844
    Change-Id: I59c9817a7829e950f40878d376ccb5216207b7e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2337194
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794865}

---
+++ third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/module/dynamic-import/alpha/worker-importScripts.sub.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+"use strict";
+
+importScripts("/resources/testharness.js");
+
+// CORS-same-origin
+self.testName = "same-origin importScripts()";
+self.baseUrlSanitized = false;
+importScripts("../beta/redirect.py?location=http://{{host}}:{{ports[http][0]}}/html/semantics/scripting-1/the-script-element/module/dynamic-import/gamma/base-url.sub.js");
+
+// CORS-cross-origin
+self.testName = "cross-origin importScripts()";
+self.baseUrlSanitized = true;
+importScripts("../beta/redirect.py?location=http://{{hosts[alt][]}}:{{ports[http][0]}}/html/semantics/scripting-1/the-script-element/module/dynamic-import/gamma/base-url.sub.js");
+
+done();
