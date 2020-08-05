59603375aa2762c8a7ed34dea0846b5e895a8765
Dale Curtis <dalecurtis@chromium.org>
2020-08-05 02:44:30 +0000

    Add support for newer macOS color space types.
    This adds a new vt_config_util.mm file which handles converting to
    and from macOS color space information into a gfx::ColorSpace.
    
    Most notably this adds support for HDR color spaces.
    
    R=ccameron, sandersd
    
    Bug: 1103432
    Test: New tests.
    Change-Id: I17f5c93276c1cc19a1623c007370e4f480bd2e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2336179
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794827}

--- ui/accelerated_widget_mac/ca_renderer_layer_tree.mm
+++ ui/accelerated_widget_mac/ca_renderer_layer_tree.mm
@@ -448,6 +448,7 @@ CARendererLayerTree::ContentLayer::ContentLayer(
         if (has_hdr_color_space)
           type = CALayerType::kHDRCopier;
         break;
+      // TODO(crbug.com/1103432): We'll likely need YpCbCr10 here for HDR.
       case kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange:
         // Only allow 4:2:0 frames which fill the layer's contents to be
         // promoted to AV layers.
