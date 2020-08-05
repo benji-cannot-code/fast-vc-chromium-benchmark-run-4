036458d532ed3f374b1f497c207552d7da1051f2
Mike Wittman <wittman@chromium.org>
2020-08-05 17:38:05 +0000

    [base] Remove enum switch default case in elf_reader_unittest.cc
    All enum values are expected to be handled in the switch.
    
    Bug: 1105170, 1101023
    Change-Id: Ia979d79e630375350ca6b260edbc3663cbc99ab2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339378
    Auto-Submit: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795082}

--- base/debug/elf_reader_unittest.cc
+++ base/debug/elf_reader_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/test_elf_image_builder.h"
 #include "base/files/memory_mapped_file.h"
 #include "base/native_library.h"
-#include "base/notreached.h"
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -162,10 +161,6 @@ TEST_P(ElfReaderTest, GetRelocationOffset) {
 
     case TestElfImageBuilder::NON_RELOCATABLE:
       EXPECT_EQ(0u, GetRelocationOffset(image.elf_start()));
-      break;
-
-    default:
-      NOTREACHED();
       break;
   }
 }
