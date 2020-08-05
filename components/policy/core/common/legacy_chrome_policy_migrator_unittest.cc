7c390f4eb340b2c2ed4d0b363025e86ad9f70fb6
Aya ElAttar <ayaelattar@chromium.org>
2020-08-05 17:21:05 +0000

    Remove the old version of PolicyMap::Set
    1. Removed the old version of PolicyMap::Set,
    which passes value as unique_ptr.
    2. Changed all the remaining refs use the new
    version which passes value parameter as base::Value
    instead of unique_ptr.
    
    Bug: 1092469
    Change-Id: Ie45bb05b603cedcad8119a65134c4f5f008964dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332191
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795078}

--- components/policy/core/common/legacy_chrome_policy_migrator_unittest.cc
+++ components/policy/core/common/legacy_chrome_policy_migrator_unittest.cc
@@ -29,9 +29,7 @@ void MultiplyByThree(base::Value* val) {
   *val = base::Value(val->GetInt() * 3);
 }
 
-void SetPolicy(PolicyMap* policy,
-               const char* policy_name,
-               std::unique_ptr<base::Value> value) {
+void SetPolicy(PolicyMap* policy, const char* policy_name, base::Value value) {
   policy->Set(policy_name, POLICY_LEVEL_MANDATORY, POLICY_SCOPE_USER,
               POLICY_SOURCE_CLOUD, std::move(value), nullptr);
 }
@@ -43,9 +41,8 @@ TEST(LegacyChromePolicyMigratorTest, CopyPolicyIfUnset) {
 
   PolicyMap& chrome_map = bundle.Get(PolicyNamespace(POLICY_DOMAIN_CHROME, ""));
 
-  SetPolicy(&chrome_map, kOldPolicy, std::make_unique<base::Value>(kOldValue));
-  SetPolicy(&chrome_map, kOtherPolicy,
-            std::make_unique<base::Value>(kOtherValue));
+  SetPolicy(&chrome_map, kOldPolicy, base::Value(kOldValue));
+  SetPolicy(&chrome_map, kOtherPolicy, base::Value(kOtherValue));
 
   LegacyChromePolicyMigrator migrator(kOldPolicy, kNewPolicy);
 
@@ -72,7 +69,7 @@ TEST(LegacyChromePolicyMigratorTest, TransformPolicy) {
 
   PolicyMap& chrome_map = bundle.Get(PolicyNamespace(POLICY_DOMAIN_CHROME, ""));
 
-  SetPolicy(&chrome_map, kOldPolicy, std::make_unique<base::Value>(kOldValue));
+  SetPolicy(&chrome_map, kOldPolicy, base::Value(kOldValue));
 
   LegacyChromePolicyMigrator migrator(kOldPolicy, kNewPolicy,
                                       base::BindRepeating(&MultiplyByThree));
@@ -89,8 +86,8 @@ TEST(LegacyChromePolicyMigratorTest, IgnoreOldIfNewIsSet) {
 
   PolicyMap& chrome_map = bundle.Get(PolicyNamespace(POLICY_DOMAIN_CHROME, ""));
 
-  SetPolicy(&chrome_map, kOldPolicy, std::make_unique<base::Value>(kOldValue));
-  SetPolicy(&chrome_map, kNewPolicy, std::make_unique<base::Value>(kNewValue));
+  SetPolicy(&chrome_map, kOldPolicy, base::Value(kOldValue));
+  SetPolicy(&chrome_map, kNewPolicy, base::Value(kNewValue));
 
   LegacyChromePolicyMigrator migrator(kOldPolicy, kNewPolicy);
 
