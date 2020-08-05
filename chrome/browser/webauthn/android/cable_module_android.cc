072d65e83b1c57802a1444d7197dc080c5bab270
Adam Langley <agl@chromium.org>
2020-08-05 21:39:59 +0000

    cablev2: pass a NetworkContext into the module.
    The caBLEv2 authenticator will need to use the network in the future. In
    order to avoid a dependency cycle from //chrome/browser to the feature
    module, and back to //chrome/browser (for getting the system network
    context), this change injects the Mojo object into the module.
    
    This involves moving the wrapper class
    (CableAuthenticatorModuleProvider.java) into chrome/browser/webauthn.
    
    BUG=1002262
    
    Change-Id: I0240d3b05e1d51f7fe492eea5ad4e64af4111356
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2330930
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795201}

---
+++ chrome/browser/webauthn/android/cable_module_android.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/net/system_network_context_manager.h"
+
+// This "header" actually contains function definitions and thus can only be
+// included once across Chromium.
+#include "chrome/browser/webauthn/android/jni_headers/CableAuthenticatorModuleProvider_jni.h"
+
+static jlong JNI_CableAuthenticatorModuleProvider_GetSystemNetworkContext(
+    JNIEnv* env) {
+  static_assert(sizeof(jlong) >= sizeof(uintptr_t),
+                "Java longs are too small to contain pointers");
+  return static_cast<jlong>(reinterpret_cast<uintptr_t>(
+      SystemNetworkContextManager::GetInstance()->GetContext()));
+}
