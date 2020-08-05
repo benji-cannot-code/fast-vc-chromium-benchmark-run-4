aab8523d89fb5d55be7e8b3a8346a46baba798ed
Regan Hsu <hsuregan@google.com>
2020-08-05 21:35:32 +0000

    [CrOS Settings] Clicking the row should also toggle reverse scrolling.
    The CL that previously addressed this problem only allowed pressing
    the toggle button to enable/disable reverse scrolling, when the entire
    row can be clicked in order to enable/disable reverse scrolling
    (except for the learn more link).
    
    Fixed: 1109966
    Change-Id: Ica6eb360f2ac22770ff130f57b4cae5e58df2000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339603
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795198}

--- chrome/browser/resources/settings/chromeos/device_page/pointers.js
+++ chrome/browser/resources/settings/chromeos/device_page/pointers.js
@@ -113,6 +113,6 @@ Polymer({
   onReverseScrollRowClicked_: function() {
     this.setPrefValue(
         'settings.touchpad.natural_scroll',
-        this.getPref('settings.touchpad.natural_scroll').value);
+        !this.getPref('settings.touchpad.natural_scroll').value);
   },
 });
