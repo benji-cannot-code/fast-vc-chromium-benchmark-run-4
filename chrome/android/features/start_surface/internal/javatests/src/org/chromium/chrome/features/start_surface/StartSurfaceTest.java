840663c2b52a8be979077dd08223c58e5a642c7b
Xi Han <hanxi@google.com>
2020-08-05 13:30:08 +0000

    [Instant Start] Record the late FeedContentFirstLoadedTime UMA.
    The ContentChangedListener::onAddFinished is not guaranteed to come
    before the onOverviewShownAtLaunch(). To fix it, we cache the event of
    onOverviewShownAtLaunch() if the feed content is still loading. The UMA
    will be recorded when onAddFinished() is called if it is missed on
    startup.
    
    Bug: 1107000
    Change-Id: I23336149addb7ee4ee986083ce09212e982b1d6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316667
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#794997}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
@@ -76,7 +76,7 @@ import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.DeferredStartupHandler;
 import org.chromium.chrome.browser.compositor.layouts.phone.StackLayout;
-import org.chromium.chrome.browser.feed.FeedSurfaceCoordinator;
+import org.chromium.chrome.browser.feed.FeedSurfaceMediator;
 import org.chromium.chrome.browser.flags.CachedFeatureFlags;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -989,7 +989,7 @@ public class StartSurfaceTest {
         Assert.assertEquals(expectedRecordCount,
                 RecordHistogram.getHistogramTotalCountForTesting(
                         StartSurfaceConfiguration.getHistogramName(
-                                FeedSurfaceCoordinator.FEED_CONTENT_FIRST_LOADED_TIME_MS_UMA,
+                                FeedSurfaceMediator.FEED_CONTENT_FIRST_LOADED_TIME_MS_UMA,
                                 isInstantStart)));
         Assert.assertEquals(isInstantReturn() ? 1 : 0,
                 RecordHistogram.getHistogramTotalCountForTesting(
