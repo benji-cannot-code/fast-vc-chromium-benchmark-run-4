99a557585f49aba066960045f4cd262b404b477c
David Tseng <dtseng@chromium.org>
2020-08-05 19:10:57 +0000

    Do not consider combo box nodes groups
    Two changes:
    - always return false for ComboBoxNode.isGroup. Eventually, we can maybe consider a combo box a group if it isn't collapsed, but for the time being, we should avoid this path (see the bug for a crash/exception)
    - do not cache the value of is group in NodeWrapper; it's possible the descendants of the current node changes
    
    R=anastasi@google.com, dmazzoni@chromium.org
    
    Fixed: 1108633
    Change-Id: I201e49c9f85f2b3fcf864dc2ca241f5bce99f84a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339212
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795129}

--- chrome/browser/resources/chromeos/accessibility/switch_access/nodes/node_wrapper.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/nodes/node_wrapper.js
@@ -21,9 +21,6 @@ class NodeWrapper extends SAChildNode {
     /** @private {?SARootNode} */
     this.parent_ = parent;
 
-    /** @private {boolean} */
-    this.isGroup_ = SwitchAccessPredicate.isGroup(this.baseNode_, parent);
-
     /** @private {RepeatedEventHandler} */
     this.locationChangedHandler_;
   }
@@ -106,7 +103,7 @@ class NodeWrapper extends SAChildNode {
 
   /** @override */
   isGroup() {
-    return this.isGroup_;
+    return SwitchAccessPredicate.isGroup(this.baseNode_, this.parent_);
   }
 
   /** @override */
