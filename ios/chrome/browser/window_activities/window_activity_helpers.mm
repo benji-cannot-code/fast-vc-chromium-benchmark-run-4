df92e45956ed6711c92386f4dd09527d330ee38f
edchin <edchin@chromium.org>
2020-08-05 17:17:32 +0000

    [ios] Support drop to create new Chrome window with moved tab
    This CL allows dragging a tab to the edge of the screen to create
    a new window with the tab.
    
    Bug: 1087850
    Change-Id: I366d8acaa949a693341fd7159a643c738bddd386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2251055
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795073}

--- ios/chrome/browser/window_activities/window_activity_helpers.mm
+++ ios/chrome/browser/window_activities/window_activity_helpers.mm
@@ -10,15 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #include "base/mac/foundation_util.h"
+#include "base/strings/sys_string_conversions.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
 #import "ios/chrome/browser/url_loading/url_loading_params.h"
+#include "ios/chrome/browser/window_activities/move_tab_activity_type_buildflags.h"
 #import "ios/web/public/navigation/navigation_manager.h"
 #import "net/base/mac/url_conversions.h"
 
 // Activity types.
 NSString* const kLoadURLActivityType = @"org.chromium.load.url";
 NSString* const kLoadIncognitoURLActivityType = @"org.chromium.load.otr-url";
-NSString* const kMoveTabActivityType = @"org.chromium.move-tab";
 
 // User info keys.
 NSString* const kURLKey = @"LoadParams_URL";
@@ -70,8 +71,10 @@ NSUserActivity* ActivityToLoadURL(WindowActivityOrigin origin,
 }
 
 NSUserActivity* ActivityToMoveTab(NSString* tab_id) {
+  NSString* moveTabActivityType =
+      base::SysUTF8ToNSString(BUILDFLAG(IOS_MOVE_TAB_ACTIVITY_TYPE));
   NSUserActivity* activity =
-      [[NSUserActivity alloc] initWithActivityType:kMoveTabActivityType];
+      [[NSUserActivity alloc] initWithActivityType:moveTabActivityType];
   [activity addUserInfoEntriesFromDictionary:@{kTabIdentifierKey : tab_id}];
   return activity;
 }
@@ -82,7 +85,9 @@ bool ActivityIsURLLoad(NSUserActivity* activity) {
 }
 
 bool ActivityIsTabMove(NSUserActivity* activity) {
-  return [activity.activityType isEqualToString:kMoveTabActivityType];
+  NSString* moveTabActivityType =
+      base::SysUTF8ToNSString(BUILDFLAG(IOS_MOVE_TAB_ACTIVITY_TYPE));
+  return [activity.activityType isEqualToString:moveTabActivityType];
 }
 
 UrlLoadParams LoadParamsFromActivity(NSUserActivity* activity) {
@@ -114,4 +119,10 @@ WindowActivityOrigin OriginOfActivity(NSUserActivity* activity) {
   NSNumber* origin = activity.userInfo[kOriginKey];
   return origin ? static_cast<WindowActivityOrigin>(origin.intValue)
                 : WindowActivityUnknownOrigin;
+}
+
+NSString* GetTabIDFromActivity(NSUserActivity* activity) {
+  if (!ActivityIsTabMove(activity))
+    return nil;
+  return activity.userInfo[kTabIdentifierKey];
 }
