17362ca746415c1e7a140c4ff281b9182a5a2297
James Long <yjlong@google.com>
2020-08-05 21:06:54 +0000

    [Lorenz] Modify filter list to use checkboxes
    As part of UX discussions, the filter list is now a list of nodes that
    can be used in the filter. Whether a node is actually in the filter or
    not is toggled by its checkbox. "Select all" and "Deselect all" buttons
    have been added to make checkbox manipulation easier.
    
    A node enters the filter list through the filter input or the node
    details panel. It always enters in the checked state. A node exits the
    filter list through the user manually clicking the x beside it. Once in
    the filter list, the node's filter visibility can be toggled by a
    checkbox beside it or through the node details panel.
    
    Checkbox information is also encoded in the URL.
    
    Bug: 1111836
    Change-Id: If92d64a9bdcfc93d6b86d1a335b795a5ab628c01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2333066
    Commit-Queue: James Long <yjlong@google.com>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795184}

--- tools/android/dependency_analysis/js/src/vue_components/graph_selected_node_details.vue
+++ tools/android/dependency_analysis/js/src/vue_components/graph_selected_node_details.vue
@@ -17,13 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       </ul>
       <button
           v-if="selectedNode.visualizationState.selectedByFilter"
-          @click="removeSelectedFromFilter">
-        Remove from filter
+          @click="uncheckNodeInFilter">
+        Uncheck in filter
       </button>
       <button
           v-else
-          @click="addSelectedToFilter">
-        Add to filter
+          @click="checkNodeInFilter">
+        Add/check in filter
       </button>
     </template>
     <div v-else>
@@ -44,12 +44,11 @@ const GraphSelectedNodeDetails = {
     return this.selectedNodeDetailsData;
   },
   methods: {
-    addSelectedToFilter: function() {
-      this.$emit(CUSTOM_EVENTS.ADD_TO_FILTER_CLICKED, this.selectedNode.id);
+    checkNodeInFilter: function(check) {
+      this.$emit(CUSTOM_EVENTS.DETAILS_CHECK_NODE, this.selectedNode.id);
     },
-    removeSelectedFromFilter: function() {
-      this.$emit(
-          CUSTOM_EVENTS.REMOVE_FROM_FILTER_CLICKED, this.selectedNode.id);
+    uncheckNodeInFilter: function(check) {
+      this.$emit(CUSTOM_EVENTS.DETAILS_UNCHECK_NODE, this.selectedNode.id);
     },
   },
 };
--- tools/android/dependency_analysis/js/src/vue_components/link_to_graph.vue
+++ tools/android/dependency_analysis/js/src/vue_components/link_to_graph.vue
@@ -20,7 +20,7 @@ const LinkToGraph = {
   computed: {
     url: function() {
       const urlProcessor = UrlProcessor.createForOutput();
-      urlProcessor.appendArray(URL_PARAM_KEYS.FILTER, this.filter);
+      urlProcessor.appendArray(URL_PARAM_KEYS.FILTER_NAMES, this.filter);
       return urlProcessor.getUrl(document.URL, this.graphType);
     },
   },
