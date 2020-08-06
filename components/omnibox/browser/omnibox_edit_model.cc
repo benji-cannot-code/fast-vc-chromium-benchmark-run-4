974ff01a06d577f49a093b5589b4e1cf2d153e5b
Tomasz Wiszkowski <ender@google.com>
2020-08-06 18:44:31 +0000

    Reland "Log abandoned omnibox interactions."
    The previous change was reverted because the newly introduced focus_resulted_in_navigation_ member
    caused MSAN failures as in some test scenarios the value would end up being uninitialized.
    
    The value is now default-initialized in constructor.
    
    This is a reland of 2ce4939f0bf1156753672332445e19b26c163216
    
    Original change's description:
    > Log abandoned omnibox interactions.
    > 
    > Introduces new metric recording outcome of every Omnibox focus event.
    > Logged result will allow us to monitor for increase in abandoned
    > interactions as we introduce new suggestion types.
    > 
    > Bug: 1105156
    > Change-Id: I0b12e8cfa748e6a33f1d44e96ade77bf0b39cdd5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2329878
    > Commit-Queue: Ender 💬 = 🕐 (ping me for faster reviews) <ender@google.com>
    > Reviewed-by: Mark Pearson <mpearson@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Tommy Li <tommycli@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#795155}
    
    Bug: 1105156
    Change-Id: I67231a7c653617f3f197b66bf9c25a0183710fc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340411
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#795560}

--- components/omnibox/browser/omnibox_edit_model.cc
+++ components/omnibox/browser/omnibox_edit_model.cc
@@ -73,6 +73,9 @@ namespace {
 // OnAfterPossibleChange()).
 const char kOmniboxUserTextClearedHistogram[] = "Omnibox.UserTextCleared";
 
+const char kOmniboxFocusResultedInNavigation[] =
+    "Omnibox.FocusResultedInNavigation";
+
 enum UserTextClearedType {
   OMNIBOX_USER_TEXT_CLEARED_BY_EDITING = 0,
   OMNIBOX_USER_TEXT_CLEARED_WITH_ESCAPE = 1,
@@ -141,6 +144,7 @@ OmniboxEditModel::OmniboxEditModel(OmniboxView* view,
       focus_state_(OMNIBOX_FOCUS_NONE),
       user_input_in_progress_(false),
       user_input_since_focus_(true),
+      focus_resulted_in_navigation_(false),
       just_deleted_text_(false),
       has_temporary_text_(false),
       paste_state_(NONE),
@@ -726,6 +730,9 @@ void OmniboxEditModel::OpenMatch(AutocompleteMatch match,
   autocomplete_controller()->UpdateMatchDestinationURLWithQueryFormulationTime(
       elapsed_time_since_user_first_modified_omnibox, &match);
 
+  // Save the result of the interaction, but do not record the histogram yet.
+  focus_resulted_in_navigation_ = true;
+
   // Matches with |pedal| may be opened normally or executed, but when a match
   // is a dedicated Pedal suggestion, it should always be executed. This only
   // happens when the button row feature is disabled.
@@ -1070,6 +1077,7 @@ void OmniboxEditModel::ClearKeyword() {
 void OmniboxEditModel::OnSetFocus(bool control_down) {
   last_omnibox_focus_ = base::TimeTicks::Now();
   user_input_since_focus_ = false;
+  focus_resulted_in_navigation_ = false;
 
   // If the omnibox lost focus while the caret was hidden and then regained
   // focus, OnSetFocus() is called and should restore visibility. Note that
@@ -1132,6 +1140,8 @@ void OmniboxEditModel::OnWillKillFocus() {
 }
 
 void OmniboxEditModel::OnKillFocus() {
+  UMA_HISTOGRAM_BOOLEAN(kOmniboxFocusResultedInNavigation,
+                        focus_resulted_in_navigation_);
   SetFocusState(OMNIBOX_FOCUS_NONE, OMNIBOX_FOCUS_CHANGE_EXPLICIT);
   focus_source_ = OmniboxFocusSource::INVALID;
   last_omnibox_focus_ = base::TimeTicks();
