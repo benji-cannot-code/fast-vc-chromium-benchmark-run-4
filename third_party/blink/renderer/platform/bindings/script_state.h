6811cc0aaf136d1a4a105d7a6168d63e7350ae1b
Chris Hamilton <chrisha@chromium.org>
2020-08-06 15:31:34 +0000

    Create V8ContextToken and v8::Context lifecycle tracking.
    This adorns ScriptStates with a unique identifier, and adds
    lifecycle tracking instrumentation hooks. A further CL will
    wire this up in order to implement detached context tracking
    as part of the performance.measureMemory API.
    
    BUG=1085129
    
    Change-Id: I4ddb84c547430a28615dea57e7fd346c9e27b1f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2327851
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795477}

--- third_party/blink/renderer/platform/bindings/script_state.h
+++ third_party/blink/renderer/platform/bindings/script_state.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gin/public/context_holder.h"
 #include "gin/public/gin_embedders.h"
+#include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/renderer/platform/bindings/scoped_persistent.h"
 #include "third_party/blink/renderer/platform/bindings/v8_cross_origin_callback_info.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class DOMWrapperWorld;
+class ExecutionContext;
 class ScriptValue;
 class V8PerContextData;
 
@@ -122,7 +124,11 @@ class PLATFORM_EXPORT ScriptState final : public GarbageCollected<ScriptState> {
     v8::Local<v8::Context> context_;
   };
 
-  ScriptState(v8::Local<v8::Context>, scoped_refptr<DOMWrapperWorld>);
+  // If this ScriptState is associated with an ExecutionContext then it must be
+  // provided here, otherwise providing nullptr is fine.
+  ScriptState(v8::Local<v8::Context>,
+              scoped_refptr<DOMWrapperWorld>,
+              ExecutionContext* execution_context);
   ~ScriptState();
 
   void Trace(Visitor*) const {}
@@ -174,6 +180,7 @@ class PLATFORM_EXPORT ScriptState final : public GarbageCollected<ScriptState> {
 
   v8::Isolate* GetIsolate() const { return isolate_; }
   DOMWrapperWorld& World() const { return *world_; }
+  const V8ContextToken& GetToken() const { return token_; }
 
   // This can return an empty handle if the v8::Context is gone.
   v8::Local<v8::Context> GetContext() const {
@@ -219,6 +226,10 @@ class PLATFORM_EXPORT ScriptState final : public GarbageCollected<ScriptState> {
   // lifetime of |reference_from_v8_context_| and the internal field must match
   // exactly.
   SelfKeepAlive<ScriptState> reference_from_v8_context_;
+
+  // Serves as a unique ID for this context, which can be used to name the
+  // context in browser/renderer communications.
+  V8ContextToken token_;
 
   static constexpr int kV8ContextPerContextDataIndex =
       static_cast<int>(gin::kPerContextDataStartIndex) +
