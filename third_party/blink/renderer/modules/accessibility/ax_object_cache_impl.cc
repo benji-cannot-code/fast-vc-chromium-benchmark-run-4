48fe0171f10b3c8d0ce52f3a72ce0ced0eed36d8
Dominic Mazzoni <dmazzoni@chromium.org>
2020-08-06 19:08:20 +0000

    Avoid enqueuing extra deferred accessibility updates for the same node
    When a node's children have changed, accessibility code enqueues
    information about that in order to examine the accessibility tree
    after layout is clean. However, under some circumstances that led
    to enqueuing the same node over and over again.
    
    Fix that by adding a set to keep track of nodes already in the queue.
    
    This patch also adds tracing for
    AXObjectCacheImpl::ProcessDeferredAccessibilityEvents and adds a new
    perf test to track this optimization. I'm seeing a ~20x speedup.
    
    Bug: 1107988
    Change-Id: I1263c5139991287a35b89c9c0e6e021f07628c17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340384
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795571}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/accessibility/ax_virtual_object.h"
 #include "third_party/blink/renderer/modules/media_controls/elements/media_control_elements_helper.h"
 #include "third_party/blink/renderer/modules/permissions/permission_utils.h"
+#include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "ui/accessibility/ax_enums.mojom-blink.h"
 #include "ui/accessibility/ax_event.h"
@@ -1104,6 +1105,10 @@ void AXObjectCacheImpl::ChildrenChanged(Node* node) {
   if (!node)
     return;
 
+  // Don't enqueue a deferred event on the same node more than once.
+  if (!nodes_with_pending_children_changed_.insert(node).is_new_entry)
+    return;
+
   DeferTreeUpdate(&AXObjectCacheImpl::ChildrenChangedWithCleanLayout, node);
 }
 
@@ -1114,6 +1119,10 @@ void AXObjectCacheImpl::ChildrenChanged(const LayoutObject* layout_object) {
   Node* node = GetClosestNodeForLayoutObject(layout_object);
 
   if (node) {
+    // Don't enqueue a deferred event on the same node more than once.
+    if (!nodes_with_pending_children_changed_.insert(node).is_new_entry)
+      return;
+
     DeferTreeUpdate(&AXObjectCacheImpl::ChildrenChangedWithCleanLayout, node);
     return;
   }
@@ -1171,6 +1180,8 @@ void AXObjectCacheImpl::ChildrenChangedWithCleanLayout(Node* optional_node,
 }
 
 void AXObjectCacheImpl::ProcessDeferredAccessibilityEvents(Document& document) {
+  TRACE_EVENT0("accessibility", "ProcessDeferredAccessibilityEvents");
+
   if (document.Lifecycle().GetState() != DocumentLifecycle::kInAccessibility) {
     DCHECK(false) << "Deferred events should only be processed during the "
                      "accessibility document lifecycle";
@@ -1201,6 +1212,8 @@ void AXObjectCacheImpl::ProcessUpdates(Document& document) {
 
   TreeUpdateCallbackQueue old_tree_update_callback_queue;
   tree_update_callback_queue_.swap(old_tree_update_callback_queue);
+  nodes_with_pending_children_changed_.clear();
+
   for (auto& tree_update : old_tree_update_callback_queue) {
     Node* node = tree_update->node;
     AXID axid = tree_update->axid;
@@ -2291,6 +2304,7 @@ void AXObjectCacheImpl::Trace(Visitor* visitor) const {
   visitor->Trace(permission_observer_receiver_);
   visitor->Trace(documents_);
   visitor->Trace(tree_update_callback_queue_);
+  visitor->Trace(nodes_with_pending_children_changed_);
   AXObjectCache::Trace(visitor);
 }
 
