af427feaad9b7f33e1e4147eef3a651118123841
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-08-06 16:52:08 +0000

    [CrOS settings] Add Phone Hub feature to multidevice page
    - Adds new string search tags for Phone Hub and related features
    - Adds new sections for new Phone Hub realated features
    - Update test to include new Phone Hub feature
    
    Bug: 1106937
    Change-Id: I4dbe78b0c94a9e26118ea4e51b725294472e6cb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315394
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/master@{#795514}

--- chrome/browser/resources/settings/chromeos/multidevice_page/multidevice_constants.js
+++ chrome/browser/resources/settings/chromeos/multidevice_page/multidevice_constants.js
@@ -41,6 +41,10 @@ cr.define('settings', function() {
     INSTANT_TETHERING: 1,
     MESSAGES: 2,
     SMART_LOCK: 3,
+    PHONE_HUB: 4,
+    PHONE_HUB_NOTIFICATIONS: 5,
+    PHONE_HUB_NOTIFICATION_BADGE: 6,
+    PHONE_HUB_TASK_CONTINUATION: 7,
   };
 
   /**
@@ -59,6 +63,7 @@ cr.define('settings', function() {
     UNAVAILABLE_INSUFFICIENT_SECURITY: 6,
     UNAVAILABLE_SUITE_DISABLED: 7,
     FURTHER_SETUP_REQUIRED: 8,
+    UNAVAILABLE_TOP_LEVEL_FEATURE_DISABLED: 9,
   };
 
   /**
@@ -81,6 +86,10 @@ cr.define('settings', function() {
    *   instantTetheringState: !settings.MultiDeviceFeatureState,
    *   messagesState: !settings.MultiDeviceFeatureState,
    *   smartLockState: !settings.MultiDeviceFeatureState,
+   *   phoneHubState: !settings.MultiDeviceFeatureState,
+   *   phoneHubNotificationsState: !settings.MultiDeviceFeatureState,
+   *   phoneHubNotificationBadgeState: !settings.MultiDeviceFeatureState,
+   *   phoneHubTaskContinuationState: !settings.MultiDeviceFeatureState,
    *   isAndroidSmsPairingComplete: boolean
    * }}
    */
