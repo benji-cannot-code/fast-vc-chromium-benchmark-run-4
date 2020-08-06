5ab27d268895a5f362c1510ace3d2e975126ac3e
Livvie Lin <livvielin@chromium.org>
2020-08-06 21:38:45 +0000

    [iOS] Gate lookalike interstitials on committed interstitials
    Lookalike interstitials require committed interstitials to be
    enabled, so add a check in tab_helper_util.
    
    Change-Id: Ia991dcb4630b576cc7d59fd0ec2ce5a78c3d4de2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2341040
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Livvie Lin <livvielin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795647}

--- ios/chrome/browser/tabs/tab_helper_util.mm
+++ ios/chrome/browser/tabs/tab_helper_util.mm
@@ -193,7 +193,8 @@ void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
   PageloadForegroundDurationTabHelper::CreateForWebState(web_state);
 
   if (base::FeatureList::IsEnabled(
-          web::features::kIOSLookalikeUrlNavigationSuggestionsUI)) {
+          web::features::kIOSLookalikeUrlNavigationSuggestionsUI) &&
+      base::FeatureList::IsEnabled(web::features::kSSLCommittedInterstitials)) {
     LookalikeUrlTabHelper::CreateForWebState(web_state);
     LookalikeUrlTabAllowList::CreateForWebState(web_state);
     LookalikeUrlContainer::CreateForWebState(web_state);
