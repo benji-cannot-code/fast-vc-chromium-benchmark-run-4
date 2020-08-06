d6973fbb885be7e3f2cfa272a590fe83ea68df4a
Gauthier Ambard <gambard@chromium.org>
2020-08-06 15:38:43 +0000

    [iOS] Additional fixes for UserAgent tests
    This is making sure that the tests are passing when the flag to use the
    UserAgent of the WebClients is enabled or disabled.
    
    Bug: none
    Change-Id: If61994b6c6ec13327475e4f2f525a93ea121ed1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332606
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795482}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -428,6 +428,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Returns YES if collections are presented in cards.
 + (BOOL)isCollectionsCardPresentationStyleEnabled WARN_UNUSED_RESULT;
 
+// Returns whether the mobile version of the websites are requested by default.
++ (BOOL)isMobileModeByDefault WARN_UNUSED_RESULT;
+
 #pragma mark - Popup Blocking
 
 // Gets the current value of the popup content setting preference for the
