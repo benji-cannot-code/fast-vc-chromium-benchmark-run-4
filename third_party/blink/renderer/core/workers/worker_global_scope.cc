694109cee38d32c23461dccb25c164098dd2d3f3
Hiroshige Hayashizaki <hiroshige@chromium.org>
2020-08-06 10:14:34 +0000

    Introduce WorkerOrWorkletScriptController::EvaluateAndReturnValue()
    To unify WorkerOrWorkletScriptController entry point, this CL
    
    - Renames WorkerOrWorkletScriptController::Evaluate() to
      EvaluateAndReturnValue(),
    - Makes it (and EvaluateInternal()) return v8::Local<v8::Value>,
    - Moves ScriptState::Scope to callers of
      EvaluateAndReturnValue(), and
    - Merges EvaluateAndReturnValueForTest() to
      EvaluateAndReturnValue().
    
    This CL doesn't change the non-test behavior.
    
    This CL changes EvaluateAndReturnValue() behavior in tests:
    Previously, for
    EvaluateAndReturnValueForTest(ScriptSourceCode("someJavaScriptValue")),
    when `someJavaScriptValue` is evaluated to `undefined`,
    EvaluateAndReturnValueForTest() returned an empty ScriptValue().
    After this CL, EvaluateAndReturnValue() returns a
    v8::Local<v8::Value> representing `undefined`, not
    an empty v8::Local<v8::Value>.
    However, this doesn't affect observable test behavior
    (`undefined` is not expected anyway in existing tests).
    
    Bug: 1111134
    Change-Id: If4a319d1fce2a69a4f39d7ab611ebc6ea0d9f957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2331999
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795404}

--- third_party/blink/renderer/core/workers/worker_global_scope.cc
+++ third_party/blink/renderer/core/workers/worker_global_scope.cc
@@ -304,7 +304,8 @@ void WorkerGlobalScope::ImportScriptsInternal(const Vector<String>& urls,
                                                 std::move(cached_meta_data)));
     ReportingProxy().WillEvaluateImportedClassicScript(
         source_code.length(), handler ? handler->GetCodeCacheSize() : 0);
-    ScriptController()->Evaluate(
+    ScriptState::Scope scope(ScriptController()->GetScriptState());
+    ScriptController()->EvaluateAndReturnValue(
         ScriptSourceCode(source_code, ScriptSourceLocationType::kUnknown,
                          handler,
                          ScriptSourceCode::UsePostRedirectURL() ? response_url
