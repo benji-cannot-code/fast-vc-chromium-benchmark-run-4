cd3051e937bb8d6f0ac3e9fde6981434a91d0469
Maxim Kolosovskiy <kolos@chromium.org>
2020-08-06 09:22:36 +0000

    [Password Manager] Pressing "Change password" buttons launches CCT
    Bug: 1086114, 1092444
    Change-Id: I2906d756d0090734b23a711da27f497ec7296937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2336595
    Commit-Queue: Maxim Kolosovskiy  <kolos@chromium.org>
    Reviewed-by: Lukasz Suder <lsuder@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795390}

--- chrome/browser/password_check/android/junit/src/org/chromium/chrome/browser/password_check/PasswordCheckControllerTest.java
+++ chrome/browser/password_check/android/junit/src/org/chromium/chrome/browser/password_check/PasswordCheckControllerTest.java
@@ -27,6 +27,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import org.chromium.base.Consumer;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.password_check.PasswordCheckProperties.ItemType;
@@ -54,6 +55,10 @@ public class PasswordCheckControllerTest {
     @Mock
     private PasswordCheckComponentUi.Delegate mDelegate;
     @Mock
+    private Consumer<String> mLaunchCctWithChangePasswordUrlConsumer;
+    @Mock
+    private Consumer<CompromisedCredential> mLaunchCctWithScriptConsumer;
+    @Mock
     private PasswordCheck mPasswordCheck;
 
     // DO NOT INITIALIZE HERE! The objects would be shared here which leaks state between tests.
@@ -64,7 +69,8 @@ public class PasswordCheckControllerTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         mModel = PasswordCheckProperties.createDefaultModel();
-        mMediator = new PasswordCheckMediator();
+        mMediator = new PasswordCheckMediator(
+                mLaunchCctWithChangePasswordUrlConsumer, mLaunchCctWithScriptConsumer);
         PasswordCheckFactory.setPasswordCheckForTesting(mPasswordCheck);
         mMediator.initialize(mModel, mDelegate);
     }
@@ -122,5 +128,17 @@ public class PasswordCheckControllerTest {
     public void testRemovingElementTriggersDelegate() {
         mMediator.onRemove(ANA);
         verify(mDelegate).removeCredential(eq(ANA));
+    }
+
+    @Test
+    public void testOnChangePasswordButtonClick() {
+        mMediator.onChangePasswordButtonClick(ANA);
+        verify(mLaunchCctWithChangePasswordUrlConsumer).accept(eq(ANA.getOriginUrl()));
+    }
+
+    @Test
+    public void testOnChangePasswordWithScriptButtonClick() {
+        mMediator.onChangePasswordWithScriptButtonClick(BOB);
+        verify(mLaunchCctWithScriptConsumer).accept(eq(BOB));
     }
 }
