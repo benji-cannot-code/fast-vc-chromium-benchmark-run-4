3b4314c88ecb6989c0312148bba73694e0089308
Brian Sheedy <bsheedy@chromium.org>
2020-08-06 19:28:33 +0000

    Revert "Disable minidump tests on Linux"
    This reverts commit 6ae1f4216baef155c508a59f481d5cb6fe263026.
    
    Reason for revert: Incorrect test disabled.
    
    Original change's description:
    > Disable minidump tests on Linux
    >
    > Disables the Telemetry minidump symbolization tests on Linux, as they
    > are currently flaking pretty badly.
    >
    > TBR=johnchen@chromium.org
    >
    > Bug: 1108432
    > Change-Id: I3fab14b82de7440eff534169cf028d324d6f37c7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2333667
    > Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    > Reviewed-by: John Chen <johnchen@chromium.org>
    > Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#794208}
    
    TBR=bsheedy@chromium.org,johnchen@chromium.org
    
    Change-Id: I3cd4f00497602c008664e83413d6016e66e0b64d
    Bug: 1108432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2335482
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795585}

--- tools/perf/core/minidump_unittest.py
+++ tools/perf/core/minidump_unittest.py
@@ -36,7 +36,6 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
       'chromeos-local',
-      'linux',  # https://crbug.com/1108432
       'win7'  # https://crbug.com/1084931
   )
   def testSymbolizeMinidump(self):
@@ -82,7 +81,6 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # still read-only, so skip the test in that case.
   @decorators.Disabled(
       'chromeos-local',
-      'linux',  # https://crbug.com/1108432
       'win7'  # https://crbug.com/1084931
   )
   def testMultipleCrashMinidumps(self):
