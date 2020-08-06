596281228f7b692b70050a8892d689824bc37a9e
Ioana Pandele <ioanap@chromium.org>
2020-08-06 11:03:25 +0000

    [PwdCheckAndroid] Add basic functionality to PasswordCheckManager
    This CL adds
    - the ability to start/stop the check
    - check status update
    - compromised credentials change message to the UI
    - saved passwords fetched call for the UI (this is used by safety check)
    
    The list of compromised credentials as well as check progress updates
    will be added in the next CLs.
    
    Bug: 1102025, 1092444
    Change-Id: I3ad9af201d2158f956479a76b9ca8c184a05d397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2333845
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795413}

--- chrome/browser/password_check/android/password_check_bridge.h
+++ chrome/browser/password_check/android/password_check_bridge.h
@@ -8,14 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 #include "base/android/scoped_java_ref.h"
+#include "chrome/browser/password_check/android/password_check_manager.h"
 #include "chrome/browser/password_check/android/password_check_ui_status.h"
+#include "chrome/browser/profiles/profile_manager.h"
 
 // C++ counterpart of |PasswordCheckBridge.java|. Used to mediate the
 // communication between the UI and the password check logic.
-class PasswordCheckBridge {
+class PasswordCheckBridge : public PasswordCheckManager::Observer {
  public:
   explicit PasswordCheckBridge(
       const base::android::JavaParamRef<jobject>& java_bridge);
+  ~PasswordCheckBridge() override;
+
   PasswordCheckBridge(const PasswordCheckBridge&) = delete;
   PasswordCheckBridge& operator=(const PasswordCheckBridge&) = delete;
 
@@ -45,15 +49,26 @@ class PasswordCheckBridge {
   // Called by Java when the bridge is no longer needed. Destructs itself.
   void Destroy(JNIEnv* env);
 
+  // Called by the check manager when the saved passwords have been first loaded
+  // in memory. `count` is the number of saved passwords.
+  void OnSavedPasswordsFetched(int count) override;
+
+  // Called by the check manager whenever the stored compromised credentials
+  // change. `count` is the number of compromised credentials.
+  void OnCompromisedCredentialsChanged(int count) override;
+
   // Called by the check manager when the status of the check changes.
   void OnPasswordCheckStatusChanged(
-      password_manager::PasswordCheckUIStatus status);
+      password_manager::PasswordCheckUIStatus status) override;
 
  private:
-  ~PasswordCheckBridge();
-
   // The corresponding java object.
   base::android::ScopedJavaGlobalRef<jobject> java_bridge_;
+
+  // Manager handling the communication with the check service, owning and
+  // observing a `CompromisedCredentialManager` and a `SavedPasswordsPresenter`.
+  PasswordCheckManager check_manager_{ProfileManager::GetLastUsedProfile(),
+                                      this};
 };
 
 #endif  // CHROME_BROWSER_PASSWORD_CHECK_ANDROID_PASSWORD_CHECK_BRIDGE_H_
