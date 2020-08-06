e67b4043908149f14265cf0d761878936760116d
Yue Zhang <yuezhanggg@chromium.org>
2020-08-06 21:39:53 +0000

    [ntp][modules] Use ntp-grid in dummy module
    This CL adds a dummy ntp-grid into the dummy module. This works as a
    proof of concept that NTP-provided custom elements can be used in
    modules.
    
    Bug: 1110062
    Change-Id: Ib106888c99680f14ed8b0a1282f9660a46f4ffab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339338
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795649}

--- chrome/browser/resources/new_tab_page/modules/dummy/module.js
+++ chrome/browser/resources/new_tab_page/modules/dummy/module.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import '../../grid.js';
+
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {ModuleDescriptor} from '../module_descriptor.js';
@@ -19,6 +21,19 @@ class DummyModuleElement extends PolymerElement {
 
   static get template() {
     return html`{__html_template__}`;
+  }
+
+  static get properties() {
+    return {
+      tiles: {
+        type: Array,
+        value: () => ([
+          {label: 'item1', value: 'foo'},
+          {label: 'item2', value: 'bar'},
+          {label: 'item3', value: 'baz'},
+        ]),
+      }
+    };
   }
 }
 
