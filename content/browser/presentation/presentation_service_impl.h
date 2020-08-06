642f35ab893793ec3abf65e70c062fde905649bc
Clovis PJ <clovispj@google.com>
2020-08-06 14:40:04 +0000

    Change PresentationService to use ReceiverSet
    Bug: 1109123
    Change-Id: Ifa755c1d1a0325558f499ba0fc462559c9f47c30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316209
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: Clovis PJ <clovispj@google.com>
    Cr-Commit-Position: refs/heads/master@{#795457}

--- content/browser/presentation/presentation_service_impl.h
+++ content/browser/presentation/presentation_service_impl.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/frame_navigate_params.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
-#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/presentation/presentation.mojom.h"
 #include "url/gurl.h"
@@ -284,9 +284,8 @@ class CONTENT_EXPORT PresentationServiceImpl
   std::unordered_map<int, std::unique_ptr<NewPresentationCallbackWrapper>>
       pending_reconnect_presentation_cbs_;
 
-  // RAII receiver of |this| to PresentationService request.
-  mojo::Receiver<blink::mojom::PresentationService>
-      presentation_service_receiver_{this};
+  mojo::ReceiverSet<blink::mojom::PresentationService>
+      presentation_service_receivers_;
 
   // ID of the RenderFrameHost this object is associated with.
   int render_process_id_;
