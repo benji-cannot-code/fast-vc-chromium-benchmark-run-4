a06daad0c0e0eab5c5375be450478b79161af722
Alison Maher <almaher@microsoft.com>
2020-08-06 16:28:47 +0000

    [LayoutNG] Add new fragmentainers to hold OOF positioned elements
    If an OOF positioned element starts layout in a non-existing
    fragmentainer or fragments into a non-existing fragmentainer, new
    fragmentainers must be added to hold these OOF fragments. This
    change makes the necessary updates to handle the above cases.
    
    Bug: 1079031
    Change-Id: I643c1cbc8c30220d07ef9d0fbd843269eb1583c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2335643
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795505}

--- third_party/blink/renderer/core/layout/ng/ng_simplified_oof_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/ng_simplified_oof_layout_algorithm.h
@@ -27,7 +27,8 @@ class CORE_EXPORT NGSimplifiedOOFLayoutAlgorithm
                                NGBlockBreakToken> {
  public:
   NGSimplifiedOOFLayoutAlgorithm(const NGLayoutAlgorithmParams&,
-                                 const NGPhysicalBoxFragment&);
+                                 const NGPhysicalBoxFragment&,
+                                 bool is_new_fragment);
 
   scoped_refptr<const NGLayoutResult> Layout() override;
   MinMaxSizesResult ComputeMinMaxSizes(const MinMaxSizesInput&) const override {
