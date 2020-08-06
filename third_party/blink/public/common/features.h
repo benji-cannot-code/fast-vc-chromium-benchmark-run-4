c2c2ae54ba5513f5ceb5617f4171ad7e885ce2c5
Dominic Farolino <dom@chromium.org>
2020-08-06 22:28:00 +0000

    Add DelayAsyncScriptExecution param value to delegate to OptimizationGuide
    We'll soon be adding another Finch group that indicates to the client
    that we should defer to the BlinkOptimizationGuide (being implemented in
    [1]) for the delay async script execution milestone, as opposed to
    relying on the field trial param value being set by Finch.
    
    This CL adds the kOptimizationGuide value as an option for the
    kDelayAsyncScriptExecutionDelayParam field trial parameter. When the
    BlinkOptimizationGuide is implemented [1], there will be a follow-up to
    this CL that actually uses the OptimizationGuide hint once it is
    plumbed to Blink.
    
    [1]: https://crrev.com/c/2337050
    
    R=falken@chromium.org, kinuko@chromium.org, nhiroki@chromium.org
    
    Bug: 1086227
    Change-Id: I8b8419b03b2a52399971cd32262ba003f2750ab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2337767
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795678}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -187,6 +187,7 @@ BLINK_COMMON_EXPORT extern const base::Feature kDelayAsyncScriptExecution;
 enum class DelayAsyncScriptDelayType {
   kFinishedParsing,
   kFirstPaintOrFinishedParsing,
+  kUseOptimizationGuide,
 };
 BLINK_COMMON_EXPORT extern const base::FeatureParam<DelayAsyncScriptDelayType>
     kDelayAsyncScriptExecutionDelayParam;
