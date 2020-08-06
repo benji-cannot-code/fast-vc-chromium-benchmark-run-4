07d0128a156c973ac304423f5e377b8dc60e9c8d
Patrick Noland <pnoland@chromium.org>
2020-08-06 15:30:25 +0000

    Always call set on mIntentWithEffectSupplier
    Bug: 1111296
    Change-Id: I7f0609ee89c00362885cf9ab8df1b41d26ee453e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339703
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795475}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1112,13 +1112,14 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
             if (getSavedInstanceState() == null && intent != null) {
                 if (!mIntentHandler.shouldIgnoreIntent(intent)) {
                     mIntentWithEffect = mIntentHandler.onNewIntent(intent);
-                    mIntentWithEffectSupplier.set(mIntentWithEffect);
                 }
 
                 if (isMainIntentFromLauncher(intent)) {
                     logMainIntentBehavior(intent);
                 }
             }
+
+            mIntentWithEffectSupplier.set(mIntentWithEffect);
 
             // If we have tabs to reparent and getSavedInstanceState() is non-null, then the tabs
             // are coming from night mode tab reparenting. In this case, reparenting happens
