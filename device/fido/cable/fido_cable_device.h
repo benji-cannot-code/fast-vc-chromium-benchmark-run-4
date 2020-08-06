9108113c17fb4a340535b86b2c2c463a2e885fa7
Adam Langley <agl@chromium.org>
2020-08-06 17:31:42 +0000

    cablev2: disconnect on the desktop and mobile.
    This change disconnects caBLEv2 on the desktop and mobile so that
    further changes can be made incrementally. Otherwise, in order to
    keep everything working, large changes cannot be broken up.
    
    BUG=1002262
    
    Change-Id: If2e4c5c561fd2b57cd8f70dbe33c769109ca9c1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332375
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795534}

--- device/fido/cable/fido_cable_device.h
+++ device/fido/cable/fido_cable_device.h
@@ -23,10 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
-namespace cablev2 {
-class Crypter;
-}
-
 class BluetoothAdapter;
 class FidoBleFrame;
 
@@ -69,8 +65,6 @@ class COMPONENT_EXPORT(DEVICE_FIDO) FidoCableDevice : public FidoDevice {
   // Configure caBLE v1 keys.
   void SetV1EncryptionData(base::span<const uint8_t, 32> session_key,
                            base::span<const uint8_t, 8> nonce);
-  // Configure caBLE v2 keys.
-  void SetV2EncryptionData(std::unique_ptr<cablev2::Crypter> crypter);
 
   // SetCountersForTesting allows tests to set the message counters. Non-test
   // code must not call this function.
@@ -129,12 +123,6 @@ class COMPONENT_EXPORT(DEVICE_FIDO) FidoCableDevice : public FidoDevice {
   bool EncryptOutgoingMessage(std::vector<uint8_t>* message_to_encrypt);
   bool DecryptIncomingMessage(FidoBleFrame* incoming_frame);
 
-  static bool EncryptV1OutgoingMessage(
-      EncryptionData* encryption_data,
-      std::vector<uint8_t>* message_to_encrypt);
-  static bool DecryptV1IncomingMessage(EncryptionData* encryption_data,
-                                       FidoBleFrame* incoming_frame);
-
   base::OneShotTimer timer_;
 
   std::unique_ptr<FidoBleConnection> connection_;
@@ -150,7 +138,6 @@ class COMPONENT_EXPORT(DEVICE_FIDO) FidoCableDevice : public FidoDevice {
   Observer* observer_ = nullptr;
 
   base::Optional<EncryptionData> encryption_data_;
-  base::Optional<std::unique_ptr<cablev2::Crypter>> v2_crypter_;
   base::WeakPtrFactory<FidoCableDevice> weak_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(FidoCableDevice);
