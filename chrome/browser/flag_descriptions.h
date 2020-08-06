0c9f8af9b4678794dcbe16bd0436b42741bb100e
Alexey Baskakov <loyso@chromium.org>
2020-08-06 07:52:40 +0000

    Revert "Reland "Tab Search: Add chrome://flags entry for Tab Search""
    This reverts commit dcf6cef17b03ef0ceaf9bb89a2dd2bcac2aad428.
    
    Reason for revert: Fails
    https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome/7545
    
    Original change's description:
    > Reland "Tab Search: Add chrome://flags entry for Tab Search"
    > 
    > This is a reland of 5da77d649b71bdf592cd15224678b1926acf809c
    > 
    > Original change's description:
    > > Tab Search: Add chrome://flags entry for Tab Search
    > > 
    > > This addition is necessary to allow users to disable the feature once
    > > the experiment is enabled.
    > > 
    > > Bug: 1099917
    > > Change-Id: I4e537a1fe92d1329c75ff8c190f92ce7f1cde8b7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2336020
    > > Reviewed-by: Robert Liao <robliao@chromium.org>
    > > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#794867}
    > 
    > Bug: 1099917
    > Change-Id: I639aea54269a221d32049f98b715b4853c94f2bd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339922
    > Reviewed-by: Robert Liao <robliao@chromium.org>
    > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#795337}
    
    TBR=robliao@chromium.org,tluk@chromium.org
    
    Change-Id: I5b4d345cc87d982139157babd52918c2fd89e70d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1099917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340353
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795378}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -565,11 +565,6 @@ extern const char kEnableSubresourceRedirectDescription[];
 extern const char kEnableSyncTrustedVaultName[];
 extern const char kEnableSyncTrustedVaultDescription[];
 
-#if BUILDFLAG(ENABLE_TAB_SEARCH)
-extern const char kEnableTabSearchName[];
-extern const char kEnableTabSearchDescription[];
-#endif  // BUILDFLAG(ENABLE_TAB_SEARCH)
-
 extern const char kEnableTextFragmentAnchorName[];
 extern const char kEnableTextFragmentAnchorDescription[];
 
