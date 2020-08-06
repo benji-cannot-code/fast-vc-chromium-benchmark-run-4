063161de79816dd027805f5fea3b6457ff8f6b85
Kenichi Ishibashi <bashi@chromium.org>
2020-08-06 23:01:53 +0000

    Reland "service worker: Use mojo version of GetUserDataForAllRegistrations"
    This is a reland of 853e0dd9d54bdab27fa032f88f8904dd5626ce60
    
    The original CL was reverted due to a data race. The race was fixed
    in crrev.com/c/2340297
    
    Original change's description:
    > service worker: Use mojo version of GetUserDataForAllRegistrations
    >
    > This CL starts using mojo version of
    > GetUserDataForAllRegistrations{,ByKeyPrefix}().
    >
    > The type of the return value of these methods is also changed from
    > map<registration_id, value> to array<ServiceWorkerUserData>,
    > and ServiceWorkerUserData now has registration_id.
    > These changes are required to handle a situation where a
    > registration_id is associated with multiple values. Some features like
    > payments could associate multiple values for a registration_id.
    >
    > Bug: 1055677
    > Change-Id: I29e9ae7c1722316b2ac2f163cfc5b50fe7590d54
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2331376
    > Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#794447}
    
    Bug: 1055677
    Change-Id: I9f9ea4f6deb9953c4b18af941e6e8b70b66afb66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340417
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795690}

--- content/browser/service_worker/service_worker_database.cc
+++ content/browser/service_worker/service_worker_database.cc
@@ -1112,7 +1112,7 @@ ServiceWorkerDatabase::Status ServiceWorkerDatabase::RewriteDB() {
 ServiceWorkerDatabase::Status
 ServiceWorkerDatabase::ReadUserDataForAllRegistrations(
     const std::string& user_data_name,
-    std::vector<std::pair<int64_t, std::string>>* user_data) {
+    std::vector<storage::mojom::ServiceWorkerUserDataPtr>* user_data) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(user_data->empty());
 
@@ -1154,7 +1154,8 @@ ServiceWorkerDatabase::ReadUserDataForAllRegistrations(
         user_data->clear();
         break;
       }
-      user_data->push_back(std::make_pair(registration_id, value));
+      user_data->emplace_back(storage::mojom::ServiceWorkerUserData::New(
+          registration_id, user_data_name, value));
     }
   }
 
@@ -1165,7 +1166,7 @@ ServiceWorkerDatabase::ReadUserDataForAllRegistrations(
 ServiceWorkerDatabase::Status
 ServiceWorkerDatabase::ReadUserDataForAllRegistrationsByKeyPrefix(
     const std::string& user_data_name_prefix,
-    std::vector<std::pair<int64_t, std::string>>* user_data) {
+    std::vector<storage::mojom::ServiceWorkerUserDataPtr>* user_data) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(user_data->empty());
 
@@ -1222,7 +1223,8 @@ ServiceWorkerDatabase::ReadUserDataForAllRegistrationsByKeyPrefix(
         user_data->clear();
         break;
       }
-      user_data->push_back(std::make_pair(registration_id, value));
+      user_data->push_back(storage::mojom::ServiceWorkerUserData::New(
+          registration_id, parts[0], value));
     }
   }
 
