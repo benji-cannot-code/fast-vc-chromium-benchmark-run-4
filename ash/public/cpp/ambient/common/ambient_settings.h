6421035f7937a41795601443a5bafbc8dc7dc777
Jeffrey Young <cowmoo@google.com>
2020-08-06 16:34:56 +0000

    ambient: enum for temperature unit
    BUG=b:154046129
    
    Change-Id: Ifa9f24aa82da6e0151181324d921591fad88b1b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339795
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795509}

--- ash/public/cpp/ambient/common/ambient_settings.h
+++ ash/public/cpp/ambient/common/ambient_settings.h
@@ -51,6 +51,13 @@ struct ASH_PUBLIC_EXPORT ArtSetting {
   std::string preview_image_url;
 };
 
+enum class AmbientModeTemperatureUnit {
+  kMinValue = 0,
+  kFahrenheit = kMinValue,
+  kCelsius = 1,
+  kMaxValue = kCelsius
+};
+
 struct ASH_PUBLIC_EXPORT AmbientSettings {
   AmbientSettings();
   AmbientSettings(const AmbientSettings&);
@@ -59,13 +66,16 @@ struct ASH_PUBLIC_EXPORT AmbientSettings {
   AmbientSettings& operator=(AmbientSettings&&);
   ~AmbientSettings();
 
-  AmbientModeTopicSource topic_source;
+  AmbientModeTopicSource topic_source = AmbientModeTopicSource::kArtGallery;
 
   // Only a subset Settings of Art gallery.
   std::vector<ArtSetting> art_settings;
 
   // Only selected album.
   std::vector<std::string> selected_album_ids;
+
+  AmbientModeTemperatureUnit temperature_unit =
+      AmbientModeTemperatureUnit::kFahrenheit;
 };
 
 struct ASH_PUBLIC_EXPORT PersonalAlbum {
