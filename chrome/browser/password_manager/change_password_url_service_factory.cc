bc38924d8c4bd66c38920eee19a2ca84c70b345e
Jonathan Mengedoht <mengedoht@google.com>
2020-08-06 12:27:10 +0000

    Add Gstatic request to ChangePasswordUrlService
    Bug: 1086141
    Change-Id: Ied9a69a6cb27e0f6c966075d6cfe2c07f4f45c57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287471
    Commit-Queue: Jonathan Mengedoht <mengedoht@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795430}

--- chrome/browser/password_manager/change_password_url_service_factory.cc
+++ chrome/browser/password_manager/change_password_url_service_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/change_password_url_service_factory.h"
 
 #include "base/no_destructor.h"
+#include "chrome/browser/profiles/profile.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/password_manager/core/browser/change_password_url_service_impl.h"
 #include "content/public/browser/browser_context.h"
@@ -37,5 +38,6 @@ KeyedService* ChangePasswordUrlServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   return new password_manager::ChangePasswordUrlServiceImpl(
       content::BrowserContext::GetDefaultStoragePartition(context)
-          ->GetURLLoaderFactoryForBrowserProcess());
+          ->GetURLLoaderFactoryForBrowserProcess(),
+      Profile::FromBrowserContext(context)->GetPrefs());
 }
