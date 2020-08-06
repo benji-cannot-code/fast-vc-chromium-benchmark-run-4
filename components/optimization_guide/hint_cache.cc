d7a7571ba76eabde3936863976e5a4e932f31ed8
Sophie Chang <sophiechang@chromium.org>
2020-08-06 03:58:06 +0000

    Send up supported key representations so we get host-keyed hints
    Bug: 1110487
    Change-Id: I78eab25150fd7424f06a3d9b6f244ad00e214b86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2333821
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795333}

--- components/optimization_guide/hint_cache.cc
+++ components/optimization_guide/hint_cache.cc
@@ -319,7 +319,6 @@ bool HintCache::ProcessAndCacheHints(
             : clock_->Now() + features::URLKeyedHintValidCacheDuration();
 
     switch (hint.key_representation()) {
-      case proto::HOST_SUFFIX:
       case proto::HOST:
         host_keyed_cache_.Put(
             hint_key,
@@ -337,6 +336,7 @@ bool HintCache::ProcessAndCacheHints(
               std::make_unique<MemoryHint>(expiry_time, std::move(hint)));
         }
         break;
+      case proto::HOST_SUFFIX:
       case proto::REPRESENTATION_UNSPECIFIED:
         NOTREACHED();
         break;
