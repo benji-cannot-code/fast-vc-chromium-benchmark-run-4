f76cee77e40ef0e5b855e7b62568518010953111
Irina Fedorova <irfedorova@google.com>
2020-08-07 08:34:19 +0000

    Add multiple IDs to changeSavedPassword
    This CL adds support for multiple IDs to changeSavedPassword.
    It allows to change saved password for account and device IDs.
    
    Bug: 377410
    Change-Id: Iaac317006f4086c32b8a007c04c21f3fd753d7c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2338797
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795827}

--- chrome/test/data/webui/settings/test_password_manager_proxy.js
+++ chrome/test/data/webui/settings/test_password_manager_proxy.js
@@ -300,8 +300,8 @@ export class TestPasswordManagerProxy extends TestBrowserProxy {
   }
 
   /** override */
-  changeSavedPassword(id, newPassword) {
-    this.methodCalled('changeSavedPassword', {id, newPassword});
+  changeSavedPassword(ids, newPassword) {
+    this.methodCalled('changeSavedPassword', {ids, newPassword});
     return Promise.resolve();
   }
 
