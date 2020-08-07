51bfbcd1a92f66672fd164c8039e871ae8ecc3b7
Ewann <ewannpv@google.com>
2020-08-07 10:37:09 +0000

    [iOS] Adds custom actions to the Bookmark Context Menu
    Adding:
    - Open in New Tab
    - Open in New Incognito Tab
    - Open in New Window (if Multi-Window is supported and iPad)
    - Delete
    - Edit
    
    Bug: 1093302
    Change-Id: Id18e46c4119cae3a9d253cea95c62c4764c1c0c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2336085
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795854}

--- ios/chrome/browser/ui/menu/action_factory.mm
+++ ios/chrome/browser/ui/menu/action_factory.mm
@@ -150,4 +150,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return action;
 }
 
+- (UIAction*)actionToEditWithBlock:(ProceduralBlock)block {
+  return [self actionWithTitle:l10n_util::GetNSString(IDS_IOS_EDIT_ACTION_TITLE)
+                         image:nil
+                          type:MenuActionType::Edit
+                         block:block];
+}
+
 @end
