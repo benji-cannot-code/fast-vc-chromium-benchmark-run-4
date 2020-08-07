a67a3faebdd08c33a5de9830778dbb1d0daf9cb6
Wenhan (Han) Zhang <zwenhan@google.com>
2020-08-07 22:09:50 +0000

    New histogram naming syntax using inline tokens
    First step in migrating to a more flexible histogram naming syntax using
    tokens and variants and instead of suffixes.
    
    Changed one UMA histogram to using token instead of suffixes, and made
    corresponding changes and tests in the model and extract_histograms.py.
    
    Change-Id: I57c20d3c172f5d3e2742b1f3459fd4f7cacf848e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2333508
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Wenhan (Han) Zhang <zwenhan@google.com>
    Cr-Commit-Position: refs/heads/master@{#796121}

--- tools/metrics/histograms/histogram_configuration_model.py
+++ tools/metrics/histograms/histogram_configuration_model.py
@@ -37,6 +37,7 @@ def _NaturalSortByName(node):
   convert = lambda text: int(text) if text.isdigit() else text
   return [convert(c) for c in re.split('([0-9]+)', name)]
 
+# The following types are used for enums.xml.
 _INT_TYPE = models.ObjectNodeType(
     'int',
     attributes=[
@@ -80,6 +81,38 @@ _ENUMS_TYPE = models.ObjectNodeType(
         models.ChildType(_ENUM_TYPE.tag, _ENUM_TYPE, multiple=True),
     ])
 
+# The following types are used for histograms.xml.
+_VARIANT_TYPE = models.ObjectNodeType(
+    'variant',
+    attributes=[
+        ('name', str, None),
+        ('label', str, None),
+    ],
+    alphabetization=[
+        (_OBSOLETE_TYPE.tag, _KEEP_ORDER),
+    ],
+    required_attributes=['name', 'label'],
+    children=[
+        models.ChildType(_OBSOLETE_TYPE.tag, _OBSOLETE_TYPE, multiple=False),
+    ])
+
+_TOKEN_TYPE = models.ObjectNodeType(
+    'token',
+    attributes=[
+        ('key', str, None),
+    ],
+    required_attributes=['key'],
+    alphabetization=[
+        (_OBSOLETE_TYPE.tag, _KEEP_ORDER),
+        (_OWNER_TYPE.tag, _KEEP_ORDER),
+        (_VARIANT_TYPE.tag, _NaturalSortByName)
+    ],
+    children=[
+        models.ChildType(_OBSOLETE_TYPE.tag, _OBSOLETE_TYPE, multiple=False),
+        models.ChildType(_OWNER_TYPE.tag, _OWNER_TYPE, multiple=True),
+        models.ChildType(_VARIANT_TYPE.tag, _VARIANT_TYPE, multiple=True),
+    ])
+
 _HISTOGRAM_TYPE = models.ObjectNodeType(
     'histogram',
     attributes=[
@@ -98,6 +131,7 @@ _HISTOGRAM_TYPE = models.ObjectNodeType(
         (_OWNER_TYPE.tag, _KEEP_ORDER),
         (_SUMMARY_TYPE.tag, _KEEP_ORDER),
         (_DETAILS_TYPE.tag, _KEEP_ORDER),
+        (_TOKEN_TYPE.tag, _KEEP_ORDER),
     ],
     extra_newlines=(1, 1, 1),
     children=[
@@ -105,11 +139,14 @@ _HISTOGRAM_TYPE = models.ObjectNodeType(
         models.ChildType(_OWNER_TYPE.tag, _OWNER_TYPE, multiple=True),
         models.ChildType(_SUMMARY_TYPE.tag, _SUMMARY_TYPE, multiple=False),
         models.ChildType(_DETAILS_TYPE.tag, _DETAILS_TYPE, multiple=False),
+        models.ChildType(_TOKEN_TYPE.tag, _TOKEN_TYPE, multiple=True),
     ])
 
 _HISTOGRAMS_TYPE = models.ObjectNodeType(
     'histograms',
-    alphabetization=[(_HISTOGRAM_TYPE.tag, _LOWERCASE_FN('name'))],
+    alphabetization=[
+        (_HISTOGRAM_TYPE.tag, _LOWERCASE_FN('name')),
+    ],
     extra_newlines=(2, 1, 1),
     indent=False,
     children=[
