d91c55dbe373e2bf7b7981539a489cc7463de473
Thomas Lukaszewicz <tluk@chromium.org>
2020-08-07 16:16:56 +0000

    Add additional metrics collection to Tab Search
    This CL introduces the following UMA histograms:
    	Tabs.TabSearch.NumTabsClosedPerInstance
    	Tabs.TabSearch.NumTabsOnOpen
    	Tabs.TabSearch.NumWindowsOnOpen
    
    These histograms aim to measure the number of windows and tabs
    sent to the Tab Search WebUI in the initial payload and the number
    of tabs closed via Tab Search controls in a given instance of the UI.
    
    See doc below for details:
    https://docs.google.com/spreadsheets/d/15ygVicrrbJUJOzTn6IlnogxktExXJQzZ2PCSGMQFpWg
    
    Bug: 1099917
    Change-Id: Ib558fdfb9dc485e464978ff99c2b6ca7b8f93547
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340868
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795952}

--- chrome/browser/ui/webui/tab_search/tab_search_page_handler.h
+++ chrome/browser/ui/webui/tab_search/tab_search_page_handler.h
@@ -22,6 +22,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Browser;
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class TabSearchCloseAction {
+  kNoAction = 0,
+  kTabSwitch = 1,
+  kMaxValue = kTabSwitch,
+};
+
 class TabSearchPageHandler : public tab_search::mojom::PageHandler,
                              public TabStripModelObserver,
                              public BrowserTabStripTrackerDelegate {
@@ -86,6 +94,18 @@ class TabSearchPageHandler : public tab_search::mojom::PageHandler,
   content::WebUI* const web_ui_;
   BrowserTabStripTracker browser_tab_strip_tracker_{this, this};
   std::unique_ptr<base::RetainingOneShotTimer> debounce_timer_;
+
+  // Tracks how many times |CloseTab()| has been evoked for the currently open
+  // instance of Tab Search for logging in UMA.
+  int num_tabs_closed_ = 0;
+
+  // Tracks whether or not we have sent the initial payload to the Tab Search
+  // UI for metric collection purposes.
+  bool sent_initial_payload_ = false;
+
+  // Tracks whether the user has evoked |SwitchToTab()| for metric collection
+  // purposes.
+  bool called_switch_to_tab_ = false;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_TAB_SEARCH_TAB_SEARCH_PAGE_HANDLER_H_
