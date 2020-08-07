37adcbc146bdfeafb920522d6434cc4a65f445d0
Regan Hsu <hsuregan@google.com>
2020-08-07 02:56:55 +0000

    [CrOS Settings] Different linked pages depending on pre/post EOL.
    Before EOL, Eol warning notifications "Learn More" button and
    About Details Page "Learn More" link will link to Auto update page.
    
    After EOL, Eol final warning notification "Learn More" button and
    About Details Page "Learn More" link will link to EOL page.
    
    Also adds unit tests.
    
    Fixed: 1098797
    Change-Id: I411702fa107363beb3fb361337a1f257195ef0dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340105
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795768}

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -310,6 +310,9 @@ extern const char kEasyUnlockLearnMoreUrl[];
 // The URL for EOL notification
 extern const char kEolNotificationURL[];
 
+// The URL for Auto Update Policy.
+extern const char kAutoUpdatePolicyURL[];
+
 // The URL for providing more information about Google nameservers.
 extern const char kGoogleNameserversLearnMoreURL[];
 
