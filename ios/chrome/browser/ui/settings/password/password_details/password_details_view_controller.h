0cf5c572325d9753845e6b257a6e44f98bee318e
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2020-08-07 10:27:28 +0000

    Typos
    Bug: N/A
    Change-Id: I5d24e01cc4480190ac7db75535e6ec16b177dd52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332714
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795848}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_view_controller.h
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_view_controller.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Dispatcher for this ViewController.
 @property(nonatomic, weak) id<ApplicationCommands> commandsDispatcher;
 
-// Module containing the reauthentication mechanism for interections
+// Module containing the reauthentication mechanism for interactions
 // with password.
 @property(nonatomic, weak) id<ReauthenticationProtocol> reauthModule;
 
