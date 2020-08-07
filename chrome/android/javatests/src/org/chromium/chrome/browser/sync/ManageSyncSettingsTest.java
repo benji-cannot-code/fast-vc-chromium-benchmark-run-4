aceb8afa9a5c3abd7e63d0f49883e4d422e9f244
Tanmoy Mollik <triploblastic@google.com>
2020-08-07 09:49:18 +0000

    [Android] Add SyncOffPreference in ManageSyncSettings page
    This cl adds the SyncOffPreference in the ManageSyncSettings page. It
    also adds "Advanced" category after the Preference. The preference is
    only shown after the user has signed in. So it is not shown in the
    advanced sync consent flow.
    The code for ClearDataProgressDialog from AccountManagementFragment
    is reused here.
    
    Bug: 1092410
    Change-Id: I0b70ad5cfc32d4c8325680f9929aaae6ccfca65f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2273193
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795840}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
@@ -5,6 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.sync;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.app.Dialog;
 import android.support.test.InstrumentationRegistry;
 import android.widget.Button;
@@ -212,6 +218,26 @@ public class ManageSyncSettingsTest {
         // empty set in the backend. But sync stop request should not be called.
         mSyncTestRule.togglePreference(getSyncEverything(fragment));
         Assert.assertTrue(SyncTestUtil.isSyncRequested());
+    }
+
+    @Test
+    @LargeTest
+    @Feature({"Sync"})
+    @Features.EnableFeatures(ChromeFeatureList.MOBILE_IDENTITY_CONSISTENCY)
+    public void testPressingTurnOffSyncAndSignOutShowsSignOutDialog() {
+        mSyncTestRule.setUpAccountAndSignInForTesting();
+        mSyncTestRule.setChosenDataTypes(true, null);
+        mSyncTestRule.startSync();
+        ManageSyncSettings fragment = startManageSyncPreferences();
+
+        Preference turnOffSyncPreference =
+                fragment.findPreference(ManageSyncSettings.PREF_TURN_OFF_SYNC);
+        Assert.assertTrue("Turn off sync and sign out button should be shown",
+                turnOffSyncPreference.isVisible());
+        TestThreadUtils.runOnUiThreadBlocking(
+                fragment.findPreference(ManageSyncSettings.PREF_TURN_OFF_SYNC)::performClick);
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+        onView(withText(R.string.signout_title)).inRoot(isDialog()).check(matches(isDisplayed()));
     }
 
     @Test
