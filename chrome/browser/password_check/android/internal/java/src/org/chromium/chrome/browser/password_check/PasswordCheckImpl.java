32746de76a4de33d73ec77b3793933d2d021cbd1
Eleonora Rocchi <erocchi@google.com>
2020-08-07 16:52:53 +0000

    [PwdCheckAndroid] Retrieve the timestamp of the last check from PasswordCheckBridge.
    This CL adds an empty method to the bridge to retrieve the timestamp of
    the last completed password check. It also adds the logic to call this
    method from PasswordCheckMediator.
    
    Bug: 1109691, 1092444
    Change-Id: Ibaa0993e11271b4ae0ee8287a86a51e7023f3bc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343268
    Commit-Queue: Eleonora Rocchi <erocchi@google.com>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795962}

--- chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckImpl.java
+++ chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckImpl.java
@@ -97,6 +97,11 @@ class PasswordCheckImpl implements PasswordCheck, PasswordCheckObserver {
     }
 
     @Override
+    public long getCheckTimestamp() {
+        return mPasswordCheckBridge.getCheckTimestamp();
+    }
+
+    @Override
     public int getCompromisedCredentialsCount() {
         return mPasswordCheckBridge.getCompromisedCredentialsCount();
     }
