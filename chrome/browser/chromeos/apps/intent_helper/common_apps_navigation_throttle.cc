c615c5684f5da790ae8d8d615cbc500c7a02e667
Maggie Cai <mxcai@chromium.org>
2020-08-07 05:54:44 +0000

    [IntentHandling] Reparent the web app from intent picker.
    Currently the intent picker launched the web app when user select the
    web app. This CL updates to reparent the web app instead of launch.
    
    BUG=1113969, 853604
    
    Change-Id: Ie7a790999f68696a9c1d58bb1ef6e1e1cd8112b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2341228
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795799}

--- chrome/browser/chromeos/apps/intent_helper/common_apps_navigation_throttle.cc
+++ chrome/browser/chromeos/apps/intent_helper/common_apps_navigation_throttle.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/apps/metrics/intent_handling_metrics.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/intent_picker_tab_helper.h"
+#include "chrome/browser/ui/web_applications/web_app_launch_utils.h"
 #include "chrome/common/chrome_features.h"
 #include "chromeos/constants/chromeos_switches.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
@@ -119,16 +120,20 @@ void CommonAppsNavigationThrottle::OnIntentPickerClosed(
     proxy->AddPreferredApp(launch_name, url);
 
   if (should_launch_app) {
-    // TODO(crbug.com/853604): Distinguish the source from link and omnibox.
-    apps::mojom::LaunchSource launch_source =
-        apps::mojom::LaunchSource::kFromLink;
-    proxy->LaunchAppWithUrl(
-        launch_name,
-        GetEventFlags(apps::mojom::LaunchContainer::kLaunchContainerWindow,
-                      WindowOpenDisposition::NEW_WINDOW,
-                      /*prefer_container=*/true),
-        url, launch_source, display::kDefaultDisplayId);
-    CloseOrGoBack(web_contents);
+    if (entry_type == PickerEntryType::kWeb) {
+      web_app::ReparentWebContentsIntoAppBrowser(web_contents, launch_name);
+    } else {
+      // TODO(crbug.com/853604): Distinguish the source from link and omnibox.
+      apps::mojom::LaunchSource launch_source =
+          apps::mojom::LaunchSource::kFromLink;
+      proxy->LaunchAppWithUrl(
+          launch_name,
+          GetEventFlags(apps::mojom::LaunchContainer::kLaunchContainerWindow,
+                        WindowOpenDisposition::NEW_WINDOW,
+                        /*prefer_container=*/true),
+          url, launch_source, display::kDefaultDisplayId);
+      CloseOrGoBack(web_contents);
+    }
   }
 
   apps::AppsNavigationThrottle::PickerAction action =
