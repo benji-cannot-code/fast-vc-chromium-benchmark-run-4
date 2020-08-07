72f9397907813ad39856d23142af6f416396c3db
Bella Bah <bellabah@chromium.org>
2020-08-07 20:22:33 +0000

    Renamed parser.py to generator_utils to prevent ImportError
    Was causing Windows trybot traffic_annotation tests to fail because of
    module naming collision.
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8872768115210425728/+/steps/test_traffic_annotation_auditor/0/stdout
    
    Change-Id: I5205f8232c633cb9028de667472f4e80f869d973
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339941
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Mohamadou Bella Bah <bellabah@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796069}

--- tools/traffic_annotation/scripts/generator_utils.py
+++ tools/traffic_annotation/scripts/generator_utils.py
@@ -23,7 +23,6 @@ import sys
 import io
 import re
 
-
 TrafficAnnotation = namedtuple(
     "TrafficAnnotation",
     ["unique_id", "description", "trigger", "data", "settings", "policy"])
@@ -34,6 +33,7 @@ class Placeholder(str, enum.Enum):
   SENDER = "sender"
   ANNOTATION = "annotation"
   ANNOTATION_BOLD = "annotation_bold"
+
 
 PLACEHOLDER_STYLES = {
     Placeholder.GROUP: {
@@ -196,7 +196,9 @@ class XMLParser:
                                              "NA")
 
     return {
-      "type": Placeholder.ANNOTATION, "traffic_annotation": traffic_annotation}
+        "type": Placeholder.ANNOTATION,
+        "traffic_annotation": traffic_annotation
+    }
 
   def build_placeholders(self):
     """
