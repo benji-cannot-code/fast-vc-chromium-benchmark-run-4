8f89c8e961583c45497b203dab2a72afb6590fa9
Jose Dapena Paz <jdapena@igalia.com>
2020-08-07 14:44:27 +0000

    IWYU: std::vector used in fuzzy_finder.h requires including <vector>
    Build with libstdc++ fails with:
    In file included from ../../chrome/browser/ui/commander/fuzzy_finder.cc:5:
    ../../chrome/browser/ui/commander/fuzzy_finder.h:30:23: error: ‘std::vector’ has not been declared
       30 |                  std::vector<gfx::Range>* matched_ranges);
          |                       ^~~~~~
    
    Bug: 957519
    Change-Id: Id290b8f93e0f5741d88d6a6e4c75f3c0c0e9f636
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340038
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#795906}

--- chrome/browser/ui/commander/fuzzy_finder.h
+++ chrome/browser/ui/commander/fuzzy_finder.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_COMMANDER_FUZZY_FINDER_H_
 #define CHROME_BROWSER_UI_COMMANDER_FUZZY_FINDER_H_
 
+#include <vector>
+
 #include "base/strings/string16.h"
 #include "ui/gfx/range/range.h"
 
