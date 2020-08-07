8702e15e95ad4f06192f6345443bd10457ed5a44
chinsenj <chinsenj@chromium.org>
2020-08-07 01:01:37 +0000

    cros: Close wallpaper preview when window cycle (alt-tab) starts
    If a user is previewing a wallpaper and begins to cycle windows, the
    preview item for the wallpaper picker is empty/transparent.
    
    Now when the window cycle starts up, it closes the wallpaper preview if
    it is open so the preview item displays the wallpaper picker properly.
    
    Test: WallpaperControllerTest.ClosePreviewWallpaperOnWindowCycleStart
    Bug: 895265
    Change-Id: Ie1900f2ce172ec03407e6801c051d34f0a94d975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2341143
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Jeremy Chinsen <chinsenj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795739}

--- ash/wm/window_cycle_controller.cc
+++ ash/wm/window_cycle_controller.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
+#include "ash/wallpaper/wallpaper_controller_impl.h"
 #include "ash/wm/desks/desk.h"
 #include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/desks/desks_util.h"
@@ -90,6 +91,11 @@ void WindowCycleController::HandleCycleWindow(Direction direction) {
 }
 
 void WindowCycleController::StartCycling() {
+  // Close the wallpaper preview if it is open to prevent visual glitches where
+  // the window view item for the preview is transparent
+  // (http://crbug.com/895265).
+  Shell::Get()->wallpaper_controller()->MaybeClosePreviewWallpaper();
+
   WindowCycleList::WindowList window_list =
       Shell::Get()->mru_window_tracker()->BuildWindowForCycleWithPipList(
           features::IsAltTabLimitedToActiveDesk() ? kActiveDesk : kAllDesks);
