f491a4ae49534adfba479660dfec370f194db7fc
Thoren Paulson <thoren@chromium.org>
2020-08-07 19:51:19 +0000

    [Chromecast] Change GrContext include to GrDirectContext.
    Looks like this was missed in crrev.com/c/2297920
    
    Bug: internal b/162634401
    Test: CQ
    Change-Id: Iafbfb2dbcf24abe1e6e31e6d4b461908382309eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343504
    Commit-Queue: Thoren Paulson <thoren@chromium.org>
    Commit-Queue: Luke Halliwell (slow) <halliwell@chromium.org>
    Auto-Submit: Thoren Paulson <thoren@chromium.org>
    Reviewed-by: Luke Halliwell (slow) <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796058}

--- chromecast/browser/webview/client/webview.cc
+++ chromecast/browser/webview/client/webview.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chromecast/browser/webview/proto/webview.pb.h"
 #include "third_party/grpc/src/include/grpcpp/grpcpp.h"
-#include "third_party/skia/include/gpu/GrContext.h"
+#include "third_party/skia/include/gpu/GrDirectContext.h"
 #include "ui/gl/gl_bindings.h"
 
 namespace chromecast {
