23b369e26cd65bc426cb819584e1658c384cac4d
Denis Kuznetsov <antrim@google.com>
2020-08-07 12:21:38 +0000

    OOBE: Restore disabled OobeConfigurationTest tests
    Seems that changes made in recent month made testing code more
    stable, so tests are not flaky anymore.
    
    TEST=run tests with --gtest_repeat=25 --gtest_break_on_failure with
         affinity to 2 codes + simulated CPU load on same cores
    
    Bug: 997685
    Change-Id: I687e0191bddfe569d5a9fcdf60761c66024e7476
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339989
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795872}

--- chrome/browser/chromeos/login/configuration_based_oobe_browsertest.cc
+++ chrome/browser/chromeos/login/configuration_based_oobe_browsertest.cc
@@ -45,10 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using testing::_;
 using testing::Invoke;
 
-// Disabled due to flakiness: https://crbug.com/997685.
-#define MAYBE_TestDemoModeOfflineNetwork DISABLED_TestDemoModeOfflineNetwork
-#define MAYBE_TestDemoModeAcceptEula DISABLED_TestDemoModeAcceptEula
-
 namespace chromeos {
 
 // This test case will use
@@ -232,8 +228,7 @@ IN_PROC_BROWSER_TEST_F(OobeConfigurationTest, TestDemoModePreferences) {
 
 // Check that configuration lets correctly use offline demo mode on network
 // screen.
-IN_PROC_BROWSER_TEST_F(OobeConfigurationTest,
-                       MAYBE_TestDemoModeOfflineNetwork) {
+IN_PROC_BROWSER_TEST_F(OobeConfigurationTest, TestDemoModeOfflineNetwork) {
   LoadConfiguration();
   OobeScreenWaiter(DemoPreferencesScreenView::kScreenId).Wait();
   SimulateOfflineEnvironment();
@@ -242,7 +237,7 @@ IN_PROC_BROWSER_TEST_F(OobeConfigurationTest,
 
 // Check that configuration lets correctly use offline demo mode on EULA
 // screen.
-IN_PROC_BROWSER_TEST_F(OobeConfigurationTest, MAYBE_TestDemoModeAcceptEula) {
+IN_PROC_BROWSER_TEST_F(OobeConfigurationTest, TestDemoModeAcceptEula) {
   LoadConfiguration();
   OobeScreenWaiter(DemoPreferencesScreenView::kScreenId).Wait();
   SimulateOfflineEnvironment();
