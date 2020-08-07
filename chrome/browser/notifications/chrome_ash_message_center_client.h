e45b4aa0f838823a12ef1aa4e8a2d7598c702870
James Cook <jamescook@chromium.org>
2020-08-07 17:00:58 +0000

    Extract NotificationDisplayServiceDelegate into its own file
    I anticipate needing it for notifications for lacros. See
    go/lacros and go/lacros-notifications.
    
    Also, clean up BUILD.gn slightly -- enable_native_notifications is
    always true on Chrome OS.
    
    Bug: 1113889
    Change-Id: I6c3bc7f81f81157568f8c1b2969f98a82a037b6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339450
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795968}

--- chrome/browser/notifications/chrome_ash_message_center_client.h
+++ chrome/browser/notifications/chrome_ash_message_center_client.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "chrome/browser/notifications/notification_platform_bridge.h"
-#include "chrome/browser/notifications/notification_platform_bridge_chromeos.h"
+#include "chrome/browser/notifications/notification_platform_bridge_delegate.h"
 #include "chrome/browser/notifications/notifier_controller.h"
 
 // This class serves as Chrome's AshMessageCenterClient, as well as the
