3c43b5c1e866f7395ffaff6043cd791a241bd91e
Robert Ma <robertma@chromium.org>
2020-08-07 22:10:57 +0000

    [WPT] Refactor WebUSB tests to use test-only-api.js
    Also switch to loading *.mojom.js from /gen/ instead of
    resources/chromium in preparation for launching official MojoJS support
    in WPT. This would not change the test results on Chromium waterfall
    (everything should continue to pass) or upstream WPT (tests currently
    fail because MojoJS isn't enabled).
    
    Move web_tests/usb to web_tests/wpt_internal/webusb so that these tests
    are served the same was as web_tests/external/wpt/webusb, making the
    resource URLs consistent and saving a special case in the helper
    function.
    
    Finally, remove unused .mojom.js files from WPT.
    
    Bug: 1094512
    Change-Id: I75f3d1a7acf494e2c881c527618a496e907a9596
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276999
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796122}

--- third_party/blink/web_tests/external/wpt/webusb/idlharness.https.any.js
+++ third_party/blink/web_tests/external/wpt/webusb/idlharness.https.any.js
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: timeout=long
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js
+// META: script=/resources/test-only-api.js
 // META: script=/webusb/resources/fake-devices.js
 // META: script=/webusb/resources/usb-helpers.js
 
--- third_party/blink/web_tests/external/wpt/webusb/usbEndpoint.https.any.js
+++ third_party/blink/web_tests/external/wpt/webusb/usbEndpoint.https.any.js
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/test-only-api.js
 // META: script=/webusb/resources/fake-devices.js
 // META: script=/webusb/resources/usb-helpers.js
 'use strict';
