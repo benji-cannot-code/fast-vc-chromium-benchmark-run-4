29ea84436aa210520d6b2f5d1123bdd3e8dc1ee5
Greg Thompson <grt@chromium.org>
2020-08-07 10:08:22 +0000

    Revert "Delete spurious User Data directory created in users' AppData folder."
    Metrics show that a big surge of cleanups happened when the code
    shipped, and that there are now diminishing returns. This reverts commit
    6cd3373513980cc0a214b3c4778ee2c57d14efc1, modulo histogram definitions.
    
    BUG=980487,1097919
    
    Change-Id: Ib47cfbc402e6a73afa36e08209eaf5faa69e6592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340051
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795845}

--- chrome/browser/sync/profile_sync_service_factory.cc
+++ chrome/browser/sync/profile_sync_service_factory.cc
@@ -84,10 +84,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/constants/chromeos_features.h"
 #endif  // defined(OS_CHROMEOS)
 
-#if defined(OS_WIN)
-#include "chrome/browser/sync/roaming_profile_directory_deleter_win.h"
-#endif  // defined(OS_WIN)
-
 namespace {
 
 void UpdateNetworkTimeOnUIThread(base::Time network_time,
@@ -272,12 +268,6 @@ KeyedService* ProfileSyncServiceFactory::BuildServiceInstanceFor(
 
   auto pss =
       std::make_unique<syncer::ProfileSyncService>(std::move(init_params));
-
-#if defined(OS_WIN)
-  if (!local_sync_backend_enabled)
-    DeleteRoamingUserDataDirectoryLater();
-#endif
-
   pss->Initialize();
 
   // Hook PSS into PersonalDataManager (a circular dependency).
