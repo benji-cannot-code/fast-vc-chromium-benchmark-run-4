463ca52aa22ed801e74b445913af9fb6b267a1f1
Josh Simmons <simmonsjosh@google.com>
2020-08-07 04:47:52 +0000

    [Files.app] Add files-swa feature flag
    Add a Chromium feature flag to enable / disable Files.app running as a
    System Web Application (SWA).
    
    Bug: 1113981
    Change-Id: Ib829df993294bef692b14aed8dab3749c5d227e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2342227
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Josh Simmons <simmonsjosh@google.com>
    Cr-Commit-Position: refs/heads/master@{#795788}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2296,6 +2296,9 @@ extern const char kExperimentalAccessibilityCursorColorsDescription[];
 extern const char kFilesNGName[];
 extern const char kFilesNGDescription[];
 
+extern const char kFilesSWAName[];
+extern const char kFilesSWADescription[];
+
 extern const char kFilesTransferDetailsName[];
 extern const char kFilesTransferDetailsDescription[];
 
