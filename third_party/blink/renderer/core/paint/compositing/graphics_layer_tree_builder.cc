31b2d10340af1f35d7fd175c1586916f4f951c9f
Xianzhu Wang <wangxianzhu@chromium.org>
2020-08-07 15:56:09 +0000

    Remove CompositedLayerMapping::overflow_controlls_host_layer_
    The only purpose of it was to group the overflow control layers.
    With it removed, we can make all GraphicsLayers collectable as
    GraphicsLayerDisplayItems.
    
    Change-Id: Ic37fa3a3902fa3bb493c2b78b15c1df5bd1f6b4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2341038
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795942}

--- third_party/blink/renderer/core/paint/compositing/graphics_layer_tree_builder.cc
+++ third_party/blink/renderer/core/paint/compositing/graphics_layer_tree_builder.cc
@@ -167,11 +167,9 @@ void GraphicsLayerTreeBuilder::RebuildRecursive(
                 return a.second < b.second;
               });
     for (auto& item : pending) {
-      if (auto* layer = item.first->GetCompositedLayerMapping()
-                            ->DetachLayerForOverflowControls()) {
-        this_layer_children.insert(item.second + offset, layer);
-        offset++;
-      }
+      offset += item.first->GetCompositedLayerMapping()
+                    ->MoveOverflowControlLayersInto(this_layer_children,
+                                                    item.second + offset);
     }
 
     if (!this_layer_children.IsEmpty())
