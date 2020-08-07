1252a5f907d98afee2d7b655ff807288d5ed8430
Juliet Levesque <julietlevesque@google.com>
2020-08-07 17:43:58 +0000

    [Nearby] Update logging tab to make it more readable for users.
    Reverse the order of logs appearing on the logging tab so newest logs
    appear on top, as well as change the color of verbose logging to gray,
    as seen here: https://screenshot.googleplex.com/rAtwCW4rJbN.
    
    Bug: 1093634
    Change-Id: I88b3ed8abefec7d5512a261db41f6d464ef23947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2336311
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/master@{#795992}

--- chrome/browser/resources/nearby_internals/types.js
+++ chrome/browser/resources/nearby_internals/types.js
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @enum {number}
  */
 export const Severity = {
+  VERBOSE: -1,
   INFO: 0,
   WARNING: 1,
   ERROR: 2,
-  VERBOSE: 3
 };
 
 /**
