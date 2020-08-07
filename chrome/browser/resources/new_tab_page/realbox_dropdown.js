ae93db9d1fb9e4c7c7f6e4ed7a3e89f4c1021b5f
Tommy Li <tommycli@chromium.org>
2020-08-07 00:00:37 +0000

    [omnibox] Whole header row expands and collapses section (on Desktop).
    Previously, the user had to click on the header button to expand or
    collapse the section.
    
    This CL makes it so they can click anywhere on the row.
    
    This only affects the Views omnibox, and the NTP realbox, both on
    Desktop.
    
    Android already works like this, so it needs no further work.
    
    Bug: 1113341
    Change-Id: I64de6db54ee63de3d428de69aa23c2759effe1cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340266
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#795713}

--- chrome/browser/resources/new_tab_page/realbox_dropdown.js
+++ chrome/browser/resources/new_tab_page/realbox_dropdown.js
@@ -286,8 +286,8 @@ class RealboxDropdownElement extends PolymerElement {
    * @param {!Event} e
    * @private
    */
-  onToggleButtonClick_(e) {
-    const groupId = e.target.dataset.id;
+  onHeaderClick_(e) {
+    const groupId = e.currentTarget.dataset.id;
 
     // Tell the backend to toggle visibility of the given suggestion group ID.
     this.pageHandler_.toggleSuggestionGroupIdVisibility(groupId);
@@ -310,7 +310,7 @@ class RealboxDropdownElement extends PolymerElement {
       return;
     }
 
-    // Simulate a click so that it gets handled by |onToggleButtonClick_|.
+    // Simulate a click so that it gets handled by |onHeaderClick_|.
     e.target.click();
     e.preventDefault();  // Prevents default browser action.
   }
