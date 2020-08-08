d7031c583f0294f02e33447482702f8f21f99db3
Ken Rockot <rockot@google.com>
2020-08-08 00:13:02 +0000

    Experimental preemptive mobile network activation
    Introduces a new PreemptiveMobileNetworkActivation feature. When this is
    enabled, the network stack will ask the OS to activate a connected
    mobile network whenever it suspects the current default (Wi-Fi) network
    has degraded connectivity.
    
    This also adds a new histogram when the feature is enabled, to capture
    how often this preemptive mobile network activation is imminently
    followed by a system-wide default network change to that same network or
    any other network.
    
    Bug: 1079396
    Change-Id: Ib8b8294addb51c60b55cf512b293fa5418976452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339595
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#796179}

--- net/base/features.cc
+++ net/base/features.cc
@@ -168,5 +168,8 @@ const base::Feature kUseLookalikesForNavigationSuggestions{
 const base::Feature kReportPoorConnectivity{"ReportPoorConnectivity",
                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kPreemptiveMobileNetworkActivation{
+    "PreemptiveMobileNetworkActivation", base::FEATURE_DISABLED_BY_DEFAULT};
+
 }  // namespace features
 }  // namespace net
