2282ea8cb0671c4edcbcb6d9eb9f00b8bc12e5ab
Mugdha Lakhani <nator@chromium.org>
2020-08-08 07:31:09 +0000

    [WebLayer] Add prerender delegate impls and PrerenderManagerFactory.
    This adds PrerenderContentsDelegateImpl, PrerenderManagerDelegateImpl
    and PrerenderManagerFactory.
    
    A simple browsertest has been added to verify functionality.
    
    Bug: 1096088
    Change-Id: I8682eecc2deb10ac5da77d0892a7abf544aa839d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339463
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796225}

---
+++ weblayer/browser/no_state_prefetch/prerender_manager_delegate_impl.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBLAYER_BROWSER_NO_STATE_PREFETCH_PRERENDER_MANAGER_DELEGATE_IMPL_H_
+#define WEBLAYER_BROWSER_NO_STATE_PREFETCH_PRERENDER_MANAGER_DELEGATE_IMPL_H_
+
+#include "components/content_settings/core/browser/cookie_settings.h"
+#include "components/prerender/browser/prerender_manager_delegate.h"
+
+namespace content {
+class BrowserContext;
+}
+
+namespace weblayer {
+
+class PrerenderManagerDelegateImpl
+    : public prerender::PrerenderManagerDelegate {
+ public:
+  explicit PrerenderManagerDelegateImpl(
+      content::BrowserContext* browser_context);
+  ~PrerenderManagerDelegateImpl() override = default;
+
+  // PrerenderManagerDelegate overrides.
+  scoped_refptr<content_settings::CookieSettings> GetCookieSettings() override;
+  std::unique_ptr<prerender::PrerenderContentsDelegate>
+  GetPrerenderContentsDelegate() override;
+
+ private:
+  content::BrowserContext* browser_context_;
+};
+
+}  // namespace weblayer
+
+#endif  // WEBLAYER_BROWSER_NO_STATE_PREFETCH_PRERENDER_MANAGER_DELEGATE_IMPL_H_
