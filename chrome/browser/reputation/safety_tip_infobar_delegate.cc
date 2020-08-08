7dab9a5addc2c7cc01695345713e41e060613298
Joe DeBlasio <jdeblasio@chromium.org>
2020-08-08 01:37:55 +0000

    [Safety Tips] Update strings for Lookalike Safety Tip.
    This CL updates the strings to reflect our latest iteration with UX.
    
    Bug: 1114359
    Change-Id: Ica65640967f7323286f65cd5f31f1ba491b49f14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343661
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796196}

--- chrome/browser/reputation/safety_tip_infobar_delegate.cc
+++ chrome/browser/reputation/safety_tip_infobar_delegate.cc
@@ -102,5 +102,5 @@ void SafetyTipInfoBarDelegate::InfoBarDismissed() {
 }
 
 base::string16 SafetyTipInfoBarDelegate::GetDescriptionText() const {
-  return GetSafetyTipDescription(safety_tip_status_, url_, suggested_url_);
+  return GetSafetyTipDescription(safety_tip_status_, suggested_url_);
 }
