bcd3f3389dd1915fa7c942be00ad65339460bd4e
Justin Cohen <justincohen@google.com>
2020-08-08 00:47:48 +0000

    ios: Disable ARQuickLook tests on iOS12/Xcode12b4.
    XCUIElement queries used in ARQuickLook are broken on Xcode 12 beta 4
    when running on the iOS 12 simulator.  Disable until Xcode is fixed.
    
    This is a re-land of a reverted CL using testInvocations to disable
    instead. Unfortunately, this doesn't work in the test runner.
    
    Bug: 1114202
    Change-Id: Ibfd60c9bb4742c03e459080be81bb7f14dc9c9a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343552
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796187}

--- ios/chrome/browser/ui/download/ar_quick_look_egtest.mm
+++ ios/chrome/browser/ui/download/ar_quick_look_egtest.mm
@@ -94,6 +94,16 @@ std::unique_ptr<net::test_server::HttpResponse> GetResponse(
 
 // Tests that QLPreviewController is shown for sucessfully downloaded USDZ file.
 - (void)testDownloadUsdz {
+#if defined(__IPHONE_14_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_14_0
+  // TODO(crbug.com/1114202): The XCUIElement queries in this test are broken on
+  // Xcode 12 beta 4 when running on the iOS 12 simulator.  Disable until Xcode
+  // is fixed.
+  if (@available(iOS 13, *)) {
+  } else {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS12.");
+  }
+#endif
+
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
   [ChromeEarlGrey waitForWebStateContainingText:"Good"];
   [ChromeEarlGrey tapWebStateElementWithID:@"good"];
@@ -126,6 +136,16 @@ std::unique_ptr<net::test_server::HttpResponse> GetResponse(
 }
 
 - (void)testDownloadUnauthorized {
+#if defined(__IPHONE_14_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_14_0
+  // TODO(crbug.com/1114202): The XCUIElement queries in this test are broken on
+  // Xcode 12 beta 4 when running on the iOS 12 simulator.  Disable until Xcode
+  // is fixed.
+  if (@available(iOS 13, *)) {
+  } else {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS12.");
+  }
+#endif
+
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
   [ChromeEarlGrey waitForWebStateContainingText:"Unauthorized"];
   [ChromeEarlGrey tapWebStateElementWithID:@"unauthorized"];
@@ -158,6 +178,16 @@ std::unique_ptr<net::test_server::HttpResponse> GetResponse(
 }
 
 - (void)testDownloadForbidden {
+#if defined(__IPHONE_14_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_14_0
+  // TODO(crbug.com/1114202): The XCUIElement queries in this test are broken on
+  // Xcode 12 beta 4 when running on the iOS 12 simulator.  Disable until Xcode
+  // is fixed.
+  if (@available(iOS 13, *)) {
+  } else {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS12.");
+  }
+#endif
+
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
   [ChromeEarlGrey waitForWebStateContainingText:"Forbidden"];
   [ChromeEarlGrey tapWebStateElementWithID:@"forbidden"];
@@ -190,6 +220,16 @@ std::unique_ptr<net::test_server::HttpResponse> GetResponse(
 }
 
 - (void)testDownloadChangingMimeType {
+#if defined(__IPHONE_14_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_14_0
+  // TODO(crbug.com/1114202): The XCUIElement queries in this test are broken on
+  // Xcode 12 beta 4 when running on the iOS 12 simulator.  Disable until Xcode
+  // is fixed.
+  if (@available(iOS 13, *)) {
+  } else {
+    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS12.");
+  }
+#endif
+
   [ChromeEarlGrey loadURL:self.testServer->GetURL("/")];
   [ChromeEarlGrey waitForWebStateContainingText:"Changing Mime Type"];
   [ChromeEarlGrey tapWebStateElementWithID:@"changing-mime-type"];
