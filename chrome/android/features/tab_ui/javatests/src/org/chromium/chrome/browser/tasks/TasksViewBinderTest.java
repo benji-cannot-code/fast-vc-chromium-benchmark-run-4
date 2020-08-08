5173460c9fa73d73676cb12de000baf6f47e4ef6
Carlos Knippschild <carlosk@chromium.org>
2020-08-08 00:27:45 +0000

    Add canonical check for reporting implicit user actions
    Reporting implicit user actions will be enabled in either of these
    cases:
    * When Feed V1 is enabled AND ReportFeedUserActions is enabled.
    * When Feed V2 is enabled.
    
    Bug: 1044139
    Change-Id: I0dfa1d8d1f05a82f2308b7a84e5f58919e81b432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339590
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796184}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/TasksViewBinderTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/TasksViewBinderTest.java
@@ -56,7 +56,7 @@ import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 import org.chromium.ui.test.util.DummyUiActivityTestCase;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -75,8 +75,10 @@ public class TasksViewBinderTest extends DummyUiActivityTestCase {
         super.setUpTest();
         MockitoAnnotations.initMocks(this);
 
-        Map<String, Boolean> testFeatures =
-                Collections.singletonMap(ChromeFeatureList.REPORT_FEED_USER_ACTIONS, true);
+        Map<String, Boolean> testFeatures = new HashMap<>();
+        testFeatures.put(ChromeFeatureList.INTEREST_FEED_V2, false);
+        testFeatures.put(ChromeFeatureList.INTEREST_FEED_CONTENT_SUGGESTIONS, true);
+        testFeatures.put(ChromeFeatureList.REPORT_FEED_USER_ACTIONS, false);
         ChromeFeatureList.setTestFeatures(testFeatures);
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
