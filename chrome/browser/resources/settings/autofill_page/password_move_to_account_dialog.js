09f69092d8a092bb4cd5e312d5ceafa1a91984c1
Victor Hugo Vianna Silva <victorvianna@google.com>
2020-08-10 08:54:41 +0000

    [b4p/settings] Add avatars and site favicons to moving dialog
    Implementation: https://screenshot.googleplex.com/4Q2LOWL28un
    Mock: https://screenshot.googleplex.com/iNV6JAtE9t9
    
    The CL also makes a change to <site-favicon> to allow embedding it as a
    round icon.
    
    Bug: 1102294
    Change-Id: I70b702a1404b7c20bd0df65f7b0db06d3c999d13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287297
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#796317}

--- chrome/browser/resources/settings/autofill_page/password_move_to_account_dialog.js
+++ chrome/browser/resources/settings/autofill_page/password_move_to_account_dialog.js
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import 'chrome://resources/cr_elements/shared_style_css.m.js';
+import './avatar_icon.js';
+import '../site_favicon.js';
 
 import {assert} from 'chrome://resources/js/assert.m.js';
 import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
