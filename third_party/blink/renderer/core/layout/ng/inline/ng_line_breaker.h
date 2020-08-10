2d03d55b265ac7ff8d28fbe42cbe073036233722
Koji Ishii <kojii@chromium.org>
2020-08-10 18:18:35 +0000

    Fix DCHECK failure in NGLineBreaker::RewindOverflow
    |NGLineBreaker::RewindOverflow| assumes that |Rewind| rewinds
    the current |item_index_| to the specified value. However, at
    some point |Rewind| was changed to not to rewind positioned
    floats.
    
    The DCHECK was to ensure the assumption. This sometimes hits
    when the line width saturates |LayoutUnit|, and it was
    considered not too harmful. But crbug.com/1114324 suggested
    that it may create unexpected internal states which can lead
    to crashes.
    
    This patch removes the assumption from |RewindOverflow|.
    
    Bug: 1114324, 1020101, 1029599
    Change-Id: I221e87d4ed089ae97aa21a99c2be73dc555f6d31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2344431
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796465}

--- third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
@@ -158,6 +158,7 @@ class CORE_EXPORT NGLineBreaker {
                                        unsigned start,
                                        unsigned end);
 
+  void HandleTrailingSpacesIfNeeded(NGLineInfo*);
   void HandleTrailingSpaces(const NGInlineItem&, NGLineInfo*);
   void HandleTrailingSpaces(const NGInlineItem&,
                             const ShapeResult&,
