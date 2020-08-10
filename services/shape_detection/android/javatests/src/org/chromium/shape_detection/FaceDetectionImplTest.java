e6ec3d3f0b7b06e0ad56c83698fd79cd06bfadf2
Thiabaud Engelbrecht <thiabaud@google.com>
2020-08-10 20:58:19 +0000

    Test batching of shape_detection unit tests
    For reference, the UNIT_TESTS batch can be run locally using:
    out/.../bin/run_chrome_public_test_apk -A Batch=UnitTests
    
    Bug: 989569
    Change-Id: Icea283a01697c4fdade7039d8b4ef629af835655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2346984
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/master@{#796529}

--- services/shape_detection/android/javatests/src/org/chromium/shape_detection/FaceDetectionImplTest.java
+++ services/shape_detection/android/javatests/src/org/chromium/shape_detection/FaceDetectionImplTest.java
@@ -17,6 +17,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.shape_detection.mojom.FaceDetection;
 import org.chromium.shape_detection.mojom.FaceDetectionResult;
@@ -29,6 +30,7 @@ import java.util.concurrent.TimeUnit;
  * Test suite for FaceDetectionImpl.
  */
 @RunWith(BaseJUnit4ClassRunner.class)
+@Batch(Batch.UNIT_TESTS)
 public class FaceDetectionImplTest {
     private static final org.chromium.skia.mojom.Bitmap MONA_LISA_BITMAP =
             TestUtils.mojoBitmapFromFile("mona_lisa.jpg");
