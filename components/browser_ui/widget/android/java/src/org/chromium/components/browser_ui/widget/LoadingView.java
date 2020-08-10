69a073fce4a089d234901befd8f9f5a2ebb85a9d
Wenyu Fu <wenyufu@google.com>
2020-08-10 23:45:50 +0000

    Add observer for LoadingView
    Add observer that will be notified when hideLoadingUI is finished.
    Also add junit tests for the widget.
    
    Bug: 1108558
    Change-Id: I78e62d10309fd4819bdf5a04cf9fda4de610d7d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2345091
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796621}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/LoadingView.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/LoadingView.java
@@ -13,7 +13,12 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.chromium.ui.interpolators.BakedBezierInterpolator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A {@link ProgressBar} that understands the hiding/showing policy defined in Material Design.
@@ -22,7 +27,21 @@ public class LoadingView extends ProgressBar {
     private static final int LOADING_ANIMATION_DELAY_MS = 500;
     private static final int MINIMUM_ANIMATION_SHOW_TIME_MS = 500;
 
+    /**
+     * A observer interface that will be notified when the progress bar is hidden.
+     */
+    public interface Observer {
+        /**
+         * Notify the listener a call to {@link #hideLoadingUI()} is complete and loading view is
+         * GONE.
+         */
+        void onHideLoadingUIComplete();
+    }
+
     private long mStartTime = -1;
+    private boolean mDisableAnimationForTest;
+
+    private final List<Observer> mListeners = new ArrayList<>();
 
     private final Runnable mDelayedShow = new Runnable() {
         @Override
@@ -46,13 +65,18 @@ public class LoadingView extends ProgressBar {
     private final Runnable mDelayedHide = new Runnable() {
         @Override
         public void run() {
+            if (mDisableAnimationForTest) {
+                onHideLoadingFinished();
+                return;
+            }
+
             animate()
                     .alpha(0.0f)
                     .setInterpolator(BakedBezierInterpolator.TRANSFORM_CURVE)
                     .setListener(new AnimatorListenerAdapter() {
                         @Override
                         public void onAnimationEnd(Animator animation) {
-                            setVisibility(GONE);
+                            onHideLoadingFinished();
                         }
                     });
         }
@@ -98,6 +122,8 @@ public class LoadingView extends ProgressBar {
                     Math.max(0,
                             mStartTime + MINIMUM_ANIMATION_SHOW_TIME_MS
                                     - SystemClock.elapsedRealtime()));
+        } else {
+            onHideLoadingFinished();
         }
     }
 
@@ -107,5 +133,34 @@ public class LoadingView extends ProgressBar {
     public void destroy() {
         removeCallbacks(mDelayedShow);
         removeCallbacks(mDelayedHide);
+        mListeners.clear();
+    }
+
+    /**
+     * Add the listener that will be notified when the spinner is completely hidden with {@link
+     * #hideLoadingUI()}.
+     * @param listener {@link Observer} that will be notified when the spinner is
+     *         completely hidden with {@link #hideLoadingUI()}.
+     */
+    public void addObserver(Observer listener) {
+        mListeners.add(listener);
+    }
+
+    private void onHideLoadingFinished() {
+        setVisibility(GONE);
+        for (Observer observer : mListeners) {
+            observer.onHideLoadingUIComplete();
+        }
+    }
+
+    /**
+     * Set disable the fading animation during {@link #hideLoadingUI()}.
+     * This function is added as a work around for disable animation during unit tests.
+     * @param disableAnimation Whether the fading animation should be disabled during {@link
+     *         #hideLoadingUI()}.
+     */
+    @VisibleForTesting
+    public void setDisableAnimationForTest(boolean disableAnimation) {
+        mDisableAnimationForTest = disableAnimation;
     }
 }
