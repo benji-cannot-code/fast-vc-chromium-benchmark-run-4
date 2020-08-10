31aab6f5f315e5c06a21d7c091507bcfce9fd8a3
Alex Ilin <alexilin@chromium.org>
2020-08-10 12:56:36 +0000

    [signin] Remove the kSigninReauthPrompt feature flag
    The flag is no longer needed since the Gaia endpoint has been rolled out
    to prod. The only consumer of Reauth feature is backed by its own
    feature flag.
    
    Bug: 1045515
    Change-Id: Ia12aa2abfc064a431c1a24ab85de231c07e8ebfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339530
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796358}

--- chrome/browser/signin/signin_features.cc
+++ chrome/browser/signin/signin_features.cc
@@ -9,6 +9,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const base::Feature kDiceWebSigninInterceptionFeature{
     "DiceWebSigninInterception", base::FEATURE_DISABLED_BY_DEFAULT};
 #endif  // ENABLE_DICE_SUPPORT
-
-const base::Feature kSigninReauthPrompt = {"SigninReauthPrompt",
-                                           base::FEATURE_DISABLED_BY_DEFAULT};
