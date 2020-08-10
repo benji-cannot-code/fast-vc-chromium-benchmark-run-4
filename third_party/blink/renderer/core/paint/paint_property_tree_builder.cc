5cc1239e2e36449dead904251230a6642db9ab86
Andrew Comminos <acomminos@fb.com>
2020-08-10 23:16:02 +0000

    Move frame element IDs from cc::Layers into the transform tree
    Instead of storing a frame element ID on each cc::Layer, store it on the
    transform tree instead. This makes impl-side attribution possible with
    CompositeAfterPaint.
    
    Bug: 910421
    Change-Id: Ica1e68155ffe84d1bb3b747afd644e052286e6ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343508
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Andrew Comminos <acomminos@fb.com>
    Cr-Commit-Position: refs/heads/master@{#796609}

--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -544,6 +544,9 @@ void FragmentPaintPropertyTreeBuilder::UpdatePaintOffsetTranslation(
         full_context_.direct_compositing_reasons &
         CompositingReason::kDirectReasonsForPaintOffsetTranslationProperty;
     state.rendering_context_id = context_.current.rendering_context_id;
+    state.frame_element_id = CompositorElementIdFromUniqueObjectId(
+        DOMNodeIds::IdForNode(&object_.GetDocument()),
+        CompositorElementIdNamespace::kDOMNodeId);
     OnUpdate(properties_->UpdatePaintOffsetTranslation(
         *context_.current.transform, std::move(state)));
     context_.current.transform = properties_->PaintOffsetTranslation();
@@ -575,6 +578,9 @@ void FragmentPaintPropertyTreeBuilder::UpdateStickyTranslation() {
       state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
           box_model.UniqueId(),
           CompositorElementIdNamespace::kStickyTranslation);
+      state.frame_element_id = CompositorElementIdFromUniqueObjectId(
+          DOMNodeIds::IdForNode(&object_.GetDocument()),
+          CompositorElementIdNamespace::kDOMNodeId);
 
       auto* layer = box_model.Layer();
       const auto* scroller_properties = layer->AncestorOverflowLayer()
@@ -718,6 +724,9 @@ void FragmentPaintPropertyTreeBuilder::UpdateTransformForSVGChild(
             context_.current.should_flatten_inherited_transform;
         state.compositor_element_id = GetCompositorElementId(
             CompositorElementIdNamespace::kPrimaryTransform);
+        state.frame_element_id = CompositorElementIdFromUniqueObjectId(
+            DOMNodeIds::IdForNode(&object_.GetDocument()),
+            CompositorElementIdNamespace::kDOMNodeId);
       }
 
       TransformPaintPropertyNode::AnimationState animation_state;
@@ -889,6 +898,9 @@ void FragmentPaintPropertyTreeBuilder::UpdateTransform() {
               : TransformPaintPropertyNode::BackfaceVisibility::kVisible;
       state.compositor_element_id = GetCompositorElementId(
           CompositorElementIdNamespace::kPrimaryTransform);
+      state.frame_element_id = CompositorElementIdFromUniqueObjectId(
+          DOMNodeIds::IdForNode(&object_.GetDocument()),
+          CompositorElementIdNamespace::kDOMNodeId);
 
       TransformPaintPropertyNode::AnimationState animation_state;
       animation_state.is_running_animation_on_compositor =
@@ -1781,6 +1793,9 @@ void FragmentPaintPropertyTreeBuilder::UpdatePerspective() {
       state.flags.flattens_inherited_transform =
           context_.current.should_flatten_inherited_transform;
       state.rendering_context_id = context_.current.rendering_context_id;
+      state.frame_element_id = CompositorElementIdFromUniqueObjectId(
+          DOMNodeIds::IdForNode(&object_.GetDocument()),
+          CompositorElementIdNamespace::kDOMNodeId);
       OnUpdate(properties_->UpdatePerspective(*context_.current.transform,
                                               std::move(state)));
     } else {
@@ -1837,6 +1852,9 @@ void FragmentPaintPropertyTreeBuilder::UpdateReplacedContentTransform() {
                                                false);
       state.flags.flattens_inherited_transform =
           context_.current.should_flatten_inherited_transform;
+      state.frame_element_id = CompositorElementIdFromUniqueObjectId(
+          DOMNodeIds::IdForNode(&object_.GetDocument()),
+          CompositorElementIdNamespace::kDOMNodeId);
       OnUpdate(properties_->UpdateReplacedContentTransform(
           *context_.current.transform, std::move(state)));
     } else {
@@ -2044,6 +2062,9 @@ void FragmentPaintPropertyTreeBuilder::UpdateScrollAndScrollTranslation() {
           object_.StyleRef().BackfaceVisibility() ==
               EBackfaceVisibility::kHidden)
         state.flags.delegates_to_parent_for_backface = true;
+      state.frame_element_id = CompositorElementIdFromUniqueObjectId(
+          DOMNodeIds::IdForNode(&object_.GetDocument()),
+          CompositorElementIdNamespace::kDOMNodeId);
       auto effective_change_type = properties_->UpdateScrollTranslation(
           *context_.current.transform, std::move(state));
       if (effective_change_type ==
