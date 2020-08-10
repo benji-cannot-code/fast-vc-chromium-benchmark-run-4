28e191ca99050f912ba84efad3d06d7fd7565da0
Eric Seckler <eseckler@chromium.org>
2020-08-10 11:55:41 +0000

    chrome/android: Add experiment to run all execution on little cores
    Adds a feature-list-controlled experiment to restrict execution of all
    Chrome processes/threads to LITTLE-cores only (on big.LITTLE-like
    architectures).
    
    We intend to run a limited experiment on Canary/Dev to estimate the
    potential power savings of running on the more efficient little cores,
    as well as the impact of this on system health metrics.
    
    We also plan to add a similar experiment for WebView. However, since
    WebView shares a process with the hosting app, we may need to be more
    careful about which threads we opt into little-core-only execution.
    
    TBR: dcheng@chromium.org
    Bug: 1111789
    Change-Id: If2bc8c811692f7e716428398db3e5d0649da42d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332715
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796342}

--- base/cpu_affinity_posix.h
+++ base/cpu_affinity_posix.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-enum class BASE_EXPORT CpuAffinityMode {
+enum class CpuAffinityMode {
   // No restrictions on affinity.
   kDefault,
   // Restrict execution to LITTLE cores only. Only has an effect on platforms
---
+++ chrome/common/android/cpu_affinity_experiments.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_COMMON_ANDROID_CPU_AFFINITY_EXPERIMENTS_H_
+#define CHROME_COMMON_ANDROID_CPU_AFFINITY_EXPERIMENTS_H_
+
+namespace chrome {
+
+// Setup CPU-affinity restriction experiments (e.g. to restrict execution to
+// little cores only) for the current process, based on the feature list. Should
+// be called during process startup after feature list initialization.
+void InitializeCpuAffinityExperiments();
+
+}  // namespace chrome
+
+#endif  // CHROME_COMMON_ANDROID_CPU_AFFINITY_EXPERIMENTS_H_
