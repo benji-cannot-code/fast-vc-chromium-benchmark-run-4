e2640ec9c2cf1f3edff9caefd1a168ef816cd641
Adam Ettenberger <Adam.Ettenberger@microsoft.com>
2020-08-10 18:17:44 +0000

    Fix Role::kButton name invalidation when descendant text is modified
    This CL addresses a bug where Role::kButton, and possibly other roles
    that derive their name from descendant contents, were not firing
    Text Changed when a descendant Node.TEXT_NODE modifies its text |data|.
    
    ax_object_cache_impl.cc :
    - When firing a text changed event, also fire a text changed event for
      the first ancestor that derives its name from descendant contents.
      This ensures the nodes are invalidated, send appropriate text changed
      events, and the changes are serialized to the browser process AXTree.
    
    browser_accessibility_manager_win.cc :
    - FireUiaTextContainerEvent made it possible to fire text events more
      than once for a given text provider so it has been removed.
    - Instead, populate a set |text_changed_events_| which contains the
      text providers via |GetUiaTextPatternProvider| and fire the UIA text
      events in BrowserAccessibilityManagerWin::FinalizeAccessibilityEvents
    - |text_selection_changed_events_| could also potentially end up with
      the same double-event problem, so it uses GetUiaTextPatternProvider
      now as well.
    - Removing usage of std::unordered_set based on the guidance in
      base/containers/README.md. In general these sets shouldn't grow large
      enough to benefit from being unordered.
    
    AX-Relnotes: Fix bug where after updating the content of complex button might not notify ATs, causing the wrong accessible name to be announced.
    Bug: 1105262
    Change-Id: I02dc7798686b09f780c90af52aeb2e36fe8fe048
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2296564
    Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796464}

--- content/browser/accessibility/browser_accessibility_manager_win.h
+++ content/browser/accessibility/browser_accessibility_manager_win.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
-#include <unordered_set>
+#include <set>
 #include <vector>
 
 #include "base/macros.h"
@@ -52,7 +52,6 @@ class CONTENT_EXPORT BrowserAccessibilityManagerWin
                                    BrowserAccessibility* node);
   void FireUiaStructureChangedEvent(StructureChangeType change_type,
                                     BrowserAccessibility* node);
-  void FireUiaTextContainerEvent(LONG uia_event, BrowserAccessibility* node);
 
   // Do event pre-processing
   void BeforeAccessibilityEvents() override;
@@ -113,6 +112,10 @@ class CONTENT_EXPORT BrowserAccessibilityManagerWin
       IsSelectedPredicate is_selected_predicate,
       FirePlatformSelectionEventsCallback fire_platform_events_callback);
 
+  void HandleAriaPropertiesChangedEvent(BrowserAccessibility* node);
+  void HandleTextChangedEvent(BrowserAccessibility* node);
+  void HandleTextSelectionChangedEvent(BrowserAccessibility* node);
+
   // Give BrowserAccessibilityManager::Create access to our constructor.
   friend class BrowserAccessibilityManager;
 
@@ -124,16 +127,22 @@ class CONTENT_EXPORT BrowserAccessibilityManagerWin
 
   // Since there could be multiple aria property changes on a node and we only
   // want to fire UIA_AriaPropertiesPropertyId once for that node, we use the
-  // unordered set here to keep track of the unique nodes that had aria property
-  // changes, so we only fire the event once for every node.
-  std::unordered_set<BrowserAccessibility*> aria_properties_events_;
+  // set here to keep track of the unique nodes that had aria property changes,
+  // so we only fire the event once for every node.
+  std::set<BrowserAccessibility*> aria_properties_events_;
 
   // Since there could be duplicate text selection changed events on a node
-  // raised from both FireBlinkEvent and FireGeneratedEvent, we use an unordered
-  // set here to keep track of the unique nodes that had
+  // raised from both FireBlinkEvent and FireGeneratedEvent, we use the set here
+  // to keep track of the unique nodes that had
   // UIA_Text_TextSelectionChangedEventId, so we only fire the event once for
   // every node.
-  std::unordered_set<BrowserAccessibility*> text_selection_changed_events_;
+  std::set<BrowserAccessibility*> text_selection_changed_events_;
+
+  // Since there could be duplicate text changed events on a node raised from
+  // both FireBlinkEvent and FireGeneratedEvent, we use the set here to keep
+  // track of the unique nodes that had UIA_Text_TextChangedEventId, so we only
+  // fire the event once for every node.
+  std::set<BrowserAccessibility*> text_changed_events_;
 
   // When the ignored state changes for a node, we only want to fire the
   // events relevant to the ignored state change (e.g. show / hide).
