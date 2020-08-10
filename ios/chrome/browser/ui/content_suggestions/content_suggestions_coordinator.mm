5be82e25b178f2ab107dd16c8533069446b3e353
adamta <adamta@google.com>
2020-08-10 19:00:42 +0000

    [iOS] Trigger infinite feed after feed reload
    When feed reloads and goes back to being one page (10 articles long),
    infinite feed would stop working because saved height was higher. We
    should only check if the saved height is different instead.
    
    Bug: 1085419
    Change-Id: I89fe5a5c854bdf1b3b46c0b78aa012088bd0011f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2342582
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Adam Trudeau-Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/master@{#796485}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -468,7 +468,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
   // TODO(crbug.com/1085419): Track number of cards from protocol instead of
   // height to determine whether or not we should fetch more cards.
-  if (currentHeight > self.discoverFeedHeight) {
+  if (currentHeight != self.discoverFeedHeight) {
     ios::GetChromeBrowserProvider()
         ->GetDiscoverFeedProvider()
         ->LoadMoreFeedArticles();
