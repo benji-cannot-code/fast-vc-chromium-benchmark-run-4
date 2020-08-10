5651f52fb0049fef2326ba2b6bb2856726ca175c
Richard Stotz <rstz@chromium.org>
2020-08-10 11:59:31 +0000

    [nativeio] Add Flush()
    This CL implements a flush function that attempts to flush any write
    buffers, including device buffers, by calling base::File::Flush(). This
    comes with the intent to evaluate developer feedback. Note that a future
    version of the API might include a faster, less reliable variant of
    flush.
    
    Bug: 914488
    Change-Id: I556cea0f4872e5536e413b91df575c4f6a00a55e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2257934
    Commit-Queue: Richard Stotz <rstz@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796343}

---
+++ third_party/blink/web_tests/external/wpt/native-io/concurrent_io/concurrent_io_flush_async.tentative.https.any.js
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=NativeIO API: Concurrent io while flush is resolving.
+// META: global=window,worker
+// META: script=operation_helpers.js
+// META: script=../resources/support.js
+
+
+'use strict';
+
+// See documentation in operation_helpers.js
+
+for (let op of kOperations) {
+  promise_test(async testCase => {
+    const file = await createFile(testCase, 'flush_file');
+
+    const res = op.prepare();
+
+    const flushPromise = file.flush();
+    op.assertRejection(testCase, file, res);
+
+    await flushPromise;
+
+    const readSharedArrayBuffer = new SharedArrayBuffer(4);
+    const readBytes = new Uint8Array(readSharedArrayBuffer);
+    assert_equals(await file.read(readBytes, 0), 4,
+                  `NativeIOFile.read() should not fail after a rejected ` +
+                    `${op.name}() during flush()`);
+    assert_array_equals(readBytes, [64, 65, 66, 67],
+                        `Rejecting ${op.name}() during flush() should not ` +
+                          `change the file.`);
+    op.assertUnchanged(res);
+  }, `${op.name}() rejects while flush() is resolving.`);
+};
