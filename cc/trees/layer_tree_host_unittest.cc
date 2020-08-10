82d6b81914d9dae0becd92dfdcf0ef25a6040e3e
Mario Bianucci <mabian@microsoft.com>
2020-08-10 20:55:06 +0000

    De-flake LayerTreeHostTestDelegatedInkMetadataOnAndOff.RunSingleThread*
    SetNeedsDisplay() was being called many times, when every other test
    limits it to a certain number of times. This test should only call it
    once, and doing so prevents it from running wildly and flaking on the
    single threaded versions.
    
    Bug: 1111302, 1112507
    Change-Id: I2038c89e73b7b92b03133a4cf8487a9a54467fb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343221
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Mario Bianucci <mabian@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#796523}

--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -8987,7 +8987,10 @@ class LayerTreeHostTestDelegatedInkMetadataOnAndOff
 
   void DidCommitAndDrawFrame() override {
     // Cause a redraw to occur.
-    layer_->SetNeedsDisplay();
+    if (set_needs_display_) {
+      layer_->SetNeedsDisplay();
+      set_needs_display_ = false;
+    }
   }
 
   void DrawLayersOnThread(LayerTreeHostImpl* impl) override {
@@ -9015,8 +9018,6 @@ class LayerTreeHostTestDelegatedInkMetadataOnAndOff
     }
   }
 
-  void AfterTest() override {}
-
   // RenderFrameMetadataObserver implementation.
   void BindToCurrentThread() override {}
   void OnRenderFrameSubmission(
@@ -9031,6 +9032,7 @@ class LayerTreeHostTestDelegatedInkMetadataOnAndOff
   base::Optional<viz::DelegatedInkMetadata> expected_metadata_;
   FakeContentLayerClient client_;
   scoped_refptr<Layer> layer_;
+  bool set_needs_display_ = true;
 };
 
 SINGLE_AND_MULTI_THREAD_TEST_F(LayerTreeHostTestDelegatedInkMetadataOnAndOff);
