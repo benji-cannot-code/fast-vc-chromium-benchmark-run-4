73a2fa831bd1893aac0eb07d07ab506438eae935
Jesse McKenna <jessemckenna@google.com>
2020-08-10 23:54:29 +0000

    Stop browser_tests leaving an open Explorer window
    This change stops browser_tests from leaving a file explorer window
    open after it completes.
    
    Currently, browser test `DownloadExtensionTest.DownloadsShowFunction`
    tests the "Show in Folder" functionality by opening a file explorer
    window, but has no way to close this window after it completes. This
    results in a stray file-explorer window. At times, the stray window
    may also come with an error popup if the temp directory it points to
    has already been deleted.
    
    With this change, the call to open the file explorer is skipped if
    shell operations have been disabled for testing. The call is skipped
    as late as possible, in order to exercise as much of the code as
    possible.
    
    Change-Id: If015143d98a3e7e5d4a6bce958780fce6fa63f32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339599
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/master@{#796626}

--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -1201,6 +1201,7 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest, FileExistenceCheckAfterSearch) {
 #if !defined(OS_CHROMEOS)
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
                        DownloadsShowFunction) {
+  platform_util::internal::DisableShellOperationsForTesting();
   ScopedCancellingItem item(CreateFirstSlowTestDownload());
   ASSERT_TRUE(item.get());
 
--- chrome/browser/platform_util.cc
+++ chrome/browser/platform_util.cc
@@ -56,6 +56,10 @@ void DisableShellOperationsForTesting() {
   shell_operations_allowed = false;
 }
 
+bool AreShellOperationsAllowed() {
+  return shell_operations_allowed;
+}
+
 }  // namespace internal
 
 void OpenItem(Profile* profile,
