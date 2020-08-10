1734543153ded0797027c8ae226ce4ade954e984
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-08-10 22:42:06 +0000

    [Extensions] Rename match_data_urls to match_origin_as_fallback
    In content scripts, rename match_data_urls to match_origin_as_fallback.
    The associated bug has the full discussion, but this field better suits
    matching multiple schemes, and will apply to any frame where we need to
    compare the initiator/precursor origin.
    
    Note: this is a pure rename CL. It does *not* adjust any of the
    functionality.
    
    Bug: 55084
    Change-Id: Iec4ba3e8f0bb0559eb800cc3e0d453c7b353598e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340494
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796587}

--- extensions/common/user_script.cc
+++ extensions/common/user_script.cc
@@ -99,7 +99,7 @@ UserScript::UserScript()
       emulate_greasemonkey_(false),
       match_all_frames_(false),
       match_about_blank_(false),
-      match_data_urls_(false),
+      match_origin_as_fallback_(false),
       incognito_enabled_(false) {}
 
 UserScript::~UserScript() {
@@ -134,7 +134,7 @@ std::unique_ptr<UserScript> UserScript::CopyMetadataFrom(
   script->emulate_greasemonkey_ = other.emulate_greasemonkey_;
   script->match_all_frames_ = other.match_all_frames_;
   script->match_about_blank_ = other.match_about_blank_;
-  script->match_data_urls_ = other.match_data_urls_;
+  script->match_origin_as_fallback_ = other.match_origin_as_fallback_;
   script->incognito_enabled_ = other.incognito_enabled_;
 
   return script;
@@ -201,7 +201,7 @@ void UserScript::Pickle(base::Pickle* pickle) const {
   pickle->WriteBool(emulate_greasemonkey());
   pickle->WriteBool(match_all_frames());
   pickle->WriteBool(match_about_blank());
-  pickle->WriteBool(match_data_urls());
+  pickle->WriteBool(match_origin_as_fallback());
   pickle->WriteBool(is_incognito_enabled());
 
   PickleHostID(pickle, host_id_);
@@ -257,7 +257,7 @@ void UserScript::Unpickle(const base::Pickle& pickle,
   CHECK(iter->ReadBool(&emulate_greasemonkey_));
   CHECK(iter->ReadBool(&match_all_frames_));
   CHECK(iter->ReadBool(&match_about_blank_));
-  CHECK(iter->ReadBool(&match_data_urls_));
+  CHECK(iter->ReadBool(&match_origin_as_fallback_));
   CHECK(iter->ReadBool(&incognito_enabled_));
 
   UnpickleHostID(pickle, iter, &host_id_);
