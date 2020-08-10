bd137b5bd62202915620033d88750412fdbaa9bc
Evan Stade <estade@chromium.org>
2020-08-10 19:33:36 +0000

    WebLayer: Add sanity test for Page Info UI.
    Bug: none
    Change-Id: I3a6df417b0bdbb105e0f22506ca764a2d328c818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2347048
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796504}

---
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/PageInfoTest.java
@@ -0,0 +1,60 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer.test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.content.Context;
+
+import androidx.test.filters.SmallTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.StrictModeContext;
+import org.chromium.content_public.browser.test.util.CriteriaHelper;
+import org.chromium.content_public.browser.test.util.CriteriaNotSatisfiedException;
+import org.chromium.content_public.browser.test.util.TestThreadUtils;
+import org.chromium.weblayer.TestWebLayer;
+import org.chromium.weblayer.shell.InstrumentationActivity;
+
+/**
+ * Tests the behavior of the Page Info UI.
+ */
+@RunWith(WebLayerJUnit4ClassRunner.class)
+public class PageInfoTest {
+    @Rule
+    public InstrumentationActivityTestRule mActivityTestRule =
+            new InstrumentationActivityTestRule();
+
+    @Test
+    @SmallTest
+    @MinWebLayerVersion(84)
+    public void testPageInfoLaunches() {
+        InstrumentationActivity activity = mActivityTestRule.launchShellWithUrl(
+                mActivityTestRule.getTestDataURL("simple_page.html"));
+
+        Context remoteContext = TestWebLayer.getRemoteContext(activity.getApplicationContext());
+        int buttonId = ResourceUtil.getIdentifier(remoteContext, "id/security_button");
+
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            StrictModeContext ignored = StrictModeContext.allowDiskReads();
+            EventUtils.simulateTouchCenterOfView(activity.findViewById(buttonId));
+        });
+        CriteriaHelper.pollInstrumentationThread(() -> {
+            try {
+                onView(withText("Your connection to this site is not secure"))
+                        .check(matches(isDisplayed()));
+            } catch (Error e) {
+                throw new CriteriaNotSatisfiedException(e.toString());
+            }
+        });
+    }
+}
