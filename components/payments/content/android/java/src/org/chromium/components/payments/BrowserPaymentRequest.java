9cd80d3a36e166c40d56f11f918bbc5afb6bfed1
Liquan (Max) Gu <maxlg@chromium.org>
2020-08-10 21:00:21 +0000

    [PRImpl] Encapsulate PRClient in CPRImpl
    Change:
    * Encapsulate PRClient in CPRImpl. Before the change, PRImpl had to
    manage the lifespan of PRClient and CPRImpl. After the change, PRImpl
    only need to manage the lifespan of CPRImpl, as PRClient is no longer
    visible to PRImpl.
    * Clarify that PR#close can only be called by the renderer
    * Create CPRImpl#teardown to play the role of closing (should have
    called #close if not for the collision with the PR#close interface).
    * Clarify the usage of CPRImpl#teardown, BPR#close, PRImpl#close. Add
    mHasClosed/mHasTorndown for CPRImpl#teardown and PRImpl#close to make
    sure these methods can be called within themself without worrying
    deadloop. For example, while PRImpl#close calls CPRImpl#teardown,
    CPRImpl#teardown can call PRImpl#close back without causing deadloop.
    
    Acronym:
    PRClient = PaymentRequestClient
    CPRImpl = ComponentPaymentRequestImpl
    BPR = BrowserPaymentRequest
    PR = PaymentRequest
    
    Bug: 1102522
    
    Change-Id: Ic6495f02593f9a1cfad9747e52e9dcf3a05b1018
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343631
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796532}

--- components/payments/content/android/java/src/org/chromium/components/payments/BrowserPaymentRequest.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/BrowserPaymentRequest.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.payments;
 
-import org.chromium.mojo.system.MojoException;
 import org.chromium.payments.mojom.PaymentDetails;
 import org.chromium.payments.mojom.PaymentMethodData;
 import org.chromium.payments.mojom.PaymentOptions;
@@ -69,18 +68,15 @@ public interface BrowserPaymentRequest {
     /** The browser part of the {@link PaymentRequest#canMakePayment} implementation. */
     void canMakePayment();
 
-    /** The browser part of the {@link PaymentRequest#close} implementation. */
-    void close();
-
-    /**
-     * The browser part of the {@link PaymentRequest#onConnectionError} implementation.
-     * @param e The detail of the error.
-     */
-    void onConnectionError(MojoException e);
-
     /** @return The JourneyLogger of PaymentRequestImpl. */
     JourneyLogger getJourneyLogger();
 
     /** Delegate to the same method of PaymentRequestImpl. */
     void disconnectFromClientWithDebugMessage(String debugMessage);
+
+    /**
+     * Close this instance. The callers of this method should stop referencing this instance upon
+     * calling it. This method can be called within itself without causing infinite loop.
+     */
+    void close();
 }
