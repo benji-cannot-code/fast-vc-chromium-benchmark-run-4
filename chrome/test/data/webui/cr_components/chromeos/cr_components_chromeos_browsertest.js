df3eb8e57285cff20673cf6f287e80c94ce9fcbd
Andy Paicu <andypaicu@chromium.org>
2020-08-10 15:15:10 +0000

    Disabled flaky ChromeOS cr components tests
    Bug: 1114675
    Change-Id: I10ff8b750702a7644e13c450f493106093ff993f
    TBR: jhawkins@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2346346
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796392}

--- chrome/test/data/webui/cr_components/chromeos/cr_components_chromeos_browsertest.js
+++ chrome/test/data/webui/cr_components/chromeos/cr_components_chromeos_browsertest.js
@@ -10,6 +10,13 @@ GEN_INCLUDE(['//chrome/test/data/webui/polymer_browser_test_base.js']);
 
 GEN('#include "content/public/test/browser_test.h"');
 
+// Tests are flaky on ChromeOS, debug (crbug.com/1114675).
+GEN('#if defined(OS_CHROMEOS) && !defined(NDEBUG)');
+GEN('#define MAYBE_All DISABLED_All');
+GEN('#else');
+GEN('#define MAYBE_All All');
+GEN('#endif');
+
 // Polymer 2 test list format:
 //
 // ['ModuleNameTest', 'module.js',
@@ -49,5 +56,5 @@ function registerTest(testName, module, deps) {
     }
   };
 
-  TEST_F(className, 'All', () => mocha.run());
+  TEST_F(className, 'MAYBE_All', () => mocha.run());
 }
