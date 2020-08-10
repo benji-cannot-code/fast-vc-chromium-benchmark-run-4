d23f928425e587c29b4e9faed4d9c332bf14755a
Kramer Ge <fangzhoug@chromium.org>
2020-08-10 21:05:53 +0000

    [ozone/wayland] Use CommitOverlays to send a frame from gpu to browser.
    CommitOverlays is a more generic IPC that packages multiple overlay
    configurations for a frame before sending it to browser process. This
    replaces the CommitBuffer IPC.
    
    This CL merely adds CommitOverlays. It does not send multiple buffers
    using it.
    
    This is 3/? CL for overlay forwarding using wl_subsurface.
    
    Bug: 1063865
    Change-Id: Id71d055bdb14b3d2e97cb2eef8811dca56323899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2314665
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Maksim Sisov (GMT+3) <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796542}

--- ui/ozone/platform/wayland/gpu/wayland_buffer_manager_gpu.h
+++ ui/ozone/platform/wayland/gpu/wayland_buffer_manager_gpu.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/bindings/remote.h"
+#include "mojo/public/cpp/bindings/type_converter.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/ozone/platform/wayland/common/wayland_util.h"
 #include "ui/ozone/public/mojom/wayland/wayland_buffer_manager.mojom.h"
@@ -34,6 +35,7 @@ namespace ui {
 class WaylandConnection;
 class WaylandSurfaceGpu;
 class WaylandWindow;
+struct OverlayPlane;
 
 // Forwards calls through an associated mojo connection to WaylandBufferManager
 // on the browser process side.
@@ -106,6 +108,12 @@ class WaylandBufferManagerGpu : public ozone::mojom::WaylandBufferManagerGpu {
                     uint32_t buffer_id,
                     const gfx::Rect& damage_region);
 
+  // Send overlay configurations for a frame to a WaylandWindow identified by
+  // |widget|.
+  void CommitOverlays(
+      gfx::AcceleratedWidget widget,
+      std::vector<ozone::mojom::WaylandOverlayConfigPtr> overlays);
+
   // Asks Wayland to destroy a wl_buffer.
   void DestroyBuffer(gfx::AcceleratedWidget widget, uint32_t buffer_id);
 
@@ -144,6 +152,9 @@ class WaylandBufferManagerGpu : public ozone::mojom::WaylandBufferManagerGpu {
   void CommitBufferInternal(gfx::AcceleratedWidget widget,
                             uint32_t buffer_id,
                             const gfx::Rect& damage_region);
+  void CommitOverlaysInternal(
+      gfx::AcceleratedWidget widget,
+      std::vector<ozone::mojom::WaylandOverlayConfigPtr> overlays);
   void DestroyBufferInternal(gfx::AcceleratedWidget widget, uint32_t buffer_id);
 
   void BindHostInterface(
@@ -214,5 +225,16 @@ class WaylandBufferManagerGpu : public ozone::mojom::WaylandBufferManagerGpu {
 };
 
 }  // namespace ui
+
+// This is a specialization of mojo::TypeConverter and has to be in the mojo
+// namespace.
+namespace mojo {
+template <>
+struct TypeConverter<ui::ozone::mojom::WaylandOverlayConfigPtr,
+                     ui::OverlayPlane> {
+  static ui::ozone::mojom::WaylandOverlayConfigPtr Convert(
+      const ui::OverlayPlane& input);
+};
+}  // namespace mojo
 
 #endif  // UI_OZONE_PLATFORM_WAYLAND_GPU_WAYLAND_BUFFER_MANAGER_GPU_H_
