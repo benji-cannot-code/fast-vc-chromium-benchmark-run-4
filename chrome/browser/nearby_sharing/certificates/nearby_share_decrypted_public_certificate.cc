1dc4e77b7ebbd80949d714fa4edda20c9b4780ea
Josh Nohle <nohle@google.com>
2020-08-11 05:35:18 +0000

    [Nearby] Add copy constructor to NearbyShareDecryptedPublicCertificate
    Change-Id: I2e4fda0aae8c4f644c6e0ef79c410554611199cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2347976
    Auto-Submit: Josh Nohle <nohle@chromium.org>
    Commit-Queue: James Vecore <vecore@google.com>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#796713}

--- chrome/browser/nearby_sharing/certificates/nearby_share_decrypted_public_certificate.cc
+++ chrome/browser/nearby_sharing/certificates/nearby_share_decrypted_public_certificate.cc
@@ -182,6 +182,12 @@ NearbyShareDecryptedPublicCertificate::NearbyShareDecryptedPublicCertificate(
       unencrypted_metadata_(std::move(unencrypted_metadata)) {}
 
 NearbyShareDecryptedPublicCertificate::NearbyShareDecryptedPublicCertificate(
+    const NearbyShareDecryptedPublicCertificate&) = default;
+NearbyShareDecryptedPublicCertificate&
+NearbyShareDecryptedPublicCertificate::operator=(
+    const NearbyShareDecryptedPublicCertificate&) = default;
+
+NearbyShareDecryptedPublicCertificate::NearbyShareDecryptedPublicCertificate(
     NearbyShareDecryptedPublicCertificate&&) = default;
 NearbyShareDecryptedPublicCertificate&
 NearbyShareDecryptedPublicCertificate::operator=(
