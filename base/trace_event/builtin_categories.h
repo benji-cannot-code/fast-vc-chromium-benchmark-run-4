123ceec0c0a5681020405ca617b65fc5bf7fd4ea
Andreas Haas <ahaas@chromium.org>
2020-08-11 18:58:09 +0000

    [tracing] Add gpu.angle tracing category
    This CL adds the "gpu.angle" category to the list of categories in
    about:tracing. The "gpu.angle" category was enabled by default and could
    not be disabled before.
    
    R=eseckler@chromium.org
    
    Change-Id: I243ffcdf1b170371b312e6895b1544cf1e20e551
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2349348
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796928}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("fonts")                                                             \
   X("GAMEPAD")                                                           \
   X("gpu")                                                               \
+  X("gpu.angle")                                                         \
   X("gpu.capture")                                                       \
   X("gpu.memory")                                                        \
   X("headless")                                                          \
