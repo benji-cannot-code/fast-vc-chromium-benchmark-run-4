d734136b5030c73926ad2c3ed2d07f23225ccb70
Sammie Quon <sammiequon@chromium.org>
2020-08-11 20:45:11 +0000

    system: Fix DCHECK hit when adding new display with stylus tools.
    We try to update visibility in constructor which then checks for widget
    in GetEffectiveVisibility. The initialize local state code looks like it
    can be done after the shelf pod is added to a widget so move it there.
    
    Test: manual
    Change-Id: Ic7c2e7856ed76dce9008a6ad9f9c735ce1a05d81
    Fixed: 1115191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2349403
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796975}

--- ash/system/palette/palette_tray.cc
+++ ash/system/palette/palette_tray.cc
@@ -188,8 +188,6 @@ PaletteTray::PaletteTray(Shelf* shelf)
   tray_container()->AddChildView(icon_);
 
   Shell::Get()->AddShellObserver(this);
-
-  InitializeWithLocalState();
 }
 
 PaletteTray::~PaletteTray() {
@@ -432,6 +430,8 @@ void PaletteTray::AnchorUpdated() {
 void PaletteTray::Initialize() {
   TrayBackgroundView::Initialize();
   ui::DeviceDataManager::GetInstance()->AddObserver(this);
+
+  InitializeWithLocalState();
 }
 
 bool PaletteTray::PerformAction(const ui::Event& event) {
