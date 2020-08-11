453aced353ff4ace577b29e04b1e506a22585680
Andrey Zaytsev <andzaytsev@google.com>
2020-08-11 21:01:48 +0000

    Safety check for Android: handled initial signed out state in passwords
    Bug: 1070620
    Change-Id: Icf1f7733b29d90c056435b649284c35f5a6c25f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2348770
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796983}

--- chrome/browser/safety_check/android/safety_check_bridge.cc
+++ chrome/browser/safety_check/android/safety_check_bridge.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/safety_check/android/jni_headers/SafetyCheckBridge_jni.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
+#include "components/password_manager/core/browser/leak_detection/authenticated_leak_check.h"
 #include "components/safety_check/safety_check.h"
 
 static jlong JNI_SafetyCheckBridge_Init(
@@ -17,6 +19,14 @@ static jlong JNI_SafetyCheckBridge_Init(
     const base::android::JavaParamRef<jobject>& j_safety_check_observer) {
   return reinterpret_cast<intptr_t>(
       new SafetyCheckBridge(env, j_safety_check_observer));
+}
+
+static jboolean JNI_SafetyCheckBridge_UserSignedIn(JNIEnv* env) {
+  signin::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForProfile(
+          ProfileManager::GetLastUsedProfile());
+  return password_manager::AuthenticatedLeakCheck::HasAccountForRequest(
+      identity_manager);
 }
 
 SafetyCheckBridge::SafetyCheckBridge(
