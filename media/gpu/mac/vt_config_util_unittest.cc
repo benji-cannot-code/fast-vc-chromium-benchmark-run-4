5e2a273873f67ca2d40755c732ff0c9f9d04c640
Yuly Novikov <ynovikov@chromium.org>
2020-08-11 01:25:23 +0000

    Revert "Add support for creating VP9 format extensions for VTVDA."
    This reverts commit 320cb6fc9b329b83709d50285f4d699d22bc66fd.
    
    Reason for revert: breaks compile on mac-arm64-rel
    
    Original change's description:
    > Add support for creating VP9 format extensions for VTVDA.
    > 
    > Creates a vpcc box based on the configuration info.
    > 
    > R=​sandersd
    > 
    > Bug: 1103432
    > Test: Updated unittests.
    > Change-Id: I3d5152b59c6f6ad82da25e699eba2d8bf5ba44ae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2346607
    > Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Dan Sanders <sandersd@chromium.org>
    > Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Dan Sanders <sandersd@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#796591}
    
    TBR=dalecurtis@chromium.org,sandersd@chromium.org
    
    Change-Id: I00b88a95637db2999f190c5945c97743112344e9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1103432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2348148
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796652}

--- media/gpu/mac/vt_config_util_unittest.cc
+++ media/gpu/mac/vt_config_util_unittest.cc
@@ -14,28 +14,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-std::string GetStrValue(CFDictionaryRef dict, CFStringRef key) {
+std::string GetStrValue(CFMutableDictionaryRef dict, CFStringRef key) {
   return base::SysCFStringRefToUTF8(
       base::mac::CFCastStrict<CFStringRef>(CFDictionaryGetValue(dict, key)));
 }
 
-CFStringRef GetCFStrValue(CFDictionaryRef dict, CFStringRef key) {
+CFStringRef GetCFStrValue(CFMutableDictionaryRef dict, CFStringRef key) {
   return base::mac::CFCastStrict<CFStringRef>(CFDictionaryGetValue(dict, key));
 }
 
-int GetIntValue(CFDictionaryRef dict, CFStringRef key) {
+int GetIntValue(CFMutableDictionaryRef dict, CFStringRef key) {
   CFNumberRef value =
       base::mac::CFCastStrict<CFNumberRef>(CFDictionaryGetValue(dict, key));
   int result;
   return CFNumberGetValue(value, kCFNumberIntType, &result) ? result : -1;
 }
 
-bool GetBoolValue(CFDictionaryRef dict, CFStringRef key) {
+bool GetBoolValue(CFMutableDictionaryRef dict, CFStringRef key) {
   return CFBooleanGetValue(
       base::mac::CFCastStrict<CFBooleanRef>(CFDictionaryGetValue(dict, key)));
 }
 
-base::span<const uint8_t> GetDataValue(CFDictionaryRef dict, CFStringRef key) {
+base::span<const uint8_t> GetDataValue(CFMutableDictionaryRef dict,
+                                       CFStringRef key) {
   CFDataRef data =
       base::mac::CFCastStrict<CFDataRef>(CFDictionaryGetValue(dict, key));
   return data ? base::span<const uint8_t>(
@@ -44,17 +45,9 @@ base::span<const uint8_t> GetDataValue(CFDictionaryRef dict, CFStringRef key) {
               : base::span<const uint8_t>();
 }
 
-base::span<const uint8_t> GetNestedDataValue(CFDictionaryRef dict,
-                                             CFStringRef key1,
-                                             CFStringRef key2) {
-  CFDictionaryRef nested_dict = base::mac::CFCastStrict<CFDictionaryRef>(
-      CFDictionaryGetValue(dict, key1));
-  return GetDataValue(nested_dict, key2);
-}
-
 base::ScopedCFTypeRef<CVImageBufferRef> CreateCVImageBuffer(
     media::VideoColorSpace cs) {
-  base::ScopedCFTypeRef<CFDictionaryRef> fmt(
+  base::ScopedCFTypeRef<CFMutableDictionaryRef> fmt(
       CreateFormatExtensions(kCMVideoCodecType_H264, media::H264PROFILE_MAIN,
                              cs, media::HDRMetadata()));
 
@@ -79,7 +72,7 @@ gfx::ColorSpace ToBT709_APPLE(gfx::ColorSpace cs) {
                          cs.GetMatrixID(), cs.GetRangeID());
 }
 
-void AssertHasEmptyHDRMetadata(CFDictionaryRef fmt) {
+void AssertHasEmptyHDRMetadata(CFMutableDictionaryRef fmt) {
   if (__builtin_available(macos 10.13, *)) {
     // We constructed with an empty HDRMetadata, so all values should be zero.
     auto mdcv = GetDataValue(
@@ -96,16 +89,12 @@ void AssertHasEmptyHDRMetadata(CFDictionaryRef fmt) {
   }
 }
 
-constexpr CMVideoCodecType kCMVideoCodecType_VP9 = 'vp09';
-constexpr char kBitDepthKey[] = "BitsPerComponent";
-constexpr char kVpccKey[] = "vpcC";
-
 }  // namespace
 
 namespace media {
 
 TEST(VTConfigUtil, CreateFormatExtensions_H264_BT709) {
-  base::ScopedCFTypeRef<CFDictionaryRef> fmt(
+  base::ScopedCFTypeRef<CFMutableDictionaryRef> fmt(
       CreateFormatExtensions(kCMVideoCodecType_H264, H264PROFILE_MAIN,
                              VideoColorSpace::REC709(), base::nullopt));
   EXPECT_EQ("avc1", GetStrValue(fmt, kCMFormatDescriptionExtension_FormatName));
@@ -130,7 +119,7 @@ TEST(VTConfigUtil, CreateFormatExtensions_H264_BT709) {
 }
 
 TEST(VTConfigUtil, CreateFormatExtensions_H264_BT2020_PQ) {
-  base::ScopedCFTypeRef<CFDictionaryRef> fmt(CreateFormatExtensions(
+  base::ScopedCFTypeRef<CFMutableDictionaryRef> fmt(CreateFormatExtensions(
       kCMVideoCodecType_H264, H264PROFILE_MAIN,
       VideoColorSpace(VideoColorSpace::PrimaryID::BT2020,
                       VideoColorSpace::TransferID::SMPTEST2084,
@@ -154,7 +143,7 @@ TEST(VTConfigUtil, CreateFormatExtensions_H264_BT2020_PQ) {
 }
 
 TEST(VTConfigUtil, CreateFormatExtensions_H264_BT2020_HLG) {
-  base::ScopedCFTypeRef<CFDictionaryRef> fmt(CreateFormatExtensions(
+  base::ScopedCFTypeRef<CFMutableDictionaryRef> fmt(CreateFormatExtensions(
       kCMVideoCodecType_H264, H264PROFILE_MAIN,
       VideoColorSpace(VideoColorSpace::PrimaryID::BT2020,
                       VideoColorSpace::TransferID::ARIB_STD_B67,
@@ -190,7 +179,7 @@ TEST(VTConfigUtil, CreateFormatExtensions_HDRMetadata) {
   mastering.primary_b = gfx::PointF(0.15, 0.06);
   mastering.white_point = gfx::PointF(0.3127, 0.3290);
 
-  base::ScopedCFTypeRef<CFDictionaryRef> fmt(CreateFormatExtensions(
+  base::ScopedCFTypeRef<CFMutableDictionaryRef> fmt(CreateFormatExtensions(
       kCMVideoCodecType_H264, H264PROFILE_MAIN,
       VideoColorSpace(VideoColorSpace::PrimaryID::BT2020,
                       VideoColorSpace::TransferID::SMPTEST2084,
@@ -236,47 +225,6 @@ TEST(VTConfigUtil, CreateFormatExtensions_HDRMetadata) {
                 hdr_meta.max_frame_average_light_level);
     }
   }
-}
-
-TEST(VTConfigUtil, CreateFormatExtensions_VP9Profile0) {
-  constexpr VideoCodecProfile kTestProfile = VP9PROFILE_PROFILE0;
-  const auto kTestColorSpace = VideoColorSpace::REC709();
-  base::ScopedCFTypeRef<CFDictionaryRef> fmt(CreateFormatExtensions(
-      kCMVideoCodecType_VP9, kTestProfile, kTestColorSpace, base::nullopt));
-  EXPECT_EQ(8, GetIntValue(fmt, base::SysUTF8ToCFStringRef(kBitDepthKey)));
-
-  auto vpcc = GetNestedDataValue(
-      fmt, kCMFormatDescriptionExtension_SampleDescriptionExtensionAtoms,
-      base::SysUTF8ToCFStringRef(kVpccKey));
-  std::unique_ptr<mp4::BoxReader> box_reader(
-      mp4::BoxReader::ReadConcatentatedBoxes(vpcc.data(), vpcc.size(),
-                                             nullptr));
-  mp4::VPCodecConfigurationRecord vpcc_box;
-  ASSERT_TRUE(vpcc_box.Parse(box_reader.get()));
-  ASSERT_EQ(kTestProfile, vpcc_box.profile);
-  ASSERT_EQ(kTestColorSpace, vpcc_box.color_space);
-}
-
-TEST(VTConfigUtil, CreateFormatExtensions_VP9Profile2) {
-  constexpr VideoCodecProfile kTestProfile = VP9PROFILE_PROFILE2;
-  const VideoColorSpace kTestColorSpace(
-      VideoColorSpace::PrimaryID::BT2020,
-      VideoColorSpace::TransferID::SMPTEST2084,
-      VideoColorSpace::MatrixID::BT2020_NCL, gfx::ColorSpace::RangeID::LIMITED);
-  base::ScopedCFTypeRef<CFDictionaryRef> fmt(CreateFormatExtensions(
-      kCMVideoCodecType_VP9, kTestProfile, kTestColorSpace, base::nullopt));
-  EXPECT_EQ(10, GetIntValue(fmt, base::SysUTF8ToCFStringRef(kBitDepthKey)));
-
-  auto vpcc = GetNestedDataValue(
-      fmt, kCMFormatDescriptionExtension_SampleDescriptionExtensionAtoms,
-      base::SysUTF8ToCFStringRef(kVpccKey));
-  std::unique_ptr<mp4::BoxReader> box_reader(
-      mp4::BoxReader::ReadConcatentatedBoxes(vpcc.data(), vpcc.size(),
-                                             nullptr));
-  mp4::VPCodecConfigurationRecord vpcc_box;
-  ASSERT_TRUE(vpcc_box.Parse(box_reader.get()));
-  ASSERT_EQ(kTestProfile, vpcc_box.profile);
-  ASSERT_EQ(kTestColorSpace, vpcc_box.color_space);
 }
 
 TEST(VTConfigUtil, GetImageBufferColorSpace_BT601) {
