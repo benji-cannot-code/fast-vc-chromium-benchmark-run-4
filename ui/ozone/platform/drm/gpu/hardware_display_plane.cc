8c561288b438966b34db90ab3de30a19ce7e159b
David Stevens <stevensd@chromium.org>
2020-08-11 03:22:57 +0000

    ozone/drm: force bt601 colorspace
    Until system wide colorspace support is improved, force planes to BT601
    for consistency across platforms and kernel versions.
    
    Bug: 1102317
    Change-Id: Ice5a1221b5a142a5c163288d4261eca2fb1c611c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2336347
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: David Stevens <stevensd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796687}

--- ui/ozone/platform/drm/gpu/hardware_display_plane.cc
+++ ui/ozone/platform/drm/gpu/hardware_display_plane.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <drm_mode.h>
 
 #include "base/logging.h"
+#include "ui/ozone/platform/drm/common/drm_util.h"
 #include "ui/ozone/platform/drm/gpu/drm_device.h"
 #include "ui/ozone/platform/drm/gpu/drm_gpu_util.h"
 
@@ -93,6 +94,14 @@ bool HardwareDisplayPlane::Initialize(DrmDevice* drm) {
   if (properties_.type.id)
     type_ = GetPlaneType(properties_.type.value);
 
+  if (properties_.plane_color_encoding.id) {
+    color_encoding_bt601_ =
+        GetEnumValueForName(drm->get_fd(), properties_.plane_color_encoding.id,
+                            "ITU-R BT.601 YCbCr");
+    color_range_limited_ = GetEnumValueForName(
+        drm->get_fd(), properties_.plane_color_range.id, "YCbCr limited range");
+  }
+
   VLOG(3) << "Initialized plane=" << id_ << " crtc_mask=" << std::hex << "0x"
           << crtc_mask_ << std::dec
           << " supported_formats_count=" << supported_formats_.size()
@@ -169,6 +178,10 @@ void HardwareDisplayPlane::InitializeProperties(DrmDevice* drm) {
   GetDrmPropertyForName(drm, props.get(), "IN_FENCE_FD",
                         &properties_.in_fence_fd);
   GetDrmPropertyForName(drm, props.get(), "PLANE_CTM", &properties_.plane_ctm);
+  GetDrmPropertyForName(drm, props.get(), "COLOR_ENCODING",
+                        &properties_.plane_color_encoding);
+  GetDrmPropertyForName(drm, props.get(), "COLOR_RANGE",
+                        &properties_.plane_color_range);
 }
 
 }  // namespace ui
