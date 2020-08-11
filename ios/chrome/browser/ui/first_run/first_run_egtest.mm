6f7af0358daf27b680c9c0fd519007430b2d5087
Nohemi Fernandez <fernandex@chromium.org>
2020-08-11 08:36:29 +0000

    [iOS] Re-enable EarlGrey test FirstRun.testToggleMetricsOn.
    This test appears to have been classified as flaky when in fact the
    analysis tools show that there were infrastructure failures for the
    ios_chrome_ui_eg2tests_module across various devices.
    
    https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyVgsSBUZsYWtlIktjaHJvbWl1bUBpb3NfY2hyb21lX3VpX2VnMnRlc3RzX21vZHVsZUBGaXJzdFJ1blRlc3RDYXNlL3Rlc3RUb2dnbGVNZXRyaWNzT24M
    
    Bug: 1109117
    Change-Id: I0ccfebacb5c04c764eb494e9ca43639a9d30005b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343109
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#796745}

--- ios/chrome/browser/ui/first_run/first_run_egtest.mm
+++ ios/chrome/browser/ui/first_run/first_run_egtest.mm
@@ -82,8 +82,7 @@ id<GREYMatcher> SkipSigninButton() {
 }
 
 // Toggle the UMA checkbox.
-// TODO(crbug.com/1109117): Test is flaky.
-- (void)DISABLED_testToggleMetricsOn {
+- (void)testToggleMetricsOn {
   [FirstRunAppInterface showFirstRunUI];
 
   id<GREYMatcher> metrics =
