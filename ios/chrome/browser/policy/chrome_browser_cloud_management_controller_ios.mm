a1872baa72d6fb5e09443c877de403308624ace3
Guillaume Jenkins <gujen@google.com>
2020-08-12 16:40:13 +0000

    [iOS CBCM] Add support for enterprise reporting
    Creates and implements the missing delegates for enterprise reporting
    on iOS.
    
    Note that this won't be functional yet because of an issue with the
    way ApplicationContext instantiates its shared URLLoaderFactory. That
    issue will be fixed in a separate CL.
    
    #Squeegee
    
    Bug: 1066495
    Change-Id: Idb493efeca63bb19eaf250ede02608f6bd842a81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2341995
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797290}

--- ios/chrome/browser/policy/chrome_browser_cloud_management_controller_ios.mm
+++ ios/chrome/browser/policy/chrome_browser_cloud_management_controller_ios.mm
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/policy/chrome_browser_cloud_management_controller_ios.h"
 
 #include "base/bind.h"
+#include "components/enterprise/browser/reporting/report_generator.h"
+#include "components/enterprise/browser/reporting/report_scheduler.h"
 #include "components/policy/core/common/cloud/machine_level_user_cloud_policy_manager.h"
 #include "components/policy/core/common/features.h"
 #include "ios/chrome/browser/application_context.h"
@@ -108,8 +110,10 @@ ChromeBrowserCloudManagementControllerIOS::GetSharedURLLoaderFactory() {
 std::unique_ptr<enterprise_reporting::ReportScheduler>
 ChromeBrowserCloudManagementControllerIOS::CreateReportScheduler(
     CloudPolicyClient* client) {
-  // TODO(crbug.com/1066495): Finish iOS CBCM implementation.
-  return nullptr;
+  auto generator = std::make_unique<enterprise_reporting::ReportGenerator>(
+      &reporting_delegate_factory_);
+  return std::make_unique<enterprise_reporting::ReportScheduler>(
+      client, std::move(generator), &reporting_delegate_factory_);
 }
 
 void ChromeBrowserCloudManagementControllerIOS::SetGaiaURLLoaderFactory(
