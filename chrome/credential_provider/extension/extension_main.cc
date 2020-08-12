cd4c18129bb247db7dcb302f6c7efab9388d1170
Yusuf Sengul <yusufsn@google.com>
2020-08-12 23:32:44 +0000

    Add bare GCPW extension service implementation
    Adds the logic that handshakes with the service control manager
    and runs the process as a service.
    
    Bug: 1101590
    Change-Id: Id0825d3ea5bbfccb17147740eda26bff04367037
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2329045
    Reviewed-by: Rakesh Soma <rakeshsoma@google.com>
    Commit-Queue: Yusuf Sengul <yusufsn@google.com>
    Cr-Commit-Position: refs/heads/master@{#797476}

--- chrome/credential_provider/extension/extension_main.cc
+++ chrome/credential_provider/extension/extension_main.cc
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/process/memory.h"
+#include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/win/process_startup_helper.h"
 #include "chrome/credential_provider/eventlog/gcp_eventlog_messages.h"
+#include "chrome/credential_provider/extension/os_service_manager.h"
+#include "chrome/credential_provider/extension/service.h"
 #include "chrome/credential_provider/gaiacp/logging.h"
 
 int APIENTRY wWinMain(HINSTANCE hInstance,
@@ -49,7 +52,10 @@ int APIENTRY wWinMain(HINSTANCE hInstance,
   // Set the event logging source and category for GCPW Extension.
   logging::SetEventSource("GCPW", GCPW_EXTENSION_CATEGORY, MSG_LOG_MESSAGE);
 
-  LOGFN(VERBOSE) << "GCPW Extension started running.";
+  // This initializes and starts ThreadPoolInstance with default params.
+  base::ThreadPoolInstance::CreateAndStartWithDefaultParams("gcpw_extension");
+
+  credential_provider::extension::Service::Get()->Run();
 
   return 0;
 }
