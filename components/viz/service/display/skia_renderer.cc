1957edaba270e420fff1a1bfabb9cc542b8f7a43
Mario Bianucci <mabian@microsoft.com>
2020-08-12 18:35:15 +0000

    Store delegated ink points and prepare to draw trail with Skia
    Flesh out the delegated ink renderer class a bit more, including a
    specific Skia flavor of it, with the expectation that others (software,
    GL) may come in the future. Storing the delegated ink points in this
    class as they arrive from the browser process, forwarding the metadata
    to this class from the aggregated frame, and filtering the points in
    the function that will eventually contain the drawing code.
    
    Bug: 1052145
    Change-Id: Ic86e37d9ed1d24b6427fa6896b668de8d0402b8e
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2296567
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Commit-Queue: Mario Bianucci <mabian@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#797347}

--- components/viz/service/display/skia_renderer.cc
+++ components/viz/service/display/skia_renderer.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/resources/platform_color.h"
 #include "components/viz/common/resources/resource_format_utils.h"
 #include "components/viz/common/skia_helper.h"
+#include "components/viz/service/display/delegated_ink_point_renderer_skia.h"
 #include "components/viz/service/display/display_resource_provider.h"
 #include "components/viz/service/display/output_surface.h"
 #include "components/viz/service/display/output_surface_frame.h"
@@ -2776,6 +2777,13 @@ gfx::Size SkiaRenderer::GetRenderPassBackingPixelSize(
   auto it = render_pass_backings_.find(render_pass_id);
   DCHECK(it != render_pass_backings_.end());
   return it->second.size;
+}
+
+bool SkiaRenderer::CreateDelegatedInkPointRenderer() {
+  DCHECK(!delegated_ink_point_renderer_);
+  delegated_ink_point_renderer_ =
+      std::make_unique<DelegatedInkPointRendererSkia>();
+  return true;
 }
 
 }  // namespace viz
--- components/viz/service/frame_sinks/root_compositor_frame_sink_impl.cc
+++ components/viz/service/frame_sinks/root_compositor_frame_sink_impl.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "components/viz/common/frame_sinks/begin_frame_source.h"
-#include "components/viz/service/display/delegated_ink_point_renderer.h"
+#include "components/viz/service/display/delegated_ink_point_renderer_base.h"
 #include "components/viz/service/display/display.h"
 #include "components/viz/service/display/output_surface.h"
 #include "components/viz/service/display_embedder/output_surface_provider.h"
@@ -307,8 +307,8 @@ void RootCompositorFrameSinkImpl::AddVSyncParameterObserver(
 
 void RootCompositorFrameSinkImpl::SetDelegatedInkPointRenderer(
     mojo::PendingReceiver<mojom::DelegatedInkPointRenderer> receiver) {
-  display_->set_delegated_ink_point_renderer(
-      std::make_unique<DelegatedInkPointRendererImpl>(std::move(receiver)));
+  if (auto* ink_renderer = display_->GetDelegatedInkPointRenderer())
+    ink_renderer->InitMessagePipeline(std::move(receiver));
 }
 
 void RootCompositorFrameSinkImpl::SetNeedsBeginFrame(bool needs_begin_frame) {
