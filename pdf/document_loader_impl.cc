d4c6933a8540b92757574bd309d1556e05337833
K. Moon <kmoon@chromium.org>
2020-08-12 19:39:40 +0000

    Control PDF partial loading with a feature
    Adds a "PdfPartialLoading" feature to control whether or not the PDF
    viewer tries to use partial loading. The default state is enabled, as
    this feature has shipped already, but we would like to experiment with
    disabling it to address certain problems related to partial loading.
    
    Bug: 1115149
    Change-Id: Ie66fddf5b633269226bc01fca20ac7013f0c1e7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2349429
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797378}

--- pdf/document_loader_impl.cc
+++ pdf/document_loader_impl.cc
@@ -14,9 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/check_op.h"
+#include "base/feature_list.h"
 #include "base/notreached.h"
 #include "base/numerics/safe_math.h"
 #include "base/strings/string_util.h"
+#include "pdf/pdf_features.h"
 #include "pdf/url_loader_wrapper.h"
 #include "ppapi/c/pp_errors.h"
 #include "ui/gfx/range/range.h"
@@ -66,7 +68,10 @@ void DocumentLoaderImpl::Chunk::Clear() {
   chunk_data.reset();
 }
 
-DocumentLoaderImpl::DocumentLoaderImpl(Client* client) : client_(client) {}
+DocumentLoaderImpl::DocumentLoaderImpl(Client* client)
+    : client_(client),
+      partial_loading_enabled_(
+          base::FeatureList::IsEnabled(features::kPdfPartialLoading)) {}
 
 DocumentLoaderImpl::~DocumentLoaderImpl() = default;
 
