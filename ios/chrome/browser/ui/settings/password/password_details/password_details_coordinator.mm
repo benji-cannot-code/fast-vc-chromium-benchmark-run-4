254308319444b8306783374c460f0544e8ac429b
Viktor Semeniuk <vsemeniuk@google.com>
2020-08-12 11:05:16 +0000

    [iOS][Password Сheck] Fixed crash when displaying alerts on iPads
    This change fixes crashes when editing/deletion confirmation alerts are
    displayed.
    
    Bug: 1115052, 1112344
    Change-Id: I3bbd31c4d37d5036655c51a3caba4de464fb9b55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2351783
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#797189}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_coordinator.mm
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_coordinator.mm
@@ -149,7 +149,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                          browser:self.browser
                            title:nil
                          message:message
-                   barButtonItem:nil];
+                   barButtonItem:self.viewController.navigationItem
+                                     .rightBarButtonItem];
 
   __weak __typeof(self) weakSelf = self;
 
@@ -178,7 +179,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                          browser:self.browser
                            title:nil
                          message:message
-                   barButtonItem:nil];
+                   barButtonItem:self.viewController.navigationItem
+                                     .rightBarButtonItem];
 
   __weak __typeof(self) weakSelf = self;
 
