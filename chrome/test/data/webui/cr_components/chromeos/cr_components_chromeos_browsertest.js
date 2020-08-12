6bf331b8509cf2032483d0ca554aa50d8c53d097
James Hawkins <jhawkins@chromium.org>
2020-08-12 21:41:31 +0000

    cr_components/network: Convert NetworkConfigElementBehavior to Polymer3.
    R=khorimoto@chromium.org
    
    Bug: 1045266
    Test: CrComponentsNetworkConfigElementBehaviorTestV3.All
    Change-Id: Iec3529a023fb5c00c341d6f8d535991fe37552d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2347072
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797428}

--- chrome/test/data/webui/cr_components/chromeos/cr_components_chromeos_browsertest.js
+++ chrome/test/data/webui/cr_components/chromeos/cr_components_chromeos_browsertest.js
@@ -39,6 +39,9 @@ GEN('#endif');
       '../../chromeos/fake_network_config_mojom.js',
     ]
   ],
+  ['NetworkConfigElementBehavior', 'network_config_element_behavior_test.js',
+    []
+  ],
 ].forEach(test => registerTest(...test));
 // clang-format on
 
@@ -56,5 +59,5 @@ function registerTest(testName, module, deps) {
     }
   };
 
-  TEST_F(className, 'MAYBE_All', () => mocha.run());
+  TEST_F(className, 'All', () => mocha.run());
 }
