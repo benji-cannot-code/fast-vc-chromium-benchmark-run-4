352a0991214eae5ec3a238cfff2a84a89e185ba6
Alex Keng <shihken@microsoft.com>
2020-08-12 20:05:58 +0000

    TSF: propagate HRESULT from TSF initialization
    This CL changes the return value of TSFBridge::Initialize from void to
    HRESULT and refactors the logics in TSFTextStore::TSFTextStore out to
    TSFTextStore::Initialize. The HRESULTs of various COM calls during TSF
    initialization are propagated so the caller can handle errors, if any,
    accordingly.
    
    Note at the moment upstream doesn't have any use case for the HRESULT,
    but downstream will use the value for WebView2 initialization, ex.
    handling errors due to creating CLSID_TF_CategoryMgr instance in a
    thread initialized with COM_MTA (while all TSF functions expect STA)
    
    Change-Id: I227000a594f203eecc37838ff877d3dcd077df2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340543
    Commit-Queue: Alex Keng <shihken@microsoft.com>
    Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797396}

--- ui/base/ime/win/tsf_text_store.h
+++ ui/base/ime/win/tsf_text_store.h
@@ -109,6 +109,7 @@ class COMPONENT_EXPORT(UI_BASE_IME_WIN) TSFTextStore
  public:
   TSFTextStore();
   virtual ~TSFTextStore();
+  HRESULT Initialize();
 
   // ITextStoreACP:
   IFACEMETHODIMP_(ULONG) AddRef() override;
