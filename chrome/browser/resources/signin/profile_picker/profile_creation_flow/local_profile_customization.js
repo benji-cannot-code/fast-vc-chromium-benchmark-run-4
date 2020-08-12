451b297e23fa2a36e0e878ce8bc9606a166fbe0a
dizg <dizg@google.com>
2020-08-12 20:00:58 +0000

    [signin] Create view for the local profile customization
    Bug: 1115056
    Change-Id: I46e9750b9755e2ee62b3e3974b0ff6663d00de41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2346354
    Commit-Queue: Diana Zagidullina <dizg@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797393}

--- chrome/browser/resources/signin/profile_picker/profile_creation_flow/local_profile_customization.js
+++ chrome/browser/resources/signin/profile_picker/profile_creation_flow/local_profile_customization.js
@@ -3,5 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './profile_type_choice.js';
-import './local_profile_customization.js';
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+Polymer({
+  is: 'local-profile-customization',
+
+  _template: html`{__html_template__}`,
+});
