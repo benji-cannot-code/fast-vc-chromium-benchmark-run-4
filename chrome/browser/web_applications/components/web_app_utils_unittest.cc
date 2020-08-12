cc7291e1ad6579fde8a5d70c6149ac8bcfbd2667
Alexey Baskakov <loyso@chromium.org>
2020-08-12 11:42:47 +0000

    dpwa: Add unit tests for WebAppUrlLoader::PrepareForLoad flushing.
    These are follow up unit tests for this diff:
    https://chromium-review.googlesource.com/c/chromium/src/+/2237542/13/chrome/browser/web_applications/components/web_app_url_loader.cc
    
    The change was:
    1) Let's load about:blank page before each WebAppInstallTask
    to reset the renderer process.
    It gives us that pseudo-Reset API with a guaranteed completion callback.
    2) Let's use about:blank url as a special barrier in WebAppUrlLoader:
    if WebAppUrlLoader awaits on
    about:blank loading, it should flush (skip) all noisy
    DidFinishLoading and DidFailLoading events.
    
    Note that these tests are complementary to existing browser tests in
    web_app_url_loader_browsertest.cc.
    
    However, we need low-level unit test environment to reproduce
    flakiness for DidFinishLoading and DidFailLoading events (unit tests
    give us that level of detail and control)
    
    Bug: 1086778
    Change-Id: I77ff72ac55fa227b3bc4af97da8062e8c445bfe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2320395
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797198}

--- chrome/browser/web_applications/components/web_app_utils_unittest.cc
+++ chrome/browser/web_applications/components/web_app_utils_unittest.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app {
 
-class WebAppUtilsTest : public WebAppTest {};
+using WebAppUtilsTest = WebAppTest;
 
 TEST_F(WebAppUtilsTest, AreWebAppsEnabled) {
   Profile* regular_profile = profile();
