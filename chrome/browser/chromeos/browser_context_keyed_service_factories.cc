3230f02d47a8d901d1b862ebfdc616a8787c0aaa
Kyle Horimoto <khorimoto@chromium.org>
2020-08-12 20:52:04 +0000

    [CrOS PhoneHub] Add PhoneHubManager class
    This class will implement the core functionality for Phone Hub.
    Currently, it simply instantiates an instance of
    FeatureStatusProviderImpl.
    
    Additionally, we provide a static Get() function to be called by //ash
    in ash::Shell::OnFirstSessionStarted() to get access to Phone Hub
    without taking a dependency on //chrome (which is not allowed).
    
    Bug: 1106937
    Change-Id: I980237970349dc3b0ec47b0d144a59680c1c14bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2350024
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797416}

--- chrome/browser/chromeos/browser_context_keyed_service_factories.cc
+++ chrome/browser/chromeos/browser_context_keyed_service_factories.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/launcher_search_provider/launcher_search_provider_service_factory.h"
 #include "chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_factory.h"
 #include "chrome/browser/chromeos/ownership/owner_settings_service_chromeos_factory.h"
+#include "chrome/browser/chromeos/phonehub/phone_hub_manager_factory.h"
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_engagement_metrics_service.h"
 #include "chrome/browser/chromeos/policy/policy_cert_service_factory.h"
 #include "chrome/browser/chromeos/policy/user_cloud_policy_token_forwarder_factory.h"
@@ -71,6 +72,7 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   KerberosCredentialsManagerFactory::GetInstance();
   launcher_search_provider::ServiceFactory::GetInstance();
   OwnerSettingsServiceChromeOSFactory::GetInstance();
+  phonehub::PhoneHubManagerFactory::GetInstance();
   plugin_vm::PluginVmEngagementMetricsService::Factory::GetInstance();
   policy::PolicyCertServiceFactory::GetInstance();
   policy::UserCloudPolicyTokenForwarderFactory::GetInstance();
