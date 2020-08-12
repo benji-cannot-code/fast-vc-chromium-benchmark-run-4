deaf605f7c42f5ec9b0186782067967f96e61e29
Yi Gu <yigu@chromium.org>
2020-08-12 13:11:43 +0000

    [WebOTP] Report SMSReceiverStart in Blink.UseCounter.Features
    We no longer report after crrev.com/c/2097041. This patch resumes the
    reporting logic.
    
    Bug: 1107590
    Change-Id: I9dd8d30a91f48077694a6a3d054f36aa9b259868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2350093
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Sam Goto <goto@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797214}

--- third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/metrics/histogram_macros.h"
 #include "build/build_config.h"
 #include "third_party/blink/public/common/sms/sms_receiver_outcome.h"
 #include "third_party/blink/public/mojom/credentialmanager/credential_manager.mojom-blink.h"
@@ -655,6 +656,8 @@ ScriptPromise CredentialsContainer::get(
         CredentialManagerProxy::From(script_state)->SmsReceiver();
     sms_receiver->Receive(WTF::Bind(&OnSmsReceive, WrapPersistent(resolver),
                                     base::TimeTicks::Now()));
+    UMA_HISTOGRAM_ENUMERATION("Blink.UseCounter.Features",
+                              WebFeature::kSMSReceiverStart);
     return promise;
   }
 
