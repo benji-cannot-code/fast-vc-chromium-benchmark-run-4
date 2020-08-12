64c3009e020ece2aba255ff19742c4634cbf4053
Sreeja Kamishetty <sreejakshetty@chromium.org>
2020-08-12 10:47:06 +0000

    Fix and Clear failing tests in bfcache.browser_test_filters
    This CL disables the failing browser_tests with BackForwardCache and
    updates the filter.
    
    Bug: 1100315
    Change-Id: I1f3af955b56b2d2eb91f793fe3c7c25826618ca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2273499
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797184}

--- chrome/browser/extensions/process_management_browsertest.cc
+++ chrome/browser/extensions/process_management_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/extension_apitest.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -51,6 +53,13 @@ bool IsExtensionProcessSharingAllowed() {
 }
 
 class ProcessManagementTest : public ExtensionBrowserTest {
+ public:
+  ProcessManagementTest() {
+    // TODO(https://crbug.com/1110891): Remove this once Extensions are
+    // supported with BackForwardCache.
+    disabled_feature_list_.InitWithFeatures({}, {features::kBackForwardCache});
+  }
+
  private:
   // This is needed for testing isolated apps, which are still experimental.
   void SetUpCommandLine(base::CommandLine* command_line) override {
@@ -63,6 +72,8 @@ class ProcessManagementTest : public ExtensionBrowserTest {
     ExtensionBrowserTest::SetUpOnMainThread();
     host_resolver()->AddRule("*", "127.0.0.1");
   }
+
+  base::test::ScopedFeatureList disabled_feature_list_;
 };
 
 class ChromeWebStoreProcessTest : public ExtensionBrowserTest {
--- chrome/browser/extensions/process_manager_browsertest.cc
+++ chrome/browser/extensions/process_manager_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
@@ -37,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/navigation_policy.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -198,6 +200,9 @@ class ProcessManagerBrowserTest : public ExtensionBrowserTest {
  public:
   ProcessManagerBrowserTest() {
     guest_view::GuestViewManager::set_factory_for_testing(&factory_);
+    // TODO(https://crbug.com/1110891): Remove this once Extensions are
+    // supported with BackForwardCache.
+    disabled_feature_list_.InitWithFeatures({}, {features::kBackForwardCache});
   }
 
   void SetUpOnMainThread() override {
@@ -299,6 +304,7 @@ class ProcessManagerBrowserTest : public ExtensionBrowserTest {
  private:
   guest_view::TestGuestViewManagerFactory factory_;
   std::vector<std::unique_ptr<TestExtensionDir>> temp_dirs_;
+  base::test::ScopedFeatureList disabled_feature_list_;
 };
 
 class DefaultProfileExtensionBrowserTest : public ExtensionBrowserTest {
