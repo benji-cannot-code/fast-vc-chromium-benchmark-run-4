ac97271b3a9d98b2a1ba6282c36d10715c3cf16b
Austin Tankiang <austinct@chromium.org>
2020-08-12 10:00:48 +0000

    Invalidate ActionsModels for Drive entries on pinned changes
    The pinning related actions change depending on whether the entry is
    pinned or not, which can change externally from the ActionsModel. So,
    invalidate ActionsModels for Drive entries when pinned state changes.
    
    Also remove destroy() as it is never used.
    
    Bug: 1096920
    Change-Id: Iaafc87bd38035f548cd403eda1ee7014360595e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2342524
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797178}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -588,6 +588,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
     ::testing::Values(TestCase("driveOpenSidebarOffline"),
                       TestCase("driveOpenSidebarSharedWithMe"),
                       TestCase("driveAutoCompleteQuery"),
+                      TestCase("drivePinMultiple"),
                       TestCase("drivePinFileMobileNetwork"),
                       TestCase("driveClickFirstSearchResult"),
                       TestCase("drivePressEnterToSearch"),
