dfe2009204a8fa2c1019337123e2fbf2fc95dd1b
Sharon Yang <yangsharon@chromium.org>
2020-08-12 02:22:08 +0000

    [fuchsia] Support scrolling to show offscreen a11y nodes onscreen
    Support scrolling an offscreen node to be visible for screen reader use.
    Clean up unused param in DeleteSubtree.
    
    Test: AccessibilityBridgeTest.PerformScrollToMakeVisible
    Bug: fuchsia:55864
    Change-Id: I3ab468e1cfb3084ee7517f6cf45e8cc0e3e8da1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2142740
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797083}

--- fuchsia/engine/browser/ax_tree_converter.h
+++ fuchsia/engine/browser/ax_tree_converter.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WEB_ENGINE_EXPORT fuchsia::accessibility::semantics::Node
 AXNodeDataToSemanticNode(const ui::AXNodeData& node);
 
-// Converts Fuchsia action of type |fuchsia_action| to an ax::mojom action of
+// Converts Fuchsia action of type |fuchsia_action| to an ax::mojom::Action of
 // type |mojom_action|. Function will return true if |fuchsia_action| is
 // supported in Chromium.
 bool ConvertAction(fuchsia::accessibility::semantics::Action fuchsia_action,
