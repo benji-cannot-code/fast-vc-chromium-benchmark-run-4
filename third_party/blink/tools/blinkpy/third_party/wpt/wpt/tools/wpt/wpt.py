e25cc3aceb613807e9d141fa2e8faa978291c7c2
Robert Ma <robertma@chromium.org>
2020-08-12 20:58:42 +0000

    [blinkpy] Roll WPT tools
    Importantly, this rolls some webdriver/ changes that will fix the
    regression in issue 1115609. Removed the related expectations introduced
    in the import https://crrev.com/c/2351549 as well.
    
    Bug: 1115609
    Change-Id: I5f0ebfc1d657556e166fc6d433b24f3f2ff8a14a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2351383
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797422}

--- third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/wpt/wpt.py
+++ third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/wpt/wpt.py
@@ -39,6 +39,8 @@ def load_commands():
                     "requirements": [os.path.join(base_dir, item)
                                      for item in props.get("requirements", [])]
                 }
+                if rv[command]["install"] or rv[command]["requirements"]:
+                    assert rv[command]["virtualenv"]
     return rv
 
 
