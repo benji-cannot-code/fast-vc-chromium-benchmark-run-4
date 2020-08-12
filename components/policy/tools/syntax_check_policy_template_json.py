70f051898b6c494686bf9e627418790373c64c7c
Brian Malcolm <bmalcolm@chromium.org>
2020-08-12 20:52:08 +0000

    Add new Printers policy
    This is intended to replace the NativePrinters policy.
    The old policy will still be used in the absence of the new one for at
    least a few releases of Chrome.
    
    NOPRESUBMIT=true
    
    BUG=chromium:1113362
    
    Change-Id: I3d0616d9f414fa391be24d71780513df86e2a5ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340363
    Commit-Queue: Brian Malcolm <bmalcolm@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797417}

--- components/policy/tools/syntax_check_policy_template_json.py
+++ components/policy/tools/syntax_check_policy_template_json.py
@@ -64,6 +64,7 @@ LEGACY_EMBEDDED_JSON_ALLOWLIST = [
     'DeviceLoginScreenAutoSelectCertificateForUrls',
     'DeviceOpenNetworkConfiguration',
     'NativePrinters',
+    'Printers',
     'OpenNetworkConfiguration',
     'RemoteAccessHostDebugOverridePolicies',
     # NOTE: Do not add any new policies to this list! Do not store policies with
