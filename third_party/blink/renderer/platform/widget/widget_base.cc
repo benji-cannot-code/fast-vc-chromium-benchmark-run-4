9e3e164064ec191bc796bdb92d9ee370a0e989ef
Miyoung Shin <myid.shin@igalia.com>
2020-08-12 08:48:30 +0000

    Reland "Reland "Convert FrameMsg_VisualStateRequest to mojo""
    This is a reland of 97950371c29067eab7473fb282239e8dc65780e5.
    
    Reason for revert: Flaky SafeBrowsing tests on android_webview: https://bugs.chromium.org/p/chromium/issues/detail?id=1111868
    
    Fix : Destroy the callbacks on the compositor thread when |widget_base_| is nullptr.
    
    Original change's description:
    > Reland "Convert FrameMsg_VisualStateRequest to mojo"
    >
    > This is a reland of f7d812d920c8ba5a16f885173cb426178e3fb8bd.
    >
    > Reason for revert: org.chromium.android_webview.test.VisualStateTest#testVisualStateCallbackWhenContainerViewDetached__multiprocess_mode was flaky
    >
    > Fix : Fire the swap callback in QueueReportTimeSwapPromise::DidActivate
    > if synchronous compositing is enabled on Android WebView.
    >
    > Original change's description:
    > > Convert FrameMsg_VisualStateRequest to mojo
    > >
    > > This CL converts FrameMsg_VisualStateRequest to FrameWidget Mojo message
    > > and FrameHostMsg_VisualStateResponse to the return callback of
    > > FrameWidget mojo message.
    > >
    > > The legacy IPC message sent a single IPC with the multiple sub IPC
    > > messages in reply, but this CL invokes each mojo message's callback
    > > queued in the queue when the frame swap has occurred.
    > >
    > > This CL also includes to
    > >  - add WidgetCompositorHost interface bound on the compositor thread.
    > >  - move the implementation of |visual_state_callbacks_| from
    > >    RenderFrameHost to RenderWidgetHost.
    > >  - introduce WidgetSwapQueue queuing |id| having the same source
    > >    frame number to invoke the callbacks.
    > >
    > > Bug: 1071588
    > > Change-Id: If8c4db40be397921d19ca6a9a9287760879f3ff1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2178480
    > > Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    > > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#789884}
    >
    > Bug: 1071588
    > Change-Id: If7bfaa21277da199632e5c6ec748b54cea7eef58
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2309317
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#793560}
    
    Bug: 1071588, 1111868
    Change-Id: I2cf298cf75ad75d9345230e34d23412b773870b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2342532
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#797161}

--- third_party/blink/renderer/platform/widget/widget_base.cc
+++ third_party/blink/renderer/platform/widget/widget_base.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/widget/compositing/layer_tree_settings.h"
 #include "third_party/blink/renderer/platform/widget/compositing/layer_tree_view.h"
+#include "third_party/blink/renderer/platform/widget/compositing/widget_compositor.h"
 #include "third_party/blink/renderer/platform/widget/frame_widget.h"
 #include "third_party/blink/renderer/platform/widget/input/ime_event_guard.h"
 #include "third_party/blink/renderer/platform/widget/input/main_thread_event_queue.h"
@@ -181,6 +182,11 @@ void WidgetBase::Shutdown(
       FROM_HERE,
       base::BindOnce([](scoped_refptr<WidgetInputHandlerManager> manager) {},
                      std::move(widget_input_handler_manager_)));
+
+  if (widget_compositor_) {
+    widget_compositor_->Shutdown();
+    widget_compositor_ = nullptr;
+  }
 }
 
 cc::LayerTreeHost* WidgetBase::LayerTreeHost() const {
@@ -664,6 +670,18 @@ void WidgetBase::ProcessTouchAction(cc::TouchAction touch_action) {
 void WidgetBase::SetFocus(bool enable) {
   has_focus_ = enable;
   client_->FocusChanged(enable);
+}
+
+void WidgetBase::BindWidgetCompositor(
+    mojo::PendingReceiver<mojom::blink::WidgetCompositor> receiver) {
+  if (widget_compositor_)
+    widget_compositor_->Shutdown();
+
+  widget_compositor_ = base::MakeRefCounted<WidgetCompositor>(
+      weak_ptr_factory_.GetWeakPtr(),
+      LayerTreeHost()->GetTaskRunnerProvider()->MainThreadTaskRunner(),
+      LayerTreeHost()->GetTaskRunnerProvider()->ImplThreadTaskRunner(),
+      std::move(receiver));
 }
 
 void WidgetBase::UpdateCompositionInfo(bool immediate_request) {
