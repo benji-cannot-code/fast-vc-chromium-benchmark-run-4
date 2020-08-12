b6aee08b293f9e4c3426a2399ff1668a218e59b5
Marc Treib <treib@chromium.org>
2020-08-12 00:50:29 +0000

    Wallet on Android: Add feature flag for first-setup-complete check
    Bug: 1113713
    Change-Id: Ibcaf5cb39b4f7b961847219547e61adc87608083
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2345310
    Commit-Queue: Jared Saul <jsaul@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#797064}

--- components/autofill/core/browser/payments/autofill_wallet_model_type_controller.cc
+++ components/autofill/core/browser/payments/autofill_wallet_model_type_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/feature_list.h"
 #include "build/build_config.h"
 #include "components/autofill/core/common/autofill_prefs.h"
 #include "components/prefs/pref_service.h"
@@ -16,6 +17,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/driver/sync_user_settings.h"
 #include "google_apis/gaia/google_service_auth_error.h"
+
+namespace {
+
+#if defined(OS_ANDROID)
+constexpr base::Feature kWalletRequiresFirstSyncSetupComplete{
+    "WalletRequiresFirstSyncSetupComplete", base::FEATURE_ENABLED_BY_DEFAULT};
+#endif
+
+}  // namespace
 
 namespace browser_sync {
 
@@ -91,10 +101,13 @@ AutofillWalletModelTypeController::GetPreconditionState() const {
       pref_service_->GetBoolean(autofill::prefs::kAutofillCreditCardEnabled) &&
       !sync_service_->GetAuthError().IsPersistentError();
 #if defined(OS_ANDROID)
-  // On Android, it's also required that the initial Sync setup is complete
-  // (i.e. the user has previously opted in to Sync-the-feature, even if it's
-  // not enabled right now).
-  preconditions_met &= sync_service_->GetUserSettings()->IsFirstSetupComplete();
+  if (base::FeatureList::IsEnabled(kWalletRequiresFirstSyncSetupComplete)) {
+    // On Android, it's also required that the initial Sync setup is complete
+    // (i.e. the user has previously opted in to Sync-the-feature, even if it's
+    // not enabled right now).
+    preconditions_met &=
+        sync_service_->GetUserSettings()->IsFirstSetupComplete();
+  }
 #endif
   return preconditions_met ? PreconditionState::kPreconditionsMet
                            : PreconditionState::kMustStopAndClearData;
