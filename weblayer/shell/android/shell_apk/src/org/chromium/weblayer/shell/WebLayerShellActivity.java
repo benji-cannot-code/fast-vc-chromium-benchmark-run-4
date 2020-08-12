0df1f0f28a971588f8c854156f2c4814961215ca
Evan Stade <estade@chromium.org>
2020-08-12 22:26:10 +0000

    WebLayer shell: turn non-URL URL bar inputs into searches.
    For example, typing "cats" searches for cats. Works from URL bar or
    command line.
    
    Bug: none
    Change-Id: Icd609ca4183398254d886d392570216eeca82d7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353059
    Auto-Submit: Evan Stade <estade@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797458}

--- weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/WebLayerShellActivity.java
+++ weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/WebLayerShellActivity.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.weblayer.shell;
 
+import static android.util.Patterns.WEB_URL;
+
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
@@ -20,7 +22,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.webkit.URLUtil;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
@@ -357,11 +358,7 @@ public class WebLayerShellActivity extends FragmentActivity {
         if (getCurrentDisplayUrl() != null) {
             return;
         }
-        String startupUrl = getUrlFromIntent(getIntent());
-        if (TextUtils.isEmpty(startupUrl) || !URLUtil.isValidUrl(startupUrl)) {
-            startupUrl = "https://google.com";
-        }
-        loadUrl(startupUrl);
+        loadUrl(getUrlFromIntent(getIntent()));
     }
 
     /* Returns the Url for the current tab as a String, or null if there is no
@@ -516,8 +513,21 @@ public class WebLayerShellActivity extends FragmentActivity {
         return fragment;
     }
 
-    public void loadUrl(String url) {
-        mBrowser.getActiveTab().getNavigationController().navigate(Uri.parse(sanitizeUrl(url)));
+    public void loadUrl(String input) {
+        String sanitized = sanitizeUrl(input);
+
+        Uri uri;
+        if (WEB_URL.matcher(sanitized).matches()) {
+            uri = Uri.parse(sanitized);
+        } else if (TextUtils.isEmpty(input)) {
+            uri = Uri.parse("https://google.com");
+        } else {
+            uri = Uri.parse("https://google.com/search")
+                          .buildUpon()
+                          .appendQueryParameter("q", input)
+                          .build();
+        }
+        mBrowser.getActiveTab().getNavigationController().navigate(uri);
     }
 
     private static String getUrlFromIntent(Intent intent) {
