077a5cd55bd32df43a0fe7a86cd38050025d6d67
Wez <wez@chromium.org>
2020-08-12 16:05:53 +0000

    [base] Fail test batches if they leak processes, under POSIX.
    Under Windows and Fuchsia the TestLauncher runs test batch processes in
    their own jobs, allowing any leaked sub-processes leaked by tests to be
    cleaned up.
    
    Under POSIX platforms each batch is run in its own process group, but the
    group was only being kill()ed if the test batch failed or crashed.
    
    TestLauncher now proactively kill()s each test batch's process group. The
    result of the call is also checked, and the test batch marked as failed
    unless kill() reports that the process-group was already gone.
    
    Note that this will still miss processes launched into new process groups
    by tests.
    
    Bug: 1094369
    Change-Id: If379d200953a823b2020766cda73f8cf27bfdb7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2346388
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797277}

--- base/test/launcher/test_launcher_unittest.cc
+++ base/test/launcher/test_launcher_unittest.cc
@@ -16,12 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/launcher/test_launcher.h"
 #include "base/test/launcher/test_launcher_test_utils.h"
 #include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/multiprocess_test.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_timeouts.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "testing/multiprocess_func_list.h"
 
 #if defined(OS_WIN)
 #include "base/win/windows_version.h"
@@ -709,12 +711,12 @@ TEST(MockUnitTests, DISABLED_FailTest) {
 TEST(MockUnitTests, DISABLED_CrashTest) {
   IMMEDIATE_CRASH();
 }
-// Basic test will not be reached with default batch size.
+// Basic test will not be reached, due to the preceding crash in the same batch.
 TEST(MockUnitTests, DISABLED_NoRunTest) {
   ASSERT_TRUE(true);
 }
 
-// Using TestLauncher to launch 3 simple unitests
+// Using TestLauncher to launch 3 basic unitests
 // and validate the resulting json file.
 TEST_F(UnitTestLauncherDelegateTester, RunMockTests) {
   CommandLine command_line(CommandLine::ForCurrentProcess()->GetProgram());
