03e5f5c3b49992122bc5342dab9ca80a80fedc4d
My Nguyen <myy@chromium.org>
2020-08-12 22:41:05 +0000

    [CrOS Settings] Add input_page.js
    Adding the first bit of the input page according to design at
    http://go/cros-lang-settings-ux slide 23.
    Current view: http://screen/K4xO3C4FXaz
    
    Bug: 1113439
    Change-Id: Ic2c5e3d1e095e5080bc2409a1c3aafe89a147fc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2345933
    Commit-Queue: My Nguyen <myy@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797463}

---
+++ chrome/browser/resources/settings/chromeos/os_languages_page/input_page.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview 'os-settings-input-page' is the input sub-page
+ * for language and input method settings.
+ */
+Polymer({
+  is: 'os-settings-input-page',
+
+  behaviors: [
+    PrefsBehavior,
+  ],
+
+  properties: {
+    /**
+     * Preferences state.
+     */
+    prefs: {
+      type: Object,
+      notify: true,
+    },
+  },
+
+  /** @private {?settings.LanguagesMetricsProxy} */
+  languagesMetricsProxy_: null,
+
+  /** @override */
+  created() {
+    this.languagesMetricsProxy_ =
+        settings.LanguagesMetricsProxyImpl.getInstance();
+  },
+
+  /**
+   * @param {!Event} e
+   * @private
+   */
+  onShowImeMenuChange_(e) {
+    this.languagesMetricsProxy_.recordToggleShowInputOptionsOnShelf(
+        e.target.checked);
+  },
+});
