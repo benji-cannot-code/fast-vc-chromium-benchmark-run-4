a3e03d2dee8b2f7b8df9ec691ac7347c7e8ac123
David Jean <djean@chromium.org>
2020-08-12 15:20:17 +0000

    [ios] Fix bottom inset issue in tab grid
    Reset insets after changing configuration during a size change.
    
    Bug: 1114134
    Change-Id: I3d32d5db03f5c2446830336d07821e325a443d22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2351833
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797260}

--- ios/chrome/browser/ui/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_view_controller.mm
@@ -200,6 +200,7 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
     [self scrollToPage:_currentPage animated:NO];
     [self configureViewControllerForCurrentSizeClassesAndPage];
     [self setInsetForRemoteTabs];
+    [self setInsetForGridViews];
   };
   [coordinator animateAlongsideTransition:animate completion:nil];
 }
