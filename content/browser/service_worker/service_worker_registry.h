6cc12d82b6973085ea5fbff5365e2c0653e68d44
Kenichi Ishibashi <bashi@chromium.org>
2020-08-12 00:26:54 +0000

    Move one PurgeResources() call from ServiceWorkerRegistry to ServiceWorkerStorage
    Resource purging logic will be moved to the Storage Service. As a
    preparation, this CL moves PurgeResources() call from
    ServiceWorkerRegistry::DidDoomUncommittedResourceIds() to
    ServiceWorkerStorage.
    
    This CL also make DoomUncommittedResources() private as it is only used
    from ServiceWorkerRegistry.
    
    Bug: 1055677
    Change-Id: If62c038ab4c272d9fa28862285cdf500f7b47ea0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2342529
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797058}

--- content/browser/service_worker/service_worker_registry.h
+++ content/browser/service_worker/service_worker_registry.h
@@ -203,7 +203,6 @@ class CONTENT_EXPORT ServiceWorkerRegistry {
                                      StatusCallback callback);
   void StoreUncommittedResourceId(int64_t resource_id, const GURL& origin);
   void DoomUncommittedResource(int64_t resource_id);
-  void DoomUncommittedResources(const std::vector<int64_t>& resource_ids);
   void GetUserData(int64_t registration_id,
                    const std::vector<std::string>& keys,
                    GetUserDataCallback callback);
@@ -270,6 +269,8 @@ class CONTENT_EXPORT ServiceWorkerRegistry {
   // what "findable" means and when a registration is returned.
   base::Optional<scoped_refptr<ServiceWorkerRegistration>>
   FindFromLiveRegistrationsForId(int64_t registration_id);
+
+  void DoomUncommittedResources(const std::vector<int64_t>& resource_ids);
 
   void DidFindRegistrationForClientUrl(
       const GURL& client_url,
