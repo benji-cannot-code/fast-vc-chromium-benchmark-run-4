50f762ee1fdd732105facc63179819358824550a
Peter Boström <pbos@chromium.org>
2020-08-12 03:43:38 +0000

    Add a model-based dialog framework
    Introduces a formal split between a dialog model and the bubble in which
    it's hosted. Refactors BookmarkBubbleView to use it.
    
    Several DCHECKs are in place and TODOs that need to be resolved while
    converting more dialogs to make this more flexible.
    
    Bug: 1106422
    Change-Id: Ied4baeaf3237f22f770562dc3161030e8640354d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292700
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797105}

--- chrome/browser/ui/views/bookmarks/bookmark_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bubble_view_browsertest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/test/bookmark_test_helpers.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "content/public/test/browser_test.h"
+#include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
 class BookmarkBubbleViewBrowserTest : public DialogBrowserTest {
  public:
