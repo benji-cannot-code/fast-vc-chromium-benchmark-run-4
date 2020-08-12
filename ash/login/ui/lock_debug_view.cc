d8eea827f625572bafbf8a78b3f5cc1e9aa23bf2
Renato Silva <rrsilva@google.com>
2020-08-12 10:05:27 +0000

    Chrome OS - Login/lock screen clean up
    Improve padding mechanism in LoginAuthUserView and
    add smart card option to login screen debug tool.
    
    Remove an extra container that used to host the password
    view and replace it with a fade animation for the
    smart card view.
    
    Bug: 1075994
    Change-Id: I102a39a327e05935b67a8f8bfec53e9271cb4285
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343110
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/master@{#797180}

--- ash/login/ui/lock_debug_view.cc
+++ ash/login/ui/lock_debug_view.cc
@@ -63,6 +63,7 @@ enum {
   kGlobalToggleManagedSessionDisclosure,
   kGlobalShowParentAccess,
   kPerUserTogglePin,
+  kPerUserToggleChallengeResponse,
   kPerUserToggleTap,
   kPerUserCycleEasyUnlockState,
   kPerUserCycleFingerprintState,
@@ -113,6 +114,7 @@ struct UserMetadata {
   std::string display_name;
   bool enable_pin = false;
   bool enable_tap_to_unlock = false;
+  bool enable_challenge_response = false;  // Smart Card
   bool enable_auth = true;
   user_manager::UserType type = user_manager::USER_TYPE_REGULAR;
   EasyUnlockIconId easy_unlock_id = EasyUnlockIconId::NONE;
@@ -270,6 +272,17 @@ class LockDebugView::DebugDataDispatcherTransformer
     debug_user->enable_pin = !debug_user->enable_pin;
     debug_dispatcher_.SetPinEnabledForUser(debug_user->account_id,
                                            debug_user->enable_pin);
+  }
+
+  // Activates or deactivates challenge response for the user at
+  // |user_index|.
+  void ToggleChallengeResponseStateForUserIndex(size_t user_index) {
+    DCHECK(user_index < debug_users_.size());
+    UserMetadata* debug_user = &debug_users_[user_index];
+    debug_user->enable_challenge_response =
+        !debug_user->enable_challenge_response;
+    debug_dispatcher_.SetChallengeResponseAuthEnabledForUser(
+        debug_user->account_id, debug_user->enable_challenge_response);
   }
 
   // Activates or deactivates tap unlock for the user at |user_index|.
@@ -1016,6 +1029,12 @@ void LockDebugView::ButtonPressed(views::Button* sender,
   if (sender->GetID() == ButtonId::kPerUserTogglePin)
     debug_data_dispatcher_->TogglePinStateForUserIndex(sender->tag());
 
+  // Enable or disable challenge response. (Smart Card)
+  if (sender->GetID() == ButtonId::kPerUserToggleChallengeResponse) {
+    debug_data_dispatcher_->ToggleChallengeResponseStateForUserIndex(
+        sender->tag());
+  }
+
   // Enable or disable tap.
   if (sender->GetID() == ButtonId::kPerUserToggleTap)
     debug_data_dispatcher_->ToggleTapStateForUserIndex(sender->tag());
@@ -1095,6 +1114,9 @@ void LockDebugView::UpdatePerUserActionContainer() {
     row->AddChildView(name);
 
     AddButton("Toggle PIN", ButtonId::kPerUserTogglePin, row)->set_tag(i);
+    AddButton("Toggle Smart card",
+              ButtonId::kPerUserToggleChallengeResponse, row)
+        ->set_tag(i);
     AddButton("Toggle Tap", ButtonId::kPerUserToggleTap, row)->set_tag(i);
     AddButton("Cycle easy unlock", ButtonId::kPerUserCycleEasyUnlockState, row)
         ->set_tag(i);
