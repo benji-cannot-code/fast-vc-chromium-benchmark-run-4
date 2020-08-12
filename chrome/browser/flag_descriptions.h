14bcde21184a3c729322923f28aa31e716069622
Thanh Nguyen <thanhdng@chromium.org>
2020-08-12 08:49:50 +0000

    Remove aggreated ml search result flag
    Remove this flag as it is expired.
    
    Bug: 1110581
    Change-Id: I6230447949feb3aeeaca92d702b1b6e2091b48b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2351479
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797162}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2002,9 +2002,6 @@ extern const char kAcceleratedMjpegDecodeDescription[];
 extern const char kAggregatedMlAppRankingName[];
 extern const char kAggregatedMlAppRankingDescription[];
 
-extern const char kAggregatedMlSearchRankingName[];
-extern const char kAggregatedMlSearchRankingDescription[];
-
 extern const char kAllowDisableMouseAccelerationName[];
 extern const char kAllowDisableMouseAccelerationDescription[];
 
