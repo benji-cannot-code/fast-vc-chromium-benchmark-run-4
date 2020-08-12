7833b45689b929dba22eb695856febeba0c3f301
Dan Harrington <harringtond@chromium.org>
2020-08-12 21:52:56 +0000

    Merging latest changes from the Feed repo up to c80d3c49fa92f00c05130106b29d5b49a735ac97
    Changes merged:
    https://chromium.googlesource.com/feed/+/c80d3c49fa92f00c05130106b29d5b49a735ac97
    
    Change-Id: I7d676b458cc1d8e32aa2db104e0e81591d7a32d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353011
    Commit-Queue: Dan H <harringtond@chromium.org>
    Auto-Submit: Dan H <harringtond@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797443}

--- chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/library/piet/DebugLoggerTest.java
+++ chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/library/piet/DebugLoggerTest.java
@@ -18,7 +18,6 @@ import android.widget.TextView;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 
@@ -35,9 +34,6 @@ public class DebugLoggerTest {
     private static final String ERROR_TEXT_1 = "Interdimensional rift formation.";
     private static final String ERROR_TEXT_2 = "Exotic particle containment breach.";
     private static final String WARNING_TEXT = "Noncompliant meson entanglement.";
-
-    @Mock
-    private FrameContext mFrameContext;
 
     private Context mContext;
 
