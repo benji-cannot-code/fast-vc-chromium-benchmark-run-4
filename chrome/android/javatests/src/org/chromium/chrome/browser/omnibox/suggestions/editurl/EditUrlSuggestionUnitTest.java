079dde09ff23c4eac59733388a0039336e6a2d0c
Michael Thiessen <mthiesse@chromium.org>
2020-08-13 00:01:25 +0000

    Clean up clipboard state in EditUrlSuggestionUnitTest
    EditUrlSuggestionUnitTest was leaking native clipboard state that was
    causing other clipboard tests to fail. Test grouping on M vs R revealed
    this dependency locally, but not on the bots.
    
    Change-Id: I98bed04aae3fa69dc82bb7a74856c37d75b6106a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353389
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797482}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/editurl/EditUrlSuggestionUnitTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/editurl/EditUrlSuggestionUnitTest.java
@@ -50,6 +50,7 @@ import org.chromium.components.search_engines.TemplateUrlService;
 import org.chromium.content_public.browser.test.NativeLibraryTestUtils;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.base.Clipboard;
+import org.chromium.ui.base.ClipboardAndroidTestSupport;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.url.GURL;
 
@@ -157,6 +158,7 @@ public final class EditUrlSuggestionUnitTest {
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             Clipboard.getInstance().overrideClipboardManagerForTesting(mOldClipboardManager);
         });
+        ClipboardAndroidTestSupport.cleanup();
     }
 
     /** Test that the suggestion is triggered. */
