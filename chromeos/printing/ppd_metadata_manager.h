5a1349fb553bca21f6bfa2e9db863078718661e5
Kalvin Lee <kdlee@chromium.org>
2020-08-13 21:57:51 +0000

    PpdProvider v3: implement using forward index
    This change implements
    PpdMetadataManager::FindAllEmmsAvailableInIndex(), which seeks out PPD
    PPD information via the forward index metadata for a given list of
    effective-make-and-model strings. This is the first method in a series
    needed to implement PpdProvider::ResolvePpdReference().
    
    Bug: chromium:888189
    Test: chromeos_unittests --gtest_filter='PpdMetadataManagerTest.*'
    Change-Id: I465d7fa6589aa519ed80709a2320d8c1ec4b0e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2333582
    Reviewed-by: Luum Habtemariam <luum@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797876}

--- chromeos/printing/ppd_metadata_manager.h
+++ chromeos/printing/ppd_metadata_manager.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/containers/flat_map.h"
+#include "base/containers/span.h"
 #include "base/optional.h"
 #include "base/strings/string_piece.h"
 #include "base/time/time.h"
@@ -37,6 +38,15 @@ class CHROMEOS_EXPORT PpdMetadataManager {
   // *  upon success, ParsedPrinters as requested.
   using GetPrintersCallback =
       base::OnceCallback<void(bool, const ParsedPrinters&)>;
+
+  // Used by FindAllEmmsAvailableInIndex().
+  // Contains a map
+  // *  whose keys are effective-make-and-model strings (provided by the
+  //    caller) that are available in forward index metadata and
+  // *  whose values contain the corresponding information read from the
+  //    forward index metadata.
+  using FindAllEmmsAvailableInIndexCallback = base::OnceCallback<void(
+      const base::flat_map<std::string, ParsedIndexValues>&)>;
 
   // Assumes ownership of |config_cache|.
   static std::unique_ptr<PpdMetadataManager> Create(
@@ -78,6 +88,17 @@ class CHROMEOS_EXPORT PpdMetadataManager {
   virtual void GetPrinters(base::StringPiece manufacturer,
                            base::TimeDelta age,
                            GetPrintersCallback cb) = 0;
+
+  // Calls |cb| with the subset of strings from |emms| that are
+  // available in forward index metadata mapped to the corresponding
+  // values read from forward index metadata.
+  // *  Does not rely on prior call to GetLocale().
+  // *  During operation, operates with metadata no older than |age|.
+  // *  On failure, calls |cb| with an empty map.
+  virtual void FindAllEmmsAvailableInIndex(
+      const std::vector<std::string>& emms,
+      base::TimeDelta age,
+      FindAllEmmsAvailableInIndexCallback cb) = 0;
 
   // Calls |cb| with the make and model of
   // |effective_make_and_model|.
