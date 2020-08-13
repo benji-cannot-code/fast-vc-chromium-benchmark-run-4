13084481d46381389c139abeaf71156889ccfa08
Matt Wolenetz <wolenetz@chromium.org>
2020-08-13 13:11:11 +0000

    MSE: DCHECK that MediaSourceRegistry |url| params are non-empty
    Improves MediaSourceRegistry code readability by DCHECKing in its
    URLRegistry overrides and in its LookupMediaSource interface that the
    KURL |url| in the former, and the wtf::String |url| in the latter are
    not empty (or null). The former is prevented by the FILE API
    PublicUrlManager already, and the latter is now (by this change)
    protected against in MediaSourceAttachment::LookupMediaSource().
    
    This is a follow-up on a review comment in an MSE-in-Worker CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/2300804/7/third_party/blink/renderer/modules/mediasource/media_source_registry_impl.cc#74
    
    BUG=878133
    
    Change-Id: I2ef93b8d3fc894ee71b1a605acdc58df88e722d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2346797
    Auto-Submit: Matthew Wolenetz <wolenetz@chromium.org>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797640}

--- third_party/blink/renderer/modules/mediasource/media_source_registry_impl.cc
+++ third_party/blink/renderer/modules/mediasource/media_source_registry_impl.cc
@@ -48,6 +48,7 @@ void MediaSourceRegistryImpl::RegisterURL(SecurityOrigin*,
                                           URLRegistrable* registrable) {
   DCHECK(IsMainThread());
   DCHECK_EQ(&registrable->Registry(), this);
+  DCHECK(!url.IsEmpty());  // Caller of interface should already enforce this.
 
   DVLOG(1) << __func__ << " url=" << url;
 
@@ -59,6 +60,7 @@ void MediaSourceRegistryImpl::RegisterURL(SecurityOrigin*,
 void MediaSourceRegistryImpl::UnregisterURL(const KURL& url) {
   DVLOG(1) << __func__ << " url=" << url;
   DCHECK(IsMainThread());
+  DCHECK(!url.IsEmpty());  // Caller of interface should already enforce this.
 
   auto iter = media_sources_.find(url.GetString());
   if (iter == media_sources_.end())
@@ -72,8 +74,8 @@ void MediaSourceRegistryImpl::UnregisterURL(const KURL& url) {
 scoped_refptr<MediaSourceAttachment> MediaSourceRegistryImpl::LookupMediaSource(
     const String& url) {
   DCHECK(IsMainThread());
-  return url.IsNull() ? scoped_refptr<MediaSourceAttachment>()
-                      : media_sources_.at(url);
+  DCHECK(!url.IsEmpty());
+  return media_sources_.at(url);
 }
 
 MediaSourceRegistryImpl::MediaSourceRegistryImpl() {
