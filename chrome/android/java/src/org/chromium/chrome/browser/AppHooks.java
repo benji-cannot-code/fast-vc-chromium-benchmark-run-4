0dfeec449c7d7e32c2ec3e534d275cca07e22d36
Justin DeWitt <dewittj@chromium.org>
2020-08-13 01:18:37 +0000

    Remove SurfaceDependencyProvider.
    No functionality changes, this is just a multi-sided rename.
    CL chain:
    - https://chromium-review.googlesource.com/c/chromium/src/+/2261633
    - https://chromium-review.googlesource.com/c/chromium/src/+/2285471
    - cl/320246139
    - https://chrome-internal-review.googlesource.com/c/clank/internal/apps/+/3167007
    - [this CL]
    
    
    Change-Id: I0430d16ad79ea1d43bcdc63dbc5795ef7c1f3d64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2284097
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797518}

--- chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
+++ chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
@@ -43,7 +43,6 @@ import org.chromium.chrome.browser.usage_stats.DigitalWellbeingClient;
 import org.chromium.chrome.browser.webapps.GooglePlayWebApkInstallDelegate;
 import org.chromium.chrome.browser.xsurface.ProcessScope;
 import org.chromium.chrome.browser.xsurface.ProcessScopeDependencyProvider;
-import org.chromium.chrome.browser.xsurface.SurfaceDependencyProvider;
 import org.chromium.components.browser_ui.widget.FeatureHighlightProvider;
 import org.chromium.components.external_intents.AuthenticatorNavigationInterceptor;
 import org.chromium.components.signin.AccountManagerDelegate;
@@ -348,19 +347,6 @@ public abstract class AppHooks {
     public @Nullable ProcessScope getExternalSurfaceProcessScope(
             ProcessScopeDependencyProvider dependencies) {
         return null;
-    }
-
-    /**
-     * Returns a new {@link SurfaceRenderer} if the xsurface implementation is included in the
-     * apk. Otherwise null is returned.
-     *
-     * This API is deprecated, and will be removed after safely renaming SurfaceDependencyProvider
-     * to ProcessScopeDependencyProvider.
-     */
-    @Deprecated
-    public @Nullable ProcessScope getExternalSurfaceProcessScope(
-            SurfaceDependencyProvider dependencies) {
-        return getExternalSurfaceProcessScope((ProcessScopeDependencyProvider) dependencies);
     }
 
     /**
