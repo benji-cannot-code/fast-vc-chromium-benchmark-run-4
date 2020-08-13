32995c1dcd4f4cbce4e278c6e4fcf909cd1e3567
Anders Hartvoll Ruud <andruud@chromium.org>
2020-08-13 09:10:57 +0000

    [scroll-animations] Don't store a CSSPropertyValueSet on the style rule
    Instead of storing a CSSPropertyValueSet, and then calling
    GetPropertyCSSValue on-demand, dig out all the CSSValues during
    construction of the StyleRuleScrollTimeline object.
    
    The reason for this is somewhat dull: if a unit test disables the
    CSSScrollTimeline runtime flag, and *then* tries to access some
    descriptor using GetPropertyCSSValue, we hit a DCHECK because the
    descriptor is (no longer) web-exposed.
    
    It would have been possible to avoid accessing the StyleRuleScroll-
    Timeline while the flag is disabled some other way (per test), but
    it's easier to just allow it in general.
    
    Added a bonus unit test for Copy(), since this CL changes how that
    works.
    
    Bug: 1074052
    Change-Id: Id59cac2d1e81ea07f1d63a8e1bcdbc10ac55a122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2351921
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797602}

--- third_party/blink/renderer/core/css/css_test_helpers.h
+++ third_party/blink/renderer/core/css/css_test_helpers.h
@@ -67,6 +67,7 @@ const CSSValue* ParseLonghand(Document& document,
 const CSSPropertyValueSet* ParseDeclarationBlock(
     const String& block_text,
     CSSParserMode mode = kHTMLStandardMode);
+StyleRuleBase* ParseRule(Document& document, String text);
 
 }  // namespace css_test_helpers
 }  // namespace blink
