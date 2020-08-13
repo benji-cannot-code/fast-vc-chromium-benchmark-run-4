0a8ca833a5ec9884e07a20503c7d2cee4da54ac7
Monica Basta <msalama@chromium.org>
2020-08-13 15:23:57 +0000

    [ProfilePicker]: Add policy helper.
    This CL adds support for device policies and adjusts UI accordingly.
    
    Bug: 1063856
    Change-Id: Ibccfc34f6fdf588f704e8a7fe8c5818dfb2c0f25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352190
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797675}

--- chrome/browser/ui/webui/signin/profile_picker_ui.cc
+++ chrome/browser/ui/webui/signin/profile_picker_ui.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/signin_util.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/webui/signin/profile_picker_handler.h"
 #include "chrome/browser/ui/webui/webui_util.h"
@@ -23,6 +24,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/webui/web_ui_util.h"
 
 namespace {
+bool IsProfileCreationAllowed() {
+  PrefService* service = g_browser_process->local_state();
+  DCHECK(service);
+  return service->GetBoolean(prefs::kBrowserAddPersonEnabled);
+}
+
+bool IsGuestModeEnabled() {
+  PrefService* service = g_browser_process->local_state();
+  DCHECK(service);
+  return service->GetBoolean(prefs::kBrowserGuestModeEnabled);
+}
 
 void AddStrings(content::WebUIDataSource* html_source) {
   static constexpr webui::LocalizedString kLocalizedStrings[] = {
@@ -61,8 +73,16 @@ void AddStrings(content::WebUIDataSource* html_source) {
                           g_browser_process->local_state()->GetBoolean(
                               prefs::kBrowserShowProfilePickerOnStartup));
   html_source->AddBoolean(
-      "signInProfileCreationFlow",
+      "signInProfileCreationFlowSupported",
       base::FeatureList::IsEnabled(features::kSignInProfileCreationFlow));
+
+  // Add policies.
+  html_source->AddBoolean("isForceSigninEnabled",
+                          signin_util::IsForceSigninEnabled());
+  html_source->AddBoolean("isGuestModeEnabled", IsGuestModeEnabled());
+  html_source->AddBoolean("isProfileCreationAllowed",
+                          IsProfileCreationAllowed());
+  // TODO(crbug.com/1063856): Check if |BrowserSignin| device policy exists.
 }
 
 }  // namespace
