f2152b9ee39fa9e177ed90ed674b560c07eeae37
Owen Min <zmin@chromium.org>
2020-08-13 18:54:54 +0000

    Add a command line policy loader
    It will create an easy workaround for developers when testing policy
    on platforms that have need complicated policy management tools.
    
    Bug: 1113792
    Change-Id: Ie0431022a3aba1d5356afb32a9987a91d6ba7929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353397
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797781}

---
+++ components/policy/core/common/policy_loader_command_line.h
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_POLICY_CORE_COMMON_POLICY_LOADER_COMMAND_LINE_H_
+#define COMPONENTS_POLICY_CORE_COMMON_POLICY_LOADER_COMMAND_LINE_H_
+
+#include "base/command_line.h"
+#include "base/memory/ref_counted.h"
+#include "components/policy/core/common/async_policy_loader.h"
+#include "components/policy/policy_export.h"
+
+namespace base {
+class SequencedTaskRunner;
+}
+
+namespace policy {
+
+// Loads policy value from command line switch for development and testing
+// purposes. It can be only used with strict limitation. For example, on
+// Android, the device must be rooted.
+class POLICY_EXPORT PolicyLoaderCommandLine : public AsyncPolicyLoader {
+ public:
+  PolicyLoaderCommandLine(scoped_refptr<base::SequencedTaskRunner> task_runner,
+                          const base::CommandLine& command_line);
+  PolicyLoaderCommandLine(const PolicyLoaderCommandLine&) = delete;
+  PolicyLoaderCommandLine& operator=(const PolicyLoaderCommandLine&) = delete;
+
+  ~PolicyLoaderCommandLine() override;
+
+  // AsumcPolicyLoader implementation.
+  void InitOnBackgroundThread() override;
+  std::unique_ptr<PolicyBundle> Load() override;
+
+ private:
+  const base::CommandLine command_line_;
+};
+
+}  // namespace policy
+
+#endif  // COMPONENTS_POLICY_CORE_COMMON_POLICY_LOADER_COMMAND_LINE_H_
