6e72cdae3b5385cc1875fec3a2f1723d8b6c2287
Rouslan Solomakhin <rouslan@chromium.org>
2020-08-13 19:33:44 +0000

    [Web Payment][Chrome OS] Android app communication test support.
    This patch adds a helper for setting up the environment necessary to
    unit test communication with Android apps. At this time, only the
    Android subsystem on Chrome OS has a concrete implementation.
    
    Bug: 1061503
    Change-Id: I6900aa13cd111d14adeaab6a0116a623c4ace817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250351
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797802}

---
+++ components/payments/content/android_app_communication_unittest.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/payments/content/android_app_communication_test_support.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace payments {
+namespace {
+
+TEST(AndroidAppCommunicationTest, SmokeTest) {
+  auto support = AndroidAppCommunicationTestSupport::Create();
+  auto scoped_initialization = support->CreateScopedInitialization();
+  support->ExpectNoListOfPaymentAppsQuery();
+  support->ExpectNoIsReadyToPayQuery();
+  support->ExpectNoPaymentAppInvoke();
+}
+
+}  // namespace
+}  // namespace payments
---
+++ components/payments/core/android_app_description.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/payments/core/android_app_description.h"
+
+namespace payments {
+
+AndroidActivityDescription::AndroidActivityDescription() = default;
+
+AndroidActivityDescription::~AndroidActivityDescription() = default;
+
+AndroidAppDescription::AndroidAppDescription() = default;
+
+AndroidAppDescription::~AndroidAppDescription() = default;
+
+}  // namespace payments
