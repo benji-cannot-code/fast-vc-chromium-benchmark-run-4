ed0f5b955a31addd41dedfe0a3c4ff27b135870a
Yoichi Osato <yoichio@chromium.org>
2020-08-13 09:08:52 +0000

    Convert base::Bind and base::Callback in components/nacl/common to Once/Repeaing
    Bug: 1007703
    Change-Id: I39d64e4e5814b57dfa69b54fd0ab70e2edfbcd75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340410
    Reviewed-by: Mark Seaborn <mseaborn@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797601}

--- components/nacl/browser/nacl_process_host.cc
+++ components/nacl/browser/nacl_process_host.cc
@@ -1138,8 +1138,9 @@ bool NaClProcessHost::AttachDebugExceptionHandler(const std::string& info,
   }
   NaClStartDebugExceptionHandlerThread(
       std::move(process), info, base::ThreadTaskRunnerHandle::Get(),
-      base::Bind(&NaClProcessHost::OnDebugExceptionHandlerLaunchedByBroker,
-                 weak_factory_.GetWeakPtr()));
+      base::BindRepeating(
+          &NaClProcessHost::OnDebugExceptionHandlerLaunchedByBroker,
+          weak_factory_.GetWeakPtr()));
   return true;
 }
 #endif
