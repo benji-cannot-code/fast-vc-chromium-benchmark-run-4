49a868579ff4f4c4bbf3c5ad00e6ff1e61166ba6
Moe Ahmadi <mahmadi@chromium.org>
2020-08-13 23:18:15 +0000

    [omnibox] Enables zero-prefix suggestions in Desktop NTP by default
    - Enables zero-prefix suggestion in Desktop NTP (Omnibox and Realbox)
      and modifies the tests according to the new expectation.
    - Removes already enabled features and related fieldtrial params from
      fieldtrial_testing_config.json
    
    Bug: 1075724
    Change-Id: I7e63dc5d6532fe8baf83fe358f47c00fa61729d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2175138
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797904}

--- components/omnibox/browser/local_history_zero_suggest_provider.cc
+++ components/omnibox/browser/local_history_zero_suggest_provider.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/trace_event/trace_event.h"
+#include "build/build_config.h"
 #include "components/google/core/common/google_util.h"
 #include "components/history/core/browser/history_database.h"
 #include "components/history/core/browser/history_service.h"
@@ -63,7 +64,12 @@ bool AllowLocalHistoryZeroSuggestSuggestions(const AutocompleteInput& input) {
 #else
   if (!base::FeatureList::IsEnabled(omnibox::kNewSearchFeatures))
     return false;
+#endif
 
+#if !defined(OS_IOS)  // Enabled by default on Desktop if not disabled by
+                      // kNewSearchFeatures.
+  return true;
+#else
   const auto current_page_classification = input.current_page_classification();
   // Reactive Zero-Prefix Suggestions (rZPS) and basically all remote ZPS on the
   // NTP are expected to be displayed alongside local history zero-prefix
