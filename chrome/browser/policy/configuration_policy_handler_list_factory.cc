da4e9e61ed005f78d17a9cbee132073de3dd2232
Brian Malcolm <bmalcolm@chromium.org>
2020-08-13 04:02:03 +0000

    Add new PrintersBulkConfiguration policy
    This is intended to replace the NativePrintersBulkConfiguration policy.
    The old policy will still be used in the absence of the new one for at
    least a few releases of Chrome.
    
    BUG=chromium:1113366
    
    Change-Id: If7fb1a53180874a5bd12f081129aed5ffadad443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340741
    Commit-Queue: Brian Malcolm <bmalcolm@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797565}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1720,7 +1720,7 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
   handlers->AddHandler(
       std::make_unique<ExternalDataPolicyHandler>(key::kWallpaperImage));
   handlers->AddHandler(std::make_unique<ExternalDataPolicyHandler>(
-      key::kNativePrintersBulkConfiguration));
+      key::kPrintersBulkConfiguration));
   handlers->AddHandler(
       std::make_unique<ExternalDataPolicyHandler>(key::kExternalPrintServers));
   handlers->AddHandler(std::make_unique<ExternalDataPolicyHandler>(
