5ea9609f61aa5b1be4b805805a8ac66bf3003d4e
harrisonsean <harrisonsean@chromium.org>
2020-08-13 16:31:05 +0000

    [iOS][Safety Check] Icon Assets
    Add remaining icon assets needed for safety check
    
    Apply the new and existing icon assets to safety check
    
    Bug: 1078782
    Change-Id: I769bd3903edd31288da4a1e77b09a0d6e50775c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2350420
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797713}

--- ios/chrome/browser/ui/badges/badge_button_factory.mm
+++ ios/chrome/browser/ui/badges/badge_button_factory.mm
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BadgeButton*)passwordsSaveBadgeButton {
   BadgeButton* button =
       [self createButtonForType:BadgeType::kBadgeTypePasswordSave
-                     imageNamed:@"infobar_passwords_icon"
+                     imageNamed:@"password_key"
                   renderingMode:UIImageRenderingModeAlwaysTemplate];
   [button addTarget:self.delegate
                 action:@selector(passwordsBadgeButtonTapped:)
@@ -63,7 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BadgeButton*)passwordsUpdateBadgeButton {
   BadgeButton* button =
       [self createButtonForType:BadgeType::kBadgeTypePasswordUpdate
-                     imageNamed:@"infobar_passwords_icon"
+                     imageNamed:@"password_key"
                   renderingMode:UIImageRenderingModeAlwaysTemplate];
   [button addTarget:self.delegate
                 action:@selector(passwordsBadgeButtonTapped:)
--- ios/chrome/browser/ui/infobars/coordinators/infobar_password_coordinator.mm
+++ ios/chrome/browser/ui/infobars/coordinators/infobar_password_coordinator.mm
@@ -87,8 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.bannerViewController.buttonText =
         base::SysUTF16ToNSString(self.passwordInfoBarDelegate->GetButtonLabel(
             ConfirmInfoBarDelegate::BUTTON_OK));
-    self.bannerViewController.iconImage =
-        [UIImage imageNamed:@"infobar_passwords_icon"];
+    self.bannerViewController.iconImage = [UIImage imageNamed:@"password_key"];
     NSString* hiddenPasswordText =
         l10n_util::GetNSString(IDS_IOS_SETTINGS_PASSWORD_HIDDEN_LABEL);
     [self.bannerViewController
