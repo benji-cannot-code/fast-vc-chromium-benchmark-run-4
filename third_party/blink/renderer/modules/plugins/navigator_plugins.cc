5685c48dd5e1115450d302484a12d005e504057b
Dylan Cutler <dylancutler@google.com>
2020-08-13 13:15:20 +0000

    Gate navigator.plugins instrumentation on identifiability study participation.
    Bug: 973801
    Change-Id: Ic5e2c7f2118c10dc2b277ad032a282f59741e6f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353110
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/master@{#797641}

--- third_party/blink/renderer/modules/plugins/navigator_plugins.cc
+++ third_party/blink/renderer/modules/plugins/navigator_plugins.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/plugins/navigator_plugins.h"
 
 #include "third_party/blink/public/common/privacy_budget/identifiability_metric_builder.h"
+#include "third_party/blink/public/common/privacy_budget/identifiability_study_settings.h"
 #include "third_party/blink/public/common/privacy_budget/identifiable_token_builder.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/navigator.h"
@@ -57,7 +58,7 @@ DOMPluginArray* NavigatorPlugins::plugins(LocalFrame* frame) const {
     plugins_ = MakeGarbageCollected<DOMPluginArray>(frame);
 
   DOMPluginArray* result = plugins_.Get();
-  if (!frame)
+  if (!IdentifiabilityStudySettings::Get()->IsActive() || !frame)
     return result;
   Document* document = frame->GetDocument();
   if (!document)
