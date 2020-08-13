70cf78832b3a4ff3f356eca1f6fcfb029d86277c
sczs <sczs@chromium.org>
2020-08-13 16:32:36 +0000

    [ios] Disables Legacy NTP Feed Tests if Discover is enabled.
    Bug: 1085419
    Change-Id: I1bcc74618fa1f0716643f0979ac1f27a7b2d627c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352429
    Auto-Submit: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797714}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_app_interface.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_constants.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
 #import "ios/chrome/browser/ui/content_suggestions/ntp_home_constant.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
@@ -142,6 +143,9 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
 // Tests that the additional items (when more is pressed) are kept when
 // switching tabs.
 - (void)testAdditionalItemsKept {
+  if (IsDiscoverFeedEnabled()) {
+    EARL_GREY_TEST_DISABLED(@"Legacy Feed Test.");
+  }
   // Set server up.
   self.testServer->RegisterRequestHandler(base::Bind(&StandardResponse));
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
@@ -176,6 +180,9 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
 // Tests that when the page is reloaded using the tools menu, the suggestions
 // are updated.
 - (void)testReloadPage {
+  if (IsDiscoverFeedEnabled()) {
+    EARL_GREY_TEST_DISABLED(@"Legacy Feed Test.");
+  }
   // Add 2 suggestions, persisted accross page loads.
   [ContentSuggestionsAppInterface addNumberOfSuggestions:2
                                 additionalSuggestionsURL:nil];
@@ -201,6 +208,9 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
 // disposition of the collection takes into account the previous scroll, even
 // when more is tapped.
 - (void)testOpenPageAndGoBackWithMoreContent {
+  if (IsDiscoverFeedEnabled()) {
+    EARL_GREY_TEST_DISABLED(@"Legacy Feed Test.");
+  }
   // Set server up.
   self.testServer->RegisterRequestHandler(base::Bind(&StandardResponse));
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
@@ -249,6 +259,9 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
 // Tests that the "Learn More" cell is present only if there is a suggestion in
 // the section.
 - (void)testLearnMore {
+  if (IsDiscoverFeedEnabled()) {
+    EARL_GREY_TEST_DISABLED(@"Legacy Feed Test.");
+  }
   id<GREYAction> action =
       grey_scrollInDirectionWithStartPoint(kGREYDirectionDown, 200, 0.5, 0.5);
   [[[EarlGrey
