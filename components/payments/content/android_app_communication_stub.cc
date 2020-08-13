cd100bb89e6bfcc49649ed184fd15bddd8a746aa
Rouslan Solomakhin <rouslan@chromium.org>
2020-08-13 22:14:48 +0000

    [Web Payment][Chrome OS] Android app communication - GetAppDescriptions.
    Before this patch, Android payment app implementation lived exclusively
    in Java, so sharing any of its logic across platforms was difficult.
    
    This patch adds a cross-platform way for Web Payment feature to query a
    list of installed Android apps. As a first step, only information about
    Trusted Web Activities installed in the Android subsystem of Chrome OS
    would be returned.
    
    After this patch, it's possible to start creating a cross-platform
    implementation of an Android payment app.
    
    Bug: 1061503
    Change-Id: Ic8320a902eec537d5aa37b3a6cf9b79b8c579c55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2242639
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797883}

---
+++ components/payments/content/android_app_communication_stub.cc
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/payments/content/android_app_communication.h"
+
+#include <utility>
+
+#include "base/callback.h"
+#include "base/optional.h"
+
+namespace payments {
+namespace {
+
+class AndroidAppCommunicationStub : public AndroidAppCommunication {
+ public:
+  explicit AndroidAppCommunicationStub(content::BrowserContext* context)
+      : AndroidAppCommunication(context) {}
+
+  ~AndroidAppCommunicationStub() override = default;
+
+  // AndroidAppCommunication implementation.
+  void GetAppDescriptions(const std::string& twa_package_name,
+                          GetAppDescriptionsCallback callback) override {
+    std::move(callback).Run(/*error_message=*/base::nullopt,
+                            /*app_descriptions=*/{});
+  }
+
+  // AndroidAppCommunication implementation.
+  void SetForTesting() override {}
+};
+
+}  // namespace
+
+// Declared in cross-platform header file. See:
+// components/payments/content/android_app_communication.h
+// static
+std::unique_ptr<AndroidAppCommunication> AndroidAppCommunication::Create(
+    content::BrowserContext* context) {
+  return std::make_unique<AndroidAppCommunicationStub>(context);
+}
+
+}  // namespace payments
