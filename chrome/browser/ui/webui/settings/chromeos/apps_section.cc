fb89d35a52f8fcf12424469dd12122d29591af58
Jimmy Gong <jimmyxgong@chromium.org>
2020-08-13 19:08:07 +0000

    Add settings search tags for android preferences
    - Search tag now includes both "Android settings" and
      "Android preferences" to link to the android settings
      subpage.
    - Set "Android preferences" as the primary search tag as it
      matches the text of the link to android preferences app.
    
    Bug: 1115330
    Change-Id: Id0dd5b40c2fab9e4a9a41f1d67357475a0a78817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353675
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797788}

--- chrome/browser/ui/webui/settings/chromeos/apps_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/apps_section.cc
@@ -77,7 +77,9 @@ const std::vector<SearchConcept>& GetAndroidSettingsSearchConcepts() {
        mojom::SearchResultIcon::kGooglePlay,
        mojom::SearchResultDefaultRank::kMedium,
        mojom::SearchResultType::kSetting,
-       {.setting = mojom::Setting::kManageAndroidPreferences}},
+       {.setting = mojom::Setting::kManageAndroidPreferences},
+       {IDS_OS_SETTINGS_TAG_ANDROID_SETTINGS_WITH_PLAY_STORE_ALT1,
+        SearchConcept::kAltTagEnd}},
   });
   return *tags;
 }
@@ -89,7 +91,8 @@ const std::vector<SearchConcept>& GetAndroidNoPlayStoreSearchConcepts() {
        mojom::SearchResultIcon::kAndroid,
        mojom::SearchResultDefaultRank::kMedium,
        mojom::SearchResultType::kSetting,
-       {.setting = mojom::Setting::kManageAndroidPreferences}},
+       {.setting = mojom::Setting::kManageAndroidPreferences},
+       {IDS_OS_SETTINGS_TAG_ANDROID_SETTINGS_ALT1, SearchConcept::kAltTagEnd}},
   });
   return *tags;
 }
