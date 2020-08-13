80f8fb2fa483ef185b1f7b91a39a808d839af314
Adithya Srinivasan <adithyas@chromium.org>
2020-08-13 17:17:25 +0000

    Portals: Make breakpoints inside portalactivate event handlers work
    Currently, we reset the DevTools session before portal activation and
    create a new session with the activated portal's WebContents. We dispatch
    the portalactivate event and run any event handlers in the interim while
    there are no agents active, and as a result, existing breakpoints do not
    work.
    
    This CL waits for DevTools to point at the portal WebContents before
    actually swapping the WebContents, thus making sure that the
    portalactivate is dispatched after the main target has been reattached.
    
    Bug: 1025761
    Change-Id: I930d5f7ec361cd594c60bcece0bd9450a94994e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2295080
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797727}

---
+++ third_party/blink/web_tests/http/tests/devtools/portals/portals-sources-activate.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+(async function () {
+  TestRunner.addResult(`Checks that breakpoint set inside a portalactivate event handler is hit on activation`);
+  await TestRunner.loadModule('sources_test_runner');
+  await TestRunner.showPanel('sources');
+
+  await TestRunner.navigatePromise('resources/append-predecessor-host.html');
+
+  await SourcesTestRunner.startDebuggerTestPromise();
+  const sourceFrame = await SourcesTestRunner.showScriptSourcePromise('append-predecessor.html');
+  await SourcesTestRunner.toggleBreakpoint(sourceFrame, 4);
+  TestRunner.evaluateInPage(`setTimeout(() => document.querySelector('portal').activate());`);
+  const callFrames = await SourcesTestRunner.waitUntilPausedPromise();
+  await SourcesTestRunner.captureStackTrace(callFrames);
+  SourcesTestRunner.completeDebuggerTest();
+})();
