b46ecd48b9ef61ea93608c087df7745e55c5dc24
Peter Kasting <pkasting@chromium.org>
2020-08-13 00:08:16 +0000

    Try to improve comments and temp/param names.
    Also makes one or two functions const.
    
    Bug: none
    Change-Id: I9a67f5f5839aef92468342c0dfcba36da864d4b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2348195
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797488}

--- chrome/browser/ui/views/download/download_item_view.h
+++ chrome/browser/ui/views/download/download_item_view.h
@@ -106,10 +106,9 @@ class DownloadItemView : public views::View,
   const DownloadUIModel* model() const { return model_.get(); }
 
   // Submits download to download feedback service if the user has approved and
-  // the download is suitable for submission, then applies |download_command|.
+  // the download is suitable for submission, then applies |command|.
   // If user hasn't seen SBER opt-in text before, show SBER opt-in dialog first.
-  void MaybeSubmitDownloadToFeedbackService(
-      DownloadCommands::Command download_command);
+  void MaybeSubmitDownloadToFeedbackService(DownloadCommands::Command command);
 
  protected:
   // views::View:
@@ -171,7 +170,7 @@ class DownloadItemView : public views::View,
   // buttons are given the same size).
   gfx::Size GetButtonSize() const;
 
-  // Returns the file name to report to user. It might be elided to fit into
+  // Returns the file name to report to the user. It might be elided to fit into
   // the text width. |label| dictates the default text style.
   base::string16 ElidedFilename(const views::StyledLabel& label) const;
 
@@ -203,10 +202,9 @@ class DownloadItemView : public views::View,
   void OpenDownloadDuringAsyncScanning();
 
   // Submits the downloaded file to the safebrowsing download feedback service.
-  // Applies |download_command| if submission succeeds. Returns whether
-  // submission was successful.
-  bool SubmitDownloadToFeedbackService(
-      DownloadCommands::Command download_command);
+  // Applies |command| if submission succeeds. Returns whether submission was
+  // successful.
+  bool SubmitDownloadToFeedbackService(DownloadCommands::Command command) const;
 
   // Forwards |command| to |commands_|; useful for callbacks.
   void ExecuteCommand(DownloadCommands::Command command);
