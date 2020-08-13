d96de12224ef256f1da7bbd9d317421085f98526
Kuo Jen Wei <inker@google.com>
2020-08-13 01:13:16 +0000

    CCA: Check external monitor with screen backlight off
    Add check of no external monitor plugged in with screen backlight forced
    off event to judge whether to suspend camera stream.
    
    Bug: 1114967
    Test: Pluggin both external camera and external monitor, check CCA's
    preview is still on after closing lid of device.
    
    Change-Id: Ia07e1efe9d21814521a62a4ad1c63324221b991d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2351579
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Kuo Jen Wei <inker@chromium.org>
    Auto-Submit: Kuo Jen Wei <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797514}

--- chromeos/components/camera_app_ui/resources/src/js/views/camera.js
+++ chromeos/components/camera_app_ui/resources/src/js/views/camera.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {browserProxy} from '../browser_proxy/browser_proxy.js';
-import {assert, assertInstanceof} from '../chrome_util.js';
+import {assert, assertInstanceof, promisify} from '../chrome_util.js';
 import {
   PhotoConstraintsPreferrer,  // eslint-disable-line no-unused-vars
   VideoConstraintsPreferrer,  // eslint-disable-line no-unused-vars
@@ -258,10 +258,6 @@ export class Camera extends View {
         this.start();
       }
     });
-
-    state.addObserver(state.State.SCREEN_OFF_AUTO, () => this.start());
-
-    this.configuring_ = null;
   }
 
   /**
@@ -281,6 +277,34 @@ export class Camera extends View {
     };
     const screenState = await helper.initScreenStateMonitor(setScreenOffAuto);
     setScreenOffAuto(screenState);
+
+    const updateExternalScreen = async () => {
+      const allInfo = await promisify(chrome.system.display.getInfo)(
+          {singleUnified: false});
+      const hasExternalScreen = allInfo.some(({isInternal}) => !isInternal);
+      state.set(state.State.HAS_EXTERNAL_SCREEN, hasExternalScreen);
+    };
+    await updateExternalScreen();
+    chrome.system.display.onDisplayChanged.addListener(updateExternalScreen);
+
+    const checkScreenOff = () => {
+      if (this.screenOff_) {
+        this.start();
+      }
+    };
+
+    state.addObserver(state.State.SCREEN_OFF_AUTO, checkScreenOff);
+    state.addObserver(state.State.HAS_EXTERNAL_SCREEN, checkScreenOff);
+  }
+
+  /**
+   * @return {boolean} If the App window is invisible to user with respect to
+   * screen off state.
+   * @private
+   */
+  get screenOff_() {
+    return state.get(state.State.SCREEN_OFF_AUTO) &&
+        !state.get(state.State.HAS_EXTERNAL_SCREEN);
   }
 
   /**
@@ -306,8 +330,8 @@ export class Camera extends View {
    */
   isSuspended() {
     return this.locked_ || browserProxy.isMinimized() ||
-        state.get(state.State.SUSPEND) ||
-        state.get(state.State.SCREEN_OFF_AUTO) || this.isTabletBackground_();
+        state.get(state.State.SUSPEND) || this.screenOff_ ||
+        this.isTabletBackground_();
   }
 
   /**
