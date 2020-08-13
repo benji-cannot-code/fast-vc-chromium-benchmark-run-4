788fa277307793731274b0c309814b105f93d5de
Yu-Hsuan Hsu <yuhsuan@chromium.org>
2020-08-13 18:42:01 +0000

    Pass switch --use-cras to new process
    We need to pass switch --use-cras to the audio service so that chrome
    will be able to use CRAS on linux.
    
    Bug: 151905045
    Change-Id: I7c8acf0f3c976ba40bf492a85a53c56a3254fcfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332057
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797769}

--- content/browser/utility_process_host.cc
+++ content/browser/utility_process_host.cc
@@ -275,6 +275,9 @@ bool UtilityProcessHost::StartProcess() {
       switches::kAlsaInputDevice,
       switches::kAlsaOutputDevice,
 #endif
+#if defined(USE_CRAS)
+      switches::kUseCras,
+#endif
 #if defined(OS_WIN)
       switches::kDisableHighResTimer,
       switches::kEnableExclusiveAudio,
