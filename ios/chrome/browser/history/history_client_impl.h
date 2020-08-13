e9eaed20dc81c1e320780c5fef301016a961c216
Peter Kasting <pkasting@chromium.org>
2020-08-13 18:43:30 +0000

    Add HistoryService::FaviconChangedCallback type alias.
    This makes it easier to track down and/or change types.
    
    Also renames CallbackList -> RepeatingCallbackList.
    
    Bug: 1113007
    Change-Id: I42357489f2c3e7a19b0f4909fe20ca7dc9ff4339
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354225
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797771}

--- ios/chrome/browser/history/history_client_impl.h
+++ ios/chrome/browser/history/history_client_impl.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "components/bookmarks/browser/base_bookmark_model_observer.h"
 #include "components/history/core/browser/history_client.h"
+#include "components/history/core/browser/history_service.h"
 
 class GURL;
 
@@ -59,8 +60,8 @@ class HistoryClientImpl : public history::HistoryClient,
   base::Callback<void(const std::set<GURL>&)> on_bookmarks_removed_;
 
   // Subscription for notifications of changes to favicons.
-  std::unique_ptr<base::CallbackList<void(const std::set<GURL>&,
-                                          const GURL&)>::Subscription>
+  std::unique_ptr<
+      history::HistoryService::FaviconsChangedCallbackList::Subscription>
       favicons_changed_subscription_;
 
   DISALLOW_COPY_AND_ASSIGN(HistoryClientImpl);
