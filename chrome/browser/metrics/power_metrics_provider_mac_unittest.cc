f0ca5bfefe27d0f07e704f8efd42a48b5670e271
Peter Kasting <pkasting@chromium.org>
2020-08-13 22:49:11 +0000

    Use TimeDelta::operator/() more, chrome/ edition.
    Bug: 1104532
    Change-Id: I5a1ba068f5ed6bbf16e98339d534f5f083c5d058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2355010
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#797892}

--- chrome/browser/metrics/power_metrics_provider_mac_unittest.cc
+++ chrome/browser/metrics/power_metrics_provider_mac_unittest.cc
@@ -142,8 +142,8 @@ TEST_F(PowerMetricsProviderTest, BatteryDischargeCaptureIsEarly) {
   // |kMetricsCollectionInterval|.
   int discharge =
       base::ClampFloor((kFirstReading - kSecondReading) *
-                       kMetricsCollectionInterval.InSecondsF() /
-                       (second_capture_time - first_capture_time).InSeconds());
+                       (kMetricsCollectionInterval /
+                        (second_capture_time - first_capture_time)));
   histogram_tester_.ExpectUniqueSample(kHistogramName, discharge, 1);
 }
 
@@ -192,7 +192,7 @@ TEST_F(PowerMetricsProviderTest, BatteryDischargeCaptureIsLate) {
   // |kMetricsCollectionInterval|.
   int discharge =
       base::ClampFloor((kFirstReading - kSecondReading) *
-                       kMetricsCollectionInterval.InSecondsF() /
-                       (second_capture_time - first_capture_time).InSeconds());
+                       (kMetricsCollectionInterval /
+                        (second_capture_time - first_capture_time)));
   histogram_tester_.ExpectUniqueSample(kHistogramName, discharge, 1);
 }
