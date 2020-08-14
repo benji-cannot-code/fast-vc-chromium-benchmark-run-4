8072a2fe5c94281b1a52c1274963dae90835d494
Robert Flack <flackr@chromium.org>
2020-08-14 21:00:48 +0000

    Perform a microtask checkpoint after updating timelines.
    Updating timelines can enqueue tasks related to ready and finish
    promises which should run before animation events and updated
    rendering.
    
    Bug: 1116020
    Change-Id: Ica1927bb3921eef574abeaca23bb198dda658ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354164
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798259}

--- third_party/blink/renderer/core/animation/document_animations.cc
+++ third_party/blink/renderer/core/animation/document_animations.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/page/page_animator.h"
+#include "third_party/blink/renderer/platform/bindings/microtask.h"
 
 namespace blink {
 
@@ -74,6 +75,14 @@ void DocumentAnimations::AddTimeline(AnimationTimeline& timeline) {
 
 void DocumentAnimations::UpdateAnimationTimingForAnimationFrame() {
   UpdateAnimationTiming(*document_, timelines_, kTimingUpdateForAnimationFrame);
+
+  // Perform a microtask checkpoint per step 3 of
+  // https://drafts.csswg.org/web-animations-1/#timelines. This is to
+  // ensure that any microtasks queued up as a result of resolving or
+  // rejecting Promise objects as part of updating timelines run their
+  // callbacks prior to dispatching animation events and generating
+  // the next main frame.
+  Microtask::PerformCheckpoint(V8PerIsolateData::MainThreadIsolate());
 }
 
 bool DocumentAnimations::NeedsAnimationTimingUpdate() {
