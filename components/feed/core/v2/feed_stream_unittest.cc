07e12d81646cdc99edba8065447e41e92d01da90
Dan Harrington <harringtond@chromium.org>
2020-08-14 21:14:56 +0000

    Record UMA for NoticeCardFulfilled
    Bug: b/162435720
    Change-Id: If23e4026e65ea28c6f78c7064dc05bb6c7cfcf86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354417
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798269}

--- components/feed/core/v2/feed_stream_unittest.cc
+++ components/feed/core/v2/feed_stream_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/test/bind_test_util.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_run_loop_timeout.h"
 #include "base/test/simple_test_clock.h"
 #include "base/test/simple_test_tick_clock.h"
@@ -36,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/feed/core/v2/feed_network.h"
 #include "components/feed/core/v2/image_fetcher.h"
 #include "components/feed/core/v2/metrics_reporter.h"
+#include "components/feed/core/v2/prefs.h"
 #include "components/feed/core/v2/protocol_translator.h"
 #include "components/feed/core/v2/refresh_task_scheduler.h"
 #include "components/feed/core/v2/scheduling.h"
@@ -1397,11 +1399,22 @@ TEST_F(FeedStreamTest, LoadMoreBeforeLoad) {
 }
 
 TEST_F(FeedStreamTest, ReadNetworkResponse) {
+  base::HistogramTester histograms;
   network_.InjectRealResponse();
   TestSurface surface(stream_.get());
   WaitForIdleTaskQueue();
 
   ASSERT_EQ("loading -> 10 slices", surface.DescribeUpdates());
+
+  // Verify we're processing some of the data on the request.
+
+  // The response has a privacy_notice_fulfilled=true.
+  histograms.ExpectUniqueSample("ContentSuggestions.Feed.NoticeCardFulfilled",
+                                1, 1);
+
+  // A request schedule with two entries was in the response.
+  RequestSchedule schedule = prefs::GetRequestSchedule(profile_prefs_);
+  EXPECT_EQ(std::vector<base::TimeDelta>({}), schedule.refresh_offsets);
 }
 
 TEST_F(FeedStreamTest, ClearAllAfterLoadResultsInRefresh) {
--- components/feed/core/v2/tasks/load_stream_task.cc
+++ components/feed/core/v2/tasks/load_stream_task.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/feed/core/proto/v2/wire/request.pb.h"
 #include "components/feed/core/v2/feed_network.h"
 #include "components/feed/core/v2/feed_stream.h"
+#include "components/feed/core/v2/metrics_reporter.h"
 #include "components/feed/core/v2/proto_util.h"
 #include "components/feed/core/v2/protocol_translator.h"
 #include "components/feed/core/v2/stream_model.h"
@@ -152,6 +153,11 @@ void LoadStreamTask::QueryRequestComplete(
           stream_->GetClock()->Now());
   if (!response_data.model_update_request)
     return Done(LoadStreamStatus::kProtoTranslationFailed);
+
+  MetricsReporter::NoticeCardFulfilled(result.response_body->feed_response()
+                                           .feed_response_metadata()
+                                           .chrome_feed_response_metadata()
+                                           .privacy_notice_fulfilled());
 
   loaded_new_content_from_network_ = true;
   stream_->GetStore()->OverwriteStream(
