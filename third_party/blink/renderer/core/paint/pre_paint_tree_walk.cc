b6bb91ec1d74905851b1f4613d23a9536c89e551
Morten Stenshorne <mstensho@chromium.org>
2020-08-14 13:17:34 +0000

    [LayoutNG] The containing block of a legend is the fieldset itself.
    Although the rendered legend always is a child of the anonymous fieldset
    content wrapper (the only child of the fieldset) in the layout tree, the
    NG fragment for the legend is a direct child of the fragment for the
    fieldset itself. Match this better on the layout object side, by
    skipping over the anonymous content wrapper (and also the flow thread,
    if present) when looking for the containing block.
    
    The fieldset will now act as the containing block of a rendered legend,
    even if there's no direct parent-child relationship on the layout side.
    This is kind of similar to how it works for out-of-flow positioned
    objects.
    
    Also add special code in pre paint for this. This is only needed when
    traversing the layout object tree; when we switch over to walking the NG
    fragment tree (LayoutNGFragmentTraversal feature), this special code
    won't be needed. A more elegant solution might be to add another
    ContainingBlockContext member for this to ContainingBlockContext in
    PaintPropertyTreeBuilderFragmentContext (so that we'd reset the state
    to the one we had at the fieldset, when entering the rendered legend),
    but it doesn't seem worth it, given that layout object tree walking will
    be replaced by NG fragment walking at some point.
    
    No intended behavior changes currently, but this code change is required
    in order to handle scrollable fieldsets correctly (which will be added
    shortly). When a fieldset is scrollable, the legend shouldn't scroll
    along with the contents (since a legend is part of the border,
    essentially). We'll therefore establish a scrollable layer on the
    anonymous wrapper, NOT on the legend.
    
    Bug: 875235, 786475
    Change-Id: Id31ed3391dcfdbd8ed15b927de69a963368fef13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2351973
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798074}

--- third_party/blink/renderer/core/paint/pre_paint_tree_walk.cc
+++ third_party/blink/renderer/core/paint/pre_paint_tree_walk.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/layout/layout_box_model_object.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_content.h"
+#include "third_party/blink/renderer/core/layout/layout_fieldset.h"
 #include "third_party/blink/renderer/core/layout/layout_multi_column_spanner_placeholder.h"
 #include "third_party/blink/renderer/core/layout/layout_shift_tracker.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
@@ -695,6 +696,17 @@ void PrePaintTreeWalk::WalkLegacyChildren(const LayoutObject& object) {
     }
   }
 
+  if (UNLIKELY(object.IsFieldsetIncludingNG())) {
+    // Handle the rendered legend of the fieldset right away. It may not be a
+    // direct child in the layout object tree (there may be an anonymous
+    // fieldset content wrapper in-between, and even a flow thread), but it is
+    // to be treated as such (similarly to out-of-flow positioned elements in a
+    // way).
+    if (const LayoutBox* legend =
+            LayoutFieldset::FindInFlowLegend(To<LayoutBlock>(object)))
+      Walk(*legend, /* iterator */ nullptr);
+  }
+
   for (const LayoutObject* child = object.SlowFirstChild(); child;
        child = child->NextSibling()) {
     if (child->IsLayoutMultiColumnSpannerPlaceholder()) {
@@ -714,6 +726,12 @@ void PrePaintTreeWalk::WalkLegacyChildren(const LayoutObject& object) {
     if (UNLIKELY(RuntimeEnabledFeatures::LayoutNGFragmentTraversalEnabled() &&
                  child->IsOutOfFlowPositioned() && object.IsLayoutNGObject()))
       continue;
+
+    // The rendered legend was handled above, before processing the children of
+    // the fieldset. So skip it when found during normal child traversal.
+    if (UNLIKELY(child->IsRenderedLegend()))
+      continue;
+
     Walk(*child, /* iterator */ nullptr);
   }
 
