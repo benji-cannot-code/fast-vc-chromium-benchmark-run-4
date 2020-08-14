1b2301c4665ec746f73a2f734c8b39eb22955aad
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-08-14 00:07:12 +0000

    Import wpt@ed728bc7b01fed9c0cf7f4794d592fb2f1e151fa
    Using wpt-import in Chromium 33d245c7183c2f0d6085a54b4e315eaa15799b18.
    With Chromium commits locally applied on WPT:
    e074b42be7 "Delay restarting inline rAF loop until page is notified of session end"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=robertma@google.com
    
    No-Export: true
    Change-Id: Ice190734b83f7a4ef0faf2d8b5ac9f3530a3e036
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353293
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#797921}

--- third_party/blink/web_tests/external/wpt/html/canvas/tools/build.sh
+++ third_party/blink/web_tests/external/wpt/html/canvas/tools/build.sh
@@ -3,6 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 set -ex
 
 cd "${0%/*}"
-virtualenv -p python2 .virtualenv
+virtualenv -p python .virtualenv
 .virtualenv/bin/pip install pyyaml cairocffi
 .virtualenv/bin/python gentest.py
