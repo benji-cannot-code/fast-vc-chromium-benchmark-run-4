d651c12543997d7bc5bdae5e3fcc39bf0af8f9f6
Robbie McElrath <rmcelrath@chromium.org>
2020-08-14 20:47:08 +0000

    Revert "Disable flaky test ErrorPageCallbackTest.testOverrideErrorPage"
    This reverts commit 0f654bd2a1a5f846fc20d3d753f9f1c53948ba43.
    
    Reason for revert: The CL that caused the flakiness was reverted.
    
    Original change's description:
    > Disable flaky test ErrorPageCallbackTest.testOverrideErrorPage
    > 
    > org.chromium.weblayer.test.ErrorPageCallbackTest.testOverrideErrorPage
    > is flaky on Android,
    > https://analysis.chromium.org/p/chromium/flake-portal/flakes/
    > occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyfAsSBUZsYWtlInFjaHJvbWl1bUB3ZWJs
    > YXllcl9pbnN0cnVtZW50YXRpb25fdGVzdF9hcGtAb3JnLmNocm9taXVtLndlYmxheWVyLnRl
    > c3QuRXJyb3JQYWdlQ2FsbGJhY2tUZXN0I3Rlc3RPdmVycmlkZUVycm9yUGFnZQw,
    > disable it for now until it is fixed.
    > 
    > TBR=estade@chromium.org
    > 
    > Bug: 1116277
    > Change-Id: Id9eaa577587cb64136cd36364dc7e882f1c34876
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2355399
    > Reviewed-by: Lan Wei <lanwei@chromium.org>
    > Commit-Queue: Lan Wei <lanwei@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#798021}
    
    TBR=estade@chromium.org,lanwei@chromium.org
    
    Change-Id: I53f4ab75620ae834b2969cf6042c201f381e443a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1116277
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2357595
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798243}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/ErrorPageCallbackTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/ErrorPageCallbackTest.java
@@ -18,7 +18,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.net.test.EmbeddedTestServer;
 import org.chromium.net.test.ServerCertificate;
@@ -152,7 +151,6 @@ public class ErrorPageCallbackTest {
     @Test
     @SmallTest
     @MinWebLayerVersion(86)
-    @DisabledTest(message = "Flaky - https://crbug.com/1116277")
     public void testOverrideErrorPage() throws Throwable {
         mCallback.mErrorPage = new ErrorPage("<html><head><title>test error</title>");
         TestThreadUtils.runOnUiThreadBlocking(
