78d44e87028b6cbde5652c68df90e3b634964214
Noah Rose Ledesma <noahrose@google.com>
2020-08-14 21:48:34 +0000

    Do not notify MediaSessionControllersManager after MediaWebContentsObserver dies
    Fix a crash caused by posting a callback to the UI thread with a pointer
    to a member variable without checking that the originating object is still
    alive.
    
    Bug: 1115364
    Change-Id: Idf1594e8072d6269d3a0d2e04c17a583400e71c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353396
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/master@{#798288}

--- content/browser/media/media_web_contents_observer.h
+++ content/browser/media/media_web_contents_observer.h
@@ -100,6 +100,10 @@ class CONTENT_EXPORT MediaWebContentsObserver : public WebContentsObserver {
     audible_metrics_ = audible_metrics;
   }
 
+  void OnReceivedTranslatedDeviceId(RenderFrameHost* render_frame_host,
+                                    int delegate_id,
+                                    const std::string& raw_device_id);
+
 #if defined(OS_ANDROID)
   // Called by the WebContents when a tab has been closed but may still be
   // available for "undo" -- indicates that all media players (even audio only
@@ -153,10 +157,6 @@ class CONTENT_EXPORT MediaWebContentsObserver : public WebContentsObserver {
   void OnAudioOutputSinkChanged(RenderFrameHost* render_frame_host,
                                 int delegate_id,
                                 std::string hashed_device_id);
-  void OnAudioOutputDeviceIdTranslated(
-      RenderFrameHost* render_frame_host,
-      int delegate_id,
-      const base::Optional<std::string>& raw_device_id);
   void OnBufferUnderflow(RenderFrameHost* render_frame_host, int delegate_id);
 
   device::mojom::WakeLock* GetAudioWakeLock();
