washezium/chromium

commit 8c5d422be343f7dbded6d5f4fa95fed4b26f4082

WebLayer shell: turn non-URL URL bar inputs into searches.
For example, typing "cats" searches for cats. Works from URL bar or
command line.

Bug: none
Change-Id: Icd609ca4183398254d886d392570216eeca82d7a
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353059
Auto-Submit: Evan Stade <estade@chromium.org>
Commit-Queue: Scott Violet <sky@chromium.org>
Reviewed-by: Scott Violet <sky@chromium.org>
Cr-Commit-Position: refs/heads/master@{#797458}

weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/WebLayerShellActivity.java
@@ -4,6 +4,8 @@
 
 package org.chromium.weblayer.shell;
 
+import static android.util.Patterns.WEB_URL;
+
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
@@ -19,7 +21,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.webkit.URLUtil;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
@@ -356,11 +357,7 @@ public class WebLayerShellActivity extends FragmentActivity {
         if (getCurrentDisplayUrl() != null) {
             return;
         }
-        String startupUrl = getUrlFromIntent(getIntent());
-        if (TextUtils.isEmpty(startupUrl) || !URLUtil.isValidUrl(startupUrl)) {
-            startupUrl = "https://google.com";
-        }
-        loadUrl(startupUrl);
+        loadUrl(getUrlFromIntent(getIntent()));
     }
 
     /* Returns the Url for the current tab as a String, or null if there is no
@@ -515,8 +512,21 @@ public class WebLayerShellActivity extends FragmentActivity {
         return fragment;
     }
 
-    public void loadUrl(String url) {
-        mBrowser.getActiveTab().getNavigationController().navigate(Uri.parse(sanitizeUrl(url)));
+    public void loadUrl(String input) {
+        String sanitized = sanitizeUrl(input);
+
+        Uri uri;
+        if (WEB_URL.matcher(sanitized).matches()) {
+            uri = Uri.parse(sanitized);
+        } else if (TextUtils.isEmpty(input)) {
+            uri = Uri.parse("https://google.com");
+        } else {
+            uri = Uri.parse("https://google.com/search")
+                          .buildUpon()
+                          .appendQueryParameter("q", input)
+                          .build();
+        }
+        mBrowser.getActiveTab().getNavigationController().navigate(uri);
     }
 
     private static String getUrlFromIntent(Intent intent) {

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 373eabd7ff90a2278285a690f9780d58fbbd46c5

WebLayer shell: fix crash on null input
This fixes run_weblayer_shell crashing on startup when no URL is passed
(broken in 8c5d422be343f7dbded6d5f4fa95fed4b26f4082)

Bug: none
Change-Id: I1b4a82853686f14f6a244eac226db21cecc69dbb
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354611
Auto-Submit: Evan Stade <estade@chromium.org>
Reviewed-by: Mugdha Lakhani <nator@chromium.org>
Commit-Queue: Evan Stade <estade@chromium.org>
Cr-Commit-Position: refs/heads/master@{#798213}

weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/WebLayerShellActivity.java
@@ -514,20 +514,7 @@ public class WebLayerShellActivity extends FragmentActivity {
     }
 
     public void loadUrl(String input) {
-        String sanitized = sanitizeUrl(input);
-
-        Uri uri;
-        if (WEB_URL.matcher(sanitized).matches()) {
-            uri = Uri.parse(sanitized);
-        } else if (TextUtils.isEmpty(input)) {
-            uri = Uri.parse("https://google.com");
-        } else {
-            uri = Uri.parse("https://google.com/search")
-                          .buildUpon()
-                          .appendQueryParameter("q", input)
-                          .build();
-        }
-        mBrowser.getActiveTab().getNavigationController().navigate(uri);
+        mBrowser.getActiveTab().getNavigationController().navigate(getUriFromInput(input));
     }
 
     private static String getUrlFromIntent(Intent intent) {
@@ -535,14 +522,27 @@ public class WebLayerShellActivity extends FragmentActivity {
     }
 
     /**
-     * Given an URL, this performs minimal sanitizing to ensure it will be valid.
-     * @param url The url to be sanitized.
-     * @return The sanitized URL.
+     * Given input which may be empty, null, a URL, or search terms, this forms a URI suitable for
+     * loading in a tab.
+     * @param input The text.
+     * @return A valid URL.
      */
-    public static String sanitizeUrl(String url) {
-        if (url == null) return null;
-        if (url.startsWith("www.") || url.indexOf(":") == -1) url = "http://" + url;
-        return url;
+    public static Uri getUriFromInput(String input) {
+        if (TextUtils.isEmpty(input)) {
+            return Uri.parse("https://google.com");
+        }
+
+        if (input.startsWith("www.") || input.indexOf(":") == -1) {
+            String url = "http://" + input;
+            if (WEB_URL.matcher(url).matches()) {
+                return Uri.parse(url);
+            }
+        }
+
+        return Uri.parse("https://google.com/search")
+                .buildUpon()
+                .appendQueryParameter("q", input)
+                .build();
     }
 
     @Override