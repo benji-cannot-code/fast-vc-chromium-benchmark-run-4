1d16228498c7c790bb8b8d8faed6db0310cd56fe
Juliet Levesque <julietlevesque@google.com>
2020-08-14 20:53:35 +0000

    [Nearby] Fix HTTP client parameters for UpdateDevice RPC
    1) Set correct HTTP host, OAuth2 token, and UpdateDevice URLs.
    2) Start the local device data manger.
    3) Log the HTTP request URL.
    
    Bug: b/154863110
    Change-Id: I6a2d758f52091f1cbbe290e2a4bed198f1584397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2350541
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798250}

--- chrome/browser/nearby_sharing/client/nearby_share_api_call_flow_impl.cc
+++ chrome/browser/nearby_sharing/client/nearby_share_api_call_flow_impl.cc
@@ -94,7 +94,7 @@ GURL NearbyShareApiCallFlowImpl::CreateApiCallUrl() {
           request_url_, key_value_pair.first, key_value_pair.second);
     }
   }
-
+  NS_LOG(VERBOSE) << "Creating Nearby Share HTTP URL: " << request_url_;
   return request_url_;
 }
 
