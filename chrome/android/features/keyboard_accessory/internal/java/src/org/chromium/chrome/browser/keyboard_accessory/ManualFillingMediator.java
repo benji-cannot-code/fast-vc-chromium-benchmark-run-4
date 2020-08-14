521f14fa7f05c3b1d5693b8448c597fea662c625
Friedrich Horschig <fhorschig@chromium.org>
2020-08-14 09:13:53 +0000

    [MFill][Android] Remove scene change observer from mediator
    This CL cleans up the usage of a scene change observer that used to be
    necessary to reset the accessory before scene changes (to avoid layout
    height changes affecting the tab switcher).
    
    Screenshots verifying the correct behavior are in the linked first bug.
    
    Bug: 1115644, 876254
    Change-Id: If522cfc5686028afadfbd03bf1c5b19c748b07d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352916
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798054}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingMediator.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingMediator.java
@@ -33,9 +33,6 @@ import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeKeyboardVisibilityDelegate;
 import org.chromium.chrome.browser.ChromeWindow;
 import org.chromium.chrome.browser.compositor.CompositorViewHolder;
-import org.chromium.chrome.browser.compositor.layouts.Layout;
-import org.chromium.chrome.browser.compositor.layouts.LayoutManager;
-import org.chromium.chrome.browser.compositor.layouts.SceneChangeObserver;
 import org.chromium.chrome.browser.contextualsearch.ContextualSearchManager;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.fullscreen.FullscreenManager;
@@ -98,20 +95,6 @@ class ManualFillingMediator extends EmptyTabObserver
     private DropdownPopupWindow mPopup;
     private BottomSheetController mBottomSheetController;
 
-    private final SceneChangeObserver mTabSwitcherObserver = new SceneChangeObserver() {
-        @Override
-        public void onTabSelectionHinted(int tabId) {}
-
-        @Override
-        public void onSceneStartShowing(Layout layout) {
-            // Includes events like side-swiping between tabs and triggering contextual search.
-            pause();
-        }
-
-        @Override
-        public void onSceneChange(Layout layout) {}
-    };
-
     private final TabObserver mTabObserver = new EmptyTabObserver() {
         @Override
         public void onHidden(Tab tab, @TabHidingType int type) {
@@ -163,8 +146,6 @@ class ManualFillingMediator extends EmptyTabObserver
                 * mActivity.getResources().getDimensionPixelSize(
                         R.dimen.keyboard_accessory_suggestion_height));
         setInsetObserverViewSupplier(mActivity::getInsetObserverView);
-        LayoutManager manager = getLayoutManager();
-        if (manager != null) manager.addSceneChangeObserver(mTabSwitcherObserver);
         mActivity.findViewById(android.R.id.content).addOnLayoutChangeListener(this);
         mTabModelObserver = new TabModelSelectorTabModelObserver(mActivity.getTabModelSelector()) {
             @Override
@@ -263,8 +244,6 @@ class ManualFillingMediator extends EmptyTabObserver
         mStateCache.destroy();
         for (Tab tab : mObservedTabs) tab.removeObserver(mTabObserver);
         mObservedTabs.clear();
-        LayoutManager manager = getLayoutManager();
-        if (manager != null) manager.removeSceneChangeObserver(mTabSwitcherObserver);
         mActivity.getFullscreenManager().removeObserver(mFullscreenObserver);
         mBottomSheetController.removeObserver(mBottomSheetObserver);
         mWindowAndroid = null;
@@ -540,18 +519,6 @@ class ManualFillingMediator extends EmptyTabObserver
         Tab tab = getActiveBrowserTab();
         if (tab == null) return null;
         return tab.getContentView();
-    }
-
-    /**
-     * Shorthand to check whether there is a valid {@link LayoutManager} for the current activity.
-     * If there isn't (e.g. before initialization or after destruction), return null.
-     * @return {@code null} or a {@link LayoutManager}.
-     */
-    private @Nullable LayoutManager getLayoutManager() {
-        if (mActivity == null) return null;
-        CompositorViewHolder compositorViewHolder = mActivity.getCompositorViewHolder();
-        if (compositorViewHolder == null) return null;
-        return compositorViewHolder.getLayoutManager();
     }
 
     /**
