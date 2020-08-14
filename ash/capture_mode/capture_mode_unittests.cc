7b9c6a5f75071bfba7d47b4dfa7f98ae263fbf28
Ahmed Fakhry <afakhry@chromium.org>
2020-08-14 21:24:32 +0000

    capture_mode: More capture session work
    This CL fills in more impl of the capture session:
    - Paints the capture region according to the current selected
      source.
    - Add support for event handling.
    - Pressing ESC dismisses the session, while ENTER performs
      the capture and dismisses the session.
    
    BUG=1116137
    TEST=Manual
    
    Change-Id: Ic86fd1230dea04d8d59ed19cbf7455e70d0a7795
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354567
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798276}

--- ash/capture_mode/capture_mode_unittests.cc
+++ ash/capture_mode/capture_mode_unittests.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ash/capture_mode/capture_mode_bar_view.h"
 #include "ash/capture_mode/capture_mode_close_button.h"
 #include "ash/capture_mode/capture_mode_controller.h"
 #include "ash/capture_mode/capture_mode_session.h"
