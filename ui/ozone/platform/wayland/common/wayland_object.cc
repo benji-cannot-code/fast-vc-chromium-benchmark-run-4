049e1e6cc76e10d7efd18d540fd58dcb453c66ec
Antonio Gomes <tonikitoo@igalia.com>
2020-08-14 16:54:28 +0000

    [ozone/wayland] zaura_shell initial support
    This allows lacros/chrome to auto hide/show ChromeOS shelf
    when it enters in fullscreen mode. It is the so called
    'immersive fullscreen' mode that chromeos/chrome has.
    
    MANUAL TEST: Enter lacros/chrome in fullscreen and hover
    the mouse pointer over the bottom border of the screen
    (shelf area). The shelf should slide-up.
    
    Note that this brings lacros/chrome fullscreen behavior
    closer to chromeos/chrome's. In a follow up CL, the omnibox
    widget that slides down from the upper border of Ash will
    be plumbed up to lacros/chrome.
    
    BUG=1113900
    R=msisov@igalia.com, nickdiego@igalia.com
    
    Change-Id: I4b7c04d18b73747a0c70bffa5a6d6741df6c4346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353116
    Reviewed-by: Maksim Sisov (GMT+3) <msisov@igalia.com>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Antonio Gomes (GMT-4) <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#798117}

--- ui/ozone/platform/wayland/common/wayland_object.cc
+++ ui/ozone/platform/wayland/common/wayland_object.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/ozone/platform/wayland/common/wayland_object.h"
 
+#include <aura-shell-client-protocol.h>
 #include <gtk-primary-selection-client-protocol.h>
 #include <keyboard-extension-unstable-v1-client-protocol.h>
 #include <linux-dmabuf-unstable-v1-client-protocol.h>
@@ -190,6 +191,15 @@ const wl_interface* ObjectTraits<xdg_positioner>::interface =
     &xdg_positioner_interface;
 void (*ObjectTraits<xdg_positioner>::deleter)(xdg_positioner*) =
     &xdg_positioner_destroy;
+
+const wl_interface* ObjectTraits<zaura_shell>::interface =
+    &zaura_shell_interface;
+void (*ObjectTraits<zaura_shell>::deleter)(zaura_shell*) = &zaura_shell_destroy;
+
+const wl_interface* ObjectTraits<zaura_surface>::interface =
+    &zaura_surface_interface;
+void (*ObjectTraits<zaura_surface>::deleter)(zaura_surface*) =
+    &zaura_surface_destroy;
 
 const wl_interface* ObjectTraits<zcr_keyboard_extension_v1>::interface =
     &zcr_keyboard_extension_v1_interface;
