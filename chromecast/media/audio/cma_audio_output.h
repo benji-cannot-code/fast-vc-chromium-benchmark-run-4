f5205f04c8eab9a1f956c610ac1380f6c1df4999
ziyangch <ziyangch@google.com>
2020-08-14 03:53:34 +0000

    [Chromecast] Add Observer in CmaBackend::AudioDecoder.
    Bug: Internal b/162079069
    
    Merge-With: eureka-internal/424693
    
    Test: Check whether the observer receive the signal on device.
    
    Change-Id: I44c15917a4104cfb94ca619d986587fd96b6857b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2349415
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Ziyang Cheng <ziyangch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798007}

--- chromecast/media/audio/cma_audio_output.h
+++ chromecast/media/audio/cma_audio_output.h
@@ -42,6 +42,8 @@ class CmaAudioOutput {
   CmaAudioOutput& operator=(const CmaAudioOutput&) = delete;
   ~CmaAudioOutput();
 
+  void SetObserver(CmaBackend::AudioDecoder::Observer* observer);
+
   bool Start(int64_t start_pts);
   void Stop();
   bool Pause();
