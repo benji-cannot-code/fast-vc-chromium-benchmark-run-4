2c12d889121847ab0e877d44a96a01d3465edbc9
Ulan Degenbaev <ulan@chromium.org>
2020-08-14 09:38:29 +0000

    Add a helper class for measuring memory usage of workers in a renderer
    V8WorkerMemoryReporter posts a memory measurement task on each worker
    thread and then collects the results. A memory measurement is performed
    using V8's MeasureMemory API. The new class will be used by
    V8PerContextMemoryReporter to serve requests from PerformanceManager.
    
    Design doc: https://bit.ly/30Y8Js6
    
    Bug: 1085129
    Change-Id: Ic02c9e41f74fb5f4a890d19c59cf222d1aa7ca82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332275
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798056}

---
+++ third_party/blink/renderer/core/workers/dedicated_worker_test.h
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_WORKERS_DEDICATED_WORKER_TEST_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_WORKERS_DEDICATED_WORKER_TEST_H_
+
+#include "third_party/blink/renderer/core/testing/page_test_base.h"
+#include "third_party/blink/renderer/platform/heap/persistent.h"
+
+namespace blink {
+
+class DedicatedWorkerThreadForTest;
+class DedicatedWorkerMessagingProxyForTest;
+
+class DedicatedWorkerTest : public PageTestBase {
+ public:
+  DedicatedWorkerTest() = default;
+
+  void SetUp() override;
+
+  void TearDown() override;
+
+  void DispatchMessageEvent();
+
+  DedicatedWorkerMessagingProxyForTest* WorkerMessagingProxy();
+
+  DedicatedWorkerThreadForTest* GetWorkerThread();
+
+  void StartWorker(const String& source_code);
+  void WaitUntilWorkerIsRunning();
+
+ private:
+  Persistent<DedicatedWorkerMessagingProxyForTest> worker_messaging_proxy_;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_WORKERS_DEDICATED_WORKER_TEST_H_
