ec7166abaaf1263909546c369b468d04050f2bfe
Camille Lamy <clamy@chromium.org>
2020-08-14 18:11:08 +0000

    Extract CrossOriginOpenerPolicyStatus into class
    This CL extracts CrossOriginOpenerPolicyStatus into its own class. The
    struct has grown quite big, and it will allow to better encapsulate
    CrossOriginOpenerPolicy enforcement. This is a preparatory CL for
    having reports sent on redirects.
    
    Bug: 1059303
    Change-Id: Ia2d71e0f592296ac6f7a89519068ac84796bf224
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353264
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798153}

--- content/browser/cross_origin_opener_policy_browsertest.cc
+++ content/browser/cross_origin_opener_policy_browsertest.cc
@@ -131,7 +131,7 @@ IN_PROC_BROWSER_TEST_P(CrossOriginOpenerPolicyBrowserTest,
   EXPECT_TRUE(NavigateToURL(shell(), starting_page));
 
   RenderFrameHostImpl* main_frame = current_frame_host();
-  main_frame->set_cross_origin_opener_policy(CoopSameOrigin());
+  main_frame->set_cross_origin_opener_policy_for_testing(CoopSameOrigin());
 
   ShellAddedObserver shell_observer;
   RenderFrameHostImpl* iframe = main_frame->child_at(0)->current_frame_host();
@@ -154,7 +154,8 @@ IN_PROC_BROWSER_TEST_P(CrossOriginOpenerPolicyBrowserTest,
   EXPECT_TRUE(NavigateToURL(shell(), starting_page));
 
   RenderFrameHostImpl* main_frame = current_frame_host();
-  main_frame->set_cross_origin_opener_policy(CoopSameOriginAllowPopups());
+  main_frame->set_cross_origin_opener_policy_for_testing(
+      CoopSameOriginAllowPopups());
 
   ShellAddedObserver shell_observer;
   RenderFrameHostImpl* iframe = main_frame->child_at(0)->current_frame_host();
@@ -179,7 +180,7 @@ IN_PROC_BROWSER_TEST_P(CrossOriginOpenerPolicyBrowserTest,
   EXPECT_TRUE(NavigateToURL(shell(), starting_page));
 
   RenderFrameHostImpl* main_frame = current_frame_host();
-  main_frame->set_cross_origin_opener_policy(CoopSameOrigin());
+  main_frame->set_cross_origin_opener_policy_for_testing(CoopSameOrigin());
 
   ShellAddedObserver shell_observer;
   RenderFrameHostImpl* iframe = main_frame->child_at(0)->current_frame_host();
@@ -203,7 +204,7 @@ IN_PROC_BROWSER_TEST_P(
   EXPECT_TRUE(NavigateToURL(shell(), starting_page));
 
   RenderFrameHostImpl* main_frame = current_frame_host();
-  main_frame->set_cross_origin_opener_policy(CoopSameOrigin());
+  main_frame->set_cross_origin_opener_policy_for_testing(CoopSameOrigin());
 
   ShellAddedObserver new_shell_observer;
   RenderFrameHostImpl* iframe = main_frame->child_at(0)->current_frame_host();
