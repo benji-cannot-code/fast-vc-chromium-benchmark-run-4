fb81deb6c1d0b124ace72b4e3636535b3ef688b6
Lexi Stavrakos <astavrakos@google.com>
2020-08-14 18:32:00 +0000

    Quota: Make sure quota_host_ is bound before adding change listeners
    Binds quota_host_ in AddedChangeListener() if not already bound so that StartObserving() can pass the listener back to the browser side.
    
    Bug: 1116316
    Change-Id: I43e5bed0dc6cb931c3f950faee05b1c1f3e60ea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2356284
    Reviewed-by: Jarryd Goodman <jarrydg@chromium.org>
    Commit-Queue: Lexi Stavrakos <astavrakos@google.com>
    Cr-Commit-Position: refs/heads/master@{#798169}

--- third_party/blink/renderer/modules/quota/storage_manager.cc
+++ third_party/blink/renderer/modules/quota/storage_manager.cc
@@ -190,6 +190,10 @@ void StorageManager::OnQuotaChange() {
 void StorageManager::AddedEventListener(
     const AtomicString& event_type,
     RegisteredEventListener& registered_listener) {
+  if (!quota_host_.is_bound()) {
+    // This method will bind quota_host_.
+    GetQuotaHost(GetExecutionContext());
+  }
   EventTargetWithInlineData::AddedEventListener(event_type,
                                                 registered_listener);
   StartObserving();
@@ -231,7 +235,7 @@ void StorageManager::PermissionRequestComplete(ScriptPromiseResolver* resolver,
 }
 
 void StorageManager::StartObserving() {
-  if (change_listener_receiver_.is_bound() || quota_host_.is_bound())
+  if (change_listener_receiver_.is_bound())
     return;
 
   // Using kMiscPlatformAPI because the Storage specification does not
