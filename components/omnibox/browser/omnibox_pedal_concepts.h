c107238ade80c3df7557fd9d57cb5ae1e3fa8ea9
Orin Jaworski <orinj@chromium.org>
2020-08-14 22:44:35 +0000

    [omnibox] Add 'am' language support for Pedals
    This CL adds a single language data set to serve
    as a minimal example of how to cover all aspects
    including JSON data, header, unit tests, etc.
    
    Bug: 893183
    Change-Id: Iee33b87dbb331946652c64dbf9c1714c1c8879f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2347972
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Auto-Submit: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798323}

--- components/omnibox/browser/omnibox_pedal_concepts.h
+++ components/omnibox/browser/omnibox_pedal_concepts.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This value is generated during Pedal concept data processing, and written
 // to all data files as well as the source code here to ensure synchrony.
 // The runtime loaded data must match this version exactly or it won't load.
-constexpr int OMNIBOX_PEDAL_CONCEPTS_DATA_VERSION = 15453828;
+constexpr int OMNIBOX_PEDAL_CONCEPTS_DATA_VERSION = 15462706;
 
 // Unique identifiers for Pedals, used to bind loaded data to implementations.
 enum class OmniboxPedalId {
