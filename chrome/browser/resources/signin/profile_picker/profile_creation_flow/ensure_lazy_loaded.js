5d3a36b6b1e59bbd905e96377e65997922fd1e78
dizg <dizg@google.com>
2020-08-14 16:24:46 +0000

    [signin] Add test for signin button implementation in the profile creation flow
    Bug: 1105865
    Change-Id: Ie0b4d9f57be1edb784c108f774905ecf2d687c9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339315
    Commit-Queue: Diana Zagidullina <dizg@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798097}

--- chrome/browser/resources/signin/profile_picker/profile_creation_flow/ensure_lazy_loaded.js
+++ chrome/browser/resources/signin/profile_picker/profile_creation_flow/ensure_lazy_loaded.js
@@ -3,10 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/** @type {?Promise<void>} */
+/** @type {?Promise<!Array<void>>} */
 let lazyLoadPromise = null;
 
-/** @return {!Promise<void>} Resolves when the lazy load module is imported. */
+/**
+ * @return {!Promise<!Array<void>>} Resolves when the lazy load module is
+ *     imported.
+ */
 export function ensureLazyLoaded() {
   if (!lazyLoadPromise) {
     const script = document.createElement('script');
