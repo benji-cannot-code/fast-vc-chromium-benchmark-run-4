07190afe15eccaa7473b13958a3db0193fdd6887
Lan Wei <lanwei@chromium.org>
2020-08-14 05:09:47 +0000

    Disable flaky test ErrorPageCallbackTest.testOverrideErrorPage
    org.chromium.weblayer.test.ErrorPageCallbackTest.testOverrideErrorPage
    is flaky on Android,
    https://analysis.chromium.org/p/chromium/flake-portal/flakes/
    occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyfAsSBUZsYWtlInFjaHJvbWl1bUB3ZWJs
    YXllcl9pbnN0cnVtZW50YXRpb25fdGVzdF9hcGtAb3JnLmNocm9taXVtLndlYmxheWVyLnRl
    c3QuRXJyb3JQYWdlQ2FsbGJhY2tUZXN0I3Rlc3RPdmVycmlkZUVycm9yUGFnZQw,
    disable it for now until it is fixed.
    
    TBR=estade@chromium.org
    
    Bug: 1116277
    Change-Id: Id9eaa577587cb64136cd36364dc7e882f1c34876
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2355399
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798021}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/ErrorPageCallbackTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/ErrorPageCallbackTest.java
@@ -18,6 +18,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.net.test.EmbeddedTestServer;
 import org.chromium.net.test.ServerCertificate;
@@ -151,6 +152,7 @@ public class ErrorPageCallbackTest {
     @Test
     @SmallTest
     @MinWebLayerVersion(86)
+    @DisabledTest(message = "Flaky - https://crbug.com/1116277")
     public void testOverrideErrorPage() throws Throwable {
         mCallback.mErrorPage = new ErrorPage("<html><head><title>test error</title>");
         TestThreadUtils.runOnUiThreadBlocking(
