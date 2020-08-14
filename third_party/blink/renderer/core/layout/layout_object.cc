9d4e3697374ed268987c5b23bf9665d8cc6294e6
Keishi Hattori <keishi@chromium.org>
2020-08-14 20:21:29 +0000

    Add CHECK for Layout::Destroy() called inside a finalizer
    LayoutObject::Destroy() should not be called inside a finalizer. This becomes dangerous when we migrate LayoutObje ct to Oilpan as finalizers are not allowed to access other on-heap objects.
    
    This CL adds AllowDestroyingLayoutObjectInFinalizerScope to mark certain SVGImage() as exempt from this CHECK. This is OK as SVGImage is retaining the LayoutObject via a Persistent and so it is guaranteed to survive the current GC cycle.
    
    Bug: 1030176
    Change-Id: Ibdd0a29e7630747ea118d92d442776cf1bac8537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352294
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798217}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -150,6 +150,18 @@ LayoutObject* FindAncestorByPredicate(const LayoutObject* descendant,
 
 }  // namespace
 
+static int g_allow_destroying_layout_object_in_finalizer = 0;
+
+AllowDestroyingLayoutObjectInFinalizerScope::
+    AllowDestroyingLayoutObjectInFinalizerScope() {
+  ++g_allow_destroying_layout_object_in_finalizer;
+}
+AllowDestroyingLayoutObjectInFinalizerScope::
+    ~AllowDestroyingLayoutObjectInFinalizerScope() {
+  CHECK_GT(g_allow_destroying_layout_object_in_finalizer, 0);
+  --g_allow_destroying_layout_object_in_finalizer;
+}
+
 #if DCHECK_IS_ON()
 
 LayoutObject::SetLayoutNeededForbiddenScope::SetLayoutNeededForbiddenScope(
@@ -3561,6 +3573,9 @@ void LayoutObject::DestroyAndCleanupAnonymousWrappers() {
 }
 
 void LayoutObject::Destroy() {
+  CHECK(g_allow_destroying_layout_object_in_finalizer ||
+        !ThreadState::Current()->InAtomicSweepingPause());
+
   // Mark as being destroyed to avoid trouble with merges in |RemoveChild()| and
   // other house keepings.
   bitfields_.SetBeingDestroyed(true);
