b65886f4b99a71960e2d8f6dd120ae4f86fec435
Xianzhu Wang <wangxianzhu@chromium.org>
2020-08-14 09:56:25 +0000

    Ensure opaque background with transformed rasterization
    Transformed rasterization is a method to achieve clear text under
    fractional transform. When applicable, it applies the fraction part of
    the draw trantransform during rasterization, and the rastered texels
    are aligned with the physical pixels, to avoid blurriness of text
    during compositing.
    
    Previously when it was enabled, because it would cause the texels at
    the top and left edges of the rastered result non-opaque, we forced
    the PictureLayerImpl to be non-opaque, which disables the occlusion
    optimization and LCD text.
    
    This CL extends the original clearing for opaque logic (extracted from
    raster_source.cc and paint_op_buffer_serializer.cc into
    clear_for_opaque_raster.cc) to all raster transforms (previously it was
    for scale only). When there is raster translation and the layer has
    content_opaque(), fill the top and left edges of the rastered result
    with the background color to ensure the layer is still fully opaque.
    
    This doesn't fix crbug.com/1111195, but prepares for the fix which will
    apply transformed rasterization in more cases.
    
    Bug: 1111195
    Change-Id: I2ba4a091cf47e795154a41a2ccbd762f12523bd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2344006
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798059}

--- cc/layers/picture_layer_impl.cc
+++ cc/layers/picture_layer_impl.cc
@@ -1074,11 +1074,6 @@ void PictureLayerImpl::SetNearestNeighbor(bool nearest_neighbor) {
 }
 
 void PictureLayerImpl::SetUseTransformedRasterization(bool use) {
-  // With transformed rasterization, the pixels along the edge of the layer may
-  // become translucent, so clear contents_opaque.
-  if (use)
-    SetContentsOpaque(false);
-
   if (use_transformed_rasterization_ == use)
     return;
 
@@ -1592,8 +1587,6 @@ gfx::Vector2dF PictureLayerImpl::CalculateRasterTranslation(
     float raster_scale) {
   if (!use_transformed_rasterization_)
     return gfx::Vector2dF();
-
-  DCHECK(!contents_opaque());
 
   gfx::Transform draw_transform = DrawTransform();
   // TODO(enne): for performance reasons, we should only have a raster
