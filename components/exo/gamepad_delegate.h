a30c0c87b23f795733d633cf2e36098473863f0c
Merissa Tan <liyingtan@google.com>
2020-08-14 20:56:06 +0000

    Add gamepad rumble support in Exo and Wayland.
    - Add exo::Gamepad interface.
    - Add exo::GamepadObserver interface.
    - Add WaylandGamepadVibratorImpl to handle vibration and cancel
    vibration requests from the client.
    - Refactor WaylandGamingSeatDelegate so that WaylandGamepadDelegate
    handles all gamepad-specific events.
    - Add Wayland protocol to support gamepad rumble. The zcr_gamepad_vibrator_v2 interface is created when the server calls
    the vibrator_added event. The client can then trigger/disable vibration
    events on a gamepad vibrator through the interface.
    
    BUG=b:158686809
    TEST=Add unit tests in gamepad_unittest. All tests pass in
    exo_unittests.
    
    Change-Id: Ie4b1b3b5f9748f31846c54c97cb4f7c315d09a05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2313976
    Commit-Queue: Merissa Tan <liyingtan@google.com>
    Auto-Submit: Merissa Tan <liyingtan@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798253}

--- components/exo/gamepad_delegate.h
+++ components/exo/gamepad_delegate.h
@@ -13,6 +13,7 @@ namespace exo {
 // Handles events for a specific gamepad.
 class GamepadDelegate {
  public:
+  virtual ~GamepadDelegate() {}
   // Called when the gamepad has been removed.
   virtual void OnRemoved() = 0;
 
@@ -27,9 +28,6 @@ class GamepadDelegate {
   // Called after all gamepad information of this frame has been set and the
   // client should evaluate the updated state.
   virtual void OnFrame(base::TimeTicks timestamp) = 0;
-
- protected:
-  virtual ~GamepadDelegate() {}
 };
 
 }  // namespace exo
