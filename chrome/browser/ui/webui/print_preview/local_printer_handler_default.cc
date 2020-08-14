7daae8eb1d91ed81d2fb7132740b038ce01c4309
Alan Screen <awscreen@chromium.org>
2020-08-14 19:44:14 +0000

    Cleanup: drop settings parameter when creating PrintBackend
    Instantiating a PrintBackend class is done through a CreateInstance()
    call.  This call has included a parameter to optionally seed the new
    instance with some initial values different from the defaults.  This
    parameter is never made use of in the existing code.
    
    Cleanup of this parameter will ease the effort to mojoify the
    PrintBackend class.
    
    Bug: 809738
    Change-Id: I460e3edd14c2fd830d9d8f126b84619ec9e53597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354541
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798201}

--- chrome/browser/ui/webui/print_preview/local_printer_handler_default.cc
+++ chrome/browser/ui/webui/print_preview/local_printer_handler_default.cc
@@ -68,7 +68,7 @@ PrinterList LocalPrinterHandlerDefault::EnumeratePrintersAsync(
 #endif
 
   scoped_refptr<PrintBackend> print_backend(
-      PrintBackend::CreateInstance(nullptr, locale));
+      PrintBackend::CreateInstance(locale));
 
   PrinterList printer_list;
   print_backend->EnumeratePrinters(&printer_list);
@@ -92,7 +92,7 @@ base::Value LocalPrinterHandlerDefault::FetchCapabilitiesAsync(
 #endif
 
   scoped_refptr<PrintBackend> print_backend(
-      PrintBackend::CreateInstance(nullptr, locale));
+      PrintBackend::CreateInstance(locale));
 
   VLOG(1) << "Get printer capabilities start for " << device_name;
 
@@ -117,7 +117,7 @@ std::string LocalPrinterHandlerDefault::GetDefaultPrinterAsync(
 #endif
 
   scoped_refptr<PrintBackend> print_backend(
-      PrintBackend::CreateInstance(nullptr, locale));
+      PrintBackend::CreateInstance(locale));
 
   std::string default_printer = print_backend->GetDefaultPrinterName();
   VLOG(1) << "Default Printer: " << default_printer;
--- chrome/service/cloud_print/print_system_win.cc
+++ chrome/service/cloud_print/print_system_win.cc
@@ -65,8 +65,7 @@ class PrintSystemWatcherWin : public base::win::ObjectWatcher::Delegate {
 
   bool Start(const std::string& printer_name, Delegate* delegate) {
     scoped_refptr<printing::PrintBackend> print_backend(
-        printing::PrintBackend::CreateInstance(nullptr,
-                                               /*locale=*/std::string()));
+        printing::PrintBackend::CreateInstance(/*locale=*/std::string()));
     printer_info_ = print_backend->GetPrinterDriverInfo(printer_name);
     crash_keys::ScopedPrinterInfo crash_key(printer_info_);
 
@@ -241,8 +240,7 @@ class JobSpoolerWin : public PrintSystem::JobSpooler {
              JobSpooler::Delegate* delegate) override {
     // TODO(gene): add tags handling.
     scoped_refptr<printing::PrintBackend> print_backend(
-        printing::PrintBackend::CreateInstance(nullptr,
-                                               /*locale=*/std::string()));
+        printing::PrintBackend::CreateInstance(/*locale=*/std::string()));
     crash_keys::ScopedPrinterInfo crash_key(
         print_backend->GetPrinterDriverInfo(printer_name));
     return core_->Spool(print_ticket, print_ticket_mime_type,
@@ -646,8 +644,7 @@ class PrintSystemWin : public PrintSystem {
 
 PrintSystemWin::PrintSystemWin()
     : print_backend_(
-          printing::PrintBackend::CreateInstance(nullptr,
-                                                 /*locale=*/std::string())) {}
+          printing::PrintBackend::CreateInstance(/*locale=*/std::string())) {}
 
 PrintSystem::PrintSystemResult PrintSystemWin::Init() {
   return PrintSystemResult(true, std::string());
