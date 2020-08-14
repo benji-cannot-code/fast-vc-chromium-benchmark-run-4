f6144ff1016bede1e293cc75fe4d42ffd24c91e4
Emily Stark <estark@google.com>
2020-08-14 20:34:40 +0000

    Simplified domains: ignore private registries
    This CL ignores private registries when computing the simplified
    domain, i.e. registrable domain. For example, "foo.example.com" will
    be simplified to "example.com", and "foo.blogspot.com" will be
    simplified to "blogspot.com". (Note that as of
    https://chromium-review.googlesource.com/c/chromium/src/+/2337761, the
    simplified domain logic only applies in unusual cases; most of the
    time, we show the full hostname).
    
    The reason for ignoring private registries is that we don't want to
    create negative incentives for malicious websites to add themselves as
    private registries to the Public Suffix List.
    
    This policy isn't perfect, but we think it best balances the security
    benefit of eliding malicious subdomains while minimally impacting
    benign websites that use subdomains.
    
    Bug: 1106962
    Change-Id: Ifb75a510707c1dcac0825f899625069656838932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2356936
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798230}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.h
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.h
@@ -180,6 +180,7 @@ class OmniboxViewViews : public OmniboxView,
  private:
   FRIEND_TEST_ALL_PREFIXES(OmniboxViewViewsRevealOnHoverTest, HoverAndExit);
   FRIEND_TEST_ALL_PREFIXES(OmniboxViewViewsRevealOnHoverTest, HoverAndExitIDN);
+  FRIEND_TEST_ALL_PREFIXES(OmniboxViewViewsRevealOnHoverTest, PrivateRegistry);
   FRIEND_TEST_ALL_PREFIXES(
       OmniboxViewViewsHideOnInteractionAndRevealOnHoverTest,
       UserInteractionAndHover);
