9c3166ac05edbcf26cef9b7f91b2186b1db6d2ce
Moe Ahmadi <mahmadi@chromium.org>
2020-08-15 01:20:03 +0000

    Enables Desktop NTP realbox by default
    Feature being launched in cr/326256148
    
    - Enables ntp_features::kRealbox and
      omnibox::kZeroSuggestionsOnNTPRealbox which control the realbox and
      zero-suggest in realbox respectively.
    - Enables omnibox::kZeroSuggestionsOnNTP which should've been enabled
      in crrev.com/c/2175138.
    - Removes applicable fieldtrial_testing_config.json entries and adjusts
      test expectations.
    
    Change-Id: I1889e65c23e02624b85f0291600b26382e75f828
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354993
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798392}

--- chrome/browser/search/search_suggest/search_suggest_service_unittest.cc
+++ chrome/browser/search/search_suggest/search_suggest_service_unittest.cc
@@ -208,9 +208,7 @@ TEST_F(SearchSuggestServiceTest, IsEnabled) {
     // Note: ZPS variant 15 is NTP Realbox as starting focus.
     feature_params["ZeroSuggestVariant:15:*"] = kRemoteNoUrlLocal;
     feature_list.InitWithFeaturesAndParameters(
-        {{omnibox::kZeroSuggestionsOnNTP, {}},
-         {omnibox::kZeroSuggestionsOnNTPRealbox, {}},
-         {omnibox::kOnFocusSuggestions, feature_params},
+        {{omnibox::kOnFocusSuggestions, feature_params},
          {ntp_features::kSearchSuggestChips, {}}},
         {});
     EXPECT_TRUE(SearchSuggestService::IsEnabled());
@@ -237,9 +235,7 @@ TEST_F(SearchSuggestServiceTest, IsEnabled) {
     // Note: ZPS variant 15 is NTP Realbox as starting focus.
     feature_params["ZeroSuggestVariant:15:*"] = kRemoteNoUrlLocal;
     feature_list.InitWithFeaturesAndParameters(
-        {{omnibox::kZeroSuggestionsOnNTP, {}},
-         {omnibox::kZeroSuggestionsOnNTPRealbox, {}},
-         {omnibox::kOnFocusSuggestions, feature_params},
+        {{omnibox::kOnFocusSuggestions, feature_params},
          {ntp_features::kSearchSuggestChips, {}},
          {ntp_features::kDisableSearchSuggestChips, {}}},
         {});
