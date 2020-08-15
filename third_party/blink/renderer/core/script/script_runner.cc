7ea1d812f1b0e7fe4df74ddbf5e28da65e514251
Dominic Farolino <dom@chromium.org>
2020-08-15 01:20:07 +0000

    Add PerfectHeuristics UKM event, and metric for DelayAsyncScriptExecution
    This CL introduces the PerfectHeuristics UKM event, along with a boolean
    metric for when a blink::ScriptRunner comes across an async script
    before the script element's node document has finished parsing. The
    PerfectHeuristics UKM event is logged in the UkmPageLoadMetricsObserver.
    
    See the Google-internal UKM privacy review:
    https://docs.google.com/document/d/1UqkpGXc4LEh5jzzVR8fCVVTuCuG2mwvhQTuCy36zT3g/edit#
    
    See the Perfect Heuristics [1] project document (currently
    Google-internal).
    
    [1]: https://docs.google.com/document/d/1Y7uF76Gq0VlJZky2j7x-lbD1_awH64eW0WRg1bNLuFs/edit
    
    R=falken@chromium.org, nhiroki@chromium.org, ryansturm@chromium.org, sophiechang@chromium.org
    
    Bug: 1086227
    Change-Id: I71954491a1a559c2e1244a543092f1b8f38b8136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340845
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798393}

--- third_party/blink/renderer/core/script/script_runner.cc
+++ third_party/blink/renderer/core/script/script_runner.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/script/script_loader.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
@@ -148,6 +149,10 @@ bool ScriptRunner::CanDelayAsyncScripts() {
           DelayAsyncScriptExecutionUntilFinishedParsingEnabled() ||
       RuntimeEnabledFeatures::
           DelayAsyncScriptExecutionUntilFirstPaintOrFinishedParsingEnabled();
+  if (!document_->Parsing()) {
+    document_->Loader()->DidObserveLoadingBehavior(
+        kLoadingBehaviorAsyncScriptReadyBeforeDocumentFinishedParsing);
+  }
   return !delay_async_script_milestone_reached_ && flags_enabled;
 }
 
