34663cacc4f80f1e3997db1dd3fd51127b1e12e1
Viktor Semeniuk <vsemeniuk@google.com>
2020-08-16 12:05:00 +0000

    [iOS][Password Check] Changed selection highlight style
    This change adds default selection style to Password Check Details cell
    on Passwords screen and to Change password button on Passwords details
    screen.
    
    Bug: 1113816
    Change-Id: I7ae74f009e62c9700c4a85480ddf416102e71d70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353260
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#798485}

--- ios/chrome/browser/ui/settings/cells/settings_check_item.mm
+++ ios/chrome/browser/ui/settings/cells/settings_check_item.mm
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [super configureCell:cell withStyler:styler];
   cell.textLabel.text = self.text;
   cell.detailTextLabel.text = self.detailText;
-  cell.selectionStyle = UITableViewCellSelectionStyleNone;
   if (self.enabled) {
     [cell setInfoButtonHidden:self.infoButtonHidden];
     [cell setLeadingImage:self.leadingImage
--- ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_controller.mm
@@ -267,8 +267,6 @@ typedef NS_ENUM(NSInteger, ReauthenticationReason) {
 
   cell.selectionStyle = UITableViewCellSelectionStyleNone;
 
-  // TODO:(crbug.com/1075494) - Add action to Show/Hide password when user tap
-  // eye icon.
   NSInteger itemType = [self.tableViewModel itemTypeForIndexPath:indexPath];
   switch (itemType) {
     case ItemTypePassword: {
@@ -281,9 +279,11 @@ typedef NS_ENUM(NSInteger, ReauthenticationReason) {
           forControlEvents:UIControlEventTouchUpInside];
       return textFieldCell;
     }
+    case ItemTypeChangePasswordButton:
+      cell.selectionStyle = UITableViewCellSelectionStyleDefault;
+      break;
     case ItemTypeWebsite:
     case ItemTypeUsername:
-    case ItemTypeChangePasswordButton:
     case ItemTypeChangePasswordRecommendation:
       break;
   }
