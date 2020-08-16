9bbb79809646fe22bd6641f08726130f4582f4d0
Findit <findit-for-me@appspot.gserviceaccount.com>
2020-08-16 16:20:35 +0000

    Revert "[Files] Add file-type filter buttons in Recents view."
    This reverts commit 4de146084ef772725fb604f4c91921c9ed5c6acc.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 798490 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzRkZTE0NjA4NGVmNzcyNzI1ZmI2MDRmNGM5MTkyMWM5ZWQ1YzZhY2MM
    
    Sample Failed Build: https://ci.chromium.org/b/8871798512306610112
    
    Sample Failed Step: compile
    
    Original change's description:
    > [Files] Add file-type filter buttons in Recents view.
    > 
    > This adds file-type filter buttons (Audio, Images, Videos) above the
    > file list only in the Recents view.
    > Expected behavior:
    >   Users can toggle(enable/disable) file-type filters by clicking it.
    >   Only one filter can be applied at the same time.
    >   Active filter should be cleared when a user leaves Recents view.
    > 
    > Bug: 1114721
    > Test: Manually tested with flag #files-filters-in-recents
    > Change-Id: I50ecd9c1c2f9a587d63e6ae89f88d6b5fe0cba4a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354115
    > Commit-Queue: Naoki Fukino <fukino@chromium.org>
    > Reviewed-by: Noel Gordon <noel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#798490}
    
    
    Change-Id: I78ec51191bd1ce062f1a5cf06a74baf213a915fe
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1114721
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359469
    Cr-Commit-Position: refs/heads/master@{#798494}

--- ui/file_manager/file_manager/foreground/js/file_manager.js
+++ ui/file_manager/file_manager/foreground/js/file_manager.js
@@ -260,9 +260,6 @@ class FileManager extends cr.EventTarget {
     /** @private {?QuickViewController} */
     this.quickViewController_ = null;
 
-    /** @private {?FileTypeFiltersController} */
-    this.fileTypeFiltersController_ = null;
-
     /**
      * Records histograms of directory-changed event.
      * @private {?NavigationUma}
@@ -331,12 +328,6 @@ class FileManager extends cr.EventTarget {
      * @private {?NavigationModelFakeItem}
      */
     this.fakeDriveItem_ = null;
-
-    /**
-     * A fake entry for Recents.
-     * @private {?FakeEntry}
-     */
-    this.recentEntry_ = null;
   }
 
   /**
@@ -1061,10 +1052,6 @@ class FileManager extends cr.EventTarget {
         this.launchParams_.showAndroidPickerApps,
         this.launchParams_.includeAllFiles, this.launchParams_.typeList);
 
-    this.recentEntry_ = new FakeEntry(
-        str('RECENT_ROOT_LABEL'), VolumeManagerCommon.RootType.RECENT,
-        this.getSourceRestriction_());
-
     assert(this.launchParams_);
     this.selectionHandler_ = new FileSelectionHandler(
         assert(this.directoryModel_), assert(this.fileOperationManager_),
@@ -1135,13 +1122,6 @@ class FileManager extends cr.EventTarget {
         this.metadataModel_, this.volumeManager_, this.fileFilter_,
         this.namingController_, this.selectionHandler_, this.launchParams_);
 
-    // Create file-type filter controller.
-    if (util.isRecentsFilterEnabled()) {
-      this.fileTypeFiltersController_ = new FileTypeFiltersController(
-          this.ui_.fileTypeFilterContainer, this.directoryModel_,
-          this.recentEntry_);
-    }
-
     return directoryTreePromise;
   }
 
@@ -1165,7 +1145,10 @@ class FileManager extends cr.EventTarget {
                 !DialogType.isFolderDialog(this.launchParams_.type) ?
             new NavigationModelFakeItem(
                 str('RECENT_ROOT_LABEL'), NavigationModelItemType.RECENT,
-                assert(this.recentEntry_)) :
+                new FakeEntry(
+                    str('RECENT_ROOT_LABEL'),
+                    VolumeManagerCommon.RootType.RECENT,
+                    this.getSourceRestriction_())) :
             null,
         assert(this.directoryModel_), assert(this.androidAppListModel_));
 
