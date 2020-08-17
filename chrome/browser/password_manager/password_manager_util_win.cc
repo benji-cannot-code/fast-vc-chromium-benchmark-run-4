f47653348912e51125896c1e6d9ad32d6049b0f1
OlivierLi <olivierli@chromium.org>
2020-08-17 19:30:25 +0000

    Activate hang watching on the browser UI thread.
    This also introduces the first use of HangWatchScopeDisabled to ignore
    cases where tasks are not processed because an nested loop is processing
    only native events.
    
    Bug: 1034046
    Change-Id: I5f425226d20d3c0f75038f0ea234ae7308bcbc9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340076
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Auto-Submit: Oliver Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798769}

--- chrome/browser/password_manager/password_manager_util_win.cc
+++ chrome/browser/password_manager/password_manager_util_win.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/thread_pool.h"
+#include "base/threading/hang_watcher.h"
 #include "base/threading/scoped_thread_priority.h"
 #include "base/time/time.h"
 #include "base/win/win_util.h"
@@ -516,6 +517,10 @@ bool AuthenticateUserNew(gfx::NativeWindow window,
   cui.pszMessageText = password_prompt.c_str();
   cui.pszCaptionText = product_name.c_str();
   cui.hbmBanner = nullptr;
+
+  // Disable hang watching until the end of the function since the user can take
+  // unbounded time to answer the password prompt. (http://crbug.com/806174)
+  base::HangWatchScopeDisabled disabler;
 
   CredentialBufferValidator validator;
 
