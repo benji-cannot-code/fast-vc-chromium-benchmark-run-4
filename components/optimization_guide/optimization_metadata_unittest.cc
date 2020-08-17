6ef527377835524f5776dce9e36420609c6181b3
Sophie Chang <sophiechang@chromium.org>
2020-08-17 18:34:47 +0000

    Switch DelayAsyncScriptExecution to use the any metadata field
    Bug: 1113287
    Change-Id: I0dd3b14bc9538b71686e9dbac9b258eaec0a2015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2356749
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798728}

--- components/optimization_guide/optimization_metadata_unittest.cc
+++ components/optimization_guide/optimization_metadata_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/optimization_guide/optimization_metadata.h"
 
+#include "components/optimization_guide/proto/delay_async_script_execution_metadata.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace optimization_guide {
