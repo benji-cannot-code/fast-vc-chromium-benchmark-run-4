4f5c2f81672a183c66d71f1caddda38d97fd2a93
Victor Costan <pwnall@chromium.org>
2020-08-17 17:32:33 +0000

    Bookmarks: Switch test to use temporary directory.
    BookmarkExpandedStateTrackerTest passes an empty path into
    BookmarkModel::Load(), which causes the loader to look for a "Bookmarks"
    file in the current directory. Asides from the attached issue, this
    means the test behavior may change if the current directory does happen
    to have a "Bookmarks" file.
    
    This CL adds a base::ScopedTempDir to the test, making it not depend on
    the current directory.
    
    Bug: 1116809
    Change-Id: I18db1ffdbdb2ae29aa17fef8920dde1ba8638f6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359389
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798692}

--- components/bookmarks/browser/bookmark_expanded_state_tracker_unittest.cc
+++ components/bookmarks/browser/bookmark_expanded_state_tracker_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/files/file_path.h"
+#include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
@@ -32,6 +33,7 @@ class BookmarkExpandedStateTrackerTest : public testing::Test {
   void SetUp() override;
   void TearDown() override;
 
+  base::ScopedTempDir scoped_temp_dir_;
   base::test::TaskEnvironment task_environment_;
   TestingPrefServiceSimple prefs_;
   std::unique_ptr<BookmarkModel> model_;
@@ -39,15 +41,17 @@ class BookmarkExpandedStateTrackerTest : public testing::Test {
   DISALLOW_COPY_AND_ASSIGN(BookmarkExpandedStateTrackerTest);
 };
 
-BookmarkExpandedStateTrackerTest::BookmarkExpandedStateTrackerTest() {}
+BookmarkExpandedStateTrackerTest::BookmarkExpandedStateTrackerTest() = default;
 
-BookmarkExpandedStateTrackerTest::~BookmarkExpandedStateTrackerTest() {}
+BookmarkExpandedStateTrackerTest::~BookmarkExpandedStateTrackerTest() = default;
 
 void BookmarkExpandedStateTrackerTest::SetUp() {
+  ASSERT_TRUE(scoped_temp_dir_.CreateUniqueTempDir());
   prefs_.registry()->RegisterListPref(prefs::kBookmarkEditorExpandedNodes);
   prefs_.registry()->RegisterListPref(prefs::kManagedBookmarks);
-  model_.reset(new BookmarkModel(std::make_unique<TestBookmarkClient>()));
-  model_->Load(&prefs_, base::FilePath());
+  model_ =
+      std::make_unique<BookmarkModel>(std::make_unique<TestBookmarkClient>());
+  model_->Load(&prefs_, scoped_temp_dir_.GetPath());
   test::WaitForBookmarkModelToLoad(model_.get());
 }
 
