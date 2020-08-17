19baf64fd209331bf9b107905e84d339de5e27f3
Sophie Chang <sophiechang@chromium.org>
2020-08-17 18:50:06 +0000

    Revert "Add PerfectHeuristics UKM event, and metric for DelayAsyncScriptExecution"
    This reverts commit 647d93e9931af6cb29e15602eae9c320ea0ba5f2.
    
    Reason for revert: crbug/1116821 - causing crashes
    TBR=ryansturm@chromium.org,falken@chromium.org,rkaplow@chromium.org
    
    Original change's description:
    > Add PerfectHeuristics UKM event, and metric for DelayAsyncScriptExecution
    >
    > This CL introduces the PerfectHeuristics UKM event, along with a boolean
    > metric for when a blink::ScriptRunner comes across an async script
    > before the script element's node document has finished parsing. The
    > PerfectHeuristics UKM event is logged in the UkmPageLoadMetricsObserver.
    >
    > See the Google-internal UKM privacy review:
    > https://docs.google.com/document/d/1UqkpGXc4LEh5jzzVR8fCVVTuCuG2mwvhQTuCy36zT3g/edit#
    >
    > See the Perfect Heuristics [1] project document (currently
    > Google-internal).
    >
    > [1]: https://docs.google.com/document/d/1Y7uF76Gq0VlJZky2j7x-lbD1_awH64eW0WRg1bNLuFs/edit
    >
    > R=​falken@chromium.org, nhiroki@chromium.org, ryansturm@chromium.org, sophiechang@chromium.org
    >
    > Bug: 1086227
    > Change-Id: I71954491a1a559c2e1244a543092f1b8f38b8136
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340845
    > Commit-Queue: Dominic Farolino <dom@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#798393}
    
    TBR=falken@chromium.org,rkaplow@chromium.org,nhiroki@chromium.org,sophiechang@chromium.org,ryansturm@chromium.org,dom@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1086227,1116821
    Change-Id: I0131884d123e2122fa674097381293174795fecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359573
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798738}

--- chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.cc
@@ -320,7 +320,6 @@ void UkmPageLoadMetricsObserver::OnComplete(
     RecordInputTimingMetrics();
   }
   ReportLayoutStability();
-  ReportPerfectHeuristicsMetrics();
   ReportAbortMetrics(timing, current_time);
 }
 
@@ -879,15 +878,6 @@ void UkmPageLoadMetricsObserver::ReportLayoutStability() {
   }
 }
 
-void UkmPageLoadMetricsObserver::ReportPerfectHeuristicsMetrics() {
-  ukm::builders::PerfectHeuristics builder(GetDelegate().GetPageUkmSourceId());
-  if (!delay_async_script_execution_before_finished_parsing_seen_)
-    return;
-
-  builder.Setdelay_async_script_execution_before_finished_parsing(1).Record(
-      ukm::UkmRecorder::Get());
-}
-
 void UkmPageLoadMetricsObserver::ReportAbortMetrics(
     const page_load_metrics::mojom::PageLoadTiming& timing,
     base::TimeTicks page_end_time) {
@@ -1154,11 +1144,5 @@ void UkmPageLoadMetricsObserver::OnLoadingBehaviorObserved(
   if (behavior_flag & blink::LoadingBehaviorFlag::
                           kLoadingBehaviorFontPreloadStartedBeforeRendering) {
     font_preload_started_before_rendering_observed_ = true;
-  }
-
-  if (behavior_flag &
-      blink::LoadingBehaviorFlag::
-          kLoadingBehaviorAsyncScriptReadyBeforeDocumentFinishedParsing) {
-    delay_async_script_execution_before_finished_parsing_seen_ = true;
   }
 }
