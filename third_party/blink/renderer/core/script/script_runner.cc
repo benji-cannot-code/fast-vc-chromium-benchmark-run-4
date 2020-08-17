b77f870ae9c7ae2c089a7a1b5d8e098b43657165
Dominic Farolino <dom@chromium.org>
2020-08-17 21:20:11 +0000

    Reland "Add PerfectHeuristics UKM event, and metric for DelayAsyncScriptExecution"
    This reverts commit f97fae2506b9adc569c05af1952ca771e3747a88.
    
    Reason for revert: Fixing the null dereference of Document's loader
    which caused the crash in the first place. This crash was similar to
    what we saw in crbug.com/931330 and fixed with crrev.com/c/1470543.
    
    Original change's description:
    > Revert "Add PerfectHeuristics UKM event, and metric for DelayAsyncScriptExecution"
    > 
    > This reverts commit 647d93e9931af6cb29e15602eae9c320ea0ba5f2.
    > 
    > Reason for revert: crbug/1116821 - causing crashes
    > TBR=ryansturm@chromium.org,falken@chromium.org,rkaplow@chromium.org
    > 
    > Original change's description:
    > > Add PerfectHeuristics UKM event, and metric for DelayAsyncScriptExecution
    > >
    > > This CL introduces the PerfectHeuristics UKM event, along with a boolean
    > > metric for when a blink::ScriptRunner comes across an async script
    > > before the script element's node document has finished parsing. The
    > > PerfectHeuristics UKM event is logged in the UkmPageLoadMetricsObserver.
    > >
    > > See the Google-internal UKM privacy review:
    > > https://docs.google.com/document/d/1UqkpGXc4LEh5jzzVR8fCVVTuCuG2mwvhQTuCy36zT3g/edit#
    > >
    > > See the Perfect Heuristics [1] project document (currently
    > > Google-internal).
    > >
    > > [1]: https://docs.google.com/document/d/1Y7uF76Gq0VlJZky2j7x-lbD1_awH64eW0WRg1bNLuFs/edit
    > >
    > > R=​falken@chromium.org, nhiroki@chromium.org, ryansturm@chromium.org, sophiechang@chromium.org
    > >
    > > Bug: 1086227
    > > Change-Id: I71954491a1a559c2e1244a543092f1b8f38b8136
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340845
    > > Commit-Queue: Dominic Farolino <dom@chromium.org>
    > > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > > Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    > > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#798393}
    > 
    > TBR=falken@chromium.org,rkaplow@chromium.org,nhiroki@chromium.org,sophiechang@chromium.org,ryansturm@chromium.org,dom@chromium.org
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: 1086227,1116821
    > Change-Id: I0131884d123e2122fa674097381293174795fecf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359573
    > Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#798738}
    
    TBR=falken@chromium.org,rkaplow@chromium.org,nhiroki@chromium.org,sophiechang@chromium.org,ryansturm@chromium.org,dom@chromium.org
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1086227
    Bug: 1116821
    Change-Id: I7722da2c17dc34952ea9155d72840708b4eaa5b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359742
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798832}

--- third_party/blink/renderer/core/script/script_runner.cc
+++ third_party/blink/renderer/core/script/script_runner.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/script/script_loader.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
@@ -148,6 +149,12 @@ bool ScriptRunner::CanDelayAsyncScripts() {
           DelayAsyncScriptExecutionUntilFinishedParsingEnabled() ||
       RuntimeEnabledFeatures::
           DelayAsyncScriptExecutionUntilFirstPaintOrFinishedParsingEnabled();
+  // The document's loader can be null here, e.g., if the frame is being
+  // detached.
+  if (!document_->Parsing() && document_->Loader()) {
+    document_->Loader()->DidObserveLoadingBehavior(
+        kLoadingBehaviorAsyncScriptReadyBeforeDocumentFinishedParsing);
+  }
   return !delay_async_script_milestone_reached_ && flags_enabled;
 }
 
