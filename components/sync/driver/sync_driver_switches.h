1da54f17bce5d4698a966103957c6a3bc2c0aa8b
Maksim Moskvitin <mmoskvitin@google.com>
2020-08-17 11:18:10 +0000

    [TrustedVault] Implement device registration logic
    This CL adds high-level device registration logic for trusted vault
    together with required interfaces/skeletons.
    
    Device has to be registered in order to silently follow further key
    rotations in the future. Device registration means generation of device
    key pair and sending public key to the server. Most recent vault key
    must be locally available in order to register the device. Currently,
    device registration is supported only for active sync account.
    
    Bug: 1102340
    Change-Id: If7f6034db56752622bd108598a80c3e19681ba17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339484
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#798583}

--- components/sync/driver/sync_driver_switches.h
+++ components/sync/driver/sync_driver_switches.h
@@ -32,6 +32,7 @@ extern const base::Feature
 extern const base::Feature kSyncWifiConfigurations;
 extern const base::Feature kSyncDeviceInfoInTransportMode;
 extern const base::Feature kDecoupleSyncFromAndroidMasterSync;
+extern const base::Feature kFollowTrustedVaultKeyRotation;
 
 }  // namespace switches
 
--- components/sync/trusted_vault/standalone_trusted_vault_client.cc
+++ components/sync/trusted_vault/standalone_trusted_vault_client.cc
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_runner_util.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/sync/trusted_vault/standalone_trusted_vault_backend.h"
+#include "components/sync/trusted_vault/trusted_vault_access_token_fetcher.h"
+#include "components/sync/trusted_vault/trusted_vault_connection_impl.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
 
 namespace syncer {
 
@@ -97,11 +100,22 @@ void StandaloneTrustedVaultClient::TriggerLazyInitializationIfNeeded() {
     return;
   }
 
-  backend_ = base::MakeRefCounted<StandaloneTrustedVaultBackend>(file_path_);
+  // TODO(crbug.com/1113597): populate TrustedVaultAccessTokenFetcher into
+  // TrustedVaultConnectionImpl ctor.
+  // TODO(crbug.com/1113598): populate URLLoaderFactory into
+  // TrustedVaultConnectionImpl ctor.
+  // TODO(crbug.com/1102340): allow setting custom TrustedVaultConnection for
+  // testing.
+  backend_ = base::MakeRefCounted<StandaloneTrustedVaultBackend>(
+      file_path_,
+      std::make_unique<TrustedVaultConnectionImpl>(
+          /*url_loader_factory=*/nullptr, /*access_token_fetcher=*/nullptr));
   backend_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&StandaloneTrustedVaultBackend::ReadDataFromDisk,
                      backend_));
+  // TODO(crbug.com/1113597): populate current syncing account to |backend_|
+  // here and upon syncing account change.
 }
 
 bool StandaloneTrustedVaultClient::IsInitializationTriggeredForTesting() const {
