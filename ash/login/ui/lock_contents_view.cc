1be365dcd1e66dd6d7c77cd9a17fb4124545cd31
Renato Silva <rrsilva@google.com>
2020-08-17 19:42:43 +0000

    CrOS - Login Screen - Minor cleanup
    Rename AnimationState to UiState and use it to determine the
    visibility of elements. Remove multiple variables in favor of
    UiState.
    
    Bug: 1075994
    Change-Id: I0d7413fb173dadbff22cde0eb3af6f43586bdee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352883
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798780}

--- ash/login/ui/lock_contents_view.cc
+++ ash/login/ui/lock_contents_view.cc
@@ -1775,7 +1775,7 @@ void LockContentsView::LayoutAuth(LoginBigUserView* to_update,
   DCHECK(to_update);
 
   auto capture_animation_state_pre_layout = [&](LoginBigUserView* view) {
-    if (!animate || !view)
+    if (!view)
       return;
     if (view->auth_user())
       view->auth_user()->CaptureStateForAnimationPreLayout();
@@ -1830,10 +1830,10 @@ void LockContentsView::LayoutAuth(LoginBigUserView* to_update,
   };
 
   auto apply_animation_post_layout = [&](LoginBigUserView* view) {
-    if (!animate || !view)
+    if (!view)
       return;
     if (view->auth_user())
-      view->auth_user()->ApplyAnimationPostLayout();
+      view->auth_user()->ApplyAnimationPostLayout(animate);
   };
 
   // The high-level layout flow:
