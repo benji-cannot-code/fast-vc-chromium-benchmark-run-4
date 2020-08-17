92d16ba54f745c07c49ad0c842c14302ecf40cb9
David Roger <droger@chromium.org>
2020-08-17 13:00:37 +0000

    Revert "Reland "Change HeapObserverList to HeapObserverSet""
    This reverts commit 944fde321c86d9cdc9d9dfeb27519d8d4f646466.
    
    Reason for revert: strongly suspecting this breaks All/DeclarativeNetRequestResourceTypeBrowserTest
    See https://crbug.com/1116995
    
    Original change's description:
    > Reland "Change HeapObserverList to HeapObserverSet"
    > 
    > This is a reland of f29d2ae1e409a8b4596d5f3d2a707a0dbf790727
    > 
    > ExecutionContext::NotifyContextDestroyed() has relied on the
    > deterministic iteration order of ObserverList, so landing the original
    > CL caused a problem. It is fixed in another CL:
    > https://crrev.com/c/2352588
    > 
    > Original change's description:
    > > Change HeapObserverList to HeapObserverSet
    > >
    > > The previous CL for HeapObserverList https://crrev.com/c/2060348
    > > addressed two things: changing HeapObserverList to HeapObserverSet and
    > > allowing removal while iteration, and was reverted for performance
    > > reasons. This CL only does the former and avoids performance
    > > implications of the latter.
    > >
    > > This CL is for migration of NewLinkedHashSet.
    > > See: https://crrev.com/c/2134039
    > >
    > > Bug: 1100257
    > > Change-Id: I2426d14d77a68f200bf1398b16638e1605dd4ff1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2319087
    > > Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    > > Reviewed-by: Keishi Hattori <keishi@chromium.org>
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#797115}
    > 
    > Bug: 1100257, 1115411
    > Change-Id: Id8e1b9a4141777de89bbc471f6ac8c6508a0adff
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352292
    > Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    > Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    > Reviewed-by: Keishi Hattori <keishi@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#798576}
    
    TBR=haraken@chromium.org,keishi@chromium.org,yukiy@chromium.org,bartekn@chromium.org
    
    Change-Id: I8c3d3a159efb802cda84fe3cbb1779abbbe2670d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1100257
    Bug: 1115411
    Bug: 1116995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359036
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798604}

--- third_party/blink/renderer/platform/context_lifecycle_observer.h
+++ third_party/blink/renderer/platform/context_lifecycle_observer.h
@@ -19,7 +19,7 @@ class PLATFORM_EXPORT ContextLifecycleObserver : public GarbageCollectedMixin {
   virtual void ContextDestroyed() = 0;
 
   // Call before clearing an observer list.
-  void ObserverSetWillBeCleared();
+  void ObserverListWillBeCleared();
 
   ContextLifecycleNotifier* GetContextLifecycleNotifier() const {
     return notifier_;
