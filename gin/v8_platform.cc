f27465776fa34842a28075d2296af503d48575ce
Etienne Pierre-doray <etiennep@chromium.org>
2020-08-17 15:50:26 +0000

    [Jobs API]: Implement missing Jobs pieces in gin.
    Forwarding to base:: implementation:
    - JobHandle::IsCompleted()
    - JobDelegate::GetTaskId()
    - worker_count passed as argument to GetMaxConcurrency().
    
    Bug: 1114823
    Change-Id: I9348a6171c19a39081205a33fbb5e19179a4601a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354163
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798654}

--- gin/v8_platform.cc
+++ gin/v8_platform.cc
@@ -284,6 +284,7 @@ class JobDelegateImpl : public v8::JobDelegate {
   void NotifyConcurrencyIncrease() override {
     delegate_->NotifyConcurrencyIncrease();
   }
+  uint8_t GetTaskId() override { return delegate_->GetTaskId(); }
 
  private:
   base::JobDelegate* delegate_;
@@ -304,7 +305,7 @@ class JobHandleImpl : public v8::JobHandle {
   }
   void Join() override { handle_.Join(); }
   void Cancel() override { handle_.Cancel(); }
-
+  bool IsCompleted() override { return handle_.IsCompleted(); }
   bool IsRunning() override { return !!handle_; }
 
  private:
@@ -508,8 +509,8 @@ std::unique_ptr<v8::JobHandle> V8Platform::PostJob(
                         },
                         base::Unretained(job_task.get())),
                     base::BindRepeating(
-                        [](v8::JobTask* job_task, size_t /*worker_count*/) {
-                          return job_task->GetMaxConcurrency();
+                        [](v8::JobTask* job_task, size_t worker_count) {
+                          return job_task->GetMaxConcurrency(worker_count);
                         },
                         base::Unretained(job_task.get())));
 
