5fb9a32110be75a3a684d2ca65a15c45d35b467f
Jose Dapena Paz <jdapena@igalia.com>
2020-08-17 16:53:56 +0000

    IWYU: missing <memory> include in payments android app description for std::unique_ptr
    This fixes this build error:
    ./../components/payments/core/android_app_description.h:51:20: error: ‘unique_ptr’ is not a member of ‘std’
       51 |   std::vector<std::unique_ptr<AndroidActivityDescription>> activities;
          |                    ^~~~~~~~~~
    
    Bug: 957519
    Change-Id: I63cd3c3caa743c5b8a8ec0d91706d8655a1bbea6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360080
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#798673}

--- components/payments/core/android_app_description.h
+++ components/payments/core/android_app_description.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PAYMENTS_CORE_ANDROID_APP_DESCRIPTION_H_
 #define COMPONENTS_PAYMENTS_CORE_ANDROID_APP_DESCRIPTION_H_
 
+#include <memory>
 #include <string>
 #include <vector>
 
