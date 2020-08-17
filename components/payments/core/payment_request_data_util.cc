426e91154bc496896cbf13a3854435cc7be2a101
Rouslan Solomakhin <rouslan@chromium.org>
2020-08-17 12:45:35 +0000

    [Web Payment] Query IS_READY_TO_PAY service.
    Before this patch, all TWA payment apps on Chrome OS were available for
    payment, even if they implemented an IS_READY_TO_PAY service that
    replied "false" to the query (when
    chrome://flags/#enable-web-payments-experimental-features flag was
    enabled).
    
    This patch queries the IS_READY_TO_PAY service of the TWA payment app on
    Chrome OS and enables payments only if the service is absent or returns
    "true".
    
    After this patch, the TWA payment app can control at runtime whether
    it's able to handle payments.
    
    Bug: 1061503
    Change-Id: Idb267c0bbe2d8bb9173bcfde1b9a25e47a7fc505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2269744
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798600}

--- components/payments/core/payment_request_data_util.cc
+++ components/payments/core/payment_request_data_util.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/payments/core/payment_request_data_util.h"
 
-#include <memory>
+#include <utility>
 
 #include "base/stl_util.h"
 #include "base/strings/string_split.h"
@@ -167,13 +167,15 @@ base::string16 FormatCardNumberForDisplay(const base::string16& card_number) {
   return number;
 }
 
-std::map<std::string, std::set<std::string>> FilterStringifiedMethodData(
+std::unique_ptr<std::map<std::string, std::set<std::string>>>
+FilterStringifiedMethodData(
     const std::map<std::string, std::set<std::string>>& stringified_method_data,
     const std::set<std::string>& supported_payment_method_names) {
-  std::map<std::string, std::set<std::string>> result;
+  auto result =
+      std::make_unique<std::map<std::string, std::set<std::string>>>();
   for (const auto& pair : stringified_method_data) {
     if (base::Contains(supported_payment_method_names, pair.first)) {
-      result[pair.first] = pair.second;
+      result->insert({pair.first, pair.second});
     }
   }
   return result;
--- components/payments/core/payment_request_data_util.h
+++ components/payments/core/payment_request_data_util.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PAYMENTS_CORE_PAYMENT_REQUEST_DATA_UTIL_H_
 #define COMPONENTS_PAYMENTS_CORE_PAYMENT_REQUEST_DATA_UTIL_H_
 
+#include <memory>
 #include <set>
 #include <string>
 #include <vector>
@@ -78,7 +79,8 @@ base::string16 FormatCardNumberForDisplay(const base::string16& card_number);
 // Value: The set of all payment method specific parameters for the given
 //        payment method identifier, each one serialized into a JSON string,
 //        e.g., '{"key": "value"}'.
-std::map<std::string, std::set<std::string>> FilterStringifiedMethodData(
+std::unique_ptr<std::map<std::string, std::set<std::string>>>
+FilterStringifiedMethodData(
     const std::map<std::string, std::set<std::string>>& stringified_method_data,
     const std::set<std::string>& supported_payment_method_names);
 
