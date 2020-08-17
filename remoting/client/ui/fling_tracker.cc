abd438f7ca348c6b98a8edd24a271f0ed8a83ef5
Peter Kasting <pkasting@chromium.org>
2020-08-17 18:53:30 +0000

    Misc. cleanup.
    * Inline temps
    * constexpr
    * Format
    * Shorten code
    * Avoid unnecessary precision loss
    * Improve naming
    * Narrow scope
    
    Bug: none
    Change-Id: I457b636ca926654fb01e34f2d201e547d9070265
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359005
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798745}

--- remoting/client/ui/fling_tracker.cc
+++ remoting/client/ui/fling_tracker.cc
@@ -91,7 +91,7 @@ bool FlingTracker::TrackMovement(base::TimeDelta time_elapsed,
     return false;
   }
 
-  float time_elapsed_ms = time_elapsed.InMilliseconds();
+  float time_elapsed_ms = time_elapsed.InMillisecondsF();
 
   if (time_elapsed_ms > fling_duration_) {
     StopFling();
