6fad0793871ace2d0f6734a8731a7ded6499e5c9
Thomas Anderson <thomasanderson@chromium.org>
2020-08-17 22:58:30 +0000

    Revert "Reland "LoginScreen: Show TPM lock status.""
    This reverts commit 065ade05b5758d826482588cffb880ccdd274a86.
    
    Reason for revert: Suspected cause of All/OobeInteractiveUITest.SimpleEndToEnd/9 failure on Builder linux-chromeos-rel Build 40523:
    
    https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-rel/40523
    
    Original change's description:
    > Reland "LoginScreen: Show TPM lock status."
    > 
    > This is a reland of 67a4df6f9ae3bc92cd217e4759f60a2b5ddb68df
    > CL was reverted cause of failing
    > chromeos-betty-pi-arc-cfi-thin-lto-chrome due to reaching deadline
    > on one of the tests. This is gonna be fixed by increasing time limit.
    > See details here: https://crbug.com/1116379
    > 
    > Original change's description:
    > > LoginScreen: Show TPM lock status.
    > >
    > > If TPM is locked show banner with steps on how to fix this issue.
    > >
    > > Bug: 1114656
    > > Change-Id: I85dab0f4ce003ac3dad61c318efb3333457993ac
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2346266
    > > Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    > > Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#797992}
    > 
    > Bug: 1114656
    > Change-Id: Iba50daf1d1b892539e00d9be04fc2b8c0a5a279e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359992
    > Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    > Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    > Cr-Commit-Position: refs/heads/master@{#798698}
    
    TBR=tbarzic@chromium.org,antrim@chromium.org,raleksandrov@google.com
    
    Change-Id: I55fca0fb2f7fdbdacf1de9d7122571a888b89d11
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1114656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2358906
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798867}

--- chrome/browser/chromeos/login/screens/user_selection_screen.h
+++ chrome/browser/chromeos/login/screens/user_selection_screen.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_LOGIN_SCREENS_USER_SELECTION_SCREEN_H_
 
 #include <map>
-#include <memory>
 #include <string>
 #include <vector>
 
@@ -15,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/session/user_info.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
-#include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/login/screens/base_screen.h"
@@ -23,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/ui/login_display.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chromeos/components/proximity_auth/screenlock_bridge.h"
-#include "chromeos/dbus/cryptohome/rpc.pb.h"
 #include "components/account_id/account_id.h"
 #include "components/session_manager/core/session_manager_observer.h"
 #include "components/user_manager/user.h"
@@ -79,8 +76,6 @@ class UserSelectionScreen
   void OnUserActivity(const ui::Event* event) override;
 
   void InitEasyUnlock();
-
-  void SetTpmLockedState(bool is_locked, base::TimeDelta time_left);
 
   // proximity_auth::ScreenlockBridge::LockHandler implementation:
   void ShowBannerMessage(const base::string16& message,
@@ -150,7 +145,6 @@ class UserSelectionScreen
 
  private:
   class DircryptoMigrationChecker;
-  class TpmLockedChecker;
 
   EasyUnlockService* GetEasyUnlockServiceForUser(
       const AccountId& account_id) const;
@@ -179,9 +173,6 @@ class UserSelectionScreen
 
   // Helper to check whether a user needs dircrypto migration.
   std::unique_ptr<DircryptoMigrationChecker> dircrypto_migration_checker_;
-
-  // Helper to check whether TPM is locked or not.
-  std::unique_ptr<TpmLockedChecker> tpm_locked_checker_;
 
   user_manager::UserList users_to_send_;
 
