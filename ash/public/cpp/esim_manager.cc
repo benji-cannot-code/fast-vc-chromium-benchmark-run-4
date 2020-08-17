b2bdd8eb911fd74d72e39ff489ea2d934e50c984
Azeem Arshad <azeemarshad@chromium.org>
2020-08-17 17:22:38 +0000

    [CrOS Cellular] Add ESimManager mojo interface
    This CL adds ESimManager class and mojo interface.
    ESimManager communicates with Hermes, the ChromeOS
    eSIM LPA daemon, to provide eSIM management methods
    over mojo. See http://go/cros-esim-design-doc for
    details.
    
    An in-process instance of ESimManager is owned in Ash
    in the same way as network_config service.
    
    Bug: 1093185
    Change-Id: I444ae644d020c7df7eea68025d1cb479cdec7c11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315925
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798685}

---
+++ ash/public/cpp/esim_manager.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/esim_manager.h"
+
+#include "chromeos/services/cellular_setup/in_process_esim_manager.h"
+
+namespace ash {
+
+void GetESimManager(
+    mojo::PendingReceiver<chromeos::cellular_setup::mojom::ESimManager>
+        receiver) {
+  chromeos::cellular_setup::BindToInProcessESimManager(std::move(receiver));
+}
+
+}  // namespace ash
