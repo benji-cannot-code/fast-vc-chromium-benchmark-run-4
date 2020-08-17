33f2550b762be12d26195121bed62d7627b68d7d
Renato Silva <rrsilva@google.com>
2020-08-17 19:07:03 +0000

    CrOS - PIN Auto submit - Add LoginPinInputView
    LoginPinInputView is the view that will be used for PIN auto submit.
    It contains a child view - LoginPinInput - that is created anew as
    the length of the pin input field changes for different users.
    
    Minor modifications to FixedLengthCodeInput were made to enable code
    reuse. Unit tests will be adeed once the view becomes embedded in
    LoginAuthUserView in a upcoming CL.
    
    Bug: 1075994
    Change-Id: Ia451a2eeabdede24c29667eecc883d213fc4288f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2226308
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798756}

--- ash/login/ui/access_code_input.cc
+++ ash/login/ui/access_code_input.cc
@@ -194,7 +194,7 @@ FixedLengthCodeInput::FixedLengthCodeInput(int length,
   DCHECK_LT(0, length);
   DCHECK(on_input_change_);
 
-  SetLayoutManager(std::make_unique<views::BoxLayout>(
+  auto* layout = SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kHorizontal, gfx::Insets(),
       kAccessCodeBetweenInputFieldsGapDp));
   SetGroup(kFixedLengthInputGroup);
@@ -226,6 +226,7 @@ FixedLengthCodeInput::FixedLengthCodeInput(int length,
     field->GetViewAccessibility().OverrideIsIgnored(true);
     input_fields_.push_back(field);
     AddChildView(field);
+    layout->SetFlexForView(field, 1);
   }
 
   text_value_for_a11y_ = std::string(length, ' ');
@@ -250,6 +251,10 @@ void FixedLengthCodeInput::InsertDigit(int value) {
 // Clears input from the |active_field_|. If |active_field| is empty moves
 // focus to the previous field (if exists) and clears input there.
 void FixedLengthCodeInput::Backspace() {
+  // Ignore backspace on the first field, if empty.
+  if (IsFirstFieldActive() && ActiveInput().empty())
+    return;
+
   if (ActiveInput().empty()) {
     FocusPreviousField();
   }
@@ -347,10 +352,10 @@ bool FixedLengthCodeInput::HandleKeyEvent(views::Textfield* sender,
     InsertDigit(key_code - ui::VKEY_0);
   } else if (key_code >= ui::VKEY_NUMPAD0 && key_code <= ui::VKEY_NUMPAD9) {
     InsertDigit(key_code - ui::VKEY_NUMPAD0);
-  } else if (key_code == ui::VKEY_LEFT) {
+  } else if (key_code == ui::VKEY_LEFT && arrow_navigation_allowed_) {
     FocusPreviousField();
     NotifyAccessibilityEvent(ax::mojom::Event::kTextSelectionChanged, true);
-  } else if (key_code == ui::VKEY_RIGHT) {
+  } else if (key_code == ui::VKEY_RIGHT && arrow_navigation_allowed_) {
     // Do not allow to leave empty field when moving focus with arrow key.
     if (!ActiveInput().empty()) {
       FocusNextField();
@@ -408,11 +413,30 @@ bool FixedLengthCodeInput::HandleGestureEvent(
 }
 
 void FixedLengthCodeInput::SetInputEnabled(bool input_enabled) {
-  NOTIMPLEMENTED();
+  for (auto* field : input_fields_) {
+    field->SetEnabled(input_enabled);
+  }
 }
 
 void FixedLengthCodeInput::ClearInput() {
-  NOTIMPLEMENTED();
+  for (auto* field : input_fields_) {
+    field->SetText(base::string16());
+  }
+  active_input_index_ = 0;
+  text_value_for_a11y_.clear();
+  ActiveField()->RequestFocus();
+}
+
+bool FixedLengthCodeInput::IsEmpty() const {
+  for (auto* field : input_fields_) {
+    if (field->GetText().length())
+      return false;
+  }
+  return true;
+}
+
+void FixedLengthCodeInput::SetAllowArrowNavigation(bool allowed) {
+  arrow_navigation_allowed_ = allowed;
 }
 
 void FixedLengthCodeInput::FocusPreviousField() {
@@ -433,6 +457,10 @@ void FixedLengthCodeInput::FocusNextField() {
 
 bool FixedLengthCodeInput::IsLastFieldActive() const {
   return active_input_index_ == (static_cast<int>(input_fields_.size()) - 1);
+}
+
+bool FixedLengthCodeInput::IsFirstFieldActive() const {
+  return active_input_index_ == 0;
 }
 
 AccessibleInputField* FixedLengthCodeInput::ActiveField() const {
