4bbc386a26273b5e8fde2585c8bd452c3a73bf17
Prashant Nevase <prashant.n@samsung.com>
2020-08-17 04:22:19 +0000

    Remove dark mode element roles added for image.
    There is no need to treat raster images differently. The element role
    added for images were used for deciding initial classification which is
    now moved to DarkModeFilter::AnalyzeShouldApplyToImage(), which should
    be called before calling ApplyToImageFlagsIfNeeded(). The functions
    added in Image class which were used for deciding element role for
    svg image container and gradient generated image are removed.
    
    Along with skipping filter to images drawn into larger rects, this
    patch adds logic for considering images for applying dark mode filter
    which have very smaller source rect. The analysis based on |src| and
    |dst| rects is extended for all kinds of images. With this rastered
    content from SVG images and gradient generated images undergo
    classification.
    
    Bug: 1094005
    Change-Id: I6d24c770e2c703ddccfb70c00c0a5c9c51507498
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352251
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Prashant Nevase <prashant.n@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#798531}

--- third_party/blink/renderer/core/svg/graphics/svg_image_for_container.h
+++ third_party/blink/renderer/core/svg/graphics/svg_image_for_container.h
@@ -70,8 +70,6 @@ class SVGImageForContainer final : public Image {
         image, container_size_without_zoom, zoom, url));
   }
 
-  bool IsSVGImageForContainer() const override { return true; }
-
   IntSize Size() const override;
   FloatSize SizeAsFloat(RespectImageOrientationEnum) const override;
 
--- third_party/blink/renderer/platform/graphics/dark_mode_image_classifier.cc
+++ third_party/blink/renderer/platform/graphics/dark_mode_image_classifier.cc
@@ -38,37 +38,6 @@ const int kMaxSampledPixels = 1000;
 const int kMaxBlocks = 10;
 const float kMinOpaquePixelPercentageForForeground = 0.2;
 
-const int kMinImageSizeForClassification1D = 24;
-const int kMaxImageSizeForClassification1D = 100;
-
-class DarkModeBitmapImageClassifier : public DarkModeImageClassifier {
-  DarkModeClassification DoInitialClassification(const SkRect& dst) override {
-    if (dst.width() < kMinImageSizeForClassification1D ||
-        dst.height() < kMinImageSizeForClassification1D)
-      return DarkModeClassification::kApplyFilter;
-
-    if (dst.width() > kMaxImageSizeForClassification1D ||
-        dst.height() > kMaxImageSizeForClassification1D) {
-      return DarkModeClassification::kDoNotApplyFilter;
-    }
-
-    return DarkModeClassification::kNotClassified;
-  }
-};
-
-class DarkModeSVGImageClassifier : public DarkModeImageClassifier {
-  DarkModeClassification DoInitialClassification(const SkRect& dst) override {
-    return DarkModeClassification::kNotClassified;
-  }
-};
-
-class DarkModeGradientGeneratedImageClassifier
-    : public DarkModeImageClassifier {
-  DarkModeClassification DoInitialClassification(const SkRect& dst) override {
-    return DarkModeClassification::kApplyFilter;
-  }
-};
-
 // DarkModeImageClassificationCache - Implements classification caches for
 // different paint image ids. The classification result for the given |src|
 // rect is added to cache identified by |image_id| and result for the same
@@ -138,21 +107,6 @@ DarkModeImageClassifier::DarkModeImageClassifier() = default;
 
 DarkModeImageClassifier::~DarkModeImageClassifier() = default;
 
-std::unique_ptr<DarkModeImageClassifier>
-DarkModeImageClassifier::MakeBitmapImageClassifier() {
-  return std::make_unique<DarkModeBitmapImageClassifier>();
-}
-
-std::unique_ptr<DarkModeImageClassifier>
-DarkModeImageClassifier::MakeSVGImageClassifier() {
-  return std::make_unique<DarkModeSVGImageClassifier>();
-}
-
-std::unique_ptr<DarkModeImageClassifier>
-DarkModeImageClassifier::MakeGradientGeneratedImageClassifier() {
-  return std::make_unique<DarkModeGradientGeneratedImageClassifier>();
-}
-
 DarkModeClassification DarkModeImageClassifier::Classify(
     const PaintImage& paint_image,
     const SkRect& src,
@@ -164,7 +118,6 @@ DarkModeClassification DarkModeImageClassifier::Classify(
   if (result != DarkModeClassification::kNotClassified)
     return result;
 
-  result = DoInitialClassification(dst);
   if (result != DarkModeClassification::kNotClassified) {
     cache->Add(image_id, src, result);
     return result;
