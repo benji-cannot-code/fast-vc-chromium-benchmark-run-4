359f30d45ae57ea99299739d6bc109cadc8f476b
Becca Hughes <beccahughes@chromium.org>
2020-08-17 22:51:45 +0000

    Setup mojo for Kaleidoscope NTP module
    Adds a dedicated KaleidoscopeNTPDataProvider that
    provides the methods that are needed by the NTP.
    
    BUG=1114862
    
    Change-Id: I29c182f5c032f8dc0553bbe14b405f228942d3eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2357521
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#798863}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -105,6 +105,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/accessibility/caption_host_impl.h"
 #include "chrome/browser/badging/badge_manager.h"
 #include "chrome/browser/media/feeds/media_feeds_store.mojom.h"
+#include "chrome/browser/media/kaleidoscope/kaleidoscope_data_provider_impl.h"
 #include "chrome/browser/media/kaleidoscope/kaleidoscope_ui.h"
 #include "chrome/browser/media/kaleidoscope/mojom/kaleidoscope.mojom.h"
 #include "chrome/browser/payments/payment_request_factory.h"
@@ -122,6 +123,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/tab_search/tab_search.mojom.h"
 #include "chrome/browser/ui/webui/tab_search/tab_search_ui.h"
 #include "chrome/common/caption.mojom.h"
+#include "chrome/common/webui_url_constants.h"
 #include "media/mojo/mojom/speech_recognition_service.mojom.h"
 #endif
 
@@ -640,7 +642,7 @@ void PopulateChromeWebUIFrameBinders(
 
 #if !defined(OS_ANDROID)
   RegisterWebUIControllerInterfaceBinder<media::mojom::KaleidoscopeDataProvider,
-                                         KaleidoscopeUI>(map);
+                                         KaleidoscopeUI, NewTabPageUI>(map);
 #endif  // !defined(OS_ANDROID)
 
 #if defined(OS_CHROMEOS)
