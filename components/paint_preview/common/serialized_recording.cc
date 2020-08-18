034e49a9068daee190d339139a359c66ac60d257
Michael Tang <michael.tang@microsoft.com>
2020-08-18 13:29:16 +0000

    [Paint Preview] Add methods to PaintPreviewCompositor to do root frame composition
    Change-Id: I94d7526807c7204ef13f0ad42a5c95499211c926
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2334005
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799079}

--- components/paint_preview/common/serialized_recording.cc
+++ components/paint_preview/common/serialized_recording.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/notreached.h"
 #include "base/optional.h"
+#include "base/trace_event/common/trace_event_common.h"
+#include "base/trace_event/trace_event.h"
 #include "components/paint_preview/common/file_stream.h"
 #include "components/paint_preview/common/paint_preview_tracker.h"
 #include "components/paint_preview/common/serial_utils.h"
@@ -49,9 +51,7 @@ SerializedRecording::SerializedRecording(base::FilePath path)
 SerializedRecording::SerializedRecording(base::File file)
     : persistence_(RecordingPersistence::kFileSystem),
       file_(std::move(file)),
-      buffer_() {
-  DCHECK(file_.IsValid());
-}
+      buffer_() {}
 
 SerializedRecording::SerializedRecording(mojo_base::BigBuffer buffer)
     : persistence_(RecordingPersistence::kMemoryBuffer),
@@ -77,6 +77,7 @@ bool SerializedRecording::IsValid() const {
 }
 
 base::Optional<SkpResult> SerializedRecording::Deserialize() && {
+  TRACE_EVENT0("paint_preview", "SerializedRecording::Deserialize");
   SkpResult result;
   SkDeserialProcs procs = MakeDeserialProcs(&result.ctx);
 
@@ -94,6 +95,26 @@ base::Optional<SkpResult> SerializedRecording::Deserialize() && {
   }
 
   return {std::move(result)};
+}
+
+sk_sp<SkPicture> SerializedRecording::DeserializeWithContext(
+    LoadedFramesDeserialContext* ctx) && {
+  TRACE_EVENT0("paint_preview", "SerializedRecording::DeserializeWithContext");
+
+  SkDeserialProcs procs = MakeDeserialProcs(ctx);
+
+  if (is_file()) {
+    FileRStream stream(std::move(file_));
+    return SkPicture::MakeFromStream(&stream, &procs);
+  } else if (is_buffer()) {
+    CHECK(buffer_.has_value());
+    SkMemoryStream stream(buffer_->data(), buffer_->size(),
+                          /*copyData=*/false);
+    return SkPicture::MakeFromStream(&stream, &procs);
+  } else {
+    NOTREACHED();
+    return nullptr;
+  }
 }
 
 bool RecordToFile(base::File file,
