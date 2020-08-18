12b2cb3a3f5224c3f5482f75555da64461135823
Dan Harrington <harringtond@chromium.org>
2020-08-18 22:41:05 +0000

    add histogram ContentSuggestions.Feed.ImageFetchStatus
    Bug: 1044139
    Change-Id: I31fea9098e29e0c0bc700a4ab646c402e55bf003
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2357621
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799365}

--- components/feed/core/v2/image_fetcher.cc
+++ components/feed/core/v2/image_fetcher.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/feed/core/v2/image_fetcher.h"
 
+#include "components/feed/core/v2/metrics_reporter.h"
 #include "components/feed/core/v2/public/types.h"
 #include "net/http/http_request_headers.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -58,6 +59,9 @@ void ImageFetcher::OnFetchComplete(
     std::unique_ptr<network::SimpleURLLoader> simple_loader,
     ImageCallback callback,
     std::unique_ptr<std::string> response_data) {
+  MetricsReporter::OnImageFetched(
+      response_data ? simple_loader->ResponseInfo()->headers->response_code()
+                    : simple_loader->NetError());
   NetworkResponse response{std::string(), simple_loader->NetError()};
   if (simple_loader->ResponseInfo() && simple_loader->ResponseInfo()->headers) {
     response.status_code =
