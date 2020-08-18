78db6269d129f8f53d343d4ed06edc19d55affee
Becca Hughes <beccahughes@chromium.org>
2020-08-18 23:00:06 +0000

    Public part of internal CL
    Makes sure we don't load non-JS module code
    as modules and also updates the NTP so it
    can embed chrome-untrusted://kaleidoscope.
    
    Change-Id: I8027daec3a31bdc42d22fb40c1d733b7f3dd6cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360475
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799373}

--- chrome/browser/media/kaleidoscope/kaleidoscope_ui.h
+++ chrome/browser/media/kaleidoscope/kaleidoscope_ui.h
@@ -26,6 +26,8 @@ class KaleidoscopeUI : public ui::MojoWebUIController {
 
   static content::WebUIDataSource* CreateWebUIDataSource();
 
+  static content::WebUIDataSource* CreateUntrustedWebUIDataSource();
+
  private:
   std::unique_ptr<media::mojom::KaleidoscopeDataProvider> provider_;
 
