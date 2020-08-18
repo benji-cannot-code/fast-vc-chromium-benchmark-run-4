e037152bd9922c8fd1550d00ed6e10568acf287d
Evan Stade <estade@chromium.org>
2020-08-18 16:44:03 +0000

    Remove some //chrome dependencies from Contacts Picker
    This is in preparation for componentization of the contacts picker.
    
    Bug: 1016938
    Change-Id: Iba35bbd752087f17c5636fb10706f029a2522da7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2321563
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799135}

--- chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/PickerCategoryView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/PickerCategoryView.java
@@ -22,8 +22,6 @@ import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.AsyncTask;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.ChromeActivity;
-import org.chromium.chrome.browser.vr.VrModeProviderImpl;
 import org.chromium.components.browser_ui.util.BitmapCache;
 import org.chromium.components.browser_ui.util.ConversionUtils;
 import org.chromium.components.browser_ui.util.GlobalDiscardableReferencePool;
@@ -34,6 +32,9 @@ import org.chromium.components.browser_ui.widget.selectable_list.SelectionDelega
 import org.chromium.content.browser.contacts.ContactsPickerPropertiesRequested;
 import org.chromium.ui.ContactsPickerListener;
 import org.chromium.ui.UiUtils;
+import org.chromium.ui.base.WindowAndroid;
+import org.chromium.ui.modaldialog.ModalDialogManager;
+import org.chromium.ui.vr.VrModeProvider;
 import org.chromium.ui.widget.OptimizedFrameLayout;
 
 import java.util.ArrayList;
@@ -68,8 +69,8 @@ public class PickerCategoryView extends OptimizedFrameLayout
     // The view containing the RecyclerView and the toolbar, etc.
     private SelectableListLayout<ContactDetails> mSelectableListLayout;
 
-    // Our activity.
-    private ChromeActivity mActivity;
+    // The window for the main Activity.
+    private WindowAndroid mWindowAndroid;
 
     // The callback to notify the listener of decisions reached in the picker.
     private ContactsPickerListener mListener;
@@ -130,13 +131,14 @@ public class PickerCategoryView extends OptimizedFrameLayout
      * selected.
      */
     @SuppressWarnings("unchecked") // mSelectableListLayout
-    public PickerCategoryView(Context context, boolean multiSelectionAllowed,
+    public PickerCategoryView(WindowAndroid windowAndroid, boolean multiSelectionAllowed,
             boolean shouldIncludeNames, boolean shouldIncludeEmails, boolean shouldIncludeTel,
             boolean shouldIncludeAddresses, boolean shouldIncludeIcons, String formattedOrigin,
-            ContactsPickerToolbar.ContactsToolbarDelegate delegate) {
-        super(context, null);
+            ContactsPickerToolbar.ContactsToolbarDelegate delegate, VrModeProvider vrModeProvider) {
+        super(windowAndroid.getContext().get(), null);
 
-        mActivity = (ChromeActivity) context;
+        mWindowAndroid = windowAndroid;
+        Context context = windowAndroid.getContext().get();
         mMultiSelectionAllowed = multiSelectionAllowed;
         includeNames = shouldIncludeNames;
         includeEmails = shouldIncludeEmails;
@@ -167,7 +169,7 @@ public class PickerCategoryView extends OptimizedFrameLayout
                                             : R.string.contacts_picker_select_contact;
         mToolbar = (ContactsPickerToolbar) mSelectableListLayout.initializeToolbar(
                 R.layout.contacts_picker_toolbar, mSelectionDelegate, titleId, 0, 0, null, false,
-                false, new VrModeProviderImpl());
+                false, vrModeProvider);
         mToolbar.setNavigationOnClickListener(this);
         mToolbar.initializeSearchView(this, R.string.contacts_picker_search, 0);
         mToolbar.setDelegate(delegate);
@@ -324,8 +326,8 @@ public class PickerCategoryView extends OptimizedFrameLayout
         return mBitmapCache;
     }
 
-    ChromeActivity getActivity() {
-        return mActivity;
+    ModalDialogManager getModalDialogManager() {
+        return mWindowAndroid.getModalDialogManager();
     }
 
     void setTopView(TopView topView) {
@@ -351,7 +353,8 @@ public class PickerCategoryView extends OptimizedFrameLayout
         if (includeIcons && PickerAdapter.includesIcons()) {
             // Fetch missing icons and compress them first.
             new CompressContactIconsWorkerTask(
-                    mActivity.getContentResolver(), mBitmapCache, selectedContacts, this)
+                    mWindowAndroid.getContext().get().getContentResolver(), mBitmapCache,
+                    selectedContacts, this)
                     .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
             return;
         }
