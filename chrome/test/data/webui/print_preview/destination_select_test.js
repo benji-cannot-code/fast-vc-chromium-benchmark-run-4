01c0e03ce554b3a0ff160710fca4148c371bb6f3
Daniel Hosseinian <dhoss@chromium.org>
2020-08-18 18:41:35 +0000

    Add "Save to Drive" deprecation warning under destination select
    Add a deprecation warning message for the "Save to Drive" destination
    under the destination select.
    
    Screenshot of change: https://imgur.com/ix1GjIB
    
    Bug: 1112581
    Change-Id: Icecc4e90e97f0e4faae0ceca32e19fe10a5de51f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353555
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799230}

--- chrome/test/data/webui/print_preview/destination_select_test.js
+++ chrome/test/data/webui/print_preview/destination_select_test.js
@@ -103,17 +103,15 @@ suite(destination_select_test.suiteName, function() {
 
     return selectOption(destinationSelect, driveKey)
         .then(() => {
-          // Icon updates early based on the ID.
-          // TODO(dhoss): This icon should be 'save-to-drive-not-supported'
-          // after all cloud print deprecation warnings are implemented.
-          compareIcon(selectEl, 'save-to-drive');
-
-          // Update the destination.
-          destinationSelect.destination = getGoogleDriveDestination(account);
-
           const saveToDriveIcon = cloudPrintDeprecationWarningsSuppressed ?
               'save-to-drive' :
               'save-to-drive-not-supported';
+
+          // Icon updates early based on the ID.
+          compareIcon(selectEl, saveToDriveIcon);
+
+          // Update the destination.
+          destinationSelect.destination = getGoogleDriveDestination(account);
 
           // Still Save to Drive icon.
           compareIcon(selectEl, saveToDriveIcon);
@@ -156,6 +154,7 @@ suite(destination_select_test.suiteName, function() {
   function testUpdateStatus(cloudPrintDeprecationWarningsSuppressed) {
     loadTimeData.overrideValues({
       offline: 'offline',
+      saveToDriveNotSupportedWarning: 'saveToDriveNotSupportedWarning',
     });
 
     assertFalse(destinationSelect.$$('.throbber-container').hidden);
@@ -172,8 +171,12 @@ suite(destination_select_test.suiteName, function() {
     destinationSelect.driveDestinationKey = driveKey;
     destinationSelect.destination = getGoogleDriveDestination(account);
     destinationSelect.updateDestination();
-    assertTrue(additionalInfoEl.hidden);
-    assertEquals('', statusEl.innerHTML);
+    const saveToDriveStatus = cloudPrintDeprecationWarningsSuppressed ?
+        '' :
+        'saveToDriveNotSupportedWarning';
+    assertEquals(
+        cloudPrintDeprecationWarningsSuppressed, additionalInfoEl.hidden);
+    assertEquals(saveToDriveStatus, statusEl.innerHTML);
 
     destinationSelect.destination = recentDestinationList[0];
     destinationSelect.updateDestination();
