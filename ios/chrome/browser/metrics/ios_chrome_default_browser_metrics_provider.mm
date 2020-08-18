0e61feae80b5963cee328237745ba2731011bcae
Chris Lu <thegreenfrog@chromium.org>
2020-08-18 17:04:56 +0000

    [ios] Add default browser metric provider
    When Chrome is the default browser, it will receive all URL opens,
    which is likely to change certain stability metrics. Thus, it will be
    good to filter those metrics by default browser status. This metrics
    records whether the user was deemed to have set Chrome as the device's
    default browser.
    
    Bug: 1107489
    Change-Id: Ie0bc34e7ac2979a82c67267e80fb8318dd2c6a51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2347954
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799156}

---
+++ ios/chrome/browser/metrics/ios_chrome_default_browser_metrics_provider.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/metrics/ios_chrome_default_browser_metrics_provider.h"
+
+#include "base/metrics/histogram_functions.h"
+#include "base/metrics/histogram_macros.h"
+#import "ios/chrome/browser/ui/whats_new/default_browser_utils.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+IOSChromeDefaultBrowserMetricsProvider::
+    IOSChromeDefaultBrowserMetricsProvider() {}
+
+IOSChromeDefaultBrowserMetricsProvider::
+    ~IOSChromeDefaultBrowserMetricsProvider() {}
+
+void IOSChromeDefaultBrowserMetricsProvider::ProvideCurrentSessionData(
+    metrics::ChromeUserMetricsExtension* uma_proto) {
+  base::UmaHistogramBoolean("IOS.IsDefaultBrowser",
+                            IsChromeLikelyDefaultBrowser());
+}
