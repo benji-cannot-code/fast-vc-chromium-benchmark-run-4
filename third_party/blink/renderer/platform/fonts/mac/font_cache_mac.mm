236830da24e79e2149dc512548ac93e6ce192848
Olivier Yiptong <oyiptong@chromium.org>
2020-08-18 19:16:55 +0000

    FontAccess: Enable Origin Trial
    Enables Origin Trial for the Font Access API.
    
    Bug: 535764
    Change-Id: I257a329c3bcb52696c043eebe2d809542733f6e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359470
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Olivier Yiptong <oyiptong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799250}

--- third_party/blink/renderer/platform/fonts/mac/font_cache_mac.mm
+++ third_party/blink/renderer/platform/fonts/mac/font_cache_mac.mm
@@ -327,7 +327,6 @@ std::unique_ptr<FontPlatformData> FontCache::CreateFontPlatformData(
 }
 
 std::vector<FontEnumerationEntry> FontCache::EnumeratePlatformAvailableFonts() {
-  DCHECK(RuntimeEnabledFeatures::FontAccessEnabled());
   @autoreleasepool {
     std::vector<FontEnumerationEntry> output;
 
