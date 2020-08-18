428c11d31cb54b13f344ea396d3affa672b427ff
Ankit Kumar 🌪️ <ankk@microsoft.com>
2020-08-18 22:31:29 +0000

    Migrate pp::Point to gfx::Point/gfx::Vector2d at PDFEngine interface
    Migrate all instances of pp::Point to gfx::Point/gfx::Vector2d at
    PDFEngine interface. The internals still have pp::Point dependency but
    the interface is now independent of pp::Point. The internal dependency
    will be migrated in a subsequent CL.
    
    Bug: 1101101
    Change-Id: Ib8a729314ec6101183281235d35afc697a243b03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2326512
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#799361}

--- pdf/test/test_utils.h
+++ pdf/test/test_utils.h
@@ -8,14 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace pp {
 class FloatRect;
-class Point;
 class Rect;
 }  // namespace pp
 
 namespace chrome_pdf {
 
-void ComparePoint(const pp::Point& expected_point,
-                  const pp::Point& given_point);
 void CompareRect(const pp::Rect& expected_rect, const pp::Rect& given_rect);
 void CompareRect(const pp::FloatRect& expected_rect,
                  const pp::FloatRect& given_rect);
