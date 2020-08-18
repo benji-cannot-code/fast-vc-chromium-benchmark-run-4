b35f5a53f000681a32c900570708f188b4cebf0b
Himanshu Jaju <himanshujaju@chromium.org>
2020-08-18 17:02:49 +0000

    Close connection when fail to connect to remote target.
    - Posts a delayed task to close connection
    - Adds a test to check kTimedOut status in TransferMetadata
    
    Bug: 1085068
    Change-Id: I7c74b19533a97272d0aede9940eaa3f477e47827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359129
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Commit-Queue: Himanshu Jaju <himanshujaju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799155}

--- chrome/browser/nearby_sharing/nearby_sharing_service_impl.cc
+++ chrome/browser/nearby_sharing/nearby_sharing_service_impl.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/nearby_sharing/certificates/nearby_share_encrypted_metadata_key.h"
 #include "chrome/browser/nearby_sharing/client/nearby_share_client_impl.h"
 #include "chrome/browser/nearby_sharing/common/nearby_share_prefs.h"
+#include "chrome/browser/nearby_sharing/constants.h"
 #include "chrome/browser/nearby_sharing/contacts/nearby_share_contact_manager_impl.h"
 #include "chrome/browser/nearby_sharing/fast_initiation_manager.h"
 #include "chrome/browser/nearby_sharing/local_device_data/nearby_share_local_device_data_manager_impl.h"
@@ -39,15 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace {
-
-constexpr base::TimeDelta kReadFramesTimeout = base::TimeDelta::FromSeconds(15);
-constexpr base::TimeDelta kReadResponseFrameTimeout =
-    base::TimeDelta::FromSeconds(60);
-constexpr base::TimeDelta kIncomingRejectionDelay =
-    base::TimeDelta::FromSeconds(2);
-// Time to delay running the task to invalidate send and receive surfaces.
-constexpr base::TimeDelta kInvalidateDelay =
-    base::TimeDelta::FromMilliseconds(500);
 
 // Used to hash a token into a 4 digit string.
 constexpr int kHashModulo = 9973;
@@ -1252,7 +1244,15 @@ void NearbySharingServiceImpl::Fail(const ShareTarget& share_target,
     return;
   }
 
-  // TODO(himanshujaju) - Create alarm and cancel in SetDisconnectionListener().
+  base::SequencedTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&NearbySharingServiceImpl::CloseConnection,
+                     weak_ptr_factory_.GetWeakPtr(), share_target),
+      kIncomingRejectionDelay);
+
+  connection->SetDisconnectionListener(
+      base::BindOnce(&NearbySharingServiceImpl::UnregisterShareTarget,
+                     weak_ptr_factory_.GetWeakPtr(), share_target));
 
   // Send response to remote device.
   sharing::nearby::ConnectionResponseFrame::Status response_status;
@@ -1707,10 +1707,6 @@ void NearbySharingServiceImpl::OnIncomingMutualAcceptanceTimeout(
       << ": Incoming mutual acceptance timed out, closing connection for "
       << share_target.device_name;
 
-  OnIncomingTransferUpdate(share_target,
-                           TransferMetadataBuilder()
-                               .set_status(TransferMetadata::Status::kTimedOut)
-                               .build());
   Fail(share_target, TransferMetadata::Status::kTimedOut);
 }
 
