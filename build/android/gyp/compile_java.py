626d7334e97c561f8c3964b61cd2695ae0777d97
Andrew Grieve <agrieve@chromium.org>
2020-08-18 20:17:28 +0000

    Add -noverify to build commands to improve runtime (local builds only)
    The flag is guarded by treat_warnings_as_errors=false so that it
    will apply only to local developer builds.
    
    Results from //tools/android/build_speed/benchmark.py base_java_sig:
    With change:
    base_java_sig: 68.0s avg (68.8s, 67.2s)
    base_java_sig: 68.4s avg (68.6s, 68.2s)
    
    Without change:
    base_java_sig: 69.4s avg (69.3s, 69.5s)
    base_java_sig: 69.7s avg (69.7s, 69.7s)
    
    time out/Debug/bin/run_base_junit_tests:
    With Change:
    real 0m22.741s
    real 0m23.543s
    
    Without change:
    real 0m23.176s
    real 0m24.328s
    
    Bug: 1117222
    Change-Id: I4bfd742165468bf9538e6fa0dc8f35daa9e0f23f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2313299
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799285}

--- build/android/gyp/compile_java.py
+++ build/android/gyp/compile_java.py
@@ -396,8 +396,7 @@ def _OnStaleMd5(options, javac_cmd, javac_args, java_files):
       raise Exception('--enable-kythe-annotations requires '
                       'KYTHE_ROOT_DIRECTORY and KYTHE_OUTPUT_DIRECTORY '
                       'environment variables to be set.')
-    javac_extractor_cmd = [
-        build_utils.JAVA_PATH,
+    javac_extractor_cmd = build_utils.JavaCmd() + [
         '-jar',
         _JAVAC_EXTRACTOR,
     ]
