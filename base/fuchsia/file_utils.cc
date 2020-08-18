b0ad79675d7cdc0595e723521bfb4b833d56ebcc
RJ Ascani <rjascani@google.com>
2020-08-18 19:44:44 +0000

    [Fuchsia] Use isolated-cache for the cdm_data_directory
    The ContextParams.cdm_data_directory is provided by the client for the
    storage of CDM user data. This CL populates this field for CastRunner
    with persistent-isolated-cache. This is temporary to prevent breakage.
    
    Bug: 1051225, b/162078899
    Change-Id: Idd291e318ccaf256194ae207018c77c8cfedd574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315001
    Commit-Queue: RJ Ascani <rjascani@google.com>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799270}

--- base/fuchsia/file_utils.cc
+++ base/fuchsia/file_utils.cc
@@ -21,6 +21,7 @@ namespace base {
 namespace fuchsia {
 
 const char kPersistedDataDirectoryPath[] = "/data";
+const char kPersistedCacheDirectoryPath[] = "/cache";
 const char kServiceDirectoryPath[] = "/svc";
 const char kPackageRootDirectoryPath[] = "/pkg";
 
