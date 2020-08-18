71b0a54b29cd19e2c6f4b4d4f642ff6f5ef15db7
Sergei Datsenko <dats@google.com>
2020-08-18 11:08:56 +0000

    Boilerplate for establishing mojo comms for Files app.
    Set up bidirectional communication between WebUI page and the backing
    browser and set up a "hello world" example.
    
    Bug: 1111229
    
    Change-Id: I8a8ad38a02952a13a69723f33f5fbaa2fdf195d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353970
    Commit-Queue: Sergei Datsenko <dats@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799054}

--- chrome/browser/resources/chromeos/file_manager/file_manager.js
+++ chrome/browser/resources/chromeos/file_manager/file_manager.js
@@ -3,6 +3,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Basic example of establishing comms with the backend.
+
+// There must be only one listener returning values.
+theBrowserProxy.callbackRouter.getBar.addListener((foo) => {
+  console.log('GetBar(' + foo + ')');
+  return Promise.resolve({bar: 'baz'});
+});
+
+// Listen-only callbacks can be multiple.
+theBrowserProxy.callbackRouter.onSomethingHappened.addListener(
+    (something, other) => {
+      console.log('OnSomethingHappened(' + something + ', ' + other + ')');
+    });
+theBrowserProxy.callbackRouter.onSomethingHappened.addListener(
+    (something, other) => {
+      console.log('eh? ' + something + '. what? ' + other);
+    });
+
 document.addEventListener('DOMContentLoaded', function() {
   alert('DOMContentLoaded');
 });
