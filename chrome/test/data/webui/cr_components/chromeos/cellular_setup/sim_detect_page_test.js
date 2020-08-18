9eefbba9a95a9994b3f954b0badcd079a2f3e9e0
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-08-18 21:29:10 +0000

    [CrOS settings] Migrate cellular_setup/sim_detection_page.js to Polymer3
    Bug: 1045266
    Change-Id: If57ab1625e934ed165e9ec94951927dbaaf6be69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359368
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799323}

---
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/sim_detect_page_test.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// clang-format off
+// #import 'chrome://resources/cr_components/chromeos/cellular_setup/sim_detect_page.m.js';
+
+// #import {flush, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// #import {assertFalse, assertTrue} from '../../../chai_assert.js';
+// clang-format on
+
+suite('CrComponentsSimDetectPageTest', function() {
+  let simDetectPage;
+  setup(function() {
+    simDetectPage = document.createElement('sim-detect-page');
+    document.body.appendChild(simDetectPage);
+    Polymer.dom.flush();
+  });
+
+  test('Base test', function() {
+    const basePage = simDetectPage.$$('base-page');
+    assertTrue(!!basePage);
+  });
+});
