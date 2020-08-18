2784311612657ce0533af075a84982c545175a60
Dan Harrington <harringtond@chromium.org>
2020-08-18 19:19:29 +0000

    feed v2: fix reporting for feed opens
    See bug for detailed problem.
    This change adds a new signal sent to the feed component, informing
    that the feed is shown. This happens the first time feed content is
    seen, even if a card is hardly shown.
    
    Bug: 1117265
    Change-Id: Ic0eb7fbb67a16b7f06554bc1ae48efa34662d106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360600
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799253}

--- components/feed/core/v2/types.cc
+++ components/feed/core/v2/types.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64.h"
 #include "base/pickle.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/util/values/values_util.h"
 #include "base/values.h"
@@ -109,14 +110,21 @@ NetworkResponseInfo& NetworkResponseInfo::operator=(
     const NetworkResponseInfo&) = default;
 
 std::string ToString(ContentRevision c) {
-  return base::NumberToString(c.value());
+  // The 'c/' prefix is used to identify slices as content. Don't change this
+  // without updating the Java side.
+  return base::StrCat({"c/", base::NumberToString(c.value())});
 }
 
 ContentRevision ToContentRevision(const std::string& str) {
-  uint32_t value;
-  if (!base::StringToUint(str, &value))
+  if (str.size() < 3)
     return {};
-  return ContentRevision(value);
+
+  uint32_t value;
+  if (str[0] == 'c' && str[1] == '/' &&
+      base::StringToUint(base::StringPiece(str).substr(2), &value)) {
+    return ContentRevision(value);
+  }
+  return {};
 }
 
 std::string SerializeDebugStreamData(const DebugStreamData& data) {
