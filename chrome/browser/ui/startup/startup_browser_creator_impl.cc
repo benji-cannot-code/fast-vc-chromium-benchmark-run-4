1b816f226693cbc9ababe9820aada56015e3f746
Elly Fong-Jones <ellyjones@chromium.org>
2020-08-19 14:18:01 +0000

    mac: introduce system infobar
    This infobar will be used (via field trials) to display updated info
    about current OS support and provide users with a link to more info.
    The infobar receives both activation info (which configs to show for)
    and the actual strings to display via field trials, so it can be updated
    for deployed Chrome instances without an update cycle.
    
    This change adds:
    * The infobar itself
    * A feature MacSystemInfobar that controls whether the feature is
      enabled and if so which configs (Arm or Rosetta) it's enabled in
    * A switch --force-mac-system-infobar which causes the infobar to act as
      though it was running on a specified system, for both manual and
      automatic tests
    * A variety of IDS_MAC_SYSTEM_INFOBAR_* constants to customize the
      infobar's message, link, and link text
    * Unit tests for the logic to interpret the feature and the
      enable/disable configs
    
    Bug: 1116578
    Change-Id: Ie7fd6e772a0112a17e794d1b8fcb009c57ebed4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360479
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799615}

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/mac/dock.h"
 #include "chrome/browser/mac/install_from_dmg.h"
 #include "chrome/browser/ui/cocoa/keystone_infobar_delegate.h"
+#include "chrome/browser/ui/startup/mac_system_infobar_delegate.h"
 #endif
 
 #if defined(OS_WIN)
@@ -913,6 +914,11 @@ void StartupBrowserCreatorImpl::AddInfoBarsIfNecessary(
           !local_state->GetBoolean(prefs::kSuppressUnsupportedOSWarning))
         ObsoleteSystemInfoBarDelegate::Create(infobar_service);
     }
+
+#if defined(OS_MAC)
+    if (MacSystemInfoBarDelegate::ShouldShow())
+      MacSystemInfoBarDelegate::Create(infobar_service);
+#endif
 
 #if !defined(OS_CHROMEOS)
     if (!command_line_.HasSwitch(switches::kNoDefaultBrowserCheck)) {
