f17a0eaad5c8f0ef46b7a36a3a0bec0fab5689f1
Sky Malice <skym@chromium.org>
2020-08-19 06:30:37 +0000

    Revert "Disable WebViewThreadTest#testCookieManagerBeforeCreateWebView"
    This reverts commit 4f081fd83d15c6c9fb6926f857d985944e9ed217.
    
    Reason for revert: Reverted culprit CL, shouldn't have disabled this test.
    
    Original change's description:
    > Disable WebViewThreadTest#testCookieManagerBeforeCreateWebView
    > 
    > TBR: timvolodine@chromium.org
    > Bug: 1117683
    > Change-Id: Ibb9538fc0530dd48effa973af8628565af639b32
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363486
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Commit-Queue: Sky Malice <skym@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#799356}
    
    TBR=timvolodine@chromium.org,skym@chromium.org
    
    Change-Id: I9a70de742d31933e21e49f2e8fc49a5a61641466
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1117683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2362971
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799517}

--- android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewThreadTest.java
+++ android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewThreadTest.java
@@ -23,7 +23,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.webview_shell.WebViewThreadTestActivity;
 
 /**
@@ -98,7 +97,6 @@ public class WebViewThreadTest {
      */
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crubg.com/1117683")
     public void testCookieManagerBeforeCreateWebView() throws InterruptedException {
         CookieManager.getInstance();
         mActivity.createWebViewOnUiThread(TIMEOUT);
