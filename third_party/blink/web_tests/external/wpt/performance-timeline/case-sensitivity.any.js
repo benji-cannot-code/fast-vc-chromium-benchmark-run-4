04a681d247b63dc82cf9b5dfa29da6588c4ed0c7
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-08-19 10:39:26 +0000

    Import wpt@17ebc3aea0d6321e69554067c39ab5855e6fb67e
    Using wpt-import in Chromium 21278d154d990022ae65fae312157914055d49c9.
    With Chromium commits locally applied on WPT:
    ed257e2b7d "[CORS] Set preflight request mode correctly"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=foolip@google.com
    
    No-Export: true
    Change-Id: Ib9f9a921976278cdb87bfdcbeb3f39e717a11a15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2362340
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#799565}

--- third_party/blink/web_tests/external/wpt/performance-timeline/case-sensitivity.any.js
+++ third_party/blink/web_tests/external/wpt/performance-timeline/case-sensitivity.any.js
@@ -28,3 +28,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     assert_equals(mixedList.length, 0, "getEntriesByName('" + location2 + "').length");
 
   }, "getEntriesByName values are case sensitive");
+
+  async_test(function (t) {
+    // Test type/buffered case sensitivity.
+    observer = new PerformanceObserver(
+      t.step_func(function (entryList, obs) {
+        assert_unreached("Observer(type) should not be called.");
+      })
+    );
+    observer.observe({type: "Mark"});
+    observer.observe({type: "Measure"});
+    observer.observe({type: "MARK"});
+    observer.observe({type: "MEASURE"});
+    observer.observe({type: "Mark", buffered: true});
+    observer.observe({type: "Measure", buffered: true});
+    observer.observe({type: "MARK", buffered: true});
+    observer.observe({type: "MEASURE", buffered: true});
+    self.performance.mark("mark1");
+    self.performance.measure("measure1");
+
+    // Test entryTypes case sensitivity.
+    observer = new PerformanceObserver(
+      t.step_func(function (entryList, obs) {
+        assert_unreached("Observer(entryTypes) should not be called.");
+      })
+    );
+    observer.observe({entryTypes: ["Mark", "Measure"]});
+    observer.observe({entryTypes: ["MARK", "MEASURE"]});
+    self.performance.mark("mark1");
+    self.performance.measure("measure1");
+
+    t.step_timeout(function() {
+      t.done();
+    }, 1000);
+
+  }, "observe() and case sensitivity for types/entryTypes and buffered.");
