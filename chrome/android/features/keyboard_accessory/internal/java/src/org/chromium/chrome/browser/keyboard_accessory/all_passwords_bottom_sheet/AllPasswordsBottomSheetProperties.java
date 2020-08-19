1903368d298ae1c0aa4b604d2ab2673ee8ac8094
Reda Tawfik <redatawfik@noogler.google.com>
2020-08-19 16:04:43 +0000

    [Android][Mfill] Add a controller for AllPasswordsBottomSheet
    This Cl adds the controller and the model for AllPasswordsBottomSheet
    and sends the credentials from the bridge to the coordinator to
    the mediator.
    Adds a unit test for the controller.
    
    Bug: 1104132
    Change-Id: I5a56ea9d3095cad29fed19a058805ed0121be47c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2317797
    Commit-Queue: Reda Tawfik <redatawfik@noogler.google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799649}

---
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/all_passwords_bottom_sheet/AllPasswordsBottomSheetProperties.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.keyboard_accessory.all_passwords_bottom_sheet;
+
+import org.chromium.base.Callback;
+import org.chromium.ui.modelutil.ListModel;
+import org.chromium.ui.modelutil.MVCListAdapter.ListItem;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/**
+ * Properties defined here reflect the visible state of the AllPasswordsBottomSheet.
+ */
+class AllPasswordsBottomSheetProperties {
+    static final PropertyModel.WritableBooleanPropertyKey VISIBLE =
+            new PropertyModel.WritableBooleanPropertyKey("visible");
+    static final PropertyModel.ReadableObjectPropertyKey<Callback<Integer>> DISMISS_HANDLER =
+            new PropertyModel.ReadableObjectPropertyKey<>("dismiss_handler");
+    static final PropertyModel.ReadableObjectPropertyKey<ListModel<ListItem>> SHEET_ITEMS =
+            new PropertyModel.ReadableObjectPropertyKey<>("sheet_items");
+
+    static PropertyModel createDefaultModel(Callback<Integer> handler) {
+        return new PropertyModel.Builder(VISIBLE, SHEET_ITEMS, DISMISS_HANDLER)
+                .with(VISIBLE, false)
+                .with(SHEET_ITEMS, new ListModel<>())
+                .with(DISMISS_HANDLER, handler)
+                .build();
+    }
+}
