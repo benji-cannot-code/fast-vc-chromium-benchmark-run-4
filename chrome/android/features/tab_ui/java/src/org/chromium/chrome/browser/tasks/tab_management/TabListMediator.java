ab7fc560fced601fe476b17e50b69646ae99693f
David Maunder <davidjm@chromium.org>
2020-08-19 03:45:57 +0000

    Add assertion for tab.isInitialized()
    To assist in the investigation of how uninitialized
    Tabs are appearing in getDomain()
    
    Bug: 1116613
    Change-Id: Ia0ab0d7aca20d724fd1200b1fdbe1a2f951f9633
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363485
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799482}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
@@ -1337,6 +1337,7 @@ class TabListMediator {
     protected static String getDomain(Tab tab) {
         // TODO(crbug.com/1116613) Investigate how uninitialized Tabs are appearing
         // here.
+        assert tab.isInitialized();
         if (!tab.isInitialized()) {
             return "";
         }
