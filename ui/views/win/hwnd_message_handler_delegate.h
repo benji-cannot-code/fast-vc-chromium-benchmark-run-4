4f0f73375a1f6a294db77e5633654fdd5243817d
Elly Fong-Jones <ellyjones@chromium.org>
2020-08-19 22:53:00 +0000

    views: remove WidgetDelegate::WillProcessWorkAreaChange
    This method is only implemented on the base class, which always returns
    false. Delete the method, its implementation, a wrapper around it in
    DesktopWindowTreeHostWin, and the only call to it in HWNDMessageHandler.
    
    Open question: is WidgetDelegate::HandleWorkAreaChanged() now dead? It
    can only be called in HWNDMessageHandler::OnSettingChange() when
    GetParent(hwnd()), but under what circumstances is that true?
    
    Bug: None
    Change-Id: Ib00d0b67597ded401c624786b54051bdf2373769
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364212
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799829}

--- ui/views/win/hwnd_message_handler_delegate.h
+++ ui/views/win/hwnd_message_handler_delegate.h
@@ -83,8 +83,6 @@ class VIEWS_EXPORT HWNDMessageHandlerDelegate {
   // window.
   virtual int GetInitialShowState() const = 0;
 
-  virtual bool WillProcessWorkAreaChange() const = 0;
-
   virtual int GetNonClientComponent(const gfx::Point& point) const = 0;
   virtual void GetWindowMask(const gfx::Size& size, SkPath* mask) = 0;
 
