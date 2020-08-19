7bd6dc665ead9e53b8fac9e380877bfbe7021adf
Zhenyao Mo <zmo@chromium.org>
2020-08-19 02:22:59 +0000

    Force full damage on main back buffer for overlays.
    This is Windows only.
    
    DWM won't consider a swap chain as an overlay candidate
    unless it's updating at high enough fps with full damages.
    
    So we need to force full damage when the actual damage is
    closer to full damage in order to promote the main back
    buffer as an overlay.
    
    This CL also added the bit to allow viz to make sure
    swap chain's buffers always have up to date pixels
    inside or outside damage rects. This enables us to
    force full damage.
    
    BUG=1117185
    TEST=manual
    R=sunnyps@chromium.org,magchen@chromium.org
    
    Change-Id: Idff7a90952c810a07992df0ace040be92b63e4d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359730
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799445}

--- components/viz/service/display_embedder/skia_output_device_gl.cc
+++ components/viz/service/display_embedder/skia_output_device_gl.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_surface.h"
+#include "ui/gl/gl_switches.h"
 #include "ui/gl/gl_utils.h"
 #include "ui/gl/gl_version_info.h"
 
@@ -53,6 +54,15 @@ SkiaOutputDeviceGL::SkiaOutputDeviceGL(
   capabilities_.uses_default_gl_framebuffer = true;
   capabilities_.output_surface_origin = gl_surface_->GetOrigin();
   capabilities_.supports_post_sub_buffer = gl_surface_->SupportsPostSubBuffer();
+#if defined(OS_WIN)
+  if (base::FeatureList::IsEnabled(
+          features::kDirectCompositionForceFullDamage)) {
+    // We need to set this bit to allow viz to track the previous damage rect
+    // of a backbuffer in a multiple backbuffer system, so backbuffers always
+    // have valid pixels, even outside the current damage rect.
+    capabilities_.preserve_buffer_content = true;
+  }
+#endif  // OS_WIN
   if (feature_info->workarounds()
           .disable_post_sub_buffers_for_onscreen_surfaces) {
     capabilities_.supports_post_sub_buffer = false;
