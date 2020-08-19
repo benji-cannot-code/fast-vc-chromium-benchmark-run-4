104fef832ea5ed2db2be536a3babad6464123e08
Marc Treib <treib@chromium.org>
2020-08-19 17:20:37 +0000

    Add default config for IPH_PasswordsAccountStorage
    This CL adds a default FeatureConfig for
    kIPHPasswordsAccountStorageFeature. It also adds an about:flags entry
    to enable the feature. This will make manual testing much easier.
    
    Bug: 1115476
    Change-Id: I30efa2fc4d3177fb47b363a8076a484d7eca630e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352935
    Auto-Submit: Marc Treib <treib@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799693}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -255,6 +255,9 @@ extern const char kDnsHttpssvcDescription[];
 extern const char kEnablePasswordsAccountStorageName[];
 extern const char kEnablePasswordsAccountStorageDescription[];
 
+extern const char kEnablePasswordsAccountStorageIPHName[];
+extern const char kEnablePasswordsAccountStorageIPHDescription[];
+
 extern const char kExperimentalAccessibilityLanguageDetectionName[];
 extern const char kExperimentalAccessibilityLanguageDetectionDescription[];
 
