428a81cb4644d8e18f21de2c9a2baf98eb9e5624
Sky Malice <skym@chromium.org>
2020-08-19 06:11:27 +0000

    Read real CCTToSDialogEnabled policy.
    Bug: 1108118
    Change-Id: If843abbcd6dd5451775d2d79bc6d55c151552ca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2344813
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799516}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/TosAndUmaFirstRunFragmentWithEnterpriseSupport.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/TosAndUmaFirstRunFragmentWithEnterpriseSupport.java
@@ -14,7 +14,9 @@ import androidx.annotation.VisibleForTesting;
 import org.chromium.base.CallbackController;
 import org.chromium.base.Log;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.policy.PolicyServiceFactory;
 import org.chromium.components.browser_ui.widget.LoadingView;
+import org.chromium.policy.PolicyService;
 
 /**
  * Another FirstRunFragment that is only used when running with CCT.
@@ -43,6 +45,7 @@ public class TosAndUmaFirstRunFragmentWithEnterpriseSupport
     private boolean mViewCreated;
     private LoadingView mLoadingSpinner;
     private CallbackController mCallbackController;
+    private PolicyService.Observer mPolicyServiceObserver;
 
     /**
      * Whether app restriction is found on the device. This can be null when this information is not
@@ -71,6 +74,10 @@ public class TosAndUmaFirstRunFragmentWithEnterpriseSupport
         if (mLoadingSpinner != null) {
             mLoadingSpinner.destroy();
             mLoadingSpinner = null;
+        }
+        if (mPolicyServiceObserver != null) {
+            PolicyServiceFactory.getGlobalPolicyService().removeObserver(mPolicyServiceObserver);
+            mPolicyServiceObserver = null;
         }
         super.onDestroy();
     }
@@ -154,8 +161,7 @@ public class TosAndUmaFirstRunFragmentWithEnterpriseSupport
                 mCallbackController.makeCancelable(this::onAppRestrictionDetected));
     }
 
-    @VisibleForTesting
-    void onAppRestrictionDetected(boolean hasAppRestriction) {
+    private void onAppRestrictionDetected(boolean hasAppRestriction) {
         mHasRestriction = hasAppRestriction;
 
         if (!shouldWaitForPolicyLoading() && mViewCreated) {
@@ -165,20 +171,21 @@ public class TosAndUmaFirstRunFragmentWithEnterpriseSupport
     }
 
     private void checkEnterprisePolicies() {
-        // TODO(crbug.com/1106812): Monitor policy changes when it is ready.
-        if (!sBlockPolicyLoadingForTest) {
-            onCctTosPolicyDetected(getPolicyCctTosDialogEnabled());
+        PolicyService policyService = PolicyServiceFactory.getGlobalPolicyService();
+        if (policyService.isInitializationComplete()) {
+            updateCctTosPolicy();
+        } else {
+            mPolicyServiceObserver = () -> {
+                policyService.removeObserver(mPolicyServiceObserver);
+                mPolicyServiceObserver = null;
+                updateCctTosPolicy();
+            };
+            policyService.addObserver(mPolicyServiceObserver);
         }
     }
 
-    private boolean getPolicyCctTosDialogEnabled() {
-        // TODO(crbug.com/1108118): Do the actual fetching for CCT Policy to replace the fake one.
-        return true;
-    }
-
-    @VisibleForTesting
-    void onCctTosPolicyDetected(boolean cctTosDialogEnabled) {
-        mPolicyCctTosDialogEnabled = cctTosDialogEnabled;
+    private void updateCctTosPolicy() {
+        mPolicyCctTosDialogEnabled = FirstRunUtils.isCctTosDialogEnabled();
         if (mViewCreated) {
             mLoadingSpinner.hideLoadingUI();
         }
@@ -192,10 +199,5 @@ public class TosAndUmaFirstRunFragmentWithEnterpriseSupport
         //  and skip waiting for future cold starts.
         Log.d(TAG, "TosAndUmaFirstRunFragmentWithEnterpriseSupport finished.");
         getPageDelegate().exitFirstRun();
-    }
-
-    @VisibleForTesting
-    static void setBlockPolicyLoadingForTest(boolean blockPolicyLoadingForTest) {
-        sBlockPolicyLoadingForTest = blockPolicyLoadingForTest;
     }
 }
--- chrome/browser/policy/android/java/src/org/chromium/chrome/browser/policy/PolicyServiceFactory.java
+++ chrome/browser/policy/android/java/src/org/chromium/chrome/browser/policy/PolicyServiceFactory.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.policy;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -17,12 +19,16 @@ import org.chromium.policy.PolicyService;
  */
 @JNINamespace("policy::android")
 public class PolicyServiceFactory {
+    private static PolicyService sPolicyServiceForTest;
+
     /**
      * Returns the PolicyService instance that contains browser policies.
      * The associated C++ instance is deleted during shutdown.
      */
     public static PolicyService getGlobalPolicyService() {
-        return PolicyServiceFactoryJni.get().getGlobalPolicyService();
+        return sPolicyServiceForTest == null
+                ? PolicyServiceFactoryJni.get().getGlobalPolicyService()
+                : sPolicyServiceForTest;
     }
 
     /**
@@ -31,7 +37,17 @@ public class PolicyServiceFactory {
      * deletion.
      */
     public static PolicyService getProfilePolicyService(Profile profile) {
-        return PolicyServiceFactoryJni.get().getProfilePolicyService(profile);
+        return sPolicyServiceForTest == null
+                ? PolicyServiceFactoryJni.get().getProfilePolicyService(profile)
+                : sPolicyServiceForTest;
+    }
+
+    /**
+     * @param policyService Mock {@link PolicyService} for testing.
+     */
+    @VisibleForTesting
+    public static void setPolicyServiceForTest(PolicyService policyService) {
+        sPolicyServiceForTest = policyService;
     }
 
     @NativeMethods
