9bf5dc4b0ecd44ccc9dbad0925dc0e1af4e5ce5e
Nate Fischer <ntfschr@chromium.org>
2020-08-19 16:32:37 +0000

    [AW][Dev-UI] add a histogram for the search bar
    No change to logic. This adds a histogram for the flag UI's new search
    bar, to determine if the search bar was used each time a user toggles a
    flag.
    
    Fixed: 1114247
    Test: manually verify we log the correct value
    Change-Id: I57ce73641146b95ce31e44497f561c9a7aca852f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364003
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799669}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/devui/FlagsFragment.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/devui/FlagsFragment.java
@@ -45,6 +45,7 @@ import org.chromium.android_webview.common.ProductionSupportedFlagList;
 import org.chromium.android_webview.common.services.IDeveloperUiService;
 import org.chromium.android_webview.common.services.ServiceNames;
 import org.chromium.base.Log;
+import org.chromium.base.metrics.RecordHistogram;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -73,6 +74,7 @@ public class FlagsFragment extends DevUiBaseFragment {
     private FlagsListAdapter mListAdapter;
 
     private Context mContext;
+    private EditText mSearchBar;
 
     private static volatile @Nullable Runnable sFilterListener;
 
@@ -113,8 +115,8 @@ public class FlagsFragment extends DevUiBaseFragment {
         Button resetFlagsButton = view.findViewById(R.id.reset_flags_button);
         resetFlagsButton.setOnClickListener((View flagButton) -> { resetAllFlags(); });
 
-        EditText searchBar = view.findViewById(R.id.flag_search_bar);
-        searchBar.addTextChangedListener(new TextWatcher() {
+        mSearchBar = view.findViewById(R.id.flag_search_bar);
+        mSearchBar.addTextChangedListener(new TextWatcher() {
             private boolean mPreviouslyHadText;
             @Override
             public void onTextChanged(CharSequence cs, int start, int before, int count) {
@@ -123,7 +125,7 @@ public class FlagsFragment extends DevUiBaseFragment {
                 // As an optimization, only change the clear text button if the search bar just now
                 // became empty or non-empty.
                 if (mPreviouslyHadText != currentlyHasText) {
-                    setClearTextButtonEnabled(searchBar, currentlyHasText);
+                    setClearTextButtonEnabled(mSearchBar, currentlyHasText);
                 }
                 mPreviouslyHadText = currentlyHasText;
             }
@@ -135,7 +137,7 @@ public class FlagsFragment extends DevUiBaseFragment {
             public void afterTextChanged(Editable e) {}
         });
 
-        searchBar.setOnFocusChangeListener((View v, boolean hasFocus) -> {
+        mSearchBar.setOnFocusChangeListener((View v, boolean hasFocus) -> {
             if (!hasFocus) hideKeyboard(mContext, v);
         });
     }
@@ -258,6 +260,10 @@ public class FlagsFragment extends DevUiBaseFragment {
                 if (grandparent instanceof View) {
                     formatListEntry((View) grandparent, newState);
                 }
+
+                boolean hasSearchQuery = !mSearchBar.getText().toString().isEmpty();
+                RecordHistogram.recordBooleanHistogram(
+                        "Android.WebView.DevUi.FlagsUi.ToggledFromSearch", hasSearchQuery);
             }
         }
 
