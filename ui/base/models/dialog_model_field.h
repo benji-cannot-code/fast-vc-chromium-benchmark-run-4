e4bf7138f49561903956f96926eaad4c65a08368
Peter Boström <pbos@chromium.org>
2020-08-19 02:19:29 +0000

    Move DialogModelField downcasts to base class
    This introduces DialogModelField::AsButton, etc. instead of having
    DialogModelHost and DialogModel both downcast.
    
    Also adds DialogModel::AddField to make sure OnFieldAdded() is called
    from all call sites that add a field.
    
    Bug: 1106422
    Change-Id: I512f5df9cf642314de7bde5b9ef4fe1d7c3efedc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363870
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799439}

--- ui/base/models/dialog_model_field.h
+++ ui/base/models/dialog_model_field.h
@@ -16,7 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 class DialogModel;
+class DialogModelButton;
+class DialogModelBodyText;
+class DialogModelCombobox;
 class DialogModelHost;
+class DialogModelTextfield;
 class Event;
 
 // These "field" classes represent entries in a DialogModel. They are owned
@@ -42,6 +46,10 @@ class COMPONENT_EXPORT(UI_BASE) DialogModelField {
       util::PassKey<DialogModelHost>) const {
     return accelerators_;
   }
+  DialogModelButton* AsButton(util::PassKey<DialogModelHost>);
+  DialogModelBodyText* AsBodyText(util::PassKey<DialogModelHost>);
+  DialogModelCombobox* AsCombobox(util::PassKey<DialogModelHost>);
+  DialogModelTextfield* AsTextfield(util::PassKey<DialogModelHost>);
 
  protected:
   // Children of this class need to be constructed through DialogModel to help
@@ -51,6 +59,11 @@ class COMPONENT_EXPORT(UI_BASE) DialogModelField {
                    Type type,
                    int unique_id,
                    base::flat_set<Accelerator> accelerators);
+
+  DialogModelButton* AsButton();
+  DialogModelBodyText* AsBodyText();
+  DialogModelCombobox* AsCombobox();
+  DialogModelTextfield* AsTextfield();
 
  private:
   friend class DialogModel;
