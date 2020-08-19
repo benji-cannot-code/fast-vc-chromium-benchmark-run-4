a75e0cd8c26d08a88654837713942347f32911fe
Daniel Rubery <drubery@chromium.org>
2020-08-19 02:14:23 +0000

    Populate screenshot_digest in CSD pings
    For SBER users, we send the SHA256 hash of the blurred image data
    along with the screenshot_phash.
    
    Fixed: 1115720
    Change-Id: Ia9ecbbb3e83bd927a12590934492bc1d3f4d5c3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363952
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799430}

--- chrome/browser/safe_browsing/client_side_detection_host_unittest.cc
+++ chrome/browser/safe_browsing/client_side_detection_host_unittest.cc
@@ -1208,6 +1208,7 @@ TEST_F(ClientSideDetectionHostTest, ClearsScreenshotData) {
   verdict.set_url("http://phishingurl.com/");
   verdict.set_client_score(1.0f);
   verdict.set_is_phishing(true);
+  verdict.set_screenshot_digest("screenshot_digest");
   verdict.set_screenshot_phash("screenshot_phash");
   verdict.set_phash_dimension_size(48);
 
@@ -1225,6 +1226,7 @@ TEST_F(ClientSideDetectionHostTest, ClearsScreenshotData) {
   EXPECT_TRUE(Mock::VerifyAndClear(csd_host_.get()));
   EXPECT_FALSE(request->has_phash_dimension_size());
   EXPECT_FALSE(request->has_screenshot_phash());
+  EXPECT_FALSE(request->has_screenshot_digest());
   delete request;
 }
 
@@ -1240,6 +1242,7 @@ TEST_F(ClientSideDetectionHostTest, AllowsScreenshotDataForSBER) {
   verdict.set_url("http://phishingurl.com/");
   verdict.set_client_score(1.0f);
   verdict.set_is_phishing(true);
+  verdict.set_screenshot_digest("screenshot_digest");
   verdict.set_screenshot_phash("screenshot_phash");
   verdict.set_phash_dimension_size(48);
 
@@ -1257,6 +1260,7 @@ TEST_F(ClientSideDetectionHostTest, AllowsScreenshotDataForSBER) {
   EXPECT_TRUE(Mock::VerifyAndClear(csd_host_.get()));
   EXPECT_TRUE(request->has_phash_dimension_size());
   EXPECT_TRUE(request->has_screenshot_phash());
+  EXPECT_TRUE(request->has_screenshot_digest());
   delete request;
 }
 
--- chrome/renderer/safe_browsing/scorer.cc
+++ chrome/renderer/safe_browsing/scorer.cc
@@ -12,10 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_set>
 
 #include "base/metrics/histogram_macros.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "chrome/renderer/safe_browsing/features.h"
 #include "components/safe_browsing/content/password_protection/visual_utils.h"
 #include "components/safe_browsing/core/proto/client_model.pb.h"
+#include "crypto/sha2.h"
 
 namespace {
 // Enum used to keep stats about the status of the Scorer creation.
@@ -102,6 +104,9 @@ bool Scorer::GetMatchingVisualTargets(const SkBitmap& bitmap,
     // the browser process if they are not needed.
     VisualFeatures::BlurredImage blurred_image;
     if (visual_utils::GetBlurredImage(bitmap, &blurred_image)) {
+      std::string raw_digest = crypto::SHA256HashString(blurred_image.data());
+      request->set_screenshot_digest(
+          base::HexEncode(raw_digest.data(), raw_digest.size()));
       request->set_screenshot_phash(
           visual_utils::GetHashFromBlurredImage(blurred_image));
       request->set_phash_dimension_size(48);
