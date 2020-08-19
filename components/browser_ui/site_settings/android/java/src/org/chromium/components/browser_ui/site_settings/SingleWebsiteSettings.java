be385b9725580c97e022ec540dfecb9526b25e13
Christian Dullweber <dullweber@chromium.org>
2020-08-19 13:25:35 +0000

    Fix site settings page not closing after clear
    mMaxPermissionOrder was not updated correctly. Avoid this problem by
    not storing it globally.
    
    Bug: 1117422
    Change-Id: Ic200c7f6130347cec6635e4d09fa460c99a3a7da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364533
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799595}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleWebsiteSettings.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleWebsiteSettings.java
@@ -142,8 +142,6 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
     // The Preference key for chooser object permissions.
     private static final String CHOOSER_PERMISSION_PREFERENCE_KEY = "chooser_permission_list";
 
-    // The maximum order of a permission preference.
-    private int mMaxPermissionOrder;
     // The number of user and policy chosen object permissions displayed.
     private int mObjectUserPermissionCount;
     private int mObjectPolicyPermissionCount;
@@ -351,7 +349,7 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
         }
         SettingsUtils.addPreferencesFromResource(this, R.xml.single_website_preferences);
 
-        mMaxPermissionOrder = 0;
+        int maxPermissionOrder = 0;
         PreferenceScreen preferenceScreen = getPreferenceScreen();
         // Iterate over preferences in reverse order because some preferences will be removed during
         // this setup, causing indices of later preferences to change.
@@ -360,10 +358,10 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
             setUpPreference(preference);
             if (getContentSettingsTypeFromPreferenceKey(preference.getKey())
                     != ContentSettingsType.DEFAULT) {
-                mMaxPermissionOrder = Math.max(mMaxPermissionOrder, preference.getOrder());
+                maxPermissionOrder = Math.max(maxPermissionOrder, preference.getOrder());
             }
         }
-        setUpChosenObjectPreferences(mMaxPermissionOrder);
+        setUpChosenObjectPreferences(maxPermissionOrder);
         setUpOsWarningPreferences();
 
         setUpAdsInformationalBanner();
@@ -796,7 +794,13 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
 
     private boolean hasPermissionsPreferences() {
         if (mObjectUserPermissionCount > 0 || mObjectPolicyPermissionCount > 0) return true;
-        if (mMaxPermissionOrder > 0) return true;
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        for (int i = 0; i < preferenceScreen.getPreferenceCount(); i++) {
+            String key = preferenceScreen.getPreference(i).getKey();
+            if (getContentSettingsTypeFromPreferenceKey(key) != ContentSettingsType.DEFAULT) {
+                return true;
+            }
+        }
         return false;
     }
 
