b0c34f1a25790e69b8737f9e37a466ee6997638a
Xiaocheng Hu <xiaochengh@chromium.org>
2020-08-19 22:56:52 +0000

    Rename descriptor 'letter-spacing-override' to 'advance-override'
    This follows from a discussion that 'advance-override' more accurately
    summarizes what the descriptor does.
    
    Bug: 1098355
    Change-Id: Ib497288f9e824ee33adb77b2e1b76e8aed709f42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363857
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799836}

--- third_party/blink/renderer/core/css/font_face.h
+++ third_party/blink/renderer/core/css/font_face.h
@@ -137,7 +137,7 @@ class CORE_EXPORT FontFace : public ScriptWrappable,
 
   bool HasFontMetricsOverride() const {
     return ascent_override_ || descent_override_ || line_gap_override_ ||
-           letter_spacing_override_;
+           advance_override_;
   }
   FontMetricsOverride GetFontMetricsOverride() const;
 
@@ -182,7 +182,7 @@ class CORE_EXPORT FontFace : public ScriptWrappable,
   Member<const CSSValue> ascent_override_;
   Member<const CSSValue> descent_override_;
   Member<const CSSValue> line_gap_override_;
-  Member<const CSSValue> letter_spacing_override_;
+  Member<const CSSValue> advance_override_;
   LoadStatusType status_;
   Member<DOMException> error_;
 
