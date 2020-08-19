0d47fd09695c358df0e38276738427dfdcd30c90
Sky Malice <skym@chromium.org>
2020-08-19 18:26:26 +0000

    Add several timing metrics to the FRE.
    Bug: 1111970
    Change-Id: I473c0eed6a0975062055691f663ee9601cd70de0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2339760
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799729}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunAppRestrictionInfo.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunAppRestrictionInfo.java
@@ -42,7 +42,9 @@ class FirstRunAppRestrictionInfo {
     private boolean mInitialized;
     private boolean mIsFetching;
     private boolean mHasAppRestriction;
-    private Queue<Callback<Boolean>> mCallBacks = new LinkedList<>();
+    private long mCompletionElapsedRealtimeMs;
+    private Queue<Callback<Boolean>> mCallbacks = new LinkedList<>();
+    private Queue<Callback<Long>> mCompletionTimeCallbacks = new LinkedList<>();
 
     private AsyncTask<Boolean> mFetchAppRestrictionAsyncTask;
     private final AppRestrictionsProvider mProvider;
@@ -92,7 +94,8 @@ class FirstRunAppRestrictionInfo {
         if (mProvider != null) {
             mProvider.destroy();
         }
-        mCallBacks.clear();
+        mCallbacks.clear();
+        mCompletionTimeCallbacks.clear();
     }
 
     /**
@@ -106,7 +109,23 @@ class FirstRunAppRestrictionInfo {
         if (mInitialized) {
             callback.onResult(mHasAppRestriction);
         } else {
-            mCallBacks.add(callback);
+            mCallbacks.add(callback);
+        }
+    }
+
+    /**
+     * Registers a callback for the timestamp from {@link SystemClock#elapsedRealtime} when the app
+     * restrictions call finished. If the restrictions have already been fetched, the callback will
+     * be invoked immediately.
+     *
+     * @param callback Callback to run with the timestamp of completing the fetch.
+     */
+    public void getCompletionElapsedRealtimeMs(Callback<Long> callback) {
+        ThreadUtils.assertOnUiThread();
+        if (mInitialized) {
+            callback.onResult(mCompletionElapsedRealtimeMs);
+        } else {
+            mCompletionTimeCallbacks.add(callback);
         }
     }
 
@@ -154,16 +173,22 @@ class FirstRunAppRestrictionInfo {
 
         // Only record histogram when startTime is valid.
         if (startTime > 0) {
-            long runTime = SystemClock.elapsedRealtime() - startTime;
+            mCompletionElapsedRealtimeMs = SystemClock.elapsedRealtime();
+            long runTime = mCompletionElapsedRealtimeMs - startTime;
             RecordHistogram.recordTimesHistogram(
                     "Enterprise.FirstRun.AppRestrictionLoadTime", runTime);
+            RecordHistogram.recordMediumTimesHistogram(
+                    "Enterprise.FirstRun.AppRestrictionLoadTime.Medium", runTime);
             Log.d(TAG,
                     String.format(Locale.US, "Policy received. Runtime: [%d], result: [%s]",
                             runTime, isAppRestricted));
         }
 
-        while (!mCallBacks.isEmpty()) {
-            mCallBacks.remove().onResult(mHasAppRestriction);
+        while (!mCallbacks.isEmpty()) {
+            mCallbacks.remove().onResult(mHasAppRestriction);
+        }
+        while (!mCompletionTimeCallbacks.isEmpty()) {
+            mCompletionTimeCallbacks.remove().onResult(mCompletionElapsedRealtimeMs);
         }
     }
 
