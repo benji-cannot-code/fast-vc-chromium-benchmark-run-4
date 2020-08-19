47578dd094f8e67407b94f3923b45ebfad836232
Alexander Dunaev <adunaev@igalia.com>
2020-08-19 14:07:18 +0000

    [ozone/x11] Enabled the Linux.WindowManager histogram.
    The functionality had been available for non-Ozone path.  This CL moves
    the necessary enum and utility function to the component so Ozone can
    use it.
    
    Bug: 1097007
    Change-Id: Id7070dc7a87e4efc36d3611f072b89c4ce9e4da2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359040
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Maksim Sisov (GMT+3) <msisov@igalia.com>
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#799613}

--- ui/ozone/platform/x11/ozone_platform_x11.cc
+++ ui/ozone/platform/x11/ozone_platform_x11.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/message_loop/message_pump_type.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "ui/base/buildflags.h"
@@ -193,6 +194,8 @@ class OzonePlatformX11 : public OzonePlatform,
         std::make_unique<GtkUiDelegateX11>(x11::Connection::Get());
     GtkUiDelegate::SetInstance(gtk_ui_delegate_.get());
 #endif
+
+    base::UmaHistogramEnumeration("Linux.WindowManager", GetWindowManagerUMA());
   }
 
   void InitializeGPU(const InitParams& params) override {
