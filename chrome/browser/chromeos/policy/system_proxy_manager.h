d2005bbba51be4fce40d674debe0bb55be6f1ab2
Andreea Costinas <acostinas@google.com>
2020-08-19 15:41:13 +0000

    system-proxy: Ask user for proxy credentials
    Currently, if System-proxy requires credentials to authenticate to a
    remote proxy, it will send a request to the Browser to fetch credentials
    from the Network Service authentication cache. If the user didn't
    authenticate in the Browser yet, System-proxy will receive empty
    credentials.
    
    This CL changes the flow to notify the user that System-proxy requires
    credentials and allows the user to enter the credentials in a dedicated
    dialogue, without needing to go through the Browser.
    
    Bug: 1112722
    Test: unittests, manually on DUT
    Change-Id: I0bb1e0e2d0a644b47dd022a87664cab1bb4e5205
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2356204
    Reviewed-by: Andreea-Elena Costinas <acostinas@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Omar Morsi <omorsi@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Andreea-Elena Costinas <acostinas@google.com>
    Cr-Commit-Position: refs/heads/master@{#799641}

--- chrome/browser/chromeos/policy/system_proxy_manager.h
+++ chrome/browser/chromeos/policy/system_proxy_manager.h
@@ -19,7 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 class RequestSystemProxyCredentialsView;
-}
+class SystemProxyNotification;
+}  // namespace chromeos
 
 namespace system_proxy {
 class SetAuthenticationDetailsResponse;
@@ -68,6 +69,9 @@ class SystemProxyManager {
 
   void SetSystemProxyEnabledForTest(bool enabled);
   void SetSystemServicesProxyUrlForTest(const std::string& local_proxy_url);
+  void SetSendAuthDetailsClosureForTest(base::RepeatingClosure closure);
+  chromeos::RequestSystemProxyCredentialsView* GetActiveAuthDialogForTest();
+  void CloseAuthDialogForTest();
 
   // Registers prefs stored in user profiles.
   static void RegisterProfilePrefs(PrefRegistrySimple* registry);
@@ -124,6 +128,10 @@ class SystemProxyManager {
       const system_proxy::ProtectionSpace& protection_space,
       const base::Optional<net::AuthCredentials>& credentials);
 
+  void ShowAuthenticationNotification(
+      const system_proxy::ProtectionSpace& protection_space,
+      bool show_error);
+
   // Shows a dialog which prompts the user to introduce proxy authentication
   // credentials for OS level traffic. If |show_error_label| is true, the
   // dialog will show a label that indicates the previous attempt to
@@ -135,8 +143,8 @@ class SystemProxyManager {
   void OnDialogCanceled(const system_proxy::ProtectionSpace& protection_space);
   void OnDialogClosed(const system_proxy::ProtectionSpace& protection_space);
 
-  // Closes the authentication dialog if shown.
-  void CloseAuthenticationDialog();
+  // Closes the authentication notification or dialog if shown.
+  void CloseAuthenticationUI();
 
   chromeos::CrosSettings* cros_settings_;
   std::unique_ptr<chromeos::CrosSettings::ObserverSubscription>
@@ -150,6 +158,10 @@ class SystemProxyManager {
   // Local state prefs, not owned.
   PrefService* local_state_ = nullptr;
 
+  // Notification which informs the user that System-proxy requires credentials
+  // for authentication to the remote proxy.
+  std::unique_ptr<chromeos::SystemProxyNotification> notification_handler_;
+
   // Owned by |auth_widget_|.
   chromeos::RequestSystemProxyCredentialsView* active_auth_dialog_ = nullptr;
   // Owned by the UI code (NativeWidget).
@@ -161,6 +173,8 @@ class SystemProxyManager {
   // Observer for Kerberos-related prefs.
   std::unique_ptr<PrefChangeRegistrar> local_state_pref_change_registrar_;
   std::unique_ptr<PrefChangeRegistrar> profile_pref_change_registrar_;
+
+  base::RepeatingClosure send_auth_details_closure_for_test_;
 
   base::WeakPtrFactory<SystemProxyManager> weak_factory_{this};
 };
