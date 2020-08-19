b94da15607fce463289cfb0f8b27eefd2f6b8e8d
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-08-19 15:28:45 +0000

    [cleanup] Remove LayoutTheme::AnimationDurationForProgressBar
    and LayoutTheme::AnimationRepeatIntervalForProgressBar, and moves them
    into LayoutProgress.
    
    There should be no behaviour change.
    
    Change-Id: I4e03085cd81810fde8f1a32f109af5a6b5861667
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270862
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799635}

--- third_party/blink/renderer/core/layout/layout_progress.cc
+++ third_party/blink/renderer/core/layout/layout_progress.cc
@@ -28,6 +28,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+
+constexpr base::TimeDelta kAnimationInterval =
+    base::TimeDelta::FromMilliseconds(125);
+constexpr base::TimeDelta kAnimationDuration = kAnimationInterval * 20;
+
+}  // namespace
+
 LayoutProgress::LayoutProgress(Element* element)
     : LayoutBlockFlow(element),
       position_(HTMLProgressElement::kInvalidPosition),
@@ -65,7 +73,7 @@ double LayoutProgress::AnimationProgress() const {
     return 0;
   const base::TimeDelta elapsed =
       base::TimeTicks::Now() - animation_start_time_;
-  return (elapsed % animation_duration_) / animation_duration_;
+  return (elapsed % kAnimationDuration) / kAnimationDuration;
 }
 
 bool LayoutProgress::IsDeterminate() const {
@@ -84,24 +92,18 @@ bool LayoutProgress::IsAnimating() const {
 void LayoutProgress::AnimationTimerFired(TimerBase*) {
   SetShouldDoFullPaintInvalidation();
   if (!animation_timer_.IsActive() && animating_)
-    animation_timer_.StartOneShot(animation_repeat_interval_, FROM_HERE);
+    animation_timer_.StartOneShot(kAnimationInterval, FROM_HERE);
 }
 
 void LayoutProgress::UpdateAnimationState() {
-  animation_duration_ =
-      LayoutTheme::GetTheme().AnimationDurationForProgressBar();
-  animation_repeat_interval_ =
-      LayoutTheme::GetTheme().AnimationRepeatIntervalForProgressBar();
-
-  bool animating = !IsDeterminate() && StyleRef().HasEffectiveAppearance() &&
-                   animation_duration_ > base::TimeDelta();
+  bool animating = !IsDeterminate() && StyleRef().HasEffectiveAppearance();
   if (animating == animating_)
     return;
 
   animating_ = animating;
   if (animating_) {
     animation_start_time_ = base::TimeTicks::Now();
-    animation_timer_.StartOneShot(animation_repeat_interval_, FROM_HERE);
+    animation_timer_.StartOneShot(kAnimationInterval, FROM_HERE);
   } else {
     animation_timer_.Stop();
   }
