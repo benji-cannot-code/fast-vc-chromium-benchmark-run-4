da54bb4d4008fa38f373593ee45b1c05ecce22d8
Robert Sesek <rsesek@chromium.org>
2020-08-19 16:06:23 +0000

    mac: Stop deleting the old Versions/ directory.
    The build system has not put files there in more than a year.
    
    Change-Id: Id2d1b95d05a67bd361dd58b26923fe5b0b0a024a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364011
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799651}

--- chrome/tools/build/mac/clean_up_old_versions.py
+++ chrome/tools/build/mac/clean_up_old_versions.py
@@ -20,10 +20,6 @@ def CleanUpOldVersions(args):
         else:
           os.unlink(path)
 
-  for path in args.delete:
-    if os.path.exists(path):
-      shutil.rmtree(path)
-
   open(args.stamp, 'w').close()
   os.utime(args.stamp, None)
 
@@ -40,12 +36,6 @@ def Main():
       default=[],
       help=('The names of items to keep in the `--versions-dir`. '
             'Can be specified multiple times.'))
-  parser.add_argument(
-      '--delete',
-      action='append',
-      default=[],
-      help=('Unconditionally deletes the tree at this path. Can be '
-            'specified multiple times.'))
   parser.add_argument(
       '--stamp', required=True, help='Path to write the stamp file.')
   args = parser.parse_args()
