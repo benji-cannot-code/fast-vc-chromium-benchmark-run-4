11490370eee71065d52ccce57b5afaa4aa96c8fe
Miguel Casas <mcasas@chromium.org>
2020-08-19 00:45:54 +0000

    media/gpu/vaapi: use new |va_buffer_for_vpp_| for Vpp/BlitSurface()
    ToT VaapiWrapper uses |va_buffers_| for internal Vpp uses and also for
    external VEA/JEA use. This CL starts refactoring these VABufferIDs
    lifetime, by splitting the Vpp VABufferID out of |va_buffers_| and
    into a ScopedID |va_buffer_for_vpp_|.
    
    Bug: b:162962069
    Change-Id: Ic54d0d26f5d17ea49fe896cf7ca5e1c931d07e0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352522
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799410}

--- media/gpu/vaapi/vaapi_wrapper.h
+++ media/gpu/vaapi/vaapi_wrapper.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/thread_annotations.h"
 #include "media/gpu/media_gpu_export.h"
 #include "media/gpu/vaapi/va_surface.h"
+#include "media/gpu/vaapi/vaapi_utils.h"
 #include "media/video/video_decode_accelerator.h"
 #include "media/video/video_encode_accelerator.h"
 #include "ui/gfx/geometry/size.h"
@@ -47,8 +48,6 @@ class Rect;
 namespace media {
 constexpr unsigned int kInvalidVaRtFormat = 0u;
 
-class ScopedVAImage;
-class ScopedVASurface;
 class VideoFrame;
 
 // Enum, function and callback type to allow VaapiWrapper to log errors in VA
@@ -468,8 +467,12 @@ class MEDIA_GPU_EXPORT VaapiWrapper
   // Data queued up for HW codec, to be committed on next execution.
   std::vector<VABufferID> pending_va_buffers_;
 
-  // Buffers for kEncode or kVideoProcess.
+  // VABufferIDs for kEncode*.
   std::set<VABufferID> va_buffers_;
+
+  // VABufferID to be used for kVideoProcess. Allocated the first time around,
+  // and reused afterwards.
+  std::unique_ptr<ScopedID<VABufferID>> va_buffer_for_vpp_;
 
   // Called to report codec errors to UMA. Errors to clients are reported via
   // return values from public methods.
