2f9f347228249e7acb2b45deda22ea752df6712e
Rachel Carpenter <carpenterr@chromium.org>
2020-08-19 22:40:15 +0000

    Record metric if we are opening help app from Release Notes chip.
    It would be good to be consistent in the logging of our release notes
    entry points for now as we have an experiment running on this as well as
    the swap from PWA to help app in M86. Will clean up to have better
    logging of the various pages HelpApp can be opened to/from at a later
    date.
    
    Bug: b/165421480
    Change-Id: Ia6a128aaeed139694f39421abb9ebe1f86c30827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2362240
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Rachel Carpenter <carpenterr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799816}

--- chrome/browser/ui/app_list/search/app_service_app_result.cc
+++ chrome/browser/ui/app_list/search/app_service_app_result.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/app_list_types.h"
 #include "ash/public/cpp/app_list/internal_app_id_constants.h"
 #include "base/bind.h"
+#include "base/metrics/user_metrics.h"
 #include "chrome/browser/apps/app_service/app_service_metrics.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
@@ -162,6 +163,12 @@ void AppServiceAppResult::Launch(int event_flags,
 
   if (id() == chromeos::default_web_apps::kHelpAppId &&
       query_url().has_value()) {
+    // This matches the logging of the release notes app in
+    // chrome/browser/apps/app_service/built_in_chromeos_apps.cc.
+    // TODO(carpenterr): Have more consistent logging of the places Help App can
+    // be opened to/from.
+    base::RecordAction(
+        base::UserMetricsAction("ReleaseNotes.SuggestionChipLaunched"));
     proxy->LaunchAppWithUrl(app_id(), event_flags, query_url().value(),
                             launch_source, controller()->GetAppListDisplayId());
     return;
