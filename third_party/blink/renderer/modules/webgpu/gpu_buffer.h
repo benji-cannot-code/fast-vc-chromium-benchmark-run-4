5383ab3f6d9051e875823fa0429473204434db76
Corentin Wallez <cwallez@chromium.org>
2020-08-19 20:04:16 +0000

    WebGPU: Remove deprecated buffer mapping code paths
    Following the PSA for API deprecations, the following are removed:
    
     - GPUDevice.createBufferMapped
     - GPUBuffer.mapReadAsync
     - GPUBuffer.mapWriteAsync
    
    Also a small check in ValidateRange is fixed.
    
    Bug: dawn:22
    Change-Id: I390b9992683e660fece3a17e0671149a40efcd1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2351971
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799762}

--- third_party/blink/renderer/modules/webgpu/gpu_buffer.h
+++ third_party/blink/renderer/modules/webgpu/gpu_buffer.h
@@ -25,10 +25,6 @@ class GPUBuffer : public DawnObject<WGPUBuffer> {
  public:
   static GPUBuffer* Create(GPUDevice* device,
                            const GPUBufferDescriptor* webgpu_desc);
-  static std::pair<GPUBuffer*, DOMArrayBuffer*> CreateMapped(
-      GPUDevice* device,
-      const GPUBufferDescriptor* webgpu_desc,
-      ExceptionState& exception_state);
   explicit GPUBuffer(GPUDevice* device,
                      uint64_t size,
                      bool mapped_at_creation,
@@ -52,10 +48,6 @@ class GPUBuffer : public DawnObject<WGPUBuffer> {
   DOMArrayBuffer* getMappedRange(uint64_t offset,
                                  uint64_t size,
                                  ExceptionState& exception_state);
-  ScriptPromise mapReadAsync(ScriptState* script_state,
-                             ExceptionState& exception_state);
-  ScriptPromise mapWriteAsync(ScriptState* script_state,
-                              ExceptionState& exception_state);
   void unmap(ScriptState* script_state);
   void destroy(ScriptState* script_state);
   // TODO(crbug.com/877147): implement GPUBuffer.
@@ -74,10 +66,6 @@ class GPUBuffer : public DawnObject<WGPUBuffer> {
                           uint64_t map_start,
                           uint64_t map_end,
                           WGPUBufferMapAsyncStatus status);
-  void OnOldMapAsyncCallback(ScriptPromiseResolver* resolver,
-                             WGPUBufferMapAsyncStatus status,
-                             void* data,
-                             uint64_t data_length);
 
   DOMArrayBuffer* CreateArrayBufferForMappedData(void* data,
                                                  size_t data_length);
