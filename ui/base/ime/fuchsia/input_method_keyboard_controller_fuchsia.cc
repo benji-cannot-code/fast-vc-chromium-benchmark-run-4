c214af323a3cb3b0bb7c624161006bd3b72b6605
Sharon Yang <yangsharon@chromium.org>
2020-08-19 17:41:39 +0000

    [fuchsia] Remove default_context.h/cc
    To finish the default_context migration, remove the old default_context
    files.
    
    Test: CQ
    Bug: 1090364
    Change-Id: I415b793572ed6d9042a7fded0ae258f83446ad04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303781
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799703}

--- ui/base/ime/fuchsia/input_method_keyboard_controller_fuchsia.cc
+++ ui/base/ime/fuchsia/input_method_keyboard_controller_fuchsia.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check.h"
-#include "base/fuchsia/default_context.h"
 #include "base/fuchsia/fuchsia_logging.h"
 #include "base/fuchsia/process_context.h"
 #include "base/notreached.h"
