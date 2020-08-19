134d955ff77ce574ac2cce7a5674ebb54755d886
Yao Xiao <yaoxia@chromium.org>
2020-08-19 16:31:22 +0000

    For floc computation, use a cached swaa/nac/account_type status
    Use a cached swaa/nac/account_type status to avoid querying the server
    too often. This is to prepare for an upcoming CL that computes the floc
    (which will go through all the checks) on history-deletion.
    
    Bug: 1062736
    Change-Id: I6d48e102f664c3dd1e673f63f5afaf4d7eadef05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363006
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799666}

--- chrome/browser/federated_learning/floc_id_provider_impl.cc
+++ chrome/browser/federated_learning/floc_id_provider_impl.cc
@@ -29,6 +29,8 @@ constexpr size_t kMinHistoryDomainSizeToReportFlocId = 1;
 constexpr base::TimeDelta kFlocScheduledUpdateInterval =
     base::TimeDelta::FromDays(1);
 constexpr int kQueryHistoryWindowInDays = 7;
+constexpr base::TimeDelta kSwaaNacAccountEnabledCachePeriod =
+    base::TimeDelta::FromHours(12);
 
 }  // namespace
 
@@ -86,6 +88,14 @@ bool FlocIdProviderImpl::AreThirdPartyCookiesAllowed() {
 
 void FlocIdProviderImpl::IsSwaaNacAccountEnabled(
     CanComputeFlocCallback callback) {
+  if (!last_swaa_nac_account_enabled_query_time_.is_null() &&
+      last_swaa_nac_account_enabled_query_time_ +
+              kSwaaNacAccountEnabledCachePeriod >
+          base::TimeTicks::Now()) {
+    std::move(callback).Run(cached_swaa_nac_account_enabled_);
+    return;
+  }
+
   net::PartialNetworkTrafficAnnotationTag partial_traffic_annotation =
       net::DefinePartialNetworkTrafficAnnotation(
           "floc_id_provider_impl", "floc_remote_permission_service",
@@ -116,7 +126,9 @@ void FlocIdProviderImpl::IsSwaaNacAccountEnabled(
         })");
 
   floc_remote_permission_service_->QueryFlocPermission(
-      std::move(callback), partial_traffic_annotation);
+      base::BindOnce(&FlocIdProviderImpl::OnCheckSwaaNacAccountEnabledCompleted,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)),
+      partial_traffic_annotation);
 }
 
 void FlocIdProviderImpl::Shutdown() {
@@ -198,6 +210,14 @@ void FlocIdProviderImpl::OnCheckCanComputeFlocCompleted(
   GetRecentlyVisitedURLs(
       base::BindOnce(&FlocIdProviderImpl::OnGetRecentlyVisitedURLsCompleted,
                      weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+}
+
+void FlocIdProviderImpl::OnCheckSwaaNacAccountEnabledCompleted(
+    CanComputeFlocCallback callback,
+    bool enabled) {
+  cached_swaa_nac_account_enabled_ = enabled;
+  last_swaa_nac_account_enabled_query_time_ = base::TimeTicks::Now();
+  std::move(callback).Run(enabled);
 }
 
 void FlocIdProviderImpl::GetRecentlyVisitedURLs(
