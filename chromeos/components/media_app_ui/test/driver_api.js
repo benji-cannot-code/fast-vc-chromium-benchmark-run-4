7a90e09d61bff7d90e02733758b1913c2b1e3c28
David <dlei@google.com>
2020-08-19 04:52:19 +0000

    Update renamed file in the untrusted context for chrome://media-app.
    Prior we re-launched upon a successful rename so the file was
    reloaded with the correct name. crrev/c/2352586 changes that but we
    forgot to update it in the untrusted context.
    
    Also adds tests for it.
    
    Bug: b/165431994
    Change-Id: I3b17c88b40ed563e043be7bb0b70c3dc098caeaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2362169
    Commit-Queue: David Lei <dlei@google.com>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799495}

--- chromeos/components/media_app_ui/test/driver_api.js
+++ chromeos/components/media_app_ui/test/driver_api.js
@@ -17,6 +17,7 @@ let TestMessageResponseData;
  * @typedef {{
  *     deleteLastFile: (boolean|undefined),
  *     getFileErrors: (boolean|undefined),
+ *     getLastFileName: (boolean|undefined),
  *     navigate: (string|undefined),
  *     overwriteLastFile: (string|undefined),
  *     pathToRoot: (!Array<string>|undefined),
--- chromeos/components/media_app_ui/test/guest_query_receiver.js
+++ chromeos/components/media_app_ui/test/guest_query_receiver.js
@@ -126,6 +126,8 @@ async function runTestQuery(data) {
         assertCast(lastReceivedFileList).files.map(file => file.error).join();
   } else if (data.openFile) {
     await DELEGATE.openFile();
+  } else if (data.getLastFileName) {
+    result = firstReceivedItem().name;
   }
   return {testQueryResult: result, testQueryResultData: extraResultData};
 }
