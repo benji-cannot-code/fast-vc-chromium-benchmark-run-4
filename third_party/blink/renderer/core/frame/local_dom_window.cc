26279da226bd520e07dd3a8936753e5727c82736
Rakina Zata Amni <rakina@chromium.org>
2020-08-19 17:50:27 +0000

    Add metric to track certain actions after pagehide was dispatched
    When doing a main-frame same-site navigation where we did a proactive
    Browsing Instance swap and we're reusing the old page's process, we
    will run the pagehide and visibilitychange handlers of the old page
    before we run the new page, without unloading the old page. This means
    script can still run after we've dispatched these events - this was not
    possible before.
    
    This CL adds a way to record occurences of certain actions happening on
    a page after the pagehide event gets dispatched. We will only track
    actions that might affect the user after we've navigated away from the
    old page such as modifications to storage, navigations, or postMessage.
    
    See: https://docs.google.com/document/d/1RXGDkrSWTfaeio76joXKWBLGDHAbiB48sHUdcTX6rdc/edit#heading=h.476deggm397n
    
    Bug: 977562
    Change-Id: I5307309421b880ae556cc6dec30cffcec7ff6e96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352304
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799707}

--- third_party/blink/renderer/core/frame/local_dom_window.cc
+++ third_party/blink/renderer/core/frame/local_dom_window.cc
@@ -30,8 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/metrics/histogram_macros.h"
 #include "cc/input/snap_selection_strategy.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
+#include "third_party/blink/public/common/action_after_pagehide.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 #include "third_party/blink/public/common/widget/screen_info.h"
 #include "third_party/blink/public/mojom/feature_policy/policy_disposition.mojom-blink.h"
@@ -1008,6 +1010,18 @@ void LocalDOMWindow::DispatchMessageEventWithOriginCheck(
                           WebFeature::kMessageEventSharedArrayBufferSameOrigin);
       }
     }
+  }
+
+  if (GetFrame() && GetFrame()->GetPage() &&
+      GetFrame()->GetPage()->DispatchedPagehideAndStillHidden()) {
+    // The message arrived after the pagehide event got dispatched and the page
+    // is still hidden, which is not normally possible (this  might happen if
+    // we're doing a same-site cross-RenderFrame navigation where we dispatch
+    // pagehide during the new RenderFrame's commit but won't unload/freeze the
+    // page after the new RenderFrame finished committing). We should track
+    // this case to measure how often this is happening.
+    UMA_HISTOGRAM_ENUMERATION("BackForwardCache.SameSite.ActionAfterPagehide",
+                              ActionAfterPagehide::kReceivedPostMessage);
   }
   DispatchEvent(*event);
 }
