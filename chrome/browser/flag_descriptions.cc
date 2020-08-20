5dc5a2ba6b6683f796f1ec495a7ab0640a0924e7
Moe Ahmadi <mahmadi@chromium.org>
2020-08-20 19:25:50 +0000

    Adds an about:flags entry for local history zero-suggest frecency ranking
    Bug: 1110077
    Change-Id: Ie167aa8a1d9317b431769f009e01a5550c8e371c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364119
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Ramya Nagarajan <ramyan@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800267}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1550,6 +1550,11 @@ const char kOmniboxLocalEntitySuggestionsDescription[] =
     "layout, similar to #omnibox-rich-entity-suggestions. Enabling this feature"
     "will also enable #omnibox-rich-entity-suggestions.";
 
+const char kOmniboxLocalZeroSuggestFrecencyRankingName[] =
+    "Frecency ranking for local history zero-prefix suggestions";
+const char kOmniboxLocalZeroSuggestFrecencyRankingDescription[] =
+    "Enable frecency ranking for local history zero-prefix suggestions.";
+
 const char kOmniboxMostVisitedTilesName[] = "Omnibox Most Visited Tiles";
 const char kOmniboxMostVisitedTilesDescription[] =
     "Display a list of frquently visited pages from history as a single row "
