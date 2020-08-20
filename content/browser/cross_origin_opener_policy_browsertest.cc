7a757e2259619ad42f392a95d05a60586983c339
Salvador Guerrero <salg@google.com>
2020-08-20 00:45:25 +0000

    Disabled flaky CoopReportingOriginTrialBrowserTests
    The following tests are failing on builder "android-bfcache-rel"
    
    CoopReportingOriginTrialBrowserTest.AccessReportingWithToken
    CoopReportingOriginTrialBrowserTest.CoopStateWithToken
    
    TBR=arthursonzogni@chromium.org
    
    Bug: 1119555
    Change-Id: I3cf6acde50f9ea42773a92f5b2d4229d59433722
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2365362
    Reviewed-by: Salvador Guerrero <salg@google.com>
    Commit-Queue: Salvador Guerrero <salg@google.com>
    Cr-Commit-Position: refs/heads/master@{#799894}

--- content/browser/cross_origin_opener_policy_browsertest.cc
+++ content/browser/cross_origin_opener_policy_browsertest.cc
@@ -1989,8 +1989,9 @@ IN_PROC_BROWSER_TEST_F(CoopReportingOriginTrialBrowserTest,
             network::mojom::CrossOriginOpenerPolicyValue::kUnsafeNone);
 }
 
+// TODO(http://crbug.com/1119555): Flaky on android-bfcache-rel.
 IN_PROC_BROWSER_TEST_F(CoopReportingOriginTrialBrowserTest,
-                       CoopStateWithToken) {
+                       DISABLED_CoopStateWithToken) {
   URLLoaderInterceptor interceptor(base::BindLambdaForTesting(
       [&](URLLoaderInterceptor::RequestParams* params) {
         if (params->url_request.url != OriginTrialURL())
@@ -2018,8 +2019,9 @@ IN_PROC_BROWSER_TEST_F(CoopReportingOriginTrialBrowserTest,
             network::mojom::CrossOriginOpenerPolicyValue::kSameOriginPlusCoep);
 }
 
+// TODO(http://crbug.com/1119555): Flaky on android-bfcache-rel.
 IN_PROC_BROWSER_TEST_F(CoopReportingOriginTrialBrowserTest,
-                       AccessReportingWithoutToken) {
+                       DISABLED_AccessReportingWithoutToken) {
   URLLoaderInterceptor interceptor(base::BindLambdaForTesting(
       [&](URLLoaderInterceptor::RequestParams* params) {
         if (params->url_request.url != OriginTrialURL())
