171c2d165b7e1a1262745492ba1c15c1d1eb2a0b
Nohemi Fernandez <fernandex@chromium.org>
2020-08-20 02:17:32 +0000

    [iOS] Removes deprecated method from SigninEarlGreyUI.
    Removes method confirmSigninConfirmationDialog from SigninEarlGreyUI.
    
    Bug: 1103274
    Change-Id: I6c1aead8dbd94e0a5f760c83139af4f99c0e1098
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364633
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799955}

--- ios/chrome/browser/ui/authentication/signin_earl_grey_ui.h
+++ ios/chrome/browser/ui/authentication/signin_earl_grey_ui.h
@@ -40,9 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // opened before calling this method.
 + (void)tapSettingsLink;
 
-// Deprecated. Use |tapSigninConfirmationDialog|.
-+ (void)confirmSigninConfirmationDialog;
-
 // Taps the sign in confirmation page, scrolls first to make the OK button
 // visible on short devices (e.g. iPhone 5s).
 + (void)tapSigninConfirmationDialog;
