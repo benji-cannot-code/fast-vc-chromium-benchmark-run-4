da151bfbac79a09cd3f46e4c39cad7fe70f52fbd
Peng Huang <penghuang@chromium.org>
2020-08-20 23:39:37 +0000

    vulkan: add two params for vulkan field trial.
     disable_by_gl_renderer: a string contain patterns to be used to match
             with gl renderer string. If any pattern in the string matches,
             vulkan will be disabled.
     force_enable_by_gl_renderer: similar to disable_by_gl_renderer, but if
             any pattern matches, vulkan will be enabled even if the GPU is
             blocklisted.
    
    params: 
    Bug: 1022495
    Change-Id: I04c1affd7c3c22baa4aa0bc048463f8f0a80d3a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2361042
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800370}

--- gpu/vulkan/x/vulkan_implementation_x11.cc
+++ gpu/vulkan/x/vulkan_implementation_x11.cc
@@ -147,11 +147,7 @@ bool VulkanImplementationX11::GetPhysicalDevicePresentationSupport(
 
 std::vector<const char*>
 VulkanImplementationX11::GetRequiredDeviceExtensions() {
-  std::vector<const char*> extensions = {
-      VK_KHR_EXTERNAL_MEMORY_EXTENSION_NAME,
-      VK_KHR_EXTERNAL_MEMORY_FD_EXTENSION_NAME,
-      VK_KHR_EXTERNAL_SEMAPHORE_EXTENSION_NAME,
-      VK_KHR_EXTERNAL_SEMAPHORE_FD_EXTENSION_NAME};
+  std::vector<const char*> extensions = {};
   if (using_surface_)
     extensions.push_back(VK_KHR_SWAPCHAIN_EXTENSION_NAME);
   return extensions;
@@ -159,7 +155,11 @@ VulkanImplementationX11::GetRequiredDeviceExtensions() {
 
 std::vector<const char*>
 VulkanImplementationX11::GetOptionalDeviceExtensions() {
-  return {VK_KHR_INCREMENTAL_PRESENT_EXTENSION_NAME};
+  return {VK_KHR_EXTERNAL_MEMORY_EXTENSION_NAME,
+          VK_KHR_EXTERNAL_MEMORY_FD_EXTENSION_NAME,
+          VK_KHR_EXTERNAL_SEMAPHORE_EXTENSION_NAME,
+          VK_KHR_EXTERNAL_SEMAPHORE_FD_EXTENSION_NAME,
+          VK_KHR_INCREMENTAL_PRESENT_EXTENSION_NAME};
 }
 
 VkFence VulkanImplementationX11::CreateVkFenceForGpuFence(VkDevice vk_device) {
