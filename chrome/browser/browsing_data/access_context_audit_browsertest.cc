87f446064df01a1840a86a052841a547d70fe647
sauski <sauski@google.com>
2020-08-20 06:15:28 +0000

    Access Context Auditing: Record origin keyed storage API accesses
    Extends the PageSpecificContentSettings & associated delegegate to
    inform the audit service of access to:
    	- DOM (Local) storage
    	- File System
    	- Service Workers
    	- Web Database
    
    The CookiesTreeModel is also updated to report deletion events for these
    storage types to the audit service.
    
    Bug: 1101675
    Change-Id: Idf332624cac0260a1bdc4c99c01057cda10b42a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363749
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#800010}

--- chrome/browser/browsing_data/access_context_audit_browsertest.cc
+++ chrome/browser/browsing_data/access_context_audit_browsertest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browsing_data_remover.h"
 #include "content/public/browser/storage_partition.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/browsing_data_remover_test_util.h"
@@ -44,6 +45,10 @@ constexpr char kEmbeddedHost[] = "b.test";
 // embedded pages in testing.
 const std::set<AccessContextAuditDatabase::StorageAPIType>&
     kOriginStorageTypes = {
+        AccessContextAuditDatabase::StorageAPIType::kLocalStorage,
+        AccessContextAuditDatabase::StorageAPIType::kFileSystem,
+        AccessContextAuditDatabase::StorageAPIType::kWebDatabase,
+        AccessContextAuditDatabase::StorageAPIType::kServiceWorker,
         AccessContextAuditDatabase::StorageAPIType::kCacheStorage,
         AccessContextAuditDatabase::StorageAPIType::kIndexedDB};
 
@@ -122,6 +127,18 @@ void CheckContainsOriginStorageRecords(
   }
 }
 
+// Calls the accessStorage javascript function and awaits its completion for
+// each frame in the active web contents for |browser|.
+void EnsurePageAccessedStorage(Browser* browser) {
+  auto frames =
+      browser->tab_strip_model()->GetActiveWebContents()->GetAllFrames();
+  for (auto* frame : frames) {
+    ASSERT_TRUE(content::EvalJs(
+                    frame, "(async () => { return await accessStorage();})()")
+                    .value.GetBool());
+  }
+}
+
 }  // namespace
 
 class CookiesTreeObserver : public CookiesTreeModel::Observer {
@@ -197,12 +214,14 @@ class AccessContextAuditBrowserTest : public InProcessBrowserTest {
   void NavigateToTopLevelPage() {
     ui_test_utils::NavigateToURL(browser(), top_level_url());
     base::RunLoop().RunUntilIdle();
+    EnsurePageAccessedStorage(browser());
   }
 
   // Navigate directly to the embedded page.
   void NavigateToEmbeddedPage() {
     ui_test_utils::NavigateToURL(browser(), embedded_url());
     base::RunLoop().RunUntilIdle();
+    EnsurePageAccessedStorage(browser());
   }
 
   url::Origin top_level_origin() {
--- components/content_settings/browser/page_specific_content_settings.cc
+++ components/content_settings/browser/page_specific_content_settings.cc
@@ -595,10 +595,12 @@ void PageSpecificContentSettings::OnDomStorageAccessed(const GURL& url,
       local ? container.local_storages() : container.session_storages();
   helper->Add(url::Origin::Create(url));
 
-  if (blocked_by_policy)
+  if (blocked_by_policy) {
     OnContentBlocked(ContentSettingsType::COOKIES);
-  else
+  } else {
+    delegate_->OnDomStorageAccessAllowed(url::Origin::Create(url));
     OnContentAllowed(ContentSettingsType::COOKIES);
+  }
 
   handler_.NotifySiteDataObservers();
 }
@@ -657,6 +659,7 @@ void PageSpecificContentSettings::OnServiceWorkerAccessed(
   if (allowed) {
     allowed_local_shared_objects_.service_workers()->Add(
         url::Origin::Create(scope));
+    delegate_->OnServiceWorkerAccessAllowed(url::Origin::Create(scope));
   } else {
     blocked_local_shared_objects_.service_workers()->Add(
         url::Origin::Create(scope));
@@ -699,6 +702,7 @@ void PageSpecificContentSettings::OnWebDatabaseAccessed(
     OnContentBlocked(ContentSettingsType::COOKIES);
   } else {
     allowed_local_shared_objects_.databases()->Add(url::Origin::Create(url));
+    delegate_->OnWebDatabaseAccessAllowed(url::Origin::Create(url));
     OnContentAllowed(ContentSettingsType::COOKIES);
   }
 
@@ -715,6 +719,7 @@ void PageSpecificContentSettings::OnFileSystemAccessed(const GURL& url,
     OnContentBlocked(ContentSettingsType::COOKIES);
   } else {
     allowed_local_shared_objects_.file_systems()->Add(url::Origin::Create(url));
+    delegate_->OnFileSystemAccessAllowed(url::Origin::Create(url));
     OnContentAllowed(ContentSettingsType::COOKIES);
   }
 
