11285561157f522492d7e2b99c113580c00078d5
Jana Grill <janagrill@chromium.org>
2020-08-20 17:55:22 +0000

    Show a different text for long-term support builds
    When the DeviceReleaseLtsTag policy is set, display a different text in
    Chrome OS settings to tell the user that they are currently using a
    long-term support (LTS) build.
    
    Additonally add the LTS-specific text to "Channel" entry when Build
    Details are copied to clipboard.
    
    Note: For the LTS pilot, we only check whether the DeviceReleaseLtsTag
    policy is set and do not consider the value it is set to. This will
    change for future LTS releases once the exact value is decided on.
    
    Bug: 1105495
    Change-Id: Ic1c11e14e690de8ab900337c81487d931ba9fd32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2355925
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Commit-Queue: Jana Grill <janagrill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800217}

--- chrome/browser/resources/settings/chromeos/os_about_page/os_about_page.js
+++ chrome/browser/resources/settings/chromeos/os_about_page/os_about_page.js
@@ -55,6 +55,12 @@ Polymer({
     /** @private {!BrowserChannel} */
     targetChannel_: String,
 
+    /** @private */
+    isLts_: {
+      type: Boolean,
+      value: false,
+    },
+
     /** @private {?RegulatoryInfo} */
     regulatoryInfo_: Object,
 
@@ -177,6 +183,7 @@ Polymer({
     this.aboutBrowserProxy_.getChannelInfo().then(info => {
       this.currentChannel_ = info.currentChannel;
       this.targetChannel_ = info.targetChannel;
+      this.isLts_ = info.isLts;
       this.startListening_();
     });
 
@@ -336,8 +343,8 @@ Polymer({
         if (this.currentChannel_ != this.targetChannel_) {
           return this.i18nAdvanced('aboutUpgradeUpdatingChannelSwitch', {
             substitutions: [
-              this.i18nAdvanced(
-                  settings.browserChannelToI18nId(this.targetChannel_)),
+              this.i18nAdvanced(settings.browserChannelToI18nId(
+                  this.targetChannel_, this.isLts_)),
               progressPercent
             ]
           });
