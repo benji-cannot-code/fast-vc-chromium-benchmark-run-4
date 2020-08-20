2c8c54876c328a64cd19782cf74ebd0d55a3cbd8
Lucía Cantú-Miller <cantumiller@google.com>
2020-08-20 23:12:53 +0000

    [chromedriver] Code clean-up
    Delete chrome/test/chromedriver/embed_extension_in_cpp.py, which is
    not used anymore.
    Edit chrome/test/chromedriver/chrome/navigation_tracker.cc, to
    remove kAutomationExtensionBackgroundPage.
    
    Bug: chromedriver:3376
    Change-Id: Ia1a9c17263e682de7a57a826a0e1d7042ea6db02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367772
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800363}

--- chrome/test/chromedriver/chrome/navigation_tracker.cc
+++ chrome/test/chromedriver/chrome/navigation_tracker.cc
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char kAutomationExtensionBackgroundPage[] =
-    "chrome-extension://aapnijgdinlhnhlmodcfapnahmbfebeb/"
-    "_generated_background_page.html";
-
 // Match to content/browser/devtools/devTools_session const of same name
 const char kTargetClosedMessage[] = "Inspected target navigated or closed";
 
@@ -180,15 +176,7 @@ Status NavigationTracker::IsPendingNavigation(const Timeout* timeout,
       *loading_state_ = kLoading;
       return Status(kOk);
     }
-    // If we're loading the ChromeDriver automation extension background page,
-    // look for a known function to determine the loading status.
-    if (base_url == kAutomationExtensionBackgroundPage) {
-      bool function_exists = false;
-      status = CheckFunctionExists(timeout, &function_exists);
-      if (status.IsError())
-        return MakeNavigationCheckFailedStatus(status);
-      *loading_state_ = function_exists ? kNotLoading : kLoading;
-    }
+
     status = UpdateCurrentLoadingState();
     if (status.code() == kNoSuchExecutionContext)
       *loading_state_ = kLoading;
