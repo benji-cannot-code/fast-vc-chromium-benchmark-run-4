e0151e598e9f5c65b5bbb9afb894481c03328a34
Titouan Rigoudy <titouan@chromium.org>
2020-08-20 12:09:34 +0000

    [CORS-RFC1918] Delete unused feature flag.
    It was mistakenly added in crrev.com/c/2282932 after a bad rebase
    operation.
    
    Bug: chromium:986744
    Change-Id: Iefdfac6cfa564f7d4cd3b12b7a322f56a2945ecc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2366734
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800082}

--- components/content_settings/core/common/features.cc
+++ components/content_settings/core/common/features.cc
@@ -24,7 +24,4 @@ const base::Feature kDisallowExtensionsToSetPluginContentSettings{
     "DisallowExtensionsToSetPluginContentSettings",
     base::FEATURE_ENABLED_BY_DEFAULT};
 
-const base::Feature kBlockInsecurePrivateNetworkRequests{
-    "BlockInsecurePrivateNetworkRequests", base::FEATURE_DISABLED_BY_DEFAULT};
-
 }  // namespace content_settings
