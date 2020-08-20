4ef95dca22e5e5d73f87ca2de840617552f3eb45
Anastasiia N <anastasiian@chromium.org>
2020-08-20 11:05:53 +0000

    Migrate resources/chromeos/account_manager/ to Polymer 3
    resources/chromeos/account_manager/ folder contains the following pages,
    which are opened from the different dialogs:
    - account_manager_welcome ("Sign in changed" welcome page)
    - account_migration_welcome ("Update your account" page)
    - account_manager_error ("Cannot add account" error page)
    
    Bug: 1111846
    Change-Id: I60b3806b4651842ccc587851f2a948c3cd25acf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2352882
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800070}

--- chrome/test/data/webui/chromeos/account_manager/account_manager_browsertest.js
+++ chrome/test/data/webui/chromeos/account_manager/account_manager_browsertest.js
@@ -5,32 +5,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /** @fileoverview Runs UI tests for account manager dialogs. */
 
+// Polymer BrowserTest fixture.
 GEN_INCLUDE(['//chrome/test/data/webui/polymer_browser_test_base.js']);
 
 GEN('#include "content/public/test/browser_test.h"');
 
-/**
- * @constructor
- * @extends {PolymerTest}
- */
-function AccountManagerBrowserTest() {}
-
-AccountManagerBrowserTest.prototype = {
-  __proto__: PolymerTest.prototype,
+// eslint-disable-next-line no-var
+var AccountMigrationWelcomeTest = class extends PolymerTest {
+  /** @override */
+  get browsePreload() {
+    return 'chrome://account-migration-welcome/test_loader.html?module=chromeos/account_manager/account_migration_welcome_test.js';
+  }
 
   /** @override */
-  browsePreload: 'chrome://account-migration-welcome/',
-
-  /** @override */
-  extraLibraries: [
-    ...PolymerTest.prototype.extraLibraries,
-    '../../test_browser_proxy.js',
-    'test_account_manager_browser_proxy.js',
-    'account_migration_welcome_test.js',
-  ],
+  get extraLibraries() {
+    return [
+      '//third_party/mocha/mocha.js',
+      '//chrome/test/data/webui/mocha_adapter.js',
+    ];
+  }
 };
 
-TEST_F('AccountManagerBrowserTest', 'AccountMigrationWelcomeTests', () => {
-  account_manager.account_migration_welcome_tests.registerTests();
-  mocha.run();
+TEST_F('AccountMigrationWelcomeTest', 'CloseDialog', () => {
+  this.runMochaTest(
+      account_migration_welcome_test.suiteName,
+      account_migration_welcome_test.TestNames.CloseDialog);
+});
+
+TEST_F('AccountMigrationWelcomeTest', 'MigrateAccount', () => {
+  this.runMochaTest(
+      account_migration_welcome_test.suiteName,
+      account_migration_welcome_test.TestNames.MigrateAccount);
 });
--- chrome/test/data/webui/chromeos/account_manager/test_account_manager_browser_proxy.js
+++ chrome/test/data/webui/chromeos/account_manager/test_account_manager_browser_proxy.js
@@ -3,8 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/** @implements {account_manager.AccountManagerBrowserProxy} */
-class TestAccountManagerBrowserProxy extends TestBrowserProxy {
+import {AccountManagerBrowserProxy} from 'chrome://account-migration-welcome/account_manager_browser_proxy.js';
+
+import {TestBrowserProxy} from '../../test_browser_proxy.m.js';
+
+/** @implements {AccountManagerBrowserProxy} */
+export class TestAccountManagerBrowserProxy extends TestBrowserProxy {
   constructor() {
     super([
       'closeDialog',
