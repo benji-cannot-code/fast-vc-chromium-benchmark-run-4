f9b9b8c372c2ab73258ebcb5fb936f73b2d11620
Mason Freed <masonfreed@chromium.org>
2020-08-20 16:58:31 +0000

    Add use counter for attachInternals()
    This previously had no use counter.
    
    I'm adding NOTRY here because branch day seems to be an
    impossible day to land changes to enums.xml. Hopefully this
    sails through. Feel free to revert if this breaks something.
    NOTRY=true
    
    Bug: 905922
    Change-Id: I613782dc839a9afc9bce93406200557e4f81c024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363361
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Auto-Submit: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800189}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -1597,6 +1597,7 @@ ElementInternals* HTMLElement::attachInternals(
         "ElementInternals for the specified element was already attached.");
     return nullptr;
   }
+  UseCounter::Count(GetDocument(), WebFeature::kElementAttachInternals);
   SetDidAttachInternals();
   return &EnsureElementInternals();
 }
