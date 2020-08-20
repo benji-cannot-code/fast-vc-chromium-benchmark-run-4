ed76d7e6ea29591840e621cc39d983ff11c05f9d
arthursonzogni <arthursonzogni@chromium.org>
2020-08-20 08:58:51 +0000

    [COOP] reporting: Add UseCounter.
    Based on the useful doc:
    docs/use_counter_wiki.md
    
    Add a WebFeature for the CrossOriginOpenerPolicyReporting, so that its
    usage could be monitored during the Origin Trial.
    
    Bug: 1116413
    Change-Id: I4230613345f3eee6749bb5e669f41b38d032ccbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364492
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800045}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -677,6 +677,11 @@ void RecordCrossOriginIsolationMetrics(RenderFrameHostImpl* rfh) {
     client->LogWebFeatureForCurrentPage(
         rfh, blink::mojom::WebFeature::kCoopAndCoepIsolated);
   }
+
+  if (rfh->coop_reporter()) {
+    client->LogWebFeatureForCurrentPage(
+        rfh, blink::mojom::WebFeature::kCrossOriginOpenerPolicyReporting);
+  }
 }
 
 // Subframe navigations can optionally have associated Trust Tokens operations
