ad92b6336ebf189e23dc3ae676f4a6fb547759ac
Shakti Sahu <shaktisahu@chromium.org>
2020-08-20 19:59:09 +0000

    Video Tutorials : Added Factory and JNI bridge for VideoTutorialService
    Bug: 1114883
    Change-Id: I4b067fd6fa31feb0673186170f116134619f6e69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2347915
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800285}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/video_tutorials/VideoTutorialServiceFactory.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.video_tutorials;
+
+import org.chromium.base.annotations.NativeMethods;
+import org.chromium.chrome.browser.profiles.Profile;
+
+/**
+ * Basic factory that creates and returns an {@link VideoTutorialService} that is attached
+ * natively to the given {@link Profile}.
+ */
+public class VideoTutorialServiceFactory {
+    private static VideoTutorialService sVideoTutorialServiceForTesting;
+
+    /**
+     * Used to get access to the video tutorials backend.
+     * @return An {@link VideoTutorialService} instance.
+     */
+    public static VideoTutorialService getForProfile(Profile profile) {
+        if (sVideoTutorialServiceForTesting != null) return sVideoTutorialServiceForTesting;
+        return VideoTutorialServiceFactoryJni.get().getForProfile(profile);
+    }
+
+    /** For testing only. */
+    public static void setVideoTutorialServiceForTesting(VideoTutorialService provider) {
+        sVideoTutorialServiceForTesting = provider;
+    }
+
+    @NativeMethods
+    interface Natives {
+        VideoTutorialService getForProfile(Profile profile);
+    }
+}
