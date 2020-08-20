8006d5288193656d2286161d5a26a47f0172340c
Collin Baker <collinbaker@chromium.org>
2020-08-20 19:23:11 +0000

    Replace FeaturePromoBubbleTimeout argument with plain TimeDeltas
    This gives equivalent functionality, but avoids the need to construct
    a complex object for each params object. Plus, it makes the params
    copy-constructible.
    
    Bug: 1106523
    Change-Id: If35476314b33cdaf19703d58a084370c95a5f709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353574
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800266}

--- chrome/browser/ui/views/in_product_help/feature_promo_bubble_params.h
+++ chrome/browser/ui/views/in_product_help/feature_promo_bubble_params.h
@@ -9,10 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/optional.h"
+#include "base/time/time.h"
 #include "ui/base/accelerators/accelerator.h"
 #include "ui/views/bubble/bubble_border.h"
-
-class FeaturePromoBubbleTimeout;
 
 // Describes the content and appearance of an in-product help bubble.
 // |body_string_specifier|, |anchor_view|, and |arrow| are required, all
@@ -22,7 +21,7 @@ struct FeaturePromoBubbleParams {
   FeaturePromoBubbleParams();
   ~FeaturePromoBubbleParams();
 
-  FeaturePromoBubbleParams(FeaturePromoBubbleParams&&);
+  FeaturePromoBubbleParams(const FeaturePromoBubbleParams&);
 
   // Promo contents:
 
@@ -63,7 +62,8 @@ struct FeaturePromoBubbleParams {
   ActivationAction activation_action = ActivationAction::DO_NOT_ACTIVATE;
 
   // Changes the bubble timeout. Intended for tests, avoid use.
-  std::unique_ptr<FeaturePromoBubbleTimeout> timeout;
+  base::Optional<base::TimeDelta> timeout_default;
+  base::Optional<base::TimeDelta> timeout_short;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_IN_PRODUCT_HELP_FEATURE_PROMO_BUBBLE_PARAMS_H_
