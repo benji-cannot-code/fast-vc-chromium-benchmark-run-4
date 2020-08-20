5dc4cb1e970359592b29fc1fc0e0aa2e861a9c4a
Orin Jaworski <orinj@chromium.org>
2020-08-20 00:51:17 +0000

    [omnibox] Add 'hu' to the Pedals language set
    This CL adds one last language that was previously excluded due to
    pak file versus spreadsheet-data naming mismatch: hu vs. hu-HU.
    
    Bug: 893183
    Change-Id: I3443fe36588d4c91d217b019d324313235e07fcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364582
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Auto-Submit: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799898}

--- chrome/browser/ui/omnibox/omnibox_pedals_unittest.cc
+++ chrome/browser/ui/omnibox/omnibox_pedals_unittest.cc
@@ -49,6 +49,7 @@ TEST(OmniboxPedals, DataLoadsForAllLocales) {
       {"he", "איפוס נתונים"},
       {"hi", "वाइप करें कैश मेमोरी"},
       {"hr", "odstranjivanje privremena memorija"},
+      {"hu", "eltávolítása gyorsítótár"},
       {"id", "hapus informasi"},
       {"it", "cancellare informazioni"},
       {"ja", "クリアキャッシュ"},
