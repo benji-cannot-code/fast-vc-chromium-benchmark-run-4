85a5befd0926fe34dabf0ab9ec548fba3259da4c
Dominic Schulz <dominicschulz@google.com>
2020-08-20 03:27:54 +0000

    Fix SWA launch metrics recording.
    Simply put, attempt to not record metrics for SWAs when they're
    coming through the usual app launch path, so that we can record
    them in LaunchSystemWebApp. We're encouraging SWAs that launch
    programmatically to use LaunchSystemWebApp, and those launches weren't
    being recorded.
    
    Bug: 1112660
    Change-Id: I3b24fc6f374ef875dac9c456a2ea616ab687919f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2340283
    Commit-Queue: Dominic Schulz <dominicschulz@google.com>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799985}

--- chrome/browser/web_applications/system_web_app_manager_browsertest.cc
+++ chrome/browser/web_applications/system_web_app_manager_browsertest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/native_file_system/native_file_system_permission_request_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/web_applications/system_web_app_ui_utils.h"
 #include "chrome/browser/web_applications/components/app_registrar.h"
 #include "chrome/browser/web_applications/components/web_app_constants.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
@@ -36,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/permissions/permission_util.h"
 #include "components/services/app_service/public/cpp/app_registry_cache.h"
 #include "components/services/app_service/public/cpp/app_update.h"
+#include "components/services/app_service/public/cpp/intent_util.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_controller_factory.h"
@@ -48,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_registry.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
+#include "ui/display/types/display_constants.h"
 #include "url/gurl.h"
 
 #if defined(OS_CHROMEOS)
@@ -98,11 +101,12 @@ apps::AppLaunchParams SystemWebAppManagerBrowserTestBase::LaunchParamsForApp(
     SystemAppType system_app_type) {
   base::Optional<AppId> app_id =
       GetManager().GetAppIdForSystemApp(system_app_type);
+
   CHECK(app_id.has_value());
   return apps::AppLaunchParams(
       *app_id, apps::mojom::LaunchContainer::kLaunchContainerWindow,
       WindowOpenDisposition::CURRENT_TAB,
-      apps::mojom::AppLaunchSource::kSourceTest);
+      apps::mojom::AppLaunchSource::kSourceAppLauncher);
 }
 
 content::WebContents* SystemWebAppManagerBrowserTestBase::LaunchApp(
@@ -296,6 +300,73 @@ IN_PROC_BROWSER_TEST_P(SystemWebAppManagerWebAppInfoBrowserTest,
       app_browser->tab_strip_model()->GetActiveWebContents(), off_scheme_page,
       1);
   EXPECT_TRUE(app_browser->app_controller()->ShouldShowCustomTabBar());
+}
+
+IN_PROC_BROWSER_TEST_P(SystemWebAppManagerWebAppInfoBrowserTest,
+                       LaunchMetricsWork) {
+  WaitForTestSystemAppInstall();
+
+  base::HistogramTester histograms;
+  apps::AppLaunchParams params = LaunchParamsForApp(GetMockAppType());
+  params.launch_source = apps::mojom::LaunchSource::kFromAppListGrid;
+
+  content::TestNavigationObserver navigation_observer(
+      maybe_installation_->GetAppUrl());
+  navigation_observer.StartWatchingNewWebContents();
+
+  LaunchSystemWebApp(browser()->profile(), GetMockAppType(),
+                     maybe_installation_->GetAppUrl(), params);
+
+  navigation_observer.Wait();
+  histograms.ExpectTotalCount("Apps.DefaultAppLaunch.FromAppListGrid", 1);
+  histograms.ExpectUniqueSample("Apps.DefaultAppLaunch.FromAppListGrid", 39, 1);
+}
+
+IN_PROC_BROWSER_TEST_P(SystemWebAppManagerWebAppInfoBrowserTest,
+                       LaunchMetricsWorkFromAppProxy) {
+  WaitForTestSystemAppInstall();
+
+  base::HistogramTester histograms;
+  content::TestNavigationObserver navigation_observer(
+      maybe_installation_->GetAppUrl());
+  navigation_observer.StartWatchingNewWebContents();
+
+  apps::AppServiceProxy* proxy =
+      apps::AppServiceProxyFactory::GetForProfile(browser()->profile());
+
+  proxy->Launch(GetManager().GetAppIdForSystemApp(GetMockAppType()).value(),
+                ui::EventFlags::EF_NONE,
+                apps::mojom::LaunchSource::kFromAppListGrid,
+                display::kDefaultDisplayId);
+  navigation_observer.Wait();
+
+  histograms.ExpectTotalCount("Apps.DefaultAppLaunch.FromAppListGrid", 1);
+  histograms.ExpectUniqueSample("Apps.DefaultAppLaunch.FromAppListGrid", 39, 1);
+}
+
+IN_PROC_BROWSER_TEST_P(SystemWebAppManagerWebAppInfoBrowserTest,
+                       LaunchMetricsWorkWithIntent) {
+  WaitForTestSystemAppInstall();
+
+  base::HistogramTester histograms;
+  content::TestNavigationObserver navigation_observer(
+      maybe_installation_->GetAppUrl());
+  navigation_observer.StartWatchingNewWebContents();
+
+  apps::AppServiceProxy* proxy =
+      apps::AppServiceProxyFactory::GetForProfile(browser()->profile());
+  auto intent = apps::mojom::Intent::New();
+  intent->action = apps_util::kIntentActionView;
+  intent->mime_type = "text/plain";
+
+  proxy->LaunchAppWithIntent(
+      GetManager().GetAppIdForSystemApp(GetMockAppType()).value(),
+      ui::EventFlags::EF_NONE, std::move(intent),
+      apps::mojom::LaunchSource::kFromAppListGrid, display::kDefaultDisplayId);
+  navigation_observer.Wait();
+
+  histograms.ExpectTotalCount("Apps.DefaultAppLaunch.FromAppListGrid", 1);
+  histograms.ExpectUniqueSample("Apps.DefaultAppLaunch.FromAppListGrid", 39, 1);
 }
 
 class SystemWebAppManagerFileHandlingBrowserTestBase
