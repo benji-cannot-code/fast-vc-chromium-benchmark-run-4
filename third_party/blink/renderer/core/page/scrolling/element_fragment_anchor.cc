7a6f2e13dcbb2f5f2e5fba438031819ec08bc8ad
Joey Arhar <jarhar@chromium.org>
2020-08-20 01:10:32 +0000

    Beforematch and hidden-matchable origin trial
    Bug: 1117589
    Change-Id: Id2e0e344553189018324ebfe30418419944400c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363434
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799910}

--- third_party/blink/renderer/core/page/scrolling/element_fragment_anchor.cc
+++ third_party/blink/renderer/core/page/scrolling/element_fragment_anchor.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/dom/node.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/svg/svg_svg_element.h"
 #include "third_party/blink/renderer/platform/bindings/script_forbidden_scope.h"
@@ -90,9 +91,11 @@ ElementFragmentAnchor* ElementFragmentAnchor::TryCreate(const KURL& url,
   if (!should_scroll)
     return nullptr;
 
-  if (RuntimeEnabledFeatures::BeforeMatchEventEnabled())
+  if (RuntimeEnabledFeatures::BeforeMatchEventEnabled(
+          frame.GetDocument()->GetExecutionContext())) {
     anchor_node->DispatchEvent(
         *Event::CreateBubble(event_type_names::kBeforematch));
+  }
 
   return MakeGarbageCollected<ElementFragmentAnchor>(*anchor_node, frame);
 }
