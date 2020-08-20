09ef5010766dbca1c7a27cd66614160a4dc57359
Himanshu Jaju <himanshujaju@chromium.org>
2020-08-20 22:33:32 +0000

    PayloadTracker implementation
    Implements the payload tracker class which is responsible for relaying
    progress updates in percent and maintaining status of incoming payloads.
    Also ensures that connection is closed when a terminal status is reached.
    
    Bug: 1085068
    Change-Id: I329674a3e27784860955e25280024de678f49b5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2362896
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Commit-Queue: Himanshu Jaju <himanshujaju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800348}

--- chrome/browser/nearby_sharing/share_target_info.h
+++ chrome/browser/nearby_sharing/share_target_info.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/nearby_sharing/certificates/nearby_share_decrypted_public_certificate.h"
 #include "chrome/browser/nearby_sharing/incoming_frames_reader.h"
 #include "chrome/browser/nearby_sharing/paired_key_verification_runner.h"
+#include "chrome/browser/nearby_sharing/payload_tracker.h"
 
 class NearbyConnection;
 
@@ -66,6 +67,12 @@ class ShareTargetInfo {
     key_verification_runner_ = std::move(key_verification_runner);
   }
 
+  PayloadTracker* payload_tracker() { return payload_tracker_.get(); }
+
+  void set_payload_tracker(std::unique_ptr<PayloadTracker> payload_tracker) {
+    payload_tracker_ = std::move(payload_tracker);
+  }
+
  private:
   base::Optional<std::string> endpoint_id_;
   base::Optional<NearbyShareDecryptedPublicCertificate> certificate_;
@@ -73,6 +80,7 @@ class ShareTargetInfo {
   base::Optional<std::string> token_;
   std::unique_ptr<IncomingFramesReader> frames_reader_;
   std::unique_ptr<PairedKeyVerificationRunner> key_verification_runner_;
+  std::unique_ptr<PayloadTracker> payload_tracker_;
 };
 
 #endif  // CHROME_BROWSER_NEARBY_SHARING_SHARE_TARGET_INFO_H_
