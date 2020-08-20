50105e89eb2a5b499f459ca0e52ea3da9e1c0612
Solomon Kinard <solomonkinard@chromium.org>
2020-08-20 07:00:04 +0000

    [Extensions] Provide a Search API for NTP extensions to leverage
    Bug: 1108940
    
    Example:
    chrome.search.query({"search": "search words"});
    
    Change-Id: I53c79a73411588596c49225482c16822257795a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2333031
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800019}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -1560,6 +1560,7 @@ enum HistogramValue {
   FILEMANAGERPRIVATEINTERNAL_INVOKESHARESHEET = 1496,
   FILEMANAGERPRIVATEINTERNAL_GETCONTENTMIMETYPE = 1497,
   FILEMANAGERPRIVATEINTERNAL_GETCONTENTMETADATA = 1498,
+  SEARCH_QUERY = 1499,
   // Last entry: Add new entries above, then run:
   // python tools/metrics/histograms/update_extension_histograms.py
   ENUM_BOUNDARY
