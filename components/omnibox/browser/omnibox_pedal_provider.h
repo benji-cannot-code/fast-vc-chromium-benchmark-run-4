4dec3a7de565d6efa782da774d6a400dcd185cfc
Angela Yoeurng <yoangela@chromium.org>
2020-08-20 16:32:14 +0000

    [omnibox] Add support for pedals counterfactual logging
    Bug: 893183
    Change-Id: Ib8eea4c29e68ec0483c1f97522ac516e6d18fb44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360416
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800173}

--- components/omnibox/browser/omnibox_pedal_provider.h
+++ components/omnibox/browser/omnibox_pedal_provider.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_offset_string_conversions.h"
 #include "base/values.h"
+#include "components/omnibox/browser/autocomplete_provider.h"
 #include "components/omnibox/browser/omnibox_pedal.h"
 #include "components/omnibox/browser/omnibox_pedal_implementations.h"
 
@@ -27,7 +28,20 @@ class OmniboxPedalProvider {
 
   // Returns the Pedal triggered by given |match_text| or nullptr if none
   // trigger.
-  OmniboxPedal* FindPedalMatch(const base::string16& match_text) const;
+  OmniboxPedal* FindPedalMatch(const base::string16& match_text);
+
+  // "Fake" implementation of AutocompleteProvider AddProviderInfo, though this
+  // class is not a true subclass of AutocompleteProvider. This is used
+  // for logging and reporting for our field trial.
+  void AddProviderInfo(ProvidersInfo* provider_info) const;
+
+  // "Fake" implementation of AutocompleteProvider::ResetSession. Resets the
+  // field trial flags.
+  void ResetSession();
+
+  void set_field_trial_triggered(bool triggered) {
+    field_trial_triggered_ = triggered;
+  }
 
  protected:
   FRIEND_TEST_ALL_PREFIXES(OmniboxPedalImplementationsTest,
@@ -70,6 +84,10 @@ class OmniboxPedalProvider {
   // This serves as an upper bound on the number of tokens we will accept from
   // text before giving up and treating it as non-match for all Pedals.
   size_t max_tokens_ = 0;
+
+  // Whether a field trial has triggered for this query and this session
+  bool field_trial_triggered_ = false;
+  bool field_trial_triggered_in_session_ = false;
 };
 
 #endif  // COMPONENTS_OMNIBOX_BROWSER_OMNIBOX_PEDAL_PROVIDER_H_
