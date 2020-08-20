3a5b451b31eff67e33f308482a1322d7c5daff72
Mohamed Amir Yosef <mamir@chromium.org>
2020-08-20 14:30:05 +0000

    [Passwords] Check login_db_ in DeleteAndRecreateDatabaseFile()
    Change-Id: I03c1dd29b972819f6021a402f2a481ce4b304f3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2366724
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800115}

--- components/password_manager/core/browser/password_store_default.cc
+++ components/password_manager/core/browser/password_store_default.cc
@@ -360,7 +360,7 @@ bool PasswordStoreDefault::IsAccountStore() const {
 }
 
 bool PasswordStoreDefault::DeleteAndRecreateDatabaseFile() {
-  return login_db_->DeleteAndRecreateDatabaseFile();
+  return login_db_ && login_db_->DeleteAndRecreateDatabaseFile();
 }
 
 void PasswordStoreDefault::ResetLoginDB() {
