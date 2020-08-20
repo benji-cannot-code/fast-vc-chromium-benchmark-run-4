1790eb62d0bcc0fefdf6b8ba1459afa3b56ac27e
nancylingwang <nancylingwang@chromium.org>
2020-08-20 15:45:43 +0000

    Fix the flaky browser test LauncherPlatformAppBrowserTest.SetIcon
    AppService is used to load icons for windows. AppService uses async
    mojom. So it might be not very appropriate to test with update number
    when we have the custom icon, because if the icon is updated fast from
    AppService, then it might be updated 4 times, however if slow, then
    there are 3 updates only, becuase it has got the customer icon, and the
    icon update from AppService could be skipped.
    
    Modify the test case LauncherPlatformAppBrowserTest.SetIcon to check
    the final image. If the custom icon is set, check the window's icon is
    set as the custom icon.
    
    BUG=1115533
    
    Change-Id: Ief034842fef9360a49a504effb54cf51c8d4c51a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364156
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800144}

--- chrome/browser/ui/test/test_app_window_icon_observer.cc
+++ chrome/browser/ui/test/test_app_window_icon_observer.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/app_window/app_window.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
+#include "ui/gfx/image/image_unittest_util.h"
 
 TestAppWindowIconObserver::TestAppWindowIconObserver(
     content::BrowserContext* context)
@@ -34,8 +35,15 @@ void TestAppWindowIconObserver::WaitForIconUpdates(int updates) {
   expected_icon_updates_ = updates + icon_updates_;
   icon_updated_callback_ = run_loop.QuitClosure();
   run_loop.Run();
-  base::RunLoop().RunUntilIdle();
   DCHECK_EQ(expected_icon_updates_, icon_updates_);
+}
+
+void TestAppWindowIconObserver::WaitForIconUpdates(
+    const gfx::ImageSkia& image_skia) {
+  base::RunLoop run_loop;
+  expected_image_skia_ = image_skia;
+  icon_image_updated_callback_ = run_loop.QuitClosure();
+  run_loop.Run();
 }
 
 void TestAppWindowIconObserver::OnAppWindowAdded(
@@ -95,5 +103,12 @@ void TestAppWindowIconObserver::OnWindowPropertyChanged(aura::Window* window,
   if (icon_updates_ == expected_icon_updates_ &&
       !icon_updated_callback_.is_null()) {
     std::move(icon_updated_callback_).Run();
+  }
+
+  if (!icon_image_updated_callback_.is_null() &&
+      gfx::test::AreBitmapsEqual(
+          expected_image_skia_.GetRepresentation(1.0f).GetBitmap(),
+          image->GetRepresentation(1.0f).GetBitmap())) {
+    std::move(icon_image_updated_callback_).Run();
   }
 }
