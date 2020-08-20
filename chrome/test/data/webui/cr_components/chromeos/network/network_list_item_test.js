adefac1f1edb564a1ba0774b62190de55288d0e2
James Hawkins <jhawkins@chromium.org>
2020-08-20 02:14:09 +0000

    cr_components/network: Convert NetworkList to Polymer 3.
    R=khorimoto@chromium.org
    
    Bug: 1045266
    Test: NetworkNetworkListTest*
    Change-Id: I34f03657486f03027c4adb5c6ae36aa421a2c239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360925
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799951}

--- chrome/test/data/webui/cr_components/chromeos/network/network_list_item_test.js
+++ chrome/test/data/webui/cr_components/chromeos/network/network_list_item_test.js
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 // clang-format on
 
-suite('CrComponentsNetworkPasswordInputTest', function() {
+suite('NetworkListTest', function() {
   /** @type {!NetworkListItem|undefined} */
   let listItem;
 
