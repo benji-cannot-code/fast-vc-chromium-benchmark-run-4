b8dfa55437160397864d5005a3a73066ca112825
Joshua Bell <jsbell@chromium.org>
2020-08-20 00:56:33 +0000

    IndexedDB: Remove unnecessary WebIDBFactory layer
    Now that Blink can talk directly to Mojo (it's only been what... 3
    years?) remove the WebIDBFactory cruft and have blink::IDBFactory make
    calls directly to the backend mojom::blink::IDBFactory.
    
    No functional changes.
    
    Bug: 843764,717812
    Change-Id: I7f8370570a21fd19c5a98b2b1d5d8e31db702a81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363176
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#799900}

--- third_party/blink/renderer/modules/indexeddb/idb_factory.h
+++ third_party/blink/renderer/modules/indexeddb/idb_factory.h
@@ -30,9 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_INDEXEDDB_IDB_FACTORY_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_INDEXEDDB_IDB_FACTORY_H_
 
+#include "mojo/public/cpp/bindings/pending_associated_remote.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/remote.h"
+#include "third_party/blink/public/mojom/feature_observer/feature_observer.mojom-blink.h"
+#include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom-blink-forward.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/modules/indexeddb/idb_open_db_request.h"
-#include "third_party/blink/renderer/modules/indexeddb/web_idb_factory.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
@@ -42,13 +46,15 @@ namespace blink {
 
 class ExceptionState;
 class ScriptState;
+class IndexedDBDatabaseCallbacksImpl;
+class WebIDBCallbacks;
 
 class MODULES_EXPORT IDBFactory final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
   IDBFactory();
-  IDBFactory(std::unique_ptr<WebIDBFactory>);
+  ~IDBFactory() override;
 
   // Implement the IDBFactory IDL
   IDBOpenDBRequest* open(ScriptState*, const String& name, ExceptionState&);
@@ -72,8 +78,12 @@ class MODULES_EXPORT IDBFactory final : public ScriptWrappable {
 
   ScriptPromise GetDatabaseInfo(ScriptState*, ExceptionState&);
 
+  void SetFactoryForTesting(mojo::Remote<mojom::blink::IDBFactory> factory);
+
  private:
-  WebIDBFactory* GetFactory(ExecutionContext* execution_context);
+  // Lazy initialize the mojo pipe to the back end.
+  mojo::Remote<mojom::blink::IDBFactory>& GetFactory(
+      ExecutionContext* execution_context);
 
   IDBOpenDBRequest* OpenInternal(ScriptState*,
                                  const String& name,
@@ -88,7 +98,17 @@ class MODULES_EXPORT IDBFactory final : public ScriptWrappable {
   bool AllowIndexedDB(ScriptState* script_state);
   bool CachedAllowIndexedDB(ScriptState* script_state);
 
-  std::unique_ptr<WebIDBFactory> web_idb_factory_;
+  mojo::PendingAssociatedRemote<mojom::blink::IDBCallbacks> GetCallbacksProxy(
+      std::unique_ptr<WebIDBCallbacks> callbacks);
+  mojo::PendingAssociatedRemote<mojom::blink::IDBDatabaseCallbacks>
+  GetDatabaseCallbacksProxy(
+      std::unique_ptr<IndexedDBDatabaseCallbacksImpl> callbacks);
+  mojo::PendingRemote<mojom::blink::ObservedFeature> GetObservedFeature();
+
+  mojo::Remote<mojom::blink::IDBFactory> factory_;
+  mojo::Remote<mojom::blink::FeatureObserver> feature_observer_;
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+
   base::Optional<bool> cached_allowed_;
 };
 
