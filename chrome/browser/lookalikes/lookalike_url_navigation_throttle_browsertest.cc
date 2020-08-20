493ebdd0e80e5abd8d7f6f2c188536f5de9b39a9
Mustafa Emre Acer <meacer@chromium.org>
2020-08-20 18:21:49 +0000

    Lookalikes: Add a console message pointing to the appeals bug template
    Bug: 1119460
    Change-Id: I43a2b33c5977e187d4d17e3bc666431d51d1eae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364759
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Livvie Lin <livvielin@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Livvie Lin <livvielin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800232}

--- chrome/browser/lookalikes/lookalike_url_navigation_throttle_browsertest.cc
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/bind.h"
+#include "base/strings/pattern.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
@@ -56,6 +57,9 @@ const char kInterstitialDecisionMetric[] = "interstitial.lookalike.decision";
 const char kInterstitialInteractionMetric[] =
     "interstitial.lookalike.interaction";
 
+const char kConsoleMessage[] =
+    "Chrome has determined that * could be fake or fraudulent*";
+
 static std::unique_ptr<net::test_server::HttpResponse>
 NetworkErrorResponseHandler(const net::test_server::HttpRequest& request) {
   return std::unique_ptr<net::test_server::HttpResponse>(
@@ -93,23 +97,21 @@ bool IsUrlShowing(Browser* browser) {
   return !browser->location_bar_model()->GetFormattedFullURL().empty();
 }
 
+// Navigate to |url| and wait for the load to complete before returning.
 // Simulates a link click navigation. We don't use
 // ui_test_utils::NavigateToURL(const GURL&) because it simulates the user
 // typing the URL, causing the site to have a site engagement score of at
 // least LOW.
-void NavigateToURL(Browser* browser, const GURL& url) {
+void NavigateToURLSync(Browser* browser, const GURL& url) {
+  content::TestNavigationObserver navigation_observer(
+      browser->tab_strip_model()->GetActiveWebContents(), 1);
+
   NavigateParams params(browser, url, ui::PAGE_TRANSITION_LINK);
   params.initiator_origin = url::Origin::Create(GURL("about:blank"));
   params.disposition = WindowOpenDisposition::CURRENT_TAB;
   params.is_renderer_initiated = true;
   ui_test_utils::NavigateToURL(&params);
-}
 
-// Same as NavigateToUrl, but wait for the load to complete before returning.
-void NavigateToURLSync(Browser* browser, const GURL& url) {
-  content::TestNavigationObserver navigation_observer(
-      browser->tab_strip_model()->GetActiveWebContents(), 1);
-  NavigateToURL(browser, url);
   navigation_observer.Wait();
 }
 
@@ -117,6 +119,8 @@ void NavigateToURLSync(Browser* browser, const GURL& url) {
 void LoadAndCheckInterstitialAt(Browser* browser, const GURL& url) {
   content::WebContents* web_contents =
       browser->tab_strip_model()->GetActiveWebContents();
+  content::WebContentsConsoleObserver console_observer(web_contents);
+  console_observer.SetPattern(kConsoleMessage);
 
   EXPECT_EQ(nullptr, GetCurrentInterstitial(web_contents));
 
@@ -124,6 +128,10 @@ void LoadAndCheckInterstitialAt(Browser* browser, const GURL& url) {
   EXPECT_EQ(LookalikeUrlBlockingPage::kTypeForTesting,
             GetInterstitialType(web_contents));
   EXPECT_FALSE(IsUrlShowing(browser));
+
+  console_observer.Wait();
+  EXPECT_TRUE(
+      base::MatchPattern(console_observer.GetMessageAt(0u), kConsoleMessage));
 }
 
 void SendInterstitialCommand(content::WebContents* web_contents,
--- components/lookalikes/core/lookalike_url_ui_util.cc
+++ components/lookalikes/core/lookalike_url_ui_util.cc
@@ -40,7 +40,8 @@ void ReportUkmForLookalikeUrlBlockingPageIfNeeded(
 
 void PopulateLookalikeUrlBlockingPageStrings(
     base::DictionaryValue* load_time_data,
-    const GURL& safe_url) {
+    const GURL& safe_url,
+    const GURL& request_url) {
   CHECK(load_time_data);
 
   PopulateStringsForSharedHTML(load_time_data);
@@ -95,6 +96,7 @@ void PopulateLookalikeUrlBlockingPageStrings(
     }
 #endif
   }
+  load_time_data->SetString("lookalikeRequestHostname", request_url.host());
 }
 
 void PopulateStringsForSharedHTML(base::DictionaryValue* load_time_data) {
