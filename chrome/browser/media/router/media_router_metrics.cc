eb8e91c28c89752820aba5ff436ac07695e86570
mark a. foltz <mfoltz@chromium.org>
2020-08-20 19:05:05 +0000

    [Media Router] Adds an UMA histogram to record presentation URL usage.
    This adds a histogram MediaRouter.PresentationUrl.BySink that records
    the types of URLs passed into the Presentation API and what types of
    sinks (screens/devices) they are rendered on.
    
    This is to help assess the real-world usage of various scenarios and
    inform the future roadmap of the API.
    
    Note that the kExtension bucket will exclusively capture Google Meetings
    by the time this hits the beta channel; however, I'm naming that bucket
    kExtension for consistency with MediaRouteProviderId.
    
    Bug: 1112866
    Change-Id: Idc21cf0fff6d3b3984a21163ec26a01f4ed929df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2350840
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800261}

--- chrome/browser/media/router/media_router_metrics.cc
+++ chrome/browser/media/router/media_router_metrics.cc
@@ -12,8 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
 #include "base/time/default_clock.h"
+#include "chrome/common/media_router/media_route_provider_helper.h"
 #include "chrome/common/media_router/media_sink.h"
-#include "chrome/common/media_router/media_source.h"
+#include "extensions/common/constants.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
 
@@ -177,6 +178,8 @@ const char MediaRouterMetrics::kHistogramUiFirstAction[] =
     "MediaRouter.Ui.FirstAction";
 const char MediaRouterMetrics::kHistogramUiIconStateAtInit[] =
     "MediaRouter.Ui.IconStateAtInit";
+constexpr char kHistogramPresentationRequestUrlBySink[] =
+    "MediaRouter.PresentationRequest.UrlBySink";
 
 // static
 void MediaRouterMetrics::RecordMediaRouterDialogOrigin(
@@ -337,6 +340,44 @@ void MediaRouterMetrics::RecordDialogActivationLocationAndCastMode(
       kHistogramUiDialogActivationLocationAndCastMode,
       GetActivationLocationAndCastMode(activation_location, cast_mode,
                                        is_icon_pinned));
+}
+
+void MediaRouterMetrics::RecordPresentationRequestUrlBySink(
+    const MediaSource& source,
+    MediaRouteProviderId provider_id) {
+  PresentationUrlBySink value = PresentationUrlBySink::kUnknown;
+  // URLs that can be rendered in offscreen tabs (for cloud or Chromecast
+  // sinks), or on a wired display.
+  bool is_normal_url = source.url().SchemeIs(url::kHttpsScheme) ||
+                       source.url().SchemeIs(extensions::kExtensionScheme) ||
+                       source.url().SchemeIs(url::kFileScheme);
+  switch (provider_id) {
+    case MediaRouteProviderId::EXTENSION:
+      if (is_normal_url) {
+        value = PresentationUrlBySink::kNormalUrlToExtension;
+      }
+      break;
+    case MediaRouteProviderId::WIRED_DISPLAY:
+      if (is_normal_url) {
+        value = PresentationUrlBySink::kNormalUrlToWiredDisplay;
+      }
+      break;
+    case MediaRouteProviderId::CAST:
+      if (source.IsCastPresentationUrl()) {
+        value = PresentationUrlBySink::kCastUrlToChromecast;
+      } else if (is_normal_url) {
+        value = PresentationUrlBySink::kNormalUrlToChromecast;
+      }
+      break;
+    case MediaRouteProviderId::DIAL:
+      if (source.IsDialSource()) {
+        value = PresentationUrlBySink::kDialUrlToDial;
+      }
+      break;
+    case MediaRouteProviderId::UNKNOWN:
+      break;
+  }
+  base::UmaHistogramEnumeration(kHistogramPresentationRequestUrlBySink, value);
 }
 
 }  // namespace media_router
