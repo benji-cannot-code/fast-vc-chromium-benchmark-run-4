7ef0b3ba30ec22880ff16926a03dfb960cc67ba0
Mugdha Lakhani <nator@chromium.org>
2020-08-21 10:18:42 +0000

    [WebLayer] Let the embedder know if  navigation entry will be replaced.
    Bug: 1119726
    Change-Id: If3a82e7718590e51bafeee191e2176cd2a3f2a4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2366754
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800527}

--- weblayer/public/java/org/chromium/weblayer/NavigateParams.java
+++ weblayer/public/java/org/chromium/weblayer/NavigateParams.java
@@ -51,4 +51,13 @@ public class NavigateParams {
     org.chromium.weblayer_private.interfaces.NavigateParams toInterfaceParams() {
         return mInterfaceParams;
     }
+
+    /**
+     * Returns true if the current navigation will be replaced, false otherwise.
+     *
+     * @since 83
+     */
+    public boolean getShouldReplaceCurrentEntry() {
+        return mInterfaceParams.mShouldReplaceCurrentEntry;
+    }
 }
