b9c0c913eeab9d4d44993e4370d61fe17bddf043
James Hawkins <jhawkins@chromium.org>
2020-08-21 01:57:48 +0000

    cr_components/network: Convert NetworkPropertyListMojo to Polymer 3.
    Bug: 1045266
    Test: NetworkComponentsNetworkPropertyListMojoTest*
    Change-Id: I14e9be994b77445a4106b156fcce3e48421c340b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368494
    Auto-Submit: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800421}

--- ui/webui/resources/cr_components/chromeos/network/network_property_list_mojo.js
+++ ui/webui/resources/cr_components/chromeos/network/network_property_list_mojo.js
@@ -8,9 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * in a list. This also supports editing fields inline for fields listed in
  * editFieldTypes.
  */
-(function() {
-'use strict';
-
 Polymer({
   is: 'network-property-list-mojo',
 
@@ -383,4 +380,3 @@ Polymer({
     return '';
   },
 });
-})();
