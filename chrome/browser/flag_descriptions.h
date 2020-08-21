a92cb5ae2527a8be28781bd372a53585012af510
Irina Fedorova <irfedorova@google.com>
2020-08-21 12:26:22 +0000

    Add Passwords Weakness Check feature flag
    This feature flag allows as to enable checking passwords for weakness
    in chrome://settings/passwords/check.
    
    Bug: 1119752
    Change-Id: I24984ee3cd64778260195b4319262c8b7851895d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367033
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800539}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1925,6 +1925,9 @@ extern const char kOmniboxKeywordSearchButtonDescription[];
 extern const char kOmniboxReverseAnswersName[];
 extern const char kOmniboxReverseAnswersDescription[];
 
+extern const char kPasswordsWeaknessCheckName[];
+extern const char kPasswordsWeaknessCheckDescription[];
+
 extern const char kTabFreezeName[];
 extern const char kTabFreezeDescription[];
 
