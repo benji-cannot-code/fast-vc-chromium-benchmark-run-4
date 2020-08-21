aaa7af40331c3ee6f9641b967d5c4dc184a86b97
Ken MacKay <kmackay@chromium.org>
2020-08-21 16:32:57 +0000

    [Chromecast] Allow sounds to be played as other content types
    Merge-With: eureka-internal/439192
    
    Bug: internal b/162442148
    Change-Id: I48246585ffde00158b1aa9ee7a6c403b6668259f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363447
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800598}

--- chromecast/media/api/cast_sounds_manager.h
+++ chromecast/media/api/cast_sounds_manager.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/time/time.h"
+#include "chromecast/public/volume_control.h"
 
 namespace chromecast {
 namespace media {
@@ -44,7 +45,7 @@ class CastSoundsManager {
                                      bool repeat) = 0;
 
   // Plays the sound added for |key|.
-  virtual void Play(int key) = 0;
+  virtual void Play(int key, AudioContentType content_type) = 0;
 
   // Stops playing the sound added for |key| if it is currently playing.
   virtual void Stop(int key) = 0;
