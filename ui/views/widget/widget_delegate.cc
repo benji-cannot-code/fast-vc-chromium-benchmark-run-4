82467a5548848420372a10e3c14a4c1bcc59f2ae
Elly Fong-Jones <ellyjones@chromium.org>
2020-08-21 02:11:13 +0000

    views: remove WidgetDelegate::ShouldRestoreWindowSize
    This method has no overrides and always returns true. This change
    removes it, removes the only call to it, and simplifies code around
    that call site as a result.
    
    Bug: None
    Change-Id: I2c5b13552923f11615e5b2e629fc2da70fb26eb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367650
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800426}

--- ui/views/widget/widget_delegate.cc
+++ ui/views/widget/widget_delegate.cc
@@ -154,10 +154,6 @@ bool WidgetDelegate::GetSavedWindowPlacement(
   return display.bounds().Intersects(*bounds);
 }
 
-bool WidgetDelegate::ShouldRestoreWindowSize() const {
-  return true;
-}
-
 void WidgetDelegate::WidgetInitializing(Widget* widget) {
   widget_ = widget;
   OnWidgetInitializing();
