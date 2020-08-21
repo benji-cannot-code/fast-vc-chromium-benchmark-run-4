ced2617f3074bd943bcfbdce16d34a70431a55fe
Yann Dago <ydago@chromium.org>
2020-08-21 16:34:37 +0000

    Change appconfig.xml  bundle_id to 'com.google.chrome.ios'
    Bug: 1109322
    Change-Id: I73d3143fdf37e84a953da464b561f15c4ee11827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368427
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800600}

--- components/policy/tools/template_writers/writers/jamf_writer_unittest.py
+++ components/policy/tools/template_writers/writers/jamf_writer_unittest.py
@@ -74,7 +74,7 @@ class JamfWriterUnitTests(writer_unittest_common.WriterUnittestCommon):
                 'type': policy_type
             }
         },
-        'title': 'com.google.chrome'
+        'title': 'com.google.chrome.ios'
     }
     if initial_type == 'int-enum' or initial_type == 'string-enum':
       output['properties'][policy_name]['enum'] = [1]
@@ -151,7 +151,7 @@ class JamfWriterUnitTests(writer_unittest_common.WriterUnittestCommon):
                 'type': 'integer'
             }
         },
-        'title': 'com.google.chrome'
+        'title': 'com.google.chrome.ios'
     }
     expected_json = _JsonFormat(expected)
 
@@ -269,7 +269,7 @@ class JamfWriterUnitTests(writer_unittest_common.WriterUnittestCommon):
                 }
             }
         },
-        'title': 'com.google.chrome'
+        'title': 'com.google.chrome.ios'
     }
 
     for i in range(0, 5):
