27975e6781ef4e670389e106d799c6a6da2112f9
Dan Sanders <sandersd@chromium.org>
2020-08-21 20:24:27 +0000

    [webcodecs] Implement H.264 bitstream conversion.
    WebCodecs expects AVC-formatted H.264 streams, but our decoders expect
    Annex B.
    
    Bug: 1119947
    Change-Id: I8a0d92450890a9d625d1b2a67b1ea20377bdfd1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368162
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800704}

--- third_party/blink/renderer/modules/webcodecs/decoder_template.cc
+++ third_party/blink/renderer/modules/webcodecs/decoder_template.cc
@@ -74,7 +74,7 @@ void DecoderTemplate<Traits>::configure(const ConfigType* config,
   String console_message;
 
   CodecConfigEval eval =
-      Traits::CreateMediaConfig(*config, media_config.get(), &console_message);
+      MakeMediaConfig(*config, media_config.get(), &console_message);
   switch (eval) {
     case CodecConfigEval::kInvalid:
       exception_state.ThrowTypeError(console_message);
@@ -107,7 +107,7 @@ void DecoderTemplate<Traits>::decode(const InputType* chunk,
 
   Request* request = MakeGarbageCollected<Request>();
   request->type = Request::Type::kDecode;
-  request->chunk = chunk;
+  request->decoder_buffer = MakeDecoderBuffer(*chunk);
   requests_.push_back(request);
   ++requested_decodes_;
   ProcessRequests();
@@ -245,7 +245,6 @@ bool DecoderTemplate<Traits>::ProcessDecodeRequest(Request* request) {
   DCHECK(!is_closed_);
   DCHECK(!pending_request_);
   DCHECK_EQ(request->type, Request::Type::kDecode);
-  DCHECK(request->chunk);
   DCHECK_GT(requested_decodes_, 0);
 
   // TODO(sandersd): If a reset has been requested, complete immediately.
@@ -261,9 +260,8 @@ bool DecoderTemplate<Traits>::ProcessDecodeRequest(Request* request) {
     return false;
   }
 
-  scoped_refptr<media::DecoderBuffer> decoder_buffer =
-      Traits::MakeDecoderBuffer(*request->chunk);
-  if (decoder_buffer->data_size() == 0) {
+  // The request may be invalid, if so report that now.
+  if (!request->decoder_buffer || request->decoder_buffer->data_size() == 0) {
     HandleError();
     return false;
   }
@@ -277,7 +275,7 @@ bool DecoderTemplate<Traits>::ProcessDecodeRequest(Request* request) {
     ;
   pending_decodes_.Set(pending_decode_id_, request);
   --requested_decodes_;
-  decoder_->Decode(std::move(decoder_buffer),
+  decoder_->Decode(std::move(request->decoder_buffer),
                    WTF::Bind(&DecoderTemplate::OnDecodeDone,
                              WrapWeakPersistent(this), pending_decode_id_));
   return true;
@@ -492,7 +490,6 @@ void DecoderTemplate<Traits>::Trace(Visitor* visitor) const {
 
 template <typename Traits>
 void DecoderTemplate<Traits>::Request::Trace(Visitor* visitor) const {
-  visitor->Trace(chunk);
   visitor->Trace(resolver);
 }
 
