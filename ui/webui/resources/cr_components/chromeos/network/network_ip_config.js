4c83652281957e0509deea6f2b073ffe687dc008
James Hawkins <jhawkins@chromium.org>
2020-08-21 21:37:59 +0000

    cr_components/network: Migrate NetworkIpConfig to Polymer 3.
    R=khorimoto@chromium.org
    
    Bug: 1045266
    Test: NetworkComponentsNetworkIpConfigTest*
    Change-Id: Ie94e86efab513c315fff2edf1da9b29b27de19e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368468
    Auto-Submit: James Hawkins <jhawkins@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800744}

--- ui/webui/resources/cr_components/chromeos/network/network_ip_config.js
+++ ui/webui/resources/cr_components/chromeos/network/network_ip_config.js
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Polymer element for displaying the IP Config properties for
  * a network state.
  */
-(function() {
-'use strict';
 
 /**
  * Returns the routing prefix as a string for a given prefix length. If
@@ -360,4 +358,3 @@ Polymer({
     });
   },
 });
-})();
