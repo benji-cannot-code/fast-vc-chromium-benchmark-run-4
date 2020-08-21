ca9ea558f50855110a238c8ab126a02485be4310
Noah Rose Ledesma <noahrose@google.com>
2020-08-21 23:38:05 +0000

    Reland "GMC: Redesign audio picker UI"
    This is a reland of 7d2d8cc1bff2782df4cd5e91891114dce0d4a0e6
    
    Original change's description:
    > GMC: Redesign audio picker UI
    > 
    > Implement the new UI for the audio device picker in global media
    > controls. As part of this change, the MediaNotificationBackground will
    > draw a gradient between the device picker and notification artwork if
    > the picker is availabile.
    > 
    > Screenshots of this updated UI will be posted on the crbug.
    > 
    > Change-Id: Ic7186c770102663b08e85b56eeddfbfa13833c3c
    > Bug: 1117160
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2358512
    > Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#800243}
    
    Bug: 1117160
    Change-Id: I3adf5e58dcb8cd77758abd75aa5d497ab0f50c2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368504
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/master@{#800781}

--- components/media_message_center/media_notification_view_impl.cc
+++ components/media_message_center/media_notification_view_impl.cc
@@ -475,6 +475,12 @@ void MediaNotificationViewImpl::OnThemeChanged() {
   UpdateForegroundColor();
 }
 
+void MediaNotificationViewImpl::UpdateAudioDeviceSelectorAvailability(
+    bool availability) {
+  GetMediaNotificationBackground()->UpdateAudioDeviceSelectorAvailability(
+      availability);
+}
+
 views::Button* MediaNotificationViewImpl::GetHeaderRowForTesting() const {
   return header_row_;
 }
