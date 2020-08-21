d41a5f5db19cf5ead0ad3e78ba0af4a42f524815
Bo Majewski <majewski@chromium.org>
2020-08-21 01:26:34 +0000

    Explicitly disallow copy and assign
    DISALLOW_COPY_AND_ASSIGN macro is deprecated as indicated on the review
    of CL 2251524.
    
    Bug: 1111229
    Change-Id: Ib25ce85a8ace16b8eb7b00a0974fec5a73e536fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2362364
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800414}

--- chrome/browser/ui/webui/chromeos/file_manager/file_manager_ui.h
+++ chrome/browser/ui/webui/chromeos/file_manager/file_manager_ui.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/macros.h"
 #include "chrome/browser/ui/webui/chromeos/file_manager/file_manager.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -27,6 +26,10 @@ class FileManagerUI : public ui::MojoWebUIController,
   explicit FileManagerUI(content::WebUI* web_ui);
   ~FileManagerUI() override;
 
+  // Disallow copy and assign.
+  FileManagerUI(const FileManagerUI&) = delete;
+  FileManagerUI& operator=(const FileManagerUI&) = delete;
+
   void BindInterface(
       mojo::PendingReceiver<mojom::PageHandlerFactory> pending_receiver);
 
@@ -40,7 +43,6 @@ class FileManagerUI : public ui::MojoWebUIController,
   std::unique_ptr<FileManagerPageHandler> page_handler_;
 
   WEB_UI_CONTROLLER_TYPE_DECL();
-  DISALLOW_COPY_AND_ASSIGN(FileManagerUI);
 };
 
 }  // namespace file_manager
