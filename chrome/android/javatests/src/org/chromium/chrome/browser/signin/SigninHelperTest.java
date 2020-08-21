40837fa1c5ccafe3aeffd27c41e52f966c4769f9
Alice Wang <aliceywang@chromium.org>
2020-08-21 17:43:10 +0000

    [Android][Test] Batch SigninHelperTest
    This CL batches signin.SigninHelperTest.
    
    Bug: 1120258
    Change-Id: Ie2b907c070437a2f76005c181763c78db7ea75e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367860
    Auto-Submit: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800629}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninHelperTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninHelperTest.java
@@ -7,12 +7,14 @@ package org.chromium.chrome.browser.signin;
 
 import androidx.test.filters.SmallTest;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.test.util.Batch;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.browser.signin.AccountManagerTestRule;
 import org.chromium.chrome.test.util.browser.signin.MockChangeEventChecker;
@@ -21,6 +23,7 @@ import org.chromium.chrome.test.util.browser.signin.MockChangeEventChecker;
  * Instrumentation tests for {@link SigninHelper}.
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
+@Batch(Batch.UNIT_TESTS)
 public class SigninHelperTest {
     @Rule
     public final AccountManagerTestRule mAccountManagerTestRule = new AccountManagerTestRule();
@@ -30,6 +33,11 @@ public class SigninHelperTest {
     @Before
     public void setUp() {
         mEventChecker = new MockChangeEventChecker();
+    }
+
+    @After
+    public void tearDown() {
+        SigninPreferencesManager.getInstance().clearAccountsStateSharedPrefsForTesting();
     }
 
     @Test
