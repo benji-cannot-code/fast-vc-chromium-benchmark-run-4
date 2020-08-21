cec4859c7a91efaa35afe303f48c25da27fd67ea
Thomas Lukaszewicz <tluk@chromium.org>
2020-08-21 16:05:39 +0000

    Views: Update the logic for invoking an instance of Tab Search
    This CL updates the TabSearchBubbleView and the TabSearchButton
    such that there can only be a single instance of the
    Tab Search UI shown at any given time.
    
    This CL also changes the TabSearchButton such that it enters the
    active state immediately on bubble creation so that the bubble's
    status is communicated to the user while it loads in
    asynchronously.
    
    This behavior mimics that in the IconLabelBubbleView.
    
    Bug: 1099917
    Change-Id: Ifb5602e55750fd6dd4b834c4dc42835ca08d0f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354509
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800585}

--- chrome/browser/ui/views/tabs/new_tab_button.h
+++ chrome/browser/ui/views/tabs/new_tab_button.h
@@ -42,7 +42,7 @@ class NewTabButton : public views::ImageButton,
 
   void AnimateInkDropToStateForTesting(views::InkDropState state);
 
-  // views::View:
+  // views::ImageButton:
   const char* GetClassName() const override;
   void AddLayerBeneathView(ui::Layer* new_layer) override;
   void RemoveLayerBeneathView(ui::Layer* old_layer) override;
@@ -50,9 +50,11 @@ class NewTabButton : public views::ImageButton,
  protected:
   virtual void PaintIcon(gfx::Canvas* canvas);
 
+  TabStrip* tab_strip() { return tab_strip_; }
+
   SkColor GetForegroundColor() const;
 
-  // views::View:
+  // views::ImageButton:
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
 
  private:
--- chrome/browser/ui/views/tabs/tab_search_button.cc
+++ chrome/browser/ui/views/tabs/tab_search_button.cc
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/tabs/tab_search_button.h"
 
+#include "base/scoped_observer.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/ui/views/tab_search/tab_search_bubble_view.h"
+#include "chrome/browser/ui/views/tabs/tab_strip_controller.h"
 #include "ui/gfx/paint_vector_icon.h"
+#include "ui/views/widget/widget.h"
 
 namespace {
 constexpr int kIconSize = 20;
@@ -14,9 +18,36 @@ constexpr int kIconSize = 20;
 
 TabSearchButton::TabSearchButton(TabStrip* tab_strip,
                                  views::ButtonListener* listener)
-    : NewTabButton(tab_strip, listener) {
+    : NewTabButton(tab_strip, nullptr) {
   SetImageHorizontalAlignment(HorizontalAlignment::ALIGN_CENTER);
   SetImageVerticalAlignment(VerticalAlignment::ALIGN_MIDDLE);
+
+  auto menu_button_controller = std::make_unique<views::MenuButtonController>(
+      this, this,
+      std::make_unique<views::Button::DefaultButtonControllerDelegate>(this));
+  menu_button_controller_ = menu_button_controller.get();
+  SetButtonController(std::move(menu_button_controller));
+}
+
+TabSearchButton::~TabSearchButton() = default;
+
+void TabSearchButton::ButtonPressed(views::Button* sender,
+                                    const ui::Event& event) {
+  if (bubble_)
+    return;
+  bubble_ = TabSearchBubbleView::CreateTabSearchBubble(
+      tab_strip()->controller()->GetProfile(), this);
+  observed_bubble_widget_.Add(bubble_);
+
+  // Hold the pressed lock while the |bubble_| is active.
+  pressed_lock_ = menu_button_controller_->TakeLock();
+}
+
+void TabSearchButton::OnWidgetClosing(views::Widget* widget) {
+  DCHECK_EQ(bubble_, widget);
+  observed_bubble_widget_.Remove(bubble_);
+  bubble_ = nullptr;
+  pressed_lock_.reset();
 }
 
 void TabSearchButton::PaintIcon(gfx::Canvas* canvas) {
