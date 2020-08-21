46bd7ec970bd7d1f9a7ae83869a7df3df15adf1e
Sinan Sahin <sinansahin@google.com>
2020-08-21 22:29:29 +0000

    🤠 Disable TrustedWebActivityPermissionsTest on Tablets
    Bug: 1120707
    Change-Id: Ifd791aef0358e6a41d4ec58e2cf9d9fa214fa850
    Tbr: peconn
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368320
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#800755}

--- chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/permissiondelegation/TrustedWebActivityPermissionsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/permissiondelegation/TrustedWebActivityPermissionsTest.java
@@ -22,6 +22,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.browser.ChromeApplication;
 import org.chromium.chrome.browser.ShortcutHelper;
 import org.chromium.chrome.browser.customtabs.CustomTabActivityTestRule;
@@ -34,6 +35,7 @@ import org.chromium.components.embedder_support.util.Origin;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.net.test.EmbeddedTestServer;
 import org.chromium.net.test.ServerCertificate;
+import org.chromium.ui.test.util.UiDisableIf;
 
 import java.util.concurrent.TimeoutException;
 
@@ -43,6 +45,8 @@ import java.util.concurrent.TimeoutException;
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+// See: https://crbug.com/1120707
+@DisableIf.Device(type = {UiDisableIf.TABLET})
 public class TrustedWebActivityPermissionsTest {
     @Rule
     public CustomTabActivityTestRule mCustomTabActivityTestRule = new CustomTabActivityTestRule();
