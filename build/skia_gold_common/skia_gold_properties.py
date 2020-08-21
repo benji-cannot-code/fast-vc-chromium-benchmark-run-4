aca03e7122d396714774949c37659bde760beb0c
Brian Sheedy <bsheedy@chromium.org>
2020-08-21 18:28:06 +0000

    Add Gold CRS arg
    Adds the --code-review-system flag to the Android test runner and adds
    the necessary code to handle it for use with Gold. This is necessary
    because we will be specifying 'gerrit-internal' when running tryjobs
    for internal CLs so that Gold knows where to comment if new images are
    generated.
    
    Bug: 1077274
    Change-Id: I5ce089a511275afed0c06378d9d49e977e330f85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2366072
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800652}

--- build/skia_gold_common/skia_gold_properties.py
+++ build/skia_gold_common/skia_gold_properties.py
@@ -30,10 +30,9 @@ class SkiaGoldProperties(object):
     self._local_pixel_tests = None
     self._no_luci_auth = None
     self._bypass_skia_gold_functionality = None
-
+    self._code_review_system = None
     # Could in theory be configurable, but hard-coded for now since there's
     # no plan to support anything else.
-    self._code_review_system = 'gerrit'
     self._continuous_integration_system = 'buildbucket'
 
     self._InitializeProperties(args)
@@ -47,7 +46,7 @@ class SkiaGoldProperties(object):
 
   @property
   def code_review_system(self):
-    return self._code_review_system
+    return self._code_review_system or 'gerrit'
 
   @property
   def git_revision(self):
@@ -120,6 +119,9 @@ class SkiaGoldProperties(object):
 
     if hasattr(args, 'bypass_skia_gold_functionality'):
       self._bypass_skia_gold_functionality = args.bypass_skia_gold_functionality
+
+    if hasattr(args, 'code_review_system'):
+      self._code_review_system = args.code_review_system
 
     # Will be automatically determined later if needed.
     if not hasattr(args, 'git_revision') or not args.git_revision:
