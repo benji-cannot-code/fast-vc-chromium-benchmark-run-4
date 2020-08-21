21f1a5117b41738f7ea2b13106551c53debc9457
David Black <dmblack@google.com>
2020-08-21 20:07:57 +0000

    Remove Assistant warmer welcome code.
    Feature deprecated, this code is unused.
    
    Bug: b:157510970
    Change-Id: Ie6b0c6f7d75d2b11a41056b7811ff596c000569d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368417
    Reviewed-by: Jeroen Dhollander <jeroendh@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#800692}

--- chromeos/services/assistant/public/cpp/assistant_enums.h
+++ chromeos/services/assistant/public/cpp/assistant_enums.h
@@ -55,7 +55,7 @@ enum class AssistantQuerySource {
   kVoiceInput = 5,
   kProactiveSuggestions = 6,
   kLibAssistantInitiated = 7,
-  kWarmerWelcome = 8,
+  // kWarmerWelcomeDeprecated = 8,
   kConversationStarter = 9,
   kWhatsOnMyScreen = 10,
   kQuickAnswers = 11,
