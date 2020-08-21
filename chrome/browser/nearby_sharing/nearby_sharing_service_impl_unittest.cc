43cc109557d89412668da508f710ae8e7d0361fd
Josh Nohle <nohle@google.com>
2020-08-21 22:40:25 +0000

    [Nearby] Reset testing factories on unit-test tear-down
    Fix heap-use-after-free error in Nearby Share unit tests caused by not
    reseting the testing factories after a unit test finishes.
    
    Also, reorder member variables in certificate manager unit tests in
    rough order of creation time.
    
    Change-Id: I9c17f467f388716b8f83fb59dceb47d80282da41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2369498
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Commit-Queue: James Vecore <vecore@google.com>
    Auto-Submit: Josh Nohle <nohle@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#800757}

--- chrome/browser/nearby_sharing/nearby_sharing_service_impl_unittest.cc
+++ chrome/browser/nearby_sharing/nearby_sharing_service_impl_unittest.cc
@@ -322,6 +322,12 @@ class NearbySharingServiceImplTest : public testing::Test {
     }
 
     profile_manager_.DeleteAllTestingProfiles();
+
+    NearbyShareLocalDeviceDataManagerImpl::Factory::SetFactoryForTesting(
+        nullptr);
+    NearbyShareContactManagerImpl::Factory::SetFactoryForTesting(nullptr);
+    NearbyShareCertificateManagerImpl::Factory::SetFactoryForTesting(nullptr);
+    FastInitiationManager::Factory::SetFactoryForTesting(nullptr);
   }
 
   std::unique_ptr<NearbySharingServiceImpl> CreateService(
