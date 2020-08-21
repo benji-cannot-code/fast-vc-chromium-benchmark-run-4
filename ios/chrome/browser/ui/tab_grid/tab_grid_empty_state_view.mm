d71cdbef75afcf2a1b4ff333213c83efb7e93d28
Tommy Martino <tmartino@google.com>
2020-08-21 23:44:40 +0000

    Fix alignment issues on Tab Switcher
    The text on the Tab Switcher is currently misaligned in landscape mode
    on notched phones. This affects both the illustrated-empty-states on
    and off cases.
    
    Before and After Screenshots (googlers only, sorry):
    https://docs.google.com/presentation/d/1cZ151DN3Pywk2Ry-NSrMcM-Vf9v2Qbx35GBZFslSoTk/edit#slide=id.g91d5f68a07_0_6
    
    Bug: 1116296
    Change-Id: Icfa15e02abfba5c07b792adb168820755a596206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2369260
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800785}

--- ios/chrome/browser/ui/tab_grid/tab_grid_empty_state_view.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_empty_state_view.mm
@@ -91,7 +91,10 @@ const CGFloat kImageWidth = 150.0;
     if (self.subviews.count == 0)
       [self setupViews];
     [self.container.widthAnchor
-        constraintEqualToAnchor:self.safeAreaLayoutGuide.widthAnchor]
+        constraintLessThanOrEqualToAnchor:self.safeAreaLayoutGuide.widthAnchor]
+        .active = YES;
+    [self.container.centerXAnchor
+        constraintEqualToAnchor:self.safeAreaLayoutGuide.centerXAnchor]
         .active = YES;
   }
 }
@@ -172,6 +175,7 @@ const CGFloat kImageWidth = 150.0;
     [topLabel.bottomAnchor
         constraintEqualToAnchor:bottomLabel.topAnchor
                        constant:-kTabGridEmptyStateVerticalMargin],
+
     [bottomLabel.leadingAnchor constraintEqualToAnchor:container.leadingAnchor],
     [bottomLabel.trailingAnchor
         constraintEqualToAnchor:container.trailingAnchor],
@@ -180,7 +184,6 @@ const CGFloat kImageWidth = 150.0;
 
     [container.topAnchor constraintEqualToAnchor:scrollView.topAnchor],
     [container.bottomAnchor constraintEqualToAnchor:scrollView.bottomAnchor],
-    [container.centerXAnchor constraintEqualToAnchor:scrollView.centerXAnchor],
 
     [scrollView.centerYAnchor constraintEqualToAnchor:self.centerYAnchor],
     [scrollView.topAnchor constraintGreaterThanOrEqualToAnchor:self.topAnchor],
