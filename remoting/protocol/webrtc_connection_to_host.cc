28baaa9caceb402088f2844dd27d4019f48c6560
Lambros Lambrou <lambroslambrou@chromium.org>
2020-08-21 20:28:48 +0000

    [remoting] Log route-change events for WebRTC clients.
    WebRTC provides a native C++ API for getting notified of changes to
    the selected ICE candidate-pair. This CL uses it to notify the
    HostStatusObserver of route changes, which enables the HostEventLogger
    to log the connecting client's IP address.
    
    Bug: 1119960
    Change-Id: Ifa22faaeeddbf4f19e8ed422a581b840ec4caa33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368428
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800706}

--- remoting/protocol/webrtc_connection_to_host.cc
+++ remoting/protocol/webrtc_connection_to_host.cc
@@ -173,6 +173,9 @@ void WebrtcConnectionToHost::OnWebrtcTransportMediaStreamRemoved(
     video_adapter_.reset();
 }
 
+void WebrtcConnectionToHost::OnWebrtcTransportRouteChanged(
+    const TransportRoute& route) {}
+
 void WebrtcConnectionToHost::OnChannelInitialized(
     ChannelDispatcherBase* channel_dispatcher) {
   NotifyIfChannelsReady();
