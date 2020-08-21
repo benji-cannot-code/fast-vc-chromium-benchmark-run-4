ff3d6a5060b67052ea9ed6d6623426d3f080043e
Mohamed Amir Yosef <mamir@chromium.org>
2020-08-21 12:22:42 +0000

    [Passwords] Introduce CompromisedCredentialsReader
    This CL only introduce the simplest version of the reader which handles
    only one store.
    
    Follow-up CL will wire it up to the rest of the code, and then make it
    handle multiple stores.
    
    Bug: 1108422
    Change-Id: If5202f5112cb841bd1c3ac65b37b5a1fa0eade4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367065
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800538}

---
+++ components/password_manager/core/browser/ui/compromised_credentials_reader.cc
@@ -0,0 +1,41 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/core/browser/ui/compromised_credentials_reader.h"
+
+namespace password_manager {
+CompromisedCredentialsReader::CompromisedCredentialsReader(PasswordStore* store)
+    : store_(store) {
+  DCHECK(store_);
+  observed_password_store_.Add(store_);
+}
+
+CompromisedCredentialsReader::~CompromisedCredentialsReader() = default;
+
+void CompromisedCredentialsReader::Init() {
+  store_->GetAllCompromisedCredentials(this);
+}
+
+void CompromisedCredentialsReader::OnCompromisedCredentialsChanged() {
+  // Cancel ongoing requests to the password store and issue a new request.
+  cancelable_task_tracker()->TryCancelAll();
+  store_->GetAllCompromisedCredentials(this);
+}
+
+void CompromisedCredentialsReader::OnGetCompromisedCredentials(
+    std::vector<CompromisedCredentials> compromised_credentials) {
+  for (auto& observer : observers_)
+    observer.OnCompromisedCredentialsChanged(compromised_credentials);
+}
+
+void CompromisedCredentialsReader::AddObserver(Observer* observer) {
+  observers_.AddObserver(observer);
+}
+
+void CompromisedCredentialsReader::RemoveObserver(Observer* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+}  // namespace password_manager
