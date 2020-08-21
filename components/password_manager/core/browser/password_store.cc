a609f6942b6cd2a07c6b3e0ec31f4a8c42556e5d
Mohamed Amir Yosef <mamir@chromium.org>
2020-08-21 11:37:45 +0000

    [Passwords] Introduce OnCompromisedCredentialsChangedIn() in ...
    ... DatabaseCompromisedCredentialsObserver.
    This is similar to OnCompromisedCredentialsChanged(), but also receives
    the  originating PasswordStore as a parameter.
    
    Bug: 1108422
    Change-Id: I76fffc36c65554bbe3e812c9e21ab229437f20a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368554
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800535}

--- components/password_manager/core/browser/password_store.cc
+++ components/password_manager/core/browser/password_store.cc
@@ -90,6 +90,11 @@ void PasswordStore::Observer::OnLoginsChangedIn(
   OnLoginsChanged(changes);
 }
 
+void PasswordStore::DatabaseCompromisedCredentialsObserver::
+    OnCompromisedCredentialsChangedIn(PasswordStore* store) {
+  OnCompromisedCredentialsChanged();
+}
+
 #if defined(SYNC_PASSWORD_REUSE_DETECTION_ENABLED)
 PasswordStore::CheckReuseRequest::CheckReuseRequest(
     PasswordReuseDetectorConsumer* consumer)
@@ -787,8 +792,10 @@ void PasswordStore::InvokeAndNotifyAboutCompromisedPasswordsChange(
   DCHECK(background_task_runner_->RunsTasksInCurrentSequence());
   if (std::move(callback).Run()) {
     compromised_credentials_observers_->Notify(
-        FROM_HERE, &DatabaseCompromisedCredentialsObserver::
-                       OnCompromisedCredentialsChanged);
+        FROM_HERE,
+        &DatabaseCompromisedCredentialsObserver::
+            OnCompromisedCredentialsChangedIn,
+        base::RetainedRef(this));
   }
 }
 
