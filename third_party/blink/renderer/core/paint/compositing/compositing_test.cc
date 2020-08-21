35f38c8a0cf2c6885777ae4c7187749a5107b7e9
jongdeok.kim <jongdeok.kim@navercorp.com>
2020-08-21 03:34:23 +0000

    Apply disallowing LCD text for will-change:transform to descendants.
    In this CL, create a transform node instead of setting Layer::SetHasWillChangeTransformHint().
    By doing so, each node can see if it's parents have will-change:transform compositing reason.
    
    Bug: 1114504
    Change-Id: Ia5e9fe82126dc343e0cffbdbccff3b2d4cf4fcac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2355660
    Commit-Queue: jongdeok.kim <jongdeok.kim@navercorp.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800448}

--- third_party/blink/renderer/core/paint/compositing/compositing_test.cc
+++ third_party/blink/renderer/core/paint/compositing/compositing_test.cc
@@ -95,6 +95,11 @@ class CompositingTest : public PaintTestConfigurations, public testing::Test {
     return LayerTreeHost()->property_trees();
   }
 
+  cc::TransformNode* GetTransformNode(const cc::Layer* layer) {
+    return GetPropertyTrees()->transform_tree.Node(
+        layer->transform_tree_index());
+  }
+
  private:
   PaintArtifactCompositor* paint_artifact_compositor() {
     return GetLocalFrameView()->GetPaintArtifactCompositor();
@@ -224,7 +229,8 @@ TEST_P(CompositingTest, WillChangeTransformHint) {
   )HTML");
   UpdateAllLifecyclePhases();
   auto* layer = CcLayerByDOMElementId("willChange");
-  EXPECT_TRUE(layer->has_will_change_transform_hint());
+  auto* transform_node = GetTransformNode(layer);
+  EXPECT_TRUE(transform_node->will_change_transform);
 }
 
 TEST_P(CompositingTest, WillChangeTransformHintInSVG) {
@@ -244,7 +250,8 @@ TEST_P(CompositingTest, WillChangeTransformHintInSVG) {
   )HTML");
   UpdateAllLifecyclePhases();
   auto* layer = CcLayerByDOMElementId("willChange");
-  EXPECT_TRUE(layer->has_will_change_transform_hint());
+  auto* transform_node = GetTransformNode(layer);
+  EXPECT_TRUE(transform_node->will_change_transform);
 }
 
 TEST_P(CompositingTest, PaintPropertiesWhenCompositingSVG) {
