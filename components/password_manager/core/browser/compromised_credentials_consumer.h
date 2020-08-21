09d44ffd8553b5b7570aadc406cf1d58a8c999e5
Mohamed Amir Yosef <mamir@chromium.org>
2020-08-21 13:19:37 +0000

    [Passwords] Introduce OnGetCompromisedCredentialsFrom() in  ...
    ... in CompromisedCredentialsConsumer.
    This is similar to OnGetCompromisedCredentials(), but also receives
    the  originating PasswordStore as a parameter.
    
    Bug: 1108422
    Change-Id: Icc4e8135f4181b17f23fbee23666d20ad4f04277
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367717
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800545}

--- components/password_manager/core/browser/compromised_credentials_consumer.h
+++ components/password_manager/core/browser/compromised_credentials_consumer.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace password_manager {
 
 struct CompromisedCredentials;
+class PasswordStore;
 
 // Reads information associated with compromised credentials history from the
 // PasswordStore. Reads are done asynchronously on a separate thread. It
@@ -27,6 +28,15 @@ class CompromisedCredentialsConsumer {
   // the associated |compromised_credentials|.
   virtual void OnGetCompromisedCredentials(
       std::vector<CompromisedCredentials> compromised_credentials) = 0;
+
+  // Like OnGetCompromisedCredentials(), but also receives the originating
+  // PasswordStore as a parameter. This is useful for consumers that query
+  // both the profile-scoped and the account-scoped store. The default
+  // implementation simply calls OnGetCompromisedCredentials(), so consumers
+  // that don't care about the store can just ignore this.
+  virtual void OnGetCompromisedCredentialsFrom(
+      PasswordStore* store,
+      std::vector<CompromisedCredentials> compromised_credentials);
 
   // The base::CancelableTaskTracker can be used for cancelling the tasks
   // associated with the consumer.
