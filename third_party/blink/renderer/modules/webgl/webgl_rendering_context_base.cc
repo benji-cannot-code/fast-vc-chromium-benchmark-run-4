4e74e49eb9def7124c68ec9e37f61745c5c3ff23
Dylan Cutler <dylancutler@google.com>
2020-08-22 04:05:38 +0000

    Fix naming style of GetUkmParameters().
    Before this change it was ukm_parameters(), which is against Blink
    naming convention.
    
    Also have the functions return a const ref.
    
    Bug: None
    Change-Id: Ie527e69712be37ac5b362d462acf9b2e376e7a29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2366115
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800813}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -3286,7 +3286,7 @@ void WebGLRenderingContextBase::RecordIdentifiableGLParameterDigest(
     IdentifiableToken value) {
   if (!IsUserInIdentifiabilityStudy() || !IsIdentifiableGLParam(pname))
     return;
-  if (base::Optional<UkmParameters> ukm_params = ukm_parameters()) {
+  if (const auto& ukm_params = GetUkmParameters()) {
     blink::IdentifiabilityMetricBuilder(ukm_params->source_id)
         .Set(blink::IdentifiableSurface::FromTypeAndInput(
                  blink::IdentifiableSurface::Type::kWebGLParameter, pname),
@@ -4584,8 +4584,7 @@ void WebGLRenderingContextBase::readPixels(
     GLenum type,
     MaybeShared<DOMArrayBufferView> pixels) {
   if (IsUserInIdentifiabilityStudy()) {
-    base::Optional<UkmParameters> ukm_params = ukm_parameters();
-    if (ukm_params) {
+    if (const auto& ukm_params = GetUkmParameters()) {
       blink::IdentifiabilityMetricBuilder(ukm_params->source_id)
           .Set(blink::IdentifiableSurface::FromTypeAndInput(
                    blink::IdentifiableSurface::Type::kCanvasReadback,
