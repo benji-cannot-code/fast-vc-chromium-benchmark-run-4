b1cc6985dec79949d0e87911dcca9d3c5a8132dd
Arthur Wang <wuwang@google.com>
2020-08-22 00:50:46 +0000

    Enable WPR test again and add to CQ bot android-marshmallow-x86-rel.
    Bug: 1060399
    Change-Id: Ice41c7ef4154a07c6078a66bda05283161e1c13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2316624
    Commit-Queue: Arthur Wang <wuwang@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800799}

--- chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/network_fetch/FeedNewTabPageCardInstrumentationTest.java
+++ chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/network_fetch/FeedNewTabPageCardInstrumentationTest.java
@@ -17,7 +17,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -45,8 +44,9 @@ public final class FeedNewTabPageCardInstrumentationTest {
             new ChromeActivityTestRule<>(ChromeActivity.class);
 
     @Rule
-    public ChromeRenderTestRule mRenderTestRule =
-            ChromeRenderTestRule.Builder.withPublicCorpus().build();
+    public ChromeRenderTestRule mRenderTestRule = ChromeRenderTestRule.Builder.withPublicCorpus()
+                                                          .setFailOnUnsupportedConfigs(true)
+                                                          .build();
 
     @Before
     public void setUp() {
@@ -58,7 +58,6 @@ public final class FeedNewTabPageCardInstrumentationTest {
     @Feature({"FeedNewTabPage", "WPRRecordReplayTest", "RenderTest"})
     @WPRArchiveDirectory("chrome/android/feed/core/javatests/src/org/chromium/chrome/"
             + "browser/feed/wpr_tests")
-    @FlakyTest(message = "https://crbug.com/1096656")
     public void
     launchNTP_withMultipleFeedCardsRendered() throws IOException, InterruptedException {
         mActivityTestRule.loadUrlInNewTab(UrlConstants.NTP_URL);
