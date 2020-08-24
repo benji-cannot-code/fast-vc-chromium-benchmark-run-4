2793679a85d7f2936aba864166b663c3ff07323c
David Bertoni <dbertoni@chromium.org>
2020-08-24 22:13:40 +0000

    [Extensions] Enable ExecuteScriptApiTest tests for SW-based extensons.
    This CL enables most of these tests. Two tests are flaky and are
    tracked by crbug.com/1115182. One test requires user gesture support,
    which is tracked by crbug.com/977629.
    
    Bug: 1093066
    Change-Id: I01d73276cc11a50ae991ae36e7b1be58a94fb9c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2317759
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801166}

--- chrome/test/data/extensions/api_test/executescript/basic/test.js
+++ chrome/test/data/extensions/api_test/executescript/basic/test.js
@@ -93,8 +93,9 @@ chrome.test.getConfig(function(config) {
             chrome.test.listenForever(chrome.tabs.onUpdated, onUpdated);
         chrome.tabs.update(tabId, {url: testFailureUrl});
 
-        function onUpdated(updatedTabId, changeInfo) {
-          if (updatedTabId !== tabId || changeInfo.url === testFailureUrl)
+        function onUpdated(updatedTabId, changeInfo, tab) {
+          if (updatedTabId !== tabId || tab.status != 'complete' ||
+             tab.url != testFailureUrl)
             return;
           var script_file = {};
           script_file.code = "document.title = 'executeScript';";
@@ -127,4 +128,3 @@ chrome.test.getConfig(function(config) {
 
   chrome.tabs.create({ url: testUrl });
 });
-
