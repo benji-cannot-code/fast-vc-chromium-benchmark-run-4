94a0bae62a66820b157e5bfd048a3d1fc413bfd4
Nazerke <Nazerke@google.com>
2020-08-24 13:25:54 +0000

    [iOS][tabmodel] Factor ClosingWebStateObserver into a BrowserAgent.
    As a tabmodel needs to be deprecated, this CL extracts
    ClosingWebStateObserver into a browser agent to remove its usage in
    TabModel.
    
    Bug: 1050111,1045575,1045142
    Change-Id: Ic6c5b65ff55bd211d38752b274c4dcbb9d854bcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287434
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800977}

--- ios/chrome/browser/main/browser_agent_util.mm
+++ ios/chrome/browser/main/browser_agent_util.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sessions/session_restoration_browser_agent.h"
 #import "ios/chrome/browser/sessions/session_service_ios.h"
 #import "ios/chrome/browser/snapshots/snapshot_browser_agent.h"
+#import "ios/chrome/browser/tabs/closing_web_state_observer_browser_agent.h"
 #include "ios/chrome/browser/tabs/synced_window_delegate_browser_agent.h"
 #import "ios/chrome/browser/url_loading/url_loading_browser_agent.h"
 #import "ios/chrome/browser/url_loading/url_loading_notifier_browser_agent.h"
@@ -43,6 +44,7 @@ void AttachBrowserAgents(Browser* browser) {
   UrlLoadingNotifierBrowserAgent::CreateForBrowser(browser);
   AppLauncherBrowserAgent::CreateForBrowser(browser);
   WebStateListMetricsBrowserAgent::CreateForBrowser(browser);
+  ClosingWebStateObserverBrowserAgent::CreateForBrowser(browser);
   SnapshotBrowserAgent::CreateForBrowser(browser);
 
   // Send Tab To Self is non-OTR only.
