47055b46a22b6d8f8aca451d0a4848268d31947b
Asanka Herath <asanka@chromium.org>
2020-08-24 18:23:48 +0000

    [privacy_budget] Remove obsolete comment.
    R=caraitto@chromium.org
    
    Bug: 973801
    Change-Id: Ice5b139becd3af1c163246d1fe2139c0bccc8c01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2370602
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801051}

--- chrome/browser/privacy_budget/privacy_budget_ukm_entry_filter_unittest.cc
+++ chrome/browser/privacy_budget/privacy_budget_ukm_entry_filter_unittest.cc
@@ -57,7 +57,6 @@ TEST(PrivacyBudgetUkmEntryFilterStandaloneTest,
   EXPECT_TRUE(filtered.empty());
 }
 
-// TODO(crbug.com/1117358) Flaky
 TEST(PrivacyBudgetUkmEntryFilterStandaloneTest, AllowsOtherMetricsByDefault) {
   IdentifiabilityStudyState::ResetStateForTesting();
   TestingPrefServiceSimple pref_service;
