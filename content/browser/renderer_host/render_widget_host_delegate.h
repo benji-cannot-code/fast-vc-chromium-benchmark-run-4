57d36d20cfcbc92152219315ed59205df62365fb
Shimi Zhang <ctzsm@chromium.org>
2020-08-24 21:48:25 +0000

    content: Remove InputEventShim
    There is no actual usage of this abstract class anymore.
    
    Bug: 533069
    Change-Id: I31ce5c99727a1ce11918b22a2252b3113c2c8878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2371683
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801161}

--- content/browser/renderer_host/render_widget_host_delegate.h
+++ content/browser/renderer_host/render_widget_host_delegate.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "build/build_config.h"
 #include "components/viz/common/vertical_scroll_direction.h"
-#include "content/browser/renderer_host/input_event_shim.h"
 #include "content/common/content_export.h"
 #include "content/common/drag_event_source_info.h"
 #include "content/public/common/drop_data.h"
