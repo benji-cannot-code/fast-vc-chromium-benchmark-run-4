c44497a649fc8aadde7b681fcfa47e3a77a0c33d
Ryan Hansberry <hansberry@chromium.org>
2020-08-24 20:59:38 +0000

    [Nearby] Create concrete BlePeripheral and skeleton BleMedium.
    Subsequent CLs will use Nearby's bluetooth::mojom::Adapter to
    scan for and register BLE advertisements.
    
    Note that incoming and outgoing BLE GATT connections are not
    supported by Nearby Chrome at the moment and the respective
    methods on BleMedium are left as no-ops.
    
    See go/nearby-chrome-bt for more details.
    
    Bug: b:154845685
    Change-Id: I31537b957129a7cac6272b5f9eed3157a2e6dfa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364032
    Reviewed-by: James Vecore <vecore@google.com>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801134}

--- chrome/services/sharing/nearby/platform.cc
+++ chrome/services/sharing/nearby/platform.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/services/sharing/nearby/nearby_connections.h"
 #include "chrome/services/sharing/nearby/platform_v2/atomic_boolean.h"
 #include "chrome/services/sharing/nearby/platform_v2/atomic_uint32.h"
+#include "chrome/services/sharing/nearby/platform_v2/ble_medium.h"
 #include "chrome/services/sharing/nearby/platform_v2/bluetooth_adapter.h"
 #include "chrome/services/sharing/nearby/platform_v2/bluetooth_classic_medium.h"
 #include "chrome/services/sharing/nearby/platform_v2/condition_variable.h"
@@ -137,11 +138,13 @@ ImplementationPlatform::CreateBluetoothClassicMedium(
 
 std::unique_ptr<BleMedium> ImplementationPlatform::CreateBleMedium(
     api::BluetoothAdapter& adapter) {
-  return nullptr;
+  // TODO (hansberry): Inject bluetooth::mojom::Adapter into BleMedium.
+  return std::make_unique<chrome::BleMedium>();
 }
 
 std::unique_ptr<ble_v2::BleMedium> ImplementationPlatform::CreateBleV2Medium(
     api::BluetoothAdapter& adapter) {
+  // Do nothing. ble_v2::BleMedium is not yet supported in Chrome Nearby.
   return nullptr;
 }
 
---
+++ chrome/services/sharing/nearby/platform_v2/ble_peripheral.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/services/sharing/nearby/platform_v2/ble_peripheral.h"
+
+namespace location {
+namespace nearby {
+namespace chrome {
+
+BlePeripheral::BlePeripheral(api::BluetoothDevice& bluetooth_device)
+    : bluetooth_device_(bluetooth_device) {}
+
+BlePeripheral::~BlePeripheral() = default;
+
+api::BluetoothDevice& BlePeripheral::GetBluetoothDevice() {
+  return bluetooth_device_;
+}
+
+}  // namespace chrome
+}  // namespace nearby
+}  // namespace location
