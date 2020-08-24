34648cd1b7802aaf2093fef6c20fe6523a33b571
Adam Langley <agl@chromium.org>
2020-08-24 18:51:31 +0000

    qr: fix several issues with larger QR codes.
    When building larger QR codes, the previous code would read off the end
    of a buffer and interleaved blocks incorrectly, thus generating invalid
    QR codes.
    
    Change-Id: Id00dcd5c3d5f4bacde281baaa759c3920d664048
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2369258
    Reviewed-by: Travis Skare <skare@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801089}

--- chrome/common/qr_code_generator/qr_code_generator.h
+++ chrome/common/qr_code_generator/qr_code_generator.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <vector>
+
 #include "base/containers/span.h"
 #include "base/optional.h"
 
@@ -113,8 +115,8 @@ class QRCodeGenerator {
   // |in| should have length block_data_bytes for the code's version.
   // |block_bytes| and |block_ec_bytes| must be provided for the current
   // version/level/group.
-  void AddErrorCorrection(uint8_t out[],
-                          const uint8_t in[],
+  void AddErrorCorrection(base::span<uint8_t> out,
+                          base::span<const uint8_t> in,
                           size_t block_bytes,
                           size_t block_ec_bytes);
 
@@ -128,7 +130,7 @@ class QRCodeGenerator {
   // is part of the structure of the QR code, i.e. finder or alignment symbols,
   // timing patterns, or format data.
   // Initialized and possibly reinitialized in Generate().
-  std::unique_ptr<uint8_t[]> d_;
+  std::vector<uint8_t> d_;
 
   // clip_dump_ is the target of paints that would otherwise fall outside of the
   // QR code.
