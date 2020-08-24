dd9437e022e561ae1c007bfcafef500707b78ac4
Alice Boxhall <aboxhall@chromium.org>
2020-08-24 02:45:40 +0000

    Revert "Enable Lacros to send a simple feedback report with all Lacros data."
    This reverts commit 4f00646f51e6db5c0f62f13d75d1f5d589984812.
    
    Reason for revert: Broke feedback_common_test on lacros:
    https://ci.chromium.org/p/chromium/builders/ci/linux-lacros-tester-rel/1305
    
    Original change's description:
    > Enable Lacros to send a simple feedback report with all Lacros data.
    > 
    > The simple lacros feedback report includes the following:
    > 
    > 1. Lacros feedback reports are sent with ChromeOS product id, so
    > that they will be found under ChromeOS product on Listnr.
    > 
    > 2. Prefix the chrome version string with "Lacros ", so that lacros
    > reports can be filtered by "Version" (for example "Lacros") on Listnr.
    > 
    > 3. Attach lacros user log in the system log.
    > 
    > Bug:1109387
    > TEST:
    > 1. Open Lacros browser, send a feedback report by clicking
    > "Report an issue" under Help menu.
    > 2. Verify the report can be found at
    > http://listnr site. The chrome version string should have
    > a "Lacros " at the beginning, for example, "Lacros 86.0.4238.0".
    > 3. Verify the report contains both system_logs.zip and histograms.zip.
    > The system log should contain lacros log data from
    > /home/chronos/user/lacros/lacros.log under "lacros_user_log" section.
    > 
    > 
    > Change-Id: I98b381724866d163dc760147448a80e2249c0a1b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2362977
    > Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    > Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#800772}
    
    TBR=jamescook@chromium.org,jennyz@chromium.org,afakhry@chromium.org,mutexlox@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1109387
    Change-Id: Ibc3fa78f5ddd42860d858e4f84d38ba87090a263
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2371422
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#800905}

--- components/feedback/feedback_common.cc
+++ components/feedback/feedback_common.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/memory/ptr_util.h"
-#include "build/lacros_buildflags.h"
 #include "components/feedback/feedback_report.h"
 #include "components/feedback/feedback_util.h"
 #include "components/feedback/proto/common.pb.h"
@@ -19,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-#if defined(OS_CHROMEOS) || BUILDFLAG(IS_LACROS)
+#if defined(OS_CHROMEOS)
 constexpr int kChromeOSProductId = 208;
 #else
 constexpr int kChromeBrowserProductId = 237;
@@ -125,7 +124,7 @@ void FeedbackCommon::PrepareReport(
 
   // Set whether we're reporting from ChromeOS or Chrome on another platform.
   userfeedback::ChromeData chrome_data;
-#if defined(OS_CHROMEOS) || BUILDFLAG(IS_LACROS)
+#if defined(OS_CHROMEOS)
   const userfeedback::ChromeData_ChromePlatform chrome_platform =
       userfeedback::ChromeData_ChromePlatform_CHROME_OS;
   const int default_product_id = kChromeOSProductId;
