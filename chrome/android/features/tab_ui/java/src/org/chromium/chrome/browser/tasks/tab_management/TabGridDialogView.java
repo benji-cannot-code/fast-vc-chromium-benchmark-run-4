58f8257b41649df4694b082cbf4376b9989f8238
Theresa <twellington@chromium.org>
2020-08-24 22:17:19 +0000

    Revert "Add an undo closure snack bar in TabGridDialog"
    This reverts commit 114425f8a8cec04716de73e3295614cdc27fbde4.
    
    Reason for revert: Failing on x86 bots
    
    Original change's description:
    > Add an undo closure snack bar in TabGridDialog
    > 
    > This CL adds a snack bar to TabGridDialog that handles closure within
    > dialog. Note that this change doesn't prevent the original snack bar
    > from showing when closure happens, so there will be two snack bars
    > showing when a closure happens within dialog. This issue will be
    > addressed by later CLs.
    > 
    > Bug: 1119899
    > Change-Id: If0e5b010996a80c9a111c8bf2cb8af44feb63124
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368236
    > Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    > Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#801037}
    
    TBR=wychen@chromium.org,yuezhanggg@chromium.org
    
    Change-Id: I1c2ccc03142c50c203ec08817d8817cfa614b519
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1119899,1121363
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2373232
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801169}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogView.java
@@ -75,7 +75,6 @@ public class TabGridDialogView extends FrameLayout
     private View mAnimationCardView;
     private View mItemView;
     private View mUngroupBar;
-    private ViewGroup mSnackBarContainer;
     private ViewGroup mParent;
     private TextView mUngroupBarTextView;
     private RelativeLayout mDialogContainerView;
@@ -159,7 +158,6 @@ public class TabGridDialogView extends FrameLayout
         mBackgroundFrame = findViewById(R.id.dialog_frame);
         mBackgroundFrame.setLayoutParams(mContainerParams);
         mAnimationCardView = findViewById(R.id.dialog_animation_card_view);
-        mSnackBarContainer = findViewById(R.id.dialog_snack_bar_container_view);
         updateDialogWithOrientation(mContext.getResources().getConfiguration().orientation);
 
         prepareAnimation();
@@ -677,7 +675,6 @@ public class TabGridDialogView extends FrameLayout
         mDialogContainerView.addView(toolbarView);
         mDialogContainerView.addView(recyclerView);
         mDialogContainerView.addView(mUngroupBar);
-        mDialogContainerView.addView(mSnackBarContainer);
         RelativeLayout.LayoutParams params =
                 (RelativeLayout.LayoutParams) recyclerView.getLayoutParams();
         params.setMargins(0, mToolbarHeight, 0, 0);
@@ -806,13 +803,6 @@ public class TabGridDialogView extends FrameLayout
      */
     void updateUngroupBarTextAppearance(int textAppearance) {
         mUngroupBarTextAppearance = textAppearance;
-    }
-
-    /**
-     * Return the container view for undo closure snack bar.
-     */
-    ViewGroup getSnackBarContainer() {
-        return mSnackBarContainer;
     }
 
     @VisibleForTesting
--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediatorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediatorUnitTest.java
@@ -58,8 +58,6 @@ import org.chromium.chrome.browser.tabmodel.TabModelFilterProvider;
 import org.chromium.chrome.browser.tabmodel.TabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorImpl;
 import org.chromium.chrome.browser.tasks.tab_groups.TabGroupModelFilter;
-import org.chromium.chrome.browser.ui.messages.snackbar.Snackbar;
-import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.tab_ui.R;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.content_public.browser.LoadUrlParams;
@@ -131,8 +129,6 @@ public class TabGridDialogMediatorUnitTest {
     Editable mEditable;
     @Mock
     ObservableSupplier<ShareDelegate> mShareDelegateSupplier;
-    @Mock
-    SnackbarManager mSnackbarManager;
     @Captor
     ArgumentCaptor<TabModelObserver> mTabModelObserverCaptor;
 
@@ -197,7 +193,7 @@ public class TabGridDialogMediatorUnitTest {
         mModel = new PropertyModel(TabGridPanelProperties.ALL_KEYS);
         mMediator = new TabGridDialogMediator(mContext, mDialogController, mModel,
                 mTabModelSelector, mTabCreatorManager, mTabSwitcherResetHandler,
-                mAnimationSourceViewProvider, mShareDelegateSupplier, mSnackbarManager, "");
+                mAnimationSourceViewProvider, mShareDelegateSupplier, "");
 
         // TabModelObserver is registered when native is ready.
         assertThat(mTabModelObserverCaptor.getAllValues().isEmpty(), equalTo(true));
@@ -606,7 +602,6 @@ public class TabGridDialogMediatorUnitTest {
 
         assertThat(mModel.get(TabGridPanelProperties.HEADER_TITLE), equalTo(DIALOG_TITLE1));
         verify(mTabSwitcherResetHandler).resetWithTabList(mTabGroupModelFilter, false, false);
-        verify(mSnackbarManager).dismissSnackbars(eq(mMediator), eq(TAB1_ID));
     }
 
     @Test
@@ -629,7 +624,6 @@ public class TabGridDialogMediatorUnitTest {
         assertThat(
                 mModel.get(TabGridPanelProperties.HEADER_TITLE), equalTo(CUSTOMIZED_DIALOG_TITLE));
         verify(mTabSwitcherResetHandler).resetWithTabList(mTabGroupModelFilter, false, false);
-        verify(mSnackbarManager).dismissSnackbars(eq(mMediator), eq(TAB2_ID));
     }
 
     @Test
@@ -646,32 +640,6 @@ public class TabGridDialogMediatorUnitTest {
         assertThat(mModel.get(TabGridPanelProperties.IS_DIALOG_VISIBLE), equalTo(false));
         verify(mTabSwitcherResetHandler, never())
                 .resetWithTabList(mTabGroupModelFilter, false, false);
-        verify(mSnackbarManager).dismissSnackbars(eq(mMediator), eq(TAB1_ID));
-    }
-
-    @Test
-    public void tabClosureCommitted() {
-        mTabModelObserverCaptor.getValue().tabClosureCommitted(mTab1);
-
-        verify(mSnackbarManager).dismissSnackbars(eq(mMediator), eq(TAB1_ID));
-    }
-
-    @Test
-    public void tabPendingClosure_DialogVisible() {
-        mModel.set(TabGridPanelProperties.IS_DIALOG_VISIBLE, true);
-
-        mTabModelObserverCaptor.getValue().tabPendingClosure(mTab1);
-
-        verify(mSnackbarManager).showSnackbar(any(Snackbar.class));
-    }
-
-    @Test
-    public void tabPendingClosure_DialogInVisible() {
-        mModel.set(TabGridPanelProperties.IS_DIALOG_VISIBLE, false);
-
-        mTabModelObserverCaptor.getValue().tabPendingClosure(mTab1);
-
-        verify(mSnackbarManager, never()).showSnackbar(any(Snackbar.class));
     }
 
     @Test
@@ -915,7 +883,7 @@ public class TabGridDialogMediatorUnitTest {
         // the animationParamsProvider is null.
         mMediator = new TabGridDialogMediator(mContext, mDialogController, mModel,
                 mTabModelSelector, mTabCreatorManager, mTabSwitcherResetHandler, null,
-                mShareDelegateSupplier, mSnackbarManager, "");
+                mShareDelegateSupplier, "");
         mMediator.initWithNative(mTabSelectionEditorController, mTabGroupTitleEditor);
 
         // Mock that the dialog is hidden and animation source view, header title and scrim click
@@ -946,7 +914,7 @@ public class TabGridDialogMediatorUnitTest {
         // the animationParamsProvider is null.
         mMediator = new TabGridDialogMediator(mContext, mDialogController, mModel,
                 mTabModelSelector, mTabCreatorManager, mTabSwitcherResetHandler, null,
-                mShareDelegateSupplier, mSnackbarManager, "");
+                mShareDelegateSupplier, "");
         mMediator.initWithNative(mTabSelectionEditorController, mTabGroupTitleEditor);
         // Mock that the dialog is hidden and animation source view, header title and scrim click
         // runnable are all null.
@@ -980,7 +948,7 @@ public class TabGridDialogMediatorUnitTest {
         // the animationParamsProvider is null.
         mMediator = new TabGridDialogMediator(mContext, mDialogController, mModel,
                 mTabModelSelector, mTabCreatorManager, mTabSwitcherResetHandler, null,
-                mShareDelegateSupplier, mSnackbarManager, "");
+                mShareDelegateSupplier, "");
         mMediator.initWithNative(mTabSelectionEditorController, mTabGroupTitleEditor);
         // Mock that the dialog is hidden and animation source view is set to some mock view for
         // testing purpose.
@@ -1042,24 +1010,6 @@ public class TabGridDialogMediatorUnitTest {
 
         doReturn(tabgroup2).when(mTabGroupModelFilter).getRelatedTabList(TAB1_ID);
         assertThat(shareString2, equalTo(mMediator.getTabGroupStringForSharingForTesting()));
-    }
-
-    @Test
-    public void testSnackbarController_onAction() {
-        doReturn(mTabModel).when(mTabModelSelector).getModelForTabId(TAB1_ID);
-
-        mMediator.onAction(TAB1_ID);
-
-        verify(mTabModel).cancelTabClosure(eq(TAB1_ID));
-    }
-
-    @Test
-    public void testSnackbarController_onDismissNoAction() {
-        doReturn(mTabModel).when(mTabModelSelector).getModelForTabId(TAB1_ID);
-
-        mMediator.onDismissNoAction(TAB1_ID);
-
-        verify(mTabModel).commitTabClosure(eq(TAB1_ID));
     }
 
     @Test
