c053d9f6732da4ddba30c5207de1b66dc17332c5
Alex Chau <alexchau@chromium.org>
2020-08-25 18:32:38 +0000

    Fix active profile handling in NearbySharingServiceImpl
    - Moved check for active profile into InvalidateSurfaceState,
      so send/receive surface can still be registered without profile being
      active
    - OnNearbyProcessStopped will InvalidateSurfaceState to potentially
      restart the flow if profile is still active
    
    Bug: 1120087
    Change-Id: I40cc43949a46808d2c090c1681d34e0450f62fd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2375289
    Commit-Queue: Alex Chau <alexchau@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801474}

--- chrome/browser/nearby_sharing/nearby_sharing_service_impl.cc
+++ chrome/browser/nearby_sharing/nearby_sharing_service_impl.cc
@@ -256,12 +256,6 @@ NearbySharingServiceImpl::NearbySharingServiceImpl(
 
   nearby_process_observer_.Add(process_manager_);
 
-  if (process_manager_->IsActiveProfile(profile_)) {
-    // TODO(crbug.com/1084576): Initialize NearbyConnectionsManager with
-    // NearbyConnectionsMojom from |process_manager|:
-    // process_manager_->GetOrStartNearbyConnections(profile_)
-  }
-
   settings_.AddSettingsObserver(settings_receiver_.BindNewPipeAndPassRemote());
 
   GetBluetoothAdapter();
@@ -285,12 +279,6 @@ NearbySharingService::StatusCodes NearbySharingServiceImpl::RegisterSendSurface(
   DCHECK(transfer_callback);
   DCHECK(discovery_callback);
   DCHECK_NE(state, SendSurfaceState::kUnknown);
-
-  if (!process_manager_->IsActiveProfile(profile_)) {
-    NS_LOG(VERBOSE) << __func__
-                    << ": RegisterSendSurface failed, since profile not active";
-    return StatusCodes::kError;
-  }
 
   if (foreground_send_transfer_callbacks_.HasObserver(transfer_callback) ||
       background_send_transfer_callbacks_.HasObserver(transfer_callback)) {
@@ -398,13 +386,6 @@ NearbySharingServiceImpl::RegisterReceiveSurface(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(transfer_callback);
   DCHECK_NE(state, ReceiveSurfaceState::kUnknown);
-
-  if (!process_manager_->IsActiveProfile(profile_)) {
-    NS_LOG(VERBOSE)
-        << __func__
-        << ": registerReceiveSurface failed, since profile not active";
-    return StatusCodes::kError;
-  }
 
   if (is_sending_files_) {
     UnregisterReceiveSurface(transfer_callback);
@@ -587,6 +568,7 @@ void NearbySharingServiceImpl::OnNearbyProfileChanged(Profile* profile) {
   // TODO(crbug.com/1084576): Notify UI about the new active profile.
   NS_LOG(VERBOSE) << __func__ << ": Nearby profile changed to "
                   << process_manager_->IsActiveProfile(profile_);
+  InvalidateSurfaceState();
 }
 
 void NearbySharingServiceImpl::OnNearbyProcessStarted() {
@@ -595,10 +577,9 @@ void NearbySharingServiceImpl::OnNearbyProcessStarted() {
 }
 
 void NearbySharingServiceImpl::OnNearbyProcessStopped() {
-  if (process_manager_->IsActiveProfile(profile_)) {
-    // TODO(crbug.com/1084576): Check if process should be running and restart
-    // it after a delay.
-  }
+  InvalidateSurfaceState();
+  if (process_manager_->IsActiveProfile(profile_))
+    NS_LOG(VERBOSE) << __func__ << ": Nearby process stopped!";
 }
 
 void NearbySharingServiceImpl::OnIncomingConnection(
@@ -972,6 +953,35 @@ void NearbySharingServiceImpl::AdapterPoweredChanged(
 void NearbySharingServiceImpl::InvalidateSurfaceState() {
   InvalidateSendSurfaceState();
   InvalidateReceiveSurfaceState();
+  if (ShouldStopNearbyProcess()) {
+    NS_LOG(VERBOSE) << __func__ << ": Stopping process because it's not in use";
+    process_manager_->StopProcess(profile_);
+  }
+}
+
+bool NearbySharingServiceImpl::ShouldStopNearbyProcess() {
+  // Cannot stop process without being the active profile.
+  if (!process_manager_->IsActiveProfile(profile_))
+    return false;
+
+  // We're currently advertising.
+  if (advertising_power_level_ != PowerLevel::kUnknown)
+    return false;
+
+  // We're currently discovering.
+  if (is_scanning_)
+    return false;
+
+  // We're currently attempting to connect to a remote device.
+  if (is_connecting_)
+    return false;
+
+  // We're currently sending or receiving a file.
+  if (is_transferring_)
+    return false;
+
+  // We're not using NearbyConnections, should stop the process.
+  return true;
 }
 
 void NearbySharingServiceImpl::InvalidateReceiveSurfaceState() {
@@ -980,6 +990,13 @@ void NearbySharingServiceImpl::InvalidateReceiveSurfaceState() {
 }
 
 void NearbySharingServiceImpl::InvalidateAdvertisingState() {
+  if (!process_manager_->IsActiveProfile(profile_)) {
+    NS_LOG(VERBOSE) << __func__
+                    << ": Stopping advertising because profile not active";
+    StopAdvertising();
+    return;
+  }
+
   // Screen is off. Do no work.
   if (ui::CheckIdleStateIsLocked()) {
     StopAdvertising();
@@ -1124,6 +1141,13 @@ void NearbySharingServiceImpl::InvalidateSendSurfaceState() {
 }
 
 void NearbySharingServiceImpl::InvalidateScanningState() {
+  if (!process_manager_->IsActiveProfile(profile_)) {
+    NS_LOG(VERBOSE) << __func__
+                    << ": Stopping discovery because profile not active";
+    StopScanning();
+    return;
+  }
+
   // Screen is off. Do no work.
   if (ui::CheckIdleStateIsLocked()) {
     StopScanning();
