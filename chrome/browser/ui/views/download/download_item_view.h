20b71e8cbab49bdcb052e0d6ee1e25e2f99d8c66
Daniel Rubery <drubery@chromium.org>
2020-08-25 23:20:51 +0000

    Add throbbing animation during deep scanning to indicate progress
    Following UX feedback, we add an animation that runs during deep
    scanning.
    
    Screencast: https://screencast.googleplex.com/cast/NjI3ODYxNjQ2OTk5NTUyMHwzMTZhNTk3YS02ZA
    Fixed: 1076092
    Change-Id: I1b12fcb88409740842bf5aeed6d5927ab94533bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367115
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801603}

--- chrome/browser/ui/views/download/download_item_view.h
+++ chrome/browser/ui/views/download/download_item_view.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/gfx/animation/slide_animation.h"
+#include "ui/gfx/animation/throb_animation.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/image/image_skia.h"
@@ -141,6 +142,10 @@ class DownloadItemView : public views::View,
   // Update accessible status text, and announce it if desired.
   void UpdateAccessibleAlert(const base::string16& alert);
 
+  // Updates the animation used during deep scanning. The animation is started
+  // or stopped depending on the current mode.
+  void UpdateAnimationForDeepScanningMode();
+
   // Get the accessible alert text for a download that is currently in progress.
   base::string16 GetInProgressAccessibleAlertText() const;
 
@@ -162,6 +167,9 @@ class DownloadItemView : public views::View,
 
   // When not in normal mode, returns the current help/warning/error icon.
   ui::ImageModel GetIcon() const;
+
+  // When not in nromal mode, returns the bounds of the current icon.
+  gfx::RectF GetIconBounds() const;
 
   // Returns the text and style to use for the status label.
   std::pair<base::string16, int> GetStatusTextAndStyle() const;
@@ -270,6 +278,8 @@ class DownloadItemView : public views::View,
   base::TimeDelta indeterminate_progress_time_elapsed_;
 
   gfx::SlideAnimation complete_animation_{this};
+
+  gfx::ThrobAnimation scanning_animation_{this};
 
   // The tooltip.  Only displayed when not showing a warning dialog.
   base::string16 tooltip_text_;
