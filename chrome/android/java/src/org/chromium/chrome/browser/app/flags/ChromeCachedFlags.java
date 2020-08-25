b1c3188af482b210540e0607963dfb3e973cf821
Gang Wu <gangwu@chromium.org>
2020-08-25 21:42:36 +0000

    Add new action bar item variations for the app menu
    This CL add two more variations of action bar items for the app menu.
    
    Bug: 1119353
    Change-Id: I428985de426acbdf5db53b71f45b2225fe4cf2fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364052
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801556}

--- chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.app.flags;
 import android.text.TextUtils;
 
 import org.chromium.base.annotations.RemovableInRelease;
+import org.chromium.chrome.browser.app.appmenu.AppMenuPropertiesDelegateImpl;
 import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.firstrun.FirstRunUtils;
 import org.chromium.chrome.browser.flags.CachedFeatureFlags;
@@ -84,6 +85,7 @@ public class ChromeCachedFlags {
 
         // clang-format off
         List<CachedFieldTrialParameter> fieldTrialsToCache = Arrays.asList(
+                AppMenuPropertiesDelegateImpl.ACTION_BAR_VARIATION,
                 BottomToolbarVariationManager.BOTTOM_TOOLBAR_VARIATION,
                 ConditionalTabStripUtils.CONDITIONAL_TAB_STRIP_INFOBAR_LIMIT,
                 ConditionalTabStripUtils.CONDITIONAL_TAB_STRIP_INFOBAR_PERIOD,
