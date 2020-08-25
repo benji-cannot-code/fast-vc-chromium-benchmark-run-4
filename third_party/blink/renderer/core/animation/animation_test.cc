3e9ebabc6ea50a1ddbde71e9fba022d4259d317e
Xianzhu Wang <wangxianzhu@chromium.org>
2020-08-25 21:36:45 +0000

    Fix missing overlay scrollbars when a scroller contains composited child
    When a scrollbar contains composited child, the overlay scrollbars may
    need to paint above the composited child, which requires the scroller to
    use composited scrolling.
    
    Bug: 1120957
    Change-Id: I1d32a68a71ae245e42d6649c93d4351455f026e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2370714
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801551}

--- third_party/blink/renderer/core/animation/animation_test.cc
+++ third_party/blink/renderer/core/animation/animation_test.cc
@@ -1950,6 +1950,8 @@ TEST_F(AnimationAnimationTestCompositing,
   SetBodyInnerHTML(R"HTML(
     <style>
       #scroller { overflow: scroll; width: 100px; height: 100px; }
+      /* to prevent the mock overlay scrollbar from affecting compositing. */
+      #scroller::-webkit-scrollbar { display: none; }
       #target { width: 100px; height: 200px; will-change: transform; }
       #spacer { width: 200px; height: 2000px; }
     </style>
