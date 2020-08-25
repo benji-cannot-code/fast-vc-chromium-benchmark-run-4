d1eeea77847c2c27e2053f34774e5babc0918754
Monica Basta <msalama@chromium.org>
2020-08-25 20:35:01 +0000

    [ProfilePicker]: Add enterprise badge for managed accounts.
    This CL shows 'Enterprise' icon for the profiles with managed primary
    account.
    
    Screenshot:
    https://drive.google.com/file/d/1bFBxKj8dwOIgmWxXjMU_wq2MBCg4pYkv/view?usp=sharing
    https://drive.google.com/file/d/1m6kwH6cLUHlJzDFDmjc0WfoXhyOqGTJ_/view?usp=sharing
    
    Bug: 1063856
    Change-Id: I705a2e13528c85b2448f9efc5e699ca1b19bbb14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2366902
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801526}

--- chrome/browser/resources/signin/profile_picker/profile_picker_main_view.js
+++ chrome/browser/resources/signin/profile_picker/profile_picker_main_view.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import './icons.js';
 import './profile_card.js';
 import './profile_picker_shared_css.js';
