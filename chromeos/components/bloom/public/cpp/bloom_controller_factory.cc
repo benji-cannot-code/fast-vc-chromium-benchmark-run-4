3db6eec6417ccab187e8df6ae25bc8a0d2861043
Jeroen Dhollander <jeroendh@google.com>
2020-08-25 15:59:13 +0000

    Add BloomController skeleton
    This is the main controller for the Bloom feature.
    
    In this CL I simply:
       * Introduce the |BloomController| class, and an (mostly) empty
        |BloomControllerImpl|.
       * Create the |BloomController| inside the |AssistantClient|, by means
        of a |BloomControllerFactory|.
    
    In later CLs this will actually start doing something.
    
    Bug: b/165356952
    Change-Id: Ib4d22016bd60ae27e940807e9bb47a2d317eaf96
    Tests: Compiled
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363399
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801390}

---
+++ chromeos/components/bloom/public/cpp/bloom_controller_factory.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/bloom/public/cpp/bloom_controller_factory.h"
+
+#include "base/callback.h"
+#include "chromeos/components/bloom/bloom_controller_impl.h"
+
+namespace chromeos {
+namespace bloom {
+
+// static
+std::unique_ptr<BloomController> BloomControllerFactory::Create(
+    scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
+    signin::IdentityManager* identity_manager,
+    ash::AssistantInteractionController* assistant_interaction_controller) {
+  return std::make_unique<BloomControllerImpl>(
+      identity_manager, assistant_interaction_controller);
+}
+
+}  // namespace bloom
+}  // namespace chromeos
