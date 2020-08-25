65cd8528e7d876aaace1f33c636cee08d670bdc5
Charlie Hu <chenleihu@google.com>
2020-08-25 16:04:37 +0000

    [Document Policy] Migrate document-write to document policy
    Change-Id: Ice3b53cbce49c5989d5a45761fb15922bfe7ba21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359567
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Cr-Commit-Position: refs/heads/master@{#801395}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -7136,7 +7136,7 @@ bool Document::AllowedToUseDynamicMarkUpInsertion(
     return true;
   }
   if (!GetFrame() || GetExecutionContext()->IsFeatureEnabled(
-                         mojom::blink::FeaturePolicyFeature::kDocumentWrite,
+                         mojom::blink::DocumentPolicyFeature::kDocumentWrite,
                          ReportOptions::kReportOnFailure)) {
     return true;
   }
