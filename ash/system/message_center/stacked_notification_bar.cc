aff18fb1e5ed6b0ac365db10107aceaa39d50365
Andre Le <leandre@chromium.org>
2020-08-25 16:45:42 +0000

    Ash Notification: Add SetPaintToLayer to stacked notification bar.
    The regression bug below was caused by ff070b8 where we changed
    SetPaintToLayer in TrayBubbleView. Added this to stacked notification
    bar to fix the issue.
    
    BUG=1116396
    
    Change-Id: Ied59608d5cce78396a8352faf9d583ca877de74a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2372103
    Reviewed-by: Tim Song <tengs@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801419}

--- ash/system/message_center/stacked_notification_bar.cc
+++ ash/system/message_center/stacked_notification_bar.cc
@@ -285,6 +285,8 @@ StackedNotificationBar::StackedNotificationBar(
 
   expand_all_button_->SetVisible(false);
   AddChildView(expand_all_button_);
+
+  SetPaintToLayer();
 }
 
 StackedNotificationBar::~StackedNotificationBar() {
