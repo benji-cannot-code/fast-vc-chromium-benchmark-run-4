99446add04563ab24181891b157225f5fe69212c
Nohemi Fernandez <fernandex@chromium.org>
2020-08-25 21:16:37 +0000

    [iOS] Disable "Yes, I'm in" button during sign-in.
    Currently the button is hidden during sign-in, which causes the
    left-aligned buttons to become center-aligned. Instead of hiding the
    button, disable it to ensure there is no user interaction and buttons
    remain aligned.
    
    Bug: 1113687
    Change-Id: I71f3d766c27f83d1d9a513f042338ecf81fe6a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2375345
    Auto-Submit: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801548}

--- ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_view_controller.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_view_controller.mm
@@ -159,12 +159,12 @@ enum AuthenticationButtonType {
 }
 
 - (void)signinWillStart {
-  self.confirmationButton.hidden = YES;
+  self.confirmationButton.enabled = NO;
   [self startAnimatingActivityIndicator];
 }
 
 - (void)signinDidStop {
-  self.confirmationButton.hidden = NO;
+  self.confirmationButton.enabled = YES;
   [self stopAnimatingActivityIndicator];
 }
 
@@ -428,7 +428,7 @@ enum AuthenticationButtonType {
   [self.view addSubview:self.activityIndicator];
 
   self.activityIndicator.translatesAutoresizingMaskIntoConstraints = NO;
-  AddSameCenterConstraints(self.confirmationButton, self.activityIndicator);
+  AddSameCenterConstraints(self.containerView, self.activityIndicator);
 }
 
 // Sets the text, styling, and other button properties for the skip sign-in
