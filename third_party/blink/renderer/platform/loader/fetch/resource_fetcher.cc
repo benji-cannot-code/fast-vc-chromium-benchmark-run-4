9ae7814f429b8a8c7a436950d1b64dd0d92fbd29
Tsuyoshi Horo <horo@chromium.org>
2020-08-25 05:29:33 +0000

    Don't use DefaultCacheIdentifier while loading resources from WebBundle
    This CL changes ResourceFetcher::GetCacheIdentifier() to use the
    bundle's URL as the cache identifier, when the resource is listed in the
    resource= attribute of <link rel="webbundle">,
    
    
    Bug: 1082020
    Change-Id: I2035f9ef37bf40c8a636baecf38031ed1a585813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2371424
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801276}

--- third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
@@ -707,7 +707,7 @@ Resource* ResourceFetcher::ResourceForStaticData(
   if (!archive_ && factory.GetType() == ResourceType::kRaw)
     return nullptr;
 
-  const String cache_identifier = GetCacheIdentifier();
+  const String cache_identifier = GetCacheIdentifier(url);
   // Most off-main-thread resource fetches use Resource::kRaw and don't reach
   // this point, but off-main-thread module fetches might.
   if (IsMainThread()) {
@@ -1094,8 +1094,8 @@ Resource* ResourceFetcher::RequestResource(FetchParameters& params,
       // found, we may need to make it block the onload event.
       MakePreloadedResourceBlockOnloadIfNeeded(resource, params);
     } else if (IsMainThread()) {
-      resource =
-          GetMemoryCache()->ResourceForURL(params.Url(), GetCacheIdentifier());
+      resource = GetMemoryCache()->ResourceForURL(
+          params.Url(), GetCacheIdentifier(params.Url()));
       if (resource) {
         policy = DetermineRevalidationPolicy(resource_type, params, *resource,
                                              is_static_data);
@@ -1289,7 +1289,8 @@ void ResourceFetcher::AddToMemoryCacheIfNeeded(const FetchParameters& params,
 Resource* ResourceFetcher::CreateResourceForLoading(
     const FetchParameters& params,
     const ResourceFactory& factory) {
-  const String cache_identifier = GetCacheIdentifier();
+  const String cache_identifier =
+      GetCacheIdentifier(params.GetResourceRequest().Url());
   DCHECK(!IsMainThread() || params.IsStaleRevalidation() ||
          !GetMemoryCache()->ResourceForURL(params.GetResourceRequest().Url(),
                                            cache_identifier));
@@ -2120,13 +2121,19 @@ void ResourceFetcher::UpdateAllImageResourcePriorities() {
   to_be_removed.clear();
 }
 
-String ResourceFetcher::GetCacheIdentifier() const {
+String ResourceFetcher::GetCacheIdentifier(const KURL& url) const {
   if (properties_->GetControllerServiceWorkerMode() !=
       mojom::ControllerServiceWorkerMode::kNoController) {
     return String::Number(properties_->ServiceWorkerId());
   }
   if (properties_->WebBundlePhysicalUrl().IsValid())
     return properties_->WebBundlePhysicalUrl().GetString();
+
+  for (auto& bundle : subresource_web_bundles_) {
+    if (bundle->CanHandleRequest(url))
+      return bundle->GetCacheIdentifier();
+  }
+
   return MemoryCache::DefaultCacheIdentifier();
 }
 
