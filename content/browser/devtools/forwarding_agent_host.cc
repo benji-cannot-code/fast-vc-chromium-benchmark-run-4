82e4b067ecf9bf6ddb8853e9527f10969b3d742f
Michele Mancina <micantox@google.com>
2020-08-25 08:01:03 +0000

    Add parameter to control whether to aquire the wake lock or not.
    Bug: b/161674018
    Change-Id: If93e5eaab7391fad3af8b29d4030a6cc43d47737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360070
    Commit-Queue: Michele Mancina <micantox@google.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801297}

--- content/browser/devtools/forwarding_agent_host.cc
+++ content/browser/devtools/forwarding_agent_host.cc
@@ -20,7 +20,8 @@ ForwardingAgentHost::ForwardingAgentHost(
 
 ForwardingAgentHost::~ForwardingAgentHost() = default;
 
-bool ForwardingAgentHost::AttachSession(DevToolsSession* session) {
+bool ForwardingAgentHost::AttachSession(DevToolsSession* session,
+                                        bool acquire_wake_lock) {
   session->TurnIntoExternalProxy(delegate_.get());
   return true;
 }
--- content/browser/devtools/render_frame_devtools_agent_host.cc
+++ content/browser/devtools/render_frame_devtools_agent_host.cc
@@ -286,7 +286,8 @@ WebContents* RenderFrameDevToolsAgentHost::GetWebContents() {
   return web_contents();
 }
 
-bool RenderFrameDevToolsAgentHost::AttachSession(DevToolsSession* session) {
+bool RenderFrameDevToolsAgentHost::AttachSession(DevToolsSession* session,
+                                                 bool acquire_wake_lock) {
   if (!ShouldAllowSession(session))
     return false;
 
@@ -359,7 +360,8 @@ bool RenderFrameDevToolsAgentHost::AttachSession(DevToolsSession* session) {
 #endif
     UpdateRawHeadersAccess(nullptr, frame_host_);
 #if defined(OS_ANDROID)
-    GetWakeLock()->RequestWakeLock();
+    if (acquire_wake_lock)
+      GetWakeLock()->RequestWakeLock();
 #endif
   }
   return true;
