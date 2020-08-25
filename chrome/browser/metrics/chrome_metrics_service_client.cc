9a094ea28f0eb3a5ac77ab21a87d05c9c5b46553
Scott Violet <sky@chromium.org>
2020-08-25 16:57:13 +0000

    metrics/weblayer: move ChromeStabilityMetricsProvider
    to components/metrics/content and renames
    ContentStabilityMetricsProvider. I'm doing this to share
    with WebLayer.
    
    Having components depend upon extensions seems bad (and has
    compile errors). So, I had to refactor that dependency out.
    
    I will sort out unit-test next.
    
    BUG=1120537
    TEST=pure rename.
    
    Change-Id: I4fd9907fa5b52c3a06222b96bb7550f02a12ab95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2372779
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801425}

--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -43,8 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/google/google_brand.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/metrics/cached_metrics_profile.h"
+#include "chrome/browser/metrics/chrome_metrics_extensions_helper.h"
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
-#include "chrome/browser/metrics/chrome_stability_metrics_provider.h"
 #include "chrome/browser/metrics/desktop_platform_features_metrics_provider.h"
 #include "chrome/browser/metrics/desktop_session_duration/desktop_profile_session_durations_service_factory.h"
 #include "chrome/browser/metrics/https_engagement_metrics_provider.h"
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/history_service.h"
 #include "components/metrics/call_stack_profile_metrics_provider.h"
 #include "components/metrics/component_metrics_provider.h"
+#include "components/metrics/content/content_stability_metrics_provider.h"
 #include "components/metrics/content/gpu_metrics_provider.h"
 #include "components/metrics/content/rendering_perf_metrics_provider.h"
 #include "components/metrics/content/subprocess_metrics_provider.h"
@@ -628,7 +629,8 @@ void ChromeMetricsServiceClient::RegisterMetricsServiceProviders() {
       std::make_unique<OmniboxMetricsProvider>());
 
   metrics_service_->RegisterMetricsProvider(
-      std::make_unique<ChromeStabilityMetricsProvider>(local_state));
+      std::make_unique<metrics::ContentStabilityMetricsProvider>(
+          local_state, std::make_unique<ChromeMetricsExtensionsHelper>()));
 
   metrics_service_->RegisterMetricsProvider(
       std::make_unique<metrics::GPUMetricsProvider>());
