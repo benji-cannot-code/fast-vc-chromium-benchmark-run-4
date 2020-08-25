74ac0cb233e6f06e50d224be728bb9ad21a378f8
David Bokan <bokan@chromium.org>
2020-08-25 21:37:56 +0000

    [Refactor] Add interface for input->LTHI
    This CL adds an interface for the InputHandler implementation to
    communicate with the compositor. This CL is a start that mostly just
    imposes an interface on the existing structure  of these objects. Future
    CLs will continue to refine this into an interface that makes sense for
    the compositor to expose. In particular, there are some cases that
    definitely shouldn't be part of the compositor interface that require an
    "escape hatch" where ThreadedInputHandler currently uses a reference to
    the LayerTreeHostImpl:
    
    - cc::Viewport
    - The compositor Animation system
    - FrameTrackers and metrics
    - BrowserControls
    - Scrollbar Animations
    - Interaction with the PropertyTrees
    
    These will need additional thought and more meaningful changes so will
    be done separately.
    
    Bug: 915926
    Change-Id: I4bc79478f387614712c0f74bcb3c1355a917f591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359390
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801553}

--- cc/input/threaded_input_handler.h
+++ cc/input/threaded_input_handler.h
@@ -32,8 +32,6 @@ class ScrollOffset;
 namespace cc {
 
 class LayerImpl;
-class LayerTreeHostImpl;
-class LayerTreeSettings;
 class ScrollbarController;
 class ScrollElasticityHelper;
 struct ScrollNode;
@@ -43,7 +41,8 @@ class Viewport;
 
 class CC_EXPORT ThreadedInputHandler : public InputDelegateForCompositor {
  public:
-  explicit ThreadedInputHandler(LayerTreeHostImpl* host_impl);
+  explicit ThreadedInputHandler(
+      CompositorDelegateForInput& compositor_delegate);
   ~ThreadedInputHandler();
 
   // =========== InputHandler "Interface" - will override in a future CL
@@ -182,16 +181,12 @@ class CC_EXPORT ThreadedInputHandler : public InputDelegateForCompositor {
   LayerTreeImpl& ActiveTree();
   LayerTreeImpl& ActiveTree() const;
 
-  const LayerTreeSettings& Settings() const;
-
   bool IsMainThreadScrolling(const InputHandler::ScrollStatus& status,
                              const ScrollNode* scroll_node) const;
 
   bool IsTouchDraggingScrollbar(
       LayerImpl* first_scrolling_layer_or_drawn_scrollbar,
       ui::ScrollInputType type);
-
-  void ShowScrollbarsForImplScroll(ElementId element_id);
 
   void UpdateRootLayerStateForSynchronousInputHandler();
 
@@ -335,7 +330,9 @@ class CC_EXPORT ThreadedInputHandler : public InputDelegateForCompositor {
     return scrollbar_controller_.get();
   }
 
-  LayerTreeHostImpl& host_impl_;
+  // The input handler is owned by the delegate so their lifetimes are tied
+  // together.
+  CompositorDelegateForInput& compositor_delegate_;
 
   InputHandlerClient* input_handler_client_ = nullptr;
 
