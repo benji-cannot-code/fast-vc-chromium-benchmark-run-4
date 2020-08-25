20608da8fb0d3659cbd15021f84948cafcc0d2be
Kent Tamura <tkent@chromium.org>
2020-08-25 01:37:37 +0000

    Revert "input[type=file]: display:inline-flex/flex/inline-grid/grid should not affect the internal layout"
    This reverts commit d04cef4c208f677b5d8b56553d66945c84a1f7c5.
    
    Reason for revert: Specification violation. Changing ComputedStyle::
    Display() affects web-exposed getComputedStyle().
    
    Original change's description:
    > input[type=file]: display:inline-flex/flex/inline-grid/grid should not affect the internal layout
    > 
    > In LayoutNG, we use LayoutNGBlockFlow for input[type=file]. If
    > flex/grid is specified, NGBlockLayoutAlgorithm is applied but children
    > are blockified. So the internal [Choose File] button and
    > "No file chosen" text were placed on different lines.
    > 
    > This CL fixes it by adding "display" adjustment code.
    > 
    > Bug: 1119312
    > Change-Id: Id15bfa5a047f56ee00adb43d74e6b3d09f1f6433
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2371066
    > Reviewed-by: Koji Ishii <kojii@chromium.org>
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#801209}
    
    TBR=yosin@chromium.org,tkent@chromium.org,kojii@chromium.org
    
    Change-Id: I393b32565e44ea8dbfce2eb6c347d5b423470764
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1119312
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2371847
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801238}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_table_cell_element.h"
 #include "third_party/blink/renderer/core/html/media/html_media_element.h"
 #include "third_party/blink/renderer/core/html_names.h"
-#include "third_party/blink/renderer/core/input_type_names.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/layout_replaced.h"
 #include "third_party/blink/renderer/core/layout/layout_theme.h"
@@ -349,17 +348,6 @@ static void AdjustStyleForHTMLElement(ComputedStyle& style,
     style.SetOverflowX(EOverflow::kHidden);
     style.SetOverflowY(EOverflow::kHidden);
     return;
-  }
-
-  if (const auto* input = DynamicTo<HTMLInputElement>(element)) {
-    if (input->type() == input_type_names::kFile) {
-      if (style.Display() == EDisplay::kFlex ||
-          style.Display() == EDisplay::kGrid)
-        style.SetDisplay(EDisplay::kBlock);
-      else if (style.Display() == EDisplay::kInlineFlex ||
-               style.Display() == EDisplay::kInlineGrid)
-        style.SetDisplay(EDisplay::kInlineBlock);
-    }
   }
 
   if (IsA<HTMLTextAreaElement>(element)) {
