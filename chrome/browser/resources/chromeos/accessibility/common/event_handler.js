3080ce7c78a9b863fba1e9fda9142c841d795b21
Anastasia Helfinstein <anastasi@google.com>
2020-08-25 23:43:21 +0000

    [Switch Access] Use EventHandler to look for nodes
    Code clean-up. Does not change the behavior of Switch Access.
    
    AX-Relnotes: n/a.
    Bug: None.
    Change-Id: I49b5eefd69c9165f9729927f85779b8bdacfc224
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2373290
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#801612}

--- chrome/browser/resources/chromeos/accessibility/common/event_handler.js
+++ chrome/browser/resources/chromeos/accessibility/common/event_handler.js
@@ -13,7 +13,7 @@ class EventHandler {
    *         !Array<!chrome.automation.AutomationNode>} nodes
    * @param {!chrome.automation.EventType |
    *     !Array<!chrome.automation.EventType>} types
-   * @param {!function(!chrome.automation.AutomationEvent)} callback
+   * @param {?function(!chrome.automation.AutomationEvent)} callback
    * @param {{capture: (boolean|undefined), exactMatch: (boolean|undefined),
    *     listenOnce: (boolean|undefined), predicate:
    *     ((function(chrome.automation.AutomationEvent): boolean)|undefined)}}
@@ -33,7 +33,7 @@ class EventHandler {
     /** @private {!Array<!chrome.automation.EventType>} */
     this.types_ = types instanceof Array ? types : [types];
 
-    /** @private {!function(!chrome.automation.AutomationEvent)} */
+    /** @private {?function(!chrome.automation.AutomationEvent)} */
     this.callback_ = callback;
 
     /** @private {boolean} */
@@ -80,6 +80,11 @@ class EventHandler {
       }
     }
     this.listening_ = false;
+  }
+
+  /** @param {?function(!chrome.automation.AutomationEvent)} callback */
+  setCallback(callback) {
+    this.callback_ = callback;
   }
 
   /**
@@ -139,6 +144,8 @@ class EventHandler {
       this.stop();
     }
 
-    this.callback_(event);
+    if (this.callback_) {
+      this.callback_(event);
+    }
   }
 }
