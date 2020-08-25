281cc30d582dfa30a788a55f4bbebd8bbbdeb1d1
Sean McAllister <smcallis@google.com>
2020-08-25 16:28:05 +0000

    Refactor OS_LINUX preprocessor directive for LaCrOS effort.
    Currently, ChromeOS defines the OS_LINUX directive as well as
    OS_CHROMEOS.  We're working to separate these two, so we're
    making the fact that OS_LINUX == OS_LINUX || OS_CHROMEOS
    explicit.
    
    This is changes for /base/allocator/partition_allocator
    
    This CL was uploaded by git cl split.
    
    R=lizeb@chromium.org
    
    Bug: 1110266
    Change-Id: Ibf15087393996835bdcedabeacec04114d96c6be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2371209
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Sean McAllister <smcallis@google.com>
    Cr-Commit-Position: refs/heads/master@{#801411}

--- base/allocator/partition_allocator/page_allocator_internals_posix.h
+++ base/allocator/partition_allocator/page_allocator_internals_posix.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_ANDROID)
 #include <sys/prctl.h>
 #endif
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
 #include <sys/resource.h>
 
 #include <algorithm>
