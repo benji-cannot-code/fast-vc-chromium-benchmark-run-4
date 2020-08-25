0241a38a27b56c713e6ff95686e65f3894484227
Sean McAllister <smcallis@google.com>
2020-08-25 05:14:02 +0000

    Refactor OS_LINUX preprocessor directive for LaCrOS effort.
    Currently, ChromeOS defines the OS_LINUX directive as well as
    OS_CHROMEOS.  We're working to separate these two, so we're
    making the fact that OS_LINUX == OS_LINUX || OS_CHROMEOS
    explicit.
    
    This is changes for /chrome/browser/notifications
    
    This CL was uploaded by git cl split.
    
    R=yoshiki@chromium.org
    
    Bug: 1110266
    Change-Id: Ia3805830c319754c760ddf58fddba687f33d68c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2371256
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Sean McAllister <smcallis@google.com>
    Cr-Commit-Position: refs/heads/master@{#801273}

--- chrome/browser/notifications/notification_display_service_impl.cc
+++ chrome/browser/notifications/notification_display_service_impl.cc
@@ -38,7 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/notification_platform_bridge_message_center.h"
 #endif
 
-#if defined(OS_LINUX) || defined(OS_MAC) || defined(OS_WIN)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_MAC) || \
+    defined(OS_WIN)
 #include "chrome/browser/nearby_sharing/nearby_notification_handler.h"
 #include "chrome/browser/send_tab_to_self/desktop_notification_handler.h"
 #include "chrome/browser/sharing/sharing_notification_handler.h"
@@ -53,7 +54,7 @@ namespace {
 
 #if !defined(OS_CHROMEOS)
 bool NativeNotificationsEnabled(Profile* profile) {
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
   if (profile) {
     PrefService* prefs = profile->GetPrefs();
     if (!prefs->GetBoolean(prefs::kAllowNativeNotifications))
@@ -152,7 +153,8 @@ NotificationDisplayServiceImpl::NotificationDisplayServiceImpl(Profile* profile)
     AddNotificationHandler(NotificationHandler::Type::WEB_PERSISTENT,
                            std::make_unique<PersistentNotificationHandler>());
 
-#if defined(OS_LINUX) || defined(OS_MAC) || defined(OS_WIN)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_MAC) || \
+    defined(OS_WIN)
     AddNotificationHandler(
         NotificationHandler::Type::SEND_TAB_TO_SELF,
         std::make_unique<send_tab_to_self::DesktopNotificationHandler>(
