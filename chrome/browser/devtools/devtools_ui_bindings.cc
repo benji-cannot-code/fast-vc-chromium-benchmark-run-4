22cb2426016b2fc7e5005442673e034d2b98b90f
Alex Rudenko <alexrudenko@chromium.org>
2020-08-26 09:47:30 +0000

    [DevTools]: new histogram for grid settings
    The set of CSS Grid settings has changed so we add a new histogram
    to track the new set of settings. The previously used histogram
    becomes obsolete.
    
    Frontend CL: https://crrev.com/c/2366793
    
    Bug: 1109177
    Change-Id: I391595622ef99c7e2e6be554f2141acc4e44d752
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367833
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Brandon Goddard <brgoddar@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#801703}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -114,7 +114,7 @@ static const char kDevToolsDualScreenDeviceEmulatedHistogram[] =
 static const char kDevtoolsGridSettingChangedHistogram[] =
     "DevTools.GridSettingChanged";
 static const char kDevtoolsCSSGridSettingsHistogram[] =
-    "DevTools.CSSGridSettings";
+    "DevTools.CSSGridSettings2";
 static const char kDevtoolsExperimentEnabledHistogram[] =
     "DevTools.ExperimentEnabled";
 static const char kDevtoolsExperimentDisabledHistogram[] =
