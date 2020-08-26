0f14b8e1b55f8cb46ec3877cf91b9beaeaabbff4
Maksim Sisov <msisov@igalia.com>
2020-08-26 18:43:44 +0000

    Fix ASAN bot - linking of remoting
    This fixes the following error:
    
    ld.lld: error: duplicate symbol: remoting::DesktopResizer::Create()
    >>> defined at desktop_resizer_ozone.cc:53 (../../remoting/host/desktop_resizer_ozone.cc:53)
    >>>            common/desktop_resizer_ozone.o:(remoting::DesktopResizer::Create()) in archive obj/remoting/host/libcommon.a
    >>> defined at desktop_resizer_x11.cc:362 (../../remoting/host/desktop_resizer_x11.cc:362)
    >>>            common/desktop_resizer_x11.o:(.text._ZN8remoting14DesktopResizer6CreateEv+0x0) in archive obj/remoting/host/libcommon.a
    clang: error: linker command failed with exit code 1 (use -v to see invocation)
    
    This patch uses the same approach as in another places. For example,
    DesktopScreenLinux.
    
    Bug: 1085700
    Change-Id: I0bb0c8e6d62ea66c618422d4af9044c52c57c729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2376854
    Commit-Queue: Maksim Sisov (GMT+3) <msisov@igalia.com>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Auto-Submit: Maksim Sisov (GMT+3) <msisov@igalia.com>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801862}

---
+++ remoting/host/desktop_resizer_linux.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/desktop_resizer.h"
+
+#include <memory>
+
+#include "base/notreached.h"
+
+#if defined(USE_X11)
+#include "remoting/host/desktop_resizer_x11.h"
+#endif
+
+#if defined(USE_OZONE)
+#include "remoting/host/desktop_resizer_ozone.h"
+#include "ui/base/ui_base_features.h"
+#endif
+
+namespace remoting {
+
+std::unique_ptr<DesktopResizer> DesktopResizer::Create() {
+#if defined(USE_OZONE)
+  if (features::IsUsingOzonePlatform())
+    return std::make_unique<DesktopResizerOzone>();
+#endif
+#if defined(USE_X11)
+  return std::make_unique<DesktopResizerX11>();
+#else
+  NOTREACHED();
+  return nullptr;
+#endif
+}
+
+}  // namespace remoting
