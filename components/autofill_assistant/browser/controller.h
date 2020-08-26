358c35009882e0bd59138a74ab95b67cd17b929c
Clemens Arbesser <arbesser@google.com>
2020-08-26 15:56:58 +0000

    [Autofill Assistant] Avoid showing error message in lite scripts.
    Bug: b/166429363
    Change-Id: Ib20ad349a2f1e5efe5e1d6171ddee9563c7cb125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2377491
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Auto-Submit: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#801807}

--- components/autofill_assistant/browser/controller.h
+++ components/autofill_assistant/browser/controller.h
@@ -342,6 +342,7 @@ class Controller : public ScriptExecutorDelegate,
   void PerformDelayedShutdownIfNecessary();
 
   bool StateNeedsUI(AutofillAssistantState state);
+  bool IsRunningLiteScript() const;
 
   ClientSettings settings_;
   Client* const client_;
