2f24eff18b8b2a9c951e1c8a7d8044c4f0137b25
dizg <dizg@google.com>
2020-08-26 14:00:05 +0000

    [signin] Browser test for supporting OAuth outage in Dice.
    Bug: 1115247
    Change-Id: I0b4c90250ea532b6d8d427437073a785b54b8515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2373082
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Diana Zagidullina <dizg@google.com>
    Cr-Commit-Position: refs/heads/master@{#801770}

--- chrome/browser/signin/dice_response_handler.h
+++ chrome/browser/signin/dice_response_handler.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/cancelable_callback.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/sequenced_task_runner.h"
+#include "base/timer/timer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/signin/core/browser/account_reconcilor.h"
 #include "components/signin/core/browser/signin_header_helper.h"
@@ -79,6 +82,9 @@ class DiceResponseHandler : public KeyedService {
 
   // Returns the number of pending DiceTokenFetchers. Exposed for testing.
   size_t GetPendingDiceTokenFetchersCountForTesting() const;
+
+  // Sets |task_runner_| for testing.
+  void SetTaskRunner(scoped_refptr<base::SequencedTaskRunner> task_runner);
 
  private:
   // Helper class to fetch a refresh token from an authorization code.
@@ -169,8 +175,8 @@ class DiceResponseHandler : public KeyedService {
   // Lock the account reconcilor for kLockAccountReconcilorTimeoutHours
   // when there was OAuth outage in Dice.
   std::unique_ptr<AccountReconcilor::Lock> lock_;
-  base::OneShotTimer timer_;
-
+  std::unique_ptr<base::OneShotTimer> timer_;
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
   DISALLOW_COPY_AND_ASSIGN(DiceResponseHandler);
 };
 
