0f195862d53faec22cc6a92f9162f11f556841c0
Christopher Cameron <ccameron@chromium.org>
2020-08-26 22:20:29 +0000

    Remove createImageData that takes an ImageDataArray
    The following function exists but is not documented in any spec:
    
        [RuntimeEnabled=CanvasColorManagement, RaisesException]
        ImageData createImageData(
            ImageDataArray data,
            unsigned long sw, unsigned long sh,
            ImageDataColorSettings imageDataColorSettings);
    
    Remove it.
    
    Bug: 1119507
    Change-Id: Ic3bd46c30e9ff2937a4edf817d8002ef97b57c02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2364758
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801984}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
@@ -189,15 +189,6 @@ class MODULES_EXPORT BaseRenderingContext2D : public GarbageCollectedMixin,
                              unsigned,
                              ImageDataColorSettings*,
                              ExceptionState&) const;
-  ImageData* createImageData(ImageDataArray&,
-                             unsigned,
-                             unsigned,
-                             ExceptionState&) const;
-  ImageData* createImageData(ImageDataArray&,
-                             unsigned,
-                             unsigned,
-                             ImageDataColorSettings*,
-                             ExceptionState&) const;
 
   // For deferred canvases this will have the side effect of drawing recorded
   // commands in order to finalize the frame
