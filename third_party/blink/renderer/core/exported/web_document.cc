84da6f489ad144e9cf02da04dec8d6cb8e9846b3
Nate Chapin <japhet@chromium.org>
2020-08-26 00:16:34 +0000

    Make WebDocument's use of Document::GetExecutionContext() safe
    There are 4 places Document::GetExecutionContext() is currently
    invoked in WebDocument:
    * GetSecurityOrigin() is the only one of the 4 that null-checks it.
    * OutgoingReferrer() is unused and can be removed.
    * GetReferrerPolicy() has a single use that can easily be adjusted so
      that it can be removed.
    * IsSecureContext() needs a null-check.
    
    Bug: 1121726
    Change-Id: I0f65a56909e4bc1a5ee6aee30e0e59c8c698b434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2375704
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801618}

--- third_party/blink/renderer/core/exported/web_document.cc
+++ third_party/blink/renderer/core/exported/web_document.cc
@@ -94,7 +94,9 @@ WebSecurityOrigin WebDocument::GetSecurityOrigin() const {
 
 bool WebDocument::IsSecureContext() const {
   const Document* document = ConstUnwrap<Document>();
-  return document && document->GetExecutionContext()->IsSecureContext();
+  ExecutionContext* context =
+      document ? document->GetExecutionContext() : nullptr;
+  return context && context->IsSecureContext();
 }
 
 WebString WebDocument::Encoding() const {
@@ -234,15 +236,6 @@ void WebDocument::WatchCSSSelectors(const WebVector<WebString>& web_selectors) {
   Vector<String> selectors;
   selectors.Append(web_selectors.Data(), web_selectors.size());
   CSSSelectorWatch::From(*document).WatchCSSSelectors(selectors);
-}
-
-network::mojom::ReferrerPolicy WebDocument::GetReferrerPolicy() const {
-  return ConstUnwrap<Document>()->GetExecutionContext()->GetReferrerPolicy();
-}
-
-WebString WebDocument::OutgoingReferrer() {
-  return WebString(
-      Unwrap<Document>()->GetExecutionContext()->OutgoingReferrer());
 }
 
 WebVector<WebDraggableRegion> WebDocument::DraggableRegions() const {
