099649e08760ec94e3ba1ea212b7228d7ef82f15
Danyao Wang <danyao@chromium.org>
2020-08-26 12:08:01 +0000

    [Web Payments] Add canMakePayment tests for Secure Payment Confirmation.
    Both canMakePayment() and hasEnrolledInstrument() are expected to return
    true if a device supports Secure Payment Confirmation: i.e. has a
    compatible authenticator, regardless of the presence of payment
    credentials. These new tests verify these behaviors.
    
    Also fixed a crash that happens when PaymentRequest is destroyed after
    the associated RenderFrameHost. This is new to Secure Payment
    Confirmation because PaymentRequest now holds an InternalAuthenticator,
    which must be out-lived by the RenderFrameHost. This patch fixes the
    bug by observing WebContentsObserver::RenderFrameDeleted() and perform
    the necessary clean up before the RenderFrameHost is actually destroyed.
    
    This crash was detected by the new tests, which triggers PaymentRequest
    destructor during browser shutdown.
    
    Bug: 1121168
    Change-Id: I1e7ab94c3c6fac23ef5e3104beccc1ab1da2f3e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2375887
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801738}

--- chrome/browser/payments/secure_payment_confirmation_browsertest.cc
+++ chrome/browser/payments/secure_payment_confirmation_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/command_line.h"
+#include "base/strings/stringprintf.h"
 #include "build/build_config.h"
 #include "chrome/test/payments/payment_request_platform_browsertest_base.h"
 #include "content/public/common/content_switches.h"
@@ -14,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace payments {
 namespace {
 
-static constexpr char kInvokePaymentRequest[] =
-    "getStatusForMethodData([{"
+static constexpr char kTestMethodData[] =
+    "[{ "
     "  supportedMethods: 'secure-payment-confirmation',"
     "  data: {"
     "    action: 'authenticate',"
@@ -23,7 +24,11 @@ static constexpr char kInvokePaymentRequest[] =
     "    networkData: Uint8Array.from('x', c => c.charCodeAt(0)),"
     "    timeout: 60000,"
     "    fallbackUrl: 'https://fallback.example/url'"
-    "}}])";
+    "}}]";
+
+std::string getInvokePaymentRequestSnippet() {
+  return base::StringPrintf("getStatusForMethodData(%s)", kTestMethodData);
+}
 
 class SecurePaymentConfirmationTest
     : public PaymentRequestPlatformBrowserTestBase {
@@ -42,7 +47,8 @@ IN_PROC_BROWSER_TEST_F(SecurePaymentConfirmationTest, NoAuthenticator) {
   // EvalJs waits for JavaScript promise to resolve.
   EXPECT_EQ(
       "The payment method \"secure-payment-confirmation\" is not supported.",
-      content::EvalJs(GetActiveWebContents(), kInvokePaymentRequest));
+      content::EvalJs(GetActiveWebContents(),
+                      getInvokePaymentRequestSnippet()));
 }
 
 #if defined(OS_ANDROID)
@@ -60,12 +66,60 @@ IN_PROC_BROWSER_TEST_F(SecurePaymentConfirmationTest,
 
   // ExecJs starts executing JavaScript and immediately returns, not waiting for
   // any promise to return.
-  EXPECT_TRUE(content::ExecJs(GetActiveWebContents(), kInvokePaymentRequest));
+  EXPECT_TRUE(content::ExecJs(GetActiveWebContents(),
+                              getInvokePaymentRequestSnippet()));
 
   WaitForObservedEvent();
   ASSERT_FALSE(test_controller()->app_descriptions().empty());
   EXPECT_EQ(1u, test_controller()->app_descriptions().size());
   EXPECT_EQ("Stub label", test_controller()->app_descriptions().front().label);
+}
+
+// canMakePayment() and hasEnrolledInstrument() should return false on platforms
+// without a compatible authenticator.
+IN_PROC_BROWSER_TEST_F(SecurePaymentConfirmationTest,
+                       CanMakePayment_NoAuthenticator) {
+  test_controller()->SetHasAuthenticator(false);
+  NavigateTo("a.com", "/can_make_payment_checker.html");
+
+  {
+    std::string snippet =
+        base::StringPrintf("canMakePaymentForMethodData(%s)", kTestMethodData);
+    EXPECT_EQ("false", content::EvalJs(GetActiveWebContents(), snippet));
+  }
+  {
+    std::string snippet = base::StringPrintf(
+        "hasEnrolledInstrumentForMethodData(%s)", kTestMethodData);
+    EXPECT_EQ("false", content::EvalJs(GetActiveWebContents(), snippet));
+  }
+}
+
+// canMakePayment() and hasEnrolledInstrument() should return true on platforms
+// with a compatible authenticator regardless of the presence of payment
+// credentials.
+#if defined(OS_ANDROID)
+// TODO(https://crbug.com/1110320): Implement SetHasAuthenticator() for Android,
+// so this behavior can be tested on Android as well.
+#define MAYBE_CanMakePayment_HasAuthenticator \
+  DISABLED_CanMakePayment_HasAuthenticator
+#else
+#define MAYBE_CanMakePayment_HasAuthenticator CanMakePayment_HasAuthenticator
+#endif  // OS_ANDROID
+IN_PROC_BROWSER_TEST_F(SecurePaymentConfirmationTest,
+                       MAYBE_CanMakePayment_HasAuthenticator) {
+  test_controller()->SetHasAuthenticator(true);
+  NavigateTo("a.com", "/can_make_payment_checker.html");
+
+  {
+    std::string snippet =
+        base::StringPrintf("canMakePaymentForMethodData(%s)", kTestMethodData);
+    EXPECT_EQ("true", content::EvalJs(GetActiveWebContents(), snippet));
+  }
+  {
+    std::string snippet = base::StringPrintf(
+        "hasEnrolledInstrumentForMethodData(%s)", kTestMethodData);
+    EXPECT_EQ("true", content::EvalJs(GetActiveWebContents(), snippet));
+  }
 }
 
 // Intentionally do not enable the "SecurePaymentConfirmation" Blink runtime
@@ -81,7 +135,25 @@ IN_PROC_BROWSER_TEST_F(SecurePaymentConfirmationDisabledTest,
   // EvalJs waits for JavaScript promise to resolve.
   EXPECT_EQ(
       "The payment method \"secure-payment-confirmation\" is not supported.",
-      content::EvalJs(GetActiveWebContents(), kInvokePaymentRequest));
+      content::EvalJs(GetActiveWebContents(),
+                      getInvokePaymentRequestSnippet()));
+}
+
+IN_PROC_BROWSER_TEST_F(SecurePaymentConfirmationDisabledTest,
+                       CannotMakePayment) {
+  test_controller()->SetHasAuthenticator(true);
+  NavigateTo("a.com", "/can_make_payment_checker.html");
+
+  {
+    std::string snippet =
+        base::StringPrintf("canMakePaymentForMethodData(%s)", kTestMethodData);
+    EXPECT_EQ("false", content::EvalJs(GetActiveWebContents(), snippet));
+  }
+  {
+    std::string snippet = base::StringPrintf(
+        "hasEnrolledInstrumentForMethodData(%s)", kTestMethodData);
+    EXPECT_EQ("false", content::EvalJs(GetActiveWebContents(), snippet));
+  }
 }
 
 }  // namespace
--- components/test/data/payments/can_make_payment_checker.js
+++ components/test/data/payments/can_make_payment_checker.js
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
+const kDetails = {
+  total: {label: 'TEST', amount: {currency: 'USD', value: '0.01'}},
+};
+
 /**
  * Checks whether the given payment method can make payments.
  * @param {string} method - The payment method identifier to check.
@@ -12,10 +16,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 async function canMakePayment(method) { // eslint-disable-line no-unused-vars, max-len
   try {
-    const request = new PaymentRequest(
-        [{supportedMethods: method}],
-        {total: {label: 'TEST', amount: {currency: 'USD', value: '0.01'}}});
+    const request = new PaymentRequest([{supportedMethods: method}], kDetails);
     const result = await request.canMakePayment();
+    return result ? 'true' : 'false';
+  } catch (e) {
+    return e.message;
+  }
+}
+
+/**
+ * Creates a PaymentRequest with |methodData| and checks canMakePayment.
+ * @param {object} methodData - The payment method data to build the request.
+ * @return {string} - 'true', 'false', or error message on failure.
+ */
+async function canMakePaymentForMethodData(methodData) { // eslint-disable-line no-unused-vars, max-len
+  try {
+    const request = new PaymentRequest(methodData, kDetails);
+    const result = await request.canMakePayment();
+    return result ? 'true' : 'false';
+  } catch (e) {
+    return e.message;
+  }
+}
+
+/**
+ * Creates a PaymentRequest with |methodData| and checks hasEnrolledInstrument.
+ * @param {object} methodData - The payment method data to build the request.
+ * @return {string} - 'true', 'false', or error message on failure.
+ */
+async function hasEnrolledInstrumentForMethodData(methodData) { // eslint-disable-line no-unused-vars, max-len
+  try {
+    const request = new PaymentRequest(methodData, kDetails);
+    const result = await request.hasEnrolledInstrument();
     return result ? 'true' : 'false';
   } catch (e) {
     return e.message;
