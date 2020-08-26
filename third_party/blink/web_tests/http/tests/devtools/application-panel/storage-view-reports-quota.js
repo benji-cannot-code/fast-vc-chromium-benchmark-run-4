9f8042db3213e7fcee5d6373ffca2b025a27cc1f
Peter Marshall <petermarshall@chromium.org>
2020-08-26 08:28:50 +0000

    devtools: Enable a quota chart test after PieChart rewrite
    Change-Id: I482ddc9931d7fac057769be9c1bddbfcdd5ba430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360072
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801684}

--- third_party/blink/web_tests/http/tests/devtools/application-panel/storage-view-reports-quota.js
+++ third_party/blink/web_tests/http/tests/devtools/application-panel/storage-view-reports-quota.js
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     TestRunner.addResult(quotaStripped);
 
     TestRunner.addResult('Usage breakdown:');
-    const legendElement = view._pieChart.element.shadowRoot.querySelector('.pie-chart-legend');
+    const legendElement = view._pieChart.shadowRoot.querySelector('.pie-chart-legend');
 
     for (var i = 0; i < legendElement.children.length; i++) {
       var typeUsage = ': ';
