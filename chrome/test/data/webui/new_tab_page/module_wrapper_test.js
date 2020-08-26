dc7be5f87cd333e88b11b18800fb0633638da1f6
Tibor Goldschwendt <tiborg@chromium.org>
2020-08-26 16:35:21 +0000

    [ntp][modules] Allow modules to set their height
    + Shrink Kaleidoscope module according to spec.
    
    + Add closure compiler annotation to module descriptor.
    
    Change-Id: Id99185a8b1ee62d154cc663d2d525beaa91a29b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2376460
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801816}

--- chrome/test/data/webui/new_tab_page/module_wrapper_test.js
+++ chrome/test/data/webui/new_tab_page/module_wrapper_test.js
@@ -23,6 +23,7 @@ suite('NewTabPageModuleWrapperTest', () => {
     moduleWrapper.descriptor = {
       id: 'foo',
       name: 'Foo',
+      heightPx: 100,
       title: 'Foo Title',
       element: moduleElement,
     };
@@ -30,6 +31,7 @@ suite('NewTabPageModuleWrapperTest', () => {
     // Assert.
     assertEquals('Foo Title', moduleWrapper.$.title.textContent);
     assertEquals(' • Foo', moduleWrapper.$.name.textContent);
+    assertEquals(100, $$(moduleWrapper, '#moduleElement').offsetHeight);
     assertDeepEquals(
         moduleElement, $$(moduleWrapper, '#moduleElement').children[0]);
   });
