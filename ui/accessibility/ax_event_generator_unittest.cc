de38e83e2b3757be2968cd5df67947eb1169ce02
Joanmarie Diggs <jdiggs@igalia.com>
2020-08-26 16:33:18 +0000

    Fire ATK text-attribute-changed when alignment or indentation change
    Text alignment and indentation are being exposed as object attributes to
    support IA2 and minimize work needed to calculate attributes which can
    only apply to an entire object. However, ATK treats alignment as a text
    attribute and platform ATs expect text-attribute-changed events (there
    is no object-attribute-changed event in ATK). Therefore, fire an AtkText
    text-attribute-changed event to notify ATs when either of these text
    properties change. It is up to platform ATs to check object attributes
    for these properties.
    
    Bug: 1108872
    
    AX-Relnotes: Orca now includes alignment and indentation when its font-
    announcement command is used in Chrome/Chromium. It will be possible for
    Orca to add support for presentation of changes to these properties.
    
    Change-Id: I971777ba4a5f672bf129a4687ff2c54c250c41f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343267
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801815}

--- ui/accessibility/ax_event_generator_unittest.cc
+++ ui/accessibility/ax_event_generator_unittest.cc
@@ -807,6 +807,10 @@ TEST(AXEventGeneratorTest, ObjectAttributeChanged) {
   EXPECT_THAT(
       event_generator,
       UnorderedElementsAre(
+          HasEventAtNode(
+              AXEventGenerator::Event::ATK_TEXT_OBJECT_ATTRIBUTE_CHANGED, 2),
+          HasEventAtNode(
+              AXEventGenerator::Event::ATK_TEXT_OBJECT_ATTRIBUTE_CHANGED, 3),
           HasEventAtNode(AXEventGenerator::Event::OBJECT_ATTRIBUTE_CHANGED, 2),
           HasEventAtNode(AXEventGenerator::Event::OBJECT_ATTRIBUTE_CHANGED,
                          3)));
