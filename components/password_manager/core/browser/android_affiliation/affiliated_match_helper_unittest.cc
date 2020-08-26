aed5a93a273efee9c54e73616a699424ec2f6acd
Nela Kaczmarek <nelakaczmarek@google.com>
2020-08-26 11:57:43 +0000

    [Passwords] Rename MockAffiliationService to MockAndroidAffiliationService.
    This change renames MockAffiliationService to MockAndroidAffiliationService due to change 2339661.
    
    Bug: 1108279
    Change-Id: I26bb0a1a265e595b1a7640efb3cdd1faa8b9467b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2372394
    Commit-Queue: Nela Kaczmarek <nelakaczmarek@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801736}

--- components/password_manager/core/browser/android_affiliation/affiliated_match_helper_unittest.cc
+++ components/password_manager/core/browser/android_affiliation/affiliated_match_helper_unittest.cc
@@ -30,11 +30,9 @@ namespace {
 
 using StrategyOnCacheMiss = AndroidAffiliationService::StrategyOnCacheMiss;
 
-class MockAffiliationService
-    : public testing::StrictMock<AndroidAffiliationService> {
+class MockAndroidAffiliationService : public AndroidAffiliationService {
  public:
-  MockAffiliationService()
-      : testing::StrictMock<AndroidAffiliationService>(nullptr) {
+  MockAndroidAffiliationService() : AndroidAffiliationService(nullptr) {
     testing::DefaultValue<AffiliatedFacets>::Set(AffiliatedFacets());
   }
 
@@ -88,9 +86,6 @@ class MockAffiliationService
                            expected_facet_uri_spec)))
         .RetiresOnSaturation();
   }
-
- private:
-  DISALLOW_ASSIGN(MockAffiliationService);
 };
 
 const char kTestWebFacetURIAlpha1[] = "https://one.alpha.example.com";
@@ -306,7 +301,7 @@ class AffiliatedMatchHelperTest : public testing::Test {
 
   TestPasswordStore* password_store() { return password_store_.get(); }
 
-  MockAffiliationService* mock_affiliation_service() {
+  MockAndroidAffiliationService* mock_affiliation_service() {
     return mock_affiliation_service_;
   }
 
@@ -329,7 +324,8 @@ class AffiliatedMatchHelperTest : public testing::Test {
 
   // testing::Test:
   void SetUp() override {
-    auto service = std::make_unique<MockAffiliationService>();
+    auto service =
+        std::make_unique<testing::StrictMock<MockAndroidAffiliationService>>();
     mock_affiliation_service_ = service.get();
 
     password_store_->Init(nullptr);
@@ -358,9 +354,7 @@ class AffiliatedMatchHelperTest : public testing::Test {
   std::unique_ptr<AffiliatedMatchHelper> match_helper_;
 
   // Owned by |match_helper_|.
-  MockAffiliationService* mock_affiliation_service_ = nullptr;
-
-  DISALLOW_COPY_AND_ASSIGN(AffiliatedMatchHelperTest);
+  MockAndroidAffiliationService* mock_affiliation_service_ = nullptr;
 };
 
 // GetAffiliatedAndroidRealm* tests verify that GetAffiliatedAndroidRealms()
