74ab1576ebd2fe4e0fb88883ec9b78b237f13c4b
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-08-26 23:34:07 +0000

    [CrOS cellular] Migrate setup_selection_flow.js to Polymer3
    Bug: 1111477
    Change-Id: I15c70e99c644e5a74b06868673fe091679daa8c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2376575
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802018}

---
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/setup_selection_flow_test.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// clang-format off
+// #import 'chrome://os-settings/strings.m.js';
+// #import 'chrome://resources/cr_components/chromeos/cellular_setup/setup_selection_flow.m.js';
+
+// #import {flush, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// #import {assertTrue} from '../../../chai_assert.js';
+// clang-format on
+
+suite('CrComponentsSetupSelectionFlowTest', function() {
+  let setupSelectionFlow;
+  setup(function() {
+    setupSelectionFlow = document.createElement('setup-selection-flow');
+    document.body.appendChild(setupSelectionFlow);
+    Polymer.dom.flush();
+  });
+
+  test('Base test', function() {
+    const crRadio = setupSelectionFlow.$$('cr-radio-group');
+    assertTrue(!!crRadio);
+  });
+});
