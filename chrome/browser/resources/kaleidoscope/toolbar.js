7f2a0229f369e3b8ba909693317f0fb759289a09
Tommy Steimel <steimel@chromium.org>
2020-08-26 16:38:12 +0000

    Kaleidoscope: Let JS set toolbar menu button tooltip text
    Bug: b:166321735
    Change-Id: I53aa13485ac9f014c6ebc0ab8bf38f65204e0637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2375600
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801817}

--- chrome/browser/resources/kaleidoscope/toolbar.js
+++ chrome/browser/resources/kaleidoscope/toolbar.js
@@ -20,6 +20,9 @@ class KaleidoscopeToolbarElement extends PolymerElement {
     return {
       // Controls whether the search field is shown.
       showSearch: {type: Boolean, value: false},
+
+      // Sets the tooltip text displayed on the menu button.
+      menuLabel: {type: String, value: ''},
     };
   }
 }
