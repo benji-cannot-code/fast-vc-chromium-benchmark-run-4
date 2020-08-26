71a1199f315a3225fe81ba1864164259416614ad
Hesen Zhang <hesen@google.com>
2020-08-26 18:57:31 +0000

    Fixed unintialized warning in clang.
    Reland "[Video Tutorials] Add internal tutorial group."
    
    This is a reland of a96793a704d69ebccdbec9b00d066fd22ae2a42e
    
    Original change's description:
    > [Video Tutorials] Add internal tutorial group.
    >
    > Bug: 1115755
    > Change-Id: I481a8f6498188681216d13df32ee41436cd1306c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2358056
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Commit-Queue: Hesen Zhang <hesen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#800642}
    
    Bug: 1115755
    Change-Id: I0960fef750bba2e37fc0d36574376e9c4de44c7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2372883
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801868}

--- chrome/browser/video_tutorials/tutorial.cc
+++ chrome/browser/video_tutorials/tutorial.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace video_tutorials {
 
-Tutorial::Tutorial() = default;
+Tutorial::Tutorial() : feature(FeatureType::kInvalid), video_length(0) {}
 
 Tutorial::Tutorial(FeatureType feature,
                    const std::string& title,
--- chrome/browser/video_tutorials/tutorial.h
+++ chrome/browser/video_tutorials/tutorial.h
@@ -44,7 +44,7 @@ struct Tutorial {
   Tutorial& operator=(const Tutorial& other);
 
   // Type of feature where this video tutorial targeted.
-  FeatureType feature{FeatureType::kInvalid};
+  FeatureType feature;
 
   // The title of the video.
   std::string title;
