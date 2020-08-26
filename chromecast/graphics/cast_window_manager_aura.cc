001a4d28c94e8320d90d7ab1f58192173cf13aea
Shawn Gallea <sagallea@google.com>
2020-08-26 01:12:51 +0000

    Enable reduced framerate on headless chromecast devices
    Removes experiment, enables 4hz framerate on all headless
    chromecast devices.
    
    Bug: b/139299756
    Test: Locally on device
    Change-Id: I8d6b9b1010c186837b16edffced8d7dc7cac6f3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2376459
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Cr-Commit-Position: refs/heads/master@{#801628}

--- chromecast/graphics/cast_window_manager_aura.cc
+++ chromecast/graphics/cast_window_manager_aura.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/graphics/cast_window_manager_aura.h"
 
-#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "build/build_config.h"
 #include "chromecast/base/cast_features.h"
@@ -237,11 +236,8 @@ void CastWindowManagerAura::Setup() {
   rounded_window_corners_ = RoundedWindowCorners::Create(this);
 
 #if BUILDFLAG(IS_CAST_AUDIO_ONLY)
-  if (base::FeatureList::IsEnabled(kReduceHeadlessFrameRate)) {
-    ui::Compositor* compositor = window_tree_host_->compositor();
-    compositor->SetDisplayVSyncParameters(
-        base::TimeTicks(), base::TimeDelta::FromMilliseconds(250));
-  }
+  window_tree_host_->compositor()->SetDisplayVSyncParameters(
+      base::TimeTicks(), base::TimeDelta::FromMilliseconds(250));
 #endif
 }
 
