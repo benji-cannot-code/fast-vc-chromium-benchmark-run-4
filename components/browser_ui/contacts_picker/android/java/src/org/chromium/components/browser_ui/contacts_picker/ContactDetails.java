0a373afcd2ff8c5f3e14cd0cf0b83c3e8c33ae87
Evan Stade <estade@chromium.org>
2020-08-26 20:28:50 +0000

    Reland "Move Contacts Picker to //components/browser_ui/contacts_picker"
    This relands commit c4aceb1609a4851a48f34b15a07d8080a4e0db8a.
    
    Difference: updates to ContactsPickerDialogTest to fix L tester
    
    Original change's description:
    > Move Contacts Picker to //components/browser_ui/contacts_picker
    >
    > Bug: 1016938
    > Change-Id: Ib3a86b4a3e3c0356c292ce55291ef0487e94c5ae
    > Skip-Translation-Screenshots-Check: True
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2325272
    > Commit-Queue: Evan Stade <estade@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Lijin Shen <lazzzis@google.com>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#801610}
    
    TBR=dcheng@chromium.org,finnur@chromium.org,tedchoc@chromium.org,wfh@chromium.org,lazzzis@google.com
    
    Skip-Translation-Screenshots-Check: True
    Exempt-From-Owner-Approval: TBRing
    Bug: 1016938
    Change-Id: Ifa455c12c37b98120ad0bac2a75796eaa6ff2190
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378211
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801918}

--- components/browser_ui/contacts_picker/android/java/src/org/chromium/components/browser_ui/contacts_picker/ContactDetails.java
+++ components/browser_ui/contacts_picker/android/java/src/org/chromium/components/browser_ui/contacts_picker/ContactDetails.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.contacts_picker;
+package org.chromium.components.browser_ui.contacts_picker;
 
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
@@ -11,7 +11,6 @@ import android.graphics.drawable.Drawable;
 import androidx.annotation.Nullable;
 
 import org.chromium.blink.mojom.ContactIconBlob;
-import org.chromium.chrome.R;
 import org.chromium.payments.mojom.PaymentAddress;
 
 import java.util.ArrayList;
