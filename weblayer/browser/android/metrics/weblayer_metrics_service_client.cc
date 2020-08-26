edb434aa705ec80567a522102db1b6f5ae45429d
Scott Violet <sky@chromium.org>
2020-08-26 17:26:31 +0000

    weblayer: wires up ContentStabilityMetricsProvider
    This gives the core stability metrics.
    
    BUG=1120537
    TEST=covered by weblayer_browsertests
    
    Change-Id: I22e99959dacb9b9593af02cea70088d449e604a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2376565
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801836}

--- weblayer/browser/android/metrics/weblayer_metrics_service_client.cc
+++ weblayer/browser/android/metrics/weblayer_metrics_service_client.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64.h"
 #include "base/no_destructor.h"
+#include "components/metrics/content/content_stability_metrics_provider.h"
+#include "components/metrics/content/extensions_helper.h"
 #include "components/metrics/metrics_provider.h"
 #include "components/metrics/metrics_service.h"
 #include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
@@ -151,6 +153,9 @@ int WebLayerMetricsServiceClient::GetPackageNameLimitRatePerMille() {
 
 void WebLayerMetricsServiceClient::RegisterAdditionalMetricsProviders(
     metrics::MetricsService* service) {
+  service->RegisterMetricsProvider(
+      std::make_unique<metrics::ContentStabilityMetricsProvider>(pref_service(),
+                                                                 nullptr));
   service->RegisterMetricsProvider(std::make_unique<PageLoadMetricsProvider>());
 }
 
