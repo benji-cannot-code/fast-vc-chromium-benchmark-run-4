51448ec3b308a2c805958a6f029908c86df438be
David Tseng <dtseng@chromium.org>
2020-08-26 22:50:02 +0000

    Add ChromeVox gesture metrics
    See go/chromevox-touch-gestures
    
    R=dmazzoni@chromium.org
    
    AX-Relnotes: n/a
    Change-Id: Ia69121b5889e50ba04c466ca811dfd186eb44290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2377897
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801998}

--- ash/accessibility/touch_exploration_manager.cc
+++ ash/accessibility/touch_exploration_manager.cc
@@ -18,10 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wm/window_util.h"
 #include "base/command_line.h"
+#include "base/metrics/histogram_functions.h"
 #include "chromeos/audio/chromeos_sounds.h"
 #include "chromeos/audio/cras_audio_handler.h"
 #include "chromeos/constants/chromeos_switches.h"
 #include "extensions/common/constants.h"
+#include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/wm/public/activation_client.h"
@@ -129,6 +131,8 @@ void TouchExplorationManager::PlayEnterScreenEarcon() {
 void TouchExplorationManager::HandleAccessibilityGesture(
     ax::mojom::Gesture gesture,
     gfx::PointF location) {
+  base::UmaHistogramEnumeration("Accessibility.ChromeVox.PerformGestureType",
+                                gesture);
   GetA11yController()->HandleAccessibilityGesture(gesture, location);
 }
 
