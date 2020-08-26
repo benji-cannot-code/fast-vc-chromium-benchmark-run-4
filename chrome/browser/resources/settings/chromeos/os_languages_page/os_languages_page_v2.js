175e201a04b9df61e005d29d56ad95d1fb0dd24e
My Nguyen <myy@chromium.org>
2020-08-26 22:45:44 +0000

    [OsSettingsLanguage] Add Change Device Language Dialog
    Add blank dialog for now.
    Both Action and Cancel buttons currently close the dialog.
    Current view: http://screen/3NCtVAFiCFdFYcx
    
    Strings are being finalised by UX writing so keeping them as
    translateable=false and no screenshots required.
    
    Bug: 1113439
    Change-Id: I0128a8bf1c3989c691f0ca5a1a3701c1faea171a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367254
    Commit-Queue: My Nguyen <myy@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801994}

--- chrome/browser/resources/settings/chromeos/os_languages_page/os_languages_page_v2.js
+++ chrome/browser/resources/settings/chromeos/os_languages_page/os_languages_page_v2.js
@@ -47,6 +47,12 @@ Polymer({
 
     /** @private */
     showAddLanguagesDialog_: Boolean,
+
+    /** @private */
+    showChangeDeviceLanguageDialog_: {
+      type: Boolean,
+      value: false,
+    },
   },
 
   /** @private {?settings.LanguagesMetricsProxy} */
@@ -69,7 +75,13 @@ Polymer({
 
   /** @private */
   onChangeSystemLanguageClick_() {
-    // TODO(crbug/1113439): Implement change system language dialog.
+    this.showChangeDeviceLanguageDialog_ = true;
+  },
+
+  /** @private */
+  onChangeDeviceLanguageDialogClose_() {
+    this.showChangeDeviceLanguageDialog_ = false;
+    cr.ui.focusWithoutInk(assert(this.$.changeSystemLanguage));
   },
 
   /**
