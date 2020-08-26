25024a028a0bcd4f12e8b461dc2b90ca26bf1677
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-08-26 14:13:53 +0000

    Import wpt@8adc6cbbf26a654baff822ceaa4ac6faf17d3b99
    Using wpt-import in Chromium dc5e5f36bcc0a92b77ead0788c79390c151a4086.
    With Chromium commits locally applied on WPT:
    9f52c7e47e "COOP: add reporting to redirects"
    d18db80440 "Fix test flake in animation-state-changes-positive-playback-rate.html"
    4e9cbb662e "Fixed test/reference mismatch for border."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    guidou@chromium.org, armax@chromium.org:
      external/wpt/mediacapture-streams
    
    NOAUTOREVERT=true
    TBR=robertma@google.com
    
    No-Export: true
    Change-Id: I234b171fda432540cad21c18a24a0ee2f6b0faf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378005
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#801778}

---
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-embedder-policy/resources/empty-coep.py
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    headers = [(b'Content-Type', b'text/html')]
+
+    for value in request.GET.get_list(b'value'):
+        headers.append((b'Cross-Origin-Embedder-Policy', value))
+
+    return (200, headers, u'')
