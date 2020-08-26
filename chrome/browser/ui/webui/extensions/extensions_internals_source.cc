8e1bb362a22425f9b85a64b54ae8f940bdae1d7d
David Bienvenu <davidbienvenu@chromium.org>
2020-08-26 22:49:43 +0000

    remove DISALLOW_COPY_ASSIGN from chrome/browser extensions files
    Bug: 1010217
    Change-Id: I93e5284aeaa6aef40265ecfae71e4b8b48d7d6f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378105
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801997}

--- chrome/browser/ui/webui/extensions/extensions_internals_source.cc
+++ chrome/browser/ui/webui/extensions/extensions_internals_source.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/extensions/extensions_internals_source.h"
 
+#include <memory>
 #include <string>
 #include <unordered_map>
 #include <utility>
