5ea6185bb89358f7b0ffc62dd0b2870c288e9c94
Monica Basta <msalama@chromium.org>
2020-08-26 15:08:26 +0000

    [ProfilePicker]: Add create local profile functionality.
    This CL adds 'Done' button that on click will create a new profile. Once
    the profile is created, it is opened and the picker is closed. This CL
    also adjusts the layout of the page based on the latest mocks that takes
    into consideration different screen sizes.
    
    Screenshots:
    https://drive.google.com/file/d/1js21n3dxRaSkOqdBnn3v27e096UaiPE5/view?usp=sharing
    https://drive.google.com/file/d/11SIROfvDnIud8JkqZDsmlegnNkoDwmYD/view?usp=sharing
    https://drive.google.com/file/d/1x12IWRlhcQbLf4cmJ_1WoeQXzc9WDmn3/view?usp=sharing
    https://drive.google.com/file/d/17cfK6xuAkim9jvNxIgHIUVVljwOsUTu0/view?usp=sharing
    https://drive.google.com/file/d/1bkzZlNmae1ZIA_7b_13vf-THGcNZ8nD0/view?usp=sharing
    https://drive.google.com/file/d/1GnnSQ4qTRJxXEnsOPLyWoaNjnO_4zxDa/view?usp=sharing
    
    Bug: 1115056
    Change-Id: I72c6f2ee07751b61f264443eab468583f7b2c95a
    Fixed: 1121662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2375292
    Auto-Submit: Monica Basta <msalama@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801790}

--- chrome/test/data/webui/signin/test_manage_profiles_browser_proxy.js
+++ chrome/test/data/webui/signin/test_manage_profiles_browser_proxy.js
@@ -20,6 +20,7 @@ export class TestManageProfilesBrowserProxy extends TestBrowserProxy {
       'removeProfile',
       'getProfileStatistics',
       'loadSignInProfileCreationFlow',
+      'createProfile',
     ]);
   }
 
@@ -53,6 +54,7 @@ export class TestManageProfilesBrowserProxy extends TestBrowserProxy {
     this.methodCalled('getNewProfileSuggestedThemeInfo');
     return Promise.resolve({
       colorId: 0,
+      color: 0,
       themeFrameColor: '',
       themeShapeColor: '',
       themeFrameTextColor: '',
@@ -73,5 +75,13 @@ export class TestManageProfilesBrowserProxy extends TestBrowserProxy {
   /** @override */
   loadSignInProfileCreationFlow() {
     this.methodCalled('loadSignInProfileCreationFlow');
+  }
+
+  /** @override */
+  createProfile(
+      profileName, profileColor, avatarUrl, isGeneric, createShortcut) {
+    this.methodCalled(
+        'createProfile',
+        [profileName, profileColor, avatarUrl, isGeneric, createShortcut]);
   }
 }
