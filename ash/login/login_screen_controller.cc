01d8443d9a451edc4b0082c2c727de813adb5adf
Thomas Tellier <tellier@google.com>
2020-08-27 11:44:45 +0000

    Reland "Add time loading reporter for Chrome OS views-based multiprofile"
    This reverts commit ca53ea05b3febd348297deb6d2b81edbf0774bee.
    
    Reason for revert: Found a fix for use-after-free error
    
    Original change's description:
    > Revert "Add time loading reporter for Chrome OS views-based multiprofile"
    > 
    > This reverts commit a89614a196d83755c76459947fa9faa1082fa562.
    > 
    > Reason for revert: Seems to be causing heap-use-after-free failures on Linux ChromiumOS MSan/LSan/TSan builders:
    > 
    > https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/20296
    > 
    > https://ci.chromium.org/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/38328
    > 
    > Original change's description:
    > > Add time loading reporter for Chrome OS views-based multiprofile
    > > 
    > > Bug: 1066489
    > > Change-Id: Iceeaee0f33dff50d75361d1f14e14299a37a0830
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292477
    > > Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    > > Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    > > Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#800055}
    > 
    > TBR=antrim@chromium.org,asvitkine@chromium.org,rsorokin@chromium.org,tellier@google.com
    > 
    > Change-Id: Iaae1e4466b6b5ac06f609efacc2f02398ab27fd2
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1066489
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367193
    > Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    > Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#800171}
    
    TBR=antrim@chromium.org,mohsen@chromium.org,asvitkine@chromium.org,rsorokin@chromium.org,tellier@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1066489
    Change-Id: I2ee0bae97dfebd2be6f745088f954e8afdd7688c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2377877
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Thomas Tellier <tellier@google.com>
    Cr-Commit-Position: refs/heads/master@{#802185}

--- ash/login/login_screen_controller.cc
+++ ash/login/login_screen_controller.cc
@@ -479,4 +479,10 @@ void LoginScreenController::OnFocusLeavingSystemTray(bool reverse) {
   client_->OnFocusLeavingSystemTray(reverse);
 }
 
+void LoginScreenController::NotifyLoginScreenShown() {
+  if (!client_)
+    return;
+  client_->OnLoginScreenShown();
+}
+
 }  // namespace ash
