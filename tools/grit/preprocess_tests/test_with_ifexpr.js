bf325625d474e2400ead44933a645a781c4fd916
rbpotter <rbpotter@chromium.org>
2020-08-27 02:02:41 +0000

    Web UI: Add preprocess_grit build rule and script
    Add a preprocess_grit build rule and script. This will allow Web UI
    files to be preprocessed by grit without going through the pak/unpack
    process, and without requiring such files to be listed in a grd file.
    This simplifies the process of creating generated output folders with
    fully preprocessed files for bundling.
    
    Bug: 1071641
    Change-Id: Ifd69f9d985083555987a7d968378a7f7aeff15c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2369981
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802071}

---
+++ tools/grit/preprocess_tests/test_with_ifexpr.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Polymer({
+  is: 'cr-foo',
+
+  _template: html`
+    <if expr="bar">
+      <button on-click="onClick_">I should be included in HTML</button>
+    </if>
+    <if expr="apple">
+      <div>I should be excluded from HTML</div>
+    </if>
+  `,
+
+  onClick_() {
+    // <if expr="orange">
+    console.log('I should be excluded from JS');
+    // </if>
+    // <if expr="foo">
+    console.log('I should be included in JS');
+    // </if>
+  }
+});
