dd349d680cb4954cd11fdec31deb177db1617d83
Avi Drissman <avi@chromium.org>
2020-08-27 23:30:37 +0000

    Update Mac memory code references
    The Mac malloc interceptor relies on internals of macOS.
    Update those internals and links to the 10.15 versions.
    
    This relands 994f22b51a586390c36b5713812baeae1e96262a
    with a fix for profiling_client.cc which correctly only
    includes a Mac file header on the Mac.
    
    Bug: none
    Change-Id: I8132dc1ffeeb0c6b65edb80cc729bcb4836f2121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2377983
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802429}

--- components/services/heap_profiling/public/cpp/profiling_client.cc
+++ components/services/heap_profiling/public/cpp/profiling_client.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_set>
 #include <vector>
 
-#include "base/allocator/allocator_interception_mac.h"
 #include "base/bind.h"
 #include "base/debug/stack_trace.h"
 #include "base/lazy_instance.h"
@@ -24,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_ANDROID) && BUILDFLAG(CAN_UNWIND_WITH_CFI_TABLE) && \
     defined(OFFICIAL_BUILD)
 #include "base/trace_event/cfi_backtrace_android.h"
+#endif
+
+#if defined(OS_APPLE)
+#include "base/allocator/allocator_interception_mac.h"
 #endif
 
 namespace heap_profiling {
