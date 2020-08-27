e612226c442827905eb1b23f8642a5f23ebe076e
Sebastien Marchand <sebmarchand@chromium.org>
2020-08-27 20:56:09 +0000

    Ensure that child processes dump their profiling data when terminating
    This ensures that child processes don't die before flushing their
    profile data to disk. This is supposed to happen automatically at
    shutdown but it doesn't always have a chance to complete (probably
    because we tend to fast kill processes). This affects mostly the utility
    processes, and as they end up writing their data into the same file as
    the renderer they can sometime end up corrupting these files and make us
    lose some precious profiling data.
    
    This only affects profile instrumented builds (PGO profiling builds and
    coverage builds).
    
    
    Bug: 1119887
    Change-Id: Ib50e876e7637a670ad05982c5ff03a898dc84ff5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378272
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802348}

--- content/child/child_process.cc
+++ content/child/child_process.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include "base/bind.h"
+#include "base/clang_profiling_buildflags.h"
 #include "base/lazy_instance.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/process/process_handle.h"
@@ -23,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/policy/sandbox_type.h"
 #include "services/tracing/public/cpp/trace_startup.h"
 #include "third_party/blink/public/common/features.h"
+
+#if BUILDFLAG(CLANG_PROFILING_INSIDE_SANDBOX)
+#include "base/test/clang_profiling.h"
+#endif
 
 namespace content {
 
@@ -122,6 +127,13 @@ ChildProcess::~ChildProcess() {
     DCHECK(base::ThreadPoolInstance::Get());
     base::ThreadPoolInstance::Get()->Shutdown();
   }
+
+#if BUILDFLAG(CLANG_PROFILING_INSIDE_SANDBOX)
+  // Flush the profiling data to disk. Doing this manually (vs relying on this
+  // being done automatically when the process exits) will ensure that this data
+  // doesn't get lost if the process is fast killed.
+  base::WriteClangProfilingProfile();
+#endif
 }
 
 ChildThreadImpl* ChildProcess::main_thread() {
