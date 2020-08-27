329ac79da75e695e8a23c0935de65f073de9134e
Jonah Chin <jochin@microsoft.com>
2020-08-27 01:18:29 +0000

    Remove more PaintImage::GetSkImage() callers
    PaintImage::GetSkImage() is being deprecated.
    
    This CL cleans up more GetSkImage() callsites by using the new
    GetSwSkImage() API, other APIs directly on PaintImage, or using
    SkImageInfo to obtain image metadata.
    
    Bug: 1018894
    Change-Id: Iad80746565a2a9b8193a6025893c0beda6fc7cb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2355100
    Commit-Queue: Jonah Chin <jochin@microsoft.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802055}

--- cc/paint/paint_image.h
+++ cc/paint/paint_image.h
@@ -266,7 +266,7 @@ class CC_PAINT_EXPORT PaintImage {
     return paint_worklet_input_ ? 0 : GetSkImage()->uniqueID();
   }
   explicit operator bool() const {
-    return paint_worklet_input_ || !!GetSkImage() || texture_backing_;
+    return paint_worklet_input_ || cached_sk_image_ || texture_backing_;
   }
   bool IsLazyGenerated() const {
     return paint_record_ || paint_image_generator_;
@@ -337,6 +337,7 @@ class CC_PAINT_EXPORT PaintImage {
   friend class PlaybackImageProvider;
   friend class DrawImageRectOp;
   friend class DrawImageOp;
+  friend class AcceleratedStaticBitmapImageTest;
 
   bool DecodeFromGenerator(void* memory,
                            SkImageInfo* info,
