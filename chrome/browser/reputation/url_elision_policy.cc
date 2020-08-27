ce61f43df3a4329e8fe57cc7ef586514ca921b59
Joe DeBlasio <jdeblasio@chromium.org>
2020-08-27 01:25:17 +0000

    [Simplified domains] Add feature parameter for keyword heuristic.
    This CL just adds a feature parameter to tune whether or not to use the keyword
    heuristic for eTLD+1 elision. The param defaults to true so that
    manually-opted-in users get the full elision behavior.
    
    Fixed: 1122309
    Change-Id: I334305d9ada6044dadbdf39d283253aae51d26f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378739
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802058}

--- chrome/browser/reputation/url_elision_policy.cc
+++ chrome/browser/reputation/url_elision_policy.cc
@@ -19,6 +19,8 @@ namespace {
 
 const base::FeatureParam<int> kMaximumUnelidedHostnameLength{
     &omnibox::kMaybeElideToRegistrableDomain, "max_unelided_host_length", 25};
+const base::FeatureParam<bool> kEnableKeywordBasedElision{
+    &omnibox::kMaybeElideToRegistrableDomain, "enable_keyword_elision", true};
 
 }  // namespace
 
@@ -43,7 +45,8 @@ bool ShouldElideToRegistrableDomain(const GURL& url) {
   // Hostnames using sensitive keywords (typically, brandnames) are often social
   // engineering, and thus should only show the registrable domain.
   auto eTLD_plus_one = GetETLDPlusOne(host);
-  if (HostnameContainsKeyword(url, eTLD_plus_one, top500_domains::kTopKeywords,
+  if (kEnableKeywordBasedElision.Get() &&
+      HostnameContainsKeyword(url, eTLD_plus_one, top500_domains::kTopKeywords,
                               top500_domains::kNumTopKeywords)) {
     return true;
   }
