a545219b7b5683a4c44bf9a7b2c19d9ffbb672cf
Kuo Jen Wei <inker@google.com>
2020-08-27 07:24:33 +0000

    CCA: Separate modes class into multiple files
    Bug: 1113514
    Test: tast run <DUT> "camera.CCA*"
    
    Change-Id: I6134faa821936a7e130d319c6709cba414728e93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303331
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802134}

--- chromeos/components/camera_app_ui/resources/src/js/views/camera.js
+++ chromeos/components/camera_app_ui/resources/src/js/views/camera.js
@@ -30,12 +30,12 @@ import {
 import * as util from '../util.js';
 
 import {Layout} from './camera/layout.js';
-import {
+import {   // eslint-disable-line no-unused-vars
   Modes,
-  PhotoResult,  // eslint-disable-line no-unused-vars
+  PhotoHandler,  // eslint-disable-line no-unused-vars
   Video,
-  VideoResult,  // eslint-disable-line no-unused-vars
-} from './camera/modes.js';
+  VideoHandler,  // eslint-disable-line no-unused-vars
+} from './camera/mode/index.js';
 import {Options} from './camera/options.js';
 import {Preview} from './camera/preview.js';
 import * as timertick from './camera/timertick.js';
@@ -56,6 +56,8 @@ class CameraSuspendedError extends Error {
 
 /**
  * Camera-view controller.
+ * @implements {VideoHandler}
+ * @implements {PhotoHandler}
  */
 export class Camera extends View {
   /**
@@ -124,13 +126,6 @@ export class Camera extends View {
      */
     this.activeDeviceId_ = null;
 
-    const createVideoSaver = async () => resultSaver.startSaveVideo();
-
-    const playShutterEffect = () => {
-      sound.play('#sound-shutter');
-      util.animateOnce(this.preview_.video);
-    };
-
     /**
      * Modes for the camera.
      * @type {!Modes}
@@ -138,9 +133,7 @@ export class Camera extends View {
      */
     this.modes_ = new Modes(
         this.defaultMode_, photoPreferrer, videoPreferrer,
-        this.start.bind(this), this.doSavePhoto_.bind(this), createVideoSaver,
-        this.doSaveVideo_.bind(this), playShutterEffect,
-        () => this.preview_.toImage());
+        this.start.bind(this), this, this);
 
     /**
      * @type {!Facing}
@@ -390,13 +383,9 @@ export class Camera extends View {
   }
 
   /**
-   * Handles captured photo result.
-   * @param {!PhotoResult} result Captured photo result.
-   * @param {string} name Name of the photo result to be saved as.
-   * @return {!Promise} Promise for the operation.
-   * @protected
+   * @override
    */
-  async doSavePhoto_({resolution, blob, isVideoSnapshot = false}, name) {
+  async handleResultPhoto({resolution, blob, isVideoSnapshot}, name) {
     metrics.sendCaptureEvent({
       facing: this.facingMode_,
       resolution,
@@ -412,12 +401,31 @@ export class Camera extends View {
   }
 
   /**
-   * Handles captured video result.
-   * @param {!VideoResult} result Captured video result.
-   * @return {!Promise} Promise for the operation.
-   * @protected
+   * @override
    */
-  async doSaveVideo_({resolution, duration, videoSaver, everPaused}) {
+  createVideoSaver() {
+    return this.resultSaver_.startSaveVideo();
+  }
+
+  /**
+   * @override
+   */
+  playShutterEffect() {
+    sound.play('#sound-shutter');
+    util.animateOnce(this.preview_.video);
+  }
+
+  /**
+   * @override
+   */
+  getPreviewFrame() {
+    return this.preview_.toImage();
+  }
+
+  /**
+   * @override
+   */
+  async handleResultVideo({resolution, duration, videoSaver, everPaused}) {
     metrics.sendCaptureEvent({
       facing: this.facingMode_,
       duration,
