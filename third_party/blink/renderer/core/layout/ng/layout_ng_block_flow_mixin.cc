4161ffbb71eefbc0d152832bbe623309a6bd0a1c
Fredrik Söderqvist <fs@opera.com>
2020-08-27 19:48:59 +0000

    Reduce the size of hit_test_result.h
    Reduces the estimated size from ~9.28M to ~2.14M.
    
    Bug: 242216
    Change-Id: Ibfa4ba9f9ecd57e97a0efffb553d153402abed84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379756
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802322}

--- third_party/blink/renderer/core/layout/ng/layout_ng_block_flow_mixin.cc
+++ third_party/blink/renderer/core/layout/ng/layout_ng_block_flow_mixin.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/editing/position_with_affinity.h"
+#include "third_party/blink/renderer/core/editing/visible_units.h"
 #include "third_party/blink/renderer/core/layout/hit_test_location.h"
 #include "third_party/blink/renderer/core/layout/layout_analyzer.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
