badfe7cc5dc769743b9ba264219931bb8c953e0d
Tal Pressman <talp@chromium.org>
2020-08-27 00:41:00 +0000

    Revert "[webui][ntp] Move GN targets close to source files"
    This reverts commit e2431d7562c98e40b3895a1a9b349ca68432aa23.
    
    Reason for revert: Broke a number of chrome builders with the following error:
    Step _generate_buildfiles failed. Error logs are shown below:
    ERROR Unresolved dependencies.
    //chrome/browser/media/kaleidoscope/internal/resources/module:module(//build/toolchain/linux:clang_x64)
      needs //chrome/browser/resources/new_tab_page:module_descriptor(//build/toolchain/linux:clang_x64)
    
    Sample failed build: ci.chromium.org/p/chrome/builders/ci/linux-chrome/14497
    
    Original change's description:
    > [webui][ntp] Move GN targets close to source files
    > 
    > This is more consistent with the GN targets of other WebUIs.
    > 
    > + Move module_wrapper.* to modules/.
    > 
    > Change-Id: I23be97df455e8cb0a9ed35b15b184902b3fa1a9c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2376529
    > Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    > Reviewed-by: dpapad <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#802008}
    
    TBR=dpapad@chromium.org,tiborg@chromium.org
    
    Change-Id: Idb6c8115599e74333fab357cb2d40a040e8c0c82
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2377038
    Reviewed-by: Tal Pressman <talp@chromium.org>
    Commit-Queue: Tal Pressman <talp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802045}

--- chrome/test/data/webui/new_tab_page/module_wrapper_test.js
+++ chrome/test/data/webui/new_tab_page/module_wrapper_test.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {$$} from 'chrome://new-tab-page/new_tab_page.js';
 
-suite('NewTabPageModulesModuleWrapperTest', () => {
+suite('NewTabPageModuleWrapperTest', () => {
   /** @type {!ModuleWrapperElement} */
   let moduleWrapper;
 
