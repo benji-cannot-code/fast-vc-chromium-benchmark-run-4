9774e92e3990667d46403ab1434e9406eb1a422e
Tom Anderson <thomasanderson@chromium.org>
2020-08-27 12:09:52 +0000

    [XProto] Address hferreiro's review comments from r789288
    R=hferreiro
    BUG=1066670
    
    Change-Id: Ifa76975393af60b056ec05326b1fc6252e6873a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378142
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#802190}

--- ui/base/x/x11_util.cc
+++ ui/base/x/x11_util.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
-#include "base/debug/stack_trace.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted_memory.h"
