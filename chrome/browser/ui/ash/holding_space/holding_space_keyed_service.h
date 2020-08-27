0ab88002e4a8d81356499e071e26eb142dc9d9df
Ana Salazar <anasalazar@chromium.org>
2020-08-27 20:25:15 +0000

    Add Download Files to Holding Space Model
    Holding space keyed service now observes downloads. As new downloads are
    completed, they are added to the holding space model.
    
    Bug: 1111995
    Change-Id: Id45b276a3ada06d94f7efd42bc35cf89cc3fe070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378648
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802335}

--- chrome/browser/ui/ash/holding_space/holding_space_keyed_service.h
+++ chrome/browser/ui/ash/holding_space/holding_space_keyed_service.h
@@ -10,9 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/holding_space/holding_space_model_observer.h"
 #include "base/scoped_observer.h"
 #include "base/strings/string16.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/profiles/profile_manager_observer.h"
 #include "chrome/browser/ui/ash/holding_space/holding_space_client_impl.h"
 #include "components/account_id/account_id.h"
+#include "components/download/public/common/download_item.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "content/public/browser/download_manager.h"
 
 class GURL;
 
@@ -38,7 +42,10 @@ namespace ash {
 // *   Manages the temporary holding space per-profile data model.
 // *   Serves as an entry point to add holding space items from Chrome.
 class HoldingSpaceKeyedService : public KeyedService,
-                                 public HoldingSpaceModelObserver {
+                                 public HoldingSpaceModelObserver,
+                                 public ProfileManagerObserver,
+                                 public content::DownloadManager::Observer,
+                                 public download::DownloadItem::Observer {
  public:
   // Preference path at which holding space items are persisted.
   // NOTE: Any changes to persistence must be backwards compatible.
@@ -54,11 +61,31 @@ class HoldingSpaceKeyedService : public KeyedService,
   // Registers profile preferences for holding space.
   static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 
-  // Adds a screenshot item backed by the provided absolute file path.
+  // Methods to add an item backed by the provided absolute file path.
   // The path is expected to be under a mount point path recognized by the file
   // manager app (otherwise, the item will be dropped silently).
   void AddScreenshot(const base::FilePath& screenshot_path,
                      const gfx::ImageSkia& image);
+  void AddDownload(const base::FilePath& download_path);
+
+  void RemoveDownloadManagerObservers();
+
+  // KeyedService:
+  void Shutdown() override;
+
+  // ProfileManagerObserver:
+  void OnProfileAdded(Profile* profile) override;
+
+  // content::DownloadManager::Observer implementation:
+  void OnDownloadCreated(content::DownloadManager* manager,
+                         download::DownloadItem* item) override;
+  void OnDownloadDropped(content::DownloadManager* manager) override;
+  void OnManagerInitialized() override;
+  void ManagerGoingDown(content::DownloadManager* manager) override;
+
+  // download::DownloadItem::Observer implementation:
+  void OnDownloadUpdated(download::DownloadItem* item) override;
+
 
   const HoldingSpaceClient* client_for_testing() const {
     return &holding_space_client_;
@@ -67,6 +94,7 @@ class HoldingSpaceKeyedService : public KeyedService,
   const HoldingSpaceModel* model_for_testing() const {
     return &holding_space_model_;
   }
+  void SetDownloadManagerForTesting(content::DownloadManager* manager);
 
  private:
   // HoldingSpaceModelObserver:
@@ -86,6 +114,12 @@ class HoldingSpaceKeyedService : public KeyedService,
 
   ScopedObserver<HoldingSpaceModel, HoldingSpaceModelObserver>
       holding_space_model_observer_{this};
+
+  content::DownloadManager* download_manager_ = nullptr;
+  ScopedObserver<ProfileManager, ProfileManagerObserver>
+      observed_profile_manager_{this};
+  ScopedObserver<download::DownloadItem, download::DownloadItem::Observer>
+      download_items_observer_{this};
 };
 
 }  // namespace ash
