50889be622575202e2222692fa8a68343188e89e
Noah Rose Ledesma <noahrose@google.com>
2020-08-27 18:44:26 +0000

    Create IPC messages for when audio output device changing is disabled
    This change adds IPC message declarations, senders, and receivers for
    communicating between the renderer and browser that audio device
    changing is no longer possible.
    
    When the audio output device cannot be changed, the Global Media
    Controls UI should not display a device picker. This change is a part
    of a series of changes for appropriately hiding this UI.
    
    Bug: 1120620
    Change-Id: Ib5610fa95d56f284e1957c59bd4f098112c1a81a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368285
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802305}

--- content/browser/media/session/media_session_controller.cc
+++ content/browser/media/session/media_session_controller.cc
@@ -167,6 +167,8 @@ void MediaSessionController::OnAudioOutputSinkChanged(
   media_session_->OnAudioOutputSinkIdChanged();
 }
 
+void MediaSessionController::OnAudioOutputSinkChangingDisabled() {}
+
 bool MediaSessionController::IsMediaSessionNeeded() const {
   if (!is_playback_in_progress_)
     return false;
