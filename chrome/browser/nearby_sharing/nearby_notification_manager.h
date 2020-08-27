f5a3df05b96930a8b628b46119be0fb457fa752e
Himanshu Jaju <himanshujaju@chromium.org>
2020-08-27 14:20:23 +0000

    Handle click on success notification.
    Handles action on success notification as follows:
    - Copies text to clipboard
    - Copies image to clipboard
    - Opens downloads folder for multiple files
    
    Bug: 1085069
    Change-Id: Ie26712e0195a261cade102a74037611f11fa5dab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2375250
    Commit-Queue: Himanshu Jaju <himanshujaju@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802213}

--- chrome/browser/nearby_sharing/nearby_notification_manager.h
+++ chrome/browser/nearby_sharing/nearby_notification_manager.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class NearbySharingService;
 class NotificationDisplayService;
 class PrefService;
+class Profile;
 
 // Manages notifications shown for Nearby Share. Only a single notification will
 // be shown as simultaneous connections are not supported. All methods should be
@@ -28,10 +29,18 @@ class NearbyNotificationManager : public TransferUpdateCallback,
   static constexpr base::TimeDelta kOnboardingDismissedTimeout =
       base::TimeDelta::FromMinutes(15);
 
+  enum class SuccessNotificationAction {
+    kNone,
+    kCopyText,
+    kCopyImage,
+    kOpenDownloads,
+  };
+
   NearbyNotificationManager(
       NotificationDisplayService* notification_display_service,
       NearbySharingService* nearby_service,
-      PrefService* pref_service);
+      PrefService* pref_service,
+      Profile* profile);
   ~NearbyNotificationManager() override;
 
   // TransferUpdateCallback:
@@ -91,10 +100,16 @@ class NearbyNotificationManager : public TransferUpdateCallback,
   // another one for a certain time period after this.
   void OnOnboardingDismissed();
 
+  void CloseSuccessNotification();
+
+  void SetOnSuccessClickedForTesting(
+      base::OnceCallback<void(SuccessNotificationAction)> callback);
+
  private:
   NotificationDisplayService* notification_display_service_;
   NearbySharingService* nearby_service_;
   PrefService* pref_service_;
+  Profile* profile_;
 
   // Maps notification ids to notification delegates.
   base::flat_map<std::string, std::unique_ptr<NearbyNotificationDelegate>>
@@ -102,6 +117,9 @@ class NearbyNotificationManager : public TransferUpdateCallback,
 
   // ShareTarget of the current transfer.
   base::Optional<ShareTarget> share_target_;
+
+  base::OnceCallback<void(SuccessNotificationAction)>
+      success_action_test_callback_;
 };
 
 #endif  // CHROME_BROWSER_NEARBY_SHARING_NEARBY_NOTIFICATION_MANAGER_H_
