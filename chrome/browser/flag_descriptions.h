393b5a359811a20d30dad6519eebed903bede189
Dale Curtis <dalecurtis@chromium.org>
2020-08-27 01:33:35 +0000

    Remove expired chrome://flags entry for try-supported-channel-layouts.
    Has been hidden since M76, so time to remove.
    
    R=cassew
    
    Fixed: 1110734
    Change-Id: I4d07728d13b0df2420a58cae21ace36cba7af744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378632
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Will Cassella <cassew@google.com>
    Cr-Commit-Position: refs/heads/master@{#802060}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1374,9 +1374,6 @@ extern const char kTreatUnsafeDownloadsAsActiveDescription[];
 extern const char kTrustTokensName[];
 extern const char kTrustTokensDescription[];
 
-extern const char kTrySupportedChannelLayoutsName[];
-extern const char kTrySupportedChannelLayoutsDescription[];
-
 extern const char kTurnOffStreamingMediaCachingOnBatteryName[];
 extern const char kTurnOffStreamingMediaCachingOnBatteryDescription[];
 
