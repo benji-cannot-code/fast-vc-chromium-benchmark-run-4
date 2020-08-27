68448c13ce09f5d89677c420418aac42dd96b4a3
Michael Crouse <mcrouse@chromium.org>
2020-08-27 20:42:10 +0000

    [LiteVide] Deflake HasHint browsertest.
    The browser test times outs and causes flakes. There are 2 wait events
    that could cause timeouts: blocklist loaded and a histogram check.
    
    This CL uses the updated blocklist loaded provided by the delegate which
    which more clearly marks when the blocklist is loaded.
    
    This CL also shifts the tab closing later to remove potential race of
    the hint check logic with the tab closing. A future change will remove
    the tab closing logic that may improve this further.
    
    Bug: 1122603
    Change-Id: I898096f3350c057a14661163a8b99ac04fedd299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2380153
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802341}

--- components/blocklist/opt_out_blocklist/opt_out_blocklist_delegate.h
+++ components/blocklist/opt_out_blocklist/opt_out_blocklist_delegate.h
@@ -31,7 +31,7 @@ class OptOutBlocklistDelegate {
   virtual void OnUserBlocklistedStatusChange(bool blocklisted) {}
 
   // Notifies |this| the blocklist loaded state changed to |is_loaded|.
-  virtual void OnLoadingStateChanged(bool is_load) {}
+  virtual void OnLoadingStateChanged(bool is_loaded) {}
 
   // Notifies |this| that the blocklist is cleared at |time|.
   virtual void OnBlocklistCleared(base::Time time) {}
