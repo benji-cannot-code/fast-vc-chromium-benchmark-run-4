341d086ec9776fbf089aff4f9fc7ede78415b782
Xing Liu <xingliu@chromium.org>
2020-08-28 20:16:17 +0000

    Download later: Fix an issue that time is not parsed correctly.
    We should use Calendar.HOUR_OF_DAY to parse the time. The Android time
    picker widget gives us HOUR_OF_DAY instead of Calendar.HOUR.
    
    Bug: 1122999
    Change-Id: I7b4de8b9316ed84281e2b8b3aec2fbc0b92c08cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2382294
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802765}

--- chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadTimePickerDialog.java
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadTimePickerDialog.java
@@ -20,11 +20,11 @@ import org.chromium.chrome.browser.download.R;
  */
 class DownloadTimePickerDialog extends TimePickerDialog {
     interface Controller {
-        void onDownloadTimePicked(int hour, int minute);
+        void onDownloadTimePicked(int hourOfDay, int minute);
         void onDownloadTimePickerCanceled();
     }
 
-    private int mHour;
+    private int mHourOfDay;
     private int mMinute;
     private final Controller mController;
 
@@ -32,7 +32,7 @@ class DownloadTimePickerDialog extends TimePickerDialog {
             Context context, @NonNull Controller controller, int hourOfDay, int minute) {
         super(context, R.style.Theme_DownloadDateTimePickerDialog, null, hourOfDay, minute,
                 false /*is24HourView*/);
-        mHour = hourOfDay;
+        mHourOfDay = hourOfDay;
         mMinute = minute;
         mController = controller;
     }
@@ -48,7 +48,7 @@ class DownloadTimePickerDialog extends TimePickerDialog {
         assert button != null;
         button.setText(R.string.download_date_time_picker_next_text);
         button.setOnClickListener((view) -> {
-            mController.onDownloadTimePicked(mHour, mMinute);
+            mController.onDownloadTimePicked(mHourOfDay, mMinute);
             dismiss();
         });
 
@@ -62,7 +62,7 @@ class DownloadTimePickerDialog extends TimePickerDialog {
 
     @Override
     public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-        mHour = hourOfDay;
+        mHourOfDay = hourOfDay;
         mMinute = minute;
     }
 }
