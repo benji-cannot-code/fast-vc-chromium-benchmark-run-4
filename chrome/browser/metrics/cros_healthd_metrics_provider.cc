20638f0922e99f9e15ba09e99102ec8cea3213b4
Daniil Lunev <dlunev@chromium.org>
2020-08-28 01:05:57 +0000

    Feature-gate cros_healthd_metrics_provider.
    Bug: b:151176984
    Change-Id: I7ca4d9d222ba7db6f6f13a85a7e86daba3fd522b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379383
    Commit-Queue: Daniil Lunev <dlunev@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802463}

--- chrome/browser/metrics/cros_healthd_metrics_provider.cc
+++ chrome/browser/metrics/cros_healthd_metrics_provider.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
+#include "chrome/common/chrome_features.h"
 #include "chromeos/services/cros_healthd/public/cpp/service_connection.h"
 #include "chromeos/services/cros_healthd/public/mojom/cros_healthd.mojom.h"
 #include "chromeos/services/cros_healthd/public/mojom/cros_healthd_probe.mojom.h"
@@ -43,6 +44,12 @@ void CrosHealthdMetricsProvider::AsyncInit(base::OnceClosure done_callback) {
   DCHECK(init_callback_.is_null());
   init_callback_ = std::move(done_callback);
   initialized_ = false;
+
+  if (!base::FeatureList::IsEnabled(::features::kUmaStorageDimensions)) {
+    DVLOG(1) << "cros_healthd metrics provider is not enabled";
+    std::move(init_callback_).Run();
+    return;
+  }
 
   base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE,
@@ -178,6 +185,8 @@ void CrosHealthdMetricsProvider::OnDisconnect() {
 
 void CrosHealthdMetricsProvider::ProvideSystemProfileMetrics(
     metrics::SystemProfileProto* system_profile_proto) {
+  if (!initialized_)
+    return;
   auto* mutable_hardware_proto = system_profile_proto->mutable_hardware();
   mutable_hardware_proto->clear_internal_storage_devices();
 
--- chrome/browser/metrics/cros_healthd_metrics_provider_unittest.cc
+++ chrome/browser/metrics/cros_healthd_metrics_provider_unittest.cc
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/test/bind_test_util.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
+#include "chrome/common/chrome_features.h"
 #include "chromeos/dbus/cros_healthd/cros_healthd_client.h"
 #include "chromeos/dbus/cros_healthd/fake_cros_healthd_client.h"
 #include "chromeos/services/cros_healthd/public/cpp/service_connection.h"
@@ -23,6 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class CrosHealthdMetricsProviderTest : public testing::Test {
  public:
+  CrosHealthdMetricsProviderTest() {
+    scoped_feature_list_.InitAndEnableFeature(features::kUmaStorageDimensions);
+  }
+
   void SetUp() override { chromeos::CrosHealthdClient::InitializeFake(); }
 
   void TearDown() override {
@@ -35,6 +41,9 @@ class CrosHealthdMetricsProviderTest : public testing::Test {
 
   base::test::TaskEnvironment task_environment_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
+
+ protected:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 TEST_F(CrosHealthdMetricsProviderTest, EndToEnd) {
@@ -121,6 +130,20 @@ TEST_F(CrosHealthdMetricsProviderTest, EndToEndTimeout) {
 
   // FastForward by timeout period.
   task_environment_.FastForwardBy(CrosHealthdMetricsProvider::GetTimeout());
+  run_loop.Run();
+  ASSERT_FALSE(provider.IsInitialized());
+}
+
+TEST_F(CrosHealthdMetricsProviderTest, EndToEndNoFeature) {
+  scoped_feature_list_.Reset();
+  scoped_feature_list_.Init();
+
+  base::RunLoop run_loop;
+  CrosHealthdMetricsProvider provider;
+  provider.AsyncInit(base::BindOnce(
+      [](base::OnceClosure callback) { std::move(callback).Run(); },
+      run_loop.QuitClosure()));
+
   run_loop.Run();
   ASSERT_FALSE(provider.IsInitialized());
 }
