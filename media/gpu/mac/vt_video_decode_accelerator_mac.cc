b7d8b5dfffcac5992401139bf5793142a8a5639d
Jonah Ryan-Davis <jonahr@google.com>
2020-08-28 21:38:50 +0000

    Pass along correct texture type for video textures on Mac/ANGLE.
    Historically, CGL on Mac always requests TEXTURE_RECTANGLE for GL
    textures. However, ANGLE's new SwiftShader and Metal backends request
    TEXTURE_2D to be used. This needs to be forwarded to the video
    decode accelerator on Mac.
    
    Bug: chromium:1122647
    Bug: angleproject:4846
    Bug: chromium:1099978
    Bug: chromium:1099979
    Bug: chromium:1119939
    Change-Id: Ic9696046eda29c4b86ce09d7b36c3c81eddae828
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2380097
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Jonah Ryan-Davis <jonahr@google.com>
    Cr-Commit-Position: refs/heads/master@{#802808}

--- media/gpu/mac/vt_video_decode_accelerator_mac.cc
+++ media/gpu/mac/vt_video_decode_accelerator_mac.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/process_memory_dump.h"
 #include "base/version.h"
 #include "components/crash/core/common/crash_key.h"
+#include "gpu/command_buffer/common/gpu_memory_buffer_support.h"
 #include "media/base/limits.h"
 #include "media/base/media_switches.h"
 #include "media/filters/vp9_parser.h"
@@ -1261,7 +1262,8 @@ void VTVideoDecodeAccelerator::ReusePictureBuffer(int32_t picture_id) {
   // Drop references to allow the underlying buffer to be released.
   PictureInfo* picture_info = it->second.get();
   gl_client_.bind_image.Run(picture_info->client_texture_id,
-                            GL_TEXTURE_RECTANGLE_ARB, nullptr, false);
+                            gpu::GetPlatformSpecificTextureTarget(), nullptr,
+                            false);
   picture_info->gl_image = nullptr;
   picture_info->bitstream_id = 0;
 
@@ -1456,7 +1458,7 @@ bool VTVideoDecodeAccelerator::ProcessFrame(const Frame& frame) {
                << frame.image_size.ToString() << ")";
       client_->ProvidePictureBuffers(kNumPictureBuffers, PIXEL_FORMAT_UNKNOWN,
                                      1, frame.image_size,
-                                     GL_TEXTURE_RECTANGLE_ARB);
+                                     gpu::GetPlatformSpecificTextureTarget());
       return false;
     }
     if (!SendFrame(frame))
@@ -1497,7 +1499,8 @@ bool VTVideoDecodeAccelerator::SendFrame(const Frame& frame) {
   gl_image->SetColorSpaceForYUVToRGBConversion(color_space);
 
   if (!gl_client_.bind_image.Run(picture_info->client_texture_id,
-                                 GL_TEXTURE_RECTANGLE_ARB, gl_image, false)) {
+                                 gpu::GetPlatformSpecificTextureTarget(),
+                                 gl_image, false)) {
     DLOG(ERROR) << "Failed to bind image";
     NotifyError(PLATFORM_FAILURE, SFT_PLATFORM_ERROR);
     return false;
