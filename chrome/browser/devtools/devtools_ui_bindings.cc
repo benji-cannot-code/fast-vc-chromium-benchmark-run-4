2704364950c555bfc641163e8311928615c3d5c8
Christy Chen <chrche@microsoft.com>
2020-08-28 10:15:15 +0000

    DevTools: Plumbing host locale to DevTools frontend
    Sending browser backend locale in a query parameter to the frontend. We will determine the DevTools locale based on this.
    See 'Loading a locale' section in Loc design doc for more details:
    https://docs.google.com/document/d/1L6TkT2-42MMQ72ZSBMFwUaq7M6mDgA2X0x8oHHKaV_U/edit?ts=5d945064#heading=h.bd504lfafn49
    
    Bug: 941561
    Change-Id: I68c7b00eac21ea17b6bebd79cfc8ab46ab1eb24f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278846
    Commit-Queue: Christy Chen <chrche@microsoft.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Vidal Diazleal <vidorteg@microsoft.com>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802600}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "build/lacros_buildflags.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/devtools/devtools_file_watcher.h"
 #include "chrome/browser/devtools/devtools_window.h"
@@ -673,6 +674,8 @@ DevToolsUIBindings::DevToolsUIBindings(content::WebContents* web_contents)
   g_devtools_ui_bindings_instances.Get().push_back(this);
   frontend_contents_observer_.reset(new FrontendWebContentsObserver(this));
   web_contents_->GetMutableRendererPrefs()->can_accept_load_drops = false;
+  web_contents_->GetMutableRendererPrefs()->accept_languages =
+      g_browser_process->GetApplicationLocale();
 
   file_helper_.reset(new DevToolsFileHelper(web_contents_, profile_, this));
   file_system_indexer_ = new DevToolsFileSystemIndexer();
