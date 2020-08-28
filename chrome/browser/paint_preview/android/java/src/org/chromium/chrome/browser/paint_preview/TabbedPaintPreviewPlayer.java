0e7cddca9296c1baf2ff6c47148eaf358adcf80f
ckitagawa <ckitagawa@chromium.org>
2020-08-28 18:07:50 +0000

    [Paint Preview] Avoid showing a preview if already loaded
    This CL stops a preview from being shown if the page is already loaded.
    This can happen if the utility process is very slow to initialize.
    
    Change-Id: I7a0d3ff140c9efc47551c63b483351593ac4e25a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2381952
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802716}

--- chrome/browser/paint_preview/android/java/src/org/chromium/chrome/browser/paint_preview/TabbedPaintPreviewPlayer.java
+++ chrome/browser/paint_preview/android/java/src/org/chromium/chrome/browser/paint_preview/TabbedPaintPreviewPlayer.java
@@ -51,6 +51,7 @@ public class TabbedPaintPreviewPlayer implements TabViewProvider, UserData {
     private Runnable mOnDismissed;
     private Boolean mInitializing;
     private boolean mHasUserInteraction;
+    private boolean mFirstMeaningfulPaintHappened;
     private TabbedPaintPreviewObserver mObserver;
     private long mLastShownSnackBarTime;
     private boolean mDidStartRestore;
@@ -129,6 +130,7 @@ public class TabbedPaintPreviewPlayer implements TabViewProvider, UserData {
 
         if (mTab.getWebContents() != webContents) return;
 
+        mFirstMeaningfulPaintHappened = true;
         mObserver.onFirstMeaningfulPaint();
     }
 
@@ -152,12 +154,17 @@ public class TabbedPaintPreviewPlayer implements TabViewProvider, UserData {
         mInitializing = hasCapture;
         mMetricsHelper.recordHadCapture(hasCapture);
         if (!hasCapture) return false;
+        mFirstMeaningfulPaintHappened = false;
 
         mPlayerManager = new PlayerManager(mTab.getUrl(), mTab.getContext(),
                 mPaintPreviewTabService, String.valueOf(mTab.getId()), this::onLinkClicked,
                 () -> removePaintPreview(ExitCause.PULL_TO_REFRESH),
                 () -> {
                     mInitializing = false;
+                    if (mFirstMeaningfulPaintHappened) {
+                        removePaintPreview(ExitCause.TAB_FINISHED_LOADING);
+                        return;
+                    }
                     onShown.run();
                     mMetricsHelper.onShown();
                 },
