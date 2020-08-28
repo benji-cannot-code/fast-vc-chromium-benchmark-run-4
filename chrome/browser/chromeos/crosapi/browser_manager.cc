b422fb755eeda84fe489fac1a743c6c98c1ca113
Yuta Hijikata <ythjkt@google.com>
2020-08-28 08:53:21 +0000

    Lacros: Create user dir before log file creationl.
    Debug edge case where CreateLogFile fails due to user dir not existing
    beforehand.
    
    Related Patch: crrev.com/c/2371443
    Test: Observed behaviour during launch in 1. no user dir. 2. user dir exists but no log file. 3. both exists.
    
    Bug: 1121031
    Change-Id: I45631fb21be2e1ef9680f687c571e894750d08dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2381214
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802586}

--- chrome/browser/chromeos/crosapi/browser_manager.cc
+++ chrome/browser/chromeos/crosapi/browser_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/environment.h"
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
@@ -67,10 +68,21 @@ base::ScopedFD CreateLogFile() {
       PLOG(ERROR) << "Failed to unlink the log file " << log_path;
       return base::ScopedFD();
     }
+
+    // If log file does not exist, most likely the user directory does not exist
+    // either. So create it here.
+    base::File::Error error;
+    if (!base::CreateDirectoryAndGetError(browser_util::GetUserDataDir(),
+                                          &error)) {
+      LOG(ERROR) << "Failed to make directory "
+                 << browser_util::GetUserDataDir()
+                 << base::File::ErrorToString(error);
+      return base::ScopedFD();
+    }
   }
 
   int fd =
-      HANDLE_EINTR(open(log_path.c_str(), O_WRONLY | O_CREAT | O_EXCL, 644));
+      HANDLE_EINTR(open(log_path.c_str(), O_WRONLY | O_CREAT | O_EXCL, 0644));
 
   if (fd < 0) {
     PLOG(ERROR) << "Failed to get file descriptor for " << log_path;
