77598b484a9d53d235b1f2963cdcf4f8d29d6589
Tibor Goldschwendt <tiborg@chromium.org>
2020-08-28 18:37:15 +0000

    [webui][ntp] Move GN targets close to source files (reland)
    This is more consistent with the GN targets of other WebUIs.
    
    + Move module_wrapper.* to modules/.
    
    This is a reland of e2431d7562c98e40b3895a1a9b349ca68432aa23. Downstream
    has been fixed to work with this CL. Original change's info:
    > Change-Id: I23be97df455e8cb0a9ed35b15b184902b3fa1a9c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2376529
    > Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    > Reviewed-by: dpapad <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#802008}
    
    TBR=dpapad@chromium.org
    
    Change-Id: I6db4ed44fc6bb6fc9e85a9a5e671238b23df009a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2382212
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802726}

--- chrome/test/data/webui/new_tab_page/modules/module_wrapper_test.js
+++ chrome/test/data/webui/new_tab_page/modules/module_wrapper_test.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {$$} from 'chrome://new-tab-page/new_tab_page.js';
 
-suite('NewTabPageModuleWrapperTest', () => {
+suite('NewTabPageModulesModuleWrapperTest', () => {
   /** @type {!ModuleWrapperElement} */
   let moduleWrapper;
 
