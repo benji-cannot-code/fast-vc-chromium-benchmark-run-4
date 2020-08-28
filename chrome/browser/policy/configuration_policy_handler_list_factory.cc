fba55ce18319442b68b99faef495acec2f7a3836
Yann Dago <ydago@chromium.org>
2020-08-28 21:30:48 +0000

    Deprecate QuickUnlockModeWhitelist in favor of QuickUnlockModeAllowlist
    Bug: 1103814
    Change-Id: I75ba3682f6bbff424b26269a9bb65ca479b8be31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2372810
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802800}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1094,9 +1094,6 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kAllowScreenLock,
     ash::prefs::kAllowScreenLock,
     base::Value::Type::BOOLEAN },
-  { key::kQuickUnlockModeWhitelist,
-    prefs::kQuickUnlockModeWhitelist,
-    base::Value::Type::LIST },
   { key::kQuickUnlockTimeout,
     prefs::kQuickUnlockTimeout,
     base::Value::Type::INTEGER },
@@ -1669,6 +1666,15 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
   handlers->AddHandler(std::make_unique<extensions::ExtensionListPolicyHandler>(
       key::kAttestationExtensionWhitelist,
       prefs::kAttestationExtensionWhitelist, false));
+
+  handlers->AddHandler(std::make_unique<SimpleDeprecatingPolicyHandler>(
+      std::make_unique<SimplePolicyHandler>(key::kQuickUnlockModeAllowlist,
+                                            prefs::kQuickUnlockModeAllowlist,
+                                            base::Value::Type::LIST),
+      std::make_unique<SimplePolicyHandler>(key::kQuickUnlockModeWhitelist,
+                                            prefs::kQuickUnlockModeAllowlist,
+                                            base::Value::Type::LIST)));
+
   handlers->AddHandler(base::WrapUnique(
       NetworkConfigurationPolicyHandler::CreateForDevicePolicy()));
   handlers->AddHandler(base::WrapUnique(
