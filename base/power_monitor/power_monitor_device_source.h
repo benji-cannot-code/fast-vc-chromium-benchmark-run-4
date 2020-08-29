48ec7b0ee1daa72cb79d3e611feee8006df61663
Puthikorn Voravootivat <puthik@google.com>
2020-08-29 04:41:15 +0000

    chromeos/dbus: base/power_monitor: Add thermal hinting api
    Add proto for power manager to provide thermal
    hinting to Chrome.
    
    Bug: 1081959
    Test: See ThermalEvent in chrome://device-log
    chromeos_unittest --gtest_filter=PowerManagerClientTest.ChangeThermalState
    
    Change-Id: Ie7435852da01a5acf3580b5bb569ba117113f658
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2300983
    Commit-Queue: Puthikorn Voravootivat <puthik@chromium.org>
    Auto-Submit: Puthikorn Voravootivat <puthik@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802880}

--- base/power_monitor/power_monitor_device_source.h
+++ base/power_monitor/power_monitor_device_source.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_POWER_MONITOR_POWER_MONITOR_DEVICE_SOURCE_H_
 #define BASE_POWER_MONITOR_POWER_MONITOR_DEVICE_SOURCE_H_
 
+#include <memory>
+#include <vector>
+
 #include "base/base_export.h"
 #include "base/macros.h"
 #include "base/power_monitor/power_monitor_source.h"
@@ -45,6 +48,7 @@ class BASE_EXPORT PowerMonitorDeviceSource : public PowerMonitorSource {
   static void SetPowerSource(bool on_battery);
   static void HandleSystemSuspending();
   static void HandleSystemResumed();
+  static void ThermalEventReceived(PowerObserver::DeviceThermalState state);
 #endif
 
  private:
--- base/power_monitor/power_monitor_device_source_chromeos.cc
+++ base/power_monitor/power_monitor_device_source_chromeos.cc
@@ -38,4 +38,10 @@ bool PowerMonitorDeviceSource::IsOnBatteryPowerImpl() {
   return g_on_battery;
 }
 
+// static
+void PowerMonitorDeviceSource::ThermalEventReceived(
+    PowerObserver::DeviceThermalState state) {
+  ProcessThermalEvent(state);
+}
+
 }  // namespace base
