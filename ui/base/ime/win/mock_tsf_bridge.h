e3a422deb2a7ba7c833af5d57c35471b3135a0a1
Tomasz Moniuszko <tmoniuszko@opera.com>
2020-08-31 18:28:43 +0000

    Avoid duplicated MockTSFBridge symbols
    mock_tsf_bridge.cc was compiled as a part of //ui/base:test_support and
    //ui/base/ime/win:win. This caused link errors in some configurations as
    MockTSFBridge symbols were duplicated. //ui/base:test_support depends on
    //ui/base/ime/win:win so mock_tsf_bridge.cc is redundant there.
    
    Bug: 968222
    Change-Id: Ibc159c336c0feef285e1066d3dc73c57c62b981c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379764
    Commit-Queue: Yohei Yukawa <yukawa@chromium.org>
    Auto-Submit: Tomasz Moniuszko <tmoniuszko@opera.com>
    Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Anupam Snigdha <snianu@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#803186}

--- ui/base/ime/win/mock_tsf_bridge.h
+++ ui/base/ime/win/mock_tsf_bridge.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-class MockTSFBridge : public TSFBridge {
+class COMPONENT_EXPORT(UI_BASE_IME_WIN) MockTSFBridge : public TSFBridge {
  public:
   MockTSFBridge();
   ~MockTSFBridge() override;
