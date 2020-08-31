024ba3b983fd95f4a082d773d4bf83a826688ef8
Minoru Chikamune <chikamune@chromium.org>
2020-08-31 22:35:17 +0000

    [HeapMojo] Make HeapMojoWrapperMode::kWithContextObserver default
    Recent experiment shows that there is no huge impact on crash rates. So this CL is going to change the default mode to use HeapMojoWrapperMode::kWithContextObserver.
    
    Bug: 1049056
    Change-Id: I801a57fdacc6195b95aef9d9fa6f1a896595f9b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2383406
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803272}

--- third_party/blink/renderer/platform/mojo/features.cc
+++ third_party/blink/renderer/platform/mojo/features.cc
@@ -8,5 +8,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 // HeapMojo experiments to deprecate kWithoutContextObserver
 const base::Feature kHeapMojoUseContextObserver{
-    "HeapMojoUseContextObserver", base::FEATURE_DISABLED_BY_DEFAULT};
+    "HeapMojoUseContextObserver", base::FEATURE_ENABLED_BY_DEFAULT};
 }  // namespace blink
