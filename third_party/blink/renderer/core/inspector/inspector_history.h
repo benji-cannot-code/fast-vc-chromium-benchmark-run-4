4e9b2dbbb6bc5722f382343e10434e62f84910d4
Peter Marshall <petermarshall@chromium.org>
2020-08-31 13:33:25 +0000

    devtools: Add unit tests for InspectorHistory
    Change-Id: I06732b384f760a235f42078899e54c80123d4e4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2369312
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803093}

--- third_party/blink/renderer/core/inspector/inspector_history.h
+++ third_party/blink/renderer/core/inspector/inspector_history.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -42,9 +43,10 @@ namespace blink {
 
 class ExceptionState;
 
-class InspectorHistory final : public GarbageCollected<InspectorHistory> {
+class CORE_EXPORT InspectorHistory final
+    : public GarbageCollected<InspectorHistory> {
  public:
-  class Action : public GarbageCollected<Action> {
+  class CORE_EXPORT Action : public GarbageCollected<Action> {
    public:
     explicit Action(const String& name);
     virtual ~Action();
