8b783f9ac5605209cb9059ff82f0ae9ae25ed098
Peter Wen <wnwen@chromium.org>
2020-08-31 18:43:12 +0000

    Android: Remove unused suppressions and fix some
    Remove test-specific suppressions. If and when we enable lint on test
    targets again, blanket testonly suppressions should be added to lint.py
    instead.
    
    Fix some WrongConstant issues. These are useful to understand and make
    sure we are passing the right @IntDef and @StringDef variables to
    android APIs.
    
    Bug: 1116130
    Change-Id: I43bea517bca2436f2063829fe74d280b5df6bca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2375700
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Dan McArdle <dmcardle@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803198}

--- media/base/android/java/src/org/chromium/media/MediaCodecEncoder.java
+++ media/base/android/java/src/org/chromium/media/MediaCodecEncoder.java
@@ -49,7 +49,8 @@ class MediaCodecEncoder extends MediaCodecBridge {
     }
 
     // Save the config frame(SPS/PPS NALs) and append it to each keyframe.
-    @SuppressWarnings("deprecation")
+    // WrongConstant: MediaCodec#dequeueOutputBuffer returns the index when successful.
+    @SuppressWarnings({"deprecation", "WrongConstant"})
     @Override
     protected int dequeueOutputBufferInternal(MediaCodec.BufferInfo info, long timeoutUs) {
         int indexOrStatus = -1;
