fb97737780b20f524a97f95bb342ec1de68ce08d
ckitagawa <ckitagawa@chromium.org>
2020-08-31 19:16:12 +0000

    [Paint Preview] Only capture on activity hidden
    We are seeing some possibly statistically significant memory increases
    with the show on startup experiment. One avenue to optimize is to only
    capture when the tab is hidden with the activity (may avoid capturing
    renderer staying around too long).
    
    Pros:
    - Probable reduction in memory usage
    - Fewer captures/disk-usage
    
    Cons:
    - Possibly there are scenarios where the user was on the tab switcher
      or in another state where we won't have hidden the tab with the
      activity meaning fewer cases where we have a capture to show.
    - We need to re-add this behavior for the tab-switching experiment
      where we need to capture on all hides anyway.
    
    Bug: 1123523
    Change-Id: Icbb5e71b6255d32670dea558e63e4aba00a77506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2382224
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803216}

--- chrome/android/javatests/src/org/chromium/chrome/browser/paint_preview/services/PaintPreviewTabServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/paint_preview/services/PaintPreviewTabServiceTest.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.paint_preview.services;
 
+import android.app.Activity;
+import android.support.test.InstrumentationRegistry;
+
 import androidx.test.filters.MediumTest;
 
 import org.junit.Assert;
@@ -14,7 +17,6 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.ScalableTimeout;
@@ -61,7 +63,61 @@ public class PaintPreviewTabServiceTest {
     }
 
     /**
-     * Verifies that a Tab's contents are captured when the page is loaded and subsequently deleted
+     * Verifies that a Tab's contents are captured when the activity is stopped.
+     */
+    @Test
+    @MediumTest
+    @Feature({"PaintPreview"})
+    public void testCapturedOnStopped() throws Exception {
+        EmbeddedTestServer testServer = mActivityTestRule.getTestServer();
+        final String url = testServer.getURL("/chrome/test/data/android/about.html");
+
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            mPaintPreviewTabService = PaintPreviewTabServiceFactory.getServiceInstance();
+            mPaintPreviewTabService.onRestoreCompleted(mTabModelSelector, true, false);
+            mTab.loadUrl(new LoadUrlParams(url));
+        });
+        // Give the tab time to complete layout before hiding.
+        TimeUnit.SECONDS.sleep(1);
+        int tabId = mTab.getId();
+
+        // Simulate closing the app.
+        Activity activity = mActivityTestRule.getActivity();
+        activity.getWindow().setLocalFocus(false, false);
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            InstrumentationRegistry.getInstrumentation().callActivityOnPause(activity);
+        });
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            InstrumentationRegistry.getInstrumentation().callActivityOnStop(activity);
+        });
+
+        // Allow time to capture.
+        CriteriaHelper.pollUiThread(() -> {
+            mPaintPreviewTabService = PaintPreviewTabServiceFactory.getServiceInstance();
+            return mPaintPreviewTabService.hasCaptureForTab(tabId);
+        }, "Paint Preview didn't get captured.", TIMEOUT_MS, POLLING_INTERVAL_MS);
+
+        // Simulate unpausing the app (for cleanup).
+        activity.getWindow().setLocalFocus(true, true);
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            InstrumentationRegistry.getInstrumentation().callActivityOnRestart(activity);
+        });
+
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            mTabModelSelector = mActivityTestRule.getActivity().getTabModelSelector();
+            mTab = mTabModelSelector.getTabById(tabId);
+            mTabModel = mTabModelSelector.getModel(/*incognito*/ false);
+        });
+
+        TestThreadUtils.runOnUiThreadBlocking(() -> { mTabModel.closeTab(mTab); });
+
+        CriteriaHelper.pollUiThread(() -> {
+            return !mPaintPreviewTabService.hasCaptureForTab(tabId);
+        }, "Paint Preview didn't get deleted.", TIMEOUT_MS, POLLING_INTERVAL_MS);
+    }
+
+    /**
+     * Verifies that a Tab's contents are captured when the tab is switched and subsequently deleted
      * when the tab is closed.
      */
     @Test
@@ -73,7 +129,8 @@ public class PaintPreviewTabServiceTest {
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             mPaintPreviewTabService = PaintPreviewTabServiceFactory.getServiceInstance();
-            mPaintPreviewTabService.onRestoreCompleted(mTabModelSelector, true);
+            // Use capture on switch mode.
+            mPaintPreviewTabService.onRestoreCompleted(mTabModelSelector, true, true);
             mTab.loadUrl(new LoadUrlParams(url));
         });
         // Give the tab time to complete layout before hiding.
@@ -87,8 +144,6 @@ public class PaintPreviewTabServiceTest {
             return mPaintPreviewTabService.hasCaptureForTab(tabId);
         }, "Paint Preview didn't get captured.", TIMEOUT_MS, POLLING_INTERVAL_MS);
 
-        CallbackHelper callbackHelper = new CallbackHelper();
-        int callCount = callbackHelper.getCallCount();
         TestThreadUtils.runOnUiThreadBlocking(() -> { mTabModel.closeTab(mTab); });
 
         CriteriaHelper.pollUiThread(() -> {
