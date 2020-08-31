eec0e8c3ef64ad07489a9e9622af03e14cf82510
My Nguyen <myy@chromium.org>
2020-08-31 01:10:30 +0000

    [OsSettingsLanguages] Add Restart for ChangeDeviceLanguageDialog
    Make the button actually confirm and restart.
    Spec: http://go/cros-lang-settings-ux-slide#slide=15
    
    Bug: 1113439
    Change-Id: I4d728f8d2e237ceb15139493fadeaf4b7f0b83f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379405
    Commit-Queue: My Nguyen <myy@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802992}

--- chrome/test/data/webui/settings/chromeos/os_languages_page_v2_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_languages_page_v2_tests.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // clang-format off
-// #import {LanguagesBrowserProxyImpl, LanguagesMetricsProxyImpl, LanguagesPageInteraction} from 'chrome://os-settings/chromeos/lazy_load.js';
+// #import {LanguagesBrowserProxyImpl, LanguagesMetricsProxyImpl, LanguagesPageInteraction, LifetimeBrowserProxyImpl} from 'chrome://os-settings/chromeos/lazy_load.js';
 // #import {CrSettingsPrefs, Router} from 'chrome://os-settings/chromeos/os_settings.js';
 // #import {assert} from 'chrome://resources/js/assert.m.js';
 // #import {keyDownOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {FakeSettingsPrivate} from '../fake_settings_private.m.js';
 // #import {TestLanguagesBrowserProxy} from './test_os_languages_browser_proxy.m.js';
 // #import {TestLanguagesMetricsProxy} from './test_os_languages_metrics_proxy.m.js';
+// #import {TestLifetimeBrowserProxy} from './test_os_lifetime_browser_proxy.m.js';
 // #import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
 // #import {fakeDataBind} from '../../test_util.m.js';
 // clang-format on
@@ -28,6 +29,8 @@ suite('languages page', () => {
   let actionMenu;
   /** @type {!settings.LanguagesBrowserProxy} */
   let browserProxy;
+  /** @type {!settings.TestLifetimeBrowserProxy} */
+  let lifetimeProxy;
   /** @type {!settings.LanguagesMetricsProxy} */
   let metricsProxy;
 
@@ -54,6 +57,9 @@ suite('languages page', () => {
     // Sets up test browser proxy.
     browserProxy = new settings.TestLanguagesBrowserProxy();
     settings.LanguagesBrowserProxyImpl.instance_ = browserProxy;
+
+    lifetimeProxy = new settings.TestLifetimeBrowserProxy();
+    settings.LifetimeBrowserProxyImpl.instance_ = lifetimeProxy;
 
     // Sets up test metrics proxy.
     metricsProxy = new settings.TestLanguagesMetricsProxy();
@@ -256,7 +262,7 @@ suite('languages page', () => {
       assertTrue(actionButton.disabled);
     });
 
-    test('sets device language', async () => {
+    test('sets device language and restarts device', async () => {
       // selects a language
       dialogItems[0].click();  // en-CA
       assertFalse(actionButton.disabled);
@@ -264,6 +270,10 @@ suite('languages page', () => {
       actionButton.click();
       assertEquals(
           'en-CA', await browserProxy.whenCalled('setProspectiveUILanguage'));
+      assertEquals(
+          settings.LanguagesPageInteraction.RESTART,
+          await metricsProxy.whenCalled('recordInteraction'));
+      await lifetimeProxy.whenCalled('signOutAndRestart');
     });
 
     // Test that searching languages works whether the displayed or native
