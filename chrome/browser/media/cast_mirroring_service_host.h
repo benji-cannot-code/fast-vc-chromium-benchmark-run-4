f63b413d5f4134e6fd3b84727d216ee981828bd2
mark a. foltz <mfoltz@chromium.org>
2020-08-31 21:14:09 +0000

    [Cast Streaming] Update BUILD rules for Cast mirroring and remoting.
    Mirroring Service and Media Remoting code don't depend on extensions
    any more.  This updates the BUILD file to reflect that.
    
    It also deletes CastTransportHostFilter which is unused.
    
    Bug: 879012,1120230
    Change-Id: Ia2962b352566ac90a198420952be0480f617b4d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367735
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803266}

--- chrome/browser/media/cast_mirroring_service_host.h
+++ chrome/browser/media/cast_mirroring_service_host.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/sequenced_task_runner.h"
+#include "chrome/browser/media/offscreen_tab.h"
 #include "components/mirroring/mojom/mirroring_service.mojom.h"
 #include "components/mirroring/mojom/mirroring_service_host.mojom.h"
 #include "components/mirroring/mojom/resource_provider.mojom.h"
@@ -24,12 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "ui/gfx/geometry/size.h"
-
-// TODO(crbug.com/879012): Remove the build flag. OffscreenTab should not only
-// be defined when extension is enabled.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "chrome/browser/media/offscreen_tab.h"
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 namespace content {
 class AudioLoopbackStreamCreator;
@@ -48,9 +43,7 @@ namespace mirroring {
 // Service, and provides the resources to the Mirroring Service as requested.
 class CastMirroringServiceHost final : public mojom::MirroringServiceHost,
                                        public mojom::ResourceProvider,
-#if BUILDFLAG(ENABLE_EXTENSIONS)
                                        public OffscreenTab::Owner,
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
                                        public content::WebContentsObserver {
  public:
   static void GetForTab(
