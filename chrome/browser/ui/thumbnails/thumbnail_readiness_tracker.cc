6f2cde0463bee00467e0df534fbcb16e57254334
Collin Baker <collinbaker@chromium.org>
2020-08-31 23:22:23 +0000

    Handle same-document navigations in ThumbnailReadinessTracker
    Same-document navigations (such as history.pushState() calls) cause
    associated DidStartNavigation() and DidFinishNavigation()
    calls. However, since this isn't considered a load,
    DocumentOnLoadCompletedInMainFrame() is sensibly not called.
    
    ThumbnailReadinessTracker assumed DidStartNavigation eventually lead
    to a DocumentOnLoadCompletedInMainFrame call (or an error). This meant
    that pages using history.pushState(), such as YouTube, had
    never-ending thumbnail capture.
    
    This CL fixes the never-ending capture.
    
    Bug: 1120940
    Change-Id: Ia298d384b44bcc2b860260826952898972daf480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2382426
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803294}

--- chrome/browser/ui/thumbnails/thumbnail_readiness_tracker.cc
+++ chrome/browser/ui/thumbnails/thumbnail_readiness_tracker.cc
@@ -9,6 +9,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/navigation_handle.h"
 
+namespace {
+
+bool NavigationShouldInvalidateThumbnail(
+    content::NavigationHandle* navigation) {
+  // Ignore subframe navigations.
+  if (!navigation->IsInMainFrame())
+    return false;
+
+  // Some navigations change the tab URL but don't create a new
+  // document. They aren't considered loading; onload is never triggered
+  // after they complete. They shouldn't affect the thumbnail.
+  //
+  // See crbug.com/1120940 for why this is necessary.
+  if (navigation->IsSameDocument())
+    return false;
+
+  return true;
+}
+
+}  // namespace
+
 ThumbnailReadinessTracker::ThumbnailReadinessTracker(
     content::WebContents* web_contents,
     ReadinessChangeCallback callback)
@@ -21,15 +42,23 @@ ThumbnailReadinessTracker::~ThumbnailReadinessTracker() = default;
 
 void ThumbnailReadinessTracker::DidStartNavigation(
     content::NavigationHandle* navigation_handle) {
-  if (!navigation_handle->IsInMainFrame())
+  if (!NavigationShouldInvalidateThumbnail(navigation_handle))
     return;
+
+  pending_navigation_ = navigation_handle;
   UpdateReadiness(Readiness::kNotReady);
 }
 
 void ThumbnailReadinessTracker::DidFinishNavigation(
     content::NavigationHandle* navigation_handle) {
-  if (!navigation_handle->IsInMainFrame())
+  // Only respond to the last navigation handled by DidStartNavigation.
+  // Others may be unimportant or outdated.
+  if (navigation_handle != pending_navigation_)
     return;
+
+  pending_navigation_ = nullptr;
+  UpdateReadiness(Readiness::kReadyForInitialCapture);
+
   if (last_readiness_ > Readiness::kReadyForInitialCapture)
     return;
   UpdateReadiness(Readiness::kReadyForInitialCapture);
@@ -40,6 +69,7 @@ void ThumbnailReadinessTracker::DocumentOnLoadCompletedInMainFrame() {
 }
 
 void ThumbnailReadinessTracker::WebContentsDestroyed() {
+  pending_navigation_ = nullptr;
   UpdateReadiness(Readiness::kNotReady);
 }
 
