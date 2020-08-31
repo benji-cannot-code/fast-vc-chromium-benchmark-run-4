bfcf56cf09c3eeaa65b8838d3605a1d8a762a42d
Fredrik Söderqvist <fs@opera.com>
2020-08-31 08:58:26 +0000

    Simplify SVGAnimationElement::IsAdditive
    Hoist the check for 'by' animations out of IsAdditive() and leave it to
    only check the 'additive' attribute value.
    The SVGAnimateElement::IsAdditive() override can be removed since the
    additional check for non-additive types is handled during the animation
    value calculation, and non-additive types are rejected while validating
    the animation parameters.
    
    While at it, randomly add some comments for additive handling, and
    add more comments in OverwritesUnderlyingAnimationValue(). Also make
    IsAdditive(), IsAccumulated(), GetAnimationMode() and GetCalcMode()
    non-public as appropriate.
    
    Bug: 1017723
    Change-Id: I0907742ca1e568ba6faee1b08199bccad683a5e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379919
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#803032}

--- third_party/blink/renderer/core/svg/svg_animate_motion_element.cc
+++ third_party/blink/renderer/core/svg/svg_animate_motion_element.cc
@@ -212,6 +212,7 @@ void SVGAnimateMotionElement::CalculateAnimatedValue(float percentage,
   AffineTransform* transform = target_element->AnimateMotionTransform();
   DCHECK(transform);
 
+  // If additive, we accumulate into the underlying (transform) value.
   if (!parameters.is_additive)
     transform->MakeIdentity();
 
