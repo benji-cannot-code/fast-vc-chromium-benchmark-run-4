a582d45ce71a456df6ee2b2efe9393c00387cfbf
Maksim Ivanov <emaxx@chromium.org>
2020-08-31 13:52:52 +0000

    Fix use-after-move in //components/services/paint_preview_compositor/
    Fix use-after-move (potential) bugs found by the
    "bugprone-use-after-move" clang-tidy check.
    
    Bug: 1122844
    Change-Id: Iecad2dc339be6d352c3cf420a50b3f9c58cbe68f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2382594
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803099}

--- components/services/paint_preview_compositor/paint_preview_compositor_impl.cc
+++ components/services/paint_preview_compositor/paint_preview_compositor_impl.cc
@@ -145,6 +145,7 @@ void PaintPreviewCompositorImpl::BeginSeparatedFrameComposite(
     std::move(callback).Run(mojom::PaintPreviewCompositor::
                                 BeginCompositeStatus::kDeserializingFailure,
                             std::move(response));
+    return;
   }
   auto frames = DeserializeAllFrames(std::move(request->recording_map));
 
@@ -217,6 +218,7 @@ void PaintPreviewCompositorImpl::BeginMainFrameComposite(
     DVLOG(1) << "No valid root frame guid";
     std::move(callback).Run(mojom::PaintPreviewCompositor::
                                 BeginCompositeStatus::kDeserializingFailure);
+    return;
   }
 
   base::flat_map<base::UnguessableToken, sk_sp<SkPicture>> loaded_frames;
