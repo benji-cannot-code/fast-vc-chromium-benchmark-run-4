e1735fcef1eb1d07ee9bc3698bdba9fbdac7b301
Darren Shen <shend@chromium.org>
2020-08-31 00:47:40 +0000

    ime: Remove flags for launched feature.
    Native rule-based has already been launched for 2 milestones. Deleting
    the flags code related to it.
    
    The corresponding internal code was deleted in cl/307494837.
    
    Bug: 1034355
    Change-Id: I831816b9dd023c14f2e21017af9b2fa69983c88b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367300
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#802988}

--- chromeos/constants/chromeos_features.h
+++ chromeos/constants/chromeos_features.h
@@ -140,8 +140,6 @@ COMPONENT_EXPORT(CHROMEOS_CONSTANTS) extern const base::Feature kMediaApp;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kMinimumChromeVersion;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
-extern const base::Feature kNativeRuleBasedTyping;
-COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kNewOsSettingsSearch;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kOobeScreensPriority;
