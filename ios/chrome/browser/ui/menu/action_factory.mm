78c38a23ef98d61dac3238b7b85dd1327cbe412a
Ewann <ewannpv@google.com>
2020-08-31 15:33:38 +0000

    [iOS] Adds custom actions to the ReadingList Context Menu
    This CL adds the following actions to the ReadingList Context Menu:
    - Mark As Read
    - Mark As Unread
    - View Offline Version in New Tab
    
    Bug: 1093302
    Change-Id: Ie17e58417738cbc5cc2c3a345c281464339170b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2366799
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803116}

--- ios/chrome/browser/ui/menu/action_factory.mm
+++ ios/chrome/browser/ui/menu/action_factory.mm
@@ -192,4 +192,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 block:block];
 }
 
+- (UIAction*)actionToMarkAsReadWithBlock:(ProceduralBlock)block {
+  return [self actionWithTitle:l10n_util::GetNSString(
+                                   IDS_IOS_READING_LIST_MARK_AS_READ_ACTION)
+                         image:[UIImage imageNamed:@"mark_read"]
+                          type:MenuActionType::Read
+                         block:block];
+}
+
+- (UIAction*)actionToMarkAsUnreadWithBlock:(ProceduralBlock)block {
+  return [self actionWithTitle:l10n_util::GetNSString(
+                                   IDS_IOS_READING_LIST_MARK_AS_UNREAD_ACTION)
+                         image:[UIImage imageNamed:@"remove"]
+                          type:MenuActionType::Unread
+                         block:block];
+}
+
+- (UIAction*)actionToViewOfflineVersionInInNewTabWithBlock:
+    (ProceduralBlock)block {
+  return [self actionWithTitle:l10n_util::GetNSString(
+                                   IDS_IOS_READING_LIST_CONTENT_CONTEXT_OFFLINE)
+                         image:nil
+                          type:MenuActionType::ViewOffline
+                         block:block];
+}
+
 @end
