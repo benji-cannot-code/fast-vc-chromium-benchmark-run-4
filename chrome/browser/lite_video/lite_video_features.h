b5d860a35d110206ff4894e145afc7c3c1871150
Michael Crouse <mcrouse@chromium.org>
2020-08-31 19:58:18 +0000

    [LiteVideo] Add permanent host blocklist.
    Queries to the optimization guide are based only on the mainframe
    host so if a hint for a mainframe is provided, it is applied to
    all relevant subframes. To prevent LiteVideos on certain hosts
    that are 1) frequently embedded subframes and 2) do not
    provide any benefit from LiteVideos, a host blocklist is added.
    Any queries to the decider with a host on this list will
    explicitedly be prevented. The set of hosts are controlled
    via Finch.
    
    Bug: 1117064
    Change-Id: I7debd153c2578efd5d4638cfcaed809d3283adef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2385975
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803235}

--- chrome/browser/lite_video/lite_video_features.h
+++ chrome/browser/lite_video/lite_video_features.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_LITE_VIDEO_LITE_VIDEO_FEATURES_H_
 #define CHROME_BROWSER_LITE_VIDEO_LITE_VIDEO_FEATURES_H_
 
+#include "base/containers/flat_set.h"
 #include "base/feature_list.h"
 #include "base/optional.h"
 #include "net/nqe/effective_connection_type.h"
@@ -65,6 +66,10 @@ net::EffectiveConnectionType MinLiteVideoECT();
 // The maximum number of hints the LiteVideoDecider should cache locally
 // for reuse by subframes.
 int MaxOptimizationGuideHintCacheSize();
+
+// Return the set of hosts that LiteVideos are permanently blocked from
+// being applied on.
+base::flat_set<std::string> GetLiteVideoPermanentBlocklist();
 
 }  // namespace features
 }  // namespace lite_video
