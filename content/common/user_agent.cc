e2e714b88456cc86b9ba5c8d87b4c2f72fa55f17
Avi Drissman <avi@chromium.org>
2020-08-31 21:09:49 +0000

    Implement Sec-CH-UA-Arch for the Mac
    Bug: 1110914
    Change-Id: I5d6375da9a9e9855e5a8b568fcd9914e39fa94f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2382228
    Reviewed-by: Aaron Tagliaboschi <aarontag@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803261}

--- content/common/user_agent.cc
+++ content/common/user_agent.cc
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/lacros_buildflags.h"
 #include "build/util/webkit_version.h"
 
+#if defined(OS_MAC)
+#include "base/mac/mac_util.h"
+#endif
+
 #if defined(OS_WIN)
 #include "base/win/windows_version.h"
 #elif defined(OS_POSIX) && !defined(OS_MAC)
@@ -91,10 +95,27 @@ std::string BuildCpuInfo() {
   return cpuinfo;
 }
 
-// Return the CPU architecture in Linux or Windows and the empty string
+// Return the CPU architecture in Windows/Mac/POSIX and the empty string
 // elsewhere.
 std::string GetLowEntropyCpuArchitecture() {
-#if !defined(OS_MAC) && !defined(OS_ANDROID) && defined(OS_POSIX)
+#if defined(OS_WIN)
+  base::win::OSInfo::WindowsArchitecture windows_architecture =
+      base::win::OSInfo::GetInstance()->GetArchitecture();
+  if (windows_architecture == base::win::OSInfo::ARM64_ARCHITECTURE) {
+    return "arm";
+  } else if ((windows_architecture == base::win::OSInfo::X86_ARCHITECTURE) ||
+             (windows_architecture == base::win::OSInfo::X64_ARCHITECTURE)) {
+    return "x86";
+  }
+#elif defined(OS_MAC)
+  base::mac::CPUType cpu_type = base::mac::GetCPUType();
+  if (cpu_type == base::mac::CPUType::kIntel) {
+    return "x86";
+  } else if (cpu_type == base::mac::CPUType::kArm ||
+             cpu_type == base::mac::CPUType::kTranslatedIntel) {
+    return "arm";
+  }
+#elif defined(OS_POSIX) && !defined(OS_ANDROID)
   // This extra cpu_info_str variable is required to make sure the compiler
   // doesn't optimize the copy away and have the StringPiece point at the
   // internal std::string, resulting in a memory violation.
@@ -106,15 +127,6 @@ std::string GetLowEntropyCpuArchitecture() {
   } else if ((base::StartsWith(cpu_info, "i") &&
               cpu_info.substr(2, 2) == "86") ||
              base::StartsWith(cpu_info, "x86")) {
-    return "x86";
-  }
-#elif defined(OS_WIN)
-  base::win::OSInfo::WindowsArchitecture windows_architecture =
-      base::win::OSInfo::GetInstance()->GetArchitecture();
-  if (windows_architecture == base::win::OSInfo::ARM64_ARCHITECTURE) {
-    return "arm";
-  } else if ((windows_architecture == base::win::OSInfo::X86_ARCHITECTURE) ||
-             (windows_architecture == base::win::OSInfo::X64_ARCHITECTURE)) {
     return "x86";
   }
 #endif
