66db03020d1049e2df8c072925e680675abcc1d9
Nela Kaczmarek <nelakaczmarek@google.com>
2020-08-31 18:08:33 +0000

    [Passwords] Affiliation Service - change SchemeHostPort to GURL type for parameters in public methods.
    Changes type of parameters in public methods of Affiliation Service from SchemeHostPort to GURL. This change makes AffiliationService compliant with the public interface of ChangePasswordUrlService.
    
    Bug: 1108279
    Change-Id: I2b59560f991176ac94ac9a57cc2db5577bd72ab6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379717
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Nela Kaczmarek <nelakaczmarek@google.com>
    Cr-Commit-Position: refs/heads/master@{#803171}

--- components/password_manager/core/browser/site_affiliation/affiliation_service.h
+++ components/password_manager/core/browser/site_affiliation/affiliation_service.h
@@ -12,24 +12,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GURL;
 
-namespace url {
-class SchemeHostPort;
-}
-
 namespace password_manager {
 
 class AffiliationService : public KeyedService {
  public:
   // Prefetches change password URLs for sites requested.
-  virtual void PrefetchChangePasswordURLs(
-      const std::vector<url::SchemeHostPort>& tuple_origins) = 0;
+  virtual void PrefetchChangePasswordURLs(const std::vector<GURL>& urls) = 0;
 
   // Clears the result of URLs fetch.
   virtual void Clear() = 0;
 
   // Returns a URL with change password form for a site requested.
-  virtual GURL GetChangePasswordURL(
-      const url::SchemeHostPort& scheme_host_port) const = 0;
+  virtual GURL GetChangePasswordURL(const GURL& url) const = 0;
 };
 
 }  // namespace password_manager
