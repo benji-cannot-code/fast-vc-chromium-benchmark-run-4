5ecad8da26fe35675890e6d825cd2fd8ca23a188
Michael Bai <michaelbai@chromium.org>
2020-09-01 17:08:55 +0000

    Reland "Make ContentCapture ready for experiment"
    This reverts commit c8026dea507a3ac3ddf8d8f7f385dea5a81f8d11.
    
    Reason for revert: The fix will be in blink side, revert it by disable the feature by default, to make landing fix easily.
    
    Original change's description:
    > Revert "Make ContentCapture ready for experiment"
    > 
    > This reverts commit 564da6d9d812d7e3b53a9d05eac1daf46c72f9a2.
    > 
    > Reason for revert: Very likely the cause of crbug.com/1122472
    > See the analysis in crbug.com/1123677#c4 for detail
    > 
    > Original change's description:
    > > Make ContentCapture ready for experiment
    > > 
    > > Currently the triggering of ContentCapture is unpredictable, it is
    > > hard for us to get the unbiased result for the UserActivatedDelay
    > > experiment.
    > > 
    > > This patch adds an ExperimentContentCaptureConsumer which triggers
    > > the ContentCapture independently and is enabled by default.
    > > 
    > > The allow list check is moved to consumer if the experiment
    > > is enabled.
    > > 
    > > As ContentCapture is potentially used for all Android versions,
    > > we plan to run the experiment for all of them.
    > > 
    > > This mechanism will also be used for future experiments.
    > > 
    > > This patch hasn't supported multiple consumers yet.
    > > 
    > > Test: Added new tests and pass the existing tests
    > > 
    > > Bug: 1114819, 1119663
    > > Change-Id: I72e0b991767329caec37080caae2d5c2a9068eaf
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368104
    > > Commit-Queue: Tao Bai <michaelbai@chromium.org>
    > > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > > Reviewed-by: Changwan Ryu <changwan@chromium.org>
    > > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#801672}
    > 
    > TBR=michaelbai@chromium.org,wangxianzhu@chromium.org,dtrainor@chromium.org,changwan@chromium.org
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: 1114819
    > Bug: 1119663
    > Bug: 1122472
    > Change-Id: Ife98d73f4e77062606f18adb0ba12e9baaf4f6c3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2386578
    > Reviewed-by: Haiyang Pan <hypan@google.com>
    > Commit-Queue: Haiyang Pan <hypan@google.com>
    > Cr-Commit-Position: refs/heads/master@{#803349}
    
    TBR=michaelbai@chromium.org,wangxianzhu@chromium.org,dtrainor@chromium.org,changwan@chromium.org,hypan@google.com
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1114819
    Bug: 1119663
    Bug: 1122472
    Change-Id: I0763e728f9f75701c05ef9ae51f359eb78723660
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2387429
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803538}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentCaptureTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentCaptureTest.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.android_webview.test;
 
 import android.graphics.Rect;
+import android.net.Uri;
 
 import androidx.test.filters.LargeTest;
 import androidx.test.filters.SmallTest;
@@ -24,6 +25,7 @@ import org.chromium.base.test.util.Feature;
 import org.chromium.components.content_capture.ContentCaptureConsumer;
 import org.chromium.components.content_capture.ContentCaptureController;
 import org.chromium.components.content_capture.ContentCaptureData;
+import org.chromium.components.content_capture.ExperimentContentCaptureConsumer;
 import org.chromium.components.content_capture.FrameSession;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
@@ -58,6 +60,7 @@ public class AwContentCaptureTest {
             String[] allowlist = null;
             boolean[] isRegEx = null;
             if (mAllowlist != null && mIsRegEx != null) {
+                allowlist = new String[mAllowlist.size()];
                 mAllowlist.toArray(allowlist);
                 isRegEx = new boolean[mAllowlist.size()];
                 int i = 0;
@@ -66,6 +69,13 @@ public class AwContentCaptureTest {
                 }
             }
             setAllowlist(allowlist, isRegEx);
+        }
+
+        public void setAllowURL(String host) {
+            mAllowlist = new ArrayList<String>();
+            mAllowlist.add(host);
+            mIsRegEx = new ArrayList<Boolean>();
+            mIsRegEx.add(Boolean.FALSE);
         }
 
         private ArrayList<String> mAllowlist;
@@ -83,6 +93,10 @@ public class AwContentCaptureTest {
         public TestAwContentCaptureConsumer(WebContents webContents) {
             super(webContents);
             mCapturedContentIds = new HashSet<Long>();
+        }
+
+        public void setContentCaptureController(ContentCaptureController controller) {
+            mController = controller;
         }
 
         @Override
@@ -123,6 +137,12 @@ public class AwContentCaptureTest {
             }
             mCallbacks.add(CONTENT_REMOVED);
             mCallbackHelper.notifyCalled();
+        }
+
+        @Override
+        public boolean shouldCapture(String[] urls) {
+            if (mController == null) return true;
+            return mController.shouldCapture(urls);
         }
 
         public FrameSession getParentFrame() {
@@ -188,6 +208,7 @@ public class AwContentCaptureTest {
 
         // Use our own call count to avoid unexpected callback issue.
         private int mCallCount;
+        // TODO: (crbug.com/1121827) Remove volatile if possible.
         private volatile Set<Long> mCapturedContentIds;
         private volatile FrameSession mParentFrame;
         private volatile ContentCaptureData mCapturedContent;
@@ -198,6 +219,7 @@ public class AwContentCaptureTest {
         private volatile ArrayList<Integer> mCallbacks = new ArrayList<Integer>();
 
         private CallbackHelper mCallbackHelper = new CallbackHelper();
+        private volatile ContentCaptureController mController;
     }
 
     private static final String MAIN_FRAME_FILE = "/main_frame.html";
@@ -213,6 +235,7 @@ public class AwContentCaptureTest {
     private AwTestContainerView mContainerView;
     private TestAwContentCaptureConsumer mConsumer;
     private TestAwContentCatpureController mController;
+    private TestAwContentCaptureConsumer mSecondConsumer;
 
     private void loadUrlSync(String url) {
         try {
@@ -582,5 +605,87 @@ public class AwContentCaptureTest {
                     + "frame.parentNode.removeChild(frame);");
         }, toIntArray(TestAwContentCaptureConsumer.SESSION_REMOVED));
         verifyFrameSession(removedSession, mConsumer.getRemovedSession());
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    public void testMultipleConsumers() throws Throwable {
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            mSecondConsumer = new TestAwContentCaptureConsumer(mAwContents.getWebContents());
+        });
+        final String response = "<html><head></head><body>"
+                + "<div id='place_holder'>"
+                + "<p style=\"height: 100vh\">Hello</p>"
+                + "<p>world</p>"
+                + "</body></html>";
+        final String url = mWebServer.setResponse(MAIN_FRAME_FILE, response, null);
+        runAndVerifyCallbacks(() -> {
+            loadUrlSync(url);
+        }, toIntArray(TestAwContentCaptureConsumer.CONTENT_CAPTURED));
+        // Verify the other one also get the content.
+        verifyCallbacks(toIntArray(TestAwContentCaptureConsumer.CONTENT_CAPTURED),
+                mSecondConsumer.getCallbacks());
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    @CommandLineFlags.Add({"enable-features=ContentCaptureTriggeringForExperiment"})
+    public void testHostNotAllowed() throws Throwable {
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            mSecondConsumer = new TestAwContentCaptureConsumer(mAwContents.getWebContents());
+        });
+        final String response = "<html><head></head><body>"
+                + "<div id='place_holder'>"
+                + "<p style=\"height: 100vh\">Hello</p>"
+                + "<p>world</p>"
+                + "</body></html>";
+        final String url = mWebServer.setResponse(MAIN_FRAME_FILE, response, null);
+        mController.setAllowURL("www.chromium.org");
+        mSecondConsumer.setContentCaptureController(mController);
+        runAndVerifyCallbacks(() -> {
+            loadUrlSync(url);
+        }, toIntArray(TestAwContentCaptureConsumer.CONTENT_CAPTURED));
+        // Verify the other one didn't get the content.
+        Assert.assertEquals(0, mSecondConsumer.getCallbacks().length);
+    }
+
+    private void runHostAllowedTest() throws Throwable {
+        final String response = "<html><head></head><body>"
+                + "<div id='place_holder'>"
+                + "<p style=\"height: 100vh\">Hello</p>"
+                + "<p>world</p>"
+                + "</body></html>";
+        final String url = mWebServer.setResponse(MAIN_FRAME_FILE, response, null);
+        mController.setAllowURL(Uri.parse(url).getHost());
+        mConsumer.setContentCaptureController(mController);
+        runAndVerifyCallbacks(() -> {
+            loadUrlSync(url);
+        }, toIntArray(TestAwContentCaptureConsumer.CONTENT_CAPTURED));
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    @CommandLineFlags.Add({"disable-features=ContentCaptureTriggeringForExperiment"})
+    public void testHostAllowed() throws Throwable {
+        runHostAllowedTest();
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    @CommandLineFlags.Add({"enable-features=ContentCaptureTriggeringForExperiment"})
+    public void testHostAllowedForExperiment() throws Throwable {
+        runHostAllowedTest();
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"AndroidWebView"})
+    @CommandLineFlags.Add({"disable-features=ContentCaptureTriggeringForExperiment"})
+    public void testCantCreateExperimentConsumer() throws Throwable {
+        Assert.assertNull(ExperimentContentCaptureConsumer.create(mAwContents.getWebContents()));
     }
 }
