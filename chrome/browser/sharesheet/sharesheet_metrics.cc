413799d833426066611ecfef2cf81e4d454f4416
Melissa Zhang <melzhang@chromium.org>
2020-09-01 03:29:42 +0000

    [Sharesheet] Metrics
    This CL adds metrics for the sharesheet. We record when a user
    makes an action and how many apps are showing in the sharesheet
    with each invocation.
    
    Bug: 1097623
    Change-Id: I089524187db80aa31bff3ff2c11ac366da073f42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379394
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803373}

---
+++ chrome/browser/sharesheet/sharesheet_metrics.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/sharesheet/sharesheet_metrics.h"
+
+#include "base/metrics/histogram_functions.h"
+
+namespace {
+// Should be comfortably larger than any max number of apps
+// a user could have installed.
+constexpr size_t kMaxAppCount = 1000;
+}  // namespace
+
+namespace sharesheet {
+
+SharesheetMetrics::SharesheetMetrics() = default;
+
+void SharesheetMetrics::RecordSharesheetActionMetrics(UserAction action) {
+  base::UmaHistogramEnumeration("ChromeOS.Sharesheet.UserAction", action);
+}
+
+void SharesheetMetrics::RecordSharesheetAppCount(int app_count) {
+  base::UmaHistogramExactLinear("ChromeOS.Sharesheet.AppCount", app_count,
+                                kMaxAppCount);
+}
+
+}  // namespace sharesheet
--- chrome/browser/sharesheet/sharesheet_service.cc
+++ chrome/browser/sharesheet/sharesheet_service.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/apps/app_service/launch_utils.h"
 #include "chrome/browser/sharesheet/share_action.h"
+#include "chrome/browser/sharesheet/sharesheet_metrics.h"
 #include "chrome/browser/sharesheet/sharesheet_service_delegate.h"
 #include "chrome/browser/sharesheet/sharesheet_types.h"
 #include "chrome/common/chrome_features.h"
@@ -90,6 +91,9 @@ void SharesheetService::OnTargetSelected(uint32_t delegate_id,
     delegate->OnActionLaunched();
     share_action->LaunchAction(delegate, share_action_view, std::move(intent));
   } else if (type == TargetType::kApp) {
+    // TODO(crbug.com/1097623) Update this when we support more app types.
+    sharesheet::SharesheetMetrics::RecordSharesheetActionMetrics(
+        sharesheet::SharesheetMetrics::UserAction::kArc);
     auto launch_source = apps::mojom::LaunchSource::kFromSharesheet;
     app_service_proxy_->LaunchAppWithIntent(
         base::UTF16ToUTF8(target_name),
@@ -195,6 +199,8 @@ void SharesheetService::ShowBubbleWithDelegate(
 
   std::vector<apps::IntentLaunchInfo> intent_launch_info =
       app_service_proxy_->GetAppsForIntent(intent);
+  sharesheet::SharesheetMetrics::RecordSharesheetAppCount(
+      intent_launch_info.size());
   LoadAppIcons(std::move(intent_launch_info), std::move(targets), 0,
                base::BindOnce(&SharesheetService::OnAppIconsLoaded,
                               weak_factory_.GetWeakPtr(), std::move(delegate),
