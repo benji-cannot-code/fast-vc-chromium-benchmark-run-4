3da74f1bf06a759e18e0f7b542eefbd3b136014f
Maks Orlovich <morlovich@chromium.org>
2020-09-01 16:29:04 +0000

    Add NavigationHandle::GetNextPageUkmSourceId
    ... and use it to fix incorrect computation of nav UKM ID
    for extension file access in some cases (making sure to use the
    parent frame ID and not child frame ID for childframes, to keep
    the same ID for same-document navigations, and to properly match the
    ID when restoring from back-forward cache).
    
    Bug: 1103288
    
    Change-Id: I8439a3e2e1a22284077dc0078ff618986e6ee870
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363148
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803518}

--- content/public/browser/navigation_handle.h
+++ content/public/browser/navigation_handle.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/dns/public/resolve_error_info.h"
 #include "net/http/http_response_info.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 #include "services/network/public/cpp/resource_request_body.h"
 #include "third_party/blink/public/mojom/loader/referrer.mojom.h"
 #include "third_party/blink/public/mojom/loader/transferrable_url_loader.mojom.h"
@@ -61,6 +62,10 @@ class CONTENT_EXPORT NavigationHandle {
 
   // Get a unique ID for this navigation.
   virtual int64_t GetNavigationId() = 0;
+
+  // Get the page UKM ID that will be in use once this navigation fully commits
+  // (the eventual value of GetRenderFrameHost()->GetPageUkmSourceId()).
+  virtual ukm::SourceId GetNextPageUkmSourceId() = 0;
 
   // The URL the frame is navigating to. This may change during the navigation
   // when encountering a server redirect.
--- content/public/test/navigation_handle_observer.h
+++ content/public/test/navigation_handle_observer.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle_timing.h"
 #include "content/public/browser/reload_type.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 
 namespace content {
 
@@ -37,6 +38,7 @@ class NavigationHandleObserver : public WebContentsObserver {
   net::Error net_error_code() { return net_error_code_; }
   int64_t navigation_id() { return navigation_id_; }
   bool is_download() { return is_download_; }
+  ukm::SourceId next_page_ukm_source_id() { return next_page_ukm_source_id_; }
   base::Optional<net::AuthChallengeInfo> auth_challenge_info() {
     return auth_challenge_info_;
   }
@@ -68,6 +70,7 @@ class NavigationHandleObserver : public WebContentsObserver {
   net::Error net_error_code_ = net::OK;
   int64_t navigation_id_ = -1;
   bool is_download_ = false;
+  ukm::SourceId next_page_ukm_source_id_ = ukm::kInvalidSourceId;
   base::Optional<net::AuthChallengeInfo> auth_challenge_info_;
   net::ResolveErrorInfo resolve_error_info_;
   base::TimeTicks navigation_start_;
