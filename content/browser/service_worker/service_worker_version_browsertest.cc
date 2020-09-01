00bd0cb3cfef6c5a0846fdb4304d578f6822b720
Ben Kelly <wanderview@chromium.org>
2020-09-01 23:47:19 +0000

    CacheStorage: Remove eager reading feature flag.
    This removes the feature flag backing the CacheStorageEagerReading trial.
    The trial launched to 100% of stable for M84+ and was enabled by default
    on ToT in M86.
    
    Bug: 1010624
    Change-Id: I3885bd047ff6fd88c1fb488c01d111d83f36d566
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2387430
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803704}

--- content/browser/service_worker/service_worker_version_browsertest.cc
+++ content/browser/service_worker/service_worker_version_browsertest.cc
@@ -1733,16 +1733,8 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerVersionBrowserV8FullCodeCacheTest,
   StopWorker();
 }
 
-class CacheStorageEagerReadingTestBase
-    : public ServiceWorkerVersionBrowserTest {
+class CacheStorageEagerReadingTest : public ServiceWorkerVersionBrowserTest {
  public:
-  explicit CacheStorageEagerReadingTestBase(bool enabled) {
-    if (enabled)
-      feature_list.InitAndEnableFeature(features::kCacheStorageEagerReading);
-    else
-      feature_list.InitAndDisableFeature(features::kCacheStorageEagerReading);
-  }
-
   void SetupServiceWorkerAndDoFetch(
       std::string fetch_url,
       blink::mojom::FetchAPIResponsePtr* response_out) {
@@ -1805,42 +1797,14 @@ class CacheStorageEagerReadingTestBase
   base::test::ScopedFeatureList feature_list;
 };
 
-class CacheStorageEagerReadingEnabledTest
-    : public CacheStorageEagerReadingTestBase {
- public:
-  CacheStorageEagerReadingEnabledTest()
-      : CacheStorageEagerReadingTestBase(true) {}
-};
-
-class CacheStorageEagerReadingDisabledTest
-    : public CacheStorageEagerReadingTestBase {
- public:
-  CacheStorageEagerReadingDisabledTest()
-      : CacheStorageEagerReadingTestBase(false) {}
-};
-
-IN_PROC_BROWSER_TEST_F(CacheStorageEagerReadingDisabledTest,
-                       CacheMatchInRelatedFetchEvent) {
-  blink::mojom::FetchAPIResponsePtr response;
-  SetupServiceWorkerAndDoFetch(kCacheMatchURL, &response);
-  ExpectNormalCacheResponse(std::move(response));
-}
-
-IN_PROC_BROWSER_TEST_F(CacheStorageEagerReadingDisabledTest,
-                       CacheMatchInUnrelatedFetchEvent) {
-  blink::mojom::FetchAPIResponsePtr response;
-  SetupServiceWorkerAndDoFetch(kOtherURL, &response);
-  ExpectNormalCacheResponse(std::move(response));
-}
-
-IN_PROC_BROWSER_TEST_F(CacheStorageEagerReadingEnabledTest,
+IN_PROC_BROWSER_TEST_F(CacheStorageEagerReadingTest,
                        CacheMatchInRelatedFetchEvent) {
   blink::mojom::FetchAPIResponsePtr response;
   SetupServiceWorkerAndDoFetch(kCacheMatchURL, &response);
   ExpectEagerlyReadCacheResponse(std::move(response));
 }
 
-IN_PROC_BROWSER_TEST_F(CacheStorageEagerReadingEnabledTest,
+IN_PROC_BROWSER_TEST_F(CacheStorageEagerReadingTest,
                        CacheMatchInUnrelatedFetchEvent) {
   blink::mojom::FetchAPIResponsePtr response;
   SetupServiceWorkerAndDoFetch(kOtherURL, &response);
