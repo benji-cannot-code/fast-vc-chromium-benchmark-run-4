3e1062777a03563a552fd0d18584fabd8b81fcbf
Tal Pressman <talp@chromium.org>
2020-09-01 00:24:23 +0000

    [MBI] Bind AgentSchedulingGroup(Host) mojo interfaces.
    The interfaces are still empty and don't do anything.
    
    Bug: 1111231
    Change-Id: I778d365eaaaf53cf8e9855bc131f1cebc69d4f11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2374186
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Tal Pressman <talp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803324}

--- content/browser/renderer_host/agent_scheduling_group_host.h
+++ content/browser/renderer_host/agent_scheduling_group_host.h
@@ -7,13 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_RENDERER_HOST_AGENT_SCHEDULING_GROUP_HOST_H_
 
 #include <stdint.h>
-#include <memory>
 
 #include "content/common/agent_scheduling_group.mojom.h"
 #include "content/common/associated_interfaces.mojom-forward.h"
 #include "content/common/content_export.h"
 #include "content/common/renderer.mojom-forward.h"
-#include "ipc/ipc_listener.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
 namespace IPC {
@@ -35,7 +34,8 @@ class SiteInstance;
 //
 // An AgentSchedulingGroupHost is stored as (and owned by) UserData on the
 // RenderProcessHost.
-class CONTENT_EXPORT AgentSchedulingGroupHost {
+class CONTENT_EXPORT AgentSchedulingGroupHost
+    : public mojom::AgentSchedulingGroupHost {
  public:
   // Get the appropriate AgentSchedulingGroupHost for the given |instance| and
   // |process|. For now, each RenderProcessHost has a single
@@ -46,7 +46,7 @@ class CONTENT_EXPORT AgentSchedulingGroupHost {
 
   // Should not be called explicitly. Use Get() instead.
   explicit AgentSchedulingGroupHost(RenderProcessHost& process);
-  ~AgentSchedulingGroupHost();
+  ~AgentSchedulingGroupHost() override;
 
   RenderProcessHost* GetProcess();
 
@@ -66,14 +66,13 @@ class CONTENT_EXPORT AgentSchedulingGroupHost {
   // The RenderProcessHost this AgentSchedulingGroup is assigned to.
   RenderProcessHost& process_;
 
-  // Internal implementation of content::mojom::AgentSchedulingGroupHost, used
-  // for responding to calls from the (renderer-side) AgentSchedulingGroup.
-  std::unique_ptr<content::mojom::AgentSchedulingGroupHost> mojo_impl_;
+  // Implementation of `mojom::AgentSchedulingGroupHost`, used for responding to
+  // calls from the (renderer-side) `AgentSchedulingGroup`.
+  mojo::Receiver<mojom::AgentSchedulingGroupHost> receiver_{this};
 
-  // Remote stub of content::mojom::AgentSchedulingGroup, used for sending calls
-  // to the (renderer-side) AgentSchedulingGroup.
-  std::unique_ptr<mojo::Remote<content::mojom::AgentSchedulingGroup>>
-      mojo_remote_;
+  // Remote stub of `mojom::AgentSchedulingGroup`, used for sending calls to the
+  // (renderer-side) `AgentSchedulingGroup`.
+  mojo::Remote<mojom::AgentSchedulingGroup> mojo_remote_;
 };
 
 }  // namespace content
