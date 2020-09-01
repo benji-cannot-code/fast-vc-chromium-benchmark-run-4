35aa4c4956101c3289c2faafedfa74d2ba2f853f
Maksim Sisov <msisov@igalia.com>
2020-09-01 11:27:38 +0000

    Reland "Reland "Reland "X11 and Ozone: enable use_x11 and use_ozone"""
    This is a reland of 8ce938a2f69dcd0ad814f5045d30ba13d99b910e
    
    Fixes vulkan tests on Linux FYI SkiaRenderer Vulkan
    and angle tests on Linux Ozone FYI bot.
    
    Original change's description:
    > Reland "Reland "X11 and Ozone: enable use_x11 and use_ozone""
    >
    > This is a reland of 75363679c701c78fac57dea6b816f4c21c80bf5e
    >
    > Original change's description:
    > > Reland "X11 and Ozone: enable use_x11 and use_ozone"
    > >
    > > This is a reland of f985905ebdd488c63b7819a65c817aa98fb8da49.
    > >
    > > This change broke our headless build:
    > > ERROR at //ui/events/platform/x11/BUILD.gn:51:5: Assertion failed.
    > >     assert(ozone_platform_x11)
    > >     ^-----
    > > See //components/viz/demo/BUILD.gn:73:7: which caused the file to be included.
    > >       "//ui/events/platform/x11",.
    > >
    > > Fix:
    > > Don't include x11 build for headless case.
    > >
    > > Original change's description:
    > > > X11 and Ozone: enable use_x11 and use_ozone
    > > >
    > > > This is the final change that enables use_x11
    > > > and use_ozone for Linux builds and (as it was
    > > > before) excludes X11 for chromeos, chromecast,
    > > > lacros.
    > > >
    > > > in order to run ozone/X11/wayland, one must pass
    > > > —enable-features=UseOzonePlatform
    > > > —ozone-platform=x11 or wayland. otherwise,
    > > > non-ozone/X11 is used.
    > > >
    > > > it’s also possible to completely disable X11
    > > > so that clients, who need that kind of build can
    > > > continue to have that.
    > > >
    > > > Bug: 1085700
    > > > Change-Id: I171bcb53c406555a1d4bec3e88d76dc676310d48
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2315869
    > > > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > > > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > > > Reviewed-by: Scott Violet <sky@chromium.org>
    > > > Commit-Queue: Maksim Sisov (GMT+3) <msisov@igalia.com>
    > > > Cr-Commit-Position: refs/heads/master@{#801523}
    > >
    > > Bug: 1085700
    > > Change-Id: I8bf0abead6c94c95ddc58443de7cf2268c977cf1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2376806
    > > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > > Commit-Queue: Maksim Sisov (GMT+3) <msisov@igalia.com>
    > > Cr-Commit-Position: refs/heads/master@{#801692}
    >
    > Bug: 1085700
    > Change-Id: Ia5ed2af9376354d3378d8edd98a7a1446f13c88b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379190
    > Commit-Queue: Maksim Sisov (GMT+3) <msisov@igalia.com>
    > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#802773}
    
    Bug: 1085700
    Change-Id: I26fb00938adc6a9e49576257ca316f23830e9cf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2382834
    Commit-Queue: Maksim Sisov (GMT+3) <msisov@igalia.com>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803448}

--- gpu/vulkan/tests/vulkan_tests_main.cc
+++ gpu/vulkan/tests/vulkan_tests_main.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/platform/platform_event_source.h"
 
 #if defined(USE_OZONE)
+#include "ui/base/ui_base_features.h"
 #include "ui/ozone/public/ozone_platform.h"
 #endif
 
@@ -31,13 +32,15 @@ class VulkanTestSuite : public base::TestSuite {
 
 #if defined(USE_OZONE)
     // Make Ozone run in single-process mode.
-    ui::OzonePlatform::InitParams params;
-    params.single_process = true;
+    if (features::IsUsingOzonePlatform()) {
+      ui::OzonePlatform::InitParams params;
+      params.single_process = true;
 
-    // This initialization must be done after TaskEnvironment has
-    // initialized the UI thread.
-    ui::OzonePlatform::InitializeForUI(params);
-    ui::OzonePlatform::InitializeForGPU(params);
+      // This initialization must be done after TaskEnvironment has
+      // initialized the UI thread.
+      ui::OzonePlatform::InitializeForUI(params);
+      ui::OzonePlatform::InitializeForGPU(params);
+    }
 #endif
   }
 
