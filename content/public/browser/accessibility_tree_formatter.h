e90f54cc3820e5640f784fb47695864ff370c5a9
Alexander Surkov <asurkov@igalia.com>
2020-09-01 23:45:18 +0000

    DumpAccTree testsuite: convert utf16 to utf8 in NodeFilter
    Bug: 1115489
    Change-Id: I0987625fcb7a2c46ee13aa276f6ab1ddaae6d072
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363883
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#803702}

--- content/public/browser/accessibility_tree_formatter.h
+++ content/public/browser/accessibility_tree_formatter.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/process/process_handle.h"
-#include "base/strings/string16.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
@@ -93,9 +92,9 @@ class CONTENT_EXPORT AccessibilityTreeFormatter
   //   {property='internalRole', pattern='inlineTextBox'};
   struct NodeFilter {
     std::string property;
-    base::string16 pattern;
+    std::string pattern;
 
-    NodeFilter(std::string property, base::string16 pattern)
+    NodeFilter(const std::string& property, const std::string& pattern)
         : property(property), pattern(pattern) {}
   };
 
