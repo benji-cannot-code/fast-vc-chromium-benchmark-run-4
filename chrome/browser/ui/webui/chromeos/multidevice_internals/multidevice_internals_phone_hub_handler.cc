9219a3e50398674056d4e3faee62553952bedaae
Regan Hsu <hsuregan@chromium.org>
2020-09-01 18:46:26 +0000

    [CrOS PhoneHub] Add set feature status row to PhoneHub tab.
    * PhoneHub tab in chrome://multidevice-internals includes new feature
      status dropdown.
    * Selecting a feature changes the feature on demand.
    
    Screenshot:
    https://screenshot.googleplex.com/7NwZJKgRF4WiNmT
    
    Bug: 1106937
    Change-Id: Ibb53e737f29f4bc3e209928804cb2d1ff5876a88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2386322
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803578}

--- chrome/browser/ui/webui/chromeos/multidevice_internals/multidevice_internals_phone_hub_handler.cc
+++ chrome/browser/ui/webui/chromeos/multidevice_internals/multidevice_internals_phone_hub_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/system_tray.h"
 #include "chrome/browser/chromeos/phonehub/phone_hub_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chromeos/components/multidevice/logging/logging.h"
 #include "chromeos/components/phonehub/fake_phone_hub_manager.h"
 
 namespace chromeos {
@@ -26,6 +27,11 @@ void MultidevicePhoneHubHandler::RegisterMessages() {
       base::BindRepeating(
           &MultidevicePhoneHubHandler::HandleSetFakePhoneHubManagerEnabled,
           base::Unretained(this)));
+
+  web_ui()->RegisterMessageCallback(
+      "setFeatureStatus",
+      base::BindRepeating(&MultidevicePhoneHubHandler::HandleSetFeatureStatus,
+                          base::Unretained(this)));
 }
 
 void MultidevicePhoneHubHandler::SetSystemPhoneHubManagerEnabled() {
@@ -51,6 +57,15 @@ void MultidevicePhoneHubHandler::HandleSetFakePhoneHubManagerEnabled(
     return;
   }
   SetSystemPhoneHubManagerEnabled();
+}
+
+void MultidevicePhoneHubHandler::HandleSetFeatureStatus(
+    const base::ListValue* args) {
+  int feature_as_int = 0;
+  CHECK(args->GetInteger(0, &feature_as_int));
+
+  auto feature = static_cast<phonehub::FeatureStatus>(feature_as_int);
+  fake_phone_hub_manager_->fake_feature_status_provider()->SetStatus(feature);
 }
 
 }  // namespace multidevice
