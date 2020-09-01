340e4ef996382f715998048f0f1177e6d22dd05b
Glen Robertson <glenrob@chromium.org>
2020-09-01 16:40:06 +0000

    smart_dim: Remove unneeded include and expose metrics.h.
    Fixes `gn check` errors.
    Split out from crrev.com/c/2383400
    
    Bug: 898837
    Change-Id: Ia8e32e907bddc56c6e214d5c2ab1f5477fe1a2f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2384709
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803525}

--- chrome/browser/component_updater/smart_dim_component_installer.cc
+++ chrome/browser/component_updater/smart_dim_component_installer.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
 #include "base/version.h"
-#include "chrome/browser/chromeos/power/ml/smart_dim/download_worker.h"
 #include "chrome/browser/chromeos/power/ml/smart_dim/metrics.h"
 #include "chrome/browser/chromeos/power/ml/smart_dim/ml_agent.h"
 #include "chromeos/constants/chromeos_features.h"
