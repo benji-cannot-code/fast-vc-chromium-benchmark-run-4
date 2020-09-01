85322e940b7c8eccd23c01283f0cc44a2abbe5de
Ahmed Mehfooz <amehfooz@chromium.org>
2020-09-01 23:05:39 +0000

    Add item chip view for holding space
    Bug: 1123454
    Change-Id: I936c38b274b62d0c3f5218bc414833ee6e6a7223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2384909
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Xiyuan Slow <xiyuan@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#803691}

---
+++ ash/system/holding_space/holding_space_item_chips_container.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_HOLDING_SPACE_HOLDING_SPACE_ITEM_CHIPS_CONTAINER_H_
+#define ASH_SYSTEM_HOLDING_SPACE_HOLDING_SPACE_ITEM_CHIPS_CONTAINER_H_
+
+#include "ash/ash_export.h"
+#include "ui/views/layout/grid_layout.h"
+#include "ui/views/view.h"
+
+namespace views {
+class ColumnSet;
+class GridLayout;
+}  // namespace views
+
+namespace ash {
+
+class HoldingSpaceItem;
+
+// A container view which automatically arranges item chips into a 2 column
+// grid.
+class HoldingSpaceItemChipsContainer : public views::View {
+ public:
+  HoldingSpaceItemChipsContainer();
+  HoldingSpaceItemChipsContainer(const HoldingSpaceItemChipsContainer& other) =
+      delete;
+  HoldingSpaceItemChipsContainer& operator=(
+      const HoldingSpaceItemChipsContainer& other) = delete;
+  ~HoldingSpaceItemChipsContainer() override;
+
+  void AddItemChip(HoldingSpaceItem* item);
+
+  // views::View:
+  const char* GetClassName() const override;
+
+ private:
+  views::GridLayout* layout_ = nullptr;
+  views::ColumnSet* column_set_ = nullptr;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_HOLDING_SPACE_HOLDING_SPACE_ITEM_CHIPS_CONTAINER_H_
--- ash/system/holding_space/pinned_files_container.h
+++ ash/system/holding_space/pinned_files_container.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+class HoldingSpaceItemChipsContainer;
+
 // Container for pinned files that the user adds to the holding space bubble.
 class PinnedFilesContainer : public views::View {
  public:
@@ -20,6 +22,9 @@ class PinnedFilesContainer : public views::View {
 
   // views::View:
   const char* GetClassName() const override;
+
+ private:
+  HoldingSpaceItemChipsContainer* item_chips_container_ = nullptr;
 };
 
 }  // namespace ash
