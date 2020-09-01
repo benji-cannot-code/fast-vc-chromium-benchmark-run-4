43dc0006b6772abd2bb57eff933e7f18637bc045
Sean McAllister <smcallis@google.com>
2020-09-01 15:54:57 +0000

    Refactor OS_LINUX preprocessor directive for LaCrOS effort.
    Currently, ChromeOS defines the OS_LINUX directive as well as
    OS_CHROMEOS.  We're working to separate these two, so we're
    making the fact that OS_LINUX == OS_LINUX || OS_CHROMEOS
    explicit.
    
    This is changes for /chrome/browser/ui/views/web_apps
    
    This CL was uploaded by git cl split.
    
    R=mgiuca@chromium.org
    
    Bug: 1110266
    Change-Id: I452196d7fb9de8634c2c8b6044d9b0bafb4691ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2371282
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Sean McAllister <smcallis@google.com>
    Cr-Commit-Position: refs/heads/master@{#803501}

--- chrome/browser/ui/views/web_apps/pwa_confirmation_bubble_view.cc
+++ chrome/browser/ui/views/web_apps/pwa_confirmation_bubble_view.cc
@@ -34,7 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-#if defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX)
+#if defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) || \
+    defined(OS_CHROMEOS)
 constexpr char kDeviceTypeForCheckbox[] = "computer";
 #else
 constexpr char kDeviceTypeForCheckbox[] = "other";
