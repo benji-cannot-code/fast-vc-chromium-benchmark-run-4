f91265f09a2bb0032e32773076d3db928ccf9fb9
John Budorick <jbudorick@chromium.org>
2020-09-01 16:57:50 +0000

    Use denylist throughout //build/android.
    Bug: 1097306
    Change-Id: I7a96cddd408d6b75e3a55c68f7e18fd481476361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2384079
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803531}

--- build/android/tombstones.py
+++ build/android/tombstones.py
@@ -20,7 +20,7 @@ from multiprocessing.pool import ThreadPool
 
 import devil_chromium
 
-from devil.android import device_blacklist
+from devil.android import device_denylist
 from devil.android import device_errors
 from devil.android import device_utils
 from devil.utils import run_tests_helper
@@ -234,7 +234,12 @@ def main():
   parser.add_argument('--device',
                       help='The serial number of the device. If not specified '
                            'will use all devices.')
-  parser.add_argument('--blacklist-file', help='Device blacklist JSON file.')
+  # TODO(crbug.com/1097306): Remove this once callers have all switched to
+  # --denylist-file.
+  parser.add_argument('--blacklist-file',
+                      dest='denylist_file',
+                      help=argparse.SUPPRESS)
+  parser.add_argument('--denylist-file', help='Device denylist JSON file.')
   parser.add_argument('-a', '--all-tombstones', action='store_true',
                       help='Resolve symbols for all tombstones, rather than '
                            'just the most recent.')
@@ -254,9 +259,8 @@ def main():
 
   devil_chromium.Initialize(adb_path=args.adb_path)
 
-  blacklist = (device_blacklist.Blacklist(args.blacklist_file)
-               if args.blacklist_file
-               else None)
+  denylist = (device_denylist.Denylist(args.denylist_file)
+              if args.denylist_file else None)
 
   if args.output_directory:
     constants.SetOutputDirectory(args.output_directory)
@@ -266,7 +270,7 @@ def main():
   if args.device:
     devices = [device_utils.DeviceUtils(args.device)]
   else:
-    devices = device_utils.DeviceUtils.HealthyDevices(blacklist)
+    devices = device_utils.DeviceUtils.HealthyDevices(denylist)
 
   # This must be done serially because strptime can hit a race condition if
   # used for the first time in a multithreaded environment.
