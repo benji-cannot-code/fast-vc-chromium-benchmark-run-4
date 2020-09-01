18e1a01b09735481942c12b08725d574659f6e24
Daniel Rubery <drubery@chromium.org>
2020-09-01 16:11:11 +0000

    Report files blocked by DownloadRestrictions policy
    This CL adds reporting events for files blocked due to the
    DownloadRestrictions. These broadly fall into two categories. those
    blocked due to a dangerous verdict from Safe Browsing will report an
    event like:
    {
       "dangerousDownloadEvent": {
          "clickedThrough": false,
          "contentSize": 276,
          "contentType": "application/x-msdos-program",
          "downloadDigestSha256": "0BA8709CFD4AA9CAF41287295D9E1B8E6ADA4E0111BFAE9FD41F08EEBB57517E",
          "eventResult": "EVENT_RESULT_BLOCKED",
          "fileName": "drubery/Downloads/content (2).exe",
          "profileUserName": "",
          "threatType": "DANGEROUS_FILE_TYPE",
          "trigger": "FILE_DOWNLOAD",
          "url": "https://testsafebrowsing.appspot.com/s/content.exe"
       },
       "time": "2020-08-22T17:26:27.372Z",
       "uploaded_successfully": true
    }
    
    When DownloadRestrictions is set to 2, we also block files based on
    file type alone. This leads to reports like:
    {
       "dangerousDownloadEvent": {
          "clickedThrough": false,
          "contentSize": 0,
          "contentType": "application/x-shockwave-flash",
          "downloadDigestSha256": "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855",
          "eventResult": "EVENT_RESULT_BLOCKED",
          "fileName": "drubery/Downloads/test (3).swf",
          "profileUserName": "",
          "threatType": "DANGEROUS_FILE_TYPE",
          "trigger": "FILE_DOWNLOAD",
          "url": "http://localhost:8000/test.swf"
       },
       "time": "2020-08-22T17:28:41.252Z",
       "uploaded_successfully": true
    }
    
    Fixed: 1119418
    Change-Id: I6dad9bcf97e45018d857f6e660613a1efcab1c12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2370505
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803508}

--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router_unittest.cc
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router_unittest.cc
@@ -177,14 +177,15 @@ class SafeBrowsingPrivateEventRouterTest : public testing::Test {
                                           "PHISHING", -201);
   }
 
-  void TriggerOnDangerousDownloadWarningEvent() {
+  void TriggerOnDangerousDownloadEvent() {
     SafeBrowsingPrivateEventRouterFactory::GetForProfile(profile_)
-        ->OnDangerousDownloadWarning(
+        ->OnDangerousDownloadEvent(
             GURL("https://maybevil.com/warning.exe"), "/path/to/warning.exe",
-            "sha256_of_warning_exe", "POTENTIALLY_UNWANTED", "exe", 567);
+            "sha256_of_warning_exe", "POTENTIALLY_UNWANTED", "exe", 567,
+            safe_browsing::EventResult::WARNED);
   }
 
-  void TriggerOnDangerousDownloadWarningEventBypass() {
+  void TriggerOnDangerousDownloadEventBypass() {
     SafeBrowsingPrivateEventRouterFactory::GetForProfile(profile_)
         ->OnDangerousDownloadWarningBypassed(
             GURL("https://bypassevil.com/bypass.exe"), "/path/to/bypass.exe",
@@ -470,7 +471,7 @@ TEST_F(SafeBrowsingPrivateEventRouterTest, TestOnDangerousDownloadWarning) {
   EXPECT_CALL(*client_, UploadRealtimeReport_(_, _))
       .WillOnce(CaptureArg(&report));
 
-  TriggerOnDangerousDownloadWarningEvent();
+  TriggerOnDangerousDownloadEvent();
   base::RunLoop().RunUntilIdle();
 
   Mock::VerifyAndClearExpectations(client_.get());
@@ -512,7 +513,7 @@ TEST_F(SafeBrowsingPrivateEventRouterTest,
   EXPECT_CALL(*client_, UploadRealtimeReport_(_, _))
       .WillOnce(CaptureArg(&report));
 
-  TriggerOnDangerousDownloadWarningEventBypass();
+  TriggerOnDangerousDownloadEventBypass();
   base::RunLoop().RunUntilIdle();
 
   Mock::VerifyAndClearExpectations(client_.get());
@@ -677,7 +678,7 @@ TEST_F(SafeBrowsingPrivateEventRouterTest,
   base::Value report;
   EXPECT_CALL(*client_, UploadRealtimeReport_(_, _)).Times(0);
 
-  TriggerOnDangerousDownloadWarningEvent();
+  TriggerOnDangerousDownloadEvent();
   base::RunLoop().RunUntilIdle();
 
   Mock::VerifyAndClearExpectations(client_.get());
@@ -694,7 +695,7 @@ TEST_F(SafeBrowsingPrivateEventRouterTest,
   base::Value report;
   EXPECT_CALL(*client_, UploadRealtimeReport_(_, _)).Times(0);
 
-  TriggerOnDangerousDownloadWarningEventBypass();
+  TriggerOnDangerousDownloadEventBypass();
   base::RunLoop().RunUntilIdle();
 
   Mock::VerifyAndClearExpectations(client_.get());
