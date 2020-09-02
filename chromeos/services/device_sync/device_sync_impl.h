fda4e83112f0410dda8aa4db8181c5645c6c3905
Glen Robertson <glenrob@chromium.org>
2020-09-02 18:11:59 +0000

    Expose device_sync RegisterProfilePrefs separately from impl.
    This allows us to stop depending on the impl in browser_prefs.cc.
    Fixes `gn check` errors. Split out from crrev.com/c/2383400
    
    Bug: 898837
    Change-Id: I777200b6a49710e1496fb3189f8bbed80fb3cbb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2386437
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803925}

--- chromeos/services/device_sync/device_sync_impl.h
+++ chromeos/services/device_sync/device_sync_impl.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 
-class PrefRegistrySimple;
 class PrefService;
 
 namespace base {
@@ -102,8 +101,6 @@ class DeviceSyncImpl : public DeviceSyncBase,
    private:
     static Factory* test_factory_instance_;
   };
-
-  static void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
   ~DeviceSyncImpl() override;
 
--- chromeos/services/device_sync/public/cpp/device_sync_client_impl_unittest.cc
+++ chromeos/services/device_sync/public/cpp/device_sync_client_impl_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/device_sync/fake_device_sync.h"
 #include "chromeos/services/device_sync/feature_status_change.h"
 #include "chromeos/services/device_sync/proto/cryptauth_common.pb.h"
+#include "chromeos/services/device_sync/public/cpp/device_sync_prefs.h"
 #include "chromeos/services/device_sync/public/cpp/fake_client_app_metadata_provider.h"
 #include "chromeos/services/device_sync/public/cpp/fake_gcm_device_info_provider.h"
 #include "chromeos/services/device_sync/public/mojom/device_sync.mojom.h"
@@ -177,7 +178,7 @@ class DeviceSyncClientImplTest : public testing::Test {
             }));
 
     test_pref_service_ = std::make_unique<TestingPrefServiceSimple>();
-    DeviceSyncImpl::RegisterProfilePrefs(test_pref_service_->registry());
+    RegisterProfilePrefs(test_pref_service_->registry());
 
     device_sync_ = DeviceSyncImpl::Factory::Create(
         identity_test_environment_->identity_manager(), fake_gcm_driver_.get(),
