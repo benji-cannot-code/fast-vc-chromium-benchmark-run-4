2512a400b8a4e4b31b8949495b227ea7b6ca5232
Nicholas Verne <nverne@chromium.org>
2020-09-02 02:29:46 +0000

    Get rid of priority setting for USB sharing notifications.
    Default priority is fine - the notification persists for 8 seconds and
    won't wake the display.
    
    Fixed: 1117307
    Change-Id: I2734a1391b7679224e2fb24e319dc4320aace59b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2389283
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: David Munro <davidmunro@google.com>
    Auto-Submit: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/master@{#803743}

--- chrome/browser/chromeos/usb/cros_usb_detector.cc
+++ chrome/browser/chromeos/usb/cros_usb_detector.cc
@@ -241,7 +241,6 @@ void ShowNotificationForDevice(device::mojom::UsbDeviceInfoPtr device_info) {
       rich_notification_data,
       base::MakeRefCounted<CrosUsbNotificationDelegate>(
           notification_id, std::move(device_info)));
-  notification.SetSystemPriority();
   SystemNotificationHelper::GetInstance()->Display(notification);
 }
 
