f5512b8eacf7b3c1039095b2be77c8dc017326d7
Mathias Bynens <mathias@chromium.org>
2020-09-02 10:08:43 +0000

    Make FPS meter color vision deficiency-friendly
    This patch updates the FPS meter colors per the UX team’s suggestions:
    
    | description                 | old color | new color                |
    | --------------------------- | --------- | ------------------------ |
    | dropped frame               | red       | #CA5B1DFF (orange/brown) |
    | partial/missed frame        | yellow    | #FFF500FF (light yellow) |
    | successfully rendered frame | green     | #AEDDFFBF (light blue)   |
    
    Bug: chromium:1104087
    Change-Id: I0654562172b5f8219502f9d121a8294c138ce0f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2384033
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803809}

--- cc/debug/debug_colors.h
+++ cc/debug/debug_colors.h
@@ -125,6 +125,9 @@ class CC_DEBUG_EXPORT DebugColors {
 
   static SkColor PlatformLayerTreeTextColor();
   static SkColor FPSDisplayTextAndGraphColor();
+  static SkColor FPSDisplayDroppedFrame();
+  static SkColor FPSDisplayMissedFrame();
+  static SkColor FPSDisplaySuccessfulFrame();
   static SkColor MemoryDisplayTextColor();
   static SkColor PaintTimeDisplayTextAndGraphColor();
 
