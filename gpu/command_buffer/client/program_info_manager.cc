10fc1c6577c74e82631be395f2468bf1e8f8f116
jchen10 <jie.a.chen@intel.com>
2020-09-02 01:45:51 +0000

    Send back program information asynchronously
    This sends back program information asynchronously in
    GL_PROGRAM_COMPLETION_QUERY_CHROMIUM via GpuControlReturnData IPC.
    
    Bug: 1103499
    Change-Id: I41eacb590bb6fc98ce2c8efc0fdb1272efce57c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2377745
    Commit-Queue: Jie A Chen <jie.a.chen@intel.com>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803728}

--- gpu/command_buffer/client/program_info_manager.cc
+++ gpu/command_buffer/client/program_info_manager.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 template <typename T>
-static T LocalGetAs(const std::vector<int8_t>& data,
+static T LocalGetAs(base::span<const int8_t> data,
                     uint32_t offset,
                     size_t size) {
   const int8_t* p = data.data() + offset;
@@ -359,7 +359,7 @@ bool ProgramInfoManager::Program::GetUniformsiv(
   return false;
 }
 
-void ProgramInfoManager::Program::UpdateES2(const std::vector<int8_t>& result) {
+void ProgramInfoManager::Program::UpdateES2(base::span<const int8_t> result) {
   if (cached_es2_) {
     return;
   }
@@ -415,7 +415,7 @@ void ProgramInfoManager::Program::UpdateES2(const std::vector<int8_t>& result) {
 }
 
 void ProgramInfoManager::Program::UpdateES3UniformBlocks(
-    const std::vector<int8_t>& result) {
+    base::span<const int8_t> result) {
   if (cached_es3_uniform_blocks_) {
     return;
   }
@@ -486,7 +486,7 @@ void ProgramInfoManager::Program::UpdateES3UniformBlocks(
 }
 
 void ProgramInfoManager::Program::UpdateES3Uniformsiv(
-    const std::vector<int8_t>& result) {
+    base::span<const int8_t> result) {
   if (cached_es3_uniformsiv_) {
     return;
   }
@@ -528,7 +528,7 @@ void ProgramInfoManager::Program::UpdateES3Uniformsiv(
 }
 
 void ProgramInfoManager::Program::UpdateES3TransformFeedbackVaryings(
-    const std::vector<int8_t>& result) {
+    base::span<const int8_t> result) {
   if (cached_es3_transform_feedback_varyings_) {
     return;
   }
@@ -1060,6 +1060,37 @@ GLint ProgramInfoManager::GetProgramResourceLocation(
   return gl->GetProgramResourceLocationHelper(program, program_interface, name);
 }
 
+void ProgramInfoManager::UpdateProgramInfo(GLuint program,
+                                           base::span<const int8_t> data,
+                                           ProgramInfoType type) {
+  base::AutoLock auto_lock(lock_);
+  ProgramInfoMap::iterator it = program_infos_.find(program);
+  // It's possible that the program has been deleted already. Imagine the code
+  // snippet below:
+  //   ...
+  //   gl.linkProgram(program);
+  //   gl.deleteProgram(program);
+  //   ...
+  if (it == program_infos_.end())
+    return;
+  Program* info = &it->second;
+  switch (type) {
+    case kES2:
+      info->UpdateES2(data);
+      break;
+    case kES3UniformBlocks:
+      info->UpdateES3UniformBlocks(data);
+      break;
+    case kES3TransformFeedbackVaryings:
+      info->UpdateES3TransformFeedbackVaryings(data);
+      break;
+    case kES3Uniformsiv:
+      info->UpdateES3Uniformsiv(data);
+      break;
+    default:
+      NOTREACHED();
+  }
+}
+
 }  // namespace gles2
 }  // namespace gpu
-
