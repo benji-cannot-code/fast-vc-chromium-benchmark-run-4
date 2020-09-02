f0286a79511dd05dbb85a56b87edecf9dd0af419
Alan Cutter <alancutter@chromium.org>
2020-09-02 02:26:36 +0000

    Rename ExternalInstallOptions::url to install_url
    This is a mechanical rename to be more specific about the purpose of
    the ExternalInstallOptions url.
    
    Change-Id: Ib51ea13f8be3e5df4d16ddf98c53c991ec6f8934
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2386916
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803741}

--- chrome/browser/web_applications/components/external_install_options.cc
+++ chrome/browser/web_applications/components/external_install_options.cc
@@ -15,10 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace web_app {
 
 ExternalInstallOptions::ExternalInstallOptions(
-    const GURL& url,
+    const GURL& install_url,
     DisplayMode user_display_mode,
     ExternalInstallSource install_source)
-    : url(url),
+    : install_url(install_url),
       user_display_mode(user_display_mode),
       install_source(install_source) {}
 
@@ -35,7 +35,7 @@ ExternalInstallOptions& ExternalInstallOptions::operator=(
 
 bool ExternalInstallOptions::operator==(
     const ExternalInstallOptions& other) const {
-  return std::tie(url, user_display_mode, install_source,
+  return std::tie(install_url, user_display_mode, install_source,
                   add_to_applications_menu, add_to_desktop,
                   add_to_quick_launch_bar, add_to_search, add_to_management,
                   is_disabled, override_previous_user_uninstall,
@@ -43,11 +43,11 @@ bool ExternalInstallOptions::operator==(
                   force_reinstall, wait_for_windows_closed, install_placeholder,
                   reinstall_placeholder, uninstall_and_replace,
                   additional_search_terms) ==
-         std::tie(other.url, other.user_display_mode, other.install_source,
-                  other.add_to_applications_menu, other.add_to_desktop,
-                  other.add_to_quick_launch_bar, other.add_to_search,
-                  other.add_to_management, other.is_disabled,
-                  other.override_previous_user_uninstall,
+         std::tie(other.install_url, other.user_display_mode,
+                  other.install_source, other.add_to_applications_menu,
+                  other.add_to_desktop, other.add_to_quick_launch_bar,
+                  other.add_to_search, other.add_to_management,
+                  other.is_disabled, other.override_previous_user_uninstall,
                   other.bypass_service_worker_check, other.require_manifest,
                   other.force_reinstall, other.wait_for_windows_closed,
                   other.install_placeholder, other.reinstall_placeholder,
@@ -56,7 +56,8 @@ bool ExternalInstallOptions::operator==(
 
 std::ostream& operator<<(std::ostream& out,
                          const ExternalInstallOptions& install_options) {
-  return out << "url: " << install_options.url << "\n user_display_mode: "
+  return out << "install_url: " << install_options.install_url
+             << "\n user_display_mode: "
              << static_cast<int32_t>(install_options.user_display_mode)
              << "\n install_source: "
              << static_cast<int32_t>(install_options.install_source)
@@ -93,7 +94,7 @@ InstallManager::InstallParams ConvertExternalInstallOptionsToParams(
 
   params.user_display_mode = install_options.user_display_mode;
 
-  params.fallback_start_url = install_options.url;
+  params.fallback_start_url = install_options.install_url;
 
   params.add_to_applications_menu = install_options.add_to_applications_menu;
   params.add_to_desktop = install_options.add_to_desktop;
