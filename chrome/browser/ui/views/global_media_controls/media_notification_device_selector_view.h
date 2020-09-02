e8df7dd3ecb8f5f8e17f9cfdc568cca791cce52e
Noah Rose Ledesma <noahrose@google.com>
2020-09-02 21:29:19 +0000

    GMC: Tweak the device selector entry point button
    This change tweaks the look and feel of the button to open the audio
    device picker. Screenshots of this button are on the crbug.
    
    Bug: 1117160
    Change-Id: Idc0faa6685b73ab4246a0544bdead23b1aeccef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2382872
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803980}

--- chrome/browser/ui/views/global_media_controls/media_notification_device_selector_view.h
+++ chrome/browser/ui/views/global_media_controls/media_notification_device_selector_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "chrome/browser/ui/global_media_controls/media_notification_device_provider.h"
+#include "chrome/browser/ui/views/location_bar/icon_label_bubble_view.h"
 #include "media/audio/audio_device_description.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/md_text_button.h"
@@ -15,12 +16,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 class DeviceEntryView;
+class ExpandDeviceSelectorButton;
 }  // anonymous namespace
 
 class MediaNotificationDeviceSelectorViewDelegate;
 
-class MediaNotificationDeviceSelectorView : public views::View,
-                                            public views::ButtonListener {
+class MediaNotificationDeviceSelectorView
+    : public views::View,
+      public views::ButtonListener,
+      public IconLabelBubbleView::Delegate {
  public:
   MediaNotificationDeviceSelectorView(
       MediaNotificationDeviceSelectorViewDelegate* delegate,
@@ -43,6 +47,11 @@ class MediaNotificationDeviceSelectorView : public views::View,
   // views::ButtonListener
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
+  // IconLabelBubbleView::Delegate
+  SkColor GetIconLabelBubbleSurroundingForegroundColor() const override;
+  SkColor GetIconLabelBubbleBackgroundColor() const override;
+
+  views::Button* get_expand_button_for_testing();
   static std::string get_entry_label_for_testing(views::View* entry_view);
   static bool get_entry_is_highlighted_for_testing(views::View* entry_view);
 
@@ -76,7 +85,7 @@ class MediaNotificationDeviceSelectorView : public views::View,
 
   // Child views
   views::View* expand_button_strip_;
-  views::LabelButton* expand_button_;
+  ExpandDeviceSelectorButton* expand_button_;
   views::View* audio_device_entries_container_;
 
   std::unique_ptr<MediaNotificationDeviceProvider::
