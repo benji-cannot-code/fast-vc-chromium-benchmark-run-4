8eab517a57dbe1d4d56a2b7d6243ecada00c5025
Oleh Lamzin <lamzin@google.com>
2020-09-02 09:51:05 +0000

    [Telemetry SWX] add probe service errors browser test
    Bug: b:158658869
    Change-Id: I03743e22ec8af9cea22aac92a86ed2cbe3906f4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2388441
    Reviewed-by: Mahmoud Gawad <mgawad@google.com>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/master@{#803808}

--- chromeos/components/telemetry_extension_ui/test/telemetry_extension_ui_browsertest.h
+++ chromeos/components/telemetry_extension_ui/test/telemetry_extension_ui_browsertest.h
@@ -25,6 +25,8 @@ class TelemetryExtensionUiBrowserTest : public SandboxedWebUiAppTestBase {
 
   void ConfigureDiagnosticsForInteractiveUpdate();
   void ConfigureDiagnosticsForNonInteractiveUpdate();
+
+  void ConfigureProbeServiceToReturnErrors();
 };
 
 #endif  // CHROMEOS_COMPONENTS_TELEMETRY_EXTENSION_UI_TEST_TELEMETRY_EXTENSION_UI_BROWSERTEST_H_
