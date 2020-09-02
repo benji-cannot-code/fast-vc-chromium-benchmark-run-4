561e5d5b7d6fed40ee01e3d43852f94910d923cd
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-09-02 14:07:33 +0000

    Import wpt@953dd708a465fc3f89a6c74812cc18b62bbfc724
    Using wpt-import in Chromium 8e596ab565abfa4510bb55561d11cd90257da300.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    npm@chromium.org, yoavweiss@chromium.org:
      external/wpt/navigation-timing
    
    NOAUTOREVERT=true
    TBR=lpz@google.com
    
    No-Export: true
    Change-Id: I0191abdacdb8b91236ef34ad6c022eb5bdc8df22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2390280
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#803846}

--- third_party/blink/web_tests/external/wpt/navigation-timing/idlharness.window.js
+++ third_party/blink/web_tests/external/wpt/navigation-timing/idlharness.window.js
@@ -7,24 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // http://www.w3.org/TR/navigation-timing/
 
-promise_test(async (t) => {
-  const srcs = ['navigation-timing', 'hr-time', 'resource-timing', 'performance-timeline', 'dom', 'html'];
-  const [navigationTiming, hrTime, resourceTiming, performanceTimeline, dom, html] =
-      await Promise.all(srcs.map(i => fetch(`/interfaces/${i}.idl`).then(r => r.text())));
-  const idlArray = new IdlArray();
-  idlArray.add_idls(hrTime);
-  idlArray.add_idls(navigationTiming);
-  idlArray.add_dependency_idls(resourceTiming);
-  idlArray.add_dependency_idls(performanceTimeline);
-  idlArray.add_dependency_idls(dom);
-  idlArray.add_dependency_idls(html);
-  idlArray.add_objects({
-    Performance: ['performance'],
-    PerformanceNavigation: ['performance.navigation'],
-    PerformanceTiming: ['performance.timing'],
-    PerformanceNavigationTiming: [
-      'performance.getEntriesByType("navigation")[0]'
-    ]
-  });
-  return idlArray.test();
-})
+idl_test(
+  ['navigation-timing', 'hr-time'],
+  ['resource-timing', 'performance-timeline', 'dom', 'html'],
+  idlArray => {
+    idlArray.add_objects({
+      Performance: ['performance'],
+      PerformanceNavigation: ['performance.navigation'],
+      PerformanceTiming: ['performance.timing'],
+      PerformanceNavigationTiming: ['performance.getEntriesByType("navigation")[0]']
+    });
+  }
+);
