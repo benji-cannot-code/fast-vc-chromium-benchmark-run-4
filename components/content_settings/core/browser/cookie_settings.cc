88beae926e7b00496b0d7965556b71774cd7121c
Christian Dullweber <dullweber@chromium.org>
2020-09-02 14:00:08 +0000

    Remove BlockThirdPartyCookies preference
    The boolean BlockThirdPartyCookies preference was replaced by
    CookieControlsMode enum. Existing settings were migrated since M83.
    Since all usage of the preference has been removed, the preference
    can be removed and existing settings cleared.
    
    Bug: 1104836
    Change-Id: Ie6e38b0a424981395c627459d684030ea84a58b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2387805
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803844}

--- components/content_settings/core/browser/cookie_settings.cc
+++ components/content_settings/core/browser/cookie_settings.cc
@@ -61,9 +61,6 @@ void CookieSettings::GetCookieSettings(
 
 void CookieSettings::RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
-  registry->RegisterBooleanPref(
-      prefs::kBlockThirdPartyCookies, false,
-      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterIntegerPref(
       prefs::kCookieControlsMode,
       static_cast<int>(CookieControlsMode::kIncognitoOnly),
@@ -241,7 +238,9 @@ void CookieSettings::GetCookieSettingInternal(
 
 CookieSettings::~CookieSettings() = default;
 
-bool CookieSettings::IsCookieControlsEnabled() {
+bool CookieSettings::ShouldBlockThirdPartyCookiesInternal() {
+  DCHECK(thread_checker_.CalledOnValidThread());
+
 #if defined(OS_IOS)
   if (!base::FeatureList::IsEnabled(kImprovedCookieControls))
     return false;
@@ -275,8 +274,7 @@ void CookieSettings::OnContentSettingChanged(
 void CookieSettings::OnCookiePreferencesChanged() {
   DCHECK(thread_checker_.CalledOnValidThread());
 
-  bool new_block_third_party_cookies =
-      IsCookieControlsEnabled();
+  bool new_block_third_party_cookies = ShouldBlockThirdPartyCookiesInternal();
 
   // Safe to read |block_third_party_cookies_| without locking here because the
   // only place that writes to it is this method and it will always be run on
