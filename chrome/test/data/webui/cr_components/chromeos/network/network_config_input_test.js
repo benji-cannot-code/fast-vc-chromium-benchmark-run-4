49a2c70cd879bb268b6e0d26f09ba678c916cb5f
James Hawkins <jhawkins@chromium.org>
2020-09-02 18:39:34 +0000

    cr_components/network: Convert NetworkConfigInput to Polymer 3.
    R=khorimoto@chromium.org
    
    Bug: 1045266
    Test: NetworkComponentsNetworkConfigInputTest*
    Change-Id: I2cd588e70a0f7dbb92e4550ee7f0365368abbbb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2388988
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803938}

---
+++ chrome/test/data/webui/cr_components/chromeos/network/network_config_input_test.js
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// clang-format off
+// #import 'chrome://resources/cr_components/chromeos/network/network_config_input.m.js';
+
+// #import {keyEventOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
+// #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// clang-format on
+
+suite('NetworkConfigInputTest', function() {
+  /** @type {!NetworkConfigInput|undefined} */
+  let configInput;
+
+  setup(function() {
+    configInput = document.createElement('network-config-input');
+    document.body.appendChild(configInput);
+    Polymer.dom.flush();
+  });
+
+  test('Enter key propagates up enter event', function(done) {
+    // Set up a listener to assert the 'enter' event fired.
+    configInput.addEventListener('enter', function() {
+      done();
+    });
+
+    // Simulate pressing enter on the input element.
+    const inputEl = configInput.$$('cr-input');
+    MockInteractions.keyEventOn(
+        inputEl, 'keypress', /* keyCode */ 13, /* modifiers */ undefined,
+        'Enter');
+  });
+});
