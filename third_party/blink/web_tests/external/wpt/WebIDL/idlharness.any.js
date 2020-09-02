56a0da07df41c503f0af52f063c8b1a567f7571b
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-09-02 01:28:53 +0000

    Import wpt@f135c6d792d0d5a50a8bf948f2eaf19a96d48787
    Using wpt-import in Chromium 1a887adfdfc9a9cb51ae4c56e03040aaa9507bf1.
    With Chromium commits locally applied on WPT:
    3a85024b19 "A column-spanning legend isn't part of the flow thread."
    8e73193556 "ContentVisibility: Make the lifecycle tasks run only on full lifecycle."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    binji@chromium.org:
      external/wpt/wasm
    mkwst@chromium.org:
      external/wpt/secure-contexts
    
    NOAUTOREVERT=true
    TBR=lpz@google.com
    
    No-Export: true
    Change-Id: I68772fa15fa85a0980042fcdabad85fd03f458e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2388605
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#803723}

---
+++ third_party/blink/web_tests/external/wpt/WebIDL/idlharness.any.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+"use strict";
+
+idl_test(
+  ['WebIDL'],
+  [],
+  idl_array => {
+    idl_array.add_objects({
+      DOMException: ['new DOMException()',
+                     'new DOMException("my message")',
+                     'new DOMException("my message", "myName")']
+    });
+  }
+);
