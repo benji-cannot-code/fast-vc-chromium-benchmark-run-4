17c89c71f5ab84251f36d1e12ce2970302b3e467
Joel Riley <joelriley@google.com>
2020-09-02 17:35:37 +0000

    Include SVG g element in AX tree on Chrome OS.
    Do not ignore SVG grouping elements in the accessibility tree on ChromeOS, so Select-to-speak can properly order SVG elements when reading them aloud.
    
    AX-Relnotes: SVG grouping elements will now appear in the accessibility tree on ChromeOS.
    
    Bug: 892822
    Change-Id: I50226cef7d5d19f4c9326a10bb48c8828c16cabf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2375989
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Joel Riley <joelriley@google.com>
    Cr-Commit-Position: refs/heads/master@{#803908}

--- third_party/blink/renderer/modules/accessibility/ax_layout_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_layout_object.cc
@@ -86,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/style/computed_style_constants.h"
 #include "third_party/blink/renderer/core/svg/graphics/svg_image.h"
 #include "third_party/blink/renderer/core/svg/svg_document_extensions.h"
+#include "third_party/blink/renderer/core/svg/svg_g_element.h"
 #include "third_party/blink/renderer/core/svg/svg_svg_element.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_image_map_link.h"
 #include "third_party/blink/renderer/modules/accessibility/ax_inline_text_box.h"
@@ -745,6 +746,14 @@ bool AXLayoutObject::ComputeAccessibilityIsIgnored(
     if (ignored_reasons)
       ignored_reasons->push_back(IgnoredReason(kAXUninteresting));
     return true;
+  }
+
+  // If setting enabled, do not ignore SVG grouping (<g>) elements.
+  if (IsA<SVGGElement>(GetNode())) {
+    Settings* settings = GetDocument()->GetSettings();
+    if (settings->GetAccessibilityIncludeSvgGElement()) {
+      return false;
+    }
   }
 
   // By default, objects should be ignored so that the AX hierarchy is not
