16deaa28d505ac2e6cda5b96c1df577f5b7f8e2e
Ramin Halavati <rhalavati@chromium.org>
2020-09-02 18:08:15 +0000

    Update OTR Profile API in Window AppleScript.
    Profile API is being updated (issue 1033903) and the usage of
    GetOffTheRecordProfile and HasOffTheRecordProfile without argument is
    getting deprecated. Use cases of this call that ONLY refer to incognito
    or guest mode can use [Get/Has]PrimaryOTRProfile() instead.
    
    This CL does not change any behavior and is just code clean up.
    
    Bug: 1060940
    Change-Id: Ie155364573e060093d3fb67bd2a163467346fe49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2388283
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#803921}

--- chrome/browser/ui/cocoa/applescript/window_applescript.mm
+++ chrome/browser/ui/cocoa/applescript/window_applescript.mm
@@ -56,7 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   Profile* profile;
   if ([mode isEqualToString:AppleScript::kIncognitoWindowMode]) {
-    profile = lastProfile->GetOffTheRecordProfile();
+    profile = lastProfile->GetPrimaryOTRProfile();
   }
   else if ([mode isEqualToString:AppleScript::kNormalWindowMode] || !mode) {
     profile = lastProfile;
