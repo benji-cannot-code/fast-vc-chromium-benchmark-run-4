384159a966972df640d93a0367b7c7b121764b06
Ryan Sturm <ryansturm@chromium.org>
2020-09-03 03:53:49 +0000

    Enabling PreconnectToSearch by default on Android
    Bug: 1032153
    Change-Id: I6202ac7ece548ba8a5f63ef36a2151fc9cd7e3ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2391584
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804041}

--- chrome/browser/navigation_predictor/search_engine_preconnector.cc
+++ chrome/browser/navigation_predictor/search_engine_preconnector.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "chrome/browser/predictors/loading_predictor.h"
 #include "chrome/browser/predictors/loading_predictor_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -21,8 +22,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 // Feature to control preconnect to search.
-const base::Feature kPreconnectToSearch{"PreconnectToSearch",
-                                        base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kPreconnectToSearch {
+  "PreconnectToSearch",
+
+#if defined(OS_ANDROID)
+      base::FEATURE_ENABLED_BY_DEFAULT
+#else
+      base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+};
 
 // Feature to limit experimentation to Google search only.
 const base::Feature kPreconnectToSearchNonGoogle{
