cb4875e23def852f3f7d09e0e31ae87ce5b9f2b7
Elly Fong-Jones <ellyjones@chromium.org>
2020-09-03 19:32:07 +0000

    cbui: never show DownloadInProgressDialogView as system modal
    We've decided to abandon system modality on non-ChromeOS platforms, so
    always use window modality for this dialog instead. Some minor cleanups
    of DownloadInProgressDialogView are included in this change.
    
    Bug: 1121132
    Change-Id: Ia1e5efaa63a38a0f97608253ee688b08b8a86a8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2377989
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804358}

--- chrome/browser/ui/views/download/download_in_progress_dialog_view_unittest.cc
+++ chrome/browser/ui/views/download/download_in_progress_dialog_view_unittest.cc
@@ -29,7 +29,7 @@ TEST_F(DownloadInProgressDialogTest, CallbackIsRunOnClose) {
                                        "DownloadInProgressDialogView");
   DownloadInProgressDialogView::Show(
       parent->GetNativeWindow(), 1,
-      Browser::DownloadCloseType::kBrowserShutdown, false,
+      Browser::DownloadCloseType::kBrowserShutdown,
       base::BindLambdaForTesting([&](bool b) { result = b; }));
   waiter.WaitIfNeededAndGet()->Close();
 
--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -431,7 +431,6 @@ class BrowserView : public BrowserWindow,
   void ConfirmBrowserCloseWithPendingDownloads(
       int download_count,
       Browser::DownloadCloseType dialog_type,
-      bool app_modal,
       const base::Callback<void(bool)>& callback) override;
   void UserChangedTheme(BrowserThemeChangeType theme_change_type) override;
   void ShowAppMenu() override;
