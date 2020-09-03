f4049fc13dffe8873296622020da0e0b03999e14
Moe Ahmadi <mahmadi@chromium.org>
2020-09-03 05:26:36 +0000

    [omnibox] Adds support for trending match icon on Desktop
    - Displays trending icon for non-historical matches with SUBTYPE_TRENDS.
    - Adds Vector and SVG icons for use in Omnibox and Realbox respectively.
    
    The SVG was optimized using svgomg and converted to .icon using skiafy.
    
    Omnibox: https://screenshot.googleplex.com/7iRhFnNGXP2Kh4j
    Realbox: https://screenshot.googleplex.com/FjvWaT4DXonaFWz
    
    Bug: 1122669
    Change-Id: Ic9fd6594e633128eed0c2ff621509acdddf100f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2386096
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804097}

--- chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
@@ -208,7 +208,8 @@ content::WebUIDataSource* CreateNewTabPageUiHtmlSource(Profile* profile) {
       {omnibox::kExtensionAppIconResourceName,
        IDR_LOCAL_NTP_ICONS_EXTENSION_APP},
       {omnibox::kPageIconResourceName, IDR_LOCAL_NTP_ICONS_PAGE},
-      {omnibox::kSearchIconResourceName, IDR_WEBUI_IMAGES_ICON_SEARCH}};
+      {omnibox::kSearchIconResourceName, IDR_WEBUI_IMAGES_ICON_SEARCH},
+      {omnibox::kTrendingUpIconResourceName, IDR_LOCAL_NTP_ICONS_TRENDING_UP}};
   webui::AddResourcePathsBulk(source, kImages);
 
 #if BUILDFLAG(ENABLE_KALEIDOSCOPE)
