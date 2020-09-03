91cb96f5da52cc4804fb6dd873ef2f51e13ede6f
Ahmed Mehfooz <amehfooz@chromium.org>
2020-09-03 03:54:28 +0000

    Add screenshots skeleton code in holding space tray
    Bug: 1124145
    Change-Id: I620444d44648a2b1d408f12a1b9137c23012e72c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2388859
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804042}

---
+++ ash/system/holding_space/holding_space_screenshot_view.h
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_HOLDING_SPACE_HOLDING_SPACE_SCREENSHOT_VIEW_H_
+#define ASH_SYSTEM_HOLDING_SPACE_HOLDING_SPACE_SCREENSHOT_VIEW_H_
+
+#include "ash/ash_export.h"
+#include "ui/views/view.h"
+
+namespace ash {
+
+class HoldingSpaceItem;
+
+namespace tray {
+class RoundedImageView;
+}  // namespace tray
+
+class ASH_EXPORT HoldingSpaceScreenshotView : public views::View {
+ public:
+  explicit HoldingSpaceScreenshotView(const HoldingSpaceItem* item);
+  HoldingSpaceScreenshotView(const HoldingSpaceScreenshotView&) = delete;
+  HoldingSpaceScreenshotView& operator=(const HoldingSpaceScreenshotView&) =
+      delete;
+  ~HoldingSpaceScreenshotView() override;
+
+  // views::View:
+  const char* GetClassName() const override;
+
+ private:
+  void Update();
+
+  const HoldingSpaceItem* const item_;
+  tray::RoundedImageView* image_ = nullptr;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_HOLDING_SPACE_HOLDING_SPACE_SCREENSHOT_VIEW_H_
