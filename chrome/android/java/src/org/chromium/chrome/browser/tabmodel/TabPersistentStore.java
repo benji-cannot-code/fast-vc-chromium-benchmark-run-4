0990a9bfb2530c198de2dd29af93b7883bb9f278
David Maunder <davidjm@chromium.org>
2020-09-03 05:17:48 +0000

    Add histograms for TabState and CriticalPersistedTabData comparison
    CriticalPersistedTabData is a replacement for TabState which usess
    protocol buffers to serialize persisted Tab metadata in a file. It
    is imperative we compare the old and the new methods of persisting
    Tab metadata across restarts.
    
    Bug: 1122220
    Change-Id: I649fe8271c1301c76e9a7c9bb317ab03c30b2fa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2380603
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804081}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
@@ -65,6 +65,7 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 /**
@@ -1338,11 +1339,13 @@ public class TabPersistentStore extends TabPersister {
         // TabState and CriticalPersistedTabData are acquired asynchronously and Tab
         // restoration continues when both have been returned
         private int mRemaining = isCriticalPersistedTabDataEnabled() ? 2 : 1;
+        private long mStartTime;
 
         public LoadTabTask(TabRestoreDetails tabToRestore) {
             mTabToRestore = tabToRestore;
             TraceEvent.startAsync("LoadTabTask", mTabToRestore.id);
             TraceEvent.startAsync("LoadTabState", mTabToRestore.id);
+            mStartTime = SystemClock.elapsedRealtime();
             // TODO(crbug.com/1119455) decouple CriticalPersistedTabData from
             // LoadTabTask
             if (isCriticalPersistedTabDataEnabled()) {
@@ -1351,6 +1354,11 @@ public class TabPersistentStore extends TabPersister {
                         tabToRestore.id, tabToRestore.isIncognito, (res) -> {
                             TraceEvent.finishAsync(
                                     "LoadCriticalPersistedTabData", mTabToRestore.id);
+                            RecordHistogram.recordTimesHistogram(
+                                    String.format(Locale.US,
+                                            "Tabs.SavedTabLoadTime.CriticalPersistedTabData.%s",
+                                            res == null ? "Null" : "Exists"),
+                                    SystemClock.elapsedRealtime() - mStartTime);
                             mSerializedCriticalPersistedTabData = res;
                             mRemaining--;
                             if (mRemaining == 0) {
@@ -1374,6 +1382,10 @@ public class TabPersistentStore extends TabPersister {
         @Override
         protected void onPostExecute(TabState tabState) {
             TraceEvent.finishAsync("LoadTabState", mTabToRestore.id);
+            RecordHistogram.recordTimesHistogram(
+                    String.format(Locale.US, "Tabs.SavedTabLoadTime.TabState.%s",
+                            tabState == null ? "Null" : "Exists"),
+                    SystemClock.elapsedRealtime() - mStartTime);
             mTabState = tabState;
             mRemaining--;
             if (mRemaining == 0) {
