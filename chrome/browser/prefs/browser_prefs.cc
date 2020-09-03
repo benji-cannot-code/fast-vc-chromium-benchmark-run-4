82f91674e863d11df7716d72fe98ee7205f80abb
Owen Min <zmin@chromium.org>
2020-09-03 21:55:37 +0000

    Delete unused enterprise reporting pref
    Those prefs values are set by policies only. |kCloudReportingEnabled|
    was moved to local_state a while ago while the other policies are never
    launched.
    
    Bug: 1107440
    Change-Id: Ie0c9ff83ac4ef5e9f950a4d446c16f687fbffac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360570
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804431}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -186,8 +186,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/system_proxy_manager.h"
 #include "chrome/browser/chromeos/settings/stats_reporting_controller.h"
 #include "chrome/browser/component_updater/metadata_table_chromeos.h"
-#else
-#include "chrome/browser/extensions/api/enterprise_reporting_private/prefs.h"
 #endif  // defined(OS_CHROMEOS)
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
@@ -1131,10 +1129,6 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
 
 #if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
   default_apps::RegisterProfilePrefs(registry);
-#endif
-
-#if !defined(OS_CHROMEOS) && BUILDFLAG(ENABLE_EXTENSIONS)
-  extensions::enterprise_reporting::RegisterProfilePrefs(registry);
 #endif
 
 #if defined(TOOLKIT_VIEWS)
