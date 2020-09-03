ca85d97ca9526e72db6bb7ff238a61cce8c92b97
Sean McAllister <smcallis@google.com>
2020-09-03 15:35:53 +0000

    Refactor OS_LINUX preprocessor directive for LaCrOS effort.
    Currently, ChromeOS defines the OS_LINUX directive as well as
    OS_CHROMEOS.  We're working to separate these two, so we're
    making the fact that OS_LINUX == OS_LINUX || OS_CHROMEOS
    explicit.
    
    This is changes for /services/video_capture
    
    This CL was uploaded by git cl split.
    
    R=chfremer@chromium.org
    
    Bug: 1110266
    Change-Id: I40bf9a0a66648430358fb0de198bd648c52a1324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2370733
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Sean McAllister <smcallis@google.com>
    Cr-Commit-Position: refs/heads/master@{#804248}

--- services/video_capture/test/fake_device_unittest.cc
+++ services/video_capture/test/fake_device_unittest.cc
@@ -160,7 +160,7 @@ TEST_F(FakeVideoCaptureDeviceTest, BuffersGetRetiredWhenDeviceIsStopped) {
 
 // This requires the linux platform, where shared regions are backed by a file
 // descriptor.
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
 TEST_F(FakeVideoCaptureDeviceTest,
        ReceiveFramesViaFileDescriptorHandlesForSharedMemory) {
   base::RunLoop wait_loop;
@@ -232,6 +232,6 @@ TEST_F(FakeVideoCaptureDeviceTest,
   wait_loop.Run();
   EXPECT_FALSE(found_unexpected_all_zero_frame);
 }
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_CHROMEOS)
 
 }  // namespace video_capture
