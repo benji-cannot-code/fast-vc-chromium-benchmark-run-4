a609ca4bd94b31871e4a2c0ffc9f45b43e359955
Matt Wolenetz <wolenetz@chromium.org>
2020-09-03 22:04:07 +0000

    MSE-in-Workers: Add modules-specific attachment interface
    Adds a supplemental interface in modules to extend the core
    MediaSourceAttachment, so common methods in specialized attachments can
    be defined without requiring them known by core. This reduces complexity
    of the core side of the attachment interface.
    
    Specifically, adds MediaSourceAttachmentSupplement, with a new method
    (implemented in concrete SameThreadMediaSourceAttachment) to notify the
    attached element that the duration of the resource has changed. Later
    specialization in a cross-thread version of the attachment will need to
    perform this notification asynchronously.
    
    Lifts the Unregister() method and the persistent MediaSource OilPan
    reference member from the concrete implementation into this common
    modules attachment supplement's implementation, since these are expected
    to remain the same across all concrete attachment implementations.
    
    Updates SameThreadMediaSourceAttachment to give the MediaSource a
    thread-safe refcounted scoped_refptr for the attachment during
    StartAttachingToMediaElement(). MediaSource, like HTMLMediaElement does
    already, is updated to know both the attachment and the
    MediaSourceTracer while the attachment is active.
    
    Starts using NotifyDurationChanged from the MediaSource object via the
    attachment instead of directly talking to the attached media element
    from the MediaSource.
    
    Further extensions of this attachment interface will come in later
    changes for similar scenarios, eventually enabling the dropping of the
    |attached_element_| member from the MediaSource class, with all
    interactions on the element eventually done via the attachment.
    
    BUG=878133
    
    Change-Id: I86206f740b62f9ba6dc6a1b9fd16a44aff21f49a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2387121
    Reviewed-by: Will Cassella <cassew@google.com>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804434}

--- third_party/blink/renderer/modules/mediasource/same_thread_media_source_attachment.cc
+++ third_party/blink/renderer/modules/mediasource/same_thread_media_source_attachment.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/mediasource/same_thread_media_source_attachment.h"
 
+#include "base/memory/scoped_refptr.h"
+#include "third_party/blink/renderer/core/html/media/html_media_element.h"
 #include "third_party/blink/renderer/modules/mediasource/media_source.h"
 #include "third_party/blink/renderer/modules/mediasource/media_source_tracer_impl.h"
 
@@ -21,13 +23,17 @@ blink::MediaSource* GetMediaSource(blink::MediaSourceTracer* tracer) {
   return GetTracerImpl(tracer)->GetMediaSource();
 }
 
+blink::HTMLMediaElement* GetMediaElement(blink::MediaSourceTracer* tracer) {
+  return GetTracerImpl(tracer)->GetMediaElement();
+}
+
 }  // namespace
 
 namespace blink {
 
 SameThreadMediaSourceAttachment::SameThreadMediaSourceAttachment(
     MediaSource* media_source)
-    : registered_media_source_(media_source) {
+    : MediaSourceAttachmentSupplement(media_source) {
   // This kind of attachment only operates on the main thread.
   DCHECK(IsMainThread());
 
@@ -38,20 +44,17 @@ SameThreadMediaSourceAttachment::SameThreadMediaSourceAttachment(
   DCHECK(HasOneRef());
 }
 
-SameThreadMediaSourceAttachment::~SameThreadMediaSourceAttachment() = default;
-
-void SameThreadMediaSourceAttachment::Unregister() {
+SameThreadMediaSourceAttachment::~SameThreadMediaSourceAttachment() {
   DVLOG(1) << __func__ << " this=" << this;
+}
 
-  // The only expected caller is a MediaSourceRegistryImpl on the main thread.
-  DCHECK(IsMainThread());
+void SameThreadMediaSourceAttachment::NotifyDurationChanged(
+    MediaSourceTracer* tracer,
+    double duration) {
+  HTMLMediaElement* element = GetMediaElement(tracer);
 
-  // Release our strong reference to the MediaSource. Note that revokeObjectURL
-  // of the url associated with this attachment could commonly follow this path
-  // while the MediaSource (and any attachment to an HTMLMediaElement) may still
-  // be alive/active.
-  DCHECK(registered_media_source_);
-  registered_media_source_ = nullptr;
+  bool request_seek = element->currentTime() > duration;
+  element->DurationChanged(duration, request_seek);
 }
 
 MediaSourceTracer*
@@ -60,7 +63,8 @@ SameThreadMediaSourceAttachment::StartAttachingToMediaElement(
   if (!registered_media_source_)
     return nullptr;
 
-  return registered_media_source_->StartAttachingToMediaElement(element);
+  return registered_media_source_->StartAttachingToMediaElement(
+      WrapRefCounted(this), element);
 }
 
 void SameThreadMediaSourceAttachment::CompleteAttachingToMediaElement(
