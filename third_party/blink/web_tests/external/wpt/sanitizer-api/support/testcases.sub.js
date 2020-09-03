85a6f01373cbb93d7d42477c47250bb6e374e1c1
Yifan Luo <lyf@google.com>
2020-09-03 18:11:36 +0000

    [Sanitizer API] Add wpt tests.
    Bug: 1116418
    Change-Id: I33bd57b176c9c603d60b83ca0e020aa5f5b4abff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2390910
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804309}

---
+++ third_party/blink/web_tests/external/wpt/sanitizer-api/support/testcases.sub.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const testcases = [
+  {value: "test", result: "test", message: "string"},
+  {value: "<b>bla</b>", result: "<b>bla</b>", message: "html fragment"},
+  {value: "<a<embla", result: "", message: "broken html"},
+  {value: {}, result: "[object Object]", message: "empty object"},
+  {value: 1, result: "1", message: "number"},
+  {value: 000, result: "0", message: "zeros"},
+  {value: 1+2, result: "3", message: "arithmetic"},
+  {value: "", result: "", message: "empty string"},
+  {value: undefined, result: "undefined", message: "undefined"},
+  {value: null, result: "null", message: "null"},
+  {value: "<html><head></head><body>test</body></html>", result: "test", message: "document"},
+  {value: "<div>test", result: "<div>test</div>", message: "html without close tag"},
+  {value: "<script>alert('i am a test')<\/script>", result: "", message: "scripts"},
+  {value: "<p onclick='a= 123'>Click.</p>", result: "<p>Click.</p>", message: "onclick scripts"}
+
+];
