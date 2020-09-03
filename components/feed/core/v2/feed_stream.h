f83363395503e556a225dc7d0b75f37dafdf2711
Roger McFarlane <rogerm@chromium.org>
2020-09-03 22:36:46 +0000

    Enable Feed VE logging only for signed-in+WAA-on users.
    This CL plumbs a WAA on flag from the feed response metadata, through
    the FeedStore and FeedStreamApi and across xsurface. It also moves the
    point in time where the session info is fetched to ensure the correct
    info is used when logging.
    
    Bug: 1122882, 1122175
    Change-Id: I1be7c2fdf9587638187da37346646bc1e9912899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2381173
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804448}

--- components/feed/core/v2/feed_stream.h
+++ components/feed/core/v2/feed_stream.h
@@ -80,6 +80,7 @@ class FeedStream : public FeedStreamApi,
     virtual RefreshResponseData TranslateWireResponse(
         feedwire::Response response,
         StreamModelUpdateRequest::Source source,
+        bool was_signed_in_request,
         base::Time current_time) const;
   };
 
@@ -122,6 +123,7 @@ class FeedStream : public FeedStreamApi,
 
   // FeedStreamApi.
 
+  bool IsActivityLoggingEnabled() const override;
   void AttachSurface(SurfaceInterface*) override;
   void DetachSurface(SurfaceInterface*) override;
   void SetArticlesListVisible(bool is_visible) override;
@@ -273,6 +275,9 @@ class FeedStream : public FeedStreamApi,
     return weak_ptr_factory_.GetWeakPtr();
   }
 
+  // Re-evaluate whether or not activity logging should currently be enabled.
+  void UpdateIsActivityLoggingEnabled();
+
   void GetPrefetchSuggestions(
       base::OnceCallback<void(std::vector<offline_pages::PrefetchSuggestion>)>
           suggestions_callback);
@@ -328,6 +333,7 @@ class FeedStream : public FeedStreamApi,
   std::vector<base::OnceCallback<void(bool)>> load_more_complete_callbacks_;
   Metadata metadata_;
   int unload_on_detach_sequence_number_ = 0;
+  bool is_activity_logging_enabled_ = false;
 
   // To allow tests to wait on task queue idle.
   base::RepeatingClosure idle_callback_;
