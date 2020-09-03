436ab87345d7421f8840097de10591ceb2666a98
David Tseng <dtseng@chromium.org>
2020-09-03 23:26:51 +0000

    Add four finger swipe gesture commands to move from pane to pane
    See go/chromevox-touch-gestures
    
    R=akihiroota@chromium.org
    
    AX-Relnotes: Adds four finger swipe for ChromeVox to move among on-screen panes (launcher, status tray, web page, etc).
    Bug: 1124454
    Test: browser_tests --gtest_filter=ChromeVox*.SwipeLeftRight*
    Change-Id: Ie8a98c53d456e3a2e632f4307622494f8f82f3e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379458
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804466}

--- chrome/browser/ui/ash/accessibility/fake_accessibility_controller.cc
+++ chrome/browser/ui/ash/accessibility/fake_accessibility_controller.cc
@@ -62,6 +62,9 @@ base::string16 FakeAccessibilityController::GetBatteryDescription() const {
 
 void FakeAccessibilityController::SetVirtualKeyboardVisible(bool is_visible) {}
 
+void FakeAccessibilityController::PerformAcceleratorAction(
+    ash::AcceleratorAction accelerator_action) {}
+
 void FakeAccessibilityController::NotifyAccessibilityStatusChanged() {}
 
 bool FakeAccessibilityController::IsAccessibilityFeatureVisibleInTrayMenu(
