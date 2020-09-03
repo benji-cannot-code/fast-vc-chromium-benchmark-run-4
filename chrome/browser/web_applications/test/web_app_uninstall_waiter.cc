a660c00f2dd1839f345336a8f9f1a78cf9b1a82a
Alan Cutter <alancutter@chromium.org>
2020-09-03 13:22:05 +0000

    Fix default web app migration failing after revert
    The default web app migration process guards its uninstall_and_replace
    logic on whether the web app has been installed before. Instead it
    should check whether the app to migrate exists or not.
    
    Without this CL the default web app would not perform the migration if
    it had previously been installed then reverted.
    
    Bug: 1104696
    Change-Id: Iedc8752a9824e85080d227c818307dbbfdd5248b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2389740
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804206}

---
+++ chrome/browser/web_applications/test/web_app_uninstall_waiter.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/web_applications/test/web_app_uninstall_waiter.h"
+
+#include "chrome/browser/web_applications/components/web_app_provider_base.h"
+
+namespace web_app {
+
+WebAppUninstallWaiter::WebAppUninstallWaiter(Profile* profile, AppId app_id)
+    : app_id_(std::move(app_id)) {
+  observer_.Add(&WebAppProviderBase::GetProviderBase(profile)->registrar());
+}
+WebAppUninstallWaiter::~WebAppUninstallWaiter() = default;
+
+void WebAppUninstallWaiter::Wait() {
+  run_loop_.Run();
+}
+
+void WebAppUninstallWaiter::OnWebAppUninstalled(const AppId& app_id) {
+  if (app_id == app_id_)
+    run_loop_.Quit();
+}
+
+}  // namespace web_app
