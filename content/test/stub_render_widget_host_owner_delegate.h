ae1b55b6baad96c3e94980abcc54ab2b44bcc88d
danakj <danakj@chromium.org>
2020-09-03 16:37:57 +0000

    Remove dead RenderWidgetHostViewEventHandler::ShowContextMenu()
    R=avi@chromium.org
    
    Change-Id: Ia1beb8f5e22c53e38803f44302785d63abe82363
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2391728
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804264}

--- content/test/stub_render_widget_host_owner_delegate.h
+++ content/test/stub_render_widget_host_owner_delegate.h
@@ -27,8 +27,6 @@ class StubRenderWidgetHostOwnerDelegate : public RenderWidgetHostOwnerDelegate {
   bool IsMainFrameActive() override;
   bool IsNeverComposited() override;
   WebPreferences GetWebkitPreferencesForWidget() override;
-  void ShowContextMenu(RenderFrameHost* render_frame_host,
-                       const ContextMenuParams& params) override {}
 };
 
 }  // namespace content
