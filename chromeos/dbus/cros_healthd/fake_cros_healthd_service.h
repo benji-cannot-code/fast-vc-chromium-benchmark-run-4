cfd9d5c41aec14fb39d19de53936b4828824101f
Paul Moy <pmoy@chromium.org>
2020-09-03 05:20:02 +0000

    ServiceConnection: Add battery charge routine
    Expose a new routine through cros_healthd's ServiceConnection.
    
    Bug: chromium:1094018
    Change-Id: I25d2b2e1eb6652d127dcfb50925e75950423b6b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2388432
    Commit-Queue: Paul Moy <pmoy@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804085}

--- chromeos/dbus/cros_healthd/fake_cros_healthd_service.h
+++ chromeos/dbus/cros_healthd/fake_cros_healthd_service.h
@@ -94,6 +94,10 @@ class FakeCrosHealthdService final
       uint32_t length_seconds,
       uint32_t maximum_discharge_percent_allowed,
       RunBatteryDischargeRoutineCallback callback) override;
+  void RunBatteryChargeRoutine(
+      uint32_t length_seconds,
+      uint32_t minimum_charge_percent_required,
+      RunBatteryChargeRoutineCallback callback) override;
 
   // CrosHealthdEventService overrides:
   void AddBluetoothObserver(
