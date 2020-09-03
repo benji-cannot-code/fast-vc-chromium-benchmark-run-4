87e62bf9afc759525dd442e8e4c5138ae2d85ab7
edchin <edchin@chromium.org>
2020-09-03 19:31:45 +0000

    [ios] Add ScreenTime experiment flag
    Bug: 1123704
    Change-Id: Ifb025407075376c7046c79e361bdc4c690db905d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2386843
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804357}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/fullscreen/fullscreen_features.h"
 #import "ios/chrome/browser/ui/infobars/infobar_feature.h"
 #import "ios/chrome/browser/ui/page_info/features.h"
+#include "ios/chrome/browser/ui/screen_time/features.h"
 #include "ios/chrome/browser/ui/settings/autofill/features.h"
 #import "ios/chrome/browser/ui/tab_grid/features.h"
 #import "ios/chrome/browser/ui/table_view/feature_flags.h"
@@ -683,6 +684,10 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableCloseAllTabsConfirmationName,
      flag_descriptions::kEnableCloseAllTabsConfirmationDescription,
      flags_ui::kOsIos, FEATURE_VALUE_TYPE(kEnableCloseAllTabsConfirmation)},
+    {"screen-time-integration-ios",
+     flag_descriptions::kScreenTimeIntegrationName,
+     flag_descriptions::kScreenTimeIntegrationDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kScreenTimeIntegration)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
