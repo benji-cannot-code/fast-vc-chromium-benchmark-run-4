ecb213fa7bc6245aae9f4d6242e6462126536346
Clovis PJ <clovispj@google.com>
2020-09-03 19:44:57 +0000

    Generate MojoLPM protobufs from src/
    Removing mojolpm.proto copy. Moving generated C++
    into fully qualified location.
    
    Change-Id: I27e9a22002506862780258294c4688e5935516d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2388440
    Commit-Queue: Clovis PJ <clovispj@google.com>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804366}

--- mojo/public/tools/fuzzers/mojolpm.h
+++ mojo/public/tools/fuzzers/mojolpm.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/platform/platform_handle.h"
 #include "mojo/public/cpp/system/core.h"
 #include "mojo/public/cpp/system/data_pipe.h"
-#include "mojolpm.pb.h"
+#include "mojo/public/tools/fuzzers/mojolpm.pb.h"
 
 #define MOJOLPM_DBG 0
 #if MOJOLPM_DBG
