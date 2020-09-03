08acae8c30962026e5097dbc3f649fc5978b70b0
Hidehiko Abe <hidehiko@chromium.org>
2020-09-03 18:10:33 +0000

    lacros: Migrate chromeos/chromeos_buildflags.h to build/chromeos_buildflags.h
    This CL unifies those two buildflags file for Chrome OS.
    
    BUG=1114198
    TEST=Build locally. Ran tryjob.
    
    Change-Id: Id7343da06ac6b17cb20cb20babdd8ab06ac40028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2377525
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804308}

--- chromeos/components/run_all_unittests.cc
+++ chromeos/components/run_all_unittests.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
-#include "chromeos/chromeos_buildflags.h"
+#include "build/chromeos_buildflags.h"
 #include "mojo/core/embedder/embedder.h"
 
 #if BUILDFLAG(IS_CHROMEOS_DEVICE)
--- ui/chromeos/run_all_unittests.cc
+++ ui/chromeos/run_all_unittests.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
-#include "chromeos/chromeos_buildflags.h"
+#include "build/chromeos_buildflags.h"
 #include "mojo/core/embedder/embedder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/env.h"
