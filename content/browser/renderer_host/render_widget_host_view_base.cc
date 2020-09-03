dd4c035f980162dd33dc49457f04fe19e86a72c8
danakj <danakj@chromium.org>
2020-09-03 19:25:59 +0000

    Call the BrowserAccessibilityManager from RenderWidgetHostImpl
    There's a weird split of responsibility between RenderWidgetHostImpl
    and RenderWidgetHostViewBase, both of which are used for all platforms.
    This merges some responsibility up to RenderWidgetHostImpl for the
    OnRenderFrameMetadataChangedAfterActivation() event.
    
    R=avi@chromium.org
    
    Change-Id: I2349391493b1bb1e71393c5a7bb34e931d650527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2392620
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804352}

--- content/browser/renderer_host/render_widget_host_view_base.cc
+++ content/browser/renderer_host/render_widget_host_view_base.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/viz/common/features.h"
 #include "components/viz/host/host_frame_sink_manager.h"
-#include "content/browser/accessibility/browser_accessibility_manager.h"
 #include "content/browser/compositor/surface_utils.h"
 #include "content/browser/gpu/gpu_data_manager_impl.h"
 #include "content/browser/renderer_host/delegated_frame_host.h"
@@ -121,11 +120,6 @@ void RenderWidgetHostViewBase::OnRenderFrameMetadataChangedBeforeActivation(
 void RenderWidgetHostViewBase::OnRenderFrameMetadataChangedAfterActivation() {
   const cc::RenderFrameMetadata& metadata =
       host()->render_frame_metadata_provider()->LastRenderFrameMetadata();
-
-  BrowserAccessibilityManager* manager =
-      host()->GetRootBrowserAccessibilityManager();
-  if (manager)
-    manager->SetPageScaleFactor(metadata.page_scale_factor);
 
   is_drawing_delegated_ink_trails_ = metadata.has_delegated_ink_metadata;
 }
