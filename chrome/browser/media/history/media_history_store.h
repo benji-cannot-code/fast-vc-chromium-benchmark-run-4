4cc5e0a4327266b18d42c5ead677ae6aa254d7a2
Becca Hughes <beccahughes@chromium.org>
2020-09-03 17:22:41 +0000

    Add caching for frameless code
    Adds caching for the frameless code. Will store the
    data in Media History and will clear every 24 hours
    or if the user signs in or out.
    
    BUG=1121360
    
    Change-Id: I0ae9112cbd5dc272158f2e369d8807da02f0e2b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2375701
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804285}

--- chrome/browser/media/history/media_history_store.h
+++ chrome/browser/media/history/media_history_store.h
@@ -43,6 +43,7 @@ class MediaHistorySessionImagesTable;
 class MediaHistoryImagesTable;
 class MediaHistoryFeedsTable;
 class MediaHistoryFeedItemsTable;
+class MediaHistoryKaleidoscopeDataTable;
 
 // Refcounted as it is created, initialized and destroyed on a different thread
 // from the DB sequence provided to the constructor of this class that is
@@ -203,6 +204,14 @@ class MediaHistoryStore : public base::RefCountedThreadSafe<MediaHistoryStore> {
   void UpdateFeedUserStatus(const int64_t feed_id,
                             media_feeds::mojom::FeedUserStatus status);
 
+  void SetKaleidoscopeData(media::mojom::GetCollectionsResponsePtr data,
+                           const std::string& gaia_id);
+
+  media::mojom::GetCollectionsResponsePtr GetKaleidoscopeData(
+      const std::string& gaia_id);
+
+  void DeleteKaleidoscopeData();
+
  private:
   friend class base::RefCountedThreadSafe<MediaHistoryStore>;
 
@@ -225,6 +234,7 @@ class MediaHistoryStore : public base::RefCountedThreadSafe<MediaHistoryStore> {
   scoped_refptr<MediaHistoryImagesTable> images_table_;
   scoped_refptr<MediaHistoryFeedsTable> feeds_table_;
   scoped_refptr<MediaHistoryFeedItemsTable> feed_items_table_;
+  scoped_refptr<MediaHistoryKaleidoscopeDataTable> kaleidoscope_table_;
   bool initialization_successful_;
   base::AtomicFlag cancelled_;
 };
