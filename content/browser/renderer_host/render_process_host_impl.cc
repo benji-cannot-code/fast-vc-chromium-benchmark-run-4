c31bc681b6ef56abb28f053d9767e8b45194174d
Dominik Röttsches <drott@chromium.org>
2020-09-03 17:08:44 +0000

    Move scheduling of bridged AndroidFontLookup binding to UI thread
    Follow-up to: https://chromium-review.googlesource.com/c/chromium/src/+/2375329
    
    content::GetGlobalJavaInterfaces() requires to be scheduled on the UI
    thread. RenderProcessHostImpl already has a path to schedule bindings
    there. Move AndroidFontLookup binding to being scheduled on this
    thread. Testing in [1] shows that otherwise the DCHECK in
    GetGlobalJavaInterfaces() is hit when binding to the AndroidFontLookup
    service.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2387017
    
    Bug: 1121589
    Change-Id: I003b4884a2373f6fc152a616e44b6be829f9f05a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2391240
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804279}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1372,13 +1372,6 @@ class RenderProcessHostImpl::IOThreadHostImpl
       return;
     }
 
-#if defined(OS_ANDROID)
-    if (auto r = receiver.As<blink::mojom::AndroidFontLookup>()) {
-      content::GetGlobalJavaInterfaces()->GetInterface(std::move(r));
-      return;
-    }
-#endif
-
     std::string interface_name = *receiver.interface_name();
     mojo::ScopedMessagePipeHandle pipe = receiver.PassPipe();
     if (binders_->TryBindInterface(interface_name, &pipe))
@@ -5194,6 +5187,14 @@ void RenderProcessHostImpl::BindTracedProcess(
 
 void RenderProcessHostImpl::OnBindHostReceiver(
     mojo::GenericPendingReceiver receiver) {
+#if defined(OS_ANDROID)
+  // content::GetGlobalJavaInterfaces() works only on the UI Thread.
+  if (auto r = receiver.As<blink::mojom::AndroidFontLookup>()) {
+    content::GetGlobalJavaInterfaces()->GetInterface(std::move(r));
+    return;
+  }
+#endif
+
   GetContentClient()->browser()->BindHostReceiverForRenderer(
       this, std::move(receiver));
 }
