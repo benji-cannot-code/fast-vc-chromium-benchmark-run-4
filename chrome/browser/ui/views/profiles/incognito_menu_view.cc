59d62a70d8ce5342f20d78383cc0cf2d21e316ee
Mariam Ali <alimariam@google.com>
2020-09-03 21:39:24 +0000

    Update close button strings and remove others section in guest
    This CL updates close button strings in both incognito and guest menu
    to align with the new menu design, as well as removing Other People
    section in guest mode.
    
    Design Deck: go/alignmenus
    
    Screenshots:
    https://screenshot.googleplex.com/6Vu7adRHK62CgxG.png
    
    Bug: 1105763
    Change-Id: Ib81fdcfe5816c08978c28b86ce22656b5e5f5d94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2391069
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804426}

--- chrome/browser/ui/views/profiles/incognito_menu_view.cc
+++ chrome/browser/ui/views/profiles/incognito_menu_view.cc
@@ -19,8 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/image_model.h"
 #include "ui/views/accessibility/view_accessibility.h"
@@ -77,11 +79,16 @@ void IncognitoMenuView::BuildMenu() {
   }
 #endif
 
+  const bool new_menu_design =
+      base::FeatureList::IsEnabled(features::kNewProfilePicker);
+
   AddFeatureButton(
-      l10n_util::GetStringUTF16(IDS_INCOGNITO_PROFILE_MENU_CLOSE_BUTTON),
+      l10n_util::GetStringUTF16(
+          new_menu_design ? IDS_INCOGNITO_PROFILE_MENU_CLOSE_BUTTON_NEW
+                          : IDS_INCOGNITO_PROFILE_MENU_CLOSE_BUTTON),
       base::BindRepeating(&IncognitoMenuView::OnExitButtonClicked,
                           base::Unretained(this)),
-      kCloseAllIcon);
+      new_menu_design ? vector_icons::kCloseIcon : kCloseAllIcon);
 }
 
 base::string16 IncognitoMenuView::GetAccessibleWindowTitle() const {
