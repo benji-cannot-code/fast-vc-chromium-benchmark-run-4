71f5c63f6bb2e2d3809950fb91090ed0b3b96e93
nancylingwang <nancylingwang@chromium.org>
2020-09-03 03:57:44 +0000

    Fix the crash issue when loading more than 200 Arc app icons.
    When more than 200 Arc apps are installed, due to different icon size,
    scale, too many files are opened, causing the system crash. Add a
    limitation to control icon loading requests to be less than 250. If
    there are more icon loading requests, add them to the pending list, and
    when some icon requests finished, continue loading icons from the
    pending list.
    
    Modify ArcAppIcon to observer_->OnIconUpdated to notify the observer for
    all error cases, because the observer in AppService depends on the
    notification to update the pending list and the in-flight list.
    
    BUG=1123796
    
    Change-Id: I00ab3be5326e21574c1757cedacd7701e8b66710
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2386688
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804048}

--- chrome/browser/apps/app_service/arc_icon_once_loader.cc
+++ chrome/browser/apps/app_service/arc_icon_once_loader.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace apps {
 
+constexpr size_t kMaxSimultaneousIconRequests = 250;
+
 // A part of an ArcIconOnceLoader, for a specific size_in_dip and
 // icon_type. This two-level structure (an ArcIconOnceLoader contains
 // multiple SizeSpecificLoader instances) is needed because each ArcAppIcon is
@@ -18,7 +20,8 @@ class ArcIconOnceLoader::SizeSpecificLoader : public ArcAppIcon::Observer {
  public:
   SizeSpecificLoader(Profile* profile,
                      int32_t size_in_dip,
-                     apps::mojom::IconType icon_type);
+                     apps::mojom::IconType icon_type,
+                     ArcIconOnceLoader& host);
   ~SizeSpecificLoader() override;
 
   void LoadIcon(const std::string& app_id,
@@ -28,11 +31,13 @@ class ArcIconOnceLoader::SizeSpecificLoader : public ArcAppIcon::Observer {
 
   // ArcAppIcon::Observer overrides.
   void OnIconUpdated(ArcAppIcon* icon) override;
+  void OnIconFailed(ArcAppIcon* icon) override;
 
  private:
   Profile* const profile_;
   const int32_t size_in_dip_;
   const apps::mojom::IconType icon_type_;
+  ArcIconOnceLoader& host_;
 
   // Maps App IDs to their icon loaders (for a specific size_in_dip and
   // icon_compression).
@@ -47,8 +52,12 @@ class ArcIconOnceLoader::SizeSpecificLoader : public ArcAppIcon::Observer {
 ArcIconOnceLoader::SizeSpecificLoader::SizeSpecificLoader(
     Profile* profile,
     int32_t size_in_dip,
-    apps::mojom::IconType icon_type)
-    : profile_(profile), size_in_dip_(size_in_dip), icon_type_(icon_type) {}
+    apps::mojom::IconType icon_type,
+    ArcIconOnceLoader& host)
+    : profile_(profile),
+      size_in_dip_(size_in_dip),
+      icon_type_(icon_type),
+      host_(host) {}
 
 ArcIconOnceLoader::SizeSpecificLoader::~SizeSpecificLoader() {
   for (auto& kv_pair : callbacks_) {
@@ -90,17 +99,26 @@ void ArcIconOnceLoader::SizeSpecificLoader::LoadIcon(
       icon_type = ArcAppIcon::IconType::kAdaptive;
       break;
   }
+
   iter = icons_
              .insert(std::make_pair(
                  app_id, std::make_unique<ArcAppIcon>(
                              profile_, app_id, size_in_dip_, this, icon_type)))
              .first;
+  if (base::FeatureList::IsEnabled(features::kAppServiceAdaptiveIcon)) {
+    host_.MaybeStartIconRequest(iter->second.get(),
+                                ui::ScaleFactor::NUM_SCALE_FACTORS);
+    return;
+  }
   iter->second->LoadSupportedScaleFactors();
 }
 
 void ArcIconOnceLoader::SizeSpecificLoader::Remove(const std::string& app_id) {
   auto iter = icons_.find(app_id);
   if (iter != icons_.end()) {
+    if (base::FeatureList::IsEnabled(features::kAppServiceAdaptiveIcon)) {
+      host_.RemoveArcAppIcon(iter->second.get());
+    }
     icons_.erase(iter);
   }
 }
@@ -110,14 +128,27 @@ void ArcIconOnceLoader::SizeSpecificLoader::Reload(
     ui::ScaleFactor scale_factor) {
   auto iter = icons_.find(app_id);
   if (iter != icons_.end()) {
+    if (base::FeatureList::IsEnabled(features::kAppServiceAdaptiveIcon)) {
+      host_.MaybeStartIconRequest(iter->second.get(), scale_factor);
+      return;
+    }
     iter->second->LoadForScaleFactor(scale_factor);
   }
 }
 
 void ArcIconOnceLoader::SizeSpecificLoader::OnIconUpdated(ArcAppIcon* icon) {
-  if (!icon || !icon->EverySupportedScaleFactorIsLoaded()) {
+  if (!icon) {
     return;
   }
+
+  if (base::FeatureList::IsEnabled(features::kAppServiceAdaptiveIcon)) {
+    host_.RemoveArcAppIcon(icon);
+  }
+
+  if (!icon->EverySupportedScaleFactorIsLoaded()) {
+    return;
+  }
+
   auto range = callbacks_.equal_range(icon->app_id());
   auto count = std::distance(range.first, range.second);
   if (count <= 0) {
@@ -150,14 +181,18 @@ void ArcIconOnceLoader::SizeSpecificLoader::OnIconUpdated(ArcAppIcon* icon) {
   }
 }
 
+void ArcIconOnceLoader::SizeSpecificLoader::OnIconFailed(ArcAppIcon* icon) {
+  OnIconUpdated(icon);
+}
+
 ArcIconOnceLoader::ArcIconOnceLoader(Profile* profile)
     : profile_(profile), stop_observing_called_(false) {
   ArcAppListPrefs::Get(profile)->AddObserver(this);
 }
 
 ArcIconOnceLoader::~ArcIconOnceLoader() {
-  // Check that somebody called StopObserving. We can't call StopObserving here
-  // in the destructor, because we need a ArcAppListPrefs* prefs, and for
+  // Check that somebody called StopObserving. We can't call StopObserving
+  // here in the destructor, because we need a ArcAppListPrefs* prefs, and for
   // tests, the prefs pointer for a profile can change over time (e.g. by
   // ArcAppListPrefsFactory::RecreateServiceInstanceForTesting).
   //
@@ -180,11 +215,11 @@ void ArcIconOnceLoader::LoadIcon(
   auto key = std::make_pair(size_in_dip, icon_type);
   auto iter = size_specific_loaders_.find(key);
   if (iter == size_specific_loaders_.end()) {
-    iter =
-        size_specific_loaders_
-            .insert(std::make_pair(key, std::make_unique<SizeSpecificLoader>(
-                                            profile_, size_in_dip, icon_type)))
-            .first;
+    iter = size_specific_loaders_
+               .insert(std::make_pair(
+                   key, std::make_unique<SizeSpecificLoader>(
+                            profile_, size_in_dip, icon_type, *this)))
+               .first;
   }
   iter->second->LoadIcon(app_id, std::move(callback));
 }
@@ -205,6 +240,56 @@ void ArcIconOnceLoader::OnAppIconUpdated(
     if (iter != size_specific_loaders_.end()) {
       iter->second->Reload(app_id, descriptor.scale_factor);
     }
+  }
+}
+
+void ArcIconOnceLoader::MaybeStartIconRequest(ArcAppIcon* arc_app_icon,
+                                              ui::ScaleFactor scale_factor) {
+  DCHECK(arc_app_icon);
+  if (in_flight_requests_.size() < kMaxSimultaneousIconRequests) {
+    in_flight_requests_.insert(arc_app_icon);
+    if (scale_factor == ui::ScaleFactor::NUM_SCALE_FACTORS) {
+      arc_app_icon->LoadSupportedScaleFactors();
+    } else {
+      arc_app_icon->LoadForScaleFactor(scale_factor);
+    }
+    return;
+  }
+
+  pending_requests_[arc_app_icon].insert(scale_factor);
+}
+
+void ArcIconOnceLoader::RemoveArcAppIcon(ArcAppIcon* arc_app_icon) {
+  DCHECK(arc_app_icon);
+
+  in_flight_requests_.erase(arc_app_icon);
+  pending_requests_.erase(arc_app_icon);
+
+  MaybeLoadPendingIconRequest();
+}
+
+void ArcIconOnceLoader::MaybeLoadPendingIconRequest() {
+  while (!pending_requests_.empty() &&
+         in_flight_requests_.size() < kMaxSimultaneousIconRequests) {
+    auto it = pending_requests_.begin();
+
+    ArcAppIcon* arc_app_icon = it->first;
+    DCHECK(arc_app_icon);
+
+    std::set<ui::ScaleFactor>& scale_factors = it->second;
+    DCHECK(!scale_factors.empty());
+
+    // Handle all pending icon loading requests for |arc_app_icon|.
+    for (auto scale_factor : scale_factors) {
+      if (scale_factor == ui::ScaleFactor::NUM_SCALE_FACTORS) {
+        arc_app_icon->LoadSupportedScaleFactors();
+      } else {
+        arc_app_icon->LoadForScaleFactor(scale_factor);
+      }
+    }
+    in_flight_requests_.insert(arc_app_icon);
+
+    pending_requests_.erase(it);
   }
 }
 
