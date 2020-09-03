736ac2b4d09c15d8a61d91b2c77ac5eaa990bc8a
Brandon Wylie <wylieb@chromium.org>
2020-09-03 22:30:46 +0000

    Prevent the URL bar from overlapping with the actions container
    Replacing a workaround with a less hacky solution.
    
    Bug: 1122212,1012436,1121577
    Change-Id: I7d3cb641a695d4c8f4a70b982316f38957941cc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2386344
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804444}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarPhone.java
@@ -45,7 +45,6 @@ public class LocationBarPhone extends LocationBarLayout {
         super.onFinishInflate();
 
         mUrlBar = findViewById(R.id.url_bar);
-        updateUrlBarPaddingForSearchEngineIcon();
         mStatusView = findViewById(R.id.location_bar_status);
         // Assign the first visible view here only if it hasn't been set by the DSE icon experiment.
         // See onNativeLibrary ready for when this variable is set for the DSE icon case.
@@ -82,7 +81,6 @@ public class LocationBarPhone extends LocationBarLayout {
             int lateralPadding = getResources().getDimensionPixelOffset(
                     R.dimen.sei_location_bar_lateral_padding);
             setPaddingRelative(lateralPadding, getPaddingTop(), lateralPadding, getPaddingBottom());
-            updateUrlBarPaddingForSearchEngineIcon();
         }
 
         // This branch will be hit if the search engine logo experiment is enabled and we should
@@ -91,34 +89,12 @@ public class LocationBarPhone extends LocationBarLayout {
             mStatusView.updateSearchEngineStatusIcon(
                     shouldShowSearchEngineLogo, isSearchEngineGoogle, searchEngineUrl);
             mFirstVisibleFocusedView = mStatusView;
-            updateUrlBarPaddingForSearchEngineIcon();
 
             // When the search engine icon is enabled, icons are translations into the parent view's
             // padding area. Set clip padding to false to prevent them from getting clipped.
             setClipToPadding(false);
         }
         setShowIconsWhenUrlFocused(shouldShowSearchEngineLogo);
-    }
-
-    /**
-     * Factor in extra padding added for the focused state when the search engine icon is active.
-     */
-    private void updateUrlBarPaddingForSearchEngineIcon() {
-        if (mUrlBar == null || mStatusCoordinator == null) return;
-
-        mUrlBar.post(() -> {
-            // TODO(crbug.com/1019019): Come up with a better solution for M80 or M81.
-            // This padding prevents the UrlBar's content from extending past the available space
-            // and into the next view while focused.
-            final int endPadding = SearchEngineLogoUtils.shouldShowSearchEngineLogo(
-                                           mToolbarDataProvider.isIncognito())
-                            && hasFocus()
-                    ? mStatusCoordinator.getEndPaddingPixelSizeOnFocusDelta()
-                    : 0;
-
-            mUrlBar.setPaddingRelative(mUrlBar.getPaddingStart(), mUrlBar.getPaddingTop(),
-                    endPadding, mUrlBar.getPaddingBottom());
-        });
     }
 
     /**
@@ -268,7 +244,6 @@ public class LocationBarPhone extends LocationBarLayout {
             setFocusable(false);
             setFocusableInTouchMode(false);
         }
-        updateUrlBarPaddingForSearchEngineIcon();
         setUrlFocusChangeInProgress(true);
         updateShouldAnimateIconChanges();
         super.onUrlFocusChange(hasFocus);
@@ -304,7 +279,6 @@ public class LocationBarPhone extends LocationBarLayout {
         if (!hasFocus) {
             mUrlActionContainer.setVisibility(GONE);
         }
-        updateUrlBarPaddingForSearchEngineIcon();
         mStatusCoordinator.onUrlAnimationFinished(hasFocus);
     }
 
@@ -340,7 +314,6 @@ public class LocationBarPhone extends LocationBarLayout {
         mFirstVisibleFocusedView = shouldShowSearchEngineLogo ? mStatusView : mUrlBar;
 
         updateStatusVisibility();
-        updateUrlBarPaddingForSearchEngineIcon();
     }
 
     @Override
