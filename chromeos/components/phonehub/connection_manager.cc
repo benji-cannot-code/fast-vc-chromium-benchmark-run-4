ae065bfb99057e2a7e93151ca4ee43605fe6d8f4
Jimmy Gong <jimmyxgong@chromium.org>
2020-09-03 05:27:38 +0000

    Add ConnectionManager class
    - This class exposes an AttemptConnection() function to connect the
      local Chrome OS to a remote phone device.
    - Also exposes a GetStatus() function to retrieve the current status
      of the connection.
    
    Bug: 1106937
    Test: unit_tests
    Change-Id: I2851cd28ba83034af8c2bddcc17f5a50ddd0c90c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2387149
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804098}

---
+++ chromeos/components/phonehub/connection_manager.cc
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/phonehub/connection_manager.h"
+
+namespace chromeos {
+namespace phonehub {
+
+ConnectionManager::ConnectionManager() = default;
+ConnectionManager::~ConnectionManager() = default;
+
+void ConnectionManager::AddObserver(Observer* observer) {
+  observer_list_.AddObserver(observer);
+}
+
+void ConnectionManager::RemoveObserver(Observer* observer) {
+  observer_list_.RemoveObserver(observer);
+}
+
+void ConnectionManager::NotifyStatusChanged() {
+  for (auto& observer : observer_list_)
+    observer.OnStatusChanged();
+}
+
+std::ostream& operator<<(std::ostream& stream,
+                         ConnectionManager::Status status) {
+  switch (status) {
+    case ConnectionManager::Status::kConnecting:
+      stream << "[Connecting]";
+      break;
+    case ConnectionManager::Status::kConnected:
+      stream << "[Connected]";
+      break;
+    case ConnectionManager::Status::kDisconnected:
+      stream << "[Disconnected]";
+      break;
+  }
+  return stream;
+}
+
+}  // namespace phonehub
+}  // namespace chromeos
---
+++ chromeos/components/phonehub/fake_connection_manager.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/phonehub/fake_connection_manager.h"
+
+namespace chromeos {
+namespace phonehub {
+
+FakeConnectionManager::FakeConnectionManager()
+    : status_(Status::kDisconnected) {}
+
+FakeConnectionManager::~FakeConnectionManager() = default;
+
+void FakeConnectionManager::SetStatus(Status status) {
+  if (status_ == status)
+    return;
+
+  status_ = status;
+  NotifyStatusChanged();
+}
+
+ConnectionManager::Status FakeConnectionManager::GetStatus() const {
+  return status_;
+}
+
+void FakeConnectionManager::AttemptConnection() {
+  if (status_ == Status::kDisconnected)
+    SetStatus(Status::kConnecting);
+}
+
+}  // namespace phonehub
+}  // namespace chromeos
