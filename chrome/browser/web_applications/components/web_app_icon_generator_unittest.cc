afbe04efd9294557015492f7da9f450564668a19
Sean McAllister <smcallis@google.com>
2020-09-03 15:02:54 +0000

    Refactor OS_LINUX preprocessor directive for LaCrOS effort.
    Currently, ChromeOS defines the OS_LINUX directive as well as
    OS_CHROMEOS.  We're working to separate these two, so we're
    making the fact that OS_LINUX == OS_LINUX || OS_CHROMEOS
    explicit.
    
    This is changes for /chrome/browser/web_applications
    
    This CL was uploaded by git cl split.
    
    R=ortuno@chromium.org
    
    Bug: 1110266
    Change-Id: Idfa976a6741f19d840933bbd7eb3150e4cf50136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2371283
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Sean McAllister <smcallis@google.com>
    Cr-Commit-Position: refs/heads/master@{#804231}

--- chrome/browser/web_applications/components/web_app_icon_generator_unittest.cc
+++ chrome/browser/web_applications/components/web_app_icon_generator_unittest.cc
@@ -404,7 +404,7 @@ TEST_F(WebAppIconGeneratorTest, GenerateIcons) {
     // Only for large icons with a sharp letter: Peek a pixel at the center of
     // icon. This is tested on Linux and ChromeOS only because different OSes
     // use different text shaping engines.
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
     const SkColor letter_color = color_utils::GetColorWithMaxContrast(bg_color);
     if (size >= icon_size::k256) {
       SkColor center_color = bitmap.getColor(center_x, center_y);
@@ -412,7 +412,7 @@ TEST_F(WebAppIconGeneratorTest, GenerateIcons) {
       SCOPED_TRACE(center_color);
       EXPECT_TRUE(AreColorsEqual(letter_color, center_color, /*threshold=*/50));
     }
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_CHROMEOS)
     sizes.erase(size);
   }
 
