be6e5d7534a09ece738ab88c188f5da2bf9f2de9
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-09-04 00:44:29 +0000

    [Extensions UI] Don't use BrowserActionsBarBrowserTest in bubble tests
    Currently, ExtensionMessageBubbleBrowserTest inherits from
    BrowserActionsBarBrowserTest. Unfortunately, this means that none of
    these tests run with the new Extensions Menu UI. Since the
    BrowserActionsBar-related tests are going away, we need to migrate these
    to be compatible with the new UI.
    
    Instead of inheriting from BrowserActionsBarBrowserTest, inherit from
    ExtensionBrowserTest directly. There were relatively few dependencies,
    and this paves the way to having these tests run with the new UI.
    
    Bug: 1100412
    Change-Id: I43cd5852ddf2e04eaee7c55f1d0b4086e31c59fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2392702
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804491}

--- chrome/browser/ui/extensions/extension_message_bubble_browsertest.cc
+++ chrome/browser/ui/extensions/extension_message_bubble_browsertest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/startup/startup_browser_creator.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_bar.h"
+#include "chrome/browser/ui/toolbar/toolbar_actions_model.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "components/omnibox/browser/omnibox_edit_model.h"
@@ -31,15 +32,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/test_extension_dir.h"
 
-ExtensionMessageBubbleBrowserTest::ExtensionMessageBubbleBrowserTest() {
-}
-
-ExtensionMessageBubbleBrowserTest::~ExtensionMessageBubbleBrowserTest() {
-}
+ExtensionMessageBubbleBrowserTest::ExtensionMessageBubbleBrowserTest() =
+    default;
+ExtensionMessageBubbleBrowserTest::~ExtensionMessageBubbleBrowserTest() =
+    default;
 
 void ExtensionMessageBubbleBrowserTest::SetUpCommandLine(
     base::CommandLine* command_line) {
-  BrowserActionsBarBrowserTest::SetUpCommandLine(command_line);
+  ExtensionBrowserTest::SetUpCommandLine(command_line);
   // The dev mode warning bubble is an easy one to trigger, so we use that for
   // our testing purposes.
   dev_mode_bubble_override_.reset(
@@ -49,12 +49,20 @@ void ExtensionMessageBubbleBrowserTest::SetUpCommandLine(
   ExtensionMessageBubbleFactory::set_override_for_tests(
       ExtensionMessageBubbleFactory::OVERRIDE_ENABLED);
   ToolbarActionsBar::set_extension_bubble_appearance_wait_time_for_testing(0);
+  ToolbarActionsBar::disable_animations_for_testing_ = true;
+}
+
+void ExtensionMessageBubbleBrowserTest::SetUpOnMainThread() {
+  extensions::ExtensionBrowserTest::SetUpOnMainThread();
+
+  toolbar_model_ = ToolbarActionsModel::Get(profile());
 }
 
 void ExtensionMessageBubbleBrowserTest::TearDownOnMainThread() {
   ExtensionMessageBubbleFactory::set_override_for_tests(
       ExtensionMessageBubbleFactory::NO_OVERRIDE);
-  BrowserActionsBarBrowserTest::TearDownOnMainThread();
+  ToolbarActionsBar::disable_animations_for_testing_ = false;
+  ExtensionBrowserTest::TearDownOnMainThread();
 }
 
 void ExtensionMessageBubbleBrowserTest::AddSettingsOverrideExtension(
