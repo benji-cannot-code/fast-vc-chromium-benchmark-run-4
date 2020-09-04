d58ca28ce058960dd8cb0bc1178d7b30ed0485e0
Evan Stade <estade@chromium.org>
2020-09-04 19:26:46 +0000

    WebLayer: hook up contacts picker for navigator.contacts.
    Bug: 1016938
    Change-Id: I4e259c8782382305d38834a68f5225b92d28ec2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2386835
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804576}

--- content/public/android/java/src/org/chromium/content_public/browser/ContactsPicker.java
+++ content/public/android/java/src/org/chromium/content_public/browser/ContactsPicker.java
@@ -17,6 +17,12 @@ public final class ContactsPicker {
      */
     private static ContactsPickerDelegate sContactsPickerDelegate;
 
+    /**
+     * The object that represents the currently visible contacts picker UI, or null if none is
+     * visible.
+     */
+    private static Object sPicker;
+
     private ContactsPicker() {}
 
     /**
@@ -47,7 +53,8 @@ public final class ContactsPicker {
             boolean includeEmails, boolean includeTel, boolean includeAddresses,
             boolean includeIcons, String formattedOrigin) {
         if (sContactsPickerDelegate == null) return false;
-        sContactsPickerDelegate.showContactsPicker(windowAndroid, listener, allowMultiple,
+        assert sPicker == null;
+        sPicker = sContactsPickerDelegate.showContactsPicker(windowAndroid, listener, allowMultiple,
                 includeNames, includeEmails, includeTel, includeAddresses, includeIcons,
                 formattedOrigin);
         return true;
@@ -57,7 +64,7 @@ public final class ContactsPicker {
      * Called when the contacts picker dialog has been dismissed.
      */
     public static void onContactsPickerDismissed() {
-        if (sContactsPickerDelegate == null) return;
-        sContactsPickerDelegate.onContactsPickerDismissed();
+        assert sPicker != null;
+        sPicker = null;
     }
 }
