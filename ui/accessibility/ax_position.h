d8ece510a40324a54c4a5951dfc589900b0854dd
Victor Fei <vicfei@microsoft.com>
2020-09-04 07:12:36 +0000

    Fix AXPosition::AtEndOfTextSpan to not rely on CreateParentPosition
    AXPosition::AtEndOfTextSpan relies on CreateParentPosition to
    determine if an inline text box marks the end of text span. Since
    CreateParentPosition calls into AtStartOfLine which calls
    AtEndOfTextSpan so we end up with an infinite loop and stack overflow.
    
    https://crrev.com/c/2245611 fixed most cases with this infinite loop
    of AtStartOfLine/CreateParentPosition/AtEndOfTextSpan by checking for
    AtEndOfAnchor first before calling AtEndOfTextSpan.
    
    However consider the following example:
    <style>
      .required-label::after {
        content: " *";
      }
    </style>
    <label class="required-label">Required </label>
    
    labelText #1
    ++staticText name='Required ' nextOnLineId=inlineTextBox  #2
    ++++inlineTextBox name='Required ' nextOnLineId=inlineTextBox #3
    ++staticText name=' *' previousOnLineId=inlineTextBox #4
    ++++inlineTextBox name='*' previousOnLineId=inlineTextBox #5
    
    AtEndOfAnchor check would not prevent this infinite loop, if
    AtStartOfLine is called on staticText #4 name=' *' as the max text
    offsets differ between staticText #4 and its inlineText #5.
    
    This CL fixes the above issue by making AtEndOfTextSpan not to call
    CreateParentPosition. Instead it determines end of text span by
    directly looking at the current position's parent role and its index
    among its siblings.
    
    AX-Relnotes: Fixes hangs in certain accessibility scenarios.
    
    Bug: 1120636
    Change-Id: Ic99c27b069f220f26da736b3946560ef23d6b622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2369681
    Commit-Queue: Victor Fei <vicfei@microsoft.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804531}

--- ui/accessibility/ax_position.h
+++ ui/accessibility/ax_position.h
@@ -969,7 +969,7 @@ class AXPosition {
   bool AtStartOfDocument() const {
     if (IsNullPosition())
       return false;
-    return IsDocument(GetRole()) && AtStartOfAnchor();
+    return IsDocument(GetAnchorRole()) && AtStartOfAnchor();
   }
 
   bool AtEndOfDocument() const {
@@ -3149,8 +3149,8 @@ class AXPosition {
     // infinite loop. However, GetAnchor()->IsIgnored() is sufficient here
     // because we know that the anchor at this position doesn't have an
     // unignored child, making this a leaf tree or text position.
-    return !GetAnchor()->IsIgnored() && !IsDocument(GetRole()) &&
-           !IsInTextObject() && !IsIframe(GetRole());
+    return !GetAnchor()->IsIgnored() && !IsDocument(GetAnchorRole()) &&
+           !IsInTextObject() && !IsIframe(GetAnchorRole());
   }
 
   bool IsInDescendantOfEmptyObject() const {
@@ -3344,6 +3344,7 @@ class AXPosition {
   // When we call the following method on TextField, it would return 1.
   virtual int AnchorUnignoredChildCount() const = 0;
   virtual int AnchorIndexInParent() const = 0;
+  virtual int AnchorSiblingCount() const = 0;
   virtual base::stack<AXNodeType*> GetAncestorAnchors() const = 0;
   virtual AXNodeType* GetLowestUnignoredAncestor() const = 0;
   virtual void AnchorParent(AXTreeID* tree_id, int32_t* parent_id) const = 0;
@@ -3367,7 +3368,8 @@ class AXPosition {
   // break in the text representation, e.g. a block level element or a <br>.
   virtual bool IsInLineBreakingObject() const = 0;
 
-  virtual ax::mojom::Role GetRole() const = 0;
+  virtual ax::mojom::Role GetAnchorRole() const = 0;
+  virtual ax::mojom::Role GetRole(AXNodeType* node) const = 0;
   virtual AXNodeTextStyles GetTextStyles() const = 0;
   virtual std::vector<int32_t> GetWordStartOffsets() const = 0;
   virtual std::vector<int32_t> GetWordEndOffsets() const = 0;
@@ -3401,12 +3403,17 @@ class AXPosition {
   // A text span is defined by a series of inline text boxes that make up a
   // single static text object.
   bool AtEndOfTextSpan() const {
-    if (GetRole() != ax::mojom::Role::kInlineTextBox || !AtEndOfAnchor())
+    if (GetAnchorRole() != ax::mojom::Role::kInlineTextBox || !AtEndOfAnchor())
       return false;
 
-    AXPositionInstance parent_position = CreateParentPosition();
-    return parent_position->GetRole() == ax::mojom::Role::kStaticText &&
-           parent_position->AtEndOfAnchor();
+    // We are at the end of text span if |this| position has
+    // role::kInlineTextBox, the parent of |this| has role::kStaticText, and the
+    // anchor node of |this| is the last child of parent's children.
+    const bool is_last_child =
+        AnchorIndexInParent() == (AnchorSiblingCount() - 1);
+
+    return is_last_child && GetRole(GetLowestUnignoredAncestor()) ==
+                                ax::mojom::Role::kStaticText;
   }
 
   // Uses depth-first pre-order traversal.
@@ -3651,8 +3658,8 @@ class AXPosition {
     }
 
     // Treat moving into or out of nodes with certain roles as a format break.
-    ax::mojom::Role from_role = move_from.GetRole();
-    ax::mojom::Role to_role = move_to.GetRole();
+    ax::mojom::Role from_role = move_from.GetAnchorRole();
+    ax::mojom::Role to_role = move_to.GetAnchorRole();
     if (from_role != to_role) {
       if (IsFormatBoundary(from_role) || IsFormatBoundary(to_role))
         return true;
@@ -3834,7 +3841,7 @@ class AXPosition {
   AXPositionInstance CreateDocumentAncestorPosition() const {
     AXPositionInstance iterator = Clone();
     while (!iterator->IsNullPosition()) {
-      if (IsDocument(iterator->GetRole()) &&
+      if (IsDocument(iterator->GetAnchorRole()) &&
           iterator->CreateParentPosition()->IsNullPosition()) {
         break;
       }
