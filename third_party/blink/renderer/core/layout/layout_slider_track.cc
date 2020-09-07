1bc1028347021de7cbc1f6bd73d8575b8848edaa
Kent Tamura <tkent@chromium.org>
2020-09-07 09:35:50 +0000

    Slider NG: Rename layout_slider_container.{h,cc}
    to layout_slider_track.{h,cc} respectively.
    This CL has no behavior changes.
    
    Bug: 1040826
    Change-Id: Ia56c1df2671ea637b89305be01801a32c84b9c56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2395139
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804713}

--- third_party/blink/renderer/core/layout/layout_slider_track.cc
+++ third_party/blink/renderer/core/layout/layout_slider_track.cc
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "third_party/blink/renderer/core/layout/layout_slider_container.h"
+#include "third_party/blink/renderer/core/layout/layout_slider_track.h"
 
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/html/forms/html_input_element.h"
