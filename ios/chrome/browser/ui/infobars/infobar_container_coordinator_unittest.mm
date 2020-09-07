e2a1afa905c418a7f911c60c8b3ebf1b6d46c56d
Robbie Gibson <rkgibson@google.com>
2020-09-07 16:33:24 +0000

    Revert "Reland "[ios] Turn on FullscreenControllerBrowserScoped by default""
    This reverts commit ddd44b8b55e5f95639761ddc19b4457a280c32c0.
    
    Reason for revert: Internal tests still failing
    
    Original change's description:
    > Reland "[ios] Turn on FullscreenControllerBrowserScoped by default"
    > 
    > This is a reland of 5f4895f50d50d62bbea1cd05bebcdb1593e5a5cf
    > 
    > Original change's description:
    > > [ios] Turn on FullscreenControllerBrowserScoped by default
    > > 
    > > Bug: 790886
    > > Change-Id: Ide1eee5634376b90cfa820bb61644c871969ff86
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2358589
    > > Reviewed-by: Sergio Collazos <sczs@chromium.org>
    > > Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    > > Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#800354}
    > 
    > Bug: 790886
    > Change-Id: I279349dd1b45de347600c26e8d32e5d9d33b5b65
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2395695
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Reviewed-by: Robbie Gibson <rkgibson@google.com>
    > Commit-Queue: Mark Cogan <marq@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#804737}
    
    TBR=marq@chromium.org,sczs@chromium.org,thegreenfrog@chromium.org,rkgibson@google.com
    
    Change-Id: Ief9473b6d8a3eb772f78aa21e353751c0de785ed
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 790886
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2396123
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#804770}

--- ios/chrome/browser/ui/infobars/infobar_container_coordinator_unittest.mm
+++ ios/chrome/browser/ui/infobars/infobar_container_coordinator_unittest.mm
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/infobars/infobar_container_coordinator.h"
 
-#import <WebKit/WebKit.h>
-
 #import "base/test/ios/wait_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
@@ -30,11 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/web_state_list/web_state_opener.h"
 #import "ios/chrome/test/scoped_key_window.h"
-#import "ios/web/common/crw_web_view_content_view.h"
 #import "ios/web/public/test/fakes/test_navigation_manager.h"
 #import "ios/web/public/test/fakes/test_web_state.h"
-#import "ios/web/public/ui/crw_web_view_proxy.h"
-#import "ios/web/public/ui/crw_web_view_scroll_view_proxy.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
@@ -75,13 +70,7 @@ class InfobarContainerCoordinatorTest : public PlatformTest {
   InfobarContainerCoordinatorTest()
       : browser_(std::make_unique<TestBrowser>()),
         base_view_controller_([[FakeBaseViewController alloc] init]),
-        positioner_([[TestContainerCoordinatorPositioner alloc] init]),
-        web_view_([[WKWebView alloc]
-            initWithFrame:scoped_window_.Get().bounds
-            configuration:[[WKWebViewConfiguration alloc] init]]),
-        content_view_([[CRWWebViewContentView alloc]
-            initWithWebView:web_view_
-                 scrollView:web_view_.scrollView]) {
+        positioner_([[TestContainerCoordinatorPositioner alloc] init]) {
     // Enable kIOSInfobarUIReboot flag.
     feature_list_.InitWithFeatures({kIOSInfobarUIReboot},
                                    {kInfobarUIRebootOnlyiOS13});
@@ -96,15 +85,6 @@ class InfobarContainerCoordinatorTest : public PlatformTest {
     navigation_manager_ = navigation_manager.get();
     web_state->SetNavigationManager(std::move(navigation_manager));
     web_state->SetBrowserState(browser_->GetBrowserState());
-    web_state->SetView(content_view_);
-    CRWWebViewScrollViewProxy* scroll_view_proxy =
-        [[CRWWebViewScrollViewProxy alloc] init];
-    UIScrollView* scroll_view = [[UIScrollView alloc] init];
-    [scroll_view_proxy setScrollView:scroll_view];
-    id web_view_proxy_mock = OCMProtocolMock(@protocol(CRWWebViewProxy));
-    [[[web_view_proxy_mock stub] andReturn:scroll_view_proxy] scrollViewProxy];
-    web_state->SetWebViewProxy(web_view_proxy_mock);
-
     browser_->GetWebStateList()->InsertWebState(0, std::move(web_state),
                                                 WebStateList::INSERT_NO_FLAGS,
                                                 WebStateOpener());
@@ -208,14 +188,6 @@ class InfobarContainerCoordinatorTest : public PlatformTest {
   void AddSecondWebstate() {
     std::unique_ptr<web::TestWebState> second_web_state =
         std::make_unique<web::TestWebState>();
-    second_web_state->SetView(content_view_);
-    CRWWebViewScrollViewProxy* scroll_view_proxy =
-        [[CRWWebViewScrollViewProxy alloc] init];
-    UIScrollView* scroll_view = [[UIScrollView alloc] init];
-    [scroll_view_proxy setScrollView:scroll_view];
-    id web_view_proxy_mock = OCMProtocolMock(@protocol(CRWWebViewProxy));
-    [[[web_view_proxy_mock stub] andReturn:scroll_view_proxy] scrollViewProxy];
-    second_web_state->SetWebViewProxy(web_view_proxy_mock);
     InfoBarManagerImpl::CreateForWebState(second_web_state.get());
     InfobarBadgeTabHelper::CreateForWebState(second_web_state.get());
     browser_->GetWebStateList()->InsertWebState(1, std::move(second_web_state),
@@ -250,9 +222,6 @@ class InfobarContainerCoordinatorTest : public PlatformTest {
   std::unique_ptr<IOSChromeSavePasswordInfoBarDelegate> infobar_delegate_;
   ConfirmInfoBarController* legacy_controller_;
   std::unique_ptr<ConfirmInfoBarDelegate> legacy_infobar_delegate_;
-  ScopedKeyWindow scoped_window_;
-  WKWebView* web_view_ = nil;
-  CRWWebViewContentView* content_view_ = nil;
 };
 
 // Tests infobarBannerState is InfobarBannerPresentationState::Presented once an
