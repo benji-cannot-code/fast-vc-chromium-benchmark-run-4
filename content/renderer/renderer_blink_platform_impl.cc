1476b18bb9a020a7fdffc0100e5864e400b2748a
Matt Falkenhagen <falken@chromium.org>
2020-09-07 07:51:43 +0000

    code cache: Remove sync primitives from task runner.
    It does not look like this trait is needed.
    
    Change-Id: I17868f8ee29b405d43a100e87e6fa0cae65b3615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2394876
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804688}

--- content/renderer/renderer_blink_platform_impl.cc
+++ content/renderer/renderer_blink_platform_impl.cc
@@ -919,8 +919,7 @@ blink::mojom::CodeCacheHost& RendererBlinkPlatformImpl::GetCodeCacheHost() {
   if (!code_cache_host_) {
     code_cache_host_ = mojo::SharedRemote<blink::mojom::CodeCacheHost>(
         std::move(code_cache_host_remote_),
-        base::ThreadPool::CreateSequencedTaskRunner(
-            {base::WithBaseSyncPrimitives()}));
+        base::ThreadPool::CreateSequencedTaskRunner({}));
   }
   return *code_cache_host_;
 }
