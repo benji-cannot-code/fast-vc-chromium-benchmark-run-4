4d77da8b2d1260214f96fd89b8f614e7a1de79bf
Clovis PJ <clovispj@google.com>
2020-09-07 12:03:19 +0000

    Separated DescriptorDeviceProvider from unittest
    Now called FakeDeviceProvider, existing separately with exposed header, to allow for reuse elsewhere.
    Also moved test dependents into 'test_support', out of unittest target. Cleaner + allows for reuse.
    
    Change-Id: I76ca2f2c6de68bb27cf254907e3d7d9a87518ae1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2368558
    Commit-Queue: Clovis PJ <clovispj@google.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804739}

--- media/capture/video/linux/video_capture_device_factory_linux_unittest.cc
+++ media/capture/video/linux/video_capture_device_factory_linux_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/task_environment.h"
+#include "media/capture/video/linux/fake_device_provider.h"
 #include "media/capture/video/linux/fake_v4l2_impl.h"
 #include "media/capture/video/mock_video_capture_device_client.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -17,57 +18,6 @@ using ::testing::_;
 using ::testing::InvokeWithoutArgs;
 
 namespace media {
-
-class DescriptorDeviceProvider
-    : public VideoCaptureDeviceFactoryLinux::DeviceProvider {
- public:
-  void AddDevice(const VideoCaptureDeviceDescriptor& descriptor) {
-    descriptors_.emplace_back(descriptor);
-  }
-
-  void GetDeviceIds(std::vector<std::string>* target_container) override {
-    for (const auto& entry : descriptors_) {
-      target_container->emplace_back(entry.device_id);
-    }
-  }
-
-  std::string GetDeviceModelId(const std::string& device_id) override {
-    auto iter =
-        std::find_if(descriptors_.begin(), descriptors_.end(),
-                     [&device_id](const VideoCaptureDeviceDescriptor& val) {
-                       return val.device_id == device_id;
-                     });
-    if (iter == descriptors_.end())
-      CHECK(false) << "Unknown device_id " << device_id;
-
-    return iter->model_id;
-  }
-
-  std::string GetDeviceDisplayName(const std::string& device_id) override {
-    auto iter =
-        std::find_if(descriptors_.begin(), descriptors_.end(),
-                     [&device_id](const VideoCaptureDeviceDescriptor& val) {
-                       return val.device_id == device_id;
-                     });
-    if (iter == descriptors_.end())
-      CHECK(false) << "Unknown device_id " << device_id;
-
-    return iter->display_name();
-  }
-
-  VideoFacingMode GetCameraFacing(const std::string& device_id,
-                                  const std::string& model_id) override {
-    return MEDIA_VIDEO_FACING_NONE;
-  }
-
-  int GetOrientation(const std::string& device_id,
-                     const std::string& model_id) override {
-    return 0;
-  }
-
- private:
-  std::vector<VideoCaptureDeviceDescriptor> descriptors_;
-};
 
 class VideoCaptureDeviceFactoryLinuxTest
     : public ::testing::TestWithParam<VideoCaptureDeviceDescriptor> {
@@ -80,7 +30,7 @@ class VideoCaptureDeviceFactoryLinuxTest
         base::ThreadTaskRunnerHandle::Get());
     scoped_refptr<FakeV4L2Impl> fake_v4l2(new FakeV4L2Impl());
     fake_v4l2_ = fake_v4l2.get();
-    auto fake_device_provider = std::make_unique<DescriptorDeviceProvider>();
+    auto fake_device_provider = std::make_unique<FakeDeviceProvider>();
     fake_device_provider_ = fake_device_provider.get();
     factory_->SetV4L2EnvironmentForTesting(std::move(fake_v4l2),
                                            std::move(fake_device_provider));
@@ -88,7 +38,7 @@ class VideoCaptureDeviceFactoryLinuxTest
 
   base::test::TaskEnvironment task_environment_;
   FakeV4L2Impl* fake_v4l2_;
-  DescriptorDeviceProvider* fake_device_provider_;
+  FakeDeviceProvider* fake_device_provider_;
   std::unique_ptr<VideoCaptureDeviceFactoryLinux> factory_;
 };
 
