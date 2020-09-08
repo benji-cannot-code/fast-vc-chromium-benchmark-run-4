ae04c870416e7638ca52b23d07a0da4f010fb80b
Austin Tankiang <austinct@chromium.org>
2020-09-08 01:23:37 +0000

    Add detail icon for pinned files in Drive
    Bug: 1115025
    Change-Id: If5d1c23f2a267b6e6b674e2f2d0105e0d2e3164f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360057
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804825}

--- ui/file_manager/file_manager/foreground/js/ui/file_table.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_table.js
@@ -844,6 +844,9 @@ class FileTable extends cr.ui.Table {
     label.className = 'detail-name';
     label.appendChild(
         filelist.renderFileNameLabel(this.ownerDocument, entry, locationInfo));
+    if (locationInfo.isDriveBased) {
+      label.appendChild(filelist.renderPinned(this.ownerDocument));
+    }
     return label;
   }
 
@@ -1082,7 +1085,7 @@ class FileTable extends cr.ui.Table {
                 [entry],
                 [
                   'availableOffline', 'customIconUrl', 'shared',
-                  'isMachineRoot', 'isExternalMedia', 'hosted'
+                  'isMachineRoot', 'isExternalMedia', 'hosted', 'pinned'
                 ])[0],
             util.isTeamDriveRoot(entry));
       });
@@ -1107,7 +1110,7 @@ class FileTable extends cr.ui.Table {
     const dateId = item.id + '-date';
     filelist.decorateListItem(item, entry, assert(this.metadataModel_));
     item.setAttribute('file-name', entry.name);
-    item.querySelector('.entry-name').setAttribute('id', nameId);
+    item.querySelector('.detail-name').setAttribute('id', nameId);
     item.querySelector('.size').setAttribute('id', sizeId);
     item.querySelector('.date').setAttribute('id', dateId);
     item.setAttribute('aria-labelledby', nameId);
