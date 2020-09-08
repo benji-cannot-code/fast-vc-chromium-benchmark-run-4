d5c5ccb1bdb23d3af5ad489bb7fb0b7a0b92abf9
Rahul Arakeri <arakeri@microsoft.com>
2020-09-08 20:36:24 +0000

    Fix for UAF when referencing a deleted scrollbar layer.
    This CL fixes an issue where autoscroll may be called on a scrollbar
    layer that is deleted. When the pointer is pressed down, an autoscroll
    task is scheduled to be executed after ~250ms. The task will execute if
    the pointer remains held down. In LayerTreeImpl::UnregisterScrollbar,
    DidUnregisterScrollbarLayer only gets called after both scrollbars are
    removed. So if you go from 2 to 1 scrollbar while an autoscroll task is
    queued, the autoscroll task won't get cancelled. At this point, the
    ScrollbarController tries to access the deleted scrollbar and that
    leads to an access violation. The fix here is to ensure that the call
    to DidUnregisterScrollbarLayer happens for each scrollbar.
    
    Bug: 1106612
    Change-Id: I4f1d684830012db8fdd73258c9a9e5231807bcb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2356809
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Rahul Arakeri <arakeri@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#805057}

--- third_party/blink/web_tests/resources/scrollbar-util.js
+++ third_party/blink/web_tests/resources/scrollbar-util.js
@@ -93,14 +93,22 @@ const SCROLLBAR_SCROLL_PIXELS = 40;
 
 /*
   Getters for the center point in a scroller's scrollbar buttons (CSS visual
-  coordinates).
+  coordinates). An empty argument requests the point for the main frame's
+  scrollbars.
 */
 function downArrow(scroller) {
   assert_true(!internals.overlayScrollbarsEnabled);
-  const scrollerRect = scroller.getBoundingClientRect();
   const TRACK_WIDTH = calculateScrollbarThickness();
   const BUTTON_WIDTH = TRACK_WIDTH;
   const SCROLL_CORNER = TRACK_WIDTH;
+  if (typeof(scroller) == 'undefined') {
+    // The main frame's scrollbars don't scale with pinch zoom so there's no
+    // need to convert from client to visual.
+    return { x: window.innerWidth - BUTTON_WIDTH / 2,
+             y: window.innerHeight - SCROLL_CORNER - BUTTON_WIDTH / 2 }
+  }
+
+  const scrollerRect = scroller.getBoundingClientRect();
   const down_arrow = {
     x: scrollerRect.right - BUTTON_WIDTH / 2,
     y: scrollerRect.bottom - SCROLL_CORNER - BUTTON_WIDTH / 2
@@ -110,9 +118,16 @@ function downArrow(scroller) {
 
 function upArrow(scroller) {
   assert_true(!internals.overlayScrollbarsEnabled);
-  const scrollerRect = scroller.getBoundingClientRect();
   const TRACK_WIDTH = calculateScrollbarThickness();
   const BUTTON_WIDTH = TRACK_WIDTH;
+  if (typeof(scroller) == 'undefined') {
+    // The main frame's scrollbars don't scale with pinch zoom so there's no
+    // need to convert from client to visual.
+    return { x: window.innerWidth - BUTTON_WIDTH / 2,
+             y: BUTTON_WIDTH / 2 }
+  }
+
+  const scrollerRect = scroller.getBoundingClientRect();
   const up_arrow = {
     x: scrollerRect.right - BUTTON_WIDTH / 2,
     y: scrollerRect.top + BUTTON_WIDTH / 2
@@ -122,9 +137,16 @@ function upArrow(scroller) {
 
 function leftArrow(scroller) {
   assert_true(!internals.overlayScrollbarsEnabled);
-  const scrollerRect = scroller.getBoundingClientRect();
   const TRACK_WIDTH = calculateScrollbarThickness();
   const BUTTON_WIDTH = TRACK_WIDTH;
+  if (typeof(scroller) == 'undefined') {
+    // The main frame's scrollbars don't scale with pinch zoom so there's no
+    // need to convert from client to visual.
+    return { x: BUTTON_WIDTH / 2,
+             y: window.innerHeight - BUTTON_WIDTH / 2 }
+  }
+
+  const scrollerRect = scroller.getBoundingClientRect();
   const left_arrow = {
     x: scrollerRect.left + BUTTON_WIDTH / 2,
     y: scrollerRect.bottom  - BUTTON_WIDTH / 2
@@ -134,10 +156,17 @@ function leftArrow(scroller) {
 
 function rightArrow(scroller) {
   assert_true(!internals.overlayScrollbarsEnabled);
-  const scrollerRect = scroller.getBoundingClientRect();
   const TRACK_WIDTH = calculateScrollbarThickness();
   const BUTTON_WIDTH = TRACK_WIDTH;
   const SCROLL_CORNER = TRACK_WIDTH;
+  if (typeof(scroller) == 'undefined') {
+    // The main frame's scrollbars don't scale with pinch zoom so there's no
+    // need to convert from client to visual.
+    return { x: window.innerWidth - SCROLL_CORNER - BUTTON_WIDTH / 2,
+             y: window.innerHeight - BUTTON_WIDTH / 2 }
+  }
+
+  const scrollerRect = scroller.getBoundingClientRect();
   const right_arrow = {
     x: scrollerRect.right - SCROLL_CORNER - BUTTON_WIDTH / 2,
     y: scrollerRect.bottom  - BUTTON_WIDTH / 2
@@ -151,7 +180,7 @@ function verticalThumb(scroller) {
   const TRACK_WIDTH = calculateScrollbarThickness();
   const BUTTON_WIDTH = calculateScrollbarButtonWidth();
 
-  if (scroller === document.documentElement) {
+  if (scroller === document.documentElement || typeof(scroller) == 'undefined') {
     // HTML element is special, since scrollbars are not part of its client rect
     // and page scale doesn't affect the scrollbars. Use window properties instead.
     let x = window.innerWidth - TRACK_WIDTH / 2;
