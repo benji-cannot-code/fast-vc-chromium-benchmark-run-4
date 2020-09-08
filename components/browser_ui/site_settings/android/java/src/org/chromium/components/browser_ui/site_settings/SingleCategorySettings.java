7c7b4e80836f853c73d2be2908416c0024def205
Christian Dullweber <dullweber@chromium.org>
2020-09-08 10:22:40 +0000

    Fix manage storage crash
    ManageSpaceActivity would crash when the manage storage button is
    clicked because it tried to create the wrong Fragment.
    This is fixed and a test is added that clicks the button to verify that
    it works.
    Asserts are changed to exceptions to get more meaningful errors if there
    are more false usages.
    
    Bug: 1125062
    Change-Id: Ie78af6d04c91d4b85b21b04b4f98605e0c4c46a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2395498
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804880}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleCategorySettings.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleCategorySettings.java
@@ -24,7 +24,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
@@ -89,8 +88,6 @@ public class SingleCategorySettings extends SiteSettingsPreferenceFragment
 
     // The list that contains preferences.
     private RecyclerView mListView;
-    // The view to show when the list is empty.
-    private TextView mEmptyView;
     // The item for searching the list of items.
     private MenuItem mSearchItem;
     // The Site Settings Category we are showing.
@@ -153,10 +150,6 @@ public class SingleCategorySettings extends SiteSettingsPreferenceFragment
             int chooserDataType = mCategory.getObjectChooserDataType();
             boolean hasEntries =
                     chooserDataType == -1 ? addWebsites(sites) : addChosenObjects(sites);
-
-            if (mEmptyView == null) return;
-
-            mEmptyView.setVisibility(hasEntries ? View.GONE : View.VISIBLE);
         }
     }
 
@@ -306,8 +299,10 @@ public class SingleCategorySettings extends SiteSettingsPreferenceFragment
                     browserContextHandle, getArguments().getString(EXTRA_CATEGORY, ""));
         }
 
-        assert !(mCategory.showSites(SiteSettingsCategory.Type.ALL_SITES)
-                || mCategory.showSites(SiteSettingsCategory.Type.USE_STORAGE));
+        if (mCategory.showSites(SiteSettingsCategory.Type.ALL_SITES)
+                || mCategory.showSites(SiteSettingsCategory.Type.USE_STORAGE)) {
+            throw new IllegalArgumentException("Use AllSiteSettings instead.");
+        }
 
         int contentType = mCategory.getContentSettingsType();
         mRequiresTriStateSetting =
