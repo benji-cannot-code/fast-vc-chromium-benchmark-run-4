e07496cfbda13fbdc834df7d253ce106f3607402
Jose Dapena Paz <jdapena@igalia.com>
2020-09-08 07:53:55 +0000

    IWYU: usage of std::bitset in ui::AXEventGenerator requires bitset include.
    Bug: 957519
    Change-Id: I7b763c1d5dee77d3a2b12f5169d6ad86e5dcbc7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2394164
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804853}

--- ui/accessibility/ax_event_generator.h
+++ ui/accessibility/ax_event_generator.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_ACCESSIBILITY_AX_EVENT_GENERATOR_H_
 #define UI_ACCESSIBILITY_AX_EVENT_GENERATOR_H_
 
+#include <bitset>
 #include <map>
 #include <ostream>
 #include <set>
