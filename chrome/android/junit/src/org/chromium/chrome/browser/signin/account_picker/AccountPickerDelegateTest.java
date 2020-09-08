89a5cdc27c850dd989e5735f8db98b943d5d071b
Alice Wang <aliceywang@chromium.org>
2020-09-08 15:30:50 +0000

    [Android][WebSignin] Remove all ChromeActivity usage in delegate
    This CL removes all ChromeActivity usage in AccountPickerDelegate.
    
    Bug: 1125068
    Change-Id: Ib2afc7f5d8ab1ebe8138e96aff41cdd1c1c8dea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2398129
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804921}

--- chrome/android/junit/src/org/chromium/chrome/browser/signin/account_picker/AccountPickerDelegateTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/signin/account_picker/AccountPickerDelegateTest.java
@@ -15,7 +15,6 @@ import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import android.accounts.Account;
-import android.app.Activity;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -23,7 +22,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Answers;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InOrder;
@@ -31,11 +29,12 @@ import org.mockito.Mock;
 
 import org.chromium.base.Callback;
 import org.chromium.base.test.BaseRobolectricTestRunner;
-import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.SigninManager;
 import org.chromium.chrome.browser.signin.WebSigninBridge;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.TabCreator;
 import org.chromium.chrome.test.util.browser.signin.AccountManagerTestRule;
 import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.base.GoogleServiceAuthError;
@@ -43,8 +42,6 @@ import org.chromium.components.signin.base.GoogleServiceAuthError.State;
 import org.chromium.components.signin.identitymanager.IdentityManager;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.base.WindowAndroid;
-
-import java.lang.ref.WeakReference;
 
 /**
  * This class tests the {@link AccountPickerDelegate}.
@@ -71,8 +68,11 @@ public class AccountPickerDelegateTest {
     @Mock
     private WindowAndroid mWindowAndroidMock;
 
-    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
-    private ChromeActivity mChromeActivityMock;
+    @Mock
+    private Tab mTabMock;
+
+    @Mock
+    private TabCreator mTabCreatorMock;
 
     @Captor
     private ArgumentCaptor<LoadUrlParams> mLoadUrlParamsCaptor;
@@ -90,10 +90,8 @@ public class AccountPickerDelegateTest {
         when(IdentityServicesProvider.get().getIdentityManager(any())).thenReturn(mIdentityManager);
         when(IdentityServicesProvider.get().getSigninManager(any())).thenReturn(mSigninManagerMock);
 
-        when(mWindowAndroidMock.getActivity())
-                .thenReturn(new WeakReference<Activity>(mChromeActivityMock));
-        mDelegate = new AccountPickerDelegate(
-                mWindowAndroidMock, mWebSigninBridgeFactoryMock, CONTINUE_URL);
+        mDelegate = new AccountPickerDelegate(mWindowAndroidMock, mTabMock, mTabCreatorMock,
+                mWebSigninBridgeFactoryMock, CONTINUE_URL);
         when(mWebSigninBridgeFactoryMock.create(eq(mProfileMock), any(), eq(mDelegate)))
                 .thenReturn(mWebSigninBridgeMock);
     }
@@ -114,7 +112,7 @@ public class AccountPickerDelegateTest {
                 .create(mProfileMock, coreAccountInfo, mDelegate);
         calledInOrder.verify(mSigninManagerMock).signin(eq(coreAccountInfo), any());
         mDelegate.onSigninSucceded();
-        verify(mChromeActivityMock.getActivityTab()).loadUrl(mLoadUrlParamsCaptor.capture());
+        verify(mTabMock).loadUrl(mLoadUrlParamsCaptor.capture());
         LoadUrlParams loadUrlParams = mLoadUrlParamsCaptor.getValue();
         Assert.assertEquals("Continue url does not match!", CONTINUE_URL, loadUrlParams.getUrl());
     }
