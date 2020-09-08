ef1414d43137dd340caccfae1aeb7c09ef1fbcdf
Matthias Körber <koerber@google.com>
2020-09-08 12:37:26 +0000

    [Autofill][Leizig] Add parsing for house number fields.
    Change-Id: Ied46d737413cfb5fcd1eae5995c2bea8c1be50d4
    Bug: 978014, 1099202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2282752
    Auto-Submit: Matthias Körber <koerber@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/master@{#804899}

--- chrome/browser/autofill/form_structure_browsertest.cc
+++ chrome/browser/autofill/form_structure_browsertest.cc
@@ -153,6 +153,8 @@ FormStructureBrowserTest::FormStructureBrowserTest()
       {
           // TODO(crbug.com/1098943): Remove once experiment is over.
           autofill::features::kAutofillEnableSupportForMoreStructureInNames,
+          // TODO(crbug.com/1125978): Remove once launched.
+          autofill::features::kAutofillEnableSupportForHouseNumbers,
       },
       // Disabled
       {autofill::features::kAutofillEnforceMinRequiredFieldsForHeuristics,
