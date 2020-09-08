075cf8aac8fb7e9df6081cf92a93daccf39c0927
cfredric <cfredric@google.com>
2020-09-08 13:49:48 +0000

    Remove unused user_data_dir argument.
    Change-Id: I1ad064b9da87821b578d9f183a7a78ba8da5929a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2393056
    Auto-Submit: Chris Fredrickson <cfredric@google.com>
    Reviewed-by: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804904}

--- chrome/browser/component_updater/trust_token_key_commitments_component_installer.cc
+++ chrome/browser/component_updater/trust_token_key_commitments_component_installer.cc
@@ -167,8 +167,7 @@ TrustTokenKeyCommitmentsComponentInstallerPolicy::GetMimeTypes() const {
 }
 
 void RegisterTrustTokenKeyCommitmentsComponentIfTrustTokensEnabled(
-    ComponentUpdateService* cus,
-    const base::FilePath& user_data_dir) {
+    ComponentUpdateService* cus) {
   if (!base::FeatureList::IsEnabled(network::features::kTrustTokens))
     return;
 
