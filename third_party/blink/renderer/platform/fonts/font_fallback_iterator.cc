f932fdaf8d2dd5872f25b2b33a90adfce795a9fc
Alex Turner <alexmt@chromium.org>
2020-09-08 17:17:40 +0000

    Fix font metrics recording to only include font data in output digest
    Currently in FontMatchingMetrics, the check_type and is_loading_fallback
    flag are included as part of the output digest. However, these do not
    represent an output. Instead, they represent different kinds of lookup
    or a temporary state. By including them in the output digest, we cannot
    determine if two lookups at different points in the font matching
    process resulted in the same font or not.
    
    We remove check_type and instead split the IdentifiableSurface::Type to
    account for different kinds of lookup, including splitting lookups that
    can match only unique font names (i.e. src:local specifiers) and lookups
    that can also match family names (i.e. font-family specifiers). We adapt
    FontMatchingMetrics' HashMaps accordingly.
    
    We also ensure other lookup values are appropriate by:
     - including FontFallbackPriority for lookups by fallback character,
     - handling FontDescription::GenericFamilyType properly,
     - considering font names to be case-insensitive, and
     - ignoring any lookups where is_loading_fallback is true.
    
    Bug: 1121669
    Change-Id: I398ad1359c5c6bae51b8bb8b4f236c1944c3e000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2376037
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804957}

--- third_party/blink/renderer/platform/fonts/font_fallback_iterator.cc
+++ third_party/blink/renderer/platform/fonts/font_fallback_iterator.cc
@@ -141,7 +141,6 @@ scoped_refptr<FontDataForRangeSet> FontFallbackIterator::Next(
     if (FontSelector* font_selector = font_fallback_list_->GetFontSelector()) {
       font_selector->ReportLastResortFallbackFontLookup(
           font_description_,
-          LocalFontLookupType::kLastResortInFontFallbackIterator,
           last_resort.get());
     }
 
@@ -233,8 +232,7 @@ scoped_refptr<SimpleFontData> FontFallbackIterator::FallbackPriorityFont(
 
   if (FontSelector* font_selector = font_fallback_list_->GetFontSelector()) {
     font_selector->ReportFontLookupByFallbackCharacter(
-        hint, font_description_, LocalFontLookupType::kFallbackPriorityFont,
-        font_data.get());
+        hint, font_fallback_priority_, font_description_, font_data.get());
   }
   return font_data;
 }
@@ -281,8 +279,7 @@ scoped_refptr<SimpleFontData> FontFallbackIterator::UniqueSystemFontForHintList(
 
   if (FontSelector* font_selector = font_fallback_list_->GetFontSelector()) {
     font_selector->ReportFontLookupByFallbackCharacter(
-        hint, font_description_, LocalFontLookupType::kSystemFallbackFont,
-        font_data.get());
+        hint, FontFallbackPriority::kText, font_description_, font_data.get());
   }
   return font_data;
 }
