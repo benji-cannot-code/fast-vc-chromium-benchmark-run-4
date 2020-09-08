70f5ca8035bc6029dc7d93714bb512cb0ca900eb
Fredrik Söderqvist <fs@opera.com>
2020-09-08 00:43:16 +0000

    Reduce size of dactyloscoper.h
    Reduces the estimated pre-processed size by ~2.1M.
    
    Bug: 242216
    Change-Id: I68ea30c3c7437dffc5667d26d8972276c7d79792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2396302
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#804817}

--- third_party/blink/renderer/core/frame/dactyloscoper.cc
+++ third_party/blink/renderer/core/frame/dactyloscoper.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
+#include "third_party/blink/renderer/core/svg/svg_string_list_tear_off.h"
 #include "third_party/blink/renderer/platform/privacy_budget/identifiability_digest_helpers.h"
 
 namespace blink {
