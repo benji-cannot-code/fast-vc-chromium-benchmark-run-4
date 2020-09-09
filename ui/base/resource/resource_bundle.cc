917276c992f7e7ed982ce80e65108ff484f23656
Etienne Bergeron <etienneb@chromium.org>
2020-09-09 18:49:17 +0000

    Make const the Resource Bundle APIs
    This CL is making const the function that are not modifying the
    state of the resource bundle.
    
    This step is required to make the transition to a Reader/Writer
    lock.
    
    Bug: 1123115
    Change-Id: I764a8657fe358ce8cc5097d7ca7140a48682afd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401324
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805414}

--- ui/base/resource/resource_bundle.cc
+++ ui/base/resource/resource_bundle.cc
@@ -493,12 +493,12 @@ void ResourceBundle::OverrideLocaleStringResource(
   overridden_locale_strings_[resource_id] = string;
 }
 
-const base::FilePath& ResourceBundle::GetOverriddenPakPath() {
+const base::FilePath& ResourceBundle::GetOverriddenPakPath() const {
   return overridden_pak_path_;
 }
 
 base::string16 ResourceBundle::MaybeMangleLocalizedString(
-    const base::string16& str) {
+    const base::string16& str) const {
   if (!mangle_localized_strings_)
     return str;
 
@@ -721,7 +721,7 @@ base::string16 ResourceBundle::GetLocalizedString(int resource_id) {
 }
 
 base::RefCountedMemory* ResourceBundle::LoadLocalizedResourceBytes(
-    int resource_id) {
+    int resource_id) const {
   {
     base::AutoLock lock_scope(*locale_resources_data_lock_);
     base::StringPiece data;
@@ -1056,7 +1056,7 @@ gfx::Image& ResourceBundle::GetEmptyImage() {
   return empty_image_;
 }
 
-base::string16 ResourceBundle::GetLocalizedStringImpl(int resource_id) {
+base::string16 ResourceBundle::GetLocalizedStringImpl(int resource_id) const {
   base::string16 string;
   if (delegate_ && delegate_->GetLocalizedString(resource_id, &string))
     return MaybeMangleLocalizedString(string);
