f7051163319b07642be90ad0651e61904b808633
sandromaggi <sandromaggi@google.com>
2020-09-09 11:23:02 +0000

    [Autofill Assistant] Failing |GetAutofillAgent| calls
    There are some crashes from |WebController| when resetting the keyboard
    state through the |AutofillAgent|. These can happen if the
    |RenderFrameHost| is no longer valid when the final callback to clean up
    the state is executed.
    
    Bug: 1119918
    Change-Id: I9bcd69ae125a1ab7918bc5671cb3b95c4cc82d71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2396121
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Marian Fechete <marianfe@google.com>
    Reviewed-by: Maxim Kolosovskiy  <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805275}

--- components/autofill_assistant/browser/web/web_controller.h
+++ components/autofill_assistant/browser/web/web_controller.h
@@ -28,13 +28,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/web/element_position_getter.h"
 #include "components/autofill_assistant/browser/web/element_rect_getter.h"
 #include "components/autofill_assistant/browser/web/web_controller_worker.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "third_party/icu/source/common/unicode/umachine.h"
 #include "url/gurl.h"
 
 namespace autofill {
 class AutofillProfile;
 class CreditCard;
-class ContentAutofillDriver;
 struct FormData;
 struct FormFieldData;
 }  // namespace autofill
@@ -255,11 +255,13 @@ class WebController {
 
   // RAII object that sets the action state to "running" when the object is
   // allocated and to "not running" when it gets deallocated.
-  class ScopedAssistantActionStateRunning {
+  class ScopedAssistantActionStateRunning
+      : private content::WebContentsObserver {
    public:
     explicit ScopedAssistantActionStateRunning(
-        autofill::ContentAutofillDriver* content_autofill_driver);
-    ~ScopedAssistantActionStateRunning();
+        content::WebContents* web_contents,
+        content::RenderFrameHost* render_frame_host);
+    ~ScopedAssistantActionStateRunning() override;
 
     ScopedAssistantActionStateRunning(
         const ScopedAssistantActionStateRunning&) = delete;
@@ -269,7 +271,11 @@ class WebController {
    private:
     void SetAssistantActionState(bool running);
 
-    autofill::ContentAutofillDriver* content_autofill_driver_;
+    // Overrides content::WebContentsObserver:
+    void RenderFrameDeleted(
+        content::RenderFrameHost* render_frame_host) override;
+
+    content::RenderFrameHost* render_frame_host_;
   };
 
   void OnJavaScriptResult(
