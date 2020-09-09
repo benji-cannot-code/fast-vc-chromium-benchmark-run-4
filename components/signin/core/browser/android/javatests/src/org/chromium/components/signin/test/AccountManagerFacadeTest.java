cb09aef95864d272d3429e651aebbfda8a04451c
Alice Wang <aliceywang@chromium.org>
2020-09-09 17:51:06 +0000

    [Android][Test] Batch AccountManagerFacadeTest
    This CL batches AccountManagerFacadeTest.
    
    Bug: 1120258
    Change-Id: I8dd38caa05b3d689b022d2e1f14ac959b346f51a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2398479
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805391}

--- components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/AccountManagerFacadeTest.java
+++ components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/AccountManagerFacadeTest.java
@@ -13,12 +13,14 @@ import android.support.test.InstrumentationRegistry;
 
 import androidx.test.filters.SmallTest;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.Batch;
 import org.chromium.components.signin.AccountManagerDelegateException;
 import org.chromium.components.signin.AccountManagerFacadeImpl;
 import org.chromium.components.signin.AccountManagerFacadeProvider;
@@ -30,8 +32,9 @@ import java.util.concurrent.CountDownLatch;
  * Tests for {@link AccountManagerFacade}. See also {@link AccountManagerFacadeRobolectricTest}.
  */
 @RunWith(BaseJUnit4ClassRunner.class)
+@Batch(Batch.UNIT_TESTS)
 public class AccountManagerFacadeTest {
-    private FakeAccountManagerDelegate mDelegate =
+    private final FakeAccountManagerDelegate mDelegate =
             new FakeAccountManagerDelegate(FakeAccountManagerDelegate.ENABLE_BLOCK_GET_ACCOUNTS);
 
     @Before
@@ -40,6 +43,11 @@ public class AccountManagerFacadeTest {
             AccountManagerFacadeProvider.setInstanceForTests(
                     new AccountManagerFacadeImpl(mDelegate));
         });
+    }
+
+    @After
+    public void tearDown() {
+        AccountManagerFacadeProvider.resetInstanceForTests();
     }
 
     @Test
