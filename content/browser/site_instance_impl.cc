3c2dec797ab81592ec908544d64b9ac2f24f3133
Aaron Colwell <acolwell@chromium.org>
2020-09-09 23:01:12 +0000

    Change DoesSiteInfoRequireDedicatedProcess() param to SiteInfo.
    This change converts the site_url param to a SiteInfo in preparation
    for adding new checks that are based on other SiteInfo fields. This is
    part of an ongoing effort to replace site URL usage in method
    signatures with SiteInfo.
    
    Bug: 1085275
    Change-Id: I67cc62db168f1d107890f66970041796ea9dcb8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401333
    Commit-Queue: Aaron Colwell <acolwell@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Aaron Colwell <acolwell@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805520}

--- content/browser/site_instance_impl.cc
+++ content/browser/site_instance_impl.cc
@@ -640,9 +640,8 @@ bool SiteInstanceImpl::IsSuitableForURL(const GURL& url) {
     // If the site URLs do not match, but neither this SiteInstance nor the
     // destination site_url require dedicated processes, then it is safe to use
     // this SiteInstance.
-    if (!RequiresDedicatedProcess() &&
-        !DoesSiteURLRequireDedicatedProcess(GetIsolationContext(),
-                                            site_info.site_url())) {
+    if (!RequiresDedicatedProcess() && !DoesSiteInfoRequireDedicatedProcess(
+                                           GetIsolationContext(), site_info)) {
       return true;
     }
 
@@ -661,8 +660,7 @@ bool SiteInstanceImpl::RequiresDedicatedProcess() {
   if (!has_site_)
     return false;
 
-  return DoesSiteURLRequireDedicatedProcess(GetIsolationContext(),
-                                            site_info_.site_url());
+  return DoesSiteInfoRequireDedicatedProcess(GetIsolationContext(), site_info_);
 }
 
 void SiteInstanceImpl::IncrementActiveFrameCount() {
@@ -1178,8 +1176,7 @@ bool SiteInstanceImpl::CanBePlacedInDefaultSiteInstance(
 
   // Allow the default SiteInstance to be used for sites that don't need to be
   // isolated in their own process.
-  return !DoesSiteURLRequireDedicatedProcess(isolation_context,
-                                             site_info.site_url());
+  return !DoesSiteInfoRequireDedicatedProcess(isolation_context, site_info);
 }
 
 // static
@@ -1210,22 +1207,23 @@ bool SiteInstanceImpl::DoesSiteRequireDedicatedProcess(
     const GURL& url) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   return SiteIsolationPolicy::UseDedicatedProcessesForAllSites() ||
-         DoesSiteURLRequireDedicatedProcess(
+         DoesSiteInfoRequireDedicatedProcess(
              isolation_context,
-             SiteInstanceImpl::ComputeSiteInfo(isolation_context, url)
-                 .site_url());
+             SiteInstanceImpl::ComputeSiteInfo(isolation_context, url));
 }
 
 // static
-bool SiteInstanceImpl::DoesSiteURLRequireDedicatedProcess(
+bool SiteInstanceImpl::DoesSiteInfoRequireDedicatedProcess(
     const IsolationContext& isolation_context,
-    const GURL& site_url) {
+    const SiteInfo& site_info) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   DCHECK(isolation_context.browser_or_resource_context());
 
   // If --site-per-process is enabled, site isolation is enabled everywhere.
   if (SiteIsolationPolicy::UseDedicatedProcessesForAllSites())
     return true;
+
+  const GURL& site_url = site_info.site_url();
 
   // Always require a dedicated process for isolated origins.
   auto* policy = ChildProcessSecurityPolicyImpl::GetInstance();
@@ -1267,15 +1265,13 @@ bool SiteInstanceImpl::ShouldLockProcess(
       isolation_context.browser_or_resource_context().ToBrowserContext();
   DCHECK(browser_context);
 
-  const GURL& site_url = site_info.site_url();
-
   // Don't lock to origin in --single-process mode, since this mode puts
   // cross-site pages into the same process.  Note that this also covers the
   // single-process mode in Android Webview.
   if (RenderProcessHost::run_renderer_in_process())
     return false;
 
-  if (!DoesSiteURLRequireDedicatedProcess(isolation_context, site_url))
+  if (!DoesSiteInfoRequireDedicatedProcess(isolation_context, site_info))
     return false;
 
   // Guest processes cannot be locked to their site because guests always have
@@ -1285,6 +1281,8 @@ bool SiteInstanceImpl::ShouldLockProcess(
   // RenderFrame routing id.
   if (is_guest)
     return false;
+
+  const GURL& site_url = site_info.site_url();
 
   // Most WebUI processes should be locked on all platforms.  The only exception
   // is NTP, handled via the separate callout to the embedder.
