5ed809c139da5aff868d3d6ba1deb87a0bd18d40
Noah Rose Ledesma <noahrose@google.com>
2020-09-09 20:09:47 +0000

    GMC: Fix UAF in MaybeRemoveDefaultDevice
    This change eliminates a possible reference invalidation. Previously, a
    pointer to an element in a vector was being dereferenced after erase
    was called.
    
    Bug: 1126351
    Change-Id: Icfca65af49a1ddc716915dc16d5e4bb6ea45b149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401509
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/master@{#805458}

--- chrome/browser/ui/global_media_controls/media_notification_device_provider_impl.cc
+++ chrome/browser/ui/global_media_controls/media_notification_device_provider_impl.cc
@@ -45,10 +45,11 @@ void MaybeRemoveDefaultDevice(media::AudioDeviceDescriptions& descriptions) {
             default_device_name_prefix.size());
 
     // Find all the devices that have the name of the real default device.
-    std::vector<media::AudioDeviceDescription*> devices_with_real_default_name;
-    for (auto& description : descriptions) {
-      if (description.device_name == real_default_device_name) {
-        devices_with_real_default_name.push_back(&description);
+    std::vector<media::AudioDeviceDescriptions::iterator>
+        devices_with_real_default_name;
+    for (auto it = descriptions.begin(); it != descriptions.end(); ++it) {
+      if (it->device_name == real_default_device_name) {
+        devices_with_real_default_name.push_back(it);
       }
     }
 
@@ -57,9 +58,9 @@ void MaybeRemoveDefaultDevice(media::AudioDeviceDescriptions& descriptions) {
       // there is no ambiguity as to if this device is the real default device.
       // In this case, we should remove the "default" fallback device from the
       // list and mark the real device as "default".
-      descriptions.erase(default_device_it);
       devices_with_real_default_name.front()->unique_id =
           media::AudioDeviceDescription::kDefaultDeviceId;
+      descriptions.erase(default_device_it);
     }
   }
 }
@@ -128,7 +129,6 @@ void MediaNotificationDeviceProviderImpl::NotifySubscribers(
     media::AudioDeviceDescriptions descriptions) {
   is_querying_for_output_devices_ = false;
   audio_device_descriptions_ = std::move(descriptions);
-  MaybeRemoveDefaultDevice(audio_device_descriptions_);
   has_device_list_ = true;
   output_device_callback_list_.Notify(audio_device_descriptions_);
 }
