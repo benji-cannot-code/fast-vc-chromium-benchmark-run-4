f92701cb7442c4f717dbcdf7d7ee8ce19f30d3f9
Oleh Lamzin <lamzin@google.com>
2020-09-09 15:29:21 +0000

    Use FlushForTesting for remote commands
    Use new method ServiceConnection::FlushForTesting instead of
    RunUntilIdle.
    
    Change-Id: If8b30b4b7a42206af83ae248a109590316e46a3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2398546
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/master@{#805328}

--- chrome/browser/chromeos/policy/remote_commands/device_command_get_routine_update_job_unittest.cc
+++ chrome/browser/chromeos/policy/remote_commands/device_command_get_routine_update_job_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chromeos/dbus/cros_healthd/cros_healthd_client.h"
 #include "chromeos/dbus/cros_healthd/fake_cros_healthd_client.h"
+#include "chromeos/services/cros_healthd/public/cpp/service_connection.h"
 #include "chromeos/services/cros_healthd/public/mojom/cros_healthd_diagnostics.mojom.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -168,9 +169,8 @@ DeviceCommandGetRoutineUpdateJobTest::DeviceCommandGetRoutineUpdateJobTest() {
 DeviceCommandGetRoutineUpdateJobTest::~DeviceCommandGetRoutineUpdateJobTest() {
   chromeos::CrosHealthdClient::Shutdown();
 
-  // Wait for DeviceCommandGetRoutineUpdateJobTest to observe the
-  // destruction of the client.
-  base::RunLoop().RunUntilIdle();
+  // Wait for ServiceConnection to observe the destruction of the client.
+  chromeos::cros_healthd::ServiceConnection::GetInstance()->FlushForTesting();
 }
 
 void DeviceCommandGetRoutineUpdateJobTest::InitializeJob(
