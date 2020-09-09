7ae5d3054d26aaff2890411cefeccc9a4bd990c9
Aaron Colwell <acolwell@chromium.org>
2020-09-09 15:16:42 +0000

    Rename single param GetSiteInfoForURL() to ComputeSiteInfoForURL()
    Renamed the single param version of GetSiteInfoForURL() to
    ComputeSiteInfoForURL() to make it a little more clear that these
    functions behave differently in subtle ways. The new name reflects
    that this method behaves like SiteInstanceImpl::ComputeSiteInfo().
    This change also updates a few pieces of code to go through this
    renamed method so that it is clear that each of these places expect the
    same behavior. There are no behavior changes.
    
    Bug: 1085275
    Change-Id: Ie1a1911c5d92ba620789f456f10cdc732ee95244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2399450
    Auto-Submit: Aaron Colwell <acolwell@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805320}

--- content/browser/browsing_instance.cc
+++ content/browser/browsing_instance.cc
@@ -100,14 +100,14 @@ SiteInfo BrowsingInstance::GetSiteInfoForURL(const GURL& url,
   if (site_instance)
     return site_instance->GetSiteInfo();
 
-  return SiteInstanceImpl::ComputeSiteInfo(isolation_context_, url);
+  return ComputeSiteInfoForURL(url);
 }
 
 bool BrowsingInstance::TrySettingDefaultSiteInstance(
     SiteInstanceImpl* site_instance,
     const GURL& url) {
   DCHECK(!site_instance->HasSite());
-  const SiteInfo site_info = GetSiteInfoForURL(url);
+  const SiteInfo site_info = ComputeSiteInfoForURL(url);
   if (default_site_instance_ ||
       !SiteInstanceImpl::CanBePlacedInDefaultSiteInstance(
           isolation_context_, url, site_info.site_url())) {
@@ -125,7 +125,7 @@ bool BrowsingInstance::TrySettingDefaultSiteInstance(
 scoped_refptr<SiteInstanceImpl> BrowsingInstance::GetSiteInstanceForURLHelper(
     const GURL& url,
     bool allow_default_instance) {
-  const SiteInfo site_info = GetSiteInfoForURL(url);
+  const SiteInfo site_info = ComputeSiteInfoForURL(url);
   auto i = site_instance_map_.find(site_info);
   if (i != site_instance_map_.end())
     return i->second;
@@ -223,7 +223,7 @@ BrowsingInstance::~BrowsingInstance() {
   policy->RemoveOptInIsolatedOriginsForBrowsingInstance(isolation_context_);
 }
 
-SiteInfo BrowsingInstance::GetSiteInfoForURL(const GURL& url) const {
+SiteInfo BrowsingInstance::ComputeSiteInfoForURL(const GURL& url) const {
   return SiteInstanceImpl::ComputeSiteInfo(isolation_context_, url);
 }
 
