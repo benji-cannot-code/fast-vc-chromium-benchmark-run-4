27bf5f85e0ec029e570754776fb471ab01693bb6
Caroline Rising <corising@chromium.org>
2020-09-09 17:10:14 +0000

    Disable read later star menu option for non-http/https urls.
    This restriction comes from ReadingListModel's implementation.
    
    Bug: 1117023
    Change-Id: Ibc3f05b7d9523e1dde3661a9559936adb12c52f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2399015
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805372}

--- chrome/browser/ui/views/location_bar/star_menu_model.h
+++ chrome/browser/ui/views/location_bar/star_menu_model.h
@@ -13,6 +13,7 @@ class StarMenuModel : public ui::SimpleMenuModel {
  public:
   StarMenuModel(ui::SimpleMenuModel::Delegate* delegate,
                 bool bookmarked,
+                bool can_move_to_read_later,
                 bool exists_as_unread_in_read_later);
   StarMenuModel(const StarMenuModel&) = delete;
   StarMenuModel& operator=(const StarMenuModel&) = delete;
@@ -25,7 +26,9 @@ class StarMenuModel : public ui::SimpleMenuModel {
   };
 
  private:
-  void Build(bool bookmarked, bool exists_as_unread_in_read_later);
+  void Build(bool bookmarked,
+             bool can_move_to_read_later,
+             bool exists_as_unread_in_read_later);
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_LOCATION_BAR_STAR_MENU_MODEL_H_
