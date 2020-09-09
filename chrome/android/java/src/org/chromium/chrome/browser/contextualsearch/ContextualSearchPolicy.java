e005f00a64656082925a5cc295d105f87017823d
Donn Denman <donnd@chromium.org>
2020-09-09 19:58:37 +0000

    [TTS] Remove special treatment of plain HTTP pages
    Removes the special treatment of plain HTTP pages with respect to
    privacy. These pages were considered privacy-insensitive, but now
    we treat them the same way as secure HTTPS pages.
    
    BUG=1124397
    
    Change-Id: Ic3f5640ffa020e7f4656a78965b6f87662c039a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2393526
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805448}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchPolicy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchPolicy.java
@@ -154,7 +154,7 @@ class ContextualSearchPolicy {
             return false;
         }
 
-        return isPromoAvailable() ? isBasePageHTTP(mNetworkCommunicator.getBasePageUrl()) : true;
+        return !isUserUndecided(); // The user must have decided on privacy to resolve page content.
     }
 
     /** @return Whether a long-press gesture can resolve. */
@@ -170,7 +170,7 @@ class ContextualSearchPolicy {
     boolean canSendSurroundings() {
         if (mDidOverrideDecidedStateForTesting) return mDecidedStateForTesting;
 
-        return isPromoAvailable() ? isBasePageHTTP(mNetworkCommunicator.getBasePageUrl()) : true;
+        return !isUserUndecided(); // The user must have decided on privacy to send page content.
     }
 
     /**
