fe17332236a1d65e945c266783636dc00a382a15
Oleg Davydov <burunduk@chromium.org>
2020-09-09 11:02:43 +0000

    Add test for SessionLengthLimiter suspend case
    When device is suspened during the session, session length limit should
    apply correctly. The new test simulates suspend and resume and verifies
    that session will be terminated at correct time.
    
    Bug: b:160454261
    Change-Id: I382d5bd3c7b5a64e49783ed97eec08e19ff91810
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283591
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805269}

--- base/test/test_mock_time_task_runner.cc
+++ base/test/test_mock_time_task_runner.cc
@@ -234,6 +234,11 @@ void TestMockTimeTaskRunner::AdvanceMockTickClock(TimeDelta delta) {
   ForwardClocksUntilTickTime(NowTicks() + delta);
 }
 
+void TestMockTimeTaskRunner::AdvanceWallClock(TimeDelta delta) {
+  now_ += delta;
+  OnAfterTimePassed();
+}
+
 void TestMockTimeTaskRunner::RunUntilIdle() {
   DCHECK(thread_checker_.CalledOnValidThread());
   ProcessAllTasksNoLaterThan(TimeDelta());
