ff5a44618159b8606da6cf653a02478781cab398
Arnaud Mandy <arnaud.mandy@intel.com>
2020-09-09 11:10:45 +0000

    WPT: Refactor contacts tests to use test-only-api.js
    use of test-only-api.js in preparation for launching official MojoJS support
    in WPT. This would not change the test results on Chromium waterfall
    (everything should continue to pass) or upstream WPT (tests currently
    fail because MojoJS isn't enabled).
    
    Bug: 1123989
    Change-Id: Ib7fed092b37243b82fbbf38d871bb75728cb7744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2395395
    Commit-Queue: Arnaud Mandy <arnaud.mandy@intel.com>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805272}

--- third_party/blink/web_tests/external/wpt/contacts/contacts-select.https.window.js
+++ third_party/blink/web_tests/external/wpt/contacts/contacts-select.https.window.js
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/test-only-api.js
 // META: script=/resources/testdriver.js
 // META: script=/resources/testdriver-vendor.js
 // META: script=resources/helpers.js
