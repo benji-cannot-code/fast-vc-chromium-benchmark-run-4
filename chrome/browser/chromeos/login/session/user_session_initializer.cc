c825ac3c3b0f272b8606ef2ed7ddd0ca847c4eed
cfredric <cfredric@google.com>
2020-09-09 16:02:11 +0000

    Remove unused user_data_dir parameter.
    Change-Id: Iabb1bdac97627239293f4077631ad0519876da56
    Bug: 1126207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2398944
    Commit-Queue: Chris Fredrickson <cfredric@google.com>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@google.com>
    Cr-Commit-Position: refs/heads/master@{#805341}

--- chrome/browser/chromeos/login/session/user_session_initializer.cc
+++ chrome/browser/chromeos/login/session/user_session_initializer.cc
@@ -100,7 +100,7 @@ void UserSessionInitializer::OnUserProfileLoaded(const AccountId& account_id) {
   if (user_manager::UserManager::Get()->GetPrimaryUser() == user) {
     InitRlz(profile);
     InitializeCerts(profile);
-    InitializeCRLSetFetcher(user);
+    InitializeCRLSetFetcher();
     InitializeCertificateTransparencyComponents(user);
     InitializePrimaryProfileServices(profile, user);
 
@@ -153,17 +153,11 @@ void UserSessionInitializer::InitializeCerts(Profile* profile) {
   }
 }
 
-void UserSessionInitializer::InitializeCRLSetFetcher(
-    const user_manager::User* user) {
-  const std::string username_hash = user->username_hash();
-  if (!username_hash.empty()) {
-    base::FilePath path =
-        ProfileHelper::GetProfilePathByUserIdHash(username_hash);
-    component_updater::ComponentUpdateService* cus =
-        g_browser_process->component_updater();
-    if (cus)
-      component_updater::RegisterCRLSetComponent(cus, path);
-  }
+void UserSessionInitializer::InitializeCRLSetFetcher() {
+  component_updater::ComponentUpdateService* cus =
+      g_browser_process->component_updater();
+  if (cus)
+    component_updater::RegisterCRLSetComponent(cus);
 }
 
 void UserSessionInitializer::InitializeCertificateTransparencyComponents(
