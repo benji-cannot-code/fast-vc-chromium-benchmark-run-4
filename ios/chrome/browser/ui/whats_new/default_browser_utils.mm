68eb21b0e1e08a27735539ea389d78eba4b91985
Chris Lu <thegreenfrog@chromium.org>
2020-09-09 16:42:39 +0000

    [ios] Refine Default Browser Promo trigger
    Instead of relying on SceneState order calls, which cannot be
    guaranteed, a new AppState shouldShowDefaultBrowserPromo property is
    added to keep track of when a resumption of session is due to a cold
    start.
    
    A new DefaultBrowserSceneAgent class will listen to SceneActivationLevel
    and check the shouldShowDefaultBrowserPromo property to determine
    when to show the promo.
    
    Also, an iOS14 check is added to IsLikelyInterestedDefaultBrowserUser to
    ensure that only users on iOS14 see this promo.
    
    Bug: 1107489
    Change-Id: I71c77a98e20644c620f4c4d3c60cb0b253931cc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378710
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805357}

--- ios/chrome/browser/ui/whats_new/default_browser_utils.mm
+++ ios/chrome/browser/ui/whats_new/default_browser_utils.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/whats_new/default_browser_utils.h"
 
+#include "base/ios/ios_util.h"
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -88,5 +90,5 @@ bool IsLikelyInterestedDefaultBrowserUser() {
       [[[NSUserDefaults standardUserDefaults]
           arrayForKey:kLastSignificantUserEvent] mutableCopy];
   pastUserEvents = SanitizePastUserEvents(pastUserEvents);
-  return [pastUserEvents count] > 1;
+  return [pastUserEvents count] > 1 && base::ios::IsRunningOnIOS14OrLater();
 }
