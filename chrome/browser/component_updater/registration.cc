2eb9c6bf1012f4a2c172c33fd88023d8666d9e72
cfredric <cfredric@google.com>
2020-09-10 17:09:25 +0000

    Remove unused user_data_dir parameter in OriginTrialsComponentInstaller.
    Change-Id: Ifff3526b22063147a403c33a323fd3a86d9efa74
    Bug: 1126207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2398963
    Auto-Submit: Chris Fredrickson <cfredric@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805810}

--- chrome/browser/component_updater/registration.cc
+++ chrome/browser/component_updater/registration.cc
@@ -143,8 +143,6 @@ void RegisterComponentsForUpdate(bool is_off_the_record_profile,
     // Chrome OS: On Chrome OS, this cleanup is delayed until user login.
     component_updater::DeleteLegacySTHSet(path);
 #endif
-
-    RegisterOriginTrialsComponent(cus, path);
   }
   RegisterSSLErrorAssistantComponent(cus);
   RegisterFileTypePoliciesComponent(cus);
@@ -155,6 +153,7 @@ void RegisterComponentsForUpdate(bool is_off_the_record_profile,
   component_updater::RegisterCRLSetComponent(cus);
 #endif  // !defined(OS_CHROMEOS)
 
+  RegisterOriginTrialsComponent(cus);
   RegisterMediaEngagementPreloadComponent(cus, base::OnceClosure());
 
 #if defined(OS_WIN)
