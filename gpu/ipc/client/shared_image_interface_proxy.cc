3821503a7e7b5f7da7546ebbcb0a2060cf3a92e4
Maksim Ivanov <emaxx@chromium.org>
2020-09-10 12:35:02 +0000

    Fix use-after-move in //gpu/ipc/client/
    Fix use-after-move (potential) bugs found by the
    "bugprone-use-after-move" clang-tidy check.
    
    Bug: 1122844
    Change-Id: I666b338a5060005e6b79bf009449d80d0ceb988a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2385300
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805705}

--- gpu/ipc/client/shared_image_interface_proxy.cc
+++ gpu/ipc/client/shared_image_interface_proxy.cc
@@ -203,7 +203,7 @@ Mailbox SharedImageInterfaceProxy::CreateSharedImage(
   }
 
   base::AutoLock lock(lock_);
-  AddMailbox(params.mailbox, usage);
+  AddMailbox(mailbox, usage);
   return mailbox;
 }
 
