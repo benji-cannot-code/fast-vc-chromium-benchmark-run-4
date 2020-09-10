8d9e92a01f87bb90c1505e77d4ec8e708d29d82a
John Rummell <jrummell@chromium.org>
2020-09-10 00:06:45 +0000

    Check for context destroyed in MediaKeys
    Don't allow calls to proceed once the associated content has been
    destroyed.
    
    Bug: 1121414
    Test: example in the bug no longer crashes
    Change-Id: I3bdeb86f2020f684958b624fcc30438babfb5004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2378889
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805561}

--- third_party/blink/renderer/modules/encryptedmedia/media_keys_get_status_for_policy.h
+++ third_party/blink/renderer/modules/encryptedmedia/media_keys_get_status_for_policy.h
@@ -21,7 +21,8 @@ class MediaKeysGetStatusForPolicy {
  public:
   static ScriptPromise getStatusForPolicy(ScriptState*,
                                           MediaKeys&,
-                                          const MediaKeysPolicy*);
+                                          const MediaKeysPolicy*,
+                                          ExceptionState&);
 };
 
 }  // namespace blink
