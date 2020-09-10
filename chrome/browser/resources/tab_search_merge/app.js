b606e0938baf40a42ecd73a524c20f7003974560
Yuheng Huang <yuhengh@chromium.org>
2020-09-10 08:04:56 -0700

    Sort tabs by most recent active time
    This CL needs to be checked in before the related CL to prevent a revert
    due to src-internal build test breakage.
    
    Related CL: https://chromium-review.googlesource.com/c/chromium/src/+/2382655
    Bug: 1099917
    Change-Id: Id79e1fa21be1bf0bdea52aa902f6685d8c985a4a
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/chrome/browser/resources/tab_search/+/3261333
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819593}

--- chrome/browser/resources/tab_search_merge/app.js
+++ chrome/browser/resources/tab_search_merge/app.js
@@ -289,6 +289,9 @@ export class TabSearchAppElement extends PolymerElement {
             })
             .flat() :
         windowTabs.map(window => window.tabs).flat();
+    this.filteredOpenTabs_.sort((a, b) => (b.lastActiveTimeTicks && a.lastActiveTimeTicks) ?
+        b.lastActiveTimeTicks.internalValue - a.lastActiveTimeTicks.internalValue :
+        0);
   }
 }
 
