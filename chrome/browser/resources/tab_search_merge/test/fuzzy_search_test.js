e1be88d6f1bc6fce4b955c4e6e36b1edc3987fb1
Yuheng Huang <yuhengh@chromium.org>
2020-09-10 15:17:01 -0700

    Implement fuzzy search using fuse.js
    Search for item.title and item.hostname using weighted search from
    fuse.js. Scores are calculated from matching item.title and item.hostname.
    Items are sorted by scores by desc so better matching items will show up
    first. Also extract item.hostname from item.url.
    
    Bug: 1099917
    Change-Id: Ieaee008b778dda90ef08ed504da034080b71cc58
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/chrome/browser/resources/tab_search/+/3222931
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819594}

---
+++ chrome/browser/resources/tab_search_merge/test/fuzzy_search_test.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {fuzzySearch} from 'chrome://tab-search/fuzzy_search.js'
+
+import {assertDeepEquals, assertEquals} from '../../chai_assert.js';
+
+suite('FuzzySearchTest', () => {
+  test('fuzzySearch', () => {
+    const records = [
+      {
+        title: 'OpenGL',
+        hostname: 'https://www.opengl.org',
+      },
+      {
+        title: 'Google',
+        hostname: 'https://www.google.com',
+      },
+    ];
+    assertDeepEquals(
+        [
+          records[1],
+          records[0],
+        ],
+        fuzzySearch('gle', records));
+    assertDeepEquals(records, fuzzySearch('', records));
+    assertDeepEquals([], fuzzySearch('z', records));
+  });
+});
