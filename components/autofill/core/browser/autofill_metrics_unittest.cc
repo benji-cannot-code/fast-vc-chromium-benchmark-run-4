0c7550cb7864b5d410bdcc6007a8c360bce44685
Eugene But <eugenebut@chromium.org>
2020-09-10 19:45:50 +0000

    [ios] Disable AutofillMetricsTest.FrameHasNoForm on iOS 12 device
    Test is consistently failing on downstream bots (iPhone and iPad device)
    
    Bug: 1127005
    TBR: kazinova@chromium.org
    Change-Id: I317f1df8363dfe5139810d8082209e0e9434e57b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404404
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805870}

--- components/autofill/core/browser/autofill_metrics_unittest.cc
+++ components/autofill/core/browser/autofill_metrics_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64.h"
 #include "base/feature_list.h"
+#include "base/ios/ios_util.h"
 #include "base/macros.h"
 #include "base/metrics/metrics_hashes.h"
 #include "base/metrics/statistics_recorder.h"
@@ -10422,6 +10423,13 @@ TEST_F(AutofillMetricsTest,
 // Verify that we don't log Autofill.WebOTP.OneTimeCode.FromAutocomplete if the
 // frame has no form.
 TEST_F(AutofillMetricsTest, FrameHasNoForm) {
+#if defined(OS_IOS) && !TARGET_IPHONE_SIMULATOR
+  // TODO(crbug.com/1127005): Test is failing on iOS 12 device.
+  if (!base::ios::IsRunningOnOrLater(13, 0, 0)) {
+    return;
+  }
+#endif
+
   autofill_manager_.reset();
   EXPECT_FALSE(base::StatisticsRecorder::FindHistogram(
       "Autofill.WebOTP.OneTimeCode.FromAutocomplete"));
