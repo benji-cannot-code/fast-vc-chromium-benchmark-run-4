be14cf3cb4928d67dc45a0e348f6c4bce369ff32
My Nguyen <myy@chromium.org>
2020-09-10 03:47:36 +0000

    [OsSettingsLanguages] Add Spell check languages list for input_page
    Follow mock at http://go/cros-lang-settings-ux-slide#slide=23
    
    Will add other components in following CLs.
    Current view enabled by policy: http://screen/37LZsEYnjeA5qLk
    When spell check is disabled: http://screen/7xgUwHJz82HMMpK
    View when no spell check language: http://screen/Aedx2EsgeFKUFys
    
    Note: All strings are not finalised so they are translateable false
    and no screenshots required.
    
    Bug: 1113439
    Change-Id: I1ce9110f3f1d5a295ccc815ae346de3460a340f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360093
    Commit-Queue: My Nguyen <myy@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805610}

--- chrome/test/data/webui/settings/chromeos/test_os_languages_metrics_proxy.js
+++ chrome/test/data/webui/settings/chromeos/test_os_languages_metrics_proxy.js
@@ -20,6 +20,7 @@ cr.define('settings', function() {
         'recordAddLanguages',
         'recordManageInputMethods',
         'recordToggleShowInputOptionsOnShelf',
+        'recordToggleSpellCheck',
         'recordToggleTranslate',
         'recordAddInputMethod',
       ]);
@@ -43,6 +44,11 @@ cr.define('settings', function() {
     /** @override */
     recordToggleShowInputOptionsOnShelf(value) {
       this.methodCalled('recordToggleShowInputOptionsOnShelf', value);
+    }
+
+    /** @override */
+    recordToggleSpellCheck(value) {
+      this.methodCalled('recordToggleSpellCheck', value);
     }
 
     /** @override */
