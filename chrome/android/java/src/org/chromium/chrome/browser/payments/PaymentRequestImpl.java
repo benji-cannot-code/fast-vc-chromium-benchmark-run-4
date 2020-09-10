200599fc8106a66c04bf76952d968a881e07e620
Zarah Tesfai <ztesfai@google.com>
2020-09-10 18:39:12 +0000

    Move mMinimalUI to PaymentUIManager
    Purpose: Move Minimal UI related logic to PaymentUIsManager
    
    Changes:
    * Added get/setMinimalUI() methods in PaymentUIsManager
    * Moved testing related methods
    * Added hideMinimalUI() method
    
    
    Bug: 1108099
    Change-Id: Ibb35f92f8f61c68056b37ee5f9019e7a7f05cb7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2381063
    Commit-Queue: Zarah Tesfai <ztesfai@google.com>
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805865}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
@@ -25,7 +25,6 @@ import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.NormalizedAddressRequestDelegate;
 import org.chromium.chrome.browser.payments.handler.PaymentHandlerCoordinator;
 import org.chromium.chrome.browser.payments.handler.PaymentHandlerCoordinator.PaymentHandlerWebContentsObserver;
-import org.chromium.chrome.browser.payments.minimal.MinimalUICoordinator;
 import org.chromium.chrome.browser.payments.ui.ContactDetailsSection;
 import org.chromium.chrome.browser.payments.ui.PaymentInformation;
 import org.chromium.chrome.browser.payments.ui.PaymentRequestUI;
@@ -36,7 +35,6 @@ import org.chromium.chrome.browser.payments.ui.ShoppingCart;
 import org.chromium.chrome.browser.settings.SettingsLauncher;
 import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
 import org.chromium.components.autofill.EditableOption;
-import org.chromium.components.browser_ui.bottomsheet.BottomSheetControllerProvider;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.components.payments.AbortReason;
 import org.chromium.components.payments.BrowserPaymentRequest;
@@ -188,7 +186,6 @@ public class PaymentRequestImpl
     private int mShippingType;
     private boolean mIsFinishedQueryingPaymentApps;
     private List<PaymentApp> mPendingApps = new ArrayList<>();
-    private MinimalUICoordinator mMinimalUi;
     private PaymentApp mInvokedPaymentApp;
     private boolean mHideServerAutofillCards;
     private boolean mWaitForUpdatedDetails;
@@ -426,7 +423,7 @@ public class PaymentRequestImpl
     public void show(boolean isUserGesture, boolean waitForUpdatedDetails) {
         if (mComponentPaymentRequestImpl == null) return;
 
-        if (mPaymentUIsManager.getPaymentRequestUI() != null || mMinimalUi != null) {
+        if (mPaymentUIsManager.isShowingUI()) {
             // Can be triggered only by a compromised renderer. In normal operation, calling show()
             // twice on the same instance of PaymentRequest in JavaScript is rejected at the
             // renderer level.
@@ -517,7 +514,14 @@ public class PaymentRequestImpl
             assert mPaymentUIsManager.getPaymentRequestUI() != null;
 
             if (isMinimalUiApplicable()) {
-                triggerMinimalUi(chromeActivity);
+                if (mPaymentUIsManager.triggerMinimalUI(chromeActivity, mSpec.getRawTotal(),
+                        this::onMinimalUIReady, this::onMinimalUiConfirmed,
+                        this::onMinimalUiDismissed)) {
+                    mDidRecordShowEvent = true;
+                    mJourneyLogger.setEventOccurred(Event.SHOWN);
+                } else {
+                    disconnectFromClientWithDebugMessage(ErrorStrings.MINIMAL_UI_SUPPRESSED);
+                }
                 return;
             }
 
@@ -554,30 +558,6 @@ public class PaymentRequestImpl
         return PaymentFeatureList.isEnabled(PaymentFeatureList.WEB_PAYMENTS_MINIMAL_UI);
     }
 
-    /**
-     * Triggers the minimal UI.
-     * @param chromeActivity The Android activity for the Chrome UI that will host the minimal UI.
-     */
-    private void triggerMinimalUi(ChromeActivity chromeActivity) {
-        // Do not show the Payment Request UI dialog even if the minimal UI is suppressed.
-        mPaymentUIsManager.getPaymentUisShowStateReconciler().onBottomSheetShown();
-
-        mMinimalUi = new MinimalUICoordinator();
-        if (mMinimalUi.show(chromeActivity,
-                    BottomSheetControllerProvider.from(chromeActivity.getWindowAndroid()),
-                    (PaymentApp) mPaymentUIsManager.getPaymentMethodsSection().getSelectedItem(),
-                    mPaymentUIsManager.getCurrencyFormatterMap().get(
-                            mSpec.getRawTotal().amount.currency),
-                    mPaymentUIsManager.getUiShoppingCart().getTotal(), this::onMinimalUIReady,
-                    this::onMinimalUiConfirmed, this::onMinimalUiDismissed)) {
-            mDidRecordShowEvent = true;
-            mJourneyLogger.setEventOccurred(Event.SHOWN);
-            return;
-        }
-
-        disconnectFromClientWithDebugMessage(ErrorStrings.MINIMAL_UI_SUPPRESSED);
-    }
-
     private void onMinimalUIReady() {
         if (ComponentPaymentRequestImpl.getNativeObserverForTest() != null) {
             ComponentPaymentRequestImpl.getNativeObserverForTest().onMinimalUIReady();
@@ -606,6 +586,14 @@ public class PaymentRequestImpl
             mComponentPaymentRequestImpl.onComplete();
         }
         close();
+        closeUIAndDestroyNativeObjects();
+    }
+
+    private void onPaymentRequestCompleteForNonMinimalUI() {
+        if (ComponentPaymentRequestImpl.getNativeObserverForTest() != null) {
+            ComponentPaymentRequestImpl.getNativeObserverForTest().onCompleteCalled();
+        }
+
         closeUIAndDestroyNativeObjects();
     }
 
@@ -741,9 +729,7 @@ public class PaymentRequestImpl
 
     @VisibleForTesting(otherwise = VisibleForTesting.NONE)
     private boolean confirmMinimalUIForTestInternal() {
-        if (mMinimalUi == null) return false;
-        mMinimalUi.confirmForTest();
-        return true;
+        return mPaymentUIsManager.confirmMinimalUIForTest();
     }
 
     /**
@@ -759,9 +745,7 @@ public class PaymentRequestImpl
 
     @VisibleForTesting(otherwise = VisibleForTesting.NONE)
     private boolean dismissMinimalUIForTestInternal() {
-        if (mMinimalUi == null) return false;
-        mMinimalUi.dismissForTest();
-        return true;
+        return mPaymentUIsManager.dismissMinimalUIForTest();
     }
 
     /**
@@ -1308,28 +1292,8 @@ public class PaymentRequestImpl
                     mSpec.getRawTotal().amount.value, true /*completed*/);
         }
 
-        /** Update records of the used payment app for sorting payment apps next time. */
-        EditableOption selectedPaymentMethod =
-                mPaymentUIsManager.getPaymentMethodsSection().getSelectedItem();
-        PaymentPreferencesUtil.increasePaymentAppUseCount(selectedPaymentMethod.getIdentifier());
-        PaymentPreferencesUtil.setPaymentAppLastUseDate(
-                selectedPaymentMethod.getIdentifier(), System.currentTimeMillis());
-
-        if (mMinimalUi != null) {
-            if (result == PaymentComplete.FAIL) {
-                mMinimalUi.showErrorAndClose(
-                        this::onMinimalUiErroredAndClosed, R.string.payments_error_message);
-            } else {
-                mMinimalUi.showCompleteAndClose(this::onMinimalUiCompletedAndClosed);
-            }
-            return;
-        }
-
-        if (ComponentPaymentRequestImpl.getNativeObserverForTest() != null) {
-            ComponentPaymentRequestImpl.getNativeObserverForTest().onCompleteCalled();
-        }
-
-        closeUIAndDestroyNativeObjects();
+        mPaymentUIsManager.onPaymentRequestComplete(result, this::onMinimalUiErroredAndClosed,
+                this::onMinimalUiCompletedAndClosed, this::onPaymentRequestCompleteForNonMinimalUI);
     }
 
     // Implement BrowserPaymentRequest:
@@ -1873,9 +1837,9 @@ public class PaymentRequestImpl
     public void onInstrumentDetailsError(String errorMessage) {
         if (mComponentPaymentRequestImpl == null) return;
         mInvokedPaymentApp = null;
-        if (mMinimalUi != null) {
+        if (mPaymentUIsManager.getMinimalUI() != null) {
             mJourneyLogger.setAborted(AbortReason.ABORTED_BY_USER);
-            mMinimalUi.showErrorAndClose(
+            mPaymentUIsManager.getMinimalUI().showErrorAndClose(
                     this::onMinimalUiErroredAndClosed, R.string.payments_error_message);
             return;
         }
@@ -1945,10 +1909,7 @@ public class PaymentRequestImpl
      */
     private void closeUIAndDestroyNativeObjects() {
         mPaymentUIsManager.ensureHideAndResetPaymentHandlerUi();
-        if (mMinimalUi != null) {
-            mMinimalUi.hide();
-            mMinimalUi = null;
-        }
+        mPaymentUIsManager.hideMinimalUI();
 
         if (mPaymentUIsManager.getPaymentRequestUI() != null) {
             mPaymentUIsManager.getPaymentRequestUI().close();
