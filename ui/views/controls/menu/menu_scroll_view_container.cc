0d5185dc591f088aa5bde5476bd1e01993c8f4d0
Federico Paredes <fedpar@microsoft.com>
2020-09-10 20:25:33 +0000

    macOS: Use ignored accessibility role for MenuScrollViewContainer.
    MenuScrollViewContainer now has a kIgnored (instead of kMenuBar) role on
    Mac. This allows the SubmenuView to be recognized as a menu by VoiceOver,
    so it can now read the total number of MenuItemViews inside it when
    navigating via keyboard.
    
    Bug: 954701
    Change-Id: I4ee11c09d3331d55899ece6741f9821cbd3b2285
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2402098
    Commit-Queue: Federico Paredes <fedpar@microsoft.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805884}

--- ui/views/controls/menu/menu_scroll_view_container.cc
+++ ui/views/controls/menu/menu_scroll_view_container.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "build/build_config.h"
 #include "cc/paint/paint_flags.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/accessibility/ax_enums.mojom.h"
@@ -253,7 +254,16 @@ void MenuScrollViewContainer::OnPaintBackground(gfx::Canvas* canvas) {
 void MenuScrollViewContainer::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   // Get the name from the submenu view.
   content_view_->GetAccessibleNodeData(node_data);
+
+  // On macOS, NSMenus are not supposed to have anything wrapped around them. To
+  // allow VoiceOver to recognize this as a menu and to read aloud the total
+  // number of items inside it, we ignore the MenuScrollViewContainer (which
+  // holds the menu itself: the SubmenuView).
+#if defined(OS_MAC)
+  node_data->role = ax::mojom::Role::kIgnored;
+#else
   node_data->role = ax::mojom::Role::kMenuBar;
+#endif
 }
 
 void MenuScrollViewContainer::OnBoundsChanged(
