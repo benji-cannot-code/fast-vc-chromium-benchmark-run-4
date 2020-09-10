532a396852d8f2d459353340d0704111ddc1d9d2
Olivier Robin <olivierrobin@google.com>
2020-09-10 16:33:24 +0000

    Use ScopedKeyWindow in SelectorCoordinatorTest
    Change-Id: Ic3b4511444a691de76537616be0f82193a953dae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2403561
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805781}

--- ios/chrome/browser/ui/elements/selector_coordinator_unittest.mm
+++ ios/chrome/browser/ui/elements/selector_coordinator_unittest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/main/test_browser.h"
 #import "ios/chrome/browser/ui/elements/selector_picker_view_controller.h"
 #import "ios/chrome/browser/ui/elements/selector_view_controller_delegate.h"
+#import "ios/chrome/test/scoped_key_window.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
 #include "testing/platform_test.h"
@@ -32,8 +33,8 @@ using SelectorCoordinatorTest = PlatformTest;
 // that invoking stop dismisses the view and invokes the delegate.
 TEST_F(SelectorCoordinatorTest, StartAndStop) {
   base::test::TaskEnvironment task_environment_;
-
-  UIWindow* keyWindow = [[UIApplication sharedApplication] keyWindow];
+  ScopedKeyWindow scopedKeyWindow;
+  UIWindow* keyWindow = scopedKeyWindow.Get();
   UIViewController* rootViewController = keyWindow.rootViewController;
   std::unique_ptr<Browser> browser_ = std::make_unique<TestBrowser>();
   SelectorCoordinator* coordinator =
@@ -46,9 +47,10 @@ TEST_F(SelectorCoordinatorTest, StartAndStop) {
                 rootViewController.presentedViewController);
 
     [coordinator stop];
-    base::test::ios::WaitUntilCondition(^{
+    bool success = base::test::ios::WaitUntilConditionOrTimeout(1.0, ^{
       return !rootViewController.presentedViewController;
     });
+    EXPECT_TRUE(success);
   };
   // Ensure any other presented controllers are dismissed before starting the
   // coordinator.
@@ -60,7 +62,8 @@ TEST_F(SelectorCoordinatorTest, StartAndStop) {
 TEST_F(SelectorCoordinatorTest, Delegate) {
   base::test::TaskEnvironment task_environment_;
 
-  UIWindow* keyWindow = [[UIApplication sharedApplication] keyWindow];
+  ScopedKeyWindow scopedKeyWindow;
+  UIWindow* keyWindow = scopedKeyWindow.Get();
   UIViewController* rootViewController = keyWindow.rootViewController;
   std::unique_ptr<Browser> browser_ = std::make_unique<TestBrowser>();
   SelectorCoordinator* coordinator =
@@ -77,9 +80,10 @@ TEST_F(SelectorCoordinatorTest, Delegate) {
                   didCompleteWithSelection:testOption];
     [coordinator selectorViewController:coordinator.selectorPickerViewController
                         didSelectOption:testOption];
-    base::test::ios::WaitUntilCondition(^{
+    bool success = base::test::ios::WaitUntilConditionOrTimeout(1.0, ^{
       return !rootViewController.presentedViewController;
     });
+    EXPECT_TRUE(success);
   };
   // Ensure any other presented controllers are dismissed before starting the
   // coordinator.
