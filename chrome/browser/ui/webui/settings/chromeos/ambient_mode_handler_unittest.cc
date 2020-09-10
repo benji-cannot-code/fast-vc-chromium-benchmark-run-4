0f40fb16a20c5356e1d7a3c9ec34f3e9feb4e0a7
wutao <wutao@chromium.org>
2020-09-10 08:29:25 +0000

    ambient: Enable photo preview for album selection
    Bug: b/163888215
    Test: passed current tests
    Change-Id: I200f0f03d5ed71d6cdc317aecfd7698aa6f43472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2399911
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805651}

--- chrome/browser/ui/webui/settings/chromeos/ambient_mode_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/chromeos/ambient_mode_handler_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/ambient/common/ambient_settings.h"
 #include "ash/public/cpp/ambient/fake_ambient_backend_controller_impl.h"
+#include "ash/public/cpp/test/test_image_downloader.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "content/public/test/test_web_ui.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -47,6 +48,7 @@ class AmbientModeHandlerTest : public testing::Test {
     handler_->AllowJavascript();
     fake_backend_controller_ =
         std::make_unique<ash::FakeAmbientBackendControllerImpl>();
+    image_downloader_ = std::make_unique<ash::TestImageDownloader>();
   }
 
   void RequestSettings() {
@@ -185,6 +187,7 @@ class AmbientModeHandlerTest : public testing::Test {
   std::unique_ptr<content::TestWebUI> web_ui_;
   std::unique_ptr<ash::FakeAmbientBackendControllerImpl>
       fake_backend_controller_;
+  std::unique_ptr<ash::TestImageDownloader> image_downloader_;
   std::unique_ptr<TestAmbientModeHandler> handler_;
 };
 
