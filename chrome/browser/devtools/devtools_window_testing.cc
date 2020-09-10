966839801ef4e9aa296610d44db158519b81d7e8
Ghazale Hosseinabadi <ghazale@google.com>
2020-09-10 18:33:20 +0000

    [DevTools] Support newly created devtools window in DTWCObserver.
    DevToolsWindowCreationObserver::WaitForLoad clears its MessageLoopRunner
    right when DevToolsWindowCreated is called, this means the runner would
    be destroyed while its QuitClosure is running. This CL fixes the issue
    by moving the destruction after MessageLoopRunner::Run().
    
    This bug is evident when there is no DevToolsWindow at DTWCObserver
    instantiation time. This CL adds a regression test for that.
    
    Bug: 1126105
    Change-Id: I68b122c657abbc9a0ecca7875ed19dc6d33914eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2363886
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805859}

--- chrome/browser/devtools/devtools_window_testing.cc
+++ chrome/browser/devtools/devtools_window_testing.cc
@@ -196,6 +196,7 @@ void DevToolsWindowCreationObserver::Wait() {
     return;
   runner_ = new content::MessageLoopRunner();
   runner_->Run();
+  runner_ = nullptr;
 }
 
 void DevToolsWindowCreationObserver::WaitForLoad() {
@@ -207,10 +208,8 @@ void DevToolsWindowCreationObserver::WaitForLoad() {
 void DevToolsWindowCreationObserver::DevToolsWindowCreated(
     DevToolsWindow* devtools_window) {
   devtools_windows_.push_back(devtools_window);
-  if (runner_.get()) {
+  if (runner_.get())
     runner_->QuitClosure().Run();
-    runner_ = nullptr;
-  }
 }
 
 DevToolsWindow* DevToolsWindowCreationObserver::devtools_window() {
