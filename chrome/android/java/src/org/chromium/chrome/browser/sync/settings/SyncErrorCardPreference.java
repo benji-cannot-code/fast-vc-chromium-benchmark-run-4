92b9fe876b6a0639147a91f2dd8ac4a7e9d9b8fa
Tanmoy Mollik <triploblastic@chromium.org>
2020-09-10 16:31:14 +0000

    [Android] Remove unused string in PersonalizedSigninPromoView
    This cl removes signin_promo_status_message string. This string was used
    in personalized sync promos. But this string is now hidden and no longer
    shown on the UI for sync promos.
    It is replaced by sync_error_card_title that is used in SyncErrorCards.
    
    Bug: 1124701
    Change-Id: Id111d1df6fbee1b336b253867161cfb9b3c217fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2391086
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805778}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/settings/SyncErrorCardPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/settings/SyncErrorCardPreference.java
@@ -128,7 +128,6 @@ public class SyncErrorCardPreference extends Preference
 
         errorCardView.getDismissButton().setVisibility(View.GONE);
         errorCardView.getStatusMessage().setVisibility(View.VISIBLE);
-        errorCardView.getStatusMessage().setText(R.string.sync_error_card_title);
         errorCardView.getDescription().setText(
                 SyncSettingsUtils.getSyncErrorHint(getContext(), mSyncError));
 
