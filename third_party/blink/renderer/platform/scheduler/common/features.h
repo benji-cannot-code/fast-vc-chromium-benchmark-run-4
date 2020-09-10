f497a6cb172b617adf92763fb847a1815eaae59f
Tsuyoshi Horo <horo@chromium.org>
2020-09-10 22:24:15 +0000

    Remove unused BlinkSchedulerHighPriorityInputOnCompositorThread
    Bug: 1053702
    Change-Id: I65814fdd8ba45c46195141c5d5303bf49baf2c81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2397996
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805947}

--- third_party/blink/renderer/platform/scheduler/common/features.h
+++ third_party/blink/renderer/platform/scheduler/common/features.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 namespace scheduler {
 
-const base::Feature kHighPriorityInputOnCompositorThread{
-    "BlinkSchedulerHighPriorityInputOnCompositorThread",
-    base::FEATURE_DISABLED_BY_DEFAULT};
-
 const base::Feature kDedicatedWorkerThrottling{
     "BlinkSchedulerWorkerThrottling", base::FEATURE_DISABLED_BY_DEFAULT};
 
