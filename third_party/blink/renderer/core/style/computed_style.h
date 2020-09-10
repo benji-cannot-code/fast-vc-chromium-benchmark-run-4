c23c749214f7888dfec759d0750e2ff09982bfd8
Alison Maher <almaher@microsoft.com>
2020-09-10 17:38:50 +0000

    Visited background-color alpha channel in Forced Colors Mode
    Handle :visited background-colors in a similar way to the normal
    background-color property in Forced Colors Mode.
    
    Bug: 970285
    Change-Id: I1dd0ac2530f3dec7de4b79de459d34e1d2ae193b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2393299
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#805825}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -2938,6 +2938,7 @@ class ComputedStyle : public ComputedStyleBase,
   FRIEND_TEST_ALL_PREFIXES(ComputedStyleTest, InitialVariableNames);
   FRIEND_TEST_ALL_PREFIXES(ComputedStyleTest,
                            InitialAndInheritedAndNonInheritedVariableNames);
+  FRIEND_TEST_ALL_PREFIXES(StyleCascadeTest, ForcedVisitedBackgroundColor);
 };
 
 inline bool ComputedStyle::HasAnyPseudoElementStyles() const {
