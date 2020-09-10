30f15574ff25a69d056e1ee0fdf5b83e0d9b07aa
Roman Aleksandrov <raleksandrov@google.com>
2020-09-10 19:49:28 +0000

    oobe-carousel: Improve UX and A11y of dots navigation.
    Make dots clickable and add proper aria-labels and role.
    Add aria-label to slides for better spoken feedback.
    
    Bug: 1125620
    Change-Id: I6f20d4b1e3293aa89c81d4a6da960b6fd0fb0b6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2396084
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805876}

--- chrome/browser/resources/chromeos/login/components/oobe_carousel.js
+++ chrome/browser/resources/chromeos/login/components/oobe_carousel.js
@@ -36,6 +36,21 @@ Polymer({
       value: 10,
       observer: 'restartAutoTransition_',
     },
+
+    /**
+     * Slide aria-label.
+     */
+    slideLabel: String,
+
+    /**
+     * Selected button aria-label.
+     */
+    selectedButtonLabel: String,
+
+    /**
+     * Unselected button aria-label.
+     */
+    unselectedButtonLabel: String,
   },
 
   /**
@@ -71,23 +86,35 @@ Polymer({
   attached() {
     this.removeAnimateToHandler = this.removeAnimateTo_.bind(this);
     this.removeAnimateFromHandler = this.removeAnimateFrom_.bind(this);
-    this.countSlides_();
+    this.prepareCarousel_();
     this.restartAutoTransition_();
     this.hideNonActiveSlides_();
   },
 
   /**
    * @private
-   * Count slides and create dots.
+   * Count slides and create dots. Set a11y label on slides.
    */
-  countSlides_() {
+  prepareCarousel_() {
     this.slides = this.$.slot.assignedElements();
     this.totalSlides = this.slides.length;
     let array = [];
     for (let i = 0; i < this.totalSlides; ++i) {
+      this.slides[i].setAttribute('aria-label', this.getSlideLabel_(i));
+      this.slides[i].setAttribute('role', 'group');
       array.push(i);
     }
     this.dots = array;
+  },
+
+  /**
+   * @private
+   * @param {number} index Index of slide.
+   * Returns string label for slide.
+   */
+  getSlideLabel_(index) {
+    return loadTimeData.getStringF(
+        this.slideLabel, index + 1, this.totalSlides);
   },
 
   /**
@@ -171,7 +198,6 @@ Polymer({
           toIndex, fromIndex, 'backward', 'forward');
     }
   },
-
 
   /**
    * @private
@@ -279,11 +305,45 @@ Polymer({
 
   /**
    * @private
+   * @param {Event} e keypress event.
+   * On key press function.
+   */
+  onKeypress_(e) {
+    // Space (32) and enter (13) key codes.
+    if (e.keyCode == 32 || e.keyCode == 13)
+      this.slideIndex = e.model.item;
+  },
+
+  /**
+   * @private
+   * @param {Event} e click event.
+   * On dot click function.
+   */
+  onClick_(e) {
+    this.slideIndex = e.model.item;
+  },
+
+  /**
+   * @private
    * @param {number} index Index of dot.
    * Returns whether a dot is active.
    */
   isActive_(index) {
     return index == this.slideIndex;
+  },
+
+  /**
+   * @private
+   * @param {number} index Index of slide.
+   * Returns string label for dot.
+   */
+  getDotLabel_(index) {
+    if (index == this.slideIndex) {
+      return loadTimeData.getStringF(
+          this.selectedButtonLabel, index + 1, this.totalSlides);
+    }
+    return loadTimeData.getStringF(
+        this.unselectedButtonLabel, index + 1, this.totalSlides);
   },
 
   /**
