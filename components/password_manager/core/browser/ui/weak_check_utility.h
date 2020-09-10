0c16411bc464377532782af96e0fa82b52c82d6b
Irina Fedorova <irfedorova@google.com>
2020-09-10 13:50:55 +0000

    Add StartWeakCheck to InsecureCredentialsManager
    This CL adds a simple version of the StartWeakCheck function to the
    InsecureCredentialsManager that doesn't use the zxcvbn-cpp library yet.
    Checking long passwords is implemented as it will be in the final
    version of password weakness check. It also adds the GetWeakCredentials
    function to the InsecureCredentialsManager.
    
    Bug: 1119752
    Change-Id: I50013c9824701fdccabda7a76644f37c90fa6227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2398622
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805723}

---
+++ components/password_manager/core/browser/ui/weak_check_utility.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_UI_WEAK_CHECK_UTILITY_H_
+#define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_UI_WEAK_CHECK_UTILITY_H_
+
+#include "base/containers/flat_set.h"
+#include "base/strings/string16.h"
+#include "components/password_manager/core/browser/ui/saved_passwords_presenter.h"
+
+namespace password_manager {
+
+// Checks each password for weakness.
+base::flat_set<base::string16> BulkWeakCheck(
+    SavedPasswordsPresenter::SavedPasswordsView passwords);
+
+}  // namespace password_manager
+
+#endif  // COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_UI_WEAK_CHECK_UTILITY_H_
