5310f6361d1fc3e846b0f81241e41a66788b0c94
Shengfa Lin <shengfa@google.com>
2020-09-10 22:36:47 +0000

    [chromedriver] Navigation completes prematurely if OOPIF loads before main page
    Chrome has recent behavior change that it sends out load event fired
    for Out of Process Iframe(OOPIF). When ChromeDriver received it, it assumed that it's for the
    main page causing navigation to complete prematurely.
    
    The fix is to check the load event fired come from main page.
    
    Bug: chromedriver:3594
    Change-Id: Icdc589ec6733c28e549b9110ea225fe7df00ce01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2402323
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Shengfa Lin <shengfa@google.com>
    Cr-Commit-Position: refs/heads/master@{#805953}

--- chrome/test/chromedriver/chrome/devtools_client_impl.cc
+++ chrome/test/chromedriver/chrome/devtools_client_impl.cc
@@ -340,7 +340,7 @@ DevToolsClientImpl::ResponseInfo::ResponseInfo(const std::string& method)
 
 DevToolsClientImpl::ResponseInfo::~ResponseInfo() {}
 
-DevToolsClientImpl* DevToolsClientImpl::GetRootClient() {
+DevToolsClient* DevToolsClientImpl::GetRootClient() {
   return parent_ ? parent_ : this;
 }
 
@@ -371,7 +371,8 @@ Status DevToolsClientImpl::SendCommandInternal(
     VLOG(1) << "DevTools WebSocket Command: " << method << " (id=" << command_id
             << ") " << id_ << " " << FormatValueForDisplay(params);
   }
-  SyncWebSocket* socket = GetRootClient()->socket_.get();
+  SyncWebSocket* socket =
+      static_cast<DevToolsClientImpl*>(GetRootClient())->socket_.get();
   if (!socket->Send(message)) {
     return Status(kDisconnected, "unable to send message to renderer");
   }
--- chrome/test/chromedriver/chrome/stub_devtools_client.cc
+++ chrome/test/chromedriver/chrome/stub_devtools_client.cc
@@ -96,3 +96,7 @@ Status StubDevToolsClient::HandleReceivedEvents() {
 void StubDevToolsClient::SetDetached() {}
 
 void StubDevToolsClient::SetOwner(WebViewImpl* owner) {}
+
+DevToolsClient* StubDevToolsClient::GetRootClient() {
+  return this;
+}
