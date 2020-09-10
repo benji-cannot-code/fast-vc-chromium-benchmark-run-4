67910162a3dc4325d922276a962eb70316704c4d
Keren Zhu <kerenzhu@chromium.org>
2020-09-10 01:42:43 +0000

    Add IPH Snooze UI
    * Add MdIPHBubbleButton, a transparent MD button with optional
    white border.
    * Add snoozable option to IPH bubble. Disable auto-dismiss for
    snoozable button.
    
    UX spec: https://docs.google.com/presentation/d/1l19RBFdJjsLIO2KPVANY-ObfwmUCkpQ1Lj8IeLAqJZM/edit#slide=id.g92aa3ae4f7_1_43
    Screenshot: https://screenshot.googleplex.com/5brgyBvGcaoMeEP
    
    
    Bug: 1121399
    Change-Id: Ic3ca76b6edcb52bdc9d856aa6ee11476e018218d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2394157
    Reviewed-by: Wei Li <weili@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805592}

--- chrome/browser/ui/views/chrome_typography.cc
+++ chrome/browser/ui/views/chrome_typography.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/default_style.h"
 #include "ui/base/pointer/touch_ui_controller.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/font.h"
 #include "ui/gfx/platform_font.h"
 
 int GetFontSizeDeltaBoundedByAvailableHeight(int available_height,
@@ -100,5 +101,9 @@ void ApplyCommonFontStyles(int context,
       *size_delta = 15 - gfx::PlatformFont::kDefaultBaseFontSize;
       break;
 #endif
+    case CONTEXT_IPH_BUBBLE_BUTTON: {
+      *size_delta = GetFontSizeDeltaIgnoringUserOrLocaleSettings(12);
+      break;
+    }
   }
 }
