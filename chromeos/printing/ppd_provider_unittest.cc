48e2b0d1e787a477bc2da6edc030c2c23534ba20
Luum Habtemariam <luum@chromium.org>
2020-09-10 18:02:06 +0000

    Releasing cached URLLoaderFactory* in PpdProvider
    The URLLoaderFactory interface grabbed from g_process can become
    invalidated, like on network changes, so we can't save it. This change
    modifies PpdProvider to grab it fresh before every fetch.
    
    Test: chromeos_unittests passes
    Bug: chromium:1123567
    Change-Id: I2718548c45b61b34d78c17966037e8ccb91d654f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2389020
    Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Luum Habtemariam <luum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805835}

--- chromeos/printing/ppd_provider_unittest.cc
+++ chromeos/printing/ppd_provider_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/bind_test_util.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_message_loop.h"
 #include "base/threading/sequenced_task_runner_handle.h"
@@ -95,8 +96,15 @@ class PpdProviderTest : public ::testing::Test {
     } else {
       ppd_cache_ = PpdCache::Create(ppd_cache_temp_dir_.GetPath());
     }
-    return PpdProvider::Create(locale, &loader_factory_, ppd_cache_,
-                               base::Version("40.8.6753.09"), provider_options);
+    return PpdProvider::Create(
+        locale, base::BindLambdaForTesting([this]() {
+          // Casting here is necessary b/c RepeatingCallback needs help
+          // coercing types.
+          // Casting is safe since its a guaranteed upcast.
+          return dynamic_cast<network::mojom::URLLoaderFactory*>(
+              &loader_factory_);
+        }),
+        ppd_cache_, base::Version("40.8.6753.09"), provider_options);
   }
 
   // Fill loader_factory_ with preset contents for test URLs
