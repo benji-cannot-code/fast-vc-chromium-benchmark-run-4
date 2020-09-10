e28ea785fd95abcd84973f6d1bfa1366be9a94e2
Mario Bianucci <mabian@microsoft.com>
2020-09-10 14:31:11 +0000

    include-what-you-use cleanups in surface.cc
    Add <limits> and <utility> includes to surface.cc to silence
    presubmit errors.
    
    Change-Id: I6d8cc83099e46608b1caf8372e1410d06dcda82f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401587
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Mario Bianucci <mabian@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#805740}

--- components/viz/service/surfaces/surface.cc
+++ components/viz/service/surfaces/surface.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <algorithm>
+#include <limits>
+#include <utility>
 
 #include "base/metrics/histogram_functions.h"
 #include "base/stl_util.h"
