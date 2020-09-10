248ee1c606ca5833f2377af9865e01b313be9785
Findit <findit-for-me@appspot.gserviceaccount.com>
2020-09-10 03:15:29 +0000

    Revert "Files SWA: Moving to a new directory."
    This reverts commit 4ccb650800cd7de83ae3cdb71d33bfd97cfe0d9f.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 805598 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzRjY2I2NTA4MDBjZDdkZTgzYWUzY2RiNzFkMzNiZmQ5N2NmZTBkOWYM
    
    Sample Failed Build: https://ci.chromium.org/b/8869579849484323600
    
    Sample Failed Step: compile
    
    Original change's description:
    > Files SWA: Moving to a new directory.
    > 
    > Bug: 1111229
    > Change-Id: Id0509caf9d1d9de8efa293344e682610cf1a8699
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2381330
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    > Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Commit-Queue: Bo Majewski <majewski@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#805598}
    
    
    Change-Id: Iaced93329ca1f5e37d6cec4f9e3fdf6b5ae8664f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1111229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401927
    Cr-Commit-Position: refs/heads/master@{#805604}

--- chrome/browser/resources/chromeos/file_manager/file_manager.js
+++ chrome/browser/resources/chromeos/file_manager/file_manager.js
@@ -21,6 +21,6 @@ theBrowserProxy.callbackRouter.onSomethingHappened.addListener(
       console.log('eh? ' + something + '. what? ' + other);
     });
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.info('File manager launched ...');
+document.addEventListener('DOMContentLoaded', function() {
+  alert('DOMContentLoaded');
 });
