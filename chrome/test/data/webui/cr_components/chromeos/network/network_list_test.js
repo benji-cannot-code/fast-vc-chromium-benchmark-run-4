2ec4cbfbd846040905ce60fc791a0c478b5561dc
James Hawkins <jhawkins@chromium.org>
2020-09-11 20:25:53 +0000

    cr_components/network: Convert NetworkSelect to Polymer 3.
    R=khorimoto@chromium.org
    
    Bug: 1045266
    Test: NetworkComponentsNetworkSelectTest*
    Change-Id: I7b10ffe2e7080941bddc5adeee3bb65e18fb0463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404071
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806307}

--- chrome/test/data/webui/cr_components/chromeos/network/network_list_test.js
+++ chrome/test/data/webui/cr_components/chromeos/network/network_list_test.js
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {getDeepActiveElement} from 'chrome://resources/js/util.m.js';
 // clang-format on
 
-suite('NetworkNetworkListTest', function() {
+suite('NetworkListTest', function() {
   /** @type {!NetworkList|undefined} */
   let networkList;
 
