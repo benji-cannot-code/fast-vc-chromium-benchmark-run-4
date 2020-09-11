8a093932965f6d0cec1a5f60950b323c701e43ba
Tim Song <tengs@chromium.org>
2020-09-11 22:42:19 +0000

    [CrOS PhoneHub] Hook up PhoneHubManager with tray UI.
    BUG=1106937,1126208
    
    Change-Id: I58ef9ad83bb18ee8193563820c3c0a223295e6a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2400765
    Commit-Queue: Tim Song <tengs@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806376}

--- ash/system/model/system_tray_model.cc
+++ ash/system/model/system_tray_model.cc
@@ -16,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/model/virtual_keyboard_model.h"
 #include "ash/system/network/active_network_icon.h"
 #include "ash/system/network/tray_network_state_model.h"
+#include "ash/system/phonehub/phone_hub_tray.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/system/unified/unified_system_tray.h"
+#include "chromeos/components/phonehub/phone_hub_manager.h"
 
 namespace ash {
 
@@ -118,7 +120,10 @@ void SystemTrayModel::ShowNetworkDetailedViewBubble(bool show_by_click) {
 
 void SystemTrayModel::SetPhoneHubManager(
     chromeos::phonehub::PhoneHubManager* phone_hub_manager) {
-  // TODO(tengs): Use |phone_hub_manager|.
+  auto* phone_hub_tray = Shell::GetPrimaryRootWindowController()
+                             ->GetStatusAreaWidget()
+                             ->phone_hub_tray();
+  phone_hub_tray->SetPhoneHubManager(phone_hub_manager);
 }
 
 }  // namespace ash
