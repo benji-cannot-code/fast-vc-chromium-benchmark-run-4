43067157567899deeacde1ca061fcf591c005e61
Harvey Yang <chenghaoyang@chromium.org>
2020-09-11 00:06:20 +0000

    Chromium: Add Sensors in chromeos/components
    This commit adds Sensor Hal Dispatcher, sensor mojo interfaces, and
    basic usages of them to connect clients to iioservice.
    
    BUG=chromium:1006141, b:162154663
    TEST=builds, unit tests and test on octopus: Chromium connects to
    iioservice as a DBus client, and invites iioservice into the mojo
    network. Chromium then waits for iioservice_client (test executable) to
    register and establish a connection between iioservice and
    iioservice_client. All re-connections between Chromium and iioservice
    work well.
    
    Change-Id: I9f9ed4267093191ad306c25216300f850dc42006
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332056
    Commit-Queue: Cheng-Hao Yang <chenghaoyang@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#805973}

---
+++ chromeos/components/sensors/fake_sensor_hal_client.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/sensors/fake_sensor_hal_client.h"
+
+namespace chromeos {
+namespace sensors {
+
+FakeSensorHalClient::FakeSensorHalClient() {}
+FakeSensorHalClient::~FakeSensorHalClient() = default;
+
+void FakeSensorHalClient::SetUpChannel(
+    mojo::PendingRemote<mojom::SensorService> sensor_service) {
+  DCHECK(!SensorServiceIsValid());
+  sensor_service_ = std::move(sensor_service);
+}
+
+mojo::PendingRemote<mojom::SensorHalClient> FakeSensorHalClient::PassRemote() {
+  CHECK(!receiver_.is_bound());
+  return receiver_.BindNewPipeAndPassRemote();
+}
+
+bool FakeSensorHalClient::SensorServiceIsValid() {
+  return sensor_service_.is_valid();
+}
+
+void FakeSensorHalClient::ResetSensorService() {
+  sensor_service_.reset();
+}
+
+}  // namespace sensors
+}  // namespace chromeos
