db890995d27a237ed3edbaa274db9cc79378c106
Chris Lu <thegreenfrog@chromium.org>
2020-09-11 05:33:39 +0000

    [ios] Add URL open user metric actions
    Adds user metric actions for default browser and first party
    URL opens. This will be used observe user action sequences.
    
    Bug: 1107489
    Change-Id: I7ba0290912a5568219c13b8c4c246d86d4ebf3c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2399084
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806063}

--- ios/chrome/app/startup/chrome_app_startup_parameters.mm
+++ ios/chrome/app/startup/chrome_app_startup_parameters.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/foundation_util.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
 #include "base/strings/sys_string_conversions.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
@@ -169,13 +170,18 @@ enum SearchExtensionAction {
     MobileSessionStartAction action = START_ACTION_OTHER;
     if (gurl.SchemeIs(url::kHttpScheme)) {
       action = START_ACTION_OPEN_HTTP_FROM_OS;
+      base::RecordAction(
+          base::UserMetricsAction("MobileDefaultBrowserViewIntent"));
     } else if (gurl.SchemeIs(url::kHttpsScheme)) {
       action = START_ACTION_OPEN_HTTPS_FROM_OS;
+      base::RecordAction(
+          base::UserMetricsAction("MobileDefaultBrowserViewIntent"));
     } else {
       // Replace the scheme with https or http depending on whether the input
       // |url| scheme ends with an 's'.
       BOOL useHttps = gurl.scheme()[gurl.scheme().length() - 1] == 's';
       action = useHttps ? START_ACTION_OPEN_HTTPS : START_ACTION_OPEN_HTTP;
+      base::RecordAction(base::UserMetricsAction("MobileFirstPartyViewIntent"));
 
       GURL::Replacements replace_scheme;
       if (useHttps)
