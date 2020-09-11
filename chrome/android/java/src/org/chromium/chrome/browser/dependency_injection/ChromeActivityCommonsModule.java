41710da51f72029a36450eaa4a4af98ea3ae1fc9
Rohit Agarwal <roagarwal@chromium.org>
2020-09-11 10:49:05 +0000

    Add support for UnownedUserData{Host} in CustomTabIncognitoManager.
    This CL adds the support of linking CustomTabIncognitoManager instance
    with a WindowAndroid object instance. This would enable querying the
    non primary OTR profile from the CustomTabIncognitoManager instance.
    
    This CL is step [1] for the refactoring the TabModelImpl to take in
    profile as an argument.
    
    [1] go/incognito-cct-profile-passing-support
    
    Bug: 1099642
    Change-Id: I0b4e0e3959247925f41292ce8b15855aa8fe4347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2396336
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806127}

--- chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeActivityCommonsModule.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeActivityCommonsModule.java
@@ -36,6 +36,7 @@ import org.chromium.components.browser_ui.notifications.NotificationManagerProxy
 import org.chromium.components.browser_ui.notifications.NotificationManagerProxyImpl;
 import org.chromium.content_public.browser.ScreenOrientationProvider;
 import org.chromium.ui.base.ActivityWindowAndroid;
+import org.chromium.ui.base.WindowAndroid;
 
 import javax.inject.Named;
 
@@ -102,6 +103,11 @@ public class ChromeActivityCommonsModule {
         // Ideally providing Context or Activity should be enough, but currently a lot of code is
         // coupled specifically to ChromeActivity.
         return mActivity;
+    }
+
+    @Provides
+    public WindowAndroid provideWindowAndroid() {
+        return mActivity.getWindowAndroid();
     }
 
     @Provides
