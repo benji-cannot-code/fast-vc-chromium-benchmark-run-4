1ed9f23252978929b3beea427b626014933643bc
sandromaggi <sandromaggi@google.com>
2020-09-11 11:05:35 +0000

    [Autofill Assistant] Make |GetElementTag| take an Element
    This continues the effort to make |WebController| actions use
    |ElementFinder::Result| instead of |Selector|.
    
    Bug: b/168107066
    Change-Id: Idfa4e86075d4c650740ac5d7a325243c4d73d850
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401103
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#806131}

--- components/autofill_assistant/browser/actions/fallback_handler/required_fields_fallback_handler.h
+++ components/autofill_assistant/browser/actions/fallback_handler/required_fields_fallback_handler.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/actions/action.h"
 #include "components/autofill_assistant/browser/actions/fallback_handler/required_field.h"
 #include "components/autofill_assistant/browser/batch_element_checker.h"
+#include "components/autofill_assistant/browser/web/element_finder.h"
 
 namespace autofill_assistant {
 class ClientStatus;
@@ -65,11 +66,20 @@ class RequiredFieldsFallbackHandler {
   // Sets fallback field values for empty fields.
   void SetFallbackFieldValuesSequentially(size_t required_fields_index);
 
+  // Called after attempting to find one of the elements to execute a fallback
+  // action on.
+  void OnFindElement(const std::string& value,
+                     size_t required_fields_index,
+                     const ClientStatus& element_status,
+                     std::unique_ptr<ElementFinder::Result> element_result);
+
   // Called after retrieving tag name from a field.
-  void OnGetFallbackFieldTag(const std::string& value,
-                             size_t required_fields_index,
-                             const ClientStatus& element_tag_status,
-                             const std::string& element_tag);
+  void OnGetFallbackFieldElementTag(
+      const std::string& value,
+      size_t required_fields_index,
+      std::unique_ptr<ElementFinder::Result> element,
+      const ClientStatus& element_tag_status,
+      const std::string& element_tag);
 
   // Called after clicking a fallback element.
   void OnClickOrTapFallbackElement(const std::string& value,
@@ -83,6 +93,7 @@ class RequiredFieldsFallbackHandler {
   // Called after trying to set form values without Autofill in case of
   // fallback after failed validation.
   void OnSetFallbackFieldValue(size_t required_fields_index,
+                               std::unique_ptr<ElementFinder::Result> element,
                                const ClientStatus& status);
 
   ClientStatus client_status_;
--- components/autofill_assistant/browser/actions/mock_action_delegate.h
+++ components/autofill_assistant/browser/actions/mock_action_delegate.h
@@ -230,7 +230,7 @@ class MockActionDelegate : public ActionDelegate {
                                             const std::string&)> callback));
 
   MOCK_METHOD2(GetElementTag,
-               void(const Selector& selector,
+               void(const ElementFinder::Result& element,
                     base::OnceCallback<void(const ClientStatus&,
                                             const std::string&)> callback));
 
