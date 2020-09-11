79c5e4f4d942a6540381443671a692412f4bea41
Robbie Gibson <rkgibson@google.com>
2020-09-11 17:52:58 +0000

    [iOS] Set WebPageDefaultZoomFromDynamicType to disabled
    This flag was mistakenly created enabled by default instead of disabled
    by default.
    
    Bug: 1127308
    Change-Id: Ie20fbbf5ad63081f8b106ebe5066e98a8f3d9063
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404792
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806230}

--- ios/chrome/browser/web/font_size_tab_helper_unittest.mm
+++ ios/chrome/browser/web/font_size_tab_helper_unittest.mm
@@ -10,12 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #import "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "components/sync_preferences/pref_service_mock_factory.h"
 #include "components/sync_preferences/pref_service_syncable.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #include "ios/chrome/browser/pref_names.h"
 #include "ios/chrome/browser/prefs/browser_prefs.h"
+#include "ios/chrome/browser/web/features.h"
 #include "ios/web/public/test/fakes/fake_web_frame.h"
 #import "ios/web/public/test/fakes/fake_web_frames_manager.h"
 #import "ios/web/public/test/fakes/test_web_state.h"
@@ -37,6 +39,9 @@ class FontSizeTabHelperTest : public PlatformTest {
  public:
   FontSizeTabHelperTest()
       : application_(OCMPartialMock([UIApplication sharedApplication])) {
+    scoped_feature_list_.InitAndEnableFeature(
+        {web::kWebPageDefaultZoomFromDynamicType});
+
     OCMStub([application_ preferredContentSizeCategory])
         .andDo(^(NSInvocation* invocation) {
           [invocation setReturnValue:&preferred_content_size_category_];
@@ -92,6 +97,7 @@ class FontSizeTabHelperTest : public PlatformTest {
   }
 
  protected:
+  base::test::ScopedFeatureList scoped_feature_list_;
   base::test::TaskEnvironment task_environment_;
   std::unique_ptr<TestChromeBrowserState> chrome_browser_state_;
   web::TestWebState web_state_;
