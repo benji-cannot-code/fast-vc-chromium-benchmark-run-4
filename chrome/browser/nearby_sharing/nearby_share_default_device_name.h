daaf289ead04fcdfe2ab3f273dbf0322b8634bdc
Josh Nohle <nohle@google.com>
2020-09-11 20:00:06 +0000

    [Nearby] Create default device name
    Create a default device name of the form
    
      <profile name>'s <device model>.
    
    The profile name is retrieved in the same way as profile_info_handler.cc
    [1]. So, the name should be consistent with the one shown in Settings >
    "You and Google". The device model is set to model name from SysInfo
    HardwareInfo. If the model name is empty, a variation of "Chromebook" is
    used for Chrome OS and "device" for other platforms.
    
    [1]https://source.chromium.org/chromium/chromium/src/+/master:chrome/browser/ui/webui/settings/profile_info_handler.cc;l=132-168;drc=b93f16ad9f7a986d964033bbd539a8eec677bfbf
    
    Bug: b/154863679, 1127017
    Change-Id: I219cc3965ae0180f66c3953f0a9213dc5c55a1fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2298169
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#806294}

---
+++ chrome/browser/nearby_sharing/nearby_share_default_device_name.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_NEARBY_SHARING_NEARBY_SHARE_DEFAULT_DEVICE_NAME_H_
+#define CHROME_BROWSER_NEARBY_SHARING_NEARBY_SHARE_DEFAULT_DEVICE_NAME_H_
+
+#include <string>
+
+#include "base/callback.h"
+#include "base/optional.h"
+
+class Profile;
+
+// Creates a default device name of the form <profile name>'s <device model>.
+void GetNearbyShareDefaultDeviceName(
+    Profile* profile,
+    base::OnceCallback<void(const base::Optional<std::string>&)> callback);
+
+#endif  // CHROME_BROWSER_NEARBY_SHARING_NEARBY_SHARE_DEFAULT_DEVICE_NAME_H_
