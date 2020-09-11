b8944ad465d1ae0502f0ac1d32654ed343588308
Robbie McElrath <rmcelrath@chromium.org>
2020-09-11 20:14:25 +0000

    [WebLayer] Add more Site Settings tests
    This CL adds tests for most Site Settings UI surfaces, including each
    Fragment type, popup type, and links to system settings activities.
    
    Bug: 1122705
    Change-Id: I3232d490a8798adf9553629f4d5634221e83e100
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2382483
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806304}

---
+++ weblayer/public/javatestutil/org/chromium/weblayer/SiteSettingsTestUtils.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer;
+
+import android.content.Context;
+import android.content.Intent;
+
+import org.chromium.weblayer_private.interfaces.SiteSettingsIntentHelper;
+
+/**
+ * Helpers for writing tests for the Site Settings UI.
+ */
+public final class SiteSettingsTestUtils {
+    // This can be removed if/when we move this into SiteSettingsActivity. Currently no embedders
+    // need to launch the single site settings UI directly.
+    public static Intent createIntentForSingleWebsite(
+            Context context, String profileName, String url) {
+        return SiteSettingsIntentHelper.createIntentForSingleWebsite(context, profileName, url);
+    }
+}
