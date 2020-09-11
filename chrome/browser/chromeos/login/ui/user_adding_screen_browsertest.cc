e77098ac72dac975605b0ea251943c13c1d1816a
Thomas Tellier <tellier@google.com>
2020-09-11 19:50:37 +0000

    Reland "Add a pop-up for Chrome OS secondary login screen"
    This reverts commit 253d9157a0ddd806162e8798d50bc094ad91b862.
    
    Reason for revert: Need to implement the feature
    
    Original change's description:
    > Revert "Add a pop-up for Chrome OS secondary login screen"
    > 
    > This reverts commit 3e085ec3e79f16fb2ab83282adfc87b5a159c202.
    > 
    > Reason for revert: Test is crashing with a segmentation fault
    > 
    > Original change's description:
    > > Add a pop-up for Chrome OS secondary login screen
    > > 
    > > Bug: 1066489
    > > Change-Id: I5af5d0ab745176572dfa01cb02e6d45ac1b27c99
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2303429
    > > Commit-Queue: Thomas Tellier <tellier@google.com>
    > > Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    > > Reviewed-by: Thomas Tellier <tellier@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#805844}
    > 
    > TBR=antrim@chromium.org,raleksandrov@google.com,tellier@google.com
    > 
    > Change-Id: I35550da66bba011cb35b9e50d38b4b02b572ce29
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1066489
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404061
    > Reviewed-by: K. Moon <kmoon@chromium.org>
    > Commit-Queue: K. Moon <kmoon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#805939}
    
    TBR=antrim@chromium.org,raleksandrov@google.com,kmoon@chromium.org,tellier@google.com
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1066489
    Change-Id: Id1eebd7da875a5c12f5f4821875b4590f7e76331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404350
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Thomas Tellier <tellier@google.com>
    Commit-Queue: Thomas Tellier <tellier@google.com>
    Cr-Commit-Position: refs/heads/master@{#806288}

--- chrome/browser/chromeos/login/ui/user_adding_screen_browsertest.cc
+++ chrome/browser/chromeos/login/ui/user_adding_screen_browsertest.cc
@@ -151,10 +151,10 @@ IN_PROC_BROWSER_TEST_F(UserAddingScreenTest, CancelAdding) {
             users[0].account_id);
 }
 
-class UserAddingScreenWithViewBasedTest : public UserAddingScreenTest,
-                                          public LoginScreenShownObserver {
+class UserAddingScreenViewBasedTest : public UserAddingScreenTest,
+                                      public LoginScreenShownObserver {
  public:
-  UserAddingScreenWithViewBasedTest() : UserAddingScreenTest() {
+  UserAddingScreenViewBasedTest() : UserAddingScreenTest() {
     feature_list_.InitWithFeatures(
         {chromeos::features::kViewBasedMultiprofileLogin}, {});
   }
@@ -180,7 +180,7 @@ class UserAddingScreenWithViewBasedTest : public UserAddingScreenTest,
   base::test::ScopedFeatureList feature_list_;
 };
 
-IN_PROC_BROWSER_TEST_F(UserAddingScreenWithViewBasedTest, CancelAdding) {
+IN_PROC_BROWSER_TEST_F(UserAddingScreenViewBasedTest, CancelAdding) {
   const auto& users = login_mixin_.users();
   EXPECT_EQ(users.size(), user_manager::UserManager::Get()->GetUsers().size());
   EXPECT_EQ(user_manager::UserManager::Get()->GetLoggedInUsers().size(), 0u);
@@ -254,7 +254,7 @@ IN_PROC_BROWSER_TEST_F(UserAddingScreenTest, UILogin) {
             session_manager::SessionState::ACTIVE);
 }
 
-IN_PROC_BROWSER_TEST_F(UserAddingScreenWithViewBasedTest, UILogin) {
+IN_PROC_BROWSER_TEST_F(UserAddingScreenViewBasedTest, UILogin) {
   const auto& users = login_mixin_.users();
   EXPECT_EQ(session_manager::SessionManager::Get()->session_state(),
             session_manager::SessionState::LOGIN_PRIMARY);
@@ -315,8 +315,8 @@ IN_PROC_BROWSER_TEST_F(UserAddingScreenTest, AddingSeveralUsers) {
     EXPECT_EQ(session_manager::SessionManager::Get()->session_state(),
               session_manager::SessionState::LOGIN_SECONDARY);
 
-    AddUser(users[n - i].account_id);
-    users_in_session_order_.push_back(users[n - i].account_id);
+    AddUser(users[i].account_id);
+    users_in_session_order_.push_back(users[i].account_id);
     WaitUntilUserAddingFinishedOrCancelled();
 
     EXPECT_EQ(user_adding_finished(), i);
@@ -403,7 +403,7 @@ IN_PROC_BROWSER_TEST_F(UserAddingScreenTest, AddingSeveralUsers) {
     EXPECT_EQ(users_in_session_order_[i], unlock_users[i]->GetAccountId());
 }
 
-IN_PROC_BROWSER_TEST_F(UserAddingScreenWithViewBasedTest, AddingSeveralUsers) {
+IN_PROC_BROWSER_TEST_F(UserAddingScreenViewBasedTest, AddingSeveralUsers) {
   const auto& users = login_mixin_.users();
   EXPECT_EQ(session_manager::SessionManager::Get()->session_state(),
             session_manager::SessionState::LOGIN_PRIMARY);
@@ -425,8 +425,8 @@ IN_PROC_BROWSER_TEST_F(UserAddingScreenWithViewBasedTest, AddingSeveralUsers) {
     EXPECT_EQ(session_manager::SessionManager::Get()->session_state(),
               session_manager::SessionState::LOGIN_SECONDARY);
 
-    AddUser(users[n - i].account_id);
-    users_in_session_order_.push_back(users[n - i].account_id);
+    AddUser(users[i].account_id);
+    users_in_session_order_.push_back(users[i].account_id);
     WaitUntilUserAddingFinishedOrCancelled();
 
     EXPECT_EQ(user_adding_finished(), i);
@@ -545,7 +545,7 @@ IN_PROC_BROWSER_TEST_F(UserAddingScreenTest, ScreenVisibilityAfterLock) {
   WaitUntilUserAddingFinishedOrCancelled();
 }
 
-IN_PROC_BROWSER_TEST_F(UserAddingScreenWithViewBasedTest,
+IN_PROC_BROWSER_TEST_F(UserAddingScreenViewBasedTest,
                        ScreenVisibilityAfterLock) {
   const auto& users = login_mixin_.users();
   LoginUser(users[0].account_id);
@@ -575,6 +575,49 @@ IN_PROC_BROWSER_TEST_F(UserAddingScreenWithViewBasedTest,
   EXPECT_TRUE(ash::LoginScreenTestApi::ClickCancelButton());
 
   WaitUntilUserAddingFinishedOrCancelled();
+}
+
+IN_PROC_BROWSER_TEST_F(UserAddingScreenViewBasedTest, InfoBubbleVisible) {
+  const auto& users = login_mixin_.users();
+  EXPECT_EQ(users.size(), user_manager::UserManager::Get()->GetUsers().size());
+  EXPECT_EQ(user_manager::UserManager::Get()->GetLoggedInUsers().size(), 0u);
+  EXPECT_EQ(session_manager::SessionManager::Get()->session_state(),
+            session_manager::SessionState::LOGIN_PRIMARY);
+
+  EXPECT_FALSE(ash::LoginScreenTestApi::IsUserAddingScreenBubbleShown());
+
+  LoginUser(users[0].account_id);
+  EXPECT_EQ(user_manager::UserManager::Get()->GetLoggedInUsers().size(), 1u);
+  EXPECT_EQ(session_manager::SessionManager::Get()->session_state(),
+            session_manager::SessionState::ACTIVE);
+
+  user_manager::UserManager* user_manager = user_manager::UserManager::Get();
+
+  // Check if the user adding screen bubble is still shown after adding other
+  // users, as a re-layout is executed each time.
+  const int n = users.size();
+  for (int i = 1; i < n; ++i) {
+    UserAddingScreen::Get()->Start();
+    EXPECT_EQ(user_adding_started(), i);
+    EXPECT_EQ(session_manager::SessionManager::Get()->session_state(),
+              session_manager::SessionState::LOGIN_SECONDARY);
+
+    EXPECT_TRUE(ash::LoginScreenTestApi::IsUserAddingScreenBubbleShown());
+
+    AddUser(users[i].account_id);
+    users_in_session_order_.push_back(users[i].account_id);
+    WaitUntilUserAddingFinishedOrCancelled();
+
+    EXPECT_EQ(user_adding_finished(), i);
+    EXPECT_EQ(session_manager::SessionManager::Get()->session_state(),
+              session_manager::SessionState::ACTIVE);
+    EXPECT_TRUE(LoginDisplayHost::default_host() == nullptr);
+    ASSERT_EQ(user_manager->GetLoggedInUsers().size(),
+              static_cast<size_t>(i + 1));
+  }
+
+  EXPECT_EQ(session_manager::SessionManager::Get()->session_state(),
+            session_manager::SessionState::ACTIVE);
 }
 
 }  // namespace chromeos
