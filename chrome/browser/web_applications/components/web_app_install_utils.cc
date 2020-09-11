e26d126e5c4ec57116836687d4366d5df264fe21
Eric Willigers <ericwilligers@chromium.org>
2020-09-11 06:52:59 +0000

    Web Share Target: Copy share target from manifest to WebApp
    UpdateWebAppInfoFromManifest is updated to copy the share target.
    https://w3c.github.io/web-share-target/level-2/
    
    web_app_install_utils.cc standalone method SetWebAppManifestFields()
    is extracted from
    WebAppInstallFinalizer::SetWebAppManifestFieldsAndWriteData()
    and updated to copy the share target.
    
    Bug: 1125880
    Change-Id: I897c2c25e73ea99f358d06a03df38381262222ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2398046
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806076}

--- chrome/browser/web_applications/components/web_app_install_utils.cc
+++ chrome/browser/web_applications/components/web_app_install_utils.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/optional.h"
 #include "base/stl_util.h"
@@ -210,6 +211,8 @@ void UpdateWebAppInfoFromManifest(const blink::Manifest& manifest,
     web_app_info->icon_infos = std::move(web_app_icons);
 
   web_app_info->file_handlers = manifest.file_handlers;
+
+  web_app_info->share_target = manifest.share_target;
 
   web_app_info->protocol_handlers = manifest.protocol_handlers;
 
---
+++ chrome/browser/web_applications/web_app_installation_utils.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_WEB_APPLICATIONS_WEB_APP_INSTALLATION_UTILS_H_
+#define CHROME_BROWSER_WEB_APPLICATIONS_WEB_APP_INSTALLATION_UTILS_H_
+
+struct WebApplicationInfo;
+
+namespace web_app {
+
+class WebApp;
+
+// Updates |web_app| using |web_app_info|
+void SetWebAppManifestFields(const WebApplicationInfo& web_app_info,
+                             WebApp& web_app);
+
+}  // namespace web_app
+
+#endif  // CHROME_BROWSER_WEB_APPLICATIONS_WEB_APP_INSTALLATION_UTILS_H_
