c6cb3c26260946604ad491852d54eb6fd53ecfdb
Andrew Xu <andrewxu@chromium.org>
2020-09-11 16:52:38 +0000

    [Multipaste] Disable Multipaste in a few user modes
    This CL disables multipaste in the following cases:
    (1) Not log in
    (2) Screen locked
    (3) Kiosk
    (4) Public mode
    
    Bug: 1100739
    Change-Id: I5301d22a7a16786ee862a3d127886c95c06175d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2394718
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806209}

--- ash/clipboard/clipboard_history.cc
+++ ash/clipboard/clipboard_history.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/clipboard/clipboard_history.h"
 
-#include "ash/clipboard/clipboard_history_controller.h"
 #include "ash/clipboard/clipboard_history_util.h"
 #include "ash/clipboard/clipboard_nudge_controller.h"
 #include "ash/session/session_controller_impl.h"
@@ -79,6 +78,9 @@ void ClipboardHistory::RemoveItemForId(const base::UnguessableToken& id) {
 }
 
 void ClipboardHistory::OnClipboardDataChanged() {
+  if (!IsEnabledInCurrentMode())
+    return;
+
   // TODO(newcomer): Prevent Clipboard from recording metrics when pausing
   // observation.
   if (num_pause_ > 0)
@@ -104,6 +106,20 @@ void ClipboardHistory::OnClipboardDataChanged() {
       FROM_HERE,
       base::BindOnce(&ClipboardHistory::MaybeCommitData,
                      commit_data_weak_factory_.GetWeakPtr(), *clipboard_data));
+}
+
+bool ClipboardHistory::IsEnabledInCurrentMode() const {
+  switch (Shell::Get()->session_controller()->login_status()) {
+    case LoginStatus::NOT_LOGGED_IN:
+    case LoginStatus::LOCKED:
+    case LoginStatus::KIOSK_APP:
+    case LoginStatus::PUBLIC:
+      return false;
+    case LoginStatus::USER:
+    case LoginStatus::GUEST:
+    case LoginStatus::SUPERVISED:
+      return true;
+  }
 }
 
 void ClipboardHistory::MaybeCommitData(ui::ClipboardData data) {
