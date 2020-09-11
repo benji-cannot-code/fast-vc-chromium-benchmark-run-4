6aff32922e1a2e91ee2423c09f9d2ffde4f8e3aa
Andre Le <leandre@chromium.org>
2020-09-11 01:06:55 +0000

    [CrOS PhoneHub] Add task continuation UI to PhoneHub tray.
    Add the task continuation section (continue browsing) to PhoneHub tray bubble (mock UI with implementation).
    
    Bug: 1106937,1126208
    Change-Id: I15144c8f875c56454b0125498417bd37ee4bc7c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2399802
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806001}

---
+++ ash/system/phonehub/task_continuation_view.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_PHONEHUB_TASK_CONTINUATION_VIEW_H_
+#define ASH_SYSTEM_PHONEHUB_TASK_CONTINUATION_VIEW_H_
+
+#include "ash/ash_export.h"
+#include "ui/views/view.h"
+
+namespace ash {
+
+// A view in Phone Hub bubble that allows user to pick up unfinished task left
+// off from their phone, currently only support web browsing.
+class ASH_EXPORT TaskContinuationView : public views::View {
+ public:
+  TaskContinuationView();
+  ~TaskContinuationView() override;
+  TaskContinuationView(TaskContinuationView&) = delete;
+  TaskContinuationView operator=(TaskContinuationView&) = delete;
+
+  // views::View:
+  const char* GetClassName() const override;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_PHONEHUB_TASK_CONTINUATION_VIEW_H_
