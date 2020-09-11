ca17f2db2ccccd4485b4e32c92f1592f0014a673
Akihiro Ota <akihiroota@chromium.org>
2020-09-11 21:32:07 +0000

    Promote doCmd to ChromeVoxNextE2ETest
    Remove repeated declarations of doCmd in the ChromeVox testing code by
    promoting it to ChromeVoxNextE2ETest.
    
    Bug: 1127449
    Change-Id: I9d15640894a7f06e8ae3f78e06b50c6173df55c6
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2406379
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806343}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/settings_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/settings_test.js
@@ -14,11 +14,7 @@ GEN_INCLUDE(['../testing/mock_feedback.js', '../testing/fake_objects.js']);
 ChromeVoxSettingsPagesTest = class extends ChromeVoxNextE2ETest {
   /** @override */
   setUp() {
-    window.doCmd = (command) => {
-      return () => {
-        CommandHandler.onCommand(command);
-      };
-    };
+    window.doCmd = this.doCmd;
   }
 
   /** @override */
