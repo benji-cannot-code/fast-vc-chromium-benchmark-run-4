e2b8174282a1d3f5578221d5c8575c7579fe3b02
Xianzhu Wang <wangxianzhu@chromium.org>
2020-09-12 18:02:00 +0000

    Fix DCHECK failure when starting an SVG animation
    Bug: 1126782
    Change-Id: Ib13adc90f6807750e14fbb3ff6a6606f85800eb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404154
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806461}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -2252,7 +2252,8 @@ void LayoutObject::SetStyle(scoped_refptr<const ComputedStyle> style,
       (diff.TransformChanged() || diff.OpacityChanged() ||
        diff.ZIndexChanged() || diff.FilterChanged() ||
        diff.BackdropFilterChanged() || diff.CssClipChanged() ||
-       diff.BlendModeChanged() || diff.MaskChanged())) {
+       diff.BlendModeChanged() || diff.MaskChanged() ||
+       diff.CompositingReasonsChanged())) {
     SetNeedsPaintPropertyUpdate();
   }
 }
