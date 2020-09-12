731ab657ee106a29528738a814bc84a2d9ba72db
Yusuke Sato <yusukes@chromium.org>
2020-09-12 00:44:03 +0000

    reland: arcvm: Start arcvm-adbd on starting ARCVM
    This CL depends on the following CLs:
    https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2380431
    https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/2378806
    https://chrome-internal-review.googlesource.com/c/chromeos/overlays/project-cheets-private/+/3238221
    
    Diff from the original: None. This CL is safe to reland as-is because
      go/lkgm was updated past 13439.0.0.
    Original CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/2380733
    
    BUG=b:166314117
    BUG=chromium:1123901
    TEST=arc.Boot.vm still passes, arcvm-adbd runs
    
    Change-Id: I828323a877128ae32283110d709c85ceb72e96c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404874
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Cr-Commit-Position: refs/heads/master@{#806411}

--- components/arc/session/file_system_status.cc
+++ components/arc/session/file_system_status.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace arc {
 namespace {
 
+constexpr const char kAdbdJson[] = "/etc/arc/adbd.json";
 constexpr const char kArcVmConfigJsonPath[] = "/usr/share/arcvm/config.json";
 constexpr const char kBuiltinPath[] = "/opt/google/vms/android";
 constexpr const char kFstabPath[] = "/run/arcvm/host_generated/fstab";
@@ -43,7 +44,8 @@ FileSystemStatus::FileSystemStatus()
       vendor_image_path_(base::FilePath(kBuiltinPath).Append(kVendorImage)),
       guest_kernel_path_(base::FilePath(kBuiltinPath).Append(kKernel)),
       fstab_path_(kFstabPath),
-      is_system_image_ext_format_(IsSystemImageExtFormat(system_image_path_)) {}
+      is_system_image_ext_format_(IsSystemImageExtFormat(system_image_path_)),
+      has_adbd_json_(base::PathExists(base::FilePath(kAdbdJson))) {}
 
 // static
 bool FileSystemStatus::IsAndroidDebuggable(const base::FilePath& json_path) {
