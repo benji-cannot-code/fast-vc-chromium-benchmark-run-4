7e8f928512027dbd65754e9b249ad140d585e693
Roberto Moura <mouraroberto@google.com>
2020-09-14 11:16:30 +0000

    Add Tab Switcher Layout and Horizontal Layout classes.
    As specified in the "Thumb Strip Layout" subsection of the Thumb Strip
    design doc (go/bling-thumb-strip-design):
    - Create a new Horizontal Layout class.
    - Create a Tab Switcher Layout class that will contain the intersection
    of the new Horizontal Layout and the existing Grid Layout.
    - Make the Grid Layout and Horizontal Layout subclasses of the Tab
    Switcher Layout.
    
    Bug: 1127604
    Change-Id: I8da3e43563e6a18abb3bf0234db26aeccefc8e06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367092
    Commit-Queue: Roberto Moura <mouraroberto@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806573}

--- ios/chrome/browser/ui/tab_grid/grid/grid_layout.h
+++ ios/chrome/browser/ui/tab_grid/grid/grid_layout.h
@@ -6,21 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_TAB_GRID_GRID_GRID_LAYOUT_H_
 #define IOS_CHROME_BROWSER_UI_TAB_GRID_GRID_GRID_LAYOUT_H_
 
-#import <UIKit/UIKit.h>
+#import "ios/chrome/browser/ui/tab_grid/grid/tab_switcher_layout.h"
 
-// Collection view flow layout that displays items in a grid. Items are
-// square-ish. Item sizes adapt to the size classes they are shown in. Item
-// deletions are animated.
-@interface GridLayout : UICollectionViewFlowLayout
-
-// Whether to animate item insertions and deletions.
-@property(nonatomic, assign) BOOL animatesItemUpdates;
-
-@end
-
-// A specialization of GridLayout that shows the UI in its "reordering" state,
-// with the moving cell enlarged and the non-moving cells transparent.
-@interface GridReorderingLayout : GridLayout
+// A specialization of TabSwitcherLayout that displays items in a grid.
+@interface GridLayout : TabSwitcherLayout
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_TAB_GRID_GRID_GRID_LAYOUT_H_
--- ios/chrome/browser/ui/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_grid/grid/grid_view_controller.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tab_grid/grid/grid_image_data_source.h"
 #import "ios/chrome/browser/ui/tab_grid/grid/grid_item.h"
 #import "ios/chrome/browser/ui/tab_grid/grid/grid_layout.h"
+#import "ios/chrome/browser/ui/tab_grid/grid/tab_switcher_layout.h"
 #import "ios/chrome/browser/ui/tab_grid/transitions/grid_transition_layout.h"
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
@@ -77,8 +78,8 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
 @property(nonatomic, assign) CGPoint itemReorderTouchPoint;
 // Animator to show or hide the empty state.
 @property(nonatomic, strong) UIViewPropertyAnimator* emptyStateAnimator;
-// The default layout for the tab grid.
-@property(nonatomic, strong) GridLayout* defaultLayout;
+// The default layout for the tab switcher.
+@property(nonatomic, strong) TabSwitcherLayout* defaultLayout;
 // The layout used while the grid is being reordered.
 @property(nonatomic, strong) UICollectionViewLayout* reorderingLayout;
 // YES if, when reordering is enabled, the order of the cells has changed.
@@ -138,7 +139,7 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
     collectionView.dropDelegate = self;
     collectionView.dragInteractionEnabled = YES;
   } else {
-    self.reorderingLayout = [[GridReorderingLayout alloc] init];
+    self.reorderingLayout = [[TabSwitcherReorderingLayout alloc] init];
     self.itemReorderRecognizer = [[UILongPressGestureRecognizer alloc]
         initWithTarget:self
                 action:@selector(handleItemReorderingWithGesture:)];
