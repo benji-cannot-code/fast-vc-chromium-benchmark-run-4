0bd16721a13e9be77f231b44d5a2ce0af85b4a24
Danil Chapovalov <danilchap@chromium.org>
2020-09-14 13:05:41 +0000

    Roll abseil_revision 3c8b5d7587..6af91b3510
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/3c8b5d7587..6af91b3510
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/3c8b5d7587..6af91b3510
    
    No changes required to .def files.
    
    Bug: None
    Change-Id: Ieb71bd9b716a1d2644e554566766ab83a39871f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2408753
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806623}

--- third_party/abseil-cpp/absl/container/internal/hashtablez_sampler_test.cc
+++ third_party/abseil-cpp/absl/container/internal/hashtablez_sampler_test.cc
@@ -39,6 +39,7 @@ constexpr int kProbeLength = 8;
 namespace absl {
 ABSL_NAMESPACE_BEGIN
 namespace container_internal {
+#if defined(ABSL_INTERNAL_HASHTABLEZ_SAMPLE)
 class HashtablezInfoHandlePeer {
  public:
   static bool IsSampled(const HashtablezInfoHandle& h) {
@@ -47,6 +48,13 @@ class HashtablezInfoHandlePeer {
 
   static HashtablezInfo* GetInfo(HashtablezInfoHandle* h) { return h->info_; }
 };
+#else
+class HashtablezInfoHandlePeer {
+ public:
+  static bool IsSampled(const HashtablezInfoHandle&) { return false; }
+  static HashtablezInfo* GetInfo(HashtablezInfoHandle*) { return nullptr; }
+};
+#endif  // defined(ABSL_INTERNAL_HASHTABLEZ_SAMPLE)
 
 namespace {
 using ::absl::synchronization_internal::ThreadPool;
@@ -173,7 +181,7 @@ TEST(HashtablezInfoTest, RecordRehash) {
   EXPECT_EQ(info.num_rehashes.load(), 1);
 }
 
-#if defined(ABSL_HASHTABLEZ_SAMPLE)
+#if defined(ABSL_INTERNAL_HASHTABLEZ_SAMPLE)
 TEST(HashtablezSamplerTest, SmallSampleParameter) {
   SetHashtablezEnabled(true);
   SetHashtablezSampleParameter(100);
@@ -217,7 +225,6 @@ TEST(HashtablezSamplerTest, Sample) {
   }
   EXPECT_NEAR(sample_rate, 0.01, 0.005);
 }
-#endif
 
 TEST(HashtablezSamplerTest, Handle) {
   auto& sampler = HashtablezSampler::Global();
@@ -247,6 +254,8 @@ TEST(HashtablezSamplerTest, Handle) {
   });
   EXPECT_FALSE(found);
 }
+#endif
+
 
 TEST(HashtablezSamplerTest, Registration) {
   HashtablezSampler sampler;
