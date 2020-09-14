21f93f58d262c54933666a3931b48ab7f695fc02
Tomasz Wiszkowski <ender@google.com>
2020-09-14 23:33:10 +0000

    Add subtype support and identify Trends.
    This change propagated suggestion subtypes to Java, enabling
    Clank to identify Trending Query suggestions.
    
    Bug: 1122669
    Change-Id: Id6669bd9c9344e786836486fd5c66087bbc6f1a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2402135
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806788}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/basic/BasicSuggestionProcessor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/basic/BasicSuggestionProcessor.java
@@ -92,6 +92,9 @@ public class BasicSuggestionProcessor extends BaseSuggestionViewProcessor {
                     return SuggestionIcon.HISTORY;
 
                 default:
+                    if (suggestion.getSubtypes().contains(/* SUBTYPE_TRENDS = */ 143)) {
+                        return SuggestionIcon.TRENDS;
+                    }
                     return SuggestionIcon.MAGNIFIER;
             }
         } else {
@@ -128,6 +131,10 @@ public class BasicSuggestionProcessor extends BaseSuggestionViewProcessor {
 
             case SuggestionIcon.VOICE:
                 icon = R.drawable.btn_mic;
+                break;
+
+            case SuggestionIcon.TRENDS:
+                icon = R.drawable.trending_up_black_24dp;
                 break;
 
             default:
