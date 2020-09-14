94d36f2dadcf3b7edf3b4f6dc48ff10c4d1b6d39
Maggie Cai <mxcai@chromium.org>
2020-09-14 01:28:31 +0000

    [Sharesheet] Add proper name for drive share action.
    This CL uses the international string label from file manager for drive
    share action.
    
    BUG=1097623
    
    Change-Id: I3f5aa1e3873b2677630fc49e2c341f2a3a63194f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404517
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806518}

--- chrome/browser/sharesheet/drive_share_action.cc
+++ chrome/browser/sharesheet/drive_share_action.cc
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/base/window_open_disposition.h"
+#include "ui/chromeos/strings/grit/ui_chromeos_strings.h"
 #include "ui/gfx/image/image_skia.h"
 #include "url/gurl.h"
 
@@ -23,8 +25,7 @@ DriveShareAction::DriveShareAction() = default;
 DriveShareAction::~DriveShareAction() = default;
 
 const base::string16 DriveShareAction::GetActionName() {
-  // TODO(crbug.com/1097623): Get the Action name from files app strings.
-  return base::UTF8ToUTF16("ShareWithOthers");
+  return l10n_util::GetStringUTF16(IDS_FILE_BROWSER_SHARE_BUTTON_LABEL);
 }
 
 const gfx::ImageSkia DriveShareAction::GetActionIcon() {
