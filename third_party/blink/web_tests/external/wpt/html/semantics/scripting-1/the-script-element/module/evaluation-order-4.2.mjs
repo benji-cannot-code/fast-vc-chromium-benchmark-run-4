cc807aa603ee98dfdfd24f42702b6732089e30e6
Avi Drissman <avi@chromium.org>
2020-09-14 19:40:42 +0000

    Revert "[blink] Fix top-level-await error reporting"
    This reverts commit 077d63da3268e6761d20c56409d37c57d37f1685.
    
    Reason for revert:
    
    https://ci.chromium.org/p/chromium/builders/ci/WebKit%20Linux%20Leak
    
    is constantly failing on
    
    * virtual/module-top-level-await/external/wpt/html/semantics/scripting-1/the-script-element/module/evaluation-order-4-tla.html
    * virtual/module-top-level-await/external/wpt/html/webappapis/dynamic-markup-insertion/document-write/module-tla-delayed.html
    * virtual/module-top-level-await/external/wpt/html/webappapis/dynamic-markup-insertion/document-write/module-tla-immediate-promise.html
    * virtual/module-top-level-await/external/wpt/html/webappapis/dynamic-markup-insertion/document-write/module-tla-import.html
    
    
    Original change's description:
    > [blink] Fix top-level-await error reporting
    > 
    > - Enable tests with TLA enabled
    > - Fix top-level error reporting with TLA
    > 
    > Based on the discussions on the spec, we fire error events on rejection
    > of the result promise:
    > https://github.com/whatwg/html/pull/4352#discussion_r435178136
    > 
    > Bug: 1022182, 1096455, 1127215, v8:9344
    > Change-Id: I16e83cb4e279c1e44be7fa70a51a103ee94aacc3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228881
    > Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    > Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Reviewed-by: Dominic Farolino <dom@chromium.org>
    > Reviewed-by: Domenic Denicola <domenic@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#806593}
    
    TBR=hiroshige@chromium.org,kouhei@chromium.org,cbruni@chromium.org,dom@chromium.org,domenic@chromium.org
    
    Change-Id: I66b085b05ad30020772e80324e226275e41a8782
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1022182
    Bug: 1096455
    Bug: 1127215
    Bug: v8:9344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410725
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806724}

--- third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/module/evaluation-order-4.2.mjs
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-log.push("step-4.2-1");
-queueMicrotask(() => log.push("microtask-4.2"));
-log.push("step-4.2-2");
-
-throw new Error("error");
