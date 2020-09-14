001658251ed27c3cede99aa0755e0beeeed56fbb
Christian Dullweber <dullweber@chromium.org>
2020-09-14 12:37:59 +0000

    Hide connection details if connection is secure
    The new page info UI should not show a details string if the connection
    is secure. The information will be shown in a subpage instead.
    
    Bug: 1077766
    Change-Id: Iee7ef8041ff056d5274f24f5f14901146ba2b754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2400500
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806616}

--- components/page_info/page_info_ui.cc
+++ components/page_info/page_info_ui.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
+#include "components/page_info/features.h"
 #include "components/page_info/page_info_ui_delegate.h"
 #include "components/permissions/permission_manager.h"
 #include "components/permissions/permission_result.h"
@@ -198,19 +199,21 @@ std::unique_ptr<PageInfoUI::SecurityDescription> CreateSecurityDescription(
     int summary_id,
     int details_id,
     PageInfoUI::SecurityDescriptionType type) {
-  std::unique_ptr<PageInfoUI::SecurityDescription> security_description(
-      new PageInfoUI::SecurityDescription());
+  auto security_description =
+      std::make_unique<PageInfoUI::SecurityDescription>();
   security_description->summary_style = style;
-  security_description->summary = l10n_util::GetStringUTF16(summary_id);
-  security_description->details = l10n_util::GetStringUTF16(details_id);
+  if (summary_id)
+    security_description->summary = l10n_util::GetStringUTF16(summary_id);
+  if (details_id)
+    security_description->details = l10n_util::GetStringUTF16(details_id);
   security_description->type = type;
   return security_description;
 }
 
 std::unique_ptr<PageInfoUI::SecurityDescription>
 CreateSecurityDescriptionForLookalikeSafetyTip(const GURL& safe_url) {
-  std::unique_ptr<PageInfoUI::SecurityDescription> security_description(
-      new PageInfoUI::SecurityDescription());
+  auto security_description =
+      std::make_unique<PageInfoUI::SecurityDescription>();
   security_description->summary_style = PageInfoUI::SecuritySummaryColor::RED;
 
   const base::string16 safe_host =
@@ -257,7 +260,7 @@ PageInfoUI::ChosenObjectInfo::ChosenObjectInfo(
     std::unique_ptr<permissions::ChooserContextBase::Object> chooser_object)
     : ui_info(ui_info), chooser_object(std::move(chooser_object)) {}
 
-PageInfoUI::ChosenObjectInfo::~ChosenObjectInfo() {}
+PageInfoUI::ChosenObjectInfo::~ChosenObjectInfo() = default;
 
 PageInfoUI::IdentityInfo::IdentityInfo()
     : identity_status(PageInfo::SITE_IDENTITY_STATUS_UNKNOWN),
@@ -267,16 +270,13 @@ PageInfoUI::IdentityInfo::IdentityInfo()
       show_ssl_decision_revoke_button(false),
       show_change_password_buttons(false) {}
 
-PageInfoUI::IdentityInfo::~IdentityInfo() {}
+PageInfoUI::IdentityInfo::~IdentityInfo() = default;
 
 PageInfoUI::PageFeatureInfo::PageFeatureInfo()
     : is_vr_presentation_in_headset(false) {}
 
 std::unique_ptr<PageInfoUI::SecurityDescription>
 PageInfoUI::GetSecurityDescription(const IdentityInfo& identity_info) const {
-  std::unique_ptr<PageInfoUI::SecurityDescription> security_description(
-      new PageInfoUI::SecurityDescription());
-
   switch (identity_info.safe_browsing_status) {
     case PageInfo::SAFE_BROWSING_STATUS_NONE:
       break;
@@ -320,11 +320,9 @@ PageInfoUI::GetSecurityDescription(const IdentityInfo& identity_info) const {
   switch (identity_info.identity_status) {
     case PageInfo::SITE_IDENTITY_STATUS_INTERNAL_PAGE:
 #if defined(OS_ANDROID)
-      // We provide identical summary and detail strings for Android, which
-      // deduplicates them in the UI code.
-      return CreateSecurityDescription(
-          SecuritySummaryColor::GREEN, IDS_PAGE_INFO_INTERNAL_PAGE,
-          IDS_PAGE_INFO_INTERNAL_PAGE, SecurityDescriptionType::INTERNAL);
+      return CreateSecurityDescription(SecuritySummaryColor::GREEN, 0,
+                                       IDS_PAGE_INFO_INTERNAL_PAGE,
+                                       SecurityDescriptionType::INTERNAL);
 #else
       // Internal pages on desktop have their own UI implementations which
       // should never call this function.
@@ -358,10 +356,16 @@ PageInfoUI::GetSecurityDescription(const IdentityInfo& identity_info) const {
                                            IDS_PAGE_INFO_LEGACY_TLS_DETAILS,
                                            SecurityDescriptionType::CONNECTION);
         default:
-          return CreateSecurityDescription(SecuritySummaryColor::GREEN,
-                                           IDS_PAGE_INFO_SECURE_SUMMARY,
-                                           IDS_PAGE_INFO_SECURE_DETAILS,
-                                           SecurityDescriptionType::CONNECTION);
+          int secure_details = IDS_PAGE_INFO_SECURE_DETAILS;
+#if defined(OS_ANDROID)
+          if (base::FeatureList::IsEnabled(page_info::kPageInfoV2)) {
+            // Do not show details for secure connections.
+            secure_details = 0;
+          }
+#endif
+          return CreateSecurityDescription(
+              SecuritySummaryColor::GREEN, IDS_PAGE_INFO_SECURE_SUMMARY,
+              secure_details, SecurityDescriptionType::CONNECTION);
       }
     case PageInfo::SITE_IDENTITY_STATUS_DEPRECATED_SIGNATURE_ALGORITHM:
     case PageInfo::SITE_IDENTITY_STATUS_UNKNOWN:
@@ -374,7 +378,7 @@ PageInfoUI::GetSecurityDescription(const IdentityInfo& identity_info) const {
   }
 }
 
-PageInfoUI::~PageInfoUI() {}
+PageInfoUI::~PageInfoUI() = default;
 
 // static
 base::string16 PageInfoUI::PermissionTypeToUIString(ContentSettingsType type) {
@@ -395,7 +399,7 @@ base::string16 PageInfoUI::PermissionActionToUIString(
     content_settings::SettingSource source) {
   ContentSetting effective_setting =
       GetEffectiveSetting(type, setting, default_setting);
-  const int* button_text_ids = NULL;
+  const int* button_text_ids = nullptr;
   switch (source) {
     case content_settings::SETTING_SOURCE_USER:
       if (setting == CONTENT_SETTING_DEFAULT) {
