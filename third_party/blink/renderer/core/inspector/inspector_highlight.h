3b7b76b1f1db2c39a94291c33c932302ef21eef1
Patrick Brosset <patrick.brosset@microsoft.com>
2020-09-14 11:28:36 +0000

    DevTools: Add configurable grid overlay background color
    In order to achieve the right UX in the grid debugging tools in devtools
    we need to be able to fill the background of the grid overlay with a
    customizable color.
    
    This change adds a new parameter for this in the protocol file.
    
    Bug: 1047356
    Change-Id: I4b9cec0017f1cde5f54da95f6f0aa9274375c5b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2405997
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Patrick Brosset <patrick.brosset@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#806595}

--- third_party/blink/renderer/core/inspector/inspector_highlight.h
+++ third_party/blink/renderer/core/inspector/inspector_highlight.h
@@ -43,6 +43,7 @@ struct CORE_EXPORT InspectorGridHighlightConfig {
   Color row_hatch_color;
   Color column_hatch_color;
   Color area_border_color;
+  Color grid_background_color;
 
   bool show_grid_extension_lines;
   bool grid_border_dash;
