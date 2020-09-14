2beb02f9de661bf1d834e0ef17cc854b1bb4ed65
Ahmed Mehfooz <amehfooz@chromium.org>
2020-09-14 01:55:05 +0000

    Fix battery icon regression for tethered device battery
    This fix changes the battery icon slightly to match modified specs
    (It is 1px shorter).
    The spec also standardizes all battery icons to the same size
    so the regression for tethered device battery is also fixed.
    
    Bug: 1126674
    Change-Id: Ia71972930c51c964e4110ba887f17b0eca027847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404417
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806521}

--- ash/system/power/tray_power.cc
+++ ash/system/power/tray_power.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/notification.h"
 #include "ui/message_center/public/cpp/notification_delegate.h"
+#include "ui/views/border.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/view.h"
 
@@ -45,6 +46,8 @@ namespace ash {
 namespace tray {
 
 PowerTrayView::PowerTrayView(Shelf* shelf) : TrayItemView(shelf) {
+  SetBorder(
+      views::CreateEmptyBorder(0, 0, kUnifiedTrayBatteryBottomPadding, 0));
   CreateImageView();
   UpdateImage();
   UpdateStatus();
