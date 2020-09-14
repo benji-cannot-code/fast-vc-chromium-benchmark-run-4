4d736cafec81d88590a26e09297eb3e1db483f2d
Gauthier Ambard <gambard@chromium.org>
2020-09-14 11:46:37 +0000

    [iOS] Add default browser setting
    This CL adds a cell in the settings to open Settings.app to set the
    default browser.
    Launch bug: crbug.com/1126436
    
    Bug: 1127225
    Change-Id: I9dea25330f999c0ed76bad6b6635dce1029bee28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2400480
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806603}

--- ios/chrome/browser/ui/settings/settings_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_table_view_controller.mm
@@ -79,6 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/table_view/cells/table_view_image_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_info_button_cell.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_info_button_item.h"
+#import "ios/chrome/browser/ui/table_view/cells/table_view_text_item.h"
 #import "ios/chrome/browser/ui/table_view/table_view_model.h"
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
@@ -136,6 +137,7 @@ typedef NS_ENUM(NSInteger, SectionIdentifier) {
   SectionIdentifierAdvanced,
   SectionIdentifierInfo,
   SectionIdentifierDebug,
+  SectionIdentifierDefaultBrowser,
 };
 
 typedef NS_ENUM(NSInteger, ItemType) {
@@ -160,6 +162,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeTableCellCatalog,
   ItemTypeArticlesForYou,
   ItemTypeSafetyCheck,
+  ItemTypeDefaultBrowser,
 };
 
 #if BUILDFLAG(CHROMIUM_BRANDING) && !defined(NDEBUG)
@@ -425,6 +428,14 @@ NSString* kDevViewSourceKey = @"DevViewSource";
   [model addItem:[self googleServicesCellItem]
       toSectionWithIdentifier:SectionIdentifierAccount];
 
+  if (@available(iOS 14, *)) {
+    if (base::FeatureList::IsEnabled(kDefaultBrowserSettings)) {
+      [model addSectionWithIdentifier:SectionIdentifierDefaultBrowser];
+      [model addItem:[self defaultBrowserCellItem]
+          toSectionWithIdentifier:SectionIdentifierDefaultBrowser];
+    }
+  }
+
   // Basics section
   [model addSectionWithIdentifier:SectionIdentifierBasics];
   // Show managed UI if default search engine is managed by policy.
@@ -530,6 +541,16 @@ NSString* kDevViewSourceKey = @"DevViewSource";
       kSettingsGoogleSyncAndServicesCellId;
   [self updateGoogleServicesItem:googleServicesItem];
   return googleServicesItem;
+}
+
+- (TableViewItem*)defaultBrowserCellItem {
+  TableViewTextItem* defaultBrowser =
+      [[TableViewTextItem alloc] initWithType:ItemTypeDefaultBrowser];
+  defaultBrowser.text =
+      l10n_util::GetNSString(IDS_IOS_SETTINGS_SET_DEFAULT_BROWSER);
+  defaultBrowser.textColor = [UIColor colorNamed:kBlueColor];
+
+  return defaultBrowser;
 }
 
 - (TableViewItem*)accountCellItem {
@@ -917,6 +938,15 @@ NSString* kDevViewSourceKey = @"DevViewSource";
     case ItemGoogleServices:
       base::RecordAction(base::UserMetricsAction("Settings.GoogleServices"));
       [self showSyncGoogleService];
+      break;
+    case ItemTypeDefaultBrowser:
+      [tableView deselectRowAtIndexPath:indexPath animated:YES];
+      base::RecordAction(base::UserMetricsAction("Settings.DefaultBrowser"));
+      [[UIApplication sharedApplication]
+                    openURL:
+                        [NSURL URLWithString:UIApplicationOpenSettingsURLString]
+                    options:{}
+          completionHandler:nil];
       break;
     case ItemTypeSearchEngine:
       base::RecordAction(base::UserMetricsAction("EditSearchEngines"));
