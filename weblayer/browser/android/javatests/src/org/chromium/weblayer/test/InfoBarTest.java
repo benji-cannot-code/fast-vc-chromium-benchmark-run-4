77852c78b845bdbec4eeee62088a5ab1b0b3210d
Colin Blundell <blundell@chromium.org>
2020-09-14 09:31:30 +0000

    [WebLayer] Re-enable InfoBarTest.testTabDestruction
    This flake was likely caused by https://bugs.chromium.org/p/chromium/issues/detail?id=1116311,
    as indicated on
    https://bugs.chromium.org/p/chromium/issues/detail?id=1116247
    ; re-enabling to verify that that is the case.
    
    Original change's description:
    > Disable flaky test InfoBarTest.testTabDestruction
    >
    > org.chromium.weblayer.test.InfoBarTest.testTabDestruction is flaky on
    > Android,
    > https://analysis.chromium.org/p/chromium/flake-portal/flakes/
    > occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVydwsSBUZsYWtlImxjaHJvbWl1bUB3ZWJs
    > YXllcl9wcml2YXRlX2luc3RydW1lbnRhdGlvbl90ZXN0X2Fwa0BvcmcuY2hyb21pdW0ud2Vi
    > bGF5ZXIudGVzdC5JbmZvQmFyVGVzdCN0ZXN0VGFiRGVzdHJ1Y3Rpb24M,
    > disable it for now until it is fixed.
    >
    > TBR=blundell@chromium.org
    >
    > Bug: 1116247
    > Change-Id: I98a6348a7120de2adb121b221084965ab6cac1e8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2355389
    > Reviewed-by: Lan Wei <lanwei@chromium.org>
    > Commit-Queue: Lan Wei <lanwei@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#797974}
    
    TBR=blundell@chromium.org,lanwei@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1116247
    Change-Id: Ie1e34760d5b3e2f82bcbc39a057c85bcb718b71f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2407256
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806569}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/InfoBarTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/InfoBarTest.java
@@ -149,7 +149,6 @@ public class InfoBarTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "Flaky - https://crbug.com/1116247")
     /**
      * Tests that the infobar container view is removed as part of tab destruction.
      *
