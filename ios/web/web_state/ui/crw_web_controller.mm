d894c6f77e38162f784e6fe38a634337346481c8
Justin Cohen <justincohen@google.com>
2020-09-14 19:33:36 +0000

    ios: Use webkit.postMessage to flush IPC during session restore.
    Session restore uses history.pushState to inject URLs and titles into
    each WKWebView session.  For large numbers of pages, this can generate
    a very large number of IPC calls. iOS 12.2 introduced IPC throttling
    that can delay the async pushState calls before the sync history.go
    call. This was worked around via a check to see when all the push state
    calls are correctly processed.
    
    iOS14 introduced an optimization which breaks the above mentioned
    workaround.  Instead, this change uses our own IPC call to native code
    (which will be enqueued to the end of the IPC queue) to finalize session
    restore.
    
    The effect is faster restore for the average case, but slightly slower
    restore for the edge case of a large numbers of pages.
    
    Bug: 1127521, 1126863
    Change-Id: Ibb70008afef312897e8165e06f16ed5c03808b38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2407332
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806721}

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -87,6 +87,9 @@ NSString* const kIsMainFrame = @"isMainFrame";
 // URL scheme for messages sent from javascript for asynchronous processing.
 NSString* const kScriptMessageName = @"crwebinvoke";
 
+// URL scheme for session restore.
+NSString* const kSessionRestoreScriptMessageName = @"session_restore";
+
 }  // namespace
 
 @interface CRWWebController () <CRWWKNavigationHandlerDelegate,
@@ -442,6 +445,15 @@ typedef void (^ViewportStateCompletion)(const web::PageViewportState*);
           [weakSelf didReceiveScriptMessage:message];
         }
                            name:kScriptMessageName
+                        webView:_webView];
+
+    // TODO(crbug.com/1127521) Consider consolidating session restore script
+    // logic into a different place.
+    [messageRouter
+        setScriptMessageHandler:^(WKScriptMessage* message) {
+          [weakSelf didReceiveSessionRestoreScriptMessage:message];
+        }
+                           name:kSessionRestoreScriptMessageName
                         webView:_webView];
 
     _webView.allowsBackForwardNavigationGestures =
@@ -1103,6 +1115,22 @@ typedef void (^ViewportStateCompletion)(const web::PageViewportState*);
   // Broken out into separate method to catch errors.
   if (![self respondToWKScriptMessage:message]) {
     DLOG(WARNING) << "Message from JS not handled due to invalid format";
+  }
+}
+
+// TODO(crbug.com/1127521) Consider consolidating session restore script
+// logic into a different place.
+- (void)didReceiveSessionRestoreScriptMessage:(WKScriptMessage*)message {
+  if ([message.name isEqualToString:kSessionRestoreScriptMessageName] &&
+      [message.body[@"offset"] isKindOfClass:[NSNumber class]]) {
+    NSString* method =
+        [NSString stringWithFormat:@"_crFinishSessionRestoration('%@')",
+                                   message.body[@"offset"]];
+    // Don't use |_jsInjector| -executeJavaScript here, as it relies on
+    // |windowID| being injected before window.onload starts.
+    web::ExecuteJavaScript(self.webView, method, nil);
+  } else {
+    DLOG(WARNING) << "Invalid session restore JS message name.";
   }
 }
 
