9f5cf3414b612162f9519c798ef0106f749d7fd0
Pranav Batra <batrapranav@chromium.org>
2020-09-15 23:05:40 +0000

    Add PrintPreviewStickySettings::GetRecentlyUsedPrinters()
    Add method GetRecentlyUsedPrinters() for use with the CUPS proxy.
    
    Bug: 1015662
    Test: xvfb-run ./unit_tests --gtest_filter=PrintPreviewStickySettingsUnittest.*
    
    Change-Id: I908f47d45259ab8dd941d320dad66275b95e00b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359729
    Commit-Queue: Pranav Batra <batrapranav@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807228}

--- chrome/browser/printing/print_preview_sticky_settings.h
+++ chrome/browser/printing/print_preview_sticky_settings.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_PRINTING_PRINT_PREVIEW_STICKY_SETTINGS_H_
 
 #include <string>
+#include <vector>
 
 #include "base/containers/flat_map.h"
 #include "base/optional.h"
@@ -43,6 +44,11 @@ class PrintPreviewStickySettings {
   // The rank is the position in the list of recently used printers. The lower
   // the rank the more recent the printer was used.
   base::flat_map<std::string, int> GetPrinterRecentlyUsedRanks();
+
+  // Parses serialized printing sticky settings state and extracts the list of
+  // recently used printers. The list is ordered from most recently used to
+  // least recently used.
+  std::vector<std::string> GetRecentlyUsedPrinters();
 
   static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 
