199c0a5448116139e3f7dac1406f61e9d9081c56
Tom Anderson <thomasanderson@chromium.org>
2020-09-15 20:35:25 +0000

    [XProto] Remove old x11::* constants
    BUG=1066670
    R=sky
    
    Change-Id: I3df7737031e8aecc9463b8f42fb5bcf8b5722693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410841
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807165}

--- ui/gfx/x/x11_error_tracker.cc
+++ ui/gfx/x/x11_error_tracker.cc
@@ -23,7 +23,7 @@ int X11ErrorHandler(Display* display, XErrorEvent* error) {
 namespace gfx {
 
 X11ErrorTracker::X11ErrorTracker() {
-  XSync(GetXDisplay(), x11::False);
+  XSync(GetXDisplay(), false);
   old_handler_ = reinterpret_cast<void*>(XSetErrorHandler(X11ErrorHandler));
   g_x11_error_code = 0;
 }
@@ -33,7 +33,7 @@ X11ErrorTracker::~X11ErrorTracker() {
 }
 
 bool X11ErrorTracker::FoundNewError() {
-  XSync(GetXDisplay(), x11::False);
+  XSync(GetXDisplay(), false);
   unsigned char error = g_x11_error_code;
   g_x11_error_code = 0;
   return error != 0;
--- ui/gl/gl_context_glx_unittest.cc
+++ ui/gl/gl_context_glx_unittest.cc
@@ -35,7 +35,7 @@ TEST(GLContextGLXTest, MAYBE_DoNotDestroyOnFailedMakeCurrent) {
   XSetWindowAttributes swa;
   memset(&swa, 0, sizeof(swa));
   swa.background_pixmap = 0;
-  swa.override_redirect = x11::True;
+  swa.override_redirect = true;
   auto xwindow = static_cast<x11::Window>(XCreateWindow(
       xdisplay, XDefaultRootWindow(xdisplay), 0, 0, 10,
       10,  // x, y, width, height
@@ -48,7 +48,7 @@ TEST(GLContextGLXTest, MAYBE_DoNotDestroyOnFailedMakeCurrent) {
   XMapWindow(xdisplay, static_cast<uint32_t>(xwindow));
   // Since this window is override-redirect, syncing is sufficient
   // to ensure the map is complete.
-  XSync(xdisplay, x11::False);
+  XSync(xdisplay, false);
 
   GLImageTestSupport::InitializeGL(base::nullopt);
   auto surface = gl::InitializeGLSurface(base::MakeRefCounted<GLSurfaceGLXX11>(
@@ -65,7 +65,7 @@ TEST(GLContextGLXTest, MAYBE_DoNotDestroyOnFailedMakeCurrent) {
   XDestroyWindow(xdisplay, static_cast<uint32_t>(xwindow));
   // Since this window is override-redirect, syncing is sufficient
   // to ensure the window is destroyed and unmapped.
-  XSync(xdisplay, x11::False);
+  XSync(xdisplay, false);
   ASSERT_FALSE(error_tracker.FoundNewError());
 
   if (context->MakeCurrent(surface.get())) {
@@ -84,7 +84,7 @@ TEST(GLContextGLXTest, MAYBE_DoNotDestroyOnFailedMakeCurrent) {
   // not destroyed.
   ASSERT_TRUE(context->GetHandle());
   surface = nullptr;
-  XSync(xdisplay, x11::True);
+  XSync(xdisplay, true);
 }
 
 }  // namespace gl
