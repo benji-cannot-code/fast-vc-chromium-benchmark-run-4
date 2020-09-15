100f2bc968f06057844de1eaba6023d99aeed846
David Tseng <dtseng@chromium.org>
2020-09-15 03:00:36 +0000

    Fixes undefined access in group node's location getter
    R=anastasi@google.com, dmazzoni@chromium.org
    
    AX-Relnotes: n/a
    Fixed: 1126707
    Change-Id: I812918d101fba188b7c6f488bb9658b571fe612b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410347
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806864}

--- chrome/browser/resources/chromeos/accessibility/switch_access/nodes/group_node.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/nodes/group_node.js
@@ -36,7 +36,8 @@ class GroupNode extends SAChildNode {
 
   /** @override */
   get location() {
-    const childLocations = this.children_.map(c => c.location);
+    const childLocations =
+        this.children_.filter(c => c.isValidAndVisible()).map(c => c.location);
     return RectUtil.unionAll(childLocations);
   }
 
