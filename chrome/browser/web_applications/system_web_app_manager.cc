54bb5e739ea5e5dc32b6d72f1467746760bcc458
Bo Majewski <majewski@chromium.org>
2020-09-15 03:02:15 +0000

    Reland "Files SWA: Moving to a new directory."
    This is a reland of 4ccb650800cd7de83ae3cdb71d33bfd97cfe0d9f
    
    http://crrev.com/c/2367397 and this CL were prepared without
    the authors being aware of each other's efforts.
    
    Timeline for 2367397:
    Sep 10, 2020, 6:06:39AM UTC+10 it is submitted to CQ
    Sep 10, 2020, 9:01:21AM UTC+10 it is successfully submitted
    
    Timeline for 2402560
    Sep 10, 2020, 10:46:31AM UTC+10 dry run is successfully completed
    Sep 10, 2020, 12:19:23PM UTC+10 it is submitted to CQ
    Sep 10, 2020, 12:23:56PM UTC+10 it is successfully submitted
    
    
    Original change's description:
    > Files SWA: Moving to a new directory.
    >
    > Bug: 1111229
    > Change-Id: Id0509caf9d1d9de8efa293344e682610cf1a8699
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2381330
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    > Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Commit-Queue: Bo Majewski <majewski@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#805598}
    
    Bug: 1111229
    Change-Id: I8894da793702136d5e394b9854993026603c311d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2402560
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806865}

--- chrome/browser/web_applications/system_web_app_manager.cc
+++ chrome/browser/web_applications/system_web_app_manager.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/constants.h"
 
 #if !defined(OFFICIAL_BUILD)
+#include "chrome/browser/chromeos/web_applications/file_manager_web_app_info.h"
 #include "chrome/browser/chromeos/web_applications/sample_system_web_app_info.h"
 #include "chrome/browser/chromeos/web_applications/telemetry_extension_web_app_info.h"
 #endif  // !defined(OFFICIAL_BUILD)
@@ -196,6 +197,12 @@ base::flat_map<SystemAppType, SystemAppInfo> CreateSystemWebApps() {
   }
 
   infos.emplace(
+      SystemAppType::FILE_MANAGER,
+      SystemAppInfo("File Manager", GURL("chrome://file-manager"),
+                    base::BindRepeating(&CreateWebAppInfoForFileManager)));
+  infos.at(SystemAppType::FILE_MANAGER).capture_navigations = true;
+
+  infos.emplace(
       SystemAppType::SAMPLE,
       SystemAppInfo(
           "Sample", GURL("chrome://sample-system-web-app/pwa.html"),
@@ -327,6 +334,8 @@ bool SystemWebAppManager::IsAppEnabled(SystemAppType type) {
     case SystemAppType::TELEMETRY:
       return base::FeatureList::IsEnabled(
           chromeos::features::kTelemetryExtension);
+    case SystemAppType::FILE_MANAGER:
+      return base::FeatureList::IsEnabled(chromeos::features::kFilesSWA);
     case SystemAppType::SAMPLE:
       NOTREACHED();
       return false;
