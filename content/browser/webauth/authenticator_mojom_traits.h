9d151f905a6123389a4a2297e8cd49aed6321890
Ken Rockot <rockot@google.com>
2020-09-15 23:30:18 +0000

    Remove //content typemap files
    This merges all remaining typemap configs in //content into their
    respective mojom target definitions.
    
    Bug: 1059389
    Change-Id: Ieeb43ce49ff4bdb2a0c4b95965f374dcca73bc3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2407928
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807244}

--- content/browser/webauth/authenticator_mojom_traits.h
+++ content/browser/webauth/authenticator_mojom_traits.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/enum_traits.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 #include "third_party/blink/public/common/common_export.h"
-#include "third_party/blink/public/mojom/webauthn/authenticator.mojom.h"
+#include "third_party/blink/public/mojom/webauthn/authenticator.mojom-shared.h"
 
 namespace mojo {
 
