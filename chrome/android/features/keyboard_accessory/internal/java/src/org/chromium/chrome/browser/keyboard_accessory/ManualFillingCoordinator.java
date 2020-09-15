b1de776bab2749a0568a5b0136c2e8fb1a23c327
Friedrich Horschig <fhorschig@chromium.org>
2020-09-15 08:37:47 +0000

    [Mfill][Android] Recreate destroyed filling components
    There are rare cases when the the Android native UI is recreated but the
    native parts remain untouched (e.g. when the theme is changed).
    
    To prevent native classes using stale providers, reset them when the
    component is destroyed and recreate them lazily as needed.
    (Since this touches the way the native object is created, clean that
    up as well to prevent rare crashes.)
    
    Bug: 1094397, 1051030
    Change-Id: I9c5cd1e225db1b4ca61c5136571a0e552fe86940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2403641
    Reviewed-by: Vadym Doroshenko  <dvadym@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806934}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingCoordinator.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingCoordinator.java
@@ -10,6 +10,7 @@ import android.view.ViewStub;
 
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.ObserverList;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryCoordinator;
 import org.chromium.chrome.browser.keyboard_accessory.data.KeyboardAccessoryData;
@@ -31,6 +32,7 @@ import org.chromium.ui.base.WindowAndroid;
  */
 class ManualFillingCoordinator implements ManualFillingComponent {
     private final ManualFillingMediator mMediator = new ManualFillingMediator();
+    private ObserverList<Observer> mObserverList = new ObserverList<>();
 
     public ManualFillingCoordinator() {}
 
@@ -56,6 +58,7 @@ class ManualFillingCoordinator implements ManualFillingComponent {
 
     @Override
     public void destroy() {
+        for (Observer observer : mObserverList) observer.onDestroy();
         mMediator.destroy();
     }
 
@@ -125,6 +128,16 @@ class ManualFillingCoordinator implements ManualFillingComponent {
     @Override
     public boolean isFillingViewShown(View view) {
         return mMediator.isFillingViewShown(view);
+    }
+
+    @Override
+    public boolean addObserver(Observer observer) {
+        return mObserverList.addObserver(observer);
+    }
+
+    @Override
+    public boolean removeObserver(Observer observer) {
+        return mObserverList.addObserver(observer);
     }
 
     @VisibleForTesting
