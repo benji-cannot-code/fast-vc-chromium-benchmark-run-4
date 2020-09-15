4847b3a3f41377c122739edf99234fc374223e21
Adlai Holler <adlai@google.com>
2020-09-15 20:05:26 +0000

    Use GrRecordingContext to get maxTextureSize
    GrContext is going away, and GrRecordingContext can do the job.
    
    Change-Id: Id0108ea8336fb6e2acb82261a8a80fa618fcf9b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2411648
    Auto-Submit: Adlai Holler <adlai@google.com>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807152}

--- cc/paint/skia_paint_canvas.h
+++ cc/paint/skia_paint_canvas.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/paint_flags.h"
 #include "cc/paint/paint_record.h"
 #include "third_party/skia/include/core/SkCanvas.h"
-#include "third_party/skia/include/gpu/GrContext.h"
+#include "third_party/skia/include/gpu/GrRecordingContext.h"
 
 namespace cc {
 class ImageProvider;
@@ -152,7 +152,7 @@ class CC_PAINT_EXPORT SkiaPaintCanvas final : public PaintCanvas {
   void FlushAfterDrawIfNeeded();
 
   int max_texture_size() const {
-    auto* context = canvas_->getGrContext();
+    auto* context = canvas_->recordingContext();
     return context ? context->maxTextureSize() : 0;
   }
 
