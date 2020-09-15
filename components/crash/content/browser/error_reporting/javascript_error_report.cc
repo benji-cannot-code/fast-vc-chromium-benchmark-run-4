88fae3dfb7cab610d77e679bbef5533fe5c0849d
Ian Barkley-Yeung <iby@chromium.org>
2020-09-15 00:48:25 +0000

    Split off code that sends JavaScript error reports
    Split the code that sends the JavaScript error reports out from
    CrashReportPrivateReportErrorFunction.
    
    Future updates mean this code will also be called from
    content/browser/webui/.
    
    This CL should be have no functional changes other than a few small
    bug fixes, namely:
    * Redaction should happen after removing the error
      message to ensure the string matching works as expected.
    * Properly convert line number and column number to strings.
    
    Bug: chromium:1121816
    Change-Id: Ibf1636fc3d32323a7d0350ee3cb91b3d81259d57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2399577
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806798}

---
+++ components/crash/content/browser/error_reporting/javascript_error_report.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/crash/content/browser/error_reporting/javascript_error_report.h"
+
+JavaScriptErrorReport::JavaScriptErrorReport() = default;
+JavaScriptErrorReport::JavaScriptErrorReport(const JavaScriptErrorReport& rhs) =
+    default;
+JavaScriptErrorReport::JavaScriptErrorReport(
+    JavaScriptErrorReport&& rhs) noexcept = default;
+JavaScriptErrorReport::~JavaScriptErrorReport() = default;
+JavaScriptErrorReport& JavaScriptErrorReport::operator=(
+    const JavaScriptErrorReport& rhs) = default;
+JavaScriptErrorReport& JavaScriptErrorReport::operator=(
+    JavaScriptErrorReport&& rhs) noexcept = default;
--- components/crash/content/browser/error_reporting/mock_crash_endpoint.cc
+++ components/crash/content/browser/error_reporting/mock_crash_endpoint.cc
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "extensions/browser/api/crash_report_private/mock_crash_endpoint.h"
+#include "components/crash/content/browser/error_reporting/mock_crash_endpoint.h"
 
 #include "base/run_loop.h"
 #include "base/threading/scoped_blocking_call.h"
+#include "build/build_config.h"
+#include "components/crash/content/browser/error_reporting/send_javascript_error_report.h"
 #include "components/crash/core/app/crash_reporter_client.h"
-#include "extensions/browser/api/crash_report_private/crash_report_private_api.h"
 #include "net/http/http_status_code.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_request.h"
@@ -31,27 +32,31 @@ class MockCrashEndpoint::Client : public crash_reporter::CrashReporterClient {
         FROM_HERE, base::BlockingType::MAY_BLOCK);
     return owner_->consented_;
   }
+#if defined(OS_POSIX) && !defined(OS_MAC)
   void GetProductNameAndVersion(std::string* product_name,
                                 std::string* version,
                                 std::string* channel) override {
-    *product_name = "Chrome (Chrome OS)";
+    *product_name = "Chrome_ChromeOS";
     *version = "1.2.3.4";
     *channel = "Stable";
   }
-
+#endif
  private:
   MockCrashEndpoint* owner_;
 };
 
+MockCrashEndpoint::Report::Report(std::string query_value,
+                                  std::string content_value)
+    : query(std::move(query_value)), content(std::move(content_value)) {}
+
 MockCrashEndpoint::MockCrashEndpoint(
     net::test_server::EmbeddedTestServer* test_server)
     : test_server_(test_server) {
-  test_server->RegisterRequestHandler(
-      base::Bind(&MockCrashEndpoint::HandleRequest, base::Unretained(this)));
+  test_server->RegisterRequestHandler(base::BindRepeating(
+      &MockCrashEndpoint::HandleRequest, base::Unretained(this)));
   EXPECT_TRUE(test_server->Start());
 
-  extensions::api::SetCrashEndpointForTesting(
-      test_server->GetURL(kTestCrashEndpoint).spec());
+  SetCrashEndpointForTesting(test_server->GetURL(kTestCrashEndpoint).spec());
   client_ = std::make_unique<Client>(this);
   crash_reporter::SetCrashReporterClient(client_.get());
 }
@@ -60,15 +65,15 @@ MockCrashEndpoint::~MockCrashEndpoint() {
   crash_reporter::SetCrashReporterClient(nullptr);
 }
 
-const MockCrashEndpoint::Report MockCrashEndpoint::WaitForReport() {
-  if (!last_report_.query.empty()) {
-    return std::move(last_report_);
+MockCrashEndpoint::Report MockCrashEndpoint::WaitForReport() {
+  if (last_report_) {
+    return *last_report_;
   }
   base::RunLoop run_loop;
   on_report_ = run_loop.QuitClosure();
   run_loop.Run();
   on_report_.Reset();
-  return std::move(last_report_);
+  return *last_report_;
 }
 
 std::unique_ptr<net::test_server::HttpResponse>
@@ -78,7 +83,7 @@ MockCrashEndpoint::HandleRequest(const net::test_server::HttpRequest& request) {
     return nullptr;
   }
 
-  last_report_ = {absolute_url.query(), request.content};
+  last_report_ = Report(absolute_url.query(), request.content);
   auto http_response = std::make_unique<net::test_server::BasicHttpResponse>();
   http_response->set_code(net::HTTP_OK);
   http_response->set_content("123");
