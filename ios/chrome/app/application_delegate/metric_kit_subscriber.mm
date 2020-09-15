7bef1ef67494564841def4e777de871d560750a2
Olivier Robin <olivierrobin@google.com>
2020-09-15 15:34:25 +0000

    Fix unit for IOS.MetricKit.ForegroundTimePerDay
    Histogram is recorded using base::UmaHistogramTimes and has an expected
    value of 10-86400 seconds.
    The current display does not useful as everything is in the 10000+ bucket.
    
    Bug: 1127878
    Change-Id: I3f2fa6228bdb0313cc2f1cc9edbb42aad2897abf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2409909
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807040}

--- ios/chrome/app/application_delegate/metric_kit_subscriber.mm
+++ ios/chrome/app/application_delegate/metric_kit_subscriber.mm
@@ -58,7 +58,7 @@ void ReportExitReason(base::HistogramBase* histogram,
 }
 #endif
 
-void ReportDuration(const char* histogram_name, NSMeasurement* measurement)
+void ReportLongDuration(const char* histogram_name, NSMeasurement* measurement)
     API_AVAILABLE(ios(13.0)) {
   if (!measurement) {
     return;
@@ -66,7 +66,10 @@ void ReportDuration(const char* histogram_name, NSMeasurement* measurement)
   double value =
       [measurement measurementByConvertingToUnit:NSUnitDuration.seconds]
           .doubleValue;
-  base::UmaHistogramTimes(histogram_name, base::TimeDelta::FromSecondsD(value));
+  base::UmaHistogramCustomTimes(
+      histogram_name, base::TimeDelta::FromSecondsD(value),
+      base::TimeDelta::FromSeconds(1),
+      base::TimeDelta::FromSeconds(86400 /* secs per day */), 50);
 }
 
 void ReportMemory(const char* histogram_name, NSMeasurement* measurement)
@@ -278,10 +281,10 @@ void WriteDiagnosticPayloads(NSArray<MXDiagnosticPayload*>* payloads)
     return;
   }
 
-  ReportDuration("IOS.MetricKit.ForegroundTimePerDay",
-                 payload.applicationTimeMetrics.cumulativeForegroundTime);
-  ReportDuration("IOS.MetricKit.BackgroundTimePerDay",
-                 payload.applicationTimeMetrics.cumulativeBackgroundTime);
+  ReportLongDuration("IOS.MetricKit.ForegroundTimePerDay",
+                     payload.applicationTimeMetrics.cumulativeForegroundTime);
+  ReportLongDuration("IOS.MetricKit.BackgroundTimePerDay",
+                     payload.applicationTimeMetrics.cumulativeBackgroundTime);
   ReportMemory("IOS.MetricKit.AverageSuspendedMemory",
                payload.memoryMetrics.averageSuspendedMemory.averageMeasurement);
   ReportMemory("IOS.MetricKit.PeakMemoryUsage",
