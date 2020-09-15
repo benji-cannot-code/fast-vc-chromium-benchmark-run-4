d203f98ca6c1d6f861bb7d3efbd343abb4421a94
Ken Rockot <rockot@google.com>
2020-09-15 02:41:45 +0000

    Clean up unused Service Manager code
    There's a bunch of stuff here to support non-Content processes like a
    standalone Service Manager and Content-free service processes. None of
    it is used in production anymore (since the end of mus+ash), so this CL
    deletes all of it.
    
    Bug: 977637
    Change-Id: I93c9a54ccf39e5659d0aae2f495bc1c378ae5c4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410919
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#806854}

--- content/app/content_service_manager_main_delegate.h
+++ content/app/content_service_manager_main_delegate.h
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
-#include "base/memory/scoped_refptr.h"
-#include "base/single_thread_task_runner.h"
-#include "build/build_config.h"
 #include "content/public/app/content_main.h"
 #include "services/service_manager/embedder/main_delegate.h"
 
@@ -29,19 +26,7 @@ class ContentServiceManagerMainDelegate : public service_manager::MainDelegate {
   bool IsEmbedderSubprocess() override;
   int RunEmbedderProcess() override;
   void ShutDownEmbedderProcess() override;
-  service_manager::ProcessType OverrideProcessType() override;
   void InitializeMojo(mojo::core::Configuration* config) override;
-  std::vector<service_manager::Manifest> GetServiceManifests() override;
-  bool ShouldLaunchAsServiceProcess(
-      const service_manager::Identity& identity) override;
-  void AdjustServiceProcessCommandLine(
-      const service_manager::Identity& identity,
-      base::CommandLine* command_line) override;
-  void OnServiceManagerInitialized(
-      base::OnceClosure quit_closure,
-      service_manager::BackgroundServiceManager* service_manager) override;
-  std::unique_ptr<service_manager::Service> CreateEmbeddedService(
-      const std::string& service_name) override;
 
   // Sets the flag whether to start the Service Manager without starting the
   // full browser.
