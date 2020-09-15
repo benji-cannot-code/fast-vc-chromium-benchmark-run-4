f86e461fe970ccfb39b5271c0d8fa101f1c39caf
Alan Cutter <alancutter@chromium.org>
2020-09-15 04:11:11 +0000

    dpwa: Clean up fallout from user_display_mode migration bug
    This is a follow up CL to https://chromium-review.googlesource.com/c/chromium/src/+/2392168.
    
    Background:
    The above CL fixes a bug that caused bookmark app -> BMO migration to
    set the user_display_mode to kBrowser for non locally installed apps.
    The bug fix unfortunately doesn't fix the effects of that bug for users
    that ran the migration code bug.
    
    Change:
    This CL specifically looks for users affected by the bug (where their
    bookmark app is set to open in a window while the BMO app is set to
    open in a tab) and re-migrates their bookmark app user_display_mode.
    
    Known issues:
     - If the user sets their user_display_mode to open in browser tab
       after migration and before the clean up runs it will be reverted
       back to window (this is indistinguishable from the migration bug
       but should be rare).
     - This won't run for users with sync disabled (though they are
       unlikely to have this bug as sync is required to have
       non-locally installed bookmark apps).
    
    Bug: 1127128
    Change-Id: I4268fbf450760b0ef1403be209d1983fd83a8b17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2391704
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806888}

--- chrome/browser/web_applications/components/install_finalizer.h
+++ chrome/browser/web_applications/components/install_finalizer.h
@@ -96,6 +96,7 @@ class InstallFinalizer {
                            content::WebContents* web_contents);
 
   virtual void RemoveLegacyInstallFinalizerForTesting() {}
+  virtual InstallFinalizer* legacy_finalizer_for_testing();
 
   virtual void Start() {}
   virtual void Shutdown() {}
