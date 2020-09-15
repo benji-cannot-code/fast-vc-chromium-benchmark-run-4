83a2ad0f8527324dbe6812186e50d4c290335fe2
Lukasz Anforowicz <lukasza@chromium.org>
2020-09-15 16:05:22 +0000

    Make WebUIURLLoaderFactory always owned by its |receivers_|.
    This CL changes (and slightly renames) CreateWebUIURLLoaderFactory so
    that it returns mojo::PendingRemote to a WebUIURLLoaderFactory that is
    owned by its |receivers_| and will self-delete when the last receiver
    disconnects.
    
    This CL removes the ability to directly construct and own a
    std::unique_ptr<WebUIURLLoaderFactory>, because this ability means that
    the factory can be destructed while receivers bound via the Clone method
    are still alive (see the associated bug).
    
    This CL mostly just follows the pattern established earlier by
    https://crrev.com/c/2337411.
    
    Bug: 1106995
    Change-Id: I6079a8b4b3a17d838539a62234b788a3353c76c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2358309
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807050}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -129,7 +129,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/webtransport/quic_transport_connector_impl.h"
 #include "content/browser/webui/url_data_manager_backend.h"
 #include "content/browser/webui/web_ui_controller_factory_registry.h"
-#include "content/browser/webui/web_ui_url_loader_factory_internal.h"
 #include "content/browser/worker_host/dedicated_worker_host_factory_impl.h"
 #include "content/browser/worker_host/shared_worker_service_impl.h"
 #include "content/common/associated_interfaces.mojom.h"
@@ -164,6 +163,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/site_isolation_policy.h"
 #include "content/public/browser/sms_fetcher.h"
 #include "content/public/browser/storage_partition.h"
+#include "content/public/browser/web_ui_url_loader_factory.h"
 #include "content/public/common/bindings_policy.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_constants.h"
@@ -6103,8 +6103,10 @@ void RenderFrameHostImpl::CommitNavigation(
           base::nullopt /* navigation_id */, &factory_receiver,
           nullptr /* header_client */, nullptr /* bypass_redirect_checks */,
           nullptr /* disable_secure_dns */, nullptr /* factory_override */);
-      CreateWebUIURLLoaderBinding(frame_tree_node(), scheme,
-                                  std::move(factory_receiver));
+      mojo::Remote<network::mojom::URLLoaderFactory> direct_factory_for_webui(
+          CreateWebUIURLLoaderFactory(this, scheme, {}));
+      direct_factory_for_webui->Clone(std::move(factory_receiver));
+
       // If the renderer has webui bindings, then don't give it access to
       // network loader for security reasons.
       // http://crbug.com/829412: make an exception for a small whitelist
