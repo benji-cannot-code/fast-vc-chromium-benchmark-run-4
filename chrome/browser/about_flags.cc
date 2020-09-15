1b1df9e0165b7cb95ddd4a0b3c1e957671124e61
Jinsuk Kim <jinsukkim@chromium.org>
2020-09-15 08:55:09 +0000

    GestureNav: Remove NavigationSheet variation option
    Navigation sheet won't be included in the gesture navigation feature.
    This CL removes the option from #overscroll-history-navigation in
    chrome://flags.
    
    The sheet is now only used for history popup triggered by long press
    on back button.
    
    Bug: 1126341
    Change-Id: Ib4739c30fe5fbbd8d56cb1ae990313f579f60bad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2400786
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806943}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1496,12 +1496,6 @@ const FeatureEntry::FeatureVariation
          base::size(kTranslateForceTriggerOnEnglishGeo), nullptr},
         {"(Zero threshold)", kTranslateForceTriggerOnEnglishBackoff,
          base::size(kTranslateForceTriggerOnEnglishBackoff), nullptr}};
-
-const FeatureEntry::FeatureParam kOverscrollHistoryNavigationBottomSheet[] = {
-    {"overscroll_history_navigation_bottom_sheet", "true"}};
-const FeatureEntry::FeatureVariation kOverscrollHistoryNavigationVariations[] =
-    {{"Navigation sheet", kOverscrollHistoryNavigationBottomSheet,
-      base::size(kOverscrollHistoryNavigationBottomSheet), nullptr}};
 #endif  // defined(OS_ANDROID)
 
 #if defined(OS_ANDROID)
@@ -2783,12 +2777,8 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kOverscrollHistoryNavigationName,
      flag_descriptions::kOverscrollHistoryNavigationDescription,
      kOsAura | kOsAndroid,
-#if defined(OS_ANDROID)
-     FEATURE_WITH_PARAMS_VALUE_TYPE(features::kOverscrollHistoryNavigation,
-                                    kOverscrollHistoryNavigationVariations,
-                                    "OverscrollNavigation")},
-#else
      FEATURE_VALUE_TYPE(features::kOverscrollHistoryNavigation)},
+#if !defined(OS_ANDROID)
     {"pull-to-refresh", flag_descriptions::kPullToRefreshName,
      flag_descriptions::kPullToRefreshDescription, kOsAura,
      MULTI_VALUE_TYPE(kPullToRefreshChoices)},
