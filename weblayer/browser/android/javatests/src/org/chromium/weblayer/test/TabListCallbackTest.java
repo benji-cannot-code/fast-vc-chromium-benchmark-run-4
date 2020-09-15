a9e8c4f21ccf5491701fb9a745d8659d0d29a7b9
Scott Violet <sky@chromium.org>
2020-09-15 22:40:13 +0000

    weblayer: adds TabListCallback.onWillDestroyBrowserAndAllTabs
    This gives embedders a way to detect when a Browser and all tab
    are about to be destroyed. This is particularly important as
    onTabRemoved() is not called, and after this call, the Browser and
    Tabs should not be used.
    
    BUG=none
    TEST=covered by test
    
    Change-Id: I8f737a4041e717399e63510d0375f256418b22f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412473
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807217}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/TabListCallbackTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/TabListCallbackTest.java
@@ -41,6 +41,7 @@ public class TabListCallbackTest {
         public static final String ADDED = "added";
         public static final String ACTIVE = "active";
         public static final String REMOVED = "removed";
+        public static final String WILL_DESTROY = "willdestroy";
 
         private List<String> mObservedValues =
                 Collections.synchronizedList(new ArrayList<String>());
@@ -58,6 +59,11 @@ public class TabListCallbackTest {
         @Override
         public void onTabRemoved(Tab tab) {
             recordValue(REMOVED);
+        }
+
+        @Override
+        public void onWillDestroyBrowserAndAllTabs() {
+            recordValue(WILL_DESTROY);
         }
 
         private void recordValue(String parameter) {
@@ -193,5 +199,27 @@ public class TabListCallbackTest {
             mActivity.getBrowser().destroyTab(mActivity.getBrowser().createTab());
         });
         callbackHelper.waitForFirst();
+    }
+
+    @Test
+    @SmallTest
+    public void testOnWillDestroyBrowserAndAllTabs() throws Exception {
+        mActivity = mActivityTestRule.launchShellWithUrl("about:blank");
+        TabListCallbackImpl tabListCallback = new TabListCallbackImpl();
+        CallbackHelper callbackHelper = new CallbackHelper();
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            mActivity.getBrowser().registerTabListCallback(tabListCallback);
+            mActivity.getBrowser().registerTabListCallback(new TabListCallback() {
+                @Override
+                public void onWillDestroyBrowserAndAllTabs() {
+                    callbackHelper.notifyCalled();
+                }
+            });
+            mActivity.destroyFragment();
+        });
+        callbackHelper.waitForFirst();
+        Assert.assertEquals(1, tabListCallback.getObservedValues().size());
+        Assert.assertTrue(
+                tabListCallback.getObservedValues().contains(TabListCallbackImpl.WILL_DESTROY));
     }
 }
