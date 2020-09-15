9f0c4f65e0818f4fc212c8d5e69d540c5727b690
Caitlin Fischer <caitlinfischer@google.com>
2020-09-15 01:17:24 +0000

    Associate VariationIDs with the two new IDCollectionKeys.
    The keys are GOOGLE_WEB_PROPERTIES_FIRST_PARTY and
    GOOGLE_WEB_PROPERTIES_TRIGGER_FIRST_PARTY.
    
    The new keys will be used to limit the set of experiment IDs included in
    X-Client-Data headers sent in third-party contexts when the
    RestrictGoogleWebVisibility feature is enabled. Note that the feature is
    disabled by default.
    
    Also, update relevant code to consider IDs associated with the new
    FIRST_PARTY groups. To do this, GetVariationsVector() and
    GetVariationsString() were changed to accept collections of
    IDCollectionKeys.
    
    Bug: 1094303
    Change-Id: If931d8281f5a687548427101e07f3b3075cd349b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367989
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/master@{#806803}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
@@ -703,7 +703,8 @@ NSString* SerializedPref(const PrefService::Preference* pref) {
   variations::VariationsIdsProvider* provider =
       variations::VariationsIdsProvider::GetInstance();
   std::vector<variations::VariationID> ids = provider->GetVariationsVector(
-      variations::GOOGLE_WEB_PROPERTIES_ANY_CONTEXT);
+      {variations::GOOGLE_WEB_PROPERTIES_ANY_CONTEXT,
+       variations::GOOGLE_WEB_PROPERTIES_FIRST_PARTY});
   return std::find(ids.begin(), ids.end(), variationID) != ids.end();
 }
 
@@ -711,7 +712,8 @@ NSString* SerializedPref(const PrefService::Preference* pref) {
   variations::VariationsIdsProvider* provider =
       variations::VariationsIdsProvider::GetInstance();
   std::vector<variations::VariationID> ids = provider->GetVariationsVector(
-      variations::GOOGLE_WEB_PROPERTIES_TRIGGER_ANY_CONTEXT);
+      {variations::GOOGLE_WEB_PROPERTIES_TRIGGER_ANY_CONTEXT,
+       variations::GOOGLE_WEB_PROPERTIES_TRIGGER_FIRST_PARTY});
   return std::find(ids.begin(), ids.end(), variationID) != ids.end();
 }
 
