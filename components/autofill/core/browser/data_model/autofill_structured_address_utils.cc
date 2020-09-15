b59cbfe6cb9e01e0799d18e90b04f85bd04ac49a
Matthias Körber <koerber@google.com>
2020-09-15 09:36:40 +0000

    [Autofill][Leipzig] Add support for address lines
    This CL adds support for address lines as a special view on street
    addresses.
    
    Change-Id: Ifade7edd874321f7f65788a3091ffdee5199febf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410017
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/master@{#806953}

--- components/autofill/core/browser/data_model/autofill_structured_address_utils.cc
+++ components/autofill/core/browser/data_model/autofill_structured_address_utils.cc
@@ -42,6 +42,11 @@ bool StructuredNamesEnabled() {
       features::kAutofillEnableSupportForMoreStructureInNames);
 }
 
+bool StructuredAddressesEnabled() {
+  return base::FeatureList::IsEnabled(
+      features::kAutofillEnableSupportForMoreStructureInAddresses);
+}
+
 Re2RegExCache::Re2RegExCache() = default;
 
 // static
