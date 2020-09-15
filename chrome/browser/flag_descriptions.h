74657f332c0b804cff8c3f51960f4094c5d1f9f0
manukh <manukh@chromium.org>
2020-09-15 03:42:14 +0000

    [omnibox] [max-suggest] Bubble URL suggestions after search grouping.
    Search suggestions are grouped above URL suggestions regardless of
    relevance scores. There are a some exceptions e.g. default match.
    
    After grouping, this CL, bubbles URLs back above search suggestions if 2
    conditions are met: 1) There must be a sufficient score gap between the
    adjacent searches. E.g. for (S1, U1, S2), the difference in scores of S1
    and S2 must be larger than some threshold. 2) There must be a sufficient
    buffer between the URL and search scores. This only applies to the first
    URL suggestion in a series. E.g. for (S1, U1, U2, S2, U3, S3), U1 & U3
    must score higher than S2 + threshold & S3 + threshold respectively, but
    U2 need only score higher than S2.
    
    The intention is to allow good URLs to rank above relatively bad
    searches while maintaining some propensity to group URLs and searches
    into clusters.
    
    These changes are behind a 'OmniboxBubbleUrlSuggestions' feature with 4
    params:
    - OmniboxBubbleUrlSuggestionsAbsoluteGap
    - OmniboxBubbleUrlSuggestionsRelativeGap
    - OmniboxBubbleUrlSuggestionsAbsoluteBuffer
    - OmniboxBubbleUrlSuggestionsRelativeBuffer
    And chrome://flags flag 'omnibox-bubble-url-suggestions'.
    
    Bug: 1127125
    Change-Id: I43662284537e9a89ab19b7bab040f8a6ad996e2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404753
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806883}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -984,6 +984,9 @@ extern const char kOmniboxMaxURLMatchesDescription[];
 extern const char kOmniboxDynamicMaxAutocompleteName[];
 extern const char kOmniboxDynamicMaxAutocompleteDescription[];
 
+extern const char kOmniboxBubbleUrlSuggestionsName[];
+extern const char kOmniboxBubbleUrlSuggestionsDescription[];
+
 extern const char kOmniboxOnDeviceHeadSuggestionsIncognitoName[];
 extern const char kOmniboxOnDeviceHeadSuggestionsIncognitoDescription[];
 
--- components/omnibox/common/omnibox_features.h
+++ components/omnibox/common/omnibox_features.h
@@ -47,7 +47,7 @@ extern const base::Feature kEntitySuggestionsReduceLatency;
 
 // Num suggestions - these affect how many suggestions are shown based on e.g.
 // focus, page context, provider, or URL v non-URL.
-// Note that all of these are overriden and default values used instead if
+// Note that all of these are overridden and default values used instead if
 // kNewSearchFeatures is disabled.
 extern const base::Feature kMaxZeroSuggestMatches;
 extern const base::Feature kUIExperimentMaxAutocompleteMatches;
@@ -56,6 +56,9 @@ extern const base::Feature kUIExperimentMaxAutocompleteMatches;
 extern const bool kOmniboxMaxURLMatchesEnabledByDefault;
 extern const base::Feature kOmniboxMaxURLMatches;
 extern const base::Feature kDynamicMaxAutocomplete;
+
+// Ranking
+extern const base::Feature kBubbleUrlSuggestions;
 
 // On-Focus Suggestions a.k.a. ZeroSuggest.
 extern const base::Feature kClobberTriggersContextualWebZeroSuggest;
