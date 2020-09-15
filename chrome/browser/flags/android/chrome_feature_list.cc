4a342cb9ca2e1c33b4e31c71ce79c0b010508e29
Hung Vu <vuhung@google.com>
2020-09-15 22:38:24 +0000

    [Location Suggestion] Expose feature flag to Java.
    This CL exposes the smart suggestion for large downloads feature flag to Java.
    
    Bug: 1118207
    Change-Id: I034376b620414ae674a68b927e2aebd419f203c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410960
    Commit-Queue: Hung Vu <vuhung@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807216}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -81,6 +81,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &device::kWebAuthPhoneSupport,
     &download::features::kDownloadAutoResumptionNative,
     &download::features::kDownloadLater,
+    &download::features::kSmartSuggestionForLargeDownloads,
     &download::features::kUseDownloadOfflineContentProvider,
     &features::kClearOldBrowsingData,
     &features::kDownloadsLocationChange,
