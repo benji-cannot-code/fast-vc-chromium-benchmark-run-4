b64f41472ae60b3d7031ba9128aae3a830f374ad
Shakti Sahu <shaktisahu@chromium.org>
2020-09-15 06:54:53 +0000

    Video IPH : Added video iph card
    This CL adds:
    1 - VideoIPHCreator that takes a ViewStub and inflates into an IPH
    2 - A manager class on NTP to manage showing various video tutorial IPHs
    3 - Added IPH features for search and download
    4 - Moved VideoTutorialServiceFactory package to upper level
    
    TODO:
     - The IPH layout isn't upto spec. Needs one more pass.
     - The IPH configs aren't fully correct. Needs one more pass. I added
     them here for the sake of review clarity and completeness.
    
    Bug: 1117147
    Change-Id: Iefdd02da06f8667fbd85a44a821d940277571559
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2367438
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#806913}

--- chrome/browser/video_tutorials/internal/android/java/src/org/chromium/chrome/browser/video_tutorials/VideoTutorialServiceFactory.java
+++ chrome/browser/video_tutorials/internal/android/java/src/org/chromium/chrome/browser/video_tutorials/VideoTutorialServiceFactory.java
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.video_tutorials.bridges;
+package org.chromium.chrome.browser.video_tutorials;
 
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.video_tutorials.VideoTutorialService;
 
 /**
  * Basic factory that creates and returns an {@link VideoTutorialService} that is attached
