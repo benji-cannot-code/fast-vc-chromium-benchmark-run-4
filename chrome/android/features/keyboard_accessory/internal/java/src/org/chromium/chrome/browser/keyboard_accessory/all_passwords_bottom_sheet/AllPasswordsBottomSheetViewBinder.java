db6c179c33f89cf461d7b240a2d346e151c58a2b
Reda Tawfik <redatawfik@google.com>
2020-09-16 14:52:59 +0000

    [Android][Mfill] Add search view to AllPasswordsBottomSheet
    This CL adds a search view to the bottom sheet and filters
    the credentials list with every newly typed character.
    
    Screenshot: https://bugs.chromium.org/p/chromium/issues/detail?id=1104132#c18
    
    Bug: 1104132
    Change-Id: Ib9528abcc8ffa3981f37fe3d7334156aa71ff0fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2391132
    Commit-Queue: Reda Tawfik <redatawfik@google.com>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807471}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/all_passwords_bottom_sheet/AllPasswordsBottomSheetViewBinder.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/all_passwords_bottom_sheet/AllPasswordsBottomSheetViewBinder.java
@@ -9,6 +9,7 @@ import static org.chromium.chrome.browser.keyboard_accessory.all_passwords_botto
 import static org.chromium.chrome.browser.keyboard_accessory.all_passwords_bottom_sheet.AllPasswordsBottomSheetProperties.CredentialProperties.IS_PASSWORD_FIELD;
 import static org.chromium.chrome.browser.keyboard_accessory.all_passwords_bottom_sheet.AllPasswordsBottomSheetProperties.CredentialProperties.ON_CLICK_LISTENER;
 import static org.chromium.chrome.browser.keyboard_accessory.all_passwords_bottom_sheet.AllPasswordsBottomSheetProperties.DISMISS_HANDLER;
+import static org.chromium.chrome.browser.keyboard_accessory.all_passwords_bottom_sheet.AllPasswordsBottomSheetProperties.ON_QUERY_TEXT_CHANGE;
 import static org.chromium.chrome.browser.keyboard_accessory.all_passwords_bottom_sheet.AllPasswordsBottomSheetProperties.SHEET_ITEMS;
 import static org.chromium.chrome.browser.keyboard_accessory.all_passwords_bottom_sheet.AllPasswordsBottomSheetProperties.VISIBLE;
 
@@ -51,6 +52,8 @@ class AllPasswordsBottomSheetViewBinder {
             view.setDismissHandler(model.get(DISMISS_HANDLER));
         } else if (propertyKey == VISIBLE) {
             view.setVisible(model.get(VISIBLE));
+        } else if (propertyKey == ON_QUERY_TEXT_CHANGE) {
+            view.setSearchQueryChangeHandler(model.get(ON_QUERY_TEXT_CHANGE));
         } else if (propertyKey == SHEET_ITEMS) {
             view.setSheetItemListAdapter(new RecyclerViewAdapter<>(
                     new SimpleRecyclerViewMcp<>(model.get(SHEET_ITEMS),
