82e472fb2ad3a888c737e2bf5b6588e256446cbe
Elly Fong-Jones <ellyjones@chromium.org>
2020-09-16 19:36:20 +0000

    exo: migrate some WidgetDelegate overrides to setters
    Instead of overriding GetWindow{Title,Icon}, use the explicit
    setters for these values, and don't store the title/icon locally.
    
    Bug: 1075649
    Change-Id: Ic69156fa8c5265bb2b421e9a56470ad5812c84be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412987
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807613}

--- components/exo/shell_surface_base.cc
+++ components/exo/shell_surface_base.cc
@@ -317,6 +317,8 @@ ShellSurfaceBase::ShellSurfaceBase(Surface* surface,
   SetRootSurface(surface);
   host_window()->Show();
   set_owned_by_client();
+
+  SetShowTitle(false);
 }
 
 ShellSurfaceBase::~ShellSurfaceBase() {
@@ -353,18 +355,12 @@ void ShellSurfaceBase::Activate() {
 void ShellSurfaceBase::SetTitle(const base::string16& title) {
   TRACE_EVENT1("exo", "ShellSurfaceBase::SetTitle", "title",
                base::UTF16ToUTF8(title));
-
-  title_ = title;
-  if (widget_)
-    widget_->UpdateWindowTitle();
+  WidgetDelegate::SetTitle(title);
 }
 
 void ShellSurfaceBase::SetIcon(const gfx::ImageSkia& icon) {
   TRACE_EVENT0("exo", "ShellSurfaceBase::SetIcon");
-
-  icon_ = icon;
-  if (widget_)
-    widget_->UpdateWindowIcon();
+  WidgetDelegate::SetIcon(icon);
 }
 
 void ShellSurfaceBase::SetSystemModal(bool system_modal) {
@@ -522,7 +518,7 @@ std::unique_ptr<base::trace_event::TracedValue>
 ShellSurfaceBase::AsTracedValue() const {
   std::unique_ptr<base::trace_event::TracedValue> value(
       new base::trace_event::TracedValue());
-  value->SetString("title", base::UTF16ToUTF8(title_));
+  value->SetString("title", base::UTF16ToUTF8(GetWindowTitle()));
   if (GetWidget() && GetWidget()->GetNativeWindow()) {
     const std::string* application_id =
         GetShellApplicationId(GetWidget()->GetNativeWindow());
@@ -693,18 +689,6 @@ bool ShellSurfaceBase::CanMaximize() const {
 bool ShellSurfaceBase::CanMinimize() const {
   // Non-transient shell surfaces can be minimized.
   return !parent_ && can_minimize_;
-}
-
-base::string16 ShellSurfaceBase::GetWindowTitle() const {
-  return title_;
-}
-
-bool ShellSurfaceBase::ShouldShowWindowTitle() const {
-  return false;
-}
-
-gfx::ImageSkia ShellSurfaceBase::GetWindowIcon() {
-  return icon_;
 }
 
 bool ShellSurfaceBase::OnCloseRequested(
