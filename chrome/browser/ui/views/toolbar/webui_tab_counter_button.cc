2157f784af2283e72512199933df6d8514490d4a
Dana Fried <dfried@chromium.org>
2020-09-16 18:32:32 +0000

    Move tab counter font calculations into chrome typography
    Previously, we were creating a separate, smaller font for two-digit
    counter numbers based on the default font.
    
    This CL does two things:
    - makes the double-digit font the STYLE_SECONDARY version of the primary
      font for the tab counter
    - changes the secondary font to Roboto Condensed on ChromeOS, as per
      design spec
    
    By moving the size and face adjustment into chrome_typography_provider,
    we bring all of the font face specifications in Top Chrome into the
    standard, unified location, avoiding having to have extraneous font
    specification code in WebUITabCounterButton.
    
    Change-Id: I187a658d6ec321c883391bb860270fb401687379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404944
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807581}

--- chrome/browser/ui/views/toolbar/webui_tab_counter_button.cc
+++ chrome/browser/ui/views/toolbar/webui_tab_counter_button.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/throbber.h"
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/layout_provider.h"
+#include "ui/views/style/typography.h"
 #include "ui/views/view_class_properties.h"
 
 namespace {
@@ -58,15 +59,10 @@ constexpr base::TimeDelta kFirstPartDuration =
 // tab counter border, the font shrinks when the count is 10 or higher.
 class NumberLabel : public views::Label {
  public:
-  NumberLabel() : Label(base::string16(), CONTEXT_WEB_UI_TAB_COUNTER) {
-    // Use the default font for single-digit tab counts.
+  NumberLabel() : Label(base::string16(), CONTEXT_TAB_COUNTER) {
     single_digit_font_ = font_list();
-
-    // Use a size adjustment of -2 because -1 makes the font impossible to
-    // center in the view.
-    constexpr int kDoubleDigitSizeAdjustment = -2;
-    double_digit_font_ =
-        single_digit_font_.DeriveWithSizeDelta(kDoubleDigitSizeAdjustment);
+    double_digit_font_ = views::style::GetFont(CONTEXT_TAB_COUNTER,
+                                               views::style::STYLE_SECONDARY);
   }
 
   ~NumberLabel() override {}
