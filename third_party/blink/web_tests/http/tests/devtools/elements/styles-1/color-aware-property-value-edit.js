3f8ee243d3a9c4a356af88a3c32127329dee2ada
Alex Rudenko <alexrudenko@chromium.org>
2020-09-16 10:46:03 +0000

    Re-enable http/tests/devtools/elements/styles-1/color-aware-property-value-edit.js
    Re-enables the test and fixes it to account for async rendering of the
    styles panel.
    
    Bug: chromium:1075869
    Change-Id: Ia148e6dc7e99a29fd14df22ea3f80253bdd10827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412731
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807403}

--- third_party/blink/web_tests/http/tests/devtools/elements/styles-1/color-aware-property-value-edit.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/styles-1/color-aware-property-value-edit.js
@@ -20,38 +20,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
 
     function editKeywordAsOriginal(next) {
-      startEditingAndDumpValue(Common.Color.Format.Original, 'border', next);
+      startEditingAndDumpValue('inspected1', Common.Color.Format.Original, 'border', next);
     },
 
     function editKeywordAsHex(next) {
-      startEditingAndDumpValue(Common.Color.Format.HEX, 'border', next);
+      startEditingAndDumpValue('inspected1', Common.Color.Format.HEX, 'border', next);
     },
 
     function editKeywordAsHSL(next) {
-      startEditingAndDumpValue(Common.Color.Format.HSL, 'border', next);
+      startEditingAndDumpValue('inspected1', Common.Color.Format.HSL, 'border', next);
     },
 
     function editKeywordAsRGB(next) {
-      startEditingAndDumpValue(Common.Color.Format.RGB, 'border', onValueDumped);
+      startEditingAndDumpValue('inspected1', Common.Color.Format.RGB, 'border', onValueDumped);
       function onValueDumped() {
         ElementsTestRunner.selectNodeAndWaitForStyles('inspected2', next);
       }
     },
 
     function editHexAsOriginal(next) {
-      startEditingAndDumpValue(Common.Color.Format.Original, 'color', next);
+      startEditingAndDumpValue('inspected2', Common.Color.Format.Original, 'color', next);
     },
 
     function editHexAsHex(next) {
-      startEditingAndDumpValue(Common.Color.Format.HEX, 'color', next);
+      startEditingAndDumpValue('inspected2', Common.Color.Format.HEX, 'color', next);
     },
 
     function editHexAsHSL(next) {
-      startEditingAndDumpValue(Common.Color.Format.HSL, 'color', next);
+      startEditingAndDumpValue('inspected2', Common.Color.Format.HSL, 'color', next);
     },
 
     function editHexAsRGB(next) {
-      startEditingAndDumpValue(Common.Color.Format.RGB, 'color', next);
+      startEditingAndDumpValue('inspected2', Common.Color.Format.RGB, 'color', next);
     },
 
     async function editNewProperty(next) {
@@ -76,15 +76,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     UI.panels.elements._stylesWidget.doUpdate().then(callback);
   }
 
-  function startEditingAndDumpValue(format, propertyName, next) {
+  function startEditingAndDumpValue(nodeId, format, propertyName, next) {
     setFormat(format, onFormatSet);
 
-    function onFormatSet() {
+    async function onFormatSet() {
+      await waitForStylesRebuild();
       treeElement = ElementsTestRunner.getElementStylePropertyTreeItem(propertyName);
       treeElement.startEditing(treeElement.valueElement);
       TestRunner.addResult(treeElement.valueElement.textContent);
       treeElement.valueElement.dispatchEvent(TestRunner.createKeyEvent('Escape'));
       next();
+    }
+
+    function waitForStylesRebuild(node) {
+      if (node && node.getAttribute('id') === nodeId)
+        return;
+      return TestRunner.addSnifferPromise(Elements.StylesSidebarPane.prototype, '_nodeStylesUpdatedForTest').then(waitForStylesRebuild);
     }
   }
 })();
