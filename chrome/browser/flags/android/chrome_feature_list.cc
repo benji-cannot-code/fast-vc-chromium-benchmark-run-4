fb5c21cb2d1138997d5e1c12043a40b5a30d15db
Xing Liu <xingliu@chromium.org>
2020-09-16 23:47:27 +0000

    Read later: Build the feature and flag for read later on Android.
    This CL adds the existing read later feature and flag to Android.
    
    We notice the backend of read later may change, but these still can be
    reused directly.
    
    TBR=dtrainor@chromium.org
    
    Bug: 1128074
    Change-Id: I0b023bf9d542743131bddbe86a8377ff6f10c18d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410669
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807718}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/performance_hints/performance_hints_features.h"
 #include "chrome/browser/share/features.h"
 #include "chrome/browser/sharing/shared_clipboard/feature_flags.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/video_tutorials/switches.h"
 #include "chrome/common/chrome_features.h"
 #include "components/autofill/core/common/autofill_features.h"
@@ -218,6 +219,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kWebApkAdaptiveIcon,
     &kPrefetchNotificationSchedulingIntegration,
     &features::kDnsOverHttps,
+    &features::kReadLater,
     &net::features::kSameSiteByDefaultCookies,
     &net::features::kCookiesWithoutSameSiteMustBeSecure,
     &paint_preview::kPaintPreviewDemo,
