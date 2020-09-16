397038e7644f158f4e0d609035a6f99fab80e609
Sharon Yang <yangsharon@chromium.org>
2020-09-16 18:26:15 +0000

    Revert "[bindings] Define cross-platform NamedMessagePortConnector impl."
    This reverts commit 13c126e31cd01c6b30fc4939005c8f3a67d84a3f.
    
    Reason for revert: This CL pulls in ui/base and increasing the size of CastRunner. The size increase is breaking the Chromium (into Fuchsia) roller.
    
    Original change's description:
    > [bindings] Define cross-platform NamedMessagePortConnector impl.
    > 
    > Refactors NamedMessagePortConnector into a platform-agnostic component,
    > which can be reused across FIDL, in-process, and testing contexts.
    > It uses Blink messages and message ports as a common basis for
    > message and port transport.
    > 
    > * Implements some missing portions of the Blink/FIDL MessagePort
    >   conversion layer.
    > * Adds a Fuchsia implementation of NMPC.
    > 
    > Bug: 1104369
    > Change-Id: I01e86c38963bfb114a7466c6f73b143e5302ad1e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2293110
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Reviewed-by: David Dorwin <ddorwin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#806684}
    
    TBR=ddorwin@chromium.org,kmarshall@chromium.org,jochen@chromium.org,lijiawei@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1104369
    Change-Id: Ie5505f33dc2b29ce82fe5d44f8919e985da35102
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414651
    Reviewed-by: Sharon Yang <yangsharon@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807575}

--- fuchsia/runners/cast/cast_component.cc
+++ fuchsia/runners/cast/cast_component.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <lib/fidl/cpp/binding.h>
 #include <lib/ui/scenic/cpp/view_ref_pair.h>
 #include <algorithm>
-#include <string>
 #include <utility>
 
 #include "base/auto_reset.h"
@@ -19,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/current_thread.h"
 #include "fuchsia/base/agent_manager.h"
 #include "fuchsia/base/mem_buffer_util.h"
-#include "fuchsia/base/message_port.h"
 #include "fuchsia/fidl/chromium/cast/cpp/fidl.h"
 #include "fuchsia/runners/cast/cast_runner.h"
 #include "fuchsia/runners/cast/cast_streaming.h"
@@ -81,7 +79,7 @@ void CastComponent::StartComponent() {
 
   WebComponent::StartComponent();
 
-  connector_ = std::make_unique<NamedMessagePortConnectorFuchsia>(frame());
+  connector_ = std::make_unique<NamedMessagePortConnector>(frame());
 
   url_rewrite_rules_provider_.set_error_handler([this](zx_status_t status) {
     ZX_LOG_IF(ERROR, status != ZX_OK, status)
@@ -118,9 +116,8 @@ void CastComponent::StartComponent() {
                              fuchsia::sys::TerminationReason::INTERNAL_ERROR);
           }
         });
-    api_bindings_client_->OnPortConnected(
-        kCastStreamingMessagePortName,
-        cr_fuchsia::BlinkMessagePortFromFidl(std::move(message_port)));
+    api_bindings_client_->OnPortConnected(kCastStreamingMessagePortName,
+                                          std::move(message_port));
   }
 
   api_bindings_client_->AttachToFrame(
@@ -170,7 +167,6 @@ void CastComponent::DestroyComponent(int64_t exit_code,
   // frame() is about to be destroyed, so there is no need to perform cleanup
   // such as removing before-load JavaScripts.
   api_bindings_client_->DetachFromFrame(frame());
-  connector_->DetachFromFrame();
 
   WebComponent::DestroyComponent(exit_code, reason);
 }
@@ -186,18 +182,8 @@ void CastComponent::OnRewriteRulesReceived(
 void CastComponent::OnNavigationStateChanged(
     fuchsia::web::NavigationState change,
     OnNavigationStateChangedCallback callback) {
-  if (change.has_is_main_document_loaded() &&
-      change.is_main_document_loaded()) {
-    // Send the NamedMessagePortConnector handshake to the page.
-    frame()->PostMessage("*",
-                         *cr_fuchsia::FidlWebMessageFromBlink(
-                             connector_->GetConnectMessage(),
-                             cr_fuchsia::TransferableHostType::kRemote),
-                         [](fuchsia::web::Frame_PostMessage_Result result) {
-                           DCHECK(result.is_response());
-                         });
-  }
-
+  if (change.has_is_main_document_loaded() && change.is_main_document_loaded())
+    connector_->OnPageLoad();
   WebComponent::OnNavigationStateChanged(std::move(change),
                                          std::move(callback));
 }
--- fuchsia/runners/cast/cast_component.h
+++ fuchsia/runners/cast/cast_component.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fuchsia/fidl/chromium/cast/cpp/fidl.h"
 #include "fuchsia/runners/cast/api_bindings_client.h"
 #include "fuchsia/runners/cast/application_controller_impl.h"
-#include "fuchsia/runners/cast/named_message_port_connector_fuchsia.h"
+#include "fuchsia/runners/cast/named_message_port_connector.h"
 #include "fuchsia/runners/common/web_component.h"
 
 namespace cr_fuchsia {
@@ -109,7 +109,7 @@ class CastComponent : public WebComponent,
   std::vector<fuchsia::web::UrlRequestRewriteRule> initial_url_rewrite_rules_;
 
   bool constructor_active_ = false;
-  std::unique_ptr<NamedMessagePortConnectorFuchsia> connector_;
+  std::unique_ptr<NamedMessagePortConnector> connector_;
   std::unique_ptr<ApiBindingsClient> api_bindings_client_;
   std::unique_ptr<ApplicationControllerImpl> application_controller_;
   chromium::cast::ApplicationContextPtr application_context_;
