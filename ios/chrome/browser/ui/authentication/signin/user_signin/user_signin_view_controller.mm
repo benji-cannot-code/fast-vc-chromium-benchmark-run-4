7462da613c4f43d13e99cdbbceeda2770bbacdd7
edchin <edchin@chromium.org>
2020-09-16 16:46:32 +0000

    [ios] Handle both opaque and transparent buttons on sign-in
    https://crrev.com/c/2312556 is a refactor that introduced a DCHECK
    failure regarding (mouse) pointer support.
    
    The signin confirmation button can either have an opaque background
    (i.e., "Add Account" or "Yes, I'm in!") or a transparent background
    (i.e., "More") when scrolling is needed to reach the bottom of the
    screen.
    
    This CL changes the style provider to one that handles both opaque
    and transparent buttons.
    
    Bug: 1127293
    Change-Id: I9e27f012d79885324b32ba918bd4f3f48a59d235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2413172
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807516}

--- ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_view_controller.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_view_controller.mm
@@ -462,7 +462,7 @@ enum AuthenticationButtonType {
     if (base::FeatureList::IsEnabled(kPointerSupport)) {
       button.pointerInteractionEnabled = YES;
       button.pointerStyleProvider =
-          CreateTransparentButtonPointerStyleProvider();
+          CreateOpaqueOrTransparentButtonPointerStyleProvider();
     }
   }
 #endif  // defined(__IPHONE_13_4)
