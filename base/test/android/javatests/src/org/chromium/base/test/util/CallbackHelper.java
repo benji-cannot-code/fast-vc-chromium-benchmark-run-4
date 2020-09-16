0e0c920602f5e20f3aba7125dbaefb5548165785
Victor Hugo Vianna Silva <victorvianna@google.com>
2020-09-16 13:08:20 +0000

    Refactor: Flip condition in CallbackHelper
    Flipping the condition to refer to exception thrown by its branch
    makes the method more readable.
    
    Bug: None
    Change-Id: I7b7d0bff7f9a43cc2664ee0f31d5a25767439ec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414035
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#807440}

--- base/test/android/javatests/src/org/chromium/base/test/util/CallbackHelper.java
+++ base/test/android/javatests/src/org/chromium/base/test/util/CallbackHelper.java
@@ -191,7 +191,7 @@ public class CallbackHelper {
                     Assert.fail(s);
                 }
             }
-            if (callCountWhenDoneWaiting > mCallCount) {
+            if (timer.isTimedOut()) {
                 throw new TimeoutException(msg == null ? "waitForCallback timed out!" : msg);
             }
         }
