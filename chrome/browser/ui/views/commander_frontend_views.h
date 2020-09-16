5f870d1781355267b4c651e0c862b8a97d44a95e
Leonard Grey <lgrey@chromium.org>
2020-09-16 19:32:24 +0000

    Commander WebUI scaffolding
    No actual WebUI code added yet; this is just the layer that sets
    everything up and handles communication.
    
    Bug: 1014639
    Change-Id: Ie121d6c02d4d92b84eb24d3412291f93b40de09f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2380670
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807610}

--- chrome/browser/ui/views/commander_frontend_views.h
+++ chrome/browser/ui/views/commander_frontend_views.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/webui/commander/commander_handler.h"
 
 class CommanderWebView;
 
@@ -29,7 +30,8 @@ struct CommanderViewModel;
 // this class is responsible for setting up the infrastructure to host the
 // WebUI in its own widget and mediating between the WebUI implementation and
 // the controller.
-class CommanderFrontendViews : public commander::CommanderFrontend {
+class CommanderFrontendViews : public commander::CommanderFrontend,
+                               public CommanderHandler::Delegate {
  public:
   explicit CommanderFrontendViews(commander::CommanderBackend* backend);
   ~CommanderFrontendViews() override;
@@ -38,17 +40,12 @@ class CommanderFrontendViews : public commander::CommanderFrontend {
   void Show(Browser* browser) override;
   void Hide() override;
 
-  // TODO(lgrey): When the WebUI layer is added, these declarations should
-  // be moved to the CommanderHandler::Delegate interface.
-  // Called when the text is changed in the WebUI interface.
-  void OnTextChanged(const base::string16& text);
-  // Called when an option is selected (clicked or enter pressed) in the WebUI
-  // interface.
-  void OnOptionSelected(size_t option_index, int result_set_id);
-  // Called when the WebUI interface wants to dismiss the UI.
-  void OnDismiss();
-  // Called when the WebUI interface's content height has changed.
-  void OnHeightChanged(int new_height);
+  // CommanderHandler::Delegate overrides;
+  void OnTextChanged(const base::string16& text) override;
+  void OnOptionSelected(size_t option_index, int result_set_id) override;
+  void OnDismiss() override;
+  void OnHeightChanged(int new_height) override;
+  void OnHandlerEnabled(bool is_enabled) override;
 
  private:
   // Receives view model updates from |backend_|.
@@ -84,7 +81,8 @@ class CommanderFrontendViews : public commander::CommanderFrontend {
   std::unique_ptr<CommanderWebView> web_view_;
   // The browser |widget_| is attached to.
   Browser* browser_;
-
+  // Whether the web UI interface is loaded and ready to accept view models.
+  bool is_handler_enabled_ = false;
   base::WeakPtrFactory<CommanderFrontendViews> weak_ptr_factory_{this};
 };
 
