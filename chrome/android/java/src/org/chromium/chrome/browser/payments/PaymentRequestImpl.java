b67bbeb26b1f19b186a2d8bebf91749b5b6ac858
Liquan (Max) Gu <maxlg@chromium.org>
2020-09-16 16:50:40 +0000

    [PRImpl] Move SkipToGPayHelper into //components
    Change:
    * Take the browser dependency canActivateExperiment() out of
    SkipToGPayHelper into an individual class SkipToGPayHelperUtil because
    this method happens to be a static one.
    * Move SkipToGPayHelper into //components
    
    Bug: 1102522
    
    Change-Id: I7076c49908a0d86c97e4cb423dc3a80704c1d423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2405637
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807519}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
@@ -58,6 +58,7 @@ import org.chromium.components.payments.PaymentRequestUpdateEventListener;
 import org.chromium.components.payments.PaymentUIsObserver;
 import org.chromium.components.payments.PaymentValidator;
 import org.chromium.components.payments.Section;
+import org.chromium.components.payments.SkipToGPayHelper;
 import org.chromium.components.payments.UrlUtil;
 import org.chromium.content_public.browser.RenderFrameHost;
 import org.chromium.content_public.browser.WebContents;
@@ -253,7 +254,7 @@ public class PaymentRequestImpl
         assert details != null;
 
         boolean googlePayBridgeActivated = googlePayBridgeEligible
-                && SkipToGPayHelper.canActivateExperiment(mWebContents, rawMethodData);
+                && SkipToGPayHelperUtil.canActivateExperiment(mWebContents, rawMethodData);
 
         Map<String, PaymentMethodData> methodData =
                 getValidatedMethodData(rawMethodData, googlePayBridgeActivated);
--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/SkipToGPayHelperTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/SkipToGPayHelperTest.java
@@ -74,7 +74,7 @@ public final class SkipToGPayHelperTest {
     void assertCanActivateExperiment(boolean expected) {
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             Assert.assertEquals(expected,
-                    SkipToGPayHelper.canActivateExperiment(
+                    SkipToGPayHelperUtil.canActivateExperiment(
                             mRule.getWebContents(), mTestMethodData));
         });
     }
