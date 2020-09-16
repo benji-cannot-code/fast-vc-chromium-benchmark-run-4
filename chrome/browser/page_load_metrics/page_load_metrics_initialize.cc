b1cc8ba8b2ee810d9b3b31ede04fb9951b777922
Max Curran <curranmax@chromium.org>
2020-09-16 00:51:06 +0000

    Created initial classes for the redesigned Translate Metrics.
    These new classes will eventually be used to log UKM and UMA metrics related to Translate at the end of each page load. For now the classes are just skeletons with the key timings added.
    
    Design Doc with more details: https://docs.google.com/document/d/1dyWh1Xw5VgUA00VA-5PTgKQ6ItziPBnSyeDR8saJ9vM/edit
    
    Bug: 1114868
    Change-Id: Ie03b807124b8cf0d44f22665978c67bfd51a4e72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360499
    Commit-Queue: Max Curran <curranmax@chromium.org>
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Auto-Submit: Max Curran <curranmax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807276}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/observers/signed_exchange_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/tab_restore_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/third_party_metrics_observer.h"
+#include "chrome/browser/page_load_metrics/observers/translate_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.h"
 #include "chrome/browser/prerender/chrome_prerender_contents_delegate.h"
 #include "chrome/browser/profiles/profile.h"
@@ -166,6 +167,10 @@ void PageLoadMetricsEmbedder::RegisterEmbedderObservers(
       SecurityStatePageLoadMetricsObserver::MaybeCreateForProfile(
           web_contents()->GetBrowserContext()));
   tracker->AddObserver(std::make_unique<DataUseMetricsObserver>());
+  std::unique_ptr<TranslatePageLoadMetricsObserver> translate_observer =
+      TranslatePageLoadMetricsObserver::CreateIfNeeded();
+  if (translate_observer != nullptr)
+    tracker->AddObserver(std::move(translate_observer));
 }
 
 bool PageLoadMetricsEmbedder::IsPrerendering() const {
---
+++ components/translate/core/browser/translate_metrics_logger_impl.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/translate/core/browser/translate_metrics_logger_impl.h"
+
+namespace translate {
+
+void TranslateMetricsLoggerImpl::OnPageLoadStart(bool is_foreground) {
+  is_foreground_ = is_foreground;
+}
+
+void TranslateMetricsLoggerImpl::OnForegroundChange(bool is_foreground) {
+  is_foreground_ = is_foreground;
+}
+
+void TranslateMetricsLoggerImpl::RecordMetrics(bool is_final) {
+  // TODO(curranmax): Log UKM and UMA metrics now that the page load is.
+  // completed. https://crbug.com/1114868.
+
+  sequence_no_++;
+}
+
+}  // namespace translate
