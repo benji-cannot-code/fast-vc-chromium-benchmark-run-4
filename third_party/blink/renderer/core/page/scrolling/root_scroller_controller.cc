0888b7a0dbc953cca55ab9f8cb97c5e37354a866
David Bokan <bokan@chromium.org>
2020-09-16 00:13:57 +0000

    Fix root scroller crash
    Root scroller assumes that if a LayoutBox is scrollable it must have an
    associated Node. However, there are some edge cases where an anonymous
    LayoutBox is created, such as for overflowing <input> and <fieldset>
    controls.
    
    To be strictly correct, we could make root scroller work in these cases
    but it's not likely to be useful in any use cases. It'd also be
    non-trivial work to fix root scroller machinery - for example, "is root
    scroller" bits are currently cached on the Node's LayoutBox rather than
    on the LayoutBoxForScrolling. Instead, we'll simply ignore these cases
    to avoid crashing.
    
    Bug: 1125621
    Change-Id: I92880f4430aaa4a4b67d9bdf1ebed2e8dada7d9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2411396
    Auto-Submit: David Bokan <bokan@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807262}

--- third_party/blink/renderer/core/page/scrolling/root_scroller_controller.cc
+++ third_party/blink/renderer/core/page/scrolling/root_scroller_controller.cc
@@ -91,8 +91,10 @@ PaintLayerScrollableArea* GetScrollableArea(const Element& element) {
     return frame_view->LayoutViewport();
   }
 
-  DCHECK(element.GetLayoutObject()->IsBox());
-  return ToLayoutBox(element.GetLayoutObject())->GetScrollableArea();
+  if (!element.GetLayoutBoxForScrolling())
+    return nullptr;
+
+  return element.GetLayoutBoxForScrolling()->GetScrollableArea();
 }
 
 }  // namespace
