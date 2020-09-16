f16f6d085c91f18d34db5ebca1fd2793e82c397b
Sigurd Schneider <sigurds@chromium.org>
2020-09-16 12:45:12 +0000

    [DevTools] Restores HeapSnapshot tests
    In https://crrev.com/c/1962191 we switched off various HeapSnapshot
    tests in order to complete the ES Modules migration. However, switching
    these tests back on is not as trivial since they are inspector/CDP tests
    that rely on the DevTools frontend JS as a dependency, and it is not
    possible to load the frontend code over the file:// protocol used to
    boot these tests. This CL switches the tests back on, and duplicates the
    functionality from the frontend necessary to run these tests, on the
    assumption that the supporting JS for these tests will need to be
    managed independently of the frontend going forward.
    
    Bug: 1110817
    Change-Id: I2d0d6714e1a2b42a324dbdddda5f94ec49ee72a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2396078
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807430}

--- third_party/blink/web_tests/inspector-protocol/heap-profiler/resources/heap-snapshot-common.js
+++ third_party/blink/web_tests/inspector-protocol/heap-profiler/resources/heap-snapshot-common.js
@@ -7,18 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // This script is supposed to be evaluated in inspector-protocol/heap-profiler tests
   // and the relative paths below are relative to that location.
-  await testRunner.loadScriptModule(
-      '../../../../devtools-frontend/src/front_end/platform/utilities.js');
-  await testRunner.loadScriptModule(
-      '../../../../devtools-frontend/src/front_end/common/common-legacy.js');
-  await testRunner.loadScriptModule(
-      '../../../../devtools-frontend/src/front_end/heap_snapshot_model/HeapSnapshotModel.js');
-  await testRunner.loadScriptModule(
-      '../../../../devtools-frontend/src/front_end/heap_snapshot_worker/HeapSnapshot.js');
-  await testRunner.loadScriptModule(
-      '../../../../devtools-frontend/src/front_end/text_utils/TextUtils.js');
-  await testRunner.loadScriptModule(
-      '../../../../devtools-frontend/src/front_end/heap_snapshot_worker/HeapSnapshotLoader.js');
+  await testRunner.loadScript('./resources/HeapSnapshotLoader.js');
 
   // Expose the (de)serialize code from Common because the worker expects it on self.
   // TODO(https://crbug.com/680046) Remove the dupe code below.
