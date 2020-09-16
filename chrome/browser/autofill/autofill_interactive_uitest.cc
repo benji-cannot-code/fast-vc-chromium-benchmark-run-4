ff81a87dd0b89d8ac65a728baf0e4e18c17403d9
Carlos IL <carlosil@chromium.org>
2020-09-16 20:33:45 +0000

    Add mixed forms warnings to testing config
    This also adapts ssl_browsertest.cc with the new behavior (i.e. not
    removing the lock icon for mixed forms if mixed form warnings are
    enabled) and removed mixed content from autofill_interactive_uitest.cc
    
    Bug: 1105210
    Change-Id: I39f6910ac3bdfbbc7d33b719c1d9eca3b7b8ee43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2405555
    Auto-Submit: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807645}

--- chrome/browser/autofill/autofill_interactive_uitest.cc
+++ chrome/browser/autofill/autofill_interactive_uitest.cc
@@ -94,7 +94,7 @@ namespace autofill {
 namespace {
 
 static const char kTestShippingFormString[] =
-    "<form action=\"http://www.example.com/\" method=\"POST\">"
+    "<form action=\"https://www.example.com/\" method=\"POST\">"
     "<label for=\"firstname\">First name:</label>"
     " <input type=\"text\" id=\"firstname\"><br>"
     "<label for=\"lastname\">Last name:</label>"
@@ -124,7 +124,7 @@ static const char kTestShippingFormString[] =
     "</form>";
 
 static const char kTestBillingFormString[] =
-    "<form action=\"http://www.example.com/\" method=\"POST\">"
+    "<form action=\"https://www.example.com/\" method=\"POST\">"
     "<label for=\"firstname_billing\">First name:</label>"
     " <input type=\"text\" id=\"firstname_billing\"><br>"
     "<label for=\"lastname_billing\">Last name:</label>"
@@ -174,7 +174,7 @@ static const char kTestEventFormString[] =
     "var selectchange = false;"
     "var selectblur = false;"
     "</script>"
-    "<form action=\"http://www.example.com/\" method=\"POST\">"
+    "<form action=\"https://www.example.com/\" method=\"POST\">"
     "<label for=\"firstname\">First name:</label>"
     " <input type=\"text\" id=\"firstname\"><br>"
     "<label for=\"lastname\">Last name:</label>"
@@ -214,7 +214,7 @@ static const char kTestEventFormString[] =
     "</form>";
 
 static const char kTestShippingFormWithCompanyString[] =
-    "<form action=\"http://www.example.com/\" method=\"POST\">"
+    "<form action=\"https://www.example.com/\" method=\"POST\">"
     "<label for=\"firstname\">First name:</label>"
     " <input type=\"text\" id=\"firstname\"><br>"
     "<label for=\"lastname\">Last name:</label>"
@@ -1269,7 +1269,7 @@ IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest,
   // is no enough space to show at least one row of the autofill popup and hence
   // the autofill shouldn't be shown.
   static const char kTestFormWithLargeInputField[] =
-      "<form action=\"http://www.example.com/\" method=\"POST\">"
+      "<form action=\"https://www.example.com/\" method=\"POST\">"
       "<label for=\"firstname\">First name:</label>"
       " <input type=\"text\" id=\"firstname\" style=\"position:fixed; "
       "top:-100px;height:5120px\"><br>"
@@ -1326,7 +1326,7 @@ IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest, OnDeleteValueAfterAutofill) {
 // background color when choosing an option from the datalist suggestion list.
 IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest, OnSelectOptionFromDatalist) {
   static const char kTestForm[] =
-      "<form action=\"http://www.example.com/\" method=\"POST\">"
+      "<form action=\"https://www.example.com/\" method=\"POST\">"
       "  <input list=\"dl\" type=\"search\" id=\"firstname\"><br>"
       "  <datalist id=\"dl\">"
       "  <option value=\"Adam\"></option>"
@@ -1363,7 +1363,7 @@ IN_PROC_BROWSER_TEST_F(
     AutofillInteractiveTest,
     OnSelectOptionFromDatalistTurningToPasswordFieldAndBack) {
   static const char kTestForm[] =
-      "<form action=\"http://www.example.com/\" method=\"POST\">"
+      "<form action=\"https://www.example.com/\" method=\"POST\">"
       "  <input list=\"dl\" type=\"search\" id=\"firstname\"><br>"
       "  <datalist id=\"dl\">"
       "  <option value=\"Adam\"></option>"
@@ -1649,7 +1649,7 @@ IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest,
 // JavaScript, with only one actually visible at any given time.
 IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest, AutofillFormWithRepeatedField) {
   static const char kForm[] =
-      "<form action=\"http://www.example.com/\" method=\"POST\">"
+      "<form action=\"https://www.example.com/\" method=\"POST\">"
       "<label for=\"firstname\">First name:</label>"
       " <input type=\"text\" id=\"firstname\""
       "        onfocus=\"domAutomationController.send(true)\"><br>"
@@ -1698,7 +1698,7 @@ IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest, AutofillFormWithRepeatedField) {
 IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest,
                        AutofillFormWithNonAutofillableField) {
   static const char kForm[] =
-      "<form action=\"http://www.example.com/\" method=\"POST\">"
+      "<form action=\"https://www.example.com/\" method=\"POST\">"
       "<label for=\"firstname\">First name:</label>"
       " <input type=\"text\" id=\"firstname\""
       "        onfocus=\"domAutomationController.send(true)\"><br>"
@@ -1744,7 +1744,7 @@ IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest,
 // Test that we can Autofill dynamically generated forms.
 IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest, DynamicFormFill) {
   static const char kDynamicForm[] =
-      "<form id=\"form\" action=\"http://www.example.com/\""
+      "<form id=\"form\" action=\"https://www.example.com/\""
       "      method=\"POST\"></form>"
       "<script>"
       "function AddElement(name, label) {"
@@ -1964,7 +1964,7 @@ IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest, MAYBE_AutofillAfterTranslate) {
   CreateTestProfile();
 
   static const char kForm[] =
-      "<form action=\"http://www.example.com/\" method=\"POST\">"
+      "<form action=\"https://www.example.com/\" method=\"POST\">"
       "<label for=\"fn\">なまえ</label>"
       " <input type=\"text\" id=\"fn\""
       "        onfocus=\"domAutomationController.send(true)\""
