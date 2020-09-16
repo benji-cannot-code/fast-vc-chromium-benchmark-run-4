1cd57c6493b48054106996598bb55c50477b27f9
James Hawkins <jhawkins@chromium.org>
2020-09-16 00:42:47 +0000

    cr_components/network: Convert NetworkProxy to Polymer 3.
    R=khorimoto@chromium.org
    
    Bug: 1045266
    Test: NetworkComponentsNetworkProxyTest*
    Change-Id: I500d1455da3ecfcb3c53c8477db539eb353e88b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412969
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807273}

--- ui/webui/resources/cr_components/chromeos/network/network_proxy.js
+++ ui/webui/resources/cr_components/chromeos/network/network_proxy.js
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview Polymer element for displaying and editing network proxy
  * values.
  */
-(function() {
-'use strict';
 
 Polymer({
   is: 'network-proxy',
@@ -564,4 +562,3 @@ Polymer({
     return property === value;
   },
 });
-})();
