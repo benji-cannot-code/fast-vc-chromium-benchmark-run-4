09354e2708dd431a704080cb1911750b2e26d6f4
Anastasiia N <anastasiian@chromium.org>
2020-09-16 18:01:30 +0000

    Add closure targets for chrome/test/data/webui/inline_login/
    Bug: 1127861
    Change-Id: I26642c3fb4e465ca1adf6dde3eb363fd079476c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2409692
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807564}

--- chrome/test/data/webui/inline_login/inline_login_test.js
+++ chrome/test/data/webui/inline_login/inline_login_test.js
@@ -12,10 +12,10 @@ import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {assertDeepEquals, assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
-
 import {getFakeAccountsList, TestAuthenticator, TestInlineLoginBrowserProxy} from './inline_login_test_util.js';
 
 window.inline_login_test = {};
+const inline_login_test = window.inline_login_test;
 inline_login_test.suiteName = 'InlineLoginTest';
 
 /** @enum {string} */
@@ -35,10 +35,12 @@ suite(inline_login_test.suiteName, () => {
   let testAuthenticator;
 
   setup(() => {
+    document.body.innerHTML = '';
     testBrowserProxy = new TestInlineLoginBrowserProxy();
     InlineLoginBrowserProxyImpl.instance_ = testBrowserProxy;
     document.body.innerHTML = '';
-    inlineLoginComponent = document.createElement('inline-login-app');
+    inlineLoginComponent = /** @type {InlineLoginAppElement} */ (
+        document.createElement('inline-login-app'));
     document.body.appendChild(inlineLoginComponent);
     testAuthenticator = new TestAuthenticator();
     inlineLoginComponent.setAuthExtHostForTest(testAuthenticator);
@@ -65,14 +67,15 @@ suite(inline_login_test.suiteName, () => {
     assertEquals(fakeAuthExtensionData, testAuthenticator.data);
     assertEquals(fakeAuthExtensionData.authMode, testAuthenticator.authMode);
 
-    const fakeLstFetchResults = '{result: "fakeLstFetchResults"}';
-    webUIListenerCallback('send-lst-fetch-results', fakeLstFetchResults);
-    assertEquals(
-        1,
-        testBrowserProxy.getCallCount('lstFetchResults', fakeLstFetchResults));
-
     webUIListenerCallback('close-dialog');
     assertEquals(1, testBrowserProxy.getCallCount('dialogClose'));
+
+    const fakeLstFetchResults = '{result: "fakeLstFetchResults"}';
+    webUIListenerCallback('send-lst-fetch-results', fakeLstFetchResults);
+    assertEquals(1, testBrowserProxy.getCallCount('lstFetchResults'));
+    return testBrowserProxy.whenCalled('lstFetchResults').then(args => {
+      assertEquals(fakeLstFetchResults, args);
+    });
   });
 
   test(assert(inline_login_test.TestNames.AuthExtHostCallbacks), async () => {
@@ -102,7 +105,7 @@ suite(inline_login_test.suiteName, () => {
 
     testAuthenticator.dispatchEvent(new Event('getAccounts'));
     assertEquals(1, testBrowserProxy.getCallCount('getAccounts'));
-    testBrowserProxy.whenCalled('getAccounts').then(function() {
+    return testBrowserProxy.whenCalled('getAccounts').then(function() {
       assertEquals(1, testAuthenticator.getAccountsResponseCalls);
       assertDeepEquals(
           getFakeAccountsList(), testAuthenticator.getAccountsResponseResult);
@@ -118,10 +121,10 @@ suite(inline_login_test.suiteName, () => {
     }
 
     let backInWebviewCalls = 0;
-    inlineLoginComponent.$.signinFrame.back = () => backInWebviewCalls++;
+    inlineLoginComponent.$$('#signinFrame').back = () => backInWebviewCalls++;
 
     // If we cannot go back in the webview - we should close the dialog.
-    inlineLoginComponent.$.signinFrame.canGoBack = () => false;
+    inlineLoginComponent.$$('#signinFrame').canGoBack = () => false;
     backButton.click();
     assertEquals(1, testBrowserProxy.getCallCount('dialogClose'));
     assertEquals(0, backInWebviewCalls);
@@ -129,7 +132,7 @@ suite(inline_login_test.suiteName, () => {
     testBrowserProxy.reset();
 
     // Go back in the webview if possible.
-    inlineLoginComponent.$.signinFrame.canGoBack = () => true;
+    inlineLoginComponent.$$('#signinFrame').canGoBack = () => true;
     backButton.click();
     assertEquals(0, testBrowserProxy.getCallCount('dialogClose'));
     assertEquals(1, backInWebviewCalls);
--- chrome/test/data/webui/inline_login/inline_login_test_util.js
+++ chrome/test/data/webui/inline_login/inline_login_test_util.js
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {AuthMode, AuthParams} from 'chrome://chrome-signin/gaia_auth_host/authenticator.m.js';
 import {InlineLoginBrowserProxy} from 'chrome://chrome-signin/inline_login_browser_proxy.js';
 import {NativeEventTarget as EventTarget} from 'chrome://resources/js/cr/event_target.m.js';
 
@@ -17,9 +16,20 @@ export function getFakeAccountsList() {
 export class TestAuthenticator extends EventTarget {
   constructor() {
     super();
-    /** @type {?AuthMode} */
+    // Note: We cannot import types from authenticator.m.js because we replace
+    // "chrome://chrome-signin/" with "chrome/browser/resources/inline_login/"
+    // and authenticator is in "chrome/browser/resources/gaia_auth_host/"
+    // folder.
+
+    /**
+     * Type AuthMode (see Authenticator).
+     * @type {?Object}
+     */
     this.authMode = null;
-    /** @type {?AuthParams} */
+    /**
+     * Type AuthParams (see Authenticator).
+     * @type {?Object}
+     */
     this.data = null;
     /** @type {number} */
     this.loadCalls = 0;
@@ -30,8 +40,9 @@ export class TestAuthenticator extends EventTarget {
   }
 
   /**
-   * @param {AuthMode} authMode Authorization mode.
-   * @param {AuthParams} data Parameters for the authorization flow.
+   * @param {Object} authMode Authorization mode (type AuthMode).
+   * @param {Object} data Parameters for the authorization flow (type
+   *     AuthParams).
    */
   load(authMode, data) {
     this.loadCalls++;
