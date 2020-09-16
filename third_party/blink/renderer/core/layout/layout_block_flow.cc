1da71ff9c92ab4abcec07acbc122ed9c62557248
Frédéric Wang <fwang@igalia.com>
2020-09-16 06:50:57 +0000

    [mathml] Disable multicol for MathML layout
    This CL disables multicol for MathML, since it has been causing issue
    and is not needed for math layout. This is consistent with flexbox,
    grid, table or custom layout for example. In particular, this fixes
    an assertion failure when multicol is used on MathML token elements.
    
    Bug: 6606, 1128267
    Change-Id: I9bb365b7af25e11b994dfc8f2470b3fa4ea2adeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410131
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#807361}

--- third_party/blink/renderer/core/layout/layout_block_flow.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow.cc
@@ -4408,6 +4408,10 @@ void LayoutBlockFlow::CreateOrDestroyMultiColumnFlowThreadIfNeeded(
   if (IsLayoutNGCustom())
     return;
 
+  // MathML layout objects don't support multicol.
+  if (IsMathML())
+    return;
+
   auto* flow_thread = LayoutMultiColumnFlowThread::CreateAnonymous(
       GetDocument(), StyleRef(), !CanTraversePhysicalFragments());
   AddChild(flow_thread);
