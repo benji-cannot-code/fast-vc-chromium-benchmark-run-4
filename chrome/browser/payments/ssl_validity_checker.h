276c0a867bbbcbdc57727f31aa7a12d3af72a7f8
Jaeyong Bae <jdragon.bae@gmail.com>
2020-09-16 17:02:23 +0000

    [Web Payment] Reland header view changes.
    Original code reviews: https://crrev.com/c/2351959
                           https://crrev.com/c/2345928,
                           https://crrev.com/c/2302970
                           https://crrev.com/c/2257090
    Reverted in: https://crrev.com/c/2354149
    Reason for revert: Suspected of causing the
                       https://crbug.com/1114556 crash.
    Reason for reland: The root cause of the crash was determined
                       to be something else. More test coverage
                       is added.
    
    Original change's description:
    > [Web Payment] Revert header view changes.
    >
    > This reverts https://crrev.com/c/2351959, https://crrev.com/c/2345928,
    > https://crrev.com/c/2302970, and https://crrev.com/c/2257090 because
    > they are causing hard-to-diagnose crashes in production for a feature
    > that is supposed to be still turned off. The patches should be
    > relanded with more test coverage.
    >
    > Bug: 1114556, 1052493
    > Change-Id: Ic221c99e28246676d24bcd2185773657abef4781
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2354149
    > Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    > Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#797651}
    
    Bug: 1052493
    Change-Id: I4b16ee3d383fd01431498444f8d2ef709b3d6204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2389860
    Commit-Queue: Jaeyong Bae <jdragon.bae@gmail.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807531}

--- chrome/browser/payments/ssl_validity_checker.h
+++ chrome/browser/payments/ssl_validity_checker.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "components/security_state/core/security_state.h"
 
 namespace content {
 class WebContents;
@@ -32,6 +33,11 @@ class SslValidityChecker {
   // Whether the given page should be allowed to be displayed in a payment
   // handler window.
   static bool IsValidPageInPaymentHandlerWindow(
+      content::WebContents* web_contents);
+
+  // Returns the security level of `web_contents`. The `web_contents` parameter
+  // should not be null.
+  static security_state::SecurityLevel GetSecurityLevel(
       content::WebContents* web_contents);
 
  private:
