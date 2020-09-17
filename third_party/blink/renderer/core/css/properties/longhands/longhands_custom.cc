7ecfc653cae887dfac5e1e193a121e4ff1cf2f94
Fredrik Söderqvist <fs@opera.com>
2020-09-17 19:07:21 +0000

    Rework foreignObject zoom and remove -internal-reset-effective
    Remove the "foreignObject zoom hack" and apply the reverse zoom factor
    before processing foreignObject children instead.
    This means that descendants of the <foreignObject> will get zoom applied
    "as normal" and thus they will convert back to unzoomed space properly.
    For resources the <foreignObject> element now needs to use a zoomed
    reference box because the userspace of the element is after the zoom
    factor has been reversed.
    
    Since font-size for <foreignObject> descendants are now subjected to
    proper zoom there is some changes to how text of those descendants are
    rendered (arguably more accurate).
    The ref. for svg/custom/foreignObject-font-size-on-zoom.html is adjusted
    to account for this.
    
    Bug: 976224
    Change-Id: Id98539322d28c0cb66b57b6380d6095ab63d6d86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2179515
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807994}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -8217,10 +8217,7 @@ const CSSValue* Zoom::ParseSingleValue(CSSParserTokenRange& range,
   const CSSParserToken& token = range.Peek();
   CSSValue* zoom = nullptr;
   if (token.GetType() == kIdentToken) {
-    CSSIdentifierValue* ident = css_parsing_utils::ConsumeIdent<
-        CSSValueID::kNormal, CSSValueID::kInternalResetEffective>(range);
-    if (ident && isValueAllowedInMode(ident->GetValueID(), context.Mode()))
-      zoom = ident;
+    zoom = css_parsing_utils::ConsumeIdent<CSSValueID::kNormal>(range);
   } else {
     zoom = css_parsing_utils::ConsumePercent(range, context,
                                              kValueRangeNonNegative);
@@ -8258,14 +8255,6 @@ void Zoom::ApplyInherit(StyleResolverState& state) const {
 }
 
 void Zoom::ApplyValue(StyleResolverState& state, const CSSValue& value) const {
-  // TODO(crbug.com/976224): Support zoom on foreignObject
-  if (const auto* ident = DynamicTo<CSSIdentifierValue>(value)) {
-    if (ident->GetValueID() == CSSValueID::kInternalResetEffective) {
-      state.SetEffectiveZoom(ComputedStyleInitialValues::InitialZoom());
-      return;
-    }
-  }
-
   state.SetZoom(StyleBuilderConverter::ConvertZoom(state, value));
 }
 
--- third_party/blink/renderer/core/layout/svg/layout_svg_resource_masker.h
+++ third_party/blink/renderer/core/layout/svg/layout_svg_resource_masker.h
@@ -41,7 +41,8 @@ class LayoutSVGResourceMasker final : public LayoutSVGResourceContainer {
 
   void RemoveAllClientsFromCache() override;
 
-  FloatRect ResourceBoundingBox(const FloatRect& reference_box);
+  FloatRect ResourceBoundingBox(const FloatRect& reference_box,
+                                float reference_box_zoom);
 
   SVGUnitTypes::SVGUnitType MaskUnits() const;
   SVGUnitTypes::SVGUnitType MaskContentUnits() const;
@@ -49,8 +50,7 @@ class LayoutSVGResourceMasker final : public LayoutSVGResourceContainer {
   static const LayoutSVGResourceType kResourceType = kMaskerResourceType;
   LayoutSVGResourceType ResourceType() const override { return kResourceType; }
 
-  sk_sp<const PaintRecord> CreatePaintRecord(AffineTransform&,
-                                             const FloatRect&,
+  sk_sp<const PaintRecord> CreatePaintRecord(const AffineTransform&,
                                              GraphicsContext&);
 
  private:
