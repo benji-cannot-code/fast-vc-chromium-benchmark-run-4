6aadf88c5e8f7e89089e8a6e36099ae641c13dc6
David Tseng <dtseng@chromium.org>
2020-09-17 22:30:49 +0000

    Do not move ChromeVox focus ring on value changes
    R=akihiroota@chromium.org
    
    Fixed: 1098914
    AX-Relnotes: ChromeVox will not move its focus indicator when adjusting the volume using hardware keys.
    Change-Id: I6d64fb39dd63bab76630b667ba37ea5d96a02c25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416751
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808112}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/desktop_automation_handler.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/desktop_automation_handler.js
@@ -429,6 +429,7 @@ DesktopAutomationHandler = class extends BaseAutomationHandler {
       this.lastValueChanged_ = new Date();
 
       const output = new Output();
+      output.withoutFocusRing();
 
       if (fromDesktop &&
           (!this.lastValueTarget_ || this.lastValueTarget_ !== t)) {
