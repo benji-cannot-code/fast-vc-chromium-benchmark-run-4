6580da305aacb907bbe8b689a2f4f21d4e3042bd
Peter Wen <wnwen@chromium.org>
2020-09-17 18:27:35 +0000

    Android: Add lint regression test (reland)
    New API related checks are some of our most valuable lint checks, as
    triggering an error causes a crash directly.
    
    This CL adds compile test targets to ensure that code with API level
    errors trigger lint warnings. This prevents lint being accidentally
    turned off.
    
    Also delete now obsolete //build/android/lint directory and remove some
    already-fixed suppressions from lint-suppressions.xml.
    
    This is a reland of 3f2f2acf908f07e204634e09ebe89f1f8a81fd14 with
    missing LintTest.java added back.
    
    Tbr: mheikal@chromium.org
    Bug: 1108309
    Change-Id: Ic5ac63ccd8aac2ebd3193d26294b8e27b233c903
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416575
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807963}

---
+++ build/android/java/test/LintTest.java
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package test;
+
+import android.content.Context;
+
+public class LintTest {
+    public static String testTriggerInlinedApiCheck() {
+        // This was added in API level 30
+        return Context.CONNECTIVITY_DIAGNOSTICS_SERVICE;
+    }
+    public static String testTriggerNewApiCheck() {
+        // This was added in API level 30
+        return new Context().getAttributionTag();
+    }
+}
