30ce48850471456be675beb58332f5904fd60fb6
Sergei Datsenko <dats@google.com>
2020-09-17 11:06:58 +0000

    Expose Drive lookup by document ID
    Bug: 1105381
    Change-Id: I4f63f8d7de3bf9bd530dbe842fe47d736e2b04d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2413567
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Commit-Queue: Sergei Datsenko <dats@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807844}

--- chromeos/components/drivefs/fake_drivefs.h
+++ chromeos/components/drivefs/fake_drivefs.h
@@ -56,7 +56,8 @@ class FakeDriveFs : public drivefs::mojom::DriveFs,
                    bool pinned,
                    bool shared,
                    const mojom::Capabilities& capabilities,
-                   const mojom::FolderFeature& folder_feature);
+                   const mojom::FolderFeature& folder_feature,
+                   const std::string& doc_id);
 
   const base::FilePath& mount_path() { return mount_path_; }
 
@@ -122,6 +123,10 @@ class FakeDriveFs : public drivefs::mojom::DriveFs,
       drivefs::mojom::ExtensionConnectionParamsPtr params,
       mojo::PendingReceiver<drivefs::mojom::NativeMessagingHost> session,
       mojo::PendingRemote<drivefs::mojom::NativeMessagingPort> port) override;
+
+  void LocateFilesByItemIds(
+      const std::vector<std::string>& item_ids,
+      drivefs::mojom::DriveFs::LocateFilesByItemIdsCallback callback) override;
 
   const base::FilePath mount_path_;
 
