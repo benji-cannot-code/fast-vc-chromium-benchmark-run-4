2df61170720fd13149c30607d088dde6949b0f1f
Xiaohan Wang <xhwang@chromium.org>
2020-09-17 21:30:33 +0000

    media: Update CDM_USE_PLATFORM_SPECIFIC_PATH define in CdmPathsTest
    Update the condition here to be consistent with cdm_paths.gni. Also
    update the comments to make sure they keep consistent.
    
    Bug: 1128458
    Change-Id: I7580d30637f9aae2d856846d74e086c64a2f123d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417137
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808070}

--- media/cdm/cdm_paths_unittest.cc
+++ media/cdm/cdm_paths_unittest.cc
@@ -13,11 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 // Only verify platform specific path on some platforms.
+// Note: The condition list here must be consistent with condition on
+// "cdm_platform_specific_path" in cdm_paths.gni.
 // TODO(crbug.com/971433). Move the CDMs out of the install directory on
 // ChromeOS.
-#if (defined(OS_MAC) || defined(OS_WIN) ||          \
-     (defined(OS_LINUX) && !defined(OS_CHROMEOS))) && \
-    (defined(ARCH_CPU_X86) || defined(ARCH_CPU_X86_64))
+#if (defined(OS_MAC) || defined(OS_WIN) || \
+     (defined(OS_LINUX) && !defined(OS_CHROMEOS)))
 #define CDM_USE_PLATFORM_SPECIFIC_PATH
 #endif
 
