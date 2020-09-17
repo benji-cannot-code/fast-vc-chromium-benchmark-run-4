1f24fd4d530ad115ec59384a2f8bf8b061245d33
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2020-09-17 18:47:29 +0000

    [BrowserSwitcher] Add ${edge} preset browser
    On Windows, the AlternativeBrowserPath policy can now be set to
    "${edge}", to open Microsoft Edge instead of IE.
    
    This will display the browser name as "Microsoft Edge" on the
    interstitial before opening it:
    
    https://i.imgur.com/Qr1rPs4.png
    
    Bug: 1124758
    Change-Id: Ibe3c24b209c1054510cceb4b0237cf068acb84ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414621
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807979}

--- chrome/browser/browser_switcher/alternative_browser_driver_unittest.cc
+++ chrome/browser/browser_switcher/alternative_browser_driver_unittest.cc
@@ -120,6 +120,10 @@ TEST_F(AlternativeBrowserDriverTest, GetBrowserName) {
   SetBrowserPath("${ie}");
   actual = driver()->GetBrowserName();
   EXPECT_EQ("Internet Explorer", actual);
+
+  SetBrowserPath("${edge}");
+  actual = driver()->GetBrowserName();
+  EXPECT_EQ("Microsoft Edge", actual);
 #endif
 
 #if defined(OS_WIN) || defined(OS_MAC)
@@ -174,6 +178,10 @@ TEST_F(AlternativeBrowserDriverTest, GetBrowserType) {
       "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
   actual = driver()->GetBrowserType();
   EXPECT_EQ(BrowserType::kChrome, actual);
+
+  SetBrowserPath("${edge}");
+  actual = driver()->GetBrowserType();
+  EXPECT_EQ(BrowserType::kEdge, actual);
 #endif
 
 #if defined(OS_WIN) || defined(OS_MAC)
