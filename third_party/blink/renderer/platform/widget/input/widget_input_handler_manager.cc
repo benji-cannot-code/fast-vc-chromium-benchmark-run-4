8143eb93659a1b89aae11a2de522eb044eb88682
Liviu Tinta <liviutinta@chromium.org>
2020-09-17 20:42:38 +0000

    Main thread Scrollbar interaction injects GSB/GSU/GSE to compositor
    If Scroll Unification is enabled, send GSB, GSU and GSE to
    CompositorThreadEventQueue instead of MainThreadEventQueue in
    |WebFrameWidgetBase::InjectGestureScrollEvent|.
    
    
    Bug: 1047198
    Change-Id: Iec178bf6053d937ed1b6cdd977ac7e42267b4ef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2392409
    Commit-Queue: Liviu Tinta <liviutinta@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808052}

--- third_party/blink/renderer/platform/widget/input/widget_input_handler_manager.cc
+++ third_party/blink/renderer/platform/widget/input/widget_input_handler_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/check_op.h"
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "cc/base/features.h"
@@ -390,6 +391,27 @@ void WidgetInputHandlerManager::LogInputTimingUMA() {
   }
 }
 
+void WidgetInputHandlerManager::DispatchScrollGestureToCompositor(
+    std::unique_ptr<WebGestureEvent> event) {
+  DCHECK(base::FeatureList::IsEnabled(features::kScrollUnification));
+  std::unique_ptr<WebCoalescedInputEvent> web_scoped_gesture_event =
+      std::make_unique<WebCoalescedInputEvent>(std::move(event),
+                                               ui::LatencyInfo());
+  DCHECK(compositor_task_runner_);
+  compositor_task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(&WidgetInputHandlerManager::
+                                    HandleInputEventWithLatencyInfoOnCompositor,
+                                this, std::move(web_scoped_gesture_event)));
+}
+
+void WidgetInputHandlerManager::HandleInputEventWithLatencyInfoOnCompositor(
+    std::unique_ptr<WebCoalescedInputEvent> event) {
+  DCHECK(base::FeatureList::IsEnabled(features::kScrollUnification));
+  DCHECK(input_handler_proxy_);
+  input_handler_proxy_->HandleInputEventWithLatencyInfo(std::move(event),
+                                                        base::DoNothing());
+}
+
 void WidgetInputHandlerManager::DispatchEvent(
     std::unique_ptr<WebCoalescedInputEvent> event,
     mojom::blink::WidgetInputHandler::DispatchEventCallback callback) {
@@ -399,7 +421,7 @@ void WidgetInputHandlerManager::DispatchEvent(
   if (!event_is_move)
     LogInputTimingUMA();
 
-  // Drop input if we are deferring a rendring pipeline phase, unless it's a
+  // Drop input if we are deferring a rendering pipeline phase, unless it's a
   // move event.
   // We don't want users interacting with stuff they can't see, so we drop it.
   // We allow moves because we need to keep the current pointer location up
@@ -437,7 +459,7 @@ void WidgetInputHandlerManager::DispatchEvent(
       return;
     }
 
-    // The InputHandlerProxy will be the first to try handing the event on the
+    // The InputHandlerProxy will be the first to try handling the event on the
     // compositor thread. It will respond to this class by calling
     // DidHandleInputEventSentToCompositor with the result of its attempt. Based
     // on the resulting disposition, DidHandleInputEventSentToCompositor will
--- third_party/blink/renderer/platform/widget/input/widget_input_handler_manager.h
+++ third_party/blink/renderer/platform/widget/input/widget_input_handler_manager.h
@@ -99,6 +99,8 @@ class PLATFORM_EXPORT WidgetInputHandlerManager final
       const WebGestureEvent& gesture_event,
       const cc::InputHandlerScrollResult& scroll_result);
 
+  void DispatchScrollGestureToCompositor(
+      std::unique_ptr<WebGestureEvent> event);
   void DispatchEvent(
       std::unique_ptr<blink::WebCoalescedInputEvent> event,
       mojom::blink::WidgetInputHandler::DispatchEventCallback callback);
@@ -225,6 +227,9 @@ class PLATFORM_EXPORT WidgetInputHandlerManager final
   void ObserveGestureEventOnInputHandlingThread(
       const WebGestureEvent& gesture_event,
       const cc::InputHandlerScrollResult& scroll_result);
+
+  void HandleInputEventWithLatencyInfoOnCompositor(
+      std::unique_ptr<WebCoalescedInputEvent>);
 
   // Returns the task runner for the thread that receives input. i.e. the
   // "Mojo-bound" thread.
