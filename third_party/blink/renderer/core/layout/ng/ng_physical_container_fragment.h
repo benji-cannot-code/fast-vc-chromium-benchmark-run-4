171518d7b8560207802dff108b40b4c8e688e5d1
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-09-17 12:36:20 +0000

    [layout] Ensure fragment tree consistency.
    This patch ensures that we always have a "consistent" fragment-tree (or
    rather a portion of a fragment-tree if we have legacy nodes).
    
    Previously it was possible for a fragment-tree to become inconsistent
    whenever we performed layout from a subtree root. Now when we layout
    from a node like this, we rebuild the "spine" of the tree.
    
    After this change we'll be able to remove most "PostLayout" calls,
    except where they are used for building this "consistent" tree.
    
    Bug: 1066616
    Change-Id: I3993fcc5be1cd9043555ecad553fc569fc985314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2407032
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807866}

--- third_party/blink/renderer/core/layout/ng/ng_physical_container_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_container_fragment.h
@@ -156,6 +156,9 @@ class CORE_EXPORT NGPhysicalContainerFragment : public NGPhysicalFragment {
                               NGFragmentType,
                               unsigned sub_type);
 
+  NGPhysicalContainerFragment(const NGPhysicalContainerFragment& other,
+                              NGLink* buffer);
+
   void AddScrollableOverflowForInlineChild(
       const NGPhysicalBoxFragment& container,
       const ComputedStyle& container_style,
