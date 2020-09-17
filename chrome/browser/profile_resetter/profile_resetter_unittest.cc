4c88cd038bfdb89fa02e4943a66db235fd8c66f1
Veranika Liaukevich <veranika@chromium.org>
2020-09-17 18:43:27 +0000

    Profile reset: ensure that all Chrome shortcuts are user-visible
    Remove all unexpected attributes from shortcuts pointing to the current Chrome binary.
    
    Bug: 970253
    Change-Id: Idfaf5cadfd8e3641f8c6593fb0f0796dce981e2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410922
    Commit-Queue: Veranika Liaukevich <veranika@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807974}

--- chrome/browser/profile_resetter/profile_resetter_unittest.cc
+++ chrome/browser/profile_resetter/profile_resetter_unittest.cc
@@ -238,6 +238,8 @@ class ShortcutHandler {
                                       const base::string16& args);
   void CheckShortcutHasArguments(const base::string16& desired_args) const;
   void Delete();
+  void HideFile();
+  bool IsFileHidden() const;
 
  private:
 #if defined(OS_WIN)
@@ -294,6 +296,20 @@ void ShortcutHandler::Delete() {
   EXPECT_TRUE(base::DeleteFile(shortcut_path_));
   shortcut_path_.clear();
 }
+
+void ShortcutHandler::HideFile() {
+  DWORD attributes = ::GetFileAttributes(shortcut_path_.value().c_str());
+  ASSERT_NE(attributes, INVALID_FILE_ATTRIBUTES);
+  ASSERT_TRUE(::SetFileAttributes(shortcut_path_.value().c_str(),
+                                  attributes | FILE_ATTRIBUTE_HIDDEN));
+}
+
+bool ShortcutHandler::IsFileHidden() const {
+  DWORD attributes = ::GetFileAttributes(shortcut_path_.value().c_str());
+  EXPECT_NE(attributes, INVALID_FILE_ATTRIBUTES);
+  return attributes & FILE_ATTRIBUTE_HIDDEN;
+}
+
 #else
 ShortcutHandler::ShortcutHandler() {}
 
@@ -315,6 +331,12 @@ void ShortcutHandler::CheckShortcutHasArguments(
 }
 
 void ShortcutHandler::Delete() {
+}
+
+void ShortcutHandler::HideFile() {}
+
+bool ShortcutHandler::IsFileHidden() const {
+  return false;
 }
 #endif  // defined(OS_WIN)
 
@@ -730,13 +752,18 @@ TEST_F(ProfileResetterTest, ResetShortcuts) {
   ShortcutCommand command_line = shortcut.CreateWithArguments(
       base::ASCIIToUTF16("chrome.lnk"),
       base::ASCIIToUTF16("--profile-directory=Default foo.com"));
+  shortcut.HideFile();
   shortcut.CheckShortcutHasArguments(base::ASCIIToUTF16(
       "--profile-directory=Default foo.com"));
+#if defined(OS_WIN)
+  ASSERT_TRUE(shortcut.IsFileHidden());
+#endif
 
   ResetAndWait(ProfileResetter::SHORTCUTS);
 
   shortcut.CheckShortcutHasArguments(base::ASCIIToUTF16(
       "--profile-directory=Default"));
+  EXPECT_FALSE(shortcut.IsFileHidden());
 }
 
 TEST_F(ProfileResetterTest, ResetFewFlags) {
