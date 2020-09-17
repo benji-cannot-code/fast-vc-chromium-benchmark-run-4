5d41bad9b234a351b16ba0164094377d20c09b09
Gabriel Marin <gmx@chromium.org>
2020-09-17 18:08:15 +0000

    Recognize and enable collection for Tigerlake devices.
    BUG=b:168615813
    
    Change-Id: Ie4f7f2c13fd050ceeb28d8b70040ef0c34126e69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412831
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Gabriel Marin <gmx@chromium.org>
    Commit-Queue: Gabriel Marin <gmx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807956}

--- chrome/browser/metrics/perf/perf_events_collector.cc
+++ chrome/browser/metrics/perf/perf_events_collector.cc
@@ -91,7 +91,7 @@ void ExtractVersionNumbers(const std::string& version,
 // Returns if a micro-architecture supports LBR callgraph profiling.
 bool MicroarchitectureHasLBRCallgraph(const std::string& uarch) {
   return uarch == "Haswell" || uarch == "Broadwell" || uarch == "Skylake" ||
-         uarch == "Kabylake";
+         uarch == "Kabylake" || uarch == "Tigerlake";
 }
 
 // Returns if a kernel release supports LBR callgraph profiling.
@@ -126,7 +126,7 @@ const char kPerfITLBMissCyclesCmdIvyBridge[] =
 const char kPerfDTLBMissCyclesCmdIvyBridge[] =
     "perf record -a -e dtlb_load_misses.walk_duration -g -c 160001";
 
-// TLB miss cycles for Skylake and Kabylake.
+// TLB miss cycles for Skylake, Kabylake, Tigerlake.
 const char kPerfITLBMissCyclesCmdSkylake[] =
     "perf record -a -e itlb_misses.walk_pending -c 30001";
 
@@ -158,7 +158,8 @@ const std::vector<RandomSelector::WeightAndValue> GetDefaultCommands_x86_64(
   const char* dtlb_miss_cycles_cmd = kPerfDTLBMissCyclesCmdIvyBridge;
   const char* lbr_cmd = kPerfLBRCmd;
 
-  if (cpu_uarch == "Skylake" || cpu_uarch == "Kabylake") {
+  if (cpu_uarch == "Skylake" || cpu_uarch == "Kabylake" ||
+      cpu_uarch == "Tigerlake") {
     itlb_miss_cycles_cmd = kPerfITLBMissCyclesCmdSkylake;
     dtlb_miss_cycles_cmd = kPerfDTLBMissCyclesCmdSkylake;
   }
@@ -172,8 +173,9 @@ const std::vector<RandomSelector::WeightAndValue> GetDefaultCommands_x86_64(
   if (cpu_uarch == "IvyBridge" || cpu_uarch == "Haswell" ||
       cpu_uarch == "Broadwell" || cpu_uarch == "SandyBridge" ||
       cpu_uarch == "Skylake" || cpu_uarch == "Kabylake" ||
-      cpu_uarch == "Silvermont" || cpu_uarch == "Airmont" ||
-      cpu_uarch == "Goldmont" || cpu_uarch == "GoldmontPlus") {
+      cpu_uarch == "Tigerlake" || cpu_uarch == "Silvermont" ||
+      cpu_uarch == "Airmont" || cpu_uarch == "Goldmont" ||
+      cpu_uarch == "GoldmontPlus") {
     cmds.push_back(WeightAndValue(50.0, kPerfCyclesCmd));
     // Haswell and newer big Intel cores support LBR callstack profiling. This
     // requires kernel support, which was added in kernel 4.4, and it was
