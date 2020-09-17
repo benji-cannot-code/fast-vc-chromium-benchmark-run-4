7989c9327b6fb1220930784353bd4ae15f720d64
Joshua Pawlicki <waffles@chromium.org>
2020-09-17 13:13:06 +0000

    Revert "USB devices are only shared with one VM at a time"
    This reverts commit 4e4ea3fdf034069879c3d14cb7118f3afd0ac0d5.
    
    Reason for revert: Speculative revert to fix BrowserTest.CloseWithAppMenuOpen failures on linux-lacros-tester-rel bot.
    
    [ RUN      ] BrowserTest.CloseWithAppMenuOpen
    [27984:27984:0917/013814.891745:WARNING:ozone_platform_wayland.cc(203)] Failed to find drm render node path.
    [27813:28015:0917/013814.933599:ERROR:object_proxy.cc(621)] Failed to call method: org.freedesktop.DBus.Properties.Get: object_path= /org/freedesktop/UPower: org.freedesktop.DBus.Error.ServiceUnknown: The name org.freedesktop.UPower was not provided by any .service files
    [27813:28015:0917/013814.933632:WARNING:property.cc(144)] DaemonVersion: GetAndBlock: failed.
    [27813:28015:0917/013814.933859:ERROR:object_proxy.cc(621)] Failed to call method: org.freedesktop.UPower.GetDisplayDevice: object_path= /org/freedesktop/UPower: org.freedesktop.DBus.Error.ServiceUnknown: The name org.freedesktop.UPower was not provided by any .service files
    [27813:28015:0917/013814.934033:ERROR:object_proxy.cc(621)] Failed to call method: org.freedesktop.UPower.EnumerateDevices: object_path= /org/freedesktop/UPower: org.freedesktop.DBus.Error.ServiceUnknown: The name org.freedesktop.UPower was not provided by any .service files
    [destroyed object]: error 1: popup parent not constructed
    
    Original change's description:
    > USB devices are only shared with one VM at a time
    > 
    > Remove map of VmSharingInfo from CrosUsbDeviceInfo since device
    > is only shared with one VM at a time.
    > 
    > TBR=khorimoto@chromium.org
    > 
    > Bug: 1129266
    > Change-Id: I2025091bf63eeed538c62d8b39cfbb744339aa64
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2413716
    > Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    > Reviewed-by: Nicholas Verne <nverne@chromium.org>
    > Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#807786}
    
    TBR=joelhockey@chromium.org,nverne@chromium.org
    
    Change-Id: I96cab98518776d10ef10d7c8376eb7379c92d6e3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1129266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416389
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807879}

--- chrome/browser/chromeos/usb/cros_usb_detector.cc
+++ chrome/browser/chromeos/usb/cros_usb_detector.cc
@@ -292,6 +292,10 @@ CrosUsbDeviceInfo::CrosUsbDeviceInfo() = default;
 CrosUsbDeviceInfo::CrosUsbDeviceInfo(const CrosUsbDeviceInfo&) = default;
 CrosUsbDeviceInfo::~CrosUsbDeviceInfo() = default;
 
+CrosUsbDeviceInfo::VmSharingInfo::VmSharingInfo() = default;
+CrosUsbDeviceInfo::VmSharingInfo::VmSharingInfo(const VmSharingInfo&) = default;
+CrosUsbDeviceInfo::VmSharingInfo::~VmSharingInfo() = default;
+
 std::string CrosUsbDetector::MakeNotificationId(const std::string& guid) {
   return "cros:" + guid;
 }
@@ -507,8 +511,10 @@ void CrosUsbDetector::OnDeviceRemoved(
 
   std::string guid = device_info->guid;
   for (const auto& device : usb_devices_) {
-    if (device.guid == guid && device.shared_vm_name) {
-      DetachUsbDeviceFromVm(*device.shared_vm_name, guid, base::DoNothing());
+    if (device.guid == guid) {
+      for (const auto& sharing_info_pair : device.vm_sharing_info) {
+        DetachUsbDeviceFromVm(sharing_info_pair.first, guid, base::DoNothing());
+      }
     }
   }
   const auto& start = std::remove_if(
@@ -531,11 +537,13 @@ void CrosUsbDetector::ConnectSharedDevicesOnVmStartup(
     const std::string& vm_name) {
   // Reattach shared devices when the VM becomes available.
   for (auto& device : usb_devices_) {
-    if (device.shared_vm_name == vm_name) {
-      VLOG(1) << "Connecting " << device.label << " to " << vm_name;
-      // Clear any older guest_port setting.
-      device.guest_port = base::nullopt;
-      AttachUsbDeviceToVm(vm_name, device.guid, base::DoNothing());
+    for (auto& sharing_pair : device.vm_sharing_info) {
+      if (sharing_pair.second.shared && sharing_pair.first == vm_name) {
+        VLOG(1) << "Connecting " << device.label << " to " << vm_name;
+        // Clear any older guest_port setting.
+        sharing_pair.second.guest_port = base::nullopt;
+        AttachUsbDeviceToVm(vm_name, device.guid, base::DoNothing());
+      }
     }
   }
 }
@@ -551,7 +559,7 @@ void CrosUsbDetector::AttachUsbDeviceToVm(
       // restart.
       // Setting this flag early also allows the UI not to flicker because of
       // the notification resulting from the default VM detach below.
-      device.shared_vm_name = vm_name;
+      device.vm_sharing_info[vm_name].shared = true;
       allowed_interfaces_mask = device.allowed_interfaces_mask;
       // The guest port will be set on completion.
       break;
@@ -594,9 +602,12 @@ void CrosUsbDetector::DetachUsbDeviceFromVm(
 
   base::Optional<uint8_t> guest_port;
   for (const auto& device : usb_devices_) {
-    if (device.guid == guid && device.shared_vm_name == vm_name) {
-      guest_port = device.guest_port;
-      break;
+    if (device.guid == guid) {
+      const auto it = device.vm_sharing_info.find(vm_name);
+      if (it != device.vm_sharing_info.end()) {
+        guest_port = it->second.guest_port;
+        break;
+      }
     }
   }
 
@@ -642,7 +653,8 @@ void CrosUsbDetector::OnAttachUsbDeviceOpened(
   }
   for (const auto& device : usb_devices_) {
     if (device.guid == device_info->guid) {
-      if (device.shared_vm_name == vm_name && device.guest_port) {
+      const auto it = device.vm_sharing_info.find(vm_name);
+      if (it != device.vm_sharing_info.end() && it->second.guest_port) {
         LOG(ERROR) << "Device " << device.label << " is already shared";
         // The device is already attached.
         std::move(callback).Run(/*success=*/true);
@@ -683,8 +695,9 @@ void CrosUsbDetector::OnUsbDeviceAttachFinished(
   if (success) {
     for (auto& device : usb_devices_) {
       if (device.guid == guid) {
-        device.shared_vm_name = vm_name;
-        device.guest_port = response->guest_port();
+        auto& vm_sharing_info = device.vm_sharing_info[vm_name];
+        vm_sharing_info.shared = true;
+        vm_sharing_info.guest_port = response->guest_port();
         break;
       }
     }
@@ -709,8 +722,7 @@ void CrosUsbDetector::OnUsbDeviceDetachFinished(
 
   for (auto& device : usb_devices_) {
     if (device.guid == guid) {
-      device.shared_vm_name = base::nullopt;
-      device.guest_port = base::nullopt;
+      device.vm_sharing_info.erase(vm_name);
       break;
     }
   }
