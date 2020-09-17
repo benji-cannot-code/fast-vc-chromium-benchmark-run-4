e52a4b20d9003900a2887855d06e386efdcf2254
Sinan Sahin <sinansahin@google.com>
2020-09-17 01:02:22 +0000

    [Offline indicator v2] Fix NPE caused by tab modal dialog
    Tab modal dialogs cause the renderer to pause and block showing and
    hiding the status indicator. Because of this, we can't reliably destroy
    and recreate as needed since we rely on signals from the renderer to do
    so.
    
    This CL reverts most of the optimizations done by destroying and
    recreating the view, and instead, only drops the bitmap cache in
    ViewResourceAdapter after the status indicator is hidden.
    
    Bug: 1126964
    Change-Id: I5f879a5cd56e68025811624c82928166ca583b0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2411164
    Auto-Submit: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807733}

--- chrome/android/junit/src/org/chromium/chrome/browser/status_indicator/StatusIndicatorMediatorTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/status_indicator/StatusIndicatorMediatorTest.java
@@ -47,9 +47,9 @@ public class StatusIndicatorMediatorTest {
     @Mock
     StatusIndicatorCoordinator.StatusIndicatorObserver mObserver;
     @Mock
-    Runnable mInflateView;
+    Runnable mRegisterResource;
     @Mock
-    Runnable mDestroyView;
+    Runnable mUnregisterResource;
     @Mock
     Supplier<Boolean> mCanAnimateNativeBrowserControls;
     @Mock
@@ -63,8 +63,8 @@ public class StatusIndicatorMediatorTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        doNothing().when(mInflateView).run();
-        doNothing().when(mDestroyView).run();
+        doNothing().when(mRegisterResource).run();
+        doNothing().when(mUnregisterResource).run();
         when(mCanAnimateNativeBrowserControls.get()).thenReturn(true);
         doNothing().when(mInvalidateCompositorView).onResult(any(Runnable.class));
         doNothing().when(mRequestLayout).run();
@@ -72,10 +72,10 @@ public class StatusIndicatorMediatorTest {
                          .with(StatusIndicatorProperties.ANDROID_VIEW_VISIBILITY, View.GONE)
                          .with(StatusIndicatorProperties.COMPOSITED_VIEW_VISIBLE, false)
                          .build();
-        mMediator = new StatusIndicatorMediator(mBrowserControlsStateProvider, () -> Color.WHITE,
-                mInflateView, mDestroyView, mCanAnimateNativeBrowserControls,
+        mMediator = new StatusIndicatorMediator(
+                mBrowserControlsStateProvider, () -> Color.WHITE, mCanAnimateNativeBrowserControls);
+        mMediator.initialize(mModel, mRegisterResource, mUnregisterResource,
                 mInvalidateCompositorView, mRequestLayout);
-        mMediator.setModel(mModel);
     }
 
     @Test
