e8eb769394157f157a8679c12faa35182e925777
Joe DeBlasio <jdeblasio@chromium.org>
2020-09-17 23:20:12 +0000

    [Lookalikes] Fail-safe on no allowlist available yet.
    Before this CL, the lookalike interstitial would still block if the
    allowlist proto was not yet available, such as immediately following
    browser start.
    
    That's bad. This CL changes the behavior to match that of Safety Tips,
    and fails-open when the proto isn't yet available.
    
    Fixed: 1129548
    Change-Id: I45ee8876223158011f740863639c4f3abcc2a29c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416403
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808130}

--- chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "chrome/browser/reputation/safety_tip_test_utils.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "components/lookalikes/core/features.h"
 #include "components/url_formatter/spoof_checks/idn_spoof_checker.h"
@@ -89,6 +90,8 @@ class LookalikeThrottleTest : public ChromeRenderViewHostTestHarness {};
 // Tests that spoofy hostnames are properly handled in the throttle.
 TEST_F(LookalikeThrottleTest, SpoofsBlocked) {
   base::HistogramTester test;
+
+  InitializeSafetyTipConfig();
 
   const struct TestCase {
     const char* hostname;
