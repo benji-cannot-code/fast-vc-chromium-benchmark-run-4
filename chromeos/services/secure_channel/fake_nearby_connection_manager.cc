614a9d634eaff6484444bec9d05af559504fbd8a
Kyle Horimoto <khorimoto@chromium.org>
2020-09-17 21:42:09 +0000

    [CrOS PhoneHub] Add NearbyConnectionManager
    This class is responsible for handling connection requests as well as
    requests to cancel an ongoing Nearby Connections attempt. This CL
    creates the a base class a a fake, as well as a skeleton implementation
    class. The implementation will be completed in a follow-up CL.
    
    Bug: 1106937
    Change-Id: I45d997192f5a055de1206f283676c7d490cd81b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2415224
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: James Vecore <vecore@google.com>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#808081}

---
+++ chromeos/services/secure_channel/fake_nearby_connection_manager.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/secure_channel/fake_nearby_connection_manager.h"
+
+namespace chromeos {
+
+namespace secure_channel {
+
+FakeNearbyConnectionManager::FakeNearbyConnectionManager() = default;
+
+FakeNearbyConnectionManager::~FakeNearbyConnectionManager() = default;
+
+void FakeNearbyConnectionManager::PerformAttemptNearbyInitiatorConnection(
+    const DeviceIdPair& device_id_pair) {}
+
+void FakeNearbyConnectionManager::PerformCancelNearbyInitiatorConnectionAttempt(
+    const DeviceIdPair& device_id_pair) {}
+
+}  // namespace secure_channel
+
+}  // namespace chromeos
---
+++ chromeos/services/secure_channel/nearby_connection_manager_impl_unittest.cc
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/secure_channel/nearby_connection_manager_impl.h"
+
+#include <memory>
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+
+namespace secure_channel {
+
+class SecureChannelNearbyConnectionManagerImplTest : public testing::Test {
+ protected:
+  SecureChannelNearbyConnectionManagerImplTest() = default;
+  SecureChannelNearbyConnectionManagerImplTest(
+      const SecureChannelNearbyConnectionManagerImplTest&) = delete;
+  SecureChannelNearbyConnectionManagerImplTest& operator=(
+      const SecureChannelNearbyConnectionManagerImplTest&) = delete;
+  ~SecureChannelNearbyConnectionManagerImplTest() override = default;
+
+  // testing::Test:
+  void SetUp() override {
+    manager_ = NearbyConnectionManagerImpl::Factory::Create();
+  }
+
+  std::unique_ptr<NearbyConnectionManager> manager_;
+};
+
+// TODO(https://crbug.com/1106937): Delete when a real test is added.
+TEST_F(SecureChannelNearbyConnectionManagerImplTest, Create) {
+  EXPECT_TRUE(manager_);
+}
+
+}  // namespace secure_channel
+
+}  // namespace chromeos
