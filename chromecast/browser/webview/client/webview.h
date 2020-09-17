d4c71054abae12e1da69369832ff84740740a5b2
Ryan Daum <rdaum@chromium.org>
2020-09-17 16:30:18 +0000

    [chromecast] Add keyboard event support for webviews
      * Convert gRPC keyboard events to ui::KeyEvents and dispatch
      * Add support for creating synthetic keyboard events to the
        test client.
    
    (Note: Due to issues with webview focus I haven't been able to confirm
    that  the events dispatch correctly, but the logic mirrors other input
    events.)
    
    Bug: internal b/161811385
    Test: manual
    Change-Id: I75f8b237cbde4f28c69f1a4d7257c4e8fe06d34d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416570
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Ryan Daum <rdaum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807920}

--- chromecast/browser/webview/client/webview.h
+++ chromecast/browser/webview/client/webview.h
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/wayland/clients/client_base.h"
 #include "third_party/grpc/src/include/grpcpp/grpcpp.h"
 #include "ui/events/event_constants.h"
+#include "ui/events/keycodes/dom/dom_code.h"
+#include "ui/events/keycodes/dom/dom_key.h"
+#include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/events/types/event_type.h"
 #include "ui/gfx/geometry/point.h"
 
@@ -86,12 +89,21 @@ class WebviewClient : public exo::wayland::clients::ClientBase {
   void SendForwardRequest(const std::vector<std::string>& tokens);
   void SendNavigationRequest(const std::vector<std::string>& tokens);
   void SendResizeRequest(const std::vector<std::string>& tokens);
+  void SendKeyRequest(const std::vector<std::string>& tokens);
+
   void SendTouchInput(const Webview* webview,
                       int x,
                       int y,
                       ui::EventType event_type,
                       uint32_t time,
                       int32_t id);
+  void SendKeyEvent(const Webview* webview,
+                    const base::TimeDelta& time,
+                    ui::DomKey dom_key,
+                    ui::DomCode dom_code,
+                    ui::KeyboardCode keyboard_code,
+                    bool down);
+
   void SetPosition(const std::vector<std::string>& tokens);
   void TakeExclusiveAccess();
   void WlDisplayCallback();
