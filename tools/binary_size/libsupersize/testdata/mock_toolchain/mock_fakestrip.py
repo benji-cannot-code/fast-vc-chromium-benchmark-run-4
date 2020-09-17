7e0920735daab54ac816a90a4f1e3c5a101986ec
Andrew Grieve <agrieve@chromium.org>
2020-09-17 05:41:15 +0000

    SuperSize: Measure section_sizes from stripped .so
    When analyzing .so files that are not from .apk files, unstripped .so
    files must be used to get symbol information, but it's not useful to
    report unstripped section_sizes.
    
    Rather than add a --stripped-elf-file parameter, always run "strip"
    before measuring section sizes (since the command is really fast).
    
    Bug: None
    Change-Id: I840fe02a9d3795cb8b1ba3f46c909f20f9997062
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2411651
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#807751}

---
+++ tools/binary_size/libsupersize/testdata/mock_toolchain/mock_fakestrip.py
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import shutil
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-o', dest='output', required=True)
+  parser.add_argument('input')
+  args = parser.parse_args()
+  shutil.copy(args.input, args.output)
+
+
+if __name__ == '__main__':
+  main()
