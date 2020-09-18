1fd968631cd40cb276ed5628e2c16ffd7ba3952b
Ioana Pandele <ioanap@chromium.org>
2020-09-18 16:18:52 +0000

    Scroll to elements that are off-screen in PasswordSettingsTest
    A few test cases require access to list elements that can be off-screen.
    Those tests should first attempt to scroll to the required element, so
    that its corresponding view actually exists in the hierarchy.
    
    Bug: 1129884
    Change-Id: Ifa5df8bdd26823c244339197d70806843763cd13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2418672
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808376}

--- chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/settings/PasswordSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/settings/PasswordSettingsTest.java
@@ -13,6 +13,7 @@ import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.scrollToHolder;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.BundleMatchers.hasEntry;
@@ -68,6 +69,8 @@ import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.view.menu.ActionMenuItemView;
 import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.preference.PreferenceViewHolder;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
@@ -155,6 +158,27 @@ public class PasswordSettingsTest {
             new SettingsActivityTestRule<>(PasswordEntryEditor.class);
     @Mock
     private PasswordCheck mPasswordCheck;
+
+    /**
+     * @param text The text that the view holder has in its view hierarchy.
+     * @return A Matcher to find a particular {@link ViewHolder} that contains certain text.
+     */
+    private static Matcher<ViewHolder> hasTextInViewHolder(String text) {
+        return new BoundedMatcher<ViewHolder, PreferenceViewHolder>(PreferenceViewHolder.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has text: " + text);
+            }
+
+            @Override
+            protected boolean matchesSafely(PreferenceViewHolder preferenceViewHolder) {
+                ArrayList<View> outViews = new ArrayList<>();
+                preferenceViewHolder.itemView.findViewsWithText(
+                        outViews, text, View.FIND_VIEWS_WITH_TEXT);
+                return !outViews.isEmpty();
+            }
+        };
+    }
 
     private static final class FakePasswordManagerHandler implements PasswordManagerHandler {
         // This class has exactly one observer, set on construction and expected to last at least as
@@ -848,7 +872,7 @@ public class PasswordSettingsTest {
     @Test
     @SmallTest
     @Feature({"Preferences"})
-    @EnableFeatures(ChromeFeatureList.EDIT_PASSWORDS_IN_SETTINGS)
+    @EnableFeatures({ChromeFeatureList.EDIT_PASSWORDS_IN_SETTINGS})
     public void testSelectedStoredPasswordIndexIsSameAsInShowPasswordEntryEditingView() {
         PasswordEditingDelegateProvider.getInstance().setPasswordEditingDelegate(
                 mMockPasswordEditingDelegate);
@@ -858,7 +882,8 @@ public class PasswordSettingsTest {
                         new SavedPasswordEntry("https://test.com", "test user", "test password")});
 
         startPasswordSettingsFromMainSettings();
-
+        Espresso.onView(withId(R.id.recycler_view))
+                .perform(scrollToHolder(hasTextInViewHolder("test user")));
         Espresso.onView(withText(containsString("test user"))).perform(click());
 
         Espresso.onView(withEditMenuIdOrText()).perform(click());
@@ -903,6 +928,8 @@ public class PasswordSettingsTest {
 
         startPasswordSettingsFromMainSettings();
 
+        Espresso.onView(withId(R.id.recycler_view))
+                .perform(scrollToHolder(hasTextInViewHolder("test user")));
         Espresso.onView(withText(containsString("test user"))).perform(click());
 
         Espresso.onView(withEditMenuIdOrText()).perform(click());
@@ -932,6 +959,8 @@ public class PasswordSettingsTest {
 
         startPasswordSettingsFromMainSettings();
 
+        Espresso.onView(withId(R.id.recycler_view))
+                .perform(scrollToHolder(hasTextInViewHolder("test user")));
         Espresso.onView(withText(containsString("test user"))).perform(click());
 
         Espresso.onView(withEditMenuIdOrText()).perform(click());
@@ -947,6 +976,8 @@ public class PasswordSettingsTest {
 
         Espresso.pressBack();
         // Check if the password preferences activity has the updated data in the list of passwords.
+        Espresso.onView(withId(R.id.recycler_view))
+                .perform(scrollToHolder(hasTextInViewHolder("test user new")));
         Espresso.onView(withText("test user new")).check(matches(isDisplayed()));
     }
 
@@ -1755,6 +1786,8 @@ public class PasswordSettingsTest {
         final SettingsActivity settingsActivity = startPasswordSettingsFromMainSettings();
 
         View mainDecorView = settingsActivity.getWindow().getDecorView();
+        Espresso.onView(withId(R.id.recycler_view))
+                .perform(scrollToHolder(hasTextInViewHolder("test user")));
         Espresso.onView(withText(containsString("test user"))).perform(click());
         Espresso.onView(withContentDescription(R.string.password_entry_viewer_copy_stored_password))
                 .perform(click());
@@ -1778,7 +1811,8 @@ public class PasswordSettingsTest {
                 ReauthenticationManager.OverrideState.AVAILABLE);
 
         startPasswordSettingsFromMainSettings();
-
+        Espresso.onView(withId(R.id.recycler_view))
+                .perform(scrollToHolder(hasTextInViewHolder("test user")));
         Espresso.onView(withText(containsString("test user"))).perform(click());
 
         // Before tapping the view button, pretend that the last successful reauthentication just
@@ -1824,7 +1858,8 @@ public class PasswordSettingsTest {
                 new SavedPasswordEntry("https://example.com", "test user", "test password"));
 
         startPasswordSettingsFromMainSettings();
-
+        Espresso.onView(withId(R.id.recycler_view))
+                .perform(scrollToHolder(hasTextInViewHolder("test user")));
         Espresso.onView(withText(containsString("test user"))).perform(click());
 
         Espresso.onView(withEditMenuIdOrText()).check(matches(isDisplayed()));
