749017828195a25a6ac8ced5075a477698d849a6
Olivier Robin <olivierrobin@google.com>
2020-09-18 10:08:50 +0000

    Fix double external URL open
    1. pendingUserActivity is handled even when session is not active
    Revert the faulty change
    2. Only send notification if presentingModalOverlay changes
    
    This is a fix for crrev.com/c/2409300
    
    Bug: 1129638
    Change-Id: I1c63d83e03ceb31179b4a836d03dc6078a734b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417892
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808297}

--- ios/chrome/browser/ui/main/scene_state.mm
+++ ios/chrome/browser/ui/main/scene_state.mm
@@ -111,6 +111,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)setPresentingModalOverlay:(BOOL)presentingModalOverlay {
+  if (_presentingModalOverlay == presentingModalOverlay) {
+    return;
+  }
   if (presentingModalOverlay) {
     [self.observers sceneStateWillShowModalOverlay:self];
   } else {
