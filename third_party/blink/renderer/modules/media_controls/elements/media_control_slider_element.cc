9cb7cb1df1ff412f0d13123bab8603e7129e0f0d
Kent Tamura <tkent@chromium.org>
2020-09-18 19:19:53 +0000

    Add two names to shadow_element_names.json5
    This CL adds "-internal-media-controls-segmented-track" and
    "-webkit-slider-runnable-track" to shadow_element_names.json5.
    
    This CL has no behavior changes, and is a preparation to port
    LayoutSliderTrack to LayoutNG.
    
    Bug: 1040826
    Change-Id: I6558e1221dc748091b2972fc6e5e7452346206df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2418271
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808457}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_slider_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_slider_element.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/media_controls/elements/media_control_slider_element.h"
 
 #include "third_party/blink/renderer/core/html/html_div_element.h"
+#include "third_party/blink/renderer/core/html/shadow/shadow_element_names.h"
 #include "third_party/blink/renderer/core/input_type_names.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/layout/layout_box_model_object.h"
@@ -115,7 +116,8 @@ void MediaControlSliderElement::SetupBarSegments() {
     return;
 
   Element& track = GetTrackElement();
-  track.SetShadowPseudoId("-internal-media-controls-segmented-track");
+  track.SetShadowPseudoId(
+      shadow_element_names::kPseudoMediaControlsSegmentedTrack);
 
   // Add the following structure to #track.
   //
