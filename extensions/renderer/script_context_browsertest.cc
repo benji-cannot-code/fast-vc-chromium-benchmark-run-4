b0fe35aeb1a3baa58a0cefbcfdd5e7023770a293
Dave Tapuska <dtapuska@chromium.org>
2020-09-18 20:15:07 +0000

    Fix a DCHECK that can fire if disallowdocumentaccess is enabled.
    Agent Clusters can be different even if the origins are the same.
    
    BUG=961448
    
    Change-Id: I1cfc8d7c386735b6c718ba6da5d932d5b459980d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416263
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808509}

--- extensions/renderer/script_context_browsertest.cc
+++ extensions/renderer/script_context_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/script_constants.h"
 #include "extensions/renderer/script_context.h"
 #include "extensions/renderer/script_context_set.h"
+#include "third_party/blink/public/platform/web_runtime_features.h"
 #include "third_party/blink/public/web/web_document.h"
 #include "third_party/blink/public/web/web_local_frame.h"
 #include "url/gurl.h"
@@ -52,7 +53,8 @@ TEST_F(ScriptContextTest, GetEffectiveDocumentURL) {
          <iframe name='frame4' src="data:text/html,<html>Hi</html>"></iframe>
          <iframe name='frame5'
              src="data:text/html,<html>Hi</html>"
-             sandbox=''></iframe>)";
+             sandbox=''></iframe>
+         <iframe disallowdocumentaccess name='frame6'></iframe>)";
 
   const char frame3_html[] =
       R"(<iframe name='frame3_1'></iframe>
@@ -62,6 +64,7 @@ TEST_F(ScriptContextTest, GetEffectiveDocumentURL) {
 
   WebLocalFrame* frame = GetMainFrame();
   ASSERT_TRUE(frame);
+  blink::WebRuntimeFeatures::EnableDisallowDocumentAccess(true);
 
   content::RenderFrame::FromWebFrame(frame)->LoadHTMLString(
       frame_html, top_url, "UTF-8", GURL(), false /* replace_current_item */);
@@ -91,6 +94,9 @@ TEST_F(ScriptContextTest, GetEffectiveDocumentURL) {
   WebLocalFrame* frame5 = frame4->NextSibling()->ToWebLocalFrame();
   ASSERT_TRUE(frame5);
   ASSERT_EQ("frame5", frame5->AssignedName());
+  WebLocalFrame* frame6 = frame5->NextSibling()->ToWebLocalFrame();
+  ASSERT_TRUE(frame6);
+  ASSERT_EQ("frame6", frame6->AssignedName());
 
   // Load a blank document in a frame from a different origin.
   content::RenderFrame::FromWebFrame(frame3)->LoadHTMLString(
@@ -154,6 +160,11 @@ TEST_F(ScriptContextTest, GetEffectiveDocumentURL) {
       GetEffectiveDocumentURLForInjection(
           frame5,
           MatchOriginAsFallbackBehavior::kMatchForAboutSchemeAndClimbTree));
+
+  // Documents with disallowed access (disallowdocumentaccess) behave much like
+  // sandboxed documents.
+  EXPECT_EQ(blank_url, GetEffectiveDocumentURLForContext(frame6));
+  EXPECT_EQ(top_url, GetEffectiveDocumentURLForInjection(frame6));
 
   // top -> different origin = different origin
   EXPECT_EQ(different_url, GetEffectiveDocumentURLForContext(frame3));
--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -590,6 +590,10 @@ void WebRuntimeFeatures::EnableExperimentalProductivityFeatures(bool enable) {
   RuntimeEnabledFeatures::SetExperimentalProductivityFeaturesEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnableDisallowDocumentAccess(bool enable) {
+  RuntimeEnabledFeatures::SetDisallowDocumentAccessEnabled(enable);
+}
+
 void WebRuntimeFeatures::EnableDisplayCutoutAPI(bool enable) {
   RuntimeEnabledFeatures::SetDisplayCutoutAPIEnabled(enable);
 }
