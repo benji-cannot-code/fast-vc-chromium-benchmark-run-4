1248b9bb45e27a2a36fb622b59fed81c91b07481
Evan Stade <estade@chromium.org>
2020-09-18 01:54:53 +0000

    WebLayer: remove IDownloadCallbackClient#createIntent
    It's only called for client versions <83, which are no longer supported.
    Thus the calling code on the impl side can be removed, and the method
    can be removed from the interface and client side.
    
    Bug: 1110811
    Change-Id: Ia8396dab7703e74ee0cf52cbdee3b6b7e5efa0df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414515
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808189}

--- weblayer/public/java/org/chromium/weblayer/Profile.java
+++ weblayer/public/java/org/chromium/weblayer/Profile.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.weblayer;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.RemoteException;
@@ -423,16 +422,6 @@ public class Profile {
         public void downloadFailed(IClientDownload download) {
             StrictModeWorkaround.apply();
             mCallback.onDownloadFailed((Download) download);
-        }
-
-        @Override
-        // Deprecated, implementations past 83 call IWebLayerClient.createIntent instead.
-        public Intent createIntent() {
-            StrictModeWorkaround.apply();
-            // Intent objects need to be created in the client library so they can refer to the
-            // broadcast receiver that will handle them. The broadcast receiver needs to be in the
-            // client library because it's referenced in the manifest.
-            return new Intent(WebLayer.getAppContext(), BroadcastReceiver.class);
         }
     }
 }
