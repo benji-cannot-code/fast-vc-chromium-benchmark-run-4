585173ffbe0b440c49841361a4339c29985e8952
Boris Sazonov <bsazonov@chromium.org>
2020-09-18 22:28:38 +0000

    [Signin][Android] Trigger sign-out when account cookies are cleared
    Adds logic to trigger sign-out if account cookies are cleared while
    there's only an unconsented primary account.
    
    Bug: 1095112
    Change-Id: Iaa65f31ef8da6759cd9afaed2ce9dd6f16e511d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410875
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808561}

--- components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/IdentityManager.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/IdentityManager.java
@@ -119,7 +119,8 @@ public class IdentityManager {
     }
 
     @CalledByNative
-    private void onAccountsCookieDeletedByUserAction() {
+    @VisibleForTesting
+    public void onAccountsCookieDeletedByUserAction() {
         for (Observer observer : mObservers) {
             observer.onAccountsCookieDeletedByUserAction();
         }
