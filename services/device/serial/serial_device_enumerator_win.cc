2a48e7960e440b1ecb0e18c4594922d3ec3a093b
Reilly Grant <reillyg@chromium.org>
2020-09-18 16:03:19 +0000

    serial: Add option to switch enumeration methods
    A potential solution for issue 1119497 is to switch from enumerating
    devices implementing GUID_DEVINTERFACE_COMPORT to using the "bus
    enumerator" identified by GUID_DEVINTERFACE_SERENUM_BUS_ENUMERATOR.
    
    I'm putting this behind a flag so that developers can try it out in
    canary-channel. If it works I can enable it by default and keep the flag
    around for a couple releases in case it causes other problems.
    
    Bug: 1119497
    Change-Id: Icad9008799ed331c3da15e5a70e0184144c47a58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417460
    Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808368}

--- services/device/serial/serial_device_enumerator_win.cc
+++ services/device/serial/serial_device_enumerator_win.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/numerics/ranges.h"
 #include "base/scoped_generic.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/scoped_blocking_call.h"
 #include "base/win/registry.h"
 #include "base/win/scoped_devinfo.h"
+#include "services/device/public/cpp/device_features.h"
 #include "third_party/re2/src/re2/re2.h"
 
 namespace device {
@@ -111,6 +113,13 @@ bool GetProductID(const std::string& instance_id, uint32_t* product_id) {
          base::HexStringToUInt(product_id_str, product_id);
 }
 
+const GUID& GetDeviceInterfaceGuid() {
+  if (base::FeatureList::IsEnabled(features::kUseSerialBusEnumerator))
+    return GUID_DEVINTERFACE_SERENUM_BUS_ENUMERATOR;
+
+  return GUID_DEVINTERFACE_COMPORT;
+}
+
 }  // namespace
 
 class SerialDeviceEnumeratorWin::UiThreadHelper
@@ -132,7 +141,7 @@ class SerialDeviceEnumeratorWin::UiThreadHelper
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     enumerator_ = std::move(enumerator);
     device_observer_.Add(
-        DeviceMonitorWin::GetForDeviceInterface(GUID_DEVINTERFACE_COMPORT));
+        DeviceMonitorWin::GetForDeviceInterface(GetDeviceInterfaceGuid()));
   }
 
   void OnDeviceAdded(const GUID& class_guid,
@@ -240,7 +249,7 @@ void SerialDeviceEnumeratorWin::DoInitialEnumeration() {
                                                 base::BlockingType::MAY_BLOCK);
   // Make a device interface query to find all serial devices.
   base::win::ScopedDevInfo dev_info(
-      SetupDiGetClassDevs(&GUID_DEVINTERFACE_COMPORT, nullptr, 0,
+      SetupDiGetClassDevs(&GetDeviceInterfaceGuid(), nullptr, 0,
                           DIGCF_DEVICEINTERFACE | DIGCF_PRESENT));
   if (!dev_info.is_valid())
     return;
