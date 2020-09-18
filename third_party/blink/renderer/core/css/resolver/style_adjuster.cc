40737e5b009898e104108b20c0ea6f383c9ce4c8
Shimi Zhang <ctzsm@chromium.org>
2020-09-18 19:29:47 +0000

    Cursor Control: Register listener for content editable element
    In this CL, we register listener for content editable element
    including <input> and <textarea> elements.
    
    This is used to notify that the browser side that there will be a
    handler for touchstart event, so it needs to send the event to the
    renderer.
    
    Please refer to the bug for more information about the overall design.
    
    Bug: 1126778
    Change-Id: I35457d5f1ca02c943b5bf594c06a6a70e2c6e3bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401786
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808475}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
+#include "third_party/blink/renderer/core/frame/event_handler_registry.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
@@ -66,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/transforms/transform_operations.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
+#include "ui/base/ui_base_features.h"
 
 namespace blink {
 
@@ -73,6 +75,22 @@ namespace {
 
 bool IsOverflowClipOrVisible(EOverflow overflow) {
   return overflow == EOverflow::kClip || overflow == EOverflow::kVisible;
+}
+
+bool IsEditableElement(Element* element, const ComputedStyle& style) {
+  if (style.UserModify() != EUserModify::kReadOnly)
+    return true;
+
+  if (!element)
+    return false;
+
+  if (auto* textarea = DynamicTo<HTMLTextAreaElement>(*element))
+    return !textarea->IsDisabledOrReadOnly();
+
+  if (auto* input = DynamicTo<HTMLInputElement>(*element))
+    return !input->IsDisabledOrReadOnly() && input->IsTextField();
+
+  return false;
 }
 
 TouchAction AdjustTouchActionForElement(TouchAction touch_action,
@@ -572,6 +590,13 @@ static void AdjustEffectiveTouchAction(ComputedStyle& style,
   TouchAction enforced_by_policy = TouchAction::kNone;
   if (element->GetDocument().IsVerticalScrollEnforced())
     enforced_by_policy = TouchAction::kPanY;
+  if (base::FeatureList::IsEnabled(::features::kSwipeToMoveCursor) &&
+      IsEditableElement(element, style)) {
+    EventHandlerRegistry& registry =
+        element->GetDocument().GetFrame()->GetEventHandlerRegistry();
+    registry.DidAddEventHandler(
+        *element, EventHandlerRegistry::kTouchStartOrMoveEventBlocking);
+  }
 
   // Apply the adjusted parent effective touch actions.
   style.SetEffectiveTouchAction((element_touch_action & inherited_action) |
