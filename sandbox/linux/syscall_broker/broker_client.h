75f62d4047347f777a4379dbcb37876e1170ee14
Matthew Denton <mpdenton@chromium.org>
2020-09-18 20:30:46 +0000

    Linux sandbox: add integration tests for syscall broker
    The syscall broker has unittests in
    //sandbox/linux/syscall_broker/broker_process_unittest.cc, but these
    test direct IPC to the broker, and don't use a BPF policy and so aren't
    integration tests. This CL adds a suite of integration tests that
    mirror the unittests.
    
    This makes it easier to test the user notifier syscall broker, which
    is even more in need of integration tests since the kernel itself
    sends the syscall arguments to the broker, rather than a custom signal
    handler.
    
    The integration tests use a new macro BROKER_TEST, built on top of
    the existing BPF_TEST and SANDBOX_TEST infrastructure. It runs each
    test with each combination of |fast_check_in_client_| = (true, false),
    and method of making a syscall (direct IPC, direct syscall,
    libc lib functions).
    
    Bug: 1117351
    Change-Id: If1c050f55722487fea51cb479558916983c44985
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2360802
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808515}

--- sandbox/linux/syscall_broker/broker_client.h
+++ sandbox/linux/syscall_broker/broker_client.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "sandbox/linux/syscall_broker/broker_channel.h"
 #include "sandbox/linux/syscall_broker/broker_command.h"
+#include "sandbox/sandbox_export.h"
 
 namespace sandbox {
 namespace syscall_broker {
@@ -26,7 +27,7 @@ class BrokerPermissionList;
 // thread-safe and async-signal safe way. The goal is to be able to use it to
 // replace the open() or access() system calls happening anywhere in a process
 // (as allowed for instance by seccomp-bpf's SIGSYS mechanism).
-class BrokerClient {
+class SANDBOX_EXPORT BrokerClient {
  public:
   // |policy| needs to match the policy used by BrokerHost. This
   // allows to predict some of the requests which will be denied
