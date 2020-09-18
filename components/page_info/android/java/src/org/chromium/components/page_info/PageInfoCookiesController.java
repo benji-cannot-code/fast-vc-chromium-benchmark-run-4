0139926ba12e56c20d74b6bdea2b4e532da6efb4
Christian Dullweber <dullweber@chromium.org>
2020-09-18 15:34:20 +0000

    Animate PageInfo subpage transition
    Create "fade-through" animation to switch between main page and subpage.
    Remove animation for page-info rows.
    
    Video: https://crbug.com/1077766#c42
    Bug: 1077766
    Change-Id: I330b57b79a0e6dbd421b1d10ac897f16246f2f6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2418373
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808358}

--- components/page_info/android/java/src/org/chromium/components/page_info/PageInfoCookiesController.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoCookiesController.java
@@ -76,7 +76,7 @@ public class PageInfoCookiesController
         mSubPage.setParams(params);
 
         AppCompatActivity host = (AppCompatActivity) mRowView.getContext();
-        host.getSupportFragmentManager().beginTransaction().add(mSubPage, "FOO").commitNow();
+        host.getSupportFragmentManager().beginTransaction().add(mSubPage, null).commitNow();
         return mSubPage.requireView();
     }
 
@@ -100,6 +100,7 @@ public class PageInfoCookiesController
 
     @Override
     public void onSubpageRemoved() {
+        assert mSubPage != null;
         AppCompatActivity host = (AppCompatActivity) mRowView.getContext();
         host.getSupportFragmentManager().beginTransaction().remove(mSubPage).commitNow();
         mSubPage = null;
