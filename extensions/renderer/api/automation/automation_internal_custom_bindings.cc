ae87eb17d4e6da36ecb4000c937bb31bac42dab3
Dominic Mazzoni <dmazzoni@chromium.org>
2020-09-18 01:58:23 +0000

    Simplify parsing code in ax_enum_util
    Replace the hand-written code to parse each enum from ax_enums with a
    templatized function that builds a map from string to attribute value
    on-demand.
    
    This saves a little bit of manual effort each time someone updates an enum.
    
    Note that these parsing functions are only used by the automation API.
    
    Bug: None
    Change-Id: I4be5e192041cf0686175a20e68782184295a1f9c
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2404164
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808190}

--- extensions/renderer/api/automation/automation_internal_custom_bindings.cc
+++ extensions/renderer/api/automation/automation_internal_custom_bindings.cc
@@ -484,8 +484,8 @@ class NodeIDPlusEventWrapper
     ui::AXTreeID tree_id =
         ui::AXTreeID::FromString(*v8::String::Utf8Value(isolate, args[0]));
     int node_id = args[1].As<v8::Int32>()->Value();
-    ax::mojom::Event event_type =
-        ui::ParseEvent(*v8::String::Utf8Value(isolate, args[2]));
+    auto event_type = ui::ParseAXEnum<ax::mojom::Event>(
+        *v8::String::Utf8Value(isolate, args[2]));
 
     AutomationAXTreeWrapper* tree_wrapper =
         automation_bindings_->GetAutomationAXTreeWrapperFromTreeID(tree_id);
@@ -1000,8 +1000,8 @@ void AutomationInternalCustomBindings::AddRoutes() {
       [](v8::Isolate* isolate, v8::ReturnValue<v8::Value> result,
          ui::AXTree* tree, ui::AXNode* node,
          const std::string& attribute_name) {
-        ax::mojom::StringAttribute attribute =
-            ui::ParseStringAttribute(attribute_name.c_str());
+        auto attribute =
+            ui::ParseAXEnum<ax::mojom::StringAttribute>(attribute_name.c_str());
         const char* attr_value;
         if (attribute == ax::mojom::StringAttribute::kFontFamily ||
             attribute == ax::mojom::StringAttribute::kLanguage) {
@@ -1020,8 +1020,8 @@ void AutomationInternalCustomBindings::AddRoutes() {
       [](v8::Isolate* isolate, v8::ReturnValue<v8::Value> result,
          ui::AXTree* tree, ui::AXNode* node,
          const std::string& attribute_name) {
-        ax::mojom::BoolAttribute attribute =
-            ui::ParseBoolAttribute(attribute_name.c_str());
+        auto attribute =
+            ui::ParseAXEnum<ax::mojom::BoolAttribute>(attribute_name.c_str());
         bool attr_value;
         if (!node->data().GetBoolAttribute(attribute, &attr_value))
           return;
@@ -1033,8 +1033,8 @@ void AutomationInternalCustomBindings::AddRoutes() {
       [](v8::Isolate* isolate, v8::ReturnValue<v8::Value> result,
          ui::AXTree* tree, ui::AXNode* node,
          const std::string& attribute_name) {
-        ax::mojom::IntAttribute attribute =
-            ui::ParseIntAttribute(attribute_name.c_str());
+        auto attribute =
+            ui::ParseAXEnum<ax::mojom::IntAttribute>(attribute_name.c_str());
         int attr_value;
 
         if (attribute == ax::mojom::IntAttribute::kPosInSet &&
@@ -1054,8 +1054,8 @@ void AutomationInternalCustomBindings::AddRoutes() {
       [](v8::Isolate* isolate, v8::ReturnValue<v8::Value> result,
          ui::AXTree* tree, ui::AXNode* node,
          const std::string& attribute_name) {
-        ax::mojom::IntAttribute attribute =
-            ui::ParseIntAttribute(attribute_name.c_str());
+        auto attribute =
+            ui::ParseAXEnum<ax::mojom::IntAttribute>(attribute_name.c_str());
         std::set<int32_t> ids =
             tree->GetReverseRelations(attribute, node->id());
         v8::Local<v8::Context> context = isolate->GetCurrentContext();
@@ -1074,8 +1074,8 @@ void AutomationInternalCustomBindings::AddRoutes() {
       [](v8::Isolate* isolate, v8::ReturnValue<v8::Value> result,
          ui::AXTree* tree, ui::AXNode* node,
          const std::string& attribute_name) {
-        ax::mojom::FloatAttribute attribute =
-            ui::ParseFloatAttribute(attribute_name.c_str());
+        auto attribute =
+            ui::ParseAXEnum<ax::mojom::FloatAttribute>(attribute_name.c_str());
         float attr_value;
 
         if (!node->data().GetFloatAttribute(attribute, &attr_value))
@@ -1092,8 +1092,8 @@ void AutomationInternalCustomBindings::AddRoutes() {
       [](v8::Isolate* isolate, v8::ReturnValue<v8::Value> result,
          ui::AXTree* tree, ui::AXNode* node,
          const std::string& attribute_name) {
-        ax::mojom::IntListAttribute attribute =
-            ui::ParseIntListAttribute(attribute_name.c_str());
+        auto attribute = ui::ParseAXEnum<ax::mojom::IntListAttribute>(
+            attribute_name.c_str());
         if (!node->data().HasIntListAttribute(attribute))
           return;
         const std::vector<int32_t>& attr_value =
@@ -1114,8 +1114,8 @@ void AutomationInternalCustomBindings::AddRoutes() {
       [](v8::Isolate* isolate, v8::ReturnValue<v8::Value> result,
          ui::AXTree* tree, ui::AXNode* node,
          const std::string& attribute_name) {
-        ax::mojom::IntListAttribute attribute =
-            ui::ParseIntListAttribute(attribute_name.c_str());
+        auto attribute = ui::ParseAXEnum<ax::mojom::IntListAttribute>(
+            attribute_name.c_str());
         std::set<int32_t> ids =
             tree->GetReverseRelations(attribute, node->id());
         v8::Local<v8::Context> context = isolate->GetCurrentContext();
@@ -1279,8 +1279,8 @@ void AutomationInternalCustomBindings::AddRoutes() {
       [](v8::Isolate* isolate, v8::ReturnValue<v8::Value> result,
          ui::AXTree* tree, ui::AXNode* node,
          const std::string& attribute_name) {
-        ax::mojom::StringAttribute attr =
-            ui::ParseStringAttribute(attribute_name.c_str());
+        auto attr =
+            ui::ParseAXEnum<ax::mojom::StringAttribute>(attribute_name.c_str());
         if (attr == ax::mojom::StringAttribute::kNone) {
           // Set result as empty array.
           result.Set(v8::Array::New(isolate, 0));
@@ -1384,7 +1384,7 @@ void AutomationInternalCustomBindings::AddRoutes() {
         // spin button.
         const std::string& role_string =
             node->GetStringAttribute(ax::mojom::StringAttribute::kRole);
-        ax::mojom::Role role = ui::ParseRole(role_string.c_str());
+        auto role = ui::ParseAXEnum<ax::mojom::Role>(role_string.c_str());
         if (role == ax::mojom::Role::kSlider ||
             role == ax::mojom::Role::kSpinButton) {
           standard_actions.push_back(
