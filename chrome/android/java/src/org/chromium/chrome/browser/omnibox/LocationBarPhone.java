93ab147d89f7804a91735193d9a1a339d9ef87a6
bttk <bttk@chromium.org>
2020-09-18 22:06:35 +0000

    Remove uses of StatusView from LocationBarPhone
    Bug: 1109369
    Change-Id: If22a2762311ecdce441fa695a19c528173b35214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2332361
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Auto-Submit: who/bttk <bttk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808554}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarPhone.java
@@ -18,7 +18,6 @@ import org.chromium.base.TraceEvent;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior;
 import org.chromium.chrome.browser.ntp.NewTabPage;
-import org.chromium.chrome.browser.omnibox.status.StatusView;
 import org.chromium.ui.interpolators.BakedBezierInterpolator;
 
 import java.util.List;
@@ -31,7 +30,7 @@ public class LocationBarPhone extends LocationBarLayout {
 
     private View mFirstVisibleFocusedView;
     private View mUrlBar;
-    private StatusView mStatusView;
+    private View mStatusView;
 
     /**
      * Constructor used to inflate from XML.
@@ -86,10 +85,6 @@ public class LocationBarPhone extends LocationBarLayout {
         // This branch will be hit if the search engine logo experiment is enabled and we should
         // show the logo.
         if (shouldShowSearchEngineLogo) {
-            mStatusView.updateSearchEngineStatusIcon(
-                    shouldShowSearchEngineLogo, isSearchEngineGoogle, searchEngineUrl);
-            mFirstVisibleFocusedView = mStatusView;
-
             // When the search engine icon is enabled, icons are translations into the parent view's
             // padding area. Set clip padding to false to prevent them from getting clipped.
             setClipToPadding(false);
@@ -141,7 +136,7 @@ public class LocationBarPhone extends LocationBarLayout {
      * @return The offset for the location bar when showing the dse icon.
      */
     public int getLocationBarOffsetForFocusAnimation(boolean hasFocus) {
-        if (mStatusView == null) return 0;
+        if (mStatusCoordinator == null) return 0;
 
         // No offset is required if the experiment is disabled.
         if (!SearchEngineLogoUtils.shouldShowSearchEngineLogo(
@@ -176,7 +171,7 @@ public class LocationBarPhone extends LocationBarLayout {
     public float getUrlBarTranslationXForToolbarAnimation(
             float urlExpansionPercent, boolean hasFocus) {
         // This will be called before status view is ready.
-        if (mStatusView == null || mStatusCoordinator == null) return 0;
+        if (mStatusCoordinator == null) return 0;
 
         // No offset is required if the experiment is disabled.
         if (!SearchEngineLogoUtils.shouldShowSearchEngineLogo(
@@ -192,7 +187,7 @@ public class LocationBarPhone extends LocationBarLayout {
         float translation =
                 urlExpansionPercent * mStatusCoordinator.getEndPaddingPixelSizeOnFocusDelta();
 
-        if (!hasFocus && mStatusView.isSearchEngineStatusIconVisible()
+        if (!hasFocus && mStatusCoordinator.isSearchEngineStatusIconVisible()
                 && SearchEngineLogoUtils.currentlyOnNTP(mToolbarDataProvider)) {
             // When:
             // 1. unfocusing the LocationBar on the NTP.
@@ -296,6 +291,7 @@ public class LocationBarPhone extends LocationBarLayout {
     @Override
     public void setShowIconsWhenUrlFocused(boolean showIcon) {
         super.setShowIconsWhenUrlFocused(showIcon);
+        mFirstVisibleFocusedView = showIcon ? mStatusView : mUrlBar;
         mStatusCoordinator.setShowIconsWhenUrlFocused(showIcon);
     }
 
@@ -308,11 +304,7 @@ public class LocationBarPhone extends LocationBarLayout {
     public void updateVisualsForState() {
         super.updateVisualsForState();
         boolean isIncognito = getToolbarDataProvider().isIncognito();
-        boolean shouldShowSearchEngineLogo =
-                SearchEngineLogoUtils.shouldShowSearchEngineLogo(isIncognito);
-        setShowIconsWhenUrlFocused(shouldShowSearchEngineLogo);
-        mFirstVisibleFocusedView = shouldShowSearchEngineLogo ? mStatusView : mUrlBar;
-
+        setShowIconsWhenUrlFocused(SearchEngineLogoUtils.shouldShowSearchEngineLogo(isIncognito));
         updateStatusVisibility();
     }
 
