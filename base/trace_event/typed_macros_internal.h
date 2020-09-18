8365ad70a5cc980b8942f767dbd2319fcc0abb53
Sami Kyostila <skyostil@chromium.org>
2020-09-18 19:34:40 +0000

    trace_event: Add support for interned data in typed events
    This patch adds support for interned data in typed trace points. This
    helps to reduce trace size for frequently repeated data.
    
    As an example, an interning index for base::Location could be defined as
    follows:
    
    struct InternedLocationIndex
        : public perfetto::TrackEventInternedDataIndex<
              InternedLocationIndex,
              perfetto::protos::pbzero::
                  InternedData::kSourceLocationsFieldNumber,
              Location> {
      static void Add(perfetto::protos::pbzero::InternedData* interned_data,
                      size_t iid,
                      const Location& location) {
        auto* loc = interned_data->add_source_locations();
        loc->set_file_name(location.file_name());
        loc->set_function_name(location.function_name());
        loc->set_line_number(location.line_number());
      }
    };
    
    A corresponding typed trace point using the index could look like this:
    
      TRACE_EVENT("cat", "Name", [](perfetto::EventContext ctx) {
        auto* log = ctx.event()->set_log_message();
        size_t iid = InternedLocationIndex::Get(&ctx, FROM_HERE);
        log->set_body_iid(iid);
      }
    
    Bug: b/156727531
    Change-Id: I5a78784be688071f7373ac1655b1ce74428f7b0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410117
    Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808484}

--- base/trace_event/typed_macros_internal.h
+++ base/trace_event/typed_macros_internal.h
@@ -114,7 +114,8 @@ static inline void AddTraceEvent(char phase,
   if (track)
     track_event->set_track_uuid(track.uuid);
 
-  argument_func(perfetto::EventContext(track_event.get()));
+  argument_func(perfetto::EventContext(track_event.get(),
+                                       track_event.incremental_state()));
 }
 
 template <
--- services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
+++ services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/tracing/public/mojom/perfetto_service.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/perfetto/include/perfetto/tracing/track.h"
+#include "third_party/perfetto/include/perfetto/tracing/track_event_interned_data_index.h"
 #include "third_party/perfetto/protos/perfetto/trace/clock_snapshot.pb.h"
 #include "third_party/perfetto/protos/perfetto/trace/trace_packet.pb.h"
 #include "third_party/perfetto/protos/perfetto/trace/trace_packet.pbzero.h"
@@ -1989,6 +1990,46 @@ TEST_F(TraceEventDataSourceTest, UserActionEvent) {
   ASSERT_TRUE(e_packet->track_event().has_chrome_user_event());
   EXPECT_EQ(e_packet->track_event().chrome_user_event().action_hash(),
             base::HashMetricName("Test_Action"));
+}
+
+namespace {
+
+struct InternedLogMessageBody
+    : public perfetto::TrackEventInternedDataIndex<
+          InternedLogMessageBody,
+          perfetto::protos::pbzero::InternedData::kLogMessageBodyFieldNumber,
+          std::string> {
+  static void Add(perfetto::protos::pbzero::InternedData* interned_data,
+                  size_t iid,
+                  const std::string& body) {
+    auto* msg = interned_data->add_log_message_body();
+    msg->set_iid(iid);
+    msg->set_body(body);
+  }
+};
+
+}  // namespace
+
+TEST_F(TraceEventDataSourceTest, TypedEventInterning) {
+  CreateTraceEventDataSource();
+
+  {
+    TRACE_EVENT("browser", "bar", [&](perfetto::EventContext ctx) {
+      size_t iid = InternedLogMessageBody::Get(&ctx, "Hello interned world!");
+      ctx.event()->set_log_message()->set_body_iid(iid);
+    });
+  }
+  size_t packet_index = ExpectStandardPreamble();
+  auto* e_packet = producer_client()->GetFinalizedPacket(packet_index++);
+
+  ExpectEventCategories(e_packet, {{1u, "browser"}});
+  ExpectEventNames(e_packet, {{1u, "bar"}});
+  ASSERT_TRUE(e_packet->track_event().has_log_message());
+  ASSERT_TRUE(e_packet->has_interned_data());
+  EXPECT_EQ(e_packet->track_event().log_message().body_iid(),
+            e_packet->interned_data().log_message_body()[0].iid());
+  ASSERT_EQ("Hello interned world!",
+            e_packet->interned_data().log_message_body()[0].body());
 }
 
 // TODO(eseckler): Add startup tracing unittests.
