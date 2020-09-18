c0602c268660b372c3b070ba51ace0bb17e78110
Gavin Williams <gavinwill@chromium.org>
2020-09-18 00:24:14 +0000

    Move printer status request inside the Destination object
    This change moves the call to NativeLayer.requestPrinterStatusUpdate to
    inside the Destination object. Shifting responsibility of requesting
    statuses to Destination helps centralize the managing of the request
    promises as printer status will be used by multiple UIs in Print
    Preview.
    
    |Destination.printerStatusReason_| changes to null/nullable to make it
    clear when a destination has never received a status. And since
    |Destination.printerStatusReason_| will only be populated after a
    successful printer status request, the tracker |statusRequestedMap_| is
    no longer needed.
    
    Bug: 1123754
    Change-Id: I0ade10e7183179e995dda593f3f4f898e708bcfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414771
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808146}

--- chrome/browser/resources/print_preview/native_layer.js
+++ chrome/browser/resources/print_preview/native_layer.js
@@ -284,7 +284,7 @@ export class NativeLayer {
   /**
    * Records the histogram to capture the printer status of the current
    * destination and whether the user chose to print or cancel.
-   * @param {!PrinterStatusReason} statusReason Current destination printer
+   * @param {?PrinterStatusReason} statusReason Current destination printer
    * status
    * @param {boolean} didUserAttemptPrint True if user printed, false if user
    * canceled.
@@ -401,6 +401,10 @@ export class NativeLayerImpl {
 
   /** @override */
   recordPrinterStatusHistogram(statusReason, didUserAttemptPrint) {
+    if (!statusReason) {
+      return;
+    }
+
     let histogram;
     switch (statusReason) {
       case (PrinterStatusReason.NO_ERROR):
