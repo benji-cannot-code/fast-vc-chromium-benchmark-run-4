91382ccd579ba6b81ad9417c53a5f4b46d74462f
Michael Bai <michaelbai@chromium.org>
2020-09-18 23:32:23 +0000

    WebView: Persist unsent log.
    Test manually, the persisted log loaded at next startup.
    
    Bug: 1076549
    Change-Id: Ibfc273431b8ccd313435b55bdb1889b7c5913045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173960
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Michael Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808588}

--- android_webview/browser/aw_feature_list_creator.cc
+++ android_webview/browser/aw_feature_list_creator.cc
@@ -75,6 +75,9 @@ const char* const kPersistentPrefsAllowlist[] = {
     metrics::prefs::kStabilityRendererHangCount,
     metrics::prefs::kStabilityRendererLaunchCount,
     metrics::prefs::kUninstallMetricsPageLoadCount,
+    // Unsent logs.
+    metrics::prefs::kMetricsInitialLogs,
+    metrics::prefs::kMetricsOngoingLogs,
     // Unsent logs metadata.
     metrics::prefs::kMetricsInitialLogsMetadata,
     metrics::prefs::kMetricsOngoingLogsMetadata,
