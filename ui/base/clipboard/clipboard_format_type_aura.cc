bbd585f86ff0cd534e0202e47274c2c34691a11b
Darwin Huang <huangdarwin@chromium.org>
2020-09-18 21:22:09 +0000

    Clipboard: Restrict Windows-only formats to Windows builds.
    On the clipboard, MozUrl and Filename ClipboardFormatTypes are only
    really used on Windows. Restricting this allows us to delete some code,
    and make it more clear where these formats are (and are not) used.
    
    Change-Id: Iead2dcac183147b3b6285f9a546e3a7e3f9d94a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2413396
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Auto-Submit: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808534}

--- ui/base/clipboard/clipboard_format_type_aura.cc
+++ ui/base/clipboard/clipboard_format_type_aura.cc
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-namespace {
-constexpr char kMimeTypeFilename[] = "chromium/filename";
-}
-
 // TODO(huangdarwin): Investigate creating a new clipboard_format_type_x11 as a
 // wrapper around an X11 ::Atom. This wasn't possible in the past, because unit
 // tests spawned a new X11 server for each test, meaning Atom numeric values
@@ -61,20 +57,8 @@ const ClipboardFormatType& ClipboardFormatType::GetUrlType() {
 }
 
 // static
-const ClipboardFormatType& ClipboardFormatType::GetMozUrlType() {
-  static base::NoDestructor<ClipboardFormatType> type(kMimeTypeMozillaURL);
-  return *type;
-}
-
-// static
 const ClipboardFormatType& ClipboardFormatType::GetPlainTextType() {
   static base::NoDestructor<ClipboardFormatType> type(kMimeTypeText);
-  return *type;
-}
-
-// static
-const ClipboardFormatType& ClipboardFormatType::GetFilenameType() {
-  static base::NoDestructor<ClipboardFormatType> type(kMimeTypeFilename);
   return *type;
 }
 
