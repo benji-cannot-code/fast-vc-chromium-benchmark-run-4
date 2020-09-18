b318b87a2961ba18385dbfeb0c850f55eb1c148f
Alexandre Courbot <acourbot@chromium.org>
2020-09-18 09:14:20 +0000

    media/gpu/v4l2vea: make clang-tidy happy
    Fix a few warnings reported by clang-tidy.
    
    BUG=None
    TEST=Chrome builds.
    
    Change-Id: Id3b7249365d8a9c68fcb59e676c35036a219c21d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2413944
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Auto-Submit: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808279}

--- media/gpu/v4l2/v4l2_video_encode_accelerator.cc
+++ media/gpu/v4l2/v4l2_video_encode_accelerator.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 
+#include <memory>
 #include <numeric>
 #include <utility>
 
@@ -162,7 +163,7 @@ V4L2VideoEncodeAccelerator::InputFrameInfo::InputFrameInfo(
 V4L2VideoEncodeAccelerator::InputFrameInfo::InputFrameInfo(
     const InputFrameInfo&) = default;
 
-V4L2VideoEncodeAccelerator::InputFrameInfo::~InputFrameInfo() {}
+V4L2VideoEncodeAccelerator::InputFrameInfo::~InputFrameInfo() = default;
 
 V4L2VideoEncodeAccelerator::V4L2VideoEncodeAccelerator(
     scoped_refptr<V4L2Device> device)
@@ -213,7 +214,7 @@ bool V4L2VideoEncodeAccelerator::Initialize(const Config& config,
 
   encoder_input_visible_rect_ = gfx::Rect(config.input_visible_size);
 
-  client_ptr_factory_.reset(new base::WeakPtrFactory<Client>(client));
+  client_ptr_factory_ = std::make_unique<base::WeakPtrFactory<Client>>(client);
   client_ = client_ptr_factory_->GetWeakPtr();
 
   output_format_fourcc_ =
@@ -1372,8 +1373,7 @@ bool V4L2VideoEncodeAccelerator::StopDevicePoll() {
   // Reset all our accounting info.
   while (!encoder_input_queue_.empty())
     encoder_input_queue_.pop();
-  for (size_t i = 0; i < input_buffer_map_.size(); ++i) {
-    InputRecord& input_record = input_buffer_map_[i];
+  for (auto& input_record : input_buffer_map_) {
     input_record.frame = nullptr;
   }
 
