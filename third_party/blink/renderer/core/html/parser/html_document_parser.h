779503630d70b01f060ca8733a2f6c45ab6af4f9
Mason Freed <masonfreed@chromium.org>
2020-09-18 19:34:18 +0000

    Allow Finch control of tokenization budget
    The synchronous parser previously had a fixed token budget. This CL
    enables Finch experimentation on the number of tokens for that budget.
    If no Finch is enabled, the budget stays the same, 500.
    
    Bug: 1128608
    Change-Id: I5a0d21833a3b254188dce86fc019a494ce3e3dd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412827
    Reviewed-by: Richard Townsend <richard.townsend@arm.com>
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808483}

--- third_party/blink/renderer/core/html/parser/html_document_parser.h
+++ third_party/blink/renderer/core/html/parser/html_document_parser.h
@@ -141,6 +141,10 @@ class CORE_EXPORT HTMLDocumentParser : public ScriptableDocumentParser,
   void Flush() final;
   void SetDecoder(std::unique_ptr<TextResourceDecoder>) final;
 
+  void SetMaxTokenizationBudgetForTesting(int budget) {
+    max_tokenization_budget_ = budget;
+  }
+
  protected:
   void insert(const String&) final;
   void Append(const String&) override;
@@ -279,6 +283,7 @@ class CORE_EXPORT HTMLDocumentParser : public ScriptableDocumentParser,
   // would require keeping track of token positions of preload requests.
   CompactHTMLToken* pending_csp_meta_token_;
 
+  int max_tokenization_budget_;
   bool can_parse_asynchronously_;
   bool end_was_delayed_;
   bool have_background_parser_;
