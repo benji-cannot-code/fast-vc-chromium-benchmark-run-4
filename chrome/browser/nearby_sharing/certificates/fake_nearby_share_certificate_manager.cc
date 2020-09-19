d1a8e94a42594ba970d6abb38091f9b6a2de4e91
Josh Nohle <nohle@google.com>
2020-09-19 02:27:40 +0000

    [Nearby] Add private cert crypto methods to the cert manager interface
    The private certificate needs to track the salts it uses to encrypt the
    metadata key. We need to ensure that storage is updated when
    EncryptMetadataKey() is called.
    
    We perform all private certificate operations via certificate manager
    methods, never returning a private certificate to the user. The
    certificate manager is responsible for internally updating its storage
    when necessary.
    
    Fixed: b/166473931, 1121443
    Change-Id: I90fd432eed32bb9d0a1437a94c8615d1c816f0c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417550
    Reviewed-by: James Vecore <vecore@google.com>
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808648}

--- chrome/browser/nearby_sharing/certificates/fake_nearby_share_certificate_manager.cc
+++ chrome/browser/nearby_sharing/certificates/fake_nearby_share_certificate_manager.cc
@@ -51,13 +51,6 @@ FakeNearbyShareCertificateManager::FakeNearbyShareCertificateManager() =
 FakeNearbyShareCertificateManager::~FakeNearbyShareCertificateManager() =
     default;
 
-NearbySharePrivateCertificate
-FakeNearbyShareCertificateManager::GetValidPrivateCertificate(
-    nearby_share::mojom::Visibility visibility) {
-  ++num_get_valid_private_certificate_calls_;
-  return GetNearbyShareTestPrivateCertificate(visibility);
-}
-
 std::vector<nearbyshare::proto::PublicCertificate>
 FakeNearbyShareCertificateManager::GetPrivateCertificatesAsPublicCertificates(
     nearby_share::mojom::Visibility visibility) {
@@ -79,3 +72,12 @@ void FakeNearbyShareCertificateManager::DownloadPublicCertificates() {
 void FakeNearbyShareCertificateManager::OnStart() {}
 
 void FakeNearbyShareCertificateManager::OnStop() {}
+
+base::Optional<NearbySharePrivateCertificate>
+FakeNearbyShareCertificateManager::GetValidPrivateCertificate(
+    nearby_share::mojom::Visibility visibility) const {
+  return GetNearbyShareTestPrivateCertificate(visibility);
+}
+
+void FakeNearbyShareCertificateManager::UpdatePrivateCertificateInStorage(
+    const NearbySharePrivateCertificate& private_certificate) {}
