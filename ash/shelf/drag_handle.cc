6a3e6553a82f640a458fb22a87f5c1822472e00e
minch <minch@chromium.org>
2020-09-19 05:44:03 +0000

    dark_mode: Add kShelfHandle.
    Bug: 1129526
    Change-Id: I47be5d06e9ce30157c2a737789186f9c157d9602
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416933
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808668}

--- ash/shelf/drag_handle.cc
+++ ash/shelf/drag_handle.cc
@@ -171,11 +171,6 @@ void DragHandle::ScheduleShowDragHandleNudge() {
                      base::Unretained(this)));
 }
 
-void DragHandle::SetColorAndOpacity(SkColor color, float opacity) {
-  layer()->SetColor(color);
-  layer()->SetOpacity(opacity);
-}
-
 void DragHandle::HideDragHandleNudge(
     contextual_tooltip::DismissNudgeReason reason) {
   StopDragHandleNudgeShowTimer();
@@ -222,6 +217,11 @@ void DragHandle::SetWindowDragFromShelfInProgress(bool gesture_in_progress) {
     HideDragHandleNudge(
         contextual_tooltip::DismissNudgeReason::kPerformedGesture);
   }
+}
+
+void DragHandle::UpdateColor() {
+  layer()->SetColor(AshColorProvider::Get()->GetContentLayerColor(
+      AshColorProvider::ContentLayerType::kShelfHandleColor));
 }
 
 void DragHandle::OnGestureEvent(ui::GestureEvent* event) {
