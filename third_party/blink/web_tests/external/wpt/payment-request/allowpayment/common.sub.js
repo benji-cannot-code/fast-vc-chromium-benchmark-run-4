b81af5b4dea4d27c09f74eafba48c9c298c1ebc3
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-09-19 14:40:20 +0000

    Import wpt@aa0d5711933473ebd2c10d169edb259f08d1136b
    Using wpt-import in Chromium 818f45b67730e3ea56ea3946f27eeda2f53c168d.
    With Chromium commits locally applied on WPT:
    b943d006a3 "[COOP] Access reporting with 2 reporters."
    82ad19e161 "sensors: Cleanup iframe-related tests."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=robertma@google.com
    
    No-Export: true
    Change-Id: Ied0340ee7a92168f6b7acbc03adade7af7fe7572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2420000
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#808689}

--- third_party/blink/web_tests/external/wpt/payment-request/allowpayment/common.sub.js
+++ third_party/blink/web_tests/external/wpt/payment-request/allowpayment/common.sub.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Test should set these:
 // const expectSuccess = {'iframe': bool, 'frame': bool, 'object': bool, 'embed': bool};
-// const setAllowPaymentRequest = bool;
+// const setAllowPayment = bool;
 // const testCrossOrigin = bool;
 
 const tests = {};
@@ -33,8 +33,8 @@ window.onmessage = e => {
 ["iframe", "frame", "object", "embed"].forEach((tagName, i) => {
   tests[tagName] = async_test(t => {
     const elm = document.createElement(tagName);
-    if (setAllowPaymentRequest) {
-      elm.setAttribute("allowpaymentrequest", "");
+    if (setAllowPayment) {
+      elm.setAttribute("allow", "payment");
     }
     const path = location.pathname.substring(
       0,
