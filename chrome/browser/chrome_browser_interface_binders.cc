d0a16d30b9dc6cdd398ca243d9d72c7281cbce19
Xiaohan Wang <xhwang@chromium.org>
2020-09-19 00:05:11 +0000

    mac: Update IDS_MAC_SYSTEM_INFOBAR_TEXT_ARM string
    Update the string per design doc (see Bug).
    
    Also check ShouldShow() which was missed in the last CL due to
    rebase error.
    
    See screenshot at: https://photos.app.goo.gl/Ho4rr76RK4nnckJe7
    
    Tbr: dcheng@chromium.org
    Bug: 1126568
    Change-Id: I775715eba04f3ff243e3db8b73bba04a42771a33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2419578
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808598}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -445,8 +445,10 @@ class CdmInfobarServiceImpl final
     if (!web_contents)
       return;
 
-    MacSystemInfoBarDelegate::Create(
-        InfoBarService::FromWebContents(web_contents));
+    if (MacSystemInfoBarDelegate::ShouldShow()) {
+      MacSystemInfoBarDelegate::Create(
+          InfoBarService::FromWebContents(web_contents));
+    }
   }
 };
 
