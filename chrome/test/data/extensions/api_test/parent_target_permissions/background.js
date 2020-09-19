1b7196939e3a25be7f16dc6260902e38e5243ca7
Andrey Kosyakov <caseq@chromium.org>
2020-09-19 06:02:05 +0000

    chrome.debugger extentsions: check permissions to attach to parent targets
    ... when client attempts to attach to a subframe.
    
    Bug: 1116444
    Change-Id: I74bd06cf9b91482a35c035d2e4d064b8ac66adf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412799
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808669}

---
+++ chrome/test/data/extensions/api_test/parent_target_permissions/background.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const protocolVersion = '1.3';
+
+chrome.test.getConfig(config => chrome.test.runTests([
+  async function testParentTargetPermissions() {
+    const {openTab} = await import('/_test_resources/test_util/tabs_util.js');
+    const fileFrameURL =
+        config.testDataDirectory + '/parent_target_permissions/top_page.html';
+    const subframeURL = chrome.runtime.getURL('subframe.html');
+    openTab(fileFrameURL + '?' + subframeURL);
+    await new Promise(resolve => {
+      chrome.runtime.onMessage.addListener(message => {
+        if (message === 'ready')
+          resolve();
+      });
+    });
+    const targets = await new Promise(resolve =>
+        chrome.debugger.getTargets(resolve));
+    const subframeTarget =
+        targets.find(t => t.type === 'other' && t.url === subframeURL);
+    const debuggee = {targetId: subframeTarget.id};
+    await new Promise(resolve =>
+        chrome.debugger.attach(debuggee, protocolVersion, resolve));
+    chrome.test.assertLastError('Cannot attach to this target.');
+    chrome.test.succeed();
+  }
+]));
