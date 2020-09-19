d25a59dd60d3f77dbf77ffd3b6fca661b2136fb6
Zentaro Kavanagh <zentaro@chromium.org>
2020-09-19 19:30:19 +0000

    Diagnostics: Add skeleton battery health card
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I3c7a0e6b691b80a481e7556ad0a1d06646ed8f5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2419618
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808706}

--- chromeos/components/diagnostics_ui/resources/battery_health_card.js
+++ chromeos/components/diagnostics_ui/resources/battery_health_card.js
@@ -3,26 +3,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './battery_health_card.js';
-import './cpu_card.js';
+import './diagnostics_card.js';
 import './diagnostics_shared_css.js';
-import './memory_card.js';
-import './overview_card.js';
 
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {SystemDataProviderInterface} from './diagnostics_types.js'
+import {getSystemDataProvider} from './mojo_interface_provider.js';
 
 /**
  * @fileoverview
- * 'diagnostics-app' is the main page for viewing telemetric system information
- * and running diagnostic tests.
+ * 'battery-health-card' shows information about battery health.
  */
 Polymer({
-  is: 'diagnostics-app',
+  is: 'battery-health-card',
 
   _template: html`{__html_template__}`,
 
+  /**
+   * @private {?SystemDataProviderInterface}
+   */
+  systemDataProvider_: null,
+
   /** @override */
-  ready() {
+  created() {
+    this.systemDataProvider_ = getSystemDataProvider();
   },
 
 });
