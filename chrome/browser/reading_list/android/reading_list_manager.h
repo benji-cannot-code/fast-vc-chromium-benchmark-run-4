745f7a9fedbac53c6bcf9d26cef19e6d7ef59cb7
Xing Liu <xingliu@chromium.org>
2020-09-20 00:11:57 +0000

    Read later: Add ReadingListManager keyed service boilerplates.
    This CL adds the keyed service and service factory for reading list
    backend.
    
    Currently only a reading list model pointer is passed in. Implementation
    details will be added later.
    
    Bug: 1128074
    Change-Id: I88a06e22273d41c3951d1a7edad297538032e7f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417267
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808720}

---
+++ chrome/browser/reading_list/android/reading_list_manager.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_READING_LIST_ANDROID_READING_LIST_MANAGER_H_
+#define CHROME_BROWSER_READING_LIST_ANDROID_READING_LIST_MANAGER_H_
+
+#include "components/keyed_service/core/keyed_service.h"
+
+class ReadingListModel;
+
+// Owns a reading list model and converts reading list data to bookmark nodes.
+class ReadingListManager : public KeyedService {
+ public:
+  explicit ReadingListManager(ReadingListModel* reading_list_model);
+  ~ReadingListManager() override;
+
+  ReadingListManager(const ReadingListManager&) = delete;
+  ReadingListManager& operator=(const ReadingListManager&) = delete;
+
+ private:
+  // Contains reading list data, outlives this class.
+  ReadingListModel* reading_list_model_;
+};
+
+#endif  // CHROME_BROWSER_READING_LIST_ANDROID_READING_LIST_MANAGER_H_
