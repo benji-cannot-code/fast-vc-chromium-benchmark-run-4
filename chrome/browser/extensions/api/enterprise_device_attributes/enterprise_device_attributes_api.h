8a0d535e1e72e6f4a8aa05e31a0b9aeda72a19fc
Omar Morsi <omorsi@google.com>
2020-09-21 08:56:00 +0000

    DeviceAttrs: Add TODOs to report error for unaffiliated users
    Bug: 1056550
    Change-Id: Idafa91e74cd7707632dbeaaaa0a27b448ae92719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416372
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Omar Morsi <omorsi@google.com>
    Cr-Commit-Position: refs/heads/master@{#808800}

--- chrome/browser/extensions/api/enterprise_device_attributes/enterprise_device_attributes_api.h
+++ chrome/browser/extensions/api/enterprise_device_attributes/enterprise_device_attributes_api.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
+// Note: When updating this function, consider changing the way errors are
+// returned.
+// TODO(https://crbug.com/1056550): Return an error in case of unaffiliated user
+// in enterprise.deviceAttributes API
 class EnterpriseDeviceAttributesGetDirectoryDeviceIdFunction
     : public ExtensionFunction {
  public:
@@ -26,6 +30,10 @@ class EnterpriseDeviceAttributesGetDirectoryDeviceIdFunction
                              ENTERPRISE_DEVICEATTRIBUTES_GETDIRECTORYDEVICEID)
 };
 
+// Note: When updating this function, consider changing the way errors are
+// returned.
+// TODO(https://crbug.com/1056550): Return an error in case of unaffiliated user
+// in enterprise.deviceAttributes API
 class EnterpriseDeviceAttributesGetDeviceSerialNumberFunction
     : public ExtensionFunction {
  public:
@@ -42,6 +50,10 @@ class EnterpriseDeviceAttributesGetDeviceSerialNumberFunction
       ENTERPRISE_DEVICEATTRIBUTES_GETDEVICESERIALNUMBER)
 };
 
+// Note: When updating this function, consider changing the way errors are
+// returned.
+// TODO(https://crbug.com/1056550): Return an error in case of unaffiliated user
+// in enterprise.deviceAttributes API
 class EnterpriseDeviceAttributesGetDeviceAssetIdFunction
     : public ExtensionFunction {
  public:
@@ -57,6 +69,10 @@ class EnterpriseDeviceAttributesGetDeviceAssetIdFunction
                              ENTERPRISE_DEVICEATTRIBUTES_GETDEVICEASSETID)
 };
 
+// Note: When updating this function, consider changing the way errors are
+// returned.
+// TODO(https://crbug.com/1056550): Return an error in case of unaffiliated user
+// in enterprise.deviceAttributes API
 class EnterpriseDeviceAttributesGetDeviceAnnotatedLocationFunction
     : public ExtensionFunction {
  public:
@@ -73,6 +89,10 @@ class EnterpriseDeviceAttributesGetDeviceAnnotatedLocationFunction
       ENTERPRISE_DEVICEATTRIBUTES_GETDEVICEANNOTATEDLOCATION)
 };
 
+// Note: When updating this function, consider changing the way errors are
+// returned.
+// TODO(https://crbug.com/1056550): Return an error in case of unaffiliated user
+// in enterprise.deviceAttributes API
 class EnterpriseDeviceAttributesGetDeviceHostnameFunction
     : public ExtensionFunction {
  public:
