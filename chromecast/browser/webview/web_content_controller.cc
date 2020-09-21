615ae126093a792eab789369e786051ac79ce61e
Ryan Daum <rdaum@chromium.org>
2020-09-21 16:34:57 +0000

    [chromecast] Fix synthetic key events for webviews
      * Unless is_char is true blink will interpret the key event as a raw
        keypress instead of a proper keystroke. Derive and pass through
        this property correctly.
    
      * Mark the event as EF_IS_SYNTHESIZED and ui::kPropertyFromVK, in
        the same manner that chromeos's virtual keyboard implementation
        does.
    
    Bug: internal b/161811385
    Test: manual x86 cast_shell build with wayland_webview_client attached
    Change-Id: I4d752bd219eb540a719e0bd339ab674bbbd3db23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2418999
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Ryan Daum <rdaum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808906}

--- chromecast/browser/webview/web_content_controller.cc
+++ chromecast/browser/webview/web_content_controller.cc
@@ -26,7 +26,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/input/web_touch_event.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_delegate.h"
+#include "ui/base/ime/constants.h"
 #include "ui/events/event.h"
+#include "ui/events/event_constants.h"
 
 namespace chromecast {
 
@@ -274,12 +276,20 @@ void WebContentController::ProcessInputEvent(const webview::InputEvent& ev) {
     case ui::ET_KEY_PRESSED:
     case ui::ET_KEY_RELEASED:
       if (ev.has_key()) {
-        ui::KeyEvent evt(type,
-                         static_cast<ui::KeyboardCode>(ev.key().key_code()),
-                         static_cast<ui::DomCode>(ev.key().dom_code()),
-                         ev.flags(), ui::DomKey(ev.key().dom_key()),
-                         base::TimeTicks() +
-                             base::TimeDelta::FromMicroseconds(ev.timestamp()));
+        ui::KeyEvent evt(
+            type, static_cast<ui::KeyboardCode>(ev.key().key_code()),
+            static_cast<ui::DomCode>(ev.key().dom_code()),
+            ev.flags() | ui::EF_IS_SYNTHESIZED, ui::DomKey(ev.key().dom_key()),
+            base::TimeTicks() +
+                base::TimeDelta::FromMicroseconds(ev.timestamp()),
+            ev.key().is_char());
+
+        // Marks the simulated key event is from a Virtual Keyboard.
+        ui::Event::Properties properties;
+        properties[ui::kPropertyFromVK] =
+            std::vector<uint8_t>(ui::kPropertyFromVKSize);
+        evt.SetProperties(properties);
+
         handler->OnKeyEvent(&evt);
       } else {
         client_->OnError("key() not supplied for key event");
