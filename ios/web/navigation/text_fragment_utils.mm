3ff2740273f9fa4a84394e9df317073fe1c72302
Tommy Martino <tmartino@google.com>
2020-09-21 22:45:46 +0000

    [SH iOS] URL-decoding values before passing to JS land
    This fixes a bug where text fragments in the URL are not matched in the
    page because spaces (and other special characters) are still URL-
    encoded.
    
    It also terminates early if no text fragment could be parsed, to avoid
    needless JS calls.
    
    Bug: 1099268
    Change-Id: I3a135a4804bc57cde6ef1b1cc22785016507db53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422724
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809104}

--- ios/web/navigation/text_fragment_utils.mm
+++ ios/web/navigation/text_fragment_utils.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstring.h>
 
 #include "base/json/json_writer.h"
+#include "base/strings/escape.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -23,6 +24,10 @@ namespace {
 
 const char kDirectivePrefix[] = ":~:";
 const char kTextFragmentPrefix[] = "text=";
+
+base::Value DecodeStringToValue(const std::string& str) {
+  return base::Value(base::UnescapeBinaryURLComponent(str));
+}
 
 }  // namespace
 
@@ -43,10 +48,14 @@ bool AreTextFragmentsAllowed(NavigationContext* context) {
 }
 
 void HandleTextFragments(WebState* state) {
+  base::Value parsed_fragments =
+      internal::ParseTextFragments(state->GetLastCommittedURL());
+
+  if (parsed_fragments.type() == base::Value::Type::NONE)
+    return;
+
   std::string fragment_param;
-  base::JSONWriter::Write(
-      internal::ParseTextFragments(state->GetLastCommittedURL()),
-      &fragment_param);
+  base::JSONWriter::Write(parsed_fragments, &fragment_param);
 
   std::string script = base::ReplaceStringPlaceholders(
       "__gCrWeb.textFragments.handleTextFragments($1, $2)",
@@ -149,16 +158,16 @@ base::Value TextFragmentToValue(std::string fragment) {
   }
 
   if (prefix.size())
-    dict.SetKey("prefix", base::Value(prefix));
+    dict.SetKey("prefix", DecodeStringToValue(prefix));
 
   // Guaranteed non-empty after checking for malformed input above.
-  dict.SetKey("textStart", base::Value(text_start));
+  dict.SetKey("textStart", DecodeStringToValue(text_start));
 
   if (text_end.size())
-    dict.SetKey("textEnd", base::Value(text_end));
+    dict.SetKey("textEnd", DecodeStringToValue(text_end));
 
   if (suffix.size())
-    dict.SetKey("suffix", base::Value(suffix));
+    dict.SetKey("suffix", DecodeStringToValue(suffix));
 
   return dict;
 }
