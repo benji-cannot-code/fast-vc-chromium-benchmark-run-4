b762d6c48fab153d0b59cde43d41a75782f972e1
Filip Gorski <fgorski@chromium.org>
2020-09-21 17:16:50 +0000

    [GAR4] Fixing Talk Back/Switch Access order of Find in Page controls
    The issue observed with Find in Page is that when there is nothing yet
    to search for (no input) the Talk Back order of visited controls is
    incorrect: Text entry field is followed by web contents and its
    elements that can be jumped to, and not by the remaining Find in Page
    toolbar controls (previous, next and close buttons).
    
    The problem happens because the search results counter field, while
    visible with no input, carries no focusable content. That makes the
    accessibility API fail and jump to web contents.
    
    This patch ensures that the field is gone, when there is no content
    for it to hold, and makes it visible as soon as there is content.
    
    Bug: 1093480
    Change-Id: I158114b8b07f7defa12671ab900736ea728401e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2421293
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808930}

--- chrome/android/java/src/org/chromium/chrome/browser/findinpage/FindToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/findinpage/FindToolbar.java
@@ -17,6 +17,7 @@ import android.provider.Settings;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.Selection;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.ActionMode;
@@ -336,6 +337,7 @@ public class FindToolbar extends LinearLayout {
         });
 
         mFindStatus = (TextView) findViewById(R.id.find_status);
+        setStatus("", false);
 
         mFindPrevButton = findViewById(R.id.find_prev_button);
         mFindPrevButton.setOnClickListener(new OnClickListener() {
@@ -770,6 +772,7 @@ public class FindToolbar extends LinearLayout {
         mFindStatus.setText(text);
         mFindStatus.setContentDescription(null);
         mFindStatus.setTextColor(getStatusColor(failed, isIncognito()));
+        mFindStatus.setVisibility(TextUtils.isEmpty(text) ? GONE : VISIBLE);
     }
 
     /**
