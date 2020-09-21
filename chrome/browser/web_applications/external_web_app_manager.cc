c402749ee3fb7baea977c688fbec46853c6a2ac0
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-09-21 21:53:29 +0000

    [Web App Migration] Provide a flow for migrating to hard-coded web apps
    Currently, the ExternalWebAppManager provides a flow for migrating from
    an extension-app to a web app specified in a known directory through a
    JSON config. This will be used on ChromeOS.
    
    On other platforms, we'll simply hard-code the web app data, and use it
    directly. This CL provides an entry point to do so, allowing for the
    specification of default apps that are directly included in the binary.
    
    Bug: 809304, 1118195
    Change-Id: I0bb83de21ac1f59bdf62fcf313e98db3889bd01f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417535
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809081}

--- chrome/browser/web_applications/external_web_app_manager.cc
+++ chrome/browser/web_applications/external_web_app_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/web_applications/external_web_app_manager.h"
 
+#include <iterator>
 #include <map>
 #include <memory>
 #include <string>
@@ -30,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/web_app_constants.h"
 #include "chrome/browser/web_applications/components/web_app_install_utils.h"
 #include "chrome/browser/web_applications/external_web_app_utils.h"
+#include "chrome/browser/web_applications/preinstalled_web_apps.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_paths.h"
 #include "content/public/browser/browser_thread.h"
@@ -220,6 +222,14 @@ void ExternalWebAppManager::SynchronizeExternalInstallOptions(
     PendingAppManager::SynchronizeCallback callback,
     std::vector<ExternalInstallOptions> desired_apps_install_options) {
   DCHECK(pending_app_manager_);
+
+  // Add in any web apps that should be pre-installed.
+  std::vector<ExternalInstallOptions> default_apps = GetPreinstalledWebApps();
+  desired_apps_install_options.insert(
+      desired_apps_install_options.end(),
+      std::make_move_iterator(default_apps.begin()),
+      std::make_move_iterator(default_apps.end()));
+
   pending_app_manager_->SynchronizeInstalledApps(
       std::move(desired_apps_install_options),
       ExternalInstallSource::kExternalDefault, std::move(callback));
