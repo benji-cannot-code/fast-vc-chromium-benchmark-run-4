223a62873b6960659d2e5961238b69c25dac92e9
Andre Le <leandre@chromium.org>
2020-09-21 21:35:34 +0000

    [CrOS PhoneHub] Hook up task continuation UI to real phone data.
    - Connect task continuation view to phone model to hook up data.
    - Fix continue browsing view to display data.
    - A small fix in multidevice-internals to display favicon in debug UI.
    
    Screenshot:
    https://screenshot.googleplex.com/AXzJduFaBPTaLmQ
    
    BUG=1106937,1126208
    
    Change-Id: I2dde0aeb25183f010427644f0eb54582056ad9b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414889
    Reviewed-by: Tim Song <tengs@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809071}

--- ash/system/phonehub/continue_browsing_chip.h
+++ ash/system/phonehub/continue_browsing_chip.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_PHONEHUB_CONTINUE_BROWSING_CHIP_H_
 
 #include "ash/ash_export.h"
+#include "chromeos/components/phonehub/browser_tabs_model.h"
 #include "ui/gfx/canvas.h"
 #include "ui/views/controls/button/button.h"
 
@@ -17,7 +18,9 @@ namespace ash {
 class ASH_EXPORT ContinueBrowsingChip : public views::Button,
                                         public views::ButtonListener {
  public:
-  ContinueBrowsingChip();
+  explicit ContinueBrowsingChip(
+      const chromeos::phonehub::BrowserTabsModel::BrowserTabMetadata& metadata);
+
   ~ContinueBrowsingChip() override;
   ContinueBrowsingChip(ContinueBrowsingChip&) = delete;
   ContinueBrowsingChip operator=(ContinueBrowsingChip&) = delete;
@@ -26,6 +29,9 @@ class ASH_EXPORT ContinueBrowsingChip : public views::Button,
   void OnPaintBackground(gfx::Canvas* canvas) override;
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
   const char* GetClassName() const override;
+
+ private:
+  GURL url_;
 };
 
 }  // namespace ash
