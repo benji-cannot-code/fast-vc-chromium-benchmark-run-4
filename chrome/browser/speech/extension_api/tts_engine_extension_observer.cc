012e0257a058cb75b4758382a53f0a21bf9a5cc6
David Tseng <dtseng@google.com>
2020-09-21 17:23:36 +0000

    Keep Google tts engine alive when Select to Speak is enabled
    This change is somewhat of a workaround to shield Chrome from any issues
    contained within the underlying tts engine.
    
    Issues:
    - the extension system evicts extensions that don't explicitly fire an Activity
      as defined by extensions/browser/activity.h
    - audio playback isn't plumbed through as an activity
    - engines like Espeak work even for long utterances that hit the eviction time,
      because they trigger the extension api activity via word callbacks
    - the Google tts engine currently is buggy for some sentences in emitting
      callbacks
    
    All interleavings of increment/decrements were considered:
    - engine loads, enable ChromeVox/STS, disable ChromeVox/STS
    - enable ChromeVox, engine loads, enable STS, disable STS/ChromeVox
    - enable both STS and ChromeVox, engine loads, disable STS/ChromeVox
    
    In all cases, the keep alive count will be 0 at the end.
    
    R=dmazzoni@chromium.org
    
    Fixed: 1128093
    Change-Id: I92b3fc32cd777961e4fbba40c5891c49943b9645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416571
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808936}

--- chrome/browser/speech/extension_api/tts_engine_extension_observer.cc
+++ chrome/browser/speech/extension_api/tts_engine_extension_observer.cc
@@ -197,6 +197,11 @@ void TtsEngineExtensionObserver::OnExtensionLoaded(
     UpdateGoogleSpeechSynthesisKeepAliveCount(browser_context,
                                               true /* increment */);
   }
+
+  if (chromeos::AccessibilityManager::Get()->IsSelectToSpeakEnabled()) {
+    UpdateGoogleSpeechSynthesisKeepAliveCount(browser_context,
+                                              true /* increment */);
+  }
 #endif  // defined(OS_CHROMEOS)
 }
 
@@ -220,12 +225,14 @@ void TtsEngineExtensionObserver::OnExtensionUnloaded(
 void TtsEngineExtensionObserver::OnAccessibilityStatusChanged(
     const chromeos::AccessibilityStatusEventDetails& details) {
   if (details.notification_type != chromeos::AccessibilityNotificationType::
-                                       ACCESSIBILITY_TOGGLE_SPOKEN_FEEDBACK)
+                                       ACCESSIBILITY_TOGGLE_SPOKEN_FEEDBACK &&
+      details.notification_type != chromeos::AccessibilityNotificationType::
+                                       ACCESSIBILITY_TOGGLE_SELECT_TO_SPEAK)
     return;
 
   // Google speech synthesis might not be loaded yet. If it isn't, the call in
   // |OnExtensionLoaded| will do the increment. If it is, the call below will
-  // increment. Decrements only occur when toggling off ChromeVox here.
+  // increment. Decrements only occur when toggling off here.
   UpdateGoogleSpeechSynthesisKeepAliveCount(profile(), details.enabled);
 }
 #endif
