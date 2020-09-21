eedef99514dc2579a7861f839696b97491192844
Swapnil <swapnilgupta@google.com>
2020-09-21 14:59:47 +0000

    Introduce new stages to investigate CREATED stage
    We have many extensions stuck in CREATED stage which is reported
    in ExtensionManagement::UpdateForcedExtensions when extension list is
    fetched from kInstallForceList pref. Some of the intermediate stages
    were added to investigate CREATED stage. This CL moves these
    intermediate stages to a new enum and introduces a few new stages
    for investigation.
    
    Following histograms are added:
    Extensions.ForceInstalledCreationStage
    Extensions.ForceInstalledStage2
    
    Following histogram is deprecated:
    Extensions.ForceInstalledStage
    
    Bug: 989526
    Change-Id: I17a1bd7d49c31b6363a0512a424017775098042f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2390222
    Reviewed-by: Saurabh Nijhara <snijhara@google.com>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Swapnil Gupta <swapnilgupta@google.com>
    Cr-Commit-Position: refs/heads/master@{#808876}

--- chrome/browser/extensions/extension_management.h
+++ chrome/browser/extensions/extension_management.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/observer_list.h"
 #include "base/values.h"
+#include "chrome/browser/extensions/forced_extensions/install_stage_tracker.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
@@ -220,6 +221,14 @@ class ExtensionManagement : public KeyedService {
 
   void OnExtensionPrefChanged();
   void NotifyExtensionManagementPrefChanged();
+
+  // Reports install creation stage to InstallStageTracker for the extensions.
+  // |forced_stage| is reported for the extensions which have installation mode
+  // as INSTALLATION_FORCED, and |other_stage| is reported for all other
+  // installation modes.
+  void ReportExtensionManagementInstallCreationStage(
+      InstallStageTracker::InstallCreationStage forced_stage,
+      InstallStageTracker::InstallCreationStage other_stage);
 
   // Helper to return an extension install list, in format specified by
   // ExternalPolicyLoader::AddExtension().
