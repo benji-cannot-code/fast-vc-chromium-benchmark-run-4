f2b1d56e596dccfa2f90ccb2a4769206da4481ce
Dale Curtis <dalecurtis@chromium.org>
2020-09-21 20:29:54 +0000

    Roll src/third_party/dav1d/libdav1d/ ebc8e4d9f..f90ada0d0 (42 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/ebc8e4d9fb94..f90ada0d08e9
    
    $ git log ebc8e4d9f..f90ada0d0 --date=short --no-merges --format='%ad %ae %s'
    2020-09-20 janne-vlc tests: avoid using sed in header test
    2020-09-20 martin meson: Set msvc like warning options for clang-cl
    2020-09-18 martin meson: Use gas-preprocessor as generator, for targets that need it
    2020-09-20 janne-vlc build: increase minimal meson to 0.49
    2020-09-13 gramner x86: Add misc mc asm tweaks
    2020-09-15 wtc Ban op->idc that may drop all layer-specific OBUs
    2020-09-06 gramner cli: Add support for Unicode and long paths on Windows 10
    2020-08-28 martin arm32: mc: NEON implementation of put/prep 8tap/bilin for 16 bpc
    2020-09-03 martin arm64: mc: Apply tuning from w4/w8 case to w2 case in 16 bpc 8tap_hv
    2020-09-03 martin arm: mc: Avoid an unnecessary mov in 8tap_hv w2
    2020-09-01 martin arm32: mc: Load 8tap filter coefficients with alignment where possible
    2020-09-01 martin arm32: mc: Use narrower vext.8 in 8tap_w4_h
    2020-08-29 martin arm64: mc: Use more descriptive element specifiers for loads/stores in 16 bpc put_neon
    2020-09-01 gramner cli: Use proper integer math in Y4M PAR calculations
    2020-08-20 rzumer Output render size to Y4M
    2020-08-27 martin arm32: mc: NEON implementation of avg/mask/w_avg for 16 bpc
    2020-08-28 martin cli: Print the decoding fps even if the file lacks a nominal framerate
    2020-08-22 janne-vlc tests: test stand alone API header compilation
    2020-08-22 janne-vlc dav1d/headers.h: add missing stdint.h include
    2020-08-22 janne-vlc contributing: document the allowed internal use of anonymous structs/unions
    2020-08-21 janne-vlc bump soname for API changes
    2020-08-21 janne-vlc API: move reserved space in Dav1dSettings to the end
    2020-07-21 janne-vlc API: remove anonymous struct and union from Dav1dWarpedMotionParams
    2020-07-09 janne-vlc CI: compare x86inc.asm with upstream
    2020-07-10 janne-vlc x86inc.asm: remove private_prefix define and config.asm include
    2020-07-09 janne-vlc x86inc.asm: use standalone x86inc.asm as upstream
    2020-06-24 gramner x86inc.asm: Properly sort instructions in alphabetical order
    2020-08-07 martin checkasm: Add ifdefs around the readtime check
    2020-08-06 martin checkasm: Enforce declare_func to be outside of check_func
    2020-08-06 jamrial obu: remove a few unnecessary calls to memset()
    2020-07-17 jamrial obu: reduce scope of some variables
    2020-08-05 martin checkasm: Use mach_absolute_time() as timer on darwin on ARM
    2020-08-05 martin checkasm: msac: Fix signal handler recovery in msac_decode_bool*
    2020-08-05 martin checkasm: Explicitly test whether the readtime() function works
    2020-06-26 victorien x86: Add {put/prep}_{8tap/bilin} SSSE3 asm (64-bit)
    2020-06-30 victorien x86: Minor changes to MC scaled AVX2 asm
    2020-07-14 gramner x86: Add cdef_filter SSE optimizations
    2020-07-20 epirat07 dav1dplay: Fix type mismatch warning
    2020-07-13 code README: Update roadmap
    2020-07-11 gramner Add enum entries for the maximum valid metadata values
    2020-07-13 code Update README.md
    2020-07-10 thakis Hide const symbols names too when building for mac/arm64
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    
    TBR=wtc
    
    Bug: 1130138
    Change-Id: I170af4e38c41278df942de6a8c22f0d4a589b760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422127
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809032}

--- third_party/dav1d/config/win/x86/config.asm
+++ third_party/dav1d/config/win/x86/config.asm
@@ -13,3 +13,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 %define PREFIX 1
 
 ; %define STACK_ALIGNMENT 4 -- Stack alignment is controlled by Chromium
+
+%define private_prefix dav1d
