66087d9f3c8e65757f4bca162ad61e027cc62a07
Thomas Lukaszewicz <tluk@chromium.org>
2020-09-21 06:43:34 +0000

    Tab Search: Fix favicon2 dependency
    This CL adds a missing data source needed by Tab Search WebUI to use
    favicon2 resources.
    
    This fixes loading issues for all profiles non off the record profiles.
    
    Bug: 1099917
    Change-Id: I20b8f709580fcab544726e546c06a7cdc020f638
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2419390
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808787}

--- chrome/browser/ui/webui/tab_search/tab_search_ui.cc
+++ chrome/browser/ui/webui/tab_search/tab_search_ui.cc
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/tab_search/tab_search_ui.h"
 
 #include "build/branding_buildflags.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/webui/favicon_source.h"
 #include "chrome/browser/ui/webui/tab_search/tab_search_page_handler.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/favicon_base/favicon_url_parser.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -45,6 +48,11 @@ TabSearchUI::TabSearchUI(content::WebUI* web_ui)
       kGeneratedPath, IDR_TAB_SEARCH_PAGE_HTML);
   content::WebUIDataSource::Add(web_ui->GetWebContents()->GetBrowserContext(),
                                 source);
+
+  Profile* profile = Profile::FromWebUI(web_ui);
+  content::URLDataSource::Add(
+      profile, std::make_unique<FaviconSource>(
+                   profile, chrome::FaviconUrlFormat::kFavicon2));
 #endif  // BUILDFLAG(ENABLE_TAB_SEARCH)
 }
 
