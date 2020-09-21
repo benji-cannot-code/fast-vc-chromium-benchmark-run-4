53bdb77805efd91d50978b3d22d4d0104e804c9c
Renato Silva <rrsilva@google.com>
2020-09-21 16:46:10 +0000

    CrOS OOBE - Fix Assistant message about battery
    The Assistant flow in OOBE mentions that it will only listen when
    the device is connected to a power source in order to save battery
    when no DSP is available. This prevents this message from showing
    up on Chromebases, Chromebits and Chromeboxes.
    
    Fixed: 1109363
    Bug: 1109363
    Change-Id: I188a06d8e7c42064a6daab8f27a2e963930ee1fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416696
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/master@{#808911}

--- chrome/browser/ui/webui/chromeos/login/assistant_optin_flow_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/assistant_optin_flow_screen_handler.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/webui/chromeos/assistant_optin/assistant_optin_utils.h"
 #include "chrome/grit/generated_resources.h"
+#include "chromeos/dbus/power/power_manager_client.h"
 #include "chromeos/services/assistant/public/cpp/assistant_prefs.h"
 #include "chromeos/services/assistant/public/cpp/assistant_service.h"
 #include "chromeos/services/assistant/public/cpp/features.h"
@@ -148,6 +149,7 @@ void AssistantOptInFlowScreenHandler::RegisterMessages() {
 void AssistantOptInFlowScreenHandler::GetAdditionalParameters(
     base::DictionaryValue* dict) {
   dict->SetBoolean("hotwordDspAvailable", chromeos::IsHotwordDspAvailable());
+  dict->SetBoolean("deviceHasNoBattery", !DeviceHasBattery());
   dict->SetBoolean("voiceMatchDisabled",
                    chromeos::assistant::features::IsVoiceMatchDisabled());
   BaseScreenHandler::GetAdditionalParameters(dict);
@@ -602,6 +604,19 @@ void AssistantOptInFlowScreenHandler::HandleFlowInitialized(
 
   if (flow_type_ == ash::FlowType::kConsentFlow)
     SendGetSettingsRequest();
+}
+
+bool AssistantOptInFlowScreenHandler::DeviceHasBattery() {
+  // Assume that the device has a battery if we can't determine otherwise.
+  if (!chromeos::PowerManagerClient::Get())
+    return true;
+
+  auto status = PowerManagerClient::Get()->GetLastStatus();
+  if (!status.has_value() || !status->has_battery_state())
+    return true;
+
+  return status->battery_state() !=
+         power_manager::PowerSupplyProperties_BatteryState_NOT_PRESENT;
 }
 
 }  // namespace chromeos
--- chrome/browser/ui/webui/chromeos/login/assistant_optin_flow_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/assistant_optin_flow_screen_handler.h
@@ -129,6 +129,9 @@ class AssistantOptInFlowScreenHandler
   void HandleFlowFinished();
   void HandleFlowInitialized(const int flow_type);
 
+  // Power related
+  bool DeviceHasBattery();
+
   AssistantOptInFlowScreen* screen_ = nullptr;
 
   base::OnceClosure on_initialized_;
