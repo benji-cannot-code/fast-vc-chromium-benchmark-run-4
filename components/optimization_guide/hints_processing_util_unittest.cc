78f97ef64da6e935b00e684ab43e402ea877a2f5
Sophie Chang <sophiechang@chromium.org>
2020-09-21 22:44:14 +0000

    Remove experiment name and excluded experiment name from hints processing
    Change-Id: Ic0a1e559dad64808d1ca54acaf2db6ffae2402d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422464
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809102}

--- components/optimization_guide/hints_processing_util_unittest.cc
+++ components/optimization_guide/hints_processing_util_unittest.cc
@@ -58,6 +58,7 @@ TEST(HintsProcessingUtilTest, FindPageHintForSubstringPagePattern) {
   EXPECT_EQ(page_hint3, FindPageHintForURL(
                             GURL("https://www.foo.org/bar/three.jpg"), &hint1));
 }
+
 TEST(HintsProcessingUtilTest, ConvertProtoEffectiveConnectionType) {
   EXPECT_EQ(
       ConvertProtoEffectiveConnectionType(
