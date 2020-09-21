84c4e8ac67431ea479ab01e62cc0b9f493300f2d
Alice Wang <aliceywang@chromium.org>
2020-09-21 17:16:10 +0000

    [Android][WebSignin] Adjust methods scope of WebSigninBridge
    This CL adjusts the scope of the methods called only in native in
    WebSigninBridge.
    
    Bug: 1124737
    Change-Id: I353dea64e7382da0dac4569cd4a4b418d7c869c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2421112
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#808929}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/WebSigninBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/WebSigninBridge.java
@@ -27,7 +27,7 @@ public class WebSigninBridge {
         /**
          * Sign-in completed successfully and the primary account is available in the cookie jar.
          */
-        void onSigninSucceded();
+        void onSigninSucceeded();
 
         /**
          * Sign-in process failed.
@@ -76,12 +76,12 @@ public class WebSigninBridge {
     }
 
     @CalledByNative
-    static void onSigninSucceded(Listener listener) {
-        listener.onSigninSucceded();
+    private static void onSigninSucceded(Listener listener) {
+        listener.onSigninSucceeded();
     }
 
     @CalledByNative
-    static void onSigninFailed(Listener listener, GoogleServiceAuthError error) {
+    private static void onSigninFailed(Listener listener, GoogleServiceAuthError error) {
         listener.onSigninFailed(error);
     }
 
