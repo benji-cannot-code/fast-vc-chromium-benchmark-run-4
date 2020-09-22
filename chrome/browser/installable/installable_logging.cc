15060315fea086ade9b60a67ec22274c8edefd85
Adithya Srinivasan <adithyas@chromium.org>
2020-09-22 14:31:45 +0000

    Remove redundant main frame check in InstallableManager
    Found while doing an audit of WebContents::GetMainFrame().
    web_contents->GetMainFrame()->GetParent() will always return null.
    
    Change-Id: Idc5d2ed719a400a300198b9c36ab4e3307934556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422440
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809320}

--- chrome/browser/installable/installable_logging.cc
+++ chrome/browser/installable/installable_logging.cc
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Error message strings corresponding to the InstallableStatusCode enum.
-static const char kNotInMainFrameMessage[] =
-    "Page is not loaded in the main frame";
 static const char kNotFromSecureOriginMessage[] =
     "Page is not served from a secure origin";
 static const char kNoManifestMessage[] = "Page has no manifest <link> URL";
@@ -69,7 +67,6 @@ static const char kManifestDisplayOverrideNotSupportedMessage[] =
     "Manifest contains 'display_override' field, and the first supported "
     "display mode must be one of 'standalone', 'fullscreen', or 'minimal-ui'";
 
-static const char kNotInMainFrameId[] = "not-in-main-frame";
 static const char kNotFromSecureOriginId[] = "not-from-secure-origin";
 static const char kNoManifestId[] = "no-manifest";
 static const char kManifestEmptyId[] = "manifest-empty";
@@ -132,9 +129,6 @@ std::string GetErrorMessage(InstallableStatusCode code) {
     case WAITING_FOR_NATIVE_DATA:
     case SHOWING_APP_INSTALLATION_DIALOG:
     case MAX_ERROR_CODE:
-      break;
-    case NOT_IN_MAIN_FRAME:
-      message = kNotInMainFrameMessage;
       break;
     case NOT_FROM_SECURE_ORIGIN:
       message = kNotFromSecureOriginMessage;
@@ -238,9 +232,6 @@ content::InstallabilityError GetInstallabilityError(
     case WAITING_FOR_NATIVE_DATA:
     case SHOWING_APP_INSTALLATION_DIALOG:
     case MAX_ERROR_CODE:
-      break;
-    case NOT_IN_MAIN_FRAME:
-      error_id = kNotInMainFrameId;
       break;
     case NOT_FROM_SECURE_ORIGIN:
       error_id = kNotFromSecureOriginId;
