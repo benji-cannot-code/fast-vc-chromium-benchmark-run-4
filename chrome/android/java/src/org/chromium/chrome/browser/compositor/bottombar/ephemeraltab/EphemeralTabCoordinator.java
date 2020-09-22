f5777aa49909ff1c6f55b192dcfbdb04e5fd0fb6
Michael Thiessen <mthiesse@chromium.org>
2020-09-22 22:59:28 +0000

    Reland "Migrate NavigationHandle from String URLs to GURL"
    Reland as 3-sided patch to avoid downstream break.
    
    Original change's description:
    > Migrate NavigationHandle from String URLs to GURL
    >
    > Migrates NavigationHandle to use GURL internally and adds getUrlString
    > for callsites that still need URLs to be a string (to be fixed in
    > followups).
    >
    > Some unit tests needed to be moved to Instrumentation tests, one
    > requiring a change to CriteriaHelper to allow checking for a condition
    > after every task.
    >
    > QualityEnforcerUnitTest was too difficult to migrate to Instrumentation
    > tests, so it now uses JUnitTestGURLs to use GURL in junit tests.
    >
    > Bug: 783819
    
    Bug: 783819
    Change-Id: Ic2043b8e871a4cb8a6d59ed7d6334486bd7ebe59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2424986
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809560}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabCoordinator.java
@@ -45,6 +45,7 @@ import org.chromium.ui.UiUtils;
 import org.chromium.ui.base.ActivityWindowAndroid;
 import org.chromium.ui.base.PageTransition;
 import org.chromium.ui.base.ViewAndroidDelegate;
+import org.chromium.url.GURL;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -310,7 +311,7 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
          * @param callback The callback to be invoked to display the final image.
          * @param profile The profile for which favicon service is used.
          */
-        public void loadFavicon(final String url, Callback<Drawable> callback, Profile profile) {
+        public void loadFavicon(final GURL url, Callback<Drawable> callback, Profile profile) {
             assert profile != null;
             FaviconHelper.FaviconImageCallback imageCallback = (bitmap, iconUrl) -> {
                 Drawable drawable;
@@ -325,7 +326,8 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
                 callback.onResult(drawable);
             };
 
-            mFaviconHelper.getLocalFaviconImageForURL(profile, url, mFaviconSize, imageCallback);
+            mFaviconHelper.getLocalFaviconImageForURL(
+                    profile, url.getSpec(), mFaviconSize, imageCallback);
         }
     }
 }
--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
@@ -803,7 +803,7 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
             initWebContents(webContents);
 
             if (!creatingWebContents && webContents.isLoadingToDifferentDocument()) {
-                didStartPageLoad(webContents.getVisibleUrlString());
+                didStartPageLoad(webContents.getVisibleUrl());
             }
 
         } finally {
@@ -935,10 +935,12 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
      * Called when a page has started loading.
      * @param validatedUrl URL being loaded.
      */
-    void didStartPageLoad(String validatedUrl) {
+    void didStartPageLoad(GURL validatedUrl) {
         updateTitle();
         if (mIsRendererUnresponsive) handleRendererResponsiveStateChanged(true);
-        for (TabObserver observer : mObservers) observer.onPageLoadStarted(this, validatedUrl);
+        for (TabObserver observer : mObservers) {
+            observer.onPageLoadStarted(this, validatedUrl.getSpec());
+        }
     }
 
     /**
@@ -969,11 +971,11 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
      * @param url The URL that was loaded.
      * @param transitionType The transition type to the current URL.
      */
-    void handleDidFinishNavigation(String url, Integer transitionType) {
+    void handleDidFinishNavigation(GURL url, Integer transitionType) {
         mIsNativePageCommitPending = false;
         boolean isReload = (transitionType != null
                 && (transitionType & PageTransition.CORE_MASK) == PageTransition.RELOAD);
-        if (!maybeShowNativePage(url, isReload)) {
+        if (!maybeShowNativePage(url.getSpec(), isReload)) {
             showRenderedPage();
         }
     }
@@ -1166,14 +1168,12 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
             initWebContents(webContents);
         });
 
-        String url = getUrlString();
-
         if (didStartLoad) {
             // Simulate the PAGE_LOAD_STARTED notification that we did not get.
-            didStartPageLoad(url);
+            didStartPageLoad(getUrl());
 
             // Simulate the PAGE_LOAD_FINISHED notification that we did not get.
-            if (didFinishLoad) didFinishPageLoad(url);
+            if (didFinishLoad) didFinishPageLoad(getUrlString());
         }
 
         for (TabObserver observer : mObservers) {
