148a6c8570ccf22a1f35d5e113d463b820878610
Markus Handell <handellm@google.com>
2020-09-22 19:17:55 +0000

    Mac: zero-copy frame encode for IOSurface-backed media::VideoFrames.
    When forwarding a frame to Video Encode Accelerator, RTCVideoEncoder
    would almost always create a frame copy unless the incoming
    media::VideoFrame was backed by STORAGE_SHMEM. The copy would be backed
    by STORAGE_SHMEM, which results in the video decode accelerator wrapping
    the VideoFrame's memory in a CVPixelBufferRef.
    
    Changes:
    1) Break out code in media::VideoFrame::WrapIOSurface() that creates
       an IOSurface from a GMB handle.
    2) Code is added to WrapVideoFrameInCVPixelBuffer to create a
       CVPixelBufferRef via GpuMemoryBuffer::CloneHandle().
    3) RTCVideoEncoder is changed to pass on GMB-backed video frames.
    
    Bug: 1127791, 1125879
    Change-Id: I59dd1eae59ae4dffb591830152f44e470fb3cce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410244
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Markus Handell <handellm@google.com>
    Cr-Commit-Position: refs/heads/master@{#809450}

--- media/base/video_frame.cc
+++ media/base/video_frame.cc
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/buffer_format_util.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/gpu_memory_buffer.h"
+#if defined(OS_MAC)
+#include "ui/gfx/mac/io_surface.h"
+#endif
 
 namespace media {
 
@@ -695,14 +698,9 @@ scoped_refptr<VideoFrame> VideoFrame::WrapIOSurface(
     DLOG(ERROR) << "Non-IOSurface handle.";
     return nullptr;
   }
-  if (!handle.mach_port) {
-    DLOG(ERROR) << "Invalid mach port.";
-    return nullptr;
-  }
-  base::ScopedCFTypeRef<IOSurfaceRef> io_surface(
-      IOSurfaceLookupFromMachPort(handle.mach_port));
+  base::ScopedCFTypeRef<IOSurfaceRef> io_surface =
+      gfx::IOSurfaceMachPortToIOSurface(std::move(handle.mach_port));
   if (!io_surface) {
-    DLOG(ERROR) << "Unable to lookup IOSurface.";
     return nullptr;
   }
 
