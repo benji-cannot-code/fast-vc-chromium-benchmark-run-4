d6d9fe340652c93742c98717680900c715382e60
Yuheng Huang <yuhengh@chromium.org>
2020-09-22 13:16:33 -0700

    Fix favicon not showing up on OTR profile
    Related CL: https://chromium-review.googlesource.com/c/chromium/src/+/2424510
    Bug: 1099917
    Change-Id: I9a3272fb1040a002498be4dd876a579804234695
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/chrome/browser/resources/tab_search/+/3284531
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819611}

--- chrome/browser/resources/tab_search_merge/test/tab_search_app_test.js
+++ chrome/browser/resources/tab_search_merge/test/tab_search_app_test.js
@@ -86,7 +86,6 @@ suite('TabSearchAppTest', () => {
     const tabData = {
       index: 0,
       tabId: 1,
-      favIconUrl: '',
       title: 'Google',
       url: 'https://www.google.com',
     };
@@ -205,7 +204,6 @@ suite('TabSearchAppTest', () => {
     const updatedTab = /** @type {!tabSearch.mojom.Tab} */ ({
       index: 0,
       tabId: 1,
-      favIconUrl: '',
       title: 'Example',
       url: 'https://example.com',
       lastActiveTimeTicks: { internalValue: 1 },
@@ -336,7 +334,6 @@ suite('TabSearchAppTest', () => {
     {
       index: 0,
       tabId: 1,
-      favIconUrl: '',
       title: 'Google',
       url: 'https://www.google.com',
       lastActiveTimeTicks: { internalValue: 2 },
@@ -344,7 +341,6 @@ suite('TabSearchAppTest', () => {
     {
       index: 1,
       tabId: 2,
-      favIconUrl: '',
       title: 'Bing',
       url: 'https://www.bing.com',
       lastActiveTimeTicks: { internalValue: 4 },
@@ -352,7 +348,6 @@ suite('TabSearchAppTest', () => {
     {
       index: 2,
       tabId: 3,
-      favIconUrl: '',
       title: 'Yahoo',
       url: 'https://www.yahoo.com',
       lastActiveTimeTicks: { internalValue: 3 },
