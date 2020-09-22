2db8b20160b6738bc6fecd3118337473bde122d6
Polina Bondarenko <pbond@google.com>
2020-09-22 17:27:06 +0000

    arc: add clear snapshot flow.
    Add parsing/syncing to local state arc.snapshot pref of snapshot related info.
    Add blocked UI mode Chrome start up along with clearing outdated
    snapshots flow.
    Currently, ArcDataSnapshotdManager functionality is disabled for testing.
    
    BUG=b:161221001
    TEST=components_unittests
    
    Change-Id: I6ccfe44c3b85a6dc864914609fa41c071e81e4a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2385396
    Commit-Queue: Polina Bondarenko <pbond@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Auto-Submit: Polina Bondarenko <pbond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809390}

--- components/arc/enterprise/arc_data_snapshotd_bridge_unittest.cc
+++ components/arc/enterprise/arc_data_snapshotd_bridge_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/bind_helpers.h"
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/task_environment.h"
@@ -63,7 +64,7 @@ class ArcDataSnapshotdBridgeTest : public testing::Test {
 // Test basic scenario: D-Bus service is available immediately.
 TEST_F(ArcDataSnapshotdBridgeTest, ServiceAvailable) {
   dbus_client()->set_available(true /* is_available */);
-  ArcDataSnapshotdBridge bridge;
+  ArcDataSnapshotdBridge bridge{base::DoNothing()};
   EXPECT_FALSE(bridge.is_available_for_testing());
   RunGenerateKeyPair(&bridge, false /* expected_result */);
 
@@ -76,7 +77,7 @@ TEST_F(ArcDataSnapshotdBridgeTest, ServiceAvailable) {
 // Test basic scenario: D-Bus service is not available.
 TEST_F(ArcDataSnapshotdBridgeTest, ServiceUnavailable) {
   dbus_client()->set_available(false /* is_available */);
-  ArcDataSnapshotdBridge bridge;
+  ArcDataSnapshotdBridge bridge{base::DoNothing()};
 
   task_environment_.RunUntilIdle();
 
@@ -87,7 +88,7 @@ TEST_F(ArcDataSnapshotdBridgeTest, ServiceUnavailable) {
 // Test that service is available from the max attempt.
 TEST_F(ArcDataSnapshotdBridgeTest, ServiceAvailableMaxAttempt) {
   dbus_client()->set_available(false /* is_available */);
-  ArcDataSnapshotdBridge bridge;
+  ArcDataSnapshotdBridge bridge{base::DoNothing()};
 
   // Not available from the first attempt.
   task_environment_.RunUntilIdle();
@@ -110,7 +111,7 @@ TEST_F(ArcDataSnapshotdBridgeTest, ServiceAvailableMaxAttempt) {
 // Test that service is available from the max + 1 attempt and is not picked up.
 TEST_F(ArcDataSnapshotdBridgeTest, ServiceUnavailableMaxAttempts) {
   dbus_client()->set_available(false /* is_available */);
-  ArcDataSnapshotdBridge bridge;
+  ArcDataSnapshotdBridge bridge{base::DoNothing()};
 
   // Not available from the first attempt.
   task_environment_.RunUntilIdle();
