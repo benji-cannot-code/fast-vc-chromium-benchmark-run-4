8815ff2990cd238d85f17640d715e994d27bfd80
Mohsen Izadi <mohsen@chromium.org>
2020-09-22 16:17:59 +0000

    Mark WebWidget::ProcessInputEventSynchronously() for testing
    As of r783010, this method is only there to be used in tests. Renaming
    to make this explicitly clear.
    
    Bug: 1079116
    Change-Id: I655950e6c9c7a9b2834f1500eaa6754c8a373a25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2420532
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809360}

--- content/public/test/render_view_test.cc
+++ content/public/test/render_view_test.cc
@@ -589,7 +589,7 @@ void RenderViewTest::SendNativeKeyEvent(
 void RenderViewTest::SendInputEvent(const blink::WebInputEvent& input_event) {
   RenderViewImpl* view = static_cast<RenderViewImpl*>(view_);
   RenderWidget* widget = view->GetMainRenderFrame()->GetLocalRootRenderWidget();
-  widget->GetWebWidget()->ProcessInputEventSynchronously(
+  widget->GetWebWidget()->ProcessInputEventSynchronouslyForTesting(
       blink::WebCoalescedInputEvent(input_event, ui::LatencyInfo()),
       base::DoNothing());
 }
@@ -674,11 +674,11 @@ void RenderViewTest::SimulatePointClick(const gfx::Point& point) {
   mouse_event.click_count = 1;
   RenderViewImpl* view = static_cast<RenderViewImpl*>(view_);
   RenderWidget* widget = view->GetMainRenderFrame()->GetLocalRootRenderWidget();
-  widget->GetWebWidget()->ProcessInputEventSynchronously(
+  widget->GetWebWidget()->ProcessInputEventSynchronouslyForTesting(
       blink::WebCoalescedInputEvent(mouse_event, ui::LatencyInfo()),
       base::DoNothing());
   mouse_event.SetType(WebInputEvent::Type::kMouseUp);
-  widget->GetWebWidget()->ProcessInputEventSynchronously(
+  widget->GetWebWidget()->ProcessInputEventSynchronouslyForTesting(
       blink::WebCoalescedInputEvent(mouse_event, ui::LatencyInfo()),
       base::DoNothing());
 }
@@ -700,11 +700,11 @@ void RenderViewTest::SimulatePointRightClick(const gfx::Point& point) {
   mouse_event.click_count = 1;
   RenderViewImpl* view = static_cast<RenderViewImpl*>(view_);
   RenderWidget* widget = view->GetMainRenderFrame()->GetLocalRootRenderWidget();
-  widget->GetWebWidget()->ProcessInputEventSynchronously(
+  widget->GetWebWidget()->ProcessInputEventSynchronouslyForTesting(
       blink::WebCoalescedInputEvent(mouse_event, ui::LatencyInfo()),
       base::DoNothing());
   mouse_event.SetType(WebInputEvent::Type::kMouseUp);
-  widget->GetWebWidget()->ProcessInputEventSynchronously(
+  widget->GetWebWidget()->ProcessInputEventSynchronouslyForTesting(
       blink::WebCoalescedInputEvent(mouse_event, ui::LatencyInfo()),
       base::DoNothing());
 }
@@ -719,7 +719,7 @@ void RenderViewTest::SimulateRectTap(const gfx::Rect& rect) {
   gesture_event.data.tap.height = rect.height();
   RenderViewImpl* view = static_cast<RenderViewImpl*>(view_);
   RenderWidget* widget = view->GetMainRenderFrame()->GetLocalRootRenderWidget();
-  widget->GetWebWidget()->ProcessInputEventSynchronously(
+  widget->GetWebWidget()->ProcessInputEventSynchronouslyForTesting(
       blink::WebCoalescedInputEvent(gesture_event, ui::LatencyInfo()),
       base::DoNothing());
 }
--- third_party/blink/renderer/core/frame/web_frame_widget_base.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_base.cc
@@ -1144,7 +1144,7 @@ void WebFrameWidgetBase::SetHandlingInputEvent(bool handling) {
   widget_base_->input_handler().set_handling_input_event(handling);
 }
 
-void WebFrameWidgetBase::ProcessInputEventSynchronously(
+void WebFrameWidgetBase::ProcessInputEventSynchronouslyForTesting(
     const WebCoalescedInputEvent& event,
     HandledEventCallback callback) {
   widget_base_->input_handler().HandleInputEvent(event, std::move(callback));
