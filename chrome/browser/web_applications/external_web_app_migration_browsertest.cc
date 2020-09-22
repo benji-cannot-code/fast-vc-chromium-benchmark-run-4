55edf9f29517e20e7eea75e30c4d77e288dfefba
Alan Cutter <alancutter@chromium.org>
2020-09-22 11:20:53 +0000

    Consolidate GetPreinstalledWebApps() as part of ExternalWebAppManager's ScanDir()
    This CL moves the call to GetPreinstalledWebApps() from
    SynchronizeExternalInstallOptions() to ScanDir() in preparation for
    adding metrics for enabled/disabled preinstalled web apps:
    https://chromium-review.googlesource.com/c/chromium/src/+/2418756
    
    This enables the metrics to record for both JSON config files and hard
    coded install options.
    
    As part of this change ScanDir() is not just reading JSON config files
    and has been renamed to LoadInstallOptionsBlocking(). Other methods
    have been renamed to remove the "ScanDir" naming from the vicinity.
    
    Bug: 1128801
    Change-Id: Ic95089c7c56c78f653c8aaa7683683fe2bd8f79f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422022
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809269}

--- chrome/browser/web_applications/external_web_app_migration_browsertest.cc
+++ chrome/browser/web_applications/external_web_app_migration_browsertest.cc
@@ -60,7 +60,7 @@ namespace web_app {
 class ExternalWebAppMigrationBrowserTest : public InProcessBrowserTest {
  public:
   ExternalWebAppMigrationBrowserTest() {
-    ExternalWebAppManager::SkipStartupScanForTesting();
+    ExternalWebAppManager::SkipStartupForTesting();
     disable_scope_ =
         extensions::ExtensionService::DisableExternalUpdatesForTesting();
   }
