960d4be6d59b82ed13cfe3add66b080daf2f5a5f
Owen Min <zmin@chromium.org>
2020-09-22 18:19:17 +0000

    Handle the new enterprise server response code.
    In the past, Conflict(409) are used when
    1) There are multiple device records for one device id.
    2) Database concurrent update exception.
    
    Few months ago, the enterprise server began to return the
    Too Many Request(429) for the second situation above to allow the client
    side to setup different retry strategies.
    
    Policy Fetch and CBCM reporting will retry on 429 but not 409.
    Chrome OS demo mode will show the error message and retry.
    Chrome OS enrollment will record a different UMA value.
    
    Bug: 1108817
    Skip-Translation-Screenshots-Check: True
    Change-Id: I3b302ab03e16a41a30103f809c1b51813647dd7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2419119
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809420}

--- chrome/browser/chromeos/login/demo_mode/demo_setup_controller.h
+++ chrome/browser/chromeos/login/demo_mode/demo_setup_controller.h
@@ -113,6 +113,8 @@ class DemoSetupController
       kDMTokenStoreError,
       // Unexpected/fatal error.
       kUnexpectedError,
+      // Too many requests error.
+      kTooManyRequestsError,
     };
 
     // Type of recommended recovery from the setup error.
