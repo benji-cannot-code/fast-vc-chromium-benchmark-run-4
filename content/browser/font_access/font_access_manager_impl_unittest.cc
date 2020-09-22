e02b95747303e40999ddc74a7dd9b702de28a678
Olivier Yiptong <oyiptong@chromium.org>
2020-09-22 08:33:42 +0000

    FontAccess: Consolidate build configuration macros
    This cleans up the per-platform build configuration preprocessor
    directives. With this change, the platform build configurations are
    consolidated in one place.
    
    Bug: 1043306
    Change-Id: Id9e2da3bf156e245b510ce7b32346df285fd6f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422811
    Commit-Queue: Olivier Yiptong <oyiptong@chromium.org>
    Auto-Submit: Olivier Yiptong <oyiptong@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809240}

--- content/browser/font_access/font_access_manager_impl_unittest.cc
+++ content/browser/font_access/font_access_manager_impl_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/scoped_feature_list.h"
-#include "build/build_config.h"
 #include "content/browser/font_access/font_enumeration_cache.h"
 #include "content/browser/permissions/permission_controller_impl.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
@@ -178,8 +177,7 @@ class FontAccessManagerImplTest : public RenderViewHostImplTestHarness {
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS) || \
-    defined(OS_MAC)
+#if defined(PLATFORM_HAS_LOCAL_FONT_ENUMERATION_IMPL)
 namespace {
 
 void ValidateFontEnumerationBasic(FontEnumerationStatus status,
