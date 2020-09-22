fbd1c1cf68d91549ede368cf67b97835205e4ae8
Shakti Sahu <shaktisahu@chromium.org>
2020-09-22 06:09:41 +0000

    Video Player: Set up untrusted data source for serving HTML/JS
    This CL sets up the video player HTML/JS files to be served locally
    through chrome-untrusted:// scheme. It currently uses a WebUIDataSource
    and will be moved to an untrusted WebUIController factory once it is
    ready.
    
    Bug: 1129715
    Change-Id: I1d514c5f316a462547ef6e1dff78cfecbf143fdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414787
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809209}

---
+++ chrome/browser/resources/video_tutorials/video_player.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {$} from 'chrome://resources/js/util.m.js';
+
+function onDocumentLoaded() {
+  // Find out the video, image, and caption urls from the url params.
+  const urlParams = new URLSearchParams(window.location.search);
+  video.src = urlParams.get('video_url');
+  video.poster = urlParams.get('poster_url');
+  track.src = urlParams.get('caption_url');
+  video.play();
+}
+
+function onVideoEnded() {
+  // Resize the poster.
+  video.style.classList.add('video-ended');
+  video.controls = false;
+}
+
+const video = $('video');
+const track = $('track');
+video.addEventListener('ended', onVideoEnded);
+document.addEventListener('DOMContentLoaded', onDocumentLoaded);
