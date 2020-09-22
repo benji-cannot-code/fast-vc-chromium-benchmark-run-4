926c7558ba23ba1e2fca83d4e9c228398f9c1feb
Angela Yoeurng <yoangela@chromium.org>
2020-09-22 23:52:14 +0000

    [omnibox] Add feature flag for new focus UI
    Bug: 1129612
    Change-Id: I5f3ac715e136a616f2cdd6c9906c1f7cd5998a01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422481
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809582}

--- components/omnibox/browser/omnibox_field_trial.h
+++ components/omnibox/browser/omnibox_field_trial.h
@@ -410,6 +410,9 @@ bool IsKeywordSearchButtonEnabled();
 // assortment of keyword mode experiments.
 bool IsExperimentalKeywordModeEnabled();
 
+// Returns true if the new focus UI is enabled.
+bool IsRefinedFocusStateEnabled();
+
 // Rich autocompletion.
 bool IsRichAutocompletionEnabled();
 bool RichAutocompletionAutocompleteTitles();
