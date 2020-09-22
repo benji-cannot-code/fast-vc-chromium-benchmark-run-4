715bf9dc178c6693e5be030f9b893412a3888c81
Sammie Quon <sammiequon@chromium.org>
2020-09-22 22:32:24 +0000

    desks: Add flag to expose enhanced desk animations.
    Test: none
    Bug: 1111445
    Change-Id: I8ee9546149c0c052677f897efedd7f950cd95c70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417370
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809542}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -6417,6 +6417,10 @@ const FeatureEntry kFeatureEntries[] = {
     {"enable-holding-space", flag_descriptions::kHoldingSpaceName,
      flag_descriptions::kHoldingSpaceDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(ash::features::kTemporaryHoldingSpace)},
+
+    {"enhanced-desk-animations", flag_descriptions::kEnhancedDeskAnimationsName,
+     flag_descriptions::kEnhancedDeskAnimationsDescription, kOsCrOS,
+     FEATURE_VALUE_TYPE(ash::features::kEnhancedDeskAnimations)},
 #endif
 
 #if defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) || \
