70e275c7f404384f9c6280b8ee4dc6e20d79e2cd
Leonard Grey <lgrey@chromium.org>
2020-09-22 15:19:25 +0000

    Revert "Remove updatesCollectionView from grid view controller."
    This reverts commit 28310fb55130113c2009d88431728a4b3ec2ccf1.
    
    Reason for revert: [Sheriff] Failing deterministically on ios-simulator-noncq
    Example: https://ci.chromium.org/p/chromium/builders/ci/ios-simulator-noncq/12130
    
    Original change's description:
    > Remove updatesCollectionView from grid view controller.
    > 
    > In order to allow the tab thumbnails to be up to date when the thumb
    > strip is visible, the grid's collection view needs to be updated even
    > when other view controllers are obscuring it. When selecting a new item
    > from the collection view (blue highlight around the thumbnail), the item
    > that is currently selected needs to be deselected to prevent multiple
    > simultaneous selection.
    > 
    > Bug: 1127604
    > Change-Id: Ic1e4a2dd4d0f2c269031b9d27b47b38c56b07a40
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410494
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Roberto Moura <mouraroberto@google.com>
    > Auto-Submit: Roberto Moura <mouraroberto@google.com>
    > Cr-Commit-Position: refs/heads/master@{#809263}
    
    TBR=gambard@chromium.org,mouraroberto@google.com
    
    Change-Id: Ia7c8b2a2946424754838f5d2fdcd76d203745e59
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1127604
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2423170
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809337}

--- ios/chrome/browser/web/browsing_egtest.mm
+++ ios/chrome/browser/web/browsing_egtest.mm
@@ -78,13 +78,12 @@ class ReloadResponseProvider : public web::DataResponseProvider {
 
 @implementation BrowsingTestCase
 
-// Matcher for the title of the current tab (on tablet only), which is
-// sufficiently visible.
+// Matcher for the title of the current tab (on tablet only).
 id<GREYMatcher> TabWithTitle(const std::string& tab_title) {
   id<GREYMatcher> notPartOfOmnibox =
       grey_not(grey_ancestor(chrome_test_util::Omnibox()));
   return grey_allOf(grey_accessibilityLabel(base::SysUTF8ToNSString(tab_title)),
-                    notPartOfOmnibox, grey_sufficientlyVisible(), nil);
+                    notPartOfOmnibox, nil);
 }
 
 // Tests that page successfully reloads.
@@ -122,7 +121,7 @@ id<GREYMatcher> TabWithTitle(const std::string& tab_title) {
       destinationURL.spec().substr(destinationURL.scheme().length() + 3);
 
   [[EarlGrey selectElementWithMatcher:TabWithTitle(URLWithoutScheme)]
-      assertWithMatcher:grey_notNil()];
+      assertWithMatcher:grey_sufficientlyVisible()];
 }
 
 // Tests that after a PDF is loaded, the title appears in the tab bar on iPad.
@@ -140,7 +139,7 @@ id<GREYMatcher> TabWithTitle(const std::string& tab_title) {
       destinationURL.spec().substr(destinationURL.scheme().length() + 3);
 
   [[EarlGrey selectElementWithMatcher:TabWithTitle(URLWithoutScheme)]
-      assertWithMatcher:grey_notNil()];
+      assertWithMatcher:grey_sufficientlyVisible()];
 }
 
 // Tests that tab title is set to the specified title from a JavaScript.
@@ -157,7 +156,7 @@ id<GREYMatcher> TabWithTitle(const std::string& tab_title) {
   [ChromeEarlGrey loadURL:URL];
 
   [[EarlGrey selectElementWithMatcher:TabWithTitle(kPageTitle)]
-      assertWithMatcher:grey_notNil()];
+      assertWithMatcher:grey_sufficientlyVisible()];
 }
 
 // Tests clicking a link with target="_blank" and "event.stopPropagation()"
