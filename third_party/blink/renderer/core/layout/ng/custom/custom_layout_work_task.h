6b0cb2ba717038a09705ba13061fd3cf43b439ce
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-09-22 06:12:58 +0000

    [css-layout-api] Fix using an object which has been reallocated.
    Per: crbug.com/1119873
    
    It was possible to mutate the work queue while iterating over it. Using
    this it was possible to trigger a UAF.
    
    This patch converts CustomLayoutWorkTask to oilpan (not strictly
    required as a copy of the CustomLayoutWorkTask would have also
    sufficed), and now copies the Member<CustomLayoutWorkTask> before using
    it.
    
    Bug: 1119873
    Change-Id: I3c66859af8c9a0f33fe8c7df7c30efd2913c2985
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2380135
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809215}

--- third_party/blink/renderer/core/layout/ng/custom/custom_layout_work_task.h
+++ third_party/blink/renderer/core/layout/ng/custom/custom_layout_work_task.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_custom_layout_constraints_options.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/heap/heap.h"
 
 namespace blink {
 
@@ -23,7 +24,8 @@ class ScriptPromiseResolver;
 
 // Contains all the information needed to resolve a promise with a fragment or
 // intrinsic-sizes.
-class CustomLayoutWorkTask {
+class CustomLayoutWorkTask final
+    : public GarbageCollected<CustomLayoutWorkTask> {
  public:
   enum TaskType {
     kLayoutFragment,
@@ -44,6 +46,7 @@ class CustomLayoutWorkTask {
                        scoped_refptr<SerializedScriptValue> constraint_data,
                        const TaskType type);
   ~CustomLayoutWorkTask();
+  void Trace(Visitor*) const;
 
   // Runs this work task.
   void Run(const NGConstraintSpace& parent_space,
@@ -52,10 +55,10 @@ class CustomLayoutWorkTask {
            bool* child_depends_on_percentage_block_size = nullptr);
 
  private:
-  Persistent<CustomLayoutChild> child_;
-  Persistent<CustomLayoutToken> token_;
-  Persistent<ScriptPromiseResolver> resolver_;
-  Persistent<const CustomLayoutConstraintsOptions> options_;
+  Member<CustomLayoutChild> child_;
+  Member<CustomLayoutToken> token_;
+  Member<ScriptPromiseResolver> resolver_;
+  Member<const CustomLayoutConstraintsOptions> options_;
   scoped_refptr<SerializedScriptValue> constraint_data_;
   TaskType type_;
 
