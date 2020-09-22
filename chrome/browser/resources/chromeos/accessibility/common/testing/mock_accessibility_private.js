5ba3922cd242cc5a835ebcfe74f000c9e1f48ac6
Josiah K <josiahk@google.com>
2020-09-22 23:21:11 +0000

    Rename findScrollableBoundsForPoint to onScrollableBoundsForPointRequested
    (Fixing accessibilityPrivate.on* names to only be used for events 2/3)
    
    No functional changes - should be mechanical only.
    
    AX-Relnotes: N/A
    Change-Id: Ie31e44e0cdcce932865f2039c4cac5f9b6f9cb38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422651
    Auto-Submit: Josiah Krutz <josiahk@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809574}

--- chrome/browser/resources/chromeos/accessibility/common/testing/mock_accessibility_private.js
+++ chrome/browser/resources/chromeos/accessibility/common/testing/mock_accessibility_private.js
@@ -23,9 +23,9 @@ var MockAccessibilityPrivate = {
 
   // Methods from AccessibilityPrivate API. //
 
-  findScrollableBoundsForPoint: {
+  onScrollableBoundsForPointRequested: {
     /**
-     * Adds a listener to findScrollableBoundsForPoint.
+     * Adds a listener to onScrollableBoundsForPointRequested.
      * @param {function<number, number>} listener
      */
     addListener: (listener) => {
@@ -74,7 +74,7 @@ var MockAccessibilityPrivate = {
    * @param {number} y
    * @param {!function<>} onScrollableBoundsForPointFoundCallback
    */
-  callFindScrollableBoundsForPoint:
+  callOnScrollableBoundsForPointRequested:
       (x, y, onScrollableBoundsForPointFoundCallback) => {
         onScrollableBoundsForPointFoundCallback_ =
             onScrollableBoundsForPointFoundCallback;
