3ad820a9a1fc484edb329950a2f2b8426e31efd7
chinsenj <chinsenj@chromium.org>
2020-09-22 01:41:18 +0000

    cros: Fix window cycle list (alt-tab) resets if open during desk swap.
    Original CL dc64daa2404b28fda65211362e1d832a6cad5f7f was reverted by
    a326c157c3c3016060dc6a421c42ce9993294e09 due to a container-overflow.
    This CL fixes the container-flow.
    
    Original commit message:
    
    While the flag for limiting windows to the current active desk is
    enabled, if a user swaps desks while the window cycle list is open, the
    contents will not update to the new active desk.
    
    This CL makes the window cycle list reset if it is open and a desk
    swap occurs while the aforementioned flag is enabled.
    
    Test: LimitedWindowCycleControllerTest.CycleShowsActiveDeskWindows
    Bug: 1067327
    Change-Id: I9dddd0e780db33f5b727770583e0517f12eecece
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417213
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Jeremy Chinsen <chinsenj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809160}

--- ash/wm/desks/desks_controller.cc
+++ ash/wm/desks/desks_controller.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_item.h"
 #include "ash/wm/splitview/split_view_controller.h"
 #include "ash/wm/splitview/split_view_utils.h"
+#include "ash/wm/window_cycle_controller.h"
 #include "ash/wm/window_util.h"
 #include "base/auto_reset.h"
 #include "base/check_op.h"
@@ -536,6 +537,13 @@ void DesksController::ActivateDeskInternal(const Desk* desk,
   active_desk_->Activate(update_window_activation);
 
   MaybeUpdateShelfItems(old_active->windows(), active_desk_->windows());
+
+  // If in the middle of a window cycle gesture, reset the window cycle list
+  // contents so it contains the new active desk's windows.
+  if (features::IsAltTabLimitedToActiveDesk()) {
+    auto* window_cycle_controller = Shell::Get()->window_cycle_controller();
+    window_cycle_controller->MaybeResetCycleList();
+  }
 
   for (auto& observer : observers_)
     observer.OnDeskActivationChanged(active_desk_, old_active);
