cd7006d2ee223b89da8f2bedd7fcc0f123ccdabe
Shakti Sahu <shaktisahu@chromium.org>
2020-09-22 00:32:42 +0000

    [Video Tutorials] Prefs.
    - Add Keys for preferred language and last updated time.
    - Register to PrefService.
    
    Change-Id: I844b4897ade5478603e386c9b4d615ed53b3c03d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2394518
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809137}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -223,6 +223,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/first_run/android/first_run_prefs.h"
 #include "chrome/browser/media/android/cdm/media_drm_origin_id_manager.h"
 #include "chrome/browser/ssl/known_interception_disclosure_infobar_delegate.h"
+#include "chrome/browser/video_tutorials/prefs.h"
 #include "components/cdm/browser/media_drm_storage_impl.h"  // nogncheck crbug.com/1125897
 #include "components/feed/buildflags.h"
 #include "components/feed/core/shared_prefs/pref_names.h"
@@ -623,7 +624,7 @@ void RegisterLocalState(PrefRegistrySimple* registry) {
   ::android::RegisterPrefs(registry);
 
   registry->RegisterBooleanPref(first_run::kCCTToSDialogEnabled, true);
-#else   // defined(OS_ANDROID)
+#else  // defined(OS_ANDROID)
   enterprise_connectors::RegisterLocalStatePrefs(registry);
   enterprise_reporting::RegisterLocalStatePrefs(registry);
   gcm::RegisterPrefs(registry);
@@ -881,6 +882,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
   RecentTabsPagePrefs::RegisterProfilePrefs(registry);
   usage_stats::UsageStatsBridge::RegisterProfilePrefs(registry);
   variations::VariationsService::RegisterProfilePrefs(registry);
+  video_tutorials::RegisterPrefs(registry);
   feed::prefs::RegisterFeedSharedProfilePrefs(registry);
 #if BUILDFLAG(ENABLE_FEED_IN_CHROME)
   feed::RegisterProfilePrefs(registry);
