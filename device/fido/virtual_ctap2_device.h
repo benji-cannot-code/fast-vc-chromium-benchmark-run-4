211de44730ec3cc94bb56a16b564c0e8717d2446
Nina Satragno <nsatragno@chromium.org>
2020-09-22 21:50:48 +0000

    [fido] Large blob PinUvAuthToken support
    Add support for PinUvAuthToken on large blobs.
    
    https://w3c.github.io/webauthn/#sctn-large-blob-extension
    
    Bug: 1114875
    Change-Id: I57a094eed4cb0cd3d8b20722804826a46ccf0e27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422707
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#809523}

--- device/fido/virtual_ctap2_device.h
+++ device/fido/virtual_ctap2_device.h
@@ -186,6 +186,9 @@ class COMPONENT_EXPORT(DEVICE_FIDO) VirtualCtap2Device
   VirtualCtap2Device(scoped_refptr<State> state, const Config& config);
   ~VirtualCtap2Device() override;
 
+  // Configures and sets a PIN on the authenticator.
+  void SetPin(std::string pin);
+
   // FidoDevice:
   void Cancel(CancelToken) override;
   CancelToken DeviceTransact(std::vector<uint8_t> command,
