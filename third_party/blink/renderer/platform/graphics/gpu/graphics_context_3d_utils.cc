44f2c5884e198ed5434514fed427a430b8fa77fe
Adlai Holler <adlai@google.com>
2020-09-22 17:56:55 +0000

    Remove GrContext references from blink
    Change-Id: Ic258eb1562dc776805d1530c480493b0739ffba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2423062
    Auto-Submit: Adlai Holler <adlai@google.com>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809408}

--- third_party/blink/renderer/platform/graphics/gpu/graphics_context_3d_utils.cc
+++ third_party/blink/renderer/platform/graphics/gpu/graphics_context_3d_utils.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.h"
 #include "third_party/blink/renderer/platform/graphics/web_graphics_context_3d_provider_wrapper.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
-#include "third_party/skia/include/core/SkImage.h"
-#include "third_party/skia/include/gpu/GrContext.h"
 
 namespace blink {
 
--- third_party/blink/renderer/platform/graphics/static_bitmap_image.cc
+++ third_party/blink/renderer/platform/graphics/static_bitmap_image.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkImage.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkSurface.h"
-#include "third_party/skia/include/gpu/GrContext.h"
 #include "v8/include/v8.h"
 
 namespace blink {
