56fb4cf59584d0cddb7c71312eda64376ca6a358
Mike Wittman <wittman@chromium.org>
2020-09-22 02:20:07 +0000

    [Sampling profiler] Move ENABLE_ARM_CFI_TABLE to profiler header
    ENABLE_ARM_CFI_TABLE is in service of the sampling profiler on Android,
    and is unrelated to any debugging functionality, so move it to a
    buildflag header under base/profiler.
    
    Also add the required includes to the files that use it.
    
    Bug: 1129939
    Change-Id: I884b2128fc2ab2f6f1bec2343c59134448328529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2421443
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809167}

--- base/profiler/stack_sampling_profiler_test_util.cc
+++ base/profiler/stack_sampling_profiler_test_util.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/location.h"
 #include "base/path_service.h"
+#include "base/profiler/profiler_buildflags.h"
 #include "base/profiler/stack_buffer.h"
 #include "base/profiler/stack_sampling_profiler.h"
 #include "base/profiler/unwinder.h"
