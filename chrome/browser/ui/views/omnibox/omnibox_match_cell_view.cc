5589521ca3fe0167fe8ad1bd26456157bf2b917d
Thanh Nguyen <thanhdng@chromium.org>
2020-09-22 01:00:24 +0000

    Fix icon issues for FAR recommendations
    1. Adds an icon as the badge icon for the FAR recommendations
    2. Make the icon consistent with arc playstore search result.
    3. Move EncircledImageSource to ImageSkiaOperations so that it can be
    reused (no logic changed)
    
    Bug: 1028373
    Change-Id: Ieabf6f39e63472f6d1b0eee70b6bc9841dbbd13f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2402472
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809149}

--- chrome/browser/ui/views/omnibox/omnibox_match_cell_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_match_cell_view.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/image/canvas_image_source.h"
+#include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/render_text.h"
 #include "ui/views/border.h"
@@ -68,44 +69,6 @@ void PlaceholderImageSource::Draw(gfx::Canvas* canvas) {
       views::EMPHASIS_MEDIUM);
   canvas->sk_canvas()->drawRoundRect(gfx::RectToSkRect(gfx::Rect(size())),
                                      corner_radius, corner_radius, flags);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// EncircledImageSource:
-
-class EncircledImageSource : public gfx::CanvasImageSource {
- public:
-  EncircledImageSource(int radius, SkColor color, const gfx::ImageSkia& image);
-  ~EncircledImageSource() override = default;
-
-  // gfx::CanvasImageSource:
-  void Draw(gfx::Canvas* canvas) override;
-
- private:
-  const int radius_;
-  const SkColor color_;
-  const gfx::ImageSkia image_;
-
-  DISALLOW_COPY_AND_ASSIGN(EncircledImageSource);
-};
-
-EncircledImageSource::EncircledImageSource(int radius,
-                                           SkColor color,
-                                           const gfx::ImageSkia& image)
-    : gfx::CanvasImageSource(gfx::Size(radius * 2, radius * 2)),
-      radius_(radius),
-      color_(color),
-      image_(image) {}
-
-void EncircledImageSource::Draw(gfx::Canvas* canvas) {
-  cc::PaintFlags flags;
-  flags.setAntiAlias(true);
-  flags.setStyle(cc::PaintFlags::kFill_Style);
-  flags.setColor(color_);
-  canvas->DrawCircle(gfx::Point(radius_, radius_), radius_, flags);
-  const int x = radius_ - image_.width() / 2;
-  const int y = radius_ - image_.height() / 2;
-  canvas->DrawImageInt(image_, x, y);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -185,7 +148,7 @@ void OmniboxMatchCellView::OnMatchUpdate(const OmniboxResultView* result_view,
     answer_image_view_->SetImageSize(
         gfx::Size(kAnswerImageSize, kAnswerImageSize));
     answer_image_view_->SetImage(
-        gfx::CanvasImageSource::MakeImageSkia<EncircledImageSource>(
+        gfx::ImageSkiaOperations::CreateImageWithCircleBackground(
             kAnswerImageSize / 2, gfx::kGoogleBlue600, icon));
   };
   if (match.type == AutocompleteMatchType::CALCULATOR) {
