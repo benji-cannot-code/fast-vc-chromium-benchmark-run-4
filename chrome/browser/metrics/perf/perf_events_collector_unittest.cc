95393cf8bbe8c18fe2e3e6bef081bd805bda805d
Gabriel Marin <gmx@chromium.org>
2020-09-23 18:57:47 +0000

    Collect cycles precise=3 where available.
    Precise attribution at instruction level helps to better understand hostspots
    in assembly view.
    
    Updated a few unit tests to check for the right UMA metric being incremented.
    
    BUG=b:168312716
    
    Change-Id: I537395d5ec6714c75b4224765ebc247a6ba00ff3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2424517
    Reviewed-by: Gabriel Marin <gmx@chromium.org>
    Reviewed-by: George Burgess <gbiv@chromium.org>
    Commit-Queue: Gabriel Marin <gmx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809884}

--- chrome/browser/metrics/perf/perf_events_collector_unittest.cc
+++ chrome/browser/metrics/perf/perf_events_collector_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/task/post_task.h"
 #include "base/test/bind_test_util.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/metrics/perf/cpu_identity.h"
 #include "chrome/browser/metrics/perf/windowed_incognito_observer.h"
 #include "components/variations/variations_associated_data.h"
@@ -33,6 +34,11 @@ const char kPerfCyclesCmd[] = "perf record -a -e cycles -c 1000003";
 const char kPerfFPCallgraphCmd[] = "perf record -a -e cycles -g -c 4000037";
 const char kPerfLBRCallgraphCmd[] =
     "perf record -a -e cycles -c 4000037 --call-graph lbr";
+const char kPerfCyclesPPPCmd[] = "perf record -a -e cycles:ppp -c 1000003";
+const char kPerfFPCallgraphPPPCmd[] =
+    "perf record -a -e cycles:ppp -g -c 4000037";
+const char kPerfLBRCallgraphPPPCmd[] =
+    "perf record -a -e cycles:ppp -c 4000037 --call-graph lbr";
 const char kPerfLBRCmd[] = "perf record -a -e r20c4 -b -c 200011";
 const char kPerfLBRCmdAtom[] = "perf record -a -e rc4 -b -c 300001";
 const char kPerfITLBMissCyclesCmdIvyBridge[] =
@@ -151,6 +157,7 @@ class TestPerfCollector : public PerfCollector {
  public:
   TestPerfCollector() = default;
 
+  using MetricCollector::CollectionAttemptStatus;
   using MetricCollector::CollectPerfDataAfterSessionRestore;
   using MetricCollector::OnJankStarted;
   using MetricCollector::OnJankStopped;
@@ -265,11 +272,15 @@ TEST_F(PerfCollectorTest, NoCollectionWhenProfileCacheFull) {
   EXPECT_TRUE(perf_collector_->IsRunning());
   // Pretend the cache is full.
   perf_collector_->AddCachedDataDelta(4 * 1024 * 1024);
+  base::HistogramTester histogram_tester;
 
   // Advance the clock by a periodic collection interval. We shouldn't find a
   // profile because the cache is full.
   task_environment_.FastForwardBy(kPeriodicCollectionInterval);
   EXPECT_TRUE(cached_profile_data_.empty());
+  histogram_tester.ExpectUniqueSample(
+      "ChromeOS.CWP.CollectPerf",
+      TestPerfCollector::CollectionAttemptStatus::NOT_READY_TO_COLLECT, 1);
 }
 
 // Simulate opening and closing of incognito window in between calls to
@@ -305,6 +316,7 @@ TEST_F(PerfCollectorTest, IncognitoWindowOpened) {
   EXPECT_GT(profile1.cpu_max_frequency_mhz_size(), 0);
   cached_profile_data_.clear();
 
+  base::HistogramTester histogram_tester;
   sampled_profile = std::make_unique<SampledProfile>();
   sampled_profile->set_trigger_event(SampledProfile::RESUME_FROM_SUSPEND);
   // An incognito window opens.
@@ -315,6 +327,9 @@ TEST_F(PerfCollectorTest, IncognitoWindowOpened) {
   task_environment_.RunUntilIdle();
 
   EXPECT_TRUE(cached_profile_data_.empty());
+  histogram_tester.ExpectUniqueSample(
+      "ChromeOS.CWP.CollectPerf",
+      TestPerfCollector::CollectionAttemptStatus::INCOGNITO_LAUNCHED, 1);
 
   sampled_profile = std::make_unique<SampledProfile>();
   sampled_profile->set_trigger_event(SampledProfile::RESUME_FROM_SUSPEND);
@@ -484,10 +499,10 @@ TEST_F(PerfCollectorTest, DefaultCommandsBasedOnUarch_Tigerlake) {
   std::vector<RandomSelector::WeightAndValue> cmds =
       internal::GetDefaultCommandsForCpu(cpuid);
   ASSERT_GE(cmds.size(), 3UL);
-  EXPECT_EQ(cmds[0].value, kPerfCyclesCmd);
+  EXPECT_EQ(cmds[0].value, kPerfCyclesPPPCmd);
   // We have both FP and LBR based callstacks.
-  EXPECT_EQ(cmds[1].value, kPerfFPCallgraphCmd);
-  EXPECT_EQ(cmds[2].value, kPerfLBRCallgraphCmd);
+  EXPECT_EQ(cmds[1].value, kPerfFPCallgraphPPPCmd);
+  EXPECT_EQ(cmds[2].value, kPerfLBRCallgraphPPPCmd);
   auto found =
       std::find_if(cmds.begin(), cmds.end(),
                    [](const RandomSelector::WeightAndValue& cmd) -> bool {
@@ -517,13 +532,13 @@ TEST_F(PerfCollectorTest, DefaultCommandsBasedOnUarch_Goldmont) {
   std::vector<RandomSelector::WeightAndValue> cmds =
       internal::GetDefaultCommandsForCpu(cpuid);
   ASSERT_GE(cmds.size(), 2UL);
-  EXPECT_EQ(cmds[0].value, kPerfCyclesCmd);
-  EXPECT_EQ(cmds[1].value, kPerfFPCallgraphCmd);
+  EXPECT_EQ(cmds[0].value, kPerfCyclesPPPCmd);
+  EXPECT_EQ(cmds[1].value, kPerfFPCallgraphPPPCmd);
   // No LBR callstacks because the microarchitecture doesn't support it.
   auto found =
       std::find_if(cmds.begin(), cmds.end(),
                    [](const RandomSelector::WeightAndValue& cmd) -> bool {
-                     return cmd.value == kPerfLBRCallgraphCmd;
+                     return cmd.value == kPerfLBRCallgraphPPPCmd;
                    });
   EXPECT_EQ(cmds.end(), found);
   found = std::find_if(cmds.begin(), cmds.end(),
@@ -554,13 +569,13 @@ TEST_F(PerfCollectorTest, DefaultCommandsBasedOnUarch_GoldmontPlus) {
   std::vector<RandomSelector::WeightAndValue> cmds =
       internal::GetDefaultCommandsForCpu(cpuid);
   ASSERT_GE(cmds.size(), 2UL);
-  EXPECT_EQ(cmds[0].value, kPerfCyclesCmd);
-  EXPECT_EQ(cmds[1].value, kPerfFPCallgraphCmd);
+  EXPECT_EQ(cmds[0].value, kPerfCyclesPPPCmd);
+  EXPECT_EQ(cmds[1].value, kPerfFPCallgraphPPPCmd);
   // No LBR callstacks because the microarchitecture doesn't support it.
   auto found =
       std::find_if(cmds.begin(), cmds.end(),
                    [](const RandomSelector::WeightAndValue& cmd) -> bool {
-                     return cmd.value == kPerfLBRCallgraphCmd;
+                     return cmd.value == kPerfLBRCallgraphPPPCmd;
                    });
   EXPECT_EQ(cmds.end(), found);
   found = std::find_if(cmds.begin(), cmds.end(),
