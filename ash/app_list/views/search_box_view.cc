22cca58762ce43470a4fc0c071f507f83ea02523
Yulun Wu <yulunwu@chromium.org>
2020-09-23 18:20:04 +0000

    Fix issue where cycling search box box state caused incorrect coloring.
    ZergoQuerySearchBoxColor should be deprecated and
    kSearchBoxBackgroundDefault should not be used outside of the
    SearchBoxViewBase constructor.
    
    Bug: 1131223
    Change-Id: I1f2497618d3c3bfd0fdb93c6b0f162873a2b2111
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2425623
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809866}

--- ash/app_list/views/search_box_view.cc
+++ ash/app_list/views/search_box_view.cc
@@ -337,6 +337,8 @@ void SearchBoxView::UpdateBackground(double progress,
       progress, GetBackgroundColorForState(current_state),
       GetBackgroundColorForState(target_state));
   UpdateBackgroundColor(color);
+  search_box()->SetTextColor(
+      AppListColorProvider::Get()->GetSearchBoxTextColor());
 }
 
 void SearchBoxView::UpdateLayout(double progress,
