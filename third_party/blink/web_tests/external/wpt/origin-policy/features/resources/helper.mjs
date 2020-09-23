f57b2ca7ddfb28a1f547f76221f9e407c1336719
Ian Clelland <iclelland@chromium.org>
2020-09-23 16:28:11 +0000

    Invert use of feature policy in Origin Policy WPT.
    With an upcoming change to Feature/Permissions Policy, the header
    alone will no longer be sufficient to delegate permissions to use
    powerful features in subframes. The iframe allow attribute must
    be used in conjunction with it. The header can still be used to
    block delegation.
    
    This change updates the Origin Policy WPTs to use the header to block
    features, rather than to implicitly allow them, which as a test
    mechanism, will work both before and after the behaviour change.
    
    Bug: 1095641
    Change-Id: I3e40480a23ebaea942df661b9a3ff233d108e495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2425003
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809801}

--- third_party/blink/web_tests/external/wpt/origin-policy/features/resources/helper.mjs
+++ third_party/blink/web_tests/external/wpt/origin-policy/features/resources/helper.mjs
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 export function runFPTest({ camera, geolocation }) {
   test(() => {
-    assert_equals(document.featurePolicy.allowsFeature('camera', 'https://example.com/'), camera, 'camera');
-    assert_equals(document.featurePolicy.allowsFeature('geolocation', 'https://example.com/'), geolocation, 'geolocation');
+    assert_equals(document.featurePolicy.allowsFeature('camera'), camera, 'camera');
+    assert_equals(document.featurePolicy.allowsFeature('geolocation'), geolocation, 'geolocation');
   });
 }
