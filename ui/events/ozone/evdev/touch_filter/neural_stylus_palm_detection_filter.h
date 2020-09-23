96c6c8172224188bf89601f7c62d0ede510412cc
Lei Zhang <thestig@chromium.org>
2020-09-23 16:30:26 +0000

    Remove static initializer in NeuralStylusPalmDetectionFilter.
    Reduce the scode of `kRequiredAbsMtCodes` to a local scope, so it is
    initialized on first use.
    
    Also remove an unneeded included.
    
    Bug: 537099
    Change-Id: I984919c41d51a04ecbe6a85ddc56bffa9db833ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2419916
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809805}

--- ui/events/ozone/evdev/touch_filter/neural_stylus_palm_detection_filter.h
+++ ui/events/ozone/evdev/touch_filter/neural_stylus_palm_detection_filter.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <bitset>
 #include <cstdint>
-#include <deque>
 #include <map>
 #include <memory>
 #include <string>
@@ -94,8 +93,6 @@ class COMPONENT_EXPORT(EVDEV) NeuralStylusPalmDetectionFilter
   int tracking_ids_[kNumTouchEvdevSlots];
   const PalmFilterDeviceInfo palm_filter_dev_info_;
   std::unique_ptr<NeuralStylusPalmDetectionFilterModel> model_;
-
-  static const std::vector<int> kRequiredAbsMtCodes;
 
   DISALLOW_COPY_AND_ASSIGN(NeuralStylusPalmDetectionFilter);
 };
