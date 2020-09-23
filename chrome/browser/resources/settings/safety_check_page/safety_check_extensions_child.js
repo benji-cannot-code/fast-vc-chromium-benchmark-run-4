f2f5a86451977a4a72732707a0cae8c85efde390
Xinghui Lu <xinghuilu@chromium.org>
2020-09-23 20:51:18 +0000

    Replace the hairline button with the window icon in the extension row.
    See the discussion thread linked in https://crbug.com/1103015#c15.
    before: http://screen/5TxkBPCY3kXKtCa
    after: http://screen/9kcGmdc3ptJp4q7
    
    Bug: 1103015
    Change-Id: I0a074e8023be0fd1455d566ff508d128994ecdb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422713
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809943}

--- chrome/browser/resources/settings/safety_check_page/safety_check_extensions_child.js
+++ chrome/browser/resources/settings/safety_check_page/safety_check_extensions_child.js
@@ -64,6 +64,7 @@ Polymer({
       value: () => new Set([
         SafetyCheckExtensionsStatus.NO_BLOCKLISTED_EXTENSIONS,
         SafetyCheckExtensionsStatus.ERROR,
+        SafetyCheckExtensionsStatus.BLOCKLISTED_ALL_DISABLED,
       ]),
     },
   },
@@ -118,26 +119,11 @@ Polymer({
    */
   getButtonLabel_: function() {
     switch (this.status_) {
-      case SafetyCheckExtensionsStatus.BLOCKLISTED_ALL_DISABLED:
       case SafetyCheckExtensionsStatus.BLOCKLISTED_REENABLED_ALL_BY_USER:
       case SafetyCheckExtensionsStatus.BLOCKLISTED_REENABLED_SOME_BY_USER:
         return this.i18n('safetyCheckReview');
       default:
         return null;
-    }
-  },
-
-  /**
-   * @private
-   * @return {string}
-   */
-  getButtonClass_: function() {
-    switch (this.status_) {
-      case SafetyCheckExtensionsStatus.BLOCKLISTED_REENABLED_ALL_BY_USER:
-      case SafetyCheckExtensionsStatus.BLOCKLISTED_REENABLED_SOME_BY_USER:
-        return 'action-button';
-      default:
-        return '';
     }
   },
 
