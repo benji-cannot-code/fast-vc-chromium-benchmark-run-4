cbf6ea967da412ac86ecbf0baa7ac81fe6b24567
Gabriel Marin <gmx@chromium.org>
2020-09-23 15:33:41 +0000

    Export a count of profile sessions with zero samples.
    These are indicative of unsupported perf record commands.
    
    BUG=b:169068625
    
    Change-Id: Id04dd286cd77ab0ee6d04f9e3e3ff3986802c878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2424710
    Reviewed-by: Gabriel Marin <gmx@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Gabriel Marin <gmx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809780}

--- chrome/browser/metrics/perf/metric_collector_unittest.cc
+++ chrome/browser/metrics/perf/metric_collector_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/task/post_task.h"
 #include "base/test/bind_test_util.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -79,9 +80,9 @@ class TestMetricCollector : public MetricCollector {
  public:
   TestMetricCollector() : TestMetricCollector(CollectionParams()) {}
   explicit TestMetricCollector(const CollectionParams& collection_params)
-      : MetricCollector("UMA.CWP.TestData", collection_params) {}
+      : MetricCollector("Test", collection_params) {}
 
-  const char* ToolName() const override { return "test"; }
+  const char* ToolName() const override { return "Test"; }
   base::WeakPtr<MetricCollector> GetWeakPtr() override {
     return weak_factory_.GetWeakPtr();
   }
@@ -94,6 +95,7 @@ class TestMetricCollector : public MetricCollector {
   }
 
   using MetricCollector::collection_params;
+  using MetricCollector::CollectionAttemptStatus;
   using MetricCollector::CurrentTimerDelay;
   using MetricCollector::Init;
   using MetricCollector::IsRunning;
@@ -180,22 +182,50 @@ TEST_F(MetricCollectorTest, CheckSetup) {
 TEST_F(MetricCollectorTest, EmptyProtosAreNotSaved) {
   auto sampled_profile = std::make_unique<SampledProfile>();
   sampled_profile->set_trigger_event(SampledProfile::PERIODIC_COLLECTION);
+  base::HistogramTester histogram_tester;
 
   metric_collector_->SaveSerializedPerfProto(std::move(sampled_profile),
                                              std::string());
   task_environment_.RunUntilIdle();
 
   EXPECT_TRUE(cached_profile_data_.empty());
+  histogram_tester.ExpectUniqueSample(
+      "ChromeOS.CWP.CollectTest",
+      TestMetricCollector::CollectionAttemptStatus::ILLEGAL_DATA_RETURNED, 1);
+}
+
+TEST_F(MetricCollectorTest, ProtosWithNoSamplesAreNotSaved) {
+  auto sampled_profile = std::make_unique<SampledProfile>();
+  sampled_profile->set_trigger_event(SampledProfile::PERIODIC_COLLECTION);
+  base::HistogramTester histogram_tester;
+
+  PerfDataProto proto = GetExamplePerfDataProto();
+  PerfDataProto_PerfEventStats* stats = proto.mutable_stats();
+  stats->set_num_sample_events(0);
+
+  metric_collector_->SaveSerializedPerfProto(std::move(sampled_profile),
+                                             proto.SerializeAsString());
+  task_environment_.RunUntilIdle();
+
+  EXPECT_TRUE(cached_profile_data_.empty());
+  histogram_tester.ExpectUniqueSample(
+      "ChromeOS.CWP.CollectTest",
+      TestMetricCollector::CollectionAttemptStatus::SESSION_HAS_ZERO_SAMPLES,
+      1);
 }
 
 TEST_F(MetricCollectorTest, PerfDataProto) {
   auto sampled_profile = std::make_unique<SampledProfile>();
   sampled_profile->set_trigger_event(SampledProfile::PERIODIC_COLLECTION);
+  base::HistogramTester histogram_tester;
 
   metric_collector_->SaveSerializedPerfProto(
       std::move(sampled_profile), perf_data_proto_.SerializeAsString());
   task_environment_.RunUntilIdle();
   ASSERT_EQ(1U, cached_profile_data_.size());
+  histogram_tester.ExpectUniqueSample(
+      "ChromeOS.CWP.CollectTest",
+      TestMetricCollector::CollectionAttemptStatus::SUCCESS, 1);
 
   const SampledProfile& profile = cached_profile_data_[0];
   EXPECT_EQ(SampledProfile::PERIODIC_COLLECTION, profile.trigger_event());
