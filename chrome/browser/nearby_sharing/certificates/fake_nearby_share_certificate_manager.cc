8224f6ab90871e98dde2dbc82666c1666c041101
Michael Hansen <hansenmichael@google.com>
2020-09-23 20:14:26 +0000

    [Nearby] Periodically rotate Nearby Connections advertisement.
    This adds a mechanism to rotate the Nearby Connections advertisement
    and Bluetooth MAC address every [12, 15) minutes. This is to protect
    user privacy by preventing tracking using this data.
    
    Spec: go/nearby-chrome-bt
    
    Bug: b:154846485
    Change-Id: I841aa103fb374a4812ab928bc862af026835a03c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2419692
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809928}

--- chrome/browser/nearby_sharing/certificates/fake_nearby_share_certificate_manager.cc
+++ chrome/browser/nearby_sharing/certificates/fake_nearby_share_certificate_manager.cc
@@ -45,8 +45,8 @@ FakeNearbyShareCertificateManager::GetDecryptedPublicCertificateCall::operator=(
 FakeNearbyShareCertificateManager::GetDecryptedPublicCertificateCall::
     ~GetDecryptedPublicCertificateCall() = default;
 
-FakeNearbyShareCertificateManager::FakeNearbyShareCertificateManager() =
-    default;
+FakeNearbyShareCertificateManager::FakeNearbyShareCertificateManager()
+    : next_salt_(GetNearbyShareTestSalt()) {}
 
 FakeNearbyShareCertificateManager::~FakeNearbyShareCertificateManager() =
     default;
@@ -76,7 +76,10 @@ void FakeNearbyShareCertificateManager::OnStop() {}
 base::Optional<NearbySharePrivateCertificate>
 FakeNearbyShareCertificateManager::GetValidPrivateCertificate(
     nearby_share::mojom::Visibility visibility) const {
-  return GetNearbyShareTestPrivateCertificate(visibility);
+  auto cert = GetNearbyShareTestPrivateCertificate(visibility);
+  cert.next_salts_for_testing() = base::queue<std::vector<uint8_t>>();
+  cert.next_salts_for_testing().push(next_salt_);
+  return cert;
 }
 
 void FakeNearbyShareCertificateManager::UpdatePrivateCertificateInStorage(
--- chrome/browser/nearby_sharing/nearby_sharing_service_impl.h
+++ chrome/browser/nearby_sharing/nearby_sharing_service_impl.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observer.h"
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "base/unguessable_token.h"
 #include "chrome/browser/nearby_sharing/attachment.h"
 #include "chrome/browser/nearby_sharing/attachment_info.h"
@@ -182,6 +183,8 @@ class NearbySharingServiceImpl
   void StopAdvertising();
   void StartScanning();
   StatusCodes StopScanning();
+  void ScheduleRotateBackgroundAdvertisementTimer();
+  void OnRotateBackgroundAdvertisementTimerFired();
 
   void OnTransferComplete();
   void OnTransferStarted(bool is_incoming);
@@ -329,6 +332,7 @@ class NearbySharingServiceImpl
   NearbyShareSettings settings_;
   NearbyFileHandler file_handler_;
   bool is_screen_locked_ = false;
+  base::OneShotTimer rotate_background_advertisement_timer_;
 
   // A list of service observers.
   base::ObserverList<NearbySharingService::Observer> observers_;
