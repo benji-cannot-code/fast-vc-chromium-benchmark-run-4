d1f8004fdec2c2e3285200203dca14d6c6586afd
Azhara Assanova <azharaa@google.com>
2020-09-23 11:34:36 +0000

    [AW][Dev-UI] Hide a crash with a missing JSON log file
    Hiding a crash sets a boolean field in the JSON log file. When this
    file is not found, the method used to do nothing. Now it creates
    the JSON log file and writes to it.
    
    run_webview_instrumentation_test_apk -f *CrashReceiverServiceTest*
    
    Bug: 1122058
    Test: run_webview_instrumentation_test_apk -f *WebViewCrashInfoCollectorTest*
    Change-Id: I3175f09839254b98c0b051873320253103675d15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2405584
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Laís Minchillo <laisminchillo@chromium.org>
    Commit-Queue: Azhara Assanova <azharaa@google.com>
    Auto-Submit: Azhara Assanova <azharaa@google.com>
    Cr-Commit-Position: refs/heads/master@{#809730}

--- android_webview/javatests/src/org/chromium/android_webview/test/devui/util/WebViewCrashInfoCollectorTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/devui/util/WebViewCrashInfoCollectorTest.java
@@ -231,6 +231,6 @@ public class WebViewCrashInfoCollectorTest {
         WebViewCrashInfoCollector.updateCrashLogFileWithNewCrashInfo(newCrashInfo);
 
         CrashInfo resultCrashInfo = getCrashFromJsonLogFile("xyz123");
-        Assert.assertThat(resultCrashInfo, equalsTo(null));
+        Assert.assertThat(resultCrashInfo, equalsTo(newCrashInfo));
     }
 }
