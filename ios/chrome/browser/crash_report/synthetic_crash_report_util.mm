c6fc3f81d15e313e096c0dfbd1856513f534be88
Eugene But <eugenebut@chromium.org>
2020-09-23 18:54:37 +0000

    Mark Stability.iOS.UTE.CreatedSyntheticCrashReportConfig as obsolete
    I learned what I want from this histogram
    
    Bug: 1117138
    Change-Id: Iae2d2a95b55103f91fed2dead8c7a5bd5401af12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2425011
    Commit-Queue: Steven Holte <holte@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809882}

--- ios/chrome/browser/crash_report/synthetic_crash_report_util.mm
+++ ios/chrome/browser/crash_report/synthetic_crash_report_util.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/memory_mapped_file.h"
 #include "base/ios/device_util.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
@@ -101,7 +100,4 @@ void CreateSyntheticCrashReportForUte(
     std::strcpy(reinterpret_cast<char*>(mapped_config_file.data()),
                 config_string.data());
   }
-
-  UMA_STABILITY_HISTOGRAM_BOOLEAN(
-      "Stability.iOS.UTE.CreatedSyntheticCrashReportConfig", created_config);
 }
