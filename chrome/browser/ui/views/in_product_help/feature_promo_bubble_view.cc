e100c907dc3115788815445c779627be47267f79
Keren Zhu <kerenzhu@chromium.org>
2020-09-23 17:38:25 +0000

    cbuiv: Fix focus of snoozable IPH bubble
    * Allow focus on snoozable bubble.
    * Adjust focused button text and background color according to
      UX spec: go/desktop-iph-snooze-ux-spec
    
    The spec also stipulates a new set of font sizes and text alignment.
    I will submit a separate CL to address them.
    
    Bug: 1121399
    Change-Id: Ifeb9caf51ba328697b930b4eba19106e83664f48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2423307
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809839}

--- chrome/browser/ui/views/in_product_help/feature_promo_bubble_view.cc
+++ chrome/browser/ui/views/in_product_help/feature_promo_bubble_view.cc
@@ -17,9 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/in_product_help/feature_promo_bubble_timeout.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/variations/variations_associated_data.h"
-#include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/theme_provider.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/text_utils.h"
 #include "ui/views/accessibility/view_accessibility.h"
@@ -48,7 +48,18 @@ constexpr base::TimeDelta kDelayShort = base::TimeDelta::FromSeconds(3);
 // The insets from the bubble border to the text inside.
 constexpr gfx::Insets kBubbleContentsInsets(12, 16);
 
+// The insets from the button border to the text inside.
 constexpr gfx::Insets kBubbleButtonPadding(8, 10);
+
+// The text color of the button.
+constexpr SkColor kBubbleButtonTextColor = SK_ColorWHITE;
+
+// The focus ring color of the button.
+constexpr SkColor kBubbleButtonFocusRingColor = SK_ColorWHITE;
+
+// The background color of the button when focused.
+constexpr SkColor kBubbleButtonFocusedBackgroundColor =
+    gfx::kGoogleBlue600;
 }  // namespace
 
 namespace views {
@@ -66,6 +77,9 @@ class MdIPHBubbleButton : public MdTextButton {
     SetProminent(true);
     // Button color is the same as IPH bubble's color.
     SetBgColorOverride(SK_ColorTRANSPARENT);
+    // Do not grey out button text on blur.
+    SetTextColor(ButtonState::STATE_DISABLED, kBubbleButtonTextColor);
+    focus_ring()->SetColor(kBubbleButtonFocusRingColor);
   }
 
   void UpdateBackgroundColor() override {
@@ -74,7 +88,8 @@ class MdIPHBubbleButton : public MdTextButton {
     // Adapted from MdTextButton::UpdateBackgroundColor()
     ui::NativeTheme* theme = GetNativeTheme();
 
-    SkColor bg_color = SK_ColorTRANSPARENT;
+    SkColor bg_color =
+        HasFocus() ? kBubbleButtonFocusedBackgroundColor : SK_ColorTRANSPARENT;
     if (GetState() == STATE_PRESSED)
       theme->GetSystemButtonPressedColor(bg_color);
 
