02779d1e9e443312f83187d176e8df0da3353166
Sébastien Séguin-Gagnon <sebsg@chromium.org>
2020-09-23 20:18:50 +0000

    Reland "Reland "[Empties] Enable the IllustratedEmptyStates field trial testing config.""
    This is a reland of d777f5d09f7a2f7ce8741be8c902d91c66849414
    
    Original change's description:
    > Reland "[Empties] Enable the IllustratedEmptyStates field trial testing config."
    > 
    > This is a reland of 74b8ec6f27605b13c69d7d07ac3cf03f6250c4b8
    > 
    > Original change's description:
    > > [Empties] Enable the IllustratedEmptyStates field trial testing config.
    > > 
    > > Change-Id: I664fa63b5b69369cb69fb4b87463ba3bb4cfc94d
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2386295
    > > Reviewed-by: Sergio Collazos <sczs@chromium.org>
    > > Reviewed-by: Steven Holte <holte@chromium.org>
    > > Commit-Queue: sebsg <sebsg@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#805136}
    > 
    > Change-Id: Ieb3e461efb4c55e7ef601417819cc4470e8fe4cc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401398
    > Commit-Queue: sebsg <sebsg@chromium.org>
    > Reviewed-by: Sergio Collazos <sczs@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Auto-Submit: sebsg <sebsg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#807867}
    
    Change-Id: I0c3a85ef61dc74064e9e9d49a27a5efe1e333452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2418891
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809933}

--- ios/chrome/browser/ui/bookmarks/bookmarks_egtest.mm
+++ ios/chrome/browser/ui/bookmarks/bookmarks_egtest.mm
@@ -728,7 +728,7 @@ using chrome_test_util::TappableBookmarkNodeWithLabel;
   // Reopen bookmarks.
   [BookmarkEarlGreyUI openBookmarks];
 
-  if (base::FeatureList::IsEnabled(kIllustratedEmptyStates)) {
+  if ([ChromeEarlGrey isIllustratedEmptyStatesEnabled]) {
     // Ensure the root node is opened, by verifying that there isn't a Back
     // button in the navigation bar.
     [[EarlGrey selectElementWithMatcher:chrome_test_util::
@@ -835,7 +835,7 @@ using chrome_test_util::TappableBookmarkNodeWithLabel;
 - (void)testRootEmptyState {
   [BookmarkEarlGreyUI openBookmarks];
 
-  if (base::FeatureList::IsEnabled(kIllustratedEmptyStates)) {
+  if ([ChromeEarlGrey isIllustratedEmptyStatesEnabled]) {
     // When the user has no bookmarks, the root view should be an empty state.
     [BookmarkEarlGreyUI verifyEmptyState];
   } else {
@@ -863,7 +863,7 @@ using chrome_test_util::TappableBookmarkNodeWithLabel;
                                           BookmarksNavigationBarBackButton()]
       performAction:grey_tap()];
 
-  if (base::FeatureList::IsEnabled(kIllustratedEmptyStates)) {
+  if ([ChromeEarlGrey isIllustratedEmptyStatesEnabled]) {
     // When the user has no bookmarks, the root view should be an empty state.
     [BookmarkEarlGreyUI verifyEmptyState];
   } else {
--- ios/chrome/browser/ui/recent_tabs/recent_tabs_egtest.mm
+++ ios/chrome/browser/ui/recent_tabs/recent_tabs_egtest.mm
@@ -153,8 +153,8 @@ id<GREYMatcher> TitleOfTestPage() {
                                                   IDS_HISTORY_TITLE)),
                                           grey_accessibilityTrait(
                                               UIAccessibilityTraitHeader),
-                                          nil)]
-      assertWithMatcher:grey_sufficientlyVisible()];
+                                          grey_sufficientlyVisible(), nil)]
+      assertWithMatcher:grey_notNil()];
 
   // Close History.
   id<GREYMatcher> exitMatcher =
@@ -168,6 +168,15 @@ id<GREYMatcher> TitleOfTestPage() {
 // Tests that the sign-in promo can be reloaded correctly.
 - (void)testRecentTabSigninPromoReloaded {
   OpenRecentTabsPanel();
+
+  // Scroll to sign-in promo, if applicable.
+  [[EarlGrey
+      selectElementWithMatcher:
+          grey_allOf(grey_accessibilityID(
+                         kRecentTabsTableViewControllerAccessibilityIdentifier),
+                     grey_sufficientlyVisible(), nil)]
+      performAction:grey_scrollToContentEdge(kGREYContentEdgeBottom)];
+
   // Sign-in promo should be visible with cold state.
   [SigninEarlGreyUI
       verifySigninPromoVisibleWithMode:SigninPromoViewModeColdState
@@ -186,6 +195,15 @@ id<GREYMatcher> TitleOfTestPage() {
 // crbug.com/776939
 - (void)testRecentTabSigninPromoReloadedWhileHidden {
   OpenRecentTabsPanel();
+
+  // Scroll to sign-in promo, if applicable
+  [[EarlGrey
+      selectElementWithMatcher:
+          grey_allOf(grey_accessibilityID(
+                         kRecentTabsTableViewControllerAccessibilityIdentifier),
+                     grey_sufficientlyVisible(), nil)]
+      performAction:grey_scrollToContentEdge(kGREYContentEdgeBottom)];
+
   [SigninEarlGreyUI
       verifySigninPromoVisibleWithMode:SigninPromoViewModeColdState
                            closeButton:NO];
@@ -207,6 +225,13 @@ id<GREYMatcher> TitleOfTestPage() {
   // Tap on "Other Devices", to show the sign-in promo.
   [[EarlGrey selectElementWithMatcher:otherDevicesMatcher]
       performAction:grey_tap()];
+  // Scroll to sign-in promo, if applicable
+  [[EarlGrey
+      selectElementWithMatcher:
+          grey_allOf(grey_accessibilityID(
+                         kRecentTabsTableViewControllerAccessibilityIdentifier),
+                     grey_sufficientlyVisible(), nil)]
+      performAction:grey_scrollToContentEdge(kGREYContentEdgeBottom)];
   [SigninEarlGreyUI
       verifySigninPromoVisibleWithMode:SigninPromoViewModeWarmState
                            closeButton:NO];
@@ -290,6 +315,14 @@ id<GREYMatcher> TitleOfTestPage() {
   } else {
     [illustratedCell assertWithMatcher:grey_nil()];
   }
+
+  // Scroll to sign-in promo, if applicable
+  [[EarlGrey
+      selectElementWithMatcher:
+          grey_allOf(grey_accessibilityID(
+                         kRecentTabsTableViewControllerAccessibilityIdentifier),
+                     grey_sufficientlyVisible(), nil)]
+      performAction:grey_scrollToContentEdge(kGREYContentEdgeBottom)];
 
   [SigninEarlGreyUI
       verifySigninPromoVisibleWithMode:SigninPromoViewModeColdState
