72cb3b76642d390ff39c7d6bf203a0c65a6d8b50
Trent Begin <tbegin@google.com>
2020-09-23 22:26:22 +0000

    network-health: add signal strength to network health snapshot
    This change adds a new field to the network snapshot, signal strength.
    It is populated with the signal strength of wireless network devices as
    measured from the device. The value ranges from 0 - 100. This value is
    also added to the network health summary UI and feedback reports.
    
    Bug: chromium:1128460
    Test: checked chrome://network for signal strength
    Change-Id: I447d5753b16ca79d9383543bb16a669eb7b80184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412216
    Commit-Queue: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809997}

--- chrome/browser/chromeos/system_logs/network_health_source.cc
+++ chrome/browser/chromeos/system_logs/network_health_source.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
+#include "base/strings/string_number_conversions.h"
 #include "chrome/browser/chromeos/net/network_health/network_health_service.h"
 #include "chromeos/network/network_event_log.h"
 #include "content/public/browser/browser_thread.h"
@@ -35,6 +36,11 @@ std::string GetFormattedString(
 
     output << "Type: " << net->type << "\n";
     output << "State: " << net->state << "\n";
+    output << "Signal Strength: "
+           << (net->signal_strength
+                   ? base::NumberToString(net->signal_strength->value)
+                   : "N/A")
+           << "\n";
     output << "MAC Address: " << net->mac_address.value_or("N/A") << "\n";
     output << "\n";
   }
