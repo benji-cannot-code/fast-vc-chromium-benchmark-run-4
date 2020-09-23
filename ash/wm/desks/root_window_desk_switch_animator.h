6b4e16ac661d28be76c5682ca4934c6143c48f92
Elly Fong-Jones <ellyjones@chromium.org>
2020-09-23 23:27:31 +0000

    Revert "desks: Add tests for RootWindowDeskSwitchAnimator."
    This reverts commit 4c93f79bd63979100d88f7bd016f2a3bb3bbfd1b.
    
    Reason for revert: broke ash_unittests on linux-chromeos-dbg: https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-dbg/20568
    
    2020-09-23T19:55:14.797906Z 17555455662 FATAL ash_unittests[27683:27683]: [gles2_implementation.cc(6920)] Check failed: mailbox.Verify(). CreateAndConsumeTextureCHROMIUM was passed a mailbox that was not generated by ProduceTextureDirectCHROMIUM.
    
    
    Original change's description:
    > desks: Add tests for RootWindowDeskSwitchAnimator.
    > 
    > Test: added
    > Bug: 1111445
    > Change-Id: I0f79be1c09d4ef0eeff276e3c50f491efbe56244
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2380517
    > Reviewed-by: Khushal <khushalsagar@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#809854}
    
    TBR=afakhry@chromium.org,khushalsagar@chromium.org,sammiequon@chromium.org
    
    Change-Id: I7803aa521023f1063f7cb584126bbffb5311c420
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1111445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427037
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810013}

--- ash/wm/desks/root_window_desk_switch_animator.h
+++ ash/wm/desks/root_window_desk_switch_animator.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "ash/ash_export.h"
 #include "base/memory/weak_ptr.h"
 #include "ui/compositor/layer_animation_observer.h"
 
@@ -159,8 +158,7 @@ namespace ash {
 // slightly more to accommodate the continuous desk animations feature. The base
 // algorithm is still valid, but once the features are near completion these
 // need to be updated.
-class ASH_EXPORT RootWindowDeskSwitchAnimator
-    : public ui::ImplicitAnimationObserver {
+class RootWindowDeskSwitchAnimator : public ui::ImplicitAnimationObserver {
  public:
   class Delegate {
    public:
@@ -181,15 +179,6 @@ class ASH_EXPORT RootWindowDeskSwitchAnimator
    protected:
     virtual ~Delegate() = default;
   };
-
-  // The space between the starting and ending desks screenshots in dips.
-  static constexpr int kDesksSpacing = 50;
-
-  // The animation layer has extra padding at its two edges. The width in dips
-  // is a ratio of the root window width. This padding is to notify users there
-  // are no more desks on that side by showing a black region as we swipe
-  // continuously.
-  static constexpr float kEdgePaddingRatio = 0.15f;
 
   RootWindowDeskSwitchAnimator(aura::Window* root,
                                int starting_desk_index,
@@ -247,8 +236,6 @@ class ASH_EXPORT RootWindowDeskSwitchAnimator
   void OnImplicitAnimationsCompleted() override;
 
  private:
-  friend class RootWindowDeskSwitchAnimatorTestApi;
-
   // Completes the first phase of the animation using the given |layer| as the
   // screenshot layer of the starting desk. This layer will be parented to the
   // animation layer, which will be setup with its initial transform according
