0cd939cfc663b9d06199a2581b006e83aea3f847
Khushal Sagar <khushalsagar@chromium.org>
2020-09-23 21:27:29 +0000

    canvas: Cleanup mailbox sharing across threads and synchronization.
    This updates 2 code-paths pertaining to sharing mailboxes wrapped in
    AcceleratedStaticBitmapImage:
    
    1) Always use a sync token when wrapping a canvas mailbox to an image.
    This was earlier removed to be lazily generated inside the image, if the
    mailbox is used on the same context, with the expectation that avoiding
    an ordering barrier in the process would be beneficial. But this doesn't
    seem to be the case and the extra complexity is not worth it.
    
    2) Only verify sync tokens when shared cross-process, instead of
    cross-thread usage. All callers which acquire the image mailbox directly
    already verify sync tokens before dispatching them cross-process so this
    simply removes this requirement from AcceleratedStaticBitmapImage.
    
    R=sunnyps@chromium.org
    
    Change-Id: I8c8d5befdf8741ddcecb11309927eb76356dcd86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2423269
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809965}

--- third_party/blink/renderer/platform/graphics/gpu/webgpu_image_bitmap_handler_test.cc
+++ third_party/blink/renderer/platform/graphics/gpu/webgpu_image_bitmap_handler_test.cc
@@ -28,8 +28,8 @@ namespace blink {
 namespace {
 gpu::SyncToken GenTestSyncToken(GLbyte id) {
   gpu::SyncToken token;
-  // Store id in the first byte
-  reinterpret_cast<GLbyte*>(&token)[0] = id;
+  token.Set(gpu::CommandBufferNamespace::GPU_IO,
+            gpu::CommandBufferId::FromUnsafeValue(64), id);
   return token;
 }
 
