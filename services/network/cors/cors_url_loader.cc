167cc03830c515ee63959806034da51b436c72f6
David Van Cleve <davidvc@chromium.org>
2020-09-23 20:53:13 +0000

    Trust Tokens: Log net errors for subresource operations
    We're seeing a pretty high failure rate for Trust Tokens issuance via
    the Fetch API on Windows. This CL adds some logging to help debug this
    by recording the net errors for Trust Tokens operations associated with
    subresource requests. It:
    
    - expands URLLoaderCompletionStatus to store a Trust Tokens
    operation type as well as a Trust Tokens operation status code;
    - adds logging code in resoure_load_stats.cc to log this error; and
    - adds a new Trust Tokens browser test confirming that a couple
    diverse cases log the correct errors (successful Trust Tokens operation
    begin followed by a network-level error; unsuccessful Trust Tokens
    operation).
    
    Fixed: 1128180
    Change-Id: I2ae6b1c5e81fc8ffaa4e9e912a994f5181af1a49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416882
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809944}

--- services/network/cors/cors_url_loader.cc
+++ services/network/cors/cors_url_loader.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/header_util.h"
 #include "services/network/public/cpp/request_mode.h"
+#include "services/network/trust_tokens/trust_token_operation_metrics_recorder.h"
 #include "services/network/url_loader.h"
 #include "url/url_util.h"
 
@@ -539,6 +540,12 @@ void CorsURLLoader::StartNetworkRequest(
 }
 
 void CorsURLLoader::HandleComplete(const URLLoaderCompletionStatus& status) {
+  if (request_.trust_token_params) {
+    HistogramTrustTokenOperationNetError(request_.trust_token_params->type,
+                                         status.trust_token_operation_status,
+                                         status.error_code);
+  }
+
   forwarding_client_->OnComplete(status);
   std::move(delete_callback_).Run(this);
   // |this| is deleted here.
