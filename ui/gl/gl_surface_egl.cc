0b0f14c7537e10dfa92763d25b6b08d0262f3a95
Peng Huang <penghuang@chromium.org>
2020-09-23 18:31:31 +0000

    Fix VANGLE initialize failure for remote desktop environment.
    For remote desktop environment, when Vulkan is used, VANGLE is only used
    for WebGL. It is not responsible for rendering to the native windows (we
    will not create GLSurfaceEGL for native windows). We will use Vulkan
    swapchain to render on native windows. However some Vulkan drivers
    (NVIDIA) cannot be initialized successfully, if the $DISPLAY env
    variable is set, but the Xserver(remote desktop Xserver) doesn't have
    necessary extensions (NV-CONTROL, NV-GLX). To workaround this issue,
    this CL unsets the $DISPLAY env variable before initializing VANGLE and
    sets it back after it, if the Xserver doesn't support vulkan swapchain.
    
    Change-Id: I528a6f7d2f618123ea519f0ab4f15e788c4dd607
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2424519
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809872}

--- ui/gl/gl_surface_egl.cc
+++ ui/gl/gl_surface_egl.cc
@@ -53,6 +53,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EGL_OPENGL_ES3_BIT 0x00000040
 #endif
 
+#if defined(USE_X11)
+#include "ui/base/x/x11_util.h"
+#endif
+
 // Not present egl/eglext.h yet.
 
 #ifndef EGL_EXT_gl_colorspace_display_p3
@@ -348,6 +352,7 @@ EGLDisplay GetPlatformANGLEDisplay(
   // TODO(dbehr) Add an attrib to Angle to pass EGL platform.
 
   display_attribs.push_back(EGL_NONE);
+
   // This is an EGL 1.5 function that we know ANGLE supports. It's used to pass
   // EGLAttribs (pointers) instead of EGLints into the display
   return eglGetPlatformDisplay(
@@ -1289,6 +1294,14 @@ EGLDisplay GLSurfaceEGL::InitializeDisplay(EGLDisplayPlatform native_display) {
       SetANGLEImplementation(
           GetANGLEImplementationFromDisplayType(display_type));
     }
+
+#if defined(USE_X11)
+    // Unset DISPLAY env, so the vulkan can be initialized successfully, if the
+    // X server doesn't support Vulkan surface.
+    base::Optional<ui::ScopedUnsetDisplay> unset_display;
+    if (display_type == ANGLE_VULKAN && !ui::IsVulkanSurfaceSupported())
+      unset_display.emplace();
+#endif  // defined(USE_X11)
 
     if (!eglInitialize(display, nullptr, nullptr)) {
       bool is_last = disp_index == init_displays.size() - 1;
