d0ec5c498a0bb3db751d5c35ab1698524b41d43e
Jose Dapena Paz <jdapena@igalia.com>
2020-09-23 14:34:57 +0000

    IWYU: uint32_t requires stdint.h include in printing PageRange.
    Fix this error:
    ../../printing/page_range.h:20:3: error: ‘uint32_t’ does not name a type
    
    Introduced in this commit:
    https://chromium-review.googlesource.com/c/chromium/src/+/2351580
    
    Bug: 951519
    Change-Id: I0c86cfc5dc711b062021dd8661d27d6442e7d47d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2423909
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809758}

--- printing/page_range.h
+++ printing/page_range.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PRINTING_PAGE_RANGE_H_
 #define PRINTING_PAGE_RANGE_H_
 
+#include <stdint.h>
+
 #include <vector>
 
 #include "printing/printing_export.h"
