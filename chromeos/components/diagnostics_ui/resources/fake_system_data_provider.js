d03fd15d8af62f483bad243137603733b9a3d00e
Zentaro Kavanagh <zentaro@chromium.org>
2020-09-23 17:53:56 +0000

    Diagnostics: Add support for observing memory usage
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I39f8d765f841aae135ed543f9fc2238ec8a1837e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422184
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#809852}

--- chromeos/components/diagnostics_ui/resources/fake_system_data_provider.js
+++ chromeos/components/diagnostics_ui/resources/fake_system_data_provider.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {BatteryChargeStatus, BatteryHealth, BatteryInfo, CpuUsage, CpuUsageObserver, ExternalPowerSource, SystemInfo} from './diagnostics_types.js';
+import {BatteryChargeStatus, BatteryHealth, BatteryInfo, CpuUsage, CpuUsageObserver, ExternalPowerSource, MemoryUsage, MemoryUsageObserver, SystemInfo} from './diagnostics_types.js';
 import {FakeMethodResolver} from './fake_method_resolver.js';
 import {FakeObservables} from './fake_observables.js';
 
@@ -29,6 +29,7 @@ export class FakeSystemDataProvider {
         'BatteryChargeStatusObserver_onBatteryChargeStatusUpdated');
     this.observables_.register('BatteryHealthObserver_onBatteryHealthUpdated');
     this.observables_.register('CpuUsageObserver_onCpuUsageUpdated');
+    this.observables_.register('MemoryUsageObserver_onMemoryUsageUpdated');
   }
 
   /**
@@ -144,5 +145,32 @@ export class FakeSystemDataProvider {
   setFakeCpuUsage(cpuUsageList) {
     this.observables_.setObservableData(
         'CpuUsageObserver_onCpuUsageUpdated', cpuUsageList);
+  }
+
+  /*
+   * Implements SystemDataProviderInterface.ObserveMemoryUsage.
+   * @param {!MemoryUsageObserver} remote
+   * @return {!Promise}
+   */
+  observeMemoryUsage(remote) {
+    return new Promise((resolve) => {
+      this.observables_.observe(
+          'MemoryUsageObserver_onMemoryUsageUpdated', (memoryUsage) => {
+            remote.onMemoryUsageUpdated(
+                /** @type {!MemoryUsage} */ (memoryUsage));
+          });
+
+      this.observables_.trigger('MemoryUsageObserver_onMemoryUsageUpdated');
+      resolve();
+    });
+  }
+
+  /**
+   * Sets the values that will observed from ObserveCpuUsage.
+   * @param {!Array<!MemoryUsage>} memoryUsageList
+   */
+  setFakeMemoryUsage(memoryUsageList) {
+    this.observables_.setObservableData(
+        'MemoryUsageObserver_onMemoryUsageUpdated', memoryUsageList);
   }
 }
