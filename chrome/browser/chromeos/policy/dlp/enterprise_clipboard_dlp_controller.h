304dce629d25be47c9d07dcf77cc101f34da09f6
Aya ElAttar <ayaelattar@chromium.org>
2020-09-24 13:59:26 +0000

    Show toast on paste failure
    Changed EnterpriseClipboardDlpController to
    show toast notification when clipboard data
    access is blocked.
    
    Bug: 1096461, 1102332
    Change-Id: I8f3aaf066c04468ba3a1c4716da02bdf878b7beb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416514
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Nikita Podguzov <nikitapodguzov@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810184}

--- chrome/browser/chromeos/policy/dlp/enterprise_clipboard_dlp_controller.h
+++ chrome/browser/chromeos/policy/dlp/enterprise_clipboard_dlp_controller.h
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_POLICY_DLP_ENTERPRISE_CLIPBOARD_DLP_CONTROLLER_H_
 #define CHROME_BROWSER_CHROMEOS_POLICY_DLP_ENTERPRISE_CLIPBOARD_DLP_CONTROLLER_H_
 
-#include "ui/base/clipboard/clipboard_data_endpoint.h"
+#include "base/strings/string16.h"
 #include "ui/base/clipboard/clipboard_dlp_controller.h"
 
-namespace chromeos {
+namespace ui {
+class ClipboardDataEndpoint;
+}
+
+namespace policy {
 
 // EnterpriseClipboardDlpController is responsible for preventing leaks of
 // confidential clipboard data by controlling read operations according to the
@@ -23,12 +27,23 @@ class EnterpriseClipboardDlpController : public ui::ClipboardDlpController {
       delete;
   void operator=(const EnterpriseClipboardDlpController&) = delete;
 
-  // nullptr can be passed instead of |data_src| or |data_dst|.
+  // nullptr can be passed instead of |data_src| or |data_dst|. If clipboard
+  // data read is not allowed, this function will show a toast to the user.
   bool IsDataReadAllowed(
       const ui::ClipboardDataEndpoint* const data_src,
       const ui::ClipboardDataEndpoint* const data_dst) const override;
+
+ private:
+  // Shows toast in case the access to the clipboard data is blocked.
+  // TODO(crbug.com/1131670): Move `ShowBlockToast` to a separate util/helper.
+  void ShowBlockToast(const base::string16& text) const;
+
+  // The text will be different if the clipboard data is shared with Crostini
+  // or Parallels or both of them.
+  base::string16 GetToastText(
+      const ui::ClipboardDataEndpoint* const data_dst) const;
 };
 
-}  // namespace chromeos
+}  // namespace policy
 
 #endif  // CHROME_BROWSER_CHROMEOS_POLICY_DLP_ENTERPRISE_CLIPBOARD_DLP_CONTROLLER_H_
