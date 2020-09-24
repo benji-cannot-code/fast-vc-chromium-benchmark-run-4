a95398d15405d5b810137051bee2e9a4415b87aa
Lei Zhang <thestig@chromium.org>
2020-09-24 23:26:33 +0000

    Remove static initializer in chromeos::Uri::Pim.
    Give `Uri::Pim::kDefaultPorts` a local scope, by moving it inside
    Uri::Pim::GetDefaultPorts(). Then it is only initialized on first use
    instead on process startup.
    
    Bug: 537099
    Change-Id: I66033ca86eb2182ae670351cd53869d988d04c0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427034
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Piotr Pawliczek <pawliczek@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810341}

--- chromeos/printing/uri.cc
+++ chromeos/printing/uri.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
-
 #include "chromeos/printing/uri_impl.h"
 
 namespace chromeos {
@@ -127,8 +126,8 @@ Uri::Uri(const std::string& uri) : pim_(std::make_unique<Pim>()) {
 }
 
 int Uri::GetDefaultPort(const std::string& scheme) {
-  auto it = Pim::kDefaultPorts.find(scheme);
-  if (it == Pim::kDefaultPorts.end())
+  auto it = Pim::GetDefaultPorts().find(scheme);
+  if (it == Pim::GetDefaultPorts().end())
     return -1;
   return it->second;
 }
@@ -165,8 +164,8 @@ std::string Uri::GetNormalized(bool always_print_port) const {
   // Calculates a string representation of the Port number.
   std::string port;
   if (pim_->port() >= 0 &&
-      (always_print_port || Pim::kDefaultPorts.count(pim_->scheme()) == 0 ||
-       Pim::kDefaultPorts.at(pim_->scheme()) != pim_->port()))
+      (always_print_port || Pim::GetDefaultPorts().count(pim_->scheme()) == 0 ||
+       Pim::GetDefaultPorts().at(pim_->scheme()) != pim_->port()))
     port = base::NumberToString(pim_->port());
 
   // Output string. Adds Scheme.
