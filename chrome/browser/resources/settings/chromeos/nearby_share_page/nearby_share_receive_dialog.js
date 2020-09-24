2e18a2a61e50328d6d2a214075aad2faa9b1540e
James Vecore <vecore@google.com>
2020-09-24 03:34:35 +0000

    [Nearby] Reland: Trigger onboarding when first enabling Nearby
    Reland as is because the fix was in the parent CL:
    https://crrev.com/c/2426864/2
    
    Original commit message:
    > [Nearby] Trigger onboarding when first enabling Nearby
    >
    > This CL adds a pref to track if onboarding was run by this user before.
    > If it has not, when enabling for the first time, onboarding is run first
    > by navigating to the subpage and showing in a dialog. Once onboarding
    > has been done once, the user can freely toggle on/off without
    > triggering onboarding. However, onboarding will still run if the user
    > triggers the pod button and the feature is disabled. This matches the
    > Android behavior.
    >
    > Additionally, this CL fixes a bug with the premature closing of the
    > shared receiveManager which needs to stay alive and connected after the
    > dialog closes otherwise getReceiveManager returns a remote with a closed
    > pipe.
    >
    > Bug: b/154866951
    > Change-Id: I1e3cbd15f4995f019b3476c2f7a69f67d6eb1888
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2424763
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Commit-Queue: James Vecore <vecore@google.com>
    > Cr-Commit-Position: refs/heads/master@{#809623}
    
    Change-Id: I91e3a3749ae15ae6bf7879c259e3622e74eba337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427004
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#810082}

--- chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_receive_dialog.js
+++ chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_receive_dialog.js
@@ -104,11 +104,6 @@ Polymer({
     if (this.observerReceiver_) {
       this.observerReceiver_.$.close();
     }
-
-    if (this.receiveManager_) {
-      /** @type {nearbyShare.mojom.ReceiveManagerRemote} */
-      (this.receiveManager_).$.close();
-    }
   },
 
   /**
