b8733d7c3feb346c8bc009e1d174c7679217d168
Aaron Leventhal <aleventhal@google.com>
2020-09-24 15:52:53 +0000

    Remove ARIA Annotations flag
    Bug: 1129111
    Change-Id: I5c5db22feb752e42d0620f03c3e765c6319517ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422719
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810224}

--- ui/accessibility/accessibility_features.h
+++ ui/accessibility/accessibility_features.h
@@ -13,12 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 
-AX_BASE_EXPORT extern const base::Feature
-    kEnableAccessibilityExposeARIAAnnotations;
-
-// Returns true if ARIA annotations should be exposed to the browser AX Tree.
-AX_BASE_EXPORT bool IsAccessibilityExposeARIAAnnotationsEnabled();
-
 AX_BASE_EXPORT extern const base::Feature kEnableAccessibilityExposeDisplayNone;
 
 // Returns true if "display: none" nodes should be exposed to the
