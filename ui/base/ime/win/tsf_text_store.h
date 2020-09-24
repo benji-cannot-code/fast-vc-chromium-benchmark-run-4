d2c37c5a5ecd32de10c7bd17d4437a23fbf3b6c8
Siye Liu <siliu@microsoft.com>
2020-09-24 23:40:45 +0000

    Revert change to notify input method about type change in omnibox during CJK tip.
    The change to notify input method about type change in omnibox during
    CJK TIP[1] causes regression that switching to a new tab will change JPN
    TIP to English mode instead of Hiragana mode. The reason is that when
    input method is notified about the text input type change, Windows input
    service is also notified about the change. Therefore, the input mode is
    changed from hiragana mode to English mode in JPN TIP.
    
    We should only notify input service if the input type is changed to a
    different value. We should also use the cached input type to find the
    associated TSF document.
    
     [1]: I0266d9617277696ccb8bfa42ad76f9cbc3d802f3
          fe7e213cecb91264a3bd1f6aed8da3495e158963
    
    Bug: 1108845
    Bug: 1124612
    
    Change-Id: I9ec643c7d2058695e8bc396d764ba598d6235b88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2392415
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Siye Liu <siliu@microsoft.com>
    Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    Commit-Queue: Siye Liu <siliu@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#810350}

--- ui/base/ime/win/tsf_text_store.h
+++ ui/base/ime/win/tsf_text_store.h
@@ -103,7 +103,6 @@ class TextInputClient;
 class COMPONENT_EXPORT(UI_BASE_IME_WIN) TSFTextStore
     : public ITextStoreACP,
       public ITfContextOwnerCompositionSink,
-      public ITfLanguageProfileNotifySink,
       public ITfKeyTraceEventSink,
       public ITfTextEditSink {
  public:
@@ -218,10 +217,6 @@ class COMPONENT_EXPORT(UI_BASE_IME_WIN) TSFTextStore
                                      ITfRange* range) override;
   IFACEMETHODIMP OnEndComposition(
       ITfCompositionView* composition_view) override;
-
-  // ITfLanguageProfileNotifySink:
-  IFACEMETHODIMP OnLanguageChange(LANGID langid, BOOL* pfAccept) override;
-  IFACEMETHODIMP OnLanguageChanged() override;
 
   // ITfTextEditSink:
   IFACEMETHODIMP OnEndEdit(ITfContext* context,
