c84fb12b593a0e2d5bab66bab96e3a66dc21ee6e
Rouslan Solomakhin <rouslan@chromium.org>
2020-09-24 10:42:27 +0000

    [Web Payment] Limit manifest download size.
    Before this patch, a website could create a PaymentRequest object with a
    payment method that pointed to a >5MB manifest file, which would cause a
    crash in DownloadToStringOfUnboundedSizeUntilCrashAndDie() method.
    
    This patch replaces the
    DownloadToStringOfUnboundedSizeUntilCrashAndDie() method with
    DownloadToString() with an upper bound of 1MB file size for both payment
    method manifest and web app manifest.
    
    After this patch, if the payment method manifest or web app manifest are
    larger than 1MB, then PaymentRequest.canMakePayment() and
    PaymentRequest.hasEnrolledInstrument() will return false,
    PaymentRequest.show() will reject with NotSupportedError.
    
    Bug: 1025336
    Change-Id: I209116d3c1ec1721148e94992adfa2a20a7beaa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2426966
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810122}

--- components/payments/core/payment_manifest_downloader.cc
+++ components/payments/core/payment_manifest_downloader.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/payments/core/native_error_strings.h"
 #include "components/payments/core/url_util.h"
 #include "net/base/load_flags.h"
+#include "net/base/net_errors.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "net/base/url_util.h"
 #include "net/http/http_response_headers.h"
@@ -34,6 +35,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace payments {
 namespace {
+
+static constexpr size_t kMaxManifestSize = 1024 * 1024;
+static_assert(kMaxManifestSize <=
+                  network::SimpleURLLoader::kMaxBoundedStringDownloadSize,
+              "Max manifest size bigger than largest allowed download size");
 
 // Invokes |callback| with |error_format|.
 void RespondWithError(const base::StringPiece& error_format,
@@ -208,6 +214,12 @@ void PaymentManifestDownloader::OnURLLoaderCompleteInternal(
   std::unique_ptr<Download> download = std::move(download_it->second);
   downloads_.erase(download_it);
 
+  if (net_error != net::OK) {
+    RespondWithError(errors::kPaymentManifestDownloadFailed, final_url, *log_,
+                     std::move(download->callback));
+    return;
+  }
+
   std::string error_message;
   if (download->type == Download::Type::RESPONSE_BODY) {
     if (!headers || headers->response_code() != net::HTTP_OK) {
@@ -347,10 +359,12 @@ void PaymentManifestDownloader::InitiateDownload(
   loader->SetOnRedirectCallback(
       base::BindRepeating(&PaymentManifestDownloader::OnURLLoaderRedirect,
                           weak_ptr_factory_.GetWeakPtr(), loader.get()));
-  loader->DownloadToStringOfUnboundedSizeUntilCrashAndDie(
+
+  loader->DownloadToString(
       url_loader_factory_.get(),
       base::BindOnce(&PaymentManifestDownloader::OnURLLoaderComplete,
-                     weak_ptr_factory_.GetWeakPtr(), loader.get()));
+                     weak_ptr_factory_.GetWeakPtr(), loader.get()),
+      kMaxManifestSize);
 
   auto download = std::make_unique<Download>();
   download->request_initiator = request_initiator;
