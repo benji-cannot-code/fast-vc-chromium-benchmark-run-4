5d7218ec97f6290eaa3de2422a055e09733e750c
jchen10 <jie.a.chen@intel.com>
2020-09-24 22:39:13 +0000

    [WebCodecs] Cleanup of DestroySharedImage in VideoFrame
    This makes MailboxTextureBacking keep a reference to MailboxRef and
    updates its release SyncToken when MailboxTextureBacking goes out of
    life.
    
    Bug: 897297, 1121019, 1131123
    Change-Id: Ia121d3c3898eeab89f814eba6de43406871f4c37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2351452
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810313}

--- third_party/blink/renderer/platform/graphics/mailbox_texture_backing.h
+++ third_party/blink/renderer/platform/graphics/mailbox_texture_backing.h
@@ -14,19 +14,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 class WebGraphicsContext3DProviderWrapper;
+class MailboxRef;
 
 class MailboxTextureBacking : public TextureBacking {
  public:
   explicit MailboxTextureBacking(
       sk_sp<SkImage> sk_image,
+      scoped_refptr<MailboxRef> mailbox_ref,
       const SkImageInfo& info,
       base::WeakPtr<WebGraphicsContext3DProviderWrapper>
           context_provider_wrapper);
   explicit MailboxTextureBacking(
       const gpu::Mailbox& mailbox,
+      scoped_refptr<MailboxRef> mailbox_ref,
       const SkImageInfo& info,
       base::WeakPtr<WebGraphicsContext3DProviderWrapper>
           context_provider_wrapper);
+  ~MailboxTextureBacking() override;
   const SkImageInfo& GetSkImageInfo() override;
   gpu::Mailbox GetMailbox() const override;
   sk_sp<SkImage> GetAcceleratedSkImage() override;
@@ -41,6 +45,7 @@ class MailboxTextureBacking : public TextureBacking {
  private:
   const sk_sp<SkImage> sk_image_;
   const gpu::Mailbox mailbox_;
+  scoped_refptr<MailboxRef> mailbox_ref_;
   const SkImageInfo sk_image_info_;
   base::WeakPtr<WebGraphicsContext3DProviderWrapper> context_provider_wrapper_;
 };
