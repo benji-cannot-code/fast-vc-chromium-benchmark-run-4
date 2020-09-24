8b875ddfbf26fa145b55734753ad7f55e759f2fb
Monica Basta <msalama@chromium.org>
2020-09-24 09:44:25 +0000

    [SyncSetup]: Remove pre-sync setup friendly settings code.
    The sync setup frienldy settings has been fully launched. This CL
    removes the flag and cleans the code accordingly.
    
    Bug: 1045423
    Change-Id: I32eb5123b6cd473b7b5dc73fb8c99f445afae369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2418954
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810114}

--- chrome/test/data/webui/settings/people_page_sync_page_test.js
+++ chrome/test/data/webui/settings/people_page_sync_page_test.js
@@ -56,8 +56,7 @@ suite('SyncSettingsTests', function() {
   }
 
   suiteSetup(function() {
-    loadTimeData.overrideValues(
-        {syncSetupFriendlySettings: true, signinAllowed: true});
+    loadTimeData.overrideValues({signinAllowed: true});
   });
 
   setup(function() {
