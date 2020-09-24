7d861a0e4fa5571d4cbde56a0dcfc4899b1f8aa6
Hiroshige Hayashizaki <hiroshige@chromium.org>
2020-09-24 03:04:35 +0000

    [WPT] Add module version of service-worker/registration-script.https.html
    Bug: 1129795
    Change-Id: Ibff0827b206b9a0290fcdbcaccb3c57cae2a00da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2413725
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810076}

--- third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/registration-tests-script.js
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/registration-tests-script.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Registration tests that mostly exercise the service worker script contents or
 // response.
-function registration_tests_script(register_method) {
+function registration_tests_script(register_method, type) {
   promise_test(function(t) {
       var script = 'resources/invalid-chunked-encoding.py';
       var scope = 'resources/scope/invalid-chunked-encoding/';
@@ -47,14 +47,16 @@ function registration_tests_script(register_method) {
           'Registration of script including uncaught exception should fail.');
     }, 'Registering script including uncaught exception');
 
-  promise_test(function(t) {
-      var script = 'resources/malformed-worker.py?import-malformed-script';
-      var scope = 'resources/scope/import-malformed-script';
-      return promise_rejects_js(t,
-          TypeError,
-          register_method(script, {scope: scope}),
-          'Registration of script importing malformed script should fail.');
-    }, 'Registering script importing malformed script');
+  if (type === 'classic') {
+    promise_test(function(t) {
+        var script = 'resources/malformed-worker.py?import-malformed-script';
+        var scope = 'resources/scope/import-malformed-script';
+        return promise_rejects_js(t,
+            TypeError,
+            register_method(script, {scope: scope}),
+            'Registration of script importing malformed script should fail.');
+      }, 'Registering script importing malformed script');
+  }
 
   promise_test(function(t) {
       var script = 'resources/no-such-worker.js';
@@ -65,14 +67,16 @@ function registration_tests_script(register_method) {
           'Registration of non-existent script should fail.');
     }, 'Registering non-existent script');
 
-  promise_test(function(t) {
-      var script = 'resources/malformed-worker.py?import-no-such-script';
-      var scope = 'resources/scope/import-no-such-script';
-      return promise_rejects_js(t,
-          TypeError,
-          register_method(script, {scope: scope}),
-          'Registration of script importing non-existent script should fail.');
-    }, 'Registering script importing non-existent script');
+  if (type === 'classic') {
+    promise_test(function(t) {
+        var script = 'resources/malformed-worker.py?import-no-such-script';
+        var scope = 'resources/scope/import-no-such-script';
+        return promise_rejects_js(t,
+            TypeError,
+            register_method(script, {scope: scope}),
+            'Registration of script importing non-existent script should fail.');
+      }, 'Registering script importing non-existent script');
+  }
 
   promise_test(function(t) {
       var script = 'resources/malformed-worker.py?caught-exception';
