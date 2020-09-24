6d399685817245632c660d5a04c4a4478096616f
Stepan Khapugin <stkhapugin@chromium.org>
2020-09-24 17:13:26 +0000

    [multiball] Use the global dispatcher for BrowsingDataCommands.
    Instead of exposing MainController's conformance to BrowsingDataCommands
    use the app state's dispatcher to access it.
    
    Bug: 1045659
    Change-Id: I71bd5d4693d5d2eefdc67def9abb345a84d24ce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2418336
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810242}

--- ios/chrome/browser/ui/main/scene_controller.mm
+++ ios/chrome/browser/ui/main/scene_controller.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/url_formatter/url_formatter.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
+#import "ios/chrome/app/application_delegate/startup_information.h"
 #include "ios/chrome/app/application_delegate/tab_opening.h"
 #import "ios/chrome/app/application_delegate/url_opener.h"
 #import "ios/chrome/app/application_delegate/url_opener_params.h"
@@ -34,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/crash_report/breadcrumbs/features.h"
 #include "ios/chrome/browser/crash_report/crash_keys_helper.h"
 #include "ios/chrome/browser/crash_report/crash_report_helper.h"
+#import "ios/chrome/browser/crash_report/crash_restore_helper.h"
 #import "ios/chrome/browser/first_run/first_run.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/main/browser_list.h"
@@ -51,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/authentication/signin/signin_utils.h"
 #import "ios/chrome/browser/ui/browser_view/browser_view_controller.h"
 #import "ios/chrome/browser/ui/commands/browser_commands.h"
+#import "ios/chrome/browser/ui/commands/browsing_data_commands.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
 #import "ios/chrome/browser/ui/commands/omnibox_commands.h"
 #import "ios/chrome/browser/ui/commands/open_new_tab_command.h"
@@ -246,13 +249,18 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
 
 #pragma mark - Setters and getters
 
+- (id<BrowsingDataCommands>)browsingDataCommandsHandler {
+  return HandlerForProtocol(self.sceneState.appState.appCommandDispatcher,
+                            BrowsingDataCommands);
+}
+
 - (TabGridCoordinator*)mainCoordinator {
   if (!_mainCoordinator) {
     // Lazily create the main coordinator.
     TabGridCoordinator* tabGridCoordinator = [[TabGridCoordinator alloc]
                      initWithWindow:self.sceneState.window
          applicationCommandEndpoint:self
-        browsingDataCommandEndpoint:self.mainController
+        browsingDataCommandEndpoint:self.browsingDataCommandsHandler
                      regularBrowser:self.mainInterface.browser
                    incognitoBrowser:self.incognitoInterface.browser];
     _mainCoordinator = tabGridCoordinator;
@@ -543,7 +551,7 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
              initWithBrowserState:self.sceneState.appState.mainBrowserState
                        sceneState:self.sceneState
        applicationCommandEndpoint:self
-      browsingDataCommandEndpoint:self.mainController];
+      browsingDataCommandEndpoint:self.browsingDataCommandsHandler];
 
   // Ensure the main browser is created. This also creates the BVC.
   [self.browserViewWrangler createMainBrowser];
@@ -2118,7 +2126,7 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
   ChromeBrowserState* otrBrowserState =
       self.sceneState.appState.mainBrowserState
           ->GetOffTheRecordChromeBrowserState();
-  [self.mainController
+  [self.browsingDataCommandsHandler
       removeBrowsingDataForBrowserState:otrBrowserState
                              timePeriod:browsing_data::TimePeriod::ALL_TIME
                              removeMask:BrowsingDataRemoveMask::REMOVE_ALL
