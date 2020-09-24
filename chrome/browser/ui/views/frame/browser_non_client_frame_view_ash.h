f5e3cb319e5bd5e01c8c7a614635d4fc337e7c16
Antonio Gomes <tonikitoo@igalia.com>
2020-09-24 15:42:37 +0000

    Remove ash::switches::kAshEnableV1AppBackButton
    browser_non_client_frame_view_ash.cc includes
    ash/public/cpp/ash_switches.h to make use of the
    `kAshEnableV1AppBackButton` runtime switch. However, as
    for the bug linked to its declaration, the support is
    flagged for removal (https://crbug.com/749713 - WONTFIX'ed).
    
    This CL removes it, and the code associated with it, including
    if-blocks it was being checked and unittests.
    
    This is phase 2.3 in the design document [1].
    
    [1] https://docs.google.com/document/d/1xHwcHrAiEaWuA4usGEqKZ9zm1H8SGk3WkS-Jf2Q_los
    
    The idea is to move browser_non_client_frame_view_ash.cc|h away from
    using "ash" symbols. Ultimately, this file will be used by lacros/chrome
    builds.
    
    BUG=749713, 1113900
    
    Change-Id: Iab1a7766f0142e66abda34a265d1308b341b2328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2424637
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Antonio Gomes (GMT-4) <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#810219}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.h
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observer.h"
-#include "chrome/browser/command_observer.h"
 #include "chrome/browser/ui/views/frame/browser_frame_header_ash.h"
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
@@ -32,17 +31,12 @@ namespace ash {
 class FrameCaptionButtonContainerView;
 }  // namespace ash
 
-namespace views {
-class FrameCaptionButton;
-}  // namespace views
-
 // Provides the BrowserNonClientFrameView for Chrome OS.
 class BrowserNonClientFrameViewAsh
     : public BrowserNonClientFrameView,
       public BrowserFrameHeaderAsh::AppearanceProvider,
       public ash::TabletModeObserver,
       public TabIconViewModel,
-      public CommandObserver,
       public aura::WindowObserver,
       public ImmersiveModeController::Observer {
  public:
@@ -98,9 +92,6 @@ class BrowserNonClientFrameViewAsh
   bool ShouldTabIconViewAnimate() const override;
   gfx::ImageSkia GetFaviconForTabIconView() override;
 
-  // CommandObserver:
-  void EnabledStateChangedForCommand(int id, bool enabled) override;
-
   // aura::WindowObserver:
   void OnWindowDestroying(aura::Window* window) override;
   void OnWindowPropertyChanged(aura::Window* window,
@@ -134,8 +125,6 @@ class BrowserNonClientFrameViewAsh
                            AppHeaderVisibilityInTabletModeTest);
   FRIEND_TEST_ALL_PREFIXES(BrowserNonClientFrameViewAshTest,
                            ImmersiveModeTopViewInset);
-  FRIEND_TEST_ALL_PREFIXES(BrowserNonClientFrameViewAshBackButtonTest,
-                           V1BackButton);
   FRIEND_TEST_ALL_PREFIXES(BrowserNonClientFrameViewAshTest,
                            ToggleTabletModeOnMinimizedWindow);
   FRIEND_TEST_ALL_PREFIXES(WebAppNonClientFrameViewAshTest,
@@ -215,8 +204,6 @@ class BrowserNonClientFrameViewAsh
 
   // View which contains the window controls.
   ash::FrameCaptionButtonContainerView* caption_button_container_ = nullptr;
-
-  views::FrameCaptionButton* back_button_ = nullptr;
 
   // For popups, the window icon.
   TabIconView* window_icon_ = nullptr;
