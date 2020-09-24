3b44ba5e940cd02f9aa25fde12bd186290cedb69
Ken Rockot <rockot@google.com>
2020-09-24 16:38:54 +0000

    Allow basic 5G awareness for NCN
    On devices and Android versions which support 5G, this allows the
    NetworkChangeNotifier to update the ConnectionType to 5G when observing
    use of a network of type NETWORK_TYPE_NR, which corresponds exclusively
    to 5G standalone networks.
    
    The NCN Java code is also updated to treat TYPE_MOBILE_DUN and
    TYPE_MOBILE_HIPRI networks the same as TYPE_MOBILE. Currently they are
    ignored and treated as "unknown" network types, but they are legitimate
    values when on a cellular network, up to and including 5G.
    
    Bug: 1127134
    Change-Id: I5b054635a633970d26f4cfcc34b5a58376ed2fa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427104
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#810236}

--- net/android/java/src/org/chromium/net/NetworkChangeNotifierAutoDetect.java
+++ net/android/java/src/org/chromium/net/NetworkChangeNotifierAutoDetect.java
@@ -123,6 +123,8 @@ public class NetworkChangeNotifierAutoDetect extends BroadcastReceiver {
                 case ConnectivityManager.TYPE_BLUETOOTH:
                     return ConnectionSubtype.SUBTYPE_UNKNOWN;
                 case ConnectivityManager.TYPE_MOBILE:
+                case ConnectivityManager.TYPE_MOBILE_DUN:
+                case ConnectivityManager.TYPE_MOBILE_HIPRI:
                     // Use information from TelephonyManager to classify the connection.
                     switch (getNetworkSubType()) {
                         case TelephonyManager.NETWORK_TYPE_GPRS:
@@ -1100,6 +1102,8 @@ public class NetworkChangeNotifierAutoDetect extends BroadcastReceiver {
             case ConnectivityManager.TYPE_BLUETOOTH:
                 return ConnectionType.CONNECTION_BLUETOOTH;
             case ConnectivityManager.TYPE_MOBILE:
+            case ConnectivityManager.TYPE_MOBILE_DUN:
+            case ConnectivityManager.TYPE_MOBILE_HIPRI:
                 // Use information from TelephonyManager to classify the connection.
                 switch (subtype) {
                     case TelephonyManager.NETWORK_TYPE_GPRS:
@@ -1120,6 +1124,8 @@ public class NetworkChangeNotifierAutoDetect extends BroadcastReceiver {
                         return ConnectionType.CONNECTION_3G;
                     case TelephonyManager.NETWORK_TYPE_LTE:
                         return ConnectionType.CONNECTION_4G;
+                    case TelephonyManager.NETWORK_TYPE_NR:
+                        return ConnectionType.CONNECTION_5G;
                     default:
                         return ConnectionType.CONNECTION_UNKNOWN;
                 }
