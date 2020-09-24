6e4f9d20a69ebd15d6619c8d2e6b6e417696ac19
Christian Biesinger <cbiesinger@chromium.org>
2020-09-24 08:10:44 +0000

    [AspectRatio] In abspos, handle transferred min/max
    ..even if contain-intrinsic-size is specified.
    
    R=ikilpatrick@chromium.org, mstensho@chromium.org
    
    Bug: 1124922
    Change-Id: If4c887a3536b65fbeb1c22a8a2e3a66896046ae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2423584
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810105}

--- third_party/blink/renderer/core/layout/ng/ng_absolute_utils.h
+++ third_party/blink/renderer/core/layout/ng/ng_absolute_utils.h
@@ -22,7 +22,7 @@ struct NGLogicalStaticPosition;
 
 struct CORE_EXPORT NGLogicalOutOfFlowDimensions {
   NGBoxStrut inset;
-  LogicalSize size;
+  LogicalSize size = {kIndefiniteSize, kIndefiniteSize};
   NGBoxStrut margins;
 };
 
