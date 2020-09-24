94152953288ec53008f58b610820de3eec78ecbe
David Dorwin <ddorwin@chromium.org>
2020-09-24 18:52:15 +0000

    [fuchsia] Remove FILE_PATH_LITERAL() from Fuchsia code
    Fuchsia-specific code does not need to be compatible with Windows.
    
    Change-Id: I6dc72f36a261f4d3f50936ddde712640675b2ec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427845
    Auto-Submit: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810264}

--- fuchsia/engine/test/test_data.cc
+++ fuchsia/engine/test/test_data.cc
@@ -9,6 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cr_fuchsia {
 
-const char kTestServerRoot[] = FILE_PATH_LITERAL("fuchsia/engine/test/data");
+const char kTestServerRoot[] = "fuchsia/engine/test/data";
 
 }  // namespace cr_fuchsia
--- fuchsia/engine/web_engine_debug_integration_test.cc
+++ fuchsia/engine/web_engine_debug_integration_test.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char kTestServerRoot[] = FILE_PATH_LITERAL("fuchsia/engine/test/data");
+const char kTestServerRoot[] = "fuchsia/engine/test/data";
 
 }  // namespace
 
