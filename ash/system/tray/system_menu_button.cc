0980013c0ee7a21f49a94f5b5c0adafceeeefe96
minch <minch@chromium.org>
2020-09-24 21:17:39 +0000

    dark_mode: Unify inkdrop and highlight color.
    - Inkdrop ripple and highlight colors are based on the background color
      currently. And the color is WHITE in dark and BLACK in light mode.
      Removing the hard code, get the colors from AshColorProvider instead.
    - There is no need to set the inkdrop color on the underneath image's
      color now. Removing the |ink_drop_color_| related logic inside
      SystemMenuButton.
    
    Bug: 1107056
    Change-Id: Id67a5b22845aae5df6fa85be775e892f7a958c5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2426548
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810297}

--- ash/system/tray/system_menu_button.cc
+++ ash/system/tray/system_menu_button.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_popup_ink_drop_style.h"
 #include "ash/system/tray/tray_popup_utils.h"
+#include "ash/system/unified/unified_system_tray_view.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/animation/flood_fill_ink_drop_ripple.h"
@@ -63,30 +64,23 @@ void SystemMenuButton::SetVectorIcon(const gfx::VectorIcon& icon) {
 
 SystemMenuButton::~SystemMenuButton() = default;
 
-void SystemMenuButton::SetInkDropColor(SkColor color) {
-  ink_drop_color_ = color;
-}
-
 std::unique_ptr<views::InkDrop> SystemMenuButton::CreateInkDrop() {
   return TrayPopupUtils::CreateInkDrop(this);
 }
 
-// TODO(minch): Do not hard code the background color for InkDropRipple and
-// InkDropHighlight. Add it as a constructor argument to SystemMenuButton.
-// Then, |ink_drop_color_| related logic can be removed.
 std::unique_ptr<views::InkDropRipple> SystemMenuButton::CreateInkDropRipple()
     const {
   return TrayPopupUtils::CreateInkDropRipple(
       TrayPopupInkDropStyle::HOST_CENTERED, this,
       GetInkDropCenterBasedOnLastEvent(),
-      ink_drop_color_.value_or(SK_ColorWHITE));
+      UnifiedSystemTrayView::GetBackgroundColor());
 }
 
 std::unique_ptr<views::InkDropHighlight>
 SystemMenuButton::CreateInkDropHighlight() const {
   return TrayPopupUtils::CreateInkDropHighlight(
       TrayPopupInkDropStyle::HOST_CENTERED, this,
-      ink_drop_color_.value_or(SK_ColorWHITE));
+      UnifiedSystemTrayView::GetBackgroundColor());
 }
 
 const char* SystemMenuButton::GetClassName() const {
