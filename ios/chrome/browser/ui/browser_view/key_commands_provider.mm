255968cec1548970bee505ee93bd45d2b00c6f59
Mike Pinkerton <pinkerton@google.com>
2020-09-24 22:55:56 +0000

    Add additional keyboard shortcuts for next/prev tab.
    Add cmd-{/} keyboard shortcuts to match Mac desktop, in addition to the
    commands that are already there. Additionally fix up the directionality
    to take RTL into account.
    
    BUG=988313
    
    Change-Id: I9b8b69c30a85f0dbc9e38a5d8a6b72a97355d06f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427422
    Auto-Submit: Mike Pinkerton <pinkerton@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810322}

--- ios/chrome/browser/ui/browser_view/key_commands_provider.mm
+++ ios/chrome/browser/ui/browser_view/key_commands_provider.mm
@@ -67,6 +67,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     };
   }
 
+  // Blocks for next/previous tab.
+  void (^focusTabLeft)();
+  void (^focusTabRight)();
+  if (useRTLLayout) {
+    focusTabLeft = ^{
+      [weakConsumer focusNextTab];
+    };
+    focusTabRight = ^{
+      [weakConsumer focusPreviousTab];
+    };
+  } else {
+    focusTabLeft = ^{
+      [weakConsumer focusPreviousTab];
+    };
+    focusTabRight = ^{
+      [weakConsumer focusNextTab];
+    };
+  }
+
   // New tab blocks.
   void (^newTab)() = ^{
     OpenNewTabCommand* newTabCommand = [OpenNewTabCommand command];
@@ -170,6 +189,44 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                         [weakDispatcher closeCurrentTab];
                                       }
                                     }],
+    ]];
+
+    // Deal with the multiple next/previous tab commands we have, only one pair
+    // of which appears in the HUD. Take RTL into account for the direction.
+    const int tabLeftDescriptionID = useRTLLayout
+                                          ? IDS_IOS_KEYBOARD_NEXT_TAB
+                                          : IDS_IOS_KEYBOARD_PREVIOUS_TAB;
+    const int tabRightDescriptionID = useRTLLayout
+                                           ? IDS_IOS_KEYBOARD_PREVIOUS_TAB
+                                           : IDS_IOS_KEYBOARD_NEXT_TAB;
+    NSString* tabLeftTitle = l10n_util::GetNSStringWithFixup(
+        tabLeftDescriptionID);
+    NSString* tabRightTitle = l10n_util::GetNSStringWithFixup(
+        tabRightDescriptionID);
+    [keyCommands addObjectsFromArray:@[
+      [UIKeyCommand
+           cr_keyCommandWithInput:UIKeyInputLeftArrow
+                    modifierFlags:UIKeyModifierCommand | UIKeyModifierAlternate
+                            title:tabLeftTitle
+                           action:focusTabLeft],
+       [UIKeyCommand
+           cr_keyCommandWithInput:UIKeyInputRightArrow
+                    modifierFlags:UIKeyModifierCommand | UIKeyModifierAlternate
+                            title:tabRightTitle
+                           action:focusTabRight],
+       [UIKeyCommand
+           cr_keyCommandWithInput:@"{"
+                    modifierFlags:UIKeyModifierCommand
+                            title:nil
+                           action:focusTabLeft],
+       [UIKeyCommand
+           cr_keyCommandWithInput:@"}"
+                    modifierFlags:UIKeyModifierCommand
+                            title:nil
+                           action:focusTabRight],
+    ]];
+
+    [keyCommands addObjectsFromArray:@[
       [UIKeyCommand
           cr_keyCommandWithInput:@"d"
                    modifierFlags:UIKeyModifierCommand
@@ -340,20 +397,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                     action:^{
                                       focusTab([weakConsumer tabsCount] - 1);
                                     }],
-      [UIKeyCommand
-          cr_keyCommandWithInput:UIKeyInputLeftArrow
-                   modifierFlags:UIKeyModifierCommand | UIKeyModifierAlternate
-                           title:nil
-                          action:^{
-                            [weakConsumer focusPreviousTab];
-                          }],
-      [UIKeyCommand
-          cr_keyCommandWithInput:UIKeyInputRightArrow
-                   modifierFlags:UIKeyModifierCommand | UIKeyModifierAlternate
-                           title:nil
-                          action:^{
-                            [weakConsumer focusNextTab];
-                          }],
       [UIKeyCommand
           cr_keyCommandWithInput:@"\t"
                    modifierFlags:UIKeyModifierControl | UIKeyModifierShift
