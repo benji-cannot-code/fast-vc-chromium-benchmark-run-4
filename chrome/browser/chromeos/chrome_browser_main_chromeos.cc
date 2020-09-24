3bc5387ea9dd883e81d9e7479d378567fc510c2e
Kyle Williams <kdgwill@google.com>
2020-09-24 22:50:01 +0000

    cfm: Implement Cfm Observer List
    Populates an Observer List to be triggered by a Pending Service Request
    DBus Signal from the system CfmServiceContext service, providing access to chromium services and APIs.
    
    New mojoms are added for APIs which are tailored to the services that
    require them such as messaging_layer.
    
    Services are added to the primary cfm chromium mojom pending security
    reviews such as cros_healthd
    
    Note: The primary goal of this cl is to establish the correct directory
    structure for registering new services to the |CfmServiceContext|.
    
    Future projects requires dependencies on directories including but not
    limited to:
     * //chromium/src/services/video_capture/
     * //chrome/browser/policy/messaging_layer/
     * //chromeos/services/cros_healthd/
     * etc.
    
    BUG=chromium:1105567, b:165864612
    TEST='autoninja -C out/Default chromeos:chromeos_unittests \
           && ./out/Default/chromeos_unittests --dbus-stub \
             --single-process-tests --gtest_filter=CfmHotlineClientTest.*'
    
    Change-Id: Id0c687a88b126dc10975803020b1ec431af6ebaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2419799
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810319}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -167,6 +167,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/network_cert_loader.h"
 #include "chromeos/network/network_handler.h"
 #include "chromeos/network/portal_detector/network_portal_detector_stub.h"
+#include "chromeos/services/cfm/public/buildflags/buildflags.h"  // PLATFORM_CFM
 #include "chromeos/services/cros_healthd/public/cpp/service_connection.h"
 #include "chromeos/system/statistics_provider.h"
 #include "chromeos/tpm/install_attributes.h"
@@ -210,6 +211,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_features.h"
 #include "ui/chromeos/events/pref_names.h"
 #include "ui/events/event_utils.h"
+
+#if BUILDFLAG(PLATFORM_CFM)
+#include "chrome/browser/chromeos/cfm/cfm_chrome_services.h"
+#endif
 
 #if BUILDFLAG(ENABLE_RLZ)
 #include "components/rlz/rlz_tracker.h"
@@ -630,6 +635,10 @@ void ChromeBrowserMainPartsChromeos::PreMainMessageLoopRun() {
     breakpad_consent_watcher_ =
         system::BreakpadConsentWatcher::Initialize(stats_controller);
   }
+
+#if BUILDFLAG(PLATFORM_CFM)
+  chromeos::cfm::InitializeCfmServices();
+#endif  // BUILDFLAG(PLATFORM_CFM)
 
   ChromeBrowserMainPartsLinux::PreMainMessageLoopRun();
 }
--- chromeos/dbus/cfm/cfm_hotline_client.h
+++ chromeos/dbus/cfm/cfm_hotline_client.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_file.h"
 #include "dbus/object_proxy.h"
 
+#include "chromeos/dbus/cfm/cfm_observer.h"
+
 namespace dbus {
 class Bus;
 }
@@ -64,6 +66,13 @@ class COMPONENT_EXPORT(CFM_HOTLINE_CLIENT) CfmHotlineClient {
   virtual void BootstrapMojoConnection(
       base::ScopedFD fd,
       BootstrapMojoConnectionCallback result_callback) = 0;
+
+  // Adds an observer instance to the observers list to listen on changes like
+  // DLC state change, etc.
+  virtual void AddObserver(cfm::CfmObserver* observer) = 0;
+
+  // Removes an observer from observers list.
+  virtual void RemoveObserver(cfm::CfmObserver* observer) = 0;
 
  protected:
   // Initialize/Shutdown should be used instead.
