f0a91452149aba4d80618a830076e5aa53d6764a
Josh Nohle <nohle@google.com>
2020-09-25 15:27:47 +0000

    [Nearby] Uprev nearby-connections to 8482024f9c944ff0ef778f74308876dbfa00d4ac
    The following changes to //chrome/services/sharing/nearby/platform_v2/
    were necessary to support this uprev:
    (1) BluetoothClassicMedium::FindRemoteDevice() was changed to
        BluetoothClassicMedium::GetRemoteDevice().
    (2) The argument |fast_advertisement_service_uuid| was added to
        BleMedium::StartScanning() but was not used therein.
    
    Bug: 1132108
    Change-Id: I54f7e508cecdf3f611f0ecbed22041c065fe73d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2430293
    Auto-Submit: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810630}

--- chrome/services/sharing/nearby/platform_v2/bluetooth_classic_medium.cc
+++ chrome/services/sharing/nearby/platform_v2/bluetooth_classic_medium.cc
@@ -112,7 +112,7 @@ BluetoothClassicMedium::ListenForService(const std::string& service_name,
   return nullptr;
 }
 
-BluetoothDevice* BluetoothClassicMedium::FindRemoteDevice(
+BluetoothDevice* BluetoothClassicMedium::GetRemoteDevice(
     const std::string& mac_address) {
   auto it = discovered_bluetooth_devices_map_.find(mac_address);
   return it == discovered_bluetooth_devices_map_.end() ? nullptr : &it->second;
