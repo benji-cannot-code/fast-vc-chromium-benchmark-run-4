b8ecd7df8e56397ff86d2232a36417c0dce85cf5
Eric Orth <ericorth@chromium.org>
2020-09-25 21:36:19 +0000

    Move DnsConfigOverrides to /net/dns/public/
    This struct is designed for use outside the network stack and is used as
    part of the NetworkService mojo API.
    
    Bug: 1129993
    Change-Id: I6990e3f65a6a8a5a4d10d2f2f182288c4a0fc697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427024
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810785}

--- chrome/browser/net/dns_probe_service.h
+++ chrome/browser/net/dns_probe_service.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "components/error_page/common/net_error_info.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "net/dns/dns_config_overrides.h"
+#include "net/dns/public/dns_config_overrides.h"
 
 namespace chrome_browser_net {
 
--- net/dns/public/dns_config_overrides.h
+++ net/dns/public/dns_config_overrides.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_DNS_DNS_CONFIG_OVERRIDES_H_
-#define NET_DNS_DNS_CONFIG_OVERRIDES_H_
+#ifndef NET_DNS_PUBLIC_DNS_CONFIG_OVERRIDES_H_
+#define NET_DNS_PUBLIC_DNS_CONFIG_OVERRIDES_H_
 
 #include <string>
 #include <vector>
@@ -71,4 +71,4 @@ struct NET_EXPORT DnsConfigOverrides {
 
 }  // namespace net
 
-#endif  // NET_DNS_DNS_CONFIG_OVERRIDES_H_
+#endif  // NET_DNS_PUBLIC_DNS_CONFIG_OVERRIDES_H_
