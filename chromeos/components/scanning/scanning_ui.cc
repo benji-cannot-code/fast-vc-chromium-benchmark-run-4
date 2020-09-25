ab251bafeaf467c4decd561479505a300a809db5
Jesse Schettler <jschettler@chromium.org>
2020-09-25 02:06:59 +0000

    scanning: Add string for scanner dropdown label
    Add a finalized string for the scanner dropdown label and implement i18n
    for strings.
    
    Bug: 1059779
    Change-Id: Ia216da7e9c9fc5a6b8928aa02e0889ade65acdad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429227
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810405}

--- chromeos/components/scanning/scanning_ui.cc
+++ chromeos/components/scanning/scanning_ui.cc
@@ -13,10 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/components/scanning/url_constants.h"
 #include "chromeos/grit/chromeos_scanning_app_resources.h"
 #include "chromeos/grit/chromeos_scanning_app_resources_map.h"
+#include "chromeos/strings/grit/chromeos_strings.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "services/network/public/mojom/content_security_policy.mojom.h"
+#include "ui/base/webui/web_ui_util.h"
 #include "ui/resources/grit/webui_resources.h"
 
 namespace chromeos {
@@ -45,6 +47,16 @@ void SetUpWebUIDataSource(content::WebUIDataSource* source,
   source->AddResourcePath("test_loader.js", IDR_WEBUI_JS_TEST_LOADER);
 }
 
+void AddScanningAppStrings(content::WebUIDataSource* html_source) {
+  static constexpr webui::LocalizedString kLocalizedStrings[] = {
+      {"scannerDropdownLabel", IDS_SCANNING_APP_SCANNER_DROPDOWN_LABEL}};
+
+  for (const auto& str : kLocalizedStrings)
+    html_source->AddLocalizedString(str.name, str.id);
+
+  html_source->UseStringsJs();
+}
+
 }  // namespace
 
 ScanningUI::ScanningUI(content::WebUI* web_ui, BindScanServiceCallback callback)
@@ -64,6 +76,8 @@ ScanningUI::ScanningUI(content::WebUI* web_ui, BindScanServiceCallback callback)
 
   html_source->AddResourcePath("scanning.mojom-lite.js",
                                IDR_SCANNING_MOJO_LITE_JS);
+
+  AddScanningAppStrings(html_source.get());
 
   content::WebUIDataSource::Add(web_ui->GetWebContents()->GetBrowserContext(),
                                 html_source.release());
