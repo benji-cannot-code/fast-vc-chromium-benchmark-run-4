bbba30c404f9e8c2888d10e32373d8ccc2cdbf78
Minoru Chikamune <chikamune@chromium.org>
2020-09-25 08:14:46 +0000

    [MBI] Add TRACE_EVENTs to show if the ThreadTaskRunnerHandle::Get() returns default task runner from MTS or AGS.
    [related design doc]
    https://docs.google.com/document/d/1y-vHkrD1z2RtyWYwT6rJkSLHClYNjDDpUbTtBU7l95A
    
    Bug: 1105403
    Change-Id: Iab2c3c9e187f6961f3924222c8ba35c29be82afd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414329
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Tal Pressman <talp@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810512}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -167,6 +167,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("WebCore")                                                           \
   X("webrtc")                                                            \
   X("xr")                                                                \
+  X(TRACE_DISABLED_BY_DEFAULT("agent_scheduling_group"))                 \
   X(TRACE_DISABLED_BY_DEFAULT("animation-worklet"))                      \
   X(TRACE_DISABLED_BY_DEFAULT("audio"))                                  \
   X(TRACE_DISABLED_BY_DEFAULT("audio-worklet"))                          \
