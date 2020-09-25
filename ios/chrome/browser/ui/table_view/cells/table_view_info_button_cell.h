41b7d894df4de74f20810c72c1dc02af490fa2b6
Tina Wang <tinazwang@google.com>
2020-09-25 21:47:03 +0000

    [ios] Refactor the exposed accessibilityHint
    Previously we check if the accessibilityHint is set in accessibilityHint method, which blocks the EG test when printing out the UI hierarchy by calling self.accessibilityHint infinitely. Refactor it to customizedAccessibilityHint and expose it in TableViewInfoButtonCell resolve this and make the logic clear. Because for now only settings UI are using TableViewInfoButtonCell and the accessibilityHint for it is customized.
    
    This CL is followed by EG test for settings UI.
    
    Change-Id: Ic56eb1fdd235bca9da06d076b31ee8c9537c805f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2431725
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810804}

--- ios/chrome/browser/ui/table_view/cells/table_view_info_button_cell.h
+++ ios/chrome/browser/ui/table_view/cells/table_view_info_button_cell.h
@@ -35,6 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // UIButton displayed aligned to the trailing of the view.
 @property(nonatomic, readonly, strong) UIButton* trailingButton;
 
+// The customized accessibility hint text string.
+@property(nonatomic, copy) NSString* customizedAccessibilityHint;
+
 // Sets the icon |image| and tint |color| for it that should be displayed at the
 // leading edge of the cell. If set to nil, the icon will be hidden and the
 // remaining content will expand to fill the full width of the cell.
--- ios/chrome/browser/ui/table_view/cells/table_view_info_button_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_info_button_item.mm
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
   cell.statusTextLabel.text = self.statusText;
   if (self.accessibilityHint) {
-    cell.accessibilityHint = self.accessibilityHint;
+    cell.customizedAccessibilityHint = self.accessibilityHint;
   }
   cell.selectionStyle = UITableViewCellSelectionStyleNone;
 
