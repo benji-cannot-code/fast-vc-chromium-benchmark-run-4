edbd0bc22db0257de3aa2924766b82962ed75b92
Dave Tapuska <dtapuska@chromium.org>
2020-09-25 16:56:02 +0000

    Remove WebFloatSize is is no longer used.
    WebFloatSize has been replaced with gfx::SizeF and is no
    longer used.
    
    BUG=1097816
    
    Change-Id: Ifa45a28d412fb134614e4447e196e5480db760e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2428827
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810679}

--- content/common/frame_messages.h
+++ content/common/frame_messages.h
@@ -69,7 +69,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/security_context/insecure_request_policy.mojom.h"
 #include "third_party/blink/public/mojom/web_feature/web_feature.mojom.h"
 #include "third_party/blink/public/platform/viewport_intersection_state.h"
-#include "third_party/blink/public/platform/web_float_size.h"
 #include "third_party/blink/public/web/web_frame_owner_properties.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
@@ -139,11 +138,6 @@ IPC_STRUCT_TRAITS_BEGIN(content::Impression)
   IPC_STRUCT_TRAITS_MEMBER(reporting_origin)
   IPC_STRUCT_TRAITS_MEMBER(impression_data)
   IPC_STRUCT_TRAITS_MEMBER(expiry)
-IPC_STRUCT_TRAITS_END()
-
-IPC_STRUCT_TRAITS_BEGIN(blink::WebFloatSize)
-  IPC_STRUCT_TRAITS_MEMBER(width)
-  IPC_STRUCT_TRAITS_MEMBER(height)
 IPC_STRUCT_TRAITS_END()
 
 IPC_STRUCT_TRAITS_BEGIN(content::UntrustworthyContextMenuParams)
