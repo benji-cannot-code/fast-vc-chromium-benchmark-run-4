769342873d0273c6eb4bf6b9fe2246993856495b
Ahmed Mehfooz <amehfooz@chromium.org>
2020-09-25 02:17:11 +0000

    Create separate class to handle creation of holding space bubble
    This class will own pinned_files_container and recent_files_container.
    It will also be responsible for setting the max size for the pinned
    files container based on available screen space and update it when
    screen space changes.
    
    Bug: 1131268
    Change-Id: If09810570e59f7beeac0667e84d4cd558ee67bc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427081
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810423}

--- ash/public/cpp/holding_space/holding_space_constants.h
+++ ash/public/cpp/holding_space/holding_space_constants.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 // Appearance.
+constexpr int kHoldingSpaceBubbleWidth = 360;
 constexpr gfx::Insets kHoldingSpaceContainerPadding(16);
 constexpr int kHoldingSpaceContainerChildSpacing = 16;
 constexpr int kHoldingSpaceContainerSpacing = 8;
