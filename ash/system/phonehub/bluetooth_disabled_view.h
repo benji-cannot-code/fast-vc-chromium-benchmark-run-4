53aa46472c9c2a469334374372c6c68db82d6560
Meilin Wang <meilinw@google.com>
2020-09-25 23:52:33 +0000

    [CrOS PhoneHub] Add phone status view for edge case UIs.
    This CL moves the existing |PhoneStatusView| out of the |PhoneHubView|
    and adds it as a separate child view of the bubble, so that it can always
    show on top of UIs and display available phone information and the
    settings icon.
    
    Misc: remove unnecessary header files.
    
    Screenshot: https://screenshot.googleplex.com/6DXqin6MZPcTVVj.png
    
    BUG=1106937,1126208
    
    Change-Id: I321d0511874cfa95dcbb8276fedf41b14e0f684b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429390
    Reviewed-by: Tim Song <tengs@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Meilin Wang <meilinw@chromium.org>
    Commit-Queue: Meilin Wang <meilinw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810830}

--- ash/system/phonehub/bluetooth_disabled_view.h
+++ ash/system/phonehub/bluetooth_disabled_view.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_PHONEHUB_BLUETOOTH_DISABLED_VIEW_H_
 
 #include "ash/ash_export.h"
-#include "ash/system/phonehub/phone_hub_interstitial_view.h"
 #include "ui/views/controls/button/button.h"
 
 namespace ash {
--- ash/system/phonehub/initial_connecting_view.h
+++ ash/system/phonehub/initial_connecting_view.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_PHONEHUB_INITIAL_CONNECTING_VIEW_H_
 
 #include "ash/ash_export.h"
-#include "ash/system/phonehub/phone_hub_interstitial_view.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/metadata/metadata_header_macros.h"
 
