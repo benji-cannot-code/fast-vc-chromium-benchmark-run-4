7a4467bf70020f7cdfef717e1517188d244e5626
Wei Li <weili@chromium.org>
2020-09-25 02:03:53 +0000

    Add builder macro to Separator class
    Add builder macro to allow using Separator builder class.
    
    Bug: 1130078
    Change-Id: I50ce0b279a75e86bb328228f2f9ccb606ddc0a1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427583
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810400}

--- ui/views/controls/separator.h
+++ ui/views/controls/separator.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/optional.h"
+#include "ui/views/metadata/view_factory.h"
 #include "ui/views/view.h"
 
 namespace views {
@@ -42,6 +43,11 @@ class VIEWS_EXPORT Separator : public View {
 
   DISALLOW_COPY_AND_ASSIGN(Separator);
 };
+
+BEGIN_VIEW_BUILDER(VIEWS_EXPORT, Separator, View)
+VIEW_BUILDER_PROPERTY(SkColor, Color)
+VIEW_BUILDER_PROPERTY(int, PreferredHeight)
+END_VIEW_BUILDER(VIEWS_EXPORT, Separator)
 
 }  // namespace views
 
