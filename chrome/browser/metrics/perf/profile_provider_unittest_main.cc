66c054a77e1975d33d157dc0f116dd9bef18192c
Nina Satragno <nsatragno@chromium.org>
2020-09-25 15:08:04 +0000

    sheriff: disable ProfileProviderRealCollectionTest.OnJankStopped
    Disable ProfileProviderRealCollectionTest.OnJankStopped. It's flaky on
    chromeos-amd64-generic-rel.
    
    Bug: 1132045
    Tbr: gmx@chromium.org
    
    Change-Id: I3a84cfb16083b1b7075e6e1d545e723e62475ddb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429607
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Gabriel Marin <gmx@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810620}

--- chrome/browser/metrics/perf/profile_provider_unittest_main.cc
+++ chrome/browser/metrics/perf/profile_provider_unittest_main.cc
@@ -220,7 +220,7 @@ TEST_F(ProfileProviderRealCollectionTest, OnJankStarted) {
   ASSERT_TRUE(profile.has_perf_data());
 }
 
-TEST_F(ProfileProviderRealCollectionTest, OnJankStopped) {
+TEST_F(ProfileProviderRealCollectionTest, DISABLED_OnJankStopped) {
   // Trigger a resume from suspend.
   profile_provider_->OnJankStarted();
 
