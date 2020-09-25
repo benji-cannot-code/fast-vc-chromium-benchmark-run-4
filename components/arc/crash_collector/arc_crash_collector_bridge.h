9673e584ef4108066d5b036e411a1b8f0377a326
Kimiyuki Onaka <kimiyuki@google.com>
2020-09-25 23:57:12 +0000

    Add DumpNativeCrash interface
    ArcCrashCollector calls DumpNativeCrash with file names and FDs when a
    new crash dump appears. DumpNativeCrash is responsible to call
    crash_reporter. FDs are used for passing the file contents.
    
    BUG=b:136697350
    TEST=tast run my_crbook arc.NativeCrash.\* arc.AppCrash.\*
    
    Change-Id: I5017d388eb3182454023b354c3714bbecc664354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379331
    Commit-Queue: Kimiyuki Onaka <kimiyuki@google.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810839}

--- components/arc/crash_collector/arc_crash_collector_bridge.h
+++ components/arc/crash_collector/arc_crash_collector_bridge.h
@@ -33,11 +33,17 @@ class ArcCrashCollectorBridge
   ArcCrashCollectorBridge(content::BrowserContext* context,
                           ArcBridgeService* bridge);
 
+  ArcCrashCollectorBridge(const ArcCrashCollectorBridge&) = delete;
+  ArcCrashCollectorBridge& operator=(const ArcCrashCollectorBridge&) = delete;
+
   ~ArcCrashCollectorBridge() override;
 
   // mojom::CrashCollectorHost overrides.
   void DumpCrash(const std::string& type, mojo::ScopedHandle pipe) override;
-
+  void DumpNativeCrash(const std::string& exec_name,
+                       int32_t pid,
+                       int64_t timestamp,
+                       mojo::ScopedHandle minidump_fd) override;
   void SetBuildProperties(
       const std::string& device,
       const std::string& board,
@@ -45,14 +51,14 @@ class ArcCrashCollectorBridge
       const base::Optional<std::string>& fingerprint) override;
 
  private:
+  std::vector<std::string> CreateCrashReporterArgs();
+
   ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
 
   std::string device_;
   std::string board_;
   std::string cpu_abi_;
   base::Optional<std::string> fingerprint_;
-
-  DISALLOW_COPY_AND_ASSIGN(ArcCrashCollectorBridge);
 };
 
 }  // namespace arc
