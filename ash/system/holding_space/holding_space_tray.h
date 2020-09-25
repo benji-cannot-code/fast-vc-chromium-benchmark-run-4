dcca2fc71b2b9d436f54dc4727f93abf17d2970f
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-09-25 09:00:02 +0000

    Revert "Create separate class to handle creation of holding space bubble"
    This reverts commit 349707ef647ba693c015627c659dcb2e225f93b0.
    
    Reason for revert: Suspected culprit of https://crbug.com/1132219.
    
    Original change's description:
    > Create separate class to handle creation of holding space bubble
    >
    > This class will own pinned_files_container and recent_files_container.
    > It will also be responsible for setting the max size for the pinned
    > files container based on available screen space and update it when
    > screen space changes.
    >
    > Bug: 1131268
    > Change-Id: If09810570e59f7beeac0667e84d4cd558ee67bc0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427081
    > Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    > Reviewed-by: David Black <dmblack@google.com>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#810423}
    
    TBR=xiyuan@chromium.org,dmblack@google.com,amehfooz@chromium.org
    
    Change-Id: Ia221abfb393dcd7f9cdfa7e539133477bd7852ca
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1131268
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429275
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810524}

--- ash/system/holding_space/holding_space_tray.h
+++ ash/system/holding_space/holding_space_tray.h
@@ -9,15 +9,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/ash_export.h"
-#include "ash/system/holding_space/holding_space_tray_bubble.h"
+#include "ash/system/holding_space/holding_space_item_view_delegate.h"
 #include "ash/system/tray/tray_background_view.h"
 #include "base/memory/weak_ptr.h"
+
+namespace gfx {
+class Point;
+}
 
 namespace views {
 class ImageView;
 }
 
 namespace ash {
+
+class PinnedFilesContainer;
+class RecentFilesContainer;
+class TrayBubbleWrapper;
+
 // The HoldingSpaceTray shows the tray button in the bottom area of the screen.
 // This class also controls the lifetime for all of the tools available in the
 // palette. HoldingSpaceTray has one instance per-display.
@@ -27,6 +36,10 @@ class ASH_EXPORT HoldingSpaceTray : public TrayBackgroundView {
   HoldingSpaceTray(const HoldingSpaceTray& other) = delete;
   HoldingSpaceTray& operator=(const HoldingSpaceTray& other) = delete;
   ~HoldingSpaceTray() override;
+
+  // Returns true if the tray contains the given point. This is useful
+  // for determining if an event should be propagated through to the palette.
+  bool ContainsPointInScreen(const gfx::Point& point);
 
   // TrayBackgroundView:
   void ClickedOutsideBubble() override;
@@ -38,6 +51,7 @@ class ASH_EXPORT HoldingSpaceTray : public TrayBackgroundView {
   bool PerformAction(const ui::Event& event) override;
   void CloseBubble() override;
   void ShowBubble(bool show_by_click) override;
+  TrayBubbleView* GetBubbleView() override;
   const char* GetClassName() const override;
 
  private:
@@ -46,7 +60,14 @@ class ASH_EXPORT HoldingSpaceTray : public TrayBackgroundView {
   bool ShouldEnableExtraKeyboardAccessibility() override;
   void HideBubble(const TrayBubbleView* bubble_view) override;
 
-  std::unique_ptr<HoldingSpaceTrayBubble> bubble_;
+  // The singleton delegate for `HoldingSpaceItemView`s that implements support
+  // for context menu, drag-and-drop, and multiple selection.
+  HoldingSpaceItemViewDelegate delegate_;
+
+  std::unique_ptr<TrayBubbleWrapper> bubble_;
+
+  PinnedFilesContainer* pinned_files_container_ = nullptr;
+  RecentFilesContainer* recent_files_container_ = nullptr;
 
   // Weak pointer, will be parented by TrayContainer for its lifetime.
   views::ImageView* icon_ = nullptr;
