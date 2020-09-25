e5764ce713f605b53374fa93392e964fe6361b05
Sunny Sachanandani <sunnyps@chromium.org>
2020-09-25 03:31:41 +0000

    gpu: Remove obsolete direct composition feature flags
    Remove the NV12, decode swap chain, and underlay feature flags since
    these features have shipped for quite some time.  The NV12 feature flag
    was also used for using YUY2 over NV12 for tests so introduce a new flag
    for choosing the default SDR video overlay swap chain format.  Add tests
    for NV12, YUY2, and BGRA instead of just YUY2.  Also cleanup some of the
    testing code to be consistent in other_args, switch names, etc.
    
    Bug: 869677
    Change-Id: I0831b103506f394d6fe26bf1085401e2e2d50599
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2424576
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810453}

--- components/viz/service/display/overlay_dc_unittest.cc
+++ components/viz/service/display/overlay_dc_unittest.cc
@@ -262,8 +262,6 @@ class DCLayerOverlayTest : public testing::Test {
 };
 
 TEST_F(DCLayerOverlayTest, Occluded) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kDirectCompositionUnderlays);
   {
     auto pass = CreateRenderPass();
     SharedQuadState* first_shared_state = pass->shared_quad_state_list.back();
@@ -361,8 +359,6 @@ TEST_F(DCLayerOverlayTest, Occluded) {
 }
 
 TEST_F(DCLayerOverlayTest, DamageRectWithoutVideoDamage) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kDirectCompositionUnderlays);
   {
     auto pass = CreateRenderPass();
     SharedQuadState* shared_quad_state = pass->shared_quad_state_list.back();
@@ -470,9 +466,6 @@ TEST_F(DCLayerOverlayTest, DamageRect) {
 }
 
 TEST_F(DCLayerOverlayTest, ClipRect) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kDirectCompositionUnderlays);
-
   // Process twice. The second time through the overlay list shouldn't change,
   // which will allow the damage rect to reflect just the changes in that
   // frame.
@@ -545,9 +538,6 @@ TEST_F(DCLayerOverlayTest, TransparentOnTop) {
 }
 
 TEST_F(DCLayerOverlayTest, UnderlayDamageRectWithQuadOnTopUnchanged) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kDirectCompositionUnderlays);
-
   for (int i = 0; i < 3; i++) {
     auto pass = CreateRenderPass();
     // Add a solid color quad on top
