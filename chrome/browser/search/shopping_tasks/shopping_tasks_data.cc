99efff89213dc8c704b7f7ef0de15d15ba562617
Tibor Goldschwendt <tiborg@chromium.org>
2020-09-25 00:24:59 +0000

    [ntp][modules] Add service to provide shopping tasks
    This is in preparation to render a shopping task module on the NTP. In
    follow up CLs, we will use the shopping tasks service to query available
    tasks upon NTP load. The shopping tasks service queries GWS for
    available tasks and expects GWS to respond with a JSON like the one
    below:
    
    {
      "update": {
        "shopping_tasks": [
          {
            "title": "hello world",
            "products": [
              {
                "name": "foo",
                "image_url": "https://foo.com",
                "price": "$500",
                "info": "visited 5 days ago",
                "target_url": "https://google.com/foo"
              }
            ],
            "related_searches": [
              {
                "text": "baz",
                "target_url": "https://google.com/baz"
              }
            ]
          }
        ]
      }
    }
    
    Fixed: 1130852
    Change-Id: I544a2034540d132869b7d55da5b22b7e614358c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2426767
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810372}

---
+++ chrome/browser/search/shopping_tasks/shopping_tasks_data.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/search/shopping_tasks/shopping_tasks_data.h"
+
+ShoppingTasksData::Product::Product() = default;
+ShoppingTasksData::Product::Product(const Product&) = default;
+ShoppingTasksData::Product::~Product() = default;
+ShoppingTasksData::RelatedSearch::RelatedSearch() = default;
+ShoppingTasksData::RelatedSearch::RelatedSearch(const RelatedSearch&) = default;
+ShoppingTasksData::RelatedSearch::~RelatedSearch() = default;
+ShoppingTasksData::ShoppingTasksData() = default;
+ShoppingTasksData::ShoppingTasksData(const ShoppingTasksData&) = default;
+ShoppingTasksData::~ShoppingTasksData() = default;
