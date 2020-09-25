ee04f8dc3651445856960a796580f749312160e5
Mike Wittman <wittman@chromium.org>
2020-09-25 21:45:52 +0000

    Revert "[Sampling profiler] Move ENABLE_ARM_CFI_TABLE to profiler header"
    This reverts commit 74826cab877ec2c5a8af6b0ce191a7e59fbc7755.
    
    Reason for revert: testing whether this is responsible for rendering.mobile perf regression
    
    Original change's description:
    > [Sampling profiler] Move ENABLE_ARM_CFI_TABLE to profiler header
    >
    > ENABLE_ARM_CFI_TABLE is in service of the sampling profiler on Android,
    > and is unrelated to any debugging functionality, so move it to a
    > buildflag header under base/profiler.
    >
    > Also add the required includes to the files that use it.
    >
    > Bug: 1129939
    > Change-Id: I884b2128fc2ab2f6f1bec2343c59134448328529
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2421443
    > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Auto-Submit: Mike Wittman <wittman@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#809167}
    
    TBR=thestig@chromium.org,wittman@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1130974, 1129939
    Change-Id: I00050d0bf8cd7988d0ad8969f0b8a29e8accf669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2432308
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810801}

--- base/profiler/stack_sampling_profiler.cc
+++ base/profiler/stack_sampling_profiler.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/singleton.h"
-#include "base/profiler/profiler_buildflags.h"
 #include "base/profiler/stack_buffer.h"
 #include "base/profiler/stack_sampler.h"
 #include "base/profiler/unwinder.h"
--- base/profiler/stack_sampling_profiler_unittest.cc
+++ base/profiler/stack_sampling_profiler_unittest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/metrics_hashes.h"
-#include "base/profiler/profiler_buildflags.h"
 #include "base/profiler/sample_metadata.h"
 #include "base/profiler/stack_sampler.h"
 #include "base/profiler/stack_sampling_profiler.h"
