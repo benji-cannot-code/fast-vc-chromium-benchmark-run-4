f15862f1d65c4d6293a24e458d43dc08758c9125
Esmael El-Moslimany <aee@chromium.org>
2020-09-25 16:47:56 +0000

    WebUI NTP: style the middle-slot promo to match modules
    The restyle to match the modules is only enabled when the ntp-modules
    feature flag is enabled.
    
    Bug: 1110067
    Change-Id: Ib394df60498bca8845b3cbd49bd49cdedd72c2c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429230
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810673}

--- chrome/browser/resources/new_tab_page/middle_slot_promo.js
+++ chrome/browser/resources/new_tab_page/middle_slot_promo.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/hidden_style_css.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {BrowserProxy} from './browser_proxy.js';
@@ -22,6 +23,17 @@ class MiddleSlotPromoElement extends PolymerElement {
 
   static get template() {
     return html`{__html_template__}`;
+  }
+
+  static get properties() {
+    return {
+      /** @private */
+      modulesEnabled_: {
+        type: Boolean,
+        value: () => loadTimeData.getBoolean('modulesEnabled'),
+        reflectToAttribute: true,
+      },
+    };
   }
 
   constructor() {
