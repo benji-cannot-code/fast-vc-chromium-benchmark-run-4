ef4a97e3b6bc67ef0de6a9368771d4feea12b83c
Olga Sharonova <olka@chromium.org>
2020-09-25 21:34:13 +0000

    Add histograms for PlatformThread realtime period on Mac
    To be observed during experimentation with "OptimizedRealtimeThreadingMac"
    feature. Should be removed after the feature launched (or cancelled).
    
    Bug: 1112952, 1132225
    Change-Id: Id8dad2f4de06f61cf3ac1c2e3ba470a5cf223b75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429273
    Auto-Submit: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810782}

--- base/threading/platform_thread_mac.mm
+++ base/threading/platform_thread_mac.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/mac/foundation_util.h"
 #include "base/mac/mach_logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/threading/thread_id_name_manager.h"
 #include "base/threading/threading_features.h"
 #include "build/build_config.h"
@@ -210,6 +211,15 @@ void SetPriorityRealtimeAudio(TimeDelta realtime_period) {
                         THREAD_TIME_CONSTRAINT_POLICY_COUNT);
   MACH_DVLOG_IF(1, result != KERN_SUCCESS, result) << "thread_policy_set";
 
+  UmaHistogramCustomMicrosecondsTimes(
+      "PlatformThread.Mac.AttemptedRealtimePeriod", realtime_period,
+      base::TimeDelta(), base::TimeDelta::FromMilliseconds(100), 100);
+
+  if (result == KERN_SUCCESS) {
+    UmaHistogramCustomMicrosecondsTimes(
+        "PlatformThread.Mac.SucceededRealtimePeriod", realtime_period,
+        base::TimeDelta(), base::TimeDelta::FromMilliseconds(100), 100);
+  }
   return;
 }
 
