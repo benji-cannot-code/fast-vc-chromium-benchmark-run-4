aa48499a01f6504551ae5ac29a4e5a13fcf6579c
Colin Blundell <blundell@chromium.org>
2020-09-25 08:25:32 +0000

    [Extensions] Remove API permission for deleted cast.streaming API
    This API was removed in
    https://chromium-review.googlesource.com/c/chromium/src/+/2225821. The
    continued presence of this permissions check for the deleted API causes
    a crash in debug builds. This CL removes the check, adjusts associated
    unittests, and deprecates the permission.
    
    Bug: 986981
    Change-Id: I1b9a6596d8dd0af5a80f0fd2348b11569273cbaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2428923
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810515}

--- chrome/common/extensions/permissions/permission_set_unittest.cc
+++ chrome/common/extensions/permissions/permission_set_unittest.cc
@@ -750,7 +750,6 @@ TEST(PermissionsTest, PermissionMessages) {
   skip.insert(APIPermission::kAppView);
   skip.insert(APIPermission::kAudio);
   skip.insert(APIPermission::kBrowsingData);
-  skip.insert(APIPermission::kCastStreaming);
   skip.insert(APIPermission::kCommandsAccessibility);
   skip.insert(APIPermission::kContextMenus);
   skip.insert(APIPermission::kCryptotokenPrivate);
@@ -824,7 +823,6 @@ TEST(PermissionsTest, PermissionMessages) {
   skip.insert(APIPermission::kBookmarkManagerPrivate);
   skip.insert(APIPermission::kBrailleDisplayPrivate);
   skip.insert(APIPermission::kCast);
-  skip.insert(APIPermission::kCastStreaming);
   skip.insert(APIPermission::kCecPrivate);
   skip.insert(APIPermission::kChromeosInfoPrivate);
   skip.insert(APIPermission::kCloudPrintPrivate);
