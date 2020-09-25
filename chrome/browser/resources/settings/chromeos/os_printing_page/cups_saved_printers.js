0155a7764c1df114bb195acaea76a64695357962
Jimmy Gong <jimmyxgong@chromium.org>
2020-09-25 03:16:10 +0000

    Revert "Printer settings: Make saved/nearby printers use FocusRowBehavior"
    This reverts commit 4f4586fd98f9210936c39b7b026fabdb9be1a0e4.
    
    Reason for revert: Reverting due to flaky browsertests behaviors.
    See more details: crbug/1131452
    
    Original change's description:
    > Printer settings: Make saved/nearby printers use FocusRowBehavior
    >
    > Description:
    >  This change allows users to tab to the first printer in the saved
    >  and nearby printer lists and use the arrow keys to navigate the
    >  remaining printers. Users can also use the arrow keys to navigate
    >  through a printer entry's associated buttons/icons. Lastly, the
    >  printer entry row will be highlighted (blue background) if it is
    >  being focused.
    >
    > Screenshots:
    >  - http://shortn/_Fqf1Nn9qjF nearby printers
    >  - http://shortn/_BSspETkjzV saved printers
    >
    > Bug: 987422
    > Test: gtest_filter=OSSettingsCupsPrinterLandingPageV3Test.All
    > Change-Id: I68fc5fc3ff2ceec88ce81f95da5b451a6ad3beb1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2399601
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Commit-Queue: Michael Checo <michaelcheco@google.com>
    > Cr-Commit-Position: refs/heads/master@{#809354}
    
    TBR=jimmyxgong@chromium.org,michaelcheco@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 987422, 1131452
    Change-Id: Id5f71a13ee85f9d58c4fa38ed9e0a7422f797160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429289
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810446}

--- chrome/browser/resources/settings/chromeos/os_printing_page/cups_saved_printers.js
+++ chrome/browser/resources/settings/chromeos/os_printing_page/cups_saved_printers.js
@@ -97,18 +97,6 @@ Polymer({
       type: Boolean,
       value: false,
     },
-
-    /**
-     * Used by FocusRowBehavior to track the last focused element on a row.
-     * @private
-     */
-    lastFocused_: Object,
-
-    /**
-     * Used by FocusRowBehavior to track if the list has been blurred.
-     * @private
-     */
-    listBlurred_: Boolean,
   },
 
   listeners: {
