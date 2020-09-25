30e6ea738812f6e0183ea40fbeffbaacff53e948
Sergey Poromov <poromov@chromium.org>
2020-09-25 07:48:42 +0000

    DLP: Connect DlpContentManager to the policy.
    By implementing GetRestrictionSetForURL() we finally connect on screen content
    restrictions to the policy rules engine (DlpRulesManager) and resolve the TODO.
    
    Bug: 1109783
    Change-Id: I29e62a2b33e7051cfc00b40179bfe5c8d48d0e72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2421895
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Nikita Podguzov <nikitapodguzov@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810505}

--- chrome/browser/chromeos/policy/dlp/dlp_content_manager.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_content_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/stl_util.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
 #include "chrome/browser/ui/ash/chrome_screenshot_grabber.h"
 #include "content/public/browser/visibility.h"
 #include "content/public/browser/web_contents.h"
@@ -151,7 +152,26 @@ void DlpContentManager::OnWebContentsDestroyed(
 DlpContentRestrictionSet DlpContentManager::GetRestrictionSetForURL(
     const GURL& url) const {
   DlpContentRestrictionSet set;
-  // TODO(crbug/1109783): Implement based on the policy.
+  if (!DlpRulesManager::IsInitialized())
+    return set;
+  DlpRulesManager* dlp_rules_manager = DlpRulesManager::Get();
+
+  static const base::NoDestructor<
+      base::flat_map<DlpRulesManager::Restriction, DlpContentRestriction>>
+      kRestrictionsMap({{DlpRulesManager::Restriction::kScreenshot,
+                         DlpContentRestriction::kScreenshot},
+                        {DlpRulesManager::Restriction::kPrivacyScreen,
+                         DlpContentRestriction::kPrivacyScreen},
+                        {DlpRulesManager::Restriction::kPrinting,
+                         DlpContentRestriction::kPrint}});
+
+  for (const auto& restriction : *kRestrictionsMap) {
+    if (dlp_rules_manager->IsRestricted(url, restriction.first) ==
+        DlpRulesManager::Level::kBlock) {
+      set.SetRestriction(restriction.second);
+    }
+  }
+
   return set;
 }
 
