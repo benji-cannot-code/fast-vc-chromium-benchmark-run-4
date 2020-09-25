2cd531a09af66436b2a17c2a961e9cd35d7f4d12
Roman Arora <romanarora@chromium.org>
2020-09-25 16:30:37 -0700

    Reland "TabSearch: Enable/Disable feedback button based on a config"
    This is a reland of 218a19d8afddea116cf66cd2764fa5fb1988716a
    
    Updated TabSearchAppFocusTest that expects submit feedback footer.
    Please review agains the Patchset 1 for new changes.
    
    Original change's description:
    > TabSearch: Enable/Disable feedback button based on a config
    >
    > Bug: 1099917
    > Change-Id: Ia79387286a56d411dead0018c6a22a36fbc4daac
    > Reviewed-on: https://chrome-internal-review.googlesource.com/c/chrome/browser/resources/tab_search/+/3288761
    > Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    > Reviewed-by: John Lee <johntlee@chromium.org>
    
    Bug: 1099917
    Change-Id: Ibe13fd00d7e00c669b7e675cda1daa6d2470605c
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/chrome/browser/resources/tab_search/+/3291234
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819619}

--- chrome/browser/resources/tab_search_merge/test/tab_search_app_focus_test.js
+++ chrome/browser/resources/tab_search_merge/test/tab_search_app_focus_test.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {getDeepActiveElement} from 'chrome://resources/js/util.m.js';
 import {keyDownOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
 import {TabSearchAppElement} from 'chrome://tab-search/app.js';
@@ -26,6 +27,7 @@ suite('TabSearchAppFocusTest', () => {
     testProxy.setProfileTabs(sampleData());
     TabSearchApiProxyImpl.instance_ = testProxy;
 
+    loadTimeData.overrideValues({'submitFeedbackEnabled': true});
     tabSearchApp = /** @type {!TabSearchAppElement} */
         (document.createElement('tab-search-app'));
 
