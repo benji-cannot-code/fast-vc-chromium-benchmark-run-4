db0c77d76a97fda7f4c1b219aaa9623f3a2a4ee1
Erik Chen <erikchen@chromium.org>
2020-09-25 19:34:16 +0000

    lacros: Add availability checks to methods on AshChromeService.
    This CL adds the basic plumbing necessary for Lacros to avoid calling
    unavailable methods on ash due to ash being too old.
    
    This requires modifying browser tests to disable all crosapi
    functionality, and to modify production logic to avoid using crosapi
    functionality if it's unavailable.
    
    Bug: 1130810
    Change-Id: Iedf7faae93372e42c59a61186df49d683d859f50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2424544
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810762}

--- chrome/browser/notifications/notification_platform_bridge_lacros.cc
+++ chrome/browser/notifications/notification_platform_bridge_lacros.cc
@@ -173,7 +173,6 @@ NotificationPlatformBridgeLacros::NotificationPlatformBridgeLacros(
     : bridge_delegate_(delegate),
       message_center_remote_(message_center_remote) {
   DCHECK(bridge_delegate_);
-  DCHECK(message_center_remote_);
 }
 
 NotificationPlatformBridgeLacros::~NotificationPlatformBridgeLacros() = default;
@@ -183,6 +182,9 @@ void NotificationPlatformBridgeLacros::Display(
     Profile* profile,
     const message_center::Notification& notification,
     std::unique_ptr<NotificationCommon::Metadata> metadata) {
+  if (!message_center_remote_)
+    return;
+
   // |profile| is ignored because Profile management is handled in
   // NotificationPlatformBridgeChromeOs, which includes a profile ID as part of
   // the notification ID. Lacros does not support Chrome OS multi-signin, so we
@@ -202,6 +204,9 @@ void NotificationPlatformBridgeLacros::Display(
 void NotificationPlatformBridgeLacros::Close(
     Profile* profile,
     const std::string& notification_id) {
+  if (!message_center_remote_)
+    return;
+
   (*message_center_remote_)->CloseNotification(notification_id);
   // |remote_notifications_| is cleaned up after the remote notification closes
   // and notifies us via the delegate.
@@ -216,9 +221,7 @@ void NotificationPlatformBridgeLacros::GetDisplayed(
 
 void NotificationPlatformBridgeLacros::SetReadyCallback(
     NotificationBridgeReadyCallback callback) {
-  // We don't handle the absence of Ash or a failure to open a Mojo connection,
-  // so just assume the client is ready.
-  std::move(callback).Run(true);
+  std::move(callback).Run(!!message_center_remote_);
 }
 
 void NotificationPlatformBridgeLacros::DisplayServiceShutDown(
