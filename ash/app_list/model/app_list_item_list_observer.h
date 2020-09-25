ac2f8dbf41faebecb1e08c137b84384443a4947a
Alex Newcomer <newcomer@chromium.org>
2020-09-25 23:49:18 +0000

    Speculative ~AppListModel crash fix
    Speculative fix. We now observe a folders item list on
    creation, but it seems we forgot to stop observing in
    one case. When the scoped observer list destructs, it tries
    to stop observing things that no longer exist.
    
    This should fix the crash we are seeing in M-86.
    
    Bug: 1130901
    Change-Id: I078e570a9beb67fda7efd48ba40ec52bda47aab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429524
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810825}

--- ash/app_list/model/app_list_item_list_observer.h
+++ ash/app_list/model/app_list_item_list_observer.h
@@ -9,12 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "ash/app_list/model/app_list_model_export.h"
+#include "base/observer_list_types.h"
 
 namespace ash {
 
 class AppListItem;
 
-class APP_LIST_MODEL_EXPORT AppListItemListObserver {
+class APP_LIST_MODEL_EXPORT AppListItemListObserver
+    : public base::CheckedObserver {
  public:
   // Triggered after |item| has been added to the list at |index|.
   virtual void OnListItemAdded(size_t index, AppListItem* item) {}
@@ -30,7 +32,7 @@ class APP_LIST_MODEL_EXPORT AppListItemListObserver {
                                AppListItem* item) {}
 
  protected:
-  virtual ~AppListItemListObserver() {}
+  ~AppListItemListObserver() override = default;
 };
 
 }  // namespace ash
