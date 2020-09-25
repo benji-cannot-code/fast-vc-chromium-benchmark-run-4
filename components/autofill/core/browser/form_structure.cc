3a3b03684549ea3494799738c96552934dac8515
Mohamed Amir Yosef <mamir@chromium.org>
2020-09-25 17:30:14 +0000

    [Autofill] Delete legacy_proto_bridge.*
    After recent CLs, no conversion from legacy server proto to the new API
    server protos is required.
    
    This CL is getting rid of the code responsible for that.
    
    Bug: 1114655
    Change-Id: I98d04fca0a13a13b43a950bbcb4c3f400259115a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2431044
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810701}

--- components/autofill/core/browser/form_structure.cc
+++ components/autofill/core/browser/form_structure.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/form_parsing/field_candidates.h"
 #include "components/autofill/core/browser/form_parsing/form_field.h"
 #include "components/autofill/core/browser/logging/log_manager.h"
-#include "components/autofill/core/browser/proto/legacy_proto_bridge.h"
 #include "components/autofill/core/browser/randomized_encoder.h"
 #include "components/autofill/core/browser/rationalization_util.h"
 #include "components/autofill/core/browser/validation.h"
