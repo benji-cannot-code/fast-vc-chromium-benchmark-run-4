b7038dcc42e3f67a4cc616aa10d291fb1d52d362
Maggie Cai <mxcai@chromium.org>
2020-09-25 08:14:06 +0000

    [IntentHandling] Not showing browser apps in intent picker.
    Currently there are couple of issues related to the preferred apps
    handling for browser apps in the intent picker.
    1. Setting browser apps as preferred app will set it as preferred app
    for all urls. This is inconsistent with the behaviour for other apps.
    2. If we set the browser as preferred app or set as always stay in
    chrome for the browser, then navigate to a website that has other apps
    that can handle the url, should we show the intent picker?
    It is reasonable for a browser to not suggest other browsers for
    handling http/https links, so this CL stops browser apps from appearing
    in the intent picker. Users can still open links in other browsers by
    opening them explicitly, matching Android's behaviour.
    
    BUG=853604, 1113124
    
    Change-Id: I38b8f7be0d08becca2604ce6998bf40cab8a9729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2342331
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810511}

--- components/services/app_service/public/cpp/intent_filter_util.cc
+++ components/services/app_service/public/cpp/intent_filter_util.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/intent_filter_util.h"
 
 #include "components/services/app_service/public/cpp/intent_util.h"
+#include "url/url_constants.h"
 
 namespace {
 
@@ -142,6 +143,25 @@ void UpgradeFilter(apps::mojom::IntentFilterPtr& filter) {
   auto condition = apps_util::MakeCondition(apps::mojom::ConditionType::kAction,
                                             std::move(condition_values));
   filter->conditions.insert(filter->conditions.begin(), std::move(condition));
+}
+
+bool IsBrowserFilter(const apps::mojom::IntentFilterPtr& filter) {
+  if (GetFilterMatchLevel(filter) != IntentFilterMatchLevel::kScheme) {
+    return false;
+  }
+  for (const auto& condition : filter->conditions) {
+    if (condition->condition_type != apps::mojom::ConditionType::kScheme) {
+      continue;
+    }
+    for (const auto& condition_value : condition->condition_values) {
+      if (condition_value->value == url::kHttpScheme ||
+          condition_value->value == url::kHttpsScheme) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 }  // namespace apps_util
