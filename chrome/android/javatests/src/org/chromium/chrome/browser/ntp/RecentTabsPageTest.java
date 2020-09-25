77d1226d6ab6026dca899e6fb331713cf88ba062
Findit <findit-for-me@appspot.gserviceaccount.com>
2020-09-25 11:00:22 +0000

    Revert "[Android] Show signed in account in sync promo"
    This reverts commit adc862b094c2d9189b175ab3d5f9d6bb44573b18.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 810545 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2FkYzg2MmIwOTRjMmQ5MTg5YjE3NWFiM2Q1ZjlkNmJiNDQ1NzNiMTgM
    
    Sample Failed Build: https://ci.chromium.org/b/8868191608621476032
    
    Sample Failed Step: compile
    
    Original change's description:
    > [Android] Show signed in account in sync promo
    > 
    > Sync promo always shows the first account returned by AccountManagerFacade
    > which might not be signed in. This cl makes sure that the account that
    > is shown is the first signed in account.
    > 
    > This cl also refactors some of the tests regrading sync promos. The
    > behavior of the tests are not changed.
    > 
    > Bug: 1131465
    > Change-Id: I46f2e607494c68d3b0608543095da4b22471f710
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2426485
    > Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    > Reviewed-by: Alice Wang <aliceywang@chromium.org>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#810545}
    
    
    Change-Id: I5fc911e9f5175b65d10ff045dfd48103ed08b099
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1131465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2430974
    Cr-Commit-Position: refs/heads/master@{#810554}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentTabsPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentTabsPageTest.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.ntp;
 
-import android.accounts.Account;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -55,11 +54,9 @@ import java.util.concurrent.ExecutionException;
 public class RecentTabsPageTest {
     // FakeProfileDataSource is required to create the ProfileDataCache entry with sync_off badge
     // for Sync promo.
-    private final FakeProfileDataSource mFakeProfileDataSource = new FakeProfileDataSource();
-
     @Rule
     public final AccountManagerTestRule mAccountManagerTestRule =
-            new AccountManagerTestRule(mFakeProfileDataSource);
+            new AccountManagerTestRule(new FakeProfileDataSource());
 
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
@@ -109,12 +106,8 @@ public class RecentTabsPageTest {
     @LargeTest
     @Feature("RenderTest")
     public void testPersonalizedSigninPromoInRecentTabsPage() throws Exception {
-        Account account = mAccountManagerTestRule.addAndSignInTestAccount();
-        TestThreadUtils.runOnUiThreadBlocking(
-                ()
-                        -> mFakeProfileDataSource.setProfileData(account.name,
-                                new ProfileDataSource.ProfileData(
-                                        account.name, createAvatar(), "Full Name", "Given Name")));
+        mAccountManagerTestRule.addAccount(new ProfileDataSource.ProfileData(
+                "test@gmail.com", createAvatar(), "Full Name", "Given Name"));
         RecentTabsManager.forcePromoStateForTests(
                 RecentTabsManager.PromoState.PROMO_SIGNIN_PERSONALIZED);
         mPage = loadRecentTabsPage();
@@ -125,12 +118,8 @@ public class RecentTabsPageTest {
     @LargeTest
     @Feature("RenderTest")
     public void testPersonalizedSyncPromoInRecentTabsPage() throws Exception {
-        Account account = mAccountManagerTestRule.addAndSignInTestAccount();
-        TestThreadUtils.runOnUiThreadBlocking(
-                ()
-                        -> mFakeProfileDataSource.setProfileData(account.name,
-                                new ProfileDataSource.ProfileData(
-                                        account.name, createAvatar(), "Full Name", "Given Name")));
+        mAccountManagerTestRule.addAccount(new ProfileDataSource.ProfileData(
+                "test@gmail.com", createAvatar(), "Full Name", "Given Name"));
         RecentTabsManager.forcePromoStateForTests(
                 RecentTabsManager.PromoState.PROMO_SYNC_PERSONALIZED);
         mPage = loadRecentTabsPage();
