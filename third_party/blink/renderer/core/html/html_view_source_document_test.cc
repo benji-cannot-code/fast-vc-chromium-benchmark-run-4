03c61d8afa0c82aa1b2de60c7bc531c578096c12
Mason Freed <masonfreed@chromium.org>
2020-09-25 00:01:34 +0000

    Change view-source table from word-break:keep-all to white-space:pre
    In [1], the view source document table was changed to word-break:
    keep-all, to avoid the slow layout path for word-break:break-all.
    The intention there was to not allow word breaking at all, and
    have each source line on a single displayed line. The better
    (correct) way to achieve that is to use the white-space property,
    as pointed out by kojii@ in [1]. This CL changes the white space
    property to pre, instead of nowrap (as suggested), to also
    preserve white space in the source file.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2424038
    
    Bug: 920571
    Change-Id: I3271dcce2f7f546ea5f8a33997226e2ec4894fd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429204
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810360}

--- third_party/blink/renderer/core/html/html_view_source_document_test.cc
+++ third_party/blink/renderer/core/html/html_view_source_document_test.cc
@@ -39,8 +39,8 @@ TEST_F(HTMLViewSourceDocumentTest, ViewSource1) {
   EXPECT_EQ(
       GetDocument().documentElement()->outerHTML(),
       "<html><head></head><body><div "
-      "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-      "keep-all;\"><tbody><tr><td "
+      "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+      "pre;\"><tbody><tr><td "
       "class=\"line-number\" value=\"1\"></td><td "
       "class=\"line-content\"><br></td></tr><tr><td class=\"line-number\" "
       "value=\"2\"></td><td class=\"line-content\">      <span "
@@ -93,8 +93,8 @@ TEST_F(HTMLViewSourceDocumentTest, ViewSource2) {
   EXPECT_EQ(
       GetDocument().documentElement()->outerHTML(),
       "<html><head></head><body><div "
-      "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-      "keep-all;\"><tbody><tr><td "
+      "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+      "pre;\"><tbody><tr><td "
       "class=\"line-number\" value=\"1\"></td><td "
       "class=\"line-content\"><br></td></tr><tr><td class=\"line-number\" "
       "value=\"2\"></td><td class=\"line-content\">      <span "
@@ -146,8 +146,8 @@ TEST_F(HTMLViewSourceDocumentTest, ViewSource3) {
   EXPECT_EQ(
       GetDocument().documentElement()->outerHTML(),
       "<html><head></head><body><div "
-      "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-      "keep-all;\"><tbody><tr><td "
+      "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+      "pre;\"><tbody><tr><td "
       "class=\"line-number\" value=\"1\"></td><td "
       "class=\"line-content\"><br></td></tr><tr><td class=\"line-number\" "
       "value=\"2\"></td><td class=\"line-content\">      <span "
@@ -212,8 +212,8 @@ TEST_F(HTMLViewSourceDocumentTest, ViewSource4) {
   EXPECT_EQ(
       GetDocument().documentElement()->outerHTML(),
       "<html><head></head><body><div "
-      "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-      "keep-all;\"><tbody><tr><td "
+      "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+      "pre;\"><tbody><tr><td "
       "class=\"line-number\" value=\"1\"></td><td "
       "class=\"line-content\"><br></td></tr><tr><td class=\"line-number\" "
       "value=\"2\"></td><td class=\"line-content\">      <span "
@@ -281,8 +281,8 @@ TEST_F(HTMLViewSourceDocumentTest, ViewSource5) {
   EXPECT_EQ(
       GetDocument().documentElement()->outerHTML(),
       "<html><head></head><body><div "
-      "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-      "keep-all;\"><tbody><tr><td "
+      "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+      "pre;\"><tbody><tr><td "
       "class=\"line-number\" value=\"1\"></td><td "
       "class=\"line-content\"><br></td></tr><tr><td class=\"line-number\" "
       "value=\"2\"></td><td class=\"line-content\"><br></td></tr><tr><td "
@@ -314,8 +314,8 @@ TEST_F(HTMLViewSourceDocumentTest, ViewSource6) {
   LoadMainResource((many_spaces + std::string("       <b>A</b>  ")).c_str());
   std::string expected_beginning(
       "<html><head></head><body><div "
-      "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-      "keep-all;\"><tbody><tr><td "
+      "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+      "pre;\"><tbody><tr><td "
       "class=\"line-number\" value=\"1\"></td><td class=\"line-content\">     "
       " ");
   std::string expected_ending(
@@ -331,8 +331,8 @@ TEST_F(HTMLViewSourceDocumentTest, ViewSource7) {
   LoadMainResource("1234567");
   EXPECT_EQ(GetDocument().documentElement()->outerHTML(),
             "<html><head></head><body><div "
-            "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-            "keep-all;\"><tbody><tr><td "
+            "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+            "pre;\"><tbody><tr><td "
             "class=\"line-number\" value=\"1\"></td><td "
             "class=\"line-content\">1234567<span "
             "class=\"html-end-of-file\"></span></td></tr></tbody></table></"
@@ -354,8 +354,8 @@ TEST_F(HTMLViewSourceDocumentTest, ViewSource8) {
   EXPECT_EQ(
       GetDocument().documentElement()->outerHTML(),
       "<html><head></head><body><div "
-      "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-      "keep-all;\"><tbody><tr><td "
+      "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+      "pre;\"><tbody><tr><td "
       "class=\"line-number\" value=\"1\"></td><td "
       "class=\"line-content\"><br></td></tr><tr><td class=\"line-number\" "
       "value=\"2\"></td><td class=\"line-content\">      <span "
@@ -422,8 +422,8 @@ TEST_F(HTMLViewSourceDocumentTest, ViewSource9) {
   EXPECT_EQ(
       GetDocument().documentElement()->outerHTML(),
       "<html><head></head><body><div "
-      "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-      "keep-all;\"><tbody><tr><td "
+      "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+      "pre;\"><tbody><tr><td "
       "class=\"line-number\" value=\"1\"></td><td "
       "class=\"line-content\"><br></td></tr><tr><td class=\"line-number\" "
       "value=\"2\"></td><td class=\"line-content\">      <span "
@@ -454,8 +454,8 @@ TEST_F(HTMLViewSourceDocumentTest, IncompleteToken) {
   EXPECT_EQ(
       GetDocument().documentElement()->outerHTML(),
       "<html><head></head><body><div "
-      "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-      "keep-all;\"><tbody><tr><td "
+      "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+      "pre;\"><tbody><tr><td "
       "class=\"line-number\" value=\"1\"></td><td "
       "class=\"line-content\"><br></td></tr><tr><td class=\"line-number\" "
       "value=\"2\"></td><td class=\"line-content\">      Incomplete token "
@@ -477,8 +477,8 @@ TEST_F(HTMLViewSourceDocumentTest, UnfinishedTextarea) {
   EXPECT_EQ(
       GetDocument().documentElement()->outerHTML(),
       "<html><head></head><body><div "
-      "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-      "keep-all;\"><tbody><tr><td "
+      "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+      "pre;\"><tbody><tr><td "
       "class=\"line-number\" value=\"1\"></td><td class=\"line-content\"><span "
       "class=\"html-tag\">&lt;textarea&gt;</span>foobar in "
       "textarea</td></tr><tr><td class=\"line-number\" value=\"2\"></td><td "
@@ -493,8 +493,8 @@ TEST_F(HTMLViewSourceDocumentTest, UnfinishedScript) {
   EXPECT_EQ(
       GetDocument().documentElement()->outerHTML(),
       "<html><head></head><body><div "
-      "class=\"line-gutter-backdrop\"></div><table style=\"word-break: "
-      "keep-all;\"><tbody><tr><td "
+      "class=\"line-gutter-backdrop\"></div><table style=\"white-space: "
+      "pre;\"><tbody><tr><td "
       "class=\"line-number\" value=\"1\"></td><td class=\"line-content\"><span "
       "class=\"html-tag\">&lt;script&gt;</span>foobar in "
       "script</td></tr><tr><td class=\"line-number\" value=\"2\"></td><td "
