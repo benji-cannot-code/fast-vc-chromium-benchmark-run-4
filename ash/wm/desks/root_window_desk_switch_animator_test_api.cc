cfa1472106615fe3e00bec46b20054a32ebd6a45
Sammie Quon <sammiequon@chromium.org>
2020-09-25 19:09:02 +0000

    Reland "desks: Add tests for RootWindowDeskSwitchAnimator."
    This is a reland of 4c93f79bd63979100d88f7bd016f2a3bb3bbfd1b. The
    original change patch is in ps1.
    
    The reason for the revert was an error was something with managing
    GPU textures (mailbox name not found). Tried copying some GPU test
    code [1] but still got the same error and was building up a lot of
    GPU dependencies.
    
    Instead of creating fake output, this cl wraps the existing
    TakeScreenshot calls and puts a waiter in.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/master:components/viz/test/fake_skia_output_surface.cc;drc=361ef8a161b516ba106966bb0740314c41dc2678;l=272
    
    Original change's description:
    > desks: Add tests for RootWindowDeskSwitchAnimator.
    >
    > Test: added
    > Bug: 1111445
    > Change-Id: I0f79be1c09d4ef0eeff276e3c50f491efbe56244
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2380517
    > Reviewed-by: Khushal <khushalsagar@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#809854}
    
    Bug: 1111445
    Change-Id: Ib4e50c22fb063b82f50c3f4f79bb026e648a6157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429363
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810754}

---
+++ ash/wm/desks/root_window_desk_switch_animator_test_api.cc
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/wm/desks/root_window_desk_switch_animator_test_api.h"
+
+#include "ash/wm/desks/root_window_desk_switch_animator.h"
+#include "ui/compositor/layer.h"
+#include "ui/compositor/layer_tree_owner.h"
+
+namespace ash {
+
+RootWindowDeskSwitchAnimatorTestApi::RootWindowDeskSwitchAnimatorTestApi(
+    RootWindowDeskSwitchAnimator* animator)
+    : animator_(animator) {
+  DCHECK(animator_);
+}
+
+RootWindowDeskSwitchAnimatorTestApi::~RootWindowDeskSwitchAnimatorTestApi() =
+    default;
+
+ui::Layer* RootWindowDeskSwitchAnimatorTestApi::GetAnimationLayer() {
+  return animator_->animation_layer_owner_->root();
+}
+
+ui::Layer*
+RootWindowDeskSwitchAnimatorTestApi::GetScreenshotLayerOfDeskWithIndex(
+    int desk_index) {
+  auto screenshot_layers = animator_->screenshot_layers_;
+
+  DCHECK_GE(desk_index, 0);
+  DCHECK_LT(desk_index, int{screenshot_layers.size()});
+
+  ui::Layer* layer = screenshot_layers[desk_index];
+  DCHECK(layer);
+  return layer;
+}
+
+int RootWindowDeskSwitchAnimatorTestApi::GetEndingDeskIndex() const {
+  return animator_->ending_desk_index_;
+}
+
+}  // namespace ash
