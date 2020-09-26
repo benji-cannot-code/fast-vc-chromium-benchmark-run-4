d828949d8f0d385c1f0c11e007bd80077f737dc0
Xing Liu <xingliu@chromium.org>
2020-09-26 00:07:30 +0000

    Download notification: Fix a crash that startForeground is not called.
    If stopForeground is called between startForegroundService and
    startForeground. startForeground will not be called since we clear the
    cached service object. This will lead to a crash on O_MR1+.
    
    Android can reuse the service binder object when this happens.
    
    Bug: 1121096
    Change-Id: Ic4088af174ca9561714442b29c2e6162995ddc51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429635
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810857}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundServiceManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundServiceManager.java
@@ -277,7 +277,6 @@ public class DownloadForegroundServiceManager {
         stopAndUnbindServiceInternal(
                 stopForegroundNotification, mPinnedNotificationId, oldNotification);
 
-        mBoundService = null;
 
         mPinnedNotificationId = INVALID_NOTIFICATION_ID;
     }
