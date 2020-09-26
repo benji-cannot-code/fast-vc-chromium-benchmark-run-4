ea63c4c520106eff3f78cac057066670b715e055
Reilly Grant <reillyg@chromium.org>
2020-09-26 03:55:21 +0000

    Reland "serial: Add option to switch enumeration methods"
    This reverts commit 412cff1e7077722f0f3dfb16602c2d154bf5b021.
    
    Reason for revert: Figured out why the change had no effect.
    
    Original change's description:
    > Revert "serial: Add option to switch enumeration methods"
    >
    > This reverts commit fdbcb258e0dcc2eccf6927aa4c77f6af56fc0b0e.
    >
    > Reason for revert: The new flag didn't resolve the issue.
    >
    > Original change's description:
    > > serial: Add option to switch enumeration methods
    > > 
    > > A potential solution for issue 1119497 is to switch from enumerating
    > > devices implementing GUID_DEVINTERFACE_COMPORT to using the "bus
    > > enumerator" identified by GUID_DEVINTERFACE_SERENUM_BUS_ENUMERATOR.
    > > 
    > > I'm putting this behind a flag so that developers can try it out in
    > > canary-channel. If it works I can enable it by default and keep the flag
    > > around for a couple releases in case it causes other problems.
    > > 
    > > Bug: 1119497
    > > Change-Id: Icad9008799ed331c3da15e5a70e0184144c47a58
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417460
    > > Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    > > Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    > > Auto-Submit: Reilly Grant <reillyg@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#808368}
    >
    > TBR=reillyg@chromium.org,odejesush@chromium.org
    >
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Bug: 1119497
    > Change-Id: I3e39d5de932e94b980e7fb5f6de71a2cfa45b0f3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2423053
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#809207}
    
    Bug: 1119497
    Change-Id: I39bb97d5e1cc5ca5dc971ba2dfad3e9eb987141d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2432005
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810925}

--- services/device/public/cpp/device_features.h
+++ services/device/public/cpp/device_features.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SERVICES_DEVICE_PUBLIC_CPP_DEVICE_FEATURES_H_
 
 #include "base/feature_list.h"
+#include "build/build_config.h"
 #include "services/device/public/cpp/device_features_export.h"
 
 namespace features {
@@ -22,6 +23,10 @@ DEVICE_FEATURES_EXPORT extern const base::Feature
     kWinrtGeolocationImplementation;
 DEVICE_FEATURES_EXPORT extern const base::Feature
     kMacCoreLocationImplementation;
+
+#if defined(OS_WIN)
+DEVICE_FEATURES_EXPORT extern const base::Feature kUseSerialBusEnumerator;
+#endif  // defined(OS_WIN)
 
 }  // namespace features
 
