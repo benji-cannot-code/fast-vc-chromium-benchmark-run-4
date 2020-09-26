ad2ede03d2a08b4fb98602079fb3a3c3109c1122
Renjie Tang <renjietang@chromium.org>
2020-09-26 02:18:45 +0000

    Roll src/net/third_party/quiche/src/ ba9d95e43..f2e0d90a9 (3 commits)
    https://quiche.googlesource.com/quiche.git/+log/ba9d95e4328b..f2e0d90a970b
    
    $ git log ba9d95e43..f2e0d90a9 --date=short --no-merges --format='%ad %ae %s'
    2020-09-25 fayang Notify debug_visitor once any 0-RTT packet gets acknowledged. Client only (server + TLS does not send 0-RTT packets).
    2020-09-25 wub Add flag --ip_version_for_host_lookup to quic_client to indicate the preferred IP version of host address, if host lookup is needed.
    2020-09-25 quiche-dev Fix a latent query-of-death in a GFE+QUIC+Leto codepath
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: I8c71cc9b663b6c2287661a999b7f72a02ecf6579
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2432758
    Commit-Queue: Renjie Tang <renjietang@chromium.org>
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Auto-Submit: Renjie Tang <renjietang@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810917}

---
+++ net/quic/platform/impl/quic_testvalue_impl.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_QUIC_PLATFORM_IMPL_QUIC_TESTVALUE_IMPL_H_
+#define NET_QUIC_PLATFORM_IMPL_QUIC_TESTVALUE_IMPL_H_
+
+namespace quic {
+
+template <class T>
+void AdjustTestValueImpl(quiche::QuicheStringPiece label, T* var) {}
+
+}  // namespace quic
+
+#endif  // NET_QUIC_PLATFORM_IMPL_QUIC_TESTVALUE_IMPL_H_
