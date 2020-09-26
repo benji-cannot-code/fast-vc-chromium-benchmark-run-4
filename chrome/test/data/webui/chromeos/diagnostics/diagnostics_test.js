bb297f61e8fd71a65222b9dfc7bdfe0c4819d18d
Michael Checo <michaelcheco@google.com>
2020-09-26 19:20:20 +0000

    Diagnostics: Remove tests checking for hard-coded strings
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I5fadda5c3cf995619bb17aa2da01ca86584724a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2432128
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811007}

--- chrome/test/data/webui/chromeos/diagnostics/diagnostics_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/diagnostics_test.js
@@ -79,9 +79,6 @@ suite('DiagnosticsAppTest', () => {
   });
 
   test('LandingPageLoaded', () => {
-    // TODO(zentaro): Update when strings are finalized and localized.
-    assertEquals('Diagnostics', page.$$('#header').textContent);
-
     // Verify the overview card is in the page.
     const overview = page.$$('#overviewCard');
     assertTrue(!!overview);
@@ -151,10 +148,6 @@ suite('BatteryStatusCardTest', () => {
     return initializeBatteryStatusCard(
                fakeBatteryInfo, fakeBatteryChargeStatus, fakeBatteryHealth)
         .then(() => {
-          // TODO(zentaro): Update when strings are finalized.
-          assertEquals(
-              'Battery Status',
-              batteryStatusElement.$$('#cardTitle').textContent);
           assertEquals(
               fakeBatteryInfo.manufacturer,
               batteryStatusElement.$$('#manufacturer').textContent);
@@ -222,9 +215,6 @@ suite('CpuCardTest', () => {
 
   test('CpuCardPopulated', () => {
     return initializeCpuCard(fakeCpuUsage).then(() => {
-      // TODO(zentaro): Update when strings are finalized.
-      assertEquals('CPU', cpuElement.$$('#cardTitle').textContent);
-
       assertEquals(
           fakeCpuUsage[0].cpu_temp_degrees_celcius.toString(),
           cpuElement.$$('#cpuTemp').textContent);
@@ -337,9 +327,6 @@ suite('MemoryCardTest', () => {
 
   test('MemoryCardPopulated', () => {
     return initializeMemoryCard(fakeMemoryUsage).then(() => {
-      // TODO(zentaro): Update when strings are finalized.
-      assertEquals('Memory', memoryElement.$$('#cardTitle').textContent);
-
       assertEquals(
           fakeMemoryUsage[0].total_memory_kib.toString(),
           memoryElement.$$('#memoryTotal').textContent);
