6ead0452ebd28df136a765ecad71c34e090c584b
Brandon Jones <bajones@chromium.org>
2020-09-26 07:54:50 +0000

    Stop sending XRDeviceId with VRDisplayInfo
    This value is not used by Blink, and does not need to be communicated
    over the mojo interface. The XRDeviceId enum should not be removed
    because it is used for usage stats.
    
    Bug: 1017477
    Change-Id: I5e8cfff00c374f48495290c5dfb8911a1824a2de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2424684
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Auto-Submit: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#810958}

--- device/vr/orientation/orientation_device.cc
+++ device/vr/orientation/orientation_device.cc
@@ -28,12 +28,6 @@ using gfx::Vector3dF;
 namespace {
 static constexpr int kDefaultPumpFrequencyHz = 60;
 
-mojom::VRDisplayInfoPtr CreateVRDisplayInfo(mojom::XRDeviceId id) {
-  mojom::VRDisplayInfoPtr display_info = mojom::VRDisplayInfo::New();
-  display_info->id = id;
-  return display_info;
-}
-
 display::Display::Rotation GetRotation() {
   display::Screen* screen = display::Screen::GetScreen();
   if (!screen) {
@@ -55,7 +49,7 @@ VROrientationDevice::VROrientationDevice(mojom::SensorProvider* sensor_provider,
                              base::BindOnce(&VROrientationDevice::SensorReady,
                                             base::Unretained(this)));
 
-  SetVRDisplayInfo(CreateVRDisplayInfo(GetId()));
+  SetVRDisplayInfo(mojom::VRDisplayInfo::New());
 }
 
 VROrientationDevice::~VROrientationDevice() {
--- device/vr/vr_device_base.cc
+++ device/vr/vr_device_base.cc
@@ -67,7 +67,6 @@ void VRDeviceBase::ListenToDeviceChanges(
 
 void VRDeviceBase::SetVRDisplayInfo(mojom::VRDisplayInfoPtr display_info) {
   DCHECK(display_info);
-  DCHECK(display_info->id == id_);
   display_info_ = std::move(display_info);
 
   if (listener_)
