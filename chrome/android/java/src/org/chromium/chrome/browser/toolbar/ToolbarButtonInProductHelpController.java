ef7a83fd95c73139220b7392026f5022c9f38911
Jinsuk Kim <jinsukkim@chromium.org>
2020-09-27 12:21:19 +0000

    Android: Modularize user_education (IPH)
    user_education/ is used as a shim allowing the classes in
    chrome/android::chrome_java to use feature_engagement component. This
    CL modularizes it under chrome/browser/user_education so that toolbar/
    can use it without relying on chrome_java.
    
    Bug: 1127732
    Change-Id: I33d2e4851914f743f72540181f1cc222f2c6d70e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422060
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811048}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarButtonInProductHelpController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarButtonInProductHelpController.java
@@ -55,7 +55,8 @@ public class ToolbarButtonInProductHelpController
             AppMenuCoordinator appMenuCoordinator, ActivityLifecycleDispatcher lifecycleDispatcher,
             ActivityTabProvider tabProvider) {
         mActivity = activity;
-        mUserEducationHelper = new UserEducationHelper(mActivity, mHandler);
+        mUserEducationHelper =
+                new UserEducationHelper(mActivity, mHandler, TrackerFactory::getTrackerForProfile);
         mScreenshotMonitor = new ScreenshotMonitor(this);
         lifecycleDispatcher.register(this);
         mPageLoadObserver = new ActivityTabTabObserver(tabProvider) {
