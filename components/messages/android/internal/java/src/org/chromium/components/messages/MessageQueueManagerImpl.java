4155d240f945d4c94f80bd87ecf0b44b1d54ca21
Pavel Yatsuk <pavely@google.com>
2020-09-27 06:55:01 +0000

    [Messages] Basic implementation of MessageQueueManager
    This is a basic implementation of MessageQueueManager.
    MessageQueueManager:
    - maintains a queue of messages and a mapping from message key objects to
      MessageStateHandlers.
    - shows the message at the head of the queue
    - hides and dismisses the message when it gets dismissed through the API
    
    BUG=1123947
    R=lazzzis@chromium.org
    
    Change-Id: Ifa3fbba6162323bdc79305796472fdaea0829ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416740
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/master@{#811037}

--- components/messages/android/internal/java/src/org/chromium/components/messages/MessageQueueManagerImpl.java
+++ components/messages/android/internal/java/src/org/chromium/components/messages/MessageQueueManagerImpl.java
@@ -5,9 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.messages;
 
+import androidx.annotation.Nullable;
+
 import org.chromium.base.UnownedUserData;
 import org.chromium.base.UnownedUserDataKey;
 import org.chromium.ui.base.WindowAndroid;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
 
 /**
  * A class managing the queue of messages. Its primary role is to decide when to show/hide current
@@ -16,6 +23,11 @@ import org.chromium.ui.base.WindowAndroid;
 class MessageQueueManagerImpl implements MessageQueueManager, UnownedUserData {
     private static final UnownedUserDataKey<MessageQueueManagerImpl> KEY =
             new UnownedUserDataKey<>(MessageQueueManagerImpl.class);
+
+    private final Queue<MessageStateHandler> mMessageQueue = new ArrayDeque<>();
+    private final Map<Object, MessageStateHandler> mMessageMap = new HashMap<>();
+    @Nullable
+    private MessageStateHandler mCurrentDisplayedMessage;
 
     /**
      * Get the activity's MessageQueueManager from the provided WindowAndroid.
@@ -43,5 +55,45 @@ class MessageQueueManagerImpl implements MessageQueueManager, UnownedUserData {
     @Override
     public void destroy() {
         KEY.detachFromAllHosts(this);
+    }
+
+    /**
+     * Enqueues a message. Associates the message with its key; the key is used later to dismiss the
+     * message. Displays the message if there is no other message shown.
+     * @param message The message to enqueue
+     * @param key The key to associate with this message.
+     */
+    public void enqueueMessage(MessageStateHandler message, Object key) {
+        if (mMessageMap.containsKey(key)) {
+            throw new IllegalStateException("Message with the given key has already been enqueued");
+        }
+        mMessageMap.put(key, message);
+        mMessageQueue.add(message);
+        updateCurrentDisplayedMessage();
+    }
+
+    /**
+     * Dismisses a message specified by its key. Hdes the message if it is currently displayed.
+     * Displays the next message in the queue if available.
+     * @param key The key associated with the message to dismiss.
+     */
+    public void dismissMessage(Object key) {
+        MessageStateHandler message = mMessageMap.get(key);
+        if (message == null) return;
+        mMessageMap.remove(key);
+        mMessageQueue.remove(message);
+        if (mCurrentDisplayedMessage == message) {
+            mCurrentDisplayedMessage.hide();
+            mCurrentDisplayedMessage = null;
+        }
+        message.dismiss();
+        updateCurrentDisplayedMessage();
+    }
+
+    private void updateCurrentDisplayedMessage() {
+        if (mCurrentDisplayedMessage != null) return;
+        if (mMessageQueue.isEmpty()) return;
+        mCurrentDisplayedMessage = mMessageQueue.element();
+        mCurrentDisplayedMessage.show();
     }
 }
