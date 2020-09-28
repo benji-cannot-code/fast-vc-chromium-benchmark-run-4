5a4951dd148510d38a1cb2a0f79f914888065d4c
Charles Zhao <charleszhao@chromium.org>
2020-09-28 02:13:34 +0000

    A few nit fixes in HandwritingModelLoader code.
    Bug: 1054628
    
    Change-Id: I6eb96bbdc04736ecb17ca6d825b26f60f0bace6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2428408
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Charles . <charleszhao@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Charles . <charleszhao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811090}

--- chromeos/services/machine_learning/public/cpp/handwriting_model_loader.cc
+++ chromeos/services/machine_learning/public/cpp/handwriting_model_loader.cc
@@ -4,9 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chromeos/services/machine_learning/public/cpp/handwriting_model_loader.h"
+
+#include <string>
+#include <utility>
+
 #include "base/command_line.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/no_destructor.h"
 #include "chromeos/services/machine_learning/public/cpp/service_connection.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
--- chromeos/services/machine_learning/public/cpp/handwriting_model_loader_unittest.cc
+++ chromeos/services/machine_learning/public/cpp/handwriting_model_loader_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/services/machine_learning/public/cpp/handwriting_model_loader.h"
 
+#include <string>
+
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/run_loop.h"
@@ -17,7 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 namespace machine_learning {
-using chromeos::machine_learning::mojom::LoadHandwritingModelResult;
+
+using ::base::test::ScopedCommandLine;
+using ::base::test::TaskEnvironment;
+using mojom::LoadHandwritingModelResult;
 
 constexpr char kOndeviceHandwritingSwitch[] = "ondevice_handwriting";
 constexpr char kLibHandwritingDlcId[] = "libhandwriting";
@@ -75,10 +80,10 @@ class HandwritingModelLoaderTest : public testing::Test {
   }
 
  private:
-  base::test::TaskEnvironment task_environment_{
-      base::test::TaskEnvironment::MainThreadType::DEFAULT,
-      base::test::TaskEnvironment::ThreadPoolExecutionMode::QUEUED};
-  base::test::ScopedCommandLine scoped_command_line_;
+  TaskEnvironment task_environment_{
+      TaskEnvironment::MainThreadType::DEFAULT,
+      TaskEnvironment::ThreadPoolExecutionMode::QUEUED};
+  ScopedCommandLine scoped_command_line_;
   FakeDlcserviceClient fake_client_;
   FakeServiceConnectionImpl fake_service_connection_;
   LoadHandwritingModelResult result_;
