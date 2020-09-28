c385f3083dd1e6bdfb6ee1c0b8d6cb7633e3d894
Reilly Grant <reillyg@chromium.org>
2020-09-28 23:41:19 +0000

    usb: Enable new Windows USB backend by default
    Based on a stable 1% experimental launch in M-85 new backend appears to
    be stable. Enabling it by default so that it rolls out to all users as
    part of M-87.
    
    Bug: 637404
    Change-Id: I57d996c1cef5c5c097561c9c23b9ae713825e4a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2436292
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: James Hollyer <jameshollyer@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: James Hollyer <jameshollyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811437}

--- device/base/features.cc
+++ device/base/features.cc
@@ -9,10 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
-#if defined(OS_MAC) || defined(OS_WIN)
+#if defined(OS_MAC)
 const base::Feature kNewUsbBackend{"NewUsbBackend",
                                    base::FEATURE_DISABLED_BY_DEFAULT};
-#endif  // defined(OS_MAC) || defined(OS_WIN)
+#endif  // defined(OS_MAC)
+
+#if defined(OS_WIN)
+const base::Feature kNewUsbBackend{"NewUsbBackend",
+                                   base::FEATURE_ENABLED_BY_DEFAULT};
+#endif  // defined(OS_WIN)
 
 #if defined(OS_WIN)
 const base::Feature kNewBLEWinImplementation{"NewBLEWinImplementation",
