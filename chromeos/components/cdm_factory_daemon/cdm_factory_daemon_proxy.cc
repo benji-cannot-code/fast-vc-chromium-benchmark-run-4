f1c9e45ec697fe2d17f68e9aba87abba1e00d9f4
Jeffrey Kardatzke <jkardatzke@google.com>
2020-09-28 23:26:28 +0000

    Add OutputProtection mojo interface for Chrome OS daemon
    This adds the mojo OutputProtection interface and implementation that is
    used to provide the OutputProtection service to the Chrome OS
    cdm-oemcrypto daemon.
    
    BUG=b:168230642,b:153111783,b:132039686,b:169530695
    TEST=Unit tests pass, HDCP enabling/verify works with daemon
    
    Change-Id: Ia40a8631894f937e79ab4504a7121d31b3713ba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2432804
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/master@{#811423}

--- chromeos/components/cdm_factory_daemon/cdm_factory_daemon_proxy.cc
+++ chromeos/components/cdm_factory_daemon/cdm_factory_daemon_proxy.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "chromeos/components/cdm_factory_daemon/output_protection_impl.h"
 #include "chromeos/dbus/cdm_factory_daemon/cdm_factory_daemon_client.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -54,16 +55,27 @@ void CdmFactoryDaemonProxy::CreateFactory(const std::string& key_system,
                      base::Unretained(this), key_system, std::move(callback)));
 }
 
-void CdmFactoryDaemonProxy::ConnectOemCrypto(
+void CdmFactoryDaemonProxy::ConnectOemCryptoDeprecated(
     mojo::PendingReceiver<arc::mojom::OemCryptoService> oemcryptor,
     mojo::PendingRemote<arc::mojom::ProtectedBufferManager>
         protected_buffer_manager) {
+  // This will never get called because this originates from Chrome as well
+  // which will always be on the same version as us.
+  NOTIMPLEMENTED();
+}
+
+void CdmFactoryDaemonProxy::ConnectOemCrypto(
+    mojo::PendingReceiver<arc::mojom::OemCryptoService> oemcryptor,
+    mojo::PendingRemote<arc::mojom::ProtectedBufferManager>
+        protected_buffer_manager,
+    mojo::PendingRemote<cdm::mojom::OutputProtection> output_protection) {
   // This gets invoked from ArcBridge which uses a different thread.
   if (!mojo_task_runner_->RunsTasksInCurrentSequence()) {
     mojo_task_runner_->PostTask(
         FROM_HERE, base::BindOnce(&CdmFactoryDaemonProxy::ConnectOemCrypto,
                                   base::Unretained(this), std::move(oemcryptor),
-                                  std::move(protected_buffer_manager)));
+                                  std::move(protected_buffer_manager),
+                                  std::move(output_protection)));
     return;
   }
 
@@ -71,14 +83,20 @@ void CdmFactoryDaemonProxy::ConnectOemCrypto(
   if (daemon_remote_.is_bound()) {
     DVLOG(1) << "CdmFactoryDaemon mojo connection already exists, re-use it";
     CompleteOemCryptoConnection(std::move(oemcryptor),
-                                std::move(protected_buffer_manager));
+                                std::move(protected_buffer_manager),
+                                std::move(output_protection));
     return;
   }
 
-  EstablishDaemonConnection(
-      base::BindOnce(&CdmFactoryDaemonProxy::CompleteOemCryptoConnection,
-                     base::Unretained(this), std::move(oemcryptor),
-                     std::move(protected_buffer_manager)));
+  EstablishDaemonConnection(base::BindOnce(
+      &CdmFactoryDaemonProxy::CompleteOemCryptoConnection,
+      base::Unretained(this), std::move(oemcryptor),
+      std::move(protected_buffer_manager), std::move(output_protection)));
+}
+
+void CdmFactoryDaemonProxy::GetOutputProtection(
+    mojo::PendingReceiver<cdm::mojom::OutputProtection> output_protection) {
+  OutputProtectionImpl::Create(std::move(output_protection));
 }
 
 void CdmFactoryDaemonProxy::SendDBusRequest(base::ScopedFD fd,
@@ -156,7 +174,8 @@ void CdmFactoryDaemonProxy::GetFactoryInterface(
 void CdmFactoryDaemonProxy::CompleteOemCryptoConnection(
     mojo::PendingReceiver<arc::mojom::OemCryptoService> oemcryptor,
     mojo::PendingRemote<arc::mojom::ProtectedBufferManager>
-        protected_buffer_manager) {
+        protected_buffer_manager,
+    mojo::PendingRemote<cdm::mojom::OutputProtection> output_protection) {
   if (!daemon_remote_ || !daemon_remote_.is_bound()) {
     LOG(ERROR) << "daemon_remote_ interface is not connected";
     // Just let the mojo objects go out of scope and be destructed to signal
@@ -164,7 +183,8 @@ void CdmFactoryDaemonProxy::CompleteOemCryptoConnection(
     return;
   }
   daemon_remote_->ConnectOemCrypto(std::move(oemcryptor),
-                                   std::move(protected_buffer_manager));
+                                   std::move(protected_buffer_manager),
+                                   std::move(output_protection));
 }
 
 // static
