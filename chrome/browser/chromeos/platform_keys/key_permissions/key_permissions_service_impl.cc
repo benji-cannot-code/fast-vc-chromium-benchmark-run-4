0b7559060bc715e8de3fe750b1d50706b2621a5b
Omar Morsi <omorsi@google.com>
2020-09-28 13:11:28 +0000

    KeyPermissionsService: Remove IsCorporateKeyForProfile
    This CL removes the static function
    KeyPermissionsService::IsCorporateKeyForProfile because it does not
    account for device-wide keys which are currently always "corporate".
    KeyPermissionsService::IsCorporateKey can be used instead.
    
    Bug: 1127326
    Change-Id: I927611abe296f710c6b7d2187cd1c6c6b5ae47e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2426607
    Commit-Queue: Omar Morsi <omorsi@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811210}

--- chrome/browser/chromeos/platform_keys/key_permissions/key_permissions_service_impl.cc
+++ chrome/browser/chromeos/platform_keys/key_permissions/key_permissions_service_impl.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
 #include "extensions/browser/state_store.h"
+#include "net/cert/scoped_nss_types.h"
+#include "net/cert/x509_util_nss.h"
 
 namespace chromeos {
 namespace platform_keys {
@@ -612,7 +614,6 @@ void KeyPermissionsServiceImpl::IsCorporateKeyWithLocations(
   if (status != Status::kSuccess) {
     LOG(ERROR) << "Key locations retrieval failed: " << StatusToString(status);
     std::move(callback).Run(/*corporate=*/false);
-    return;
   }
 
   std::string public_key_spki_der_b64;
@@ -621,7 +622,7 @@ void KeyPermissionsServiceImpl::IsCorporateKeyWithLocations(
   for (const auto key_location : key_locations) {
     switch (key_location) {
       case TokenId::kUser:
-        if (IsCorporateKeyForProfile(public_key_spki_der_b64, profile_prefs_)) {
+        if (IsUserKeyCorporate(public_key_spki_der_b64)) {
           std::move(callback).Run(/*corporate=*/true);
           return;
         }
@@ -689,21 +690,6 @@ void KeyPermissionsServiceImpl::SetCorporateKeyWithLocations(
 }
 
 // static
-bool KeyPermissionsServiceImpl::IsCorporateKeyForProfile(
-    const std::string& public_key_spki_der_b64,
-    const PrefService* const profile_prefs) {
-  const base::DictionaryValue* prefs_entry =
-      GetPrefsEntry(public_key_spki_der_b64, profile_prefs);
-  if (prefs_entry) {
-    const base::Value* key_usage = prefs_entry->FindKey(kPrefKeyUsage);
-    if (!key_usage || !key_usage->is_string())
-      return false;
-    return key_usage->GetString() == kPrefKeyUsageCorporate;
-  }
-  return false;
-}
-
-// static
 std::vector<std::string>
 KeyPermissionsServiceImpl::GetCorporateKeyUsageAllowedAppIds(
     policy::PolicyService* const profile_policies) {
@@ -741,6 +727,19 @@ void KeyPermissionsServiceImpl::SetPlatformKeysOfExtension(
     std::unique_ptr<base::Value> value) {
   extensions_state_store_->SetExtensionValue(
       extension_id, kStateStorePlatformKeys, std::move(value));
+}
+
+bool KeyPermissionsServiceImpl::IsUserKeyCorporate(
+    const std::string& public_key_spki_der_b64) const {
+  const base::DictionaryValue* prefs_entry =
+      GetPrefsEntry(public_key_spki_der_b64, profile_prefs_);
+  if (prefs_entry) {
+    const base::Value* key_usage = prefs_entry->FindKey(kPrefKeyUsage);
+    if (!key_usage || !key_usage->is_string())
+      return false;
+    return key_usage->GetString() == kPrefKeyUsageCorporate;
+  }
+  return false;
 }
 
 }  // namespace platform_keys
