14a11f76b518251a9975bd1b735745177097d33c
Thanh Nguyen <thanhdng@chromium.org>
2020-09-28 07:18:55 +0000

    [cros search service] Add ClearIndex function to the index
    This CL ClearIndex function to Index and its subclasses. This function
    clears all the data stored in the index.
    
    Bug: 1132170
    Change-Id: Idbec8ed4c721f09577b40c59b9cf24ebe5060252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2428273
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811136}

--- chromeos/components/local_search_service/inverted_index.h
+++ chromeos/components/local_search_service/inverted_index.h
@@ -96,6 +96,9 @@ class InvertedIndex {
   // Builds the inverted index.
   void BuildInvertedIndex();
 
+  // Clears all the data from the inverted index.
+  void ClearInvertedIndex();
+
   // Checks if the inverted index has been built: returns |true| if the inverted
   // index is up to date, returns |false| if there are some modified document
   // since the last time the index has been built.
@@ -119,6 +122,9 @@ class InvertedIndex {
   // Called on the main thread after UpdateDocuments is completed.
   void OnUpdateDocumentsComplete(
       DocumentStateVariables&& document_state_variables);
+
+  void OnDataCleared(
+      std::pair<DocumentStateVariables, TfidfCache>&& inverted_index_data);
 
   base::RepeatingCallback<void()> on_index_built_;
 
@@ -146,6 +152,7 @@ class InvertedIndex {
   bool request_to_build_index_ = false;
   bool update_in_progress_ = false;
   bool index_building_in_progress_ = false;
+  bool request_to_clear_index_ = false;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
