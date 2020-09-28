6aa2885d64d3bd262a275e59e550f589b4a7de8f
Shakti Sahu <shaktisahu@chromium.org>
2020-09-28 04:16:12 +0000

    Reland "Video Tutorials: Added video player UI"
    This is a reland of 278cb45fd7170c0f3afa0d8da1c4cc4f9e4c93e8
    
    Original change's description:
    > Video Tutorials: Added video player UI
    >
    > This CL adds the video player UI.
    > 1 - Added VideoPlayerActivity at chrome layer, which creates
    > VideoPlayerCoordinator and passes a WebContent supplier in order to
    > create ThinWebView.
    > 2 - Added video player MVC, the mediator handles display logic between
    > loading screen, controls, thinwebview, and language picker.
    > It also handles callbacks for control buttons, and observes media
    > session through a PlaybackStateObserver.
    >
    > TODO :
    > Add screenshot. Add UI polish. Define share/watchnext behavior.
    > Polish media session logic.
    >
    > Bug: 1117172
    > Binary-Size: Unavoidable method count increase (includes methods from previous CL).
    > Change-Id: Ide7c0d7bf4fa20a9bfbe0c10717e6983d0da88dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2415672
    > Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#811094}
    
    TBR=dtrainor@chromium.org
    
    Bug: 1117172
    Change-Id: If6c7fd26ba1643ded722eb274f876dad085dd075
    Binary-Size: Unavoidable method count increase (includes methods from previous CL).
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434560
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811112}

--- chrome/browser/ui/webui/video_tutorials/video_player_source.cc
+++ chrome/browser/ui/webui/video_tutorials/video_player_source.cc
@@ -28,7 +28,8 @@ content::WebUIDataSource* CreateVideoPlayerUntrustedDataSource() {
   source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::StyleSrc, "style-src 'self';");
   source->OverrideContentSecurityPolicy(
-      network::mojom::CSPDirectiveName::ScriptSrc, "script-src 'self';");
+      network::mojom::CSPDirectiveName::ScriptSrc,
+      "script-src chrome-untrusted://resources/ 'self';");
 
   return source;
 }
