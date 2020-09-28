19ed1dd8c2b2c61fbe077c4780dd3dbb6a020880
Alan Cutter <alancutter@chromium.org>
2020-09-28 10:22:03 +0000

    Reland "Refactor ExternalWebAppManager config loading flow"
    This is a reland of 02f9f7014c3edc5b6977ec74fc6ed41577d14851
    
    The original change uncovered an incompatibility with the test profile
    generated by AccountManagerUIHandlerTest and
    chromeos::ProfileHelper::IsPrimaryProfile(). This change uncovered it
    by changing the timing of when it gets called resulting in flaky
    failures.
    
    The AccountManagerUIHandlerTest tests were disabled in the meantime
    due to flakiness. A follow up CL re-enables the tests and disables the
    ExternalWebAppManager start up flow such that it doesn't trigger the
    incompatibility with the custom test profile:
    https://chromium-review.googlesource.com/c/chromium/src/+/2434412
    
    Original change's description:
    > Refactor ExternalWebAppManager config loading flow
    >
    > This CL splits ExternalWebAppManager's config loading flow into discrete
    > steps and removes the various testing entry points that omit or
    > duplicate logic used by the real thing.
    >
    > Tests can now inject data (file_utils, config_dir, configs) into the
    > config loading pipeline instead of having separate mini pipelines just
    > for them.
    >
    > This change is in preparation for adding a new "FilterExtensionIds" step
    > to the config loading flow. Without this refactor there would be
    > duplication of logic between real and test code code paths that would be
    > prone to bitrot and missing real coverage.
    >
    > Bug: 1128801
    > Change-Id: Ie5e87b26ab47782fab6589a4127df3ea5e876fec
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2423823
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Glen Robertson <glenrob@chromium.org>
    > Commit-Queue: Alan Cutter <alancutter@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#810025}
    
    Bug: 1128801
    Change-Id: I345ea879861dd62ee38f4e04a72b968f38e2bb15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2428539
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811174}

--- chrome/browser/web_applications/file_utils_wrapper.cc
+++ chrome/browser/web_applications/file_utils_wrapper.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app {
 
-std::unique_ptr<FileUtilsWrapper> FileUtilsWrapper::Clone() {
+std::unique_ptr<FileUtilsWrapper> FileUtilsWrapper::Clone() const {
   return std::make_unique<FileUtilsWrapper>();
 }
 
