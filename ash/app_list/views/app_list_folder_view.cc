b44694ca4b7681e9752a65ef19e518a073e27d9d
Yulun Wu <yulunwu@chromium.org>
2020-09-28 23:45:35 +0000

    Add necessary includes for ash/app_list files.
    chromium/src/ash/app_list/views/app_list_folder_view.cc:260:
    Add #include <utility> for move  [build/include_what_you_use] [4]
    
    chromium/src/ash/app_list/views/folder_header_view.cc:46:
    Add #include <memory> for make_unique<>  [build/include_what_you_use] [4]
    
    Bug: 1130129
    Change-Id: I0f9351349ae983d45e12591f74b97c3d5c5ceff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2419756
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811438}

--- ash/app_list/views/app_list_folder_view.cc
+++ ash/app_list/views/app_list_folder_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/views/app_list_folder_view.h"
 
 #include <algorithm>
+#include <utility>
 #include <vector>
 
 #include "ash/app_list/app_list_metrics.h"
--- ash/app_list/views/folder_header_view.cc
+++ ash/app_list/views/folder_header_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/views/folder_header_view.h"
 
 #include <algorithm>
+#include <memory>
 
 #include "ash/app_list/app_list_util.h"
 #include "ash/app_list/model/app_list_folder_item.h"
