f77d7fa29b2b111a526c6401cbd0a97c49abdc27
Findit <findit-for-me@appspot.gserviceaccount.com>
2020-09-28 07:09:42 +0000

    Revert "Video Tutorials : Moved language to a server side concept"
    This reverts commit 26e5926b66971083d9a050b4348e13ec96cedbe0.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 811131 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzI2ZTU5MjZiNjY5NzEwODNkOWEwNTBiNDM0OGUxM2VjOTZjZWRiZTAM
    
    Sample Failed Build: https://ci.chromium.org/b/8867933801572145104
    
    Sample Failed Step: compile
    
    Original change's description:
    > Video Tutorials : Moved language to a server side concept
    > 
    > This CL
    > 1 - Adds language struct to the proto consisting of name, native text,
    >   and locale.
    > 2 - VideoTutorialService will still store the preferred locale in a
    >   pref which will be used as a key when iterating through the
    >   supporting languages.
    > 3 - Removes the checked in strings for languages
    > 
    > TODO : Change server side proto
    > 
    > Bug: 1132161
    > Change-Id: I1483903e0615714aa790b6af1fbfaa4c6637c802
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2429979
    > Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#811131}
    
    
    Change-Id: I88a5a6858e0ca686b5a28327664dd80c6eeb61a8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1132161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434695
    Cr-Commit-Position: refs/heads/master@{#811134}

--- chrome/browser/video_tutorials/internal/tutorial_service_impl.cc
+++ chrome/browser/video_tutorials/internal/tutorial_service_impl.cc
@@ -64,8 +64,8 @@ void TutorialServiceImpl::OnFetchFinished(
   // TODO(shaktisahu): Save tutorials to the database.
 }
 
-const std::vector<Language>& TutorialServiceImpl::GetSupportedLanguages() {
-  return tutorial_manager_->GetSupportedLanguages();
+std::vector<std::string> TutorialServiceImpl::GetSupportedLocales() {
+  return tutorial_manager_->GetSupportedLocales();
 }
 
 std::string TutorialServiceImpl::GetPreferredLocale() {
