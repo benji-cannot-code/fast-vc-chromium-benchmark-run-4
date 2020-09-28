103f542c88b1403ffe1b6720331b12ff34ce2a5f
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-09-28 04:00:02 +0000

    Import wpt@f5492d8c9fb0c43c0b31fedf5dcb80340cda8585
    Using wpt-import in Chromium 944f0d0b97b0b3ec8529029ad0f1348d1729eb12.
    With Chromium commits locally applied on WPT:
    74e4b2fc2a "[COOP] access reporting: Use infinite timeout for receive."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=foolip@google.com
    
    No-Export: true
    Change-Id: Idccab5c3db88a1c5cee086ee92dedcff52f3f510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434626
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#811109}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/element_click/navigate.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/element_click/navigate.py
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from tests.support.asserts import assert_success
 from tests.support.inline import inline
+from tests.support.sync import Poll
 
 
 def element_click(session, element):
@@ -93,7 +94,34 @@ def test_link_hash(session):
             """, args=(element,)) is True
 
 
-def test_link_closes_window(session, url):
+def test_link_open_target_in_new_window(session, url):
+    orig_handles = session.handles
+
+    session.url = inline("""
+        <a href="{}" target="_blank">Open in new window</a>
+    """.format(inline("<p id=foo")))
+    element = session.find.css("a", all=False)
+
+    response = element_click(session, element)
+    assert_success(response)
+
+    def find_new_handle(session):
+        new_handles = list(set(session.handles) - set(orig_handles))
+        if new_handles and len(new_handles) == 1:
+            return new_handles[0]
+        return None
+
+    wait = Poll(
+        session,
+        timeout=5,
+        message="No new window has been opened")
+    new_handle = wait.until(find_new_handle)
+
+    session.window_handle = new_handle
+    session.find.css("#foo")
+
+
+def test_link_closes_window(session):
     new_handle = session.new_window()
     session.window_handle = new_handle
 
