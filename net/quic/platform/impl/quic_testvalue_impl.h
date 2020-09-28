a3421fab2d369fee34867492fc95b50ffa016f0d
Fan Yang <fayang@chromium.org>
2020-09-28 22:38:34 +0000

    Roll src/net/third_party/quiche/src/ ba9d95e4328b..09a013c1993f704e (8 commits)
    https://quiche.googlesource.com/quiche.git/+log/ba9d95e4328b..09a013c1993f704e
    
    $ git log ba9d95e4328b..09a013c1993f704e --date=short --no-merges --format='%ad %ae %s'
    2020-09-28 wub Change quic_toy_client.cc to include the correct header file for AF_INET/AF_INET6/AF_UNSPEC on windows.
    2020-09-28 bnc Add code count for quic_fix_http3_goaway_stream_id.
    2020-09-28 fayang Dropping initial keys at the end of writing to avoid potential missing write keys in the middle of writing.
    2020-09-28 bnc Do not clip error codes larger than QUIC_LAST_ERROR.
    2020-09-28 quiche-dev Fix //third_party/quic/core:quic_connection_test with randomized flags
    2020-09-25 fayang Notify debug_visitor once any 0-RTT packet gets acknowledged. Client only (server + TLS does not send 0-RTT packets).
    2020-09-25 wub Add flag --ip_version_for_host_lookup to quic_client to indicate the preferred IP version of host address, if host lookup is needed.
    2020-09-25 quiche-dev Fix a latent query-of-death
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    R=dschinazi@chromium.org
    
    Change-Id: I4e66876896183a09d1c33fc3d2f0768bbe71f619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2435748
    Commit-Queue: Fan Yang <fayang@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Auto-Submit: Fan Yang <fayang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811399}

---
+++ net/quic/platform/impl/quic_testvalue_impl.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_QUIC_PLATFORM_IMPL_QUIC_TESTVALUE_IMPL_H_
+#define NET_QUIC_PLATFORM_IMPL_QUIC_TESTVALUE_IMPL_H_
+
+#include "net/third_party/quiche/src/common/platform/api/quiche_string_piece.h"
+
+namespace quic {
+
+template <class T>
+void AdjustTestValueImpl(quiche::QuicheStringPiece label, T* var) {}
+
+}  // namespace quic
+
+#endif  // NET_QUIC_PLATFORM_IMPL_QUIC_TESTVALUE_IMPL_H_
