bf748da1ea6b2c0f443de84125ae2161235359d9
Collin Baker <collinbaker@chromium.org>
2020-09-28 18:22:11 +0000

    Fix //chrome:* build dependencies and enable GN check
    Targets in //chrome:* had several unspecified dependencies that were
    still linked in by other listed dependencies. The targets linked
    correctly, but GN checked failed for these targets. This CL adds all
    dependencies explicitly and re-enables GN check for these targets.
    
    Bug: 949535
    Change-Id: I9855b68e5616164cddad54c7ac49bd44eecd024c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422347
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811303}

--- chrome/app/chrome_exe_main_mac.cc
+++ chrome/app/chrome_exe_main_mac.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_version.h"
 
 #if defined(HELPER_EXECUTABLE)
-#include "sandbox/mac/seatbelt_exec.h"
+#include "sandbox/mac/seatbelt_exec.h"  // nogncheck
 #endif  // defined(HELPER_EXECUTABLE)
 
 extern "C" {
