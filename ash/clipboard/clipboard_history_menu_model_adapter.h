6c3262d978dec30e6ea8aac937112030831a4cf5
Alex Newcomer <newcomer@chromium.org>
2020-09-28 19:54:59 +0000

    cros: Add ClipboardHistory metrics
    Add this metric to help us understand usage and plan
    improvements.
    
    Add ClipboardHistory.ContextMenu.NumberOfItemsShown.
    This metric will help UX see if we should increase the
    size of multipaste, or whether 5 is enough. It will also
    function as a heartbeat metric for the feature.
    
    Add UserJourneyTime to understand usage.
    
    Bug: 1099338
    Change-Id: If3253db5da7ba22554a806d8903929de3e81f17a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2425005
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811341}

--- ash/clipboard/clipboard_history_menu_model_adapter.h
+++ ash/clipboard/clipboard_history_menu_model_adapter.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/optional.h"
+#include "base/time/time.h"
 #include "base/unguessable_token.h"
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/views/controls/menu/menu_model_adapter.h"
@@ -95,6 +96,9 @@ class ASH_EXPORT ClipboardHistoryMenuModelAdapter : views::MenuModelAdapter {
   views::MenuItemView* root_view_ = nullptr;
   // Responsible for showing |root_view_|.
   std::unique_ptr<views::MenuRunner> menu_runner_;
+
+  // The timestamp taken when the menu is opened. Used in metrics.
+  base::TimeTicks menu_open_time_;
 
   // The mapping between the command ids and items that are copied from
   // `clipboard_history_` when the menu is created. It is used to solve the
