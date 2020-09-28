020fadb89f7730e36da27e0b81c50dc039126cfd
Tomasz Wiszkowski <ender@google.com>
2020-09-28 18:10:26 +0000

    Move the SuggestionHost and SuggestionViewDelegate to common Suggestions namespace.
    This change moves the two frequently used interfaces to a shared
    Omnibox suggestions namespace.
    
    Bug: 1106109
    Change-Id: Ida2aa4f3ea2f961b5ab901df62e570fa4402122b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2433053
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#811292}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionHost.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionHost.java
@@ -3,10 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.omnibox.suggestions.basic;
+package org.chromium.chrome.browser.omnibox.suggestions;
 
-import org.chromium.chrome.browser.omnibox.suggestions.DropdownItemProcessor;
-import org.chromium.chrome.browser.omnibox.suggestions.OmniboxSuggestion;
 import org.chromium.ui.modelutil.PropertyModel;
 
 /** A mechanism for creating {@link SuggestionViewDelegate}s. */
--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionViewDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionViewDelegate.java
@@ -3,8 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.omnibox.suggestions.basic;
-
+package org.chromium.chrome.browser.omnibox.suggestions;
 
 /**
  * Handler for actions that happen on suggestion view.
