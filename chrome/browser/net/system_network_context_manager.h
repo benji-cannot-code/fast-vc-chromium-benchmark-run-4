2752ad1f9df2e5114a37504e9ac7a6b80ead4966
Eric Orth <ericorth@chromium.org>
2020-09-28 18:51:09 +0000

    Cleanup HostResolver dependencies
    Remove //chrome/browser from the friend list for
    //net/dns:host_resolve.  gn check passes without it now because all of
    //chrome/browser's usage has been fixed to use //net/dns/public.
    
    Also had to fix one last unused dns_config.h include.
    
    Fixed: 1129993
    Change-Id: I405ecb969398972ed335fa6b3674dc659ad033b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427346
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811320}

--- chrome/browser/net/system_network_context_manager.h
+++ chrome/browser/net/system_network_context_manager.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_member.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
-#include "net/dns/dns_config.h"
 #include "services/network/public/mojom/host_resolver.mojom-forward.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/network_service.mojom-forward.h"
