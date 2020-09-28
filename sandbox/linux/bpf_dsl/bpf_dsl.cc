c3531e4c1d04b0c3d1706cfa3f317384ee17a4df
Matthew Denton <mpdenton@chromium.org>
2020-09-28 08:55:29 +0000

    Linux sandbox: Add UserNotify to bpf dsl
    ...to support the SECCOMP_RET_USER_NOTIF return code for seccomp bpf
    programs.
    
    Bug: 1117351
    Change-Id: I55dfcb4169a4b434cb5cc8894534e1d1e0e7782e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2407035
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811156}

--- sandbox/linux/bpf_dsl/bpf_dsl.cc
+++ sandbox/linux/bpf_dsl/bpf_dsl.cc
@@ -34,7 +34,8 @@ class ReturnResultExprImpl : public internal::ResultExprImpl {
   bool IsAllow() const override { return IsAction(SECCOMP_RET_ALLOW); }
 
   bool IsDeny() const override {
-    return IsAction(SECCOMP_RET_ERRNO) || IsAction(SECCOMP_RET_KILL);
+    return IsAction(SECCOMP_RET_ERRNO) || IsAction(SECCOMP_RET_KILL) ||
+           IsAction(SECCOMP_RET_USER_NOTIF);
   }
 
  private:
@@ -261,6 +262,10 @@ ResultExpr Trap(TrapRegistry::TrapFnc trap_func, const void* aux) {
 ResultExpr UnsafeTrap(TrapRegistry::TrapFnc trap_func, const void* aux) {
   return std::make_shared<TrapResultExprImpl>(trap_func, aux,
                                               false /* unsafe */);
+}
+
+ResultExpr UserNotify() {
+  return std::make_shared<ReturnResultExprImpl>(SECCOMP_RET_USER_NOTIF);
 }
 
 BoolExpr BoolConst(bool value) {
