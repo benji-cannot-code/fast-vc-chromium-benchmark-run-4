fdc946788a2ea0ea2f9c60bf3baeb5e614906129
Alexander Alekseev <alemate@chromium.org>
2020-09-28 21:43:33 +0000

    Revert "exo: Move XkbTracker from WaylandKeyboardDelegate to Seat."
    This reverts commit c1744bc09acfc82f7d7ae176939ca9d803d7b919.
    
    Reason for revert: Broke build: https://crbug.com/1132766
    
    Original change's description:
    > exo: Move XkbTracker from WaylandKeyboardDelegate to Seat.
    >
    > This is preparation to share XkbTracker with wayland IME server.
    >
    > BUG=1123705
    > TEST=Built locally. Run exo_unittests.
    >
    > Change-Id: I7859c2193578cc5a18fa39a96c3fe86d2f39f4dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2428505
    > Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    > Reviewed-by: Jun Mukai <mukai@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#810863}
    
    TBR=mukai@chromium.org,hidehiko@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1123705,1132766
    Change-Id: I92c8df6383ea569ca2b88780c6017c619e26d1af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434097
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811383}

--- components/exo/keyboard_delegate.h
+++ components/exo/keyboard_delegate.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
-#include "base/strings/string_piece.h"
 #include "base/time/time.h"
 
 namespace ui {
@@ -16,7 +15,6 @@ enum class DomCode;
 }
 
 namespace exo {
-struct KeyboardModifiers;
 class Surface;
 
 // Handles events on keyboards in context-specific ways.
@@ -45,7 +43,7 @@ class KeyboardDelegate {
                                  bool pressed) = 0;
 
   // Called when keyboard modifier state changed.
-  virtual void OnKeyboardModifiers(const KeyboardModifiers& modifiers) = 0;
+  virtual void OnKeyboardModifiers(int modifier_flags) = 0;
 
   // Called when key repeat settings are changed.
   virtual void OnKeyRepeatSettingsChanged(bool enabled,
@@ -53,7 +51,9 @@ class KeyboardDelegate {
                                           base::TimeDelta interval) = 0;
 
   // Called when keyboard layout is updated.
-  virtual void OnKeyboardLayoutUpdated(base::StringPiece keymap) = 0;
+  // TODO(hidehiko): Update the argument to pass the keymap
+  // when XkbTracker is moved out from WaylandKeyboardDelegate.
+  virtual void OnKeyboardLayoutUpdated(const std::string& layout_name) = 0;
 };
 
 }  // namespace exo
