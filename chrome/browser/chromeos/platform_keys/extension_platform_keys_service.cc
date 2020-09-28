6d6fa25611bb287ec3a0e7cf22e3d4ec3036cfd3
Omar Morsi <omorsi@google.com>
2020-09-28 20:18:02 +0000

    ExtensionPlatformKeysService: Add a missing return statement
    This CL fixes a bug in
    ExtensionPlatformKeysService::SignTask::OnCanUseKeyForSigningKnown where
    a return statement is missing. That led to calling
    ExtensionPlatformKeysService::SignTask::OnCanUseKeyForSigningKnown::DoStep
    after the task is finished and deleted.
    
    Bug: 1132926
    Change-Id: I0e5be5b4a90e54e045c33e0bfa3568c0737f9272
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2435197
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Omar Morsi <omorsi@google.com>
    Commit-Queue: Omar Morsi <omorsi@google.com>
    Cr-Commit-Position: refs/heads/master@{#811355}

--- chrome/browser/chromeos/platform_keys/extension_platform_keys_service.cc
+++ chrome/browser/chromeos/platform_keys/extension_platform_keys_service.cc
@@ -355,6 +355,7 @@ class ExtensionPlatformKeysService::SignTask : public Task {
                     platform_keys::Status::kErrorKeyNotAllowedForSigning);
       next_step_ = Step::DONE;
       DoStep();
+      return;
     }
 
     DoStep();
