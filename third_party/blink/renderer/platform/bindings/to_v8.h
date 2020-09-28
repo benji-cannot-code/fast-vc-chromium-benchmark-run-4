59f1de41ab1787d116731fa7469420e51bd11d9d
Yuki Shiino <yukishiino@chromium.org>
2020-09-28 06:39:48 +0000

    bind-gen: Move IDL callback APIs to public
    Since https://crrev.com/c/2434004 will be going to introduce
    bindings::CallbackInvokeHelper, which is expected to reduce
    the APK size by 7KB, then some of IDL callback APIs need to
    be accessible from CallbackInvokeHelper.  So this patch
    moves such APIs to public.
    
    Bug: 839389
    Change-Id: I3eafa3992d21d7c011b7fcff07e4c7d57eb8a32f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2432149
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811129}

--- third_party/blink/renderer/platform/bindings/to_v8.h
+++ third_party/blink/renderer/platform/bindings/to_v8.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/callback_interface_base.h"
 #include "third_party/blink/renderer/platform/bindings/dictionary_base.h"
 #include "third_party/blink/renderer/platform/bindings/dom_data_store.h"
+#include "third_party/blink/renderer/platform/bindings/enumeration_base.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/bindings/union_base.h"
@@ -85,6 +86,14 @@ inline v8::Local<v8::Value> ToV8(CallbackInterfaceBase* callback,
           &ScriptState::From(creation_context->CreationContext())->World()));
   return callback ? callback->CallbackObject().As<v8::Value>()
                   : v8::Null(isolate).As<v8::Value>();
+}
+
+// Enumeration
+
+inline v8::Local<v8::Value> ToV8(const bindings::EnumerationBase& enumeration,
+                                 v8::Local<v8::Object> creation_context,
+                                 v8::Isolate* isolate) {
+  return V8String(isolate, enumeration.AsCStr());
 }
 
 // Union type
