b6afbca86ccdd72f2ff9a3a43e5b3ba0adffaab9
tby <tby@chromium.org>
2020-09-29 05:45:07 +0000

    [Suggested files] Several misc fixes.
     - Increases the max response size to 1MB. 10KB was too small, and
       valid responses were being ignored.
    
     - Removes the `type_detail_fields` in the request. I may re-add this
       later once we have a good idea of what fields we want to filter.
    
     - Adds TODOs for error metrics in drive_zero_state_provider.
    
     - Fixes how the FilePathOrErrorPtr is used in OnFilePathsLocated.
       Calling get_error() isn't valid unless is_error().
    
     - Reparents result file paths into the Drive mount. It turns out the
       LocateFilesByItemIds only returns paths relative to the root of
       the user's drive fs mount, so we need to prepend the location of the
       mount itself. This is copied from drive_quick_access_provider.
    
     - If suggested files is enabled, create chip results.
    
    Bug: 1034842
    Change-Id: I1aa8aad4b313a9e15af7beb1a6346bb00e1ba98d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434596
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811591}

--- chrome/browser/ui/app_list/search/files/item_suggest_cache.cc
+++ chrome/browser/ui/app_list/search/files/item_suggest_cache.cc
@@ -31,8 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace app_list {
 namespace {
 
-// Maximum accepted size of an ItemSuggest response. 10 KB.
-constexpr int kMaxResponseSize = 10 * 1024;
+// Maximum accepted size of an ItemSuggest response. 1MB.
+constexpr int kMaxResponseSize = 1024 * 1024;
 
 // TODO(crbug.com/1034842): Investigate:
 //  - enterprise policies that should limit this traffic.
@@ -70,8 +70,7 @@ constexpr char kRequestBody[] = R"({
         'platform_type': 'CHROME_OS',
         'scenario_type': 'CHROME_OS_ZSS_FILES'
       },
-      'max_suggestions': 10,
-      'type_detail_fields': []
+      'max_suggestions': 10
     })";
 
 bool IsDisabledByPolicy(const Profile* profile) {
