8c53b24dff796b0b00572e84cef7ad8ff6f5ce08
Dominic Farolino <dom@chromium.org>
2020-09-29 00:14:10 +0000

    [MBI]: Move RouteProvider impl from ChildThreadImpl => RenderThreadImpl
    This CL moves the RouteProvider implementation from ChildThreadImpl,
    to RenderThreadImpl. This is because RenderThreadImpl is the only type
    of ChildThread(Impl) that uses this functionality, therefore this CL
    creates a healthier boundary between render threads and child threads.
    
    At the same time, it simplifies a nasty code path present in both
    RenderFrameImpl/RenderFrameProxy::GetRemoteAssociatedInterfaces() that
    separates a "production" path vs a "test" path. We do this by
    introducing the GetRemoteRouteProvider() method on the RenderThread
    public API, and providing two implementations:
     - RenderThreadImpl
        - Responsible for returning a legitimate mojom::RouteProvider* that
          will be hooked up to a real cross-process implementation
     - ChildThreadImpl
        - Responsible for providing a dummy mojom::RouteProvider* bound to
          nothing, so that the messages sent over it are simply drained
    
    This is a reasonable tradeoff over the alternative solution of
    introducing a MockAgentSchedulingGroup to handle cases where its
    RenderThread is a MockRenderThread. If things regarding RenderThread
    should be mocked, then it makes sense for RenderThread implementations
    to mock them, as opposed to have mocks appear in both MockRenderThread
    *and* MockAgentSchedulingGroup, which is slightly more awkward and
    cumbersome.
    
    
    Bug: 1111231, 1132901
    Change-Id: I903553a67f14d22d654556c4398d169196763a76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2433793
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811451}

--- content/public/test/mock_render_thread.cc
+++ content/public/test/mock_render_thread.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/unguessable_token.h"
 #include "build/build_config.h"
+#include "content/common/associated_interfaces.mojom.h"
 #include "content/common/frame_messages.h"
 #include "content/common/render_message_filter.mojom.h"
 #include "content/common/view_messages.h"
@@ -57,6 +58,18 @@ class MockRenderMessageFilterImpl : public mojom::RenderMessageFilter {
                          base::ThreadPriority thread_priority) override {}
 #endif
 };
+
+// Some tests require that a valid mojo::RouteProvider* be accessed to send
+// messages over. The RouteProvider does not need to be bound to any real
+// implementation, so we simply bind it to a pipe that we'll forget about, as to
+// drain all messages sent over the remote.
+mojom::RouteProvider* GetStaticRemoteRouteProvider() {
+  static mojo::Remote<mojom::RouteProvider> remote;
+  if (!remote) {
+    ignore_result(remote.BindNewPipeAndPassReceiver());
+  }
+  return remote.get();
+}
 
 }  // namespace
 
@@ -157,6 +170,10 @@ void MockRenderThread::AddObserver(RenderThreadObserver* observer) {
 
 void MockRenderThread::RemoveObserver(RenderThreadObserver* observer) {
   observers_.RemoveObserver(observer);
+}
+
+mojom::RouteProvider* MockRenderThread::GetRemoteRouteProvider() {
+  return GetStaticRemoteRouteProvider();
 }
 
 void MockRenderThread::SetResourceDispatcherDelegate(
