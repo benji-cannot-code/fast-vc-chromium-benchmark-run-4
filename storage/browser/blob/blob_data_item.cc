768a8e50d1abd0ce8691d57165c8ca93b6a8ae17
Ben Kelly <wanderview@chromium.org>
2020-09-29 21:16:56 +0000

    Blob: Reduce backing std::vector capacity when shrinking bytes.
    As pointed out by mek@, currently BlobDataItem::ShrinkBytes only resizes
    the number of elements in the backing std::vector.  The backing capacity
    of the vector remains the same.  For any blob less than 512KB this will
    result in wasted memory usage.
    
    Bug: 1133110
    Change-Id: I71de9ce6cf75afbc7029d86841c8e5a754b69b52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2436868
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811834}

--- storage/browser/blob/blob_data_item.cc
+++ storage/browser/blob/blob_data_item.cc
@@ -194,6 +194,7 @@ void BlobDataItem::ShrinkBytes(size_t new_length) {
   DCHECK_EQ(type_, Type::kBytes);
   length_ = new_length;
   bytes_.resize(length_);
+  bytes_.shrink_to_fit();
 }
 
 void BlobDataItem::PopulateFile(
