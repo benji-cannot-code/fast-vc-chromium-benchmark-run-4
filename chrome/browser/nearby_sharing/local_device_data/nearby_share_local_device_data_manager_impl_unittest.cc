6551f271915f96023a27ee4be9ab55a60e6542ee
Josh Nohle <nohle@google.com>
2020-09-29 18:01:01 +0000

    [Nearby] Fix device name tests by null-terminating test string
    Verified that local device data manager unit tests failed with an ASAN
    build before this change and pass after the change.
    
    Fixed: 1133295
    Change-Id: I51dfd183948c967aefff77defa36e689afa16a59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438167
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Commit-Queue: James Vecore <vecore@google.com>
    Auto-Submit: Josh Nohle <nohle@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#811762}

--- chrome/browser/nearby_sharing/local_device_data/nearby_share_local_device_data_manager_impl_unittest.cc
+++ chrome/browser/nearby_sharing/local_device_data/nearby_share_local_device_data_manager_impl_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/optional.h"
 #include "base/test/bind_test_util.h"
-#include "build/build_config.h"
 #include "chrome/browser/nearby_sharing/client/fake_nearby_share_client.h"
 #include "chrome/browser/nearby_sharing/common/nearby_share_prefs.h"
 #include "chrome/browser/nearby_sharing/local_device_data/fake_nearby_share_device_data_updater.h"
@@ -29,7 +28,7 @@ const char kFakeDefaultDeviceName[] = "Barack's Chromebook";
 const char kFakeDeviceName[] = "My Cool Chromebook";
 const char kFakeEmptyDeviceName[] = "";
 const char kFakeTooLongDeviceName[] = "this string is 33 bytes in UTF-8!";
-const char kFakeInvalidDeviceName[] = {0xC0};
+const char kFakeInvalidDeviceName[] = {0xC0, 0x00};
 const char kFakeFullName[] = "Barack Obama";
 const char kFakeIconUrl[] = "https://www.google.com";
 
@@ -261,13 +260,7 @@ TEST_F(NearbyShareLocalDeviceDataManagerImplTest, DeviceId) {
   EXPECT_EQ(id, manager()->GetId());
 }
 
-// Test is flaky. crbug.com/1133295.
-#if defined(OS_CHROMEOS)
-#define MAYBE_ValidateDeviceName DISABLED_ValidateDeviceName
-#else
-#define MAYBE_ValidateDeviceName ValidateDeviceName
-#endif
-TEST_F(NearbyShareLocalDeviceDataManagerImplTest, MAYBE_ValidateDeviceName) {
+TEST_F(NearbyShareLocalDeviceDataManagerImplTest, ValidateDeviceName) {
   CreateManager();
   EXPECT_EQ(manager()->ValidateDeviceName(kFakeDeviceName),
             nearby_share::mojom::DeviceNameValidationResult::kValid);
@@ -280,13 +273,7 @@ TEST_F(NearbyShareLocalDeviceDataManagerImplTest, MAYBE_ValidateDeviceName) {
       nearby_share::mojom::DeviceNameValidationResult::kErrorNotValidUtf8);
 }
 
-// Test is flaky. crbug.com/1133295.
-#if defined(OS_CHROMEOS)
-#define MAYBE_SetDeviceName DISABLED_SetDeviceName
-#else
-#define MAYBE_SetDeviceName SetDeviceName
-#endif
-TEST_F(NearbyShareLocalDeviceDataManagerImplTest, MAYBE_SetDeviceName) {
+TEST_F(NearbyShareLocalDeviceDataManagerImplTest, SetDeviceName) {
   CreateManager();
 
   // The default device name is set in the ctor when the device name is empty.
