8e2109ee67cdd6243250be62c512e051ef7dd6be
tby <tby@chromium.org>
2020-09-29 10:40:12 +0000

    [Suggested files] Fixes for chip results.
    This CL contains a handful of fixes that prevent chip results appearing.
    
     - On login, query ItemSuggest once, so we have results ready to display
       the first time the launcher is opened. This is mostly copied from
       drive_quick_access_provider.
    
     - Change some display and result type enums for chip results, needed to
       rank and display them correctly.
    
     - Use a separate schema for chip results, which prevents them being
       deduplicated with list results.
    
    Bug: 1034842
    Change-Id: I4da6cc219e19ad05383acdd2f98f64c6b4451a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434111
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811626}

--- chrome/browser/ui/app_list/search/files/file_result.cc
+++ chrome/browser/ui/app_list/search/files/file_result.cc
@@ -59,8 +59,10 @@ FileResult::FileResult(const std::string& schema,
   SetResultType(result_type);
   switch (result_type) {
     case ResultType::kDriveQuickAccess:
+    case ResultType::kDriveQuickAccessChip:
       SetMetricsType(ash::DRIVE_QUICK_ACCESS);
       break;
+    case ResultType::kFileChip:
     case ResultType::kZeroStateFile:
       SetMetricsType(ash::ZERO_STATE_FILE);
       break;
