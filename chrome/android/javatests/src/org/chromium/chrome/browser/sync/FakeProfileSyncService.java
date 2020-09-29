72bab3f3a0731b0072a9b34bdca2f6ec8b0fb28f
Patrick Noland <pnoland@chromium.org>
2020-09-29 22:16:57 +0000

    Revert "[Android] Implement tests for SyncErrorCardPreference"
    This reverts commit 773b24b93249962d73f8c74ff84473db62eb62cf.
    
    Reason for revert: Tests are failing on android-arm-official-tests
    https://ci.chromium.org/p/chrome/builders/ci/android-arm-official-tests/1369
    
    Original change's description:
    > [Android] Implement tests for SyncErrorCardPreference
    >
    > This cl adds render tests for different types of sync errors in
    > SyncErrorCardPreference.
    >
    > Bug: 1107420
    > Change-Id: I3ebc308932eadb677310ea041559bb54bbb1765c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2353255
    > Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Reviewed-by: Alice Wang <aliceywang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#811273}
    
    TBR=treib@chromium.org,triploblastic@chromium.org,aliceywang@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1107420
    Change-Id: Ib86c9353dd3f5389ec7446add5c60b1031f59893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438611
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811865}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/FakeProfileSyncService.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/FakeProfileSyncService.java
@@ -26,7 +26,6 @@ public class FakeProfileSyncService extends ProfileSyncService {
     private boolean mTrustedVaultKeyRequired;
     private boolean mTrustedVaultKeyRequiredForPreferredDataTypes;
     private boolean mEncryptEverythingEnabled;
-    private boolean mRequiresClientUpgrade;
     private Set<Integer> mChosenTypes = new HashSet<>();
     private boolean mCanSyncFeatureStart;
     @GoogleServiceAuthError.State
@@ -140,14 +139,5 @@ public class FakeProfileSyncService extends ProfileSyncService {
 
     public void setCanSyncFeatureStart(boolean canSyncFeatureStart) {
         mCanSyncFeatureStart = canSyncFeatureStart;
-    }
-
-    @Override
-    public boolean requiresClientUpgrade() {
-        return mRequiresClientUpgrade;
-    }
-
-    public void setRequiresClientUpgrade(boolean requiresClientUpgrade) {
-        mRequiresClientUpgrade = requiresClientUpgrade;
     }
 }
