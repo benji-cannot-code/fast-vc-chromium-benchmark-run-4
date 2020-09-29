694cf58aec37df509d8c04659bbaad2b0f357a29
Christopher Cameron <ccameron@chromium.org>
2020-09-29 18:10:20 +0000

    Let VideoDecodeAccelerator indicate SharedImage support
    Add a method to media::VideoDecodeAccelerator to indicate whether
    SharedImage-backed picture buffers are supported. Make this return
    true only for the VTVideoDecodeAccelerator.
    
    Add a parameter to media::PictureBufferManager::CreatePictureBuffers
    to indicate if SharedImage-backed PictureBuffers are desired.
    
    Hook the two of these together in the ProvidePictureBuffersAsync
    method of media::VdaVideoDecoder. Leave this disabled for now.
    
    Bug: 1108909
    Change-Id: I539f03bdca232fe32969ab5e92b6aea34c6d99f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2436313
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811766}

--- media/gpu/mac/vt_video_decode_accelerator_mac.cc
+++ media/gpu/mac/vt_video_decode_accelerator_mac.cc
@@ -1691,6 +1691,11 @@ bool VTVideoDecodeAccelerator::TryToSetupDecodeOnSeparateThread(
   return false;
 }
 
+bool VTVideoDecodeAccelerator::SupportsSharedImagePictureBuffers() const {
+  // TODO(https://crbug.com/1108909): Enable shared image use on macOS.
+  return false;
+}
+
 // static
 VideoDecodeAccelerator::SupportedProfiles
 VTVideoDecodeAccelerator::GetSupportedProfiles() {
