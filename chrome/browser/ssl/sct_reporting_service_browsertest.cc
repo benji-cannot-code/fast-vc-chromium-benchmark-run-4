be7cf332fab8d9537d9f7218bf5975b9dc706a20
Jochen Eisinger <jochen@chromium.org>
2020-09-29 12:47:09 +0000

    Fix flaky SCTReportingServiceBrowserTest tests
    Bug: 1131759
    Change-Id: Ic46050d835b76a9f972c262fb8f9e3958412541f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2436336
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811649}

--- chrome/browser/ssl/sct_reporting_service_browsertest.cc
+++ chrome/browser/ssl/sct_reporting_service_browsertest.cc
@@ -54,7 +54,7 @@ class ConnectionListener
       std::unique_ptr<net::StreamSocket> socket) override {}
 
   void WaitForConnections(size_t num_connections) {
-    if (connections_seen_ >= connections_expected_)
+    if (connections_seen_ >= num_connections)
       return;
 
     connections_expected_ = num_connections;
@@ -207,7 +207,7 @@ IN_PROC_BROWSER_TEST_F(SCTReportingServiceBrowserTest,
 
 // Tests that disabling Extended Reporting causes the cache to be cleared.
 IN_PROC_BROWSER_TEST_F(SCTReportingServiceBrowserTest,
-                       DISABLED_OptingOutClearsSCTAuditingCache) {
+                       OptingOutClearsSCTAuditingCache) {
   // Enable SCT auditing and enqueue a report.
   SetExtendedReportingEnabled(true);
 
@@ -232,7 +232,7 @@ IN_PROC_BROWSER_TEST_F(SCTReportingServiceBrowserTest,
 // Tests that reports are still sent for opted-in profiles after the network
 // service crashes and is restarted.
 IN_PROC_BROWSER_TEST_F(SCTReportingServiceBrowserTest,
-                       DISABLED_ReportsSentAfterNetworkServiceRestart) {
+                       ReportsSentAfterNetworkServiceRestart) {
   // This test is only applicable to out-of-process network service because it
   // tests what happens when the network service crashes and restarts.
   if (content::IsInProcessNetworkService()) {
