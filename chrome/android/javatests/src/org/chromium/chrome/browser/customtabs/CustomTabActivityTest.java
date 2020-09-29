950991044b66c1e562af96eeca5d9082fdaca986
Patrick Noland <pnoland@chromium.org>
2020-09-29 17:31:46 +0000

    [ToolbarMVC] Create dedicated ThemeColorProvider for CustomTabToolbar
    CustomTabToolbar follows different coloring logic that isn't fully
    captured by either AppThemeColorProvider or TabThemeColorProvider, since
    it also tracks the color set by CustomTabToolbarColorController due to,
    e.g. the possibility of customization by the host app. Unfortunately
    CustomTabToolbarColorController isn't a ThemeColorProvider, but we can
    instead create a dumb ThemeColorProvider that just blindly tracks it.
    
    Bug: 1128337
    Change-Id: I9d910ee049a7a96d485053214b6dab6c0110f28d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2430228
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811755}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -114,6 +114,7 @@ import org.chromium.chrome.browser.tab.TabTestUtils;
 import org.chromium.chrome.browser.tabmodel.TabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.test.ScreenShooter;
+import org.chromium.chrome.browser.toolbar.menu_button.MenuButton;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuCoordinator;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuHandler;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuTestSupport;
@@ -809,6 +810,10 @@ public class CustomTabActivityTest {
             assertEquals(ColorUtils.getDarkenedColorForStatusBar(expectedColor),
                     mCustomTabActivityTestRule.getActivity().getWindow().getStatusBarColor());
         }
+
+        MenuButton menuButtonView = toolbarView.findViewById(R.id.menu_button_wrapper);
+        assertEquals(menuButtonView.getUseLightDrawablesForTesting(),
+                ColorUtils.shouldUseLightForegroundOnBackground(expectedColor));
     }
 
     /**
@@ -2144,6 +2149,9 @@ public class CustomTabActivityTest {
 
         Assert.assertTrue("Night mode should be enabled on K+ with dark color scheme set.",
                 cctActivity.getNightModeStateProvider().isInNightMode());
+
+        MenuButton menuButtonView = cctActivity.findViewById(R.id.menu_button_wrapper);
+        assertTrue(menuButtonView.getUseLightDrawablesForTesting());
     }
 
     @Test
@@ -2158,6 +2166,9 @@ public class CustomTabActivityTest {
 
         Assert.assertNotNull(cctActivity.getNightModeStateProvider());
         Assert.assertFalse(cctActivity.getNightModeStateProvider().isInNightMode());
+
+        MenuButton menuButtonView = cctActivity.findViewById(R.id.menu_button_wrapper);
+        assertFalse(menuButtonView.getUseLightDrawablesForTesting());
     }
 
     private void addColorSchemeToIntent(Intent intent, int colorScheme) {
