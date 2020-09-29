6340d31e3baa4ceb5fd1ef845f6c6ea19f42d77b
Klaus Weidner <klausw@chromium.org>
2020-09-29 22:04:08 +0000

    Remove unused "border" uniforms for ARRenderer
    The shaders were originally copied from VR Browser, and AR
    mode doesn't need support for a border region. Remove that
    to simplify it.
    
    Change-Id: I5ac59e32e4b029e6f94d54a64a1638a7df92f758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438610
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Auto-Submit: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811861}

--- chrome/browser/android/vr/arcore_device/ar_renderer.h
+++ chrome/browser/android/vr/arcore_device/ar_renderer.h
@@ -18,10 +18,7 @@ class ArRenderer {
   ArRenderer();
   ~ArRenderer();
 
-  void Draw(int texture_handle,
-            const float (&uv_transform)[16],
-            float xborder,
-            float yborder);
+  void Draw(int texture_handle, const float (&uv_transform)[16]);
 
  private:
   GLuint program_handle_ = 0;
@@ -29,8 +26,6 @@ class ArRenderer {
   GLuint clip_rect_handle_ = 0;
   GLuint texture_handle_ = 0;
   GLuint uv_transform_ = 0;
-  GLuint x_border_handle_ = 0;
-  GLuint y_border_handle_ = 0;
   GLuint vertex_buffer_ = 0;
   GLuint index_buffer_ = 0;
 
