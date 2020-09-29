30bdf7b3ea6c7029023ad82ce54882805b134ec2
Yi Gu <yigu@chromium.org>
2020-09-29 21:52:30 +0000

    Disable ThreadDelegatePosixTest.CurrentThreadBase on ChromeOS
    The test has been flaky since it was introduced. See bug for details.
    
    TBR=ssid@chromium.org
    
    Bug: 1133434
    Change-Id: I7ecb38170912745d7b86fc3128b1601c8e6537e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438969
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811854}

--- base/profiler/thread_delegate_posix_unittest.cc
+++ base/profiler/thread_delegate_posix_unittest.cc
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 // ASAN moves local variables outside of the stack extents.
-#if defined(ADDRESS_SANITIZER)
+// Test is flaky on ChromeOS. crbug.com/1133434.
+#if defined(ADDRESS_SANITIZER) || defined(OS_CHROMEOS)
 #define MAYBE_CurrentThreadBase DISABLED_CurrentThreadBase
 #else
 #define MAYBE_CurrentThreadBase CurrentThreadBase
