eccd2175d808fe16d69e8f70fdc5db5160ea5ae0
Titouan Rigoudy <titouan@chromium.org>
2020-09-29 13:29:55 +0000

    [CORS-RFC1918] Block insecure navigation requests.
    For subresource URL requests, the renderer URLLoader uses its factory's
    parameters to determine the client security state of the request
    initiator. Navigations however use a central URLLoaderFactory in the
    browser process.
    
    This change propagates the client security state in the ResourceRequest
    to the URLLoader as a trusted parameter, and acts on this state in
    URLLoader.
    
    It only improves the situation for non-top-level navigation requests.
    The latter need some thought given to the UX first, see:
    https://crbug.com/1129326.
    
    Bug: chromium:1124346
    Change-Id: I8736706ef402831375f34f9c7f2a96d3059c25be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410406
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811670}

--- content/browser/renderer_host/render_frame_host_impl_browsertest.cc
+++ content/browser/renderer_host/render_frame_host_impl_browsertest.cc
@@ -88,7 +88,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 namespace {
 
-using ::testing::EndsWith;
 using ::testing::NotNull;
 
 // Implementation of ContentBrowserClient that overrides
@@ -3939,11 +3938,8 @@ IN_PROC_BROWSER_TEST_F(
   // Check that the child iframe failed to fetch.
   ASSERT_EQ(1ul, root_frame_host()->child_count());
   auto* child_frame = root_frame_host()->child_at(0)->current_frame_host();
-  // TODO(crbug.com/1124346): Expect 0 once the load fails.
-  EXPECT_EQ(200, child_frame->last_http_status_code());
-  // TODO(crbug.com/1124346): Expect an empty URL once the load fails.
-  EXPECT_THAT(child_frame->last_successful_url().spec(),
-              EndsWith("/empty.html"));
+  EXPECT_EQ(0, child_frame->last_http_status_code());
+  EXPECT_EQ(GURL(), child_frame->last_successful_url());
 }
 
 namespace {
