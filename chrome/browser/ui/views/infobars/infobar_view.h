fcdca324e66e8a130673cdb1b1321a1afbf4d8d3
Peter Kasting <pkasting@chromium.org>
2020-09-29 02:31:36 +0000

    Use callbacks instead of ButtonPressed overrides: c/b/ui/views/infobars/
    Bug: 772945
    Change-Id: I81bdb9ed1de679f6b726c672bbd1519c0fcb2c7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2436413
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811514}

--- chrome/browser/ui/views/infobars/infobar_view.h
+++ chrome/browser/ui/views/infobars/infobar_view.h
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/infobars/core/infobar.h"
 #include "components/infobars/core/infobar_container.h"
 #include "third_party/skia/include/core/SkPath.h"
-#include "ui/views/controls/button/button.h"
 #include "ui/views/controls/menu/menu_types.h"
 #include "ui/views/focus/external_focus_tracker.h"
+#include "ui/views/view.h"
 
 namespace views {
 class ImageButton;
@@ -25,7 +25,6 @@ class MenuRunner;
 
 class InfoBarView : public infobars::InfoBar,
                     public views::View,
-                    public views::ButtonListener,
                     public views::ExternalFocusTracker {
  public:
   explicit InfoBarView(std::unique_ptr<infobars::InfoBarDelegate> delegate);
@@ -42,11 +41,6 @@ class InfoBarView : public infobars::InfoBar,
       const views::ViewHierarchyChangedDetails& details) override;
   void OnPaint(gfx::Canvas* canvas) override;
   void OnThemeChanged() override;
-
-  // views::ButtonListener:
-  // NOTE: This must not be called if we're unowned.  (Subclasses should ignore
-  // calls to ButtonPressed() in this case.)
-  void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
   // views::ExternalFocusTracker:
   void OnWillChangeFocus(View* focused_before, View* focused_now) override;
@@ -111,6 +105,8 @@ class InfoBarView : public infobars::InfoBar,
 
   // Callback used by the link created by CreateLink().
   void LinkClicked(const ui::Event& event);
+
+  void CloseButtonPressed();
 
   // The optional icon at the left edge of the InfoBar.
   views::ImageView* icon_ = nullptr;
