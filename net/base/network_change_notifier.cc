fdef807ef4c481aafd2d86a211e573397077681c
Ken Rockot <rockot@google.com>
2020-09-29 00:16:38 +0000

    Add histogram for connection type changes
    This records the network connection type any time it changes.
    
    Also adds an enum histogram value for the new 5G type that was
    recently added to the ConnectionType enum.
    
    Bug: 1127134
    Change-Id: I5a7d1306b69c4714a8b24a5462871aa0842121d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2431589
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811453}

--- net/base/network_change_notifier.cc
+++ net/base/network_change_notifier.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "base/optional.h"
 #include "base/sequence_checker.h"
@@ -150,6 +151,10 @@ class NetworkChangeNotifier::NetworkChangeCalculator
         (pending_connection_type_ == CONNECTION_NONE)) {
       return;
     }
+
+    base::UmaHistogramEnumeration("Net.NetworkChangeNotifier.NewConnectionType",
+                                  pending_connection_type_, CONNECTION_LAST);
+
     have_announced_ = true;
     last_announced_connection_type_ = pending_connection_type_;
     // Immediately before sending out an online signal, send out an offline
