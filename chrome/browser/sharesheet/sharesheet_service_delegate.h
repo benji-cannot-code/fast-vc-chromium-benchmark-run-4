3b9d880336b916be2c40d2c5cb1c406273bcdec6
Melissa Zhang <melzhang@chromium.org>
2020-09-29 01:40:25 +0000

    [Sharesheet] Remove unused code. Fix crash bug.
    This CL removes unused constructor code and fixes crash when
    sharesheet is launched from non-files app.
    
    Bug: 1132697
    Change-Id: Ib61cf18393eaec5eae9b9f6ea7d16b2bfe6153fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434069
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811498}

--- chrome/browser/sharesheet/sharesheet_service_delegate.h
+++ chrome/browser/sharesheet/sharesheet_service_delegate.h
@@ -33,9 +33,6 @@ class SharesheetService;
 class SharesheetServiceDelegate : public SharesheetController {
  public:
   SharesheetServiceDelegate(uint32_t id,
-                            views::View* bubble_anchor_view,
-                            SharesheetService* sharesheet_service);
-  SharesheetServiceDelegate(uint32_t id,
                             content::WebContents* web_contents,
                             SharesheetService* sharesheet_service);
   ~SharesheetServiceDelegate() override;
