30da494f1b1e12249df43d79493661c01ba5a3fb
Dominic Mazzoni <dmazzoni@chromium.org>
2020-09-29 03:19:33 +0000

    Add an initial perf test for automation.
    Measures the amount of time spent unserializing changes to the
    accessibility tree and sending automation events.
    
    In order to scale reasonably well across different machines, this
    time is compared to the amount of time spent in renderer
    accessibility code, that way the test only fails if the
    disparity is too large.
    
    The test code is deliberately all one test for ease of review.
    As we add more tests, a lot of the code could be broken up into
    helpers.
    
    Bug: none
    Change-Id: I44a169a7c80d9ccea5cf70cb7450731a43638c7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2430304
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811534}

---
+++ chrome/test/data/extensions/api_test/automation/tests/tabs/textarea_append_perf.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var allTests = [
+  function testTextareaAppend() {
+    let runButton = rootNode.find({ role: 'button'});
+    assertEq('button', runButton.role);
+
+    let doneLink = rootNode.find({ role: 'link'});
+    assertEq('link', doneLink.role);
+
+    rootNode.addEventListener('childrenChanged', (evt) => {
+    });
+
+    doneLink.addEventListener('focus', () => {
+      chrome.test.succeed();
+    });
+
+    runButton.doDefault();
+  }
+];
+
+setUpAndRunTests(allTests, 'textarea_append.html');
--- extensions/renderer/api/automation/automation_ax_tree_wrapper.cc
+++ extensions/renderer/api/automation/automation_ax_tree_wrapper.cc
@@ -45,6 +45,9 @@ AutomationAXTreeWrapper* AutomationAXTreeWrapper::GetParentOfTreeId(
 bool AutomationAXTreeWrapper::OnAccessibilityEvents(
     const ExtensionMsg_AccessibilityEventBundleParams& event_bundle,
     bool is_active_profile) {
+  TRACE_EVENT0("accessibility",
+               "AutomationAXTreeWrapper::OnAccessibilityEvents");
+
   base::Optional<gfx::Rect> previous_accessibility_focused_global_bounds =
       owner_->GetAccessibilityFocusedLocation();
 
