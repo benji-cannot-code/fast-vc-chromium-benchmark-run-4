016bd3a54910e2963b5c8bc4a5a2ecd8a0481ea1
Alan Cutter <alancutter@chromium.org>
2020-09-29 11:37:05 +0000

    Add launch_query_params option to default web app config
    This CL adds a "launch_query_params" field to default web app JSON
    configs that causes the web app to launch with the specified query
    params appended to the app's start_url.
    
    Bug: 1045537
    Change-Id: Ib6025ae39eac7a6f710b2817a0f2ad30437e29c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2377044
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811641}

--- chrome/browser/web_applications/components/install_manager.h
+++ chrome/browser/web_applications/components/install_manager.h
@@ -123,6 +123,8 @@ class InstallManager {
     bool require_manifest = false;
 
     std::vector<std::string> additional_search_terms;
+
+    base::Optional<std::string> launch_query_params;
   };
   // Starts a background web app installation process for a given
   // |web_contents|.
