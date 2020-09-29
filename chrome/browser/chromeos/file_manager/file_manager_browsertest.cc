4718d30a568b1eeefa5633d2732939c4791a3ee9
Satoshi Niwa <niwa@google.com>
2020-09-29 18:05:34 +0000

    Add an integration test for LaunchParams.searchQuery
    CL that implements the feature : crrev.com/c/2408502
    Doc about the feature : go/arc-r-documents-ui
    
    Cq-Depend: chromium:2434070
    Bug: 1126742
    Test: browser_tests --gtest_filter="*FilesApp*SearchQueryLaunchParam"
    Change-Id: I7d241f9e9e42b5669be6a4a012296537aad9f218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434008
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Auto-Submit: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811764}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -1000,7 +1000,8 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
                       TestCase("searchDownloadsClearSearch"),
                       TestCase("searchHidingViaTab"),
                       TestCase("searchHidingTextEntryField"),
-                      TestCase("searchButtonToggles")));
+                      TestCase("searchButtonToggles"),
+                      TestCase("searchQueryLaunchParam")));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
     Metrics, /* metrics.js */
