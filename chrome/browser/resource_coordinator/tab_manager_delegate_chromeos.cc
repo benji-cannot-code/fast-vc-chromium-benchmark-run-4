cdd9a9ba2fb91594a7a47f2c5c71aa920104ad49
Kuo-Hsin Yang <vovoy@chromium.org>
2020-09-29 19:29:21 +0000

    Remove feature option CrOSUserSpaceLowMemoryNotification
    And remove these dead code path, these dead code paths make it harder to
    modify SystemMemoryPressureEvaluator.
    
    BUG=b:149833548
    TEST=run chromeos_unittests
    
    Change-Id: I594009d05b39be43332708fd973e1a02031879a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2421189
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Kuo-Hsin Yang <vovoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811801}

--- chrome/browser/resource_coordinator/tab_manager_delegate_chromeos.cc
+++ chrome/browser/resource_coordinator/tab_manager_delegate_chromeos.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/memory/pressure/pressure.h"
 #include "chromeos/memory/pressure/system_memory_pressure_evaluator.h"
 #include "components/arc/arc_service_manager.h"
 #include "components/arc/arc_util.h"
@@ -235,14 +236,14 @@ int TabManagerDelegate::MemoryStat::LowMemoryMarginKB() {
 // memory back to the margin.
 int TabManagerDelegate::MemoryStat::TargetMemoryToFreeKB() {
   uint64_t available_mem_mb;
-  auto* monitor = chromeos::memory::SystemMemoryPressureEvaluator::Get();
-  if (monitor) {
-    available_mem_mb = monitor->GetAvailableMemoryKB();
+  if (chromeos::memory::SystemMemoryPressureEvaluator::Get()) {
+    available_mem_mb = chromeos::memory::pressure::GetAvailableMemoryKB();
   } else {
     // When TabManager::DiscardTab(LifecycleUnitDiscardReason::EXTERNAL) is
     // called by a test or an extension, TabManagerDelegate might be used
     // without chromeos SystemMemoryPressureEvaluator, e.g. the browser test
-    // DiscardTabsWithMinimizedWindow.
+    // DiscardTabsWithMinimizedWindow. Set available to 0 to force discarding a
+    // tab to pass the test.
     LOG(WARNING) << "SystemMemoryPressureEvaluator is not available";
     available_mem_mb = 0;
   }
