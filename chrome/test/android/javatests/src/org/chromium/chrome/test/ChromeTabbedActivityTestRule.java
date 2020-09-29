6653fe7d52d376ed74d57acdafe9d38d96a33d05
Michael Thiessen <mthiesse@chromium.org>
2020-09-29 15:19:41 +0000

    Remove Feed setup from ChromeTabbedActivityTestRule
    The Feed setup isn't used by most tests and is required to happen once
    per process init, which make batching tests that use
    ChromeTabbedActivityTestRule more difficult.
    
    There's already a rule to do what CTATestRule was doing, so I've
    migrated the only test that seems to care about that setup to the
    FeedDataInjectRule.
    
    Bug: 989569
    Change-Id: I8142a3904276a294356789f246d977c93f6e48a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2437691
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811714}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeTabbedActivityTestRule.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeTabbedActivityTestRule.java
@@ -5,55 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.test;
 
-import org.junit.runner.Description;
-import org.junit.runners.model.Statement;
-
-import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
-import org.chromium.chrome.browser.feed.FeedProcessScopeFactory;
-import org.chromium.chrome.browser.feed.TestNetworkClient;
 
 /**
- * Custom ActivityTestRule for test using ChromeTabbedActivity
+ * Custom ActivityTestRule for tests using ChromeTabbedActivity
  */
 public class ChromeTabbedActivityTestRule extends ChromeActivityTestRule<ChromeTabbedActivity> {
-    // Response file for Feed's network stub.
-    private static final String DEFAULT_FEED_TEST_RESPONSE_FILE_PATH =
-            "/chrome/test/data/android/feed/feed_large.gcl.bin";
-
-    private final String mFeedTestResponseFilePath;
-
     public ChromeTabbedActivityTestRule() {
-        this(DEFAULT_FEED_TEST_RESPONSE_FILE_PATH);
-    }
-
-    /**
-     * @param feedTestResponseFilePath The file path of the response that the feed library returns
-     *                                 in tests.
-     */
-    public ChromeTabbedActivityTestRule(String feedTestResponseFilePath) {
         super(ChromeTabbedActivity.class);
-        mFeedTestResponseFilePath = feedTestResponseFilePath;
-    }
-
-    @Override
-    public Statement apply(final Statement base, Description description) {
-        Statement tabbedActivityStatement = new Statement() {
-            @Override
-            public void evaluate() throws Throwable {
-                // Setup Feed stubs.
-                TestNetworkClient client = new TestNetworkClient();
-                client.setNetworkResponseFile(
-                        UrlUtils.getIsolatedTestFilePath(mFeedTestResponseFilePath));
-                FeedProcessScopeFactory.setTestNetworkClient(client);
-
-                base.evaluate();
-
-                // Teardown the network stubs for Feed if they've been setup.
-                FeedProcessScopeFactory.setTestNetworkClient(null);
-            }
-        };
-
-        return super.apply(tabbedActivityStatement, description);
     }
 }
