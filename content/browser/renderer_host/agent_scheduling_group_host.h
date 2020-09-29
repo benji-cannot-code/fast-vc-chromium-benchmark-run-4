438fa117e994a4924baec0c27b0fe1e32a3cefd3
Tal Pressman <talp@chromium.org>
2020-09-29 14:07:52 +0000

    [MBI] Move the various Create* methods from Renderer to AgentSchedulingGroup.
    The AgentSchedulingGroup interface is (currently) associated with the
    Renderer interface, so message ordering is preserved. For now, the ASG
    simply calls the appropriate method implementations on RenderThreadImpl.
    
    Bug: 1111231
    Change-Id: I1dc1ffced95f6210894550d1864a93ef92b4b943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2409721
    Commit-Queue: Tal Pressman <talp@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811689}

--- content/browser/renderer_host/agent_scheduling_group_host.h
+++ content/browser/renderer_host/agent_scheduling_group_host.h
@@ -82,6 +82,14 @@ class CONTENT_EXPORT AgentSchedulingGroupHost
   void CreateFrame(mojom::CreateFrameParamsPtr params);
   void CreateView(mojom::CreateViewParamsPtr params);
   void DestroyView(int32_t routing_id);
+  void CreateFrameProxy(
+      int32_t routing_id,
+      int32_t render_view_routing_id,
+      const base::Optional<base::UnguessableToken>& opener_frame_token,
+      int32_t parent_routing_id,
+      const FrameReplicationState& replicated_state,
+      const base::UnguessableToken& frame_token,
+      const base::UnguessableToken& devtools_frame_token);
 
  private:
   // `MaybeAssociatedReceiver` and `MaybeAssociatedRemote` are temporary helper
@@ -137,6 +145,7 @@ class CONTENT_EXPORT AgentSchedulingGroupHost
 
     void reset();
     bool is_bound();
+    mojom::AgentSchedulingGroup* get();
 
    private:
     absl::variant<mojo::Remote<mojom::AgentSchedulingGroup>,
