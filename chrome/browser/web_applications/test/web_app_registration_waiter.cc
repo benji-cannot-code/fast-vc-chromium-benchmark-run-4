496075d2ee6238e4e433c48a3a1bfaf5fcaf6f9e
Alan Cutter <alancutter@chromium.org>
2020-09-29 20:45:27 +0000

    Add comments (and tweaks) to PendingAppManagerImplBrowserTest.AlreadyRegistered
    This CL adds clarifying comments to
    PendingAppManagerImplBrowserTest.AlreadyRegistered.
    
    In addition to comments this also adds
    WebAppRegistrationWaiter::AwaitNextNonFailedRegistration() to avoid
    having to use /web_apps/no_service_worker.html for the sake of asserting
    on a particular RegistrationResultCode. The normal install flow with
    /web_apps/basic.html is flakily either kSuccess or kAlreadyInstalled as
    the service worker may asynchronously register during the first install
    pass.
    
    Change-Id: I6e6aa14882424c3ccc2affa90afd2798cfaed897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2437549
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811824}

--- chrome/browser/web_applications/test/web_app_registration_waiter.cc
+++ chrome/browser/web_applications/test/web_app_registration_waiter.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/test/web_app_registration_waiter.h"
 
 #include "base/test/bind_test_util.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace web_app {
 
@@ -13,8 +14,11 @@ WebAppRegistrationWaiter::WebAppRegistrationWaiter(PendingAppManager* manager)
     : manager_(manager) {
   manager_->SetRegistrationCallbackForTesting(base::BindLambdaForTesting(
       [this](const GURL& install_url, RegistrationResultCode code) {
-        CHECK_EQ(install_url_, install_url);
-        CHECK_EQ(code_, code);
+        ASSERT_EQ(install_url_, install_url);
+        if (code_)
+          ASSERT_EQ(code_, code);
+        else
+          ASSERT_NE(code, RegistrationResultCode::kTimeout);
         run_loop_.Quit();
       }));
   manager_->SetRegistrationsCompleteCallbackForTesting(
@@ -30,6 +34,13 @@ void WebAppRegistrationWaiter::AwaitNextRegistration(
     RegistrationResultCode code) {
   install_url_ = install_url;
   code_ = code;
+  run_loop_.Run();
+}
+
+void WebAppRegistrationWaiter::AwaitNextNonFailedRegistration(
+    const GURL& install_url) {
+  install_url_ = install_url;
+  code_ = base::nullopt;
   run_loop_.Run();
 }
 
