29811f305ba75c69cc73ec11ba9500f3ad8ac29b
Boris Sazonov <bsazonov@chromium.org>
2020-09-29 23:59:25 +0000

    [Signin][Android] Rollback support for MobileIdentityConsistency
    Adds code to rollback MobileIdentityConsistency: when the feature is
    disabled, SigninManager will trigger a signout if there's only primary
    account, but no sync consent.
    
    Bug: 1132290
    Change-Id: Ia9f0143b908c1867743e0b89b7589ddbb03bff02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434330
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811883}

--- chrome/browser/android/signin/signin_manager_android.cc
+++ chrome/browser/android/signin/signin_manager_android.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/cloud/user_cloud_policy_manager.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/signin_pref_names.h"
+#include "components/signin/public/identity_manager/accounts_cookie_mutator.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browsing_data_filter_builder.h"
@@ -276,6 +277,13 @@ SigninManagerAndroid::GetManagementDomain(JNIEnv* env) {
   }
 
   return domain;
+}
+
+void SigninManagerAndroid::
+    LogOutAllAccountsForMobileIdentityConsistencyRollback(JNIEnv* env) {
+  identity_manager_->GetAccountsCookieMutator()->LogOutAllAccounts(
+      gaia::GaiaSource::kAccountReconcilorMirror,
+      signin::AccountsCookieMutator::LogOutFromCookieCompletedCallback());
 }
 
 void SigninManagerAndroid::WipeProfileData(
