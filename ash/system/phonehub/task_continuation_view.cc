ad0a9c8e60d5ac3bd12f189b9392d7c07c7efcea
Andre Le <leandre@chromium.org>
2020-09-30 18:18:46 +0000

    [CrOS PhoneHub] Adjust bubble size when browser tabs changed in task continuation.
    When browser tabs changed and the bubble is not closed, the bubble's
    size need to be re-adjusted in TaskContinuationView and
    PhoneConnectedView.
    
    BUG=1106937,1126208
    
    Change-Id: I110b8cd25ce45764b0072f99c6ec32e1bf00444f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438753
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812206}

--- ash/system/phonehub/task_continuation_view.cc
+++ ash/system/phonehub/task_continuation_view.cc
@@ -143,6 +143,8 @@ void TaskContinuationView::TaskChipsView::CalculateIdealBounds() {
 }
 
 void TaskContinuationView::Update() {
+  chips_view_->Reset();
+
   if (!phone_model_->browser_tabs_model()) {
     SetVisible(false);
     return;
@@ -157,12 +159,12 @@ void TaskContinuationView::Update() {
     return;
   }
 
-  chips_view_->Reset();
   for (const BrowserTabsModel::BrowserTabMetadata& metadata :
        browser_tabs.most_recent_tabs()) {
     chips_view_->AddTaskChip(new ContinueBrowsingChip(metadata));
   }
 
+  PreferredSizeChanged();
   SetVisible(true);
 }
 
