87969bf533abed07aa755c07f87c630574fa1e58
Michael Checo <michaelcheco@google.com>
2020-09-30 18:03:42 +0000

    Diagnostics: Update battery card markup
    Screenshots:
    - Diagnostics App (before) http://shortn/_F7krCJQK2W
    - Diagnostics App (after) http://shortn/_D86kuHAMy8
    
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I36218799b5f7574e54947dd894c92724ccc56c36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438710
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812185}

--- chrome/test/data/webui/chromeos/diagnostics/diagnostics_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/diagnostics_test.js
@@ -151,7 +151,7 @@ suite('BatteryStatusCardTest', () => {
           const dataPoints =
             diagnostics_test_utils.getDataPointElements(batteryStatusElement);
           assertEquals(
-              fakeBatteryInfo.manufacturer,
+              fakeBatteryChargeStatus[0].current_now_milliamps,
               dataPoints[0].value);
           assertEquals(
               fakeBatteryHealth[0].charge_full_design_milliamp_hours,
@@ -169,6 +169,7 @@ suite('BatteryStatusCardTest', () => {
           assertEquals(
               fakeBatteryChargeStatus[0].power_adapter_status,
               dataPoints[5].value);
+          assertEquals(fakeBatteryHealth[0].cycle_count, dataPoints[6].value);
         });
   });
 });
