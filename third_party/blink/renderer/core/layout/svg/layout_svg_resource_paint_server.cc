5b2f1002fb873add86c9a73de1ecb9ecada02755
Fredrik Söderqvist <fs@opera.com>
2020-09-30 14:04:17 +0000

    Move LayoutSVGResourcePaintServer::RequestPaintDescription to SVG DRT
    This functions is only used by the SVG layout tree dumper, so move it
    there as part of deconstructing SVGPaintServer and related classes.
    
    Make SVGPaintDescription strictly an implement detail now that it's not
    exposed.
    
    Bug: 109212
    Change-Id: I924603534154de84e0726cb4b9164ad558b0f157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2435191
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#811962}

--- third_party/blink/renderer/core/layout/svg/layout_svg_resource_paint_server.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_resource_paint_server.cc
@@ -33,6 +33,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+
+// If |SVGPaintDescription::has_fallback| is true, |SVGPaintDescription::color|
+// is set to a fallback color.
+struct SVGPaintDescription {
+  STACK_ALLOCATED();
+
+ public:
+  SVGPaintDescription() = default;
+  explicit SVGPaintDescription(Color color) : color(color), is_valid(true) {}
+  explicit SVGPaintDescription(LayoutSVGResourcePaintServer* resource)
+      : resource(resource), is_valid(true) {
+    DCHECK(resource);
+  }
+  SVGPaintDescription(LayoutSVGResourcePaintServer* resource,
+                      Color fallback_color)
+      : resource(resource),
+        color(fallback_color),
+        is_valid(true),
+        has_fallback(true) {
+    DCHECK(resource);
+  }
+
+  LayoutSVGResourcePaintServer* resource = nullptr;
+  Color color;
+  bool is_valid = false;
+  bool has_fallback = false;
+};
+
+}  // namespace
+
 SVGPaintServer::SVGPaintServer(Color color) : color_(color) {}
 
 SVGPaintServer::SVGPaintServer(scoped_refptr<Gradient> gradient,
@@ -154,12 +185,5 @@ LayoutSVGResourcePaintServer::LayoutSVGResourcePaintServer(SVGElement* element)
     : LayoutSVGResourceContainer(element) {}
 
 LayoutSVGResourcePaintServer::~LayoutSVGResourcePaintServer() = default;
-
-SVGPaintDescription LayoutSVGResourcePaintServer::RequestPaintDescription(
-    const LayoutObject& layout_object,
-    const ComputedStyle& style,
-    LayoutSVGResourceMode resource_mode) {
-  return RequestPaint(layout_object, style, resource_mode);
-}
 
 }  // namespace blink
