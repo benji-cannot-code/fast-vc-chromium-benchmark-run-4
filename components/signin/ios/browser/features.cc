1b8473486fe006b7c22dfb5a8560c04da53af1ac
Nohemi Fernandez <fernandex@chromium.org>
2020-09-30 20:35:57 +0000

    [iOS] Add testing option to change GAIA cookie refresh delay.
    Allows manual testing of the GAIA refresh feature by reducing the delay
    between polls to verify if GAIA cookies are available for a signed-in
    user.
    
    Bug: 1131027
    Change-Id: Ib163a91ec3e5b922d652cff68c60b4e08708a828
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440505
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812310}

--- components/signin/ios/browser/features.cc
+++ components/signin/ios/browser/features.cc
@@ -17,4 +17,7 @@ bool ForceStartupSigninPromo() {
 const base::Feature kRestoreGaiaCookiesIfDeleted{
     "RestoreGAIACookiesIfDeleted", base::FEATURE_DISABLED_BY_DEFAULT};
 
+const char kDelayThresholdMinutesToUpdateGaiaCookie[] =
+    "minutes-delay-to-restore-gaia-cookies-if-deleted";
+
 }  // namespace signin
