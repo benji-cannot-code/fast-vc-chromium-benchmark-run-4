553afe530963986b0828dee1b5b0839d7d509a5b
Taylor Brandstetter <deadbeef@chromium.org>
2020-09-30 14:15:24 +0000

    Add measurement for total data transferred in ten seconds.
    Note that this required making WebrtcPage inherit from PressStory.
    
    Bug: chromium:1033483
    Change-Id: I98972369b9349c06203da27c6bfc0f62294cb006
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2148277
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811983}

--- tools/perf/benchmarks/webrtc.py
+++ tools/perf/benchmarks/webrtc.py
@@ -3,12 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from core import perf_benchmark
-
 import page_sets
 from telemetry import benchmark
 from telemetry.timeline import chrome_trace_category_filter
 from telemetry.web_perf import timeline_based_measurement
+from benchmarks import press
 
 
 @benchmark.Info(emails=['qiangchen@chromium.org', # For smoothness metrics
@@ -16,7 +15,7 @@ from telemetry.web_perf import timeline_based_measurement
                         'phoglund@chromium.org'],
                 component='Blink>WebRTC',
                 documentation_url='http://bit.ly/webrtc-benchmark')
-class WebrtcPerfBenchmark(perf_benchmark.PerfBenchmark):
+class WebrtcPerfBenchmark(press._PressBenchmark):  # pylint: disable=protected-access
   """Base class for WebRTC metrics for real-time communications tests."""
   page_set = page_sets.WebrtcPageSet
 
