03662183763b1b942767d69bf87a9d19717ac493
Aaron Tagliaboschi <aarontag@chromium.org>
2020-09-30 16:45:01 +0000

    [client-hints] Test client hint availability in service workers
    Change-Id: Icbb9de8fa340ac216a05a1093a1840c7903ba53c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2432187
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812131}

---
+++ third_party/blink/web_tests/external/wpt/client-hints/service-workers/resources/echo-hint-in-html.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import sys
+
+def main(request, response):
+    """
+    Simple handler that sets a response header based on which client hint
+    request headers were received.
+    """
+
+    response.headers.append(b"Content-Type", b"text/html; charset=UTF-8")
+    response.headers.append(b"Access-Control-Allow-Origin", b"*")
+    response.headers.append(b"Access-Control-Allow-Headers", b"*")
+    response.headers.append(b"Access-Control-Expose-Headers", b"*")
+
+    response.headers.append(b"Cache-Control", b"no-store")
+
+    result = "FAIL"
+
+    if b"device-memory" in request.headers:
+      result = "PASS"
+
+    response.content = result
