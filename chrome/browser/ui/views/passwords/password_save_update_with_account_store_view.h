16f22223e97d0b2b307b7921a99384ae83609808
Mohamed Amir Yosef <mamir@chromium.org>
2020-09-30 17:27:42 +0000

    [Passwords] Announce when changing between password Save/Update
    This CL is announcing for screen readers when the state of the password
    Save bubble changes its state between Save and Update.
    
    TBR=vasilii@chromium.org
    
    Bug: 1099697
    Change-Id: Iba5e3de59e86b4605f82f507379ed600d0772233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438354
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812157}

--- chrome/browser/ui/views/passwords/password_save_update_with_account_store_view.h
+++ chrome/browser/ui/views/passwords/password_save_update_with_account_store_view.h
@@ -105,6 +105,10 @@ class PasswordSaveUpdateWithAccountStoreView
 
   void CloseIPHBubbleIfOpen();
 
+  // Announces to the screen readers a change in the bubble between Save and
+  // Update states.
+  void AnnounceSaveUpdateChange();
+
   // Used for both the username and password editable comboboxes.
   void OnContentChanged();
 
@@ -135,6 +139,10 @@ class PasswordSaveUpdateWithAccountStoreView
   // promo is open and get called back when it closes.
   ScopedObserver<views::Widget, views::WidgetObserver>
       observed_account_storage_promo_{this};
+
+  // Hidden view that will contain status text for immediate output by
+  // screen readers when the bubble changes state between Save and Update.
+  views::View* accessibility_alert_ = nullptr;
 
   // Used to add |username_dropdown_| as an observer to the
   // AnimatingLayoutManager. This is needed such that the |username_dropdown_|
