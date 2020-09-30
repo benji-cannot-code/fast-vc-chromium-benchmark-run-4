7cb037a6c9a89103c229c61ac7cfa7094d0411e8
Muyao Xu <muyaoxu@google.com>
2020-09-30 21:26:04 +0000

    [Cast+Zenith] Start cast session from the GMC dialog
    Users can start a cast session from the GMC dialog by clicking on the
    cast sinks in the device selector list.
    After a new session is started, the local media notification will be
    hidden, and the cast media notification will show up in the dialog.
    
    Bug: b/161610050, 1107162
    Change-Id: Ia3ad0634f0a45159ed92581ca2c6f097566c1a72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422737
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/master@{#812353}

--- chrome/browser/ui/media_router/media_router_ui_unittest.cc
+++ chrome/browser/ui/media_router/media_router_ui_unittest.cc
@@ -156,7 +156,7 @@ class MediaRouterViewsUITest : public ChromeRenderViewHostTestHarness {
 
     CreateSessionServiceTabHelper(web_contents());
     ui_ = std::make_unique<MediaRouterUI>(web_contents());
-    ui_->InitWithDefaultMediaSource();
+    ui_->InitWithDefaultMediaSourceAndMirroring();
   }
 
   void TearDown() override {
@@ -176,7 +176,7 @@ class MediaRouterViewsUITest : public ChromeRenderViewHostTestHarness {
         &web_contents()->GetController());
     CreateSessionServiceTabHelper(web_contents());
     ui_ = std::make_unique<MediaRouterUI>(web_contents());
-    ui_->InitWithDefaultMediaSource();
+    ui_->InitWithDefaultMediaSourceAndMirroring();
   }
 
   // These methods are used so that we don't have to friend each test case that
