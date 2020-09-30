563c5e2395d84b4016e75e0a618a119b91a87399
Siyu An <siyua@chromium.org>
2020-09-30 18:13:45 +0000

    [Autofill Offer] Complete merchant_domain related logic
    After discussion, we will stick to the GURL implementation for
    merchant domain.
    
    Therefore add missing logic to in offer data setter and validation
    checker regarding merchant domain
    
    Bug: 1112095
    Change-Id: I60d021c034e68646720414bb808db3eaa46f6296
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434466
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#812200}

--- components/autofill/core/browser/autofill_test_utils.cc
+++ components/autofill/core/browser/autofill_test_utils.cc
@@ -589,9 +589,7 @@ AutofillOfferData GetCardLinkedOfferData1() {
   // Sets the expiry to be 45 days later.
   data.expiry = AutofillClock::Now() + base::TimeDelta::FromDays(45);
   data.offer_details_url = GURL("http://www.example1.com");
-  data.merchant_domain = std::vector<GURL>();
-  data.merchant_domain.emplace_back(GURL("http://www.example1.com"));
-  data.eligible_instrument_id = std::vector<int64_t>();
+  data.merchant_domain.emplace_back("http://www.example1.com");
   data.eligible_instrument_id.emplace_back(111111);
   return data;
 }
@@ -603,9 +601,7 @@ AutofillOfferData GetCardLinkedOfferData2() {
   // Sets the expiry to be 40 days later.
   data.expiry = AutofillClock::Now() + base::TimeDelta::FromDays(40);
   data.offer_details_url = GURL("http://www.example2.com");
-  data.merchant_domain = std::vector<GURL>();
-  data.merchant_domain.emplace_back(GURL("http://www.example2.com"));
-  data.eligible_instrument_id = std::vector<int64_t>();
+  data.merchant_domain.emplace_back("http://www.example2.com");
   data.eligible_instrument_id.emplace_back(222222);
   return data;
 }
