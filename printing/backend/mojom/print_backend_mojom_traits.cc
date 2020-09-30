1e351a3afb8226ca77679e8641b036372f025859
Alan Screen <awscreen@chromium.org>
2020-09-30 23:17:15 +0000

    Introduce print backend mojom with Paper struct
    Adding support for PrintBackend queries to happen out of the browser
    process requires related data structures to be made available via a Mojo
    interface.  These are moved into their own mojom file separate from the
    common definition in printing/mojom since they are specific to a new
    process being introduced and do not need to be encumbered by the
    dependencies which exist for print.mojom.
    
    Starting with the historically most prolific cause of browser crashes
    from printing (GetPrinterSemanticCapsAndDefaults), work towards having
    support definitions to enable that query to be moved out-of-process.
    
    Start with a mojo definition for the
    printing::PrinterSemanticCapsAndDefaults::Paper structure, which is
    required for the larger query.
    
    Bug: 809738
    Change-Id: Id7d52f131f8587b9edc50c8e1c4f8b6718998965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2387409
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812424}

---
+++ printing/backend/mojom/print_backend_mojom_traits.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "printing/backend/mojom/print_backend_mojom_traits.h"
+#include "ui/gfx/geometry/mojom/geometry.mojom-shared.h"
+#include "ui/gfx/geometry/mojom/geometry_mojom_traits.h"
+
+namespace mojo {
+
+// static
+bool StructTraits<printing::mojom::PaperDataView,
+                  printing::PrinterSemanticCapsAndDefaults::Paper>::
+    Read(printing::mojom::PaperDataView data,
+         printing::PrinterSemanticCapsAndDefaults::Paper* out) {
+  return data.ReadDisplayName(&out->display_name) &&
+         data.ReadVendorId(&out->vendor_id) && data.ReadSizeUm(&out->size_um);
+}
+
+}  // namespace mojo
