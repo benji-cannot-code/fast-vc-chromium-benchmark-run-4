5ecd1b8f066207e10d045b53b415b0b4293d0fd0
Aaron Krajeski <aaronhk@chromium.org>
2020-09-30 16:37:10 +0000

    Add reset function to CanvasRenderingContext2D
    This functionresets everything (transformation stack, clip, styles,
    font, etc). If there are pending transforms in the stack they get
    discarded as well.
    
    In the interest of getting the API ready for demoing I'm rushing
    through these a bit.
    
    TODO: Add WPT tests (crbug.com/1131449)
    
    Bug: 1131137
    Change-Id: If98217fe4de804352d279aa46ab128e3bf6ae53d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2425067
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812128}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -139,7 +139,7 @@ void BaseRenderingContext2D::UnwindStateStack() {
   }
 }
 
-void BaseRenderingContext2D::Reset() {
+void BaseRenderingContext2D::reset() {
   ValidateStateStack();
   UnwindStateStack();
   state_stack_.resize(1);
