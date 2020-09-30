58204f88d8bf2756c3fce179f5604780f57cc5af
Xing Liu <xingliu@chromium.org>
2020-09-30 05:48:28 +0000

    Read later: Implement an empty ReadingListManager.
    The empty implementation will be used when the read later feature is
    disabled. So we won't hit the actual backend.
    
    TBR=dtrainor@chromium.org
    
    Bug: 1128074
    Change-Id: I5c2bed4b2136d758a6c504eabf6a77547650f69c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438484
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811935}

--- chrome/browser/android/reading_list/reading_list_manager_factory.cc
+++ chrome/browser/android/reading_list/reading_list_manager_factory.cc
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profiles/profile_key.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/reading_list/android/empty_reading_list_manager.h"
 #include "chrome/browser/reading_list/android/reading_list_manager.h"
 #include "chrome/browser/ui/read_later/reading_list_model_factory.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "components/keyed_service/core/simple_dependency_manager.h"
 
 // static
@@ -36,6 +38,9 @@ ReadingListManagerFactory::~ReadingListManagerFactory() = default;
 std::unique_ptr<KeyedService>
 ReadingListManagerFactory::BuildServiceInstanceFor(
     SimpleFactoryKey* key) const {
+  if (!base::FeatureList::IsEnabled(features::kReadLater))
+    return std::make_unique<EmptyReadingListManager>();
+
   auto* profile = ProfileManager::GetProfileFromProfileKey(
       ProfileKey::FromSimpleFactoryKey(key));
   auto* reading_list_model =
---
+++ chrome/browser/reading_list/android/empty_reading_list_manager.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/reading_list/android/empty_reading_list_manager.h"
+
+EmptyReadingListManager::EmptyReadingListManager() = default;
+
+EmptyReadingListManager::~EmptyReadingListManager() = default;
+
+const bookmarks::BookmarkNode* EmptyReadingListManager::Add(
+    const GURL& url,
+    const std::string& title) {
+  return nullptr;
+}
+
+const bookmarks::BookmarkNode* EmptyReadingListManager::Get(const GURL& url) {
+  return nullptr;
+}
+
+void EmptyReadingListManager::Delete(const GURL& url) {}
+
+const bookmarks::BookmarkNode* EmptyReadingListManager::GetRoot() const {
+  return nullptr;
+}
+
+size_t EmptyReadingListManager::size() const {
+  return 0u;
+}
+
+size_t EmptyReadingListManager::unread_size() const {
+  return 0u;
+}
+
+void EmptyReadingListManager::SetReadStatus(const GURL& url, bool read) {}
