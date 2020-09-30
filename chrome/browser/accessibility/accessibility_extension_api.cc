ec6dd20fe9fad07d44c9d48ad8a5eeb8e4ea5a40
Josiah K <josiahk@google.com>
2020-09-30 18:40:37 +0000

    Create new accessibilityPrivate method to update magnifier viewport
    Parent Tracking Bug: 1131153
    
    Fixed: 1129711
    AX-Relnotes: Add accessibilityPrivate method to update magnifier viewport and focus following highlight
    Change-Id: Id5fa2c05e39f763b7a599e3445f40945a71f5e42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417457
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/master@{#812228}

--- chrome/browser/accessibility/accessibility_extension_api.cc
+++ chrome/browser/accessibility/accessibility_extension_api.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
 #include "build/build_config.h"
+#include "chrome/browser/chromeos/accessibility/magnification_manager.h"
 #include "chrome/browser/extensions/api/tabs/tabs_constants.h"
 #include "chrome/browser/extensions/chrome_extension_function_details.h"
 #include "chrome/browser/extensions/extension_service.h"
@@ -407,6 +408,22 @@ AccessibilityPrivateHandleScrollableBoundsForPointFoundFunction::Run() {
   gfx::Rect bounds(rect.left, rect.top, rect.width, rect.height);
   ash::AccessibilityController::Get()->HandleAutoclickScrollableBoundsFound(
       bounds);
+  return RespondNow(NoArguments());
+}
+
+ExtensionFunction::ResponseAction
+AccessibilityPrivateMoveMagnifierToRectFunction::Run() {
+  std::unique_ptr<accessibility_private::MoveMagnifierToRect::Params> params =
+      accessibility_private::MoveMagnifierToRect::Params::Create(*args_);
+  EXTENSION_FUNCTION_VALIDATE(params);
+  accessibility_private::ScreenRect rect = std::move(params->rect);
+  gfx::Rect bounds(rect.left, rect.top, rect.width, rect.height);
+
+  chromeos::MagnificationManager* magnification_manager =
+      chromeos::MagnificationManager::Get();
+  if (magnification_manager)
+    magnification_manager->HandleMoveMagnifierToRectIfEnabled(bounds);
+
   return RespondNow(NoArguments());
 }
 
