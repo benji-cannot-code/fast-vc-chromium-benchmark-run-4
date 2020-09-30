2d7c7703e75748281613edcf32b01eb0ead7ad65
Derek Basehore <dbasehore@chromium.org>
2020-09-30 06:09:53 +0000

    Disable unmount-on-suspend for FUSE
    This removes the code for unmounting the FUSEs (filesystem in userspace)
    on suspend. Instead of unmounting to resolve the FUSE/freeze on suspend
    race condition, we will be using powerd code that orders freeze using
    freezer cgroups.
    
    Crostini still has some work left to do before this can be done for it,
    so unmount on suspend for its FUSE, sshfs, will be done in a later
    patch.
    
    Test: "suspend_stress_test -c 2500" on device
    Bug: b/134792837
    Change-Id: Icb8f9e309f6780e6f935be550c6c4e048a44b1fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2336869
    Commit-Queue: Derek Basehore <dbasehore@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Sergei Datsenko <dats@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/master@{#811950}

--- chrome/browser/chromeos/smb_client/smb_service.h
+++ chrome/browser/chromeos/smb_client/smb_service.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/smb_client/smb_task_queue.h"
 #include "chrome/browser/chromeos/smb_client/smbfs_share.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chromeos/dbus/power/power_manager_client.h"
 #include "chromeos/dbus/smb_provider_client.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "net/base/network_change_notifier.h"
@@ -50,7 +49,6 @@ class SmbShareInfo;
 // Creates and manages an smb file system.
 class SmbService : public KeyedService,
                    public net::NetworkChangeNotifier::NetworkChangeObserver,
-                   public chromeos::PowerManagerClient::Observer,
                    public base::SupportsWeakPtr<SmbService> {
  public:
   using MountResponse = base::OnceCallback<void(SmbMountResult result)>;
@@ -127,10 +125,6 @@ class SmbService : public KeyedService,
   void SetSmbFsMounterCreationCallbackForTesting(
       SmbFsShare::MounterCreationCallback callback);
 
-  // chromeos::PowerManagerClient::Observer overrides
-  void SuspendImminent(power_manager::SuspendImminent::Reason reason) override;
-  void SuspendDone(const base::TimeDelta& sleep_duration) override;
-
  private:
   friend class SmbServiceTest;
 
@@ -170,11 +164,6 @@ class SmbService : public KeyedService,
   void OnSmbfsMountDone(const std::string& smbfs_mount_id,
                         MountInternalCallback callback,
                         SmbMountResult result);
-
-  // Callback passed to SmbFsShare::Unmount() during a power management
-  // suspension. Ensures that suspension is blocked until the unmount completes.
-  void OnSuspendUnmountDone(base::UnguessableToken power_manager_suspend_token,
-                            chromeos::MountError result);
 
   // Retrieves the mount_id for |file_system_info|.
   int32_t GetMountId(const ProvidedFileSystemInfo& info) const;
