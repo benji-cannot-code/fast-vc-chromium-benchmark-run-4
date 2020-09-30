5b1cb2ef24ae3e48270b80f08b7840039b6d0332
Andre Le <leandre@chromium.org>
2020-09-30 21:13:55 +0000

    [CrOS PhoneHub] Hook up quick actions UI to controllers. (Reland)
    This is a reland CL for crrev/c/2419552, which ran into a compilation failure and merge conflict when reland.
    
    In this CL:
    - Create a base class for quick actions controller.
    - Create silence phone controller that connect to the DND controller in PhoneHubManager. Display the icons according to the states.
    - Hook up the quick actions UI in the bubble to silence phone controller. Enable hotspot and locate phone implementation will be added in the next CL.
    
    BUG=1106937,1126208
    
    Change-Id: Ie8540bdc29de8b73297dcf5c3f0a0d963f76d63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438590
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812338}

---
+++ ash/system/phonehub/quick_action_controller_base.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_PHONEHUB_QUICK_ACTION_CONTROLLER_BASE_H_
+#define ASH_SYSTEM_PHONEHUB_QUICK_ACTION_CONTROLLER_BASE_H_
+
+#include "ash/ash_export.h"
+#include "ash/system/phonehub/quick_action_item.h"
+
+namespace ash {
+
+class QuickActionItem;
+
+// Base class for controllers of quick action item.
+// To add a new quick action item, implement this class, and add to the list in
+// QuickActionsView::InitQuickActionItems().
+class ASH_EXPORT QuickActionControllerBase : public QuickActionItem::Delegate {
+ public:
+  virtual ~QuickActionControllerBase() = default;
+
+  // Create the view. Subclasses instantiate QuickActionItem.
+  // The view will be owned by views hierarchy. The view will be always deleted
+  // after the controller is destructed.
+  virtual QuickActionItem* CreateItem() = 0;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_PHONEHUB_QUICK_ACTION_CONTROLLER_BASE_H_
