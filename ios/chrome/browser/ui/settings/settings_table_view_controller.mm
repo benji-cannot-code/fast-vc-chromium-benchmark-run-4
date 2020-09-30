d0a0f9345a1bbd8fa16f8869ebe5281123bb5133
Chris Lu <thegreenfrog@chromium.org>
2020-09-30 17:00:34 +0000

    [ios] Create DefaultBrowserSettingsTableViewController
    Instead of a button in the Settings page that will take the
    user to the Chrome Panel in Settings.app to set the default browser,
    the row will open a page with the same button and the steps
    to make Chrome the default browser.
    
    Screenshot:
    https://drive.google.com/file/d/1_CEPv563hGMi_Ol4KQsrcNu3V4XE5egy/view
    
    Bug: 1131251
    Change-Id: I7561d3f40d0b786c85a1a6caf46c53df33457002
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2425118
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812141}

--- ios/chrome/browser/ui/settings/settings_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_table_view_controller.mm
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_cell.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_item.h"
 #import "ios/chrome/browser/ui/settings/content_settings_table_view_controller.h"
+#import "ios/chrome/browser/ui/settings/default_browser/default_browser_settings_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/elements/enterprise_info_popover_view_controller.h"
 #import "ios/chrome/browser/ui/settings/google_services/accounts_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/google_services/google_services_settings_coordinator.h"
@@ -131,6 +132,7 @@ NSString* const kSettingsAboutChromeImageName = @"settings_about_chrome";
 NSString* const kSettingsDebugImageName = @"settings_debug";
 NSString* const kSettingsArticleSuggestionsImageName =
     @"settings_article_suggestions";
+NSString* const kDefaultBrowserWorldImageName = @"default_browser_world";
 
 typedef NS_ENUM(NSInteger, SectionIdentifier) {
   SectionIdentifierSignIn = kSectionIdentifierEnumZero,
@@ -139,7 +141,7 @@ typedef NS_ENUM(NSInteger, SectionIdentifier) {
   SectionIdentifierAdvanced,
   SectionIdentifierInfo,
   SectionIdentifierDebug,
-  SectionIdentifierDefaultBrowser,
+  SectionIdentifierDefaults,
 };
 
 typedef NS_ENUM(NSInteger, ItemType) {
@@ -434,11 +436,12 @@ NSString* kDevViewSourceKey = @"DevViewSource";
   [model addItem:[self googleServicesCellItem]
       toSectionWithIdentifier:SectionIdentifierAccount];
 
+  // Defaults section.
   if (@available(iOS 14, *)) {
     if (base::FeatureList::IsEnabled(kDefaultBrowserSettings)) {
-      [model addSectionWithIdentifier:SectionIdentifierDefaultBrowser];
+      [model addSectionWithIdentifier:SectionIdentifierDefaults];
       [model addItem:[self defaultBrowserCellItem]
-          toSectionWithIdentifier:SectionIdentifierDefaultBrowser];
+          toSectionWithIdentifier:SectionIdentifierDefaults];
     }
   }
 
@@ -450,8 +453,18 @@ NSString* kDevViewSourceKey = @"DevViewSource";
     [model addItem:[self managedSearchEngineItem]
         toSectionWithIdentifier:SectionIdentifierBasics];
   } else {
-    [model addItem:[self searchEngineDetailItem]
-        toSectionWithIdentifier:SectionIdentifierBasics];
+    if (@available(iOS 14, *)) {
+      if (base::FeatureList::IsEnabled(kDefaultBrowserSettings)) {
+        [model addItem:[self searchEngineDetailItem]
+            toSectionWithIdentifier:SectionIdentifierDefaults];
+      } else {
+        [model addItem:[self searchEngineDetailItem]
+            toSectionWithIdentifier:SectionIdentifierBasics];
+      }
+    } else {
+      [model addItem:[self searchEngineDetailItem]
+          toSectionWithIdentifier:SectionIdentifierBasics];
+    }
   }
   [model addItem:[self passwordsDetailItem]
       toSectionWithIdentifier:SectionIdentifierBasics];
@@ -550,12 +563,12 @@ NSString* kDevViewSourceKey = @"DevViewSource";
 }
 
 - (TableViewItem*)defaultBrowserCellItem {
-  TableViewTextItem* defaultBrowser =
-      [[TableViewTextItem alloc] initWithType:ItemTypeDefaultBrowser];
+  TableViewDetailIconItem* defaultBrowser =
+      [[TableViewDetailIconItem alloc] initWithType:ItemTypeDefaultBrowser];
+  defaultBrowser.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
   defaultBrowser.text =
       l10n_util::GetNSString(IDS_IOS_SETTINGS_SET_DEFAULT_BROWSER);
-  defaultBrowser.accessibilityTraits |= UIAccessibilityTraitButton;
-  defaultBrowser.textColor = [UIColor colorNamed:kBlueColor];
+  defaultBrowser.iconImageName = kDefaultBrowserWorldImageName;
 
   return defaultBrowser;
 }
@@ -949,13 +962,9 @@ NSString* kDevViewSourceKey = @"DevViewSource";
       [self showSyncGoogleService];
       break;
     case ItemTypeDefaultBrowser:
-      [tableView deselectRowAtIndexPath:indexPath animated:YES];
-      base::RecordAction(base::UserMetricsAction("Settings.DefaultBrowser"));
-      [[UIApplication sharedApplication]
-                    openURL:
-                        [NSURL URLWithString:UIApplicationOpenSettingsURLString]
-                    options:{}
-          completionHandler:nil];
+      base::RecordAction(
+          base::UserMetricsAction("Settings.ShowDefaultBrowser"));
+      controller = [[DefaultBrowserSettingsTableViewController alloc] init];
       break;
     case ItemTypeSearchEngine:
       base::RecordAction(base::UserMetricsAction("EditSearchEngines"));
