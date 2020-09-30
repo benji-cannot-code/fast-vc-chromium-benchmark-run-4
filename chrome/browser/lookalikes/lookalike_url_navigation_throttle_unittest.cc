a686f7c626b4d53c93a9920c220f459bde163319
Joe DeBlasio <jdeblasio@chromium.org>
2020-09-30 17:46:27 +0000

    [Lookalikes] Change how the lookalike interstitial handles redirects
    This CL changes what domains we consider for the lookalike interstitial.
    Previously, if Chrome saw a navigation that looked like
        A -> B -> C -> D
    (where site A is redirecting to B, etc.), the lookalike logic would
    inspect each stage, A through D, for lookalikes. It accomplished this by
    implementing DidRedirectNavigation() and DidFinishNavigation(). This
    added a bunch of complexity, but allowed us to stop a navigation
    mid-redirect.
    
    This CL changes that behavior to wait until the entire redirect chain
    has completed, and then inspect only sites A and D. These domains are
    the only ones visible to the user, and thus the only ones wherein a
    lookalike URL might be effective. This change also means that we no
    longer implement DidRedirectNavigation().
    
    Bug: 1092690
    Change-Id: I2cfa724b9793b60dd5200d1bf6847cc6919fa469
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438888
    Reviewed-by: Livvie Lin <livvielin@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812173}

--- chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
@@ -17,74 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace lookalikes {
 
-// These redirects are safe:
-// - http[s]://sité.test -> http[s]://site.test
-// - http[s]://sité.test/path -> http[s]://site.test
-// - http[s]://subdomain.sité.test -> http[s]://site.test
-// - http[s]://random.test -> http[s]://sité.test -> http[s]://site.test
-// - http://sité.test/path -> https://sité.test/path -> https://site.test ->
-// <any_url>
-// - "subdomain" on either side.
-//
-// These are not safe:
-// - http[s]://[subdomain.]sité.test -> http[s]://[subdomain.]site.test/path
-// because the redirected URL has a path.
-TEST(LookalikeUrlNavigationThrottleTest, IsSafeRedirect) {
-  EXPECT_TRUE(IsSafeRedirect(
-      "example.com", {GURL("http://éxample.com"), GURL("http://example.com")}));
-  EXPECT_TRUE(IsSafeRedirect(
-      "example.com", {GURL("http://éxample.com"), GURL("http://example.com")}));
-  EXPECT_TRUE(IsSafeRedirect(
-      "example.com",
-      {GURL("http://éxample.com"), GURL("http://subdomain.example.com")}));
-  EXPECT_TRUE(IsSafeRedirect(
-      "example.com", {GURL("http://éxample.com"), GURL("http://example.com"),
-                      GURL("https://example.com")}));
-  // Original site redirects to HTTPS.
-  EXPECT_TRUE(IsSafeRedirect(
-      "example.com", {GURL("http://éxample.com"), GURL("https://éxample.com"),
-                      GURL("https://example.com")}));
-  // Original site redirects to HTTPS which redirects to HTTP which redirects
-  // back to HTTPS of the non-IDN version.
-  EXPECT_TRUE(IsSafeRedirect(
-      "example.com",
-      {GURL("http://éxample.com/redir1"), GURL("https://éxample.com/redir1"),
-       GURL("http://éxample.com/redir2"), GURL("https://example.com/")}));
-  // Same as above, but there is another redirect at the end of the chain.
-  EXPECT_TRUE(IsSafeRedirect(
-      "example.com",
-      {GURL("http://éxample.com/redir1"), GURL("https://éxample.com/redir1"),
-       GURL("http://éxample.com/redir2"), GURL("https://example.com/"),
-       GURL("https://totallydifferentsite.com/somepath")}));
-
-  // Not a redirect, the chain is too short.
-  EXPECT_FALSE(IsSafeRedirect("example.com", {GURL("http://éxample.com")}));
-  // Not safe: Redirected site is not the same as the matched site.
-  EXPECT_FALSE(IsSafeRedirect("example.com", {GURL("http://éxample.com"),
-                                              GURL("http://other-site.com")}));
-  // Not safe: Initial URL doesn't redirect to the root of the suggested domain.
-  EXPECT_FALSE(IsSafeRedirect(
-      "example.com",
-      {GURL("http://éxample.com"), GURL("http://example.com/path")}));
-  // Not safe: The first redirect away from éxample.com is not to the matching
-  // non-IDN site.
-  EXPECT_FALSE(IsSafeRedirect("example.com", {GURL("http://éxample.com"),
-                                              GURL("http://intermediate.com"),
-                                              GURL("http://example.com")}));
-
-  // Not safe: The redirect stays unsafe from éxample.com to éxample.com.
-  EXPECT_FALSE(IsSafeRedirect(
-      "example.com", {GURL("http://éxample.com"), GURL("http://éxample.com")}));
-  // Not safe: Same, but to a path on the bad domain
-  EXPECT_FALSE(IsSafeRedirect(
-      "example.com",
-      {GURL("http://éxample.com"), GURL("http://éxample.com/path")}));
-  // Not safe: Same, but with an intermediary domain.
-  EXPECT_FALSE(IsSafeRedirect("example.com", {GURL("http://éxample.com/path"),
-                                              GURL("http://intermediate.com/p"),
-                                              GURL("http://éxample.com/dir")}));
-}
-
 class LookalikeThrottleTest : public ChromeRenderViewHostTestHarness {};
 
 // Tests that spoofy hostnames are properly handled in the throttle.
@@ -147,6 +79,7 @@ TEST_F(LookalikeThrottleTest, SpoofsBlocked) {
 
     GURL url(std::string("http://") + test_case.hostname);
     content::MockNavigationHandle handle(url, main_rfh());
+    handle.set_redirect_chain({url});
     handle.set_page_transition(ui::PAGE_TRANSITION_TYPED);
 
     auto throttle =
