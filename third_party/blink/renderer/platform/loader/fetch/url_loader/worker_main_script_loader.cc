78f18cdcea8b0dae34467f77399b66dd85157783
Minggang Wang <minggang.wang@intel.com>
2020-09-30 03:57:20 +0000

    Fix WorkerMainScriptLoaderTest.ResponseWithFailureThenOnComplete failure
    Currently, when running the unit test
    WorkerMainScriptLoaderTest.ResponseWithFailureThenOnComplete locally,
    the result reports all tests passed:
    
    [1/1] WorkerMainScriptLoaderTest.ResponseWithFailureThenOnComplete (12 ms)
    SUCCESS: all tests passed.
    
    but actually, there is an error reported:
    
    Actual function call count doesn't match
    EXPECT_CALL(*mock_observer, DidReceiveResponse(_, _, _, _, _))
    Expected: to be called once
    Actual: never called - unsatisfied and active
    
    The root cause is that we must execute a forced GC in order to finalize
    objects depending on the mock object before the test finished, and
    the test begins to fail consistently after executing a GC in the dtor.
    Meanwhile, we should call ResourceLoadObserver's DidReceiveResponsei()
    method before stopping loading the main script by WorkerMainScriptLoader.
    
    The WorkerMainScriptLoader class is designed to load the main script
    for workers, which is pre-requested in browser process and passed to
    renderer process through WorkerMainScriptLoadParams, and when the http
    status code indicates a failure, we should notify the
    |resource_load_observer_| by invoking its DidReceiveResponse() method,
    thus the devtools could be aware of this response.
    
    This patch implements that behavior to make the test runs successfully.
    
    Change-Id: I03f062276abad2253b35ebfa4ed8b4f58eddab4f
    Bug: 1132634
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2433631
    Commit-Queue: Minggang Wang <minggang.wang@intel.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#811929}

--- third_party/blink/renderer/platform/loader/fetch/url_loader/worker_main_script_loader.cc
+++ third_party/blink/renderer/platform/loader/fetch/url_loader/worker_main_script_loader.cc
@@ -73,6 +73,11 @@ void WorkerMainScriptLoader::Start(
   resource_response_ = response.ToResourceResponse();
   NotifyResponseReceived(std::move(response_head));
 
+  resource_load_observer_->DidReceiveResponse(
+      initial_request_.InspectorId(), initial_request_, resource_response_,
+      /*resource=*/nullptr,
+      ResourceLoadObserver::ResponseSource::kNotFromMemoryCache);
+
   if (resource_response_.IsHTTP() &&
       !cors::IsOkStatus(resource_response_.HttpStatusCode())) {
     client_->OnFailedLoadingWorkerMainScript();
@@ -86,10 +91,6 @@ void WorkerMainScriptLoader::Start(
     return;
   }
 
-  resource_load_observer_->DidReceiveResponse(
-      initial_request_.InspectorId(), initial_request_, resource_response_,
-      /*resource=*/nullptr,
-      ResourceLoadObserver::ResponseSource::kNotFromMemoryCache);
   script_encoding_ =
       resource_response_.TextEncodingName().IsEmpty()
           ? UTF8Encoding()
