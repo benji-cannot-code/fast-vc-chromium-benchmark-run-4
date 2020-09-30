718e060d81a07fce15e46bf72bf00db3eca17d59
Tomasz Wiszkowski <ender@google.com>
2020-09-30 21:13:51 +0000

    Create an basic Omnibox Carousel suggestion.
    This change introduces an abstract CarouselSuggestion item that
    features title and configurable RecyclerView component.
    The content of the RecyclerView component (items) can be
    easily configured to display any type of elements.
    
    Change-Id: Ibdf9505c919c1f47a9cd1af14d538ff2bcbeacb3
    Doc: http://doc/1IFsDKYhy_ARMOdT8jd1pMtk497xe03lb3hVnsaY-dOM
    Slides: http://slides/1c1SzJLzEsTjLmqIVLTomC8l9EgslBgYaBN6I7t8mbDI#slide=id.g8b3736bab3_0_0
    Figma: https://www.figma.com/file/h8qjD0s9tezRzHdCMxIhYJ/On-Focus-Suggestions?node-id=290%3A309
    Bug: 1106109
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427724
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812337}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/carousel/BaseCarouselSuggestionViewBinder.java
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.omnibox.suggestions.carousel;
+
+import org.chromium.ui.modelutil.MVCListAdapter.ListItem;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.modelutil.SimpleRecyclerViewAdapter;
+
+import java.util.Collection;
+
+/**
+ * Binder for the Carousel suggestions.
+ */
+public final class BaseCarouselSuggestionViewBinder {
+    /** @see PropertyModelChangeProcessor.ViewBinder#bind(Object, Object, Object) */
+    public static void bind(PropertyModel model, BaseCarouselSuggestionView view, PropertyKey key) {
+        if (key == BaseCarouselSuggestionViewProperties.TILES) {
+            final Collection<ListItem> items =
+                    model.get(BaseCarouselSuggestionViewProperties.TILES);
+            final SimpleRecyclerViewAdapter adapter = view.getAdapter();
+            if (items != null) {
+                adapter.getModelList().set(items);
+            } else {
+                adapter.getModelList().clear();
+            }
+        } else if (key == BaseCarouselSuggestionViewProperties.TITLE) {
+            view.getHeaderTextView().setText(model.get(BaseCarouselSuggestionViewProperties.TITLE));
+        }
+    }
+}
--- ui/android/java/src/org/chromium/ui/modelutil/SimpleRecyclerViewAdapter.java
+++ ui/android/java/src/org/chromium/ui/modelutil/SimpleRecyclerViewAdapter.java
@@ -101,6 +101,11 @@ public class SimpleRecyclerViewAdapter
         mListData.removeObserver(mListObserver);
     }
 
+    /** @return The ModelList describing RecyclerView children. */
+    public ModelList getModelList() {
+        return mListData;
+    }
+
     @Override
     public int getItemCount() {
         return mListData.size();
