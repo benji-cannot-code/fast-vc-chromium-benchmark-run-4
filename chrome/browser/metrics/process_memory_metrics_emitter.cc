49569bced59d9ec3a3e1426e51dc049cc693f767
Egor Pasko <pasko@chromium.org>
2020-09-30 14:29:30 +0000

    Rename histogram MappedAndResidentMemoryFootprint2
    The device coverage where this histogram is recorded successfully has
    significantly increased. To avoid bias from previous Chrome versions,
    rename this histogram.
    
    Also fix a typo.
    
    Bug: 1070618
    Change-Id: Id7d4cc1b49d90122a75b4c6bd2e69093b53e162e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2435186
    Reviewed-by: ssid <ssid@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812008}

--- chrome/browser/metrics/process_memory_metrics_emitter.cc
+++ chrome/browser/metrics/process_memory_metrics_emitter.cc
@@ -930,7 +930,7 @@ void ProcessMemoryMetricsEmitter::CollateResults() {
       // Size of the native library on android is ~40MB.
       // More precision is needed in the middle buckets, hence the range.
       base::UmaHistogramCustomCounts(
-          "Memory.NativeLibrary.MappedAndResidentMemoryFootprint2",
+          "Memory.NativeLibrary.MappedAndResidentMemoryFootprint3",
           native_resident_kb, 1000, 100000, 100);
       if (native_library_not_resident_ordered_kb != metrics.kInvalid) {
         base::UmaHistogramCustomCounts(
