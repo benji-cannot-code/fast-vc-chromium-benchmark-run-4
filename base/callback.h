7a66178487afd89e69815827ee4b85cfed531812
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-09-30 15:15:40 +0000

    [base] Add ResultType to base::Callback
    This change adds a public ResultType alias to base::Callback, specifying
    the return type of `Run()`. This is analogous to std::function's nested
    result_type.
    
    Bug: 554299
    Change-Id: Ie478345b68051b03cd5b064b0833d528d47990b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2436536
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812087}

--- base/callback.h
+++ base/callback.h
@@ -58,6 +58,7 @@ namespace base {
 template <typename R, typename... Args>
 class OnceCallback<R(Args...)> : public internal::CallbackBase {
  public:
+  using ResultType = R;
   using RunType = R(Args...);
   using PolymorphicInvoke = R (*)(internal::BindStateBase*,
                                   internal::PassingType<Args>...);
@@ -104,6 +105,7 @@ class OnceCallback<R(Args...)> : public internal::CallbackBase {
 template <typename R, typename... Args>
 class RepeatingCallback<R(Args...)> : public internal::CallbackBaseCopyable {
  public:
+  using ResultType = R;
   using RunType = R(Args...);
   using PolymorphicInvoke = R (*)(internal::BindStateBase*,
                                   internal::PassingType<Args>...);
