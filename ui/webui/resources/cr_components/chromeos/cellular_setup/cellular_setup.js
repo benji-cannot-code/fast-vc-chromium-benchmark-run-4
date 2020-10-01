47465e80ba926ef147327115484f23e73d9401e6
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-10-01 23:54:06 +0000

    [CrOS cellular] Add carrier title to provisioning page
    Requirements:
       - https://docs.google.com/presentation/d/1Mfk5ZMEmeHwSbadUbyRU0Si5k6XDIvw-j4u7TUaCIGY/edit#slide=id.g7dc3c1fe01_5_125
    Screenshot:
       - https://screenshot.googleplex.com/7S9tRak4yYCiP2a.png
    
    Bug: 1093185
    Change-Id: I6f71cbf42be9780ea1f5341c31c9b36a2219583f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2441271
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812997}

--- ui/webui/resources/cr_components/chromeos/cellular_setup/cellular_setup.js
+++ ui/webui/resources/cr_components/chromeos/cellular_setup/cellular_setup.js
@@ -16,6 +16,11 @@ Polymer({
     /** @type {!cellular_setup.CellularSetupDelegate} */
     delegate: Object,
 
+    flowTitle: {
+      type: String,
+      notify: true,
+    },
+
     /**
      * Name of the currently displayed sub-page.
      * @private {!cellularSetup.CellularSetupPageName|null}
@@ -66,6 +71,7 @@ Polymer({
   /** @private */
   onPageChange_() {
     if (this.currentPage_) {
+      this.flowTitle = '';
       this.currentPage_.initSubflow();
     }
   },
