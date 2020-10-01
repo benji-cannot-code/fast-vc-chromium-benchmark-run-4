e34a306934e5aee7a97227140a528bc8db760ed6
manukh <manukh@chromium.org>
2020-10-01 17:33:45 +0000

    [omnibox] [bookmark-paths] Add no-op feature, params, & flag.
    Currently, bookmark suggestions are limited to title & URL matches. E.g
    the input 'planets jupiter' won't suggest a bookmark titled 'jupiter' in
    a directory 'planets'.
    
    This is the 1st of 3 CLs implementing bookmark-paths which will allow
    inputs to match bookmark paths (without contributing to the bookmark
    suggestion's score).
    
    This CL creates 4 params that will support 5 planned UI variations:
    1) Title - URL (current behaviour)
    2) Title - Path
    3) Title : Path - URL
    4) Path/Title - URL
    5) Title - Path|URL (either path or URL depending on which matched the
    input; favor path if both or neither matched the input)
    
    Bug: 1129524
    Change-Id: Iba575d8fc7db2fcb8ec9bd9b8c384dac83eaba1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2416931
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812784}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1195,6 +1195,43 @@ const FeatureEntry::FeatureVariation
         },
 };
 
+const FeatureEntry::FeatureVariation kOmniboxBookmarkPathsVariations[] = {
+    {
+        "Default UI (Title - URL)",
+        (FeatureEntry::FeatureParam[]){},
+        0,
+        nullptr,
+    },
+    {
+        "Replace title (Path/Title - URL)",
+        (FeatureEntry::FeatureParam[]){
+            {"OmniboxBookmarkPathsUiReplaceTitle", "true"}},
+        1,
+        nullptr,
+    },
+    {
+        "Replace URL (Title - Path)",
+        (FeatureEntry::FeatureParam[]){
+            {"OmniboxBookmarkPathsUiReplaceUrl", "true"}},
+        1,
+        nullptr,
+    },
+    {
+        "Append after title (Title : Path - URL)",
+        (FeatureEntry::FeatureParam[]){
+            {"OmniboxBookmarkPathsUiAppendAfterTitle", "true"}},
+        1,
+        nullptr,
+    },
+    {
+        "Dynamic Replace URL (Title - Path|URL)",
+        (FeatureEntry::FeatureParam[]){
+            {"OmniboxBookmarkPathsUiDynamicReplaceUrl", "true"}},
+        1,
+        nullptr,
+    },
+};
+
 #endif  // defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_MAC) ||
         // defined(OS_WIN)
 
@@ -3986,6 +4023,11 @@ const FeatureEntry kFeatureEntries[] = {
          omnibox::kRichAutocompletion,
          kOmniboxRichAutocompletionPromisingVariations,
          "OmniboxBundledExperimentV1")},
+    {"omnibox-bookmark-paths", flag_descriptions::kOmniboxBookmarkPathsName,
+     flag_descriptions::kOmniboxBookmarkPathsDescription, kOsDesktop,
+     FEATURE_WITH_PARAMS_VALUE_TYPE(omnibox::kBookmarkPaths,
+                                    kOmniboxBookmarkPathsVariations,
+                                    "OmniboxBundledExperimentV1")},
 #endif  // defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_MAC) ||
         // defined(OS_WIN)
 
