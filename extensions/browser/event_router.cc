6c09b2e2075e1505a8cbdfeb29c086cbf5ac03f2
Istiaque Ahmed <lazyboy@chromium.org>
2020-10-01 07:46:20 +0000

    [Extensions Service Worker] Add histogram for event dispatch.
    This CL adds the histogram
    Extensions.Events.DispatchWithServiceWorkerBackground when an extension
    event is dispatched to the background service worker of an extension.
    
    Bug: None
    Change-Id: I6a119dfb4d39b518201c8cf166e99a2f4595dac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2441949
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812576}

--- extensions/browser/event_router.cc
+++ extensions/browser/event_router.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/atomic_sequence_num.h"
 #include "base/bind.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/task/post_task.h"
@@ -832,6 +833,10 @@ void EventRouter::ReportEvent(events::HistogramValue histogram_value,
           "Extensions.Events.DispatchWithRunningEventPage", histogram_value,
           events::ENUM_BOUNDARY);
     }
+  } else if (BackgroundInfo::IsServiceWorkerBased(extension)) {
+    base::UmaHistogramEnumeration(
+        "Extensions.Events.DispatchWithServiceWorkerBackground",
+        histogram_value, events::ENUM_BOUNDARY);
   }
 }
 
