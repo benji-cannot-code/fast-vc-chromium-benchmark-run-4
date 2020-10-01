911d2a58ee62ef53857d14c393fd23e363995291
Nela Kaczmarek <nelakaczmarek@google.com>
2020-10-01 13:20:08 +0000

    Add HashAffiliationFetcher skeleton to retrieve affiliations by hash prefix.
    This change creates HashAffiliationFetcher skeleton that extends AffiliationFetcherBase.
    This fetcher will provide additional privacy layer as it will request affiliations for a larger group of facets by passing only the prefixes of hashed URLs.
    
    Bug: 1108279
    Change-Id: I4a2b09aa7d8c3c51f4b30ba966a17ba5afb9b09b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2423883
    Commit-Queue: Nela Kaczmarek <nelakaczmarek@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812653}

---
+++ components/password_manager/core/browser/site_affiliation/hash_affiliation_fetcher_unittest.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/core/browser/site_affiliation/hash_affiliation_fetcher.h"
+
+#include "components/password_manager/core/browser/android_affiliation/mock_affiliation_fetcher_delegate.h"
+#include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
+#include "services/network/test/test_url_loader_factory.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+namespace password_manager {
+
+class HashAffiliationFetcherTest : public testing::Test {
+ public:
+  HashAffiliationFetcherTest() = default;
+  ~HashAffiliationFetcherTest() override = default;
+};
+
+TEST_F(HashAffiliationFetcherTest, BuildQueryURL) {
+  network::TestURLLoaderFactory test_url_loader_factory;
+  scoped_refptr<network::SharedURLLoaderFactory> test_shared_loader_factory =
+      base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
+          &test_url_loader_factory);
+  MockAffiliationFetcherDelegate mock_delegate;
+
+  HashAffiliationFetcher fetcher(test_shared_loader_factory, &mock_delegate);
+
+  EXPECT_EQ(GURL("https://www.googleapis.com/affiliation/v1/"
+                 "affiliation:lookupByHashPrefix?key=dummytoken"),
+            fetcher.BuildQueryURL());
+}
+
+}  // namespace password_manager
