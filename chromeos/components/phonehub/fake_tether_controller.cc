9a3fc222d5ac299f0725d992ea3b4da697003084
Karan Bhatia <karandeepb@chromium.org>
2020-10-01 23:15:41 +0000

    Revert "[CrOS PhoneHub] Add enable hotspot and locate phone controller to quick"
    This reverts commit 2e1031ec577b81781b3a6cd7a9badce2ca300171.
    
    Reason for revert: Causes compile failure on linux-chromeos-dbg.
    
    Original change's description:
    > [CrOS PhoneHub] Add enable hotspot and locate phone controller to quick
    > actions UI.
    >
    > - Add enable hotspot and locate phone quick actions controllers.
    > - Connect the controllers to quick actions UI.
    > - Minor changes in fake tether controller for testing.
    >
    > BUG=1106937,1126208
    >
    > Change-Id: I1d4ace03120a4c51fcb351b7c19f33252b3fb81c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2428735
    > Commit-Queue: Andre Le <leandre@chromium.org>
    > Reviewed-by: Tim Song <tengs@chromium.org>
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#812915}
    
    TBR=khorimoto@chromium.org,tengs@chromium.org,leandre@chromium.org
    
    Change-Id: I431b965f354a2d63bd98e361b8ae3233ba5288e7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1106937
    Bug: 1126208
    Bug: 1134378
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2444132
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812986}

--- chromeos/components/phonehub/fake_tether_controller.cc
+++ chromeos/components/phonehub/fake_tether_controller.cc
@@ -8,8 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace phonehub {
 
-FakeTetherController::FakeTetherController()
-    : status_(Status::kConnectionAvailable) {}
+FakeTetherController::FakeTetherController() = default;
 
 FakeTetherController::~FakeTetherController() = default;
 
