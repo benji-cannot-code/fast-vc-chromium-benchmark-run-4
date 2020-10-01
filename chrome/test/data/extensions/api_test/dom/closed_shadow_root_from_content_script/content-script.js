e326d88e2b557cdb4acf8c341582b75630b7dcb5
Miyoung Shin <myid.shin@igalia.com>
2020-10-01 07:47:41 +0000

    Reland "Allow access to closed shadow root from Extension"
    This is a reland of fe6e27d68a44c714dcce906749390378ede7d423
    
    Old ExtensionApiTest.OpenOrClosedShadowRootInBackgroundPage
    navigated to unnecessary empty.html and timeout error occurs
    as flaky, but TC already runs the background page successfully,
    so this CL rewrites the test case using
    ExtensionApiTest::RunExtensionTest simply to fix a flaky issue.
    
    Original change's description:
    > Allow access to closed shadow root from Extension
    >
    > This CL introduces chrome.dom.openOrClosedShadowRoot that is a
    > read-only property to allow access to the closed shadow root in
    > the Extension's content script.
    >
    > Bug: 778816
    > Change-Id: I933bc5d3996a74f46ee75181c535150901a5a46a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2319951
    > Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Mason Freed <masonfreed@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#809277}
    
    TBR=peria@chromium.org,yukishiino@chromium.org,tkent@chromium.org,karandeepb@chromium.org,mkwst@chromium.org,masonfreed@chromium.org
    
    Bug: 778816
    Change-Id: I3bb7289cbc0c7c8a1c970000d87458f937acd4b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2428528
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812578}

---
+++ chrome/test/data/extensions/api_test/dom/closed_shadow_root_from_content_script/content-script.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([function() {
+  const host = document.getElementById('host');
+  let shadowRoot = chrome.dom.openOrClosedShadowRoot(host);
+  chrome.test.assertEq(shadowRoot.childElementCount, 2);
+  chrome.test.assertThrows(
+      chrome.dom.openOrClosedShadowRoot, [new Object()],
+      'Error in invocation of dom.openOrClosedShadowRoot(HTMLElement ' +
+          'element): ');
+  chrome.test.succeed();
+}]);
