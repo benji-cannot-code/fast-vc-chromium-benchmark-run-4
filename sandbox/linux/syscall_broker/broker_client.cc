8ee6832a387f965e44f73be03afeeee1e932f1a0
Matthew Denton <mpdenton@chromium.org>
2020-10-01 00:32:01 +0000

    Linux sandbox: BrokerSimpleMessage support for send/recv multiple fds
    For USER_NOTIF support in the syscall broker, the broker will need to
    send two fds to the CLONE_FILES process (temporary IPC socket for reply,
    and the newly opened FD). The current BrokerSimpleMessage only supports
    sending and receiving a single FD, so this CL adds support for
    async-signal-safe sending/receiving of multiple FDs.
    
    Bug: 1117351
    Change-Id: I708f200b047857816a9c2958535a360811e25741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412761
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812478}

--- sandbox/linux/syscall_broker/broker_client.cc
+++ sandbox/linux/syscall_broker/broker_client.cc
@@ -94,7 +94,7 @@ int BrokerClient::Readlink(const char* path, char* buf, size_t bufsize) const {
   RAW_CHECK(message.AddIntToMessage(COMMAND_READLINK));
   RAW_CHECK(message.AddStringToMessage(path));
 
-  int returned_fd = -1;
+  base::ScopedFD returned_fd;
   BrokerSimpleMessage reply;
   ssize_t msg_len =
       message.SendRecvMsgWithFlags(ipc_channel_.get(), 0, &returned_fd, &reply);
@@ -138,7 +138,7 @@ int BrokerClient::Rename(const char* oldpath, const char* newpath) const {
   RAW_CHECK(message.AddStringToMessage(oldpath));
   RAW_CHECK(message.AddStringToMessage(newpath));
 
-  int returned_fd = -1;
+  base::ScopedFD returned_fd;
   BrokerSimpleMessage reply;
   ssize_t msg_len =
       message.SendRecvMsgWithFlags(ipc_channel_.get(), 0, &returned_fd, &reply);
@@ -213,7 +213,7 @@ int BrokerClient::PathOnlySyscall(BrokerCommand syscall_type,
   RAW_CHECK(message.AddIntToMessage(syscall_type));
   RAW_CHECK(message.AddStringToMessage(pathname));
 
-  int returned_fd = -1;
+  base::ScopedFD returned_fd;
   BrokerSimpleMessage reply;
   ssize_t msg_len =
       message.SendRecvMsgWithFlags(ipc_channel_.get(), 0, &returned_fd, &reply);
@@ -240,7 +240,7 @@ int BrokerClient::PathAndFlagsSyscall(BrokerCommand syscall_type,
   RAW_CHECK(message.AddStringToMessage(pathname));
   RAW_CHECK(message.AddIntToMessage(flags));
 
-  int returned_fd = -1;
+  base::ScopedFD returned_fd;
   BrokerSimpleMessage reply;
   ssize_t msg_len =
       message.SendRecvMsgWithFlags(ipc_channel_.get(), 0, &returned_fd, &reply);
@@ -281,7 +281,7 @@ int BrokerClient::PathAndFlagsSyscallReturningFD(BrokerCommand syscall_type,
   RAW_CHECK(message.AddStringToMessage(pathname));
   RAW_CHECK(message.AddIntToMessage(flags));
 
-  int returned_fd = -1;
+  base::ScopedFD returned_fd;
   BrokerSimpleMessage reply;
   ssize_t msg_len = message.SendRecvMsgWithFlags(
       ipc_channel_.get(), recvmsg_flags, &returned_fd, &reply);
@@ -296,8 +296,8 @@ int BrokerClient::PathAndFlagsSyscallReturningFD(BrokerCommand syscall_type,
     return return_value;
 
   // We have a real file descriptor to return.
-  RAW_CHECK(returned_fd >= 0);
-  return returned_fd;
+  RAW_CHECK(returned_fd.is_valid());
+  return returned_fd.release();
 }
 
 // Make a remote system call over IPC for syscalls that take a path
@@ -314,7 +314,7 @@ int BrokerClient::StatFamilySyscall(BrokerCommand syscall_type,
   RAW_CHECK(message.AddStringToMessage(pathname));
   RAW_CHECK(message.AddIntToMessage(static_cast<int>(follow_links)));
 
-  int returned_fd = -1;
+  base::ScopedFD returned_fd;
   BrokerSimpleMessage reply;
   ssize_t msg_len =
       message.SendRecvMsgWithFlags(ipc_channel_.get(), 0, &returned_fd, &reply);
