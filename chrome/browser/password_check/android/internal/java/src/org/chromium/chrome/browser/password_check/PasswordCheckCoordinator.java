3504c6271d6b9a069fd1caff1ca157453310aa52
Maxim Kolosovskiy <kolos@chromium.org>
2020-10-01 10:20:11 +0000

    [Passwords] Report |DID_NOTHING| in automatic change metrics
    Bug: 1132230
    Change-Id: Ibae4da2522b4df619ad0848217cf3382b35c81f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438454
    Commit-Queue: Maxim Kolosovskiy  <kolos@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812622}

--- chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckCoordinator.java
+++ chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckCoordinator.java
@@ -104,6 +104,7 @@ class PasswordCheckCoordinator implements PasswordCheckComponentUi, LifecycleObs
 
     @Override
     public void onResumeFragment() {
+        mMediator.onResumeFragment();
         mReauthenticationHelper.onReauthenticationMaybeHappened();
     }
 
@@ -111,6 +112,8 @@ class PasswordCheckCoordinator implements PasswordCheckComponentUi, LifecycleObs
     public void onDestroyFragment() {
         mMediator.stopCheck();
         if (mFragmentView.getActivity() == null || mFragmentView.getActivity().isFinishing()) {
+            mMediator
+                    .onUserLeavesCheckPage(); // Should be called only if the activity is finishing.
             mMediator.destroy();
             mModel = null;
         }
