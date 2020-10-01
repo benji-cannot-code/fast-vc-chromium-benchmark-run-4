2501a3f5d72d9f058c04a773e27166983c984201
Dan Harrington <harringtond@chromium.org>
2020-10-01 22:55:13 +0000

    Fix crash on NTP
    The problem was that we were handing the same view to RecyclerView
    multiple times. We already wrap views in parent views before handing
    them off to the recycler view. The fix is to always wrap the child
    in a new parent.
    
    Bug: 1131975
    Change-Id: I6f1a92e3d8201d4395d9c5208d8289e961f450b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2436420
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812978}

--- chrome/android/junit/src/org/chromium/chrome/browser/feed/v2/FeedListContentManagerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/feed/v2/FeedListContentManagerTest.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.feed.v2;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -216,12 +217,12 @@ public class FeedListContentManagerTest implements ListContentManagerObserver {
         ViewParent p = v.getParent();
 
         // Remove and re-add the same view, but with a new NativeViewContent.
-        // This time, getNativeView() does not need to create the enclosing parent view.
+        // This time, getNativeView() creates a new enclosing parent view.
         removeContents(0, 1);
         c = createNativeViewContent(v);
         addContents(0, ImmutableList.of(c));
         assertEquals(v, getNativeView(mManager.getViewType(0)));
-        assertEquals(p, v.getParent());
+        assertNotEquals(p, v.getParent());
     }
 
     @Test
