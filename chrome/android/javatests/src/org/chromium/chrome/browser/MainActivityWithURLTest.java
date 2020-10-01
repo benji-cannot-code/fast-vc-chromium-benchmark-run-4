715915984fcf551f03ac597de1147c10fe6b6038
Ben Joyce <bjoyce@google.com>
2020-10-01 00:00:42 +0000

    Batch MainActivityWithURLTest
    Reduces time from 14123ms to 4339ms
    
    Bug: 989569
    Change-Id: I12318738a72d6c17b0cc41de360a550e860392df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438633
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: benjamin joyce <bjoyce@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812456}

--- chrome/android/javatests/src/org/chromium/chrome/browser/MainActivityWithURLTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/MainActivityWithURLTest.java
@@ -14,6 +14,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -28,6 +29,7 @@ import org.chromium.net.test.EmbeddedTestServer;
  * Tests starting the activity with URLs.
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
+@Batch(Batch.PER_CLASS)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 public class MainActivityWithURLTest {
     @Rule
