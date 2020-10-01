f586d12968769634937dd069270c92a7b1a8742b
Tomasz Tylenda <ttylenda@chromium.org>
2020-10-01 01:58:50 +0000

    Revert "Migrate base::Callback to base::OnceCallback."
    This reverts commit a0282608035344add1595bd22bf1ca3de98d4483.
    
    Reason for revert: b/169804713 
    
    Original change's description:
    > Migrate base::Callback to base::OnceCallback.
    >
    > BUG=764795
    > TEST=build
    >
    > Change-Id: I8afa2fd87eb5cf5d9a8f7ecde9fad95d420c5399
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434405
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#811980}
    
    TBR=hidehiko@chromium.org,ttylenda@chromium.org
    
    Change-Id: I7de1e3dac102f4c760aef90da690016b020baa3f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 764795
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440088
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812516}

--- chrome/browser/chromeos/arc/policy/arc_android_management_checker.cc
+++ chrome/browser/chromeos/arc/policy/arc_android_management_checker.cc
@@ -71,7 +71,7 @@ void ArcAndroidManagementChecker::StartClient() {
   GetDeviceManagementService()->ScheduleInitialization(0);
 }
 
-void ArcAndroidManagementChecker::StartCheck(CheckCallback callback) {
+void ArcAndroidManagementChecker::StartCheck(const CheckCallback& callback) {
   DCHECK(callback_.is_null());
 
   // Do not send requests for Chrome OS managed users, nor for well-known
@@ -79,11 +79,11 @@ void ArcAndroidManagementChecker::StartCheck(CheckCallback callback) {
   if (policy_util::IsAccountManaged(profile_) ||
       policy::BrowserPolicyConnector::IsNonEnterpriseUser(
           profile_->GetProfileUserName())) {
-    std::move(callback).Run(policy::AndroidManagementClient::Result::UNMANAGED);
+    callback.Run(policy::AndroidManagementClient::Result::UNMANAGED);
     return;
   }
 
-  callback_ = std::move(callback);
+  callback_ = callback;
   EnsureRefreshTokenLoaded();
 }
 
