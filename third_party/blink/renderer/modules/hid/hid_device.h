d67010590ba98a8b8cd35dd807da406e8e76a884
Francois Beaufort <beaufort.francois@gmail.com>
2020-10-01 11:00:35 +0000

    [WebHID] Change some interfaces to dictionaries
    This CL changes HIDCollectionInfo, HIDReportInfo, and HIDReportItem
    interfaces to dictionaries for easier logging with JSON and to avoid
    polluting the window web namespace.
    
    Spec: https://github.com/WICG/webhid/issues/22
    Test: https://webhid-collections.glitch.me/
    
    Change-Id: Ife8d089505312bba9f42e5d8fb1c466f44bd8e45
    Bug: 890096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2398541
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#812626}

--- third_party/blink/renderer/modules/hid/hid_device.h
+++ third_party/blink/renderer/modules/hid/hid_device.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/hid/hid.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/array_buffer_or_array_buffer_view.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_hid_report_item.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
@@ -70,6 +71,9 @@ class MODULES_EXPORT HIDDevice
 
   // ExecutionContextLifecycleObserver:
   void ContextDestroyed() override;
+
+  static HIDReportItem* ToHIDReportItem(
+      const device::mojom::blink::HidReportItem& report_item);
 
   void Trace(Visitor*) const override;
 
--- third_party/blink/web_tests/external/wpt/webhid/idlharness.https.window.js
+++ third_party/blink/web_tests/external/wpt/webhid/idlharness.https.window.js
@@ -15,9 +15,6 @@ idl_test(
       Navigator: ['navigator'],
       // TODO: HIDConnectionEvent
       // TODO: HIDInputReportEvent
-      // TODO: HIDReportItem
-      // TODO: HIDReportInfo
-      // TODO: HIDCollectionInfo
       // TODO: HIDDevice
     });
   }
