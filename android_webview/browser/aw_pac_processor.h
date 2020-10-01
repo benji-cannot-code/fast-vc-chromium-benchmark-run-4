023704aed9179d0766698bbc4e7b17aa771bba85
Anna Malova <amalova@chromium.org>
2020-10-01 01:01:11 +0000

    [aw] Allow to set Network in AwPacProcessor.
    Bug: 1085115
    Change-Id: I8846261a0f21ecb48692b70ab778f68996843760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414173
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Anna Malova <amalova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812494}

--- android_webview/browser/aw_pac_processor.h
+++ android_webview/browser/aw_pac_processor.h
@@ -23,7 +23,7 @@ class HostResolver;
 
 class AwPacProcessor {
  public:
-  AwPacProcessor(net_handle_t net_handle);
+  AwPacProcessor();
   AwPacProcessor(const AwPacProcessor&) = delete;
   AwPacProcessor& operator=(const AwPacProcessor&) = delete;
 
@@ -40,8 +40,9 @@ class AwPacProcessor {
       const base::android::JavaParamRef<jobject>& obj,
       const base::android::JavaParamRef<jstring>& jurl);
   std::string MakeProxyRequest(std::string url);
-  void SetNetworkLinkAddresses(
+  void SetNetworkAndLinkAddresses(
       JNIEnv* env,
+      net_handle_t net_handle,
       const base::android::JavaParamRef<jobjectArray>& addresses);
 
  private:
@@ -64,7 +65,6 @@ class AwPacProcessor {
   std::unique_ptr<HostResolver> host_resolver_;
 
   std::set<Job*> jobs_;
-  net_handle_t net_handle_;
 };
 }  // namespace android_webview
 
