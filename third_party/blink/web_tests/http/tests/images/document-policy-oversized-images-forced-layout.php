a84c0f845ad8d56b093709cbff0d7834454931c6
Charlie Hu <chenleihu@google.com>
2020-10-01 01:17:58 +0000

    [Document Policy] Move test content to main frame for image policy tests
    Image policies used to be feature poilcy and the only way to
    specify feature policy is by specifying 'allow' attribute on iframe.
    
    The tests were set up with subframes holding the test content while
    the main frame specifies the policy.
    
    With image policies migrated to document policy which uses HTTP header
    to specify policy, there is no reason to split the test content to
    subframe right now.
    
    This CL moves test content to main frame for image policy tests.
    
    Change-Id: Idf95c84d27fafbdb5fa926fefacfbcf1b913b7c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414561
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812502}

---
+++ third_party/blink/web_tests/http/tests/images/document-policy-oversized-images-forced-layout.php
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("Document-Policy: oversized-images=2.0");
+?>
+<!DOCTYPE html>
+
+<head>
+  <script>
+    function loaded() {
+      if (window.testRunner) {
+        testRunner.waitUntilDone();
+        testRunner.updateAllLifecyclePhasesAndCompositeThen(() => testRunner.notifyDone());
+      }
+    }
+  </script>
+  <style>
+    body {
+      margin: 0;
+    }
+  </style>
+</head>
+
+<body onload=loaded()>
+  <img src="resources/green-256x256.jpg" width="100" height="100">
+  <img src="resources/green-256x256.jpg" style="width: 100px; height: 100px">
+  <script>
+    document.body.offsetTop;
+  </script>
+</body>
