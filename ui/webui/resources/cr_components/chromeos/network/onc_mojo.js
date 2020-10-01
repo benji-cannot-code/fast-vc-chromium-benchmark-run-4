55b6909d37088dc6884710c2b6bc0ecba5aab48a
Azeem Arshad <azeemarshad@chromium.org>
2020-10-01 21:05:50 +0000

    [CrOS Cellular] Store custom APN in pref storage.
    This CL addresses issue with "Other" APN values getting
    erased when changing APN options. This is fixed by saving
    APN value that are not in the APN db to the pref store.
    This list is made available in cros_network_config mojo
    api as a new custom_apn_list property.
    
    This also fixes issue with "Other" apn text fields getting
    overwritten because of managed property changes.
    
    Bug: b/167297672
    Change-Id: I0ffb497be6d274eba99835f56c79e2d4a3004639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2435978
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812907}

--- ui/webui/resources/cr_components/chromeos/network/onc_mojo.js
+++ ui/webui/resources/cr_components/chromeos/network/onc_mojo.js
@@ -1104,6 +1104,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return a.lockType === b.lockType && a.lockEnabled === b.lockEnabled &&
         a.retriesLeft === b.retriesLeft;
   }
+
+  /**
+   * Returns true if the APN properties match.
+   * @param {chromeos.networkConfig.mojom.ApnProperties} a
+   * @param {chromeos.networkConfig.mojom.ApnProperties} b
+   * @return {boolean}
+   */
+  static apnMatch(a, b) {
+    if (!a || !b) {
+      return !!a === !!b;
+    }
+    return a.accessPointName === b.accessPointName &&
+           a.name === b.name && a.username === b.username &&
+           a.password === b.password;
+  }
+
+  /**
+   * Returns true if the APN List matches.
+   * @param {Array<!chromeos.networkConfig.mojom.ApnProperties>|undefined} a
+   * @param {Array<!chromeos.networkConfig.mojom.ApnProperties>|undefined} b
+   * @return {boolean}
+   */
+  static apnListMatch(a, b) {
+    if (!a || !b) {
+      return !!a === !!b;
+    }
+    if (a.length !== b.length) {
+      return false;
+    }
+    return a.every((apn, index) => OncMojo.apnMatch(apn, b[index]));
+  }
 }
 
 /** @typedef {chromeos.networkConfig.mojom.DeviceStateProperties} */
