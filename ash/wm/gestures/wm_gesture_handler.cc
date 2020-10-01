ea6b801f877b385fde8817b4fa13e144a8279470
chinsenj <chinsenj@chromium.org>
2020-10-01 17:47:39 +0000

    cros: Remove log in wm gesture handler.
    Remove LOG(ERROR) is ash/wm/gestures/wm_gesture_handler.cc
    
    Change-Id: I53135342ea8e060c802a7ee34cf2af96bb436d82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2442301
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Jeremy Chinsen <chinsenj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812799}

--- ash/wm/gestures/wm_gesture_handler.cc
+++ ash/wm/gestures/wm_gesture_handler.cc
@@ -252,7 +252,6 @@ bool WmGestureHandler::ProcessScrollEvent(const ui::ScrollEvent& event) {
 bool WmGestureHandler::ProcessEventImpl(int finger_count,
                                         float delta_x,
                                         float delta_y) {
-  LOG(ERROR) << "Scroll: " << delta_x;
   if (!scroll_data_)
     return false;
 
