208e3d55a83d0ef3c1cf7d1cbd6af0ce137ffbba
Joon Ahn <joonbug@chromium.org>
2020-10-01 15:30:10 +0000

    Diagnostics: Wire percent chart to battery card
    https://screenshot.googleplex.com/4HWAicLVp6pnhNp
    
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I5f53c37dd5e99631438b736eed8211214292d70b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2436953
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812711}

--- chromeos/components/diagnostics_ui/resources/battery_status_card.js
+++ chromeos/components/diagnostics_ui/resources/battery_status_card.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './data_point.js';
 import './diagnostics_card.js';
 import './diagnostics_shared_css.js';
+import './percent_bar_chart.js';
 
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {BatteryChargeStatus, BatteryHealth, BatteryInfo, SystemDataProviderInterface} from './diagnostics_types.js'
