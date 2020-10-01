7d91af2d0e08479ab92fb44b8f906a2b44c3ce01
Piotr Tworek <ptworek@vewd.com>
2020-10-01 09:19:03 +0000

    Fix partition_alloc build issues with libstdc++.
    Two minor and pretty generic issues are exposed when building the code
    with libstdc++ instead of bundled libcxx.
    
    1. Code in thread_cache.h uses std::atomic but fails to include required
       header file.
    2. Code in partition_alloc_constants.h uses std::max_align_t which is
       defined in cstddef, not its C version stddef.h.
    
    In both cases libcxx basically pulls in required headers indirectly.
    
    Bug: 957519
    Change-Id: Idcee3e7abfab94cb40e1b95c636b97cf9c766b75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440436
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#812598}

--- base/allocator/partition_allocator/partition_alloc_constants.h
+++ base/allocator/partition_allocator/partition_alloc_constants.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_PARTITION_ALLOC_CONSTANTS_H_
 
 #include <limits.h>
-#include <stddef.h>
+#include <cstddef>
 
 #include <algorithm>
 
--- base/allocator/partition_allocator/thread_cache.h
+++ base/allocator/partition_allocator/thread_cache.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_ALLOCATOR_PARTITION_ALLOCATOR_THREAD_CACHE_H_
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_THREAD_CACHE_H_
 
+#include <atomic>
 #include <cstdint>
 #include <memory>
 
