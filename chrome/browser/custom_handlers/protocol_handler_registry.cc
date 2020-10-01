0a0271ff3e9c7e32f072ed269bdb250f36e765a0
Andrew Shulaev <ddrone@google.com>
2020-10-01 12:48:27 +0000

    Replace Bind with BindOnce
    Context: https://docs.google.com/document/d/1J4N3V5UmzZL9BZGNrn95E4xJu4cz3ynbg21zXR-J_qA/edit#
    
    Bug: 1007635
    Change-Id: I62505df729c87f425b4d5589d32e83874ef12368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440635
    Auto-Submit: Andrew Shulaev <ddrone@google.com>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812648}

--- chrome/browser/custom_handlers/protocol_handler_registry.cc
+++ chrome/browser/custom_handlers/protocol_handler_registry.cc
@@ -789,7 +789,7 @@ void ProtocolHandlerRegistry::AddPredefinedHandler(
 shell_integration::DefaultWebClientWorkerCallback
 ProtocolHandlerRegistry::GetDefaultWebClientCallback(
     const std::string& protocol) {
-  return base::Bind(
+  return base::BindOnce(
       &ProtocolHandlerRegistry::OnSetAsDefaultProtocolClientFinished,
       weak_ptr_factory_.GetWeakPtr(), protocol);
 }
