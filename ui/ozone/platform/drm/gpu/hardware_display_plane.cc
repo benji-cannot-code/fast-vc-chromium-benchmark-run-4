028a036c90095f40da0530dbab0584c5593122a8
Gil Dekel <gildekel@chromium.org>
2020-10-01 18:18:03 +0000

    Ozone/DRM: Remove Plane Type kDummy
    As of today, all ChromeOS releases utilize kernels that robustly support
    universal planes.
    
    This makes the need to accommodate for non-supporting code paths
    obsolete.
    
    This CL removes this code.
    
    Bug: 1129546
    Change-Id: I2df722646cb85b3b4cdf04354941824e72aa15e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2413407
    Commit-Queue: Gil Dekel <gildekel@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#812813}

--- ui/ozone/platform/drm/gpu/hardware_display_plane.cc
+++ ui/ozone/platform/drm/gpu/hardware_display_plane.cc
@@ -13,35 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/drm/gpu/drm_device.h"
 #include "ui/ozone/platform/drm/gpu/drm_gpu_util.h"
 
-#ifndef DRM_PLANE_TYPE_OVERLAY
-#define DRM_PLANE_TYPE_OVERLAY 0
-#endif
-
-#ifndef DRM_PLANE_TYPE_PRIMARY
-#define DRM_PLANE_TYPE_PRIMARY 1
-#endif
-
-#ifndef DRM_PLANE_TYPE_CURSOR
-#define DRM_PLANE_TYPE_CURSOR 2
-#endif
-
 namespace ui {
 
 namespace {
-
-HardwareDisplayPlane::Type GetPlaneType(int value) {
-  switch (value) {
-    case DRM_PLANE_TYPE_CURSOR:
-      return HardwareDisplayPlane::kCursor;
-    case DRM_PLANE_TYPE_PRIMARY:
-      return HardwareDisplayPlane::kPrimary;
-    case DRM_PLANE_TYPE_OVERLAY:
-      return HardwareDisplayPlane::kOverlay;
-    default:
-      NOTREACHED();
-      return HardwareDisplayPlane::kDummy;
-  }
-}
 
 void ParseSupportedFormatsAndModifiers(
     drmModePropertyBlobPtr blob,
@@ -92,7 +66,7 @@ bool HardwareDisplayPlane::Initialize(DrmDevice* drm) {
   }
 
   if (properties_.type.id)
-    type_ = GetPlaneType(properties_.type.value);
+    type_ = properties_.type.value;
 
   if (properties_.plane_color_encoding.id) {
     color_encoding_bt601_ =
