5c97d150c902dd35156da59b6bf42cc4dfad6a0a
Jeroen Dhollander <jeroendh@google.com>
2020-10-02 18:53:42 +0000

    Apply menu boundary changes to QuickAnswer views
    Previously the menu boundary changes were not applied if the quick
    answer views (QuickAnswerView or NotificationView) were still pending,
    causing the quick answer view to show up too low.
    
    Bug: b/168936614
    Tests: ash_unittests --gtest_filter="QuickAnswer*" and manually deployed
    Change-Id: I1f81cbed4d9c5e8e718d556e2763ec9727c5fe36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2439538
    Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Jeroen Dhollander <jeroendh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813298}

--- ash/quick_answers/quick_answers_controller_impl.cc
+++ ash/quick_answers/quick_answers_controller_impl.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
-// TODO(yanxiao):Add a unit test for QuickAnswersControllerImpl.
 namespace {
 using chromeos::quick_answers::Context;
 using chromeos::quick_answers::IntentType;
@@ -212,8 +211,6 @@ void QuickAnswersControllerImpl::OnQuickAnswerClick() {
 
 void QuickAnswersControllerImpl::UpdateQuickAnswersAnchorBounds(
     const gfx::Rect& anchor_bounds) {
-  if (visibility_ != QuickAnswersVisibility::kVisible)
-    return;
   anchor_bounds_ = anchor_bounds;
   quick_answers_ui_controller_->UpdateQuickAnswersBounds(anchor_bounds);
 }
