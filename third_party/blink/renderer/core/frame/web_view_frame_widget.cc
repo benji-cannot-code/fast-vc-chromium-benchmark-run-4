27d34cbb512c4dc3d212c6563eafef0664fac3ec
Dave Tapuska <dtapuska@chromium.org>
2020-10-02 17:28:49 +0000

    Remove SelectionBounds from WebWidget interface.
    Keep the API internal on WebFrameWidgetBase but change the API
    to use gfx::Rect. The return value from SelectionBounds was removed
    since the return code was never checked.
    
    BUG=1097816
    
    Change-Id: I0d4427eb78f646da06763fb99dc701726d73ce91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2442642
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813253}

--- third_party/blink/renderer/core/frame/web_view_frame_widget.cc
+++ third_party/blink/renderer/core/frame/web_view_frame_widget.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/web_view_frame_widget.h"
 
 #include "third_party/blink/public/platform/platform.h"
+#include "third_party/blink/renderer/core/editing/frame_selection.h"
 #include "third_party/blink/renderer/core/exported/web_view_impl.h"
 #include "third_party/blink/renderer/core/frame/local_frame_ukm_aggregator.h"
 #include "third_party/blink/renderer/core/frame/screen_metrics_emulator.h"
+#include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/widget/widget_base.h"
@@ -203,9 +205,27 @@ float WebViewFrameWidget::GetEmulatorScale() {
   return 1.0f;
 }
 
-bool WebViewFrameWidget::SelectionBounds(WebRect& anchor,
-                                         WebRect& focus) const {
-  return web_view_->SelectionBounds(anchor, focus);
+void WebViewFrameWidget::CalculateSelectionBounds(gfx::Rect& anchor_root_frame,
+                                                  gfx::Rect& focus_root_frame) {
+  const Frame* frame = View()->FocusedCoreFrame();
+  const auto* local_frame = DynamicTo<LocalFrame>(frame);
+  if (!local_frame)
+    return;
+
+  LocalFrameView* frame_view = local_frame->View();
+  if (!frame_view)
+    return;
+
+  IntRect anchor;
+  IntRect focus;
+  if (!local_frame->Selection().ComputeAbsoluteBounds(anchor, focus))
+    return;
+
+  VisualViewport& visual_viewport = GetPage()->GetVisualViewport();
+  anchor_root_frame = visual_viewport.RootFrameToViewport(
+      frame_view->ConvertToRootFrame(anchor));
+  focus_root_frame = visual_viewport.RootFrameToViewport(
+      frame_view->ConvertToRootFrame(focus));
 }
 
 WebURL WebViewFrameWidget::GetURLForDebugTrace() {
