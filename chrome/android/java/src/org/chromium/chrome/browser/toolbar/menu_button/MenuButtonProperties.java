31f0ad36d8ed0ec8ef234cf05a88ca93a0396627
Patrick Noland <pnoland@chromium.org>
2020-10-02 03:31:17 +0000

    [ToolbarMVC] Encapsulate MenuButton animations
    This CL encapsulates MenuButton animations behind its coordinator. This is accomplished in two different ways. The url focus animation uses new, property model-targeting Animators instead of operating directly on the view. The TabSwitcher animation is effectively just moved to the coordinator, enabled by moving translateCanvasToView to ViewUtils.
    
    Bug: 1086676
    Change-Id: I0da3feba45aa009ceff9fad1113d90ef314fcc8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414768
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813067}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/menu_button/MenuButtonProperties.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/menu_button/MenuButtonProperties.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuButtonHelper;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableBooleanPropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableFloatPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
 
 class MenuButtonProperties {
@@ -36,6 +37,7 @@ class MenuButtonProperties {
         }
     }
 
+    public static final WritableFloatPropertyKey ALPHA = new WritableFloatPropertyKey();
     public static final WritableObjectPropertyKey<AppMenuButtonHelper> APP_MENU_BUTTON_HELPER =
             new WritableObjectPropertyKey<>();
     public static final WritableObjectPropertyKey<String> CONTENT_DESCRIPTION =
@@ -48,8 +50,9 @@ class MenuButtonProperties {
             new WritableObjectPropertyKey(true);
     public static final WritableObjectPropertyKey<ThemeProperty> THEME =
             new WritableObjectPropertyKey<>(true);
+    public static final WritableFloatPropertyKey TRANSLATION_X = new WritableFloatPropertyKey();
 
     public static final PropertyKey[] ALL_KEYS =
-            new PropertyKey[] {APP_MENU_BUTTON_HELPER, CONTENT_DESCRIPTION, IS_CLICKABLE,
-                    IS_HIGHLIGHTING, IS_VISIBLE, SHOW_UPDATE_BADGE, THEME};
+            new PropertyKey[] {ALPHA, APP_MENU_BUTTON_HELPER, CONTENT_DESCRIPTION, IS_CLICKABLE,
+                    IS_HIGHLIGHTING, IS_VISIBLE, SHOW_UPDATE_BADGE, THEME, TRANSLATION_X};
 }
