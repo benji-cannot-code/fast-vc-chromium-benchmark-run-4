ae9e81ddd6b5fae492c54b25780ec95d81eca178
Matt Menke <mmenke@chromium.org>
2020-10-02 19:24:07 +0000

    Instrument userAgentData for identifiability study.
    This CL instruments [Worker]Navigator.userAgentData for the
    identifiability study. In particular, it logs a hash of the mobile bool
    and brand information as WebFeature::kNavigatorUserAgent, and adds a
    new IdentifiableSurface::Type for logging hint/value information for
    getHighEntropyValues() calls. Each individual passed in hint value is
    recorded with the corresponding returned value, so one
    getHighEntropyValues() call may log multiple values.
    
    Bug: 973801
    Change-Id: Ia548d2a7698809339e06e1eb32d4caf741bffb9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2443497
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Aaron Tagliaboschi <aarontag@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813323}

--- third_party/blink/renderer/core/frame/navigator_ua_data.cc
+++ third_party/blink/renderer/core/frame/navigator_ua_data.cc
@@ -5,12 +5,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/frame/navigator_ua_data.h"
 
+#include "base/compiler_specific.h"
 #include "base/single_thread_task_runner.h"
+#include "third_party/blink/public/common/privacy_budget/identifiability_metric_builder.h"
+#include "third_party/blink/public/common/privacy_budget/identifiability_study_settings.h"
+#include "third_party/blink/public/common/privacy_budget/identifiable_surface.h"
+#include "third_party/blink/public/common/privacy_budget/identifiable_token.h"
+#include "third_party/blink/public/common/privacy_budget/identifiable_token_builder.h"
+#include "third_party/blink/public/mojom/web_feature/web_feature.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_ua_data_values.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/page/page.h"
 
 namespace blink {
+
+namespace {
+
+// Record identifiability study metrics for a single field requested by a
+// getHighEntropyValues() call if the user is in the study.
+void MaybeRecordMetric(bool record_identifiability,
+                       const String& hint,
+                       const String& value,
+                       ExecutionContext* execution_context) {
+  if (LIKELY(!record_identifiability))
+    return;
+  auto identifiable_surface = IdentifiableSurface::FromTypeAndToken(
+      IdentifiableSurface::Type::kNavigatorUAData_GetHighEntropyValues,
+      IdentifiableToken(hint.Utf8()));
+  IdentifiabilityMetricBuilder(execution_context->UkmSourceID())
+      .Set(identifiable_surface, IdentifiableToken(value.Utf8()))
+      .Record(execution_context->UkmRecorder());
+}
+
+}  // namespace
 
 NavigatorUAData::NavigatorUAData(ExecutionContext* context)
     : ExecutionContextClient(context) {
@@ -77,25 +105,39 @@ ScriptPromise NavigatorUAData::getHighEntropyValues(
     Vector<String>& hints) const {
   auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
   ScriptPromise promise = resolver->Promise();
-  auto* executionContext =
+  auto* execution_context =
       ExecutionContext::From(script_state);  // GetExecutionContext();
+  DCHECK(execution_context);
+
+  bool record_identifiability =
+      IdentifiabilityStudySettings::Get()->IsTypeAllowed(
+          IdentifiableSurface::Type::kNavigatorUAData_GetHighEntropyValues);
   UADataValues* values = MakeGarbageCollected<UADataValues>();
   for (const String& hint : hints) {
     if (hint == "platform") {
       values->setPlatform(platform_);
+      MaybeRecordMetric(record_identifiability, hint, platform_,
+                        execution_context);
     } else if (hint == "platformVersion") {
       values->setPlatformVersion(platform_version_);
+      MaybeRecordMetric(record_identifiability, hint, platform_version_,
+                        execution_context);
     } else if (hint == "architecture") {
       values->setArchitecture(architecture_);
+      MaybeRecordMetric(record_identifiability, hint, architecture_,
+                        execution_context);
     } else if (hint == "model") {
       values->setModel(model_);
+      MaybeRecordMetric(record_identifiability, hint, model_,
+                        execution_context);
     } else if (hint == "uaFullVersion") {
       values->setUaFullVersion(ua_full_version_);
+      MaybeRecordMetric(record_identifiability, hint, ua_full_version_,
+                        execution_context);
     }
   }
 
-  DCHECK(executionContext);
-  executionContext->GetTaskRunner(TaskType::kPermission)
+  execution_context->GetTaskRunner(TaskType::kPermission)
       ->PostTask(
           FROM_HERE,
           WTF::Bind([](ScriptPromiseResolver* resolver,
