8b919354e2066a73963ff15fbc6eb68de508e7d5
Anders Hartvoll Ruud <andruud@chromium.org>
2020-10-02 16:20:34 +0000

    Copy 'name_' member during StyleRuleProperty::Copy
    This was neglected, end therefore ends up as a null-string in the copy,
    causing crashes.
    
    Bug: 1134550
    Change-Id: Ib2d5cf649e10d2d510417a43edd3189617092514
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2445470
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813218}

--- third_party/blink/renderer/core/css/style_rule.h
+++ third_party/blink/renderer/core/css/style_rule.h
@@ -189,7 +189,7 @@ class StyleRulePage : public StyleRuleBase {
   CSSSelectorList selector_list_;
 };
 
-class StyleRuleProperty : public StyleRuleBase {
+class CORE_EXPORT StyleRuleProperty : public StyleRuleBase {
  public:
   StyleRuleProperty(const String& name, CSSPropertyValueSet*);
   StyleRuleProperty(const StyleRuleProperty&);
