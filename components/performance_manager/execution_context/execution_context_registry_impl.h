75a948bf57a2262da8c343a482cd3c7476aec68e
Chris Hamilton <chrisha@chromium.org>
2020-10-02 17:49:43 +0000

    [PM] Create V8ContextTrackerImpl.
    This CL implements the data structures that are needed for bookkeeping
    V8 contexts, and related concepts. The lifetime management of these
    structures is testing in isolation.
    
    A further CL will implement business logic on V8ContextTracker, and add
    tests for that.
    
    BUG=1085129
    
    Change-Id: Iececc631fa38f04359ebeb823c53b641e57a4f24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2443490
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813266}

--- components/performance_manager/execution_context/execution_context_registry_impl.h
+++ components/performance_manager/execution_context/execution_context_registry_impl.h
@@ -39,6 +39,7 @@ class ExecutionContextRegistryImpl
 
   // ExecutionContextRegistry implementation:
   void AddObserver(ExecutionContextObserver* observer) override;
+  bool HasObserver(ExecutionContextObserver* observer) const override;
   void RemoveObserver(ExecutionContextObserver* observer) override;
   const ExecutionContext* GetExecutionContextByToken(
       const blink::ExecutionContextToken& token) override;
