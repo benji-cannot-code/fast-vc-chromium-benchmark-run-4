f67db7a85a92ce6cd51c84e0fd77fe86b50cc8c1
Andrew Shulaev <ddrone@google.com>
2020-10-02 07:33:56 +0000

    Replace base::Closure with base::{Once,Repeating}Closure
    Couple of usages seem to use the callback multiple times, thus base::RepeatingClosure is used in these places.
    
    Context: https://docs.google.com/document/d/1J4N3V5UmzZL9BZGNrn95E4xJu4cz3ynbg21zXR-J_qA/edit#
    
    Bug: 1007635
    Change-Id: I3bc08d2a1b9149eeaaa0386f05334b04697be40c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440581
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Auto-Submit: Andrew Shulaev <ddrone@google.com>
    Cr-Commit-Position: refs/heads/master@{#813096}

--- chrome/browser/installable/installable_manager_browsertest.cc
+++ chrome/browser/installable/installable_manager_browsertest.cc
@@ -96,15 +96,16 @@ InstallableParams GetPrimaryIconPreferMaskableAndSplashIconParams() {
 class LazyWorkerInstallableManager : public InstallableManager {
  public:
   LazyWorkerInstallableManager(content::WebContents* web_contents,
-                               base::Closure quit_closure)
-      : InstallableManager(web_contents), quit_closure_(quit_closure) {}
-  ~LazyWorkerInstallableManager() override {}
+                               base::OnceClosure quit_closure)
+      : InstallableManager(web_contents),
+        quit_closure_(std::move(quit_closure)) {}
+  ~LazyWorkerInstallableManager() override = default;
 
  protected:
-  void OnWaitingForServiceWorker() override { quit_closure_.Run(); }
+  void OnWaitingForServiceWorker() override { std::move(quit_closure_).Run(); }
 
  private:
-  base::Closure quit_closure_;
+  base::OnceClosure quit_closure_;
 };
 
 // Used only for testing pages where the manifest URL is changed. This class
@@ -115,7 +116,7 @@ class ResetDataInstallableManager : public InstallableManager {
       : InstallableManager(web_contents) {}
   ~ResetDataInstallableManager() override {}
 
-  void SetQuitClosure(base::Closure quit_closure) {
+  void SetQuitClosure(base::RepeatingClosure quit_closure) {
     quit_closure_ = quit_closure;
   }
 
@@ -126,12 +127,12 @@ class ResetDataInstallableManager : public InstallableManager {
   }
 
  private:
-  base::Closure quit_closure_;
+  base::RepeatingClosure quit_closure_;
 };
 
 class CallbackTester {
  public:
-  explicit CallbackTester(base::Closure quit_closure)
+  explicit CallbackTester(base::RepeatingClosure quit_closure)
       : quit_closure_(quit_closure) {}
 
   void OnDidFinishInstallableCheck(const InstallableData& data) {
@@ -163,7 +164,7 @@ class CallbackTester {
   bool has_worker() const { return has_worker_; }
 
  private:
-  base::Closure quit_closure_;
+  base::RepeatingClosure quit_closure_;
   std::vector<InstallableStatusCode> errors_;
   GURL manifest_url_;
   blink::Manifest manifest_;
@@ -180,8 +181,10 @@ class NestedCallbackTester {
  public:
   NestedCallbackTester(InstallableManager* manager,
                        const InstallableParams& params,
-                       base::Closure quit_closure)
-      : manager_(manager), params_(params), quit_closure_(quit_closure) {}
+                       base::OnceClosure quit_closure)
+      : manager_(manager),
+        params_(params),
+        quit_closure_(std::move(quit_closure)) {}
 
   void Run() {
     manager_->GetData(
@@ -218,13 +221,13 @@ class NestedCallbackTester {
     EXPECT_EQ(manifest_.short_name, data.manifest->short_name);
     EXPECT_EQ(manifest_.display_override, data.manifest->display_override);
 
-    quit_closure_.Run();
+    std::move(quit_closure_).Run();
   }
 
  private:
   InstallableManager* manager_;
   InstallableParams params_;
-  base::Closure quit_closure_;
+  base::OnceClosure quit_closure_;
   std::vector<InstallableStatusCode> errors_;
   GURL manifest_url_;
   blink::Manifest manifest_;
