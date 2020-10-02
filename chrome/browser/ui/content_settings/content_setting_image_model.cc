b67b76777f9e24fd25238614c9b2b4d690ce9094
Colin Blundell <blundell@chromium.org>
2020-10-02 10:53:41 +0000

    [Subresource Filter] Componentize ads blocked infobar C++ strings
    This CL componentizes the strings used by
    ads_blocked_infobar_delegate.cc in preparation for componentization of
    the ads blocked infobar itself. We skip the addition of translation
    screenshots as this is just a move of existing strings plus the addition
    of a generic //components-level IDS_RELOAD string that will be used
    across multiple contexts.
    
    Bug: 1116095
    Change-Id: I3af1a25ea260e436184bbb75d515a8bd6acd21ce
    Skip-Translation-Screenshots-Check: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440523
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813124}

--- chrome/browser/ui/content_settings/content_setting_image_model.cc
+++ chrome/browser/ui/content_settings/content_setting_image_model.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/permissions/permission_request_manager.h"
 #include "components/prefs/pref_service.h"
 #include "components/prerender/browser/prerender_manager.h"
+#include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/web_contents.h"
 #include "services/device/public/cpp/device_features.h"
