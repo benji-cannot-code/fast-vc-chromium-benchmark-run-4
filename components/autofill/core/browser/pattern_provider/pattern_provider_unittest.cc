89466f079184a4e0f29d7451e0de3bb87872c9fe
Martynas Sinkievic <marsin@google.com>
2020-10-02 08:10:22 +0000

    [Autofill] Add the pattern configuration parser
    Add functions which support validating and parsing the
    JSON matching pattern configuration string into a format
    supported by |PatternProvider|. Also provide support for
    versioning and replacing the configuration in
    |PatternProvider|. Due to the nature of the change,
    |PatternProvider| was changed to support testing with a
    synchronously loaded configuration.
    
    Change-Id: I662ecb933a4067c0ee6d188defe5e982b85469a7
    Bug: 1121990
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2359990
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Martynas Sinkievič <marsin@google.com>
    Cr-Commit-Position: refs/heads/master@{#813097}

--- components/autofill/core/browser/pattern_provider/pattern_provider_unittest.cc
+++ components/autofill/core/browser/pattern_provider/pattern_provider_unittest.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/autofill/core/browser/pattern_provider/pattern_provider.h"
-
 #include <stddef.h>
 
 #include <map>
@@ -12,9 +10,45 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/test/gtest_util.h"
+#include "base/test/task_environment.h"
+#include "components/autofill/core/browser/autofill_type.h"
+#include "components/autofill/core/browser/pattern_provider/pattern_configuration_parser.h"
+#include "components/autofill/core/browser/pattern_provider/pattern_provider.h"
+#include "components/autofill/core/browser/pattern_provider/test_pattern_provider.h"
+#include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace autofill {
+
+namespace {
+
+// Pattern Provider with custom values set for testing.
+class UnitTestPatternProvider : public PatternProvider {
+ public:
+  UnitTestPatternProvider();
+  ~UnitTestPatternProvider();
+};
+
+UnitTestPatternProvider::UnitTestPatternProvider() {
+  auto& company_patterns = patterns_[AutofillType(COMPANY_NAME).ToString()];
+  company_patterns["EN"].push_back(GetCompanyPatternEn());
+  company_patterns["DE"].push_back(GetCompanyPatternDe());
+
+  PatternProvider::SetPatternProviderForTesting(this);
+}
+
+UnitTestPatternProvider::~UnitTestPatternProvider() {
+  PatternProvider::ResetPatternProvider();
+}
+
+}  // namespace
+
+class AutofillPatternProviderTest : public testing::Test {
+ protected:
+  UnitTestPatternProvider pattern_provider_;
+
+  ~AutofillPatternProviderTest() override = default;
+};
 
 bool operator==(const MatchingPattern& mp1, const MatchingPattern& mp2) {
   return (mp1.language == mp2.language &&
@@ -26,15 +60,50 @@ bool operator==(const MatchingPattern& mp1, const MatchingPattern& mp2) {
           mp1.positive_score == mp2.positive_score);
 }
 
-TEST(AutofillPatternProvider, Single_Match) {
+TEST_F(AutofillPatternProviderTest, Single_Match) {
   MatchingPattern kCompanyPatternEn = GetCompanyPatternEn();
   MatchingPattern kCompanyPatternDe = GetCompanyPatternDe();
-  PatternProvider* pattern_provider = PatternProvider::getInstance();
+  PatternProvider& pattern_provider = PatternProvider::GetInstance();
 
-  ASSERT_TRUE(pattern_provider->GetMatchPatterns("COMPANY_NAME", "EN").size() >
+  ASSERT_TRUE(pattern_provider.GetMatchPatterns("COMPANY_NAME", "EN").size() >
               0);
-  EXPECT_EQ(pattern_provider->GetMatchPatterns("COMPANY_NAME", "EN")[0],
+  EXPECT_EQ(pattern_provider.GetMatchPatterns("COMPANY_NAME", "EN")[0],
             kCompanyPatternEn);
+}
+
+// Test that the default pattern provider loads without crashing.
+TEST(AutofillPatternProviderPipelineTest, DefaultPatternProviderLoads) {
+  base::test::TaskEnvironment task_environment_;
+  data_decoder::test::InProcessDataDecoder in_process_data_decoder_;
+
+  base::RunLoop run_loop;
+  field_type_parsing::PopulateFromResourceBundle(run_loop.QuitClosure());
+  run_loop.Run();
+  PatternProvider& default_pattern_provider = PatternProvider::GetInstance();
+
+  EXPECT_FALSE(default_pattern_provider.patterns_.empty());
+
+  // Call the getter to ensure sequence checks work correctly.
+  default_pattern_provider.GetMatchPatterns("EMAIL_ADDRESS", "en");
+}
+
+// Test that the TestPatternProvider class uses a PatternProvider::Map
+// equivalent to the DefaultPatternProvider. This is also an example of what is
+// needed to test the DefaultPatternProvider. Warning: If this crashes, check
+// that no state carried over from other tests using the singleton.
+TEST(AutofillPatternProviderPipelineTest, TestParsingEquivalent) {
+  base::test::TaskEnvironment task_environment_;
+  data_decoder::test::InProcessDataDecoder in_process_data_decoder_;
+
+  base::RunLoop run_loop;
+  field_type_parsing::PopulateFromResourceBundle(run_loop.QuitClosure());
+  run_loop.Run();
+  PatternProvider& default_pattern_provider = PatternProvider::GetInstance();
+
+  TestPatternProvider test_pattern_provider;
+
+  EXPECT_EQ(default_pattern_provider.patterns_,
+            test_pattern_provider.patterns_);
 }
 
 }  // namespace autofill
---
+++ components/autofill/core/browser/pattern_provider/test_pattern_provider.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/pattern_provider/test_pattern_provider.h"
+
+#include "components/autofill/core/browser/pattern_provider/pattern_configuration_parser.h"
+
+namespace autofill {
+
+TestPatternProvider::TestPatternProvider() {
+  base::Optional<PatternProvider::Map> patterns =
+      field_type_parsing::GetPatternsFromResourceBundleSynchronously();
+  if (patterns)
+    patterns_ = patterns.value();
+
+  PatternProvider::SetPatternProviderForTesting(this);
+}
+
+TestPatternProvider::~TestPatternProvider() {
+  PatternProvider::ResetPatternProvider();
+}
+
+}  // namespace autofill
