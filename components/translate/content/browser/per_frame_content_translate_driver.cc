492d3b06c612d5c5fba46568dbc006034229a30e
Maksim Ivanov <emaxx@chromium.org>
2020-10-02 22:48:38 +0000

    Fix use-after-move in //components/translate/content/browser/
    Fix use-after-move (potential) bugs found by the
    "bugprone-use-after-move" clang-tidy check.
    
    Bug: 1122844
    Change-Id: I4036e35e8d8d8737a327d6133a193c810eb8e929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2382595
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813408}

--- components/translate/content/browser/per_frame_content_translate_driver.cc
+++ components/translate/content/browser/per_frame_content_translate_driver.cc
@@ -169,7 +169,8 @@ void PerFrameContentTranslateDriver::TranslateFrame(
   mojo::AssociatedRemote<mojom::TranslateAgent> frame_agent;
   render_frame_host->GetRemoteAssociatedInterfaces()->GetInterface(
       &frame_agent);
-  frame_agent->TranslateFrame(
+  mojom::TranslateAgent* frame_agent_ptr = frame_agent.get();
+  frame_agent_ptr->TranslateFrame(
       translate_script, source_lang, target_lang,
       base::BindOnce(&PerFrameContentTranslateDriver::OnFrameTranslated,
                      weak_pointer_factory_.GetWeakPtr(), translate_seq_no,
@@ -337,7 +338,8 @@ void PerFrameContentTranslateDriver::StartLanguageDetection() {
   mojo::AssociatedRemote<mojom::TranslateAgent> frame_agent;
   web_contents()->GetMainFrame()->GetRemoteAssociatedInterfaces()->GetInterface(
       &frame_agent);
-  frame_agent->GetWebLanguageDetectionDetails(base::BindOnce(
+  mojom::TranslateAgent* frame_agent_ptr = frame_agent.get();
+  frame_agent_ptr->GetWebLanguageDetectionDetails(base::BindOnce(
       &PerFrameContentTranslateDriver::OnWebLanguageDetectionDetails,
       weak_pointer_factory_.GetWeakPtr(), std::move(frame_agent)));
 }
