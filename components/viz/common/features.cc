83fe65d384f1c4e207b4d3a69922799107c787d7
Malay Keshav <malaykeshav@chromium.org>
2020-10-02 00:34:48 +0000

    Viz: Use glClear to draw solid color draw quads when possible
    This patch uses glClear to draw the solid color draw quad whenever
    possible. It also replaces the usage of color transform matrix used
    in the fragment shader for solid color draw quad with a precomputed
    color that is in the correct color space.
    
    Improves draw time of a full screen solid color quad by approximately
    80% on Atlas. (From ~1.5ms to about ~0.25ms)
    
    Also adds a feature flag that is disabled by default and will be enabled
    in subsequent CLs. (For easy revert of feature)
    
    Bug: 1072895,1072910
    Test: Manually tested on Chrome OS
    Change-Id: I0d0174bffa802d146bce26f985bc3150e051dc05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2343970
    Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813018}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -43,6 +43,10 @@ const base::Feature kDynamicColorGamut{"DynamicColorGamut",
                                        base::FEATURE_ENABLED_BY_DEFAULT};
 #endif
 
+// Uses glClear to composite solid color quads whenever possible.
+const base::Feature kFastSolidColorDraw{"FastSolidColorDraw",
+                                        base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Viz for WebView architecture.
 const base::Feature kVizForWebView{"VizForWebView",
                                    base::FEATURE_DISABLED_BY_DEFAULT};
@@ -127,6 +131,10 @@ bool IsDynamicColorGamutEnabled() {
   return base::FeatureList::IsEnabled(kDynamicColorGamut);
 }
 #endif
+
+bool IsUsingFastPathForSolidColorQuad() {
+  return base::FeatureList::IsEnabled(kFastSolidColorDraw);
+}
 
 bool IsUsingVizForWebView() {
   // Viz for WebView requires shared images to be enabled.
