55c36d99ccf235e505f38f4e2df69715bd879818
Kelvin Jiang <kelvinjiang@chromium.org>
2020-10-03 01:55:31 +0000

    [Sheriff] Disable StartupTracingThrottle test
    This CL disables
    ChromeTracingDelegateBrowserTestOnStartup.StartupTracingThrottle as it
    is failing on multiple mac builders.
    
    Bug: 1134793
    TBR: eseckler@chromium.org
    Change-Id: I72f1702bdb5613d4c86f71adb0c66283ccdb3ea6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2446994
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813467}

--- chrome/browser/tracing/chrome_tracing_delegate_browsertest.cc
+++ chrome/browser/tracing/chrome_tracing_delegate_browsertest.cc
@@ -286,8 +286,10 @@ IN_PROC_BROWSER_TEST_F(ChromeTracingDelegateBrowserTestOnStartup,
       content::BackgroundTracingManager::GetInstance()->HasActiveScenario());
 }
 
+// TODO(crbug.com/1134793): Test is disabled after failing on multiple mac
+// builders.
 IN_PROC_BROWSER_TEST_F(ChromeTracingDelegateBrowserTestOnStartup,
-                       PRE_PRE_StartupTracingThrottle) {
+                       DISABLED_PRE_PRE_StartupTracingThrottle) {
   // This test exists just to make sure the browser is created at least once and
   // so a default profile is created. Then, the next time the browser is
   // created, kMetricsReportingEnabled is explicitly read from the profile and
@@ -295,7 +297,7 @@ IN_PROC_BROWSER_TEST_F(ChromeTracingDelegateBrowserTestOnStartup,
 }
 
 IN_PROC_BROWSER_TEST_F(ChromeTracingDelegateBrowserTestOnStartup,
-                       PRE_StartupTracingThrottle) {
+                       DISABLED_PRE_StartupTracingThrottle) {
   EXPECT_TRUE(
       content::BackgroundTracingManager::GetInstance()->HasActiveScenario());
 
@@ -309,7 +311,7 @@ IN_PROC_BROWSER_TEST_F(ChromeTracingDelegateBrowserTestOnStartup,
 // https://crbug.com/832981: The test is reenabled to check if flakiness still
 // exists.
 IN_PROC_BROWSER_TEST_F(ChromeTracingDelegateBrowserTestOnStartup,
-                       StartupTracingThrottle) {
+                       DISABLED_StartupTracingThrottle) {
   // The startup scenario should *not* be started, since not enough
   // time has elapsed since the last upload (set in the PRE_ above).
   EXPECT_FALSE(
