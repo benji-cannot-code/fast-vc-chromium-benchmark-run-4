0871faf346578583da6355de8e075609aeb0146f
Jesse Schettler <jschettler@chromium.org>
2020-10-03 02:56:55 +0000

    scanning: Add and implement ScanService::Scan()
    Add and implement ScanService::Scan() to allow clients to perform scans.
    The ScanService currently saves each scanned image to the user's
    "My files" folder, but future changes will allow clients to specify the
    file type and save-to location.
    
    Bug: 1059779
    Change-Id: I628571f8663498bc8499e9b7e53d1d6b144a0816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2436385
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813480}

--- chrome/browser/chromeos/scanning/scan_service.h
+++ chrome/browser/chromeos/scanning/scan_service.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_map.h"
+#include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "base/unguessable_token.h"
@@ -38,10 +39,16 @@ class ScanService : public scanning::mojom::ScanService, public KeyedService {
   void GetScanners(GetScannersCallback callback) override;
   void GetScannerCapabilities(const base::UnguessableToken& scanner_id,
                               GetScannerCapabilitiesCallback callback) override;
+  void Scan(const base::UnguessableToken& scanner_id,
+            scanning::mojom::ScanSettingsPtr settings,
+            ScanCallback callback) override;
 
   // Binds receiver_ by consuming |pending_receiver|.
   void BindInterface(
       mojo::PendingReceiver<scanning::mojom::ScanService> pending_receiver);
+
+  // Sets the root directory to use when saving scanned images for tests.
+  void SetRootDirForTesting(const base::FilePath& root_dir);
 
  private:
   // KeyedService:
@@ -57,6 +64,17 @@ class ScanService : public scanning::mojom::ScanService, public KeyedService {
       GetScannerCapabilitiesCallback callback,
       const base::Optional<lorgnette::ScannerCapabilities>& capabilities);
 
+  // Processes each |scanned_image| received after calling
+  // LorgnetteScannerManager::Scan().
+  void OnPageReceived(std::string scanned_image);
+
+  // Processes the final result of calling LorgnetteScannerManager::Scan().
+  void OnScanCompleted(ScanCallback callback, bool success);
+
+  // Returns the scanner name corresponding to the given |scanner_id| or an
+  // empty string if the name cannot be found.
+  std::string GetScannerName(const base::UnguessableToken& scanner_id);
+
   // Map of scanner IDs to display names. Used to pass the correct display name
   // to LorgnetteScannerManager when clients provide an ID.
   base::flat_map<base::UnguessableToken, std::string> scanner_names_;
@@ -67,6 +85,13 @@ class ScanService : public scanning::mojom::ScanService, public KeyedService {
 
   // Unowned. Used to get scanner information and perform scans.
   LorgnetteScannerManager* lorgnette_scanner_manager_;
+
+  // The root directory where scanned images are saved. Allows tests to set a
+  // different root.
+  base::FilePath root_dir_ = base::FilePath("/");
+
+  // Indicates whether there was a failure to save scanned images.
+  bool save_failed_;
 
   base::WeakPtrFactory<ScanService> weak_ptr_factory_{this};
 };
