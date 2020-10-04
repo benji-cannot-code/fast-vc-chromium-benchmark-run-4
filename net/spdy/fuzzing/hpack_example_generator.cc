b6da6725520d99554ad84d5dad2f34a56e37152a
Nick Harper <nharper@chromium.org>
2020-10-04 01:21:26 +0000

    Roll src/net/third_party/quiche/src/ 89ec3381c..d10bbe3dc (8 commits)
    https://quiche.googlesource.com/quiche.git/+log/89ec3381ca3d..d10bbe3dc764
    
    $ git log 89ec3381c..d10bbe3dc --date=short --no-merges --format='%ad %ae %s'
    2020-10-02 nharper Use C++14 friendly idiom in http2::HuffmanEncodeFast
    2020-10-02 vasilvv Use absl::string_view in QUICHE directly.
    2020-10-02 bnc Add faster Huffman encoding implementation, not yet used in production.
    2020-10-02 renjietang Rename v99_streamid_manager to ietf_streamid_manager.
    2020-10-02 bnc Remove HpackHuffmanTable argument from HpackEncoder constructor.
    2020-10-02 fayang Handle PINGs in QUIC connection.
    2020-10-02 bnc Use QuicheTest in third_party/spdy.
    2020-10-02 fayang Deprecate gfe2_reloadable_flag_quic_add_silent_idle_timeout.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: I6b715ea3cc708196b5de3b203926d070cb1771df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2447052
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Reviewed-by: Bence Béky <bnc@chromium.org>
    Auto-Submit: Nick Harper <nharper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813541}

--- net/spdy/fuzzing/hpack_example_generator.cc
+++ net/spdy/fuzzing/hpack_example_generator.cc
@@ -56,7 +56,7 @@ int main(int argc, char** argv) {
 
   HpackFuzzUtil::GeneratorContext context;
   HpackFuzzUtil::InitializeGeneratorContext(&context);
-  spdy::HpackEncoder encoder(spdy::ObtainHpackHuffmanTable());
+  spdy::HpackEncoder encoder;
 
   for (int i = 0; i != example_count; ++i) {
     spdy::SpdyHeaderBlock headers =
