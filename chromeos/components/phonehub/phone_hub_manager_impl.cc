6fec57c553aebb164fc2aba74c36841bf4cd6793
Jimmy Gong <jimmyxgong@chromium.org>
2020-10-05 21:33:54 +0000

    Add CrosStateSender
    Responsible for sending the the cros state to the user's phone.
    
    Bug: 1106937
    Test: chromeos_components_unittests
    Change-Id: Ibe730b1796dd6d100db27def461821da8f9bbcde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2443758
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813931}

--- chromeos/components/phonehub/phone_hub_manager_impl.cc
+++ chromeos/components/phonehub/phone_hub_manager_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/components/phonehub/browser_tabs_model_provider.h"
 #include "chromeos/components/phonehub/connection_manager_impl.h"
 #include "chromeos/components/phonehub/connection_scheduler_impl.h"
+#include "chromeos/components/phonehub/cros_state_sender.h"
 #include "chromeos/components/phonehub/do_not_disturb_controller_impl.h"
 #include "chromeos/components/phonehub/feature_status_provider_impl.h"
 #include "chromeos/components/phonehub/find_my_device_controller_impl.h"
@@ -45,6 +46,10 @@ PhoneHubManagerImpl::PhoneHubManagerImpl(
           std::make_unique<MessageReceiverImpl>(connection_manager_.get())),
       message_sender_(
           std::make_unique<MessageSenderImpl>(connection_manager_.get())),
+      cros_state_sender_(
+          std::make_unique<CrosStateSender>(message_sender_.get(),
+                                            connection_manager_.get(),
+                                            multidevice_setup_client)),
       do_not_disturb_controller_(
           std::make_unique<DoNotDisturbControllerImpl>(message_sender_.get())),
       connection_scheduler_(std::make_unique<ConnectionSchedulerImpl>(
@@ -130,6 +135,7 @@ void PhoneHubManagerImpl::Shutdown() {
   find_my_device_controller_.reset();
   connection_scheduler_.reset();
   do_not_disturb_controller_.reset();
+  cros_state_sender_.reset();
   message_sender_.reset();
   message_receiver_.reset();
   feature_status_provider_.reset();
