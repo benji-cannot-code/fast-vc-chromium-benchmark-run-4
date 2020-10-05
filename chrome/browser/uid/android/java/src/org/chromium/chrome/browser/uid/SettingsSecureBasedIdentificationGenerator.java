0dd67be78b3a519e1e3db9d32097552e0e6a65bb
Andrey Zaytsev <andzaytsev@google.com>
2020-10-05 17:53:29 +0000

    Modularize Omaha on Android: copied identity/ from chrome_java to //chrome/browser/uid
    Part 1/3 for relanding crrev.com/c/2426045.
    The old files will be deleted in a follow up CL after the internal CL for updating references (https://chrome-internal-review.googlesource.com/c/clank/internal/apps/+/3301233) lands.
    
    Bug: 1131415
    Change-Id: Iadae724b6e00a04cbb84e46e04f2c2b1b687df42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2442533
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/master@{#813806}

--- chrome/browser/uid/android/java/src/org/chromium/chrome/browser/uid/SettingsSecureBasedIdentificationGenerator.java
+++ chrome/browser/uid/android/java/src/org/chromium/chrome/browser/uid/SettingsSecureBasedIdentificationGenerator.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.identity;
+package org.chromium.chrome.browser.uid;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -36,8 +36,7 @@ public class SettingsSecureBasedIdentificationGenerator implements UniqueIdentif
             return "";
         }
 
-        String md5Hash = HashUtil.getMd5Hash(
-                new HashUtil.Params(androidId).withSalt(salt));
+        String md5Hash = HashUtil.getMd5Hash(new HashUtil.Params(androidId).withSalt(salt));
         return md5Hash == null ? "" : md5Hash;
     }
 
