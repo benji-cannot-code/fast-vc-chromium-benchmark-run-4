3c323be0034ef860af9193369d89a04fcca71bce
Sebastien Marchand <sebmarchand@chromium.org>
2020-10-05 20:51:30 +0000

    Disable the child processes LLVM profile collection in non-PGO builds
    This create some issues in some tests, they fail with the following
    stack trace:
    
    BrowserTestBase received signal: Segmentation fault. Backtrace:
    #0 0x56400bd836c9 base::debug::CollectStackTrace()
    #1 0x56400bcb90c3 base::debug::StackTrace::StackTrace()
    #2 0x56400b74b5e6 content::(anonymous namespace)::DumpStackTraceSignalHandler()
    #3 0x7fdc5b8c84c0 (/lib/x86_64-linux-gnu/libc-2.23.so+0x354bf)
    #4 0x564010b453b0 __llvm_profile_merge_from_buffer
    #5 0x564010b43705 writeFile
    #6 0x564010b43547 __llvm_profile_write_file
    #7 0x564010b43874 __llvm_profile_dump
    #8 0x56400bda6373 base::WriteClangProfilingProfile()
    #9 0x56400d8cb484 content::ChildProcess::~ChildProcess()
    #10 0x564010780118 content::RenderProcessImpl::~RenderProcessImpl()
    #11 0x564010688274 content::InProcessRendererThread::CleanUp()
    #12 0x56400bd6c2b8 base::Thread::ThreadMain()
    #13 0x56400bd9a8d9 base::(anonymous namespace)::ThreadFunc()
    #14 0x7fdc60fa16ba start_thread
    #15 0x7fdc5b99a4dd clone
    
    This call to base::WriteClangProfilingProfile has been recently[1] added
    to ChildProcess::~ChildProcess to ensure that the child utility
    processes properly flushed their profiling data at shutdown. This was an
    issue when running the PGO profiling scenarios as some child processes
    were terminated too aggressively and sometime didn't had a chance to
    fully write their profiling data. It doesn't seem to affect the coverage
    data (and this change brings the coverage build back to the state it was
    in before [1]).
    
    We should ideally also look at this crash and understand why it's
    happening, this CL really just prevent this crash from happening by not
    calling the problematic function when it's not needed.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2378272
    
    Bug: 1065485
    Change-Id: I051ab0993216a887a38863070b554e02ba60bb77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450332
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813907}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/scoped_blocking_call.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
+#include "build/config/compiler/compiler_buildflags.h"
 #include "chrome/browser/about_flags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/buildflags.h"
--- content/child/child_process.cc
+++ content/child/child_process.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "base/threading/thread_local.h"
 #include "build/build_config.h"
+#include "build/config/compiler/compiler_buildflags.h"
 #include "content/child/child_thread_impl.h"
 #include "content/common/android/cpu_time_metrics.h"
 #include "content/common/mojo_core_library_support.h"
@@ -136,7 +137,7 @@ ChildProcess::~ChildProcess() {
     base::ThreadPoolInstance::Get()->Shutdown();
   }
 
-#if BUILDFLAG(CLANG_PROFILING_INSIDE_SANDBOX)
+#if BUILDFLAG(CLANG_PROFILING_INSIDE_SANDBOX) && BUILDFLAG(CLANG_PGO)
   // Flush the profiling data to disk. Doing this manually (vs relying on this
   // being done automatically when the process exits) will ensure that this data
   // doesn't get lost if the process is fast killed.
