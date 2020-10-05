2b48d5586a314ee0fb049860190adc98298f5a67
Prashant Nevase <prashant.n@samsung.com>
2020-10-05 14:25:22 +0000

    Fix regression in dark mode filter creation.
    There is regression caused by creating dark mode filter in
    GraphicsContext ctor. This patch creates dark mode filter
    once it is accessed first time.
    
    Bug: 1134430, 1134476, 1082339
    Change-Id: I97521749167fcd1fb7ddfeb1b2fe7e8a7de8846f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2449829
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Prashant Nevase <prashant.n@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#813730}

--- third_party/blink/renderer/platform/graphics/graphics_context.cc
+++ third_party/blink/renderer/platform/graphics/graphics_context.cc
@@ -131,6 +131,7 @@ GraphicsContext::GraphicsContext(PaintController& paint_controller,
       disable_destruction_checks_(false),
 #endif
       device_scale_factor_(1.0f),
+      dark_mode_filter_(nullptr),
       printing_(false),
       is_painting_preview_(false),
       in_drawing_recorder_(false),
@@ -139,9 +140,6 @@ GraphicsContext::GraphicsContext(PaintController& paint_controller,
   // allocate several here.
   paint_state_stack_.push_back(std::make_unique<GraphicsContextState>());
   paint_state_ = paint_state_stack_.back().get();
-
-  dark_mode_filter_ = std::make_unique<DarkModeFilter>();
-  dark_mode_filter_->UpdateSettings(GetCurrentDarkModeSettings());
 }
 
 GraphicsContext::~GraphicsContext() {
@@ -153,6 +151,15 @@ GraphicsContext::~GraphicsContext() {
     DCHECK(!SaveCount());
   }
 #endif
+}
+
+DarkModeFilter* GraphicsContext::GetDarkModeFilter() {
+  if (!dark_mode_filter_) {
+    dark_mode_filter_ = std::make_unique<DarkModeFilter>();
+    dark_mode_filter_->UpdateSettings(GetCurrentDarkModeSettings());
+  }
+
+  return dark_mode_filter_.get();
 }
 
 void GraphicsContext::UpdateDarkModeSettingsForTest(
