468e908c5e7441d6197e36f0438e26dae2bc2b04
Christopher Cameron <ccameron@chromium.org>
2020-10-05 07:03:00 +0000

    MacPWAs: Re-create app shims when arch changes
    In crbug.com/353047, we encountered the problem where Chrome
    would upgrade itself from 32-bit to 64-bit, but app shims
    would be stuck on 32-bit. A mechanism was added to upgrade
    the shims "when needed", based on a  kAppShortcutsVersion local
    preference. This number has been incremented several times for
    various incompatibilities.
    
    Add a separate kAppShortcutsArch preference which saves the
    architecture that apps have been being created in. Whenever
    this does not match base::SysInfo::OperatingSystemArchitecture,
    re-create all shims.
    
    This will fix the situation whereby a user data dir, including
    all of its local prefs, is copied from an Intel mac to an ARM
    mac.
    
    A few caveats:
    - This will only re-create PWA shims. Legacy apps will need to
      be re-launched from Chrome.
    - This only fixes the issue if the user launches Chrome before
      launching a PWA. The PWA shim copied from x86, if it succeeds
      in running, will start Chrome with the start URL for the PWA.
    
    Bug: 1117599
    Change-Id: I4d9e1a87e91c1d461b592b0a8250b22120368d94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2444130
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813631}

--- chrome/browser/apps/platform_apps/shortcut_manager.cc
+++ chrome/browser/apps/platform_apps/shortcut_manager.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/one_shot_event.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/system/sys_info.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/extension_service.h"
@@ -37,13 +38,23 @@ using extensions::Extension;
 
 namespace {
 
+#if defined(OS_MAC)
 // This version number is stored in local prefs to check whether app shortcuts
 // need to be recreated. This might happen when we change various aspects of app
 // shortcuts like command-line flags or associated icons, binaries, etc.
-#if defined(OS_MAC)
 const int kCurrentAppShortcutsVersion = APP_SHIM_VERSION_NUMBER;
+
+// The architecture that was last used to create app shortcuts for this user
+// directory.
+std::string CurrentAppShortcutsArch() {
+  return base::SysInfo::OperatingSystemArchitecture();
+}
 #else
+// Non-mac platforms do not update shortcuts.
 const int kCurrentAppShortcutsVersion = 0;
+std::string CurrentAppShortcutsArch() {
+  return "";
+}
 #endif
 
 // Delay in seconds before running UpdateShortcutsForAllApps.
@@ -76,6 +87,7 @@ void AppShortcutManager::RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
   // Indicates whether app shortcuts have been created.
   registry->RegisterIntegerPref(prefs::kAppShortcutsVersion, 0);
+  registry->RegisterStringPref(prefs::kAppShortcutsArch, "");
 }
 
 AppShortcutManager::AppShortcutManager(Profile* profile)
@@ -170,6 +182,8 @@ void AppShortcutManager::UpdateShortcutsForAllAppsNow() {
 void AppShortcutManager::SetCurrentAppShortcutsVersion() {
   profile_->GetPrefs()->SetInteger(prefs::kAppShortcutsVersion,
                                    kCurrentAppShortcutsVersion);
+  profile_->GetPrefs()->SetString(prefs::kAppShortcutsArch,
+                                  CurrentAppShortcutsArch());
 }
 
 void AppShortcutManager::UpdateShortcutsForAllAppsIfNeeded() {
@@ -180,8 +194,13 @@ void AppShortcutManager::UpdateShortcutsForAllAppsIfNeeded() {
 
   int last_version =
       profile_->GetPrefs()->GetInteger(prefs::kAppShortcutsVersion);
-  if (last_version >= kCurrentAppShortcutsVersion)
+  std::string last_arch =
+      profile_->GetPrefs()->GetString(prefs::kAppShortcutsArch);
+
+  if (last_version == kCurrentAppShortcutsVersion &&
+      last_arch == CurrentAppShortcutsArch()) {
     return;
+  }
 
   content::GetUIThreadTaskRunner({})->PostDelayedTask(
       FROM_HERE,
