6c33f8829de6603c46af3866d794a42671e4bdc9
Chris Thompson <cthomp@chromium.org>
2020-10-05 21:53:48 +0000

    Reland "Remove net::ct::CTVerifyResult and merge with net::CertVerifyResult"
    This reverts commit 8fd53893290a9472743160279ddfcc5b1aade198 (which
    reverted the original change in commit 0d9bc82732ce761a6e2cbf3413189cbd8d5952cb).
    The cause for the revert was fixed in
    commit 06c4cc7450bb74f1fa6f985139af5315b48c08e1.
    
    Original change's description:
    > Remove net::ct::CTVerifyResult and merge with net::CertVerifyResult
    >
    > As part of moving CT verification to occur within the state machine
    > of cert verification, move the CTVerifyResult fields into the
    > CertVerifyResult as appropriate.
    >
    > As a nice side benefit, this allows CT verification details to be
    > handled in browser tests using ContentMockCertVerifier.
    >
    > Bug: 848277, 1016635
    > Change-Id: I365358ca12ecd9205d8103ed0e3396cdb48144b3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2423060
    > Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Reviewed-by: Emily Stark <estark@chromium.org>
    > Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    > Commit-Queue: Emily Stark <estark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#810364}
    
    TBR=rsleevi@chromium.org,kouhei@chromium.org,estark@chromium.org,cthomp@chromium.org,rmcelrath@chromium.org
    
    Bug: 848277, 1016635
    Change-Id: I170cafdd9aed46fed4de0e6afa34b3308ebbd6ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450735
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813946}

--- components/certificate_transparency/chrome_ct_policy_enforcer_unittest.cc
+++ components/certificate_transparency/chrome_ct_policy_enforcer_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/rsa_private_key.h"
 #include "crypto/sha2.h"
 #include "net/cert/ct_policy_status.h"
-#include "net/cert/ct_verify_result.h"
 #include "net/cert/x509_certificate.h"
 #include "net/cert/x509_util.h"
 #include "net/log/net_log_with_source.h"
--- net/ssl/ssl_info.h
+++ net/ssl/ssl_info.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_export.h"
 #include "net/cert/cert_status_flags.h"
 #include "net/cert/ct_policy_status.h"
-#include "net/cert/ct_verify_result.h"
 #include "net/cert/ocsp_verify_result.h"
 #include "net/cert/sct_status_flags.h"
 #include "net/cert/signed_certificate_timestamp_and_status.h"
@@ -45,16 +44,6 @@ class NET_EXPORT SSLInfo {
   void Reset();
 
   bool is_valid() const { return cert.get() != nullptr; }
-
-  // Adds the SignedCertificateTimestamps and policy compliance details
-  // from ct_verify_result to |signed_certificate_timestamps| and
-  // |ct_policy_compliance_details|. SCTs are held in three separate
-  // vectors in ct_verify_result, each vetor representing a particular
-  // verification state, this method associates each of the SCTs with
-  // the corresponding SCTVerifyStatus as it adds it to the
-  // |signed_certificate_timestamps| list.
-  void UpdateCertificateTransparencyInfo(
-      const ct::CTVerifyResult& ct_verify_result);
 
   // The SSL certificate.
   scoped_refptr<X509Certificate> cert;
@@ -119,11 +108,6 @@ class NET_EXPORT SSLInfo {
   // not, why not.
   ct::CTPolicyCompliance ct_policy_compliance =
       ct::CTPolicyCompliance::CT_POLICY_COMPLIANCE_DETAILS_NOT_AVAILABLE;
-
-  // True if the connection was required to comply with the CT cert policy. Only
-  // meaningful if |ct_policy_compliance| is not
-  // COMPLIANCE_DETAILS_NOT_AVAILABLE.
-  bool ct_policy_compliance_required = false;
 
   // OCSP stapling details.
   OCSPVerifyResult ocsp_result;
