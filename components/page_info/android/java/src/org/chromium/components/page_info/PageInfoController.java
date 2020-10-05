7a376ba69a59de5547a2f83a8bd22c4be8a3bb81
Christian Dullweber <dullweber@chromium.org>
2020-10-05 17:17:37 +0000

    Fix PageInfo UX issues
    Set android:includeFontPadding="false" to better align favicon with URL.
    Use padding instead of margin to get the right background color.
    Show details link if there is a connection message.
    Show red warning triangle if there is a connection security issue.
    Also includes some general cleanup for margins.
    
    Bug: 1135124
    Change-Id: Iff1f880b51a2a8bd60ef0606afec03fea82e9318
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2449372
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813785}

--- components/page_info/android/java/src/org/chromium/components/page_info/PageInfoController.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoController.java
@@ -414,7 +414,7 @@ public class PageInfoController implements PageInfoMainController, ModalDialogPr
             messageBuilder.append(details);
         }
 
-        if (!mIsV2Enabled && isConnectionDetailsLinkVisible()) {
+        if (isConnectionDetailsLinkVisible() && messageBuilder.length() > 0) {
             messageBuilder.append(" ");
             SpannableString detailsText =
                     new SpannableString(mContext.getString(R.string.details_link));
