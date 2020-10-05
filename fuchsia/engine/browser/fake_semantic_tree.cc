9f1434c2cc15c3c9331cfe7a1c3ee263203c2a16
Sharon Yang <yangsharon@chromium.org>
2020-10-05 18:10:29 +0000

    Revert "[fuchsia][a11y] Sends full semantic tree when semantics are enabled."
    This reverts commit fdae2298518693c507d7c1e0adf3f11150017722.
    
    Reason for revert: Test is causing Fuchsia trunk builder to fail https://ci.chromium.org/p/chrome/builders/ci/fuchsia-x64-trunk
    
    Original change's description:
    > [fuchsia][a11y] Sends full semantic tree when semantics are enabled.
    >
    > This change fixes the bug where chrome was sending the full semantic tree only for the first time that semantics were enabled. This happened because the semantic tree was not getting cleared on the chrome side when disabled. when semantic were disabled / enabled, the events fired on the tree included only the diffs from the previous state of the tree.
    >
    > Test: web_engine_browsertests
    >
    > Bug: fuchsia:59680, 1134147
    > Change-Id: I8266d6436f9bfb9ee9e89a0f46c0916bac7a5e75
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2442646
    > Commit-Queue: Lucas Radaelli <lucasradaelli@google.com>
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Reviewed-by: Sharon Yang <yangsharon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#813771}
    
    TBR=wez@chromium.org,sergeyu@chromium.org,teisenbe@google.com,yangsharon@chromium.org,lucasradaelli@google.com
    
    Change-Id: Id3df2d7e4c31632f65d2dcc4a2a9b07fc92f9b83
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: fuchsia:59680
    Bug: 1134147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450693
    Reviewed-by: Sharon Yang <yangsharon@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813812}

--- fuchsia/engine/browser/fake_semantic_tree.cc
+++ fuchsia/engine/browser/fake_semantic_tree.cc
@@ -140,7 +140,3 @@ void FakeSemanticTree::CommitUpdates(CommitUpdatesCallback callback) {
 void FakeSemanticTree::NotImplemented_(const std::string& name) {
   NOTIMPLEMENTED() << name;
 }
-
-void FakeSemanticTree::Clear() {
-  nodes_.clear();
-}
