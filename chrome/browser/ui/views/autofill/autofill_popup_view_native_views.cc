507844353a99062f8f537a559316e9364cdbe36d
Mohamed Amir Yosef <mamir@chromium.org>
2020-10-05 22:37:07 +0000

    [Autofill] Use the ColorTrackingIconView in autofill code
    Instead of having its own implementation, this CL uses the recently
    introduced ColorTrackingIconView in the autofill popup native views.
    
    Bug: 1115952
    Change-Id: I77ed51c6fc903d8c8f7eee3bd9a1174f69b44ca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440672
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813969}

--- chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
 #include "ui/views/border.h"
+#include "ui/views/controls/color_tracking_icon_view.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/menu/menu_config.h"
@@ -109,23 +110,6 @@ void BuildColumnSet(views::GridLayout* layout) {
                         views::GridLayout::ColumnSize::kUsePreferred, 0, 0);
 }
 
-// An image view that shows a vector icon and tracks changes in the theme.
-class VectorIconView : public views::ImageView {
- public:
-  explicit VectorIconView(const gfx::VectorIcon& icon) : icon_(icon) {}
-
-  // views::ImageView
-  void OnThemeChanged() override {
-    ImageView::OnThemeChanged();
-    const SkColor color = GetNativeTheme()->GetSystemColor(
-        ui::NativeTheme::kColorId_DefaultIconColor);
-    SetImage(gfx::CreateVectorIcon(icon_, gfx::kFaviconSize, color));
-  }
-
- private:
-  const gfx::VectorIcon& icon_;
-};
-
 std::unique_ptr<views::ImageView> ImageViewFromImageSkia(
     const gfx::ImageSkia& image_skia) {
   if (image_skia.isNull())
@@ -137,7 +121,8 @@ std::unique_ptr<views::ImageView> ImageViewFromImageSkia(
 
 std::unique_ptr<views::ImageView> ImageViewFromVectorIcon(
     const gfx::VectorIcon& vector_icon) {
-  return std::make_unique<VectorIconView>(vector_icon);
+  return std::make_unique<views::ColorTrackingIconView>(vector_icon,
+                                                        gfx::kFaviconSize);
 }
 
 std::unique_ptr<views::ImageView> GetIconImageViewByName(
