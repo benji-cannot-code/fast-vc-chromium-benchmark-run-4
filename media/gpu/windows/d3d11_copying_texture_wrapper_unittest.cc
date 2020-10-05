ea9ab2fa59c2f4e81b199904506b41660b0fd119
Frank Liberato <liberato@chromium.org>
2020-10-05 22:19:19 +0000

    Create VideoProcessor per TextureSelector.
    Rather than create one VideoProcessor per wrapper, create one per
    TextureSelector.  This reduces the number of required processors
    to one per decoder init, rather than one per picture buffer.
    
    Bug: 1133568
    Change-Id: I87d58c49742e083ca3cdf3c67841a153375f1a4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2441290
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813957}

--- media/gpu/windows/d3d11_copying_texture_wrapper_unittest.cc
+++ media/gpu/windows/d3d11_copying_texture_wrapper_unittest.cc
@@ -81,6 +81,9 @@ class MockVideoProcessorProxy : public VideoProcessorProxy {
   base::Optional<gfx::ColorSpace> last_output_color_space_;
   base::Optional<DXGI_HDR_METADATA_HDR10> last_stream_metadata_;
   base::Optional<DXGI_HDR_METADATA_HDR10> last_display_metadata_;
+
+ private:
+  ~MockVideoProcessorProxy() override = default;
 };
 
 class MockTexture2DWrapper : public Texture2DWrapper {
@@ -137,8 +140,8 @@ class D3D11CopyingTexture2DWrapperTest
     gpu_task_runner_ = task_environment_.GetMainThreadTaskRunner();
   }
 
-  std::unique_ptr<MockVideoProcessorProxy> ExpectProcessorProxy() {
-    auto result = std::make_unique<MockVideoProcessorProxy>();
+  scoped_refptr<MockVideoProcessorProxy> ExpectProcessorProxy() {
+    auto result = base::MakeRefCounted<MockVideoProcessorProxy>();
     ON_CALL(*result.get(), MockInit(_, _))
         .WillByDefault(Return(GetProcessorProxyInit()
                                   ? StatusCode::kOk
@@ -216,8 +219,7 @@ TEST_P(D3D11CopyingTexture2DWrapperTest,
   auto texture_wrapper = ExpectTextureWrapper();
   MockTexture2DWrapper* texture_wrapper_raw = texture_wrapper.get();
   auto wrapper = std::make_unique<CopyingTexture2DWrapper>(
-      size, std::move(texture_wrapper), std::move(processor), nullptr,
-      copy_color_space);
+      size, std::move(texture_wrapper), processor, nullptr, copy_color_space);
 
   // TODO: check |gpu_task_runner_|.
 
