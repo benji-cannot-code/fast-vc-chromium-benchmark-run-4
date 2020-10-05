c0fb3d04254ea88cb68b2bfd20326e457d7c9fe4
Tom Anderson <thomasanderson@chromium.org>
2020-10-05 20:00:48 +0000

    [CRD] Fix crash when connecting to a Linux host
    base::FileDescriptorWatcher is not usable on the main thread because it
    is initialized as a UI thread, when an IO thread is required.  Since
    it's a UI thread, it's able to dispatch events already without special
    FD watching.  All that's required is creating an X11EventSource.  This
    CL creates the event source where necessary so that
    KeyboardLayoutMonitorLinux can dispatch events.
    
    R=lambroslambrou
    CC=rkjnsn@chromium.org
    
    Fixes: 1133481
    Change-Id: I6e0a380c7c1168c4b1515be10ec34e67d0ae0d47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2444530
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813864}

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -121,9 +121,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_LINUX) || defined(OS_CHROMEOS)
 #include <gtk/gtk.h>
+
 #include "base/linux_util.h"
 #include "remoting/host/audio_capturer_linux.h"
 #include "remoting/host/linux/certificate_watcher.h"
+#include "ui/events/platform/x11/x11_event_source.h"
 #include "ui/gfx/x/x11.h"
 #endif  // defined(OS_LINUX) || defined(OS_CHROMEOS)
 
@@ -1687,10 +1689,15 @@ int HostProcessMain() {
   HOST_LOG << "Starting host process: version " << STRINGIZE(VERSION);
 
 #if defined(OS_LINUX) || defined(OS_CHROMEOS)
+  std::unique_ptr<ui::X11EventSource> event_source;
   if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
           kReportOfflineReasonSwitchName)) {
     // Required in order for us to run multiple X11 threads.
     XInitThreads();
+
+    // Create an X11EventSource so the global X11 connection
+    // (x11::Connection::Get()) can dispatch X events.
+    event_source = std::make_unique<ui::X11EventSource>(x11::Connection::Get());
 
     // Required for any calls into GTK functions, such as the Disconnect and
     // Continue windows, though these should not be used for the Me2Me case
--- remoting/test/it2me_standalone_host_main.cc
+++ remoting/test/it2me_standalone_host_main.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gtk/gtk.h>
 
 #include "base/linux_util.h"
+#include "ui/events/platform/x11/x11_event_source.h"
 #include "ui/gfx/x/x11.h"
 #endif  // defined(OS_LINUX) || defined(OS_CHROMEOS)
 
@@ -25,6 +26,11 @@ int main(int argc, const char** argv) {
 #if defined(OS_LINUX) || defined(OS_CHROMEOS)
   // Required in order for us to run multiple X11 threads.
   XInitThreads();
+
+  // Create an X11EventSource so the global X11 connection
+  // (x11::Connection::Get()) can dispatch X events.
+  auto event_source =
+      std::make_unique<ui::X11EventSource>(x11::Connection::Get());
 
   // Required for any calls into GTK functions, such as the Disconnect and
   // Continue windows. Calling with nullptr arguments because we don't have
