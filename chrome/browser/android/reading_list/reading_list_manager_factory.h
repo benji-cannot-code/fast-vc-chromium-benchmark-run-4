013df5422a7bf218ab8ff67a5679769aede7cce2
Xing Liu <xingliu@chromium.org>
2020-10-05 20:45:58 +0000

    Read later: Use BrowserContextKeyedServiceFactory for Android glue.
    ReadingListManagerFactory should be BrowserContextKeyedServiceFactory
    instead of SimpleKeyedServiceFactory, since reduced mode won't work
    for sync and ReadingListModel.
    
    Also we build components/reading_list to Android APK, which causes
    android binary size increase.
    
    Bug: 1128074
    Binary-Size: Size increase is unavoidable (see above).
    Change-Id: If5393fa5dfb133b35c1ff1b17bf709f31f8815ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2441204
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813899}

--- chrome/browser/android/reading_list/reading_list_manager_factory.h
+++ chrome/browser/android/reading_list/reading_list_manager_factory.h
@@ -7,15 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ANDROID_READING_LIST_READING_LIST_MANAGER_FACTORY_H_
 
 #include "base/memory/singleton.h"
-#include "components/keyed_service/core/simple_keyed_service_factory.h"
+#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 
 class ReadingListManager;
 
 // A factory to create the ReadingListManager singleton.
-class ReadingListManagerFactory : public SimpleKeyedServiceFactory {
+class ReadingListManagerFactory : public BrowserContextKeyedServiceFactory {
  public:
   static ReadingListManagerFactory* GetInstance();
-  static ReadingListManager* GetForKey(SimpleFactoryKey* key);
+  static ReadingListManager* GetForBrowserContext(
+      content::BrowserContext* context);
 
  private:
   friend struct base::DefaultSingletonTraits<ReadingListManagerFactory>;
@@ -27,8 +28,8 @@ class ReadingListManagerFactory : public SimpleKeyedServiceFactory {
   ReadingListManagerFactory& operator=(const ReadingListManagerFactory&) =
       delete;
 
-  std::unique_ptr<KeyedService> BuildServiceInstanceFor(
-      SimpleFactoryKey* key) const override;
+  KeyedService* BuildServiceInstanceFor(
+      content::BrowserContext* context) const override;
 };
 
 #endif  // CHROME_BROWSER_ANDROID_READING_LIST_READING_LIST_MANAGER_FACTORY_H_
--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -106,6 +106,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "chrome/browser/android/explore_sites/explore_sites_service_factory.h"
+#include "chrome/browser/android/reading_list/reading_list_manager_factory.h"
 #include "chrome/browser/android/search_permissions/search_permissions_service.h"
 #include "chrome/browser/android/thin_webview/chrome_thin_webview_initializer.h"
 #include "chrome/browser/media/android/cdm/media_drm_origin_id_manager_factory.h"
@@ -343,10 +344,14 @@ void ChromeBrowserMainExtraPartsProfiles::
   ProfileThemeUpdateServiceFactory::GetInstance();
 #endif
   ProtocolHandlerRegistryFactory::GetInstance();
-#if !defined(OS_ANDROID)
-  if (base::FeatureList::IsEnabled(features::kReadLater))
+  if (base::FeatureList::IsEnabled(features::kReadLater)) {
     ReadingListModelFactory::GetInstance();
+
+#if defined(OS_ANDROID)
+    ReadingListManagerFactory::GetInstance();
 #endif
+  }
+
   RendererUpdaterFactory::GetInstance();
 #if !defined(OS_ANDROID)
   performance_manager::SiteDataCacheFacadeFactory::GetInstance();
