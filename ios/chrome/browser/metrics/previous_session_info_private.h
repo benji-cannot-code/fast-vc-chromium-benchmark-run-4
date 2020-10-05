8a1d7101bb3eabf05f63a34399df78db35858ee5
Eugene But <eugenebut@chromium.org>
2020-10-05 19:05:27 +0000

    [ios] Add some more key-values to synthetic crash report
    This CL adds the following key-values:
      - OSVersion (string)
      - in-background (bool)
      - session restoration (bool)
    
    Bug: 1103752
    Change-Id: Ib9217d451bc4684a819e9f07fa968cf42792e586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2444389
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813834}

--- ios/chrome/browser/metrics/previous_session_info_private.h
+++ ios/chrome/browser/metrics/previous_session_info_private.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, assign)
     previous_session_info_constants::DeviceBatteryState deviceBatteryState;
 @property(nonatomic, assign) BOOL OSRestartedAfterPreviousSession;
+@property(nonatomic, copy) NSString* OSVersion;
+@property(nonatomic, assign) BOOL terminatedDuringSessionRestoration;
 @property(nonatomic, strong) NSMutableSet<NSString*>* connectedSceneSessionsIDs;
 
 + (void)resetSharedInstanceForTesting;
