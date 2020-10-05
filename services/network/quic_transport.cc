a4848b6f8d6136a58aa354f363e7aad8ea276fd6
Renjie Tang <renjietang@chromium.org>
2020-10-05 05:11:01 +0000

    Use a more error-proof way to send RESET_STREAM in QUIC transport.
    Bug: 1123769
    Change-Id: I6fb06abfee7868ae4b951f70d80305eaf6755b78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2446198
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Victor Vasiliev <vasilvv@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Auto-Submit: Renjie Tang <renjietang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813622}

--- services/network/quic_transport.cc
+++ services/network/quic_transport.cc
@@ -150,11 +150,11 @@ class QuicTransport::Stream final {
   }
 
   ~Stream() {
-    if (!incoming_ && !outgoing_) {
+    auto* stream = incoming_ ? incoming_ : outgoing_;
+    if (!stream) {
       return;
     }
-    transport_->transport_->session()->ResetStream(
-        id_, quic::QuicRstStreamErrorCode::QUIC_STREAM_CANCELLED);
+    stream->Reset(quic::QuicRstStreamErrorCode::QUIC_STREAM_CANCELLED);
   }
 
  private:
@@ -517,15 +517,13 @@ void QuicTransport::OnIncomingBidirectionalStreamAvailable() {
         sizeof(options), MOJO_CREATE_DATA_PIPE_FLAG_NONE, 1, 256 * 1024};
     if (mojo::CreateDataPipe(&options, &writable_for_outgoing,
                              &readable_for_outgoing) != MOJO_RESULT_OK) {
-      transport_->session()->ResetStream(
-          stream->id(), quic::QuicRstStreamErrorCode::QUIC_STREAM_CANCELLED);
+      stream->Reset(quic::QuicRstStreamErrorCode::QUIC_STREAM_CANCELLED);
       // TODO(yhirano): Error the entire connection.
       return;
     }
     if (mojo::CreateDataPipe(&options, &writable_for_incoming,
                              &readable_for_incoming) != MOJO_RESULT_OK) {
-      transport_->session()->ResetStream(
-          stream->id(), quic::QuicRstStreamErrorCode::QUIC_STREAM_CANCELLED);
+      stream->Reset(quic::QuicRstStreamErrorCode::QUIC_STREAM_CANCELLED);
       // TODO(yhirano): Error the entire connection.
       return;
     }
@@ -560,8 +558,7 @@ void QuicTransport::OnIncomingUnidirectionalStreamAvailable() {
         sizeof(options), MOJO_CREATE_DATA_PIPE_FLAG_NONE, 1, 256 * 1024};
     if (mojo::CreateDataPipe(&options, &writable_for_incoming,
                              &readable_for_incoming) != MOJO_RESULT_OK) {
-      transport_->session()->ResetStream(
-          stream->id(), quic::QuicRstStreamErrorCode::QUIC_STREAM_CANCELLED);
+      stream->Reset(quic::QuicRstStreamErrorCode::QUIC_STREAM_CANCELLED);
       // TODO(yhirano): Error the entire connection.
       return;
     }
