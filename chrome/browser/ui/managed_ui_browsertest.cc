df6635746e2ff6a2d9eb82a1174471c9287a77c9
Brian Malcolm <bmalcolm@chromium.org>
2020-10-05 18:53:32 +0000

    GetDeviceManagedUiWebUILabel:display device info rather than user info
    This is used as a header in the CROS device settings page.
    
    BUG=chromium:1081272
    
    Change-Id: I20995464c23df17911ed5b425d0dfdb221434aca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2446535
    Commit-Queue: Brian Malcolm <bmalcolm@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813829}

--- chrome/browser/ui/managed_ui_browsertest.cc
+++ chrome/browser/ui/managed_ui_browsertest.cc
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/policy/device_policy_cros_browser_test.h"
+#endif
+
 class ManagedUiTest : public InProcessBrowserTest {
  public:
   ManagedUiTest() = default;
@@ -88,14 +92,14 @@ IN_PROC_BROWSER_TEST_F(ManagedUiTest, GetManagedUiWebUILabel) {
           "Your <a href=\"chrome://management\">browser is managed</a> by "
           "example.com"),
       chrome::GetManagedUiWebUILabel(profile_with_domain.get()));
+}
+
 #if defined(OS_CHROMEOS)
-  EXPECT_EQ(base::ASCIIToUTF16("Your <a target=\"_blank\" "
-                               "href=\"chrome://management\">Chrome device is "
-                               "managed</a> by your organization"),
-            chrome::GetDeviceManagedUiWebUILabel(profile.get()));
+using ManagedUiTestCros = policy::DevicePolicyCrosBrowserTest;
+IN_PROC_BROWSER_TEST_F(ManagedUiTestCros, GetManagedUiWebUILabel) {
   EXPECT_EQ(base::ASCIIToUTF16("Your <a target=\"_blank\" "
                                "href=\"chrome://management\">Chrome device is "
                                "managed</a> by example.com"),
-            chrome::GetDeviceManagedUiWebUILabel(profile_with_domain.get()));
-#endif
+            chrome::GetDeviceManagedUiWebUILabel());
 }
+#endif
