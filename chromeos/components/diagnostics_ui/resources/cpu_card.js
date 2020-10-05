8caca578466fa926f01487d7f8b3d698da7280a9
Joon Ahn <joonbug@chromium.org>
2020-10-05 23:04:16 +0000

    Diagnostics: Wire realtime cpu chart into cpu card
    https://screenshot.googleplex.com/7fXF2GjLWEPcAbs
    
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I77af9ea199c2363636471d3b2b8b1ef739fdc1ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2441531
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813981}

--- chromeos/components/diagnostics_ui/resources/cpu_card.js
+++ chromeos/components/diagnostics_ui/resources/cpu_card.js
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './data_point.js';
 import './diagnostics_card.js';
 import './diagnostics_shared_css.js';
+import './realtime_cpu_chart.js';
 
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
 import {CpuUsage, SystemDataProviderInterface} from './diagnostics_types.js'
 import {getSystemDataProvider} from './mojo_interface_provider.js';
 import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
