1c138454187c4bc4e4629c1da622d71c1300963c
Solomon Kinard <solomonkinard@chromium.org>
2020-10-05 14:26:15 +0000

    [Extensions] Silence future manifest error with flag.
    $ chrome --allow-future-manifest-version
    
    Bug: None
    Change-Id: I99a4d23386b99f707e21ab221b62b6d51b7f6658
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434904
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#813731}

--- extensions/common/switches.h
+++ extensions/common/switches.h
@@ -33,6 +33,7 @@ extern const char kSetExtensionThrottleTestParams[];
 extern const char kShowComponentExtensionOptions[];
 extern const char kTraceAppSource[];
 extern const char kEnableCrxHashCheck[];
+extern const char kAllowFutureManifestVersion[];
 
 }  // namespace switches
 
