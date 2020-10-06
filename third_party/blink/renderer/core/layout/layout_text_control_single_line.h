a9fe54c968f150347293271fec42128eb94983f5
Kent Tamura <tkent@chromium.org>
2020-10-06 11:17:41 +0000

    TextControl NG: Remove LayoutTextControl::ComputeLogicalHeight() override
    Move the main logic of |LayoutTextControl::ComputeLogicalHeight()| to
    TextAreaIntrinsicBlockSize() and TextFieldIntrinsicBlockSize() in
    layout_box.cc like other form controls' intrinsic sizes in order to
    share the code with LayoutNG.
    
    * ClampIntrinsicBlockSize() and LayoutBox::ComputeLogicalHeight()
      <textarea>'s intrinsic size should not depend on scrollbar existence.
      So we need to subtract the scrollbar thickness if a scrollbar exists.
    
    This CL has no behavior changes.
    
    Bug: 1040826
    Change-Id: I1abdf34617364c7c30398d9dc20be43e3eae7da9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2452069
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814173}

--- third_party/blink/renderer/core/layout/layout_text_control_single_line.h
+++ third_party/blink/renderer/core/layout/layout_text_control_single_line.h
@@ -65,7 +65,6 @@ class LayoutTextControlSingleLine : public LayoutTextControl {
   LayoutUnit ScrollHeight() const final;
 
   int TextBlockWidth() const;
-  LayoutUnit ComputeControlLogicalHeight(LayoutUnit line_height) const override;
 
   void ComputeVisualOverflow(bool recompute_floats) override;
 
