a0355da5e73fa4327afc77326771cd99c143639a
Austin Eng <enga@chromium.org>
2020-10-06 15:43:56 +0000

    Update to use Dawn's dawn_native::vulkan::ExportVulkanImage API
    The new API allows passing/getting the old/new image layouts to
    do proper image layout transitions. ExportSignalSemaphoreOpaqueFD
    is deprecated.
    
    Bug: dawn:200, 976495
    Change-Id: Icbdd354bf5d8231c16b1bbc64317bcd158e0c135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2451710
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814221}

--- gpu/command_buffer/service/shared_image_representation_dawn_ozone.cc
+++ gpu/command_buffer/service/shared_image_representation_dawn_ozone.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <dawn_native/VulkanBackend.h>
 
 #include <vulkan/vulkan.h>
+#include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "gpu/command_buffer/service/memory_tracking.h"
@@ -70,7 +71,7 @@ WGPUTexture SharedImageRepresentationDawnOzone::BeginAccess(
 
   dawn_native::vulkan::ExternalImageDescriptorDmaBuf descriptor = {};
   descriptor.cTextureDescriptor = &texture_descriptor;
-  descriptor.isCleared = IsCleared();
+  descriptor.isInitialized = IsCleared();
   // Import the dma-buf into Dawn via the Vulkan backend. As per the Vulkan
   // documentation, importing memory from a file descriptor transfers
   // ownership of the fd from the application to the Vulkan implementation.
@@ -95,12 +96,19 @@ void SharedImageRepresentationDawnOzone::EndAccess() {
     return;
   }
 
-  if (dawn_native::IsTextureSubresourceInitialized(texture_, 0, 1, 0, 1)) {
-    SetCleared();
-  }
+  // Grab the signal semaphore from dawn
+  dawn_native::vulkan::ExternalImageExportInfoOpaqueFD export_info;
+  if (!dawn_native::vulkan::ExportVulkanImage(
+          texture_, VK_IMAGE_LAYOUT_UNDEFINED, &export_info)) {
+    DLOG(ERROR) << "Failed to export Dawn Vulkan image.";
+  } else {
+    if (export_info.isInitialized) {
+      SetCleared();
+    }
 
-  // TODO(hob): Synchronize access to the dma-buf by exporting the VkSemaphore
-  // from the WebGPU texture.
+    // TODO(hob): Synchronize access to the dma-buf by waiting on
+    // |export_info.semaphoreHandles|
+  }
   dawn_procs_->data.textureDestroy(texture_);
   dawn_procs_->data.textureRelease(texture_);
   texture_ = nullptr;
