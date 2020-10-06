f7ed3d073db252e25aa4c47cdb947a01a409195d
Lu Huang <luhua@microsoft.com>
2020-10-06 18:40:26 +0000

    Add a blink feature flag to control web app URL handling features.
    design doc: https://docs.google.com/document/d/19dGklalQTRtRrG3PKrVbDRmYPLHGLnsGEwUaed7sAFQ/edit?usp=sharing
    
    design overview cl: https://chromium-review.googlesource.com/c/chromium/src/+/2393252
    
    Chrome status: https://www.chromestatus.com/feature/5739732661174272
    
    Bug: 1072058
    Change-Id: I6ef280af95514e0ac1abfacdcf6b113ee7b7af57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2444439
    Commit-Queue: Lu Huang <luhua@microsoft.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814308}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -310,6 +310,8 @@ BLINK_COMMON_EXPORT extern const base::Feature kPreferCompositingToLCDText;
 
 BLINK_COMMON_EXPORT extern const base::Feature
     kLogUnexpectedIPCPostedToBackForwardCachedDocuments;
+
+BLINK_COMMON_EXPORT extern const base::Feature kWebAppEnableUrlHandlers;
 }  // namespace features
 }  // namespace blink
 
