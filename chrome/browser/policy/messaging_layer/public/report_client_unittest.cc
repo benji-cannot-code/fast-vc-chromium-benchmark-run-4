500a4b3c604cb69121d39e1bda68aadd29f1c15a
Leonid Baraz <lbaraz@chromium.org>
2020-10-06 00:31:57 +0000

    Remove Setup_test
    Bug: None
    Change-Id: Ie4a0bf883e97cfd2b412583151030312048628a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450922
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/master@{#814009}

--- chrome/browser/policy/messaging_layer/public/report_client_unittest.cc
+++ chrome/browser/policy/messaging_layer/public/report_client_unittest.cc
@@ -70,16 +70,15 @@ class ReportingClientTest : public testing::Test {
     user_manager_ = std::make_unique<user_manager::ScopedUserManager>(
         std::move(mock_user_manager));
 #endif  // OS_CHROMEOS
-
     // Provide a mock cloud policy client.
     auto client = std::make_unique<policy::MockCloudPolicyClient>();
     client->SetDMToken(
         policy::DMToken::CreateValidTokenForTesting("FAKE_DM_TOKEN").value());
-    ReportingClient::Setup_test(std::move(client));
+    test_reporting_ =
+        std::make_unique<ReportingClient::TestEnvironment>(std::move(client));
   }
 
   void TearDown() override {
-    ReportingClient::Reset_test();
 #ifdef OS_CHROMEOS
     user_manager_.reset();
     profile_.reset();
@@ -88,6 +87,7 @@ class ReportingClientTest : public testing::Test {
 
  protected:
   content::BrowserTaskEnvironment task_envrionment_;
+  std::unique_ptr<ReportingClient::TestEnvironment> test_reporting_;
 #ifdef OS_CHROMEOS
   std::unique_ptr<TestingProfile> profile_;
   std::unique_ptr<user_manager::ScopedUserManager> user_manager_;
