3db9fa61d02c7d779ac044adb13d54981d42e279
Dan Harrington <harringtond@chromium.org>
2020-10-06 17:45:53 +0000

    Some fixes for when building without feed v1
    For context, enable_feed_v1=false removes the v1 version of the NTP
    feed from the build. This CL fixes various problems with that
    configuration:
    
    * fixed some tests
    * fixed some unused resource errors
    * some tests were more difficult to fix, i've made some
      assertions conditional. Instant start isn't yet implemented
      for v2, so it's likely these require more than test updates.
    
    Bug: 1129187
    Change-Id: If1cfbdd9e596c61c082565279a545326f1dba244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2441958
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814276}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/cards/promo/HomepagePromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/cards/promo/HomepagePromoTest.java
@@ -28,6 +28,7 @@ import androidx.test.espresso.action.GeneralLocation;
 import androidx.test.espresso.action.GeneralSwipeAction;
 import androidx.test.espresso.action.Press;
 import androidx.test.espresso.action.Swipe;
+import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.filters.MediumTest;
 import androidx.test.filters.SmallTest;
@@ -49,6 +50,7 @@ import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
+import org.chromium.chrome.browser.feed.shared.FeedFeatures;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.homepage.HomepageManager;
@@ -263,6 +265,9 @@ public class HomepagePromoTest {
     @SmallTest
     @Features.DisableFeatures({ChromeFeatureList.INTEREST_FEED_V2})
     public void testToggleFeed_WithSignIn() {
+        if (FeedFeatures.isV2Enabled()) {
+            return; // FeedV1 must not be available.
+        }
         // Test to toggle stream when HomepagePromo is hide. Toggle feed should hide promo still.
         launchNewTabPage();
 
@@ -531,16 +536,29 @@ public class HomepagePromoTest {
     }
 
     private void toggleFeedHeader(int feedHeaderPosition, ViewGroup rootView, boolean expanded) {
-        onView(instanceOf(RecyclerView.class))
-                .perform(RecyclerViewActions.scrollToPosition(feedHeaderPosition),
-                        RecyclerViewActions.actionOnItemAtPosition(feedHeaderPosition, click()));
+        if (FeedFeatures.isV2Enabled()) {
+            onView(allOf(instanceOf(RecyclerView.class), withId(R.id.feed_stream_recycler_view)))
+                    .perform(RecyclerViewActions.scrollToPosition(feedHeaderPosition));
+            onView(withId(R.id.header_menu)).perform(click());
 
-        // Scroll to the same position in case the refresh brings the section header out of the
-        // screen.
-        onView(instanceOf(RecyclerView.class))
-                .perform(RecyclerViewActions.scrollToPosition(feedHeaderPosition));
-        waitForView(rootView,
-                allOf(withId(R.id.header_status),
-                        withText(expanded ? R.string.hide_content : R.string.show_content)));
+            onView(withText(expanded ? R.string.ntp_turn_on_feed : R.string.ntp_turn_off_feed))
+                    .perform(click());
+
+            onView(withText(expanded ? R.string.ntp_discover_on : R.string.ntp_discover_off))
+                    .check(ViewAssertions.matches(isDisplayed()));
+        } else {
+            onView(instanceOf(RecyclerView.class))
+                    .perform(RecyclerViewActions.scrollToPosition(feedHeaderPosition),
+                            RecyclerViewActions.actionOnItemAtPosition(
+                                    feedHeaderPosition, click()));
+
+            // Scroll to the same position in case the refresh brings the section header out of
+            // the screen.
+            onView(instanceOf(RecyclerView.class))
+                    .perform(RecyclerViewActions.scrollToPosition(feedHeaderPosition));
+            waitForView(rootView,
+                    allOf(withId(R.id.header_status),
+                            withText(expanded ? R.string.hide_content : R.string.show_content)));
+        }
     }
 }
