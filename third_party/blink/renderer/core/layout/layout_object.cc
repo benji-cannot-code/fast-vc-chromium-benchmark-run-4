bde66651070905958dbf9fbe3bc345010fa83a13
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-10-06 20:06:02 +0000

    [FlexNG] Move "-webkit-box" (non line-clamp) over to FlexNG.
    As above. Previously this was handled by LayoutFlexibleBox.
    
    Bug: 845235
    Change-Id: Id4899b487c92b723341c8251601f317abb6618c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2451732
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814361}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -273,7 +273,8 @@ LayoutObject* LayoutObject::CreateObject(Element* element,
         return LayoutObjectFactory::CreateBlockForLineClamp(*element, style,
                                                             legacy);
       }
-
+      if (RuntimeEnabledFeatures::LayoutNGWebkitBoxEnabled())
+        return LayoutObjectFactory::CreateFlexibleBox(*element, style, legacy);
       UseCounter::Count(element->GetDocument(),
                         WebFeature::kLegacyLayoutByFlexBox);
       return new LayoutFlexibleBox(element);
