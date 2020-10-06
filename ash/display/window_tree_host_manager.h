dfc38141838b00326ee105fbaf8b540c16aba172
Malay Keshav <malaykeshav@chromium.org>
2020-10-06 01:20:59 +0000

    Ash: Add UMA to measure primary display's effective resolution
    Adds an UMA metric that tracks the effective resolution of the primary
    display. To ensure the metric being measured is one that the user is to
    spends most time on, the signal is sent at every fixed interval of
    30 minutes.
    
    Effective resolution takes into account the display zoom, device scale
    factor, and the display rotation.
    
    Bug: 1126489
    Test: Tested manually on atlas and krane
    Change-Id: Id0f92867c6927f06f0c5c2288198f5bff3a5b8b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412837
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    Auto-Submit: Malay Keshav <malaykeshav@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814037}

--- ash/display/window_tree_host_manager.h
+++ ash/display/window_tree_host_manager.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host_observer.h"
 #include "ui/base/ime/input_method.h"
@@ -205,6 +206,10 @@ class ASH_EXPORT WindowTreeHostManager
   // Stores the cursor's display. The id is used to determine whether the mouse
   // should be moved after a display configuration change.
   int64_t cursor_display_id_for_restore_;
+
+  // A repeating timer to trigger sending UMA metrics for primary display's
+  // effective resolution at fixed intervals.
+  std::unique_ptr<base::RepeatingTimer> effective_resolution_UMA_timer_;
 
   base::WeakPtrFactory<WindowTreeHostManager> weak_ptr_factory_{this};
 
