cf8542a9184e296f1be0d0b4a4cf9b469963b6ce
Alexander Bolodurin <alexbn@google.com>
2020-10-06 09:05:35 +0000

    [filesapp] Split private getThumbnail API into per-type variants
    Split fileManagerPrivate.getThumbnail() into getDriveThumbnail() and
    getPdfThumbnail() to allow implementation-specific arguments: PDF
    thumbnailer supports size arguments, Drive thumbnailer only supports
    crop and, in future, others that may have their own (and different)
    argument needs.
    
    For getPdfThumbnail(), pass through size arguments from the image loader
    request to the PDF thumbnailer and pull up default values from C++ to
    the image loader JS code, to make them usable in other thumbnailers that
    take explicit dimension arguments.
    
    Bug: 1049966
    Change-Id: Ia17a32028a7fb510c1755c9fa0283f9a37978331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434013
    Commit-Queue: Alexander Bolodurin <alexbn@google.com>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814141}

--- chrome/browser/chromeos/extensions/file_manager/file_manager_private_api_functions.h
+++ chrome/browser/chromeos/extensions/file_manager/file_manager_private_api_functions.h
@@ -20,5 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/extensions/file_manager/private_api_sharesheet.h"
 #include "chrome/browser/chromeos/extensions/file_manager/private_api_strings.h"
 #include "chrome/browser/chromeos/extensions/file_manager/private_api_tasks.h"
+#include "chrome/browser/chromeos/extensions/file_manager/private_api_thumbnail.h"
 
 #endif  // CHROME_BROWSER_CHROMEOS_EXTENSIONS_FILE_MANAGER_FILE_MANAGER_PRIVATE_API_FUNCTIONS_H_
