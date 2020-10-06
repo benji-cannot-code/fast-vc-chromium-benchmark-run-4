744dcbd99dc5aa732fab3b0b917f750c93c87169
Matt Wolenetz <wolenetz@chromium.org>
2020-10-06 19:06:01 +0000

    MSE-in-Workers: Remove direct usage of media element from MSE
    Replaces remaining direct usage of the attached HTMLMediaElement from
    MediaSource and SourceBuffer, relying instead on the attachment to
    moderate that communication.
    
    Specifically, this change:
    
    * Adds attachment helpers to enable SourceBuffers to create their audio
      and video tracklist members, including a same-thread attachment
      implementation. Cross-thread attachment implementation of this is
      deferred until TrackListBase and TrackBase can be owned off the main
      thread (in a worker thread), and until they can function without
      themselves directly accessing an HTMLMediaElement.
    
    * Removes superfluous usage in SourceBuffer of `source_->MediaElement()`
      in DCHECKs; other DCHECKs already suffice.
    
    * Removes MediaSource::MediaElement() and `attached_element_`. Removes
      their superfluous implementation and usage from MediaSource.
    
    * Fixes outdated assumptions in various DCHECKs that all attachments use
      a tracer. Only same-thread attachments use a tracer.
    
    BUG=878133
    
    Change-Id: I346b320804a0ffbf731788b7ea7c344588f660b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2439677
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Will Cassella <cassew@google.com>
    Cr-Commit-Position: refs/heads/master@{#814329}

--- third_party/blink/renderer/modules/mediasource/media_source_attachment_supplement.h
+++ third_party/blink/renderer/modules/mediasource/media_source_attachment_supplement.h
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class AudioTrackList;
 class MediaSource;
+class VideoTrackList;
 
 // Modules-specific common extension of the core MediaSourceAttachment
 // interface. Includes extra interface methods used by concrete attachments to
@@ -47,6 +49,13 @@ class MediaSourceAttachmentSupplement : public MediaSourceAttachment {
   // rely on the element to correctly pump when it has an error to this
   // attachment (in a cross-thread implementation).
   virtual bool GetElementError(MediaSourceTracer* tracer) = 0;
+
+  // Construct track lists for use by a SourceBuffer.
+  // TODO(https://crbug.com/878133): Update these to support worker-owned
+  // SourceBuffer lists when completing AudioVideoTracks feature support with
+  // MSE-in-Workers.
+  virtual AudioTrackList* CreateAudioTrackList(MediaSourceTracer* tracer) = 0;
+  virtual VideoTrackList* CreateVideoTrackList(MediaSourceTracer* tracer) = 0;
 
   // Add/Remove tracks to/from the media Element's audioTracks() or
   // videoTracks() list. Note that this is synchronous in
