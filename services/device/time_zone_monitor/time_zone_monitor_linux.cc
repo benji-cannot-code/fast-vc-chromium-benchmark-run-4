1ab1057db12bb84309e51d68d796e82182eb8875
Sylvain Defresne <sdefresne@chromium.org>
2020-10-06 10:47:02 +0000

    Clean up deprecated sources assignment filter in //services
    Remove deprecated_default_sources_assignment_filter and replace
    it with explicit is_win, is_mac, etc. sections.
    
    Bug: 1018739
    Change-Id: If9e6ec1dc94c9b8d1c5ca7b83f4f4e8368f6c71d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2442796
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814167}

--- services/device/time_zone_monitor/time_zone_monitor_linux.cc
+++ services/device/time_zone_monitor/time_zone_monitor_linux.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#if !defined(OS_CHROMEOS)
-
 #include "services/device/time_zone_monitor/time_zone_monitor.h"
 
 #include <stddef.h>
@@ -204,5 +202,3 @@ std::unique_ptr<TimeZoneMonitor> TimeZoneMonitor::Create(
 }
 
 }  // namespace device
-
-#endif  // !OS_CHROMEOS
