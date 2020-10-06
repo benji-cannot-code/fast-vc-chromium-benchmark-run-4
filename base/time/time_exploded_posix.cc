f60df61cd131af5fa2f977fbb5d18cd7ed19eb9f
Sean Topping <seantopping@chromium.org>
2020-10-06 23:03:39 +0000

    [Chromecast] Don't add icui18n dep for Cast
    This dep increases the OTA size by 600kB, which exceeds the size
    allowance for debug builds on some devices. Cast likely needs a
    different mitigation for Y2038 as a proper long-term fix.
    
    Bug: internal b/167763382
    Test: CQ
    Change-Id: I71d1834be09e070a6816731b17e0ae8fb7831676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2399452
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814440}

--- base/time/time_exploded_posix.cc
+++ base/time/time_exploded_posix.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_math.h"
 #include "base/synchronization/lock.h"
 #include "build/build_config.h"
+#include "build/chromecast_buildflags.h"
 
 #if defined(OS_NACL)
 #include "base/os_compat_nacl.h"
@@ -127,8 +128,12 @@ void Time::Explode(bool is_local, Exploded* exploded) const {
 
   // For systems with a Y2038 problem, use ICU as the Explode() implementation.
   if (sizeof(SysTime) < 8) {
+// TODO(b/167763382) Find an alternate solution for Chromecast devices, since
+// adding the icui18n dep significantly increases the binary size.
+#if !BUILDFLAG(IS_CHROMECAST)
     ExplodeUsingIcu(millis_since_unix_epoch, is_local, exploded);
     return;
+#endif  // !BUILDFLAG(IS_CHROMECAST)
   }
 
   // Split the |millis_since_unix_epoch| into separate seconds and millisecond
