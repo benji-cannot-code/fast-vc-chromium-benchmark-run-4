5497b1ec45550726e7850a7efa956ebdc806bab9
Ben Reich <benreich@chromium.org>
2020-10-06 01:02:12 +0000

    [Files App] Remove browser_test prefix and order test BUILD file
    Renames the browser_test_devtools_listener class to remove the
    browser_test prefix and updates the naming in the places that
    refer to it.
    
    Bug: 1113941
    Change-Id: I4fe0827f5c332c8650367bff39eb144725723b4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2449940
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814027}

--- chrome/browser/chromeos/file_manager/devtools_listener.cc
+++ chrome/browser/chromeos/file_manager/devtools_listener.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/file_manager/browser_test_devtools_listener.h"
+#include "chrome/browser/chromeos/file_manager/devtools_listener.h"
 
 #include <stddef.h>
 #include <map>
