43c9af4f09bb32361544bad8fae761a0d04fde52
John Abd-El-Malek <jam@chromium.org>
2020-10-06 04:56:46 +0000

    Always propagate user_gesture for browser initiated requests
    This was propagated only on Android and not desktop because of tests that inadvertently depended on this.
    -history test said it wanted no user gesture, but ui_test_utils::NavigateToURL always enabled it
    -dice code was setting user gesture but this wasn't propagated so it wasn't necessary
    -download tests: I'm unsure if the tests were correct or if there are bugs to fix in the production code. For now setting user_gesture to false to simulate old behavior before this change
    -signin_view_controller: no-op change since this is a browser initiated navigation so the user_gesture was always ignored
    
    Fixes: 
    Bug: 1132491
    Change-Id: Ib3486090d4d18a66e0e76f9135b45a7abcbdaf1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2430451
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814091}

--- chrome/browser/download/download_browsertest.cc
+++ chrome/browser/download/download_browsertest.cc
@@ -2338,7 +2338,10 @@ IN_PROC_BROWSER_TEST_F(DownloadTest, CrxDenyInstall) {
       DangerousDownloadWaiter(
           browser(), 1,
           content::DownloadTestObserver::ON_DANGEROUS_DOWNLOAD_DENY));
-  ui_test_utils::NavigateToURL(browser(), extension_url);
+
+  NavigateParams params(browser(), extension_url, ui::PAGE_TRANSITION_TYPED);
+  params.user_gesture = false;
+  ui_test_utils::NavigateToURL(&params);
 
   observer->WaitForFinished();
   EXPECT_EQ(1u, observer->NumDownloadsSeenInState(DownloadItem::CANCELLED));
@@ -2369,7 +2372,9 @@ IN_PROC_BROWSER_TEST_F(DownloadTest, CrxInstallDenysPermissions) {
       DangerousDownloadWaiter(
           browser(), 1,
           content::DownloadTestObserver::ON_DANGEROUS_DOWNLOAD_ACCEPT));
-  ui_test_utils::NavigateToURL(browser(), extension_url);
+  NavigateParams params(browser(), extension_url, ui::PAGE_TRANSITION_TYPED);
+  params.user_gesture = false;
+  ui_test_utils::NavigateToURL(&params);
 
   observer->WaitForFinished();
   EXPECT_EQ(1u, observer->NumDownloadsSeenInState(DownloadItem::COMPLETE));
@@ -2409,7 +2414,9 @@ IN_PROC_BROWSER_TEST_F(DownloadTest, CrxInstallAcceptPermissions) {
       DangerousDownloadWaiter(
           browser(), 1,
           content::DownloadTestObserver::ON_DANGEROUS_DOWNLOAD_ACCEPT));
-  ui_test_utils::NavigateToURL(browser(), extension_url);
+  NavigateParams params(browser(), extension_url, ui::PAGE_TRANSITION_TYPED);
+  params.user_gesture = false;
+  ui_test_utils::NavigateToURL(&params);
 
   observer->WaitForFinished();
   EXPECT_EQ(1u, observer->NumDownloadsSeenInState(DownloadItem::COMPLETE));
@@ -2477,7 +2484,9 @@ IN_PROC_BROWSER_TEST_F(DownloadTest, CrxLargeTheme) {
       DangerousDownloadWaiter(
           browser(), 1,
           content::DownloadTestObserver::ON_DANGEROUS_DOWNLOAD_ACCEPT));
-  ui_test_utils::NavigateToURL(browser(), extension_url);
+  NavigateParams params(browser(), extension_url, ui::PAGE_TRANSITION_TYPED);
+  params.user_gesture = false;
+  ui_test_utils::NavigateToURL(&params);
 
   observer->WaitForFinished();
   EXPECT_EQ(1u, observer->NumDownloadsSeenInState(DownloadItem::COMPLETE));
@@ -4959,7 +4968,9 @@ IN_PROC_BROWSER_TEST_F(DownloadTest, CrxDenyInstallClosesShelf) {
       DangerousDownloadWaiter(
           browser(), 1,
           content::DownloadTestObserver::ON_DANGEROUS_DOWNLOAD_DENY));
-  ui_test_utils::NavigateToURL(browser(), extension_url);
+  NavigateParams params(browser(), extension_url, ui::PAGE_TRANSITION_TYPED);
+  params.user_gesture = false;
+  ui_test_utils::NavigateToURL(&params);
 
   observer->WaitForFinished();
 
