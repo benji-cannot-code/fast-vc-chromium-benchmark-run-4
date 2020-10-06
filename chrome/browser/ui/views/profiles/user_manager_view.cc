495d4d33fd2f4fd1ca057bd1ecfd1514c004102d
Monica Basta <msalama@chromium.org>
2020-10-06 09:36:53 +0000

    [Signin]: Fix for crash in UserManagerProfileDialog::DisplayErrorMessage
    The code assumes if no browser exists for the profile where the signin
    is happening then the sign in error is related to the user manager. The
    previous assumption is not valid if the user decides to close the
    profile while the user manager or another profile is open.  This CL
    compares the profile path with |UserManager::GetSigninProfilePath| and
    only in case of equality uses
    |UserManagerProfileDialog::DisplayErrorMessage| to show the sign in
    error.
    
    Bug: 1132175
    Change-Id: Icccdc44d3927d8750bea9957120d7fef6677c4aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2435309
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: Monica Basta <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814147}

--- chrome/browser/ui/views/profiles/user_manager_view.cc
+++ chrome/browser/ui/views/profiles/user_manager_view.cc
@@ -267,6 +267,9 @@ void UserManager::AddOnUserManagerShownCallbackForTesting(
 
 // static
 base::FilePath UserManager::GetSigninProfilePath() {
+  if (!g_user_manager_view)
+    return base::FilePath();
+
   return g_user_manager_view->GetSigninProfilePath();
 }
 
