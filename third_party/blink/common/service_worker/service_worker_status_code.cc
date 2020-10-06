374f35e43acf8a2fdd0c271a23d1aea728e485e2
Kenichi Ishibashi <bashi@chromium.org>
2020-10-06 02:54:24 +0000

    service worker: Add error code for remote storage disconnection
    This CL:
    * Introduces an error code that indicates a mojo connection to the
      storage is disconnected. This won't happen yet but could happen in
      the future. It could happen due to a Storage Service crash after
      ServiceWorkerStorage is migrated into the Storage Service, and the
      Storage Service is hosted outside the browser process.
    * Updates GetStorageUsageForOrigin() to make sure that the callback of
      the method is always called. Currently it uses
      WrapCallbackWithDefaultInvokeIfNotRun() but we may use the different
      approach in the future.
      Other methods in ServiceWorkerRegistry also need to be updated.
      Subsequent CLs will do that.
    * Sets a mojo disconnection handler. Currently it just resets the
      remote. Subsequent CLs will implement crash recovery logic.
    
    See [1] for the overall plan.
    
    [1] https://docs.google.com/document/d/1RQgiuAZVbQZbYJeLOfmydBMwXTTfsL2RqZKx90yPT7U/edit?usp=sharing
    
    Bug: 1133143
    Change-Id: I28cef47d66c6f95b33d7a4a8b152e7d38d32e49f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2437082
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814063}

--- third_party/blink/common/service_worker/service_worker_status_code.cc
+++ third_party/blink/common/service_worker/service_worker_status_code.cc
@@ -52,6 +52,8 @@ const char* ServiceWorkerStatusToString(ServiceWorkerStatusCode status) {
       return "Worker disallowed";
     case ServiceWorkerStatusCode::kErrorInvalidArguments:
       return "Invalid arguments";
+    case ServiceWorkerStatusCode::kErrorStorageDisconnected:
+      return "Storage operation error";
   }
   NOTREACHED();
   return "";
