3179dc6000efd11dcb8f14ca77a480f76166da84
Sébastien Séguin-Gagnon <sebsg@chromium.org>
2020-10-06 14:10:59 +0000

    [SH] Add common shared-highlighting metrics file to use on all platforms
    Bug: 1135144
    Change-Id: I061f9282ce17775b537f10ca2f81917a0b2224d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2448055
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Commit-Queue: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814197}

---
+++ components/shared_highlighting/README.md
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Shared Highlighting
+
+Shared Highlighting is a feature allowing users to generate and share links to specific parts of a web page, and to open these links.
+When a Shared Highlighting link is opened, the page scrolls to and highlights the passage specified by the sender.
+
+This feature is being developed across platforms. This component hosts the common code shared by all platforms.
+For now, this mainly consists of metrics, but will encompass more parts of the feature in future milestones.
---
+++ components/shared_highlighting/core/common/shared_highlighting_metrics.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_SHARED_HIGHLIGHTING_METRICS_H_
+#define COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_SHARED_HIGHLIGHTING_METRICS_H_
+
+namespace shared_highlighting {
+
+// Update corresponding |LinkGenerationError| in enums.xml.
+enum LinkGenerationError {
+  kIncorrectSelector,
+  kNoRange,
+  kNoContext,
+  kContextExhausted,
+  kContextLimitReached,
+  kEmptySelection,
+
+  kMaxValue = kContextLimitReached
+};
+}  // namespace shared_highlighting
+
+#endif  // COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_SHARED_HIGHLIGHTING_METRICS_H_
