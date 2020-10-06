dec70d9572ae692b9c758c6c3bab05f03033c964
Elaine Chien <elainec@chromium.org>
2020-10-06 00:48:56 +0000

    Add View Builder support and metadata to reload button
    Bug: 1130078
    Change-Id: Ic4a7e8812d14e68c2221d49e4cb23937c1e64d1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2451293
    Commit-Queue: Elaine Chien <elainec@chromium.org>
    Auto-Submit: Elaine Chien <elainec@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814020}

--- chrome/browser/ui/views/toolbar/reload_button.h
+++ chrome/browser/ui/views/toolbar/reload_button.h
@@ -7,8 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_TOOLBAR_RELOAD_BUTTON_H_
 
 #include "base/timer/timer.h"
+#include "chrome/browser/ui/views/chrome_views_export.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "ui/base/models/simple_menu_model.h"
+#include "ui/views/metadata/metadata_header_macros.h"
+#include "ui/views/metadata/view_factory.h"
 
 class CommandUpdater;
 
@@ -25,10 +28,9 @@ class CommandUpdater;
 class ReloadButton : public ToolbarButton,
                      public ui::SimpleMenuModel::Delegate {
  public:
-  enum class Mode { kReload = 0, kStop };
+  METADATA_HEADER(ReloadButton);
 
-  // The button's class name.
-  static const char kViewClassName[];
+  enum class Mode { kReload = 0, kStop };
 
   explicit ReloadButton(CommandUpdater* command_updater);
   ReloadButton(const ReloadButton&) = delete;
@@ -40,8 +42,9 @@ class ReloadButton : public ToolbarButton,
   void ChangeMode(Mode mode, bool force);
   Mode visible_mode() const { return visible_mode_; }
 
-  // Enable reload drop-down menu.
-  void set_menu_enabled(bool enable) { menu_enabled_ = enable; }
+  // Gets/Sets whether reload drop-down menu is enabled.
+  bool GetMenuEnabled() const;
+  void SetMenuEnabled(bool enable);
 
   // views::View:
   void OnThemeChanged() override;
@@ -49,7 +52,6 @@ class ReloadButton : public ToolbarButton,
   // ToolbarButton:
   void OnMouseExited(const ui::MouseEvent& event) override;
   base::string16 GetTooltipText(const gfx::Point& p) const override;
-  const char* GetClassName() const override;
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
   bool ShouldShowMenu() override;
   void ShowDropDownMenu(ui::MenuSourceType source_type) override;
@@ -105,5 +107,9 @@ class ReloadButton : public ToolbarButton,
   // test code can tell whether we did so (as there may be no |browser_|).
   int testing_reload_count_ = 0;
 };
+
+BEGIN_VIEW_BUILDER(CHROME_VIEWS_EXPORT, ReloadButton, ToolbarButton)
+VIEW_BUILDER_PROPERTY(bool, MenuEnabled)
+END_VIEW_BUILDER(VIEWS_EXPORT, ReloadButton)
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TOOLBAR_RELOAD_BUTTON_H_
