302693eecf5e92257749469fba0f9b0f5f0ade68
Maksim Sisov <msisov@igalia.com>
2020-10-06 16:45:10 +0000

    ozone/x11: implement OzoneUIControlsTestHelper
    This is a follow up CL that implements OzoneUIControlsTestHelper in
    Ozone/X11. It basically reuses the X11UIControlsTestHelper that
    was extracted to ui/base/x in https://crrev.com/c/2445109
    
    Test: all interactive_ui_tests with Ozone/X11 pass
    Bug: 1134495
    Change-Id: Ic9c123c74a5f425b78b5a51f8f4fd452029c0971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2443850
    Commit-Queue: Maksim Sisov (GMT+3) <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814244}

--- ui/ozone/common/test/stub_ozone_ui_controls_test_helper.cc
+++ ui/ozone/common/test/stub_ozone_ui_controls_test_helper.cc
@@ -18,10 +18,6 @@ OzoneUIControlsTestHelper* PrintErrorAndReturnNullptr() {
 }
 }  // namespace
 
-OzoneUIControlsTestHelper* CreateOzoneUIControlsTestHelperX11() {
-  return PrintErrorAndReturnNullptr();
-}
-
 OzoneUIControlsTestHelper* CreateOzoneUIControlsTestHelperWayland() {
   return PrintErrorAndReturnNullptr();
 }
--- ui/ozone/common/test/stub_ozone_ui_controls_test_helper.h
+++ ui/ozone/common/test/stub_ozone_ui_controls_test_helper.h
@@ -10,7 +10,6 @@ namespace ui {
 
 class OzoneUIControlsTestHelper;
 
-OzoneUIControlsTestHelper* CreateOzoneUIControlsTestHelperX11();
 OzoneUIControlsTestHelper* CreateOzoneUIControlsTestHelperWayland();
 OzoneUIControlsTestHelper* CreateOzoneUIControlsTestHelperWindows();
 OzoneUIControlsTestHelper* CreateOzoneUIControlsTestHelperDrm();
