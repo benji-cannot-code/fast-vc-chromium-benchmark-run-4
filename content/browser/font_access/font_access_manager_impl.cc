d5ddfc7ef20b2f0a52ba5908eae28a77859c975f
Olivier Yiptong <oyiptong@chromium.org>
2020-10-06 20:36:45 +0000

    FontAccess: Make DidRequestPermission platform delegation more readable
    Minor refactor: DidRequestPermission is rendered more readable with an
    early exit in case there is no implementation.
    
    Change-Id: Icd06cd9e18162a3363138d830fe8e01e41c32a14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450839
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Auto-Submit: Olivier Yiptong <oyiptong@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814376}

--- content/browser/font_access/font_access_manager_impl.cc
+++ content/browser/font_access/font_access_manager_impl.cc
@@ -102,6 +102,11 @@ void FontAccessManagerImpl::EnumerateLocalFonts(
 void FontAccessManagerImpl::DidRequestPermission(
     EnumerateLocalFontsCallback callback,
     blink::mojom::PermissionStatus status) {
+#if !defined(PLATFORM_HAS_LOCAL_FONT_ENUMERATION_IMPL)
+  std::move(callback).Run(blink::mojom::FontEnumerationStatus::kUnimplemented,
+                          base::ReadOnlySharedMemoryRegion());
+  return;
+#else
   if (status != blink::mojom::PermissionStatus::GRANTED) {
     std::move(callback).Run(
         blink::mojom::FontEnumerationStatus::kPermissionDenied,
@@ -111,7 +116,6 @@ void FontAccessManagerImpl::DidRequestPermission(
 
 // Per-platform delegation for obtaining cached font enumeration data occurs
 // here, after the permission has been granted.
-#if defined(PLATFORM_HAS_LOCAL_FONT_ENUMERATION_IMPL)
   ipc_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(
                      [](EnumerateLocalFontsCallback callback,
@@ -121,9 +125,6 @@ void FontAccessManagerImpl::DidRequestPermission(
                                results_task_runner, std::move(callback));
                      },
                      std::move(callback), results_task_runner_));
-#else
-  std::move(callback).Run(blink::mojom::FontEnumerationStatus::kUnimplemented,
-                          base::ReadOnlySharedMemoryRegion());
 #endif
 }
 
