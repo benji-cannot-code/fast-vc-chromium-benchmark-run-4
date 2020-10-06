2216faf6b8045070b228ea6beb8665e2a1fa19e8
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2020-10-06 13:16:56 +0000

    [Sync] Destroy leveldb::DB on a file thread
    Previously, the LevelDB object was destroyed on the UI thread, but it
    in reality its destructor can do file IO. This wasn't causing problems,
    because the object is deleted late during teardown (after the UI
    thread gets a ScopedAllowBlocking).
    
    With this patch, delete the LevelDB object on a ThreadPool thread, so
    this fact is made more explicit and won't cause problems if we change
    profile teardown order.
    
    Bug: 1134321, 88586
    Change-Id: I81ede6325b616eb7baecb532379034f0efeea7e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2443710
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Auto-Submit: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814189}

--- components/sync/model_impl/model_type_store_backend.cc
+++ components/sync/model_impl/model_type_store_backend.cc
@@ -52,7 +52,9 @@ ModelTypeStoreBackend::CreateUninitialized() {
   return new ModelTypeStoreBackend(/*env=*/nullptr);
 }
 
-ModelTypeStoreBackend::~ModelTypeStoreBackend() {}
+ModelTypeStoreBackend::~ModelTypeStoreBackend() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
 
 base::Optional<ModelError> ModelTypeStoreBackend::Init(
     const base::FilePath& path) {
