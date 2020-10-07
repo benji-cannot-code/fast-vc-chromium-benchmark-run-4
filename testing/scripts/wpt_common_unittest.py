d64ff5c0ee8f6e6a6e9cff688cab5efa3901faaf
Stephen McGruer <smcgruer@chromium.org>
2020-10-07 22:51:43 +0000

    [wptrunner] Do not produce -actual.txt files for passes
    If we have a PASS result, it means one of two things:
    
      i. The test passed and has no baseline file.
      ii. The test failed, but matches its baseline file exactly.
    
    In either case, producing a diff is not interesting for the test author.
    
    Bug: None
    Change-Id: I3b81eacc958176b3b8ccea27ba10b8c445d3a1ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2446779
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Luke Z <lpz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814918}

--- testing/scripts/wpt_common_unittest.py
+++ testing/scripts/wpt_common_unittest.py
@@ -84,6 +84,9 @@ class BaseWptScriptAdapterTest(unittest.TestCase):
 
     def test_write_log_artifact(self):
         # Ensure that log artifacts are written to the correct location.
+
+        # We only generate an actual.txt if our actual wasn't PASS, so in this
+        # case we shouldn't write anything.
         json_dict = {
             'tests': {
                 'test.html': {
@@ -97,6 +100,20 @@ class BaseWptScriptAdapterTest(unittest.TestCase):
             },
             'path_delimiter': '/',
         }
+        self._create_json_output(json_dict)
+        self.wpt_adapter.do_post_test_run_tasks()
+        written_files = self.wpt_adapter.fs.written_files
+        actual_path = os.path.join("layout-test-results", "test-actual.txt")
+        diff_path = os.path.join("layout-test-results", "test-diff.txt")
+        pretty_diff_path = os.path.join("layout-test-results",
+                                        "test-pretty-diff.html")
+        assert actual_path not in written_files
+        assert diff_path not in written_files
+        assert pretty_diff_path not in written_files
+
+        # Now we change the outcome to be a FAIL, which should generate an
+        # actual.txt
+        json_dict['tests']['test.html']['actual'] = 'FAIL'
         self._create_json_output(json_dict)
         self.wpt_adapter.do_post_test_run_tasks()
         written_files = self.wpt_adapter.fs.written_files
@@ -206,7 +223,7 @@ class BaseWptScriptAdapterTest(unittest.TestCase):
             'tests': {
                 'test.html': {
                     'expected': 'PASS',
-                    'actual': 'PASS',
+                    'actual': 'FAIL',
                     'artifacts': {
                         'wpt_actual_status': ['OK'],
                         'log': ['test.html actual text'],
@@ -270,7 +287,7 @@ class BaseWptScriptAdapterTest(unittest.TestCase):
             'tests': {
                 'variant.html?foo=bar/abc': {
                     'expected': 'PASS',
-                    'actual': 'PASS',
+                    'actual': 'FAIL',
                     'artifacts': {
                         'wpt_actual_status': ['OK'],
                         'log': ['variant bar/abc actual text'],
@@ -324,7 +341,7 @@ class BaseWptScriptAdapterTest(unittest.TestCase):
             'tests': {
                 'dir/multiglob.https.any.worker.html': {
                     'expected': 'PASS',
-                    'actual': 'PASS',
+                    'actual': 'FAIL',
                     'artifacts': {
                         'wpt_actual_status': ['OK'],
                         'log': ['dir/multiglob worker actual text'],
