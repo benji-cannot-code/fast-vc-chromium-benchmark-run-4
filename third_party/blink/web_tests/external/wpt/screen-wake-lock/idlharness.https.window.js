1a03e3f4e0bc2ddb0866d3982aa307ea5e3a5bab
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-10-07 20:31:49 +0000

    Import wpt@90d5ebe5329b3918ba4908797e6211a3b099adaf
    Using wpt-import in Chromium 8f2546436e53c0e123f87e8aae23b59bea4bdde9.
    With Chromium commits locally applied on WPT:
    710f745e6b "Handle transferred channels in ConvolverNode"
    d267d95097 "Delete WebRTC-QUIC Blink Tests"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=lpz@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-payments-fyi-rel
    Change-Id: I8c5e1209ae73bdf9c2aa08ccd5ff547b63805352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2457549
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#814847}

--- third_party/blink/web_tests/external/wpt/screen-wake-lock/idlharness.https.window.js
+++ third_party/blink/web_tests/external/wpt/screen-wake-lock/idlharness.https.window.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 idl_test(
-  ['wake-lock'],
+  ['screen-wake-lock'],
   ['dom', 'html'],
   async idl_array => {
     idl_array.add_objects({ Navigator: ['navigator'] });
