1ebc726ae99a96cd4a7a8bbb2a0e63218ad6a806
zhaoyangli <zhaoyangli@chromium.org>
2020-10-07 23:27:30 +0000

    [iOS][test runner] Report test results to result sink.
    Report testId, status, expected, summaryHtml and artifacts to ResultSink.
    
    Bug: 1104247, 1132476
    Change-Id: I425290ab52bcab302de3ae215606b33d0af15692
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2430626
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Chan Li <chanli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814941}

--- ios/build/bots/scripts/standard_json_util.py
+++ ios/build/bots/scripts/standard_json_util.py
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 from collections import OrderedDict
 
+import result_sink_util
+
 LOGGER = logging.getLogger(__name__)
 
 
@@ -18,6 +20,7 @@ class StdJson():
     """
 
     self.tests = OrderedDict()
+    self.result_sink = result_sink_util.ResultSinkClient()
 
     if 'passed' in kwargs:
       self.mark_all_passed(kwargs['passed'])
@@ -27,7 +30,7 @@ class StdJson():
       self.mark_all_passed(kwargs['flaked'], flaky=True)
 
   def mark_passed(self, test, flaky=False):
-    """Set test as passed
+    """Sets test as passed
 
     Params:
       test (str): a test in format "{TestCase}/{testMethod}"
@@ -38,6 +41,10 @@ class StdJson():
     if not test:
       LOGGER.warn('Empty or None test name passed to standard_json_util')
       return
+
+    result_sink_test_result = result_sink_util.compose_test_result(
+        test, 'PASS', True)
+    self.result_sink.post(result_sink_test_result)
 
     if test in self.tests:
       self.tests[test]['actual'] = self.tests[test]['actual'] + " PASS"
@@ -50,19 +57,24 @@ class StdJson():
     self.tests[test].pop('is_unexpected', None)
 
   def mark_all_passed(self, tests, flaky=False):
-    """Mark all tests as PASS"""
+    """Marks all tests as PASS"""
     for test in tests:
       self.mark_passed(test, flaky)
 
-  def mark_failed(self, test):
-    """Set test(s) as failed.
+  def mark_failed(self, test, test_log=None):
+    """Sets test(s) as failed.
 
     Params:
       test (str): a test in format "{TestCase}/{testMethod}"
+      test_log (str): log of the specific test
     """
     if not test:
       LOGGER.warn('Empty or None test name passed to standard_json_util')
       return
+
+    result_sink_test_result = result_sink_util.compose_test_result(
+        test, 'FAIL', False, test_log=test_log)
+    self.result_sink.post(result_sink_test_result)
 
     if test in self.tests:
       self.tests[test]['actual'] = self.tests[test]['actual'] + " FAIL"
@@ -75,12 +87,12 @@ class StdJson():
       }
 
   def mark_all_failed(self, tests):
-    """Mark all tests as FAIL"""
+    """Marks all tests as FAIL"""
     for test in tests:
       self.mark_failed(test)
 
   def mark_skipped(self, test):
-    """Set test(s) as SKIP.
+    """Sets test(s) as expected SKIP.
 
     Params:
       test (str): a test in format "{TestCase}/{testMethod}"
@@ -88,6 +100,10 @@ class StdJson():
     if not test:
       LOGGER.warn('Empty or None test name passed to standard_json_util')
       return
+
+    result_sink_test_result = result_sink_util.compose_test_result(
+        test, 'SKIP', True, tags=[('disabled_test', 'true')])
+    self.result_sink.post(result_sink_test_result)
 
     self.tests[test] = {'expected': 'SKIP', 'actual': 'SKIP'}
 
@@ -96,7 +112,7 @@ class StdJson():
       self.mark_skipped(test)
 
   def mark_timeout(self, test):
-    """Set test as TIMEOUT, which is used to indicate a test abort/timeout
+    """Sets test as TIMEOUT, which is used to indicate a test abort/timeout
 
     Params:
       test (str): a test in format "{TestCase}/{testMethod}"
@@ -104,6 +120,17 @@ class StdJson():
     if not test:
       LOGGER.warn('Empty or None test name passed to standard_json_util')
       return
+
+    # Timeout tests in iOS test runner are tests that's unexpectedly not run.
+    test_log = ('The test is compiled in test target but was unexpectedly not'
+                ' run or not finished.')
+    result_sink_test_result = result_sink_util.compose_test_result(
+        test,
+        'SKIP',
+        False,
+        test_log=test_log,
+        tags=[('disabled_test', 'false')])
+    self.result_sink.post(result_sink_test_result)
 
     if test in self.tests:
       self.tests[test]['actual'] = self.tests[test]['actual'] + " TIMEOUT"
