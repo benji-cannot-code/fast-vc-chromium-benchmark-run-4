26c5eafd51053dbaa6218e910d0357b4cafdef6f
Vasilii Sukhanov <vasilii@chromium.org>
2020-10-07 11:42:01 +0000

    Disable flaky TopControlsSlideControllerTest.TestFocusEditableElements
    Tbr: bsep@chromium.org
    Bug: 1136011
    Change-Id: Ie874f5f4b10479856af54f7ab3fd4d9012b60d04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2454719
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814629}

--- chrome/browser/ui/views/frame/top_controls_slide_controller_chromeos_browsertest.cc
+++ chrome/browser/ui/views/frame/top_controls_slide_controller_chromeos_browsertest.cc
@@ -800,8 +800,9 @@ IN_PROC_BROWSER_TEST_F(TopControlsSlideControllerTest, MAYBE_TestClosingATab) {
                                TopChromeShownState::kFullyHidden);
 }
 
+// Times out on CrOS. crbug.com/1136011
 IN_PROC_BROWSER_TEST_F(TopControlsSlideControllerTest,
-                       TestFocusEditableElements) {
+                       DISABLED_TestFocusEditableElements) {
   ToggleTabletMode();
   ASSERT_TRUE(GetTabletModeEnabled());
   EXPECT_TRUE(top_controls_slide_controller()->IsEnabled());
