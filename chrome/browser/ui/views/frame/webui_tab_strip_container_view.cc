301eb9973e4dac3eb0850fc90548cfabfe81ddbc
Collin Baker <collinbaker@chromium.org>
2020-10-07 18:07:09 +0000

    Add synthetic field trial WebUITabStripOnTablets
    The WebUITabStrip feature is controlled by an external field
    trial. However, this field trial includes all devices, including ones
    that can't enter tablet mode.
    
    This CL adds a synthetic field trial that mirrors the feature's state,
    but is only recorded if the device is a tablet.
    
    Bug: 1134713
    Change-Id: I866f5dc7814f01c18a8ef1a77f7886c08708d63f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2446520
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814772}

--- chrome/browser/ui/views/frame/webui_tab_strip_container_view.cc
+++ chrome/browser/ui/views/frame/webui_tab_strip_container_view.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
 #include "chrome/browser/ui/views/frame/top_container_view.h"
+#include "chrome/browser/ui/views/frame/webui_tab_strip_field_trial.h"
 #include "chrome/browser/ui/views/in_product_help/feature_promo_colors.h"
 #include "chrome/browser/ui/views/in_product_help/feature_promo_controller_views.h"
 #include "chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.h"
@@ -512,6 +513,11 @@ bool WebUITabStripContainerView::SupportsTouchableTabStrip(
 
 // static
 bool WebUITabStripContainerView::UseTouchableTabStrip(const Browser* browser) {
+  // This is called at Browser start to check which mode to use. It is a
+  // good place to check the feature state and set up a synthetic field
+  // trial.
+  WebUITabStripFieldTrial::RegisterFieldTrialIfNecessary();
+
   return browser->is_type_normal() &&
          base::FeatureList::IsEnabled(features::kWebUITabStrip) &&
          ui::TouchUiController::Get()->touch_ui();
