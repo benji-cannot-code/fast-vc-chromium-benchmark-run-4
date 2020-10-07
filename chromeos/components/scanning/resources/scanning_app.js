a414fb037740b95646aa57c1583db924fc7ad682
Jesse Schettler <jschettler@chromium.org>
2020-10-07 03:22:22 +0000

    scanning: Add scan button to scanning app
    Add a scan button to the scanning app. When the button is clicked, use
    the ScanService to perform a scan with the selected scanner and
    settings.
    
    Bug: 1059779
    Change-Id: I52231a5a49fa01a2b56e42bcf2f9842fdf54ddf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2437103
    Auto-Submit: Jesse Schettler <jschettler@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814535}

--- chromeos/components/scanning/resources/scanning_app.js
+++ chromeos/components/scanning/resources/scanning_app.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import 'chrome://resources/mojo/mojo/public/mojom/base/big_buffer.mojom-lite.js';
 import 'chrome://resources/mojo/mojo/public/mojom/base/string16.mojom-lite.js';
 import 'chrome://resources/mojo/mojo/public/mojom/base/unguessable_token.mojom-lite.js';
@@ -51,6 +52,24 @@ Polymer({
     /** @type {?string} */
     selectedSource: String,
 
+    /**
+     * @type {?string}
+     * @private
+     */
+    statusText_: String,
+
+    /** @private */
+    settingsDisabled_: {
+      type: Boolean,
+      value: false,
+    },
+
+    /** @private */
+    scanButtonDisabled_: {
+      type: Boolean,
+      value: true,
+    },
+
     /** @private */
     loaded_: {
       type: Boolean,
@@ -81,6 +100,8 @@ Polymer({
     // Set the first source as the selected source since it will be the first
     // option in the dropdown.
     this.selectedSource = this.capabilities_.sources[0].name;
+
+    this.scanButtonDisabled_ = false;
   },
 
   /**
@@ -113,8 +134,51 @@ Polymer({
       return;
     }
 
+    this.scanButtonDisabled_ = true;
+
     this.scanService_
         .getScannerCapabilities(this.scannerIds_.get(selectedScannerId))
         .then(this.onCapabilitiesReceived_.bind(this));
+  },
+
+  /** @private */
+  onScanClick_() {
+    if (!this.selectedScannerId || !this.selectedSource) {
+      // TODO(jschettler): Replace status text with finalized i18n strings.
+      this.statusText_ = 'Failed to start scan.';
+      return;
+    }
+
+    this.statusText_ = 'Scanning...';
+    this.settingsDisabled_ = true;
+    this.scanButtonDisabled_ = true;
+
+    // TODO(jschettler): Set color mode and resolution using the selected values
+    // when the corresponding dropdowns are added.
+    const settings = {
+      'sourceName': this.selectedSource,
+      'colorMode': chromeos.scanning.mojom.ColorMode.kColor,
+      'resolutionDpi': 100,
+    };
+    this.scanService_
+        .scan(this.scannerIds_.get(this.selectedScannerId), settings)
+        .then(
+            /*@type {!{success: boolean}}*/ (response) => {
+                this.onScanCompleted_(response)});
+  },
+
+  /**
+   * @param {!{success: boolean}} response
+   * @private
+   */
+  onScanCompleted_(response) {
+    if (response.success) {
+      this.statusText_ = 'Scan complete! File(s) saved to My files.';
+    } else {
+      this.statusText_ = 'Scan failed.';
+    }
+
+    this.settingsDisabled_ = false;
+    this.scanButtonDisabled_ = false;
   },
 });
