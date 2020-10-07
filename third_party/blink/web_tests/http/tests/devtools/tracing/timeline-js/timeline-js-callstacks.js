6d0d4b0973101d01a412cca6d26c39f17db78990
Peter Marshall <petermarshall@chromium.org>
2020-10-07 12:24:08 +0000

    Fix devtools timeline test
    This test calls generateJSFrame after createPerformanceModelWithEvents,
    except this already calls generateJSFrames. It just happens that this
    is idempotent under some circumstances which this test narrowly falls
    into. This meant that `events` was always empty and thus the merge
    did not. Remove all this. Test results are still the same.
    
    Bug: 1127827
    Change-Id: I6c1d0d776bf692f3563583ea4fa72d1fff9ea3b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2455706
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814636}

--- third_party/blink/web_tests/http/tests/devtools/tracing/timeline-js/timeline-js-callstacks.js
+++ third_party/blink/web_tests/http/tests/devtools/tracing/timeline-js/timeline-js-callstacks.js
@@ -499,11 +499,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
   ];
 
-  var model = PerformanceTestRunner.createPerformanceModelWithEvents(rawTraceEvents);
-  var events = TimelineModel.TimelineJSProfileProcessor.generateJSFrameEvents(PerformanceTestRunner.mainTrackEvents());
-  events =
-      events.mergeOrdered(PerformanceTestRunner.mainTrackEvents(), SDK.TracingModel.Event.orderedCompareStartTime);
-  events
+  PerformanceTestRunner.createPerformanceModelWithEvents(rawTraceEvents);
+  PerformanceTestRunner.mainTrackEvents()
       .filter(function(e) {
         return e.duration;
       })
