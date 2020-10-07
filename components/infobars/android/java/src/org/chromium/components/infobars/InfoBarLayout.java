4815c4e268c891d4a3a04bac830f7a10d879826f
Anne Lim <annelim@google.com>
2020-10-07 16:40:08 +0000

    Add footer to InfoBarLayout
    Adds a footer at the bottom of the InfoBar which spans the InfoBar's whole width.
    
    Screenshot where a footer is added to SavePasswordInfoBar:
    https://screenshot.googleplex.com/AVqcryn4jnEcoT9
    
    Bug: 1135847
    Change-Id: I8c3cd9968ced6ee4280b3929671e5e61043d4fcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2454694
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Anne Lim <annelim@google.com>
    Cr-Commit-Position: refs/heads/master@{#814714}

--- components/infobars/android/java/src/org/chromium/components/infobars/InfoBarLayout.java
+++ components/infobars/android/java/src/org/chromium/components/infobars/InfoBarLayout.java
@@ -90,6 +90,7 @@ public final class InfoBarLayout extends ViewGroup implements View.OnClickListen
     private final ImageButton mCloseButton;
     private final InfoBarControlLayout mMessageLayout;
     private final List<InfoBarControlLayout> mControlLayouts;
+    private ViewGroup mFooterViewGroup;
 
     private TextView mMessageTextView;
     private ImageView mIconView;
@@ -211,6 +212,16 @@ public final class InfoBarLayout extends ViewGroup implements View.OnClickListen
     }
 
     /**
+     * Adds a footer at the bottom of the InfoBar which spans the InfoBar's whole width.
+     *
+     * @param footerView footer to be added.
+     */
+    public ViewGroup addFooterView(ViewGroup footerView) {
+        mFooterViewGroup = footerView;
+        return footerView;
+    }
+
+    /**
      * Adds one or two buttons to the layout.
      *
      * @param primaryText Text for the primary button.  If empty, no buttons are added at all.
@@ -305,6 +316,7 @@ public final class InfoBarLayout extends ViewGroup implements View.OnClickListen
         addView(mMessageLayout);
         for (View v : mControlLayouts) addView(v);
         if (mButtonRowLayout != null) addView(mButtonRowLayout);
+        if (mFooterViewGroup != null) addView(mFooterViewGroup);
         addView(mCloseButton);
     }
 
@@ -356,6 +368,8 @@ public final class InfoBarLayout extends ViewGroup implements View.OnClickListen
      * |  | BOTTOM ROW LAYOUT                                                      |  |
      * |  -------------------------------------------------------------------------|  |
      * |                                                                              |
+     * --------------------------------------------------------------------------------
+     * |  FOOTER                                                                      |
      * --------------------------------------------------------------------------------
      */
     @Override
@@ -429,6 +443,14 @@ public final class InfoBarLayout extends ViewGroup implements View.OnClickListen
 
         // Apply padding to the bottom of the infobar.
         layoutBottom += mPadding;
+
+        if (mFooterViewGroup != null) {
+            LayoutParams footerParams = getChildLayoutParams(mFooterViewGroup);
+            measureChildWithFixedWidth(mFooterViewGroup, layoutWidth);
+            footerParams.start = 0;
+            footerParams.top = layoutBottom;
+            layoutBottom += mFooterViewGroup.getMeasuredHeight();
+        }
 
         setMeasuredDimension(resolveSize(layoutWidth, widthMeasureSpec),
                 resolveSize(layoutBottom, heightMeasureSpec));
