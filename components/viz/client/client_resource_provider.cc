be1f2b0896054a9ef08741b5f54b4e6b770be9f0
Adlai Holler <adlai@google.com>
2020-10-07 19:58:27 +0000

    Migrate some last cases of GrContext to GrDirectContext
    As far as Chromium is concerned, this is a direct rename with no
    functional changes.
    
    Change-Id: Ia48e6602c237f3504fac41cb0f951f8f878f125b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2456511
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Auto-Submit: Adlai Holler <adlai@google.com>
    Cr-Commit-Position: refs/heads/master@{#814820}

--- components/viz/client/client_resource_provider.cc
+++ components/viz/client/client_resource_provider.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/client/raster_interface.h"
 #include "gpu/command_buffer/common/capabilities.h"
 #include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/gpu/GrDirectContext.h"
 
 namespace viz {
 
@@ -359,7 +360,7 @@ void ClientResourceProvider::ShutdownAndReleaseAllResources() {
 }
 
 ClientResourceProvider::ScopedSkSurface::ScopedSkSurface(
-    GrContext* gr_context,
+    GrDirectContext* gr_context,
     sk_sp<SkColorSpace> color_space,
     GLuint texture_id,
     GLenum texture_target,
