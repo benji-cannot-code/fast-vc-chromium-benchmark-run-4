2dabdc71ac5720a936832ca51367ee14f7035b10
Sean Topping <seantopping@chromium.org>
2020-10-07 00:14:54 +0000

    [Chromecast] Forward declare content::WebContents
    Including the WebContents header fails when a target doesn't depend on
    content. Instead, we should only forward declare these classes in the
    Cast web primary interfaces (CastWebContents, CastWebView,
    CastContentWindow).
    
    Merge-With: eureka-internal/461423
    
    Bug: None
    Test: CQ
    Change-Id: Id18c669c7d39f4ed847a8c4970f5d6c8847a234c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2454370
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814466}

--- chromecast/browser/cast_web_view.h
+++ chromecast/browser/cast_web_view.h
@@ -17,8 +17,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/browser/cast_web_contents.h"
 #include "chromecast/ui/mojom/ui_service.mojom.h"
 #include "content/public/browser/bluetooth_chooser.h"
-#include "content/public/browser/web_contents.h"
 #include "url/gurl.h"
+
+namespace content {
+class RenderFrameHost;
+class WebContents;
+}  // namespace content
 
 namespace chromecast {
 
