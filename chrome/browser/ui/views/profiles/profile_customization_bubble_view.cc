86183038a1485d19936f8c7caf65fa9a9acfc488
David Roger <droger@chromium.org>
2020-10-07 09:48:06 +0000

    [signin] Improve style for profile customization bubble
    Notes:
    * on the mock, there is a color row missing. I've confirmed that it
      should be added (this is done in this CL).
    * The input is not plumbed (does not update the profile name)
    * the pink color is hardcoded
    * the brush icon for the custom color is larger than the mocks
    
    Screenshot:
    https://screenshot.googleplex.com/AStdxggC7tftUN2.png
    
    Mock:
    https://screenshot.googleplex.com/4Ntu44ka2iBMJxm.png
    
    Bug: 1130945
    Change-Id: I736119eae9714f6662955c41f30e13624710dd52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450010
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814607}

--- chrome/browser/ui/views/profiles/profile_customization_bubble_view.cc
+++ chrome/browser/ui/views/profiles/profile_customization_bubble_view.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace {
-constexpr int kCustomizationBubbleHeight = 452;
+constexpr int kCustomizationBubbleHeight = 495;
 constexpr int kCustomizationBubbleWidth = 290;
 }  // namespace
 
