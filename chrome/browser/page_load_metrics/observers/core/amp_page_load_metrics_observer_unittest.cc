5d345b02f94b9ca419de8b9448333f0185027eb2
Nicolás Peña Moreno <npm@chromium.org>
2020-10-07 16:14:27 +0000

    [LargestContentfulPaint] Add experimental LCP for AMP
    UKM privacy review:
    https://docs.google.com/document/d/1M1nsLapTIoNgaD8SXtrqEirVQajYKmfmQKHbtI7J6-w/edit#heading=h.k5jx6iluw4yt
    
    Bug: 1045640
    Change-Id: I09e2e779f28d42e3760ed1acaf08c7614e3f9194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450909
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814700}

--- chrome/browser/page_load_metrics/observers/core/amp_page_load_metrics_observer_unittest.cc
+++ chrome/browser/page_load_metrics/observers/core/amp_page_load_metrics_observer_unittest.cc
@@ -333,6 +333,10 @@ TEST_F(AMPPageLoadMetricsObserverTest, SubFrameMetrics) {
       ->largest_image_paint_size = 1;
   subframe_timing.paint_timing->largest_contentful_paint->largest_image_paint =
       base::TimeDelta::FromMilliseconds(10);
+  subframe_timing.paint_timing->experimental_largest_contentful_paint
+      ->largest_text_paint_size = 3;
+  subframe_timing.paint_timing->experimental_largest_contentful_paint
+      ->largest_text_paint = base::TimeDelta::FromMilliseconds(8);
   subframe_timing.interactive_timing->first_input_timestamp =
       base::TimeDelta::FromMilliseconds(20);
   subframe_timing.interactive_timing->first_input_delay =
@@ -367,6 +371,9 @@ TEST_F(AMPPageLoadMetricsObserverTest, SubFrameMetrics) {
   tester()->test_ukm_recorder().ExpectEntryMetric(
       entry.get(), "SubFrame.PaintTiming.NavigationToLargestContentfulPaint",
       10);
+  tester()->test_ukm_recorder().ExpectEntryMetric(
+      entry.get(),
+      "SubFrame.PaintTiming.NavigationToExperimentalLargestContentfulPaint", 8);
 }
 
 TEST_F(AMPPageLoadMetricsObserverTest, SubFrameMetrics_LayoutInstability) {
@@ -441,6 +448,10 @@ TEST_F(AMPPageLoadMetricsObserverTest, SubFrameMetricsFullNavigation) {
       ->largest_image_paint_size = 1;
   subframe_timing.paint_timing->largest_contentful_paint->largest_image_paint =
       base::TimeDelta::FromMilliseconds(10);
+  subframe_timing.paint_timing->experimental_largest_contentful_paint
+      ->largest_image_paint_size = 1;
+  subframe_timing.paint_timing->experimental_largest_contentful_paint
+      ->largest_image_paint = base::TimeDelta::FromMilliseconds(5);
   subframe_timing.interactive_timing->first_input_timestamp =
       base::TimeDelta::FromMilliseconds(20);
   subframe_timing.interactive_timing->first_input_delay =
@@ -476,6 +487,9 @@ TEST_F(AMPPageLoadMetricsObserverTest, SubFrameMetricsFullNavigation) {
   tester()->test_ukm_recorder().ExpectEntryMetric(
       entry.get(), "SubFrame.PaintTiming.NavigationToLargestContentfulPaint",
       10);
+  tester()->test_ukm_recorder().ExpectEntryMetric(
+      entry.get(),
+      "SubFrame.PaintTiming.NavigationToExperimentalLargestContentfulPaint", 5);
 }
 
 TEST_F(AMPPageLoadMetricsObserverTest, SubFrameRecordOnFullNavigation) {
