82049279236f74a0845594019a7ceabcc24fe194
Andre Le <leandre@chromium.org>
2020-10-07 22:18:56 +0000

    [CrOS PhoneHub] Hook up settings button to settings page.
    Add the functionality to open settings page in the settings buttons
    of PhoneHub bubble and PhoneHub notification.
    
    BUG=1106937,1126208
    
    Change-Id: I751c0dfb227c5d0d0d0e47f6fd97753a460e530a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2453872
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814903}

--- ash/system/phonehub/phone_hub_notification_controller.cc
+++ ash/system/phonehub/phone_hub_notification_controller.cc
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/phonehub/phone_hub_notification_controller.h"
 
+#include "ash/public/cpp/system_tray_client.h"
+#include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
+#include "ash/system/model/system_tray_model.h"
+#include "ash/system/tray/tray_popup_utils.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/strcat.h"
@@ -143,7 +147,8 @@ void PhoneHubNotificationController::OnNotificationsRemoved(
 }
 
 void PhoneHubNotificationController::OpenSettings() {
-  // TODO(tengs): Open the PhoneHub settings page.
+  DCHECK(TrayPopupUtils::CanOpenWebUISettings());
+  Shell::Get()->system_tray_model()->client()->ShowConnectedDevicesSettings();
 }
 
 void PhoneHubNotificationController::DismissNotification(
@@ -240,8 +245,10 @@ PhoneHubNotificationController::CreateNotification(
       IDS_ASH_PHONE_HUB_NOTIFICATION_INLINE_CANCEL_BUTTON);
   optional_fields.buttons.push_back(cancel_button);
 
-  optional_fields.settings_button_handler =
-      message_center::SettingsButtonHandler::DELEGATE;
+  if (TrayPopupUtils::CanOpenWebUISettings()) {
+    optional_fields.settings_button_handler =
+        message_center::SettingsButtonHandler::DELEGATE;
+  }
 
   return std::make_unique<message_center::Notification>(
       notification_type, cros_id, title, message, icon, display_source,
--- ash/system/phonehub/phone_status_view.cc
+++ ash/system/phonehub/phone_status_view.cc
@@ -7,12 +7,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/network_icon_image_source.h"
 #include "ash/public/cpp/shelf_config.h"
+#include "ash/public/cpp/system_tray_client.h"
 #include "ash/resources/vector_icons/vector_icons.h"
+#include "ash/root_window_controller.h"
+#include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
+#include "ash/system/model/system_tray_model.h"
+#include "ash/system/phonehub/phone_hub_tray.h"
 #include "ash/system/phonehub/phone_hub_view_ids.h"
+#include "ash/system/status_area_widget.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_popup_item_style.h"
+#include "ash/system/tray/tray_popup_utils.h"
 #include "base/i18n/number_formatting.h"
 #include "base/strings/string16.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -102,6 +109,9 @@ PhoneStatusView::PhoneStatusView(chromeos::phonehub::PhoneModel* phone_model)
                                            IDS_ASH_STATUS_TRAY_SETTINGS);
   AddView(TriView::Container::END, settings_button_);
 
+  separator->SetVisible(TrayPopupUtils::CanOpenWebUISettings());
+  settings_button_->SetVisible(TrayPopupUtils::CanOpenWebUISettings());
+
   Update();
 }
 
@@ -111,7 +121,9 @@ PhoneStatusView::~PhoneStatusView() {
 
 void PhoneStatusView::ButtonPressed(views::Button* sender,
                                     const ui::Event& event) {
-  // TODO(leandre): implement open settings/other buttons.
+  DCHECK_EQ(settings_button_, sender);
+  DCHECK(TrayPopupUtils::CanOpenWebUISettings());
+  Shell::Get()->system_tray_model()->client()->ShowConnectedDevicesSettings();
 }
 
 void PhoneStatusView::OnModelChanged() {
