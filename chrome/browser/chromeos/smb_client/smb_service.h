7f7e99a791819bceeee83dfc22fcc71cc6452ddb
Derek Basehore <dbasehore@chromium.org>
2020-10-07 22:28:48 +0000

    Revert "Disable unmount-on-suspend for FUSE"
    This reverts commit 436c119d042f7298482f189956d3bc0c7a3b336c.
    
    Reason for revert: An edge case for platforms without ARC++ was not handled correctly.
    
    Original change's description:
    > Disable unmount-on-suspend for FUSE
    >
    > This removes the code for unmounting the FUSEs (filesystem in userspace)
    > on suspend. Instead of unmounting to resolve the FUSE/freeze on suspend
    > race condition, we will be using powerd code that orders freeze using
    > freezer cgroups.
    >
    > Crostini still has some work left to do before this can be done for it,
    > so unmount on suspend for its FUSE, sshfs, will be done in a later
    > patch.
    >
    > Test: "suspend_stress_test -c 2500" on device
    > Bug: b/134792837
    > Change-Id: Icb8f9e309f6780e6f935be550c6c4e048a44b1fd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2336869
    > Commit-Queue: Derek Basehore <dbasehore@chromium.org>
    > Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    > Reviewed-by: Sergei Datsenko <dats@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: David Munro <davidmunro@google.com>
    > Cr-Commit-Position: refs/heads/master@{#811950}
    
    TBR=jamescook@chromium.org,achuith@chromium.org,dbasehore@chromium.org,joelhockey@chromium.org,dats@chromium.org,davidmunro@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: b/134792837
    Change-Id: Ia50e43dceeb0eea448d6fdf7232af328a9a1fd1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2451400
    Reviewed-by: David Munro <davidmunro@google.com>
    Reviewed-by: Sergei Datsenko <dats@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Derek Basehore <dbasehore@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814910}

--- chrome/browser/chromeos/smb_client/smb_service.h
+++ chrome/browser/chromeos/smb_client/smb_service.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/smb_client/smb_task_queue.h"
 #include "chrome/browser/chromeos/smb_client/smbfs_share.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chromeos/dbus/power/power_manager_client.h"
 #include "chromeos/dbus/smb_provider_client.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "net/base/network_change_notifier.h"
@@ -49,6 +50,7 @@ class SmbShareInfo;
 // Creates and manages an smb file system.
 class SmbService : public KeyedService,
                    public net::NetworkChangeNotifier::NetworkChangeObserver,
+                   public chromeos::PowerManagerClient::Observer,
                    public base::SupportsWeakPtr<SmbService> {
  public:
   using MountResponse = base::OnceCallback<void(SmbMountResult result)>;
@@ -125,6 +127,10 @@ class SmbService : public KeyedService,
   void SetSmbFsMounterCreationCallbackForTesting(
       SmbFsShare::MounterCreationCallback callback);
 
+  // chromeos::PowerManagerClient::Observer overrides
+  void SuspendImminent(power_manager::SuspendImminent::Reason reason) override;
+  void SuspendDone(const base::TimeDelta& sleep_duration) override;
+
  private:
   friend class SmbServiceTest;
 
@@ -164,6 +170,11 @@ class SmbService : public KeyedService,
   void OnSmbfsMountDone(const std::string& smbfs_mount_id,
                         MountInternalCallback callback,
                         SmbMountResult result);
+
+  // Callback passed to SmbFsShare::Unmount() during a power management
+  // suspension. Ensures that suspension is blocked until the unmount completes.
+  void OnSuspendUnmountDone(base::UnguessableToken power_manager_suspend_token,
+                            chromeos::MountError result);
 
   // Retrieves the mount_id for |file_system_info|.
   int32_t GetMountId(const ProvidedFileSystemInfo& info) const;
