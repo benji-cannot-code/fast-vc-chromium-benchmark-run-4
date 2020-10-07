81541e18425747c363a5058fae1a91277ae7fc03
Muyao Xu <muyaoxu@google.com>
2020-10-07 22:41:59 +0000

    [Cast+Zenith] Show the device selector for local media sessions only
    This CL adds a |SourceType| enum to represent three different
    notification item types: local media sessions, cast sessions, and
    Presentation requests.
    The device selector view should only show up for local media sessions.
    
    Bug: b/161610050, 1107162
    Change-Id: I45ac39af46e9aa931795fd5bb763f9c0c56c1f42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2453891
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814914}

--- components/media_message_center/media_notification_item.h
+++ components/media_message_center/media_notification_item.h
@@ -17,6 +17,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media_message_center {
 
+enum class SourceType {
+  kLocalMediaSession,
+  kCast,
+  kPresentationRequest,
+  kMaxValue = kPresentationRequest,
+};
+
 class MediaNotificationView;
 
 // MediaNotificationItem manages hiding/showing a MediaNotificationView.
@@ -59,8 +66,8 @@ class COMPONENT_EXPORT(MEDIA_MESSAGE_CENTER) MediaNotificationItem {
   // Hides the media notification.
   virtual void Dismiss() = 0;
 
-  // Return true if this item belongs to a cast media session, false otherwise.
-  virtual bool SourceIsCast() = 0;
+  // Returns the type of source.
+  virtual SourceType SourceType() = 0;
 };
 
 }  // namespace media_message_center
