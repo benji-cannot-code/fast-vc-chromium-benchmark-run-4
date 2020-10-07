3a011c8f6cd90419124caa9a50c10e34b1ecbb1e
Daniel Ng <danielng@google.com>
2020-10-07 01:56:21 +0000

    borealis: Add base for the borealis starter flow
    This cl introduces the basic architecture to support the borealis
    starter flow. It introduces the borealis context manager, which is
    intended to manage the start up flow of the Borealis VM and be the
    source of truth for run-time information about the Borealis VM. See
    go/borealis-starter-flow for additional context.
    
    Tests: Wrote and ran tests
    Bug: b:168425531
    Change-Id: I88bece091d5d9a5af4a121ff498c2ebdc504cff1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434020
    Commit-Queue: Daniel Ng <danielng@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/master@{#814512}

---
+++ chrome/browser/chromeos/borealis/borealis_context.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_BOREALIS_BOREALIS_CONTEXT_H_
+#define CHROME_BROWSER_CHROMEOS_BOREALIS_BOREALIS_CONTEXT_H_
+
+#include "chrome/browser/profiles/profile.h"
+
+namespace borealis {
+
+// An object to track information about the state of the Borealis VM.
+// BorealisContext objects should only be created by the Borealis Context
+// Manager, which is why the constructor is private.
+class BorealisContext {
+ public:
+  BorealisContext(const BorealisContext&) = delete;
+  BorealisContext& operator=(const BorealisContext&) = delete;
+  ~BorealisContext() = default;
+
+  static BorealisContext* CreateBorealisContextForTesting() {
+    return new BorealisContext();
+  }
+
+  Profile* profile() { return profile_; }
+  void set_profile(Profile* profile) { profile_ = profile; }
+
+  bool borealis_running() const { return borealis_running_; }
+  void set_borealis_running(bool success) { borealis_running_ = success; }
+
+ private:
+  friend class BorealisContextManagerImpl;
+
+  BorealisContext() = default;
+  explicit BorealisContext(Profile* profile) : profile_(profile) {}
+
+  Profile* profile_ = nullptr;
+  bool borealis_running_ = false;
+};
+
+}  // namespace borealis
+
+#endif  // CHROME_BROWSER_CHROMEOS_BOREALIS_BOREALIS_CONTEXT_H_
