1cd874d2f6eace2e8390b65cc673aa5b18af631d
Miyoung Shin <myid.shin@igalia.com>
2020-10-07 02:46:25 +0000

    [remoteobjects] Add a reference count's map in RemoteObjectHostImpl
    This is precursor CL of https://crrev.com/c/2379270, and adds the
    reference count's map to remove the object from the registry when
    the reference count is zero.
    
    Bug: 1105925
    Change-Id: I5b04c1fe17c4376dad97dedf3f5750e05f611c32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2417740
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814521}

--- content/public/android/java/src/org/chromium/content/browser/remoteobjects/RemoteObjectRegistry.java
+++ content/public/android/java/src/org/chromium/content/browser/remoteobjects/RemoteObjectRegistry.java
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser.remoteobjects;
 
-import java.util.HashMap;
+import android.util.SparseArray;
+
+import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,8 +19,29 @@ import java.util.Set;
  */
 final class RemoteObjectRegistry implements RemoteObjectImpl.ObjectIdAllocator {
     private final Set<? super RemoteObjectRegistry> mRetainingSet;
-    private final Map<Integer, Object> mObjectsById = new HashMap<>();
-    private final Map<Object, Integer> mIdsByObject = new HashMap<>();
+
+    private static class Entry {
+        Entry(int id, Object object) {
+            this.id = id;
+            this.object = object;
+        }
+
+        // The ID assigned to the object, which can be used by the renderer
+        // to refer to it.
+        public final int id;
+
+        // The injected object itself
+        public final Object object;
+
+        // The number of outstanding references to this object.
+        // This includes:
+        //   * wrapper objects in the renderer (removed when it closes the pipe)
+        //   * names assigned to the object by developer Java code
+        public int referenceCount = 1;
+    }
+
+    private final SparseArray<Entry> mEntriesById = new SparseArray<>();
+    private final Map<Object, Entry> mEntriesByObject = new IdentityHashMap<>();
     private int mNextId;
 
     RemoteObjectRegistry(Set<? super RemoteObjectRegistry> retainingSet) {
@@ -32,26 +55,43 @@ final class RemoteObjectRegistry implements RemoteObjectImpl.ObjectIdAllocator {
     }
 
     @Override
-    public int getObjectId(Object object) {
-        Integer existingId = mIdsByObject.get(object);
-        if (existingId != null) {
-            return existingId;
+    public synchronized int getObjectId(Object object) {
+        Entry entry = mEntriesByObject.get(object);
+        if (entry != null) {
+            entry.referenceCount++;
+            return entry.id;
         }
 
         int newId = mNextId++;
         assert newId >= 0;
-        mObjectsById.put(newId, object);
-        mIdsByObject.put(object, newId);
+        entry = new Entry(newId, object);
+        mEntriesById.put(newId, entry);
+        mEntriesByObject.put(object, entry);
         return newId;
     }
 
     @Override
-    public Object getObjectById(int id) {
-        return mObjectsById.get(id);
+    public synchronized Object getObjectById(int id) {
+        Entry entry = mEntriesById.get(id);
+        return entry != null ? entry.object : null;
     }
 
-    public void removeObjectById(int id) {
-        Object o = mObjectsById.remove(id);
-        mIdsByObject.remove(o);
+    public synchronized void refObjectById(int id) {
+        Entry entry = mEntriesById.get(id);
+        if (entry == null) return;
+
+        entry.referenceCount++;
+    }
+
+    public synchronized void unrefObjectById(int id) {
+        Entry entry = mEntriesById.get(id);
+        if (entry == null) return;
+
+        entry.referenceCount--;
+        assert entry.referenceCount >= 0;
+        if (entry.referenceCount > 0) return;
+
+        mEntriesById.remove(entry.id);
+        mEntriesByObject.remove(entry.object);
     }
 }
