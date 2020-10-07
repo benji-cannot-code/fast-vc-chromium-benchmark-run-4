eee343ee52435ab1dcc67bd2ed055cc420ec97ba
Wei Lee <wtlee@google.com>
2020-10-07 05:49:35 +0000

    [CCA WebUI] Don't capture navigation other than main page URL for CCA
    We have the following requirements:
    1. We prefer loading CCA UI in the app window rather than browser tab
      => Therefore we added "capture_navigation=true" for CCA
    2. We don't want to load resource URLs or invalid URL in the app window
      => Short term: Landing this CL to filter out unwanted URLs
    
      We decide not to support URL filtering feature for capture navigation
      since only camera app has such requirement at this moment.
      (crbug.com/1132151)
    
    Bug: 980846
    Test: Type CCA url other than chrome://camera-app/views/main.html and
    the page is loaded in browser tab
    
    Change-Id: I467aa1498aa9fe975d34bdf82adee7f6dff6a14c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440076
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814557}

--- chrome/browser/web_applications/system_web_app_manager.cc
+++ chrome/browser/web_applications/system_web_app_manager.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/web_applications/scanning_system_web_app_info.h"
 #include "chrome/browser/chromeos/web_applications/terminal_source.h"
 #include "chrome/browser/chromeos/web_applications/terminal_system_web_app_info.h"
+#include "chromeos/components/camera_app_ui/url_constants.h"
 #include "chromeos/components/help_app_ui/url_constants.h"
 #include "chromeos/components/media_app_ui/url_constants.h"
 #include "chromeos/constants/chromeos_features.h"
@@ -624,6 +625,12 @@ base::Optional<SystemAppType> SystemWebAppManager::GetCapturingSystemAppForURL(
 
   if (!it->second.capture_navigations)
     return base::nullopt;
+
+#if defined(OS_CHROMEOS)
+  if (type == SystemAppType::CAMERA &&
+      url.spec() != chromeos::kChromeUICameraAppMainURL)
+    return base::nullopt;
+#endif  // defined(OS_CHROMEOS)
 
   return type;
 }
