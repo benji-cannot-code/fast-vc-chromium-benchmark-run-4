4c8980156230a36ffabde0cc050bc12708a961bb
Oleh Lamzin <lamzin@google.com>
2020-10-07 13:09:21 +0000

    Fix path to README.md file for app_service
    Remove // from path for README.md since it's not searchable and
    README.md file path is components/services/app_service/README.md
    in code search.
    
    Change-Id: I2ac9033a5584b4d75d841996606b58d102988547
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2453255
    Auto-Submit: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/master@{#814645}

--- chrome/browser/apps/app_service/README.md
+++ chrome/browser/apps/app_service/README.md
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 This is the browser-specific part of the App Service.
 
-See `//components/services/app_service/README.md`.
+See `components/services/app_service/README.md`.
--- components/services/app_service/public/cpp/app_registry_cache.h
+++ components/services/app_service/public/cpp/app_registry_cache.h
@@ -32,7 +32,7 @@ namespace apps {
 //
 // This class is not thread-safe.
 //
-// See //components/services/app_service/README.md for more details.
+// See components/services/app_service/README.md for more details.
 class COMPONENT_EXPORT(APP_UPDATE) AppRegistryCache {
  public:
   class COMPONENT_EXPORT(APP_UPDATE) Observer : public base::CheckedObserver {
