36d8aa21fadbfc0ca7e8cdc66be21bd5cc4dc783
Xinghui Lu <xinghuilu@chromium.org>
2020-10-07 18:26:40 +0000

    Fix render tests testRenderDifferentSignedInStates*
    See the failure in https://crbug.com/1135290#c1. Explicitly enable
    the SAFE_BROWSING_SECURITY_SECTION_UI flag to show "Privacy and
    security".
    
    Bug: 1135290
    Change-Id: I28d4656e8df987d79312dad2e00d67f861465c3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450665
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814784}

--- chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
@@ -150,6 +150,7 @@ public class MainSettingsFragmentTest {
     @Test
     @LargeTest
     @Feature({"RenderTest"})
+    @EnableFeatures({ChromeFeatureList.SAFE_BROWSING_SECURITY_SECTION_UI})
     @DisableFeatures({ChromeFeatureList.SAFETY_CHECK_ANDROID})
     public void testRenderDifferentSignedInStates() throws IOException {
         launchSettingsActivity();
@@ -167,8 +168,10 @@ public class MainSettingsFragmentTest {
     @Test
     @LargeTest
     @Feature({"RenderTest"})
-    @EnableFeatures({ChromeFeatureList.SAFETY_CHECK_ANDROID})
-    public void testRenderDifferentSignedInStatesWithSafetyCheck() throws IOException {
+    @EnableFeatures({ChromeFeatureList.SAFETY_CHECK_ANDROID,
+            ChromeFeatureList.SAFE_BROWSING_SECURITY_SECTION_UI})
+    public void
+    testRenderDifferentSignedInStatesWithSafetyCheck() throws IOException {
         launchSettingsActivity();
         View view = mSettingsActivityTestRule.getActivity()
                             .findViewById(android.R.id.content)
