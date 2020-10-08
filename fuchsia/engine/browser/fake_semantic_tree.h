d9982ff34ae63683311d86ac7e88588bd7f1d328
Lucas Radaelli <lucasradaelli@google.com>
2020-10-08 22:10:59 +0000

    [fuchsia][a11y] Accessibility Bridge forwards valid updates.
    This change refactors parts of the Accessibility Bridge to solve the
    following bugs: * Previously, it was deleting nodes that were marked as
    reparented or a subtree that was reparented. This assumed that nodes
    marked this way would appear in the list of updates sent by the
    AxTreeObserver in the final atomic update, but this assumption is not
    correct. This fixes the problem where parents were pointing to children
    no longer in the tree.
    
    * Allow trees of size 1 to be sent. Previously, the loop that dealt with
      sending updates started from the second item, which would not send
      anything if the tree had only one element.
    
    * Simplifies the order of updates: first deletions and then
      updates. Because atomic updates always finish with the list of nodes
      added / changed, we can use this property to simplify the code to
      always follow this order.
    
    Todos: There are some todos left in the code which I opted to follow-up
    in a future cl so that I can split work among people and parallelize.
    
    Test: AccessibilityBridgeTest.*
    Bug: 1134384,fuchsia:58989,fuchsia:50187,fuchsia:61155
    Change-Id: I6ec09a5d6fa5d5ad37fb42a059d0d0f122d39d97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2446358
    Commit-Queue: Lucas Radaelli <lucasradaelli@google.com>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815346}

--- fuchsia/engine/browser/fake_semantic_tree.h
+++ fuchsia/engine/browser/fake_semantic_tree.h
@@ -37,6 +37,7 @@ class FakeSemanticTree
   void Disconnect();
 
   void RunUntilNodeCountAtLeast(size_t count);
+  void RunUntilCommitCountIs(size_t count);
   void SetNodeUpdatedCallback(uint32_t node_id,
                               base::OnceClosure node_updated_callback);
   fuchsia::accessibility::semantics::Node* GetNodeWithId(uint32_t id);
@@ -50,6 +51,10 @@ class FakeSemanticTree
 
   size_t tree_size() const { return nodes_.size(); }
   void Clear();
+
+  size_t num_delete_calls() const { return num_delete_calls_; }
+  size_t num_update_calls() const { return num_update_calls_; }
+  size_t num_commit_calls() const { return num_commit_calls_; }
 
   // fuchsia::accessibility::semantics::SemanticTree implementation.
   void UpdateSemanticNodes(
@@ -67,6 +72,10 @@ class FakeSemanticTree
 
   uint32_t node_wait_id_;
   base::OnceClosure on_node_updated_callback_;
+
+  size_t num_delete_calls_ = 0;
+  size_t num_update_calls_ = 0;
+  size_t num_commit_calls_ = 0;
 };
 
 #endif  // FUCHSIA_ENGINE_BROWSER_FAKE_SEMANTIC_TREE_H_
