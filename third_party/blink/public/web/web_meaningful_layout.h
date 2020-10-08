554dfa5007e4f1023aa72ddab4aaef238a420170
Morten Stenshorne <mstensho@chromium.org>
2020-10-08 16:53:14 +0000

    Make WebMeaningfulLayout an enum class.
    Compiling with -Wshadow showed that one of the values conflicted with
    Document::ParsingState
    
    Bug: 294205
    Change-Id: I4f932767d1962e7615acfefa5af76ba554aabaa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461009
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Michael Bai <michaelbai@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815191}

--- third_party/blink/public/web/web_meaningful_layout.h
+++ third_party/blink/public/web/web_meaningful_layout.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-enum WebMeaningfulLayout {
+enum class WebMeaningfulLayout {
   // Signifies that one of the following things were involved during the layout:
   // * > 200 text characters
   // * > 1024 image pixels
