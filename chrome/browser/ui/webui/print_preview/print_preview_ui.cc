3d50efb1f406da237ea4a87f21ee2a8ea8eea6cf
Julie Jeongeun Kim <jkim@igalia.com>
2020-10-08 08:47:46 +0000

    [printing] Early return when the request id is invalid
    The request id could not be set from PrintRenderFrameHelper
    when it calls PrintPreviewFailed(), PrintPreviewCancelled(),
    or PrinterSettingsInvalid() as the request id is set from
    |print_pages_params_| and it could be null.
    
    This CL adds early return when the request id is -1.
    
    Bug: 1136025
    Change-Id: I06caaef4050653ddb9ff82b477a74600e9796a55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2455826
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815069}

--- chrome/browser/ui/webui/print_preview/print_preview_ui.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_ui.cc
@@ -828,6 +828,8 @@ void PrintPreviewUI::OnClosePrintPreviewDialog() {
 void PrintPreviewUI::SetOptionsFromDocument(
     const mojom::OptionsFromDocumentParamsPtr params,
     int32_t request_id) {
+  if (request_id == -1)
+    return;
   handler_->SendPrintPresetOptions(params->is_scaling_disabled, params->copies,
                                    params->duplex, request_id);
 }
@@ -835,6 +837,8 @@ void PrintPreviewUI::SetOptionsFromDocument(
 void PrintPreviewUI::PrintPreviewFailed(int32_t document_cookie,
                                         int32_t request_id) {
   StopWorker(document_cookie);
+  if (request_id == -1)
+    return;
   OnPrintPreviewFailed(request_id);
 }
 
@@ -842,12 +846,16 @@ void PrintPreviewUI::PrintPreviewCancelled(int32_t document_cookie,
                                            int32_t request_id) {
   // Always need to stop the worker.
   StopWorker(document_cookie);
+  if (request_id == -1)
+    return;
   handler_->OnPrintPreviewCancelled(request_id);
 }
 
 void PrintPreviewUI::PrinterSettingsInvalid(int32_t document_cookie,
                                             int32_t request_id) {
   StopWorker(document_cookie);
+  if (request_id == -1)
+    return;
   handler_->OnInvalidPrinterSettings(request_id);
 }
 
