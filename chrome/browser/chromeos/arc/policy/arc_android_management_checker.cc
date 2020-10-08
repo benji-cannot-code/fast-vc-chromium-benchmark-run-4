4f4a0d3e6a3833d62927d25b345f81210393d022
Tomasz Tylenda <ttylenda@chromium.org>
2020-10-08 04:41:06 +0000

    Reland "Migrate base::Callback to base::OnceCallback."
    This reverts commit 1864e6e3e34567e1301ae550aef5b557261d34e7.
    
    Reason for revert: fixing b/169804713 - we use RepeatingCallback in
    arc_file_system_watcher_service.cc.
    
    Original change's description:
    > Revert "Migrate base::Callback to base::OnceCallback."
    >
    > This reverts commit a0282608035344add1595bd22bf1ca3de98d4483.
    >
    > Reason for revert: b/169804713
    >
    > Original change's description:
    > > Migrate base::Callback to base::OnceCallback.
    > >
    > > BUG=764795
    > > TEST=build
    > >
    > > Change-Id: I8afa2fd87eb5cf5d9a8f7ecde9fad95d420c5399
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434405
    > > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > > Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#811980}
    >
    > TBR=hidehiko@chromium.org,ttylenda@chromium.org
    >
    > Change-Id: I7de1e3dac102f4c760aef90da690016b020baa3f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 764795
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440088
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#812516}
    
    TBR=hidehiko@chromium.org,ttylenda@chromium.org,vecore@google.com
    
    Bug: 764795
    Change-Id: I5cc6f59381be3ced7d07d9629414bdfe0c2edfde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440233
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815022}

--- chrome/browser/chromeos/arc/policy/arc_android_management_checker.cc
+++ chrome/browser/chromeos/arc/policy/arc_android_management_checker.cc
@@ -71,7 +71,7 @@ void ArcAndroidManagementChecker::StartClient() {
   GetDeviceManagementService()->ScheduleInitialization(0);
 }
 
-void ArcAndroidManagementChecker::StartCheck(const CheckCallback& callback) {
+void ArcAndroidManagementChecker::StartCheck(CheckCallback callback) {
   DCHECK(callback_.is_null());
 
   // Do not send requests for Chrome OS managed users, nor for well-known
@@ -79,11 +79,11 @@ void ArcAndroidManagementChecker::StartCheck(const CheckCallback& callback) {
   if (policy_util::IsAccountManaged(profile_) ||
       policy::BrowserPolicyConnector::IsNonEnterpriseUser(
           profile_->GetProfileUserName())) {
-    callback.Run(policy::AndroidManagementClient::Result::UNMANAGED);
+    std::move(callback).Run(policy::AndroidManagementClient::Result::UNMANAGED);
     return;
   }
 
-  callback_ = callback;
+  callback_ = std::move(callback);
   EnsureRefreshTokenLoaded();
 }
 
