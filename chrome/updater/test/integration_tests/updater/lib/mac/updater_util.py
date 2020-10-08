851ead10bdf351bc2b7170a3eadb9920a0a53e5c
adoneria <adoneria@google.com>
2020-10-08 19:21:37 +0000

    [updater] add structure for updater install test for windows.
    This is the prelim CL to the implementation of updater install test
    to create a basic folder structure and import constants from C++
    integration tests.
    
    Bug: 1130847
    Change-Id: I0d414517f749d76f54e66643a0d64b4e43df6c6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422092
    Commit-Queue: Anjali Doneria <adoneria@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815266}

---
+++ chrome/updater/test/integration_tests/updater/lib/mac/updater_util.py
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Updater utility functions for mac platform."""
+
+import logging
+
+def Clean():
+  pass
+
+def ExpectClean():
+  pass
+
+def GetExecutablePath():
+  pass
+
+def GetInstallerPath():
+  pass
+
+def Install():
+  pass
