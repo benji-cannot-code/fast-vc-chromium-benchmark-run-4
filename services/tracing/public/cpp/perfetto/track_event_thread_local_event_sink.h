edb1abe42f9986e35e8ce68ddb0faf89e2490d41
Eric Seckler <eseckler@chromium.org>
2020-10-08 14:32:42 +0000

    perfetto: Write track descriptors for custom perfetto::Tracks
    Up to now, while it is possible to pass custom perfetto::Track objects
    to typed TRACE_EVENT macros, the TrackDescriptors for these tracks were
    not written to the trace.
    
    This patch hooks up typed macros to perfetto's TrackRegistry. For this
    purpose, it adds another delegation callback through which the layer in
    //base can request a new TracePacket (for the descriptor) from the
    event sink in services/tracing.
    
    TBR: weili@chromium.org
    Bug: 1065905
    Change-Id: Iadc7c47b9840a3c18ab9372cb7e3361c9b78d3d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2455608
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815131}

--- services/tracing/public/cpp/perfetto/track_event_thread_local_event_sink.h
+++ services/tracing/public/cpp/perfetto/track_event_thread_local_event_sink.h
@@ -33,7 +33,8 @@ namespace tracing {
 // ThreadLocalEventSink that emits TrackEvent protos.
 class COMPONENT_EXPORT(TRACING_CPP) TrackEventThreadLocalEventSink
     : public base::ThreadIdNameManager::Observer,
-      public base::trace_event::TrackEventHandle::CompletionListener {
+      public base::trace_event::TrackEventHandle::CompletionListener,
+      public base::trace_event::TracePacketHandle::CompletionListener {
  public:
   enum class IndexType {
     kName = 0,
@@ -76,6 +77,7 @@ class COMPONENT_EXPORT(TRACING_CPP) TrackEventThreadLocalEventSink
 
   base::trace_event::TrackEventHandle AddTypedTraceEvent(
       base::trace_event::TraceEvent* trace_event);
+  base::trace_event::TracePacketHandle AddTracePacket();
 
   void UpdateDuration(
       const unsigned char* category_group_enabled,
@@ -95,6 +97,9 @@ class COMPONENT_EXPORT(TRACING_CPP) TrackEventThreadLocalEventSink
 
   // base::trace_event::TrackEventHandle::CompletionListener implementation:
   void OnTrackEventCompleted() override;
+
+  // base::trace_event::TracePacketHandle::CompletionListener implementation:
+  void OnTracePacketCompleted() override;
 
  private:
   static constexpr size_t kMaxCompleteEventDepth = 30;
