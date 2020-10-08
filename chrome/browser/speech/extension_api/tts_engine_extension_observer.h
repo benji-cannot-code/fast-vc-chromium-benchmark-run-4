2122ef8908e2e8990aa96d3f30f74fb70ad00cd1
David Tseng <dtseng@google.com>
2020-10-08 01:39:21 +0000

    Native audio tts playback
    Moves text-to-speech playback from js-based web audio to C++.
    See bug for more details.
    
    Depends on
    https://chromium-review.googlesource.com/c/chromiumos/overlays/chromiumos-overlay/+/2456208/1
    
    Fixed: 1134289
    Change-Id: I7ecfa800f6b688f6a3d0d655f557e86efa73188a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2435919
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814980}

--- chrome/browser/speech/extension_api/tts_engine_extension_observer.h
+++ chrome/browser/speech/extension_api/tts_engine_extension_observer.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/scoped_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "content/public/browser/audio_service.h"
 #include "extensions/browser/event_router.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_registry_observer.h"
