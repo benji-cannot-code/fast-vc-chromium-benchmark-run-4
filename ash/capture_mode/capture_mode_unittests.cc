1bf856f237bfa95b350ef6f3fa396599e3fb6834
Xiaoqian Dai <xdai@chromium.org>
2020-10-08 01:55:06 +0000

    capture mode: Implement the capture icon/message and count down UI.
    In different capture mode type and source, we may show different icons/
    messages in middle of the screen or in middle of the captured region and
    prompt the user to do the next step. And in video recording scenario,
    this message will transform into a 3-seconds countdown timer before
    starting video recording.
    
    This CL implements
    - showing different icon/text message in different scenarios
    - showing a 3-seconds countdown UI before video recording
    
    Animation is not implemented in this CL and will be addressed in a
    following CL.
    
    Bug: 1126669
    
    Change-Id: Id07b4c1137eb77946695899603ae43aeda84c643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2439778
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#814985}

--- ash/capture_mode/capture_mode_unittests.cc
+++ ash/capture_mode/capture_mode_unittests.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/capture_mode/capture_label_view.h"
 #include "ash/capture_mode/capture_mode_bar_view.h"
 #include "ash/capture_mode/capture_mode_close_button.h"
 #include "ash/capture_mode/capture_mode_controller.h"
@@ -154,6 +155,19 @@ class CaptureModeTest : public AshTestBase {
         ->GetNativeWindow();
   }
 
+  void WaitForCountDownToFinish() {
+    auto* controller = CaptureModeController::Get();
+    DCHECK(controller->IsActive());
+    DCHECK_EQ(controller->type(), CaptureModeType::kVideo);
+    while (!controller->is_recording_in_progress()) {
+      base::RunLoop run_loop;
+      base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+          FROM_HERE, run_loop.QuitClosure(),
+          base::TimeDelta::FromMilliseconds(100));
+      run_loop.Run();
+    }
+  }
+
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
 };
@@ -221,10 +235,12 @@ TEST_F(CaptureModeTest, VideoRecordingUiBehavior) {
   EXPECT_TRUE(controller->IsActive());
   EXPECT_FALSE(controller->is_recording_in_progress());
   EXPECT_FALSE(IsCursorCompositingEnabled());
+  CaptureLabelView::SetUseDelayForTesting(true);
 
   // Hit Enter to begin recording.
   auto* event_generator = GetEventGenerator();
   SendKey(ui::VKEY_RETURN, event_generator);
+  WaitForCountDownToFinish();
   EXPECT_FALSE(controller->IsActive());
   EXPECT_TRUE(controller->is_recording_in_progress());
 
