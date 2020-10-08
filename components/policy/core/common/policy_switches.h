0b7ea8bfae9711ecc8a52a7cf1ca0ca4ebaad16a
Zach Trudo <zatrudo@google.com>
2020-10-08 03:46:06 +0000

    Add EncryptedReportingUrl
    CloudPolicyClient will be expanded to make use of a new server endpoint.
    This change prepares BrowserPolicyConnector for that eventuality.
    
    Bug: chromium:1135951
    Change-Id: I30ffd427dbcb21399249fba80a394c3de9689c5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2453345
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/master@{#815007}

--- components/policy/core/common/policy_switches.h
+++ components/policy/core/common/policy_switches.h
@@ -15,6 +15,7 @@ namespace switches {
 
 POLICY_EXPORT extern const char kDeviceManagementUrl[];
 POLICY_EXPORT extern const char kRealtimeReportingUrl[];
+POLICY_EXPORT extern const char kEncryptedReportingUrl[];
 POLICY_EXPORT extern const char kUserAlwaysAffiliated[];
 POLICY_EXPORT extern const char kChromePolicy[];
 
