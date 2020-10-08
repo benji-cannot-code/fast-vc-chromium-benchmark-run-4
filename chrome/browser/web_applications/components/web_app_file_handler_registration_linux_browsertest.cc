82757e1e73ed580fe206edba21ff273706c1dbf3
Eric Willigers <ericwilligers@chromium.org>
2020-10-08 04:15:50 +0000

    WebAppFileHandlerRegistrationLinuxBrowserTest tidy
    Desktop PWAs without Extensions (BMO) has shipped.
    
    The test no longer needs to be parameterized.
    
    Bug: 1065748
    Change-Id: I410e0eaec8a18e0232155214b29bf22c79bdf07d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2459257
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815014}

--- chrome/browser/web_applications/components/web_app_file_handler_registration_linux_browsertest.cc
+++ chrome/browser/web_applications/components/web_app_file_handler_registration_linux_browsertest.cc
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/externally_installed_web_app_prefs.h"
 #include "chrome/browser/web_applications/components/web_app_id.h"
 #include "chrome/browser/web_applications/components/web_app_provider_base.h"
-#include "chrome/browser/web_applications/test/web_app_test.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/services/app_service/public/cpp/file_handler.h"
 #include "content/public/test/browser_test.h"
@@ -53,20 +51,11 @@ apps::FileHandler GetTestFileHandler(const std::string& action,
 }  // namespace
 
 class WebAppFileHandlerRegistrationLinuxBrowserTest
-    : public InProcessBrowserTest,
-      public ::testing::WithParamInterface<ProviderType> {
+    : public InProcessBrowserTest {
  protected:
   WebAppFileHandlerRegistrationLinuxBrowserTest() {
-    if (GetParam() == ProviderType::kWebApps) {
-      scoped_feature_list_.InitWithFeatures(
-          {blink::features::kFileHandlingAPI,
-           features::kDesktopPWAsWithoutExtensions},
-          {});
-    } else if (GetParam() == ProviderType::kBookmarkApps) {
-      scoped_feature_list_.InitWithFeatures(
-          {blink::features::kFileHandlingAPI},
-          {features::kDesktopPWAsWithoutExtensions});
-    }
+    scoped_feature_list_.InitAndEnableFeature(
+        blink::features::kFileHandlingAPI);
   }
 
   AppRegistrar& registrar() {
@@ -85,7 +74,7 @@ class WebAppFileHandlerRegistrationLinuxBrowserTest
 
 // Verify that the MIME type registration callback is called and that
 // the caller behaves as expected.
-IN_PROC_BROWSER_TEST_P(WebAppFileHandlerRegistrationLinuxBrowserTest,
+IN_PROC_BROWSER_TEST_F(WebAppFileHandlerRegistrationLinuxBrowserTest,
                        RegisterMimeTypesOnLinuxCallbackCalledSuccessfully) {
   ASSERT_TRUE(embedded_test_server()->Start());
   GURL url(embedded_test_server()->GetURL(
@@ -119,11 +108,5 @@ IN_PROC_BROWSER_TEST_P(WebAppFileHandlerRegistrationLinuxBrowserTest,
   EXPECT_EQ(InstallResultCode::kSuccessNewInstall, result_code_.value());
   ASSERT_TRUE(path_reached);
 }
-
-INSTANTIATE_TEST_SUITE_P(All,
-                         WebAppFileHandlerRegistrationLinuxBrowserTest,
-                         ::testing::Values(ProviderType::kBookmarkApps,
-                                           ProviderType::kWebApps),
-                         ProviderTypeParamToString);
 
 }  // namespace web_app
