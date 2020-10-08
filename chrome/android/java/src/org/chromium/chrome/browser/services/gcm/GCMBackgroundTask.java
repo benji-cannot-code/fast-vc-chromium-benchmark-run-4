6c745a249a5508f3cba89788cbf80ae00fe621d9
Clark DuVall <cduvall@chromium.org>
2020-10-08 03:26:02 +0000

    Convert GCM related service classes to split compat
    This change converts GCM service classes, and adds new SplitCompat
    services for GcmTaskService and GcmListenerService.
    
    Review from Patchset 1 to get the right diffs on the changes in the Impl
    classes.
    
    Bug: 1126301
    Change-Id: I2bdb1f8c1b8ccefd45bfb786a9190970db9d8f3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2459207
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815004}

--- chrome/android/java/src/org/chromium/chrome/browser/services/gcm/GCMBackgroundTask.java
+++ chrome/android/java/src/org/chromium/chrome/browser/services/gcm/GCMBackgroundTask.java
@@ -43,7 +43,7 @@ public class GCMBackgroundTask implements BackgroundTask {
             return false;
         }
 
-        ChromeGcmListenerService.dispatchMessageToDriver(message);
+        ChromeGcmListenerServiceImpl.dispatchMessageToDriver(message);
         return false;
     }
 
