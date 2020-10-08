8ef0583e4653ec0c288ec3375d649193ce9edeb7
Fabio Rocha <fabio.rocha@microsoft.com>
2020-10-08 15:45:46 +0000

    dpwa: Remove scoped feature list from WebAppIconManagerBrowserTest
    Remove scoped feature list from WebAppIconManagerBrowserTest in
    web_app_icon_manager_browsertest.cc, now that
    kDesktopPWAsWithoutExtensions is always true.
    
    Bug: 1065748
    Change-Id: I49fb16c4c4b3fbcdc791654cff60912fb848c3c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2459806
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Commit-Queue: Fabio Rocha <fabio.rocha@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#815157}

--- chrome/browser/web_applications/web_app_icon_manager_browsertest.cc
+++ chrome/browser/web_applications/web_app_icon_manager_browsertest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind_test_util.h"
-#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/apps/app_service/app_launch_params.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
@@ -36,10 +35,7 @@ namespace web_app {
 
 class WebAppIconManagerBrowserTest : public InProcessBrowserTest {
  public:
-  WebAppIconManagerBrowserTest() {
-    scoped_feature_list_.InitWithFeatures(
-        {features::kDesktopPWAsWithoutExtensions}, {});
-  }
+  WebAppIconManagerBrowserTest() = default;
   WebAppIconManagerBrowserTest(const WebAppIconManagerBrowserTest&) = delete;
   WebAppIconManagerBrowserTest& operator=(const WebAppIconManagerBrowserTest&) =
       delete;
@@ -62,7 +58,6 @@ class WebAppIconManagerBrowserTest : public InProcessBrowserTest {
   apps::AppServiceTest& app_service_test() { return app_service_test_; }
 
  private:
-  base::test::ScopedFeatureList scoped_feature_list_;
   net::EmbeddedTestServer https_server_;
   apps::AppServiceTest app_service_test_;
 
