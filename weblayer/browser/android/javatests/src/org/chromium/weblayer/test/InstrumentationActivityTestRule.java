48a152b51e883e84fdcb743572e2c467dcdb184a
Scott Violet <sky@chromium.org>
2020-10-08 22:38:34 +0000

    weblayer: fixs possible NPE if a11y enabled during Tab creation
    If a11y is enabled, then Tab creation may trigger calling some
    code paths that assume mBrowser was non-null.
    
    BUG=1136596
    TEST=TabPrivateTest.testCreateTabWithAccessibilityEnabledCrashTest
    
    Change-Id: I500fd70cb56b7d16c22284acb677c06ec57b536d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461714
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815357}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/InstrumentationActivityTestRule.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/InstrumentationActivityTestRule.java
@@ -8,6 +8,7 @@ package org.chromium.weblayer.test;
 import android.app.Activity;
 import android.app.Instrumentation.ActivityMonitor;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
@@ -74,10 +75,12 @@ public class InstrumentationActivityTestRule
     }
 
     public WebLayer getWebLayer() {
-        return TestThreadUtils.runOnUiThreadBlockingNoException(() -> {
-            return WebLayer.loadSync(
-                    InstrumentationRegistry.getTargetContext().getApplicationContext());
-        });
+        return TestThreadUtils.runOnUiThreadBlockingNoException(
+                () -> { return WebLayer.loadSync(getContextForWebLayer()); });
+    }
+
+    public Context getContextForWebLayer() {
+        return InstrumentationRegistry.getTargetContext().getApplicationContext();
     }
 
     /**
@@ -104,7 +107,7 @@ public class InstrumentationActivityTestRule
         Assert.assertNotNull(activity);
         try {
             TestThreadUtils.runOnUiThreadBlocking(
-                    () -> activity.loadWebLayerSync(activity.getApplicationContext()));
+                    () -> activity.loadWebLayerSync(getContextForWebLayer()));
         } catch (ExecutionException e) {
             throw new RuntimeException(e);
         }
