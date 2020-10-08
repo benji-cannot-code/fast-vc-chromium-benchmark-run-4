31831a3f54fc31a9bb996c8a0c04f17ce0ba13a8
Xi Han <hanxi@google.com>
2020-10-08 22:32:20 +0000

    [Instant Start] Avoid extra omnibox when changing themes.
    The bug happens when the instant start startup code path is called in a
    warm start, i.e., ChromeTabbedActivity is recreated when the dark/light
    theme is changed. We add a check of warm start to prevent this from
    happening.
    
    Bug: 1117226
    Change-Id: I65b48afd03137c33b5ed1a726aac23f9dffd7e6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2454254
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815353}

--- chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
@@ -160,7 +160,8 @@ public abstract class AsyncInitializationActivity extends ChromeBaseAppCompatAct
 
     @Override
     public final void setContentViewAndLoadLibrary(Runnable onInflationCompleteCallback) {
-        boolean enableInstantStart = TabUiFeatureUtilities.supportInstantStart(isTablet());
+        boolean enableInstantStart =
+                TabUiFeatureUtilities.supportInstantStart(isTablet()) && !mHadWarmStart;
         mOnInflationCompleteCallback = onInflationCompleteCallback;
         if (enableInstantStart) {
             triggerLayoutInflation();
