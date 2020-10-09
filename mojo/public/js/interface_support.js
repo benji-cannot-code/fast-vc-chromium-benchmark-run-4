22f6f8a71311ca35735697a41d5f2c3faf5c87d4
Ken Rockot <rockot@google.com>
2020-10-09 03:39:43 +0000

    Mojo: Introduce Mojo JS bindings modules
    This CL introduces generated mojom.m.js modules for mojom JS bindings,
    allowing most JS bindings consumers to import bindings with the
    convenience and efficiency afforded by JS modules.
    
    These modules can be used as-is by Component Extensions, Web Platform
    Tests, and Blink Web Tests. A follow-up change will introduce a slight
    variation on these modules to support consumption by WebUI pages.
    
    Bug: 1004256
    Change-Id: Ic2b4a6bcadf68023d2078bf065befafce3ccb19b
    Tbr: sky@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2451589
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815471}

--- mojo/public/js/interface_support.js
+++ mojo/public/js/interface_support.js
@@ -2,14 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-'use strict';
-
-goog.require('mojo.interfaceControl.RUN_MESSAGE_ID');
-goog.require('mojo.interfaceControl.RunResponseMessageParamsSpec');
-goog.require('mojo.internal');
-
-goog.provide('mojo.internal.interfaceSupport');
-
 
 /**
  * Handles incoming interface control messages on a remote or router endpoint.
