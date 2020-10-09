0fc1eead62e230efcfdf592e5aa2c4b88f85c903
Peter Kasting <pkasting@chromium.org>
2020-10-09 08:32:16 +0000

    Change ButtonPressed overrides to callbacks: c/b/ui/views/overlay/
    Bug: 772945
    Change-Id: I81d45dbfefbf4b4f0e66ece5a815c729cba01c83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461208
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#815538}

--- chrome/browser/ui/views/overlay/overlay_window_views.h
+++ chrome/browser/ui/views/overlay/overlay_window_views.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/timer/timer.h"
 #include "ui/gfx/geometry/size.h"
-#include "ui/views/controls/button/button.h"
 #include "ui/views/widget/widget.h"
 
 #if defined(OS_CHROMEOS)
@@ -29,7 +28,6 @@ class TrackImageButton;
 // The Chrome desktop implementation of OverlayWindow. This will only be
 // implemented in views, which will support all desktop platforms.
 class OverlayWindowViews : public content::OverlayWindow,
-                           public views::ButtonListener,
                            public views::Widget {
  public:
   static std::unique_ptr<content::OverlayWindow> Create(
@@ -68,9 +66,6 @@ class OverlayWindowViews : public content::OverlayWindow,
   void OnKeyEvent(ui::KeyEvent* event) override;
   void OnMouseEvent(ui::MouseEvent* event) override;
   void OnGestureEvent(ui::GestureEvent* event) override;
-
-  // views::ButtonListener:
-  void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
   // Gets the bounds of the controls.
   gfx::Rect GetBackToTabControlsBounds();
