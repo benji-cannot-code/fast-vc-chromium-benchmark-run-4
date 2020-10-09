e6dd8e67a13cae45e8c0d0495448e719b088345c
Mario Bianucci <mabian@microsoft.com>
2020-10-09 19:32:01 +0000

    Reland "De-couple PredictionMetricsHandler from specific histograms"
    This is a reland of 1d11f9fd034ea02b7c1a913c2267ac9224ac7003
    
    The original CL was reverted because of
    InputHandlerProxyEventQueueTest.VSyncAlignedGestureScrollPinchScroll
    failing on ChromiumOS MSan builders. This test was flaky in the exact
    same way prior to my change according to the flakiness dashboard, and
    ASan and TSan variants are already disabled for what is likely the same
    issue. The tests fails at roughly the same rate both with and without
    this CL. Therefore, I spoke with the reverter and we decided it was
    acceptable to disable the test on MSan.
    
    It is disabled on ASan and TSan via crbug.com/1029250, and I will leave
    a comment there about MSan also being disabled.
    
    Original change's description:
    > De-couple PredictionMetricsHandler from specific histograms
    >
    > PredictionMetricsHandler appears to be designed to work for anything
    > that is predicting points. However, it currently only reports metrics
    > in the Event.InputEventPrediction.Scroll* histograms. De-couple the
    > class from these histograms so it can be used for things only than
    > scroll metrics.
    >
    > Bug: 1133785
    > Change-Id: Ib34129fc46b20b45810cbd5ca195483e88c513e2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2441197
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Commit-Queue: Mario Bianucci <mabian@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#813813}
    
    Bug: 1133785
    Change-Id: Id02ea108e0f74f4f454049b5957bb7f10287cc8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2451591
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Mario Bianucci <mabian@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#815738}

--- third_party/blink/renderer/platform/widget/input/scroll_predictor.cc
+++ third_party/blink/renderer/platform/widget/input/scroll_predictor.cc
@@ -14,7 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-ScrollPredictor::ScrollPredictor() {
+ScrollPredictor::ScrollPredictor()
+    : metrics_handler_("Event.InputEventPrediction.Scroll") {
   // Get the predictor from feature flags
   std::string predictor_name = GetFieldTrialParamValueByFeature(
       blink::features::kResamplingScrollEvents, "predictor");
