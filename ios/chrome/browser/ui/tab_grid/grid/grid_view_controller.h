e2d98c3ad900608b14528bf973272a605ea67acc
Roberto Moura <mouraroberto@google.com>
2020-10-09 23:50:10 +0000

    Add functionality to the New Tab button cell in the Grid View.
    Open a new tab when the user taps the New Tab button cell.
    Extend the GridViewControllerDelegate protocol to tell the delegate when
    a new tab is opened.
    
    Bug: 1127604, 1135329
    Change-Id: If35f357df1c6a5f8eb9b181e76fb78ca241e0656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2449994
    Commit-Queue: Roberto Moura <mouraroberto@google.com>
    Auto-Submit: Roberto Moura <mouraroberto@google.com>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815851}

--- ios/chrome/browser/ui/tab_grid/grid/grid_view_controller.h
+++ ios/chrome/browser/ui/tab_grid/grid/grid_view_controller.h
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // |gridViewController|.
 - (void)gridViewController:(GridViewController*)gridViewController
         didCloseItemWithID:(NSString*)itemID;
+// Tells the delegate that the plus sign was tapped in |gridViewController|,
+// i.e., there was an intention to create a new item.
+- (void)didTapPlusSignInGridViewController:
+    (GridViewController*)gridViewController;
 // Tells the delegate that the item at |sourceIndex| was moved to
 // |destinationIndex|.
 - (void)gridViewController:(GridViewController*)gridViewController
--- ios/chrome/browser/ui/tab_grid/grid/grid_view_controller_unittest.mm
+++ ios/chrome/browser/ui/tab_grid/grid/grid_view_controller_unittest.mm
@@ -52,6 +52,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // No-op for unittests. This is only called when a user taps to close a cell,
   // not generically when items are removed from the data source.
 }
+- (void)didTapPlusSignInGridViewController:
+    (GridViewController*)gridViewController {
+  // No-op for unittests. This is only called when a user taps on a
+  // plus sign cell, not generically when items are added to the data source.
+}
 @end
 
 class GridViewControllerTest : public RootViewControllerTest {
