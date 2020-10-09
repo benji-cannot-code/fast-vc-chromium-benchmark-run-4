ff88734f2cd996cc91496f4b527699918bbecb81
Tim Song <tengs@chromium.org>
2020-10-09 22:19:55 +0000

    [CrOS PhoneHub] Add unit test for notification settings button.
    BUG=1106937,1126208
    
    Change-Id: Ibb9cfbc40f19d45d34082289d1cd999507e36ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461605
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815814}

--- ash/system/phonehub/phone_hub_notification_controller_unittest.cc
+++ ash/system/phonehub/phone_hub_notification_controller_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/phonehub/phone_hub_notification_controller.h"
 
+#include "ash/public/cpp/test/test_system_tray_client.h"
 #include "ash/shell.h"
 #include "ash/system/message_center/message_center_controller.h"
 #include "ash/test/ash_test_base.h"
@@ -170,7 +171,13 @@ TEST_F(PhoneHubNotificationControllerTest, InlineReply) {
 }
 
 TEST_F(PhoneHubNotificationControllerTest, ClickSettings) {
-  // TODO(tengs): Test this case once it is implemented.
+  notification_manager_.SetNotificationsInternal(fake_notifications_);
+  EXPECT_TRUE(
+      message_center_->FindVisibleNotificationById(kCrOSNotificationId0));
+  EXPECT_EQ(0, GetSystemTrayClient()->show_connected_devices_settings_count());
+
+  message_center_->ClickOnSettingsButton(kCrOSNotificationId0);
+  EXPECT_EQ(1, GetSystemTrayClient()->show_connected_devices_settings_count());
 }
 
 TEST_F(PhoneHubNotificationControllerTest, CancelReply) {
