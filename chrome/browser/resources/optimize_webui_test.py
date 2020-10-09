a3961ca6ade1ecff1a2bf5509e0cf810ecc09eab
rbpotter <rbpotter@chromium.org>
2020-10-09 20:54:00 +0000

    WebUI: Generate manifest files from optimize_webui
    Change optimize_webui to generate a manifest file when an |out_manifest|
    argument is provided.
    
    Started from:
    https://chromium-review.googlesource.com/c/chromium/src/+/2443211
    by dpapad@
    
    Bug: 1132403
    Change-Id: Ia80c4d2c004ec4414c500c407e2d2c8d187ff847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463962
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815777}

--- chrome/browser/resources/optimize_webui_test.py
+++ chrome/browser/resources/optimize_webui_test.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import json
 import optimize_webui
 import os
 import shutil
@@ -11,14 +12,15 @@ import tempfile
 import unittest
 
 
+_CWD = os.getcwd()
 _HERE_DIR = os.path.dirname(__file__)
 
 
 class OptimizeWebUiTest(unittest.TestCase):
   def setUp(self):
-    self._out_folder = None
     self._tmp_dirs = []
     self._tmp_src_dir = None
+    self._out_folder = self._create_tmp_dir()
 
   def tearDown(self):
     for tmp_dir in self._tmp_dirs:
@@ -46,8 +48,6 @@ class OptimizeWebUiTest(unittest.TestCase):
     return open(os.path.join(self._out_folder, file_name), 'r').read()
 
   def _run_optimize(self, input_args):
-    assert not self._out_folder
-    self._out_folder = self._create_tmp_dir()
     # TODO(dbeam): make it possible to _run_optimize twice? Is that useful?
     args = input_args + [
       '--depfile', os.path.join(self._out_folder, 'depfile.d'),
@@ -210,6 +210,7 @@ import './element_in_dir/element_in_dir.js';
     args = [
       '--js_module_in_files', 'ui.js', 'lazy.js',
       '--js_out_files', 'ui.rollup.js', 'lazy.rollup.js', 'shared.rollup.js',
+      '--out-manifest', os.path.join(self._out_folder, 'out_manifest.json'),
     ]
     self._run_optimize(args)
 
@@ -234,6 +235,16 @@ import './element_in_dir/element_in_dir.js';
     self._check_output_depfile(False)
     depfile_d = self._read_out_file('depfile.d')
     self.assertIn('lazy_element.js', depfile_d)
+
+    manifest = json.loads(self._read_out_file('out_manifest.json'))
+    self.assertEquals(3, len(manifest['files']))
+    self.assertTrue('lazy.rollup.js' in manifest['files'])
+    self.assertTrue('ui.rollup.js' in manifest['files'])
+    self.assertTrue('shared.rollup.js' in manifest['files'])
+
+    self.assertEquals(
+        os.path.relpath(self._out_folder, _CWD).replace('\\', '/'),
+        os.path.relpath(manifest['base_dir'], _CWD).replace('\\', '/'))
 
 if __name__ == '__main__':
   unittest.main()
