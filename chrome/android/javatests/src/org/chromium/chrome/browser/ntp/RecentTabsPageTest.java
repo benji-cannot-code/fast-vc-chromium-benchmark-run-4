4e887cadddfa1c3234a26d1a6c70a55e4e72079c
Tanmoy Mollik <triploblastic@chromium.org>
2020-10-09 15:55:24 +0000

    [Android] Add test resource for user profile picture
    Add a test resource to use in place of user profile picture in tests.
    The tests that require it are also updated.
    
    Bug: 1125452
    Change-Id: I0b6323b4c9d76816e3216c14d9ca5cd338c60b9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401658
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815627}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentTabsPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentTabsPageTest.java
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ntp;
 
 import android.accounts.Account;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.support.test.InstrumentationRegistry;
 import android.view.View;
 
@@ -114,8 +110,9 @@ public class RecentTabsPageTest {
         TestThreadUtils.runOnUiThreadBlocking(
                 ()
                         -> mFakeProfileDataSource.setProfileData(account.name,
-                                new ProfileDataSource.ProfileData(
-                                        account.name, createAvatar(), "Full Name", "Given Name")));
+                                new ProfileDataSource.ProfileData(account.name,
+                                        mAccountManagerTestRule.createProfileImage(), "Full Name",
+                                        "Given Name")));
         RecentTabsManager.forcePromoStateForTests(
                 RecentTabsManager.PromoState.PROMO_SIGNIN_PERSONALIZED);
         mPage = loadRecentTabsPage();
@@ -130,8 +127,9 @@ public class RecentTabsPageTest {
         TestThreadUtils.runOnUiThreadBlocking(
                 ()
                         -> mFakeProfileDataSource.setProfileData(account.name,
-                                new ProfileDataSource.ProfileData(
-                                        account.name, createAvatar(), "Full Name", "Given Name")));
+                                new ProfileDataSource.ProfileData(account.name,
+                                        mAccountManagerTestRule.createProfileImage(), "Full Name",
+                                        "Given Name")));
         RecentTabsManager.forcePromoStateForTests(
                 RecentTabsManager.PromoState.PROMO_SYNC_PERSONALIZED);
         mPage = loadRecentTabsPage();
@@ -201,24 +199,5 @@ public class RecentTabsPageTest {
                 InstrumentationRegistry.getInstrumentation(), view);
         Assert.assertTrue(InstrumentationRegistry.getInstrumentation().invokeContextMenuAction(
                 mActivityTestRule.getActivity(), contextMenuItemId, 0));
-    }
-
-    /**
-     * TODO(https://crbug.com/1125452): Remove this method and use test only resource.
-     */
-    private Bitmap createAvatar() {
-        final int avatarSize = 40;
-
-        Bitmap result = Bitmap.createBitmap(avatarSize, avatarSize, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(result);
-        canvas.drawColor(Color.RED);
-
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-
-        paint.setColor(Color.BLUE);
-        canvas.drawCircle(0, 0, avatarSize, paint);
-
-        return result;
     }
 }
