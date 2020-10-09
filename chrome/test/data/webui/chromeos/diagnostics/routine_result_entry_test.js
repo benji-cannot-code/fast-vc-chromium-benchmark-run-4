749fd9a0aa9d71a38b8fe068d411590c41c08bf9
Zentaro Kavanagh <zentaro@chromium.org>
2020-10-09 01:46:20 +0000

    Diagnostics: Add skeleton routine-result-entry element
    - This is will represent a line item in the list of routine results
    
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I597f8f1d03e13d413978c185fa97a72611fe2072
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461720
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815432}

---
+++ chrome/test/data/webui/chromeos/diagnostics/routine_result_entry_test.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO(jimmyxgong): Use es6 module for mojo binding (crbug/1004256).
+import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
+import 'chrome://diagnostics/routine_result_entry.js';
+import {flushTasks} from 'chrome://test/test_util.m.js';
+
+suite('RoutineResultEntryTest', () => {
+  /** @type {?HTMLElement} */
+  let routineResultEntryElement = null;
+
+  setup(function() {
+    PolymerTest.clearBody();
+  });
+
+  teardown(function() {
+    if (routineResultEntryElement) {
+      routineResultEntryElement.remove();
+    }
+    routineResultEntryElement = null;
+  });
+
+  function initializeRoutineResultEntry() {
+    assertFalse(!!routineResultEntryElement);
+
+    // Add the entry to the DOM.
+    routineResultEntryElement = document.createElement('routine-result-entry');
+    assertTrue(!!routineResultEntryElement);
+    document.body.appendChild(routineResultEntryElement);
+
+    return flushTasks();
+  }
+
+  test('ElementRendered', () => {
+    return initializeRoutineResultEntry().then(() => {
+      // Verify the element rendered.
+      let div = routineResultEntryElement.$$('.entryRow');
+      assertTrue(!!div);
+    });
+  });
+});
