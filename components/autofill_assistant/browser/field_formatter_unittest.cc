190b59b338126acd02c3244399f3a347fb1ca753
sandromaggi <sandromaggi@google.com>
2020-10-09 11:12:15 +0000

    [Autofill Assistant] Add new credit card field
    This change adds the non-padded month for credit card expiry to deal
    with forms where the month is "1" for January (instead of "01").
    
    Bug: b/168227057
    Change-Id: Ibf030936e638ea5df57fe8e3da36fe6cf06589ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461314
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#815568}

--- components/autofill_assistant/browser/field_formatter_unittest.cc
+++ components/autofill_assistant/browser/field_formatter_unittest.cc
@@ -109,6 +109,11 @@ TEST(FieldFormatterTest, CreditCard) {
   EXPECT_EQ(*FormatString("${-2} ${-5}",
                           CreateAutofillMappings(credit_card, "en-US")),
             "visa Visa");
+
+  // CREDIT_CARD_NON_PADDED_EXP_MONTH
+  EXPECT_EQ(
+      *FormatString("${-7}", CreateAutofillMappings(credit_card, "en-US")),
+      "1");
 }
 
 TEST(FieldFormatterTest, SpecialCases) {
@@ -190,6 +195,7 @@ TEST(FieldFormatterTest, AddsAllProfileFields) {
 
 TEST(FieldFormatterTest, AddsAllCreditCardFields) {
   std::map<std::string, std::string> expected_values = {
+      {"-7", "8"},
       {"-5", "Visa"},
       {"-4", "1111"},
       {"-2", "visa"},
