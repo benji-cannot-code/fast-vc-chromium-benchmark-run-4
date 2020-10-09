d761189d660c84f9417418048a70bff9a9004c1d
Ryan Daum <rdaum@chromium.org>
2020-10-09 13:30:37 +0000

    [chromecast] Webview text input focus events
      * Registers and watch for text input focus events within webview
        content.
    
      * Defines a new gRPC payload for said events, and broadcasts them.
    
    Bug: internal b/161811385
    Test: manually with wayland client tool
    Change-Id: Ie6d5167bc21f40d603e990b08a3e27b3802eeafd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2422501
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Commit-Queue: Ryan Daum <rdaum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815585}

--- chromecast/browser/webview/web_content_controller.h
+++ chromecast/browser/webview/web_content_controller.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/surface_observer.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "ui/base/ime/input_method.h"
+#include "ui/base/ime/input_method_observer.h"
 #include "ui/events/gestures/gesture_recognizer_impl.h"
 
 namespace aura {
@@ -139,6 +141,9 @@ class WebContentController
   std::set<std::string> current_javascript_channel_set_;
   std::set<content::RenderFrameHost*> current_render_frame_set_;
   std::set<content::RenderWidgetHost*> current_render_widget_set_;
+
+  // Used to watch key focus events.
+  std::unique_ptr<ui::InputMethodObserver> input_method_observer_;
 
   base::WeakPtrFactory<WebContentController> weak_ptr_factory_{this};
 
