7d9478dfa770f89e2f6a603275e514cb088602c7
Morten Stenshorne <mstensho@chromium.org>
2020-10-09 16:16:12 +0000

    Compile core/mathml/ with -Wshadow.
    Bug: 294205
    Change-Id: I13561748b18f77d6040bdf1256e3dd003a1b8b4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461381
    Reviewed-by: Rob Buis <rbuis@igalia.com>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815637}

--- third_party/blink/renderer/core/mathml/mathml_operator_element.cc
+++ third_party/blink/renderer/core/mathml/mathml_operator_element.cc
@@ -198,7 +198,7 @@ void MathMLOperatorElement::ComputeDictionaryCategory() {
            fallback_form++) {
         if (fallback_form == form)
           continue;
-        auto category = FindCategory(
+        category = FindCategory(
             GetOperatorContent().characters,
             static_cast<MathMLOperatorDictionaryForm>(fallback_form));
         if (category != MathMLOperatorDictionaryCategory::kNone) {
