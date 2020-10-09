70df914c9fe51918b31e94966f61cd3174aca03b
Rachel Carpenter <carpenterr@chromium.org>
2020-10-09 04:42:13 +0000

    Remove Genius app.
    All entry points were deleted several milestones ago, and the component
    extension has been uninstalled for anyone who has updated to either of
    the milestones in the interim.
    
    Actual code for the app lives elsewhere, will also be deleted.
    
    Bug: b/159863346
    Change-Id: I2792ea8d9482b9199b4dc6a394a8f659d176108b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2454960
    Commit-Queue: Rachel Carpenter <carpenterr@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815489}

--- extensions/common/constants.cc
+++ extensions/common/constants.cc
@@ -132,7 +132,6 @@ const char kGoogleSheetsDemoAppId[] = "nifkmgcdokhkjghdlgflonppnefddien";
 const char kGoogleSlidesDemoAppId[] = "hdmobeajeoanbanmdlabnbnlopepchip";
 const char kGoogleKeepAppId[] = "hmjkmjkepdijhoojdojkdfohbdgmmhki";
 const char kYoutubeAppId[] = "blpcfgokakmgnkcojhhkbfbldkacnbeo";
-const char kGeniusAppId[] = "ljoammodoonkhnehlncldjelhidljdpi";
 
 #if defined(OS_CHROMEOS)
 // TODO(michaelpg): Deprecate old app IDs before adding new ones to avoid bloat.
