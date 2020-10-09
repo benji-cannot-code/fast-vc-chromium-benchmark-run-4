91790a10ca9614cf524fa00575219b428c2b7d28
Sorin Jianu <sorin@chromium.org>
2020-10-09 20:09:24 +0000

    Implement support for metainstaller tagging.
    This CL introduces a --tag command line argument and handing
    of metainstaller tags.
    
    If the tag is present in the metainstaller, then the tag is extracted,
    and the updater is invoked with --tag. This removes the hardcoded
    --install command line argument that the metainstaller had temporarily
    used so far.
    
    For now, the metainstaller could be invoked in one of
    the following ways:
    1. sign and tag it (using the steps in the design doc), then run it
    without arguments.
    2. run it with --tag=appguid={8A69D345-D564-463c-AFF1-A69D9E530F96}
    3. run it with --install --app-id={8A69D345-D564-463c-AFF1-A69D9E530F96}
    
    There is a fix in the sign.py script to copy the build contents instead
    of compressing them, to keep the repacked archive similar to what it
    was before signing.
    
    The metainstaller code does understand BCJ (but it understand BCJ2) so
    in the future, if compression is needed, use BCJ2 and LZMA.
    
    Bug: 1128751
    Change-Id: I368a3c5f05f7b091f860c0953ed062630832261a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463524
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815747}

--- chrome/updater/test/integration_tests_win.cc
+++ chrome/updater/test/integration_tests_win.cc
@@ -21,9 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace updater {
-
 namespace test {
-
 namespace {
 
 base::FilePath GetInstallerPath() {
@@ -144,8 +142,12 @@ void RunWake(int expected_exit_code) {
 }
 
 void Install() {
+  const base::FilePath path = GetInstallerPath();
+  ASSERT_FALSE(path.empty());
+  base::CommandLine command_line(path);
+  command_line.AppendSwitch(kInstallSwitch);
   int exit_code = -1;
-  ASSERT_TRUE(Run(base::CommandLine(GetInstallerPath()), &exit_code));
+  ASSERT_TRUE(Run(command_line, &exit_code));
   EXPECT_EQ(0, exit_code);
 }
 
@@ -160,5 +162,4 @@ void Uninstall() {
 }
 
 }  // namespace test
-
 }  // namespace updater
