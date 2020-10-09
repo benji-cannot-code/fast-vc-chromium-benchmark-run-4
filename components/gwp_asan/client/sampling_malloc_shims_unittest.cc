191d10374b1413a72b56782c73aacb00ac619265
Bartek Nowierski <bartekn@chromium.org>
2020-10-09 05:41:01 +0000

    More GetAllocatedSize disambiguation
    Continuation of crrev.com/c/2455947
    
    Change-Id: I31507f9c386803dc1a379177cc62472fe674ba48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463043
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815498}

--- components/gwp_asan/client/sampling_malloc_shims_unittest.cc
+++ components/gwp_asan/client/sampling_malloc_shims_unittest.cc
@@ -29,17 +29,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include <malloc.h>
-static size_t GetAllocatedSize(void* mem) {
+static size_t GetUsableSize(void* mem) {
   return _msize(mem);
 }
 #elif defined(OS_APPLE)
 #include <malloc/malloc.h>
-static size_t GetAllocatedSize(void* mem) {
+static size_t GetUsableSize(void* mem) {
   return malloc_size(mem);
 }
 #elif defined(OS_LINUX) || defined(OS_CHROMEOS)
 #include <malloc.h>
-static size_t GetAllocatedSize(void* mem) {
+static size_t GetUsableSize(void* mem) {
   return malloc_usable_size(mem);
 }
 #endif
@@ -258,7 +258,7 @@ MULTIPROCESS_TEST_MAIN_WITH_SETUP(
     std::unique_ptr<void, decltype(&free)> alloc(malloc(kAllocationSize), free);
     CHECK_NE(alloc.get(), nullptr);
 
-    size_t alloc_sz = GetAllocatedSize(alloc.get());
+    size_t alloc_sz = GetUsableSize(alloc.get());
     if (GetMallocGpaForTesting().PointerIsMine(alloc.get()))
       CHECK_EQ(alloc_sz, kAllocationSize);
     else
