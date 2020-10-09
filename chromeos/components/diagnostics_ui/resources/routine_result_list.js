31ecece5aa1f191e4dcc01be7b1fc6be72e8444c
Zentaro Kavanagh <zentaro@chromium.org>
2020-10-09 01:54:33 +0000

    Diagnostics: Add a skeleton routine-result-list
    - Will later hold a list of routine results with the ability to update
      the status of the items
    
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: Iaebb25b50b7fc35c8dbf67043e495762e3f2dec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462381
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815435}

---
+++ chromeos/components/diagnostics_ui/resources/routine_result_list.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './diagnostics_card.js';
+import './diagnostics_shared_css.js';
+
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+/**
+ * @fileoverview
+ * 'routine-result-list' shows a list of routine result entries.
+ */
+Polymer({
+  is: 'routine-result-list',
+
+  _template: html`{__html_template__}`,
+
+  properties: {},
+
+  /** @override */
+  created() {},
+});
