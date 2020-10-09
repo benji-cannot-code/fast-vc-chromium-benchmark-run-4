cc77b30dfb165d4e8975d8d687210f3d99ba50f3
Alex Turner <alexmt@chromium.org>
2020-10-09 20:43:31 +0000

    Instrument PostScript name to typeface mapping for identifiability study
    Currently, local font lookups are instrumented with a digest of the
    lookup value and a digest of the resulting typeface being recorded. We
    expand this recording for successful local font lookups: we now also
    record a digest of the resulting typeface's PostScript name with the
    typeface digest. This will allow the identifiability study to measure
    the diversity of installed typeface files with the same PostScript name
    (for example, differing versions of the same typeface) and thus
    evaluate the privacy impact of that variation.
    
    Bug: 1136210
    Change-Id: Id7ac6229db287af776474f43abd200c51319992e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2453439
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815773}

--- third_party/blink/renderer/platform/fonts/font_global_context.h
+++ third_party/blink/renderer/platform/fonts/font_global_context.h
@@ -60,6 +60,8 @@ class PLATFORM_EXPORT FontGlobalContext {
   static FontUniqueNameLookup* GetFontUniqueNameLookup();
 
   IdentifiableToken GetOrComputeTypefaceDigest(const FontPlatformData& source);
+  IdentifiableToken GetOrComputePostScriptNameDigest(
+      const FontPlatformData& source);
 
   // Called by MemoryPressureListenerRegistry to clear memory.
   static void ClearMemory();
@@ -76,6 +78,7 @@ class PLATFORM_EXPORT FontGlobalContext {
   hb_font_funcs_t* harfbuzz_font_funcs_harfbuzz_advances_;
   std::unique_ptr<FontUniqueNameLookup> font_unique_name_lookup_;
   WTF::LruCache<SkFontID, IdentifiableToken> typeface_digest_cache_;
+  WTF::LruCache<SkFontID, IdentifiableToken> postscript_name_digest_cache_;
 
   DISALLOW_COPY_AND_ASSIGN(FontGlobalContext);
 };
