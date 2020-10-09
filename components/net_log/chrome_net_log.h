b74a159ddc633b27f296f5c74da4ee7dd1bbc3e6
Eric Lawrence <ericlaw@microsoft.com>
2020-10-09 18:57:39 +0000

    [NetLog] Remove dead function
    The GetConstantsForNetLog function is dead code with no callers. This
    CL removes it.
    
    Bug: 1136617
    Change-Id: I06edfe5873e8dd3fd4254e941b64d11529a98a1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461901
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#815722}

--- components/net_log/chrome_net_log.h
+++ components/net_log/chrome_net_log.h
@@ -14,23 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 class DictionaryValue;
-class Value;
 }
 
 namespace net_log {
 
-// Returns all the constants to include in NetLog files. This includes both
-// platform-specific details (GetPlatformConstantsForNetLog()) as well as the
-// basic src/net constants (net::GetNetConstants()) for things like symbolic
-// names of error codes.
-//
-// Safe to call on any thread.
-base::Value GetConstantsForNetLog(
-    const base::CommandLine::StringType& command_line_string,
-    const std::string& channel_string);
-
-// Returns constants to include in NetLog files for debugging purposes, which
-// includes information such as:
+// Returns constants to include in NetLog files including information such as:
 //
 //  * The version and build of Chrome
 //  * The command line arguments Chrome was launched with
