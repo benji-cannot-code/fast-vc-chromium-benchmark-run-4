cc04dad473409e6212c1ed2499082b1348828caa
Daniel Ng <danielng@google.com>
2020-10-09 03:01:55 +0000

    borealis: minor browsertest fix
    We expect this test to call Cancel(), I've added the expectation.
    
    Tests: Run browsertests for Borealis
    Change-Id: Ic05b94cb73d3a859d55f90c6a47720bc12569c35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2454573
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Daniel Ng <danielng@google.com>
    Cr-Commit-Position: refs/heads/master@{#815454}

--- chrome/browser/ui/views/borealis/borealis_installer_view_browsertest.cc
+++ chrome/browser/ui/views/borealis/borealis_installer_view_browsertest.cc
@@ -191,6 +191,8 @@ IN_PROC_BROWSER_TEST_F(BorealisInstallerViewBrowserTest, SucessfulInstall) {
 IN_PROC_BROWSER_TEST_F(BorealisInstallerViewBrowserTest,
                        ConfirmationCancelled) {
   ShowUi("default");
+
+  EXPECT_CALL(*mock_installer_, Cancel());
   ClickCancel();
 }
 
