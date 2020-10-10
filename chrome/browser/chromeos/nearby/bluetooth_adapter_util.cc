bf09ddec204e67541e17f1f5da234f0f892c7f1d
Kyle Horimoto <khorimoto@chromium.org>
2020-10-10 00:33:24 +0000

    [CrOS Nearby] Create NearbyConnectionsDependenciesProvider
    This KeyedService is responsible for generating dependencies to provide
    to the Nearby Connections utility process. Code is based off of the
    existing nearby_process_manager.cc file; the only changes are that we
    only fetch the Bluetooth adapter once and that we re-organize this code
    to be a KeyedService since it is based on the Profile.
    
    Bug: 1130069
    Change-Id: I9c1efd2ff93e9a9aadba5c363d0af37097ab5e49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2454388
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#815868}

---
+++ chrome/browser/chromeos/nearby/bluetooth_adapter_util.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/nearby/bluetooth_adapter_util.h"
+
+#include <memory>
+#include <utility>
+
+#include "device/bluetooth/adapter.h"
+#include "mojo/public/cpp/bindings/self_owned_receiver.h"
+
+namespace chromeos {
+namespace nearby {
+
+void MakeSelfOwnedBluetoothAdapter(
+    mojo::PendingReceiver<bluetooth::mojom::Adapter> pending_receiver,
+    scoped_refptr<device::BluetoothAdapter> adapter) {
+  mojo::MakeSelfOwnedReceiver(
+      std::make_unique<bluetooth::Adapter>(std::move(adapter)),
+      std::move(pending_receiver));
+}
+
+}  // namespace nearby
+}  // namespace chromeos
