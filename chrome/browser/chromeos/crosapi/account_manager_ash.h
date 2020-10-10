ccb2d0f3e074727040798f8c8fcedafa98b78076
Kushagra Sinha <sinhak@chromium.org>
2020-10-10 06:45:35 +0000

    Lacros: Notify AccountManager Mojo observers
    Notify AccountManager observers in lacros-chrome about account updates
    in AccountManager in ash-chrome.
    
    Bug: 1117478
    Test: unit_tests --gtest_filter="*AccountManagerAshTest*"
    Change-Id: Ibddec136dc428c6f07413e106a49cc5e16d227e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461272
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815922}

--- chrome/browser/chromeos/crosapi/account_manager_ash.h
+++ chrome/browser/chromeos/crosapi/account_manager_ash.h
@@ -6,21 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_CROSAPI_ACCOUNT_MANAGER_ASH_H_
 #define CHROME_BROWSER_CHROMEOS_CROSAPI_ACCOUNT_MANAGER_ASH_H_
 
+#include "chromeos/components/account_manager/account_manager.h"
 #include "chromeos/crosapi/mojom/account_manager.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote_set.h"
-
-namespace chromeos {
-class AccountManager;
-}  // namespace chromeos
 
 namespace crosapi {
 
 // Implements the |crosapi::mojom::AccountManager| interface in ash-chrome.
 // It enables lacros-chrome to interact with accounts stored in the Chrome OS
 // Account Manager.
-class AccountManagerAsh : public mojom::AccountManager {
+class AccountManagerAsh : public mojom::AccountManager,
+                          public chromeos::AccountManager::Observer {
  public:
   AccountManagerAsh(chromeos::AccountManager* account_manager,
                     mojo::PendingReceiver<mojom::AccountManager> receiver);
@@ -32,8 +30,27 @@ class AccountManagerAsh : public mojom::AccountManager {
   void IsInitialized(IsInitializedCallback callback) override;
   void AddObserver(AddObserverCallback callback) override;
 
+  // chromeos::AccountManager::Observer:
+  void OnTokenUpserted(
+      const chromeos::AccountManager::Account& account) override;
+  void OnAccountRemoved(
+      const chromeos::AccountManager::Account& account) override;
+
  private:
   friend class AccountManagerAshTest;
+  friend class TestAccountManagerObserver;
+
+  // Following util functions are static for ease of testing.
+  static chromeos::AccountManager::Account FromMojoAccount(
+      const mojom::AccountPtr& mojom_account);
+  static mojom::AccountPtr ToMojoAccount(
+      const chromeos::AccountManager::Account& account);
+  static chromeos::account_manager::AccountType FromMojoAccountType(
+      const mojom::AccountType& account_type);
+  static mojom::AccountType ToMojoAccountType(
+      const chromeos::account_manager::AccountType& account_type);
+
+  void FlushMojoForTesting();
 
   chromeos::AccountManager* const account_manager_;
   mojo::Receiver<mojom::AccountManager> receiver_;
