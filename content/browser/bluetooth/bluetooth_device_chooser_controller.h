282d7c481e28f38e2215933263f72339ac98b68c
James Hollyer <jameshollyer@chromium.org>
2020-10-10 02:24:37 +0000

    bluetooth: Consider filters that cannot be mapped to OS
    We used to only consider filters if they had something to filter.
    However, this means that if there is a filter with a name prefix we do
    not consider it as Android does not have a name prefix filter. Since
    the filters are unioned together, missing a filter could cause results
    we are looking for to be filtered out. To ensure this does not happen
    we are adding an open filter to replace the name prefix filter instead
    of disregarding it entirely. Therefore we never filter out a result
    which should pass through the name prefix filter.
    
    Bug: 1121697
    Change-Id: Ib6f4ff52978c62e0a5f16056524459f1116576a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2389340
    Commit-Queue: James Hollyer <jameshollyer@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815902}

--- content/browser/bluetooth/bluetooth_device_chooser_controller.h
+++ content/browser/bluetooth/bluetooth_device_chooser_controller.h
@@ -21,6 +21,7 @@ namespace device {
 class BluetoothAdapter;
 class BluetoothDevice;
 class BluetoothDiscoverySession;
+class BluetoothDiscoveryFilter;
 }
 
 namespace content {
@@ -94,6 +95,10 @@ class CONTENT_EXPORT BluetoothDeviceChooserController final {
   static void SetTestScanDurationForTesting(
       TestScanDurationSetting setting =
           TestScanDurationSetting::IMMEDIATE_TIMEOUT);
+
+  static std::unique_ptr<device::BluetoothDiscoveryFilter> ComputeScanFilter(
+      const base::Optional<
+          std::vector<blink::mojom::WebBluetoothLeScanFilterPtr>>& filters);
 
  private:
   // Populates the chooser with the GATT connected devices.
