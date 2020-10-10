4d8b1436d2ecb859a7eb04cdbc9947a78e866e58
Xinghui Lu <xinghuilu@chromium.org>
2020-10-10 01:35:08 +0000

    Remove RT Lookup Experiment in chrome://safe-browsing/#tab-rt-lookup
    This flag was added for testing purpose. It is now misleading because
    RT Lookup can still be performed when this flag is false(e.g. for ESB
    users).
    
    The same information can also be checked in chrome://safe-browsing,
    so this flag is not needed anymore.
    
    Bug: 1069651
    Change-Id: Ib96f9e456b6477bff77e18f9049309b5d75728b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2146795
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#815891}

--- components/safe_browsing/content/web_ui/resources/safe_browsing.js
+++ components/safe_browsing/content/web_ui/resources/safe_browsing.js
@@ -112,9 +112,6 @@ cr.define('safe_browsing', function() {
       addRTLookupResponse(result);
     });
 
-    cr.sendWithPromise('getRTLookupExperimentEnabled', [])
-        .then((enabled) => addRTLookupExperimentEnabled(enabled));
-
     cr.sendWithPromise('getLogMessages', []).then((logMessages) => {
       logMessages.forEach(function(message) {
         addLogMessage(message);
@@ -322,12 +319,6 @@ cr.define('safe_browsing', function() {
         addResultToTable('deep-scan-list', result['token'], resultFormatted, 1);
       }
     }
-  }
-
-  function addRTLookupExperimentEnabled(enabled) {
-    const enabledFormatted = $('rt-lookup-template').content.cloneNode(true);
-    enabledFormatted.querySelector('#experiment-bool').textContent = enabled;
-    $('rt-lookup-experiment-enabled').appendChild(enabledFormatted);
   }
 
   function addLogMessage(result) {
