1ccab790882283d9b6a10f8ed3d9af260930c52e
Aaron Leventhal <aleventhal@google.com>
2020-10-12 23:54:26 +0000

    Accessibility batch deferment
    Defer batches of accessibility updates unless they include user actions.
    
    changes within a short period of time.
    
    AX-Relnotes: performance improvements for web pages that make many
    Bug: 1107988
    Change-Id: I42fb6ac7a48f833cb4a42a53e80870bdf5dd7d28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2446089
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816357}

--- third_party/blink/renderer/core/exported/web_document.cc
+++ third_party/blink/renderer/core/exported/web_document.cc
@@ -281,6 +281,10 @@ uint64_t WebDocument::GetVisualViewportScrollingElementIdForTesting() {
       .GetStableId();
 }
 
+bool WebDocument::IsLoaded() {
+  return !ConstUnwrap<Document>()->Parser();
+}
+
 WebDocument::WebDocument(Document* elem) : WebNode(elem) {}
 
 DEFINE_WEB_NODE_TYPE_CASTS(WebDocument, ConstUnwrap<Node>()->IsDocumentNode())
