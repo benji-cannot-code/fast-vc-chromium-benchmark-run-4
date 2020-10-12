6272eeb7e7846b35e4ac5ba5a8c3c2af893d47c5
Eric Willigers <ericwilligers@chromium.org>
2020-10-12 07:34:24 +0000

    Desktop PWAs: ProfileSyncServiceFactoryTest tidy
    DesktopPWAsWithoutExtensions has shipped. Tests no
    longer need to check the flag.
    
    Bug: 1065748
    Change-Id: I43715dcec87cc523b247a5fc27e6f2a7f9e0bc68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463048
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816076}

--- chrome/browser/sync/profile_sync_service_factory_unittest.cc
+++ chrome/browser/sync/profile_sync_service_factory_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool/thread_pool_instance.h"
 #include "build/build_config.h"
 #include "chrome/common/buildflags.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/browser_sync/browser_sync_switches.h"
 #include "components/sync/base/model_type.h"
@@ -94,8 +93,7 @@ class ProfileSyncServiceFactoryTest : public testing::Test {
     datatypes.push_back(syncer::EXTENSIONS);
     datatypes.push_back(syncer::EXTENSION_SETTINGS);
     datatypes.push_back(syncer::APP_SETTINGS);
-    if (base::FeatureList::IsEnabled(features::kDesktopPWAsWithoutExtensions))
-      datatypes.push_back(syncer::WEB_APPS);
+    datatypes.push_back(syncer::WEB_APPS);
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 #if !defined(OS_ANDROID)
