f7a88214fcc963034bdc25d998584ae3103082a7
Eugene Zemtsov <eugene@chromium.org>
2020-10-12 18:38:02 +0000

    [webcodecs] Replace frame sync token in VideoFrame.createImageBitmap()
    1. Attach a new sync token to the frame, so it's not destroyed before
    the resulting image is fully created.
    
    2. Introduce WaitAndReplaceSyncTokenClient to avoid duplicating
    code from paint_canvs_video_renderer.cc
    
    Bug: 1136410
    Test: https://webcodecs-blogpost-demo.glitch.me/ doesn't crash
    Change-Id: I62d091776424f2ee3d550c8c5ef98f99f6720e9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2465504
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816234}

---
+++ media/base/wait_and_replace_sync_token_client.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/wait_and_replace_sync_token_client.h"
+#include "gpu/command_buffer/client/interface_base.h"
+
+namespace media {
+
+WaitAndReplaceSyncTokenClient::WaitAndReplaceSyncTokenClient(
+    gpu::InterfaceBase* ib)
+    : ib_(ib) {}
+
+void WaitAndReplaceSyncTokenClient::GenerateSyncToken(
+    gpu::SyncToken* sync_token) {
+  ib_->GenSyncTokenCHROMIUM(sync_token->GetData());
+}
+
+void WaitAndReplaceSyncTokenClient::WaitSyncToken(
+    const gpu::SyncToken& sync_token) {
+  ib_->WaitSyncTokenCHROMIUM(sync_token.GetConstData());
+}
+
+}  // namespace media
