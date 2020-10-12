84a67d54529f99addc50ff126a5748c4d2cb03eb
Allen Bauer <kylixrd@chromium.org>
2020-10-12 18:25:31 +0000

    Use button callback instead of listener and misc cleanup.
    Bug: 772945
    Change-Id: Id3e59075546185b145f7389a2bcf30388ac59060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2464322
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816227}

--- ash/app_list/views/apps_grid_view.h
+++ ash/app_list/views/apps_grid_view.h
@@ -35,14 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/views/animation/bounds_animator.h"
 #include "ui/views/animation/bounds_animator_observer.h"
-#include "ui/views/controls/button/button.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/view.h"
 #include "ui/views/view_model.h"
-
-namespace views {
-class ButtonListener;
-}
 
 namespace ash {
 
@@ -62,7 +57,7 @@ class GhostImageView;
 
 // Represents the index to an item view in the grid.
 struct APP_LIST_EXPORT GridIndex {
-  GridIndex() : page(-1), slot(-1) {}
+  GridIndex() = default;
   GridIndex(int page, int slot) : page(page), slot(slot) {}
 
   bool operator==(const GridIndex& other) const {
@@ -76,13 +71,12 @@ struct APP_LIST_EXPORT GridIndex {
   }
   std::string ToString() const;
 
-  int page;  // Which page an item view is on.
-  int slot;  // Which slot in the page an item view is in.
+  int page = -1;  // Which page an item view is on.
+  int slot = -1;  // Which slot in the page an item view is in.
 };
 
 // AppsGridView displays a grid for AppListItemList sub model.
 class APP_LIST_EXPORT AppsGridView : public views::View,
-                                     public views::ButtonListener,
                                      public AppListItemListObserver,
                                      public PaginationModelObserver,
                                      public AppListModelObserver,
@@ -513,8 +507,9 @@ class APP_LIST_EXPORT AppsGridView : public views::View,
   // shelf.
   bool IsPointWithinBottomDragBuffer(const gfx::Point& point) const;
 
-  // Overridden from views::ButtonListener:
-  void ButtonPressed(views::Button* sender, const ui::Event& event) override;
+  // AppListItemView pressed callback binds here:
+  void OnAppListItemViewPressed(AppListItemView* pressed_item_view,
+                                const ui::Event& event);
 
   // Overridden from AppListItemListObserver:
   void OnListItemAdded(size_t index, AppListItem* item) override;
@@ -875,7 +870,7 @@ class APP_LIST_EXPORT AppsGridView : public views::View,
   bool is_end_gesture_ = false;
 
   // view structure used only for non-folder.
-  PagedViewStructure view_structure_;
+  PagedViewStructure view_structure_{this};
 
   // True if an extra page is opened after the user drags an app to the bottom
   // of last page with intention to put it in a new page. This is only used for
