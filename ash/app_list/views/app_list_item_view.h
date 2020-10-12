482a0a9c9964c4cb537ef07c7274452e036d41c6
Allen Bauer <kylixrd@chromium.org>
2020-10-12 19:30:06 +0000

    Fixed missing calls to set the button pressed callback.
    Bug: 772945
    Change-Id: Idb01c79d10872f91aa83c915e16034e727d70d30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2466387
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816255}

--- ash/app_list/views/app_list_item_view.h
+++ ash/app_list/views/app_list_item_view.h
@@ -48,9 +48,6 @@ class APP_LIST_EXPORT AppListItemView : public views::Button,
 
   AppListItemView(AppsGridView* apps_grid_view,
                   AppListItem* item,
-                  AppListViewDelegate* delegate);
-  AppListItemView(AppsGridView* apps_grid_view,
-                  AppListItem* item,
                   AppListViewDelegate* delegate,
                   bool is_in_folder);
   ~AppListItemView() override;
