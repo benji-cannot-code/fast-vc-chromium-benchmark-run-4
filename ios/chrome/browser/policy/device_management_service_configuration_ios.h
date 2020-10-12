acdedc24375c9402ad1a8364cc48e915f863de5a
Zach Trudo <zatrudo@google.com>
2020-10-12 17:45:58 +0000

    Add GetEncryptedReportUrl
    CloudPolicyClient will be expanded to make use of a new server endpoint.
    This change prepares DeviceManagementService for that eventuality.
    
    Bug: chromium:1135951
    Change-Id: I8b23d8dc112a8f583ddc249f01572cae9522d99b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2454278
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/master@{#816213}

--- ios/chrome/browser/policy/device_management_service_configuration_ios.h
+++ ios/chrome/browser/policy/device_management_service_configuration_ios.h
@@ -20,8 +20,9 @@ class DeviceManagementServiceConfigurationIOS
     : public DeviceManagementService::Configuration {
  public:
   DeviceManagementServiceConfigurationIOS(
-      const std::string& server_url,
-      const std::string& reporting_server_url);
+      const std::string& dm_server_url,
+      const std::string& realtime_reporting_server_url,
+      const std::string& encrypted_reporting_server_url);
   DeviceManagementServiceConfigurationIOS(
       const DeviceManagementServiceConfigurationIOS&) = delete;
   DeviceManagementServiceConfigurationIOS& operator=(
@@ -32,12 +33,14 @@ class DeviceManagementServiceConfigurationIOS
   std::string GetDMServerUrl() override;
   std::string GetAgentParameter() override;
   std::string GetPlatformParameter() override;
-  std::string GetReportingServerUrl() override;
+  std::string GetRealtimeReportingServerUrl() override;
+  std::string GetEncryptedReportingServerUrl() override;
   std::string GetReportingConnectorServerUrl() override;
 
  private:
-  const std::string server_url_;
-  const std::string reporting_server_url_;
+  const std::string dm_server_url_;
+  const std::string realtime_reporting_server_url_;
+  const std::string encrypted_reporting_server_url_;
 };
 
 }  // namespace policy
