3afd0412c68d2edf91efa744b013025776df0f1e
Ramin Halavati <rhalavati@chromium.org>
2020-10-12 06:14:04 +0000

    Disable webapps popout for ephemeral Guest profiles.
    WebApps popout is disabled for Guest sessions. Same restriction is added
    for ephemeral Guest profiles.
    
    This change is behind disabled |EnableEphemeralGuestProfilesOnDesktop|
    flag.
    Please see go/ephemeral-guest-profiles for more context.
    
    Bug: 1125474
    Change-Id: Ic6bde4313a5107f165eb00ac5d8c0e3ff1c9496f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461383
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816065}

--- chrome/browser/ui/web_applications/web_app_dialog_utils.cc
+++ chrome/browser/ui/web_applications/web_app_dialog_utils.cc
@@ -96,7 +96,7 @@ bool CanCreateWebApp(const Browser* browser) {
 
 bool CanPopOutWebApp(Profile* profile) {
   return AreWebAppsEnabled(profile) && !profile->IsGuestSession() &&
-         !profile->IsOffTheRecord();
+         !profile->IsEphemeralGuestProfile() && !profile->IsOffTheRecord();
 }
 
 void CreateWebAppFromCurrentWebContents(Browser* browser,
