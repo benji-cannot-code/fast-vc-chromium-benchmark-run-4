9673860a3302785dceb696a912dd856bcfc791c0
Xiaohui Chen <xiaohuic@google.com>
2020-10-12 17:19:12 +0000

    ambient: avoid restart lock timer if it's running
    Bug: b:170509871
    Test: unittests and manually on nocturne
    Change-Id: Ib59a1c9e00056a5fdd13e5ee94cac51c365cf9b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462025
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816206}

--- ash/ambient/test/ambient_ash_test_base.h
+++ ash/ambient/test/ambient_ash_test_base.h
@@ -106,6 +106,7 @@ class AmbientAshTestBase : public AshTestBase {
 
   // Advance the task environment timer to ambient mode lock screen delay.
   void FastForwardToLockScreen();
+  void FastForwardHalfLockScreenDelay();
 
   void SetPowerStateCharging();
   void SetPowerStateDischarging();
