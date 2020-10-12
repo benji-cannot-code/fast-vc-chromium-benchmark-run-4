da95b3fba1847cefd78a8afb799be8d9d3d99ece
Kenichi Ishibashi <bashi@chromium.org>
2020-10-12 01:41:49 +0000

    service worker: Expose SetPurgingCompleteCallbackForTest()
    This function is used by unittests which depend on in-memory
    representations like ServiceWorkerVersion. Expose it as a
    mojo test helper function so that these tests can be moved
    out service_worker_storage_unittest.cc.
    
    This CL doesn't move any tests. Subsequent CLs will move
    these tests.
    
    Bug: 1016064
    Change-Id: Icc33be2dc0ddc186237cdd8ff731a11b678bfdc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462942
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816038}

--- content/browser/service_worker/service_worker_storage_unittest.cc
+++ content/browser/service_worker/service_worker_storage_unittest.cc
@@ -1461,7 +1461,7 @@ TEST_F(ServiceWorkerResourceStorageTest, DeleteRegistration_NoLiveVersion) {
   // purgeable list and then doomed in the disk cache and removed from that
   // list.
   base::RunLoop loop;
-  storage()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
+  storage_control()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
   EXPECT_EQ(blink::ServiceWorkerStatusCode::kOk,
             DeleteRegistration(registration_, scope_.GetOrigin()));
   // At this point registration_->waiting_version() has a remote reference, so
@@ -1491,7 +1491,7 @@ TEST_F(ServiceWorkerResourceStorageTest, DeleteRegistration_WaitingVersion) {
 
   // Doom the version. The resources should be purged.
   base::RunLoop loop;
-  storage()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
+  storage_control()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
   registration_->waiting_version()->Doom();
   loop.Run();
   EXPECT_TRUE(GetPurgeableResourceIdsFromDB().empty());
@@ -1525,7 +1525,7 @@ TEST_F(ServiceWorkerResourceStorageTest, DeleteRegistration_ActiveVersion) {
 
   // Dooming the version should cause the resources to be deleted.
   base::RunLoop loop;
-  storage()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
+  storage_control()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
   registration_->active_version()->RemoveControllee(
       container_host->client_uuid());
   registration_->active_version()->Doom();
@@ -1580,7 +1580,7 @@ TEST_F(ServiceWorkerResourceStorageDiskTest, CleanupOnRestart) {
 
   // Store a new uncommitted resource. This triggers stale resource cleanup.
   base::RunLoop loop;
-  storage()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
+  storage_control()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
   int64_t kNewResourceId = GetNewResourceIdSync(storage_control());
   WriteBasicResponse(storage_control(), kNewResourceId);
   registry()->StoreUncommittedResourceId(kNewResourceId,
@@ -1717,7 +1717,7 @@ TEST_F(ServiceWorkerResourceStorageTest, UpdateRegistration) {
   // Remove the controllee to allow the new version to become active, making the
   // old version redundant.
   base::RunLoop loop;
-  storage()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
+  storage_control()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
   scoped_refptr<ServiceWorkerVersion> old_version(
       registration_->active_version());
   old_version->RemoveControllee(container_host->client_uuid());
@@ -1753,7 +1753,7 @@ TEST_F(ServiceWorkerResourceStorageTest, UpdateRegistration_NoLiveVersion) {
   // Writing the registration should purge the old version's resources,
   // since it's not live.
   base::RunLoop loop;
-  storage()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
+  storage_control()->SetPurgingCompleteCallbackForTest(loop.QuitClosure());
   EXPECT_EQ(
       blink::ServiceWorkerStatusCode::kOk,
       StoreRegistration(registration_.get(), registration_->waiting_version()));
