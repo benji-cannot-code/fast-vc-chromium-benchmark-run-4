1e45a02af6f2ff5cbabf4622a46a7f44cb7d893b
Saho Kobayashi <sahok@chromium.org>
2020-10-12 18:44:39 +0000

    Avoid doubled output of ChromeVox for ARC++ compound buttons.
    The information of checked property and state description is
    duplicated, and this causes doubled output of ChromeVox on ARC++
    Checkbox. State description is added in R, so this was not a
    problem until P.
    Instead of description property, add new field property to send
    state description. Use this new property to recognize whether
    this description is came from state description or not, and avoid
    doubled output.
    
    only once)
          out/Release/browser_tests --test-launcher-jobs=20
          --gtest_filter=ChromeVox
    
    Test: manual (manually check that ChromeVox says checked and not checked
    Bug: b:154433831
    AX-Relnotes: n/a
    Change-Id: I6055bb317db6525a8eafd2cff2213e7eb6434274
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2444875
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Reviewed-by: Sara Kato <sarakato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816237}

--- third_party/closure_compiler/externs/automation.js
+++ third_party/closure_compiler/externs/automation.js
@@ -1169,6 +1169,14 @@ chrome.automation.AutomationNode.prototype.unclippedLocation;
 chrome.automation.AutomationNode.prototype.description;
 
 /**
+ * Description of the state of the checkbox. Used only when the node is
+ * checkable.
+ * @type {(string|undefined)}
+ * @see https://developer.chrome.com/extensions/automation#type-checkedStateDescription
+ */
+chrome.automation.AutomationNode.prototype.checkedStateDescription;
+
+/**
  * The placeholder for this text field, if any.
  * @type {(string|undefined)}
  * @see https://developer.chrome.com/extensions/automation#type-placeholder
@@ -1595,6 +1603,13 @@ chrome.automation.AutomationNode.prototype.selectionEndOffset;
  * @see https://developer.chrome.com/extensions/automation#type-selectionEndAffinity
  */
 chrome.automation.AutomationNode.prototype.selectionEndAffinity;
+
+/**
+ * Indicates that the node is marked user-select:none
+ * @type {(boolean|undefined)}
+ * @see https://developer.chrome.com/extensions/automation#type-notUserSelectableStyle
+ */
+chrome.automation.AutomationNode.prototype.notUserSelectableStyle;
 
 /**
  * The current value for this range.
