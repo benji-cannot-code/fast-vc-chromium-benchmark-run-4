b4c987078f2b8f1d70e2c4574b70bdbf86194e98
Pavel Yatsuk <pavely@google.com>
2020-10-12 20:34:13 +0000

    Avoid use-after-free of InfoBarAndroid from pointer in InfoBar.java
    InfoBar.java has a native reference to InfoBarAndroid which should be
    cleared before InfoBarAndroid is destroyed. Calls to
    java_info_bar_.Reset() without corresponding onNativeDestroyed() or
    setNativeInfobar() allow scenarios where java InfoBar still has non-null
    pointer after native object was destroyed. This results in occasional
    use-after-free.
    
    This CL:
    - renames onNativeDestroyed method to resetNativeInfoBar to indicate
      that it is not necessarily called when native is destroyed, but in
      general when native-java association changes.
    - adds a call to reset native infobar into CloseJavaInfoBar to prevent
      java infobar from referring potentially destroyed native object.
    - removes unused ReassignJavaInfoBar().
    
    BUG=1135130
    R=mdjones@chromium.org
    
    Change-Id: I1329ed6a78cc589d8f23df883a8fcb60689230a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2459268
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816281}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/TranslateCompactInfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/TranslateCompactInfoBar.java
@@ -378,9 +378,9 @@ public class TranslateCompactInfoBar extends InfoBar
     }
 
     @Override
-    protected void onNativeDestroyed() {
+    protected void resetNativeInfoBar() {
         mNativeTranslateInfoBarPtr = 0;
-        super.onNativeDestroyed();
+        super.resetNativeInfoBar();
     }
 
     private void closeInfobar(boolean explicitly) {
