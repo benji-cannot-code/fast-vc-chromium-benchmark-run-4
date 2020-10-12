d36cae1afae5064e4189d9a5b53c738f48f44238
Bo Majewski <majewski@chromium.org>
2020-10-12 22:23:25 +0000

    Files SWA: Move mojo files to their own directory.
    Bug: 1113981
    Change-Id: I90081b67e4e1cc4313e8a9c441ac05fed7ba7320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2437715
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Sergei Datsenko <dats@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816319}

--- chromeos/components/file_manager/file_manager_page_handler.h
+++ chromeos/components/file_manager/file_manager_page_handler.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/timer/timer.h"
-#include "chromeos/components/file_manager/file_manager.mojom.h"
+#include "chromeos/components/file_manager/mojom/file_manager.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
