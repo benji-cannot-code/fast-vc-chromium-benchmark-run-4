a69573ba9594ef7f96ed5bcc794c97dde959153a
Avi Drissman <avi@chromium.org>
2020-10-12 16:58:05 +0000

    Make the fullscreen touchbar customizable
    Users might not like the URL display on the touchbar in fullscreen.
    This CL makes the fullscreen touchbar customizable. Right now, that
    means that the user can add or remove the single item in it (the
    URL display) but that should help for the case in the bug.
    
    Fixed: 1135102
    Change-Id: I99e3ca0d4d9d6575a0ec79ab3329e54613475e7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2464080
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816197}

--- chrome/browser/ui/cocoa/touchbar/browser_window_default_touch_bar.mm
+++ chrome/browser/ui/cocoa/touchbar/browser_window_default_touch_bar.mm
@@ -274,6 +274,9 @@ class API_AVAILABLE(macos(10.12.2)) TouchBarNotificationBridge
   base::scoped_nsobject<NSButton> _starredButton;
 }
 
+// Creates and returns a touch bar for tab non-fullscreen mode.
+- (NSTouchBar*)createTabTouchBar;
+
 // Creates and returns a touch bar for tab fullscreen mode.
 - (NSTouchBar*)createTabFullscreenTouchBar;
 
@@ -305,37 +308,7 @@ class API_AVAILABLE(macos(10.12.2)) TouchBarNotificationBridge
     return [self createTabFullscreenTouchBar];
   }
 
-  base::scoped_nsobject<NSTouchBar> touchBar([[NSTouchBar alloc] init]);
-  [touchBar
-      setCustomizationIdentifier:ui::GetTouchBarId(kBrowserWindowTouchBarId)];
-  [touchBar setDelegate:self];
-
-  NSMutableArray<NSString*>* customIdentifiers = [NSMutableArray array];
-  NSMutableArray<NSString*>* defaultIdentifiers = [NSMutableArray array];
-
-  NSArray<NSString*>* touchBarItems = @[
-    kBackTouchId, kForwardTouchId, kReloadOrStopTouchId, kHomeTouchId,
-    kSearchTouchId, kStarTouchId, kNewTabTouchId
-  ];
-
-  for (NSString* item in touchBarItems) {
-    NSString* itemIdentifier =
-        ui::GetTouchBarItemId(kBrowserWindowTouchBarId, item);
-    [customIdentifiers addObject:itemIdentifier];
-
-    // Don't add the home button if it's not shown in the toolbar.
-    if (item == kHomeTouchId && !_notificationBridge->show_home_button())
-      continue;
-
-    [defaultIdentifiers addObject:itemIdentifier];
-  }
-
-  [customIdentifiers addObject:NSTouchBarItemIdentifierFlexibleSpace];
-
-  [touchBar setDefaultItemIdentifiers:defaultIdentifiers];
-  [touchBar setCustomizationAllowedItemIdentifiers:customIdentifiers];
-
-  return touchBar.autorelease();
+  return [self createTabTouchBar];
 }
 
 - (NSTouchBarItem*)touchBar:(NSTouchBar*)touchBar
@@ -436,6 +409,9 @@ class API_AVAILABLE(macos(10.12.2)) TouchBarNotificationBridge
 
     [touchBarItem
         setView:[NSTextField labelWithAttributedString:attributedString.get()]];
+    [touchBarItem
+        setCustomizationLabel:l10n_util::GetNSString(
+                                  IDS_TOUCH_BAR_URL_CUSTOMIZATION_LABEL)];
   } else {
     return nil;
   }
@@ -443,12 +419,52 @@ class API_AVAILABLE(macos(10.12.2)) TouchBarNotificationBridge
   return touchBarItem.autorelease();
 }
 
+- (NSTouchBar*)createTabTouchBar {
+  base::scoped_nsobject<NSTouchBar> touchBar([[NSTouchBar alloc] init]);
+  [touchBar
+      setCustomizationIdentifier:ui::GetTouchBarId(kBrowserWindowTouchBarId)];
+  [touchBar setDelegate:self];
+
+  NSMutableArray<NSString*>* customIdentifiers = [NSMutableArray array];
+  NSMutableArray<NSString*>* defaultIdentifiers = [NSMutableArray array];
+
+  NSArray<NSString*>* touchBarItems = @[
+    kBackTouchId, kForwardTouchId, kReloadOrStopTouchId, kHomeTouchId,
+    kSearchTouchId, kStarTouchId, kNewTabTouchId
+  ];
+
+  for (NSString* item in touchBarItems) {
+    NSString* itemIdentifier =
+        ui::GetTouchBarItemId(kBrowserWindowTouchBarId, item);
+    [customIdentifiers addObject:itemIdentifier];
+
+    // Don't add the home button if it's not shown in the toolbar.
+    if (item == kHomeTouchId && !_notificationBridge->show_home_button())
+      continue;
+
+    [defaultIdentifiers addObject:itemIdentifier];
+  }
+
+  [customIdentifiers addObject:NSTouchBarItemIdentifierFlexibleSpace];
+
+  [touchBar setDefaultItemIdentifiers:defaultIdentifiers];
+  [touchBar setCustomizationAllowedItemIdentifiers:customIdentifiers];
+
+  return touchBar.autorelease();
+}
+
 - (NSTouchBar*)createTabFullscreenTouchBar {
   base::scoped_nsobject<NSTouchBar> touchBar([[NSTouchBar alloc] init]);
+  [touchBar
+      setCustomizationIdentifier:ui::GetTouchBarId(kTabFullscreenTouchBarId)];
   [touchBar setDelegate:self];
-  [touchBar setDefaultItemIdentifiers:@[ ui::GetTouchBarItemId(
-                                          kTabFullscreenTouchBarId,
-                                          kFullscreenOriginLabelTouchId) ]];
+
+  NSArray<NSString*>* touchBarItems = @[ ui::GetTouchBarItemId(
+      kTabFullscreenTouchBarId, kFullscreenOriginLabelTouchId) ];
+
+  [touchBar setDefaultItemIdentifiers:touchBarItems];
+  [touchBar setCustomizationAllowedItemIdentifiers:touchBarItems];
+
   return touchBar.autorelease();
 }
 
