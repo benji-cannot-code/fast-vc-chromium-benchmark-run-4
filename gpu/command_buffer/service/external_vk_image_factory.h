de79369a6561eef3b8c4d70bcc6f8eccc19cd2d7
Austin Eng <enga@chromium.org>
2020-10-12 18:40:58 +0000

    Use correct image layout transitions for Dawn+Skia Vulkan interop
    This CL fixes SharedImage interop between Dawn and Skia on Linux
    so that WebGPU can be enabled when SkiaRenderer Vulkan is used.
    
    To get the correct layout for import, we query the GrBackendTexture
    for the layout information on BeginAccess, and on EndAccess, we save
    the exported layout information on the GrBackendTexture. This makes it
    so that future users of the shared image import it with the correct
    layout.
    
    Bug: dawn:200, 976495
    Change-Id: I6303812bf088f21f01bc3ca0d1cfee12b662815f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462634
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816235}

--- gpu/command_buffer/service/external_vk_image_factory.h
+++ gpu/command_buffer/service/external_vk_image_factory.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/command_buffer/service/external_vk_image_backing.h"
 #include "gpu/command_buffer/service/shared_image_backing_factory.h"
+#include "gpu/gpu_gles2_export.h"
 
 namespace gpu {
 class SharedContextState;
@@ -21,7 +22,8 @@ class VulkanCommandPool;
 // can be exported out of Vulkan and be used in GL. Synchronization between
 // Vulkan and GL is done using VkSemaphores that are created with special flags
 // that allow it to be exported out and shared with GL.
-class ExternalVkImageFactory : public SharedImageBackingFactory {
+class GPU_GLES2_EXPORT ExternalVkImageFactory
+    : public SharedImageBackingFactory {
  public:
   explicit ExternalVkImageFactory(
       scoped_refptr<SharedContextState> context_state);
