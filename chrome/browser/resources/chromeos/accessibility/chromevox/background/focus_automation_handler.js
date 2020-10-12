1922d8b83fc01b3f059251cd9c893268f79a9511
David Tseng <dtseng@google.com>
2020-10-12 17:56:58 +0000

    Use value changes for reading popup buttons in ChromeVox
    Change-Id: I86274af81a2d60223813cdfb91253abca97a766b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2465383
    Reviewed-by: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816219}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/focus_automation_handler.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/focus_automation_handler.js
@@ -43,7 +43,7 @@ FocusAutomationHandler = class extends BaseAutomationHandler {
         EventType.ACTIVE_DESCENDANT_CHANGED, this.onActiveDescendantChanged);
     this.addListener_(
         EventType.MENU_LIST_ITEM_SELECTED, this.onEventIfSelected);
-    this.addListener_(EventType.TEXT_CHANGED, this.onTextChanged_);
+    this.addListener_(EventType.VALUE_CHANGED, this.onValueChanged_);
   }
 
   /**
@@ -93,9 +93,7 @@ FocusAutomationHandler = class extends BaseAutomationHandler {
   /**
    * @param {!ChromeVoxEvent} evt
    */
-  onTextChanged_(evt) {
-    // TODO: listen to value changes instead when they are generated.
-    // Here only to handle popup buttons.
+  onValueChanged_(evt) {
     if (evt.target.role != RoleType.POP_UP_BUTTON ||
         evt.target.state.editable) {
       return;
