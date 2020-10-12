1241feb0a8718322abb6ab4ab71c5d4e9239a7a9
Brian Sheedy <bsheedy@chromium.org>
2020-10-12 22:27:36 +0000

    Refactor GPU tags
    Refactors several sets of GPU expectation tags for clarity:
    1. ANGLE backend tags now have an angle- prefix
    2. Tags that only make sense in the context of Skia Renderer have been
       consolidated instead of having separate use/no-use tags for each
       version.
    
    Bug: 1011107, 1111876
    Change-Id: I55d68444b4aa88116666d5464c470f7433dbc6ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462222
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816321}

--- content/test/gpu/gpu_tests/gpu_integration_test_unittest.py
+++ content/test/gpu/gpu_tests/gpu_integration_test_unittest.py
@@ -159,7 +159,7 @@ class GpuIntegrationTestUnittest(unittest.TestCase):
     tag_set = _GenerateNvidiaExampleTagsForTestClassAndArgs(test_class, args)
     self.assertTrue(
         set([
-            'win', 'win10', 'd3d9', 'release', 'nvidia', 'nvidia-0x1cb3',
+            'win', 'win10', 'angle-d3d9', 'release', 'nvidia', 'nvidia-0x1cb3',
             'no-passthrough'
         ]).issubset(tag_set))
     return tag_set
@@ -202,9 +202,8 @@ class GpuIntegrationTestUnittest(unittest.TestCase):
     self.assertEqual(
         _GetTagsToTest(browser),
         set([
-            'win', 'win10', 'release', 'nvidia', 'nvidia-0x1cb3', 'd3d9',
-            'no-passthrough', 'no-swiftshader-gl', 'no-use-vulkan',
-            'no-skia-renderer'
+            'win', 'win10', 'release', 'nvidia', 'nvidia-0x1cb3', 'angle-d3d9',
+            'no-passthrough', 'no-swiftshader-gl', 'no-skia-renderer'
         ]))
 
   def testGenerateVendorTagUsingVendorString(self):
@@ -219,8 +218,8 @@ class GpuIntegrationTestUnittest(unittest.TestCase):
         _GetTagsToTest(browser),
         set([
             'mac', 'mojave', 'release', 'imagination',
-            'imagination-PowerVR-SGX-554', 'opengles', 'passthrough',
-            'no-swiftshader-gl', 'no-use-vulkan', 'no-skia-renderer'
+            'imagination-PowerVR-SGX-554', 'angle-opengles', 'passthrough',
+            'no-swiftshader-gl', 'no-skia-renderer'
         ]))
 
   def testGenerateVendorTagUsingDeviceString(self):
@@ -233,8 +232,8 @@ class GpuIntegrationTestUnittest(unittest.TestCase):
         _GetTagsToTest(browser),
         set([
             'mac', 'mojave', 'release', 'imagination',
-            'imagination-Triangle-Monster-3000', 'no-angle', 'no-passthrough',
-            'no-swiftshader-gl', 'no-use-vulkan', 'no-skia-renderer'
+            'imagination-Triangle-Monster-3000', 'angle-no-backend',
+            'no-passthrough', 'no-swiftshader-gl', 'no-skia-renderer'
         ]))
 
   def testSimpleIntegrationTest(self):
