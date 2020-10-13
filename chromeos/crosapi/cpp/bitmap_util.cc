c4f5033acea9659acb71d35c2d2ca0b4c70171ff
James Cook <jamescook@chromium.org>
2020-10-13 01:28:51 +0000

    lacros: Stop sending deprecated notification image fields
    Ash stopped handling these fields in M87. Going forward, all Lacros
    releases will target M87 beta or later (M87 beta + M88 dev/canary).
    Therefore Lacros can stop sending these fields.
    
    Clean up SkBitmapFromBitmap utility function, since it is no longer
    used. We can't clean up BitmapFromSkBitmap yet, since it's still used
    by ScreenManager.
    
    Bug: 1123969
    Test: lacros unit_tests
    Test: trigger notifications using Notifications Galore test app
    Change-Id: I631d1d955cdbe6c34cfecf00d701eec57c9455dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2466491
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816392}

--- chromeos/crosapi/cpp/bitmap_util.cc
+++ chromeos/crosapi/cpp/bitmap_util.cc
@@ -5,16 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/crosapi/cpp/bitmap_util.h"
 
-#include <algorithm>
 #include <vector>
 
-#include "base/check.h"
 #include "base/check_op.h"
 #include "base/numerics/checked_math.h"
 #include "base/numerics/safe_conversions.h"
 #include "chromeos/crosapi/cpp/bitmap.h"
 #include "third_party/skia/include/core/SkBitmap.h"
-#include "third_party/skia/include/core/SkImageInfo.h"
 
 namespace crosapi {
 
@@ -32,19 +29,6 @@ Bitmap BitmapFromSkBitmap(const SkBitmap& sk_bitmap) {
   snapshot.height = sk_bitmap.height();
   snapshot.pixels.swap(bytes);
   return snapshot;
-}
-
-SkBitmap SkBitmapFromBitmap(const Bitmap& snapshot) {
-  SkImageInfo info =
-      SkImageInfo::Make(snapshot.width, snapshot.height, kBGRA_8888_SkColorType,
-                        kPremul_SkAlphaType);
-  CHECK_EQ(info.computeByteSize(info.minRowBytes()), snapshot.pixels.size());
-  SkBitmap sk_bitmap;
-  CHECK(sk_bitmap.tryAllocPixels(info));
-  std::copy(snapshot.pixels.begin(), snapshot.pixels.end(),
-            static_cast<uint8_t*>(sk_bitmap.getPixels()));
-  sk_bitmap.notifyPixelsChanged();
-  return sk_bitmap;
 }
 
 }  // namespace crosapi
