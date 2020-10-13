2d549d7e33e4ecb303296ad459e07f20cedf58df
Brandon Jones <bajones@chromium.org>
2020-10-13 19:12:49 +0000

    Reland "Use a single, consistent path to update stage parameters."
    This is a reland of e94960c9ac81f36bfeba2bdebe2c2714933a92da
    
    Original change's description:
    > Use a single, consistent path to update stage parameters.
    >
    > Removes stage_parameters from VRDisplayInfo and updates the XR frame
    > request loop to exchange an stage_parameters_id to indicate what version
    > of the parameters the session has so that only updates need to be sent.
    >
    > Part of an ongoing process to remove VRDisplayInfo entirely.
    >
    > Bug: 998146
    > Change-Id: I6f41dc2426b529694903d1e46cdadc16841b2b37
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2460976
    > Commit-Queue: Brandon Jones <bajones@chromium.org>
    > Reviewed-by: Chris Palmer <palmer@chromium.org>
    > Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#815903}
    
    Bug: 998146
    Change-Id: I433914ca4db3adfa939032946a2095e03c13eed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2466932
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816699}

--- third_party/blink/web_tests/external/wpt/resources/chromium/webxr-test.js
+++ third_party/blink/web_tests/external/wpt/resources/chromium/webxr-test.js
@@ -351,7 +351,7 @@ class MockRuntime {
     this.bounds_ = null;
     this.send_mojo_space_reset_ = false;
     this.stageParameters_ = null;
-    this.stageParametersUpdated_ = false;
+    this.stageParametersId_ = 1;
 
     this.service_ = service;
 
@@ -553,11 +553,7 @@ class MockRuntime {
 
   onStageParametersUpdated() {
     // Indicate for the frame loop that the stage parameters have been updated.
-    this.stageParametersUpdated_ = true;
-    this.displayInfo_.stageParameters = this.stageParameters_;
-    if (this.sessionClient_.ptr.isBound()) {
-      this.sessionClient_.onChanged(this.displayInfo_);
-    }
+    this.stageParametersId_++;
   }
 
   simulateResetPose() {
@@ -724,8 +720,6 @@ class MockRuntime {
         const mojo_space_reset = this.send_mojo_space_reset_;
         this.send_mojo_space_reset_ = false;
 
-        const stage_parameters_updated = this.stageParametersUpdated_;
-        this.stageParametersUpdated_ = false;
         if (this.pose_) {
           this.pose_.poseIndex++;
         }
@@ -754,7 +748,7 @@ class MockRuntime {
           bufferHolder: null,
           bufferSize: {},
           stageParameters: this.stageParameters_,
-          stageParametersUpdated: stage_parameters_updated,
+          stageParametersId: this.stageParametersId_,
         };
 
         this.next_frame_id_++;
