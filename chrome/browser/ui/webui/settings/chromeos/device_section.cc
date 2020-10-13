700cb3cd826b1199bd3bf6093bd2160250987f1e
Daniel Classon <dclasson@google.com>
2020-10-13 19:05:26 +0000

    [OsSettingsMetrics] Add metric handling for pref based setting
    Adds a new helper function that converts a (pref, prefValue) pair to
    a (setting, settingChangeValue) pair that can be used to log metrics.
    Uses this helper function to automatically capture setting changes
    for all pref based settings. As a first example, implements metrics
    for the kKeyboardFunctionKeys on the Device page.
    
    Bug:  1133553
    
    Change-Id: Ia1f0118e27bb7e0f610bfe1d75ba0ef324260f7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2459846
    Commit-Queue: Daniel Classon <dclasson@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816692}

--- chrome/browser/ui/webui/settings/chromeos/device_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/device_section.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/stylus_utils.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -843,8 +844,15 @@ std::string DeviceSection::GetSectionPath() const {
 
 bool DeviceSection::LogMetric(mojom::Setting setting,
                               base::Value& value) const {
-  // Unimplemented.
-  return false;
+  switch (setting) {
+    case mojom::Setting::kKeyboardFunctionKeys:
+      base::UmaHistogramBoolean("ChromeOS.Settings.Device.KeyboardFunctionKeys",
+                                value.GetBool());
+      return true;
+
+    default:
+      return false;
+  }
 }
 
 void DeviceSection::RegisterHierarchy(HierarchyGenerator* generator) const {
