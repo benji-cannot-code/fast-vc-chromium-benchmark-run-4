dcf8f8bca88db841393df98debe8fa81283c9c7a
Henrique Ferreiro <hferreiro@igalia.com>
2020-10-13 10:15:10 +0000

    Make Lacros cursor scale factor match the one in Ash
    Ash limits the cursor scale factor to the available resource scale
    factors, currently 100P and 200P. Lacros should follow this behavior;
    otherwise, the cursor may change sizes when hovering a Lacros window.
    
    Bug: 1127962
    Change-Id: I17c0ded04153dd10efb7b90665e1426634c9a7af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461227
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816502}

--- ui/views/widget/desktop_aura/desktop_native_cursor_manager.cc
+++ ui/views/widget/desktop_aura/desktop_native_cursor_manager.cc
@@ -8,10 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/trace_event/trace_event.h"
+#include "build/chromeos_buildflags.h"
 #include "ui/aura/window_event_dispatcher.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/cursor/cursor_loader.h"
 #include "ui/base/cursor/mojom/cursor_type.mojom-shared.h"
+
+#if BUILDFLAG(IS_LACROS)
+#include "ui/base/layout.h"
+#include "ui/base/resource/scale_factor.h"
+#endif
 
 namespace views {
 
@@ -44,8 +50,14 @@ void DesktopNativeCursorManager::RemoveHost(aura::WindowTreeHost* host) {
 void DesktopNativeCursorManager::SetDisplay(
     const display::Display& display,
     wm::NativeCursorManagerDelegate* delegate) {
-  cursor_loader_->SetDisplayData(display.rotation(),
-                                 display.device_scale_factor());
+  float cursor_scale = display.device_scale_factor();
+#if BUILDFLAG(IS_LACROS)
+  // Use the nearest resource scale factor, as Ash does, so that the cursor
+  // doesn't change its size when hovering a Lacros window.
+  cursor_scale =
+      ui::GetScaleForScaleFactor(ui::GetSupportedScaleFactor(cursor_scale));
+#endif
+  cursor_loader_->SetDisplayData(display.rotation(), cursor_scale);
 
   SetCursor(delegate->GetCursor(), delegate);
 }
