779c7bdfd2100400ea8146f6bc4db3526a3deda6
Eric Willigers <ericwilligers@chromium.org>
2020-10-13 08:23:58 +0000

    base: Remove unneeded scoped_feature_list.h
    Remove #include from source files
    that do not use ScopedFeatureList.
    
    Bug: 1137231
    Change-Id: I9d36850098c4b08e7cd406a0269e03a04ee05a1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463090
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816483}

--- base/logging_unittest.cc
+++ base/logging_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
@@ -16,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sanitizer_buildflags.h"
 #include "base/strings/string_piece.h"
 #include "base/test/bind_test_util.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_logging_settings.h"
 #include "base/test/task_environment.h"
 #include "build/build_config.h"
