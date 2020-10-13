ab69f2610c989706417c4761d09999bba59072c4
Meilin Wang <meilinw@google.com>
2020-10-13 23:04:55 +0000

    [CrOS PhoneHub] Fix bubble can't grab focus in a11y mode.
    Screenshot: https://screenshot.googleplex.com/77SePNbWg2gvy53.png
    
    BUG=1106937,1126208
    TEST=unittested.
    
    Change-Id: I75974a008dae69e282d34091202c0887c34e4ce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2464087
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Commit-Queue: Meilin Wang <meilinw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816799}

--- ash/system/phonehub/phone_hub_tray.cc
+++ ash/system/phonehub/phone_hub_tray.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/phonehub/phone_hub_tray.h"
 
 #include "ash/accessibility/accessibility_controller_impl.h"
+#include "ash/focus_cycler.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shell.h"
@@ -188,6 +189,14 @@ void PhoneHubTray::ShowBubble(bool show_by_click) {
                                                 false /* is_persistent */);
 
   SetIsActive(true);
+
+  // Only focus the widget if it's opened by the keyboard.
+  if (!show_by_click) {
+    views::Widget* widget = bubble_->GetBubbleWidget();
+    widget->widget_delegate()->SetCanActivate(true);
+    Shell::Get()->focus_cycler()->FocusWidget(widget);
+    widget->Activate();
+  }
 }
 
 TrayBubbleView* PhoneHubTray::GetBubbleView() {
