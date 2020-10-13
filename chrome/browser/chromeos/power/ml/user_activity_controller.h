c97942cb7f4f295899cba189a53cfbc1903018fe
alanlxl <alanlxl@chromium.org>
2020-10-13 04:57:17 +0000

    Clean old smart dim model and ml_service_client code
    Now that new ml agent is enabled by default, we can remove the old model
    and ml_service_client code of smart dim.
    
    Also change the unit test accordingly.
    
    Bug: 1067048, 1136331
    Test: Unit test passed & tested on DUT
    Change-Id: Ife97ecd54d0dec0ddcd6c816517c4c71629ce0eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462989
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816439}

--- chrome/browser/chromeos/power/ml/user_activity_controller.h
+++ chrome/browser/chromeos/power/ml/user_activity_controller.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "chrome/browser/chromeos/power/ml/idle_event_notifier.h"
-#include "chrome/browser/chromeos/power/ml/smart_dim/model_impl.h"
 #include "chrome/browser/chromeos/power/ml/user_activity_manager.h"
 #include "chrome/browser/chromeos/power/ml/user_activity_ukm_logger_impl.h"
 
@@ -34,7 +33,6 @@ class UserActivityController {
   std::unique_ptr<IdleEventNotifier> idle_event_notifier_;
   UserActivityUkmLoggerImpl user_activity_ukm_logger_;
   std::unique_ptr<UserActivityManager> user_activity_manager_;
-  SmartDimModelImpl smart_dim_model_;
 
   DISALLOW_COPY_AND_ASSIGN(UserActivityController);
 };
