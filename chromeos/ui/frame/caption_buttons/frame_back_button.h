0f5a227a0db2094aea6200f8f7b09e9c99ec9911
Antonio Gomes <tonikitoo@igalia.com>
2020-10-13 19:39:11 +0000

    Reland "Add TabletState::Observer and adopt in BrowserNonClientFrameViewAsh"
    This is identical to the original CL (https://crrev.com/c/2462560).
    The fix happened on the predecessor CL (https://crrev.com/c/2466739).
    
    Original change's description:
    
    > This replaces the use of ash::TabletMode and ash::TabletModeObserver in
    > BrowserNonClientFrameViewAsh.
    >
    > This corresponds to phase 2.6 on the design document [1].
    >
    > Covered by existing tests, eg
    >
    > BrowserNonClientFrameViewAshTest.BrowserHeaderVisibilityInTabletModeTest
    > HomeLauncherBrowserNonClientFrameViewAshTest.TabletModeBrowserCaptionButtonVisibility
    >
    > [1] https://docs.google.com/document/d/1xHwcHrAiEaWuA4usGEqKZ9zm1H8SGk3WkS-Jf2Q_los/
    >
    > BUG=1113900
    > R=jamescook@chromium.org, sky@chromium.org
    >
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462560
    > Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#816316}
    
    TBR=jamescook@chromium.org
    
    Change-Id: Ic9cc488266a42ca4efc40fdfe4edbf7e00c8f93c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2468496
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#816712}

--- chromeos/ui/frame/caption_buttons/frame_back_button.h
+++ chromeos/ui/frame/caption_buttons/frame_back_button.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_PUBLIC_CPP_CAPTION_BUTTONS_FRAME_BACK_BUTTON_H_
-#define ASH_PUBLIC_CPP_CAPTION_BUTTONS_FRAME_BACK_BUTTON_H_
+#ifndef CHROMEOS_UI_FRAME_CAPTION_BUTTONS_FRAME_BACK_BUTTON_H_
+#define CHROMEOS_UI_FRAME_CAPTION_BUTTONS_FRAME_BACK_BUTTON_H_
 
 #include "base/component_export.h"
 #include "ui/views/window/frame_caption_button.h"
@@ -29,4 +29,4 @@ class COMPONENT_EXPORT(CHROMEOS_UI_FRAME) FrameBackButton
 
 }  // namespace chromeos
 
-#endif  //  ASH_PUBLIC_CPP_CAPTION_BUTTONS_FRAME_BACK_BUTTON_H_
+#endif  //  CHROMEOS_UI_FRAME_CAPTION_BUTTONS_FRAME_BACK_BUTTON_H_
