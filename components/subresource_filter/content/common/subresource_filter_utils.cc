a7d6857890870e3c6fdf46f94430708224862ed6
Alex Turner <alexmt@chromium.org>
2020-10-13 00:42:32 +0000

    Inherit subresource filter activation from opener for certain mainframes
    Currently, mainframes which do not have a committed load (e.g. due to
    their initial load being aborted by a document.write() call) never have
    the subresource filter activated. Further, each popup mainframe that is
    same-origin to its opener (whether or not it has a committed load)
    should share the activation of its opener as the opener determines its
    contents including, for example, through document.write() calls.
    
    There are two cases to consider for same-origin popups. First, a popup
    with an inherited origin (e.g. an about:blank frame) should inherit the
    activation of its opener; it would not otherwise be activated as its URL
    wouldn't match the filter list. Second, a popup with a URL that is
    handled by the network stack and is same-origin does not need to inherit
    its activation as activation-rules are site-based, i.e. apply equally to
    all pages with the same eTLD+1. The activation resulting from the normal
    process for committed navigations will therefore be the same as the
    opener's.
    
    This cl creates a filter with the activation of the opener for
    mainframes without a committed load or with an inherited origin. If the
    inherited activation is disabled, no filter is created. This inheritance
    behavior is similar to that of subframes with an aborted initial load
    and of about:blank subframes, both of which inherit the activation of
    their parent frames.
    
    Design doc (internal-only): https://docs.google.com/document/d/12ocy5qgoMZh0Ntl6Ah3Q0GShXzRxHmSmArDy7gWGtJM/edit?usp=sharing
    
    Bug: 1055558
    Change-Id: I46d3d73d193f86323fe5b9e4c90ceba989ab7abe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219104
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816380}

--- components/subresource_filter/content/common/subresource_filter_utils.cc
+++ components/subresource_filter/content/common/subresource_filter_utils.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace subresource_filter {
 
-bool ShouldUseParentActivation(const GURL& url) {
+bool ShouldInheritActivation(const GURL& url) {
   return !content::IsURLHandledByNetworkStack(url);
 }
 
