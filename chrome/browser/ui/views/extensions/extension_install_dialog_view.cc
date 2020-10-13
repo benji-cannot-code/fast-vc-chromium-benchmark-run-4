fd2a2ab45c63d6f693250b7bd5607dfde8bb2e89
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2020-10-13 10:51:57 +0000

    'Add extension' bubble to use the dialog accessible role.
    This bubble is modal: it takes focus when shown up and does not let
    users manipulate other UI until having explicitly dealt with it. For
    this reason, the dialog role is more appropriate.
    
    As a result of this change, the bug where the bubble content was not
    always read is gone.
    
    The deleted code was supposed to fix crbug.com/849910, but now this
    bubble is properly announced by JAWS 2020 in addition to NVDA.
    
    Bug: 1123107
    Change-Id: I371523a4384b7338e35ba30f897bade1d3ba2ee5
    AX-relnotes: always announce add extension bubble correctly.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461276
    Commit-Queue: Jacobo Aragunde Pérez <jaragunde@igalia.com>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816510}

--- chrome/browser/ui/views/extensions/extension_install_dialog_view.cc
+++ chrome/browser/ui/views/extensions/extension_install_dialog_view.cc
@@ -422,10 +422,6 @@ void ExtensionInstallDialogView::OnShutdown(
   CloseDialog();
 }
 
-ax::mojom::Role ExtensionInstallDialogView::GetAccessibleWindowRole() {
-  return ax::mojom::Role::kAlertDialog;
-}
-
 base::string16 ExtensionInstallDialogView::GetAccessibleWindowTitle() const {
   return title_;
 }
