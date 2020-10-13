f557d7b5c1a4617f90b6445bca4e19480302ee41
Klaus Weidner <klausw@chromium.org>
2020-10-13 19:50:24 +0000

    WebXR: Tweak ARCore scheduling, add trace events
    Update the ARCore scheduling to reduce latency, running GetFrameData and
    ARCore's update with a delay when appropriate.
    
    Use sliding averages to track timing for recent frames, and use that in
    the heuristics.
    
    Also add async trace events to mark Animating/Processing/Rendering
    frames in the ArCoreGL timeline.
    
    For background, see this presentation with some annotated trace events
    explaining the new logic:
    
    https://docs.google.com/presentation/d/e/2PACX-1vQTol3A0Y_LByOuMPSIb0qk-6FNQ7f5SpqO3GWfN-JY37LO7HPXTv1FrfK5e4Qerxx4CNzw9f90auCA/pub
    
    Bug: 1136276
    Change-Id: I0550ed3666c47d6f12b2708a1e5ef9a305b07f10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2464052
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816724}

--- device/vr/android/arcore/arcore_gl.h
+++ device/vr/android/arcore/arcore_gl.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/public/mojom/isolated_xr_service.mojom.h"
 #include "device/vr/public/mojom/vr_service.mojom.h"
 #include "device/vr/util/fps_meter.h"
+#include "device/vr/util/sliding_average.h"
 #include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "mojo/public/cpp/bindings/pending_associated_remote.h"
@@ -181,6 +182,11 @@ class ArCoreGl : public mojom::XRFrameDataProvider,
 
   void GetRenderedFrameStats();
   void FinishRenderingFrame();
+  base::TimeDelta EstimatedArCoreFrameTime();
+  base::TimeDelta WaitTimeForArCoreUpdate();
+  base::TimeDelta WaitTimeForRenderCompletion();
+  void ScheduleGetFrameData();
+  void RunPendingGetFrameData();
 
   bool IsFeatureEnabled(mojom::XRSessionFeature feature);
 
@@ -251,6 +257,13 @@ class ArCoreGl : public mojom::XRFrameDataProvider,
   bool is_paused_ = true;
 
   bool restrict_frame_data_ = false;
+
+  base::TimeTicks last_arcore_update_time_;
+  base::TimeDelta last_arcore_frame_timestamp_;
+
+  device::SlidingTimeDeltaAverage average_animate_time_;
+  device::SlidingTimeDeltaAverage average_process_time_;
+  device::SlidingTimeDeltaAverage average_render_time_;
 
   FPSMeter fps_meter_;
 
