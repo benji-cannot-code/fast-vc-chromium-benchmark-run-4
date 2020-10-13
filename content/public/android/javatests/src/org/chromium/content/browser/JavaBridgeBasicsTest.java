e4661d64472b8be431cb3b10d9fa5448edde8a41
Miyoung Shin <myid.shin@igalia.com>
2020-10-13 11:39:32 +0000

    [remoteobjects] Implement RemoteObjectInjector#removeInterface
    This CL implements RemoteObjectInjector#removeInterface which
    should call the RemoveNamedObject method on a RemoteObjectGateway
    remote.
    
    This CL changes to removing the unnamed object on only the browser
    side to secure that the object is injected again on the renderer.
    It is consistent with the old code, and  fixes
    JavaBridgeBasicsTest#testRemovalNotReflectedUntilReload.
    
    Bug: 1105925
    Change-Id: I7a304dba05f4e09b190658f684c13632497481d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2379270
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#816520}

--- content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeBasicsTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/JavaBridgeBasicsTest.java
@@ -174,7 +174,7 @@ public class JavaBridgeBasicsTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView", "Android-JavaBridge"})
-    @UseMethodParameter(JavaBridgeActivityTestRule.LegacyTestParams.class)
+    @UseMethodParameter(JavaBridgeActivityTestRule.MojoTestParams.class)
     public void testRemovalNotReflectedUntilReload(boolean useMojo) throws Throwable {
         mActivityTestRule.injectObjectAndReload(new Object() {
             public void method() {
@@ -331,7 +331,7 @@ public class JavaBridgeBasicsTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView", "Android-JavaBridge"})
-    @UseMethodParameter(JavaBridgeActivityTestRule.LegacyTestParams.class)
+    @UseMethodParameter(JavaBridgeActivityTestRule.MojoTestParams.class)
     public void testReplaceInjectedObject(boolean useMojo) throws Throwable {
         mActivityTestRule.injectObjectAndReload(new Object() {
             public void method() {
@@ -362,7 +362,7 @@ public class JavaBridgeBasicsTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView", "Android-JavaBridge"})
-    @UseMethodParameter(JavaBridgeActivityTestRule.LegacyTestParams.class)
+    @UseMethodParameter(JavaBridgeActivityTestRule.MojoTestParams.class)
     public void testReplaceInjectedObjectWithNullObjectIsIgnored(boolean useMojo) throws Throwable {
         mActivityTestRule.injectObjectAndReload(new Object(), "testObject");
         Assert.assertEquals("object", executeJavaScriptAndGetStringResult("typeof testObject"));
@@ -1040,7 +1040,7 @@ public class JavaBridgeBasicsTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView", "Android-JavaBridge"})
-    @UseMethodParameter(JavaBridgeActivityTestRule.LegacyTestParams.class)
+    @UseMethodParameter(JavaBridgeActivityTestRule.MojoTestParams.class)
     public void testReplaceJavascriptInterface(boolean useMojo) throws Throwable {
         class Test {
             public Test(int value) {
