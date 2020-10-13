e376fb3d2aef25bfdf4bc27dd1f4085384876fd4
Klaus Weidner <klausw@chromium.org>
2020-10-13 20:16:20 +0000

    WebXR: add "xr.debug" DISABLED_BY_DEFAULT traces
    Add a new DISABLED_BY_DEFAULT trace category "xr.debug", and use
    that for some timing traces that are useful when debugging scheduling
    issues but a bit verbose otherwise.
    
    Change-Id: Id72c0b6c8f7a7fab2e7428adcffa095915a61f54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2468277
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816740}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -255,7 +255,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X(TRACE_DISABLED_BY_DEFAULT("viz.triangles"))                          \
   X(TRACE_DISABLED_BY_DEFAULT("webaudio.audionode"))                     \
   X(TRACE_DISABLED_BY_DEFAULT("webrtc"))                                 \
-  X(TRACE_DISABLED_BY_DEFAULT("worker.scheduler"))
+  X(TRACE_DISABLED_BY_DEFAULT("worker.scheduler"))                       \
+  X(TRACE_DISABLED_BY_DEFAULT("xr.debug"))
 
 #define INTERNAL_TRACE_INIT_CATEGORY_NAME(name) name,
 
