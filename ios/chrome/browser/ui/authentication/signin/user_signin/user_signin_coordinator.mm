2fe3783d65d2ab2e58fd775773846ee62ad9541c
Nohemi Fernandez <fernandex@chromium.org>
2020-10-13 10:06:38 +0000

    [iOS] Disallow use of UserSigninCoordinator for signed-in users.
    Adds a comment explaining the flexibility of the AuthenticationFlow
    service and how this may cause issues in sign-in UI expectations.
    
    Bug: 1125631
    Change-Id: Ia2f5aa12a7092c57a2c91f8eae1df6079ddd980d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461345
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816501}

--- ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_coordinator.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/mac/foundation_util.h"
 #import "ios/chrome/browser/main/browser.h"
+#import "ios/chrome/browser/signin/authentication_service.h"
 #import "ios/chrome/browser/signin/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/identity_manager_factory.h"
 #import "ios/chrome/browser/sync/consent_auditor_factory.h"
@@ -101,6 +102,12 @@ const CGFloat kFadeOutAnimationDuration = 0.16f;
 #pragma mark - SigninCoordinator
 
 - (void)start {
+  // The user should be signed out before triggering sign-in or upgrade states.
+  // Users are allowed to be signed-in during FirstRun for testing purposes.
+  DCHECK(!AuthenticationServiceFactory::GetForBrowserState(
+              self.browser->GetBrowserState())
+              ->IsAuthenticated() ||
+         self.signinIntent == UserSigninIntentFirstRun);
   [super start];
   self.viewController = [[UserSigninViewController alloc] init];
   self.viewController.delegate = self;
