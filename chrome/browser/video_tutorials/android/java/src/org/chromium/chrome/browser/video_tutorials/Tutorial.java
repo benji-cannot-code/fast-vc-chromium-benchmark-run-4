e5823272e0dc104b7ab26672bf8b64c2e34b5e19
Shakti Sahu <shaktisahu@chromium.org>
2020-10-13 19:05:53 +0000

    Video tutorials : Several fixes
    This CL
    1 - Changes Tutorial not to be a subclass of ImageTile.
    2 - Handled additional feature type enum types.
    3 - Added some unittests
    4 - Fixed a CSS bug
    
    Change-Id: Iceca0057f7e0d9b549af5481a12c7f1565e21441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2464855
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816693}

--- chrome/browser/video_tutorials/android/java/src/org/chromium/chrome/browser/video_tutorials/Tutorial.java
+++ chrome/browser/video_tutorials/android/java/src/org/chromium/chrome/browser/video_tutorials/Tutorial.java
@@ -5,13 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.video_tutorials;
 
-import org.chromium.components.browser_ui.widget.image_tiles.ImageTile;
-
 /**
  * Class encapsulating data needed to show a video tutorial on the UI.
  */
-public class Tutorial extends ImageTile {
+public class Tutorial {
     public final @FeatureType int featureType;
+    public final String title;
+    public final String accessibilityText;
     public final String videoUrl;
     public final String posterUrl;
     public final String captionUrl;
@@ -21,16 +21,13 @@ public class Tutorial extends ImageTile {
     /** Constructor */
     public Tutorial(@FeatureType int featureType, String title, String videoUrl, String posterUrl,
             String captionUrl, String shareUrl, int videoLength) {
-        super(createIdFromFeatureType(featureType), title, title);
         this.featureType = featureType;
+        this.title = title;
+        this.accessibilityText = title;
         this.videoUrl = videoUrl;
         this.posterUrl = posterUrl;
         this.captionUrl = captionUrl;
         this.shareUrl = shareUrl;
         this.videoLength = videoLength;
-    }
-
-    private static String createIdFromFeatureType(int featureType) {
-        return String.valueOf(featureType);
     }
 }
