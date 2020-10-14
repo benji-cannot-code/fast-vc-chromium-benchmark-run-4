6bd8abd5271975f102ab99a639d33164c708e169
Adlai Holler <adlai@google.com>
2020-10-14 16:09:05 +0000

    Migrate skia_surfaceless_gl_renderer to GrDirectContext
    Just a name update to keep up with Skia.
    
    Change-Id: I61118e4cf4a18f5bdadbd7005dbf1f69be12e223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2470464
    Auto-Submit: Adlai Holler <adlai@google.com>
    Commit-Queue: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817032}

--- ui/ozone/demo/skia/skia_surfaceless_gl_renderer.cc
+++ ui/ozone/demo/skia/skia_surfaceless_gl_renderer.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkDeferredDisplayListRecorder.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 #include "third_party/skia/include/gpu/GrBackendSurface.h"
+#include "third_party/skia/include/gpu/GrDirectContext.h"
 #include "third_party/skia/include/gpu/gl/GrGLAssembleInterface.h"
 #include "third_party/skia/include/gpu/gl/GrGLInterface.h"
 #include "ui/display/types/display_snapshot.h"
@@ -81,7 +82,7 @@ class SurfacelessSkiaGlRenderer::BufferWrapper {
   gl::GLImage* image() const { return image_.get(); }
   SkSurface* sk_surface() const { return sk_surface_.get(); }
 
-  bool Initialize(GrContext* gr_context,
+  bool Initialize(GrDirectContext* gr_context,
                   gfx::AcceleratedWidget widget,
                   const gfx::Size& size);
   void BindFramebuffer();
@@ -107,7 +108,7 @@ SurfacelessSkiaGlRenderer::BufferWrapper::~BufferWrapper() {
 }
 
 bool SurfacelessSkiaGlRenderer::BufferWrapper::Initialize(
-    GrContext* gr_context,
+    GrDirectContext* gr_context,
     gfx::AcceleratedWidget widget,
     const gfx::Size& size) {
   glGenTextures(1, &gl_tex_);
