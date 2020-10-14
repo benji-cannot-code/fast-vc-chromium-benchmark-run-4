2164730879133e86fb58f2cd48b5404924278c52
Fabio Tirelo <ftirelo@chromium.org>
2020-10-14 16:17:22 +0000

    [Sheriff] Reverts many CLs that tried to fix the same issue
    This should reopen the tree:
    
    This reverts commit 53d410bbd3cea8de9389ea2f454dcd2dc16b2745.
    This reverts commit eab74a794178251b55da72984babfc59ec49e9c5.
    This reverts commit 9cdd5ba89ae79af952a3794790664af24e2e67bb.
    
    Change-Id: Ifa97474155de2803dc95f5eb73573d7171ef6cb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2471677
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817036}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -152,6 +152,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/paint/paint_chunk_subset_recorder.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_controller.h"
 #include "third_party/blink/renderer/platform/instrumentation/histogram.h"
+#include "third_party/blink/renderer/platform/instrumentation/resource_coordinator/document_resource_coordinator.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/traced_value.h"
 #include "third_party/blink/renderer/platform/language.h"
