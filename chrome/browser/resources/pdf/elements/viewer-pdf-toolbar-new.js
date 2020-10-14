6c16a47798c87d86f59ea8729da01e8eb95d13a1
rbpotter <rbpotter@chromium.org>
2020-10-14 19:04:03 +0000

    PDF Viewer Update: Disable zoom buttons when at limits
    Bug: 1136706
    Change-Id: I3e713ae638d14b9d0d3a9af2357bd7147217d326
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2467580
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817132}

--- chrome/browser/resources/pdf/elements/viewer-pdf-toolbar-new.js
+++ chrome/browser/resources/pdf/elements/viewer-pdf-toolbar-new.js
@@ -73,10 +73,7 @@ export class ViewerPdfToolbarNewElement extends PolymerElement {
       pdfFormSaveEnabled: Boolean,
       printingEnabled: Boolean,
       rotated: Boolean,
-      viewportZoom: {
-        type: Number,
-        observer: 'viewportZoomChanged_',
-      },
+      viewportZoom: Number,
       /** @type {!{min: number, max: number}} */
       zoomBounds: Object,
 
@@ -94,6 +91,13 @@ export class ViewerPdfToolbarNewElement extends PolymerElement {
       fitToButtonIcon_: {
         type: String,
         computed: 'computeFitToButtonIcon_(fittingType_)',
+      },
+
+      /** @private */
+      viewportZoomPercent_: {
+        type: Number,
+        computed: 'computeViewportZoomPercent_(viewportZoom)',
+        observer: 'viewportZoomPercentChanged_',
       },
 
       // <if expr="chromeos">
@@ -157,6 +161,14 @@ export class ViewerPdfToolbarNewElement extends PolymerElement {
   }
 
   /**
+   * @return {number}
+   * @private
+   */
+  computeViewportZoomPercent_() {
+    return Math.round(100 * this.viewportZoom);
+  }
+
+  /**
    * @param {string} fitToPageTooltip
    * @param {string} fitToWidthTooltip
    * @return {string} The appropriate tooltip for the current state
@@ -173,9 +185,8 @@ export class ViewerPdfToolbarNewElement extends PolymerElement {
   }
 
   /** @private */
-  viewportZoomChanged_() {
-    const zoom = Math.round(this.viewportZoom * 100);
-    this.getZoomInput_().value = `${zoom}%`;
+  viewportZoomPercentChanged_() {
+    this.getZoomInput_().value = `${this.viewportZoomPercent_}%`;
   }
 
   // <if expr="chromeos">
@@ -301,8 +312,7 @@ export class ViewerPdfToolbarNewElement extends PolymerElement {
       return;
     }
 
-    const zoom = Math.round(this.viewportZoom * 100);
-    const zoomString = `${zoom}%`;
+    const zoomString = `${this.viewportZoomPercent_}%`;
     input.value = zoomString;
   }
 
@@ -350,6 +360,24 @@ export class ViewerPdfToolbarNewElement extends PolymerElement {
    */
   onMoreOpenChanged_(e) {
     this.moreMenuOpen_ = e.detail.value;
+  }
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  isAtMinimumZoom_() {
+    return this.zoomBounds !== undefined &&
+        this.viewportZoomPercent_ === this.zoomBounds.min;
+  }
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  isAtMaximumZoom_() {
+    return this.zoomBounds !== undefined &&
+        this.viewportZoomPercent_ === this.zoomBounds.max;
   }
 
   // <if expr="chromeos">
