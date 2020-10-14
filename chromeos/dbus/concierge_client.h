88ba6d0ee74db39bac3af35b6bd055780997c5f2
Muhammad Hasan Khan <mhasank@chromium.org>
2020-10-14 06:31:58 +0000

    login: use same observer list policy in real and fake concierge client
    This change is needed to ensure that fake behaves the same as real
    client and we can use it in the unit test to validate that arc
    instance stopped is not fired on unrelated instance.
    
    BUG=b:164816080
    TEST=cq
    
    Change-Id: Ice5e588b81231ac87b78dafd25188d6629b38bd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2469231
    Auto-Submit: Muhammad Hasan Khan <mhasank@chromium.org>
    Commit-Queue: Muhammad Hasan Khan <mhasank@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816907}

--- chromeos/dbus/concierge_client.h
+++ chromeos/dbus/concierge_client.h
@@ -22,6 +22,9 @@ namespace chromeos {
 // start and stop VMs, as well as for disk image management.
 class COMPONENT_EXPORT(CHROMEOS_DBUS) ConciergeClient : public DBusClient {
  public:
+  static constexpr base::ObserverListPolicy kObserverListPolicy =
+      base::ObserverListPolicy::EXISTING_ONLY;
+
   // Used for observing Concierge service itself.
   class Observer : public base::CheckedObserver {
    public:
