1ff726c6a9b4f16bdc3cf54e7af1857f8a7539b3
Prashant Nevase <prashant.n@samsung.com>
2020-10-14 20:55:56 +0000

    Plumb DarkModeFilter to cc.
    This patch adds plumbing code needed to call blink dark mode filter
    in compositor. The dark mode filter created in blink will be used for
    applying dark mode to images during rasterization.
    
    Bug: 1094005
    Change-Id: I620e8ab8b6dbb6d9050a3696e37ff2e89d6728b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2452091
    Commit-Queue: Prashant Nevase <prashant.n@samsung.com>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817185}

--- third_party/blink/renderer/platform/graphics/dark_mode_image_classifier.cc
+++ third_party/blink/renderer/platform/graphics/dark_mode_image_classifier.cc
@@ -42,7 +42,7 @@ DarkModeImageClassifier::DarkModeImageClassifier() = default;
 DarkModeImageClassifier::~DarkModeImageClassifier() = default;
 
 DarkModeResult DarkModeImageClassifier::Classify(const SkPixmap& pixmap,
-                                                 const SkIRect& src) {
+                                                 const SkIRect& src) const {
   // Empty pixmap or |src| out of bounds cannot be classified.
   SkIRect bounds = pixmap.bounds();
   if (src.isEmpty() || bounds.isEmpty() || !bounds.contains(src) ||
@@ -58,7 +58,7 @@ DarkModeResult DarkModeImageClassifier::Classify(const SkPixmap& pixmap,
 
 base::Optional<DarkModeImageClassifier::Features>
 DarkModeImageClassifier::GetFeatures(const SkPixmap& pixmap,
-                                     const SkIRect& src) {
+                                     const SkIRect& src) const {
   DCHECK(!pixmap.bounds().isEmpty());
   float transparency_ratio;
   float background_ratio;
@@ -81,7 +81,7 @@ void DarkModeImageClassifier::GetSamples(const SkPixmap& pixmap,
                                          const SkIRect& src,
                                          std::vector<SkColor>* sampled_pixels,
                                          float* transparency_ratio,
-                                         float* background_ratio) {
+                                         float* background_ratio) const {
   DCHECK(!src.isEmpty());
 
   int num_sampled_pixels =
@@ -148,7 +148,7 @@ void DarkModeImageClassifier::GetBlockSamples(
     const SkIRect& block,
     const int required_samples_count,
     std::vector<SkColor>* sampled_pixels,
-    int* transparent_pixels_count) {
+    int* transparent_pixels_count) const {
   *transparent_pixels_count = 0;
 
   DCHECK(pixmap.bounds().contains(block));
@@ -174,7 +174,7 @@ void DarkModeImageClassifier::GetBlockSamples(
 DarkModeImageClassifier::Features DarkModeImageClassifier::ComputeFeatures(
     const std::vector<SkColor>& sampled_pixels,
     const float transparency_ratio,
-    const float background_ratio) {
+    const float background_ratio) const {
   int samples_count = static_cast<int>(sampled_pixels.size());
 
   // Is image grayscale.
@@ -199,7 +199,7 @@ DarkModeImageClassifier::Features DarkModeImageClassifier::ComputeFeatures(
 
 float DarkModeImageClassifier::ComputeColorBucketsRatio(
     const std::vector<SkColor>& sampled_pixels,
-    const ColorMode color_mode) {
+    const ColorMode color_mode) const {
   std::set<uint16_t> buckets;
 
   // If image is in color, use 4 bits per color channel, otherwise 4 bits for
@@ -229,7 +229,7 @@ float DarkModeImageClassifier::ComputeColorBucketsRatio(
 }
 
 DarkModeResult DarkModeImageClassifier::ClassifyWithFeatures(
-    const Features& features) {
+    const Features& features) const {
   DarkModeResult result = ClassifyUsingDecisionTree(features);
 
   // If decision tree cannot decide, we use a neural network to decide whether
@@ -254,7 +254,7 @@ DarkModeResult DarkModeImageClassifier::ClassifyWithFeatures(
 }
 
 DarkModeResult DarkModeImageClassifier::ClassifyUsingDecisionTree(
-    const DarkModeImageClassifier::Features& features) {
+    const DarkModeImageClassifier::Features& features) const {
   float low_color_count_threshold =
       kLowColorCountThreshold[features.is_colorful];
   float high_color_count_threshold =
