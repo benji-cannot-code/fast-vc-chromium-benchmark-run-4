62942d7d6573a4a734dd830eead8864201e5e83b
Aaron Leventhal <aleventhal@google.com>
2020-10-14 17:02:54 +0000

    Do not run perf checker on Android
    TBR=dmazzoni@chromium.org
    
    AX-RelNotes: n/a
    Bug: 1137892
    Change-Id: Iaa1108d255b568eb2866b8a82c70efe06ff61837
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2469097
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817057}

--- content/browser/accessibility/cross_platform_accessibility_browsertest.cc
+++ content/browser/accessibility/cross_platform_accessibility_browsertest.cc
@@ -41,9 +41,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/win/atl_module.h"
 #endif
 
-#if defined(NDEBUG) && !defined(ADDRESS_SANITIZER) &&         \
-    !defined(LEAK_SANITIZER) && !defined(MEMORY_SANITIZER) && \
-    !defined(THREAD_SANITIZER) && !defined(UNDEFINED_SANITIZER)
+#if defined(NDEBUG) && !defined(ADDRESS_SANITIZER) &&              \
+    !defined(LEAK_SANITIZER) && !defined(MEMORY_SANITIZER) &&      \
+    !defined(THREAD_SANITIZER) && !defined(UNDEFINED_SANITIZER) && \
+    !defined(OS_ANDROID)
 #define IS_FAST_BUILD
 #endif
 
