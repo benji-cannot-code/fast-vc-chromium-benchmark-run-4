4f67ea16f147c6a677ec98211d78462e1d5dc0cd
Rushan Suleymanov <rushans@google.com>
2020-10-14 09:08:15 +0000

    [Sync] Fix bookmark test flakiness.
    For bookmarks reupload the server state should be checked before the
    sync is enabled. After sync is enabled all bookmarks will be reuploaded
    right away.
    
    Bug: 1137866
    Change-Id: I555062ee3b2753cd38ccf9500af7284d5b11f64a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2470520
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#816937}

--- chrome/browser/sync/test/integration/single_client_bookmarks_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_bookmarks_sync_test.cc
@@ -1488,8 +1488,6 @@ IN_PROC_BROWSER_TEST_F(
 IN_PROC_BROWSER_TEST_F(
     SingleClientBookmarksSyncTestWithEnabledReuploadRemoteBookmarks,
     ShouldReuploadFullTitleAfterRestartOnIncrementalChange) {
-  ASSERT_TRUE(SetupClients());
-
   // Check that the full title was not uploaded to the server yet.
   ASSERT_EQ(
       1u,
@@ -1501,6 +1499,7 @@ IN_PROC_BROWSER_TEST_F(
                    .bookmark()
                    .has_full_title());
 
+  ASSERT_TRUE(SetupClients());
 #if defined(OS_CHROMEOS)
   // signin::SetRefreshTokenForPrimaryAccount() is needed on ChromeOS in order
   // to get a non-empty refresh token on startup.
