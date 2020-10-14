6871fc2d414ce7710cc375e8fd967380b0f8c326
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-10-14 13:07:22 +0000

    [cleanup] Remove VisualOverflowRect check in LayoutSVGRoot::UpdateLayout
    There should be no behaviour change.
    
    Change-Id: Ic935f758ffff558a4d746518cf413dde275e04e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2468540
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816984}

--- third_party/blink/renderer/core/layout/svg/layout_svg_root.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_root.cc
@@ -259,7 +259,6 @@ void LayoutSVGRoot::UpdateLayout() {
     needs_boundaries_or_transform_update_ = false;
   }
 
-  const auto& old_overflow_rect = VisualOverflowRect();
   ClearSelfNeedsLayoutOverflowRecalc();
   ClearLayoutOverflow();
 
@@ -267,7 +266,7 @@ void LayoutSVGRoot::UpdateLayout() {
   // (the entire SVG) could have moved or new content may have been exposed, so
   // mark the entire subtree as needing paint invalidation checking.
   if (transform_change != SVGTransformChange::kNone ||
-      viewport_may_have_changed || old_overflow_rect != VisualOverflowRect()) {
+      viewport_may_have_changed) {
     SetSubtreeShouldCheckForPaintInvalidation();
     SetNeedsPaintPropertyUpdate();
     if (Layer())
