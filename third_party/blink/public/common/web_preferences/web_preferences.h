3260352dabaaee5bf3a4b31e5f909cb95dfbc039
Gyuyoung Kim <gyuyoung@igalia.com>
2020-10-14 00:16:01 +0000

    Replace all uses of web_pref::ViewportStyle with mojom::ViewportStyle
    This CL replaces all uses of web_pref::ViewportStyle with
    mojom::ViewportStyle to reduce unnecessary type
    conversions.
    
    Bug: 1097943
    Change-Id: I60654404773b4c94520bb5a1736d41b0c33dfe28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463000
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#816828}

--- third_party/blink/public/common/web_preferences/web_preferences.h
+++ third_party/blink/public/common/web_preferences/web_preferences.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "net/nqe/effective_connection_type.h"
 #include "third_party/blink/public/common/common_export.h"
-#include "third_party/blink/public/common/web_preferences/viewport_style.h"
 #include "third_party/blink/public/mojom/css/preferred_color_scheme.mojom-shared.h"
 #include "third_party/blink/public/mojom/v8_cache_options.mojom-forward.h"
 #include "third_party/blink/public/mojom/webpreferences/web_preferences.mojom-shared.h"
@@ -145,7 +144,7 @@ struct BLINK_COMMON_EXPORT WebPreferences {
   // viewport_enabled is on.
   bool shrinks_viewport_contents_to_fit;
 
-  web_pref::ViewportStyle viewport_style;
+  blink::mojom::ViewportStyle viewport_style;
   bool always_show_context_menu_on_touch;
   bool smooth_scroll_for_find_enabled;
   bool main_frame_resizes_are_orientation_changes;
--- third_party/blink/public/web/web_settings.h
+++ third_party/blink/public/web/web_settings.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unicode/uscript.h>
 
 #include "third_party/blink/public/common/css/navigation_controls.h"
-#include "third_party/blink/public/common/web_preferences/viewport_style.h"
 #include "third_party/blink/public/common/web_preferences/web_preferences.h"
 #include "third_party/blink/public/mojom/v8_cache_options.mojom-forward.h"
 #include "third_party/blink/public/mojom/webpreferences/web_preferences.mojom-forward.h"
@@ -242,7 +241,7 @@ class WebSettings {
   virtual void SetTouchDragEndContextMenu(bool) = 0;
   virtual void SetBarrelButtonForDragEnabled(bool) = 0;
   virtual void SetUseLegacyBackgroundSizeShorthandBehavior(bool) = 0;
-  virtual void SetViewportStyle(web_pref::ViewportStyle) = 0;
+  virtual void SetViewportStyle(mojom::ViewportStyle) = 0;
   virtual void SetUseWideViewport(bool) = 0;
   virtual void SetV8CacheOptions(mojom::V8CacheOptions) = 0;
   virtual void SetValidationMessageTimerMagnification(int) = 0;
