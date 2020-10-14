f0a1fb2b1a8891dd90320b5f008f149ac0c91fa0
Zentaro Kavanagh <zentaro@chromium.org>
2020-10-14 17:05:08 +0000

    Diagnostics: Remove redundant flush imports
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I124bec778c1fc7376b463b78bb323538cd6a5395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2469837
    Auto-Submit: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817061}

--- chrome/test/data/webui/chromeos/diagnostics/data_point_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/data_point_test.js
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
 import 'chrome://diagnostics/data_point.js';
 
-import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {flushTasks} from 'chrome://test/test_util.m.js';
 
 
