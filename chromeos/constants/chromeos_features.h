2e7b5ea6ba3de82e3c809d9c87c4d9999cf3f2a1
Felipe Andrade <fsandrade@chromium.org>
2020-10-14 13:17:44 +0000

    Create a flag for Kerberos Settings section
    Adds a feature flag for the new Kerberos section in ChromeOS settings.
    When disabled, Kerberos settings will stay under People Section,
    keeping current behaviour.
    
    Bug: 983041
    Change-Id: Id750ae851c6a3ef522c0afdf6d29a91014f63cc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462255
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816988}

--- chromeos/constants/chromeos_features.h
+++ chromeos/constants/chromeos_features.h
@@ -178,6 +178,8 @@ COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kVirtualKeyboardFloatingDefault;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kInstantTethering;
+COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
+extern const base::Feature kKerberosSettingsSection;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS) extern const base::Feature kLacrosSupport;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kLanguageSettingsUpdate;
