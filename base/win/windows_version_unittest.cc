9940df188608f316b44818376b5bde0057e03141
Will Harris <wfh@chromium.org>
2020-10-14 01:54:33 +0000

    Renumber base::win::Version and stop emitting histograms based on it.
    Add missing WIN10_19H2 entry, and update tests.
    
    BUG=1130738
    
    Change-Id: Ia9208cebdd092c084468bf31482907afe56a4ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2458328
    Auto-Submit: Will Harris <wfh@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816845}

--- base/win/windows_version_unittest.cc
+++ base/win/windows_version_unittest.cc
@@ -11,11 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace win {
 
-TEST(WindowsVersion, GetVersionExAndKernelVersionMatch) {
+TEST(WindowsVersion, GetVersionExAndKernelOsVersionMatch) {
   // If this fails, we're running in compatibility mode, or need to update the
   // application manifest.
-  EXPECT_EQ(OSInfo::GetInstance()->version(),
-            OSInfo::GetInstance()->Kernel32Version());
+  // Note: not all versions of Windows return identical build numbers e.g.
+  // 1909/19H2 kernel32.dll has build number 18362 but OS version build number
+  // 18363.
+  EXPECT_EQ(OSInfo::GetInstance()->Kernel32VersionNumber().major,
+            OSInfo::GetInstance()->version_number().major);
+  EXPECT_EQ(OSInfo::GetInstance()->Kernel32VersionNumber().minor,
+            OSInfo::GetInstance()->version_number().minor);
 }
 
 TEST(OSInfo, MajorMinorBuildToVersion) {
@@ -23,6 +28,8 @@ TEST(OSInfo, MajorMinorBuildToVersion) {
             Version::WIN10_20H1);
   EXPECT_EQ(OSInfo::MajorMinorBuildToVersion(10, 0, 19041),
             Version::WIN10_20H1);
+  EXPECT_EQ(OSInfo::MajorMinorBuildToVersion(10, 0, 18363),
+            Version::WIN10_19H2);
   EXPECT_EQ(OSInfo::MajorMinorBuildToVersion(10, 0, 18362),
             Version::WIN10_19H1);
   EXPECT_EQ(OSInfo::MajorMinorBuildToVersion(10, 0, 17763), Version::WIN10_RS5);
