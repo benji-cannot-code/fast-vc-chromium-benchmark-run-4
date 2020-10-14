64058edec0ac5073d0f77c215ef75738c4720c37
Toni Barzic <tbarzic@chromium.org>
2020-10-14 19:48:01 +0000

    Remove location a11y notifications for transform animations
    These were added so chromevox properly updates the highlight
    location when transform animations complete. Previously, the view
    location was updated only when view bounds changed. This has since
    been addressed in the views code, so app list specific workarounds are
    no longer needed.
    
    TEST=Enable chromevox, show the partial launcher with the highlight on
    the search box. Expand the app list to fullscreen. Verify that the
    highlight stays with the search box.
    
    Change-Id: Iee6be1a1ec2173282a39e2af31e2df1204ab081b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2472877
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817154}

--- ash/app_list/views/assistant/assistant_page_view.cc
+++ ash/app_list/views/assistant/assistant_page_view.cc
@@ -355,8 +355,8 @@ void AssistantPageView::AnimateYPosition(AppListViewState target_view_state,
   if (needs_layout())
     Layout();
 
-  animator.Run(default_offset, layer(), this);
-  animator.Run(default_offset, view_shadow_->shadow()->shadow_layer(), nullptr);
+  animator.Run(default_offset, layer());
+  animator.Run(default_offset, view_shadow_->shadow()->shadow_layer());
 }
 
 void AssistantPageView::UpdatePageOpacityForState(AppListState state,
