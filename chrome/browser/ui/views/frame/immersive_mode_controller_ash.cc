0c78c6cdaf314760a0c42f17b6d83fd5a0362d62
Mike Wasserman <msw@chromium.org>
2020-10-14 18:01:15 +0000

    Window Placement: Fix cross-screen fullscreen exit on Mac V2
    Post the task to restore bounds after the exit transition completes.
    (as opposed to when the fullscreen exit transition starts)
    See crrev.com/c/1990137 for the original fix.
    
    Plumb *Start and *Complete signals through NativeWidgetMac.
    Plumb as *Changing and *Changed signals to BrowserView.
    
    Continue running other BrowserView logic in *Start/*Changing.
    Update ImmersiveModeControllerAsh to call the intended function.
    
    TODO: Refine naming and/or move all BrowserView logic to *Changed.
    
    Bug: 1134705
    Test: jsfiddle.net/cterefinko/rwo08as4 Enter->Swap->Exit WAI
    Change-Id: If81438181dd4454a975fcc9198d57136b9137044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2468345
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817098}

--- chrome/browser/ui/views/frame/immersive_mode_controller_ash.cc
+++ chrome/browser/ui/views/frame/immersive_mode_controller_ash.cc
@@ -235,7 +235,7 @@ void ImmersiveModeControllerAsh::OnWindowPropertyChanged(aura::Window* window,
                                                          intptr_t old) {
   // Track locked fullscreen changes.
   if (key == ash::kWindowPinTypeKey) {
-    browser_view_->FullscreenStateChanged();
+    browser_view_->FullscreenStateChanging();
     return;
   }
 
@@ -252,7 +252,7 @@ void ImmersiveModeControllerAsh::OnWindowPropertyChanged(aura::Window* window,
         old_state == ui::SHOW_STATE_FULLSCREEN) {
       // If the browser view initiated this state change,
       // BrowserView::ProcessFullscreen will no-op, so this call is harmless.
-      browser_view_->FullscreenStateChanged();
+      browser_view_->FullscreenStateChanging();
     }
   }
 }
