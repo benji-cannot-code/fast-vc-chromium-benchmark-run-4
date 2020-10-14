b296d1e415591a91625a98fc6bedae24d4a6f39d
ckitagawa <ckitagawa@chromium.org>
2020-10-14 17:52:46 +0000

    [Paint Preview] Omit Alpha channel
    Frames can't have transparent backgrounds so omit the alpha channel to
    simplify things.
    
    Change-Id: Ic9d1de433144123d5572a9557c03598fc82de635
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463601
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817092}

--- components/services/paint_preview_compositor/paint_preview_compositor_impl_unittest.cc
+++ components/services/paint_preview_compositor/paint_preview_compositor_impl_unittest.cc
@@ -600,7 +600,8 @@ TEST(PaintPreviewCompositorTest, TestComposite) {
   gfx::Rect rect = gfx::ScaleToEnclosingRect(
       gfx::Rect(root_frame_scroll_extent), scale_factor);
   SkBitmap bitmap;
-  bitmap.allocPixels(SkImageInfo::MakeN32Premul(rect.width(), rect.height()));
+  bitmap.allocPixels(
+      SkImageInfo::MakeN32(rect.width(), rect.height(), kOpaque_SkAlphaType));
   SkCanvas canvas(bitmap);
   canvas.scale(scale_factor, scale_factor);
   DrawDummyTestPicture(&canvas, SK_ColorDKGRAY, root_frame_scroll_extent);
@@ -671,7 +672,8 @@ TEST(PaintPreviewCompositorTest, TestCompositeWithMemoryBuffer) {
   gfx::Rect rect = gfx::ScaleToEnclosingRect(
       gfx::Rect(root_frame_scroll_extent), scale_factor);
   SkBitmap bitmap;
-  bitmap.allocPixels(SkImageInfo::MakeN32Premul(rect.width(), rect.height()));
+  bitmap.allocPixels(
+      SkImageInfo::MakeN32(rect.width(), rect.height(), kOpaque_SkAlphaType));
   SkCanvas canvas(bitmap);
   canvas.scale(scale_factor, scale_factor);
   DrawDummyTestPicture(&canvas, SK_ColorDKGRAY, root_frame_scroll_extent);
@@ -720,7 +722,8 @@ TEST(PaintPreviewCompositorTest, TestCompositeMainFrameNoDependencies) {
   gfx::Rect rect = gfx::ScaleToEnclosingRect(
       gfx::Rect(root_frame_scroll_extent), scale_factor);
   SkBitmap bitmap;
-  bitmap.allocPixels(SkImageInfo::MakeN32Premul(rect.width(), rect.height()));
+  bitmap.allocPixels(
+      SkImageInfo::MakeN32(rect.width(), rect.height(), kOpaque_SkAlphaType));
   SkCanvas canvas(bitmap);
   canvas.scale(scale_factor, scale_factor);
   DrawDummyTestPicture(&canvas, SK_ColorDKGRAY, root_frame_scroll_extent);
@@ -774,7 +777,8 @@ TEST(PaintPreviewCompositorTest, TestCompositeMainFrameOneDependency) {
   gfx::Rect rect = gfx::ScaleToEnclosingRect(
       gfx::Rect(root_frame_scroll_extent), scale_factor);
   SkBitmap bitmap;
-  bitmap.allocPixels(SkImageInfo::MakeN32Premul(rect.width(), rect.height()));
+  bitmap.allocPixels(
+      SkImageInfo::MakeN32(rect.width(), rect.height(), kOpaque_SkAlphaType));
   SkCanvas canvas(bitmap);
   canvas.scale(scale_factor, scale_factor);
   DrawDummyTestPicture(&canvas, SK_ColorDKGRAY, root_frame_scroll_extent);
@@ -832,7 +836,8 @@ TEST(PaintPreviewCompositorTest, TestCompositeMainFrameOneDependencyScrolled) {
   gfx::Rect rect = gfx::ScaleToEnclosingRect(
       gfx::Rect(root_frame_scroll_extent), scale_factor);
   SkBitmap bitmap;
-  bitmap.allocPixels(SkImageInfo::MakeN32Premul(rect.width(), rect.height()));
+  bitmap.allocPixels(
+      SkImageInfo::MakeN32(rect.width(), rect.height(), kOpaque_SkAlphaType));
   SkCanvas canvas(bitmap);
   canvas.scale(scale_factor, scale_factor);
   DrawDummyTestPicture(&canvas, SK_ColorDKGRAY, root_frame_scroll_extent);
@@ -893,7 +898,8 @@ TEST(PaintPreviewCompositorTest,
   gfx::Rect rect =
       gfx::ScaleToEnclosingRect(root_frame_clip_rect, scale_factor);
   SkBitmap bitmap;
-  bitmap.allocPixels(SkImageInfo::MakeN32Premul(rect.width(), rect.height()));
+  bitmap.allocPixels(
+      SkImageInfo::MakeN32(rect.width(), rect.height(), kOpaque_SkAlphaType));
   SkCanvas canvas(bitmap);
   canvas.scale(scale_factor, scale_factor);
   // Offset the canvas to simulate the root frame being scrolled.
