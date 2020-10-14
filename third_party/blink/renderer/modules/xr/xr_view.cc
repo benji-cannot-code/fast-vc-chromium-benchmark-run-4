af6602668d50ecf5390589a2b8eaae4cb1f83de7
Alan Cutter <alancutter@chromium.org>
2020-10-14 01:59:10 +0000

    Revert "WebXR: add recommendedViewportScale"
    This reverts commit 2dbccf3e8572f81233297f98224b275852f88bea.
    
    Reason for revert: Flaky crashes on bots.
    external/wpt/webxr/xrStationaryReferenceSpace_floorlevel_updates.https.html
    STDERR: #4 0x55ea51c21abc blink::XRSession::UpdatePresentationFrameState()
    
    Bug: 1138047
    
    Original change's description:
    > WebXR: add recommendedViewportScale
    >
    > This uses an estimated GPU utilization ratio from ArCoreGl. The goal is
    > to reduce the recommended viewport scale if the web app is unable to hit
    > a target framerate due to excessive GPU work, but not do so if it's slow
    > due to excessive JS computations or similar where reducing the viewport
    > wouldn't help. This is opt-in, the scale only takes effect if the app
    > calls view.requestViewportScale(view.recommendedViewportScale) followed
    > by xrwebgllayer.getViewport(view).
    >
    > (Dynamic viewport scaling is only available if WebXRIncubations is
    > enabled.)
    >
    > Spec link for context:
    > https://immersive-web.github.io/webxr/#dom-xrview-recommendedviewportscale
    >
    > Change-Id: I0a8598d8bd03f27fd1d2cc6c542ff79812825d74
    > Bug: 1133381
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2213113
    > Commit-Queue: Klaus Weidner <klausw@chromium.org>
    > Reviewed-by: Sam McNally <sammc@chromium.org>
    > Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#816813}
    
    TBR=sammc@chromium.org,klausw@chromium.org,bialpio@chromium.org,elalish@chromium.org
    
    Change-Id: Ic34275b3c147c9428d7d57c1a876a91b172bc586
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1133381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2469640
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816854}

--- third_party/blink/renderer/modules/xr/xr_view.cc
+++ third_party/blink/renderer/modules/xr/xr_view.cc
@@ -158,7 +158,7 @@ XRRigidTransform* XRView::transform() const {
 }
 
 base::Optional<double> XRView::recommendedViewportScale() const {
-  return view_data_->recommendedViewportScale();
+  return base::nullopt;
 }
 
 void XRView::requestViewportScale(base::Optional<double> scale) {
@@ -174,7 +174,7 @@ void XRView::Trace(Visitor* visitor) const {
 }
 
 base::Optional<double> XRViewData::recommendedViewportScale() const {
-  return recommended_viewport_scale_;
+  return base::nullopt;
 }
 
 void XRViewData::requestViewportScale(base::Optional<double> scale) {
