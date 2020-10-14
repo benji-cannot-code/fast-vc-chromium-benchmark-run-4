e21b8c609c81dda40efa0237fc2ae9b6371f8f8e
Fredrik Söderqvist <fs@opera.com>
2020-10-14 18:33:38 +0000

    Make SVG resources invalidation flags more granular
    The kPaintInvalidation flag performs both paint invalidation paint
    property invalidation and clip-path cache invalidation. Split out the
    two latter so that they can be used in a more targeted way and avoid
    unnecessarily "wide" invalidations. For example we don't need to
    invalidate neither the paint properties nor the clip-path cache when a
    paint server changes.
    
    Take this opportunity to change the invalidation flags for masks, clips
    and filters to not invalidate bounds and trigger layout since they
    should no longer have any effect on a clients bounds (and thus also
    don't need to trigger a layout).
    
    Add an additional kFilterCacheInvalidation flag for the invalidation of
    a filter, and make that invalidation conditional.
    
    Bug: 1028061, 1028063
    Change-Id: If96237ec2b5f7a08fd41fb51e1eb3f245b809b63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2467997
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#817120}

--- third_party/blink/renderer/core/svg/svg_filter_element.cc
+++ third_party/blink/renderer/core/svg/svg_filter_element.cc
@@ -121,8 +121,8 @@ void SVGFilterElement::PrimitiveAttributeChanged(
 
 void SVGFilterElement::InvalidateFilterChain() {
   if (LocalSVGResource* resource = AssociatedResource()) {
-    resource->NotifyContentChanged(SVGResourceClient::kLayoutInvalidation |
-                                   SVGResourceClient::kBoundariesInvalidation);
+    resource->NotifyContentChanged(SVGResourceClient::kPaintInvalidation |
+                                   SVGResourceClient::kFilterCacheInvalidation);
   }
 }
 
