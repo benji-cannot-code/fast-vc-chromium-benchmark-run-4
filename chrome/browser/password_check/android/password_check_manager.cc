9f9a361598739e4bcc8666326ed0a2d4ba5ead72
Norge Vizcay <vizcay@google.com>
2020-10-14 10:15:32 +0000

    [Passwords] Enable automatic password change for android app credentials
    * Makes use of PasswordForm::affiliated_web_realm to fetch the
    android URI affiliated domain.
    * Introduces a new variable url_domain that is used as entry point for
    automatic password change scripts.
    
    Bug: 1136886
    Change-Id: Ib394d9b6ad06e5d954f9322a98b7d72c81235a8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2452652
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Maxim Kolosovskiy  <kolos@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#816953}

--- chrome/browser/password_check/android/password_check_manager.cc
+++ chrome/browser/password_check/android/password_check_manager.cc
@@ -230,17 +230,6 @@ CompromisedCredentialForUI PasswordCheckManager::MakeUICredential(
   auto facet = password_manager::FacetURI::FromPotentiallyInvalidSpec(
       credential.signon_realm);
 
-  ui_credential.display_username = GetDisplayUsername(credential.username);
-  ui_credential.has_startable_script =
-      !credential.username.empty() && ShouldFetchPasswordScripts() &&
-      password_script_fetcher_->IsScriptAvailable(
-          url::Origin::Create(credential.url.GetOrigin()),
-          version_info::GetVersion());
-  ui_credential.has_auto_change_button =
-      ui_credential.has_startable_script &&
-      base::FeatureList::IsEnabled(
-          password_manager::features::kPasswordChangeInSettings);
-
   if (facet.IsValidAndroidFacetURI()) {
     const PasswordForm& android_form =
         insecure_credentials_manager_.GetSavedPasswordsFor(credential)[0];
@@ -257,6 +246,12 @@ CompromisedCredentialForUI PasswordCheckManager::MakeUICredential(
       ui_credential.display_origin =
           base::UTF8ToUTF16(android_form.app_display_name);
     }
+    // In case no affiliated_web_realm could be obtained we should not have an
+    // associated url for android credential.
+    ui_credential.url = android_form.affiliated_web_realm.empty()
+                            ? GURL::EmptyGURL()
+                            : GURL(android_form.affiliated_web_realm);
+
   } else {
     ui_credential.display_origin = url_formatter::FormatUrl(
         credential.url.GetOrigin(),
@@ -268,6 +263,17 @@ CompromisedCredentialForUI PasswordCheckManager::MakeUICredential(
     ui_credential.change_password_url =
         password_manager::CreateChangePasswordUrl(ui_credential.url).spec();
   }
+
+  ui_credential.display_username = GetDisplayUsername(credential.username);
+  ui_credential.has_startable_script =
+      !credential.username.empty() && ShouldFetchPasswordScripts() &&
+      password_script_fetcher_->IsScriptAvailable(
+          url::Origin::Create(ui_credential.url.GetOrigin()),
+          version_info::GetVersion());
+  ui_credential.has_auto_change_button =
+      ui_credential.has_startable_script &&
+      base::FeatureList::IsEnabled(
+          password_manager::features::kPasswordChangeInSettings);
 
   return ui_credential;
 }
