402ae2a428f23d7218ebfdfed83d608ca495620f
Ken Rockot <rockot@google.com>
2020-10-14 18:46:14 +0000

    Enable OOP Storage Service by default
    In preparation for the experiment going to 100% on stable, this enables
    OOP Storage Service by default on ToT.
    
    Bug: 994911
    Change-Id: I235ff43a24d0bf3449816936fe7545742c386a01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463965
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817125}

--- content/public/test/unittest_test_suite.cc
+++ content/public/test/unittest_test_suite.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_suite.h"
 #include "build/build_config.h"
 #include "content/browser/network_service_instance_impl.h"
+#include "content/browser/storage_partition_impl.h"
 #include "content/public/browser/network_service_instance.h"
 #include "content/public/test/test_host_resolver.h"
 #include "content/test/test_blink_web_unit_test_support.h"
@@ -83,6 +84,7 @@ UnitTestTestSuite::UnitTestTestSuite(base::TestSuite* test_suite)
       command_line->GetSwitchValueASCII(switches::kDisableFeatures);
 
   ForceCreateNetworkServiceDirectlyForTesting();
+  StoragePartitionImpl::ForceInProcessStorageServiceForTesting();
 
   testing::TestEventListeners& listeners =
       testing::UnitTest::GetInstance()->listeners();
