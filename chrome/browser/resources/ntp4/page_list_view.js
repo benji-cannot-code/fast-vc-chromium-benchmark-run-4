f295ad9bb036c3b1cec1ff1f3e7a6f909dfdbef3
Carlos Frias <carlos.frias@microsoft.com>
2020-10-14 17:23:11 +0000

    DPWA: Add Run on OS Login UX as context menu option in chrome://apps
    This CL adds a new context menu option to enable/disable Run on OS Login
    for a web app listed in chrome://apps list. The context menu option will
    be shown only when the web app is locally installed and the feature flag
    for Run on OS Login is enabled.
    
    Handler for UX activation is to call OsIntegrationManager class and
    install or uninstall the Run on OS Login hook as indicated by the user.
    
    Testing was performed by manual testing in chrome://apps by right click
    on app icon, and verifying the new UI option is only shown for installed
    web apps, and also verifying the activation handler calls the
    OsIntegrationManger's APIs.
    
    The manual testing guidance document describing these tests can be found
    here:
    https://docs.google.com/document/d/1XlvL17xAGivmQNiHaUCYBcGtnkPpZ0GijjjWpCma5H4/edit
    
    Bug: 1110407
    Change-Id: Ifef1cb57068b0836248f3a780ccff6e051abaf03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401902
    Commit-Queue: Carlos Frias <carlos.frias@microsoft.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817073}

--- chrome/browser/resources/ntp4/page_list_view.js
+++ chrome/browser/resources/ntp4/page_list_view.js
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *            launch_container: number,
  *            launch_type: number,
  *            mayChangeLaunchType: boolean,
+ *            mayChangeRunOnOsLoginMode: boolean,
  *            mayCreateShortcuts: boolean,
  *            mayDisable: boolean,
  *            name: string,
@@ -41,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *            optionsUrl: string,
  *            packagedApp: boolean,
  *            page_index: number,
+ *            runOnOsLoginMode: string,
  *            title: string,
  *            url: string,
  *            version: string}}
