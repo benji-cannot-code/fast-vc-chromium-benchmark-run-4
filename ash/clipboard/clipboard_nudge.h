cd117eb8f7bf0e86667ecab2e342d76868386b4f
Matthew Mourgos <mmourgos@chromium.org>
2020-10-14 22:37:16 +0000

    Multipaste: Adjust the clipboard nudge bounds when hotseat is shown
    This change has the clipboard nudge observe the hotseat state and will
    animate the bounds of the clipboard nudge so that the nudge is always
    placed above the hotseat.
    
    Bug: 1137019
    Change-Id: If00afa1585e0382d7aa41789eca2a981b1323f1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2469364
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817235}

--- ash/clipboard/clipboard_nudge.h
+++ ash/clipboard/clipboard_nudge.h
@@ -7,18 +7,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_CLIPBOARD_CLIPBOARD_NUDGE_H_
 
 #include "ash/ash_export.h"
+#include "ash/shelf/shelf.h"
+#include "ash/shelf/shelf_observer.h"
+#include "base/scoped_observer.h"
 #include "ui/views/widget/widget.h"
 
 namespace ash {
 
 // Implements a contextual nudge for multipaste.
-class ASH_EXPORT ClipboardNudge {
+class ASH_EXPORT ClipboardNudge : public ShelfObserver {
  public:
   ClipboardNudge();
   ClipboardNudge(const ClipboardNudge&) = delete;
   ClipboardNudge& operator=(const ClipboardNudge&) = delete;
-  ~ClipboardNudge();
+  ~ClipboardNudge() override;
 
+  // ShelfObserver overrides:
+  void OnHotseatStateChanged(HotseatState old_state,
+                             HotseatState new_state) override;
   void Close();
 
  private:
@@ -31,6 +37,10 @@ class ASH_EXPORT ClipboardNudge {
   std::unique_ptr<views::Widget> widget_;
 
   ClipboardNudgeView* nudge_view_ = nullptr;  // not_owned
+
+  aura::Window* const root_window_;
+
+  ScopedObserver<Shelf, ShelfObserver> shelf_observer_{this};
 };
 
 }  // namespace ash
