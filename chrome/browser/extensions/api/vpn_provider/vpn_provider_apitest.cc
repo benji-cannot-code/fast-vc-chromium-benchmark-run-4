72b6707ee64a115890a95342401348a67266f756
Steven Bennetts <stevenjb@chromium.org>
2020-10-15 23:57:24 +0000

    FakeShillServiceClient: Elim remaining use of DictionaryValue
    Bug: 1137487
    Change-Id: I6164788dc2f84fa670027c41d48f8997c5dbc053
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2466823
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817750}

--- chrome/browser/extensions/api/vpn_provider/vpn_provider_apitest.cc
+++ chrome/browser/extensions/api/vpn_provider/vpn_provider_apitest.cc
@@ -191,6 +191,14 @@ class VpnProviderApiTest : public extensions::ExtensionApiTest {
         base::DoNothing(), base::Bind(DoNothingFailureCallback));
   }
 
+  bool HasService(const std::string& service_path) const {
+    std::string profile_path;
+    base::Value properties =
+        ShillProfileClient::Get()->GetTestInterface()->GetService(
+            service_path, &profile_path);
+    return properties.is_dict();
+  }
+
  protected:
   TestShillThirdPartyVpnDriverClient* test_client_ = nullptr;  // Unowned
   VpnService* service_ = nullptr;
@@ -215,11 +223,7 @@ IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, CreateConfig) {
   AddNetworkProfileForUser();
   EXPECT_TRUE(RunExtensionTest("createConfigSuccess"));
   EXPECT_TRUE(DoesConfigExist(kTestConfig));
-  const std::string service_path = GetSingleServicePath();
-  std::string profile_path;
-  base::DictionaryValue properties;
-  EXPECT_TRUE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_TRUE(HasService(GetSingleServicePath()));
 }
 
 IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, DestroyConfig) {
@@ -228,15 +232,11 @@ IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, DestroyConfig) {
   EXPECT_TRUE(CreateConfigForTest(kTestConfig));
   EXPECT_TRUE(DoesConfigExist(kTestConfig));
   const std::string service_path = GetSingleServicePath();
-  std::string profile_path;
-  base::DictionaryValue properties;
-  EXPECT_TRUE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_TRUE(HasService(service_path));
 
   EXPECT_TRUE(RunExtensionTest("destroyConfigSuccess"));
   EXPECT_FALSE(DoesConfigExist(kTestConfig));
-  EXPECT_FALSE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_FALSE(HasService(service_path));
 }
 
 IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, DestroyConnectedConfig) {
@@ -246,10 +246,7 @@ IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, DestroyConnectedConfig) {
   EXPECT_TRUE(CreateConfigForTest(kTestConfig));
   EXPECT_TRUE(DoesConfigExist(kTestConfig));
   const std::string service_path = GetSingleServicePath();
-  std::string profile_path;
-  base::DictionaryValue properties;
-  EXPECT_TRUE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_TRUE(HasService(service_path));
   EXPECT_FALSE(IsConfigConnected());
 
   const std::string object_path = shill::kObjectPathBase + GetKey(kTestConfig);
@@ -263,8 +260,7 @@ IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, DestroyConnectedConfig) {
 
   EXPECT_TRUE(DestroyConfigForTest(kTestConfig));
   EXPECT_FALSE(DoesConfigExist(kTestConfig));
-  EXPECT_FALSE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_FALSE(HasService(service_path));
 
   ASSERT_TRUE(catcher.GetNextResult());
 }
@@ -274,11 +270,7 @@ IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, VpnSuccess) {
   AddNetworkProfileForUser();
   EXPECT_TRUE(RunExtensionTest("createConfigConnectAndDisconnect"));
   EXPECT_TRUE(DoesConfigExist(kTestConfig));
-  const std::string service_path = GetSingleServicePath();
-  std::string profile_path;
-  base::DictionaryValue properties;
-  EXPECT_TRUE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_TRUE(HasService(GetSingleServicePath()));
   EXPECT_FALSE(IsConfigConnected());
 
   const std::string object_path = shill::kObjectPathBase + GetKey(kTestConfig);
@@ -372,16 +364,12 @@ IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, CreateUninstall) {
   EXPECT_TRUE(DoesConfigExist(kTestConfig));
 
   const std::string service_path = GetSingleServicePath();
-  std::string profile_path;
-  base::DictionaryValue properties;
-  EXPECT_TRUE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_TRUE(HasService(service_path));
 
   UninstallExtension(extension_id_);
   content::RunAllPendingInMessageLoop();
   EXPECT_FALSE(DoesConfigExist(kTestConfig));
-  EXPECT_FALSE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_FALSE(HasService(service_path));
 }
 
 IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, CreateDisable) {
@@ -391,10 +379,7 @@ IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, CreateDisable) {
   EXPECT_TRUE(DoesConfigExist(kTestConfig));
 
   const std::string service_path = GetSingleServicePath();
-  std::string profile_path;
-  base::DictionaryValue properties;
-  EXPECT_TRUE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_TRUE(HasService(service_path));
 
   extensions::ExtensionService* extension_service =
       extensions::ExtensionSystem::Get(profile())->extension_service();
@@ -402,8 +387,7 @@ IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, CreateDisable) {
       extension_id_, extensions::disable_reason::DISABLE_USER_ACTION);
   content::RunAllPendingInMessageLoop();
   EXPECT_FALSE(DoesConfigExist(kTestConfig));
-  EXPECT_FALSE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_FALSE(HasService(service_path));
 }
 
 IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, CreateBlocklist) {
@@ -413,18 +397,14 @@ IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, CreateBlocklist) {
   EXPECT_TRUE(DoesConfigExist(kTestConfig));
 
   const std::string service_path = GetSingleServicePath();
-  std::string profile_path;
-  base::DictionaryValue properties;
-  EXPECT_TRUE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_TRUE(HasService(service_path));
 
   extensions::ExtensionService* extension_service =
       extensions::ExtensionSystem::Get(profile())->extension_service();
   extension_service->BlocklistExtensionForTest(extension_id_);
   content::RunAllPendingInMessageLoop();
   EXPECT_FALSE(DoesConfigExist(kTestConfig));
-  EXPECT_FALSE(ShillProfileClient::Get()->GetTestInterface()->GetService(
-      service_path, &profile_path, &properties));
+  EXPECT_FALSE(HasService(service_path));
 }
 
 }  // namespace chromeos
