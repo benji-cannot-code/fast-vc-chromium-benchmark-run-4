6867d9fe9b43c8cc0295e3a0c9b284083e384f48
Sophey Dong <sophey@chromium.org>
2020-10-15 17:42:03 +0000

    [SharingHub] Call cancel callback when the sharesheet is destroyed.
    Bug: 1138750
    Change-Id: I0cf69bfad56ed24704d0e2e02050537c2fbea73e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2473804
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817550}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ShareSheetCoordinator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ShareSheetCoordinator.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.share.share_sheet;
 
 import android.app.Activity;
 import android.content.res.Configuration;
-import android.graphics.Bitmap;
 import android.view.View;
 
 import androidx.annotation.VisibleForTesting;
@@ -59,12 +58,11 @@ public class ShareSheetCoordinator implements ActivityStateObserver, ChromeOptio
     private Activity mActivity;
     private ActivityLifecycleDispatcher mLifecycleDispatcher;
     private ChromeProvidedSharingOptionsProvider mChromeProvidedSharingOptionsProvider;
+    private ShareParams mShareParams;
     private ShareSheetBottomSheetContent mBottomSheet;
     private WindowAndroid mWindowAndroid;
     private final BottomSheetObserver mBottomSheetObserver;
     private final LargeIconBridge mIconBridge;
-    private String mUrl;
-    private Bitmap mIconForPreview;
 
     /**
      * Constructs a new ShareSheetCoordinator.
@@ -106,6 +104,12 @@ public class ShareSheetCoordinator implements ActivityStateObserver, ChromeOptio
     }
 
     protected void destroy() {
+        if (mShareParams != null) {
+            ShareParams.TargetChosenCallback callback = mShareParams.getCallback();
+            if (callback != null) {
+                callback.onCancel();
+            }
+        }
         if (mWindowAndroid != null) {
             mWindowAndroid.removeActivityStateObserver(this);
             mWindowAndroid = null;
@@ -119,6 +123,7 @@ public class ShareSheetCoordinator implements ActivityStateObserver, ChromeOptio
     // TODO(crbug/1022172): Should be package-protected once modularization is complete.
     public void showShareSheet(
             ShareParams params, ChromeShareExtras chromeShareExtras, long shareStartTime) {
+        mShareParams = params;
         mActivity = params.getWindow().getActivity().get();
         if (mActivity == null) return;
 
