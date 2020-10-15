985e6988af1f72cfe102eea2f61785fbb3b04602
Evan Stade <estade@chromium.org>
2020-10-15 02:33:29 +0000

    WebLayer MediaRouter: add services and notification IDs.
    The services will be foregrounded and the notifications will be visible
    when a cast session is ongoing. The service/notification ID design
    matches that of MediaSession. The notification IDs are defined in client
    xml, so //components/media_router code needs to ask the embedder for
    these values.
    
    Casting is still not functional in WebLayer even after this change as
    a CastOptionsProvider is required.
    
    Bug: 1057100
    Change-Id: I4d6fa44d1073a0a26c618c501e58251f118379c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2467197
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817314}

--- weblayer/browser/content_browser_client_impl.cc
+++ weblayer/browser/content_browser_client_impl.cc
@@ -639,7 +639,7 @@ content::ControllerPresentationServiceDelegate*
 ContentBrowserClientImpl::GetControllerPresentationServiceDelegate(
     content::WebContents* web_contents) {
 #if defined(OS_ANDROID)
-  if (WebLayerFactoryImplAndroid::GetClientMajorVersion() < 87)
+  if (WebLayerFactoryImplAndroid::GetClientMajorVersion() < 88)
     return nullptr;
 
   if (base::FeatureList::IsEnabled(features::kMediaRouter)) {
---
+++ weblayer/browser/java/org/chromium/weblayer_private/interfaces/RemoteMediaServiceConstants.java
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer_private.interfaces;
+
+/** Keys for remote media service intent extras. */
+public interface RemoteMediaServiceConstants {
+    String NOTIFICATION_ID_KEY = "remote_media_service_notification_id_key";
+}
