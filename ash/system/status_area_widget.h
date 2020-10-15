1be91333a989584b7221499716e11cb72712009a
Jazz Xu <jazzhsu@chromium.org>
2020-10-15 22:16:34 +0000

    CrOS GMC: Anchor GMC dialog to media tray.
    This CL changes the anchor of GMC dialog from bottom right to media tray
    button. We are anchoring it to rect rather than the media tray view
    since the dialog won't close if the media tray became hidden (e.g. when
    user clicks the unpin button in the dialog).
    
    Bug: 1137542
    Change-Id: Ic71f4f1b46bb2e0a6e6b66c5806127631fa47a4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2464787
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Jazz Xu <jazzhsu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817697}

--- ash/system/status_area_widget.h
+++ ash/system/status_area_widget.h
@@ -104,6 +104,9 @@ class ASH_EXPORT StatusAreaWidget : public SessionObserver,
   // |overview_button_tray_|.
   TrayBackgroundView* GetSystemTrayAnchor() const;
 
+  // Called by media tray to calculate anchor rect.
+  gfx::Rect GetMediaTrayAnchorRect() const;
+
   StatusAreaWidgetDelegate* status_area_widget_delegate() {
     return status_area_widget_delegate_;
   }
@@ -168,6 +171,8 @@ class ASH_EXPORT StatusAreaWidget : public SessionObserver,
   }
 
  private:
+  friend class MediaTrayTest;
+
   struct LayoutInputs {
     gfx::Rect bounds;
     CollapseState collapse_state = CollapseState::NOT_COLLAPSIBLE;
