21566181337144e8ac1e6e70f077e78156a20bdf
Peter Kasting <pkasting@chromium.org>
2020-10-15 21:30:31 +0000

    Change ButtonPressed overrides to callbacks: .../try_chrome_dialog_win/
    Bug: 772945
    Change-Id: I3b5131b40eabe286131692bc9891ee1d94afbd1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2476604
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Travis Skare <skare@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Travis Skare <skare@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817673}

--- chrome/browser/ui/views/try_chrome_dialog_win/try_chrome_dialog.h
+++ chrome/browser/ui/views/try_chrome_dialog_win/try_chrome_dialog.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "chrome/installer/util/experiment_metrics.h"
 #include "ui/events/event_handler.h"
-#include "ui/views/controls/button/button.h"
 #include "ui/views/widget/widget_observer.h"
 
 namespace gfx {
@@ -41,9 +40,7 @@ class Widget;
 //   +-----------------------------------------------+
 //
 // Some variants do not have body text, or only have one button.
-class TryChromeDialog : public views::ButtonListener,
-                        public views::WidgetObserver,
-                        public ui::EventHandler {
+class TryChromeDialog : public views::WidgetObserver, public ui::EventHandler {
  public:
   // Receives a closure to run upon process singleton notification when the
   // modal dialog is open, or a null closure when the active dialog is
@@ -125,10 +122,8 @@ class TryChromeDialog : public views::ButtonListener,
   void GainedMouseHover();
   void LostMouseHover();
 
-  // views::ButtonListener:
-  // Updates the result_ and state_ based on which button was pressed and
-  // closes the dialog.
-  void ButtonPressed(views::Button* sender, const ui::Event& event) override;
+  // Updates the result_ and state_ based on |state| and closes the dialog.
+  void ButtonPressed(installer::ExperimentMetrics::State state);
 
   // views::WidgetObserver:
   void OnWidgetClosing(views::Widget* widget) override;
