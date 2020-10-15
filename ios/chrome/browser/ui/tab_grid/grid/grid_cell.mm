9e9aad088d532fd397a401231a65c3a752508032
Roberto Moura <mouraroberto@google.com>
2020-10-15 15:24:48 +0000

    Add a New Tab button (plus sign cell) to the end of the Thumb Strip.
    The plus sign cell is a cell added to the end of the collection view of
    the Tab Grid / Thumb Strip that opens a new tab when tapped. It has the
    same shape of the thumbnail cells but has a plus sign in its center. A
    new class was created for that: PlusSignCell.
    
    The plus sign cell is always the last element of the collection view.
    
    Screenshot: http://screen/49Y5RU8dPwDVXkW.png
    
    Bug: 1127604, 1137983, 1137986
    Change-Id: Ib1d9df1c22e6d3c2ca1340317dc7a39104275663
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2431668
    Commit-Queue: Roberto Moura <mouraroberto@google.com>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Roberto Moura <mouraroberto@google.com>
    Cr-Commit-Position: refs/heads/master@{#817498}

--- ios/chrome/browser/ui/tab_grid/grid/grid_cell.mm
+++ ios/chrome/browser/ui/tab_grid/grid/grid_cell.mm
@@ -153,7 +153,7 @@ void PositionView(UIView* view, CGPoint point) {
   self.selected = NO;
 }
 
-#pragma mark - Accessibility
+#pragma mark - UIAccessibility
 
 - (BOOL)isAccessibilityElement {
   // This makes the whole cell tappable in VoiceOver rather than the individual
@@ -180,7 +180,7 @@ void PositionView(UIView* view, CGPoint point) {
 
   // This background color must be set to avoid the corners behind the rounded
   // layer from showing when dragging and dropping.
-  self.backgroundColor = [UIColor colorNamed:kGridBackgroundColor];
+  self.backgroundColor = [UIColor clearColor];
 
   self.iconView.backgroundColor = UIColor.clearColor;
   switch (theme) {
