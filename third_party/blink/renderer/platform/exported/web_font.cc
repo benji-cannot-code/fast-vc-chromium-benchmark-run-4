e6a208d2d4fde1da253337a994637a729b407bdb
Dominik Röttsches <drott@chromium.org>
2020-10-15 17:41:49 +0000

    Remove WebFloatRect from WebFont
    Change-Id: I541eca7671cc2904e71b3ab959058cc5881c52dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2476153
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817549}

--- third_party/blink/renderer/platform/exported/web_font.cc
+++ third_party/blink/renderer/platform/exported/web_font.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/platform/web_font.h"
 
-#include "third_party/blink/public/platform/web_float_rect.h"
 #include "third_party/blink/public/platform/web_font_description.h"
 #include "third_party/blink/public/platform/web_rect.h"
 #include "third_party/blink/public/platform/web_text_run.h"
@@ -111,11 +110,11 @@ int WebFont::OffsetForPosition(const WebTextRun& run, float position) const {
       run, position, IncludePartialGlyphs, DontBreakGlyphs);
 }
 
-WebFloatRect WebFont::SelectionRectForText(const WebTextRun& run,
-                                           const gfx::PointF& left_baseline,
-                                           int height,
-                                           int from,
-                                           int to) const {
+gfx::RectF WebFont::SelectionRectForText(const WebTextRun& run,
+                                         const gfx::PointF& left_baseline,
+                                         int height,
+                                         int from,
+                                         int to) const {
   return private_->GetFont().SelectionRectForText(
       run, FloatPoint(left_baseline), height, from, to);
 }
