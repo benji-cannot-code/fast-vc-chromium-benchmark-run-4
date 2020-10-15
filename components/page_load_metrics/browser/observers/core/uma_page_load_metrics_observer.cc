b154599a8f5d3193fc2bed4728479b5e68a66d5b
Jesse McKenna <jessemckenna@google.com>
2020-10-15 17:51:44 +0000

    Remove suffixes OnBattery, NotOnBattery from FCP
    This change removes histogram suffixes
    PageLoad.PaintTiming.NavigationToFirstContentfulPaint.OnBattery and
    PageLoad.PaintTiming.NavigationToFirstContentfulPaint.NotOnBattery,
    which allowed the filtering of FirstContentfulPaint measurements by
    whether the system was on battery or wall power during page load.
    
    This was used to filter FirstContentfulPaint regressions for
    battery-specific Finch experiment SlowDCTimerInterruptsWin, which is
    now on Stable. Because the experiment is finished and I'm not aware
    of any other users, this histogram can be removed.
    
    Change-Id: I78a6a2026596cb9aaa0783129ee3f4d8034fc039
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2468337
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/master@{#817559}

--- components/page_load_metrics/browser/observers/core/uma_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/core/uma_page_load_metrics_observer.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/power_monitor/power_monitor.h"
 #include "components/page_load_metrics/browser/observers/core/largest_contentful_paint_handler.h"
 #include "components/page_load_metrics/browser/page_load_metrics_util.h"
 #include "content/public/common/process_type.h"
@@ -187,11 +186,6 @@ const char kHistogramParseBlockedOnScriptExecutionDocumentWrite[] =
 
 const char kHistogramFirstContentfulPaintNoStore[] =
     "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.NoStore";
-
-const char kHistogramFirstContentfulPaintOnBattery[] =
-    "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.OnBattery";
-const char kHistogramFirstContentfulPaintNotOnBattery[] =
-    "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.NotOnBattery";
 
 const char kHistogramFirstContentfulPaintHiddenWhileFlushing[] =
     "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.HiddenWhileFlushing";
@@ -520,14 +514,6 @@ void UmaPageLoadMetricsObserver::OnFirstContentfulPaintInPage(
 
     if (was_no_store_main_resource_) {
       PAGE_LOAD_HISTOGRAM(internal::kHistogramFirstContentfulPaintNoStore,
-                          timing.paint_timing->first_contentful_paint.value());
-    }
-
-    if (base::PowerMonitor::IsOnBatteryPower()) {
-      PAGE_LOAD_HISTOGRAM(internal::kHistogramFirstContentfulPaintOnBattery,
-                          timing.paint_timing->first_contentful_paint.value());
-    } else {
-      PAGE_LOAD_HISTOGRAM(internal::kHistogramFirstContentfulPaintNotOnBattery,
                           timing.paint_timing->first_contentful_paint.value());
     }
 
