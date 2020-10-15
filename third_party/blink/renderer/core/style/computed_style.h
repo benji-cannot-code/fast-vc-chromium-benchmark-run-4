1984a43b1ccf01d1e078d0a8ebc7808405145860
Rune Lillesveen <futhark@chromium.org>
2020-10-15 12:40:24 +0000

    Use cached ::selection pseudo styles.
    There should be no reason to recompute the ::selection style if it is
    already cached on the originating element's ComputedStyle. Also, we had
    another method which did the cache lookup, but that method did not
    handle input selection like the other method did. This coincidentally
    fixes an issue with missing text-shadow in text input selection.
    
    TEST=virtual/forced-high-contrast-colors/fast/css/forced-colors-mode/forced-colors-mode-15.html
    
    Bug: 1055101
    Change-Id: I40eac078e61f626d0ff25913530a2bbe4ea975b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2465750
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817463}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -396,7 +396,7 @@ class ComputedStyle : public ComputedStyleBase,
   }
   void SetStyleType(PseudoId style_type) { SetStyleTypeInternal(style_type); }
 
-  const ComputedStyle* GetCachedPseudoElementStyle(PseudoId) const;
+  CORE_EXPORT const ComputedStyle* GetCachedPseudoElementStyle(PseudoId) const;
   const ComputedStyle* AddCachedPseudoElementStyle(
       scoped_refptr<const ComputedStyle>) const;
   void ClearCachedPseudoElementStyles() const {
