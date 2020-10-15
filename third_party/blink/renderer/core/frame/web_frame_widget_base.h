3061e9015cbd73ff6b859e454fba16890438bf77
Dave Tapuska <dtapuska@chromium.org>
2020-10-15 20:07:10 +0000

    Move IME requests via WebPlugin instead of WebWidgetClient.
    With the removal of Fullscreen Pepper we no longer need to traverse the
    frame tree to decide if something has focus or not. Now focus will
    match what blink believes it is.
    
    Since the plugin will be focused so we can look for the focused
    WebPlugin and if it exists ask via that API. This brings the flow
    inline with other editing command (selection, editing).
    
    This change has minor changes on the SimulateIMEEvent code flow in that
    simulated IME events are now dispatched to the pepper instance they
    were invoked on and not the focused one that they previously would
    have. The tests always focus the instance so this shouldn't
    be an issue.
    
    Verified in Pepper via:
    out/Release/chrome --register-pepper-plugins="out/Release/libppapi_example_ime.so;application/x-ppapi-example-ime"
    
    BUG=1097816
    
    Change-Id: I9a6b4514b80432938783043d392a96448e530216
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463553
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817638}

--- third_party/blink/renderer/core/frame/web_frame_widget_base.h
+++ third_party/blink/renderer/core/frame/web_frame_widget_base.h
@@ -53,6 +53,7 @@ class PageWidgetEventHandler;
 class PaintWorkletPaintDispatcher;
 class RemoteFrame;
 class WebLocalFrameImpl;
+class WebPlugin;
 class WebViewImpl;
 class WidgetBase;
 class ScreenMetricsEmulator;
@@ -242,6 +243,18 @@ class CORE_EXPORT WebFrameWidgetBase
       bool request_unadjusted_movement,
       mojom::blink::WidgetInputHandlerHost::RequestMouseLockCallback callback)
       override;
+  bool CanComposeInline() override;
+  bool ShouldDispatchImeEventsToPlugin() override;
+  void ImeSetCompositionForPlugin(const String& text,
+                                  const Vector<ui::ImeTextSpan>& ime_text_spans,
+                                  const gfx::Range& replacement_range,
+                                  int selection_start,
+                                  int selection_end) override;
+  void ImeCommitTextForPlugin(const String& text,
+                              const Vector<ui::ImeTextSpan>& ime_text_spans,
+                              const gfx::Range& replacement_range,
+                              int relative_cursor_pos) override;
+  void ImeFinishComposingTextForPlugin(bool keep_selection) override;
 
   // WebFrameWidget implementation.
   WebLocalFrame* LocalRoot() const override;
@@ -574,6 +587,9 @@ class CORE_EXPORT WebFrameWidgetBase
   // clipped or obscured by ancestor frames in another process.
   virtual void SetRemoteViewportIntersection(
       const mojom::blink::ViewportIntersectionState& intersection_state) {}
+
+  // Return the focused WebPlugin if there is one.
+  WebPlugin* GetFocusedPluginContainer();
 
  protected:
   enum DragAction { kDragEnter, kDragOver };
