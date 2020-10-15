1419d78ced9c147891baf0bd2d8765883a808db0
Zentaro Kavanagh <zentaro@chromium.org>
2020-10-15 22:26:35 +0000

    Scanning: import strings.m.js not strings.js
    - New code should import the .m.js version which does not try to
      initialize loadTimeData during import which causes an order
      dependency on imports.
    - Also ran git cl format --js to format/sort the imports
    
    Bug: None
    Test: browser_tests --gtest_filter=ScanningApp*
    Change-Id: Ie365b9563bca357b9467f556a5c12d28788b9716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2477515
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Auto-Submit: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817702}

--- chromeos/components/scanning/resources/color_mode_select.js
+++ chromeos/components/scanning/resources/color_mode_select.js
@@ -5,11 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './scanning.mojom-lite.js';
 import './scan_settings_section.js';
+import './strings.m.js';
+
+import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getColorModeString} from './scanning_app_util.js';
-import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
-import './strings.js';
 
 /** @type {number} */
 const NUM_REQUIRED_COLOR_MODES = 2;
--- chromeos/components/scanning/resources/file_type_select.js
+++ chromeos/components/scanning/resources/file_type_select.js
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './scanning.mojom-lite.js';
 import './scan_settings_section.js';
+import './strings.m.js';
 
-import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
-import './strings.js';
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 /**
  * @fileoverview
