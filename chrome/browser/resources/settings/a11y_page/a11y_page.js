9c37cda1ed407cfa1e556075cbdfffbba2e266cc
Alice Boxhall <aboxhall@chromium.org>
2020-10-15 02:37:52 +0000

    Add a histogram tracking toggling the focus highlight preference
    Bug: 1075862
    Change-Id: I5da5ee8984896e5c39baef36c06f46eb451524bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2327363
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817322}

--- chrome/browser/resources/settings/a11y_page/a11y_page.js
+++ chrome/browser/resources/settings/a11y_page/a11y_page.js
@@ -199,6 +199,15 @@ Polymer({
   onEnableLiveCaptionSubtitleChanged_(enableLiveCaptionSubtitle) {
     this.enableLiveCaptionSubtitle_ = enableLiveCaptionSubtitle;
   },
+
+  /**
+   * @private
+   * @param {!Event} event
+   */
+  onFocusHighlightChange_(event) {
+    chrome.metricsPrivate.recordBoolean(
+        'Accessibility.FocusHighlight.ToggleEnabled', event.target.checked);
+  },
   // </if>
 
   // <if expr="chromeos">
