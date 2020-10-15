d6339baf0b4c137aef473cb881bfba36bc860d8b
bttk <bttk@chromium.org>
2020-10-15 16:35:58 +0000

    [ToolbarMVC] Remove unnecessary LocationBar methods
    These methods are only called on LocationBarCoordinator and have no
    effect on CustomTabToolbar.
    
    Change-Id: I4af4c32d8aadfdade5d5c4c04faa31c90abe63dd
    Bug: 1133482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2464085
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: who/bttk <bttk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817520}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarTablet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarTablet.java
@@ -142,11 +142,6 @@ class LocationBarTablet extends LocationBarLayout {
         return selectedTarget.onTouchEvent(event);
     }
 
-    // Returns amount by which to adjust to move value inside the given range.
-    private static float distanceToRange(float min, float max, float value) {
-        return value < min ? (min - value) : value > max ? (max - value) : 0;
-    }
-
     @Override
     public void handleUrlFocusAnimation(final boolean hasFocus) {
         super.handleUrlFocusAnimation(hasFocus);
@@ -182,15 +177,6 @@ class LocationBarTablet extends LocationBarLayout {
         });
         setUrlFocusChangeInProgress(true);
         mUrlFocusChangeAnimator.start();
-    }
-
-    /**
-     * @param shouldShowButtons Whether buttons should be displayed in the URL bar when it's not
-     *                          focused.
-     */
-    public void setShouldShowButtonsWhenUnfocused(boolean shouldShowButtons) {
-        mShouldShowButtonsWhenUnfocused = shouldShowButtons;
-        updateButtonVisibility();
     }
 
     /**
@@ -261,6 +247,15 @@ class LocationBarTablet extends LocationBarLayout {
         if (mAnimatingWidthChange) {
             setWidthChangeAnimationFraction(mWidthChangeFraction);
         }
+    }
+
+    /**
+     * @param shouldShowButtons Whether buttons should be displayed in the URL bar when it's not
+     *                          focused.
+     */
+    public void setShouldShowButtonsWhenUnfocused(boolean shouldShowButtons) {
+        mShouldShowButtonsWhenUnfocused = shouldShowButtons;
+        updateButtonVisibility();
     }
 
     /**
@@ -402,6 +397,11 @@ class LocationBarTablet extends LocationBarLayout {
         }
 
         return animators;
+    }
+
+    /** Returns amount by which to adjust to move value inside the given range. */
+    private static float distanceToRange(float min, float max, float value) {
+        return value < min ? (min - value) : value > max ? (max - value) : 0;
     }
 
     /**
