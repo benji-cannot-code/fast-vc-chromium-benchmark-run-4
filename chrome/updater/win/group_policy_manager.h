8b5660db3485baf5af49f0f3acf1bfcb1566cc7b
Greg Thompson <grt@chromium.org>
2020-10-15 15:09:54 +0000

    IWYU fixes in /chrome/updater.
    base/win/registry.h includes more than it uses. These files were
    inadvertently relying on that fact. This CL adds missing includes so
    that registry.h can be cleaned up.
    
    BUG=None
    This CL was uploaded by git cl split.
    
    R=ganesh@chromium.org
    
    Change-Id: I9870f7a77b294b14475c63a4b2752de0d86ef87b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2475113
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817492}

--- chrome/updater/win/group_policy_manager.h
+++ chrome/updater/win/group_policy_manager.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/strings/string16.h"
 #include "base/win/registry.h"
 #include "chrome/updater/policy_manager.h"
 
