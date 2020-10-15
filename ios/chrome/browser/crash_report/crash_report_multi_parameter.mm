1daf28cd0cb5d271d87d5244f807438ea25aaf1d
Eugene But <eugenebut@chromium.org>
2020-10-15 05:50:25 +0000

    [ios] Add user_application_state to synthetic crash reports
    user_application_state contains information about device orientation,
    size class, sign in status, number of tabs, etc.
    
    Bug: 1103752
    Change-Id: If3c3a379c77b7b2076af4ec2200047291de63ae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463772
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817383}

--- ios/chrome/browser/crash_report/crash_report_multi_parameter.mm
+++ ios/chrome/browser/crash_report/crash_report_multi_parameter.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/values.h"
+#import "components/previous_session_info/previous_session_info.h"
 #import "ios/chrome/browser/crash_report/breakpad_helper.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -80,11 +81,11 @@ const int kMaximumBreakpadValueSize = 255;
     NOTREACHED();
     return;
   }
-  breakpad_helper::AddReportParameter(
-      _crashReportKey,
-      [NSString stringWithCString:stateAsJson.c_str()
-                         encoding:[NSString defaultCStringEncoding]],
-      true);
+  NSString* value = base::SysUTF8ToNSString(stateAsJson);
+  breakpad_helper::AddReportParameter(_crashReportKey, value, /*async=*/true);
+  [[PreviousSessionInfo sharedInstance]
+      setReportParameterValue:value
+                       forKey:_crashReportKey];
 }
 
 @end
