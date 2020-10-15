a5d6be6e11ea43a30a0031c7db69eeb7428dbb19
Kyle Horimoto <khorimoto@chromium.org>
2020-10-15 20:04:37 +0000

    [CrOS Nearby] Create NearbyProcessManager and a KeyedService impl
    This class manages startup/shutdown of the Nearby utility process by
    using a reference count of the active clients. It is used by a future CL
    to connect to the process for Nearby Sharing.
    
    This CL leaves a TODO for a future CL to add the asynchronous shutdown
    flow. Tests for this new class are omitted in this CL and are added in
    the follow-up CL which adds the asynchronous shutdown flow [1] to
    prevent unnecessary churn in the test.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2460230
    
    Bug: 1130069
    Change-Id: I72782c4c2d8c5024cbcc7613497b7efbd46ecb23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2459112
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#817634}

--- chrome/browser/chromeos/browser_context_keyed_service_factories.cc
+++ chrome/browser/chromeos/browser_context_keyed_service_factories.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/launcher_search_provider/launcher_search_provider_service_factory.h"
 #include "chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_factory.h"
 #include "chrome/browser/chromeos/nearby/nearby_connections_dependencies_provider_factory.h"
+#include "chrome/browser/chromeos/nearby/nearby_process_manager_factory.h"
 #include "chrome/browser/chromeos/ownership/owner_settings_service_chromeos_factory.h"
 #include "chrome/browser/chromeos/phonehub/phone_hub_manager_factory.h"
 #include "chrome/browser/chromeos/platform_keys/key_permissions/key_permissions_service_factory.h"
@@ -74,6 +75,7 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   KerberosCredentialsManagerFactory::GetInstance();
   launcher_search_provider::ServiceFactory::GetInstance();
   nearby::NearbyConnectionsDependenciesProviderFactory::GetInstance();
+  nearby::NearbyProcessManagerFactory::GetInstance();
   OwnerSettingsServiceChromeOSFactory::GetInstance();
   phonehub::PhoneHubManagerFactory::GetInstance();
   platform_keys::KeyPermissionsServiceFactory::GetInstance();
