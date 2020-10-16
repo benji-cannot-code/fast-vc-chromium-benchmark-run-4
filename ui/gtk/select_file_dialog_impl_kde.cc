911baf9554faacb3f95a0f6810ab0ded5a3b147c
Ivan Murashov <ivan.murashov@lge.com>
2020-10-16 09:38:19 +0000

    Remove unneeded dependencies on x11 components
    After the change
    https://chromium-review.googlesource.com/c/chromium/src/+/2249389
    some dependencies on x11 components in the //ui/gtk no more needed
    
    BUG=1066670
    
    Change-Id: Idac2546ed1f79afccb91a15b3e6e55f3b65372b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2475815
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Maksim Sisov (GMT+3) <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#817855}

--- ui/gtk/select_file_dialog_impl_kde.cc
+++ ui/gtk/select_file_dialog_impl_kde.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/version.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/gfx/x/x11.h"
 #include "ui/gtk/select_file_dialog_impl.h"
 #include "ui/strings/grit/ui_strings.h"
 
