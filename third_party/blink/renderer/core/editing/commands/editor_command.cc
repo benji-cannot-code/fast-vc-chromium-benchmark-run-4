4902e298b7dc856c5cee94de2a7df756a8f227b5
Anupam Snigdha <snianu@microsoft.com>
2020-10-16 22:48:50 +0000

    Disable toggling of Overwrite mode when insert key is pressed.
    Added a flag to enable/disable toggle mode when insert key is pressed
    This is currently disabled by-default.
    
    Bug: 1030231
    
    Change-Id: I9c2dd62c0afe24d27848d189bf151d6a87a64068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2473771
    Reviewed-by: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#818145}

--- third_party/blink/renderer/core/editing/commands/editor_command.cc
+++ third_party/blink/renderer/core/editing/commands/editor_command.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/editing/commands/editor_command.h"
 
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
@@ -719,8 +720,14 @@ static bool ExecuteToggleOverwrite(LocalFrame& frame,
                                    Event*,
                                    EditorCommandSource,
                                    const String&) {
-  frame.GetEditor().ToggleOverwriteModeEnabled();
-  return true;
+  // Overwrite mode is disabled by-default. We only return true
+  // if the flag is enabled explicitly by the user in about:flags page.
+  if (base::FeatureList::IsEnabled(features::kInsertKeyToggleMode)) {
+    frame.GetEditor().ToggleOverwriteModeEnabled();
+    return true;
+  }
+  // We return false to match the expectation of the ExecCommand.
+  return false;
 }
 
 static bool ExecutePrint(LocalFrame& frame,
