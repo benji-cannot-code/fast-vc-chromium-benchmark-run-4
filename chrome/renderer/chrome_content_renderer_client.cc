725c1941848b8f2e01bdeb6ef7854c62c5dd0283
Dominic Farolino <dom@chromium.org>
2020-10-16 22:30:57 +0000

    Remove SearchBouncer mojo interface and singleton
    It became clear during the auditing of process-global associated
    interfaces that the SearchBouncer interface was clinging to dead code
    so this CL removes it entirely.
    
    More context:
    https://docs.google.com/document/d/12jyv-8aBwMlSS_Y5roEl_ykZT0_eQszGiiNs7Hxktmk/edit#heading=h.4zjjh3zdcgd
    
    Bug: 1132901
    Change-Id: Ib71d962d9d41c44fba52c420850b78b23b38b7e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2481087
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818136}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -167,7 +167,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/sandbox_status_extension_android.h"
 #else
 #include "chrome/renderer/media/chrome_speech_recognition_client.h"
-#include "chrome/renderer/searchbox/search_bouncer.h"
 #include "chrome/renderer/searchbox/searchbox.h"
 #include "chrome/renderer/searchbox/searchbox_extension.h"
 #endif
@@ -372,10 +371,6 @@ void ChromeContentRendererClient::RenderThreadStarted() {
 
   thread->AddObserver(chrome_observer_.get());
   thread->AddObserver(subresource_filter_ruleset_dealer_.get());
-
-#if !defined(OS_ANDROID)
-  thread->AddObserver(SearchBouncer::GetInstance());
-#endif
 
   thread->RegisterExtension(extensions_v8::LoadTimesExtension::Get());
 
@@ -1216,14 +1211,6 @@ bool ChromeContentRendererClient::HasErrorPage(int http_status_code) {
   // Use an internal error page, if we have one for the status code.
   return error_page::LocalizedError::HasStrings(
       error_page::Error::kHttpErrorDomain, http_status_code);
-}
-
-bool ChromeContentRendererClient::ShouldTrackUseCounter(const GURL& url) {
-  bool is_instant_ntp = false;
-#if !defined(OS_ANDROID)
-  is_instant_ntp = SearchBouncer::GetInstance()->IsNewTabPage(url);
-#endif
-  return !is_instant_ntp;
 }
 
 void ChromeContentRendererClient::PrepareErrorPage(
