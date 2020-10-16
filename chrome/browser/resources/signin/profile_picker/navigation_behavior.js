3269954995ec6ad86941f173958c502c9ca97009
Monica Basta <msalama@chromium.org>
2020-10-16 10:37:42 +0000

    [ProfilePicker]: Add BrowserSignin policy support.
    In case of BrowserSignin policy set to '0' => Disabled, the profile
    creation flow needs to load the local profile creation flow directly
    without showing the signin promo.
    
    Change-Id: I417edad354ad80538d16814f43004e99dd6eaede
    Bug: 1063856
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2475701
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817869}

--- chrome/browser/resources/signin/profile_picker/navigation_behavior.js
+++ chrome/browser/resources/signin/profile_picker/navigation_behavior.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assert, assertNotReached} from 'chrome://resources/js/assert.m.js';
 
-import {isForceSigninEnabled, isSignInProfileCreationSupported} from './policy_helper.js';
+import {isBrowserSigninAllowed, isForceSigninEnabled, isSignInProfileCreationSupported} from './policy_helper.js';
 
 /**
  * ProfilePickerPages enum.
@@ -51,8 +51,7 @@ function computeStep(route) {
       return 'mainView';
     case Routes.NEW_PROFILE:
       // TODO(msalama): Adjust once sign in profile creation is supported.
-      // Check DisallowSignIn policy.
-      if (!isSignInProfileCreationSupported()) {
+      if (!isSignInProfileCreationSupported() || !isBrowserSigninAllowed()) {
         assert(!isForceSigninEnabled());
         return ProfileCreationSteps.LOCAL_PROFILE_CUSTOMIZATION;
       }
