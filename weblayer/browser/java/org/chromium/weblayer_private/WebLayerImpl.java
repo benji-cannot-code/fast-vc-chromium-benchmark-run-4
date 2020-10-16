feeefa8b532f00292f025078ddfd32ce5c2b8ff5
Clark DuVall <cduvall@chromium.org>
2020-10-16 06:51:20 +0000

    Add some helper methods for dealing with isolated splits
    A follow up to http://crrev.com/c/2469235, this collects some common
    isolated splits logic in BundleUtils. Also, weblayer no longer needs to
    get resources from the isolated split, since all resources were moved to
    the base module in http://crrev.com/c/2404392.
    
    Bug: 1126301
    Change-Id: Ie88787c4990623e668b33ad535c3a247d4787f1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2473358
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817821}

--- weblayer/browser/java/org/chromium/weblayer_private/WebLayerImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/WebLayerImpl.java
@@ -43,7 +43,6 @@ import org.chromium.base.TraceEvent;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
-import org.chromium.base.compat.ApiHelperForO;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.metrics.RecordHistogram;
@@ -220,7 +219,6 @@ public final class WebLayerImpl extends IWebLayer.Stub {
             notifyWebViewRunningInProcess(remoteContext.getClassLoader());
         }
 
-        remoteContext = processRemoteContext(remoteContext);
         Context appContext = minimalInitForContext(
                 ObjectWrapper.unwrap(appContextWrapper, Context.class), remoteContext);
         PackageInfo packageInfo = WebViewFactory.getLoadedPackageInfo();
@@ -375,7 +373,7 @@ public final class WebLayerImpl extends IWebLayer.Stub {
         StrictModeWorkaround.apply();
         // This is a no-op if init has already happened.
         minimalInitForContext(ObjectWrapper.unwrap(appContext, Context.class),
-                processRemoteContext(ObjectWrapper.unwrap(remoteContext, Context.class)));
+                ObjectWrapper.unwrap(remoteContext, Context.class));
         return CrashReporterControllerImpl.getInstance();
     }
 
@@ -426,7 +424,7 @@ public final class WebLayerImpl extends IWebLayer.Stub {
         assert ContextUtils.getApplicationContext() == null;
         CommandLine.init(null);
         minimalInitForContext(ObjectWrapper.unwrap(appContext, Context.class),
-                processRemoteContext(ObjectWrapper.unwrap(remoteContext, Context.class)));
+                ObjectWrapper.unwrap(remoteContext, Context.class));
         LibraryLoader.getInstance().setLibraryProcessType(
                 LibraryProcessType.PROCESS_WEBLAYER_CHILD);
         LibraryLoader.getInstance().ensureInitialized();
@@ -855,18 +853,6 @@ public final class WebLayerImpl extends IWebLayer.Stub {
         } catch (Exception e) {
             Log.w(TAG, "Unable to notify WebView running in process.");
         }
-    }
-
-    private static Context processRemoteContext(Context remoteContext) {
-        // If WebLayer is in a DFM, make sure the correct resources are used.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            try {
-                return ApiHelperForO.createContextForSplit(remoteContext, "weblayer");
-            } catch (PackageManager.NameNotFoundException e) {
-                // WebLayer is not in a split, the original context will have the resources.
-            }
-        }
-        return remoteContext;
     }
 
     private static Context createContextForMode(Context remoteContext, int uiMode) {
