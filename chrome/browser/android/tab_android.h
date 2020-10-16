7925f8ba0c79ea3cc382de211495c728dde3c3aa
Gang Wu <gangwu@chromium.org>
2020-10-16 22:27:13 +0000

    TabAndroid supports base::SupportUserData
    This CL lets TabAndroid supports base::SupportUserData, then
    ChromeAutocompleteProviderClient can store stripped urls in TabAndroid
    to avoid stripping url over and over again.
    
    Bug: 1094056
    Change-Id: If0448c825965ddc7f43bd2aacc9c1d868e27b09f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2472461
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818132}

--- chrome/browser/android/tab_android.h
+++ chrome/browser/android/tab_android.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/strings/string16.h"
+#include "base/supports_user_data.h"
 #include "chrome/browser/sync/glue/synced_tab_delegate_android.h"
 #include "chrome/browser/tab/web_contents_state.h"
 #include "components/infobars/core/infobar_manager.h"
@@ -38,7 +39,7 @@ class DevToolsAgentHost;
 class WebContents;
 }
 
-class TabAndroid {
+class TabAndroid : public base::SupportsUserData {
  public:
   // A Java counterpart will be generated for this enum.
   // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.chrome.browser
@@ -62,7 +63,7 @@ class TabAndroid {
   static void AttachTabHelpers(content::WebContents* web_contents);
 
   TabAndroid(JNIEnv* env, const base::android::JavaRef<jobject>& obj);
-  ~TabAndroid();
+  ~TabAndroid() override;
 
   base::android::ScopedJavaLocalRef<jobject> GetJavaObject();
 
