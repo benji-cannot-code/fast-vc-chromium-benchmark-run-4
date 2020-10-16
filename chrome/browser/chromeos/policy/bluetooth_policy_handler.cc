fadf24c1734f1b5992520bdb21b7acf8c886bb5f
Howard Chung <howardchung@google.com>
2020-10-16 03:50:38 +0000

    Power off bluetooth adapter if policy disallow
    When DeviceAllowBluetooth is set to False, users can still use
    crosh > bt_console to scan/pair etc.
    
    This patch power off the bluetooth adapter when DeviceAllowBluetooth is
    applied. Since crosh > bt_console launches bluetoothctl in restricted
    mode, which disallow any power operations, users will no longer be able
    to bypass enterprise policy in this way.
    
    BUG=b:158568520
    TEST=set DeviceAllowBluetooth to False and verify the above method won't
    work.
    
    Change-Id: Ic02be25a19f2b73033c4c83e78fc3c707f86e48f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2467637
    Commit-Queue: Yun-Hao Chung <howardchung@google.com>
    Reviewed-by: James Vecore <vecore@google.com>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817791}

--- chrome/browser/chromeos/policy/bluetooth_policy_handler.cc
+++ chrome/browser/chromeos/policy/bluetooth_policy_handler.cc
@@ -47,6 +47,7 @@ void BluetoothPolicyHandler::SetBluetoothPolicy(
 
   if (!allow_bluetooth) {
     adapter_ = adapter;
+    adapter_->SetPowered(false, base::DoNothing(), base::DoNothing());
     adapter_->Shutdown();
   }
 }
