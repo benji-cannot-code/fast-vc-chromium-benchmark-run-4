4b1886d1ba7b5a2847ad05477651f7678a4a3af7
Adam Ettenberger <Adam.Ettenberger@microsoft.com>
2020-10-16 20:31:06 +0000

    Reland "Fix TextChanged UIA events"
    This is a reland of 2c428e5462fb5a2c951aa442d249a2be336b9f5f
    
    Reason for revert: Breaking:
    - win7 All/DumpAccessibilityEventsTest.AccessibilityEventsLiveRegionChange/uia
    https://ci.chromium.org/p/chromium/builders/ci/Win%207%20Tests%20x64%20%281%29/72572
    
    This reland updates the failing win7 test.
    
    Original change's description:
    > Fix TextChanged UIA events
    >
    > Blink fires the TextChanged event for all sorts of reasons, and it
    > leads to flakiness and difficulty fixing other bugs.
    >
    > Instead, fire this event when a static text node has changed its text
    > using AXEventGenerator, which is more stable and predictable.
    >
    > When static text changes under UIA, fire a text-changed event
    > rather than raising a property-changed for UIA_NamePropertyId.
    >
    > Add AXEventGenerator::Event::EDITABLE_TEXT_CHANGED which is fired on an
    > editable root when static text within an editable text region changes.
    >
    > Bug: 904977, 1062063, 1085481, 1105262
    > Change-Id: Ic655deba0d1a5ab1fb575ffa8a56cef1c538c923
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2393067
    > Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#816341}
    
    Bug: 904977
    Bug: 1062063
    Bug: 1085481
    Bug: 1105262
    Tbr: dmazzoni@chromium.org
    Change-Id: Ia485dee949d063e22befd3532475f0136775bcf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2465213
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#818075}

--- third_party/blink/renderer/modules/accessibility/ax_relation_cache.h
+++ third_party/blink/renderer/modules/accessibility/ax_relation_cache.h
@@ -127,7 +127,6 @@ class AXRelationCache {
   AXObject* GetOrCreate(Node*);
   AXObject* Get(Node*);
   void ChildrenChanged(AXObject*);
-  void TextChanged(AXObject*);
 
   DISALLOW_COPY_AND_ASSIGN(AXRelationCache);
 };
