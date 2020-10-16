df36d81649f0cb83be2878b58b37eadb58364978
Nate Fischer <ntfschr@chromium.org>
2020-10-16 19:47:03 +0000

    AW UMA: test for renderer histograms
    No change to production logic. This adds a test to verify we collect
    renderer process histograms and merge them with browser process
    histograms (at which point we can be confident they'll be included in
    the next UMA log).
    
    Fixed: 1112018
    Test: run_webview_instrumentation_test_apk -f AwMetricsIntegrationTest.*
    Change-Id: I2a4cd8328061fea6e67f9f4fb43f1fdd07669525
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2473807
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818037}

--- android_webview/browser/metrics/aw_metrics_service_client.cc
+++ android_webview/browser/metrics/aw_metrics_service_client.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/metrics/aw_stability_metrics_provider.h"
 #include "android_webview/browser_jni_headers/AwMetricsServiceClient_jni.h"
+#include "base/android/callback_android.h"
 #include "base/android/jni_android.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/persistent_histogram_allocator.h"
@@ -160,6 +161,16 @@ void JNI_AwMetricsServiceClient_SetUploadIntervalForTesting(
     jlong upload_interval_ms) {
   AwMetricsServiceClient::GetInstance()->SetUploadIntervalForTesting(
       base::TimeDelta::FromMilliseconds(upload_interval_ms));
+}
+
+// static
+void JNI_AwMetricsServiceClient_SetOnFinalMetricsCollectedListenerForTesting(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jobject>& listener) {
+  AwMetricsServiceClient::GetInstance()
+      ->SetOnFinalMetricsCollectedListenerForTesting(base::BindRepeating(
+          base::android::RunRunnableAndroid,
+          base::android::ScopedJavaGlobalRef<jobject>(listener)));
 }
 
 }  // namespace android_webview
