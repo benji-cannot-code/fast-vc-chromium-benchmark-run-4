11b220c11ce8b3209be72942ebcfe59bd8f7902f
Andrew Grieve <agrieve@chromium.org>
2020-10-16 17:54:39 +0000

    SuperSize: Make authenticate=1 the default
    But hardcode it to false for public tryjobs.
    
    This is a quick-fix to avoid sharing links where authenticate=1 is
    dropped.
    
    Note: There is no harm in authenticating unnecessarily (except for users
    needing to go though the steps of signing in).
    
    Bug: 1131568
    Change-Id: I96dbd4133df7ca3315c72d88ffb5ad515c401ab2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2477038
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#817999}

--- tools/binary_size/libsupersize/static/index.js
+++ tools/binary_size/libsupersize/static/index.js
@@ -31,7 +31,7 @@ function setSubmitListener(form, fetchDataUrl) {
     event.preventDefault();
     const dataUrl = fetchDataUrl();
     window.open(
-        `${FIREBASE_HOST}/viewer.html?load_url=${dataUrl}&authenticate=1`);
+        `${FIREBASE_HOST}/viewer.html?load_url=${dataUrl}`);
   });
 }
 
