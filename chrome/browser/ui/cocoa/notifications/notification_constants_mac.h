a7e53f8ec48a6d817fb913d14798f4f4de0e503e
Mohamed Adel <adelm@google.com>
2020-10-16 08:07:39 +0000

    Resubmit crrev.com/c/2471637 Fixed Tests
    Add user defined categories to UNNotifications
    
    -Adds complex categories that support 2 developer buttons
    
    -Moves all init methods from builders to the base builder
    
    -Modifies response builder to accommodate for developer buttons
    
    -Tests the built category and its response
    
    Bug: 1136061
    Change-Id: I13231b85298d37c9b7d6f27d4c55864aca0e945f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2476279
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Mohamed Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/master@{#817836}

--- chrome/browser/ui/cocoa/notifications/notification_constants_mac.h
+++ chrome/browser/ui/cocoa/notifications/notification_constants_mac.h
@@ -21,6 +21,7 @@ extern NSString* const kNotificationTag;
 extern NSString* const kNotificationCloseButtonTag;
 extern NSString* const kNotificationOptionsButtonTag;
 extern NSString* const kNotificationSettingsButtonTag;
+extern NSString* const kNotificationCategoryIdentifier;
 
 extern NSString* const kNotificationOrigin;
 extern NSString* const kNotificationId;
--- chrome/browser/ui/cocoa/notifications/notification_constants_mac.mm
+++ chrome/browser/ui/cocoa/notifications/notification_constants_mac.mm
@@ -20,6 +20,7 @@ NSString* const kNotificationTag = @"tag";
 NSString* const kNotificationCloseButtonTag = @"closeButton";
 NSString* const kNotificationOptionsButtonTag = @"optionsButton";
 NSString* const kNotificationSettingsButtonTag = @"settingsButton";
+NSString* const kNotificationCategoryIdentifier = @"categoryIdentifier";
 
 // Applicable to NotificationBuilder and NotificationResponseBuilder
 NSString* const kNotificationOrigin = @"notificationOrigin";
