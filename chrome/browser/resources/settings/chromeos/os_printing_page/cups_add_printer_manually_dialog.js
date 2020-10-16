fe5a34a3c0aebf2cbd04d53fc54d94286bec2224
Michael Checo <michaelcheco@google.com>
2020-10-16 23:44:21 +0000

    Printing: Hide queue input when protocol is set to "App Socket"
    Bug: 1132952
    Test: browser_tests --gtest_filter=OSSettingsCupsPrinterPageV3Test.All
    Change-Id: Ib32f26778d72f7b9c8ccf85883a2d0a09f335b84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2481662
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/master@{#818165}

--- chrome/browser/resources/settings/chromeos/os_printing_page/cups_add_printer_manually_dialog.js
+++ chrome/browser/resources/settings/chromeos/os_printing_page/cups_add_printer_manually_dialog.js
@@ -53,6 +53,12 @@ Polymer({
       type: String,
       value: '',
     },
+
+    /** @private */
+    showPrinterQueue_: {
+      type: Boolean,
+      value: true,
+    },
   },
 
   observers: [
@@ -163,6 +169,8 @@ Polymer({
    * @private
    */
   onProtocolChange_(event) {
+    // Queue input should be hidden when protocol is set to "App Socket".
+    this.showPrinterQueue_ = event.target.value !== 'socket';
     this.set('newPrinter.printerProtocol', event.target.value);
   },
 
