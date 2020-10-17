799cd8353c6b4e639b546df90378269c767627db
Chris Davis (EDGE) <chrdavis@microsoft.com>
2020-10-17 04:21:36 +0000

    JSONWriter should support adding non containers at max depth
    The JSON fuzzing tests failed to write JSON via JSONWriter::Write that
    was successfully read with JSONReader.  This was due to an edge case
    where we are writing the JSON and have reached max depth (200) but have
    non-container leaf entries at that level and nothing deeper.  This case
    should be supported by the JSONWriter to match the JSONReader and
    JSONParser. Also added a unit test for this case.
    
    Bug: 1138450, 1013934
    Change-Id: I98fcd63c957ab4e85a322d923bef18b981220383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2477239
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Chris Davis <chrdavis@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#818212}

--- base/json/json_writer_unittest.cc
+++ base/json/json_writer_unittest.cc
@@ -4,9 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/json/json_writer.h"
+#include "base/json/json_reader.h"
 
 #include "base/containers/span.h"
 #include "base/memory/ptr_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -185,6 +187,28 @@ TEST(JSONWriterTest, StackOverflow) {
     if (next_list)
       next_list->Remove(0, nullptr);
   }
+}
+
+TEST(JSONWriterTest, TestMaxDepthWithValidNodes) {
+  // Create JSON to the max depth - 1.  Nodes at that depth are still valid
+  // for writing which matches the JSONParser logic.
+  std::string nested_json;
+  for (int i = 0; i < 199; ++i) {
+    std::string node = "[";
+    for (int j = 0; j < 5; j++) {
+      node.append(StringPrintf("%d,", j));
+    }
+    nested_json.insert(0, node);
+    nested_json.append("]");
+  }
+
+  // Ensure we can read and write the JSON
+  JSONReader::ValueWithError json_val = JSONReader::ReadAndReturnValueWithError(
+      nested_json, JSON_ALLOW_TRAILING_COMMAS);
+  EXPECT_TRUE(json_val.value);
+  const Value& value = json_val.value.value();
+  std::string serialized;
+  EXPECT_TRUE(JSONWriter::Write(value, &serialized));
 }
 
 }  // namespace base
