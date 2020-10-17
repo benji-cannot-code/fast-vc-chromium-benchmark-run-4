4a9ab3f171e23e0503f4a4e01391271bfe404d38
Becca Hughes <beccahughes@chromium.org>
2020-10-17 02:02:18 +0000

    Add PAL token generation (public part)
    Change-Id: I28592a8cd09e89c2d980b55cb2e495d789a2ca1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2480714
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818193}

--- chrome/browser/media/kaleidoscope/constants.cc
+++ chrome/browser/media/kaleidoscope/constants.cc
@@ -12,4 +12,7 @@ const char kKaleidoscopeUIURL[] = "chrome://kaleidoscope";
 const char kKaleidoscopeUntrustedContentUIURL[] =
     "chrome-untrusted://kaleidoscope/";
 
+const char kKaleidoscopeUntrustedPALChildURL[] =
+    "chrome-untrusted://kaleidoscope-pal-generator/";
+
 const int kKaleidoscopeFirstRunLatestVersion = 2;
