df66be873dc16d5570dcdcb3e58bccc93c17abbe
Vincent Boisselle <vincb@chromium.org>
2020-10-17 01:05:25 +0000

    Fix bug where upload of clicks/views is disabled on feed stream destroy
    Other related changes:
      > Added a gate for the feature in the action manager that would have prevented the problem on not spreading on clients that don't have the feature enabled
      > Added test coverage.
    
    Bug: 1139394
    Change-Id: Ib9105b038f506e897fdca7fb99a987bd473e1a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2481302
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/master@{#818176}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/library/basicstream/BasicStream.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/library/basicstream/BasicStream.java
@@ -309,7 +309,7 @@ public class BasicStream implements Stream, ModelProviderObserver, OnLayoutChang
         mStreamOfflineMonitor.onDestroy();
         mUiSessionRequestLogger.onDestroy();
         mActionManager.setViewport(null);
-        mActionManager.setCanUploadClicksAndViewsWhenNoticeCardIsPresent(false);
+        mActionManager.setCanUploadClicksAndViewsWhenNoticeCardIsPresent(canUpload());
         mIsDestroyed = true;
     }
 
