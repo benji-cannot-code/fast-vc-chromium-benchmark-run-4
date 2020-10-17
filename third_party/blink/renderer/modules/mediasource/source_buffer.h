73a7e270a6f8f989182b0513ed95f9e46e32249e
Matt Wolenetz <wolenetz@chromium.org>
2020-10-17 01:04:19 +0000

    MSE-in-Workers: Cross-thread function and tests
    Includes implementation of CrossThreadMediaSourceAttachment (CTMSA) and
    updates to MediaSource and SourceBuffer to use it.
    
    CTMSA owns a lock that it takes on most operations. It also provides a
    utility method (RunExclusively()), which is used by the MSE API to take
    that same lock during most operations on the API. The same thread
    attachment provides a basic version of the same, though no lock is used.
    
    CTMSA understands whether or not it is attached, has ever started
    closing, and whether or not either of the attached contexts has ever
    destructed. It conditions cross-thread operations to safely behave when
    one or both contexts is being destructed.
    
    MediaSource is given its own small lock that it uses to guard its
    reference to the attachment instance, when attached. This is required
    because attachment start is synchronous on the main thread, even if the
    MediaSource is owned by worker context. CTMSA and MediaSource cooperate
    to ensure mutex and safety of the two-stage attachment start. In
    MediaSource::ContextDestroyed(), further care is taken to prevent
    attachment start success when worker context destruction is possibly
    racing the main thread's attempt to start attachment.
    `context_already_destroyed_`, protected by MediaSource's lock, is used
    to prevent that start from succeeding.
    
    MediaSource's context destruction can no longer always assume that
    accessing the demuxer via the WebMediaSource (and WebSourceBuffers) is
    safe. The worker version of MediaSource context destruction uses the
    attachment's RunExclusively() to safely know if it can cleanly Close the
    underlying demuxer, or if instead it must do a simpler shutdown.
    
    Future specification work will likely determine some signal to the media
    element when the worker-owned MediaSource's context is shutdown, yet the
    element itself is still alive. For now, sane results are returned by the
    attachment (nothing buffered nor seekable), with no other error
    provided. Possible app workarounds might include main and worker
    watchdogs and being careful about when the main thread explicitly
    terminates the worker.
    
    This experimental implementation attempts to retain
    BackgroundVideoOptimization, even for MSE-in-Workers, but does not
    support AudioVideoTracks in the worker MediaSource or SourceBuffer.
    Plumbing of appropriately-identified track metadata parsed from
    initialization segment in the worker is used to populate (and remove)
    media element AudioVideoTracks that should agree with the track id's
    used internally in WebMediaPlayerImpl to accomplish
    BackgroundVideoOptimization.
    
    As a simplification, CTMSA assumes it can be used for at most one
    attachment. Effectively, this assumes that the currently on-by-default
    MediaSourceRevokeObjectURLOnAttach feature behavior will be always-on
    soon. If CTMSA is not unregistered automatically after successful
    attachment start (e.g., if that feature is disabled) and if the app
    attempts to re-use the corresponding object URL for a later attachment,
    that attachment will be rejected by CTMSA. Note that the same-thread
    attachment would still allow such re-use when that feature is disabled.
    
    Updated web-test:
      mediasource-worker-attach.html changed to ...-play.html and fixed
    New web-test:
      mediasource-worker-play-terminate-worker.html
    Manual test: page refreshing while running mediasource-worker-play*.html
    
    BUG=878133
    
    Change-Id: I21f6542b90d51bdc28096500fb1441d202ab4ee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2459431
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Will Cassella <cassew@google.com>
    Cr-Commit-Position: refs/heads/master@{#818174}

--- third_party/blink/renderer/modules/mediasource/source_buffer.h
+++ third_party/blink/renderer/modules/mediasource/source_buffer.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/core/typed_arrays/array_buffer_view_helpers.h"
 #include "third_party/blink/renderer/modules/event_target_modules.h"
+#include "third_party/blink/renderer/modules/mediasource/media_source_attachment_supplement.h"
 #include "third_party/blink/renderer/modules/mediasource/track_default_list.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cancellable_task.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
@@ -78,7 +79,6 @@ class SourceBuffer final : public EventTargetWithInlineData,
   void setMode(const AtomicString&, ExceptionState&);
   bool updating() const { return updating_; }
   TimeRanges* buffered(ExceptionState&) const;
-  WebTimeRanges buffered() const;
   double timestampOffset() const;
   void setTimestampOffset(double, ExceptionState&);
   void appendBuffer(DOMArrayBuffer* data, ExceptionState&);
@@ -100,6 +100,18 @@ class SourceBuffer final : public EventTargetWithInlineData,
 
   AudioTrackList& audioTracks();
   VideoTrackList& videoTracks();
+
+  // "_Locked" requires these be called while in the scope of callback of
+  // |source_|'s attachment's RunExclusively(). Other methods without "_Locked"
+  // may also require the same, since they can be called from within these
+  // methods.
+  void SetMode_Locked(
+      AtomicString,
+      ExceptionState*,
+      MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */);
+  void GetBuffered_Locked(
+      WebTimeRanges* /* out */,
+      MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */) const;
 
   void RemovedFromMediaSource();
   double HighestPresentationTimestamp();
@@ -128,12 +140,9 @@ class SourceBuffer final : public EventTargetWithInlineData,
 
   bool PrepareAppend(double media_time, size_t new_data_size, ExceptionState&);
   bool EvictCodedFrames(double media_time, size_t new_data_size);
-  void AppendBufferInternal(double media_time,
-                            const unsigned char*,
-                            size_t,
-                            ExceptionState&);
+  void AppendBufferInternal(const unsigned char*, size_t, ExceptionState&);
   void AppendBufferAsyncPart();
-  void AppendError();
+  void AppendError(MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */);
 
   void RemoveAsyncPart();
 
@@ -141,6 +150,41 @@ class SourceBuffer final : public EventTargetWithInlineData,
   void AbortIfUpdating();
 
   void RemoveMediaTracks();
+
+  // "_Locked" requires these be called while in the scope of callback of
+  // |source_|'s attachment's RunExclusively(). Other methods without "_Locked"
+  // may also require the same, since they can be called from within these
+  // methods.
+  void SetTimestampOffset_Locked(
+      double,
+      ExceptionState*,
+      MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */);
+  void SetAppendWindowStart_Locked(
+      double,
+      MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */);
+  void SetAppendWindowEnd_Locked(
+      double,
+      MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */);
+  void Abort_Locked(
+      MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */);
+  void Remove_Locked(
+      double start,
+      double end,
+      ExceptionState*,
+      MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */);
+  void ChangeType_Locked(
+      const String& type,
+      ExceptionState*,
+      MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */);
+  void AppendBufferInternal_Locked(
+      const unsigned char*,
+      size_t,
+      ExceptionState*,
+      MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */);
+  void AppendBufferAsyncPart_Locked(
+      MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */);
+  void RemoveAsyncPart_Locked(
+      MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */);
 
   // Returns MediaElement playback position (i.e. MediaElement.currentTime() )
   // in seconds, or NaN if media element is not available.
