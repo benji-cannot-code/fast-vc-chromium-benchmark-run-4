db07b95e5f595bd1591c190a567e1dcdc808446a
Alice Boxhall <aboxhall@chromium.org>
2020-10-18 23:25:14 +0000

    Hide the focus highlight on tab change
    Bug: 1122961
    Change-Id: I7e442291cb2b830227ee6f1e0aa1b3896ac8eb5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2466012
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818321}

--- chrome/browser/ui/views/accessibility/accessibility_focus_highlight.h
+++ chrome/browser/ui/views/accessibility/accessibility_focus_highlight.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/time/time.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
@@ -30,7 +31,8 @@ class Layer;
 // highlight the focused UI element for accessibility.
 class AccessibilityFocusHighlight : public ui::LayerDelegate,
                                     public ui::CompositorAnimationObserver,
-                                    public content::NotificationObserver {
+                                    public content::NotificationObserver,
+                                    public TabStripModelObserver {
  public:
   explicit AccessibilityFocusHighlight(BrowserView* browser_view);
   ~AccessibilityFocusHighlight() override;
@@ -57,8 +59,8 @@ class AccessibilityFocusHighlight : public ui::LayerDelegate,
   // Get rid of the layer and stop animation.
   void RemoveLayer();
 
-  // Handle preference changes by adding or removing the layer as necessary.
-  void AddOrRemoveFocusObserver();
+  // Handle preference changes by adding or removing observers as necessary.
+  void AddOrRemoveObservers();
 
   // content::NotificationObserver overrides:
   void Observe(int type,
@@ -73,6 +75,11 @@ class AccessibilityFocusHighlight : public ui::LayerDelegate,
   // CompositorAnimationObserver overrides:
   void OnAnimationStep(base::TimeTicks timestamp) override;
   void OnCompositingShuttingDown(ui::Compositor* compositor) override;
+
+  // TabStripModelObserver
+  void OnTabStripModelChanged(TabStripModel*,
+                              const TabStripModelChange&,
+                              const TabStripSelectionChange&) override;
 
   // Compute the highlight color based on theme colors and defaults.
   SkColor GetHighlightColor();
