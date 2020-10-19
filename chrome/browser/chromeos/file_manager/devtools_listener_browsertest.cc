928fe61e94b2f2f5e2a7da59d4e07fc5c1122a50
Noel Gordon <noel@chromium.org>
2020-10-19 04:47:47 +0000

    [filesapp] Add CollectCodeCoverage test_name argument
    Instead of "fake_test_name", collect coverage for the given test_name,
    which would be the browser test fixture name in practice.
    
    Tbr: benreich
    Bug: 1113941
    Change-Id: Ib6b4a30cc09a37def86d3f0267a2596cda370602
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2483731
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818351}

--- chrome/browser/chromeos/file_manager/devtools_listener_browsertest.cc
+++ chrome/browser/chromeos/file_manager/devtools_listener_browsertest.cc
@@ -60,7 +60,7 @@ class DevToolsListenerBrowserTest : public content::DevToolsAgentHostObserver,
     LOG(FATAL) << "Host crashed: " << DevToolsListener::HostString(host);
   }
 
-  void CollectCodeCoverage() {
+  void CollectCodeCoverage(const std::string test_name) {
     base::ScopedAllowBlockingForTesting allow_blocking;
 
     CHECK(tmp_dir_.CreateUniqueTempDir());
@@ -71,7 +71,7 @@ class DevToolsListenerBrowserTest : public content::DevToolsAgentHostObserver,
     for (auto& agent : devtools_agent_) {
       auto* host = agent.first;
       EXPECT_TRUE(agent.second->HasCoverage(host));
-      agent.second->GetCoverage(host, coverage_store, "fake_test_name");
+      agent.second->GetCoverage(host, coverage_store, test_name);
       agent.second->Detach(host);
     }
 
@@ -107,7 +107,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsListenerBrowserTest, CanCollectCodeCoverage) {
   content::DevToolsAgentHost::RemoveObserver(this);
   content::RunAllTasksUntilIdle();
 
-  CollectCodeCoverage();
+  CollectCodeCoverage("CanCollectCodeCoverage");
 
   content::DevToolsAgentHost::DetachAllClients();
   content::RunAllTasksUntilIdle();
