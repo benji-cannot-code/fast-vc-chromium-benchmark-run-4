3a77438b2418573a18fd8267309ff9c1d329a68e
Peter Kasting <pkasting@chromium.org>
2020-10-19 23:04:56 +0000

    Change ButtonPressed overrides to callbacks: .../send_tab_to_self/
    This also reworks how the class is tested so only the public API is
    tested, and the actual construction/teardown are more like production.
    
    Bug: 772945
    Change-Id: I8cd6970d314359296398104484afc7551441808f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2469666
    Commit-Queue: Travis Skare <skare@chromium.org>
    Reviewed-by: Travis Skare <skare@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818678}

--- chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_bubble_device_button.h
+++ chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_bubble_device_button.h
@@ -14,15 +14,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace send_tab_to_self {
 
+class SendTabToSelfBubbleViewImpl;
 struct TargetDeviceInfo;
 
 // A button representing a device in share bubble. It is highlighted when
 // hovered.
 class SendTabToSelfBubbleDeviceButton : public HoverButton {
  public:
-  SendTabToSelfBubbleDeviceButton(views::ButtonListener* button_listener,
-                                  const TargetDeviceInfo& device_info,
-                                  int button_tag);
+  SendTabToSelfBubbleDeviceButton(SendTabToSelfBubbleViewImpl* bubble,
+                                  const TargetDeviceInfo& device_info);
   ~SendTabToSelfBubbleDeviceButton() override;
 
   const std::string& device_name() const { return device_name_; }
