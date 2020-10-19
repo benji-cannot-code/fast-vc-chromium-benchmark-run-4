9b1cda46e1dc20d096d23d491583d105a4ebe864
Sergey Ulanov <sergeyu@chromium.org>
2020-10-19 19:12:22 +0000

    [Fuchsia] Allow SetVolume on uninitialized FuchsiaAudioRenderer
    initialized. FuchsiaAudioRenderer was not handling this correctly.
    It was trying to call AudioConsumer.BindVolumeControl before the
    AudioConsumer is connected. As result it was logging an error
    message about disconnected VolumeControl without setting the volume.
    Updated SetVolume() implementation to persist the volume value and
    send it as soon as AudioConsumer is connected.
    
    AudioRenderer: :SetVolume() may be called before the renderer is
    Change-Id: Ic5418c2c002c1181da01e8a278b0b53845baee47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2481958
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818564}

--- media/fuchsia/audio/fuchsia_audio_renderer.cc
+++ media/fuchsia/audio/fuchsia_audio_renderer.cc
@@ -114,6 +114,8 @@ void FuchsiaAudioRenderer::Initialize(DemuxerStream* stream,
     OnError(AUDIO_RENDERER_ERROR);
   });
 
+  UpdateVolume();
+
   audio_consumer_.events().OnEndOfStream = [this]() { OnEndOfStream(); };
   RequestAudioConsumerStatus();
 
@@ -144,6 +146,18 @@ void FuchsiaAudioRenderer::Initialize(DemuxerStream* stream,
   demuxer_stream_ = stream;
 
   std::move(init_cb_).Run(PIPELINE_OK);
+}
+
+void FuchsiaAudioRenderer::UpdateVolume() {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  DCHECK(audio_consumer_);
+  if (!volume_control_) {
+    audio_consumer_->BindVolumeControl(volume_control_.NewRequest());
+    volume_control_.set_error_handler([](zx_status_t status) {
+      ZX_LOG(ERROR, status) << "VolumeControl disconnected.";
+    });
+  }
+  volume_control_->SetVolume(volume_);
 }
 
 void FuchsiaAudioRenderer::InitializeStreamSink(
@@ -229,13 +243,9 @@ void FuchsiaAudioRenderer::StartPlaying() {
 
 void FuchsiaAudioRenderer::SetVolume(float volume) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-  if (!volume_control_) {
-    audio_consumer_->BindVolumeControl(volume_control_.NewRequest());
-    volume_control_.set_error_handler([](zx_status_t status) {
-      ZX_LOG(ERROR, status) << "VolumeControl disconnected.";
-    });
-  }
-  volume_control_->SetVolume(volume);
+  volume_ = volume;
+  if (audio_consumer_)
+    UpdateVolume();
 }
 
 void FuchsiaAudioRenderer::SetLatencyHint(
