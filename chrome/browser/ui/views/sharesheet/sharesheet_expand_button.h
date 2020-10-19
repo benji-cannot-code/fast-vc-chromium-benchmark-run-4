ab8038fbedd4a2f660087e48bf5d3f30a222fab4
Melissa Zhang <melzhang@chromium.org>
2020-10-19 05:38:33 +0000

    Revert "Change ButtonPressed overrides to callbacks: c/b/ui/views/sharesheet/"
    This reverts commit 1ac9a1da351bf0b2461f2d8b1fe0ded2aa031a2b.
    
    Reason for revert: Breaking UI
    
    Original change's description:
    > Change ButtonPressed overrides to callbacks: c/b/ui/views/sharesheet/
    >
    > This allows converting |targets_| from a member to a parameter that's
    > passed around.  I also did some other cleanup while there.
    >
    > Bug: 772945
    > Change-Id: Iaa7f70594ba109d1598da3566856e90c8b09629d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2481246
    > Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    > Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    > Auto-Submit: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#818324}
    
    TBR=pkasting@chromium.org,melzhang@chromium.org
    
    Change-Id: Ief930a27f44caef219bf782a7534ef8bc76d83ef
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 772945
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2483745
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818357}

--- chrome/browser/ui/views/sharesheet/sharesheet_expand_button.h
+++ chrome/browser/ui/views/sharesheet/sharesheet_expand_button.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class SharesheetExpandButton : public views::Button {
  public:
-  explicit SharesheetExpandButton(PressedCallback callback);
+  explicit SharesheetExpandButton(views::ButtonListener* listener);
   SharesheetExpandButton(const SharesheetExpandButton&) = delete;
   SharesheetExpandButton& operator=(const SharesheetExpandButton&) = delete;
 
