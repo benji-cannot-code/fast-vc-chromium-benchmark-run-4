fc207cbaa4ab386b3a67eecd56a6eb78fcf3f0aa
Finnur Thorarinsson <finnur@chromium.org>
2020-10-19 16:55:16 +0000

    Revert "Fix GrantForChromePages flakiness"
    This reverts commit fde5beb9ed80fa35b5bd6f4131775d959ecf5e25.
    
    Reason for revert: The test is still super flaky. See for example:
    https://ci.chromium.org/p/chromium/builders/ci/Win10%20Tests%20x64?limit=200
    
    Original change's description:
    > Fix GrantForChromePages flakiness
    >
    > Currently, TabCaptureApitTest.GrantForChromePages is flaky due to the
    > permission not being granted properly occasionally. I believe that this
    > is due to the permission granter not granting the correct tab permission
    > due to a race condition between the page load and the permission
    > granting.
    >
    > This patch fixes this issue by adding a new message and standardizing
    > this test to work similarly to other stable tests.
    >
    > Bug: 1134562
    > Change-Id: Iea40786bbfb86b6a5f6e47731ecc29939068e878
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2466489
    > Commit-Queue: Jordan Bayles <jophba@chromium.org>
    > Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#818064}
    
    TBR=mfoltz@chromium.org,miu@chromium.org,jophba@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1134562
    Change-Id: Icc4758584c36c216af248ac1d65b804d98e62cef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485212
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818501}

--- chrome/test/data/extensions/api_test/tab_capture/active_tab_chrome_pages.js
+++ chrome/test/data/extensions/api_test/tab_capture/active_tab_chrome_pages.js
@@ -3,9 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var afterGrantPermission = function() {
+var afterTabOpened = function() {
   chrome.tabCapture.capture({audio: true, video: true}, function(stream) {
-    chrome.test.assertNoLastError();
     chrome.test.assertTrue(!!stream);
     stream.getVideoTracks()[0].stop();
     stream.getAudioTracks()[0].stop();
@@ -13,9 +12,5 @@ var afterGrantPermission = function() {
   });
 };
 
-var afterOpenTab = function() {
-  chrome.test.sendMessage('ready2', afterGrantPermission);
-};
-
 chrome.test.notifyPass();
-chrome.test.sendMessage('ready1', afterOpenTab);
+chrome.test.sendMessage('ready1', afterTabOpened);
