fb5bb92328661d75d311fee7b50b848453250e11
Lan Wei <lanwei@chromium.org>
2020-10-19 22:33:47 +0000

    Revert "Give test hooks to allow autofill test to keep popup open"
    This reverts commit 4e484a8fb18812030c82d11421ab3299bb45f4f9.
    
    Reason for revert: 
    AutofillInteractiveTest.PrefillFormAndFill fails on Mac10.11 Tests,
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.11%20Tests.
    
    Original change's description:
    > Give test hooks to allow autofill test to keep popup open
    >
    > On some test bots, mac dock or external values cause the browser to
    > make minor resizings of the frame. These are extraneous to our tests
    > so we would like to ignore those during tests.
    >
    > This CL provides a test hook by adding test access methods to
    > ChromeAutofillClient to make the autofill popup more
    > resistant to hiding from frame resizes, and then has AutofillUiTest
    > access that hook when running Interactive UI tests.
    >
    > This re-enables autofill interactive tests on mac bots.
    >
    >
    > Bug: 967588
    > Change-Id: Id9e25988f0cf64f0aa0631a48d48eb982b116c34
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2473802
    > Commit-Queue: Ian Struiksma <ianstruiksma@google.com>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Reviewed-by: Dominic Battré <battre@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#818550}
    
    TBR=battre@chromium.org,vasilii@chromium.org,ianstruiksma@google.com
    
    Change-Id: I82610a8d1f4a1161a13f0ce62771fac6a12b091f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 967588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485539
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818661}

--- chrome/browser/autofill/autofill_interactive_uitest.cc
+++ chrome/browser/autofill/autofill_interactive_uitest.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "build/build_config.h"
+// Disable all tests in this file on Mac for flake (crbug.com/1079249)
+#if !defined(OS_MAC)
 
 #include <string>
 #include <tuple>
@@ -342,6 +344,14 @@ class AutofillInteractiveTestBase : public AutofillUiTest {
     response->set_content_type("text/html;charset=utf-8");
     response->set_content(test_url_content_);
     return std::move(response);
+  }
+
+  content::WebContents* GetWebContents() {
+    return browser()->tab_strip_model()->GetActiveWebContents();
+  }
+
+  content::RenderViewHost* GetRenderViewHost() {
+    return GetWebContents()->GetRenderViewHost();
   }
 
   void CreateTestProfile() {
@@ -3497,3 +3507,5 @@ INSTANTIATE_TEST_SUITE_P(All,
                          AutofillRestrictUnownedFieldsTest,
                          testing::Bool());
 }  // namespace autofill
+
+#endif  // !defined(OS_MAC)
