a17b8fb444ec291a0785ce9652b2d4314c32bfb9
Steven Bennetts <stevenjb@chromium.org>
2020-10-19 16:14:40 +0000

    FakeShill*Client: Elim DictionaryValue
    This removes DictionaryValue from the fake Manager, Device, and IPConfig
    classes.
    
    Bug: 1137487
    Change-Id: I823c7cb6b6fd3620e750931dc9ac983365884b83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2465777
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818489}

--- chromeos/dbus/shill/shill_ipconfig_client.h
+++ chromeos/dbus/shill/shill_ipconfig_client.h
@@ -15,8 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 class Value;
-class DictionaryValue;
-}  // namespace base
+}
 
 namespace dbus {
 class Bus;
@@ -36,7 +35,7 @@ class COMPONENT_EXPORT(SHILL_CLIENT) ShillIPConfigClient {
    public:
     // Adds an IPConfig entry.
     virtual void AddIPConfig(const std::string& ip_config_path,
-                             const base::DictionaryValue& properties) = 0;
+                             const base::Value& properties) = 0;
 
    protected:
     virtual ~TestInterface() {}
