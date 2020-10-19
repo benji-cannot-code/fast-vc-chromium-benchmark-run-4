98793c8e83c2b0f4fd65aeb0ff88ed9d9457a04f
Christopher Cameron <ccameron@chromium.org>
2020-10-19 19:55:52 +0000

    MacPWAs: Close shims when it is uninstalled for all profiles
    Change AppShimManager::AppState::ShouldDeleteAppState to return true
    if the app is not installed for any profiles. Previously, the app
    would continue running even after it had been deleted, resulting in
    all sorts of cool behaviors. This winds up being hit in two paths.
    
    First path is the AppShimManager::OnAppDeactivated call that is made
    when a profile closes its last app window. This fixes crbug.com/1139254
    where an app kept running even after it was uninstalled.
    
    Second path is AppShimManager::OnProfileMarkedForPermanentDeletion,
    for when a profile is deleted. This is for crbug.com/1132223 and is
    a bit more involved.
    * Listening for chrome::NOTIFICATION_PROFILE_CREATED/DESTROYED has
      been deprecated for a half-decade, so change AppShimManager to be
      a ProfileManagerObserver instead.
    * Inline AppShimManager::CloseShimsForProfile to be a lambda inside
      of OnProfileMarkedForPermanentDeletion.
    * Of note about ProfileManagerObserver is that closing app shims
      inside OnProfileMarkedForPermanentDeletion can cause crashes
      because it is not expected that Browser instances will be closed
      inside that call. For that reason, post a task to do the work.
    
    Update tests (because now they're affected by the AppShimRegistry)
    and add tests for both paths (shim installed vs uninstalled).
    
    Since AppShimManager is now a ProfileManagerObserver we have to
    make sure that the AppShimManager stops observing the ProfileManager
    before the ProfileManager is destroyed. To do this, a new call,
    BrowserProcessPlatformPart::BeginStartTearDown, which happens at
    the beginning of BrowserProcessPlatformPart::StartTearDown.
    
    Bug: 1139254,1132223
    Change-Id: I19990f6ae5ff8d8727e1fb0ecf46d1142b543764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2481790
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818594}

--- chrome/browser/apps/app_shim/app_shim_manager_mac.h
+++ chrome/browser/apps/app_shim/app_shim_manager_mac.h
@@ -19,13 +19,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_shim/app_shim_host_bootstrap_mac.h"
 #include "chrome/browser/apps/app_shim/app_shim_host_mac.h"
 #include "chrome/browser/profiles/avatar_menu_observer.h"
+#include "chrome/browser/profiles/profile_manager_observer.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/web_applications/components/web_app_id.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 
 class Profile;
+class ProfileManager;
 
 namespace base {
 class FilePath;
@@ -41,10 +41,10 @@ namespace apps {
 // extension.
 class AppShimManager : public AppShimHostBootstrap::Client,
                        public AppShimHost::Client,
-                       public content::NotificationObserver,
                        public AppLifetimeMonitor::Observer,
                        public BrowserListObserver,
-                       public AvatarMenuObserver {
+                       public AvatarMenuObserver,
+                       public ProfileManagerObserver {
  public:
   class Delegate {
    public:
@@ -114,6 +114,10 @@ class AppShimManager : public AppShimHostBootstrap::Client,
   explicit AppShimManager(std::unique_ptr<Delegate> delegate);
   ~AppShimManager() override;
 
+  // Called at the beginning of browser shut down. Is used to remove |this| as
+  // a ProfileManager and AvatarMenuObserver observer.
+  void OnBeginTearDown();
+
   // Get the host corresponding to a profile and app id, or null if there is
   // none.
   AppShimHost* FindHost(Profile* profile, const web_app::AppId& app_id);
@@ -156,10 +160,9 @@ class AppShimManager : public AppShimHostBootstrap::Client,
   void OnAppStop(content::BrowserContext* context,
                  const std::string& app_id) override;
 
-  // content::NotificationObserver overrides:
-  void Observe(int type,
-               const content::NotificationSource& source,
-               const content::NotificationDetails& details) override;
+  // ProfileManagerObserver overrides:
+  void OnProfileAdded(Profile* profile) override;
+  void OnProfileMarkedForPermanentDeletion(Profile* profile) override;
 
   // BrowserListObserver overrides;
   void OnBrowserAdded(Browser* browser) override;
@@ -213,9 +216,6 @@ class AppShimManager : public AppShimHostBootstrap::Client,
   // Terminate Chrome if Chrome attempted to quit, but was prevented from
   // quitting due to apps being open.
   virtual void MaybeTerminate();
-
-  // Exposed for testing.
-  content::NotificationRegistrar& registrar() { return registrar_; }
 
   // Called when profile menu items may have changed. Rebuilds the profile
   // menu item list and sends updated lists to all apps.
@@ -312,10 +312,11 @@ class AppShimManager : public AppShimHostBootstrap::Client,
   ProfileState* GetOrCreateProfileState(Profile* profile,
                                         const web_app::AppId& app_id);
 
+  // Weak, reset during OnBeginTearDown.
+  ProfileManager* profile_manager_ = nullptr;
+
   // Map from extension id to the state for that app.
   std::map<std::string, std::unique_ptr<AppState>> apps_;
-
-  content::NotificationRegistrar registrar_;
 
   // The avatar menu instance used by all app shims.
   std::unique_ptr<AvatarMenu> avatar_menu_;
--- chrome/browser/browser_process_platform_part_base.cc
+++ chrome/browser/browser_process_platform_part_base.cc
@@ -19,6 +19,8 @@ void BrowserProcessPlatformPartBase::PlatformSpecificCommandLineProcessing(
     const base::CommandLine& /* command_line */) {
 }
 
+void BrowserProcessPlatformPartBase::BeginStartTearDown() {}
+
 void BrowserProcessPlatformPartBase::StartTearDown() {
 }
 
