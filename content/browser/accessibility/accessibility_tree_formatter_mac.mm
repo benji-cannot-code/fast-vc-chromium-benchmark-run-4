1ee48cc28f74e5d7f09e7e572dc8cd3beba12a27
Alexander Surkov <asurkov@igalia.com>
2020-10-19 20:10:43 +0000

    dumpacctree testsuite: children property shall not be exposed in the formatted tree
    Correct implementation of
    https://chromium-review.googlesource.com/c/chromium/src/+/2412192:
    * optimal since children are not populated and not appended into
    a tree dictionary structure
    * clean because all logic is kept by tree formatter
    
    Bug: n/a
    Change-Id: Ic4108c407d6f6d9b4786dd188efbd57b4648684d
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2481762
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#818605}

--- content/browser/accessibility/accessibility_tree_formatter_mac.mm
+++ content/browser/accessibility/accessibility_tree_formatter_mac.mm
@@ -499,7 +499,13 @@ std::string AccessibilityTreeFormatterMac::ProcessTreeForOutput(
       continue;
     }
 
-    // Special processing for position and size.
+    // Special case: children.
+    // Children are used to generate the tree
+    // itself, thus no sense to expose them on each node.
+    if (item.first == kChildrenDictAttr) {
+      continue;
+    }
+    // Special case: position.
     if (item.first == kPositionDictAttr) {
       WriteAttribute(false,
                      FormatCoordinates(
@@ -508,6 +514,7 @@ std::string AccessibilityTreeFormatterMac::ProcessTreeForOutput(
                      &line);
       continue;
     }
+    // Special case: size.
     if (item.first == kSizeDictAttr) {
       WriteAttribute(
           false,
