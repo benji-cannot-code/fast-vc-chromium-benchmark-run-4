c1773b5686a7fcdc849727c546535a54b2fac17c
Luca Hunkeler <hluca@google.com>
2020-10-19 14:12:28 +0000

    [Autofill Assistant] Improve logic to hide chips on keyboard shown
    Currently, if an element check is being performed the chips will not
    be correctly hidden when the keyboard is shown. This happens because
    after the periodic element check we reset the visibity to the default
    value (true). This change reworks how we hide the chips and fixes that
    problem.
    
    Bug: b/171179162
    Change-Id: Icb3149fb31449d2ad2dedc114b710f195275ff57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2484696
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#818457}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantActionsCarouselUiTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantActionsCarouselUiTest.java
@@ -97,7 +97,7 @@ public class AutofillAssistantActionsCarouselUiTest {
                         -> model.set(AssistantCarouselModel.CHIPS,
                                 Collections.singletonList(new AssistantChip(
                                         AssistantChip.Type.BUTTON_HAIRLINE, AssistantChip.Icon.NONE,
-                                        "Test", false, true, "", null))));
+                                        "Test", false, true, true, null))));
 
         // Chip was created and is displayed on the screen.
         onView(is(coordinator.getView()))
@@ -118,10 +118,10 @@ public class AutofillAssistantActionsCarouselUiTest {
         List<AssistantChip> chips = new ArrayList<>();
         for (int i = 0; i < numChips; i++) {
             chips.add(new AssistantChip(AssistantChip.Type.BUTTON_HAIRLINE, AssistantChip.Icon.NONE,
-                    "T" + i, false, false, "", null));
+                    "T" + i, false, false, true, null));
         }
         chips.add(new AssistantChip(AssistantChip.Type.BUTTON_HAIRLINE, AssistantChip.Icon.NONE,
-                "X", false, true, "", null));
+                "X", false, true, true, null));
 
         TestThreadUtils.runOnUiThreadBlocking(() -> model.set(AssistantCarouselModel.CHIPS, chips));
 
@@ -146,10 +146,10 @@ public class AutofillAssistantActionsCarouselUiTest {
         List<AssistantChip> chips = new ArrayList<>();
         for (int i = 0; i < numChips; i++) {
             chips.add(new AssistantChip(AssistantChip.Type.BUTTON_HAIRLINE, AssistantChip.Icon.NONE,
-                    "Test" + i, false, false, "", null));
+                    "Test" + i, false, false, true, null));
         }
         chips.add(new AssistantChip(AssistantChip.Type.BUTTON_HAIRLINE, AssistantChip.Icon.NONE,
-                "Cancel", false, true, "", null));
+                "Cancel", false, true, true, null));
         TestThreadUtils.runOnUiThreadBlocking(() -> model.set(AssistantCarouselModel.CHIPS, chips));
 
         // Cancel chip is initially displayed to the user.
@@ -175,9 +175,9 @@ public class AutofillAssistantActionsCarouselUiTest {
 
         List<AssistantChip> chips = new ArrayList<>();
         chips.add(new AssistantChip(AssistantChip.Type.BUTTON_HAIRLINE, AssistantChip.Icon.NONE,
-                "Test 2", false, false, "", null));
+                "Test 2", false, false, true, null));
         chips.add(new AssistantChip(AssistantChip.Type.BUTTON_HAIRLINE, AssistantChip.Icon.NONE,
-                "Cancel", false, true, "", null));
+                "Cancel", false, true, true, null));
         TestThreadUtils.runOnUiThreadBlocking(() -> model.set(AssistantCarouselModel.CHIPS, chips));
         onView(withText("Cancel")).check(matches(isDisplayed()));
         onView(withText("Test 2")).check(matches(isDisplayed()));
@@ -186,7 +186,7 @@ public class AutofillAssistantActionsCarouselUiTest {
         List<AssistantChip> newChips = new ArrayList<>();
         newChips.add(chips.get(0));
         newChips.add(new AssistantChip(AssistantChip.Type.BUTTON_HAIRLINE, AssistantChip.Icon.NONE,
-                "Test 1", false, false, "", null));
+                "Test 1", false, false, true, null));
         newChips.add(chips.get(1));
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> model.set(AssistantCarouselModel.CHIPS, newChips));
--- components/autofill_assistant/browser/controller.h
+++ components/autofill_assistant/browser/controller.h
@@ -252,6 +252,7 @@ class Controller : public ScriptExecutorDelegate,
   bool ShouldShowOverlay() const override;
   void ShutdownIfNecessary() override;
   bool IsRunningLiteScript() const override;
+  void OnKeyboardVisibilityChanged(bool visible) override;
 
  private:
   friend ControllerTest;
@@ -354,6 +355,8 @@ class Controller : public ScriptExecutorDelegate,
   void PerformDelayedShutdownIfNecessary();
 
   bool StateNeedsUI(AutofillAssistantState state);
+
+  void SetVisibilityAndUpdateUserActions();
 
   ClientSettings settings_;
   Client* const client_;
@@ -522,6 +525,7 @@ class Controller : public ScriptExecutorDelegate,
   bool expand_sheet_for_prompt_action_ = true;
   std::vector<std::string> browse_domains_allowlist_;
   bool browse_mode_invisible_ = false;
+  bool is_keyboard_showing_ = false;
 
   // Only set during a ShowGenericUiAction.
   std::unique_ptr<GenericUserInterfaceProto> generic_user_interface_;
