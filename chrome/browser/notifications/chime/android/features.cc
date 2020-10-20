5e09694c549f85339ae05ee3990b04d2f2599fe1
Xing Liu <xingliu@chromium.org>
2020-10-20 02:00:05 +0000

    Chime: Add an initialize method.
    Chime needs to be initialized during ChromeApplication.java's startup
    code.
    
    This CL adds ChimeSession.initialize() entry point API, and a upstream
    placeholder function for Chime DFM.
    
    Also clean up the feature flag JNI call for Chime, now it can directly
    depends on chrome/browser/flags:java.
    
    Bug: 1136305
    Change-Id: Id8424fc22d2f419860318b12bd8753da448cd50b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2466897
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818755}

--- chrome/browser/notifications/chime/android/features.cc
+++ chrome/browser/notifications/chime/android/features.cc
@@ -4,12 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/notifications/chime/android/features.h"
-#include "chrome/browser/notifications/chime/android/jni_headers/ChimeSession_jni.h"
-
-jboolean JNI_ChimeSession_IsEnabled(JNIEnv* env) {
-  return base::FeatureList::IsEnabled(
-      notifications::features::kUseChimeAndroidSdk);
-}
 
 namespace notifications {
 namespace features {
