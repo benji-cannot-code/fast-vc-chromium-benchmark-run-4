8b00b4890628e25d13c6a55a59f4822c39bd7608
Morten Stenshorne <mstensho@chromium.org>
2020-10-20 10:37:37 +0000

    Enable "gn check" for //third_party/blink/renderer/core
    Bug: 800764
    Change-Id: I54f490a37baee7f8e98198fa28794aa216f9f33c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2469563
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818849}

--- third_party/blink/renderer/bindings/core/v8/script_streamer.cc
+++ third_party/blink/renderer/bindings/core/v8/script_streamer.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
-#include "third_party/blink/renderer/core/html/parser/text_resource_decoder.h"
 #include "third_party/blink/renderer/core/inspector/inspector_trace_events.h"
 #include "third_party/blink/renderer/core/loader/resource/script_resource.h"
 #include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
