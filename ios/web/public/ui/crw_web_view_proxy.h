cec08eacadaca7d36b7d15b2beb5452f364bd9ef
Mike Dougherty <michaeldo@chromium.org>
2020-10-20 19:13:32 +0000

    Remove unused find in page implementation
    The Find in Page feature implementation has moved to
    ios/web/public/find_in_page. This code is unused and can be removed.
    
    Bug: 487804 , 949651, 996324
    Change-Id: I55bfe528d428085f4f59de80d5bfb56130cabfa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2480887
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819027}

--- ios/web/public/ui/crw_web_view_proxy.h
+++ ios/web/public/ui/crw_web_view_proxy.h
@@ -64,10 +64,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Wrapper around the addSubview method of the webview.
 - (void)addSubview:(UIView*)view;
 
-// Returns YES if it makes sense to search for text right now.
-// TODO(crbug.com/949651): Remove once JSFindInPageManager is removed.
-- (BOOL)hasSearchableTextContent;
-
 // Returns the currently visible keyboard accessory, or nil.
 - (UIView*)keyboardAccessory;
 
