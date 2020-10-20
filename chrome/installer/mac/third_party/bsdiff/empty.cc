8bbe6839cb2892a3c90afe2c45fc0fd1c990c016
Michael Moss <mmoss@chromium.org>
2020-10-20 16:33:14 +0000

    Support building diff tools for multiple archs at the same time.
    Some tools need to be used both when creating diffs and when installing
    diffs, so need to support the archs of both the target system and the
    system generating the diffs. These are generally the same thing, but
    when cross-compiling for arm64 on x86, it requires universal binaries.
    
    BUG=1119472
    
    Cq-Include-Trybots: luci.chromium.try:mac-rel,mac-arm64-rel
    Change-Id: Ic329561b8f034477d491121e507ba34821419516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462242
    Commit-Queue: Michael Moss <mmoss@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Michael Moss <mmoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818949}

--- chrome/installer/mac/third_party/bsdiff/empty.cc
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// This is an empty file with a .cc extension, to convince the toolchain
-// (I'm looking at YOU, Xcode) that it needs to link any target this file
-// belongs to as C++.
