b491a6b0da513d146b2c80cf6afd1924458d4368
Yi Gu <yigu@chromium.org>
2020-10-20 14:11:31 +0000

    [WebOTP] Record metrics when a prompt is timeout
    This patch adds metrics to record when a prompt is timeout due to
    unresponsiveness.
    
    Currently prompt timeout is not handled. This patch adds necessary
    functionality to address that issue. Note that it only records when a
    single pending origin is present to avoid over-counting. If multiple
    origins are pending in the queue we cannot handle the failed request on
    a particular origin due to the lack of origin information. This issue is
    tracked via crbug.com/1138454.
    
    Bug: 1127397
    Change-Id: I6f888261ec5ea433521d914f2498d20f100cee6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427560
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818892}

--- content/browser/sms/sms_queue.h
+++ content/browser/sms/sms_queue.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/observer_list.h"
-#include "content/browser/sms/sms_parser.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/sms_fetcher.h"
 #include "url/origin.h"
@@ -23,7 +22,6 @@ class CONTENT_EXPORT SmsQueue {
   ~SmsQueue();
 
   using FailureType = SmsFetcher::FailureType;
-  using SmsParsingStatus = SmsParser::SmsParsingStatus;
   using Subscriber = SmsFetcher::Subscriber;
 
   void Push(const url::Origin& origin, Subscriber* subscriber);
@@ -31,7 +29,9 @@ class CONTENT_EXPORT SmsQueue {
   void Remove(const url::Origin& origin, Subscriber* subscriber);
   bool HasSubscribers();
   bool HasSubscriber(const url::Origin& origin, const Subscriber* subscriber);
-  void NotifyParsingFailure(SmsParsingStatus status);
+  // Pops and notifies the first subscriber of an origin iff there's one origin
+  // in the |subscribers_| map. Returns true if the failure is handled.
+  bool NotifyFailure(FailureType failure_type);
 
  private:
   std::map<url::Origin, base::ObserverList<Subscriber>> subscribers_;
