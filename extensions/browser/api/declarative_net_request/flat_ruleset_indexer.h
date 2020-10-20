6ca21956c2cabb33a88e4d58516240eac0e1eed6
Karandeep Bhatia <karandeepb@chromium.org>
2020-10-20 19:10:23 +0000

    DNR: Modify FlatRulesetIndexer to release data buffer.
    FlatBufferBuilder now supports returning the released data buffer.
    Modify FlatRulesetIndexer to use it. This is more safer and ergonomical
    as the lifetime of the buffer is not tied to FlatRulesetIndexer now.
    
    BUG=1043200
    
    Change-Id: I983531ebd93820c9c28876c424302135e96651e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485841
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Auto-Submit: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819025}

--- extensions/browser/api/declarative_net_request/flat_ruleset_indexer.h
+++ extensions/browser/api/declarative_net_request/flat_ruleset_indexer.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <vector>
 
-#include "base/containers/span.h"
 #include "base/macros.h"
 #include "components/url_pattern_index/url_pattern_index.h"
 #include "extensions/browser/api/declarative_net_request/flat/extension_ruleset_generated.h"
@@ -36,12 +35,9 @@ class FlatRulesetIndexer {
   // Returns the number of rules added till now.
   size_t indexed_rules_count() const { return indexed_rules_count_; }
 
-  // Finishes the ruleset construction.
-  void Finish();
-
-  // Returns the data buffer, which is still owned by FlatRulesetIndexer.
-  // Finish() must be called prior to calling this.
-  base::span<const uint8_t> GetData();
+  // Finishes the ruleset construction and releases the underlying indexed data
+  // buffer.
+  flatbuffers::DetachedBuffer FinishAndReleaseBuffer();
 
  private:
   using UrlPatternIndexBuilder = url_pattern_index::UrlPatternIndexBuilder;
--- extensions/browser/api/declarative_net_request/ruleset_source.cc
+++ extensions/browser/api/declarative_net_request/ruleset_source.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/callback_helpers.h"
 #include "base/check_op.h"
+#include "base/containers/span.h"
 #include "base/feature_list.h"
 #include "base/files/file_util.h"
 #include "base/json/json_reader.h"
@@ -471,9 +472,9 @@ ParseInfo RulesetSource::IndexAndPersistRules(
       }
     }
   }
-  indexer.Finish();
-
-  if (!PersistIndexedRuleset(indexed_path_, indexer.GetData(),
+  flatbuffers::DetachedBuffer buffer = indexer.FinishAndReleaseBuffer();
+  if (!PersistIndexedRuleset(indexed_path_,
+                             base::make_span(buffer.data(), buffer.size()),
                              &ruleset_checksum)) {
     return ParseInfo(ParseResult::ERROR_PERSISTING_RULESET,
                      nullptr /* rule_id */);
