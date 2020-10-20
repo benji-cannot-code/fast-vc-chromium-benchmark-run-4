9535fb31d7e4d1fb97b3cbebda002b9cea254598
minch <minch@chromium.org>
2020-10-20 00:32:56 +0000

    dark_mode: Add colors for radio button.
    Bug: 1133748
    Change-Id: I33c8d0d97e20ba86943332d41a9ee68ae1fbf4be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2481463
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818722}

--- ash/style/ash_color_provider.h
+++ ash/style/ash_color_provider.h
@@ -116,7 +116,11 @@ class ASH_EXPORT AshColorProvider : public SessionObserver {
 
     // Color for slider.
     kSliderColorActive,
-    kSliderColorInactive
+    kSliderColorInactive,
+
+    // Color for radio button.
+    kRadioColorActive,
+    kRadioColorInactive,
   };
 
   // Types of ash styled buttons.
