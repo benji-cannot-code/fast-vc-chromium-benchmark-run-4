c0aaa50a891b63cf7f2c361caf1e5358c3cb11a6
Giovanni Panaro <tsrwebgl@gmail.com>
2020-10-20 23:33:51 +0000

    Fix Read-only GRDP strings in File Manager
    Bug: 1114103
    Change-Id: I9fdb97d06cadc5dde6672e9df7e2baca35ab0b2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2450711
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819154}

--- ui/file_manager/integration_tests/file_manager/files_tooltip.js
+++ ui/file_manager/integration_tests/file_manager/files_tooltip.js
@@ -106,8 +106,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     label =
         await remoteCall.waitForElement(appId, [tooltipQueryVisible, '#label']);
     chrome.test.assertEq(
-        'Files in this folder are read only.' +
-            ' Some activities are not supported.',
+        'The contents of this folder are read-only.' +
+        ' Some activities are not supported.',
         label.text);
     chrome.test.assertEq('card-tooltip', tooltip.attributes['class']);
     chrome.test.assertEq('card-label', label.attributes['class']);
