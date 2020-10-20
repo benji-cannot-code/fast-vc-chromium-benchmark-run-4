1b9010a8be60950c093d2eadd70f33a31aa26e6d
Toby Huang <tobyhuang@chromium.org>
2020-10-20 22:01:18 +0000

    [Metrics] Fix guest crash and restructure FamilyUserMetricsProvider
    FamilyUserMetricsProvider currently causes a crash in guest mode. This
    CL fixes the crash and categorizes guest users into the other bucket.
    
    This CL also restructures the FamilyUserMetricsProvider to an
    observer-based approach that improves stability and avoids unnecessary
    work by caching results.
    
    Bug: 1137352,1103077
    Change-Id: Ie421861970f6b26def1cf7390bd89480081bcab5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2476897
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819118}

--- chrome/browser/chromeos/child_accounts/family_features.cc
+++ chrome/browser/chromeos/child_accounts/family_features.cc
@@ -8,6 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 const base::Feature kFamilyUserMetricsProvider{
-    "FamilyUserMetricsProvider", base::FEATURE_DISABLED_BY_DEFAULT};
+    "FamilyUserMetricsProvider", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace chromeos
--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -145,6 +145,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_CHROMEOS)
+#include "base/feature_list.h"
+#include "chrome/browser/chromeos/child_accounts/family_features.h"
 #include "chrome/browser/chromeos/printing/printer_metrics_provider.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/metrics/ambient_mode_metrics_provider.h"
@@ -744,8 +746,10 @@ void ChromeMetricsServiceClient::RegisterMetricsServiceProviders() {
   metrics_service_->RegisterMetricsProvider(
       std::make_unique<AmbientModeMetricsProvider>());
 
-  metrics_service_->RegisterMetricsProvider(
-      std::make_unique<FamilyUserMetricsProvider>());
+  if (base::FeatureList::IsEnabled(chromeos::kFamilyUserMetricsProvider)) {
+    metrics_service_->RegisterMetricsProvider(
+        std::make_unique<FamilyUserMetricsProvider>());
+  }
 #endif  // defined(OS_CHROMEOS)
 
 #if !defined(OS_CHROMEOS)
