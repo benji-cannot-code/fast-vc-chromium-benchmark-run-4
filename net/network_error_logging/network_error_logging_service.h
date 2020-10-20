bbe697754b562bde1a15e5715cab2f2b7e8bc7d8
Matt Menke <mmenke@chromium.org>
2020-10-20 19:59:16 +0000

    Apply NIK to reporting and NEL, part 1.
    Add NIK to the on-disk reporting and NEL information.
    
    Bug: 993805
    Change-Id: I27b1242458e48423b7615effcee3de5afda916e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485253
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819056}

--- net/network_error_logging/network_error_logging_service.h
+++ net/network_error_logging/network_error_logging_service.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/ip_address.h"
 #include "net/base/net_errors.h"
 #include "net/base/net_export.h"
+#include "net/base/network_isolation_key.h"
 #include "url/gurl.h"
 #include "url/origin.h"
 
@@ -49,6 +50,10 @@ class NET_EXPORT NetworkErrorLoggingService {
     NelPolicy();
     NelPolicy(const NelPolicy& other);
     ~NelPolicy();
+
+    // TODO(mmenke): Actually populate this field. Currently only used by the
+    // persistence layer.
+    NetworkIsolationKey network_isolation_key = NetworkIsolationKey::Todo();
 
     url::Origin origin;
     IPAddress received_ip_address = IPAddress();
