e4d0a2c8eb86a9b83bad2096144f55942d17a27b
Arthur Sonzogni <arthursonzogni@chromium.org>
2020-10-20 09:20:39 +0000

    Revert "[TTS] Fix a disabled test (OOP issue)"
    This reverts commit 6132920801f3539d1b4f6074d48fea3e3e7378ec.
    
    Reason for revert: https://crbug.com/1140413
    
    Original change's description:
    > [TTS] Fix a disabled test (OOP issue)
    >
    > Speculate fix for testLivePrefetchFailoverRequestMadeAfterOpen
    > which was failing with out-of-process network service error.
    >
    > BUG=1071721
    >
    > Change-Id: I95cc3bba5ffd17e38b58fc88a3ff227045fe9feb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2486072
    > Commit-Queue: Sinan Sahin <sinansahin@google.com>
    > Reviewed-by: Sinan Sahin <sinansahin@google.com>
    > Auto-Submit: Donn Denman <donnd@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#818664}
    
    TBR=donnd@chromium.org,sinansahin@google.com,jds@google.com
    
    Change-Id: I1e90b5f194fce483f3440fe125c0fd6f4c492015
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1071721
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485107
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818833}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -1579,6 +1579,9 @@ public class ContextualSearchManagerTest {
     @ParameterAnnotations.UseMethodParameter(FeatureParamProvider.class)
     public void testLivePrefetchFailoverRequestMadeAfterOpen(@EnabledFeature int enabledFeature)
             throws Exception {
+        // Test fails with out-of-process network service. crbug.com/1071721
+        if (!ChromeFeatureList.isEnabled("NetworkServiceInProcess")) return;
+
         mFakeServer.reset();
         mFakeServer.setLowPriorityPathInvalid();
         simulateResolveSearch("search");
