584f3028d0a093cd900fe3d5c57ebe5e090a2616
Dom Schulz <dominicschulz@google.com>
2020-10-20 19:25:39 +0000

    Port OSSettings to non-manifest install
    Delete the pwa.html and os-settings-manifest.json files, and install via
    a WebApplicationInfo.
    
    Bug: 1085274
    Bug: 1126752
    Change-Id: Ie269b46771daa7f302821491735e42b53e7bfc2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2475310
    Commit-Queue: Dominic Schulz <dominicschulz@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Auto-Submit: Dominic Schulz <dominicschulz@google.com>
    Cr-Commit-Position: refs/heads/master@{#819034}

--- chrome/browser/ui/webui/settings/chromeos/main_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/main_section.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/os_settings_resources.h"
-#include "chromeos/components/web_applications/manifest_request_filter.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/user_manager/user_manager.h"
@@ -174,9 +173,6 @@ void MainSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
 
   // Add the System Web App resources for Settings.
   html_source->AddResourcePath("icon-192.png", IDR_SETTINGS_LOGO_192);
-  html_source->AddResourcePath("pwa.html", IDR_PWA_HTML);
-  web_app::SetManifestRequestFilter(html_source, IDR_OS_SETTINGS_MANIFEST,
-                                    IDS_SETTINGS_SETTINGS);
 
   html_source->AddResourcePath("constants/routes.mojom-lite.js",
                                IDR_OS_SETTINGS_ROUTES_MOJOM_LITE_JS);
