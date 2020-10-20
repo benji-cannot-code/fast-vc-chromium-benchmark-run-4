7f5141e365136ca6c4a538e9469c2a454ec48210
Rune Lillesveen <futhark@chromium.org>
2020-10-20 06:33:31 +0000

    ScheduleLayoutTreeUpdate for dirty slot assignments.
    We did not schedule a visual update for dirty slot assignments. If
    adding a host child is the only thing that happened for a frame, we
    would not run the lifecycle update.
    
    Bug: 1137711
    Change-Id: Icd3fe88999129e56c7c6eaa5174f4a89ac4df6c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485099
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818804}

--- third_party/blink/renderer/core/dom/slot_assignment.cc
+++ third_party/blink/renderer/core/dom/slot_assignment.cc
@@ -233,6 +233,7 @@ void SlotAssignment::SetNeedsAssignmentRecalc() {
   if (owner_->isConnected()) {
     owner_->GetDocument().GetSlotAssignmentEngine().AddShadowRootNeedingRecalc(
         *owner_);
+    owner_->GetDocument().ScheduleLayoutTreeUpdateIfNeeded();
   }
 }
 
--- third_party/blink/renderer/core/dom/slot_assignment_test.cc
+++ third_party/blink/renderer/core/dom/slot_assignment_test.cc
@@ -12,8 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/node_traversal.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/dom/text.h"
+#include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/html/html_collection.h"
-#include "third_party/blink/renderer/core/html/html_element.h"
+#include "third_party/blink/renderer/core/html/html_div_element.h"
 #include "third_party/blink/renderer/core/html/html_slot_element.h"
 #include "third_party/blink/renderer/core/testing/dummy_page_holder.h"
 #include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
@@ -163,6 +164,25 @@ TEST_F(SlotAssignmentTest, AssignedNodesAreSet) {
   HeapVector<Member<Node>> expected_nodes;
   expected_nodes.push_back(host_child);
   EXPECT_EQ(expected_nodes, slot->AssignedNodes());
+}
+
+TEST_F(SlotAssignmentTest, ScheduleVisualUpdate) {
+  SetBody(R"HTML(
+    <div id="host">
+      <shadowroot>
+        <slot></slot>
+      </shadowroot>
+      <div></div>
+    </div>
+  )HTML");
+
+  GetDocument().View()->UpdateAllLifecyclePhases(DocumentUpdateReason::kTest);
+  GetDocument().View()->RunPostLifecycleSteps();
+
+  auto* div = MakeGarbageCollected<HTMLDivElement>(GetDocument());
+  GetDocument().getElementById("host")->appendChild(div);
+  EXPECT_EQ(DocumentLifecycle::kVisualUpdatePending,
+            GetDocument().Lifecycle().GetState());
 }
 
 }  // namespace blink
