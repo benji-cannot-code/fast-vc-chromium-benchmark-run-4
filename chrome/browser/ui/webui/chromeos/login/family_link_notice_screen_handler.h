937d75b9b944ff60f00ceeeab1f8384685adbe38
Denis Kuznetsov <antrim@google.com>
2020-10-20 11:57:56 +0000

    Change comment code reference style from || to `` in OOBE WebUI handlers
    https://chromium.googlesource.com/chromium/src/+/master/styleguide/c++/c++-dos-and-donts.md#comment-style
    
    Bug: None
    Change-Id: If445946f56871e7cf0e5750f3314df6a83a9e10b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2484659
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Auto-Submit: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/master@{#818865}

--- chrome/browser/ui/webui/chromeos/login/family_link_notice_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/family_link_notice_screen_handler.h
@@ -25,7 +25,7 @@ class FamilyLinkNoticeView {
   // Shows the contents of the screen.
   virtual void Show() = 0;
 
-  // Binds |screen| to the view.
+  // Binds `screen` to the view.
   virtual void Bind(FamilyLinkNoticeScreen* screen) = 0;
 
   // Unbinds the screen from the view.
