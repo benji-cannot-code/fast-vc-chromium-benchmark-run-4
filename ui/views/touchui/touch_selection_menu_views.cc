1ab8311823a71346f3c434d1ee298a1417028a03
Peter Kasting <pkasting@chromium.org>
2020-10-20 19:15:54 +0000

    Change ButtonPressed overrides to callbacks: ui/views/touchui/
    Bug: 772945
    Change-Id: Ibfb794373df3f30e88c645ae7678cd3ccb3f9fdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485040
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819029}

--- ui/views/touchui/touch_selection_menu_views.cc
+++ ui/views/touchui/touch_selection_menu_views.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/touchui/touch_selection_menu_views.h"
 
 #include <memory>
+#include <utility>
 
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -124,27 +125,33 @@ TouchSelectionMenuViews::~TouchSelectionMenuViews() = default;
 
 void TouchSelectionMenuViews::CreateButtons() {
   for (const auto& command : kMenuCommands) {
-    if (!client_->IsCommandIdEnabled(command.command_id))
-      continue;
-
-    Button* button =
-        CreateButton(l10n_util::GetStringUTF16(command.message_id));
-    button->set_tag(command.command_id);
-    AddChildView(button);
+    if (client_->IsCommandIdEnabled(command.command_id)) {
+      CreateButton(
+          l10n_util::GetStringUTF16(command.message_id),
+          base::BindRepeating(&TouchSelectionMenuViews::ButtonPressed,
+                              base::Unretained(this), command.command_id));
+    }
   }
 
   // Finally, add ellipsis button.
-  LabelButton* ellipsis_button = CreateButton(base::ASCIIToUTF16("..."));
-  ellipsis_button->SetID(ButtonViewId::kEllipsisButton);
-  AddChildView(ellipsis_button);
+  CreateButton(base::ASCIIToUTF16("..."),
+               base::BindRepeating(
+                   [](TouchSelectionMenuViews* menu) {
+                     menu->CloseMenu();
+                     menu->client_->RunContextMenu();
+                   },
+                   base::Unretained(this)))
+      ->SetID(ButtonViewId::kEllipsisButton);
   InvalidateLayout();
 }
 
 LabelButton* TouchSelectionMenuViews::CreateButton(
-    const base::string16& title) {
+    const base::string16& title,
+    Button::PressedCallback callback) {
   base::string16 label =
       gfx::RemoveAcceleratorChar(title, '&', nullptr, nullptr);
-  LabelButton* button = new LabelButton(this, label, style::CONTEXT_TOUCH_MENU);
+  auto* button = AddChildView(std::make_unique<LabelButton>(
+      std::move(callback), label, style::CONTEXT_TOUCH_MENU));
   constexpr gfx::Size kMenuButtonMinSize = gfx::Size(63, 38);
   button->SetMinSize(kMenuButtonMinSize);
   button->SetFocusForPlatform();
@@ -180,13 +187,10 @@ void TouchSelectionMenuViews::WindowClosing() {
     DisconnectOwner();
 }
 
-void TouchSelectionMenuViews::ButtonPressed(Button* sender,
+void TouchSelectionMenuViews::ButtonPressed(int command,
                                             const ui::Event& event) {
   CloseMenu();
-  if (sender->GetID() != ButtonViewId::kEllipsisButton)
-    client_->ExecuteCommand(sender->tag(), event.flags());
-  else
-    client_->RunContextMenu();
+  client_->ExecuteCommand(command, event.flags());
 }
 
 BEGIN_METADATA(TouchSelectionMenuViews, BubbleDialogDelegateView)
