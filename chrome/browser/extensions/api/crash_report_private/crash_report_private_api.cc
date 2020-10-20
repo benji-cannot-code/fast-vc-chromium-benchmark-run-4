d0a962bf908bf196cb488cb4d31dacdedb61907a
David <dlei@google.com>
2020-10-20 21:07:23 +0000

    Migrate crashReportPrivate to chrome/browser/extensions/api.
    This is part of a series of changes to improve crash reporting for
    chrome://media-app (& incidentally other clients of crashReportPrivate).
    
    Design doc: go/backlight-better-crash-report
    
    We need chrome/ dependencies for
    * determining chrome window type (regular, app, SWA).
    * solid testing for opening & closing devtools
    https://chromium-review.googlesource.com/c/chromium/src/+/2465407/comment/90edd371_2e37132d/
    (that cl will be rebased on this)
    And once this is ported, it also makes sense to
    * grab app locale from the browser process
    * grab renderer uptime with a chrome dep
    
    This also ports crashReportPrivate tests from extensions_browsertests
    to browser_tests.
    
    Change-Id: I4bc51f0cb69927ffb07d8045a82b180ed308590b
    Bug: b/169635499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438099
    Auto-Submit: David Lei <dlei@google.com>
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: David Lei <dlei@google.com>
    Cr-Commit-Position: refs/heads/master@{#819088}

--- chrome/browser/extensions/api/crash_report_private/crash_report_private_api.cc
+++ chrome/browser/extensions/api/crash_report_private/crash_report_private_api.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "extensions/browser/api/crash_report_private/crash_report_private_api.h"
+#include "chrome/browser/extensions/api/crash_report_private/crash_report_private_api.h"
 
 #include "base/time/default_clock.h"
 #include "components/crash/content/browser/error_reporting/javascript_error_report.h"
--- chrome/browser/extensions/api/crash_report_private/crash_report_private_api.h
+++ chrome/browser/extensions/api/crash_report_private/crash_report_private_api.h
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef EXTENSIONS_BROWSER_API_CRASH_REPORT_PRIVATE_CRASH_REPORT_PRIVATE_API_H_
-#define EXTENSIONS_BROWSER_API_CRASH_REPORT_PRIVATE_CRASH_REPORT_PRIVATE_API_H_
+#ifndef CHROME_BROWSER_EXTENSIONS_API_CRASH_REPORT_PRIVATE_CRASH_REPORT_PRIVATE_API_H_
+#define CHROME_BROWSER_EXTENSIONS_API_CRASH_REPORT_PRIVATE_CRASH_REPORT_PRIVATE_API_H_
 
 #include <string>
 
+#include "chrome/common/extensions/api/crash_report_private.h"
 #include "extensions/browser/extension_function.h"
 #include "extensions/browser/extension_function_histogram_value.h"
-#include "extensions/common/api/crash_report_private.h"
 
 namespace base {
 class Clock;
@@ -40,4 +40,4 @@ void SetClockForTesting(base::Clock* clock);
 }  // namespace api
 }  // namespace extensions
 
-#endif  // EXTENSIONS_BROWSER_API_CRASH_REPORT_PRIVATE_CRASH_REPORT_PRIVATE_API_H_
+#endif  // CHROME_BROWSER_EXTENSIONS_API_CRASH_REPORT_PRIVATE_CRASH_REPORT_PRIVATE_API_H_
