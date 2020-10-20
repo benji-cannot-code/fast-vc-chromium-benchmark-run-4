eab5801845c6267c7f3bebbbcf068cb5d900997d
minch <minch@chromium.org>
2020-10-20 00:31:42 +0000

    dark_mode: Live update colors of overview.
    Bug: 1131543
    Change-Id: I196eb050b6b6a91dba4da76ee9915a2011f1b57e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2480802
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818721}

--- ash/wm/window_mini_view.h
+++ ash/wm/window_mini_view.h
@@ -78,6 +78,7 @@ class ASH_EXPORT WindowMiniView : public views::View,
   // views::View:
   void Layout() override;
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
+  void OnThemeChanged() override;
 
   // aura::WindowObserver:
   void OnWindowPropertyChanged(aura::Window* window,
