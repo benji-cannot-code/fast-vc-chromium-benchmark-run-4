42f2a13efba84e34ef11c9fc709b80fdd1025e90
Jan Krcal <jkrcal@chromium.org>
2020-10-20 06:02:04 +0000

    [Profile creation] Handle errors for sync consent
    This CL adds error handling for the sync consent in signed-in profile
    creation flow. All the special cases are handled by closing the creation
    flow, opening a browser window for the new profile and showing standard
    error dialogs in the actual browser window.
    
    The only case left for a later CL is when sync is disabled by the admin.
    Namely, this case needs more refactoring which will be easier to
    understand in a standalone CL.
    
    Bug: 1126913
    Change-Id: I604d03ea101219f69bb6b2a20b311de4fb75366d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2435349
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#818796}

--- chrome/browser/ui/views/profiles/profile_picker_view_sync_delegate.h
+++ chrome/browser/ui/views/profiles/profile_picker_view_sync_delegate.h
@@ -19,8 +19,7 @@ class ProfilePickerViewSyncDelegate : public DiceTurnSyncOnHelper::Delegate,
                                       public LoginUIService::Observer {
  public:
   using OpenBrowserCallback =
-      base::OnceCallback<void(Profile*,
-                              ProfilePickerView::BrowserOpenedCallback)>;
+      base::OnceCallback<void(ProfilePickerView::BrowserOpenedCallback)>;
 
   ProfilePickerViewSyncDelegate(Profile* profile,
                                 OpenBrowserCallback open_browser_callback);
@@ -48,6 +47,7 @@ class ProfilePickerViewSyncDelegate : public DiceTurnSyncOnHelper::Delegate,
       LoginUIService::SyncConfirmationUIClosedResult result) override;
 
   Profile* profile_;
+  bool enterprise_confirmation_shown_ = false;
   OpenBrowserCallback open_browser_callback_;
   base::OnceCallback<void(LoginUIService::SyncConfirmationUIClosedResult)>
       sync_confirmation_callback_;
