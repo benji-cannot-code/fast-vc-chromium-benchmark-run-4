edd1b3cd0b5277d5ef0ff558aba88c24d2cee24e
Rachel Wong <wrong@chromium.org>
2020-10-21 14:46:01 +0000

    Reland "Enable Suggested Content by default."
    This is a reland of be611a0e289e06672767c4a56bfff98c584cab71.
    
    The |selected_action| member variable was sometimes uninitialized before
    being accessed, causing msan failures. This reland initializes the
    variable.
    
    Original change's description:
    > Enable Suggested Content by default.
    >
    > The Suggested Content toggle has been approved in
    > https://bugs.chromium.org/p/chromium/issues/detail?id=990279#c148. This
    > CL enables the feature by default, and updates tests that need to take
    > this into account.
    >
    > Bug: 990279
    > Change-Id: Icc2c8bdf72cff8734aa5941f3fbc2774b38e4c98
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462752
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Jia Meng <jiameng@chromium.org>
    > Commit-Queue: Rachel Wong <wrong@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#818758}
    
    Bug: 990279
    Change-Id: I17ac908b10fef847ed02c0a08980417c1d1998c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2488261
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819365}

--- ash/app_list/app_list_presenter_delegate_unittest.cc
+++ ash/app_list/app_list_presenter_delegate_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/views/apps_grid_view.h"
 #include "ash/app_list/views/contents_view.h"
 #include "ash/app_list/views/expand_arrow_view.h"
+#include "ash/app_list/views/privacy_container_view.h"
 #include "ash/app_list/views/search_box_view.h"
 #include "ash/app_list/views/search_result_actions_view.h"
 #include "ash/app_list/views/search_result_base_view.h"
@@ -467,6 +468,16 @@ TEST_F(AppListPresenterDelegateTest, ClickSearchBoxInTabletMode) {
 
 TEST_F(AppListPresenterDelegateTest, RemoveSuggestionShowsConfirmDialog) {
   ShowZeroStateSearchInHalfState();
+
+  // Mark the privacy notices as dismissed so that they do not interfere with
+  // the layout.
+  Shell::Get()->app_list_controller()->MarkAssistantPrivacyInfoDismissed();
+  Shell::Get()->app_list_controller()->MarkSuggestedContentInfoDismissed();
+  GetAppListView()
+      ->app_list_main_view()
+      ->contents_view()
+      ->privacy_container_view()
+      ->Update();
 
   // Add a zero state suggestion results - the result that will be tested is in
   // the second place.
