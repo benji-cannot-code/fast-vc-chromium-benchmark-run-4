1a4e0faeb453a8d0a17c788bb0b2baf95eeeb428
Jonathan Backer <backer@chromium.org>
2020-10-21 15:07:52 +0000

    Use NV_robustness_video_memory_purge
    Tested by hand on gLapstation with GLX and ANGLE backends. GLX works as
    intended. ANGLE tickles a driver bug that causes a GL_OUT_OF_MEMORY error and
    followed by a driver crash on glDrawElements. This forces a redraw of
    everything so is a better user experience than just showing corrupted
    textures.
    
    Bug: 1113040
    Change-Id: I404441e1c39541aa925931c15364b4d1ee6761b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2456190
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819367}

--- ui/gl/gl_context_glx.cc
+++ ui/gl/gl_context_glx.cc
@@ -18,6 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_implementation.h"
 #include "ui/gl/gl_surface_glx.h"
 
+#ifndef GLX_NV_robustness_video_memory_purge
+#define GLX_NV_robustness_video_memory_purge 1
+#define GLX_GENERATE_RESET_ON_VIDEO_MEMORY_PURGE_NV 0x20F7
+#endif /* GLX_NV_robustness_video_memory_purge */
+
 namespace gl {
 
 namespace {
@@ -34,6 +39,11 @@ GLXContext CreateContextAttribs(x11::Connection* connection,
   if (GLSurfaceGLX::IsCreateContextRobustnessSupported()) {
     attribs.push_back(GLX_CONTEXT_RESET_NOTIFICATION_STRATEGY_ARB);
     attribs.push_back(GLX_LOSE_CONTEXT_ON_RESET_ARB);
+
+    if (GLSurfaceGLX::IsRobustnessVideoMemoryPurgeSupported()) {
+      attribs.push_back(GLX_GENERATE_RESET_ON_VIDEO_MEMORY_PURGE_NV);
+      attribs.push_back(GL_TRUE);
+    }
   }
 
   if (version.first != 0 || version.second != 0) {
--- ui/gl/gl_surface_glx.cc
+++ ui/gl/gl_surface_glx.cc
@@ -50,6 +50,7 @@ namespace {
 
 bool g_glx_context_create = false;
 bool g_glx_create_context_robustness_supported = false;
+bool g_glx_robustness_video_memory_purge_supported = false;
 bool g_glx_create_context_profile_supported = false;
 bool g_glx_create_context_profile_es2_supported = false;
 bool g_glx_texture_from_pixmap_supported = false;
@@ -519,6 +520,8 @@ bool GLSurfaceGLX::InitializeExtensionSettingsOneOff() {
   g_glx_context_create = HasGLXExtension("GLX_ARB_create_context");
   g_glx_create_context_robustness_supported =
       HasGLXExtension("GLX_ARB_create_context_robustness");
+  g_glx_robustness_video_memory_purge_supported =
+      HasGLXExtension("GLX_NV_robustness_video_memory_purge");
   g_glx_create_context_profile_supported =
       HasGLXExtension("GLX_ARB_create_context_profile");
   g_glx_create_context_profile_es2_supported =
@@ -543,6 +546,7 @@ void GLSurfaceGLX::ShutdownOneOff() {
   initialized_ = false;
   g_glx_context_create = false;
   g_glx_create_context_robustness_supported = false;
+  g_glx_robustness_video_memory_purge_supported = false;
   g_glx_create_context_profile_supported = false;
   g_glx_create_context_profile_es2_supported = false;
   g_glx_texture_from_pixmap_supported = false;
@@ -591,6 +595,11 @@ bool GLSurfaceGLX::IsCreateContextSupported() {
 // static
 bool GLSurfaceGLX::IsCreateContextRobustnessSupported() {
   return g_glx_create_context_robustness_supported;
+}
+
+// static
+bool GLSurfaceGLX::IsRobustnessVideoMemoryPurgeSupported() {
+  return g_glx_robustness_video_memory_purge_supported;
 }
 
 // static
