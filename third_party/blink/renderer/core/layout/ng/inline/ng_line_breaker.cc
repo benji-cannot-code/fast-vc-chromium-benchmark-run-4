8f7b5ddec59a652afa97bfca2c2e89f1d1e0bda9
Koji Ishii <kojii@chromium.org>
2020-10-21 21:58:07 +0000

    Inline NGLineBreaker::HandleText
    This patch inlines |NGLineBreaker::HandleText|.
    
    This is a very small function with "inline", but while
    investigating disassembly for an issue, it turns out that this
    function is not inlined, and has |__security_check_cookie|.
    This patch inlines it to avoid the cost.
    
    This patch has no behavior changes.
    
    Change-Id: I8d796a31fb952afc99c0caab427b96b4c503a9ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490336
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819531}

--- third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.cc
@@ -522,7 +522,7 @@ void NGLineBreaker::BreakLine(
     const NGInlineItem& item = items[item_index_];
     if (item.Type() == NGInlineItem::kText) {
       if (item.Length())
-        HandleText(item, line_info);
+        HandleText(item, *item.TextShapeResult(), line_info);
       else
         HandleEmptyText(item, line_info);
 #if DCHECK_IS_ON()
@@ -634,6 +634,7 @@ void NGLineBreaker::HandleText(const NGInlineItem& item,
   DCHECK(item.Type() == NGInlineItem::kText ||
          (item.Type() == NGInlineItem::kControl &&
           Text()[item.StartOffset()] == kTabulationCharacter));
+  DCHECK(&shape_result);
   DCHECK_EQ(auto_wrap_, item.Style()->AutoWrap());
 
   // If we're trailing, only trailing spaces can be included in this line.
