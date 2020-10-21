eca6bedccb513eb4b469a6e3cc7ad1f2038feaa9
Ghazale Hosseinabadi <ghazale@google.com>
2020-10-21 16:42:12 +0000

    Remove flaky test for WINDOWS_GETCURRENT API
    This CL removes WINDOWS_GETCURRENT API test coverage from
    ServiceWorkerBasedBackgroundTest. The reason is 1)The test is flaky, 2)The API test coverage is already added in NonPersistentExtensionTabsTest.TabCurrentWindow.
    
    Bug: 1140254
    Change-Id: I90d93a27d3c3f420b2a12f17a38503850f33bf79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2487868
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819399}

--- chrome/test/data/extensions/api_test/service_worker/worker_based_background/windows_basic/service_worker_background.js
+++ chrome/test/data/extensions/api_test/service_worker/worker_based_background/windows_basic/service_worker_background.js
@@ -28,14 +28,6 @@ var getWindowUtil = function(windowId, getCallback) {
   }
 }
 
-var getCurrentWindowUtil = function(getCurrentCallback) {
-  try {
-    chrome.windows.getCurrent(getCurrentCallback);
-  } catch (e) {
-    chrome.test.fail(e);
-  }
-}
-
 chrome.test.runTests([
   // Get the window that was automatically created.
   function testWindowGetAllBeforeCreate() {
@@ -64,12 +56,6 @@ chrome.test.runTests([
         chrome.test.assertEq(400, windowData.height);
         chrome.test.succeed();
       });
-      // Check that the created window is the current window.
-      getCurrentWindowUtil(function(currentWindowData) {
-        chrome.test.assertEq(createdWindowId, currentWindowData.id);
-        chrome.test.succeed();
-      });
-      chrome.test.succeed();
     });
   },
   ]);
