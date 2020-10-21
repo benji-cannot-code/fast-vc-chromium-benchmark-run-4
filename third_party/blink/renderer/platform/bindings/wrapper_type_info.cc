89f8177f65e11a428b522c7314c8556f75db45f6
Yuki Shiino <yukishiino@chromium.org>
2020-10-21 00:54:47 +0000

    v8bindings: Refactor WrapperTypeInfo and related code
    In order to support IDL namespaces, refactors WrapperTypeInfo
    and related code.
    
    - WrapperTypeInfo::GetV8ClassTemplate replaces old DomTemplate
      member function.
    - WrapperTypeInfo::idl_definition_kind distinguishes IDL
      interface from IDL namespace.
    - Installer functions take v8::Template and v8::Object because
      an IDL namespace is not a JS function.
    - bindings::SetupIDLInterfaceTemplate in generated_code_helper.h
      replaces old V8DOMConfiguration::InitializeDOMInterfaceTemplate.
      (V8DOMConfiguration is no longer used in the code base.)
    
    Since we no longer use adapter functions for WTI::DomTemplate,
    the APK size will be reduced by 10 KB.
    
    Bug: 839389
    Change-Id: I0adb59372adbfa6f4611009324b399c5249ba249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2464515
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819182}

--- third_party/blink/renderer/platform/bindings/wrapper_type_info.cc
+++ third_party/blink/renderer/platform/bindings/wrapper_type_info.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/bindings/custom_wrappable.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
-#include "third_party/blink/renderer/platform/heap/handle.h"
-#include "third_party/blink/renderer/platform/heap/heap_stats_collector.h"
+#include "third_party/blink/renderer/platform/bindings/v8_object_constructor.h"
+#include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
 
 namespace blink {
 
@@ -16,6 +16,39 @@ static_assert(offsetof(struct WrapperTypeInfo, gin_embedder) ==
                   offsetof(struct gin::WrapperInfo, embedder),
               "offset of WrapperTypeInfo.ginEmbedder must be the same as "
               "gin::WrapperInfo.embedder");
+
+v8::Local<v8::Template> WrapperTypeInfo::GetV8ClassTemplate(
+    v8::Isolate* isolate,
+    const DOMWrapperWorld& world) const {
+  V8PerIsolateData* per_isolate_data = V8PerIsolateData::From(isolate);
+  v8::Local<v8::Template> v8_template =
+      per_isolate_data->FindV8Template(world, this);
+  if (!v8_template.IsEmpty())
+    return v8_template;
+
+  switch (idl_definition_kind) {
+    case kIdlInterface:
+      v8_template = v8::FunctionTemplate::New(
+          isolate, V8ObjectConstructor::IsValidConstructorMode);
+      break;
+    case kIdlNamespace:
+      v8_template = v8::ObjectTemplate::New(isolate);
+      break;
+    case kIdlCallbackInterface:
+      v8_template = v8::FunctionTemplate::New(
+          isolate, V8ObjectConstructor::IsValidConstructorMode);
+      break;
+    case kCustomWrappableKind:
+      v8_template = v8::FunctionTemplate::New(isolate);
+      break;
+    default:
+      NOTREACHED();
+  }
+  install_interface_template_func(isolate, world, v8_template);
+
+  per_isolate_data->AddV8Template(world, this, v8_template);
+  return v8_template;
+}
 
 void WrapperTypeInfo::Trace(Visitor* visitor, const void* impl) const {
   switch (wrapper_class_id) {
