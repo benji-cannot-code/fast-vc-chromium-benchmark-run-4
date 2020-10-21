e7f6ea1ddad0924ff2ae7a43c42d20ca7ab5d9d8
dpapad <dpapad@chromium.org>
2020-10-21 20:00:17 +0000

    Put chrome://usb-internals UI under a top-level WebComponent.
    This is done so that the UI can be programmatically instantiated
    from within a test, with
    document.createElement('usb-internals-app');
    
    in preparation of migrating to JS modules, where during tests,
    test_loader.html is loaded instead of the original landing HTML
    page.
    
    Bug: 1139849
    Change-Id: I919179150385e906665746afbfc6ce0aa2480498
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2486963
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819490}

--- chrome/browser/resources/usb_internals/app.js
+++ chrome/browser/resources/usb_internals/app.js
@@ -6,11 +6,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * Javascript for usb_internals.html, served from chrome://usb-internals/.
  */
-cr.define('usb_internals', function() {
-  class UsbInternals {
-    constructor() {}
 
-    async initializeViews() {
+window.setupFn = window.setupFn || function() {
+  return Promise.resolve();
+};
+
+  class UsbInternalsAppElement extends HTMLElement {
+    static get template() {
+      return `{__html_template__}`;
+    }
+
+    constructor() {
+      super();
+
+      this.attachShadow({mode: 'open'});
+      const template = document.createElement('template');
+      template.innerHTML = this.constructor.template || '';
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    /**
+     * @param {string} query
+     * @return {?Element}
+     */
+    $(query) {
+      return this.shadowRoot.querySelector(query);
+    }
+
+    async connectedCallback() {
       // window.setupFn() provides a hook for the test suite to perform setup
       // actions after the page is loaded but before any script is run.
       await window.setupFn();
@@ -25,28 +48,29 @@ cr.define('usb_internals', function() {
           usbManager.$.bindNewPipeAndPassReceiver());
 
       /** @private {devices_page.DevicesPage} */
-      this.devicesPage_ = new devices_page.DevicesPage(usbManager);
+      this.devicesPage_ = new devices_page.DevicesPage(
+          usbManager, assert(this.shadowRoot));
 
       /** @private {device.mojom.UsbDeviceManagerTestRemote} */
       this.usbManagerTest_ = new device.mojom.UsbDeviceManagerTestRemote;
       await pageHandler.bindTestInterface(
           this.usbManagerTest_.$.bindNewPipeAndPassReceiver());
 
-      $('add-test-device-form').addEventListener('submit', (event) => {
+      this.$('#add-test-device-form').addEventListener('submit', (event) => {
         this.addTestDevice(event);
       });
       this.refreshTestDeviceList();
 
-      cr.ui.decorate('tabbox', cr.ui.TabBox);
+      cr.ui.decorate(assert(this.$('tabbox')), cr.ui.TabBox);
     }
 
     async refreshTestDeviceList() {
       const response = await this.usbManagerTest_.getTestDevices();
 
-      const tableBody = $('test-device-list');
+      const tableBody = this.$('#test-device-list');
       tableBody.innerHTML = trustedTypes.emptyHTML;
 
-      const rowTemplate = document.querySelector('#test-device-row');
+      const rowTemplate = this.$('#test-device-row');
       const td = rowTemplate.content.querySelectorAll('td');
 
       for (const device of response.devices) {
@@ -70,28 +94,16 @@ cr.define('usb_internals', function() {
       event.preventDefault();
 
       const response = await this.usbManagerTest_.addDeviceForTesting(
-          $('test-device-name').value, $('test-device-serial').value,
-          $('test-device-landing-page').value);
+          this.$('#test-device-name').value, this.$('#test-device-serial').value,
+          this.$('#test-device-landing-page').value);
       if (response.success) {
         this.refreshTestDeviceList();
       }
 
-      $('add-test-device-result').textContent = response.message;
-      $('add-test-device-result').className =
+      this.$('#add-test-device-result').textContent = response.message;
+      this.$('#add-test-device-result').className =
           response.success ? 'action-success' : 'action-failure';
     }
   }
+  customElements.define('usb-internals-app', UsbInternalsAppElement);
 
-  return {
-    UsbInternals,
-  };
-});
-
-window.setupFn = window.setupFn || function() {
-  return Promise.resolve();
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-  const usbInternalsPage = new usb_internals.UsbInternals();
-  usbInternalsPage.initializeViews();
-});
