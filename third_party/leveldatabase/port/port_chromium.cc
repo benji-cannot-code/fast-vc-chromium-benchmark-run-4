e3b106912d496b77d69ad3b12c16e38a3e4aa594
Victor Costan <pwnall@chromium.org>
2020-10-21 19:43:09 +0000

    leveldb: Migrate leveldb::Port back to //base primitives.
    Bug: 1140147
    Change-Id: Iccfaf6221f791673f84052b3916ced02cfd677ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2489324
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819479}

--- third_party/leveldatabase/port/port_chromium.cc
+++ third_party/leveldatabase/port/port_chromium.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "port/port_chromium.h"
 
+#include <cstddef>
+#include <cstdint>
 #include <string>
 
 #include "third_party/crc32c/src/include/crc32c/crc32c.h"
