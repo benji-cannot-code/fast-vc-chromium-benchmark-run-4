550d062c1bfc83cfec7fc5c633b030d1b4e6bc08
Owen Min <zmin@chromium.org>
2020-10-21 18:19:48 +0000

    Generate profile report with extension requests only.
    Extension request only report only contains
    - profile id
    - extension request.
    But not
    - extension list
    - policies info
    
    Profile name and sign in user info only added on CrOS.
    
    Bug: 1137964
    Change-Id: I76bc72acde86f149ac090d839d159ddb56b6bb6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485276
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819442}

--- components/enterprise/browser/reporting/profile_report_generator.h
+++ components/enterprise/browser/reporting/profile_report_generator.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/values.h"
+#include "components/enterprise/browser/reporting/report_request_definition.h"
+#include "components/enterprise/browser/reporting/report_type.h"
 #include "components/policy/core/browser/policy_conversions_client.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 
@@ -73,7 +75,8 @@ class ProfileReportGenerator {
   // generated.
   std::unique_ptr<enterprise_management::ChromeUserProfileInfo> MaybeGenerate(
       const base::FilePath& path,
-      const std::string& name);
+      const std::string& name,
+      ReportType report_type = ReportType::kFull);
 
  protected:
   void GetChromePolicyInfo();
