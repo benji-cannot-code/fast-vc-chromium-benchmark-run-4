5b2a09ca104087f1083bb020e851ca2af84cfb86
Alice Wang <aliceywang@chromium.org>
2020-10-21 13:28:46 +0000

    [Sheriff] Disable testShowCastOnDocumentElementInScrolledBrowserWindow on tablet
    TBR=sandromaggi@google.com
    
    Bug: 1140957
    Change-Id: Ibc27ca9632d9bba356fc4c628a49bd51034b3a33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2489623
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819349}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantOverlayIntegrationTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantOverlayIntegrationTest.java
@@ -35,6 +35,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.autofill_assistant.proto.ActionProto;
 import org.chromium.chrome.browser.autofill_assistant.proto.BitmapDrawableProto;
@@ -58,6 +59,7 @@ import org.chromium.chrome.browser.customtabs.CustomTabsTestUtils;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.content_public.browser.WebContents;
+import org.chromium.ui.test.util.UiDisableIf;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -204,6 +206,7 @@ public class AutofillAssistantOverlayIntegrationTest {
      */
     @Test
     @MediumTest
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/1140957
     public void testShowCastOnDocumentElementInScrolledBrowserWindow() throws Exception {
         SelectorProto element =
                 (SelectorProto) SelectorProto.newBuilder()
