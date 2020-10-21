a34dfe175375de1012c7a03da39c9abddb5c2375
Dom Schulz <dominicschulz@google.com>
2020-10-21 18:42:00 +0000

    Delete the manifest_request_filter. It was only used in manifest install of SWAs, which is being deleted in the parent branch.
    Bug: 1085274
    Change-Id: I8c053f82b4e5186c7336d731b8deafa3dbbb95ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485760
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Dominic Schulz <dominicschulz@google.com>
    Cr-Commit-Position: refs/heads/master@{#819452}

--- chromeos/components/help_app_ui/help_app_ui.cc
+++ chromeos/components/help_app_ui/help_app_ui.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/components/local_search_service/local_search_service_proxy.h"
 #include "chromeos/components/local_search_service/local_search_service_proxy_factory.h"
 #include "chromeos/components/local_search_service/mojom/types.mojom.h"
-#include "chromeos/components/web_applications/manifest_request_filter.h"
 #include "chromeos/grit/chromeos_help_app_resources.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
--- chromeos/components/media_app_ui/media_app_ui.cc
+++ chromeos/components/media_app_ui/media_app_ui.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/components/media_app_ui/media_app_guest_ui.h"
 #include "chromeos/components/media_app_ui/media_app_page_handler.h"
 #include "chromeos/components/media_app_ui/url_constants.h"
-#include "chromeos/components/web_applications/manifest_request_filter.h"
 #include "chromeos/grit/chromeos_media_app_bundle_resources.h"
 #include "chromeos/grit/chromeos_media_app_resources.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
