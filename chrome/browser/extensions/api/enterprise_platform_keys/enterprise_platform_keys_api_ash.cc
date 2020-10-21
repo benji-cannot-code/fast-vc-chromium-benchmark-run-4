12852b813f2d827286d64d7bf148522e0b0ae9fc
Erik Chen <erikchen@chromium.org>
2020-10-21 08:18:26 +0000

    Update PlatformKeysService methods to use OnceCallback.
    This CL is a refactor with no intended behavior change. It updates the
    method parameter from Callback -> OnceCallback.
    
    Bug: 1127505
    Change-Id: I4a46a10822c02397ca09ffc3da531c685f623652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2488719
    Auto-Submit: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819283}

--- chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api_ash.cc
+++ chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api_ash.cc
@@ -121,7 +121,7 @@ EnterprisePlatformKeysGetCertificatesFunction::Run() {
           browser_context());
   platform_keys_service->GetCertificates(
       platform_keys_token_id.value(),
-      base::Bind(
+      base::BindOnce(
           &EnterprisePlatformKeysGetCertificatesFunction::OnGotCertificates,
           this));
   return RespondLater();
@@ -182,9 +182,9 @@ EnterprisePlatformKeysImportCertificateFunction::Run() {
 
   platform_keys_service->ImportCertificate(
       platform_keys_token_id.value(), cert_x509,
-      base::Bind(&EnterprisePlatformKeysImportCertificateFunction::
-                     OnImportedCertificate,
-                 this));
+      base::BindOnce(&EnterprisePlatformKeysImportCertificateFunction::
+                         OnImportedCertificate,
+                     this));
   return RespondLater();
 }
 
@@ -229,9 +229,9 @@ EnterprisePlatformKeysRemoveCertificateFunction::Run() {
 
   platform_keys_service->RemoveCertificate(
       platform_keys_token_id.value(), cert_x509,
-      base::Bind(&EnterprisePlatformKeysRemoveCertificateFunction::
-                     OnRemovedCertificate,
-                 this));
+      base::BindOnce(&EnterprisePlatformKeysRemoveCertificateFunction::
+                         OnRemovedCertificate,
+                     this));
   return RespondLater();
 }
 
