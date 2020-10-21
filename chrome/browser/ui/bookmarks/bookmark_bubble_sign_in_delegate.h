dfa61f12b42891eada47b3193bb08216840e9b1d
Mihai Sardarescu <msarda@chromium.org>
2020-10-21 17:17:49 +0000

    Clean-up BookmarkBubbleSignInDelegate
    Use std::make_unique to create unique pointers for BookmarkBubbleSignInDelegate.
    
    Bug: None
    Change-Id: Iabffb64eed63a554f4def7a0f2c1f68de38ec765
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490043
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819422}

--- chrome/browser/ui/bookmarks/bookmark_bubble_sign_in_delegate.h
+++ chrome/browser/ui/bookmarks/bookmark_bubble_sign_in_delegate.h
@@ -20,8 +20,6 @@ class BookmarkBubbleSignInDelegate : public BubbleSyncPromoDelegate,
                                      public BrowserListObserver {
  public:
   explicit BookmarkBubbleSignInDelegate(Browser* browser);
-
- private:
   ~BookmarkBubbleSignInDelegate() override;
 
   // BubbleSyncPromoDelegate:
@@ -30,6 +28,7 @@ class BookmarkBubbleSignInDelegate : public BubbleSyncPromoDelegate,
   // BrowserListObserver:
   void OnBrowserRemoved(Browser* browser) override;
 
+ private:
   // Makes sure |browser_| points to a valid browser.
   void EnsureBrowser();
 
