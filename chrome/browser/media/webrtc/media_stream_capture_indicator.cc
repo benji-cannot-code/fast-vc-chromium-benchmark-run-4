da0f4dfbdc67acb763bc3d175a22bf27790b3716
Guido Urdaneta <guidou@chromium.org>
2020-10-21 13:20:58 +0000

    Use actual WebContents to track desktop capture.
    This ensures that the actual WebContents is used to track desktop
    capture in MediaCaptureIndicator. Prior to this CL a null WebContents
    was used.
    
    Bug: 1140487
    Change-Id: I3078dc3d071ca1dc34716de491996a275026f793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2488160
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819335}

--- chrome/browser/media/webrtc/media_stream_capture_indicator.cc
+++ chrome/browser/media/webrtc/media_stream_capture_indicator.cc
@@ -333,6 +333,7 @@ MediaStreamCaptureIndicator::RegisterMediaStream(
     content::WebContents* web_contents,
     const blink::MediaStreamDevices& devices,
     std::unique_ptr<MediaStreamUI> ui) {
+  DCHECK(web_contents);
   auto& usage = usage_map_[web_contents];
   if (!usage)
     usage = std::make_unique<WebContentsDeviceUsage>(this, web_contents);
