cdc24f2db5ea6a30e324270a210bbd2ab675786a
Thomas Lukaszewicz <tluk@chromium.org>
2020-10-21 00:02:44 +0000

    Tab Search: Enable Tab Search for all desktop platforms.
    This flag enables tab search on all Chrome desktop platforms. It also
    removes the deprecated kTabSearchFixedEntrypoint flag.
    
    Bug: 1099917
    Change-Id: I9b695d399fdf7db8e3ae6c6f2c53be001b4f6248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2486991
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819167}

--- chrome/browser/ui/ui_features.h
+++ chrome/browser/ui/ui_features.h
@@ -65,8 +65,6 @@ extern const base::Feature kTabSearch;
 
 extern const base::Feature kTabSearchFeedback;
 
-extern const base::Feature kTabSearchFixedEntrypoint;
-
 // Setting this to true will ignore the distance parameter when finding matches.
 // This means that it will not matter where in the string the pattern occurs.
 extern const base::FeatureParam<bool> kTabSearchSearchIgnoreLocation;
