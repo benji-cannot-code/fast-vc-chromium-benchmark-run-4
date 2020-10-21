677718dbddb523e67a8f38efda60c1a4a58d0f1d
Yi Zhang <yi.y.zhang@intel.com>
2020-10-21 01:51:56 +0000

    Part 1: Convert base::Bind to Once/Repeating in //chrome/browser/devtools/
    This CL is the 1st part of converting base::Bind to Once/Repeating in
    //chrome/browser/devtools/
    
    Bug: 1007635
    Change-Id: I8a58f155c3ae402f860492af77df21c133c5f453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2486573
    Reviewed-by: Leon Han <leon.han@intel.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Leon Han <leon.han@intel.com>
    Cr-Commit-Position: refs/heads/master@{#819199}

--- chrome/browser/devtools/devtools_file_system_indexer.h
+++ chrome/browser/devtools/devtools_file_system_indexer.h
@@ -28,10 +28,10 @@ class DevToolsFileSystemIndexer
     : public base::RefCountedThreadSafe<DevToolsFileSystemIndexer> {
  public:
 
-  typedef base::Callback<void(int)> TotalWorkCallback;
-  typedef base::Callback<void(int)> WorkedCallback;
-  typedef base::Callback<void()> DoneCallback;
-  typedef base::Callback<void(const std::vector<std::string>&)> SearchCallback;
+  typedef base::OnceCallback<void(int)> TotalWorkCallback;
+  typedef base::RepeatingCallback<void(int)> WorkedCallback;
+  typedef base::OnceCallback<void()> DoneCallback;
+  typedef base::OnceCallback<void(const std::vector<std::string>&)> SearchCallback;
 
   class FileSystemIndexingJob
       : public base::RefCountedThreadSafe<FileSystemIndexingJob> {
@@ -43,9 +43,9 @@ class DevToolsFileSystemIndexer
     friend class DevToolsFileSystemIndexer;
     FileSystemIndexingJob(const base::FilePath& file_system_path,
                           const std::vector<base::FilePath>& excluded_folders,
-                          const TotalWorkCallback& total_work_callback,
+                          TotalWorkCallback total_work_callback,
                           const WorkedCallback& worked_callback,
-                          const DoneCallback& done_callback);
+                          DoneCallback done_callback);
     virtual ~FileSystemIndexingJob();
 
     void Start();
@@ -89,14 +89,14 @@ class DevToolsFileSystemIndexer
   scoped_refptr<FileSystemIndexingJob> IndexPath(
       const std::string& file_system_path,
       const std::vector<std::string>& excluded_folders,
-      const TotalWorkCallback& total_work_callback,
+      TotalWorkCallback total_work_callback,
       const WorkedCallback& worked_callback,
-      const DoneCallback& done_callback);
+      DoneCallback done_callback);
 
   // Performs trigram search for given |query| in |file_system_path|.
   void SearchInPath(const std::string& file_system_path,
                     const std::string& query,
-                    const SearchCallback& callback);
+                    SearchCallback callback);
 
  private:
   friend class base::RefCountedThreadSafe<DevToolsFileSystemIndexer>;
@@ -105,7 +105,7 @@ class DevToolsFileSystemIndexer
 
   void SearchInPathOnImplSequence(const std::string& file_system_path,
                                   const std::string& query,
-                                  const SearchCallback& callback);
+                                  SearchCallback callback);
 
   DISALLOW_COPY_AND_ASSIGN(DevToolsFileSystemIndexer);
 };
