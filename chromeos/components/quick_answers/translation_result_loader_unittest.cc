c07b77044bb4c969418fa71e83602712a0f3ae44
Yue Li <updowndota@google.com>
2020-10-21 07:47:57 +0000

    Quick Answers: Add unit tests for translation result loader
    Bug: b/169453041
    Test: Run TranslationResultLoaderTest.* in chromeos_components_unittests
    Change-Id: I47769ca8e86847fac34130eaa9048ff128c44f23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2484333
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Auto-Submit: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819274}

--- chromeos/components/quick_answers/translation_result_loader_unittest.cc
+++ chromeos/components/quick_answers/translation_result_loader_unittest.cc
@@ -3,11 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chromeos/components/quick_answers/search_result_loader.h"
+#include "chromeos/components/quick_answers/translation_result_loader.h"
 
 #include <memory>
 #include <string>
 
+#include "ash/public/cpp/quick_answers/controller/quick_answers_browser_client.h"
 #include "base/test/task_environment.h"
 #include "chromeos/components/quick_answers/quick_answers_model.h"
 #include "chromeos/components/quick_answers/test/test_helpers.h"
@@ -22,85 +23,97 @@ namespace chromeos {
 namespace quick_answers {
 namespace {
 
-constexpr char kValidResponse[] = R"()]}'
+constexpr char kCloudTranslationApiRequest[] =
+    "https://translation.googleapis.com/language/translate/v2";
+
+constexpr char kValidResponse[] = R"(
   {
-    "results": [
-      {
-        "oneNamespaceType": 13668,
-        "unitConversionResult": {
-          "source": {
-            "valueAndUnit": {
-              "rawText": "23 centimeters"
-            }
-          },
-          "destination": {
-            "valueAndUnit": {
-              "rawText": "9.055 inches"
-            }
-          }
+    "data": {
+      "translations": [
+        {
+          "translatedText": "prueba"
         }
-      }
-    ]
+      ]
+    }
   }
 )";
 
+constexpr char kTestTranslationResult[] = "prueba";
+
+const auto kTestTranslationIntent =
+    IntentInfo("test", IntentType::kTranslation, "en", "es");
+
+class FakeQuickAnswersBrowserClient : public ash::QuickAnswersBrowserClient {
+ public:
+  FakeQuickAnswersBrowserClient() = default;
+  ~FakeQuickAnswersBrowserClient() override = default;
+
+  // ash::QuickAnswersBrowserClient:
+  void RequestAccessToken(GetAccessTokenCallback callback) override {
+    std::move(callback).Run(std::string());
+  }
+};
+
 }  // namespace
 
-class SearchResultLoaderTest : public testing::Test {
+class TranslationResultLoaderTest : public testing::Test {
  public:
-  SearchResultLoaderTest() = default;
+  TranslationResultLoaderTest() = default;
 
-  SearchResultLoaderTest(const SearchResultLoaderTest&) = delete;
-  SearchResultLoaderTest& operator=(const SearchResultLoaderTest&) = delete;
+  TranslationResultLoaderTest(const TranslationResultLoaderTest&) = delete;
+  TranslationResultLoaderTest& operator=(const TranslationResultLoaderTest&) =
+      delete;
 
   // testing::Test:
   void SetUp() override {
+    quick_answers_browser_client_ =
+        std::make_unique<FakeQuickAnswersBrowserClient>();
     mock_delegate_ = std::make_unique<MockResultLoaderDelegate>();
-    loader_ = std::make_unique<SearchResultLoader>(&test_url_loader_factory_,
-                                                   mock_delegate_.get());
+    loader_ = std::make_unique<TranslationResultLoader>(
+        &test_url_loader_factory_, mock_delegate_.get());
   }
 
   void TearDown() override { loader_.reset(); }
 
  protected:
   base::test::SingleThreadTaskEnvironment task_environment_;
-  std::unique_ptr<SearchResultLoader> loader_;
+  std::unique_ptr<FakeQuickAnswersBrowserClient> quick_answers_browser_client_;
+  std::unique_ptr<TranslationResultLoader> loader_;
   std::unique_ptr<MockResultLoaderDelegate> mock_delegate_;
   data_decoder::test::InProcessDataDecoder in_process_data_decoder_;
   network::TestURLLoaderFactory test_url_loader_factory_;
 };
 
-TEST_F(SearchResultLoaderTest, Success) {
+TEST_F(TranslationResultLoaderTest, Success) {
   std::unique_ptr<QuickAnswer> expected_quick_answer =
       std::make_unique<QuickAnswer>();
-  expected_quick_answer->primary_answer = "9.055 inches";
-  test_url_loader_factory_.AddResponse(assistant::kSampleKnowledgeApiRequest,
+  expected_quick_answer->primary_answer = kTestTranslationResult;
+  test_url_loader_factory_.AddResponse(kCloudTranslationApiRequest,
                                        kValidResponse);
   EXPECT_CALL(
       *mock_delegate_,
       OnQuickAnswerReceived(QuickAnswerEqual(expected_quick_answer.get())));
   EXPECT_CALL(*mock_delegate_, OnNetworkError()).Times(0);
-  loader_->Fetch(PreprocessRequest(IntentInfo("23cm", IntentType::kUnknown)));
+  loader_->Fetch(PreprocessRequest(kTestTranslationIntent));
   base::RunLoop().RunUntilIdle();
 }
 
-TEST_F(SearchResultLoaderTest, NetworkError) {
+TEST_F(TranslationResultLoaderTest, NetworkError) {
   test_url_loader_factory_.AddResponse(
-      GURL(assistant::kSampleKnowledgeApiRequest),
-      network::mojom::URLResponseHead::New(), std::string(),
-      network::URLLoaderCompletionStatus(net::HTTP_NOT_FOUND));
+      GURL(kCloudTranslationApiRequest), network::mojom::URLResponseHead::New(),
+      std::string(), network::URLLoaderCompletionStatus(net::HTTP_NOT_FOUND));
   EXPECT_CALL(*mock_delegate_, OnNetworkError());
   EXPECT_CALL(*mock_delegate_, OnQuickAnswerReceived(testing::_)).Times(0);
-  loader_->Fetch(PreprocessRequest(IntentInfo("23cm", IntentType::kUnknown)));
+  loader_->Fetch(PreprocessRequest(kTestTranslationIntent));
   base::RunLoop().RunUntilIdle();
 }
 
-TEST_F(SearchResultLoaderTest, EmptyResponse) {
-  test_url_loader_factory_.AddResponse(assistant::kSampleKnowledgeApiRequest,
+TEST_F(TranslationResultLoaderTest, EmptyResponse) {
+  test_url_loader_factory_.AddResponse(kCloudTranslationApiRequest,
                                        std::string());
   EXPECT_CALL(*mock_delegate_, OnQuickAnswerReceived(testing::Eq(nullptr)));
   EXPECT_CALL(*mock_delegate_, OnNetworkError()).Times(0);
-  loader_->Fetch(PreprocessRequest(IntentInfo("23cm", IntentType::kUnknown)));
+  loader_->Fetch(PreprocessRequest(kTestTranslationIntent));
   base::RunLoop().RunUntilIdle();
 }
 
