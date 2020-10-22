eb8e7c043fef27dbd0274702dc00d84cabcb71d9
Hiroshige Hayashizaki <hiroshige@chromium.org>
2020-10-22 06:21:15 +0000

    [WPT] Add tests for module service worker static import + path restriction
    Path restriction check
    https://w3c.github.io/ServiceWorker/#path-restriction
    shouldn't be done for non-top-level module scripts.
    
    Bug: 1136775
    Change-Id: Id75fd427aaceca189324335fae1efb37f5df7c6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462947
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819723}

---
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/scope1/module-worker-importing-redirect-to-scope2.js
@@ -0,0 +1,2 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import * as module from './redirect.py?Redirect=/service-workers/service-worker/resources/scope2/imported-module-script.js';
