166faa0e295ae0ba4e3e4b247938125b94a44a0e
Piotr Tworek <ptworek@vewd.com>
2020-10-22 07:39:10 +0000

    IWYU in net/dns/dns_server_iterator.h.
    The code uses size_t, so it needs to include stddef.h. This works
    fine in default setup using libcxx, but fails with libstdc++.
    
    Bug: 957519
    Change-Id: Id6bca0bbc911253a3be9a67b01dc348e467490ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2489303
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#819742}

--- net/dns/dns_server_iterator.h
+++ net/dns/dns_server_iterator.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_DNS_DNS_SERVER_ITERATOR_H_
 #define NET_DNS_DNS_SERVER_ITERATOR_H_
 
+#include <stddef.h>
 #include <vector>
 
 #include "net/base/net_export.h"
