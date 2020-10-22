c2b747e8a74e289c2cb556826929c3c1b215bb6d
Monica Basta <msalama@chromium.org>
2020-10-22 13:59:50 +0000

    [Signin]: Use the shared icons file instead of 'signin_icons'.
    This CL deletes 'signin_icons' and replaces all usage of
    'signin:business' with 'cr:domain'.
    
    Change-Id: Ifa234f23af00f0d45e2b528ad4b1d37f3568f2fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490045
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819817}

--- chrome/browser/resources/signin/dice_web_signin_intercept/dice_web_signin_intercept_app.js
+++ chrome/browser/resources/signin/dice_web_signin_intercept/dice_web_signin_intercept_app.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
-import './signin_icons.js';
+import 'chrome://resources/cr_elements/icons.m.js';
 import './signin_shared_css.js';
 import './signin_vars_css.js';
 
--- chrome/browser/resources/signin/profile_picker/profile_card.js
+++ chrome/browser/resources/signin/profile_picker/profile_card.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
-import './signin_icons.js';
+import 'chrome://resources/cr_elements/icons.m.js';
 import './profile_card_menu.js';
 import './profile_picker_shared_css.js';
 
