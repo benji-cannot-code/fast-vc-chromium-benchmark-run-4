9818bf55ece94b73b64b74eee28325b023bd358b
Tommy Li <tommycli@chromium.org>
2020-10-22 19:55:08 +0000

    [omnibox] Revive InMemoryURLIndex restore and save cache time histograms
    These histograms were obsoleted recently.
    
    I would like to bring them back to validate the hypothesis that the
    HistoryQuickProvider / InMemoryURLIndex cache is not needed.
    
    Details of experiment: go/chrome-omnibox-hqp-ablate-cache-file
    
    Specifically, my theory is that the time it takes to restore + save the
    cache file is in the same order of magnitude as it takes to rebuild the
    index from the History DB.
    
    The cache causes two known problems:
     1. Saving it is responsible for roughly 10% of shutdown hangs.
     2. Restoring it seems to take a lot of memory.
    
    This CL is a partial revert of:
    https://chromium-review.googlesource.com/c/chromium/src/+/2324551
    
    Bug: 750845, 1032679, 1141539
    Change-Id: I7e679c0ba602cf48ab10ad0a89bd75bc3dbf5ee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490148
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819950}

--- components/omnibox/browser/url_index_private_data.cc
+++ components/omnibox/browser/url_index_private_data.cc
@@ -384,6 +384,7 @@ bool URLIndexPrivateData::DeleteURL(const GURL& url) {
 // static
 scoped_refptr<URLIndexPrivateData> URLIndexPrivateData::RestoreFromFile(
     const base::FilePath& file_path) {
+  base::TimeTicks beginning_time = base::TimeTicks::Now();
   if (!base::PathExists(file_path))
     return nullptr;
   std::string data;
@@ -410,6 +411,8 @@ scoped_refptr<URLIndexPrivateData> URLIndexPrivateData::RestoreFromFile(
   if (!restored_data->RestorePrivateData(index_cache))
     return nullptr;
 
+  UMA_HISTOGRAM_TIMES("History.InMemoryURLIndexRestoreCacheTime",
+                      base::TimeTicks::Now() - beginning_time);
   UMA_HISTOGRAM_COUNTS_1M("History.InMemoryURLHistoryItems",
                           restored_data->history_id_word_map_.size());
   if (restored_data->Empty())
@@ -935,6 +938,7 @@ void URLIndexPrivateData::ResetSearchTermCache() {
 }
 
 bool URLIndexPrivateData::SaveToFile(const base::FilePath& file_path) {
+  base::TimeTicks beginning_time = base::TimeTicks::Now();
   InMemoryURLIndexCacheItem index_cache;
   SavePrivateData(&index_cache);
   std::string data;
@@ -948,6 +952,8 @@ bool URLIndexPrivateData::SaveToFile(const base::FilePath& file_path) {
     LOG(WARNING) << "Failed to write " << file_path.value();
     return false;
   }
+  UMA_HISTOGRAM_TIMES("History.InMemoryURLIndexSaveCacheTime",
+                      base::TimeTicks::Now() - beginning_time);
   return true;
 }
 
