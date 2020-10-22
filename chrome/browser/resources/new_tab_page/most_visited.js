2be6849a3272a1243dd12de0092ee8a1198d1e8d
Moe Ahmadi <mahmadi@chromium.org>
2020-10-22 19:03:17 +0000

    [NTP][RQ] Reports impressions of visible MV tiles only
    Fixed: 1141138
    Change-Id: Ic99a4259b1a0eb35fac38625a309256352d7d380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490861
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819925}

--- chrome/browser/resources/new_tab_page/most_visited.js
+++ chrome/browser/resources/new_tab_page/most_visited.js
@@ -97,8 +97,7 @@ class MostVisitedElement extends PolymerElement {
       /** @private */
       columnCount_: {
         type: Number,
-        computed: `computeColumnCount_(tiles_, screenWidth_, maxTiles_,
-            visible_)`,
+        computed: `computeColumnCount_(tiles_, screenWidth_, maxTiles_)`,
       },
 
       /** @private */
@@ -142,14 +141,21 @@ class MostVisitedElement extends PolymerElement {
       /** @private */
       maxTiles_: {
         type: Number,
-        computed: 'computeMaxTiles_(visible_, customLinksEnabled_)',
+        computed: 'computeMaxTiles_(customLinksEnabled_)',
+      },
+
+      /** @private */
+      maxVisibleTiles_: {
+        type: Number,
+        computed: 'computeMaxVisibleTiles_(columnCount_, rowCount_)',
       },
 
       /** @private */
       showAdd_: {
         type: Boolean,
         value: false,
-        computed: 'computeShowAdd_(tiles_, columnCount_, customLinksEnabled_)',
+        computed:
+            'computeShowAdd_(tiles_, maxVisibleTiles_, customLinksEnabled_)',
       },
 
       /** @private */
@@ -216,7 +222,7 @@ class MostVisitedElement extends PolymerElement {
           performance.measure('most-visited-mojo', 'most-visited-mojo-start');
           this.visible_ = info.visible;
           this.customLinksEnabled_ = info.customLinksEnabled;
-          this.tiles_ = info.tiles.slice(0, 10);
+          this.tiles_ = info.tiles.slice(0, assert(this.maxTiles_));
         });
     performance.mark('most-visited-mojo-start');
     this.eventTracker_.add(document, 'visibilitychange', () => {
@@ -280,10 +286,6 @@ class MostVisitedElement extends PolymerElement {
    * @private
    */
   computeColumnCount_() {
-    if (!this.visible_) {
-      return 0;
-    }
-
     let maxColumns = 3;
     if (this.screenWidth_ === ScreenWidth.WIDE) {
       maxColumns = 5;
@@ -319,7 +321,15 @@ class MostVisitedElement extends PolymerElement {
    * @private
    */
   computeMaxTiles_() {
-    return !this.visible_ ? 0 : (this.customLinksEnabled_ ? 10 : 8);
+    return this.customLinksEnabled_ ? 10 : 8;
+  }
+
+  /**
+   * @return {number}
+   * @private
+   */
+  computeMaxVisibleTiles_() {
+    return this.columnCount_ * this.rowCount_;
   }
 
   /**
@@ -328,7 +338,7 @@ class MostVisitedElement extends PolymerElement {
    */
   computeShowAdd_() {
     return this.customLinksEnabled_ && this.tiles_ &&
-        this.tiles_.length < this.columnCount_ * 2;
+        this.tiles_.length < this.maxVisibleTiles_;
   }
 
   /**
@@ -503,7 +513,7 @@ class MostVisitedElement extends PolymerElement {
    * @private
    */
   isHidden_(index) {
-    return index >= this.columnCount_ * 2;
+    return index >= this.maxVisibleTiles_;
   }
 
   /** @private */
@@ -815,7 +825,8 @@ class MostVisitedElement extends PolymerElement {
   onTilesRendered_() {
     performance.measure('most-visited-rendered');
     this.pageHandler_.onMostVisitedTilesRendered(
-        this.tiles_, BrowserProxy.getInstance().now());
+        this.tiles_.slice(0, assert(this.maxVisibleTiles_)),
+        BrowserProxy.getInstance().now());
   }
 
   /**
