4960dc341f8091350ee2019f972e951056484d24
Rune Lillesveen <futhark@chromium.org>
2020-10-22 16:18:25 +0000

    Introduce ApplyValue that takes ScopedCSSValue.
    For properties which takes tree-scoped names, use a flag in
    css_properties.json5 that says so and invoke an ApplyProperty which
    takes a ScopedCSSValue. For all other properties call ApplyProperty with
    a CSSValue.
    
    Bug: 336876
    Change-Id: Id1b4da29660e15f2149d36145bb3894fba7f369a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490001
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819854}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/properties/longhands.h"
 #include "third_party/blink/renderer/core/css/resolver/style_builder_converter.h"
 #include "third_party/blink/renderer/core/css/resolver/style_resolver_state.h"
+#include "third_party/blink/renderer/core/css/scoped_css_value.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/css/zoom_adjusted_pixel_value.h"
 #include "third_party/blink/renderer/core/css_value_keywords.h"
@@ -311,6 +312,12 @@ const CSSValue* AnimationName::InitialValue() const {
   DEFINE_STATIC_LOCAL(const Persistent<CSSValue>, value,
                       (CSSIdentifierValue::Create(CSSValueID::kNone)));
   return value;
+}
+
+void AnimationName::ApplyValue(StyleResolverState& state,
+                               const ScopedCSSValue& scoped_value) const {
+  // TODO(futhark): Set the TreeScope on CSSAnimationData.
+  ApplyValue(state, scoped_value.GetCSSValue());
 }
 
 const CSSValue* AnimationPlayState::ParseSingleValue(
@@ -2712,6 +2719,13 @@ const CSSValue* FontFamily::CSSValueFromComputedStyleInternal(
     const LayoutObject*,
     bool allow_visited_style) const {
   return ComputedStyleUtils::ValueForFontFamily(style);
+}
+
+void FontFamily::ApplyValue(StyleResolverState& state,
+                            const ScopedCSSValue& scoped_value) const {
+  // TODO(futhark): Set TreeScope on the FontBuilder so that it can create a
+  // Font with a FontSelector for that TreeScope.
+  ApplyValue(state, scoped_value.GetCSSValue());
 }
 
 const CSSValue* FontFeatureSettings::ParseSingleValue(
