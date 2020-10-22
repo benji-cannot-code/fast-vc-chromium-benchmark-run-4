0e83fe6603ff7c1b27d7381b73d62df00621d2ca
Jan Krcal <jkrcal@chromium.org>
2020-10-22 14:26:28 +0000

    [Profile picker] Treat lack of extended account info gracefully
    This CL introduces a timeout so that profile creation is not blocked
    on the lack of extended account info. This is useful when the request
    gets blocked by a firewall, etc. When this happens, the account email
    address is used as the name of the new profile instead of the account's
    given name.
    
    Bug: 1126913
    Change-Id: I6e251caed78800ee075295f6677c7db3d7d44d0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2489911
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819824}

--- chrome/browser/ui/profile_picker.h
+++ chrome/browser/ui/profile_picker.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_PROFILE_PICKER_H_
 #define CHROME_BROWSER_UI_PROFILE_PICKER_H_
 
+#include "base/time/time.h"
 #include "third_party/skia/include/core/SkColor.h"
 
 namespace views {
@@ -53,6 +54,9 @@ class ProfilePicker {
 
   // Returns the web view (embedded in the picker) for testing.
   static views::WebView* GetWebViewForTesting();
+
+  // Overrides the timeout delay for waiting for extended account info.
+  static void SetExtendedAccountInfoTimeoutForTesting(base::TimeDelta timeout);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ProfilePicker);
