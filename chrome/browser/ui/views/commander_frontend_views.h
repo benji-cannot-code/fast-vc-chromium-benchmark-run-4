2138d518984018c968511f3c7d90f23475082802
Leonard Grey <lgrey@chromium.org>
2020-10-22 20:51:24 +0000

    Commander: add CommanderFrontend::ToggleForBrowser
    This will be the entry point to show/hide commander from a command or
    hotkey. The logic is:
    - If nothing is showing, show the UI attached to the given browser.
    - If the UI is showing and attached to the given browser, hide the UI.
    - If the UI is showing on a different browser, hide it and then show it
    on the given browser.
    
    Bug: 1014639
    Change-Id: I97dab906694dd1e34095b9bee0077968b9ea51c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2454379
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819983}

--- chrome/browser/ui/views/commander_frontend_views.h
+++ chrome/browser/ui/views/commander_frontend_views.h
@@ -12,7 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/ui/webui/commander/commander_handler.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 
 class CommanderWebView;
 
@@ -31,21 +34,32 @@ struct CommanderViewModel;
 // WebUI in its own widget and mediating between the WebUI implementation and
 // the controller.
 class CommanderFrontendViews : public commander::CommanderFrontend,
-                               public CommanderHandler::Delegate {
+                               public CommanderHandler::Delegate,
+                               public BrowserListObserver,
+                               public content::NotificationObserver {
  public:
   explicit CommanderFrontendViews(commander::CommanderBackend* backend);
   ~CommanderFrontendViews() override;
 
   // commander::CommanderFrontend overrides
+  void ToggleForBrowser(Browser* browser) override;
   void Show(Browser* browser) override;
   void Hide() override;
 
-  // CommanderHandler::Delegate overrides;
+  // CommanderHandler::Delegate overrides
   void OnTextChanged(const base::string16& text) override;
   void OnOptionSelected(size_t option_index, int result_set_id) override;
   void OnDismiss() override;
   void OnHeightChanged(int new_height) override;
   void OnHandlerEnabled(bool is_enabled) override;
+
+  // BrowserListObserver overrides
+  void OnBrowserClosing(Browser* browser) override;
+
+  // content::NotificationObserver overrides
+  void Observe(int type,
+               const content::NotificationSource& source,
+               const content::NotificationDetails& details) override;
 
  private:
   // Receives view model updates from |backend_|.
@@ -56,9 +70,6 @@ class CommanderFrontendViews : public commander::CommanderFrontend,
   // Creates a web_view_ to host the WebUI interface for |profile|. Should only
   // be called once when the system profile becomes available.
   void CreateWebView(Profile* profile);
-
-  // Called by widget delegate when widget closes.
-  void OnWindowClosing();
 
   bool is_showing() { return widget_ != nullptr; }
   bool is_web_view_created() {
@@ -80,9 +91,12 @@ class CommanderFrontendViews : public commander::CommanderFrontend,
   // |web_view_ptr_| is held here when the widget is *not* showing.
   std::unique_ptr<CommanderWebView> web_view_;
   // The browser |widget_| is attached to.
-  Browser* browser_;
+  Browser* browser_ = nullptr;
   // Whether the web UI interface is loaded and ready to accept view models.
   bool is_handler_enabled_ = false;
+  // Registrar for observing app termination.
+  content::NotificationRegistrar registrar_;
+
   base::WeakPtrFactory<CommanderFrontendViews> weak_ptr_factory_{this};
 };
 
