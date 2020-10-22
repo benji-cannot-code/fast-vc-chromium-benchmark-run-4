ffd74c1f208e6ba7ba6ce9483aae3623b1fc735b
Joel Hockey <joelhockey@chromium.org>
2020-10-22 02:51:48 +0000

    FilesApp Trash.restore and Undo toast
    go/chrome-ss/b025494050650332c0d6964cb82b977199bd9fd0
    go/chrome-ss/8d3b3f36cf84c787244001584e2b8425be3e7549
    
    The foreground page adds a listener to the background 'delete' event,
    and for any of the operation-done events (CANCEL, ERROR, SUCCESS)
    where trashedItems is populated, the foreground page will display an
    Undo toast.
    
    The toast message is '<filename> deleted' or '<n> items deleted', and
    has an action 'Undo' button to allow users to undo the delete.
    
    Bug: 953310
    Change-Id: I55f2a59e03fc6c355f3362db9cc5a6767c4086ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2469340
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819677}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -1092,6 +1092,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
     Trash, /* trash.js */
     FilesAppBrowserTest,
-    ::testing::Values(TestCase("trashMoveToTrash").EnableTrash()));
+    ::testing::Values(TestCase("trashMoveToTrash").EnableTrash(),
+                      TestCase("trashRestore").EnableTrash()));
 
 }  // namespace file_manager
--- ui/file_manager/file_manager/foreground/js/file_manager.js
+++ ui/file_manager/file_manager/foreground/js/file_manager.js
@@ -703,6 +703,7 @@ class FileManager extends cr.EventTarget {
     this.document_.addEventListener(
         'command',
         this.ui_.listContainer.clearHover.bind(this.ui_.listContainer));
+    CommandHandler.registerUndoDeleteToast(this);
   }
 
   /**
