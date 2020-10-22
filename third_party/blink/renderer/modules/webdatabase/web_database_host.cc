1ab4747c98c7d167cfe191408b93dca7de271049
Johann <johannkoenig@google.com>
2020-10-22 17:14:56 +0000

    WebDatabase: remove unused instance_
    A static local |instance| is set up in GetInstance. |instance_| is unused.
    
    Change-Id: Ia84422f46643eb869d1f03d0a73fbfe7d91bff11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2492280
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Auto-Submit: Johann Koenig <johannkoenig@google.com>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819884}

--- third_party/blink/renderer/modules/webdatabase/web_database_host.cc
+++ third_party/blink/renderer/modules/webdatabase/web_database_host.cc
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-WebDatabaseHost* WebDatabaseHost::instance_ = nullptr;
-
 // static
 WebDatabaseHost& WebDatabaseHost::GetInstance() {
   DEFINE_STATIC_LOCAL(WebDatabaseHost, instance, ());
