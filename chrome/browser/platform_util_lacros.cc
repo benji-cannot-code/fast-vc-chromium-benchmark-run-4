f7cf9c4ae29f7ec1c9eb50a6d43e3201814a3bea
James Cook <jamescook@chromium.org>
2020-10-22 17:40:58 +0000

    Introduce platform_util_lacros.cc
    platform_util contains utilities for showing a file in a file explorer
    and opening a URL with an external protocol handler. These will need
    to be plumbed from lacros to ash via crosapi.
    
    Introduce a stub file for lacros.  Rename the existing _chromeos file
    to _ash to match our naming scheme.
    
    No functional changes.
    
    Bug: 1139128, 1140585
    Change-Id: I51d50f3687010f8397c8ea584cce21caccbe6982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2488084
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Darin Fisher <darin@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819892}

---
+++ chrome/browser/platform_util_lacros.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/platform_util.h"
+
+#include "base/notreached.h"
+#include "chrome/browser/platform_util_internal.h"
+
+namespace platform_util {
+namespace internal {
+
+void PlatformOpenVerifiedItem(const base::FilePath& path, OpenItemType type) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace internal
+
+void ShowItemInFolder(Profile* profile, const base::FilePath& full_path) {
+  // TODO(https://crbug.com/1139128): Add crosapi to show item in file manager.
+  NOTIMPLEMENTED();
+}
+
+void OpenExternal(Profile* profile, const GURL& url) {
+  // TODO(https://crbug.com/1140585): Add crosapi for opening links with
+  // external protocol handlers.
+  NOTIMPLEMENTED();
+}
+
+}  // namespace platform_util
