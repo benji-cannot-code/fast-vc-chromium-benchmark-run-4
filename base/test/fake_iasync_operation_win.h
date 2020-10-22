9844c2eb9108837868a63e9892aeb48e7e45f222
Hoch Hochkeppel <mhochk@microsoft.com>
2020-10-22 23:19:45 +0000

    Add missing #include to FakeIAsyncOperation
    Adding a #include to FakeIAsyncOperation that should have been included
    originally but was missed.
    
    Bug: 1141584
    Change-Id: I0da3062a08b103d535c947f56c326f33db54179d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2493062
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820048}

--- base/test/fake_iasync_operation_win.h
+++ base/test/fake_iasync_operation_win.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_TEST_FAKE_IASYNC_OPERATION_WIN_H_
 
 #include <wrl/client.h>
+#include <wrl/implements.h>
 
 #include "base/notreached.h"
 #include "base/win/winrt_foundation_helpers.h"
