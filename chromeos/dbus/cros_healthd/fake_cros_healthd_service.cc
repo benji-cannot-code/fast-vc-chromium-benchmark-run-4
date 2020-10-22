cda9f3d46c94dab1606b522e4ee2457bd2bf9648
Kartik Hegde <khegde@chromium.org>
2020-10-22 01:12:43 +0000

    cros_healthd: Add DnsResolution routine
    Add the DnsResolution routine to the ServiceConnection.
    
    BUG=chromium:956783
    TEST=1) chromeos_unittests --gtest_filter=CrosHealthdServiceConnectionTest.*
    2) unit_tests --gtest_filter=DeviceCommandRunRoutineJobTest*
    3) Applied DnsResolution changes and successfully ran the
    DnsResolution routine on a DUT (verified using cros-health-tool diag
    --action=run_routine --routine=dns_resolution).
    
    Change-Id: If10388526b50c05aab047ba30d00642d28c9e079
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2345005
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Reviewed-by: Paul Moy <pmoy@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819654}

--- chromeos/dbus/cros_healthd/fake_cros_healthd_service.cc
+++ chromeos/dbus/cros_healthd/fake_cros_healthd_service.cc
@@ -234,6 +234,11 @@ void FakeCrosHealthdService::RunDnsLatencyRoutine(
   std::move(callback).Run(run_routine_response_.Clone());
 }
 
+void FakeCrosHealthdService::RunDnsResolutionRoutine(
+    RunDnsResolutionRoutineCallback callback) {
+  std::move(callback).Run(run_routine_response_.Clone());
+}
+
 void FakeCrosHealthdService::AddBluetoothObserver(
     mojom::CrosHealthdBluetoothObserverPtr observer) {
   bluetooth_observers_.Add(observer.PassInterface());
--- chromeos/dbus/cros_healthd/fake_cros_healthd_service.h
+++ chromeos/dbus/cros_healthd/fake_cros_healthd_service.h
@@ -106,6 +106,8 @@ class FakeCrosHealthdService final
   void RunDnsResolverPresentRoutine(
       RunDnsResolverPresentRoutineCallback callback) override;
   void RunDnsLatencyRoutine(RunDnsLatencyRoutineCallback callback) override;
+  void RunDnsResolutionRoutine(
+      RunDnsResolutionRoutineCallback callback) override;
 
   // CrosHealthdEventService overrides:
   void AddBluetoothObserver(
