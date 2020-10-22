474f7bf070f168632871c1b4f6bf0226b8ba389a
Sophey Dong <sophey@chromium.org>
2020-10-22 21:58:59 +0000

    [ShareCCT] Implement sharing in the toolbar and by default in CCT.
    Bug: 1135311
    Change-Id: I967300591d383a21b9f3f40233422b763180ddb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2459403
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820029}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTestRule.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTestRule.java
@@ -13,8 +13,10 @@ import org.hamcrest.Matchers;
 import org.junit.Assert;
 
 import org.chromium.base.ApplicationStatus;
+import org.chromium.base.FeatureList;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.chrome.browser.DeferredStartupHandler;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabTestUtils;
@@ -22,6 +24,7 @@ import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.content_public.browser.test.util.Criteria;
 import org.chromium.content_public.browser.test.util.CriteriaHelper;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -52,6 +55,10 @@ public class CustomTabActivityTestRule extends ChromeActivityTestRule<CustomTabA
 
     @Override
     public void startActivityCompletely(Intent intent) {
+        if (!FeatureList.hasTestFeatures()) {
+            FeatureList.setTestFeatures(
+                    Collections.singletonMap(ChromeFeatureList.SHARE_BY_DEFAULT_IN_CCT, true));
+        }
         putCustomTabIdInIntent(intent);
         int currentIntentId = getCustomTabIdFromIntent(intent);
 
