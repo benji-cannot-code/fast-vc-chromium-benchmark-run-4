24cb70bb5090cb19763fece6c9214be8683cf485
Sam Thorogood <thorogood@google.com>
2020-10-22 07:00:36 +0000

    Adds missing common types for Extensions JSON schemas.
    This is internal cleanup work so that there are fewer inline types
    inside the Extension APIs.
    
    Change-Id: I332fb6dbb4d78c46c599e1ba83e27d361d1ba40a
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2440721
    Commit-Queue: Sam Thorogood <thorogood@google.com>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819733}

--- third_party/closure_compiler/externs/management.js
+++ third_party/closure_compiler/externs/management.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2019 The Chromium Authors. All rights reserved.
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,9 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /** @fileoverview Externs generated from namespace: management */
 
-/**
- * @const
- */
+/** @const */
 chrome.management = {};
 
 /**
@@ -104,6 +102,15 @@ chrome.management.ExtensionInstallType = {
 chrome.management.ExtensionInfo;
 
 /**
+ * Information about an icon belonging to an extension, app, or theme.
+ * @typedef {{
+ *   showConfirmDialog: (boolean|undefined)
+ * }}
+ * @see https://developer.chrome.com/extensions/management#type-UninstallOptions
+ */
+chrome.management.UninstallOptions;
+
+/**
  * Returns a list of information about installed extensions and apps.
  * @param {function(!Array<!chrome.management.ExtensionInfo>): void=} callback
  * @see https://developer.chrome.com/extensions/management#method-getAll
@@ -164,9 +171,7 @@ chrome.management.setEnabled = function(id, enabled, callback) {};
  * Uninstalls a currently installed app or extension.
  * @param {string} id This should be the id from an item of
  *     $(ref:management.ExtensionInfo).
- * @param {{
- *   showConfirmDialog: (boolean|undefined)
- * }=} options
+ * @param {!chrome.management.UninstallOptions=} options
  * @param {function(): void=} callback
  * @see https://developer.chrome.com/extensions/management#method-uninstall
  */
@@ -175,9 +180,7 @@ chrome.management.uninstall = function(id, options, callback) {};
 /**
  * Uninstalls the calling extension. Note: This function can be used without
  * requesting the 'management' permission in the manifest.
- * @param {{
- *   showConfirmDialog: (boolean|undefined)
- * }=} options
+ * @param {!chrome.management.UninstallOptions=} options
  * @param {function(): void=} callback
  * @see https://developer.chrome.com/extensions/management#method-uninstallSelf
  */
