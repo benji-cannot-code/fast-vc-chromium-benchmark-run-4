48f22bf5a9733441f0a03c91022d963cf347a7e2
Simon Zünd <szuend@chromium.org>
2020-10-22 11:53:19 +0000

    Add new tracing category 'v8.stack_trace'
    Various places in Chromium collect and symbolize V8 stack traces.
    This could be DevTools or heavy ads instrumentation. To get better
    insights we want to add some trace events around stack trace
    capturing and symbolization under a new, disabled by default, tracing
    category.
    
    Corresponding V8 CL: https://crrev.com/c/2491028
    
    R=petermarshall@chromium.org
    
    Change-Id: I266b0783dcf6266efc512503b7b13044bd8b71c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490044
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819793}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -242,6 +242,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X(TRACE_DISABLED_BY_DEFAULT("v8.runtime"))                             \
   X(TRACE_DISABLED_BY_DEFAULT("v8.runtime_stats"))                       \
   X(TRACE_DISABLED_BY_DEFAULT("v8.runtime_stats_sampling"))              \
+  X(TRACE_DISABLED_BY_DEFAULT("v8.stack_trace"))                         \
   X(TRACE_DISABLED_BY_DEFAULT("v8.turbofan"))                            \
   X(TRACE_DISABLED_BY_DEFAULT("v8.wasm.detailed"))                       \
   X(TRACE_DISABLED_BY_DEFAULT("video_and_image_capture"))                \
