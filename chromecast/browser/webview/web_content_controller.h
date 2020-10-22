9d68e23ae04c0efc6296971bee44f32ccc448030
Ryan Daum <rdaum@chromium.org>
2020-10-22 17:53:15 +0000

    [chromecast] Reland webview text input focus events
      * Relanding of functionality in chromium/2422501
      * With fixes to register the IME observer in OnVisible instead of
        AttachTo, and breaks the observer out into its own source unit.
      * Registers and watch for text input focus events within webview
        content.
      * Defines a new gRPC payload for said events, and broadcasts them.
    
    Bug: internal b/161811385
    Test: on device & manually with wayland client tool
    Change-Id: I3d264f5a76931becae2e83c35ae18dceb13b84d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490359
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Ryan Daum <rdaum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819895}

--- chromecast/browser/webview/web_content_controller.h
+++ chromecast/browser/webview/web_content_controller.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/surface_observer.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "ui/base/ime/input_method.h"
+#include "ui/base/ime/input_method_observer.h"
 #include "ui/events/gestures/gesture_recognizer_impl.h"
 
 namespace aura {
@@ -166,6 +168,7 @@ class WebContentController
   };
 
   std::unique_ptr<WebviewWindowVisibilityObserver> window_visibility_observer_;
+  std::unique_ptr<ui::InputMethodObserver> input_method_observer_;
 
   base::WeakPtrFactory<WebContentController> weak_ptr_factory_{this};
 
