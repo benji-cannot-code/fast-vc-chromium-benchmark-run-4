5a7218e8cd430fbfd1b67087f5b935fd9fc9fec9
Geoff Lang <geofflang@google.com>
2020-10-22 20:12:00 +0000

    Skip robust resource init for YUV video copies.
    Tell ANGLE that it doesn't need to perform robust resource init
    on the result texture for YUV to RGB copies because the entire
    result will be overwritten.
    
    Also block GLES clients from modifying the resource initialization
    state.
    
    Bug: chromium:1132514
    Change-Id: Ie233bc129a4e3acdd13e77110b2dc572347bbf15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2476414
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819957}

--- ui/gl/generate_bindings.py
+++ ui/gl/generate_bindings.py
@@ -2923,6 +2923,7 @@ FUNCTION_SETS = [
       'GLES2/gl2chromium.h',
       'GLES2/gl2extchromium.h'
     ], [
+      "GL_ANGLE_robust_resource_initialization",
       "GL_ARB_texture_swizzle",
       "GL_EXT_texture_swizzle",
       "GL_EXT_texture_format_BGRA8888",
