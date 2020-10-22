37f311125b2478c044197a1189e854a15be2942d
Willie Koomson <wvk@google.com>
2020-10-22 18:58:47 +0000

    Rename Arc.OOMKills.* and Arc.LowMemoryKiller.* to use Memory prefix
    Since these metrics currently represent all of the OOM kills and low
    memory kills on the system, use Memory prefix instead of Arc. Also
    updates histograms to use functions instead of macros, since they aren't
    on a critical path.
    
    BUG=b:165194376
    BUG=916757
    TEST=Build success and MemoryKillsMonitorTest. Expect UMA results with
     new prefix
    
    Change-Id: Ie4492feada24349a72bc8cec64e32f45ca6dae53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2488397
    Commit-Queue: Willie Koomson <wvk@google.com>
    Reviewed-by: Kuo-Hsin Yang <vovoy@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819921}

--- chrome/browser/memory/memory_kills_monitor.cc
+++ chrome/browser/memory/memory_kills_monitor.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_macros.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/process/process_metrics.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
@@ -88,8 +88,9 @@ void MemoryKillsMonitor::StartMonitoring() {
 
   // Insert a zero kill record at the begining of each login session for easy
   // comparison to those with non-zero kill sessions.
-  UMA_HISTOGRAM_CUSTOM_COUNTS("Arc.OOMKills.Count", 0, 1, 1000, 1001);
-  UMA_HISTOGRAM_CUSTOM_COUNTS("Arc.LowMemoryKiller.Count", 0, 1, 1000, 1001);
+  base::UmaHistogramCustomCounts("Memory.OOMKills.Count", 0, 1, 1000, 1001);
+  base::UmaHistogramCustomCounts("Memory.LowMemoryKiller.Count", 0, 1, 1000,
+                                 1001);
 
   monitoring_started_.Set();
 
@@ -127,8 +128,8 @@ void MemoryKillsMonitor::CheckOOMKillImpl(unsigned long current_oom_kills) {
     // Report the cumulative count of killed process in one login session. For
     // example if there are 3 processes killed, it would report 1 for the first
     // kill, 2 for the second kill, then 3 for the final kill.
-    UMA_HISTOGRAM_CUSTOM_COUNTS("Arc.OOMKills.Count", oom_kills_count_, 1, 1000,
-                                1001);
+    base::UmaHistogramCustomCounts("Memory.OOMKills.Count", oom_kills_count_, 1,
+                                   1000, 1001);
   }
   last_oom_kills_count_ = current_oom_kills;
 }
@@ -149,15 +150,16 @@ void MemoryKillsMonitor::LogLowMemoryKillImpl(const std::string& type,
   const base::TimeDelta time_delta = last_low_memory_kill_time_.is_null()
                                          ? kMaxMemoryKillTimeDelta
                                          : (now - last_low_memory_kill_time_);
-  UMA_HISTOGRAM_MEMORY_KILL_TIME_INTERVAL("Arc.LowMemoryKiller.TimeDelta",
+  UMA_HISTOGRAM_MEMORY_KILL_TIME_INTERVAL("Memory.LowMemoryKiller.TimeDelta",
                                           time_delta);
   last_low_memory_kill_time_ = now;
 
   ++low_memory_kills_count_;
-  UMA_HISTOGRAM_CUSTOM_COUNTS("Arc.LowMemoryKiller.Count",
-                              low_memory_kills_count_, 1, 1000, 1001);
+  base::UmaHistogramCustomCounts("Memory.LowMemoryKiller.Count",
+                                 low_memory_kills_count_, 1, 1000, 1001);
 
-  UMA_HISTOGRAM_MEMORY_KB("Arc.LowMemoryKiller.FreedSize", estimated_freed_kb);
+  base::UmaHistogramMemoryKB("Memory.LowMemoryKiller.FreedSize",
+                             estimated_freed_kb);
 }
 
 void MemoryKillsMonitor::LogArcOOMKillImpl(unsigned long current_oom_kills) {
@@ -171,8 +173,8 @@ void MemoryKillsMonitor::LogArcOOMKillImpl(unsigned long current_oom_kills) {
   for (int i = 0; i < oom_kills_delta; ++i) {
     ++oom_kills_count_;
     // Report cumulative count of killed processes in one login session.
-    UMA_HISTOGRAM_CUSTOM_COUNTS("Arc.OOMKills.Count", oom_kills_count_, 1, 1000,
-                                1001);
+    base::UmaHistogramCustomCounts("Memory.OOMKills.Count", oom_kills_count_, 1,
+                                   1000, 1001);
   }
   last_arc_oom_kills_count_ = current_oom_kills;
 }
