c0b47ad7ee6c933ba08134157caad65835b89b14
David Van Cleve <davidvc@chromium.org>
2020-10-22 05:06:59 +0000

    blink: Add absl::variant to the blink symbol allowlist
    absl::variant was recently approved for general Chromium usage. This CL
    adds it to the Blink symbol allowlist.
    
    (I had a union I wanted to implement in the child CL and remembered
    `variant` had recently been approved. I'm not familiar with the criteria
    for permitting/forbidding symbols in Blink, so I wouldn't be too
    surprised if it turns out we don't want `variant` in Blink.)
    
    R=dcheng
    
    Bug: None
    Change-Id: I0885ef203472bcdb17481423d88eeb2add801766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2442230
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819703}

---
+++ tools/clang/blink_gc_plugin/tests/variant_of_gced_type.cpp
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "variant_of_gced_type.h"
+
+namespace blink {
+
+void ForbidsVariantsOfGcedTypes() {
+  absl::variant<Base> not_ok;
+  (void)not_ok;
+
+  absl::variant<Base, Base> similarly_not_ok;
+  (void)similarly_not_ok;
+
+  absl::variant<int, Base> not_ok_either;
+  (void)not_ok_either;
+
+  absl::variant<int, Derived> ditto;
+  (void)ditto;
+
+  new absl::variant<Mixin>;
+}
+
+}  // namespace blink
