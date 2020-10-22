c136c8c41fc2e473bad4d351a8a1c3c25389cf56
Rob Buis <rbuis@igalia.com>
2020-10-22 16:20:05 +0000

    [mathml] Implement special drawing of largeop operator in displaystyle
    This implements special drawing of largeop <mo> element in display style
    as described in step 3. of [1]. A new algorithm is introduced for that
    purpose and in particular only used for <mo> elements that are made of
    a single glyph. Existing painting logic for stretchy radical symbol is
    reused and adapted to work with largeop operators.
    
    [1] https://mathml-refresh.github.io/mathml-core/#layout-of-operators
    
    Bug: 6606
    Change-Id: Icca70f4bf464417809970fecc101e5aa7f149621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2414298
    Commit-Queue: Rob Buis <rbuis@igalia.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Frédéric Wang <fwang@igalia.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819855}

--- third_party/blink/renderer/core/mathml/mathml_operator_element.h
+++ third_party/blink/renderer/core/mathml/mathml_operator_element.h
@@ -25,6 +25,7 @@ class CORE_EXPORT MathMLOperatorElement final : public MathMLElement {
 
   struct OperatorContent {
     String characters;
+    UChar32 code_point;
     bool is_vertical = true;
   };
   enum OperatorPropertyFlag {
