7d409a9d3be4802bd494d054b49c513085b7ae0a
Sergey Poromov <poromov@chromium.org>
2020-10-22 11:46:00 +0000

    DLP: Observe confidential window visible area.
    In order to support partial window screen capture we need
    to track events when some confidential content appears in
    the captured area.
    This is done via observing OcclusionState for the corresponding windows.
    
    Bug: 1133324
    Change-Id: Ia0ffa3ca79a2b58ccef1902acc8b6772df773b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2456606
    Reviewed-by: Nikita Podguzov <nikitapodguzov@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819790}

--- chrome/browser/chromeos/policy/dlp/dlp_content_manager.h
+++ chrome/browser/chromeos/policy/dlp/dlp_content_manager.h
@@ -6,16 +6,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_POLICY_DLP_DLP_CONTENT_MANAGER_H_
 #define CHROME_BROWSER_CHROMEOS_POLICY_DLP_DLP_CONTENT_MANAGER_H_
 
+#include <memory>
+
 #include "base/callback.h"
 #include "base/containers/flat_map.h"
 #include "base/gtest_prod_util.h"
 #include "base/optional.h"
 #include "base/time/time.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_content_restriction_set.h"
+#include "chrome/browser/chromeos/policy/dlp/dlp_window_observer.h"
 #include "chrome/browser/ui/ash/screenshot_area.h"
 
 class GURL;
 struct ScreenshotArea;
+
+namespace aura {
+class Window;
+}  // namespace aura
 
 namespace content {
 class WebContents;
@@ -27,11 +34,14 @@ namespace policy {
 // WebContents and whether any of them are currently visible.
 // If any confidential WebContents is visible, the corresponding restrictions
 // will be enforced according to the current enterprise policy.
-class DlpContentManager {
+class DlpContentManager : public DlpWindowObserver::Delegate {
  public:
   // Creates the instance if not yet created.
   // There will always be a single instance created on the first access.
   static DlpContentManager* Get();
+
+  // DlpWindowObserver::Delegate overrides:
+  void OnWindowOcclusionChanged(aura::Window* window) override;
 
   // Returns which restrictions are applied to the |web_contents| according to
   // the policy.
@@ -67,12 +77,18 @@ class DlpContentManager {
 
  private:
   FRIEND_TEST_ALL_PREFIXES(DlpContentManagerBrowserTest, ScreenshotsRestricted);
+  FRIEND_TEST_ALL_PREFIXES(DlpContentManagerBrowserTest,
+                           VideoCaptureStoppedWhenConfidentialWindowResized);
+  FRIEND_TEST_ALL_PREFIXES(DlpContentManagerBrowserTest,
+                           VideoCaptureStoppedWhenNonConfidentialWindowResized);
+  FRIEND_TEST_ALL_PREFIXES(DlpContentManagerBrowserTest,
+                           VideoCaptureNotStoppedWhenConfidentialWindowHidden);
   friend class DlpContentManagerTest;
   friend class DlpContentTabHelper;
   friend class MockDlpContentManager;
 
   DlpContentManager();
-  virtual ~DlpContentManager();
+  ~DlpContentManager() override;
   DlpContentManager(const DlpContentManager&) = delete;
   DlpContentManager& operator=(const DlpContentManager&) = delete;
 
@@ -120,6 +136,10 @@ class DlpContentManager {
   // Map from currently known confidential WebContents to the restrictions.
   base::flat_map<content::WebContents*, DlpContentRestrictionSet>
       confidential_web_contents_;
+
+  // Map of window observers for the current confidential WebContents.
+  base::flat_map<content::WebContents*, std::unique_ptr<DlpWindowObserver>>
+      window_observers_;
 
   // Set of restriction applied to the currently visible content.
   DlpContentRestrictionSet on_screen_restrictions_;
