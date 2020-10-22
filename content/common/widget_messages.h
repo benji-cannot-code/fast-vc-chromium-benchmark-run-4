df62a62183dcafb9d93a65ac925e5db814447d08
Dave Tapuska <dtapuska@chromium.org>
2020-10-22 07:34:53 +0000

    Remove WebFloatRect
    Last usage of WebFloatRect has been removed on the blink public API.
    
    BUG=738465
    
    Change-Id: I83917a10a80dda63c735918969f37103f7877816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490255
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#819740}

--- content/common/widget_messages.h
+++ content/common/widget_messages.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_message_macros.h"
 #include "third_party/blink/public/common/widget/visual_properties.h"
 #include "third_party/blink/public/mojom/page/record_content_to_visible_time_request.mojom-forward.h"
-#include "third_party/blink/public/platform/web_float_rect.h"
 #include "ui/base/ime/text_input_action.h"
 #include "ui/base/ime/text_input_mode.h"
 #include "ui/base/ui_base_types.h"
