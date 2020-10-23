0caaaed2949c3f9dfd64d609e8d5b73a8fa0f5e3
Alex Rudenko <alexrudenko@chromium.org>
2020-10-23 12:48:36 +0000

    Fix image preloading for <picture> elements with explicitly empty image types
    HTMLPreloadScanner and HTMLImageElement were using slightly different
    logic to determine if an image type is supported. In particular,
    HTMLImageElement considered an empty image type as supported and
    HTMLPreloadScanner did not. In the following case, it'd result
    in HTMLPreloadScanner trying to preload a resource (.png) that will not
    be used by HTMLImageElement (it will use .avif):
    
    ```
    <picture>
      <source srcset="images/test.avif" type="">
      <img src="images/test.png">
    </picture>
    ```
    
    This CL adds a static method HTMLImageElement::SupportedImageType
    that is used by HTMLPreloadScanner to check if an image type is
    supported. Also, redundant checks for type emptiness are removed
    from HTMLImageElement.
    
    Fixed: 1141776
    Change-Id: I89ed95c18bbbc86613f6b362ba935f428e1d05b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494720
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#820206}

--- third_party/blink/renderer/core/html/html_image_element.cc
+++ third_party/blink/renderer/core/html/html_image_element.cc
@@ -336,7 +336,7 @@ String HTMLImageElement::AltText() const {
   return FastGetAttribute(html_names::kTitleAttr);
 }
 
-static bool SupportedImageType(const String& type) {
+bool HTMLImageElement::SupportedImageType(const String& type) {
   String trimmed_type = ContentType(type).GetType();
   // An empty type attribute is implicitly supported.
   if (trimmed_type.IsEmpty())
@@ -368,7 +368,7 @@ ImageCandidate HTMLImageElement::FindBestFitImageFromPictureParent() {
     if (srcset.IsEmpty())
       continue;
     String type = source->FastGetAttribute(html_names::kTypeAttr);
-    if (!type.IsEmpty() && !SupportedImageType(type))
+    if (!SupportedImageType(type))
       continue;
 
     if (!source->MediaQueryMatches())
