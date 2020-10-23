31a84994aebf8cd46cf727128af6448e09ad0f88
Eric Willigers <ericwilligers@chromium.org>
2020-10-23 01:27:39 +0000

    Direct Sockets: Browser implementation for TCP connection establishment
    DirectSocketsServiceImpl::OpenTcpSocket() now calls
    CreateTCPConnectedSocket() on the network context.
    
    Browser test DirectSocketsBrowserTest.OpenTcp_Options verifies
    that the remote address, port, buffer sizes and no-delay flag
    are passed successfully.
    
    NavigatorSocket::PendingRequest has been split into
    TCPSocket and UDPSocket classes, currently with no web-exposed
    members implemented.
    https://github.com/WICG/raw-sockets/blob/master/docs/explainer.md#tcp
    https://github.com/WICG/raw-sockets/blob/master/docs/explainer.md#udp
    
    
    
    
    Explainer: https://github.com/WICG/raw-sockets/blob/master/docs/explainer.md
    
    Intent to Prototype:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/ARtkaw4e9T4/m/npjeMssPCAAJ
    
    Design doc:
    https://docs.google.com/document/d/1Xa5nFkIWxkL3hZHvDYWPhT8sZvNeFpCUKNuqIwZHxnE/edit?usp=sharing
    
    
    This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/2391181
    which was reverted https://chromium-review.googlesource.com/c/chromium/src/+/2404821
    as 'New tests are failing on many builders'
    
    
    Some new tests are temporarily disabled while we investigate their
    failures on specific bots - crbug.com/1141241
    
    Bug: 905818,1141241
    Change-Id: I13bedf1fdc160b3d1fc857a3debde73e3203ffee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2465343
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820088}

--- content/browser/direct_sockets/direct_sockets_service_impl.h
+++ content/browser/direct_sockets/direct_sockets_service_impl.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "net/base/ip_address.h"
 #include "net/base/net_errors.h"
 #include "third_party/blink/public/mojom/direct_sockets/direct_sockets.mojom.h"
 
@@ -29,8 +30,8 @@ class CONTENT_EXPORT DirectSocketsServiceImpl
     : public blink::mojom::DirectSocketsService,
       public WebContentsObserver {
  public:
-  using PermissionCallback = base::RepeatingCallback<net::Error(
-      const blink::mojom::DirectSocketOptions&)>;
+  using PermissionCallback = base::RepeatingCallback<
+      net::Error(const blink::mojom::DirectSocketOptions&, net::IPAddress&)>;
 
   explicit DirectSocketsServiceImpl(RenderFrameHost& frame_host);
   ~DirectSocketsServiceImpl() override = default;
@@ -43,10 +44,17 @@ class CONTENT_EXPORT DirectSocketsServiceImpl
       mojo::PendingReceiver<blink::mojom::DirectSocketsService> receiver);
 
   // blink::mojom::DirectSocketsService override:
-  void OpenTcpSocket(blink::mojom::DirectSocketOptionsPtr options,
-                     OpenTcpSocketCallback callback) override;
-  void OpenUdpSocket(blink::mojom::DirectSocketOptionsPtr options,
-                     OpenUdpSocketCallback callback) override;
+  void OpenTcpSocket(
+      blink::mojom::DirectSocketOptionsPtr options,
+      const net::MutableNetworkTrafficAnnotationTag& traffic_annotation,
+      mojo::PendingReceiver<network::mojom::TCPConnectedSocket> socket,
+      mojo::PendingRemote<network::mojom::SocketObserver> observer,
+      OpenTcpSocketCallback callback) override;
+  void OpenUdpSocket(
+      blink::mojom::DirectSocketOptionsPtr options,
+      mojo::PendingReceiver<network::mojom::UDPSocket> receiver,
+      mojo::PendingRemote<network::mojom::UDPSocketListener> listener,
+      OpenUdpSocketCallback callback) override;
 
   // WebContentsObserver override:
   void RenderFrameDeleted(RenderFrameHost* render_frame_host) override;
@@ -54,10 +62,15 @@ class CONTENT_EXPORT DirectSocketsServiceImpl
 
   static void SetPermissionCallbackForTesting(PermissionCallback callback);
 
+  static void SetNetworkContextForTesting(network::mojom::NetworkContext*);
+
  private:
   friend class DirectSocketsUnitTest;
 
-  net::Error EnsurePermission(const blink::mojom::DirectSocketOptions& options);
+  // Returns net::OK and populates |remote_address| if the options are valid and
+  // the connection is permitted.
+  net::Error ValidateOptions(const blink::mojom::DirectSocketOptions& options,
+                             net::IPAddress& remote_address);
 
   network::mojom::NetworkContext* GetNetworkContext();
 
