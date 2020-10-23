c8dca2de2282e8807606cc0b0705f5bab391729d
Mikel Astiz <mastiz@chromium.org>
2020-10-23 14:43:51 +0000

    [search engines] Add sync integration test for deletion of default
    This patch adds a sync integration test that exercises a problematic
    scenario: the deletion of a search engine via sync while the engine is
    the default one locally.
    
    This is possible because the default search engine is sync-ed via
    preferences, that is, syncer::PREFERENCES, whereas the actual list of
    search engines is sync-ed via another sync datatype, namely
    syncer::SEARCH_ENGINES.
    
    Ordering across different sync datatypes is not guaranteed and
    therefore, if a user changes the default search engine and deletes the
    old one, it is possible that a remote sync-ing device observes this
    changes in the opposite order.
    
    There is logic in TemplateURLService to special-case this scenario to
    append underscores, and this patch simply documents this behavior using
    a two-client sync integration test. Because reproducing the precise
    problematic ordering is not feasible, the test leverages a
    newly-introduced functionality in FakeServer that allows throttling
    sync datatypes (resembling a very slow sync for preferences).
    
    Change-Id: Ia0929717d0ea99a38969298b94f68a1c1ab44364
    Bug: 1022775
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2471398
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#820238}

--- chrome/browser/sync/test/integration/search_engines_helper.h
+++ chrome/browser/sync/test/integration/search_engines_helper.h
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/scoped_observer.h"
 #include "base/strings/string16.h"
-#include "chrome/browser/sync/test/integration/await_match_status_change_checker.h"
+#include "chrome/browser/sync/test/integration/status_change_checker.h"
+#include "components/search_engines/template_url_service.h"
+#include "components/search_engines/template_url_service_observer.h"
 
 class Profile;
 class TemplateURL;
@@ -33,6 +36,7 @@ bool ServiceMatchesVerifier(int profile_index);
 
 // Returns true iff all TemplateURLServices match with the verifier.
 bool AllServicesMatch();
+bool AllServicesMatch(std::ostream* os);
 
 // Create a TemplateURL with some test values based on |seed|.
 std::unique_ptr<TemplateURL> CreateTestTemplateURL(
@@ -73,12 +77,54 @@ void ChangeDefaultSearchProvider(int profile_index, int seed);
 // the search engine generated with |seed|.
 bool HasSearchEngine(int profile_index, int seed);
 
-}  // namespace search_engines_helper
+// Returns true if the profile at |profile_index| has a search engine matching
+// |keyword|.
+bool HasSearchEngineWithKeyword(int profile_index,
+                                const base::string16& keyword);
+
+// Returns the keyword for the default search engine at |profile_index|.
+base::string16 GetDefaultSearchEngineKeyword(int profile_index);
 
 // Checker that blocks until all services have the same search engine data.
-class SearchEnginesMatchChecker : public AwaitMatchStatusChangeChecker {
+class SearchEnginesMatchChecker : public StatusChangeChecker,
+                                  public TemplateURLServiceObserver {
  public:
   SearchEnginesMatchChecker();
+  ~SearchEnginesMatchChecker() override;
+
+  // StatusChangeChecker overrides.
+  bool IsExitConditionSatisfied(std::ostream* os) override;
+
+  // TemplateURLServiceObserver overrides.
+  void OnTemplateURLServiceChanged() override;
+
+ private:
+  ScopedObserver<TemplateURLService, TemplateURLServiceObserver> observer_{
+      this};
 };
+
+// Checker that blocks until |profile_index| has a search engine matching the
+// search engine generated with |seed| or |keyword|.
+class HasSearchEngineChecker : public StatusChangeChecker,
+                               public TemplateURLServiceObserver {
+ public:
+  HasSearchEngineChecker(int profile_index, int seed);
+  HasSearchEngineChecker(int profile_index, const base::string16& keyword);
+  ~HasSearchEngineChecker() override;
+
+  // StatusChangeChecker overrides.
+  bool IsExitConditionSatisfied(std::ostream* os) override;
+
+  // TemplateURLServiceObserver overrides.
+  void OnTemplateURLServiceChanged() override;
+
+ private:
+  TemplateURLService* const service_;
+  const base::string16 keyword_;
+  ScopedObserver<TemplateURLService, TemplateURLServiceObserver> observer_{
+      this};
+};
+
+}  // namespace search_engines_helper
 
 #endif  // CHROME_BROWSER_SYNC_TEST_INTEGRATION_SEARCH_ENGINES_HELPER_H_
--- components/sync/test/fake_server/fake_server.cc
+++ components/sync/test/fake_server/fake_server.cc
@@ -578,6 +578,10 @@ void FakeServer::DisallowSendingEncryptionKeys() {
   disallow_sending_encryption_keys_ = true;
 }
 
+void FakeServer::SetThrottledTypes(syncer::ModelTypeSet types) {
+  loopback_server_->SetThrottledTypesForTesting(types);
+}
+
 bool FakeServer::ShouldSendTriggeredError() const {
   if (!alternate_triggered_errors_)
     return true;
