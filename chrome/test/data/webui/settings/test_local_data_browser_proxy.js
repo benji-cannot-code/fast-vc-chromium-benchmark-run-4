6b2bd485259e70fefd29c70a0332a685bb6d86a4
sauski <sauski@google.com>
2020-10-23 13:00:00 +0000

    WebUI Settings: Prevent site_data.js running multiple handler requests
    A previous change added the site_data.js search filter to the url
    query parameters. This resulted in the page sending two requests to
    the CookiesViewHandler for each filter change. The CookiesViewHandler
    has a strong requirement that only one request be in flight at any time.
    
    This CL adds logic to prevent sending a request on route change if a
    filter query parameters is present.
    
    Bug: 141785
    Change-Id: I9ba6431b14e06fbd2211c9edb17c812cdbd1726f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494805
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#820211}

--- chrome/test/data/webui/settings/test_local_data_browser_proxy.js
+++ chrome/test/data/webui/settings/test_local_data_browser_proxy.js
@@ -59,6 +59,7 @@ export class TestLocalDataBrowserProxy extends TestBrowserProxy {
 
   /** @override */
   getDisplayList(filter) {
+    this.methodCalled('getDisplayList', filter);
     if (filter === undefined) {
       filter = '';
     }
