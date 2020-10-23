4bfbbf7d20b3d37a88fc5eaaf2968b2450fbe7ef
Ana Salazar <anasalazar@chromium.org>
2020-10-23 20:14:57 +0000

    Set Launcher's window title for ChromeVox
    Update the title for launcher's window when the launcher changes states.
    This will give Chomevox the information needed to read the title of the
    window when the user is navigating through the launcher and presses
    search + a, w.
    
    For a partially shown launcher (peeking), the title will be "Launcher,
    partial view".
    
    For the fullscreen launcher, the title will be "Launcher, all apps".
    
    If the searchbox or the assistant is displaying results, the title will
    be "Launcher"
    
    Bug: 1111165
    Change-Id: I6715ea095b568f72466182ed20a33b7fffde8538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2476618
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820394}

--- ash/app_list/views/contents_view.cc
+++ ash/app_list/views/contents_view.cc
@@ -28,11 +28,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/notreached.h"
 #include "base/numerics/ranges.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
 #include "ui/events/event.h"
+#include "ui/strings/grit/ui_strings.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/view_constants_aura.h"
 #include "ui/views/view_model.h"
@@ -310,6 +312,7 @@ void ContentsView::ActivePageChanged() {
 
   GetAppListMainView()->model()->SetState(state);
   UpdateSearchBoxVisibility(state);
+  app_list_view_->UpdateWindowTitle();
 }
 
 void ContentsView::ShowSearchResults(bool show) {
