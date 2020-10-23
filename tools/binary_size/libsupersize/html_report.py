0f6a7c2f4375597b7c591f0b63ff63d5df329a79
Mohamed Heikal <mheikal@chromium.org>
2020-10-23 16:16:45 +0000

    Add local deployment option to upload_html_viewer.py
    Currently it is not easy to test the supersize viewer without uploading
    a dev version to the production site. Add option to
    upload_html_viewer.py to host locally. Also adds a staging option
    because WFH and local development don't mix so well.
    
    Change-Id: Ieb79a47ba836898cf9da28805ce3c6a9a3e208a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485781
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820273}

--- tools/binary_size/libsupersize/html_report.py
+++ tools/binary_size/libsupersize/html_report.py
@@ -288,7 +288,7 @@ def Run(args, on_config_error):
   logging.warning('Done!')
   msg = [
       'View using a local server via: ',
-      '    {0} start_server {1}',
+      '    {0}/upload_html_viwer.py --local',
       'or run:',
       '    gsutil.py cp -a public-read {1} gs://chrome-supersize/oneoffs/'
       '{2}.ndjson',
@@ -297,8 +297,7 @@ def Run(args, on_config_error):
       '?load_url=oneoffs/{2}.ndjson',
   ]
   supersize_path = os.path.relpath(
-      os.path.join(path_util.TOOLS_SRC_ROOT, 'tools', 'binary_size',
-                   'supersize'))
+      os.path.join(path_util.TOOLS_SRC_ROOT, 'tools', 'binary_size'))
   # Use a random UUID as the filename so user can copy-and-paste command
   # directly without a name collision.
   upload_id = uuid.uuid4()
