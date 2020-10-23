f9e793e8aea9445ee3cd1ff9197fc378c1ac123c
David Van Cleve <davidvc@chromium.org>
2020-10-23 18:36:37 +0000

    Trust Tokens: Add NetworkContextClient support for local issuance
    The Trust Token API (https://github.com/wicg/trust-token-api) adds Web
    Platform support for executing certain cryptographic operations
    alongside resource requests. We're implementing an expansion,
    "platform-issued trust tokens," where one of these operations
    ("issuance") can be executed against OS-mediated providers (e.g.
    registered by some kind of system service).
    
    This CL expands NetworkContextClient's interface to provide a method
    that can answer Trust Tokens issuance requests locally. This will
    subsequently be implemented by querying an embedder-provided Mojo
    remote. This change's child (crrev.com/c/2491300) wires this interface
    to the network service Trust Tokens implementation.
    
    Design doc: bit.ly/platform-provided-trust-tokens
    
    Bug: 1130273
    Change-Id: Iea07b36cdd3369ca51a667e0f4f0cada05c2aad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2433104
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820346}

--- chrome/browser/prerender/isolated/isolated_prerender_network_context_client.h
+++ chrome/browser/prerender/isolated/isolated_prerender_network_context_client.h
@@ -73,6 +73,9 @@ class IsolatedPrerenderNetworkContextClient
 #if defined(OS_CHROMEOS)
   void OnTrustAnchorUsed() override;
 #endif
+  void OnTrustTokenIssuanceDivertedToSystem(
+      network::mojom::FulfillTrustTokenIssuanceRequestPtr request,
+      OnTrustTokenIssuanceDivertedToSystemCallback callback) override;
 };
 
 #endif  // CHROME_BROWSER_PRERENDER_ISOLATED_ISOLATED_PRERENDER_NETWORK_CONTEXT_CLIENT_H_
--- content/public/browser/network_context_client_base.h
+++ content/public/browser/network_context_client_base.h
@@ -76,6 +76,9 @@ class CONTENT_EXPORT NetworkContextClientBase
 #if defined(OS_CHROMEOS)
   void OnTrustAnchorUsed() override;
 #endif
+  void OnTrustTokenIssuanceDivertedToSystem(
+      network::mojom::FulfillTrustTokenIssuanceRequestPtr request,
+      OnTrustTokenIssuanceDivertedToSystemCallback callback) override;
 };
 
 }  // namespace content
