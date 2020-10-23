d197f217774db28c9e0ea207dd7974aa79e76312
Shakti Sahu <shaktisahu@chromium.org>
2020-10-23 20:49:32 +0000

    Video tutorials : Handle unknown features
    This CL adds
    1 - Support unknown features sent from server at the client
    2 - UI improvement for video length
    
    Change-Id: I1666e23aae6a3772e2b8478d7a7d37a99e8f104d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494422
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820414}

--- chrome/browser/video_tutorials/internal/proto_conversions.cc
+++ chrome/browser/video_tutorials/internal/proto_conversions.cc
@@ -27,9 +27,8 @@ FeatureType ToFeatureType(proto::FeatureType type) {
     case proto::FeatureType::TEST:
       return FeatureType::kTest;
     default:
-      NOTREACHED();
+      return static_cast<FeatureType>(type);
   }
-  return FeatureType::kInvalid;
 }
 
 proto::FeatureType FromFeatureType(FeatureType type) {
@@ -49,9 +48,8 @@ proto::FeatureType FromFeatureType(FeatureType type) {
     case FeatureType::kTest:
       return proto::FeatureType::TEST;
     default:
-      NOTREACHED();
+      return static_cast<proto::FeatureType>(type);
   }
-  return proto::FeatureType::INVALID;
 }
 
 }  // namespace
