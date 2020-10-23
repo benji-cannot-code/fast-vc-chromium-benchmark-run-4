da68ff5df3955c8d9047e6d5206d4263df929aac
John Lee <johntlee@chromium.org>
2020-10-23 22:59:58 +0000

    WebUI Tab Strip: Update dropEffect on dataTransfer for dragged tabs
    The WebUI tab strip is a tab strip built using JavaScript. It allows
    users to drag tabs, and to do so, it relies on JS drag events. The JS
    checks the dropEffect on a DragEvent's dataTransfer object to determine
    if a tab was moved.
    
    This CL overrides the dropEffect to `move` when dragging a tab using the
    WebUI tab strip to allow the front-end to correctly catch cases in which
    a tab is dragged into its own window.
    
    Bug: 1081905
    Change-Id: Ief542c8eeb15a8324efbcc26cbde366ad84ba029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2488741
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820473}

--- ash/drag_drop/drag_drop_controller_unittest.cc
+++ ash/drag_drop/drag_drop_controller_unittest.cc
@@ -9,14 +9,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/drag_drop/drag_drop_tracker.h"
 #include "ash/drag_drop/drag_image_view.h"
+#include "ash/public/cpp/ash_features.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
+#include "ash/test_shell_delegate.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/location.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "ui/aura/client/capture_client.h"
 #include "ui/aura/client/drag_drop_client_observer.h"
 #include "ui/aura/client/drag_drop_delegate.h"
@@ -39,6 +43,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia_rep.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
+
+using ::testing::_;
+using ::testing::NiceMock;
+using ::testing::Return;
 
 namespace ash {
 
@@ -294,13 +302,29 @@ void DispatchGesture(ui::EventType gesture_type, gfx::Point location) {
 
 }  // namespace
 
+class MockShellDelegate : public TestShellDelegate {
+ public:
+  MockShellDelegate() = default;
+  ~MockShellDelegate() override = default;
+
+  MOCK_METHOD(bool, IsTabDrag, (const ui::OSExchangeData&), (override));
+  MOCK_METHOD(aura::Window*,
+              CreateBrowserForTabDrop,
+              (aura::Window * source_window,
+               const ui::OSExchangeData& drop_data),
+              (override));
+};
+
 class DragDropControllerTest : public AshTestBase {
  public:
   DragDropControllerTest() = default;
   ~DragDropControllerTest() override = default;
 
   void SetUp() override {
-    AshTestBase::SetUp();
+    auto mock_shell_delegate = std::make_unique<NiceMock<MockShellDelegate>>();
+    mock_shell_delegate_ = mock_shell_delegate.get();
+    AshTestBase::SetUp(std::move(mock_shell_delegate));
+
     drag_drop_controller_ = std::make_unique<TestDragDropController>();
     drag_drop_controller_->set_should_block_during_drag_drop(false);
     drag_drop_controller_->set_enabled(true);
@@ -347,6 +371,8 @@ class DragDropControllerTest : public AshTestBase {
     return drag_drop_controller_->drag_drop_tracker_.get();
   }
 
+  MockShellDelegate* mock_shell_delegate() { return mock_shell_delegate_; }
+
   void CompleteCancelAnimation() {
     CompletableLinearAnimation* animation =
         static_cast<CompletableLinearAnimation*>(
@@ -367,6 +393,7 @@ class DragDropControllerTest : public AshTestBase {
   }
 
   std::unique_ptr<TestDragDropController> drag_drop_controller_;
+  NiceMock<MockShellDelegate>* mock_shell_delegate_ = nullptr;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(DragDropControllerTest);
@@ -1266,6 +1293,43 @@ TEST_F(DragDropControllerTest, EventTarget) {
   EXPECT_EQ(EventTargetTestDelegate::State::kPerformDropInvoked,
             delegate.state());
   base::RunLoop().RunUntilIdle();
+}
+
+// Verifies that a tab drag changes the drag operation to a move.
+TEST_F(DragDropControllerTest, DragTabChangesDragOperationToMove) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(features::kWebUITabStripTabDragIntegration);
+
+  EXPECT_CALL(*mock_shell_delegate(), IsTabDrag(_))
+      .Times(1)
+      .WillOnce(Return(true));
+  std::unique_ptr<aura::Window> new_window = CreateToplevelTestWindow();
+  EXPECT_CALL(*mock_shell_delegate(), CreateBrowserForTabDrop(_, _))
+      .Times(1)
+      .WillOnce(Return(new_window.get()));
+
+  std::unique_ptr<views::Widget> widget = CreateFramelessWidget();
+  aura::Window* window = widget->GetNativeWindow();
+
+  // Posted task will be run when the inner loop runs in StartDragAndDrop.
+  ui::test::EventGenerator generator(window->GetRootWindow(), window);
+  generator.PressLeftButton();
+  // For drag enter.
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(&ui::test::EventGenerator::MoveMouseBy,
+                                base::Unretained(&generator), 0, 1));
+  // For perform drop.
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(&ui::test::EventGenerator::ReleaseLeftButton,
+                                base::Unretained(&generator)));
+
+  drag_drop_controller_->set_should_block_during_drag_drop(true);
+  int operation = drag_drop_controller_->StartDragAndDrop(
+      std::make_unique<ui::OSExchangeData>(), window->GetRootWindow(), window,
+      gfx::Point(5, 5), ui::DragDropTypes::DRAG_NONE,
+      ui::mojom::DragEventSource::kMouse);
+
+  EXPECT_EQ(operation, ui::DragDropTypes::DRAG_MOVE);
 }
 
 }  // namespace ash
