9db0543e1d7f2d927e23671379b6fcd29d1d48ee
Rohit Agarwal <roagarwal@chromium.org>
2020-10-23 16:27:37 +0000

    Audit Profile usage in OfflinePageArchivePublisherBridgeTest
    This CL explictly makes uses of Regular profile in the tests as
    OfflinePageBridge is null for OTR profiles and it's already tested in
    OfflinePageBridgeTest
    
    Bug: 1060940
    Change-Id: If215f7ea0beee9a1c5d7cdb20863a8df321dc6c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494860
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820278}

--- chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/OfflinePageArchivePublisherBridgeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/OfflinePageArchivePublisherBridgeTest.java
@@ -54,17 +54,13 @@ public class OfflinePageArchivePublisherBridgeTest {
     private OfflinePageBridge mOfflinePageBridge;
     private EmbeddedTestServer mTestServer;
     private String mTestPage;
+    private Profile mProfile;
 
-    private void initializeBridgeForProfile(final boolean incognitoProfile)
-            throws InterruptedException {
+    private void initializeBridgeForProfile() throws InterruptedException {
         final Semaphore semaphore = new Semaphore(0);
         PostTask.runOrPostTask(UiThreadTaskTraits.DEFAULT, () -> {
-            Profile profile = Profile.getLastUsedRegularProfile();
-            if (incognitoProfile) {
-                profile = profile.getOffTheRecordProfile();
-            }
             // Ensure we start in an offline state.
-            mOfflinePageBridge = OfflinePageBridge.getForProfile(profile);
+            mOfflinePageBridge = OfflinePageBridge.getForProfile(mProfile);
             if (mOfflinePageBridge == null || mOfflinePageBridge.isOfflinePageModelLoaded()) {
                 semaphore.release();
                 return;
@@ -78,7 +74,6 @@ public class OfflinePageArchivePublisherBridgeTest {
             });
         });
         Assert.assertTrue(semaphore.tryAcquire(TIMEOUT_MS, TimeUnit.MILLISECONDS));
-        if (!incognitoProfile) Assert.assertNotNull(mOfflinePageBridge);
     }
 
     @Before
@@ -93,7 +88,10 @@ public class OfflinePageArchivePublisherBridgeTest {
             }
         });
 
-        initializeBridgeForProfile(false);
+        TestThreadUtils.runOnUiThreadBlocking(
+                () -> { mProfile = Profile.getLastUsedRegularProfile(); });
+        initializeBridgeForProfile();
+        Assert.assertNotNull(mOfflinePageBridge);
 
         mTestServer = EmbeddedTestServer.createAndStartServer(InstrumentationRegistry.getContext());
         mTestPage = mTestServer.getURL(TEST_PAGE);
