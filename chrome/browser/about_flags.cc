707c742441f8bd037dc37e27e21f2f534e0643d4
Yu Su <yusuyoutube@google.com>
2020-10-23 07:15:21 +0000

    Clean up deprecated code for Lens shopping menu item + Prime
    - remove the temporary implementation for shopping menu item with Prime
    - delete #classifyImage from LensController. The clank implementation is deleted in https://chrome-internal-review.googlesource.com/c/clank/internal/apps/+/3355632
    
    Change-Id: I4d772729c77f20f8a693f23aa30618c212ac5037
    Bug: b/171421941
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2493002
    Commit-Queue: Yu Su <yusuyoutube@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Ben Goldberger <benwgold@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#820145}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1831,9 +1831,6 @@ const FeatureEntry::FeatureParam kContextMenuShopImageWithGoogleLens[] = {
     {"lensShopVariation", "ShopImageWithGoogleLens"}};
 const FeatureEntry::FeatureParam kContextMenuSearchSimilarProducts[] = {
     {"lensShopVariation", "SearchSimilarProducts"}};
-const FeatureEntry::FeatureParam
-    kContextMenuShopImageWithGoogleLensShoppyImage[] = {
-        {"lensShopVariation", "ShopImageWithGoogleLensShoppyImage"}};
 
 const FeatureEntry::FeatureVariation
     kContextMenuShopWithGoogleLensShopVariations[] = {
@@ -1842,10 +1839,7 @@ const FeatureEntry::FeatureVariation
         {"ShopImageWithGoogleLens", kContextMenuShopImageWithGoogleLens,
          base::size(kContextMenuShopImageWithGoogleLens), nullptr},
         {"SearchSimilarProducts", kContextMenuSearchSimilarProducts,
-         base::size(kContextMenuSearchSimilarProducts), nullptr},
-        {"ShopImageWithGoogleLensShoppyImage",
-         kContextMenuShopImageWithGoogleLensShoppyImage,
-         base::size(kContextMenuShopImageWithGoogleLensShoppyImage), nullptr}};
+         base::size(kContextMenuSearchSimilarProducts), nullptr}};
 #endif  // defined(OS_ANDROID)
 
 const FeatureEntry::FeatureParam kLazyFrameLoadingAutomatic[] = {
