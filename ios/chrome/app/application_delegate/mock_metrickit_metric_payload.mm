01350ba5971320bf4f44a81ae69ee79d185aa560
Justin Cohen <justincohen@google.com>
2020-10-23 16:42:52 +0000

    ios: Use mainBundle bundleVersion as part of version_lock workaround.
    Bug: 1140474
    Change-Id: I264270edec37ffa425a1525af5a95a9e53009310
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495005
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820288}

--- ios/chrome/app/application_delegate/mock_metrickit_metric_payload.mm
+++ ios/chrome/app/application_delegate/mock_metrickit_metric_payload.mm
@@ -18,9 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 id MockMXMetadata() API_AVAILABLE(ios(13.0)) {
+  // TODO(crbug.com/1140474): See related bug for why |bundleVersion| comes from
+  // mainBundle instead of from version_info::GetVersionNumber(). Remove once
+  // iOS 14.2 reaches mass adoption.
+  NSString* bundleVersion =
+      [[NSBundle mainBundle] infoDictionary][(NSString*)kCFBundleVersionKey];
   id metadata = OCMClassMock([MXMetaData class]);
-  OCMStub([metadata applicationBuildVersion])
-      .andReturn(base::SysUTF8ToNSString(version_info::GetVersionNumber()));
+  OCMStub([metadata applicationBuildVersion]).andReturn(bundleVersion);
   return metadata;
 }
 
