47f5a03534eb7cdf8adc1cb56928665a83b460dc
Nate Chapin <japhet@chromium.org>
2020-10-23 22:07:25 +0000

    Merge NavigatorHID and HID
    HID becomes a Supplement<Navigator> instead of an
    ExecutionContextClient. This means it retains a references
    to the Navigator, which is itself an ExecutionContextClient,
    so it still has access to the ExecutionContext.
    
    Change-Id: Ie1d789fb6588c816f39c5b0645d1973aee43caef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495503
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820450}

--- third_party/blink/renderer/modules/hid/hid.cc
+++ third_party/blink/renderer/modules/hid/hid.cc
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/frame/navigator.h"
 #include "third_party/blink/renderer/modules/event_target_modules.h"
 #include "third_party/blink/renderer/modules/hid/hid_connection_event.h"
 #include "third_party/blink/renderer/modules/hid/hid_device.h"
@@ -83,12 +85,27 @@ mojom::blink::HidDeviceFilterPtr ConvertDeviceFilter(
 
 }  // namespace
 
-HID::HID(ExecutionContext& context)
-    : ExecutionContextClient(&context),
-      service_(&context),
-      feature_handle_for_scheduler_(context.GetScheduler()->RegisterFeature(
-          SchedulingPolicy::Feature::kWebHID,
-          {SchedulingPolicy::RecordMetricsForBackForwardCache()})) {}
+const char HID::kSupplementName[] = "HID";
+
+HID* HID::hid(Navigator& navigator) {
+  if (!navigator.DomWindow())
+    return nullptr;
+
+  HID* hid = Supplement<Navigator>::From<HID>(navigator);
+  if (!hid) {
+    hid = MakeGarbageCollected<HID>(navigator);
+    ProvideTo(navigator, hid);
+  }
+  return hid;
+}
+
+HID::HID(Navigator& navigator)
+    : Supplement<Navigator>(navigator),
+      service_(navigator.DomWindow()),
+      feature_handle_for_scheduler_(
+          navigator.DomWindow()->GetScheduler()->RegisterFeature(
+              SchedulingPolicy::Feature::kWebHID,
+              {SchedulingPolicy::RecordMetricsForBackForwardCache()})) {}
 
 HID::~HID() {
   DCHECK(get_devices_promises_.IsEmpty());
@@ -96,7 +113,7 @@ HID::~HID() {
 }
 
 ExecutionContext* HID::GetExecutionContext() const {
-  return ExecutionContextClient::GetExecutionContext();
+  return GetSupplementable()->DomWindow();
 }
 
 const AtomicString& HID::InterfaceName() const {
@@ -165,8 +182,7 @@ ScriptPromise HID::getDevices(ScriptState* script_state,
 ScriptPromise HID::requestDevice(ScriptState* script_state,
                                  const HIDDeviceRequestOptions* options,
                                  ExceptionState& exception_state) {
-  auto* frame = GetFrame();
-  if (!frame || !frame->GetDocument()) {
+  if (!GetExecutionContext()) {
     exception_state.ThrowDOMException(DOMExceptionCode::kNotSupportedError,
                                       kContextGone);
     return ScriptPromise();
@@ -179,7 +195,8 @@ ScriptPromise HID::requestDevice(ScriptState* script_state,
     return ScriptPromise();
   }
 
-  if (!LocalFrame::HasTransientUserActivation(frame)) {
+  if (!LocalFrame::HasTransientUserActivation(
+          GetSupplementable()->DomWindow()->GetFrame())) {
     exception_state.ThrowSecurityError(
         "Must be handling a user gesture to show a permission request.");
     return ScriptPromise();
@@ -290,7 +307,7 @@ void HID::Trace(Visitor* visitor) const {
   visitor->Trace(request_device_promises_);
   visitor->Trace(device_cache_);
   EventTargetWithInlineData::Trace(visitor);
-  ExecutionContextClient::Trace(visitor);
+  Supplement<Navigator>::Trace(visitor);
 }
 
 }  // namespace blink
