2f54879275b7cf80bd13b80e983b75daeb5a5f8c
Vidhan <vidhanj@google.com>
2020-10-23 23:01:08 +0000

    [Autofill] Remove TODO(crbug.com/904390) comment
    Bug: 904390
    Change-Id: If057d964a0e4224233aa074b6172b98793cae98e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494812
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/master@{#820474}

--- components/autofill/core/browser/autofill_profile_sync_util.h
+++ components/autofill/core/browser/autofill_profile_sync_util.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
-// TODO(crbug.com/904390): Remove when the investigation is over.
-#include <vector>
 
 namespace syncer {
 struct EntityData;
