8347ad2e41a0ed439a6c1b93984ee3447a81e519
Liquan (Max) Gu <maxlg@chromium.org>
2020-10-23 01:25:43 +0000

    [WebLayer] Move journey_logger_android into //components
    Before the CL, //components' JourneyLogger depends on //chrome's
    journey_logger_android. This disallows WebLayer from depending on
    JourneyLogger.
    
    So this CL moves journey_logger_android into //components.
    
    After the CL, WebLayer can depend on //components' JourneyLogger.
    
    Bug: 1140637
    
    Change-Id: I881fc8dd65071907e424f1184ea221f5bca0d147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2491606
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820086}

--- components/payments/content/android/journey_logger_android.cc
+++ components/payments/content/android/journey_logger_android.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/payments/android/journey_logger_android.h"
+#include "components/payments/content/android/journey_logger_android.h"
 
 #include "base/android/jni_string.h"
 #include "components/payments/content/android/jni_headers/JourneyLogger_jni.h"
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace payments {
 namespace {
 
-using ::base::android::JavaParamRef;
 using ::base::android::ConvertJavaStringToUTF8;
+using ::base::android::JavaParamRef;
 
 }  // namespace
 
--- components/payments/content/android/journey_logger_android.h
+++ components/payments/content/android/journey_logger_android.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PAYMENTS_ANDROID_JOURNEY_LOGGER_ANDROID_H_
-#define CHROME_BROWSER_PAYMENTS_ANDROID_JOURNEY_LOGGER_ANDROID_H_
+#ifndef COMPONENTS_PAYMENTS_CONTENT_ANDROID_JOURNEY_LOGGER_ANDROID_H_
+#define COMPONENTS_PAYMENTS_CONTENT_ANDROID_JOURNEY_LOGGER_ANDROID_H_
 
 #include "base/android/scoped_java_ref.h"
 #include "base/macros.h"
@@ -85,4 +85,4 @@ class JourneyLoggerAndroid {
 
 }  // namespace payments
 
-#endif  // CHROME_BROWSER_PAYMENTS_ANDROID_JOURNEY_LOGGER_ANDROID_H_
+#endif  // COMPONENTS_PAYMENTS_CONTENT_ANDROID_JOURNEY_LOGGER_ANDROID_H_
