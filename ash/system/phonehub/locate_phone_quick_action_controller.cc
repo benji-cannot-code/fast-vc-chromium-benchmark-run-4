d00f25f6ca00e5ed8b3085d4a18c93ab0bfedf20
Andre Le <leandre@chromium.org>
2020-10-23 19:29:45 +0000

    Reland "[CrOS PhoneHub] Disable Locate Phone button when Silence Phone is on."
    This is a reland of 55f62f27a4121702ed00aa5cce1c90b70fad39bd
    
    BUG=1140857,1140980
    
    Original change's description:
    > [CrOS PhoneHub] Disable Locate Phone button when Silence Phone is on.
    >
    > - Add an observer for state change in Silence Phone so that Locate Phone
    > can be disable when Silence Phone is on.
    > - Refactor InitQuickActionsItems() since Locate Phone and Silence Phone
    > is no longer independent.
    >
    > BUG=1106937,1126208
    >
    > Change-Id: I6f015e868d6d00df6a40447de62f3efd0b2f2ad0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2480662
    > Reviewed-by: Tim Song <tengs@chromium.org>
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Commit-Queue: Andre Le <leandre@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#819161}
    
    Bug: 1106937
    Bug: 1126208
    Change-Id: I3bcae322124af80f0947a26c7b0e776757f2ccfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490380
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820376}

--- ash/system/phonehub/locate_phone_quick_action_controller.cc
+++ ash/system/phonehub/locate_phone_quick_action_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/phonehub/quick_action_item.h"
+#include "ash/system/phonehub/silence_phone_quick_action_controller.h"
 #include "base/timer/timer.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -25,14 +26,19 @@ constexpr base::TimeDelta kWaitForRequestTimeout =
 using Status = chromeos::phonehub::FindMyDeviceController::Status;
 
 LocatePhoneQuickActionController::LocatePhoneQuickActionController(
-    chromeos::phonehub::FindMyDeviceController* find_my_device_controller)
-    : find_my_device_controller_(find_my_device_controller) {
+    chromeos::phonehub::FindMyDeviceController* find_my_device_controller,
+    SilencePhoneQuickActionController* silence_phone_controller)
+    : find_my_device_controller_(find_my_device_controller),
+      silence_phone_controller_(silence_phone_controller) {
   DCHECK(find_my_device_controller_);
+  DCHECK(silence_phone_controller_);
   find_my_device_controller_->AddObserver(this);
+  silence_phone_controller_->AddObserver(this);
 }
 
 LocatePhoneQuickActionController::~LocatePhoneQuickActionController() {
   find_my_device_controller_->RemoveObserver(this);
+  silence_phone_controller_->RemoveObserver(this);
 }
 
 QuickActionItem* LocatePhoneQuickActionController::CreateItem() {
@@ -56,20 +62,35 @@ void LocatePhoneQuickActionController::OnButtonPressed(bool is_now_enabled) {
   find_my_device_controller_->RequestNewPhoneRingingState(!is_now_enabled);
 }
 
+void LocatePhoneQuickActionController::OnSilencePhoneItemStateChanged() {
+  is_silence_enabled_ = silence_phone_controller_->IsItemEnabled();
+  UpdateState();
+}
+
 void LocatePhoneQuickActionController::OnPhoneRingingStateChanged() {
-  switch (find_my_device_controller_->GetPhoneRingingStatus()) {
-    case Status::kRingingOff:
-      state_ = ActionState::kOff;
-      break;
-    case Status::kRingingOn:
-      state_ = ActionState::kOn;
-      break;
-    case Status::kRingingNotAvailable:
-      item_->SetEnabled(false);
-      return;
+  UpdateState();
+}
+
+void LocatePhoneQuickActionController::UpdateState() {
+  // Disable Locate Phone if Silence Phone is on, otherwise change accordingly
+  // based on status from FindMyDeviceController.
+  if (is_silence_enabled_) {
+    state_ = ActionState::kNotAvailable;
+  } else {
+    switch (find_my_device_controller_->GetPhoneRingingStatus()) {
+      case Status::kRingingOff:
+        state_ = ActionState::kOff;
+        break;
+      case Status::kRingingOn:
+        state_ = ActionState::kOn;
+        break;
+      case Status::kRingingNotAvailable:
+        state_ = ActionState::kNotAvailable;
+        break;
+    }
   }
+
   SetItemState(state_);
-  item_->SetEnabled(true);
 
   // If |requested_state_| correctly resembles the current state, reset it and
   // the timer.
@@ -84,6 +105,9 @@ void LocatePhoneQuickActionController::SetItemState(ActionState state) {
   int state_text_id;
   int sub_label_text;
   switch (state) {
+    case ActionState::kNotAvailable:
+      item_->SetEnabled(false);
+      return;
     case ActionState::kOff:
       icon_enabled = false;
       state_text_id = IDS_ASH_PHONE_HUB_QUICK_ACTIONS_DISABLED_STATE_TOOLTIP;
@@ -96,6 +120,7 @@ void LocatePhoneQuickActionController::SetItemState(ActionState state) {
       break;
   }
 
+  item_->SetEnabled(true);
   item_->SetToggled(icon_enabled);
   item_->SetSubLabel(l10n_util::GetStringUTF16(sub_label_text));
   base::string16 tooltip_state =
