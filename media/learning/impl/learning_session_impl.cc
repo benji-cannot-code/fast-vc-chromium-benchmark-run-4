5f59aa955103179c7122f29ec6639026642af25e
Daniel Cheng <dcheng@chromium.org>
2020-10-23 08:55:56 +0000

    SequenceBound: migrate from Post() to AsyncCall() in media classes.
    Bug: 1140588
    Change-Id: I2c55eeae2125a4c4dea4683a2a017723c3f14e22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2488320
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820173}

--- media/learning/impl/learning_session_impl.cc
+++ media/learning/impl/learning_session_impl.cc
@@ -38,12 +38,11 @@ class WeakLearningTaskController : public LearningTaskController {
     for (auto& id : outstanding_observations_) {
       const base::Optional<TargetValue>& default_value = id.second;
       if (default_value) {
-        controller_->Post(FROM_HERE,
-                          &LearningTaskController::CompleteObservation,
-                          id.first, *default_value);
+        controller_->AsyncCall(&LearningTaskController::CompleteObservation)
+            .WithArgs(id.first, *default_value);
       } else {
-        controller_->Post(FROM_HERE, &LearningTaskController::CancelObservation,
-                          id.first);
+        controller_->AsyncCall(&LearningTaskController::CancelObservation)
+            .WithArgs(id.first);
       }
     }
   }
@@ -60,8 +59,8 @@ class WeakLearningTaskController : public LearningTaskController {
     // We don't send along the default value because LearningTaskControllerImpl
     // doesn't support it.  Since all client calls eventually come through us
     // anyway, it seems okay to handle it here.
-    controller_->Post(FROM_HERE, &LearningTaskController::BeginObservation, id,
-                      features, base::nullopt, source_id);
+    controller_->AsyncCall(&LearningTaskController::BeginObservation)
+        .WithArgs(id, features, base::nullopt, source_id);
   }
 
   void CompleteObservation(base::UnguessableToken id,
@@ -69,16 +68,16 @@ class WeakLearningTaskController : public LearningTaskController {
     if (!weak_session_)
       return;
     outstanding_observations_.erase(id);
-    controller_->Post(FROM_HERE, &LearningTaskController::CompleteObservation,
-                      id, completion);
+    controller_->AsyncCall(&LearningTaskController::CompleteObservation)
+        .WithArgs(id, completion);
   }
 
   void CancelObservation(base::UnguessableToken id) override {
     if (!weak_session_)
       return;
     outstanding_observations_.erase(id);
-    controller_->Post(FROM_HERE, &LearningTaskController::CancelObservation,
-                      id);
+    controller_->AsyncCall(&LearningTaskController::CancelObservation)
+        .WithArgs(id);
   }
 
   void UpdateDefaultTarget(
@@ -94,8 +93,8 @@ class WeakLearningTaskController : public LearningTaskController {
 
   void PredictDistribution(const FeatureVector& features,
                            PredictionCB callback) override {
-    controller_->Post(FROM_HERE, &LearningTaskController::PredictDistribution,
-                      features, std::move(callback));
+    controller_->AsyncCall(&LearningTaskController::PredictDistribution)
+        .WithArgs(features, std::move(callback));
   }
 
   base::WeakPtr<LearningSessionImpl> weak_session_;
