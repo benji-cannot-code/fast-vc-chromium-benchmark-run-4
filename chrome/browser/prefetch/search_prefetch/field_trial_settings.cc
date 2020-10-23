1c5353ecd9e68099c2a2bb0681ceaf41310a354c
Ryan Sturm <ryansturm@chromium.org>
2020-10-23 21:26:04 +0000

    Adding an experiment arm for only prefetching the default AC match
    When there is a match above the search prefetch match, it should not be
    prefetched in this arm.
    
    Bug: 1138649
    Change-Id: I2854284607718659b73323c9994cf19af13ff272
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2493432
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820428}

--- chrome/browser/prefetch/search_prefetch/field_trial_settings.cc
+++ chrome/browser/prefetch/search_prefetch/field_trial_settings.cc
@@ -49,3 +49,8 @@ base::TimeDelta SearchPrefetchErrorBackoffDuration() {
                                              "error_backoff_duration_ms",
                                              60000));
 }
+
+bool SearchPrefetchOnlyFetchDefaultMatch() {
+  return base::GetFieldTrialParamByFeatureAsBool(
+      kSearchPrefetchServicePrefetching, "only_prefetch_default_match", false);
+}
