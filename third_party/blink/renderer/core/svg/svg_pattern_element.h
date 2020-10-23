139786c6b185fc579508f2bffe9f806dde056b33
Fredrik Söderqvist <fs@opera.com>
2020-10-23 09:26:46 +0000

    Fold SVGResourcesCache::ResourceReferenceChanged
    Fold SVGResourcesCache::ResourceReferenceChanged into its two users,
    greatly tailoring the resulting code for one user (SVGPatternElement).
    Expose a function for updating the SVGResources object.
    
    Bug: 1028063
    Change-Id: I439ca043c659bad7dbb3fa89d9a4d3d9578f7c17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2489650
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#820179}

--- third_party/blink/renderer/core/svg/svg_pattern_element.h
+++ third_party/blink/renderer/core/svg/svg_pattern_element.h
@@ -77,6 +77,7 @@ class SVGPatternElement final : public SVGElement,
   }
 
   void InvalidatePattern(LayoutInvalidationReasonForTracing);
+  void InvalidateDependentPatterns();
 
   const SVGPatternElement* ReferencedElement() const;
 
