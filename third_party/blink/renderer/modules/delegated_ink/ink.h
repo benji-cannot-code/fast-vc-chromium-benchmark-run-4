f17575938494a9c15f72cad54fccf39687161117
Nate Chapin <japhet@chromium.org>
2020-10-23 00:59:29 +0000

    Merge NavigatorInk and Ink
    Change-Id: Iefa5a511d95b711458409170d908f1907f24cf28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2493095
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820075}

--- third_party/blink/renderer/modules/delegated_ink/ink.h
+++ third_party/blink/renderer/modules/delegated_ink/ink.h
@@ -8,28 +8,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/delegated_ink/delegated_ink_trail_presenter.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
 
 class Element;
-class LocalFrame;
+class Navigator;
 class ScriptPromise;
 class ScriptState;
 
-class Ink : public ScriptWrappable {
+class Ink : public ScriptWrappable, public Supplement<Navigator> {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  explicit Ink(LocalFrame* frame);
+  static const char kSupplementName[];
+  static Ink* ink(Navigator& navigator);
+
+  explicit Ink(Navigator&);
   ScriptPromise requestPresenter(ScriptState* state,
                                  String type,
                                  Element* presentationArea = nullptr);
 
   void Trace(blink::Visitor*) const override;
-
- private:
-  Member<LocalFrame> local_frame_;
 };
 
 }  // namespace blink
