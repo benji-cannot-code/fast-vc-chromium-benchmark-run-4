76e57d8d68ce0e1972d6cf087bf04aa7f2e97f6a
Liquan (Max) Gu <maxlg@chromium.org>
2020-10-24 04:03:26 +0000

    Compile //components payment into //weblayer
    Changes:
    
    * This CL compiled the //components payment targets into //weblayer.
    
    * Created WebLayerPaymentRequestFactory as an entry into java
    payment request service from web layer.
    
    * Added a base::Feature "WebLayerPayments" in PaymentFeatureList; used
    it in WebLayerPaymentRequestFactory to prevent accidental calling
    the WebLayer payment request service under construction.
    
    * Shared ChromePaymentRequestFactory#InvalidPaymentRequest with
    WebLayerPaymentRequestFactory, and stored it in the newly created
    PaymentRequestFactoryUtils.
    
    Bug: 1140798
    
    Change-Id: I1afded32372400cce62a36041002e2247e7caa73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2491124
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820542}

--- components/payments/content/android/payment_feature_list.h
+++ components/payments/content/android/payment_feature_list.h
@@ -15,6 +15,7 @@ namespace android {
 // Android only payment features in alphabetical order:
 extern const base::Feature kAndroidAppPaymentUpdateEvents;
 extern const base::Feature kScrollToExpandPaymentHandler;
+extern const base::Feature kWebLayerPayments;
 
 }  // namespace android
 }  // namespace payments
---
+++ weblayer/browser/java/org/chromium/weblayer_private/payments/WebLayerPaymentRequestFactory.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer_private.payments;
+
+import org.chromium.components.payments.InvalidPaymentRequest;
+import org.chromium.components.payments.PaymentFeatureList;
+import org.chromium.content_public.browser.RenderFrameHost;
+import org.chromium.payments.mojom.PaymentRequest;
+import org.chromium.services.service_manager.InterfaceFactory;
+
+/** Creates an instance of PaymentRequest for use in WebLayer. */
+public class WebLayerPaymentRequestFactory implements InterfaceFactory<PaymentRequest> {
+    private final RenderFrameHost mRenderFrameHost;
+
+    /**
+     * Creates an instance of WebLayerPaymentRequestFactory.
+     * @param renderFrameHost The frame that issues the payment request on the merchant page.
+     */
+    public WebLayerPaymentRequestFactory(RenderFrameHost renderFrameHost) {
+        mRenderFrameHost = renderFrameHost;
+    }
+
+    @Override
+    public PaymentRequest createImpl() {
+        if (!PaymentFeatureList.isEnabled(PaymentFeatureList.WEB_LAYER_PAYMENTS)) {
+            return new InvalidPaymentRequest();
+        }
+
+        // TODO(maxlg): replace the returned value with a PaymentRequest.
+        return null;
+    }
+}
