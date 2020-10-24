6ba7714f925871d9d44f4786a44e224094510d7c
Jesse Schettler <jschettler@chromium.org>
2020-10-24 01:59:08 +0000

    scanning: Change SelectBehavior property to public
    A private property of SelectBehavior is referenced outside the behavior,
    so change it from private to public.
    
    Bug: 1059779
    Change-Id: I5ef3dbf338393540ab0c4905f2f3122e7e758150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495407
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820530}

--- chromeos/components/scanning/resources/file_type_select.js
+++ chromeos/components/scanning/resources/file_type_select.js
@@ -34,6 +34,6 @@ Polymer({
   /** @override */
   created() {
     // The dropdown always contains one option per FileType.
-    this.numOptions = chromeos.scanning.mojom.FileType.MAX_VALUE + 1;
+    this.onNumOptionsChange(chromeos.scanning.mojom.FileType.MAX_VALUE + 1);
   },
 });
