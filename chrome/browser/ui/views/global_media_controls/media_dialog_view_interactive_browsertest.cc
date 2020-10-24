e43eeb1973681ea6c4cee7a4fb57ad2419313124
Muyao Xu <muyaoxu@google.com>
2020-10-24 00:24:14 +0000

    Add logs in CastToolbarButton
    This CL adds logs every time |has_local_display_route_|'s value changes.
    When its value is true, the cast toolbar icon should be blue, unless
    there are FATL or WARNING issues.
    
    Bug: b/165366974, 1117673
    Change-Id: I082fadb913cdad4527020f2f10f06051d610deee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2472531
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820503}

--- chrome/browser/ui/views/global_media_controls/media_dialog_view_interactive_browsertest.cc
+++ chrome/browser/ui/views/global_media_controls/media_dialog_view_interactive_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/media/router/chrome_media_router_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/global_media_controls/media_toolbar_button_observer.h"
@@ -274,6 +275,12 @@ class TestMediaRouter : public media_router::MockMediaRouter {
     return std::make_unique<TestMediaRouter>();
   }
 
+  media_router::LoggerImpl* GetLogger() override {
+    if (!logger_)
+      logger_ = std::make_unique<media_router::LoggerImpl>();
+    return logger_.get();
+  }
+
   void RegisterMediaRoutesObserver(
       media_router::MediaRoutesObserver* observer) override {
     routes_observers_.push_back(observer);
@@ -292,6 +299,7 @@ class TestMediaRouter : public media_router::MockMediaRouter {
 
  private:
   std::vector<media_router::MediaRoutesObserver*> routes_observers_;
+  std::unique_ptr<media_router::LoggerImpl> logger_;
 };
 
 }  // anonymous namespace
