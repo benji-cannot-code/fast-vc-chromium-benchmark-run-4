b06106a969288cbca6fff512612521390ba1e99b
Ryan Sturm <ryansturm@chromium.org>
2020-10-24 22:03:56 +0000

    Canceling in-flight prefetch requests if they become irrelevant
    When the autocomplete provider no longer has a match for a prefetch
    request, it can be cancelled if the request is in flight. This reduces
    network contention for navigations or changing user action away from
    omnibox.
    
    Bug: 1138638
    Change-Id: I7f7b5a1cc65fe22b4d3ba3055d0d658061212cab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2493726
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820589}

--- chrome/browser/prefetch/search_prefetch/search_prefetch_service.h
+++ chrome/browser/prefetch/search_prefetch/search_prefetch_service.h
@@ -29,6 +29,8 @@ enum class SearchPrefetchStatus {
   kSuccessfullyCompleted = 2,
   // The request hit an error and cannot be served.
   kRequestFailed = 3,
+  // The request was cancelled before completion.
+  kRequestCancelled = 4,
 };
 
 class SearchPrefetchService : public KeyedService {
@@ -74,6 +76,9 @@ class SearchPrefetchService : public KeyedService {
 
     // Starts the network request to prefetch |prefetch_url_|.
     void StartPrefetchRequest(Profile* profile);
+
+    // Cancels the on-going prefetch and marks the status appropriately.
+    void CancelPrefetch();
 
     SearchPrefetchStatus current_status() const { return current_status_; }
 
