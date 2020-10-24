0656623a2a8fe3eed5aba1a47e713a5367ecc47f
Michael Irani <michaelirani@chromium.org>
2020-10-24 02:15:55 +0000

    Fix ID mismatch in keyboard.js
    - Mismatch introduced in 2487866
    
    Bug: 1029954
    Test: Manual
    Change-Id: I422bc73c0f04232f011e6f29835b3ae304348ae7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2496037
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Michael Irani <michaelirani@chromium.org>
    Auto-Submit: Michael Irani <michaelirani@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820532}

--- chrome/browser/resources/settings/chromeos/device_page/keyboard.js
+++ chrome/browser/resources/settings/chromeos/device_page/keyboard.js
@@ -189,7 +189,7 @@ cr.define('settings', function() {
         id = '#showLanguagesInput';
       } else {
         path = settings.routes.OS_LANGUAGES_DETAILS.path;
-        id = '#showLanguageDetails';
+        id = '#showLanguagesDetails';
       }
 
       this.focusConfig.set(path, () => {
