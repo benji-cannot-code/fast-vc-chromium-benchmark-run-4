1b5fa9c66056865431bd39a206190d19ad3d63b4
Melissa Zhang <melzhang@chromium.org>
2020-10-25 23:22:05 +0000

    [Sharesheet] Override AccessibleWindowRole.
    This CL overrides AccessibleWindowRole to |kDialog| because
    BubbleDialogDelegateView sets it to |kAlertDialog|. |AlertDialog|
    triggers screenreaders to repeat the whole of the
    |sharesheet_bubble_view| which is undesirable behaviour that
    is resolved with |kDialog|.
    
    Bug: 1140272
    Change-Id: Ie42811cc95a39af5f12268314117a6dc9ea189a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494190
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820640}

--- chrome/browser/ui/views/sharesheet/sharesheet_bubble_view.cc
+++ chrome/browser/ui/views/sharesheet/sharesheet_bubble_view.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/app_window/app_window.h"
 #include "extensions/browser/app_window/app_window_registry.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/accessibility/ax_enums.mojom-forward.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/font_list.h"
@@ -328,6 +329,13 @@ void SharesheetBubbleView::OnKeyEvent(ui::KeyEvent* event) {
   View::OnKeyEvent(event);
 }
 
+ax::mojom::Role SharesheetBubbleView::GetAccessibleWindowRole() {
+  // We override the role because the base class sets it to alert dialog.
+  // This would make screen readers repeatedly announce the whole of the
+  // |sharesheet_bubble_view| which is undesirable.
+  return ax::mojom::Role::kDialog;
+}
+
 std::unique_ptr<views::NonClientFrameView>
 SharesheetBubbleView::CreateNonClientFrameView(views::Widget* widget) {
   auto bubble_border =
@@ -338,6 +346,10 @@ SharesheetBubbleView::CreateNonClientFrameView(views::Widget* widget) {
   static_cast<views::BubbleFrameView*>(frame.get())
       ->SetBubbleBorder(std::move(bubble_border));
   return frame;
+}
+
+gfx::Size SharesheetBubbleView::CalculatePreferredSize() const {
+  return gfx::Size(width_, height_);
 }
 
 void SharesheetBubbleView::OnWidgetDestroyed(views::Widget* widget) {
@@ -351,10 +363,6 @@ void SharesheetBubbleView::OnWidgetDestroyed(views::Widget* widget) {
   if (close_callback_) {
     std::move(close_callback_).Run(sharesheet::SharesheetResult::kCancel);
   }
-}
-
-gfx::Size SharesheetBubbleView::CalculatePreferredSize() const {
-  return gfx::Size(width_, height_);
 }
 
 void SharesheetBubbleView::CreateBubble() {
