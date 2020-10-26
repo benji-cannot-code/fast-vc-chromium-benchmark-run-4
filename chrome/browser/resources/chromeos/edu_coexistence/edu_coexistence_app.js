036c08f25deb3c3781265767eb276cb941b68c29
Danan S <danan@chromium.org>
2020-10-26 18:48:54 +0000

    Add EDU Coexistence ChromeOS UI elements and behaviors
    This CL adds:
    - Polymer template for all the screens of the UI
    - Shared styles for all the screens
    - Button templates and behaviors
    - Error screen
    - Network offline screens
    
    Also:
    - UI behaviors and event handler implementation to enable the flow to operate.
    These are mostly in:
    chrome/browser/resources/chromeos/edu_coexistence/edu_coexistence_ui.js
    chrome/browser/resources/chromeos/edu_coexistence/edu_coexistence_app.js
    
    But there are smaller bits of functionality in the other js files as
    required by their respective screens.
    
    Bug: 1133828,1133827,1123218
    Change-Id: I297fe1a5c18bb3806d94b4c5e635c0dd4a990f70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462673
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Dan S <danan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820844}

--- chrome/browser/resources/chromeos/edu_coexistence/edu_coexistence_app.js
+++ chrome/browser/resources/chromeos/edu_coexistence/edu_coexistence_app.js
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './edu_coexistence_css.js';
+import './edu_coexistence_template.js';
+import './edu_coexistence_button.js';
+import './edu_coexistence_error.js';
+import './edu_coexistence_offline.js';
 import './edu_coexistence_ui.js';
 import 'chrome://resources/cr_elements/cr_view_manager/cr_view_manager.m.js';
 
@@ -11,11 +16,12 @@ import {WebUIListenerBehavior} from 'chrome://resources/js/web_ui_listener_behav
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {EduCoexistenceBrowserProxyImpl} from './edu_coexistence_browser_proxy.js';
 
-
-// import {EduAccountLoginBrowserProxyImpl} from './browser_proxy.js';
-// import {EduCoexistenceFlowResult, EduLoginErrorType, EduLoginParams,
-// ParentAccount} from './edu_login_util.js';
-//
+/** @enum {string} */
+const Screens = {
+  ONLINE_FLOW: 'edu-coexistence-ui',
+  ERROR: 'edu-coexistence-error',
+  OFFLINE: 'edu-coexistence-offline',
+};
 
 Polymer({
   is: 'edu-coexistence-app',
@@ -33,15 +39,56 @@ Polymer({
       type: Boolean,
       value: false,
     },
+
+    /**
+     * Specifies what the current screen is.
+     * @private {Screens}
+     */
+    currentScreen_: {type: Screens, value: Screens.ONLINE_FLOW},
+  },
+
+  listeners: {
+    'go-error': 'onError_',
+  },
+
+  /**
+   * Displays the error screen.
+   * @private
+   */
+  onError_() {
+    this.switchToScreen_(Screens.ERROR);
+  },
+
+  /**
+   * Switches to the specified screen.
+   * @private
+   * @param {Screens} screen
+   */
+  switchToScreen_(screen) {
+    if (this.currentScreen_ === screen) {
+      return;
+    }
+    this.currentScreen_ = screen;
+    /** @type {CrViewManagerElement} */ (this.$.viewManager)
+        .switchView(this.currentScreen_);
   },
 
   /** @override */
-  created() {},
-
-  /** @override */
   ready() {
-    /** @type {CrViewManagerElement} */ (this.$.viewManager)
-        .switchView('edu-coexistence-ui');
+    window.addEventListener('online', () => {
+      if (this.currentScreen_ !== Screens.ERROR) {
+        this.switchToScreen_(Screens.ONLINE_FLOW);
+      }
+    });
+
+    window.addEventListener('offline', () => {
+      if (this.currentScreen_ !== Screens.ERROR) {
+        this.switchToScreen_(Screens.OFFLINE);
+      }
+    });
+
+    this.switchToScreen_(
+        navigator.onLine ? Screens.ONLINE_FLOW : Screens.OFFLINE);
   },
 
 });
