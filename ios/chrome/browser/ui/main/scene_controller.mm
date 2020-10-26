d018e9b9ba231cfdf3e80e9874d06fed8fc25066
Eugene But <eugenebut@chromium.org>
2020-10-26 03:20:44 +0000

    [ios] Remove unused showReportAnIssueFromViewController:specificProductData:
    This method is no longer called downstream
    
    Bug: 1138523
    Change-Id: I8c5284f1accba661c2542a5090095d0ea0e20e80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2493426
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820662}

--- ios/chrome/browser/ui/main/scene_controller.mm
+++ ios/chrome/browser/ui/main/scene_controller.mm
@@ -993,15 +993,6 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
 
 - (void)
     showReportAnIssueFromViewController:(UIViewController*)baseViewController
-                    specificProductData:(NSDictionary<NSString*, NSString*>*)
-                                            specificProductData {
-  [self showReportAnIssueFromViewController:baseViewController
-                                     sender:UserFeedbackSender::ToolsMenu
-                        specificProductData:specificProductData];
-}
-
-- (void)
-    showReportAnIssueFromViewController:(UIViewController*)baseViewController
                                  sender:(UserFeedbackSender)sender
                     specificProductData:(NSDictionary<NSString*, NSString*>*)
                                             specificProductData {
