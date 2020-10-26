b8a2ce81ae88e76e778e76e76bf391e4ba69b88d
Patrick Noland <pnoland@chromium.org>
2020-10-26 16:32:34 +0000

    [Sheriff] Disable flaky test TabSwitcherControllerTest#testOpenMenu
    Bug: 1140998
    Change-Id: Ibf5c415e21cc0fd974be55c2628c712ba7205c9e
    TBR: aluo@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2499163
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820773}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/pagecontroller/tests/TabSwitcherControllerTest.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/pagecontroller/tests/TabSwitcherControllerTest.java
@@ -17,6 +17,7 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.test.pagecontroller.controllers.ntp.NewTabPageController;
 import org.chromium.chrome.test.pagecontroller.controllers.tabswitcher.TabSwitcherController;
 import org.chromium.chrome.test.pagecontroller.controllers.tabswitcher.TabSwitcherMenuController;
@@ -77,6 +78,7 @@ public class TabSwitcherControllerTest {
         Assert.assertTrue(NewTabPageController.getInstance().isCurrentPageThis());
     }
 
+    @DisabledTest(message = "https://crbug.com/1140998")
     @Test
     public void testOpenMenu() {
         mController.clickMenu();
