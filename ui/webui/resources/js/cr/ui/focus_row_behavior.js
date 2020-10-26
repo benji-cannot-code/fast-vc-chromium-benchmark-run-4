1ec4025616b2271b0edcffa43218e69865a0c9ca
Jon Mann <jonmann@chromium.org>
2020-10-26 23:02:08 +0000

    Wi-Fi List: Set the active row to use aria-live.
    Previously when details on the active row changed, users with a
    screenreader were not notified.  This change allows the screenreader to
    announce changes as they happen. In order to determine which row is
    focused, this also adds an an isFocused property to FocusRowBehavior.
    
    
    Bug: 720274
    Change-Id: I3ba1d41200c0fdd1383ec548870ec71e395b2edb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2489959
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820961}

--- ui/webui/resources/js/cr/ui/focus_row_behavior.js
+++ ui/webui/resources/js/cr/ui/focus_row_behavior.js
@@ -108,6 +108,12 @@ cr.define('cr.ui', function() {
         reflectToAttribute: true,
       },
 
+      /** For notifying when the row is in focus. */
+      isFocused: {
+        type: Boolean,
+        notify: true,
+      },
+
       /** Should be bound to the index of the item from the iron-list */
       focusRowIndex: {
         type: Number,
@@ -332,6 +338,7 @@ cr.define('cr.ui', function() {
         cr.ui.focusWithoutInk(firstFocusable);
       }
       this.listBlurred = false;
+      this.isFocused = true;
     },
 
     /** @param {!KeyboardEvent} e */
@@ -364,7 +371,9 @@ cr.define('cr.ui', function() {
      * @private
      */
     onBlur_(e) {
-      this.mouseFocused_ = false;  // Reset flag since it's not active anymore.
+      // Reset focused flags since it's not active anymore.
+      this.mouseFocused_ = false;
+      this.isFocused = false;
 
       const node =
           e.relatedTarget ? /** @type {!Node} */ (e.relatedTarget) : null;
