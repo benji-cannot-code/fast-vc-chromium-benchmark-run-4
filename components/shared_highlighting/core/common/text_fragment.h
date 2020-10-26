bb293d7cb2264b1bd9bcf9834440e1ccc3cc7db2
Sebastien Lalancette <seblalancette@chromium.org>
2020-10-26 13:34:11 +0000

    [SH] Adding Link-To-Text URL Generation Utility Functions
    Added helper functions to be used to generate a URL for selected text
    on a page, based on TextFragment values.
    
    Bug: 1136043
    Change-Id: I8adab1b8cf3dfd00f09387f74f5dab76d682289b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495920
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820731}

---
+++ components/shared_highlighting/core/common/text_fragment.h
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_TEXT_FRAGMENT_H_
+#define COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_TEXT_FRAGMENT_H_
+
+#include <string>
+
+namespace shared_highlighting {
+
+// Class representing a text fragment.
+class TextFragment {
+ public:
+  explicit TextFragment(const std::string& text_start);
+  TextFragment(const std::string& text_start,
+               const std::string& text_end,
+               const std::string& prefix,
+               const std::string& suffix);
+  TextFragment(const TextFragment& other);
+  ~TextFragment();
+
+  const std::string text_start() const { return text_start_; }
+  const std::string text_end() const { return text_end_; }
+  const std::string prefix() const { return prefix_; }
+  const std::string suffix() const { return suffix_; }
+
+  // Converts the current fragment to its URL parameter format:
+  // text=[prefix-,]textStart[,textEnd][,-suffix]
+  // Returns an empty string if |text_start| does not have a value.
+  std::string ToString();
+
+ private:
+  std::string text_start_;
+  std::string text_end_;
+  std::string prefix_;
+  std::string suffix_;
+};
+
+}  // namespace shared_highlighting
+
+#endif  // COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_TEXT_FRAGMENT_H_
---
+++ components/shared_highlighting/core/common/text_fragments_constants.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_TEXT_FRAGMENTS_CONSTANTS_H_
+#define COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_TEXT_FRAGMENTS_CONSTANTS_H_
+
+namespace shared_highlighting {
+
+// Delimiter indicating the start of the text fragments in a URL.
+extern const char kFragmentsUrlDelimiter[];
+
+// Parameter name for a single text fragment in a URL.
+extern const char kFragmentParameterName[];
+
+}  // namespace shared_highlighting
+
+#endif  // COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_TEXT_FRAGMENTS_CONSTANTS_H_
