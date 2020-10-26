b6bff7ce466108f9ed819acfaf9aa2e05d7cc579
Daniel Hosseinian <dhoss@chromium.org>
2020-10-26 22:01:58 +0000

    Change the PDF Viewer's PluginController to a singleton
    Currently, there is only one instance of a PluginController at a time,
    but references to it only live inside PDFViewerBaseElement and derived
    classes. Therefore, descendants of the <pdf-viewer> element fire an
    event handled by the PDFViewerElement. Making the controller a
    singleton allows other elements to directly access the controller using
    PluginController.getInstance().
    
    In making PluginController a singleton, the body of the constructor is
    moved to an init() method, because getInstance() creates a new object
    using the default constructor. Consumers of the PluginController would
    need to make sure the singleton has been initialized before using it.
    
    Eventually, InkController will also be altered to be a singleton,
    similarly to PluginController.
    
    Bug: 1134208
    Change-Id: I5f8a6b565e6fd565e67c41d841ab92fec43e46c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495504
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820941}

--- chrome/browser/resources/pdf/controller.js
+++ chrome/browser/resources/pdf/controller.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {assert} from 'chrome://resources/js/assert.m.js';
+import {addSingletonGetter} from 'chrome://resources/js/cr.m.js';
 import {NativeEventTarget as EventTarget} from 'chrome://resources/js/cr/event_target.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {PromiseResolver} from 'chrome://resources/js/promise_resolver.m.js';
@@ -67,6 +68,12 @@ function createToken() {
 export class ContentController {
   constructor() {}
 
+  /** @return {boolean} */
+  get isActive() {}
+
+  /** @param {boolean} isActive */
+  set isActive(isActive) {}
+
   beforeZoom() {}
 
   afterZoom() {}
@@ -122,9 +129,10 @@ export class ContentController {
 }
 
 /**
- * PDF plugin controller, responsible for communicating with the embedded plugin
- * element. Dispatches a 'plugin-message' event containing the message from the
- * plugin, if a message type not handled by this controller is received.
+ * PDF plugin controller singleton, responsible for communicating with the
+ * embedded plugin element. Dispatches a 'plugin-message' event containing the
+ * message from the plugin, if a message type not handled by this controller is
+ * received.
  * @implements {ContentController}
  */
 export class PluginController {
@@ -134,7 +142,10 @@ export class PluginController {
    * @param {function():boolean} getIsUserInitiatedCallback
    * @param {function():?Promise} getLoadedCallback
    */
-  constructor(plugin, viewport, getIsUserInitiatedCallback, getLoadedCallback) {
+  init(plugin, viewport, getIsUserInitiatedCallback, getLoadedCallback) {
+    /** @private {boolean} */
+    this.isActive_ = false;
+
     /** @private {!HTMLEmbedElement} */
     this.plugin_ = plugin;
 
@@ -163,6 +174,23 @@ export class PluginController {
 
     /** @private {!Map<string, !PromiseResolver>} */
     this.requestResolverMap_ = new Map();
+  }
+
+  /**
+   * @return {boolean}
+   * @override
+   */
+  get isActive() {
+    // Check whether `plugin_` is defined as a signal that `init()` was called.
+    return !!this.plugin_ && this.isActive_;
+  }
+
+  /**
+   * @param {boolean} isActive
+   * @override
+   */
+  set isActive(isActive) {
+    this.isActive_ = isActive;
   }
 
   /**
@@ -402,6 +430,7 @@ export class PluginController {
     this.plugin_.style.display = 'block';
     try {
       await this.getLoadedCallback_();
+      this.isActive = true;
     } finally {
       URL.revokeObjectURL(url);
     }
@@ -410,6 +439,7 @@ export class PluginController {
   /** @override */
   unload() {
     this.plugin_.style.display = 'none';
+    this.isActive = false;
   }
 
   /**
@@ -497,3 +527,5 @@ export class PluginController {
     resolver.resolve(messageData);
   }
 }
+
+addSingletonGetter(PluginController);
