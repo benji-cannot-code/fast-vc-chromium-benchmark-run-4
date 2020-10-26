badb21c54cb6eb4f193dfb4140b7b998871b4996
Richard Knoll <knollr@chromium.org>
2020-10-26 11:45:44 +0000

    Show "Notifications muted" notification
    Instead of silently muting notifications we want to let users know that
    there are new notifications without revealing their content. This will
    show a generic notification instead of the actual one and show a counter
    of how many notifications have been muted.
    
    Bug: 1131375
    Change-Id: I585ead38610895a9465c068b45fdc896e846e2ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2438371
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820708}

--- chrome/browser/notifications/notification_handler.h
+++ chrome/browser/notifications/notification_handler.h
@@ -39,7 +39,8 @@ class NotificationHandler {
     SHARING = 6,
     ANNOUNCEMENT = 7,
     NEARBY_SHARE = 8,
-    MAX = NEARBY_SHARE,
+    NOTIFICATIONS_MUTED = 9,
+    MAX = NOTIFICATIONS_MUTED,
   };
 
   virtual ~NotificationHandler();
