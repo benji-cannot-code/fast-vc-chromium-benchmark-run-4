0cbdb6af3fcb16b961a7710d27cb82443f1cc6bc
Toni Barzic <tbarzic@chromium.org>
2020-10-26 20:37:05 +0000

    Remove GetOptionalAnimationDuration from home_scree_delegate
    Last (only?) usage was removed in CL:2482042
    
    BUG=1137452
    
    Change-Id: I66c9a940c4fd182a99a322fa47eb3a17b0342a33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2496187
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820885}

--- ash/app_list/app_list_controller_impl.h
+++ ash/app_list/app_list_controller_impl.h
@@ -286,7 +286,6 @@ class ASH_EXPORT AppListControllerImpl
       float opacity,
       base::Optional<AnimationInfo> animation_info,
       UpdateAnimationSettingsCallback callback) override;
-  base::Optional<base::TimeDelta> GetOptionalAnimationDuration() override;
   base::ScopedClosureRunner DisableHomeScreenBackgroundBlur() override;
   void OnHomeLauncherAnimationComplete(bool shown, int64_t display_id) override;
   void OnHomeLauncherPositionChanged(int percent_shown,
