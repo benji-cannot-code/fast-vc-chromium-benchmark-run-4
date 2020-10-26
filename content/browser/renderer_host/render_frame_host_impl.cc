c4566726480925d41d8425c67280ec4422274a7f
Alex Moshchuk <alexmos@chromium.org>
2020-10-26 23:08:15 +0000

    Modify logging for sad frames shown while a tab is loading.
    Currently, if a sad frame becomes visible on a tab that's being
    reloaded, the sad frame is recorded as kShownAfterCrashing.  Since we
    mark hidden tabs for reload if they contain a sad frame, activating
    that reload when the user switches to such a tab, and since the tab
    reload is going to wipe out the old document along with the sad frame,
    it's misleading to count that sad frame in that category, since it's
    going to disappear momentarily and thus has a lower impact on user
    experience.  Instead, count such sad frames in a new category,
    kShownWhileAncestorIsLoading.  This category is used whenever there's
    a pending cross-document navigation in an ancestor frame which would
    wipe out the sad frame that was just shown.
    
    Bug: 1132938
    Change-Id: Ibd32919af28f8a1545cbbb3e2964a19e921e8f5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2482425
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820962}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -3010,8 +3010,12 @@ GlobalFrameRoutingId RenderFrameHostImpl::GetGlobalFrameRoutingId() {
 }
 
 bool RenderFrameHostImpl::HasPendingCommitNavigation() const {
-  return navigation_request_ || same_document_navigation_request_ ||
-         !navigation_requests_.empty();
+  return HasPendingCommitForCrossDocumentNavigation() ||
+         same_document_navigation_request_;
+}
+
+bool RenderFrameHostImpl::HasPendingCommitForCrossDocumentNavigation() const {
+  return navigation_request_ || !navigation_requests_.empty();
 }
 
 void RenderFrameHostImpl::ResetNavigationRequests() {
