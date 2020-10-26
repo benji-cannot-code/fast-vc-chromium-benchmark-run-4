1e251a90b1d8c10c0f9bb5f94eddb26a3d9c779c
Titouan Rigoudy <titouan@chromium.org>
2020-10-26 15:51:45 +0000

    [CORS-RFC1918] Do not count main-frame navigations.
    These requests are not blocked by content/, so there is no point in
    counting them in Blink. When we get closer to having a plan for these
    requests, we will want to revisit this so as to gather data.
    
    Bug: chromium:1129326
    Change-Id: Ib5829385727db3a29955c5246aa9c0ce07d55f09
    Fixed: chromium:1134601
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2489918
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Pâris Meuleman <pmeuleman@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820759}

--- chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using page_load_metrics::PageLoadMetricsTestWaiter;
 using TimingField = page_load_metrics::PageLoadMetricsTestWaiter::TimingField;
 using WebFeature = blink::mojom::WebFeature;
+using testing::SizeIs;
 using testing::UnorderedElementsAre;
 using NoStatePrefetch = ukm::builders::NoStatePrefetch;
 
@@ -1643,7 +1644,7 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
 
   const auto& entries = test_ukm_recorder_->GetEntriesByName(
       ukm::builders::Blink_UseCounter::kEntryName);
-  EXPECT_EQ(5u, entries.size());
+  EXPECT_THAT(entries, SizeIs(4));
   std::vector<int64_t> ukm_features;
   for (const auto* entry : entries) {
     test_ukm_recorder_->ExpectEntrySourceHasUrl(entry, url);
@@ -1661,10 +1662,7 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
           static_cast<int64_t>(WebFeature::kFullscreenSecureOrigin),
           static_cast<int64_t>(WebFeature::kNavigatorVibrate),
           static_cast<int64_t>(
-              WebFeature::kApplicationCacheManifestSelectSecureOrigin),
-          static_cast<int64_t>(
-              WebFeature::
-                  kAddressSpaceLocalEmbeddedInUnknownNonSecureContext)));
+              WebFeature::kApplicationCacheManifestSelectSecureOrigin)));
 }
 
 // Test UseCounter UKM mixed content features observed.
@@ -1687,7 +1685,7 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTestWithAutoupgradesDisabled,
 
   const auto& entries = test_ukm_recorder_->GetEntriesByName(
       ukm::builders::Blink_UseCounter::kEntryName);
-  EXPECT_EQ(8u, entries.size());
+  EXPECT_THAT(entries, SizeIs(7));
   std::vector<int64_t> ukm_features;
   for (const auto* entry : entries) {
     test_ukm_recorder_->ExpectEntrySourceHasUrl(entry, url);
@@ -1698,20 +1696,16 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTestWithAutoupgradesDisabled,
     DCHECK(metric);
     ukm_features.push_back(*metric);
   }
-  EXPECT_THAT(
-      ukm_features,
-      UnorderedElementsAre(
-          static_cast<int64_t>(WebFeature::kPageVisits),
-          static_cast<int64_t>(WebFeature::kFullscreenSecureOrigin),
-          static_cast<int64_t>(WebFeature::kNavigatorVibrate),
-          static_cast<int64_t>(
-              WebFeature::kApplicationCacheManifestSelectSecureOrigin),
-          static_cast<int64_t>(WebFeature::kMixedContentImage),
-          static_cast<int64_t>(WebFeature::kMixedContentAudio),
-          static_cast<int64_t>(WebFeature::kMixedContentVideo),
-          static_cast<int64_t>(
-              WebFeature::
-                  kAddressSpaceLocalEmbeddedInUnknownNonSecureContext)));
+  EXPECT_THAT(ukm_features,
+              UnorderedElementsAre(
+                  static_cast<int64_t>(WebFeature::kPageVisits),
+                  static_cast<int64_t>(WebFeature::kFullscreenSecureOrigin),
+                  static_cast<int64_t>(WebFeature::kNavigatorVibrate),
+                  static_cast<int64_t>(
+                      WebFeature::kApplicationCacheManifestSelectSecureOrigin),
+                  static_cast<int64_t>(WebFeature::kMixedContentImage),
+                  static_cast<int64_t>(WebFeature::kMixedContentAudio),
+                  static_cast<int64_t>(WebFeature::kMixedContentVideo)));
 }
 
 // Test UseCounter Features observed in a child frame are recorded, exactly
