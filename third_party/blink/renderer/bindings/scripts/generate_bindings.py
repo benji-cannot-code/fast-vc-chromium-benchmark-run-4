f218785d73202663585553f7c00320ce993525cc
Yuki Shiino <yukishiino@chromium.org>
2020-10-26 09:13:01 +0000

    bind-gen: Support IDL namespace
    Supports IDL namespaces in the new bindings generator.
    
    Bug: 839389
    Change-Id: I72131352f52e6bd51fe071d87706d59aa8b82082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434005
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820694}

--- third_party/blink/renderer/bindings/scripts/generate_bindings.py
+++ third_party/blink/renderer/bindings/scripts/generate_bindings.py
@@ -68,6 +68,7 @@ def main():
         'dictionary': bind_gen.generate_dictionaries,
         'enumeration': bind_gen.generate_enumerations,
         'interface': bind_gen.generate_interfaces,
+        'namespace': bind_gen.generate_namespaces,
     }
 
     for task in tasks:
