05ec252017b5c08cd5e8c5bf66e535fe0d5c6dae
Dale Curtis <dalecurtis@chromium.org>
2020-10-26 20:16:20 +0000

    [MediaRecorder] Don't hop to the main thread to retrieve frames.
    This gives VideoTrackRecorderImpl a WebGraphicsContext3DProvider
    bound to the encoder thread so frames can be retrieved inline
    before encoding instead of having to hop to the main thread.
    
    Local testing shows this is ~10% faster in terms of unit test run time;
    ~16s to ~14s.
    
    To facilitate this change, the existing worker-thread based context
    creation used by WebGLRenderingContextBase is extracted into a
    utility class in "blink/renderer/platform/graphics/".
    
    Fixed: 1138712
    Change-Id: I137f4f6830ac1c7b0860f6c65584a85b276607a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495867
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820870}

---
+++ third_party/blink/renderer/platform/graphics/web_graphics_context_3d_provider_util.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_WEB_GRAPHICS_CONTEXT_3D_PROVIDER_UTIL_H_
+#define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_WEB_GRAPHICS_CONTEXT_3D_PROVIDER_UTIL_H_
+
+#include "third_party/blink/public/platform/platform.h"
+#include "third_party/blink/public/platform/web_graphics_context_3d_provider.h"
+#include "third_party/blink/renderer/platform/weborigin/kurl.h"
+
+namespace blink {
+
+// Synchronously creates a WebGraphicsContext3DProvider on a non-main thread.
+//
+// Returns a newly allocated and initialized offscreen context provider, backed
+// by an independent context. Returns null if the context cannot be created or
+// initialized.
+//
+// Upon successful completion, |gl_info| and |using_gpu_compositing| will be
+// filled in with their actual values.
+//
+// A blocking task is posted to the main thread to create the context, so do not
+// call this method from code which may block main thread progress.
+PLATFORM_EXPORT std::unique_ptr<WebGraphicsContext3DProvider>
+CreateContextProviderOnWorkerThread(
+    Platform::ContextAttributes context_attributes,
+    Platform::GraphicsInfo* gl_info,
+    bool* using_gpu_compositing,
+    const KURL& url);
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_WEB_GRAPHICS_CONTEXT_3D_PROVIDER_UTIL_H_
