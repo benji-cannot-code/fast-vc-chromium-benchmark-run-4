65c454c9ef99af6eb6f4cfe30e339113782ed67f
Chih-Yu Huang <akahuang@google.com>
2020-10-26 11:44:24 +0000

    components/arc: GAVDA ignore output buffers after ProvidePictureBuffers
    The calling flow of VDA changing resolution is:
    1. VDA calls Client::ProvidePictureBuffers()
    2. Client calls VDA::AssignPictureBuffers()
    3. Client calls VDA::ImportBufferForPicture() for N times
    4. Client calls VDA::ReusePictureBuffer() when a buffer is recycled.
    
    If VDA::ImportBufferForPicture() or VDA::ReusePictureBuffer() is
    called right after calling Client::ProvidePictureBuffers() without
    VDA::AssignPictureBuffers() be called, then the buffer contains
    previous resolution and should be ignored.
    
    This CL makes GpuArcVideoDecodeAccelerator ignores the output buffer
    in this situation.
    
    BUG=b:171442927
    TEST=pass android.media.cts.AdaptivePlaybackTest#testVP9_adaptiveDrc
    
    Change-Id: I5c78dcb04aa8bc13c58390033c3b26d16ffa98ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494602
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820707}

--- components/arc/video_accelerator/gpu_arc_video_decode_accelerator.cc
+++ components/arc/video_accelerator/gpu_arc_video_decode_accelerator.cc
@@ -115,6 +115,8 @@ void GpuArcVideoDecodeAccelerator::ProvidePictureBuffersWithVisibleRect(
 
   pending_coded_size_ = dimensions;
 
+  decoder_state_ = DecoderState::kAwaitingAssignPictureBuffers;
+
   auto pbf = mojom::PictureBufferFormat::New();
   pbf->min_num_buffers = requested_num_of_buffers;
   pbf->coded_size = dimensions;
@@ -440,9 +442,9 @@ void GpuArcVideoDecodeAccelerator::AssignPictureBuffers(uint32_t count) {
         mojom::VideoDecodeAccelerator::Result::INVALID_ARGUMENT);
     return;
   }
-  if (assign_picture_buffers_called_) {
-    VLOGF(1) << "AssignPictureBuffers is called twice without "
-             << "ImportBufferForPicture()";
+  if (decoder_state_ != DecoderState::kAwaitingAssignPictureBuffers) {
+    VLOGF(1) << "AssignPictureBuffers is not called right after "
+             << "Client::ProvidePictureBuffers()";
     client_->NotifyError(
         mojom::VideoDecodeAccelerator::Result::INVALID_ARGUMENT);
     return;
@@ -450,7 +452,7 @@ void GpuArcVideoDecodeAccelerator::AssignPictureBuffers(uint32_t count) {
 
   coded_size_ = pending_coded_size_;
   output_buffer_count_ = static_cast<size_t>(count);
-  assign_picture_buffers_called_ = true;
+  decoder_state_ = DecoderState::kAwaitingFirstImport;
 }
 
 void GpuArcVideoDecodeAccelerator::ImportBufferForPicture(
@@ -464,6 +466,12 @@ void GpuArcVideoDecodeAccelerator::ImportBufferForPicture(
     VLOGF(1) << "VDA not initialized.";
     return;
   }
+  if (decoder_state_ == DecoderState::kAwaitingAssignPictureBuffers) {
+    DVLOGF(3) << "AssignPictureBuffers() hasn't been called after calling "
+              << "Client::ProvidePictureBuffers(), ignored.";
+    return;
+  }
+
   if (picture_buffer_id < 0 ||
       static_cast<size_t>(picture_buffer_id) >= output_buffer_count_) {
     VLOGF(1) << "Invalid picture_buffer_id=" << picture_buffer_id;
@@ -534,7 +542,7 @@ void GpuArcVideoDecodeAccelerator::ImportBufferForPicture(
 
   // This is the first time of ImportBufferForPicture() after
   // AssignPictureBuffers() is called. Call VDA::AssignPictureBuffers() here.
-  if (assign_picture_buffers_called_) {
+  if (decoder_state_ == DecoderState::kAwaitingFirstImport) {
     gfx::Size picture_size(gmb_handle.native_pixmap_handle.planes[0].stride,
                            coded_size_.height());
     std::vector<media::PictureBuffer> buffers;
@@ -544,7 +552,7 @@ void GpuArcVideoDecodeAccelerator::ImportBufferForPicture(
     }
 
     vda_->AssignPictureBuffers(std::move(buffers));
-    assign_picture_buffers_called_ = false;
+    decoder_state_ = DecoderState::kDecoding;
   }
 
   vda_->ImportBufferForPicture(picture_buffer_id, pixel_format,
@@ -557,6 +565,11 @@ void GpuArcVideoDecodeAccelerator::ReusePictureBuffer(
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   if (!vda_) {
     VLOGF(1) << "VDA not initialized.";
+    return;
+  }
+  if (decoder_state_ == DecoderState::kAwaitingAssignPictureBuffers) {
+    DVLOGF(3) << "AssignPictureBuffers() hasn't been called after calling "
+              << "Client::ProvidePictureBuffers(), ignored.";
     return;
   }
 
