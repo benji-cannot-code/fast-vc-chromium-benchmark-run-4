80c19167cd071e90a2eb72dc1a4543e91d32cd33
Ken Rockot <rockot@google.com>
2020-10-26 18:30:32 +0000

    Avoid soft message size limits on ImageDecoder
    ImageDecoder methods take their encoded data as an inlined byte array,
    which can very realistically exceed the soft message size limit that
    triggers crash dumps within Mojo.
    
    This changes the messages to use BigBuffer for their image data,
    effectively eliminating the possibility of oversized messages.
    
    Fixed: 1141981
    Change-Id: I1ba83ba4ebafd40c38178bb2206da425ec6d18d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495408
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#820830}

--- services/data_decoder/image_decoder_impl.cc
+++ services/data_decoder/image_decoder_impl.cc
@@ -61,7 +61,7 @@ ImageDecoderImpl::ImageDecoderImpl() = default;
 
 ImageDecoderImpl::~ImageDecoderImpl() = default;
 
-void ImageDecoderImpl::DecodeImage(const std::vector<uint8_t>& encoded_data,
+void ImageDecoderImpl::DecodeImage(mojo_base::BigBuffer encoded_data,
                                    mojom::ImageCodec codec,
                                    bool shrink_to_fit,
                                    int64_t max_size_in_bytes,
@@ -98,7 +98,7 @@ void ImageDecoderImpl::DecodeImage(const std::vector<uint8_t>& encoded_data,
   std::move(callback).Run(decoded_image);
 }
 
-void ImageDecoderImpl::DecodeAnimation(const std::vector<uint8_t>& encoded_data,
+void ImageDecoderImpl::DecodeAnimation(mojo_base::BigBuffer encoded_data,
                                        bool shrink_to_fit,
                                        int64_t max_size_in_bytes,
                                        DecodeAnimationCallback callback) {
