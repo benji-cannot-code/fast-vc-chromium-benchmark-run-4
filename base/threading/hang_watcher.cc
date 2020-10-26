2e21e666b9d159c02f8361ad1f2205cbad64faea
Olivier Li <olivierli@chromium.org>
2020-10-26 20:39:55 +0000

    Introduce the HangWatcher.SleepDrift metric.
    This helps track whether the HangWatcher is able to periodically wake up
    fast enough to watch for hangs.
    
    Bug: 1135528
    Change-Id: I85d3c4896de35351dc2d68482e48f79b0c55d1ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495275
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Oliver Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820888}

--- base/threading/hang_watcher.cc
+++ base/threading/hang_watcher.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
@@ -333,6 +334,9 @@ void HangWatcher::Wait() {
     const base::TimeDelta wait_time = time_after_wait - time_before_wait;
     const bool wait_was_normal =
         wait_time <= (monitor_period_ + kWaitDriftTolerance);
+
+    UMA_HISTOGRAM_TIMES("HangWatcher.SleepDrift.BrowserProcess",
+                        wait_time - monitor_period_);
 
     if (!wait_was_normal) {
       // If the time spent waiting was too high it might indicate the machine is
