422fdf7368c11d05a812551c7356c1e3a0607d88
Matt Jones <mdjones@chromium.org>
2020-10-26 14:47:51 +0000

    Re-enable testOpenManyTabsQuickly
    Bug: 223587
    Change-Id: I1b61f97f9c864d467593c490fa4fc5b7efd16989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2493001
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820745}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
@@ -607,7 +607,7 @@ public class TabsTest {
      *
      */
     @Test
-    @FlakyTest
+    @LargeTest
     public void testOpenManyTabsQuickly() {
         int startCount = mActivityTestRule.getActivity().getCurrentTabModel().getCount();
         for (int i = 1; i <= STRESSFUL_TAB_COUNT; ++i) {
