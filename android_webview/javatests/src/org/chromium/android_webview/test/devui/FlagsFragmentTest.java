95024f3482904884bc60054a42708b041a88a82d
Patrick Noland <pnoland@chromium.org>
2020-10-26 17:06:05 +0000

    [Sheriff] Disable flaky test FlagsFragmentTest#testToggledFlagsFloatToTop
    Bug: 1141442
    Change-Id: I3dd0b01a6c5dfa2d3c7cb4ec634f34e4b140c4e2
    TBR: ntfschr@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2498832
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820793}

--- android_webview/javatests/src/org/chromium/android_webview/test/devui/FlagsFragmentTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/devui/FlagsFragmentTest.java
@@ -62,6 +62,7 @@ import org.chromium.android_webview.devui.R;
 import org.chromium.android_webview.services.DeveloperUiService;
 import org.chromium.android_webview.test.AwJUnit4ClassRunner;
 import org.chromium.base.test.util.CallbackHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 
@@ -399,6 +400,7 @@ public class FlagsFragmentTest {
     @Test
     @MediumTest
     @Feature({"AndroidWebView"})
+    @DisabledTest(message = "https://crbug.com/1141442")
     public void testToggledFlagsFloatToTop() throws Throwable {
         ListView flagsList = mRule.getActivity().findViewById(R.id.flags_list);
         int totalNumFlags = flagsList.getCount();
