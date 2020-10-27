a4b758b2f18355170a9cd6e0e7a3f8b9860ef041
James Cook <jamescook@chromium.org>
2020-10-27 20:41:00 +0000

    lacros: Fix default download and documents paths
    Prior to this CL, files downloaded in Lacros would "disappear" -- they
    would not show up in the file manager. The downloads code would choose
    its default directory using the Chrome for Linux path provider, which
    does not know about Chrome OS paths, and the files would end up in a
    directory the file manager can't navigate to.
    
    Add chrome_paths_lacros.cc to supply the correct paths.
    
    Because Lacros does not support Chrome OS multi-signin, we make the
    decision that lacros-chrome does not need to know about the under-the-
    hood /home/chronos/u-{hash} multi-signin profile directories. It uses
    the hard-linked /home/chronos/user path for the system primary profile.
    This avoids exposing knowledge of the hash to Lacros.
    
    Bug: 1139082
    Change-Id: I8836760632fd9a1a8f9d49d7f03f1e6f6dfb0841
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500278
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821426}

--- chrome/common/chrome_paths_unittest.cc
+++ chrome/common/chrome_paths_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 #include "chrome/common/chrome_constants.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -45,8 +46,9 @@ TEST(ChromePaths, UserCacheDir) {
 #endif
 
   // Verify that a profile in the special platform-specific source
-  // location ends up in the special target location.
-#if !defined(OS_WIN)  // No special behavior on Windows.
+  // location ends up in the special target location. Ignore this assertion on
+  // platforms that don't use a special cache directory.
+#if !defined(OS_WIN) && !BUILDFLAG(IS_LACROS)
   GetUserCacheDirectory(test_profile_dir, &cache_dir);
   EXPECT_EQ(expected_cache_dir.value(), cache_dir.value());
 #endif
@@ -62,7 +64,8 @@ TEST(ChromePaths, UserCacheDir) {
 #endif
 }
 
-#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+// Chrome OS doesn't use any of the desktop linux configuration.
+#if defined(OS_LINUX) && !BUILDFLAG(IS_LACROS) && !BUILDFLAG(IS_ASH)
 TEST(ChromePaths, DefaultUserDataDir) {
   std::unique_ptr<base::Environment> env(base::Environment::Create());
   std::string orig_chrome_config_home;
@@ -98,6 +101,16 @@ TEST(ChromePaths, DefaultUserDataDir) {
     env->SetVar("CHROME_CONFIG_HOME", orig_chrome_config_home);
   else
     env->UnSetVar("CHROME_CONFIG_HOME");
+}
+#endif
+
+#if BUILDFLAG(IS_LACROS) || BUILDFLAG(IS_ASH)
+TEST(ChromePaths, UserMediaDirectories) {
+  base::FilePath path;
+  // Chrome OS does not support custom media directories.
+  EXPECT_FALSE(GetUserMusicDirectory(&path));
+  EXPECT_FALSE(GetUserPicturesDirectory(&path));
+  EXPECT_FALSE(GetUserVideosDirectory(&path));
 }
 #endif
 
