7aecf33abf580951932f846abdc5710e6e7c60ae
Lucas Furukawa Gadani <lfg@chromium.org>
2020-10-27 22:13:08 +0000

    Remove usages of WebContents::GetRenderViewHost().
    This CL was uploaded by git cl split.
    
    R=mahmadi@chromium.org
    
    Bug: 1142671
    Change-Id: Ida6f990ab48b6a1dbecb775db783ffe8b7f27f04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500559
    Auto-Submit: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821467}

--- chrome/browser/search/search_unittest.cc
+++ chrome/browser/search/search_unittest.cc
@@ -205,7 +205,7 @@ TEST_F(SearchTest, ProcessIsolation) {
     const content::RenderProcessHost* start_rph =
         contents->GetMainFrame()->GetProcess();
     const content::RenderViewHost* start_rvh =
-        contents->GetRenderViewHost();
+        contents->GetMainFrame()->GetRenderViewHost();
 
     // Navigate to end URL.
     NavigateAndCommitActiveTab(GURL(test.end_url));
@@ -216,7 +216,7 @@ TEST_F(SearchTest, ProcessIsolation) {
               start_site_instance.get() == contents->GetSiteInstance())
         << test.description;
     EXPECT_EQ(test.same_site_instance,
-              start_rvh == contents->GetRenderViewHost())
+              start_rvh == contents->GetMainFrame()->GetRenderViewHost())
         << test.description;
     EXPECT_EQ(test.same_process,
               start_rph == contents->GetMainFrame()->GetProcess())
@@ -242,7 +242,7 @@ TEST_F(SearchTest, ProcessIsolation_RendererInitiated) {
     const content::RenderProcessHost* start_rph =
         contents->GetMainFrame()->GetProcess();
     const content::RenderViewHost* start_rvh =
-        contents->GetRenderViewHost();
+        contents->GetMainFrame()->GetRenderViewHost();
 
     // Navigate to end URL via a renderer-initiated navigation.
     content::NavigationSimulator::NavigateAndCommitFromDocument(
@@ -255,7 +255,7 @@ TEST_F(SearchTest, ProcessIsolation_RendererInitiated) {
               start_site_instance.get() == contents->GetSiteInstance())
         << test.description;
     EXPECT_EQ(test.same_site_instance,
-              start_rvh == contents->GetRenderViewHost())
+              start_rvh == contents->GetMainFrame()->GetRenderViewHost())
         << test.description;
     EXPECT_EQ(test.same_process,
               start_rph == contents->GetMainFrame()->GetProcess())
