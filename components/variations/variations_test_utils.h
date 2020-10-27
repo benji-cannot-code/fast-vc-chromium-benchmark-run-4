564609aa95c63973ff9adf71c495e5059d9521f8
Karthika Pai <karthikapai@google.com>
2020-10-27 23:32:01 +0000

    Move Variations IDs utils into helper file
    This CL moves two helper functions that extracts IDs and creates trials
    and associate IDs into their own file.
    
    Bug: 1134444
    Change-Id: I056871f2245ba3e9c72a05ffd019f2e96499a45e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500434
    Commit-Queue: Karthika Pai <karthikapai@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821492}

---
+++ components/variations/variations_test_utils.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_VARIATIONS_VARIATIONS_TEST_UTILS_H_
+#define COMPONENTS_VARIATIONS_VARIATIONS_TEST_UTILS_H_
+
+#include <set>
+#include <string>
+
+#include "base/metrics/field_trial.h"
+#include "components/variations/variations_associated_data.h"
+
+namespace variations {
+
+// Decodes the variations header and extracts the variation ids.
+bool ExtractVariationIds(const std::string& variations,
+                         std::set<VariationID>* variation_ids,
+                         std::set<VariationID>* trigger_ids);
+
+// Creates FieldTrial from given |key| and |id|.
+scoped_refptr<base::FieldTrial> CreateTrialAndAssociateId(
+    const std::string& trial_name,
+    const std::string& default_group_name,
+    IDCollectionKey key,
+    VariationID id);
+
+}  // namespace variations
+
+#endif  // COMPONENTS_VARIATIONS_VARIATIONS_TEST_UTILS_H_
