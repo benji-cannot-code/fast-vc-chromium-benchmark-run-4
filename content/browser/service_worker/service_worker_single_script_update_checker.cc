fd51e6143d41595ee2dfabe817d1dff500fcb2c3
Kenichi Ishibashi <bashi@chromium.org>
2020-10-27 03:38:51 +0000

    service worker: Remove unused appcache includes
    We removed AppCache dependencies from service worker as of
    crrev.com/c/2452029. Remove unused includes.
    
    Bug: 1117369
    Change-Id: If5133261a88ae47229d37826d14816a46b35e7bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500386
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821058}

--- content/browser/service_worker/service_worker_single_script_update_checker.cc
+++ content/browser/service_worker/service_worker_single_script_update_checker.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/trace_event/trace_event.h"
-#include "content/browser/appcache/appcache_disk_cache_ops.h"
 #include "content/browser/loader/browser_initiated_resource_request.h"
 #include "content/browser/service_worker/service_worker_cache_writer.h"
 #include "content/browser/service_worker/service_worker_consts.h"
--- content/browser/service_worker/service_worker_updated_script_loader.cc
+++ content/browser/service_worker/service_worker_updated_script_loader.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/task/post_task.h"
-#include "content/browser/appcache/appcache_disk_cache_ops.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/browser/service_worker/service_worker_cache_writer.h"
 #include "content/browser/service_worker/service_worker_consts.h"
