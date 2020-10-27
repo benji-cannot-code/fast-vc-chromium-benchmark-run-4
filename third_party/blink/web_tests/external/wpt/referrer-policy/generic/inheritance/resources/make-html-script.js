95e2c03be043d5178a323c7ed39804621e71ca97
Antonio Sartori <antoniosartori@chromium.org>
2020-10-27 15:20:07 +0000

    Add WP test for referrer policy inheritance when navigating back
    When navigating back (through history navigation) to a srcdoc iframe,
    we should expect the iframe to apply the same referrer policy it had
    originally, even if in the mean time the referrer policy of the main
    frame changed.
    
    This CL adds a web platform test for this behaviour. Although the test
    is failing at the moment in Chrome, we plan to fix this with the
    Policy Container (https://crbug.com/1130587).
    
    Bug: 1130587
    Change-Id: I843808fddfc0c6fa5d65ce88599bce51471aa691
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2475033
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821243}

--- third_party/blink/web_tests/external/wpt/referrer-policy/generic/inheritance/resources/make-html-script.js
+++ third_party/blink/web_tests/external/wpt/referrer-policy/generic/inheritance/resources/make-html-script.js
@@ -1,11 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function createScriptString(origin) {
-  return `<script src = "${origin}/common/security-features/resources/common.sub.js"><\/script>
-          <script>
-            requestViaXhr("${origin}/common/security-features/subresource/xhr.py").then(msg => {
-              top.postMessage({referrer: msg.referrer}, "*")
-            }).catch(e => {
-              top.postMessage({referrer: "FAILURE"}, "*");
-            });
+function createScriptString(origin, referrer) {
+  let request_init = referrer ? `{referrer: "${referrer}"}` : "";
+  return `<script>
+            fetch("${origin}/common/security-features/subresource/xhr.py",
+                  ${request_init})
+              .then(r => r.json())
+              .then(j => {
+                top.postMessage({referrer: j.headers.referer}, "*")
+              }).catch(e => {
+                top.postMessage({referrer: "FAILURE"}, "*");
+              });
           <\/script>`;
 }
