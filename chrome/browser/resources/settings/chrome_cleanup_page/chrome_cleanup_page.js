ce39e3a920e35edf74a5813736fa2edd281fa173
Rainhard Findling <rainhard@chromium.org>
2020-10-27 11:54:07 +0000

    CCT scan completion notification: checkbox in Settings UI behind flag
    Bug: 1087263
    Change-Id: I85e269707a7579c8894a25ab1d8dc7da71009906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494766
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821174}

--- chrome/browser/resources/settings/chrome_cleanup_page/chrome_cleanup_page.js
+++ chrome/browser/resources/settings/chrome_cleanup_page/chrome_cleanup_page.js
@@ -19,6 +19,7 @@ import '../settings_shared_css.m.js';
 
 import {assert} from 'chrome://resources/js/assert.m.js';
 import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {WebUIListenerBehavior} from 'chrome://resources/js/web_ui_listener_behavior.m.js';
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
@@ -69,6 +70,7 @@ const ChromeCleanupCardFlags = {
   SHOW_LOGS_PERMISSIONS: 1 << 0,
   WAITING_FOR_RESULT: 1 << 1,
   SHOW_ITEMS_TO_REMOVE: 1 << 2,
+  SHOW_NOTIFICATION_PERMISSION: 1 << 3,
 };
 
 /**
@@ -190,6 +192,12 @@ Polymer({
 
     /** @private */
     showLogsPermission_: {
+      type: Boolean,
+      value: false,
+    },
+
+    /** @private */
+    showNotificationPermission_: {
       type: Boolean,
       value: false,
     },
@@ -545,6 +553,12 @@ Polymer({
         (flags & ChromeCleanupCardFlags.WAITING_FOR_RESULT) !== 0;
     this.showItemsToRemove_ =
         (flags & ChromeCleanupCardFlags.SHOW_ITEMS_TO_REMOVE) !== 0;
+    this.showNotificationPermission_ =
+        (flags & ChromeCleanupCardFlags.SHOW_NOTIFICATION_PERMISSION) !== 0 &&
+        loadTimeData.valueExists(
+            'chromeCleanupScanCompletedNotificationEnabled') &&
+        loadTimeData.getBoolean(
+            'chromeCleanupScanCompletedNotificationEnabled');
 
     // Files to remove list should only be expandable if details are being
     // shown, otherwise it will add extra padding at the bottom of the card.
@@ -567,7 +581,8 @@ Polymer({
    */
   startScanning_() {
     this.browserProxy_.startScanning(
-        this.$.chromeCleanupLogsUploadControl.checked);
+        this.$.chromeCleanupLogsUploadControl.checked,
+        this.$.chromeCleanupShowNotificationControl.checked);
   },
 
   /**
@@ -686,7 +701,8 @@ Polymer({
           title: this.i18n('chromeCleanupTitleFindAndRemove'),
           explanation: this.i18n('chromeCleanupExplanationFindAndRemove'),
           actionButton: actionButtons.FIND,
-          flags: ChromeCleanupCardFlags.SHOW_LOGS_PERMISSIONS,
+          flags: ChromeCleanupCardFlags.SHOW_LOGS_PERMISSIONS |
+              ChromeCleanupCardFlags.SHOW_NOTIFICATION_PERMISSION,
         }
       ],
       [
