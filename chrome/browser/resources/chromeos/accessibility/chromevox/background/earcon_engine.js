b1f7a535bc37208e5ea66e87c70320dc517a431f
Dominic Mazzoni <dmazzoni@chromium.org>
2020-10-27 03:10:30 +0000

    Improve inclusiveness of language in earcon_engine.js
    To replace "master", I chose outputVolume for masterVolume because it's
    applied as a final factor, and then basePan and baseReverb because
    individual earcons use those values as a starting point for further
    adjustments.
    
    Bug: 842296
    Change-Id: I5b36205b6dcafd1351daf3b66d2686618804007a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2487882
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821053}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/earcon_engine.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/earcon_engine.js
@@ -18,11 +18,11 @@ EarconEngine = class {
   constructor() {
     // Public control parameters. All of these are meant to be adjustable.
 
-    /** @type {number} The master volume, as an amplification factor. */
-    this.masterVolume = 1.0;
+    /** @type {number} The output volume, as an amplification factor. */
+    this.outputVolume = 1.0;
 
     /** @type {number} The base relative pitch adjustment, in half-steps. */
-    this.masterPitch = -4;
+    this.basePitch = -4;
 
     /** @type {number} The click volume, as an amplification factor. */
     this.clickVolume = 0.4;
@@ -36,11 +36,11 @@ EarconEngine = class {
     /** @type {number} The base delay for repeated sounds, in seconds. */
     this.baseDelay = 0.045;
 
-    /** @type {number} The master stereo panning, from -1 to 1. */
-    this.masterPan = EarconEngine.CENTER_PAN_;
+    /** @type {number} The base stereo panning, from -1 to 1. */
+    this.basePan = EarconEngine.CENTER_PAN_;
 
-    /** @type {number} The master reverb level as an amplification factor. */
-    this.masterReverb = 0.4;
+    /** @type {number} The base reverb level as an amplification factor. */
+    this.baseReverb = 0.4;
 
     /**
      * @type {string} The choice of the reverb impulse response to use.
@@ -156,7 +156,7 @@ EarconEngine = class {
 
   /**
    * Return an AudioNode containing the final processing that all
-   * sounds go through: master volume / gain, panning, and reverb.
+   * sounds go through: output volume / gain, panning, and reverb.
    * The chain is hooked up to the destination automatically, so you
    * just need to connect your source to the return value from this
    * method.
@@ -166,12 +166,12 @@ EarconEngine = class {
    *          reverb: (number | undefined)}} properties
    *     An object where you can override the default
    *     gain, pan, and reverb, otherwise these are taken from
-   *     masterVolume, masterPan, and masterReverb.
+   *     outputVolume, basePan, and baseReverb.
    * @return {!AudioNode} The filters to be applied to all sounds, connected
    *     to the destination node.
    */
   createCommonFilters(properties) {
-    let gain = this.masterVolume;
+    let gain = this.outputVolume;
     if (properties.gain) {
       gain *= properties.gain;
     }
@@ -180,7 +180,7 @@ EarconEngine = class {
     const first = gainNode;
     let last = gainNode;
 
-    let pan = this.masterPan;
+    let pan = this.basePan;
     if (properties.pan !== undefined) {
       pan = properties.pan;
     }
@@ -192,7 +192,7 @@ EarconEngine = class {
       last = panNode;
     }
 
-    let reverb = this.masterReverb;
+    let reverb = this.baseReverb;
     if (properties.reverb !== undefined) {
       reverb = properties.reverb;
     }
@@ -250,7 +250,7 @@ EarconEngine = class {
       opt_properties = /** @type {undefined} */ ({});
     }
 
-    let pitch = this.masterPitch;
+    let pitch = this.basePitch;
     if (opt_properties.pitch) {
       pitch += opt_properties.pitch;
     }
@@ -427,7 +427,7 @@ EarconEngine = class {
    * and the decay time |decay|, in seconds.
    *
    * As with other functions, |pan| and |reverb| can be used to override
-   * masterPan and masterReverb.
+   * basePan and baseReverb.
    *
    * @param {{gain: number,
    *          freq: number,
@@ -754,14 +754,14 @@ EarconEngine = class {
     // pan position.
     x = (2 * x - 1) * EarconEngine.MAX_PAN_ABS_X_POSITION;
 
-    this.masterPan = x;
+    this.basePan = x;
   }
 
   /**
    * Resets panning to default (centered).
    */
   resetPan() {
-    this.masterPan = EarconEngine.CENTER_PAN_;
+    this.basePan = EarconEngine.CENTER_PAN_;
   }
 };
 
