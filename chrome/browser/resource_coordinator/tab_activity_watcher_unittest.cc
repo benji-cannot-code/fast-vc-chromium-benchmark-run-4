b1181c7256afddfc4642cdeaaa4a1ed702898c8d
Lucas Furukawa Gadani <lfg@chromium.org>
2020-10-27 15:30:41 +0000

    Remove usages of WebContents::GetRenderViewHost().
    This CL was uploaded by git cl split.
    
    R=chrisha@chromium.org
    
    Bug: 1142671
    Change-Id: I32ce49b4eb448c0a18a44ae7a4b570124260466c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500213
    Auto-Submit: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821255}

--- chrome/browser/resource_coordinator/tab_activity_watcher_unittest.cc
+++ chrome/browser/resource_coordinator/tab_activity_watcher_unittest.cc
@@ -527,7 +527,7 @@ TEST_F(TabMetricsTest, InputEvents) {
 
   // Fake some input events.
   content::RenderWidgetHost* widget_1 =
-      test_contents_1->GetRenderViewHost()->GetWidget();
+      test_contents_1->GetMainFrame()->GetRenderViewHost()->GetWidget();
   widget_1->ForwardMouseEvent(
       CreateMouseEvent(WebInputEvent::Type::kMouseDown));
   widget_1->ForwardMouseEvent(CreateMouseEvent(WebInputEvent::Type::kMouseUp));
@@ -544,7 +544,7 @@ TEST_F(TabMetricsTest, InputEvents) {
 
   // The second tab's counts are independent of the other's.
   content::RenderWidgetHost* widget_2 =
-      test_contents_2->GetRenderViewHost()->GetWidget();
+      test_contents_2->GetMainFrame()->GetRenderViewHost()->GetWidget();
   widget_2->ForwardMouseEvent(
       CreateMouseEvent(WebInputEvent::Type::kMouseMove));
   expected_metrics_2[TabManager_TabMetrics::kMouseEventCountName] = 1;
@@ -576,7 +576,7 @@ TEST_F(TabMetricsTest, InputEvents) {
   // After a navigation, test that the counts are reset.
   WebContentsTester::For(test_contents_1)->NavigateAndCommit(TestUrls()[2]);
   // The widget may have been invalidated by the navigation.
-  widget_1 = test_contents_1->GetRenderViewHost()->GetWidget();
+  widget_1 = test_contents_1->GetMainFrame()->GetRenderViewHost()->GetWidget();
   widget_1->ForwardMouseEvent(
       CreateMouseEvent(WebInputEvent::Type::kMouseMove));
   expected_metrics_1[TabManager_TabMetrics::kMouseEventCountName] = 1;
