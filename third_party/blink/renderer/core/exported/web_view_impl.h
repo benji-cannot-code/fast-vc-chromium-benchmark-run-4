22b1b0b5f4652bba0678204df07c729937786b1a
Mario Sanchez Prada <mario@igalia.com>
2020-10-27 14:52:18 +0000

    Move RemoteSet<blink::mojom::RendererPreferenceWatcher> to WebViewImpl
    Now that we have migrated the PageMsg_SetRendererPrefs legacy IPC to
    Mojo and the ownership of blink::RendererPreferences to WebViewImpl,
    we can also move this mojo RemoteSet to WebViewImpl as well, enabling
    us to move even more code from RenderViewImpl into Blink.
    
    Thus, this CL moves |renderer_preferences_watcher| into WebViewImpl by
    defining a new WebView::RegisterRendererPreferenceWatcher() method that
    would receive a CrossVariantMojoRemote as parameter to bind the remote
    in Blink. With that in place WebViewImpl::SetRendererPreferences() does
    no longer rely on RenderViewImpl::DidUpdateRendererPreferences() to
    notify the watchers, getting us one step closer to entirely remove that
    method from WebViewClient and its only implementor i.e. RenderViewImpl.
    
    Bug: 1102442
    Change-Id: If35c5302616e6411198965d35f55f04683a73db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2498503
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821236}

--- third_party/blink/renderer/core/exported/web_view_impl.h
+++ third_party/blink/renderer/core/exported/web_view_impl.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
+#include "mojo/public/cpp/bindings/remote_set.h"
 #include "third_party/blink/public/common/input/web_gesture_event.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
 #include "third_party/blink/public/common/renderer_preferences/renderer_preferences.h"
@@ -45,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/input/focus_type.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/page/page.mojom-blink.h"
 #include "third_party/blink/public/mojom/page/page_visibility_state.mojom-blink.h"
+#include "third_party/blink/public/mojom/renderer_preference_watcher.mojom-blink.h"
 #include "third_party/blink/public/platform/scheduler/web_agent_group_scheduler.h"
 #include "third_party/blink/public/platform/web_input_event_result.h"
 #include "third_party/blink/public/platform/web_rect.h"
@@ -207,6 +209,9 @@ class CORE_EXPORT WebViewImpl final : public WebView,
   void SetDeviceColorSpaceForTesting(
       const gfx::ColorSpace& color_space) override;
   void PaintContent(cc::PaintCanvas*, const gfx::Rect&) override;
+  void RegisterRendererPreferenceWatcher(
+      CrossVariantMojoRemote<mojom::RendererPreferenceWatcherInterfaceBase>
+          watcher) override;
   void SetRendererPreferences(const RendererPreferences& preferences) override;
   const RendererPreferences& GetRendererPreferences() override;
   void SetWebPreferences(const web_pref::WebPreferences& preferences) override;
@@ -866,6 +871,11 @@ class CORE_EXPORT WebViewImpl final : public WebView,
 
   mojom::blink::PageLifecycleStatePtr lifecycle_state_;
   mojo::AssociatedReceiver<mojom::blink::PageBroadcast> receiver_;
+
+  // These are observing changes in |renderer_preferences_|. This is used for
+  // keeping WorkerFetchContext in sync.
+  mojo::RemoteSet<mojom::blink::RendererPreferenceWatcher>
+      renderer_preference_watchers_;
 
   base::WeakPtrFactory<WebViewImpl> weak_ptr_factory_{this};
 };
