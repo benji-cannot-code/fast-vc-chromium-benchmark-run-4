65e718873c3acb3950bc626465b819cdba4833c5
Sergey Ulanov <sergeyu@chromium.org>
2020-10-27 22:57:06 +0000

    [Fuchsia] Set AudioRenderer usage
    Previously AudioOutputStreamFuchsia wasn't setting stream usage for the
    AudioRenderer. The usage is required by the system audio service to make
    correct audio policy decisions for the stream. This change updates
    AudioOutputStreamFuchsia to SetUsage() appropriately.
    
    Bug: 1065605
    Change-Id: If96a9b0713b4acdff63035725b7322d1a5ad65a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500431
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821483}

--- media/audio/fuchsia/audio_output_stream_fuchsia.cc
+++ media/audio/fuchsia/audio_output_stream_fuchsia.cc
@@ -18,11 +18,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+namespace {
+
 // Current AudioRenderer implementation allows only one buffer with id=0.
 // TODO(crbug.com/1131179): Replace with an incrementing buffer id now that
 // AddPayloadBuffer() and RemovePayloadBuffer() are implemented properly in
 // AudioRenderer.
 const uint32_t kBufferId = 0;
+
+fuchsia::media::AudioRenderUsage GetStreamUsage(
+    const AudioParameters& parameters) {
+  if (parameters.latency_tag() == AudioLatency::LATENCY_RTC)
+    return fuchsia::media::AudioRenderUsage::COMMUNICATION;
+  return fuchsia::media::AudioRenderUsage::MEDIA;
+}
+
+}  // namespace
 
 AudioOutputStreamFuchsia::AudioOutputStreamFuchsia(
     AudioManagerFuchsia* manager,
@@ -47,6 +58,8 @@ bool AudioOutputStreamFuchsia::Open() {
   audio_server->CreateAudioRenderer(audio_renderer_.NewRequest());
   audio_renderer_.set_error_handler(
       fit::bind_member(this, &AudioOutputStreamFuchsia::OnRendererError));
+
+  audio_renderer_->SetUsage(GetStreamUsage(parameters_));
 
   // Inform the |audio_renderer_| of the format required by the caller.
   fuchsia::media::AudioStreamType format;
