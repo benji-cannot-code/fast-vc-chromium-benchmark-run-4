cd60673da3fc8f6775a3c90135f73c77f39d6ccd
John Palmer <jopalmer@chromium.org>
2020-10-27 00:30:08 +0000

    Add JS API for chrome.input.ime.autocorrect
    BUG=1138092, 1111135
    
    Change-Id: I35366c90794f9e26917360464ec3553798073a65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2483719
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#820993}

--- chrome/browser/extensions/api/input_ime/input_ime_api_chromeos.h
+++ chrome/browser/extensions/api/input_ime/input_ime_api_chromeos.h
@@ -22,6 +22,15 @@ class InputMethodEngine;
 
 namespace extensions {
 
+class InputImeAutocorrectFunction : public ExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("input.ime.autocorrect", INPUT_IME_AUTOCORRECT)
+ protected:
+  ~InputImeAutocorrectFunction() override = default;
+  // ExtensionFunction:
+  ResponseAction Run() override;
+};
+
 class InputImeClearCompositionFunction : public ExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("input.ime.clearComposition",
