2acdcf8cf1ea0f144b0e276c927b01e353d105f2
Gang Wu <gangwu@chromium.org>
2020-10-27 19:33:29 +0000

    Add a round corner rectangle IPH for ChipView
    This CL add a new shape, round corner rectangle IPH for ChipView.
    There will be a follow up CL for renaming the turnOnHighlight to
    turnOnCircularHighlight and turnOnRectangularHighlight in ViewHighlighter
    to unify the name in the class.
    
    Bug: 1136169
    Change-Id: I1c8efefb788942905f8142e04621b350b2c602a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500274
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821400}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/highlight/PulseDrawable.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/highlight/PulseDrawable.java
@@ -12,6 +12,7 @@ import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.os.SystemClock;
@@ -19,6 +20,7 @@ import android.view.animation.Interpolator;
 
 import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
+import androidx.annotation.Px;
 import androidx.core.view.animation.PathInterpolatorCompat;
 
 import org.chromium.base.ApiCompatibilityUtils;
@@ -132,11 +134,13 @@ public class PulseDrawable extends Drawable implements Animatable {
     /**
      * Creates a {@link PulseDrawable} that will fill the bounds with a pulsing color.
      * @param context The {@link Context} under which the drawable is created.
+     * @param cornerRadius The corner radius in pixels of the highlight rectangle, 0 may be passed
+     *         if the rectangle should not be rounded.
      * @param pulseEndAuthority The {@link PulseEndAuthority} associated with this drawable.
      * @return A new {@link PulseDrawable} instance.
      */
-    public static PulseDrawable createHighlight(
-            Context context, PulseEndAuthority pulseEndAuthority) {
+    public static PulseDrawable createRoundedRectangle(
+            Context context, @Px int cornerRadius, PulseEndAuthority pulseEndAuthority) {
         Painter painter = new Painter() {
             @Override
             public void modifyDrawable(PulseDrawable drawable, float interpolation) {
@@ -146,7 +150,8 @@ public class PulseDrawable extends Drawable implements Animatable {
             @Override
             public void draw(
                     PulseDrawable drawable, Paint paint, Canvas canvas, float interpolation) {
-                canvas.drawRect(drawable.getBounds(), paint);
+                canvas.drawRoundRect(
+                        new RectF(drawable.getBounds()), cornerRadius, cornerRadius, paint);
             }
         };
 
@@ -159,10 +164,22 @@ public class PulseDrawable extends Drawable implements Animatable {
      * PulseDrawable} will continue pulsing forever (if this is not the desired behavior, please use
      * {@link PulseEndAuthority}).
      * @param context The {@link Context} under which the drawable is created.
+     * @param cornerRadius The corner radius in pixels of the highlight rectangle.
      * @return A new {@link PulseDrawable} instance.
      */
-    public static PulseDrawable createHighlight(Context context) {
-        return createHighlight(context, new EndlessPulser());
+    public static PulseDrawable createRoundedRectangle(Context context, @Px int cornerRadius) {
+        return createRoundedRectangle(context, cornerRadius, new EndlessPulser());
+    }
+
+    /**
+     * Creates a {@link PulseDrawable} that will fill the bounds with a pulsing color. The {@link
+     * PulseDrawable} will continue pulsing forever (if this is not the desired behavior, please use
+     * {@link PulseEndAuthority}).
+     * @param context The {@link Context} under which the drawable is created.
+     * @return A new {@link PulseDrawable} instance.
+     */
+    public static PulseDrawable createRectangle(Context context) {
+        return createRoundedRectangle(context, 0);
     }
 
     /**
