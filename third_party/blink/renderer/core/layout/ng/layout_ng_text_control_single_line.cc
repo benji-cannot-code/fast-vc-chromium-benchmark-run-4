fd06bba8cc76acdf10138b7d9bdf031d1f8de92c
Kent Tamura <tkent@chromium.org>
2020-10-27 08:28:49 +0000

    TextControl NG: Do not add scrollbars to text fields
    This CL ports LayoutTextControlSingleLine::AllowsNonVisibleOverflow()
    to LayoutNGTextControlSingleLine.
    
    This CL has no behavior changes now, but it fixes
    baseline-alignment-and-overflow.tentative.html in WPT with
    LayoutNGTextField flag.
    
    Bug: 1040826
    Change-Id: If21b1b150922229a180293dd9f8c1cceb43ab70a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2501169
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821135}

--- third_party/blink/renderer/core/layout/ng/layout_ng_text_control_single_line.cc
+++ third_party/blink/renderer/core/layout/ng/layout_ng_text_control_single_line.cc
@@ -80,4 +80,10 @@ bool LayoutNGTextControlSingleLine::NodeAtPoint(
   return true;
 }
 
+bool LayoutNGTextControlSingleLine::AllowsNonVisibleOverflow() const {
+  NOT_DESTROYED();
+  // Do not show scrollbars even if overflow:scroll is specified.
+  return false;
+}
+
 }  // namespace blink
