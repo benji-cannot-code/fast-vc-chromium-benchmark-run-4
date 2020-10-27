58a6bf212c47d642da911181f932b94e0f567e35
Daniel Hosseinian <dhoss@chromium.org>
2020-10-27 02:59:16 +0000

    Remove Chai dependency from whenAttributeIs() WebUI test util
    The Chai dependency prohibits the util from being used in WebUI tests
    that are not configured to use Chai, like the PDF Viewer's.
    
    Change the assertion to be part of a condition. The assertion was
    already strong and unlikely to fail.
    
    Change-Id: I008a6b6ccb1cd8c83d3ec2dcea3476243e89cb0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495506
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821049}

--- chrome/test/data/webui/test_util.js
+++ chrome/test/data/webui/test_util.js
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // clang-format off
 // #import {afterNextRender, beforeNextRender, flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-// #import {assertEquals} from './chai_assert.js';
 // #import {NativeEventTarget as EventTarget} from 'chrome://resources/js/cr/event_target.m.js';
 // clang-format on
+
+// Do not depend on the Chai Assertion Library in this file. Some consumers of
+// the following test utils are not configured to use Chai.
 
 cr.define('test_util', function() {
   /**
@@ -27,8 +29,8 @@ cr.define('test_util', function() {
     return isDone() ? Promise.resolve() : new Promise(function(resolve) {
       new MutationObserver(function(mutations, observer) {
         for (const mutation of mutations) {
-          assertEquals('attributes', mutation.type);
-          if (mutation.attributeName === attributeName && isDone()) {
+          if (mutation.type === 'attributes' &&
+              mutation.attributeName === attributeName && isDone()) {
             observer.disconnect();
             resolve();
             return;
