e08f3c4e39ea2b22c7a9a9100fea760ec6dbd1ec
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-10-27 12:48:40 +0000

    Import wpt@64b32a9a020c3fa5bcd275cb60accad6fc4ec8c8
    Using wpt-import in Chromium cb73fbe5db8eefb98be329592ed447886b8dd64d.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    nzolghadr@chromium.org, yosin@chromium.org:
      external/wpt/input-events
    
    NOAUTOREVERT=true
    TBR=foolip@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-payments-fyi-rel
    Change-Id: I16879c47d22f1986fede07b4b516451e1ae84fdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2502101
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#821191}

---
+++ third_party/blink/web_tests/external/wpt/cors/preflight-cache-partitioning.sub.window.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/utils.js
+
+const TEST_PAGE =
+  "http://{{host}}:{{ports[http][0]}}/cors/resources/preflight-cache-partitioning.sub.html";
+const TEST_ANOTHER_PAGE =
+  "http://{{hosts[alt][]}}:{{ports[http][0]}}/cors/resources/preflight-cache-partitioning.sub.html";
+
+promise_test(async t => {
+  let uuid_token = token();
+
+  const TEST_PAGES = [TEST_PAGE, TEST_ANOTHER_PAGE];
+
+  // We will load the same page with different top-level origins to check if the
+  // CORS preflight cache is partitioned. The page will load the iframe with one
+  // origin and trigger the CORS preflight through fetching a cross-origin
+  // resources in the iframe.
+
+  for (let test_page of TEST_PAGES) {
+    let win;
+
+    await new Promise(resolve => {
+      window.onmessage = (e) => {
+        if (e.data.type === "loaded") {
+          resolve();
+        }
+      };
+
+      win = window.open(test_page);
+    });
+
+    await new Promise(resolve => {
+      win.postMessage({ type: "run", token: uuid_token }, "*");
+
+      window.onmessage = (e) => {
+        assert_equals(e.data.type, "pass", e.data.msg);
+        resolve();
+      };
+    });
+
+    win.close();
+  }
+}, "The preflight cache should be partitioned");
--- third_party/blink/web_tests/external/wpt/webdriver/tests/switch_to_window/switch.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/switch_to_window/switch.py
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from webdriver.transport import Response
 
 from tests.support.asserts import assert_error, assert_success
+from tests.support.inline import iframe, inline
 
 
 def switch_to_window(session, handle):
@@ -52,3 +53,16 @@ def test_no_browsing_context(session, url):
     assert_success(response)
 
     assert session.window_handle == new_handle
+
+
+def test_switch_to_window_sets_top_level_context(session):
+    session.url = inline(iframe("<p>foo"))
+
+    frame = session.find.css("iframe", all=False)
+    session.switch_frame(frame)
+    session.find.css("p", all=False)
+
+    response = switch_to_window(session, session.window_handle)
+    assert_success(response)
+
+    session.find.css("iframe", all=False)
