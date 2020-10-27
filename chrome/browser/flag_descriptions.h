99f210ce1a413db2b05684d17bcb1773e5acb4c2
Joel Riley <joelriley@google.com>
2020-10-27 23:07:37 +0000

    Add feature flag for new select-to-speak enhancements.
    Also exposed in chrome://flags.
    
    Bug: 1142601
    Change-Id: I0067a298e160510150dd793830a13837ccc083ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500261
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Joel Riley <joelriley@google.com>
    Cr-Commit-Position: refs/heads/master@{#821487}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2552,6 +2552,9 @@ extern const char kSchedulerConfigurationDescription[];
 extern const char kSchedulerConfigurationConservative[];
 extern const char kSchedulerConfigurationPerformance[];
 
+extern const char kSelectToSpeakNavigationControlName[];
+extern const char kSelectToSpeakNavigationControlDescription[];
+
 extern const char kSeparatePointingStickSettingsName[];
 extern const char kSeparatePointingStickSettingsDescription[];
 
