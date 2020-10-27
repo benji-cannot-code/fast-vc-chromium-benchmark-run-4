5d29985849ff5a668709530fea64d94ffc3272d3
Morten Stenshorne <mstensho@chromium.org>
2020-10-27 01:37:14 +0000

    Enable "gn check" for //third_party/blink/renderer/core/exported
    Bug: 800764
    Change-Id: I3a7afb3095b5f686664d5061cfe11dd906116980
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2484699
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821021}

--- third_party/blink/renderer/bindings/core/v8/script_controller.cc
+++ third_party/blink/renderer/bindings/core/v8/script_controller.cc
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/scriptable_document_parser.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
-#include "third_party/blink/renderer/core/exported/web_plugin_container_impl.h"
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
