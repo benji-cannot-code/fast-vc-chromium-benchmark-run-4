de0b583c9f111ac248439d5e9a3ef918a88c53c6
Leo Lai <cylai@google.com>
2020-10-27 14:29:55 +0000

    use AttestationClient to get enrollment id.
    We are deprecating attestation methods by CryptohomeClient.
    
    BUG=b:158955123
    TEST=unit_tests.
    
    Change-Id: I4a046aa0c11b720ecb1d6fee99afdba025234537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2497390
    Commit-Queue: Leo Lai <cylai@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821226}

--- chrome/browser/chromeos/attestation/enrollment_policy_observer.h
+++ chrome/browser/chromeos/attestation/enrollment_policy_observer.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/settings/device_settings_service.h"
+#include "chromeos/dbus/attestation/interface.pb.h"
 #include "chromeos/dbus/constants/attestation_constants.h"
 
 namespace policy {
@@ -20,9 +21,6 @@ class CloudPolicyClient;
 }
 
 namespace chromeos {
-
-class CryptohomeClient;
-
 namespace attestation {
 
 // A class which observes policy changes and triggers uploading identification
@@ -37,8 +35,7 @@ class EnrollmentPolicyObserver : public DeviceSettingsService::Observer {
 
   // A constructor which accepts custom instances useful for testing.
   EnrollmentPolicyObserver(policy::CloudPolicyClient* policy_client,
-                           DeviceSettingsService* device_settings_service,
-                           CryptohomeClient* cryptohome_client);
+                           DeviceSettingsService* device_settings_service);
 
   ~EnrollmentPolicyObserver() override;
 
@@ -57,8 +54,8 @@ class EnrollmentPolicyObserver : public DeviceSettingsService::Observer {
   // Gets an enrollment identifier directly.
   void GetEnrollmentId();
 
-  // Handles an enrollment identifer obtained directly.
-  void HandleEnrollmentId(const std::string& enrollment_id);
+  // Handles an enrollment identifier obtained directly.
+  void OnGetEnrollmentId(const ::attestation::GetEnrollmentIdReply& reply);
 
   // Reschedule an attempt to get an enrollment identifier directly.
   void RescheduleGetEnrollmentId();
@@ -70,7 +67,6 @@ class EnrollmentPolicyObserver : public DeviceSettingsService::Observer {
 
   DeviceSettingsService* device_settings_service_;
   policy::CloudPolicyClient* policy_client_;
-  CryptohomeClient* cryptohome_client_;
   int num_retries_;
   int retry_limit_;
   int retry_delay_;
--- chromeos/dbus/attestation/fake_attestation_client.h
+++ chromeos/dbus/attestation/fake_attestation_client.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/attestation/attestation_client.h"
 
 #include <deque>
+#include <string>
 #include <vector>
 
 #include "base/component_export.h"
@@ -110,6 +111,9 @@ class COMPONENT_EXPORT(CHROMEOS_DBUS_ATTESTATION) FakeAttestationClient
   const std::vector<::attestation::DeleteKeysRequest>& delete_keys_history()
       const override;
   void ClearDeleteKeysHistory() override;
+  void set_enrollment_id_ignore_cache(const std::string& id) override;
+  void set_cached_enrollment_id(const std::string& id) override;
+  void set_enrollment_id_dbus_error_count(int count) override;
 
   AttestationClient::TestInterface* GetTestInterface() override;
 
@@ -137,6 +141,11 @@ class COMPONENT_EXPORT(CHROMEOS_DBUS_ATTESTATION) FakeAttestationClient
 
   // Maintains the input request history of `DeleteKeys()`.
   std::vector<::attestation::DeleteKeysRequest> delete_keys_history_;
+
+  // Maintains building components reply to `GetEnrollmentId()`.
+  std::string enrollment_id_;
+  std::string enrollment_id_ignore_cache_;
+  int enrollment_id_dbus_error_count_ = 0;
 };
 
 }  // namespace chromeos
