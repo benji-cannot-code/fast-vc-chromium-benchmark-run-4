3ad88bccdca17d53a9c816db6cc545af36a0923b
Lucas Furukawa Gadani <lfg@chromium.org>
2020-10-27 08:16:44 +0000

    Remove usages of WebContents::GetRenderViewHost().
    This CL was uploaded by git cl split.
    
    R=lazyboy@chromium.org
    
    Bug: 1142671
    Change-Id: I7c026fe91d2a8333295694d6b9bae1aaf62a0bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2501124
    Auto-Submit: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821129}

--- components/renderer_context_menu/render_view_context_menu_base.cc
+++ components/renderer_context_menu/render_view_context_menu_base.cc
@@ -296,7 +296,7 @@ void RenderViewContextMenuBase::RemoveAdjacentSeparators() {
 }
 
 RenderViewHost* RenderViewContextMenuBase::GetRenderViewHost() const {
-  return source_web_contents_->GetRenderViewHost();
+  return source_web_contents_->GetMainFrame()->GetRenderViewHost();
 }
 
 WebContents* RenderViewContextMenuBase::GetWebContents() const {
