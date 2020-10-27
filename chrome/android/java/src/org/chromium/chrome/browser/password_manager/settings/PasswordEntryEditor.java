36ebef8fc26b16204c93d74ebdfe6a438b25591c
Ioana Pandele <ioanap@chromium.org>
2020-10-27 19:58:36 +0000

    [PwdEditAndroid] Replace entry viewer fragment with editor
    The editor should now be launched directly by tapping on the password
    entry in Settings > Password.
    
    This CL also separates the editing-specific tests from
    PasswordSettingsTest.java. Only integration tests that actually rely on
    PasswordSettings are left there.
    
    Bug: 1141409, 1122310
    Change-Id: Ie8d742d64e40b9881b35bf24a333d59c9b4cef02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494922
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821413}

--- chrome/android/java/src/org/chromium/chrome/browser/password_manager/settings/PasswordEntryEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/settings/PasswordEntryEditor.java
@@ -28,7 +28,6 @@ import org.chromium.ui.widget.Toast;
 /**
  * Password entry editor that allows editing passwords stored in Chrome.
  */
-
 public class PasswordEntryEditor extends Fragment {
     private EditText mSiteText;
     private EditText mUsernameText;
