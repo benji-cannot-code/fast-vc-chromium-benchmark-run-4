5dc27679ac1b86e687ad2c55872882418eca188c
Noel Gordon <noel@chromium.org>
2020-10-27 03:36:12 +0000

    [fileapp] Move media SWA test to FileManagerBrowserTest fixture
    Add new test Option for media_swa and use it in enable and load SWA in
    test from file_manager_browsertest_base.cc.
    
    Remove the SWAsFilesAppBrowserTest fixture: move its media SWA test to
    the FileManagerBrowserTest fixture.
    
    Bug: 1122379
    Change-Id: Ifbe75220777d0115a8e81c61ff2bb7fea447dcf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500683
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821057}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/file_manager/file_manager_browsertest_base.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/web_applications/system_web_app_manager.h"
-#include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/constants/chromeos_switches.h"
 #include "components/prefs/pref_service.h"
@@ -76,6 +74,11 @@ struct TestCase {
 
   TestCase& DisableFilesNg() {
     options.files_ng = false;
+    return *this;
+  }
+
+  TestCase& MediaSwa() {
+    options.media_swa = true;
     return *this;
   }
 
@@ -223,26 +226,6 @@ IN_PROC_BROWSER_TEST_P(FilesAppBrowserTest, Test) {
   StartTest();
 }
 
-// Files app tests that require SWA (System Web Apps).
-class SWAsFilesAppBrowserTest : public FilesAppBrowserTest {
- public:
-  SWAsFilesAppBrowserTest() = default;
-  SWAsFilesAppBrowserTest(const SWAsFilesAppBrowserTest&) = delete;
-  SWAsFilesAppBrowserTest& operator=(const SWAsFilesAppBrowserTest&) = delete;
-
-  void SetUpOnMainThread() override {
-    web_app::WebAppProvider::Get(profile())
-        ->system_web_app_manager()
-        .InstallSystemAppsForTesting();
-
-    FilesAppBrowserTest::SetUpOnMainThread();
-  }
-};
-
-IN_PROC_BROWSER_TEST_P(SWAsFilesAppBrowserTest, Test) {
-  StartTest();
-}
-
 // A version of the FilesAppBrowserTest that supports spanning browser restart
 // to allow testing prefs and other things.
 class ExtendedFilesAppBrowserTest : public FilesAppBrowserTest {
@@ -343,9 +326,9 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
     OpenImageBacklight, /* open_image_backlight.js */
-    SWAsFilesAppBrowserTest,
-    ::testing::Values(TestCase("imageOpenBacklight").InGuestMode(),
-                      TestCase("imageOpenBacklight")));
+    FilesAppBrowserTest,
+    ::testing::Values(TestCase("imageOpenBacklight").MediaSwa().InGuestMode(),
+                      TestCase("imageOpenBacklight").MediaSwa()));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
     OpenImageFiles, /* open_image_files.js */
--- chrome/browser/chromeos/file_manager/file_manager_browsertest_base.h
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest_base.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/metrics/user_action_tester.h"
 #include "base/test/scoped_feature_list.h"
-#include "base/values.h"
 #include "chrome/browser/chromeos/crostini/fake_crostini_features.h"
 #include "chrome/browser/chromeos/drive/drive_integration_service.h"
 #include "chrome/browser/chromeos/file_manager/devtools_listener.h"
@@ -78,6 +77,9 @@ class FileManagerBrowserTestBase : public content::DevToolsAgentHostObserver,
 
     // Whether test needs the files-ng feature.
     bool files_ng = true;
+
+    // Whether test needs the media-swa apps.
+    bool media_swa = false;
 
     // Whether test needs a native SMB file system provider.
     bool native_smb = true;
