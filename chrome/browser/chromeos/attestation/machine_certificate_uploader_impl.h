ad77aaa967de4449c99d0893eb2f1142c784a577
Leo Lai <cylai@google.com>
2020-10-27 16:43:59 +0000

    use AttestationClient to get/set key payloads.
    We are deprecating attestation methods by CryptohomeClient.
    
    BUG=b:158955123
    TEST=unit_tests.
    
    Change-Id: I244899c638df91c27f34ee3e5888077d61248c70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2497903
    Commit-Queue: Leo Lai <cylai@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821289}

--- chrome/browser/chromeos/attestation/machine_certificate_uploader_impl.h
+++ chrome/browser/chromeos/attestation/machine_certificate_uploader_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/attestation/machine_certificate_uploader.h"
+#include "chromeos/dbus/attestation/interface.pb.h"
 #include "chromeos/dbus/constants/attestation_constants.h"
 
 namespace policy {
@@ -72,19 +73,14 @@ class MachineCertificateUploaderImpl : public MachineCertificateUploader {
 
   // Checks if a certificate has already been uploaded and, if not, upload.
   void CheckIfUploaded(const std::string& pem_certificate_chain,
-                       const std::string& key_payload);
-
-  // Gets the payload associated with the EMK and sends it to |callback|,
-  // or call |on_failure| with no arguments if the payload cannot be obtained.
-  void GetKeyPayload(base::RepeatingCallback<void(const std::string&)> callback,
-                     base::RepeatingCallback<void()> on_failure);
+                       const ::attestation::GetKeyInfoReply& reply);
 
   // Called when a certificate upload operation completes.  On success, |status|
   // will be true.
   void OnUploadComplete(bool status);
 
   // Marks a key as uploaded in the payload proto.
-  void MarkAsUploaded(const std::string& key_payload);
+  void MarkAsUploaded(const ::attestation::GetKeyInfoReply& reply);
 
   // Handles failure of getting a certificate.
   void HandleGetCertificateFailure(AttestationStatus status);
