cf002ed930eaab6a5303327f0018a5d7356a42d2
Sammie Quon <sammiequon@chromium.org>
2020-10-27 22:05:00 +0000

    capture_mode: Fix some glitches with affordance circles.
    Add anti-aliasing to circles, makes more smooth, screenshot in bug.
    Damage a larger region on repaint to account for the shadows.
    
    Test: manual
    Change-Id: I4707fd057adcb4d5a0f6f4c412683203fa107ea3
    Fixed: 1142935
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2503400
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821464}

--- ash/capture_mode/capture_mode_session.cc
+++ ash/capture_mode/capture_mode_session.cc
@@ -85,6 +85,7 @@ constexpr SkColor kRegionBorderColor = SK_ColorWHITE;
 constexpr SkColor kCaptureRegionColor = SkColorSetA(gfx::kGoogleBlue300, 77);
 
 // Values for the shadows of the capture region components.
+constexpr int kRegionAffordanceCircleShadow2Blur = 6;
 constexpr gfx::ShadowValue kRegionOutlineShadow(gfx::Vector2d(0, 0),
                                                 2,
                                                 SkColorSetARGB(41, 0, 0, 0));
@@ -94,8 +95,15 @@ constexpr gfx::ShadowValue kRegionAffordanceCircleShadow1(
     SkColorSetARGB(76, 0, 0, 0));
 constexpr gfx::ShadowValue kRegionAffordanceCircleShadow2(
     gfx::Vector2d(0, 2),
-    6,
+    kRegionAffordanceCircleShadow2Blur,
     SkColorSetARGB(38, 0, 0, 0));
+
+// When updating the capture region, request a repaint on the region and inset
+// such that the border, affordance circles and affordance circle shadows are
+// all repainted as well.
+constexpr int kDamageInsetDp = kCaptureRegionBorderStrokePx +
+                               kAffordanceCircleRadiusDp +
+                               kRegionAffordanceCircleShadow2Blur;
 
 // The minimum padding on each side of the capture region. If the capture button
 // cannot be placed in the center of the capture region and maintain this
@@ -508,6 +516,7 @@ void CaptureModeSession::PaintCaptureRegion(gfx::Canvas* canvas) {
   cc::PaintFlags circle_flags;
   circle_flags.setColor(kRegionBorderColor);
   circle_flags.setStyle(cc::PaintFlags::kFill_Style);
+  circle_flags.setAntiAlias(true);
   circle_flags.setLooper(gfx::CreateShadowDrawLooper(
       {kRegionAffordanceCircleShadow1, kRegionAffordanceCircleShadow2}));
 
@@ -777,11 +786,9 @@ void CaptureModeSession::OnLocatedEventReleased(
       GetCursorType(GetFineTunePosition(location_in_root, /*is_touch=*/false),
                     is_event_on_capture_bar));
 
-  // Do a repaint to show the affordance circles. See UpdateCaptureRegion to see
-  // how damage is calculated.
+  // Do a repaint to show the affordance circles.
   gfx::Rect damage_region = controller_->user_capture_region();
-  damage_region.Inset(
-      gfx::Insets(-kAffordanceCircleRadiusDp - kCaptureRegionBorderStrokePx));
+  damage_region.Inset(gfx::Insets(-kDamageInsetDp));
   layer()->SchedulePaint(damage_region);
 
   UpdateDimensionsLabelWidget(/*is_resizing=*/false);
@@ -807,8 +814,7 @@ void CaptureModeSession::UpdateCaptureRegion(
   // repainted.
   gfx::Rect damage_region = old_capture_region;
   damage_region.Union(new_capture_region);
-  damage_region.Inset(
-      gfx::Insets(-kAffordanceCircleRadiusDp - kCaptureRegionBorderStrokePx));
+  damage_region.Inset(gfx::Insets(-kDamageInsetDp));
   layer()->SchedulePaint(damage_region);
 
   controller_->set_user_capture_region(new_capture_region);
