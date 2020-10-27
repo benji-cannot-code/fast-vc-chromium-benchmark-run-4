0f69dc867a8fa264208f93681ca7b0516454cd16
Kent Tamura <tkent@chromium.org>
2020-10-27 05:43:32 +0000

    TextControl NG: Port LayoutTextControlSingleLine::NodeAtPoint
    ... to LayoutNGTextControlSingleLine.
    
    This CL has no behavior changes for now, however this CL fixes at least
    four unit test failures with LayoutNGTextField flag.
    
    Bug: 1040826
    Change-Id: I9c6677b7de9d595602caa312678856d79ffb94d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2501103
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821097}

--- third_party/blink/renderer/core/layout/ng/layout_ng_text_control_single_line.h
+++ third_party/blink/renderer/core/layout/ng/layout_ng_text_control_single_line.h
@@ -17,6 +17,8 @@ class LayoutNGTextControlSingleLine final : public LayoutNGBlockFlow {
 
  private:
   HTMLElement* InnerEditorElement() const;
+  Element* ContainerElement() const;
+  Element* EditingViewPortElement() const;
 
   bool IsOfType(LayoutObjectType) const override;
 
@@ -31,6 +33,11 @@ class LayoutNGTextControlSingleLine final : public LayoutNGBlockFlow {
   }
 
   void StyleDidChange(StyleDifference, const ComputedStyle* old_style) override;
+
+  bool NodeAtPoint(HitTestResult& result,
+                   const HitTestLocation& hit_test_location,
+                   const PhysicalOffset& accumulated_offset,
+                   HitTestAction hit_test_action) override;
 };
 
 }  // namespace blink
