3c3f11653c496a7b7098b1d5522d44352cdb4b13
ssid <ssid@chromium.org>
2020-10-27 21:27:50 +0000

    Track tasks run by task runner wrapper
    Chained tasks and async tasks are traced using the wrapper class name.
    Add trace events to track these tasks
    
    BUG=1136635
    
    Change-Id: Ibafce336fbfc4f7491bd196b93b92bf42ad143e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2491402
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821440}

--- base/android/java/src/org/chromium/base/task/AsyncTask.java
+++ base/android/java/src/org/chromium/base/task/AsyncTask.java
@@ -52,7 +52,7 @@ public abstract class AsyncTask<Result> {
     private static final StealRunnableHandler STEAL_RUNNABLE_HANDLER = new StealRunnableHandler();
 
     private final Callable<Result> mWorker;
-    private final FutureTask<Result> mFuture;
+    private final NamedFutureTask mFuture;
 
     private volatile @Status int mStatus = Status.PENDING;
 
@@ -396,6 +396,14 @@ public abstract class AsyncTask<Result> {
 
         Class getBlamedClass() {
             return AsyncTask.this.getClass();
+        }
+
+        @Override
+        @SuppressWarnings("NoDynamicStringsInTraceEventCheck")
+        public void run() {
+            try (TraceEvent e = TraceEvent.scoped("AsyncTask.run: " + mFuture.getBlamedClass())) {
+                super.run();
+            }
         }
 
         @Override
