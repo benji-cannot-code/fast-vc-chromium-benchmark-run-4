6d62b4573382f41fc1aef7e6703847cb053b65ce
Lucas Furukawa Gadani <lfg@chromium.org>
2020-10-27 15:08:35 +0000

    Remove usages of WebContents::GetRenderViewHost().
    This CL was uploaded by git cl split.
    
    R=bsep@chromium.org
    
    Bug: 1142671
    Change-Id: I7b1fdd8a277ab829520a07e0781a02325d9f43fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500612
    Auto-Submit: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821239}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -2638,7 +2638,9 @@ void BrowserView::OnWindowBeginUserBoundsChange() {
   if (!web_contents)
     return;
   interactive_resize_in_progress_ = true;
-  web_contents->GetRenderViewHost()->NotifyMoveOrResizeStarted();
+  web_contents->GetMainFrame()
+      ->GetRenderViewHost()
+      ->NotifyMoveOrResizeStarted();
 }
 
 void BrowserView::OnWindowEndUserBoundsChange() {
