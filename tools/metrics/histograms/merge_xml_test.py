2a7a4e16c367af04e95115be6a3b49c0de9e2213
Weilun Shi <sweilun@chromium.org>
2020-10-28 23:27:56 +0000

    Disable expand_owners in most of the MergeFiles call
    The expand_owners.ExpandHistogramsOWNERS function call takes 5 sec and
    it's unnecessary for most of the case. This helps reduce the PRESUBMIT
    runtime by at least 5 sec.
    
    Bug: 1142714
    Change-Id: I5c56cbb9904a861fc7e0954e360cb692fb26edaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505463
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Auto-Submit: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821947}

--- tools/metrics/histograms/merge_xml_test.py
+++ tools/metrics/histograms/merge_xml_test.py
@@ -54,6 +54,7 @@ class MergeXmlTest(unittest.TestCase):
 <histogram name="Foo.Bar" units="xxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyzzzz"
     expires_after="M85">
   <owner>person@chromium.org</owner>
+  <component>Component</component>
   <summary>Foo</summary>
 </histogram>
 
@@ -153,6 +154,7 @@ class MergeXmlTest(unittest.TestCase):
 <histogram name="Foo.Bar" units="xxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyzzzz"
     expires_after="M85">
   <owner>person@chromium.org</owner>
+  <component>Component</component>
   <summary>Foo</summary>
 </histogram>
 
