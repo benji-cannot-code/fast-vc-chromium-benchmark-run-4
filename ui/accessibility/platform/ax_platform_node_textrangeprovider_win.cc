862b4262adcca9f700f6535be03edfc910623986
Benjamin Beaudry <bebeaudr@microsoft.com>
2020-10-28 23:16:57 +0000

    Replace use of ScopedObserver by a simple observer in TextRangeEndpoints
    In CL:2432805, we added a ScopedObserver on
    AXPlatformNodeTextRangeProviderWin::TextRangeProvider. However, that
    appears to cause a crash and we decided to go back to a simple observer
    instead.
    
    The crash was caused by the difference in lifetime of the AXTreeManager
    and the AXPlatformNodeTextRangeProviderWin. In some cases, the
    AXTreeManager can get deleted before the TextRangeEndpoints does, so
    when the destructor of the ScopedObserver calls
    ScopedObserver::RemoveAll on an already deleted AXTreeManager, it
    crashes.
    
    Bug: N/A
    Change-Id: I24291b6609d9f1102f94e177d1751b14e005425b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505774
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#821942}

--- ui/accessibility/platform/ax_platform_node_textrangeprovider_win.cc
+++ ui/accessibility/platform/ax_platform_node_textrangeprovider_win.cc
@@ -1448,7 +1448,10 @@ AXPlatformNodeTextRangeProviderWin::TextRangeEndpoints::TextRangeEndpoints() {
   end_ = AXNodePosition::CreateNullPosition();
 }
 
-AXPlatformNodeTextRangeProviderWin::TextRangeEndpoints::~TextRangeEndpoints() {}
+AXPlatformNodeTextRangeProviderWin::TextRangeEndpoints::~TextRangeEndpoints() {
+  SetStart(AXNodePosition::CreateNullPosition());
+  SetEnd(AXNodePosition::CreateNullPosition());
+}
 
 void AXPlatformNodeTextRangeProviderWin::TextRangeEndpoints::SetStart(
     AXPositionInstance new_start) {
@@ -1487,15 +1490,15 @@ void AXPlatformNodeTextRangeProviderWin::TextRangeEndpoints::AddObserver(
   AXTreeManager* ax_tree_manager =
       AXTreeManagerMap::GetInstance().GetManager(tree_id);
   DCHECK(ax_tree_manager);
-  observer_.Add(ax_tree_manager);
+  ax_tree_manager->AddObserver(this);
 }
 
 void AXPlatformNodeTextRangeProviderWin::TextRangeEndpoints::RemoveObserver(
     const AXTreeID tree_id) {
   AXTreeManager* ax_tree_manager =
       AXTreeManagerMap::GetInstance().GetManager(tree_id);
-  DCHECK(ax_tree_manager);
-  observer_.Remove(ax_tree_manager);
+  if (ax_tree_manager)
+    ax_tree_manager->RemoveObserver(this);
 }
 
 void AXPlatformNodeTextRangeProviderWin::TextRangeEndpoints::
