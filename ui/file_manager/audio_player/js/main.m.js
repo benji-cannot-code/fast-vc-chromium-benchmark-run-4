c263f96cb3350a5430c8ab309030705d2c5ac327
Luciano Pacheco <lucmult@chromium.org>
2020-10-28 10:58:37 +0000

    Audio Player: Add the main entry point for JS modules
    Add the main entry point for JS modules for Audio Player. Currently it
    contains the first two dependencies that have the module generated.
    
    Use this main.m.js generate the JS bundle for Audio Player.
    
    Use preprocess_grit() rules to group all required resources in the same
    directory to be able to generate the JS bundle. These are also used to
    inline images in CSS files.
    
    Bug: 1133186, 1030998
    Change-Id: Ia40deab89d86cbeb9cf0a7855fda7268830fb15d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2504440
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821672}

---
+++ ui/file_manager/audio_player/js/main.m.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Main JS Module for Audio Player. It replaces
+ * audio_player_scripts.js
+ */
+
+import '../../base/js/app_util.m.js';
+import '../../base/js/volume_manager_types.m.js';
+
+console.log('AudioPlayer main.m.js loaded');
