c715e1c1d18da3d8c32ceb158b1a365800dd0a0e
Hidehiko Abe <hidehiko@chromium.org>
2020-10-28 06:24:10 +0000

    Lacros IME: apply preedit styles.
    Bug: 1140537
    Test: Ran Lacros with enabling IME locally with JP IME.
    Change-Id: Ie4c4533f3352fdfbfb57047bde7176047c311ba9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2487740
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821624}

--- ui/ozone/platform/wayland/host/zwp_text_input_wrapper_v1.h
+++ ui/ozone/platform/wayland/host/zwp_text_input_wrapper_v1.h
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_OZONE_PLATFORM_WAYLAND_HOST_ZWP_TEXT_INPUT_WRAPPER_V1_H_
 #define UI_OZONE_PLATFORM_WAYLAND_HOST_ZWP_TEXT_INPUT_WRAPPER_V1_H_
 
+#include <stdint.h>
+
+#include <vector>
+
 #include <text-input-unstable-v1-client-protocol.h>
-#include <string>
 
 #include "ui/ozone/platform/wayland/host/zwp_text_input_wrapper.h"
 
@@ -99,7 +102,8 @@ class ZWPTextInputWrapperV1 : public ZWPTextInputWrapper {
   wl::Object<zwp_text_input_v1> obj_;
   ZWPTextInputWrapperClient* client_;
 
-  int32_t preedit_cursor_;
+  std::vector<ZWPTextInputWrapperClient::SpanStyle> spans_;
+  int32_t preedit_cursor_ = -1;
 };
 
 }  // namespace ui
