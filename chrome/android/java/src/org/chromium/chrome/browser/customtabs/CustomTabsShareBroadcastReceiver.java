3e575484a072134bc43aa7e992b6cf141ffda5b1
Sophey Dong <sophey@chromium.org>
2020-10-28 20:07:58 +0000

    Reland "[ShareCCT] Implement sharing in the toolbar and by default in CCT."
    This is a reland of 6769a912e3506c27de0132ba26767da80aebee87
    This caches the SHARE_BY_DEFAULT_IN_CCT flag, which should prevent JNI not enabled crashes.
    
    Original change's description:
    > [ShareCCT] Implement sharing in the toolbar and by default in CCT.
    >
    > Bug: 1135311
    > Change-Id: I967300591d383a21b9f3f40233422b763180ddb0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2459403
    > Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Peter Conn <peconn@chromium.org>
    > Commit-Queue: Sophey Dong <sophey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#820029}
    
    Bug: 1135311
    Change-Id: I46e7b10e56f092e6fcec5c07b1610dfd71649ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2501189
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821864}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsShareBroadcastReceiver.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.customtabs;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Receives shared content broadcast from Chrome Custom Tabs and shows a share sheet to share the
+ * url.
+ */
+public final class CustomTabsShareBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String url = intent.getDataString();
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, url);
+        Intent chooserIntent = Intent.createChooser(shareIntent, null);
+        chooserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(chooserIntent);
+    }
+}
