ddfb16417a1247ffcfb2cda39731a25400afd7b0
Michael Checo <michaelcheco@google.com>
2020-10-28 03:26:10 +0000

    Scanning: Add scan-preview element
    - Shows a preview of a scanned document
    - A follow-up CL will use and style this element
    
    Bug: 1059779
    Test: browser_tests --gtest_filter=ScanningUIBrowserTest.All
    Change-Id: Ib5a9fe49f87c14059391900777247615cf7a6184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500430
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821591}

--- chrome/test/data/webui/chromeos/scanning/scanning_app_test.js
+++ chrome/test/data/webui/chromeos/scanning/scanning_app_test.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // TODO(jschettler): Use es6 module for mojo binding (crbug/1004256).
 import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
+import 'chrome://scanning/scan_preview.js';
 import 'chrome://scanning/scanning_app.js';
 
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -716,5 +717,27 @@ suite('ResolutionSelectTest', () => {
     // Verify the dropdown is enabled when there's more than one option.
     assertEquals(2, select.length);
     assertFalse(select.disabled);
+  });
+});
+
+suite('ScanPreviewTest', () => {
+  /** @type {?ScanPreviewElement} */
+  let scanPreview = null;
+
+  setup(() => {
+    scanPreview = document.createElement('scan-preview');
+    assertTrue(!!scanPreview);
+    document.body.appendChild(scanPreview);
+  });
+
+  teardown(() => {
+    if (scanPreview) {
+      scanPreview.remove();
+    }
+    scanPreview = null;
+  });
+
+  test('initializeScanPreview', () => {
+    assertTrue(!!scanPreview.$$('.preview'));
   });
 });
