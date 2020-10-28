b3784058aa192bc7593171298b840a4ae4a9a670
Shrek Shao <shrekshao@google.com>
2020-10-28 02:49:13 +0000

    GPUDevice and webgpu canvas context ukm metrics
    New UKM metrics Request doc: go/client-rendering-api-ukm-review
    
    A follow up of http://crrev.com/c/2473043
    
    Adding UKM metrics collection for WebGPU canvas context and GPUDevice request
    
    Bug: 1135706
    Change-Id: I0432ad82f5f5a99c85d966ca3b8ff58a5be3cecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2501069
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Shrek Shao <shrekshao@google.com>
    Cr-Commit-Position: refs/heads/master@{#821577}

--- third_party/blink/renderer/modules/webgpu/gpu_adapter.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_adapter.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/webgpu/gpu_adapter.h"
 
+#include "services/metrics/public/cpp/ukm_builders.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_object_builder.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_device_descriptor.h"
@@ -68,6 +69,9 @@ void GPUAdapter::OnRequestDeviceCallback(ScriptPromiseResolver* resolver,
         execution_context, GetDawnControlClient(), this, device_client_id,
         descriptor);
     resolver->Resolve(device);
+    ukm::builders::ClientRenderingAPI(execution_context->UkmSourceID())
+        .SetGPUDevice(static_cast<int>(true))
+        .Record(execution_context->UkmRecorder());
   } else {
     resolver->Reject(MakeGarbageCollected<DOMException>(
         DOMExceptionCode::kOperationError,
