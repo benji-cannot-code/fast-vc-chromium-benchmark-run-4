b36f846c86d287de7cce4159e827265fcdf16301
Xianzhu Wang <wangxianzhu@chromium.org>
2020-10-28 00:08:14 +0000

    Use visual overflow rect for layout shift tracking
    Bug: 1141739
    Change-Id: I3b41c6328bf328b6734c6eadc422c45534fe42da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2503330
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821511}

--- third_party/blink/renderer/core/layout/layout_shift_tracker.h
+++ third_party/blink/renderer/core/layout/layout_shift_tracker.h
@@ -99,8 +99,7 @@ class CORE_EXPORT LayoutShiftTracker final
     struct Geometry {
       PhysicalOffset paint_offset;
       LayoutSize size;
-      bool has_overflow_clip;
-      PhysicalRect layout_overflow_rect;
+      PhysicalRect visual_overflow_rect;
     };
     HeapHashMap<Member<const Node>, Geometry> geometries_before_detach_;
   };
