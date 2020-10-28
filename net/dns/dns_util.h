e154cc2e7a844e777bd42a209ac43a05bbc659c2
Eric Orth <ericorth@chromium.org>
2020-10-28 18:00:47 +0000

    Improve DNS name parsing
    *Allow optionally enforcing that the name ends with a zero-length label
     (the root label).
    *Add additional validation that the name doesn't use DNS name
     compression, and that the name is within the max DNS name length.
    *Add an overload that takes a BigEndianReader as input. Better supports
     parsing within a DNS message when the name needs to be parsed to know
     its total length and where the next data starts after the name.
    
    These improvements should be useful in parsing HTTPS records.
    
    Bug: 1138620
    Change-Id: Ice66aa880a8c5407af3bfd9a14d720d518ae2136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2493153
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821794}

--- net/dns/dns_util.h
+++ net/dns/dns_util.h
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/public/dns_query_type.h"
 #include "net/dns/public/secure_dns_mode.h"
 
+namespace base {
+class BigEndianReader;
+}  // namespace base
+
 namespace net {
 
 class AddressList;
@@ -66,10 +70,21 @@ NET_EXPORT_PRIVATE bool IsValidUnrestrictedDNSDomain(
 NET_EXPORT_PRIVATE bool IsValidHostLabelCharacter(char c, bool is_first_char);
 
 // Converts a domain in DNS format to a dotted string. Excludes the dot at the
-// end. Assumes the standard terminating zero-length label at the end if not
-// included in the input. Returns nullopt on malformed input.
+// end.  Returns nullopt on malformed input.
+//
+// If `require_complete` is true, input will be considered malformed if it does
+// not contain a terminating zero-length label. If false, assumes the standard
+// terminating zero-length label at the end if not included in the input.
+//
+// DNS name compression (see RFC 1035, section 4.1.4) is disallowed and
+// considered malformed. To handle a potentially compressed name, in a
+// DnsResponse object, use DnsRecordParser::ReadName().
 NET_EXPORT base::Optional<std::string> DnsDomainToString(
-    base::StringPiece dns_name);
+    base::StringPiece dns_name,
+    bool require_complete = false);
+NET_EXPORT base::Optional<std::string> DnsDomainToString(
+    base::BigEndianReader& reader,
+    bool require_complete = false);
 
 // Return the expanded template when no variables have corresponding values.
 NET_EXPORT_PRIVATE std::string GetURLFromTemplateWithoutParameters(
