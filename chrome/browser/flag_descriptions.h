f3441e732ce8928fe877fdf0c5d3677a9c154080
Matt Menard <mattme@google.com>
2020-10-28 19:15:10 +0000

    Add flag for print server scaling
    Enables print server scaling support where print server may be selected
    individually when more than 16 print servers are available via policy.
    
    Relevant Design Docs: go/cros-print-server-scaling and go/cros-print-servers-backend
    
    Bug: b:168650771
    Change-Id: I16c10f4e80504e1c0b0cfda8ce93af5b724e917d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2499164
    Commit-Queue: Matt Menard <mattme@google.com>
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821830}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1097,6 +1097,9 @@ extern const char kPrintJobManagementAppDescription[];
 extern const char kPrintSaveToDriveName[];
 extern const char kPrintSaveToDriveDescription[];
 
+extern const char kPrintServerScalingName[];
+extern const char kPrintServerScalingDescription[];
+
 extern const char kPrivacyAdvisorName[];
 extern const char kPrivacyAdvisorDescription[];
 
