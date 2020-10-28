a3c24fdd4ab85b68d784ea133d2434e8fc5145f3
Dominik Röttsches <drott@chromium.org>
2020-10-28 12:06:00 +0000

    Mark two ChromeVoxEditingTest as disabled
    Bug: 1143192
    Change-Id: Ic26cb52ff89f80aacdfb65a1611ddb6b68e27490
    Tbr: nektar, dmazzoni
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2503735
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821679}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/editing_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/editing_test.js
@@ -1524,7 +1524,8 @@ TEST_F('ChromeVoxEditingTest', 'InputEvents', function() {
   });
 });
 
-TEST_F('ChromeVoxEditingTest', 'TextAreaEvents', function() {
+// Flaky test, see https://crbug.com/1143192
+TEST_F('ChromeVoxEditingTest', 'DISABLED_TextAreaEvents', function() {
   const site = `<textarea></textarea>`;
   this.runWithLoadedTree(site, async function(root) {
     const textArea = root.find({role: RoleType.TEXT_FIELD});
@@ -1564,7 +1565,8 @@ TEST_F('ChromeVoxEditingTest', 'TextAreaEvents', function() {
   });
 });
 
-TEST_F('ChromeVoxEditingTest', 'ContentEditableEvents', function() {
+// Flaky test, see https://crbug.com/1143192
+TEST_F('ChromeVoxEditingTest', 'DISABLED_ContentEditableEvents', function() {
   const site = `<div role="textbox" contenteditable></div>`;
   this.runWithLoadedTree(site, async function(root) {
     const contentEditable = root.find({role: RoleType.TEXT_FIELD});
