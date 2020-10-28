f72f17a46e1c91fd97a4193d629835d663c0e0bb
Nate Chapin <japhet@chromium.org>
2020-10-28 16:09:13 +0000

    Handle null GetExecutionContext() in LinkStyle::Process
    Bug: 1139708
    Test: http/tests/misc/favicon-link-in-detached-frame.html
    Change-Id: Ie6428708a3ea3dd80abad835deff189deeeedb28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2504577
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821736}

--- third_party/blink/renderer/core/html/link_style.cc
+++ third_party/blink/renderer/core/html/link_style.cc
@@ -336,6 +336,8 @@ void LinkStyle::Process() {
       params.href.IsValid() && !params.href.IsEmpty()) {
     if (!owner_->ShouldLoadLink())
       return;
+    if (!GetExecutionContext())
+      return;
     if (!GetExecutionContext()->GetSecurityOrigin()->CanDisplay(params.href))
       return;
     if (!GetExecutionContext()
