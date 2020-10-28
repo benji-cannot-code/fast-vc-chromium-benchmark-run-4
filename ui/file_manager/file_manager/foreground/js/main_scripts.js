9f7b54f2fe8551b691d3d006fafa0b7832974827
dpapad <dpapad@chromium.org>
2020-10-28 19:06:58 +0000

    Explicitly pull-in js/assert.js in ui/file_manager/
    This is in preparation of removing <include src="assert.js"> and
    flattenhtml="true" from js/util.js and js/cr/ui/array_data_model.js,
    which in turn is necessary to auto-generate grd entries for
    ui/webui/resources/js/cr/ui/.
    
    Bug: 1143194
    Change-Id: I128845c864e439cbf262f343cfa77153916d8dfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2504740
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821823}

--- ui/file_manager/file_manager/foreground/js/main_scripts.js
+++ ui/file_manager/file_manager/foreground/js/main_scripts.js
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="../../../image_loader/load_image_request.js">
 // <include src="../../../image_loader/image_loader_client.js">
 //
+// <include src="../../../../webui/resources/js/assert.js">
 // <include src="../../../../webui/resources/js/load_time_data.js">
 // <include src="../../../../webui/resources/js/action_link.js">
 // <include src="../../../../webui/resources/js/cr.js">
