3f3c76927cb370b7a8e5a206d59d84966f7cf37d
Dave Tapuska <dtapuska@chromium.org>
2020-10-28 19:29:12 +0000

    Remove GuestViewMsg_GuestDetached.
    DidDetach wasn't getting called and there was no receiver for the
    message anymore. So all of this code is dead. Remove it.
    
    BUG=993189,533069
    
    Change-Id: I71b712fd0f21a89ab60cd1c509afbb6e02604a23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505126
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821839}

--- components/guest_view/renderer/guest_view_request.cc
+++ components/guest_view/renderer/guest_view_request.cc
@@ -102,23 +102,4 @@ void GuestViewAttachRequest::HandleResponse(const IPC::Message& message) {
   ExecuteCallbackIfAvailable(argc, std::move(argv));
 }
 
-GuestViewDetachRequest::GuestViewDetachRequest(
-    GuestViewContainer* container,
-    v8::Local<v8::Function> callback,
-    v8::Isolate* isolate)
-    : GuestViewRequest(container, callback, isolate) {
-}
-
-GuestViewDetachRequest::~GuestViewDetachRequest() {
-}
-
-void GuestViewDetachRequest::PerformRequest() {
-  // TODO(wjmaclean): Remove this function.
-}
-
-void GuestViewDetachRequest::HandleResponse(const IPC::Message& message) {
-  DCHECK(message.type() == GuestViewMsg_GuestDetached::ID);
-  ExecuteCallbackIfAvailable(0 /* argc */, nullptr);
-}
-
 }  // namespace guest_view
