c4a89e71b630efb7a276fbe996e71048951ea441
Jordan Bayles <jophba@chromium.org>
2020-10-28 00:46:43 +0000

    Show Screen Recording prompt on Mac 10.15+
    Currently, in Chrome when trying to cast the screen we check for
    permission and tell the user to check System settings if we aren't
    allowed to cast the screen.  However, apps don't show up in System
    settings until the user has already approved or denied permission for
    them through a modal dialog that only appears if you try to capture a
    stream in your application without checking permission.
    
    This patch adds a call to CGDisplayStreamCreate in a new method in
    permissions_utils.h,  TryPromptUserForScreenCapture.
    
    Bug: 1139695
    Change-Id: I719aa6fb4188e723faa1fee3d3b51243edb0059e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495824
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821528}

--- ui/base/cocoa/permissions_utils.mm
+++ ui/base/cocoa/permissions_utils.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Foundation/Foundation.h>
 
 #include "base/mac/foundation_util.h"
+#include "base/mac/mac_util.h"
 #include "base/mac/scoped_cftyperef.h"
 
 namespace ui {
@@ -17,7 +18,7 @@ namespace ui {
 // or dock window running on another process is visible.
 // See https://crbug.com/993692.
 bool IsScreenCaptureAllowed() {
-  if (@available(macOS 10.15, *)) {
+  if (base::mac::IsAtLeastOS10_15()) {
     base::ScopedCFTypeRef<CFArrayRef> window_list(
         CGWindowListCopyWindowInfo(kCGWindowListOptionAll, kCGNullWindowID));
     int current_pid = [[NSProcessInfo processInfo] processIdentifier];
@@ -48,6 +49,24 @@ bool IsScreenCaptureAllowed() {
 
   // Screen capture is always allowed in older macOS versions.
   return true;
+}
+
+bool TryPromptUserForScreenCapture() {
+  if (base::mac::IsAtLeastOS10_15()) {
+    // On 10.15+, macOS will show the permissions prompt for Screen Recording
+    // if we request to create a display stream and our application is not
+    // in the applications list in System permissions. Stream creation will
+    // fail if the user denies permission, or if our application is already
+    // in the system permssion and is unchecked.
+    base::ScopedCFTypeRef<CGDisplayStreamRef> stream(CGDisplayStreamCreate(
+        CGMainDisplayID(), 1, 1, 'BGRA', nullptr,
+        ^(CGDisplayStreamFrameStatus status, uint64_t displayTime,
+          IOSurfaceRef frameSurface, CGDisplayStreamUpdateRef updateRef){
+        }));
+    return stream != nullptr;
+  } else {
+    return true;
+  }
 }
 
 }  // namespace ui
