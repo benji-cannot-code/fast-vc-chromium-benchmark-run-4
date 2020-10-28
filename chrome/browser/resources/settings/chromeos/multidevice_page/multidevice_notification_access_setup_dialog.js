a64ff922b7083f1298504d90c01a37acf91926db
Regan Hsu <hsuregan@google.com>
2020-10-28 16:00:34 +0000

    [CrOS PhoneHub] Show connecting state always upon clicking confirm.
    Currently, if the dialog does not receive a 'Connecting' status, it
    will not advance to the next state. This may happen if there is no
    bluetooth connection. This CL ensures that the dialog shows the next
    page upon requesting a connection instead of being stuck in the initial
    state.
    
    Bug: 1141596, 1106937
    Change-Id: If671d9c3d5bf2561944707b5686f2dd4a71bb47d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2499368
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821733}

--- chrome/browser/resources/settings/chromeos/multidevice_page/multidevice_notification_access_setup_dialog.js
+++ chrome/browser/resources/settings/chromeos/multidevice_page/multidevice_notification_access_setup_dialog.js
@@ -12,11 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Numerical values should not be changed because they must stay in sync with
- * notification_access_setup_operation.h, with the exception of NOT_STARTED.
+ * notification_access_setup_operation.h, with the exception of
+ * CONNECTION_REQUESTED.
  * @enum{number}
  */
 /* #export */ const NotificationAccessSetupOperationStatus = {
-  NOT_STARTED: 0,
+  CONNECTION_REQUESTED: 0,
   CONNECTING: 1,
   TIMED_OUT_CONNECTING: 2,
   CONNECTION_DISCONNECTED: 3,
@@ -33,10 +34,13 @@ Polymer({
   ],
 
   properties: {
-    /** @private {NotificationAccessSetupOperationStatus} */
+    /**
+     * A null |setupState_| indicates that the operation has not yet started.
+     * @private {?NotificationAccessSetupOperationStatus}
+     */
     setupState_: {
       type: Number,
-      value: NotificationAccessSetupOperationStatus.NOT_STARTED,
+      value: null,
     },
 
     /** @private */
@@ -78,13 +82,19 @@ Polymer({
 
   /** @private */
   showCancelButton_() {
-    return this.setupState_ ===
-        NotificationAccessSetupOperationStatus.NOT_STARTED ||
+    return this.setupState_ === null ||
+        this.setupState_ ===
+        NotificationAccessSetupOperationStatus.CONNECTION_REQUESTED ||
         this.setupState_ ===
         NotificationAccessSetupOperationStatus.CONNECTING ||
         this.setupState_ ===
         NotificationAccessSetupOperationStatus
             .SENT_MESSAGE_TO_PHONE_AND_WAITING_FOR_RESPONSE;
+  },
+
+  /** @private */
+  showConfirmButton_() {
+    return this.setupState_ === null;
   },
 
   /** @private */
@@ -96,6 +106,8 @@ Polymer({
   /** @private */
   onConfirmButtonClicked_() {
     this.browserProxy_.attemptNotificationSetup();
+    this.setupState_ =
+        NotificationAccessSetupOperationStatus.CONNECTION_REQUESTED;
   },
 
   /** @private */
@@ -114,17 +126,24 @@ Polymer({
    * @private
    */
   getTitle_() {
+    if (this.setupState_ === null) {
+      return this.i18n('multideviceNotificationAccessSetupAckTitle');
+    }
+
     const Status = NotificationAccessSetupOperationStatus;
     switch (this.setupState_) {
-      case Status.NOT_STARTED:
-        return this.i18n('multideviceNotificationAccessSetupAckTitle');
+      case Status.CONNECTION_REQUESTED:
       case Status.CONNECTING:
       case Status.SENT_MESSAGE_TO_PHONE_AND_WAITING_FOR_RESPONSE:
         return this.i18n('multideviceNotificationAccessSetupConnectingTitle');
       case Status.COMPLETED_SUCCESSFULLY:
         return this.i18n('multideviceNotificationAccessSetupCompletedTitle');
       case Status.TIMED_OUT_CONNECTING:
+        // TODO(hsuregan): Get the appropriate strings.
+        return 'Timed out connecting title';
       case Status.CONNECTION_DISCONNECTED:
+        // TODO(hsuregan): Get the appropriate strings.
+        return 'Connection disconnected title';
       default:
         return '';
     }
@@ -135,16 +154,24 @@ Polymer({
    * @private
    */
   getDescription_() {
+    if (this.setupState_ === null) {
+      return this.i18n('multideviceNotificationAccessSetupInstructions');
+    }
+
     const Status = NotificationAccessSetupOperationStatus;
     switch (this.setupState_) {
-      case Status.NOT_STARTED:
+      case Status.CONNECTION_REQUESTED:
       case Status.CONNECTING:
       case Status.SENT_MESSAGE_TO_PHONE_AND_WAITING_FOR_RESPONSE:
         return this.i18n('multideviceNotificationAccessSetupInstructions');
       case Status.COMPLETED_SUCCESSFULLY:
         return this.i18n('multideviceNotificationAccessSetupCompletedSummary');
       case Status.TIMED_OUT_CONNECTING:
+        // TODO(hsuregan): Get the appropriate strings.
+        return 'Timed out connecting body';
       case Status.CONNECTION_DISCONNECTED:
+        // TODO(hsuregan): Get the appropriate strings.
+        return 'Connection disconnected body';
       default:
         return '';
     }
