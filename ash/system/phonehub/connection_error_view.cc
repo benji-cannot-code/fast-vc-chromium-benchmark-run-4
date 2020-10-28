22802b79002e16f28cba91980db35045d4b7336d
Meilin Wang <meilinw@google.com>
2020-10-28 04:43:44 +0000

    [CrOS PhoneHub] Wire up buttons for Bluetooth disabled view.
    This CL adds handlers for buttons on the Bluetooth disabled view.
    
    Misc: test minor refactoring.
    
    BUG=1106937,1126208
    TEST=unittested.
    
    Change-Id: I891d81bb67f3ef7b52cdee8835d66a05f49b8341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495827
    Commit-Queue: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821610}

--- ash/system/phonehub/connection_error_view.cc
+++ ash/system/phonehub/connection_error_view.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/phonehub/phone_hub_interstitial_view.h"
 #include "ash/system/phonehub/phone_hub_metrics.h"
 #include "ash/system/phonehub/phone_hub_view_ids.h"
+#include "ash/system/phonehub/url_constants.h"
 #include "chromeos/components/phonehub/connection_scheduler.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -26,14 +27,6 @@ namespace ash {
 using phone_hub_metrics::InterstitialScreen;
 using phone_hub_metrics::InterstitialScreenEvent;
 using phone_hub_metrics::LogInterstitialScreenEvent;
-
-namespace {
-
-// TODO(meilinw): replace it with the real one.
-constexpr char kLearnMoreUrl[] =
-    "https://support.google.com/chromebook/?p=multi_device";
-
-}  // namespace
 
 ConnectionErrorView::ConnectionErrorView(
     ErrorStatus error,
---
+++ ash/system/phonehub/url_constants.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_PHONEHUB_URL_CONSTANTS_H_
+#define ASH_SYSTEM_PHONEHUB_URL_CONSTANTS_H_
+
+namespace ash {
+
+// TODO(meilinw): replace it with the real one.
+constexpr char kLearnMoreUrl[] =
+    "https://support.google.com/chromebook/?p=multi_device";
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_PHONEHUB_URL_CONSTANTS_H_
