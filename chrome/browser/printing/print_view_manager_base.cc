076a991a7377fc24edc985ba777e3e6cd3bebc07
Lucas Furukawa Gadani <lfg@chromium.org>
2020-10-28 04:36:29 +0000

    Remove usages of WebContents::GetRenderViewHost().
    This CL was uploaded by git cl split.
    
    R=rbpotter@chromium.org
    
    Bug: 1142671
    Change-Id: I82287d4fa4972fadf79f5336e89d2528521f3c4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500557
    Auto-Submit: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821607}

--- chrome/browser/printing/print_view_manager_base.cc
+++ chrome/browser/printing/print_view_manager_base.cc
@@ -711,9 +711,11 @@ bool PrintViewManagerBase::RenderAllMissingPagesNow() {
   }
 
   // We can't print if there is no renderer.
-  if (!web_contents() ||
-      !web_contents()->GetRenderViewHost() ||
-      !web_contents()->GetRenderViewHost()->IsRenderViewLive()) {
+  if (!web_contents() || !web_contents()->GetMainFrame()->GetRenderViewHost() ||
+      !web_contents()
+           ->GetMainFrame()
+           ->GetRenderViewHost()
+           ->IsRenderViewLive()) {
     return false;
   }
 
@@ -757,8 +759,11 @@ bool PrintViewManagerBase::CreateNewPrintJob(
   DisconnectFromCurrentPrintJob();
 
   // We can't print if there is no renderer.
-  if (!web_contents()->GetRenderViewHost() ||
-      !web_contents()->GetRenderViewHost()->IsRenderViewLive()) {
+  if (!web_contents()->GetMainFrame()->GetRenderViewHost() ||
+      !web_contents()
+           ->GetMainFrame()
+           ->GetRenderViewHost()
+           ->IsRenderViewLive()) {
     return false;
   }
 
