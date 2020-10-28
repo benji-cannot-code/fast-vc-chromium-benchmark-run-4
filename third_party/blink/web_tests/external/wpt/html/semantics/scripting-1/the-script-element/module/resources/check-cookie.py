2f2f343ee639e393eb252fd9ccf98cf693181b9c
Hiroshige Hayashizaki <hiroshige@chromium.org>
2020-10-28 14:09:20 +0000

    [WPT] Fix/add credentials mode tests for dynamic import()
    This CL fixes
    html/semantics/scripting-1/the-script-element/module/dynamic-import/dynamic-imports-credentials.sub.html
    and
    html/semantics/scripting-1/the-script-element/module/credentials.sub.html
    by:
    
    - Using `set-cookie.py` instead of `document.cookie = 'same=1';`, and
    - Passing `origin` parameter instead of relying on Origin request header,
      as Firefox doesn't send Origin header in same-origin requests.
    
    This CL also adds credentials mode tests for
    dynamic imports from classic scripts and
    setTimeout(with a string argument).
    
    Bug: 1133238, 1114988
    Change-Id: I18dce474f46e79c7d4161461760f0739806a1984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2437556
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821704}

--- third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/module/resources/check-cookie.py
+++ third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/module/resources/check-cookie.py
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
     headers = [
         (b"Content-Type", b"text/javascript"),
-        (b"Access-Control-Allow-Origin", request.headers.get(b"Origin")),
+        (b"Access-Control-Allow-Origin", request.GET.first(b"origin")),
         (b"Access-Control-Allow-Credentials", b"true")
     ]
     identifier = request.GET.first(b"id")
