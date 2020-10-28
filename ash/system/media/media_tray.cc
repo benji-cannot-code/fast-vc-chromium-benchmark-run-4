a09e5101f990f7a2c8479cb7c438dbb2b87bfe9a
Jazz Xu <jazzhsu@chromium.org>
2020-10-28 12:05:53 +0000

    CrOS GMC: Allow focus to media dialog and quick setting controls.
    This CL fix the issue where user cannot use keyboard to access
    global media controls dialog and detailed controls in quick settings.
    
    Change-Id: I909e00e4e55ad0460886eb3ea3e231eaf0d79195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2503399
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Jazz Xu <jazzhsu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821678}

--- ash/system/media/media_tray.cc
+++ ash/system/media/media_tray.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/media/media_tray.h"
 
+#include "ash/focus_cycler.h"
 #include "ash/public/cpp/ash_pref_names.h"
 #include "ash/public/cpp/media_notification_provider.h"
 #include "ash/resources/vector_icons/vector_icons.h"
@@ -289,6 +290,13 @@ void MediaTray::ShowBubble(bool show_by_click) {
   bubble_ = std::make_unique<TrayBubbleWrapper>(this, bubble_view,
                                                 false /*is_persistent*/);
   SetIsActive(true);
+
+  // Only focus the widget if it's opened by the keyboard.
+  if (!show_by_click) {
+    views::Widget* widget = bubble_->GetBubbleWidget();
+    widget->widget_delegate()->SetCanActivate(true);
+    Shell::Get()->focus_cycler()->FocusWidget(widget);
+  }
 
   base::UmaHistogramBoolean("Media.CrosGlobalMediaControls.RepeatUsageOnShelf",
                             bubble_has_shown_);
--- ash/system/media/unified_media_controls_view.cc
+++ ash/system/media/unified_media_controls_view.cc
@@ -106,6 +106,9 @@ UnifiedMediaControlsView::MediaActionButton::MediaActionButton(
 
   TrayPopupUtils::ConfigureTrayPopupButton(this);
   views::InstallCircleHighlightPathGenerator(this);
+
+  focus_ring()->SetColor(AshColorProvider::Get()->GetControlsLayerColor(
+      AshColorProvider::ControlsLayerType::kFocusRingColor));
 }
 
 void UnifiedMediaControlsView::MediaActionButton::SetAction(
@@ -148,7 +151,9 @@ UnifiedMediaControlsView::MediaActionButton::CreateInkDropRipple() const {
 UnifiedMediaControlsView::UnifiedMediaControlsView(
     UnifiedMediaControlsController* controller)
     : views::Button(this), controller_(controller) {
-  SetFocusBehavior(views::View::FocusBehavior::ACCESSIBLE_ONLY);
+  SetFocusBehavior(views::View::FocusBehavior::ALWAYS);
+  focus_ring()->SetColor(AshColorProvider::Get()->GetControlsLayerColor(
+      AshColorProvider::ControlsLayerType::kFocusRingColor));
 
   SetBackground(views::CreateRoundedRectBackground(
       AshColorProvider::Get()->GetControlsLayerColor(
