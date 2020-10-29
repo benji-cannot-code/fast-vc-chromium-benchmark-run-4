729b4dab2b72896ab7dcbc6246e59d7a57119aa9
Matt Falkenhagen <falken@chromium.org>
2020-10-29 00:30:40 +0000

    Clean up namespace and unused code in ChromeContentBrowserClient test.
    Most of the code was already written assuming not in content namespace,
    so just remove it.
    
    Change-Id: I7b637c8b372da83ce16be85eac0a238f15b6568b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2479882
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821973}

--- chrome/browser/chrome_content_browser_client_browsertest.cc
+++ chrome/browser/chrome_content_browser_client_browsertest.cc
@@ -67,16 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/launchservices_utils_mac.h"
 #endif
 
-namespace content {
-
 namespace {
-
-enum class NetworkServiceState {
-  kDisabled,
-  kEnabled,
-};
-
-}  // namespace
 
 // Use a test class with SetUpCommandLine to ensure the flag is sent to the
 // first renderer process.
@@ -85,7 +76,7 @@ class ChromeContentBrowserClientBrowserTest : public InProcessBrowserTest {
   ChromeContentBrowserClientBrowserTest() {}
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
-    IsolateAllSitesForTesting(command_line);
+    content::IsolateAllSitesForTesting(command_line);
   }
 
  private:
@@ -101,11 +92,11 @@ IN_PROC_BROWSER_TEST_F(ChromeContentBrowserClientBrowserTest,
   const GURL url(embedded_test_server()->GetURL("/title1.html"));
 
   ui_test_utils::NavigateToURL(browser(), url);
-  NavigationEntry* entry = browser()
-                               ->tab_strip_model()
-                               ->GetWebContentsAt(0)
-                               ->GetController()
-                               .GetLastCommittedEntry();
+  content::NavigationEntry* entry = browser()
+                                        ->tab_strip_model()
+                                        ->GetWebContentsAt(0)
+                                        ->GetController()
+                                        .GetLastCommittedEntry();
 
   ASSERT_TRUE(entry != NULL);
   EXPECT_EQ(url, entry->GetURL());
@@ -157,8 +148,8 @@ IN_PROC_BROWSER_TEST_F(IsolatedOriginNTPBrowserTest,
   // dedicated process.
   content::BrowserContext* context = browser()->profile();
   GURL isolated_url(https_test_server().GetURL("ntp.com", "/title1.html"));
-  scoped_refptr<SiteInstance> site_instance =
-      SiteInstance::CreateForURL(context, isolated_url);
+  scoped_refptr<content::SiteInstance> site_instance =
+      content::SiteInstance::CreateForURL(context, isolated_url);
   EXPECT_TRUE(site_instance->RequiresDedicatedProcess());
 
   // The site URL for the NTP URL should resolve to a chrome-search:// URL via
@@ -515,4 +506,4 @@ IN_PROC_BROWSER_TEST_F(ProtocolHandlerTest, ExternalProgramNotLaunched) {
 }
 #endif
 
-}  // namespace content
+}  // namespace
