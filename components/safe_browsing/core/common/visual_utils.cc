6ed022218f6349331bca64acc0baedde56fedfd4
Tim Volodine <timvolodine@google.com>
2020-10-29 18:37:43 +0000

    Move visual_utils to common/ to fix dependency check.
    Move visual_utils code and tests from components/safe_browsing/
    content/password_protection to compontents/safe_browsing/core/
    common/. The code for visual_utils currently lives in
    components/safe_browsing/content/password_protection/, but is now
    used by both password_protection and phishing_classifier code,
    therefore this CL moves the files and tests to the common/ directory.
    
    This solves the assert_no_deps issue and allows to remove the
    "nogncheck" in scorer.h when depending on the
    "c/s_b/c/password_protection" target from the phishing_classifier
    (which is in deps for chrome/renderer/BUILD.gn), see crbug.com/1138547
    for details.
    
    BUG=1138547,1136950
    
    Change-Id: Ic3796eec31c32ff6aee9e9267fbbebb0be090ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495066
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822278}

--- components/safe_browsing/core/common/visual_utils.cc
+++ components/safe_browsing/core/common/visual_utils.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_map>
 #include <vector>
 
-#include "components/safe_browsing/content/password_protection/visual_utils.h"
+#include "components/safe_browsing/core/common/visual_utils.h"
 
 #include "base/check_op.h"
 #include "base/numerics/checked_math.h"
--- components/safe_browsing/core/common/visual_utils_unittest.cc
+++ components/safe_browsing/core/common/visual_utils_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/safe_browsing/content/password_protection/visual_utils.h"
+#include "components/safe_browsing/core/common/visual_utils.h"
 
 #include "base/test/test_discardable_memory_allocator.h"
 #include "testing/gmock/include/gmock/gmock.h"
