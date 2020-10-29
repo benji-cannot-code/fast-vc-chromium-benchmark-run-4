463041ecbd3dd627939bb3d39fa116100c5d1e96
Adam Langley <agl@chromium.org>
2020-10-29 19:14:43 +0000

    webauthn: register caBLEv2 GCM messages at browser startup.
    For demo purposes it was fine to register the GCM handler only once the
    code had been manually invoked. In general, however, the Chrome process
    on Android might be evicted at any point and, unless the handler is
    registered during browser startup, GCM messages can be lost.
    
    The |Registration| is now owned outside of the module and is passed in
    as needed, replacing the pointer to the |InstanceIDDriver|.
    
    In order to do the minimum possible at startup, creating a
    |Registration| now only registers a handler. Ensuring that a
    registration exists with the GCM servers is deferred until the module is
    actually triggered.
    
    BUG=1002262
    
    Change-Id: Ide8fc91c5f35c92b74ab70568bb05115d7ce3e44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2488601
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822307}

---
+++ chrome/browser/webauthn/android/cable_module_android.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_WEBAUTHN_ANDROID_CABLE_MODULE_ANDROID_H_
+#define CHROME_BROWSER_WEBAUTHN_ANDROID_CABLE_MODULE_ANDROID_H_
+
+namespace webauthn {
+namespace authenticator {
+
+// RegisterForCloudMessages installs a |GCMAppHandler| that handles caBLEv2
+// message in the |GCMDriver| connected to the primary profile. This should be
+// called during browser startup to ensure that the |GCMAppHandler| is
+// registered before any GCM messages are processed. (Otherwise they will be
+// dropped.)
+void RegisterForCloudMessages();
+
+}  // namespace authenticator
+}  // namespace webauthn
+
+#endif
