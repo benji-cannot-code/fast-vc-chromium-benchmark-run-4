3ead754aa5ab1f0146140bf6cb87ad3620c9fd28
Hoch Hochkeppel <mhochk@microsoft.com>
2020-10-29 21:54:06 +0000

    WebShare: Windows Implementation (Reland)
    Completing the implementation of navigator.share() for Windows by
    hooking in the relevant components and updating the corresponding tests.
    
    Previously committed (https://crrev.com/c/2506573) but reverted due to
    the ShareServiceUnitTest.Multimedia test failing from lack of an
    IsSupportedEnvironment check. Patchset 1 is a cherry pick of this
    original change for easy comparison.
    
    Bug: 1035527
    Change-Id: I57454fcf0da211c49cb582f77261fe2afe6b1e23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2507652
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Hoch Hochkeppel <mhochk@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#822385}

--- chrome/browser/webshare/share_service_impl.cc
+++ chrome/browser/webshare/share_service_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/webshare/share_service_impl.h"
 
 #include <algorithm>
+#include <memory>
 
 #include "base/feature_list.h"
 #include "base/strings/string_piece.h"
@@ -13,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 #include "content/public/browser/web_contents.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
+
+#if defined(OS_WIN)
+#include "chrome/browser/webshare/win/share_operation.h"
+#endif
 
 // IsDangerousFilename() and IsDangerousMimeType() should be kept in sync with
 // //third_party/blink/renderer/modules/webshare/FILE_TYPES.md
@@ -166,10 +171,17 @@ void ShareServiceImpl::Share(const std::string& title,
 #if defined(OS_CHROMEOS)
   sharesheet_client_.Share(title, text, share_url, std::move(files),
                            std::move(callback));
+#elif defined(OS_WIN)
+  auto share_operation = std::make_unique<webshare::ShareOperation>(
+      title, text, share_url, std::move(files), web_contents);
+  share_operation->Run(base::BindOnce(
+      [](std::unique_ptr<webshare::ShareOperation> share_operation,
+         ShareCallback callback,
+         blink::mojom::ShareError result) { std::move(callback).Run(result); },
+      std::move(share_operation), std::move(callback)));
 #else
-  // TODO(crbug.com/1035527): Add implementation for OS_WIN
-  NOTIMPLEMENTED();
-  std::move(callback).Run(blink::mojom::ShareError::OK);
+  NOTREACHED();
+  std::move(callback).Run(blink::mojom::ShareError::INTERNAL_ERROR);
 #endif
 }
 
