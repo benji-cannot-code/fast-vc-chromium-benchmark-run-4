0242572c79d92daaf8297733edee7b2cfefc5ff4
David Bertoni <dbertoni@chromium.org>
2020-10-29 20:36:18 +0000

    [Extensions] Save context menu data for SW-based extensions
    Like event page-based extensions, Service Worker-based extensions
    must have their context menu data written to storage, so they can
    be re-created if the extension's Service Worker is restarted.
    
    Bug: 1136568
    Change-Id: Ifdebcbeb262906ee9153fff87f5304576e6fa6b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461987
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822346}

--- chrome/browser/extensions/menu_manager.h
+++ chrome/browser/extensions/menu_manager.h
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/observer_list.h"
+#include "base/observer_list_types.h"
 #include "base/scoped_observer.h"
 #include "base/strings/string16.h"
 #include "base/values.h"
@@ -292,6 +294,12 @@ class MenuManager : public ProfileObserver,
   static const char kOnContextMenus[];
   static const char kOnWebviewContextMenus[];
 
+  class TestObserver : public base::CheckedObserver {
+   public:
+    ~TestObserver() override = default;
+    virtual void DidReadFromStorage(const std::string& extension_id) = 0;
+  };
+
   MenuManager(content::BrowserContext* context, StateStore* store_);
   ~MenuManager() override;
 
@@ -380,6 +388,9 @@ class MenuManager : public ProfileObserver,
   // Removes all "incognito" "split" mode context items.
   void RemoveAllIncognitoContextItems();
 
+  void AddObserver(TestObserver* observer);
+  void RemoveObserver(TestObserver* observer);
+
  private:
   FRIEND_TEST_ALL_PREFIXES(MenuManagerTest, DeleteParent);
   FRIEND_TEST_ALL_PREFIXES(MenuManagerTest, RemoveOneByOne);
@@ -417,6 +428,8 @@ class MenuManager : public ProfileObserver,
 
   // Owned by ExtensionSystem.
   StateStore* store_;
+
+  base::ObserverList<TestObserver> observers_;
 
   DISALLOW_COPY_AND_ASSIGN(MenuManager);
 };
