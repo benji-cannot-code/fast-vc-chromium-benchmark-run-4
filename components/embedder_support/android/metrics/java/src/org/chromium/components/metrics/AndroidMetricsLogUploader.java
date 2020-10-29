1a8ded2f284f1a2ace71274465b606e41c78120b
Nate Fischer <ntfschr@chromium.org>
2020-10-29 21:46:42 +0000

    AW UMA: add system_profile validity checks in Java
    This removes the C++ CHECK()s (we saw very few reports triggering these
    CHECKs) and adds the logical equivalent in Java. The goal is to
    understand if data is corrupted during the C++ -> Java conversion or if
    the problem arises later in the pipeline.
    
    Bug: 1081925
    Test: validity check: I verified this doesn't crash in "normal" case
    Change-Id: I763d567bcf6237bb9767f30e01dbba3bbeafb709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2504590
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822378}

--- components/embedder_support/android/metrics/java/src/org/chromium/components/metrics/AndroidMetricsLogUploader.java
+++ components/embedder_support/android/metrics/java/src/org/chromium/components/metrics/AndroidMetricsLogUploader.java
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.metrics;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+
 import org.chromium.base.Consumer;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.components.metrics.ChromeUserMetricsExtensionProtos.ChromeUserMetricsExtension;
 
 /**
  * Passes UMA logs from native to a java uploader.
@@ -27,9 +30,19 @@ public class AndroidMetricsLogUploader {
     }
 
     @CalledByNative
-    public static void uploadLog(byte[] data) {
+    public static void uploadLog(byte[] data) throws InvalidProtocolBufferException {
         final Consumer<byte[]> uploader = sUploader;
         if (uploader != null) {
+            // Speculative validity checks to see why WebView UMA (and probably other embedders of
+            // this component) are missing system_profiles for a small fraction of records.
+            // TODO(https://crbug.com/1081925): remove entirely when we figure out the issue.
+            if (data.length == 0) {
+                throw new RuntimeException("UMA log is completely empty");
+            }
+            ChromeUserMetricsExtension log = ChromeUserMetricsExtension.parseFrom(data);
+            if (!log.hasSystemProfile()) {
+                throw new RuntimeException("UMA log is missing a system_profile");
+            }
             uploader.accept(data);
         }
     }
