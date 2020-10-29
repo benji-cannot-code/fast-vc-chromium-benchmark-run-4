1bc671a459f73ada2ce6b44c2bf3eda550c3c730
Emircan Uysaler <emircan@chromium.org>
2020-10-29 05:15:40 +0000

    [Fuchsia] Apply crop rect to scenic overlays
    This CL applies the crop rect calculated by the overlay processor on
    Scenic's scene graph.
    
    Bug: 1127984
    Change-Id: Ifc4a7078c1323fcdc2921e8fccb367e05dfd3824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2503857
    Auto-Submit: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822041}

--- ui/ozone/platform/scenic/scenic_surface.h
+++ ui/ozone/platform/scenic/scenic_surface.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "mojo/public/cpp/platform/platform_handle.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/size_f.h"
 #include "ui/gfx/native_pixmap_handle.h"
 #include "ui/gfx/native_widget_types.h"
@@ -56,7 +57,8 @@ class ScenicSurface : public ui::PlatformWindowSurface {
   // |id| to be added by PresentOverlayView() before.
   bool UpdateOverlayViewPosition(gfx::SysmemBufferCollectionId id,
                                  int plane_z_order,
-                                 const gfx::Rect& display_bounds);
+                                 const gfx::Rect& display_bounds,
+                                 const gfx::RectF& crop_rect);
 
   // Remove ViewHolder specified by |id|.
   bool RemoveOverlayView(gfx::SysmemBufferCollectionId id);
@@ -91,11 +93,14 @@ class ScenicSurface : public ui::PlatformWindowSurface {
   const gfx::AcceleratedWidget window_;
 
   struct OverlayViewInfo {
-    OverlayViewInfo(scenic::EntityNode node) : entity_node(std::move(node)) {}
+    OverlayViewInfo(scenic::ViewHolder holder, scenic::EntityNode node)
+        : view_holder(std::move(holder)), entity_node(std::move(node)) {}
 
+    scenic::ViewHolder view_holder;
     scenic::EntityNode entity_node;
     int plane_z_order = 0;
     gfx::Rect display_bounds;
+    gfx::RectF crop_rect;
   };
   std::unordered_map<gfx::SysmemBufferCollectionId,
                      OverlayViewInfo,
