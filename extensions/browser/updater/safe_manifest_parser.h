a91f16067c68b66789a9c26372bef90db088237a
Saurabh Nijhara <snijhara@google.com>
2020-10-29 21:51:03 +0000

    Add manifest invalid error event for force installed extensions
    We store detailed reason if the extension installation fails
    due to invalid manifest.
    We want to add manifest invalid error event to the existing events for
    the event based reporting for force installed extensions so that
    detailed reason can be reported.
    
    Bug: 1048640
    Change-Id: I5682a1b3866e903b370cd0b1b93bfc0e41bcf4e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505789
    Commit-Queue: Saurabh Nijhara <snijhara@google.com>
    Reviewed-by: Swapnil Gupta <swapnilgupta@google.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822383}

--- extensions/browser/updater/safe_manifest_parser.h
+++ extensions/browser/updater/safe_manifest_parser.h
@@ -17,9 +17,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
-// Note: enum used for UMA. Do NOT reorder or remove entries. Don't forget to
-// update enums.xml (name: ManifestInvalidError) when adding new
-// entries. Some errors are common for the entire fetched update manifest which
+// Note: enum used for UMA. Do NOT reorder or remove entries.
+// 1) Don't forget to update enums.xml (name: ManifestInvalidError) when adding
+// new entries.
+// 2) Don't forget to update device_management_backend.proto (name:
+// ExtensionInstallReportLogEvent::ManifestInvalidError) when adding new
+// entries.
+// 3) Don't forget to update ConvertManifestInvalidErrorToProto method in
+// ExtensionInstallEventLogCollector.
+// Some errors are common for the entire fetched update manifest which
 // contains manifests of different extensions, while some errors are per
 // extension basis.
 enum class ManifestInvalidError {
