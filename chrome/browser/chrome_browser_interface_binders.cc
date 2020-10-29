19d8a7191aa541382f8c4404c8ad1d52dc6501e8
Moe Ahmadi <mahmadi@chromium.org>
2020-10-29 18:54:13 +0000

    Reland "[NTP][RQ] Moves ntp_features.* from c/b/search to components/search"
    This reverts commit 6cc853a2fed59b77229fa5f34e870d23fa29b466.
    
    Reason for revert: kaleidoscope/internal/ changes will land in https://chrome-internal-review.googlesource.com/c/chrome/browser/media/kaleidoscope/internal/+/3366986
    
    PLEASE DO NOT REVERT
    
    Original change's description:
    > Revert "[NTP][RQ] Moves ntp_features.* from c/b/search to components/search"
    >
    > This reverts commit 9de8d0a88e26f2783abf0f6bc98d5232ff3a2aaf.
    >
    > Reason for revert: Causes compile error on win-chrome builders
    >
    > See e.g.:
    > https://ci.chromium.org/p/chrome/builders/ci/win-chrome/16430
    >
    > Original change's description:
    > > [NTP][RQ] Moves ntp_features.* from c/b/search to components/search
    > >
    > > Allows using NTP features in MostVisitedSites and related services in
    > > components/
    > >
    > > Fixed: 1143475
    > >
    > > Change-Id: I673bed5c25651acca3ef7c44512567599bb6b9f5
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2504639
    > > Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    > > Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    > > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > > Reviewed-by: Scott Violet <sky@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#821986}
    >
    > TBR=sky@chromium.org,mahmadi@chromium.org,ajgo@chromium.org
    >
    > Change-Id: I9abc3b7addd2a1f6e5c0d3abfc721acbb86fa3e1
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2507070
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#822088}
    
    TBR=sky@chromium.org,mahmadi@chromium.org,futhark@chromium.org,ajgo@chromium.org
    
    Change-Id: Ica9beab6f812d907a6ee5ee87bd9896ba3132ed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2507989
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822291}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -111,7 +111,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/payments/payment_credential_factory.h"
 #include "chrome/browser/payments/payment_request_factory.h"
 #include "chrome/browser/promo_browser_command/promo_browser_command.mojom.h"
-#include "chrome/browser/search/ntp_features.h"
 #include "chrome/browser/search/task_module/task_module.mojom.h"
 #include "chrome/browser/speech/speech_recognition_client_browser_interface.h"
 #include "chrome/browser/speech/speech_recognition_client_browser_interface_factory.h"
@@ -119,6 +118,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/speech/speech_recognition_service_factory.h"
 #include "chrome/browser/ui/webui/downloads/downloads.mojom.h"
 #include "chrome/browser/ui/webui/downloads/downloads_ui.h"
+#include "components/search/ntp_features.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #if !defined(OFFICIAL_BUILD)
 #include "chrome/browser/ui/webui/new_tab_page/foo/foo.mojom.h"  // nogncheck crbug.com/1125897
--- components/search/ntp_features.h
+++ components/search/ntp_features.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_SEARCH_NTP_FEATURES_H_
-#define CHROME_BROWSER_SEARCH_NTP_FEATURES_H_
+#ifndef COMPONENTS_SEARCH_NTP_FEATURES_H_
+#define COMPONENTS_SEARCH_NTP_FEATURES_H_
 
 #include "base/feature_list.h"
 
@@ -57,4 +57,4 @@ double GetLocalHistoryRepeatableQueriesFrequencyExponent();
 
 }  // namespace ntp_features
 
-#endif  // CHROME_BROWSER_SEARCH_NTP_FEATURES_H_
+#endif  // COMPONENTS_SEARCH_NTP_FEATURES_H_
