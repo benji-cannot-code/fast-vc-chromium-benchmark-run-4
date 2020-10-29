397aacc83654d62af6596807f1803f802ff91e0b
Moe Ahmadi <mahmadi@chromium.org>
2020-10-29 01:45:23 +0000

    [NTP][RQ] Moves ntp_features.* from c/b/search to components/search
    Allows using NTP features in MostVisitedSites and related services in
    components/
    
    Fixed: 1143475
    
    Change-Id: I673bed5c25651acca3ef7c44512567599bb6b9f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2504639
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821986}

--- chrome/browser/search/search_suggest/search_suggest_service_factory.cc
+++ chrome/browser/search/search_suggest/search_suggest_service_factory.cc
@@ -13,12 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/search/ntp_features.h"
 #include "chrome/browser/search/search_suggest/search_suggest_loader_impl.h"
 #include "chrome/browser/search/search_suggest/search_suggest_service.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/search/ntp_features.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/storage_partition.h"
 
--- chrome/browser/ui/toolbar/chrome_location_bar_model_delegate_unittest.cc
+++ chrome/browser/ui/toolbar/chrome_location_bar_model_delegate_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/test/scoped_feature_list.h"
-#include "chrome/browser/search/ntp_features.h"
 #include "chrome/browser/search/search.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/ui/browser.h"
@@ -17,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/search_test_utils.h"
+#include "components/search/ntp_features.h"
 #include "components/search_engines/template_url_service.h"
 
 // Concrete implementation of ChromeLocationBarModelDelegate.
