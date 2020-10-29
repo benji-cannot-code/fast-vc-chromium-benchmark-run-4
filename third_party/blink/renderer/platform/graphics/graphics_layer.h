3adc7f2cfef28d4e860957ffb9bb5b76c903f641
Xianzhu Wang <wangxianzhu@chromium.org>
2020-10-29 23:47:48 +0000

    Reland "Remove GraphicsLayer::SetNeedsDisplay()" part 1 of 2
    This relands the part of the original change not including the change
    for LayoutView::InvalidatePaintForViewAndDescendants().
    
    Original change's description:
    > Remove GraphicsLayer::SetNeedsDisplay()
    >
    > With unified PaintController, to support SetNeedsDisplay() we would
    > need to add PaintController mechanism to invalidate all display items
    > in a range for a GraphicsLayer. Removing SetNeedsDisplay() can avoid
    > that. We instead use the normal paint invalidation code path. For
    > needed invalidation, now invalidate the display items of and in the
    > GraphicsLayer. Some invalidations are not needed because we have
    > already invalidated the DisplayItemClients.
    >
    > Also rename SetContentsNeedsDisplay() to InvalidateContents(), and
    > SetNeedsDisplayInrect() to InvaldiateRaster() which is only called by
    > RasterInvalidator.
    >
    > Bug: 1132717
    > Change-Id: I535cdcc7ee1d07e23a490fdf2aa267573a54d0ba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2478026
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#818673}
    
    TBR=wangxianzhu@chromium.org, pdr@chromium.org
    
    Bug: 1132717, 11406534
    Change-Id: I7df36010f633a0ad4274eb542aacf0948b63c7ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508742
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822426}

--- third_party/blink/renderer/platform/graphics/graphics_layer.h
+++ third_party/blink/renderer/platform/graphics/graphics_layer.h
@@ -155,7 +155,7 @@ class PLATFORM_EXPORT GraphicsLayer : public DisplayItemClient,
   void SetPaintingPhase(GraphicsLayerPaintingPhase);
 
   void SetNeedsDisplay();
-  void SetContentsNeedsDisplay();
+  void InvalidateContents();
 
   // Set that the position/size of the contents (image or video).
   void SetContentsRect(const IntRect&);
@@ -271,7 +271,7 @@ class PLATFORM_EXPORT GraphicsLayer : public DisplayItemClient,
                      bool prevent_contents_opaque_changes);
 
   RasterInvalidator& EnsureRasterInvalidator();
-  void SetNeedsDisplayInRect(const IntRect&);
+  void InvalidateRaster(const IntRect&);
 
   GraphicsLayerClient& client_;
 
