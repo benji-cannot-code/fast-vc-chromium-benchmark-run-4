c8edecfd267cc04164d4d2f30e41f0659c9722ab
James Vecore <vecore@google.com>
2020-10-29 07:08:28 +0000

    [Nearby] Wait for FastPathReady before sending msgs
    We now wait for the FastPathReady message before returning the result
    for the StartReceivingMessages mojo call.
    
    Fixed: 1134212
    Change-Id: Ic1b64421544a90a7ae82b0d11f69edf64c2d1021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2506391
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#822061}

--- chrome/browser/nearby_sharing/instantmessaging/stream_parser.h
+++ chrome/browser/nearby_sharing/instantmessaging/stream_parser.h
@@ -21,7 +21,8 @@ class StreamBody;
 class StreamParser {
  public:
   explicit StreamParser(
-      base::RepeatingCallback<void(const std::string& message)> listener);
+      base::RepeatingCallback<void(const std::string& message)> listener,
+      base::OnceClosure fastpath_ready);
   ~StreamParser();
 
   void Append(base::StringPiece data);
@@ -34,6 +35,7 @@ class StreamParser {
           stream_body);
 
   base::RepeatingCallback<void(const std::string& message)> listener_;
+  base::OnceClosure fastpath_ready_callback_;
   std::string data_;
 };
 
