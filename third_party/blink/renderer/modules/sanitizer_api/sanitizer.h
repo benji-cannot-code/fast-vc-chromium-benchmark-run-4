b8296d857233d13838ea25e5846408cf64b823c6
Yifan Luo <lyf@google.com>
2020-10-29 14:05:21 +0000

    [Sanitizer API] Add Document and DocumentFragment as inputs on all methods
    Add Document and DocumentFragment as inputs on both sanitize and
    sanitizeToString methods.
    
    Bug: 1116418
    Change-Id: I06cd10dde9a5ca637140f7c4a71a302faa77abc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2487085
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822147}

--- third_party/blink/renderer/modules/sanitizer_api/sanitizer.h
+++ third_party/blink/renderer/modules/sanitizer_api/sanitizer.h
@@ -12,10 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class Document;
 class DocumentFragment;
 class ExceptionState;
 class SanitizerConfig;
 class ScriptState;
+class StringOrDocumentFragmentOrDocument;
 
 class MODULES_EXPORT Sanitizer final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
@@ -25,8 +27,12 @@ class MODULES_EXPORT Sanitizer final : public ScriptWrappable {
   explicit Sanitizer(const SanitizerConfig*);
   ~Sanitizer() override;
 
-  String sanitizeToString(ScriptState*, const String&, ExceptionState&);
-  DocumentFragment* sanitize(ScriptState*, const String&, ExceptionState&);
+  String sanitizeToString(ScriptState*,
+                          StringOrDocumentFragmentOrDocument&,
+                          ExceptionState&);
+  DocumentFragment* sanitize(ScriptState*,
+                             StringOrDocumentFragmentOrDocument&,
+                             ExceptionState&);
 
   void Trace(Visitor*) const override;
 
