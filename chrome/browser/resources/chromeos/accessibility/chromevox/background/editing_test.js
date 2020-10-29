a794ca41cffa263d42f9bf2d3de322d6dbf52268
David Tseng <dtseng@google.com>
2020-10-29 00:30:20 +0000

    Fixes expected events for text areas and content editables
    Value changes (either previously or in its new value in text field
    changed) events were never used to process output for content
    editables nor text areas.  Doing so will at least be more costly if
    not cause unwanted output.
    
    Fixed: 1143161, 1143157
    AX-Relnotes: n/a
    
    R=dmazzoni@chromium.org, nektar@chromium.org
    
    Change-Id: I3e7414b9df3853d6e86409db7bc398fb8a8f80d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2506469
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#821972}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/editing_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/editing_test.js
@@ -1524,8 +1524,7 @@ TEST_F('ChromeVoxEditingTest', 'InputEvents', function() {
   });
 });
 
-// Flaky test, see https://crbug.com/1143192
-TEST_F('ChromeVoxEditingTest', 'DISABLED_TextAreaEvents', function() {
+TEST_F('ChromeVoxEditingTest', 'TextAreaEvents', function() {
   const site = `<textarea></textarea>`;
   this.runWithLoadedTree(site, async function(root) {
     const textArea = root.find({role: RoleType.TEXT_FIELD});
@@ -1546,27 +1545,16 @@ TEST_F('ChromeVoxEditingTest', 'DISABLED_TextAreaEvents', function() {
     assertEquals(textArea, event.target);
     assertEquals('a', textArea.value);
 
-    event = await this.waitForEditableEvent();
-    assertEquals(EventType.VALUE_IN_TEXT_FIELD_CHANGED, event.type);
-    assertEquals(textArea, event.target);
-    assertEquals('a', textArea.value);
-
     this.press(KeyCode.B)();
 
     event = await this.waitForEditableEvent();
     assertEquals(EventType.DOCUMENT_SELECTION_CHANGED, event.type);
     assertEquals(textArea, event.target);
     assertEquals('ab', textArea.value);
-
-    event = await this.waitForEditableEvent();
-    assertEquals(EventType.VALUE_IN_TEXT_FIELD_CHANGED, event.type);
-    assertEquals(textArea, event.target);
-    assertEquals('ab', textArea.value);
   });
 });
 
-// Flaky test, see https://crbug.com/1143192
-TEST_F('ChromeVoxEditingTest', 'DISABLED_ContentEditableEvents', function() {
+TEST_F('ChromeVoxEditingTest', 'ContentEditableEvents', function() {
   const site = `<div role="textbox" contenteditable></div>`;
   this.runWithLoadedTree(site, async function(root) {
     const contentEditable = root.find({role: RoleType.TEXT_FIELD});
@@ -1587,20 +1575,10 @@ TEST_F('ChromeVoxEditingTest', 'DISABLED_ContentEditableEvents', function() {
     assertEquals(contentEditable, event.target);
     assertEquals('a', contentEditable.value);
 
-    event = await this.waitForEditableEvent();
-    assertEquals(EventType.VALUE_IN_TEXT_FIELD_CHANGED, event.type);
-    assertEquals(contentEditable, event.target);
-    assertEquals('a', contentEditable.value);
-
     this.press(KeyCode.B)();
 
     event = await this.waitForEditableEvent();
     assertEquals(EventType.DOCUMENT_SELECTION_CHANGED, event.type);
-    assertEquals(contentEditable, event.target);
-    assertEquals('ab', contentEditable.value);
-
-    event = await this.waitForEditableEvent();
-    assertEquals(EventType.VALUE_IN_TEXT_FIELD_CHANGED, event.type);
     assertEquals(contentEditable, event.target);
     assertEquals('ab', contentEditable.value);
   });
