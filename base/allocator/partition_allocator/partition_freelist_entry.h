12d6d06316de7806ad5c0e73aab4b6291fea6471
Keishi Hattori <keishi@chromium.org>
2020-10-29 06:37:11 +0000

    Check if all FreelistEntry pointers in the linked list are valid
    Check if all FreelistEntry pointers in the linked list are valid.
    By making sure the FreelistEntry and the next one are on the same super page.
    
    Bug: 1137799
    Change-Id: Ic37f4fb80a0a14251247fa39912f9d4cc8f90901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500652
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822058}

--- base/allocator/partition_allocator/partition_freelist_entry.h
+++ base/allocator/partition_allocator/partition_freelist_entry.h
@@ -29,6 +29,13 @@ struct PartitionFreelistEntry {
     return reinterpret_cast<EncodedPartitionFreelistEntry*>(Transform(ptr));
   }
 
+  ALWAYS_INLINE void SetNext(PartitionFreelistEntry* ptr) {
+    PA_DCHECK(!ptr ||
+              (reinterpret_cast<uintptr_t>(this) & kSuperPageBaseMask) ==
+                  (reinterpret_cast<uintptr_t>(ptr) & kSuperPageBaseMask));
+    next = Encode(ptr);
+  }
+
  private:
   friend struct EncodedPartitionFreelistEntry;
   static ALWAYS_INLINE void* Transform(void* ptr) {
--- base/allocator/partition_allocator/partition_root.cc
+++ base/allocator/partition_allocator/partition_root.cc
@@ -130,7 +130,7 @@ static size_t PartitionPurgeSlotSpan(
           head = entry;
           back = entry;
         } else {
-          back->next = internal::PartitionFreelistEntry::Encode(entry);
+          back->SetNext(entry);
           back = entry;
         }
         num_new_entries++;
@@ -141,7 +141,7 @@ static size_t PartitionPurgeSlotSpan(
 
       slot_span->SetFreelistHead(head);
       if (back)
-        back->next = internal::PartitionFreelistEntry::Encode(nullptr);
+        back->SetNext(nullptr);
 
       PA_DCHECK(num_new_entries == num_slots - slot_span->num_allocated_slots);
       // Discard the memory.
