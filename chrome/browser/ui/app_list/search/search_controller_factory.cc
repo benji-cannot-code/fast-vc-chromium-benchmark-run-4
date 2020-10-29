965aa98cf0594c5342a422699546df43df521c05
Rachel Carpenter <carpenterr@chromium.org>
2020-10-29 23:04:48 +0000

    Add a new Help App provider.
    This just provides the release notes suggestion chip when that should be
    there. Allows us to remove the hacks that are currently in place for
    that chip. Other help app entries may be added in the future.
    
    The suggestion chip is still disabled, will be re-enabled in a follow
    change.
    
    Bug: b/171519930, b/169711884
    Change-Id: Iea989b9d267ebcf00135bf91b4aba9c7b179251a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494282
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Rachel Carpenter <carpenterr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822409}

--- chrome/browser/ui/app_list/search/search_controller_factory.cc
+++ chrome/browser/ui/app_list/search/search_controller_factory.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/search/assistant_text_search_provider.h"
 #include "chrome/browser/ui/app_list/search/drive_quick_access_provider.h"
 #include "chrome/browser/ui/app_list/search/files/drive_zero_state_provider.h"
+#include "chrome/browser/ui/app_list/search/help_app_provider.h"
 #include "chrome/browser/ui/app_list/search/launcher_search/launcher_search_provider.h"
 #include "chrome/browser/ui/app_list/search/mixer.h"
 #include "chrome/browser/ui/app_list/search/omnibox_provider.h"
@@ -205,6 +206,10 @@ std::unique_ptr<SearchController> CreateSearchController(
     controller->AddProvider(os_settings_search_group_id,
                             std::make_unique<OsSettingsProvider>(profile));
   }
+
+  size_t help_app_group_id = controller->AddGroup(kGenericMaxResults);
+  controller->AddProvider(help_app_group_id,
+                          std::make_unique<HelpAppProvider>(profile));
 
   return controller;
 }
