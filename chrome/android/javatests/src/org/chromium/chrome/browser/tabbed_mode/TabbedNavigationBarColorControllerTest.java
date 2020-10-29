fc48bf38112808fa88d415d42f550dad383b8644
Sinan Sahin <sinansahin@google.com>
2020-10-29 05:03:29 +0000

    Update bottom navigation bar color for dark mode and incognito
    Bug: 1134927
    Change-Id: Icd06307a11bdfd218c77f8aaf3ab6e651bb1e978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500483
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#822038}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorControllerTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
-import android.graphics.Color;
+import android.content.res.Resources;
 import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.view.Window;
@@ -48,9 +48,11 @@ public class TabbedNavigationBarColorControllerTest {
     public void setUp() throws InterruptedException {
         mActivityTestRule.startMainActivityOnBlankPage();
         mWindow = mActivityTestRule.getActivity().getWindow();
-        mLightNavigationColor = ApiCompatibilityUtils.getColor(
-                mActivityTestRule.getActivity().getResources(), R.color.bottom_system_nav_color);
-        mDarkNavigationColor = Color.BLACK;
+        final Resources resources = mActivityTestRule.getActivity().getResources();
+        mLightNavigationColor =
+                ApiCompatibilityUtils.getColor(resources, R.color.default_bg_color_light);
+        mDarkNavigationColor =
+                ApiCompatibilityUtils.getColor(resources, R.color.default_bg_color_dark_elev_3);
     }
 
     @Test
@@ -81,8 +83,8 @@ public class TabbedNavigationBarColorControllerTest {
         ChromeTabUtils.newTabFromMenu(InstrumentationRegistry.getInstrumentation(),
                 mActivityTestRule.getActivity(), true, true);
 
-        assertEquals("Navigation bar should be black on incognito tabs.", mDarkNavigationColor,
-                mWindow.getNavigationBarColor());
+        assertEquals("Navigation bar should be dark_elev_3 on incognito tabs.",
+                mDarkNavigationColor, mWindow.getNavigationBarColor());
 
         ChromeTabUtils.newTabFromMenu(InstrumentationRegistry.getInstrumentation(),
                 mActivityTestRule.getActivity(), false, true);
