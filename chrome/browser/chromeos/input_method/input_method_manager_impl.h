c86ca5f988103c0a7235561b86c45e2b062aed86
Alexander Alekseev <alemate@chromium.org>
2020-10-30 00:49:13 +0000

    Input methods: Fix a bug in InputMethodManagerImpl::StateImpl::InitFrom().
    This Cl fixes an issue in StateImpl::InitFrom() and cleans up code.
    - input_view_url_overridden field is missing in InitFrom() method.
    - input_view_url_overridden in Dump() method.
    - Update InitFrom() and Dump() to match declaration order.
    
    Bug: 1143657
    Change-Id: Ibb2aaf67e391b50f7583e07c7051d95bcf7e8d7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2507390
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822455}

--- chrome/browser/chromeos/input_method/input_method_manager_impl.h
+++ chrome/browser/chromeos/input_method/input_method_manager_impl.h
@@ -161,7 +161,7 @@ class InputMethodManagerImpl : public InputMethodManager,
     InputMethodManagerImpl* const manager_;
 
     // True if the opt-in IME menu is activated.
-    bool menu_activated;
+    bool menu_activated = false;
 
    protected:
     friend base::RefCounted<chromeos::input_method::InputMethodManager::State>;
@@ -190,6 +190,9 @@ class InputMethodManagerImpl : public InputMethodManager,
         InputMethodManager::UIStyle::kNormal;
 
     std::unique_ptr<ImeServiceConnector> ime_service_connector_;
+
+    // Do not forget to update StateImpl::InitFrom(const StateImpl& other) and
+    // StateImpl::Dump() when adding new data members!!!
   };
 
   // Constructs an InputMethodManager instance. The client is responsible for
