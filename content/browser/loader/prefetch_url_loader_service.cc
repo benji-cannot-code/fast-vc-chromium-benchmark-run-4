fce02560f322eb4f6a8997367922ad74f6c2f4e3
Matt Menke <mmenke@chromium.org>
2020-10-30 14:07:15 +0000

    Plumb NetworkIsolationKey through SXG prefetch.
    This allows signed exchange certificate transparency checks to look
    up information learned from previously observed Expect-CT headers.
    
    Bug: 1087091
    Change-Id: Ie1675dad10a89d751797786dbb31d65b0a177dc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500562
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822629}

--- content/browser/loader/prefetch_url_loader_service.cc
+++ content/browser/loader/prefetch_url_loader_service.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "net/base/load_flags.h"
+#include "net/base/network_isolation_key.h"
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
@@ -210,7 +211,12 @@ void PrefetchURLLoaderService::CreateLoaderAndStart(
   mojo::MakeSelfOwnedReceiver(
       std::make_unique<PrefetchURLLoader>(
           routing_id, request_id, options, current_context.frame_tree_node_id,
-          resource_request, std::move(client), traffic_annotation,
+          resource_request,
+          resource_request.trusted_params
+              ? resource_request.trusted_params->isolation_info
+                    .network_isolation_key()
+              : current_context.render_frame_host->GetNetworkIsolationKey(),
+          std::move(client), traffic_annotation,
           std::move(network_loader_factory_to_use),
           base::BindRepeating(
               &PrefetchURLLoaderService::CreateURLLoaderThrottles, this,
