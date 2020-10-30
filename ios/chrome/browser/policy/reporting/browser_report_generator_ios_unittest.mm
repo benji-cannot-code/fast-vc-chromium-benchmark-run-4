17c07952e7de598ee5d76e2fb2aa152dfb5a008f
Guillaume Jenkins <gujen@google.com>
2020-10-30 15:04:56 +0000

    [iOS CBCM] Add unit test for ReportGeneratorIOS
    Adds a simple unit test case for the ReportGeneratorIOS class. The test
    generates a full report and checks that the values in the proto make
    sense.
    
    
    Bug: 1114853
    Change-Id: Ic1ad0979b2adfa41adf93c7ba9298102c23bd830
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508553
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#822645}

--- ios/chrome/browser/policy/reporting/browser_report_generator_ios_unittest.mm
+++ ios/chrome/browser/policy/reporting/browser_report_generator_ios_unittest.mm
@@ -9,10 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-namespace em = enterprise_management;
-
 #import <Foundation/Foundation.h>
 
+#include "base/files/file_path.h"
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/task_environment.h"
@@ -22,6 +21,8 @@ namespace em = enterprise_management;
 #include "ios/chrome/test/ios_chrome_scoped_testing_chrome_browser_state_manager.h"
 #include "ios/chrome/test/testing_application_context.h"
 #include "testing/platform_test.h"
+
+namespace em = enterprise_management;
 
 namespace enterprise_reporting {
 
--- ios/chrome/browser/policy/reporting/report_generator_ios_unittest.mm
+++ ios/chrome/browser/policy/reporting/report_generator_ios_unittest.mm
@@ -3,18 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/chrome/browser/policy/reporting/profile_report_generator_ios.h"
+#include "ios/chrome/browser/policy/reporting/report_generator_ios.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
-#import <Foundation/Foundation.h>
+#include <vector>
 
 #include "base/files/file_path.h"
 #include "base/run_loop.h"
-#include "base/strings/sys_string_conversions.h"
 #include "base/test/bind_test_util.h"
+#include "base/test/metrics/histogram_tester.h"
+#include "components/policy/core/common/cloud/cloud_policy_util.h"
 #include "components/policy/core/common/mock_policy_service.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/schema_registry.h"
@@ -25,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/signin/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/authentication_service_fake.h"
 #include "ios/chrome/test/ios_chrome_scoped_testing_chrome_browser_state_manager.h"
-#include "ios/chrome/test/testing_application_context.h"
-#include "ios/public/provider/chrome/browser/signin/fake_chrome_identity_service.h"
 #include "ios/web/public/test/web_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
@@ -38,13 +37,14 @@ namespace enterprise_reporting {
 namespace {
 
 const base::FilePath kProfilePath = base::FilePath("/fake/profile/default");
-const std::string kAccount = "fake_account";
 
 }  // namespace
 
-class ProfileReportGeneratorIOSTest : public PlatformTest {
+class ReportGeneratorIOSTest : public PlatformTest {
  public:
-  ProfileReportGeneratorIOSTest() : generator_(&delegate_factory_) {
+  using ReportRequest = definition::ReportRequest;
+
+  ReportGeneratorIOSTest() : generator_(&delegate_factory_) {
     TestChromeBrowserState::Builder builder;
     builder.SetPath(kProfilePath);
     builder.AddTestingFactory(
@@ -58,19 +58,11 @@ class ProfileReportGeneratorIOSTest : public PlatformTest {
     std::unique_ptr<TestChromeBrowserState> browser_state = builder.Build();
 
     InitPolicyMap();
-
-    authentication_service_ =
-        AuthenticationServiceFactory::GetForBrowserState(browser_state.get());
     scoped_browser_state_manager_ =
         std::make_unique<IOSChromeScopedTestingChromeBrowserStateManager>(
             std::make_unique<TestChromeBrowserStateManager>(
                 std::move(browser_state)));
   }
-
-  ProfileReportGeneratorIOSTest(const ProfileReportGeneratorIOSTest&) = delete;
-  ProfileReportGeneratorIOSTest& operator=(
-      const ProfileReportGeneratorIOSTest&) = delete;
-  ~ProfileReportGeneratorIOSTest() override = default;
 
   void InitMockPolicyService() {
     policy_service_ = std::make_unique<policy::MockPolicyService>();
@@ -90,80 +82,91 @@ class ProfileReportGeneratorIOSTest : public PlatformTest {
                     base::Value(true), nullptr);
   }
 
-  void SignIn() {
-    ios::FakeChromeIdentityService* identityService =
-        ios::FakeChromeIdentityService::GetInstanceFromChromeProvider();
-    identityService->AddIdentities(@[ base::SysUTF8ToNSString(kAccount) ]);
-    ChromeIdentity* identity =
-        [identityService->GetAllIdentitiesSortedForDisplay() objectAtIndex:0];
-    authentication_service_->SignIn(identity);
+  ReportGeneratorIOSTest(const ReportGeneratorIOSTest&) = delete;
+  ReportGeneratorIOSTest& operator=(const ReportGeneratorIOSTest&) = delete;
+  ~ReportGeneratorIOSTest() override = default;
+
+  std::vector<std::unique_ptr<ReportRequest>> GenerateRequests() {
+    histogram_tester_ = std::make_unique<base::HistogramTester>();
+    base::RunLoop run_loop;
+    std::vector<std::unique_ptr<ReportRequest>> reqs;
+    generator_.Generate(
+        ReportType::kFull,
+        base::BindLambdaForTesting(
+            [&run_loop, &reqs](ReportGenerator::ReportRequests requests) {
+              while (!requests.empty()) {
+                reqs.push_back(std::move(requests.front()));
+                requests.pop();
+              }
+              run_loop.Quit();
+            }));
+    run_loop.Run();
+    VerifyMetrics(reqs);
+    return reqs;
   }
 
-  std::unique_ptr<em::ChromeUserProfileInfo> GenerateReport() {
-    std::unique_ptr<em::ChromeUserProfileInfo> report =
-        generator_.MaybeGenerate(kProfilePath,
-                                 kProfilePath.BaseName().AsUTF8Unsafe(),
-                                 ReportType::kFull);
-
-    if (!report)
-      return nullptr;
-
-    EXPECT_EQ(kProfilePath.BaseName().AsUTF8Unsafe(), report->name());
-    EXPECT_EQ(kProfilePath.AsUTF8Unsafe(), report->id());
-    EXPECT_TRUE(report->is_full_report());
-
-    return report;
+  void VerifyMetrics(std::vector<std::unique_ptr<ReportRequest>>& rets) {
+    histogram_tester_->ExpectUniqueSample(
+        "Enterprise.CloudReportingRequestCount", rets.size(), 1);
+    histogram_tester_->ExpectUniqueSample(
+        "Enterprise.CloudReportingBasicRequestSize",
+        /*basic request size floor to KB*/ 0, 1);
   }
-
-  ReportingDelegateFactoryIOS delegate_factory_;
-  ProfileReportGenerator generator_;
 
  private:
   web::WebTaskEnvironment task_environment_;
 
+  ReportingDelegateFactoryIOS delegate_factory_;
+  ReportGenerator generator_;
+
+  std::unique_ptr<base::HistogramTester> histogram_tester_;
+
   std::unique_ptr<policy::MockPolicyService> policy_service_;
   policy::SchemaRegistry schema_registry_;
   policy::PolicyMap policy_map_;
+
   std::unique_ptr<IOSChromeScopedTestingChromeBrowserStateManager>
       scoped_browser_state_manager_;
-  AuthenticationService* authentication_service_;
 };
 
-TEST_F(ProfileReportGeneratorIOSTest, UnsignedInProfile) {
-  auto report = GenerateReport();
-  ASSERT_TRUE(report);
-  EXPECT_FALSE(report->has_chrome_signed_in_user());
-}
+TEST_F(ReportGeneratorIOSTest, GenerateBasicReport) {
+  auto requests = GenerateRequests();
+  EXPECT_EQ(1u, requests.size());
 
-TEST_F(ProfileReportGeneratorIOSTest, SignedInProfile) {
-  SignIn();
-  auto report = GenerateReport();
-  ASSERT_TRUE(report);
-  EXPECT_TRUE(report->has_chrome_signed_in_user());
-  EXPECT_EQ(kAccount + "@gmail.com", report->chrome_signed_in_user().email());
-  EXPECT_EQ(kAccount + "_hashID",
-            report->chrome_signed_in_user().obfudscated_gaia_id());
-}
+  // Verify the basic request
+  auto* basic_request = requests[0].get();
 
-TEST_F(ProfileReportGeneratorIOSTest, PoliciesReportedOnlyWhenEnabled) {
-  // Policies are reported by default.
-  std::unique_ptr<em::ChromeUserProfileInfo> report = GenerateReport();
-  ASSERT_TRUE(report);
-  EXPECT_EQ(2, report->chrome_policies_size());
+  EXPECT_NE(std::string(), basic_request->computer_name());
+  EXPECT_EQ(std::string(), basic_request->serial_number());
+  EXPECT_EQ(
+      policy::GetBrowserDeviceIdentifier()->SerializePartialAsString(),
+      basic_request->browser_device_identifier().SerializePartialAsString());
 
-  // Make sure policies are no longer reported when |set_policies_enabled| is
-  // set to false.
-  generator_.set_policies_enabled(false);
-  report = GenerateReport();
-  ASSERT_TRUE(report);
-  EXPECT_EQ(0, report->chrome_policies_size());
+  // Verify the OS report
+  EXPECT_TRUE(basic_request->has_os_report());
+  auto& os_report = basic_request->os_report();
+  EXPECT_NE(std::string(), os_report.name());
+  EXPECT_NE(std::string(), os_report.arch());
+  EXPECT_NE(std::string(), os_report.version());
 
-  // Make sure policies are once again being reported after setting
-  // |set_policies_enabled| back to true.
-  generator_.set_policies_enabled(true);
-  report = GenerateReport();
-  ASSERT_TRUE(report);
-  EXPECT_EQ(2, report->chrome_policies_size());
+  // Ensure there are no partial reports
+  EXPECT_EQ(0, basic_request->partial_report_types_size());
+
+  // Verify the browser report
+  EXPECT_TRUE(basic_request->has_browser_report());
+  auto& browser_report = basic_request->browser_report();
+  EXPECT_NE(std::string(), browser_report.browser_version());
+  EXPECT_TRUE(browser_report.has_channel());
+  EXPECT_NE(std::string(), browser_report.executable_path());
+
+  // Verify the profile report
+  EXPECT_EQ(1, browser_report.chrome_user_profile_infos_size());
+  auto profile_info = browser_report.chrome_user_profile_infos(0);
+  EXPECT_EQ(kProfilePath.AsUTF8Unsafe(), profile_info.id());
+  EXPECT_EQ(kProfilePath.BaseName().AsUTF8Unsafe(), profile_info.name());
+  EXPECT_TRUE(profile_info.has_is_full_report());
+  EXPECT_TRUE(profile_info.is_full_report());
+  EXPECT_EQ(2, profile_info.chrome_policies_size());
 }
 
 }  // namespace enterprise_reporting
