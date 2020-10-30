b8e5cf88be22d46b9bc0628c1c7fb26bbffbbd53
Roman Arora <romanarora@chromium.org>
2020-10-30 00:38:42 +0000

    Tab Search: WebUI source folder rename
    Tab search WebUI source migration effort. Final step involving a rename
    of the tab_search_merge directories to tab_search.
    
    Bug: 1134167
    Change-Id: I417214c807e9c921fef9f77ac58872d4af6f8e2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508215
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822451}

--- chrome/browser/ui/webui/tab_search/tab_search_ui.cc
+++ chrome/browser/ui/webui/tab_search/tab_search_ui.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 constexpr char kGeneratedPath[] =
-    "@out_folder@/gen/chrome/browser/resources/tab_search_merge/";
+    "@out_folder@/gen/chrome/browser/resources/tab_search/";
 }
 
 TabSearchUI::TabSearchUI(content::WebUI* web_ui)
