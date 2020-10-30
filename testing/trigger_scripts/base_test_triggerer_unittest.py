b8e31e92149ef72e687105053753e6c3d48f60b8
Ye Kuang <yekuang@google.com>
2020-10-30 00:18:33 +0000

    Reland "Add --use-swarming-go to the custom trigger scripts"
    This reverts commit 8181b37c43a064ca597cf7a8f8d7f500fcb080ce.
    
    Reason for revert: Create a reland CL for me to work on
    
    Original change's description:
    > Revert "Add --use-swarming-go to the custom trigger scripts"
    >
    > This reverts commit 0d7be96e7dfb056293a0a5c4ecdf9b6286924def.
    >
    > Reason for revert: Suspected of causing failures on CrOS device bots, see https://crbug.com/1143224
    >
    > Original change's description:
    > > Add --use-swarming-go to the custom trigger scripts
    > >
    > > Design doc: http://go/roll-go-swarming-to-trigger-scripts
    > >
    > > Bug: 1127205
    > > Change-Id: I8ca8e2bb381f340fc23b91339cdb56374f8eead8
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500976
    > > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > > Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    > > Commit-Queue: Ye Kuang <yekuang@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#821619}
    >
    > TBR=tikuta@chromium.org,wenbinzhang@google.com,yekuang@google.com
    >
    > Change-Id: I017fc7ef2947a7d0f2e077e6d8eafd98b7f2865f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1127205
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505509
    > Reviewed-by: Dominik Röttsches <drott@chromium.org>
    > Commit-Queue: Dominik Röttsches <drott@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#821687}
    
    TBR=drott@chromium.org,tikuta@chromium.org,wenbinzhang@google.com,yekuang@google.com
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1127205
    
    Change-Id: Ia2cdab1fd79b79641a6243e42558a5553dbf20fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505355
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Ye Kuang <yekuang@google.com>
    Cr-Commit-Position: refs/heads/master@{#822439}

--- testing/trigger_scripts/base_test_triggerer_unittest.py
+++ testing/trigger_scripts/base_test_triggerer_unittest.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Tests for base_device_trigger.py."""
 
+import argparse
 import unittest
 
 import base_test_triggerer
@@ -56,6 +57,17 @@ class UnitTest(unittest.TestCase):
     for args, ex in invalid_args:
       self.assertRaises(ex, base_test_triggerer._convert_to_go_swarming_args,
                         args)
+
+  def test_arg_parser(self):
+    # Added for https://crbug.com/1143224
+    parser = argparse.ArgumentParser()
+    base_test_triggerer.BaseTestTriggerer.add_use_swarming_go_arg(parser)
+    swarming_args = ['--server', 'x.apphost.com', '--dimension', 'os', 'Linux']
+    args, _ = parser.parse_known_args(swarming_args)
+    self.assertFalse(args.use_swarming_go)
+
+    args, _ = parser.parse_known_args(swarming_args + ['--use-swarming-go'])
+    self.assertTrue(args.use_swarming_go)
 
 if __name__ == '__main__':
     unittest.main()
