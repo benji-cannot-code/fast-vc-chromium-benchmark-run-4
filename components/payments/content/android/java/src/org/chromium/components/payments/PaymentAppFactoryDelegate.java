03d7e41f96a3636a69ad0cd3a1f0dd1d984775b3
Liquan (Max) Gu <maxlg@chromium.org>
2020-10-30 21:13:02 +0000

    [WebLayer] Move PaymentAppService into //components
    Changes:
    * Moved PaymentAppService,
    PaymentAppFactoryDelegate, PaymentAppFactoryInterface into //components.
    
    Note that this CL will break clank in compile time because the GPay
    integration depends on the changed classes in this CL. A fix[1] on
    clank will be submitted immediately after this CL.
    
    [1] https://chrome-internal-review.googlesource.com/c/clank/internal/apps/+/3358145
    
    Bug: 1098908, 1098911, 1142846
    
    Change-Id: Ie3e05b34e5ceb42d40fbc3d472c203297d5b7ab3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2493144
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822811}

--- components/payments/content/android/java/src/org/chromium/components/payments/PaymentAppFactoryDelegate.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/PaymentAppFactoryDelegate.java
@@ -3,10 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.payments;
-
-import org.chromium.components.payments.PaymentApp;
-import org.chromium.components.payments.PaymentAppFactoryParams;
+package org.chromium.components.payments;
 
 /**
  * Interface for providing information to a payment app factory and receiving the list of payment
