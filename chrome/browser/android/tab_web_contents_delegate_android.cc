74ae78c444cb978d287f8fa2bf07e2c93646bca5
ckitagawa <ckitagawa@chromium.org>
2020-10-30 00:08:48 +0000

    [Paint Preview] Promote DCHECK to if
    Also ensure document guid is never empty.
    
    Bug: 1143783
    Change-Id: I5ec5e8a9a2198c75f2d26c71f63424dbaddb9314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508266
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822435}

--- chrome/browser/android/tab_web_contents_delegate_android.cc
+++ chrome/browser/android/tab_web_contents_delegate_android.cc
@@ -537,8 +537,8 @@ void TabWebContentsDelegateAndroid::CapturePaintPreviewOfSubframe(
     content::RenderFrameHost* render_frame_host) {
   auto* client =
       paint_preview::PaintPreviewClient::FromWebContents(web_contents);
-  DCHECK(client);
-  client->CaptureSubframePaintPreview(guid, rect, render_frame_host);
+  if (client)
+    client->CaptureSubframePaintPreview(guid, rect, render_frame_host);
 }
 #endif
 
