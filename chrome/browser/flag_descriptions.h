a59ffaf45cc88a13d58188166b43d2ba9aacb250
Ramin Halavati <rhalavati@chromium.org>
2020-10-30 15:35:53 +0000

    Add flag to enable Incognito screenshots on Android.
    Enabling user triggered screenshots in Incognito mode is not possible
    now due to an external dependency (b/145247758).
    As a short term solution until the dependency is fixed, a flag is added
    to enable it for users/developers who need it.
    
    Bug: 985245
    Change-Id: Ib1b467ec4ad0f9aa216f040b19dc2670aa0184d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505211
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822662}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -763,6 +763,9 @@ extern const char kCompositorThreadedScrollbarScrollingDescription[];
 extern const char kImpulseScrollAnimationsName[];
 extern const char kImpulseScrollAnimationsDescription[];
 
+extern const char kIncognitoScreenshotName[];
+extern const char kIncognitoScreenshotDescription[];
+
 extern const char kInProductHelpDemoModeChoiceName[];
 extern const char kInProductHelpDemoModeChoiceDescription[];
 
--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -171,6 +171,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kHomepageSettingsUIConversion,
     &kHorizontalTabSwitcherAndroid,
     &kImmersiveUiMode,
+    &kIncognitoScreenshot,
     &kInlineUpdateFlow,
     &kInstantStart,
     &kKitKatSupported,
@@ -490,6 +491,9 @@ const base::Feature kHorizontalTabSwitcherAndroid{
 
 const base::Feature kImmersiveUiMode{"ImmersiveUiMode",
                                      base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kIncognitoScreenshot{"IncognitoScreenshot",
+                                         base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kInlineUpdateFlow{"InlineUpdateFlow",
                                       base::FEATURE_DISABLED_BY_DEFAULT};
