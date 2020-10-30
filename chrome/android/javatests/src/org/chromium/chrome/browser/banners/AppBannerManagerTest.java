96b97178dea0cd28fbb3bdf0caddebd064c8a326
Salvador Guerrero <salg@google.com>
2020-10-30 18:10:19 +0000

    Disable flaky AppBannerManagerTest
    This CL disables the following test:
    
    * AppBannerManagerTest.testBlockedModalNativeAppBannerResolveUserChoice
    
    Because it crashes on builder:
    
    * test-o-phone
    
    TBR=dominickn@chromium.org
    
    Bug: 1144199
    Change-Id: I31cd1c0f718f559d67587b7d9a62ac9c7622ab15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2510358
    Reviewed-by: Salvador Guerrero <salg@google.com>
    Commit-Queue: Salvador Guerrero <salg@google.com>
    Cr-Commit-Position: refs/heads/master@{#822719}

--- chrome/android/javatests/src/org/chromium/chrome/browser/banners/AppBannerManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/banners/AppBannerManagerTest.java
@@ -564,6 +564,7 @@ public class AppBannerManagerTest {
     @Test
     @MediumTest
     @Feature({"AppBanners"})
+    @DisabledTest(message = "crbug.com/1144199")
     public void testBlockedModalNativeAppBannerResolveUserChoice() throws Exception {
         triggerModalNativeAppBanner(mTabbedActivityTestRule,
                 WebappTestPage.getNonServiceWorkerUrlWithManifestAndAction(
