d86c9f35908ce5b3d8afd5a5e3b36b611fa9d178
Nick Diego Yamane <nickdiego@igalia.com>
2020-10-30 17:05:42 +0000

    exo: extended-drag: Implement shell surface dragging
    Context: Wayland Protocol needs to be extended to make it possible to
    properly support full Chrome's tab dragging experience. Further details
    in the Design document [1].
    
    This is the first of a patch series which implements extended-drag in
    Exo compositor. More specifically, this implements the core feature of
    the protocol: Making it possible to drag toplevel shell surfaces during
    DND sessions. It does it by making ExtendedDragSource an implementation
    of the ash::ToplevelWindowDragDelegate interface and plumbing it into
    ash's ToplevelWindowEventHandler, delegating to it the actual toplevel
    window drag handling. Refer to the design doc for more details.
    
    [1] https://docs.google.com/document/d/1s6OwTi_WC-pS21WLGQYI39yw2m42ZlVolUXBclljXB4/edit?usp=sharing
    
    R=oshima@chromium.org
    
    Bug: 1099418
    Test: exo_unittests --gtest_filter='ExtendedDragSourceTest.*'
    Change-Id: I43908dda9f542e5813d96a2e331af8df0a703efb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2401280
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#822682}

--- components/exo/drag_drop_operation.cc
+++ components/exo/drag_drop_operation.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "components/exo/data_offer.h"
 #include "components/exo/data_source.h"
+#include "components/exo/extended_drag_source.h"
 #include "components/exo/seat.h"
 #include "components/exo/surface.h"
 #include "components/viz/common/frame_sinks/copy_output_request.h"
@@ -116,6 +117,13 @@ DragDropOperation::DragDropOperation(DataSource* source,
     drag_drop_controller_->DragCancel();
 
   drag_drop_controller_->AddObserver(this);
+
+  if (auto* ext_drag_source = source_->get()->extended_drag_source()) {
+#if defined(OS_CHROMEOS)
+    drag_drop_controller_->set_toplevel_window_drag_delegate(ext_drag_source);
+#endif
+    ext_drag_source->AddObserver(this);
+  }
 
   if (icon)
     icon_ = std::make_unique<ScopedSurface>(icon, this);
@@ -273,11 +281,18 @@ void DragDropOperation::StartDragDropOperation() {
     source_->get()->DndFinished();
 
     // Reset |source_| so it the destructor doesn't try to cancel it.
-    source_.reset();
+    ResetSource();
   }
 
   // On failure the destructor will handle canceling the data source.
   delete this;
+}
+
+void DragDropOperation::ResetSource() {
+  DCHECK(source_);
+  if (source_->get()->extended_drag_source())
+    source_->get()->extended_drag_source()->RemoveObserver(this);
+  source_.reset();
 }
 
 void DragDropOperation::OnDragStarted() {
@@ -306,6 +321,17 @@ void DragDropOperation::OnDragActionsChanged(int actions) {
 }
 #endif
 
+void DragDropOperation::OnExtendedDragSourceDestroying(
+    ExtendedDragSource* source) {
+#if defined(OS_CHROMEOS)
+  drag_drop_controller_->set_toplevel_window_drag_delegate(nullptr);
+#endif
+  if (source_) {
+    DCHECK(source_->get()->extended_drag_source());
+    source_->get()->extended_drag_source()->RemoveObserver(this);
+  }
+}
+
 void DragDropOperation::OnSurfaceDestroying(Surface* surface) {
   if (surface == origin_->get() || surface == icon_->get()) {
     delete this;
@@ -316,7 +342,7 @@ void DragDropOperation::OnSurfaceDestroying(Surface* surface) {
 
 void DragDropOperation::OnDataSourceDestroying(DataSource* source) {
   if (source == source_->get()) {
-    source_.reset();
+    ResetSource();
     delete this;
   } else {
     NOTREACHED();
--- components/exo/drag_drop_operation.h
+++ components/exo/drag_drop_operation.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/data_device.h"
 #include "components/exo/data_offer_observer.h"
 #include "components/exo/data_source_observer.h"
+#include "components/exo/extended_drag_source.h"
 #include "components/exo/surface_observer.h"
 #include "components/exo/surface_tree_host.h"
 #include "components/exo/wm_helper.h"
@@ -45,7 +46,8 @@ class ScopedDataSource;
 class DragDropOperation : public DataSourceObserver,
                           public SurfaceTreeHost,
                           public SurfaceObserver,
-                          public aura::client::DragDropClientObserver {
+                          public aura::client::DragDropClientObserver,
+                          public ExtendedDragSource::Observer {
  public:
   // Create an operation for a drag-drop originating from a wayland app.
   static base::WeakPtr<DragDropOperation> Create(
@@ -74,6 +76,9 @@ class DragDropOperation : public DataSourceObserver,
   void OnDragActionsChanged(int actions) override;
 #endif
 
+  // ExtendedDragSource::Observer:
+  void OnExtendedDragSourceDestroying(ExtendedDragSource* source) override;
+
  private:
   // A private constructor and destructor are used to prevent anyone else from
   // attempting to manage the lifetime of a DragDropOperation.
@@ -95,6 +100,8 @@ class DragDropOperation : public DataSourceObserver,
   // This operation triggers a nested RunLoop, and should not be called
   // directly. Use ScheduleStartDragDropOperation instead.
   void StartDragDropOperation();
+
+  void ResetSource();
 
   std::unique_ptr<ScopedDataSource> source_;
   std::unique_ptr<ScopedSurface> icon_;
