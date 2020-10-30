fb57be11fbcaa72fc43b628f6f0016907db1af8a
Nazerke <Nazerke@google.com>
2020-10-30 16:41:06 +0000

    [ios] Create a TabStrip Mediator.
    This CL creates a mediator for connecting TabStrip to WebStateList and
    as for now the TabStrip shows the number of tabs open.
    
    Bug: 1136834,1128249
    Change-Id: I9082b675f88a30002f9113a363b1b1e7c2b62465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2461010
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822673}

--- ios/chrome/browser/ui/tab_strip/tab_strip_view_controller.mm
+++ ios/chrome/browser/ui/tab_strip/tab_strip_view_controller.mm
@@ -5,18 +5,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/tab_strip/tab_strip_view_controller.h"
 
+#import "base/allocator/partition_allocator/partition_alloc.h"
 #import "ios/chrome/browser/ui/tab_strip/tab_strip_cell.h"
+#import "ios/chrome/browser/ui/tab_strip/tab_strip_mediator.h"
 #import "ios/chrome/browser/ui/tab_strip/tab_strip_view_layout.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
-@implementation TabStripViewController
-
 namespace {
 static NSString* const kReuseIdentifier = @"TabView";
 }  // namespace
+
+@interface TabStripViewController ()
+// Returns the number of tabs, the value is taken from the count() of
+// the WebStateList.
+@property(nonatomic, assign) NSUInteger tabsCount;
+@end
+
+@implementation TabStripViewController
 
 - (instancetype)init {
   TabStripViewLayout* layout = [[TabStripViewLayout alloc] init];
@@ -36,6 +44,11 @@ static NSString* const kReuseIdentifier = @"TabView";
 - (NSInteger)numberOfSectionsInCollectionView:
     (UICollectionView*)collectionView {
   return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView*)collectionView
+     numberOfItemsInSection:(NSInteger)section {
+  return _tabsCount;
 }
 
 - (UICollectionViewCell*)collectionView:(UICollectionView*)collectionView
