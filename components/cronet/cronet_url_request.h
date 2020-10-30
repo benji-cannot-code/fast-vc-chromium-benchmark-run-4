f4c1ad6423a95e68fb0e7ea28415fe6eefdc0602
Yu Su <yuyansu@google.com>
2020-10-30 02:23:55 +0000

    Enable 0-RTT for Cronet idempotent request.
    Currently Cronet disables 0-RTT features for all POST requests, as they
    are not considered idempotent. But for Youtube /videoplayback request,
    we are POSTing client abr state information. We want to create an
    option in Cronet UrlRequestParams to specify enabling 0-RTT when
    creating idempotent requests.
    
    This cl only makes it available for iOS. Enabling 0-RTT for idempotent
    request  in Android interface will be implemented in the next cl,
    since it requires a different env.
    
    Change-Id: Ib2c5184b37e0ebe795c3f99dd0250a0638decaf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508299
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Yu Su <yuyansu@google.com>
    Cr-Commit-Position: refs/heads/master@{#822491}

--- components/cronet/cronet_url_request.h
+++ components/cronet/cronet_url_request.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/time/time.h"
+#include "net/base/idempotency.h"
 #include "net/base/request_priority.h"
 #include "net/url_request/url_request.h"
 #include "url/gurl.h"
@@ -150,7 +151,8 @@ class CronetURLRequest {
                    bool traffic_stats_tag_set,
                    int32_t traffic_stats_tag,
                    bool traffic_stats_uid_set,
-                   int32_t traffic_stats_uid);
+                   int32_t traffic_stats_uid,
+                   net::Idempotency idempotency);
 
   // Methods called prior to Start are never called on network thread.
 
@@ -208,7 +210,8 @@ class CronetURLRequest {
                  bool traffic_stats_tag_set,
                  int32_t traffic_stats_tag,
                  bool traffic_stats_uid_set,
-                 int32_t traffic_stats_uid);
+                 int32_t traffic_stats_uid,
+                 net::Idempotency idempotency);
 
     // Invoked on the network thread.
     ~NetworkTasks() override;
@@ -286,6 +289,8 @@ class CronetURLRequest {
     const bool traffic_stats_uid_set_;
     // UID to be applied to URLRequest.
     const int32_t traffic_stats_uid_;
+    // Idempotency of the request.
+    const net::Idempotency idempotency_;
 
     scoped_refptr<net::IOBuffer> read_buffer_;
     std::unique_ptr<net::URLRequest> url_request_;
