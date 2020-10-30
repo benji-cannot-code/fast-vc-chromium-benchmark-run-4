f86da4f4c5d6b845dabf79d8907e7931a305f107
Elvin Hu <elvinhu@chromium.org>
2020-10-30 17:15:04 +0000

    [iOS][WidgetKit] Add main file and widget files.
    Bug: 1138715, 1141522, 1141524, 1141523
    Change-Id: I3f483577c27383be01f5150b72ad1d323d097f41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2471577
    Commit-Queue: Javier Flores <javierrobles@chromium.org>
    Commit-Queue: Elvin Hu <elvinhu@chromium.org>
    Reviewed-by: Javier Flores <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822688}

---
+++ ios/chrome/widget_kit_extension/main.swift
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import Foundation
+import SwiftUI
+import WidgetKit
+
+struct Provider: TimelineProvider {
+  typealias Entry = SimpleEntry
+  func placeholder(in context: Context) -> SimpleEntry {
+    SimpleEntry(date: Date())
+  }
+
+  func getSnapshot(
+    in context: Context,
+    completion: @escaping (SimpleEntry) -> Void
+  ) {
+    let entry = SimpleEntry(date: Date())
+    completion(entry)
+  }
+
+  func getTimeline(
+    in context: Context,
+    completion: @escaping (Timeline<Entry>) -> Void
+  ) {
+    let entry = SimpleEntry(date: Date())
+    let timeline = Timeline(entries: [entry], policy: .never)
+    completion(timeline)
+  }
+}
+
+struct SimpleEntry: TimelineEntry {
+  let date: Date
+}
+
+@main
+struct ChromeWidgets: WidgetBundle {
+  @WidgetBundleBuilder
+  var body: some Widget {
+    QuickActionsWidget()
+    SearchWidget()
+    DinoGameWidget()
+  }
+}
