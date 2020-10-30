afa8a5e49b8e44a1f22c90882f68a69d7b6e6876
rbpotter <rbpotter@chromium.org>
2020-10-30 20:05:10 +0000

    Nearby Share: Generate grdps for shared resources and mojo
    Generate grdps for shared resources and mojo resources from
    c/b/r/nearby_share, instead of preprocessing and directly including
    the shared resources from OS settings.
    
    This will also simplify the process of moving the nearby shared dialog
    to use preprocessing and autogeneration.
    
    Bug: 1132403
    Change-Id: I0a4f77604aafc5ab95b89125edc08ffe7ee53152
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505967
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822783}

--- chrome/browser/ui/webui/settings/chromeos/os_settings_ui.cc
+++ chrome/browser/ui/webui/settings/chromeos/os_settings_ui.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/nearby_sharing/nearby_sharing_service_factory.h"
 #include "chrome/browser/nearby_sharing/nearby_sharing_service_impl.h"
 #include "chrome/browser/ui/webui/managed_ui_handler.h"
-#include "chrome/browser/ui/webui/nearby_share/shared_resources.h"
 #include "chrome/browser/ui/webui/settings/chromeos/device_storage_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/os_settings_manager.h"
 #include "chrome/browser/ui/webui/settings/chromeos/os_settings_manager_factory.h"
@@ -63,10 +62,6 @@ OSSettingsUI::OSSettingsUI(content::WebUI* web_ui)
   web_ui->AddMessageHandler(
       std::make_unique<chromeos::settings::StorageHandler>(profile,
                                                            html_source));
-
-  // We only need to register the mojo resources here because the rest are
-  // bundled or included in the grd.
-  RegisterNearbySharedMojoResources(html_source);
 
   int default_resource =
       base::FeatureList::IsEnabled(chromeos::features::kOsSettingsPolymer3)
