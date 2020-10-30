268e4f45796318f692a3dca47b73d3130afbc988
Fredrik Söderqvist <fs@opera.com>
2020-10-30 15:31:13 +0000

    Remove non-static SVGDocumentExtensions::rootElement()
    This is just an alias for documentElement(), which should preferably
    only be used by the SVGDocument (DOM) property.
    
    Change-Id: I2bae17b4efd30b5be6488166d511879eaa0196b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2509577
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822660}

--- third_party/blink/renderer/core/svg/graphics/svg_image.cc
+++ third_party/blink/renderer/core/svg/graphics/svg_image.cc
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/svg/animation/smil_time_container.h"
 #include "third_party/blink/renderer/core/svg/graphics/svg_image_chrome_client.h"
 #include "third_party/blink/renderer/core/svg/svg_animated_preserve_aspect_ratio.h"
-#include "third_party/blink/renderer/core/svg/svg_document_extensions.h"
 #include "third_party/blink/renderer/core/svg/svg_fe_image_element.h"
 #include "third_party/blink/renderer/core/svg/svg_image_element.h"
 #include "third_party/blink/renderer/core/svg/svg_svg_element.h"
@@ -225,8 +224,8 @@ bool SVGImage::CurrentFrameHasSingleSecurityOrigin() const {
 
   CheckLoaded();
 
-  SVGSVGElement* root_element =
-      frame->GetDocument()->AccessSVGExtensions().rootElement();
+  auto* root_element =
+      DynamicTo<SVGSVGElement>(frame->GetDocument()->documentElement());
   if (!root_element)
     return true;
 
@@ -253,7 +252,7 @@ static SVGSVGElement* SvgRootElement(Page* page) {
   if (!page)
     return nullptr;
   auto* frame = To<LocalFrame>(page->MainFrame());
-  return frame->GetDocument()->AccessSVGExtensions().rootElement();
+  return DynamicTo<SVGSVGElement>(frame->GetDocument()->documentElement());
 }
 
 LayoutSize SVGImage::ContainerSize() const {
