1ef52e5d72146b99246261adce1ce6534c125859
Anton Bikineev <bikineev@chromium.org>
2020-10-30 15:26:17 +0000

    PartitionAlloc: PCScan: Make quarantine limit bounded by heap size.
    This also makes PartitionRoot::total_size_of_*_pages atomic. Since there
    are no side effects around reading/writing them (other than the
    invariant check), using the relaxed semantics should be safe.
    
    Bug: 1129751
    Change-Id: Ibeb74484f25ba915a1b31659f3c2d6e13560f83b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2506441
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822658}

--- base/allocator/partition_allocator/pcscan.h
+++ base/allocator/partition_allocator/pcscan.h
@@ -57,7 +57,7 @@ class BASE_EXPORT PCScan final {
     // Account freed bytes. Returns true if limit was reached.
     ALWAYS_INLINE bool Account(size_t bytes);
 
-    void GrowLimitIfNeeded();
+    void GrowLimitIfNeeded(size_t heap_size);
     void ResetAndAdvanceEpoch();
 
     size_t epoch() const { return epoch_.load(std::memory_order_relaxed); }
@@ -67,8 +67,7 @@ class BASE_EXPORT PCScan final {
     size_t last_size() const { return last_size_; }
 
    private:
-    static constexpr size_t kQuarantineSizeMinLimit = 16 * 1024 * 1024;
-    static constexpr double kQuarantineSizeGrowingFactor = 1.1;
+    static constexpr size_t kQuarantineSizeMinLimit = 1 * 1024 * 1024;
 
     std::atomic<size_t> current_size_{0u};
     std::atomic<size_t> size_limit_{kQuarantineSizeMinLimit};
@@ -86,9 +85,6 @@ class BASE_EXPORT PCScan final {
 
 template <bool thread_safe>
 constexpr size_t PCScan<thread_safe>::QuarantineData::kQuarantineSizeMinLimit;
-template <bool thread_safe>
-constexpr double
-    PCScan<thread_safe>::QuarantineData::kQuarantineSizeGrowingFactor;
 
 template <bool thread_safe>
 bool PCScan<thread_safe>::QuarantineData::Account(size_t size) {
@@ -103,12 +99,13 @@ void PCScan<thread_safe>::QuarantineData::ResetAndAdvanceEpoch() {
 }
 
 template <bool thread_safe>
-void PCScan<thread_safe>::QuarantineData::GrowLimitIfNeeded() {
+void PCScan<thread_safe>::QuarantineData::GrowLimitIfNeeded(size_t heap_size) {
+  static constexpr double kQuarantineSizeFraction = 0.1;
+  // |heap_size| includes the current quarantine size, we intentionally leave
+  // some slack till hitting the limit.
   size_limit_.store(
-      std::max(
-          kQuarantineSizeMinLimit,
-          static_cast<size_t>(kQuarantineSizeGrowingFactor *
-                              current_size_.load(std::memory_order_relaxed))),
+      std::max(kQuarantineSizeMinLimit,
+               static_cast<size_t>(kQuarantineSizeFraction * heap_size)),
       std::memory_order_relaxed);
 }
 
