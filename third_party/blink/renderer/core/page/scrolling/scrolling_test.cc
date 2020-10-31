ac97235a2dc459527678830271baef1820906d91
Shimi Zhang <ctzsm@chromium.org>
2020-10-31 00:40:35 +0000

    Cursor Control: Add bit to scroll gestures
    We reinterpret scroll gestures conditionally for cursor control purpose.
    
    This is done by add a new bit |kInternalPanXScrolls| to TouchAction,
    This bit is suppressed by TouchAction::kPanX, i.e. we only set this bit
    when TouchAction::kPanLeft or TouchAction::kPanRight is set. By default,
    if an element has TouchAction::kPanX, it will be set. Therefore,
    TouchAction::kAuto has this bit as well. When |kInternalPanXScrolls|
    is set, we don't do cursor control.
    
    When an element is flagged as editable, we remove |kInternalPanXScrolls|
    bit for that element and its descendants. Therefore, if there is an
    editable element, because it is not equal to TouchAction::kAuto, the
    browser side will get a signal and set |has_touch_event_handler_| as
    true in TouchActionFilter.
    
    In painting stage, we also calculate horizontal scrollable information
    for an element, if an element or its ancestor is horizontal scrollable,
    we put |kInternalPanXScrolls| bit back to the corresponding
    TouchActionRegion to disable cursor control.
    
    When a touch start event happens, it will be dispatched to the renderer
    since we know there is a touch event handler, the browser will get the
    compositor touch action from the compositor hit test. The event will be
    further dispatched to the renderer main thread, we do another hit test,
    we also need to acquire the horizontal scrollable information for the
    hit test result element, so we could decide to put
    |kInternalPanXScrolls| back to the main thread touch action or not.
    Main thread touch action will also be sent to the browser.
    
    When a GestureScrollBegin generates, we use the compositor touch action
    and the main thread touch action to filter it, because compositor touch
    action is permissive, if we don't have |kInternalPanXScrolls| bit on
    the touch action, we delay the event until we get the main thread
    touch action.
    
    If |kInternalPanXScrolls| is not set and |kPanX| is allowed and this is
    a horizontal scroll, we set the GestureScrollBegin's |cursor_control|
    flag to true, therefore, the whole scroll gesture sequence will be used
    as cursor control.
    
    In this way, we fulfilled multiple design goals:
    1) cursor control can be disabled by not set touch-action: pan-x
    2) cursor control can be disabled by register a non-passive event
    handler of the touchstart event and calling event.preventDefault()
    inside.
    3) cursor control is disabled when the element is horizontally
    scrollable.
    4) cursor control is not interleaved with scroll.
    
    Bug: 1126778
    Change-Id: I7a1c58f60740ff87dbee460524de66c67878dc64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2435324
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822908}

--- third_party/blink/renderer/core/page/scrolling/scrolling_test.cc
+++ third_party/blink/renderer/core/page/scrolling/scrolling_test.cc
@@ -74,6 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/testing/paint_test_configurations.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/url_test_helpers.h"
+#include "ui/base/ui_base_features.h"
 
 namespace blink {
 
@@ -508,7 +509,8 @@ TEST_P(ScrollingTest, touchAction) {
 
   const auto* cc_layer = ScrollingContentsLayerByDOMElementId("scrollable");
   cc::Region region = cc_layer->touch_action_region().GetRegionForTouchAction(
-      TouchAction::kPanX | TouchAction::kPanDown);
+      TouchAction::kPanX | TouchAction::kPanDown |
+      TouchAction::kInternalPanXScrolls);
   EXPECT_EQ(region.GetRegionComplexity(), 1);
   EXPECT_EQ(region.bounds(), gfx::Rect(0, 0, 1000, 1000));
 }
@@ -521,12 +523,14 @@ TEST_P(ScrollingTest, touchActionRegions) {
   const auto* cc_layer = ScrollingContentsLayerByDOMElementId("scrollable");
 
   cc::Region region = cc_layer->touch_action_region().GetRegionForTouchAction(
-      TouchAction::kPanDown | TouchAction::kPanX);
+      TouchAction::kPanDown | TouchAction::kPanX |
+      TouchAction::kInternalPanXScrolls);
   EXPECT_EQ(region.GetRegionComplexity(), 1);
   EXPECT_EQ(region.bounds(), gfx::Rect(0, 0, 100, 100));
 
   region = cc_layer->touch_action_region().GetRegionForTouchAction(
-      TouchAction::kPanDown | TouchAction::kPanRight);
+      TouchAction::kPanDown | TouchAction::kPanRight |
+      TouchAction::kInternalPanXScrolls);
   EXPECT_EQ(region.GetRegionComplexity(), 1);
   EXPECT_EQ(region.bounds(), gfx::Rect(0, 0, 50, 50));
 
@@ -568,7 +572,7 @@ TEST_P(ScrollingTest, touchActionNesting) {
   const auto* cc_layer = ScrollingContentsLayerByDOMElementId("scrollable");
 
   cc::Region region = cc_layer->touch_action_region().GetRegionForTouchAction(
-      TouchAction::kPanX);
+      TouchAction::kPanX | TouchAction::kInternalPanXScrolls);
   EXPECT_EQ(region.GetRegionComplexity(), 2);
   EXPECT_EQ(region.bounds(), gfx::Rect(5, 5, 150, 100));
 }
@@ -605,7 +609,7 @@ TEST_P(ScrollingTest, nestedTouchActionInvalidation) {
   const auto* cc_layer = ScrollingContentsLayerByDOMElementId("scrollable");
 
   cc::Region region = cc_layer->touch_action_region().GetRegionForTouchAction(
-      TouchAction::kPanX);
+      TouchAction::kPanX | TouchAction::kInternalPanXScrolls);
   EXPECT_EQ(region.GetRegionComplexity(), 2);
   EXPECT_EQ(region.bounds(), gfx::Rect(5, 5, 150, 100));
 
@@ -613,7 +617,7 @@ TEST_P(ScrollingTest, nestedTouchActionInvalidation) {
   scrollable->setAttribute("style", "touch-action: none", ASSERT_NO_EXCEPTION);
   ForceFullCompositingUpdate();
   region = cc_layer->touch_action_region().GetRegionForTouchAction(
-      TouchAction::kPanX);
+      TouchAction::kPanX | TouchAction::kInternalPanXScrolls);
   EXPECT_TRUE(region.IsEmpty());
 }
 
@@ -642,7 +646,7 @@ TEST_P(ScrollingTest, nestedTouchActionChangesUnion) {
   const auto* cc_layer = MainFrameScrollingContentsLayer();
 
   cc::Region region = cc_layer->touch_action_region().GetRegionForTouchAction(
-      TouchAction::kPanX);
+      TouchAction::kPanX | TouchAction::kInternalPanXScrolls);
   EXPECT_EQ(region.bounds(), gfx::Rect(8, 8, 150, 50));
   region = cc_layer->touch_action_region().GetRegionForTouchAction(
       TouchAction::kNone);
@@ -656,11 +660,53 @@ TEST_P(ScrollingTest, nestedTouchActionChangesUnion) {
       TouchAction::kPanY);
   EXPECT_EQ(region.bounds(), gfx::Rect(8, 8, 100, 100));
   region = cc_layer->touch_action_region().GetRegionForTouchAction(
-      TouchAction::kPanX);
+      TouchAction::kPanX | TouchAction::kInternalPanXScrolls);
   EXPECT_TRUE(region.IsEmpty());
   region = cc_layer->touch_action_region().GetRegionForTouchAction(
       TouchAction::kNone);
   EXPECT_EQ(region.bounds(), gfx::Rect(8, 8, 150, 50));
+}
+
+TEST_P(ScrollingTest, touchActionEditableElement) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitWithFeatures({::features::kSwipeToMoveCursor}, {});
+  ASSERT_TRUE(base::FeatureList::IsEnabled(::features::kSwipeToMoveCursor));
+  // Long text that will overflow in y-direction.
+  LoadHTML(R"HTML(
+    <style>
+      #touchaction {
+        touch-action: manipulation;
+        width: 100px;
+        height: 50px;
+        overflow: scroll;
+      }
+    </style>
+    <div id="touchaction" contenteditable>
+      <div id="child"></div>
+    </div>
+  )HTML");
+  ForceFullCompositingUpdate();
+  const auto* cc_layer = MainFrameScrollingContentsLayer();
+  cc::Region region = cc_layer->touch_action_region().GetRegionForTouchAction(
+      TouchAction::kManipulation);
+  EXPECT_EQ(region.bounds(), gfx::Rect(8, 8, 100, 50));
+  region = cc_layer->touch_action_region().GetRegionForTouchAction(
+      TouchAction::kNone);
+  EXPECT_TRUE(region.IsEmpty());
+
+  // Make touchaction scrollable by making child overflow.
+  Element* child = GetFrame()->GetDocument()->getElementById("child");
+  child->setAttribute("style", "width: 1000px; height: 100px;",
+                      ASSERT_NO_EXCEPTION);
+  ForceFullCompositingUpdate();
+
+  cc_layer = ScrollingContentsLayerByDOMElementId("touchaction");
+  region = cc_layer->touch_action_region().GetRegionForTouchAction(
+      TouchAction::kManipulation | TouchAction::kInternalPanXScrolls);
+  EXPECT_EQ(region.bounds(), gfx::Rect(0, 0, 1000, 100));
+  region = cc_layer->touch_action_region().GetRegionForTouchAction(
+      TouchAction::kNone);
+  EXPECT_TRUE(region.IsEmpty());
 }
 
 // Box shadow is not hit testable and should not be included in touch action.
