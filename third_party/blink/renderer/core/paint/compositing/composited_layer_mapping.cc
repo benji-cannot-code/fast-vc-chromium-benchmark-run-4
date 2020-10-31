9b89222b24cd4f0b18755341c0195fb723c25275
Xianzhu Wang <wangxianzhu@chromium.org>
2020-10-31 17:32:47 +0000

    Reland "Remove GraphicsLayer::SetNeedsDisplay()" part 2 of 2
    This reland the remaining part of the original change after
    crrev.com/c/2508742.
    
    Original change's description:
    > Remove GraphicsLayer::SetNeedsDisplay()
    >
    > With unified PaintController, to support SetNeedsDisplay() we would
    > need to add PaintController mechanism to invalidate all display items
    > in a range for a GraphicsLayer. Removing SetNeedsDisplay() can avoid
    > that. We instead use the normal paint invalidation code path. For
    > needed invalidation, now invalidate the display items of and in the
    > GraphicsLayer. Some invalidations are not needed because we have
    > already invalidated the DisplayItemClients.
    >
    > Also rename SetContentsNeedsDisplay() to InvalidateContents(), and
    > SetNeedsDisplayInrect() to InvaldiateRaster() which is only called by
    > RasterInvalidator.
    >
    > Bug: 1132717
    > Change-Id: I535cdcc7ee1d07e23a490fdf2aa267573a54d0ba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2478026
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#818673}
    
    TBR=wangxianzhu@chromium.org, pdr@chromium.org
    
    Bug: 1132717, 1140634
    Change-Id: If073c6c9f885fec3f9e303ed9274384ec4373189
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508364
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822975}

--- third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.cc
+++ third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.cc
@@ -1478,13 +1478,6 @@ GraphicsLayer* CompositedLayerMapping::SquashingLayer(
   return NonScrollingSquashingLayer();
 }
 
-void CompositedLayerMapping::SetAllLayersNeedDisplay() {
-  ApplyToGraphicsLayers(this, [](GraphicsLayer* graphics_layer) {
-    if (graphics_layer->PaintsContentOrHitTest())
-      graphics_layer->SetNeedsDisplay();
-  });
-}
-
 void CompositedLayerMapping::SetNeedsCheckRasterInvalidation() {
   ApplyToGraphicsLayers(this, [](GraphicsLayer* graphics_layer) {
     if (graphics_layer->DrawsContent())
--- third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.h
+++ third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.h
@@ -133,8 +133,6 @@ class CORE_EXPORT CompositedLayerMapping final : public GraphicsLayerClient {
     return non_scrolling_squashing_layer_offset_from_layout_object_;
   }
 
-  void SetAllLayersNeedDisplay();
-
   // Let all DrawsContent GraphicsLayers check raster invalidations after
   // a no-change paint.
   void SetNeedsCheckRasterInvalidation();
