468bc69a8617581d0ee12d7a83870149ee8e1e9e
Jimmy Gong <jimmyxgong@chromium.org>
2020-10-31 00:40:21 +0000

    Phonehub: Disable onboarding UI if feature has already been enabled
    Onboarding UI is now hidden when either the user dismisses the UI or
    if the user has completed the onboarding flow.
    
    Bug: 1106937
    Fixed: 1141634
    Test: chromeos_components_unittests
    Change-Id: I0e34bed1977f6b72cf45bab1f2fd289deaa630da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508953
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822907}

--- chromeos/components/phonehub/onboarding_ui_tracker_impl.cc
+++ chromeos/components/phonehub/onboarding_ui_tracker_impl.cc
@@ -14,8 +14,7 @@ namespace chromeos {
 namespace phonehub {
 
 void OnboardingUiTrackerImpl::RegisterPrefs(PrefRegistrySimple* registry) {
-  registry->RegisterBooleanPref(prefs::kHasDismissedUiAfterCompletingOnboarding,
-                                false);
+  registry->RegisterBooleanPref(prefs::kHideOnboardingUi, false);
 }
 
 OnboardingUiTrackerImpl::OnboardingUiTrackerImpl(
@@ -29,11 +28,14 @@ OnboardingUiTrackerImpl::OnboardingUiTrackerImpl(
       show_multidevice_setup_dialog_callback_(
           std::move(show_multidevice_setup_dialog_callback)) {
   feature_status_provider_->AddObserver(this);
+  multidevice_setup_client_->AddObserver(this);
+
   should_show_onboarding_ui_ = ComputeShouldShowOnboardingUi();
 }
 
 OnboardingUiTrackerImpl::~OnboardingUiTrackerImpl() {
   feature_status_provider_->RemoveObserver(this);
+  multidevice_setup_client_->RemoveObserver(this);
 }
 
 bool OnboardingUiTrackerImpl::ShouldShowOnboardingUi() const {
@@ -41,8 +43,7 @@ bool OnboardingUiTrackerImpl::ShouldShowOnboardingUi() const {
 }
 
 void OnboardingUiTrackerImpl::DismissSetupUi() {
-  pref_service_->SetBoolean(prefs::kHasDismissedUiAfterCompletingOnboarding,
-                            true);
+  pref_service_->SetBoolean(prefs::kHideOnboardingUi, true);
   UpdateShouldShowOnboardingUi();
 }
 
@@ -71,12 +72,27 @@ void OnboardingUiTrackerImpl::OnFeatureStatusChanged() {
   UpdateShouldShowOnboardingUi();
 }
 
+void OnboardingUiTrackerImpl::OnFeatureStatesChanged(
+    const multidevice_setup::MultiDeviceSetupClient::FeatureStatesMap&
+        feature_states_map) {
+  const multidevice_setup::mojom::FeatureState phonehub_state =
+      feature_states_map.find(multidevice_setup::mojom::Feature::kPhoneHub)
+          ->second;
+  // User has gone through the onboarding process, prevent the UI from
+  // displaying again.
+  if (phonehub_state ==
+      multidevice_setup::mojom::FeatureState::kEnabledByUser) {
+    pref_service_->SetBoolean(prefs::kHideOnboardingUi, true);
+    UpdateShouldShowOnboardingUi();
+  }
+}
+
 bool OnboardingUiTrackerImpl::ComputeShouldShowOnboardingUi() {
   FeatureStatus status = feature_status_provider_->GetStatus();
+
   if (status == FeatureStatus::kEligiblePhoneButNotSetUp ||
       status == FeatureStatus::kDisabled) {
-    return !pref_service_->GetBoolean(
-        prefs::kHasDismissedUiAfterCompletingOnboarding);
+    return !pref_service_->GetBoolean(prefs::kHideOnboardingUi);
   }
   return false;
 }
