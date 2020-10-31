ceb9ffa9fd67ee3e33de6c24be8eba645cc3b82e
Yuki Shiino <yukishiino@chromium.org>
2020-10-31 16:50:37 +0000

    v8binding: Remove unused code after the new bindings generator
    The migration to the new IDL interface implementation has been
    done, and this patch removes temporary adapter code.
    
    Bug: 839389, 1050474
    Change-Id: Id3093d26b20c60c68d213188de5d5de182def14c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2511150
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#822969}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -2137,7 +2137,7 @@ void WebGLRenderingContextBase::bufferSubData(
     const FlexibleArrayBufferView& data) {
   if (isContextLost())
     return;
-  DCHECK(data);
+  DCHECK(!data.IsNull());
   BufferSubDataImpl(target, offset, data.ByteLengthAsSizeT(),
                     data.BaseAddressMaybeOnStack());
 }
