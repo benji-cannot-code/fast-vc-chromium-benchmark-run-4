f16527ad9172a0ac63a31ab4ec77e92376f6f86c
Antonio Gomes <tonikitoo@igalia.com>
2020-11-01 15:22:13 +0000

    [ozone/wayland] Wrap zaura_shell object with a WaylandZAuraRemote class
    This is a follow up to https://crrev.com/c/2508132, where
    the capability of firing tablet state changes was added to
    EXO, via zaura_shell extension.
    
    It wraps the current ui::WaylandConnection::zaura_shell_ instance
    so that adding capabilities to listen to compositor event is easier
    (see crrev.com/c/2491517 in relation chain).
    No behavior change expected.
    
    BUG=1113900
    
    R=jamescook@chromium.org, rjkroege@chromium.org
    
    Change-Id: I2f6e83d53ae05b5674dd2dfc9d7fe146cfd8e18c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2510932
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823019}

---
+++ ui/ozone/platform/wayland/host/wayland_zaura_shell.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_PLATFORM_WAYLAND_HOST_WAYLAND_ZAURA_SHELL_H_
+#define UI_OZONE_PLATFORM_WAYLAND_HOST_WAYLAND_ZAURA_SHELL_H_
+
+#include "ui/ozone/platform/wayland/common/wayland_object.h"
+
+namespace ui {
+
+class WaylandConnection;
+
+// Wraps the zaura_shell object.
+class WaylandZAuraShell {
+ public:
+  WaylandZAuraShell(zaura_shell* aura_shell, WaylandConnection* connection);
+  WaylandZAuraShell(const WaylandZAuraShell&) = delete;
+  WaylandZAuraShell& operator=(const WaylandZAuraShell&) = delete;
+  ~WaylandZAuraShell();
+
+  zaura_shell* wl_object() const { return obj_.get(); }
+
+ private:
+  wl::Object<zaura_shell> obj_;
+  WaylandConnection* const connection_;
+};
+
+}  // namespace ui
+
+#endif  // UI_OZONE_PLATFORM_WAYLAND_HOST_WAYLAND_ZAURA_SHELL_H_
