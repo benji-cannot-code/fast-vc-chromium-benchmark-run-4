977465615b22a122e6564520b4be727fa0394d73
Patrick Brosset <patrick.brosset@microsoft.com>
2020-11-02 10:00:46 +0000

    DevTools: Deleting 3 web_tests and re-enabling one
    Some recent refactoring happened in devtools-frontend that forced me to
    disable a few web_tests temporarily.
    
    Now that this has landed, I am able to fix and re-enable those tests.
    
    I did so for one of the tests:
    /http/tests/devtools/sources/color-swatch-position.js
    
    However for the 3 others, because we already had e2e/unit test infra
    in devtools-frontend to test those things, I instead created equivalent
    tests there, and am now deleting these web_tests.
    
    This is the frontend CL where this is done:
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2506954/
    
    Bug: 1137438
    Change-Id: Ibf4007e3762d01c9e36d3b00bf03a2594625a99b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2507510
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Patrick Brosset <patrick.brosset@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#823086}

--- third_party/blink/web_tests/http/tests/devtools/sources/color-swatch-position.js
+++ third_party/blink/web_tests/http/tests/devtools/sources/color-swatch-position.js
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     TestRunner.runTestSuite([
       function testEditSpectrum(next) {
-        var swatch = sourceFrame.textEditor._codeMirrorElement.querySelector('span[is=color-swatch]');
+        var swatch = sourceFrame.textEditor._codeMirrorElement.querySelector('devtools-color-swatch');
         swatch.shadowRoot.querySelector('.color-swatch-inner').click();
         cssPlugin._spectrum._innerSetColor(
             Common.Color.parse('#008000').hsva(), '', undefined /* colorName */, Common.Color.Format.HEX,
