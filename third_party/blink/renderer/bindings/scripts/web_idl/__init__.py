06b8502a085d25e6e9eafb676b68605e7221c909
Yuki Shiino <yukishiino@chromium.org>
2020-11-02 14:25:26 +0000

    IDL compiler: Implement a new Union class
    Renames the existing old Union class to BackwardCompatibleUnion
    and introduces a new Union class as NewUnion.
    
    NewUnion's APIs are described at API documentations in union.py.
    
    Some (not comprehensive) issues of the old unions are:
    - (double or DOMString) and (DOMString or double) are different
      types.
    - (double or DOMString) can be generated in bindings/modules/v8/.
    - (A or B?) and (A or B) are indistinguishable.
    - (A or B or sequence<C>) and (A or sequence<(B or C)>) will be
      AOrBOrCSequence.
    
    Bug: 839389
    Change-Id: Ifa4bebba158c845089a5dd2b75688eda0d79feab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2497342
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823140}

--- third_party/blink/renderer/bindings/scripts/web_idl/__init__.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/__init__.py
@@ -70,6 +70,7 @@ from .operation import Operation
 from .operation import OperationGroup
 from .runtime_enabled_features import RuntimeEnabledFeatures
 from .typedef import Typedef
+from .union import NewUnion
 from .union import Union
 
 
