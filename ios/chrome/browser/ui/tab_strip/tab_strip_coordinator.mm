4779408f08aadd283f1a5f0dfae567a96da4e5c8
Nazerke <Nazerke@google.com>
2020-11-02 14:04:34 +0000

    [ios] WebStateList Observer for updating the tabs number.
    This CL adds a webstatelist observer for updating tabStrip
    collectionview about the number of tabs open.
    
    Bug:1128249,1136834
    
    Change-Id: I5ebb1d47f2007dd27431aab9f2cff12a7a53f98c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2509578
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823130}

--- ios/chrome/browser/ui/tab_strip/tab_strip_coordinator.mm
+++ ios/chrome/browser/ui/tab_strip/tab_strip_coordinator.mm
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stop {
+  [self.mediator disconnect];
   self.mediator = nil;
   self.tabStripViewController = nil;
 }
