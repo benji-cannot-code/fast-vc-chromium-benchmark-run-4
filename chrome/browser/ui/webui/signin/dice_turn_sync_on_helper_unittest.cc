bd67a99fba276a4da43662af68928e42c6654262
Jan Krcal <jkrcal@chromium.org>
2020-11-02 16:36:44 +0000

    Reland "[Dice sync delegate] Split sync confirmation and sync disabled"
    This is a reland of d42167ebafe3dc799306e95549a2be4acf9e2f5a
    
    Compared to the original CL, this CL minimizes the changes in the test
    that was failing on Win7.
    
    Original change's description:
    > [Dice sync delegate] Split sync confirmation and sync disabled
    >
    > This CL splits the ShowSyncConfirmation() function of the dice turn on
    > sync helper delegate into one that shows the normal sync confirmation
    > and one that shows a error message that sync is disabled by the admin.
    >
    > Since both the dialogs are implemented by the same webUI page, there
    > is not much behavioral change. The only change is for the delegate
    > for the signed-in profile creation flow which opens a new browser
    > window and shows the dialog in this window instead of showing it in
    > the creation flow window.
    >
    > In follow-up CLs, the variant of the UI will be passed as a param
    > to the WebUI page replacing the logic to decide which variant to show.
    >
    > Bug: 1126913
    > Change-Id: Ibac9a339c4fb071f960f15864482c818247f5719
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2487100
    > Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#820222}
    
    Bug: 1126913
    Change-Id: I741140df31b8ca91fe5eb1a05a643fc0d2401f58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495069
    Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823175}

--- chrome/browser/ui/webui/signin/dice_turn_sync_on_helper_unittest.cc
+++ chrome/browser/ui/webui/signin/dice_turn_sync_on_helper_unittest.cc
@@ -88,6 +88,9 @@ class TestDiceTurnSyncOnHelperDelegate : public DiceTurnSyncOnHelper::Delegate {
   void ShowSyncConfirmation(
       base::OnceCallback<void(LoginUIService::SyncConfirmationUIClosedResult)>
           callback) override;
+  void ShowSyncDisabledConfirmation(
+      base::OnceCallback<void(LoginUIService::SyncConfirmationUIClosedResult)>
+          callback) override {}
   void ShowSyncSettings() override;
   void SwitchToProfile(Profile* new_profile) override;
 
