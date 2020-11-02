310d03d1412a5c99365a9fd4953a6b1f949bc9de
Patrick Noland <pnoland@chromium.org>
2020-11-02 18:11:33 +0000

    [ToolbarMVC] Use LocationBarCoordinator in SearchActivity
    This will allow it to continue to function as logic migrates out of
    LocationBarLayout. SearchActivity-specific logic remains in
    SearchActivityLocationBarLayout for now.
    
    Change-Id: I1a32a6fbfd584590b982ecb4c8639664e752819d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495785
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: bttk <bttk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823210}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
@@ -55,11 +55,12 @@ public final class LocationBarCoordinator implements LocationBar, FakeboxDelegat
     /**
      * Creates {@link LocationBarCoordinator} and its subcoordinator: {@link
      * LocationBarCoordinatorPhone} or {@link LocationBarCoordinatorTablet}, depending on the type
-     * of {@code locationBarLayout}.
+     * of {@code locationBarLayout}; no sub-coordinator is created for other LocationBarLayout
+     * subclasses.
      * {@code LocationBarCoordinator} owns the subcoordinator. Destroying the former destroys the
      * latter.
      *
-     * @param locationBarLayout Inflated {@link LocationBarPhone} or {@link LocationBarTablet}.
+     * @param locationBarLayout Inflated {@link LocationBarLayout}.
      *         {@code LocationBarCoordinator} takes ownership and will destroy this object.
      * @param profileObservableSupplier The supplier of the active profile.
      * @param locationBarDataProvider {@link LocationBarDataProvider} to be used for accessing
@@ -74,8 +75,6 @@ public final class LocationBarCoordinator implements LocationBar, FakeboxDelegat
      * @param incognitoStateProvider An {@link IncognitoStateProvider} to access the current
      *         incognito state.
      * @param activityLifecycleDispatcher Allows observation of the activity state.
-     * @throws IllegalArgumentException if the view is neither {@link LocationBarPhone} nor {@link
-     *         LocationBarTablet}.
      */
     public LocationBarCoordinator(View locationBarLayout,
             ObservableSupplier<Profile> profileObservableSupplier,
@@ -93,10 +92,6 @@ public final class LocationBarCoordinator implements LocationBar, FakeboxDelegat
         } else if (locationBarLayout instanceof LocationBarTablet) {
             mSubCoordinator =
                     new LocationBarCoordinatorTablet((LocationBarTablet) locationBarLayout);
-        } else {
-            assert false : "Expected LocationBarPhone or LocationBarTablet, got "
-                           + locationBarLayout.getClass();
-            throw new IllegalArgumentException(locationBarLayout.getClass().toString());
         }
 
         mLocationbarDataProvider = locationBarDataProvider;
