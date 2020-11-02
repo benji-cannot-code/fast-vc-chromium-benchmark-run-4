c354d766e7d57649d49f44d3ed92965c8315ef87
Matt Menke <mmenke@chromium.org>
2020-11-02 14:28:10 +0000

    Add NetworkIsolationKey to NetworkContext::QueueSignedExchangeReport.
    Also update SignedExchangeReporter to pass in the correct
    NetworkIsolationKey.
    
    PrefetchedSignedExchangeCache still needs to be updated to pass in
    the correct NetworkIsolationKey.
    
    Change-Id: Ibd5496248ae2eb55a9d74b4a74e83da61aab13f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505747
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823141}

--- content/browser/web_package/signed_exchange_reporter.h
+++ content/browser/web_package/signed_exchange_reporter.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/web_package/signed_exchange_error.h"
 #include "content/common/content_export.h"
 #include "net/base/ip_address.h"
+#include "net/base/network_isolation_key.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/url_response_head.mojom-forward.h"
 #include "url/gurl.h"
@@ -28,6 +29,7 @@ class CONTENT_EXPORT SignedExchangeReporter {
       const GURL& outer_url,
       const std::string& referrer,
       const network::mojom::URLResponseHead& response,
+      const net::NetworkIsolationKey& network_isolation_key,
       int frame_tree_node_id);
 
   ~SignedExchangeReporter();
@@ -46,10 +48,12 @@ class CONTENT_EXPORT SignedExchangeReporter {
   SignedExchangeReporter(const GURL& outer_url,
                          const std::string& referrer,
                          const network::mojom::URLResponseHead& response,
+                         const net::NetworkIsolationKey& network_isolation_key,
                          int frame_tree_node_id);
 
   network::mojom::SignedExchangeReportPtr report_;
   const base::TimeTicks request_start_;
+  const net::NetworkIsolationKey network_isolation_key_;
   const int frame_tree_node_id_;
   net::IPAddress cert_server_ip_address_;
 
