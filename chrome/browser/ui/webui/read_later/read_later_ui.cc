7995ab21bd16a3a52a631c39eb3fe60e17087c0f
rbpotter <rbpotter@chromium.org>
2020-11-02 23:59:00 +0000

    Read Later: Use preprocess_grit and generate_grd
    Bug: 1132403
    Change-Id: I2d4fcc058d236a7c4e43350965e36b10c820738f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2510352
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823357}

--- chrome/browser/ui/webui/read_later/read_later_ui.cc
+++ chrome/browser/ui/webui/read_later/read_later_ui.cc
@@ -22,9 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/webui/web_ui_util.h"
 
 namespace {
-constexpr char kGeneratedPath[] =
-    "@out_folder@/gen/chrome/browser/resources/read_later/";
-
 void AddLocalizedString(content::WebUIDataSource* source,
                         const std::string& message,
                         int id) {
@@ -38,8 +35,6 @@ ReadLaterUI::ReadLaterUI(content::WebUI* web_ui)
     : ui::MojoBubbleWebUIController(web_ui) {
   content::WebUIDataSource* source =
       content::WebUIDataSource::Create(chrome::kChromeUIReadLaterHost);
-  source->AddResourcePath("read_later.mojom-lite.js",
-                          IDR_READ_LATER_MOJO_LITE_JS);
   static constexpr webui::LocalizedString kLocalizedStrings[] = {
       {"readHeader", IDS_READ_LATER_MENU_READ_HEADER},
       {"title", IDS_READ_LATER_TITLE},
@@ -53,7 +48,7 @@ ReadLaterUI::ReadLaterUI(content::WebUI* web_ui)
 
   webui::SetupWebUIDataSource(
       source, base::make_span(kReadLaterResources, kReadLaterResourcesSize),
-      kGeneratedPath, IDR_READ_LATER_HTML);
+      /*generated_path=*/std::string(), IDR_READ_LATER_READ_LATER_HTML);
   content::WebUIDataSource::Add(web_ui->GetWebContents()->GetBrowserContext(),
                                 source);
 }
