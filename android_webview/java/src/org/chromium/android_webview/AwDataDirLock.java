2342f366592d285f0062b7de3e8b94f21edf2bb7
Torne (Richard Coles) <torne@google.com>
2020-11-02 21:43:37 +0000

    webview: remove obsolete data dir lock metrics.
    The retry behaviour of the WebView data directory lock is now
    well-understood and we're not going to change the code any further;
    remove the retry count metric.
    
    Also remove the metric logging when init has already been started
    previously - this has shown that a very small number of apps are
    actually hitting this case and only in a very tiny percentage of cases.
    Just remove the metric and don't introduce any new check for this
    condition; these apps will either work or not and it's not worth us
    worrying about it.
    
    Fixed: 1142488
    Change-Id: I90b85e999ca74ebecdc66c2ac7e11aa4b0bf1d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2498110
    Auto-Submit: Richard Coles <torne@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823305}

--- android_webview/java/src/org/chromium/android_webview/AwDataDirLock.java
+++ android_webview/java/src/org/chromium/android_webview/AwDataDirLock.java
@@ -16,7 +16,6 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.PathUtils;
 import org.chromium.base.StrictModeContext;
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.ScopedSysTraceEvent;
 
 import java.io.File;
@@ -87,7 +86,6 @@ abstract class AwDataDirLock {
                 if (sExclusiveFileLock != null) {
                     // We got the lock; write out info for debugging.
                     writeCurrentProcessInfo(sLockFile);
-                    recordLockAttempts(attempts);
                     return;
                 }
 
@@ -109,8 +107,6 @@ abstract class AwDataDirLock {
                 throw new RuntimeException(error);
             } else {
                 Log.w(TAG, error);
-                // Record an attempt count of 0 to indicate that we proceeded without the lock.
-                recordLockAttempts(0);
             }
         }
     }
@@ -125,14 +121,6 @@ abstract class AwDataDirLock {
             // Don't crash just because something failed here, as it's only for debugging.
             Log.w(TAG, "Failed to write info to lock file", e);
         }
-    }
-
-    private static void recordLockAttempts(int attempts) {
-        // We log values from [0, LOCK_RETRIES]. Histogram samples are expected to be [0, max).
-        // 0 just goes to the underflow bucket, so min=1 and max=LOCK_RETRIES+1.
-        // To get bucket width 1, buckets must be max-min+2
-        RecordHistogram.recordLinearCountHistogram("Android.WebView.Startup.DataDirLockAttempts",
-                attempts, 1, LOCK_RETRIES + 1, LOCK_RETRIES + 2);
     }
 
     private static String getLockFailureReason(final RandomAccessFile file) {
