abbfe78f2451a9c06529a4e9d69097a833624fce
Angus L. M. McLean IV <angusmclean@chromium.org>
2020-11-02 21:29:00 +0000

    Move volume/mic/brightness slider when auto-hidden shelf is shown
    This prevents the slider views from awkwardly overlapping with the
    shelf.
    
    Bug: 1136564
    Change-Id: I83551c6c5c2efb244d245a1055238ba6841d96c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2499641
    Commit-Queue: Angus McLean <angusmclean@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823297}

--- ash/system/unified/unified_slider_bubble_controller.h
+++ ash/system/unified/unified_slider_bubble_controller.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_UNIFIED_UNIFIED_SLIDER_BUBBLE_CONTROLLER_H_
 
 #include "ash/ash_export.h"
+#include "ash/shelf/shelf_observer.h"
 #include "ash/system/audio/unified_volume_slider_controller.h"
 #include "ash/system/tray/tray_bubble_view.h"
 #include "ash/system/unified/unified_system_tray_model.h"
@@ -24,7 +25,8 @@ class ASH_EXPORT UnifiedSliderBubbleController
     : public TrayBubbleView::Delegate,
       public chromeos::CrasAudioHandler::AudioObserver,
       public UnifiedSystemTrayModel::Observer,
-      public UnifiedVolumeSliderController::Delegate {
+      public UnifiedVolumeSliderController::Delegate,
+      public ShelfObserver {
  public:
   enum SliderType {
     SLIDER_TYPE_VOLUME = 0,
@@ -59,6 +61,9 @@ class ASH_EXPORT UnifiedSliderBubbleController
 
   // UnifiedVolumeSliderController::Delegate:
   void OnAudioSettingsButtonClicked() override;
+
+  // ShelfObserver:
+  void OnShelfWorkAreaInsetsChanged() override;
 
  private:
   friend class UnifiedSystemTrayTest;
