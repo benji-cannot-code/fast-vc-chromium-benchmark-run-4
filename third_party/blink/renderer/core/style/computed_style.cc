9abf5d357bd5f00243260983eb4175bb5a23799b
Koji Ishii <kojii@chromium.org>
2020-11-02 01:22:41 +0000

    Disable hyphenation when content language is not set
    The spec changed to:
    >  The UA must therefore only automatically hyphenate text for
    which the content language is known and for which it has an
    appropriate hyphenation resource.
    https://drafts.csswg.org/css-text-3/#valdef-hyphens-auto
    
    This patch matches the updated spec.
    
    Bug: 676270
    Change-Id: I79bd9c260bde8e744f1261af38b665e0ebc72500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505692
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823045}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -1434,9 +1434,11 @@ AtomicString ComputedStyle::LocaleForLineBreakIterator() const {
 }
 
 Hyphenation* ComputedStyle::GetHyphenation() const {
-  return GetHyphens() == Hyphens::kAuto
-             ? GetFontDescription().LocaleOrDefault().GetHyphenation()
-             : nullptr;
+  if (GetHyphens() != Hyphens::kAuto)
+    return nullptr;
+  if (const LayoutLocale* locale = GetFontDescription().Locale())
+    return locale->GetHyphenation();
+  return nullptr;
 }
 
 const AtomicString& ComputedStyle::HyphenString() const {
