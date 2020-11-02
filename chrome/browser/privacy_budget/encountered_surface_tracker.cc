12ea06add1cc76d7c9d4304aa352eb799593d4fc
Asanka Herath <asanka@chromium.org>
2020-11-02 21:37:42 +0000

    [privacy_budget] Some renames for readability.
    We are using the word "sample" in too many ways. Try to move to less
    ambiguous terminology.
    
    Bug: 973801
    Change-Id: I4d1feeeef22e756e82952e4b53a2a6615dac8715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2510240
    Auto-Submit: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823303}

--- chrome/browser/privacy_budget/encountered_surface_tracker.cc
+++ chrome/browser/privacy_budget/encountered_surface_tracker.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/privacy_budget/sampled_surface_tracker.h"
+#include "chrome/browser/privacy_budget/encountered_surface_tracker.h"
 
 #include <vector>
 
@@ -29,20 +29,21 @@ uint64_t hash(uint64_t x) {
 
 }  // namespace
 
-const unsigned SampledSurfaceTracker::kMaxTrackedSurfaces;
+const unsigned EncounteredSurfaceTracker::kMaxTrackedSurfaces;
 
-SampledSurfaceTracker::SampledSurfaceTracker() {
+EncounteredSurfaceTracker::EncounteredSurfaceTracker() {
   Reset();
 }
 
-SampledSurfaceTracker::~SampledSurfaceTracker() = default;
+EncounteredSurfaceTracker::~EncounteredSurfaceTracker() = default;
 
-void SampledSurfaceTracker::Reset() {
+void EncounteredSurfaceTracker::Reset() {
   seed_ = base::RandUint64();
   surfaces_.clear();
 }
 
-bool SampledSurfaceTracker::ShouldRecord(uint64_t source_id, uint64_t surface) {
+bool EncounteredSurfaceTracker::IsNewEncounter(uint64_t source_id,
+                                               uint64_t surface) {
   if (blink::IdentifiableSurface::FromMetricHash(surface).GetType() ==
       blink::IdentifiableSurface::Type::kReservedInternal)
     return false;
--- chrome/browser/privacy_budget/identifiability_study_state.h
+++ chrome/browser/privacy_budget/identifiability_study_state.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/strings/string_piece_forward.h"
 #include "base/thread_annotations.h"
+#include "chrome/browser/privacy_budget/encountered_surface_tracker.h"
 #include "chrome/browser/privacy_budget/privacy_budget_prefs.h"
-#include "chrome/browser/privacy_budget/sampled_surface_tracker.h"
 #include "chrome/common/privacy_budget/privacy_budget_settings_provider.h"
 #include "components/prefs/pref_service.h"
 #include "third_party/blink/public/common/privacy_budget/identifiability_study_settings.h"
@@ -65,7 +65,7 @@ class IdentifiabilityStudyState {
   // Returns true if metrics collection is enabled for `surface`.
   //
   // Calling this method may alter the state of the study settings.
-  bool ShouldSampleSurface(blink::IdentifiableSurface surface);
+  bool ShouldRecordSurface(blink::IdentifiableSurface surface);
 
   // Should be called from unit-tests if multiple IdentifiabilityStudyState
   // instances are to be constructed.
@@ -73,8 +73,8 @@ class IdentifiabilityStudyState {
 
   // Returns true if tracking metrics should be recorded for this
   // source_id/surface combination.
-  bool ShouldRecordSurface(uint64_t source_id,
-                           blink::IdentifiableSurface surface);
+  bool ShouldReportEncounteredSurface(uint64_t source_id,
+                                      blink::IdentifiableSurface surface);
 
   // Clear the sampled surface state from the state tracker. Ideally this would
   // be called each time a UKM report generated.
@@ -230,7 +230,7 @@ class IdentifiabilityStudyState {
   //
   //   * tracked_surfaces_.GetType() ∩ settings_.blocked_types() = Ø.
   //
-  SampledSurfaceTracker tracked_surfaces_;
+  EncounteredSurfaceTracker tracked_surfaces_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 };
