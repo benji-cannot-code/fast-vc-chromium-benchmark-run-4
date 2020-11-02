d2b30f7b68b663d0b1badc98012626f962002c4f
Michael Checo <michaelcheco@google.com>
2020-11-02 19:28:58 +0000

    Scanning: Add more settings expandable section
    Screenshots:
     Expanded: http://shortn/_ozPsFShPLu
     Closed: http://shortn/_810zjzDQgC
    
    Bug: 1059779
    Test: browser_tests --gtest_filter=ScanningAppBrowserTest.All
    Change-Id: Ia9ad13ae2e8baf09e4c0a42fb9bc2b2a9942e297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2506392
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823250}

--- chromeos/components/scanning/resources/scanning_app.js
+++ chromeos/components/scanning/resources/scanning_app.js
@@ -4,9 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/cr_elements/icons.m.js';
 import 'chrome://resources/mojo/mojo/public/mojom/base/big_buffer.mojom-lite.js';
 import 'chrome://resources/mojo/mojo/public/mojom/base/string16.mojom-lite.js';
 import 'chrome://resources/mojo/mojo/public/mojom/base/unguessable_token.mojom-lite.js';
+import 'chrome://resources/polymer/v3_0/iron-collapse/iron-collapse.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import './file_path.mojom-lite.js';
 import './color_mode_select.js';
 import './file_type_select.js';
@@ -15,6 +18,7 @@ import './resolution_select.js';
 import './scan_preview.js';
 import './scan_to_select.js';
 import './scanner_select.js';
+import './scanning_fonts_css.js';
 import './scanning_shared_css.js';
 import './source_select.js';
 
@@ -267,5 +271,19 @@ Polymer({
 
     this.settingsDisabled_ = false;
     this.scanButtonDisabled_ = false;
+  },
+
+  /** @private */
+  toggleClicked_() {
+    this.$.collapse.toggle();
+  },
+
+  /**
+   * @param {boolean} opened Whether the section is expanded or not.
+   * @return {string} Icon name.
+   * @private
+   */
+  getArrowIcon_(opened) {
+    return opened ? 'cr:expand-less' : 'cr:expand-more';
   },
 });
