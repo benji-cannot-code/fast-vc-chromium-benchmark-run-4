4ea80f7dbfc60d116065cf3569118c2e09576ed5
Jeffrey Kardatzke <jkardatzke@google.com>
2020-11-03 16:45:43 +0000

    Initial H265 decoder implementation
    This adds the H265Decoder (modeled after the H264Decoder) with only
    handling of SPS/SPS.
    
    BUG=chromium:1141237,b:153111783
    TEST=media_unittests
    
    Change-Id: Ia2dc95675b9c200626e96c500ed91fb4ba27b6dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2515040
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/master@{#823569}

---
+++ media/gpu/h265_dpb.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_GPU_H265_DPB_H_
+#define MEDIA_GPU_H265_DPB_H_
+
+namespace media {
+
+// DPB - Decoded Picture Buffer.
+// Stores decoded pictures that will be used for future display
+// and/or reference.
+class H265DPB {
+ public:
+  H265DPB();
+
+  H265DPB(const H265DPB&) = delete;
+  H265DPB& operator=(const H265DPB&) = delete;
+
+  ~H265DPB();
+
+  void set_max_num_pics(size_t max_num_pics);
+  size_t max_num_pics() const { return max_num_pics_; }
+
+ private:
+  size_t max_num_pics_;
+};
+
+}  // namespace media
+
+#endif  // MEDIA_GPU_H265_DPB_H_
