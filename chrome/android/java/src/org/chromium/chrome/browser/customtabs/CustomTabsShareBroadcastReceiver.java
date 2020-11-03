205301a45fa35bdf43ddadf1a5875ccc00bb3d25
Sophey Dong <sophey@chromium.org>
2020-11-03 20:45:06 +0000

    [ShareCCT] Add logging for shares in CCT.
    Bug: 1135311
    Change-Id: I5fa5d931a9eba2ff82bed1bda09dcb4723f35c88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508198
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823699}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsShareBroadcastReceiver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsShareBroadcastReceiver.java
@@ -9,6 +9,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import org.chromium.base.metrics.RecordUserAction;
+
 /**
  * Receives shared content broadcast from Chrome Custom Tabs and shows a share sheet to share the
  * url.
@@ -16,6 +18,7 @@ import android.content.Intent;
 public final class CustomTabsShareBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        RecordUserAction.record("MobileTopToolbarShareButton");
         String url = intent.getDataString();
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
