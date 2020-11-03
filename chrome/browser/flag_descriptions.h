135f2ff847cba025f169265242815c5248726344
Leo Zhang <googleo@chromium.org>
2020-11-03 01:06:59 +0000

    [IME] Add mojo decoder flag.
    Let user enable IME mojo decoder from chrome://flags.
    
    BUG=b/171846787
    
    Change-Id: Ie101b36d3b8bf64695039992b2aac108be6c69cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2509150
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Leo Zhang <googleo@chromium.org>
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823387}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2470,6 +2470,9 @@ extern const char kImeInputLogicMozcDescription[];
 extern const char kImeMozcProtoName[];
 extern const char kImeMozcProtoDescription[];
 
+extern const char kImeServiceDecoderName[];
+extern const char kImeServiceDecoderDescription[];
+
 extern const char kImeServiceSandboxName[];
 extern const char kImeServiceSandboxDescription[];
 
