5d83c8dfea7df381de1e9cfac83fb3b8f8fa0f9f
Oksana Zhuravlova <oksamyt@chromium.org>
2020-11-03 22:48:48 +0000

    [aw][tracing] Set up bg tracing on startup
    This change adds code to enable background tracing in WebView under the
    following conditions:
    - system tracing is configured
    - the config specifies system tracing mode.
    
    Bug: b/170513316
    Change-Id: I5a38255a0098d3d91bbdd33facb23df5c9c8f2c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508312
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Michael Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823759}

--- android_webview/browser/aw_browser_main_parts.cc
+++ android_webview/browser/aw_browser_main_parts.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/aw_web_ui_controller_factory.h"
 #include "android_webview/browser/metrics/aw_metrics_service_client.h"
 #include "android_webview/browser/network_service/aw_network_change_notifier_factory.h"
+#include "android_webview/browser/tracing/background_tracing_field_trial.h"
 #include "android_webview/common/aw_descriptors.h"
 #include "android_webview/common/aw_paths.h"
 #include "android_webview/common/aw_resource.h"
@@ -142,6 +143,8 @@ void AwBrowserMainParts::PostCreateThreads() {
   heap_profiling::Mode mode = heap_profiling::GetModeForStartup();
   if (mode != heap_profiling::Mode::kNone)
     heap_profiling::Supervisor::GetInstance()->Start(base::NullCallback());
+
+  SetupBackgroundTracingFieldTrial();
 }
 
 }  // namespace android_webview
---
+++ android_webview/browser/tracing/background_tracing_field_trial.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "android_webview/browser/tracing/background_tracing_field_trial.h"
+
+#include "base/bind_helpers.h"
+#include "content/public/browser/background_tracing_config.h"
+#include "content/public/browser/background_tracing_manager.h"
+#include "services/tracing/public/cpp/tracing_features.h"
+
+namespace android_webview {
+
+void SetupBackgroundTracingFieldTrial() {
+  std::unique_ptr<content::BackgroundTracingConfig> config =
+      content::BackgroundTracingManager::GetInstance()
+          ->GetBackgroundTracingConfig();
+
+  if (config &&
+      config->tracing_mode() == content::BackgroundTracingConfig::SYSTEM &&
+      tracing::ShouldSetupSystemTracing()) {
+    // Only enable background tracing for system tracing if the system producer
+    // is enabled.
+    content::BackgroundTracingManager::GetInstance()->SetActiveScenario(
+        std::move(config),
+        base::BindRepeating(
+            base::DoNothing::Repeatedly<std::unique_ptr<std::string>,
+                                        content::BackgroundTracingManager::
+                                            FinishedProcessingCallback>()),
+        content::BackgroundTracingManager::ANONYMIZE_DATA);
+  }
+}
+
+}  // namespace android_webview
