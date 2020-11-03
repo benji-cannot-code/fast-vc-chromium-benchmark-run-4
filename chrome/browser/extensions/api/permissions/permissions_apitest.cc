c3e0537b061cba626643081414558db13a8918ad
Istiaque Ahmed <lazyboy@chromium.org>
2020-11-03 23:55:03 +0000

    [Extensions SW] Expand two PermissionsApiTest for service workers
    This CL runs PermissionsApiTest.OptionalPermission and
    PermissionsApiTest.OptionalPermissionsAutoConfirm tests with service
    worker based extensions.
    
    Bug: 986416, 1093066
    Test: Test only change
    Change-Id: I9135147ca731f9d93ec723c989fe872275e7d3dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2516392
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823794}

--- chrome/browser/extensions/api/permissions/permissions_apitest.cc
+++ chrome/browser/extensions/api/permissions/permissions_apitest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/common/permissions/permission_set.h"
+#include "extensions/common/scoped_worker_based_extensions_channel.h"
 #include "extensions/common/switches.h"
 #include "net/dns/mock_host_resolver.h"
 
@@ -29,6 +30,8 @@ static void AddPattern(URLPatternSet* extent, const std::string& pattern) {
 }
 
 }  // namespace
+
+using ContextType = ExtensionBrowserTest::ContextType;
 
 class ExperimentalApiTest : public ExtensionApiTest {
  public:
@@ -44,6 +47,23 @@ class PermissionsApiTest : public ExtensionApiTest {
     ExtensionApiTest::SetUpOnMainThread();
     host_resolver()->AddRule("*", "127.0.0.1");
   }
+};
+
+class PermissionsApiTestWithContextType
+    : public PermissionsApiTest,
+      public testing::WithParamInterface<ContextType> {
+ public:
+  PermissionsApiTestWithContextType() {
+    // Service Workers are currently only available on certain channels, so set
+    // the channel for those tests.
+    if (GetParam() == ContextType::kServiceWorker) {
+      current_channel_ = std::make_unique<ScopedWorkerBasedExtensionsChannel>();
+    }
+  }
+
+ private:
+  std::unique_ptr<extensions::ScopedWorkerBasedExtensionsChannel>
+      current_channel_;
 };
 
 IN_PROC_BROWSER_TEST_F(PermissionsApiTest, PermissionsFail) {
@@ -97,7 +117,8 @@ IN_PROC_BROWSER_TEST_F(PermissionsApiTest, MAYBE_AlwaysAllowed) {
 }
 
 // Tests that the optional permissions API works correctly.
-IN_PROC_BROWSER_TEST_F(PermissionsApiTest, OptionalPermissionsGranted) {
+IN_PROC_BROWSER_TEST_P(PermissionsApiTestWithContextType,
+                       OptionalPermissionsGranted) {
   // Mark all the tested APIs as granted to bypass the confirmation UI.
   APIPermissionSet apis;
   apis.insert(APIPermission::kBookmark);
@@ -116,7 +137,8 @@ IN_PROC_BROWSER_TEST_F(PermissionsApiTest, OptionalPermissionsGranted) {
 }
 
 // Tests that the optional permissions API works correctly.
-IN_PROC_BROWSER_TEST_F(PermissionsApiTest, OptionalPermissionsAutoConfirm) {
+IN_PROC_BROWSER_TEST_P(PermissionsApiTestWithContextType,
+                       OptionalPermissionsAutoConfirm) {
   // Rather than setting the granted permissions, set the UI autoconfirm flag
   // and run the same tests.
   PermissionsRequestFunction::SetAutoConfirmForTests(true);
@@ -243,5 +265,12 @@ IN_PROC_BROWSER_TEST_F(PermissionsApiTest, OptionalPermissionsUpdatesBindings) {
   ASSERT_TRUE(RunExtensionTest("permissions/optional_updates_bindings"))
       << message_;
 }
+
+INSTANTIATE_TEST_SUITE_P(PersistentBackground,
+                         PermissionsApiTestWithContextType,
+                         testing::Values(ContextType::kPersistentBackground));
+INSTANTIATE_TEST_SUITE_P(ServiceWorker,
+                         PermissionsApiTestWithContextType,
+                         testing::Values(ContextType::kServiceWorker));
 
 }  // namespace extensions
