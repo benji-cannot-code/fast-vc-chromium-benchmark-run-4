f800205f7d8ce910f3d73a2a544a46731bf35f10
Xinghui Lu <xinghuilu@chromium.org>
2020-11-03 03:21:45 +0000

    Add browser tests for real time URL check.
    Bug: 1144940
    Change-Id: Icbc8084ca209b121388f5910a52b0735681ad1ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2515207
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823429}

--- components/safe_browsing/core/verdict_cache_manager.h
+++ components/safe_browsing/core/verdict_cache_manager.h
@@ -105,6 +105,7 @@ class VerdictCacheManager : public history::HistoryServiceObserver,
   void StopCleanUpTimerForTesting();
 
  private:
+  friend class SafeBrowsingBlockingPageRealTimeUrlCheckTest;
   FRIEND_TEST_ALL_PREFIXES(VerdictCacheManagerTest, TestCleanUpExpiredVerdict);
   FRIEND_TEST_ALL_PREFIXES(VerdictCacheManagerTest,
                            TestCleanUpExpiredVerdictWithInvalidEntry);
