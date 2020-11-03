1a65c4a2315003c8c66e0daa8269e38d6a26b477
Omar Morsi <omorsi@google.com>
2020-11-03 20:13:52 +0000

    KeyPermissions: Add ARC key permissions manager delegate
    This CL introduces ARC key permissions manager delegate which observes
    changes that affect ARC usage allowance of corporate keys residing on a
    specific token. If an ArcKpmDelegate observes a change in the state of
    ARC usage allowance, it notifies all observers. ArcKpmDelegates are
    utilized by key permissions managers to keep key permissions updated in
    chaps.
    
    Bug: 1113115, 1127284
    Change-Id: Ib7636c83f313904c7d5a23f91baca6bf43907344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2479706
    Commit-Queue: Omar Morsi <omorsi@google.com>
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823681}

--- chrome/browser/chromeos/platform_keys/key_permissions/mock_key_permissions_manager.h
+++ chrome/browser/chromeos/platform_keys/key_permissions/mock_key_permissions_manager.h
@@ -36,6 +36,8 @@ class MockKeyPermissionsManager : public KeyPermissionsManager {
                KeyUsage key_usage,
                const std::string& public_key_spki_der),
               (override));
+
+  MOCK_METHOD(bool, AreCorporateKeysAllowedForArcUsage, (), (const, override));
 };
 
 }  // namespace platform_keys
