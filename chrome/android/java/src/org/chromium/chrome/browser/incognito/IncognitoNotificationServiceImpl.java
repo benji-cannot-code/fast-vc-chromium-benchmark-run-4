e593e72c955e51cd9dc6b0a80cf9b54903304da8
Rohit Agarwal <roagarwal@chromium.org>
2020-11-03 09:50:07 +0000

    Remove the deprecated Profile#hasOffTheRecordProfile method.
    This CL removes the deprecated Profile#hasOffTheRecordProfile method
    which invoked Profile#hasPrimaryOTRProfile internally.
    
    The remaining callers of hasOffTheRecordProfile are updated to directly
    call hasPrimaryOTRProfile instead.
    
    The code requiring the check of primary OTR profile would rely on
    invoking the method Profile#hasPrimaryOTRProfile explicitly instead.
    
    Bug: 1060940
    Change-Id: I0955789b0cdb7c3ea86eccb2840d35c0e10d79d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2516222
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823486}

--- chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationServiceImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationServiceImpl.java
@@ -64,7 +64,7 @@ public class IncognitoNotificationServiceImpl extends IncognitoNotificationServi
             IncognitoNotificationManager.dismissIncognitoNotification();
 
             if (BrowserStartupController.getInstance().isFullBrowserStarted()) {
-                if (Profile.getLastUsedRegularProfile().hasOffTheRecordProfile()) {
+                if (Profile.getLastUsedRegularProfile().hasPrimaryOTRProfile()) {
                     Profile.getLastUsedRegularProfile()
                             .getPrimaryOTRProfile()
                             .destroyWhenAppropriate();
--- chrome/browser/profiles/android/java/src/org/chromium/chrome/browser/cookies/CookiesFetcher.java
+++ chrome/browser/profiles/android/java/src/org/chromium/chrome/browser/cookies/CookiesFetcher.java
@@ -140,7 +140,7 @@ public class CookiesFetcher {
      */
     public static boolean deleteCookiesIfNecessary() {
         try {
-            if (Profile.getLastUsedRegularProfile().hasOffTheRecordProfile()) return false;
+            if (Profile.getLastUsedRegularProfile().hasPrimaryOTRProfile()) return false;
             scheduleDeleteCookiesFile();
         } catch (RuntimeException e) {
             e.printStackTrace();
