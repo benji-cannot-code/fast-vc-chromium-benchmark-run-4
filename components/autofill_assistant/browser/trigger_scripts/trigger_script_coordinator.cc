63dbd4432200edf12397364bf813ff0628eb97b8
Clemens Arbesser <arbesser@google.com>
2020-11-03 13:41:01 +0000

    [Autofill Assistant] Implement TriggerScripts and basic file structure.
    This CL adds the basic file structure for trigger scripts. Most classes
    are only stubs, but the TriggerScript condition checking is already in
    place. Subsequent CLs will replace the stubs to make this fully
    functional.
    
    Bug: b/171776026
    Change-Id: Ic6b687242048c3f6ebf665d7611ff115a9a91ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2513595
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Marian Fechete <marianfe@google.com>
    Cr-Commit-Position: refs/heads/master@{#823523}

---
+++ components/autofill_assistant/browser/trigger_scripts/trigger_script_coordinator.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill_assistant/browser/trigger_scripts/trigger_script_coordinator.h"
+
+namespace autofill_assistant {
+
+// TODO(b/171776026): implement this stub.
+TriggerScriptCoordinator::TriggerScriptCoordinator(
+    Client* client,
+    std::unique_ptr<WebController> web_controller,
+    std::unique_ptr<ServiceRequestSender> request_sender,
+    const GURL& get_trigger_scripts_server)
+    : content::WebContentsObserver(client->GetWebContents()) {}
+
+TriggerScriptCoordinator::~TriggerScriptCoordinator() = default;
+
+void TriggerScriptCoordinator::Start(const GURL& url) {}
+
+void TriggerScriptCoordinator::PerformTriggerScriptAction(
+    TriggerScriptProto::TriggerScriptAction action) {}
+
+void TriggerScriptCoordinator::AddObserver(Observer* observer) {
+  observers_.AddObserver(observer);
+}
+
+void TriggerScriptCoordinator::RemoveObserver(const Observer* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+void TriggerScriptCoordinator::OnVisibilityChanged(
+    content::Visibility visibility) {}
+
+}  // namespace autofill_assistant
