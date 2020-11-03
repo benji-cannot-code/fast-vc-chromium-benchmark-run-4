99dffeed37e19035b7eba6fe0443b2fb08397716
rbpotter <rbpotter@chromium.org>
2020-11-03 08:06:13 +0000

    Tab Search: Autogenerate grd file
    Bug: 1132403
    Change-Id: I76cd8b57066a9f4e22a1fff2f2ff7885d351e4c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2509994
    Auto-Submit: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823468}

--- chrome/browser/ui/webui/tab_search/tab_search_ui.cc
+++ chrome/browser/ui/webui/tab_search/tab_search_ui.cc
@@ -23,11 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/webui/web_ui_util.h"
 #include "ui/views/style/platform_style.h"
 
-namespace {
-constexpr char kGeneratedPath[] =
-    "@out_folder@/gen/chrome/browser/resources/tab_search/";
-}
-
 TabSearchUI::TabSearchUI(content::WebUI* web_ui)
     : ui::MojoBubbleWebUIController(web_ui,
                                     true /* Needed for webui browser tests */),
@@ -69,12 +64,9 @@ TabSearchUI::TabSearchUI(content::WebUI* web_ui)
                     features::kTabSearchTitleToHostnameWeightRatio.Get());
 
   source->AddLocalizedString("close", IDS_CLOSE);
-  source->AddResourcePath("tab_search.mojom-lite.js",
-                          IDR_TAB_SEARCH_MOJO_LITE_JS);
-  source->AddResourcePath("fuse.js", IDR_FUSE_JS);
   webui::SetupWebUIDataSource(
       source, base::make_span(kTabSearchResources, kTabSearchResourcesSize),
-      kGeneratedPath, IDR_TAB_SEARCH_PAGE_HTML);
+      /*generated_path=*/std::string(), IDR_TAB_SEARCH_TAB_SEARCH_PAGE_HTML);
   content::WebUIDataSource::Add(web_ui->GetWebContents()->GetBrowserContext(),
                                 source);
 
