6ddbefc9549cb71b2cd885458870fa851226586b
Sergey Poromov <poromov@chromium.org>
2020-11-03 15:06:50 +0000

    DLP: Connect video capture restriction to the policy.
    The screen recording part of the new screen capture feature
    should be connected to the same SCREENSHOT restriction in DLP policy.
    
    Tests: New browsertest
    Bug: 1133324
    Change-Id: If43440522edde5b1d4cc51400ec7f501c9d63074
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2510330
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Nikita Podguzov <nikitapodguzov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823538}

--- chrome/browser/chromeos/policy/dlp/dlp_content_manager.h
+++ chrome/browser/chromeos/policy/dlp/dlp_content_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_POLICY_DLP_DLP_CONTENT_MANAGER_H_
 
 #include <memory>
+#include <utility>
 
 #include "base/callback.h"
 #include "base/containers/flat_map.h"
