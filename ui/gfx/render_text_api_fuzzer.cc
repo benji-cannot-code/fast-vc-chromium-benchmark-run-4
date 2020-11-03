0f41d1f36c8a4a82402dcb9064df1c244685af90
Etienne Bergeron <etienneb@chromium.org>
2020-11-03 16:49:18 +0000

    Fix invalid nullptr detection due to missing DiscardableMemoryAllocator
    Linux requires the presence of a base::DiscardableMemoryAllocator
    for some fonts handling.
    
    This initialization code is taken from the gfx_unittest harness
    see: ui/gfx/test/run_all_unittests.cc
    
    The fuzzer incorrectly reports them as bugs since a nullptr
    is deferenced.
    
    Bug: 1144855
    Change-Id: Idfc0888b69fac8a3380659b5469bff0acaa6e806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2511697
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823571}

--- ui/gfx/render_text_api_fuzzer.cc
+++ ui/gfx/render_text_api_fuzzer.cc
@@ -18,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/render_text.h"
 
+#if defined(OS_ANDROID) || defined(OS_LINUX)
+#include "base/test/test_discardable_memory_allocator.h"
+#endif
+
 namespace {
 
 #if defined(OS_WIN)
@@ -34,10 +38,18 @@ struct Environment {
                           TestTimeouts::Initialize(),
                           base::test::TaskEnvironment::MainThreadType::UI)) {
     logging::SetMinLogLevel(logging::LOG_FATAL);
-
+#if defined(OS_ANDROID) || defined(OS_LINUX)
+    // Some platforms require discardable memory to use bitmap fonts.
+    base::DiscardableMemoryAllocator::SetInstance(
+        &discardable_memory_allocator);
+#endif
     CHECK(base::i18n::InitializeICU());
     gfx::FontList::SetDefaultFontDescription(kFontDescription);
   }
+
+#if defined(OS_ANDROID) || defined(OS_LINUX)
+  base::TestDiscardableMemoryAllocator discardable_memory_allocator;
+#endif
 
   base::AtExitManager at_exit_manager;
   base::test::TaskEnvironment task_environment;
