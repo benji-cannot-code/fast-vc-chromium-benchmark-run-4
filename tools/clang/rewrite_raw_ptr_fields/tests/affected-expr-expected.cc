c3dbc3eb70d47d99d181751bb6fe3aedf74ebd93
Lukasz Anforowicz <lukasza@chromium.org>
2020-11-03 21:59:52 +0000

    Emit constexpr-initialized fields as candidates for exclusion (vars).
    This CL is similar to r820840, but handles fields initialized not in
    constexpr constructors, but instead in constexpr variables.  After this
    CL, the rewriter will detect fields that are initialized with
    non-nullptr value in an initializer of a constexpr variable.  Such
    fields will be emitted as candidates for exclusion via --exclude-fields
    cmdline parameter.
    
    Bug: 1069567
    Change-Id: I35a901276752b21b4b4826e8447381d5647f7d0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2514763
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823736}

--- tools/clang/rewrite_raw_ptr_fields/tests/affected-expr-expected.cc
+++ tools/clang/rewrite_raw_ptr_fields/tests/affected-expr-expected.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>  // for uintptr_t
 
+#include <string>
 #include <tuple>    // for std::tie
 #include <utility>  // for std::swap
 
