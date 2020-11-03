5134ce26a993f3d237b1f0dc51629498a3dcb13c
Zentaro Kavanagh <zentaro@chromium.org>
2020-11-03 23:26:14 +0000

    Mark classes with final dtors as final
    - -Wfinal-dtor-non-final-class warns on classes with final dtors but
      not final classes.
    - Error messages are better when the class is marked final.
    - Fix existing issues in code base and a follow up will remove warning
      exemption.
    
    Bug: 999886
    Test: no errors building
    Change-Id: I93605e43f3a2f454e3a43e4cb370ac4d52f5330a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508855
    Auto-Submit: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823779}

--- media/base/fake_single_thread_task_runner.h
+++ media/base/fake_single_thread_task_runner.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-class FakeSingleThreadTaskRunner : public base::SingleThreadTaskRunner {
+class FakeSingleThreadTaskRunner final : public base::SingleThreadTaskRunner {
  public:
   explicit FakeSingleThreadTaskRunner(base::SimpleTestTickClock* clock);
 
--- media/renderers/default_decoder_factory.h
+++ media/renderers/default_decoder_factory.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-class MEDIA_EXPORT DefaultDecoderFactory : public DecoderFactory {
+class MEDIA_EXPORT DefaultDecoderFactory final : public DecoderFactory {
  public:
   // |external_decoder_factory| is optional decoder factory that provides
   // additional decoders.
