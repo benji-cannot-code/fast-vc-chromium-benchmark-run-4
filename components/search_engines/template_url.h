972da377486f798cd6519dbca5816f44d29eb5fd
Donn Denman <donnd@chromium.org>
2020-11-03 21:05:39 +0000

    [RSearches] Add a CGI param to Template URL
    Adds a new CGI param for Related Searches to Contextual Search
    that contains experiment and processing information for offline
    logs quality analysis. This param is used for both the Resolve
    and Search requests.
    
    BUG=1144916
    
    Change-Id: Ibc8b4ced4831f8c867be2185ed174b7306b248cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2515225
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823712}

--- components/search_engines/template_url.h
+++ components/search_engines/template_url.h
@@ -116,6 +116,10 @@ class TemplateURLRef {
       // is fluent in reading.  This acts as an alternate set of languages
       // to consider translating into.  The languages are ordered by
       // fluency, and encoded as a comma-separated list of BCP 47 languages.
+      // The |related_searches_stamp| string contains an information that
+      // indicates experiment status and server processing results so that
+      // can be logged in GWS Sawmill logs for offline analysis for the
+      // Related Searches MVP experiment.
       ContextualSearchParams(int version,
                              int contextual_cards_version,
                              std::string home_country,
@@ -124,7 +128,8 @@ class TemplateURLRef {
                              bool is_exact_search,
                              std::string source_lang,
                              std::string target_lang,
-                             std::string fluent_languages);
+                             std::string fluent_languages,
+                             std::string related_searches_stamp);
       ContextualSearchParams(const ContextualSearchParams& other);
       ~ContextualSearchParams();
 
@@ -165,6 +170,11 @@ class TemplateURLRef {
       // Alternate target languages that the user is fluent in, encoded in a
       // single string.
       std::string fluent_languages;
+
+      // Experiment arm and processing information for the Related Searches
+      // experiment. The value is an arbitrary string that starts with a
+      // schema version number.
+      std::string related_searches_stamp;
     };
 
     // Estimates dynamic memory usage.
--- components/search_engines/template_url_unittest.cc
+++ components/search_engines/template_url_unittest.cc
@@ -1780,7 +1780,7 @@ TEST_F(TemplateURLTest, ContextualSearchParameters) {
   // event.
   TemplateURLRef::SearchTermsArgs::ContextualSearchParams params(
       2, 1, std::string(), 0, 0, false, std::string(), std::string(),
-      std::string());
+      std::string(), std::string());
   search_terms_args.contextual_search_params = params;
   result = url.url_ref().ReplaceSearchTerms(search_terms_args,
                                             search_terms_data_);
@@ -1794,7 +1794,7 @@ TEST_F(TemplateURLTest, ContextualSearchParameters) {
   search_terms_args.contextual_search_params =
       TemplateURLRef::SearchTermsArgs::ContextualSearchParams(
           2, 2, "CH", 1657713458, 5, false, std::string(), std::string(),
-          std::string());
+          std::string(), std::string());
   result =
       url.url_ref().ReplaceSearchTerms(search_terms_args, search_terms_data_);
 
@@ -1811,7 +1811,7 @@ TEST_F(TemplateURLTest, ContextualSearchParameters) {
   search_terms_args.contextual_search_params =
       TemplateURLRef::SearchTermsArgs::ContextualSearchParams(
           2, 1, std::string(), 0, 0, true, std::string(), std::string(),
-          std::string());
+          std::string(), std::string());
   result =
       url.url_ref().ReplaceSearchTerms(search_terms_args, search_terms_data_);
   // Find our param.
@@ -1821,7 +1821,8 @@ TEST_F(TemplateURLTest, ContextualSearchParameters) {
   // Test source and target languages.
   search_terms_args.contextual_search_params =
       TemplateURLRef::SearchTermsArgs::ContextualSearchParams(
-          2, 1, std::string(), 0, 0, true, "es", "de", std::string());
+          2, 1, std::string(), 0, 0, true, "es", "de", std::string(),
+          std::string());
   result =
       url.url_ref().ReplaceSearchTerms(search_terms_args, search_terms_data_);
   // Find our params.
@@ -1834,12 +1835,23 @@ TEST_F(TemplateURLTest, ContextualSearchParameters) {
   search_terms_args.contextual_search_params =
       TemplateURLRef::SearchTermsArgs::ContextualSearchParams(
           2, 1, std::string(), 0, 0, true, std::string(), std::string(),
-          "es,de");
+          "es,de", std::string());
   result =
       url.url_ref().ReplaceSearchTerms(search_terms_args, search_terms_data_);
   // Find our param.  These may actually be URL encoded.
   size_t fluent_pos = result.find("&ctxs_fls=es,de");
   EXPECT_NE(fluent_pos, std::string::npos);
+
+  // Test Related Searches.
+  search_terms_args.contextual_search_params =
+      TemplateURLRef::SearchTermsArgs::ContextualSearchParams(
+          2, 1, std::string(), 0, 0, true, std::string(), std::string(),
+          std::string(), "1RbCu");
+  result =
+      url.url_ref().ReplaceSearchTerms(search_terms_args, search_terms_data_);
+  // Find our param.
+  size_t ctxsl_rs_pos = result.find("&ctxsl_rs=1RbCu");
+  EXPECT_NE(ctxsl_rs_pos, std::string::npos);
 }
 
 TEST_F(TemplateURLTest, GenerateKeyword) {
