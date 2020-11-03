89e9774c93842efeb9f64a0a1a5b0b0d22400289
Gayane Petrosyan <gayane@chromium.org>
2020-11-03 14:43:30 +0000

    Revert "[CrOS PhoneHub] Add notification metrics."
    This reverts commit 3dcbf868abea66be83f27e6de86529b81a9a5d0f.
    
    Reason for revert: Msan failure
    Bug: 1145157
    
    Original change's description:
    > [CrOS PhoneHub] Add notification metrics.
    >
    > We add metrics for PhoneHub notification actions by introducing' a new
    > PHONE_HUB notification type. This way, metrics will be logged into
    > existing histograms.
    >
    > Additionally, this CL also adds a new Ash.PhoneHub.NotificationCount
    > histogram counting the number of active PhoneHub notifications.
    >
    > BUG=1138137,1106937
    >
    > Change-Id: I4529eb6cc0d861ec9eb720e4eeac5c6d2d6257ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505728
    > Commit-Queue: Tim Song <tengs@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Reviewed-by: Andre Le <leandre@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#823474}
    
    TBR=yoshiki@chromium.org,khorimoto@chromium.org,isherman@chromium.org,tengs@chromium.org,leandre@chromium.org
    
    Change-Id: I523202c3a64f58d267dff229baf69dad1cfe8377
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1138137
    Bug: 1106937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2517241
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823530}

--- ash/system/phonehub/phone_hub_notification_controller.cc
+++ ash/system/phonehub/phone_hub_notification_controller.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/model/system_tray_model.h"
-#include "ash/system/phonehub/phone_hub_metrics.h"
 #include "ash/system/tray/tray_popup_utils.h"
 #include "base/bind.h"
 #include "base/logging.h"
@@ -36,9 +35,9 @@ namespace ash {
 namespace {
 const char kNotifierId[] = "chrome://phonehub";
 const char kNotifierIdSeparator[] = "-";
+const char kNotificationCustomViewType[] = "phonehub";
 const char kPhoneHubInstantTetherNotificationId[] =
     "chrome://phonehub-instant-tether";
-const char kNotificationCustomViewType[] = "phonehub";
 const int kReplyButtonIndex = 0;
 
 // The amount of time the reply button is disabled after sending an inline
@@ -230,8 +229,6 @@ void PhoneHubNotificationController::OnNotificationsAdded(
   for (int64_t id : notification_ids) {
     CreateOrUpdateNotification(manager_->GetNotification(id));
   }
-
-  LogNotificationCount();
 }
 
 void PhoneHubNotificationController::OnNotificationsUpdated(
@@ -250,8 +247,6 @@ void PhoneHubNotificationController::OnNotificationsRemoved(
     it->second->Remove();
     notification_map_.erase(it);
   }
-
-  LogNotificationCount();
 }
 
 void PhoneHubNotificationController::OnAttemptConnectionScanFailed() {
@@ -308,19 +303,6 @@ void PhoneHubNotificationController::SendInlineReply(
   manager_->SendInlineReply(notification_id, inline_reply_text);
 }
 
-void PhoneHubNotificationController::LogNotificationCount() {
-  int count = 0;
-
-  auto* message_center = message_center::MessageCenter::Get();
-  for (auto* notification : message_center->GetVisibleNotifications()) {
-    auto notifier_type = notification->notifier_id().type;
-    if (notifier_type == message_center::NotifierType::PHONE_HUB)
-      count++;
-  }
-
-  phone_hub_metrics::LogNotificationCount(count);
-}
-
 void PhoneHubNotificationController::CreateOrUpdateNotification(
     const chromeos::phonehub::Notification* notification) {
   int64_t phone_hub_id = notification->id();
@@ -351,7 +333,7 @@ PhoneHubNotificationController::CreateNotification(
     const std::string& cros_id,
     NotificationDelegate* delegate) {
   message_center::NotifierId notifier_id(
-      message_center::NotifierType::PHONE_HUB, kNotifierId);
+      message_center::NotifierType::SYSTEM_COMPONENT, kNotifierId);
 
   auto notification_type = message_center::NOTIFICATION_TYPE_CUSTOM;
 
