3ab1168a1ebe3afc5b3e4b09aac118058b00f730
Mason Freed <masonfreed@chromium.org>
2020-11-03 14:56:52 +0000

    Change deprecation message for :unresolved pseudo selector
    This just changes the wording for the deprecation message for
    the :unresolved pseudo selector. Instead of "will be removed",
    it now just says "removed".
    
    Bug: 937746
    Change-Id: I694bcefabf34f9399000206d30da50b62fbf01b9
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2514081
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Auto-Submit: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823533}

--- third_party/blink/renderer/core/frame/deprecation.cc
+++ third_party/blink/renderer/core/frame/deprecation.cc
@@ -397,10 +397,6 @@ DeprecationInfo GetDeprecationInfo(WebFeature feature) {
           "https://www.chromestatus.com/feature/5735596811091968 for more "
           "details."};
 
-#define kWebComponentsV0DeprecationPost                \
-  "https://developers.google.com/web/updates/2019/07/" \
-  "web-components-time-to-upgrade"
-
     case WebFeature::kHTMLImports:
       return {"HTMLImports", kUnknown,
               "The HTML Imports feature has been removed. See "
@@ -420,12 +416,11 @@ DeprecationInfo GetDeprecationInfo(WebFeature feature) {
               "details."};
 
     case WebFeature::kCSSSelectorPseudoUnresolved:
-      return {"CSSSelectorPseudoUnresolved", kM80,
-              ReplacedWillBeRemoved(
-                  ":unresolved pseudo selector", ":not(:defined)", kM80,
-                  "4642138092470272 and " kWebComponentsV0DeprecationPost)};
-
-#undef kWebComponentsV0DeprecationPost
+      return {
+          "CSSSelectorPseudoUnresolved", kUnknown,
+          "The Custom Elements v0 API (:unresolved pseudo selector) has been "
+          "removed. See https://www.chromestatus.com/feature/4642138092470272 "
+          "for more details."};
 
     case WebFeature::kLocalCSSFileExtensionRejected:
       return {"LocalCSSFileExtensionRejected", kM64,
