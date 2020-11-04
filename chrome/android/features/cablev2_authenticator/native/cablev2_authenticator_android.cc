436aa3e4e2f5af1183f76b7291b7f72d094616c8
Adam Langley <agl@chromium.org>
2020-11-04 02:01:58 +0000

    webauthn: allow USB or QR caBLE transactions to preempt FCM.
    If an FCM transactions triggers but then one happens to plug in a USB
    cable, the mobile side hits this DCHECK. But it's fine to cancel the FCM
    transaction if the user has taken action to try another channel.
    
    Also, drop the notification if the FCM transaction is deleted.
    
    BUG=1002262
    
    Change-Id: I857016e31a0a3d56ee17f54ca4eecb4e6b3f9fdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2511974
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823842}

--- chrome/android/features/cablev2_authenticator/native/cablev2_authenticator_android.cc
+++ chrome/android/features/cablev2_authenticator/native/cablev2_authenticator_android.cc
@@ -203,6 +203,12 @@ class AndroidPlatform : public device::cablev2::authenticator::Platform {
       : env_(env),
         interaction_needed_callback_(std::move(interaction_needed_callback)) {}
 
+  ~AndroidPlatform() override {
+    if (notification_showing_) {
+      Java_CableAuthenticator_dropNotification(env_);
+    }
+  }
+
   // Platform:
   void MakeCredential(std::unique_ptr<MakeCredentialParams> params) override {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -290,6 +296,7 @@ class AndroidPlatform : public device::cablev2::authenticator::Platform {
   void NeedInteractive() {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
+    notification_showing_ = true;
     std::move(interaction_needed_callback_)
         .Run(base::BindOnce(&AndroidPlatform::OnInteractionReady,
                             weak_factory_.GetWeakPtr()));
@@ -300,11 +307,13 @@ class AndroidPlatform : public device::cablev2::authenticator::Platform {
   void OnInteractionReady(ScopedJavaGlobalRef<jobject> cable_authenticator) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     DCHECK(!cable_authenticator_);
+    DCHECK(notification_showing_);
     DCHECK(env_->IsInstanceOf(
         cable_authenticator.obj(),
         org_chromium_chrome_browser_webauth_authenticator_CableAuthenticator_clazz(
             env_)));
     cable_authenticator_ = std::move(cable_authenticator);
+    notification_showing_ = false;
 
     DCHECK(static_cast<bool>(pending_make_credential_) ^
            static_cast<bool>(pending_get_assertion_));
@@ -316,6 +325,7 @@ class AndroidPlatform : public device::cablev2::authenticator::Platform {
   }
 
   JNIEnv* const env_;
+  bool notification_showing_ = false;
   ScopedJavaGlobalRef<jobject> cable_authenticator_;
   std::unique_ptr<MakeCredentialParams> pending_make_credential_;
   std::unique_ptr<GetAssertionParams> pending_get_assertion_;
@@ -443,7 +453,6 @@ static void JNI_CableAuthenticator_StartUSB(
   DCHECK(!global_data.usb_callback);
   global_data.usb_callback = transport->GetCallback();
 
-  DCHECK(!global_data.current_transaction);
   global_data.current_transaction =
       device::cablev2::authenticator::TransactWithPlaintextTransport(
           std::make_unique<AndroidPlatform>(env, cable_authenticator),
@@ -465,7 +474,6 @@ static jboolean JNI_CableAuthenticator_StartQR(
     return false;
   }
 
-  DCHECK(!global_data.current_transaction);
   global_data.current_transaction =
       device::cablev2::authenticator::TransactFromQRCode(
           std::make_unique<AndroidPlatform>(env, cable_authenticator),
