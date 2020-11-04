da5dfe6bf49a236d48bac6888773dd0a57871b8e
Ahmed Fakhry <afakhry@chromium.org>
2020-11-04 01:29:44 +0000

    capture_mode: Initial work for tracking events that end recording
    In this CL, we observe the following events:
    - A window being recorded gets closed.
    - A display being fullscreen-recorded gets disconnected.
    - System shutting down.
    
    The above events end video recording.
    
    BUG=1142994
    TEST=Added new tests.
    
    Change-Id: I5f03448d1399297e9589d561d9854b3c8cffc0f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2516239
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823831}

--- ash/capture_mode/capture_mode_util.h
+++ ash/capture_mode/capture_mode_util.h
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/capture_mode/capture_mode_types.h"
 
+namespace aura {
+class Window;
+}  // namespace aura
+
 namespace gfx {
 class Point;
 class Rect;
@@ -23,10 +27,14 @@ ASH_EXPORT gfx::Point GetLocationForFineTunePosition(const gfx::Rect& rect,
                                                      FineTunePosition position);
 
 // Return whether |position| is a corner.
-ASH_EXPORT bool IsCornerFineTunePosition(FineTunePosition position);
+bool IsCornerFineTunePosition(FineTunePosition position);
 
 // Return whether drag affordance circles should be hidden.
-ASH_EXPORT bool ShouldHideDragAffordance(FineTunePosition position);
+bool ShouldHideDragAffordance(FineTunePosition position);
+
+// Sets the visibility of the stop-recording button in the Shelf's status area
+// widget of the given |root| window.
+void SetStopRecordingButtonVisibility(aura::Window* root, bool visible);
 
 }  // namespace capture_mode_util
 
