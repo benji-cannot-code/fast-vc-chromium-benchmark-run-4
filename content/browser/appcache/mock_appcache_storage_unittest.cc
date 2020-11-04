e590cef8f83bbd7f3127e93a7b67f011fb561bf5
Victor Costan <pwnall@chromium.org>
2020-11-04 23:55:29 +0000

    AppCache: Use a BrowserTaskEnvironment in MockAppCacheStorageTest.
    This CL replaces the test's base::test::TaskEnvironment with a
    content::BrowserTaskEnvironment.
    
    The QuotaClient mojofication benefits from having this test use a
    content::BrowserTaskEnvironment, which can supply a TaskRunner for the
    IO thread.
    
    Bug: 1016065
    Change-Id: Ia5cbe1a1ecafc6fd0d37972c28a44dfb5bc2d0ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519884
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824215}

--- content/browser/appcache/mock_appcache_storage_unittest.cc
+++ content/browser/appcache/mock_appcache_storage_unittest.cc
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/run_loop.h"
-#include "base/test/task_environment.h"
 #include "content/browser/appcache/appcache.h"
 #include "content/browser/appcache/appcache_group.h"
 #include "content/browser/appcache/appcache_response_info.h"
 #include "content/browser/appcache/appcache_storage.h"
 #include "content/browser/appcache/mock_appcache_service.h"
+#include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/mojom/appcache/appcache_info.mojom.h"
 
@@ -86,7 +86,7 @@ class MockAppCacheStorageTest : public testing::Test {
   };
 
  private:
-  base::test::TaskEnvironment task_environment_;
+  content::BrowserTaskEnvironment task_environment_;
 };
 
 TEST_F(MockAppCacheStorageTest, LoadCache_Miss) {
