d101d93fd876d37fb0676da7392c802838b17c79
Omer Katz <omerkatz@chromium.org>
2020-11-04 18:48:24 +0000

    heap: Avoid multiple retracing of weak containers
    We found that erasing from the weak container worklist caused timeouts
    in some blink_perf benchmarks. The erase operation was meant to avoid
    retracing the same backing store multiple times (an iterator to a hash
    table would result in at least 2 on-stack references to the backing
    store). Instead of erasing, this CL pushes retraced backing stores to
    a vector. Backing stores are retraced only if they are not yet in the
    vector. Thus iterators should trigger only a single retracing of the
    backing store.
    The vector is limited in size to avoid making it uncontrollably large.
    That means it is possible to retrace the same backing store multiple
    times if it was already ejected from the backing store. However this is
    very unlikely in practice.
    
    Drive-by: re-enable skipped tests.
    
    Bug: 1142315
    Change-Id: I49bd30263db46e66293d046b85699826298aa033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2516363
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824069}

--- third_party/blink/renderer/platform/heap/impl/heap.cc
+++ third_party/blink/renderer/platform/heap/impl/heap.cc
@@ -60,24 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void WeakContainersWorklist::Push(const HeapObjectHeader* object) {
-  DCHECK(object);
-  WTF::MutexLocker locker(lock_);
-  objects_.insert(object);
-}
-
-void WeakContainersWorklist::Erase(const HeapObjectHeader* object) {
-  // This method is called only during atomic pause, so lock is not needed.
-  DCHECK(object);
-  objects_.erase(object);
-}
-
-bool WeakContainersWorklist::Contains(const HeapObjectHeader* object) {
-  // This method is called only during atomic pause, so lock is not needed.
-  DCHECK(object);
-  return objects_.find(object) != objects_.end();
-}
-
 HeapAllocHooks::AllocationHook* HeapAllocHooks::allocation_hook_ = nullptr;
 HeapAllocHooks::FreeHook* HeapAllocHooks::free_hook_ = nullptr;
 
