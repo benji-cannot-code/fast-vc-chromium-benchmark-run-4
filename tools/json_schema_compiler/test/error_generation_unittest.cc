5d7175f756936d1c505cfbfd5dc0c035cfa2d3c4
Karan Bhatia <karandeepb@chromium.org>
2020-11-04 22:52:56 +0000

    Reland "JsonSchemaCompiler: Raise error on parse failures of optional properties."
    This reverts commit 898910982f473d79b63f02f68143871c4763c4a4.
    
    Reason for revert: The original test failures were caused due to crbug.com/gn/215 which caused spurious failures in 
    incremental rebuilds. The corresponding GN roll has now been reverted.
    
    Original change's description:
    > Revert "JsonSchemaCompiler: Raise error on parse failures of optional properties."
    >
    > This reverts commit eec7bff80bef52bbe18b5d21b9e660da47842a80.
    >
    > Reason for revert:
    > After this change, JsonSchemaCompilerErrorTest.* are failing on
    > several linux bots:
    > https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests/95122
    > https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28dbg%29%281%29/92709
    > https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/21165
    >
    > Original change's description:
    > > JsonSchemaCompiler: Raise error on parse failures of optional properties.
    > >
    > > Currently when an optional property fails to parse and error generation
    > > is enabled for the schema, the auto-generated Populate function
    > > continues parsing the type but still populates error. This CL changes
    > > the code to ensure that we raise an error in these cases.
    > >
    > > We also don't raise an error on unrecognized keys any longer since we
    > > shouldn't be raising an error while returning a parse success: Most
    > > consumers currently don't distinguish between the two cases and this can
    > > lead to the returned error being a concatenation of unrelated errors,
    > > which is not ideal.
    > >
    > > We now enforce the constraint that Populate only returns an error iff
    > > there is a parse failure. We DCHECK the same in the auto-generated
    > > FromValue function.
    > >
    > > This also brings the code more in-line with what we do for
    > > auto-generated manifest parsing.
    > >
    > > Currently only 3 schemas use error generation:
    > >   - declarative_net_request.idl
    > >   - extensions_manifest_types.json
    > >   - manifest_types.json
    > >
    > > The latter two may be affected by the change. In particular, parsing
    > > manifest types declared by them may lead to a hard error when an
    > > optional property can't be parsed now.
    > >
    > > BUG=1113513
    > >
    > > Change-Id: I63966389e25f7591b4425815d32d9da59d35c3fb
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500425
    > > Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    > > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#823545}
    >
    > TBR=rdevlin.cronin@chromium.org,karandeepb@chromium.org
    >
    > Change-Id: I3b26905381996192b377f79abcde757efaf08f31
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1113513
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519330
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#823902}
    
    TBR=rdevlin.cronin@chromium.org,nhiroki@chromium.org,karandeepb@chromium.org
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1113513
    Change-Id: I57d4725126af7c019518cd2bff000403a8b749b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519173
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824187}

--- tools/json_schema_compiler/test/error_generation_unittest.cc
+++ tools/json_schema_compiler/test/error_generation_unittest.cc
@@ -157,7 +157,7 @@ TEST(JsonSchemaCompilerErrorTest, WrongTypeValueType) {
         Dictionary("otherType", std::make_unique<Value>(1.1));
     errors::ObjectType out;
     base::string16 error;
-    EXPECT_TRUE(errors::ObjectType::Populate(*value, &out, &error));
+    EXPECT_FALSE(errors::ObjectType::Populate(*value, &out, &error));
     EXPECT_TRUE(EqualsUtf16("'otherType': expected dictionary, got double",
         error));
     EXPECT_EQ(NULL, out.other_type.get());
@@ -227,9 +227,7 @@ TEST(JsonSchemaCompilerErrorTest, BadEnumValue) {
   }
 }
 
-// Warn but don't fail out errors
-
-TEST(JsonSchemaCompilerErrorTest, WarnOnOptionalFailure) {
+TEST(JsonSchemaCompilerErrorTest, ErrorOnOptionalFailure) {
   {
     std::unique_ptr<base::DictionaryValue> value =
         Dictionary("string", std::make_unique<Value>("bling"));
@@ -242,7 +240,7 @@ TEST(JsonSchemaCompilerErrorTest, WarnOnOptionalFailure) {
 
     errors::OptionalTestType out;
     base::string16 error;
-    EXPECT_TRUE(errors::OptionalTestType::Populate(*value, &out, &error));
+    EXPECT_FALSE(errors::OptionalTestType::Populate(*value, &out, &error));
     EXPECT_TRUE(EqualsUtf16("'string': expected string, got integer",
         error));
     EXPECT_EQ(NULL, out.string.get());
@@ -263,7 +261,7 @@ TEST(JsonSchemaCompilerErrorTest, OptionalBinaryTypeFailure) {
 
     errors::OptionalBinaryData out;
     base::string16 error;
-    EXPECT_TRUE(errors::OptionalBinaryData::Populate(*value, &out, &error));
+    EXPECT_FALSE(errors::OptionalBinaryData::Populate(*value, &out, &error));
     EXPECT_TRUE(EqualsUtf16("'data': expected binary, got integer",
         error));
     EXPECT_EQ(NULL, out.data.get());
@@ -281,7 +279,7 @@ TEST(JsonSchemaCompilerErrorTest, OptionalArrayTypeFailure) {
         Dictionary("TheArray", std::make_unique<Value>(5));
     errors::ArrayObject out;
     base::string16 error;
-    EXPECT_TRUE(errors::ArrayObject::Populate(*value, &out, &error));
+    EXPECT_FALSE(errors::ArrayObject::Populate(*value, &out, &error));
     EXPECT_TRUE(EqualsUtf16("'TheArray': expected list, got integer",
         error));
     EXPECT_EQ(NULL, out.the_array.get());
@@ -301,8 +299,8 @@ TEST(JsonSchemaCompilerErrorTest, OptionalUnableToPopulateArray) {
         List(std::make_unique<Value>(5), std::make_unique<Value>(false));
     errors::OptionalChoiceType::Integers out;
     base::string16 error;
-    EXPECT_TRUE(errors::OptionalChoiceType::Integers::Populate(*params_value,
-                                                               &out, &error));
+    EXPECT_FALSE(errors::OptionalChoiceType::Integers::Populate(*params_value,
+                                                                &out, &error));
     EXPECT_TRUE(EqualsUtf16(
         "expected integer, got boolean; unable to populate array 'integers'",
         error));
@@ -316,12 +314,12 @@ TEST(JsonSchemaCompilerErrorTest, MultiplePopulationErrors) {
         Dictionary("TheArray", std::make_unique<Value>(5));
     errors::ArrayObject out;
     base::string16 error;
-    EXPECT_TRUE(errors::ArrayObject::Populate(*value, &out, &error));
+    EXPECT_FALSE(errors::ArrayObject::Populate(*value, &out, &error));
     EXPECT_TRUE(EqualsUtf16("'TheArray': expected list, got integer",
         error));
     EXPECT_EQ(NULL, out.the_array.get());
 
-    EXPECT_TRUE(errors::ArrayObject::Populate(*value, &out, &error));
+    EXPECT_FALSE(errors::ArrayObject::Populate(*value, &out, &error));
     EXPECT_TRUE(EqualsUtf16("'TheArray': expected list, got integer; "
         "'TheArray': expected list, got integer",
         error));
@@ -336,10 +334,10 @@ TEST(JsonSchemaCompilerErrorTest, TooManyKeys) {
     EXPECT_TRUE(EqualsUtf16("", GetPopulateError<errors::TestType>(*value)));
   }
   {
+    // We simply ignore extra keys.
     std::unique_ptr<base::DictionaryValue> value =
         Dictionary("string", std::make_unique<Value>("yes"), "ohno",
                    std::make_unique<Value>("many values"));
-    EXPECT_TRUE(EqualsUtf16("found unexpected key 'ohno'",
-                            GetPopulateError<errors::TestType>(*value)));
+    EXPECT_TRUE(EqualsUtf16("", GetPopulateError<errors::TestType>(*value)));
   }
 }
