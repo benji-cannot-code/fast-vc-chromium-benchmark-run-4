b46440d581812e7b66af95a7b5128b2b7088eecf
Sinan Sahin <sinansahin@google.com>
2020-11-04 00:22:33 +0000

    [Messages] Make tests MediumTest so they don't time out
    Bug: 1144773
    Change-Id: I8cf714fa6923f99db29435b5c83917ac7fc9aa50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2518094
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#823805}

--- components/messages/android/java/src/org/chromium/components/messages/SingleActionMessageTest.java
+++ components/messages/android/java/src/org/chromium/components/messages/SingleActionMessageTest.java
@@ -10,7 +10,7 @@ import static org.mockito.Mockito.doNothing;
 
 import android.app.Activity;
 
-import androidx.test.filters.SmallTest;
+import androidx.test.filters.MediumTest;
 
 import org.junit.Assert;
 import org.junit.Rule;
@@ -44,7 +44,7 @@ public class SingleActionMessageTest extends DummyUiActivityTestCase {
     }
 
     @Test
-    @SmallTest
+    @MediumTest
     public void testAddAndRemoveSingleActionMessage() throws Exception {
         MessageContainer container = new MessageContainer(getActivity(), null);
         PropertyModel model = createBasicSingleActionMessageModel();
@@ -72,7 +72,7 @@ public class SingleActionMessageTest extends DummyUiActivityTestCase {
     }
 
     @Test(expected = IllegalStateException.class)
-    @SmallTest
+    @MediumTest
     public void testAddMultipleSingleActionMessage() {
         MessageContainer container = new MessageContainer(getActivity(), null);
         PropertyModel m1 = createBasicSingleActionMessageModel();
