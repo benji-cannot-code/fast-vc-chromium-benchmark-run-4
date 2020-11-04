a34b5c2b85522d0f6dd7d5dca6588e2a59a61733
Josiah K <josiahk@google.com>
2020-11-04 05:45:47 +0000

    Add Docked Magnifier support to Accessibility Common extension
    Bug: 1131153
    Change-Id: I959958dbfe5d81cac99bd3f747170fd60921e415
    AX-Relnotes: Adds Docked Magnifier support for active-descendant focus-following
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2465491
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/master@{#823896}

--- chrome/browser/chromeos/extensions/accessibility_features_apitest.cc
+++ chrome/browser/chromeos/extensions/accessibility_features_apitest.cc
@@ -324,7 +324,6 @@ IN_PROC_BROWSER_TEST_F(AccessibilityFeaturesApiTest, ObserveFeatures) {
   std::vector<std::string> enabled_features = {
       "caretHighlight",
       "cursorColor",
-      "dockedMagnifier",
       "focusHighlight",
       "stickyKeys",
   };
@@ -332,6 +331,7 @@ IN_PROC_BROWSER_TEST_F(AccessibilityFeaturesApiTest, ObserveFeatures) {
   std::vector<std::string> disabled_features = {
       "autoclick",
       "cursorHighlight",
+      "dockedMagnifier",
       "highContrast",
       "largeCursor",
       "screenMagnifier",
