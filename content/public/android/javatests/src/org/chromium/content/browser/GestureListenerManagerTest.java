86873a903c74d0fd36bd81065ece6282827bce5c
Michael Thiessen <mthiesse@chromium.org>
2020-11-04 00:47:48 +0000

    Migrate content/ to base/ CriteriaHelper
    Updates imports in content/ to use the base/ CriteriaHelper
    
    Bug: 1134178
    Change-Id: I1993bf4128c1422491bf93ca36e328f51c5b78d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2518037
    Auto-Submit: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823814}

--- content/public/android/javatests/src/org/chromium/content/browser/GestureListenerManagerTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/GestureListenerManagerTest.java
@@ -17,6 +17,8 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.CallbackHelper;
+import org.chromium.base.test.util.Criteria;
+import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content_public.browser.GestureListenerManager;
@@ -24,8 +26,6 @@ import org.chromium.content_public.browser.GestureStateListenerWithScroll;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.test.RenderFrameHostTestExt;
-import org.chromium.content_public.browser.test.util.Criteria;
-import org.chromium.content_public.browser.test.util.CriteriaHelper;
 import org.chromium.content_public.browser.test.util.TestCallbackHelperContainer;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.content_public.browser.test.util.TouchCommon;
