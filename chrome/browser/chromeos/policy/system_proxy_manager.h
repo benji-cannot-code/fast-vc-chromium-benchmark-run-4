0d637aaff1c5db62cc92160f1025dbacb68a4394
Andreea Costinas <acostinas@google.com>
2020-11-04 16:54:12 +0000

    system-proxy: Add auth schemes to policy
    Currently an attacker can obtain the System-proxy policy set credentials
    by tricking the Chromebook to connect to a proxy with a less secure auth
    scheme.
    
    To protect against a downgrade attack, this CL adds
    policy_credentials_auth_schemes to the SystemProxySettings policy which
    will allow admins to specify for which proxy auth schemes the
    credentials can be applied to.
    
    If no value is set by policy, then all three username+password auth
    schemes are allowed. This is to have backwards compatibility with the
    current policy until AdminConsole is updated.
    
    Bug: 1132247
    Test: browser test
    Change-Id: I67a21c88c411a4373159aaa5f8cadb948e5b9cc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2484446
    Commit-Queue: Andreea-Elena Costinas <acostinas@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Omar Morsi <omorsi@google.com>
    Cr-Commit-Position: refs/heads/master@{#824019}

--- chrome/browser/chromeos/policy/system_proxy_manager.h
+++ chrome/browser/chromeos/policy/system_proxy_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "base/callback_forward.h"
 #include "base/gtest_prod_util.h"
@@ -181,6 +182,10 @@ class SystemProxyManager : public chromeos::NetworkStateHandlerObserver {
   std::string system_services_address_;
   std::string system_services_username_;
   std::string system_services_password_;
+  // List of proxy authentication schemes for which the policy set credentials
+  // can be used.
+  std::vector<std::string> policy_credentials_auth_schemes_;
+
   // The credentials which were last sent to System-proxy. They can differ from
   // `system_services_username_` and `system_services_username_` if the proxy
   // configuration is not managed; in this case `last_sent_username_` and
