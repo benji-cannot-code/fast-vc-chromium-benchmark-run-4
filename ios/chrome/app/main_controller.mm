245dec74a07a6dafa8bbdc931294a0881c06e1cd
Stepan Khapugin <stkhapugin@chromium.org>
2020-11-04 17:27:01 +0000

    [iOS] Fix post-first run tasks in multiwindow.
    Due to a mistake in refactoring, the EULA was never marked as read, and
    the location pop-up was never shown. Fix these by moving relevant code
    to scene controller.
    
    It seems like running this code multiple times (if multiwindow is used
    in FRE) is safe and will not cause issues.
    
    Bug: 1145063
    Change-Id: If61e1fdb75b8cf6e1743c543d4570279f1f7a2d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2517953
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824034}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -64,7 +64,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/external_files/external_file_remover_impl.h"
 #include "ios/chrome/browser/feature_engagement/tracker_factory.h"
 #import "ios/chrome/browser/first_run/first_run.h"
-#include "ios/chrome/browser/geolocation/omnibox_geolocation_controller.h"
 #include "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/main/browser_list.h"
 #import "ios/chrome/browser/main/browser_list_factory.h"
@@ -284,8 +283,6 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
 - (void)cleanupSnapshots;
 // Cleanup discarded sessions on disk.
 - (void)cleanupDiscardedSessions;
-// Sets a LocalState pref marking the TOS EULA as accepted.
-- (void)markEulaAsAccepted;
 // Sends any feedback that happens to still be on local storage.
 - (void)sendQueuedFeedback;
 // Called whenever an orientation change is received.
@@ -325,10 +322,6 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
 - (void)scheduleDeleteTempPasswordsDirectory;
 // Crashes the application if requested.
 - (void)crashIfRequested;
-// Handles the notification that first run modal dialog UI is about to complete.
-- (void)handleFirstRunUIWillFinish;
-// Handles the notification that first run modal dialog UI completed.
-- (void)handleFirstRunUIDidFinish;
 // Performs synchronous browser state initialization steps.
 - (void)initializeBrowserState:(ChromeBrowserState*)browserState;
 // Helper methods to initialize the application to a specific stage.
@@ -592,27 +585,6 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
   search_engines::UpdateSearchEnginesIfNeeded(
       browserState->GetPrefs(),
       ios::TemplateURLServiceFactory::GetForBrowserState(browserState));
-}
-
-- (void)handleFirstRunUIWillFinish {
-  [[NSNotificationCenter defaultCenter]
-      removeObserver:self
-                name:kChromeFirstRunUIWillFinishNotification
-              object:nil];
-
-  [self markEulaAsAccepted];
-}
-
-- (void)handleFirstRunUIDidFinish {
-  [[NSNotificationCenter defaultCenter]
-      removeObserver:self
-                name:kChromeFirstRunUIDidFinishNotification
-              object:nil];
-
-  // As soon as First Run has finished, give OmniboxGeolocationController an
-  // opportunity to present the iOS system location alert.
-  [[OmniboxGeolocationController sharedInstance]
-      triggerSystemPromptForNewUser:YES];
 }
 
 #pragma mark - AppStateObserver
@@ -1082,22 +1054,6 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
   }
 }
 
-- (void)prepareForFirstRunUI {
-  // Register for notification when First Run is completed.
-  // Some initializations are held back until First Run modal dialog
-  // is dismissed.
-  [[NSNotificationCenter defaultCenter]
-      addObserver:self
-         selector:@selector(handleFirstRunUIWillFinish)
-             name:kChromeFirstRunUIWillFinishNotification
-           object:nil];
-  [[NSNotificationCenter defaultCenter]
-      addObserver:self
-         selector:@selector(handleFirstRunUIDidFinish)
-             name:kChromeFirstRunUIDidFinishNotification
-           object:nil];
-}
-
 - (void)crashIfRequested {
   if (experimental_flags::IsStartupCrashEnabled()) {
     // Flush out the value cached for breakpad::SetUploadingEnabled().
@@ -1173,15 +1129,6 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
       ->RemoveSessionsData(sessionIDs);
   sessions_storage_util::ResetDiscardedSessions();
 }
-
-- (void)markEulaAsAccepted {
-  PrefService* prefs = GetApplicationContext()->GetLocalState();
-  if (!prefs->GetBoolean(prefs::kEulaAccepted))
-    prefs->SetBoolean(prefs::kEulaAccepted, true);
-  prefs->CommitPendingWrite();
-}
-
-
 
 #pragma mark - BrowsingDataCommands
 
--- ios/chrome/browser/ui/main/scene_controller.mm
+++ ios/chrome/browser/ui/main/scene_controller.mm
@@ -12,9 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/post_task.h"
+#include "components/prefs/pref_service.h"
 #import "components/previous_session_info/previous_session_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/url_formatter/url_formatter.h"
+#include "components/web_resource/web_resource_pref_names.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
 #import "ios/chrome/app/application_delegate/startup_information.h"
 #include "ios/chrome/app/application_delegate/tab_opening.h"
@@ -24,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/app/application_mode.h"
 #import "ios/chrome/app/chrome_overlay_window.h"
 #import "ios/chrome/app/deferred_initialization_runner.h"
+#include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/browsing_data/browsing_data_remove_mask.h"
 #include "ios/chrome/browser/browsing_data/browsing_data_remover.h"
@@ -39,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/crash_report/crash_report_helper.h"
 #import "ios/chrome/browser/crash_report/crash_restore_helper.h"
 #import "ios/chrome/browser/first_run/first_run.h"
+#include "ios/chrome/browser/geolocation/omnibox_geolocation_controller.h"
 #import "ios/chrome/browser/geolocation/omnibox_geolocation_controller.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/main/browser_list.h"
@@ -838,6 +842,12 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
          selector:@selector(handleFirstRunUIWillFinish)
              name:kChromeFirstRunUIWillFinishNotification
            object:nil];
+  [[NSNotificationCenter defaultCenter]
+      addObserver:self
+         selector:@selector(handleFirstRunUIDidFinish)
+             name:kChromeFirstRunUIDidFinishNotification
+           object:nil];
+
   Browser* browser = self.mainInterface.browser;
   id<ApplicationCommands, BrowsingDataCommands> welcomeHandler =
       static_cast<id<ApplicationCommands, BrowsingDataCommands>>(
@@ -861,7 +871,17 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
                                                 completion:nil];
 }
 
+// Sets a LocalState pref marking the TOS EULA as accepted.
+- (void)markEulaAsAccepted {
+  PrefService* prefs = GetApplicationContext()->GetLocalState();
+  if (!prefs->GetBoolean(prefs::kEulaAccepted))
+    prefs->SetBoolean(prefs::kEulaAccepted, true);
+  prefs->CommitPendingWrite();
+}
+
 - (void)handleFirstRunUIWillFinish {
+  [self markEulaAsAccepted];
+
   DCHECK(self.sceneState.presentingFirstRunUI);
   _firstRunUIBlocker.reset();
   self.sceneState.presentingFirstRunUI = NO;
@@ -872,6 +892,19 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
   if (self.sceneState.activationLevel >= SceneActivationLevelForegroundActive) {
     [self handleExternalIntents];
   }
+}
+
+// Handles the notification that first run modal dialog UI completed.
+- (void)handleFirstRunUIDidFinish {
+  [[NSNotificationCenter defaultCenter]
+      removeObserver:self
+                name:kChromeFirstRunUIDidFinishNotification
+              object:nil];
+
+  // As soon as First Run has finished, give OmniboxGeolocationController an
+  // opportunity to present the iOS system location alert.
+  [[OmniboxGeolocationController sharedInstance]
+      triggerSystemPromptForNewUser:YES];
 }
 
 // Presents the sign-in upgrade promo if is relevant and possible.
