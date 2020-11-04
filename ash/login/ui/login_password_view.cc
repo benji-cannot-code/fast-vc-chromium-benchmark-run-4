f76b42b89c18bf89ac8fb8a4f9b9e0240be503ab
Renato Silva <rrsilva@google.com>
2020-11-04 14:22:21 +0000

    CrOS - Prevent multiple login attempts
    LoginPinInput and LoginPasswordView view should not accept digits
    from the pin pad when they are read only. This fixes an issue
    that would trigger multiple authentication attempts when the digits
    are inserted too fast on the pin pad when using a PIN.
    
    Additionally, these elements will make themselves read only prior
    to invoking an authentication attempt and prevent any further
    attempts until they are no longer ReadOnly.
    
    Fixed: 1133902
    Bug: 1136379
    Change-Id: I12dcb158b856ac1407f55220af583e9df2a39b52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2489883
    Reviewed-by: Thomas Tellier <tellier@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/master@{#823973}

--- ash/login/ui/login_password_view.cc
+++ ash/login/ui/login_password_view.cc
@@ -656,6 +656,9 @@ void LoginPasswordView::Clear() {
 }
 
 void LoginPasswordView::InsertNumber(int value) {
+  if (textfield_->GetReadOnly())
+    return;
+
   if (!textfield_->HasFocus()) {
     // RequestFocus on textfield to activate cursor.
     textfield_->RequestFocus();
@@ -802,6 +805,7 @@ void LoginPasswordView::SubmitPassword() {
   DCHECK(IsPasswordSubmittable());
   if (textfield_->GetReadOnly())
     return;
+  SetReadOnly(true);
   on_submit_.Run(textfield_->GetText());
 }
 
