77658aa7dd935e9ddd6cef98557276afd03c8a47
Sorin Jianu <sorin@chromium.org>
2020-11-04 15:17:07 +0000

    Handle an ill formed response from UpdateClient::CrxDataCallback.
    There could be executions path in the code where early returns in
    the callback implementations return an empty vector instead of
    a vector of CrxComponent of the same size as the input parameter
    of the callback.
    
    When this occurs, the original code indexes out of bounds and crashes.
    
    With the fix, if the callback returns fewer values than the number of
    components checked for updates, the UpdateClient::Update call is
    rejected by the update engine, and completed with a specific error code.
    
    Bug: 1144880
    Change-Id: If45910c7c9ea0ca940b2d5a3d936b96a31d9c684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2518001
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823985}

--- components/update_client/update_engine.h
+++ components/update_client/update_engine.h
@@ -128,7 +128,6 @@ struct UpdateContext : public base::RefCountedThreadSafe<UpdateContext> {
       scoped_refptr<Configurator> config,
       bool is_foreground,
       const std::vector<std::string>& ids,
-      UpdateClient::CrxDataCallback crx_data_callback,
       UpdateClient::CrxStateChangeCallback crx_state_change_callback,
       const UpdateEngine::NotifyObserversCallback& notify_observers_callback,
       UpdateEngine::Callback callback,
@@ -150,9 +149,6 @@ struct UpdateContext : public base::RefCountedThreadSafe<UpdateContext> {
 
   // Contains the map of ids to components for all the CRX in this context.
   IdToComponentPtrMap components;
-
-  // Called before an update check, when update metadata is needed.
-  UpdateEngine::CrxDataCallback crx_data_callback;
 
   // Called when the observable state of the CRX component has changed.
   UpdateClient::CrxStateChangeCallback crx_state_change_callback;
