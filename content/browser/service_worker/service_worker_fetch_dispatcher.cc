d000c76fffd402debc62a6fc8a0f2e9ed2220a81
Asami Doi <asamidoi@chromium.org>
2020-11-04 07:10:13 +0000

    ServiceWorker: Consider sites that take more than the timeout as offline capable.
    When a timeout happens in ServiceWorkerEventQueue, the error status
    transfers to ServiceWorkerFetchDispatcher::OnFetchEventFinished(), but
    the callback of an offline capability checker
    (ServiceWorkerOfflineCapabilityChecker::OnFetchResult) can't catch the
    error status.
    
    This CL set a flag `is_timeout_event_` when the timeout happens and if
    the flag is set, call DidFail() with the error status instead of
    DidFinish() which is called in a successful case.
    
    Bug: 965802
    Change-Id: I9b38c8332f3bfc539d16b0f101143e7af3db66f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2437081
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823905}

--- content/browser/service_worker/service_worker_fetch_dispatcher.cc
+++ content/browser/service_worker/service_worker_fetch_dispatcher.cc
@@ -624,6 +624,7 @@ void ServiceWorkerFetchDispatcher::DispatchFetchEvent() {
   version_->endpoint()->DispatchFetchEventForMainResource(
       std::move(params), std::move(pending_response_callback),
       base::BindOnce(&ServiceWorkerFetchDispatcher::OnFetchEventFinished,
+                     weak_factory_.GetWeakPtr(),
                      base::Unretained(version_.get()), event_finish_id,
                      url_loader_assets_));
 }
@@ -782,12 +783,14 @@ bool ServiceWorkerFetchDispatcher::IsEventDispatched() const {
   return request_.is_null();
 }
 
-// static
 void ServiceWorkerFetchDispatcher::OnFetchEventFinished(
     ServiceWorkerVersion* version,
     int event_finish_id,
     scoped_refptr<URLLoaderAssets> url_loader_assets,
     blink::mojom::ServiceWorkerEventStatus status) {
+  if (status == blink::mojom::ServiceWorkerEventStatus::TIMEOUT) {
+    DidFail(blink::ServiceWorkerStatusCode::kErrorTimeout);
+  }
   version->FinishRequest(
       event_finish_id,
       status != blink::mojom::ServiceWorkerEventStatus::ABORTED);
