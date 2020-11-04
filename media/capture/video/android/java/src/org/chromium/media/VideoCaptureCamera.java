d77c6a48c11d81838df314b3f857b9310f04121c
Sergey Ulanov <sergeyu@chromium.org>
2020-11-04 22:57:06 +0000

    [Android] Cache list of supported formats when enumerating cameras
    VideoCaptureDeviceFactoryAndroid was enumerating list of supported
    formats for each device every time when enumerating devices. This was
    problematic on some devices where opening a camera breaks existing
    stream for that camera. This CL adds cache of supported formats in
    VideoCaptureDeviceFactoryAndroid. This allows to workaround the bug
    by enumerating formats only once for each device. Also updated
    VideoCaptureFactory.java to make it possible to get unique device
    IDs, not just indices.
    
    Bug: 1138608
    Change-Id: Ib397b6cbe1f3e343fbb1e2187e91859576b6acc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494030
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824190}

--- media/capture/video/android/java/src/org/chromium/media/VideoCaptureCamera.java
+++ media/capture/video/android/java/src/org/chromium/media/VideoCaptureCamera.java
@@ -223,6 +223,10 @@ public class VideoCaptureCamera
                                   : "back");
     }
 
+    static String getDeviceId(int id) {
+        return Integer.toString(id);
+    }
+
     static VideoCaptureFormat[] getDeviceSupportedFormats(int id) {
         android.hardware.Camera camera;
         try {
