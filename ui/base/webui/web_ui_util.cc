311122d7c9d06c0aab650f40a859ecc59e9620b5
Alexander Dunaev <adunaev@igalia.com>
2020-11-04 05:33:38 +0000

    [ozone] Enabled requesting the font name.
    webui::GetFontFamily() had a Linux-specific piece of logic that was
    disabled for Ozone because use of pangocairo was not enabled for Ozone.
    However, since [1] pangocairo is enabled for desktop Linux Ozone
    platforms.
    
    This CL removes the check for Ozone in webui::GetFontFamily().
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2366901
    
    Bug: 320050
    Change-Id: I2fe8a9cc12db183e5299f953f720c4241cb43cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505752
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#823894}

--- ui/base/webui/web_ui_util.cc
+++ ui/base/webui/web_ui_util.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/template_expressions.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/base/window_open_disposition.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/font.h"
@@ -33,10 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include "base/win/windows_version.h"
-#endif
-
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-#include "ui/base/ui_base_features.h"
 #endif
 
 namespace webui {
@@ -227,17 +222,13 @@ void AppendWebUiCssTextDefaults(std::string* html) {
 std::string GetFontFamily() {
   std::string font_family = l10n_util::GetStringUTF8(IDS_WEB_FONT_FAMILY);
 
-// TODO(dnicoara) Remove Ozone check when PlatformFont support is introduced
-// into Ozone: crbug.com/320050
 #if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-  if (!features::IsUsingOzonePlatform()) {
-    std::string font_name = ui::ResourceBundle::GetSharedInstance()
-                                .GetFont(ui::ResourceBundle::BaseFont)
-                                .GetFontName();
-    // Wrap |font_name| with quotes to ensure it will always be parsed correctly
-    // in CSS.
-    font_family = "\"" + font_name + "\", " + font_family;
-  }
+  std::string font_name = ui::ResourceBundle::GetSharedInstance()
+                              .GetFont(ui::ResourceBundle::BaseFont)
+                              .GetFontName();
+  // Wrap |font_name| with quotes to ensure it will always be parsed correctly
+  // in CSS.
+  font_family = "\"" + font_name + "\", " + font_family;
 #endif
 
   return font_family;
