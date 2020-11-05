30338e0bd65902d5771f4b0c590156c834274a47
Tim Judkins <tjudkins@chromium.org>
2020-11-05 18:02:56 +0000

    [Extensions Bindings] Support returns_async in extension schemas
    (Based on previous work from https://crrev.com/c/2144684 )
    This CL is a starting point for supporting a returns_async parameter in
    Extension schemas, which is a step in supporting promise-based APIs. It
    adds a new property to the Model in json_schema_compiler, returns_async,
    which contains information about what the API function is expected to
    return.
    
    returns_async is used instead of Model.callback, and can be created
    either from a callback specification or a specific "returns_async"
    entry in the schema.
    
    It also modifies the doc generation code to handle this new property,
    displaying it as a callback at the end of the schema. This should
    result in there being no visible difference to the docs when an API is
    rolled over to using returns_async, other than previously required
    callbacks being displayed as optional.
    
    In order to do end-to-end tests, this CL also moves over tabs.create,
    tabs.duplicate and tabs.query to use the new schema format, thereby
    enabling promise support on them.
    
    
    Bug: 978538
    Change-Id: I43f5965eb8973f0d9fd25d46f1f1ae7ffa346357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2430291
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Tim Judkins <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824488}

---
+++ tools/json_schema_compiler/test/returns_async_unittest.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "tools/json_schema_compiler/test/returns_async.h"
+
+#include <memory>
+#include <utility>
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+// Tests that the returns async schema format behaves similar to how a callback
+// parameter would. See callbacks_unittest.cc for the standard callback
+// behavior.
+TEST(JsonSchemaCompilerReturnsAsyncTest, ReturnsObjectResultCreate) {
+  test::api::returns_async::ReturnsObject::Results::SomeObject some_object;
+  some_object.state = test::api::returns_async::ENUMERATION_FOO;
+  base::Value results = base::Value::FromUniquePtrValue(
+      test::api::returns_async::ReturnsObject::Results::Create(some_object));
+
+  base::Value expected_dict = base::Value(base::Value::Type::DICTIONARY);
+  expected_dict.SetKey("state", base::Value("foo"));
+  base::Value expected = base::Value(base::Value::Type::LIST);
+  expected.Append(std::move(expected_dict));
+  EXPECT_EQ(expected, results);
+}
