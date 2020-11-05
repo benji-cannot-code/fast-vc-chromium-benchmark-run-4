bf6d03db826ff17c685c4d0b976118fcfbb36f68
Xiaodan Zhu <zxdan@chromium.org>
2020-11-05 00:00:06 +0000

    Turn on the reverse scrolling gesture feature.
    This CL enable the feature of reverse scrolling gestures.
    
     - Enable reverse scrolling gesture feature.
     - Get rid of the notification for overview gestures.
     - Add toast for the gestures entering overview when the natural
     scrolling is OFF.
    
    Bug: 1107183
    Change-Id: Ibbabca1b5deb5b37c8b697ff732f5312519e3adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2510350
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824219}

--- ash/ash_prefs.cc
+++ ash/ash_prefs.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/touch/touch_devices_controller.h"
 #include "ash/wallpaper/wallpaper_controller_impl.h"
 #include "ash/wm/desks/desks_restore_util.h"
-#include "ash/wm/gestures/wm_gesture_handler.h"
 #include "chromeos/components/quick_answers/public/cpp/quick_answers_prefs.h"
 #include "chromeos/constants/chromeos_pref_names.h"
 #include "chromeos/services/assistant/public/cpp/assistant_prefs.h"
@@ -76,7 +75,6 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry, bool for_test) {
   ShelfController::RegisterProfilePrefs(registry);
   TouchDevicesController::RegisterProfilePrefs(registry, for_test);
   tray::VPNListView::RegisterProfilePrefs(registry);
-  WmGestureHandler::RegisterProfilePrefs(registry);
   MediaTray::RegisterProfilePrefs(registry);
 
   // Provide prefs registered in the browser for ash_unittests.
