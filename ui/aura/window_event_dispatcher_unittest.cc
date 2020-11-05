536e91314e28b283d7d0bf06a9b5436383c253ce
Jihwan Marc Kim <bluewhale.marc@gmail.com>
2020-11-05 23:35:26 +0000

    Rename WebGestureEvent::is_source_touch_event_set_non_blocking.
    Rename is_source_touch_event_set_non_blocking to
    is_source_touch_event_set_blocking
    
    Bug: 849420
    Change-Id: I216b9103ca29f11f041503a9746d2cf69bf0c1f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2489775
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824637}

--- ui/aura/window_event_dispatcher_unittest.cc
+++ ui/aura/window_event_dispatcher_unittest.cc
@@ -1010,7 +1010,7 @@ TEST_F(WindowEventDispatcherTest, CallToProcessedTouchEvent) {
 
   host()->dispatcher()->ProcessedTouchEvent(
       0, window.get(), ui::ER_UNHANDLED,
-      false /* is_source_touch_event_set_non_blocking */);
+      false /* is_source_touch_event_set_blocking */);
 }
 
 // This event handler requests the dispatcher to start holding pointer-move
@@ -2874,7 +2874,7 @@ class AsyncWindowDelegate : public test::TestWindowDelegate {
     event->DisableSynchronousHandling();
     dispatcher_->ProcessedTouchEvent(
         event->unique_event_id(), window_, ui::ER_UNHANDLED,
-        false /* is_source_touch_event_set_non_blocking */);
+        false /* is_source_touch_event_set_blocking */);
     event->StopPropagation();
   }
 
--- ui/events/gestures/gesture_recognizer_impl.h
+++ ui/events/gestures/gesture_recognizer_impl.h
@@ -82,7 +82,7 @@ class EVENTS_EXPORT GestureRecognizerImpl : public GestureRecognizer,
 
   Gestures AckTouchEvent(uint32_t unique_event_id,
                          ui::EventResult result,
-                         bool is_source_touch_event_set_non_blocking,
+                         bool is_source_touch_event_set_blocking,
                          GestureConsumer* consumer) override;
 
   void CancelActiveTouchesExceptImpl(GestureConsumer* not_cancelled);
