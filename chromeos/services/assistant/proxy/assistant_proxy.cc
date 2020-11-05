8f19863d13fa6ca0172aee05c0f440a1bd185e5a
Jeroen Dhollander <jeroendh@google.com>
2020-11-05 18:23:38 +0000

    Move |AssistantManagerController| to |AssistantProxy|
    There are a few changes in this CL:
       - |AssistantManagerController| is renamed to |ServiceController| and
         now owned by |AssistantProxy|.
       - |AssistantManagerController| now has a Stop() method instead of
         being destroyed.
    
    All these changes are there to bring |AssistantManagerController| more
    in line with the planned API for the mojom service.
    
    Functionally, the biggest change is that the controller now has a
    |state_| field. This is used to enforce (DCHECK) we never end up in an
    inconsistent state. Note that this was and is enforced by
    |AssistantManagerServiceImpl|. I just added the |state_| field so the
    controller can now check and enforce it itself, making the class a lot
    easier to reason about.
    
    Note that for now there is no separate build target yet for the |proxy|
    classes. This will be changed in a later CL, but it requires some more
    detangling of the internal dependencies/objects, so I figured that was
    better left out for now.
    
    Bug: b/171748795
    Test: chromeos_unittest --gtest_filter="AssistantManagerServiceTest.*"
    Change-Id: Ib6c2ba4cb84eaaf3af5129f1d45f152d428e6a42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2518201
    Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824498}

---
+++ chromeos/services/assistant/proxy/assistant_proxy.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/assistant/proxy/assistant_proxy.h"
+
+#include "base/check.h"
+#include "chromeos/services/assistant/proxy/service_controller.h"
+
+namespace chromeos {
+namespace assistant {
+
+AssistantProxy::AssistantProxy()
+    : service_controller_(std::make_unique<ServiceController>()) {}
+
+AssistantProxy::~AssistantProxy() = default;
+
+ServiceController& AssistantProxy::service_controller() {
+  DCHECK(service_controller_);
+  return *service_controller_;
+}
+
+}  // namespace assistant
+}  // namespace chromeos
---
+++ chromeos/services/assistant/proxy/assistant_proxy.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_SERVICES_ASSISTANT_PROXY_ASSISTANT_PROXY_H_
+#define CHROMEOS_SERVICES_ASSISTANT_PROXY_ASSISTANT_PROXY_H_
+
+#include <memory>
+
+namespace chromeos {
+namespace assistant {
+
+class ServiceController;
+
+// The proxy to the Assistant service, which serves as the main
+// access point to the entire Assistant API.
+class AssistantProxy {
+ public:
+  AssistantProxy();
+  AssistantProxy(AssistantProxy&) = delete;
+  AssistantProxy& operator=(AssistantProxy&) = delete;
+  ~AssistantProxy();
+
+  // Returns the controller that manages starting and stopping of the Assistant
+  // service.
+  ServiceController& service_controller();
+
+ private:
+  std::unique_ptr<ServiceController> service_controller_;
+};
+
+}  // namespace assistant
+}  // namespace chromeos
+
+#endif  // CHROMEOS_SERVICES_ASSISTANT_PROXY_ASSISTANT_PROXY_H_
