9e1e674c6816433cb04aeaea4b0203fe1e9101e1
Victor Costan <pwnall@chromium.org>
2020-11-05 20:49:54 +0000

    AppCache: Add holder for AppCacheQuotaClient in AppCacheServiceImpl.
    Before this CL, AppCacheServiceImpl owned (as much as a ref-counted
    class can be owned) AppCacheQuotaClient directly, via a scoped_refptr.
    This introduces the following complexity:
    
    * AppCacheQuotaClient (a complex class) is constructed on the UI thread,
      and accessed later on the IO thread.
    
    * AppCacheStorageImpl and AppCacheStorageImpl (two complex classes) live
      on the UI thread, but are responsible for thread-hopping to the IO
      thread when accessing AppCacheQuotaClient.
    
    * The owning reference for AppCacheQuotaClient lives in the UI thread,
      which forces AppCacheQuotaClient to inherit from RefCountedThreadSafe.
      This is the main reason why QuotaClient is ref-counted.
    
    This CL introduces a QuotaClientHolder class that's internal to
    AppCacheServiceImpl. This class lives on the UI thread, and takes on the
    responsibility for all thread-hopping needed to access
    AppCacheQuotaClient exclusively on the IO thread.
    
    Changing QuotaClient's ownership model away from ref-counting is left
    for future CLs, in the interest of keeping this CL manageable.
    
    This CL does not introduce any behavior changes.
    
    Bug: 1016065
    Change-Id: I46d7029d054a674c9252fe4b975fef17cabde010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519742
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824559}

--- content/browser/appcache/appcache_storage.cc
+++ content/browser/appcache/appcache_storage.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/appcache/appcache_disk_cache_ops.h"
 #include "content/browser/appcache/appcache_response_info.h"
 #include "content/browser/appcache/appcache_service_impl.h"
-#include "storage/browser/quota/quota_client.h"
+#include "storage/browser/quota/quota_client_type.h"
 #include "storage/browser/quota/quota_manager_proxy.h"
 #include "third_party/blink/public/mojom/quota/quota_types.mojom.h"
 
