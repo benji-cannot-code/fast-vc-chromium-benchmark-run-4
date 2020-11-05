4c13fce618cc599c442af2e7492cacd003bf523d
Peter Kvitek <kvitekp@chromium.org>
2020-11-05 14:24:20 +0000

    [Headless] Moved remaining headless tests from /lib to /test.
    Headless tests were scattered between /lib and /test directories.
    Moving them to where they all belong helps to keep things nice and tidy.
    
    Change-Id: I0aee0ae4da8e1f19dd45beb6cda2f108d9ec26a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2511751
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824409}

--- headless/test/headless_web_contents_browsertest.cc
+++ headless/test/headless_web_contents_browsertest.cc
@@ -992,9 +992,7 @@ class HeadlessWebContentsBeginFrameControlBasicTest
     return "/blue_page.html";
   }
 
-  void OnNeedsBeginFrame() override {
-    BeginFrame(true);
-  }
+  void OnNeedsBeginFrame() override { BeginFrame(true); }
 
   void OnFrameFinished(std::unique_ptr<headless_experimental::BeginFrameResult>
                            result) override {
