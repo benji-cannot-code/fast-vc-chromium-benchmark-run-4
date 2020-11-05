daf78db80112f228f4e9d79245e193cb39b33ed2
Ben Kelly <wanderview@chromium.org>
2020-11-05 03:34:18 +0000

    URLPattern: Translate path-to-regexp lexer to c++.
    This CL translates the path-to-regexp lexer from typescript to c++.
    Its based on the code here:
    
    https://github.com/pillarjs/path-to-regexp/blob/125c43e6481f68cc771a5af22b914acdb8c5ba1f/src/index.ts#L1-L124
    
    We deviate from path-to-regexp here in only how we refer to "(foo)"
    groups in the pattern.  Upstream these are referred to as `PATTERN` type
    tokens.  This CL uses the term `kRegex` instead to avoid confusion with
    the overall urlpattern name.
    
    This CL also includes extensive unit tests for the lexer output.
    
    Bug: 1141510
    Change-Id: Ifdc0fb0ea2454b7df8cd72785b72629ee6f204b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2506453
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824276}

--- third_party/liburlpattern/parse.cc
+++ third_party/liburlpattern/parse.cc
@@ -6,31 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/liburlpattern/parse.h"
 
-#include <string>
-#include "third_party/abseil-cpp/absl/strings/str_format.h"
 #include "third_party/liburlpattern/pattern.h"
+#include "third_party/liburlpattern/tokenize.h"
 
 namespace liburlpattern {
 
-namespace {
-
-bool IsValidChar(char c) {
-  // Characters should be valid ASCII code points:
-  // https://infra.spec.whatwg.org/#ascii-code-point
-  return c >= 0x0000 && c <= 0x007f;
-}
-
-}  // namespace
-
 absl::StatusOr<Pattern> Parse(absl::string_view pattern) {
-  for (char c : pattern) {
-    if (!IsValidChar(c)) {
-      return absl::InvalidArgumentError(absl::StrFormat(
-          "Invalid character '%c' in pattern '%s'.", c, pattern));
-    }
+  auto result = Tokenize(pattern);
+  if (!result.ok())
+    return result.status();
 
-    // TODO: Implement actual pattern parsing.
-  }
+  // TODO: Implement actual pattern parsing.
 
   return Pattern();
 }
