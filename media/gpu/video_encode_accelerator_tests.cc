0959470a14873b8cc3cbffc43cab4fdacb0ba7a4
Hirokazu Honda <hiroh@chromium.org>
2020-11-05 04:31:52 +0000

    media/gpu/video_encode_accelerator_tests: Rename bitrate check test cases
    This CL changes the name of bitrate check test cases, so that
    they have the common prefix, "Bitrate." This helps to understand
    the tests checks bitrates and also runs only the bitrate check
    tests.
    
    pBug: None
    Test: video_encode_accelerator_tests --gtest_filter=VideoEncoderTest.BitrateCheck*
    Change-Id: I4ae5a3a68702341b4661965dc39967bad23240d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519334
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Auto-Submit: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: David Staessens <dstaessens@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824288}

--- media/gpu/video_encode_accelerator_tests.cc
+++ media/gpu/video_encode_accelerator_tests.cc
@@ -317,7 +317,7 @@ TEST_F(VideoEncoderTest, BitrateCheck) {
               kBitrateTolerance * config.bitrate);
 }
 
-TEST_F(VideoEncoderTest, DynamicBitrateChange) {
+TEST_F(VideoEncoderTest, BitrateCheck_DynamicBitrate) {
   auto config = GetDefaultConfig();
   config.num_frames_to_encode = kNumFramesToEncodeForBitrateCheck * 2;
   auto encoder = CreateVideoEncoder(g_env->Video(), config);
@@ -348,7 +348,7 @@ TEST_F(VideoEncoderTest, DynamicBitrateChange) {
   EXPECT_TRUE(encoder->WaitForBitstreamProcessors());
 }
 
-TEST_F(VideoEncoderTest, DynamicFramerateChange) {
+TEST_F(VideoEncoderTest, BitrateCheck_DynamicFramerate) {
   auto config = GetDefaultConfig();
   config.num_frames_to_encode = kNumFramesToEncodeForBitrateCheck * 2;
   auto encoder = CreateVideoEncoder(g_env->Video(), config);
