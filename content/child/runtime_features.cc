d885f2770aa23057549d2a0adf6c36e2d03650ce
Xiaocheng Hu <xiaochengh@chromium.org>
2020-11-05 23:36:52 +0000

    Prepare CSSKeyframesMemoryReduction for finch experiment
    This patch adds a Chromium feature for CSSKeyframesMemoryReduction and
    enables it in field trial testing config. This is preparation for a
    Canary/Dev finch experiment.
    
    Bug: 1141814
    Change-Id: I03c1c9beca18bdf4b286704f60b6895f60835d12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2518097
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824639}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -339,6 +339,8 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
           {"BlockHTMLParserOnStyleSheets",
            blink::features::kBlockHTMLParserOnStyleSheets},
           {"CSSColorSchemeUARendering", features::kCSSColorSchemeUARendering},
+          {"CSSKeyframesMemoryReduction",
+           blink::features::kCSSKeyframesMemoryReduction},
           {"CSSReducedFontLoadingLayoutInvalidations",
            blink::features::kCSSReducedFontLoadingLayoutInvalidations},
           {"CSSMatchedPropertiesCacheDependencies",
