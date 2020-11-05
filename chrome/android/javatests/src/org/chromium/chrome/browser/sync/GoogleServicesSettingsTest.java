125050ff7b9d7b6c4e3c0917afe232e04f1f0790
Tanmoy Mollik <triploblastic@chromium.org>
2020-11-05 16:36:00 +0000

    [Android] Rename offer_chrome_signin_* string ids to allow_chrome_signin_*
    This change makes the id names more consistent to the representing
    strings.
    
    Bug: 1139368
    Change-Id: I235bfc51c59c2b5b3f79489d8be1ec398c352888
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2513594
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824442}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/GoogleServicesSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/GoogleServicesSettingsTest.java
@@ -89,7 +89,7 @@ public class GoogleServicesSettingsTest {
                         GoogleServicesSettings.PREF_ALLOW_SIGNIN);
         Assert.assertTrue("Chrome Signin should be allowed", allowChromeSignin.isChecked());
 
-        onView(withText(R.string.offer_chrome_signin_title)).perform(click());
+        onView(withText(R.string.allow_chrome_signin_title)).perform(click());
         TestThreadUtils.runOnUiThreadBlocking(
                 ()
                         -> Assert.assertNull("Account should be signed out!",
@@ -116,7 +116,7 @@ public class GoogleServicesSettingsTest {
                         GoogleServicesSettings.PREF_ALLOW_SIGNIN);
         Assert.assertTrue("Chrome Signin should be allowed", allowChromeSignin.isChecked());
 
-        onView(withText(R.string.offer_chrome_signin_title)).perform(click());
+        onView(withText(R.string.allow_chrome_signin_title)).perform(click());
         // Accept the sign out Dialog
         onView(withText(R.string.continue_button)).inRoot(isDialog()).perform(click());
         TestThreadUtils.runOnUiThreadBlocking(
