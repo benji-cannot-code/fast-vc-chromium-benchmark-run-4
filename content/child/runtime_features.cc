421169abd682d135d64023e9307da4e946f98359
Alexander Cooper <alcooper@chromium.org>
2020-11-05 17:31:18 +0000

    Remove WebXRReflectionEstimation flag
    WebXRReflectionEstimation is part of the WebXRLightEstimation API;
    however, it was less stable than the rest of the API. The flag was split
    out to enable shipping the rest of the Light Estimation API separately.
    However, there were delays in shipping the rest of the Light Estimation
    API, and the reflection code paths have now stabalized. This change
    remvoes and replaces the reflection-only flag with the general lighting
    estimation flag.
    
    Bug: 1070819
    Change-Id: Ica8ddc47a0eff9cc85876b5a4818650ea9159edf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2518449
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824470}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -212,7 +212,6 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
     {wf::EnableWebXRImageTracking, features::kWebXrIncubations},
     {wf::EnableWebXRLightEstimation, features::kWebXrIncubations},
     {wf::EnableWebXRPlaneDetection, features::kWebXrIncubations},
-    {wf::EnableWebXRReflectionEstimation, features::kWebXrIncubations},
     {wf::EnableWebXRViewportScale, features::kWebXrIncubations},
     {wf::EnableUserActivationSameOriginVisibility,
      features::kUserActivationSameOriginVisibility},
--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -482,10 +482,6 @@ void WebRuntimeFeatures::EnableWebXRPlaneDetection(bool enable) {
   RuntimeEnabledFeatures::SetWebXRPlaneDetectionEnabled(enable);
 }
 
-void WebRuntimeFeatures::EnableWebXRReflectionEstimation(bool enable) {
-  RuntimeEnabledFeatures::SetWebXRReflectionEstimationEnabled(enable);
-}
-
 void WebRuntimeFeatures::EnableWebXRViewportScale(bool enable) {
   RuntimeEnabledFeatures::SetWebXRViewportScaleEnabled(enable);
 }
