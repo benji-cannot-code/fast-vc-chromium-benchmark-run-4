132d07c585825e990315c19acdc12039c149c91d
Morten Stenshorne <mstensho@chromium.org>
2020-11-05 19:58:35 +0000

    [LayoutNG] caretRangeFromPoint support in multicol.
    This introduces full PositionForPoint() support to
    NGPhysicalBoxFragment. Its behavior differs slightly from that in legacy
    PositionForPoint(): if the point isn't within any of the child
    fragments, we'll use the nearest one (*really* the nearest one). We kind
    of do something similar in LayoutBlock::PositionForPoint() too, but not
    very accurately. LayoutBlock is very block-offset centric (hardly cares
    about inline offsets at all), which is certainly fine there, but that
    won't work with e.g. multicol, table cells, or some flex items.
    Additionally, LayoutBlock::PositionForPoint() doesn't really pick the
    nearest child, but rather sort of the first child whose block-end border
    edge is past the point. Not sure if anyone depends on this behavior, or
    if what we do now in NGPhysicalBoxFragment is an improvement.
    
    HitTestResult stores the NGBoxFragment that we hit. This will be a
    NGBoxFragment that's generated from the inner node itself, unless the
    inner node is text or non-atomic inline content, in which case it will
    be the NGBoxFragment of the containing block of the inner node.
    
    For now, this functionality is gated on LayoutNGBlockFragmentation,
    because it causes at least one test to regress:
    editing/selection/selectstart-event-crash.html
    
    This fixes 6 caretRangeFromPoint tests, but I think it actually fixes
    more than that (looks like there are some deliberate changes regarding
    which character we hit when pointing above/below the first/last line in
    LayoutNG, which prevents additional tests from passing). Will look into
    that as a follow-up.
    
    Bug: 829028
    Change-Id: I0cc86ecd7e89254c998218ea442944f991b36151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2515399
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824533}

--- third_party/blink/renderer/core/layout/layout_block.cc
+++ third_party/blink/renderer/core/layout/layout_block.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/legacy_layout_tree_walking.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_constraint_space.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_length_utils.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/text_autosizer.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/page/scrolling/root_scroller_controller.h"
@@ -1408,6 +1409,15 @@ static inline bool IsChildHitTestCandidate(LayoutBox* box) {
 PositionWithAffinity LayoutBlock::PositionForPoint(
     const PhysicalOffset& point) const {
   NOT_DESTROYED();
+
+  if (IsLayoutNGObject() && PhysicalFragmentCount() &&
+      NGPhysicalBoxFragment::SupportsPositionForPoint()) {
+    // Layout engine boundary. Enter NG PositionForPoint(). Assert
+    // that we're not block-fragmented here.
+    DCHECK_EQ(PhysicalFragmentCount(), 1u);
+    return GetPhysicalFragment(0)->PositionForPoint(point);
+  }
+
   if (IsTable())
     return LayoutBox::PositionForPoint(point);
 
