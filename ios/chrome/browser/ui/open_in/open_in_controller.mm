151d6ce4c506248eeac67830717e1d205d4ab754
Ewann <ewannpv@google.com>
2020-11-05 10:33:59 +0000

    [iOS][OpenIn] Refactors OpenIn toolbar
    This CL refactors the Open In toolbar and makes the following changes:
    - Updates constraints using autoLayout.
    - Replaces the MDCButton by an UIButton.
    - Button text is no longer in uppercase.
    - Cleans unused variables & methods.
    
    based on: https://crrev.com/c/2517483
    
    Bug: 1145477, 1136837, 1117398
    Change-Id: I636adc502df159c077f3dc44dbf8d86dc6c0cf97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2517574
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824368}

--- ios/chrome/browser/ui/open_in/open_in_controller.mm
+++ ios/chrome/browser/ui/open_in/open_in_controller.mm
@@ -326,7 +326,6 @@ class OpenInControllerBridge
 
   OpenInToolbar* openInToolbar = [self openInToolbar];
   if (!_isOpenInToolbarDisplayed) {
-    [openInToolbar updateBottomMarginHeight];
     [UIView animateWithDuration:kOpenInToolbarAnimationDuration
                      animations:^{
                        [openInToolbar setAlpha:1.0];
--- ios/chrome/browser/ui/open_in/open_in_toolbar.h
+++ ios/chrome/browser/ui/open_in/open_in_toolbar.h
@@ -10,10 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface OpenInToolbar : UIView
 
-- (instancetype)initWithTarget:(id)target action:(SEL)action;
+// Init with the given local target and action.
+- (instancetype)initWithTarget:(id)target
+                        action:(SEL)action NS_DESIGNATED_INITIALIZER;
 
-// Updates the constraint managing the bottom margin height using NamedGuides.
-- (void)updateBottomMarginHeight;
+- (instancetype)initWithFrame:(CGRect)aRect NS_UNAVAILABLE;
+
+- (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
 
 @end
 
