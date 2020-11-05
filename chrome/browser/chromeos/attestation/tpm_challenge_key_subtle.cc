aaaa7cf5f004afcfc643ce14e46b5151fb519b7f
Leo Lai <cylai@google.com>
2020-11-05 05:32:23 +0000

    Tidy-up some leftover after AttestationClient migration.
    There are some inconsistent comments and function calls after the
    migration.
    
    BUG=b:158955123
    TEST=unit_tests.
    
    Change-Id: I1038e52492e5c4a45e422b4d1cf7ac086cc31e77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519638
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Leo Lai <cylai@google.com>
    Cr-Commit-Position: refs/heads/master@{#824302}

--- chrome/browser/chromeos/attestation/tpm_challenge_key_subtle.cc
+++ chrome/browser/chromeos/attestation/tpm_challenge_key_subtle.cc
@@ -406,9 +406,7 @@ void TpmChallengeKeySubtleImpl::GetEnrollmentPreparationsCallback(
   }
 
   ::attestation::GetKeyInfoRequest request;
-  request.set_username(
-      cryptohome::CreateAccountIdentifierFromAccountId(GetAccountId())
-          .account_id());
+  request.set_username(cryptohome::Identification(GetAccountId()).id());
   request.set_key_label(key_name_);
   AttestationClient::Get()->GetKeyInfo(
       request, base::BindOnce(&TpmChallengeKeySubtleImpl::DoesKeyExistCallback,
@@ -510,9 +508,7 @@ void TpmChallengeKeySubtleImpl::GetPublicKey() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   ::attestation::GetKeyInfoRequest request;
-  request.set_username(
-      cryptohome::CreateAccountIdentifierFromAccountId(GetAccountId())
-          .account_id());
+  request.set_username(cryptohome::Identification(GetAccountId()).id());
   request.set_key_label(key_name_);
   AttestationClient::Get()->GetKeyInfo(
       request, base::BindOnce(&TpmChallengeKeySubtleImpl::PrepareKeyFinished,
