4e165df63faa7f2b675ace83b49b92d8cafc579d
Antonio Gomes <tonikitoo@igalia.com>
2020-11-05 20:45:32 +0000

    [ozone/wayland] Fix zaura_shell versioning check
    This CL ensures that the WL client (Chromium/Wayland, Lacros) tells
    the WL server (Exo) the higher Aura Shell protocol version it
    supports, instead of the lower.
    
    With this in place, code is safe against similar crashes observed in
    bugs 1145642 and 1145695, where the Client (Lacros) was telling the
    server (Exo) it supports a version higher than it actually does.
    
    BUG=1145642, 1145695
    
    Change-Id: Ia471748f0f3066eea901c189c06da3c1137c1cdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2518443
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#824556}

--- ui/ozone/platform/wayland/host/wayland_connection.cc
+++ ui/ozone/platform/wayland/host/wayland_connection.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 namespace {
+constexpr uint32_t kMaxAuraShellVersion = 11;
 constexpr uint32_t kMaxCompositorVersion = 4;
 constexpr uint32_t kMaxCursorShapesVersion = 1;
 constexpr uint32_t kMaxGtkPrimarySelectionDeviceManagerVersion = 1;
@@ -67,7 +68,6 @@ constexpr uint32_t kMaxWpPresentationVersion = 1;
 constexpr uint32_t kMaxWpViewporterVersion = 1;
 constexpr uint32_t kMaxTextInputManagerVersion = 1;
 constexpr uint32_t kMaxExplicitSyncVersion = 2;
-constexpr uint32_t kMinAuraShellVersion = 11;
 constexpr uint32_t kMinWlDrmVersion = 2;
 constexpr uint32_t kMinWlOutputVersion = 2;
 constexpr uint32_t kMaxXdgDecorationVersion = 1;
@@ -436,9 +436,9 @@ void WaylandConnection::Global(void* data,
     connection->drm_ =
         std::make_unique<WaylandDrm>(wayland_drm.release(), connection);
   } else if (!connection->zaura_shell_ &&
-             (strcmp(interface, "zaura_shell") == 0) &&
-             version >= kMinAuraShellVersion) {
-    auto zaura_shell = wl::Bind<struct zaura_shell>(registry, name, version);
+             (strcmp(interface, "zaura_shell") == 0)) {
+    auto zaura_shell = wl::Bind<struct zaura_shell>(
+        registry, name, std::min(version, kMaxAuraShellVersion));
     if (!zaura_shell) {
       LOG(ERROR) << "Failed to bind zaura_shell";
       return;
