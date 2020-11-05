179de7163fcb6e6ad6f1dea6126f3d795ab372fc
Vidhan <vidhanj@google.com>
2020-11-05 17:31:23 +0000

    [Autofill][States] Use local_state instead of profile prefs
    AutofillStatesComponentInstallerPolicy tried to use the profile prefs in
    a posted task.
    This caused a use-after-free when the DestroyProfileOnBrowserClose
    flag was enabled.
    
    With this change, the local_state is used for storing the installation
    path of the states data directory instead of the profile prefs directly
    in the AutofillStatesComponentInstallerPolicy::ComponentReady.
    
    More info: https://docs.google.com/document/d/11sLMW5wRH6lAU1xkh28I4kH6oavVItc2UvWA5gKLH4Q/edit?usp=sharing
    
    Bug: 1144276
    Change-Id: I7f9674f6de6bf3a778600aa4f482c8b6bd152fa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2517319
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/master@{#824471}

--- chrome/browser/component_updater/component_updater_prefs.cc
+++ chrome/browser/component_updater/component_updater_prefs.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/component_updater/component_updater_prefs.h"
 
+#include "chrome/browser/component_updater/autofill_states_component_installer.h"
 #include "chrome/browser/component_updater/chrome_component_updater_configurator.h"
 #include "chrome/browser/component_updater/recovery_component_installer.h"
 #include "chrome/common/buildflags.h"
@@ -30,6 +31,8 @@ void RegisterPrefs(PrefRegistrySimple* registry) {
 #if !defined(OS_ANDROID)
   RegisterPrefsForSodaComponent(registry);
 #endif
+
+  AutofillStatesComponentInstallerPolicy::RegisterPrefs(registry);
 }
 
 }  // namespace component_updater
--- components/autofill/core/browser/autofill_test_utils.cc
+++ components/autofill/core/browser/autofill_test_utils.cc
@@ -90,6 +90,7 @@ std::unique_ptr<PrefService> PrefServiceForTesting() {
   registry->RegisterBooleanPref(
       RandomizedEncoder::kUrlKeyedAnonymizedDataCollectionEnabled, false);
   registry->RegisterBooleanPref(::prefs::kMixedFormsWarningsEnabled, true);
+  registry->RegisterStringPref(prefs::kAutofillStatesDataDir, "");
   return PrefServiceForTesting(registry.get());
 }
 
