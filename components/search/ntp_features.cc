1d0b51e8d08024d7cbc3bcc1a41326f96eccb91f
Moe Ahmadi <mahmadi@chromium.org>
2020-11-05 19:47:56 +0000

    [NTP][RQ] Feature param to control insert position of repeatable queries
    This Feature param controls the position, with respect to the NTP MV
    tiles, at which the repeatable queries must be inserted.
    
    The available variations are either at the start or at the end of the
    MV tiles. These two variations are added to about:flags entry for the
    repeatable queries.
    
    Fixed: 1138578
    Change-Id: I65a5216ac77756027e7240d921abaa1ba9b96ac0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2520420
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Ramya Nagarajan <ramyan@chromium.org>
    Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Ramya Nagarajan <ramyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824526}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -81,6 +81,8 @@ const char kNtpRepeatableQueriesRecencyHalfLifeSecondsParam[] =
     "NtpRepeatableQueriesRecencyHalfLifeSeconds";
 const char kNtpRepeatableQueriesFrequencyExponentParam[] =
     "NtpRepeatableQueriesFrequencyExponent";
+const char kNtpRepeatableQueriesInsertPositionParam[] =
+    "NtpRepeatableQueriesInsertPosition";
 
 base::Time GetLocalHistoryRepeatableQueriesAgeThreshold() {
   const base::TimeDelta kLocalHistoryRepeatableQueriesAgeThreshold =
@@ -127,6 +129,13 @@ double GetLocalHistoryRepeatableQueriesFrequencyExponent() {
   }
 
   return param_value_as_double;
+}
+
+RepeatableQueriesInsertPosition GetRepeatableQueriesInsertPosition() {
+  std::string param_value = base::GetFieldTrialParamValueByFeature(
+      kNtpRepeatableQueries, kNtpRepeatableQueriesInsertPositionParam);
+  return param_value == "end" ? RepeatableQueriesInsertPosition::kEnd
+                              : RepeatableQueriesInsertPosition::kStart;
 }
 
 }  // namespace ntp_features
