5edff52c5c39d54a6df31c8b437be286f8fbc1ca
Takumi Fujimoto <takumif@chromium.org>
2020-11-05 02:47:31 +0000

    Change the message shown for incompatible DIAL devices
    When a DIAL device is incompatible with a website, change the message
    shown to "Available for specific video sites" to make it clearer why
    it's disabled.
    
    Bug: 1112609, b/162794109
    Change-Id: I68df407f276a2fdb2eb8c215e73eefbbfde711c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2486560
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: John Williams <jrw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824260}

--- chrome/browser/ui/media_router/ui_media_sink.h
+++ chrome/browser/ui/media_router/ui_media_sink.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "chrome/browser/ui/media_router/media_cast_mode.h"
 #include "components/media_router/common/issue.h"
+#include "components/media_router/common/media_route_provider_helper.h"
 #include "components/media_router/common/media_sink.h"
 #include "url/gurl.h"
 
@@ -57,6 +58,9 @@ struct UIMediaSink {
 
   // The icon to use for the sink.
   SinkIconType icon_type = SinkIconType::GENERIC;
+
+  // The provider of the sink.
+  MediaRouteProviderId provider = MediaRouteProviderId::UNKNOWN;
 
   // The current state of the media sink.
   UIMediaSinkState state = UIMediaSinkState::AVAILABLE;
