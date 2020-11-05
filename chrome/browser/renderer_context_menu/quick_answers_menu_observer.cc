69674d6be425a75e44b5af9b40f80b8189d050ff
Yue Li <updowndota@google.com>
2020-11-05 10:05:10 +0000

    Quick Answers: Add context menu baseline metrics
    Bug: b/170769672
    Test: None
    Change-Id: I482d2812b1191df2b5a35a7c57024dadc3f87a99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2487561
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824355}

--- chrome/browser/renderer_context_menu/quick_answers_menu_observer.cc
+++ chrome/browser/renderer_context_menu/quick_answers_menu_observer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/assistant/controller/assistant_interaction_controller.h"
 #include "ash/public/cpp/quick_answers/controller/quick_answers_controller.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
@@ -112,6 +113,9 @@ void QuickAnswersMenuObserver::OnContextMenuViewBoundsChanged(
 }
 
 void QuickAnswersMenuObserver::OnMenuClosed() {
+  base::UmaHistogramBoolean("QuickAnswers.ContextMenu.Close",
+                            is_other_command_executed_);
+
   if (!quick_answers_controller_)
     return;
 
