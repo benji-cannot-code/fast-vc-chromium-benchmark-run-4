b639157e9451452961e29f678592ff4e60242ba2
Peter Kasting <pkasting@chromium.org>
2020-11-05 02:02:40 +0000

    Change ButtonPressed overrides to callbacks: ash/system/
    Missed a few on the first couple passes...
    
    Bug: 772945
    Change-Id: Ib3e87c2de54c74557113176d0c4f316ef2db4739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519880
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824250}

--- ash/system/phonehub/quick_action_item.cc
+++ ash/system/phonehub/quick_action_item.cc
@@ -39,7 +39,7 @@ QuickActionItem::QuickActionItem(Delegate* delegate,
                                  int label_id,
                                  const gfx::VectorIcon& icon_on,
                                  const gfx::VectorIcon& icon_off)
-    : delegate_(delegate), icon_on_(icon_on), icon_off_(icon_off) {
+    : icon_on_(icon_on), icon_off_(icon_off) {
   SetPreferredSize(kUnifiedFeaturePodSize);
   auto* layout = SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical, gfx::Insets(),
@@ -47,8 +47,13 @@ QuickActionItem::QuickActionItem(Delegate* delegate,
   layout->set_cross_axis_alignment(
       views::BoxLayout::CrossAxisAlignment::kCenter);
 
-  icon_button_ = AddChildView(
-      std::make_unique<FeaturePodIconButton>(this, true /* is_togglable */));
+  icon_button_ = AddChildView(std::make_unique<FeaturePodIconButton>(
+      base::BindRepeating(
+          [](Delegate* delegate, QuickActionItem* item) {
+            delegate->OnButtonPressed(item->IsToggled());
+          },
+          delegate, this),
+      true /* is_togglable */));
 
   auto* label_view = AddChildView(std::make_unique<views::View>());
   label_view->SetLayoutManager(std::make_unique<views::BoxLayout>(
@@ -133,11 +138,6 @@ void QuickActionItem::SetEnabled(bool enabled) {
         AshColorProvider::ContentLayerType::kTextColorPrimary));
     sub_label_->SetEnabledColor(sub_label_color_);
   }
-}
-
-void QuickActionItem::ButtonPressed(views::Button* sender,
-                                    const ui::Event& event) {
-  delegate_->OnButtonPressed(IsToggled());
 }
 
 bool QuickActionItem::HasFocus() const {
--- ash/system/phonehub/quick_action_item.h
+++ ash/system/phonehub/quick_action_item.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/system/unified/feature_pod_button.h"
-#include "ui/views/controls/button/button.h"
 
 namespace views {
 class Label;
@@ -17,8 +16,7 @@ class Label;
 namespace ash {
 
 // A toggle button with labels used in the quick action view.
-class ASH_EXPORT QuickActionItem : public views::View,
-                                   public views::ButtonListener {
+class ASH_EXPORT QuickActionItem : public views::View {
  public:
   class Delegate {
    public:
@@ -66,9 +64,6 @@ class ASH_EXPORT QuickActionItem : public views::View,
   // clicked and the labels are greyed out.
   void SetEnabled(bool enabled);
 
-  // views::ButtonListener:
-  void ButtonPressed(views::Button* sender, const ui::Event& event) override;
-
   // views::View:
   bool HasFocus() const override;
   void RequestFocus() override;
@@ -77,8 +72,6 @@ class ASH_EXPORT QuickActionItem : public views::View,
   FeaturePodIconButton* icon_button() const { return icon_button_; }
 
  private:
-  Delegate* delegate_ = nullptr;
-
   // Owned by views hierarchy.
   FeaturePodIconButton* icon_button_ = nullptr;
   const gfx::VectorIcon& icon_on_;
