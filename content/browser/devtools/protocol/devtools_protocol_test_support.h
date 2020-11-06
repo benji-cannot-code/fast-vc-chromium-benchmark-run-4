caa0d49389dc272957a9a261e71c416614240260
Simon Zünd <szuend@chromium.org>
2020-11-06 08:20:38 +0000

    [devtools] Report Trust Token cache hits as successful requests
    Trust Token Redemption can be short-circuited if a valid
    Signed-Redemption-Record (SRR) already exists. In this case, the
    network service aborts the request with a special error code.
    
    In DevTools, this is repoted as a failed request, even though it was
    technically a successful request served from cache. This CL handles
    this case better and emits a loadingFinisehd event instead of a
    loadingFailed event.
    
    R=caseq@chromium.org, sigurds@chromium.org
    
    Bug: chromium:1141652
    Change-Id: Ie0408b7557928722ddb1964c1ad5d8bdf73aa831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2516225
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824778}

--- content/browser/devtools/protocol/devtools_protocol_test_support.h
+++ content/browser/devtools/protocol/devtools_protocol_test_support.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-class DevToolsProtocolTest : public ContentBrowserTest,
+class DevToolsProtocolTest : virtual public ContentBrowserTest,
                              public DevToolsAgentHostClient,
                              public WebContentsDelegate {
  public:
