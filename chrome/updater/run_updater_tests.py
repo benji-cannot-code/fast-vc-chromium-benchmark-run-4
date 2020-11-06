770b3c865b1652626e8fc943f3e309b3e7cb66cc
adoneria <adoneria@google.com>
2020-11-06 18:36:18 +0000

    [updater] populate version info from chrome version file.
    Uses a template to dynamically generate updater version_info.py.
    This is required to install correct updater version during testing.
    Also copy over the generated file to test package so that python
    can read it as a package and successfully import it.
    
    Bug: 1140211
    Change-Id: I10da968690f47fccd35bddb6c5d9f77f89bbce83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2510877
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Anjali Doneria <adoneria@google.com>
    Cr-Commit-Position: refs/heads/master@{#824940}

--- chrome/updater/run_updater_tests.py
+++ chrome/updater/run_updater_tests.py
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import argparse
 import logging
+import os
+import shutil
 import sys
 
 from test.integration_tests.common import path_finder
@@ -16,11 +18,14 @@ class Context(object):
   def __init__(self, build_dir):
     self.build_dir = build_dir
 
+def copy_file(source, destination):
+  shutil.copyfile(source, destination)
 
 def main():
   parser = typ.ArgumentParser()
   parser.add_argument('--build-dir',
                       help='Specifies chromium build directory.')
+  parser.add_argument('--target-gen-dir')
 
   runner = typ.Runner()
 
@@ -37,6 +42,21 @@ def main():
   else:
     level = logging.INFO
   logging.basicConfig(level=level)
+
+  # copy dynamically generated updater version_info.py from
+  # target gen directory to
+  # //chrome/updater/test/integration_tests/updater so that
+  # it can be imported as a module during test runs.
+  target_gen_dir_abs_path = os.path.abspath(runner.args.target_gen_dir)
+  version_file_path = os.path.join(target_gen_dir_abs_path, 'gen',
+                                   'chrome', 'updater', 'version_info.py')
+  if os.path.exists(version_file_path):
+    dest = os.path.join(path_finder.get_integration_tests_dir(),
+                        'updater', 'version_info.py')
+    copy_file(version_file_path, dest)
+  else:
+    logging.info('File not found: %s' % version_file_path)
+    return -1
 
   runner.context = Context(runner.args.build_dir)
   return runner.run()[0]
