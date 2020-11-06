2dcebd231e5d019bad53de29cc1c343af726d0c5
Bettina Dea <bdea@chromium.org>
2020-11-06 01:16:47 +0000

    Disable visual detection if on Android.
    Client side detection is currently enabled
    on Android but there was no check to prevent
    visual detection from being done and is
    causing crashes in PaintPreviewTracker on beta.
    
    Bug: 1146121
    Change-Id: If37cd5abe52ebb25fa7fb7ae126cd934c6a74cf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521353
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824679}

--- chrome/browser/safe_browsing/client_side_detection_host_browsertest.cc
+++ chrome/browser/safe_browsing/client_side_detection_host_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/prefs/pref_service.h"
+#include "components/safe_browsing/buildflags.h"
 #include "components/safe_browsing/core/proto/client_model.pb.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -83,6 +84,7 @@ class ClientSideDetectionHostBrowserTest : public InProcessBrowserTest {
   ~ClientSideDetectionHostBrowserTest() override = default;
 };
 
+#if BUILDFLAG(FULL_SAFE_BROWSING)
 IN_PROC_BROWSER_TEST_F(ClientSideDetectionHostBrowserTest,
                        VerifyVisualFeatureCollection) {
   FakeClientSideDetectionService fake_csd_service;
@@ -140,5 +142,6 @@ IN_PROC_BROWSER_TEST_F(ClientSideDetectionHostBrowserTest,
   EXPECT_CALL(*mock_ui_manager, DisplayBlockingPage(_));
   std::move(fake_csd_service.saved_callback()).Run(page_url, true);
 }
+#endif
 
 }  // namespace safe_browsing
--- components/safe_browsing/content/renderer/phishing_classifier/phishing_classifier.cc
+++ components/safe_browsing/content/renderer/phishing_classifier/phishing_classifier.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "cc/paint/skia_paint_canvas.h"
 #include "components/paint_preview/common/paint_preview_tracker.h"
+#include "components/safe_browsing/buildflags.h"
 #include "components/safe_browsing/content/renderer/phishing_classifier/features.h"
 #include "components/safe_browsing/content/renderer/phishing_classifier/phishing_dom_feature_extractor.h"
 #include "components/safe_browsing/content/renderer/phishing_classifier/phishing_term_feature_extractor.h"
@@ -163,7 +164,11 @@ void PhishingClassifier::DOMExtractionFinished(bool success) {
 
 void PhishingClassifier::TermExtractionFinished(bool success) {
   if (success) {
+#if BUILDFLAG(FULL_SAFE_BROWSING)
     ExtractVisualFeatures();
+#else
+    VisualExtractionFinished(true);
+#endif
   } else {
     RunFailureCallback();
   }
@@ -229,12 +234,15 @@ void PhishingClassifier::VisualExtractionFinished(bool success) {
   verdict->set_client_score(score);
   verdict->set_is_phishing(score >= scorer_->threshold_probability());
 
+#if BUILDFLAG(FULL_SAFE_BROWSING)
   visual_matching_start_ = base::TimeTicks::Now();
-
   scorer_->GetMatchingVisualTargets(
       *bitmap_, std::move(verdict),
       base::BindOnce(&PhishingClassifier::OnVisualTargetsMatched,
                      weak_factory_.GetWeakPtr()));
+#else
+  RunCallback(*verdict);
+#endif
 }
 
 void PhishingClassifier::OnVisualTargetsMatched(
