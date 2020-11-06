2decdc1f1ed6da89b0056019f5940bfb4a5d24f3
Drew Davenport <ddavenport@chromium.org>
2020-11-06 06:25:40 +0000

    sandbox: Allow sched_setaffinity for amd gpu sandbox
    This could be called in mesa on some SoCs to set the affinity of the
    gallium thread from the main thread.
    
    BUG=b:170305550
    TEST=gpu process does not crash
    
    Change-Id: I51aec065750ccbbe71c086ddd2492bc25933d3c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2514066
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Drew Davenport <ddavenport@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824755}

--- sandbox/policy/linux/bpf_cros_amd_gpu_policy_linux.cc
+++ sandbox/policy/linux/bpf_cros_amd_gpu_policy_linux.cc
@@ -36,6 +36,7 @@ CrosAmdGpuProcessPolicy::~CrosAmdGpuProcessPolicy() {}
 ResultExpr CrosAmdGpuProcessPolicy::EvaluateSyscall(int sysno) const {
   switch (sysno) {
     case __NR_fstatfs:
+    case __NR_sched_setaffinity:
     case __NR_sched_setscheduler:
     case __NR_sysinfo:
     case __NR_uname:
