18380a71b3813710c0269612342aa62786ee2cfd
Charlie Hu <chenleihu@google.com>
2020-11-06 21:28:50 +0000

    [CodeHealthRotation] Migrate browser/ui/profile_error_dialog.cc from base::Bind to base::BindOnce
    As |chrome::ShowWarningMessageBoxWithCheckbox| accepts
    |base::OnceCallback| as its last parameter, the call site should use
    |base::BindOnce|.
    
    Bug: 1007635
    Change-Id: I5e437d37ba1573cf552a2a51916a2e34831ffe32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2522212
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Cr-Commit-Position: refs/heads/master@{#825039}

--- chrome/browser/ui/profile_error_dialog.cc
+++ chrome/browser/ui/profile_error_dialog.cc
@@ -65,7 +65,7 @@ void ShowProfileErrorDialog(ProfileErrorType type,
       nullptr, l10n_util::GetStringUTF16(IDS_PROFILE_ERROR_DIALOG_TITLE),
       l10n_util::GetStringUTF16(message_id),
       l10n_util::GetStringUTF16(IDS_PROFILE_ERROR_DIALOG_CHECKBOX),
-      base::Bind(&OnProfileErrorDialogDismissed, diagnostics));
+      base::BindOnce(&OnProfileErrorDialogDismissed, diagnostics));
 #else   // BUILDFLAG(GOOGLE_CHROME_BRANDING)
   chrome::ShowWarningMessageBox(
       nullptr, l10n_util::GetStringUTF16(IDS_PROFILE_ERROR_DIALOG_TITLE),
