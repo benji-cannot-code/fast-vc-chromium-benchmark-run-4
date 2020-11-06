df36ec553a97cdb7355811530cc5edc6d6f35c46
Pavel Yatsuk <pavely@google.com>
2020-11-06 21:29:42 +0000

    [Messages] Introduce native interface for messages
    Native interface includes MessageWrapper and MessageDispatcherBridge.
    MessageWrapper represents a message for native feature code and allows
    feature code to set message properties before enqueueing it. Java side
    of MessageWrapper constructs PropertyModel that maintains property
    values.
    MessageDispatcherBridge enables feature code to interact with Java
    MessageDispatcher to enqueue/dismiss message.
    
    BUG=1123947
    R=lazzzis@chromium.org
    
    Change-Id: I28b58ed080dd63045f2e18f13e3875fabd9764e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2472599
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825040}

---
+++ components/messages/android/message_dispatcher_bridge.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_MESSAGES_ANDROID_MESSAGE_DISPATCHER_BRIDGE_H_
+#define COMPONENTS_MESSAGES_ANDROID_MESSAGE_DISPATCHER_BRIDGE_H_
+
+#include "components/messages/android/message_wrapper.h"
+
+namespace content {
+class WebContents;
+}
+
+namespace messages {
+
+// C++ counterpart to MessageDispatcherBridge.java. Enables C++ feature code to
+// enqueue/dismiss messages with MessageDispatcher.java.
+class MessageDispatcherBridge {
+ public:
+  static void EnqueueMessage(const MessageWrapper& message,
+                             content::WebContents* web_contents);
+  static void DismissMessage(const MessageWrapper& message,
+                             content::WebContents* web_contents);
+};
+
+}  // namespace messages
+
+#endif  // COMPONENTS_MESSAGES_ANDROID_MESSAGE_DISPATCHER_BRIDGE_H_
