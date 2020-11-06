434019f36f80fb5658e00ce8377dffc62f572a1b
Alex Newcomer <newcomer@chromium.org>
2020-11-06 18:22:07 +0000

    cros: Clear ClipboardHistory when Clipboard is cleared
    We clear the clipboard when entering LockedFullscreen.
    LockedFullscreen is a state entered to start a secure test or poll.
    
    We clear the clipboard in these cases to prevent cheating,
    so it also makes sense to clear ClipboardHistory.
    
    In the future we would like to create a new ClipboardHistory and
    restore the old one  after leaving LockedFullscreen, but for now we
    will just clear ClipboardHistory.
    
    Bug: 1145651
    Change-Id: Id5583567375384f2b2bc877f77acf6c054825977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521230
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#824930}

--- ash/clipboard/clipboard_history_controller_impl.h
+++ ash/clipboard/clipboard_history_controller_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/ash_export.h"
+#include "ash/clipboard/clipboard_history.h"
 #include "ash/clipboard/clipboard_history_item.h"
 #include "ash/public/cpp/clipboard_history_controller.h"
 #include "base/memory/weak_ptr.h"
@@ -25,7 +26,6 @@ class Rect;
 
 namespace ash {
 
-class ClipboardHistory;
 class ClipboardHistoryMenuModelAdapter;
 class ClipboardHistoryResourceManager;
 class ClipboardNudgeController;
@@ -33,7 +33,8 @@ class ClipboardNudgeController;
 // Shows a menu with the last few things saved in the clipboard when the
 // keyboard shortcut is pressed.
 class ASH_EXPORT ClipboardHistoryControllerImpl
-    : public ClipboardHistoryController {
+    : public ClipboardHistoryController,
+      public ClipboardHistory::Observer {
  public:
   class Observer : public base::CheckedObserver {
    public:
@@ -88,6 +89,9 @@ class ASH_EXPORT ClipboardHistoryControllerImpl
                 views::MenuAnchorPosition menu_anchor_position,
                 ui::MenuSourceType source_type) override;
   bool CanShowMenu() const override;
+
+  // ClipboardHistory::Observer:
+  void OnClipboardHistoryCleared() override;
 
   void ExecuteSelectedMenuItem(int event_flags);
 
