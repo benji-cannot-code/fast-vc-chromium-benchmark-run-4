68cf50e72c1d6492f8768a1300cb6925a70bccc4
Javier Fernandez <jfernandez@igalia.com>
2020-11-06 11:01:27 +0000

    Use paragraph base direction always for trailing spaces runs.
    As per Unicode UAX#9 L1.4, line trailing whitespaces should always have
    the direction of the paragraph no matter the direction they had before
    line breaking.
    
    To accomplish that, in inline layout algorithm CreateLine, when an
    NGInlineItemResult is only trailing spaces, we ignore the bidi
    level coming from the NGInlineItem, and just use the base direction
    of the line.
    
    This should fix the case of an LTR line with trailing space and
    pre-wrap in an RTL line that would end up putting the trailing
    space to the right of the last LTR line instead of to the left of
    the line.
    
    This fixes css/css-text/white-space/eol-spaces-bidi-002.html with
    width of 7 characters. It is still not fixing the case for 8
    characters.
    
    Bug: 316409, 1142926
    Change-Id: Iab220f79ba0b2526fd51d5341363cc8e9087e133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505486
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Javier Fernandez <jfernandez@igalia.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824804}

--- third_party/blink/renderer/core/layout/ng/inline/ng_logical_line_item.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_logical_line_item.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/geometry/logical_rect.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_item.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_item_result.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_physical_text_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_result.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -50,6 +51,20 @@ struct NGLogicalLineItem {
         children_count(children_count),
         bidi_level(bidi_level) {}
   // Create an in-flow text fragment.
+  NGLogicalLineItem(const NGInlineItem& inline_item,
+                    NGInlineItemResult& item_result,
+                    const NGTextOffset& text_offset,
+                    LayoutUnit block_offset,
+                    LayoutUnit inline_size,
+                    LayoutUnit text_height,
+                    UBiDiLevel bidi_level)
+      : inline_item(&inline_item),
+        shape_result(std::move(item_result.shape_result)),
+        text_offset(text_offset),
+        rect(LayoutUnit(), block_offset, LayoutUnit(), text_height),
+        inline_size(inline_size),
+        bidi_level(bidi_level),
+        has_only_trailing_spaces(item_result.has_only_trailing_spaces) {}
   NGLogicalLineItem(const NGInlineItem& inline_item,
                     scoped_refptr<const ShapeResultView> shape_result,
                     const NGTextOffset& text_offset,
@@ -216,6 +231,9 @@ struct NGLogicalLineItem {
   UBiDiLevel bidi_level = 0xff;
   // The current text direction for OOF positioned items.
   TextDirection container_direction = TextDirection::kLtr;
+  // When an item contains only trailing spaces, the original bidi level needs
+  // to be ignored, and just use paragraph direction (UAX#9 L1)
+  bool has_only_trailing_spaces = false;
 
   bool is_hidden_for_paint = false;
 };
