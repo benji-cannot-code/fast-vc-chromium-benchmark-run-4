f52478f860b2c40be8da1200a95133166df3661a
Clemens Arbesser <arbesser@google.com>
2020-11-06 17:35:55 +0000

    [Autofill Assistant] Add popup to chips.
    This allows chips to show a popup menu when tapped, instead of directly
    invoking their callback. For now, this is only intended for trigger
    scripts, specifically for the cancel popup.
    
    A screenshot of the popup is in the linked bug.
    
    Bug: b/172585820
    Change-Id: Id32c529b7f32a11b2aaef18a9391e708795be38d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521188
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/master@{#824909}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/carousel/AssistantChipViewHolder.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/carousel/AssistantChipViewHolder.java
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.autofill_assistant.carousel;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
 import org.chromium.chrome.autofill_assistant.R;
@@ -19,6 +22,7 @@ import org.chromium.chrome.autofill_assistant.R;
  */
 public class AssistantChipViewHolder extends ViewHolder {
     private final ButtonView mView;
+    private @Nullable PopupMenu mPopupMenu;
 
     /** The type of this ViewHolder, as returned by {@link #getViewType(AssistantChip)}. */
     private final int mType;
@@ -81,7 +85,22 @@ public class AssistantChipViewHolder extends ViewHolder {
 
         // Setting this view to clickable may be required for a11y to correctly announce it.
         mView.setClickable(true);
-        mView.setOnClickListener(ignoredView -> chip.getSelectedListener().run());
+
+        // If a popup is specified, instead of invoking chip.getSelectedListener, show the popup
+        // menu and invoke the popup callback.
+        if (chip.getPopupItems() != null) {
+            mPopupMenu = new PopupMenu(mView.getContext(), mView);
+            for (int i = 0; i < chip.getPopupItems().size(); i++) {
+                mPopupMenu.getMenu().add(Menu.NONE, i, Menu.NONE, chip.getPopupItems().get(i));
+            }
+            mPopupMenu.setOnMenuItemClickListener(item -> {
+                chip.getOnPopupItemSelectedCallback().onResult(item.getItemId());
+                return true;
+            });
+            mView.setOnClickListener(ignoredView -> mPopupMenu.show());
+        } else {
+            mView.setOnClickListener(ignoredView -> chip.getSelectedListener().run());
+        }
 
         int iconResource;
         int iconDescriptionResource = 0;
