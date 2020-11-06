4ccebc56b0d61470a9400fcd683daa5bea8366a1
Xing Liu <xingliu@chromium.org>
2020-11-06 23:21:37 +0000

    Read later: Add a method to notify update for reading list backend.
    Adds an observer method to notify update for reading list backend.
    
    Bug: 1139133
    Change-Id: Ic5483e075086d48334a9e347125048b5699c2560
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519151
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825086}

--- chrome/browser/android/bookmarks/bookmark_bridge.cc
+++ chrome/browser/android/bookmarks/bookmark_bridge.cc
@@ -1134,9 +1134,6 @@ void BookmarkBridge::ExtensiveBookmarkChangesEnded(BookmarkModel* model) {
 }
 
 void BookmarkBridge::PartnerShimChanged(PartnerBookmarksShim* shim) {
-  if (!IsLoaded())
-    return;
-
   BookmarkModelChanged();
 }
 
@@ -1150,6 +1147,10 @@ void BookmarkBridge::ShimBeingDeleted(PartnerBookmarksShim* shim) {
 
 void BookmarkBridge::ReadingListLoaded() {
   NotifyIfDoneLoading();
+}
+
+void BookmarkBridge::ReadingListChanged() {
+  BookmarkModelChanged();
 }
 
 void BookmarkBridge::ReorderChildren(
