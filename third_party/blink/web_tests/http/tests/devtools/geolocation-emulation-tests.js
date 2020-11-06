ff4ba2d36f1e6e1c87d30a4ccc7fb5e6f0f6ba5a
Reilly Grant <reillyg@chromium.org>
2020-11-06 23:26:13 +0000

    geolocation: Fix PositionOptions.https.html
    This change fixes the PositionOptions.https.html Web Platform Tests by
    configuring the Geolocation API so that no location is available in
    web_tests. This allows, once 'geolocation' permission has been granted,
    the tests to run as expected.
    
    Follow-up work in this area could add a test_driver method to set a
    simulated geoposition and move the tests in web_tests/geolocation-api
    into external/wpt/geolocation-API.
    
    Bug: 745079
    Change-Id: Ib916f6a2db779c39d6104db0d5a06af6751f7059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519932
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825094}

--- third_party/blink/web_tests/http/tests/devtools/geolocation-emulation-tests.js
+++ third_party/blink/web_tests/http/tests/devtools/geolocation-emulation-tests.js
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.evaluateInPagePromise(`
     async function getPositionPromise() {
       try {
-        const position = await new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
+        const position = await new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject, { timeout: 1000 }));
         if ((new Date(position.timestamp)).toDateString() != (new Date()).toDateString())
             return 'Unexpected error occured: timestamps mismatch.';
         if (position && position.coords)
