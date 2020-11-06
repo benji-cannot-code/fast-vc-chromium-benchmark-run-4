636d731566180cbcb765eb00b20cefc0430d5430
Adam Rice <ricea@chromium.org>
2020-11-06 05:32:52 +0000

    WebSocket: don't check the restricted port list synchronously
    In 2017 the HTML standard changed to no longer check the bad port list
    synchronously in the WebSocket constructor, instead relying on the
    lower-level check in Fetch to cause an asynchronous failure.
    
    Safari is now implementing
    this (https://github.com/web-platform-tests/wpt/pull/26392#issuecomment-722593477),
    so update our implementation to match.
    
    BUG=705534
    
    Change-Id: I29c35fa2307a1215d5f382f86ca97bbfd17aba2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521874
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824747}

--- third_party/blink/renderer/modules/websockets/websocket_common.cc
+++ third_party/blink/renderer/modules/websockets/websocket_common.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/websockets/websocket_channel.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
-#include "third_party/blink/renderer/platform/weborigin/known_ports.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
@@ -78,13 +77,6 @@ WebSocketCommon::ConnectResult WebSocketCommon::Connect(
         "The URL contains a fragment identifier ('" +
             url_.FragmentIdentifier() +
             "'). Fragment identifiers are not allowed in WebSocket URLs.");
-    return ConnectResult::kException;
-  }
-
-  if (!IsPortAllowedForScheme(url_)) {
-    state_ = kClosed;
-    exception_state.ThrowSecurityError(
-        "The port " + String::Number(url_.Port()) + " is not allowed.");
     return ConnectResult::kException;
   }
 
