48df41ef7644f065f95d222eca782287bd8eb756
Gordon Seto <gordonseto@google.com>
2020-11-06 21:19:37 +0000

    [CrOS Settings] Add flip camera button to Activation Code Page
    Add ability for users to switch between cameras when scanning
    for QR codes on the Activation Code Page.
    
    Screenshot:
    https://screenshot.googleplex.com/jacMJUL3HhZAAr2.png
    
    Bug: 1093185
    Change-Id: I32282cd62e0b98c72d4a76c5257b3e6f02f111c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2510740
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825032}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/esim_flow_ui_test.js
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/esim_flow_ui_test.js
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 suite('CrComponentsEsimFlowUiTest', function() {
   let eSimPage;
+
   setup(function() {
     eSimPage = document.createElement('esim-flow-ui');
     eSimPage.delegate = new cellular_setup.FakeCellularSetupDelegate();
@@ -50,7 +51,7 @@ suite('CrComponentsEsimFlowUiTest', function() {
         cellularSetup.ButtonState.SHOWN_BUT_DISABLED);
 
     const activationCodePage = eSimPage.$$('#activationCodePage');
-    activationCodePage.activationCode_ = 'ACTIVATION CODE';
+    activationCodePage.$$('#activationCode').value = 'ACTIVATION_CODE';
     Polymer.dom.flush();
 
     assertTrue(
