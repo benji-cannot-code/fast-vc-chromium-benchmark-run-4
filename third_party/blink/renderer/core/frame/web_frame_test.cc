6597d1f2d3425937c66c9ad7cbd4eb91033beefa
Daniel Cheng <dcheng@chromium.org>
2020-11-06 05:14:38 +0000

    Mark skia.mojom.Color as a copyable passed by value type.
    Change-Id: I4a0d6a554cbce5eecb25f6720f22d41919588a6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521355
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824744}

--- third_party/blink/renderer/core/frame/web_frame_test.cc
+++ third_party/blink/renderer/core/frame/web_frame_test.cc
@@ -8778,8 +8778,7 @@ class ThemeColorTestLocalFrameHost : public FakeLocalFrameHost {
 
  private:
   // FakeLocalFrameHost:
-  void DidChangeThemeColor(
-      const base::Optional<::SkColor>& theme_color) override {
+  void DidChangeThemeColor(base::Optional<::SkColor> theme_color) override {
     did_notify_ = true;
   }
 
