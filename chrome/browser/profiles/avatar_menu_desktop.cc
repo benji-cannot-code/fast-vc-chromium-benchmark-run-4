1f035149eacef4dd54b2c0a49ec93dcf159d5944
Jan Krcal <jkrcal@chromium.org>
2020-11-06 12:17:17 +0000

    Revert "[Profiles] Use new colored default avatars for taskbar badges"
    This reverts commit 6b7290b337c5a6d5ef24ef63853ed8783d24c409.
    
    Reason for revert: memory regression on Mac (crbug.com/1146299)
    
    Original change's description:
    > [Profiles] Use new colored default avatars for taskbar badges
    >
    > Recently, all logic around profile avatars has been integrated into
    > ProfileAttributesEntry::GetAvatarIcon() incl. new features such as
    > colored default avatars.
    >
    > This CL updates code that generates taskbar badges on Windows to
    > delegate most of its logic to GetAvatarIcon(). Only the pieces needed
    > for obtaining ImageLoadStatus remained in this code.
    >
    > The only behavioral change is for non-GAIA avatars: high-res /
    > 2x windows old avatars are returned instead of low-res avatars. This
    > means more downscaling is needed than before. This will be addressed
    > by further CLs that will make GetAvatarIcon() respect preferred_size in
    > more cases.
    >
    > Bug: 1122559
    > Change-Id: I5eecc7c6647dc78f9ff92d74edb64214a48e8e63
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2514153
    > Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#824494}
    
    TBR=droger@chromium.org,jkrcal@chromium.org
    
    Change-Id: I66f7b806a25b910d35b55f02805c4c8216049f55
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1122559, 1146299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521118
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#824833}

--- chrome/browser/profiles/avatar_menu_desktop.cc
+++ chrome/browser/profiles/avatar_menu_desktop.cc
@@ -13,12 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "ui/base/resource/resource_bundle.h"
 
 // static
 AvatarMenu::ImageLoadStatus AvatarMenu::GetImageForMenuButton(
     const base::FilePath& profile_path,
-    gfx::Image* image,
-    int preferred_size) {
+    gfx::Image* image) {
   if (!g_browser_process->profile_manager())
     return ImageLoadStatus::BROWSER_SHUTTING_DOWN;
   ProfileAttributesEntry* entry;
@@ -29,14 +29,30 @@ AvatarMenu::ImageLoadStatus AvatarMenu::GetImageForMenuButton(
   }
 
   ImageLoadStatus status = ImageLoadStatus::LOADED;
-  // We need to specifically report GAIA images that are not available yet.
-  if (entry->IsUsingGAIAPicture() && !entry->GetGAIAPicture()) {
+
+  // If there is a Gaia image available, try to use that.
+  if (entry->IsUsingGAIAPicture()) {
+    // The GetGAIAPicture API call will trigger an async image load from disk if
+    // it has not been loaded into memory.
+    const gfx::Image* gaia_image = entry->GetGAIAPicture();
+
+    if (gaia_image) {
+      *image = *gaia_image;
+      return ImageLoadStatus::LOADED;
+    }
+
     if (entry->IsGAIAPictureLoaded())
       status = ImageLoadStatus::MISSING;
     else
       status = ImageLoadStatus::LOADING;
   }
 
-  *image = entry->GetAvatarIcon(preferred_size);
+  // Otherwise, use the default resource, not the downloaded high-res one.
+  const size_t icon_index = entry->GetAvatarIconIndex();
+  const int resource_id =
+      profiles::GetDefaultAvatarIconResourceIDAtIndex(icon_index);
+  *image =
+      ui::ResourceBundle::GetSharedInstance().GetNativeImageNamed(resource_id);
+
   return status;
 }
