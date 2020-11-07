17a7f1bfdb17fb8b3e270eea4a70ff7027074b9f
John Abd-El-Malek <jam@chromium.org>
2020-11-07 00:54:00 +0000

    Fix crash if multiple subresources on a page request http auth.
    Bug: 1144209
    Change-Id: Icef2ca844b3c0fb4e2704253890760d0fe5f36bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2523357
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825128}

--- weblayer/browser/http_auth_handler_impl.h
+++ weblayer/browser/http_auth_handler_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/android/scoped_java_ref.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/login_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -26,14 +27,17 @@ class HttpAuthHandlerImpl : public content::LoginDelegate,
                       LoginAuthRequiredCallback callback);
   ~HttpAuthHandlerImpl() override;
 
-  void Proceed(const base::string16& user, const base::string16& password);
-  void Cancel();
-
-  GURL url() { return url_; }
+  void Proceed(JNIEnv* env,
+               const base::android::JavaParamRef<jstring>& username,
+               const base::android::JavaParamRef<jstring>& password);
+  void Cancel(JNIEnv* env);
 
  private:
+  void CloseDialog();
+
   GURL url_;
   LoginAuthRequiredCallback callback_;
+  base::android::ScopedJavaGlobalRef<jobject> java_impl_;
 };
 
 }  // namespace weblayer
--- weblayer/browser/tab_impl.cc
+++ weblayer/browser/tab_impl.cc
@@ -102,7 +102,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "weblayer/browser/browser_controls_container_view.h"
 #include "weblayer/browser/browser_controls_navigation_state_handler.h"
 #include "weblayer/browser/controls_visibility_reason.h"
-#include "weblayer/browser/http_auth_handler_impl.h"
 #include "weblayer/browser/java/jni/TabImpl_jni.h"
 #include "weblayer/browser/javascript_tab_modal_dialog_manager_delegate_android.h"
 #include "weblayer/browser/js_communication/web_message_host_factory_proxy.h"
@@ -549,28 +548,6 @@ void TabImpl::ShowContextMenu(const content::ContextMenuParams& params) {
 #endif
 }
 
-void TabImpl::ShowHttpAuthPrompt(HttpAuthHandlerImpl* auth_handler) {
-  CHECK(!auth_handler_);
-  auth_handler_ = auth_handler;
-#if defined(OS_ANDROID)
-  JNIEnv* env = AttachCurrentThread();
-  GURL url = auth_handler_->url();
-  Java_TabImpl_showHttpAuthPrompt(
-      env, java_impl_, base::android::ConvertUTF8ToJavaString(env, url.host()),
-      base::android::ConvertUTF8ToJavaString(env, url.spec()));
-#endif
-}
-
-void TabImpl::CloseHttpAuthPrompt() {
-  if (!auth_handler_)
-    return;
-  auth_handler_ = nullptr;
-#if defined(OS_ANDROID)
-  JNIEnv* env = AttachCurrentThread();
-  Java_TabImpl_closeHttpAuthPrompt(env, java_impl_);
-#endif
-}
-
 #if defined(OS_ANDROID)
 // static
 void TabImpl::DisableAutofillSystemIntegrationForTesting() {
@@ -770,21 +747,6 @@ base::android::ScopedJavaLocalRef<jobjectArray> TabImpl::GetData(JNIEnv* env) {
 jboolean TabImpl::IsRendererControllingBrowserControlsOffsets(JNIEnv* env) {
   return browser_controls_navigation_state_handler_
       ->IsRendererControllingOffsets();
-}
-
-void TabImpl::SetHttpAuth(
-    JNIEnv* env,
-    const base::android::JavaParamRef<jstring>& username,
-    const base::android::JavaParamRef<jstring>& password) {
-  auth_handler_->Proceed(
-      base::android::ConvertJavaStringToUTF16(env, username),
-      base::android::ConvertJavaStringToUTF16(env, password));
-  CloseHttpAuthPrompt();
-}
-
-void TabImpl::CancelHttpAuth(JNIEnv* env) {
-  auth_handler_->Cancel();
-  CloseHttpAuthPrompt();
 }
 
 base::android::ScopedJavaLocalRef<jstring> TabImpl::RegisterWebMessageCallback(
