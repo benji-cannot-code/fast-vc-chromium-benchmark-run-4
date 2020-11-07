9d45595b2b1b604c1b3a7dcf457843179dda34f6
Liquan (Max) Gu <maxlg@chromium.org>
2020-11-07 15:48:38 +0000

    [WebLayer] Extracts an interface for PaymentResponseHelper
    Breaks the current PaymentResponseHelper into 2 parts:
    * PaymentResponseHelperInterface: a general interface.
    * ChromePaymentResponseHelper: the helper that's used by Chrome, which
      includes the Autofill feature.
    
    With the PaymentResponseHelperInterface, we will create another general
    purpose implementation (without Autofill) that can be used by the
    WebLayer.
    
    Bug: 1146159
    
    Change-Id: I7cbfbd9486f085179d2298fdf29428fd641217e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521369
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825193}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ChromePaymentRequestService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ChromePaymentRequestService.java
@@ -43,6 +43,8 @@ import org.chromium.components.payments.PaymentOptionsUtils;
 import org.chromium.components.payments.PaymentRequestService;
 import org.chromium.components.payments.PaymentRequestServiceUtil;
 import org.chromium.components.payments.PaymentRequestSpec;
+import org.chromium.components.payments.PaymentResponseHelperInterface;
+import org.chromium.components.payments.PaymentResponseHelperInterface.PaymentResponseResultCallback;
 import org.chromium.components.payments.PaymentUIsObserver;
 import org.chromium.components.payments.PaymentValidator;
 import org.chromium.components.payments.Section;
@@ -71,12 +73,11 @@ import java.util.Set;
  * This is the Clank specific parts of {@link PaymentRequest}, with the parts shared with WebLayer
  * living in {@link PaymentRequestService}.
  */
-public class ChromePaymentRequestService
-        implements BrowserPaymentRequest, PaymentApp.AbortCallback,
-                   PaymentApp.InstrumentDetailsCallback,
-                   PaymentResponseHelper.PaymentResponseRequesterDelegate,
-                   PaymentDetailsConverter.MethodChecker, PaymentUiService.Delegate,
-                   PaymentUIsObserver {
+public class ChromePaymentRequestService implements BrowserPaymentRequest, PaymentApp.AbortCallback,
+                                                    PaymentApp.InstrumentDetailsCallback,
+                                                    PaymentResponseResultCallback,
+                                                    PaymentDetailsConverter.MethodChecker,
+                                                    PaymentUiService.Delegate, PaymentUIsObserver {
     private static final String TAG = "PaymentRequest";
 
     /**
@@ -120,7 +121,7 @@ public class ChromePaymentRequestService
     private boolean mDidRecordShowEvent;
 
     /** The helper to create and fill the response to send to the merchant. */
-    private PaymentResponseHelper mPaymentResponseHelper;
+    private PaymentResponseHelperInterface mPaymentResponseHelper;
 
     /** A helper to manage the Skip-to-GPay experimental flow. */
     private SkipToGPayHelper mSkipToGPayHelper;
@@ -855,9 +856,9 @@ public class ChromePaymentRequestService
         EditableOption selectedContact = mPaymentUiService.getContactSection() != null
                 ? mPaymentUiService.getContactSection().getSelectedItem()
                 : null;
-        mPaymentResponseHelper = new PaymentResponseHelper(selectedShippingAddress,
+        mPaymentResponseHelper = new ChromePaymentResponseHelper(selectedShippingAddress,
                 selectedShippingOption, selectedContact, selectedPaymentApp, mPaymentOptions,
-                mSkipToGPayHelper != null, this);
+                mSkipToGPayHelper != null);
 
         selectedPaymentApp.setPaymentHandlerHost(getPaymentHandlerHost());
         // Only native apps can use PaymentDetailsUpdateService.
@@ -1114,7 +1115,8 @@ public class ChromePaymentRequestService
 
         mJourneyLogger.setEventOccurred(Event.RECEIVED_INSTRUMENT_DETAILS);
 
-        mPaymentResponseHelper.onPaymentDetailsReceived(methodName, stringifiedDetails, payerData);
+        mPaymentResponseHelper.generatePaymentResponse(
+                methodName, stringifiedDetails, payerData, /*resultCallback=*/this);
     }
 
     @Override
