d7f409be18bfeb6f149d719280bb9b5df59db13a
Kyle Horimoto <khorimoto@chromium.org>
2020-11-07 01:44:16 +0000

    [CrOS Settings] Update "Whitelist" => "Allowlist"
    Change-Id: I546466b79b30b5ccf6c19e7e55eea430f551794f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521447
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825142}

--- chrome/browser/ui/webui/settings/chromeos/people_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/people_section.cc
@@ -102,7 +102,7 @@ const std::vector<SearchConcept>& GetPeopleSearchConcepts() {
        mojom::SearchResultIcon::kAvatar,
        mojom::SearchResultDefaultRank::kMedium,
        mojom::SearchResultType::kSetting,
-       {.setting = mojom::Setting::kRemoveFromUserWhitelist}},
+       {.setting = mojom::Setting::kRemoveFromUserAllowlist}},
       {IDS_OS_SETTINGS_TAG_GUEST_BROWSING,
        mojom::kManageOtherPeopleSubpagePath,
        mojom::SearchResultIcon::kAvatar,
@@ -135,7 +135,7 @@ const std::vector<SearchConcept>& GetPeopleSearchConcepts() {
        mojom::SearchResultIcon::kAvatar,
        mojom::SearchResultDefaultRank::kMedium,
        mojom::SearchResultType::kSetting,
-       {.setting = mojom::Setting::kAddToUserWhitelist}},
+       {.setting = mojom::Setting::kAddToUserAllowlist}},
   });
   return *tags;
 }
@@ -1012,8 +1012,8 @@ void PeopleSection::RegisterHierarchy(HierarchyGenerator* generator) const {
       mojom::Setting::kGuestBrowsing,
       mojom::Setting::kShowUsernamesAndPhotosAtSignIn,
       mojom::Setting::kRestrictSignIn,
-      mojom::Setting::kAddToUserWhitelist,
-      mojom::Setting::kRemoveFromUserWhitelist,
+      mojom::Setting::kAddToUserAllowlist,
+      mojom::Setting::kRemoveFromUserAllowlist,
   };
   RegisterNestedSettingBulk(mojom::Subpage::kManageOtherPeople,
                             kManageOtherPeopleSettings, generator);
