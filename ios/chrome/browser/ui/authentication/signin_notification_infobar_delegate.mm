2319aa91036c6a0b406fb6083b5b430e8e22e9bd
Nohemi Fernandez <fernandex@chromium.org>
2020-11-08 17:10:41 +0000

    [iOS] Set high priority attribute for SigninNotificationInfobarDelegate.
    The high priority attribute allows an infobar to be presented for a
    longer period of time to the user. This is necessary for the sign-in
    notification, so that the user has time to take action if needed.
    
    Bug: 1145592
    Change-Id: I3160298195643cf684213340c9aebd644505a28c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521621
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825220}

--- ios/chrome/browser/ui/authentication/signin_notification_infobar_delegate.mm
+++ ios/chrome/browser/ui/authentication/signin_notification_infobar_delegate.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/infobars/core/infobar_delegate.h"
 #include "components/infobars/core/infobar_manager.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/infobars/infobar_utils.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/signin/authentication_service.h"
 #import "ios/chrome/browser/signin/authentication_service_factory.h"
@@ -45,8 +46,9 @@ bool SigninNotificationInfoBarDelegate::Create(
   DCHECK(infobar_manager);
   std::unique_ptr<ConfirmInfoBarDelegate> delegate(
       std::make_unique<SigninNotificationInfoBarDelegate>(browser_state));
-  return !!infobar_manager->AddInfoBar(
-      infobar_manager->CreateConfirmInfoBar(std::move(delegate)));
+  std::unique_ptr<infobars::InfoBar> infobar =
+      CreateHighPriorityConfirmInfoBar(std::move(delegate));
+  return !!infobar_manager->AddInfoBar(std::move(infobar));
 }
 
 SigninNotificationInfoBarDelegate::SigninNotificationInfoBarDelegate(
