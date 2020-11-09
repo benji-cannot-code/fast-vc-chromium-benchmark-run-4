fb6ad1f66cd32e64633fc5d4238a4bb3147de709
Egor Pasko <pasko@chromium.org>
2020-11-09 16:12:44 +0000

    android: ModernLinker: UMA Histogram for RELRO sharing success rate
    When RELROs from different processes come out not completely identical,
    the LegacyLinker has functionality to share only the identical subrange.
    This is because at least on some old devices it happened frequently.
    
    The ModernLinker does not have this functionality.
    
    Record a histogram to tell how often the ModernLinker fails sharing on
    RELRO mismatches.
    
    Later this histogram can be extended to more linkers and failure modes.
    
    Bug: 979638
    Change-Id: I5430dd3fd31f01b9909de3ac758d6ab151d26877
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2523182
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825350}

--- base/android/java/src/org/chromium/base/library_loader/ModernLinker.java
+++ base/android/java/src/org/chromium/base/library_loader/ModernLinker.java
@@ -7,6 +7,7 @@ package org.chromium.base.library_loader;
 
 import org.chromium.base.Log;
 import org.chromium.base.annotations.JniIgnoreNatives;
+import org.chromium.base.metrics.RecordHistogram;
 
 import javax.annotation.concurrent.GuardedBy;
 
@@ -90,6 +91,14 @@ class ModernLinker extends Linker {
             throw new UnsatisfiedLinkError(
                     "Unable to load the library a second time with the system linker");
         }
+
+        // Record whether using shared relocations succeeded, only when an attempt was made.
+        if (!loadNoRelro && !provideRelro) {
+            int status = nativeGetRelroSharingResult();
+            assert status != RelroSharingStatus.NOT_ATTEMPTED;
+            RecordHistogram.recordEnumeratedHistogram(
+                    "ChromiumAndroidLinker.RelroSharingStatus", status, RelroSharingStatus.COUNT);
+        }
     }
 
     @GuardedBy("sLock")
@@ -104,4 +113,5 @@ class ModernLinker extends Linker {
     private static native boolean nativeLoadLibraryUseRelros(
             String dlopenExtPath, long loadAddress, int fd);
     private static native boolean nativeLoadLibraryNoRelros(String dlopenExtPath);
+    private static native int nativeGetRelroSharingResult();
 }
