8661096f1a349dbc60eee756c6a524a2ed50f4c5
Luciano Pacheco <lucmult@chromium.org>
2020-11-09 05:53:31 +0000

    Reland: Audio Player: Add generate_grd() and grit() for JS Modules
    Reland of CL:2503909, I've tested locally as described in
    crbug.com/1144470#c3 and it's fixed. The change to fix is here:
    https://chromium-review.googlesource.com/c/chromium/src/+/2519177/2..3
    
    Add a new PAK file for resources in Files app and companion apps like
    Audio Player, to host the generated JS modules.
    
    Change ChromeComponentExtensionResourceManager to also load the
    resources from this new PAK file.  This was refactored to not depend on
    file_manager_resource_util.h any longer, which allowed to remove this
    util.
    
    Rename Audio Player optimize_webu() to "build" to follow the convention.
    
    This is a preparation for converting these apps to JS modules.
    
    Bug: 1133186, 1030998, 1144470
    Change-Id: Ic803e5f071cde0ac31713578a6f17e74a07c5465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519177
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825256}

--- chrome/browser/extensions/chrome_component_extension_resource_manager.cc
+++ chrome/browser/extensions/chrome_component_extension_resource_manager.cc
@@ -28,8 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/file_manager/file_manager_string_util.h"
 #include "third_party/ink/grit/ink_resources.h"
-#include "ui/file_manager/file_manager_resource_util.h"
-#include "ui/file_manager/grit/file_manager_resources.h"
+#include "ui/file_manager/grit/file_manager_gen_resources_map.h"
+#include "ui/file_manager/grit/file_manager_resources_map.h"
 #endif
 
 #if BUILDFLAG(ENABLE_PDF)
@@ -93,11 +93,10 @@ ChromeComponentExtensionResourceManager::Data::Data() {
   AddComponentResourceEntries(kExtraComponentExtensionResources,
                               base::size(kExtraComponentExtensionResources));
 #if defined(OS_CHROMEOS)
-  size_t file_manager_resource_size;
-  const GritResourceMap* file_manager_resources =
-      file_manager::GetFileManagerResources(&file_manager_resource_size);
-  AddComponentResourceEntries(file_manager_resources,
-                              file_manager_resource_size);
+  // Add Files app JS modules resources.
+  AddComponentResourceEntries(kFileManagerResources, kFileManagerResourcesSize);
+  AddComponentResourceEntries(kFileManagerGenResources,
+                              kFileManagerGenResourcesSize);
 
   // ResourceBundle and g_browser_process are not always initialized in unit
   // tests.
