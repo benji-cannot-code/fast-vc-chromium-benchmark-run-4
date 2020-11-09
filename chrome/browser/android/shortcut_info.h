00d99de11d53569dbbb812de7ae2e8218cb67cc1
Gyuyoung Kim <gyuyoung@igalia.com>
2020-11-09 15:15:49 +0000

    Use mojom::ManifestShareTarget_Enctype instead of Manifest::ShareTarget::Enctype
    This CL replaces all uses of blink::Manifest::ShareTarget::Enctype
    with blink::mojom::ManifestShareTarget_Enctype in order to reduce type
    conversion between them.
    
    Bug: 919392
    Change-Id: Ifc173da74d8679f00cf4bb39e4c05ef5b1103fdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2522832
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825334}

--- chrome/browser/android/shortcut_info.h
+++ chrome/browser/android/shortcut_info.h
@@ -41,7 +41,7 @@ struct ShareTargetParams {
 struct ShareTarget {
   GURL action;
   blink::mojom::ManifestShareTarget_Method method;
-  blink::Manifest::ShareTarget::Enctype enctype;
+  blink::mojom::ManifestShareTarget_Enctype enctype;
   ShareTargetParams params;
   ShareTarget();
   ~ShareTarget();
