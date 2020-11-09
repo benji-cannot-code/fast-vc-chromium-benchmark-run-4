2c8a110086ead153ef66daec9cd83c12a728d7cb
Clemens Arbesser <arbesser@google.com>
2020-11-09 09:02:27 +0000

    [Autofill Assistant] Adds the native coordinator for trigger scripts.
    This is currently not yet wired up to the android UI, but is otherwise
    fully functional. There are some smaller follow-ups that I extracted
    from this CL:
    - Update to metrics enums
    - Wiring it up to android
    
    Bug: b/171776026
    Change-Id: I7a05d97ccabb8c2bbaa3bb574d74772e9c4dbfb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2517696
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Marian Fechete <marianfe@google.com>
    Cr-Commit-Position: refs/heads/master@{#825274}

---
+++ components/autofill_assistant/browser/trigger_scripts/mock_static_trigger_conditions.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill_assistant/browser/trigger_scripts/mock_static_trigger_conditions.h"
+
+namespace autofill_assistant {
+
+MockStaticTriggerConditions::MockStaticTriggerConditions() = default;
+MockStaticTriggerConditions::~MockStaticTriggerConditions() = default;
+
+}  // namespace autofill_assistant
--- components/autofill_assistant/browser/trigger_scripts/trigger_script_unittest.cc
+++ components/autofill_assistant/browser/trigger_scripts/trigger_script_unittest.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/mock_callback.h"
 #include "components/autofill_assistant/browser/service.pb.h"
 #include "components/autofill_assistant/browser/trigger_scripts/dynamic_trigger_conditions.h"
+#include "components/autofill_assistant/browser/trigger_scripts/mock_dynamic_trigger_conditions.h"
+#include "components/autofill_assistant/browser/trigger_scripts/mock_static_trigger_conditions.h"
 #include "components/autofill_assistant/browser/trigger_scripts/static_trigger_conditions.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
@@ -16,32 +18,6 @@ namespace autofill_assistant {
 
 using ::testing::NiceMock;
 using ::testing::Return;
-
-class MockStaticTriggerConditions : public StaticTriggerConditions {
- public:
-  MOCK_METHOD4(Init,
-               void(Client* client,
-                    const GURL& url,
-                    TriggerContext* trigger_context,
-                    base::OnceCallback<void(void)> callback));
-  MOCK_CONST_METHOD0(is_first_time_user, bool());
-  MOCK_CONST_METHOD0(has_stored_login_credentials, bool());
-  MOCK_CONST_METHOD1(is_in_experiment, bool(int experiment_id));
-};
-
-class MockDynamicTriggerConditions : public DynamicTriggerConditions {
- public:
-  MOCK_CONST_METHOD1(GetSelectorMatches,
-                     base::Optional<bool>(const Selector& selector));
-
-  void Update(WebController* web_controller,
-              base::OnceCallback<void(void)> callback) override {
-    OnUpdate(web_controller, callback);
-  }
-  MOCK_METHOD2(OnUpdate,
-               void(WebController* web_controller,
-                    base::OnceCallback<void(void)>& callback));
-};
 
 class TriggerScriptTest : public testing::Test {
  public:
