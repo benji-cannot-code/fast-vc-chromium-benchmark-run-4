3078871a95cddf9b773af1ce25a8d48ac4b42fd4
Yifan Luo <lyf@google.com>
2020-11-09 11:55:30 +0000

    [Sanitizer API] Fix Trusted Types bypass in Sanitizer API.
    `sanitize` as a DOMString to Fragment API need to go through Trusted Types.
    
    Bug: 1116418
    Change-Id: Icf73ff9f7a8f115d0f9f6e32ae9673c7475691e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2506046
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825303}

--- third_party/blink/renderer/modules/sanitizer_api/sanitizer.h
+++ third_party/blink/renderer/modules/sanitizer_api/sanitizer.h
@@ -18,6 +18,7 @@ class ExceptionState;
 class SanitizerConfig;
 class ScriptState;
 class StringOrDocumentFragmentOrDocument;
+class StringOrTrustedHTMLOrDocumentFragmentOrDocument;
 
 class MODULES_EXPORT Sanitizer final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
@@ -31,7 +32,7 @@ class MODULES_EXPORT Sanitizer final : public ScriptWrappable {
                           StringOrDocumentFragmentOrDocument&,
                           ExceptionState&);
   DocumentFragment* sanitize(ScriptState*,
-                             StringOrDocumentFragmentOrDocument&,
+                             StringOrTrustedHTMLOrDocumentFragmentOrDocument&,
                              ExceptionState&);
 
   void Trace(Visitor*) const override;
@@ -40,6 +41,10 @@ class MODULES_EXPORT Sanitizer final : public ScriptWrappable {
   void ElementFormatter(HashSet<String>&, const Vector<String>&);
   void AttrFormatter(HashMap<String, Vector<String>>&,
                      const Vector<std::pair<String, Vector<String>>>&);
+
+  DocumentFragment* SanitizeImpl(ScriptState*,
+                                 StringOrDocumentFragmentOrDocument&,
+                                 ExceptionState&);
 
   HashSet<String> allow_elements_ = {};
   HashSet<String> block_elements_ = {};
