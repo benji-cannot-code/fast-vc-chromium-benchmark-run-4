a85ea7e2c0dc85ba6131bbd28052f58d89209502
Friedrich Horschig <fhorschig@chromium.org>
2020-11-09 21:21:33 +0000

    [Android][Mfill] Hide "Use other password" without saved credentials
    This CL defers adding the "Use other [password|username]" button
    until the newly introduced helper confirmed that there is at
    least one credential to show.
    
    To do that, the helper fetches the passwords when it's created.
    If it finishes before suggestions are to be refreshed, the
    stored boolean is used, otherwise, the callback is queued and
    will trigger an update.
    
    Smaller clean-ups:
    * New test fixture for filling (mostly moved to save setup code
      and to prevent ScopedFeatureList issues)
    * Moved accessory sheet state (focused field) into helper since
      frame state shouldn't be used by the accessory controller
      (if possible but required by all pwds sheet)
    
    Bug: 1104132
    Change-Id: I4fa5757744dda5c2091fec24daf0654f92b2be46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2489914
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825468}

--- components/password_manager/core/browser/mock_password_store.h
+++ components/password_manager/core/browser/mock_password_store.h
@@ -98,6 +98,8 @@ class MockPasswordStore : public PasswordStore {
   MOCK_METHOD0(GetAllFieldInfoImpl, std::vector<FieldInfo>());
   MOCK_METHOD2(RemoveFieldInfoByTimeImpl, void(base::Time, base::Time));
   MOCK_METHOD0(IsEmpty, bool());
+  MOCK_METHOD1(GetAllLoginsWithAffiliationAndBrandingInformation,
+               void(PasswordStoreConsumer*));
 
   MOCK_CONST_METHOD0(IsAbleToSavePasswords, bool());
 
