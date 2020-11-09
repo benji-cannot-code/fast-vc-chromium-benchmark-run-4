e32e3c65d7f874bc138522fd35ff7e814eea42b8
Eric Willigers <ericwilligers@chromium.org>
2020-11-09 02:15:33 +0000

    Web Share: Share can succeed after rejected file share
    If excessively large files, or too many files, were passed to
    navigator.share(), we rejected the request but also incorrectly
    updated state (client_ in NavigatorShare), leading to future share
    attempts failing with InvalidStateError
    "A earlier share had not yet completed."
    
    We now only set client_ when we are about to make a mojo call.
    
    Bug: 1146317
    Change-Id: I8ced104d0c2486c75987847c0f23c2d632a6cc9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2522813
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825243}

--- third_party/blink/renderer/modules/webshare/navigator_share.cc
+++ third_party/blink/renderer/modules/webshare/navigator_share.cc
@@ -246,9 +246,6 @@ ScriptPromise NavigatorShare::share(ScriptState* script_state,
   }
 
   bool has_files = HasFiles(*data);
-  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
-  client_ = MakeGarbageCollected<ShareClientImpl>(this, has_files, resolver);
-
   WTF::Vector<mojom::blink::SharedFilePtr> files;
   uint64_t total_bytes = 0;
   if (has_files) {
@@ -267,6 +264,8 @@ ScriptPromise NavigatorShare::share(ScriptState* script_state,
     }
   }
 
+  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
+  client_ = MakeGarbageCollected<ShareClientImpl>(this, has_files, resolver);
   service_remote_->Share(
       data->hasTitle() ? data->title() : g_empty_string,
       data->hasText() ? data->text() : g_empty_string, url, std::move(files),
