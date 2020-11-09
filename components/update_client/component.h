b380e3163965ca19ce15391ea97379a80caf09b3
Sorin Jianu <sorin@chromium.org>
2020-11-09 20:55:49 +0000

    Simplify handling of Component::StateChecking.
    This seems to work and no change in the unit tests was needed
    after removing the clutter of how StateChecking::DoHandle works.
    
    High level, the state transitions kNew->kChecking and kChecking->whatever
    don't need to count callbacks. Instead, the code relies on the sequence
    provided by the task runner.
    
    This streamlines the execution path of UpdateEngine::DoUpdateCheck since
    it makes clear that it can't be invoked multiple times during
    a single call to UpdateEngine::Update.
    
    Bug: 1146864
    Change-Id: I116e0f68b6668856aa3c3564ed4f51bf7efd4026
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2523788
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825456}

--- components/update_client/component.h
+++ components/update_client/component.h
@@ -207,8 +207,6 @@ class Component {
    private:
     // State overrides.
     void DoHandle() override;
-
-    void UpdateCheckComplete();
   };
 
   class StateUpdateError : public State {
@@ -489,8 +487,6 @@ class Component {
   CallbackHandleComplete callback_handle_complete_;
   std::unique_ptr<State> state_;
   const UpdateContext& update_context_;
-
-  base::OnceClosure update_check_complete_;
 
   ComponentState previous_state_ = ComponentState::kLastStatus;
 
