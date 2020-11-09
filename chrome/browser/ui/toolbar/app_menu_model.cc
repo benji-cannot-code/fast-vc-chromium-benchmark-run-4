fb400e660f5e94aab9167e5c16f5b6f4a99db3a2
Peter Boström <pbos@chromium.org>
2020-11-09 21:32:44 +0000

    Clean up kUseTextButtonForUpdate experiment
    Bug: 1002162
    Change-Id: I2329db999c674c33865e45efc47305ba24b44f17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521157
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825476}

--- chrome/browser/ui/toolbar/app_menu_model.cc
+++ chrome/browser/ui/toolbar/app_menu_model.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/banners/app_banner_manager.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/defaults.h"
@@ -335,7 +336,9 @@ ui::ImageModel AppMenuModel::GetIconForCommandId(int command_id) const {
   if (command_id == IDC_UPGRADE_DIALOG) {
     DCHECK(browser_defaults::kShowUpgradeMenuItem);
     DCHECK(app_menu_icon_controller_);
-    return app_menu_icon_controller_->GetIconImage(false);
+    return ui::ImageModel::FromVectorIcon(
+        kBrowserToolsUpdateIcon,
+        app_menu_icon_controller_->GetIconColor(base::nullopt));
   }
   return ui::ImageModel();
 }
