33e51f0e7269d9bf036a5a0fbfac99ed00c6ff88
Mason Freed <masonfreed@chromium.org>
2020-11-09 01:28:14 +0000

    Remove some bits from document
    Attempting to reduce the memory regression seen in crbug.com/1146309.
    This doesn't seem like it could possibly account for that entire
    regression, but perhaps the story there has many iframes or
    something?
    
    Bug: 1146309
    Change-Id: I98470080f4398abd4c79d51a0bd24283c85bd3b0
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2523062
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Auto-Submit: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825240}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1663,7 +1663,11 @@ class CORE_EXPORT Document : public ContainerNode,
   void MarkFirstPaint();
   void MaybeExecuteDelayedAsyncScripts();
 
-  enum class DeclarativeShadowDomAllowState { kNotSet, kAllow, kDeny };
+  enum class DeclarativeShadowDomAllowState : uint8_t {
+    kNotSet,
+    kAllow,
+    kDeny
+  };
   DeclarativeShadowDomAllowState GetDeclarativeShadowDomAllowState() const;
   void setAllowDeclarativeShadowDom(bool val);
   bool allowDeclarativeShadowDom() const;
