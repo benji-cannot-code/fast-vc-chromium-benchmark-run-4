07c773864ee81339b76bb99711793386bbedc1b3
Alexander Bolodurin <alexbn@google.com>
2020-11-10 01:05:20 +0000

    arc: Reinstate lost error check
    It was dropped in https://crrev.com/c/2464518 which may result in all
    errors being reported as base::File::FILE_ERROR_NOT_FOUND.
    
    Bug: 1049966
    Change-Id: I78e1ac5bf664c49ecebf130d23737f8dad3721a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2505341
    Commit-Queue: Alexander Bolodurin <alexbn@google.com>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825582}

--- chrome/browser/chromeos/arc/fileapi/arc_documents_provider_root.cc
+++ chrome/browser/chromeos/arc/fileapi/arc_documents_provider_root.cc
@@ -294,10 +294,11 @@ void ArcDocumentsProviderRoot::GetFileInfoFromDocument(
     base::File::Error error,
     const mojom::DocumentPtr& document) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  if (document.is_null()) {
-    std::move(callback).Run(base::File::FILE_ERROR_NOT_FOUND, {});
+  if (error != base::File::FILE_OK) {
+    std::move(callback).Run(error, {});
     return;
   }
+  DCHECK(document);
 
   base::File::Info info;
   info.size = document->size;
@@ -713,10 +714,12 @@ void ArcDocumentsProviderRoot::GetExtraMetadataFromDocument(
     base::File::Error error,
     const mojom::DocumentPtr& document) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  if (document.is_null()) {
-    std::move(callback).Run(base::File::FILE_ERROR_NOT_FOUND, {});
+  if (error != base::File::FILE_OK) {
+    std::move(callback).Run(error, {});
     return;
   }
+  DCHECK(document);
+
   ExtraFileMetadata metadata;
   metadata.supports_delete = document->supports_delete;
   metadata.supports_rename = document->supports_rename;
