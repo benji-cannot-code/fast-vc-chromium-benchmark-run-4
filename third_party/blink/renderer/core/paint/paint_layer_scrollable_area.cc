ebbefb3890b838129cb802ac6b295516a3fe9bd9
Xianzhu Wang <wangxianzhu@chromium.org>
2020-11-10 02:02:10 +0000

    Use composited scrolling if resizer needs to be above descendants
    A resizer on an element containing composited stacked descendants
    needs to be painted after the descendants. In pre-CompositeAfterPaint
    this can only be achieved by using composited scrolling for the element
    so that we'll composite the resizer even if the element is not
    scrollable.
    
    In CompositeAfterPaint everything is good because we always use the
    correct paint order.
    
    Bug: 1135676
    Change-Id: Ic69b830ae3d71cd9f6b8f55deca591b485b189a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2523748
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825613}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
@@ -2564,7 +2564,8 @@ bool PaintLayerScrollableArea::ComputeNeedsCompositedScrollingInternal(
   if (CompositingReasonFinder::RequiresCompositingForRootScroller(*layer_))
     return true;
 
-  if (!layer_->ScrollsOverflow())
+  if (!layer_->ScrollsOverflow() &&
+      !layer_->NeedsReorderOverlayOverflowControls())
     return false;
 
   if (layer_->Size().IsEmpty())
