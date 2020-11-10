c4a4ce67ff88692e5403bc157ce1692bfd402f08
Manuel Rego Casasnovas <rego@igalia.com>
2020-11-10 18:58:51 +0000

    Small change in NGOutOfFlowLayoutPart::Layout()
    The method was getting the |container_content_size| as argument,
    but it was only used to compute the |container_physical_content_size|.
    The callers have already computed the |container_physical_content_size|
    before calling this method, so we can simply pass it
    and avoid recomputing it here.
    
    BUG=1098231
    
    Change-Id: I254256dd67123094086f3caa523735893a7b754e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2529163
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825886}

--- third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.h
+++ third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.h
@@ -121,7 +121,7 @@ class CORE_EXPORT NGOutOfFlowLayoutPart {
       NGBlockNode,
       const NGConstraintSpace&,
       const NGLogicalStaticPosition&,
-      LogicalSize container_content_size,
+      PhysicalSize container_physical_content_size,
       const ContainingBlockInfo&,
       const WritingDirectionMode,
       const LayoutBox* only_layout,
