d8360a7073f110719d61f3e3332a03fe87a9b7b3
Matthew Mourgos <mmourgos@chromium.org>
2020-11-10 20:10:32 +0000

    Remove unused includes from clipboard history
    Change-Id: I01b06c2660d6878886d28e7b4555ab723fcc74f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2528924
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825914}

--- ash/clipboard/clipboard_history.h
+++ ash/clipboard/clipboard_history.h
@@ -7,12 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_CLIPBOARD_CLIPBOARD_HISTORY_H_
 
 #include <list>
-#include <map>
 
 #include "ash/ash_export.h"
 #include "ash/clipboard/clipboard_history_item.h"
-#include "ash/public/cpp/session/session_observer.h"
-#include "ash/shell_observer.h"
 #include "base/component_export.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
