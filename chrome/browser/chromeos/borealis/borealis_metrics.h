883803ec42f59f7b7fd3efae4056ef523a9a359f
danielng <danielng@google.com>
2020-11-10 02:06:40 +0000

    borealis: adding startup metrics
    Adding metrics that will be used to recorded information about the
    borealis startup sequence. These will help to measure performance,
    usage and where errors are occurring.
    
    Bug: b/171339461
    Change-Id: Id5fae11f5bcf57d4fb659c7c6e7b9eba2cfe985c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2509072
    Commit-Queue: Daniel Ng <danielng@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/master@{#825617}

---
+++ chrome/browser/chromeos/borealis/borealis_metrics.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_BOREALIS_BOREALIS_METRICS_H_
+#define CHROME_BROWSER_CHROMEOS_BOREALIS_BOREALIS_METRICS_H_
+
+#include "base/metrics/histogram_functions.h"
+
+namespace borealis {
+
+extern const char kBorealisStartupNumAttemptsHistogram[];
+extern const char kBorealisStartupResultHistogram[];
+extern const char kBorealisStartupOverallTimeHistogram[];
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class BorealisStartupResult {
+  kSuccess = 0,
+  kCancelled = 1,
+  kMountFailed = 2,
+  kDiskImageFailed = 3,
+  kStartVmFailed = 4,
+  kAwaitBorealisStartupFailed = 5,
+  kMaxValue = kAwaitBorealisStartupFailed,
+};
+
+void RecordBorealisStartupNumAttemptsHistogram();
+void RecordBorealisStartupResultHistogram(BorealisStartupResult startup_result);
+void RecordBorealisStartupOverallTimeHistogram(base::TimeDelta startup_time);
+
+}  // namespace borealis
+
+#endif  // CHROME_BROWSER_CHROMEOS_BOREALIS_BOREALIS_METRICS_H_
