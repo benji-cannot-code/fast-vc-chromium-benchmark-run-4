6f2f3ed6b3909b36a3b4fa0c25e3dc7c51d3a875
Nohemi Fernandez <fernandex@chromium.org>
2020-11-10 09:22:22 +0000

    [iOS] Add separate title and message texts to sign-in notification.
    Previously, we only had the ability to include a title text in an
    infobar. Following crrev.com/c/2521619 we are able to include both.
    
    This patch updates sign-in notification infobar to include a title,
    "Signed in to Chrome" and the user email as the message.
    
    Bug: 1145592
    Change-Id: Ibd1b91737d3cfd6d30ea15cdec5fd11f49cba268
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2526105
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825741}

--- ios/chrome/browser/ui/authentication/signin_notification_infobar_delegate.h
+++ ios/chrome/browser/ui/authentication/signin_notification_infobar_delegate.h
@@ -45,6 +45,7 @@ class SigninNotificationInfoBarDelegate : public ConfirmInfoBarDelegate {
   InfoBarIdentifier GetIdentifier() const override;
 
   // ConfirmInfoBarDelegate implementation.
+  base::string16 GetTitleText() const override;
   base::string16 GetMessageText() const override;
   int GetButtons() const override;
   base::string16 GetButtonLabel(InfoBarButton button) const override;
@@ -53,6 +54,7 @@ class SigninNotificationInfoBarDelegate : public ConfirmInfoBarDelegate {
 
  private:
   gfx::Image icon_;
+  base::string16 title_;
   base::string16 message_;
   base::string16 button_text_;
 
