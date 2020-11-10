ab2ddbe0f0f338b3f486858210ba931f770b4f09
Sigurdur Asgeirsson <siggi@chromium.org>
2020-11-10 01:03:26 +0000

    Deprecate ScopedObserver.
    It's replaced by base::ScopedObservation for observing a single source,
    or base::ScopedMultiSourceObservation for observing multiple sources.
    
    Bug: 1145565
    Change-Id: Iadc2e3efee7860e1b17b86b077d4c6a2566b6a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527541
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825580}

--- base/scoped_observer.h
+++ base/scoped_observer.h
@@ -14,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "base/stl_util.h"
 
+// This class is DEPRECATED. Instead please use one of:
+// - base::ScopedObservation for observing a single source.
+// - base::ScopedMultiSourceObservation for observing multiple sources.
+//
 // ScopedObserver is used to keep track of the set of sources an object has
 // attached itself to as an observer. When ScopedObserver is destroyed it
 // removes the object as an observer from all sources it has been added to.
