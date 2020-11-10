64129ddeaf5e67e557369226ab6c74c109c73c46
David Tseng <dtseng@google.com>
2020-11-10 21:11:32 +0000

    Revert "Make assistive window show with autocorrect manager"
    Fixed: 1146877
    TBR=jopalmer@chromium.org
    
    Reason for revert:
    Breaks ChromeVox; found via bisect; see bug.
    
    This reverts commit 6cbffa8a5e099610b7b0457c729ee442e26258b9.
    
    Dependent changes (also reverted):
    73bcb673a78b
    84c821b57350
    b197149cccd33e6f626d3349867aa51281fb254a
    
    Change-Id: I65bc46c273cd64555676073cf6cd172128cfe068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2529695
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825948}

--- chrome/browser/chromeos/input_method/native_input_method_engine.cc
+++ chrome/browser/chromeos/input_method/native_input_method_engine.cc
@@ -156,8 +156,7 @@ bool NativeInputMethodEngine::IsConnectedForTesting() const {
 void NativeInputMethodEngine::OnAutocorrect(std::string typed_word,
                                             std::string corrected_word,
                                             int start_index) {
-  autocorrect_manager_->MarkAutocorrectRange(corrected_word, typed_word,
-                                             start_index);
+  autocorrect_manager_->MarkAutocorrectRange(corrected_word, start_index);
 }
 
 NativeInputMethodEngine::ImeObserver*
@@ -259,10 +258,7 @@ void NativeInputMethodEngine::ImeObserver::OnKeyEvent(
       return;
     }
   }
-  if (autocorrect_manager_->OnKeyEvent(event)) {
-    std::move(callback).Run(true);
-    return;
-  }
+  autocorrect_manager_->OnKeyEvent();
   auto key_event = ime::mojom::PhysicalKeyEvent::New(
       event.type == "keydown" ? ime::mojom::KeyEventType::kKeyDown
                               : ime::mojom::KeyEventType::kKeyUp,
@@ -319,7 +315,6 @@ void NativeInputMethodEngine::ImeObserver::OnSurroundingTextChanged(
     assistive_suggester_->OnSurroundingTextChanged(text, cursor_pos,
                                                    anchor_pos);
   }
-  autocorrect_manager_->OnSurroundingTextChanged(text, cursor_pos, anchor_pos);
   if (ShouldUseFstMojoEngine(engine_id) && remote_to_engine_.is_bound()) {
     auto selection = ime::mojom::SelectionRange::New();
     selection->anchor = anchor_pos;
@@ -372,8 +367,6 @@ void NativeInputMethodEngine::ImeObserver::OnAssistiveWindowButtonClicked(
       }
       break;
     case ui::ime::ButtonId::kUndo:
-      autocorrect_manager_->UndoAutocorrect();
-      break;
     case ui::ime::ButtonId::kAddToDictionary:
     case ui::ime::ButtonId::kNone:
       base_observer_->OnAssistiveWindowButtonClicked(button);
