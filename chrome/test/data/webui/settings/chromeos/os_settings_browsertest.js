726fdaa7b85890226edfc924c7d70c2d25e7aaea
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-11-10 04:45:03 +0000

    [CrOS settings] Add on click event to localized link
    Bug: 093185
    Change-Id: Ic66508d92a5eb9c0d72c4b21e7517cbabb4563d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2507295
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825669}

--- chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
@@ -55,6 +55,7 @@ var SettingsLocalizedLinkTest = class extends OSSettingsBrowserTest {
   /** @override */
   get extraLibraries() {
     return super.extraLibraries.concat([
+      BROWSER_SETTINGS_PATH + '../test_util.js',
       'localized_link_test.js',
     ]);
   }
