b4ca433f0f58796f9b4d3a64159cc7380669be83
Yuchen Liu <yucliu@chromium.org>
2020-11-10 06:49:00 +0000

    Update Crashpad to b6f2d06996335c951bfb72f98b5b9e22dc9bc03e
    b6f2d0699633 [Android] Fix un-paired namespace declare for higher
                 __ANDROID_API__
    
    Change-Id: I6894e4f9a9455f180fdca0b81e8540ff4613f727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527799
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Auto-Submit: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825699}

--- third_party/crashpad/crashpad/util/linux/initial_signal_dispositions.cc
+++ third_party/crashpad/crashpad/util/linux/initial_signal_dispositions.cc
@@ -21,9 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "build/build_config.h"
 
+namespace crashpad {
+
 #if __ANDROID_API__ <= 23
 
-namespace crashpad {
 namespace {
 bool LoggingSignal(int signum, sighandler_t handler) {
   sighandler_t previous = signal(signum, handler);
