5ffdfd3b7231491fc8a12cad15254263ed40e86e
Nate Chapin <japhet@chromium.org>
2020-11-10 03:43:03 +0000

    Merge NavigatorBluetooth and Bluetooth
    Change-Id: I47f74861053c14ddb9cc78ddfd360048968390e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2523065
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825648}

--- third_party/blink/renderer/modules/bluetooth/bluetooth.h
+++ third_party/blink/renderer/modules/bluetooth/bluetooth.h
@@ -16,25 +16,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_associated_receiver_set.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 
 namespace blink {
 
 class BluetoothLEScanOptions;
 class ExceptionState;
 class RequestDeviceOptions;
+class Navigator;
 class ScriptPromise;
 class ScriptState;
 
 class Bluetooth final : public EventTargetWithInlineData,
+                        public Supplement<Navigator>,
                         public PageVisibilityObserver,
                         public mojom::blink::WebBluetoothAdvertisementClient {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  explicit Bluetooth(LocalDOMWindow*);
+  static const char kSupplementName[];
+
+  // IDL exposed as navigator.bluetooth
+  static Bluetooth* bluetooth(Navigator&);
+
+  explicit Bluetooth(Navigator&);
   ~Bluetooth() override;
 
-  // IDL exposed interface:
+  // IDL exposed bluetooth interface:
   ScriptPromise getAvailability(ScriptState*, ExceptionState&);
   ScriptPromise getDevices(ScriptState*, ExceptionState&);
   ScriptPromise requestDevice(ScriptState*,
@@ -92,8 +100,6 @@ class Bluetooth final : public EventTargetWithInlineData,
   // Ensures only one BluetoothDevice instance represents each
   // Bluetooth device inside a single global object.
   HeapHashMap<String, Member<BluetoothDevice>> device_instance_map_;
-
-  Member<LocalDOMWindow> window_;
 
   HeapMojoAssociatedReceiverSet<mojom::blink::WebBluetoothAdvertisementClient,
                                 Bluetooth>
