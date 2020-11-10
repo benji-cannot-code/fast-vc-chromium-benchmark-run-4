e36ba752dee28440a04ead4d5aa5878ef9ecf25a
Sami Kyöstilä <skyostil@chromium.org>
2020-11-10 11:12:49 +0000

    Revert "Set Xlib error handler after GTK X11 initialization"
    This reverts commit 2ed82a41dca579689a0dc69c9c18d22babb4ef74.
    
    Reason for revert: Looks like this is causing the Linux perf tests to crash in xlib_support.cc: https://ci.chromium.org/p/chrome/builders/ci/linux-perf/13312
    
    Original change's description:
    > Set Xlib error handler after GTK X11 initialization
    >
    > The default GTK error handler exits the process, which we don't want.
    > We used to reset the error handler at some point after GTK
    > initialization, but this became lost after r815319.
    >
    > Also refactor the XlibLoader instance to be global so it doesn't have
    > to be created multiple times for each connection.
    >
    > R=​sky
    >
    > Bug: 1145929
    > Change-Id: I73a3f27551534c2262bd8994d33e30fc2a0a4e27
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527743
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#825585}
    
    TBR=sky@chromium.org,thomasanderson@chromium.org
    
    Change-Id: Ie9f3614a8773220e0d6d6596d14c858a879a6704
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1145929
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2529152
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825761}

--- ui/gfx/x/xlib_support.h
+++ ui/gfx/x/xlib_support.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 
 struct _XDisplay;
+class XlibLoader;
 
 namespace x11 {
 
@@ -26,12 +27,6 @@ enum class XlibDisplayType {
   kSyncing,
 };
 
-// Loads Xlib, initializes threads, and sets a default error handler.
-COMPONENT_EXPORT(X11) void InitXlib();
-
-// Sets an async error handler which only logs an error message.
-COMPONENT_EXPORT(X11) void SetXlibErrorHandler();
-
 // A scoped Xlib display.
 class COMPONENT_EXPORT(X11) XlibDisplay {
  public:
@@ -44,6 +39,7 @@ class COMPONENT_EXPORT(X11) XlibDisplay {
   explicit XlibDisplay(const std::string& address);
 
   struct _XDisplay* display_ = nullptr;
+  std::unique_ptr<XlibLoader> xlib_loader_;
 };
 
 // A temporary wrapper around an unowned Xlib display that adds behavior
@@ -61,10 +57,13 @@ class COMPONENT_EXPORT(X11) XlibDisplayWrapper {
   XlibDisplayWrapper& operator=(XlibDisplayWrapper&& other);
 
  private:
-  XlibDisplayWrapper(struct _XDisplay* display, XlibDisplayType type);
+  XlibDisplayWrapper(XlibLoader* xlib_loader,
+                     struct _XDisplay* display,
+                     XlibDisplayType type);
 
   friend class Connection;
 
+  XlibLoader* xlib_loader_;
   struct _XDisplay* display_;
   XlibDisplayType type_;
 };
