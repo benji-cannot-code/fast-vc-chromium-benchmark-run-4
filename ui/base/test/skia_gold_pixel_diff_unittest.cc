5976228d27d0603e9fb01e08c7143caff4ee4cc7
Sven Zheng <svenzheng@chromium.org>
2020-11-10 01:15:57 +0000

    Fix pixel diff unit tests
    For some trunk builders, they set env var CHROMIUM_TEST_LAUNCHER_BOT_MODE.
    For unit test, we need to unset the var to test some scenario.
    https://source.chromium.org/chromium/chromium/src/+/master:ui/base/test/skia_gold_pixel_diff.cc;l=153;drc=07e4c76dd0f6b37146a1c35e4a96fae53aff7371
    
    Bug: 1146399
    Change-Id: Ifb71831060a4bd779ef48e83ca8d66741e5e57e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2523555
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825590}

--- ui/base/test/skia_gold_pixel_diff_unittest.cc
+++ ui/base/test/skia_gold_pixel_diff_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/files/file_util.h"
+#include "base/test/scoped_environment_variable_override.h"
 #include "base/test/test_switches.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -109,6 +110,8 @@ TEST_F(SkiaGoldPixelDiffTest, LocalNoLuciAuth) {
   MockSkiaGoldPixelDiff mock_pixel;
   auto* cmd_line = base::CommandLine::ForCurrentProcess();
   cmd_line->RemoveSwitch(switches::kTestLauncherBotMode);
+  base::test::ScopedEnvironmentVariableOverride env_override(
+      "CHROMIUM_TEST_LAUNCHER_BOT_MODE");
 
   EXPECT_CALL(mock_pixel, LaunchProcess(_)).Times(AnyNumber());
   EXPECT_CALL(mock_pixel, LaunchProcess(AllOf(Property(
@@ -350,6 +353,8 @@ TEST_F(SkiaGoldPixelDiffTest, MakeGerritCommentInvalidFlag) {
 TEST_F(SkiaGoldPixelDiffTest, DryRunLocally) {
   auto* cmd_line = base::CommandLine::ForCurrentProcess();
   cmd_line->RemoveSwitch(switches::kTestLauncherBotMode);
+  base::test::ScopedEnvironmentVariableOverride env_override(
+      "CHROMIUM_TEST_LAUNCHER_BOT_MODE");
 
   MockSkiaGoldPixelDiff mock_pixel;
   EXPECT_CALL(mock_pixel, LaunchProcess(_)).Times(AnyNumber());
