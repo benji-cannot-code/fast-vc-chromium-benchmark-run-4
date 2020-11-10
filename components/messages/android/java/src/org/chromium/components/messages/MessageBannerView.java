483fdcfcf35dc7339d9cbbccd320a62be9bd7a15
Lijin Shen <lazzzis@google.com>
2020-11-10 17:42:01 +0000

    [Messages] Use ButtonCompat to represent text button
    Use ButtonCompat instead to represent a text button.
    
    Bug: 1123947
    Change-Id: Ibe75a7fc47d602a3a61625aa0a24fe35d7ba0c04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2524963
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825862}

--- components/messages/android/java/src/org/chromium/components/messages/MessageBannerView.java
+++ components/messages/android/java/src/org/chromium/components/messages/MessageBannerView.java
@@ -61,6 +61,7 @@ public class MessageBannerView extends LinearLayout {
     }
 
     void setPrimaryButtonText(String text) {
+        mPrimaryButton.setVisibility(VISIBLE);
         mPrimaryButton.setText(text);
     }
 
