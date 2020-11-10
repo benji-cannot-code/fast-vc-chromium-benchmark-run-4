e8578a8deeb7d1baf0cdf44c600e78f82a51c2f6
Alan Cutter <alancutter@chromium.org>
2020-11-10 06:37:40 +0000

    Migrate Google Docs preinstalled web app from Chrome OS JSON to C++
    This CL migrates the Chrome OS JSON config for the Google Docs
    preinstalled web app (located here [0]) up into the Chrome binary.
    
    This default web app is going to be used by Chrome Windows/Mac/Linux
    so this CL is deduping the offline data into one place.
    A separate CL will clean up the redundant JSON config.
    
    [0]: https://chrome-internal.googlesource.com/chromeos/overlays/chromeos-overlay/+/master/chromeos-base/chromeos-default-apps/files/web_apps/docs.json
    
    Bug: 1104692
    Change-Id: I4f713bf1dec230fe4c6f3c691d4d954ac0c5f340
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2524809
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825697}

--- chrome/browser/web_applications/external_web_app_manager.cc
+++ chrome/browser/web_applications/external_web_app_manager.cc
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/web_app_install_utils.h"
 #include "chrome/browser/web_applications/extension_status_utils.h"
 #include "chrome/browser/web_applications/external_web_app_utils.h"
-#include "chrome/browser/web_applications/preinstalled_web_apps.h"
+#include "chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_apps.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/pref_names.h"
---
+++ chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_app_utils.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_WEB_APPLICATIONS_PREINSTALLED_WEB_APPS_PREINSTALLED_WEB_APP_UTILS_H_
+#define CHROME_BROWSER_WEB_APPLICATIONS_PREINSTALLED_WEB_APPS_PREINSTALLED_WEB_APP_UTILS_H_
+
+#include "chrome/common/web_application_info.h"
+
+namespace web_app {
+
+std::map<SquareSizePx, SkBitmap> LoadBundledIcons(
+    const std::initializer_list<int>& icon_resource_ids);
+
+}  // namespace web_app
+
+#endif  // CHROME_BROWSER_WEB_APPLICATIONS_PREINSTALLED_WEB_APPS_PREINSTALLED_WEB_APP_UTILS_H_
