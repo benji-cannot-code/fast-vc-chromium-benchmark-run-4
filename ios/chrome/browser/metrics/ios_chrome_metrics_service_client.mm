fb7118c130118dda27a682c23d20df164f4f0308
Weilun Shi <sweilun@chromium.org>
2020-11-10 17:51:37 +0000

    Register EntropySourceProvider for iOS
    Register EntropySourceProvider in ios_chrome_metrics_service_client so
    that all iOS reports will have low entropy source values populated.
    
    Bug: b/172461970
    Change-Id: Ib216094ed532824fde69f97c7c772bf7bbabd25e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2528308
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825865}

--- ios/chrome/browser/metrics/ios_chrome_metrics_service_client.mm
+++ ios/chrome/browser/metrics/ios_chrome_metrics_service_client.mm
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/metrics/cpu_metrics_provider.h"
 #include "components/metrics/demographics/demographic_metrics_provider.h"
 #include "components/metrics/drive_metrics_provider.h"
+#include "components/metrics/entropy_state_provider.h"
 #include "components/metrics/field_trials_provider.h"
 #include "components/metrics/metrics_log_uploader.h"
 #include "components/metrics/metrics_pref_names.h"
@@ -265,6 +266,8 @@ void IOSChromeMetricsServiceClient::Initialize() {
 }
 
 void IOSChromeMetricsServiceClient::RegisterMetricsServiceProviders() {
+  PrefService* local_state = GetApplicationContext()->GetLocalState();
+
   metrics_service_->RegisterMetricsProvider(
       std::make_unique<metrics::NetworkMetricsProvider>(
           base::BindRepeating(&GetNetworkConnectionTrackerAsync)));
@@ -273,8 +276,7 @@ void IOSChromeMetricsServiceClient::RegisterMetricsServiceProviders() {
       std::make_unique<OmniboxMetricsProvider>());
 
   auto stability_metrics_provider =
-      std::make_unique<IOSChromeStabilityMetricsProvider>(
-          GetApplicationContext()->GetLocalState());
+      std::make_unique<IOSChromeStabilityMetricsProvider>(local_state);
   stability_metrics_provider_ = stability_metrics_provider.get();
   metrics_service_->RegisterMetricsProvider(
       std::move(stability_metrics_provider));
@@ -287,6 +289,9 @@ void IOSChromeMetricsServiceClient::RegisterMetricsServiceProviders() {
   // MetricsServiceAccessor::IsMetricsReportingEnabled() to return true.
   metrics_service_->RegisterMetricsProvider(CreateFileMetricsProvider(
       metrics_state_manager_->IsMetricsReportingEnabled()));
+
+  metrics_service_->RegisterMetricsProvider(
+        std::make_unique<metrics::EntropyStateProvider>(local_state));
 
   metrics_service_->RegisterMetricsProvider(
       std::make_unique<metrics::ScreenInfoMetricsProvider>());
