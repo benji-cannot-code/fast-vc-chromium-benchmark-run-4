485e1afed3952ac48d318295fcd1584c186bcfc6
Dan Harrington <harringtond@chromium.org>
2020-11-10 22:39:41 +0000

    Record OpenInNewTab and OpenAction from bottom sheet
    A server change caused us to fail to compute the index
    of the card when the action originates from the bottom sheet.
    This change lets us continue to record metrics even if we
    don't identify the card index, using MAX_INT as a special
    value.
    
    While this fix isn't ideal, it's better than missing out
    on these metrics. We can improve this later.
    
    Bug: 1111101
    Change-Id: I063835a44ad4abb92f05e997fd27c535dc4dcb23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2522574
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825987}

--- components/feed/core/v2/metrics_reporter.h
+++ components/feed/core/v2/metrics_reporter.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_FEED_CORE_V2_METRICS_REPORTER_H_
 #define COMPONENTS_FEED_CORE_V2_METRICS_REPORTER_H_
 
+#include <climits>
 #include <map>
 
 #include "base/memory/weak_ptr.h"
@@ -62,6 +63,11 @@ enum class FeedUserActionType {
 // Note this is inherited only for testing.
 class MetricsReporter {
  public:
+  // For 'index_in_stream' parameters, when the card index is unknown.
+  // This is most likely to happen when the action originates from the bottom
+  // sheet.
+  static const int kUnknownCardIndex = INT_MAX;
+
   explicit MetricsReporter(const base::TickClock* clock,
                            PrefService* profile_prefs);
   virtual ~MetricsReporter();
