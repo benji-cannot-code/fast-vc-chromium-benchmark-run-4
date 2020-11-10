49a6c804b7f39fe321d5f26a977bd958372c0634
Liquan (Max) Gu <maxlg@chromium.org>
2020-11-10 01:04:50 +0000

    [WebLayer] pass waitForUpdatedDetails to ui by parameter
    Before the CL, PaymentRequestUi takes waitForUpdatedDetails from
    CPRService by PaymentUiService.Delegate.waitForUpdatedDetails(). Using
    a delegate is not as simplified as a method parameter.
    
    After the CL, PaymentRequestUi takes the value by method parameters.
    
    Change-Id: Icb61a32c6df6d6f2b01d6f6669483cd4b2111b30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527543
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825581}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ChromePaymentRequestService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ChromePaymentRequestService.java
@@ -345,7 +345,7 @@ public class ChromePaymentRequestService implements BrowserPaymentRequest,
         ChromeActivity chromeActivity = ChromeActivity.fromWebContents(mWebContents);
         if (quitShowIfActivityNotFound(chromeActivity) || !buildUI(chromeActivity)) return false;
         if (!mPaymentUiService.shouldSkipShowingPaymentRequestUi() && mSkipToGPayHelper == null) {
-            mPaymentUiService.getPaymentRequestUI().show();
+            mPaymentUiService.getPaymentRequestUI().show(mWaitForUpdatedDetails);
         }
         return true;
     }
@@ -802,12 +802,6 @@ public class ChromePaymentRequestService implements BrowserPaymentRequest,
      */
     private boolean parseAndValidateDetailsForSkipToGPayHelper(PaymentDetails details) {
         return mSkipToGPayHelper == null || mSkipToGPayHelper.setShippingOptionIfValid(details);
-    }
-
-    // Implements PaymentUiService.Delegate:
-    @Override
-    public boolean waitForUpdatedDetails() {
-        return mWaitForUpdatedDetails;
     }
 
     // Implements PaymentUiService.Delegate:
