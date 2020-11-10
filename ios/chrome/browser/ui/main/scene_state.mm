c778d5b1f28c97c8b9791baa2f4b51ba74652ed2
Stepan Khapugin <stkhapugin@chromium.org>
2020-11-10 16:32:05 +0000

    [iOS] Expose connected scene agents publicly.
    Expose connected agents for features to find the associated agent.
    
    Bug: none
    Change-Id: Id8ec1e86897ea72b8b61bbf24281b6c7e90cd06c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2523179
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825833}

--- ios/chrome/browser/ui/main/scene_state.mm
+++ ios/chrome/browser/ui/main/scene_state.mm
@@ -71,6 +71,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [agent setSceneState:self];
 }
 
+- (NSArray*)connectedAgents {
+  return self.agents;
+}
+
 #pragma mark - Setters & Getters.
 
 - (void)setWindow:(UIWindow*)window {
