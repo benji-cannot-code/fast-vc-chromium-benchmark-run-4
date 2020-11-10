ba50512e3dcb30d4d49c93e805faf9c1898f35d6
Joon Ahn <joonbug@chromium.org>
2020-11-10 03:04:14 +0000

    Scanning: Show help text when scanners are not available
    http://screen/6qQmdsFTNNFnfcv
    
    Bug: 1059779
    Test: browser_tests --gtest_filter=Scanning*
    Change-Id: I3706aebdfbdfa18e495e114767984758c81c3086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521231
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825636}

--- chromeos/components/scanning/resources/scanner_select.js
+++ chromeos/components/scanning/resources/scanner_select.js
@@ -67,4 +67,12 @@ Polymer({
   getTokenAsString_(scanner) {
     return tokenToString(scanner.id);
   },
+
+  /**
+   * Helper function to decide whether to hide the scanner connection help link.
+   * @protected
+   */
+  shouldHideScanHelpLink_() {
+    return !this.loaded || this.scanners.length !== 0;
+  }
 });
