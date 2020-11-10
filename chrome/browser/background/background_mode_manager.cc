4bcef1469c3d72b616637b288c8663efd410dc10
Erik Chen <erikchen@chromium.org>
2020-11-10 00:41:20 +0000

    Lacros: Don't create status icons.
    There is no system tray on ChromeOS so it does not make sense to create
    status icons.
    
    Bug: 1128963
    Change-Id: I1a14371bd5a9d8c5e70ea72179308681917a1fcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2528126
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#825567}

--- chrome/browser/background/background_mode_manager.cc
+++ chrome/browser/background/background_mode_manager.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
@@ -868,7 +869,7 @@ void BackgroundModeManager::CreateStatusTrayIcon() {
 
   // Since there are multiple profiles which share the status tray, we now
   // use the browser process to keep track of it.
-#if !defined(OS_MAC) && !defined(OS_CHROMEOS)
+#if !defined(OS_MAC) && !defined(OS_CHROMEOS) && !BUILDFLAG(IS_LACROS)
   if (!status_tray_)
     status_tray_ = g_browser_process->status_tray();
 #endif
