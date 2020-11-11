a2cc9fe85f4eed9faf68b30b411e342381d8fcf9
Emily Stark <estark@google.com>
2020-11-11 04:52:00 +0000

    Introduce new lookalike Safety Tip feature for measuring SDD
    This CL adds a new Safety Tips feature flag which enables lookalike
    Safety Tips, specifically for the purpose of measuring how Simplified
    Domain Display behaves. This is necessary because multiple Finch
    experiments can't enable the same feature at the same time, and it's
    possible that we'll be running separate Safety Tips and Simplified
    Domain Display experiments at the same time. Ordinarily this would run
    the risk of muddying our data between the experiments, but since
    Safety Tips are only encountered very rarely, and we specifically
    filter out users who see multiple Safety Tipped sites in our
    experiment metrics (as they are outliers), we don't anticipate this to
    be a problem.
    
    Bug: 1146471
    Change-Id: I033476e8e1cb3fb4afc2c7a1f8669ea28b2c7b9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2530253
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826136}

--- components/security_state/core/security_state.cc
+++ components/security_state/core/security_state.cc
@@ -331,6 +331,8 @@ bool ShouldShowDangerTriangleForWarningLevel() {
 
 bool IsSafetyTipUIFeatureEnabled() {
   return base::FeatureList::IsEnabled(features::kSafetyTipUI) ||
+         base::FeatureList::IsEnabled(
+             features::kSafetyTipUIForSimplifiedDomainDisplay) ||
          base::FeatureList::IsEnabled(features::kSafetyTipUIOnDelayedWarning);
 }
 
