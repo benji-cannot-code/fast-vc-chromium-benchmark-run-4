52f5e7bd66c2028946026e4a56a70f5102afc665
Lukasz Anforowicz <lukasza@chromium.org>
2020-11-11 20:39:24 +0000

    Ignore browser-inititated requests in Cross-Origin-Resource-Policy.
    Fixed: 1141158
    Change-Id: Iae559652ee39fb2f63c8658e5cfdf8e199df149d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521229
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826445}

--- services/network/network_service_unittest.cc
+++ services/network/network_service_unittest.cc
@@ -1174,6 +1174,8 @@ TEST_F(NetworkServiceTestWithService, SetNetworkConditions) {
 
   ResourceRequest request;
   request.url = test_server()->GetURL("/nocache.html");
+  request.request_initiator =
+      url::Origin::Create(GURL("https://initiator.example.com"));
   request.method = "GET";
 
   StartLoadingURL(request, 0);
