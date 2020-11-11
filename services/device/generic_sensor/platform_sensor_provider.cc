1f52144efecb6cd753195163fbc0ba2aee35269a
Harvey Yang <chenghaoyang@chromium.org>
2020-11-11 07:30:31 +0000

    generic_sensor: Add PlatformSensor(Provider)ChromeOS
    This commit adds PlatformSensor(Provider)ChromeOS in generic sensor for
    chromebooks. They get data by registering a Sensor Hal Client to Sensor
    Hal Dispatcher, and waits for a mojo connection to IIO Service.
    
    BUG=chromium:1116940
    TEST=unit tests and run on octopus
    
    Change-Id: I63af90d8b617acb275e35422e2c667904c10b450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2400398
    Commit-Queue: Cheng-Hao Yang <chenghaoyang@chromium.org>
    Reviewed-by: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826170}

--- services/device/generic_sensor/platform_sensor_provider.cc
+++ services/device/generic_sensor/platform_sensor_provider.cc
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/device/generic_sensor/platform_sensor_provider_win.h"
 #include "services/device/generic_sensor/platform_sensor_provider_winrt.h"
 #include "services/device/public/cpp/device_features.h"
-#elif (defined(OS_LINUX) || defined(OS_CHROMEOS)) && defined(USE_UDEV)
+#elif defined(OS_CHROMEOS)
+#include "services/device/generic_sensor/platform_sensor_provider_chromeos.h"
+#elif defined(OS_LINUX) && defined(USE_UDEV)
 #include "services/device/generic_sensor/platform_sensor_provider_linux.h"
 #endif
 
@@ -34,7 +36,9 @@ std::unique_ptr<PlatformSensorProvider> PlatformSensorProvider::Create() {
   } else {
     return std::make_unique<PlatformSensorProviderWin>();
   }
-#elif (defined(OS_LINUX) || defined(OS_CHROMEOS)) && defined(USE_UDEV)
+#elif defined(OS_CHROMEOS)
+  return std::make_unique<PlatformSensorProviderChromeOS>();
+#elif defined(OS_LINUX) && defined(USE_UDEV)
   return std::make_unique<PlatformSensorProviderLinux>();
 #else
   return nullptr;
