1c362faeec5676d068bf3fc87de7774401ba54c4
Nate Chapin <japhet@chromium.org>
2020-11-11 22:58:26 +0000

    Merge NavigatorScheduling and Scheduling
    Change-Id: Ic9adc4348e51e831b6a4eba184a7438512774546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527776
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826514}

--- third_party/blink/renderer/core/frame/scheduling.h
+++ third_party/blink/renderer/core/frame/scheduling.h
@@ -6,22 +6,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_FRAME_SCHEDULING_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_FRAME_SCHEDULING_H_
 
-#include "third_party/blink/renderer/platform/bindings/script_state.h"
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
 
 class IsInputPendingOptions;
+class Navigator;
 
 // Low-level scheduling primitives for JS scheduler implementations.
-class Scheduling : public ScriptWrappable {
+class CORE_EXPORT Scheduling : public ScriptWrappable,
+                               public Supplement<Navigator> {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  bool isInputPending(ScriptState*, const IsInputPendingOptions* options) const;
+  static const char kSupplementName[];
+  static Scheduling* scheduling(Navigator&);
+  explicit Scheduling(Navigator&);
+
+  bool isInputPending(const IsInputPendingOptions* options) const;
   bool isFramePending() const;
+
+  void Trace(Visitor*) const override;
 };
 
 }  // namespace blink
