a22c12ed88cc8f7e31fc58c19a0d8c9388abdac0
Gavin Williams <gavinwill@chromium.org>
2020-11-11 20:53:56 +0000

    scanning: Add CancelScan to LorgnetteScannerManager
    Calls LorgnetteManagerClient CancelScan API to cancel the currently
    running scan job.
    
    Bug: 1059779
    Change-Id: I31da3b2a37caa472eac1f7b781cd3d9f6ca9f465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532723
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826452}

--- chrome/browser/chromeos/scanning/lorgnette_scanner_manager.h
+++ chrome/browser/chromeos/scanning/lorgnette_scanner_manager.h
@@ -35,6 +35,7 @@ class LorgnetteScannerManager : public KeyedService {
   using PageCallback = base::RepeatingCallback<void(std::string scan_data,
                                                     uint32_t page_number)>;
   using CompletionCallback = base::OnceCallback<void(bool success)>;
+  using CancelCallback = base::OnceCallback<void(bool success)>;
 
   ~LorgnetteScannerManager() override = default;
 
@@ -63,6 +64,10 @@ class LorgnetteScannerManager : public KeyedService {
                     ProgressCallback progress_callback,
                     PageCallback page_callback,
                     CompletionCallback completion_callback) = 0;
+
+  // Request to cancel the currently running scan job. This function makes the
+  // assumption that LorgnetteManagerClient only has one scan running at a time.
+  virtual void CancelScan(CancelCallback cancel_callback) = 0;
 };
 
 }  // namespace chromeos
