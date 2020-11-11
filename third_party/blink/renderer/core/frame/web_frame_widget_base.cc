470418481ac953b6638fbec332e1abb73f48ef23
Dave Tapuska <dtapuska@chromium.org>
2020-11-11 20:15:01 +0000

    Move hit testing functions into WebFrameWidgetBase.
    Remove duplicated hit testing implementation and use the base
    version of it.
    
    BUG=1097816
    
    Change-Id: I79feb29cf0712ee8554336b3a26e10c05fa1c348
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527263
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826427}

--- third_party/blink/renderer/core/frame/web_frame_widget_base.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_base.cc
@@ -1830,6 +1830,14 @@ WebFrameWidgetBase::EnsureCompositorMutatorDispatcher(
   return mutator_dispatcher_;
 }
 
+HitTestResult WebFrameWidgetBase::CoreHitTestResultAt(
+    const gfx::PointF& point_in_viewport) {
+  LocalFrameView* view = LocalRootImpl()->GetFrameView();
+  FloatPoint point_in_root_frame(
+      view->ViewportToFrame(FloatPoint(point_in_viewport)));
+  return HitTestResultForRootFramePos(point_in_root_frame);
+}
+
 cc::AnimationHost* WebFrameWidgetBase::AnimationHost() const {
   return widget_base_->AnimationHost();
 }
@@ -2836,7 +2844,7 @@ HitTestResult WebFrameWidgetBase::HitTestResultForRootFramePos(
           pos_in_root_frame);
   HitTestLocation location(doc_point);
   HitTestResult result =
-      LocalRootImpl()->GetFrame()->GetEventHandler().HitTestResultAtLocation(
+      LocalRootImpl()->GetFrame()->View()->HitTestWithThrottlingAllowed(
           location, HitTestRequest::kReadOnly | HitTestRequest::kActive);
   result.SetToShadowHostIfInRestrictedShadowRoot();
   return result;
@@ -2859,6 +2867,10 @@ const viz::FrameSinkId& WebFrameWidgetBase::GetFrameSinkId() {
   // printing and placeholders. But if we go to use it, it should be valid.
   DCHECK(frame_sink_id_.is_valid());
   return frame_sink_id_;
+}
+
+WebHitTestResult WebFrameWidgetBase::HitTestResultAt(const gfx::PointF& point) {
+  return CoreHitTestResultAt(point);
 }
 
 WebPlugin* WebFrameWidgetBase::GetFocusedPluginContainer() {
--- third_party/blink/renderer/core/scheduler_integration_tests/frame_throttling_test.cc
+++ third_party/blink/renderer/core/scheduler_integration_tests/frame_throttling_test.cc
@@ -452,7 +452,7 @@ TEST_P(FrameThrottlingTest, ThrottledLifecycleUpdate) {
               frame_document->Lifecycle().GetState());
 
     // A hit test will not force a complete lifecycle update.
-    WebView().HitTestResultAt(gfx::PointF());
+    WebView().MainFrameWidget()->HitTestResultAt(gfx::PointF());
     EXPECT_EQ(DocumentLifecycle::kPaintClean,
               frame_document->Lifecycle().GetState());
   } else {
@@ -462,7 +462,7 @@ TEST_P(FrameThrottlingTest, ThrottledLifecycleUpdate) {
               frame_document->Lifecycle().GetState());
 
     // A hit test will not force a complete lifecycle update.
-    WebView().HitTestResultAt(gfx::PointF());
+    WebView().MainFrameWidget()->HitTestResultAt(gfx::PointF());
     EXPECT_EQ(DocumentLifecycle::kPaintClean,
               frame_document->Lifecycle().GetState());
   }
