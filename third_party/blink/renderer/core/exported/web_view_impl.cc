ba134cfe309b579e99da3c95dacaa255f01cef49
Dave Tapuska <dtapuska@chromium.org>
2020-11-11 16:23:59 +0000

    Move BeginMainFrame into WebFrameWidgetBase.
    This collapses two implementations into a common one in the base
    class.
    
    BUG=1097816
    
    Change-Id: I31a9edbe6af04450c12521a7a9b68907df8fb18c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2526172
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826304}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -146,7 +146,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/page/page_popup_client.h"
 #include "third_party/blink/renderer/core/page/pointer_lock_controller.h"
-#include "third_party/blink/renderer/core/page/scrolling/fragment_anchor.h"
 #include "third_party/blink/renderer/core/page/scrolling/scrolling_coordinator.h"
 #include "third_party/blink/renderer/core/page/scrolling/top_document_root_scroller_controller.h"
 #include "third_party/blink/renderer/core/paint/compositing/paint_layer_compositor.h"
@@ -1333,35 +1332,6 @@ void WebViewImpl::SetKeyboardFocusURL(const KURL& url) {
 
 WebViewFrameWidget* WebViewImpl::MainFrameViewWidget() {
   return web_widget_;
-}
-
-void WebViewImpl::BeginFrame(base::TimeTicks last_frame_time) {
-  TRACE_EVENT1("blink", "WebViewImpl::beginFrame", "frameTime",
-               last_frame_time);
-  DCHECK(!last_frame_time.is_null());
-
-  if (!MainFrameImpl())
-    return;
-
-  MainFrameImpl()
-      ->GetFrame()
-      ->GetEventHandler()
-      .RecomputeMouseHoverStateIfNeeded();
-
-  if (LocalFrameView* view = MainFrameImpl()->GetFrameView()) {
-    if (FragmentAnchor* anchor = view->GetFragmentAnchor())
-      anchor->PerformPreRafActions();
-  }
-
-  base::Optional<LocalFrameUkmAggregator::ScopedUkmHierarchicalTimer> ukm_timer;
-  if (WidgetBase::ShouldRecordBeginMainFrameMetrics()) {
-    ukm_timer.emplace(MainFrameImpl()
-                          ->GetFrame()
-                          ->View()
-                          ->EnsureUkmAggregator()
-                          .GetScopedTimer(LocalFrameUkmAggregator::kAnimate));
-  }
-  PageWidgetDelegate::Animate(*page_, last_frame_time);
 }
 
 void WebViewImpl::UpdateLifecycle(WebLifecycleUpdate requested_update,
--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -80,7 +80,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/page/page_popup.h"
 #include "third_party/blink/renderer/core/page/pointer_lock_controller.h"
-#include "third_party/blink/renderer/core/page/validation_message_client.h"
 #include "third_party/blink/renderer/core/paint/compositing/paint_layer_compositor.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/platform/graphics/color.h"
@@ -308,34 +307,6 @@ void WebFrameWidgetImpl::UpdateMainFrameLayoutSize() {
   gfx::Size layout_size = *size_;
 
   view->SetLayoutSize(WebSize(layout_size));
-}
-
-void WebFrameWidgetImpl::BeginMainFrame(base::TimeTicks last_frame_time) {
-  TRACE_EVENT1("blink", "WebFrameWidgetImpl::beginFrame", "frameTime",
-               last_frame_time);
-  DCHECK(!last_frame_time.is_null());
-
-  if (!LocalRootImpl())
-    return;
-
-  // Dirty bit on MouseEventManager is not cleared in OOPIFs after scroll
-  // or layout changes. Ensure the hover state is recomputed if necessary.
-  LocalRootImpl()
-      ->GetFrame()
-      ->GetEventHandler()
-      .RecomputeMouseHoverStateIfNeeded();
-
-  if (WidgetBase::ShouldRecordBeginMainFrameMetrics()) {
-    SCOPED_UMA_AND_UKM_TIMER(
-        LocalRootImpl()->GetFrame()->View()->EnsureUkmAggregator(),
-        LocalFrameUkmAggregator::kAnimate);
-    PageWidgetDelegate::Animate(*GetPage(), last_frame_time);
-  } else {
-    PageWidgetDelegate::Animate(*GetPage(), last_frame_time);
-  }
-  // Animate can cause the local frame to detach.
-  if (LocalRootImpl())
-    GetPage()->GetValidationMessageClient().LayoutOverlay();
 }
 
 void WebFrameWidgetImpl::DidBeginMainFrame() {
