7e07e35a529e889232bd749ae4340add426dc756
Shakti Sahu <shaktisahu@chromium.org>
2020-11-11 13:19:10 +0000

    Read Later : Hooked up context menu to the reading list backend
    This CL :
    1 - Connects context menu read later item to downloads backend.
    2 - Adds reading list item when context menu is clicked
    
    Bug: 1145825
    Change-Id: Id8225bcdf74373ad2a1165698420195863e22d58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2520085
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826251}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegate.java
@@ -21,6 +21,7 @@ import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.DefaultBrowserInfo;
 import org.chromium.chrome.browser.IntentHandler;
+import org.chromium.chrome.browser.bookmarks.BookmarkUtils;
 import org.chromium.chrome.browser.compositor.bottombar.ephemeraltab.EphemeralTabCoordinator;
 import org.chromium.chrome.browser.contextmenu.ContextMenuItemDelegate;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
@@ -31,6 +32,7 @@ import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
 import org.chromium.chrome.browser.tab.state.CriticalPersistedTabData;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.document.TabDelegate;
+import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.components.embedder_support.util.UrlUtilities;
 import org.chromium.content_public.browser.LoadUrlParams;
@@ -51,15 +53,18 @@ public class TabContextMenuItemDelegate implements ContextMenuItemDelegate {
     private boolean mLoadOriginalImageRequestedForPageLoad;
     private EmptyTabObserver mDataReductionProxyContextMenuTabObserver;
     private final Supplier<EphemeralTabCoordinator> mEphemeralTabCoordinatorSupplier;
+    private final Supplier<SnackbarManager> mSnackbarManagerSupplier;
 
     /**
      * Builds a {@link TabContextMenuItemDelegate} instance.
      */
     public TabContextMenuItemDelegate(Tab tab, TabModelSelector tabModelSelector,
-            Supplier<EphemeralTabCoordinator> ephemeralTabCoordinatorSupplier) {
+            Supplier<EphemeralTabCoordinator> ephemeralTabCoordinatorSupplier,
+            Supplier<SnackbarManager> snackbarManagerSupplier) {
         mTab = (TabImpl) tab;
         mTabModelSelector = tabModelSelector;
         mEphemeralTabCoordinatorSupplier = ephemeralTabCoordinatorSupplier;
+        mSnackbarManagerSupplier = snackbarManagerSupplier;
 
         mDataReductionProxyContextMenuTabObserver = new EmptyTabObserver() {
             @Override
@@ -252,6 +257,12 @@ public class TabContextMenuItemDelegate implements ContextMenuItemDelegate {
             return;
         }
         mEphemeralTabCoordinatorSupplier.get().requestOpenSheet(url, title, mTab.isIncognito());
+    }
+
+    @Override
+    public void onReadLater(String url, String title) {
+        BookmarkUtils.addToReadingList(
+                url, title, mSnackbarManagerSupplier.get(), mTab.getContext());
     }
 
     @Override
