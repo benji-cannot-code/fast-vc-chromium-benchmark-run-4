184af2cee699d2588df1a10c999125b97417b15e
Michael Hansen <hansenmichael@google.com>
2020-11-11 00:28:56 +0000

    [Nearby] Disable/enable logic for confirm button during onboarding.
    Adds disable/enable logic for the confirm button when on the contact
    visibility selection page during the onboarding process for Nearby
    Share.
    
    Screenshot:
      https://screenshot.googleplex.com/AKUymEEvWtvCTrU.png
    
    Bug: b:168841167
    Change-Id: Idf6d92f743c282262e410b2ec2a0045a0ad5d4b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527892
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826029}

--- chrome/browser/resources/nearby_share/shared/nearby_contact_visibility.js
+++ chrome/browser/resources/nearby_share/shared/nearby_contact_visibility.js
@@ -108,6 +108,12 @@ Polymer({
       type: Array,
       value: null,
     },
+
+    isVisibilitySelected: {
+      type: Boolean,
+      computed: 'isVisibilitySelected_(selectedVisibility)',
+      notify: true,
+    },
   },
 
   /** @private {?nearbyShare.mojom.ContactManagerInterface} */
