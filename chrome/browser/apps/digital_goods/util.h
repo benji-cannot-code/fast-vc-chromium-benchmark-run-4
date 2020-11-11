4f4c54125d4b417881903a381ccfc12893831cb8
Jeevan Shikaram <jshikaram@chromium.org>
2020-11-11 04:21:07 +0000

    [Play Billing] Perform checks before returning DigitalGoods.
    This CL checks to see if the the payment method is supported and if
    there is an APK that will be able to handle the request.
    
    Bug: 1137688
    Change-Id: I93508af961fade71a584b54a76b13c610f8e3a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2528809
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826128}

---
+++ chrome/browser/apps/digital_goods/util.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_APPS_DIGITAL_GOODS_UTIL_H_
+#define CHROME_BROWSER_APPS_DIGITAL_GOODS_UTIL_H_
+
+#include <string>
+
+namespace content {
+class RenderFrameHost;
+}  // namespace content
+
+namespace apps {
+
+std::string GetTwaPackageName(content::RenderFrameHost* render_frame_host);
+std::string GetScope(content::RenderFrameHost* render_frame_host);
+
+}  // namespace apps
+
+#endif  // CHROME_BROWSER_APPS_DIGITAL_GOODS_UTIL_H_
