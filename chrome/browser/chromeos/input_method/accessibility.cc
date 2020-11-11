e4aab4a4348fd2686598b1228d65dee3f3f1e550
Darren Shen <shend@chromium.org>
2020-11-11 10:10:45 +0000

    ime: Use ScopedObservation for input_method::Accessibility.
    ScopedObservation is much safer and cleaner.
    
    Change-Id: I189991b7aafb2cda41fa77a468b1f9a8a213fa10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2522811
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826210}

--- chrome/browser/chromeos/input_method/accessibility.cc
+++ chrome/browser/chromeos/input_method/accessibility.cc
@@ -14,29 +14,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace input_method {
 
-Accessibility::Accessibility(InputMethodManager* imm)
-    : imm_(imm) {
-  DCHECK(imm_);
-  imm_->AddObserver(this);
+Accessibility::Accessibility(InputMethodManager* imm) {
+  DCHECK(imm);
+  observed_input_method_manager_.Observe(imm);
 }
 
-Accessibility::~Accessibility() {
-  DCHECK(imm_);
-  imm_->RemoveObserver(this);
-}
+Accessibility::~Accessibility() = default;
 
 void Accessibility::InputMethodChanged(InputMethodManager* imm,
                                        Profile* profile,
                                        bool show_message) {
-  DCHECK_EQ(imm, imm_);
   if (!show_message)
     return;
 
   // Get the medium name of the changed input method (e.g. US, INTL, etc.)
   const InputMethodDescriptor descriptor =
-      imm_->GetActiveIMEState()->GetCurrentInputMethod();
+      imm->GetActiveIMEState()->GetCurrentInputMethod();
   const std::string medium_name = base::UTF16ToUTF8(
-      imm_->GetInputMethodUtil()->GetInputMethodMediumName(descriptor));
+      imm->GetInputMethodUtil()->GetInputMethodMediumName(descriptor));
 
   AutomationManagerAura::GetInstance()->HandleAlert(medium_name);
 }
