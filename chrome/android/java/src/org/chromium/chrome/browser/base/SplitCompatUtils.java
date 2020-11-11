e978c3c6fdc3f0bd1b2324d3822f5566b9e08b1b
Clark DuVall <cduvall@chromium.org>
2020-11-11 18:37:21 +0000

    Fix dark mode in feed v2 with isolated splits enabled
    Dark mode was not working correctly since layouts were being inflated
    with the feed isolated split context and not the activity context. This
    change pulls only the ClassLoader from the feed isolated split context,
    and adds a util which can be used elsewhere.
    
    Bug: 1126301
    Change-Id: I9f1da598cb1df2a33c1095bc9afb8e451b518991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2531516
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826359}

--- chrome/android/java/src/org/chromium/chrome/browser/base/SplitCompatUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/base/SplitCompatUtils.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.base;
 
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.view.LayoutInflater;
 
 import org.chromium.base.BundleUtils;
 import org.chromium.base.ContextUtils;
@@ -43,5 +45,33 @@ public class SplitCompatUtils {
         } catch (ReflectiveOperationException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Creates a context which can access classes from the specified split, but inherits theme
+     * resources from the passed in context. This is useful if a context is needed to inflate
+     * layouts which reference classes from a split.
+     */
+    public static Context createContextForInflation(Context context, String splitName) {
+        if (!BundleUtils.isIsolatedSplitInstalled(context, splitName)) {
+            return context;
+        }
+        ClassLoader splitClassLoader =
+                BundleUtils.createIsolatedSplitContext(context, splitName).getClassLoader();
+        return new ContextWrapper(context) {
+            @Override
+            public ClassLoader getClassLoader() {
+                return splitClassLoader;
+            }
+
+            @Override
+            public Object getSystemService(String name) {
+                Object ret = super.getSystemService(name);
+                if (Context.LAYOUT_INFLATER_SERVICE.equals(name)) {
+                    ret = ((LayoutInflater) ret).cloneInContext(this);
+                }
+                return ret;
+            }
+        };
     }
 }
