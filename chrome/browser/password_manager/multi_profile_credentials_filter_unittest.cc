b637b9a065c1874bbc40ce70b3385ab1c95dcdef
danakj <danakj@chromium.org>
2020-11-11 21:56:28 +0000

    Reland "Remove empty base/bind_helpers.h"
    This is a reland of 6f8b5118468b9a64fccf59d7918b88f4c7c5342b
    
    Original change's description:
    > Remove empty base/bind_helpers.h
    >
    > The contents moved to callback_helpers.h.
    >
    > Also renames a few new bind_helpers.h includes that have been added
    > in the meantime.
    >
    R=dcheng@chromium.org
    TBR=
    NOTRY=true
    >
    > Change-Id: Ia511a4939d60bd2ef5b474185e3716017543f8af
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2523545
    > Commit-Queue: danakj <danakj@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#826387}
    
    Change-Id: If86adf5fc9760ac6785cbfdd211ef7ffeb940ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532967
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826486}

--- chrome/browser/password_manager/multi_profile_credentials_filter_unittest.cc
+++ chrome/browser/password_manager/multi_profile_credentials_filter_unittest.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
-#include "base/bind_helpers.h"
 #include "base/callback.h"
+#include "base/callback_helpers.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/profiles/profile_attributes_entry.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
