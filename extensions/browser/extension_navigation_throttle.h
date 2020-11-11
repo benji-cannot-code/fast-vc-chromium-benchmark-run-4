9a0fe2a983799614bff3f7e334d5613cbcb11c7a
Rakina Zata Amni <rakina@chromium.org>
2020-11-11 04:07:01 +0000

    Make MimeHandlerView sandbox error trigger navigations from browser side
    Currently MimeHandlerView sandbox errors (trying to load in a sandboxed
    iframe) triggers a new navigation within a renderer, not notifying the
    browser until it finished committing in the renderer.
    
    We want to remove cases where the renderer starts navigations and not
    notify the browser until the navigation committed. See doc for more
    context: https://docs.google.com/document/d/1UjdxiDJ3sgVDbp0CZpsSntdwAYNMSfc2qajX4nzXfLY/edit?usp=sharing
    
    This CL moves the sandbox error navigation to be triggered from the
    browser side instead by checking the sandbox flags before committing.
    
    Bug: 1133115
    Change-Id: I3c56574e36ac5115eb51df126ae19adc678a404b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2449669
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826121}

--- extensions/browser/extension_navigation_throttle.h
+++ extensions/browser/extension_navigation_throttle.h
@@ -26,6 +26,7 @@ class ExtensionNavigationThrottle : public content::NavigationThrottle {
   // content::NavigationThrottle implementation:
   ThrottleCheckResult WillStartRequest() override;
   ThrottleCheckResult WillRedirectRequest() override;
+  ThrottleCheckResult WillProcessResponse() override;
   const char* GetNameForLogging() override;
 
  private:
