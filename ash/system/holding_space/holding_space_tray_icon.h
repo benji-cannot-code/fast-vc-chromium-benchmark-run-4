75cc3c90b6bb38650949e6745b78382d1e96d608
David Black <dmblack@google.com>
2020-11-11 03:10:32 +0000

    Show fallback image when holding space previews are unavailable.
    Moving forward, the logic of when the holding space tray is visible will
    be updated such that we need to handle the case where the tray is shown
    but no previews are available.
    
    Bug: 1142572
    Change-Id: Ifae93a7506f3b8a6ce26d3210b9804b2ebf33d87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2531061
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#826103}

--- ash/system/holding_space/holding_space_tray_icon.h
+++ ash/system/holding_space/holding_space_tray_icon.h
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class PrefChangeRegistrar;
 
+namespace views {
+class ImageView;
+}  // namespace views
+
 namespace ash {
 
 class HoldingSpaceTrayIconPreview;
@@ -76,7 +80,7 @@ class ASH_EXPORT HoldingSpaceTrayIcon : public views::View,
   void UpdatePreviewsEnabled();
 
   void ShowPreviews();
-  void HidePreviews(bool force = false);
+  void HidePreviews();
 
   // Invoked when the specified preview has completed animating out. At this
   // point it is owned by `removed_previews_` and should be destroyed.
@@ -84,6 +88,10 @@ class ASH_EXPORT HoldingSpaceTrayIcon : public views::View,
 
   // The shelf associated with this holding space tray icon.
   Shelf* const shelf_;
+
+  // The child of this holding space tray icon which should be visible only if
+  // previews are disabled or there are no previews available.
+  views::ImageView* no_previews_image_view_ = nullptr;
 
   // Whether previews are currently enabled. Note that if the content forward
   // entry point feature is disabled, this will always be false. Otherwise,
