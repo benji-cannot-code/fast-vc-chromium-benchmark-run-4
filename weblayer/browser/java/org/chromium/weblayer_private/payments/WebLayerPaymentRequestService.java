0231aa2239ef74ecfbb105d987905714cf548643
Liquan (Max) Gu <maxlg@chromium.org>
2020-11-11 22:09:48 +0000

    [WebLayer] Separates onPaymentDetailsNotUpdated
    This CL separates onPaymentDetailsNotUpdated into the common part and
    the chrome specific part. The common part is moved into PRService.
    
    Change-Id: I4252f0e1227c78b3100511076ac24826b441a321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533413
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826490}

--- weblayer/browser/java/org/chromium/weblayer_private/payments/WebLayerPaymentRequestService.java
+++ weblayer/browser/java/org/chromium/weblayer_private/payments/WebLayerPaymentRequestService.java
@@ -31,7 +31,7 @@ public class WebLayerPaymentRequestService implements BrowserPaymentRequest {
     }
 
     @Override
-    public void onPaymentDetailsNotUpdated() {
+    public void onPaymentDetailsNotUpdated(String selectedShippingOptionError) {
         assert false : "Not implemented yet";
     }
 
