c2cb356399859861f1dd96921a19303b1be349f4
Bailey Berro <baileyberro@google.com>
2020-11-11 21:25:59 +0000

    Add CpuUsageDataObserver to SystemDataProvider
    This change adds the CpuUsageDataObvserver to SystemDataProvider.
    A subsequent change will add throttling data to the CpuUsageData object
    
    JS-side implementation of observer: https://source.chromium.org/chromium/chromium/src/+/master:chromeos/components/diagnostics_ui/resources/cpu_card.js;l=58-69
    
    Bug: 1128204
    Change-Id: I7ef493691dea63460753c56f066f6744591dbea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2518265
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826471}

--- chromeos/components/diagnostics_ui/backend/system_data_provider.h
+++ chromeos/components/diagnostics_ui/backend/system_data_provider.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/optional.h"
+#include "chromeos/components/diagnostics_ui/backend/cpu_usage_data.h"
 #include "chromeos/components/diagnostics_ui/mojom/system_data_provider.mojom.h"
 #include "chromeos/dbus/power/power_manager_client.h"
 #include "chromeos/services/cros_healthd/public/mojom/cros_healthd.mojom.h"
@@ -44,6 +45,8 @@ class SystemDataProvider : public mojom::SystemDataProvider,
       mojo::PendingRemote<mojom::BatteryHealthObserver> observer) override;
   void ObserveMemoryUsage(
       mojo::PendingRemote<mojom::MemoryUsageObserver> observer) override;
+  void ObserveCpuUsage(
+      mojo::PendingRemote<mojom::CpuUsageObserver> observer) override;
 
   // PowerManagerClient::Observer:
   void PowerChanged(const power_manager::PowerSupplyProperties& proto) override;
@@ -59,6 +62,8 @@ class SystemDataProvider : public mojom::SystemDataProvider,
 
   void SetMemoryUsageTimerForTesting(
       std::unique_ptr<base::RepeatingTimer> timer);
+
+  void SetCpuUsageTimerForTesting(std::unique_ptr<base::RepeatingTimer> timer);
 
  private:
   void BindCrosHealthdProbeServiceIfNeccessary();
@@ -79,6 +84,8 @@ class SystemDataProvider : public mojom::SystemDataProvider,
 
   void UpdateMemoryUsage();
 
+  void UpdateCpuUsage();
+
   void NotifyBatteryChargeStatusObservers(
       const mojom::BatteryChargeStatusPtr& battery_charge_status);
 
@@ -86,6 +93,8 @@ class SystemDataProvider : public mojom::SystemDataProvider,
       const mojom::BatteryHealthPtr& battery_health);
 
   void NotifyMemoryUsageObservers(const mojom::MemoryUsagePtr& memory_usage);
+
+  void NotifyCpuUsageObservers(const mojom::CpuUsagePtr& cpu_usage);
 
   void OnBatteryChargeStatusUpdated(
       const base::Optional<power_manager::PowerSupplyProperties>&
@@ -96,17 +105,26 @@ class SystemDataProvider : public mojom::SystemDataProvider,
 
   void OnMemoryUsageUpdated(cros_healthd::mojom::TelemetryInfoPtr info_ptr);
 
+  void OnCpuUsageUpdated(cros_healthd::mojom::TelemetryInfoPtr info_ptr);
+
+  void ComputeAndPopulateCpuUsage(const cros_healthd::mojom::CpuInfo& cpu_info,
+                                  mojom::CpuUsage& out_cpu_usage);
+
+  CpuUsageData previous_cpu_usage_data_;
+
   mojo::Remote<cros_healthd::mojom::CrosHealthdProbeService> probe_service_;
   mojo::RemoteSet<mojom::BatteryChargeStatusObserver>
       battery_charge_status_observers_;
   mojo::RemoteSet<mojom::BatteryHealthObserver> battery_health_observers_;
   mojo::RemoteSet<mojom::MemoryUsageObserver> memory_usage_observers_;
+  mojo::RemoteSet<mojom::CpuUsageObserver> cpu_usage_observers_;
 
   mojo::Receiver<mojom::SystemDataProvider> receiver_{this};
 
   std::unique_ptr<base::RepeatingTimer> battery_charge_status_timer_;
   std::unique_ptr<base::RepeatingTimer> battery_health_timer_;
   std::unique_ptr<base::RepeatingTimer> memory_usage_timer_;
+  std::unique_ptr<base::RepeatingTimer> cpu_usage_timer_;
 };
 
 }  // namespace diagnostics
