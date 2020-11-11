40e105dee868ad2d19eb90a29775b387774366ad
Jimmy Gong <jimmyxgong@chromium.org>
2020-11-11 01:21:34 +0000

    Phonehub: Check for kNoEligibleHosts in FeatureStatusProvider
    Fixes a bug where Phonehub would not attempt a connection until the tray
    is opened.
    
    Bug: 1106937
    Fixed: 1146644
    Test: chromeos_components_unittests
    Change-Id: I2ea157e597196cb64da4b23210bb605314a1f4b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527535
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826054}

--- chromeos/components/phonehub/feature_status_provider_impl_unittest.cc
+++ chromeos/components/phonehub/feature_status_provider_impl_unittest.cc
@@ -296,6 +296,12 @@ TEST_F(FeatureStatusProviderImplTest, EligiblePhoneButNotSetUp) {
   EXPECT_EQ(FeatureStatus::kEligiblePhoneButNotSetUp, GetStatus());
 }
 
+TEST_F(FeatureStatusProviderImplTest, NoEligiblePhones) {
+  SetMultiDeviceState(HostStatus::kNoEligibleHosts,
+                      FeatureState::kUnavailableNoVerifiedHost);
+  EXPECT_EQ(FeatureStatus::kNotEligibleForFeature, GetStatus());
+}
+
 TEST_F(FeatureStatusProviderImplTest, PhoneSelectedAndPendingSetup) {
   SetEligibleSyncedDevices();
 
@@ -346,6 +352,10 @@ TEST_F(FeatureStatusProviderImplTest, UnavailableBluetoothOff) {
 }
 
 TEST_F(FeatureStatusProviderImplTest, TransitionBetweenAllStatuses) {
+  EXPECT_EQ(FeatureStatus::kNotEligibleForFeature, GetStatus());
+
+  SetMultiDeviceState(HostStatus::kNoEligibleHosts,
+                      FeatureState::kUnavailableNoVerifiedHost);
   EXPECT_EQ(FeatureStatus::kNotEligibleForFeature, GetStatus());
 
   SetMultiDeviceState(HostStatus::kEligibleHostExistsButNoHostSet,
