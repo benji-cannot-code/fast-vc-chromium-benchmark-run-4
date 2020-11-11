b541a79cb1b99583a479289d3641ea7c59662546
Henrique Nakashima <hnakashima@chromium.org>
2020-11-11 22:46:02 +0000

    [Lorenz] Handle graphs without metadata
    Metadata was added recently, but older graphs do not contain it. Avoid
    errors when reading these graphs.
    
    Change-Id: Ib5c6eb01d53a1e67a0fb5b7451875e891fe3184d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2522240
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826507}

--- tools/android/dependency_analysis/modularization_stats.py
+++ tools/android/dependency_analysis/modularization_stats.py
@@ -20,6 +20,8 @@ CLASSES_TO_COUNT_INBOUND = ['ChromeActivity', 'ChromeTabbedActivity']
 
 
 def _copy_metadata(metadata: Dict) -> Dict[str, str]:
+    if metadata is None:
+        return {}
     return {f'meta_{key}': value for key, value in metadata.items()}
 
 
