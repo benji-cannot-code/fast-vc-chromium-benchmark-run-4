c7f5aa51839acfe9ac1c65521b2bc2c1aa6829fa
Aaron Colwell <acolwell@chromium.org>
2020-11-11 02:15:37 +0000

    Move Android MessagePumpUI::Run() logic into MessagePumpForUIStub.
    Moving the Run() logic from MessagePumpUI into the MessagePumpForUIStub
    because it is the only class that actually uses it. Added a CHECK as
    the MessagePumpUI::Run() implementation since this function should
    never be called in production code. Run() implementations must
    be nestable and the implementation in MessagePumpUI is not safe to
    nest because the looper code is not reentrant safe. This change
    simply makes this more explicit and helps prevent accidental misuse.
    
    Change-Id: I36e1891abd8b1aa107b0e4264f4a3952a17819c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527601
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826079}

--- base/test/test_support_android.cc
+++ base/test/test_support_android.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <android/looper.h>
 #include <stdarg.h>
 #include <string.h>
 
@@ -78,8 +79,6 @@ class MessagePumpForUIStub : public base::MessagePumpForUI {
   MessagePumpForUIStub() : base::MessagePumpForUI() { Waitable::GetInstance(); }
   ~MessagePumpForUIStub() override {}
 
-  bool IsTestImplementation() const override { return true; }
-
   // In tests, there isn't a native thread, as such RunLoop::Run() should be
   // used to run the loop instead of attaching and delegating to the native
   // loop. As such, this override ignores the Attach() request.
@@ -93,12 +92,27 @@ class MessagePumpForUIStub : public base::MessagePumpForUI {
     RunState* previous_state = g_state;
     g_state = &state;
 
-    // When not nested we can use the real implementation, otherwise fall back
+    // When not nested we can use the looper, otherwise fall back
     // to the stub implementation.
     if (g_state->run_depth > 1) {
       RunNested(delegate);
     } else {
-      MessagePumpForUI::Run(delegate);
+      ResetShouldQuit();
+
+      SetDelegate(delegate);
+
+      // Pump the loop once in case we're starting off idle as ALooper_pollOnce
+      // will never return in that case.
+      ScheduleWork();
+      while (true) {
+        // Waits for either the delayed, or non-delayed fds to be signalled,
+        // calling either OnDelayedLooperCallback, or
+        // OnNonDelayedLooperCallback, respectively. This uses Android's Looper
+        // implementation, which is based off of epoll.
+        ALooper_pollOnce(-1, nullptr, nullptr, nullptr);
+        if (ShouldQuit())
+          break;
+      }
     }
 
     g_state = previous_state;
