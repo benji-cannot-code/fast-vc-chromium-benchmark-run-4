f171e448645ac6f1d17e3d616be0116bc69a239d
danakj <danakj@chromium.org>
2020-11-11 20:13:12 +0000

    Convert bitmaps received from the renderer to N32.
    Although extensions now uses mojo to receive SkBitmaps, even those still
    have a variety of color types. And some of those colortypes use other
    pixel sizes than 32bpp, which can lead to buffer overflow bugs. So we
    convert any incoming arbitrary bitmaps received from (extensions)
    renderer processes to be N32 upon receipt.
    
    R=reillyg@chromium.org
    
    Bug: 1144462
    Change-Id: I66acc7fbfcc0565411a1b9b9bc5a01b70558c5b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532663
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826425}

--- extensions/browser/extension_action.cc
+++ extensions/browser/extension_action.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_icon_set.h"
 #include "extensions/common/manifest_handlers/icons_handler.h"
 #include "extensions/grit/extensions_browser_resources.h"
+#include "skia/ext/skia_utils_base.h"
 #include "skia/public/mojom/bitmap.mojom.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
@@ -137,10 +138,20 @@ ExtensionAction::IconParseResult ExtensionAction::ParseIconFromCanvasDictionary(
     } else {
       continue;
     }
-    SkBitmap bitmap;
-    if (!skia::mojom::InlineBitmap::Deserialize(bytes, num_bytes, &bitmap))
+    SkBitmap unsafe_bitmap;
+    if (!skia::mojom::InlineBitmap::Deserialize(bytes, num_bytes,
+                                                &unsafe_bitmap)) {
       return IconParseResult::kUnpickleFailure;
-    CHECK(!bitmap.isNull());
+    }
+    CHECK(!unsafe_bitmap.isNull());
+    // On receipt of an arbitrary bitmap from the renderer, we convert to an N32
+    // 32bpp bitmap. Other pixel sizes can lead to out-of-bounds mistakes when
+    // transferring the pixels out of the/ bitmap into other buffers.
+    SkBitmap bitmap;
+    if (!skia::SkBitmapToN32OpaqueOrPremul(unsafe_bitmap, &bitmap)) {
+      NOTREACHED() << "Unable to convert bitmap for icon";
+      return IconParseResult::kUnpickleFailure;
+    }
 
     // Chrome helpfully scales the provided icon(s), but let's not go overboard.
     const int kActionIconMaxSize = 10 * ActionIconSize();
