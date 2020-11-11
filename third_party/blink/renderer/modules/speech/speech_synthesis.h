dbbb2c5fb7fe550439adf35890f73736985ccb51
Nate Chapin <japhet@chromium.org>
2020-11-11 01:02:48 +0000

    Remove cases where classes use both Supplement<LocalDOMWindow> and ExecutionContxtClient
    Change-Id: I0f48c0823bf8a811b635522eb433bc277f5c238f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2530589
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826037}

--- third_party/blink/renderer/modules/speech/speech_synthesis.h
+++ third_party/blink/renderer/modules/speech/speech_synthesis.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_SPEECH_SPEECH_SYNTHESIS_H_
 
 #include "third_party/blink/public/mojom/speech/speech_synthesis.mojom-blink-forward.h"
-#include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/modules/event_target_modules.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/modules/speech/speech_synthesis_utterance.h"
@@ -44,7 +43,6 @@ class LocalDOMWindow;
 
 class MODULES_EXPORT SpeechSynthesis final
     : public EventTargetWithInlineData,
-      public ExecutionContextClient,
       public Supplement<LocalDOMWindow>,
       public mojom::blink::SpeechSynthesisVoiceListObserver {
   DEFINE_WRAPPERTYPEINFO();
@@ -72,9 +70,7 @@ class MODULES_EXPORT SpeechSynthesis final
 
   DEFINE_ATTRIBUTE_EVENT_LISTENER(voiceschanged, kVoiceschanged)
 
-  ExecutionContext* GetExecutionContext() const override {
-    return ExecutionContextClient::GetExecutionContext();
-  }
+  ExecutionContext* GetExecutionContext() const override;
 
   // GarbageCollected
   void Trace(Visitor*) const override;
