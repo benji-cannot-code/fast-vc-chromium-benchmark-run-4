65f08c23ff46ccee997b47b841bd35dd5ec8d3b7
Dave Tapuska <dtapuska@chromium.org>
2020-11-11 19:27:47 +0000

    Fix failed build
    crrev.com/6f8b5118468 adjusted location of callback helpers.
    
    TBR=danakj@chromium.org
    NOTRY=true
    
    Change-Id: I4af7bd6b70c4716db7059b16ec7dd7843fc0fd21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533026
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Auto-Submit: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826389}

--- content/browser/webui/web_ui_main_frame_observer.cc
+++ content/browser/webui/web_ui_main_frame_observer.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 
 #if defined(OS_LINUX) || defined(OS_CHROMEOS)
-#include "base/bind_helpers.h"
+#include "base/callback_helpers.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/strings/strcat.h"
