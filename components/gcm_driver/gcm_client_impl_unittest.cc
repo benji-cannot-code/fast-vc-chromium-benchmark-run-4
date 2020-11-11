cd662c8b323c24c73d685bb46cc7aecbcb26e160
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2020-11-11 15:13:18 +0000

    Fix ~GCMStoreImpl() performing IO on the UI thread
    This is needed to enable DestroyProfileOnBrowserClose by default in
    automated tests (crrev.com/c/2503629).
    
    GCMStoreImpl::Backend owns an sql::Database object, which may perform
    IO when destroyed. Release it on the blocking task runner, so it
    doesn't cause issues when the DestroyProfileOnBrowserClose flag is on.
    
    Bug: 88586
    Change-Id: I657ae488c6a55c1608bf45466ea6a3bc0153786f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2511250
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826278}

--- components/gcm_driver/gcm_client_impl_unittest.cc
+++ components/gcm_driver/gcm_client_impl_unittest.cc
@@ -465,6 +465,9 @@ void GCMClientImplTest::SetUp() {
 }
 
 void GCMClientImplTest::TearDown() {
+  gcm_client_.reset();
+  PumpLoopUntilIdle();
+  testing::Test::TearDown();
 }
 
 void GCMClientImplTest::SetFeatureParams(const base::Feature& feature,
--- google_apis/gcm/engine/mcs_client_unittest.cc
+++ google_apis/gcm/engine/mcs_client_unittest.cc
@@ -131,6 +131,7 @@ class MCSClientTest : public testing::Test {
   ~MCSClientTest() override;
 
   void SetUp() override;
+  void TearDown() override;
 
   void BuildMCSClient();
   void InitializeClient();
@@ -174,7 +175,7 @@ class MCSClientTest : public testing::Test {
   base::SimpleTestClock clock_;
 
   base::ScopedTempDir temp_directory_;
-  base::test::SingleThreadTaskEnvironment task_environment_;
+  base::test::TaskEnvironment task_environment_;
   std::unique_ptr<base::RunLoop> run_loop_;
   std::unique_ptr<GCMStore> gcm_store_;
 
@@ -207,6 +208,12 @@ MCSClientTest::~MCSClientTest() {}
 
 void MCSClientTest::SetUp() {
   testing::Test::SetUp();
+}
+
+void MCSClientTest::TearDown() {
+  gcm_store_.reset();
+  task_environment_.RunUntilIdle();
+  testing::Test::TearDown();
 }
 
 void MCSClientTest::BuildMCSClient() {
