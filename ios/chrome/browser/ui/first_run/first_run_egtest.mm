e1b1e6faa46c2da83efcbfb3cbe3f3637cfd5a3d
Chris Lu <thegreenfrog@chromium.org>
2020-11-12 04:02:07 +0000

    [ios] Fix FirstRunTestCase failure by disabling Location Feature
    With the location permissions client-side driven experiment,
    test runs were being bucketed in the experiment and the First Run
    modal was sometimes showing, leading to failures. Thus,
    kLocationPermissionsPrompt should be disabled.
    
    TBR=marq@chromium.org
    
    Bug: 1148054
    Change-Id: I33dee1282e77831f37e561a76ad1cff790710e4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533321
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826642}

--- ios/chrome/browser/ui/first_run/first_run_egtest.mm
+++ ios/chrome/browser/ui/first_run/first_run_egtest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui.h"
 #import "ios/chrome/browser/ui/first_run/first_run_app_interface.h"
 #import "ios/chrome/browser/ui/first_run/first_run_constants.h"
+#include "ios/chrome/browser/ui/ui_feature_flags.h"
 #include "ios/chrome/grit/ios_chromium_strings.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
@@ -54,6 +55,12 @@ id<GREYMatcher> SkipSigninButton() {
   [super tearDown];
   [FirstRunAppInterface setUMACollectionEnabled:NO];
   [FirstRunAppInterface resetUMACollectionEnabledByDefault];
+}
+
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config;
+  config.features_disabled.push_back(kLocationPermissionsPrompt);
+  return config;
 }
 
 // Navigates to the terms of service and back.
