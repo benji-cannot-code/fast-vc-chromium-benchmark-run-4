a60773d398121e010813faee44149c3833dab891
dpapad <dpapad@chromium.org>
2020-11-12 06:42:27 +0000

    WebUI: Auto-generate ui/webui/resources/css/ resources grdp file.
    Moving most IDR_WEBUI_CSS_* resources from webui_resources.grd to
    the auto-generated webui_generated_resources.grd.
    
    Resources that rely on flattenhtml=true are not affected, since
    |flattenhtml| is purposefully not supported by generate_grd().
    
    Bug: 1132403
    Change-Id: Iee43d27b9d34a0f9062dab5e6a56c1c428b5cca6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521930
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826677}

--- ui/base/webui/web_ui_util.cc
+++ ui/base/webui/web_ui_util.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/font.h"
 #include "ui/gfx/image/image_skia.h"
-#include "ui/resources/grit/webui_resources.h"
+#include "ui/resources/grit/webui_generated_resources.h"
 #include "ui/strings/grit/app_locale_settings.h"
 #include "url/gurl.h"
 
