73fbfdb6f637ee2df610953a94c4a4fab44bb02b
Xing Liu <xingliu@chromium.org>
2020-11-12 07:46:38 +0000

    Download: Add a method to get secondary storage Download directory on R.
    Android R starts to restrict other apps to access app's private
    directory on R. Based on suggestion from Android engineer, we can use
    a few new APIs added in Q and R to move the storage path a non private
    directory.
    
    Old path:
    /storage/$volume_id/$app_private_directory/Download
    New path:
    /storage/&volume_id/Download
    
    Bug: 1116905
    Change-Id: Iecb34a08648678dbd1ff986c7ee60f3a4647c725
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2515262
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826685}

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_ANDROID)
+#include "base/android/build_info.h"
 #include "base/android/path_utils.h"
 #endif
 
@@ -77,9 +78,19 @@ bool IsAccessAllowedInternal(const base::FilePath& path,
   if (external_storage_path.IsParent(path))
     return true;
 
-  auto all_download_dirs = base::android::GetAllPrivateDownloadsDirectories();
-  for (const auto& dir : all_download_dirs)
-    allowlist.push_back(dir);
+  std::vector<base::FilePath> all_download_dirs =
+      base::android::GetAllPrivateDownloadsDirectories();
+  allowlist.insert(allowlist.end(), all_download_dirs.begin(),
+                   all_download_dirs.end());
+
+  base::android::BuildInfo* build_info =
+      base::android::BuildInfo::GetInstance();
+  if (build_info->sdk_int() > base::android::SDK_VERSION_Q) {
+    std::vector<base::FilePath> all_external_download_volumes =
+        base::android::GetSecondaryStorageDownloadDirectories();
+    allowlist.insert(allowlist.end(), all_external_download_volumes.begin(),
+                     all_external_download_volumes.end());
+  }
 
   // allowlist of other allowed directories.
   static const base::FilePath::CharType* const kLocalAccessAllowList[] = {
