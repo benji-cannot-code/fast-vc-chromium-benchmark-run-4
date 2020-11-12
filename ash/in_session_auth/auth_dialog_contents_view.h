a18fe2439f1edf65c222cfbb84b282e60a2177ad
Yicheng Li <yichengli@chromium.org>
2020-11-12 21:00:17 +0000

    ash: Adjust cancel button style for in-session auth dialog
    The cancel button was added back due to privacy team's concern. UI
    reviewers requested swapping the positions of the help button and
    the cancel button, and adding button style to the cancel button.
    Screenshot of this implementation has received a LGTM on UI review
    thread.
    
    Bug: b:156258540, b:144861739
    Change-Id: I60d0bd83e305e2c737fad01cce67ee2c7b563cb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2531014
    Commit-Queue: Yicheng Li <yichengli@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826957}

--- ash/in_session_auth/auth_dialog_contents_view.h
+++ ash/in_session_auth/auth_dialog_contents_view.h
@@ -16,6 +16,7 @@ namespace views {
 class BoxLayout;
 class Label;
 class LabelButton;
+class MdTextButton;
 }  // namespace views
 
 namespace ash {
@@ -53,6 +54,7 @@ class AuthDialogContentsView : public views::View {
 
   // views::Views:
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
+  void RequestFocus() override;
 
   uint32_t auth_methods() const { return auth_methods_; }
 
@@ -153,7 +155,7 @@ class AuthDialogContentsView : public views::View {
   FingerprintView* fingerprint_view_ = nullptr;
 
   // A button to cancel authentication and close the dialog.
-  views::LabelButton* cancel_button_ = nullptr;
+  views::MdTextButton* cancel_button_ = nullptr;
 
   // A button to show a help center article.
   views::LabelButton* help_button_ = nullptr;
