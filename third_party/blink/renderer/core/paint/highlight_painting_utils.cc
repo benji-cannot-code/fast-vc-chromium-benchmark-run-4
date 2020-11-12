394e3e639ea6313dcb068576265f1edd6d15af6f
Rune Lillesveen <futhark@chromium.org>
2020-11-12 18:21:03 +0000

    Only do blending for ::selection background for replaced elements
    We did a BlendWithWhite() to not have custom selection backgrounds fully
    obscure replaced content like images. But, we also did that blending for
    text which means the author could not fully control the ::selection
    background. Instead, only do this blending for replaced content. This is
    in line with what Firefox does and what the spec says[1].
    
    The blending is still done on the UA default background color for both
    text and replaced content in LayoutTheme.
    
    [1] https://drafts.csswg.org/css-pseudo-4/#highlight-replaced
    
    Bug: 1018450
    Change-Id: Ibb2c63d549a21706a511bbb47ff4c686b246314b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532555
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826867}

--- third_party/blink/renderer/core/paint/highlight_painting_utils.cc
+++ third_party/blink/renderer/core/paint/highlight_painting_utils.cc
@@ -31,6 +31,11 @@ bool NodeIsSelectable(const ComputedStyle& style, Node* node) {
                                style.UserModify() == EUserModify::kReadOnly);
 }
 
+bool NodeIsReplaced(Node* node) {
+  return node && node->GetLayoutObject() &&
+         node->GetLayoutObject()->IsLayoutReplaced();
+}
+
 Color ForcedSystemForegroundColor(PseudoId pseudo_id,
                                   mojom::blink::ColorScheme color_scheme) {
   CSSValueID keyword = CSSValueID::kHighlighttext;
@@ -209,8 +214,11 @@ Color HighlightPaintingUtils::HighlightBackgroundColor(
         pseudo_style->ForcedColorAdjust() == EForcedColorAdjust::kNone) {
       Color highlight_color =
           pseudo_style->VisitedDependentColor(GetCSSPropertyBackgroundColor());
-      if (pseudo == kPseudoIdSelection)
+      if (pseudo == kPseudoIdSelection && NodeIsReplaced(node)) {
+        // Avoid that ::selection full obscures selected replaced elements like
+        // images.
         return highlight_color.BlendWithWhite();
+      }
       return highlight_color;
     }
     color_scheme = pseudo_style->UsedColorScheme();
