e6031b07e1d1ddee04db6b03390d21496a2c8149
Bartek Nowierski <bartekn@chromium.org>
2020-11-12 16:06:17 +0000

    [PartitionAlloc] Better guard PartitionAllocGetSlotOffset tests
    If GigaCage is disabled, PartitionAllocGetSlotOffset() triggers a DCHECK
    which checks if the pointer is in the GigaCage's normal bucket pool.
    
    Change-Id: I9ea2683944ee308b9055a2c182e7bb5387c5fc1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2534992
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826784}

--- base/allocator/partition_allocator/partition_alloc_unittest.cc
+++ base/allocator/partition_allocator/partition_alloc_unittest.cc
@@ -872,9 +872,11 @@ TEST_F(PartitionAllocTest, AllocGetSizeAndOffset) {
   EXPECT_EQ(predicted_size, actual_size);
   EXPECT_LT(requested_size, actual_size);
 #if defined(PA_HAS_64_BITS_POINTERS)
-  for (size_t offset = 0; offset < requested_size; ++offset) {
-    EXPECT_EQ(PartitionAllocGetSlotOffset(static_cast<char*>(ptr) + offset),
-              offset);
+  if (features::IsPartitionAllocGigaCageEnabled()) {
+    for (size_t offset = 0; offset < requested_size; ++offset) {
+      EXPECT_EQ(PartitionAllocGetSlotOffset(static_cast<char*>(ptr) + offset),
+                offset);
+    }
   }
 #endif
   allocator.root()->Free(ptr);
@@ -889,9 +891,11 @@ TEST_F(PartitionAllocTest, AllocGetSizeAndOffset) {
   EXPECT_EQ(predicted_size, actual_size);
   EXPECT_EQ(requested_size, actual_size);
 #if defined(PA_HAS_64_BITS_POINTERS)
-  for (size_t offset = 0; offset < requested_size; offset += 877) {
-    EXPECT_EQ(PartitionAllocGetSlotOffset(static_cast<char*>(ptr) + offset),
-              offset);
+  if (features::IsPartitionAllocGigaCageEnabled()) {
+    for (size_t offset = 0; offset < requested_size; offset += 877) {
+      EXPECT_EQ(PartitionAllocGetSlotOffset(static_cast<char*>(ptr) + offset),
+                offset);
+    }
   }
 #endif
   allocator.root()->Free(ptr);
@@ -910,9 +914,11 @@ TEST_F(PartitionAllocTest, AllocGetSizeAndOffset) {
   EXPECT_EQ(predicted_size, actual_size);
   EXPECT_EQ(requested_size + SystemPageSize(), actual_size);
 #if defined(PA_HAS_64_BITS_POINTERS)
-  for (size_t offset = 0; offset < requested_size; offset += 4999) {
-    EXPECT_EQ(PartitionAllocGetSlotOffset(static_cast<char*>(ptr) + offset),
-              offset);
+  if (features::IsPartitionAllocGigaCageEnabled()) {
+    for (size_t offset = 0; offset < requested_size; offset += 4999) {
+      EXPECT_EQ(PartitionAllocGetSlotOffset(static_cast<char*>(ptr) + offset),
+                offset);
+    }
   }
 #endif
 
@@ -924,10 +930,12 @@ TEST_F(PartitionAllocTest, AllocGetSizeAndOffset) {
   actual_size = allocator.root()->GetSize(ptr);
   EXPECT_EQ(predicted_size, actual_size);
   EXPECT_EQ(requested_size, actual_size);
-#if defined(ARCH_CPU_64_BITS) && !defined(OS_NACL)
-  for (size_t offset = 0; offset < requested_size; offset += 4999) {
-    EXPECT_EQ(PartitionAllocGetSlotOffset(static_cast<char*>(ptr) + offset),
-              offset);
+#if defined(PA_HAS_64_BITS_POINTERS)
+  if (features::IsPartitionAllocGigaCageEnabled()) {
+    for (size_t offset = 0; offset < requested_size; offset += 4999) {
+      EXPECT_EQ(PartitionAllocGetSlotOffset(static_cast<char*>(ptr) + offset),
+                offset);
+    }
   }
 #endif
 
@@ -959,6 +967,9 @@ TEST_F(PartitionAllocTest, AllocGetSizeAndOffset) {
 
 #if defined(PA_HAS_64_BITS_POINTERS)
 TEST_F(PartitionAllocTest, GetOffsetMultiplePages) {
+  if (!features::IsPartitionAllocGigaCageEnabled())
+    return;
+
   const size_t real_size = 80;
   const size_t requested_size = real_size - kExtraAllocSize;
   // Double check we don't end up with 0 or negative size.
