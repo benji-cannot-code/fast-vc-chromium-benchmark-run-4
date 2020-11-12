ccd7ac5d3938ba8dc8e73afa0f57a4c0519419c8
Yao Xiao <yaoxia@chromium.org>
2020-11-12 06:49:27 +0000

    Avoid recalculating floc on history-delete. Will either invalidate or no-op.
    Why: we don't want floc to change more often than the intended
    scheduled update cadence.
    
    What:
    - For each history-delete notification, if it's all-history or
    the time-range overlaps with the time range of the history used to
    compute the floc, we invalidate the floc. Otherwise, we keep using the
    current floc.
    - Remove the mechanism caching the swaa_nac_account_enabled status.
    It's no longer needed as we don't need to query more often than the
    scheduled update rate (% rare race condition).
    
    How: Store history_begin_time_/history_end_time_ fields to FlocId.
    Compare these fields with the history delete info.
    
    Bug: 1143597
    Change-Id: I685fd1a10cbc8044e799d7a644dfa2b7bb82ddd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2507454
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826680}

--- components/federated_learning/floc_id.h
+++ components/federated_learning/floc_id.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_FEDERATED_LEARNING_FLOC_ID_H_
 
 #include "base/optional.h"
+#include "base/time/time.h"
 #include "base/version.h"
 
 #include <stdint.h>
@@ -25,9 +26,13 @@ class FlocId {
       const std::unordered_set<std::string>& domains);
 
   FlocId();
-  explicit FlocId(uint64_t id, uint32_t sorting_lsh_version);
-  FlocId(const FlocId& id);
 
+  explicit FlocId(uint64_t id,
+                  base::Time history_begin_time,
+                  base::Time history_end_time,
+                  uint32_t sorting_lsh_version);
+
+  FlocId(const FlocId& id);
   ~FlocId();
   FlocId& operator=(const FlocId& id);
   FlocId& operator=(FlocId&& id);
@@ -36,15 +41,23 @@ class FlocId {
   bool operator!=(const FlocId& other) const;
 
   bool IsValid() const;
-  uint64_t ToUint64() const;
 
   // The id, followed by the chrome floc version, followed by the async floc
   // component versions (i.e. model and sorting-lsh). This is the format to be
   // exposed to the JS API. Precondition: |id_| must be valid.
-  std::string ToString() const;
+  std::string ToStringForJsApi() const;
+
+  base::Time history_begin_time() const { return history_begin_time_; }
+
+  base::Time history_end_time() const { return history_end_time_; }
 
  private:
   base::Optional<uint64_t> id_;
+
+  // The time range of the actual history used to compute the floc. This should
+  // always be within the time range of each history query.
+  base::Time history_begin_time_;
+  base::Time history_end_time_;
 
   // The main version (i.e. 1st int) of the sorting lsh component version.
   uint32_t sorting_lsh_version_ = 0;
--- components/federated_learning/floc_sorting_lsh_clusters_service.h
+++ components/federated_learning/floc_sorting_lsh_clusters_service.h
@@ -30,7 +30,8 @@ namespace federated_learning {
 // File reading and parsing is posted to |background_task_runner_|.
 class FlocSortingLshClustersService {
  public:
-  using ApplySortingLshCallback = base::OnceCallback<void(FlocId)>;
+  using ApplySortingLshCallback =
+      base::OnceCallback<void(base::Optional<uint64_t>, base::Version)>;
 
   class Observer {
    public:
@@ -63,6 +64,10 @@ class FlocSortingLshClustersService {
 
  private:
   friend class FlocSortingLshClustersServiceTest;
+
+  void DidApplySortingLsh(ApplySortingLshCallback callback,
+                          base::Version version,
+                          base::Optional<uint64_t> final_hash);
 
   // Runner for tasks that do not influence user experience.
   scoped_refptr<base::SequencedTaskRunner> background_task_runner_;
