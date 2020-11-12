7e5e3f90ac9012144de79da59b7cfada629841c2
Yann Dago <ydago@chromium.org>
2020-11-12 17:34:38 +0000

    Add histograms to monitor ChromeBrowsingDataLifetimeManager deletions
    Create BrowsingDataRemoverBrowserTestBase
    
    Bug: 1026442
    Change-Id: I05b0ebff060e3114a536146e82716a875485c676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2524724
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826826}

--- chrome/browser/browsing_data/chrome_browsing_data_lifetime_manager.h
+++ chrome/browser/browsing_data/chrome_browsing_data_lifetime_manager.h
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_BROWSING_DATA_CHROME_BROWSING_DATA_LIFETIME_MANAGER_H_
 #define CHROME_BROWSER_BROWSING_DATA_CHROME_BROWSING_DATA_LIFETIME_MANAGER_H_
 
+#include <stdint.h>
+#include <utility>
 #include <vector>
 
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
-#include "base/scoped_observer.h"
-#include "base/single_thread_task_runner.h"
 #include "base/time/time.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 class BrowserContext;
-class BrowsingDataRemover;
 }  // namespace content
 
 class Profile;
@@ -47,9 +46,7 @@ extern const char kDataTypes[];
 }  // namespace browsing_data
 
 // Controls the lifetime of some browsing data.
-class ChromeBrowsingDataLifetimeManager
-    : public KeyedService,
-      public content::BrowsingDataRemover::Observer {
+class ChromeBrowsingDataLifetimeManager : public KeyedService {
  public:
   struct ScheduledRemovalSettings {
     uint64_t remove_mask;
@@ -59,19 +56,14 @@ class ChromeBrowsingDataLifetimeManager
 
   explicit ChromeBrowsingDataLifetimeManager(
       content::BrowserContext* browser_context);
-  ~ChromeBrowsingDataLifetimeManager() override;
-
   ChromeBrowsingDataLifetimeManager(const ChromeBrowsingDataLifetimeManager&) =
       delete;
-
   ChromeBrowsingDataLifetimeManager& operator=(
       const ChromeBrowsingDataLifetimeManager&) = delete;
+  ~ChromeBrowsingDataLifetimeManager() override;
 
   // KeyedService:
   void Shutdown() override;
-
-  // content::BrowsingDataRemover::Observer:
-  void OnBrowsingDataRemoverDone(uint64_t failed_data_types) override;
 
   // Sets the end time of the period for which data must be deleted, for all
   // configurations. If this is |end_time_for_testing| has no value, use the
@@ -95,9 +87,6 @@ class ChromeBrowsingDataLifetimeManager
   Profile* profile_;
   content::BrowsingDataRemover::Observer* testing_data_remover_observer_ =
       nullptr;
-  ScopedObserver<content::BrowsingDataRemover,
-                 content::BrowsingDataRemover::Observer>
-      browsing_data_remover_observer_;
   base::Optional<base::Time> end_time_for_testing_;
   base::WeakPtrFactory<ChromeBrowsingDataLifetimeManager> weak_ptr_factory_{
       this};
