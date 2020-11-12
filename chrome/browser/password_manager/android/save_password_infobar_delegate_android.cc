2473052d9b37749deeb6ef150b652b0d64433258
Anne Lim <annelim@google.com>
2020-11-12 19:22:57 +0000

    Add flag for sync and single account users for InfoBar footer
    Bug: 1135847
    Change-Id: I8e378bcdfed0446abcf89731b1b32959e52b980d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2531359
    Commit-Queue: Anne Lim <annelim@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826911}

--- chrome/browser/password_manager/android/save_password_infobar_delegate_android.cc
+++ chrome/browser/password_manager/android/save_password_infobar_delegate_android.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/passwords/manage_passwords_view_utils.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/infobars/core/infobar.h"
 #include "components/infobars/core/infobar_manager.h"
 #include "components/password_manager/core/browser/password_bubble_experiment.h"
@@ -54,9 +55,16 @@ void SavePasswordInfoBarDelegate::Create(
   // passwords to their Google Account.
   bool is_smartlock_branding_enabled =
       password_bubble_experiment::IsSmartLockUser(sync_service);
-  bool should_show_account_footer = is_smartlock_branding_enabled &&
-                                    !is_single_account_user &&
-                                    account_info.has_value();
+  bool should_show_account_footer =
+      (is_smartlock_branding_enabled &&
+       base::FeatureList::IsEnabled(
+           autofill::features::
+               kAutofillEnableInfoBarAccountIndicationFooterForSyncUsers)) &&
+      (!is_single_account_user ||
+       base::FeatureList::IsEnabled(
+           autofill::features::
+               kAutofillEnableInfoBarAccountIndicationFooterForSingleAccountUsers)) &&
+      account_info.has_value();
   InfoBarService* infobar_service =
       InfoBarService::FromWebContents(web_contents);
   infobar_service->AddInfoBar(std::make_unique<SavePasswordInfoBar>(
