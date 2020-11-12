8a6bf0d1b0765af883c76f3f9245e5fd21a9bf50
Brandon Wylie <wylieb@chromium.org>
2020-11-12 18:47:17 +0000

    [Sheriff][Android] Disable flaky PasswordsSettingsTest
    TBR=fhorschig@chromium.org
    
    Bug: 1148376
    Change-Id: I6f97f5934b0196c8e6aa92db9f17b5598f49ddec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2535015
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826882}

--- chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/settings/PasswordSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/settings/PasswordSettingsTest.java
@@ -1751,6 +1751,7 @@ public class PasswordSettingsTest {
     @Test
     @SmallTest
     @Feature({"Preferences"})
+    @DisabledTest(message = "crbug.com/1148376")
     public void testSearchFiltersByUserName() {
         setPasswordSourceWithMultipleEntries(GREEK_GODS);
         startPasswordSettingsFromMainSettings();
