f4bebdbbc2f52a861b084dea64b32cebd6e0e2ce
Tibor Goldschwendt <tiborg@chromium.org>
2020-11-12 22:46:41 +0000

    [ntp][modules] Disable toggle if module visibility is policy controlled
    If the module visibility state is controlled by a policy the user is not
    able to set module visibility. We communicate that by graying out the
    toggle and showing a business icon explain the situation.
    
    Toggle off: screenshot/8dUeEADe6jyvyLa
    Toggle on: screenshot/K4WGhYvGnESmLYF
    
    Bug: 1142878
    Change-Id: Id3af801552c6c01a2bf3611233c08de892393469
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533581
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827011}

--- chrome/test/data/webui/new_tab_page/customize_modules_test.js
+++ chrome/test/data/webui/new_tab_page/customize_modules_test.js
@@ -4,13 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://new-tab-page/lazy_load.js';
-import {BrowserProxy} from 'chrome://new-tab-page/new_tab_page.js';
-import {createTestProxy} from 'chrome://test/new_tab_page/test_support.js';
+
+import {$$, BrowserProxy} from 'chrome://new-tab-page/new_tab_page.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+import {assertNotStyle, assertStyle, createTestProxy} from 'chrome://test/new_tab_page/test_support.js';
 
 suite('NewTabPageCustomizeModulesTest', () => {
-  /** @type {!CustomizeModulesElement} */
-  let customizeModules;
-
   /**
    * @implements {BrowserProxy}
    * @extends {TestBrowserProxy}
@@ -22,14 +21,13 @@ suite('NewTabPageCustomizeModulesTest', () => {
 
     testProxy = createTestProxy();
     BrowserProxy.instance_ = testProxy;
-
-    customizeModules = document.createElement('ntp-customize-modules');
-    document.body.appendChild(customizeModules);
   });
 
   [true, false].forEach(visible => {
     test('toggle hide calls handler', async () => {
       // Arrange.
+      const customizeModules = document.createElement('ntp-customize-modules');
+      document.body.appendChild(customizeModules);
       testProxy.callbackRouterRemote.setModulesVisible(visible);
 
       // Act.
@@ -38,6 +36,20 @@ suite('NewTabPageCustomizeModulesTest', () => {
 
       // Assert.
       assertEquals(1, testProxy.handler.getCallCount('setModulesVisible'));
+      assertStyle(
+          $$(customizeModules, 'cr-policy-indicator'), 'display', 'none');
     });
+  });
+
+  test('policy disables hide toggle', () => {
+    // Act.
+    loadTimeData.overrideValues({modulesVisibleManagedByPolicy: true});
+    const customizeModules = document.createElement('ntp-customize-modules');
+    document.body.appendChild(customizeModules);
+
+    // Assert.
+    assertTrue(customizeModules.$.hideToggle.disabled);
+    assertNotStyle(
+        $$(customizeModules, 'cr-policy-indicator'), 'display', 'none');
   });
 });
