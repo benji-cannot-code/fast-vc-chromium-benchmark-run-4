4ac0e608c75bb4297cdb140df2df0e5c301fd4c9
Anne Lim <annelim@google.com>
2020-11-12 00:03:18 +0000

    Reland "Reland "[AF] Add account indication footer to AutofillSaveCardInfoBar""
    This reverts commit cd50c5655c6c1bb7d1d2c40908313174554c1e34.
    
    Reason for revert: Relanding with tests removed, as discussed offline
    
    Original change's description:
    > Revert "Reland "[AF] Add account indication footer to AutofillSaveCardInfoBar""
    >
    > This reverts commit 6e85e7a4603787886fa8fc58b597e2b570e1bbc8.
    >
    > Reason for revert: crbug.com/1147678
    >
    > Original change's description:
    > > Reland "[AF] Add account indication footer to AutofillSaveCardInfoBar"
    > >
    > > This is a reland of ba7d881429d42944c172ca7ca13938f78d5c4f1f. Two changes were made:
    > > 1) A null pointer check is added to AutofillSaveCardInfoBar.java, and
    > > 2) In AutofillUpstreamTest.java, startMainActivityWithURL() is replaced with loadUrl().
    > >
    > > Original change's description:
    > > > [AF] Add account indication footer to AutofillSaveCardInfoBar
    > > >
    > > > The footer is shown only to signed-in users have more than one account, and who do not sync.
    > > >
    > > > Screenshot with feature enabled in light mode:
    > > > https://screenshot.googleplex.com/ApVmkMVQakM6ai9
    > > >
    > > > Screenshot with feature enabled in dark mode:
    > > > https://screenshot.googleplex.com/BJB2jUuGoH3T5jM
    > > >
    > > > Bug: 1135847, 1146186
    > > > Change-Id: Ia2e031a48920e44e66503fab62f26ed87b8e1d92
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2496184
    > > > Commit-Queue: Anne Lim <annelim@google.com>
    > > > Reviewed-by: Marc Treib <treib@chromium.org>
    > > > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > > > Reviewed-by: Jared Saul <jsaul@google.com>
    > > > Cr-Commit-Position: refs/heads/master@{#823710}
    > >
    > > Bug: 1135847
    > > Change-Id: Ifd9d514234a3af82330686373578f54ffd3427c8
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2522219
    > > Commit-Queue: Anne Lim <annelim@google.com>
    > > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > > Reviewed-by: Jared Saul <jsaul@google.com>
    > > Reviewed-by: Marc Treib <treib@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#825853}
    >
    > TBR=treib@chromium.org,mdjones@chromium.org,jsaul@google.com,bsazonov@chromium.org,annelim@google.com
    >
    > Change-Id: I09677d00d3c73763eb0c95ba56fee01bedbb149a
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1135847, 1147678
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2531084
    > Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    > Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#826176}
    
    TBR=treib@chromium.org,mdjones@chromium.org,wychen@chromium.org,jsaul@google.com,bsazonov@chromium.org,annelim@google.com
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1135847
    Bug: 1147678
    Change-Id: I0277fa6869b760612b960b47eca7ba6114efeef0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533277
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Anne Lim <annelim@google.com>
    Commit-Queue: Anne Lim <annelim@google.com>
    Cr-Commit-Position: refs/heads/master@{#826550}

--- chrome/browser/ui/autofill/chrome_autofill_client.h
+++ chrome/browser/ui/autofill/chrome_autofill_client.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 #include "build/build_config.h"
 #include "chrome/browser/autofill/autofill_gstatic_reader.h"
 #include "chrome/browser/profiles/profile.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/logging/log_manager.h"
 #include "components/autofill/core/browser/payments/legal_message_line.h"
 #include "components/autofill/core/browser/ui/payments/card_unmask_prompt_controller_impl.h"
+#include "components/signin/public/identity_manager/account_info.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
@@ -175,6 +177,8 @@ class ChromeAutofillClient
   explicit ChromeAutofillClient(content::WebContents* web_contents);
 
   Profile* GetProfile() const;
+  base::Optional<AccountInfo> GetAccountInfo();
+  bool IsMultipleAccountUser();
   base::string16 GetAccountHolderName();
 
   std::unique_ptr<payments::PaymentsClient> payments_client_;
