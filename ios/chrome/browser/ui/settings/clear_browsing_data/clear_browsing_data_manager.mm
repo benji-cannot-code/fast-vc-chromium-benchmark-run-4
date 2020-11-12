98a8f36c225d774d6611318eabfffa031bf9b4e0
Sigurdur Asgeirsson <siggi@chromium.org>
2020-11-12 17:32:29 +0000

    Add a missing include for ScopedObserver.
    As is, it's getting a declaration out of an include in /components
    someplace. I'm retiring ScopedObserver in /components, which breaks
    compile here.
    
    Bug: 1145565
    Change-Id: I69a2dc15aaef4199c91c01177e0ebfcc22e2da24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533140
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826824}

--- ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_manager.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_manager.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/mac/foundation_util.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/scoped_observer.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/browsing_data/core/history_notice_utils.h"
 #include "components/browsing_data/core/pref_names.h"
