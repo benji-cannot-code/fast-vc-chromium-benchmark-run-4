bab1ff17617b65152f629d4a40c2860994d4660d
Christopher Cameron <ccameron@chromium.org>
2020-11-12 23:39:41 +0000

    CanvasColorParams: Remove unused methods
    Merge GetSkColorSpaceForSkSurfaces with GetSkColorSpace. This function
    never returns nullptr, so remove branches that checked for that.
    
    Remove RequiresColorConversion and inline in its one location.
    
    TBR=fserb
    
    Bug: 1115317
    Change-Id: Icb011165f5f18a328ff52492d1c4760de96847df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2530184
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827042}

--- third_party/blink/renderer/platform/graphics/canvas_color_params_test.cc
+++ third_party/blink/renderer/platform/graphics/canvas_color_params_test.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // When drawing a color managed canvas, the target SkColorSpace is obtained by
-// calling CanvasColorParams::GetSkColorSpaceForSkSurfaces(). When drawing media
-// to the canvas, the target gfx::ColorSpace is returned by CanvasColorParams::
+// calling CanvasColorParams::GetSkColorSpace(). When drawing media to the
+// canvas, the target gfx::ColorSpace is returned by CanvasColorParams::
 // GetStorageGfxColorSpace(). This test verifies that the two different color
 // spaces are approximately the same for different CanvasColorParam objects.
 TEST(CanvasColorParamsTest, MatchSkColorSpaceWithGfxColorSpace) {
--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -144,7 +144,7 @@ class CanvasResourceProviderBitmap : public CanvasResourceProvider {
 
     SkImageInfo info = SkImageInfo::Make(
         Size().Width(), Size().Height(), ColorParams().GetSkColorType(),
-        kPremul_SkAlphaType, ColorParams().GetSkColorSpaceForSkSurfaces());
+        kPremul_SkAlphaType, ColorParams().GetSkColorSpace());
     return SkSurface::MakeRaster(info, ColorParams().GetSkSurfaceProps());
   }
 };
@@ -569,8 +569,7 @@ class CanvasResourceProviderSharedImage : public CanvasResourceProvider {
       return SkSurface::MakeFromBackendTexture(
           GetGrContext(), CreateGrTextureForResource(), GetGrSurfaceOrigin(),
           0 /* msaa_sample_count */, ColorParams().GetSkColorType(),
-          ColorParams().GetSkColorSpaceForSkSurfaces(),
-          ColorParams().GetSkSurfaceProps());
+          ColorParams().GetSkColorSpace(), ColorParams().GetSkSurfaceProps());
     }
 
     // For software raster path, we render into cpu memory managed internally
@@ -808,8 +807,7 @@ class CanvasResourceProviderSwapChain final : public CanvasResourceProvider {
     return SkSurface::MakeFromBackendTexture(
         GetGrContext(), backend_texture, kTopLeft_GrSurfaceOrigin,
         0 /* msaa_sample_count */, ColorParams().GetSkColorType(),
-        ColorParams().GetSkColorSpaceForSkSurfaces(),
-        ColorParams().GetSkSurfaceProps());
+        ColorParams().GetSkColorSpace(), ColorParams().GetSkSurfaceProps());
   }
 
   void FlushIfNeeded() {
