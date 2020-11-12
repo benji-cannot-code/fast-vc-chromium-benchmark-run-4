bb74ca9e5b65107b3eba3fda455a956f20870c23
Andy Paicu <andypaicu@chromium.org>
2020-11-12 18:21:39 +0000

    Bare-bones Permission Predictions API client.
    Implements a bare-bones version that mostly reuses code from
    RealTimeUrlLookupServiceBase without doing the work to derive from a
    base class. This will follow-up in a different CL. For this reason some
    of the callbacks have unused parameters, to make implementing a base
    common class less painful.
    
    Design doc: go/permission-predictions-client-doc
    
    Bug: 1138595
    Change-Id: Idc26e836fba565cf42f796ee3a5b20bc8d61f53f
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2513656
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Ravjit Singh Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826868}

---
+++ components/permissions/prediction_service/prediction_service_common.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PERMISSIONS_PREDICTION_SERVICE_PREDICTION_SERVICE_COMMON_H_
+#define COMPONENTS_PERMISSIONS_PREDICTION_SERVICE_PREDICTION_SERVICE_COMMON_H_
+
+#include "components/permissions/prediction_service/prediction_service_messages.pb.h"
+
+namespace permissions {
+
+// TODO(andypaicu): when available, replace with actual URL.
+constexpr char kDefaultPredictionServiceUrl[] =
+    "https://webpermissionpredictions.googleapis.com/v1/GetPredictions";
+
+// A command line witch used to override the default service url.
+constexpr char kDefaultPredictionServiceUrlSwitchKey[] =
+    "permission-predictions-service-url";
+
+// Get the current platform for proto message purposes.
+ClientFeatures_Platform GetCurrentPlatformProto();
+
+}  // namespace permissions
+
+#endif  // COMPONENTS_PERMISSIONS_PREDICTION_SERVICE_PREDICTION_SERVICE_COMMON_H_
