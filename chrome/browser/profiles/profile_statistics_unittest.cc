299ebcdc5979858df78102ac98eac4a5ce2ea66c
Jesse McKenna <jessemckenna@google.com>
2020-11-12 17:06:10 +0000

    Small cleanup to profile_statistics_unittest.cc
    This change makes the following small cleanup changes:
    * remove unused variable |quit_closure_|
    * use default initialization rather than member-list initialization
      for int member variable
    * remove constructor no longer needed after the above change
    * replace empty destructor body with "= default"
    
    Change-Id: Idb14db66dbc2131fe8bc3fffbdedce265e8b513a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532537
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/master@{#826808}

--- chrome/browser/profiles/profile_statistics_unittest.cc
+++ chrome/browser/profiles/profile_statistics_unittest.cc
@@ -61,8 +61,6 @@ bookmarks::BookmarkModel* CreateBookmarkModelWithoutLoad(Profile* profile) {
 
 class BookmarkStatHelper {
  public:
-  BookmarkStatHelper() : num_of_times_called_(0) {}
-
   void StatsCallback(profiles::ProfileCategoryStats stats) {
     if (stats.back().category == profiles::kProfileStatisticsBookmarks)
       ++num_of_times_called_;
@@ -71,15 +69,14 @@ class BookmarkStatHelper {
   int GetNumOfTimesCalled() { return num_of_times_called_; }
 
  private:
-  base::Closure quit_closure_;
-  int num_of_times_called_;
+  int num_of_times_called_ = 0;
 };
 }  // namespace
 
 class ProfileStatisticsTest : public testing::Test {
  public:
   ProfileStatisticsTest() : manager_(TestingBrowserProcess::GetGlobal()) {}
-  ~ProfileStatisticsTest() override {}
+  ~ProfileStatisticsTest() override = default;
 
  protected:
   void SetUp() override {
