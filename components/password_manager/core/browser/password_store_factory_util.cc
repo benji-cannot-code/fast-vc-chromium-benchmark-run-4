870e8f0470a2a5a0868fb573053cfa7dacaa8e5b
Viktor Semeniuk <vsemeniuk@google.com>
2020-11-12 18:36:37 +0000

    [Passwords] Use of HashAffiliationFetcher
    This change replaces usage of AffiliationFetcher to
    HashAffiliationFetcher depending on the feature state.
    
    Bug: 1108279
    Change-Id: I2398fc91ce14af80ef28a1e48dad61c60690366a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2529109
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826878}

--- components/password_manager/core/browser/password_store_factory_util.cc
+++ components/password_manager/core/browser/password_store_factory_util.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/android_affiliation/affiliation_utils.h"
 #include "components/password_manager/core/browser/android_affiliation/android_affiliation_service.h"
 #include "components/password_manager/core/browser/password_manager_constants.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/sync/base/user_selectable_type.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/driver/sync_user_settings.h"
@@ -112,6 +113,10 @@ base::FilePath GetLoginDatabaseForAccountStoragePathForTesting(
 }
 
 bool ShouldAffiliationBasedMatchingBeActive(syncer::SyncService* sync_service) {
+  if (base::FeatureList::IsEnabled(
+          password_manager::features::kUseOfHashAffiliationFetcher)) {
+    return true;
+  }
   return sync_service && sync_service->IsSyncFeatureActive() &&
          sync_service->GetUserSettings()->GetSelectedTypes().Has(
              syncer::UserSelectableType::kPasswords) &&
--- components/password_manager/core/browser/site_affiliation/affiliation_fetcher_factory_impl.cc
+++ components/password_manager/core/browser/site_affiliation/affiliation_fetcher_factory_impl.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/site_affiliation/affiliation_fetcher_factory_impl.h"
 
 #include "components/password_manager/core/browser/android_affiliation/affiliation_fetcher.h"
+#include "components/password_manager/core/browser/site_affiliation/hash_affiliation_fetcher.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 namespace password_manager {
@@ -17,6 +19,11 @@ std::unique_ptr<AffiliationFetcherInterface>
 AffiliationFetcherFactoryImpl::CreateInstance(
     scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
     AffiliationFetcherDelegate* delegate) {
+  if (base::FeatureList::IsEnabled(
+          password_manager::features::kUseOfHashAffiliationFetcher)) {
+    return std::make_unique<HashAffiliationFetcher>(
+        std::move(url_loader_factory), delegate);
+  }
   return std::make_unique<AffiliationFetcher>(std::move(url_loader_factory),
                                               delegate);
 }
