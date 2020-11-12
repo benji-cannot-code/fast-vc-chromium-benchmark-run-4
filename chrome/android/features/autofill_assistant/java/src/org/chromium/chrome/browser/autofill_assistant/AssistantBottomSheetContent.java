34d6c17cd8ecd00b7658c1b26f1b45003e71c733
Clemens Arbesser <arbesser@google.com>
2020-11-12 09:00:45 +0000

    [Autofill Assistant] Tests transitions from trigger to regular scripts.
    This CL also allows the onboarding to reuse the previous bottom sheet
    content to allow for a smooth transition.
    
    Finally, this CL fixes a potential issue where the bottom bar delegate
    was not always correctly set for users who saw the onboarding.
    
    Bug: b/171776026
    Change-Id: I747333d80c08860192eae7b8414b5c731bc792ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2526068
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#826702}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantBottomSheetContent.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantBottomSheetContent.java
@@ -51,6 +51,10 @@ public class AssistantBottomSheetContent implements BottomSheetContent {
         mContentScrollableView = scrollableView;
     }
 
+    public void setDelegate(Supplier<AssistantBottomBarDelegate> supplier) {
+        mBottomBarDelegateSupplier = supplier;
+    }
+
     public void setPeekModeDisabled(boolean disabled) {
         mPeekModeDisabled = disabled;
     }
