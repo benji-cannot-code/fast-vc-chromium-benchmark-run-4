fde94ff0de2cfd00a3fa4ac11f84860a1629be3e
Tom Anderson <thomasanderson@chromium.org>
2020-11-12 20:05:05 +0000

    [XProto] Remove XCB include from public header
    xproto_types.h is included in all xlib code, so including an xcb header
    may add unnecessary header bloat to many files in the build.
    
    R=sky
    
    Bug: None
    Change-Id: Ib77d6de6ab58e36906d37d888ea8deaa93f2e5a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532907
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826925}

--- ui/gfx/x/connection.h
+++ ui/gfx/x/connection.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/x/xlib_support.h"
 #include "ui/gfx/x/xproto.h"
 
+typedef struct xcb_connection_t xcb_connection_t;
+
 namespace x11 {
 
 class KeyboardState;
--- ui/gfx/x/xproto_types.h
+++ ui/gfx/x/xproto_types.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_X_XPROTO_TYPES_H_
 #define UI_GFX_X_XPROTO_TYPES_H_
 
-#include <xcb/xcb.h>
-
 #include <cstdint>
 #include <memory>
 
