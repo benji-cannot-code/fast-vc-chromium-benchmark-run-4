6fbf3a970aecdc000e8b82627c3dbe6d5e5e2603
Tom Anderson <thomasanderson@chromium.org>
2020-11-12 00:54:11 +0000

    Reland "Set Xlib error handler after GTK X11 initialization"
    This is a reland of 2ed82a41dca579689a0dc69c9c18d22babb4ef74
    
    Original change's description:
    > Set Xlib error handler after GTK X11 initialization
    >
    > The default GTK error handler exits the process, which we don't want.
    > We used to reset the error handler at some point after GTK
    > initialization, but this became lost after r815319.
    >
    > Also refactor the XlibLoader instance to be global so it doesn't have
    > to be created multiple times for each connection.
    >
    > R=sky
    >
    > Bug: 1145929
    > Change-Id: I73a3f27551534c2262bd8994d33e30fc2a0a4e27
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527743
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#825585}
    
    Bug: 1145929
    Change-Id: Ib65f3add91181b3bf82dd71f7b9b87a9a681a53d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533199
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826572}

--- ui/gfx/x/xlib_support.h
+++ ui/gfx/x/xlib_support.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 
 struct _XDisplay;
-class XlibLoader;
 
 namespace x11 {
 
@@ -27,6 +26,12 @@ enum class XlibDisplayType {
   kSyncing,
 };
 
+// Loads Xlib, initializes threads, and sets a default error handler.
+COMPONENT_EXPORT(X11) void InitXlib();
+
+// Sets an async error handler which only logs an error message.
+COMPONENT_EXPORT(X11) void SetXlibErrorHandler();
+
 // A scoped Xlib display.
 class COMPONENT_EXPORT(X11) XlibDisplay {
  public:
@@ -39,7 +44,6 @@ class COMPONENT_EXPORT(X11) XlibDisplay {
   explicit XlibDisplay(const std::string& address);
 
   struct _XDisplay* display_ = nullptr;
-  std::unique_ptr<XlibLoader> xlib_loader_;
 };
 
 // A temporary wrapper around an unowned Xlib display that adds behavior
@@ -57,13 +61,10 @@ class COMPONENT_EXPORT(X11) XlibDisplayWrapper {
   XlibDisplayWrapper& operator=(XlibDisplayWrapper&& other);
 
  private:
-  XlibDisplayWrapper(XlibLoader* xlib_loader,
-                     struct _XDisplay* display,
-                     XlibDisplayType type);
+  XlibDisplayWrapper(struct _XDisplay* display, XlibDisplayType type);
 
   friend class Connection;
 
-  XlibLoader* xlib_loader_;
   struct _XDisplay* display_;
   XlibDisplayType type_;
 };
--- ui/gtk/x/gtk_ui_delegate_x11.cc
+++ ui/gtk/x/gtk_ui_delegate_x11.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/x/x11_util.h"
 #include "ui/events/platform/x11/x11_event_source.h"
 #include "ui/gfx/native_widget_types.h"
+#include "ui/gfx/x/xlib_support.h"
 #include "ui/gfx/x/xproto.h"
 #include "ui/gtk/x/gtk_event_loop_x11.h"
 #include "ui/platform_window/x11/x11_window.h"
@@ -32,6 +33,7 @@ GtkUiDelegateX11::GtkUiDelegateX11(x11::Connection* connection)
     : connection_(connection) {
   DCHECK(connection_);
   gdk_set_allowed_backends("x11");
+  x11::InitXlib();
 }
 
 GtkUiDelegateX11::~GtkUiDelegateX11() = default;
@@ -39,6 +41,11 @@ GtkUiDelegateX11::~GtkUiDelegateX11() = default;
 void GtkUiDelegateX11::OnInitialized() {
   // Ensure the singleton instance of GtkEventLoopX11 is created and started.
   GtkEventLoopX11::EnsureInstance();
+
+  // GTK sets an Xlib error handler that exits the process on any async errors.
+  // We don't want this behavior, so reset the error handler to something that
+  // just logs the error.
+  x11::SetXlibErrorHandler();
 }
 
 GdkKeymap* GtkUiDelegateX11::GetGdkKeymap() {
