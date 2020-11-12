d25df8a6799c5a0ee688fe4848fca50db831ff32
Sigurdur Asgeirsson <siggi@chromium.org>
2020-11-12 15:15:37 +0000

    Retire ScopedObserver in /components/permissions.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=andypaicu@chromium.org
    
    Bug: 1145565
    Change-Id: I9075e3c8594c64f0a4a4d2b5e5320e1d21308e32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533135
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826772}

--- components/permissions/test/permission_request_observer.h
+++ components/permissions/test/permission_request_observer.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_PERMISSIONS_TEST_PERMISSION_REQUEST_OBSERVER_H_
 
 #include "base/run_loop.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "components/permissions/permission_request_manager.h"
 
 namespace permissions {
@@ -31,8 +31,9 @@ class PermissionRequestObserver : public PermissionRequestManager::Observer {
   void OnBubbleAdded() override;
 
  private:
-  ScopedObserver<PermissionRequestManager, PermissionRequestManager::Observer>
-      observer_{this};
+  base::ScopedObservation<PermissionRequestManager,
+                          PermissionRequestManager::Observer>
+      observation_{this};
   base::RunLoop loop_;
   bool request_shown_ = false;
 };
