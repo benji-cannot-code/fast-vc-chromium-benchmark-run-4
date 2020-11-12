7b07306a0d0046267e31cf944f660cc1580e486a
Collin Baker <collinbaker@chromium.org>
2020-11-12 21:41:22 +0000

    Turn on ThumbnailSchedulerImpl
    This enables use of ThumbnailSchedulerImpl in ThumbnailTabHelper.
    
    Bug: 1090038
    Change-Id: Ie1961e142d48f4c821a668e46fe804449b050725
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532910
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826984}

--- chrome/browser/ui/thumbnails/thumbnail_tab_helper.cc
+++ chrome/browser/ui/thumbnails/thumbnail_tab_helper.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/thumbnails/thumbnail_capture_driver.h"
 #include "chrome/browser/ui/thumbnails/thumbnail_readiness_tracker.h"
 #include "chrome/browser/ui/thumbnails/thumbnail_scheduler.h"
+#include "chrome/browser/ui/thumbnails/thumbnail_scheduler_impl.h"
 #include "components/history/core/common/thumbnail_score.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
@@ -84,27 +85,6 @@ class ScopedThumbnailCapture {
   // web contents has disappeared without having to add another observer.
   content::WebContentsObserver* const web_contents_observer_;
   bool captured_ = false;
-};
-
-// A scheduler that immediately captures any tab that wants it without
-// restrictions.
-class ImmediateThumbnailScheduler : public ThumbnailScheduler {
- public:
-  ImmediateThumbnailScheduler() = default;
-  ~ImmediateThumbnailScheduler() override = default;
-
-  // ThumbnailScheduler:
-  void AddTab(TabCapturer* tab) override {}
-  void RemoveTab(TabCapturer* tab) override {}
-
-  void SetTabCapturePriority(TabCapturer* tab,
-                             TabCapturePriority priority) override {
-    DCHECK(tab);
-    if (priority == TabCapturePriority::kNone)
-      tab->SetCapturePermittedByScheduler(false);
-    else
-      tab->SetCapturePermittedByScheduler(true);
-  }
 };
 
 }  // anonymous namespace
@@ -249,7 +229,7 @@ void ThumbnailTabHelper::RecordCaptureType(CaptureType type) {
 
 // static
 ThumbnailScheduler& ThumbnailTabHelper::GetScheduler() {
-  static base::NoDestructor<ImmediateThumbnailScheduler> instance;
+  static base::NoDestructor<ThumbnailSchedulerImpl> instance;
   return *instance.get();
 }
 
