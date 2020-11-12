30567438768f7881f4d42801f9855b5ebbc14fe7
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-11-12 15:58:17 +0000

    Import wpt@f9117cec5009b1d4a3c52bfcfa4d8cf4d276e436
    Using wpt-import in Chromium a2ac3a7094f75ab6e5f6fab5e7e6aaf569795b79.
    With Chromium commits locally applied on WPT:
    50d5488e6c "Revert "webauthn: move create credential tests for residentKey/credProps to WPT""
    c0e73fce6a "[webcodecs] Make sure callbacks aren't called after encoder.reset()"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=lpz@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-payments-fyi-rel
    Change-Id: I3600388ccad0872bd368e55697cbf9deb3343fed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2534710
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#826783}

--- third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/browsers/chrome_spki_certs.py
+++ third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/browsers/chrome_spki_certs.py
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # DO NOT EDIT MANUALLY.
 
 # tools/certs/web-platform.test.pem
-WPT_FINGERPRINT = 'HaGjcmfWjcURDEnkUEWZ28YAkYo5tM12hxAMQ2Z3750='
+WPT_FINGERPRINT = '2cJlW4Y613RmoaIxvps+Afm0Fd3olxiahT3z2DNw6PA='
 
 # signed-exchange/resources/127.0.0.1.sxg.pem
 SXG_WPT_FINGERPRINT = '0Rt4mT6SJXojEMHTnKnlJ/hBKMBcI4kteBlhR1eTTdk='
