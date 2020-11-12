088a5d508bfe6cad5116b1b656423aaef480bc87
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-11-12 08:38:25 +0000

    Import wpt@79331586050ac939313f48167a7a26d9c2332382
    Using wpt-import in Chromium 05227c04d91944d17949612251f8a35b57e2719f.
    With Chromium commits locally applied on WPT:
    50d5488e6c "Revert "webauthn: move create credential tests for residentKey/credProps to WPT""
    c0e73fce6a "[webcodecs] Make sure callbacks aren't called after encoder.reset()"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    cbiesinger@chromium.org:
      external/wpt/css/css-sizing
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    foolip@chromium.org, robertma@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    jsbell@chromium.org:
      external/wpt/resources
    mcasas@chromium.org:
      external/wpt/mediacapture-record
    nzolghadr@chromium.org, yosin@chromium.org:
      external/wpt/input-events
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=lpz@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-payments-fyi-rel
    Change-Id: I788499f71085d09be46aae3669f32da25627f810
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532998
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#826694}

--- third_party/blink/web_tests/external/wpt/tools/manifest/manifest.py
+++ third_party/blink/web_tests/external/wpt/tools/manifest/manifest.py
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import io
 import itertools
-import json
 import os
 from atomicwrites import atomic_write
 from copy import deepcopy
@@ -14,6 +13,7 @@ from six import (
     string_types,
 )
 
+from . import jsonlib
 from . import vcs
 from .item import (ConformanceCheckerTest,
                    CrashTest,
@@ -46,11 +46,6 @@ if MYPY:
     from typing import Type
     from typing import Union
 
-try:
-    import ujson
-    fast_json = ujson
-except ImportError:
-    fast_json = json  # type: ignore
 
 CURRENT_VERSION = 8  # type: int
 
@@ -355,7 +350,7 @@ def _load(logger,  # type: Logger
         try:
             with io.open(manifest, "r", encoding="utf-8") as f:
                 rv = Manifest.from_json(tests_root,
-                                        fast_json.load(f),
+                                        jsonlib.load(f),
                                         types=types,
                                         callee_owns_obj=True)
         except IOError:
@@ -365,7 +360,7 @@ def _load(logger,  # type: Logger
             return None
     else:
         rv = Manifest.from_json(tests_root,
-                                fast_json.load(manifest),
+                                jsonlib.load(manifest),
                                 types=types,
                                 callee_owns_obj=True)
 
@@ -477,6 +472,5 @@ def write(manifest, manifest_path):
     with atomic_write(manifest_path, overwrite=True) as f:
         # Use ',' instead of the default ', ' separator to prevent trailing
         # spaces: https://docs.python.org/2/library/json.html#json.dump
-        json.dump(manifest.to_json(caller_owns_obj=True), f,
-                  sort_keys=True, indent=1, separators=(',', ': '))
+        jsonlib.dump_dist(manifest.to_json(caller_owns_obj=True), f)
         f.write("\n")
