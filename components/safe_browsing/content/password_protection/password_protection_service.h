2e42b8ff874d41c58ea890ff9e71f968b8be4283
Sigurdur Asgeirsson <siggi@chromium.org>
2020-11-12 00:50:02 +0000

    Retire ScopedObserver in /components/safe_browsing/content/password_protection.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=bdea@chromium.org
    
    Bug: 1145565
    Change-Id: I852404bcc8df54aaa7273fd69ec47b80b0b44235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533155
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826570}

--- components/safe_browsing/content/password_protection/password_protection_service.h
+++ components/safe_browsing/content/password_protection/password_protection_service.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "base/values.h"
 #include "build/build_config.h"
@@ -523,8 +523,9 @@ class PasswordProtectionService : public history::HistoryServiceObserver {
   // dialog.
   std::list<std::string> common_spoofed_domains_;
 
-  ScopedObserver<history::HistoryService, history::HistoryServiceObserver>
-      history_service_observer_{this};
+  base::ScopedObservation<history::HistoryService,
+                          history::HistoryServiceObserver>
+      history_service_observation_{this};
 
   // Weakptr can only cancel task if it is posted to the same thread. Therefore,
   // we need CancelableTaskTracker to cancel tasks posted to IO thread.
