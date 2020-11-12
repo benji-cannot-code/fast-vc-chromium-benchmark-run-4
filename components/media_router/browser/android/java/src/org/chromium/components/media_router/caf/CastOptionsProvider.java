chromium/chromium

commit 48d0062246fd8eccdd27b8eaee5547ce2b23e5a8

Clank: Fix media router/casting with isolated splits
CastOptionsProvider is moved back into //chrome and Chrome's
base java module. A copy is also made for WebLayer shell.

Bug: 1144240
Change-Id: Ieed443a573ef8c2361f48dd4915a92569dd5a9e1
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2512808
Commit-Queue: Evan Stade <estade@chromium.org>
Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
Reviewed-by: Clark DuVall <cduvall@chromium.org>
Reviewed-by: Andrew Grieve <agrieve@chromium.org>
Cr-Commit-Position: refs/heads/master@{#823384}

chrome/android/BUILD.gn
@@ -2115,6 +2115,7 @@ android_library("base_module_java") {
     "java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationService.java",
     "java/src/org/chromium/chrome/browser/invalidation/ChromeBrowserSyncAdapterService.java",
     "java/src/org/chromium/chrome/browser/media/MediaCaptureNotificationService.java",
+    "java/src/org/chromium/chrome/browser/media/router/CastOptionsProvider.java",
     "java/src/org/chromium/chrome/browser/media/ui/ChromeMediaNotificationControllerServices.java",
     "java/src/org/chromium/chrome/browser/notifications/NotificationJobService.java",
     "java/src/org/chromium/chrome/browser/notifications/NotificationService.java",
@@ -2128,6 +2129,7 @@ android_library("base_module_java") {
   ]
   deps = [
     ":chrome_base_module_resources",
+    "$google_play_services_package:google_play_services_cast_java",
     "$google_play_services_package:google_play_services_gcm_java",
     "//base:base_java",
     "//components/embedder_support/android:application_java",

chrome/android/expectations/monochrome_public_bundle.AndroidManifest.expected
@@ -828,7 +828,7 @@
     <meta-data android:name="android.allow_multiple_resumed_activities" android:value="true"/>
     <meta-data android:name="android.content.APP_RESTRICTIONS" android:resource="@xml/app_restrictions"/>
     <meta-data android:name="com.android.webview.WebViewLibrary" android:value="libmonochrome.so"/>
-    <meta-data android:name="com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME" android:value="org.chromium.components.media_router.caf.CastOptionsProvider"/>
+    <meta-data android:name="com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME" android:value="org.chromium.chrome.browser.media.router.CastOptionsProvider"/>
     <meta-data android:name="com.google.android.gms.version" android:value="@integer/google_play_services_version"/>
     <meta-data android:name="com.google.ar.core" android:value="optional"/>
     <meta-data android:name="com.google.ar.core.min_apk_version" android:value="200501000"/>

chrome/android/expectations/trichrome_chrome_bundle.AndroidManifest.expected
@@ -782,7 +782,7 @@
     </activity-alias>  # DIFF-ANCHOR: 7c349c4f
     <meta-data android:name="android.allow_multiple_resumed_activities" android:value="true"/>
     <meta-data android:name="android.content.APP_RESTRICTIONS" android:resource="@xml/app_restrictions"/>
-    <meta-data android:name="com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME" android:value="org.chromium.components.media_router.caf.CastOptionsProvider"/>
+    <meta-data android:name="com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME" android:value="org.chromium.chrome.browser.media.router.CastOptionsProvider"/>
     <meta-data android:name="com.google.android.gms.version" android:value="@integer/google_play_services_version"/>
     <meta-data android:name="com.google.ar.core" android:value="optional"/>
     <meta-data android:name="com.google.ar.core.min_apk_version" android:value="200501000"/>

chrome/android/java/AndroidManifest.xml
@@ -1264,7 +1264,7 @@ by a child template that "extends" this file.
       <meta-data
           android:name=
           "com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME"
-          android:value="org.chromium.components.media_router.caf.CastOptionsProvider"/>
+          android:value="org.chromium.chrome.browser.media.router.CastOptionsProvider"/>
 
       {% block base_application_definitions %}
       {% endblock %}

components/media_router/browser/android/BUILD.gn
@@ -58,7 +58,6 @@ android_library("java") {
     "java/src/org/chromium/components/media_router/caf/CafMessageHandler.java",
     "java/src/org/chromium/components/media_router/caf/CafNotificationController.java",
     "java/src/org/chromium/components/media_router/caf/CastMediaSource.java",
-    "java/src/org/chromium/components/media_router/caf/CastOptionsProvider.java",
     "java/src/org/chromium/components/media_router/caf/CastSessionController.java",
     "java/src/org/chromium/components/media_router/caf/CastUtils.java",
     "java/src/org/chromium/components/media_router/caf/CreateRouteRequestInfo.java",

weblayer/shell/android/BUILD.gn
@@ -36,11 +36,13 @@ android_library("weblayer_shell_java") {
   testonly = true
   resources_package = "org.chromium.weblayer.shell"
 
-  # The play services dependency is necessary for the
+  # The google_play_services_base_java dependency is necessary for the
   # google_play_services_version definition in the manifest.
   deps = [
     ":weblayer_shell_resources",
     "$google_play_services_package:google_play_services_base_java",
+    "$google_play_services_package:google_play_services_cast_framework_java",
+    "$google_play_services_package:google_play_services_cast_java",
     "//base:base_java",
     "//components/strictmode/android:java",
     "//third_party/android_deps:android_support_v4_java",
@@ -49,6 +51,7 @@ android_library("weblayer_shell_java") {
     "//weblayer/public/java",
   ]
   sources = [
+    "shell_apk/src/org/chromium/weblayer/shell/CastOptionsProvider.java",
     "shell_apk/src/org/chromium/weblayer/shell/InstrumentationActivity.java",
     "shell_apk/src/org/chromium/weblayer/shell/TelemetryActivity.java",
     "shell_apk/src/org/chromium/weblayer/shell/WebLayerShellActivity.java",

weblayer/shell/android/shell_apk/AndroidManifest.xml
@@ -59,7 +59,7 @@
       <meta-data
           android:name=
           "com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME"
-          android:value="org.chromium.components.media_router.caf.CastOptionsProvider"/>
+          android:value="org.chromium.weblayer.shell.CastOptionsProvider"/>
 
       {% if weblayer_package is defined %}
             <meta-data android:name="org.chromium.weblayer.WebLayerPackage"

weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/CastOptionsProvider.java
@@ -1,0 +1,33 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer.shell;
+
+import android.content.Context;
+
+import com.google.android.gms.cast.LaunchOptions;
+import com.google.android.gms.cast.framework.CastOptions;
+import com.google.android.gms.cast.framework.OptionsProvider;
+import com.google.android.gms.cast.framework.SessionProvider;
+
+import java.util.List;
+
+/** {@link OptionsProvider} implementation for WebLayer Shell casting. */
+public class CastOptionsProvider implements OptionsProvider {
+    @Override
+    public CastOptions getCastOptions(Context context) {
+        return new CastOptions.Builder()
+                .setCastMediaOptions(null)
+                .setEnableReconnectionService(false)
+                .setLaunchOptions(new LaunchOptions.Builder().setRelaunchIfRunning(true).build())
+                .setResumeSavedSession(false)
+                .setStopReceiverApplicationWhenEndingSession(true)
+                .build();
+    }
+
+    @Override
+    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
+        return null;
+    }
+}

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 07b52a8f0720e4a32c064b340ffc648493c8cabc

WebLayer: Restore casting functionality to WL shell.
CastOptionsProvider has to be part of the implementation library.
This was broken in 48d0062246fd8eccdd27b8eaee5547ce2b23e5a8

Bug: 1057099
Change-Id: Idf2f0c56ff8fc65f83b22ddef77d3bca9361b148
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2534401
Reviewed-by: Clark DuVall <cduvall@chromium.org>
Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
Reviewed-by: Andrew Grieve <agrieve@chromium.org>
Commit-Queue: Evan Stade <estade@chromium.org>
Cr-Commit-Position: refs/heads/master@{#826975}

chrome/android/BUILD.gn
@@ -2121,7 +2121,6 @@ android_library("base_module_java") {
     "java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationService.java",
     "java/src/org/chromium/chrome/browser/invalidation/ChromeBrowserSyncAdapterService.java",
     "java/src/org/chromium/chrome/browser/media/MediaCaptureNotificationService.java",
-    "java/src/org/chromium/chrome/browser/media/router/CastOptionsProvider.java",
     "java/src/org/chromium/chrome/browser/media/ui/ChromeMediaNotificationControllerServices.java",
     "java/src/org/chromium/chrome/browser/notifications/NotificationJobService.java",
     "java/src/org/chromium/chrome/browser/notifications/NotificationService.java",
@@ -2135,10 +2134,10 @@ android_library("base_module_java") {
   ]
   deps = [
     ":chrome_base_module_resources",
-    "$google_play_services_package:google_play_services_cast_java",
     "$google_play_services_package:google_play_services_gcm_java",
     "//base:base_java",
     "//components/embedder_support/android:application_java",
+    "//components/media_router/browser/android:cast_options_provider_java",
     "//components/minidump_uploader:minidump_uploader_java",
     "//components/module_installer/android:module_installer_java",
     "//third_party/android_deps:androidx_annotation_annotation_java",

chrome/android/expectations/monochrome_public_bundle.AndroidManifest.expected
@@ -831,7 +831,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     <meta-data android:name="android.allow_multiple_resumed_activities" android:value="true"/>
     <meta-data android:name="android.content.APP_RESTRICTIONS" android:resource="@xml/app_restrictions"/>
     <meta-data android:name="com.android.webview.WebViewLibrary" android:value="libmonochrome.so"/>
-    <meta-data android:name="com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME" android:value="org.chromium.chrome.browser.media.router.CastOptionsProvider"/>
+    <meta-data android:name="com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME" android:value="org.chromium.components.media_router.caf.CastOptionsProvider"/>
     <meta-data android:name="com.google.android.gms.version" android:value="@integer/google_play_services_version"/>
     <meta-data android:name="com.google.ar.core" android:value="optional"/>
     <meta-data android:name="com.google.ar.core.min_apk_version" android:value="200501000"/>

chrome/android/expectations/trichrome_chrome_bundle.AndroidManifest.expected
@@ -785,7 +785,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     </activity-alias>  # DIFF-ANCHOR: 7c349c4f
     <meta-data android:name="android.allow_multiple_resumed_activities" android:value="true"/>
     <meta-data android:name="android.content.APP_RESTRICTIONS" android:resource="@xml/app_restrictions"/>
-    <meta-data android:name="com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME" android:value="org.chromium.chrome.browser.media.router.CastOptionsProvider"/>
+    <meta-data android:name="com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME" android:value="org.chromium.components.media_router.caf.CastOptionsProvider"/>
     <meta-data android:name="com.google.android.gms.version" android:value="@integer/google_play_services_version"/>
     <meta-data android:name="com.google.ar.core" android:value="optional"/>
     <meta-data android:name="com.google.ar.core.min_apk_version" android:value="200501000"/>

chrome/android/java/AndroidManifest.xml
@@ -1260,7 +1260,7 @@ by a child template that "extends" this file.
       <meta-data
           android:name=
           "com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME"
-          android:value="org.chromium.chrome.browser.media.router.CastOptionsProvider"/>
+          android:value="org.chromium.components.media_router.caf.CastOptionsProvider"/>
 
       {% block base_application_definitions %}
       {% endblock %}

components/media_router/browser/android/BUILD.gn
@@ -73,6 +73,14 @@ android_library("java") {
   annotation_processor_deps = [ "//base/android/jni_generator:jni_processor" ]
 }
 
+android_library("cast_options_provider_java") {
+  sources = [ "java/src/org/chromium/components/media_router/caf/CastOptionsProvider.java" ]
+  deps = [
+    "$google_play_services_package:google_play_services_cast_framework_java",
+    "$google_play_services_package:google_play_services_cast_java",
+  ]
+}
+
 generate_jni("jni_headers") {
   sources = [
     "java/src/org/chromium/components/media_router/BrowserMediaRouter.java",

components/media_router/browser/android/java/src/org/chromium/components/media_router/caf/CastOptionsProvider.java
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2020 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.media.router;
+package org.chromium.components.media_router.caf;
 
 import android.content.Context;
 
@@ -14,11 +14,7 @@ import com.google.android.gms.cast.framework.SessionProvider;
 
 import java.util.List;
 
-/**
- * {@link OptionsProvider} implementation for Chrome media router.
- *
- * This has to live in the base java module for it to be loaded under isolated splits.
- */
+/** {@link OptionsProvider} implementation for Chrome MR. */
 public class CastOptionsProvider implements OptionsProvider {
     @Override
     public CastOptions getCastOptions(Context context) {

weblayer/browser/java/BUILD.gn
@@ -219,6 +219,7 @@ android_library("java") {
     "//components/infobars/core:infobar_enums_java",
     "//components/javascript_dialogs/android:java",
     "//components/location/android:settings_java",
+    "//components/media_router/browser/android:cast_options_provider_java",
     "//components/media_router/browser/android:java",
     "//components/metrics:metrics_java",
     "//components/minidump_uploader:minidump_uploader_java",

weblayer/shell/android/BUILD.gn
@@ -42,8 +42,6 @@ android_library("weblayer_shell_java") {
   deps = [
     ":weblayer_shell_resources",
     "$google_play_services_package:google_play_services_base_java",
-    "$google_play_services_package:google_play_services_cast_framework_java",
-    "$google_play_services_package:google_play_services_cast_java",
     "//base:base_java",
     "//components/strictmode/android:java",
     "//third_party/android_deps:android_support_v4_java",
@@ -52,7 +50,6 @@ android_library("weblayer_shell_java") {
     "//weblayer/public/java",
   ]
   sources = [
-    "shell_apk/src/org/chromium/weblayer/shell/CastOptionsProvider.java",
     "shell_apk/src/org/chromium/weblayer/shell/InstrumentationActivity.java",
     "shell_apk/src/org/chromium/weblayer/shell/TelemetryActivity.java",
     "shell_apk/src/org/chromium/weblayer/shell/WebLayerShellActivity.java",

weblayer/shell/android/shell_apk/AndroidManifest.xml
@@ -60,7 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       <meta-data
           android:name=
           "com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME"
-          android:value="org.chromium.weblayer.shell.CastOptionsProvider"/>
+          android:value="org.chromium.components.media_router.caf.CastOptionsProvider"/>
 
       {% if weblayer_package is defined %}
             <meta-data android:name="org.chromium.weblayer.WebLayerPackage"

weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/CastOptionsProvider.java
@@ -1,34 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-package org.chromium.weblayer.shell;
-
-import android.content.Context;
-
-import com.google.android.gms.cast.LaunchOptions;
-import com.google.android.gms.cast.framework.CastOptions;
-import com.google.android.gms.cast.framework.OptionsProvider;
-import com.google.android.gms.cast.framework.SessionProvider;
-
-import java.util.List;
-
-/** {@link OptionsProvider} implementation for WebLayer Shell casting. */
-public class CastOptionsProvider implements OptionsProvider {
-    @Override
-    public CastOptions getCastOptions(Context context) {
-        return new CastOptions.Builder()
-                .setCastMediaOptions(null)
-                .setEnableReconnectionService(false)
-                .setLaunchOptions(new LaunchOptions.Builder().setRelaunchIfRunning(true).build())
-                .setResumeSavedSession(false)
-                .setStopReceiverApplicationWhenEndingSession(true)
-                .build();
-    }
-
-    @Override
-    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
-        return null;
-    }
-}