960f06be1e6009d14f7119d8dee7d254b59b7fd8
Nidhi Jaju <nidhijaju@google.com>
2020-11-12 23:24:01 +0000

    Implement Polymorphism for Readable Stream Readers
    This CL aims to add polymorphism to the readable stream reader
    classes. In particular, it makes the ReadableStreamGenericReader
    the base class, and the ReadableStreamDefaultReader and
    ReadableStreamBYOBReader classes inherit from the GenericReader.
    
    This is based on the approach discussed in
    https://docs.google.com/document/d/1rvKpGjppeqRSWntokY-ft_hU2i2us8gis6zgrrLWSTI/view
    
    Bug: 614302
    Change-Id: I4dde2976938325c21aeb87c6980ac4ad1def483f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2528557
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@google.com>
    Cr-Commit-Position: refs/heads/master@{#827034}

--- third_party/blink/renderer/core/streams/readable_stream_byob_reader.h
+++ third_party/blink/renderer/core/streams/readable_stream_byob_reader.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/streams/readable_stream_generic_reader.h"
 #include "third_party/blink/renderer/core/typed_arrays/array_buffer_view_helpers.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 
@@ -19,7 +20,8 @@ class ScriptState;
 class ReadableStream;
 class DOMArrayBufferView;
 
-class CORE_EXPORT ReadableStreamBYOBReader : public ScriptWrappable {
+class CORE_EXPORT ReadableStreamBYOBReader
+    : public ReadableStreamGenericReader {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
@@ -40,13 +42,6 @@ class CORE_EXPORT ReadableStreamBYOBReader : public ScriptWrappable {
 
   // https://streams.spec.whatwg.org/#byob-reader-release-lock
   void releaseLock(ScriptState*, ExceptionState&);
-
-  // https://streams.spec.whatwg.org/#generic-reader-closed
-  ScriptPromise closed(ScriptState*) const;
-
-  // https://streams.spec.whatwg.org/#generic-reader-cancel
-  ScriptPromise cancel(ScriptState*, ExceptionState&);
-  ScriptPromise cancel(ScriptState*, ScriptValue reason, ExceptionState&);
 
  private:
   friend class ReadableStream;
