84097bd45bb58d501d7f4d1dbb36131819643d2d
Hiroki Nakagawa <nhiroki@chromium.org>
2020-11-12 09:11:30 +0000

    Prerender: Activate prerendered contents during navigation
    This CL integrates multiple WebContents based (non-MPArch based)
    Prerender2 into navigation. The feature is used only when the runtime
    flag is enabled.
    
    Design Doc:
    https://docs.google.com/document/d/15N-HYdSnItZyDIqRFui5OVzYFcNwxtq7kYkKBx3J3xQ/edit#heading=h.88qfv92t9hg9
    
    <Overview>
    
    The Prerender2 runs navigation code twice: navigation for prerendering a
    page and navigation for displaying the prerendered page.
    
    The first navigation is basically the same as the normal navigation:
    running regular navigation code and notifying PrerenderHost of
    navigation completion via WebContentsObserver. This part was already
    implemented by the previous CLs (see https://crbug.com/1132746).
    
    The second navigation known as "activation" is special. That needs to
    look up a prerendered page and activate it instead of fetching a page
    from network. Also, activation should run throttling as well as the
    normal navigation. This CL implements this part.
    
    <Implementation Details>
    
    This CL works as follows:
    
    - On navigation start, a navigation request finds an available
      prerendered page for a request URL. When it's found, the request
      creates a noop CachedNavigationURLLoader like the BFCache not to issue
      a network request.
    - On navigation commit, the navigation request activates the prerendered
      page and aborts remaining steps for navigation.
    
    This is a minimum change for running end-to-end prerendering tests. This
    doesn't implement some essential functionalities, for example, session
    history integration yet.
    
    Bug: 1132746, 1138711, 1138723
    Change-Id: I0371de7ed01ede50a2c1887d8d558c3debdcd2df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2494291
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#826705}

--- content/browser/loader/navigation_url_loader_unittest.cc
+++ content/browser/loader/navigation_url_loader_unittest.cc
@@ -97,7 +97,7 @@ class NavigationURLLoaderTest : public testing::Test {
         browser_context_.get(),
         BrowserContext::GetDefaultStoragePartition(browser_context_.get()),
         std::move(request_info), nullptr, nullptr, nullptr, nullptr, delegate,
-        false, mojo::NullRemote());
+        NavigationURLLoader::LoaderType::kRegular, mojo::NullRemote());
   }
 
  protected:
