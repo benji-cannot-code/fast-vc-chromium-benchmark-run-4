24c34f54f05efdaafd13d89a1d6a70015b039579
Alex Tkachenko <atkachenko@google.com>
2020-11-13 11:35:28 +0000

    Fixing not working JS file from an extension tutorial
    Change-Id: I6e279db73eefa378d00efc6ebbe6376374023fc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2526400
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Paul Irish <paulirish@chromium.org>
    Commit-Queue: Paul Irish <paulirish@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827212}

--- chrome/common/extensions/docs/examples/tutorials/get_started/options.js
+++ chrome/common/extensions/docs/examples/tutorials/get_started/options.js
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 'use strict';
 
+let page = document.getElementById('buttonDiv');
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
     let button = document.createElement('button');
