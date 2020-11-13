c8f0804b0b9120a65a71fb39e5167db60197b495
Fergal Daly <fergal@chromium.org>
2020-11-13 07:18:26 +0000

    Revert "Add field trial config for HighPriorityBeforeUnload."
    This reverts commit 1b3c1be9edac0dce33fa8812a2dccdd5e152192f.
    
    Reason for revert: This is causing timeouts for lots of PDF tests on many dbg builders (see bug). I don't understand why but I have reproduced it locally and bisected and confirmed that this CL is culprit.
    
    Original change's description:
    > Add field trial config for HighPriorityBeforeUnload.
    >
    > Change-Id: Ia274d2e51503514c3fc98420fa4eee5eb040e1fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2490782
    > Commit-Queue: Katie Dillon <kdillon@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#826894}
    
    TBR=sky@chromium.org,kdillon@chromium.org
    
    Change-Id: Id2c0f05304e515034efa769ded1bd38c61b1a53f
    BUG: 1148674
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537275
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827190}

--- chrome/test/data/extensions/api_test/webnavigation/download/a.js
+++ chrome/test/data/extensions/api_test/webnavigation/download/a.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-window.addEventListener('DOMContentLoaded', () => {
+onload = function() {
   var port = location.search.substr(1);
   var redirect =
       "http://127.0.0.1:" + port + "/server-redirect";
@@ -15,4 +15,4 @@ window.addEventListener('DOMContentLoaded', () => {
   link.download = "somefile.txt";
   document.body.appendChild(link);
   link.click();
-});
+};
