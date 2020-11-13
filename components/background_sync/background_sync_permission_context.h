2adf8c7b06836757c0e3eb93ff50edd8077d94b0
Nina Satragno <nsatragno@chromium.org>
2020-11-13 19:40:20 +0000

    [background sync] Remove old TODO
    Remove an old TODO that no longer applies.
    
    Change-Id: Ib17cc59de2d4a38a31e5d9dce0eafa09687f7af5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532885
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827356}

--- components/background_sync/background_sync_permission_context.h
+++ components/background_sync/background_sync_permission_context.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The permission status is ALLOW by default and can be changed globally or on a
 // per-site basis from the content settings page. The user is not prompted for
 // permission.
-// TODO(nsatragno): actually implement the UI to allow changing the setting.
 class BackgroundSyncPermissionContext
     : public permissions::PermissionContextBase {
  public:
