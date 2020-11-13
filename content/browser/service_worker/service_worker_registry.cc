9611a502692b6dfad9112c458e8944abada30d07
Kenichi Ishibashi <bashi@chromium.org>
2020-11-13 00:55:39 +0000

    service worker: Add UMA for retry count for storage service recovery
    Bug: 1133143
    Change-Id: I3f0f082402506138ce00e9228e58549fe8ca7db6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532161
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827082}

--- content/browser/service_worker/service_worker_registry.cc
+++ content/browser/service_worker/service_worker_registry.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/stl_util.h"
 #include "base/task/post_task.h"
 #include "base/trace_event/trace_event.h"
@@ -84,6 +85,11 @@ void CompleteFindSoon(
     ServiceWorkerRegistry::FindRegistrationCallback callback) {
   RunSoon(from_here, base::BindOnce(&CompleteFindNow, std::move(registration),
                                     status, std::move(callback)));
+}
+
+void RecordRetryCount(size_t retries) {
+  base::UmaHistogramCounts100("ServiceWorker.Storage.RetryCountForRecovery",
+                              retries);
 }
 
 }  // namespace
@@ -1354,6 +1360,7 @@ void ServiceWorkerRegistry::OnRemoteStorageDisconnected() {
   if (connection_state_ == ConnectionState::kRecovering) {
     ++recovery_retry_counts_;
     if (recovery_retry_counts_ > kMaxRetryCounts) {
+      RecordRetryCount(kMaxRetryCounts);
       CHECK(false) << "The Storage Service consistently crashes.";
       return;
     }
@@ -1375,6 +1382,8 @@ void ServiceWorkerRegistry::OnRemoteStorageDisconnected() {
 
 void ServiceWorkerRegistry::DidRecover() {
   DCHECK_CURRENTLY_ON(ServiceWorkerContext::GetCoreThreadId());
+
+  RecordRetryCount(recovery_retry_counts_);
 
   recovery_retry_counts_ = 0;
   connection_state_ = ConnectionState::kNormal;
