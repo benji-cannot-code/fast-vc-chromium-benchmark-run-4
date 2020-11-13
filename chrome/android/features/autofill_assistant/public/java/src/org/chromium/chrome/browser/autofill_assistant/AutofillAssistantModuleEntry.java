bd68197f75cf2c2e24956c17d23b9180723174a1
Clemens Arbesser <arbesser@google.com>
2020-11-13 17:44:44 +0000

    [Autofill Assistant] Allow viewport resizing while triggerscript shown.
    Bug: b/173128917
    Change-Id: I6cea12bab4af38e9610c74406b35823bb3095986
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2536463
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827312}

--- chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantModuleEntry.java
+++ chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantModuleEntry.java
@@ -17,6 +17,7 @@ import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.module_installer.builder.ModuleInterface;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.ActivityKeyboardVisibilityDelegate;
+import org.chromium.ui.base.ApplicationViewportInsetSupplier;
 
 import java.util.Map;
 
@@ -36,7 +37,8 @@ interface AutofillAssistantModuleEntry {
     void start(BottomSheetController bottomSheetController,
             BrowserControlsStateProvider browserControls, CompositorViewHolder compositorViewHolder,
             Context context, @NonNull WebContents webContents,
-            ActivityKeyboardVisibilityDelegate keyboardVisibilityDelegate, boolean skipOnboarding,
+            ActivityKeyboardVisibilityDelegate keyboardVisibilityDelegate,
+            ApplicationViewportInsetSupplier bottomInsetProvider, boolean skipOnboarding,
             boolean isChromeCustomTab, @NonNull String initialUrl, Map<String, String> parameters,
             String experimentIds, @Nullable String callerAccount, @Nullable String userName);
     /**
