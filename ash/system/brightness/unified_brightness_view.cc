27b59a85d9e60bec6dfcdb417ca4db9961c72add
minch <minch@chromium.org>
2020-11-13 19:40:10 +0000

    dark_mode: Keep enabled color for brightness icon.
    Bug: 1145363
    Change-Id: Ia1d5a051aa2da436855b5891923a8b091d2039a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537592
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827355}

--- ash/system/brightness/unified_brightness_view.cc
+++ ash/system/brightness/unified_brightness_view.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
+#include "ash/style/ash_color_provider.h"
 #include "ash/system/brightness/unified_brightness_slider_controller.h"
+#include "ui/gfx/paint_vector_icon.h"
 
 namespace ash {
 
@@ -36,6 +38,19 @@ void UnifiedBrightnessView::OnDisplayBrightnessChanged(bool by_user) {
 
 const char* UnifiedBrightnessView::GetClassName() const {
   return "UnifiedBrightnessView";
+}
+
+void UnifiedBrightnessView::OnThemeChanged() {
+  UnifiedSliderView::OnThemeChanged();
+
+  // Override the color for the icon. Since the button is set to disabled but
+  // wants to keep the color for an enabled icon.
+  button()->SetImage(
+      views::Button::STATE_DISABLED,
+      gfx::CreateVectorIcon(
+          kUnifiedMenuBrightnessIcon,
+          AshColorProvider::Get()->GetContentLayerColor(
+              AshColorProvider::ContentLayerType::kButtonIconColor)));
 }
 
 }  // namespace ash
