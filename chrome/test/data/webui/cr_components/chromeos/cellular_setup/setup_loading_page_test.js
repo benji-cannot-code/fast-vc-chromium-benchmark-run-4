55225452fdc8ab62e97d5ac5c6907210a524294e
Gordon Seto <gordonseto@google.com>
2020-11-13 23:27:16 +0000

    [CrOS Settings] Update sim-detect-page to be more generic
    Rename sim-detect-page to setup-loading-page and update UI to be used
    for both psim and esim flows.
    
    Bug: 1093185
    Change-Id: Iaf8934a3423c1ce21d6115bc9ea28c9d972738b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2535231
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827473}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/setup_loading_page_test.js
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/setup_loading_page_test.js
@@ -5,17 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // clang-format off
 // #import 'chrome://os-settings/strings.m.js';
-// #import 'chrome://resources/cr_components/chromeos/cellular_setup/sim_detect_page.m.js';
+// #import 'chrome://resources/cr_components/chromeos/cellular_setup/setup_loading_page.m.js';
 
 // #import {flush, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 // #import {assertFalse, assertTrue} from '../../../chai_assert.js';
 // #import {FakeCellularSetupDelegate} from './fake_cellular_setup_delegate.m.js';
 // clang-format on
 
-suite('CrComponentsSimDetectPageTest', function() {
+suite('CrComponentsSetupLoadingPageTest', function() {
   let simDetectPage;
   setup(function() {
-    simDetectPage = document.createElement('sim-detect-page');
+    simDetectPage = document.createElement('setup-loading-page');
     simDetectPage.delegate = new cellular_setup.FakeCellularSetupDelegate();
     document.body.appendChild(simDetectPage);
     Polymer.dom.flush();
