171014e747ed75af1fb0aa653cddd6d655874e08
Jeevan Shikaram <jshikaram@chromium.org>
2020-11-13 13:47:30 +0000

    [Play Billing] Ensure payments feature enabled on browser-side.
    Check the feature the feature policy for payments.
    
    Bug: 1137688
    Change-Id: I38bd2a4db69270bf91f168310145962e1cf31f2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537212
    Auto-Submit: Jeevan Shikaram <jshikaram@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827239}

--- chrome/browser/apps/digital_goods/digital_goods_factory_impl.cc
+++ chrome/browser/apps/digital_goods/digital_goods_factory_impl.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/digital_goods/util.h"
 #include "components/payments/core/features.h"
 #include "components/payments/core/payments_experimental_features.h"
+#include "content/public/browser/render_frame_host.h"
+#include "third_party/blink/public/mojom/feature_policy/feature_policy_feature.mojom-shared.h"
 
 namespace {
 
@@ -36,6 +38,12 @@ void DigitalGoodsFactoryImpl::BindDigitalGoodsFactory(
 void DigitalGoodsFactoryImpl::CreateDigitalGoods(
     const std::string& payment_method,
     CreateDigitalGoodsCallback callback) {
+  if (!render_frame_host_->IsFeatureEnabled(
+          blink::mojom::FeaturePolicyFeature::kPayment)) {
+    mojo::ReportBadMessage("Feature policy blocks Payment");
+    return;
+  }
+
   // Check feature flag.
   if (!payments::PaymentsExperimentalFeatures::IsEnabled(
           payments::features::kAppStoreBilling)) {
