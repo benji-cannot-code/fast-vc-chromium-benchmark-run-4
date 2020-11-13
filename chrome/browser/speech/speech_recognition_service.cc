e233305b9790900ab219505f3cf99a9991b2b301
evliu <evliu@google.com>
2020-11-13 21:58:01 +0000

    Check if mojo pipe is bound before binding speech recognition context
    This CL adds a check to prevent binding the speech recognition context
    if the mojo pipe to the speech recognition service is not bound. This
    can occur if the Speech On-Device API (SODA) files were not found.
    
    Bug: 1147226
    Change-Id: I86fe0a4b9fe80de0449e196b451423cbcf2b8a60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538054
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#827427}

--- chrome/browser/speech/speech_recognition_service.cc
+++ chrome/browser/speech/speech_recognition_service.cc
@@ -33,7 +33,9 @@ SpeechRecognitionService::~SpeechRecognitionService() = default;
 void SpeechRecognitionService::Create(
     mojo::PendingReceiver<media::mojom::SpeechRecognitionContext> receiver) {
   LaunchIfNotRunning();
-  speech_recognition_service_->BindContext(std::move(receiver));
+
+  if (speech_recognition_service_.is_bound())
+    speech_recognition_service_->BindContext(std::move(receiver));
 }
 
 void SpeechRecognitionService::OnNetworkServiceDisconnect() {
