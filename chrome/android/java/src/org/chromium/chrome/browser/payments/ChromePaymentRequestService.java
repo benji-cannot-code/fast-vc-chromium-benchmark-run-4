45d76020a00ad3ea32c677b4c66ce8deacab7f4b
Liquan (Max) Gu <maxlg@chromium.org>
2020-11-13 21:45:39 +0000

    [WebLayer] Invokes test callbacks for minimal UI's completion
    Before the CL, only the non-minimal UIs invokes the "onCompleteCalled"
    "onCompleteReplied" callbacks on the test observers. Since the callback
    names indicate that they are agnostic to minimal UI, they should be
    applied to all UIs.
    
    After the CL, the minimal UIs also invokes these callbacks.
    
    This CL also cleans up the PaymentUiService#onPaymentRequestComplete
    javadoc.
    
    Bug: 1131059
    
    Change-Id: Ic0b1a381083f9ad024c4bd84eb85e8500c034617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2535872
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827421}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ChromePaymentRequestService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ChromePaymentRequestService.java
@@ -408,14 +408,7 @@ public class ChromePaymentRequestService implements BrowserPaymentRequest,
                 null /* selectedShippingAddress */, null /* selectedShippingOption */, app);
     }
 
-    private void onMinimalUiCompletedAndClosed() {
-        if (mPaymentRequestService == null) return;
-        mPaymentRequestService.onComplete();
-        close();
-    }
-
-    /** Called after the non-minimal UI has handled {@link #complete}. */
-    private void onNonMinimalUiHandledComplete() {
+    private void onUiCompleted() {
         if (PaymentRequestService.getNativeObserverForTest() != null) {
             PaymentRequestService.getNativeObserverForTest().onCompleteCalled();
         }
@@ -670,8 +663,7 @@ public class ChromePaymentRequestService implements BrowserPaymentRequest,
         }
 
         mPaymentUiService.onPaymentRequestComplete(result,
-                /*onMinimalUiErroredAndClosed=*/this::close, this::onMinimalUiCompletedAndClosed,
-                this::onNonMinimalUiHandledComplete);
+                /*onMinimalUiErroredAndClosed=*/this::close, this::onUiCompleted);
     }
 
     // Implement BrowserPaymentRequest:
