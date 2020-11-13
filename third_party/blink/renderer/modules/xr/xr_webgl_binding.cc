123d46548c164252fa74f663fbf9fdebd46f7aae
Alexander Cooper <alcooper@chromium.org>
2020-11-13 16:33:14 +0000

    Add WPT and fix logic for XRWebGlBinding constructor
    Construction logic is defined:
    https://immersive-web.github.io/layers/#dom-xrwebglbinding-xrwebglbinding
    
    Change-Id: I01fa10b1d1413a70150134e2423630dc4d0546a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2536038
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827279}

--- third_party/blink/renderer/modules/xr/xr_webgl_binding.cc
+++ third_party/blink/renderer/modules/xr/xr_webgl_binding.cc
@@ -32,6 +32,13 @@ XRWebGLBinding* XRWebGLBinding::Create(XRSession* session,
     return nullptr;
   }
 
+  if (!session->immersive()) {
+    exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
+                                      "Cannot create an XRWebGLBinding for an "
+                                      "inline XRSession.");
+    return nullptr;
+  }
+
   WebGLRenderingContextBase* webgl_context =
       webglRenderingContextBaseFromUnion(context);
 
@@ -42,7 +49,7 @@ XRWebGLBinding* XRWebGLBinding::Create(XRSession* session,
     return nullptr;
   }
 
-  if (session->immersive() && !webgl_context->IsXRCompatible()) {
+  if (!webgl_context->IsXRCompatible()) {
     exception_state.ThrowDOMException(
         DOMExceptionCode::kInvalidStateError,
         "WebGL context must be marked as XR compatible in order to "
