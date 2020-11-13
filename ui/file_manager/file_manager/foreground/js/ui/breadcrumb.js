a1528e70da9d3b0084816a6c17bdf2fd13e50bc4
Noel Gordon <noel@chromium.org>
2020-11-13 06:54:41 +0000

    [breadcrumb] Add correct closing </cr-action-menu> tag
    Testing noted the bug was already fixed.  Changed tack here therefore,
    to use the correct closing </cr-action-menu> tag.  No need for a role=
    menu on this <cr-action-menu> element: WAI in M88 testing.
    
    Bug: 1134438
    Change-Id: I78ea53e34324bfdcffa53927d6a72136f4edb7e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2534423
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827185}

--- ui/file_manager/file_manager/foreground/js/ui/breadcrumb.js
+++ ui/file_manager/file_manager/foreground/js/ui/breadcrumb.js
@@ -175,7 +175,7 @@ const breadCrumbTemplate = `
   <button id='third'></button>
   <span caret hidden></span>
   <button id='fourth'></button>
-  <cr-action-menu id='elider-menu'></cr-menu-item>
+  <cr-action-menu id='elider-menu'></cr-action-menu>
 `;
 
 /**
