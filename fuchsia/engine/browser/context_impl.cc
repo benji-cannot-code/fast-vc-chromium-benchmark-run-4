a122903f4fb8436688546aa148bc7e4ccfac9e63
Kevin Marshall <kmarshall@chromium.org>
2020-11-13 03:15:47 +0000

    [fuchsia] Apply fuchsia.web.AutoplayPolicy from the FIDL API.
    Applies the fuchsia.web.AutoplayPolicy which may be passed in at Frame
    creation time to the Frame's top-level WebContents.
    
    Test: autoplay_browsertest.cc
    Bug: 1067101
    Change-Id: Id3b69ddea474c9b8d6f2a1b05ec195270cad3cf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533555
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827132}

--- fuchsia/engine/browser/context_impl.cc
+++ fuchsia/engine/browser/context_impl.cc
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fuchsia/cast_streaming/public/cast_streaming_session.h"
 #include "fuchsia/engine/browser/frame_impl.h"
 #include "fuchsia/engine/browser/web_engine_devtools_controller.h"
+#include "third_party/blink/public/common/web_preferences/web_preferences.h"
+#include "third_party/blink/public/mojom/webpreferences/web_preferences.mojom.h"
 
 ContextImpl::ContextImpl(content::BrowserContext* browser_context,
                          WebEngineDevToolsController* devtools_controller)
@@ -71,6 +73,27 @@ void ContextImpl::CreateFrameWithParams(
   content::WebContents::CreateParams create_params(browser_context_, nullptr);
   create_params.initially_hidden = true;
   auto web_contents = content::WebContents::Create(create_params);
+
+  blink::web_pref::WebPreferences web_preferences =
+      web_contents->GetOrCreateWebPreferences();
+
+  // REQUIRE_USER_ACTIVATION is the default per the FIDL API.
+  web_preferences.autoplay_policy =
+      blink::mojom::AutoplayPolicy::kDocumentUserActivationRequired;
+
+  if (params.has_autoplay_policy()) {
+    switch (params.autoplay_policy()) {
+      case fuchsia::web::AutoplayPolicy::ALLOW:
+        web_preferences.autoplay_policy =
+            blink::mojom::AutoplayPolicy::kNoUserGestureRequired;
+        break;
+      case fuchsia::web::AutoplayPolicy::REQUIRE_USER_ACTIVATION:
+        web_preferences.autoplay_policy =
+            blink::mojom::AutoplayPolicy::kDocumentUserActivationRequired;
+        break;
+    }
+  }
+  web_contents->SetWebPreferences(web_preferences);
 
   // Register the new Frame with the DevTools controller. The controller will
   // reject registration if user-debugging is requested, but it is not enabled
--- fuchsia/engine/test/web_engine_browser_test.h
+++ fuchsia/engine/test/web_engine_browser_test.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/macros.h"
+#include "base/optional.h"
 #include "content/public/test/browser_test_base.h"
 #include "fuchsia/engine/browser/context_impl.h"
 
@@ -27,10 +28,17 @@ class WebEngineBrowserTest : public content::BrowserTestBase {
   // object by WebEngineBrowserTest.
   static void SetContextClientChannel(zx::channel channel);
 
-  // Creates a Frame for this Context.
+  // Creates a Frame for this Context using default parameters.
   // |listener|: If set, specifies the navigation listener for the Frame.
   fuchsia::web::FramePtr CreateFrame(
       fuchsia::web::NavigationEventListener* listener);
+
+  // Creates a Frame for this Context using non-default parameters.
+  // |listener|: If set, specifies the navigation listener for the Frame.
+  // |params|: The CreateFrameParams to use.
+  fuchsia::web::FramePtr CreateFrameWithParams(
+      fuchsia::web::NavigationEventListener* listener,
+      fuchsia::web::CreateFrameParams params);
 
   // Gets the client object for the Context service.
   fuchsia::web::ContextPtr& context() { return context_; }
@@ -42,6 +50,8 @@ class WebEngineBrowserTest : public content::BrowserTestBase {
   navigation_listener_bindings() {
     return navigation_listener_bindings_;
   }
+
+  void SetHeadlessInCommandLine(base::CommandLine* command_line);
 
   void set_test_server_root(const base::FilePath& path) {
     test_server_root_ = path;
