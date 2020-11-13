ad8496f5c22d126fa32ab5cd95397d5f3a65dcea
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-11-13 20:58:06 +0000

    [layout] Don't freeze OOF-positioned scrollbars within flex layout.
    Change:
    https://chromium-review.googlesource.com/c/chromium/src/+/2424899
    ... introdcued a fix for freeze more consistent scrollbars for
    arbitrary descendants of flex layout. However the original code
    (detected direct descendant scrollbar changes) contained an issue that
    it would also freeze scrollbars for any OOF positioned children.
    
    There exists a wider issue which I've detailed here:
    https://bugs.chromium.org/p/chromium/issues/detail?id=1148835
    But it's relative rare.
    
    The fix for this specific regression is to unfreeze the scrollbars
    before running the OOF layout part.
    
    Bug: 1148288
    Change-Id: I74be0bf141d13988548d38412e912c8c98d628b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538041
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827402}

--- third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
@@ -1023,9 +1023,6 @@ scoped_refptr<const NGLayoutResult> NGFlexLayoutAlgorithm::Layout() {
 scoped_refptr<const NGLayoutResult>
 NGFlexLayoutAlgorithm::RelayoutIgnoringChildScrollbarChanges() {
   DCHECK(!ignore_child_scrollbar_changes_);
-  // Freezing the scrollbars for the sub-tree shouldn't be strictly necessary,
-  // but we do this just in case we trigger an unstable layout.
-  PaintLayerScrollableArea::FreezeScrollbarsScope freeze_scrollbars;
   NGLayoutAlgorithmParams params(
       Node(), container_builder_.InitialFragmentGeometry(), ConstraintSpace(),
       BreakToken(), /* early_break */ nullptr);
@@ -1035,6 +1032,13 @@ NGFlexLayoutAlgorithm::RelayoutIgnoringChildScrollbarChanges() {
 }
 
 scoped_refptr<const NGLayoutResult> NGFlexLayoutAlgorithm::LayoutInternal() {
+  // Freezing the scrollbars for the sub-tree shouldn't be strictly necessary,
+  // but we do this just in case we trigger an unstable layout.
+  base::Optional<PaintLayerScrollableArea::FreezeScrollbarsScope>
+      freeze_scrollbars;
+  if (ignore_child_scrollbar_changes_)
+    freeze_scrollbars.emplace();
+
   PaintLayerScrollableArea::DelayScrollOffsetClampScope delay_clamp_scope;
   ConstructAndAppendFlexItems();
 
@@ -1212,6 +1216,8 @@ scoped_refptr<const NGLayoutResult> NGFlexLayoutAlgorithm::LayoutInternal() {
   if (!success)
     return nullptr;
 
+  // Un-freeze descendant scrollbars before we run the OOF layout part.
+  freeze_scrollbars.reset();
   NGOutOfFlowLayoutPart(Node(), ConstraintSpace(), &container_builder_).Run();
 
   return container_builder_.ToBoxFragment();
