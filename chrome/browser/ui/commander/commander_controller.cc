01ac8cb385629de877306961e7af7265b12a0a2a
Leonard Grey <lgrey@chromium.org>
2020-11-13 20:19:45 +0000

    Commander: bookmark command source
    Adds commands for opening bookmarks:
    - A composite command labeled "Open Bookmarks..." that allows searching
    bookmarks when selected.
    - If user input is "long enough" (currently two characters, will be
    tuned), "noun first" bookmarks will begin to surface at top level.
    
    Bug: 1014639
    Change-Id: I910e9ba4992e7705cce2f8c5e23104c8ba6f408e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2535677
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827378}

--- chrome/browser/ui/commander/commander_controller.cc
+++ chrome/browser/ui/commander/commander_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/branding_buildflags.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/commander/apps_command_source.h"
+#include "chrome/browser/ui/commander/bookmark_command_source.h"
 #include "chrome/browser/ui/commander/commander_view_model.h"
 #include "chrome/browser/ui/commander/simple_command_source.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
@@ -25,6 +26,7 @@ CommanderController::CommandSources CreateDefaultSources() {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   sources.push_back(std::make_unique<AppsCommandSource>());
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  sources.push_back(std::make_unique<BookmarkCommandSource>());
   return sources;
 }
 
