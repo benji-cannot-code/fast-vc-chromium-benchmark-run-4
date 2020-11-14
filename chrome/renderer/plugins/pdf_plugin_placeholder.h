6c15d0f6d26726337b14adae24dc05c6dd785cb8
Zentaro Kavanagh <zentaro@chromium.org>
2020-11-14 01:57:06 +0000

    Mark classes with final dtors as final
    - -Wfinal-dtor-non-final-class warns on classes with final dtors but
      not final classes.
    - Error messages are better when the class is marked final.
    - Fix existing issues in code base and remove warning exemption
    
    Bug: 999886
    Test: no errors building
    Change-Id: I0814e4c543eb7f6d25bb0dfc1990b30e0b0c2f80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2468299
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827532}

--- chrome/renderer/plugins/pdf_plugin_placeholder.h
+++ chrome/renderer/plugins/pdf_plugin_placeholder.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // plugins are disabled and the PDF fails to load.
 // TODO(amberwon): Flesh out the class more to download an embedded PDF when the
 // PDF plugin is disabled or unavailable.
-class PDFPluginPlaceholder : public plugins::PluginPlaceholderBase,
-                             public gin::Wrappable<PDFPluginPlaceholder> {
+class PDFPluginPlaceholder final : public plugins::PluginPlaceholderBase,
+                                   public gin::Wrappable<PDFPluginPlaceholder> {
  public:
   static gin::WrapperInfo kWrapperInfo;
 
