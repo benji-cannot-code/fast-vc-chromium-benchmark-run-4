7d5f0de5f043c4f6a4336ca6ba208056568c95a6
S. Ganesh <ganesh@chromium.org>
2020-11-14 01:47:32 +0000

    Use midl.py's new dynamic guid mechanism for all Updater COM Classes.
    This CL uses the feature added in Issue 1109612 to change all Updater
    COM Classes to have dynamic guids.
    
    Bug: 1148167
    Change-Id: I8026a251e5cdc51692b4b37f76407c2fa67bd33a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533865
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827528}

--- third_party/win_build_output/midl/chrome/updater/app/server/win/x86/updater_internal_idl.h
+++ third_party/win_build_output/midl/chrome/updater/app/server/win/x86/updater_internal_idl.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  /* File created by MIDL compiler version 8.xx.xxxx */
 /* at a redacted point in time
  */
-/* Compiler settings for ../../chrome/updater/app/server/win/updater_internal_idl.template:
+/* Compiler settings for gen/chrome/updater/app/server/win/updater_internal_idl.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.xx.xxxx 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
