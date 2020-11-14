f56e15f11cfaf14cecc7956b0c92909a04b5e8b5
Scott Violet <sky@chromium.org>
2020-11-14 02:42:50 +0000

    android: minor cleanup of TabImpl
    Removes unnecessary jni args.
    
    BUG=none
    TEST=none
    
    Change-Id: I19444fe8736577ac4544b0f3773ec20ffd678fcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538284
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827543}

--- chrome/browser/android/tab_android.cc
+++ chrome/browser/android/tab_android.cc
@@ -241,13 +241,12 @@ void TabAndroid::RemoveObserver(Observer* observer) {
   observers_.RemoveObserver(observer);
 }
 
-void TabAndroid::Destroy(JNIEnv* env, const JavaParamRef<jobject>& obj) {
+void TabAndroid::Destroy(JNIEnv* env) {
   delete this;
 }
 
 void TabAndroid::InitWebContents(
     JNIEnv* env,
-    const JavaParamRef<jobject>& obj,
     jboolean incognito,
     jboolean is_background_tab,
     const JavaParamRef<jobject>& jweb_contents,
@@ -293,7 +292,6 @@ void TabAndroid::InitWebContents(
 
 void TabAndroid::UpdateDelegates(
     JNIEnv* env,
-    const JavaParamRef<jobject>& obj,
     const JavaParamRef<jobject>& jweb_contents_delegate,
     const JavaParamRef<jobject>& jcontext_menu_populator_factory) {
   ContextMenuHelper::FromWebContents(web_contents())
@@ -315,8 +313,7 @@ void WillRemoveWebContentsFromTab(content::WebContents* contents) {
 }
 }  // namespace
 
-void TabAndroid::DestroyWebContents(JNIEnv* env,
-                                    const JavaParamRef<jobject>& obj) {
+void TabAndroid::DestroyWebContents(JNIEnv* env) {
   WillRemoveWebContentsFromTab(web_contents());
 
   // Terminate the renderer process if this is the last tab.
@@ -336,8 +333,7 @@ void TabAndroid::DestroyWebContents(JNIEnv* env,
   synced_tab_delegate_->ResetWebContents();
 }
 
-void TabAndroid::ReleaseWebContents(JNIEnv* env,
-                                    const JavaParamRef<jobject>& obj) {
+void TabAndroid::ReleaseWebContents(JNIEnv* env) {
   WillRemoveWebContentsFromTab(web_contents());
 
   // Ownership of |released_contents| is assumed by the code that initiated the
@@ -353,7 +349,6 @@ void TabAndroid::ReleaseWebContents(JNIEnv* env,
 
 void TabAndroid::OnPhysicalBackingSizeChanged(
     JNIEnv* env,
-    const JavaParamRef<jobject>& obj,
     const JavaParamRef<jobject>& jweb_contents,
     jint width,
     jint height) {
@@ -365,7 +360,6 @@ void TabAndroid::OnPhysicalBackingSizeChanged(
 
 TabAndroid::TabLoadStatus TabAndroid::LoadUrl(
     JNIEnv* env,
-    const JavaParamRef<jobject>& obj,
     const JavaParamRef<jstring>& url,
     const JavaParamRef<jobject>& j_initiator_origin,
     const JavaParamRef<jstring>& j_extra_headers,
@@ -442,7 +436,6 @@ TabAndroid::TabLoadStatus TabAndroid::LoadUrl(
 
 void TabAndroid::SetActiveNavigationEntryTitleForUrl(
     JNIEnv* env,
-    const JavaParamRef<jobject>& obj,
     const JavaParamRef<jstring>& jurl,
     const JavaParamRef<jstring>& jtitle) {
   DCHECK(web_contents());
@@ -461,8 +454,7 @@ void TabAndroid::SetActiveNavigationEntryTitleForUrl(
     entry->SetTitle(title);
 }
 
-void TabAndroid::LoadOriginalImage(JNIEnv* env,
-                                   const JavaParamRef<jobject>& obj) {
+void TabAndroid::LoadOriginalImage(JNIEnv* env) {
   content::RenderFrameHost* render_frame_host =
       web_contents()->GetFocusedFrame();
   mojo::AssociatedRemote<chrome::mojom::ChromeRenderFrame> renderer;
