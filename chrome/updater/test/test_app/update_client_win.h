fd14561463e703386c228efc8620aa4608f9f367
Sorin Jianu <sorin@chromium.org>
2020-11-14 00:13:17 +0000

    Split the updater IDL file.
    The IDL file is split in three parts: public, internal, and legacy
    definitions.
    
    This is a fairly straightforward change, which consists of suffixing
    the path to the typelib in the executable file with a //<index> where
    the index matches one of the there source IDLs above.
    
    Further work is needed to properly generate the GUIDs depending on
    the which IDL the GUIDs belong to. Some of the GUIDs must depend on
    branding, and some of the GUIDs must depend on branding and version.
    
    This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/2527822
    after the following fixes:
    
    diff --git a/chrome/updater/test/test_app/update_client_win.cc b/chrome/updater/test/test_app/update_client_win.cc
    index 13ffa873ce6c..71c8e4c090c6 100644
    --- a/chrome/updater/test/test_app/update_client_win.cc
    +++ b/chrome/updater/test/test_app/update_client_win.cc
    @@ -28,11 +28,12 @@
     #include "chrome/updater/update_service.h"
    
     namespace updater {
    +
     namespace {
    
     HRESULT CreateUpdaterInterface(Microsoft::WRL::ComPtr<IUpdater>* updater) {
       Microsoft::WRL::ComPtr<IUnknown> server;
    -  HRESULT hr = ::CoCreateInstance(__uuidof(UpdaterClass), nullptr,
    +  HRESULT hr = ::CoCreateInstance(__uuidof(UpdaterClass)], nullptr,
                                       CLSCTX_LOCAL_SERVER, IID_PPV_ARGS(&server));
    
       if (FAILED(hr)) {
    diff --git a/chrome/updater/test/test_app/update_client_win.h b/chrome/updater/test/test_app/update_client_win.h
    index 25e16c53cc09..e5496ec47828 100644
    --- a/chrome/updater/test/test_app/update_client_win.h
    +++ b/chrome/updater/test/test_app/update_client_win.h
    @@ -10,7 +10,7 @@
     #include <windows.h>
     #include <wrl/client.h>
    
    -struct IUpdater;
    +class IUpdater;
    
     namespace base {
     class SingleThreadTaskRunner;
    
    Bug: 1140562
    
    Change-Id: Ibce69536e2816ab089f8851191af052ff7616565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538267
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827491}

--- chrome/updater/test/test_app/update_client_win.h
+++ chrome/updater/test/test_app/update_client_win.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #include <wrl/client.h>
 
-#include "chrome/updater/app/server/win/updater_idl.h"
+struct IUpdater;
 
 namespace base {
 class SingleThreadTaskRunner;
