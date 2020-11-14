5e5d0dec26bdf5b02e77be87947efe98675f9a11
Nate Chapin <japhet@chromium.org>
2020-11-14 00:46:34 +0000

    Make USB a Supplement<NavigatorBase>
    NavigatorBase is a newly-added base class for Navigator and
    WorkerNavigator. Making USB a Supplement of NavigatorBase allows
    a single class to handle all of the work of exposing the
    navigator.usb getter for both windows and workers, and greatly
    reduces boilerplate.
    
    Bug: 1147612
    Change-Id: I4d13bf6948301b65663a48fd34d4410c9596b21b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2536387
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827506}

--- third_party/blink/renderer/modules/webusb/usb.h
+++ third_party/blink/renderer/modules/webusb/usb.h
@@ -19,22 +19,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_associated_receiver.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 
 namespace blink {
 
 class ExceptionState;
+class NavigatorBase;
 class ScriptPromiseResolver;
 class ScriptState;
 class USBDevice;
 class USBDeviceRequestOptions;
 
 class USB final : public EventTargetWithInlineData,
+                  public Supplement<NavigatorBase>,
                   public ExecutionContextLifecycleObserver,
                   public device::mojom::blink::UsbDeviceManagerClient {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  explicit USB(ExecutionContext&);
+  static const char kSupplementName[];
+
+  // Getter for navigator.usb
+  static USB* usb(NavigatorBase&);
+
+  explicit USB(NavigatorBase&);
   ~USB() override;
 
   // USB.idl
