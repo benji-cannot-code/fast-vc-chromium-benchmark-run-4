2cf2033627f2c45d4d8cecf74fae9512ddf02caf
Sorin Jianu <sorin@chromium.org>
2020-11-15 05:31:15 +0000

    Updater: ExistenceCheckerPath - check for ownership of installed app.
    Bug: 1147094
    Change-Id: I46e84ea854d6719a56885227bf19d4d39d337449
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2495332
    Commit-Queue: Mila Green <milagreen@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827621}

--- chrome/updater/win/util.cc
+++ chrome/updater/win/util.cc
@@ -388,4 +388,10 @@ base::win::ScopedHandle GetUserTokenFromCurrentSessionId() {
   return token_handle;
 }
 
+bool PathOwnedByUser(const base::FilePath& path) {
+  // TODO(crbug.com/1147094): Implement for Win.
+
+  return true;
+}
+
 }  // namespace updater
