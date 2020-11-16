5fcf24b6fd29c5d84149b188c792c0775ec3b521
Adithya Srinivasan <adithyas@chromium.org>
2020-11-16 17:05:03 +0000

    Portals: Fix a11y for orphaned portals
    Orphaned portals [1] don't have a RWHV (but did at some point), and can
    have AXTree updates sent from its renderer process which would be
    subsequently ignored in the browser process (due to there being no RWHV).
    Once an orphaned portal is inserted into the DOM tree, we create a RWHV
    for it, and at this point any new AXTree updates received will be invalid
    as the browser's tree state is different from the renderer's.
    
    To fix this, this CL makes BrowserAccessibilityManager creation
    independent of a view being present. The view is only used on android
    to pass it a pointer to WCAXAndroid, and we now do this through
    a BrowserAccessibilityDelegate method.
    
    [1] See "orphaned" here: https://wicg.github.io/portals/#concepts
    
    Bug: 1139784
    Change-Id: Ifdebe1f05e37a6f704b937101ea44bb824cf045b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2499861
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Aaron Colwell <acolwell@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827803}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -1687,17 +1687,6 @@ RenderWidgetHostViewAndroid::FilterInputEvent(
   return blink::mojom::InputEventResultState::kNotConsumed;
 }
 
-BrowserAccessibilityManager*
-RenderWidgetHostViewAndroid::CreateBrowserAccessibilityManager(
-    BrowserAccessibilityDelegate* delegate,
-    bool for_root_frame) {
-  auto* wcax = GetWebContentsAccessibilityAndroid();
-  return new BrowserAccessibilityManagerAndroid(
-      BrowserAccessibilityManagerAndroid::GetEmptyDocument(),
-      for_root_frame && host() && wcax ? wcax->GetWeakPtr() : nullptr,
-      delegate);
-}
-
 blink::mojom::PointerLockResult RenderWidgetHostViewAndroid::LockMouse(
     bool request_unadjusted_movement) {
   NOTIMPLEMENTED();
@@ -2352,6 +2341,11 @@ void RenderWidgetHostViewAndroid::DidNavigate() {
     delegated_frame_host_->DidNavigate();
   }
   is_first_navigation_ = false;
+}
+
+WebContentsAccessibility*
+RenderWidgetHostViewAndroid::GetWebContentsAccessibility() {
+  return web_contents_accessibility_;
 }
 
 viz::ScopedSurfaceIdAllocator
