96d03cf90f2def136756dd37bcefe9754a562e2d
Ehimare Okoyomon <eokoyomon@chromium.org>
2020-11-16 13:44:58 +0000

    [Android] Add blocked permission icons programmatically
    Create function to add a strikethough overlay on top of icon if the
    permission is blocked.
    
    Screenshots: crbug.com/1077766#c96
    Bug: 1077766
    Change-Id: I6d081ad69c4817be3158aacca9f8d6997a12d36d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2526302
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827767}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleWebsiteSettings.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleWebsiteSettings.java
@@ -10,6 +10,7 @@ import static org.chromium.components.browser_ui.site_settings.WebsitePreference
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -352,6 +353,19 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
         return origin.equals(info.getOrigin())
                 && (origin.equals(info.getEmbedderSafe())
                         || SITE_WILDCARD.equals(info.getEmbedderSafe()));
+    }
+
+    private Drawable getContentSettingsIcon(@ContentSettingsType int contentSettingsType,
+            @ContentSettingValues @Nullable Integer value, boolean enabled) {
+        Drawable icon = enabled
+                ? SettingsUtils.getTintedIcon(
+                        getActivity(), ContentSettingsResources.getIcon(contentSettingsType))
+                : ContentSettingsResources.getDisabledIcon(contentSettingsType, getResources());
+        if (getSiteSettingsClient().isPageInfoV2Enabled() && value != null
+                && value == ContentSettingValues.BLOCK) {
+            return ContentSettingsResources.getBlockedIcon(getResources(), icon);
+        }
+        return icon;
     }
 
     /**
@@ -859,8 +873,7 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
             preference.setTitle(titleResourceId);
         }
         if (!preference.isEnabled()) {
-            preference.setIcon(
-                    ContentSettingsResources.getDisabledIcon(contentType, getResources()));
+            preference.setIcon(getContentSettingsIcon(contentType, value, false));
             return;
         }
         SiteSettingsCategory category = SiteSettingsCategory.createFromContentSettingsType(
@@ -870,8 +883,7 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
             preference.setIcon(category.getDisabledInAndroidIcon(getActivity()));
             preference.setEnabled(false);
         } else {
-            preference.setIcon(SettingsUtils.getTintedIcon(
-                    getActivity(), ContentSettingsResources.getIcon(contentType)));
+            preference.setIcon(getContentSettingsIcon(contentType, value, true));
         }
     }
 
@@ -1046,6 +1058,7 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
                 preference.setSummary(
                         getString(ContentSettingsResources.getCategorySummary(permission)));
             }
+            preference.setIcon(getContentSettingsIcon(type, permission, true));
 
             if (mWebsiteSettingsObserver != null) {
                 mWebsiteSettingsObserver.onPermissionChanged();
