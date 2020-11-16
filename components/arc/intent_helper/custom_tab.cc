3028250e868344ed51099af6771b8cfaaf276879
Sigurdur Asgeirsson <siggi@chromium.org>
2020-11-16 20:02:34 +0000

    Retire ScopedObserver in /components/arc/intent_helper.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=djacobo@chromium.org
    
    Bug: 1145565
    Change-Id: I436bf817ddb0820060ddcc817c324cf2ae5c9cee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532715
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827867}

--- components/arc/intent_helper/custom_tab.cc
+++ components/arc/intent_helper/custom_tab.cc
@@ -21,7 +21,7 @@ namespace arc {
 
 CustomTab::CustomTab(aura::Window* arc_app_window)
     : arc_app_window_(arc_app_window) {
-  arc_app_window_observer_.Add(arc_app_window_);
+  arc_app_window_observation_.Observe(arc_app_window_);
   host_->set_owned_by_client();
   auto* const widget = views::Widget::GetWidgetForNativeWindow(arc_app_window_);
   DCHECK(widget);
@@ -36,7 +36,7 @@ void CustomTab::Attach(gfx::NativeView view) {
   host_->Attach(view);
   aura::Window* const container = host_->GetNativeViewContainer();
   container->SetEventTargeter(std::make_unique<aura::WindowTargeter>());
-  other_windows_observer_.Add(container);
+  other_windows_observation_.Observe(container);
   EnsureWindowOrders();
   UpdateHostBounds(arc_app_window_);
 }
@@ -49,7 +49,7 @@ void CustomTab::OnWindowBoundsChanged(aura::Window* window,
                                       const gfx::Rect& old_bounds,
                                       const gfx::Rect& new_bounds,
                                       ui::PropertyChangeReason reason) {
-  if (arc_app_window_observer_.IsObserving(window) &&
+  if (arc_app_window_observation_.IsObservingSource(window) &&
       old_bounds.size() != new_bounds.size()) {
     UpdateHostBounds(window);
   }
@@ -70,10 +70,10 @@ void CustomTab::OnWindowStackingChanged(aura::Window* window) {
 }
 
 void CustomTab::OnWindowDestroying(aura::Window* window) {
-  if (arc_app_window_observer_.IsObserving(window))
-    arc_app_window_observer_.Remove(window);
-  if (other_windows_observer_.IsObserving(window))
-    other_windows_observer_.Remove(window);
+  if (arc_app_window_observation_.IsObservingSource(window))
+    arc_app_window_observation_.RemoveObservation();
+  if (other_windows_observation_.IsObservingSource(window))
+    other_windows_observation_.RemoveObservation();
 }
 
 void CustomTab::UpdateHostBounds(aura::Window* arc_app_window) {
