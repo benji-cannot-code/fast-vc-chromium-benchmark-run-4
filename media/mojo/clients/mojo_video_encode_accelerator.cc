30448720edc064633ef69bff8bc1ff360b4c0590
Zhaoliang Ma <zhaoliang.ma@intel.com>
2020-11-16 03:00:57 +0000

    MojoVEA: Implement mojo video encoder IsFlushSupported/FLush
    In webcodecs, VideoEncodeAcceleratorAdapter needs to call Flush method,
    since the mojoVEA doesn't support IsFlushSupported, so the
    VideoEncodeAcceleratorAdapter cannot really call the backendVEA Flush
    even the backendVEA support Flush. This CL fix this issue.
    
    Bug: 1128768, 1110279
    Change-Id: I4853c2bd0e494cafdd806d183ab3d659ca542d7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2389840
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Zhaoliang Ma <zhaoliang.ma@intel.com>
    Cr-Commit-Position: refs/heads/master@{#827668}

--- media/mojo/clients/mojo_video_encode_accelerator.cc
+++ media/mojo/clients/mojo_video_encode_accelerator.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/mojo/clients/mojo_video_encode_accelerator.h"
 
+#include <utility>
+
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/logging.h"
@@ -249,6 +251,24 @@ void MojoVideoEncodeAccelerator::RequestEncodingParametersChange(
   DCHECK(vea_.is_bound());
 
   vea_->RequestEncodingParametersChange(bitrate, framerate);
+}
+
+bool MojoVideoEncodeAccelerator::IsFlushSupported() {
+  DVLOG(2) << __func__;
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(vea_.is_bound());
+
+  bool flush_support = false;
+  vea_->IsFlushSupported(&flush_support);
+  return flush_support;
+}
+
+void MojoVideoEncodeAccelerator::Flush(FlushCallback flush_callback) {
+  DVLOG(2) << __func__;
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(vea_.is_bound());
+
+  vea_->Flush(std::move(flush_callback));
 }
 
 void MojoVideoEncodeAccelerator::Destroy() {
