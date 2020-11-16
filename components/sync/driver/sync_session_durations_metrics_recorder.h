5558b1cc5218a7972e9804c5da991be79f777f7d
Sigurdur Asgeirsson <siggi@chromium.org>
2020-11-16 19:14:42 +0000

    Retire ScopedObserver in /components/sync.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=rushans@google.com
    
    Bug: 1145565
    Change-Id: I760ad7d03410e304b00db5497fb44120e9768fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532959
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#827855}

--- components/sync/driver/sync_session_durations_metrics_recorder.h
+++ components/sync/driver/sync_session_durations_metrics_recorder.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "base/timer/elapsed_timer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
@@ -75,10 +75,11 @@ class SyncSessionDurationsMetricsRecorder
   SyncService* const sync_service_;
   signin::IdentityManager* const identity_manager_;
 
-  ScopedObserver<syncer::SyncService, syncer::SyncServiceObserver>
-      sync_observer_{this};
-  ScopedObserver<signin::IdentityManager, signin::IdentityManager::Observer>
-      identity_manager_observer_{this};
+  base::ScopedObservation<syncer::SyncService, syncer::SyncServiceObserver>
+      sync_observation_{this};
+  base::ScopedObservation<signin::IdentityManager,
+                          signin::IdentityManager::Observer>
+      identity_manager_observation_{this};
 
   // Tracks the elapsed active session time while the browser is open. The timer
   // is absent if there's no active session.
