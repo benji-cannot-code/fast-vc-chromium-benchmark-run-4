4d56b5c45b8c812bdfdc3c41dcf2938d4d62153c
Darren Shen <shend@chromium.org>
2020-11-16 05:50:38 +0000

    ime: Remove unused statement and include.
    It does not affect the test because SystemDecoder runs in a separate
    process, so setting the fake main entry does not affect SystemDecoder.
    
    Bug: 1148157
    Change-Id: I99b5fac2da26be833f76063becbf586578d2965f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537253
    Reviewed-by: My Nguyen <myy@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827696}

--- chrome/browser/chromeos/input_method/native_input_method_engine_browsertest.cc
+++ chrome/browser/chromeos/input_method/native_input_method_engine_browsertest.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/constants/chromeos_pref_names.h"
-#include "chromeos/services/ime/decoder/system_engine.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
@@ -151,7 +150,6 @@ class NativeInputMethodEngineTest : public InProcessBrowserTest,
 
  protected:
   void SetUp() override {
-    ime::FakeEngineMainEntryForTesting(nullptr);
     mojo::core::Init();
     InProcessBrowserTest::SetUp();
     ui::IMEBridge::Initialize();
