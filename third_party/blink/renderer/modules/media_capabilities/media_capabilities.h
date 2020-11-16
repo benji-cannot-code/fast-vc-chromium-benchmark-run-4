0387cea55eb25bbd9b5389373cdfb6b5d5a20ecb
Nate Chapin <japhet@chromium.org>
2020-11-16 23:22:01 +0000

    Make MediaCapabilities a Supplement<NavigatorBase>
    NavigatorBase is a newly-added base class for Navigator and
    WorkerNavigator. Making MediaCapabilities a Supplement of NavigatorBase
    allows a single class to handle all of the work of exposing the
    navigator.mediaCapabilities getter for both windows and workers, and
    greatly reduces boilerplate.
    
    Bug: 1147612
    Change-Id: I06f0e138a2be9ce0cd170e43c7997cd69340de24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537780
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827969}

--- third_party/blink/renderer/modules/media_capabilities/media_capabilities.h
+++ third_party/blink/renderer/modules/media_capabilities/media_capabilities.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/visitor.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
 
 namespace blink {
@@ -26,18 +27,26 @@ class ExecutionContext;
 class MediaDecodingConfiguration;
 class MediaEncodingConfiguration;
 class MediaKeySystemAccess;
+class NavigatorBase;
 class ScriptPromise;
 class ScriptPromiseResolver;
 class ScriptState;
 
-class MODULES_EXPORT MediaCapabilities final : public ScriptWrappable {
+class MODULES_EXPORT MediaCapabilities final
+    : public ScriptWrappable,
+      public Supplement<NavigatorBase> {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
   static const char kLearningBadWindowThresholdParamName[];
   static const char kLearningNnrThresholdParamName[];
 
-  explicit MediaCapabilities(ExecutionContext* context);
+  static const char kSupplementName[];
+
+  // Getter for navigator.mediaCapabilities
+  static MediaCapabilities* mediaCapabilities(NavigatorBase&);
+
+  explicit MediaCapabilities(NavigatorBase&);
 
   void Trace(blink::Visitor* visitor) const override;
 
--- third_party/blink/renderer/modules/media_capabilities/media_capabilities_test.cc
+++ third_party/blink/renderer/modules/media_capabilities/media_capabilities_test.cc
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_media_configuration.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_media_decoding_configuration.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_video_configuration.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/core/frame/navigator.h"
 #include "third_party/blink/renderer/core/testing/page_test_base.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/testing/testing_platform_support.h"
@@ -287,8 +289,8 @@ class MediaCapabilitiesTestContext {
                       &MockPerfHistoryService::BindRequest,
                       base::Unretained(perf_history_service_.get()))));
 
-    media_capabilities_ = MakeGarbageCollected<MediaCapabilities>(
-        v8_scope_.GetExecutionContext());
+    media_capabilities_ = MediaCapabilities::mediaCapabilities(
+        *v8_scope_.GetWindow().navigator());
   }
 
   ~MediaCapabilitiesTestContext() {
