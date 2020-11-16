5b3b436afff141a3be361950b3f67b58bc59507f
Colin Blundell <blundell@chromium.org>
2020-11-16 10:09:29 +0000

    [WebLayer] Initial bringup of ad blocking
    This CL does the initial bringup of ad tagging and blocking
    functionality in WebLayer. To do so, we do the following:
    - Bring up an implementation of SubresourceFilterClient and attach it
      to WebContents instances
    - Instantiate subresource filter navigation throttles on navigation
    - Instantiates and configures SubresourceFilterAgent instances in the
      renderer
    
    The organization and code added is modeled after that in //chrome. We
    have left TODOs for significant pieces of functionality to be added
    in followups (e.g., showing of the infobar on Android). One thing to
    note is that ad tagging/blocking only triggers on Android in WebLayer,
    as it requires having a safe browsing database instance and WebLayer
    does not have one on platforms other than Android.
    
    This CL also adds browsertests of this initial functionality:
    - that the subresource filter activation state is computed on page loads
    - that ads are blocked on activated pages if they are flagged by the
      ruleset
    
    Bug: 1116095
    Change-Id: I1db0136709014004c221bb0251551eaeac3538e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2526087
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827735}

--- weblayer/renderer/content_renderer_client_impl.cc
+++ weblayer/renderer/content_renderer_client_impl.cc
@@ -20,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/no_state_prefetch/renderer/prerender_utils.h"
 #include "components/no_state_prefetch/renderer/prerenderer_client.h"
 #include "components/page_load_metrics/renderer/metrics_render_frame_observer.h"
+#include "components/subresource_filter/content/renderer/subresource_filter_agent.h"
+#include "components/subresource_filter/content/renderer/unverified_ruleset_dealer.h"
+#include "components/subresource_filter/core/common/common_features.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_thread.h"
 #include "content/public/renderer/render_view.h"
@@ -86,6 +89,10 @@ void ContentRendererClientImpl::RenderThreadStarted() {
 
   browser_interface_broker_ =
       blink::Platform::Current()->GetBrowserInterfaceBroker();
+
+  subresource_filter_ruleset_dealer_ =
+      std::make_unique<subresource_filter::UnverifiedRulesetDealer>();
+  thread->AddObserver(subresource_filter_ruleset_dealer_.get());
 }
 
 void ContentRendererClientImpl::RenderFrameCreated(
@@ -108,6 +115,11 @@ void ContentRendererClientImpl::RenderFrameCreated(
     agent->SetContentSettingRules(weblayer_observer_->content_setting_rules());
 
   new page_load_metrics::MetricsRenderFrameObserver(render_frame);
+
+  // TODO(crbug.com/1116095): Bring up AdResourceTracker?
+  new subresource_filter::SubresourceFilterAgent(
+      render_frame, subresource_filter_ruleset_dealer_.get(),
+      /*ad_resource_tracker=*/nullptr);
 
 #if defined(OS_ANDROID)
   // |SpellCheckProvider| manages its own lifetime (and destroys itself when the
