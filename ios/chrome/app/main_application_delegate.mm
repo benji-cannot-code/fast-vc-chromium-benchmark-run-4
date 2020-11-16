55eb56a99a1fc5eefd826e9c32cae99d32eeeec5
Sylvain Defresne <sdefresne@chromium.org>
2020-11-16 13:01:59 +0000

    [ios] Remove incorrect dependency of //base//ios on //ios
    Conceptually, //ios is on top of //base, so having a dependency
    from //base/ios on //ios is breaking the layering (this also
    make it difficult to use //base without using all of Chromium).
    
    Implement IsMultiwindowSupported() and IsSceneStartupSupported()
    by checking for the scene API manifest information in the app
    Info.plist which is more robust as it will work for any iOS app
    build on top of //base that set the information in Info.plist
    even if they do not re-use the rest of Chromium.
    
    Bug: none
    Change-Id: I5ea006ea617c284dc114b2f8f8ecd6eb2f2dcc1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2529122
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827759}

--- ios/chrome/app/main_application_delegate.mm
+++ ios/chrome/app/main_application_delegate.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/main_application_delegate.h"
 
 #include "base/ios/ios_util.h"
-#include "base/ios/multi_window_buildflags.h"
 #include "base/mac/foundation_util.h"
 #include "base/metrics/user_metrics.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
@@ -22,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/chrome_overlay_window.h"
 #import "ios/chrome/app/main_application_delegate_testing.h"
 #import "ios/chrome/app/main_controller.h"
+#include "ios/chrome/app/multi_window_buildflags.h"
 #import "ios/chrome/browser/ui/main/scene_controller.h"
 #import "ios/chrome/browser/ui/main/scene_delegate.h"
 #import "ios/chrome/browser/ui/main/scene_state.h"
--- ios/chrome/browser/ui/main/incognito_blocker_scene_agent.mm
+++ ios/chrome/browser/ui/main/incognito_blocker_scene_agent.mm
@@ -77,7 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)showOverlay {
   NSArray<UIWindow*>* windows = nil;
 
-  if (IsMultiwindowSupported()) {
+  if (IsSceneStartupSupported()) {
     if (@available(iOS 13, *)) {
       windows = self.sceneState.scene.windows;
     }
