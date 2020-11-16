c3e55109a9c090c47b5a7471a50c4753e0ec783b
Evan Stade <estade@chromium.org>
2020-11-16 17:06:11 +0000

    Remove obsolete include from site_engagement_service.cc
    Bug: 775390
    Change-Id: I10ed76b52ca4b3c544a69ce5aa99ed7be1e262d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538362
    Auto-Submit: Evan Stade <estade@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827805}

--- chrome/browser/engagement/site_engagement_service.cc
+++ chrome/browser/engagement/site_engagement_service.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/default_clock.h"
 #include "base/time/time.h"
 #include "base/values.h"
-#include "chrome/browser/banners/app_banner_settings_helper.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/engagement/site_engagement_helper.h"
 #include "chrome/browser/engagement/site_engagement_metrics.h"
