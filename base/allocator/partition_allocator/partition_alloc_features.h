2e22617a0fcf9541bb38f0dad4a2089afb52ed4b
Michael Lippautz <mlippautz@chromium.org>
2020-11-16 22:12:37 +0000

    PCScan: Remove compile-time guard
    These days PCScan and its bitmaps are lazily committed after startup.
    Since bitmaps are not committed until PCScan is enabled, there is no
    need for having a separate compile-time flag to avoid paying for used
    memory.
    
    Bug: 1129751
    Change-Id: I9226a718be6309bc43de5f9f67e7c84e223eacbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2539198
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827925}

--- base/allocator/partition_allocator/partition_alloc_features.h
+++ base/allocator/partition_allocator/partition_alloc_features.h
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <VersionHelpers.h>
 #endif
 
-#define ALLOW_ENABLING_PCSCAN 0
-
 namespace base {
 
 struct Feature;
@@ -64,9 +62,9 @@ ALWAYS_INLINE bool IsPartitionAllocGigaCageEnabled() {
 }
 
 ALWAYS_INLINE bool IsPartitionAllocPCScanEnabled() {
-#if !defined(PA_HAS_64_BITS_POINTERS) || !ALLOW_ENABLING_PCSCAN
+#if !defined(PA_HAS_64_BITS_POINTERS)
   return false;
-#endif
+#endif  // !PA_HAS_64_BITS_POINTERS
   // TODO(bikineev): Calling this function can allocate which can cause
   // reentrancy for the 'PA as malloc' configuration.
   return FeatureList::IsEnabled(kPartitionAllocPCScan);
--- base/allocator/partition_allocator/pcscan_unittest.cc
+++ base/allocator/partition_allocator/pcscan_unittest.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_alloc.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if ALLOW_ENABLING_PCSCAN
-
 namespace base {
 namespace internal {
 
@@ -307,7 +305,5 @@ TEST_F(PCScanTest, DanglingInnerReference) {
 
 }  // namespace internal
 }  // namespace base
-
-#endif
 
 #endif  // defined(MEMORY_TOOL_REPLACES_ALLOCATOR)
