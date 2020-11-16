fc8c5ae53049268c20c43b5c1681038659fb8cd8
Zhaoyang Li <zhaoyangli@chromium.org>
2020-11-16 22:27:47 +0000

    [iOS] Reenable device restore tests for devices.
    The tests can pass on device now, Reenable every test under the bug
    number and test utility.
    
    Bug: 1067821
    Change-Id: I1fdbeed8bb42eb47705f883426def8e67287d1a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2523306
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827936}

--- ios/chrome/test/earl_grey2/smoke_egtest.mm
+++ ios/chrome/test/earl_grey2/smoke_egtest.mm
@@ -247,16 +247,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Tests gracefully kill through AppLaunchManager.
 - (void)testAppLaunchManagerForceRelaunchByCleanShutdown {
-// TODO(crbug.com/1067821): ForceRelaunchByCleanShutdown only compiles and works
-// on simulator.
-#if TARGET_IPHONE_SIMULATOR
   [ChromeEarlGrey openNewTab];
   [[AppLaunchManager sharedManager] ensureAppLaunchedWithFeaturesEnabled:{}
       disabled:{}
       relaunchPolicy:ForceRelaunchByCleanShutdown];
   [[EarlGrey selectElementWithMatcher:grey_text(@"Restore")]
       assertWithMatcher:grey_notVisible()];
-#endif
 }
 
 // Tests hard kill(crash) through AppLaunchManager.
