c4b7cc2b255f7ba4332bff5c47802e24460fc96a
Eric Willigers <ericwilligers@chromium.org>
2020-11-16 05:50:33 +0000

    Web Share: Add log messages to explain errors
    When navigator.share() fails with NotAllowedError, developers
    may find log messages useful to understand the failures.
    
    Change-Id: Ife338df5c8e97cc60de98ef4480d45ba8cdfa229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533215
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827695}

--- chrome/browser/webshare/share_service_impl.cc
+++ chrome/browser/webshare/share_service_impl.cc
@@ -140,11 +140,13 @@ void ShareServiceImpl::Share(const std::string& title,
   content::WebContents* const web_contents =
       content::WebContents::FromRenderFrameHost(render_frame_host_);
   if (!web_contents) {
+    VLOG(1) << "Cannot share after navigating away";
     std::move(callback).Run(blink::mojom::ShareError::PERMISSION_DENIED);
     return;
   }
 
   if (files.size() > kMaxSharedFileCount) {
+    VLOG(1) << "Share too large: " << files.size() << " files";
     std::move(callback).Run(blink::mojom::ShareError::PERMISSION_DENIED);
     return;
   }
@@ -157,6 +159,8 @@ void ShareServiceImpl::Share(const std::string& title,
 
     if (IsDangerousFilename(file->name) ||
         IsDangerousMimeType(file->blob->content_type)) {
+      VLOG(1) << "File type is not supported: " << file->name
+              << " has mime type " << file->blob->content_type;
       std::move(callback).Run(blink::mojom::ShareError::PERMISSION_DENIED);
       return;
     }
