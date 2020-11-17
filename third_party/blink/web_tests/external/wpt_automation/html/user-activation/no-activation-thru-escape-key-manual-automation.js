326980beb5da9ab6b9a9c19ed0d84728df9d8055
Lan Wei <lanwei@chromium.org>
2020-11-17 20:05:00 +0000

    Simulate user inputs in user-activation/no-activation-thru-escape-key.html
    Use testdriver Action API to simulate mouse actions in
    html/user-activation/no-activation-thru-escape-key.html.
    
    Bug: 1145677
    Change-Id: Id37bbe39be94b096939fe81d0c8053306accecb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537132
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828330}

--- third_party/blink/web_tests/external/wpt_automation/html/user-activation/no-activation-thru-escape-key-manual-automation.js
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-importAutomationScript('/input-events/inputevent_common_input.js');
-
-function inject_input() {
-  return keyDown("Escape");
-}
--- third_party/blink/web_tests/resources/testdriver-vendor.js
+++ third_party/blink/web_tests/resources/testdriver-vendor.js
@@ -116,6 +116,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             eventSenderKeys = "ArrowRight";
           } else if (charCode == 0xE015) {
             eventSenderKeys = "ArrowDown";
+          } else if (charCode == 0xE00C) {
+            eventSenderKeys = "Escape";
           } else if (charCode >= 0xE000 && charCode <= 0xF8FF) {
             reject(new Error("No support for this code: U+" + charCode.toString(16)));
             return;
