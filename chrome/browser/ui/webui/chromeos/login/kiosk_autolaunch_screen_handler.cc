15bc5e4fc2217332fccb5c262f350d5d5636306f
Roman Sorokin <rsorokin@chromium.org>
2020-11-17 13:33:52 +0000

    cros kiosk: Remove autolaunch notifications
    This CL eliminates NOTIFICATION_KIOSK_AUTOLAUNCH_WARNING_VISIBLE and
    NOTIFICATION_KIOSK_AUTOLAUNCH_WARNING_COMPLETED notifications.
    These were used for the tests only. Replaced it with javascript waiter.
    
    Bug: none
    Change-Id: Iea1f187d43fc708c2c23e02e4ebfe37d8a38b329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2544138
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828179}

--- chrome/browser/ui/webui/chromeos/login/kiosk_autolaunch_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/kiosk_autolaunch_screen_handler.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_manager.h"
 #include "chrome/browser/chromeos/login/oobe_screen.h"
 #include "chrome/browser/chromeos/login/screens/kiosk_autolaunch_screen.h"
@@ -22,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/login/localized_values_builder.h"
 #include "components/prefs/pref_service.h"
 #include "components/strings/grit/components_strings.h"
-#include "content/public/browser/notification_details.h"
-#include "content/public/browser/notification_service.h"
 #include "ui/base/webui/web_ui_util.h"
 
 namespace chromeos {
@@ -116,11 +113,6 @@ void KioskAutolaunchScreenHandler::HandleOnCancel() {
   KioskAppManager::Get()->SetEnableAutoLaunch(false);
   if (delegate_)
     delegate_->OnExit(false);
-
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_KIOSK_AUTOLAUNCH_WARNING_COMPLETED,
-      content::NotificationService::AllSources(),
-      content::NotificationService::NoDetails());
 }
 
 void KioskAutolaunchScreenHandler::HandleOnConfirm() {
@@ -128,11 +120,6 @@ void KioskAutolaunchScreenHandler::HandleOnConfirm() {
   KioskAppManager::Get()->SetEnableAutoLaunch(true);
   if (delegate_)
     delegate_->OnExit(true);
-
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_KIOSK_AUTOLAUNCH_WARNING_COMPLETED,
-      content::NotificationService::AllSources(),
-      content::NotificationService::NoDetails());
 }
 
 void KioskAutolaunchScreenHandler::HandleOnVisible() {
@@ -141,10 +128,6 @@ void KioskAutolaunchScreenHandler::HandleOnVisible() {
 
   is_visible_ = true;
   UpdateKioskApp();
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_KIOSK_AUTOLAUNCH_WARNING_VISIBLE,
-      content::NotificationService::AllSources(),
-      content::NotificationService::NoDetails());
 }
 
 void KioskAutolaunchScreenHandler::OnKioskAppsSettingsChanged() {
