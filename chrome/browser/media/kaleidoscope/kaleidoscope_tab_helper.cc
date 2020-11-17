3185eebc26ac7cc72a8774f2ad4af3002dc3330e
Becca Hughes <beccahughes@chromium.org>
2020-11-17 20:48:16 +0000

    Add browser test for KaleidoscopeTabHelper
    Change-Id: I64be99576fe6557dd9adc7c987c3c113254be6fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2544076
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828354}

--- chrome/browser/media/kaleidoscope/kaleidoscope_tab_helper.cc
+++ chrome/browser/media/kaleidoscope/kaleidoscope_tab_helper.cc
@@ -28,16 +28,6 @@ const url::Origin& KaleidoscopeUntrustedOrigin() {
   return *origin;
 }
 
-const char kKaleidoscopeNavigationHistogramName[] =
-    "Media.Kaleidoscope.Navigation";
-
-// These values are persisted to logs. Entries should not be renumbered and
-// numeric values should never be reused.
-enum class KaleidoscopeNavigation {
-  kNormal = 0,
-  kMaxValue = kNormal,
-};
-
 bool IsOpenedFromKaleidoscope(content::NavigationHandle* handle) {
   return (handle->GetInitiatorOrigin() &&
           handle->GetInitiatorOrigin()->IsSameOriginWith(
@@ -61,6 +51,9 @@ bool ShouldAllowAutoplay(content::NavigationHandle* handle) {
 
 }  // namespace
 
+const char KaleidoscopeTabHelper::kKaleidoscopeNavigationHistogramName[] =
+    "Media.Kaleidoscope.Navigation";
+
 KaleidoscopeTabHelper::KaleidoscopeTabHelper(content::WebContents* web_contents)
     : content::WebContentsObserver(web_contents) {}
 
@@ -71,15 +64,12 @@ void KaleidoscopeTabHelper::ReadyToCommitNavigation(
   if (handle->IsSameDocument() || handle->IsErrorPage())
     return;
 
-  if (!ShouldAllowAutoplay(handle))
-    return;
+  RecordMetricsOnNavigation(handle);
+  SetAutoplayOnNavigation(handle);
+}
 
-  mojo::AssociatedRemote<blink::mojom::AutoplayConfigurationClient> client;
-  handle->GetRenderFrameHost()->GetRemoteAssociatedInterfaces()->GetInterface(
-      &client);
-  client->AddAutoplayFlags(url::Origin::Create(handle->GetURL()),
-                           blink::mojom::kAutoplayFlagUserException);
-
+void KaleidoscopeTabHelper::RecordMetricsOnNavigation(
+    content::NavigationHandle* handle) {
   // Only record metrics if this page was opened by Kaleidoscope.
   if (IsOpenedFromKaleidoscope(handle)) {
     base::UmaHistogramEnumeration(kKaleidoscopeNavigationHistogramName,
@@ -94,6 +84,18 @@ void KaleidoscopeTabHelper::ReadyToCommitNavigation(
         .SetWasFromKaleidoscope(true)
         .Record(ukm_recorder);
   }
+}
+
+void KaleidoscopeTabHelper::SetAutoplayOnNavigation(
+    content::NavigationHandle* handle) {
+  if (!ShouldAllowAutoplay(handle))
+    return;
+
+  mojo::AssociatedRemote<blink::mojom::AutoplayConfigurationClient> client;
+  handle->GetRenderFrameHost()->GetRemoteAssociatedInterfaces()->GetInterface(
+      &client);
+  client->AddAutoplayFlags(url::Origin::Create(handle->GetURL()),
+                           blink::mojom::kAutoplayFlagUserException);
 }
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(KaleidoscopeTabHelper)
