ea18fca7b73d4d29e158106c0edebf2c9bc58e4a
Ben Joyce <bjoyce@google.com>
2020-11-17 17:23:34 +0000

    Batch mojo binding tests.
    Batch tests to speed up execution. See go/test-batching.
    
    Bug: 1149694
    Change-Id: I953d198712ad60ba338344d446a0e4e99a5d9a53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2542825
    Commit-Queue: Ken Rockot <rockot@google.com>
    Auto-Submit: benjamin joyce <bjoyce@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#828244}

--- mojo/public/java/system/javatests/src/org/chromium/mojo/bindings/BindingsHelperTest.java
+++ mojo/public/java/system/javatests/src/org/chromium/mojo/bindings/BindingsHelperTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.Batch;
 
 import java.nio.charset.Charset;
 
@@ -19,6 +20,7 @@ import java.nio.charset.Charset;
  * Testing {@link BindingsHelper}.
  */
 @RunWith(BaseJUnit4ClassRunner.class)
+@Batch(Batch.UNIT_TESTS)
 public class BindingsHelperTest {
     /**
      * Testing {@link BindingsHelper#utf8StringSizeInBytes(String)}.
