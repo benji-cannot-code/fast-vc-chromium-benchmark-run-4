16a8a2ffe70154e73a4a4a3a5ff2760f930e1d38
Illia Klimov <elklm@google.com>
2020-11-17 13:26:19 +0000

    Revert "Display discarded extension patterns in settings"
    This reverts commit dfd6e35e83b79064615b75605ebe1829ac1b5ecd.
    
    Reason for revert: As part of the Flash deprecation, this logic is no longer needed.
    
    Original change's description:
    > Display discarded extension patterns in settings
    >
    > Currently we throw away invalid wildcard patterns set using extension apis for Flash.
    > After this CL instead of throwing them away, we display them in the site settings page.
    >
    > Bug: 1102380, 1073883
    > Change-Id: Ic9b1d40157cecca3db9673ee92cf208423c71bc2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2282742
    > Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    > Reviewed-by: Balazs Engedy <engedy@chromium.org>
    > Commit-Queue: Ravjit Singh Uppal <ravjit@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#785383}
    
    TBR=finnur@chromium.org,engedy@chromium.org,ravjit@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1149353
    Change-Id: I4761ee6323388ad233817b3fb828028e8aefeb4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537643
    Reviewed-by: Illia Klimov <elklm@google.com>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Illia Klimov <elklm@google.com>
    Cr-Commit-Position: refs/heads/master@{#828166}

--- chrome/browser/extensions/api/content_settings/content_settings_custom_extension_provider.h
+++ chrome/browser/extensions/api/content_settings/content_settings_custom_extension_provider.h
@@ -30,10 +30,6 @@ class CustomExtensionProvider : public ObservableProvider,
       ContentSettingsType content_type,
       bool incognito) const override;
 
-  std::unique_ptr<RuleIterator> GetDiscardedRuleIterator(
-      ContentSettingsType content_type,
-      bool incognito) const override;
-
   bool SetWebsiteSetting(
       const ContentSettingsPattern& primary_pattern,
       const ContentSettingsPattern& secondary_pattern,
