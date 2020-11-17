5553951d6d442ff7b4d9e5bb87900bf954ee81d1
Matt Menard <mattme@google.com>
2020-11-17 00:17:10 +0000

    Create PrintServersManager
    Refactor print server components from CupsPrintersManager into its own
    class.
    
    Bug: b:168650771
    Change-Id: If62b27ffde97257e9e0ac5b745ceecdd13721fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521226
    Commit-Queue: Matt Menard <mattme@google.com>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#827992}

--- chrome/browser/chromeos/printing/cups_printers_manager.h
+++ chrome/browser/chromeos/printing/cups_printers_manager.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "base/memory/ref_counted.h"
-#include "chrome/browser/chromeos/printing/print_servers_policy_provider.h"
+#include "chrome/browser/chromeos/printing/print_servers_manager.h"
 #include "chrome/browser/chromeos/printing/printer_installation_manager.h"
 #include "chromeos/printing/printer_configuration.h"
 #include "chromeos/printing/uri.h"
@@ -32,7 +32,6 @@ class PpdProvider;
 class PrinterConfigurer;
 class PrinterDetector;
 class PrinterEventTracker;
-class ServerPrintersProvider;
 class SyncedPrintersManager;
 class UsbPrinterNotificationController;
 
@@ -74,8 +73,7 @@ class CupsPrintersManager : public PrinterInstallationManager,
       std::unique_ptr<PrinterConfigurer> printer_configurer,
       std::unique_ptr<UsbPrinterNotificationController>
           usb_notification_controller,
-      std::unique_ptr<ServerPrintersProvider> server_printers_provider,
-      std::unique_ptr<PrintServersPolicyProvider> print_servers_provider,
+      std::unique_ptr<PrintServersManager> print_servers_manager,
       std::unique_ptr<EnterprisePrintersProvider> enterprise_printers_provider,
       PrinterEventTracker* event_tracker,
       PrefService* pref_service);
@@ -132,14 +130,6 @@ class CupsPrintersManager : public PrinterInstallationManager,
   // Records the total number of detected network printers and the
   // number of detected network printers that have not been saved.
   virtual void RecordNearbyNetworkPrinterCounts() const = 0;
-
-  // Selects a print server from all the available print servers. Returns true on
-  // successfully selecting the requested print server.
-  virtual bool ChoosePrintServer(
-      const base::Optional<std::string>& selected_print_server_id) = 0;
-
-  // Returns the current fetching mode strategy for print servers.
-  virtual ServerPrintersFetchingMode GetServerPrintersFetchingMode() const = 0;
 };
 
 }  // namespace chromeos
