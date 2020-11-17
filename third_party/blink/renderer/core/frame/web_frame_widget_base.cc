bee001f9a1affd3d4605f29c9f39e3b2ae49a986
Fergal Daly <fergal@chromium.org>
2020-11-17 00:39:33 +0000

    Add debugging to WebViewImpl so that we know why the main frame is null.
    This is some ugly but temporary debugging to log extra information
    when we find the the view has no main frame but the local root does.
    
    Bug: 1139104
    Change-Id: I791fc7572a4a5a5abf3c7c689c4029e1696bbd48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532535
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828003}

--- third_party/blink/renderer/core/frame/web_frame_widget_base.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_base.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/time/time.h"
@@ -1412,14 +1414,19 @@ void WebFrameWidgetBase::BeginCommitCompositorFrame() {
 void WebFrameWidgetBase::EndCommitCompositorFrame(
     base::TimeTicks commit_start_time) {
   DCHECK(commit_compositor_frame_start_time_.has_value());
-  CHECK(LocalRootImpl());
-  CHECK(LocalRootImpl()->GetFrame());
-  CHECK(LocalRootImpl()->GetFrame()->View());
-
   if (ForMainFrame()) {
     View()->Client()->DidCommitCompositorFrameForLocalMainFrame(
         commit_start_time);
     View()->UpdatePreferredSize();
+    if (!View()->MainFrameImpl()) {
+      // Trying to track down why the view's idea of the main frame varies
+      // from LocalRootImpl's.
+      // TODO(https://crbug.com/1139104): Remove this.
+      std::string reason = View()->GetNullFrameReasonForBug1139104();
+      DCHECK(false) << reason;
+      SCOPED_CRASH_KEY_STRING32(Crbug1139104, NullFrameReason, reason);
+      base::debug::DumpWithoutCrashing();
+    }
   }
 
   LocalRootImpl()
--- third_party/blink/renderer/core/frame/web_view_frame_widget.cc
+++ third_party/blink/renderer/core/frame/web_view_frame_widget.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/frame/web_view_frame_widget.h"
 
+#include "base/debug/crash_logging.h"
 #include "build/build_config.h"
 #include "third_party/blink/public/common/input/web_keyboard_event.h"
 #include "third_party/blink/public/platform/platform.h"
