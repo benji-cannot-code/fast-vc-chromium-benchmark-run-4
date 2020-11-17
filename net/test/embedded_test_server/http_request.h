c085140418a7b4ae5ec6033da80fb26b38d70c0e
Maksim Ivanov <emaxx@chromium.org>
2020-11-17 23:24:28 +0000

    Add SSLInfo into test_server::HttpRequest
    Extend the embedded_test_server's HttpRequest struct to additionally
    contain the SSLInfo structure when it's available.
    
    This allows to inspect the SSL state, like the provided client
    certificate, in net::EmbeddedTestServer's request handlers.
    
    Bug: 792204
    Change-Id: Ia24acf0c38fadee04c341d3c9192d98272bf5ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545163
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828465}

--- net/test/embedded_test_server/http_request.h
+++ net/test/embedded_test_server/http_request.h
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "base/optional.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
+#include "net/ssl/ssl_info.h"
 #include "url/gurl.h"
 
 namespace net {
@@ -64,6 +66,7 @@ struct HttpRequest {
   HeaderMap headers;
   std::string content;
   bool has_content;
+  base::Optional<SSLInfo> ssl_info;
 };
 
 // Parses the input data and produces a valid HttpRequest object. If there is
