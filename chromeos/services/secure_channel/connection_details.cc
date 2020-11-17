239a3b586a22beb4788b9842d7e8d9346bfa33db
Kyle Horimoto <khorimoto@chromium.org>
2020-11-17 23:02:26 +0000

    [CrOS PhoneHub] Correct ConnectionDetails::operator<()
    Consider ConnectionDetails::connection_medium_ when comparing
    ConnectionMedium objects. This fixes an issue in which two
    ConnectionDetails objects with the same device ID but different mediums
    were compared incorrectly, causing an issue where connection attempts to
    the same device with different mediums ended up using the same
    underlying connection instead of two separate connections.
    
    Bug: 1150070
    Change-Id: I9bb2802676aebf4a0df9a430cc33bff683dcd14e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545664
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: James Vecore <vecore@google.com>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#828456}

--- chromeos/services/secure_channel/connection_details.cc
+++ chromeos/services/secure_channel/connection_details.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/services/secure_channel/connection_details.h"
 
+#include <tuple>
+
 #include "base/check.h"
 #include "chromeos/components/multidevice/remote_device_ref.h"
 
@@ -30,12 +32,8 @@ bool ConnectionDetails::operator!=(const ConnectionDetails& other) const {
 }
 
 bool ConnectionDetails::operator<(const ConnectionDetails& other) const {
-  if (device_id() != other.device_id())
-    return device_id() < other.device_id();
-
-  // Currently, there is only one ConnectionMedium type.
-  DCHECK(connection_medium() == other.connection_medium());
-  return false;
+  return std::tie(device_id_, connection_medium_) <
+         std::tie(other.device_id_, other.connection_medium_);
 }
 
 std::ostream& operator<<(std::ostream& stream,
