0417618630dab0d69ea9efdc4c59ff3173b42612
Roman Aleksandrov <raleksandrov@google.com>
2020-11-17 14:40:49 +0000

    oobe: Serve oobe_dialog as separate resources.
    Bug: 1144159
    Change-Id: I02c5ba9fa2d67755025f364807e4c0e03bba312f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537839
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828194}

--- chrome/browser/resources/chromeos/login/structure/components_common.js
+++ chrome/browser/resources/chromeos/login/structure/components_common.js
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="../components/login_screen_behavior.js">
 // <include src="../components/multi_step_behavior.js">
 // <include src="../components/oobe_buttons.js">
-// <include src="../components/oobe_dialog.js">
 // <include src="../components/oobe_help_dialog.js">
 // <include src="../components/html-echo.js">
 // <include src="../notification_card.js">
