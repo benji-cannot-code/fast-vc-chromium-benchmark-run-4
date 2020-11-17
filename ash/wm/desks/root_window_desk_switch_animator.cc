cc27aaf400b7c195243d55d5c7fd5782f64ed52b
Sammie Quon <sammiequon@chromium.org>
2020-11-17 02:13:39 +0000

    desks: Speculative fix for crash.
    The previous attempt [1] added checks for whether the starting or ending
    screenshot was taken. In the case of the starting screenshot missing, it
    would signal the animation to remove some RootWindowDeskSwitchAnimators,
    leading to a vector out of bounds.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2506356
    
    Bug: 1148607
    Test: added regression test
    Change-Id: I7f14df877cb93619a0ca8a85f5d0ad434dda3ad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2536934
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828034}

--- ash/wm/desks/root_window_desk_switch_animator.cc
+++ ash/wm/desks/root_window_desk_switch_animator.cc
@@ -76,6 +76,8 @@ void TakeScreenshot(
       viz::CopyOutputRequest::ResultFormat::RGBA_TEXTURE,
       std::move(on_screenshot_taken));
   screenshot_request->set_area(request_bounds);
+  screenshot_request->set_result_task_runner(
+      base::SequencedTaskRunnerHandle::Get());
   screenshot_layer->RequestCopyOfOutput(std::move(screenshot_request));
 }
 
@@ -326,7 +328,7 @@ bool RootWindowDeskSwitchAnimator::UpdateSwipeAnimation(float scroll_delta_x) {
   return true;
 }
 
-void RootWindowDeskSwitchAnimator::EndSwipeAnimation() {
+int RootWindowDeskSwitchAnimator::EndSwipeAnimation() {
   // If the starting screenshot has not finished, just let our delegate know
   // that the desk animation is finished (and |this| will soon be deleted), and
   // go back to the starting desk.
@@ -334,7 +336,7 @@ void RootWindowDeskSwitchAnimator::EndSwipeAnimation() {
     animation_finished_ = true;
     ending_desk_index_ = starting_desk_index_;
     delegate_->OnDeskSwitchAnimationFinished();
-    return;
+    return ending_desk_index_;
   }
 
   // If the ending desk screenshot has not finished, |visible_desk_index_| will
@@ -345,6 +347,7 @@ void RootWindowDeskSwitchAnimator::EndSwipeAnimation() {
 
   ending_desk_index_ = visible_desk_index_;
   StartAnimation();
+  return ending_desk_index_;
 }
 
 void RootWindowDeskSwitchAnimator::OnImplicitAnimationsCompleted() {
