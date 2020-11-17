3e16858a8e69e7759b08ab318671e229c3af14d6
Nate Chapin <japhet@chromium.org>
2020-11-17 19:14:22 +0000

    Merge NavigatorBattery and BatteryManager
    BatteryManager can adequately be its own Supplement<Navigator>,
    reducing boilerplate.
    
    Bug: 1147612
    Change-Id: I15cfaa7b2778f34a1bc754a8ca54a5361b075ddd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2523362
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828299}

--- third_party/blink/renderer/modules/battery/battery_manager.h
+++ third_party/blink/renderer/modules/battery/battery_manager.h
@@ -14,19 +14,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/battery/battery_status.h"
 #include "third_party/blink/renderer/modules/event_target_modules.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 
 namespace blink {
 
+class Navigator;
+
 class BatteryManager final : public EventTargetWithInlineData,
                              public ActiveScriptWrappable<BatteryManager>,
+                             public Supplement<Navigator>,
                              public ExecutionContextLifecycleStateObserver,
                              public PlatformEventController {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  static BatteryManager* Create(ExecutionContext*);
-
-  explicit BatteryManager(ExecutionContext*);
+  static const char kSupplementName[];
+  static ScriptPromise getBattery(ScriptState*, Navigator&);
+  explicit BatteryManager(Navigator&);
   ~BatteryManager() override;
 
   // Returns a promise object that will be resolved with this BatteryManager.
