bbfdefa702b67776fa9dab6f960e2eb45cfd3666
Xing Liu <xingliu@chromium.org>
2020-11-17 22:14:12 +0000

    Read later: Implement bookmark bottom sheet view and click event.
    This CL implements more details for bookmark bottom sheet.
    
    The contents includes:
    
    1. Load bookmark top level data to bottom sheet.
    2. Implement the click event.
    3. Implement the view holder. Reuse BookmarkRow but plumb data
    through MVC view binder, might be good for future development.
    4. A few integration tests are added to BookmarkBottomSheetTest.
    
    doc: go/chrome-bottomsheet-mvc
    
    Bug: 1133496
    Change-Id: Id5ce9abb44351c3a6a350531e1622f08c725a1f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537785
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828424}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/bottomsheet/BookmarkBottomSheetRowViewBinder.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.bookmarks.bottomsheet;
+
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/**
+ * The view binder to connect each bookmark bottom sheet row to {@link PropertyModel}.
+ */
+class BookmarkBottomSheetRowViewBinder {
+    static void bind(
+            PropertyModel model, BookmarkBottomSheetFolderRow view, PropertyKey propertyKey) {
+        if (BookmarkBottomSheetItemProperties.TITLE.equals(propertyKey)) {
+            view.setTitle(model.get(BookmarkBottomSheetItemProperties.TITLE));
+        } else if (BookmarkBottomSheetItemProperties.SUBTITLE.equals(propertyKey)) {
+            view.setSubtitle(model.get(BookmarkBottomSheetItemProperties.SUBTITLE));
+        } else if (BookmarkBottomSheetItemProperties.ON_CLICK_LISTENER.equals(propertyKey)) {
+            view.setOnClickListener(model.get(BookmarkBottomSheetItemProperties.ON_CLICK_LISTENER));
+        }
+    }
+
+    private BookmarkBottomSheetRowViewBinder() {}
+}
