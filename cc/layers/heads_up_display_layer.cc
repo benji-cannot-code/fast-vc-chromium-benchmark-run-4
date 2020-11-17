242655a892b7a12d9622ed228447b68d91eb0e04
Marijn Kruisselbrink <mek@chromium.org>
2020-11-17 20:10:46 +0000

    Revert "cc: Set up a Flag for Displaying Performance Metrics"
    This reverts commit 269f716102142b6e73fa85b4a818432cac79855c.
    
    Reason for revert: Tests are failing on MSAN
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20MSan%20Tests/26262
    
    Original change's description:
    > cc: Set up a Flag for Displaying Performance Metrics
    >
    > Set up a feature flag and corresponding state inside cc to enable
    > using HUD for displaying performance metrics. Currently when we enable
    > the show performance metrics flag we only add WebVitals metrics, which
    > comes from Blink. We might want to add other metrics in the future.
    >
    > R=​sadrul
    >
    > Bug: 1149385
    > Change-Id: I437e113a5fc4f44c6fbaa542c92d1f2da6cbb393
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2540570
    > Commit-Queue: weiliangc <weiliangc@chromium.org>
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#828221}
    
    TBR=sadrul@chromium.org,bokan@chromium.org,weiliangc@chromium.org
    
    Change-Id: Ie5b290290078f669f487f8792020fe4e822798fe
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1149385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545250
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828337}

--- cc/layers/heads_up_display_layer.cc
+++ cc/layers/heads_up_display_layer.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/layers/heads_up_display_layer.h"
 
 #include <algorithm>
-#include <vector>
 
 #include "base/trace_event/trace_event.h"
 #include "cc/layers/heads_up_display_layer_impl.h"
@@ -39,7 +38,7 @@ void HeadsUpDisplayLayer::UpdateLocationAndSize(
 
   gfx::Size bounds;
 
-  if (layer_tree_host()->GetDebugState().ShowDebugRects()) {
+  if (layer_tree_host()->GetDebugState().ShowHudRects()) {
     bounds = device_viewport_in_layout_pixels;
   } else {
     // If the HUD is not displaying full-viewport rects (e.g., it is showing the
