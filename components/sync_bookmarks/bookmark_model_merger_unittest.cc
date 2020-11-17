434fa3ca0e98b9b2597e19041181b7617cf77464
Daniel Hosseinian <dhoss@chromium.org>
2020-11-17 20:08:05 +0000

    [bookmarks] Adopt base::GUID for BookmarkModel::AddFolder()
    Pass in a base::GUID in lieu of a std::string GUID.
    
    Bug: 1026195
    Change-Id: Ia7b6395c044e527c8a064cea973bc8bd0b4452e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527853
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828335}

--- components/sync_bookmarks/bookmark_model_merger_unittest.cc
+++ components/sync_bookmarks/bookmark_model_merger_unittest.cc
@@ -891,7 +891,7 @@ TEST(BookmarkModelMergerTest, ShouldMergeBookmarkByGUIDAndReparent) {
       bookmark_model->bookmark_bar_node();
   const bookmarks::BookmarkNode* folder = bookmark_model->AddFolder(
       /*parent=*/bookmark_bar_node, /*index=*/0,
-      base::UTF8ToUTF16("Folder Title"), nullptr, base::GenerateGUID());
+      base::UTF8ToUTF16("Folder Title"));
   const bookmarks::BookmarkNode* bookmark = bookmark_model->AddURL(
       /*parent=*/folder, /*index=*/0, base::UTF8ToUTF16(kLocalTitle),
       GURL(kUrl), nullptr, base::Time::Now(), kGuid.AsLowercaseString());
@@ -951,10 +951,10 @@ TEST(BookmarkModelMergerTest, ShouldMergeFolderByGUIDAndNotSemantics) {
       bookmark_model->bookmark_bar_node();
   const bookmarks::BookmarkNode* folder1 = bookmark_model->AddFolder(
       /*parent=*/bookmark_bar_node, /*index=*/0, base::UTF8ToUTF16(kTitle1),
-      nullptr, kGuid1.AsLowercaseString());
+      /*meta_info=*/nullptr, kGuid1);
   const bookmarks::BookmarkNode* folder2 = bookmark_model->AddFolder(
-      /*parent=*/folder1, /*index=*/0, base::UTF8ToUTF16(kTitle2), nullptr,
-      kGuid2.AsLowercaseString());
+      /*parent=*/folder1, /*index=*/0, base::UTF8ToUTF16(kTitle2),
+      /*meta_info=*/nullptr, kGuid2);
   ASSERT_TRUE(folder1);
   ASSERT_TRUE(folder2);
   ASSERT_THAT(bookmark_bar_node->children(), ElementRawPointersAre(folder1));
@@ -1076,7 +1076,7 @@ TEST(
       bookmark_model->bookmark_bar_node();
   const bookmarks::BookmarkNode* folder = bookmark_model->AddFolder(
       /*parent=*/bookmark_bar_node, /*index=*/0,
-      base::UTF8ToUTF16(kOriginalTitle), nullptr, kGuid1.AsLowercaseString());
+      base::UTF8ToUTF16(kOriginalTitle), /*meta_info=*/nullptr, kGuid1);
   const bookmarks::BookmarkNode* bookmark = bookmark_model->AddURL(
       /*parent=*/folder, /*index=*/0, base::UTF8ToUTF16("Bookmark Title"),
       GURL("http://foo.com/"), nullptr, base::Time::Now(),
@@ -1159,7 +1159,7 @@ TEST(BookmarkModelMergerTest,
       bookmark_model->bookmark_bar_node();
   const bookmarks::BookmarkNode* folder = bookmark_model->AddFolder(
       /*parent=*/bookmark_bar_node, /*index=*/0,
-      base::UTF8ToUTF16(kOriginalTitle), nullptr, kGuid1.AsLowercaseString());
+      base::UTF8ToUTF16(kOriginalTitle), /*meta_info=*/nullptr, kGuid1);
   const bookmarks::BookmarkNode* bookmark = bookmark_model->AddURL(
       /*parent=*/folder, /*index=*/0, base::UTF8ToUTF16("Bookmark Title"),
       GURL("http://foo.com/"), nullptr, base::Time::Now(),
@@ -1334,7 +1334,7 @@ TEST(BookmarkModelMergerTest,
       bookmark_model->bookmark_bar_node();
   const bookmarks::BookmarkNode* folder = bookmark_model->AddFolder(
       /*parent=*/bookmark_bar_node, /*index=*/0,
-      base::UTF8ToUTF16("Folder Title"), nullptr, kGuid1.AsLowercaseString());
+      base::UTF8ToUTF16("Folder Title"), /*meta_info=*/nullptr, kGuid1);
   const bookmarks::BookmarkNode* bookmark = bookmark_model->AddURL(
       /*parent=*/folder, /*index=*/0, base::UTF8ToUTF16("Foo's title"),
       GURL("http://foo.com"), nullptr, base::Time::Now(),
