e546e2164538276cf6bf1be32d335bd0ad0c4602
Andrew Xu <andrewxu@chromium.org>
2020-11-17 21:23:39 +0000

    [Multipaste] Show delete button after long gesture press
    This CL implements the feature that the delete button shows after its
    host item view is under gesture press for enough long time.
    
    Bug: 1147584
    Change-Id: I27ba1dc11939dd47712aed7a0dd08cb9a1ea2b99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532909
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828383}

--- ash/clipboard/clipboard_history_util.h
+++ ash/clipboard/clipboard_history_util.h
@@ -42,7 +42,10 @@ enum class Action {
   kPaste,
 
   // Deletes the activated item.
-  kDelete
+  kDelete,
+
+  // Selects the activated item.
+  kSelect
 };
 
 // Used in histograms, each value corresponds with an underlying format
