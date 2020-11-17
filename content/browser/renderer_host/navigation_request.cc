f10227c7f56a9421a4253f3a293e345192e60a60
Fergal Daly <fergal@chromium.org>
2020-11-17 11:31:03 +0000

    Validate speculative render frame host from multiple places.
    Move validation logic into ValidateSpeculativeFrameHostForBug1146573.
    
    Also fixes the problem of dumping too often by not dumping when
    replacing a crashed RFH.
    
    Bug: 1146573
    Change-Id: Ifcae2662ca7fa071440a5e1d069936683f763269
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538863
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828146}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -3204,6 +3204,8 @@ void NavigationRequest::AddOldPageInfoToCommitParamsIfNeeded() {
 }
 
 void NavigationRequest::CommitNavigation() {
+  frame_tree_node_->render_manager()
+      ->ValidateSpeculativeRenderFrameHostForBug1146573();
   UpdateCommitNavigationParamsHistory();
   DCHECK(NeedsUrlLoader() == !!response_head_ ||
          (was_redirected_ && common_params_->url.IsAboutBlank()));
