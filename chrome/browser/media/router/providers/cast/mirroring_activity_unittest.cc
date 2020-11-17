0fb5fc11901a211e092f3e992ba9b133c9242587
Muyao Xu <muyaoxu@google.com>
2020-11-17 22:35:09 +0000

    [Media Router]Add native logs in MirroringActivity
    This CL uses the native logger to log WebRTC messages between the
    mirroring service and mirroring receiver.
    AES related data will be scrubbed before sent to the logger.
    
    Bug: b/170651376, 1117673
    Change-Id: I4e72edc715d8f56b6ac7f3c3353dae1d119e8d10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533118
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828439}

--- chrome/browser/media/router/providers/cast/mirroring_activity_unittest.cc
+++ chrome/browser/media/router/providers/cast/mirroring_activity_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/media/router/providers/cast/mirroring_activity.h"
 
+#include "base/json/json_reader.h"
+#include "base/json/json_writer.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
@@ -328,6 +330,47 @@ TEST_F(MirroringActivityTest, OnInternalMessage) {
   activity_->OnInternalMessage(cast_channel::InternalMessage(
       cast_channel::CastMessageType::kPing, kNamespace,
       base::test::ParseJson(kPayload)));
+}
+
+TEST_F(MirroringActivityTest, GetScrubbedLogMessage) {
+  static constexpr char message[] = R"(
+    {
+      "offer": {
+        "supportedStreams": [
+          {
+            "aesIvMask": "Mask_A",
+            "aesKey": "Key_A"
+          },
+          {
+            "aesIvMask": "Mask_B",
+            "aesKey": "Key_B"
+          }
+        ]
+      },
+      "type": "OFFER"
+    })";
+  static constexpr char scrubbed_message[] = R"(
+    {
+      "offer": {
+        "supportedStreams": [
+          {
+            "aesIvMask": "AES_IV_MASK",
+            "aesKey": "AES_KEY"
+          },
+          {
+            "aesIvMask": "AES_IV_MASK",
+            "aesKey": "AES_KEY"
+          }
+        ]
+      },
+      "type": "OFFER"
+    })";
+
+  base::Optional<base::Value> message_json = base::JSONReader::Read(message);
+  EXPECT_TRUE(message_json);
+  EXPECT_THAT(scrubbed_message,
+              base::test::IsJson(MirroringActivity::GetScrubbedLogMessage(
+                  message_json.value())));
 }
 
 }  // namespace media_router
