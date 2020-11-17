8f8048faca49661928318e3f7d9f1e34d5cc1801
Jose Dapena Paz <jdapena@igalia.com>
2020-11-17 11:35:35 +0000

    IWYU: missing <memory> include for using unique_ptr in xlib_support.h
    Fix build error with libstdc++:
    In file included from ../../ui/gfx/x/xlib_support.cc:5:
    ../../ui/gfx/x/xlib_support.h:41:8: error: ‘unique_ptr’ in namespace ‘std’ does not name a template type
       41 |   std::unique_ptr<XlibLoader> xlib_loader_;
          |        ^~~~~~~~~~
    
    Bug: 957519
    Change-Id: Ibd752625f76d24749d8958c5e4d9082ecf24630a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2534874
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#828155}

--- ui/gfx/x/xlib_support.h
+++ ui/gfx/x/xlib_support.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_X_XLIB_SUPPORT_H_
 #define UI_GFX_X_XLIB_SUPPORT_H_
 
+#include <memory>
 #include <string>
 
 #include "base/component_export.h"
