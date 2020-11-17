c517a001813e1a3745035e3f976cc298599f8d4f
James Cook <jamescook@chromium.org>
2020-11-17 01:06:00 +0000

    lacros: Restrict downloads to MyFiles>Downloads and subdirectories
    Chrome OS hides many paths from the user. Users can only browse to
    MyFiles, certain Android paths, and a selected set of mount points.
    For security-in-depth, Chrome OS restricts download paths to those
    directories and their subdirectories.
    
    Many of the path utilities functions used to check for these
    directories have ash dependencies that cannot be included in the
    lacros build.
    
    For now, restrict lacros downloads to the default Downloads
    directory and its subdirectories. We can revisit this as part of
    the Files work planned for early next year.
    
    Bug: 1148848
    Test: expanded unit_tests coverage for lacros
    
    Change-Id: Ia7c41350dcfc29299579f9e7ecd4078e1eb00505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2541124
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828011}

--- chrome/browser/download/download_prefs_unittest.cc
+++ chrome/browser/download/download_prefs_unittest.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using safe_browsing::FileTypePolicies;
 
+namespace {
+
 TEST(DownloadPrefsTest, Prerequisites) {
   // Most of the tests below are based on the assumption that .swf files are not
   // allowed to open automatically, and that .txt files are allowed. If this
@@ -363,7 +365,7 @@ TEST(DownloadPrefsTest, DefaultPathChangedToInvalidValue) {
             download_prefs.GetDefaultDownloadDirectory());
 }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
 void ExpectValidDownloadDir(Profile* profile,
                             DownloadPrefs* prefs,
                             base::FilePath path) {
@@ -374,7 +376,7 @@ void ExpectValidDownloadDir(Profile* profile,
 }
 
 TEST(DownloadPrefsTest, DownloadDirSanitization) {
-  content::BrowserTaskEnvironment task_environment_;
+  content::BrowserTaskEnvironment task_environment;
   TestingProfile profile(base::FilePath("/home/chronos/u-0123456789abcdef"));
   DownloadPrefs prefs(&profile);
   const base::FilePath default_dir =
@@ -383,6 +385,20 @@ TEST(DownloadPrefsTest, DownloadDirSanitization) {
   // Test a valid path.
   ExpectValidDownloadDir(&profile, &prefs, default_dir.AppendASCII("testdir"));
 
+  // Test with an invalid path outside the download directory.
+  profile.GetPrefs()->SetString(prefs::kDownloadDefaultDirectory,
+                                "/home/chronos");
+  EXPECT_EQ(prefs.DownloadPath(), default_dir);
+
+  // Test with an invalid path containing parent references.
+  base::FilePath parent_reference = default_dir.AppendASCII("..");
+  profile.GetPrefs()->SetString(prefs::kDownloadDefaultDirectory,
+                                parent_reference.value());
+  EXPECT_EQ(prefs.DownloadPath(), default_dir);
+
+  // TODO(https://crbug.com/1148848): Sort out path sanitization for Lacros.
+  // Once the ash-only code can be shared the tests below can be enabled.
+#if BUILDFLAG(IS_CHROMEOS_ASH)
   // Test a valid path for Android files.
   ExpectValidDownloadDir(
       &profile, &prefs,
@@ -397,17 +413,6 @@ TEST(DownloadPrefsTest, DownloadDirSanitization) {
       &profile, &prefs,
       base::FilePath(
           "/media/fuse/crostini_0123456789abcdef_termina_penguin/testdir"));
-
-  // Test with an invalid path outside the download directory.
-  profile.GetPrefs()->SetString(prefs::kDownloadDefaultDirectory,
-                                "/home/chronos");
-  EXPECT_EQ(prefs.DownloadPath(), default_dir);
-
-  // Test with an invalid path containing parent references.
-  base::FilePath parent_reference = default_dir.AppendASCII("..");
-  profile.GetPrefs()->SetString(prefs::kDownloadDefaultDirectory,
-                                parent_reference.value());
-  EXPECT_EQ(prefs.DownloadPath(), default_dir);
 
   // DriveFS
   {
@@ -448,8 +453,9 @@ TEST(DownloadPrefsTest, DownloadDirSanitization) {
                                    "/media/fuse/drivefs-something-else/root");
     EXPECT_EQ(prefs2.DownloadPath(), default_dir2);
   }
-}
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+}
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
 
 #ifdef OS_ANDROID
 TEST(DownloadPrefsTest, DownloadLaterPrefs) {
@@ -477,3 +483,5 @@ TEST(DownloadPrefsTest, DownloadLaterPrefs) {
 }
 
 #endif  // OS_ANDROID
+
+}  // namespace
