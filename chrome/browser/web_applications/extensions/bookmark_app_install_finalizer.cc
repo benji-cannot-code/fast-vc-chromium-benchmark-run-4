3100cc878c31ea59bfe4b358abda256e7a8a9a4c
Jiewei Qian <qjw@chromium.org>
2020-11-17 23:34:33 +0000

    system-web-apps: remove expiring bookmark install finalizer histograms
    This CL removes two histograms that were used to investigate bookmark
    app (extension-based) install failures:
     - Webapp.InstallResultExtensionError.System.Profiles.Other
     - Webapp.InstallResultExtensionDisabledReason.System.Profiles.Other
    
    The relevant code isn't executed after BMO project launch (which moves
    SWA install to web app install finalizer).
    
    Also removes the relevant test code that were solely introduced for
    testing these two histograms (see https://crrev.com/c/1915651 ).
    
    Fixed: 1149526,1149527
    Change-Id: I195f531d7c7a8cf106ed00662e3231d2d2b12b08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2543531
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Jiewei Qian  <qjw@chromium.org>
    Auto-Submit: Jiewei Qian  <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828467}

--- chrome/browser/web_applications/extensions/bookmark_app_install_finalizer.cc
+++ chrome/browser/web_applications/extensions/bookmark_app_install_finalizer.cc
@@ -42,11 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
-static constexpr char kInstallResultExtensionErrorHistogramName[] =
-    "Webapp.InstallResultExtensionError.System.Profiles";
-static constexpr char kInstallResultExtensionDisabledReasonHistogramName[] =
-    "Webapp.InstallResultExtensionDisabledReason.System.Profiles";
-
 BookmarkAppInstallFinalizer::BookmarkAppInstallFinalizer(Profile* profile)
     : externally_installed_app_prefs_(profile->GetPrefs()), profile_(profile) {
   crx_installer_factory_ = base::BindRepeating([](Profile* profile) {
@@ -73,7 +68,6 @@ void BookmarkAppInstallFinalizer::FinalizeInstall(
       &BookmarkAppInstallFinalizer::OnExtensionInstalled,
       weak_ptr_factory_.GetWeakPtr(), web_app_info.start_url, launch_type,
       web_app_info.enable_experimental_tabbed_window, options.locally_installed,
-      options.install_source == WebappInstallSource::SYSTEM_DEFAULT,
       std::move(callback), crx_installer));
 
   switch (options.install_source) {
@@ -241,18 +235,10 @@ void BookmarkAppInstallFinalizer::OnExtensionInstalled(
     LaunchType launch_type,
     bool enable_experimental_tabbed_window,
     bool is_locally_installed,
-    bool is_system_app,
     InstallFinalizedCallback callback,
     scoped_refptr<CrxInstaller> crx_installer,
     const base::Optional<CrxInstallError>& error) {
   if (error) {
-    if (is_system_app) {
-      std::string extension_install_error_histogram_name =
-          std::string(kInstallResultExtensionErrorHistogramName) + "." +
-          web_app::GetProfileCategoryForLogging(profile_);
-      base::UmaHistogramEnumeration(extension_install_error_histogram_name,
-                                    error.value().detail());
-    }
     std::move(callback).Run(
         web_app::AppId(),
         web_app::InstallResultCode::kBookmarkExtensionInstallError);
@@ -267,13 +253,6 @@ void BookmarkAppInstallFinalizer::OnExtensionInstalled(
         ExtensionPrefs::Get(profile_)->GetDisableReasons(extension->id());
     LOG(ERROR) << "Installed extension was disabled: "
                << extension_disabled_reasons;
-    if (is_system_app) {
-      std::string extension_disabled_reason_histogram_name =
-          std::string(kInstallResultExtensionDisabledReasonHistogramName) +
-          "." + web_app::GetProfileCategoryForLogging(profile_);
-      base::UmaHistogramSparse(extension_disabled_reason_histogram_name,
-                               extension_disabled_reasons);
-    }
     std::move(callback).Run(web_app::AppId(),
                             web_app::InstallResultCode::kWebAppDisabled);
     return;
