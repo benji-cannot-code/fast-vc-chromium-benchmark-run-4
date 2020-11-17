371e8de325e4f9f7cd1938992a33bce34d8307d6
Luciano Pacheco <lucmult@chromium.org>
2020-11-17 21:02:38 +0000

    Audio Player: Generate JS module for //ui/file_manger/audio_player/js/audio_player.js
    Fix the export of `MediaSessionPlaybackState` to export the whole enum.
    
    Bug: 1133186
    Change-Id: I67897f77d34741e9bf343c157b839fd6f282243f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2543648
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828366}

--- ui/file_manager/base/js/mediasession_types.js
+++ ui/file_manager/base/js/mediasession_types.js
@@ -5,18 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * @fileoverview
- * @suppress {uselessCode} Temporary suppress because of the line exporting.
  */
 
 /**
  * @see https://wicg.github.io/mediasession/#enumdef-mediasessionplaybackstate
  * @enum {string}
  */
-const MediaSessionPlaybackState = {
+/* #export */ const MediaSessionPlaybackState = {
   NONE: 'none',
   PAUSED: 'paused',
   PLAYING: 'playing'
 };
-
-// eslint-disable-next-line semi,no-extra-semi
-/* #export */ {MediaSessionPlaybackState};
