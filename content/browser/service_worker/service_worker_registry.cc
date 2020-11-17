a6a23b769994fee9f999f7a0204144354ac8c778
Kenichi Ishibashi <bashi@chromium.org>
2020-11-17 02:39:40 +0000

    Remove unused argument from ServiceWorkerRegistry::DidDoomUncommittedResources
    `resource_ids` isn't used anymore. Removing it simplifies retries of
    DoomUncommittedResources().
    
    Bug: 1133143
    Change-Id: I99183060434429615b294538b8d719e3f4c49397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2542928
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828042}

--- content/browser/service_worker/service_worker_registry.cc
+++ content/browser/service_worker/service_worker_registry.cc
@@ -816,7 +816,7 @@ void ServiceWorkerRegistry::DoomUncommittedResources(
   GetRemoteStorageControl()->DoomUncommittedResources(
       resource_ids,
       base::BindOnce(&ServiceWorkerRegistry::DidDoomUncommittedResourceIds,
-                     weak_factory_.GetWeakPtr(), resource_ids));
+                     weak_factory_.GetWeakPtr()));
 }
 
 void ServiceWorkerRegistry::DidFindRegistrationForClientUrl(
@@ -1168,7 +1168,6 @@ void ServiceWorkerRegistry::DidWriteUncommittedResourceIds(
 }
 
 void ServiceWorkerRegistry::DidDoomUncommittedResourceIds(
-    const std::vector<int64_t>& resource_ids,
     storage::mojom::ServiceWorkerDatabaseStatus status) {
   if (status != storage::mojom::ServiceWorkerDatabaseStatus::kOk)
     ScheduleDeleteAndStartOver();
