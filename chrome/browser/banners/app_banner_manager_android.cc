0307911a15a78e98c30bf2dd40139e7e2db26835
Evan Stade <estade@chromium.org>
2020-11-17 18:37:41 +0000

    Increase encapsulation of AppBannerInProductHelp java code
    AppBannerInProductHelp is not slated for componentization.
    
    This moves the jni code which invokes the IPH UI from
    AppBannerManager.java, which is slated for componentization, to
    AppBannerInProductHelpControllerProvider.java, and thus removes some
    //chrome dependencies from the former class.
    
    Bug: 1147268
    Change-Id: If8eb505ec32081b6c85f2b901eac4887b2a49543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2541062
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828278}

--- chrome/browser/banners/app_banner_manager_android.cc
+++ chrome/browser/banners/app_banner_manager_android.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/webapk/webapk_web_manifest_checker.h"
 #include "chrome/browser/android/webapps/add_to_homescreen_coordinator.h"
 #include "chrome/browser/android/webapps/add_to_homescreen_params.h"
+#include "chrome/browser/banners/android/jni_headers/AppBannerInProductHelpControllerProvider_jni.h"
 #include "chrome/browser/banners/android/jni_headers/AppBannerManager_jni.h"
 #include "chrome/browser/banners/app_banner_metrics.h"
 #include "chrome/browser/banners/app_banner_settings_helper.h"
@@ -489,8 +490,8 @@ bool AppBannerManagerAndroid::MaybeShowInProductHelp() const {
 
   JNIEnv* env = base::android::AttachCurrentThread();
   std::string error_message = base::android::ConvertJavaStringToUTF8(
-      Java_AppBannerManager_showInProductHelp(
-          env, java_banner_manager_, web_contents()->GetJavaWebContents()));
+      Java_AppBannerInProductHelpControllerProvider_showInProductHelp(
+          env, web_contents()->GetJavaWebContents()));
   if (!error_message.empty()) {
     DVLOG(2) << "IPH for PWA showing aborted. " << error_message;
     return false;
