fc8bf9938c2e19c1609f06c9c276f444773d5050
Nazerke <Nazerke@google.com>
2020-11-17 10:23:59 +0000

    [ios] Adding the add-new-tab-"button" with no functionality.
    This CL adds the add-new-tab-button, which is not tappable and which is
    not moving the position according to the number of tabs.
    
    Bug: 1134132,1128249
    Change-Id: Ia72835684f8d65879c863041a503835dad2a67d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2536469
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828143}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/tab_strip_view_controller.h
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/tab_strip_view_controller.h
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @property(nonatomic, weak) id<TabFaviconDataSource> faviconDataSource;
 
+@property(nonatomic, readonly, retain) UIButton* buttonNewTab;
+
 - (instancetype)init NS_DESIGNATED_INITIALIZER;
 
 - (instancetype)initWithNibName:(NSString*)nibNameOrNil
