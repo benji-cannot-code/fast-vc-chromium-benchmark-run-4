97982ade3372080a40fc436d13fd56063d71296a
Donn Denman <donnd@chromium.org>
2020-11-17 18:18:53 +0000

    [TTS][RSearches] Remove the Demo UI for RSearches.
    Removes the demo-level UI for Related Searches.
    The demo allowed a single tap to trigger Related Searches while
    preserving longpress as a regular Touch to Search. Going forward
    the Related Searches UX will be triggered by the same gesture
    as TTS.
    
    This is a partial rollback of https://crrev.com/c/2200437 with minor
    changes.
    
    BUG=1148874,1119585
    
    Change-Id: Ifdefea88e23d189bd6347745fc96a733545a77bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538335
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828273}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
@@ -168,7 +168,7 @@ public class ContextualSearchManager
     private long mLoadedSearchUrlTimeMs;
     private boolean mWereSearchResultsSeen;
     private boolean mWereInfoBarsHidden;
-    private int mPromoteSearchNavigationCounter;
+    private boolean mDidPromoteSearchNavigation;
 
     private boolean mWasActivatedByTap;
     private boolean mIsInitialized;
@@ -644,8 +644,7 @@ public class ContextualSearchManager
             if (surroundingText.length() == 0) {
                 mInternalStateController.reset(StateChangeReason.UNKNOWN);
             } else {
-                mContext.setSurroundingText(encoding, surroundingText, startOffset, endOffset,
-                        mPolicy.isProcessingRelatedSearch());
+                mContext.setSurroundingText(encoding, surroundingText, startOffset, endOffset);
                 mInternalStateController.notifyFinishedWorkOn(InternalState.GATHERING_SURROUNDINGS);
             }
         }
@@ -771,10 +770,9 @@ public class ContextualSearchManager
             // TODO(donnd): Instead of preloading, we should prefetch (ie the URL should not
             // appear in the user's history until the user views it).  See crbug.com/406446.
             boolean shouldPreload = !doPreventPreload && mPolicy.shouldPrefetchSearchResult();
-            boolean doRequireGoogleUrl = !mPolicy.isProcessingRelatedSearch();
             mSearchRequest = new ContextualSearchRequest(searchTerm, alternateTerm,
                     resolvedSearchTerm.mid(), shouldPreload, resolvedSearchTerm.searchUrlFull(),
-                    resolvedSearchTerm.searchUrlPreload(), doRequireGoogleUrl);
+                    resolvedSearchTerm.searchUrlPreload());
             // Trigger translation, if enabled.
             mTranslateController.forceTranslateIfNeeded(mSearchRequest,
                     resolvedSearchTerm.contextLanguage(), mSelectionController.isTapSelection());
@@ -1041,7 +1039,7 @@ public class ContextualSearchManager
 
         @Override
         public void onContentLoadStarted(String url) {
-            mPromoteSearchNavigationCounter++;
+            mDidPromoteSearchNavigation = false;
         }
 
         @Override
@@ -1192,16 +1190,14 @@ public class ContextualSearchManager
      * @param url The URL we are navigating to.
      */
     public void onExternalNavigation(String url) {
-        if (mSearchPanel != null && !BLACKLISTED_URL.equals(url)
-                && !url.startsWith(INTENT_URL_PREFIX) && shouldPromoteSearchNavigation()
-                && mPromoteSearchNavigationCounter
-                        > mPolicy.navigateWithoutPromotionLimitForRelatedSearches()) {
+        if (!mDidPromoteSearchNavigation && mSearchPanel != null && !BLACKLISTED_URL.equals(url)
+                && !url.startsWith(INTENT_URL_PREFIX) && shouldPromoteSearchNavigation()) {
             // Do not promote to a regular tab if we're loading our Resolved Search
             // URL, otherwise we'll promote it when prefetching the Serp.
             // Don't promote URLs when they are navigating to an intent - this is
             // handled by the InterceptNavigationDelegate which uses a faster
             // maximizing animation.
-            mPromoteSearchNavigationCounter = 0;
+            mDidPromoteSearchNavigation = true;
             mSearchPanel.maximizePanelThenPromoteToTab(StateChangeReason.SERP_NAVIGATION);
         }
     }
@@ -1681,17 +1677,6 @@ public class ContextualSearchManager
             /** Starts showing the Tap UI by selecting a word around the current caret. */
             @Override
             public void startShowingTapUi() {
-                // Related Searches skips the "Show Tap UI" so the word tapped does not select.
-                // Otherwise the regular tap pipeline continues.
-                if (mPolicy.isProcessingRelatedSearch()) {
-                    // Skip showing the tap-ui (selecting the word) for Related Searches.
-                    mInternalStateController.notifyStartingWorkOn(
-                            InternalState.START_SHOWING_TAP_UI);
-                    mInternalStateController.notifyFinishedWorkOn(
-                            InternalState.START_SHOWING_TAP_UI);
-                    return;
-                }
-
                 WebContents baseWebContents = getBaseWebContents();
                 if (baseWebContents != null) {
                     mInternalStateController.notifyStartingWorkOn(
@@ -1751,8 +1736,6 @@ public class ContextualSearchManager
                 mInternalStateController.notifyStartingWorkOn(InternalState.RESOLVING);
 
                 String selection = mSelectionController.getSelectedText();
-                selection = mPolicy.overrideSelectionIfProcessingRelatedSearches(
-                        selection, mContext.getWordTapped());
                 assert !TextUtils.isEmpty(selection);
                 mNetworkCommunicator.startSearchTermResolutionRequest(
                         selection, mSelectionController.isAdjustedSelection());
