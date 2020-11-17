b8c3b21cf3a111890a65a11908860c339bbfc2a4
Dave Tapuska <dtapuska@chromium.org>
2020-11-17 19:15:26 +0000

    Move some subframe methods into WebFrameWidgetBase.
    These methods are only called from the browser and we can assert
    they are executed for subframes.
    
    BUG=1097816
    
    Change-Id: I152102fc55360769ebec3594aed03d3d452ccbfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537574
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828300}

--- third_party/blink/renderer/core/frame/web_frame_widget_base.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_base.cc
@@ -406,6 +406,20 @@ void WebFrameWidgetBase::SetTextDirection(base::i18n::TextDirection direction) {
     focusedFrame->SetTextDirection(direction);
 }
 
+void WebFrameWidgetBase::SetInheritedEffectiveTouchActionForSubFrame(
+    TouchAction touch_action) {
+  DCHECK(ForSubframe());
+  LocalRootImpl()->GetFrame()->SetInheritedEffectiveTouchAction(touch_action);
+}
+
+void WebFrameWidgetBase::UpdateRenderThrottlingStatusForSubFrame(
+    bool is_throttled,
+    bool subtree_throttled) {
+  DCHECK(ForSubframe());
+  LocalRootImpl()->GetFrameView()->UpdateRenderThrottlingStatus(
+      is_throttled, subtree_throttled, true);
+}
+
 #if defined(OS_MAC)
 void WebFrameWidgetBase::GetStringAtPoint(const gfx::Point& point_in_local_root,
                                           GetStringAtPointCallback callback) {
@@ -1308,6 +1322,11 @@ void WebFrameWidgetBase::DisableDeviceEmulation() {
     return;
   device_emulator_->DisableAndApply();
   device_emulator_ = nullptr;
+}
+
+void WebFrameWidgetBase::SetIsInertForSubFrame(bool inert) {
+  DCHECK(ForSubframe());
+  LocalRootImpl()->GetFrame()->SetIsInert(inert);
 }
 
 base::Optional<gfx::Point>
