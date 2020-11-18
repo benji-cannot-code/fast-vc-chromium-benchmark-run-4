ba9403addb647cca5a92856117fedd8d2407587c
Matthias Körber <koerber@google.com>
2020-11-18 08:45:01 +0000

    [Autofill] Exchange bare pointer with weak pointer for callbacks.
    Change-Id: I9b1467d5f34d9381c8f944009b8f7511063fa30e
    Bug: 1150004, 1148749
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2544706
    Commit-Queue: Matthias Körber <koerber@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/master@{#828642}

--- components/autofill/core/browser/ui/region_combobox_model.cc
+++ components/autofill/core/browser/ui/region_combobox_model.cc
@@ -35,7 +35,7 @@ void RegionComboboxModel::LoadRegionData(const std::string& country_code,
   region_data_loader_->LoadRegionData(
       country_code,
       base::BindRepeating(&RegionComboboxModel::OnRegionDataLoaded,
-                          base::Unretained(this)),
+                          weak_factory_.GetWeakPtr()),
       timeout_ms);
 }
 
