a995037f1cfe4531752881a35bf452b553f7f580
Sigurdur Asgeirsson <siggi@chromium.org>
2020-11-18 23:46:51 +0000

    Retire ScopedObserver in /chrome/browser/ui/views/desktop_capture.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=ellyjones@chromium.org
    
    Bug: 1145565
    Change-Id: Ie6262d31836393d9c8bdbf0cc684564a073792b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547962
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828971}

--- chrome/browser/ui/views/desktop_capture/desktop_media_list_controller.h
+++ chrome/browser/ui/views/desktop_capture/desktop_media_list_controller.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_multi_source_observation.h"
 #include "chrome/browser/media/webrtc/desktop_media_list.h"
 #include "chrome/browser/media/webrtc/desktop_media_list_observer.h"
 #include "chrome/browser/ui/views/desktop_capture/desktop_media_source_view.h"
@@ -120,7 +120,8 @@ class DesktopMediaListController : public DesktopMediaListObserver,
   DesktopMediaPickerDialogView* dialog_;
   std::unique_ptr<DesktopMediaList> media_list_;
   ListView* view_ = nullptr;
-  ScopedObserver<views::View, views::ViewObserver> view_observer_{this};
+  base::ScopedMultiSourceObservation<views::View, views::ViewObserver>
+      view_observations_{this};
 
   base::WeakPtrFactory<DesktopMediaListController> weak_factory_{this};
 };
