30306cefe64cf65b5a08dcb3e4ce74aeaa7f9a53
ckitagawa <ckitagawa@chromium.org>
2020-11-18 18:56:41 +0000

    [Paint Preview] Reduce Memory Overcommit
    Use slightly wider tiles, but request fewer at a time. This should
    reduce memory overcommit. On low mem devices don't request more than 1
    tile at a time.
    
    Bug: 1144768
    Change-Id: I115524c5a226487675d040d37865e0a858abf28c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2546964
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828823}

--- components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/PlayerCompositorDelegateImpl.java
+++ components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/PlayerCompositorDelegateImpl.java
@@ -12,6 +12,7 @@ import android.text.TextUtils;
 import androidx.annotation.NonNull;
 
 import org.chromium.base.Callback;
+import org.chromium.base.SysUtils;
 import org.chromium.base.UnguessableToken;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
@@ -28,6 +29,8 @@ import java.util.List;
  */
 @JNINamespace("paint_preview")
 class PlayerCompositorDelegateImpl implements PlayerCompositorDelegate {
+    private static final int LOW_MEMORY_THRESHOLD_KB = 2048;
+
     private CompositorListener mCompositorListener;
     private long mNativePlayerCompositorDelegate;
     private List<Runnable> mMemoryPressureListeners = new ArrayList<>();
@@ -39,7 +42,8 @@ class PlayerCompositorDelegateImpl implements PlayerCompositorDelegate {
         if (service != null && service.getNativeService() != 0) {
             mNativePlayerCompositorDelegate = PlayerCompositorDelegateImplJni.get().initialize(this,
                     service.getNativeService(), url.getSpec(), directoryKey,
-                    compositorErrorCallback);
+                    compositorErrorCallback,
+                    SysUtils.amountOfPhysicalMemoryKB() < LOW_MEMORY_THRESHOLD_KB);
         }
         // TODO(crbug.com/1021590): Handle initialization errors when
         // mNativePlayerCompositorDelegate == 0.
@@ -133,7 +137,8 @@ class PlayerCompositorDelegateImpl implements PlayerCompositorDelegate {
     @NativeMethods
     interface Natives {
         long initialize(PlayerCompositorDelegateImpl caller, long nativePaintPreviewBaseService,
-                String urlSpec, String directoryKey, Callback<Integer> compositorErrorCallback);
+                String urlSpec, String directoryKey, Callback<Integer> compositorErrorCallback,
+                boolean isLowMemory);
         void destroy(long nativePlayerCompositorDelegateAndroid);
         int requestBitmap(long nativePlayerCompositorDelegateAndroid, UnguessableToken frameGuid,
                 Callback<Bitmap> bitmapCallback, Runnable errorCallback, float scaleFactor,
