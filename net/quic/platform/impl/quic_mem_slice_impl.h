2d260205070d3614c0abc245bbd55a68861f2074
Haoyue Wang <haoyuewang@chromium.org>
2020-11-18 21:38:57 +0000

    Add a constructor that takes in heap allocated buffer to QuicMemSliceImpl.
    Change-Id: Ife0548334e75ae9e23c9c3b3824965ccfe182712
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547210
    Auto-Submit: Haoyue Wang <haoyuewang@chromium.org>
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Reviewed-by: Victor Vasiliev <vasilvv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828904}

--- net/quic/platform/impl/quic_mem_slice_impl.h
+++ net/quic/platform/impl/quic_mem_slice_impl.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_QUIC_PLATFORM_IMPL_QUIC_MEM_SLICE_IMPL_H_
 #define NET_QUIC_PLATFORM_IMPL_QUIC_MEM_SLICE_IMPL_H_
 
+#include <memory>
+
 #include "base/memory/ref_counted.h"
 #include "net/base/io_buffer.h"
 #include "net/third_party/quiche/src/quic/core/quic_buffer_allocator.h"
@@ -21,6 +23,7 @@ class QUIC_EXPORT_PRIVATE QuicMemSliceImpl {
   // Constructs a QuicMemSliceImp by let |allocator| allocate a data buffer of
   // |length|.
   QuicMemSliceImpl(QuicUniqueBufferPtr buffer, size_t length);
+  QuicMemSliceImpl(std::unique_ptr<char[]> buffer, size_t length);
 
   QuicMemSliceImpl(scoped_refptr<net::IOBuffer> io_buffer, size_t length);
 
