3585a015dd2b5d6329fffed63d44cb3683ed3ce5
Darren Shen <shend@chromium.org>
2020-11-18 22:39:55 +0000

    ime: Make AutocorrectManager more testable and add unit tests.
    AutocorrectManager was hard to test because it required a full
    InputMethodEngine as a dependency. However, it really only needs two
    dependencies: an input context handler to manipulate text, and a
    suggestion handler to manipulate the autocorrect range.
    
    Pass in the suggestion handler via the ctor, and grab the input context
    handler via IMEBridge*.
    
    Add a few unit tests with mock handlers. In a future patch, we will
    add a proper fake input context handler so that it's easy to test
    UndoAutocorrect.
    
    * Note that there's an open bug to remove IMEBridge as it is global
    state. See crbug.com/1149751.
    
    Bug: b/172969200
    Change-Id: Ic9deefb873ee94895846bd91efe608ef10418eb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2543537
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: My Nguyen <myy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828933}

--- ui/base/ime/chromeos/mock_ime_input_context_handler.cc
+++ ui/base/ime/chromeos/mock_ime_input_context_handler.cc
@@ -58,7 +58,7 @@ bool MockIMEInputContextHandler::SetComposingRange(
 }
 
 gfx::Range MockIMEInputContextHandler::GetAutocorrectRange() {
-  return gfx::Range();
+  return autocorrect_range_;
 }
 
 gfx::Rect MockIMEInputContextHandler::GetAutocorrectCharacterBounds() {
@@ -69,11 +69,13 @@ bool MockIMEInputContextHandler::SetAutocorrectRange(
     const base::string16& autocorrect_text,
     uint32_t start,
     uint32_t end) {
-  // TODO(crbug.com/1091088): Implement function.
-  return false;
+  autocorrect_range_ = gfx::Range(start, end);
+  return true;
 }
 
-void MockIMEInputContextHandler::ClearAutocorrectRange() {}
+void MockIMEInputContextHandler::ClearAutocorrectRange() {
+  autocorrect_range_ = gfx::Range();
+}
 
 bool MockIMEInputContextHandler::SetSelectionRange(uint32_t start,
                                                    uint32_t end) {
