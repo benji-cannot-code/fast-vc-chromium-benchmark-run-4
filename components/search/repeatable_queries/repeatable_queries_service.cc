bae8ee0a150fda28fffc4e09bb82f2e4559a9d2e
Moe Ahmadi <mahmadi@chromium.org>
2020-11-18 20:10:49 +0000

    [NTP][RQ] Makes in-memory URLDatabase deletion tasks asynchronous
    crrev.com/c/2531654 made deletion of queries from the in-memory URL DB
    asynchronous by posting the deletion task to a sequenced task runner on
    the UI thread. The said CL was later reverted in crrev.com/c/2532543
    for making RepeatableQueriesServiceTest.SignedOut_Deletion flaky. This
    CL fixes the flakiness by providing a way for the tests to flush the
    scheduled tasks after deletion from the in-memory URL DB and TearDown()
    
    Bug: 1148612
    Change-Id: I3f5c7b598a5d6907794b9d6ca1598626156516c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2536355
    Reviewed-by: Ramya Nagarajan <ramyan@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828854}

--- components/search/repeatable_queries/repeatable_queries_service.cc
+++ components/search/repeatable_queries/repeatable_queries_service.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/scoped_observation.h"
 #include "base/stl_util.h"
+#include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
 #include "base/values.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/history_types.h"
@@ -129,7 +131,8 @@ RepeatableQueriesService::RepeatableQueriesService(
       search_provider_observer_(std::make_unique<SearchProviderObserver>(
           template_url_service,
           base::BindRepeating(&RepeatableQueriesService::SearchProviderChanged,
-                              base::Unretained(this)))) {
+                              base::Unretained(this)))),
+      deletion_task_runner_(base::ThreadPool::CreateSequencedTaskRunner({})) {
   DCHECK(history_service_);
   DCHECK(template_url_service_);
   DCHECK(url_loader_factory_);
@@ -257,6 +260,11 @@ GURL RepeatableQueriesService::GetRequestURL() {
                                                     search_terms_data));
 }
 
+void RepeatableQueriesService::FlushForTesting(base::OnceClosure flushed) {
+  deletion_task_runner_->PostTaskAndReply(FROM_HERE, base::DoNothing(),
+                                          std::move(flushed));
+}
+
 void RepeatableQueriesService::GetRepeatableQueriesFromServer() {
   net::NetworkTrafficAnnotationTag traffic_annotation =
       net::DefineNetworkTrafficAnnotation("repeatable_queries_service", R"(
@@ -357,7 +365,7 @@ void RepeatableQueriesService::RepeatableQueriesParsed(
 void RepeatableQueriesService::GetRepeatableQueriesFromURLDatabase() {
   repeatable_queries_.clear();
 
-  // Fail if the in-memory URL database is not available.
+  // Fail if the in-memory URLDatabase is not available.
   history::URLDatabase* url_db = history_service_->InMemoryDatabase();
   if (!url_db)
     return;
@@ -462,13 +470,23 @@ void RepeatableQueriesService::DeletionResponseLoaded(
 
 void RepeatableQueriesService::DeleteRepeatableQueryFromURLDatabase(
     const base::string16& query) {
-  // Fail if the in-memory URL database is not available.
-  history::URLDatabase* url_db = history_service_->InMemoryDatabase();
+  deletion_task_runner_->PostTask(
+      FROM_HERE,
+      base::BindOnce(
+          &RepeatableQueriesService::DeleteRepeatableQueryFromURLDatabaseTask,
+          weak_ptr_factory_.GetWeakPtr(), query,
+          history_service_->InMemoryDatabase()));
+}
+
+void RepeatableQueriesService::DeleteRepeatableQueryFromURLDatabaseTask(
+    const base::string16& query,
+    history::URLDatabase* url_db) {
+  // Fail if the in-memory URLDatabase is not available.
   if (!url_db)
     return;
 
   // Delete all the search terms matching the repeatable query suggestion from
-  // the in-memory URL database.
+  // the in-memory URLDatabase.
   url_db->DeleteKeywordSearchTermForNormalizedTerm(
       template_url_service_->GetDefaultSearchProvider()->id(), query);
 }
