283fbb0cd9ec53651f54693d80a177add689b994
Yulun Wu <yulunwu@chromium.org>
2020-11-18 18:55:01 +0000

    Update scroll arrow view to use themed colors.
    Note: Classic shelf colors can now be safely removed.
    
    https://bugs.chromium.org/p/chromium/issues/detail?id=1146464
    
    Bug: 1145356
    Change-Id: If106ff7954e0b1c6f8b4136dd89fac3ceb7b075e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545489
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828822}

--- ash/shelf/scroll_arrow_view.cc
+++ ash/shelf/scroll_arrow_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_config.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shelf/shelf_button_delegate.h"
+#include "ash/style/ash_color_provider.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/image/image_skia_operations.h"
@@ -38,7 +39,8 @@ void ScrollArrowView::PaintButtonContents(gfx::Canvas* canvas) {
                                (arrow_type_ == kRight && base::i18n::IsRTL());
   gfx::ImageSkia img = CreateVectorIcon(
       show_left_arrow ? kOverflowShelfLeftIcon : kOverflowShelfRightIcon,
-      SK_ColorWHITE);
+      AshColorProvider::Get()->GetContentLayerColor(
+          AshColorProvider::ContentLayerType::kIconColorPrimary));
 
   if (!is_horizontal_alignment_) {
     img = gfx::ImageSkiaOperations::CreateRotatedImage(
