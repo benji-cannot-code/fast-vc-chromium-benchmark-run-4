7f0cdbef259b6ac109ef4b1d3eafdee05bb182c8
Changwan Ryu <changwan@chromium.org>
2020-11-18 00:31:49 +0000

    Apply zero-latency keyboard popup to pre-R
    This is a follow-up to https://crrev.com/c/2340295 to extend the work to
    pre-R version of Android.
    
    According to one experiment, if the keyboard is looking up the initial
    surrounding text, the reduction was from 164.83ms to 0.07ms.
    
    We expect a similar level of reduction for pre-R devices.
    
    Note that it is possible that accessing the compat library can slightly
    increase the time taken for the first keyboard activation, but overall
    this should a significant win.
    
    Bug: b/170464119
    Change-Id: I3cbab8d04873a6773c7302ae9cd9064636619302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2544064
    Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    Commit-Queue: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828491}

--- content/public/android/java/src/org/chromium/content/browser/input/ImeUtils.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ImeUtils.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser.input;
 
-import android.annotation.TargetApi;
-import android.os.Build;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.Selection;
@@ -16,8 +14,9 @@ import android.view.inputmethod.BaseInputConnection;
 import android.view.inputmethod.CorrectionInfo;
 import android.view.inputmethod.EditorInfo;
 
+import androidx.core.view.inputmethod.EditorInfoCompat;
+
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.annotations.VerifiesOnR;
 import org.chromium.blink_public.web.WebTextInputFlags;
 import org.chromium.blink_public.web.WebTextInputMode;
 import org.chromium.ui.base.ime.TextInputAction;
@@ -29,26 +28,6 @@ import java.util.Locale;
  * Utilities for IME such as computing outAttrs, and dumping object information.
  */
 public class ImeUtils {
-    /**
-     * A class to contain R-specific code inside a separate class to avoid performance regression.
-     *
-     * See
-     * https://source.chromium.org/chromium/chromium/src/+/master:build/android/docs/class_verification_failures.md
-     * for details.
-     */
-    @VerifiesOnR
-    @TargetApi(Build.VERSION_CODES.R)
-    private static final class HelperForR {
-        /** see {@link EditorInfo#setInitialSurroundingText(EditorInfo, String)} */
-        public static void setInitialSurroundingText(EditorInfo outAttrs, String lastText) {
-            // Note: Android's internal implementation trims the text up to 2048 chars before
-            // sending it to the IMM service. In the future, if we consider limiting the number of
-            // chars between renderer and browser, then consider calling
-            // setInitialSurroundingSubText() instead.
-            outAttrs.setInitialSurroundingText(lastText);
-        }
-    }
-
     /**
      * Compute {@link EditorInfo} based on the given parameters. This is needed for
      * {@link View#onCreateInputConnection(EditorInfo)}.
@@ -160,9 +139,11 @@ public class ImeUtils {
 
         outAttrs.initialSelStart = initialSelStart;
         outAttrs.initialSelEnd = initialSelEnd;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            HelperForR.setInitialSurroundingText(outAttrs, lastText);
-        }
+        // Note: Android's internal implementation trims the text up to 2048 chars before
+        // sending it to the IMM service. In the future, if we consider limiting the number of
+        // chars between renderer and browser, then consider calling
+        // setInitialSurroundingSubText() instead.
+        EditorInfoCompat.setInitialSurroundingText(outAttrs, lastText);
     }
 
     private static int getImeAction(int inputType, int inputFlags, int inputMode, int inputAction,
