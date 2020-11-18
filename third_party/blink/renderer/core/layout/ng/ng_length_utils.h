2cc4a36785054f6dd0af7f04fcf6301fb42d9d33
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-11-18 00:55:52 +0000

    [TableNG] Account for <caption> when determining the grid avail-space.
    Previously when determining the available size for distributing the
    table-grid into we'd simply resolve our overall fragment block-size.
    
    However when extrinsically sized (fixed, or stretched) we need to also
    subtract the space taken by the captions to correctly determine what
    space the distribute the grid into.
    
    This change performs layout on the table <caption>s up front, before
    computing the size of the sections/rows.
    
    Bug: 958381
    Change-Id: I912d2b92928e6114eb205867acc807568f5b408e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2539339
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828510}

--- third_party/blink/renderer/core/layout/ng/ng_length_utils.h
+++ third_party/blink/renderer/core/layout/ng/ng_length_utils.h
@@ -83,6 +83,7 @@ CORE_EXPORT LayoutUnit ResolveBlockLengthInternal(
     const NGBoxStrut& border_padding,
     const Length&,
     LayoutUnit intrinsic_size,
+    LayoutUnit available_block_size_adjustment = LayoutUnit(),
     const LayoutUnit* opt_percentage_resolution_block_size_for_min_max =
         nullptr);
 
@@ -204,6 +205,7 @@ inline LayoutUnit ResolveMinBlockLength(
     const NGBoxStrut& border_padding,
     const Length& length,
     LengthResolvePhase phase,
+    LayoutUnit available_block_size_adjustment = LayoutUnit(),
     const LayoutUnit* opt_percentage_resolution_block_size_for_min_max =
         nullptr) {
   if (LIKELY(BlockLengthUnresolvable(
@@ -213,6 +215,7 @@ inline LayoutUnit ResolveMinBlockLength(
 
   return ResolveBlockLengthInternal(
       constraint_space, style, border_padding, length, kIndefiniteSize,
+      available_block_size_adjustment,
       opt_percentage_resolution_block_size_for_min_max);
 }
 
@@ -223,6 +226,7 @@ inline LayoutUnit ResolveMaxBlockLength(
     const NGBoxStrut& border_padding,
     const Length& length,
     LengthResolvePhase phase,
+    LayoutUnit available_block_size_adjustment = LayoutUnit(),
     const LayoutUnit* opt_percentage_resolution_block_size_for_min_max =
         nullptr) {
   if (LIKELY(BlockLengthUnresolvable(
@@ -232,6 +236,7 @@ inline LayoutUnit ResolveMaxBlockLength(
 
   return ResolveBlockLengthInternal(
       constraint_space, style, border_padding, length, kIndefiniteSize,
+      available_block_size_adjustment,
       opt_percentage_resolution_block_size_for_min_max);
 }
 
@@ -243,6 +248,7 @@ inline LayoutUnit ResolveMainBlockLength(
     const Length& length,
     LayoutUnit intrinsic_size,
     LengthResolvePhase phase,
+    LayoutUnit available_block_size_adjustment = LayoutUnit(),
     const LayoutUnit* opt_percentage_resolution_block_size_for_min_max =
         nullptr) {
   DCHECK(!length.IsAuto());
@@ -254,6 +260,7 @@ inline LayoutUnit ResolveMainBlockLength(
 
   return ResolveBlockLengthInternal(
       constraint_space, style, border_padding, length, intrinsic_size,
+      available_block_size_adjustment,
       opt_percentage_resolution_block_size_for_min_max);
 }
 
@@ -265,6 +272,7 @@ inline LayoutUnit ResolveMainBlockLength(
     const Length& length,
     const IntrinsicBlockSizeFunc& intrinsic_block_size_func,
     LengthResolvePhase phase,
+    LayoutUnit available_block_size_adjustment = LayoutUnit(),
     const LayoutUnit* opt_percentage_resolution_block_size_for_min_max =
         nullptr) {
   DCHECK(!length.IsAuto());
@@ -280,6 +288,7 @@ inline LayoutUnit ResolveMainBlockLength(
 
   return ResolveBlockLengthInternal(
       constraint_space, style, border_padding, length, intrinsic_block_size,
+      available_block_size_adjustment,
       opt_percentage_resolution_block_size_for_min_max);
 }
 
@@ -317,6 +326,7 @@ MinMaxSizes ComputeMinMaxBlockSize(
     const ComputedStyle&,
     const NGBoxStrut& border_padding,
     LayoutUnit intrinsic_size,
+    LayoutUnit available_block_size_adjustment = LayoutUnit(),
     const LayoutUnit* opt_percentage_resolution_block_size_for_min_max =
         nullptr);
 
@@ -364,12 +374,17 @@ CORE_EXPORT LayoutUnit ComputeInlineSizeForFragment(
 // Same as ComputeInlineSizeForFragment, but uses height instead of width.
 // |inline_size| is necessary to compute the block size when an aspect ratio
 // is in use.
-CORE_EXPORT LayoutUnit
-ComputeBlockSizeForFragment(const NGConstraintSpace&,
-                            const ComputedStyle&,
-                            const NGBoxStrut& border_padding,
-                            LayoutUnit intrinsic_size,
-                            base::Optional<LayoutUnit> inline_size);
+// |available_block_size_adjustment| is needed for <table> layout. When a table
+// is under an extrinsic constraint (being stretched by its parent, or forced
+// to a fixed block-size), we need to subtract the block-size of all the
+// <caption>s from the available block-size.
+CORE_EXPORT LayoutUnit ComputeBlockSizeForFragment(
+    const NGConstraintSpace&,
+    const ComputedStyle&,
+    const NGBoxStrut& border_padding,
+    LayoutUnit intrinsic_size,
+    base::Optional<LayoutUnit> inline_size,
+    LayoutUnit available_block_size_adjustment = LayoutUnit());
 
 // Intrinsic size for replaced elements is computed as:
 // - |out_replaced_size| intrinsic size of the element. It might have no value.
