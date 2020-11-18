4f790bc1b56e903c712f694deaefece5dceb32aa
Francois Doray <fdoray@chromium.org>
2020-11-18 15:32:17 +0000

    [Project Code Inclusion] Remove all uses of "whitelist/blacklist" in //base
    Change-Id: I48f6574d9ebfadf678e248abe2d2ba98a9806fbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2540572
    Auto-Submit: François Doray <fdoray@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828716}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -367,7 +367,6 @@ class BASE_EXPORT BuiltinCategories {
       "Testing",
       "TraceEventAgentTestCategory",
       "unfiltered_cat",
-      "whitewashed",
       "x",
       TRACE_DISABLED_BY_DEFAULT("c9"),
       TRACE_DISABLED_BY_DEFAULT("cat"),
