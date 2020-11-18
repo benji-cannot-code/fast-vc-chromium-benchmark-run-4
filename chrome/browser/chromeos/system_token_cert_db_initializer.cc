e2cee96735865988bf4cee16dcb80955866cd0b0
Omar Morsi <omorsi@google.com>
2020-11-18 20:06:14 +0000

    Introduce timeout for retrieving system certificate database
    This CL handles cases when TPM initialization fails and system token
    certificate database was requested. It handles this case by introducing
    a five-minutes-timeout for the database request.
    
    Bug: 1068548, 844022
    Change-Id: If738218f7816aa9af931fa23c85da39012b86b9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410377
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Omar Morsi <omorsi@google.com>
    Cr-Commit-Position: refs/heads/master@{#828853}

--- chrome/browser/chromeos/system_token_cert_db_initializer.cc
+++ chrome/browser/chromeos/system_token_cert_db_initializer.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/sequence_checker.h"
+#include "base/time/time.h"
 #include "build/branding_buildflags.h"
 #include "build/buildflag.h"
 #include "chrome/browser/chromeos/login/startup_utils.h"
@@ -29,6 +30,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 namespace {
+
+// It is stated in cryptohome implementation that 5 minutes is enough time to
+// wait for any TPM operations. For more information, please refer to:
+// https://chromium.googlesource.com/chromiumos/platform2/+/master/cryptohome/cryptohome.cc
+constexpr base::TimeDelta kMaxCertDbRetrievalDelay =
+    base::TimeDelta::FromMinutes(5);
 
 // Called on UI Thread when the system slot has been retrieved.
 void GotSystemSlotOnUIThread(
@@ -142,8 +149,16 @@ void SystemTokenCertDBInitializer::GetSystemTokenCertDb(
 
   if (system_token_cert_database_) {
     std::move(callback).Run(system_token_cert_database_.get());
+  } else if (system_token_cert_db_retrieval_failed_) {
+    std::move(callback).Run(/*nss_cert_database=*/nullptr);
   } else {
     get_system_token_cert_db_callback_list_.AddUnsafe(std::move(callback));
+
+    if (!system_token_cert_db_retrieval_timer_.IsRunning()) {
+      system_token_cert_db_retrieval_timer_.Start(
+          FROM_HERE, kMaxCertDbRetrievalDelay, /*receiver=*/this,
+          &SystemTokenCertDBInitializer::OnSystemTokenDbRetrievalTimeout);
+    }
   }
 }
 
@@ -229,12 +244,21 @@ void SystemTokenCertDBInitializer::InitializeDatabase(
   database->SetSystemSlot(std::move(system_slot_copy));
 
   system_token_cert_database_ = std::move(database);
+  system_token_cert_db_retrieval_timer_.Stop();
   get_system_token_cert_db_callback_list_.Notify(
       system_token_cert_database_.get());
 
   VLOG(1) << "SystemTokenCertDBInitializer: Passing system token NSS "
              "database to NetworkCertLoader.";
   NetworkCertLoader::Get()->SetSystemNSSDB(system_token_cert_database_.get());
+}
+
+void SystemTokenCertDBInitializer::OnSystemTokenDbRetrievalTimeout() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  system_token_cert_db_retrieval_failed_ = true;
+  get_system_token_cert_db_callback_list_.Notify(
+      /*nss_cert_database=*/nullptr);
 }
 
 }  // namespace chromeos
