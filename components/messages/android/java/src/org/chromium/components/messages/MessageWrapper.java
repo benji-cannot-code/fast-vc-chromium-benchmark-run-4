86096c95da86741fc94ba9e4a5004ef3b0735da3
Pavel Yatsuk <pavely@google.com>
2020-11-18 02:51:13 +0000

    [Messages] Add getter methods for message properties in MessageWrapper
    Getter methods are needed for feature code unittests.
    
    BUG=1123947
    R=twellington@chromium.org
    
    Change-Id: I853a337a92f04c99e27e7c5d3205b13a3ed46c4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2540942
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828560}

--- components/messages/android/java/src/org/chromium/components/messages/MessageWrapper.java
+++ components/messages/android/java/src/org/chromium/components/messages/MessageWrapper.java
@@ -47,8 +47,18 @@ public final class MessageWrapper {
     }
 
     @CalledByNative
+    String getTitle() {
+        return mMessageProperties.get(MessageBannerProperties.TITLE);
+    }
+
+    @CalledByNative
     void setTitle(String title) {
         mMessageProperties.set(MessageBannerProperties.TITLE, title);
+    }
+
+    @CalledByNative
+    String getDescription() {
+        return mMessageProperties.get(MessageBannerProperties.DESCRIPTION);
     }
 
     @CalledByNative
@@ -57,8 +67,19 @@ public final class MessageWrapper {
     }
 
     @CalledByNative
+    String getPrimaryButtonText() {
+        return mMessageProperties.get(MessageBannerProperties.PRIMARY_BUTTON_TEXT);
+    }
+
+    @CalledByNative
     void setPrimaryButtonText(String primaryButtonText) {
         mMessageProperties.set(MessageBannerProperties.PRIMARY_BUTTON_TEXT, primaryButtonText);
+    }
+
+    @CalledByNative
+    @DrawableRes
+    int getIconResourceId() {
+        return mMessageProperties.get(MessageBannerProperties.ICON_RESOURCE_ID);
     }
 
     @CalledByNative
