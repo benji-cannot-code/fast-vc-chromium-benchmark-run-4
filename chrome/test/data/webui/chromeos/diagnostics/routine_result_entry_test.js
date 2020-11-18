3b2e9d5c62de33689eabdbdbc8862cd9ef63656a
Joon Ahn <joonbug@chromium.org>
2020-11-18 00:43:07 +0000

    Diagnostics: Use text badges for test results
    http://screen/7XAbyfjQY2eXkcm
    
    Bug: 1125150
    Test: browser_tests --gtest_Filter=DiagnosticsApp*
    Change-Id: I64c89a4feacfc74d718b6ed32de60f252aace750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538200
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828503}

--- chrome/test/data/webui/chromeos/diagnostics/routine_result_entry_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/routine_result_entry_test.js
@@ -7,6 +7,7 @@ import 'chrome://diagnostics/routine_result_entry.js';
 
 import {RoutineName, RoutineResult, StandardRoutineResult} from 'chrome://diagnostics/diagnostics_types.js';
 import {ExecutionProgress, ResultStatusItem} from 'chrome://diagnostics/routine_list_executor.js';
+import {BadgeType} from 'chrome://diagnostics/text_badge.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
 import {flushTasks} from '../../test_util.m.js';
@@ -94,13 +95,14 @@ export function routineResultEntryTestSuite() {
   }
 
   /**
-   * Returns the status element text content.
-   * @return {string}
+   * Returns the status badge content.
+   * @return {!TextBadgeElement}
    */
-  function getStatusText() {
-    const status = routineResultEntryElement.$$('#status');
-    assertTrue(!!status);
-    return status.textContent.trim();
+  function getStatusBadge() {
+    const badge = /** @type{!TextBadgeElement} */ (
+        routineResultEntryElement.$$('#status'));
+    assertTrue(!!badge);
+    return badge;
   }
 
   test('ElementRendered', () => {
@@ -119,7 +121,8 @@ export function routineResultEntryTestSuite() {
       assertEquals(getNameText(), 'kCpuStress');
 
       // Status should be empty if the test is not started.
-      assertEquals(getStatusText(), '');
+      // TODO(joonbug): Utilize isVisible util function.
+      assertTrue(getStatusBadge().hidden);
     });
   });
 
@@ -131,7 +134,8 @@ export function routineResultEntryTestSuite() {
       assertEquals(getNameText(), 'kCpuStress');
 
       // Status should be running.
-      assertEquals(getStatusText(), 'kRunning');
+      assertEquals(getStatusBadge().value, 'RUNNING');
+      assertEquals(getStatusBadge().badgeType, BadgeType.DEFAULT);
     });
   });
 
@@ -144,7 +148,8 @@ export function routineResultEntryTestSuite() {
       assertEquals(getNameText(), 'kCpuStress');
 
       // Status should show the passed result.
-      assertEquals(getStatusText(), 'kTestPassed');
+      assertEquals(getStatusBadge().value, 'SUCCESS');
+      assertEquals(getStatusBadge().badgeType, BadgeType.SUCCESS);
     });
   });
 
@@ -157,7 +162,8 @@ export function routineResultEntryTestSuite() {
       assertEquals(getNameText(), 'kCpuStress');
 
       // Status should show the passed result.
-      assertEquals(getStatusText(), 'kTestFailed');
+      assertEquals(getStatusBadge().value, 'FAILED');
+      assertEquals(getStatusBadge().badgeType, BadgeType.ERROR);
     });
   });
 }
