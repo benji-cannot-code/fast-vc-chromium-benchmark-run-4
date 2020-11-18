46d242d4ffb33902b5143171f4ca1a3554847cd9
James Cook <jamescook@chromium.org>
2020-11-18 17:28:40 +0000

    lacros: Introduce ChromeBrowserMainPartsLacros
    I need to introduce it so I have a place to put some crash and metrics
    consent code that needs to run early in browser startup.
    
    No functional changes.
    
    Bug: 1148604
    Change-Id: I9dca170b1cf9f0453e174d7cbe0a0b8652f7328b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2508367
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828773}

---
+++ chrome/browser/chrome_browser_main_parts_lacros.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chrome_browser_main_parts_lacros.h"
+
+#include "base/check.h"
+#include "chrome/browser/browser_process.h"
+#include "content/public/common/result_codes.h"
+
+ChromeBrowserMainPartsLacros::ChromeBrowserMainPartsLacros(
+    const content::MainFunctionParams& parameters,
+    StartupData* startup_data)
+    : ChromeBrowserMainPartsLinux(parameters, startup_data) {}
+
+ChromeBrowserMainPartsLacros::~ChromeBrowserMainPartsLacros() = default;
+
+int ChromeBrowserMainPartsLacros::PreEarlyInitialization() {
+  int result = ChromeBrowserMainPartsLinux::PreEarlyInitialization();
+  if (result != content::RESULT_CODE_NORMAL_EXIT)
+    return result;
+
+  DCHECK(g_browser_process->local_state());
+  // TODO(https://crbug.com/1148604): Inherit metrics consent from ash. Do it
+  // here because local state is required to check for policy overrides.
+
+  return content::RESULT_CODE_NORMAL_EXIT;
+}
