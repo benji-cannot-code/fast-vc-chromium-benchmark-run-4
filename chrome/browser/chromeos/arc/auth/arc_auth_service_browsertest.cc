b1188d8875e49bd701d9dbfa2879535471faf06a
Muhammad Hasan Khan <mhasank@chromium.org>
2020-11-18 17:01:30 +0000

    arc: remove ArcSignInStatus unused values
    ArcSignInStatus has values that are only used inside ARC and are not
    sent over mojom interfaces. In this change we're removing all such
    values from the mojom interface and we'll create a local enum in ARC for
    referencing it in the code there.
    
    BUG=b:166322619
    TEST=CQ
    
    Change-Id: I00a31e6c7f7b49d7cc0454f905539b1c494799ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2542824
    Commit-Queue: Muhammad Hasan Khan <mhasank@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828752}

--- chrome/browser/chromeos/arc/auth/arc_auth_service_browsertest.cc
+++ chrome/browser/chromeos/arc/auth/arc_auth_service_browsertest.cc
@@ -152,7 +152,7 @@ class FakeAuthInstance : public mojom::AuthInstance {
 
   mojom::AccountInfo* account_info() { return account_info_.get(); }
 
-  mojom::ArcSignInStatus sign_in_status() const { return status_; }
+  mojom::ArcAuthCodeStatus auth_code_status() const { return status_; }
 
   bool sign_in_persistent_error() const { return persistent_error_; }
 
@@ -166,7 +166,7 @@ class FakeAuthInstance : public mojom::AuthInstance {
 
  private:
   void OnPrimaryAccountInfoResponse(base::OnceClosure done_closure,
-                                    mojom::ArcSignInStatus status,
+                                    mojom::ArcAuthCodeStatus status,
                                     mojom::AccountInfoPtr account_info) {
     account_info_ = std::move(account_info);
     status_ = status;
@@ -174,7 +174,7 @@ class FakeAuthInstance : public mojom::AuthInstance {
   }
 
   void OnAccountInfoResponse(base::OnceClosure done_closure,
-                             mojom::ArcSignInStatus status,
+                             mojom::ArcAuthCodeStatus status,
                              mojom::AccountInfoPtr account_info,
                              bool persistent_error) {
     status_ = status;
@@ -184,7 +184,7 @@ class FakeAuthInstance : public mojom::AuthInstance {
   }
 
   mojo::Remote<mojom::AuthHost> host_remote_;
-  mojom::ArcSignInStatus status_;
+  mojom::ArcAuthCodeStatus status_;
   bool persistent_error_;
   mojom::AccountInfoPtr account_info_;
   base::OnceClosure done_closure_;
@@ -598,8 +598,8 @@ IN_PROC_BROWSER_TEST_P(ArcAuthServiceTest,
   run_loop.Run();
 
   EXPECT_FALSE(auth_instance().account_info());
-  EXPECT_EQ(mojom::ArcSignInStatus::CHROME_SERVER_COMMUNICATION_ERROR,
-            auth_instance().sign_in_status());
+  EXPECT_EQ(mojom::ArcAuthCodeStatus::CHROME_SERVER_COMMUNICATION_ERROR,
+            auth_instance().auth_code_status());
 }
 
 IN_PROC_BROWSER_TEST_P(ArcAuthServiceTest, FetchSecondaryAccountInfoSucceeds) {
@@ -640,8 +640,8 @@ IN_PROC_BROWSER_TEST_P(ArcAuthServiceTest,
   run_loop.Run();
 
   EXPECT_FALSE(auth_instance().account_info());
-  EXPECT_EQ(mojom::ArcSignInStatus::CHROME_SERVER_COMMUNICATION_ERROR,
-            auth_instance().sign_in_status());
+  EXPECT_EQ(mojom::ArcAuthCodeStatus::CHROME_SERVER_COMMUNICATION_ERROR,
+            auth_instance().auth_code_status());
 }
 
 IN_PROC_BROWSER_TEST_P(ArcAuthServiceTest,
@@ -658,8 +658,8 @@ IN_PROC_BROWSER_TEST_P(ArcAuthServiceTest,
   run_loop.Run();
 
   EXPECT_FALSE(auth_instance().account_info());
-  EXPECT_EQ(mojom::ArcSignInStatus::CHROME_SERVER_COMMUNICATION_ERROR,
-            auth_instance().sign_in_status());
+  EXPECT_EQ(mojom::ArcAuthCodeStatus::CHROME_SERVER_COMMUNICATION_ERROR,
+            auth_instance().auth_code_status());
   EXPECT_TRUE(auth_instance().sign_in_persistent_error());
 }
 
@@ -678,8 +678,8 @@ IN_PROC_BROWSER_TEST_P(ArcAuthServiceTest,
   run_loop.Run();
 
   EXPECT_FALSE(auth_instance().account_info());
-  EXPECT_EQ(mojom::ArcSignInStatus::CHROME_SERVER_COMMUNICATION_ERROR,
-            auth_instance().sign_in_status());
+  EXPECT_EQ(mojom::ArcAuthCodeStatus::CHROME_SERVER_COMMUNICATION_ERROR,
+            auth_instance().auth_code_status());
   EXPECT_TRUE(auth_instance().sign_in_persistent_error());
 }
 
@@ -695,8 +695,8 @@ IN_PROC_BROWSER_TEST_P(
   run_loop.Run();
 
   EXPECT_FALSE(auth_instance().account_info());
-  EXPECT_EQ(mojom::ArcSignInStatus::CHROME_ACCOUNT_NOT_FOUND,
-            auth_instance().sign_in_status());
+  EXPECT_EQ(mojom::ArcAuthCodeStatus::CHROME_ACCOUNT_NOT_FOUND,
+            auth_instance().auth_code_status());
   EXPECT_TRUE(auth_instance().sign_in_persistent_error());
 }
 
