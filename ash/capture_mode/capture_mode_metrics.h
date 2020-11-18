ebb309f9fe9d7e43d21194916fd135cbfb52f01e
chinsenj <chinsenj@chromium.org>
2020-11-18 23:41:13 +0000

    capture_mode: Add metrics for a user's capture config when they capture.
    This CL adds metrics for the configuration a user has when they capture.
    I.e. taking a fullscreen screenshot, window screenshot, etc.
    
    Test: manual + added
    Bug: 1140182
    Change-Id: Ibe4f6289d754d76e1e6ecba1d9442cdfc96d0d3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538062
    Commit-Queue: Jeremy Chinsen <chinsenj@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#828968}

--- ash/capture_mode/capture_mode_metrics.h
+++ ash/capture_mode/capture_mode_metrics.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "ash/capture_mode/capture_mode_types.h"
+
 namespace ash {
 
 // Enumeration of capture bar buttons that can be pressed while in capture mode.
@@ -21,6 +23,19 @@ enum class CaptureModeBarButtonType {
   kWindow,
   kExit,
   kMaxValue = kExit,
+};
+
+// Enumeration of the various configurations a user can have while in capture
+// mode. Note that these values are persisted to histograms so existing values
+// should remain unchanged and new values should be added to the end.
+enum class CaptureModeConfiguration {
+  kFullscreenScreenshot,
+  kRegionScreenshot,
+  kWindowScreenshot,
+  kFullscreenRecording,
+  kRegionRecording,
+  kWindowRecording,
+  kMaxValue = kWindowRecording,
 };
 
 // Enumeration of actions that can be taken to enter capture mode. Note that
@@ -37,6 +52,10 @@ enum class CaptureModeEntryType {
 
 // Records capture mode bar button presses given by |button_type|.
 void RecordCaptureModeBarButtonType(CaptureModeBarButtonType button_type);
+
+// Records a user's configuration when they perform a capture.
+void RecordCaptureModeConfiguration(CaptureModeType type,
+                                    CaptureModeSource source);
 
 // Records the method the user enters capture mode given by |entry_type|.
 void RecordCaptureModeEntryType(CaptureModeEntryType entry_type);
