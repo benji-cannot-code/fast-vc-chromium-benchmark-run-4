f328eb58aeba43ca4a3178d2d11fefd200e2a211
Sigurdur Asgeirsson <siggi@chromium.org>
2020-11-19 17:55:52 +0000

    Retire ScopedObserver in chrome/browser/ui/toolbar/.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was created by git cl split.
    
    Bug: 1145565
    Change-Id: I1480c826c9a9176d6489c4aa36672844cdb290a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550222
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829288}

--- chrome/browser/ui/views/toolbar/extension_toolbar_menu_view.h
+++ chrome/browser/ui/views/toolbar/extension_toolbar_menu_view.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_TOOLBAR_EXTENSION_TOOLBAR_MENU_VIEW_H_
 
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_bar.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_bar_observer.h"
 #include "chrome/browser/ui/views/frame/app_menu_button.h"
@@ -82,10 +82,10 @@ class ExtensionToolbarMenuView : public AppMenuButtonObserver,
   // The maximum allowed height for the view.
   int max_height_ = 0;
 
-  ScopedObserver<ToolbarActionsBar, ToolbarActionsBarObserver>
-      toolbar_actions_bar_observer_{this};
-  ScopedObserver<AppMenuButton, AppMenuButtonObserver>
-      app_menu_button_observer_{this};
+  base::ScopedObservation<ToolbarActionsBar, ToolbarActionsBarObserver>
+      toolbar_actions_bar_observation_{this};
+  base::ScopedObservation<AppMenuButton, AppMenuButtonObserver>
+      app_menu_button_observation_{this};
 
   base::WeakPtrFactory<ExtensionToolbarMenuView> weak_factory_{this};
 };
