bbde74ddcb0b42c5d092df6b869a3221183b07d2
Martin Robinson <mrobinson@igalia.com>
2020-11-19 21:28:45 +0000

    Improve AXTextStateChangeType in NSAccessibilitySelectedTextChangedNotification
    When setting the AXTextStateChangeType key in the user info data
    structure for the notification, use AXTextStateChangeTypeSelectionMove
    when we detect a focus change. This causes VoiceOver to properly
    announce the label and type of form entries when their contents are
    selected due to focus changes.
    
    type are now announced.
    
    Bug: 1127421
    Change-Id: I42d66ad60fbcba7c8c34396fdbc3f6e0c739d1a2
    AX-Relnotes: When focus changes moves to form inputs, the input label and
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2512913
    Commit-Queue: Martin Robinson <mrobinson@igalia.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829380}

--- content/browser/accessibility/browser_accessibility_manager_mac.h
+++ content/browser/accessibility/browser_accessibility_manager_mac.h
@@ -55,7 +55,8 @@ class CONTENT_EXPORT BrowserAccessibilityManagerMac
                               const std::vector<Change>& changes) override;
 
   // Returns an autoreleased object.
-  NSDictionary* GetUserInfoForSelectedTextChangedNotification();
+  NSDictionary* GetUserInfoForSelectedTextChangedNotification(
+      bool focus_changed);
 
   // Returns an autoreleased object.
   NSDictionary* GetUserInfoForValueChangedNotification(
