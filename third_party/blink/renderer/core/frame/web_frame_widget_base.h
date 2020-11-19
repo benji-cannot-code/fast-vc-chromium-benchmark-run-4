5f13de9583f30434213cf76195e567b6e3fe2230
Dave Tapuska <dtapuska@chromium.org>
2020-11-19 01:03:58 +0000

    Move SetWindowRect[Synchronously] into base.
    Move the implementations into base and protect with a
    DCHECK(ForMainFrame() as we don't expect setting the window rect to
    be an operation on a child local root.
    
    BUG=1097816
    
    Change-Id: Ia2f6d3f735ad9cd91bbd507a26a54ff44e0a6133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2543833
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829012}

--- third_party/blink/renderer/core/frame/web_frame_widget_base.h
+++ third_party/blink/renderer/core/frame/web_frame_widget_base.h
@@ -589,6 +589,9 @@ class CORE_EXPORT WebFrameWidgetBase
   // pixels).
   gfx::Size DIPsToCeiledBlinkSpace(const gfx::Size& size);
 
+  void SetWindowRect(const gfx::Rect& window_rect);
+  void SetWindowRectSynchronouslyForTesting(const gfx::Rect& new_window_rect);
+
   void SetToolTipText(const String& tooltip_text, TextDirection dir);
 
   void ShowVirtualKeyboardOnElementFocus();
@@ -788,6 +791,8 @@ class CORE_EXPORT WebFrameWidgetBase
 
   void ForEachRemoteFrameControlledByWidget(
       const base::RepeatingCallback<void(RemoteFrame*)>& callback);
+
+  void SetWindowRectSynchronously(const gfx::Rect& new_window_rect);
 
   static bool ignore_input_events_;
 
