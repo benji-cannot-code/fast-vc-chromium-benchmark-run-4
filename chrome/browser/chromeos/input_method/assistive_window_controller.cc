ad8fe9454fd7a112aa452c904ede5becb98aa1cd
John Palmer <jopalmer@chromium.org>
2020-11-19 01:22:35 +0000

    Merge tts handlers
    BUG=1148093
    
    Change-Id: Ib23c8bbc8ac1ad5b8c70a940693f14ad22acf1f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2530961
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Reviewed-by: My Nguyen <myy@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829016}

--- chrome/browser/chromeos/input_method/assistive_window_controller.cc
+++ chrome/browser/chromeos/input_method/assistive_window_controller.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "ash/public/cpp/ash_pref_names.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
 #include "ash/wm/window_util.h"
@@ -17,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/input_method/ui/suggestion_details.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/grit/generated_resources.h"
-#include "components/prefs/pref_service.h"
 #include "ui/base/ime/chromeos/ime_bridge.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/views/widget/widget.h"
@@ -45,39 +43,6 @@ constexpr base::TimeDelta kTtsShowDelay =
     base::TimeDelta::FromMilliseconds(1200);
 
 }  // namespace
-
-TtsHandler::TtsHandler(Profile* profile) : profile_(profile) {}
-TtsHandler::~TtsHandler() = default;
-
-void TtsHandler::Announce(const std::string& text,
-                          const base::TimeDelta delay) {
-  const bool chrome_vox_enabled = profile_->GetPrefs()->GetBoolean(
-      ash::prefs::kAccessibilitySpokenFeedbackEnabled);
-  if (!chrome_vox_enabled)
-    return;
-
-  delay_timer_ = std::make_unique<base::OneShotTimer>();
-  delay_timer_->Start(
-      FROM_HERE, delay,
-      base::BindOnce(&TtsHandler::Speak, base::Unretained(this), text));
-}
-
-void TtsHandler::OnTtsEvent(content::TtsUtterance* utterance,
-                            content::TtsEventType event_type,
-                            int char_index,
-                            int length,
-                            const std::string& error_message) {}
-
-void TtsHandler::Speak(const std::string& text) {
-  std::unique_ptr<content::TtsUtterance> utterance =
-      content::TtsUtterance::Create(profile_);
-  utterance->SetText(text);
-  utterance->SetEventDelegate(this);
-  utterance->SetCanEnqueue(true);
-
-  auto* tts_controller = content::TtsController::GetInstance();
-  tts_controller->SpeakOrEnqueue(std::move(utterance));
-}
 
 AssistiveWindowController::AssistiveWindowController(
     AssistiveWindowControllerDelegate* delegate,
