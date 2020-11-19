2a4a92359c3eb7504aa58635ce91292dfce0b50f
Aya ElAttar <ayaelattar@chromium.org>
2020-11-19 20:21:45 +0000

    Remove ManagedGuestSessionAutoLaunchNotificationReduced
    - This policy was marked to be removed in M89
    in this CL crrev.com/c/2360373
    - The policy was originally added in this CL
    crrev.com/c/2096445
    
    Bug: 1117198
    Change-Id: I3aa741f13de4fa8084f59cec31566193e5490f03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2544684
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829355}

--- chrome/browser/chromeos/login/existing_user_controller.h
+++ chrome/browser/chromeos/login/existing_user_controller.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/login/auth/login_performer.h"
 #include "chromeos/login/auth/user_context.h"
 #include "components/account_id/account_id.h"
-#include "components/prefs/pref_registry_simple.h"
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
 #include "components/user_manager/user_type.h"
@@ -71,10 +70,6 @@ class ExistingUserController : public LoginDisplay::Delegate,
   // Returns the current existing user controller fetched from the current
   // LoginDisplayHost instance.
   static ExistingUserController* current_controller();
-
-  // Registers the pref for ManagedGuestSessionAutoLaunchNotificationReduced
-  // policy.
-  static void RegisterLocalStatePrefs(PrefRegistrySimple* registry);
 
   // All UI initialization is deferred till Init() call.
   ExistingUserController();
