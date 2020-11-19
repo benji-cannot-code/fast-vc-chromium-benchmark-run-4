93fa8e1e3616af3ec33e45effb88881cef2b2f36
Tal Pressman <talp@chromium.org>
2020-11-19 02:02:32 +0000

    Remove a no-longer relevant comment disabling clang-format.
    Change-Id: I08cc79651c2bfabcbaa64372bac54b732c33eae1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538868
    Auto-Submit: Tal Pressman <talp@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829032}

--- extensions/renderer/api/automation/automation_internal_custom_bindings.cc
+++ extensions/renderer/api/automation/automation_internal_custom_bindings.cc
@@ -1707,8 +1707,6 @@ void AutomationInternalCustomBindings::Invalidate() {
   tree_id_to_tree_wrapper_map_.clear();
 }
 
-// http://crbug.com/784266
-// clang-format off
 void AutomationInternalCustomBindings::OnMessageReceived(
     const IPC::Message& message) {
   IPC_BEGIN_MESSAGE_MAP(AutomationInternalCustomBindings, message)
@@ -1717,7 +1715,7 @@ void AutomationInternalCustomBindings::OnMessageReceived(
     IPC_MESSAGE_HANDLER(ExtensionMsg_AccessibilityLocationChange,
                         OnAccessibilityLocationChange)
   IPC_END_MESSAGE_MAP()
-}  // clang-format on
+}
 
 AutomationAXTreeWrapper* AutomationInternalCustomBindings::
     GetAutomationAXTreeWrapperFromTreeID(ui::AXTreeID tree_id) const {
