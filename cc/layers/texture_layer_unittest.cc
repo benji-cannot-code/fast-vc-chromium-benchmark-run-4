811a68a45cb7d2b10cf329b5bd9d7e1354e93895
Sunny Sachanandani <sunnyps@chromium.org>
2020-11-19 19:05:42 +0000

    cc: Speculative fix for texture layer visibility change test
    Not resetting the |resource_changed_| flag will cause the same resource
    to be returned if multiple calls to PrepareTransferableResource() are
    made, and this triggers a DCHECK failure in TextureLayer.
    
    Bug: 1114952
    Change-Id: I6b3b6ff00fd32316d67bee46f4e45f7bdd418a23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2548520
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829317}

--- cc/layers/texture_layer_unittest.cc
+++ cc/layers/texture_layer_unittest.cc
@@ -1005,6 +1005,7 @@ class TextureLayerChangeInvisibleMailboxTest
     ++prepare_called_;
     if (!resource_changed_)
       return false;
+    resource_changed_ = false;
     *resource = resource_;
     *release_callback = viz::SingleReleaseCallback::Create(base::BindOnce(
         &TextureLayerChangeInvisibleMailboxTest::ResourceReleased,
