c03992b90fd4a5c517a1d7c3b41de6b879e3666e
Thomas Phillips <tphillips@snapchat.com>
2020-11-19 02:01:23 +0000

    Move cookie browsertests out of RenderFrameMessageFilterBrowserTest
    The remaining tests in render_frame_message_filter_browsertest.cc are all cookie tests. Renamed the file:
    
    * Old name: content/browser/frame_host/render_frame_message_filter_browsertest.cc
    * New name: content/browser/frame_host/cookie_browsertest.cc
    
    R=boliu@chromium.org
    
    Bug: 1080786
    Change-Id: If4e759a9f8f8a943c438cafbcee3f1144600c035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532766
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829030}

--- content/browser/renderer_host/cookie_browsertest.cc
+++ content/browser/renderer_host/cookie_browsertest.cc
@@ -107,10 +107,7 @@ std::string GetCookiesDirect(WebContentsImpl* tab, const GURL& url) {
 
 }  // namespace
 
-// TODO(crbug.com/965982): document.cookie is now handled by the
-// RestrictedCookieManager, not the RenderFrameMessageFilter, so these cookie
-// tests should be moved accordingly.
-class RenderFrameMessageFilterBrowserTest : public ContentBrowserTest {
+class CookieBrowserTest : public ContentBrowserTest {
  protected:
   void SetUp() override {
     base::CommandLine::ForCurrentProcess()->AppendSwitch(
@@ -133,7 +130,7 @@ class RenderFrameMessageFilterBrowserTest : public ContentBrowserTest {
 
 // Exercises basic cookie operations via javascript, including an http page
 // interacting with secure cookies.
-IN_PROC_BROWSER_TEST_F(RenderFrameMessageFilterBrowserTest, Cookies) {
+IN_PROC_BROWSER_TEST_F(CookieBrowserTest, Cookies) {
   SetupCrossSiteRedirector(embedded_test_server());
   ASSERT_TRUE(embedded_test_server()->Start());
 
@@ -207,7 +204,7 @@ IN_PROC_BROWSER_TEST_F(RenderFrameMessageFilterBrowserTest, Cookies) {
 }
 
 // Ensure "priority" cookie option is settable via document.cookie.
-IN_PROC_BROWSER_TEST_F(RenderFrameMessageFilterBrowserTest, CookiePriority) {
+IN_PROC_BROWSER_TEST_F(CookieBrowserTest, CookiePriority) {
   ASSERT_TRUE(embedded_test_server()->Start());
 
   struct {
@@ -234,7 +231,7 @@ IN_PROC_BROWSER_TEST_F(RenderFrameMessageFilterBrowserTest, CookiePriority) {
 
 // SameSite cookies (that aren't marked as http-only) should be available to
 // JavaScript.
-IN_PROC_BROWSER_TEST_F(RenderFrameMessageFilterBrowserTest, SameSiteCookies) {
+IN_PROC_BROWSER_TEST_F(CookieBrowserTest, SameSiteCookies) {
   // Must use HTTPS because SameSite=None cookies must be Secure.
   net::EmbeddedTestServer server(net::EmbeddedTestServer::TYPE_HTTPS);
   server.SetSSLConfig(net::EmbeddedTestServer::CERT_TEST_NAMES);
@@ -375,8 +372,7 @@ class CookieStoreContentBrowserClient : public ContentBrowserClient {
 // for wrong URLs are rejected.
 // TODO(https://crbug.com/954603): This should actually result in renderer
 // kills.
-IN_PROC_BROWSER_TEST_F(RenderFrameMessageFilterBrowserTest,
-                       CrossSiteCookieSecurityEnforcement) {
+IN_PROC_BROWSER_TEST_F(CookieBrowserTest, CrossSiteCookieSecurityEnforcement) {
   // The code under test is only active under site isolation.
   if (!AreAllSitesIsolatedForTesting()) {
     return;
