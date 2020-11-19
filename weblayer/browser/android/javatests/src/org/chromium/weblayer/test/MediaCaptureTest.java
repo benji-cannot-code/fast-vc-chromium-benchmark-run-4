dfe9d48dd0759102bc8823a3d9241946a7cc118a
Evan Stade <estade@chromium.org>
2020-11-19 22:32:48 +0000

    WebLayer: re-enable MediaCaptureTests
    Hopefully these have been fixed along with crbug.com/1138608
    
    Bug: 1107380
    Change-Id: I648f46be5f16d2d8c17e7c5d054b4ad298187315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550368
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829401}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/MediaCaptureTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/MediaCaptureTest.java
@@ -26,7 +26,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.weblayer.MediaCaptureCallback;
@@ -88,10 +87,7 @@ public final class MediaCaptureTest {
      */
     @Test
     @MediumTest
-    @DisableIf.Build(sdk_is_greater_than = 20, sdk_is_less_than = 24,
-            message = "Failing on {Lollipop,Marshmallow} Tablet Tester. https://crbug.com/1107380")
-    public void
-    testMediaCapture_basic() throws Throwable {
+    public void basic() throws Throwable {
         mActivityTestRule.navigateAndWait(
                 mActivityTestRule.getTestServer().getURL("/weblayer/test/data/getusermedia.html"));
 
@@ -120,10 +116,7 @@ public final class MediaCaptureTest {
      */
     @Test
     @MediumTest
-    @DisableIf.Build(sdk_is_greater_than = 20, sdk_is_less_than = 24,
-            message = "Failing on {Lollipop,Marshmallow} Tablet Tester. https://crbug.com/1107380")
-    public void
-    testMediaCapture_rememberPermission() throws Throwable {
+    public void rememberPermission() throws Throwable {
         mActivityTestRule.navigateAndWait(
                 mActivityTestRule.getTestServer().getURL("/weblayer/test/data/getusermedia.html"));
 
@@ -152,10 +145,7 @@ public final class MediaCaptureTest {
      */
     @Test
     @MediumTest
-    @DisableIf.Build(sdk_is_greater_than = 20, sdk_is_less_than = 24,
-            message = "Failing on {Lollipop,Marshmallow} Tablet Tester. https://crbug.com/1107380")
-    public void
-    testMediaCapture_twoStreams() throws Throwable {
+    public void twoStreams() throws Throwable {
         mActivityTestRule.navigateAndWait(
                 mActivityTestRule.getTestServer().getURL("/weblayer/test/data/getusermedia2.html"));
 
@@ -183,10 +173,7 @@ public final class MediaCaptureTest {
     @Test
     @MediumTest
     @MinAndroidSdkLevel(Build.VERSION_CODES.M)
-    @DisableIf.Build(sdk_is_greater_than = 20, sdk_is_less_than = 24,
-            message = "Failing on {Lollipop,Marshmallow} Tablet Tester. https://crbug.com/1107380")
-    public void
-    testMediaCapture_twoStreamsNotification() throws Throwable {
+    public void twoStreamsNotification() throws Throwable {
         mActivityTestRule.navigateAndWait(
                 mActivityTestRule.getTestServer().getURL("/weblayer/test/data/getusermedia2.html"));
 
