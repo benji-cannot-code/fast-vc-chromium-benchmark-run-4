729eae4afb67811195fa5cfdb992163617bd1d62
Dave Tapuska <dtapuska@chromium.org>
2020-11-19 17:49:52 +0000

    Move ApplyVisualProperties into WebFrameWidgetBase
    Move the two implementations into one common one in the base.
    
    BUG=1097816
    
    Change-Id: Ia4187c07676256e62e8d019e90f79b4acc24f97a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2542596
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829284}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.h
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.h
@@ -94,8 +94,6 @@ class WebFrameWidgetImpl final : public WebFrameWidgetBase {
 
   // WebFrameWidgetBase overrides:
   void DidCreateLocalRootView() override;
-  void ApplyVisualPropertiesSizing(
-      const VisualProperties& visual_properties) override;
 
   // FrameWidget overrides:
   bool ShouldHandleImeEvents() override;
