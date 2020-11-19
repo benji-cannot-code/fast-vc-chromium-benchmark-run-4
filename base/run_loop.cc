10803351055f6c20388463d0073a7cbe3cabdf99
Gabriel Charette <gab@chromium.org>
2020-11-19 14:51:13 +0000

    [base] Add flow events to RunLoop::Quit(WhenIdle)
    As discussed @
    https://chromium-review.googlesource.com/c/chromium/src/+/2537140/2#message-db3a48fe7ae821111e603331a10c74c7701a4d5e
    
    Useful to see the flow of Quit's in browser tests
    (with --enable-tracing="*")
    
    Example trace: https://snipboard.io/7R8aXC.jpg
    
    R=altimin@chromium.org
    
    Change-Id: I74369e939f566da35fb50c714445cd7ef7ef6431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538176
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829225}

--- base/run_loop.cc
+++ base/run_loop.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_local.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "base/trace_event/base_tracing.h"
 #include "build/build_config.h"
 
 namespace base {
@@ -65,7 +66,13 @@ RunLoop::Delegate::~Delegate() {
 }
 
 bool RunLoop::Delegate::ShouldQuitWhenIdle() {
-  return active_run_loops_.top()->quit_when_idle_received_;
+  const auto* top_loop = active_run_loops_.top();
+  if (top_loop->quit_when_idle_received_) {
+    TRACE_EVENT_WITH_FLOW0("toplevel.flow", "RunLoop_ExitedOnIdle",
+                           TRACE_ID_LOCAL(top_loop), TRACE_EVENT_FLAG_FLOW_IN);
+    return true;
+  }
+  return false;
 }
 
 // static
@@ -146,6 +153,12 @@ void RunLoop::Quit() {
     return;
   }
 
+  // While Quit() is an "OUT" call to reach one of the quit-states ("IN"),
+  // OUT|IN is used to visually link multiple Quit*() together which can help
+  // when debugging flaky tests.
+  TRACE_EVENT_WITH_FLOW0("toplevel.flow", "RunLoop::Quit", TRACE_ID_LOCAL(this),
+                         TRACE_EVENT_FLAG_FLOW_OUT | TRACE_EVENT_FLAG_FLOW_IN);
+
   quit_called_ = true;
   if (running_ && delegate_->active_run_loops_.top() == this) {
     // This is the inner-most RunLoop, so quit now.
@@ -164,6 +177,11 @@ void RunLoop::QuitWhenIdle() {
         FROM_HERE, BindOnce(&RunLoop::QuitWhenIdle, Unretained(this)));
     return;
   }
+
+  // OUT|IN as in Quit() to link all Quit*() together should there be multiple.
+  TRACE_EVENT_WITH_FLOW0("toplevel.flow", "RunLoop::QuitWhenIdle",
+                         TRACE_ID_LOCAL(this),
+                         TRACE_EVENT_FLAG_FLOW_OUT | TRACE_EVENT_FLAG_FLOW_IN);
 
   quit_when_idle_received_ = true;
 }
@@ -300,8 +318,11 @@ bool RunLoop::BeforeRun() {
 #endif  // DCHECK_IS_ON()
 
   // Allow Quit to be called before Run.
-  if (quit_called_)
+  if (quit_called_) {
+    TRACE_EVENT_WITH_FLOW0("toplevel.flow", "RunLoop_ExitedEarly",
+                           TRACE_ID_LOCAL(this), TRACE_EVENT_FLAG_FLOW_IN);
     return false;
+  }
 
   auto& active_run_loops = delegate_->active_run_loops_;
   active_run_loops.push(this);
@@ -323,6 +344,9 @@ void RunLoop::AfterRun() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   running_ = false;
+
+  TRACE_EVENT_WITH_FLOW0("toplevel.flow", "RunLoop_Exited",
+                         TRACE_ID_LOCAL(this), TRACE_EVENT_FLAG_FLOW_IN);
 
   auto& active_run_loops = delegate_->active_run_loops_;
   DCHECK_EQ(active_run_loops.top(), this);
