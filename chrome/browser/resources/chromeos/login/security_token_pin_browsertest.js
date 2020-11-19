39773e5a074b12ac106a52bdb50d2f6f6cfaf4f2
Renato Silva <rrsilva@google.com>
2020-11-19 18:03:45 +0000

    OOBE: Minor preparations for HTMLImports polyfill
    Prepare parts of the code to work with the HTMLImports polyfill by
    removing some global definitions usage in cr_ui.js and explicitly
    making oobe_types.js a dependency of security_token_pin_browsertest.
    
    Bug: 1111387
    Change-Id: Ifbd2374fc7117f2cefa76c1baa3f96ec813a57a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550221
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/master@{#829292}

--- chrome/browser/resources/chromeos/login/security_token_pin_browsertest.js
+++ chrome/browser/resources/chromeos/login/security_token_pin_browsertest.js
@@ -18,6 +18,10 @@ var PolymerSecurityTokenPinTest = class extends PolymerTest {
   get browsePreload() {
     return 'chrome://oobe/login';
   }
+
+  get extraLibraries() {
+    return super.extraLibraries.concat(['components/oobe_types.js']);
+  }
 };
 
 TEST_F('PolymerSecurityTokenPinTest', 'All', function() {
