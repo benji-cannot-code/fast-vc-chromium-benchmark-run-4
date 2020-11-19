c0f06e25225373a5aafeff87f1b33bbfcd49a2bc
Kelvin Jiang <kelvinjiang@chromium.org>
2020-11-19 17:39:34 +0000

    [DNR] Release global rules allocation on unload (in some cases)
    This CL releases an extension's allocation when it is unloaded for the
    following reasons:
     - when it's blocklisted
     - when it's disabled by policy
     - when it's disabled for malware
    
    Rationale on why certain UnloadedExtensionReason/DisableReason will
    cause the allocation to be released (or not) are detailed in the doc
    (internal):
    
    docs.google.com/document/d/1oGpntLGf9myZ6lI4XWJNMPaZX1_zooJni6ROsqidC6A
    
    Bug: 983299
    Change-Id: I2523ee9794a24bd1ffa320b04ff7941842781969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545735
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829279}

--- extensions/browser/api/declarative_net_request/rules_monitor_service.cc
+++ extensions/browser/api/declarative_net_request/rules_monitor_service.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/api/declarative_net_request/ruleset_matcher.h"
 #include "extensions/browser/api/web_request/permission_helper.h"
 #include "extensions/browser/api/web_request/web_request_api.h"
+#include "extensions/browser/disable_reason.h"
 #include "extensions/browser/extension_file_task_runner.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_prefs_factory.h"
@@ -63,6 +64,24 @@ bool RulesetInfoCompareByID(const RulesetInfo& lhs, const RulesetInfo& rhs) {
 
 void LogLoadRulesetResult(LoadRulesetResult result) {
   UMA_HISTOGRAM_ENUMERATION(kLoadRulesetResultHistogram, result);
+}
+
+// Returns whether the extension's allocation should be released. This would
+// return true for cases where we expect the extension to be unloaded for a
+// while.
+bool ShouldReleaseAllocationOnUnload(const ExtensionPrefs* prefs,
+                                     const ExtensionId& extension_id,
+                                     UnloadedExtensionReason reason) {
+  if (reason == UnloadedExtensionReason::DISABLE) {
+    static constexpr int kReleaseAllocationDisableReasons =
+        disable_reason::DISABLE_BLOCKED_BY_POLICY |
+        disable_reason::DISABLE_REMOTELY_FOR_MALWARE;
+
+    return (prefs->GetDisableReasons(extension_id) &
+            kReleaseAllocationDisableReasons) != 0;
+  }
+
+  return reason == UnloadedExtensionReason::BLOCKLIST;
 }
 
 }  // namespace
@@ -353,13 +372,16 @@ void RulesMonitorService::OnExtensionUnloaded(
     UnloadedExtensionReason reason) {
   DCHECK_EQ(context_, browser_context);
 
-  // If the extension is unloaded for any reason other than an update, the
-  // unused rule allocation should not be kept for this extension the next time
-  // its rulesets are loaded, as it is no longer "the first load after an
-  // update".
-  if (base::FeatureList::IsEnabled(kDeclarativeNetRequestGlobalRules) &&
-      reason != UnloadedExtensionReason::UPDATE) {
-    prefs_->SetDNRKeepExcessAllocation(extension->id(), false);
+  if (base::FeatureList::IsEnabled(kDeclarativeNetRequestGlobalRules)) {
+    // If the extension is unloaded for any reason other than an update, the
+    // unused rule allocation should not be kept for this extension the next
+    // time its rulesets are loaded, as it is no longer "the first load after an
+    // update".
+    if (reason != UnloadedExtensionReason::UPDATE)
+      prefs_->SetDNRKeepExcessAllocation(extension->id(), false);
+
+    if (ShouldReleaseAllocationOnUnload(prefs_, extension->id(), reason))
+      global_rules_tracker_.ClearExtensionAllocation(extension->id());
   }
 
   // Return early if the extension does not have an active indexed ruleset.
