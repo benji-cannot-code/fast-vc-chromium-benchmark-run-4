7f6a4bcb15054abaed64b49d4c3854a3926df8ec
Yuki Shiino <yukishiino@chromium.org>
2020-11-19 03:02:47 +0000

    base/allocator: Enable PAMemoryReclaimer for PA-Everywhere
    Runs PartitionAllocMemoryReclaimer for the PartitionRoot used by
    ShimMalloc, etc. (partition alloc as malloc).
    
    Change-Id: I636b7b2ca4fdda7de7b3a178e6193dea7c0605cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532476
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829063}

--- base/allocator/allocator_shim_default_dispatch_to_partition_alloc.cc
+++ base/allocator/allocator_shim_default_dispatch_to_partition_alloc.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/allocator_shim_default_dispatch_to_partition_alloc.h"
 
 #include "base/allocator/allocator_shim_internals.h"
+#include "base/allocator/partition_allocator/memory_reclaimer.h"
 #include "base/allocator/partition_allocator/partition_alloc.h"
 #include "base/allocator/partition_allocator/partition_alloc_constants.h"
 #include "base/allocator/partition_allocator/partition_alloc_features.h"
@@ -225,6 +226,17 @@ ThreadSafePartitionRoot* PartitionAllocMalloc::AlignedAllocator() {
 
 namespace base {
 namespace allocator {
+
+void EnablePartitionAllocMemoryReclaimer() {
+  // Allocator() and AlignedAllocator() do not register their PartitionRoots to
+  // the memory reclaimer because the memory reclaimer allocates memory.  Thus,
+  // the registration to the memory reclaimer should be done sometime later.
+  // This function will be called sometime appropriate after PartitionRoots are
+  // initialized.
+  PartitionAllocMemoryReclaimer::Instance()->RegisterPartition(Allocator());
+  PartitionAllocMemoryReclaimer::Instance()->RegisterPartition(
+      AlignedAllocator());
+}
 
 void EnablePCScanIfNeeded() {
   if (!features::IsPartitionAllocPCScanEnabled())
