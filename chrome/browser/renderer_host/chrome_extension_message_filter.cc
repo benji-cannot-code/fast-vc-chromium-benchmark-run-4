bd6ec04e78a44b6d4aac6788438c6745421a2011
Tal Pressman <talp@chromium.org>
2020-11-19 06:14:31 +0000

    Remove unneeded #include render_process_host.h
    Change-Id: I388d9ec4f1846485705c8aa8ddf6c33a9ab49c3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547232
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Tal Pressman <talp@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829110}

--- chrome/browser/renderer_host/chrome_extension_message_filter.cc
+++ chrome/browser/renderer_host/chrome_extension_message_filter.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "content/public/browser/browser_task_traits.h"
-#include "content/public/browser/render_process_host.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/extension_messages.h"
