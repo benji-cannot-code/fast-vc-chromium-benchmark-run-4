ec14980b2d1779b3b17e53d8601afacae082acf7
Jeffrey Kardatzke <jkardatzke@google.com>
2020-11-19 04:13:00 +0000

    VaapiVideoDecoder changes for protected playback
    This adds a few things to the VaapiVideoDecoder for
    protected content.
    -Accepts encrypted/CDM configurations in Initialize
    -Registers a callback with the CDM to get notified
     when new decryptions keys are available
    -When in protected mode, configures VaapiWrapper that way
    -Adds a new kWaitingForProtected state when it is waiting
     for keys, hw config, full sample decryption.
    -Callback to be used by accelerators to notify when
     protected decode should resume
    
    BUG=b:153111783,b:155509236
    TEST=Builds, protected playback works w/ full set of changes
    
    Change-Id: Ibd52ddd90af732d5ae736989c2008f4f4fdcd38f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2535578
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Jao-ke Chin-Lee <jchinlee@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829083}

--- media/gpu/vaapi/vaapi_video_decoder.h
+++ media/gpu/vaapi/vaapi_video_decoder.h
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
+#include "build/chromeos_buildflags.h"
+#include "media/base/callback_registry.h"
 #include "media/base/cdm_context.h"
 #include "media/base/status.h"
 #include "media/base/video_codecs.h"
@@ -89,14 +91,16 @@ class VaapiVideoDecoder : public DecoderInterface,
   };
 
   enum class State {
-    kUninitialized,       // not initialized yet or initialization failed.
-    kWaitingForInput,     // waiting for input buffers.
-    kWaitingForOutput,    // waiting for output buffers.
-    kDecoding,            // decoding buffers.
-    kChangingResolution,  // need to change resolution, waiting for pipeline to
-                          // be flushed.
-    kResetting,           // resetting decoder.
-    kError,               // decoder encountered an error.
+    kUninitialized,        // not initialized yet or initialization failed.
+    kWaitingForInput,      // waiting for input buffers.
+    kWaitingForOutput,     // waiting for output buffers.
+    kWaitingForProtected,  // waiting on something related to protected content,
+                           // either setup, full sample parsing or key loading.
+    kDecoding,             // decoding buffers.
+    kChangingResolution,   // need to change resolution, waiting for pipeline to
+                           // be flushed.
+    kResetting,            // resetting decoder.
+    kError,                // decoder encountered an error.
   };
 
   VaapiVideoDecoder(
@@ -120,6 +124,9 @@ class VaapiVideoDecoder : public DecoderInterface,
   void ReleaseVideoFrame(VASurfaceID surface_id);
   // Callback for |frame_pool_| to notify of available resources.
   void NotifyFrameAvailable();
+  // Callback from accelerator to indicate the protected state has been updated
+  // so we can proceed or fail.
+  void ProtectedSessionUpdate(bool success);
 
   // Flushes |decoder_|, blocking until all pending decode tasks have been
   // executed and all frames have been output.
@@ -133,6 +140,9 @@ class VaapiVideoDecoder : public DecoderInterface,
 
   // Change the current |state_| to the specified |state|.
   void SetState(State state);
+
+  // Callback for the CDM to notify |this|.
+  void OnCdmContextEvent(CdmContext::Event event);
 
   // The video decoder's state.
   State state_ = State::kUninitialized;
@@ -175,6 +185,12 @@ class VaapiVideoDecoder : public DecoderInterface,
   // TODO(crbug.com/1040291): remove this keep-alive when using SharedImages.
   base::small_map<std::map<gfx::GpuMemoryBufferId, scoped_refptr<VASurface>>>
       allocated_va_surfaces_;
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // To keep the CdmContext event callback registered.
+  std::unique_ptr<CallbackRegistration> cdm_event_cb_registration_;
+#endif
+
+  CdmContext* cdm_context_ = nullptr;  // Not owned.
 
   // Platform and codec specific video decoder.
   std::unique_ptr<AcceleratedVideoDecoder> decoder_;
