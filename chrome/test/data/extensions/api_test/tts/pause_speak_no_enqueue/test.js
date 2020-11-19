d7a09f8263b8d0805ccb9a917d6c06734a69bb9a
David Tseng <dtseng@google.com>
2020-11-19 16:44:19 +0000

    Misc fix / cleanup to c/b/s/tts_controller_impl.cc
    - in SpeakOrEnqueue, for tts platforms that were not yet ready, we were
      enqueueing the utterance then immediately clearing it if the utterance was
      !CanBeEnqueued. We should clear the queue, then add the utterance to be spoken
      later.
    - cleanup for style/readability
    
    R=dmazzoni@chromium.org
    
    Change-Id: Ifb2311792c6dca042181871797de8aa0c67ba261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2543374
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829254}

--- chrome/test/data/extensions/api_test/tts/pause_speak_no_enqueue/test.js
+++ chrome/test/data/extensions/api_test/tts/pause_speak_no_enqueue/test.js
@@ -6,38 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TTS api test for Chrome.
 // browser_tests.exe --gtest_filter="TtsApiTest.*"
 
-chrome.test.runTests([
-  function testPauseCancel() {
-    var callbacks = 0;
-    chrome.tts.pause();
-    chrome.tts.speak(
-        'text 1',
-        {
-         'enqueue': true,
-         'onEvent': function(event) {
-            chrome.test.assertEq('cancelled', event.type);
-            callbacks++;
-         }
-        },
-        function() {
-          chrome.test.assertNoLastError();
-          callbacks++;
-        });
-    chrome.tts.speak(
-        'text 2',
-        {
-         'enqueue': false,
-         'onEvent': function(event) {
-           chrome.test.assertEq('cancelled', event.type);
-           callbacks++;
-           if (callbacks == 4) {
-             chrome.test.succeed();
-           }
-         }
-        },
-        function() {
-          chrome.test.assertNoLastError();
-          callbacks++;
-        });
-  }
-]);
+chrome.test.runTests([function testPauseCancel() {
+  chrome.tts.pause();
+  chrome.tts.speak('text 1', {'enqueue': true});
+  chrome.tts.speak('text 2', {'enqueue': false}, function() {
+    chrome.test.assertNoLastError();
+    chrome.test.succeed();
+  });
+  chrome.tts.resume();
+}]);
