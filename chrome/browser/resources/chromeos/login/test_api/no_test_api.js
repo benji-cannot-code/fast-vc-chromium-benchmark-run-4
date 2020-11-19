3f52bcc35b96ff60af505286fdfcabd12f529e66
Yunke Zhou <yunkez@google.com>
2020-11-19 05:27:22 +0000

    Oobe: add test API for tast tests
    Add testing utils so Oobe tast tests could use the methods here. This fixes the problem for oobe_smoke.go test where eula screen is not shown in non-branded build.
    
    Bug: 1145697
    Change-Id: Id39a2b522699d96d43d5c849503b3221acac8008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527055
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829100}

---
+++ chrome/browser/resources/chromeos/login/test_api/no_test_api.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/*
+ * OOBE testing API is not loaded. Please use --enable-oobe-test-api
+ * command line switch to load.
+ */
---
+++ chrome/browser/ui/webui/chromeos/login/testapi/oobe_test_api_handler.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/chromeos/login/testapi/oobe_test_api_handler.h"
+
+#include "build/branding_buildflags.h"
+
+namespace chromeos {
+
+OobeTestAPIHandler::OobeTestAPIHandler(JSCallsContainer* js_calls_container)
+    : BaseWebUIHandler(js_calls_container) {
+  DCHECK(js_calls_container);
+}
+
+OobeTestAPIHandler::~OobeTestAPIHandler() {}
+
+void OobeTestAPIHandler::DeclareLocalizedValues(
+    ::login::LocalizedValuesBuilder* builder) {}
+
+void OobeTestAPIHandler::Initialize() {}
+
+void OobeTestAPIHandler::GetAdditionalParameters(base::DictionaryValue* dict) {
+  dict->SetBoolean("isBrandedBuild", BUILDFLAG(GOOGLE_CHROME_BRANDING));
+}
+
+}  // namespace chromeos
