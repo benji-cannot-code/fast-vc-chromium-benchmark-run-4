38ff145743e5eff92d362b2443bcc68278a683f1
Markus Handell <handellm@google.com>
2020-11-20 18:27:21 +0000

    IOSurfaceSetColorSpace: save 8% by handling REC709.
    The IOSurfaceSetColorSpace function doesn't recognize REC709
    which is emitted by the WebRTC VP9 libvpx decoder. This causes
    an 8%/CPU load in downstream project due to the setup of
    an ICC profile for the case.
    
    Fix this by tagging the IOSurface with kCGColorSpaceSRGB
    when encountering REC709.
    
    Tested: Manual test with https://appr.tc/?vsc=VP9&vrc=VP9 - no visual impact.
    Bug: b:173124811
    Change-Id: I58c6dbc70b92ecf33bdb23d1b78536be1ee9f219
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550554
    Commit-Queue: Markus Handell <handellm@google.com>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829722}

--- ui/gfx/mac/io_surface.cc
+++ ui/gfx/mac/io_surface.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/trace_event/trace_event.h"
 #include "ui/gfx/buffer_format_util.h"
+#include "ui/gfx/color_space.h"
 #include "ui/gfx/icc_profile.h"
 
 namespace gfx {
@@ -134,7 +135,8 @@ bool IOSurfaceSetColorSpace(IOSurfaceRef io_surface,
   // Prefer using named spaces.
   CFStringRef color_space_name = nullptr;
   if (__builtin_available(macos 10.12, *)) {
-    if (color_space == ColorSpace::CreateSRGB()) {
+    if (color_space == ColorSpace::CreateSRGB() ||
+        color_space == ColorSpace::CreateREC709()) {
       color_space_name = kCGColorSpaceSRGB;
     } else if (color_space == ColorSpace::CreateDisplayP3D65()) {
       color_space_name = kCGColorSpaceDisplayP3;
@@ -200,6 +202,7 @@ bool IOSurfaceSetColorSpace(IOSurfaceRef io_surface,
   base::ScopedCFTypeRef<CFDataRef> cf_data_icc_profile(CFDataCreate(
       nullptr, reinterpret_cast<const UInt8*>(icc_profile_data.data()),
       icc_profile_data.size()));
+
   IOSurfaceSetValue(io_surface, CFSTR("IOSurfaceColorSpace"),
                     cf_data_icc_profile);
   return true;
