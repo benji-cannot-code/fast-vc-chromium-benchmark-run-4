f49b474cd8b7a3a8fed4d6e78b9af670a0d0f732
Phillis Tang <phillis@chromium.org>
2020-11-20 00:13:56 +0000

    PWA: notify In-Product Help backend when app is installed
    Notify the event to IPH backend that user decided to install the web app
    after IPH is shown. Also defines a test field trial config for this
    feature to be used by testing. There are two fields in field trial
    config that are required by IPH backend: "event_used" and
    "event_trigger". They are used to apply trigger logic to only show IPH
    if user hasn't seen IPH and hasn't used the feature that IPH is trying
    to educate. The event name for event_used needs to be defined in
    event_constants, and NotifyEvent need to be called for it. For
    "event_trigger", this is already handled by FeaturePromoController.
    For PWA install IPH, these two fields will be set to comparator:any,
    meaning that we don't use these triggering criteria since custom
    triggering criteria is defined for this feature.
    
    
    Bug: 1150521
    Change-Id: I8af603b4d6657ffe161bd89205c1d4a977ef28c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547635
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829441}

--- chrome/browser/ui/views/web_apps/pwa_confirmation_bubble_view.h
+++ chrome/browser/ui/views/web_apps/pwa_confirmation_bubble_view.h
@@ -19,6 +19,10 @@ namespace views {
 class Checkbox;
 }
 
+namespace feature_engagement {
+class Tracker;
+}
+
 // PWAConfirmationBubbleView provides a bubble dialog for accepting or rejecting
 // the installation of a PWA (Progressive Web App) anchored off the PWA install
 // icon in the omnibox.
@@ -32,7 +36,8 @@ class PWAConfirmationBubbleView : public LocationBarBubbleDelegateView {
                             std::unique_ptr<WebApplicationInfo> web_app_info,
                             chrome::AppInstallationAcceptanceCallback callback,
                             chrome::PwaInProductHelpState iph_state,
-                            PrefService* prefs);
+                            PrefService* prefs,
+                            feature_engagement::Tracker* tracker);
   ~PWAConfirmationBubbleView() override;
 
   // LocationBarBubbleDelegateView:
@@ -52,6 +57,7 @@ class PWAConfirmationBubbleView : public LocationBarBubbleDelegateView {
 
   chrome::PwaInProductHelpState iph_state_;
   PrefService* prefs_;
+  feature_engagement::Tracker* tracker_;
 
   DISALLOW_COPY_AND_ASSIGN(PWAConfirmationBubbleView);
 };
