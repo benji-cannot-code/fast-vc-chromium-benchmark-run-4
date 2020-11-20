5842f36833dd202e61382735bc68ca5a08ce23bf
Sigurd Schneider <sigurds@chromium.org>
2020-11-20 15:22:32 +0000

    Address TODO in DedicatedWorkerServiceImpl
    This CL refactors DedicatedWorkerServiceImpl to keep a pointer to the
    DedicatedWorkerHost instead of the DedicatedWorkerInfo.
    
    Bug: chromium:1145158
    Change-Id: I081c77c04fa4741dd244ca1939cac6916a7f15c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2517579
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829661}

--- content/browser/worker_host/dedicated_worker_service_impl.cc
+++ content/browser/worker_host/dedicated_worker_service_impl.cc
@@ -10,23 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-DedicatedWorkerServiceImpl::DedicatedWorkerInfo::DedicatedWorkerInfo(
-    int worker_process_id,
-    GlobalFrameRoutingId ancestor_render_frame_host_id,
-    DedicatedWorkerHost* host)
-    : worker_process_id(worker_process_id),
-      ancestor_render_frame_host_id(ancestor_render_frame_host_id),
-      dedicated_worker_host(host) {}
-
-DedicatedWorkerServiceImpl::DedicatedWorkerInfo::DedicatedWorkerInfo(
-    const DedicatedWorkerInfo& info) = default;
-DedicatedWorkerServiceImpl::DedicatedWorkerInfo&
-DedicatedWorkerServiceImpl::DedicatedWorkerInfo::operator=(
-    const DedicatedWorkerInfo& info) = default;
-
-DedicatedWorkerServiceImpl::DedicatedWorkerInfo::~DedicatedWorkerInfo() =
-    default;
-
 DedicatedWorkerServiceImpl::DedicatedWorkerServiceImpl() = default;
 
 DedicatedWorkerServiceImpl::~DedicatedWorkerServiceImpl() = default;
@@ -40,43 +23,37 @@ void DedicatedWorkerServiceImpl::RemoveObserver(Observer* observer) {
 }
 
 void DedicatedWorkerServiceImpl::EnumerateDedicatedWorkers(Observer* observer) {
-  for (const auto& kv : dedicated_worker_infos_) {
+  for (const auto& kv : dedicated_worker_hosts_) {
     const blink::DedicatedWorkerToken& dedicated_worker_token = kv.first;
-    const DedicatedWorkerInfo& dedicated_worker_info = kv.second;
+    DedicatedWorkerHost* host = kv.second;
 
-    observer->OnWorkerCreated(
-        dedicated_worker_token, dedicated_worker_info.worker_process_id,
-        dedicated_worker_info.ancestor_render_frame_host_id);
-    if (dedicated_worker_info.final_response_url) {
-      observer->OnFinalResponseURLDetermined(
-          dedicated_worker_token, *dedicated_worker_info.final_response_url);
+    observer->OnWorkerCreated(dedicated_worker_token,
+                              host->GetProcessHost()->GetID(),
+                              host->GetAncestorRenderFrameHostId());
+    auto& maybe_url = host->GetFinalResponseURL();
+    if (maybe_url) {
+      observer->OnFinalResponseURLDetermined(dedicated_worker_token,
+                                             *maybe_url);
     }
   }
 }
 
 void DedicatedWorkerServiceImpl::NotifyWorkerCreated(
-    const blink::DedicatedWorkerToken& worker_token,
-    int worker_process_id,
-    GlobalFrameRoutingId ancestor_render_frame_host_id,
     DedicatedWorkerHost* host) {
   bool inserted =
-      dedicated_worker_infos_
-          .emplace(worker_token,
-                   DedicatedWorkerInfo(worker_process_id,
-                                       ancestor_render_frame_host_id, host))
-          .second;
+      dedicated_worker_hosts_.emplace(host->GetToken(), host).second;
   DCHECK(inserted);
 
   for (Observer& observer : observers_) {
-    observer.OnWorkerCreated(worker_token, worker_process_id,
-                             ancestor_render_frame_host_id);
+    observer.OnWorkerCreated(host->GetToken(), host->GetProcessHost()->GetID(),
+                             host->GetAncestorRenderFrameHostId());
   }
 }
 
 void DedicatedWorkerServiceImpl::NotifyBeforeWorkerDestroyed(
     const blink::DedicatedWorkerToken& dedicated_worker_token,
     GlobalFrameRoutingId ancestor_render_frame_host_id) {
-  size_t removed = dedicated_worker_infos_.erase(dedicated_worker_token);
+  size_t removed = dedicated_worker_hosts_.erase(dedicated_worker_token);
   DCHECK_EQ(1u, removed);
 
   for (Observer& observer : observers_) {
@@ -88,10 +65,8 @@ void DedicatedWorkerServiceImpl::NotifyBeforeWorkerDestroyed(
 void DedicatedWorkerServiceImpl::NotifyWorkerFinalResponseURLDetermined(
     const blink::DedicatedWorkerToken& dedicated_worker_token,
     const GURL& url) {
-  auto it = dedicated_worker_infos_.find(dedicated_worker_token);
-  DCHECK(it != dedicated_worker_infos_.end());
-
-  it->second.final_response_url = url;
+  auto it = dedicated_worker_hosts_.find(dedicated_worker_token);
+  DCHECK(it != dedicated_worker_hosts_.end());
 
   for (Observer& observer : observers_)
     observer.OnFinalResponseURLDetermined(dedicated_worker_token, url);
@@ -99,16 +74,17 @@ void DedicatedWorkerServiceImpl::NotifyWorkerFinalResponseURLDetermined(
 
 bool DedicatedWorkerServiceImpl::HasToken(
     const blink::DedicatedWorkerToken& worker_token) const {
-  return dedicated_worker_infos_.count(worker_token);
+  return dedicated_worker_hosts_.count(worker_token);
 }
 
 DedicatedWorkerHost*
 DedicatedWorkerServiceImpl::GetDedicatedWorkerHostFromToken(
     const blink::DedicatedWorkerToken& dedicated_worker_token) const {
-  auto it = dedicated_worker_infos_.find(dedicated_worker_token);
-  if (it == dedicated_worker_infos_.end())
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  auto it = dedicated_worker_hosts_.find(dedicated_worker_token);
+  if (it == dedicated_worker_hosts_.end())
     return nullptr;
-  return it->second.dedicated_worker_host;
+  return it->second;
 }
 
 }  // namespace content
