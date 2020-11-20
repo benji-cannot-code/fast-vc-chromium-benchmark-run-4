f520f9b360df8ee41702a09eaf99b117b3ac1cbb
Nate Chapin <japhet@chromium.org>
2020-11-20 01:08:32 +0000

    Make BucketManager a NavigatorBase
    NavigatorBase is a newly-added base class for Navigator and
    WorkerNavigator. Making BucketManager a Supplement of NavigatorBase
    allows a single class to handle all of the work of exposing the
    navigator.storageBuckets getter for both windows and workers, and
    greatly reduces boilerplate.
    
    Bug: 1147612
    Change-Id: Ib941f4298f05ad95efcb78ad33033453aee470c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551460
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829460}

--- third_party/blink/renderer/modules/buckets/bucket_manager.h
+++ third_party/blink/renderer/modules/buckets/bucket_manager.h
@@ -9,17 +9,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
 
 class ExceptionState;
+class NavigatorBase;
 
-class MODULES_EXPORT BucketManager final : public ScriptWrappable {
+class MODULES_EXPORT BucketManager final : public ScriptWrappable,
+                                           public Supplement<NavigatorBase> {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  BucketManager();
+  static const char kSupplementName[];
+
+  // Web-exposed as navigator.storageBuckets
+  static BucketManager* storageBuckets(NavigatorBase&, ExceptionState&);
+
+  explicit BucketManager(NavigatorBase&);
   ~BucketManager() override = default;
 
   ScriptPromise openOrCreate(ScriptState* script_state,
