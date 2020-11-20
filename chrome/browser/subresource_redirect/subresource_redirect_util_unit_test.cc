cef215824c45cd661329b3cbeb3d20f2658159c0
rajendrant <rajendrant@chromium.org>
2020-11-20 21:19:13 +0000

    Pass the full URL to robots rules fetcher
    The LitePages robots rules fetcher sends the origin in "u" request
    parameter. Instead the full robots.txt URL should be sent in that
    parameter.
    
    Bug: 1147565
    Change-Id: I0df86b28d1c4a8ee7d9053b2040f3941d894b9be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2552577
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829790}

--- chrome/browser/subresource_redirect/subresource_redirect_util_unit_test.cc
+++ chrome/browser/subresource_redirect/subresource_redirect_util_unit_test.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/scoped_feature_list.h"
@@ -33,9 +34,10 @@ TEST(SubresourceRedirectUtilTest, GetRobotsServerURL) {
 
   for (auto* origin :
        {"https://foo.com/", "https://www.foo.com/", "http://foo.com/"}) {
-    EXPECT_EQ(GURL("https://litepages.googlezip.net/robots?u=" +
-                   EscapeURLForQueryParam(origin)),
-              GetRobotsServerURL(url::SchemeHostPort(GURL(origin))));
+    EXPECT_EQ(
+        GURL("https://litepages.googlezip.net/robots?u=" +
+             EscapeURLForQueryParam(base::StrCat({origin, "robots.txt"}))),
+        GetRobotsServerURL(url::SchemeHostPort(GURL(origin))));
   }
 }
 
@@ -50,9 +52,10 @@ TEST(SubresourceRedirectUtilTest, GetRobotsServerURL_ModifiedLitePagesOrigin) {
 
   for (auto* origin :
        {"https://foo.com/", "https://www.foo.com/", "http://foo.com/"}) {
-    EXPECT_EQ(GURL("https://modified.litepages.com/robots?u=" +
-                   EscapeURLForQueryParam(origin)),
-              GetRobotsServerURL(url::SchemeHostPort(GURL(origin))));
+    EXPECT_EQ(
+        GURL("https://modified.litepages.com/robots?u=" +
+             EscapeURLForQueryParam(base::StrCat({origin, "robots.txt"}))),
+        GetRobotsServerURL(url::SchemeHostPort(GURL(origin))));
   }
 }
 
