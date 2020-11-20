ac89bd94adda9da13b6206656338a5568e39d616
David Van Cleve <davidvc@chromium.org>
2020-11-20 05:07:59 +0000

    Referrers: Remove ForceLegacyDefaultReferrerPolicy piping
    We've rolled out the new default referrer policy of
    strict-origin-when-cross-origin to 100% and, in the parent CL, removed
    the enterprise policy that emulates the prior default policy. This CL
    removes the remaining piping that was used to propagate the enterprise
    policy's effect to Blink.
    
    Bug: 1150018
    Change-Id: Ie2ef9a2137201a065f54325e5ad518efe5645ca4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545063
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829543}

--- third_party/blink/common/loader/referrer_utils.cc
+++ third_party/blink/common/loader/referrer_utils.cc
@@ -11,28 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/switches.h"
 
-namespace {
-
-// Using an atomic is necessary because this code is called from both the
-// browser and the renderer (so that access is not on a single sequence when in
-// single-process mode), and because it is called from multiple threads within
-// the renderer.
-bool ReadModifyWriteForceLegacyPolicyFlag(
-    base::Optional<bool> maybe_new_value) {
-  // Default to false in the browser process (it is not expected
-  // that the browser will be provided this switch).
-  // The value is propagated to other processes through the command line.
-  DCHECK(base::CommandLine::InitializedForCurrentProcess());
-  static std::atomic<bool> value(
-      base::CommandLine::ForCurrentProcess()->HasSwitch(
-          blink::switches::kForceLegacyDefaultReferrerPolicy));
-  if (!maybe_new_value.has_value())
-    return value;
-  return value.exchange(*maybe_new_value);
-}
-
-}  // namespace
-
 namespace blink {
 
 network::mojom::ReferrerPolicy ReferrerUtils::NetToMojoReferrerPolicy(
@@ -76,20 +54,11 @@ network::mojom::ReferrerPolicy ReferrerUtils::MojoReferrerPolicyResolveDefault(
   return referrer_policy;
 }
 
-void ReferrerUtils::SetForceLegacyDefaultReferrerPolicy(bool force) {
-  ReadModifyWriteForceLegacyPolicyFlag(force);
-}
-
-bool ReferrerUtils::ShouldForceLegacyDefaultReferrerPolicy() {
-  return ReadModifyWriteForceLegacyPolicyFlag(base::nullopt);
-}
-
 // TODO(crbug.com/1016541) Once the pertinent enterprise policy has
 // been removed in M88, update this to remove the global.
 bool ReferrerUtils::IsReducedReferrerGranularityEnabled() {
   return base::FeatureList::IsEnabled(
-             blink::features::kReducedReferrerGranularity) &&
-         !ShouldForceLegacyDefaultReferrerPolicy();
+      blink::features::kReducedReferrerGranularity);
 }
 
 }  // namespace blink
