231bbe3d7eb5ee39b4f6b2da7689b6377b7cc50b
Egor Pasko <pasko@chromium.org>
2020-11-20 08:51:16 +0000

    base: remove Android NDK workaround for PRIxPTR
    Remove the workaround for a bug that was fixed in the NDK in 2013.
    
    Bug: None
    Change-Id: I6b1e25c53c9b6d86604c3ae796d36ca5e2e50937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550801
    Commit-Queue: Albert J. Wong <ajwong@chromium.org>
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829590}

--- base/debug/proc_maps_linux.cc
+++ base/debug/proc_maps_linux.cc
@@ -18,15 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <inttypes.h>
 #endif
 
-#if defined(OS_ANDROID) && !defined(__LP64__)
-// In 32-bit mode, Bionic's inttypes.h defines PRI/SCNxPTR as an
-// unsigned long int, which is incompatible with Bionic's stdint.h
-// defining uintptr_t as an unsigned int:
-// https://code.google.com/p/android/issues/detail?id=57218
-#undef SCNxPTR
-#define SCNxPTR "x"
-#endif
-
 namespace base {
 namespace debug {
 
