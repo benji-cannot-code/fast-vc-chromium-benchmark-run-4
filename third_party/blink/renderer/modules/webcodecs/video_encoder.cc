9033f6a2177917ea3a3683a6b623d4c8e5e8a8d1
Dale Curtis <dalecurtis@chromium.org>
2020-11-20 21:51:28 +0000

    Allow zero copy WebCodecs encoding on ChromeOS/macOS for GMB frames.
    GpuMemoryBuffer frames may be passed to the hardware encoders without
    conversion to a software frame. This enables more performant encoding
    on low end devices.
    
    This defers encoder configuration until the first frame is available so
    that we can properly configure the VideoPixelFormat and StorageType for
    ChromeOS zero copy mode.
    
    R=eugene
    
    Bug: 1150069
    Test: https://storage.googleapis.com/dalecurtis/wc-camera-test.html
    
    Change-Id: I18799d536c8e8e53b7d61233c699dd374b3de9b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545031
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829809}

--- third_party/blink/renderer/modules/webcodecs/video_encoder.cc
+++ third_party/blink/renderer/modules/webcodecs/video_encoder.cc
@@ -71,19 +71,22 @@ std::unique_ptr<media::VideoEncoder> CreateAcceleratedVideoEncoder(
       continue;
 
     if (supported_profile.min_resolution.width() > options.width ||
-        supported_profile.min_resolution.height() > options.height)
+        supported_profile.min_resolution.height() > options.height) {
       continue;
+    }
 
     if (supported_profile.max_resolution.width() < options.width ||
-        supported_profile.max_resolution.height() < options.height)
+        supported_profile.max_resolution.height() < options.height) {
       continue;
+    }
 
     double max_supported_framerate =
         double{supported_profile.max_framerate_numerator} /
         supported_profile.max_framerate_denominator;
     if (options.framerate.has_value() &&
-        options.framerate.value() > max_supported_framerate)
+        options.framerate.value() > max_supported_framerate) {
       continue;
+    }
 
     found_supported_profile = true;
     break;
@@ -320,6 +323,7 @@ std::unique_ptr<media::VideoEncoder> VideoEncoder::CreateMediaVideoEncoder(
     case AccelerationPreference::kRequire: {
       auto result =
           CreateAcceleratedVideoEncoder(config.profile, config.options);
+      is_hw_accelerated_ = !!result;
       if (result)
         UpdateEncoderLog("AcceleratedVideoEncoder", true);
       return result;
@@ -327,6 +331,7 @@ std::unique_ptr<media::VideoEncoder> VideoEncoder::CreateMediaVideoEncoder(
     case AccelerationPreference::kAllow:
       if (auto result =
               CreateAcceleratedVideoEncoder(config.profile, config.options)) {
+        is_hw_accelerated_ = true;
         UpdateEncoderLog("AcceleratedVideoEncoder", true);
         return result;
       }
@@ -346,6 +351,7 @@ std::unique_ptr<media::VideoEncoder> VideoEncoder::CreateMediaVideoEncoder(
         default:
           return nullptr;
       }
+      is_hw_accelerated_ = false;
       if (!result)
         return nullptr;
       return std::make_unique<media::OffloadingVideoEncoder>(std::move(result));
@@ -585,7 +591,7 @@ void VideoEncoder::ProcessEncode(Request* request) {
   };
 
   scoped_refptr<media::VideoFrame> frame = request->frame->frame();
-  if (frame->storage_type() == media::VideoFrame::STORAGE_GPU_MEMORY_BUFFER) {
+  if (frame->HasGpuMemoryBuffer() && !is_hw_accelerated_) {
     frame = ConvertToI420Frame(frame);
     if (!frame) {
       HandleError("Unexpected frame format.",
