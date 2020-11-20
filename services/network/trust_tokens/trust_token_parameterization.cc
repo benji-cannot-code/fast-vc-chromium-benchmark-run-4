2cfd742f043fdf3660ad974cf778747b18c90de5
Steven Valdez <svaldez@chromium.org>
2020-11-20 18:12:14 +0000

    Fix TrustTokenV2 max key configuration.
    This fixes the number of max keys that can be configured in different
    TrustTokenV2 variants.
    
    Bug: 1138432, 1150839
    Change-Id: Ic111c1a6c689409993f76fc1c1e917480310b5fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547042
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: David Van Cleve <davidvc@chromium.org>
    Auto-Submit: Steven Valdez <svaldez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829715}

---
+++ services/network/trust_tokens/trust_token_parameterization.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/network/trust_tokens/trust_token_parameterization.h"
+
+#include "services/network/public/mojom/trust_tokens.mojom-forward.h"
+
+namespace network {
+
+size_t TrustTokenMaxKeysForVersion(mojom::TrustTokenProtocolVersion version) {
+  switch (version) {
+    case mojom::TrustTokenProtocolVersion::kTrustTokenV2Pmb:
+      return 3;
+    case mojom::TrustTokenProtocolVersion::kTrustTokenV2Voprf:
+      return 6;
+  }
+}
+
+}  // namespace network
