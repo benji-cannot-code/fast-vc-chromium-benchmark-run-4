03e4abc210b6b0430a52fcea4f7cc59faa03e0a3
Liquan (Max) Gu <maxlg@chromium.org>
2020-11-20 18:51:45 +0000

    [WebLayer] Clean up triggerPaymentAppUiSkipIfApplicable of continueShow
    * Now that continueShow returns any error to updateWith,
      triggerPaymentAppUiSkipIfApplicable should return the error to
      continueShow instead of handling it itself.
    * Remove isUserGestureShow from continueShow(). isUserGestureShow was
      used by the triggerPaymentAppUiSkipIfApplicable (the trigger method)
      in continueShow when the trigger method was used in continueShow.
      Now that the trigger method has been moved[1] out of continueShow, the
      isUserGestureShow parameter should be removed.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2543131
    
    Bug: 1147092
    
    Change-Id: I428220b0396fbb069f00e3e6dd4c96cd4f6e9465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551589
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829729}

--- components/payments/content/android/java/src/org/chromium/components/payments/PaymentRequestService.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/PaymentRequestService.java
@@ -680,7 +680,11 @@ public class PaymentRequestService
             }
         }
 
-        triggerPaymentAppUiSkipIfApplicable();
+        String error = triggerPaymentAppUiSkipIfApplicable();
+        if (error != null) {
+            onShowFailed(error);
+            return;
+        }
     }
 
     private void onShowFailed(String error) {
@@ -956,7 +960,11 @@ public class PaymentRequestService
             }
         }
 
-        triggerPaymentAppUiSkipIfApplicable();
+        String error = triggerPaymentAppUiSkipIfApplicable();
+        if (error != null) {
+            onShowFailed(error);
+            return;
+        }
     }
 
     // Return the error if failed, null if success.
@@ -966,13 +974,7 @@ public class PaymentRequestService
                 || mIsShowWaitingForUpdatedDetails) {
             return null;
         }
-        String error =
-                mBrowserPaymentRequest.triggerPaymentAppUiSkipIfApplicable(mIsUserGestureShow);
-        if (error != null) {
-            onShowFailed(error);
-            return error;
-        }
-        return null;
+        return mBrowserPaymentRequest.triggerPaymentAppUiSkipIfApplicable(mIsUserGestureShow);
     }
 
     // Implements PaymentDetailsConverter.MethodChecker:
@@ -999,8 +1001,7 @@ public class PaymentRequestService
         mSpec.updateWith(details);
 
         mIsShowWaitingForUpdatedDetails = false;
-        String error = mBrowserPaymentRequest.continueShow(
-                mIsFinishedQueryingPaymentApps, mIsUserGestureShow);
+        String error = mBrowserPaymentRequest.continueShow(mIsFinishedQueryingPaymentApps);
         if (error != null) return error;
         return triggerPaymentAppUiSkipIfApplicable();
     }
