chromium/chromium

commit 657b47a9eef04e784d0946d196e1a286d85774ab

WebLayer: hook up volume control for Media Router
Using the phone's volume button when casting should change the casting
volume.

Bug: 1057099
Change-Id: I50971df97df598922c6da683cbfd376d2e42859f
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2515282
Commit-Queue: Evan Stade <estade@chromium.org>
Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
Cr-Commit-Position: refs/heads/master@{#825152}

components/browser_ui/media/android/BUILD.gn
@@ -24,12 +24,14 @@ android_library("java") {
     "//components/browser_ui/notifications/android:java",
     "//components/embedder_support/android:browser_context_java",
     "//components/favicon/android:java",
+    "//components/media_router/browser/android:media_router_utils_java",
     "//components/url_formatter/android:url_formatter_java",
     "//content/public/android:content_java",
     "//services/media_session/public/cpp/android:media_session_java",
     "//services/media_session/public/mojom:mojom_java",
     "//third_party/android_deps:android_support_v4_java",
     "//third_party/android_deps:androidx_annotation_annotation_java",
+    "//third_party/android_deps:androidx_mediarouter_mediarouter_java",
     "//ui/android:ui_full_java",
     "//url:gurl_java",
   ]

components/media_router/browser/android/BUILD.gn
@@ -11,6 +11,7 @@ android_library("java") {
   resources_package = "org.chromium.components.media_router"
   deps = [
     ":java_resources",
+    ":media_router_utils_java",
     "$google_play_services_package:google_play_services_base_java",
     "$google_play_services_package:google_play_services_basement_java",
     "$google_play_services_package:google_play_services_cast_framework_java",
@@ -70,6 +71,15 @@ android_library("java") {
     "java/src/org/chromium/components/media_router/caf/remoting/StreamPositionExtrapolator.java",
   ]
   annotation_processor_deps = [ "//base/android/jni_generator:jni_processor" ]
+}
+
+android_library("media_router_utils_java") {
+  sources =
+      [ "java/src/org/chromium/components/media_router/MediaRouterUtils.java" ]
+  deps = [
+    "$google_play_services_package:google_play_services_cast_framework_java",
+    "//base:base_java",
+  ]
 }
 
 generate_jni("jni_headers") {

chrome/android/java/src/org/chromium/chrome/browser/media/ui/ChromeMediaNotificationControllerDelegate.java
@@ -11,12 +11,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.os.IBinder;
-import android.support.v4.media.session.MediaSessionCompat;
 import android.util.SparseArray;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
-import androidx.mediarouter.media.MediaRouter;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.chrome.R;
@@ -220,20 +218,6 @@ class ChromeMediaNotificationControllerDelegate implements MediaNotificationCont
     @Override
     public NotificationWrapperBuilder createNotificationWrapperBuilder() {
         return createNotificationWrapperBuilder(mNotificationId);
-    }
-
-    @Override
-    public void onMediaSessionUpdated(MediaSessionCompat session) {
-        try {
-            // Tell the MediaRouter about the session, so that Chrome can control the volume
-            // on the remote cast device (if any).
-            // Pre-MR1 versions of JB do not have the complete MediaRouter APIs,
-            // so getting the MediaRouter instance will throw an exception.
-            MediaRouter.getInstance(getContext()).setMediaSessionCompat(session);
-        } catch (NoSuchMethodError e) {
-            // Do nothing. Chrome can't be casting without a MediaRouter, so there is nothing
-            // to do here.
-        }
     }
 
     @Override

components/browser_ui/media/android/DEPS
@@ -1,6 +1,7 @@
 include_rules = [
   "+components/embedder_support/android",
   "+components/favicon/android",
+  "+components/media_router/browser/android/java/src/org/chromium/components/media_router/MediaRouterUtils.java",
   "+components/url_formatter/android",
   "+content/public/android",
   "+services/media_session/public/cpp/android",

components/browser_ui/media/android/java/src/org/chromium/components/browser_ui/media/MediaNotificationController.java
@@ -23,6 +23,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.mediarouter.media.MediaRouter;
 
 import org.chromium.base.CollectionUtil;
 import org.chromium.base.ContextUtils;
@@ -32,6 +33,7 @@ import org.chromium.components.browser_ui.notifications.NotificationManagerProxy
 import org.chromium.components.browser_ui.notifications.NotificationManagerProxyImpl;
 import org.chromium.components.browser_ui.notifications.NotificationWrapper;
 import org.chromium.components.browser_ui.notifications.NotificationWrapperBuilder;
+import org.chromium.components.media_router.MediaRouterUtils;
 import org.chromium.media_session.mojom.MediaSessionAction;
 import org.chromium.services.media_session.MediaMetadata;
 
@@ -295,9 +297,6 @@ public class MediaNotificationController {
 
         /** Returns a builder suitable as a starting point for creating the notification. */
         NotificationWrapperBuilder createNotificationWrapperBuilder();
-
-        /** Called when the Android MediaSession has been updated. */
-        void onMediaSessionUpdated(MediaSessionCompat session);
 
         /** Called when a notification has been shown and should be logged in UMA. */
         void logNotificationShown(NotificationWrapper notification);
@@ -626,7 +625,8 @@ public class MediaNotificationController {
 
         activateAndroidMediaSession(mMediaNotificationInfo.instanceId);
 
-        mDelegate.onMediaSessionUpdated(mMediaSession);
+        MediaRouter.getInstance(MediaRouterUtils.getContextForCasting())
+                .setMediaSessionCompat(mMediaSession);
 
         mMediaSession.setMetadata(createMetadata());
 

components/media_router/browser/android/java/src/org/chromium/components/media_router/MediaRouterUtils.java
@@ -1,0 +1,33 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.media_router;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.ContextWrapper;
+
+import org.chromium.base.ContextUtils;
+
+/** Utility methods for Media Router. */
+public class MediaRouterUtils {
+    /**
+     * Helper method to return the {@link Context} instance to be used for {@link MediaRouter} and
+     * {@link CastContext}.
+     */
+    public static Context getContextForCasting() {
+        Context context = ContextUtils.getApplicationContext();
+        // The GMS Cast framework assumes the passed {@link Context} returns an instance of {@link
+        // Application} from {@link getApplicationContext()}, so we make sure to remove any
+        // wrappers.
+        while (!(context.getApplicationContext() instanceof Application)) {
+            if (context instanceof ContextWrapper) {
+                context = ((ContextWrapper) context).getBaseContext();
+            } else {
+                return null;
+            }
+        }
+        return context;
+    }
+}

components/media_router/browser/android/java/src/org/chromium/components/media_router/caf/CastUtils.java
@@ -4,30 +4,15 @@
 
 package org.chromium.components.media_router.caf;
 
-import android.app.Application;
-import android.content.Context;
-import android.content.ContextWrapper;
-
 import com.google.android.gms.cast.framework.CastContext;
 
-import org.chromium.base.ContextUtils;
+import org.chromium.components.media_router.MediaRouterUtils;
 
 /** Utility methods for Cast. */
 public class CastUtils {
     /** Helper method to return the {@link CastContext} instance. */
     public static CastContext getCastContext() {
-        Context context = ContextUtils.getApplicationContext();
-        // The GMS Cast framework assumes the passed {@link Context} returns an instance of {@link
-        // Application} from {@link getApplicationContext()}, so we make sure to remove any
-        // wrappers.
-        while (!(context.getApplicationContext() instanceof Application)) {
-            if (context instanceof ContextWrapper) {
-                context = ((ContextWrapper) context).getBaseContext();
-            } else {
-                return null;
-            }
-        }
-        return CastContext.getSharedInstance(context);
+        return CastContext.getSharedInstance(MediaRouterUtils.getContextForCasting());
     }
 
     /**

weblayer/browser/java/org/chromium/weblayer_private/media/MediaRouterClientImpl.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.support.v4.media.session.MediaSessionCompat;
 
 import androidx.fragment.app.FragmentManager;
 
@@ -137,11 +136,6 @@ public class MediaRouterClientImpl extends MediaRouterClient {
         @Override
         public NotificationWrapperBuilder createNotificationWrapperBuilder() {
             return MediaSessionNotificationHelper.createNotificationWrapperBuilder(mNotificationId);
-        }
-
-        @Override
-        public void onMediaSessionUpdated(MediaSessionCompat session) {
-            // TODO(estade): implement.
         }
 
         @Override

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 087ef8ead734df8c4a5823b946a1b097f5d13826

WebLayer: add basic MediaSession test.
This would have caught the crash caused by
657b47a9eef04e784d0946d196e1a286d85774ab

Bug: none
Change-Id: I3e4e13c46c09fb2c4f17c24550ba49f41694005b
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2548386
Commit-Queue: Evan Stade <estade@chromium.org>
Reviewed-by: Clark DuVall <cduvall@chromium.org>
Cr-Commit-Position: refs/heads/master@{#829476}

weblayer/browser/android/javatests/BUILD.gn
@@ -27,6 +27,7 @@ android_library("weblayer_java_tests") {
     "src/org/chromium/weblayer/test/FullscreenSizeTest.java",
     "src/org/chromium/weblayer/test/GoogleAccountsTest.java",
     "src/org/chromium/weblayer/test/InputTypesTest.java",
+    "src/org/chromium/weblayer/test/MediaSessionTest.java",
     "src/org/chromium/weblayer/test/NavigationTest.java",
     "src/org/chromium/weblayer/test/NewTabCallbackTest.java",
     "src/org/chromium/weblayer/test/OnTabRemovedTabListCallbackImpl.java",

weblayer/browser/android/javatests/src/org/chromium/weblayer/test/MediaSessionTest.java
@@ -1,0 +1,72 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer.test;
+
+import static org.chromium.weblayer.R.id.weblayer_media_session_notification;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.service.notification.StatusBarNotification;
+
+import androidx.test.filters.MediumTest;
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.Criteria;
+import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.MinAndroidSdkLevel;
+import org.chromium.weblayer.shell.InstrumentationActivity;
+
+/**
+ * Tests that MediaSession works as expected.
+ */
+@CommandLineFlags.Add({"ignore-certificate-errors"})
+@RunWith(WebLayerJUnit4ClassRunner.class)
+public final class MediaSessionTest {
+    @Rule
+    public InstrumentationActivityTestRule mActivityTestRule =
+            new InstrumentationActivityTestRule();
+
+    private InstrumentationActivity mActivity;
+
+    @Test
+    @MediumTest
+    @MinAndroidSdkLevel(Build.VERSION_CODES.M)
+    public void basic() throws Throwable {
+        mActivity = mActivityTestRule.launchShellWithUrl(
+                mActivityTestRule.getTestDataURL("media_session.html"));
+        EventUtils.simulateTouchCenterOfView(mActivity.getWindow().getDecorView());
+
+        CriteriaHelper.pollInstrumentationThread(() -> {
+            Criteria.checkThat(getMediaSessionNotification(), Matchers.notNullValue());
+        });
+    }
+
+    /**
+     * Retrieves the active media session notification, or null if none exists.
+     * {@link NotificationManager#getActiveNotifications()} is only available from M.
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    private Notification getMediaSessionNotification() {
+        StatusBarNotification notifications[] =
+                ((NotificationManager) mActivity.getApplicationContext().getSystemService(
+                         Context.NOTIFICATION_SERVICE))
+                        .getActiveNotifications();
+        for (StatusBarNotification statusBarNotification : notifications) {
+            if (statusBarNotification.getId() == weblayer_media_session_notification) {
+                return statusBarNotification.getNotification();
+            }
+        }
+        return null;
+    }
+}

weblayer/test/data/media_session.html
@@ -1,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<html>
+  <body>
+    <p></p>
+  </body>
+  <script>
+    let audio = document.createElement('audio');
+    function playAudio() {
+      audio.src = 'test_audio.ogg';
+      audio.play();
+    }
+    document.addEventListener('touchend', function(e) { playAudio(); }, false);
+  </script>
+</html>