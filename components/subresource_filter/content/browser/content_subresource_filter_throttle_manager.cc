ae2280a4ce71f64b7cb35881d426b196af6b9a46
Yao Xiao <yaoxia@chromium.org>
2020-11-20 05:14:50 +0000

    Sticky and Overlay-popup ads recorded in ads intervention manager
    This change propagates signals for sticky ad and overlay-popup ad
    detection from the renderer to the ads intervention manager through
    the subresource filter component.
    
    As the ads intervention manager is running in dry run mode and
    does not block ads, this allows us to record intervention behavior
    when triggered by one of these two interventions. Understanding
    the intervention behavior is a precursor to the ads intervention
    manager intervening to actively intervening on sites with better
    ads standard violations.
    
    Add base::Feature flags to allow disabling the frequency capping from
    either web_tests and browsertests. Move relevant tests into a virtual
    test suite that has the flag disabled.
    
    Bug: 1099741
    Change-Id: Id0c922adfbb7cd71f641c6e9586eed9c311feb18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2525769
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Eric Robinson <ericrobinson@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829547}

--- components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.cc
+++ components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.cc
@@ -672,4 +672,10 @@ void ContentSubresourceFilterThrottleManager::SetDocumentLoadStatistics(
     statistics_->OnDocumentLoadStatistics(*statistics);
 }
 
+void ContentSubresourceFilterThrottleManager::OnAdsViolationTriggered(
+    mojom::AdsViolation violation) {
+  client_->OnAdsViolationTriggered(
+      receiver_.GetCurrentTargetFrame()->GetMainFrame(), violation);
+}
+
 }  // namespace subresource_filter
