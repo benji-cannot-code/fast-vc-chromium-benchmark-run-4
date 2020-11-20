c57e212f897f6aeaf48f2ad890413edcffaa2312
Mingjing Zhang <mjzhang@chromium.org>
2020-11-20 21:38:11 +0000

    Revert "Not disable acceleration in getImage if its Desynchronized"
    This reverts commit edce90d5920e967e3e04698fee95deeac0ef7c49.
    
    Reason for revert: crbug.com/1151440 Pixel_CanvasLowLatency2DImageData
    test consistently fails on Android FYI Release builders, e.g.:
    
    https://ci.chromium.org/p/chromium/builders/ci/Android%20FYI%20Release%20%28Nexus%205%29/b8863094711783933776
    
    Original change's description:
    > Not disable acceleration in getImage if its Desynchronized
    >
    > This is a temporary fix for this issue, that will totally be fixed
    > after we land the new 2d canvas api (that will completely remove the
    > path of code causing this regression in base_rendering_context_2d.cc:
    >   if (!RuntimeEnabledFeatures::NewCanvas2DAPIEnabled()) {
    >     // GetImagedata is faster in Unaccelerated canvases
    >     // Do not disaccelerate if Desync2d Canvas on android.
    >     // Issue 1112060: putImageData() does not work for desynchronized 2D canvas
    >     // on Android
    >     if (IsAccelerated()) {
    >       DisableAcceleration();
    >       base::UmaHistogramEnumeration("Blink.Canvas.GPUFallbackToCPU",
    >                                     GPUFallbackToCPUScenario::kGetImageData);
    >     }
    >   }
    >
    > This CL specifically disables that path if the canvas is Desynchronized.
    > To do so this CL adds some infrastructural elements to be able to access
    > to the desynchronization setting inside getImage.
    >
    > This issue is only happening in a very specific corner case, which
    > proves hard to be tested. This code will be deleted once we enable
    > the new canvas api - so this is a temporary fix.
    >
    > Bug: 1112060
    > Change-Id: I60b90f716332b3969c82dfef3431e4cbd66e6500
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2541063
    > Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    > Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#829679}
    
    TBR=fserb@chromium.org,aaronhk@chromium.org,juanmihd@chromium.org
    
    Change-Id: I35417b2a319a610b7aec7123112db153cff3a806
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1112060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2552924
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Reviewed-by: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829801}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.h
@@ -254,11 +254,6 @@ class MODULES_EXPORT BaseRenderingContext2D : public GarbageCollectedMixin,
 
   virtual bool HasAlpha() const = 0;
 
-  virtual bool IsDesynchronized() const {
-    NOTREACHED();
-    return false;
-  }
-
   virtual bool isContextLost() const = 0;
 
   virtual void WillDrawImage(CanvasImageSource*) const {}
