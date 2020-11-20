e86f7687516d7193272506c5bd034b418d83df5c
Shannon Chen <shannc@chromium.org>
2020-11-20 03:28:43 +0000

    [Switch Access] Add point scan menu after the point is set
    A point scan menu will be opened after the point is selected by point scanning. The
    current actions include left click and right click.
    
    Signed-off-by: Shannon Chen <shannc@chromium.org>
    Bug: 1061537
    Change-Id: I50b3918a25936d97cac89d2b56568954b2bec9d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538416
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Shannon Chen <shannc@google.com>
    Cr-Commit-Position: refs/heads/master@{#829508}

--- chrome/browser/resources/chromeos/accessibility/switch_access/action_manager.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/action_manager.js
@@ -19,6 +19,12 @@ class ActionManager {
 
     /** @private {!Array<!SAConstants.MenuType>} */
     this.menuStack_ = [];
+
+    /** @private {constants.Point} */
+    this.pointScanPoint_ = {x: 0, y: 0};
+
+    /** @private {function(constants.Point)} */
+    this.pointScanListener_ = this.handleOnPointScanSet_.bind(this);
   }
 
   static get instance() {
@@ -78,7 +84,9 @@ class ActionManager {
   static performAction(action) {
     const manager = ActionManager.instance;
     manager.handleGlobalActions_(action) ||
+        manager.handlePointScanActions_(action) ||
         manager.performActionOnCurrentNode_(action);
+    ActionManager.exitCurrentMenu();
   }
 
 
@@ -142,6 +150,11 @@ class ActionManager {
           SwitchAccessMenuAction.MOVE_FORWARD_ONE_CHAR_OF_TEXT,
           SwitchAccessMenuAction.END_TEXT_SELECTION
         ];
+      case SAConstants.MenuType.POINT_SCAN_MENU:
+        return [
+          SwitchAccessMenuAction.LEFT_CLICK,
+          SwitchAccessMenuAction.RIGHT_CLICK,
+        ];
       default:
         return [];
     }
@@ -179,6 +192,9 @@ class ActionManager {
     actions.filter((a) => possibleActions.includes(a));
     if (this.currentMenuType_ === SAConstants.MenuType.MAIN_MENU) {
       actions = this.addGlobalActions_(actions);
+    } else if (this.currentMenuType_ === SAConstants.MenuType.POINT_SCAN_MENU) {
+      actions = this.actionsForType_(SAConstants.MenuType.POINT_SCAN_MENU);
+      actions = this.addGlobalActions_(actions);
     }
     return actions;
   }
@@ -197,10 +213,35 @@ class ActionManager {
             'manageAccessibility/switchAccess');
         return true;
       case SwitchAccessMenuAction.POINT_SCAN:
+        ActionManager.exitCurrentMenu();
         chrome.accessibilityPrivate.activatePointScan();
         chrome.accessibilityPrivate.onPointScanSet.addListener(
-            this.pointScanClick_.bind(this));
-        ActionManager.exitCurrentMenu();
+            this.pointScanListener_);
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  /**
+   * If the action is a point scan action, perform the action and return true.
+   * Otherwise return false.
+   * @param {!SwitchAccessMenuAction} action
+   * @return {boolean}
+   * @private
+   */
+  handlePointScanActions_(action) {
+    switch (action) {
+      case SwitchAccessMenuAction.LEFT_CLICK:
+        EventGenerator.sendMouseClick(
+            this.pointScanPoint_.x, this.pointScanPoint_.y);
+        return true;
+      case SwitchAccessMenuAction.RIGHT_CLICK:
+        EventGenerator.sendMouseClick(
+            this.pointScanPoint_.x, this.pointScanPoint_.y, {
+              mouseButton:
+                  chrome.accessibilityPrivate.SyntheticMouseEventButton.RIGHT
+            });
         return true;
       default:
         return false;
@@ -253,13 +294,16 @@ class ActionManager {
   }
 
   /**
-   * Performs mouse left click action on the selected point scanning point
-   * coordinates
-   * @param {number} x
-   * @param {number} y
+   * Shows the point scan menu and sets the point scan position
+   * coordinates.
+   * @param {!constants.Point} point
    * @private
    */
-  pointScanClick_(x, y) {
-    EventGenerator.sendMouseClick(x, y);
+  handleOnPointScanSet_(point) {
+    this.pointScanPoint_ = point;
+    this.menuStack_.push(SAConstants.MenuType.POINT_SCAN_MENU);
+    this.openCurrentMenu_();
+    chrome.accessibilityPrivate.onPointScanSet.removeListener(
+        this.pointScanListener_);
   }
 }
--- third_party/closure_compiler/externs/accessibility_private.js
+++ third_party/closure_compiler/externs/accessibility_private.js
@@ -99,6 +99,7 @@ chrome.accessibilityPrivate.SwitchAccessMenuAction = {
   JUMP_TO_BEGINNING_OF_TEXT: 'jumpToBeginningOfText',
   JUMP_TO_END_OF_TEXT: 'jumpToEndOfText',
   KEYBOARD: 'keyboard',
+  LEFT_CLICK: 'leftClick',
   MOVE_BACKWARD_ONE_CHAR_OF_TEXT: 'moveBackwardOneCharOfText',
   MOVE_BACKWARD_ONE_WORD_OF_TEXT: 'moveBackwardOneWordOfText',
   MOVE_CURSOR: 'moveCursor',
@@ -108,6 +109,7 @@ chrome.accessibilityPrivate.SwitchAccessMenuAction = {
   MOVE_UP_ONE_LINE_OF_TEXT: 'moveUpOneLineOfText',
   PASTE: 'paste',
   POINT_SCAN: 'pointScan',
+  RIGHT_CLICK: 'rightClick',
   SCROLL_DOWN: 'scrollDown',
   SCROLL_LEFT: 'scrollLeft',
   SCROLL_RIGHT: 'scrollRight',
@@ -394,12 +396,12 @@ chrome.accessibilityPrivate.performAcceleratorAction = function(acceleratorActio
 chrome.accessibilityPrivate.isFeatureEnabled = function(feature, callback) {};
 
 /**
- * Updates properties on the Select-to-speak panel.
- * @param {boolean} show True if the panel should be shown, false otherwise
+ * Updates properties of the Select-to-speak panel.
+ * @param {boolean} show True to show panel, false to hide it
  * @param {!chrome.accessibilityPrivate.ScreenRect=} anchor A rectangle
  *     indicating the bounds of the object the panel should be displayed next
- * to.
- * @param {boolean=} isPaused Whether Select-to-speak playback is paused.
+ *     to.
+ * @param {boolean=} isPaused True if Select-to-speak playback is paused.
  */
 chrome.accessibilityPrivate.updateSelectToSpeakPanel = function(
     show, anchor, isPaused) {};
