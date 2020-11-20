2297772deb0f0dea2f1f418beab94c12c352056f
Ryan Sturm <ryansturm@chromium.org>
2020-11-20 21:16:57 +0000

    Adding a main frame check to search prefetch
    This checks that the search prefetch can only be served to main frame
    navigations.
    
    Bug: 1148512
    Change-Id: Idd26f0fc3b81bf9114990e60f53eb532dbeb4a0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551746
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829789}

--- chrome/browser/prefetch/search_prefetch/search_prefetch_url_loader_interceptor.cc
+++ chrome/browser/prefetch/search_prefetch/search_prefetch_url_loader_interceptor.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "components/no_state_prefetch/browser/prerender_manager.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
 
@@ -48,6 +49,21 @@ void SearchPrefetchURLLoaderInterceptor::MaybeCreateLoader(
 
   DCHECK(!loader_callback_);
   loader_callback_ = std::move(callback);
+
+  content::WebContents* web_contents =
+      content::WebContents::FromFrameTreeNodeId(frame_tree_node_id_);
+  // Make sure this is for a navigation.
+  if (!web_contents) {
+    DoNotInterceptPrefetchedNavigation();
+    return;
+  }
+  // Only intercept main frame requests.
+  content::RenderFrameHost* main_frame = web_contents->GetMainFrame();
+  if (!main_frame || main_frame->GetFrameTreeNodeId() != frame_tree_node_id_) {
+    DoNotInterceptPrefetchedNavigation();
+    return;
+  }
+
   url_ = tentative_resource_request.url;
 
   std::unique_ptr<SearchPrefetchURLLoader> prefetch =
