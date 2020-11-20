c757f36216fe7876fa54a2f030d9eb0b680d17bd
Yu-Hsuan Hsu <yuhsuan@chromium.org>
2020-11-20 03:10:25 +0000

    Support GetDefaultOutputBufferSize on LaCrOS
    Query the default output buffer size from CRAS server to reduce the
    chance of underruns.
    
    BUG=b:173675463
    
    Change-Id: Ifc1b59352596a2c4d16314f05159e0d9ee4d9cd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2548107
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829498}

--- media/audio/cras/audio_manager_cras.cc
+++ media/audio/cras/audio_manager_cras.cc
@@ -189,6 +189,9 @@ AudioParameters AudioManagerCras::GetPreferredOutputStreamParameters(
   }
 
   if (!buffer_size)  // Not user-provided.
+    buffer_size = CrasGetDefaultOutputBufferSize();
+
+  if (buffer_size <= 0)
     buffer_size = kDefaultOutputBufferSize;
 
   return AudioParameters(
