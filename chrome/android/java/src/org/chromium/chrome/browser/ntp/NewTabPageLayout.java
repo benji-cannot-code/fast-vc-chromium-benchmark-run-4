e4866403f91c6fddce53a6e4e20c09d9c18b5217
Tommy Martino <tmartino@chromium.org>
2020-11-20 18:36:58 +0000

    Revert "[Cryptids] Fix Doodle overlap bug"
    This reverts commit 8d90c018c167216ba02bc28259b044f45a6b6134.
    
    Reason for revert: crbug.com/1149279
    
    Original change's description:
    > [Cryptids] Fix Doodle overlap bug
    >
    > Cryptid rendering should be disabled whenever a Doodle is displayed.
    > Currently, this condition is occasionally violated due to caching: we
    > sometimes receive a cached response that there is no logo, followed
    > by a fresh indication that actually there is one, but the first will have
    > triggered cryptid rendering, causing overlapping.
    >
    > This CL solves this bug by invoking the OnLogoAvailable callback in the
    > LogoService in the Revalidated case (which corresponds to a confirmation
    > from the server that no logo is available), and tightening the
    > conditions in NewTabPageLayout to pick up this new invocation.
    >
    > Bug: 1125889
    >
    > Change-Id: Iceb36ebd5aa6eaebe6ec4064951eac4c3510feb2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2443890
    > Commit-Queue: Tommy Martino <tmartino@chromium.org>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#814415}
    
    TBR=treib@chromium.org,tmartino@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1125889, 1149279
    Change-Id: I88f1e3335735d7ffdf0950edfc5064915ef6c6aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551587
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829726}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageLayout.java
@@ -498,10 +498,6 @@ public class NewTabPageLayout extends LinearLayout implements TileGroup.Observer
 
         mLogoDelegate.getSearchProviderLogo((logo, fromCache) -> {
             if (logo == null) {
-                if (fromCache) {
-                    return;
-                }
-
                 if (mSearchProviderIsGoogle) {
                     // We received a null logo and the provider is Google; this means there's no
                     // doodle.
@@ -520,6 +516,8 @@ public class NewTabPageLayout extends LinearLayout implements TileGroup.Observer
                                 renderer.recordRenderEvent();
                             }));
                 }
+
+                if (fromCache) return;
             }
 
             mSearchProviderLogoView.setDelegate(mLogoDelegate);
