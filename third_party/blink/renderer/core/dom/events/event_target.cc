dc7746be35850da8f8cc2ad44a74b588c498434d
Hitoshi Yoshida <peria@chromium.org>
2020-11-20 02:40:32 +0000

    bindings: Fix checking existence of AddEventListenerOptions.signal
    We used obj->signal() to check the existence of the dictionary member
    "signal", but it is a wrong usage and crashes if we use the new IDL
    dictionary code generator.
    
    This CL fixes them to call obj->hasSignal() as expected.
    
    Bug: 839389
    Change-Id: I8db7e974adf6f95cf3c4c0107fcabe679bde8ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550961
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829491}

--- third_party/blink/renderer/core/dom/events/event_target.cc
+++ third_party/blink/renderer/core/dom/events/event_target.cc
@@ -507,7 +507,7 @@ bool EventTarget::AddEventListenerInternal(
   if (!listener)
     return false;
 
-  if (options->signal() && options->signal()->aborted())
+  if (options->hasSignal() && options->signal()->aborted())
     return false;
 
   if (event_type == event_type_names::kTouchcancel ||
@@ -537,7 +537,7 @@ bool EventTarget::AddEventListenerInternal(
   bool added = EnsureEventTargetData().event_listener_map.Add(
       event_type, listener, options, &registered_listener);
   if (added) {
-    if (options->signal()) {
+    if (options->hasSignal()) {
       options->signal()->AddAlgorithm(WTF::Bind(
           [](EventTarget* event_target, const AtomicString& event_type,
              const EventListener* listener,
