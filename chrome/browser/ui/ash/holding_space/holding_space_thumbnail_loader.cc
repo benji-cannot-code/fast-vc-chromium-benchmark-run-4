39270b652b4377f9e8699f9a176c5c157049bcfa
Toni Barzic <tbarzic@chromium.org>
2020-11-21 07:10:17 +0000

    Use correct icon for folder in holding space
    The thubmail loader is already checking whether the holding space item
    is a folder, but it used to return an empty bitmap in this case (relying
    on the placeholder icon creation to generate the icon; which relies
    purely on the file path, and may thus generate an incorrect icon -
    currently it generates a generic file icon; with this change this will
    be WAI).
    Instead, create the correct folder icon in the holding space thumbnail
    loader.
    
    BUG=1141034
    
    Change-Id: I7d619adc892d2595284df210cefcab44f9279f43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2552370
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829966}

--- chrome/browser/ui/ash/holding_space/holding_space_thumbnail_loader.cc
+++ chrome/browser/ui/ash/holding_space/holding_space_thumbnail_loader.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/holding_space/holding_space_thumbnail_loader.h"
 
+#include "ash/public/cpp/holding_space/holding_space_color_provider.h"
 #include "ash/public/cpp/image_downloader.h"
 #include "base/bind.h"
 #include "base/callback_helpers.h"
@@ -21,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/file_manager/fileapi_util.h"
 #include "chrome/browser/extensions/api/messaging/native_message_port.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/ash/holding_space/holding_space_util.h"
+#include "chromeos/ui/vector_icons/vector_icons.h"
 #include "extensions/browser/api/messaging/channel_endpoint.h"
 #include "extensions/browser/api/messaging/message_service.h"
 #include "extensions/browser/api/messaging/native_message_host.h"
@@ -33,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/image_skia.h"
+#include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/skia_util.h"
 #include "url/gurl.h"
 
@@ -44,6 +48,9 @@ namespace {
 // the image loader extension.
 constexpr char kNativeMessageHostName[] =
     "com.google.holding_space_thumbnail_loader";
+
+// The DIP size for the folder icon.
+const size_t kFolderIconDipSize = 20;
 
 using ThumbnailDataCallback = base::OnceCallback<void(const std::string& data)>;
 
@@ -190,8 +197,9 @@ HoldingSpaceThumbnailLoader::~HoldingSpaceThumbnailLoader() = default;
 
 HoldingSpaceThumbnailLoader::ThumbnailRequest::ThumbnailRequest(
     const base::FilePath& item_path,
-    const gfx::Size& size)
-    : item_path(item_path), size(size) {}
+    const gfx::Size& size,
+    float scale_factor)
+    : item_path(item_path), size(size), scale_factor(scale_factor) {}
 
 HoldingSpaceThumbnailLoader::ThumbnailRequest::~ThumbnailRequest() = default;
 
@@ -224,8 +232,16 @@ void HoldingSpaceThumbnailLoader::LoadForFileWithMetadata(
     return;
   }
 
+  // Short-circuit icons for folders.
   if (file_info.is_directory) {
-    std::move(callback).Run(nullptr);
+    std::move(callback).Run(
+        holding_space_util::CreatePlaceholderImage(
+            gfx::CreateVectorIcon(
+                chromeos::kFiletypeFolderIcon,
+                request.scale_factor * kFolderIconDipSize,
+                HoldingSpaceColorProvider::Get()->GetFileIconColor()),
+            request.size)
+            .bitmap());
     return;
   }
 
--- chrome/browser/ui/ash/holding_space/holding_space_thumbnail_loader.h
+++ chrome/browser/ui/ash/holding_space/holding_space_thumbnail_loader.h
@@ -39,7 +39,9 @@ class HoldingSpaceThumbnailLoader {
 
   // Thumbnail request data that will be forwarded to the image loader.
   struct ThumbnailRequest {
-    ThumbnailRequest(const base::FilePath& item_path, const gfx::Size& size);
+    ThumbnailRequest(const base::FilePath& item_path,
+                     const gfx::Size& size,
+                     float scale_factor);
     ~ThumbnailRequest();
 
     // The absolute item file path.
@@ -47,6 +49,9 @@ class HoldingSpaceThumbnailLoader {
 
     // The desired bitmap size.
     const gfx::Size size;
+
+    // The scale factor for which the bitmap is being generated.
+    float scale_factor;
   };
 
   // Returns a weak pointer to this instance.
