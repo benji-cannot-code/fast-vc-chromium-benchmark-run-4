ac67b6758be4a6db5a173763923f499c31ac6bf5
Kyle Horimoto <khorimoto@chromium.org>
2020-11-21 03:14:48 +0000

    [CrOS PhoneHub] Create NearbyConnectionsStreamBufferManager
    This class manages partially-transferred payloads with type "stream".
    Stream payloads are transferred in chunks of 1980 bytes each, so this
    class buffers these chunks until the transfer is complete, then provides
    an API for clients to retrieve the completed payload.
    
    This CL also updates NearbyConnections to use this class.
    
    Bug: 1150565, 1106937
    Change-Id: Ib6b18702b7feedc854f22dabe6fdeddc066e7636
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551352
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#829939}

--- chrome/services/sharing/nearby/nearby_connections.h
+++ chrome/services/sharing/nearby/nearby_connections.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/task/post_task.h"
 #include "base/thread_annotations.h"
+#include "chrome/services/sharing/nearby/nearby_connections_stream_buffer_manager.h"
 #include "chromeos/services/nearby/public/mojom/nearby_connections.mojom.h"
 #include "chromeos/services/nearby/public/mojom/webrtc_signaling_messenger.mojom.h"
 #include "device/bluetooth/public/mojom/adapter.mojom.h"
@@ -176,6 +177,10 @@ class NearbyConnections : public mojom::NearbyConnections {
   // service uses its own Core object, but all Core objects share the underlying
   // ServiceController instance.
   base::flat_map<std::string, std::unique_ptr<Core>> service_id_to_core_map_;
+
+  // Handles incoming stream payloads. This object buffers partial streams as
+  // they arrive and provides a getter for the final buffer when it is complete.
+  NearbyConnectionsStreamBufferManager buffer_manager_;
 
   // input_file_map_ is accessed from background threads.
   base::Lock input_file_lock_;
