2aef0e7f0ee1928f185aba975e925b717451741f
Olivier Yiptong <oyiptong@chromium.org>
2020-11-21 01:15:50 +0000

    FontAccess: Blink implementation for Chooser API
    This implements the blink portion of the API to
    trigger a font chooser. This extends the previous stub
    to communicate with the Browser process.
    
    The content/embedder side will be extended in a future
    CL to complete the implementation end-to-end.
    
    Bug: 1138621
    Change-Id: I0d5206b10566eed6a40227f994523cabaf3e0913
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538143
    Commit-Queue: Olivier Yiptong <oyiptong@chromium.org>
    Reviewed-by: Martin Barbella <mbarbella@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829904}

--- third_party/blink/renderer/modules/font_access/navigator_fonts.cc
+++ third_party/blink/renderer/modules/font_access/navigator_fonts.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/font_access/navigator_fonts.h"
 
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/execution_context/security_context.h"
 #include "third_party/blink/renderer/core/frame/navigator.h"
 #include "third_party/blink/renderer/core/workers/worker_navigator.h"
@@ -37,9 +38,9 @@ class NavigatorFontsImpl final : public GarbageCollected<NavigatorFontsImpl<T>>,
 
   explicit NavigatorFontsImpl(T& navigator) : Supplement<T>(navigator) {}
 
-  FontManager* GetFontManager() const {
+  FontManager* GetFontManager(ExecutionContext* context) const {
     if (!font_manager_) {
-      font_manager_ = MakeGarbageCollected<FontManager>();
+      font_manager_ = MakeGarbageCollected<FontManager>(context);
     }
     return font_manager_.Get();
   }
@@ -67,7 +68,8 @@ FontManager* NavigatorFonts::fonts(ScriptState* script_state,
                                    Navigator& navigator,
                                    ExceptionState& exception_state) {
   DCHECK(ExecutionContext::From(script_state)->IsContextThread());
-  return NavigatorFontsImpl<Navigator>::From(navigator).GetFontManager();
+  ExecutionContext* context = ExecutionContext::From(script_state);
+  return NavigatorFontsImpl<Navigator>::From(navigator).GetFontManager(context);
 }
 
 }  // namespace blink
