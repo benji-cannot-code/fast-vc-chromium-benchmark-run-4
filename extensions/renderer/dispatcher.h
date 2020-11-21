6c010242ed4c25f7b808fb6faa83932f25b7d4b3
Julie Jeongeun Kim <jkim@igalia.com>
2020-11-21 02:51:28 +0000

    Convert ExtensionMsg_SetSessionInfo to mojo
    This CL converts ExtensionMsg_SetSessionInfo to mojo
    and introduces channel_mojom_traits.h to convert
    version_info::Channel to extensions.mojom.Channel.
    
    Bug: 1146103
    Change-Id: Iacc3d116bf08a7f32eadb5066cd9690b3aec89ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2548943
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829934}

--- extensions/renderer/dispatcher.h
+++ extensions/renderer/dispatcher.h
@@ -218,6 +218,9 @@ class Dispatcher : public content::RenderThreadObserver,
   void ActivateExtension(const std::string& extension_id) override;
   void SetActivityLoggingEnabled(bool enabled) override;
   void UnloadExtension(const std::string& extension_id) override;
+  void SetSessionInfo(version_info::Channel channel,
+                      mojom::FeatureSessionType session_type,
+                      bool lock_screen_context) override;
 
   void OnRendererAssociatedRequest(
       mojo::PendingAssociatedReceiver<mojom::Renderer> receiver);
@@ -241,9 +244,6 @@ class Dispatcher : public content::RenderThreadObserver,
                        const base::ListValue& args);
   void OnDispatchEvent(const ExtensionMsg_DispatchEvent_Params& params,
                        const base::ListValue& event_args);
-  void OnSetSessionInfo(version_info::Channel channel,
-                        mojom::FeatureSessionType session_type,
-                        bool lock_screen_context);
   void OnSetScriptingAllowlist(
       const ExtensionsClient::ScriptingAllowlist& extension_ids);
   void OnSetSystemFont(const std::string& font_family,
