01afd43ec94ceed8c7e412a2d05cc2e1b49a6c23
Zhaoliang Ma <zhaoliang.ma@intel.com>
2020-11-21 09:16:06 +0000

    Cleanup VideoCodecType conversion from WebRtc to media
    Bug: None
    Change-Id: I3feb4c93e4b6b9ffe4187cb5e71623d0f2ce16f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551330
    Auto-Submit: Zhaoliang Ma <zhaoliang.ma@intel.com>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829978}

--- third_party/blink/renderer/platform/webrtc/webrtc_video_utils.h
+++ third_party/blink/renderer/platform/webrtc/webrtc_video_utils.h
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_WEBRTC_WEBRTC_VIDEO_UTILS_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_WEBRTC_WEBRTC_VIDEO_UTILS_H_
 
+#include "media/base/video_codecs.h"
 #include "media/base/video_color_space.h"
 #include "media/base/video_transformation.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/webrtc/api/video/color_space.h"
+#include "third_party/webrtc/api/video/video_codec_type.h"
 #include "third_party/webrtc/api/video/video_rotation.h"
 
 namespace blink {
@@ -19,6 +21,9 @@ namespace blink {
 
 media::VideoRotation PLATFORM_EXPORT
 WebRtcToMediaVideoRotation(webrtc::VideoRotation rotation);
+
+media::VideoCodec PLATFORM_EXPORT
+WebRtcToMediaVideoCodec(webrtc::VideoCodecType codec);
 
 media::VideoColorSpace PLATFORM_EXPORT
 WebRtcToMediaVideoColorSpace(const webrtc::ColorSpace& color_space);
