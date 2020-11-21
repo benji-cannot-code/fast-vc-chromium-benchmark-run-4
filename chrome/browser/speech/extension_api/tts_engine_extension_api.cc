347ac82775fc4e346ed264ee57143a85e3169c5b
David Tseng <dtseng@google.com>
2020-11-21 02:45:28 +0000

    Enqueue tts utterances when built in tts engines have yet to initialize
    This change builds on top of the logic to enqueue utterances for tts
    platforms (which are interfaces to native tts api for that platform)
    while initializing/loading. It adds the same treatment for
    TtsEngineDelegate, which is responsible for built-in tts engines shipped
    as extensions for each platform.
    
    This starts off for built in engines on Chrome OS, but can encompass the
    network tts engine we ship for other platforms. The new behavior will be
    to enqueue until the built-in engine(s) are ready; readiness is signaled
    by detecting at least one voice for the built-in engines
    (Googletts/Espeak). Note it can be uninitialized in this scheme.
    
    R=dmazzoni@chromium.org
    
    Change-Id: Ie681e5830909245b566969b491043225c0640be1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2552369
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829933}

--- chrome/browser/speech/extension_api/tts_engine_extension_api.cc
+++ chrome/browser/speech/extension_api/tts_engine_extension_api.cc
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/devtools/console_message.mojom.h"
 #include "ui/base/l10n/l10n_util.h"
 
+#if defined(OS_CHROMEOS)
+#include "chrome/common/extensions/extension_constants.h"
+#endif
+
 using extensions::EventRouter;
 using extensions::Extension;
 using extensions::ExtensionSystem;
@@ -343,6 +347,29 @@ bool TtsExtensionEngine::LoadBuiltInTtsEngine(
   return true;
 #else
   return false;
+#endif
+}
+
+bool TtsExtensionEngine::IsBuiltInTtsEngineInitialized(
+    content::BrowserContext* browser_context) {
+  if (!browser_context || disable_built_in_tts_engine_for_testing_)
+    return true;
+
+#if defined(OS_CHROMEOS)
+  std::vector<content::VoiceData> voices;
+  GetVoices(browser_context, &voices);
+  bool saw_google_tts = false;
+  bool saw_espeak = false;
+  for (const auto& voice : voices) {
+    saw_google_tts |=
+        voice.engine_id == extension_misc::kGoogleSpeechSynthesisExtensionId;
+    saw_espeak |=
+        voice.engine_id == extension_misc::kEspeakSpeechSynthesisExtensionId;
+  }
+  return saw_google_tts && saw_espeak;
+#else
+  // Vacuously; no built in engines on other platforms yet. TODO: network tts?
+  return true;
 #endif
 }
 
