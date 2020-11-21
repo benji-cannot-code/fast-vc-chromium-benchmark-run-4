5f49fce34cb1991ee108a4a1adafce4fb5f6f586
Alison Maher <almaher@microsoft.com>
2020-11-21 04:17:11 +0000

    Force fill/stroke color at used value time
    Forced colors should now happen at used value time. This change adjusts
    the used value of fill, stroke, -internal-visited-fill, and
    -internal-visited-stroke to take forced colors into account. No internal
    properties were needed since we only ever force fill and stroke to one
    value in forced colors mode.
    
    The other forced color properties will be moved in follow up changes.
    
    Note: This change will result in some functional differences with the
    current fill/stroke behavior in forced colors mode. SVG elements have
    "forced-color-adjust:none" set. Previously, SVG elements could inherit
    forced colors from their ancestors. Now, since we force at used value
    time, the used value of fill/stroke for SVG elements can no longer
    inherit forced colors (unless "forced-color-adjust:auto" is set by
    the author).
    
    The related web tests have been updated to reflect this change in
    behavior.
    
    Bug: 970285
    Change-Id: I5bcc498a67dbd49d7b319f7f079d062d73586013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2515228
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829948}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -2606,6 +2606,8 @@ const blink::Color Fill::ColorIncludingFallback(
   DCHECK(!visited_link);
   DCHECK(style.SvgStyle().FillPaint().HasColor());
   StyleColor fill_color = style.SvgStyle().FillPaint().GetColor();
+  if (style.ShouldForceColor(fill_color))
+    return style.GetCurrentColor();
   return fill_color.Resolve(style.GetCurrentColor(), style.UsedColorScheme());
 }
 
@@ -3721,6 +3723,8 @@ const blink::Color InternalVisitedFill::ColorIncludingFallback(
         .ColorIncludingFallback(false, style);
   }
   StyleColor visited_fill_color = paint.GetColor();
+  if (style.ShouldForceColor(visited_fill_color))
+    return style.GetInternalVisitedCurrentColor();
   return visited_fill_color.Resolve(style.GetInternalVisitedCurrentColor(),
                                     style.UsedColorScheme());
 }
@@ -3776,6 +3780,8 @@ const blink::Color InternalVisitedStroke::ColorIncludingFallback(
         .ColorIncludingFallback(false, style);
   }
   StyleColor visited_stroke_color = paint.GetColor();
+  if (style.ShouldForceColor(visited_stroke_color))
+    return style.GetInternalVisitedCurrentColor();
   return visited_stroke_color.Resolve(style.GetInternalVisitedCurrentColor(),
                                       style.UsedColorScheme());
 }
@@ -6284,6 +6290,8 @@ const blink::Color Stroke::ColorIncludingFallback(
   DCHECK(!visited_link);
   DCHECK(style.SvgStyle().StrokePaint().HasColor());
   StyleColor stroke_color = style.SvgStyle().StrokePaint().GetColor();
+  if (style.ShouldForceColor(stroke_color))
+    return style.GetCurrentColor();
   return stroke_color.Resolve(style.GetCurrentColor(), style.UsedColorScheme());
 }
 
