10050ff12245e85ac58e531fecab95515f1c7a5a
Nektarios Paisios <nektar@chromium.org>
2020-11-21 03:32:18 +0000

    Cleanup: Uses BrowserAccessibilityManager::GetActiveDescendant to retrieve the active descendant regardless of the focused node
    This patch makes a list of readability and code health improvements:
    1. BrowserAccessibilityManagerMac::GetFocus already calls GetActiveDescendant on the focused node, so
    calling it in BrowserAccessibilityCocoa::selectedChildren is unnecessary.
    2. BrowserAccessibilityManager::GetActiveDescendant does not need to be tied to the concept of the focused node, and can
    handle nullptr arguments correctly, so it can be used more easily and in more places.
    3. BrowserAccessibilityCocoa::selectedChildren was re-written to improve its readability.
    4. BrowserAccessibilityCocoa::activeDescendant is not needed and was removed.
    
    AX-Relnotes: n/a.
    
    R=dmazzoni@chromium.org, abigailbklein@chromium.org
    
    Change-Id: Ia5c3c9cf0af55a7fe9ddff175f0cda4fedf30e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551815
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Auto-Submit: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#829944}

--- content/browser/accessibility/browser_accessibility_manager_mac.mm
+++ content/browser/accessibility/browser_accessibility_manager_mac.mm
@@ -73,10 +73,6 @@ ui::AXTreeUpdate BrowserAccessibilityManagerMac::GetEmptyDocument() {
 
 BrowserAccessibility* BrowserAccessibilityManagerMac::GetFocus() const {
   BrowserAccessibility* focus = BrowserAccessibilityManager::GetFocus();
-  if (!focus)
-    return nullptr;
-
-  // Otherwise, follow the active descendant.
   return GetActiveDescendant(focus);
 }
 
