d7f09eb77e0cb32ca69c09c341ada878f1fe8fdf
Ahmed Mehfooz <amehfooz@chromium.org>
2020-11-21 05:18:51 +0000

    Fix fonts for tray bubbles
    Bring fonts for system tray and secondary trays up tp new spec.
    
    Bug: 1145336
    Change-Id: If5c102d94e21e1b84f5554896126e0acd681c77f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2548404
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829956}

--- ash/system/dark_mode/dark_mode_detailed_view.cc
+++ ash/system/dark_mode/dark_mode_detailed_view.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/system/dark_mode/dark_mode_detailed_view.h"
+#include <cstddef>
 
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -121,8 +122,10 @@ const char* DarkModeDetailedView::GetClassName() const {
 
 void DarkModeDetailedView::OnThemeChanged() {
   TrayDetailedView::OnThemeChanged();
-  themed_label_->SetEnabledColor(GetLabelColor());
-  neutral_label_->SetEnabledColor(GetLabelColor());
+  TrayPopupUtils::SetLabelFontList(themed_label_,
+                                   TrayPopupUtils::FontStyle::kSystemInfo);
+  TrayPopupUtils::SetLabelFontList(neutral_label_,
+                                   TrayPopupUtils::FontStyle::kSystemInfo);
   TrayPopupUtils::UpdateToggleButtonColors(toggle_);
 }
 
