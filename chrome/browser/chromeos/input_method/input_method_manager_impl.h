9bca2cf8b1255c358c43e5a1a1bb3b412c81d107
Leo Zhang <googleo@chromium.org>
2020-11-21 00:19:25 +0000

    Remove ime_service_connector from state.
    Store connector instances inside a map of InputMethodManager then remove
    the instance of ime service connector from InputMethodManager::State.
    
    Test: Test IME service on VM (betty).
    Bug: 1139328
    Change-Id: I32057af4ac75fa5f3392741e87004559290f4494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2509071
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829875}

--- chrome/browser/chromeos/input_method/input_method_manager_impl.h
+++ chrome/browser/chromeos/input_method/input_method_manager_impl.h
@@ -130,11 +130,6 @@ class InputMethodManagerImpl : public InputMethodManager,
     // Reset the input view URL to the default url of the current input method.
     void ResetInputViewUrl();
 
-    // Connect to an InputEngineManager instance in an IME Mojo service.
-    void ConnectMojoManager(
-        mojo::PendingReceiver<chromeos::ime::mojom::InputEngineManager>
-            receiver);
-
     // ------------------------- Data members.
     Profile* const profile;
 
@@ -188,8 +183,6 @@ class InputMethodManagerImpl : public InputMethodManager,
 
     InputMethodManager::UIStyle ui_style_ =
         InputMethodManager::UIStyle::kNormal;
-
-    std::unique_ptr<ImeServiceConnector> ime_service_connector_;
 
     // Do not forget to update StateImpl::InitFrom(const StateImpl& other) and
     // StateImpl::Dump() when adding new data members!!!
@@ -355,6 +348,12 @@ class InputMethodManagerImpl : public InputMethodManager,
   using EngineMap = std::map<std::string, ui::IMEEngineHandlerInterface*>;
   using ProfileEngineMap = std::map<Profile*, EngineMap, ProfileCompare>;
   ProfileEngineMap engine_map_;
+
+  // Map a profile to the IME service connector.
+  typedef std::
+      map<Profile*, std::unique_ptr<ImeServiceConnector>, ProfileCompare>
+          ImeServiceConnectorMap;
+  ImeServiceConnectorMap ime_service_connectors_;
 
   content::NotificationRegistrar notification_registrar_;
 
