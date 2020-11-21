4afb421757ea853ef7d3b637893543f55d2a741c
Piotr Pawliczek <pawliczek@chromium.org>
2020-11-21 05:40:45 +0000

    URI: Always add port in normalized form by default
    This patch switches the default parameter in Uri::GetNormalized(bool)
    method from false to true. As a result, simple call to
    Uri::GetNormalized() will return normalized form including port number.
    In the previous default (for parameter equals false), the port number
    is omitted when equals to the default scheme's port number.
    
    BUG=none
    TEST=unittests on my workstation
    
    Change-Id: Iee55e2f4eaa811c8b056e6b93d130eba922cc60b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550587
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#829958}

--- chrome/browser/chromeos/printing/synced_printers_manager_unittest.cc
+++ chrome/browser/chromeos/printing/synced_printers_manager_unittest.cc
@@ -125,7 +125,7 @@ TEST_F(SyncedPrintersManagerTest, UpdatePrinter) {
 
   auto printers = manager_->GetSavedPrinters();
   ASSERT_EQ(1U, printers.size());
-  EXPECT_EQ(kTestUri, printers[0].uri().GetNormalized());
+  EXPECT_EQ(kTestUri, printers[0].uri().GetNormalized(false));
 
   ExpectObservedPrinterIdsAre(observer.saved_printers(), {kTestPrinterId});
 }
