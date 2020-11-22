880f76fbc425f9c921c3aee7c32f860a385047a3
Minju Kim <mkim@igalia.com>
2020-11-22 06:23:42 +0000

    Set the toplevel surface decoration mode according to use_native_frame
    When UseNativeFrame is enabled, sets server-side decoration mode to
    wayland compositor.
    Currently, UseNativeFrame is enabled when browser type is not normal
    (popup, devtools)
    
    In this CL,
      1. Implement SetUseNativeFrame / ShouldUseNativeFrame
      2. Set the toplevel surface decoration mode according to
         use_native_frame
         When use_native_frame is true, sets a server-side decoration
         and a client-side decoration otherwise.
      3. If xdg-decoration protocol extension is not supported,
         ShouldUseNativeFrame returns false even if
         use_native_frame is enabled.
    
    Test: Verified popup, devtools, and task manager have their own
          frame decorations on KDE Plasma, Sway that implement
          xdg-decoration protocol extension.
    
    Bug: 1127917
    Change-Id: I08b6df1a3f04a30745bdf3f64ee6935b38ab5fc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2535896
    Commit-Queue: MINJU KIM <mkim@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Maksim Sisov (GMT+2) <msisov@igalia.com>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#830023}

--- ui/ozone/platform/wayland/host/wayland_toplevel_window.h
+++ ui/ozone/platform/wayland/host/wayland_toplevel_window.h
@@ -61,6 +61,8 @@ class WaylandToplevelWindow : public WaylandWindow,
   PlatformWindowState GetPlatformWindowState() const override;
   void SizeConstraintsChanged() override;
   std::string GetWindowUniqueId() const override;
+  void SetUseNativeFrame(bool use_native_frame) override;
+  bool ShouldUseNativeFrame() const override;
 
  private:
   // WaylandWindow overrides:
@@ -148,6 +150,8 @@ class WaylandToplevelWindow : public WaylandWindow,
   base::OnceClosure drag_loop_quit_closure_;
 
   wl::Object<zaura_surface> aura_surface_;
+
+  bool use_native_frame_ = false;
 
   base::WeakPtrFactory<WaylandToplevelWindow> weak_ptr_factory_{this};
 };
