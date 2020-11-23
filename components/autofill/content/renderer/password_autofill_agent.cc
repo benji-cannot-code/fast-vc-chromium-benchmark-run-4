0df45777b8075d2c228cda9da21e146e7a67445f
Maria Kazinova <kazinova@google.com>
2020-11-23 16:40:54 +0000

    Handle not being able to access PM gracefully in PasswordAutofillAgent.
    Replacing a DCHECK in PasswordAutofillAgent::InformAboutFormClearing()
    with a condition check and return, since this condition might be
    true and the method should not proceed, but it shouldn't cause a crash.
    
    Bug: 1151915
    Change-Id: Ib23746cb1e063f8539a5812fcd8105b799e636b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2553553
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/master@{#830202}

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -1376,8 +1376,8 @@ PasswordAutofillAgent::GetFormDataFromUnownedInputElements() {
 
 void PasswordAutofillAgent::InformAboutFormClearing(
     const WebFormElement& form) {
-  DCHECK(FrameCanAccessPasswordManager());
-  if (!base::FeatureList::IsEnabled(
+  if (!FrameCanAccessPasswordManager() ||
+      !base::FeatureList::IsEnabled(
           password_manager::features::kDetectFormSubmissionOnFormClear)) {
     return;
   }
