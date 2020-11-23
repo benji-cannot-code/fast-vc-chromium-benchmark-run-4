e1b57ae6c4a63902c4b617377f1664b836cda1b9
Mark Cogan <marq@google.com>
2020-11-23 17:15:42 +0000

    [iOS] Fix a crash in BVC keyCommands.
    Because BVC's -keyCommands method is a UIResponder API, it can be called
    in response to many events outside of Chrome's direct control; UIKit
    calls it (for example) after a child view controller is removed from the
    BVC. This can happen while the BVC is shutting down.
    
    KeyCommands needs to access BVC's |self.browser| method to fetch a
    browser agent. If the BVC has shut down, |self.browser| is nullptr.
    
    This CL changes -shouldRegisterKeyboardCommands to return NO when the
    BVC is shutting down, or when |self.browser| is null.
    
    Bug: 1151894
    Change-Id: Iba8f5e7fa4bf2a2b6a85815ad030935c86c1bfb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2553427
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830215}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -1399,7 +1399,16 @@ NSString* const kBrowserViewControllerSnackbarCategory =
 #pragma mark - UIResponder helpers
 
 // Whether the BVC should declare keyboard commands.
+// Since |-keyCommands| can be called by UIKit at any time, no assumptions
+// about the state of |self| can be made; accordingly, if there's anything
+// not initialized (or being torn down), this method should return NO.
 - (BOOL)shouldRegisterKeyboardCommands {
+  if (_isShutdown)
+    return NO;
+
+  if (!self.browser)
+    return NO;
+
   if ([self presentedViewController])
     return NO;
 
