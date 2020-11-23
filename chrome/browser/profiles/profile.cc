525f593831c87215849b1d793962f5f739be9fdf
Glen Robertson <glenrob@chromium.org>
2020-11-23 02:08:49 +0000

    Delete NOTIFICATION_PROFILE_DESTROYED.
    This was only used in one test and is covered by an observer interface
    method.
    This cleans up a dependency from profile.cc on //c/b target towards
    moving profile into its own target.
    
    Bug: 268984,1149277
    Change-Id: Ic12380f8175214da5ce6920172252178e3268126
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550158
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830073}

--- chrome/browser/profiles/profile.cc
+++ chrome/browser/profiles/profile.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/net/profile_network_context_service.h"
 #include "chrome/browser/net/profile_network_context_service_factory.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -485,11 +484,6 @@ void Profile::MaybeSendDestroyedNotification() {
 
     for (auto& observer : observers_)
       observer.OnProfileWillBeDestroyed(this);
-
-    content::NotificationService::current()->Notify(
-        chrome::NOTIFICATION_PROFILE_DESTROYED,
-        content::Source<Profile>(this),
-        content::NotificationService::NoDetails());
   }
 }
 
