a2500c3924602d8694ee93ad8c7a2ea6aae89fe7
Alexander Hendrich <hendrich@chromium.org>
2020-11-23 09:24:46 +0000

    Disable flaky DesksMockTimeTest.DeskTraversalNonTouchpadMetrics
    TBR=sammiequon@chromium.org, afakhry@chromium.org
    
    Bug: 1151856
    Change-Id: Ie131d72ec0f53d26d61aef0f83608fc0990140d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2554538
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830117}

--- ash/wm/desks/desks_unittests.cc
+++ ash/wm/desks/desks_unittests.cc
@@ -3735,7 +3735,8 @@ class DesksMockTimeTest : public AshTestBase {
   ~DesksMockTimeTest() override = default;
 };
 
-TEST_F(DesksMockTimeTest, DeskTraversalNonTouchpadMetrics) {
+// TODO(crbug.com/1151856) flaky test
+TEST_F(DesksMockTimeTest, DISABLED_DeskTraversalNonTouchpadMetrics) {
   NewDesk();
   NewDesk();
   NewDesk();
