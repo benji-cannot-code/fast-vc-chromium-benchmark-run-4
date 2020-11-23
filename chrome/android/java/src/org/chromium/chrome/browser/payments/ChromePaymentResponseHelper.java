7e47944696117733615c7eb7d7d9e12148e3ad08
Liquan (Max) Gu <maxlg@chromium.org>
2020-11-23 21:32:24 +0000

    [WebLayer] Implement PaymentResponseHelper
    Implements PaymentResponseHelper to be used in WebLayer.
    
    Bug: 1025619
    
    Change-Id: Icdd20b10ebacaf18f6645ef37ba1e428434fb9f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551224
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830313}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ChromePaymentResponseHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ChromePaymentResponseHelper.java
@@ -27,8 +27,8 @@ public class ChromePaymentResponseHelper
     private final PaymentApp mSelectedPaymentApp;
     private final PaymentOptions mPaymentOptions;
     private final boolean mSkipToGpay;
+    private final PaymentResponse mPaymentResponse;
     private AutofillAddress mSelectedShippingAddress;
-    private PaymentResponse mPaymentResponse;
     private PaymentResponseResultCallback mResultCallback;
     private boolean mIsWaitingForShippingNormalization;
     private boolean mIsWaitingForPaymentsDetails = true;
