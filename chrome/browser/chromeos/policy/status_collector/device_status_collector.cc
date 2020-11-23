4baf98e52093439acaffd3ce7fc8a7ae3590540f
Jit Yao Yap <jityao@google.com>
2020-11-23 05:51:38 +0000

    Re-add default values for DeviceReportingProto
    The CL (https://crrev.com/c/2418989) removed the default values in
    DeviceReportingProto since the protobuf default values are not
    respected in the Chrome. However, the server-side team feels that this
    removing the default values is unsafe as it can introduce unexpected
    behavior changes.
    
    This CL serves as a compromise where the default values are
    re-introduced to DeviceReportingProto and comments are added to indicate
    that the default values used in Chrome have to be kept in sync with the
    default values in the protobuf.
    
    Change-Id: Icb0922a4ed6a68897069854049b5302b2f9ef94c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2549300
    Commit-Queue: Jit Yao Yap <jityao@google.com>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830093}

--- chrome/browser/chromeos/policy/status_collector/device_status_collector.cc
+++ chrome/browser/chromeos/policy/status_collector/device_status_collector.cc
@@ -1546,6 +1546,8 @@ void DeviceStatusCollector::UpdateReportingSettings() {
     return;
   }
 
+  // Keep the default values in sync with DeviceReportingProto in
+  // components/policy/proto/chrome_device_policy.proto.
   if (!cros_settings_->GetBoolean(chromeos::kReportDeviceVersionInfo,
                                   &report_version_info_)) {
     report_version_info_ = true;
