34a3e0bec845ae526918430187384a0d73a9e480
Nick Burris <nburris@chromium.org>
2020-11-23 17:51:11 +0000

    Refactor SslValidityChecker for use in WebLayerPaymentRequestFactory
    This patch refactors SslValidityChecker from chrome into components,
    so that it can be used in WebLayerPaymentRequestFactory to implement
    getInvalidSslCertificateErrorMessage().
    
    SslValidityChecker is modified to check the certificate without using
    chrome's security_state_tab_helper, instead using security_state
    directly.
    
    Bug: 1144850
    Change-Id: I5964d43e97b66ed5c3470ca6d331b34303bc8bc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551798
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830228}

--- components/payments/content/android/java/src/org/chromium/components/payments/SslValidityChecker.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/SslValidityChecker.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.payments;
+package org.chromium.components.payments;
 
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
--- components/payments/content/ssl_validity_checker.h
+++ components/payments/content/ssl_validity_checker.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PAYMENTS_SSL_VALIDITY_CHECKER_H_
-#define CHROME_BROWSER_PAYMENTS_SSL_VALIDITY_CHECKER_H_
+#ifndef COMPONENTS_PAYMENTS_CONTENT_SSL_VALIDITY_CHECKER_H_
+#define COMPONENTS_PAYMENTS_CONTENT_SSL_VALIDITY_CHECKER_H_
 
 #include <string>
 
@@ -46,4 +46,4 @@ class SslValidityChecker {
 
 }  // namespace payments
 
-#endif  // CHROME_BROWSER_PAYMENTS_SSL_VALIDITY_CHECKER_H_
+#endif  // COMPONENTS_PAYMENTS_CONTENT_SSL_VALIDITY_CHECKER_H_
