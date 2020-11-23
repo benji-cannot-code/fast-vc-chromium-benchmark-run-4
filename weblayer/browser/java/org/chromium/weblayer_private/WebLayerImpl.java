60d430a9ba5dcfff95af8773b0776d9b5397b912
Liquan (Max) Gu <maxlg@chromium.org>
2020-11-23 20:11:20 +0000

    [WebLayer] Add GooglePayPaymentAppFactory hook on WebLayer init
    Similar to ProcessInitializationHandlerInternal.java where
    GPayPaymentAppFactory is added on Clank process start, we add a
    hook on WebLayer init so that WebLayer with Clank can load
    GPayPaymentAppFactory to PaymentAppService when it starts.
    
    Bug: 1144488
    Change-Id: I885fd68f0e926324be968b593f22e3541d76dbcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551257
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830286}

--- weblayer/browser/java/org/chromium/weblayer_private/WebLayerImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/WebLayerImpl.java
@@ -282,6 +282,7 @@ public final class WebLayerImpl extends IWebLayer.Stub {
             LibraryLoader.getInstance().ensureInitialized();
         }
         GmsBridge.getInstance().setSafeBrowsingHandler();
+        GmsBridge.getInstance().initializeBuiltInPaymentApps();
 
         MediaStreamManager.onWebLayerInit();
         WebLayerNotificationChannels.updateChannelsIfNecessary();
