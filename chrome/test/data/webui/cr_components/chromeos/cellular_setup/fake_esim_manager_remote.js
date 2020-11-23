f27abd952592d3d78358d5c977871f1b1a922e1d
Gordon Seto <gordonseto@google.com>
2020-11-23 19:39:14 +0000

    [CrOS Settings] Install single discovered eSIM profile
    Add new tests for each profile install result
    
    Bug: 1093185
    Change-Id: Ie15c3b7eb78aa69a791bd05e07b0b68594636d19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547841
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830270}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/fake_esim_manager_remote.js
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/fake_esim_manager_remote.js
@@ -25,11 +25,30 @@ cr.define('cellular_setup', function() {
      *     chromeos.cellularSetup.mojom.ESimProfileProperties},}>}
      */
     getProperties() {
-      return new Promise((res) => {
-        res({
-          properties: this.properties_,
-        });
+      return Promise.resolve({
+        properties: this.properties_,
       });
+    }
+
+    /**
+     * @override
+     * @param {string} confirmationCode
+     * @return {!Promise<{result:
+     *     chromeos.cellularSetup.mojom.ProfileInstallResult},}>}
+     */
+    installProfile(confirmationCode) {
+      return Promise.resolve({
+        result: this.profileInstallResult_ ?
+            this.profileInstallResult_ :
+            chromeos.cellularSetup.mojom.ProfileInstallResult.kSuccess
+      });
+    }
+
+    /**
+     * @param {chromeos.cellularSetup.mojom.ProfileInstallResult} result
+     */
+    setProfileInstallResultForTest(result) {
+      this.profileInstallResult_ = result;
     }
   }
 
@@ -48,10 +67,8 @@ cr.define('cellular_setup', function() {
      *     chromeos.cellularSetup.mojom.ESimOperationResult},}>}
      */
     requestPendingProfiles() {
-      return new Promise((res) => {
-        res({
-          result: chromeos.cellularSetup.mojom.ESimOperationResult.kSuccess
-        });
+      return Promise.resolve({
+        result: chromeos.cellularSetup.mojom.ESimOperationResult.kSuccess,
       });
     }
 
@@ -60,10 +77,8 @@ cr.define('cellular_setup', function() {
      * @return {!Promise<{profiles: Array<!ESimProfile>,}>}
      */
     getProfileList() {
-      return new Promise((res) => {
-        res({
-          profiles: this.profiles_,
-        });
+      return Promise.resolve({
+        profiles: this.profiles_,
       });
     }
 
@@ -85,8 +100,8 @@ cr.define('cellular_setup', function() {
      * @return {!Promise<{euiccs: !Array<!Euicc>,}>}
      */
     getAvailableEuiccs() {
-      return new Promise((res) => {
-        res({euiccs: this.euiccs_});
+      return Promise.resolve({
+        euiccs: this.euiccs_,
       });
     }
 
