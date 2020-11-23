6eaa8b4b42ef36b8e5e3ec643c9f6bff23414504
Andy Paicu <andypaicu@chromium.org>
2020-11-23 17:16:49 +0000

    Make prediction service url configurable via finch
    DD: go/permissions-suggestions-client-doc
    
    Since the URL has changed again and it's still not set in stone we
    should proactively set it up in such a way where we can control it
    without needing to update the code.
    
    This CL adds a new feature with a feature param that will be used as
    an override for the default URL, that way we can temporarily write a finch
    config to set the URL for M88 clients if it changes again.
    
    Bug: 1138595
    Change-Id: I61e92577f95e87cf6fd79a225e18d69b34becc64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545626
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Illia Klimov <elklm@google.com>
    Reviewed-by: Illia Klimov <elklm@chromium.org>
    Reviewed-by: Kamila Hasanbega <hkamila@google.com>
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830217}

--- components/permissions/features.cc
+++ components/permissions/features.cc
@@ -33,6 +33,12 @@ const base::Feature kOneTimeGeolocationPermission{
 const base::Feature kPermissionChip{"PermissionChip",
                                     base::FEATURE_DISABLED_BY_DEFAULT};
 
+// When enabled, use the value of the `service_url` FeatureParam as the url
+// for the Web Permission Predictions Service.
+const base::Feature kPermissionPredictionServiceUseUrlOverride{
+    "kPermissionPredictionServiceUseUrlOverride",
+    base::FEATURE_DISABLED_BY_DEFAULT};
+
 }  // namespace features
 namespace feature_params {
 
@@ -40,5 +46,10 @@ const base::FeatureParam<bool> kOkButtonBehavesAsAllowAlways(
     &permissions::features::kOneTimeGeolocationPermission,
     "OkButtonBehavesAsAllowAlways",
     true);
-}
+
+const base::FeatureParam<std::string> kPermissionPredictionServiceUrlOverride{
+    &permissions::features::kPermissionPredictionServiceUseUrlOverride,
+    "service_url", ""};
+
+}  // namespace feature_params
 }  // namespace permissions
