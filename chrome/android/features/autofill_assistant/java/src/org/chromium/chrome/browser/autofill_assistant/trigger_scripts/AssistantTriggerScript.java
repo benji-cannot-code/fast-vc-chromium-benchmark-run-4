16cd549d84fa405dd0ff875f1d00b81ffff9af26
Clemens Arbesser <arbesser@google.com>
2020-11-23 10:59:55 +0000

    [Autofill Assistant] Fix progress bar for trigger scripts.
    Before this change, the progress bar for trigger scripts would flakily
    not show the progress correctly due to missing change notifications.
    
    Bug: b/173790448
    Change-Id: I4bb51ceee69f81ad41d69739a20f0c4b1e38d60b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550918
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#830130}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/trigger_scripts/AssistantTriggerScript.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/trigger_scripts/AssistantTriggerScript.java
@@ -60,7 +60,7 @@ public class AssistantTriggerScript {
     private final ApplicationViewportInsetSupplier mApplicationViewportInsetSupplier;
 
     private AssistantHeaderCoordinator mHeaderCoordinator;
-    private final AssistantHeaderModel mHeaderModel = new AssistantHeaderModel();
+    private AssistantHeaderModel mHeaderModel;
     private LinearLayout mChipsContainer;
     private final int mInnerChipSpacing;
     /** Height of the bottom sheet's shadow, used to compute the viewport resize offset. */
@@ -110,8 +110,6 @@ public class AssistantTriggerScript {
                 R.dimen.autofill_assistant_actions_spacing);
         mShadowHeight = mContext.getResources().getDimensionPixelSize(
                 R.dimen.bottom_sheet_toolbar_shadow_height);
-        mHeaderModel.set(
-                AssistantHeaderModel.FEEDBACK_BUTTON_CALLBACK, mDelegate::onFeedbackButtonClicked);
     }
 
     private void createBottomSheetContents() {
@@ -134,10 +132,6 @@ public class AssistantTriggerScript {
         // Allow swipe-to-dismiss.
         mContent.setPeekModeDisabled(true);
 
-        if (mHeaderCoordinator != null) {
-            mHeaderCoordinator.destroy();
-        }
-        mHeaderCoordinator = new AssistantHeaderCoordinator(mContext, mHeaderModel);
         mChipsContainer = new LinearLayout(mContext);
         mChipsContainer.setOrientation(LinearLayout.HORIZONTAL);
         int horizontalMargin = mContext.getResources().getDimensionPixelSize(
@@ -197,7 +191,14 @@ public class AssistantTriggerScript {
         }
     }
 
-    public AssistantHeaderModel getHeaderModel() {
+    public AssistantHeaderModel createHeaderAndGetModel() {
+        mHeaderModel = new AssistantHeaderModel();
+        if (mHeaderCoordinator != null) {
+            mHeaderCoordinator.destroy();
+        }
+        mHeaderCoordinator = new AssistantHeaderCoordinator(mContext, mHeaderModel);
+        mHeaderModel.set(
+                AssistantHeaderModel.FEEDBACK_BUTTON_CALLBACK, mDelegate::onFeedbackButtonClicked);
         return mHeaderModel;
     }
 
@@ -249,7 +250,11 @@ public class AssistantTriggerScript {
         addChipsToContainer(mChipsContainer, mRightAlignedChips);
     }
 
-    public void show(boolean resizeVisualViewport) {
+    public boolean show(boolean resizeVisualViewport) {
+        if (mHeaderModel == null || mHeaderCoordinator == null) {
+            assert false : "createHeaderAndGetModel() must be called before show()";
+            return false;
+        }
         mResizeVisualViewport = resizeVisualViewport;
         createBottomSheetContents();
         update();
@@ -257,6 +262,7 @@ public class AssistantTriggerScript {
         mBottomSheetController.addObserver(mBottomSheetObserver);
         BottomSheetUtils.showContentAndMaybeExpand(mBottomSheetController, mContent,
                 /* shouldExpand = */ true, /* animate = */ mAnimateBottomSheet);
+        return true;
     }
 
     public void hide() {
