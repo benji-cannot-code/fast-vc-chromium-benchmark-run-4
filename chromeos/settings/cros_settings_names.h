7c416ec492fae46ecfcdd328ffe894c25e56367c
Pavol Marko <pmarko@google.com>
2020-11-23 16:26:09 +0000

    Remove unused CrosSetting kDeviceLoginScreenExtensions
    Looks like this was introduced in
    https://codereview.chromium.org/2111563002 and then renamed in
    https://codereview.chromium.org/2711553002
    but the introduced "plumbing"
    (https://codereview.chromium.org/2144313002)
    never actually used it. Instead, this is used through a pref.
    
    Bug: 1151389
    Test: compiles
    Change-Id: Idd809a01c3e9ea0eb69bb61db44e32cd9ad2cc29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2555017
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830197}

--- chromeos/settings/cros_settings_names.h
+++ chromeos/settings/cros_settings_names.h
@@ -192,9 +192,6 @@ COMPONENT_EXPORT(CHROMEOS_SETTINGS)
 extern const char kLoginVideoCaptureAllowedUrls[];
 
 COMPONENT_EXPORT(CHROMEOS_SETTINGS)
-extern const char kDeviceLoginScreenExtensions[];
-
-COMPONENT_EXPORT(CHROMEOS_SETTINGS)
 extern const char kDeviceLoginScreenLocales[];
 COMPONENT_EXPORT(CHROMEOS_SETTINGS)
 extern const char kDeviceLoginScreenInputMethods[];
