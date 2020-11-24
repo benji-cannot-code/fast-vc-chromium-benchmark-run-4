2f02bae755c639ba184eda34105948a5ac05a8f2
Sigurd Schneider <sigurds@chromium.org>
2020-11-24 09:36:22 +0000

    Properly support multi-client for requestWillBeSentExtraInfo CDP event
    If multiple CDP clients were attached, only the first would get a
    valid ClientSecurityState.
    
    Bug: chromium:1128885
    Change-Id: I6a54a1943b81dd1011288716d12e291b1ef315e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2554545
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830520}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/request-will-be-sent-extra-info-client-security-state-multiclient.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      `Tests that client security state is reported on requestWillBeSentExtraInfo.`);
+
+  await dp.Network.enable();
+  const dp2 = (await page.createSession()).protocol;
+  await dp2.Network.enable();
+  testRunner.log('Two sessions established and Network enabled on both');
+
+  const promise1 = dp.Network.onceRequestWillBeSentExtraInfo();
+  const promise2 = dp2.Network.onceRequestWillBeSentExtraInfo();
+
+  await session.evaluate(`fetch('index.html');`);
+
+  const [event1, event2] = await Promise.all([promise1, promise2]);
+  testRunner.log(event1.params.clientSecurityState)
+  testRunner.log(event2.params.clientSecurityState)
+
+
+  testRunner.completeTest();
+})
