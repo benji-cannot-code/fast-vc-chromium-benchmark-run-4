5305da5a39c6b750744dacd30f48247251a57571
Stepan Khapugin <stkhapugin@chromium.org>
2020-11-24 18:11:44 +0000

    [iOS] Fix shutdown crash with autocorrect.
    Fixes a crash when autocorrect is not accepted yet by the user, and the
    app is being killed in background.
    
    Fixed: 1150965
    Change-Id: I8f8196d882db53b5211dfcdac53afee7bc784962
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2555064
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#830630}

--- ios/chrome/browser/ui/omnibox/omnibox_view_controller.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_view_controller.mm
@@ -257,7 +257,11 @@ const CGFloat kClearButtonSize = 28.0f;
 - (BOOL)textField:(UITextField*)textField
     shouldChangeCharactersInRange:(NSRange)range
                 replacementString:(NSString*)newText {
-  DCHECK(_textChangeDelegate);
+  if (!_textChangeDelegate) {
+    // This can happen when the view controller is still alive but the model is
+    // already deconstructed on shutdown.
+    return YES;
+  }
   self.processingUserEvent = _textChangeDelegate->OnWillChange(range, newText);
   return self.processingUserEvent;
 }
@@ -281,14 +285,22 @@ const CGFloat kClearButtonSize = 28.0f;
   BOOL savedProcessingUserEvent = self.processingUserEvent;
   self.processingUserEvent = NO;
   self.forwardingOnDidChange = YES;
-  DCHECK(_textChangeDelegate);
+  if (!_textChangeDelegate) {
+    // This can happen when the view controller is still alive but the model is
+    // already deconstructed on shutdown.
+    return;
+  }
   _textChangeDelegate->OnDidChange(savedProcessingUserEvent);
   self.forwardingOnDidChange = NO;
 }
 
 // Delegate method for UITextField, called when user presses the "go" button.
 - (BOOL)textFieldShouldReturn:(UITextField*)textField {
-  DCHECK(_textChangeDelegate);
+  if (!_textChangeDelegate) {
+    // This can happen when the view controller is still alive but the model is
+    // already deconstructed on shutdown.
+    return YES;
+  }
   _textChangeDelegate->OnAccept();
   return NO;
 }
@@ -310,12 +322,20 @@ const CGFloat kClearButtonSize = 28.0f;
   self.isTextfieldEditing = YES;
 
   self.omniboxInteractedWhileFocused = NO;
-  DCHECK(_textChangeDelegate);
+  if (!_textChangeDelegate) {
+    // This can happen when the view controller is still alive but the model is
+    // already deconstructed on shutdown.
+    return;
+  }
   _textChangeDelegate->OnDidBeginEditing();
 }
 
 - (BOOL)textFieldShouldEndEditing:(UITextField*)textField {
-  DCHECK(_textChangeDelegate);
+  if (!_textChangeDelegate) {
+    // This can happen when the view controller is still alive but the model is
+    // already deconstructed on shutdown.
+    return YES;
+  }
   _textChangeDelegate->OnWillEndEditing();
 
   return YES;
@@ -333,7 +353,11 @@ const CGFloat kClearButtonSize = 28.0f;
 }
 
 - (BOOL)textFieldShouldClear:(UITextField*)textField {
-  DCHECK(_textChangeDelegate);
+  if (!_textChangeDelegate) {
+    // This can happen when the view controller is still alive but the model is
+    // already deconstructed on shutdown.
+    return YES;
+  }
   _textChangeDelegate->ClearText();
   self.processingUserEvent = YES;
   return YES;
@@ -341,17 +365,29 @@ const CGFloat kClearButtonSize = 28.0f;
 
 - (void)onCopy {
   self.omniboxInteractedWhileFocused = YES;
-  DCHECK(_textChangeDelegate);
+  if (!_textChangeDelegate) {
+    // This can happen when the view controller is still alive but the model is
+    // already deconstructed on shutdown.
+    return;
+  }
   _textChangeDelegate->OnCopy();
 }
 
 - (void)willPaste {
-  DCHECK(_textChangeDelegate);
+  if (!_textChangeDelegate) {
+    // This can happen when the view controller is still alive but the model is
+    // already deconstructed on shutdown.
+    return;
+  }
   _textChangeDelegate->WillPaste();
 }
 
 - (void)onDeleteBackward {
-  DCHECK(_textChangeDelegate);
+  if (!_textChangeDelegate) {
+    // This can happen when the view controller is still alive but the model is
+    // already deconstructed on shutdown.
+    return;
+  }
   _textChangeDelegate->OnDeleteBackward();
 }
 
