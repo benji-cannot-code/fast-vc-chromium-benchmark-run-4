0ece982d5fd8676fd8ab0fde7f2d1ccd55689e8b
Renato Silva <rrsilva@google.com>
2020-11-24 15:17:36 +0000

    OOBE - Migrate to HTMLImports polyfill
    Use the HTMLImports polyfill in OOBE to remove the dependence
    on HTML imports until everything can be fully migrated to JS
    modules/Polymer3.
    
    Bug: 1111387
    Change-Id: I3c760bdc5d41f57e44927363d47a5a193edd7c34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2522072
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830571}

--- chrome/browser/chromeos/login/test/oobe_base_test.cc
+++ chrome/browser/chromeos/login/test/oobe_base_test.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_service.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
 #include "google_apis/gaia/gaia_switches.h"
@@ -106,6 +107,12 @@ void OobeBaseTest::SetUpCommandLine(base::CommandLine* command_line) {
   if (!needs_background_networking_)
     command_line->AppendSwitch(::switches::kDisableBackgroundNetworking);
   command_line->AppendSwitchASCII(chromeos::switches::kLoginProfile, "user");
+
+  // Blink features are controlled via a command line switch. Disable HTML
+  // imports which are deprecated. OOBE uses a polyfill for imports that will
+  // be replaced once the migration to JS modules is complete.
+  command_line->AppendSwitchASCII(::switches::kDisableBlinkFeatures,
+                                  "HTMLImports");
 
   MixinBasedInProcessBrowserTest::SetUpCommandLine(command_line);
 }
--- chrome/browser/resources/chromeos/login/structure/components_common.js
+++ chrome/browser/resources/chromeos/login/structure/components_common.js
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
+function loadCommonComponents() {
+// TODO(crbug.com/1111387) - Remove excessive logging.
+  console.warn('loadCommonComponents() : Starting to load common components.');
 // This inclusion is types-only. No actual code to execute.
 // <include src="../components/oobe_types.js">
 
@@ -52,3 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="../screen_app_launch_splash.js">
 
 // <include src="components_[OOBE].js">
+// TODO(crbug.com/1111387) - Remove excessive logging.
+  console.warn('loadCommonComponents() : Common components have loaded.');
+}
