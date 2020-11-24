758b642f02eba000b8e970d3afb06bf6530481c9
Risan <risan@google.com>
2020-11-24 07:35:17 +0000

    Set MyFiles as visible device
    In the Android CLs, we allow this to happen and we prohibited the
    creation of the Android directories. So apps can access the MyFiles
    through Storage frameworks, but no default directories to be
    created.
    
    BUG=b:162793388
    BUG=b:169537013
    TEST=Manual test in Photos app, CtsAppSecurityHostTestCases,
    CtsProviderTestCases, part of CtsMediaTestCases
    
    Change-Id: I98ab33d6d68adfb1638bcdf07557817df257c6f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2536424
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Risan <risan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830496}

--- components/arc/volume_mounter/arc_volume_mounter_bridge.cc
+++ components/arc/volume_mounter/arc_volume_mounter_bridge.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
 #include "components/arc/arc_features.h"
 #include "components/arc/arc_prefs.h"
+#include "components/arc/arc_util.h"
 #include "components/arc/session/arc_bridge_service.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_prefs/user_prefs.h"
@@ -132,9 +133,11 @@ void ArcVolumeMounterBridge::SendMountEventForMyFiles() {
   // TODO(niwa): Add a new DeviceType enum value for MyFiles.
   chromeos::DeviceType device_type = chromeos::DeviceType::DEVICE_TYPE_SD;
 
+  // Conditionally set MyFiles to be visible for P and invisible for R. In R, we use IsVisibleRead
+  // so this is not needed.
   volume_mounter_instance->OnMountEvent(mojom::MountPointInfo::New(
       DiskMountManager::MOUNTING, kMyFilesPath, kMyFilesPath, kMyFilesUuid,
-      device_label, device_type, false));
+      device_label, device_type, !IsArcVmEnabled()));
 }
 
 bool ArcVolumeMounterBridge::IsVisibleToAndroidApps(
