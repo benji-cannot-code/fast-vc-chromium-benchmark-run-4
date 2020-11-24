15189338fea0bfe863920f2d0ffdddeebb3b59ab
Tina Wang <tinazwang@google.com>
2020-11-24 18:46:46 +0000

    [ios] Add feature flag for incognito mode feature
    Add a feature flag to guard the changes for IncognitoModeAvailability
    policy related changes.
    
    Bug: 1148838
    Change-Id: Ibc563259651424c61e189b9ee74dc5c96135af91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2555257
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830642}

--- ios/chrome/browser/policy/policy_features.cc
+++ ios/chrome/browser/policy/policy_features.cc
@@ -19,6 +19,9 @@ const base::Feature kEditBookmarksIOS{"EditBookmarksIOS",
 const base::Feature kManagedBookmarksIOS{"ManagedBookmarksIOS",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kEnableIncognitoModeAvailabilityIOS{
+    "EnableIncognitoModeAvailabilityIOS", base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kURLBlocklistIOS{"URLBlocklistIOS",
                                      base::FEATURE_DISABLED_BY_DEFAULT};
 
@@ -47,6 +50,10 @@ bool IsEditBookmarksIOSEnabled() {
 
 bool IsEnterprisePolicyEnabled() {
   return !IsDisableEnterprisePolicySwitchPresent();
+}
+
+bool IsIncognitoModeAvailable() {
+  return base::FeatureList::IsEnabled(kEnableIncognitoModeAvailabilityIOS);
 }
 
 bool ShouldInstallEnterprisePolicyHandlers() {
