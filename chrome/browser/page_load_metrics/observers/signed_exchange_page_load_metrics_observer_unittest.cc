869bfb770d58a72c1e09a625f3f62d1ac5df7a5d
Yi Gu <yigu@chromium.org>
2020-11-24 16:51:01 +0000

    Disable WithSignedExchangeBackground on linux tsan
    The test is very flaky on linux tsan:
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=unit_tests%20(with%20patch)&tests=SignedExchangePageLoadMetricsObserverTest.WithSignedExchangeBackground
    
    TBR=ksakamoto@chromium.org
    Bug: 1082135
    
    Change-Id: I48c41234e3d46fdb51f724f6a07dee2b743cb51a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2558164
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830596}

--- chrome/browser/page_load_metrics/observers/signed_exchange_page_load_metrics_observer_unittest.cc
+++ chrome/browser/page_load_metrics/observers/signed_exchange_page_load_metrics_observer_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "build/build_config.h"
 #include "chrome/browser/page_load_metrics/observers/page_load_metrics_observer_test_harness.h"
 #include "components/page_load_metrics/browser/page_load_tracker.h"
 #include "components/page_load_metrics/common/test/page_load_metrics_test_util.h"
@@ -317,8 +318,14 @@ TEST_F(SignedExchangePageLoadMetricsObserverTest, WithCachedSignedExchange) {
       internal::kHistogramNotCachedSignedExchangePrefix);
 }
 
+// Test is flaky on linux_tsan: crbug.com:1082135.
+#if defined(OS_LINUX) && defined(THREAD_SANITIZER)
+#define MAYBE_WithSignedExchangeBackground DISABLED_WithSignedExchangeBackground
+#else
+#define MAYBE_WithSignedExchangeBackground WithSignedExchangeBackground
+#endif
 TEST_F(SignedExchangePageLoadMetricsObserverTest,
-       WithSignedExchangeBackground) {
+       MAYBE_WithSignedExchangeBackground) {
   page_load_metrics::mojom::PageLoadTiming timing;
   page_load_metrics::InitPageLoadTimingForTest(&timing);
   PopulateRequiredTimingFields(&timing);
