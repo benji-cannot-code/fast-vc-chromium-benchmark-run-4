3e8dffecec5a0e74433c68871e1954b1fef38fb9
Greg Thompson <grt@chromium.org>
2020-11-24 00:22:35 +0000

    [iwyu] Remove needless inclusion of base/files/file_path_watcher.h.
    BUG=None
    
    Change-Id: I365e7cab9f8c1c9f64b9e09241920ff477471201
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550908
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830380}

--- chrome/browser/chrome_browser_main_win.h
+++ chrome/browser/chrome_browser_main_win.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/files/file_path_watcher.h"
 #include "chrome/browser/chrome_browser_main.h"
 
 class ModuleWatcher;
