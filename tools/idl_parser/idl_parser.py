56846b968e114838588414f5995edb808277fb31
Taeho Nam <thn7440@gmail.com>
2020-11-24 08:54:01 +0000

    Fix typo
    Change-Id: I1a73ce66491885f38bb481c9c7f2b565def536ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551892
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830510}

--- tools/idl_parser/idl_parser.py
+++ tools/idl_parser/idl_parser.py
@@ -1231,7 +1231,7 @@ class IDLParser(object):
 #
 # BuildError
 #
-# Build and Errror node as part of the recovery process.
+# Build and Error node as part of the recovery process.
 #
 #
   def BuildError(self, p, prod):
