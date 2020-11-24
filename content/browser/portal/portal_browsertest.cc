ad46d58cc210b25a88e976f9f9be7ef314429f7d
Adithya Srinivasan <adithyas@chromium.org>
2020-11-24 21:06:50 +0000

    Portals: Remove targetOrigin from portals postMessage API
    Bug: 1126901
    Change-Id: I555e37125e1deab4b470293e59efb40c340943d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2386012
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830704}

--- content/browser/portal/portal_browsertest.cc
+++ content/browser/portal/portal_browsertest.cc
@@ -1404,8 +1404,7 @@ IN_PROC_BROWSER_TEST_F(PortalBrowserTest,
 
   // Get the portal renderer to access the WebContents.
   RenderProcessHostBadIpcMessageWaiter kill_waiter(portal_frame->GetProcess());
-  ExecuteScriptAsync(portal_frame,
-                     "window.portalHost.postMessage('message', '*');");
+  ExecuteScriptAsync(portal_frame, "window.portalHost.postMessage('message');");
   EXPECT_EQ(bad_message::RPH_MOJO_PROCESS_ERROR, kill_waiter.Wait());
 }
 
