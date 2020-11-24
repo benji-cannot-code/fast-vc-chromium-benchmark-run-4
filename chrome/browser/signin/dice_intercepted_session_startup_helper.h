0aff0346d92abeb11de6dda4d9ca8c73b56c110c
David Roger <droger@chromium.org>
2020-11-24 09:39:49 +0000

    [signin] Fix timeout issues in DiceInterceptionSessionStartupHelper
    This is a tentative fix, as I could not repro the issue.
    It is theoritically possible that the call to
    TriggerCookieJarUpdate()
    does nothing.
    
    The scenario would look like this:
    - Reconcilor starts
    - Reconcilor does a ListAccounts
    - At this point DiceInterceptedSessionStartupHelper calls
      TriggerCookieJarUpdate, but this is a no-op because a ListAccount is
      already running.
    - Reconcilor finishes running and adds the account, but
      OnAccountsInCookieUpdated is never called (because bug 1051864)
    - this results in a timeout of DiceInterceptedSessionStartupHelper
    
    The CL also adds an histogram to track reconcilor errors, as they would
    explain the timeout as well.
    
    Bug: 1151313
    Change-Id: I5082ffa377c3c3c4e605d75ae79545811593ac3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550814
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830521}

--- chrome/browser/signin/dice_intercepted_session_startup_helper.h
+++ chrome/browser/signin/dice_intercepted_session_startup_helper.h
@@ -8,8 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "base/cancelable_callback.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "base/time/time.h"
+#include "components/signin/core/browser/account_reconcilor.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "google_apis/gaia/core_account_id.h"
@@ -33,7 +34,8 @@ class Profile;
 // in the content area (cookies).
 class DiceInterceptedSessionStartupHelper
     : public content::WebContentsObserver,
-      public signin::IdentityManager::Observer {
+      public signin::IdentityManager::Observer,
+      public AccountReconcilor::Observer {
  public:
   // |profile| is the new profile that was created after signin interception.
   // |account_id| is the main account for the profile, it's already in the
@@ -59,6 +61,9 @@ class DiceInterceptedSessionStartupHelper
       const signin::AccountsInCookieJarInfo& accounts_in_cookie_jar_info,
       const GoogleServiceAuthError& error) override;
 
+  // AccountReconcilor::Observer:
+  void OnStateChanged(signin_metrics::AccountReconcilorState state) override;
+
  private:
   // Creates a browser with a new tab, and closes the intercepted tab if it's
   // still open.
@@ -67,8 +72,12 @@ class DiceInterceptedSessionStartupHelper
   Profile* const profile_;
   CoreAccountId account_id_;
   base::OnceClosure callback_;
-  ScopedObserver<signin::IdentityManager, signin::IdentityManager::Observer>
+  bool reconcile_error_encountered_ = false;
+  base::ScopedObservation<signin::IdentityManager,
+                          signin::IdentityManager::Observer>
       accounts_in_cookie_observer_{this};
+  base::ScopedObservation<AccountReconcilor, AccountReconcilor::Observer>
+      reconcilor_observer_{this};
   base::TimeTicks session_startup_time_;
   // Timeout while waiting for the account to be added to the cookies in the new
   // profile.
