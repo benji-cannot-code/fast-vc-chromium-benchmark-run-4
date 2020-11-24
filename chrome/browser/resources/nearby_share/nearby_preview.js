27f002b4f23e9c203b92afa3db44b381ba299837
Curt Clemens <cclem@google.com>
2020-11-24 14:07:36 +0000

    [Nearby] Set file preview icons grey on error
    The file preview icon on the discovery page and confirmation page needs
    to be grey whenever those pages are in an error state. This CL adds a
    |disabled| property to nearby-preview that when true applies a css
    class that makes the icon grey.
    
    Bug: b/163036847
    Change-Id: I4a0f24a1ed9f013d499eb2dff9bf24064f0898e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2554901
    Reviewed-by: James Vecore <vecore@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Curt Clemens <cclem@google.com>
    Cr-Commit-Position: refs/heads/master@{#830550}

--- chrome/browser/resources/nearby_share/nearby_preview.js
+++ chrome/browser/resources/nearby_share/nearby_preview.js
@@ -32,6 +32,14 @@ Polymer({
       value: null,
     },
 
+    /**
+     * Controls whether the icon should be greyed out.
+     * @type {boolean}
+     */
+    disabled: {
+      type: Boolean,
+      value: false,
+    },
   },
 
   /**
@@ -94,5 +102,16 @@ Polymer({
             'No icon defined for share type ' + this.sendPreview.shareType);
         return 'nearbysharetype68:unknown-file';
     }
+  },
+
+  /**
+   * @return {string} The css class to be applied to the icon.
+   * @private
+   */
+  getIconClass_() {
+    if (this.disabled) {
+      return 'disabled';
+    }
+    return '';
   },
 });
