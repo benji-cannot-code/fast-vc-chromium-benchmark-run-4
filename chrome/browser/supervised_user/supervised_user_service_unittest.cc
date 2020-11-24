50a6afc2f41073d4fb8eee6528de8d923830d043
Robbie McElrath <rmcelrath@chromium.org>
2020-11-24 20:39:31 +0000

    Rename //chrome/test/data/[whitelists -> allowlists]/*
    Bug: 1097244
    Change-Id: If4b88e957f0a91d76694f68dce2a91515cff6ebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551620
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830694}

--- chrome/browser/supervised_user/supervised_user_service_unittest.cc
+++ chrome/browser/supervised_user/supervised_user_service_unittest.cc
@@ -573,7 +573,7 @@ TEST_F(SupervisedUserServiceExtensionTest, InstallContentPacks) {
   SupervisedUserAllowlistService* allowlist_service =
       supervised_user_service->GetAllowlistService();
   base::FilePath allowlist_path =
-      test_data_dir.AppendASCII("whitelists/content_pack/site_list.json");
+      test_data_dir.AppendASCII("allowlists/content_pack/site_list.json");
   allowlist_service->LoadAllowlistForTesting(id1, title1, allowlist_path);
   site_list_observer_.Wait();
 
@@ -590,7 +590,7 @@ TEST_F(SupervisedUserServiceExtensionTest, InstallContentPacks) {
 
   // Load a second allowlist.
   allowlist_path =
-      test_data_dir.AppendASCII("whitelists/content_pack_2/site_list.json");
+      test_data_dir.AppendASCII("allowlists/content_pack_2/site_list.json");
   allowlist_service->LoadAllowlistForTesting(id2, title2, allowlist_path);
   site_list_observer_.Wait();
 
