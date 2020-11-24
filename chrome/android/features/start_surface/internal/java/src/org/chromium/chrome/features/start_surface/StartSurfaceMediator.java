7b65afd6acda56702c22d4c7d54ca9a007088639
spdonghao <spdonghao@chromium.org>
2020-11-24 01:06:31 +0000

    [Instant Start] Stop showing feed placeholder on warm start.
    Add a warm start check for
    StartSurfaceMediator#shouldShowFeedPlaceholder.
    
    Bug: 1148218
    Change-Id: I57d5a9132716f84fa2c0acf2e87c8d35e95cb099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545269
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830402}

--- chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceMediator.java
+++ chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceMediator.java
@@ -167,6 +167,7 @@ class StartSurfaceMediator
      */
     @Nullable
     private Boolean mFeedVisibilityInSharedPreferenceOnStartUp;
+    private boolean mHadWarmStart;
 
     StartSurfaceMediator(TabSwitcher.Controller controller, TabModelSelector tabModelSelector,
             @Nullable PropertyModel propertyModel,
@@ -174,7 +175,8 @@ class StartSurfaceMediator
             @SurfaceMode int surfaceMode, Context context,
             BrowserControlsStateProvider browserControlsStateProvider,
             ActivityStateChecker activityStateChecker, boolean excludeMVTiles,
-            boolean showStackTabSwitcher, OneshotSupplier<StartSurface> startSurfaceSupplier) {
+            boolean showStackTabSwitcher, OneshotSupplier<StartSurface> startSurfaceSupplier,
+            boolean hadWarmStart) {
         mController = controller;
         mTabModelSelector = tabModelSelector;
         mPropertyModel = propertyModel;
@@ -186,6 +188,7 @@ class StartSurfaceMediator
         mExcludeMVTiles = excludeMVTiles;
         mShowStackTabSwitcher = showStackTabSwitcher;
         mStartSurfaceSupplier = startSurfaceSupplier;
+        mHadWarmStart = hadWarmStart;
 
         if (mPropertyModel != null) {
             assert mSurfaceMode == SurfaceMode.SINGLE_PANE || mSurfaceMode == SurfaceMode.TWO_PANES
@@ -753,7 +756,7 @@ class StartSurfaceMediator
 
         return mSurfaceMode == SurfaceMode.SINGLE_PANE
                 && CachedFeatureFlags.isEnabled(ChromeFeatureList.INSTANT_START)
-                && StartSurfaceConfiguration.getFeedArticlesVisibility();
+                && StartSurfaceConfiguration.getFeedArticlesVisibility() && !mHadWarmStart;
     }
 
     /** This interface builds the feed surface coordinator when showing if needed. */
