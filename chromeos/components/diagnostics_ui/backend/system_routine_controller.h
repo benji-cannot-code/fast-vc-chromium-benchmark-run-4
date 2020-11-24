51ab9db383151a993435d4d14ab2cbe182058814
Bailey Berro <baileyberro@google.com>
2020-11-24 00:32:49 +0000

    Add GetSupportedRoutines to SystemRoutineController
    GetSupportedRoutines returns a list of supported RoutineTypes
    
    
    Bug: 1128204
    Change-Id: I213ff5d75456ccab3e1e53b9f4dcb888f3a58ded
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2528302
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830384}

--- chromeos/components/diagnostics_ui/backend/system_routine_controller.h
+++ chromeos/components/diagnostics_ui/backend/system_routine_controller.h
@@ -42,6 +42,7 @@ class SystemRoutineController : public mojom::SystemRoutineController {
   SystemRoutineController& operator=(const SystemRoutineController&) = delete;
 
   // mojom::SystemRoutineController:
+  void GetSupportedRoutines(GetSupportedRoutinesCallback callback) override;
   void RunRoutine(mojom::RoutineType type,
                   mojo::PendingRemote<mojom::RoutineRunner> runner) override;
 
@@ -49,6 +50,11 @@ class SystemRoutineController : public mojom::SystemRoutineController {
       mojo::PendingReceiver<mojom::SystemRoutineController> pending_receiver);
 
  private:
+  void OnAvailableRoutinesFetched(
+      GetSupportedRoutinesCallback callback,
+      const std::vector<cros_healthd::mojom::DiagnosticRoutineEnum>&
+          supported_routines);
+
   void ExecuteRoutine(mojom::RoutineType routine_type);
 
   void OnRoutineStarted(
--- chromeos/components/diagnostics_ui/backend/system_routine_controller_unittest.cc
+++ chromeos/components/diagnostics_ui/backend/system_routine_controller_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/json/json_writer.h"
 #include "base/run_loop.h"
+#include "base/stl_util.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "chromeos/dbus/cros_healthd/fake_cros_healthd_client.h"
@@ -107,6 +108,12 @@ std::string ConstructPowerRoutineResultJson(double charge_percent) {
   const bool serialize_success = base::JSONWriter::Write(output_dict, &json);
   DCHECK(serialize_success);
   return json;
+}
+
+void SetAvailableRoutines(
+    const std::vector<healthd::DiagnosticRoutineEnum>& routines) {
+  cros_healthd::FakeCrosHealthdClient::Get()->SetAvailableRoutinesForTesting(
+      routines);
 }
 
 }  // namespace
@@ -425,6 +432,37 @@ TEST_F(SystemRoutineControllerTest, PowerRoutineSuccess) {
       ConstructPowerRoutineResult(mojom::StandardRoutineResult::kTestPassed,
                                   expected_percent_charge,
                                   expected_time_elapsed_seconds));
+}
+
+TEST_F(SystemRoutineControllerTest, AvailableRoutines) {
+  SetAvailableRoutines({healthd::DiagnosticRoutineEnum::kFloatingPointAccuracy,
+                        healthd::DiagnosticRoutineEnum::kMemory,
+                        healthd::DiagnosticRoutineEnum::kPrimeSearch,
+                        healthd::DiagnosticRoutineEnum::kAcPower,
+                        healthd::DiagnosticRoutineEnum::kBatteryCapacity,
+                        healthd::DiagnosticRoutineEnum::kBatteryHealth});
+
+  base::RunLoop run_loop;
+  system_routine_controller_->GetSupportedRoutines(base::BindLambdaForTesting(
+      [&](const std::vector<mojom::RoutineType>& supported_routines) {
+        EXPECT_EQ(3u, supported_routines.size());
+        EXPECT_FALSE(base::Contains(supported_routines,
+                                    mojom::RoutineType::kBatteryCharge));
+        EXPECT_FALSE(base::Contains(supported_routines,
+                                    mojom::RoutineType::kBatteryDischarge));
+        EXPECT_FALSE(
+            base::Contains(supported_routines, mojom::RoutineType::kCpuCache));
+        EXPECT_FALSE(
+            base::Contains(supported_routines, mojom::RoutineType::kCpuStress));
+        EXPECT_TRUE(base::Contains(supported_routines,
+                                   mojom::RoutineType::kCpuFloatingPoint));
+        EXPECT_TRUE(
+            base::Contains(supported_routines, mojom::RoutineType::kCpuPrime));
+        EXPECT_TRUE(
+            base::Contains(supported_routines, mojom::RoutineType::kMemory));
+        run_loop.Quit();
+      }));
+  run_loop.Run();
 }
 
 }  // namespace diagnostics
