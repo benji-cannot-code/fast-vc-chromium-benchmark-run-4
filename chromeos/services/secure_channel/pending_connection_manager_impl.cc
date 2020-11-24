536bf23921bd456d2e5a0d792f72ae38427ac398
Maksim Ivanov <emaxx@chromium.org>
2020-11-24 18:22:56 +0000

    Fix moved-from-dereference crash in //chromeos/services/secure_channel/
    Fix use-after-move (potential) bugs found by the
    "bugprone-use-after-move" clang-tidy check.
    
    Bug: 1122844
    Change-Id: I1abda42fcea0837c64c2c1aa1a82acaa5ff93aad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2556518
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830636}

--- chromeos/services/secure_channel/pending_connection_manager_impl.cc
+++ chromeos/services/secure_channel/pending_connection_manager_impl.cc
@@ -187,8 +187,7 @@ void PendingConnectionManagerImpl::HandleBleInitiatorRequest(
   if (!success) {
     PA_LOG(ERROR) << "PendingConnectionManagerImpl::"
                   << "HandleBleInitiatorRequest(): Not able to handle request. "
-                  << "Details: " << connection_attempt_details
-                  << ", Client parameters: " << *client_connection_parameters;
+                  << "Details: " << connection_attempt_details;
     NOTREACHED();
   }
 }
@@ -220,8 +219,7 @@ void PendingConnectionManagerImpl::HandleBleListenerRequest(
   if (!success) {
     PA_LOG(ERROR) << "PendingConnectionManagerImpl::"
                   << "HandleBleListenerRequest(): Not able to handle request. "
-                  << "Details: " << connection_attempt_details
-                  << ", Client parameters: " << *client_connection_parameters;
+                  << "Details: " << connection_attempt_details;
     NOTREACHED();
   }
 }
@@ -271,8 +269,7 @@ void PendingConnectionManagerImpl::HandleNearbyInitiatorRequest(
     PA_LOG(ERROR)
         << "PendingConnectionManagerImpl::"
         << "HandleNearbyInitiatorRequest(): Not able to handle request. "
-        << "Details: " << connection_attempt_details
-        << ", Client parameters: " << *client_connection_parameters;
+        << "Details: " << connection_attempt_details;
     NOTREACHED();
   }
 }
