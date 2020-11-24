19a526b22f924df4103c07662f375947a9993f70
Gordon Seto <gordonseto@google.com>
2020-11-24 21:34:10 +0000

    [CrOS Settings] Add invalid activation code UI.
    Add UI for informing when an activation code is invalid in the
    activation code page.
    
    Screenshot:
    https://screenshot.googleplex.com/3gLCppUSwZbqovc.png
    
    Bug: 1093185
    Change-Id: Ieb9959cdbd0dd754454eaa69d5c5f4ac5453c8fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2555917
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830714}

--- ui/webui/resources/cr_components/chromeos/cellular_setup/activation_code_page.js
+++ ui/webui/resources/cr_components/chromeos/cellular_setup/activation_code_page.js
@@ -17,14 +17,17 @@ const PageState = {
   SWITCHING_CAM_USER_TO_ENVIRONMENT: 4,
   SWITCHING_CAM_ENVIRONMENT_TO_USER: 5,
   SUCCESS: 6,
+  FAILURE: 7,
 };
 
 /** @enum {number} */
 const UiElement = {
   START_SCANNING: 1,
   VIDEO: 2,
-  SCAN_SUCCESS: 3,
-  SWITCH_CAMERA: 4,
+  SWITCH_CAMERA: 3,
+  SCAN_FINISH: 4,
+  SCAN_SUCCESS: 5,
+  SCAN_FAILURE: 6,
 };
 
 /**
@@ -43,6 +46,12 @@ Polymer({
       observer: 'onActivationCodeChanged_',
     },
 
+    showError: {
+      type: Boolean,
+      notify: true,
+      observer: 'onShowErrorChanged_',
+    },
+
     /**
      * @type {!PageState}
      * @private
@@ -50,6 +59,7 @@ Polymer({
     state_: {
       type: Object,
       value: PageState,
+      observer: 'onStateChanged_',
     },
 
     /** @private */
@@ -226,12 +236,13 @@ Polymer({
   onActivationCodeChanged_() {
     const activationCode = this.validateActivationCode_(this.activationCode);
     this.fire('activation-code-updated', {activationCode: activationCode});
-    // TODO(crbug.com/1093185): Handle if activation code is invalid.
     if (activationCode) {
       if (this.stream_) {
         this.stream_.getTracks()[0].stop();
       }
       this.state_ = PageState.SUCCESS;
+    } else {
+      this.state_ = PageState.FAILURE;
     }
   },
 
@@ -261,6 +272,20 @@ Polymer({
     this.startScanning_();
   },
 
+  /** @private */
+  onShowErrorChanged_() {
+    if (this.showError) {
+      this.state_ = PageState.FAILURE;
+    }
+  },
+
+  /** @private */
+  onStateChanged_() {
+    if (this.state_ !== PageState.FAILURE) {
+      this.showError = false;
+    }
+  },
+
   /**
    * @param {UiElement} uiElement
    * @param {PageState} state
@@ -274,12 +299,16 @@ Polymer({
       case UiElement.VIDEO:
         return state !== PageState.SCANNING_USER_FACING &&
             state !== PageState.SCANNING_ENVIRONMENT_FACING;
-      case UiElement.SCAN_SUCCESS:
-        return state !== PageState.SUCCESS;
       case UiElement.SWITCH_CAMERA:
         const isScanning = state === PageState.SCANNING_USER_FACING ||
             state === PageState.SCANNING_ENVIRONMENT_FACING;
         return !(isScanning && hasMultipleCameras);
+      case UiElement.SCAN_FINISH:
+        return state !== PageState.SUCCESS && state !== PageState.FAILURE;
+      case UiElement.SCAN_SUCCESS:
+        return state !== PageState.SUCCESS;
+      case UiElement.SCAN_FAILURE:
+        return state !== PageState.FAILURE;
     }
   },
 
