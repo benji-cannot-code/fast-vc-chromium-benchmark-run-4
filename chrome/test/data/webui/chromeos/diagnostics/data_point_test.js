12e2e5cc49be857df294daa80a3f5cceabfa1ceb
Michael Checo <michaelcheco@google.com>
2020-11-24 00:44:27 +0000

    Diagnostics: Add optional info icon and tooltip to data points
    Screenshots:
     Icon - http://shortn/_hG1JGQd12t
     Tooltip - http://shortn/_tJkYTmOMQG
    
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I5f73cc236774f043bbe5cf7f37735b3ecf9adef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2542846
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830390}

--- chrome/test/data/webui/chromeos/diagnostics/data_point_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/data_point_test.js
@@ -4,9 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://diagnostics/data_point.js';
-
 import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
-import {flushTasks} from '../../test_util.m.js';
+import {flushTasks, isVisible} from '../../test_util.m.js';
+
+import * as dx_utils from './diagnostics_test_utils.js';
 
 export function dataPointTestSuite() {
   /** @type {?DataPointElement} */
@@ -26,8 +27,9 @@ export function dataPointTestSuite() {
   /**
    * @param {string} header
    * @param {string} value
+   * @param {string=} tooltipText
    */
-  function initializeDataPoint(header, value) {
+  function initializeDataPoint(header, value, tooltipText = '') {
     assertFalse(!!dataPointElement);
 
     // Add the data point to the DOM.
@@ -36,6 +38,7 @@ export function dataPointTestSuite() {
     assertTrue(!!dataPointElement);
     dataPointElement.header = header;
     dataPointElement.value = value;
+    dataPointElement.tooltipText = tooltipText;
     document.body.appendChild(dataPointElement);
 
     return flushTasks();
@@ -44,9 +47,25 @@ export function dataPointTestSuite() {
   test('InitializeDataPoint', () => {
     const header = 'Test header';
     const value = 'Test value';
+    const tooltipText = 'Test tooltip';
+    return initializeDataPoint(header, value, tooltipText).then(() => {
+      dx_utils.assertElementContainsText(
+          dataPointElement.$$('.header > span'), header);
+      dx_utils.assertElementContainsText(dataPointElement.$$('.value'), value);
+      assertTrue(isVisible(
+          /**@type {!HTMLElement} */ (dataPointElement.$$('#infoIcon'))));
+      dx_utils.assertElementContainsText(
+          dataPointElement.$$('paper-tooltip'), tooltipText);
+    });
+  });
+
+  test('InitializeDataPointWithoutTooltip', () => {
+    const header = 'Test header';
+    const value = 'Test value';
     return initializeDataPoint(header, value).then(() => {
-      assertEquals(header, dataPointElement.$$('.header').textContent.trim());
-      assertEquals(value, dataPointElement.$$('.value').textContent.trim());
+      // Icon should be hidden when tooltip text is not provided.
+      assertFalse(isVisible(
+          /**@type {!HTMLElement} */ (dataPointElement.$$('#infoIcon'))));
     });
   });
 }
--- chromeos/components/diagnostics_ui/resources/data_point.js
+++ chromeos/components/diagnostics_ui/resources/data_point.js
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/icons.m.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import 'chrome://resources/polymer/v3_0/paper-tooltip/paper-tooltip.js';
 import './diagnostics_fonts_css.js';
 import './diagnostics_shared_css.js';
 
@@ -11,7 +14,8 @@ import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bun
 /**
  * @fileoverview
  * 'data-point' shows a single piece of information related to a component. It
- *  consists of a header and value.
+ *  consists of a header, value, and tooltip that provides context about the
+ *  item.
  */
 Polymer({
   is: 'data-point',
@@ -26,6 +30,12 @@ Polymer({
 
     /** @type {string} */
     value: {
+      type: String,
+      value: '',
+    },
+
+    /** @type {string} */
+    tooltipText: {
       type: String,
       value: '',
     },
