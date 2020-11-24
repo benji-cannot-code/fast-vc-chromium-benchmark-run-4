34fbce5fc53580c93b74c8bda12ee3e95dff5587
Nohemi Fernandez <fernandex@chromium.org>
2020-11-24 19:17:01 +0000

    [iOS][MICE] Display Sync consent flow when user clicks on "Sync off".
    This patch adds a sign-in entry point to the "Sync off" button in the
    Settings screen.
    
    Bug: 1125631
    Change-Id: Ib45b1794b9028c229b7cc1442de26581a0987b00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2514149
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830657}

--- ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_coordinator.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_coordinator.h"
 
+#import "base/feature_list.h"
 #import "base/mac/foundation_util.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/signin/authentication_service.h"
@@ -108,7 +109,8 @@ const CGFloat kFadeOutAnimationDuration = 0.16f;
   authenticationService->WaitUntilCacheIsPopulated();
   // The user should be signed out before triggering sign-in or upgrade states.
   // Users are allowed to be signed-in during FirstRun for testing purposes.
-  DCHECK(!authenticationService->IsAuthenticated() ||
+  DCHECK(base::FeatureList::IsEnabled(signin::kMobileIdentityConsistency) ||
+         !authenticationService->IsAuthenticated() ||
          self.signinIntent == UserSigninIntentFirstRun);
   [super start];
   self.viewController = [[UserSigninViewController alloc] init];
