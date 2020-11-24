4d826fd40ff4920efd5b2a49b1f312c16190c58e
Alice Wang <aliceywang@chromium.org>
2020-11-24 20:35:57 +0000

    [Android][Modularization] Move WebSigninBridge to signin.services target
    This CL moves WebSigninBridge to signin.services target.
    
    Bug: 1152432
    Change-Id: I5736852287f6dfdfd6577ec7cca7b6b99ef5a8ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2556845
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830692}

--- chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/WebSigninBridge.java
+++ chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/WebSigninBridge.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.signin;
+package org.chromium.chrome.browser.signin.services;
 
 import androidx.annotation.MainThread;
 
@@ -76,7 +76,7 @@ public class WebSigninBridge {
     }
 
     @CalledByNative
-    private static void onSigninSucceded(Listener listener) {
+    private static void onSigninSucceeded(Listener listener) {
         listener.onSigninSucceeded();
     }
 
