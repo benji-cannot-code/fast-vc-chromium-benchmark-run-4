1f0eb1f5a17f91c0eea9e01b648a2e4e14f3aaac
Aleks Totic <atotic@chromium.org>
2020-11-24 22:53:12 +0000

    [TablesNG] Baseline edge case tweaks
    Bug: 958381
    Change-Id: I5dfd967b14adc19da07ea23bb38fe9f8d987dbce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2559250
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830756}

--- third_party/blink/renderer/core/layout/ng/ng_box_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_box_fragment.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
-#include "third_party/blink/renderer/platform/text/text_direction.h"
 #include "third_party/blink/renderer/platform/text/writing_mode.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
 
@@ -68,6 +67,15 @@ class CORE_EXPORT NGBoxFragment final : public NGFragment {
     const NGPhysicalBoxFragment& physical_box_fragment =
         To<NGPhysicalBoxFragment>(physical_fragment_);
     return physical_box_fragment.Padding().ConvertToLogical(writing_direction_);
+  }
+
+  bool HasDescendantsForTableCell() const {
+    const NGPhysicalBoxFragment& box_fragment =
+        To<NGPhysicalBoxFragment>(physical_fragment_);
+    DCHECK(box_fragment.IsTableNGCell());
+    return !box_fragment.Children().empty() ||
+           box_fragment.HasOutOfFlowPositionedFragmentainerDescendants() ||
+           box_fragment.HasOutOfFlowPositionedDescendants();
   }
 };
 
