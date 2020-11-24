dd4642c8334275551961b866cdd44e4b95f51c98
Ehimare Okoyomon <eokoyomon@chromium.org>
2020-11-24 15:40:57 +0000

    [Android] Edit permission icon strikethrough overlay
    Make strikethrough thickness relative to icon size and introduce a bias
    to shift the line to only one side of the transparent line, in order to
    match mocks.
    
    Screenshot: crbug.com/1077766#c106
    Bug: 1077766
    Change-Id: I0a4730d8aba05b6f266da1b0ba53dba254cc4000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2552861
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830578}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleWebsiteSettings.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleWebsiteSettings.java
@@ -363,7 +363,7 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
                 : ContentSettingsResources.getDisabledIcon(contentSettingsType, getResources());
         if (getSiteSettingsClient().isPageInfoV2Enabled() && value != null
                 && value == ContentSettingValues.BLOCK) {
-            return ContentSettingsResources.getBlockedIcon(getResources(), icon);
+            return ContentSettingsResources.getBlockedSquareIcon(getResources(), icon);
         }
         return icon;
     }
