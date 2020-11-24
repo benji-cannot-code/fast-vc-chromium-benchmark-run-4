d36989c89795f946f4caf37b8423796aaae39f27
Mikhail Khokhlov <khokhlov@google.com>
2020-11-24 14:57:55 +0000

    chrome/android: Add a UMA histogram for number of radio wakeups
    Monitors the radio state every second while Chrome is in foreground
    to count the number of times the radio woke up from dormant state.
    This metric will be used to estimate the approximate power cost of
    waking the radio up depending on the signal strength.
    
    Running a check every second can potentially be expensive, so adding
    this under a feature flag.
    
    Bug: b/167544787
    Change-Id: Ia523f1894f4debb53cbb6dcbd436d6c7e555d1a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2484800
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830564}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -123,6 +123,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("paint_preview")                                                     \
   X("pepper")                                                            \
   X("PlatformMalloc")                                                    \
+  X("power")                                                             \
   X("ppapi")                                                             \
   X("ppapi proxy")                                                       \
   X("print")                                                             \
