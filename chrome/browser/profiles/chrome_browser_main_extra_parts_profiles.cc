44aa6f9b91ef4a4a01efafaf0b9418cb2e1ee1fc
Igor Tukh <itukh@google.com>
2020-11-25 15:53:05 +0000

    [Permissions Auditing] Add PermissionAuditingService and PermissionAuditingServiceFactory.
    PermissionAuditingService is a service which allows to store and audit the basic information about the permissions usage.
    
    Bug: 1152769
    Change-Id: If22ed0ad189eca8dbdd8502d3a1151d7561aeffc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2517443
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831018}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/password_store_factory.h"
 #include "chrome/browser/permissions/adaptive_quiet_notification_permission_ui_enabler.h"
 #include "chrome/browser/permissions/last_tab_standing_tracker_factory.h"
+#include "chrome/browser/permissions/permission_auditing_service_factory.h"
 #include "chrome/browser/persisted_state_db/persisted_state_db_factory.h"
 #include "chrome/browser/plugins/plugin_prefs_factory.h"
 #include "chrome/browser/policy/cloud/user_cloud_policy_invalidator_factory.h"
@@ -97,6 +98,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/unified_consent/unified_consent_service_factory.h"
 #include "chrome/browser/web_data_service_factory.h"
 #include "chrome/common/buildflags.h"
+#include "chrome/common/chrome_features.h"
 #include "components/captive_portal/core/buildflags.h"
 #include "components/permissions/features.h"
 #include "components/reading_list/features/reading_list_switches.h"
@@ -335,6 +337,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   NTPResourceCacheFactory::GetInstance();
 #endif
   PasswordStoreFactory::GetInstance();
+  PermissionAuditingServiceFactory::GetInstance();
   PersistedStateDBFactory::GetInstance();
 #if !defined(OS_ANDROID)
   PinnedTabServiceFactory::GetInstance();
