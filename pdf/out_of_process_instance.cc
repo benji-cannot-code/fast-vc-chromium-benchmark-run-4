0ee0d6778976c18a02c479b92e9703bd8467b4d1
Daniel Hosseinian <dhoss@chromium.org>
2020-11-25 08:26:13 +0000

    Reload the PDF accessibility tree on layout changes
    Currently, any layout changes applied to the PDF document (i.e. page
    rotation or two-up view) after the accessibility tree has loaded does
    not update the page bounds.
    
    Because the accessiblity tree has read-only nodes, the only way to
    resolve the issue is to reload all the nodes of the tree on each layout
    change.
    
    Note: This CL only fixes page bounds and positions. The bounds of page
    content such as text or annotations still do not rotate with the rest
    of the document. Those issues will be fixed in a follow-up.
    
    Bug: 1150665
    Change-Id: I2fbf2cb26f38ee3fed076333c0b224a8ede7947e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2553217
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830918}

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -817,6 +817,10 @@ void OutOfProcessInstance::LoadAccessibility() {
   doc_info.text_copyable =
       PP_FromBool(engine()->HasPermission(PDFEngine::PERMISSION_COPY));
 
+  // A new document layout will trigger the creation of a new accessibility
+  // tree, so |next_accessibility_page_index_| should be reset to ignore
+  // outdated asynchronous calls of SendNextAccessibilityPage().
+  next_accessibility_page_index_ = 0;
   pp::PDF::SetAccessibilityDocInfo(GetPluginInstance(), &doc_info);
 
   // If the document contents isn't accessible, don't send anything more.
@@ -837,6 +841,11 @@ void OutOfProcessInstance::LoadAccessibility() {
 }
 
 void OutOfProcessInstance::SendNextAccessibilityPage(int32_t page_index) {
+  // Outdated calls are ignored.
+  if (page_index != next_accessibility_page_index_)
+    return;
+  ++next_accessibility_page_index_;
+
   AccessibilityPageInfo page_info;
   std::vector<pp::PDF::PrivateAccessibilityTextRunInfo> text_runs;
   std::vector<AccessibilityCharInfo> chars;
@@ -1237,6 +1246,11 @@ void OutOfProcessInstance::ProposeDocumentLayout(const DocumentLayout& layout) {
   }
   dimensions.Set(kJSPageDimensions, page_dimensions_array);
   PostMessage(dimensions);
+
+  // Reload the accessibility tree on layout changes because the relative page
+  // bounds are no longer valid.
+  if (layout.dirty() && accessibility_state_ == ACCESSIBILITY_STATE_LOADED)
+    LoadAccessibility();
 }
 
 void OutOfProcessInstance::Invalidate(const gfx::Rect& rect) {
