washezium/chromium

commit e552c103dd87d26f37e58fc775e33750d52b371d

[XProto] Remove usage of Xlib from ozone
* Remove usage of Xlib from //ui/ozone.  Also replace Xlib types with
  XProto types in function signatures that are used in //ui/ozone.

* Rewrite and simplify software non-SHM codepath for drawing images.

* Moved PutARGBImage from x11_types.h into x11_util.h and renamed to
  DrawPixmap since it now takes an SkPixmap.

* xcbproto: replaced BYTE with void in PutImage/GetImage so that
  base::RefCountedMemory can be used.

* Got rid of x11_util_internal.h and moved it into x11_util.h.  It was
  originally added to prevent including Xlib.h in x11_util.h, which
  caused conflicts due to macros, however this no longer appears to be
  an issue.

BUG=1066670
R=nickdiego

Change-Id: Ieff52b3748945fbb6a5a42bbd496550ad7862e0f
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2285287
Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
Reviewed-by: Scott Violet <sky@chromium.org>
Reviewed-by: Nick Yamane <nickdiego@igalia.com>
Cr-Commit-Position: refs/heads/master@{#788359}

ui/ozone/platform/x11/ozone_platform_x11.cc
@@ -184,7 +184,8 @@ class OzonePlatformX11 : public OzonePlatform,
 
 #if BUILDFLAG(USE_GTK)
     DCHECK(!GtkUiDelegate::instance());
-    gtk_ui_delegate_ = std::make_unique<GtkUiDelegateX11>(gfx::GetXDisplay());
+    gtk_ui_delegate_ =
+        std::make_unique<GtkUiDelegateX11>(x11::Connection::Get());
     GtkUiDelegate::SetInstance(gtk_ui_delegate_.get());
 #endif
   }
@@ -229,10 +230,10 @@ class OzonePlatformX11 : public OzonePlatform,
     if (common_initialized_)
       return;
 
-    // If XOpenDisplay() failed there is nothing we can do. Crash here instead
-    // of crashing later. If you are crashing here, make sure there is an X
-    // server running and $DISPLAY is set.
-    CHECK(gfx::GetXDisplay()) << "Missing X server or $DISPLAY";
+    // If opening the connection failed there is nothing we can do. Crash here
+    // instead of crashing later. If you are crashing here, make sure there is
+    // an X server running and $DISPLAY is set.
+    CHECK(x11::Connection::Get()) << "Missing X server or $DISPLAY";
 
     ui::SetDefaultX11ErrorHandlers();
 
@@ -244,8 +245,8 @@ class OzonePlatformX11 : public OzonePlatform,
     if (event_source_)
       return;
 
-    XDisplay* display = gfx::GetXDisplay();
-    event_source_ = std::make_unique<X11EventSource>(display);
+    auto* connection = x11::Connection::Get();
+    event_source_ = std::make_unique<X11EventSource>(connection);
   }
 
   bool common_initialized_ = false;

chrome/browser/ui/views/chrome_browser_main_extra_parts_views_linux.cc
@@ -21,7 +21,7 @@
 
 #if defined(USE_X11)
 #include "ui/base/ui_base_features.h"
-#include "ui/gfx/x/x11_types.h"            // nogncheck
+#include "ui/gfx/x/connection.h"  // nogncheck
 #if BUILDFLAG(USE_GTK)
 #include "ui/gtk/x/gtk_ui_delegate_x11.h"  // nogncheck
 #endif  // BUILDFLAG(USE_GTK)
@@ -60,7 +60,7 @@ void ChromeBrowserMainExtraPartsViewsLinux::ToolkitInitialized() {
     // ChromeBrowserMainExtraPartsViewsLinux, so it can properly initialize
     // GtkUi on its |ToolkitInitialized| override.
     gtk_ui_delegate_ =
-        std::make_unique<ui::GtkUiDelegateX11>(gfx::GetXDisplay());
+        std::make_unique<ui::GtkUiDelegateX11>(x11::Connection::Get());
     ui::GtkUiDelegate::SetInstance(gtk_ui_delegate_.get());
   }
 #endif

components/viz/service/display_embedder/skia_output_device_x11.cc
@@ -7,14 +7,25 @@
 #include <utility>
 
 #include "base/logging.h"
+#include "base/memory/ref_counted_memory.h"
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrBackendSurface.h"
 #include "third_party/skia/include/gpu/vk/GrVkTypes.h"
 #include "ui/base/x/x11_util.h"
-#include "ui/base/x/x11_util_internal.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/x/xproto.h"
 
 namespace viz {
+
+namespace {
+
+x11::GraphicsContext CreateGC(x11::Connection* connection, x11::Window window) {
+  auto gc = connection->GenerateId<x11::GraphicsContext>();
+  connection->CreateGC({gc, window});
+  return gc;
+}
+
+}  // namespace
 
 SkiaOutputDeviceX11::SkiaOutputDeviceX11(
     scoped_refptr<gpu::SharedContextState> context_state,
@@ -26,15 +37,15 @@ SkiaOutputDeviceX11::SkiaOutputDeviceX11(
                                 true /* has_alpha */,
                                 memory_tracker,
                                 did_swap_buffer_complete_callback),
-      display_(gfx::GetXDisplay()),
-      widget_(widget),
-      gc_(XCreateGC(display_, static_cast<uint32_t>(widget_), 0, nullptr)) {
-  int result = XGetWindowAttributes(display_, static_cast<uint32_t>(widget_),
-                                    &attributes_);
-  LOG_IF(FATAL, !result) << "XGetWindowAttributes failed for window "
-                         << static_cast<uint32_t>(widget_);
-  bpp_ = gfx::BitsPerPixelForPixmapDepth(display_, attributes_.depth);
-  support_rendr_ = ui::QueryRenderSupport(display_);
+      connection_(x11::Connection::Get()),
+      window_(static_cast<x11::Window>(widget)),
+      gc_(CreateGC(connection_, window_)) {
+  if (auto attributes = connection_->GetWindowAttributes({window_}).Sync()) {
+    visual_ = attributes->visual;
+  } else {
+    LOG(FATAL) << "Failed to get attributes for window "
+               << static_cast<uint32_t>(window_);
+  }
 
   // |capabilities_| should be set by SkiaOutputDeviceOffscreen.
   DCHECK_EQ(capabilities_.output_surface_origin, gfx::SurfaceOrigin::kTopLeft);
@@ -42,7 +53,7 @@ SkiaOutputDeviceX11::SkiaOutputDeviceX11(
 }
 
 SkiaOutputDeviceX11::~SkiaOutputDeviceX11() {
-  XFreeGC(display_, gc_);
+  connection_->FreeGC({gc_});
 }
 
 bool SkiaOutputDeviceX11::Reshape(const gfx::Size& size,
@@ -56,7 +67,8 @@ bool SkiaOutputDeviceX11::Reshape(const gfx::Size& size,
   }
   auto ii =
       SkImageInfo::MakeN32(size.width(), size.height(), kOpaque_SkAlphaType);
-  pixels_.reserve(ii.computeMinByteSize());
+  std::vector<uint8_t> mem(ii.computeMinByteSize());
+  pixels_ = base::RefCountedBytes::TakeVector(&mem);
   return true;
 }
 
@@ -76,70 +88,17 @@ void SkiaOutputDeviceX11::PostSubBuffer(
 
   auto ii =
       SkImageInfo::MakeN32(rect.width(), rect.height(), kOpaque_SkAlphaType);
-  DCHECK_GE(pixels_.capacity(), ii.computeMinByteSize());
-  SkPixmap sk_pixmap(ii, pixels_.data(), ii.minRowBytes());
+  DCHECK_GE(pixels_->size(), ii.computeMinByteSize());
+  SkPixmap sk_pixmap(ii, pixels_->data(), ii.minRowBytes());
   bool result = sk_surface_->readPixels(sk_pixmap, rect.x(), rect.y());
   LOG_IF(FATAL, !result) << "Failed to read pixels from offscreen SkSurface.";
 
-  if (bpp_ == 32 || bpp_ == 16) {
-    // gfx::PutARGBImage() only supports 16 and 32 bpp.
-    // TODO(penghuang): Switch to XShmPutImage.
-    gfx::PutARGBImage(display_, attributes_.visual, attributes_.depth,
-                      static_cast<uint32_t>(widget_), gc_,
-                      static_cast<const uint8_t*>(sk_pixmap.addr()),
-                      rect.width(), rect.height(), 0 /* src_x */, 0 /* src_y */,
-                      rect.x() /* dst_x */, rect.y() /* dst_y */, rect.width(),
-                      rect.height());
-  } else if (support_rendr_) {
-    Pixmap pixmap = XCreatePixmap(display_, static_cast<uint32_t>(widget_),
-                                  rect.width(), rect.height(), 32);
-    GC gc = XCreateGC(display_, pixmap, 0, nullptr);
+  // TODO(penghuang): Switch to XShmPutImage.
+  ui::DrawPixmap(x11::Connection::Get(), visual_, window_, gc_, sk_pixmap,
+                 0 /* src_x */, 0 /* src_y */, rect.x() /* dst_x */,
+                 rect.y() /* dst_y */, rect.width(), rect.height());
 
-    XImage image = {};
-    image.width = rect.width();
-    image.height = rect.height();
-    image.depth = 32;
-    image.bits_per_pixel = 32;
-    image.format = static_cast<int>(x11::ImageFormat::ZPixmap);
-    image.byte_order = static_cast<int>(x11::ImageOrder::LSBFirst);
-    image.bitmap_unit = 8;
-    image.bitmap_bit_order = static_cast<int>(x11::ImageOrder::LSBFirst);
-    image.bytes_per_line = sk_pixmap.rowBytes();
-
-    image.red_mask = 0xff << SK_R32_SHIFT;
-    image.green_mask = 0xff << SK_G32_SHIFT;
-    image.blue_mask = 0xff << SK_B32_SHIFT;
-    image.data = const_cast<char*>(static_cast<const char*>(sk_pixmap.addr()));
-    XPutImage(display_, pixmap, gc, &image, 0 /* src_x */, 0 /* src_y */,
-              0 /* dest_x */, 0 /* dest_y */, rect.width(), rect.height());
-    XFreeGC(display_, gc);
-
-    Picture picture = XRenderCreatePicture(
-        display_, pixmap, ui::GetRenderARGB32Format(display_), 0, nullptr);
-    XRenderPictFormat* pictformat =
-        XRenderFindVisualFormat(display_, attributes_.visual);
-    Picture dest_picture = XRenderCreatePicture(
-        display_, static_cast<uint32_t>(widget_), pictformat, 0, nullptr);
-    XRenderComposite(display_,
-                     PictOpSrc,       // op
-                     picture,         // src
-                     0,               // mask
-                     dest_picture,    // dest
-                     0,               // src_x
-                     0,               // src_y
-                     0,               // mask_x
-                     0,               // mask_y
-                     rect.x(),        // dest_x
-                     rect.y(),        // dest_y
-                     rect.width(),    // width
-                     rect.height());  // height
-    XRenderFreePicture(display_, picture);
-    XRenderFreePicture(display_, dest_picture);
-    XFreePixmap(display_, pixmap);
-  } else {
-    NOTIMPLEMENTED();
-  }
-  XFlush(display_);
+  connection_->Flush();
   FinishSwapBuffers(gfx::SwapCompletionResult(gfx::SwapResult::SWAP_ACK),
                     gfx::Size(sk_surface_->width(), sk_surface_->height()),
                     std::move(latency_info));

components/viz/service/display_embedder/skia_output_device_x11.h
@@ -10,8 +10,10 @@
 #include "base/macros.h"
 #include "components/viz/service/display_embedder/skia_output_device_offscreen.h"
 #include "ui/gfx/native_widget_types.h"
+#include "ui/gfx/x/connection.h"
 #include "ui/gfx/x/x11.h"
 #include "ui/gfx/x/x11_types.h"
+#include "ui/gfx/x/xproto.h"
 
 namespace viz {
 
@@ -36,13 +38,11 @@ class SkiaOutputDeviceX11 final : public SkiaOutputDeviceOffscreen {
                      std::vector<ui::LatencyInfo> latency_info) override;
 
  private:
-  XDisplay* const display_;
-  const gfx::AcceleratedWidget widget_;
-  GC gc_;
-  XWindowAttributes attributes_;
-  int bpp_;
-  bool support_rendr_;
-  std::vector<char> pixels_;
+  x11::Connection* connection_ = nullptr;
+  x11::Window window_ = x11::Window::None;
+  x11::GraphicsContext gc_{};
+  x11::VisualId visual_{};
+  scoped_refptr<base::RefCountedMemory> pixels_;
 
   DISALLOW_COPY_AND_ASSIGN(SkiaOutputDeviceX11);
 };

components/viz/service/display_embedder/software_output_device_x11.cc
@@ -16,7 +16,6 @@
 #include "third_party/skia/include/core/SkImageInfo.h"
 #include "ui/base/x/x11_shm_image_pool.h"
 #include "ui/base/x/x11_util.h"
-#include "ui/base/x/x11_util_internal.h"
 #include "ui/gfx/x/x11.h"
 #include "ui/gfx/x/x11_error_tracker.h"
 #include "ui/gfx/x/x11_types.h"

content/browser/browser_main_loop.cc
@@ -224,7 +224,7 @@
 #if defined(USE_X11)
 #include "gpu/config/gpu_driver_bug_workaround_type.h"
 #include "ui/base/ui_base_features.h"
-#include "ui/base/x/x11_util_internal.h"  // nogncheck
+#include "ui/base/x/x11_util.h"           // nogncheck
 #include "ui/gfx/x/x11_types.h"           // nogncheck
 #endif
 

content/shell/browser/shell_browser_main_parts.cc
@@ -167,7 +167,8 @@ void ShellBrowserMainParts::ToolkitInitialized() {
   if (features::IsUsingOzonePlatform())
     return;
 #endif
-  gtk_ui_delegate_ = std::make_unique<ui::GtkUiDelegateX11>(gfx::GetXDisplay());
+  gtk_ui_delegate_ =
+      std::make_unique<ui::GtkUiDelegateX11>(x11::Connection::Get());
   ui::GtkUiDelegate::SetInstance(gtk_ui_delegate_.get());
   views::LinuxUI* linux_ui = BuildGtkUi(gtk_ui_delegate_.get());
   linux_ui->UpdateDeviceScaleFactor();

third_party/xcbproto/patch.diff
@@ -1,6 +1,6 @@
 diff -ru xcbproto/src/randr.xml src/src/randr.xml
---- xcbproto/src/randr.xml	2020-07-06 16:37:24.662098082 -0700
-+++ src/src/randr.xml	2020-07-06 16:31:26.724467001 -0700
+--- xcbproto/src/randr.xml	2020-07-08 10:06:09.757347365 -0700
++++ src/src/randr.xml	2020-07-07 12:47:03.842264535 -0700
 @@ -803,64 +803,6 @@
  		<item name="Lease">           <value>6</value></item>
  	</enum>
@@ -173,6 +173,27 @@ diff -ru xcbproto/src/randr.xml src/src/randr.xml
 +		</switch>
  	</event>
  </xcb>
+diff -ru xcbproto/src/xproto.xml src/src/xproto.xml
+--- xcbproto/src/xproto.xml	2020-07-08 10:06:09.761347383 -0700
++++ src/src/xproto.xml	2020-07-07 12:47:57.378510617 -0700
+@@ -4686,7 +4686,7 @@
+     <field type="CARD8" name="left_pad" />
+     <field type="CARD8" name="depth" />
+     <pad bytes="2" />
+-    <list type="BYTE" name="data" />
++    <list type="void" name="data" />
+   </request>
+ 
+   <!-- FIXME: data array in reply will include padding, but ought not to. -->
+@@ -4702,7 +4702,7 @@
+       <field type="CARD8" name="depth" />
+       <field type="VISUALID" name="visual" />
+       <pad bytes="20" />
+-      <list type="BYTE" name="data">
++      <list type="void" name="data">
+         <op op="*">
+           <fieldref>length</fieldref>
+           <value>4</value>
 Only in src/xcbgen: align.pyc
 Only in src/xcbgen: error.pyc
 Only in src/xcbgen: expr.pyc

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit aa48f19f8a75e2fe3c0d72b30b8bea82343d6c68

[XProto] Crash early in OzonePlatformX11 if X connection is down
This is a speculative fix for bug 1137614 possibly introduced by
e552c103dd87d26f37e58fc775e33750d52b371d.  The issue is the change from
    CHECK(gfx::GetXDisplay())
to
	CHECK(x11::Connection::Get())
The second call will always succeed since x11::Connection::Get() always
returns something even if the underlying connection is down.  But the
intention was to hit the CHECK() in this case, as that's what the
behavior was before (gfx::GetXDisplay() returns nullptr when the
connection failed).  This CL restores the old behvaior by explicitly
checking the connection state.

R=msisov

Bug: 1137614
Change-Id: If2b2049e39b4d892e60fd29813c915459787d8d6
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2558716
Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
Commit-Queue: Maksim Sisov (GMT+2) <msisov@igalia.com>
Reviewed-by: Maksim Sisov (GMT+2) <msisov@igalia.com>
Cr-Commit-Position: refs/heads/master@{#830895}

ui/ozone/platform/x11/ozone_platform_x11.cc
@@ -271,7 +271,7 @@ class OzonePlatformX11 : public OzonePlatform,
     // If opening the connection failed there is nothing we can do. Crash here
     // instead of crashing later. If you are crashing here, make sure there is
     // an X server running and $DISPLAY is set.
-    CHECK(x11::Connection::Get()) << "Missing X server or $DISPLAY";
+    CHECK(x11::Connection::Get()->Ready()) << "Missing X server or $DISPLAY";
 
     common_initialized_ = true;
   }