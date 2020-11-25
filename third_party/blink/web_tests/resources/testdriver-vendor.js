55ac434ea6f7a5760d3a68ab4777e5472fa8797d
Lan Wei <lanwei@chromium.org>
2020-11-25 22:17:28 +0000

    Simulate user inputs in css/selectors/focus-visible-012.html
    Use testdriver Action API to simulate key actions in
    css/selectors/focus-visible-012.html.
    
    Bug: 1145677
    Change-Id: I14a5362801ac407ad6129d02feb7003960485cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551229
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831148}

--- third_party/blink/web_tests/resources/testdriver-vendor.js
+++ third_party/blink/web_tests/resources/testdriver-vendor.js
@@ -102,6 +102,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         for(var i = 0; i < keys.length; ++i) {
           let eventSenderKeys = keys[i];
           let charCode = keys.charCodeAt(i);
+          let modifierValue;
           // See https://w3c.github.io/webdriver/#keyboard-actions and
           // EventSender::KeyDown().
           if (charCode == 0xE004) {
@@ -118,11 +119,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             eventSenderKeys = "ArrowDown";
           } else if (charCode == 0xE00C) {
             eventSenderKeys = "Escape";
+          } else if (charCode == 0xE009) {
+            eventSenderKeys = "ControlLeft";
+            modifierValue = "ctrlKey";
+          } else if (charCode == 0xE00A) {
+            eventSenderKeys = "AltLeft";
+            modifierValue = "altKey";
+          } else if (charCode == 0xE03D) {
+            eventSenderKeys = "MetaLeft";
+            modifierValue = "metaKey";
+          } else if (charCode == 0xE008) {
+            eventSenderKeys = "ShiftLeft";
+            modifierValue = "shiftKey";
           } else if (charCode >= 0xE000 && charCode <= 0xF8FF) {
             reject(new Error("No support for this code: U+" + charCode.toString(16)));
             return;
           }
-          window.eventSender.keyDown(eventSenderKeys);
+
+          window.eventSender.keyDown(eventSenderKeys, modifierValue);
         }
         resolve();
       });
