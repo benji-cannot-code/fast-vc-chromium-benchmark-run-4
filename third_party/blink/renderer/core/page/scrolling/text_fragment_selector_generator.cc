546b461974f0f270f8808e410a6f384a9be1eca9
Findit <findit-for-me@appspot.gserviceaccount.com>
2020-11-25 01:40:29 +0000

    Revert "[SH-Blink] Move Link Generation Error Logging"
    This reverts commit c754406acbcf809e58dc1ae2c0e5dc23d96033a6.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 830817 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2M3NTQ0MDZhY2JjZjgwOWU1OGRjMWFlMmMwZTVkYzIzZDk2MDMzYTYM
    
    Sample Failed Build: https://ci.chromium.org/b/8862701172936116704
    
    Sample Failed Step: compile
    
    Original change's description:
    > [SH-Blink] Move Link Generation Error Logging
    > 
    > By consolidating the error histogram logging (and import the shared
    > component), we'll be able to make use of the new UKM logging functions
    > in a follow-up CL and provide it with the error causes.
    > 
    > Added histogram validation steps in unit tests before moving the
    > logging logic.
    > 
    > Bug: 1148807
    > Change-Id: I63fd8ca1652d885705c0a17f6131e207ce2e6da3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551793
    > Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    > Reviewed-by: sebsg <sebsg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#830817}
    
    
    Change-Id: I27cc3e78742d57aba7fb9b23f595cbd6e12cf6c9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1148807
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2559585
    Cr-Commit-Position: refs/heads/master@{#830820}

--- third_party/blink/renderer/core/page/scrolling/text_fragment_selector_generator.cc
+++ third_party/blink/renderer/core/page/scrolling/text_fragment_selector_generator.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_macros.h"
 #include "base/time/default_tick_clock.h"
-#include "components/shared_highlighting/core/common/shared_highlighting_metrics.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 #include "third_party/blink/public/platform/interface_registry.h"
 #include "third_party/blink/renderer/core/editing/ephemeral_range.h"
@@ -17,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/scrolling/text_fragment_anchor_metrics.h"
 #include "third_party/blink/renderer/core/page/scrolling/text_fragment_finder.h"
 #include "third_party/blink/renderer/platform/text/text_boundaries.h"
-
-using LinkGenerationError = shared_highlighting::LinkGenerationError;
 
 namespace blink {
 
@@ -268,7 +265,6 @@ void TextFragmentSelectorGenerator::GenerateSelector(
   generation_start_time_ = base::DefaultTickClock::GetInstance()->NowTicks();
   pending_generate_selector_callback_ = std::move(callback);
   state_ = kNeedsNewCandidate;
-  error_.reset();
   step_ = kExact;
   max_available_prefix_ = "";
   max_available_suffix_ = "";
@@ -350,9 +346,10 @@ void TextFragmentSelectorGenerator::DidFindMatch(
 }
 
 void TextFragmentSelectorGenerator::NoMatchFound() {
-  state_ = kFailure;
-  error_ = LinkGenerationError::kIncorrectSelector;
-  ResolveSelectorState();
+  UMA_HISTOGRAM_ENUMERATION("SharedHighlights.LinkGenerated.Error",
+                            LinkGenerationError::kIncorrectSelector);
+  NotifySelectorReady(
+      TextFragmentSelector(TextFragmentSelector::SelectorType::kInvalid));
 }
 
 void TextFragmentSelectorGenerator::NotifySelectorReady(
@@ -383,9 +380,6 @@ void TextFragmentSelectorGenerator::NotifySelectorReady(
     UMA_HISTOGRAM_TIMES("SharedHighlights.LinkGenerated.Error.TimeToGenerate",
                         base::DefaultTickClock::GetInstance()->NowTicks() -
                             generation_start_time_);
-
-    shared_highlighting::LogLinkGenerationErrorReason(
-        error_.has_value() ? error_.value() : LinkGenerationError::kUnknown);
   }
 
   std::move(pending_generate_selector_callback_).Run(selector.ToString());
@@ -419,8 +413,9 @@ void TextFragmentSelectorGenerator::GenerateExactSelector() {
 
   String selected_text = PlainText(ephemeral_range).StripWhiteSpace();
   if (selected_text.IsEmpty()) {
+    UMA_HISTOGRAM_ENUMERATION("SharedHighlights.LinkGenerated.Error",
+                              LinkGenerationError::kEmptySelection);
     state_ = kFailure;
-    error_ = LinkGenerationError::kEmptySelection;
     return;
   }
 
@@ -472,8 +467,9 @@ void TextFragmentSelectorGenerator::ExtendRangeSelector() {
       // If from middle till end of selection there is no word break, then we
       // cannot use it for range end.
       if (mid_point == selection_length) {
+        UMA_HISTOGRAM_ENUMERATION("SharedHighlights.LinkGenerated.Error",
+                                  LinkGenerationError::kNoRange);
         state_ = kFailure;
-        error_ = LinkGenerationError::kNoRange;
         return;
       }
 
@@ -513,8 +509,9 @@ void TextFragmentSelectorGenerator::ExtendContext() {
   // Give up if context is already too long.
   if (num_prefix_words_ == kMaxContextWords ||
       num_prefix_words_ == kMaxContextWords) {
+    UMA_HISTOGRAM_ENUMERATION("SharedHighlights.LinkGenerated.Error",
+                              LinkGenerationError::kContextLimitReached);
     state_ = kFailure;
-    error_ = LinkGenerationError::kContextLimitReached;
     return;
   }
 
@@ -526,8 +523,9 @@ void TextFragmentSelectorGenerator::ExtendContext() {
   }
 
   if (max_available_prefix_.IsEmpty() && max_available_suffix_.IsEmpty()) {
+    UMA_HISTOGRAM_ENUMERATION("SharedHighlights.LinkGenerated.Error",
+                              LinkGenerationError::kNoContext);
     state_ = kFailure;
-    error_ = LinkGenerationError::kNoContext;
     return;
   }
 
@@ -536,8 +534,9 @@ void TextFragmentSelectorGenerator::ExtendContext() {
 
   // Give up if we were unable to get new prefix and suffix.
   if (prefix == selector_->Prefix() && suffix == selector_->Suffix()) {
+    UMA_HISTOGRAM_ENUMERATION("SharedHighlights.LinkGenerated.Error",
+                              LinkGenerationError::kContextExhausted);
     state_ = kFailure;
-    error_ = LinkGenerationError::kContextExhausted;
     return;
   }
   selector_ = std::make_unique<TextFragmentSelector>(
