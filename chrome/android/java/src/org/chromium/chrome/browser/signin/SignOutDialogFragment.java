5378c82d1fbdaf1592828e8ee1f2806e8060c8cb
Alice Wang <aliceywang@chromium.org>
2020-11-25 18:51:23 +0000

    [Android][Signin][Modularization] Move SigninManager to signin.services target
    This CL moves SigninManager to signin.services target.
    
    Bug: 1152717
    Change-Id: Ibdf3a5ea99bd059dbf7a971e21e486e39b88f54d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560302
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831078}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SignOutDialogFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SignOutDialogFragment.java
@@ -20,6 +20,7 @@ import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileAccountManagementMetrics;
+import org.chromium.chrome.browser.signin.services.SigninManager;
 import org.chromium.components.signin.GAIAServiceType;
 
 /**
