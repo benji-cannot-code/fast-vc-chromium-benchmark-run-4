6883612ad8a6222cc3a3942d2f2bb388999f9766
dpapad <dpapad@chromium.org>
2020-11-25 16:27:33 +0000

    WebUI: Remove incorrect imports from some Polymer2 JS files.
    Polymer2 files processed by polymer_modulizer() should not have
    "// #import " comments. Instead, their JS imports are auto-generated
    by translating HTML imports from the corresponding HTML file.
    
    Note that these comments are no-op currently, as they are not processed
    by polymer_modulizer().
    
    Bug: 1152687
    Change-Id: If908cce6d21f4766590b2c97b354f2ac899d8625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560679
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831030}

--- chrome/browser/resources/settings/chromeos/os_languages_page/os_edit_dictionary_page.js
+++ chrome/browser/resources/settings/chromeos/os_languages_page/os_edit_dictionary_page.js
@@ -8,13 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * the "dictionary" of custom words used for spell check.
  */
 
-// clang-format off
-// #import {GlobalScrollTargetBehavior} from '../../global_scroll_target_behavior.m.js';
-// #import {loadTimeData} from '../../i18n_setup.js';
-// #import {PrefsBehavior} from '../../prefs/prefs_behavior.m.js';
-// #import {LanguagesBrowserProxyImpl} from '../../languages_page/languages_browser_proxy.m.js';
-// clang-format on
-
 // Max valid word size, keep in sync with kMaxCustomDictionaryWordBytes in
 // //components/spellcheck/common/spellcheck_common.h
 const MAX_CUSTOM_DICTIONARY_WORD_BYTES = 99;
