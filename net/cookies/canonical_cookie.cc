21af10c52b7c8b4d17f9c75234a6d93ccb7f0f15
Dylan Cutler <dylancutler@google.com>
2020-11-25 18:43:02 +0000

    Make CreateUnsafeCookieForTesting return a unique_ptr.
    This will make it easier to use this factory when testing code that uses
    SetCanonicalCookie() and other methods that take unique_ptrs to
    CanonicalCookies.
    
    The factory method uses base::WrapUnique to create the unique_ptr so
    that the constructor can be made private. I plan on updating the other
    factory methods to use base::WrapUnique instead of make_unique in a
    follow-up CL.
    
    Bug: 1102874
    Change-Id: I3e84fbc68ce95438a5adfdac46573df9efe4e38d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2559311
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/master@{#831072}

--- net/cookies/canonical_cookie.cc
+++ net/cookies/canonical_cookie.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/format_macros.h"
 #include "base/logging.h"
+#include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/strcat.h"
@@ -554,7 +555,7 @@ std::unique_ptr<CanonicalCookie> CanonicalCookie::FromStorage(
 }
 
 // static
-CanonicalCookie CanonicalCookie::CreateUnsafeCookieForTesting(
+std::unique_ptr<CanonicalCookie> CanonicalCookie::CreateUnsafeCookieForTesting(
     const std::string& name,
     const std::string& value,
     const std::string& domain,
@@ -569,9 +570,9 @@ CanonicalCookie CanonicalCookie::CreateUnsafeCookieForTesting(
     bool same_party,
     CookieSourceScheme source_scheme,
     int source_port) {
-  return CanonicalCookie(name, value, domain, path, creation, expiration,
-                         last_access, secure, httponly, same_site, priority,
-                         same_party, source_scheme, source_port);
+  return base::WrapUnique(new CanonicalCookie(
+      name, value, domain, path, creation, expiration, last_access, secure,
+      httponly, same_site, priority, same_party, source_scheme, source_port));
 }
 
 std::string CanonicalCookie::DomainWithoutDot() const {
