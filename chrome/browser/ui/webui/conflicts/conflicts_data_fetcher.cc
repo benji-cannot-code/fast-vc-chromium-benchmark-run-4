0b6a880194b10f8154e166a9b0fae4259c79a4a3
Ramya Nagarajan <ramyan@chromium.org>
2020-11-25 18:04:14 +0000

    Revert "More inclusive terminology for chrome/browser/win/conflicts/."
    This reverts commit e0e81ba95ea1b2a630458fa927694660a6be8319.
    
    Reason for revert: crbug.com/1152818
    
    Original change's description:
    > More inclusive terminology for chrome/browser/win/conflicts/.
    >
    > Bug: 1097283
    > Change-Id: I570f98b1f42487aff2696c50f87fd0832667379b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547620
    > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > Auto-Submit: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: Jesse Doherty <jwd@chromium.org>
    > Reviewed-by: Patrick Monette <pmonette@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#830830}
    
    TBR=pkasting@chromium.org,chrisha@chromium.org,gab@chromium.org,jwd@chromium.org,tedchoc@chromium.org,pmonette@chromium.org
    
    Change-Id: Iecaebbf4e63fb2d0c52aa87d5eca9ce7e424070d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1097283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560807
    Reviewed-by: Ramya Nagarajan <ramyan@chromium.org>
    Commit-Queue: Ramya Nagarajan <ramyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831064}

--- chrome/browser/ui/webui/conflicts/conflicts_data_fetcher.cc
+++ chrome/browser/ui/webui/conflicts/conflicts_data_fetcher.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(GOOGLE_CHROME_BUILD)
 #include "base/win/win_util.h"
 #include "chrome/browser/win/conflicts/incompatible_applications_updater.h"
-#include "chrome/browser/win/conflicts/module_blocklist_cache_updater.h"
+#include "chrome/browser/win/conflicts/module_blacklist_cache_updater.h"
 #endif
 
 namespace {
@@ -74,7 +74,7 @@ void AppendString(base::StringPiece input, std::string* output) {
 // Returns a string describing the current module blocking status: loaded or
 // not, blocked or not, was in blocklist cache or not, bypassed blocking or not.
 std::string GetBlockingStatusString(
-    const ModuleBlocklistCacheUpdater::ModuleBlockingState& blocking_state) {
+    const ModuleBlacklistCacheUpdater::ModuleBlockingState& blocking_state) {
   std::string status;
 
   // Output status regarding the blocklist cache, current blocking, and
@@ -83,9 +83,9 @@ std::string GetBlockingStatusString(
     status = "Blocked";
   if (!blocking_state.was_loaded)
     AppendString(kNotLoaded, &status);
-  else if (blocking_state.was_in_blocklist_cache)
+  else if (blocking_state.was_in_blacklist_cache)
     AppendString("Bypassed blocking", &status);
-  if (blocking_state.was_in_blocklist_cache)
+  if (blocking_state.was_in_blacklist_cache)
     AppendString("In blocklist cache", &status);
 
   return status;
@@ -95,9 +95,9 @@ std::string GetBlockingStatusString(
 // returns the empty string to indicate that the warning decision description
 // should be used instead.
 std::string GetBlockingDecisionString(
-    const ModuleBlocklistCacheUpdater::ModuleBlockingState& blocking_state,
+    const ModuleBlacklistCacheUpdater::ModuleBlockingState& blocking_state,
     IncompatibleApplicationsUpdater* incompatible_applications_updater) {
-  using BlockingDecision = ModuleBlocklistCacheUpdater::ModuleBlockingDecision;
+  using BlockingDecision = ModuleBlacklistCacheUpdater::ModuleBlockingDecision;
 
   // Append status regarding the logic that will be applied during the next
   // startup.
@@ -172,7 +172,7 @@ std::string GetModuleWarningDecisionString(
       return "Tolerated - Could not tie to an installed application";
     case WarningDecision::kIncompatible:
       return "Incompatible";
-    case WarningDecision::kAddedToBlocklist:
+    case WarningDecision::kAddedToBlacklist:
     case WarningDecision::kUnknown:
       NOTREACHED();
       break;
@@ -184,7 +184,7 @@ std::string GetModuleWarningDecisionString(
 std::string GetModuleStatusString(
     const ModuleInfoKey& module_key,
     IncompatibleApplicationsUpdater* incompatible_applications_updater,
-    ModuleBlocklistCacheUpdater* module_blocklist_cache_updater) {
+    ModuleBlacklistCacheUpdater* module_blocklist_cache_updater) {
   if (!incompatible_applications_updater && !module_blocklist_cache_updater)
     return std::string();
 
@@ -192,7 +192,7 @@ std::string GetModuleStatusString(
 
   // The blocking status is shown over the warning status.
   if (module_blocklist_cache_updater) {
-    const ModuleBlocklistCacheUpdater::ModuleBlockingState& blocking_state =
+    const ModuleBlacklistCacheUpdater::ModuleBlockingState& blocking_state =
         module_blocklist_cache_updater->GetModuleBlockingState(module_key);
 
     status = GetBlockingStatusString(blocking_state);
@@ -273,7 +273,7 @@ ThirdPartyFeaturesStatus GetThirdPartyFeaturesStatus(
     return kPolicyDisabled;
 
   if (!IncompatibleApplicationsUpdater::IsWarningEnabled() &&
-      !ModuleBlocklistCacheUpdater::IsBlockingEnabled()) {
+      !ModuleBlacklistCacheUpdater::IsBlockingEnabled()) {
     return kFeatureDisabled;
   }
 
@@ -444,7 +444,7 @@ void ConflictsDataFetcher::OnNewModuleFound(const ModuleInfoKey& module_key,
     auto* module_blocklist_cache_updater =
         ModuleDatabase::GetInstance()
             ->third_party_conflicts_manager()
-            ->module_blocklist_cache_updater();
+            ->module_blacklist_cache_updater();
 
     data->SetString(
         "third_party_module_status",
