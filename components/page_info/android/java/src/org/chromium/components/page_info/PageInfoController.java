206b662b3e17d8ef7cbeb5cd5f54c768b2c2a531
Christian Dullweber <dullweber@chromium.org>
2020-11-25 16:14:28 +0000

    Remove PageInfo subpages when dialog is closed
    To ensure that all fragments are removed when the PageInfo dialog is
    closed, the subpage controller needs to be notified when the main
    controller is dismissed.
    
    Bug: 1152724
    Change-Id: I075a214223ceee675578615226b958880a103946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560628
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831024}

--- components/page_info/android/java/src/org/chromium/components/page_info/PageInfoController.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoController.java
@@ -489,6 +489,10 @@ public class PageInfoController implements PageInfoMainController, ModalDialogPr
             mPendingRunAfterDismissTask.run();
             mPendingRunAfterDismissTask = null;
         }
+        if (mSubpageController != null) {
+            mSubpageController.onSubpageRemoved();
+            mSubpageController = null;
+        }
         mWebContentsObserver.destroy();
         mWebContentsObserver = null;
         PageInfoControllerJni.get().destroy(mNativePageInfoController, PageInfoController.this);
