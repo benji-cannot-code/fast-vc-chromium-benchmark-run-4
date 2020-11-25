8870967286340325a02637fbf2401ef0bb91794f
Daniel Cheng <dcheng@chromium.org>
2020-11-25 10:57:24 +0000

    [navigation] Remove unused method RFHI::OnCrossDocumentCommitProcessed.
    At this point, only there is only one test that passes a value other
    than blink::mojom::CommitResult::Aborted into this helper. Refactor the
    test to use a regular navigation + CommitCallbackInterceptor instead,
    and inline the abort logic in OnCrossDocumentCommitProcessed() directly
    into RFHI::NavigationRequestCancelled().
    
    Bug: 1020175
    Change-Id: I5fcfe69020625b4dd40a0846f1c00a73573ac3d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2537597
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830950}

--- content/test/test_render_frame_host.cc
+++ content/test/test_render_frame_host.cc
@@ -602,8 +602,7 @@ TestRenderFrameHost::BuildDidCommitInterfaceParams(bool is_same_document) {
 }
 
 void TestRenderFrameHost::AbortCommit(NavigationRequest* navigation_request) {
-  OnCrossDocumentCommitProcessed(navigation_request,
-                                 blink::mojom::CommitResult::Aborted);
+  NavigationRequestCancelled(navigation_request);
 }
 
 // static
