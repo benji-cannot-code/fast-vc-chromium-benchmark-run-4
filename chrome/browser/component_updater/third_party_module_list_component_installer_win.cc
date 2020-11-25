5a917d609804c660f63626b6d9f1301ec3720f84
Peter Kasting <pkasting@chromium.org>
2020-11-25 16:24:22 +0000

    Reland "More inclusive terminology for chrome/browser/win/conflicts/."
    This is a reland of e0e81ba95ea1b2a630458fa927694660a6be8319
    
    Original change's description:
    > More inclusive terminology for chrome/browser/win/conflicts/.
    >
    > Bug: 1097283
    > Change-Id: I570f98b1f42487aff2696c50f87fd0832667379b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547620
    > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > Auto-Submit: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: Jesse Doherty <jwd@chromium.org>
    > Reviewed-by: Patrick Monette <pmonette@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#830830}
    
    Bug: 1097283
    TBR: gab, hwd, pmonette, tedchoc, chrisha, waffles
    Change-Id: I97eacb76ce1009f2534e42d20933fcd6fd9abade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560684
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831029}

--- chrome/browser/component_updater/third_party_module_list_component_installer_win.cc
+++ chrome/browser/component_updater/third_party_module_list_component_installer_win.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/values.h"
 #include "base/version.h"
-#include "chrome/browser/win/conflicts/module_blacklist_cache_util.h"
+#include "chrome/browser/win/conflicts/module_blocklist_cache_util.h"
 #include "chrome/browser/win/conflicts/module_database.h"
 #include "chrome/browser/win/conflicts/third_party_conflicts_manager.h"
 
