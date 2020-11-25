535d6c8999772e12f73616a228509340c33627d8
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2020-11-25 05:34:12 +0000

    Revert "More inclusive terminology for chrome/browser/win/conflicts/."
    This reverts commit e0e81ba95ea1b2a630458fa927694660a6be8319.
    
    Reason for revert: Breaks win64-chrome :(
    
    [12574/27367] CXX obj/chrome/browser/browser/third_party_module_list_component_installer_win.obj
    FAILED: obj/chrome/browser/browser/third_party_module_list_component_installer_win.obj
    c:\b\s\w\ir\cache\goma\client\gomacc.exe ..\..\third_party\llvm-build\Release+Asserts\bin\clang-cl.e...(too long)
    ../../chrome/browser/component_updater/third_party_module_list_component_installer_win.cc(17,10): fatal error: 'chrome/browser/win/conflicts/module_blacklist_cache_util.h' file not found
    #include "chrome/browser/win/conflicts/module_blacklist_cache_util.h"
    
    https://ci.chromium.org/p/chrome/builders/ci/win64-chrome/10472
    
    Original change's description:
    > More inclusive terminology for chrome/browser/win/conflicts/.
    >
    > Bug: 1097283
    > Change-Id: I570f98b1f42487aff2696c50f87fd0832667379b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547620
    > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > Auto-Submit: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: Jesse Doherty <jwd@chromium.org>
    > Reviewed-by: Patrick Monette <pmonette@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#830830}
    
    TBR=pkasting@chromium.org,chrisha@chromium.org,gab@chromium.org,jwd@chromium.org,tedchoc@chromium.org,pmonette@chromium.org
    
    Change-Id: Ibe482cba992b06bfc58fc1eaee200cca11b10d72
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1097283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2558083
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830887}

--- chrome/browser/win/conflicts/incompatible_applications_updater_unittest.cc
+++ chrome/browser/win/conflicts/incompatible_applications_updater_unittest.cc
@@ -27,17 +27,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Mocks an empty allowlist and blocklist.
+// Mocks an empty whitelist and blacklist.
 class MockModuleListFilter : public ModuleListFilter {
  public:
   MockModuleListFilter() = default;
 
-  bool IsAllowlisted(base::StringPiece module_basename_hash,
+  bool IsWhitelisted(base::StringPiece module_basename_hash,
                      base::StringPiece module_code_id_hash) const override {
     return false;
   }
 
-  std::unique_ptr<chrome::conflicts::BlocklistAction> IsBlocklisted(
+  std::unique_ptr<chrome::conflicts::BlacklistAction> IsBlacklisted(
       const ModuleInfoKey& module_key,
       const ModuleInfoData& module_data) const override {
     return nullptr;
@@ -405,9 +405,9 @@ TEST_F(IncompatibleApplicationsUpdaterTest, IgnoreNotLoadedModules) {
       IncompatibleApplicationsUpdater::ModuleWarningDecision::kNotLoaded);
 }
 
-// Tests that modules with a matching certificate subject are allowlisted.
+// Tests that modules with a matching certificate subject are whitelisted.
 TEST_F(IncompatibleApplicationsUpdaterTest,
-       allowlistMatchingCertificateSubject) {
+       WhitelistMatchingCertificateSubject) {
   if (base::win::GetVersion() < base::win::Version::WIN10)
     return;
 
@@ -476,11 +476,11 @@ TEST_F(IncompatibleApplicationsUpdaterTest, IgnoreRegisteredModules) {
       IncompatibleApplicationsUpdater::ModuleWarningDecision::kAllowedIME);
 }
 
-TEST_F(IncompatibleApplicationsUpdaterTest, IgnoreModulesAddedToTheBlocklist) {
+TEST_F(IncompatibleApplicationsUpdaterTest, IgnoreModulesAddedToTheBlacklist) {
   if (base::win::GetVersion() < base::win::Version::WIN10)
     return;
 
-  AddIncompatibleApplication(dll1_, L"Blocklisted Application",
+  AddIncompatibleApplication(dll1_, L"Blacklisted Application",
                              Option::ADD_REGISTRY_ENTRY);
 
   auto incompatible_applications_updater =
@@ -488,7 +488,7 @@ TEST_F(IncompatibleApplicationsUpdaterTest, IgnoreModulesAddedToTheBlocklist) {
 
   // Set the respective bit for the module.
   auto module_data = CreateLoadedModuleInfoData();
-  module_data.module_properties |= ModuleInfoData::kPropertyAddedToBlocklist;
+  module_data.module_properties |= ModuleInfoData::kPropertyAddedToBlacklist;
 
   // Simulate the module loading into the process.
   incompatible_applications_updater->OnNewModuleFound(
--- chrome/browser/win/conflicts/module_load_attempt_log_listener.cc
+++ chrome/browser/win/conflicts/module_load_attempt_log_listener.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
 #include "base/task_runner_util.h"
-#include "chrome/browser/win/conflicts/module_blocklist_cache_util.h"
+#include "chrome/browser/win/conflicts/module_blacklist_cache_util.h"
 #include "chrome/chrome_elf/third_party_dlls/public_api.h"
 
 namespace {
