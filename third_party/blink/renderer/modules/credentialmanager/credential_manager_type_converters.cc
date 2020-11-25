eff8c157a69f094445d772395513c943d763703b
Hitoshi Yoshida <peria@chromium.org>
2020-11-25 14:10:40 +0000

    bindings: Fix LargeBlob support in Credentials
    Credentials type converter can access a dictionary member[1] when it is
    not available.
    This CL fixes the issue.
    
    
    [1] PublicKeyCredentialCreationOptions.extensions.largeBlob.support
    
    Bug: 839389
    Change-Id: Ica1b43775e221a37a4be0befc920035f32fa214f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2558133
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#830987}

--- third_party/blink/renderer/modules/credentialmanager/credential_manager_type_converters.cc
+++ third_party/blink/renderer/modules/credentialmanager/credential_manager_type_converters.cc
@@ -322,12 +322,15 @@ TypeConverter<AuthenticatorAttachment, base::Optional<String>>::Convert(
 }
 
 // static
-LargeBlobSupport TypeConverter<LargeBlobSupport, String>::Convert(
-    const String& large_blob_support) {
-  if (large_blob_support == "required")
-    return LargeBlobSupport::REQUIRED;
-  if (large_blob_support == "preferred")
-    return LargeBlobSupport::PREFERRED;
+LargeBlobSupport
+TypeConverter<LargeBlobSupport, base::Optional<String>>::Convert(
+    const base::Optional<String>& large_blob_support) {
+  if (large_blob_support) {
+    if (*large_blob_support == "required")
+      return LargeBlobSupport::REQUIRED;
+    if (*large_blob_support == "preferred")
+      return LargeBlobSupport::PREFERRED;
+  }
 
   // Unknown values are treated as preferred.
   return LargeBlobSupport::PREFERRED;
@@ -571,9 +574,12 @@ TypeConverter<PublicKeyCredentialCreationOptionsPtr,
                  WebAuthenticationResidentKeyRequirementEnabled());
       mojo_options->cred_props = true;
     }
-    if (extensions->largeBlob()) {
-      mojo_options->large_blob_enable =
-          ConvertTo<LargeBlobSupport>(extensions->largeBlob()->support());
+    if (extensions->hasLargeBlob()) {
+      base::Optional<WTF::String> support;
+      if (extensions->largeBlob()->hasSupport()) {
+        support = extensions->largeBlob()->support();
+      }
+      mojo_options->large_blob_enable = ConvertTo<LargeBlobSupport>(support);
     }
   }
 
