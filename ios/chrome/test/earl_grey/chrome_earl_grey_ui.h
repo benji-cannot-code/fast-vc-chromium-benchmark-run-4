468268d2fa0d4992f01d82556879c28601c101a0
Ewann <ewannpv@google.com>
2020-11-25 16:32:26 +0000

    [iOS][PageInfo] Adds EG2 Tests
    This CL adds EG2 the following tests for the page info:
    - Tests that rotating the device will don't dismiss the page info view.
    - Tests that opening the page info on a Chromium page displays the
    correct information.
    
    Bug: 1147414
    Change-Id: I83f3353934e71dba698909fff0ac419e0a182adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2529106
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831033}

--- ios/chrome/test/earl_grey/chrome_earl_grey_ui.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_ui.h
@@ -108,6 +108,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Waits for the app to idle.
 - (void)waitForAppToIdle;
 
+// Opens pageInfo via the tools menu.
+- (void)openPageInfo;
+
 @end
 
 #endif  // IOS_CHROME_TEST_EARL_GREY_CHROME_EARL_GREY_UI_H_
