c14c3aabff35913bc925b0cb401daf81a99aa948
Bartek Nowierski <bartekn@chromium.org>
2020-11-26 05:22:27 +0000

    [PartitionAlloc] Get super pages & direct map in initially decommitted state
    Super pages and direct map have been so far allocated in a fully
    committed state and most of the pages are made inaccessible immediately
    after. This CL reverses the process and commits only necessary pages.
    
    Change-Id: I136c6ac4e1a0d96564739542e82d65ee1562b2c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2531796
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831259}

--- base/allocator/partition_allocator/pcscan.cc
+++ base/allocator/partition_allocator/pcscan.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/allocator/partition_allocator/object_bitmap.h"
+#include "base/allocator/partition_allocator/page_allocator.h"
 #include "base/allocator/partition_allocator/page_allocator_constants.h"
 #include "base/allocator/partition_allocator/partition_address_space.h"
 #include "base/allocator/partition_allocator/partition_alloc.h"
@@ -558,8 +559,9 @@ void PCScan<thread_safe>::RegisterRoot(Root* root) {
     for (char* super_page = super_page_extent->super_page_base;
          super_page != super_page_extent->super_pages_end;
          super_page += kSuperPageSize) {
-      SetSystemPagesAccess(internal::SuperPageQuarantineBitmaps(super_page),
-                           quarantine_bitmaps_size_to_commit, PageReadWrite);
+      RecommitSystemPages(internal::SuperPageQuarantineBitmaps(super_page),
+                          quarantine_bitmaps_size_to_commit, PageReadWrite,
+                          PageUpdatePermissions);
     }
   }
   roots_.Add(root);
