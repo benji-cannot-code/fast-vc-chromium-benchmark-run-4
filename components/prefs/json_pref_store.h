da3e92fe7fc29b4366f686bf44db00d9f7b51ccb
Dominic Battre <battre@chromium.org>
2020-11-26 21:13:18 +0000

    Initialize ImportantFileWriter::previous_data_size_ for JsonPrefStore
    This CL initializes ImportantFileWriter::previous_data_size_ for the
    JsonPrefStore when the latter reads data from disk. This reduces memory
    allocations on the first serialization of data when the JsonPrefStore
    is written to disk.
    
    Bug: 1132866
    Change-Id: Ifb3be7ef13ad7dcadc2203e28e30a1a145bafeb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2561623
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831422}

--- components/prefs/json_pref_store.h
+++ components/prefs/json_pref_store.h
@@ -123,6 +123,10 @@ class COMPONENTS_PREFS_EXPORT JsonPrefStore
 
   void OnStoreDeletionFromDisk() override;
 
+#if defined(UNIT_TEST)
+  base::ImportantFileWriter& get_writer() { return writer_; }
+#endif
+
  private:
   friend class base::JsonPrefStoreCallbackTest;
   friend class base::JsonPrefStoreLossyWriteTest;
