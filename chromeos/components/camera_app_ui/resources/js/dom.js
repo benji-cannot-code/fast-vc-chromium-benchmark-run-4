5209c77f10b5192a58a5e30719408b834653a508
Shik Chen <shik@chromium.org>
2020-11-26 10:52:18 +0000

    CCA: Simplify element creation with dom.create()
    Bug: b/172879638
    Test: Pass Closure check
    Change-Id: I05500d539ed4d8cde433f36e2d4435bc7d07ff41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560533
    Commit-Queue: Shik Chen <shik@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Auto-Submit: Shik Chen <shik@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831305}

--- chromeos/components/camera_app_ui/resources/js/dom.js
+++ chromeos/components/camera_app_ui/resources/js/dom.js
@@ -13,7 +13,7 @@ import {assertInstanceof} from './chrome_util.js';
  * type.
  * @param {!Node} target
  * @param {string} selector
- * @param {function(new: T, ...)} type A user-defined constructor.
+ * @param {function(new: T, ...)} type The expected element type.
  * @return {T}
  * @template T
  */
@@ -26,7 +26,7 @@ export function getFrom(target, selector, type) {
  * their type to be specific type.
  * @param {!Node} target
  * @param {string} selector
- * @param {function(new: T, ...)} type A user-defined constructor.
+ * @param {function(new: T, ...)} type The expected element type.
  * @return {!NodeList<T>}
  * @template T
  */
@@ -41,7 +41,7 @@ export function getAllFrom(target, selector, type) {
 /**
  * Gets an element in document matching css selector and checks its type.
  * @param {string} selector
- * @param {function(new: T, ...)} type A user-defined constructor.
+ * @param {function(new: T, ...)} type The expected element type.
  * @return {T}
  * @template T
  */
@@ -53,12 +53,24 @@ export function get(selector, type) {
  * Gets all elements in document matching css selector and asserts their type to
  * be specific type.
  * @param {string} selector
- * @param {function(new: T, ...)} type A user-defined constructor.
+ * @param {function(new: T, ...)} type The expected element type.
  * @return {!NodeList<T>}
  * @template T
  */
 export function getAll(selector, type) {
   return getAllFrom(document, selector, type);
+}
+
+/**
+ * Creates a typed element.
+ * @param {string} tag The HTML tag of the element to be created.
+ * @param {function(new: T, ...)} type The expected element type.
+ * @return {!T}
+ * @template T
+ */
+export function create(tag, type) {
+  const el = document.createElement(tag);
+  return assertInstanceof(el, type);
 }
 
 /* eslint-enable valid-jsdoc */
