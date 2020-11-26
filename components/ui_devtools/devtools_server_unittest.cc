3283d39640c67ee7e8ce24f274f41987e486d918
Matthew Denton <mpdenton@chromium.org>
2020-11-26 10:17:57 +0000

    Fix UIDevToolsServerTest to use CertVerifierService
    The Network Service previously used concrete net::CertVerifier
    implementations, but is now switching to use the CertVerifierService.
    When creating a NetworkContext, a remote to a CertVerifierService
    needs to be included in the NetworkContextParams.
    
    UIDevToolsServerTest doesn't do this, which is fixed in this CL in
    preparation for enabling the CertVerifierService by default.
    
    Since there's no need to test cert verification in this unit test,
    just add a pipe to a fake CertVerifierService that always passes cert
    verification.
    
    Bug: 1015134, 1111472
    Change-Id: I320d4ce9f7387c4c1faa1bd671887559f75074b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2559613
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831301}

--- components/ui_devtools/devtools_server_unittest.cc
+++ components/ui_devtools/devtools_server_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/socket/tcp_client_socket.h"
 #include "services/network/network_context.h"
 #include "services/network/network_service.h"
+#include "services/network/test/fake_test_cert_verifier_params_factory.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ui_devtools {
@@ -40,10 +41,17 @@ TEST(UIDevToolsServerTest, MAYBE_ConnectionToViewsServer) {
   mojo::Remote<network::mojom::NetworkService> network_service_remote;
   auto network_service = network::NetworkService::Create(
       network_service_remote.BindNewPipeAndPassReceiver());
+
+  network::mojom::NetworkContextParamsPtr context_params =
+      network::mojom::NetworkContextParams::New();
+  // Use a dummy CertVerifier that always passes cert verification, since
+  // these unittests don't need to test CertVerifier behavior.
+  context_params->cert_verifier_params =
+      network::FakeTestCertVerifierParamsFactory::GetCertVerifierParams();
   mojo::Remote<network::mojom::NetworkContext> network_context_remote;
   network_service_remote->CreateNetworkContext(
       network_context_remote.BindNewPipeAndPassReceiver(),
-      network::mojom::NetworkContextParams::New());
+      std::move(context_params));
 
   std::unique_ptr<UiDevToolsServer> server =
       UiDevToolsServer::CreateForViews(network_context_remote.get(), fake_port);
