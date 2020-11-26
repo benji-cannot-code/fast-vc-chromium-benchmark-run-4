6d8b0743955ccaab9a9a5968061dc6a64be9e84d
Chris Lu <thegreenfrog@chromium.org>
2020-11-26 15:39:17 +0000

    [ios] Fix Messages banner icon template coloring
    The _useIconBackgroundTint property needs to be marked as
    YES by default since we are only updating it to NO if necessary.
    The icon image also needs to be configure to use template coloring
    before being added to the backgroundImageView.
    
    Bug: 1149912
    Change-Id: I35060572054c60f85908a24e8de77d4eff8632a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560548
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831356}

--- ios/chrome/browser/ui/infobars/banners/infobar_banner_view_controller.mm
+++ ios/chrome/browser/ui/infobars/banners/infobar_banner_view_controller.mm
@@ -115,6 +115,7 @@ const CGFloat kLongPressTimeDurationInSeconds = 0.4;
     _metricsRecorder =
         [[InfobarMetricsRecorder alloc] initWithType:infobarType];
     _presentsModal = presentsModal;
+    _useIconBackgroundTint = YES;
   }
   return self;
 }
@@ -148,6 +149,12 @@ const CGFloat kLongPressTimeDurationInSeconds = 0.4;
   // Icon setup.
   UIView* iconContainerView = nil;
   if (self.iconImage) {
+    // If the icon image requires a background tint, ignore the original color
+    // information and draw the image as a template image.
+    if (self.useIconBackgroundTint) {
+      self.iconImage = [self.iconImage
+          imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    }
     UIImageView* iconImageView =
         [[UIImageView alloc] initWithImage:self.iconImage];
     iconImageView.contentMode = UIViewContentModeScaleAspectFit;
@@ -156,15 +163,10 @@ const CGFloat kLongPressTimeDurationInSeconds = 0.4;
     UIView* backgroundIconView =
         [[UIView alloc] initWithFrame:iconImageView.frame];
     backgroundIconView.layer.cornerRadius = kIconCornerRadius;
-    backgroundIconView.translatesAutoresizingMaskIntoConstraints = NO;
-
-    // If the icon image requires a background tint, ignore the original color
-    // information and draw the image as a template image.
     if (self.useIconBackgroundTint) {
-      self.iconImage = [self.iconImage
-          imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
       backgroundIconView.backgroundColor = [UIColor colorNamed:kBlueHaloColor];
     }
+    backgroundIconView.translatesAutoresizingMaskIntoConstraints = NO;
 
     iconContainerView = [[UIView alloc] init];
     [iconContainerView addSubview:backgroundIconView];
