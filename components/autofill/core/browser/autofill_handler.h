e85d4350b4747e5ccb27e999f917b779623664d3
Michael Bai <michaelbai@chromium.org>
2020-11-26 00:47:03 +0000

    Autofill: Move AutofillDownloadManager::Observer to AutofillHandler
    This patch moves AutofillDownloadManager::Observer to AutofillHandlder
    No function change.
    
    Bug: 1151542
    Change-Id: I7a290f72d4b51d9362c9cdec44ab74f4ae443a9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2553185
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Michael Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831195}

--- components/autofill/core/browser/autofill_handler.h
+++ components/autofill/core/browser/autofill_handler.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
+#include "components/autofill/core/browser/autofill_download_manager.h"
 #include "components/autofill/core/browser/autofill_driver.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill/core/common/language_code.h"
@@ -35,7 +36,7 @@ class LogManager;
 
 // This class defines the interface should be implemented by autofill
 // implementation in browser side to interact with AutofillDriver.
-class AutofillHandler {
+class AutofillHandler : public AutofillDownloadManager::Observer {
  public:
   enum AutofillDownloadManagerState {
     ENABLE_AUTOFILL_DOWNLOAD_MANAGER,
@@ -49,7 +50,7 @@ class AutofillHandler {
     virtual void OnFormParsed() = 0;
   };
 
-  virtual ~AutofillHandler();
+  ~AutofillHandler() override;
 
   // Invoked when the value of textfield is changed.
   void OnTextFieldDidChange(const FormData& form,
@@ -225,6 +226,14 @@ class AutofillHandler {
   }
 
  private:
+  // AutofillDownloadManager::Observer:
+  void OnLoadedServerPredictions(
+      std::string response,
+      const std::vector<FormSignature>& queried_form_signatures) override;
+  void OnServerRequestError(FormSignature form_signature,
+                            AutofillDownloadManager::RequestType request_type,
+                            int http_error) override;
+
   // Provides driver-level context to the shared code of the component. Must
   // outlive this object.
   AutofillDriver* const driver_;
--- components/autofill/core/browser/autofill_manager.h
+++ components/autofill/core/browser/autofill_manager.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/autofill/core/browser/autocomplete_history_manager.h"
 #include "components/autofill/core/browser/autofill_client.h"
-#include "components/autofill/core/browser/autofill_download_manager.h"
 #include "components/autofill/core/browser/autofill_driver.h"
 #include "components/autofill/core/browser/autofill_handler.h"
 #include "components/autofill/core/browser/autofill_metrics.h"
@@ -79,7 +78,6 @@ enum class ValuePatternsMetric {
 // Manages saving and restoring the user's personal information entered into web
 // forms. One per frame; owned by the AutofillDriver.
 class AutofillManager : public AutofillHandler,
-                        public AutofillDownloadManager::Observer,
                         public AutocompleteHistoryManager::SuggestionsHandler,
                         public CreditCardAccessManager::Accessor {
  public:
