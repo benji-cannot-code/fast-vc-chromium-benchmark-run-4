c5a772a5881032c60f7cbb583f22f9fe8d3afb1c
Christoph Schwering <schwering@google.com>
2020-11-26 13:51:43 +0000

    [Autofill] Simplified field_type_parsing API.
    This CL moves the code for loading of the JSON file from the resource
    bundle (that is, loading of the local file from disk) to the unit test.
    Since the JSON is now compiled to C++, we don't need the resource bundle
    anymore. It's still useful for tests to check that the JSON parsing code
    obtains the same result as the JSON-to-C++ compilation.
    
    Bug: 1147608
    Change-Id: Ie34a33027af52eb8d911f22f877c79683d9cc271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2558343
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/master@{#831331}

--- components/autofill/core/browser/pattern_provider/pattern_configuration_parser_unittest.cc
+++ components/autofill/core/browser/pattern_provider/pattern_configuration_parser_unittest.cc
@@ -24,7 +24,7 @@ namespace field_type_parsing {
 // parsed to the map structure used by |PatternProvider| as
 // expected, given the input is valid.
 TEST(PatternConfigurationParserTest, WellFormedParsedCorrectly) {
-  std::string JSON_message = R"(
+  std::string json_message = R"(
     {
       "version": "1.0",
       "FULL_NAME": {
@@ -62,14 +62,14 @@ TEST(PatternConfigurationParserTest, WellFormedParsedCorrectly) {
         ]
       }
     })";
-  base::Optional<base::Value> JSON_object =
-      base::JSONReader::Read(JSON_message);
+  base::Optional<base::Value> json_object =
+      base::JSONReader::Read(json_message);
 
-  ASSERT_TRUE(JSON_object) << "Incorrectly formatted JSON string.";
+  ASSERT_TRUE(json_object) << "Incorrectly formatted JSON string.";
 
-  base::Version version = ExtractVersionFromJsonObject(JSON_object.value());
+  base::Version version = ExtractVersionFromJsonObject(json_object.value());
   base::Optional<PatternProvider::Map> optional_patterns =
-      GetConfigurationFromJsonObject(JSON_object.value());
+      GetConfigurationFromJsonObject(json_object.value());
 
   ASSERT_TRUE(version.IsValid());
   ASSERT_TRUE(optional_patterns);
@@ -102,7 +102,7 @@ TEST(PatternConfigurationParserTest, WellFormedParsedCorrectly) {
 // Test that the parser does not return anything if some |MatchingPattern|
 // object is missing a property.
 TEST(PatternConfigurationParserTest, MalformedMissingProperty) {
-  std::string JSON_message = R"(
+  std::string json_message = R"(
     {
       "version": "1.0",
       "FULL_NAME": {
@@ -127,13 +127,13 @@ TEST(PatternConfigurationParserTest, MalformedMissingProperty) {
         ]
       }
     })";
-  base::Optional<base::Value> JSON_object =
-      base::JSONReader::Read(JSON_message);
+  base::Optional<base::Value> json_object =
+      base::JSONReader::Read(json_message);
 
-  ASSERT_TRUE(JSON_object) << "Incorrectly formatted JSON string.";
+  ASSERT_TRUE(json_object) << "Incorrectly formatted JSON string.";
 
   base::Optional<PatternProvider::Map> optional_patterns =
-      GetConfigurationFromJsonObject(JSON_object.value());
+      GetConfigurationFromJsonObject(json_object.value());
 
   ASSERT_FALSE(optional_patterns);
 }
@@ -141,7 +141,7 @@ TEST(PatternConfigurationParserTest, MalformedMissingProperty) {
 // Test that the parser correctly sets the default version if
 // it is not present in the configuration.
 TEST(PatternConfigurationParserTest, MalformedMissingVersion) {
-  std::string JSON_message = R"(
+  std::string json_message = R"(
     {
       "FULL_NAME": {
         "en": [
@@ -155,12 +155,12 @@ TEST(PatternConfigurationParserTest, MalformedMissingVersion) {
         ]
       }
     })";
-  base::Optional<base::Value> JSON_object =
-      base::JSONReader::Read(JSON_message);
+  base::Optional<base::Value> json_object =
+      base::JSONReader::Read(json_message);
 
-  ASSERT_TRUE(JSON_object) << "Incorrectly formatted JSON string.";
+  ASSERT_TRUE(json_object) << "Incorrectly formatted JSON string.";
 
-  base::Version version = ExtractVersionFromJsonObject(JSON_object.value());
+  base::Version version = ExtractVersionFromJsonObject(json_object.value());
 
   ASSERT_EQ(base::Version("0"), version);
 }
@@ -168,7 +168,7 @@ TEST(PatternConfigurationParserTest, MalformedMissingVersion) {
 // Test that the parser does not return anything if the inner key points
 // to a single object instead of a list.
 TEST(PatternConfigurationParserTest, MalformedNotList) {
-  std::string JSON_message = R"(
+  std::string json_message = R"(
     {
       "FULL_NAME": {
         "en": {
@@ -180,13 +180,13 @@ TEST(PatternConfigurationParserTest, MalformedNotList) {
         }
       }
     })";
-  base::Optional<base::Value> JSON_object =
-      base::JSONReader::Read(JSON_message);
+  base::Optional<base::Value> json_object =
+      base::JSONReader::Read(json_message);
 
-  ASSERT_TRUE(JSON_object) << "Incorrectly formatted JSON string.";
+  ASSERT_TRUE(json_object) << "Incorrectly formatted JSON string.";
 
   base::Optional<PatternProvider::Map> optional_patterns =
-      GetConfigurationFromJsonObject(JSON_object.value());
+      GetConfigurationFromJsonObject(json_object.value());
 
   ASSERT_FALSE(optional_patterns);
 }
