01a4d09d77f474f7e5129fc58b704ceef48bef7f
Anton Bikineev <bikineev@chromium.org>
2020-11-26 07:27:39 +0000

    PCScan: Fix periodic pcscans and wtf test failure
    Bug: 1129751
    Change-Id: Ib368d3f1be9218f418a832624020d6ff4dac33d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560948
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831275}

--- base/allocator/partition_allocator/pcscan.h
+++ base/allocator/partition_allocator/pcscan.h
@@ -67,6 +67,9 @@ class BASE_EXPORT PCScan final {
 
   ALWAYS_INLINE void MoveToQuarantine(void* ptr, SlotSpan* slot_span);
 
+  // Performs scanning unconditionally.
+  void PerformScan(InvocationMode invocation_mode);
+  // Performs scanning only if a certain quarantine threshold was reached.
   void PerformScanIfNeeded(InvocationMode invocation_mode);
 
   void ClearRootsForTesting();
@@ -135,8 +138,6 @@ class BASE_EXPORT PCScan final {
   };
 
   constexpr PCScan() = default;
-
-  void PerformScan(InvocationMode invocation_mode);
 
   static PCScan instance_ PA_CONSTINIT;
 
