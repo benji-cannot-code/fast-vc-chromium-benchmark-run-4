29e63633238b86d0ee14c9b1afd4ed367f4de5c6
Benoit Lize <lizeb@chromium.org>
2020-11-26 09:57:52 +0000

    [PartitionAlloc] Annotate thread cache paths with (UN)LIKELY().
    Bug: 998048
    Change-Id: I4078711821d9e0ddaa3d9f60be968b7ca3494b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560513
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831295}

--- base/allocator/partition_allocator/thread_cache.h
+++ base/allocator/partition_allocator/thread_cache.h
@@ -209,7 +209,7 @@ ALWAYS_INLINE bool ThreadCache::MaybePutInCache(void* address,
 
   INCREMENT_COUNTER(stats_.cache_fill_count);
 
-  if (bucket_index >= kBucketCount) {
+  if (UNLIKELY(bucket_index >= kBucketCount)) {
     INCREMENT_COUNTER(stats_.cache_fill_misses);
     return false;
   }
@@ -226,7 +226,7 @@ ALWAYS_INLINE bool ThreadCache::MaybePutInCache(void* address,
   INCREMENT_COUNTER(stats_.cache_fill_hits);
 
   // Batched deallocation, amortizing lock acquisitions.
-  if (bucket.count >= kMaxCountPerBucket) {
+  if (UNLIKELY(bucket.count >= kMaxCountPerBucket)) {
     ClearBucket(bucket, kMaxCountPerBucket / 2);
   }
 
@@ -236,7 +236,7 @@ ALWAYS_INLINE bool ThreadCache::MaybePutInCache(void* address,
 ALWAYS_INLINE void* ThreadCache::GetFromCache(size_t bucket_index) {
   INCREMENT_COUNTER(stats_.alloc_count);
   // Only handle "small" allocations.
-  if (bucket_index >= kBucketCount) {
+  if (UNLIKELY(bucket_index >= kBucketCount)) {
     INCREMENT_COUNTER(stats_.alloc_miss_too_large);
     INCREMENT_COUNTER(stats_.alloc_misses);
     return nullptr;
@@ -244,7 +244,7 @@ ALWAYS_INLINE void* ThreadCache::GetFromCache(size_t bucket_index) {
 
   auto& bucket = buckets_[bucket_index];
   auto* result = bucket.freelist_head;
-  if (!result) {
+  if (UNLIKELY(!result)) {
     PA_DCHECK(bucket.count == 0);
     INCREMENT_COUNTER(stats_.alloc_miss_empty);
     INCREMENT_COUNTER(stats_.alloc_misses);
