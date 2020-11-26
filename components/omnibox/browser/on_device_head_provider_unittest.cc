e864b1a44f8f277c8d797184a2dda91a76cd39fd
Ce Chen <cch@chromium.org>
2020-11-26 11:59:03 +0000

    [omnibox] Simplify OnDeviceModelUpdateListener.
    1. Use base::ThreadPool::PostTaskAndReplyWithResult instead of the
    dedicated |task_runner_| kept by the static listener instance which
    causes flakiness of OnDeviceHeadProviderTest detected in android-asan
    (heap-use-after-free e.g. http://shortn/_ZDRJq9pbxa; occurs very often)
    and ios-simulator (SEGV_MAPPER e.g. http://shortn/_xmjDeGjrgu; very
    rare).
    
    2. Make provider read model filename directly from listener instead of
    using CallbackList. CallbackList might cause crbug/1058586 since
    callbacks used for notifying the provider with the model filename might
    fail to run. This change should be thread safe because both
    OnDeviceModelUpdateListener::OnModelUpdate (called by
    OnDeviceHeadSuggestInstallerPolicy::ComponentReady which is posted to
    main thread) and the filename reading will happen on main thread.
    
    Manually tested on real iPhone iOSv14.2.
    
    Bug: 925072, 1151941, 1058586
    Change-Id: I426b468b9a38fa4eaf4e9954686f356a53b8aa8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2557878
    Commit-Queue: Ce Chen <cch@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831314}

--- components/omnibox/browser/on_device_head_provider_unittest.cc
+++ components/omnibox/browser/on_device_head_provider_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_provider_listener.h"
 #include "components/omnibox/browser/fake_autocomplete_provider_client.h"
 #include "components/omnibox/browser/on_device_head_model.h"
+#include "components/omnibox/browser/on_device_model_update_listener.h"
 #include "components/omnibox/browser/test_scheme_classifier.h"
 #include "components/omnibox/common/omnibox_features.h"
 #include "components/search_engines/omnibox_focus_type.h"
@@ -33,7 +34,6 @@ class OnDeviceHeadProviderTest : public testing::Test,
     client_.reset(new FakeAutocompleteProviderClient());
     SetTestOnDeviceHeadModel();
     provider_ = OnDeviceHeadProvider::Create(client_.get(), this);
-    provider_->AddModelUpdateCallback();
     task_environment_.RunUntilIdle();
   }
 
@@ -61,9 +61,9 @@ class OnDeviceHeadProviderTest : public testing::Test,
   }
 
   void ResetModelInstance() {
-    if (provider_) {
-      provider_->model_filename_.clear();
-    }
+    auto* update_listener = OnDeviceModelUpdateListener::GetInstance();
+    if (update_listener)
+      update_listener->ResetListenerForTest();
   }
 
   bool IsOnDeviceHeadProviderAllowed(const AutocompleteInput& input) {
