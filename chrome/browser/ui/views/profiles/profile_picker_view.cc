7c89872d535db293eca6647d75c5d12f08f98b76
Jan Krcal <jkrcal@chromium.org>
2020-11-26 13:49:23 +0000

    Disable FRE for signed-in profile creation flow
    This CL makes sure the FRE is not displayed after signed-in creation
    flow.
    
    Bug: 1126913, 1144065
    Change-Id: Ie36d73f8fd9c1d3bfadcd2bbde8636bc9e35ce90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560738
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831328}

--- chrome/browser/ui/views/profiles/profile_picker_view.cc
+++ chrome/browser/ui/views/profiles/profile_picker_view.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/signin/profile_picker_ui.h"
 #include "chrome/browser/ui/webui/signin/signin_web_dialog_ui.h"
 #include "chrome/browser/ui/webui/signin/sync_confirmation_ui.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/google_chrome_strings.h"
@@ -487,6 +488,10 @@ void ProfilePickerView::FinishSignedInCreationFlowImpl(
   // Unmark this profile ephemeral so that it is not deleted upon next startup.
   entry->SetIsEphemeral(false);
   entry->SetLocalProfileName(name_for_signed_in_profile_);
+
+  // Skip the FRE for this profile as it's replaced by profile creation flow.
+  signed_in_profile_being_created_->GetPrefs()->SetBoolean(
+      prefs::kHasSeenWelcomePage, true);
 
   // TODO(crbug.com/1126913): Change the callback of
   // profiles::OpenBrowserWindowForProfile() to be a OnceCallback as it is only
