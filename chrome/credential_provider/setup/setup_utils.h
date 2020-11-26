d0dde60b9c1091c1345fcc150f9c25e351cc2721
Yusuf Sengul <yusufsn@google.com>
2020-11-26 00:29:14 +0000

    Use installer data argument for installing GCPW from MSI
    Bug: 1152690
    Change-Id: I1cecd128f89a04eee61076b87d54b3f55e98dde9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2559804
    Reviewed-by: Robin Lewis <wrlewis@google.com>
    Commit-Queue: Yusuf Sengul <yusufsn@google.com>
    Cr-Commit-Position: refs/heads/master@{#831186}

--- chrome/credential_provider/setup/setup_utils.h
+++ chrome/credential_provider/setup/setup_utils.h
@@ -12,6 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/win/windows_types.h"
 
+namespace base {
+class DictionaryValue;
+class FilePath;
+}  // namespace base
+
 namespace credential_provider {
 
 namespace switches {
@@ -37,7 +42,11 @@ extern const char kUninstall[];
 extern const char kEnableStats[];
 extern const char kDisableStats[];
 
+// Switch that indicates the fresh installation.
 extern const char kStandaloneInstall[];
+
+// Dynamic install parameter switch which is only set for MSIs.
+extern const char kInstallerData[];
 
 }  // namespace switches
 
@@ -64,10 +73,17 @@ class StandaloneInstallerConfigurator {
 
   base::string16 GetCurrentDate();
 
-  bool is_standalone_installation_;
-};
+  // Parse the provided installer data argument and load into
+  // |installer_data_dictionary_|.
+  bool InitializeFromInstallerData(base::FilePath prefs_path);
 
-bool IsStandaloneInstallation(const base::CommandLine& command_line);
+  // Indicates that GCPW installation source is MSI.
+  bool is_msi_installation_;
+
+  // Dictionary is parsed from the installer data argument which is set only for
+  // MSIs.
+  std::unique_ptr<base::DictionaryValue> installer_data_dictionary_;
+};
 
 }  // namespace credential_provider
 
