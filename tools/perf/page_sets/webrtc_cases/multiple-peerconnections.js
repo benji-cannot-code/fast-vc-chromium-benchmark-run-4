d228a492704d45391705f3f3444588c5dd23d3ea
Ben Wagner <benjaminwagner@google.com>
2020-11-27 09:37:00 +0000

    Update WebRTC perf page sets
    Repinned source repo revisions to latest.
    
    common.js no longer exists, and adapter.js was moved/renamed
    
    The audio sample still exists, but the test cases were removed in
    https://chromium-review.googlesource.com/c/chromium/src/+/833889/, so
    I've removed the files as well.
    
    `./tools/perf/run_benchmark run webrtc --browser=system` passes locally
    on Mac.
    
    TBR=jansson@chromium.org
    
    Bug: 468732
    Change-Id: I426cbc50b82899efe60ddcb6d1c95838c4adc25b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560261
    Commit-Queue: Ben Wagner aka dogben <benjaminwagner@google.com>
    Auto-Submit: Ben Wagner aka dogben <benjaminwagner@google.com>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831512}

--- tools/perf/page_sets/webrtc_cases/multiple-peerconnections.js
+++ tools/perf/page_sets/webrtc_cases/multiple-peerconnections.js
@@ -18,7 +18,7 @@ var cpuOveruseDetectionCheckbox = $('cpuoveruse-detection');
 startTestButton.onclick = startTest;
 
 function logError(err) {
-  console.error(err);
+  console.err(err);
 }
 
 function addNewVideoElement() {
