cf356fd3162928e67258f0709ca6c7356318aee4
sandromaggi <sandromaggi@google.com>
2020-11-27 15:48:58 +0000

    [Autofill Assistant] Add action to release elements from the store
    This adds a new action that releases elements from the store. It can
    be used to define a tight scope on the element from the backend, to
    keep the "chain" scope we currently implicitly have.
    
    Bug: b/172542134
    Change-Id: If76bb10da648a9284f66ad5db8d15a6f9a6e8370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563478
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831577}

--- components/autofill_assistant/browser/protocol_utils.cc
+++ components/autofill_assistant/browser/protocol_utils.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/actions/popup_message_action.h"
 #include "components/autofill_assistant/browser/actions/presave_generated_password_action.h"
 #include "components/autofill_assistant/browser/actions/prompt_action.h"
+#include "components/autofill_assistant/browser/actions/release_elements_action.h"
 #include "components/autofill_assistant/browser/actions/save_generated_password_action.h"
 #include "components/autofill_assistant/browser/actions/select_option_action.h"
 #include "components/autofill_assistant/browser/actions/set_attribute_action.h"
@@ -331,6 +332,8 @@ std::unique_ptr<Action> ProtocolUtils::CreateAction(ActionDelegate* delegate,
           delegate, action, action.check_element_is_on_top().client_id(),
           base::BindOnce(&WebController::CheckOnTop,
                          delegate->GetWebController()->GetWeakPtr()));
+    case ActionProto::ActionInfoCase::kReleaseElements:
+      return std::make_unique<ReleaseElementsAction>(delegate, action);
     case ActionProto::ActionInfoCase::ACTION_INFO_NOT_SET: {
       VLOG(1) << "Encountered action with ACTION_INFO_NOT_SET";
       return std::make_unique<UnsupportedAction>(delegate, action);
