e25579bee60c5ba91410a04bbc532a9c976b28e3
Kenichi Ishibashi <bashi@chromium.org>
2020-11-27 07:53:33 +0000

    Support retry for service worker resource handling methods
    This is similar to crrev.com/c/2550108 but for
    StoreUncommittedResourceId() and DoomUncommittedResource().
    
    Bug: 1133143
    Change-Id: Id71677c9558c002568fc16299bce52c9d07a0bc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2562918
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831499}

--- content/browser/service_worker/service_worker_registry.cc
+++ content/browser/service_worker/service_worker_registry.cc
@@ -538,10 +538,12 @@ void ServiceWorkerRegistry::UpdateNavigationPreloadHeader(
 void ServiceWorkerRegistry::StoreUncommittedResourceId(int64_t resource_id,
                                                        const GURL& origin) {
   DCHECK_CURRENTLY_ON(ServiceWorkerContext::GetCoreThreadId());
-  GetRemoteStorageControl()->StoreUncommittedResourceId(
-      resource_id, origin,
-      base::BindOnce(&ServiceWorkerRegistry::DidWriteUncommittedResourceIds,
-                     weak_factory_.GetWeakPtr()));
+  CreateInvokerAndStartRemoteCall(
+      &storage::mojom::ServiceWorkerStorageControl::StoreUncommittedResourceId,
+      base::BindRepeating(
+          &ServiceWorkerRegistry::DidWriteUncommittedResourceIds,
+          weak_factory_.GetWeakPtr()),
+      static_cast<const int64_t>(resource_id), origin);
 }
 
 void ServiceWorkerRegistry::DoomUncommittedResource(int64_t resource_id) {
@@ -857,10 +859,11 @@ ServiceWorkerRegistry::FindFromLiveRegistrationsForId(int64_t registration_id) {
 void ServiceWorkerRegistry::DoomUncommittedResources(
     const std::vector<int64_t>& resource_ids) {
   DCHECK_CURRENTLY_ON(ServiceWorkerContext::GetCoreThreadId());
-  GetRemoteStorageControl()->DoomUncommittedResources(
-      resource_ids,
-      base::BindOnce(&ServiceWorkerRegistry::DidDoomUncommittedResourceIds,
-                     weak_factory_.GetWeakPtr()));
+  CreateInvokerAndStartRemoteCall(
+      &storage::mojom::ServiceWorkerStorageControl::DoomUncommittedResources,
+      base::BindRepeating(&ServiceWorkerRegistry::DidDoomUncommittedResourceIds,
+                          weak_factory_.GetWeakPtr()),
+      resource_ids);
 }
 
 void ServiceWorkerRegistry::DidFindRegistrationForClientUrl(
@@ -1225,13 +1228,19 @@ void ServiceWorkerRegistry::DidUpdateRegistration(
 }
 
 void ServiceWorkerRegistry::DidWriteUncommittedResourceIds(
+    uint64_t call_id,
     storage::mojom::ServiceWorkerDatabaseStatus status) {
+  DCHECK_CURRENTLY_ON(ServiceWorkerContext::GetCoreThreadId());
+  FinishRemoteCall(call_id);
   if (status != storage::mojom::ServiceWorkerDatabaseStatus::kOk)
     ScheduleDeleteAndStartOver();
 }
 
 void ServiceWorkerRegistry::DidDoomUncommittedResourceIds(
+    uint64_t call_id,
     storage::mojom::ServiceWorkerDatabaseStatus status) {
+  DCHECK_CURRENTLY_ON(ServiceWorkerContext::GetCoreThreadId());
+  FinishRemoteCall(call_id);
   if (status != storage::mojom::ServiceWorkerDatabaseStatus::kOk)
     ScheduleDeleteAndStartOver();
 }
