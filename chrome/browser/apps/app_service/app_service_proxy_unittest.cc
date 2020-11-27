050db64ae5e3383ec71da2142462b2a21ba2cdbd
Ramin Halavati <rhalavati@chromium.org>
2020-11-27 05:36:08 +0000

    Update Apps service and user type tests for ephemeral Guest profiles.
    Updates Apps tests to cover ephemeral Guest profiles.
    
    Please see go/ephemeral-guest-profiles for more context.
    
    Bug: 1125474
    Change-Id: Iabfe861a0b72603dac46294e4a6a70c5ad2582e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560950
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831480}

--- chrome/browser/apps/app_service/app_service_proxy_unittest.cc
+++ chrome/browser/apps/app_service/app_service_proxy_unittest.cc
@@ -189,7 +189,19 @@ TEST_F(AppServiceProxyTest, IconCoalescer) {
   EXPECT_EQ(6, NumOuterFinishedCallbacks());
 }
 
-TEST_F(AppServiceProxyTest, ProxyAccessPerProfile) {
+class GuestAppServiceProxyTest : public AppServiceProxyTest,
+                                 public ::testing::WithParamInterface<bool> {
+ public:
+  GuestAppServiceProxyTest() {
+    TestingProfile::SetScopedFeatureListForEphemeralGuestProfiles(
+        scoped_feature_list_, GetParam());
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+TEST_P(GuestAppServiceProxyTest, ProxyAccessPerProfile) {
   TestingProfile::Builder profile_builder;
 
   // We expect an App Service in a regular profile.
@@ -216,7 +228,7 @@ TEST_F(AppServiceProxyTest, ProxyAccessPerProfile) {
   EXPECT_NE(guest_proxy, proxy);
 }
 
-TEST_F(AppServiceProxyTest, RedirectInIncognitoProxyAccessPerProfile) {
+TEST_P(GuestAppServiceProxyTest, RedirectInIncognitoProxyAccessPerProfile) {
   TestingProfile::Builder profile_builder;
 
   // We expect an App Service in a regular profile.
@@ -249,3 +261,7 @@ TEST_F(AppServiceProxyTest, RedirectInIncognitoProxyAccessPerProfile) {
   EXPECT_TRUE(guest_proxy);
   EXPECT_NE(guest_proxy, proxy);
 }
+
+INSTANTIATE_TEST_SUITE_P(AllGuestTypes,
+                         GuestAppServiceProxyTest,
+                         /*is_ephemeral=*/testing::Bool());
