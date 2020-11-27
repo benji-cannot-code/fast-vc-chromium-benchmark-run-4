5795ab28da2ac3f5efd48438fc0368e9a967495f
Liquan (Max) Gu <maxlg@chromium.org>
2020-11-27 18:34:08 +0000

    [WebLayer] Clean up the payment details validations
    Changes:
    * Removed the extra PaymentDetails validation from
      PRService.createIfParamsValid() because PRService.initAndValidate()
      has done so.
    * Removed the extra method-data validation from
      PRService.createIfParamsValid() because
      PRService.getValidatedMethodData() has done so.
    * For the PaymentDetails validation in updateWith() and continueShow()
      add "details.id == null" check for continueShow() to be consistent
      with updateWith()'s. And move both checks into
      isPaymentDetailsUpdateValid().
    
    Bug: 1153163
    
    Change-Id: Iff5618589bb8c2435a8ec134def21b608e5a55e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2562421
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831624}

--- components/payments/content/android/java/src/org/chromium/components/payments/PaymentRequestService.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/PaymentRequestService.java
@@ -415,16 +415,8 @@ public class PaymentRequestService
             return null;
         }
 
-        for (PaymentMethodData datum : methodData) {
-            if (datum == null || TextUtils.isEmpty(datum.supportedMethod)) {
-                abortForInvalidDataFromRenderer(
-                        client, journeyLogger, ErrorStrings.INVALID_PAYMENT_METHODS_OR_DATA);
-                return null;
-            }
-        }
-
         // details has default value, so could never be null, according to payment_request.idl.
-        if (details == null || details.id == null || details.total == null) {
+        if (details == null) {
             abortForInvalidDataFromRenderer(
                     client, journeyLogger, ErrorStrings.INVALID_PAYMENT_DETAILS);
             return null;
@@ -561,6 +553,8 @@ public class PaymentRequestService
 
     private boolean initAndValidate(Factory factory, PaymentMethodData[] rawMethodData,
             PaymentDetails details, boolean googlePayBridgeEligible) {
+        assert rawMethodData != null;
+        assert details != null;
         mBrowserPaymentRequest = factory.createBrowserPaymentRequest(this);
         mJourneyLogger.recordCheckoutStep(CheckoutFunnelStep.INITIATED);
 
@@ -602,7 +596,7 @@ public class PaymentRequestService
         mQueryForQuota = new HashMap<>(methodData);
         mBrowserPaymentRequest.modifyQueryForQuotaCreatedIfNeeded(mQueryForQuota, mPaymentOptions);
 
-        if (details == null || details.id == null || details.total == null
+        if (details.id == null || details.total == null
                 || !mDelegate.validatePaymentDetails(details)) {
             mJourneyLogger.setAborted(AbortReason.INVALID_DATA_FROM_RENDERER);
             disconnectFromClientWithDebugMessage(ErrorStrings.INVALID_PAYMENT_DETAILS,
@@ -1159,13 +1153,18 @@ public class PaymentRequestService
                 && invokedPaymentApp.isValidForPaymentMethodData(methodName, null);
     }
 
-    private String continueShow(PaymentDetails details) {
+    private boolean isPaymentDetailsUpdateValid(PaymentDetails details) {
+        // ID cannot be updated. Updating the total is optional.
+        return details.id == null && mDelegate.validatePaymentDetails(details)
+                && mBrowserPaymentRequest.parseAndValidateDetailsFurtherIfNeeded(details);
+    }
+
+    private String continueShow(@Nullable PaymentDetails details) {
         assert mIsShowWaitingForUpdatedDetails;
         // mSpec.updateWith() can be used only when mSpec has not been destroyed.
         assert !mSpec.isDestroyed();
 
-        if (!mDelegate.validatePaymentDetails(details)
-                || !mBrowserPaymentRequest.parseAndValidateDetailsFurtherIfNeeded(details)) {
+        if (details == null || !isPaymentDetailsUpdateValid(details)) {
             mJourneyLogger.setAborted(AbortReason.INVALID_DATA_FROM_RENDERER);
             return ErrorStrings.INVALID_PAYMENT_DETAILS;
         }
@@ -1182,9 +1181,10 @@ public class PaymentRequestService
 
     /**
      * The component part of the {@link PaymentRequest#updateWith} implementation.
-     * @param details The details that the merchant provides to update the payment request.
+     * @param details The details that the merchant provides to update the payment request, can be
+     *         null.
      */
-    /* package */ void updateWith(PaymentDetails details) {
+    /* package */ void updateWith(@Nullable PaymentDetails details) {
         if (mBrowserPaymentRequest == null) return;
         if (mIsShowWaitingForUpdatedDetails) {
             // Under this condition, updateWith() is called in response to the resolution of
@@ -1213,12 +1213,10 @@ public class PaymentRequestService
             return;
         }
 
-        // ID cannot be updated. Updating the total is optional.
-        if (details == null || details.id != null || !mDelegate.validatePaymentDetails(details)
-                || !mBrowserPaymentRequest.parseAndValidateDetailsFurtherIfNeeded(details)) {
+        if (details == null || !isPaymentDetailsUpdateValid(details)) {
             mJourneyLogger.setAborted(AbortReason.INVALID_DATA_FROM_RENDERER);
-            disconnectFromClientWithDebugMessage(
-                    ErrorStrings.INVALID_PAYMENT_DETAILS, PaymentErrorReason.USER_CANCEL);
+            disconnectFromClientWithDebugMessage(ErrorStrings.INVALID_PAYMENT_DETAILS,
+                    PaymentErrorReason.INVALID_DATA_FROM_RENDERER);
             return;
         }
         mSpec.updateWith(details);
