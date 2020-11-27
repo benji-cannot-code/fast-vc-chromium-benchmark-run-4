ee69d344ae8d7f4d19b00239f5299d8f9fc1501f
Morten Stenshorne <mstensho@chromium.org>
2020-11-27 06:17:51 +0000

    Use ContainerFragment() when hit-testing inline formatting contexts.
    And always let SetNodeAndPosition() set the fragment on its own, rather
    than doing it manually.
    
    Set the correct container fragment right away when hit-testing inline
    content, rather than relying on someone further up on the stack doing
    it. If we do it further up on the stack, we might set the fragment of an
    ancestor node when hit-testing inside a legacy subtree (which has no
    fragments).
    
    Because of this change, we'll also set a fragment when hitting
    relatively positioned inlines. This needs to be cleared again (because
    NG PositionForPoint() doesn't work for relatively positioned inlines
    (the point might not intersect with the line boxes, even if it
    intersects with the relatively positioned inline)).
    
    This bug is easier to reproduce when LayoutNGFragmentTraversal is
    enabled, because if it isn't, we'll hit test by traversing the
    LayoutObject tree in most cases (and therefore never set any fragments
    during hit-testing at all). However, this isn't the case for flex
    containers, for instance. They traverse the fragment tree even when the
    feature is disabled (which is a bit surprising, but probably fine).
    
    This bug wasn't caught by any existing tests, because we enable
    EditingNG when running web_tests (but it's not enabled for "stable"
    yet). When EditingNG is enabled, we won't have any problems, since
    everything is NG, so everything has fragments. As such, it's tricky to
    write a web_test for it (unless we introduce a virtual test suite for
    it), so I wrote a unit test instead, which disables EditingNG and
    enables LayoutNGFragmentTraversal.
    
    Bug: 1153037
    Change-Id: I123e62db68c4bfc0c74fbb2fdac41958e04fa9bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2562246
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831484}

--- third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
+++ third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
@@ -360,18 +360,10 @@ bool NodeAtPointInFragment(const NGPhysicalBoxFragment& fragment,
 void UpdateHitTestResult(HitTestResult& result,
                          const NGPhysicalBoxFragment& fragment,
                          PhysicalOffset offset) {
-  Node* node = fragment.NodeForHitTest();
-  if (!node)
+  if (result.InnerNode())
     return;
-
-  // We may already have set an inner node, but not a box fragment, if the inner
-  // node was text or non-atomic inline content. Set the containing box fragment
-  // now, unless it's an anonymous CSS box.
-  if (!result.BoxFragment() && (!fragment.IsCSSBox() || fragment.GetNode()))
-    result.SetBoxFragment(&fragment);
-
-  if (!result.InnerNode())
-    result.SetNodeAndPosition(node, offset);
+  if (Node* node = fragment.NodeForHitTest())
+    result.SetNodeAndPosition(node, &fragment, offset);
 }
 
 // Return an ID for this fragmentainer, which is unique within the fragmentation
@@ -2142,12 +2134,14 @@ bool NGBoxFragmentPainter::HitTestTextFragment(
     return false;
 
   return hit_test.AddNodeToResult(
-      text_fragment.NodeForHitTest(), /* box_fragment */ nullptr, rect,
+      text_fragment.NodeForHitTest(), &cursor.ContainerFragment(), rect,
       physical_offset - text_paint_fragment->OffsetInContainerFragment());
 }
 
-bool NGBoxFragmentPainter::HitTestTextItem(const HitTestContext& hit_test,
-                                           const NGFragmentItem& text_item) {
+bool NGBoxFragmentPainter::HitTestTextItem(
+    const HitTestContext& hit_test,
+    const NGFragmentItem& text_item,
+    const NGInlineBackwardCursor& cursor) {
   DCHECK(text_item.IsText());
 
   if (hit_test.action != kHitTestForeground)
@@ -2169,7 +2163,7 @@ bool NGBoxFragmentPainter::HitTestTextItem(const HitTestContext& hit_test,
     return false;
 
   return hit_test.AddNodeToResult(text_item.NodeForHitTest(),
-                                  /* box_fragment */ nullptr, rect,
+                                  &cursor.ContainerFragment(), rect,
                                   hit_test.inline_root_offset);
 }
 
@@ -2329,7 +2323,7 @@ bool NGBoxFragmentPainter::HitTestChildBoxItem(
     bounds_rect = PhysicalRect(PixelSnappedIntRect(bounds_rect));
     if (hit_test.location.Intersects(bounds_rect)) {
       if (hit_test.AddNodeToResult(item.NodeForHitTest(),
-                                   /* box_fragment */ nullptr, bounds_rect,
+                                   &cursor.ContainerFragment(), bounds_rect,
                                    child_offset))
         return true;
     }
@@ -2502,7 +2496,7 @@ bool NGBoxFragmentPainter::HitTestItemsChildren(
     }
 
     if (item->IsText()) {
-      if (HitTestTextItem(hit_test, *item))
+      if (HitTestTextItem(hit_test, *item, cursor))
         return true;
     } else if (item->Type() == NGFragmentItem::kLine) {
       const NGPhysicalLineBoxFragment* child_fragment = item->LineBoxFragment();
