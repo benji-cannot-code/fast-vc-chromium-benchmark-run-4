cbdea16f1ce46b0af66451da99da365d542e181e
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-11-27 02:51:22 +0000

    Import wpt@fcb3495f6321b32f607cabcbe0e63a918f48d348
    Using wpt-import in Chromium 859fb1a0ff544649ff6a28d0df0cd40c2a6d1135.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    jsbell@chromium.org, mek@chromium.org:
      external/wpt/FileAPI
    
    NOAUTOREVERT=true
    TBR=robertma@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Ie8a7dd30ef667f2366f5b7a2b8e47b38d0f9f5c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2562940
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#831460}

--- third_party/blink/web_tests/external/wpt/FileAPI/file/resources/echo-content-escaped.py
+++ third_party/blink/web_tests/external/wpt/FileAPI/file/resources/echo-content-escaped.py
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from six import PY3
 from wptserve.utils import isomorphic_encode
 
 # Outputs the request body, with controls and non-ASCII bytes escaped
@@ -6,13 +7,13 @@ from wptserve.utils import isomorphic_encode
 # As a convenience, CRLF newlines are left as is.
 
 def escape_byte(byte):
-    # Iterating over a binary string gives different types in Py2 & Py3.
-    # Py3: bytes -> int
-    # Py2: str -> str (of length 1), so we convert it to int
-    code = byte if type(byte) is int else ord(byte)
-    if 0 <= code <= 0x1F or code >= 0x7F:
-        return b"\\x%02x" % code
-    if code == ord(b"\\"):
+    # We want a single-char binary string, but in Python 3 we may get an int
+    # from iterating over bytes.
+    if PY3:
+        byte = bytes([byte])
+    if b"\0" <= byte <= b"\x1F" or byte >= b"\x7F":
+        return b"\\x%02x" % ord(byte)
+    if byte == b"\\":
         return b"\\\\"
     return byte
 
