1c0ca56341a083c9c39061666bfbb68852c78094
Jaeyong Bae <jdragon.bae@gmail.com>
2020-11-27 01:51:02 +0000

    [CSS] Check border-image-* when serializing border shorthand
    This patch add checking logic for border-image-* initial specified
    values when serializing border shorthand.
    
    Makes tests pass:
    external/wpt/css/cssom/border-shorthand-serialization.html
    
    Bug: 1021726
    Change-Id: I3de1e8e6967e801994f488a320fb4334ee47b400
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2554279
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Jaeyong Bae <jdragon.bae@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#831448}

--- third_party/blink/renderer/core/css/style_property_serializer.cc
+++ third_party/blink/renderer/core/css/style_property_serializer.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_value_pair.h"
 #include "third_party/blink/renderer/core/css/css_value_pool.h"
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
+#include "third_party/blink/renderer/core/css/properties/longhand.h"
 #include "third_party/blink/renderer/core/css_value_keywords.h"
 #include "third_party/blink/renderer/core/style_property_shorthand.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
@@ -1109,10 +1110,29 @@ String StylePropertySerializer::BorderPropertyValue(
     const StylePropertyShorthand& width,
     const StylePropertyShorthand& style,
     const StylePropertyShorthand& color) const {
-  const StylePropertyShorthand properties[3] = {width, style, color};
+  const CSSProperty* border_image_properties[] = {
+      &GetCSSPropertyBorderImageSource(), &GetCSSPropertyBorderImageSlice(),
+      &GetCSSPropertyBorderImageWidth(), &GetCSSPropertyBorderImageOutset(),
+      &GetCSSPropertyBorderImageRepeat()};
+
+  // If any of the border-image longhands differ from their initial
+  // specified values, we should not serialize to a border shorthand
+  // declaration.
+  for (const auto* border_image_property : border_image_properties) {
+    const CSSValue* value =
+        property_set_.GetPropertyCSSValue(*border_image_property);
+    const CSSValue* initial_specified_value =
+        To<Longhand>(*border_image_property).InitialValue();
+    if (value && !value->IsInitialValue() &&
+        *value != *initial_specified_value) {
+      return String();
+    }
+  }
+
+  const StylePropertyShorthand shorthand_properties[3] = {width, style, color};
   StringBuilder result;
-  for (size_t i = 0; i < base::size(properties); ++i) {
-    String value = GetCommonValue(properties[i]);
+  for (const auto& shorthand_property : shorthand_properties) {
+    const String value = GetCommonValue(shorthand_property);
     if (value.IsNull())
       return String();
     if (value == "initial")
