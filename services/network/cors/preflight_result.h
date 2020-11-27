207b0f5cc62a9e73a7a4c29be20004e6a2b71579
Takashi Toyoshima <toyoshim@chromium.org>
2020-11-27 07:45:08 +0000

    OOR-CORS: Move preflight related files to the private directory
    As Blink CORS was removed, the network service is the only place
    to implement CORS preflight cache and we don't need to place them
    into the public/cpp directory any more.
    
    This patch replaces them into the //services/network/cors.
    
    Bug: 1053866
    Change-Id: I6e7f9cc565996942f362091157fb832c4be2b15f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551865
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Auto-Submit: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831495}

--- services/network/cors/preflight_result.h
+++ services/network/cors/preflight_result.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SERVICES_NETWORK_PUBLIC_CPP_CORS_PREFLIGHT_RESULT_H_
-#define SERVICES_NETWORK_PUBLIC_CPP_CORS_PREFLIGHT_RESULT_H_
+#ifndef SERVICES_NETWORK_CORS_PREFLIGHT_RESULT_H_
+#define SERVICES_NETWORK_CORS_PREFLIGHT_RESULT_H_
 
 #include <memory>
 #include <string>
@@ -31,7 +31,7 @@ namespace cors {
 // Holds CORS-preflight request results, and provides access check methods.
 // Each instance can be cached by CORS-preflight cache.
 // See https://fetch.spec.whatwg.org/#concept-cache.
-class COMPONENT_EXPORT(NETWORK_CPP) PreflightResult final {
+class COMPONENT_EXPORT(NETWORK_SERVICE) PreflightResult final {
  public:
   static void SetTickClockForTesting(const base::TickClock* tick_clock);
 
@@ -102,4 +102,4 @@ class COMPONENT_EXPORT(NETWORK_CPP) PreflightResult final {
 
 }  // namespace network
 
-#endif  // SERVICES_NETWORK_PUBLIC_CPP_CORS_PREFLIGHT_RESULT_H_
+#endif  // SERVICES_NETWORK_CORS_PREFLIGHT_RESULT_H_
