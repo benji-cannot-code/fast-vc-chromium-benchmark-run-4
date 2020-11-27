237d51785e4de2f6495fd557eae5557f01d6b2f8
Liquan (Max) Gu <maxlg@chromium.org>
2020-11-27 17:47:31 +0000

    [WebLayer] Tests the show and app-ready condition
    Changes:
    * PRService delegate PaymentAppService.getInstance() for the ease of
      testing.
    * Tested PRService#onDoneCreatingPaymentApps.
    
    Bug: 1152662
    Change-Id: I6cb9e26e0e3df4b6d1374602a61901dc1cab9863
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2561272
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831611}

--- components/payments/content/android/junit/src/org/chromium/components/payments/PaymentRequestServiceBuilder.java
+++ components/payments/content/android/junit/src/org/chromium/components/payments/PaymentRequestServiceBuilder.java
@@ -30,6 +30,7 @@ public class PaymentRequestServiceBuilder implements PaymentRequestService.Deleg
     private final Delegate mDelegate;
     private final RenderFrameHost mRenderFrameHost;
     private final Runnable mOnClosedListener;
+    private final PaymentAppService mPaymentAppService;
     private final Factory mBrowserPaymentRequestFactory;
     private WebContents mWebContents;
     private boolean mIsOffTheRecord = true;
@@ -49,12 +50,14 @@ public class PaymentRequestServiceBuilder implements PaymentRequestService.Deleg
     private PaymentRequestSpec mSpec;
 
     /* package */ static PaymentRequestServiceBuilder defaultBuilder(Runnable onClosedListener,
-            PaymentRequestClient client, BrowserPaymentRequest browserPaymentRequest) {
-        return new PaymentRequestServiceBuilder(onClosedListener, client, browserPaymentRequest);
+            PaymentRequestClient client, PaymentAppService appService,
+            BrowserPaymentRequest browserPaymentRequest) {
+        return new PaymentRequestServiceBuilder(
+                onClosedListener, client, appService, browserPaymentRequest);
     }
 
     private PaymentRequestServiceBuilder(Runnable onClosedListener, PaymentRequestClient client,
-            BrowserPaymentRequest browserPaymentRequest) {
+            PaymentAppService appService, BrowserPaymentRequest browserPaymentRequest) {
         mDelegate = this;
         mWebContents = Mockito.mock(WebContents.class);
         setTopLevelOrigin("https://top.level.origin");
@@ -77,6 +80,7 @@ public class PaymentRequestServiceBuilder implements PaymentRequestService.Deleg
         mBrowserPaymentRequestFactory = (paymentRequestService) -> browserPaymentRequest;
         mOnClosedListener = onClosedListener;
         mClient = client;
+        mPaymentAppService = appService;
     }
 
     @Override
@@ -140,6 +144,11 @@ public class PaymentRequestServiceBuilder implements PaymentRequestService.Deleg
             PaymentDetails details, Collection<PaymentMethodData> values,
             String defaultLocaleString) {
         return mSpec;
+    }
+
+    @Override
+    public PaymentAppService getPaymentAppService() {
+        return mPaymentAppService;
     }
 
     /* package */ PaymentRequestServiceBuilder setRenderFrameHostLastCommittedOrigin(
