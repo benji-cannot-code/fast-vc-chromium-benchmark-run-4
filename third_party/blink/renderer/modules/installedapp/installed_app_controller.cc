5673fe65f5d605f30a99f5ad1354c2b794e2fd5a
Rayan Kanso <rayankans@google.com>
2020-11-27 15:54:28 +0000

    [gIRA] Add UKM collection.
    Doc [Access Restricted]: https://docs.google.com/document/d/1tQ5J6tjNma8QItaZRpohWaoMJn30j5meg6UCjnXsyxw/
    
    Bug: 1136859
    Change-Id: Ief1132e0ac792239d91f7ae197a2e720a7a06fb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560590
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831582}

--- third_party/blink/renderer/modules/installedapp/installed_app_controller.cc
+++ third_party/blink/renderer/modules/installedapp/installed_app_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "services/metrics/public/cpp/ukm_builders.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 #include "third_party/blink/public/common/manifest/manifest.h"
 #include "third_party/blink/public/mojom/installedapp/related_application.mojom-blink.h"
@@ -106,6 +107,12 @@ void InstalledAppController::OnFilterInstalledApps(
       app->setVersion(res->version);
     applications.push_back(app);
   }
+
+  LocalDOMWindow* window = GetSupplementable();
+  ukm::builders::InstalledRelatedApps(window->UkmSourceID())
+      .SetCalled(true)
+      .Record(window->UkmRecorder());
+
   callbacks->OnSuccess(applications);
 }
 
