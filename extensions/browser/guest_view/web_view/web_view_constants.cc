a9a7ec60103a5fc803bf5459db4a2d81a9407c9e
Kevin McNee <mcnee@chromium.org>
2020-11-27 21:23:30 +0000

    Clean up WebViewGuest URL handling
    WebViewGuest is imprecise about what kinds of URLs (e.g. last committed,
    virtual) it intends to handle. In particular, it's using the visible
    (top level virtual) URL for any navigation that commits in a webview.
    
    We clean up this usage and make some clarifying comments about base URL
    usage renderer-side.
    
    Note that since this would change the loadcommit event's URL in some
    cases, we maintain the incorrect behaviour for now. We can attempt to
    correct the behaviour in a subsequent CL, which could be reverted
    independently if code has come to rely on this behaviour.
    
    Bug: 1149615
    Change-Id: Ia2277adc629134b2d137b12160790ddfa715992c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545071
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831648}

--- extensions/browser/guest_view/web_view/web_view_constants.cc
+++ extensions/browser/guest_view/web_view/web_view_constants.cc
@@ -110,6 +110,7 @@ const char kInternalBaseURLForDataURL[] = "baseUrlForDataUrl";
 const char kInternalCurrentEntryIndex[] = "currentEntryIndex";
 const char kInternalEntryCount[] = "entryCount";
 const char kInternalProcessId[] = "processId";
+const char kInternalVisibleUrl[] = "visibleUrl";
 
 // Parameters to callback functions.
 const char kFindNumberOfMatches[] = "numberOfMatches";
