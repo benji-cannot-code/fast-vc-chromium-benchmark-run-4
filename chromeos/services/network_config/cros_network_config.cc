43267eda6c9425275e08d809571f3429072c833f
Maksim Ivanov <emaxx@chromium.org>
2020-11-30 18:42:36 +0000

    Fix double-callback-call crash in //chromeos/services/network_config/
    Fix bug found by the "bugprone-use-after-move" clang-tidy check.
    
    Bug: 1122844
    Change-Id: I3420b6c260b9fa94847b9b63df0438a9bce0b1c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2556838
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831916}

--- chromeos/services/network_config/cros_network_config.cc
+++ chromeos/services/network_config/cros_network_config.cc
@@ -2001,6 +2001,7 @@ void CrosNetworkConfig::SetProperties(const std::string& guid,
           << "SetProperties called with ethernet.eap but no EAP config: "
           << guid;
       std::move(callback).Run(false, kErrorNetworkUnavailable);
+      return;
     }
     network = eap_state;
   }
