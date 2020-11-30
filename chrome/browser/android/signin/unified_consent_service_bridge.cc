9f8cd05fd0281f7b8d42086bab3d661b329d1914
Alice Wang <aliceywang@chromium.org>
2020-11-30 14:46:10 +0000

    [Signin][Modularization] Move UnifiedConsentServiceBridge to signin.services target
    This CL moves UnifiedConsentServiceBridge to signin.services target
    for signin backend modularization.
    
    Bug: 1153046
    Change-Id: I4f5c6e0848ad2c3daffdfa634217679cdf38f16d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2561384
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831837}

--- chrome/browser/android/signin/unified_consent_service_bridge.cc
+++ chrome/browser/android/signin/unified_consent_service_bridge.cc
@@ -4,9 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/android/jni_android.h"
-#include "chrome/android/chrome_jni_headers/UnifiedConsentServiceBridge_jni.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_android.h"
+#include "chrome/browser/signin/services/android/jni_headers/UnifiedConsentServiceBridge_jni.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/unified_consent/unified_consent_service_factory.h"
 #include "components/prefs/pref_service.h"
