959892e9d5dc0a94e660ee895e71b0c5ec138851
Rune Lillesveen <futhark@chromium.org>
2020-11-30 13:26:09 +0000

    Remove unnecessary "author" part.
    While working on moving user style into a separate ScopedStyleResolver,
    the term "author" did not make sense. It also generally didn't seem to
    add any value here.
    
    Bug: 336876
    Change-Id: Ib70f4ace0410fc9598e45db349e866beca37bd0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563825
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831828}

--- third_party/blink/renderer/core/css/style_sheet_collection.h
+++ third_party/blink/renderer/core/css/style_sheet_collection.h
@@ -54,8 +54,8 @@ class CORE_EXPORT StyleSheetCollection
   StyleSheetCollection& operator=(const StyleSheetCollection&) = delete;
   virtual ~StyleSheetCollection() = default;
 
-  const ActiveStyleSheetVector& ActiveAuthorStyleSheets() const {
-    return active_author_style_sheets_;
+  const ActiveStyleSheetVector& ActiveStyleSheets() const {
+    return active_style_sheets_;
   }
   const HeapVector<Member<StyleSheet>>& StyleSheetsForStyleSheetList() const {
     return style_sheets_for_style_sheet_list_;
@@ -76,7 +76,7 @@ class CORE_EXPORT StyleSheetCollection
 
  protected:
   HeapVector<Member<StyleSheet>> style_sheets_for_style_sheet_list_;
-  ActiveStyleSheetVector active_author_style_sheets_;
+  ActiveStyleSheetVector active_style_sheets_;
   bool sheet_list_dirty_ = true;
 };
 
--- third_party/blink/renderer/core/css/tree_scope_style_sheet_collection.cc
+++ third_party/blink/renderer/core/css/tree_scope_style_sheet_collection.cc
@@ -54,8 +54,7 @@ void TreeScopeStyleSheetCollection::AddStyleSheetCandidateNode(Node& node) {
 void TreeScopeStyleSheetCollection::ApplyActiveStyleSheetChanges(
     StyleSheetCollection& new_collection) {
   GetDocument().GetStyleEngine().ApplyRuleSetChanges(
-      GetTreeScope(), ActiveAuthorStyleSheets(),
-      new_collection.ActiveAuthorStyleSheets());
+      GetTreeScope(), ActiveStyleSheets(), new_collection.ActiveStyleSheets());
   new_collection.Swap(*this);
 }
 
