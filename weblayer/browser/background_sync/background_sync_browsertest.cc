ff9021e2986da36adf0f8602b05f4f87d24aa1d9
Mugdha Lakhani <nator@chromium.org>
2020-11-30 15:03:36 +0000

    Allow blocking calls from the test.
    Example failure:
    https://ci.chromium.org/p/chromium/builders/ci/Marshmallow%2064%20bit%20Tester/b8863983830613553808
    
    The test accesses the disk with a blocking call when destroying the
    incognito profile. Allow blocking calls from the test to fix failure
    on M.
    
    Bug: 1147754
    Change-Id: I7f5f00421193a7065626ba2438a77420ff8ffb6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563665
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831843}

--- weblayer/browser/background_sync/background_sync_browsertest.cc
+++ weblayer/browser/background_sync/background_sync_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/run_loop.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "components/background_sync/background_sync_controller_impl.h"
@@ -162,14 +163,10 @@ class IncognitoBackgroundSyncBrowserTest : public BackgroundSyncBrowserTest {
   IncognitoBackgroundSyncBrowserTest() { SetShellStartsInIncognitoMode(); }
 };
 
-#if defined(OS_ANDROID)
-// https://crbug.com/1147754
-#define MAYBE_OffTheRecordProfile DISABLED_OffTheRecordProfile
-#else
-#define MAYBE_OffTheRecordProfile OffTheRecordProfile
-#endif
 IN_PROC_BROWSER_TEST_F(IncognitoBackgroundSyncBrowserTest,
-                       MAYBE_OffTheRecordProfile) {
+                       OffTheRecordProfile) {
+  base::ScopedAllowBlockingForTesting allow_blocking;
+
   // TODO(crbug.com/1087486, 1091211): Make this use
   // BackgroundSyncController::ScheduleBrowserWakeup() once we support waking
   // the browser up.
