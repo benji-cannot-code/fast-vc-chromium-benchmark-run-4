109d989cd76989e78475b89cbfb354ebf6027160
Xinghui Lu <xinghuilu@chromium.org>
2020-11-30 19:28:22 +0000

    Include URL fragment in real time URL requests.
    Bug: 1152553, b/173469874
    Change-Id: I5eb5ff522c67626fa7edfbefb383f9d25923b2d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2558785
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831940}

--- components/safe_browsing/core/realtime/url_lookup_service_base.cc
+++ components/safe_browsing/core/realtime/url_lookup_service_base.cc
@@ -161,7 +161,10 @@ SBThreatType RealTimeUrlLookupServiceBase::GetSBThreatTypeForRTThreatType(
 
 // static
 GURL RealTimeUrlLookupServiceBase::SanitizeURL(const GURL& url) {
-  return net::SimplifyUrlForRequest(url);
+  GURL::Replacements replacements;
+  replacements.ClearUsername();
+  replacements.ClearPassword();
+  return url.ReplaceComponents(replacements);
 }
 
 // static
