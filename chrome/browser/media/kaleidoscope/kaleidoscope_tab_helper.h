5ba8523124d9b4f212fed886b2093172a53f0c78
Becca Hughes <beccahughes@chromium.org>
2020-11-30 20:38:19 +0000

    [Kaleidoscope] Add session result metric
    Add a metric that records whether a user opened
    a recommendation from Kaleidoscope during the
    Kaleidoscope session.
    
    BUG=1110373
    
    Change-Id: I49cc5ac2b8a3dbccb711301068b23c315e725eb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551286
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831975}

--- chrome/browser/media/kaleidoscope/kaleidoscope_tab_helper.h
+++ chrome/browser/media/kaleidoscope/kaleidoscope_tab_helper.h
@@ -14,6 +14,7 @@ class KaleidoscopeTabHelper
       public content::WebContentsUserData<KaleidoscopeTabHelper> {
  public:
   static const char kKaleidoscopeNavigationHistogramName[];
+  static const char kKaleidoscopeOpenedMediaRecommendationHistogramName[];
 
   // These values are persisted to logs. Entries should not be renumbered and
   // numeric values should never be reused.
@@ -28,10 +29,15 @@ class KaleidoscopeTabHelper
 
   // content::WebContentsObserver:
   void ReadyToCommitNavigation(content::NavigationHandle* handle) override;
+  void WebContentsDestroyed() override;
 
   // A tab is Kaleidoscope derived if the tab was opened by Kaleidoscope and
   // remains on the same origin.
   bool IsKaleidoscopeDerived() const { return is_kaleidoscope_derived_; }
+
+  // A tab is successful if it had a Kaleidoscope session in it that resulted
+  // in the user opening another tab.
+  void MarkAsSuccessful() { was_successful_ = true; }
 
  private:
   friend class content::WebContentsUserData<KaleidoscopeTabHelper>;
@@ -40,8 +46,10 @@ class KaleidoscopeTabHelper
 
   void RecordMetricsOnNavigation(content::NavigationHandle* handle);
   void SetAutoplayOnNavigation(content::NavigationHandle* handle);
+  void OnKaleidoscopeSessionEnded();
 
   bool is_kaleidoscope_derived_ = false;
+  bool was_successful_ = false;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };
