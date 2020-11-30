0063dc53417a418a93cb0cf5ce875e29f9056b51
David Roger <droger@chromium.org>
2020-11-30 14:47:58 +0000

    [profiles] Add spinner to signin intercept bubbles
    When the user accepts the bubble, it is no longer closed immediately.
    Instead the buttons are disabled and a spinner is shown.
    
    Screenshot:
    https://drive.google.com/file/d/1yTJ2jac0rq-ly6bEZ0mxhrp5hQ3fd1Ai
    
    Bug: 1076880
    Change-Id: Iff64b798ce7f11fec2c753d0642e989823a349dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563480
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831839}

--- chrome/browser/resources/signin/dice_web_signin_intercept/dice_web_signin_intercept_app.js
+++ chrome/browser/resources/signin/dice_web_signin_intercept/dice_web_signin_intercept_app.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import 'chrome://resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
 import 'chrome://resources/cr_elements/icons.m.js';
 import './signin_shared_css.js';
 import './signin_vars_css.js';
@@ -26,6 +27,12 @@ Polymer({
   properties: {
     /** @private {InterceptionParameters} */
     InterceptionParameters_: Object,
+
+    /** @private {boolean} */
+    acceptButtonClicked_: {
+      type: Boolean,
+      value: false,
+    },
   },
 
   /** @private {?DiceWebSigninInterceptBrowserProxy} */
@@ -44,6 +51,7 @@ Polymer({
 
   /** @private */
   onAccept_() {
+    this.acceptButtonClicked_ = true;
     this.diceWebSigninInterceptBrowserProxy_.accept();
   },
 
--- chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.h
+++ chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/gtest_prod_util.h"
 #include "chrome/browser/signin/dice_web_signin_interceptor.h"
+#include "chrome/browser/ui/browser_list_observer.h"
 
 namespace views {
 class View;
@@ -21,7 +22,8 @@ class Profile;
 // Bubble shown as part of Dice web signin interception. This bubble is
 // implemented as a WebUI page rendered inside a native bubble.
 class DiceWebSigninInterceptionBubbleView
-    : public views::BubbleDialogDelegateView {
+    : public views::BubbleDialogDelegateView,
+      public BrowserListObserver {
  public:
   ~DiceWebSigninInterceptionBubbleView() override;
 
@@ -47,6 +49,25 @@ class DiceWebSigninInterceptionBubbleView
  private:
   FRIEND_TEST_ALL_PREFIXES(DiceWebSigninInterceptionBubbleBrowserTest,
                            BubbleClosed);
+  FRIEND_TEST_ALL_PREFIXES(DiceWebSigninInterceptionBubbleBrowserTest,
+                           BubbleDeclined);
+  FRIEND_TEST_ALL_PREFIXES(DiceWebSigninInterceptionBubbleBrowserTest,
+                           BubbleAccepted);
+
+  // base::ScopedObservation does not work for BrowserList because its
+  // AddObserver/RemoveObserver methods are static.
+  class ScopedBrowserListObserver {
+   public:
+    explicit ScopedBrowserListObserver(BrowserListObserver* owner);
+    ~ScopedBrowserListObserver();
+
+    ScopedBrowserListObserver(const ScopedBrowserListObserver&) = delete;
+    ScopedBrowserListObserver& operator=(const ScopedBrowserListObserver&) =
+        delete;
+
+   private:
+    BrowserListObserver* owner_;
+  };
 
   DiceWebSigninInterceptionBubbleView(
       Profile* profile,
@@ -59,9 +80,16 @@ class DiceWebSigninInterceptionBubbleView
   // method, which is called by the inner web UI.
   void OnWebUIUserChoice(bool accept);
 
+  // BrowserListObserver:
+  void OnBrowserAdded(Browser* browser) override;
+
   Profile* profile_;
   DiceWebSigninInterceptor::Delegate::BubbleParameters bubble_parameters_;
   base::OnceCallback<void(SigninInterceptionResult)> callback_;
+
+  // Once the user accepted, a spinner is shown until the new browser is
+  // created, and then the bubble is closed.
+  std::unique_ptr<ScopedBrowserListObserver> browser_list_observer_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_PROFILES_DICE_WEB_SIGNIN_INTERCEPTION_BUBBLE_VIEW_H_
