37730b6ad98ab6299cac6b214d209cb39eba53d6
evliu <evliu@google.com>
2020-11-30 20:45:40 +0000

    Destroy the speech recognition service if launched before the SODA files have finished downloading
    This CL adds a check when binding the speech recognition recognizer to destroy the speech recognition service if the Speech On-Device API (SODA) binary or model files don't exist on the device. This scenario can occur if a user manually deletes the SODA files from their device and triggers the Live caption feature before the files are redownloaded. The speech recognition service will not be launched in the first place if the SODA components have never been installed before because the local prefs containing the paths to the files will be empty.
    
    Bug: 1152915
    Change-Id: I92d5abccf0ecd6c9fa5f1f80deda650f2344e879
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2561641
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#831983}

--- chrome/services/speech/speech_recognition_service_impl.cc
+++ chrome/services/speech/speech_recognition_service_impl.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/services/speech/speech_recognition_service_impl.h"
 
+#include "base/files/file_util.h"
 #include "chrome/services/speech/speech_recognition_recognizer_impl.h"
+#include "media/base/media_switches.h"
 
 namespace speech {
 
@@ -55,6 +57,15 @@ void SpeechRecognitionServiceImpl::BindRecognizer(
     mojo::PendingReceiver<media::mojom::SpeechRecognitionRecognizer> receiver,
     mojo::PendingRemote<media::mojom::SpeechRecognitionRecognizerClient> client,
     BindRecognizerCallback callback) {
+  // Destroy the speech recognition service if the SODA files haven't been
+  // downloaded yet.
+  if (base::FeatureList::IsEnabled(media::kUseSodaForLiveCaption) &&
+      (!base::PathExists(binary_path_) || !base::PathExists(config_path_))) {
+    speech_recognition_contexts_.Clear();
+    receiver_.reset();
+    return;
+  }
+
   SpeechRecognitionRecognizerImpl::Create(
       std::move(receiver), std::move(client), weak_factory_.GetWeakPtr(),
       binary_path_, config_path_);
