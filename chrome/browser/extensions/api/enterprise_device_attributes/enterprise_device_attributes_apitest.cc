0558d597394665b14029bf5a88474e3b262c00f6
Fabian Sommer <fabiansommer@chromium.org>
2020-11-30 12:35:57 +0000

    Create affiliation mixin
    Refactor common functionality from the tests
    ForceInstalledAffiliatedExtensionApitest, UserAffiliationBrowsertest,
    and UnaffiliatedArcAllowedBrowsertest into a common mixin. It can create
    an user who is affiliated with the device.
    
    Fixed: 1082195
    Change-Id: I4c5bc19750efe2c15378cdb8c1efd48bd4b8db7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2418397
    Commit-Queue: Fabian Sommer <fabiansommer@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831816}

--- chrome/browser/extensions/api/enterprise_device_attributes/enterprise_device_attributes_apitest.cc
+++ chrome/browser/extensions/api/enterprise_device_attributes/enterprise_device_attributes_apitest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chromeos/policy/affiliation_mixin.h"
 #include "chrome/browser/chromeos/policy/affiliation_test_helper.h"
 #include "chrome/browser/chromeos/policy/device_policy_builder.h"
 #include "chrome/browser/extensions/api/force_installed_affiliated_extension_apitest.h"
@@ -91,13 +92,13 @@ class EnterpriseDeviceAttributesTest
 };
 
 IN_PROC_BROWSER_TEST_P(EnterpriseDeviceAttributesTest, PRE_Success) {
-  policy::AffiliationTestHelper::PreLoginUser(affiliated_account_id_);
+  policy::AffiliationTestHelper::PreLoginUser(affiliation_mixin_.account_id());
 }
 
 IN_PROC_BROWSER_TEST_P(EnterpriseDeviceAttributesTest, Success) {
   const bool is_affiliated = GetParam();
   EXPECT_EQ(is_affiliated, user_manager::UserManager::Get()
-                               ->FindUser(affiliated_account_id_)
+                               ->FindUser(affiliation_mixin_.account_id())
                                ->IsAffiliated());
 
   const Extension* extension =
