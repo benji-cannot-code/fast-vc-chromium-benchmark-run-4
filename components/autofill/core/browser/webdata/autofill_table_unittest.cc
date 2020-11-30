3ba8dce79f2841b3c3eb3995baa269629394fc0e
Matthias Körber <koerber@google.com>
2020-11-30 17:19:55 +0000

    [Autofill] Correctly set verification statuses and reset address tokens
    This CL fixes a bug that caused the structure of a street address to
    remain upon a change of the street address in the UI.
    
    It also adds a cleaning method that detects profiles with an
    invalid street address structure and wipes the structure if necessary.
    
    Also test cases are add to verify that a) the structure is cleaned
    correctly when necessary or that structured information is incorporated
    correctly when added to an already verified profile.
    
    Change-Id: I8850e98c424242739e9341466d99dbe41dc55d65
    Bug: 1153395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563764
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/master@{#831879}

--- components/autofill/core/browser/webdata/autofill_table_unittest.cc
+++ components/autofill/core/browser/webdata/autofill_table_unittest.cc
@@ -1045,9 +1045,10 @@ TEST_F(AutofillTableTest, AutofillProfile_StructuredAddresses) {
   AutofillProfile profile;
   profile.set_origin(std::string());
 
-  profile.SetRawInfoWithVerificationStatus(ADDRESS_HOME_STREET_ADDRESS,
-                                           ASCIIToUTF16("Street Address"),
-                                           VerificationStatus::kUserVerified);
+  profile.SetRawInfoWithVerificationStatus(
+      ADDRESS_HOME_STREET_ADDRESS,
+      ASCIIToUTF16("Street Name House Number Premise Subpremise"),
+      VerificationStatus::kUserVerified);
   profile.SetRawInfoWithVerificationStatus(ADDRESS_HOME_STREET_NAME,
                                            ASCIIToUTF16("Street Name"),
                                            VerificationStatus::kFormatted);
@@ -1071,9 +1072,9 @@ TEST_F(AutofillTableTest, AutofillProfile_StructuredAddresses) {
   profile.SetRawInfoWithVerificationStatus(
       ADDRESS_HOME_COUNTRY, ASCIIToUTF16("DE"), VerificationStatus::kObserved);
 
-  profile.SetRawInfoWithVerificationStatus(
-      ADDRESS_HOME_DEPENDENT_STREET_NAME, ASCIIToUTF16("Dependent Street Name"),
-      VerificationStatus::kObserved);
+  profile.SetRawInfoWithVerificationStatus(ADDRESS_HOME_DEPENDENT_STREET_NAME,
+                                           ASCIIToUTF16(""),
+                                           VerificationStatus::kObserved);
 
   profile.SetRawInfoWithVerificationStatus(ADDRESS_HOME_HOUSE_NUMBER,
                                            ASCIIToUTF16("House Number"),
@@ -1132,7 +1133,7 @@ TEST_F(AutofillTableTest, AutofillProfile_StructuredAddresses) {
   EXPECT_EQ(db_profile->GetRawInfo(ADDRESS_HOME_STREET_NAME),
             ASCIIToUTF16("Street Name"));
   EXPECT_EQ(db_profile->GetRawInfo(ADDRESS_HOME_DEPENDENT_STREET_NAME),
-            ASCIIToUTF16("Dependent Street Name"));
+            ASCIIToUTF16(""));
   EXPECT_EQ(db_profile->GetRawInfo(ADDRESS_HOME_HOUSE_NUMBER),
             ASCIIToUTF16("House Number"));
   EXPECT_EQ(db_profile->GetRawInfo(ADDRESS_HOME_SUBPREMISE),
@@ -1163,15 +1164,16 @@ TEST_F(AutofillTableTest,
   AutofillProfile profile;
   profile.set_origin(std::string());
 
-  profile.SetRawInfoWithVerificationStatus(ADDRESS_HOME_STREET_ADDRESS,
-                                           ASCIIToUTF16("Street Address"),
-                                           VerificationStatus::kUserVerified);
+  profile.SetRawInfoWithVerificationStatus(
+      ADDRESS_HOME_STREET_ADDRESS,
+      ASCIIToUTF16("Street Name House Number Premise Subpremise"),
+      VerificationStatus::kUserVerified);
   profile.SetRawInfoWithVerificationStatus(ADDRESS_HOME_STREET_NAME,
                                            ASCIIToUTF16("Street Name"),
                                            VerificationStatus::kFormatted);
-  profile.SetRawInfoWithVerificationStatus(
-      ADDRESS_HOME_DEPENDENT_STREET_NAME, ASCIIToUTF16("Dependent Street Name"),
-      VerificationStatus::kObserved);
+  profile.SetRawInfoWithVerificationStatus(ADDRESS_HOME_DEPENDENT_STREET_NAME,
+                                           ASCIIToUTF16(""),
+                                           VerificationStatus::kObserved);
   profile.SetRawInfoWithVerificationStatus(ADDRESS_HOME_HOUSE_NUMBER,
                                            ASCIIToUTF16("House Number"),
                                            VerificationStatus::kUserVerified);
@@ -1197,7 +1199,7 @@ TEST_F(AutofillTableTest,
   EXPECT_EQ(db_profile->GetRawInfo(ADDRESS_HOME_STREET_NAME),
             ASCIIToUTF16("Street Name"));
   EXPECT_EQ(db_profile->GetRawInfo(ADDRESS_HOME_DEPENDENT_STREET_NAME),
-            ASCIIToUTF16("Dependent Street Name"));
+            ASCIIToUTF16(""));
   EXPECT_EQ(db_profile->GetRawInfo(ADDRESS_HOME_HOUSE_NUMBER),
             ASCIIToUTF16("House Number"));
   EXPECT_EQ(db_profile->GetRawInfo(ADDRESS_HOME_SUBPREMISE),
