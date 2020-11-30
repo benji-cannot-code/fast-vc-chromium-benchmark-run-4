8a84cfbb1de0c0f528e81d1ebcbcca9efa348268
Daniil Lunev <dlunev@chromium.org>
2020-11-30 02:08:34 +0000

    cryptohome: Handle cryptohome corruption error
    In conjunction with [1] makes the behaviour on detecting cryptohome
    corruption more explicit and more chromium driven. Under-the-hood
    deletion extraction from cryptohomed will be addressed in the later CLs.
    
    [1] https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2519646
    
    
    Bug: 1140868
    Change-Id: Id0ab193025322444025e75fc899c09025e43d40e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2518524
    Commit-Queue: Daniil Lunev <dlunev@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831745}

--- chromeos/login/auth/cryptohome_authenticator.cc
+++ chromeos/login/auth/cryptohome_authenticator.cc
@@ -134,6 +134,8 @@ const char* AuthStateToString(CryptohomeAuthenticator::AuthState state) {
       return "OFFLINE_NO_MOUNT";
     case CryptohomeAuthenticator::TPM_UPDATE_REQUIRED:
       return "TPM_UPDATE_REQUIRED";
+    case CryptohomeAuthenticator::OFFLINE_MOUNT_UNRECOVERABLE:
+      return "OFFLINE_MOUNT_UNRECOVERABLE";
   }
   return "UNKNOWN";
 }
@@ -997,6 +999,12 @@ void CryptohomeAuthenticator::Resolve() {
           base::BindOnce(&CryptohomeAuthenticator::OnAuthFailure, this,
                          AuthFailure(AuthFailure::TPM_UPDATE_REQUIRED)));
       break;
+    case OFFLINE_MOUNT_UNRECOVERABLE:
+      task_runner_->PostTask(
+          FROM_HERE,
+          base::BindOnce(&CryptohomeAuthenticator::OnAuthFailure, this,
+                         AuthFailure(AuthFailure::UNRECOVERABLE_CRYPTOHOME)));
+      break;
     default:
       NOTREACHED();
       break;
@@ -1077,6 +1085,13 @@ CryptohomeAuthenticator::ResolveCryptohomeFailureState() {
   if (current_state_->cryptohome_code() ==
       cryptohome::MOUNT_ERROR_TPM_UPDATE_REQUIRED) {
     return TPM_UPDATE_REQUIRED;
+  }
+
+  if (current_state_->cryptohome_code() ==
+      cryptohome::MOUNT_ERROR_VAULT_UNRECOVERABLE) {
+    // Surface up if the mount attempt failed because the vault is
+    // unrecoverable.
+    return OFFLINE_MOUNT_UNRECOVERABLE;
   }
 
   // Return intermediate states in the following case:
