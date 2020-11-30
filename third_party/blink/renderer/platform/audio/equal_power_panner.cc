cae276d6a29239766f5a08f77fdd439708c0b528
Jeremy Roman <jbroman@chromium.org>
2020-11-30 18:17:45 +0000

    audio: Remove unused field Panner::panning_model_.
    Change-Id: I53d62db14db0ff6908726c9fc9e61492f9cb1276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2561751
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831899}

--- third_party/blink/renderer/platform/audio/equal_power_panner.cc
+++ third_party/blink/renderer/platform/audio/equal_power_panner.cc
@@ -34,8 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-EqualPowerPanner::EqualPowerPanner(float sample_rate)
-    : Panner(PanningModel::kEqualPower) {}
+EqualPowerPanner::EqualPowerPanner(float sample_rate) {}
 
 void EqualPowerPanner::Pan(double azimuth,
                            double /*elevation*/,
