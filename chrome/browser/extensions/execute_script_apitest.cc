8bf22f2e8715924fb34d86e3e4616870e0e7abf2
David Bertoni <dbertoni@chromium.org>
2020-11-30 23:03:18 +0000

    [Extensions] Re-enable some execute script tests.
    These test were disabled for SW-based extensions due to flakiness.
    This CL uses a workaround to enable file access for the two tests
    that are flaky when restarted to remove file access.
    
    Bug: 1115182, 1146173
    Change-Id: Id586996467847d42a6ec6eb60a034d0152a16d4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2380137
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832057}

--- chrome/browser/extensions/execute_script_apitest.cc
+++ chrome/browser/extensions/execute_script_apitest.cc
@@ -79,11 +79,10 @@ IN_PROC_BROWSER_TEST_P(ExecuteScriptApiTest, ExecuteScriptBadEncoding) {
 
 // If failing, mark disabled and update http://crbug.com/92105.
 IN_PROC_BROWSER_TEST_P(ExecuteScriptApiTest, ExecuteScriptInFrame) {
-  // TODO(https://crbug.com/1115182): Flaky for SW-based extensions.
-  if (GetParam() == ContextType::kServiceWorker)
-    return;
-
-  ASSERT_TRUE(RunTest("executescript/in_frame")) << message_;
+  // TODO(https://crbug.com/1146173): This test is being run with
+  // file access to prevent flakiness for the SW version. It should
+  // be reverted to run without file access when this bug is fixed.
+  ASSERT_TRUE(RunTestWithFileAccess("executescript/in_frame")) << message_;
 }
 
 IN_PROC_BROWSER_TEST_P(ExecuteScriptApiTest, ExecuteScriptByFrameId) {
@@ -91,11 +90,10 @@ IN_PROC_BROWSER_TEST_P(ExecuteScriptApiTest, ExecuteScriptByFrameId) {
 }
 
 IN_PROC_BROWSER_TEST_P(ExecuteScriptApiTest, ExecuteScriptPermissions) {
-  // TODO(https://crbug.com/1115182): Flaky for SW-based extensions.
-  if (GetParam() == ContextType::kServiceWorker)
-    return;
-
-  ASSERT_TRUE(RunTest("executescript/permissions")) << message_;
+  // TODO(https://crbug.com/1146173): This test is being run with
+  // file access to prevent flakiness for the SW version. It should
+  // be reverted to run without file access when this bug is fixed.
+  ASSERT_TRUE(RunTestWithFileAccess("executescript/permissions")) << message_;
 }
 
 // If failing, mark disabled and update http://crbug.com/84760.
