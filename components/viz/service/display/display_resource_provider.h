74f9b9ce9c3498deb8b7884642df1f7b2262c61b
kylechar <kylechar@chromium.org>
2020-11-30 18:37:30 +0000

    Minor refactor on DisplayResourceProvider
    Small code improvements after spending a bit of time looking at the
    class functionality.
    
    Bug: none
    Change-Id: I60109c1c8a13e59219f728449926899ddcc686c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563377
    Reviewed-by: Sean Gilhuly <sgilhuly@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#831908}

--- components/viz/service/display/display_resource_provider.h
+++ components/viz/service/display/display_resource_provider.h
@@ -325,7 +325,7 @@ class VIZ_SERVICE_EXPORT DisplayResourceProvider
   }
 
   // Creates accounting for a child. Returns a child ID.
-  int CreateChild(const ReturnCallback& return_callback);
+  int CreateChild(ReturnCallback return_callback);
 
   // Destroys accounting for the child, deleting all accounted resources.
   void DestroyChild(int child);
@@ -398,7 +398,8 @@ class VIZ_SERVICE_EXPORT DisplayResourceProvider
 
   struct Child {
     Child();
-    Child(const Child& other);
+    Child(Child&& other);
+    Child& operator=(Child&& other);
     ~Child();
 
     std::unordered_map<ResourceId, ResourceId> child_to_parent_map;
@@ -503,7 +504,6 @@ class VIZ_SERVICE_EXPORT DisplayResourceProvider
   using ChildMap = std::unordered_map<int, Child>;
   using ResourceMap = std::unordered_map<ResourceId, ChildResource>;
 
-  ChildResource* InsertResource(ResourceId id, ChildResource resource);
   ChildResource* GetResource(ResourceId id);
 
   // TODO(ericrk): TryGetResource is part of a temporary workaround for cases
@@ -514,7 +514,7 @@ class VIZ_SERVICE_EXPORT DisplayResourceProvider
   void PopulateSkBitmapWithResource(SkBitmap* sk_bitmap,
                                     const ChildResource* resource);
 
-  void DeleteResourceInternal(ResourceMap::iterator it, DeleteStyle style);
+  void DeleteResourceInternal(ResourceMap::iterator it);
 
   void WaitSyncTokenInternal(ChildResource* resource);
 
@@ -531,7 +531,7 @@ class VIZ_SERVICE_EXPORT DisplayResourceProvider
   GLenum BindForSampling(ResourceId resource_id, GLenum unit, GLenum filter);
   bool ReadLockFenceHasPassed(const ChildResource* resource);
 #if defined(OS_ANDROID)
-  void DeletePromotionHint(ResourceMap::iterator it, DeleteStyle style);
+  void DeletePromotionHint(ResourceMap::iterator it);
 #endif
 
   void DeleteAndReturnUnusedResourcesToChild(
