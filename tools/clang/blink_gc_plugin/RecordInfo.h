90cce318040de886f03e1ebfb05b6954837c2b8b
Reid Kleckner <rnk@google.com>
2020-12-01 00:48:54 +0000

    Fix -Wsuggest-override warnings in clang plugin code
    R=dcheng@chromium.org, mlippautz@chromium.org
    
    Change-Id: I52a8377e4811edb4f6f32dbb1749b3dfd03a4467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2561108
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Reid Kleckner <rnk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832122}

--- tools/clang/blink_gc_plugin/RecordInfo.h
+++ tools/clang/blink_gc_plugin/RecordInfo.h
@@ -41,7 +41,7 @@ class BasePoint : public GraphPoint {
             RecordInfo* info,
             const TracingStatus& status)
       : spec_(spec), info_(info), status_(status) {}
-  const TracingStatus NeedsTracing() { return status_; }
+  const TracingStatus NeedsTracing() override { return status_; }
   const clang::CXXBaseSpecifier& spec() { return spec_; }
   RecordInfo* info() { return info_; }
 
@@ -55,7 +55,7 @@ class FieldPoint : public GraphPoint {
  public:
   FieldPoint(clang::FieldDecl* field, Edge* edge)
       : field_(field), edge_(edge) {}
-  const TracingStatus NeedsTracing() {
+  const TracingStatus NeedsTracing() override {
     return edge_->NeedsTracing(Edge::kRecursive);
   }
   clang::FieldDecl* field() { return field_; }
