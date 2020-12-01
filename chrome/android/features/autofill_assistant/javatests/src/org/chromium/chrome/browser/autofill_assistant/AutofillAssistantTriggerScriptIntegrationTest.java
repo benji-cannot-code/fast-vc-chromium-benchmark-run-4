c2b18da8b96519c35f086256e577d6b7e119b1ef
Clemens Arbesser <arbesser@google.com>
2020-12-01 14:05:58 +0000

    [Autofill Assistant] Fix several integration tests.
    This CL (hopefully) fixes several flakily failing integration tests.
    
    - In one case, the translate popup could block the tap on a website
    element. Furthermore, possibly related to crbug.com/1140957, tapping
    some of the elements did not reliably work. The workaround is to disable
    translate and tap a different element.
    
    - In two other cases, tests were flaky because espresso timed out
    waiting for the UI to be idle. I was not able to reproduce, but since we
    have had similar issues in the past who were related to animations, I
    have removed the progress bar from those tests. We will have to monitor
    the builders to confirm that this fixes the flakes.
    
    Bug: 1149965
    Change-Id: I149b7c930d6e57f496ab4c1f0991c34aac04b578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567393
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Auto-Submit: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#832323}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantTriggerScriptIntegrationTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantTriggerScriptIntegrationTest.java
@@ -35,7 +35,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.autofill_assistant.proto.ActionProto;
 import org.chromium.chrome.browser.autofill_assistant.proto.ChipIcon;
@@ -265,9 +264,11 @@ public class AutofillAssistantTriggerScriptIntegrationTest {
     @Test
     @MediumTest
     @Features.EnableFeatures(ChromeFeatureList.AUTOFILL_ASSISTANT_PROACTIVE_HELP)
+    // Disable translate to prevent the popup from covering part of the website.
+    @Features.DisableFeatures("Translate")
     public void elementCondition() throws Exception {
         SelectorProto.Builder touch_area_four = SelectorProto.newBuilder().addFilters(
-                SelectorProto.Filter.newBuilder().setCssSelector("#touch_area_four"));
+                SelectorProto.Filter.newBuilder().setCssSelector("#touch_area_one"));
         TriggerScriptProto.Builder buttonVisibleTriggerScript =
                 TriggerScriptProto.newBuilder()
                         .setTriggerCondition(TriggerScriptConditionProto.newBuilder().setSelector(
@@ -297,7 +298,7 @@ public class AutofillAssistantTriggerScriptIntegrationTest {
         onView(withId(R.id.progress_bar)).check(matches(not(isDisplayed())));
         onView(withId(R.id.step_progress_bar)).check(matches(isDisplayed()));
 
-        tapElement(mTestRule, "touch_area_four");
+        tapElement(mTestRule, "touch_area_one");
         waitUntilViewMatchesCondition(withText("Area invisible"), isCompletelyDisplayed());
         onView(withId(R.id.progress_bar)).check(matches(not(isDisplayed())));
         onView(withId(R.id.step_progress_bar)).check(matches(not(isDisplayed())));
@@ -358,7 +359,6 @@ public class AutofillAssistantTriggerScriptIntegrationTest {
     @Test
     @MediumTest
     @Features.EnableFeatures(ChromeFeatureList.AUTOFILL_ASSISTANT_PROACTIVE_HELP)
-    @DisabledTest(message = "https://crbug.com/1149965")
     public void transitionToRegularScriptWithoutOnboarding() throws Exception {
         TriggerScriptProto.Builder triggerScript =
                 TriggerScriptProto
@@ -366,7 +366,7 @@ public class AutofillAssistantTriggerScriptIntegrationTest {
                         /* no trigger condition */
                         .setUserInterface(createDefaultUI("Trigger script",
                                 /* bubbleMessage = */ "",
-                                /* withProgressBar = */ true)
+                                /* withProgressBar = */ false)
                                                   .setRegularScriptLoadingStatusMessage(
                                                           "Loading regular script"));
 
@@ -400,8 +400,6 @@ public class AutofillAssistantTriggerScriptIntegrationTest {
         onView(withText("Continue")).perform(click());
         waitUntilViewMatchesCondition(withText("Done"), isCompletelyDisplayed());
         onView(withText("Loading regular script")).check(matches(isDisplayed()));
-        onView(withId(R.id.progress_bar)).check(matches(not(isDisplayed())));
-        onView(withId(R.id.step_progress_bar)).check(matches(isDisplayed()));
     }
 
     @Test
@@ -420,7 +418,7 @@ public class AutofillAssistantTriggerScriptIntegrationTest {
                         /* no trigger condition */
                         .setUserInterface(createDefaultUI("Trigger script",
                                 /* bubbleMessage = */ "",
-                                /* withProgressBar = */ true));
+                                /* withProgressBar = */ false));
         GetTriggerScriptsResponseProto triggerScripts =
                 (GetTriggerScriptsResponseProto) GetTriggerScriptsResponseProto.newBuilder()
                         .addTriggerScripts(triggerScript)
