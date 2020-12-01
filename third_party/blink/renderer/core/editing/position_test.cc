1ce299e9200813c7a6dad4ca610bee582cbd19aa
Yoshifumi Inoue <yosin@chromium.org>
2020-12-01 09:14:51 +0000

    Utilize Position::LastPositionInNode()
    This patch utilizes |Position::LastPositionInNode()| instead of
    constructor with |PositionAnchorType::kAfterChildren| to reduce source
    code size and preparation of getting rid of |kAfterChildren|.
    
    Note: This CL is similar to the CL[2] which utilizes
    |FirstPositionInNode()|.
    
    [2] http://crrev.com/c/2556733: Utilize Position::FirstPositionInNode()
    
    Bug: 592887
    Change-Id: I882f98033a87d491af77484af8dd0b31b54f287e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566314
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832260}

--- third_party/blink/renderer/core/editing/position_test.cc
+++ third_party/blink/renderer/core/editing/position_test.cc
@@ -161,7 +161,7 @@ TEST_F(PositionTest, ToPositionInFlatTreeWithActiveInsertionPoint) {
             ToPositionInFlatTree(Position(anchor, 0)));
   EXPECT_EQ(PositionInFlatTree(anchor, 1),
             ToPositionInFlatTree(Position(anchor, 1)));
-  EXPECT_EQ(PositionInFlatTree(anchor, PositionAnchorType::kAfterChildren),
+  EXPECT_EQ(PositionInFlatTree::LastPositionInNode(*anchor),
             ToPositionInFlatTree(Position(anchor, 2)));
 }
 
@@ -172,7 +172,7 @@ TEST_F(PositionTest, ToPositionInFlatTreeWithInactiveInsertionPoint) {
 
   EXPECT_EQ(PositionInFlatTree(anchor, 0),
             ToPositionInFlatTree(Position(anchor, 0)));
-  EXPECT_EQ(PositionInFlatTree(anchor, PositionAnchorType::kAfterChildren),
+  EXPECT_EQ(PositionInFlatTree::LastPositionInNode(*anchor),
             ToPositionInFlatTree(Position(anchor, 1)));
 }
 
@@ -181,7 +181,7 @@ TEST_F(PositionTest, ToPositionInFlatTreeWithNotDistributed) {
   SetBodyContent("<progress id=sample>foo</progress>");
   Element* sample = GetDocument().getElementById("sample");
 
-  EXPECT_EQ(PositionInFlatTree(sample, PositionAnchorType::kAfterChildren),
+  EXPECT_EQ(PositionInFlatTree::LastPositionInNode(*sample),
             ToPositionInFlatTree(Position(sample, 0)));
 }
 
@@ -194,11 +194,10 @@ TEST_F(PositionTest, ToPositionInFlatTreeWithShadowRoot) {
 
   EXPECT_EQ(PositionInFlatTree(host, 0),
             ToPositionInFlatTree(Position(shadow_root, 0)));
-  EXPECT_EQ(PositionInFlatTree(host, PositionAnchorType::kAfterChildren),
+  EXPECT_EQ(PositionInFlatTree::LastPositionInNode(*host),
             ToPositionInFlatTree(Position(shadow_root, 1)));
-  EXPECT_EQ(PositionInFlatTree(host, PositionAnchorType::kAfterChildren),
-            ToPositionInFlatTree(
-                Position(shadow_root, PositionAnchorType::kAfterChildren)));
+  EXPECT_EQ(PositionInFlatTree::LastPositionInNode(*host),
+            ToPositionInFlatTree(Position::LastPositionInNode(*shadow_root)));
   EXPECT_EQ(PositionInFlatTree::FirstPositionInNode(*host),
             ToPositionInFlatTree(Position::FirstPositionInNode(*shadow_root)));
 }
@@ -213,7 +212,7 @@ TEST_F(PositionTest,
 
   EXPECT_EQ(PositionInFlatTree(host, 0),
             ToPositionInFlatTree(Position(shadow_root, 0)));
-  EXPECT_EQ(PositionInFlatTree(host, PositionAnchorType::kAfterChildren),
+  EXPECT_EQ(PositionInFlatTree::LastPositionInNode(*host),
             ToPositionInFlatTree(Position(shadow_root, 1)));
 }
 
@@ -224,7 +223,7 @@ TEST_F(PositionTest, ToPositionInFlatTreeWithEmptyShadowRoot) {
   ShadowRoot* shadow_root = SetShadowContent(shadow_content, "host");
   Element* host = GetDocument().getElementById("host");
 
-  EXPECT_EQ(PositionInFlatTree(host, PositionAnchorType::kAfterChildren),
+  EXPECT_EQ(PositionInFlatTree::LastPositionInNode(*host),
             ToPositionInFlatTree(Position(shadow_root, 0)));
 }
 
