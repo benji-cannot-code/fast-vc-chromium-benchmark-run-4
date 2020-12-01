39a996c693958bb88dffec1b61764207f920150f
Illia Klimov <elklm@google.com>
2020-12-01 19:24:06 +0000

    Remove Flash from PermissionRequestTypes and PermissionTypes.
    This CL removes Flash/Plugins from PermissionRequestTypes and
    PermissionTypes enums and its call sites.
    
    Bug: 1149393
    Change-Id: Ib7675b502b59704cf04d9a900fbbf469fa9c9de3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563553
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832461}

--- android_webview/browser/aw_permission_manager.cc
+++ android_webview/browser/aw_permission_manager.cc
@@ -328,7 +328,6 @@ int AwPermissionManager::RequestPermissions(
       case PermissionType::NOTIFICATIONS:
       case PermissionType::DURABLE_STORAGE:
       case PermissionType::BACKGROUND_SYNC:
-      case PermissionType::FLASH:
       case PermissionType::ACCESSIBILITY_EVENTS:
       case PermissionType::CLIPBOARD_READ_WRITE:
       case PermissionType::CLIPBOARD_SANITIZED_WRITE:
@@ -536,7 +535,6 @@ void AwPermissionManager::CancelPermissionRequest(int request_id) {
       case PermissionType::AUDIO_CAPTURE:
       case PermissionType::VIDEO_CAPTURE:
       case PermissionType::BACKGROUND_SYNC:
-      case PermissionType::FLASH:
       case PermissionType::ACCESSIBILITY_EVENTS:
       case PermissionType::CLIPBOARD_READ_WRITE:
       case PermissionType::CLIPBOARD_SANITIZED_WRITE:
