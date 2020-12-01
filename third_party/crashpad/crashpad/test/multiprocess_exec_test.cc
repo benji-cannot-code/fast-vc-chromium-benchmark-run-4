fb131ed2c90894c5fcb47f4b7f61a88c1fb936c7
Takashi Sakamoto <tasak@google.com>
2020-12-01 09:18:06 +0000

    Disable MultiprocessExec.MultiprocessExec on posix when PartitionAlloc is default allocator.
    PartitionAllocator opens /dev/urandom for address randomization when allocating memory.(c.f. //base/allocator/partition_allocator/random.cc and //base/rand_util_posix.cc)
    This causes MultiprocessExec.MultiprocessExec test failure. So temporarily disable the test on posix when PartitionAllocator is default.
    
    Bug: 1153544
    
    Change-Id: I19ea568745caf01c6c7fe9d2ad3f845891b2a9ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2562811
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#832264}

--- third_party/crashpad/crashpad/test/multiprocess_exec_test.cc
+++ third_party/crashpad/crashpad/test/multiprocess_exec_test.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "test/multiprocess_exec.h"
 
+#include "base/allocator/buildflags.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
@@ -49,7 +50,19 @@ class TestMultiprocessExec final : public MultiprocessExec {
   DISALLOW_COPY_AND_ASSIGN(TestMultiprocessExec);
 };
 
-TEST(MultiprocessExec, MultiprocessExec) {
+// TODO(tasak): enable this test after making address randomization not to
+// keep /dev/urandom open.
+// PartitionAllocator opens /dev/urandom because of address randomization.
+// (c.f. //base/rand_util_posix.cc and
+// //base/allocator/partition_allocator/random.cc) So when making
+// PartitionAllocator default, multiprocess_exec_test_child will crash because
+// of LOG(FATAL) << "close". https://crbug.com/1153544
+#if defined(OS_POSIX) && BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+#define MAYBE_MultiprocessExec DISABLED_MultiprocessExec
+#else
+#define MAYBE_MultiprocessExec MultiprocessExec
+#endif
+TEST(MultiprocessExec, MAYBE_MultiprocessExec) {
   TestMultiprocessExec multiprocess_exec;
   base::FilePath child_test_executable = TestPaths::BuildArtifact(
       FILE_PATH_LITERAL("test"),
