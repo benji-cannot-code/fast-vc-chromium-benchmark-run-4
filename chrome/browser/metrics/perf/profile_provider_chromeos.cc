1e2ac9f7049ff7cc3e5674c99dcefae6d80b25a2
Bin Du <dubin@google.com>
2020-12-01 19:16:55 +0000

    Check if App Sync is enabled during perf collection.
    We read App Sync settings from all initialized user profiles on device.
    
    If disabled, possible Android app names in perf data should be removed.
    
    ChromeOS split sync feature splits the sync setting to different places.
    
    If split sync feature is disabled, we read from Chrome sync settings.
    
    If it is enabled, we read from ChromeOS sync settings.
    
    Metrics show majority of users on stable channel enable App Sync.
    
    Add a UMA histogram to track the status of recording profile data.
    
    Bug=b:171796981
    TEST=tested sync settings on a Chromebook, MetricProvider unit test.
    
    Change-Id: Iad00f2755e3b55c0e1e380efdf9283028b0a145c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2499369
    Commit-Queue: Bin Du <dubin@google.com>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Gabriel Marin <gmx@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832457}

--- chrome/browser/metrics/perf/profile_provider_chromeos.cc
+++ chrome/browser/metrics/perf/profile_provider_chromeos.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_macros.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/metrics/perf/metric_provider.h"
 #include "chrome/browser/metrics/perf/perf_events_collector.h"
 #include "chrome/browser/metrics/perf/windowed_incognito_observer.h"
@@ -59,8 +60,8 @@ ProfileProvider::ProfileProvider()
   // Initialize the WindowedIncognitoMonitor on the UI thread.
   WindowedIncognitoMonitor::Init();
   // Register a perf events collector.
-  collectors_.push_back(
-      std::make_unique<MetricProvider>(std::make_unique<PerfCollector>()));
+  collectors_.push_back(std::make_unique<MetricProvider>(
+      std::make_unique<PerfCollector>(), g_browser_process->profile_manager()));
 }
 
 ProfileProvider::~ProfileProvider() {
