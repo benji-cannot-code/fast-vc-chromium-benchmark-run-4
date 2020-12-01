496e2ed850a33e79b73f92dae208bad5318f8726
Colin Blundell <blundell@chromium.org>
2020-12-01 09:01:26 +0000

    [WebLayer] Change version numbers for safe browsing load error
    This CL was cherrypicked to 88 with the version numbers adjusted there:
    https://chromium-review.googlesource.com/c/chromium/src/+/2561381/1..5
    
    Hence we can now adjust the version numbers on trunk.
    
    Bug: 1149955
    Change-Id: I554ae299b43eecfb66870c386e9b1ccc0d07bcb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2562842
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832256}

--- weblayer/public/java/org/chromium/weblayer/LoadError.java
+++ weblayer/public/java/org/chromium/weblayer/LoadError.java
@@ -49,7 +49,7 @@ public @interface LoadError {
     /**
      * Safe browsing error.
      *
-     * @since 89
+     * @since 88
      */
     int SAFE_BROWSING_ERROR =
             org.chromium.weblayer_private.interfaces.LoadError.SAFE_BROWSING_ERROR;
