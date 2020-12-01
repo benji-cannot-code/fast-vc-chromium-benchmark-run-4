3b508eefb81921197187f260868110990f588f4e
Daniel Cheng <dcheng@chromium.org>
2020-12-01 23:11:42 +0000

    [WebUI] Use WebUIController::RenderFrameCreated() to request MojoJS.
    ConversionInternalsUI and ProcessInternalsUI don't need to be
    WebContentsObservers at all, since WebUIController already provides a
    RenderFrameCreated() override that is scoped WebUI-specific
    RenderFrameHosts.
    
    Bug: 1148682, 1149125
    Change-Id: Iebef544d7fb7c8938d273c84279ce7e8b19f90a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566889
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832563}

--- content/browser/process_internals/process_internals_ui.cc
+++ content/browser/process_internals/process_internals_ui.cc
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 ProcessInternalsUI::ProcessInternalsUI(WebUI* web_ui)
-    : WebUIController(web_ui), WebContentsObserver(web_ui->GetWebContents()) {
+    : WebUIController(web_ui) {
   // This WebUI does not require any process bindings, so disable it early in
   // initialization time.
   web_ui->SetBindings(0);
@@ -45,7 +45,7 @@ ProcessInternalsUI::ProcessInternalsUI(WebUI* web_ui)
       network::mojom::CSPDirectiveName::TrustedTypes,
       "trusted-types cr-ui-tree-js-static;");
 
-  WebUIDataSource::Add(web_contents()->GetBrowserContext(), source);
+  WebUIDataSource::Add(web_ui->GetWebContents()->GetBrowserContext(), source);
 }
 
 WEB_UI_CONTROLLER_TYPE_IMPL(ProcessInternalsUI)
