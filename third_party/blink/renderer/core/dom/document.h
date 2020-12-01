75393ec97fce6c845a20ea8e6e07c37c569844bc
Sebastien Lalancette <seblalancette@chromium.org>
2020-12-01 17:09:07 +0000

    [SH-Blink] Add Link Opening and Generating UKM Events
    Reused UKM logging functions from the common shared_highlighting
    component to log UKM events when a link with text fragments has been
    opened and its fragments have been processed (LinkOpened event) and
    also when a link-to-text has been generated for the user (LinkGenerated
    event).
    
    Validated that the events show up on chrome://ukm.
    
    Bug: 1148807,1153990
    Change-Id: Ibcab06305a31ee6c4d280a65a64f0b21af093209
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2553002
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832386}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1695,6 +1695,7 @@ class CORE_EXPORT Document : public ContainerNode,
  private:
   friend class DocumentTest;
   friend class IgnoreDestructiveWriteCountIncrementer;
+  friend class TextFragmentSelectorGeneratorTest;
   friend class ThrowOnDynamicMarkupInsertionCountIncrementer;
   friend class IgnoreOpensDuringUnloadCountIncrementer;
   friend class NthIndexCache;
@@ -1706,6 +1707,8 @@ class CORE_EXPORT Document : public ContainerNode,
                            BeforeMatchExpandedHiddenMatchableUkm);
   FRIEND_TEST_ALL_PREFIXES(TextFinderSimTest,
                            BeforeMatchExpandedHiddenMatchableUkmNoHandler);
+  FRIEND_TEST_ALL_PREFIXES(TextFragmentAnchorMetricsTest, LinkOpenedSuccessUKM);
+  FRIEND_TEST_ALL_PREFIXES(TextFragmentAnchorMetricsTest, LinkOpenedFailedUKM);
   class NetworkStateObserver;
 
   friend class AXContext;
