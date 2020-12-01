d2d806c011109ec3511bc06df5f0cb40e554d339
Johann <johannkoenig@google.com>
2020-12-01 18:29:19 +0000

    ReportingService: Clean up extra include and typo
    Change-Id: Ia9d686316e4871da7c5e9656206d3cfa05064904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567072
    Auto-Submit: Johann Koenig <johannkoenig@google.com>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832431}

--- third_party/blink/renderer/core/frame/reporting_context.cc
+++ third_party/blink/renderer/core/frame/reporting_context.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/reporting_context.h"
 
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
-#include "third_party/blink/public/common/thread_safe_browser_interface_broker_proxy.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/core/dom/document.h"
