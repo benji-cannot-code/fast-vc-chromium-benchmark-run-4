a5e1251a80b5b4f39b1572b1126f3de7aacbc1da
Fergal Daly <fergal@chromium.org>
2020-12-01 02:30:48 +0000

    Add DepictFrameTree convenience function.
    This is a prefactor for https://crrev.com/c/2536214.
    
    This just wraps FrameTreeVisualizer().DepictFrameTree(...). This makes a
    few tests a simpler but also insulates them from changes to the
    implementation.
    
    Change-Id: I8509ea44577aa41280c18225f4e091f90f422bef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560700
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832166}

--- content/browser/renderer_host/input/touch_selection_controller_client_aura_browsertest.cc
+++ content/browser/renderer_host/input/touch_selection_controller_client_aura_browsertest.cc
@@ -385,7 +385,7 @@ IN_PROC_BROWSER_TEST_P(TouchSelectionControllerClientAuraSiteIsolationTest,
       " Site A\n"
       "   +--Site A\n"
       "Where A = http://a.com/",
-      FrameTreeVisualizer().DepictFrameTree(root));
+      DepictFrameTree(*root));
   TestNavigationObserver observer(shell()->web_contents());
   EXPECT_EQ(1u, root->child_count());
   FrameTreeNode* child = root->child_at(0);
@@ -413,7 +413,7 @@ IN_PROC_BROWSER_TEST_P(TouchSelectionControllerClientAuraSiteIsolationTest,
       "   +--Site B ------- proxies for A\n"
       "Where A = http://a.com/\n"
       "      B = http://b.com/",
-      FrameTreeVisualizer().DepictFrameTree(root));
+      DepictFrameTree(*root));
 
   // The child will change with the cross-site navigation. It shouldn't change
   // after this.
@@ -491,7 +491,7 @@ IN_PROC_BROWSER_TEST_P(TouchSelectionControllerClientAuraSiteIsolationTest,
       " Site A\n"
       "   +--Site A\n"
       "Where A = http://a.com/",
-      FrameTreeVisualizer().DepictFrameTree(root));
+      DepictFrameTree(*root));
   TestNavigationObserver observer(shell()->web_contents());
   EXPECT_EQ(1u, root->child_count());
   FrameTreeNode* child = root->child_at(0);
@@ -525,7 +525,7 @@ IN_PROC_BROWSER_TEST_P(TouchSelectionControllerClientAuraSiteIsolationTest,
       "   +--Site B ------- proxies for A\n"
       "Where A = http://a.com/\n"
       "      B = http://b.com/",
-      FrameTreeVisualizer().DepictFrameTree(root));
+      DepictFrameTree(*root));
 
   // The child will change with the cross-site navigation. It shouldn't change
   // after this.
