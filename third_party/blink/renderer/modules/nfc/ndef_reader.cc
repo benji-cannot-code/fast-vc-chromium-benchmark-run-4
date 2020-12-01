23370705467f78744d7008a13ec6dc6af6ebad01
Francois Beaufort <beaufort.francois@gmail.com>
2020-12-01 11:31:20 +0000

    [WebNFC] Remove origin trial
    This CL removes all code related to the ended origin trial for Web NFC.
    See "Changes done after Origin Trial (OT)" section at
    https://github.com/w3c/web-nfc/blob/gh-pages/EXPLAINER.md
    
    Bug: 520391, 1131141
    Change-Id: Id34926a0db53462271d88572204cb50a3b4a1bee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2554598
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Cr-Commit-Position: refs/heads/master@{#832293}

--- third_party/blink/renderer/modules/nfc/ndef_reader.cc
+++ third_party/blink/renderer/modules/nfc/ndef_reader.cc
@@ -151,9 +151,6 @@ void NDEFReader::ReadOnRequestPermission(const NDEFScanOptions* options,
     return;
   }
 
-  // TODO(https://crbug.com/994936) remove when origin trial is complete.
-  UseCounter::Count(GetExecutionContext(), WebFeature::kWebNfcAPI);
-
   GetNfcProxy()->StartReading(
       this,
       WTF::Bind(&NDEFReader::ReadOnRequestCompleted, WrapPersistent(this)));
@@ -279,9 +276,6 @@ void NDEFReader::WriteOnRequestPermission(
                                               WrapPersistent(this),
                                               WrapPersistent(resolver)));
   }
-
-  // TODO(https://crbug.com/994936) remove when origin trial is complete.
-  UseCounter::Count(GetExecutionContext(), WebFeature::kWebNfcAPI);
 
   auto callback = WTF::Bind(&NDEFReader::WriteOnRequestCompleted,
                             WrapPersistent(this), WrapPersistent(resolver));
