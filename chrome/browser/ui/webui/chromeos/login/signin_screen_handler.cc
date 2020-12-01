ab186faa3e01f4902ed0525ebe311c5c9b0063d8
Roman Sorokin <rsorokin@chromium.org>
2020-12-01 15:49:16 +0000

    OOBE: Nuke ResetSigninScreenHandlerDelegate
    Small cleanup
    
    Bug: none
    Change-Id: Ifce493d3c876fa1d067900fe8cba00c77582fb26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563462
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832355}

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -863,9 +863,6 @@ void SigninScreenHandler::OnPreferencesChanged() {
   }
 }
 
-void SigninScreenHandler::ResetSigninScreenHandlerDelegate() {
-  SetDelegate(nullptr);
-}
 
 void SigninScreenHandler::ShowError(int login_attempts,
                                     const std::string& error_text,
