2ae211a4acadd38ccd3db0723b465754140c9948
Gavin Williams <gavinwill@chromium.org>
2020-12-01 22:49:36 +0000

    scanning: Show error toast when saved scan file not found
    Show error toast when the user clicks "Show file location" to open the
    Files app but the saved scan file no longer exists at its original saved
    location.
    
    http://screen/Bufy35SXPNVM7Tp
    
    Bug: 1059779
    Change-Id: Id8a08b37f1f957c26ad013f8463b3e8be4c2d338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566779
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832555}

--- chromeos/components/scanning/resources/scanning_app.js
+++ chromeos/components/scanning/resources/scanning_app.js
@@ -537,6 +537,13 @@ Polymer({
   /** @private */
   onToastMessageKeyChange_() {
     this.showToastInfoIcon_ = this.toastMessageKey_ !== 'scanCanceledToastText';
-    this.showToastHelpLink_ = this.toastMessageKey_ !== 'scanCanceledToastText';
+    this.showToastHelpLink_ =
+        this.toastMessageKey_ !== 'scanCanceledToastText' &&
+        this.toastMessageKey_ !== 'fileNotFoundToastText';
+  },
+
+  /** @private */
+  onFileNotFound_() {
+    this.showToast_('fileNotFoundToastText');
   },
 });
