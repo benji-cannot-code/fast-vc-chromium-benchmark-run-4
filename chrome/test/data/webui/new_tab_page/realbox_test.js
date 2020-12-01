84924f5cf9226ee7cddc094718a1b501161ccd74
Moe Ahmadi <mahmadi@chromium.org>
2020-12-01 04:35:40 +0000

    [realbox] Always update realbox input when a default match is present
    Previously, the text selection in the realbox input was being set to
    the inline autocompletion of the default match (if one existed)
    assuming the current input contains the previously typed text, thus
    generating the expected input.
    
    This logic can result in undesirable outcomes where the current input
    does not match the previously typed text. As of this CL no such
    assumption is made and the input is updated to the previously typed
    input + inline autocompletion of the default match (if applicable).
    
    Fixed: 1152269
    Change-Id: I518b2095736c8ef328c820bce2e9682f1b469aad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566296
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832206}

--- chrome/test/data/webui/new_tab_page/realbox_test.js
+++ chrome/test/data/webui/new_tab_page/realbox_test.js
@@ -533,7 +533,12 @@ suite('NewTabPageRealboxTest', () => {
     });
     assertEquals(1, testProxy.handler.getCallCount('queryAutocomplete'));
 
-    const matches = [createSearchMatch(), createUrlMatch()];
+    const matches = [
+      createSearchMatch({
+        allowedToBeDefaultMatch: true,
+      }),
+      createUrlMatch()
+    ];
     testProxy.callbackRouterRemote.autocompleteResultChanged({
       input: mojoString16(realbox.$.input.value.trimLeft()),
       matches,
@@ -548,6 +553,14 @@ suite('NewTabPageRealboxTest', () => {
     assertEquals(2, matchEls.length);
     verifyMatch(matches[0], matchEls[0]);
     verifyMatch(matches[1], matchEls[1]);
+
+    // First match is selected.
+    assertTrue(matchEls[0].classList.contains(CLASSES.SELECTED));
+
+    assertEquals('      hello world', realbox.$.input.value);
+    let start = realbox.$.input.selectionStart;
+    let end = realbox.$.input.selectionEnd;
+    assertEquals('', realbox.$.input.value.substring(start, end));
   });
 
   test('autocomplete response with inline autocompletion', async () => {
@@ -578,6 +591,9 @@ suite('NewTabPageRealboxTest', () => {
         realbox.$.matches.shadowRoot.querySelectorAll('ntp-realbox-match');
     assertEquals(1, matchEls.length);
     verifyMatch(matches[0], matchEls[0]);
+
+    // First match is selected.
+    assertTrue(matchEls[0].classList.contains(CLASSES.SELECTED));
 
     assertEquals('hello world', realbox.$.input.value);
     let start = realbox.$.input.selectionStart;
