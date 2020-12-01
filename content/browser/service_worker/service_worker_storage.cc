b78f429e4709d1a5662656f3cc8a17cedb8ac969
Kenichi Ishibashi <bashi@chromium.org>
2020-12-01 00:34:44 +0000

    Support retry for ServiceWorkerRegistry::ApplyPolicyUpdates()
    ServiceWorkerStorageControl::ApplyPolicyUpdates() is changed to
    return a response so that ServiceWorkerRegistry can check whether the
    call is finished or not.
    
    ServiceWorkerStorage::ApplyPolicyUpdates(), the implementation, is also
    updated to initialize the internal storage if it's not initialized yet.
    It is because ApplyPolicyUpdates() updates a list to specify which
    resources should be purged on shutdown, and the internal storage needs
    to be initialized before purging starts in the destructor of
    ServiceWorkerStorage.
    
    Bug: 1133143
    Change-Id: Ibf9badedac78d946baed200dd84f826d9b0d26de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563187
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832114}

--- content/browser/service_worker/service_worker_storage.cc
+++ content/browser/service_worker/service_worker_storage.cc
@@ -1190,7 +1190,29 @@ void ServiceWorkerStorage::PurgeResources(
 
 void ServiceWorkerStorage::ApplyPolicyUpdates(
     const std::vector<storage::mojom::LocalStoragePolicyUpdatePtr>&
-        policy_updates) {
+        policy_updates,
+    DatabaseStatusCallback callback) {
+  switch (state_) {
+    case STORAGE_STATE_DISABLED:
+      std::move(callback).Run(ServiceWorkerDatabase::Status::kErrorDisabled);
+      return;
+    case STORAGE_STATE_INITIALIZING:
+    case STORAGE_STATE_UNINITIALIZED: {
+      // An explicit clone is needed to pass `policy_updates` to LazyInitialize.
+      std::vector<storage::mojom::LocalStoragePolicyUpdatePtr>
+          cloned_policy_updates;
+      for (const auto& entry : policy_updates)
+        cloned_policy_updates.push_back(entry.Clone());
+
+      LazyInitialize(base::BindOnce(
+          &ServiceWorkerStorage::ApplyPolicyUpdates, weak_factory_.GetWeakPtr(),
+          std::move(cloned_policy_updates), std::move(callback)));
+      return;
+    }
+    case STORAGE_STATE_INITIALIZED:
+      break;
+  }
+
   for (const auto& update : policy_updates) {
     GURL url = update->origin.GetURL();
     if (!update->purge_on_shutdown)
@@ -1198,6 +1220,8 @@ void ServiceWorkerStorage::ApplyPolicyUpdates(
     else
       origins_to_purge_on_shutdown_.insert(std::move(url));
   }
+
+  std::move(callback).Run(ServiceWorkerDatabase::Status::kOk);
 }
 
 ServiceWorkerStorage::ServiceWorkerStorage(
