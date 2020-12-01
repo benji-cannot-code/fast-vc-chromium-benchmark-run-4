92e28a10e650817f1512a2ca34a14a0cb4f53352
Thiabaud Engelbrecht <thiabaud@google.com>
2020-12-01 18:24:39 +0000

    [discardable] Fix potential use-after-free in ClientDiscardableSharedMemoryManager.
    Use scoped_refptr in ClientDiscardableSharedMemoryManager, to prevent
    it from being destroyed before all the memory it allocated is destroyed.
    
    Bug: 1153322
    Change-Id: I01675d40bb081480460d5878a18baa58d5fe8507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563898
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/master@{#832428}

--- content/renderer/discardable_memory_utils.cc
+++ content/renderer/discardable_memory_utils.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-std::unique_ptr<discardable_memory::ClientDiscardableSharedMemoryManager>
+scoped_refptr<discardable_memory::ClientDiscardableSharedMemoryManager>
 CreateDiscardableMemoryAllocator() {
   DVLOG(1) << "Using shared memory for discardable memory";
 
@@ -34,7 +34,7 @@ CreateDiscardableMemoryAllocator() {
       manager_remote;
   ChildThread::Get()->BindHostReceiver(
       manager_remote.InitWithNewPipeAndPassReceiver());
-  return std::make_unique<
+  return base::MakeRefCounted<
       discardable_memory::ClientDiscardableSharedMemoryManager>(
       std::move(manager_remote), ChildProcess::current()->io_task_runner(),
       ChildProcess::current()->main_thread()->main_thread_runner());
