85a04c0daf3191dbcb08f1de171b8ef7955885eb
Vladimir Levin <vmpstr@chromium.org>
2020-12-01 20:44:41 +0000

    [content-visibility]: Ensure to ignore hidden-matchable children.
    This patch ensures that we property check that hidden-matchable
    children should be skipped. This only affects hidden-matchable, because
    hidden hides everything to the buggy if statement would work and
    auto allows all activations so the result of the buggy return is
    the same as it would have been without the bug.
    
    The effect of this bug is that hidden-matchable children are exposed
    to accessibility, which they shouldn't be.
    
    R=chrishtr@chromium.org, jarhar@chromium.org
    
    Change-Id: I863889889cb28e4d3ab075f599f7660dc8637cff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567342
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832495}

--- third_party/blink/renderer/core/display_lock/display_lock_utilities.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_utilities.cc
@@ -399,9 +399,6 @@ bool DisplayLockUtilities::IsInUnlockedOrActivatableSubtree(
       node.GetDocument()
               .GetDisplayLockDocumentState()
               .LockedDisplayLockCount() == 0 ||
-      node.GetDocument()
-              .GetDisplayLockDocumentState()
-              .DisplayLockBlockingAllActivationCount() == 0 ||
       !node.CanParticipateInFlatTree()) {
     return true;
   }
