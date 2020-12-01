0a3a7b3a39bd85c5c988bd0a7e8c9f448a87020a
Elly Fong-Jones <ellyjones@chromium.org>
2020-12-01 01:52:43 +0000

    flags: add accessible label for search field
    Currently the field is only labelled by its placeholder text, which
    is not read by all screenreaders. This change gives it an explicit
    accessible label instead.
    
    Bug: 1083307
    Change-Id: I7b79de41576c9a9c4caf33ef54a5009329d3d0f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538531
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832153}

--- chrome/browser/ui/webui/flags_ui.cc
+++ chrome/browser/ui/webui/flags_ui.cc
@@ -164,6 +164,7 @@ void FlagsUI::AddStrings(content::WebUIDataSource* source) {
   source->AddLocalizedString("reset", IDS_FLAGS_UI_PAGE_RESET);
   source->AddLocalizedString("reset-acknowledged",
                              IDS_FLAGS_UI_RESET_ACKNOWLEDGED);
+  source->AddLocalizedString("search-label", IDS_FLAGS_UI_SEARCH_LABEL);
   source->AddLocalizedString("search-placeholder",
                              IDS_FLAGS_UI_SEARCH_PLACEHOLDER);
   source->AddLocalizedString("title", IDS_FLAGS_UI_TITLE);
@@ -198,6 +199,7 @@ void FlagsDeprecatedUI::AddStrings(content::WebUIDataSource* source) {
   source->AddLocalizedString("reset", IDS_DEPRECATED_FEATURES_PAGE_RESET);
   source->AddLocalizedString("reset-acknowledged",
                              IDS_DEPRECATED_UI_RESET_ACKNOWLEDGED);
+  source->AddLocalizedString("search-label", IDS_FLAGS_UI_SEARCH_LABEL);
   source->AddLocalizedString("search-placeholder",
                              IDS_DEPRECATED_FEATURES_SEARCH_PLACEHOLDER);
   source->AddLocalizedString("title", IDS_DEPRECATED_FEATURES_TITLE);
