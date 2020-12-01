5902c32cab7c2a76254b1da317b032bba21d45d5
Alexander Surkov <asurkov@igalia.com>
2020-12-01 14:46:23 +0000

    a11y inspect reorg: get rid of AccessibilityTreeFormatterBase::DumpAccessibilityTreeFromManager
    DumpAccessibilityTreeFromManager is small and has only one consumer: web_contents, which can use tree formatters directly.
    
    Bug: 1133330
    Change-Id: I76aba176ec5a240fcfe1b1fe00742ac52880dafe
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532669
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832341}

--- content/browser/accessibility/accessibility_tree_formatter_base.h
+++ content/browser/accessibility/accessibility_tree_formatter_base.h
@@ -42,11 +42,6 @@ class CONTENT_EXPORT AccessibilityTreeFormatterBase
   AccessibilityTreeFormatterBase();
   ~AccessibilityTreeFormatterBase() override;
 
-  static std::string DumpAccessibilityTreeFromManager(
-      BrowserAccessibilityManager* ax_mgr,
-      bool internal,
-      std::vector<AXPropertyFilter> property_filters);
-
   std::string Format(ui::AXPlatformNodeDelegate* root) const override;
 
   // Populates the given DictionaryValue with the accessibility tree.
