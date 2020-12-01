ff07bd92be70fd61504436d735e0e8d0e531f8f0
Nancy Wang <nancylingwang@chromium.org>
2020-12-01 07:31:12 +0000

    Add the restore notification for 'Ask every time' option.
    This CL implements the restore notification on startup if the
    'restore_apps_and_pages' setting is 'Ask every time', and the system is
    not crashed before reboot, based on the ui review deck
    go/cros-full-restore page 22.
    
    There will be some follow up CLs to set the default value for the
    'restore_apps_and_pages' settings, and implement the restoration, test
    cases, etc.
    
    BUG=1146900
    
    Change-Id: I71eb5dd1b7d453da4e2f1dc0a7059d7b7ff39104
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2558117
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832243}

--- chrome/browser/chromeos/full_restore/full_restore_service.cc
+++ chrome/browser/chromeos/full_restore/full_restore_service.cc
@@ -8,12 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/notification_utils.h"
 #include "base/strings/string_util.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/chromeos/full_restore/full_restore_prefs.h"
 #include "chrome/browser/chromeos/full_restore/full_restore_service_factory.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/notifications/notification_display_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/prefs/pref_service.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/message_center/public/cpp/notification.h"
 
@@ -21,6 +23,13 @@ namespace {
 
 constexpr char kRestoreForCrashNotificationId[] =
     "restore_for_crash_notification";
+constexpr char kRestoreNotificationId[] = "restore_notification";
+
+// The restore notification button index.
+enum class RestoreNotificationButtonIndex {
+  kRestore = 0,
+  kCancel,
+};
 
 }  // namespace
 
@@ -34,9 +43,24 @@ FullRestoreService::FullRestoreService(Profile* profile) : profile_(profile) {
     return;
   }
 
-  // TODO(crbug.com/909794):On startup for normal reboot, read
-  // |kRestoreAppsAndPagesPrefName| from the user pref, show the restore
-  // notification for the 'ask every time' option.
+  PrefService* prefs = profile->GetPrefs();
+  DCHECK(prefs);
+
+  // TODO(crbug.com/909794):Set the default value if
+  // |kRestoreAppsAndPagesPrefName| doesn't exist from the user pref.
+
+  RestoreOption restore_pref = static_cast<RestoreOption>(
+      prefs->GetInteger(kRestoreAppsAndPagesPrefName));
+  switch (restore_pref) {
+    case RestoreOption::kAlways:
+      // TODO(crbug.com/909794): Implement apps and pages restoration.
+      break;
+    case RestoreOption::kAskEveryTime:
+      ShowRestoreNotification(kRestoreNotificationId);
+      break;
+    case RestoreOption::kDoNotRestore:
+      return;
+  }
 }
 
 FullRestoreService::~FullRestoreService() = default;
@@ -54,11 +78,15 @@ void FullRestoreService::ShowRestoreNotification(const std::string& id) {
       base::ToUpperASCII(IDS_RESTORE_NOTIFICATION_CANCEL_BUTTON)));
   notification_data.buttons.push_back(cancel_button);
 
+  int message_id = id == kRestoreForCrashNotificationId
+                       ? IDS_RESTORE_FOR_CRASH_NOTIFICATION_MESSAGE
+                       : IDS_RESTORE_NOTIFICATION_MESSAGE;
+
   std::unique_ptr<message_center::Notification> notification =
       ash::CreateSystemNotification(
           message_center::NOTIFICATION_TYPE_SIMPLE, id,
           l10n_util::GetStringUTF16(IDS_RESTORE_NOTIFICATION_TITLE),
-          l10n_util::GetStringUTF16(IDS_RESTORE_FOR_CRASH_NOTIFICATION_MESSAGE),
+          l10n_util::GetStringUTF16(message_id),
           l10n_util::GetStringUTF16(IDS_RESTORE_NOTIFICATION_DISPLAY_SOURCE),
           GURL(),
           message_center::NotifierId(
@@ -85,12 +113,18 @@ void FullRestoreService::ShowRestoreNotification(const std::string& id) {
 void FullRestoreService::HandleRestoreNotificationClicked(
     const std::string& id,
     base::Optional<int> button_index) {
-  // TODO(crbug.com/909794):Get the user selection and start the restoration.
-
   if (!is_shut_down_) {
     NotificationDisplayService::GetForProfile(profile_)->Close(
         NotificationHandler::Type::TRANSIENT, id);
   }
+
+  if (!button_index.has_value() ||
+      button_index.value() !=
+          static_cast<int>(RestoreNotificationButtonIndex::kRestore)) {
+    return;
+  }
+
+  // TODO(crbug.com/909794):Get the user selection and start the restoration.
 }
 
 }  // namespace full_restore
