a05c91c4fc55ff269dfa6496ce8bea2d4f21eca4
Alice Wang <aliceywang@chromium.org>
2020-12-02 07:59:08 +0000

    [Android][Signin] Move IdentityServicesProvider to signin.services target
    This CL moves IdentityServicesProvider to signin.services target.
    
    Bug: 1152718
    Change-Id: I3188b04ceadc6f1644b761544ea2de1879ad2791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567871
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832716}

--- chrome/android/java/src/org/chromium/chrome/browser/password_manager/PasswordManagerLauncher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/PasswordManagerLauncher.java
@@ -11,7 +11,7 @@ import org.chromium.base.annotations.CalledByNative;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
-import org.chromium.chrome.browser.signin.IdentityServicesProvider;
+import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.sync.ProfileSyncService;
 import org.chromium.components.signin.identitymanager.IdentityManager;
 import org.chromium.components.sync.ModelType;
--- chrome/browser/signin/identity_services_provider_android.cc
+++ chrome/browser/signin/identity_services_provider_android.cc
@@ -4,9 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/android/jni_android.h"
-#include "chrome/android/chrome_jni_headers/IdentityServicesProvider_jni.h"
 #include "chrome/browser/profiles/profile_android.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/signin/services/android/jni_headers/IdentityServicesProvider_jni.h"
 #include "chrome/browser/signin/signin_manager_android_factory.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 
