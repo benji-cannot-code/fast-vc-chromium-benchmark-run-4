b708b6d0ed9527e0a35955fe3ac231b11c75cc68
Alan Cutter <alancutter@chromium.org>
2020-12-02 04:49:13 +0000

    Reland "Add WebApp.Preinstalled.UninstallAndReplaceCount UMA histogram"
    This reverts commit e2bb0435c6fd4f7cfded2472d9ea0e4b1a4e8bc1.
    
    Reason for revert:
    Original revert did not fix the issue. Real fix was:
    https://chromium-review.googlesource.com/c/chromium/src/+/2566814
    
    Original change's description:
    > Revert "Add WebApp.Preinstalled.UninstallAndReplaceCount UMA histogram"
    >
    > This reverts commit 1ca3c1538117f62f25142c9abe7c0ad369b0e674.
    >
    > Reason for revert:
    > Needed to land parent CL revert: https://chromium-review.googlesource.com/c/chromium/src/+/2567511
    >
    > Original change's description:
    > > Add WebApp.Preinstalled.UninstallAndReplaceCount UMA histogram
    > >
    > > This CL adds metrics for how often preinstalled web apps migrate and
    > > remove an existing app install.
    > >
    > > This data is useful to monitor rollouts of default web apps that are
    > > replacing old Chrome apps to ensure the migration is happening as
    > > expected.
    > >
    > > Bug: 809304, 1058265
    > > Change-Id: Ie9edb4a1cf89e84d7e56a20bad028dd8ea1aeb07
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2561999
    > > Commit-Queue: Alan Cutter <alancutter@chromium.org>
    > > Reviewed-by: Glen Robertson <glenrob@chromium.org>
    > > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#832148}
    >
    > TBR=rkaplow@chromium.org,alancutter@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,glenrob@chromium.org
    >
    > Change-Id: I46c1a678e09111d210d1385bdb17ecc4fcabb093
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 809304
    > Bug: 1058265
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567056
    > Reviewed-by: Alan Cutter <alancutter@chromium.org>
    > Commit-Queue: Alan Cutter <alancutter@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#832227}
    
    TBR=rkaplow@chromium.org,alancutter@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,glenrob@chromium.org
    
    Bug: 809304
    Bug: 1058265
    Change-Id: I1139aa9380a809a4f96ab5664aa279973a60a788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567281
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832681}

--- chrome/browser/web_applications/external_web_app_manager.cc
+++ chrome/browser/web_applications/external_web_app_manager.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/json/json_file_value_serializer.h"
 #include "base/json/json_reader.h"
-#include "base/metrics/histogram_functions.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/path_service.h"
 #include "base/stl_util.h"
@@ -220,6 +220,8 @@ const char* ExternalWebAppManager::kHistogramDisabledCount =
     "WebApp.Preinstalled.DisabledCount";
 const char* ExternalWebAppManager::kHistogramConfigErrorCount =
     "WebApp.Preinstalled.ConfigErrorCount";
+const char* ExternalWebAppManager::kHistogramUninstallAndReplaceCount =
+    "WebApp.Preinstalled.UninstallAndReplaceCount";
 
 void ExternalWebAppManager::RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
@@ -355,12 +357,12 @@ void ExternalWebAppManager::PostProcessConfigs(ConsumeInstallOptions callback,
     debug_info_->enabled_configs = parsed_configs.options_list;
   }
 
-  base::UmaHistogramCounts100(ExternalWebAppManager::kHistogramEnabledCount,
-                              parsed_configs.options_list.size());
-  base::UmaHistogramCounts100(ExternalWebAppManager::kHistogramDisabledCount,
-                              disabled_count);
-  base::UmaHistogramCounts100(ExternalWebAppManager::kHistogramConfigErrorCount,
-                              parsed_configs.errors.size());
+  UMA_HISTOGRAM_COUNTS_100(ExternalWebAppManager::kHistogramEnabledCount,
+                           parsed_configs.options_list.size());
+  UMA_HISTOGRAM_COUNTS_100(ExternalWebAppManager::kHistogramDisabledCount,
+                           disabled_count);
+  UMA_HISTOGRAM_COUNTS_100(ExternalWebAppManager::kHistogramConfigErrorCount,
+                           parsed_configs.errors.size());
 
   std::move(callback).Run(parsed_configs.options_list);
 }
@@ -387,10 +389,16 @@ void ExternalWebAppManager::OnExternalWebAppsSynchronized(
       prefs::kWebAppsLastPreinstallSynchronizeVersion,
       version_info::GetMajorVersionNumber());
 
+  size_t uninstall_and_replace_count = 0;
   for (const auto& url_and_result : install_results) {
-    base::UmaHistogramEnumeration("Webapp.InstallResult.Default",
-                                  url_and_result.second.code);
+    UMA_HISTOGRAM_ENUMERATION("Webapp.InstallResult.Default",
+                              url_and_result.second.code);
+    if (url_and_result.second.did_uninstall_and_replace)
+      ++uninstall_and_replace_count;
   }
+  UMA_HISTOGRAM_COUNTS_100(
+      ExternalWebAppManager::kHistogramUninstallAndReplaceCount,
+      uninstall_and_replace_count);
 
   if (callback) {
     std::move(callback).Run(std::move(install_results),
