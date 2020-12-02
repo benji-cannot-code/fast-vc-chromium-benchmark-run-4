fa19b033ca803dd0ccd81c851043d4c8ed638884
Fredrik Söderqvist <fs@opera.com>
2020-12-02 18:31:13 +0000

    Use TimeDelta::FromHz for SVGImage frame delay constant
    Use FromHz(60) rather than FromSecondsD(1.0 / 60) for a slightly more
    readable expression.
    
    Change-Id: Ied348b7baaa0710d7bc8ed4a7ed94e62f5835c42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566955
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832886}

--- third_party/blink/renderer/core/svg/graphics/svg_image_chrome_client.cc
+++ third_party/blink/renderer/core/svg/graphics/svg_image_chrome_client.cc
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 static constexpr base::TimeDelta kAnimationFrameDelay =
-    base::TimeDelta::FromSecondsD(1.0 / 60);
+    base::TimeDelta::FromHz(60);
 
 SVGImageChromeClient::SVGImageChromeClient(SVGImage* image)
     : image_(image),
