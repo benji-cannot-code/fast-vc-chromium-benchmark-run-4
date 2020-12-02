bd1577848ab2f29941d1b53c2ac57b544e7e274e
Timothy Loh <timloh@chromium.org>
2020-12-02 10:40:13 +0000

    Allow launching Plugin VM if VM already exists but image URL is not set
    If a user sets up Plugin VM via VMC, we should let them use the UI
    integration without having a image URL set in policy.
    
    The check for an existing VM is changed from using the dispatcher to
    using concierge. This means we no longer need to first install the DLC,
    and so can move it earlier in the flow.
    
    This behaviour was previously implemented in https://crrev.com/c/2043331
    but broken in https://crrev.com/c/2115134.
    
    Bug: b/173678297
    Change-Id: I2c4c2196d68092d3ba631e2c11be0c1bd2c10b69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2565221
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832761}

--- chrome/browser/chromeos/plugin_vm/plugin_vm_metrics_util.h
+++ chrome/browser/chromeos/plugin_vm/plugin_vm_metrics_util.h
@@ -47,8 +47,9 @@ enum class PluginVmSetupResult {
   kError = 13,
   kUserCancelledWithoutStarting = 14,
   kUserCancelledValidatingLicense = 15,
+  kUserCancelledStartingDispatcher = 16,
 
-  kMaxValue = kUserCancelledValidatingLicense,
+  kMaxValue = kUserCancelledStartingDispatcher,
 };
 
 enum class PluginVmDlcUseResult {
