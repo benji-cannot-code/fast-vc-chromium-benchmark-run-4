97991ac3c080e99eb13e3d9a58033f5912f8495b
David Tseng <dtseng@google.com>
2020-12-02 21:20:33 +0000

    Generalize Google Docs content script and use in Switch Access
    Test: manually verify that when SA is enabled, navigating to a Google
    Doc, and dumping the accessibility tree shows the content of the doc in
    the tree. Modify and make the (now) accessibility live test suite
    compile. The test has been failing and not run on bots. TODO'ed fixing.
    
    R=akihiroota@chromium.org
    
    Fixed: 1115978
    AX-Relnotes: Switch Access can now navigate within the content of a Google Doc.
    Change-Id: I06d20cdef5705eed637609597ed34a33ce3cea39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2569062
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832973}

--- chrome/browser/chromeos/accessibility/accessibility_live_site_browsertest.cc
+++ chrome/browser/chromeos/accessibility/accessibility_live_site_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_accessibility_state.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_service.h"
+#include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "extensions/browser/notification_types.h"
 #include "net/dns/mock_host_resolver.h"
@@ -23,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-class SelectToSpeakLiveSiteTest : public InProcessBrowserTest {
+class AccessibilityLiveSiteTest : public InProcessBrowserTest {
  protected:
   void SetUpOnMainThread() override {
     ASSERT_FALSE(AccessibilityManager::Get()->IsSelectToSpeakEnabled());
@@ -41,9 +42,10 @@ class SelectToSpeakLiveSiteTest : public InProcessBrowserTest {
   }
 
   void SetUpInProcessBrowserTestFixture() override {
-    // To avoid depending on external resources, browser tests don't allow
-    // non-local DNS queries by default. Override this for this specific
-    // manual test suite.
+    // TODO: this logic currently doesn't work and the test never passes due to
+    // inability to lookup docs.google.com. To avoid depending on external
+    // resources, browser tests don't allow non-local DNS queries by default.
+    // Override this for this specific manual test suite.
     scoped_refptr<net::RuleBasedHostResolverProc> resolver =
         new net::RuleBasedHostResolverProc(host_resolver());
     resolver->AllowDirectLookup("*.google.com");
@@ -56,7 +58,7 @@ class SelectToSpeakLiveSiteTest : public InProcessBrowserTest {
     mock_host_resolver_override_.reset();
   }
 
-  SpeechMonitor speech_monitor_;
+  test::SpeechMonitor speech_monitor_;
   std::unique_ptr<ui::test::EventGenerator> generator_;
 
   std::unique_ptr<net::ScopedDefaultHostResolverProc>
@@ -73,7 +75,8 @@ class SelectToSpeakLiveSiteTest : public InProcessBrowserTest {
 // To visually see what's happening while the test is running,
 // add this option:
 //    --enable-pixel-output-in-tests
-IN_PROC_BROWSER_TEST_F(SelectToSpeakLiveSiteTest, GoogleDocsSupport) {
+IN_PROC_BROWSER_TEST_F(AccessibilityLiveSiteTest,
+                       SelectToSpeakGoogleDocsSupport) {
   const char* kGoogleDocsUrl =
       "https://docs.google.com/document/d/"
       "1qpu3koSIHpBzQbxeEE-dofSKXCIgdc4yJLI-o1LpCPs/view";
@@ -88,7 +91,6 @@ IN_PROC_BROWSER_TEST_F(SelectToSpeakLiveSiteTest, GoogleDocsSupport) {
 
   content::WaitForAccessibilityTreeToContainNodeWithName(
       web_contents, "Long-string-to-test-select-to-speak");
-
   gfx::Rect bounds = browser()->window()->GetBounds();
   generator_->PressKey(ui::VKEY_LWIN, 0 /* flags */);
   generator_->MoveMouseTo(bounds.x() + 8, bounds.y() + 200);
@@ -98,11 +100,8 @@ IN_PROC_BROWSER_TEST_F(SelectToSpeakLiveSiteTest, GoogleDocsSupport) {
   generator_->ReleaseLeftButton();
   generator_->ReleaseKey(ui::VKEY_LWIN, 0 /* flags */);
 
-  for (;;) {
-    std::string utterance = speech_monitor_.GetNextUtterance();
-    if (utterance == kTextFoundInGoogleDoc)
-      break;
-  }
+  speech_monitor_.ExpectSpeech(kTextFoundInGoogleDoc);
+  speech_monitor_.Replay();
 }
 
 }  // namespace chromeos
--- chrome/browser/resources/chromeos/accessibility/common/gdocs_script.js
+++ chrome/browser/resources/chromeos/accessibility/common/gdocs_script.js
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var tries = 10;
 
-// Google Docs isn't compatible with Select-to-speak by default because
-// in order to provide screen reader support, most of the rendered
-// document has aria-hidden set on it, which has the side effect of
-// hiding it from Select-to-speak too. Fix it by changing aria-hidden
-// to false. Try multiple times in case the page isn't fully loaded when
-// the content script runs.
+// Google Docs isn't compatible with non-screen reader accessibility services by
+// default because in order to provide screen reader support, most of the
+// rendered document has aria-hidden set on it, which has the side effect of
+// hiding it from non-screen reader accessibility features too. Fix it by
+// changing aria-hidden to false. Try multiple times in case the page isn't
+// fully loaded when the content script runs.
 function RemoveAriaHiddenFromGoogleDocsContent() {
   var element = document.querySelector('.kix-zoomdocumentplugin-outer');
   if (element) {
