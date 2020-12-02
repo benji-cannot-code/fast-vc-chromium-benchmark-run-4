e7bc422cf0797ec573939231f047b2de6dcd4c96
Sreeja Kamishetty <sreejakshetty@chromium.org>
2020-12-02 07:19:13 +0000

    Add ClosedTabCache Service and Factory
    ClosedTabCache is a desktop feature to instantly restore closed tabs.
    
    To own ClosedTabCache, this CL adds a KeyedService called
    ClosedTabCacheService and a BrowserContextKeyedServiceFactory,
    ClosedTabCacheServiceFactory, to retrieve the service.
    
    ClosedTabCache design doc:
    https://docs.google.com/document/d/1SF230MYWgroe4WikDMn82ETd3-HFRzylKtIk0HZRaOU/edit?usp=sharing
    
    BUG=1100946
    
    Change-Id: I397aa3abbebd9098694b20b9385fe75a7958c484
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2564966
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832708}

---
+++ chrome/browser/sessions/closed_tab_cache_service_factory.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/sessions/closed_tab_cache_service_factory.h"
+
+#include "build/build_config.h"
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
+
+ClosedTabCacheServiceFactory::ClosedTabCacheServiceFactory()
+    : BrowserContextKeyedServiceFactory(
+          "ClosedTabCacheService",
+          BrowserContextDependencyManager::GetInstance()) {}
+
+// static
+ClosedTabCacheService* ClosedTabCacheServiceFactory::GetForProfile(
+    Profile* profile) {
+  return static_cast<ClosedTabCacheService*>(
+      GetInstance()->GetServiceForBrowserContext(profile, true));
+}
+
+ClosedTabCacheServiceFactory* ClosedTabCacheServiceFactory::GetInstance() {
+  return base::Singleton<ClosedTabCacheServiceFactory>::get();
+}
+
+KeyedService* ClosedTabCacheServiceFactory::BuildServiceInstanceFor(
+    content::BrowserContext* context) const {
+  if (context->IsOffTheRecord())
+    return nullptr;
+  return new ClosedTabCacheService(static_cast<Profile*>(context));
+}
+
+bool ClosedTabCacheServiceFactory::ServiceIsCreatedWithBrowserContext() const {
+  return true;
+}
