566fb270a57d1e12455880aa6ffd890ab3cd0b71
Shik Chen <shik@chromium.org>
2020-12-02 10:17:07 +0000

    CCA: Scale and crop large images before scanning barcodes
    The latency of barcode detection process is roughly proportional to the
    image resolution. Scaling and cropping large images before detection
    would speed up the process considerably.
    
    Bug: b/172879638
    Test: Manually in expert mode
    Change-Id: I84e4e12c20cd6d364a88789a224aae36dcb9a97c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567017
    Commit-Queue: Shik Chen <shik@chromium.org>
    Reviewed-by: Inker Kuo <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832755}

--- chromeos/components/camera_app_ui/resources/js/externs/w3c_api.js
+++ chromeos/components/camera_app_ui/resources/js/externs/w3c_api.js
@@ -3,7 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/* eslint-disable no-unused-vars, no-var, valid-jsdoc */
+/* eslint-disable no-undef, no-unused-vars, no-var, valid-jsdoc */
+
+// TODO(b/172879638): Remove this extern once we have
+// https://github.com/google/closure-compiler/pull/3735 merged in Closure
+// Compiler and Chromium.
+
+/** @type {string} */
+OffscreenCanvasRenderingContext2D.prototype.imageSmoothingQuality;
 
 // TODO(b/172879638): Upstream the externs of BarcodeDetector to Closure
 // Compiler.
