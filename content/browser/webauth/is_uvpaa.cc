51474a7ee228c08d288b9791d41a5c784fe47bae
Martin Kreichgauer <martinkr@google.com>
2020-12-02 18:24:55 +0000

    fido: annotate IsUVPlatformAuthenticatorAvailable() with MayBlock() on ChromeOS
    On ChromeOS, content::IsUVPlatformAuthenticatorAvailable() needs to make
    a dbus call into u2fd, which in theory may block. Hence, post
    invocations of that method onto a separate task and annotate with
    MayBlock() on ChromeOS.
    
    We should refactor this interface to take a callback instead, which is
    tracked in crbug.com/1154063.
    
    Also changes an IsUVPAA() check in GetAvailableTransports() to only test
    whether the embedder has injected an override for that method. As a
    consequence, platform discoveries must check IsUVPAA() before
    instantiating a platform authenticator, which ChromeOS's now does.
    
    Bug: 1150636, 1150679
    Change-Id: I7311b00fc9b37fb33cd1d1735a2664f3cd901dc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2559262
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832884}

--- content/browser/webauth/is_uvpaa.cc
+++ content/browser/webauth/is_uvpaa.cc
@@ -44,7 +44,8 @@ bool IsUVPlatformAuthenticatorAvailable(
 bool IsUVPlatformAuthenticatorAvailable() {
   return base::FeatureList::IsEnabled(
              device::kWebAuthCrosPlatformAuthenticator) &&
-         device::ChromeOSAuthenticator::IsUVPlatformAuthenticatorAvailable();
+         device::ChromeOSAuthenticator::
+             IsUVPlatformAuthenticatorAvailableBlocking();
 }
 
 #else
