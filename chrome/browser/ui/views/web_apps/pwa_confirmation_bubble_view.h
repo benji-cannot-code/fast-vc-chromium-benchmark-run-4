a7e4edec0eddb519565ae5bd857d808ab98b5a9d
Mike Jackson <mjackson@microsoft.com>
2020-12-02 21:45:56 +0000

    dpwas: Remove Run On OS Login option from install dialog
    Per UX review, removing the option to run a PWA at OS login from
    the PWA install dialog.
    
    Bug: 1110407
    Change-Id: I17999312a928fd85a0eae74f907f768c4028c022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2558753
    Commit-Queue: Mike Jackson <mjackson@microsoft.com>
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832987}

--- chrome/browser/ui/views/web_apps/pwa_confirmation_bubble_view.h
+++ chrome/browser/ui/views/web_apps/pwa_confirmation_bubble_view.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_WEB_APPS_PWA_CONFIRMATION_BUBBLE_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_WEB_APPS_PWA_CONFIRMATION_BUBBLE_VIEW_H_
 
-#include <memory>
-
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_bubble_delegate_view.h"
 #include "chrome/browser/web_applications/components/web_application_info.h"
@@ -45,12 +43,10 @@ class PWAConfirmationBubbleView : public LocationBarBubbleDelegateView {
   views::View* GetInitiallyFocusedView() override;
   void WindowClosing() override;
   bool Accept() override;
-  views::Checkbox* GetRunOnOsLoginCheckboxForTesting();
 
  private:
   std::unique_ptr<WebApplicationInfo> web_app_info_;
   chrome::AppInstallationAcceptanceCallback callback_;
-  views::Checkbox* run_on_os_login_ = nullptr;
 
   // Checkbox to launch window with tab strip.
   views::Checkbox* tabbed_window_checkbox_ = nullptr;
