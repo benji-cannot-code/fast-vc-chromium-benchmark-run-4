8d541668811fd69982c7c9882622667c4193441c
Dan Harrington <harringtond@chromium.org>
2020-12-02 02:19:14 +0000

    Add FeedV2NewTabPageTest
    - Copied tests from FeedNewTabPageTest to work with v2
    - Added a fake feed server, so that we can load fake cards in tests.
      We'll use this to replace use of feed_large.gcl.bin from feedv1.
    - updated fake response proto to provide prefetch metadata
    
    Change-Id: I705cea688b189e45f395e069f6c4e997aff1f175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2561642
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/master@{#832629}

--- components/feed/core/v2/feed_stream_unittest.cc
+++ components/feed/core/v2/feed_stream_unittest.cc
@@ -1554,6 +1554,7 @@ TEST_F(FeedStreamTest, ReadNetworkResponse) {
 
   // The stream's user attributes are set, so activity logging is enabled.
   EXPECT_TRUE(stream_->IsActivityLoggingEnabled());
+  EXPECT_EQ(1, prefetch_service_.NewSuggestionsAvailableCallCount());
 }
 
 TEST_F(FeedStreamTest, ClearAllAfterLoadResultsInRefresh) {
