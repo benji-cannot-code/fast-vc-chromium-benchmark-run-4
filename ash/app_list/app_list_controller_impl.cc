50a07b7db2331c1b4944a29767137398115c30ad
Angus L. M. McLean IV <angusmclean@chromium.org>
2020-12-02 20:39:27 +0000

    Prevent annoying scroll behavior
    Scrolling up on the first page of apps will close the full screen app
    list view. This is fine, except that it can cause the app list to close
    right after opening, which is frustrating. The intended behavior was to
    only close this view if the scrolling what happening outside the app list
    grid.
    
    Bug: 1123648
    Change-Id: I802ca96861f43ce61eed7ddcca0b0573e56debec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2554786
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Angus McLean <angusmclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832943}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -649,7 +649,7 @@ void AppListControllerImpl::EndDragFromShelf(AppListViewState app_list_state) {
 
 void AppListControllerImpl::ProcessMouseWheelEvent(
     const ui::MouseWheelEvent& event) {
-  presenter_.ProcessMouseWheelOffset(event.offset());
+  presenter_.ProcessMouseWheelOffset(event.location(), event.offset());
 }
 
 ShelfAction AppListControllerImpl::ToggleAppList(
