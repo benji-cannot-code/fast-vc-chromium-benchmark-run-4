54ea5642fe5eb11914fff36b39b2a2ee9542fc63
John Chen <johnchen@chromium.org>
2020-12-02 17:44:42 +0000

    [Benchmark] Schedule speedometer2-pcscan
    Add new benchmark speedometer2-pcscan to shard maps.
    
    Bug: v8:11180
    Change-Id: I673870ec20c7d9498e62b7f6454d93061fecd271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568854
    Reviewed-by: Tamer Tas <tmrts@chromium.org>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832865}

--- tools/perf/core/undocumented_benchmarks.py
+++ tools/perf/core/undocumented_benchmarks.py
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-
 # Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -24,7 +23,7 @@ UNDOCUMENTED_BENCHMARKS = {
     'speedometer-future',
     'speedometer2',
     'speedometer2-future',
-    'UNSCHEDULED_speedometer2-pcscan',
+    'speedometer2-pcscan',
     'startup.mobile',
     'system_health.webview_startup',
     'tab_switching.typical_25',
