1b63da76bb6523c796cb6732cca8a844976e7358
Anastasiia N <anastasiian@chromium.org>
2020-12-02 09:46:19 +0000

    Remove source field from InlineLoginDialogChromeOS
    InlineLoginDialogChromeOS uses `source` field to show the correct error
    screen if account addition in not allowed. This field will be removed
    when EDU coexistence for ARC flag will be removed
    (see https://crbug.com/1140500).
    
    Currently we pass the source to
    InlineLoginDialogChromeOS::ShowDeprecated method, but we will not pass
    it to the new Show method (see https://crrev.com/c/2550741), which will
    be used by AccountManagerFacadeAsh. (`AccountManagerFacade` itself will
    start taking a `source` parameter)
    
    We will store the is_arc_source flag instead and set it to false for
    calls from AccountManagerFacade, while ARC will keep calling the old
    ShowDeprecated method. When we can remove the is_arc_source flag, we
    will migrate ARC calls to use AccountManagerFacade.
    
    Bug: 1140469
    Change-Id: Ib0d57be7f3034a87567a1651af67c8e0fac19b4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550721
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832742}

--- chrome/browser/ui/webui/signin/inline_login_dialog_chromeos.h
+++ chrome/browser/ui/webui/signin/inline_login_dialog_chromeos.h
@@ -96,8 +96,10 @@ class InlineLoginDialogChromeOS : public SystemWebDialogDelegate,
   void SetEduCoexistenceFlowResult(EduCoexistenceFlowResult result);
 
  protected:
-  explicit InlineLoginDialogChromeOS(const Source& source);
-  InlineLoginDialogChromeOS(const GURL& url, const Source& source);
+  // |is_arc_source| parameter is used to specify whether the dialog is opened
+  // from ARC. It's used to display the correct error message for Child users.
+  explicit InlineLoginDialogChromeOS(bool is_arc_source);
+  InlineLoginDialogChromeOS(const GURL& url, bool is_arc_source);
   ~InlineLoginDialogChromeOS() override;
 
   // ui::WebDialogDelegate overrides
@@ -109,7 +111,7 @@ class InlineLoginDialogChromeOS : public SystemWebDialogDelegate,
 
  private:
   InlineLoginHandlerModalDelegate delegate_;
-  const Source source_;
+  const bool is_arc_source_;
   const GURL url_;
   base::Optional<EduCoexistenceFlowResult> edu_coexistence_flow_result_;
 
