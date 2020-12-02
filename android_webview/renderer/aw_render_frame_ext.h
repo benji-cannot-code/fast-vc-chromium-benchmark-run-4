02da9ad9d4e9219271aa1e491aabc08816fdc8f1
Gyuyoung Kim <gyuyoung@igalia.com>
2020-12-02 22:31:30 +0000

    Convert AwViewMsg_DoHitTest to Mojo message
    This CL converts AwViewMsg_DoHitTest to android_webview's
    LocalMainFrame interface with renaming to HitTest.
    
    Bug: 1146495
    Change-Id: I8c819818af62c3a7bdedd9511f0297c023cb26cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567058
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#833006}

--- android_webview/renderer/aw_render_frame_ext.h
+++ android_webview/renderer/aw_render_frame_ext.h
@@ -49,8 +49,6 @@ class AwRenderFrameExt : public content::RenderFrameObserver,
   void OnDestruct() override;
 
   void OnDocumentHasImagesRequest(uint32_t id);
-  void OnDoHitTest(const gfx::PointF& touch_center,
-                   const gfx::SizeF& touch_area);
 
   void OnSetTextZoomFactor(float zoom_factor);
 
@@ -62,6 +60,8 @@ class AwRenderFrameExt : public content::RenderFrameObserver,
 
   // mojom::LocalMainFrame overrides:
   void SetBackgroundColor(SkColor c) override;
+  void HitTest(const gfx::PointF& touch_center,
+               const gfx::SizeF& touch_area) override;
 
   void BindLocalMainFrame(
       mojo::PendingAssociatedReceiver<mojom::LocalMainFrame> pending_receiver);
