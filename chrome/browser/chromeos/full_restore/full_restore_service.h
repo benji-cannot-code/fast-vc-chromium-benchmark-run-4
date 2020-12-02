775ac176dd1090e2a42f4b41847ed4eaf482dbbb
Nancy Wang <nancylingwang@chromium.org>
2020-12-02 21:17:57 +0000

    Add the restore preference default setting.
    This CL implements the default setting for the "restore_apps_and_pages"
    setting based on:
    https://docs.google.com/presentation/d/1kg1HAyjHc3l9EnaBehCQS3K_O7BmLpE6x_JWAAGUOMc/edit?ts=5faddd25#slide=id.g838f61fb61_2_5
    
    For the new Chrome OS user, set the default value as 'Ask Every Time',
    or set the default value based on the browser's restore setting sync
    value.
    
    For the upgrading scenario, set the default value based on the current
    browser's restore setting.
    
    BUG=1146900
    
    Change-Id: I2032547103a74fe9625bbd7cf47f422f0a52ab55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563499
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832971}

--- chrome/browser/chromeos/full_restore/full_restore_service.h
+++ chrome/browser/chromeos/full_restore/full_restore_service.h
@@ -6,14 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_FULL_RESTORE_FULL_RESTORE_SERVICE_H_
 #define CHROME_BROWSER_CHROMEOS_FULL_RESTORE_FULL_RESTORE_SERVICE_H_
 
+#include <memory>
+
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/sync_preferences/pref_service_syncable_observer.h"
 
 class Profile;
 
 namespace chromeos {
 namespace full_restore {
+
+class NewUserRestorePrefHanlder;
 
 // The FullRestoreService class calls AppService and Window Management
 // interfaces to restore the app launchings and app windows.
@@ -23,11 +28,8 @@ namespace full_restore {
 // 2. For normal reboot, read the restore setting fromt the user pref, and based
 // on the setting to decide restore or not.
 //
-// TODO(crbug.com/909794):
-// 1. If the system crashed before reboot, show the notification notification.
-// Otherwise, read |kRestoreAppsAndPagesPrefName|
-// 2. Observe the AppRegistryCache to read the app info, and restore apps and
-// app windows.
+// TODO(crbug.com/909794): Observe the AppRegistryCache to read the app info,
+// and restore apps and app windows.
 class FullRestoreService : public KeyedService {
  public:
   explicit FullRestoreService(Profile* profile);
@@ -49,6 +51,8 @@ class FullRestoreService : public KeyedService {
   Profile* profile_ = nullptr;
 
   bool is_shut_down_ = false;
+
+  std::unique_ptr<NewUserRestorePrefHanlder> new_user_pref_handler_;
 
   base::WeakPtrFactory<FullRestoreService> weak_ptr_factory_{this};
 };
