da27059eeec5a9a14061be450953a9220a9c54f9
Luciano Pacheco <lucmult@chromium.org>
2020-12-02 02:59:19 +0000

    Files app: Generate JS module for <files-toast> and its unittest
    Bug: 1133186
    Change-Id: I1dc6cdc529b21298943d7157b810e85533a645e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560335
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832650}

--- ui/file_manager/file_manager/foreground/elements/files_toast_unittest.m.js
+++ ui/file_manager/file_manager/foreground/elements/files_toast_unittest.m.js
@@ -3,11 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function setUpPage() {
+// clang-format off
+import {assertEquals, assertFalse, assertTrue} from 'chrome://test/chai_assert.js';
+import {FilesToast} from './files_toast.m.js';
+// clang-format on
+
+export function setUpPage() {
   document.body.innerHTML += '<files-toast></files-toast>';
 }
 
-async function testToast(done) {
+export async function testToast(done) {
   /** @type {FilesToast|Element} */
   const toast = document.querySelector('files-toast');
   const text = toast.shadowRoot.querySelector('#text');
