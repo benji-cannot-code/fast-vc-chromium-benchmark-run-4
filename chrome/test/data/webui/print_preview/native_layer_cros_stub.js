9da0b68194219179363cb7da1e5de98aa98bbe68
Matt Menard <mattme@google.com>
2020-12-02 23:14:12 +0000

    Create PrintServerStore for destination-dialog
    Adds PrintServerStore to manage print servers and their associated
    printers in the destination-dialog element.
    
    Bug: b:168650771
    Change-Id: Ic89abf70bfb6acf05bf3c090882b5487a18caf84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538282
    Commit-Queue: Matt Menard <mattme@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833031}

--- chrome/test/data/webui/print_preview/native_layer_cros_stub.js
+++ chrome/test/data/webui/print_preview/native_layer_cros_stub.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {NativeLayerCros, NativeLayerCrosImpl, PrinterSetupResponse} from 'chrome://print/print_preview.js';
+import {NativeLayerCros, NativeLayerCrosImpl, PrinterSetupResponse, PrintServer, PrintServersConfig} from 'chrome://print/print_preview.js';
 import {assert} from 'chrome://resources/js/assert.m.js';
 import {PromiseResolver} from 'chrome://resources/js/promise_resolver.m.js';
 
@@ -26,6 +26,8 @@ export class NativeLayerCrosStub extends TestBrowserProxy {
       'getEulaUrl',
       'requestPrinterStatusUpdate',
       'setupPrinter',
+      'choosePrintServers',
+      'getPrintServersConfig',
     ]);
 
     /**
@@ -56,6 +58,12 @@ export class NativeLayerCrosStub extends TestBrowserProxy {
 
     /** @private {number} */
     this.multiplePrinterStatusRequestsCount_ = 0;
+
+    /** @private {!PrintServersConfig} */
+    this.printServersConfig_ = {
+      printServers: [],
+      isSingleServerFetchingMode: false
+    };
   }
 
   /** @override */
@@ -135,4 +143,15 @@ export class NativeLayerCrosStub extends TestBrowserProxy {
 
   /** @override */
   recordPrinterStatusHistogram(statusReason, didUserAttemptPrint) {}
+
+  /** @override */
+  choosePrintServers(printServerIds) {
+    this.methodCalled('choosePrintServers', printServerIds);
+  }
+
+  /** @override */
+  getPrintServersConfig() {
+    this.methodCalled('getPrintServersConfig');
+    return Promise.resolve(this.printServersConfig_);
+  }
 }
