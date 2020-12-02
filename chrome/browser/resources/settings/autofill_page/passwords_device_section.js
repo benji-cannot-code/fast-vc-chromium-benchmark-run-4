51aaf488e631894a2fcd4ca1d26fef074dbc6992
Mohamed Amir Yosef <mamir@chromium.org>
2020-12-02 13:32:13 +0000

    [Passwords] Introduce move multiple passwords banner
    This CL introduces a new banner in the
    chrome:/setting/passwords/device page that shows the number of passwords
    stored on the device, with a button to move them to the account.
    The appearance of this banner is guarded with a feature toggle.
    
    Screenshot: https://screenshot.googleplex.com/9cbSpM55w8BGLKN.png
    
    Bug: 1139263
    Change-Id: I56268cee2cb4e797e4153a2090c6463003c7ecfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547440
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832787}

--- chrome/browser/resources/settings/autofill_page/passwords_device_section.js
+++ chrome/browser/resources/settings/autofill_page/passwords_device_section.js
@@ -110,6 +110,17 @@ Polymer({
           'savedPasswords.splices)',
     },
 
+    /**
+     * Passwords displayed in both the device-only and 'device and account'
+     * subsections.
+     * @private {!Array<!MultiStorePasswordUiEntry>}
+     */
+    allDevicePasswords_: {
+      type: Array,
+      value: () => [],
+      computed: 'computeAllDevicePasswords_(savedPasswords.splices)',
+    },
+
     /** @private {!MultiStorePasswordUiEntry} */
     lastFocused_: Object,
 
@@ -158,6 +169,13 @@ Polymer({
     currentRoute_: {
       type: Object,
       value: null,
+    },
+
+    /** @private */
+    devicePasswordsLabel_: {
+      type: String,
+      value: '',
+      computed: 'computeDevicePasswordsLabel_(allDevicePasswords_)',
     },
 
     /** @private */
@@ -211,6 +229,14 @@ Polymer({
    * @return {!Array<!MultiStorePasswordUiEntry>}
    * @private
    */
+  computeAllDevicePasswords_() {
+    return this.savedPasswords.filter(p => p.isPresentOnDevice());
+  },
+
+  /**
+   * @return {!Array<!MultiStorePasswordUiEntry>}
+   * @private
+   */
   computeDeviceOnlyPasswords_() {
     return this.savedPasswords.filter(
         p => p.isPresentOnDevice() && !p.isPresentInAccount());
@@ -236,6 +262,17 @@ Polymer({
         (this.syncDisabled_ === null || !!this.syncDisabled_) &&
         (this.optedInForAccountStorage_ === null ||
          !!this.optedInForAccountStorage_);
+  },
+
+  /**
+   * @private
+   * @return {string}
+   */
+  computeDevicePasswordsLabel_() {
+    return this.allDevicePasswords_.length === 1 ?
+        this.i18n('devicePasswordsLinkLabelSingular') :
+        this.i18n(
+            'devicePasswordsLinkLabelPlural', this.allDevicePasswords_.length);
   },
 
   /**
