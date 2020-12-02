a65ee71763581dbb1c35e30af9e1d2c0bc053abc
Olga Sharonova <olka@chromium.org>
2020-12-02 13:14:12 +0000

    Revert "Add restore and delete actions in Trash root view"
    This reverts commit 411321535da1c158c62793173da61a00426f9599.
    
    Reason for revert: Trash/FilesAppBrowserTest.Test/trashRestoreFromTrash failed on https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/38983/overview
    
    Original change's description:
    > Add restore and delete actions in Trash root view
    >
    > http://go/chrome-ss/8aec11c357971886d745aa9ea5b89356399956d2
    >
    > Added restore-from-trash command which is shown in the Trash root as
    > both a top bar action, and a file context menu command.
    >
    > Modified delete action so that it shows for Trash root (even though the
    > Trash root FakeEntry is read-only), and added remove() and
    > removeRecursively() functions to TrashEntry which deletes both the
    > filesEntry and infoEntry.
    >
    > Background restore() code previously expected the supplied TrashItem
    > would come only from the TrashItem returned by removeFileOrDirectory(),
    > but now that it can be set directly from the restore-from-trash
    > command, it requires a change to the TrashConfig lookup and path prefix
    > validation.
    >
    > Bug: 953310
    > Change-Id: I2cdf018fd1daab8f57f0ab7f679d47eeba2109b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2564841
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#832700}
    
    TBR=noel@chromium.org,joelhockey@chromium.org,lucmult@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I73dcb3bca3453bd61c57eeba73098debcf52d99a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 953310
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566958
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#832784}

--- ui/file_manager/file_manager/foreground/js/toolbar_controller.js
+++ ui/file_manager/file_manager/foreground/js/toolbar_controller.js
@@ -61,13 +61,6 @@ class ToolbarController {
      * @private {!HTMLElement}
      * @const
      */
-    this.restoreFromTrashButton_ =
-        queryRequiredElement('#restore-from-trash-button', this.toolbar_);
-
-    /**
-     * @private {!HTMLElement}
-     * @const
-     */
     this.readOnlyIndicator_ =
         queryRequiredElement('#read-only-indicator', this.toolbar_);
 
@@ -91,15 +84,6 @@ class ToolbarController {
     this.deleteCommand_ = assertInstanceof(
         queryRequiredElement(
             '#delete', assert(this.toolbar_.ownerDocument.body)),
-        cr.ui.Command);
-
-    /**
-     * @private {!cr.ui.Command}
-     * @const
-     */
-    this.restoreFromTrashCommand_ = assertInstanceof(
-        queryRequiredElement(
-            '#restore-from-trash', assert(this.toolbar_.ownerDocument.body)),
         cr.ui.Command);
 
     /**
@@ -200,9 +184,6 @@ class ToolbarController {
     this.deleteButton_.addEventListener(
         'click', this.onDeleteButtonClicked_.bind(this));
 
-    this.restoreFromTrashButton_.addEventListener(
-        'click', this.onRestoreFromTrashButtonClicked_.bind(this));
-
     if (util.isFilesNg()) {
       this.togglePinnedCommand_.addEventListener(
           'checkedChange', this.updatePinnedToggle_.bind(this));
@@ -300,16 +281,10 @@ class ToolbarController {
 
     // Update visibility of the delete button.
     this.deleteButton_.hidden =
-        (selection.totalCount === 0 ||
-         !this.directoryModel_.canDeleteEntries() ||
+        (selection.totalCount === 0 || this.directoryModel_.isReadOnly() ||
          selection.hasReadOnlyEntry() ||
          selection.entries.some(
              entry => util.isNonModifiable(this.volumeManager_, entry)));
-
-    // Update visibility of the restore-from-trash button.
-    this.restoreFromTrashButton_.hidden = (selection.totalCount == 0) ||
-        this.directoryModel_.getCurrentRootType() !==
-            VolumeManagerCommon.RootType.TRASH;
 
     if (util.isFilesNg()) {
       this.togglePinnedCommand_.canExecuteChange(
@@ -353,17 +328,6 @@ class ToolbarController {
   onDeleteButtonClicked_() {
     this.deleteCommand_.canExecuteChange(this.listContainer_.currentList);
     this.deleteCommand_.execute(this.listContainer_.currentList);
-  }
-
-  /**
-   * Handles click event for restore from trash button to execute the restore
-   * command.
-   * @private
-   */
-  onRestoreFromTrashButtonClicked_() {
-    this.restoreFromTrashCommand_.canExecuteChange(
-        this.listContainer_.currentList);
-    this.restoreFromTrashCommand_.execute(this.listContainer_.currentList);
   }
 
   /**
