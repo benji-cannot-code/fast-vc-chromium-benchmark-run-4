3bac6e8f7904a0a48bdebbb0e77c2f08da8d5ddf
Colin Blundell <blundell@chromium.org>
2020-12-03 16:06:51 +0000

    [WebLayer] Show ads blocked infobar when directed by subresource filter
    This CL adds showing of the (now-componentized) ads blocked infobar in
    WebLayer. This presentation is made in //weblayer's
    SubresourceFilterClientImpl as directed by
    //components/subresource_filter. The implementation is adapted from
    that of //chrome's ChromeSubresourceFilterClient, but simplified:
    - We leave interaction with content settings for followup work
    - We do not track whether the infobar has previously been shown for the
      current navigation. The reason is that
      ContentSubresourceFilterThrottleManager already does such tracking
      itself in its MaybeShowNotification() method, so the client should
      never actually receive a duplicate request to show a notification for
      a given navigation.
    
    We also add a browsertest that the infobar is presented when a resource
    is blocked and is removed when navigating away from the page with a
    blocked resource. Note that while it would be useful to also add tests
    in Java, we can't at this time due to crbug.com/899903.
    
    We leave implementation of the reload functionality that the user can
    trigger via the infobar for a followup CL.
    
    Bug: 1116095
    Change-Id: Icb182143b0d27de5f03efc88ce8013be3c5c91d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563549
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833308}

--- weblayer/browser/subresource_filter_client_impl.cc
+++ weblayer/browser/subresource_filter_client_impl.cc
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "components/safe_browsing/android/remote_database_manager.h"
+#include "components/subresource_filter/android/ads_blocked_infobar_delegate.h"
+#include "weblayer/browser/infobar_service.h"
 #endif
 
 namespace weblayer {
@@ -46,8 +48,14 @@ GetDatabaseManagerFromSafeBrowsingService() {
 
 }  // namespace
 
-SubresourceFilterClientImpl::SubresourceFilterClientImpl()
-    : database_manager_(GetDatabaseManagerFromSafeBrowsingService()) {}
+SubresourceFilterClientImpl::SubresourceFilterClientImpl(
+    content::WebContents* web_contents)
+    :
+#if defined(OS_ANDROID)
+      web_contents_(web_contents),
+#endif
+      database_manager_(GetDatabaseManagerFromSafeBrowsingService()) {
+}
 
 SubresourceFilterClientImpl::~SubresourceFilterClientImpl() = default;
 
@@ -59,19 +67,27 @@ void SubresourceFilterClientImpl::CreateThrottleManagerWithClientForWebContents(
   subresource_filter::VerifiedRulesetDealer::Handle* dealer =
       ruleset_service ? ruleset_service->GetRulesetDealer() : nullptr;
   subresource_filter::ContentSubresourceFilterThrottleManager::
-      CreateForWebContents(web_contents,
-                           std::make_unique<SubresourceFilterClientImpl>(),
-                           dealer);
+      CreateForWebContents(
+          web_contents,
+          std::make_unique<SubresourceFilterClientImpl>(web_contents), dealer);
 }
 
 void SubresourceFilterClientImpl::OnReloadRequested() {
   // TODO(crbug.com/1116095): Bring up this flow on Android when user requests
   // it via the infobar.
-  NOTREACHED();
+  NOTIMPLEMENTED();
 }
 
 void SubresourceFilterClientImpl::ShowNotification() {
-  // TODO(crbug.com/1116095): Show infobar on Android.
+#if defined(OS_ANDROID)
+  // TODO(crbug.com/1116095): Move ChromeSubresourceFilterClient::ShowUI()'s
+  // interaction with metrics and content settings into code that's shared by
+  // WebLayer.
+  InfoBarService* infobar_service =
+      InfoBarService::FromWebContents(web_contents_);
+  subresource_filter::AdsBlockedInfobarDelegate::Create(
+      infobar_service, InfoBarService::GetResourceIdMapper());
+#endif
 }
 
 subresource_filter::mojom::ActivationLevel
