efec9edd6e98fc8c12c1ba3237bd7db295f64963
Patrick Monette <pmonette@chromium.org>
2020-12-03 18:35:12 +0000

    [PM] Get rid of kInvalidVoterId
    The IdType class already has built-in support for an invalid ID.
    
    Bug: 971272
    Change-Id: I7121400d72c22b90c153da32f0a0f74e46a27569
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545442
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833369}

--- components/performance_manager/public/voting/voting.h
+++ components/performance_manager/public/voting/voting.h
@@ -107,8 +107,6 @@ class Vote final {
 
 template <typename VoteImpl>
 using VoterId = util::IdTypeU32<VoteImpl>;
-template <typename VoteImpl>
-constexpr VoterId<VoteImpl> kInvalidVoterId;
 
 // A raw vote becomes an AcceptedVote once a VoteConsumer receives and stores
 // it, associating it with a VoterId.
@@ -253,7 +251,7 @@ class AcceptedVote final {
 
   // The ID of the voter that submitted the vote. This is defined by the
   // VoteConsumer.
-  VoterId<VoteImpl> voter_id_ = kInvalidVoterId<VoteImpl>;
+  VoterId<VoteImpl> voter_id_;
 
   const ContextType* context_ = nullptr;
 
@@ -314,7 +312,7 @@ class VotingChannel final {
   // Used to reach back into the factory to decrement the outstanding
   // VotingChannel count, and for routing votes to the consumer.
   VotingChannelFactory<VoteImpl>* factory_ = nullptr;
-  VoterId<VoteImpl> voter_id_ = kInvalidVoterId<VoteImpl>;
+  VoterId<VoteImpl> voter_id_;
 };
 
 // A helper for creating VotingChannels that binds a unique VoterId (and
@@ -663,7 +661,7 @@ AcceptedVote<VoteImpl>::AcceptedVote(VoteConsumer<VoteImpl>* consumer,
       vote_(vote),
       invalidated_(false) {
   DCHECK(consumer_);
-  DCHECK_NE(voter_id_, kInvalidVoterId<VoteImpl>);
+  DCHECK(voter_id_);
   DCHECK(context_);
   DCHECK(vote_.IsValid());
 }
@@ -701,8 +699,7 @@ bool AcceptedVote<VoteImpl>::HasReceipt(
 
 template <class VoteImpl>
 bool AcceptedVote<VoteImpl>::IsValid() const {
-  return consumer_ && voter_id_ != kInvalidVoterId<VoteImpl> &&
-         vote_.IsValid() && !invalidated_;
+  return consumer_ && voter_id_ && vote_.IsValid() && !invalidated_;
 }
 
 template <class VoteImpl>
@@ -777,7 +774,7 @@ void AcceptedVote<VoteImpl>::Take(AcceptedVote<VoteImpl>&& rhs) {
   DCHECK(!receipt_);
 
   consumer_ = std::exchange(rhs.consumer_, nullptr);
-  voter_id_ = std::exchange(rhs.voter_id_, kInvalidVoterId<VoteImpl>);
+  voter_id_ = std::exchange(rhs.voter_id_, VoterId<VoteImpl>());
   context_ = std::exchange(rhs.context_, nullptr);
   vote_ = std::exchange(rhs.vote_, VoteImpl());
   receipt_ = std::exchange(rhs.receipt_, nullptr);
@@ -822,17 +819,17 @@ VoteReceipt<VoteImpl> VotingChannel<VoteImpl>::SubmitVote(
 
 template <class VoteImpl>
 bool VotingChannel<VoteImpl>::IsValid() const {
-  return factory_ && voter_id_ != kInvalidVoterId<VoteImpl>;
+  return factory_ && voter_id_;
 }
 
 template <class VoteImpl>
 void VotingChannel<VoteImpl>::Reset() {
   if (!factory_)
     return;
-  DCHECK_NE(kInvalidVoterId<VoteImpl>, voter_id_);
+  DCHECK(voter_id_);
   factory_->OnVotingChannelDestroyed(PassKey());
   factory_ = nullptr;
-  voter_id_ = kInvalidVoterId<VoteImpl>;
+  voter_id_ = VoterId<VoteImpl>();
 }
 
 template <class VoteImpl>
@@ -846,7 +843,7 @@ template <class VoteImpl>
 void VotingChannel<VoteImpl>::Take(VotingChannel<VoteImpl>&& rhs) {
   Reset();
   factory_ = std::exchange(rhs.factory_, nullptr);
-  voter_id_ = std::exchange(rhs.voter_id_, kInvalidVoterId<VoteImpl>);
+  voter_id_ = std::exchange(rhs.voter_id_, VoterId<VoteImpl>());
 }
 
 /////////////////////////////////////////////////////////////////////
@@ -870,8 +867,12 @@ VotingChannel<VoteImpl> VotingChannelFactory<VoteImpl>::BuildVotingChannel() {
   ++voting_channels_outstanding_;
   // TODO(sebmarchand): Use VoterId<VoteImpl>::Generator instead of
   // FromUnsafeValue.
+  // Note: The pre-increment operator is used so that the value of the first
+  // voter ID is 1. This is required because 0 is the value for an invalid
+  // VoterId.
   VoterId<VoteImpl> new_voter_id =
       VoterId<VoteImpl>::FromUnsafeValue(++voting_channels_issued_);
+  DCHECK(!new_voter_id.is_null());
   return VotingChannel<VoteImpl>(
       base::PassKey<VotingChannelFactory<VoteImpl>>(), this, new_voter_id);
 }
