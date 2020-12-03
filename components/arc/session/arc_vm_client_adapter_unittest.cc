3cc7f0354c735b717448f36f4736524e69c91768
Ryo Hashimoto <hashimoto@chromium.org>
2020-12-03 04:04:50 +0000

    arc: Use arcvm-post-vm-start-services to control jobs
    BUG=b:171408795
    TEST=Launch Play Store
    
    Change-Id: I350bb9e05ecbd168a6452e76b59b601a27db8dc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560534
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833139}

--- components/arc/session/arc_vm_client_adapter_unittest.cc
+++ components/arc/session/arc_vm_client_adapter_unittest.cc
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace arc {
 namespace {
 
-constexpr const char kArcVmAdbdJobName[] = "arcvm_2dadbd";
 constexpr const char kArcVmPerBoardFeaturesJobName[] =
     "arcvm_2dper_2dboard_2dfeatures";
 constexpr const size_t kUnixMaxPathLen = sizeof(sockaddr_un::sun_path);
@@ -55,6 +54,8 @@ constexpr char kArcVmPreLoginServicesJobName[] =
     "arcvm_2dpre_2dlogin_2dservices";
 constexpr char kArcVmPostLoginServicesJobName[] =
     "arcvm_2dpost_2dlogin_2dservices";
+constexpr char kArcVmPostVmStartServicesJobName[] =
+    "arcvm_2dpost_2dvm_2dstart_2dservices";
 
 constexpr const char kUserIdHash[] = "this_is_a_valid_user_id_hash";
 constexpr const char kSerialNumber[] = "AAAABBBBCCCCDDDD1234";
@@ -763,14 +764,14 @@ TEST_F(ArcVmClientAdapterTest, UpgradeArc_StartArcVmPostLoginServicesFailure) {
   EXPECT_TRUE(arc_instance_stopped_called());
 }
 
-// Tests that UpgradeArc() handles arcvm-adbd stop failures properly.
-// Note that arcvm-adbd is restarted AFTER ARCVM is started.
-TEST_F(ArcVmClientAdapterTest, UpgradeArc_StopArcVmAdbdFailure) {
+// Tests that UpgradeArc() handles arcvm-post-vm-start-services stop failures
+// properly.
+TEST_F(ArcVmClientAdapterTest, UpgradeArc_StopArcVmPostVmStartServicesFailure) {
   SetValidUserInfo();
   StartMiniArc();
 
   // Inject failure to FakeUpstartClient.
-  InjectUpstartStopJobFailure(kArcVmAdbdJobName);
+  InjectUpstartStopJobFailure(kArcVmPostVmStartServicesJobName);
 
   // Upgrade should still succeed.
   UpgradeArc(true);
@@ -781,14 +782,15 @@ TEST_F(ArcVmClientAdapterTest, UpgradeArc_StopArcVmAdbdFailure) {
   EXPECT_FALSE(GetTestConciergeClient()->stop_vm_called());
 }
 
-// Tests that UpgradeArc() handles arcvm-adbd startup failures properly.
-// Note that arcvm-adbd is restarted AFTER ARCVM is started.
-TEST_F(ArcVmClientAdapterTest, UpgradeArc_StartArcVmAdbdFailure) {
+// Tests that UpgradeArc() handles arcvm-post-vm-start-services startup failures
+// properly.
+TEST_F(ArcVmClientAdapterTest,
+       UpgradeArc_StartArcVmPostVmStartServicesFailure) {
   SetValidUserInfo();
   StartMiniArc();
 
   // Inject failure to FakeUpstartClient.
-  InjectUpstartStartJobFailure(kArcVmAdbdJobName);
+  InjectUpstartStartJobFailure(kArcVmPostVmStartServicesJobName);
 
   EnableAdbOverUsbForTesting();
   UpgradeArc(false);
