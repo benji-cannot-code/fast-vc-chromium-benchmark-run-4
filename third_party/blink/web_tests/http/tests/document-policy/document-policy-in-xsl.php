0b6c5ed7f6d7f6d3cc9ffad5a1ec978a9eed4715
Charlie Hu <chenleihu@google.com>
2020-12-03 18:17:29 +0000

    [Document Policy] Fix Document Policy lost issue in XSLT documents
    This CL fixes the missing Document Policy issue for XSLT document.
    The fix is similar to the previous fix on Permissions Policy.
    See https://chromium-review.googlesource.com/c/chromium/src/+/2561144.
    
    Bug: 1151954
    Change-Id: Ie080c360ddc70ac46a63d5166b3830752ecdd94e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563440
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833361}

---
+++ third_party/blink/web_tests/http/tests/document-policy/document-policy-in-xsl.php
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("Document-Policy: oversized-images=2.0");
+header("Content-Type: application/xml");
+
+echo '<?xml version="1.0"?>
+<?xml-stylesheet href="resources/document-policy-in-xsl.xslt" type="text/xsl"?>
+<page>
+</page>';
+?>
