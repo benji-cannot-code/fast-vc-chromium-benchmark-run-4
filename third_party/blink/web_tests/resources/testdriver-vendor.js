1ad4b0506e82157da5f83ccb0bdc4236735d8698
Lan Wei <lanwei@chromium.org>
2020-12-03 03:46:21 +0000

    Simulate user inputs in input-events/input-events-get-target-ranges.html
    Use testdriver Action API to simulate key actions in
    input-events/input-events-get-target-ranges.html Added . We have to
    disable it because we do not support key actions in gpuBenchmarking
    when running the tests in our chromium bots.
    
    Bug: 1145677
    Change-Id: I5176f374aaa79d771a78ef651f63098f039101da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2533965
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833135}

--- third_party/blink/web_tests/resources/testdriver-vendor.js
+++ third_party/blink/web_tests/resources/testdriver-vendor.js
@@ -119,6 +119,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             eventSenderKeys = "ArrowDown";
           } else if (charCode == 0xE00C) {
             eventSenderKeys = "Escape";
+          } else if (charCode == 0xE003) {
+            eventSenderKeys = "Backspace";
           } else if (charCode == 0xE009) {
             eventSenderKeys = "ControlLeft";
             modifierValue = "ctrlKey";
