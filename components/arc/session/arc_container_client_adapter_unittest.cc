511d4db0faf9a5ccf2c732634ecb202c03cac252
Yury Khmel <khmel@google.com>
2020-12-03 04:43:47 +0000

    arc: Apply dalvik memory profiles for high-end devices (container).
    This sends request to ARC Android to set dalvik memory params based on
    device's memory config.
    
    BUG=b:160760318
    TEST=unit test added
         Locally on device, early tests show improvements in app loading and
         ARC boot time up to 5%. This feature is currently optional and is
         behind Chrome feature. Next data will be collected on real user
         using finch experiment and in-lab tests by adding separate testing
         variation for few reference boards that activates this feature.
    
    Change-Id: Iacb1a8c6ce874fe74152204a2e41a35bb2fa9da8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2570552
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Yury Khmel <khmel@google.com>
    Cr-Commit-Position: refs/heads/master@{#833154}

--- components/arc/session/arc_container_client_adapter_unittest.cc
+++ components/arc/session/arc_container_client_adapter_unittest.cc
@@ -8,9 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "chromeos/dbus/session_manager/fake_session_manager_client.h"
 #include "components/arc/session/arc_container_client_adapter.h"
+#include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace arc {
+
+namespace {
 
 class ArcContainerClientAdapterTest : public testing::Test {
  public:
@@ -23,6 +26,7 @@ class ArcContainerClientAdapterTest : public testing::Test {
   void SetUp() override {
     chromeos::SessionManagerClient::InitializeFake();
     client_adapter_ = CreateArcContainerClientAdapter();
+    chromeos::FakeSessionManagerClient::Get()->set_arc_available(true);
   }
 
   void TearDown() override {
@@ -35,6 +39,7 @@ class ArcContainerClientAdapterTest : public testing::Test {
 
  private:
   std::unique_ptr<ArcClientAdapter> client_adapter_;
+  content::BrowserTaskEnvironment browser_task_environment_;
 };
 
 // b/164816080 This test ensures that a new container instance that is
@@ -86,5 +91,52 @@ TEST_F(ArcContainerClientAdapterTest,
   EXPECT_TRUE(parent_observer.stopped_called());
   EXPECT_FALSE(child_observer.stopped_called());
 }
+
+struct DalvikMemoryProfileTestParam {
+  // Requested profile.
+  StartParams::DalvikMemoryProfile profile;
+  // Expected value passed to DBus.
+  login_manager::StartArcMiniContainerRequest_DalvikMemoryProfile expectation;
+};
+
+constexpr DalvikMemoryProfileTestParam kDalvikMemoryProfileTestCases[] = {
+    {StartParams::DalvikMemoryProfile::DEFAULT,
+     login_manager::
+         StartArcMiniContainerRequest_DalvikMemoryProfile_MEMORY_PROFILE_DEFAULT},
+    {StartParams::DalvikMemoryProfile::M4G,
+     login_manager::
+         StartArcMiniContainerRequest_DalvikMemoryProfile_MEMORY_PROFILE_4G},
+    {StartParams::DalvikMemoryProfile::M8G,
+     login_manager::
+         StartArcMiniContainerRequest_DalvikMemoryProfile_MEMORY_PROFILE_8G},
+    {StartParams::DalvikMemoryProfile::M16G,
+     login_manager::
+         StartArcMiniContainerRequest_DalvikMemoryProfile_MEMORY_PROFILE_16G}};
+
+class ArcContainerClientAdapterDalvikMemoryProfileTest
+    : public ArcContainerClientAdapterTest,
+      public testing::WithParamInterface<DalvikMemoryProfileTestParam> {};
+
+INSTANTIATE_TEST_SUITE_P(All,
+                         ArcContainerClientAdapterDalvikMemoryProfileTest,
+                         ::testing::ValuesIn(kDalvikMemoryProfileTestCases));
+
+void OnMiniInstanceStarted(bool result) {
+  DCHECK(result);
+}
+
+TEST_P(ArcContainerClientAdapterDalvikMemoryProfileTest, Profile) {
+  const auto& test_param = GetParam();
+  StartParams start_params;
+  start_params.dalvik_memory_profile = test_param.profile;
+  client_adapter()->StartMiniArc(std::move(start_params),
+                                 base::BindOnce(&OnMiniInstanceStarted));
+  const auto& request = chromeos::FakeSessionManagerClient::Get()
+                            ->last_start_arc_mini_container_request();
+  EXPECT_TRUE(request.has_dalvik_memory_profile());
+  EXPECT_EQ(test_param.expectation, request.dalvik_memory_profile());
+}
+
+}  // namespace
 
 }  // namespace arc
