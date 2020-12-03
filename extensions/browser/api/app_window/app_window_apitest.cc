73ffd2b2481cbc5575ebf65d668c5722e5895290
Robert Sesek <rsesek@chromium.org>
2020-12-03 21:27:00 +0000

    mac: Remove code to support OS X 10.10 in //extensions
    Bug: 1153883
    Change-Id: Ib3dede2c6cfad752cf044972419b1c64b6fc42db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2572176
    Auto-Submit: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833451}

--- extensions/browser/api/app_window/app_window_apitest.cc
+++ extensions/browser/api/app_window/app_window_apitest.cc
@@ -27,10 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/win/shell.h"
 #endif
 
-#if defined(OS_MAC)
-#include "base/mac/mac_util.h"
-#endif
-
 namespace extensions {
 
 using AppWindowApiTest = PlatformAppBrowserTest;
@@ -71,30 +67,18 @@ IN_PROC_BROWSER_TEST_F(ExperimentalAppWindowApiTest, SetIcon) {
 #endif  // defined(OS_LINUX) || defined(OS_CHROMEOS)
 
 IN_PROC_BROWSER_TEST_F(AppWindowApiTest, MAYBE_OnMinimizedEvent) {
-#if defined(OS_MAC)
-  if (base::mac::IsOS10_10())
-    return;  // Fails when swarmed. http://crbug.com/660582,
-#endif
   EXPECT_TRUE(RunExtensionTestWithArg("platform_apps/windows_api_properties",
                                       "minimized"))
       << message_;
 }
 
 IN_PROC_BROWSER_TEST_F(AppWindowApiTest, MAYBE_OnMaximizedEvent) {
-#if defined(OS_MAC)
-  if (base::mac::IsOS10_10())
-    return;  // Fails when swarmed. http://crbug.com/660582,
-#endif
   EXPECT_TRUE(RunExtensionTestWithArg("platform_apps/windows_api_properties",
                                       "maximized"))
       << message_;
 }
 
 IN_PROC_BROWSER_TEST_F(AppWindowApiTest, MAYBE_OnRestoredEvent) {
-#if defined(OS_MAC)
-  if (base::mac::IsOS10_10())
-    return;  // Fails when swarmed. http://crbug.com/660582,
-#endif
   EXPECT_TRUE(RunExtensionTestWithArg("platform_apps/windows_api_properties",
                                       "restored"))
       << message_;
