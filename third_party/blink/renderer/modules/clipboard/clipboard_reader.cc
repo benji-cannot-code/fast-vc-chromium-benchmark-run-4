c1d6d6a4a48be7466cff7d55d5751cf2fe20579f
Darwin Huang <huangdarwin@chromium.org>
2020-12-03 23:41:41 +0000

    Clipboard: Filter unsupported types when reading.
    Ensure that a format is valid before creating a placeholder Blob, so
    that clipboard.read() no longer returns nullptr blobs. In particular,
    filter out custom/pickled formats not supported by the Clipboard API
    
    Previously, custom/pickled formats not supported by the Clipboard API,
    but exposed via blink::SystemClipboard, would create nullptr Blobs when
    read via clipboard.read(), and return them in the output ClipboardItems.
    This output ClipboardItem could then be written via clipboard.write(),
    and cause a crash.
    
    Bug: 1153361
    Change-Id: Ib57ccca0e79adb09ee51e64c324c32283c788cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568818
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833501}

--- third_party/blink/renderer/modules/clipboard/clipboard_reader.cc
+++ third_party/blink/renderer/modules/clipboard/clipboard_reader.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/imagebitmap/image_bitmap.h"
 #include "third_party/blink/renderer/modules/clipboard/clipboard_promise.h"
+#include "third_party/blink/renderer/modules/clipboard/clipboard_writer.h"
 #include "third_party/blink/renderer/platform/image-encoders/image_encoder.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
@@ -304,6 +305,7 @@ class ClipboardSvgReader final : public ClipboardReader {
 ClipboardReader* ClipboardReader::Create(SystemClipboard* system_clipboard,
                                          const String& mime_type,
                                          ClipboardPromise* promise) {
+  DCHECK(ClipboardWriter::IsValidType(mime_type, /*is_raw=*/false));
   if (mime_type == kMimeTypeImagePng)
     return MakeGarbageCollected<ClipboardImageReader>(system_clipboard,
                                                       promise);
@@ -316,7 +318,9 @@ ClipboardReader* ClipboardReader::Create(SystemClipboard* system_clipboard,
   if (mime_type == kMimeTypeImageSvg &&
       RuntimeEnabledFeatures::ClipboardSvgEnabled())
     return MakeGarbageCollected<ClipboardSvgReader>(system_clipboard, promise);
-  // The MIME type is not supported.
+
+  NOTREACHED()
+      << "IsValidType() and Create() have inconsistent implementations.";
   return nullptr;
 }
 
