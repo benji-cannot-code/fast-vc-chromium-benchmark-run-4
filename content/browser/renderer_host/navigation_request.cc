124bcef1fc9138844ee284ca05f62c4eab458b14
Carlos IL <carlosil@chromium.org>
2020-12-03 03:13:41 +0000

    Reset ssl_info_ in failed navigation requests
    Previously ssl_info_ did not get reset on OnRequestFailedInternal if
    the response had no ssl info. This causes a bug if a valid HTTPS site
    (e.g. https://example.com) redirects to an invalid HTTP one (e.g.
    http://not-exist.test), the resulting error page (with
    http://not-exist.test on the omnibox) shows This Page is Secure if
    page info is opened, and shows https://example.com's certificate in
    the certificate viewer.
    This CL fixes this by setting ssl_info_ to nullopt in
    OnRequestFailedInternal if the URLLoaderCompletionStatus does not have
    ssl state set.
    
    Bug: 1154754
    Change-Id: I75b6539ff7e5695cda2f1b7f14fdb5054957b202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2569899
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Auto-Submit: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833125}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -2615,8 +2615,7 @@ void NavigationRequest::OnRequestFailedInternal(
   loader_.reset();
 
   common_params_->previews_state = blink::PreviewsTypes::PREVIEWS_OFF;
-  if (status.ssl_info.has_value())
-    ssl_info_ = status.ssl_info;
+  ssl_info_ = status.ssl_info;
 
   devtools_instrumentation::OnNavigationRequestFailed(*this, status);
 
