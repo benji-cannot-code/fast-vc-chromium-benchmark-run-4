1f3ad9bb6bae032992fc881ac2c9d1a706348488
Gavin Williams <gavinwill@chromium.org>
2020-12-03 22:07:11 +0000

    scanning: Show dialog when scan job fails
    Show the dialog when a scan job fails. The app returns to READY state
    once the dialog is closed.
    
    http://screen/6CS5wL2BB7v429a
    
    Bug: 1059779
    Change-Id: I937209ca0caf13bb00d0731248fe0d5bd991687c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2570411
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833474}

--- chromeos/components/scanning/resources/scanning_app.js
+++ chromeos/components/scanning/resources/scanning_app.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
 import 'chrome://resources/cr_elements/cr_toast/cr_toast.m.js';
 import 'chrome://resources/cr_elements/icons.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
@@ -272,14 +273,13 @@ Polymer({
    * @param {!mojoBase.mojom.FilePath} lastScannedFilePath
    */
   onScanComplete(success, lastScannedFilePath) {
-    if (success) {
-      this.lastScannedFilePath_ = lastScannedFilePath;
-      this.setAppState_(AppState.DONE);
+    if (!success) {
+      this.$.scanFailedDialog.showModal();
       return;
     }
 
-    this.statusText_ = 'Scan failed.';
-    this.setAppState_(AppState.READY);
+    this.lastScannedFilePath_ = lastScannedFilePath;
+    this.setAppState_(AppState.DONE);
   },
 
   /**
@@ -545,5 +545,11 @@ Polymer({
   /** @private */
   onFileNotFound_() {
     this.showToast_('fileNotFoundToastText');
+  },
+
+  /** @private */
+  onDialogOkClick_() {
+    this.$.scanFailedDialog.close();
+    this.setAppState_(AppState.READY);
   },
 });
