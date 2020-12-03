84f568f0928e60106ea74a85c56df811778ebd27
Patrick Monette <pmonette@chromium.org>
2020-12-03 06:52:52 +0000

    [PM] Add ExecutionContextPriorityDecorator
    This is a new class that takes care of setting up the voting tree that
    decides which priority an execution context will have depending on the
    votes of many voters.
    
    Bug: 971272
    Change-Id: Id4536e2180d7147357f80b706a8821eb44465145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538310
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833184}

--- components/performance_manager/graph_features_helper.cc
+++ components/performance_manager/graph_features_helper.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/decorators/frame_visibility_decorator.h"
 #include "components/performance_manager/decorators/page_load_tracker_decorator.h"
 #include "components/performance_manager/execution_context/execution_context_registry_impl.h"
+#include "components/performance_manager/execution_context_priority/execution_context_priority_decorator.h"
 #include "components/performance_manager/graph/frame_node_impl_describer.h"
 #include "components/performance_manager/graph/page_node_impl_describer.h"
 #include "components/performance_manager/graph/process_node_impl_describer.h"
@@ -60,8 +61,12 @@ void GraphFeaturesHelper::ConfigureGraph(Graph* graph) const {
     Install<SiteDataRecorder>(graph);
 #endif
 
-  // This class has a dependency on ExecutionContextRegistry, so must be
+  // These classes have a dependency on ExecutionContextRegistry, so must be
   // installed after it.
+  if (flags_.execution_context_priority_decorator) {
+    Install<execution_context_priority::ExecutionContextPriorityDecorator>(
+        graph);
+  }
   if (flags_.v8_context_tracker)
     Install<v8_memory::V8ContextTracker>(graph);
 }
