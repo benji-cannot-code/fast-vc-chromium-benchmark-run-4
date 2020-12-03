259f52d9c05fd6566b8941f13207b919a5acca14
Jeffrey Kardatzke <jkardatzke@google.com>
2020-12-03 21:35:50 +0000

    media: H264 CENCv1 support for VAAPI
    This adds handling of CENCv1 (full sample encryption) streams for H264
    with the VAAPI accelerator. The decoder detects when this case occurs
    and then offloads slice header parsing to the accelerator.
    
    BUG=b:153111783,b:155508443
    TEST=Unit tests pass, CENCv1 playback works
    
    Change-Id: Ieb913935269163d09d24b88a8ea987a6e8ffce33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2552634
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833458}

--- media/gpu/vaapi/h264_vaapi_video_decoder_delegate.h
+++ media/gpu/vaapi/h264_vaapi_video_decoder_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_VAAPI_H264_VAAPI_VIDEO_DECODER_DELEGATE_H_
 #define MEDIA_GPU_VAAPI_H264_VAAPI_VIDEO_DECODER_DELEGATE_H_
 
+#include "base/atomic_sequence_num.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/sequence_checker.h"
 #include "media/gpu/h264_decoder.h"
@@ -40,6 +41,13 @@ class H264VaapiVideoDecoderDelegate : public H264Decoder::H264Accelerator,
                              const H264Picture::Vector& ref_pic_listb0,
                              const H264Picture::Vector& ref_pic_listb1,
                              scoped_refptr<H264Picture> pic) override;
+  Status ParseEncryptedSliceHeader(
+      const uint8_t* data,
+      size_t size,
+      const std::vector<SubsampleEntry>& subsamples,
+      const std::vector<uint8_t>& sps_nalu_data,
+      const std::vector<uint8_t>& pps_nalu_data,
+      H264SliceHeader* slice_header_out) override;
   Status SubmitSlice(const H264PPS* pps,
                      const H264SliceHeader* slice_hdr,
                      const H264Picture::Vector& ref_pic_list0,
