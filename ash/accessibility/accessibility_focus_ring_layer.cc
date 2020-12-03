02159d9194de4af98ef24ae69c27ebc7ddb0ddcc
Joel Riley <joelriley@google.com>
2020-12-03 04:44:37 +0000

    Move focus rings and highlight layers under accessibility bubbles.
    Will allow Select-to-speak panel UI to appear above focus ring and highlight layers. This also avoids the need to create more window containers.
    
    Bug: 1143814
    Change-Id: I1c4fce614404c6cabd0336eed2ab6fa7ac1d4c48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568547
    Commit-Queue: Joel Riley <joelriley@google.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833155}

--- ash/accessibility/accessibility_focus_ring_layer.cc
+++ ash/accessibility/accessibility_focus_ring_layer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/accessibility/accessibility_focus_ring_layer.h"
 
+#include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
 #include "base/bind.h"
 #include "third_party/skia/include/core/SkPaint.h"
@@ -101,6 +102,8 @@ void AccessibilityFocusRingLayer::Set(const AccessibilityFocusRing& ring) {
   display::Display display =
       display::Screen::GetScreen()->GetDisplayMatching(bounds);
   aura::Window* root_window = Shell::GetRootWindowForDisplayId(display.id());
+  aura::Window* container = Shell::GetContainer(
+      root_window, kShellWindowId_AccessibilityPanelContainer);
 
   if (SkColorGetA(background_color_) > 0) {
     bounds = display.bounds();
@@ -108,8 +111,8 @@ void AccessibilityFocusRingLayer::Set(const AccessibilityFocusRing& ring) {
     int inset = kGradientWidth;
     bounds.Inset(-inset, -inset, -inset, -inset);
   }
-  ::wm::ConvertRectFromScreen(root_window, &bounds);
-  CreateOrUpdateLayer(root_window, "AccessibilityFocusRing", bounds);
+  ::wm::ConvertRectFromScreen(container, &bounds);
+  CreateOrUpdateLayer(container, "AccessibilityFocusRing", bounds);
 }
 
 void AccessibilityFocusRingLayer::SetAppearance(FocusRingType type,
