7605806cd6c3bbd32985b51aa0d514264815849a
Elly Fong-Jones <ellyjones@google.com>
2020-12-03 17:11:52 +0000

    mac: disable some capture unittests on arm64
    These tests are being bulk-disabled as part of arm64 bot stabilization.
    
    Bug: 1154345
    Change-Id: I903627c4ea56a175716a9618b73e1ed6fd330531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2570195
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Evan Shrubsole <eshr@google.com>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833336}

--- media/capture/video/mac/sample_buffer_transformer_mac_unittest.mm
+++ media/capture/video/mac/sample_buffer_transformer_mac_unittest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "media/capture/video/mac/test/pixel_buffer_test_utils_mac.h"
 #include "media/capture/video/mac/video_capture_device_avfoundation_utils_mac.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -396,7 +397,14 @@ TEST_P(SampleBufferTransformerPixelTransferTest, CanConvertFullScale) {
       PixelBufferIsSingleColor(output_pixel_buffer, kColorR, kColorG, kColorB));
 }
 
-TEST_P(SampleBufferTransformerPixelTransferTest, CanConvertAndScaleDown) {
+#if defined(ARCH_CPU_ARM64)
+// Bulk-disabled for arm64 bot stabilization: https://crbug.com/1154345
+#define MAYBE_CanConvertAndScaleDown DISABLED_CanConvertAndScaleDown
+#else
+#define MAYBE_CanConvertAndScaleDown CanConvertAndScaleDown
+#endif
+
+TEST_P(SampleBufferTransformerPixelTransferTest, MAYBE_CanConvertAndScaleDown) {
   OSType input_pixel_format;
   OSType output_pixel_format;
   std::tie(input_pixel_format, output_pixel_format) = GetParam();
