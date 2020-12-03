18e1147d1338163901bd57d8623afbc40115b518
Maria Petrisor <mpetrisor@chromium.org>
2020-12-03 19:19:22 +0000

    Add software fallback for system token
    The fallback will be enabled on Neverware devices using the added build
    flag: system_slot_software_fallback. Its default value is false and this
    behavior is not intended for Chromebooks which always have a TPM.
    
    In case Neverware enables the build flag:
      (a) Devices with a TPM will still initialize a TPM-backed system slot.
      (b) Devices without a TPM will initialize a software-backed system
          slot.
    
    DD: https://docs.google.com/document/d/1rbTiAMpR18SoN0be_AW-ysvptAHbYcTYeZHL6KcAcTQ/edit?usp=sharing
    Initial bug discussion: https://docs.google.com/document/d/16w4xyCi2tQTIBtS8o4Ga01KxpHwICVnCjyGC6ezlNS0/edit?usp=sharing
    
    Bug: b:171461831
    Change-Id: I533a7de7bf5367143aa539b9a7883abd6d8f405a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2552600
    Commit-Queue: Maria Petrisor <mpetrisor@chromium.org>
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833385}

--- chromeos/tpm/tpm_token_info_getter.h
+++ chromeos/tpm/tpm_token_info_getter.h
@@ -53,6 +53,8 @@ class COMPONENT_EXPORT(CHROMEOS_TPM) TPMTokenInfoGetter {
   // called).
   void Start(TpmTokenInfoCallback callback);
 
+  void SetSystemSlotSoftwareFallback(bool use_system_slot_software_fallback);
+
  private:
   enum Type {
     TYPE_SYSTEM,
@@ -63,6 +65,7 @@ class COMPONENT_EXPORT(CHROMEOS_TPM) TPMTokenInfoGetter {
     STATE_INITIAL,
     STATE_STARTED,
     STATE_TPM_ENABLED,
+    STATE_SYSTEM_SLOT_SOFTWARE_FALLBACK,
     STATE_DONE
   };
 
@@ -100,6 +103,12 @@ class COMPONENT_EXPORT(CHROMEOS_TPM) TPMTokenInfoGetter {
   AccountId account_id_;
 
   TpmTokenInfoCallback callback_;
+
+  // If set and the TPM is disabled, TPMTokenInfoGetter will still get the token
+  // info using cryptohome's Pkcs11GetTpmTokenInfo query. The token info is
+  // needed for falling back to a software-backed initialization of the system
+  // token.
+  bool use_system_slot_software_fallback_ = false;
 
   // The current request delay before the next attempt to initialize the
   // TPM. Will be adapted after each attempt.
