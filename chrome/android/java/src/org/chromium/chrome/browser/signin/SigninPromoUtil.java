c0e638e3aced14726ee6089fdd17c56baab1f57c
Alice Wang <aliceywang@chromium.org>
2020-12-03 09:31:15 +0000

    [Android][Modularization] Move DisplayableProfileData to signin.services target
    This CL moves DisplayableProfileData to signin.services target.
    
    Bug: 1153047
    Change-Id: If992b97b7373e4d0e55e2aa10e9793479bf326f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567916
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833227}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninPromoUtil.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninPromoUtil.java
@@ -17,6 +17,7 @@ import org.chromium.base.annotations.CalledByNative;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.signin.services.DisplayableProfileData;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.services.SigninPreferencesManager;
 import org.chromium.chrome.browser.version.ChromeVersionInfo;
