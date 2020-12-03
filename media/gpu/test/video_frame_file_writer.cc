2fd92c45ec27751de3b7899e94d42b651c3b671f
Hirokazu Honda <hiroh@chromium.org>
2020-12-03 02:08:26 +0000

    media/gpu/video_encode_accelerator_tests: Validates each temporal layer stream
    The test originally validates the entire bitstream. This CL
    enables the test to validate each temporal layer stream. For
    instance, when --num_temporal_layers=3, the test validates three
    streams, TL0, TL0+TL1 and TL0+TL1+TL2.
    BitstreamFileWriter and VideoFrameFileWriter ignore bitstreams
    and frames in upper layers than to be processed by them.
    VideoFrameFileWriter also writes a file with a different prefix
    from other VideoFrameFileWriter, so that multiple instances don't
    write to the same file names.
    
    Bug: b:174318867
    Test: video_encode_accelerator_tests --codec=vp9 --num_temporal_layers=3 w/o output_bitstream --output_images=all
    Change-Id: Idf2a1e4d2a50e11ca359435ec4495a43b61139f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2562801
    Reviewed-by: David Staessens <dstaessens@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833103}

--- media/gpu/test/video_frame_file_writer.cc
+++ media/gpu/test/video_frame_file_writer.cc
@@ -25,12 +25,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 namespace test {
 
-VideoFrameFileWriter::VideoFrameFileWriter(const base::FilePath& output_folder,
-                                           OutputFormat output_format,
-                                           size_t output_limit)
+VideoFrameFileWriter::VideoFrameFileWriter(
+    const base::FilePath& output_folder,
+    OutputFormat output_format,
+    size_t output_limit,
+    const base::FilePath::StringType& output_file_prefix)
     : output_folder_(output_folder),
       output_format_(output_format),
       output_limit_(output_limit),
+      output_file_prefix_(output_file_prefix),
       num_frames_writing_(0),
       frame_writer_thread_("FrameWriterThread"),
       frame_writer_cv_(&frame_writer_lock_) {
@@ -49,7 +52,8 @@ VideoFrameFileWriter::~VideoFrameFileWriter() {
 std::unique_ptr<VideoFrameFileWriter> VideoFrameFileWriter::Create(
     const base::FilePath& output_folder,
     OutputFormat output_format,
-    size_t output_limit) {
+    size_t output_limit,
+    const base::FilePath::StringType& output_file_prefix) {
   // If the directory is not absolute, consider it relative to our working dir.
   base::FilePath resolved_output_folder(output_folder);
   if (!resolved_output_folder.IsAbsolute()) {
@@ -68,7 +72,7 @@ std::unique_ptr<VideoFrameFileWriter> VideoFrameFileWriter::Create(
   }
 
   auto frame_file_writer = base::WrapUnique(new VideoFrameFileWriter(
-      resolved_output_folder, output_format, output_limit));
+      resolved_output_folder, output_format, output_limit, output_file_prefix));
   if (!frame_file_writer->Initialize()) {
     LOG(ERROR) << "Failed to initialize VideoFrameFileWriter";
     return nullptr;
@@ -130,6 +134,9 @@ void VideoFrameFileWriter::ProcessVideoFrameTask(
   const gfx::Size& visible_size = video_frame->visible_rect().size();
   base::SStringPrintf(&filename, FILE_PATH_LITERAL("frame_%04zu_%dx%d"),
                       frame_index, visible_size.width(), visible_size.height());
+  if (!output_file_prefix_.empty())
+    filename = output_file_prefix_ + "_" + filename;
+
   // Copies to |frame| in this function so that |video_frame| stays alive until
   // in the end of function.
   auto frame = video_frame;
