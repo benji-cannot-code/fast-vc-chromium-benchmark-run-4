4172bf23ed4545498c0ddbe1999fb36bf8c27239
Adam Langley <agl@chromium.org>
2020-12-03 01:45:46 +0000

    Forward declare enum to save a #include.
    This #include pulls in quite a lot of headers in render_frame_host.h, a
    fairly core header.
    
    Change-Id: I17288f2e166ec15880ebac6a11f07c574b16d94b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2553021
    Commit-Queue: Adam Langley <agl@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833096}

--- content/public/browser/render_frame_host.h
+++ content/public/browser/render_frame_host.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/frame/sudden_termination_disabler_type.mojom.h"
 #include "third_party/blink/public/mojom/frame/user_activation_notification_type.mojom.h"
 #include "third_party/blink/public/mojom/loader/pause_subresource_loading_handle.mojom-forward.h"
-#include "third_party/blink/public/mojom/webauthn/authenticator.mojom.h"
 #include "ui/accessibility/ax_tree_id.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/native_widget_types.h"
@@ -45,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 class AssociatedInterfaceProvider;
 namespace mojom {
+enum class AuthenticatorStatus;
 enum class FeaturePolicyFeature;
 }  // namespace mojom
 }  // namespace blink
