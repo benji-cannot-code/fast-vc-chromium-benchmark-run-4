8ab6c56d3de8eae3667414f24820398da6a44e6a
Kai Ninomiya <kainino@chromium.org>
2020-12-03 20:20:46 +0000

    Roll WebGPU CTS
    Bug: None
    Change-Id: Id90263faf4aade3b39742cb0db6c5c9109429e2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2570206
    Auto-Submit: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833413}

--- third_party/blink/web_tests/wpt_internal/webgpu/common/framework/version.js
+++ third_party/blink/web_tests/wpt_internal/webgpu/common/framework/version.js
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'a8196844ac6228fea3560b82d29e01fed2fbf13a';
+export const version = '6d2646eeb60e313cacbe43b266cf32948a4fef7e';
