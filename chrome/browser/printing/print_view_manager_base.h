715cafe34b907d72c2996add042f90bba2b5a20f
Julie Jeongeun Kim <jkim@igalia.com>
2020-12-03 06:40:47 +0000

    [printing] Convert PrintHostMsg_DidPrintDocument to Mojo
    This CL converts PrintHostMsg_DidPrintDocument message to
    DidPrintDocument() in mojom::PrintManagerHost.
    
    Bug: 1008939
    Change-Id: I80a7dddda4226c6158ffae505b0368ec2a0a4d69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2544295
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Wei Li <weili@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#833180}

--- chrome/browser/printing/print_view_manager_base.h
+++ chrome/browser/printing/print_view_manager_base.h
@@ -78,6 +78,8 @@ class PrintViewManagerBase : public content::NotificationObserver,
 
   // mojom::PrintManagerHost:
   void DidGetPrintedPagesCount(int32_t cookie, uint32_t number_pages) override;
+  void DidPrintDocument(mojom::DidPrintDocumentParamsPtr params,
+                        DidPrintDocumentCallback callback) override;
 #if BUILDFLAG(ENABLE_TAGGED_PDF)
   void SetAccessibilityTree(
       int32_t cookie,
@@ -134,10 +136,6 @@ class PrintViewManagerBase : public content::NotificationObserver,
   void NavigationStopped() override;
 
   // printing::PrintManager:
-  void OnDidPrintDocument(
-      content::RenderFrameHost* render_frame_host,
-      const mojom::DidPrintDocumentParams& params,
-      std::unique_ptr<DelayedFrameDispatchHelper> helper) override;
   void OnScriptedPrint(content::RenderFrameHost* render_frame_host,
                        const mojom::ScriptedPrintParams& params,
                        IPC::Message* reply_msg) override;
@@ -146,7 +144,7 @@ class PrintViewManagerBase : public content::NotificationObserver,
   void OnComposePdfDone(const gfx::Size& page_size,
                         const gfx::Rect& content_area,
                         const gfx::Point& physical_offsets,
-                        std::unique_ptr<DelayedFrameDispatchHelper> helper,
+                        DidPrintDocumentCallback callback,
                         mojom::PrintCompositor::Status status,
                         base::ReadOnlySharedMemoryRegion region);
 
