64d90d1111500d1df60bf988f5f7e28c1eeb1710
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-12-03 20:42:54 +0000

    [CrOS cellular] Add remove esim profile dialog
    - Screenshot are added in follow up CL
    https://chromium-review.googlesource.com/c/chromium/src/+/2569784/
    
    - Added esim_remove_profile_dialog to internet page to allow for
      testing.
    
    Bug: 1093185
    Change-Id: I227c56f1ff1b8cca48d0d674f617966a22ba5bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566711
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/master@{#833427}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/fake_esim_manager_remote.js
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/fake_esim_manager_remote.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 cr.define('cellular_setup', function() {
   /** @implements {chromeos.cellularSetup.mojom.ESimProfile} */
   class FakeProfile {
-    constructor(id) {
+    constructor(id, fakeEuicc) {
       this.properties_ = {
         activationCode: 'activation-code-' + id,
         eid: '1',
@@ -23,6 +23,8 @@ cr.define('cellular_setup', function() {
         },
         state: chromeos.cellularSetup.mojom.ProfileState.kPending,
       };
+
+      this.fakeEuicc_ = fakeEuicc;
     }
 
     /**
@@ -83,6 +85,22 @@ cr.define('cellular_setup', function() {
         });
       });
     }
+
+    /** @override */
+    uninstallProfile() {
+      return this.fakeEuicc_.removeProfileForTest(this.properties_.iccid)
+          .then(saved => {
+            if (saved) {
+              return {
+                result:
+                    chromeos.cellularSetup.mojom.ESimOperationResult.kSuccess
+              };
+            }
+            return {
+              result: chromeos.cellularSetup.mojom.ESimOperationResult.kFailure
+            };
+          });
+    }
   }
 
   /** @implements {chromeos.cellularSetup.mojom.Euicc} */
@@ -140,7 +158,27 @@ cr.define('cellular_setup', function() {
     /** @private */
     addProfileForTest_() {
       const id = this.profiles_.length + 1;
-      this.profiles_.push(new FakeProfile(id));
+      this.profiles_.push(new FakeProfile(id, this));
+    }
+
+    /**
+     * @param {string} iccid
+     * @private
+     */
+    async removeProfileForTest(iccid) {
+      const result = [];
+      let resp = false;
+      for (let profile of this.profiles_) {
+        const property = await profile.getProperties();
+        if (property.properties.iccid === iccid) {
+          resp = true;
+          continue;
+        }
+        result.push(profile);
+      }
+      this.profiles_ = result;
+
+      return resp;
     }
   }
 
