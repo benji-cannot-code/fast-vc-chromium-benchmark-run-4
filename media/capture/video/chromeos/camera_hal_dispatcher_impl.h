55d623fa971eb6e7965e0b59a9462e87d8826cb0
Jasmine Chen <lnishan@google.com>
2020-12-04 04:53:52 +0000

    VCD: Use ObserverListThreadSafe for CameraActiveClientObserver
    Use ObserverListThreadSafe for CameraActiveClientObserver instead of
    ObserverList. Also add a lock to guard the list.
    
    Bug: b/170075468
    Test: simplechrome
    Change-Id: I976200eed4e24f036e96481a145250aa90fdc3fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566329
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Jasmine Chen <lnishan@google.com>
    Cr-Commit-Position: refs/heads/master@{#833613}

--- media/capture/video/chromeos/camera_hal_dispatcher_impl.h
+++ media/capture/video/chromeos/camera_hal_dispatcher_impl.h
@@ -13,10 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/containers/unique_ptr_adapters.h"
 #include "base/files/scoped_file.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/memory/singleton.h"
-#include "base/observer_list.h"
+#include "base/observer_list_threadsafe.h"
 #include "base/observer_list_types.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/thread_annotations.h"
 #include "base/threading/thread.h"
 #include "base/unguessable_token.h"
 #include "components/chromeos_camera/common/jpeg_encode_accelerator.mojom.h"
@@ -191,14 +193,6 @@ class CAPTURE_EXPORT CameraHalDispatcherImpl final
       std::unique_ptr<CameraClientObserver> observer,
       base::OnceCallback<void(int32_t)> result_callback);
 
-  void AddActiveClientObserverOnProxyThread(
-      CameraActiveClientObserver* observer,
-      base::WaitableEvent* observer_added_event);
-
-  void RemoveActiveClientObserverOnProxyThread(
-      CameraActiveClientObserver* observer,
-      base::WaitableEvent* observer_removed_event);
-
   void EstablishMojoChannel(CameraClientObserver* client_observer);
 
   // Handler for incoming Mojo connection on the unix domain socket.
@@ -240,9 +234,12 @@ class CAPTURE_EXPORT CameraHalDispatcherImpl final
 
   TokenManager token_manager_;
 
+  base::Lock opened_camera_id_map_lock_;
   base::flat_map<cros::mojom::CameraClientType, base::flat_set<int32_t>>
-      opened_camera_id_map_;
-  base::ObserverList<CameraActiveClientObserver> active_client_observers_;
+      opened_camera_id_map_ GUARDED_BY(opened_camera_id_map_lock_);
+
+  scoped_refptr<base::ObserverListThreadSafe<CameraActiveClientObserver>>
+      active_client_observers_;
 
   DISALLOW_COPY_AND_ASSIGN(CameraHalDispatcherImpl);
 };
