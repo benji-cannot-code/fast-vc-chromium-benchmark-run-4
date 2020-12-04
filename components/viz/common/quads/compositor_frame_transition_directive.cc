931ad6274bc41bb17097856a3436704dd954bed6
Vladimir Levin <vmpstr@chromium.org>
2020-12-04 17:58:27 +0000

    [DocumentTransition]: Add viz transition directive.
    This patch adds a transition directive class to viz. It isn't currently
    used outside of unittests, but will be referenced in cc and indirectly
    from Blink in order to construct the right directive for viz to execute.
    
    R=kylechar@chromium.org
    
    Bug: 1150461
    Change-Id: I9cbe63507148405b0a363ee6d957d76b1a18b34e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568985
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833759}

---
+++ components/viz/common/quads/compositor_frame_transition_directive.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/viz/common/quads/compositor_frame_transition_directive.h"
+
+#include "base/time/time.h"
+
+namespace viz {
+
+constexpr base::TimeDelta CompositorFrameTransitionDirective::kMaxDuration;
+
+CompositorFrameTransitionDirective::CompositorFrameTransitionDirective(
+    uint32_t sequence_id,
+    Type type,
+    Effect effect,
+    base::TimeDelta duration)
+    : sequence_id_(sequence_id),
+      type_(type),
+      effect_(effect),
+      duration_(duration) {
+  DCHECK_LE(duration_, kMaxDuration);
+}
+
+}  // namespace viz
