8c58cb9b7661f3bcc006e8b1ae4b16eb123cd5fb
Robert Sesek <rsesek@chromium.org>
2020-12-04 18:15:57 +0000

    mac: Remove code to support OS X 10.10 in //components/metal_util
    Bug: 1153883
    Change-Id: Ieb2ee69ba93e0469cbe307dfb1f12687a0a1a702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2572379
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833778}

--- components/metal_util/device.mm
+++ components/metal_util/device.mm
@@ -13,28 +13,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace metal {
 
 MTLDevicePtr CreateDefaultDevice() {
-  if (@available(macOS 10.11, *)) {
-    // First attempt to find a low power device to use.
-    base::scoped_nsprotocol<id<MTLDevice>> device_to_use;
-    base::scoped_nsobject<NSArray<id<MTLDevice>>> devices(MTLCopyAllDevices());
-    for (id<MTLDevice> device in devices.get()) {
-      if ([device isLowPower]) {
-        device_to_use.reset(device, base::scoped_policy::RETAIN);
-        break;
-      }
+  // First attempt to find a low power device to use.
+  base::scoped_nsprotocol<id<MTLDevice>> device_to_use;
+  base::scoped_nsobject<NSArray<id<MTLDevice>>> devices(MTLCopyAllDevices());
+  for (id<MTLDevice> device in devices.get()) {
+    if ([device isLowPower]) {
+      device_to_use.reset(device, base::scoped_policy::RETAIN);
+      break;
     }
-    // Failing that, use the system default device.
-    if (!device_to_use)
-      device_to_use.reset(MTLCreateSystemDefaultDevice());
-    if (!device_to_use) {
-      DLOG(ERROR) << "Failed to find MTLDevice.";
-      return nullptr;
-    }
-    return device_to_use.release();
   }
-  // If no device was found, or if the macOS version is too old for Metal,
-  // return no context provider.
-  return nullptr;
+  // Failing that, use the system default device.
+  if (!device_to_use)
+    device_to_use.reset(MTLCreateSystemDefaultDevice());
+  if (!device_to_use) {
+    DLOG(ERROR) << "Failed to find MTLDevice.";
+    return nullptr;
+  }
+  return device_to_use.release();
 }
 
 }  // namespace metal
