b6bcf97114595b8f94f1c63447cc04e03f6172d1
Robert Sesek <rsesek@chromium.org>
2020-12-04 14:53:01 +0000

    mac: Remove code to support OS X 10.10 in //base
    Bug: 1153883
    Change-Id: I671164099c29cc91680d5f489d3c34723edb5675
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2572887
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833710}

--- base/mac/mac_util_unittest.mm
+++ base/mac/mac_util_unittest.mm
@@ -175,23 +175,10 @@ TEST_F(MacUtilTest, IsOSEllipsis) {
   EXPECT_FALSE(IsAtLeastOS##V());
 
   if (major == 10) {
-    if (minor == 10) {
-      EXPECT_TRUE(IsOS10_10());
-      EXPECT_TRUE(IsAtMostOS10_10());
+    if (minor == 11) {
+      EXPECT_TRUE(IsOS10_11());
+      EXPECT_TRUE(IsAtMostOS10_11());
 
-      TEST_FOR_FUTURE_10_OS(11);
-      TEST_FOR_FUTURE_10_OS(12);
-      TEST_FOR_FUTURE_10_OS(13);
-      TEST_FOR_FUTURE_10_OS(14);
-      TEST_FOR_FUTURE_10_OS(15);
-      TEST_FOR_FUTURE_OS(11);
-
-      EXPECT_FALSE(IsOSLaterThan11_DontCallThis());
-    } else if (minor == 11) {
-      EXPECT_FALSE(IsOS10_10());
-      EXPECT_FALSE(IsAtMostOS10_10());
-
-      TEST_FOR_SAME_10_OS(11);
       TEST_FOR_FUTURE_10_OS(12);
       TEST_FOR_FUTURE_10_OS(13);
       TEST_FOR_FUTURE_10_OS(14);
@@ -200,10 +187,9 @@ TEST_F(MacUtilTest, IsOSEllipsis) {
 
       EXPECT_FALSE(IsOSLaterThan11_DontCallThis());
     } else if (minor == 12) {
-      EXPECT_FALSE(IsOS10_10());
-      EXPECT_FALSE(IsAtMostOS10_10());
+      EXPECT_FALSE(IsOS10_11());
+      EXPECT_FALSE(IsAtMostOS10_11());
 
-      TEST_FOR_PAST_10_OS(11);
       TEST_FOR_SAME_10_OS(12);
       TEST_FOR_FUTURE_10_OS(13);
       TEST_FOR_FUTURE_10_OS(14);
@@ -212,10 +198,9 @@ TEST_F(MacUtilTest, IsOSEllipsis) {
 
       EXPECT_FALSE(IsOSLaterThan11_DontCallThis());
     } else if (minor == 13) {
-      EXPECT_FALSE(IsOS10_10());
-      EXPECT_FALSE(IsAtMostOS10_10());
+      EXPECT_FALSE(IsOS10_11());
+      EXPECT_FALSE(IsAtMostOS10_11());
 
-      TEST_FOR_PAST_10_OS(11);
       TEST_FOR_PAST_10_OS(12);
       TEST_FOR_SAME_10_OS(13);
       TEST_FOR_FUTURE_10_OS(14);
@@ -224,10 +209,9 @@ TEST_F(MacUtilTest, IsOSEllipsis) {
 
       EXPECT_FALSE(IsOSLaterThan11_DontCallThis());
     } else if (minor == 14) {
-      EXPECT_FALSE(IsOS10_10());
-      EXPECT_FALSE(IsAtMostOS10_10());
+      EXPECT_FALSE(IsOS10_11());
+      EXPECT_FALSE(IsAtMostOS10_11());
 
-      TEST_FOR_PAST_10_OS(11);
       TEST_FOR_PAST_10_OS(12);
       TEST_FOR_PAST_10_OS(13);
       TEST_FOR_SAME_10_OS(14);
@@ -236,10 +220,9 @@ TEST_F(MacUtilTest, IsOSEllipsis) {
 
       EXPECT_FALSE(IsOSLaterThan11_DontCallThis());
     } else if (minor == 15) {
-      EXPECT_FALSE(IsOS10_10());
-      EXPECT_FALSE(IsAtMostOS10_10());
+      EXPECT_FALSE(IsOS10_11());
+      EXPECT_FALSE(IsAtMostOS10_11());
 
-      TEST_FOR_PAST_10_OS(11);
       TEST_FOR_PAST_10_OS(12);
       TEST_FOR_PAST_10_OS(13);
       TEST_FOR_PAST_10_OS(14);
@@ -252,10 +235,9 @@ TEST_F(MacUtilTest, IsOSEllipsis) {
       EXPECT_TRUE(false);
     }
   } else if (major == 11) {
-    EXPECT_FALSE(IsOS10_10());
-    EXPECT_FALSE(IsAtMostOS10_10());
+    EXPECT_FALSE(IsOS10_11());
+    EXPECT_FALSE(IsAtMostOS10_11());
 
-    TEST_FOR_PAST_10_OS(11);
     TEST_FOR_PAST_10_OS(12);
     TEST_FOR_PAST_10_OS(13);
     TEST_FOR_PAST_10_OS(14);
