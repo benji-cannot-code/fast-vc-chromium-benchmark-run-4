913ba964fe5dee94bae6bb0ba0d5842adca483a0
Andrew Grieve <agrieve@chromium.org>
2020-12-04 20:58:55 +0000

    r8/README.chromium: Add filing bug instructions
    Bug: None
    Change-Id: I3473e5798ea1b790a22adecd033777637aca023b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2574829
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833851}

--- build/android/gyp/proguard.py
+++ build/android/gyp/proguard.py
@@ -119,6 +119,10 @@ def _ParseOptions():
   parser.add_argument('--show-desugar-default-interface-warnings',
                       action='store_true',
                       help='Enable desugaring warnings.')
+  parser.add_argument('--dump-inputs',
+                      action='store_true',
+                      help='Use when filing R8 bugs to capture inputs.'
+                      ' Stores inputs to r8inputs.zip')
   parser.add_argument(
       '--stamp',
       help='File to touch upon success. Mutually exclusive with --output-path')
@@ -239,6 +243,8 @@ def _OptimizeWithR8(options,
     ]
     if options.disable_outlining:
       cmd += ['-Dcom.android.tools.r8.disableOutlining=1']
+    if options.dump_inputs:
+      cmd += ['-Dcom.android.tools.r8.dumpinputtofile=r8inputs.zip']
     cmd += [
         '-cp',
         options.r8_path,
