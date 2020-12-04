2eba14da80aa5f0de1c8c2248a83b0d6948b22d2
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-12-04 18:05:59 +0000

    Move Aura deep scan cursor bug fix to ContentAnalysisDialog
    This should address the concerns raised in crrev.com/c/2391514 without
    regressing crbug.com/1100902.
    
    Bug: 1151724, 1139050, 1100902
    Change-Id: I2180278fec307555eb9714aed6f3644520838326
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2556587
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833765}

--- chrome/browser/enterprise/connectors/content_analysis_dialog.cc
+++ chrome/browser/enterprise/connectors/content_analysis_dialog.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_types.h"
@@ -210,6 +211,20 @@ void ContentAnalysisDialog::AcceptButtonCallback() {
 void ContentAnalysisDialog::CancelButtonCallback() {
   if (delegate_)
     delegate_->Cancel(is_warning());
+}
+
+void ContentAnalysisDialog::SuccessCallback() {
+#if defined(USE_AURA)
+  if (web_contents_) {
+    // It's possible focus has been lost and gained back incorrectly if the user
+    // clicked on the page between the time the scan started and the time the
+    // dialog closes. This results in the behaviour detailed in
+    // crbug.com/1139050. The fix is to preemptively take back focus when this
+    // dialog closes on its own.
+    web_contents_->SetIgnoreInputEvents(false);
+    web_contents_->Focus();
+  }
+#endif
 }
 
 bool ContentAnalysisDialog::ShouldShowCloseButton() const {
@@ -461,6 +476,9 @@ void ContentAnalysisDialog::SetupButtons() {
   } else {
     // Include no buttons otherwise.
     DialogDelegate::SetButtons(ui::DIALOG_BUTTON_NONE);
+    DialogDelegate::SetCancelCallback(
+        base::BindOnce(&ContentAnalysisDialog::SuccessCallback,
+                       weak_ptr_factory_.GetWeakPtr()));
   }
 }
 
