6b36423ff0d753153e079b94580aee702b5e089c
Stephen McGruer <smcgruer@chromium.org>
2020-12-04 20:20:26 +0000

    Import wpt@eb572878163035359e8480d439e7aeb224e51802
    Using wpt-import in Chromium 1aa111f7272fac1423609bb2021c71705e86f860.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    cbiesinger@chromium.org:
      external/wpt/css/css-flexbox
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    mstensho@chromium.org:
      external/wpt/css/css-break
      external/wpt/css/css-multicol
    rego@igalia.com:
      external/wpt/css/css-contain
      external/wpt/css/css-grid
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=foolip@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Ic7dc1808879a15bc57c0b4d65d22a323780a9a03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2572988
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833828}

---
+++ third_party/blink/web_tests/external/wpt/measure-memory/idlharness.tentative.any.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+// https://wicg.github.io/performance-measure-memory/
+
+idl_test(
+  ['performance-measure-memory'],
+  ['hr-time', 'dom'],
+  async idl_array => {
+    // Nothing to add.
+  }
+);
--- third_party/blink/web_tests/external/wpt/tools/wptserve/setup.py
+++ third_party/blink/web_tests/external/wpt/tools/wptserve/setup.py
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from setuptools import setup
 
 PACKAGE_VERSION = '3.0'
-deps = ["six>=1.8", "h2==3.0.1"]
+deps = ["six>=1.13.0", "h2==3.0.1"]
 
 setup(name='wptserve',
       version=PACKAGE_VERSION,
