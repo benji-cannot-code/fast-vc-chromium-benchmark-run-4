1bef06700ea7440f79a0c5742d363044e0449479
Pavol Marko <pmarko@google.com>
2020-12-04 12:28:18 +0000

    Don't assume client certificates are only available in user session
    NetworkConnectionHandler historically assumed that client certificates
    are only available in a user session, refusing to do anything for
    networks that could have a client certificate on the sign-in screen.
    
    This is not true anymore (since quite a while ago )- client certificates
    can be available on the sign-in screen through the 'system' token.
    
    Bug: 1152578
    Change-Id: Ic5fde537f531e04a712731b0c7d2da78dad4aa21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2573096
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833684}

--- chromeos/network/network_connection_handler_impl.cc
+++ chromeos/network/network_connection_handler_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chromeos/dbus/shill/shill_manager_client.h"
 #include "chromeos/dbus/shill/shill_service_client.h"
+#include "chromeos/login/login_state/login_state.h"
 #include "chromeos/network/client_cert_resolver.h"
 #include "chromeos/network/client_cert_util.h"
 #include "chromeos/network/device_state.h"
@@ -36,6 +37,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 namespace {
+
+// If connection to a network that may require a client certificate is requested
+// when client certificates are not loaded yet, wait this long until
+// certificates have been loaded.
+constexpr base::TimeDelta kMaxCertLoadTimeSeconds =
+    base::TimeDelta::FromSeconds(15);
 
 bool IsAuthenticationError(const std::string& error) {
   return (error == shill::kErrorBadWEPKey ||
@@ -170,7 +177,6 @@ NetworkConnectionHandlerImpl::NetworkConnectionHandlerImpl()
     : network_cert_loader_(nullptr),
       network_state_handler_(nullptr),
       configuration_handler_(nullptr),
-      logged_in_(false),
       certificates_loaded_(false) {}
 
 NetworkConnectionHandlerImpl::~NetworkConnectionHandlerImpl() {
@@ -178,17 +184,12 @@ NetworkConnectionHandlerImpl::~NetworkConnectionHandlerImpl() {
     network_state_handler_->RemoveObserver(this, FROM_HERE);
   if (network_cert_loader_)
     network_cert_loader_->RemoveObserver(this);
-  if (LoginState::IsInitialized())
-    LoginState::Get()->RemoveObserver(this);
 }
 
 void NetworkConnectionHandlerImpl::Init(
     NetworkStateHandler* network_state_handler,
     NetworkConfigurationHandler* network_configuration_handler,
     ManagedNetworkConfigurationHandler* managed_network_configuration_handler) {
-  if (LoginState::IsInitialized())
-    LoginState::Get()->AddObserver(this);
-
   if (NetworkCertLoader::IsInitialized()) {
     network_cert_loader_ = NetworkCertLoader::Get();
     network_cert_loader_->AddObserver(this);
@@ -211,18 +212,6 @@ void NetworkConnectionHandlerImpl::Init(
 
   // After this point, the NetworkConnectionHandlerImpl is fully initialized
   // (all handler references set, observers registered, ...).
-
-  if (LoginState::IsInitialized())
-    LoggedInStateChanged();
-}
-
-void NetworkConnectionHandlerImpl::LoggedInStateChanged() {
-  LoginState* login_state = LoginState::Get();
-  if (logged_in_ || !login_state->IsUserLoggedIn())
-    return;
-
-  logged_in_ = true;
-  logged_in_time_ = base::TimeTicks::Now();
 }
 
 void NetworkConnectionHandlerImpl::OnCertificatesLoaded() {
@@ -562,13 +551,6 @@ void NetworkConnectionHandlerImpl::VerifyConfiguredAndConnect(
     NET_LOG(DEBUG) << "Client cert type for: " << NetworkPathId(service_path)
                    << ": " << client_cert_type;
 
-    // User must be logged in to connect to a network requiring a certificate.
-    if (!logged_in_ || !network_cert_loader_) {
-      NET_LOG(ERROR) << "User not logged in for: "
-                     << NetworkPathId(service_path);
-      ErrorCallbackForPendingRequest(service_path, kErrorCertificateRequired);
-      return;
-    }
     // If certificates have not been loaded yet, queue the connect request.
     if (!certificates_loaded_) {
       NET_LOG(EVENT) << "Certificates not loaded for: "
@@ -651,16 +633,6 @@ void NetworkConnectionHandlerImpl::QueueConnectRequest(
     return;
   }
 
-  const int kMaxCertLoadTimeSeconds = 15;
-  base::TimeDelta dtime = base::TimeTicks::Now() - logged_in_time_;
-  if (dtime > base::TimeDelta::FromSeconds(kMaxCertLoadTimeSeconds)) {
-    NET_LOG(ERROR) << "Certificate load timeout: "
-                   << NetworkPathId(service_path);
-    InvokeConnectErrorCallback(service_path, std::move(request->error_callback),
-                               kErrorCertLoadTimeout);
-    return;
-  }
-
   NET_LOG(EVENT) << "Connect Request Queued: " << NetworkPathId(service_path);
   queued_connect_.reset(new ConnectRequest(request->mode, service_path,
                                            request->profile_path,
@@ -675,7 +647,7 @@ void NetworkConnectionHandlerImpl::QueueConnectRequest(
       FROM_HERE,
       base::BindOnce(&NetworkConnectionHandlerImpl::CheckCertificatesLoaded,
                      AsWeakPtr()),
-      base::TimeDelta::FromSeconds(kMaxCertLoadTimeSeconds) - dtime);
+      kMaxCertLoadTimeSeconds);
 }
 
 // Called after a delay to check whether certificates loaded. If they did not
--- chromeos/network/network_connection_handler_impl.h
+++ chromeos/network/network_connection_handler_impl.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "chromeos/dbus/dbus_method_call_status.h"
-#include "chromeos/login/login_state/login_state.h"
 #include "chromeos/network/network_cert_loader.h"
 #include "chromeos/network/network_connection_handler.h"
 #include "chromeos/network/network_state_handler_observer.h"
@@ -18,7 +17,6 @@ namespace chromeos {
 // Implementation of NetworkConnectionHandler.
 class COMPONENT_EXPORT(CHROMEOS_NETWORK) NetworkConnectionHandlerImpl
     : public NetworkConnectionHandler,
-      public LoginState::Observer,
       public NetworkCertLoader::Observer,
       public NetworkStateHandlerObserver,
       public base::SupportsWeakPtr<NetworkConnectionHandlerImpl> {
@@ -40,9 +38,6 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) NetworkConnectionHandlerImpl
   // NetworkStateHandlerObserver
   void NetworkListChanged() override;
   void NetworkPropertiesUpdated(const NetworkState* network) override;
-
-  // LoginState::Observer
-  void LoggedInStateChanged() override;
 
   // NetworkCertLoader::Observer
   void OnCertificatesLoaded() override;
@@ -149,9 +144,7 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) NetworkConnectionHandlerImpl
   std::unique_ptr<ConnectRequest> queued_connect_;
 
   // Track certificate loading state.
-  bool logged_in_;
   bool certificates_loaded_;
-  base::TimeTicks logged_in_time_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkConnectionHandlerImpl);
 };
