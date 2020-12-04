440d2f6b18117f2fb8d2095e30e7aee239a1a180
Amanda Deacon <amandadeacon@chromium.org>
2020-12-04 03:44:37 +0000

    Remove Top Cat inference code and references to tflite model.
    Tests: unit tests
    
    Bug: 1098112
    Change-Id: Ia17ebf3407b4f9429b6b2ab006210b38a0d7fab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2570878
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833595}

--- chrome/browser/ui/app_list/search/search_result_ranker/app_launch_event_logger.h
+++ chrome/browser/ui/app_list/search/search_result_ranker/app_launch_event_logger.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "base/values.h"
 #include "chrome/browser/ui/app_list/search/search_result_ranker/app_launch_event_logger.pb.h"
-#include "chrome/browser/ui/app_list/search/search_result_ranker/ml_app_rank_provider.h"
 #include "extensions/browser/extension_registry.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 
@@ -154,9 +153,6 @@ class AppLaunchEventLogger {
   // minutes.
   const std::unique_ptr<chromeos::power::ml::RecentEventsCounter>
       all_clicks_last_24_hours_;
-
-  // Empty until/unless CreateRankings is called.
-  std::unique_ptr<MlAppRankProvider> ml_app_rank_provider_;
 
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
   base::WeakPtrFactory<AppLaunchEventLogger> weak_factory_;
