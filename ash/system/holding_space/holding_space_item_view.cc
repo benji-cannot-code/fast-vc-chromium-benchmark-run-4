ae594139e6a817811d1a0666af8b21c08e2c0ee3
Ana Salazar <anasalazar@chromium.org>
2020-12-04 18:37:25 +0000

    Add play icon to screen recordings in holding space
    In the Holding Space's Screen Capture section, we display screenshots
    and screen recordings. At the moment, both previews display as still
    images.
    In order to differentiate between screenshots and screen recordings, we
    add a play icon over the screen recordings.
    
    Screenshot: https://screenshot.googleplex.com/9YVVkvvyAk7Duds
    
    Bug: 1144927
    Change-Id: Id1614a867caa73676d24c9ab59289337fea88c63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566051
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833785}

--- ash/system/holding_space/holding_space_item_view.cc
+++ ash/system/holding_space/holding_space_item_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_provider.h"
 #include "ash/system/holding_space/holding_space_item_view_delegate.h"
 #include "base/bind.h"
+#include "components/vector_icons/vector_icons.h"
 #include "ui/base/class_property.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/gfx/canvas.h"
@@ -242,9 +243,9 @@ views::ToggleImageButton* HoldingSpaceItemView::AddPin(views::View* parent) {
       AshColorProvider::ContentLayerType::kButtonIconColor);
 
   const gfx::ImageSkia unpinned_icon = gfx::CreateVectorIcon(
-      views::kUnpinIcon, kHoldingSpacePinIconSize, icon_color);
-  const gfx::ImageSkia pinned_icon = gfx::CreateVectorIcon(
-      views::kPinIcon, kHoldingSpacePinIconSize, icon_color);
+      views::kUnpinIcon, kHoldingSpaceIconSize, icon_color);
+  const gfx::ImageSkia pinned_icon =
+      gfx::CreateVectorIcon(views::kPinIcon, kHoldingSpaceIconSize, icon_color);
 
   pin_->SetImage(views::Button::STATE_NORMAL, unpinned_icon);
   pin_->SetToggledImage(views::Button::STATE_NORMAL, &pinned_icon);
@@ -258,6 +259,23 @@ views::ToggleImageButton* HoldingSpaceItemView::AddPin(views::View* parent) {
                                         base::Unretained(this)));
 
   return pin_;
+}
+
+views::View* HoldingSpaceItemView::CreatePlayIcon(views::View* parent) {
+  DCHECK(!play_icon_);
+  play_icon_ = parent->AddChildView(std::make_unique<views::ImageView>());
+  play_icon_->SetFocusBehavior(views::View::FocusBehavior::NEVER);
+  play_icon_->SetVisible(true);
+
+  const SkColor icon_color = AshColorProvider::Get()->GetContentLayerColor(
+      AshColorProvider::ContentLayerType::kButtonIconColor);
+
+  const gfx::ImageSkia play_icon_image = gfx::CreateVectorIcon(
+      vector_icons::kPlayArrowIcon, kHoldingSpaceIconSize, icon_color);
+
+  play_icon_->SetImage(&play_icon_image);
+
+  return play_icon_;
 }
 
 void HoldingSpaceItemView::OnPaintFocus(gfx::Canvas* canvas, gfx::Size size) {
