7bb837e503e41cb365d464e04ab253d010ee53ef
Michael Hansen <hansenmichael@google.com>
2020-12-04 00:31:30 +0000

    [Nearby] Fix "manage contacts" link within sharesheet.
    The "Manage Contacts" button doesn't work when viewing the contact
    visibility page from the sharesheet flow. We need a custom subclass of
    views::WebView to perform the navigation.
    
    Bug: 1138445
    Change-Id: Id90e83b6e307d136bd0114ee51fc42c53b2c8510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567759
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#833522}

--- chrome/browser/nearby_sharing/sharesheet/nearby_share_action.cc
+++ chrome/browser/nearby_sharing/sharesheet/nearby_share_action.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/file_manager/fileapi_util.h"
 #include "chrome/browser/nearby_sharing/attachment.h"
 #include "chrome/browser/nearby_sharing/file_attachment.h"
+#include "chrome/browser/nearby_sharing/sharesheet/nearby_share_web_view.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/sharesheet/sharesheet_types.h"
 #include "chrome/browser/ui/webui/nearby_share/nearby_share_dialog_ui.h"
@@ -94,7 +95,7 @@ void NearbyShareAction::LaunchAction(
   controller->SetSharesheetSize(size.width(), size.height());
 
   auto* profile = controller->GetProfile();
-  auto view = std::make_unique<views::WebView>(profile);
+  auto view = std::make_unique<NearbyShareWebView>(profile);
   // If this is not done, we don't see anything in our view.
   view->SetPreferredSize(size);
   views::WebView* web_view = root_view->AddChildView(std::move(view));
