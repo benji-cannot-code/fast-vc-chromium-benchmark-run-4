ec66d092076823eac8eb4fa198b37ce4938d997f
Gavin Williams <gavinwill@chromium.org>
2020-12-04 17:41:18 +0000

    Add an enterprise printer status icon for Print Preview
    Show the new status icons for local CrOS enterprise destinations in
    the dropdown and in the destination dialog.
    
    http://screen/Ar4TwJzsnGBmQeo
    http://screen/49UeCL7e9KMmuUy
    
    Bug: 1146550
    Change-Id: Iaa5c2933db43f32fe054619863b71c7483af62b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2572396
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833750}

--- chrome/browser/resources/print_preview/ui/destination_list_item.js
+++ chrome/browser/resources/print_preview/ui/destination_list_item.js
@@ -247,7 +247,9 @@ Polymer({
     // <if expr="chromeos">
     if (this.printerStatusFlagEnabled_ &&
         this.destination.origin === DestinationOrigin.CROS) {
-      return getPrinterStatusIcon(this.destination.printerStatusReason);
+      return getPrinterStatusIcon(
+          this.destination.printerStatusReason,
+          this.destination.isEnterprisePrinter);
     }
     // </if>
 
