1986278f5cb56e30096032d1ff59a4d40a6ad70e
Andy Paicu <andypaicu@chromium.org>
2020-12-04 10:59:06 +0000

    Disconnect bluetooth device when the user revokes permission
    Bug: 1128494
    Change-Id: I394e97f61268869557df5c64017504745a8fcd71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2412331
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833671}

--- content/browser/bluetooth/web_bluetooth_service_impl_unittest.cc
+++ content/browser/bluetooth/web_bluetooth_service_impl_unittest.cc
@@ -152,6 +152,9 @@ class TestBluetoothDelegate : public BluetoothDelegate {
     prompt_->RunPromptEventCallback(event);
   }
 
+  void AddPermissionObserver(FramePermissionObserver* observer) override {}
+  void RemovePermissionObserver(FramePermissionObserver* observer) override {}
+
  private:
   FakeBluetoothScanningPrompt* prompt_ = nullptr;
 };
