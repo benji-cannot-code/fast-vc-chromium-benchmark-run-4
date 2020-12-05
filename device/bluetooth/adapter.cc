b75a867f2617943559f698f008972eeec4b0294f
Ryan Hansberry <hansberry@chromium.org>
2020-12-05 01:17:58 +0000

    [Bluetooth] Only permit service connections for allowlisted UUIDs.
    Nearby Connections clients (e.g., Nearby Share) consume the Adapter
    methods to connect to or listen on RFCOMM services from the
    untrusted Nearby utility process. Only allow known UUIDs of these
    clients to be used with these methods.
    
    Bug: b:162975217, 1155668
    Change-Id: I8b94dda97df0f694b37c22fa59adfacc17b5093f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2573117
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833952}

--- device/bluetooth/adapter.cc
+++ device/bluetooth/adapter.cc
@@ -176,6 +176,11 @@ void Adapter::ConnectToServiceInsecurely(
     const std::string& address,
     const device::BluetoothUUID& service_uuid,
     ConnectToServiceInsecurelyCallback callback) {
+  if (!base::Contains(allowed_uuids_, service_uuid)) {
+    std::move(callback).Run(/*result=*/nullptr);
+    return;
+  }
+
   auto* device = adapter_->GetDevice(address);
   if (device) {
     OnDeviceFetchedForInsecureServiceConnection(service_uuid,
@@ -204,6 +209,11 @@ void Adapter::CreateRfcommServiceInsecurely(
     const std::string& service_name,
     const device::BluetoothUUID& service_uuid,
     CreateRfcommServiceInsecurelyCallback callback) {
+  if (!base::Contains(allowed_uuids_, service_uuid)) {
+    std::move(callback).Run(/*server_socket=*/mojo::NullRemote());
+    return;
+  }
+
   device::BluetoothAdapter::ServiceOptions service_options;
   service_options.name = service_name;
   service_options.require_authentication = false;
@@ -285,6 +295,11 @@ void Adapter::GattServicesDiscovered(device::BluetoothAdapter* adapter,
       ++it;
     }
   }
+}
+
+void Adapter::AllowConnectionsForUuid(
+    const device::BluetoothUUID& service_uuid) {
+  allowed_uuids_.emplace(service_uuid);
 }
 
 void Adapter::OnDeviceFetchedForInsecureServiceConnection(
--- device/bluetooth/adapter.h
+++ device/bluetooth/adapter.h
@@ -49,8 +49,6 @@ class Adapter : public mojom::Adapter,
                        SetDiscoverableCallback callback) override;
   void SetName(const std::string& name, SetNameCallback callback) override;
   void StartDiscoverySession(StartDiscoverySessionCallback callback) override;
-  // TODO(b/162975217): Add a mechanism to allowlist which address and UUID
-  // pairs clients are allowed to create a connection to.
   void ConnectToServiceInsecurely(
       const std::string& address,
       const device::BluetoothUUID& service_uuid,
@@ -77,6 +75,10 @@ class Adapter : public mojom::Adapter,
                      device::BluetoothDevice* device) override;
   void GattServicesDiscovered(device::BluetoothAdapter* adapter,
                               device::BluetoothDevice* device) override;
+
+  // Permit untrusted clients to initiate outgoing connections, or listen on
+  // incoming connections, with |service_uuid|.
+  void AllowConnectionsForUuid(const device::BluetoothUUID& service_uuid);
 
  private:
   void OnDeviceFetchedForInsecureServiceConnection(
@@ -132,6 +134,10 @@ class Adapter : public mojom::Adapter,
                          device::BluetoothUUID,
                          ConnectToServiceInsecurelyCallback>>
       pending_connect_to_service_args_;
+
+  // Allowed UUIDs for untrusted clients to initiate outgoing connections, or
+  // listen on incoming connections.
+  std::set<device::BluetoothUUID> allowed_uuids_;
 
   base::WeakPtrFactory<Adapter> weak_ptr_factory_{this};
 
