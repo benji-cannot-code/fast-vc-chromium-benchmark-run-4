659f59108862e223432026a277635671456b40e9
Jeffrey Kardatzke <jkardatzke@google.com>
2020-12-05 00:37:06 +0000

    Reland "media: Fix crash when releasing decoder with ChromeOS CDM"
    This reverts commit 78556ccd4ed284de4d994af3c9c6e008e4d0afe7.
    
    Reason for revert: Fixed crash
    
    Original change's description:
    > Revert "media: Fix crash when releasing decoder with ChromeOS CDM"
    >
    > This reverts commit 883c24e8db59cad2baa5b6a3c8f8143fe4bf195f.
    >
    > Reason for revert: Segfaults if not encrypted content
    >
    > Original change's description:
    > > media: Fix crash when releasing decoder with ChromeOS CDM
    > >
    > > When the VaapiVideoDecoder was released, if it was using the CdmContext
    > > it would have a callback associated with the CDM. Upon destruction of
    > > the decoder, that callback would be destroyed automatically which makes
    > > a call into the CDM.  The CDM is being destructed on another thread, so
    > > there was a race and this could cause a crash.
    > >
    > > The fix is to maintain a CdmContextRef in the decoder which keeps the
    > > CDM alive until the decoder is destroyed.
    > >
    > > BUG=b:153111783
    > > TEST=Page refresh 10 times in a row w/ CDM did not crash
    > >
    > > Change-Id: I0c6f19753b324e7d89db0bd79f6108d5de27ffa3
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2572262
    > > Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    > > Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#833863}
    >
    > TBR=xhwang@chromium.org,jkardatzke@google.com,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    >
    > Change-Id: I11cd7d74a92bca26e51727ccbc494cc178008320
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: b:153111783
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575717
    > Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    > Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    > Cr-Commit-Position: refs/heads/master@{#833914}
    
    TBR=xhwang@chromium.org,jkardatzke@google.com,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Bug: b:153111783
    Change-Id: I5776a90da74977ac1d6286a9d6148939175cff4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575718
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/master@{#833946}

--- chromeos/components/cdm_factory_daemon/content_decryption_module_adapter.h
+++ chromeos/components/cdm_factory_daemon/content_decryption_module_adapter.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/weak_ptr.h"
+#include "base/sequenced_task_runner_helpers.h"
 #include "base/threading/thread_checker.h"
 #include "chromeos/components/cdm_factory_daemon/cdm_storage_adapter.h"
 #include "chromeos/components/cdm_factory_daemon/chromeos_cdm_context.h"
@@ -37,8 +38,12 @@ namespace chromeos {
 // clear and not decoding.
 //
 // This implementation runs in the GPU process and expects all calls to be
-// executed on the mojo thread.  Decrypt, RegisterEventCB and CancelDecrypt
-// are exceptions, and can be called from any thread.
+// executed on the mojo thread.  Decrypt, RegisterEventCB, CancelDecrypt,
+// GetCdmContext, GetDecryptor, GetChromeOsCdmContext, GetHwKeyData and
+// GetCdmContextRef are exceptions, and can be called from any thread.
+//
+// Instances of this class will always be destructed on the mojo thread
+// automatically.
 class COMPONENT_EXPORT(CDM_FACTORY_DAEMON) ContentDecryptionModuleAdapter
     : public cdm::mojom::ContentDecryptionModuleClient,
       public media::ContentDecryptionModule,
@@ -88,6 +93,7 @@ class COMPONENT_EXPORT(CDM_FACTORY_DAEMON) ContentDecryptionModuleAdapter
   void RemoveSession(const std::string& session_id,
                      std::unique_ptr<media::SimpleCdmPromise> promise) override;
   media::CdmContext* GetCdmContext() override;
+  void DeleteOnCorrectThread() const override;
 
   // media::CdmContext:
   std::unique_ptr<media::CallbackRegistration> RegisterEventCB(
@@ -99,6 +105,7 @@ class COMPONENT_EXPORT(CDM_FACTORY_DAEMON) ContentDecryptionModuleAdapter
   void GetHwKeyData(const media::DecryptConfig* decrypt_config,
                     const std::vector<uint8_t>& hw_identifier,
                     GetHwKeyDataCB callback) override;
+  std::unique_ptr<media::CdmContextRef> GetCdmContextRef() override;
 
   // cdm::mojom::ContentDecryptionModuleClient:
   void OnSessionMessage(const std::string& session_id,
@@ -131,6 +138,9 @@ class COMPONENT_EXPORT(CDM_FACTORY_DAEMON) ContentDecryptionModuleAdapter
   bool CanAlwaysDecrypt() override;
 
  private:
+  // For DeleteSoon() in DeleteOnCorrectThread().
+  friend class base::DeleteHelper<ContentDecryptionModuleAdapter>;
+
   ~ContentDecryptionModuleAdapter() override;
   void OnConnectionError();
   void RejectTrackedPromise(uint32_t promise_id,
