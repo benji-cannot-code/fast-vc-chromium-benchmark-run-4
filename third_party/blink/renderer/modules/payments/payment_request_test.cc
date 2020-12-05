b753a97fe3c8838cae194ec525d5f768b89d89de
Danyao Wang <danyao@chromium.org>
2020-12-05 00:09:06 +0000

    [Web Payments] Requires user activation for show() behind a flag.
    The Payment Request API requires that show() be called with a transient
    user activation. This CL adds enforcement behind a runtime flag.
    
    Bug: 825270
    Change-Id: I39782301f5c4aaed32a664d3803c9142747dc41e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2574781
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#833936}

--- third_party/blink/renderer/modules/payments/payment_request_test.cc
+++ third_party/blink/renderer/modules/payments/payment_request_test.cc
@@ -6,13 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/payments/payment_request.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/mojom/frame/user_activation_notification_type.mojom-blink-forward.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/event_type_names.h"
+#include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/modules/payments/payment_test_helper.h"
 #include "third_party/blink/renderer/platform/bindings/exception_code.h"
 #include "third_party/blink/renderer/platform/heap/heap_allocator.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 namespace {
@@ -298,6 +301,35 @@ TEST(PaymentRequestTest, CannotShowAfterAborted) {
   EXPECT_EQ(scope.GetExceptionState().Code(),
             ToExceptionCode(DOMExceptionCode::kInvalidStateError));
   ;
+}
+
+TEST(PaymentRequestTest, CannotShowWithoutUserActivation) {
+  RuntimeEnabledFeatures::SetPaymentRequestShowConsumesUserActivationEnabled(
+      true);
+  PaymentRequestV8TestingScope scope;
+  PaymentRequestMockFunctionScope funcs(scope.GetScriptState());
+  PaymentRequest* request = PaymentRequest::Create(
+      scope.GetExecutionContext(), BuildPaymentMethodDataForTest(),
+      BuildPaymentDetailsInitForTest(), ASSERT_NO_EXCEPTION);
+  request->show(scope.GetScriptState(), scope.GetExceptionState());
+  EXPECT_EQ(scope.GetExceptionState().Code(),
+            ToExceptionCode(DOMExceptionCode::kNotAllowedError));
+  ;
+}
+
+TEST(PaymentRequestTest, ShowConsumesUserActivation) {
+  RuntimeEnabledFeatures::SetPaymentRequestShowConsumesUserActivationEnabled(
+      true);
+  PaymentRequestV8TestingScope scope;
+  PaymentRequestMockFunctionScope funcs(scope.GetScriptState());
+  PaymentRequest* request = PaymentRequest::Create(
+      scope.GetExecutionContext(), BuildPaymentMethodDataForTest(),
+      BuildPaymentDetailsInitForTest(), ASSERT_NO_EXCEPTION);
+
+  LocalFrame::NotifyUserActivation(
+      &(scope.GetFrame()), mojom::UserActivationNotificationType::kTest);
+  request->show(scope.GetScriptState(), ASSERT_NO_EXCEPTION)
+      .Then(funcs.ExpectNoCall(), funcs.ExpectNoCall());
 }
 
 TEST(PaymentRequestTest, RejectShowPromiseOnErrorPaymentMethodNotSupported) {
