fad8298afa1eee709eecb2701dedd4074aa96d00
Stephan Hartmann <stha09@googlemail.com>
2020-12-05 20:50:33 +0000

    IWYU: add missing include for std::condition_variable
    Bug: None
    Change-Id: Iba75af392d248846b1d86b21b5ff4b45dce2e054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575120
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834038}

--- base/allocator/partition_allocator/pcscan.cc
+++ base/allocator/partition_allocator/pcscan.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/pcscan.h"
 
 #include <algorithm>
+#include <condition_variable>
 #include <map>
 #include <mutex>
 #include <numeric>
