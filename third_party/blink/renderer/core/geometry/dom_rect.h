a6a7c4bdcc68c3208b22a2ee425fd9871c01e38f
Adam Ettenberger <Adam.Ettenberger@microsoft.com>
2020-12-05 00:25:04 +0000

    Remove unnecessary include in dom_rect.h
    third_party/blink/renderer/bindings/core/v8/dictionary.h is included by
    dom_rect.h, but this seems unnecessary since neither dom_rect.h or
    dom_rect.cc use blink::Dictionary or other structures defined in that
    header, so removing the include.
    
    Bug: none
    Change-Id: I9e55447a979e4cc0a16c187c88104c04eabfab29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575049
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#833941}

--- third_party/blink/renderer/core/geometry/dom_rect.h
+++ third_party/blink/renderer/core/geometry/dom_rect.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_GEOMETRY_DOM_RECT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_GEOMETRY_DOM_RECT_H_
 
-#include "third_party/blink/renderer/bindings/core/v8/dictionary.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/geometry/dom_rect_read_only.h"
 #include "third_party/blink/renderer/platform/geometry/float_rect.h"
