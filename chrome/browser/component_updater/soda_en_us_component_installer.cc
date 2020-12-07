ffb3c90d8420856f53b7e20a161fd7e708edffc2
Evan Liu <evliu@google.com>
2020-12-07 22:16:53 +0000

    Explicitly set file permissions when installing SODA components
    This CL updates the SODA component installers to explicitly set the read and execute bit for the BUILTIN\Users group, allowing the speech recognition sandbox to access the component files using a USER_LIMITED token.
    
    Bug: 1111079
    Change-Id: I5b92dbe8c68bd79be57010e8da7cbbc24a21e9c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2573876
    Commit-Queue: Evan Liu <evliu@google.com>
    Reviewed-by: James Forshaw <forshaw@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834403}

--- chrome/browser/component_updater/soda_en_us_component_installer.cc
+++ chrome/browser/component_updater/soda_en_us_component_installer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/component_updater/soda_component_installer.h"
 #include "chrome/common/pref_names.h"
 #include "components/component_updater/component_updater_service.h"
 #include "components/crx_file/id_util.h"
@@ -84,7 +85,8 @@ update_client::CrxInstaller::Result
 SodaEnUsComponentInstallerPolicy::OnCustomInstall(
     const base::DictionaryValue& manifest,
     const base::FilePath& install_dir) {
-  return update_client::CrxInstaller::Result(0);  // Nothing custom here.
+  return SODAComponentInstallerPolicy::SetComponentDirectoryPermission(
+      install_dir);
 }
 
 void SodaEnUsComponentInstallerPolicy::OnCustomUninstall() {}
