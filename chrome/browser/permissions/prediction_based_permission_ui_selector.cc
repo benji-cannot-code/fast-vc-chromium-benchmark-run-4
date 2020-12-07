5c8104d74b0b34ad72f5c450f88ba8b0bf0cc018
Andy Paicu <andypaicu@chromium.org>
2020-12-07 17:58:00 +0000

    Added a command line switch to mock a prediction service response
    This is needed in order to facilitate testing the feature.
    
    Bug: 1138595,1155511
    Change-Id: Ib01969e48e5686b3ffed963745c5e85a1d802221
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2573076
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834267}

--- chrome/browser/permissions/prediction_based_permission_ui_selector.cc
+++ chrome/browser/permissions/prediction_based_permission_ui_selector.cc
@@ -1,10 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2019 The Chromium Authors. All rights reserved.
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "chrome/browser/permissions/prediction_based_permission_ui_selector.h"
 
+#include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/time/default_clock.h"
 #include "base/util/values/values_util.h"
@@ -12,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/permissions/prediction_service_request.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_features.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "components/permissions/permission_util.h"
 #include "components/permissions/prediction_service/prediction_service.h"
@@ -24,7 +26,7 @@ namespace {
 using QuietUiReason = PredictionBasedPermissionUiSelector::QuietUiReason;
 using Decision = PredictionBasedPermissionUiSelector::Decision;
 
-const auto VeryUnlikely = permissions::
+constexpr auto VeryUnlikely = permissions::
     PermissionSuggestion_Likelihood_DiscretizedLikelihood_VERY_UNLIKELY;
 
 // The data we consider can only be at most 28 days old to match the data that
@@ -35,11 +37,48 @@ constexpr base::TimeDelta kPermissionActionCutoffAge =
 constexpr char kPermissionActionEntryActionKey[] = "action";
 constexpr char kPermissionActionEntryTimestampKey[] = "time";
 
+base::Optional<
+    permissions::PermissionSuggestion_Likelihood_DiscretizedLikelihood>
+ParsePredictionServiceMockLikelihood(const std::string& value) {
+  if (value == "very-unlikely") {
+    return permissions::
+        PermissionSuggestion_Likelihood_DiscretizedLikelihood_VERY_UNLIKELY;
+  } else if (value == "unlikely") {
+    return permissions::
+        PermissionSuggestion_Likelihood_DiscretizedLikelihood_UNLIKELY;
+  } else if (value == "neutral") {
+    return permissions::
+        PermissionSuggestion_Likelihood_DiscretizedLikelihood_NEUTRAL;
+  } else if (value == "likely") {
+    return permissions::
+        PermissionSuggestion_Likelihood_DiscretizedLikelihood_LIKELY;
+  } else if (value == "very-likely") {
+    return permissions::
+        PermissionSuggestion_Likelihood_DiscretizedLikelihood_VERY_LIKELY;
+  }
+
+  return base::nullopt;
+}
+
+bool ShouldPredictionTriggerQuietUi(
+    permissions::PermissionUmaUtil::PredictionGrantLikelihood likelihood) {
+  return likelihood == VeryUnlikely;
+}
+
 }  // namespace
 
 PredictionBasedPermissionUiSelector::PredictionBasedPermissionUiSelector(
     Profile* profile)
-    : profile_(profile) {}
+    : profile_(profile) {
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kPredictionServiceMockLikelihood)) {
+    auto mock_likelihood = ParsePredictionServiceMockLikelihood(
+        base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+            switches::kPredictionServiceMockLikelihood));
+    if (mock_likelihood.has_value())
+      set_likelihood_override(mock_likelihood.value());
+  }
+}
 
 PredictionBasedPermissionUiSelector::~PredictionBasedPermissionUiSelector() =
     default;
@@ -49,6 +88,18 @@ void PredictionBasedPermissionUiSelector::SelectUiToUse(
     DecisionMadeCallback callback) {
   if (!IsAllowedToUseAssistedPrompts()) {
     std::move(callback).Run(Decision::UseNormalUiAndShowNoWarning());
+    return;
+  }
+
+  if (likelihood_override_for_testing_.has_value()) {
+    if (ShouldPredictionTriggerQuietUi(
+            likelihood_override_for_testing_.value())) {
+      std::move(callback).Run(
+          Decision(QuietUiReason::kPredictedVeryUnlikelyGrant,
+                   Decision::ShowNoWarning()));
+    } else {
+      std::move(callback).Run(Decision::UseNormalUiAndShowNoWarning());
+    }
     return;
   }
 
@@ -140,8 +191,7 @@ void PredictionBasedPermissionUiSelector::LookupReponseReceived(
   last_request_grant_likelihood_ =
       response->suggestion(0).grant_likelihood().discretized_likelihood();
 
-  if (response->suggestion(0).grant_likelihood().discretized_likelihood() ==
-      VeryUnlikely) {
+  if (ShouldPredictionTriggerQuietUi(last_request_grant_likelihood_.value())) {
     std::move(callback_).Run(Decision(
         QuietUiReason::kPredictedVeryUnlikelyGrant, Decision::ShowNoWarning()));
     return;
