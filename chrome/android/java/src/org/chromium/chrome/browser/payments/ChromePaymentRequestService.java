23c351b198c80ecafb6e10cbdf44339c841281f3
Liquan (Max) Gu <maxlg@chromium.org>
2020-12-07 23:39:09 +0000

    [WebLayer] Encapsulate SettingsAutofillAndPaymentsObserver in Ui
    Context:
    Since SettingsAutofillAndPaymentsObserver is autofill logic and
    PaymentUiService is in charge of the autofill service, the observer
    class should go into PaymentUiService.
    
    Changes:
    Moved SettingsAutofillAndPaymentsObserver from:
    * CPRService#notifyPaymentUiOfPendingApps to PUiService#setPaymentApps
    * CPRService#close to PUiService#close
    
    Bug: 1025619
    Change-Id: I224cf06276b26857775c26d2a030fd5fcc7d3295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2574602
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834449}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ChromePaymentRequestService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ChromePaymentRequestService.java
@@ -558,7 +558,6 @@ public class ChromePaymentRequestService
         }
 
         mPaymentUiService.close();
-        SettingsAutofillAndPaymentsObserver.getInstance().unregisterObserver(mPaymentUiService);
 
         if (mPaymentHandlerHost != null) {
             mPaymentHandlerHost.destroy();
@@ -621,8 +620,6 @@ public class ChromePaymentRequestService
             RecordHistogram.recordSparseHistogram(
                     "PaymentRequest.MissingPaymentFields", missingFields);
         }
-
-        SettingsAutofillAndPaymentsObserver.getInstance().registerObserver(mPaymentUiService);
     }
 
     // Implements BrowserPaymentRequest:
--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentUiService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentUiService.java
@@ -383,6 +383,8 @@ public class PaymentUiService implements SettingsAutofillAndPaymentsObserver.Obs
                 Section.PAYMENT_METHOD, apps.size(), !apps.isEmpty() && apps.get(0).isComplete());
 
         updateAppModifiedTotals();
+
+        SettingsAutofillAndPaymentsObserver.getInstance().registerObserver(this);
     }
 
     /** Set the AutofillPaymentAppCreator. */
@@ -1691,6 +1693,8 @@ public class PaymentUiService implements SettingsAutofillAndPaymentsObserver.Obs
             }
             mPaymentMethodsSection = null;
         }
+
+        SettingsAutofillAndPaymentsObserver.getInstance().unregisterObserver(this);
 
         removeLeavingTabObservers();
         destroyCurrencyFormatters();
