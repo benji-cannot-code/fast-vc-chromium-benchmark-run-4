0207c5e7e44c0bfd09b81b7419c762a004776332
Wolfgang Beyer <wolfi@chromium.org>
2020-12-07 14:38:50 +0000

    [DevTools] Add test for inline document.write() not sending CDP event
    This CL adds a test which ensures that a `document.write()` when the
    document is still open does not trigger sending a `Page.documentOpened`
    CDP event.
    
    `Page.documentOpened` CL: https://crrev.com/c/2498482
    Design Doc: http://doc/1l-XzjFMdASjRzX4-vMxOoK-MXoxY5uhCe0GNfDsXeDo
    
    Change-Id: Ibbe7d539db6ee85168c99972a8e2779dcc982b03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2567007
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834198}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/inline-document-write.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, dp} = await testRunner.startBlank(
+      `Tests that inline document.write() does not send Page.documentOpened event`);
+
+  await dp.Page.enable();
+
+  dp.Page.onFrameNavigated((event) => {
+    testRunner.log(`Frame navigated to ${event.params.frame.url}`);
+  });
+  dp.Page.onDocumentOpened((event) => {
+    testRunner.log(`Document opened, new url: ${event.params.frame.url}`);
+  });
+  dp.Page.onFrameStoppedLoading((event) => {
+    testRunner.log('Frame stopped loading');
+    testRunner.completeTest();
+  });
+
+  await page.navigate('http://devtools.test:8000/inspector-protocol/page/resources/inline-document-write.html');
+})
