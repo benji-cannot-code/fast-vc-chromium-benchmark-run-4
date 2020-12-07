5c14270186a322dafd4fa059a0c25cad1625fdf1
Eric Willigers <ericwilligers@chromium.org>
2020-12-07 03:23:41 +0000

    Web Share Target: ChromeOS support for receiving share requests
    We generate a POST request from the share intent.
    
    Spec:
    https://w3c.github.io/web-share-target/level-2/
    
    
    On Chrome OS, only one file type may be shared at a time.
    
    Note that sharing title/text/url is not yet supported.
    
    
    
    Bug: 1125880
    Change-Id: I4aa2a0b887bbb208bf33fdef849b970d72bf1e5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560080
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834114}

--- chrome/browser/ui/web_applications/test/web_app_browsertest_util.cc
+++ chrome/browser/ui/web_applications/test/web_app_browsertest_util.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/apps/app_service/browser_app_launcher.h"
 #include "chrome/browser/installable/installable_metrics.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -35,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/web_app_provider_base.h"
 #include "chrome/browser/web_applications/components/web_app_tab_helper_base.h"
 #include "chrome/browser/web_applications/components/web_application_info.h"
+#include "chrome/browser/web_applications/test/service_worker_registration_waiter.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/security_interstitials/content/security_interstitial_tab_helper.h"
@@ -114,7 +116,10 @@ AppId InstallWebApp(Profile* profile,
 }
 
 AppId InstallWebAppFromManifest(Browser* browser, const GURL& app_url) {
+  ServiceWorkerRegistrationWaiter registration_waiter(browser->profile(),
+                                                      app_url);
   NavigateToURLAndWait(browser, app_url);
+  registration_waiter.AwaitRegistration();
 
   AppId app_id;
   base::RunLoop run_loop;
@@ -124,7 +129,7 @@ AppId InstallWebAppFromManifest(Browser* browser, const GURL& app_url) {
   WaitUntilReady(provider);
   provider->install_manager().InstallWebAppFromManifestWithFallback(
       browser->tab_strip_model()->GetActiveWebContents(),
-      /*force_shortcut_app=*/true, WebappInstallSource::MENU_BROWSER_TAB,
+      /*force_shortcut_app=*/false, WebappInstallSource::MENU_BROWSER_TAB,
       base::BindLambdaForTesting(
           [](content::WebContents* initiator_web_contents,
              std::unique_ptr<WebApplicationInfo> web_app_info,
