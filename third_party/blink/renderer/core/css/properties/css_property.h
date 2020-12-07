f2e1d75c41f0eb3b9b330f88c9f38f1c523fc4ce
Oriol Brufau <obrufau@igalia.com>
2020-12-07 14:43:11 +0000

    [css-logical] Implement logical property groups
    In order to have proper CSSOM support for logical properties, we will
    need to determine whether two properties belong to the same logical
    property group but with a different mapping logic.
    
    Before this patch, we only knew the physical properties belonging to the
    same logical property group as a given logical property. But given a
    physical property, we didn't know if there were logical properties that
    could map to it.
    
    Therefore this patch adds:
     - CSSProperty::IsInLogicalPropertyGroup, which returns true for logical
       properties and for their corresponding physical ones.
     - CSSProperty::IsInSameLogicalPropertyGroupWithDifferentMappingLogic
       which checks what the name says, given a CSSPropertyID.
     - CSSDirectionAwareResolver::LogicalMapping and PhysicalMapping,
       representing the properties of a logical property group with a flow-
       relative or a physical mapping logic, respectively.
    
    In css_properties.json5, the terminology becomes closer to the spec:
     - direction_aware_options becomes logical_property_group, and physical
       properties have it too.
     - direction_aware_options.physical_group becomes
       logical_property_group.name
     - direction_aware_options.resolver accepts physical axis and box sides
       (horizontal, vertical, top, right, bottom, left).
    
    This patch should have no effect in practice.
    
    Bug: 1155858
    
    Change-Id: I1a01e80a2405315d9caaf057e6bfd465f3615cce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2574940
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834200}

--- third_party/blink/renderer/core/css/properties/css_property.h
+++ third_party/blink/renderer/core/css/properties/css_property.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include "third_party/blink/renderer/core/css/css_property_name.h"
 #include "third_party/blink/renderer/core/css/css_value.h"
+#include "third_party/blink/renderer/core/css/properties/css_direction_aware_resolver.h"
 #include "third_party/blink/renderer/core/css/properties/css_unresolved_property.h"
 #include "third_party/blink/renderer/platform/heap/heap_allocator.h"
 #include "third_party/blink/renderer/platform/text/text_direction.h"
@@ -63,6 +64,9 @@ class CORE_EXPORT CSSProperty : public CSSUnresolvedProperty {
     return flags_ & kComputedValueComparable;
   }
   bool TakesTreeScopedValue() const { return flags_ & kTreeScopedValue; }
+  bool IsInLogicalPropertyGroup() const {
+    return flags_ & kInLogicalPropertyGroup;
+  }
 
   bool IsRepeated() const { return repetition_separator_ != '\0'; }
   char RepetitionSeparator() const { return repetition_separator_; }
@@ -100,6 +104,10 @@ class CORE_EXPORT CSSProperty : public CSSUnresolvedProperty {
   virtual const CSSProperty& ResolveDirectionAwareProperty(TextDirection,
                                                            WritingMode) const {
     return *this;
+  }
+  virtual bool IsInSameLogicalPropertyGroupWithDifferentMappingLogic(
+      CSSPropertyID) const {
+    return false;
   }
   virtual const CSSProperty* GetVisitedProperty() const { return nullptr; }
   virtual const CSSProperty* GetUnvisitedProperty() const { return nullptr; }
@@ -152,6 +160,8 @@ class CORE_EXPORT CSSProperty : public CSSUnresolvedProperty {
     kTreeScopedValue = 1 << 18,
     // https://drafts.csswg.org/css-pseudo-4/#highlight-styling
     kValidForHighlight = 1 << 19,
+    // https://drafts.csswg.org/css-logical/#logical-property-group
+    kInLogicalPropertyGroup = 1 << 20,
   };
 
   constexpr CSSProperty(CSSPropertyID property_id,
