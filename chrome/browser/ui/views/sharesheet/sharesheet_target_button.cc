74ae82511e0463ba6078f765497cd4d402b1b6a1
Melissa Zhang <melzhang@chromium.org>
2020-12-07 22:38:12 +0000

    [Sharesheet] Update Sharesheet_bubble typography.
    This CL adds new typography contexts to ChromeTypography to be
    in the Sharesheet. This allows us to align typography with the
    mocks, linked in the bug.
    
    Bug: 1141744
    Change-Id: Ic15e6f068d62919f71a7d5f89ac5e4ad6fe61fd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2525382
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834419}

--- chrome/browser/ui/views/sharesheet/sharesheet_target_button.cc
+++ chrome/browser/ui/views/sharesheet/sharesheet_target_button.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/public/cpp/ash_typography.h"
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -28,9 +29,6 @@ constexpr int kButtonTextMaxWidth = 76;
 constexpr int kButtonLineHeight = 20;
 constexpr int kButtonMaxLines = 2;
 constexpr int kButtonPadding = 8;
-
-constexpr char kButtonLabelFont[] = "Roboto, Medium, 14px";
-constexpr char kButtonSecondaryLabelFont[] = "Roboto, Regular, 13px";
 
 constexpr SkColor kShareTargetTitleColor = gfx::kGoogleGrey700;
 constexpr SkColor kShareTargetSecondaryTitleColor = gfx::kGoogleGrey600;
@@ -83,18 +81,20 @@ SharesheetTargetButton::SharesheetTargetButton(
   label_view->SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical, gfx::Insets(), 0, true));
 
-  auto* label =
-      label_view->AddChildView(std::make_unique<views::Label>(display_name));
-  label->SetFontList(gfx::FontList(kButtonLabelFont));
+  auto* label = label_view->AddChildView(std::make_unique<views::Label>(
+      display_name, ash::CONTEXT_SHARESHEET_BUBBLE_BODY,
+      ash::STYLE_SHARESHEET));
   label->SetEnabledColor(kShareTargetTitleColor);
   SetLabelProperties(label);
 
   base::string16 accessible_name = display_name;
   if (secondary_display_name != base::string16() &&
       secondary_display_name != display_name) {
-    auto* secondary_label = label_view->AddChildView(
-        std::make_unique<views::Label>(secondary_display_name));
-    secondary_label->SetFontList(gfx::FontList(kButtonSecondaryLabelFont));
+    auto* secondary_label =
+        label_view->AddChildView(std::make_unique<views::Label>(
+            secondary_display_name,
+            ash::CONTEXT_SHARESHEET_BUBBLE_BODY_SECONDARY,
+            ash::STYLE_SHARESHEET));
     secondary_label->SetEnabledColor(kShareTargetSecondaryTitleColor);
     SetLabelProperties(secondary_label);
     accessible_name = base::StrCat(
