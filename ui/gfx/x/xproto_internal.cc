2d402d4d59b6da336f25a5f9410474695621bc7a
Tom Anderson <thomasanderson@chromium.org>
2020-12-07 21:42:51 +0000

    [XProto] Refactor response handling
    * Now that every request is made by XProto,
      x11::Connection::Request::sequence is no longer needed.  Now sequences
      can be determined by (their index in the queue) +
      x11::Connection::first_request_id_.
      * This makes it easier to work with requests out-of-order, which will
        be necessary for creating property/window caches in the future.
    * Move some xcb calls from xproto_types.cc into connection.cc.  I want
      to dlopen() libxcb in the future to make it possible to run Chrome on
      systems without any X11 libraries, so having everything consolidated
      will make that easier.
    * Add Future::Wait().  Similar to Sync(), this blocks until a response
      is received, but instead of returning the response, it calls the
      installed response handler out-of-order.
    
    R=sky
    
    Bug: 739898
    Change-Id: I10c4c18d3c3090ba09d660a5090fd7c0f01830f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2565440
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834386}

--- ui/gfx/x/xproto_internal.cc
+++ ui/gfx/x/xproto_internal.cc
@@ -62,11 +62,11 @@ UnretainedRefCountedMemory::~UnretainedRefCountedMemory() = default;
 
 base::Optional<unsigned int> SendRequestImpl(x11::Connection* connection,
                                              WriteBuffer* buf,
-                                             bool is_void,
+                                             bool generates_reply,
                                              bool reply_has_fds) {
   xcb_protocol_request_t xpr{
       .ext = nullptr,
-      .isvoid = is_void,
+      .isvoid = !generates_reply,
   };
 
   struct RequestHeader {
@@ -126,8 +126,6 @@ base::Optional<unsigned int> SendRequestImpl(x11::Connection* connection,
 
   if (xcb_connection_has_error(conn))
     return base::nullopt;
-  if (connection->synchronous())
-    connection->Sync();
   return sequence;
 }
 
