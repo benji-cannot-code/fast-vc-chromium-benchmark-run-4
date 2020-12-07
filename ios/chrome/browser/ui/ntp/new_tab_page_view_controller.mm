9acc7a4e88b03f81b123ed5064b66af523d8730d
adamta <adamta@google.com>
2020-12-07 17:55:58 +0000

    [iOS] Creates DiscoverFeedVC for NTP
    Creates view controller to wrap Discover feed returned by Mulder. Will
    be used as the primary NTP scroll view so that feed scroll actions could
    be detected and to expose certain properties.
    
    Bug: 1114792
    Change-Id: I8ee9160b70234c1e25e9a5ea721d8982b84b5f30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2576416
    Commit-Queue: Adam Trudeau-Arcaro <adamta@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834266}

--- ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_view_controller.mm
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  [self.contentSuggestionsViewController willMoveToParentViewController:self];
   [self addChildViewController:self.contentSuggestionsViewController];
   [self.view addSubview:self.contentSuggestionsViewController.view];
   [self.contentSuggestionsViewController didMoveToParentViewController:self];
