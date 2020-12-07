6acfc078d80d8d53130cc4c4d2497baac66d0cac
chromium-autoroll <chromium-autoroll@skia-public.iam.gserviceaccount.com>
2020-12-07 15:19:40 +0000

    Roll FreeType from 9f94d8533cef to 272ae5ee2ab1 (2 revisions)
    https://chromium.googlesource.com/chromium/src/third_party/freetype2.git/+log/9f94d8533cef..272ae5ee2ab1
    
    2020-12-07 wl@gnu.org * src/*: More fixes for using a '\n' in `FT_TRACE` and `FT_ERROR`.
    2020-12-07 wl@gnu.org */*: s/FT_LOGGING/FT_DEBUG_LOGGING/.
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/freetype-chromium
    Please CC bungeman@google.com,drott@google.com,thestig@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/master/autoroll/README.md
    
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_msan_rel_ng;luci.chromium.try:linux-blink-rel;luci.chromium.try:mac10.12-blink-rel;luci.chromium.try:win10-blink-rel;luci.chromium.try:win7-blink-rel
    Tbr: bungeman@google.com,drott@google.com,thestig@google.com
    Change-Id: I64ad85f729dd9849ba2d9988c129eb53b2d99cc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575505
    Reviewed-by: chromium-autoroll <chromium-autoroll@skia-public.iam.gserviceaccount.com>
    Reviewed-by: Ben Wagner <bungeman@chromium.org>
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834212}

--- third_party/freetype/include/freetype-custom/freetype/config/ftoption.h
+++ third_party/freetype/include/freetype-custom/freetype/config/ftoption.h
@@ -448,7 +448,7 @@ FT_BEGIN_HEADER
    *
    *   This option needs a C99 compiler.
    */
-/* #define FT_LOGGING */
+/* #define FT_DEBUG_LOGGING */
 
 
   /**************************************************************************
