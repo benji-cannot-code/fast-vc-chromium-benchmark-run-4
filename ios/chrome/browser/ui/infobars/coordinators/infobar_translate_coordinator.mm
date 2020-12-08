e21a8d4a0e18302c61f7ce819bae95dfb6d5f48e
Josh Santana <joshsantana@google.com>
2020-12-08 22:24:23 +0000

    Inclusive Language Bugfix: Infobar Delegate
    Renamed methods in translate_infobar_delgate that used "blacklist" to more inclusive/descriptive names like "NeverPromptList" and fixes calls to these methods in other files.
    
    Bug: 1028966
    Change-Id: I6f2607b4f80e47d540288336eb01d26021ca2f40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2572787
    Commit-Queue: Joshua Santana <joshsantana@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Reviewed-by: Josh Simmons <jds@google.com>
    Reviewed-by: Trevor  Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834902}

--- ios/chrome/browser/ui/infobars/coordinators/infobar_translate_coordinator.mm
+++ ios/chrome/browser/ui/infobars/coordinators/infobar_translate_coordinator.mm
@@ -400,7 +400,8 @@ NSString* const kTranslateNotificationSnackbarCategory =
   [self dismissInfobarModalAnimated:YES
                          completion:^{
                            // Completely remove the Infobar along with its badge
-                           // after blacklisting the Website.
+                           // after adding site to never prompt list for the
+                           // Website.
                            [self detachView];
                          }];
 }
@@ -413,9 +414,9 @@ NSString* const kTranslateNotificationSnackbarCategory =
 }
 
 - (void)neverTranslateSite {
-  DCHECK(!self.translateInfobarDelegate->IsSiteBlacklisted());
+  DCHECK(!self.translateInfobarDelegate->IsSiteOnNeverPromptList());
   self.userAction |= UserActionNeverTranslateSite;
-  self.translateInfobarDelegate->ToggleSiteBlacklist();
+  self.translateInfobarDelegate->ToggleNeverPrompt();
   [self
       recordInfobarEvent:translate::InfobarEvent::INFOBAR_NEVER_TRANSLATE_SITE];
   [TranslateInfobarMetricsRecorder
@@ -424,14 +425,15 @@ NSString* const kTranslateNotificationSnackbarCategory =
   [self dismissInfobarModalAnimated:YES
                          completion:^{
                            // Completely remove the Infobar along with its badge
-                           // after blacklisting the Website.
+                           // after adding site to never prompt list for the
+                           // Website.
                            [self detachView];
                          }];
 }
 
 - (void)undoNeverTranslateSite {
-  DCHECK(self.translateInfobarDelegate->IsSiteBlacklisted());
-  self.translateInfobarDelegate->ToggleSiteBlacklist();
+  DCHECK(self.translateInfobarDelegate->IsSiteOnNeverPromptList());
+  self.translateInfobarDelegate->ToggleNeverPrompt();
   [self dismissInfobarModalAnimated:YES completion:nil];
   // TODO(crbug.com/1014959): implement else logic. Should aything be done?
 }
--- ios/chrome/browser/ui/infobars/coordinators/infobar_translate_mediator.mm
+++ ios/chrome/browser/ui/infobars/coordinators/infobar_translate_mediator.mm
@@ -264,7 +264,7 @@ const int kInvalidLanguageIndex = -1;
     kIsTranslatableLanguagePrefKey :
         @(self.translateInfobarDelegate->IsTranslatableLanguageByPrefs()),
     kIsSiteBlacklistedPrefKey :
-        @(self.translateInfobarDelegate->IsSiteBlacklisted()),
+        @(self.translateInfobarDelegate->IsSiteOnNeverPromptList()),
   };
 }
 
