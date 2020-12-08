cdd7fbe97398ffbe513c1acd0811120e2feb368e
Joel Hockey <joelhockey@chromium.org>
2020-12-08 02:05:17 +0000

    crosh: stick to chrome-untrusted://crosh
    Only use the builtin chrome-untrusted://crosh for Ctrl+Alt+T shortcut
    rather than allowing Secure Shell App to override.
    
    Users can still manually use Secure Shell urls such as
    chrome-extension://okddffdblfhhnmhodogpojmfkjmhinfp/html/crosh.html
    
    Bug: None
    Change-Id: I87743b662527ef8e42e27318dea47c407a951963
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2574639
    Reviewed-by: Jason Lin <lxj@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834515}

--- chrome/browser/ui/ash/chrome_new_window_client.cc
+++ chrome/browser/ui/ash/chrome_new_window_client.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/file_manager/path_util.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/web_applications/chrome_camera_app_ui_delegate.h"
-#include "chrome/browser/extensions/api/terminal/terminal_extension_helper.h"
 #include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/prefs/incognito_mode_prefs.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -417,14 +416,12 @@ void ChromeNewWindowClient::OpenDownloadsFolder() {
 
 void ChromeNewWindowClient::OpenCrosh() {
   Profile* profile = ProfileManager::GetActiveUserProfile();
-  GURL crosh_url = extensions::TerminalExtensionHelper::GetCroshURL(profile);
-  if (!crosh_url.is_valid())
-    return;
   chrome::ScopedTabbedBrowserDisplayer displayer(profile);
   Browser* browser = displayer.browser();
   content::WebContents* page = browser->OpenURL(content::OpenURLParams(
-      crosh_url, content::Referrer(), WindowOpenDisposition::NEW_FOREGROUND_TAB,
-      ui::PAGE_TRANSITION_GENERATED, false));
+      GURL(chrome::kChromeUIUntrustedCroshURL), content::Referrer(),
+      WindowOpenDisposition::NEW_FOREGROUND_TAB, ui::PAGE_TRANSITION_GENERATED,
+      false));
   browser->window()->Show();
   browser->window()->Activate();
   page->Focus();
--- chrome/common/extensions/extension_constants.cc
+++ chrome/common/extensions/extension_constants.cc
@@ -39,8 +39,6 @@ const char kGooglePlayMusicAppId[] = "icppfcnhkcmnfdhfhphakoifcfokfdhg";
 const char kGooglePlusAppId[] = "dlppkpafhbajpcmmoheippocdidnckmm";
 const char kGoogleSheetsAppId[] = "felcaaldnbdncclmgdcncolpebgiejap";
 const char kGoogleSlidesAppId[] = "aapocclcgogkmnckokdopfmhonfmgoek";
-const char kHTermAppId[] = "pnhechapfaindjhompbnflcldabbghjo";
-const char kHTermDevAppId[] = "okddffdblfhhnmhodogpojmfkjmhinfp";
 const char kIdentityApiUiAppId[] = "ahjaciijnoiaklcomgnblndopackapon";
 const char kTextEditorAppId[] = "mmfbcljfglbokpmkimbfghdkjmjhdgbg";
 const char kInAppPaymentsSupportAppId[] = "nmmhkkegccagdldgiimedpiccmgmieda";
@@ -64,8 +62,6 @@ const char* const kBuiltInFirstPartyExtensionIds[] = {
     kGooglePlusAppId,
     kGoogleSheetsAppId,
     kGoogleSlidesAppId,
-    kHTermAppId,
-    kHTermDevAppId,
     kIdentityApiUiAppId,
     kTextEditorAppId,
     kInAppPaymentsSupportAppId,
