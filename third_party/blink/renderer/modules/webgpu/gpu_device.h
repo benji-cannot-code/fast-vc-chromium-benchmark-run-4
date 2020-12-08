24b89ab0acda4a86ad5a8d4ffce308695c7c9636
Jiawei Shao <jiawei.shao@intel.com>
2020-12-08 10:42:02 +0000

    [WebGPU] Add the entry point of CreateReadyRenderPipeline
    BUG=dawn:529
    
    Change-Id: I9cae2840f7e653372d9434a2d8a9ea386ede988b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2574062
    Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834653}

--- third_party/blink/renderer/modules/webgpu/gpu_device.h
+++ third_party/blink/renderer/modules/webgpu/gpu_device.h
@@ -93,6 +93,9 @@ class GPUDevice final : public EventTargetWithInlineData,
       const GPURenderPipelineDescriptor* descriptor);
   GPUComputePipeline* createComputePipeline(
       const GPUComputePipelineDescriptor* descriptor);
+  ScriptPromise createReadyRenderPipeline(
+      ScriptState* script_state,
+      const GPURenderPipelineDescriptor* descriptor);
   ScriptPromise createReadyComputePipeline(
       ScriptState* script_state,
       const GPUComputePipelineDescriptor* descriptor);
@@ -127,6 +130,10 @@ class GPUDevice final : public EventTargetWithInlineData,
                                WGPUErrorType type,
                                const char* message);
 
+  void OnCreateReadyRenderPipelineCallback(ScriptPromiseResolver* resolver,
+                                           WGPUCreateReadyPipelineStatus status,
+                                           WGPURenderPipeline render_pipeline,
+                                           const char* message);
   void OnCreateReadyComputePipelineCallback(
       ScriptPromiseResolver* resolver,
       WGPUCreateReadyPipelineStatus status,
