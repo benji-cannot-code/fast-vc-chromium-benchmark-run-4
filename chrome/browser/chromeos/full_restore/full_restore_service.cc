f1031659d820cf5c445a945df0f3fd5e549f54ff
Nancy Wang <nancylingwang@chromium.org>
2020-12-08 23:56:34 +0000

    Add unit tests for full_restore_service.cc
    BUG=1146900
    
    Change-Id: I3063503edd5942983cd51d14ef5a74d03cf12978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563150
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834955}

--- chrome/browser/chromeos/full_restore/full_restore_service.cc
+++ chrome/browser/chromeos/full_restore/full_restore_service.cc
@@ -23,22 +23,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/message_center/public/cpp/notification.h"
 
-namespace {
-
-constexpr char kRestoreForCrashNotificationId[] =
-    "restore_for_crash_notification";
-constexpr char kRestoreNotificationId[] = "restore_notification";
-
-// The restore notification button index.
-enum class RestoreNotificationButtonIndex {
-  kRestore = 0,
-  kCancel,
-};
-
-}  // namespace
-
 namespace chromeos {
 namespace full_restore {
+
+const char kRestoreForCrashNotificationId[] = "restore_for_crash_notification";
+const char kRestoreNotificationId[] = "restore_notification";
 
 FullRestoreService::FullRestoreService(Profile* profile) : profile_(profile) {
   // If the system crashed before reboot, show the restore notification.
