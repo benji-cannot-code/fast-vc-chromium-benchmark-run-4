2e4155a2854385259646121fb1cdbdc9d92ce706
Thiabaud Engelbrecht <thiabaud@google.com>
2020-12-08 14:13:31 +0000

    [discardable] Fix ClientDiscardableSharedMemoryManager teardown.
    ClientDiscardableMemoryManager was modified to be ref-counted to guard
    against potential use-after-frees, but without correctly arranging for
    teardown on its "home" sequence, as required by the WeakPtrFactory API.
    
    Based on https://chromium-review.googlesource.com/c/chromium/src/+/2577676.
    
    Bug: 1153322
    Change-Id: I7dca52cdf01ad4bdc95e8b87e57a9a7d92a28288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2578384
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834696}

--- components/discardable_memory/client/client_discardable_shared_memory_manager.h
+++ components/discardable_memory/client/client_discardable_shared_memory_manager.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/memory/discardable_memory_allocator.h"
-#include "base/memory/ref_counted.h"
+#include "base/memory/ref_counted_delete_on_sequence.h"
 #include "base/memory/unsafe_shared_memory_region.h"
 #include "base/synchronization/lock.h"
 #include "base/trace_event/memory_dump_provider.h"
@@ -36,7 +36,8 @@ DISCARDABLE_MEMORY_EXPORT extern const base::Feature kSchedulePeriodicPurge;
 class DISCARDABLE_MEMORY_EXPORT ClientDiscardableSharedMemoryManager
     : public base::DiscardableMemoryAllocator,
       public base::trace_event::MemoryDumpProvider,
-      public base::RefCountedThreadSafe<ClientDiscardableSharedMemoryManager> {
+      public base::RefCountedDeleteOnSequence<
+          ClientDiscardableSharedMemoryManager> {
  public:
   ClientDiscardableSharedMemoryManager(
       mojo::PendingRemote<mojom::DiscardableSharedMemoryManager> manager,
@@ -97,7 +98,10 @@ class DISCARDABLE_MEMORY_EXPORT ClientDiscardableSharedMemoryManager
   // These fields are only protected for testing, they would otherwise be
   // private. Everything else should be either public or private.
  protected:
-  friend class base::RefCountedThreadSafe<ClientDiscardableSharedMemoryManager>;
+  friend class base::RefCountedDeleteOnSequence<
+      ClientDiscardableSharedMemoryManager>;
+  friend class base::DeleteHelper<ClientDiscardableSharedMemoryManager>;
+
   ~ClientDiscardableSharedMemoryManager() override;
   ClientDiscardableSharedMemoryManager(
       scoped_refptr<base::SingleThreadTaskRunner> io_task_runner,
