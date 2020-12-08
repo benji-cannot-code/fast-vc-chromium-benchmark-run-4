a3d5ba1c0c08915e80a061d5e3c83079714bb677
Sean Harrison <harrisonsean@chromium.org>
2020-12-08 18:26:22 +0000

    [Privacy Sandbox] Move prefs to components
    In order to facilitate our use of cookie managed state to control
    privacy sandbox in the interim the privacy sandbox prefs need
    to live in components.
    
    We are adding a folder for privacy sandbox as the plan is to migrate
    all of privacy sandbox to components for M90, only prefs need to
    be moved for M89.
    
    Bug: 1152336
    Change-Id: I4fb65671c21d0d24af8e38f972260891e0285843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2572216
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#834765}

--- chrome/browser/privacy_sandbox/privacy_sandbox_settings.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_settings.cc
@@ -7,10 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/time/time.h"
-#include "chrome/browser/privacy_sandbox/privacy_sandbox_prefs.h"
 #include "chrome/common/chrome_features.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/prefs/pref_service.h"
+#include "components/privacy_sandbox/privacy_sandbox_prefs.h"
 #include "url/gurl.h"
 #include "url/origin.h"
 
