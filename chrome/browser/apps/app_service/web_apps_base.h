b2fff5e9a8a2877ed80973ed9a735c561801f3ac
Nancy Wang <nancylingwang@chromium.org>
2020-12-08 22:55:47 +0000

    Add the SaveAppLaunchInfo interface to save app launch information.
    This CL adds the SaveAppLaunchInfo interface, and the class
    AppLaunchInfo, as the parameter for SaveAppLaunchInfo.
    
    Modify web apps publisher to call SaveAppLaunchInfo to save the launch
    parameters for system web apps, and web apps not opened as a browser's
    tab.
    
    BUG=1146900
    
    Change-Id: Ide9b461835c0d554b60ddf1d47a673d2b41c8a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2576069
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834918}

--- chrome/browser/apps/app_service/web_apps_base.h
+++ chrome/browser/apps/app_service/web_apps_base.h
@@ -41,6 +41,8 @@ class WebAppRegistrar;
 
 namespace apps {
 
+struct AppLaunchParams;
+
 // An app publisher (in the App Service sense) of Web Apps.
 class WebAppsBase : public apps::PublisherBase,
                     public web_app::AppRegistrarObserver,
@@ -74,6 +76,8 @@ class WebAppsBase : public apps::PublisherBase,
       apps::mojom::IntentPtr intent,
       apps::mojom::LaunchSource launch_source,
       int64_t display_id);
+
+  virtual content::WebContents* LaunchAppWithParams(AppLaunchParams params);
 
   const mojo::RemoteSet<apps::mojom::Subscriber>& subscribers() const {
     return subscribers_;
--- components/full_restore/full_restore_utils.cc
+++ components/full_restore/full_restore_utils.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/full_restore/full_restore_utils.h"
 
+#include "ash/public/cpp/ash_features.h"
+#include "base/files/file_path.h"
+#include "components/full_restore/app_launch_info.h"
+
 namespace {
 
 // Set the default restore flag as false, and it can be reset based on the
@@ -14,6 +18,15 @@ bool g_restore = false;
 }  // namespace
 
 namespace full_restore {
+
+void SaveAppLaunchInfo(const base::FilePath& profile_dir,
+                       std::unique_ptr<AppLaunchInfo> app_launch_info) {
+  if (!ash::features::IsFullRestoreEnabled())
+    return;
+
+  // TODO(crbug.com/1146900): Save the app launch parameters to the full restore
+  // file.
+}
 
 bool ShouldRestore() {
   return g_restore;
