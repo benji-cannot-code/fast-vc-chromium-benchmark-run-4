52d6c24bd28884ad75762b78e4c984d4301a5b49
Asanka Herath <asanka@chromium.org>
2020-12-08 16:13:07 +0000

    [privacy_budget] Use Document's UkmRecorder.
    Use the Document's UkmRecorder and UkmSourceID instead of the
    ExecutionContext's parameters when there's a document available. This
    can make a difference when an element is being adoped and other
    lifecycle events where the ExecutionContext is not available.
    
    Bug: 1155600
    Bug: 1146855
    Change-Id: I5767caa9e7086fbfbffe5bc94c49aba6bbffa903
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2576634
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834722}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/checked_math.h"
 #include "build/build_config.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/privacy_budget/identifiability_metric_builder.h"
 #include "third_party/blink/public/common/privacy_budget/identifiability_metrics.h"
@@ -291,13 +292,13 @@ CanvasRenderingContext* HTMLCanvasElement::GetCanvasRenderingContext(
 
   if (IdentifiabilityStudySettings::Get()->ShouldSample(
           IdentifiableSurface::Type::kCanvasRenderingContext)) {
-    auto* context = GetExecutionContext();
-    IdentifiabilityMetricBuilder(context->UkmSourceID())
+    Document& doc = GetDocument();
+    IdentifiabilityMetricBuilder(doc.UkmSourceID())
         .Set(IdentifiableSurface::FromTypeAndToken(
                  IdentifiableSurface::Type::kCanvasRenderingContext,
                  CanvasRenderingContext::ContextTypeFromId(type)),
              !!result)
-        .Record(context->UkmRecorder());
+        .Record(doc.UkmRecorder());
   }
 
   if (ContentsCcLayer() != old_contents_cc_layer)
@@ -873,8 +874,7 @@ bool HTMLCanvasElement::IsPrinting() const {
 }
 
 UkmParameters HTMLCanvasElement::GetUkmParameters() {
-  auto* context = GetExecutionContext();
-  return {context->UkmRecorder(), context->UkmSourceID()};
+  return {GetDocument().UkmRecorder(), GetDocument().UkmSourceID()};
 }
 
 void HTMLCanvasElement::SetSurfaceSize(const IntSize& size) {
