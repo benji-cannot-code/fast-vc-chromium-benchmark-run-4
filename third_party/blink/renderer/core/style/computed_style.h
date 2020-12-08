153e49811a030e91fc4ac81c619a59acea3a3bea
Aleks Totic <atotic@chromium.org>
2020-12-08 20:17:30 +0000

    [TablesNG] width:min/max-content are not table-layout:fixed
    Fixed layout table should have definite length.
    Previously, we treated all !Auto width tables as fixed.
    
    Fixes external/wpt/css/css-tables/fixed-layout-2.html
    
    Bug: 958381
    Change-Id: I98996d3f4760166f06bafb94995709658f8234f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2578705
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834823}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -2263,7 +2263,8 @@ class ComputedStyle : public ComputedStyleBase,
 
   // Table layout utility functions.
   bool IsFixedTableLayout() const {
-    return TableLayout() == ETableLayout::kFixed && !LogicalWidth().IsAuto();
+    return TableLayout() == ETableLayout::kFixed &&
+           !LogicalWidth().IsAutoOrContentOrIntrinsic();
   }
 
   LogicalSize TableBorderSpacing() const {
