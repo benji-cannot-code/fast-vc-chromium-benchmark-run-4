89348ee3cb9cfdea5dc3d410f01e61f9955639e3
Benoit Lize <lizeb@chromium.org>
2020-12-08 14:59:38 +0000

    [PartitionAlloc] Don't touch allocator buckets on thread cache hits.
    When an allocation hits the thread cache, we still need (at least for
    some configurations) to get the allocated size, which is the bucket slot
    size. This requires touching PartitionRoot::buckets[bucket_index], which
    is not on the same cache line as the rest of the members we access in
    PartitionRoot.
    
    Inline the slot size in ThreadCache, to avoid that.
    
    Bug: 998048
    Change-Id: Ifad3857b4cadcecd853e7eb0e737a406dc40509f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566967
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834709}

--- base/allocator/partition_allocator/thread_cache.h
+++ base/allocator/partition_allocator/thread_cache.h
@@ -185,8 +185,8 @@ class BASE_EXPORT ThreadCache {
   // Returns nullptr for failure.
   //
   // Has the same behavior as RawAlloc(), that is: no cookie nor ref-count
-  // handling.
-  ALWAYS_INLINE void* GetFromCache(size_t bucket_index);
+  // handling. Sets |slot_size| to the allocated size upon success.
+  ALWAYS_INLINE void* GetFromCache(size_t bucket_index, size_t* slot_size);
 
   // Asks this cache to trigger |Purge()| at a later point. Can be called from
   // any thread.
@@ -208,12 +208,15 @@ class BASE_EXPORT ThreadCache {
 
   // TODO(lizeb): Once we have periodic purge, lower the ratio.
   static constexpr uint16_t kBatchFillRatio = 8;
+  static constexpr uint8_t kMaxCountPerBucket = 128;
 
  private:
   struct Bucket {
     PartitionFreelistEntry* freelist_head;
-    uint16_t count;
-    uint16_t limit;
+    // Want to keep sizeof(Bucket) small, using small types.
+    uint8_t count;
+    uint8_t limit;
+    uint16_t slot_size;
   };
   enum class Mode { kNormal, kPurge, kNotifyRegistry };
 
@@ -287,7 +290,8 @@ ALWAYS_INLINE bool ThreadCache::MaybePutInCache(void* address,
   return true;
 }
 
-ALWAYS_INLINE void* ThreadCache::GetFromCache(size_t bucket_index) {
+ALWAYS_INLINE void* ThreadCache::GetFromCache(size_t bucket_index,
+                                              size_t* slot_size) {
   PA_REENTRANCY_GUARD(is_in_thread_cache_);
   INCREMENT_COUNTER(stats_.alloc_count);
   // Only handle "small" allocations.
@@ -320,6 +324,7 @@ ALWAYS_INLINE void* ThreadCache::GetFromCache(size_t bucket_index) {
   bucket.count--;
   PA_DCHECK(bucket.count != 0 || !next);
   bucket.freelist_head = next;
+  *slot_size = bucket.slot_size;
 
   return result;
 }
