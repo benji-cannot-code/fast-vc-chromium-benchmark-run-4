1e12a550a07b3d458dd6f45508d0fa7174b80124
Sebastien Lalancette <seblalancette@chromium.org>
2020-12-08 21:34:22 +0000

    [Blink] Create New ScopedFakeUkmRecorder To Mock Remote UkmRecorders
    This new class will be used to mock the UKM recorder living in the
    browser process and being served UKM events over Mojo. When
    instantiated, this class will set itself automatically to receive UKM
    events, and those events will be made available for validation from
    this class' recorder instance.
    
    Cleaned-up the two fixtures that used a less-than-ideal approach to
    use this new class instead.
    
    Bug: 1153990
    Change-Id: I71ba82a0150af307e7d9e426f5c42476278229a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568353
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834874}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1693,7 +1693,6 @@ class CORE_EXPORT Document : public ContainerNode,
  private:
   friend class DocumentTest;
   friend class IgnoreDestructiveWriteCountIncrementer;
-  friend class TextFragmentSelectorGeneratorTest;
   friend class ThrowOnDynamicMarkupInsertionCountIncrementer;
   friend class IgnoreOpensDuringUnloadCountIncrementer;
   friend class NthIndexCache;
@@ -1707,8 +1706,6 @@ class CORE_EXPORT Document : public ContainerNode,
                            BeforeMatchExpandedHiddenMatchableUkm);
   FRIEND_TEST_ALL_PREFIXES(TextFinderSimTest,
                            BeforeMatchExpandedHiddenMatchableUkmNoHandler);
-  FRIEND_TEST_ALL_PREFIXES(TextFragmentAnchorMetricsTest, LinkOpenedSuccessUKM);
-  FRIEND_TEST_ALL_PREFIXES(TextFragmentAnchorMetricsTest, LinkOpenedFailedUKM);
   class NetworkStateObserver;
 
   friend class AXContext;
