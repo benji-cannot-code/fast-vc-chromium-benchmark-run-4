68abaa6831e36682e4401317041e935c0b18b67d
Vladimir Levin <vmpstr@chromium.org>
2020-12-08 15:35:03 +0000

    [content-visibility]: Don't dirty style from within style recalc.
    This patch changes the way we process style. Instead of marking style
    as dirty and needing to use a scoped allowed object, we skip dirtying
    style when being forced while in style.
    
    This happens when we're forcing a nested lock. If any of the style needs
    dirtying, then it will be updated via AdjustStyleRecalcChangeForChildren
    
    R=andruud@chromium.org, chrishtr@chromium.org
    
    Change-Id: I24a2f6347ea9c13cf5778c5ba029bb4de49ebff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2577577
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834712}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -222,7 +222,7 @@ class DisplayLockStyleScope {
   StyleRecalcChange AdjustStyleRecalcChangeForChildren(
       StyleRecalcChange change) {
     DCHECK(element_->GetDisplayLockContext());
-    DCHECK(!element_->GetDisplayLockContext()->IsLocked());
+    DCHECK(element_->GetDisplayLockContext()->ShouldStyleChildren());
     return element_->GetDisplayLockContext()
         ->AdjustStyleRecalcChangeForChildren(change);
   }
@@ -3108,7 +3108,7 @@ StyleRecalcChange Element::RecalcOwnStyle(const StyleRecalcChange change) {
     // context and as a result need to process more of the subtree than we would
     // normally. Note that if this is not the first time, then
     // AdjustStyleRecalcChangeForChildren() won't do any adjustments.
-    if (!context->IsLocked())
+    if (context->ShouldStyleChildren())
       child_change = context->AdjustStyleRecalcChangeForChildren(child_change);
   }
 
