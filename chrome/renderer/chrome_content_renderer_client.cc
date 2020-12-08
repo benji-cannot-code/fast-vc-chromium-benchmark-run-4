d1561642d564894ce6381355ed3fd3766a9a7b84
rajendrant <rajendrant@chromium.org>
2020-12-08 21:55:39 +0000

    Refactor redirect decision making to decider interface
    This CL introduces PublicResourceDecider interface that has the redirect
    decision making API that subclasses should implemeent. This class also
    has the mojo interface got interacting with browser process. This
    interface also has the RedirectResult enum that will have ineligible
    reasons for public image hints approach and robots checking based
    approach.
    
    PublicImageHintsDeciderAgent (rename of SubresourceRedirectHintsAgent)
    is introduced which is the subclass of the decider interface for public
    image hints approach.
    
    Change-Id: I5ef43c2ee821f7d5aa6501683c22e09464da7972
    Bug: 1152527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2572755
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834888}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -61,7 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/plugins/pdf_plugin_placeholder.h"
 #include "chrome/renderer/plugins/plugin_uma.h"
 #include "chrome/renderer/previews/resource_loading_hints_agent.h"
-#include "chrome/renderer/subresource_redirect/subresource_redirect_hints_agent.h"
+#include "chrome/renderer/subresource_redirect/public_image_hints_decider_agent.h"
 #include "chrome/renderer/subresource_redirect/subresource_redirect_params.h"
 #include "chrome/renderer/sync_encryption_keys_extension.h"
 #include "chrome/renderer/url_loader_throttle_provider_impl.h"
@@ -608,7 +608,7 @@ void ChromeContentRendererClient::RenderFrameCreated(
   new previews::ResourceLoadingHintsAgent(associated_interfaces, render_frame);
 
   if (subresource_redirect::IsPublicImageHintsBasedCompressionEnabled())
-    new subresource_redirect::SubresourceRedirectHintsAgent(
+    new subresource_redirect::PublicImageHintsDeciderAgent(
         associated_interfaces, render_frame);
 
   if (translate::IsSubFrameTranslationEnabled()) {
