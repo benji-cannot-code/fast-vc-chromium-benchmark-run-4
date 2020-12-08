850b03208e5503bdc37f015604fc9c86fa464fb2
Victor Costan <pwnall@chromium.org>
2020-12-08 09:13:08 +0000

    base: Fix complexity of base::ranges::make_heap.
    make_heap is linear in the size of the input, not logarithmic.
    
    Change-Id: I88a7c78bfdae4c7dfd09efa433febc6ffdd2d9a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2577034
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834625}

--- base/ranges/algorithm.h
+++ base/ranges/algorithm.h
@@ -4300,7 +4300,7 @@ constexpr auto pop_heap(Range&& range, Comp comp = {}, Proj proj = {}) {
 //
 // Returns: `last`.
 //
-// Complexity: At most `3 log(last - first)` comparisons and twice as many
+// Complexity: At most `3 * (last - first)` comparisons and twice as many
 // projections.
 //
 // Reference: https://wg21.link/make.heap#:~:text=ranges::make_heap(I
@@ -4324,7 +4324,7 @@ constexpr auto make_heap(RandomAccessIterator first,
 //
 // Returns: `end(range)`.
 //
-// Complexity: At most `3 log(size(range))` comparisons and twice as many
+// Complexity: At most `3 * size(range)` comparisons and twice as many
 // projections.
 //
 // Reference: https://wg21.link/make.heap#:~:text=ranges::make_heap(R
