da39ef8dadafeeb5259c8891c0dd29545b843f57
Luciano Pacheco <lucmult@chromium.org>
2020-12-08 08:54:21 +0000

    Files app: Generate JS module for <files-password-dialog> and its test
    Bug: 1133186
    Change-Id: Ifc81fa000fa91a69e8eed2450b9d8fbe4c61dd7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2576859
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834622}

--- ui/file_manager/file_manager/foreground/elements/files_password_dialog.js
+++ ui/file_manager/file_manager/foreground/elements/files_password_dialog.js
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// #import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+// #import {AsyncUtil} from '../../common/js/async_util.m.js';
+
 /**
  * FilesPasswordDialog template.
  * @const @type {string}
@@ -55,7 +58,7 @@ const filesPasswordDialogTemplate = `
  * FilesPasswordDialog.USER_CANCELLED.
  * @extends HTMLElement
  */
-class FilesPasswordDialog extends HTMLElement {
+/* #export */ class FilesPasswordDialog extends HTMLElement {
   constructor() {
     /*
      * Create element content.
