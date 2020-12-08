5619eac382774ac030a919042da5f30b18fe7678
Ben Wagner <benjaminwagner@google.com>
2020-12-08 21:56:54 +0000

    Mark some WebRTC perf measurements as biggerIsBetter
    Change-Id: I95a145f0380b0f413a71ef5a9bbacf840eb20b9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2577205
    Commit-Queue: John Chen <johnchen@chromium.org>
    Auto-Submit: Ben Wagner aka dogben <benjaminwagner@google.com>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834889}

--- tools/perf/page_sets/webrtc_cases.py
+++ tools/perf/page_sets/webrtc_cases.py
@@ -49,7 +49,7 @@ class DataChannel(WebrtcPage):
   def ParseTestResults(self, action_runner):
     self.AddJavaScriptMeasurement(
         'data_transferred',
-        'bytes',
+        'bytes_biggerIsBetter',
         'receiveProgress.value',
         description='Amount of data transferred by data channel in 10 seconds')
 
@@ -156,7 +156,7 @@ class InsertableStreamsVideoProcessing(WebrtcPage):
 
   def ExecuteTest(self, action_runner):
     self.AddMeasurement(
-        'supported', 'count', 1 if self.supported else 0,
+        'supported', 'count_biggerIsBetter', 1 if self.supported else 0,
         'Boolean flag indicating if this benchmark is supported by the browser.'
     )
     if not self.supported:
@@ -179,7 +179,7 @@ class InsertableStreamsVideoProcessing(WebrtcPage):
       action_runner.Wait(10)
     self.AddJavaScriptMeasurement(
         'sink_decoded_frames',
-        'count',
+        'count_biggerIsBetter',
         'document.querySelector(".sinkVideo").webkitDecodedFrameCount',
         description='Number of frames received at the sink video.')
 
