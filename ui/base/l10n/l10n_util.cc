bd9c74f633034e3e465cbd43e2f570a665a13bf1
Megan Jablonski <megjablon@chromium.org>
2020-12-08 01:49:55 +0000

    Add launched Translate languages to kDefaultSupportedLanguages
    "rw", "or", "tt", "tk", and "ug" were launched by the Translate service
    in March. Add those that are supported by l10n to the default Translate
    supported languages list. Also update translate and l10n comment names
    to match those in the Translate service.
    
    Bug: 1150514
    Change-Id: Ifd69d003c1f2b917af3ac57b2631a566c33f3853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2561268
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Megan Jablonski <megjablon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834508}

--- ui/base/l10n/l10n_util.cc
+++ ui/base/l10n/l10n_util.cc
@@ -169,7 +169,7 @@ static const char* const kAcceptLanguageList[] = {
     "ny",      // Nyanja
     "oc",      // Occitan
     "om",      // Oromo
-    "or",      // Oriya
+    "or",      // Odia (Oriya)
     "pa",      // Punjabi
     "pl",      // Polish
     "ps",      // Pashto
@@ -204,7 +204,7 @@ static const char* const kAcceptLanguageList[] = {
     "tr",      // Turkish
     "tt",      // Tatar
     "tw",      // Twi
-    "ug",      // Uighur
+    "ug",      // Uyghur
     "uk",      // Ukrainian
     "ur",      // Urdu
     "uz",      // Uzbek
