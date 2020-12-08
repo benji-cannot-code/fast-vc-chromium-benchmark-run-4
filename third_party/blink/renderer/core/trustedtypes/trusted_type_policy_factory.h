b077daa5dc1450f31bf2b0b7e031f36b2d205173
Daniel Vogelheim <vogelheim@chromium.org>
2020-12-08 09:52:14 +0000

    [Trusted Types] Use same .idl for code generation and testing.
    Fix-it week: Fix failing WPT test.
    
    Change-Id: I3bf43d39f19bc9b690bb943185e7ddb8961699da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2574951
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834636}

--- third_party/blink/renderer/core/trustedtypes/trusted_type_policy_factory.h
+++ third_party/blink/renderer/core/trustedtypes/trusted_type_policy_factory.h
@@ -32,6 +32,7 @@ class CORE_EXPORT TrustedTypePolicyFactory final
   explicit TrustedTypePolicyFactory(ExecutionContext*);
 
   // TrustedTypePolicyFactory.idl
+  TrustedTypePolicy* createPolicy(const String&, ExceptionState&);
   TrustedTypePolicy* createPolicy(const String&,
                                   const TrustedTypePolicyOptions*,
                                   ExceptionState&);
