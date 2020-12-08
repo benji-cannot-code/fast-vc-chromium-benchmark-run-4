e4c28f51d4962adba46ae4469689590a1810898a
jchen10 <jie.a.chen@intel.com>
2020-12-08 03:14:24 +0000

    Mark WebCodecs as special token
    This makes 'WebCodecs' follow the naming rule as other modules like
    'WebSocket', 'WebGL', otherwise it would lead to unusual names like
    'webgl_web_codecs_video_frame.h'.
    
    Bug: 1142754
    Change-Id: I2280e3340be9adf04cd51d426a61f51b590493da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2570395
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jie A Chen <jie.a.chen@intel.com>
    Cr-Commit-Position: refs/heads/master@{#834543}

--- third_party/blink/renderer/modules/webcodecs/video_encoder.h
+++ third_party/blink/renderer/modules/webcodecs/video_encoder.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_codec_state.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_video_encoder_output_callback.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_web_codecs_error_callback.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_webcodecs_error_callback.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/modules/webcodecs/codec_logger.h"
 #include "third_party/blink/renderer/modules/webcodecs/video_frame.h"
