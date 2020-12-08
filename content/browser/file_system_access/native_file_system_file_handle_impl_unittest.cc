9222f10f392bfeeeba08474e46f072bc7d056270
Marijn Kruisselbrink <mek@chromium.org>
2020-12-08 07:20:47 +0000

    Reland "Reland "[FSA] Add IsSafePathComponent checks to GetFile/GetDirectoryHandle.""
    This is a reland of 2d41c3952d2851948a09ddcf3e97bae6c419b024
    
    The added test was modified to no longer assert that all unsafe files
    were written to disk successfully. This should make the test pass (albeit
    with less stringent checks) on file systems/platforms that don't allow
    all unsafe file names.
    
    Original change's description:
    > Reland "[FSA] Add IsSafePathComponent checks to GetFile/GetDirectoryHandle."
    >
    > This is a reland of 004377929febd7cf7392932b01df7f4a0a362679
    >
    > The main difference is to make sure iterating over a directory doesn't
    > return files we don't want to expose either (and not CHECK failing if
    > such files are found when iterating).
    >
    > Original change's description:
    > > [FSA] Add IsSafePathComponent checks to GetFile/GetDirectoryHandle.
    > >
    > > This isn't directly using net::IsSafePortablePathComponent since what
    > > is safe for the File System Access API is not the same as what is safe
    > > for Downloads. As such currently this duplicates a lot of the
    > > implementation of this method, but in a followup we should attempt to
    > > unify these two implementations as much as possible.
    > >
    > > Bug: 1150810, 1154757
    > > Change-Id: Iba4c92ef5f1cd924aa22b9dd201762d48b4bbc3b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568383
    > > Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    > > Reviewed-by: Victor Costan <pwnall@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#833042}
    >
    > Bug: 1150810
    > Bug: 1154757
    > Change-Id: I3341b9824a1ac4cbd6f100355960ad55b01f0753
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575370
    > Commit-Queue: Victor Costan <pwnall@chromium.org>
    > Reviewed-by: Victor Costan <pwnall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#834118}
    
    Bug: 1150810
    Bug: 1154757
    Change-Id: Ie5cad9a7b2383c89b96e8a7be6cfe75ad2555fa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2577614
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834598}

--- content/browser/file_system_access/native_file_system_file_handle_impl_unittest.cc
+++ content/browser/file_system_access/native_file_system_file_handle_impl_unittest.cc
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/test/test_file_system_context.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using storage::FileSystemURL;
-
 namespace content {
 
 using blink::mojom::PermissionStatus;
