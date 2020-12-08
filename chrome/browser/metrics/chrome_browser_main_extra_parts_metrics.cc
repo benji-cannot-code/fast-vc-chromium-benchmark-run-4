2aab6d668acc2989f27b42e96bc8a793446b975f
Anton Bikineev <bikineev@chromium.org>
2020-12-08 14:48:13 +0000

    base/allocator: Record a synthetic field trial: PAE-and-PCScan
    In order to perform a tri-arm experiment of
    PartitionAlloc-Everywhere and PCScan, adds a new synthetic
    field trial.
    
    Bug: 1121427
    Change-Id: I70a1b8384cd066cea2d92000364877e965d6b82c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2573560
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834706}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 #include <string>
 
+#include "base/allocator/partition_allocator/partition_alloc_features.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/cpu.h"
@@ -547,11 +548,14 @@ void ChromeBrowserMainExtraPartsMetrics::PreBrowserStart() {
 
 #endif  // defined(OS_WIN)
 
-  // Records whether or not PartitionAlloc is used as the default allocator.
+  // Records whether or not PartitionAlloc is used as the default allocator,
+  // plus whether or not PCScan is enabled.
   ChromeMetricsServiceAccessor::RegisterSyntheticFieldTrial(
-      "PartitionAllocEverywhere",
+      "PartitionAllocEverywhereAndPCScan",
 #if BUILDFLAG(USE_PARTITION_ALLOC_EVERYWHERE)
-      "Enabled"
+      base::features::IsPartitionAllocPCScanBrowserOnlyEnabled()
+          ? "EnabledWithPCScan"
+          : "EnabledWithoutPCScan"
 #else
       "Disabled"
 #endif
--- content/app/content_main_runner_impl.cc
+++ content/app/content_main_runner_impl.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/allocator_extension.h"
 #include "base/allocator/allocator_shim.h"
 #include "base/allocator/buildflags.h"
+#include "base/allocator/partition_allocator/partition_alloc_features.h"
 #include "base/at_exit.h"
 #include "base/base_switches.h"
 #include "base/bind.h"
@@ -247,12 +248,9 @@ void EnablePCScanForMallocPartitionsIfNeeded() {
 }
 
 void EnablePCScanForMallocPartitionsInBrowserProcessIfNeeded() {
-  static const base::Feature kPartitionAllocPCScanBrowserOnly{
-      "PartitionAllocPCScanBrowserOnly", base::FEATURE_DISABLED_BY_DEFAULT};
-  (void)kPartitionAllocPCScanBrowserOnly;
 #if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
   CHECK(base::FeatureList::GetInstance());
-  if (base::FeatureList::IsEnabled(kPartitionAllocPCScanBrowserOnly)) {
+  if (base::features::IsPartitionAllocPCScanBrowserOnlyEnabled()) {
     base::allocator::EnablePCScan();
   }
 #endif
