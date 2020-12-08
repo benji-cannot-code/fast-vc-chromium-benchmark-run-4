009691d19c21c0a82864380fb8e909da20905cbb
Nancy Wang <nancylingwang@chromium.org>
2020-12-08 21:38:09 +0000

    Add the OnSyncedPrefIsSynced interface to notify the pref is synced.
    For a new user, when the system starts, the "restore_apps_and_pages"
    pref could be set for below scenarios based on the ui review deck:
    https://docs.google.com/presentation/d/1iCqnD3IEeTpy5rstMq5Gw_PfYEMFCPVjrYi9TtLtjQg/edit#slide=id.g838f61fb61_2_5
    
    1. At startup, both the OS restore pref "restore_apps_and_pages" and
    the browser's "restore_on_startup" are empty, so the
    "restore_apps_and_pages"'s value is initialized as |kAskEveryTime|.
    
    2. When synced, there are 3 scenarios:
    A. The OS restore pref "restore_apps_and_pages" is not empty in the
    init sync list, then don't update the "restore_apps_and_pages"'s value,
    and use the value from sync.
    B. The OS restore pref "restore_apps_and_pages" is empty in the init
    sync list, and "restore_apps_and_pages" is set by the user, then don't
    update the "restore_apps_and_pages"'s value, and use the value set by
    the user.
    C. The OS restore pref "restore_apps_and_pages" is empty in the init
    sync list, "restore_apps_and_pages" is not set by the user, and the
    browser's "restore_on_startup" is not empty, reset the
    "restore_apps_and_pages" value based on the browser's setting
    "restore_on_startup".
    
    However, currently we don't have a notification to know whether the
    pref item is listed in the sync init list, so we don't have a good way
    to know whether we should keep the sync value or modify based on the
    other synced pref(e.g. synced browser's restore pref)
    
    To address the issue, this CL adds a OnStartedSyncing interface. When
    merge data and start syncing, if the pref path is list in the init sync
    data, call OnStartedSyncing to notify the observer.
    
    BUG=1146900
    
    Change-Id: I5f3e9462f6e4bcbf38b0aeff6cac4515a3c40b46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575777
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834881}

--- components/sync_preferences/pref_model_associator.cc
+++ components/sync_preferences/pref_model_associator.cc
@@ -224,6 +224,7 @@ PrefModelAssociator::MergeDataAndStartSyncing(
 
     remaining_preferences.erase(sync_pref_name);
     InitPrefAndAssociate(*sync_iter, sync_pref_name, &new_changes);
+    NotifyStartedSyncing(sync_pref_name);
   }
 
   // Go through and build sync data for any remaining preferences.
@@ -633,6 +634,15 @@ void PrefModelAssociator::EnforceRegisteredTypeInStore(
                             true);
     }
   }
+}
+
+void PrefModelAssociator::NotifyStartedSyncing(const std::string& path) const {
+  auto observer_iter = synced_pref_observers_.find(path);
+  if (observer_iter == synced_pref_observers_.end())
+    return;
+
+  for (auto& observer : *observer_iter->second)
+    observer.OnStartedSyncing(path);
 }
 
 }  // namespace sync_preferences
