a2b76befd7501be594da5d6df0bfd45a4ee69bde
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-12-08 22:27:29 +0000

    Import wpt@5fa6b7cbc4a02a2a5f3d23846373ba38acfe9d18
    Using wpt-import in Chromium 2cf4c898c653c0b67455eb31d0120e5b3bc5976f.
    With Chromium commits locally applied on WPT:
    86af9c7886 "Revert "[blinkpy] Re-enable four css-background/ tests that need fuzzy reftest""
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=lpz@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Id531ca6bddb13fb73b4e899ea31c8b2d3beb56fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2580143
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#834904}

--- third_party/blink/web_tests/external/wpt/html/cross-origin-opener-policy/reporting/resources/redirect.py
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-opener-policy/reporting/resources/redirect.py
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from wptserve.utils import isomorphic_encode
+
 def main(request, response):
     response.status = 302
-    response.headers.set(b"Location", request.url[request.url.find('?')+1:])
+    response.headers.set(b"Location", isomorphic_encode(request.url[request.url.find(u'?')+1:]))
