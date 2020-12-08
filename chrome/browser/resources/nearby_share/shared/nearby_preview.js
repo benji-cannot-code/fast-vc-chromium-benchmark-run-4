8a666a94ed6689b031e79772e865549d5c58bae6
James Vecore <vecore@google.com>
2020-12-08 16:39:40 +0000

    [Nearby] Move nearby-preview to be shared
    This is a mechanical change to move the nearby-preview Polymer component
    from being chrome://nearby (Poylmer3) only to being shared with
    chrome://os-settings (Polymer2/3). This necessary for Nearby Share's
    launch despite Polymer2 being phased out (the conversion won't
    be complete in time for M-89 launch).
    
    Bug: 1156035
    
    Change-Id: If9a326c9646fb5c7861322e0f4c7e6a488dadb52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575949
    Commit-Queue: James Vecore <vecore@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834728}

--- chrome/browser/resources/nearby_share/shared/nearby_preview.js
+++ chrome/browser/resources/nearby_share/shared/nearby_preview.js
@@ -8,18 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * sent to a remote device. The data might be some plain text, a URL or a file.
  */
 
-import './shared/nearby_shared_share_type_icons.m.js';
-
-import {assert, assertNotReached} from 'chrome://resources/js/assert.m.js';
-import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
-import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-
 Polymer({
   is: 'nearby-preview',
 
   behaviors: [I18nBehavior],
-
-  _template: html`{__html_template__}`,
 
   properties: {
     /**
