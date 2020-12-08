e0f151d0e03ab5e48f29bddaa8af2c04859036d4
Sammie Quon <sammiequon@chromium.org>
2020-12-08 02:47:11 +0000

    capture_mode: Properly handle display rotation and bounds change.
    For all modes for either rotation/bounds change:
      - End current drag
      - Redraw capture session layer
      - Reposition capture label and capture bar
    
    Additionally for region:
      - Reset selection region if rotation changes
      - Clamp the selection region to root window bounds if size changes
    
    Also fixes a bug where the user can select a large region on a display,
    exit and then enter capture mode on a smaller display. This CL will
    ensure the initial region is clamped to size of the smaller display.
    
    Fixed: 1154465
    Test: manual + added
    Change-Id: I4dde6b97af9532d08f7e53348d7a8a28c0c38714
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568669
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834529}

--- ash/capture_mode/capture_mode_controller.cc
+++ ash/capture_mode/capture_mode_controller.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/capture_mode/capture_mode_controller.h"
 
-#include <memory>
-#include <string>
 #include <utility>
 
 #include "ash/capture_mode/capture_mode_metrics.h"
