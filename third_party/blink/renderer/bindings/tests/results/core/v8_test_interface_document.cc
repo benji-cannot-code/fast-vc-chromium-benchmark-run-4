472195aeabc5921033f2ccb82199193dd5bf27a4
Yuki Shiino <yukishiino@chromium.org>
2020-12-08 00:51:11 +0000

    v8bindings: Fix the test expectation of run_bindings_tests.py
    Just updates the test expectation by running:
    third_party/blink/tools/run_bindings_tests.py --reset-results
    
    Change-Id: If7ca26d339d4045ea0bb9b62fe32fa8bdc0615b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2576985
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834478}

--- third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_document.cc
+++ third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_document.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_dom_configuration.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_location.h"
 #include "third_party/blink/renderer/core/animation/document_animation.h"
+#include "third_party/blink/renderer/core/document_transition/document_create_transition.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/fullscreen/document_fullscreen.h"
 #include "third_party/blink/renderer/core/svg/svg_document_extensions.h"
