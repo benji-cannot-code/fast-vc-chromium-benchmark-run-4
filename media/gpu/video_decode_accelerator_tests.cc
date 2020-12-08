7055dbcf4bbd7bdf8bdf137d8e3e67d3f8469174
Hirokazu Honda <hiroh@chromium.org>
2020-12-08 02:35:51 +0000

    media/gpu/vaapi: Enable VaapiVideoDecoder to decode av1 10bit streams
    This adds av1 10 bit streams decoding support to VaapiVideoDecoder.
    
    Bug: b:172223934, b:172217032
    Test: video_decode_accelerator_tests --use_vd bear-320x180-10bit.av1.ivf
    Change-Id: I1c50516dba300fbe5e4b1056e931fb7da7264ef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563183
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: David Staessens <dstaessens@chromium.org>
    Auto-Submit: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834524}

--- media/gpu/video_decode_accelerator_tests.cc
+++ media/gpu/video_decode_accelerator_tests.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/files/file_util.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/strings/string_number_conversions.h"
 #include "media/base/test_data_util.h"
 #include "media/gpu/test/video.h"
@@ -104,14 +105,15 @@ class VideoDecoderTest : public ::testing::Test {
             output_folder, g_env->GetFrameOutputFormat(),
             g_env->GetFrameOutputLimit());
       }
-
-      // VP9 profile 2 supports 10 and 12 bit color depths, but we currently
-      // assume a profile 2 stream contains 10 bit color depth only.
-      // TODO(hiroh): Add bit depth info to Video class and follow it here.
+      if (g_env->Video()->BitDepth() != 8u &&
+          g_env->Video()->BitDepth() != 10u) {
+        LOG(ERROR) << "Unsupported bit depth: "
+                   << base::strict_cast<int>(g_env->Video()->BitDepth());
+        return nullptr;
+      }
       const VideoPixelFormat validation_format =
-          g_env->Video()->Profile() == VP9PROFILE_PROFILE2
-              ? PIXEL_FORMAT_YUV420P10
-              : PIXEL_FORMAT_I420;
+          g_env->Video()->BitDepth() == 10 ? PIXEL_FORMAT_YUV420P10
+                                           : PIXEL_FORMAT_I420;
       frame_processors.push_back(media::test::MD5VideoFrameValidator::Create(
           video->FrameChecksums(), validation_format, std::move(frame_writer)));
     }
