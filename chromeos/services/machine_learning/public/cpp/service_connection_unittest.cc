f9e3dc33662a24f189eb522bd115b7355d0a323b
Rob Schonberger <robsc@chromium.org>
2020-12-09 01:22:03 +0000

    ml: Add Soda Recognizer client mojom to Chrome.
    Adds in the soda speech recognizer client to chrome, talking to ml
    service, with appropriate mojo bindings.
    
    Add service connection bindings with appropriate fakes, and unittests
    for creation (along with appropriate expectations on creation for
    fake.
    
    TEST=tested via the unit test /  compilation here.
    
    Bug: 1152254
    Change-Id: Ib39362e3aebef732efe4eaba29ca073653737a81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2562924
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834990}

--- chromeos/services/machine_learning/public/cpp/service_connection_unittest.cc
+++ chromeos/services/machine_learning/public/cpp/service_connection_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/run_loop.h"
+#include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "base/threading/thread.h"
 #include "chromeos/dbus/machine_learning/machine_learning_client.h"
@@ -92,6 +93,32 @@ TEST_F(ServiceConnectionTest, LoadHandwritingModelWithSpec) {
       mojom::HandwritingRecognizerSpec::New("en"),
       handwriting_recognizer.BindNewPipeAndPassReceiver(),
       base::BindOnce([](mojom::LoadModelResult result) {}));
+}
+
+class TestSodaClient : public mojom::SodaClient {};
+
+// Tests that LoadSpeechRecognizer runs OK without a crash in a basic Mojo
+// Environment.
+TEST_F(ServiceConnectionTest, LoadSpeechRecognizerAndCallback) {
+  mojo::Remote<mojom::SodaRecognizer> soda_recognizer;
+  TestSodaClient test_client;
+  FakeServiceConnectionImpl fake_service_connection;
+  ServiceConnection::UseFakeServiceConnectionForTesting(
+      &fake_service_connection);
+  mojo::Receiver<mojom::SodaClient> soda_client{&test_client};
+  bool callback_done = false;
+  auto config = mojom::SodaConfig::New();
+  base::RunLoop run_loop;
+  ServiceConnection::GetInstance()->LoadSpeechRecognizer(
+      std::move(config), soda_client.BindNewPipeAndPassRemote(),
+      soda_recognizer.BindNewPipeAndPassReceiver(),
+      base::BindLambdaForTesting([&](mojom::LoadModelResult result) {
+        callback_done = true;
+        EXPECT_EQ(result, mojom::LoadModelResult::OK);
+        run_loop.Quit();
+      }));
+  run_loop.Run();
+  ASSERT_TRUE(callback_done);
 }
 
 // Tests that LoadGrammarChecker runs OK (no crash) in a basic Mojo environment.
