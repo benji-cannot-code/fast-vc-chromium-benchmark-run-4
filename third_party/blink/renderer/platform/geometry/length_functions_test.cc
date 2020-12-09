dc5e92888d8c1b1381246c499379c94100051a73
Xiaocheng Hu <xiaochengh@chromium.org>
2020-12-09 18:45:39 +0000

    Clamp result to float range when resolving percentage lengths
    Bug: 1155018
    Change-Id: Ia2c76552745a9b1499f7339f00aacb6c34203e2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2578376
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835277}

---
+++ third_party/blink/renderer/platform/geometry/length_functions_test.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/platform/geometry/length_functions.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace blink {
+
+TEST(LengthFunctionsTest, OutOfRangePercentage) {
+  Length max = Length::Percent(std::numeric_limits<float>::max());
+  float value = FloatValueForLength(max, 800);
+  EXPECT_TRUE(isfinite(value));
+}
+
+}  // namespace blink
