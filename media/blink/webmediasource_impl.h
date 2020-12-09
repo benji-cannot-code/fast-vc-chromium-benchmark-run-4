ff89b1cf747f49255589cf521f97ae653cca639d
Matt Wolenetz <wolenetz@chromium.org>
2020-12-09 04:18:48 +0000

    [MSE][webcodecs] Plumb AddSourceBufferUsingConfig to ChunkDemuxer AddId
    This change:
    
    1) Exposes as static synchronous helpers the WebCodecs MakeMediaConfig
       methods' internals so that MediaSource can use them to obtain
       media::{Audio,Video}DecoderConfigs from WebCodecs
       {Audio,Video}DecoderConfigs.
    
    2) Implements MediaSource::AddSourceBufferUsingConfig for encoded
       configs (not decodedMediaType), plumbing the media decoder config
       through the usual steps in MSE addSourceBuffer, and through new
       methods in WebMediaSource and WebMediaSourceImpl to let
       ChunkDemuxer::AddId know it will be expected to handle WebCodecs
       encoded chunk appends for the associated SourceBuffer.
    
    3) Adds a tentative folder for MSE-for-WebCodecs web_tests and adds
       a test for the current behavior of addSourceBuffer(webcodecs decoder
       config) that this CL adds.
    
    Later changes will continue plumbing of the configs in ChunkDemuxer and
    SourceBufferState, and also add implementations and ChunkDemuxer/etc
    support for appendEncoded{Audio,Video}Chunks. The h264 parser and avcc
    conditionally created in #1, above, could then be used by SourceBuffer
    processing of appended EncodedVideoChunks, too.
    
    BUG=1144908
    
    Change-Id: I90c1d90c3a28d5cc1e33b1e50e32f4cfea639784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2548844
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835058}

--- media/blink/webmediasource_impl.h
+++ media/blink/webmediasource_impl.h
@@ -14,7 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_media_source.h"
 
 namespace media {
+class AudioDecoderConfig;
 class ChunkDemuxer;
+class VideoDecoderConfig;
 
 class MEDIA_BLINK_EXPORT WebMediaSourceImpl : public blink::WebMediaSource {
  public:
@@ -22,9 +24,16 @@ class MEDIA_BLINK_EXPORT WebMediaSourceImpl : public blink::WebMediaSource {
   ~WebMediaSourceImpl() override;
 
   // blink::WebMediaSource implementation.
-  AddStatus AddSourceBuffer(const blink::WebString& content_type,
-                            const blink::WebString& codecs,
-                            blink::WebSourceBuffer** source_buffer) override;
+  std::unique_ptr<blink::WebSourceBuffer> AddSourceBuffer(
+      const blink::WebString& content_type,
+      const blink::WebString& codecs,
+      AddStatus& out_status /* out */) override;
+  std::unique_ptr<blink::WebSourceBuffer> AddSourceBuffer(
+      std::unique_ptr<AudioDecoderConfig> audio_config,
+      AddStatus& out_status /* out */) override;
+  std::unique_ptr<blink::WebSourceBuffer> AddSourceBuffer(
+      std::unique_ptr<VideoDecoderConfig> video_config,
+      AddStatus& out_status /* out */) override;
   double Duration() override;
   void SetDuration(double duration) override;
   void MarkEndOfStream(EndOfStreamStatus status) override;
--- third_party/blink/renderer/modules/mediasource/media_source.h
+++ third_party/blink/renderer/modules/mediasource/media_source.h
@@ -25,6 +25,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "third_party/blink/renderer/platform/wtf/threading_primitives.h"
 
+namespace media {
+class AudioDecoderConfig;
+class VideoDecoderConfig;
+}  // namespace media
+
 namespace blink {
 
 class EventQueue;
@@ -173,6 +178,8 @@ class MediaSource final : public EventTargetWithInlineData,
   // also require the same, since they can be called from within these methods.
   void AddSourceBuffer_Locked(
       const String& type /* in */,
+      std::unique_ptr<media::AudioDecoderConfig> audio_config /* in */,
+      std::unique_ptr<media::VideoDecoderConfig> video_config /* in */,
       ExceptionState* exception_state /* in/out */,
       SourceBuffer** created_buffer /* out */,
       MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */)
@@ -195,10 +202,12 @@ class MediaSource final : public EventTargetWithInlineData,
 
   bool IsUpdating() const;
 
-  std::unique_ptr<WebSourceBuffer> CreateWebSourceBuffer(const String& type,
-                                                         const String& codecs,
-                                                         ExceptionState&)
-      LOCKS_EXCLUDED(attachment_link_lock_);
+  std::unique_ptr<WebSourceBuffer> CreateWebSourceBuffer(
+      const String& type,
+      const String& codecs,
+      std::unique_ptr<media::AudioDecoderConfig> audio_config,
+      std::unique_ptr<media::VideoDecoderConfig> video_config,
+      ExceptionState&) LOCKS_EXCLUDED(attachment_link_lock_);
   void ScheduleEvent(const AtomicString& event_name);
   static void RecordIdentifiabilityMetric(ExecutionContext* context,
                                           const String& type,
