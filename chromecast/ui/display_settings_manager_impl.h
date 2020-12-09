6c340e7cff316c4cefa3292722edb4923ffaf10d
kenkangxgwe <kenkangxgwe@google.com>
2020-12-09 03:55:23 +0000

    [Cast] Abstract screen power off logic to an interface.
    * Extract original implementation from display_settings_manager_impl.cc to
    screen_power_controller_aura.cc
    * This allows the display to be enabled/disabled when allow_screen_power_off_ is
    set to be false/true after screen is off.
    * This handles multiple power requests at the same time (e.g. SetScreenOn() and
    SetScreenOff() at the same time).
    * Add logs when screen is successfully powered on/off.
    * Add unittest to ScreenPowerControllerAura.
    
    Bug: b/162546246
    Bug: b/173504008
    Test: cast_display_settings_unittests --single-process-tests
    --gtest_filter=ScreenPowerControllerAuraTest.*
    Test: logcat | grep "screen_power_controller", mute the mic, in low light:
    display off mode, and toggle the display timeout on and off. Logs should show
    that "Screen is powered on/off".
    
    Change-Id: If470d72eeb11c53e6dea8eefbd63f71e0f6c4dd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2391333
    Commit-Queue: Mingyu Kang <kenkangxgwe@google.com>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835044}

--- chromecast/ui/display_settings_manager_impl.h
+++ chromecast/ui/display_settings_manager_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/weak_ptr.h"
+#include "chromecast/ui/display_settings/screen_power_controller.h"
 #include "chromecast/ui/display_settings_manager.h"
 #include "chromecast/ui/mojom/display_settings.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -33,6 +34,7 @@ class CastDisplayConfigurator;
 }
 
 class DisplaySettingsManagerImpl : public DisplaySettingsManager,
+                                   public ScreenPowerController::Delegate,
                                    public mojom::DisplaySettings {
  public:
   DisplaySettingsManagerImpl(
@@ -55,6 +57,12 @@ class DisplaySettingsManagerImpl : public DisplaySettingsManager,
   void AddReceiver(
       mojo::PendingReceiver<mojom::DisplaySettings> receiver) override;
 
+  // ScreenPowerController::Delegate implementation:
+  void SetScreenPowerOn(PowerToggleCallback callback) override;
+  void SetScreenPowerOff(PowerToggleCallback callback) override;
+  void SetScreenBrightnessOn(bool brightness_on,
+                             base::TimeDelta duration) override;
+
   // mojom::DisplaySettings implementation:
   void SetColorTemperature(float temperature) override;
   void SetColorTemperatureSmooth(float temperature,
@@ -67,16 +75,11 @@ class DisplaySettingsManagerImpl : public DisplaySettingsManager,
   void SetAllowScreenPowerOff(bool allow_power_off) override;
 
  private:
-  // mojom::DisplaySettingsObserver implementation
+  // mojom::DisplaySettingsObserver implementation:
   void AddDisplaySettingsObserver(
       mojo::PendingRemote<mojom::DisplaySettingsObserver> observer) override;
 
-  void UpdateBrightness(base::TimeDelta duration);
-#if defined(USE_AURA)
-  void OnDisplayOn(const base::flat_map<int64_t, bool>& statuses);
-  void OnDisplayOnTimeoutCompleted();
-  void OnDisplayOffTimeoutCompleted();
-#endif  // defined(USE_AURA)
+  void UpdateBrightness(float brightness, base::TimeDelta duration);
 
   CastWindowManager* const window_manager_;
   shell::CastDisplayConfigurator* const display_configurator_;
@@ -86,12 +89,8 @@ class DisplaySettingsManagerImpl : public DisplaySettingsManager,
 #endif  // defined(USE_AURA)
 
   float brightness_;
-  bool screen_on_;
-#if defined(USE_AURA)
-  bool screen_power_on_;
-  bool allow_screen_power_off_;
-#endif  // defined(USE_AURA)
 
+  std::unique_ptr<ScreenPowerController> screen_power_controller_;
   std::unique_ptr<ColorTemperatureAnimation> color_temperature_animation_;
   std::unique_ptr<BrightnessAnimation> brightness_animation_;
 
