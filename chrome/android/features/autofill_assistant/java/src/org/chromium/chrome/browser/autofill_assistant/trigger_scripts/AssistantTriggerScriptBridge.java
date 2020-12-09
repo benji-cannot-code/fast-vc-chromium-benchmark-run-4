72737d5c32af04d23e389431db5b96219d10322a
Clemens Arbesser <arbesser@google.com>
2020-12-09 16:30:35 +0000

    [Autofill Assistant] Disable trigger scripts while tab not interactable
    This fixes a bug where the trigger prompt would stay visible in
    non-interactable tabs (such as during tab selection). To keep things
    simple, changing from interactable to non-interactable and vice-versa
    is treated the same as a change to a tab's visibility. Trigger scripts
    will now only be offered if a tab is both visible and interactable.
    
    Bug: b/175132318
    Change-Id: Ie25831ecd16220e351ec1cc231fda470d34873dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2579784
    Reviewed-by: Michele Mancina <micantox@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#835211}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/trigger_scripts/AssistantTriggerScriptBridge.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/trigger_scripts/AssistantTriggerScriptBridge.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
+import org.chromium.chrome.browser.ActivityTabProvider;
 import org.chromium.chrome.browser.autofill_assistant.AssistantCoordinator;
 import org.chromium.chrome.browser.autofill_assistant.AutofillAssistantClient;
 import org.chromium.chrome.browser.autofill_assistant.AutofillAssistantPreferencesUtil;
@@ -20,6 +21,7 @@ import org.chromium.chrome.browser.autofill_assistant.carousel.AssistantChip;
 import org.chromium.chrome.browser.autofill_assistant.header.AssistantHeaderModel;
 import org.chromium.chrome.browser.autofill_assistant.metrics.LiteScriptFinishedState;
 import org.chromium.chrome.browser.feedback.HelpAndFeedbackLauncherImpl;
+import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabUtils;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.content_public.browser.WebContents;
@@ -43,6 +45,7 @@ public class AssistantTriggerScriptBridge {
     private WebContents mWebContents;
     private ActivityKeyboardVisibilityDelegate mKeyboardVisibilityDelegate;
     private KeyboardVisibilityDelegate.KeyboardVisibilityListener mKeyboardVisibilityListener;
+    private ActivityTabProvider.ActivityTabTabObserver mActivityTabObserver;
 
     /** Interface for delegates of the {@code start} method. */
     public interface Delegate {
@@ -56,7 +59,8 @@ public class AssistantTriggerScriptBridge {
      */
     public void start(BottomSheetController bottomSheetController, Context context,
             ActivityKeyboardVisibilityDelegate keyboardVisibilityDelegate,
-            ApplicationViewportInsetSupplier bottomInsetProvider, @NonNull WebContents webContents,
+            ApplicationViewportInsetSupplier bottomInsetProvider,
+            ActivityTabProvider activityTabProvider, @NonNull WebContents webContents,
             @NonNull String initialUrl, Map<String, String> scriptParameters, String experimentIds,
             Delegate delegate) {
         mDelegate = delegate;
@@ -95,6 +99,15 @@ public class AssistantTriggerScriptBridge {
         }
         mKeyboardVisibilityListener = this::safeNativeOnKeyboardVisibilityChanged;
         mKeyboardVisibilityDelegate.addKeyboardVisibilityListener(mKeyboardVisibilityListener);
+
+        mActivityTabObserver =
+                new ActivityTabProvider.ActivityTabTabObserver(activityTabProvider, true) {
+                    @Override
+                    public void onInteractabilityChanged(Tab tab, boolean isInteractable) {
+                        safeNativeOnTabInteractabilityChanged(isInteractable);
+                    }
+                };
+
         // Request the client to start the trigger script. Native will then bind itself to this java
         // instance via setNativePtr.
         AutofillAssistantClient.fromWebContents(webContents)
@@ -173,6 +186,7 @@ public class AssistantTriggerScriptBridge {
         mNativeBridge = 0;
         mTriggerScript.destroy();
         mKeyboardVisibilityDelegate.removeKeyboardVisibilityListener(mKeyboardVisibilityListener);
+        mActivityTabObserver.destroy();
     }
 
     private void safeNativeOnTriggerScriptAction(int action) {
@@ -204,6 +218,13 @@ public class AssistantTriggerScriptBridge {
         }
     }
 
+    private void safeNativeOnTabInteractabilityChanged(boolean interactable) {
+        if (mNativeBridge != 0) {
+            AssistantTriggerScriptBridgeJni.get().onTabInteractabilityChanged(
+                    mNativeBridge, AssistantTriggerScriptBridge.this, interactable);
+        }
+    }
+
     @NativeMethods
     interface Natives {
         void onTriggerScriptAction(long nativeTriggerScriptBridgeAndroid,
@@ -214,5 +235,7 @@ public class AssistantTriggerScriptBridge {
                 long nativeTriggerScriptBridgeAndroid, AssistantTriggerScriptBridge caller);
         void onKeyboardVisibilityChanged(long nativeTriggerScriptBridgeAndroid,
                 AssistantTriggerScriptBridge caller, boolean visible);
+        void onTabInteractabilityChanged(long nativeTriggerScriptBridgeAndroid,
+                AssistantTriggerScriptBridge caller, boolean interactable);
     }
 }
