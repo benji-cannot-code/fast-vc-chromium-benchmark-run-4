402b69be82e7927d6901b57c4a61a8e11d049a80
Wei Li <weili@chromium.org>
2020-12-09 18:45:49 +0000

    Simplify default focus behavior definition
    Use a variable instead of a static function to represent the default
    focus behavior in platform style files.
    
    Bug: 1001103
    Change-Id: Icf92ea6b83037ba3b285ba0c5eccb7bc16f1f460
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2579653
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835278}

--- ui/views/style/platform_style.cc
+++ ui/views/style/platform_style.cc
@@ -51,6 +51,8 @@ const bool PlatformStyle::kUseRipples = true;
 const bool PlatformStyle::kTextfieldScrollsToStartOnFocusChange = false;
 const bool PlatformStyle::kTextfieldUsesDragCursorWhenDraggable = true;
 const bool PlatformStyle::kInactiveWidgetControlsAppearDisabled = false;
+const View::FocusBehavior PlatformStyle::kDefaultFocusBehavior =
+    View::FocusBehavior::ALWAYS;
 
 // Linux clips bubble windows that extend outside their parent window
 // bounds.
@@ -82,11 +84,6 @@ gfx::Range PlatformStyle::RangeToDeleteBackwards(const base::string16& text,
   size_t previous_grapheme_index =
       gfx::UTF16OffsetToIndex(text, cursor_position, -1);
   return gfx::Range(cursor_position, previous_grapheme_index);
-}
-
-// static
-View::FocusBehavior PlatformStyle::DefaultFocusBehavior() {
-  return View::FocusBehavior::ALWAYS;
 }
 
 #endif  // OS_APPLE
