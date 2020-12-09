b46413f30f66fa8fb85266a7bd2419a255e72fd8
Andres Calderon Jaramillo <andrescj@chromium.org>
2020-12-09 09:15:00 +0000

    media/V4L2VEA: Remove dependency on GMB factory.
    This CL removes the usage of a GpuMemoryBufferFactory from the
    V4L2VideoEncodeAccelerator. This is because we don't actually need it to
    allocate buffers and it simplifies things.
    
    Note that we must start the |encoder_task_runner_| with base::MayBlock()
    because the first encoder to make use of this path will open the render
    node.
    
    Bug: b:173167846
    Test: video.EncodeAccel.h264_720p_nv12_dmabuf on elm
    Change-Id: I01d5f63f0b358c0ee9bb448ded7ba24355a72f9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568548
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835114}

--- media/gpu/v4l2/v4l2_video_encode_accelerator.h
+++ media/gpu/v4l2/v4l2_video_encode_accelerator.h
@@ -30,10 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/video/video_encode_accelerator.h"
 #include "ui/gfx/geometry/size.h"
 
-namespace gpu {
-class GpuMemoryBufferFactory;
-}  // namespace gpu
-
 namespace media {
 class BitstreamBuffer;
 
@@ -331,9 +327,6 @@ class MEDIA_GPU_EXPORT V4L2VideoEncodeAccelerator
 
   // Image processor, if one is in use.
   std::unique_ptr<ImageProcessor> image_processor_;
-  // GpuMemoryBufferFactory to create GMB-based VideoFrame. This is needed only
-  // if image processor is used and its output buffer is GMB-based VideoFrame.
-  std::unique_ptr<gpu::GpuMemoryBufferFactory> image_processor_gmb_factory_;
   // Video frames for image processor output / VideoEncodeAccelerator input.
   // Only accessed on child thread.
   std::vector<scoped_refptr<VideoFrame>> image_processor_output_buffers_;
