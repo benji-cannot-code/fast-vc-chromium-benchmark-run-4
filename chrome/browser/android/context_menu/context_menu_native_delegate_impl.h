f86defb16c5d154fbe083f7a1c64e89f1f0357de
Sinan Sahin <sinansahin@google.com>
2020-12-09 00:10:11 +0000

    [Context menu] Move RenderFrameHost reference from native to Java
    This CL updates the ContextMenuNativeDelegate bridge to hold a reference
    on the Java side and get a C++ pointer when it will be used. This is
    done to prevent a use-after-free.
    
    Bug: 1150622
    Change-Id: Iaf22d7b6ba37923ec98d7e2f7c497dc45b5ce474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2578127
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834961}

--- chrome/browser/android/context_menu/context_menu_native_delegate_impl.h
+++ chrome/browser/android/context_menu/context_menu_native_delegate_impl.h
@@ -26,18 +26,19 @@ class ContextMenuNativeDelegateImpl {
  public:
   explicit ContextMenuNativeDelegateImpl(
       content::WebContents* const web_contents,
-      content::ContextMenuParams* const context_menu_params,
-      content::RenderFrameHost* const render_frame_host);
+      content::ContextMenuParams* const context_menu_params);
 
   void RetrieveImageForContextMenu(
       JNIEnv* env,
       const base::android::JavaParamRef<jobject>& obj,
+      const base::android::JavaParamRef<jobject>& jrender_frame_host,
       const base::android::JavaParamRef<jobject>& jcallback,
       jint max_width_px,
       jint max_height_px);
   void RetrieveImageForShare(
       JNIEnv* env,
       const base::android::JavaParamRef<jobject>& obj,
+      const base::android::JavaParamRef<jobject>& jrender_frame_host,
       const base::android::JavaParamRef<jobject>& jcallback,
       jint max_width_px,
       jint max_height_px,
@@ -45,8 +46,10 @@ class ContextMenuNativeDelegateImpl {
   void StartDownload(JNIEnv* env,
                      const base::android::JavaParamRef<jobject>& obj,
                      jboolean jis_link);
-  void SearchForImage(JNIEnv* env,
-                      const base::android::JavaParamRef<jobject>& obj);
+  void SearchForImage(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& obj,
+      const base::android::JavaParamRef<jobject>& jrender_frame_host);
 
  protected:
   using ImageRetrieveCallback = base::OnceCallback<void(
@@ -61,6 +64,7 @@ class ContextMenuNativeDelegateImpl {
   void RetrieveImageInternal(
       JNIEnv* env,
       ImageRetrieveCallback retrieve_callback,
+      const base::android::JavaParamRef<jobject>& jrender_frame_host,
       const base::android::JavaParamRef<jobject>& jcallback,
       jint max_width_px,
       jint max_height_px,
@@ -68,7 +72,6 @@ class ContextMenuNativeDelegateImpl {
 
   content::WebContents* const web_contents_;
   content::ContextMenuParams* const context_menu_params_;
-  content::RenderFrameHost* const render_frame_host_;
 };
 
 #endif  // CHROME_BROWSER_ANDROID_CONTEXT_MENU_CONTEXT_MENU_NATIVE_DELEGATE_IMPL_H_
