c4db47b8c1cc0c0274c5d5817d0fb88620bc6b71
Michael Liao <michael.liao@microsoft.com>
2020-12-09 23:18:33 +0000

    [DevTools - Telemetry]: Adding CSS Editor Opened histogram
    Adds a DevToolsCssEditorOpened histogram.  The enum has four values for color picker, shadow editor, bezier editor and font editor.
    
    dt-fe change: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2410431
    
    Bug: 1149589
    Change-Id: I2a7498c7ed3ba6e8ed1f076893c843b0fa5a9a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410650
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Michael Liao <michael.liao@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#835414}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -138,6 +138,8 @@ static const char kDevtoolsIssuesPanelResourceOpenedHistogram[] =
     "DevTools.IssuesPanelResourceOpened";
 static const char kDevToolsGridOverlayOpenedFromHistogram[] =
     "DevTools.GridOverlayOpenedFrom";
+static const char kDevToolsCssEditorOpenedHistogram[] =
+    "DevTools.CssEditorOpened";
 
 static const char kRemotePageActionInspect[] = "inspect";
 static const char kRemotePageActionReload[] = "reload";
@@ -1311,7 +1313,8 @@ void DevToolsUIBindings::RecordEnumeratedHistogram(const std::string& name,
       name == kDevToolsComputedStyleGroupingHistogram ||
       name == kDevtoolsIssuesPanelIssueExpandedHistogram ||
       name == kDevtoolsIssuesPanelResourceOpenedHistogram ||
-      name == kDevToolsGridOverlayOpenedFromHistogram)
+      name == kDevToolsGridOverlayOpenedFromHistogram ||
+      name == kDevToolsCssEditorOpenedHistogram)
     base::UmaHistogramExactLinear(name, sample, boundary_value);
   else
     frontend_host_->BadMessageReceived();
