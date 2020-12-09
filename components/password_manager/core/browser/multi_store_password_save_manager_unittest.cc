48c089eeeb3592e30cdad6b22426cee25dc106b8
Maria Kazinova <kazinova@google.com>
2020-12-09 16:13:15 +0000

    [Passwords] Fixing sending votes for dynamic password forms.
    VotesUploader uploades password votes on form submission. These votes
    are used to generate server predictions that are requested and received
    on the pageload. Thus if the form is changed dynamically, it makes sense
    to send votes for the form signature that is seen on the pageload.
    This CL starts using initial form signatures instead of signatures
    seen at the moment of submission.
    
    Bug: 1157048
    Change-Id: I13f51852c2b9447625ae6d20cd335e481dcab4e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2578900
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/master@{#835202}

--- components/password_manager/core/browser/multi_store_password_save_manager_unittest.cc
+++ components/password_manager/core/browser/multi_store_password_save_manager_unittest.cc
@@ -78,7 +78,8 @@ class MultiStorePasswordSaveManagerTest : public testing::Test {
  public:
   MultiStorePasswordSaveManagerTest()
       : votes_uploader_(&client_,
-                        false /* is_possible_change_password_form */) {
+                        false /* is_possible_change_password_form */,
+                        autofill::FormSignature()) {
     GURL origin = GURL("https://accounts.google.com/a/ServiceLoginAuth");
     GURL action = GURL("https://accounts.google.com/a/ServiceLogin");
     GURL psl_origin = GURL("https://myaccounts.google.com/a/ServiceLoginAuth");
@@ -152,6 +153,9 @@ class MultiStorePasswordSaveManagerTest : public testing::Test {
     metrics_recorder_ = base::MakeRefCounted<PasswordFormMetricsRecorder>(
         client_.IsCommittedMainFrameSecure(), client_.GetUkmSourceId(),
         /*pref_service=*/nullptr);
+
+    votes_uploader_.set_initial_form_signature(
+        CalculateFormSignature(observed_form_));
 
     auto mock_profile_form_saver = std::make_unique<NiceMock<MockFormSaver>>();
     mock_profile_form_saver_ = mock_profile_form_saver.get();
