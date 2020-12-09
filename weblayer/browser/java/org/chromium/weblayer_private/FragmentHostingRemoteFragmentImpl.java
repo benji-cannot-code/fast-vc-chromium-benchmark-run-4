f6564332f54c6600ea9e1fd1b4ae7c095fc58dfb
Robbie McElrath <rmcelrath@chromium.org>
2020-12-09 03:55:56 +0000

    [WebLayer] Make BrowserFragmentImpl extend FragmentHostingRemoteFragmentImpl
    BrowserFragmentImpl extending FragmentHostingRemoteFragmentImpl means we
    have a FragmentController (and therefore a FragmentManager) available to
    us in the main WebLayer UI. Upcoming page info changes will use this
    FragmentManager, and mediarouter could be refactored to use this as well
    instead of creating its own RemoteFragment.
    
    Bug: 1077766
    Change-Id: Id659555b827feed4293b062f8b0d8c6f4889daba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575753
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835045}

--- weblayer/browser/java/org/chromium/weblayer_private/FragmentHostingRemoteFragmentImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/FragmentHostingRemoteFragmentImpl.java
@@ -161,6 +161,7 @@ public abstract class FragmentHostingRemoteFragmentImpl extends RemoteFragmentIm
         mContext = createRemoteFragmentContext(embedderContext);
         mFragmentController =
                 FragmentController.createController(new RemoteFragmentHostCallback(this));
+        mFragmentController.attachHost(null);
 
         // Some appcompat functionality depends on Fragments being hosted from within an
         // AppCompatActivity, which performs some static initialization. Even if we're running
@@ -173,10 +174,7 @@ public abstract class FragmentHostingRemoteFragmentImpl extends RemoteFragmentIm
     @Override
     public void onCreate(Bundle savedInstanceState) {
         StrictModeWorkaround.apply();
-        mFragmentController.attachHost(null);
-
         super.onCreate(savedInstanceState);
-
         mFragmentController.dispatchCreate();
     }
 
@@ -199,6 +197,14 @@ public abstract class FragmentHostingRemoteFragmentImpl extends RemoteFragmentIm
         StrictModeWorkaround.apply();
         super.onDetach();
         mContext = null;
+
+        // If the Fragment is retained, onDestory won't be called during configuration changes. We
+        // have to create a new FragmentController that's attached to the correct Context when
+        // reattaching this Fragment, so destroy the existing one here.
+        if (!mFragmentController.getSupportFragmentManager().isDestroyed()) {
+            mFragmentController.dispatchDestroy();
+            assert mFragmentController.getSupportFragmentManager().isDestroyed();
+        }
     }
 
     @Override
