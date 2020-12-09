defc5d4f6312a0f4ecfdf05e5e7f6d8e3918e008
Yulun Wu <yulunwu@chromium.org>
2020-12-09 19:11:16 +0000

    Update search box colors for peeking and fullscreen launcher.
    We will be using primary colors for inactive search box placeholder text
    and secondary colors for active search box placeholder text.
    
    Bug: 1156231
    Change-Id: I1285bb3f6630e103bfd1cf192e9065d248614b07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2581023
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835294}

--- ash/app_list/app_list_color_provider_impl.h
+++ ash/app_list/app_list_color_provider_impl.h
@@ -23,7 +23,6 @@ class AppListColorProviderImpl : public AppListColorProvider {
   SkColor GetAppListBackgroundColor() const override;
   SkColor GetSearchBoxBackgroundColor() const override;
   SkColor GetSearchBoxSecondaryTextColor(SkColor default_color) const override;
-  SkColor GetSearchBoxPlaceholderTextColor() const override;
   SkColor GetSearchBoxTextColor(SkColor default_color) const override;
   SkColor GetSuggestionChipBackgroundColor() const override;
   SkColor GetSuggestionChipTextColor() const override;
--- ash/public/cpp/app_list/app_list_color_provider.h
+++ ash/public/cpp/app_list/app_list_color_provider.h
@@ -14,6 +14,9 @@ namespace ash {
 constexpr SkColor kDeprecatedSearchBoxTextDefaultColor =
     SkColorSetRGB(0x33, 0x33, 0x33);
 
+constexpr SkColor kDeprecatedSearchBoxPlaceholderTextColor =
+    SkColorSetARGB(0xDE, 0x00, 0x00, 0x00);
+
 class ASH_PUBLIC_EXPORT AppListColorProvider {
  public:
   // Returns the singleton instance.
@@ -24,7 +27,6 @@ class ASH_PUBLIC_EXPORT AppListColorProvider {
   virtual SkColor GetExpandArrowIconBackgroundColor() const = 0;
   virtual SkColor GetAppListBackgroundColor() const = 0;
   virtual SkColor GetSearchBoxBackgroundColor() const = 0;
-  virtual SkColor GetSearchBoxPlaceholderTextColor() const = 0;
   virtual SkColor GetSearchBoxTextColor(SkColor default_color) const = 0;
   virtual SkColor GetSearchBoxSecondaryTextColor(
       SkColor default_color) const = 0;
