0925baee67c712c8fda300e03d5e9d259d1e9d65
Andrew Grieve <agrieve@chromium.org>
2020-12-09 20:16:18 +0000

    Android: Fix crash in PiiElider.isClassName()
    There are several kinds of exceptions that can occur when loading
    classes.
    
    Bug: 1156583
    Change-Id: I8440a21ab1146e9f50a9f389c2e6d32f46330237
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2579953
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835329}

--- base/android/java/src/org/chromium/base/PiiElider.java
+++ base/android/java/src/org/chromium/base/PiiElider.java
@@ -131,7 +131,8 @@ public class PiiElider {
         try {
             Class.forName(url, false, ContextUtils.getApplicationContext().getClassLoader());
             return true;
-        } catch (ClassNotFoundException e) {
+        } catch (Throwable e) {
+            // Some examples: ClassNotFoundException, NoClassDefFoundException, VerifyError.
         }
         return false;
     }
