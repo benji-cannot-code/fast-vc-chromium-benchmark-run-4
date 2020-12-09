7e99fe2c3ac1b4ff09864aea5c10efd224af0ffb
Andre Vincent <andrevincent@google.com>
2020-12-09 18:09:02 +0000

    [ntp] Changes customize string to 'Show Cards'
    toggle selected: screenshot/7ZNeh5HNji2mnvk
    toggle not selected: screenshot/9WzpztqnYxkQL56
    
    Fixed: 1154473
    Change-Id: Id99211c5881b952cedf7d1b39b5df7a46fbe3b51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2577654
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Andre Vincent <andrevincent@google.com>
    Cr-Commit-Position: refs/heads/master@{#835248}

--- chrome/browser/resources/new_tab_page/customize_modules.js
+++ chrome/browser/resources/new_tab_page/customize_modules.js
@@ -28,13 +28,13 @@ class CustomizeModulesElement extends PolymerElement {
   static get properties() {
     return {
       /** @private */
-      hide_: {
+      show_: {
         type: Boolean,
         reflectToAttribute: true,
       },
 
       /** @private */
-      hideManagedByPolicy_: {
+      showManagedByPolicy_: {
         type: Boolean,
         value: () => loadTimeData.getBoolean('modulesVisibleManagedByPolicy'),
       },
@@ -43,7 +43,7 @@ class CustomizeModulesElement extends PolymerElement {
       selected_: {
         type: Boolean,
         reflectToAttribute: true,
-        computed: 'computeSelected_(hide_, hideManagedByPolicy_)',
+        computed: 'computeSelected_(show_, showManagedByPolicy_)',
       },
     };
   }
@@ -60,7 +60,7 @@ class CustomizeModulesElement extends PolymerElement {
     this.setModulesVisibleListenerId_ =
         BrowserProxy.getInstance().callbackRouter.setModulesVisible.addListener(
             visible => {
-              this.hide_ = !visible;
+              this.show_ = visible;
             });
     BrowserProxy.getInstance().handler.updateModulesVisible();
   }
@@ -86,7 +86,7 @@ class CustomizeModulesElement extends PolymerElement {
   }
 
   apply() {
-    BrowserProxy.getInstance().handler.setModulesVisible(!this.hide_);
+    BrowserProxy.getInstance().handler.setModulesVisible(this.show_);
   }
 
   /**
@@ -94,15 +94,7 @@ class CustomizeModulesElement extends PolymerElement {
    * @private
    */
   computeSelected_() {
-    return this.hide_ && !this.hideManagedByPolicy_;
-  }
-
-  /**
-   * @param {!CustomEvent<boolean>} e
-   * @private
-   */
-  onHideChange_(e) {
-    this.hide_ = e.detail;
+    return this.show_ && !this.showManagedByPolicy_;
   }
 }
 
