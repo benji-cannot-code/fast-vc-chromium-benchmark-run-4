30385284e35cd7360ad38a4142336dc51925b397
David Black <dmblack@google.com>
2020-12-09 18:31:21 +0000

    Fix paint artifacts from holding space item focus/selection rings.
    When scheduling repaint of holding space item focus/selection rings,
    the wrong bounds were being used. As a result, there were paint
    artifacts being left behind.
    
    Bug: 1154998
    Change-Id: I33f8fb681527a94f16341198cc588cef2b4c479c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2581037
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#835267}

--- ash/system/holding_space/holding_space_item_view.cc
+++ ash/system/holding_space/holding_space_item_view.cc
@@ -67,6 +67,11 @@ gfx::Transform CreateTransformFromOffset(SkScalar x, SkScalar y) {
   return transform;
 }
 
+// Schedules repaint of `layer`.
+void InvalidateLayer(ui::Layer* layer) {
+  layer->SchedulePaint(gfx::Rect(layer->size()));
+}
+
 // CallbackPainter -------------------------------------------------------------
 
 // A painter which delegates painting to a callback.
@@ -187,8 +192,7 @@ bool HoldingSpaceItemView::HandleAccessibleAction(
 void HoldingSpaceItemView::OnBoundsChanged(const gfx::Rect& previous_bounds) {
   gfx::Rect bounds = GetLocalBounds();
   selected_layer_owner_->layer()->SetBounds(bounds);
-  selected_layer_owner_->layer()->SchedulePaint(
-      selected_layer_owner_->layer()->bounds());
+  InvalidateLayer(selected_layer_owner_->layer());
 
   // The focus ring is painted just outside the bounds for this view.
   const float kFocusInsets =
@@ -196,18 +200,15 @@ void HoldingSpaceItemView::OnBoundsChanged(const gfx::Rect& previous_bounds) {
 
   bounds.Inset(gfx::Insets(kFocusInsets));
   focused_layer_owner_->layer()->SetBounds(bounds);
-  focused_layer_owner_->layer()->SchedulePaint(
-      focused_layer_owner_->layer()->bounds());
+  InvalidateLayer(focused_layer_owner_->layer());
 }
 
 void HoldingSpaceItemView::OnFocus() {
-  focused_layer_owner_->layer()->SchedulePaint(
-      focused_layer_owner_->layer()->bounds());
+  InvalidateLayer(focused_layer_owner_->layer());
 }
 
 void HoldingSpaceItemView::OnBlur() {
-  focused_layer_owner_->layer()->SchedulePaint(
-      focused_layer_owner_->layer()->bounds());
+  InvalidateLayer(focused_layer_owner_->layer());
 }
 
 void HoldingSpaceItemView::OnGestureEvent(ui::GestureEvent* event) {
@@ -274,9 +275,7 @@ void HoldingSpaceItemView::SetSelected(bool selected) {
     return;
 
   selected_ = selected;
-
-  selected_layer_owner_->layer()->SchedulePaint(
-      selected_layer_owner_->layer()->bounds());
+  InvalidateLayer(selected_layer_owner_->layer());
 }
 
 views::ToggleImageButton* HoldingSpaceItemView::AddPin(views::View* parent) {
