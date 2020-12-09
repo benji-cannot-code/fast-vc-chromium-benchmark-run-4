fa7e2c3aa4b12348c4abc05f13132e2951999ab5
Tommy Steimel <steimel@chromium.org>
2020-12-09 18:04:04 +0000

    Reland "NTP: Allow modules to instantiate the header"
    This is a reland of 2cf475dd18f59429f1e4958006a29807ef9ee425.
    
    This adds the module_header.js file to the modules_resources.grdp file,
    which fixes the issues when optimize_webui = false.
    
    Original change's description:
    > NTP: Allow modules to instantiate the header
    >
    > This CL removes the module header from the wrapper and instead allows
    > the modules to instantiate it instead. This makes the title and actions
    > properties of the ModuleDescriptor obsolete, so this CL also removes
    > those.
    >
    > Bug: 1152205, 1152216
    > Change-Id: Ib524a59c8c8a7cc69e130b31a8557b903233ab95
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2570390
    > Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    > Commit-Queue: Tommy Steimel <steimel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#834424}
    
    Bug: 1152205
    Bug: 1152216
    Change-Id: Ibded5b577d31961786b90dc15c664a11702a5b96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2581004
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835243}

--- chrome/browser/resources/new_tab_page/modules/kaleidoscope/externs.js
+++ chrome/browser/resources/new_tab_page/modules/kaleidoscope/externs.js
@@ -4,9 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
- * @returns {Promise<?{
- *    element: !HTMLElement,
- *    title: string,
- *   }>}
+ * @returns {Promise<?HTMLElement>}
  */
 window.loadKaleidoscopeModule = () => {};
