7cca9d4784b01113b3066b0f4c4d6eb1b5d1a825
Alan Cutter <alancutter@chromium.org>
2020-12-09 01:30:36 +0000

    Remove unused InstallFinalizer methods
    InstallFinalizer::UninstallWebAppFromSyncByUser() and
    InstallFinalizer::CanUserUninstallFromSync() are not called by
    production code. This CL removes them.
    
    Change-Id: I51a0cd97da31e3b631c8a9c2a00228341156c321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2515423
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#834993}

--- chrome/browser/web_applications/manifest_update_manager_browsertest.cc
+++ chrome/browser/web_applications/manifest_update_manager_browsertest.cc
@@ -407,7 +407,7 @@ IN_PROC_BROWSER_TEST_F(ManifestUpdateManagerBrowserTest,
   GURL url = GetAppURL();
   UpdateCheckResultAwaiter awaiter(browser(), url);
   ui_test_utils::NavigateToURL(browser(), url);
-  GetProvider().install_finalizer().UninstallWebAppFromSyncByUser(
+  GetProvider().install_finalizer().UninstallExternalAppByUser(
       app_id, base::DoNothing());
   EXPECT_EQ(std::move(awaiter).AwaitNextResult(),
             ManifestUpdateResult::kAppUninstalled);
@@ -713,7 +713,7 @@ IN_PROC_BROWSER_TEST_F(ManifestUpdateManagerBrowserTest,
   OverrideManifest(kManifestTemplate, {"blue", kInstallableIconList});
   AppId app_id = InstallPolicyApp();
   EXPECT_FALSE(
-      GetProvider().install_finalizer().CanUserUninstallFromSync(app_id));
+      GetProvider().install_finalizer().CanUserUninstallExternalApp(app_id));
 
   OverrideManifest(kManifestTemplate, {"red", kInstallableIconList});
   EXPECT_EQ(GetResultAfterPageLoad(GetAppURL(), &app_id),
@@ -725,7 +725,7 @@ IN_PROC_BROWSER_TEST_F(ManifestUpdateManagerBrowserTest,
   // Policy installed apps should continue to be not uninstallable by the user
   // after updating.
   EXPECT_FALSE(
-      GetProvider().install_finalizer().CanUserUninstallFromSync(app_id));
+      GetProvider().install_finalizer().CanUserUninstallExternalApp(app_id));
 }
 
 IN_PROC_BROWSER_TEST_F(ManifestUpdateManagerBrowserTest,
