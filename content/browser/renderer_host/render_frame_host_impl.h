12ac3bf81487df5008cd9cad6f99ab5079299bc1
Rakina Zata Amni <rakina@chromium.org>
2020-12-09 10:29:07 +0000

    Fix browser expectation of "method" and "post_id" on same-document navigations
    Currently we use NavigationRequest's CommitNavigationParams' method in
    VerifyThatBrowserAndRendererCalculatedDidCommitParamsMatch, but that
    might differ than what the renderer returns on history same-document
    navigations due to the history API resetting methods to "GET".
    
    For POST ID, we use the last committed entry's POST ID on same-document
    navigations, but that will only work for main frame cases. We need to
    track the post ID directly in the RenderFrameHostImpl to cover cases
    with subframe navigations.
    
    This CL fixes the calculated values, and adds additional logging.
    
    Bug: 1155414
    Change-Id: I81358e641b7b3aaeda7fbcec58c92cef97ba9d27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2576878
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835129}

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -579,11 +579,14 @@ class CONTENT_EXPORT RenderFrameHostImpl
   // cases, use GetLastCommittedURL instead.
   const GURL& last_successful_url() { return last_successful_url_; }
 
-  // Return the http method of the last committed navigation.
+  // Returns the http method of the last committed navigation.
   const std::string& last_http_method() { return last_http_method_; }
 
-  // Return the http status code of the last committed navigation.
+  // Returns the http status code of the last committed navigation.
   int last_http_status_code() { return last_http_status_code_; }
+
+  // Returns the POST ID of the last committed navigation.
+  int64_t last_post_id() { return last_post_id_; }
 
   // Returns true if |dest_url_info| should be considered the same site as the
   // current contents of this frame. This is the primary entry point for
@@ -2524,7 +2527,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
   void VerifyThatBrowserAndRendererCalculatedDidCommitParamsMatch(
       NavigationRequest* navigation_request,
       const mojom::DidCommitProvisionalLoadParams& params,
-      bool is_same_document_navigation);
+      mojom::DidCommitSameDocumentNavigationParamsPtr same_document_params);
 
   // Evicts the document from the BackForwardCache if it is in the cache,
   // and ineligible for caching.
@@ -2657,6 +2660,9 @@ class CONTENT_EXPORT RenderFrameHostImpl
 
   // The http status code of the last committed navigation.
   int last_http_status_code_ = 0;
+
+  // The POST ID of the last committed navigation.
+  int64_t last_post_id_ = 0;
 
   // Local root subframes directly own their RenderWidgetHost.
   // Please see comments about the GetLocalRenderWidgetHost() function.
