69c51ca43158d3576b3903fe450d23a0e290457e
Joel Riley <joelriley@google.com>
2020-12-10 02:29:50 +0000

    Update STS navigation to work better with Android and System UI.
    If next adjacent valid text node is not contained within a paragraph, Select-to-speak will still navigate to it. This is useful in Android apps and system UI, where there is a lack of paragraph-like structures.
    
    Also contain navigation within root (such as browser tab).
    
    AX-Relnotes: N/A
    
    Bug: 1143819
    Change-Id: I6e36c9cd075480dd95cb49013b78942a1edae2c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2580509
    Commit-Queue: Joel Riley <joelriley@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835512}

--- chrome/browser/resources/chromeos/accessibility/select_to_speak/select_to_speak_navigation_control_test.js
+++ chrome/browser/resources/chromeos/accessibility/select_to_speak/select_to_speak_navigation_control_test.js
@@ -62,10 +62,12 @@ TEST_F(
                     .NEXT_PARAGRAPH);
 
             // Speaks second paragraph
-            assertTrue(this.mockTts.currentlySpeaking());
-            assertEquals(this.mockTts.pendingUtterances().length, 1);
-            this.assertEqualsCollapseWhitespace(
-                this.mockTts.pendingUtterances()[0], 'Paragraph 2');
+            this.waitOneEventLoop(() => {
+              assertTrue(this.mockTts.currentlySpeaking());
+              assertEquals(this.mockTts.pendingUtterances().length, 1);
+              this.assertEqualsCollapseWhitespace(
+                  this.mockTts.pendingUtterances()[0], 'Paragraph 2');
+            });
           });
     });
 
@@ -94,10 +96,12 @@ TEST_F(
                     .PREVIOUS_PARAGRAPH);
 
             // Speaks second paragraph
-            assertTrue(this.mockTts.currentlySpeaking());
-            assertEquals(this.mockTts.pendingUtterances().length, 1);
-            this.assertEqualsCollapseWhitespace(
-                this.mockTts.pendingUtterances()[0], 'Paragraph 1');
+            this.waitOneEventLoop(() => {
+              assertTrue(this.mockTts.currentlySpeaking());
+              assertEquals(this.mockTts.pendingUtterances().length, 1);
+              this.assertEqualsCollapseWhitespace(
+                  this.mockTts.pendingUtterances()[0], 'Paragraph 1');
+            });
           });
     });
 
