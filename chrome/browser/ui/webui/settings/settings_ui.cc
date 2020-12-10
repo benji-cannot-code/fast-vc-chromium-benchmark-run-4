6b1863e825c9dcd8ca11893c0eb410cf864b60ff
Ramin Halavati <rhalavati@chromium.org>
2020-12-10 13:08:54 +0000

    Revert "Make two settings sections visible for ephemeral guest profiles"
    This reverts commit f354c62753beb82862c1c9d87ad7eaae9097574a.
    
    Reason for revert: Ephemeral Guest profiles will have the same UI as old (off-the-record) Guest profiles in M89.
    
    Original change's description:
    > Make two settings sections visible for ephemeral guest profiles
    >
    > - Make the "You and Google" and "Auto-fill" sections visible,
    > when settings is based on an ephemeral guest profile.
    > - The notion of Ephemeral Guest profiles (go/ephemeral-guest-profiles)
    > is to be replacing the current guest profiles.
    >
    > Bug: 1140419
    > Change-Id: If10584bb4a8105275c2eac8c581c7abccf545225
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2483753
    > Reviewed-by: dpapad <dpapad@chromium.org>
    > Commit-Queue: Huanzhong Huang <huanzhong@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#819350}
    
    TBR=dpapad@chromium.org,rhalavati@chromium.org,huanzhong@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1140419
    Change-Id: I1ec303884ba5a5585225b50e5364197880917f14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2582084
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835631}

--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -284,9 +284,6 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
       "enableContentSettingsRedesign",
       base::FeatureList::IsEnabled(features::kContentSettingsRedesign));
 
-  html_source->AddBoolean("isEphemeralGuestProfile",
-                          profile->IsEphemeralGuestProfile());
-
 #if defined(OS_WIN)
   html_source->AddBoolean(
       "safetyCheckChromeCleanerChildEnabled",
