c6f4bdbe21ed6878d26fbfb6f4fb61f1bc7bb5a4
rbpotter <rbpotter@chromium.org>
2020-12-10 23:26:27 +0000

    Print Preview: Fix some polish issues with icon alignment
    Bug: 1156918
    Change-Id: I0d3fcd231a8896aee4d3869cc1bb7b080e1558f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2581022
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835885}

--- chrome/browser/resources/print_preview/ui/throbber_css.js
+++ chrome/browser/resources/print_preview/ui/throbber_css.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import './print_preview_vars_css.js';
 
 const template = document.createElement('template');
 template.innerHTML = `
