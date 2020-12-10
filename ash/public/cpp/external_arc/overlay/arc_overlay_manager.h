6b129062161a58589e0daaf4f9cf5c24963fd8da
Lloyd Pique <lpique@chromium.org>
2020-12-10 00:53:35 +0000

    ARC Overlays: Delay attachment until visible
    ArcOverlayManager was detecting the overlay window while the
    ShellSurfaceBase code is in the middle of setting it up.
    ShellSurfaceBase has not registered itself as an observer yet.
    
    Because of that, ShellSurfaceBase and its descendants do not get all the
    window events, and particularly the bounds change event.
    
    This patch changes ArcOverlayManager so that after recognizing a overlay
    window, it subsequently waits for the window to become visible, and only
    then attaches the overlay to the host web view window.
    
    BUG=b:172592701
    TEST=Billing test app overlay initial size is set
    
    Change-Id: I265fa03658c2a283e9971f3d0fc46ef40663e5ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575978
    Commit-Queue: Lloyd Pique <lpique@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835473}

--- ash/public/cpp/external_arc/overlay/arc_overlay_manager.h
+++ ash/public/cpp/external_arc/overlay/arc_overlay_manager.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/ash_public_export.h"
 #include "base/callback_helpers.h"
 #include "base/containers/flat_map.h"
+#include "base/scoped_multi_source_observation.h"
 #include "base/scoped_observation.h"
 #include "ui/aura/env.h"
 #include "ui/aura/env_observer.h"
@@ -66,6 +67,7 @@ class ASH_PUBLIC_EXPORT ArcOverlayManager : public aura::EnvObserver,
   void OnWindowPropertyChanged(aura::Window* window,
                                const void* key,
                                intptr_t old) override;
+  void OnWindowVisibilityChanged(aura::Window* window, bool visible) override;
 
  private:
   void DeregisterHostWindow(const std::string& overlay_token);
@@ -77,9 +79,16 @@ class ASH_PUBLIC_EXPORT ArcOverlayManager : public aura::EnvObserver,
 
   base::ScopedObservation<aura::Env, aura::EnvObserver> env_observer_{this};
 
-  aura::Window* observed_window_ = nullptr;
+  // This tracks a single newly created window until we get a confirmation that
+  // it is an exo::ShellSurfaceBase with the right settings to be an overlay
+  // (which should happen immediately after creation), or until another new
+  // window is created.
   base::ScopedObservation<aura::Window, aura::WindowObserver>
-      observed_window_observer_{this};
+      unknown_window_observation_{this};
+
+  // This tracks all the overlay candidates until they are actually ready
+  base::ScopedMultiSourceObservation<aura::Window, aura::WindowObserver>
+      overlay_window_observations_{this};
 };
 
 }  // namespace ash
