dec52a1f8d71a89a438535d6897616b1114ad7c8
Matt Reynolds <mattreynolds@google.com>
2020-12-10 02:40:56 +0000

    [webhid] Keep HIDDevice alive
    If script does not maintain a reference to a HIDDevice object, it
    may be garbage collected even if there is an open connection to
    the device with an input report event listener. This CL makes
    HIDDevice ActiveScriptWrappable so it can prevent itself from
    being GCed if the device is open and an event listener is
    registered.
    
    Bug: 1156239
    Change-Id: Ia2d394e66f9e52efc871bdb639e003ed6bb27254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2576793
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835520}

--- third_party/blink/renderer/modules/hid/hid_device.h
+++ third_party/blink/renderer/modules/hid/hid_device.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "services/device/public/mojom/hid.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/hid/hid.mojom-blink.h"
+#include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
 #include "third_party/blink/renderer/bindings/core/v8/array_buffer_or_array_buffer_view.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_hid_report_item.h"
@@ -34,6 +35,7 @@ class ScriptState;
 class MODULES_EXPORT HIDDevice
     : public EventTargetWithInlineData,
       public ExecutionContextLifecycleObserver,
+      public ActiveScriptWrappable<HIDDevice>,
       public device::mojom::blink::HidConnectionClient {
   DEFINE_WRAPPERTYPEINFO();
 
@@ -71,6 +73,9 @@ class MODULES_EXPORT HIDDevice
 
   // ExecutionContextLifecycleObserver:
   void ContextDestroyed() override;
+
+  // ActiveScriptWrappable:
+  bool HasPendingActivity() const override;
 
   static HIDReportItem* ToHIDReportItem(
       const device::mojom::blink::HidReportItem& report_item);
