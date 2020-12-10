9351abf2ebf8cbc70b48511cb97983700156ebb5
Will Cassella <cassew@google.com>
2020-12-10 20:28:43 +0000

    Don't perform initialization of media library in browser process
    This CL removes the call to `media::InitializeMediaLibrary` in the
    browser main loop. Initializing the media library in the browser process
    should not be necessary, and it's done elsewhere for the renderer
    process.
    
    Change-Id: I0b430245019b519fe1614dc9d3e9263b59a3b2a0
    Bug: 1003535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2577877
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Will Cassella <cassew@google.com>
    Cr-Commit-Position: refs/heads/master@{#835792}

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -120,7 +120,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_manager.h"
 #include "media/audio/audio_system.h"
 #include "media/audio/audio_thread_impl.h"
-#include "media/base/media.h"
 #include "media/base/user_input_monitor.h"
 #include "media/media_buildflags.h"
 #include "media/midi/midi_service.h"
@@ -722,10 +721,6 @@ void BrowserMainLoop::PostMainMessageLoopStart() {
         std::make_unique<ScreenlockMonitorDeviceSource>();
     screenlock_monitor_ = std::make_unique<ScreenlockMonitor>(
         std::move(screenlock_monitor_source));
-  }
-  {
-    TRACE_EVENT0("startup", "BrowserMainLoop::Subsystem:MediaFeatures");
-    media::InitializeMediaLibrary();
   }
   {
     TRACE_EVENT0("startup",
