54f68255671fcd176f59ec87601ffb6e642061f9
Elaine Chien <elainec@chromium.org>
2020-12-10 23:56:31 +0000

    ChromeLabs: Adding Uma histograms to track labs usage
    Bug: 1145666
    Change-Id: I7ba3301d7c5216d358953bb3223dee9992f139c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2580340
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835900}

--- chrome/browser/ui/views/toolbar/chrome_labs_bubble_view.cc
+++ chrome/browser/ui/views/toolbar/chrome_labs_bubble_view.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/toolbar/chrome_labs_bubble_view.h"
 #include "base/bind.h"
+#include "base/metrics/histogram_functions.h"
 #include "chrome/browser/about_flags.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/flag_descriptions.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/ui/webui/flags/flags_ui.h"
 #include "chrome/grit/generated_resources.h"
@@ -19,6 +21,49 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_provider.h"
 
 namespace {
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class ChromeLabsSelectedLab {
+  kUnspecifiedSelected = 0,
+  kReadLaterSelected = 1,
+  kTabSearchSelected = 2,
+  kMaxValue = kTabSearchSelected,
+};
+
+void EmitToHistogram(const base::string16& selected_lab_state,
+                     const std::string& internal_name) {
+  const auto get_histogram_name = [](const base::string16& selected_lab_state) {
+    if (selected_lab_state == base::ASCIIToUTF16(base::StringPiece(
+                                  flags_ui::kGenericExperimentChoiceDefault))) {
+      return "Toolbar.ChromeLabs.DefaultLabAction";
+    } else if (selected_lab_state ==
+               base::ASCIIToUTF16(base::StringPiece(
+                   flags_ui::kGenericExperimentChoiceEnabled))) {
+      return "Toolbar.ChromeLabs.EnableLabAction";
+    } else if (selected_lab_state ==
+               base::ASCIIToUTF16(base::StringPiece(
+                   flags_ui::kGenericExperimentChoiceDisabled))) {
+      return "Toolbar.ChromeLabs.DisableLabAction";
+    } else {
+      return "";
+    }
+  };
+
+  const auto get_enum = [](const std::string& internal_name) {
+    if (internal_name == flag_descriptions::kReadLaterFlagId) {
+      return ChromeLabsSelectedLab::kReadLaterSelected;
+    } else if (internal_name == flag_descriptions::kEnableTabSearchFlagId) {
+      return ChromeLabsSelectedLab::kTabSearchSelected;
+    } else {
+      return ChromeLabsSelectedLab::kUnspecifiedSelected;
+    }
+  };
+
+  const std::string histogram_name = get_histogram_name(selected_lab_state);
+  if (!histogram_name.empty())
+    base::UmaHistogramEnumeration(histogram_name, get_enum(internal_name));
+}
 
 ChromeLabsBubbleView* g_chrome_labs_bubble = nullptr;
 
@@ -131,6 +176,9 @@ std::unique_ptr<ChromeLabsItemView> ChromeLabsBubbleView::CreateLabItem(
         bubble_view->flags_storage_.get(),
         internal_name + "@" + base::NumberToString(selected_index), true);
     bubble_view->ShowRelaunchPrompt();
+    EmitToHistogram(
+        item_view->GetFeatureEntry()->DescriptionForOption(selected_index),
+        internal_name);
   };
 
   std::unique_ptr<ChromeLabsItemView> item_view =
