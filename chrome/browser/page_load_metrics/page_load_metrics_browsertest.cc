70a733030f9659b1add54b14f90329303fb42aa2
Hiroki Nakagawa <nhiroki@chromium.org>
2020-12-10 13:26:35 +0000

    EarlyHints: Fix UMAs for Early Hints
    Before this CL, there were 2 bugs in the Early Hints UMAs:
    
    (1) Calculation order of the time duration is wrongly reversed.
    
    (2) The current implementation assumes that
        `LoadTimingInfo::receive_headers_start` doesn't take informational
        responses (1xx) into account, but actually it does [1, 2] based on
        the Resource Timing spec (See the note on [3]). Therefore,
        `early_hints_for_final_request_time` is always equal to
        `final_response_start`. This means the duration between them is
        meaningless.
    
    For the issue (1), this CL corrects the calculation order (see changes
    in uma_page_load_metrics_observer.cc).
    
    For the issue (2), this CL introduces
    `LoadTimingInfo::receive_non_informational_headers_start`. This is
    similar to `receive_headers_start` but it captures the time only when
    the response is non-informational. This is propagated up to
    NavigationHandleTiming and used for calculating the duration instead of
    `receive_headers_start`.
    
    Other notes:
    - This changes HTTP/1.1 and HTTP/2 but doesn't change HTTP/3. This is
      because HTTP/3 doesn't support LoadTimingInfo yet. See also
      https://crbug.com/1096414.
    - UKMs for the Early Hints use a different calculation logic, so they
      don't have these issues. However, to measure the effectiveness of the
      Early Hints precisely, we have to record the new timing for
      non-informational responses in UKM as well. See the TODO comment in
      uma_page_load_metrics_observer.cc for details.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/master:net/http/http_stream_parser.cc;l=876;drc=1066e389d45dd39f22f2daf582fc56948d5cc480
    [2] https://source.chromium.org/chromium/chromium/src/+/master:net/spdy/spdy_stream.cc;l=415;drc=1066e389d45dd39f22f2daf582fc56948d5cc480
    [3] https://w3c.github.io/resource-timing/#dom-performanceresourcetiming-responsestart
    
    Bug: 1093693
    Change-Id: Id2f696669804c2763de4c21fe40689e3af05bd52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2521563
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Bence Béky <bnc@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835633}

--- chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
@@ -3218,6 +3218,19 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest, EarlyHints) {
   EXPECT_EQ(timing.early_hints_for_first_request_time,
             timing.early_hints_for_final_request_time);
 
+  // The Early Hints response (informational response) start time should be
+  // equal to the response start time.
+  EXPECT_FALSE(timing.first_response_start_time.is_null());
+  EXPECT_FALSE(timing.final_response_start_time.is_null());
+  EXPECT_EQ(timing.first_response_start_time,
+            timing.early_hints_for_first_request_time);
+  EXPECT_EQ(timing.final_response_start_time,
+            timing.early_hints_for_first_request_time);
+  // The non-informational response start time should be recorded separately.
+  EXPECT_FALSE(timing.final_non_informational_response_start_time.is_null());
+  EXPECT_LT(timing.final_response_start_time,
+            timing.final_non_informational_response_start_time);
+
   // The timings of the Early Hints response should be recorded.
   histogram_tester_->ExpectTotalCount(
       internal::kHistogramEarlyHintsFirstRequestStartToEarlyHints, 1);
@@ -3247,6 +3260,12 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest, EarlyHints_NoHints) {
   // No Early Hints responses were received.
   EXPECT_TRUE(timing.early_hints_for_first_request_time.is_null());
   EXPECT_TRUE(timing.early_hints_for_final_request_time.is_null());
+  // There were no informational responses, so the final response start time
+  // should be equal to the final non-informational response start time.
+  EXPECT_FALSE(timing.final_response_start_time.is_null());
+  EXPECT_FALSE(timing.final_non_informational_response_start_time.is_null());
+  EXPECT_EQ(timing.final_response_start_time,
+            timing.final_non_informational_response_start_time);
 
   // The timings of the Early Hints response should not be recorded.
   histogram_tester_->ExpectTotalCount(
@@ -3281,6 +3300,19 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest, EarlyHints_MultipleHints) {
   EXPECT_EQ(timing.early_hints_for_first_request_time,
             timing.early_hints_for_final_request_time);
 
+  // The Early Hints response (informational response) start time should be
+  // equal to the response start time.
+  EXPECT_FALSE(timing.first_response_start_time.is_null());
+  EXPECT_FALSE(timing.final_response_start_time.is_null());
+  EXPECT_EQ(timing.first_response_start_time,
+            timing.early_hints_for_first_request_time);
+  EXPECT_EQ(timing.final_response_start_time,
+            timing.early_hints_for_first_request_time);
+  // The non-informational response start time should be recorded separately.
+  EXPECT_FALSE(timing.final_non_informational_response_start_time.is_null());
+  EXPECT_LT(timing.final_response_start_time,
+            timing.final_non_informational_response_start_time);
+
   // The timings of the Early Hints responses should be recorded only one time.
   histogram_tester_->ExpectTotalCount(
       internal::kHistogramEarlyHintsFirstRequestStartToEarlyHints, 1);
@@ -3312,6 +3344,14 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
   // redirected request.
   EXPECT_FALSE(timing.early_hints_for_first_request_time.is_null());
   EXPECT_TRUE(timing.early_hints_for_final_request_time.is_null());
+
+  // There were no informational responses for the redirected request, so the
+  // final response start time should be equal to the final non-informational
+  // response start time.
+  EXPECT_FALSE(timing.final_response_start_time.is_null());
+  EXPECT_FALSE(timing.final_non_informational_response_start_time.is_null());
+  EXPECT_EQ(timing.final_response_start_time,
+            timing.final_non_informational_response_start_time);
 
   // The timings of the Early Hints response should be recorded only for the
   // first request.
@@ -3345,6 +3385,16 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
   // first request.
   EXPECT_TRUE(timing.early_hints_for_first_request_time.is_null());
   EXPECT_FALSE(timing.early_hints_for_final_request_time.is_null());
+
+  // The Early Hints response (informational response) start time should be
+  // equal to the final response start time.
+  EXPECT_FALSE(timing.final_response_start_time.is_null());
+  EXPECT_EQ(timing.final_response_start_time,
+            timing.early_hints_for_final_request_time);
+  // The non-informational response start time should be recorded separately.
+  EXPECT_FALSE(timing.final_non_informational_response_start_time.is_null());
+  EXPECT_LT(timing.final_response_start_time,
+            timing.final_non_informational_response_start_time);
 
   // The timings of the Early Hints response should be recorded only for the
   // redirected request.
@@ -3380,6 +3430,16 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
   EXPECT_FALSE(timing.early_hints_for_final_request_time.is_null());
   EXPECT_LT(timing.early_hints_for_first_request_time,
             timing.early_hints_for_final_request_time);
+
+  // The Early Hints response (informational response) start time should be
+  // equal to the final response start time.
+  EXPECT_FALSE(timing.final_response_start_time.is_null());
+  EXPECT_EQ(timing.final_response_start_time,
+            timing.early_hints_for_final_request_time);
+  // The non-informational response start time should be recorded separately.
+  EXPECT_FALSE(timing.final_non_informational_response_start_time.is_null());
+  EXPECT_LT(timing.final_response_start_time,
+            timing.final_non_informational_response_start_time);
 
   // The timings of the Early Hints response should be recorded.
   histogram_tester_->ExpectTotalCount(
---
+++ content/public/browser/navigation_handle_timing.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/public/browser/navigation_handle_timing.h"
+
+namespace content {
+
+NavigationHandleTiming::NavigationHandleTiming() = default;
+
+NavigationHandleTiming::NavigationHandleTiming(
+    const NavigationHandleTiming& timing) = default;
+
+}  // namespace content
