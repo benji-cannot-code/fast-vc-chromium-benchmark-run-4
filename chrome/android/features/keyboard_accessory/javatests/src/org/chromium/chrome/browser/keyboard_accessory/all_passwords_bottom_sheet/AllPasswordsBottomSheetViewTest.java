97ab7f25185251db08edc8c3e947a3464ed1f2c9
Yue Zhang <yuezhanggg@chromium.org>
2020-12-10 19:18:12 +0000

    Sheriff: Disable a flaky AllPasswordsBottomSheetViewTest test
    AllPasswordsBottomSheetViewTest#
    testCredentialsChangedByModel is failing on "Lollipop Phone Tester".
    
    TBR: fhorschig@chromium.org
    Bug: 1157497
    Change-Id: Ic36507ed53a68a13fe06d441ab4eda712c21b689
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585445
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835763}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/all_passwords_bottom_sheet/AllPasswordsBottomSheetViewTest.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/all_passwords_bottom_sheet/AllPasswordsBottomSheetViewTest.java
@@ -31,6 +31,7 @@ import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.Callback;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -120,6 +121,7 @@ public class AllPasswordsBottomSheetViewTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1157497")
     public void testCredentialsChangedByModel() {
         addDefaultCredentialsToTheModel();
 
