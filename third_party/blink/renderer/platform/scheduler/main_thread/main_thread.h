672b20c67d9718332dc19efa44db3626e5a0cc30
Francois Doray <fdoray@chromium.org>
2020-12-10 04:12:07 +0000

    [blink scheduler] Add blink::Thread::ThreadHandle.
    The thread handle should be used instead of the thread id for thread
    manipulations. It is available immediately after thread creation,
    unlike the thread id which is available only after the main function
    is entered. Also, on Windows, the thread handle remains valid after the
    thread has exited.
    
    This is a prerequisite to remove the wait after compositor thread
    initialization
    https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/renderer/platform/scheduler/common/thread.cc;l=96;drc=5b1ca7b1087e13d963bc4058d9406a838fb7f9ca
    
    Bug: 1080709
    Change-Id: Ia0b72aa1b3a86454cffa774131944db87ced0843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2578172
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835544}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread.h
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_refptr.h"
 #include "base/single_thread_task_runner.h"
+#include "base/threading/platform_thread.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 
@@ -27,6 +28,7 @@ class PLATFORM_EXPORT MainThread : public Thread {
   // Thread implementation.
   ThreadScheduler* Scheduler() override;
   PlatformThreadId ThreadId() const override;
+  base::PlatformThreadHandle ThreadHandle() const override;
   scoped_refptr<base::SingleThreadTaskRunner> GetTaskRunner() const override;
 
   void AddTaskTimeObserver(base::sequence_manager::TaskTimeObserver*) override;
@@ -34,9 +36,10 @@ class PLATFORM_EXPORT MainThread : public Thread {
       base::sequence_manager::TaskTimeObserver*) override;
 
  private:
-  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
-  MainThreadSchedulerImpl* scheduler_;  // Not owned.
-  PlatformThreadId thread_id_;
+  const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+  MainThreadSchedulerImpl* const scheduler_;  // Not owned.
+  const PlatformThreadId thread_id_;
+  const base::PlatformThreadHandle thread_handle_;
 };
 
 }  // namespace scheduler
