e5079d151ff5878d10dd05b918f6142e049d70db
Mario Sanchez Prada <mario@igalia.com>
2020-12-10 16:29:58 +0000

    Migrate MediaPlayerDelegateHostMsg_OnMutedStatusChanged to mojo
    Migrate this legacy IPC message to the media.mojom.MediaPlayerObserver
    mojo interface, implemented by MediaSessionController in the browser
    process, update its callers in the renderer process.
    
    Finally, this also migrates the code checking if the legacy IPC message
    is correctly sent and received in RendererWebMediaPlayerDelegateTest's
    SendsMessagesCorrectly unit test, by creating a new unit test in the
    HTMLMediaElementTest test suite (i.e. SendsMediaPlayerObserverMessages)
    and adding the infrastructure there to check that the new mojo message
    is sent and received in the other end. Note that this new unit test and
    the newly added infrastructure will be useful to migrate more code from
    from RWMPDTest.SendsMessagesCorrectly as these migrations progress.
    
    Relevant design document:
    https://docs.google.com/document/d/1OLMNxLvGkRO6ju_WfHbRMrgaVnW7bsMnW7XlJQI0A2E
    
    Bug: 1039252
    Change-Id: I5417d6e4707b59559fdbc567dce3844a19884c1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2552596
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835681}

--- third_party/blink/renderer/core/html/media/html_media_element.h
+++ third_party/blink/renderer/core/html/media/html_media_element.h
@@ -349,6 +349,10 @@ class CORE_EXPORT HTMLMediaElement
 
   void SetCcLayerForTesting(cc::Layer* layer) { SetCcLayer(layer); }
 
+  // Required by tests set mock receivers to check that messages are delivered.
+  void SetMediaPlayerObserverForTesting(
+      mojo::PendingRemote<media::mojom::blink::MediaPlayerObserver> observer);
+
   bool IsShowPosterFlagSet() const { return show_poster_flag_; }
 
  protected:
@@ -390,8 +394,9 @@ class CORE_EXPORT HTMLMediaElement
  private:
   // Friend class for testing.
   friend class ContextMenuControllerTest;
-  friend class VideoWakeLockTest;
+  friend class HTMLMediaElementTest;
   friend class PictureInPictureControllerTest;
+  friend class VideoWakeLockTest;
 
   bool HasPendingActivityInternal() const;
 
@@ -466,6 +471,7 @@ class CORE_EXPORT HTMLMediaElement
   bool IsInAutoPIP() const override { return false; }
   void ResumePlayback() final;
   void PausePlayback() final;
+  void DidPlayerMutedStatusChange(bool muted) override;
   void DidPlayerMediaPositionStateChange(double playback_rate,
                                          base::TimeDelta duration,
                                          base::TimeDelta position) override;
