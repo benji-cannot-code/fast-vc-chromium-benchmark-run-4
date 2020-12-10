17a1b3a6e815c31fa0cc7ff75581b89a381cd822
Brandon Wylie <wylieb@chromium.org>
2020-12-10 22:55:24 +0000

    Remove ChromeActivity reference from ChromeWindow
    Bug: 1123209
    Change-Id: I7ef370fe1e6b571bec3a3f1a74e2305e46763a14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566334
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835868}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -341,7 +341,8 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
 
     @Override
     protected ActivityWindowAndroid createWindowAndroid() {
-        return new ChromeWindow(this);
+        return new ChromeWindow(/* activity= */ this, mActivityTabProvider,
+                this::getCompositorViewHolder, getModalDialogManagerSupplier());
     }
 
     @Override
