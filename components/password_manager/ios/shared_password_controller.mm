0f08ad4a64371d834a4c1b3849b6c90c569f16a8
Maria Kazinova <kazinova@google.com>
2020-12-10 17:46:42 +0000

    [iOS, Passwords] Stop rewriting user-typed data on credential filling.
    On other platform, when the user manually typed the username into
    the login form and then selects a suggestion from the password field,
    the typed username is not rewritten. This CL ensures the same behaviour
    on iOS.
    
    Bug: 1157460
    Change-Id: I7fca9e69256c7efb3cdea3133a9a830b9df1af98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2584303
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Vadym Doroshenko  <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835725}

--- components/password_manager/ios/shared_password_controller.mm
+++ components/password_manager/ios/shared_password_controller.mm
@@ -411,6 +411,7 @@ NSString* const kSuggestionSuffix = @" ••••••••";
       }
 
       [self.formHelper fillPasswordFormWithFillData:*fillData
+                                   triggeredOnField:uniqueFieldID
                                   completionHandler:^(BOOL success) {
                                     completion();
                                   }];
