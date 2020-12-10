46ff6884857a52cd3c9404407be26b8044d6334c
Hidehiko Abe <hidehiko@chromium.org>
2020-12-10 18:13:41 +0000

    Lacros initialization with out-band file.
    Currently, startup data is passed from ash-chrome to lacros-chrome
    via LacroChromeService::Init() Mojo call. This requires Mojo initialization
    so that the data is available only after that.
    However, there are some needs that the data would like to be available
    earlier.
    
    In order to support that situation, this CL introduces another approach,
    where ash-chrome dumps the serialized data into a memory backed file
    and lacros-chrome inherits its file descriptor.
    
    For backward compatibility, both lacros-chrome and ash-chrome supports
    older protocol, i.e. LacrosChromeService::Init() for now,
    while it is now deprecated.
    Thus, any {old,new} {lacros-chrome,ash-chrome} combination should work.
    It is planned to remove the older protocol support, when newer/older
    chromes are distributed enough.
    
    BUG=1155494
    TEST=Ran tryjob. Ran {old,new} {lacros-chrome,ash-chrome} combinations
         on DUTs to make sure lacros-chrome can be launched successfully
         via Shelf icon and mojo_connection_lacros_launcher.py script.
    
    Change-Id: I09c7004d07cec9a58ccfc7ee24c0e256149e4951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2573264
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835744}

--- chrome/browser/chromeos/crosapi/browser_manager.cc
+++ chrome/browser/chromeos/crosapi/browser_manager.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process_handle.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
+#include "base/strings/stringprintf.h"
 #include "base/system/sys_info.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -39,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
 #include "chromeos/constants/chromeos_switches.h"
+#include "chromeos/startup/startup_switches.h"
 #include "components/prefs/pref_service.h"
 #include "components/session_manager/core/session_manager.h"
 #include "google_apis/google_api_keys.h"
@@ -349,6 +351,18 @@ void BrowserManager::StartWithLogFile(base::ScopedFD logfd) {
     // the new process.
     options.fds_to_remap.push_back(std::make_pair(logfd.get(), STDOUT_FILENO));
     options.fds_to_remap.push_back(std::make_pair(logfd.get(), STDERR_FILENO));
+  }
+
+  base::ScopedFD startup_fd =
+      browser_util::CreateStartupData(environment_provider_.get());
+  if (startup_fd.is_valid()) {
+    // Hardcoded to use FD 3 to make the ash-chrome's behavior more predictable.
+    // Lacros-chrome should not depend on the hardcoded value though. Instead
+    // it should take a look at the value passed via the command line flag.
+    constexpr int kStartupDataFD = 3;
+    argv.push_back(base::StringPrintf(
+        "--%s=%d", chromeos::switches::kCrosStartupDataFD, kStartupDataFD));
+    options.fds_to_remap.emplace_back(startup_fd.get(), kStartupDataFD);
   }
 
   // Set up Mojo channel.
---
+++ chromeos/startup/startup.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_STARTUP_STARTUP_H_
+#define CHROMEOS_STARTUP_STARTUP_H_
+
+#include <string>
+
+#include "base/component_export.h"
+#include "base/optional.h"
+
+namespace chromeos {
+
+// Reads the startup data. The FD to be read for the startup data should be
+// specified via the kCrosStartupDataFD command line flag. This function
+// consumes the FD, so this must not be called twice in a process.
+COMPONENT_EXPORT(CHROMEOS_STARTUP)
+base::Optional<std::string> ReadStartupData();
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_STARTUP_STARTUP_H_
