3a95bc683dcc06c1409ef519290056b34e8e7d7c
Mathias Bynens <mathias@chromium.org>
2020-12-10 11:52:07 +0000

    Support forcing the CSS :target state
    This patch extends the existing `CSS.forcePseudoState` CDP command so
    that it supports the CSS :target pseudo-class.
    
    DevTools CL: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2581544
    
    Bug: chromium:1156628
    Change-Id: I06d3bc2c76d78601fd0abd403f621e74d2e2ae03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575668
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835623}

---
+++ third_party/blink/web_tests/inspector-protocol/css/css-force-target-pseudo-state.js
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {session, dp} = await testRunner.startHTML(`
+    <style>
+      #target {
+        color: red;
+      }
+      #target:target {
+        color: green;
+      }
+    </style>
+    <div id="target">test</div>
+  `, 'Test CSS.forcePseudoStates method for :target');
+
+  await dp.DOM.enable();
+  await dp.CSS.enable();
+
+  const CSSHelper = await testRunner.loadScript('../resources/css-helper.js');
+  const cssHelper = new CSSHelper(testRunner, dp);
+
+  const documentNodeId = await cssHelper.requestDocumentNodeId();
+  const nodeId = await cssHelper.requestNodeId(documentNodeId, '#target');
+
+  async function getTargetColor() {
+    return await session.evaluate(() => {
+      return window.getComputedStyle(document.querySelector('#target')).color;
+    });
+  }
+
+  testRunner.log('Color without forced :target: ' + await getTargetColor());
+
+  await dp.CSS.forcePseudoState({
+    nodeId,
+    forcedPseudoClasses: ['target'],
+  });
+
+  testRunner.log('Color with forced :target: ' + await getTargetColor());
+
+  await dp.CSS.disable();
+  await dp.DOM.disable();
+
+  testRunner.log('Didn’t fail after disabling the CSS agent (https://crbug.com/1123526).');
+
+  testRunner.completeTest();
+});
