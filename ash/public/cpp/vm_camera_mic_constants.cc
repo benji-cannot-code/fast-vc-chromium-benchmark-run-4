9d8c803735c9c344511a615ffe78f37c980560f5
Jason Lin <lxj@google.com>
2020-12-10 05:25:53 +0000

    Clean up vm_camera_mic_constants.cc
    We only need one notifier id.
    
    Bug: b/167491603
    Change-Id: I426317ef4d87f9aeff91832c26bb613f8138320b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2580380
    Commit-Queue: Jason Lin <lxj@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835555}

--- ash/public/cpp/vm_camera_mic_constants.cc
+++ ash/public/cpp/vm_camera_mic_constants.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-const char kVmMicNotifierId[] = "ash.vm_camera_mic.mic";
-const char kVmCameraNotifierId[] = "ash.vm_camera_mic.camera";
+const char kVmCameraMicNotifierId[] = "ash.vm_camera_mic";
 
 }  // namespace ash
