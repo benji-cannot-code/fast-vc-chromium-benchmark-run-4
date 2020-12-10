f69e39bf3264fe0e74b1b7bd98aa78dcd73188e0
Boris Sazonov <bsazonov@chromium.org>
2020-12-10 21:50:33 +0000

    [Lacros] Add basic unittests for AccountManagerFacadeLacros
    Adds chrome/browser/lacros/account_manager_facade_lacros_unittest.cc and
    a couple of basic tests for AccountManagerFacadeLacros. To make that
    class testable, the mojo::Remote is now passed explicitly to the ctor.
    
    Bug: 1117472
    Change-Id: I59826ff3c7c0d6561b8fd4cb94a164884eb91a4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545923
    Reviewed-by: Roland Bock <rbock@google.com>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835836}

--- chrome/browser/lacros/account_manager_facade_lacros.cc
+++ chrome/browser/lacros/account_manager_facade_lacros.cc
@@ -19,10 +19,15 @@ constexpr uint32_t kMinVersionWithObserver = 1;
 }  // namespace
 
 AccountManagerFacadeLacros::AccountManagerFacadeLacros(
-    mojo::Remote<crosapi::mojom::AccountManager> account_manager_remote)
-    : account_manager_remote_(std::move(account_manager_remote)) {
-  if (!account_manager_remote_)
+    mojo::Remote<crosapi::mojom::AccountManager> account_manager_remote,
+    base::OnceClosure init_finished)
+    : account_manager_remote_(std::move(account_manager_remote)),
+      init_finished_(std::move(init_finished)) {
+  DCHECK(init_finished_);
+  if (!account_manager_remote_) {
+    std::move(init_finished_).Run();
     return;
+  }
 
   account_manager_remote_.QueryVersion(base::BindOnce(
       &AccountManagerFacadeLacros::OnVersionCheck, weak_factory_.GetWeakPtr()));
@@ -35,8 +40,10 @@ bool AccountManagerFacadeLacros::IsInitialized() {
 }
 
 void AccountManagerFacadeLacros::OnVersionCheck(uint32_t version) {
-  if (version < kMinVersionWithObserver)
+  if (version < kMinVersionWithObserver) {
+    std::move(init_finished_).Run();
     return;
+  }
 
   account_manager_remote_->AddObserver(
       base::BindOnce(&AccountManagerFacadeLacros::OnReceiverReceived,
@@ -57,6 +64,7 @@ void AccountManagerFacadeLacros::OnInitialized(bool is_initialized) {
   if (is_initialized)
     is_initialized_ = true;
   // else: We will receive a notification in |OnTokenUpserted|.
+  std::move(init_finished_).Run();
 }
 
 void AccountManagerFacadeLacros::OnTokenUpserted(
