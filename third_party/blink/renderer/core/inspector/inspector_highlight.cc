67249272f81ea018078c949e7d402a2475a87192
Patrick Brosset <patrick.brosset@microsoft.com>
2020-12-10 11:10:22 +0000

    DevTools: Provide gap information to the devtools overlay frontend
    With this extra piece of information, the overlay frontend can now show
    how tall or wide gaps in a flex container are, from DevTools.
    
    Corresponding devtools CL:
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2579039/
    
    Bug: 1150832
    Change-Id: Ia001a4950658624219bef05f169f2ec11566ec56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2582260
    Commit-Queue: Patrick Brosset <patrick.brosset@microsoft.com>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835616}

--- third_party/blink/renderer/core/inspector/inspector_highlight.cc
+++ third_party/blink/renderer/core/inspector/inspector_highlight.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_computed_style_declaration.h"
 #include "third_party/blink/renderer/core/css/css_grid_auto_repeat_value.h"
 #include "third_party/blink/renderer/core/css/css_grid_integer_repeat_value.h"
+#include "third_party/blink/renderer/core/css/css_numeric_literal_value.h"
 #include "third_party/blink/renderer/core/css/css_property_name.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/css/css_value.h"
@@ -993,9 +994,29 @@ std::unique_ptr<protocol::DictionaryValue> BuildFlexInfo(
   flex_info->setValue("containerBorder", container_builder.Release());
   flex_info->setArray("lines", std::move(lines_info));
   flex_info->setBoolean("isHorizontalFlow", is_horizontal);
+  flex_info->setBoolean("isReverse", is_reverse);
   flex_info->setString(
       "alignItemsStyle",
       style->GetPropertyCSSValue(CSSPropertyID::kAlignItems)->CssText());
+
+  double row_gap_value = 0;
+  const CSSValue* row_gap = style->GetPropertyCSSValue(CSSPropertyID::kRowGap);
+  if (row_gap->IsNumericLiteralValue()) {
+    row_gap_value = To<CSSNumericLiteralValue>(row_gap)->DoubleValue();
+  }
+
+  double column_gap_value = 0;
+  const CSSValue* column_gap =
+      style->GetPropertyCSSValue(CSSPropertyID::kColumnGap);
+  if (column_gap->IsNumericLiteralValue()) {
+    column_gap_value = To<CSSNumericLiteralValue>(column_gap)->DoubleValue();
+  }
+
+  flex_info->setDouble("mainGap",
+                       is_horizontal ? column_gap_value : row_gap_value);
+  flex_info->setDouble("crossGap",
+                       is_horizontal ? row_gap_value : column_gap_value);
+
   flex_info->setValue(
       "flexContainerHighlightConfig",
       BuildFlexContainerHighlightConfigInfo(flex_container_highlight_config));
