c2547461f2aa7c5861e02dd138711d5f6b078b24
Tibor Goldschwendt <tiborg@chromium.org>
2020-12-10 00:10:04 +0000

    [ntp] Host Kaleidoscope module code on NTP
    This allows the Kaleidoscope module to make use of shared NTP elements
    such as the ntp-module-header.
    
    Note: This CL effectively disables the Kaleidoscope module. We will
    re-enable once the required downstream crrev/i/3453282 has landed and
    rolled.
    
    Bug: 1152205
    Change-Id: If0d625fbc91ba92ddc835feb3c8aceaba1b65249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2580404
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835445}

--- chrome/browser/resources/new_tab_page/modules/kaleidoscope/module.js
+++ chrome/browser/resources/new_tab_page/modules/kaleidoscope/module.js
@@ -3,19 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/mojo/mojo/public/mojom/base/unguessable_token.mojom-lite.js';
-import 'chrome://resources/mojo/url/mojom/origin.mojom-lite.js';
-
-import {loadKaleidoscopeModule} from 'chrome://kaleidoscope/module.js';
-import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
-
 import {ModuleDescriptor} from '../module_descriptor.js';
 
 /** @type {!ModuleDescriptor} */
 export const kaleidoscopeDescriptor = new ModuleDescriptor(
     /*id=*/ 'kaleidoscope',
     /*heightPx=*/ 384,
-    async () => {
-      return loadKaleidoscopeModule();
-    },
+    async () => null,
 );
