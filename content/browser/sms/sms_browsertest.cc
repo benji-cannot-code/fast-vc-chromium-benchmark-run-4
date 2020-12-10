be59b61dd3ace1b4240cb4d1a3a10f7b27b2d0e8
Yi Gu <yigu@chromium.org>
2020-12-10 03:23:37 +0000

    Revert "[WebOTP] Use a feature with variations instead of a multi value switch"
    This reverts commit 7f95bc55de3bf169bc3a2ac53e61e4afda792eff.
    
    Reason for revert: Updating the backend in chrome://flags reliably leads to crash. Reverted locally and the crash was gone.
    
    Original change's description:
    > [WebOTP] Use a feature with variations instead of a multi value switch
    >
    > This allows WebOTP backend to be configured via finch trial infra.
    >
    >
    > Bug: 1153880
    > Change-Id: I645171b4635595a48091ff3f40a63839321cf5ab
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2564007
    > Commit-Queue: Majid Valipour <majidvp@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Yi Gu <yigu@chromium.org>
    > Reviewed-by: Majid Valipour <majidvp@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#835511}
    
    TBR=nasko@chromium.org,majidvp@chromium.org,yigu@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I10dc087b539041f42781525e30f527846a708203
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1153880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2583117
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835528}

--- content/browser/sms/sms_browsertest.cc
+++ content/browser/sms/sms_browsertest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
-#include "base/test/scoped_feature_list.h"
 #include "components/ukm/test_ukm_recorder.h"
 #include "content/browser/browser_main_loop.h"
 #include "content/browser/sms/sms_fetcher_impl.h"
@@ -56,6 +55,8 @@ class SmsBrowserTest : public ContentBrowserTest {
     ContentBrowserTest::SetUpCommandLine(command_line);
     command_line->AppendSwitch(
         switches::kEnableExperimentalWebPlatformFeatures);
+    command_line->AppendSwitchASCII(switches::kWebOtpBackend,
+                                    switches::kWebOtpBackendSmsVerification);
     cert_verifier_.SetUpCommandLine(command_line);
   }
 
@@ -742,6 +743,8 @@ IN_PROC_BROWSER_TEST_F(SmsBrowserTest, AbortAfterSmsRetrieval) {
 }
 
 IN_PROC_BROWSER_TEST_F(SmsBrowserTest, SmsFetcherUAF) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kWebOtpBackend, switches::kWebOtpBackendUserConsent);
   GURL url = GetTestUrl(nullptr, "simple_page.html");
   EXPECT_TRUE(NavigateToURL(shell(), url));
 
@@ -965,7 +968,8 @@ IN_PROC_BROWSER_TEST_F(SmsBrowserTest,
 // Disabled test (fails intermittently on Android): https://crbug.com/1154692
 IN_PROC_BROWSER_TEST_F(SmsBrowserTest, DISABLED_RecordUserCancelledAsOutcome) {
   base::HistogramTester histogram_tester;
-
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kWebOtpBackend, switches::kWebOtpBackendUserConsent);
   GURL url = GetTestUrl(nullptr, "simple_page.html");
   EXPECT_TRUE(NavigateToURL(shell(), url));
 
--- content/browser/sms/sms_provider_gms.cc
+++ content/browser/sms/sms_provider_gms.cc
@@ -10,8 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/bind.h"
 #include "base/command_line.h"
-#include "base/feature_list.h"
-#include "content/public/common/content_features.h"
+#include "content/public/common/content_switches.h"
 
 #include "content/public/android/content_jni_headers/SmsProviderGms_jni.h"
 #include "content/public/browser/web_contents.h"
@@ -29,14 +28,14 @@ SmsProviderGms::SmsProviderGms() {
   // launched http://crbug.com/1101050
   GmsBackend backend = GmsBackend::kUserConsent;
 
-  std::string backend_value = base::GetFieldTrialParamValueByFeature(
-      features::kWebOtpBackend, "backend");
-
-  if (backend_value == "user_consent") {
+  auto switch_value =
+      base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+          switches::kWebOtpBackend);
+  if (switch_value == switches::kWebOtpBackendUserConsent) {
     backend = GmsBackend::kUserConsent;
-  } else if (backend_value == "verification") {
+  } else if (switch_value == switches::kWebOtpBackendSmsVerification) {
     backend = GmsBackend::kVerification;
-  } else if (backend_value == "auto") {
+  } else if (switch_value == switches::kWebOtpBackendAuto) {
     backend = GmsBackend::kAuto;
   }
 
