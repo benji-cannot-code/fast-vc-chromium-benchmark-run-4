de8d93752ebae9858b790c4d3177f0162bf6e719
Gavin Williams <gavinwill@chromium.org>
2020-12-10 23:23:00 +0000

    scanning: a11y: Add ARIA labels for preview area
    This change adds an ARIA label for the preview area that is updated
    depending on the current page:
     -In the initial state, grab the scan preview instructions from the page
     -While scanning, update the label when the page number increases
     -When the scan is completed successfully, announce scan is completed
      and the total number of pages scanned.
    
    Bug: 1059779
    Change-Id: I1fcdf2552f779737a69d95fe5971c1b98c0a6aa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2580846
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835882}

--- chromeos/components/scanning/resources/scan_done_section.js
+++ chromeos/components/scanning/resources/scan_done_section.js
@@ -45,8 +45,8 @@ Polymer({
 
   /** @override */
   created() {
+    // ScanningBrowserProxy is initialized when scanning_app.js is created.
     this.browserProxy_ = ScanningBrowserProxyImpl.getInstance();
-    this.browserProxy_.initialize();
   },
 
   /** @private */
