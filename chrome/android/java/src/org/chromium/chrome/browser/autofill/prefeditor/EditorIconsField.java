3859f92093edf81fc3cfd6d59f256e20b65ebbea
Rouslan Solomakhin <rouslan@chromium.org>
2020-12-10 01:41:43 +0000

    [Autofill] Increase icon clickable area.
    Before this patch, the icon representing the allowed card networks
    (e.g., visa, mastercard) had a touch target of 38x24dp, which is smaller
    than the 48x48dp required for accessibility.
    
    This patch changes the icon column sizes from 38dp to 48dp, adds 5dp
    horizontal padding and 13dp vertical padding to each icon, and removes
    extra margins around the now larger icon touch targets.
    
    After this patch, the touch target for each icon is 48x48dp.
    
    Bug: 977272
    Change-Id: Ieb4cf01ae3afaa225a36adf1aeadd5dca336c108
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2580584
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835493}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/prefeditor/EditorIconsField.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/prefeditor/EditorIconsField.java
@@ -55,10 +55,12 @@ class EditorIconsField {
      * An instance of a {@link BaseAdapter} that provides a list of card icon views.
      */
     private static class IconListAdapter extends BaseAdapter {
-        private Context mContext;
-        private List<Integer> mIconResourceIds;
-        private List<Integer> mIconDescriptionIds;
-        private int mIconSize;
+        private final Context mContext;
+        private final List<Integer> mIconResourceIds;
+        private final List<Integer> mIconDescriptionIds;
+        private final int mIconSize;
+        private final int mHorizontalPaddingInPixels;
+        private final int mVerticalPaddingInPixels;
 
         public IconListAdapter(
                 Context context, List<Integer> iconResourceIds, List<Integer> iconDescriptionIds) {
@@ -67,6 +69,10 @@ class EditorIconsField {
             mIconDescriptionIds = iconDescriptionIds;
             mIconSize = mContext.getResources().getDimensionPixelSize(
                     R.dimen.editable_option_section_logo_width);
+            mHorizontalPaddingInPixels = mContext.getResources().getDimensionPixelSize(
+                    R.dimen.editable_option_section_logo_horizontal_padding);
+            mVerticalPaddingInPixels = mContext.getResources().getDimensionPixelSize(
+                    R.dimen.editable_option_section_logo_vertical_padding);
             assert mIconResourceIds.size() == mIconDescriptionIds.size();
         }
 
@@ -95,6 +101,8 @@ class EditorIconsField {
             iconView.setAdjustViewBounds(true);
             iconView.setMaxWidth(mIconSize);
             iconView.setMaxHeight(mIconSize);
+            iconView.setPadding(mHorizontalPaddingInPixels, mVerticalPaddingInPixels,
+                    mHorizontalPaddingInPixels, mVerticalPaddingInPixels);
             return iconView;
         }
     }
