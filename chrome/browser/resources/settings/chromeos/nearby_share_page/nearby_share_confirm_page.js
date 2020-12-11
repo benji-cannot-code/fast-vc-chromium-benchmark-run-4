c43b4069d6baf356e77adaba9a072a5263757344
Curt Clemens <cclem@google.com>
2020-12-11 00:30:03 +0000

    [Nearby] Show errors on receive confirmation page
    Add an error section to the receive confirmation page. Add a
    |transferStatus_| property the receive dialog and bind it to a
    corresponding property on the confirm page. Set the error strings
    when the transfer status changes.
    
    The error messages shown are not final -- more work to follow. I wanted
    to get this CL submitted first since it sets up some basic layout for
    the receive confirmation page, and other work on that page can be done
    in parallel.
    
    Screenshot: https://screenshot.googleplex.com/6DUqcghbkevoRwG.png
    
    Bug: 1157284
    Change-Id: I72babb7bd27821ab679cd16e4bd7e3eb3520841e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2583210
    Commit-Queue: Curt Clemens <cclem@google.com>
    Reviewed-by: James Vecore <vecore@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#835918}

--- chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_confirm_page.js
+++ chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_confirm_page.js
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'nearby-share-confirm-page',
 
+  behaviors: [I18nBehavior],
+
   properties: {
     /** @type {?nearbyShare.mojom.ShareTarget} */
     shareTarget: {
@@ -23,5 +25,65 @@ Polymer({
       type: String,
       value: null,
     },
-  }
+
+    /**
+     * @type {?nearbyShare.mojom.TransferStatus}
+     */
+    transferStatus: {
+      type: nearbyShare.mojom.TransferStatus,
+      value: null,
+      observer: 'onTransferStatusChanged_',
+    },
+
+    /**
+     * Header text for error. Controls error display on the confirm page.
+     * The error section is not displayed if this is falsey.
+     * @private {?string}
+     */
+    errorTitle_: {
+      type: String,
+      value: null,
+    },
+
+    /**
+     * Description text for error display on confirm page, displayed under the
+     * error title.
+     * @private {?string}
+     */
+    errorDescription_: {
+      type: String,
+      value: null,
+    },
+  },
+
+  /**
+   * Update the |errorTitle_| and the |errorDescription_| when the transfer
+   * status changes.
+   * @param {?nearbyShare.mojom.TransferStatus} newStatus
+   */
+  onTransferStatusChanged_(newStatus) {
+    // TODO(cclem) review this mapping, add missing statuses, make sure the
+    // strings still make sense in the context of the receive page
+    switch (newStatus) {
+      case nearbyShare.mojom.TransferStatus.kTimedOut:
+        this.errorTitle_ = this.i18n('nearbyShareErrorTimeOut');
+        this.errorDescription_ = this.i18n('nearbyShareErrorNoResponse');
+        break;
+      case nearbyShare.mojom.TransferStatus.kUnsupportedAttachmentType:
+        this.errorTitle_ = this.i18n('nearbyShareErrorCantShare');
+        this.errorDescription_ =
+            this.i18n('nearbyShareErrorUnsupportedFileType');
+        break;
+      case nearbyShare.mojom.TransferStatus.kMediaUnavailable:
+      case nearbyShare.mojom.TransferStatus.kNotEnoughSpace:
+      case nearbyShare.mojom.TransferStatus.kFailed:
+      case nearbyShare.mojom.TransferStatus.kAwaitingRemoteAcceptanceFailed:
+        this.errorTitle_ = this.i18n('nearbyShareErrorCantShare');
+        this.errorDescription_ = this.i18n('nearbyShareErrorSomethingWrong');
+        break;
+      default:
+        this.errorTitle_ = null;
+        this.errorDescription = null;
+    }
+  },
 });
--- chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_receive_dialog.js
+++ chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_receive_dialog.js
@@ -55,6 +55,15 @@ Polymer({
       type: Object,
       value: {},
     },
+
+    /**
+     * Status of the current transfer.
+     * @private {?nearbyShare.mojom.TransferStatus}
+     */
+    transferStatus_: {
+      type: nearbyShare.mojom.TransferStatus,
+      value: null,
+    }
   },
 
   listeners: {
@@ -142,6 +151,8 @@ Polymer({
    * @param {!nearbyShare.mojom.TransferMetadata} metadata
    */
   onTransferUpdate(shareTarget, metadata) {
+    this.transferStatus_ = metadata.status;
+
     if (metadata.status ===
         nearbyShare.mojom.TransferStatus.kAwaitingLocalConfirmation) {
       clearTimeout(this.closeTimeoutId_);
