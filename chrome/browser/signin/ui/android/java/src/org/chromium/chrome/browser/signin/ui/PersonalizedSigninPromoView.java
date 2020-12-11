cee2a860465cbe5a19585b232521361aa4729d1e
Alice Wang <aliceywang@chromium.org>
2020-12-11 11:40:51 +0000

    [Signin][Modularization] Move PersonalizedSigninPromoView to signin.ui target
    This CL moves PersonalizedSigninPromoView to signin.ui target.
    
    Bug: 1156619
    Change-Id: Ic0c0a1e23c05b88bb36f4daee3eba469c7fcb63f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2584505
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836067}

--- chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/PersonalizedSigninPromoView.java
+++ chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/PersonalizedSigninPromoView.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.signin;
+package org.chromium.chrome.browser.signin.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
@@ -13,7 +13,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.chromium.chrome.R;
 import org.chromium.ui.widget.ButtonCompat;
 
 /**
