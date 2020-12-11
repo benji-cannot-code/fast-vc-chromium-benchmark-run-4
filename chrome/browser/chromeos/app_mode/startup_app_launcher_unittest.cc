d73653b4acc18fd90e227fefc0d4d3105849da3e
Anqing Zhao <anqing@google.com>
2020-12-11 23:21:21 +0000

    Limit the use of KioskAppExternalLoader to Chrome App Kiosk only
    KioskAppExternalLoader is used to load apps and extensions for a Chrome
    App Kiosk session. It registers a callback and wait until app info is
    updated.
    
    This class should be only used in Chrome App Kiosk, but not in other two
    Kiosk types. But according to the current implementation, the loader is
    enabled unexpectedly and blocks the uninstallation of force-installed
    extensions when they are removed by admins. Therefore, the check logic
    should be limited to a proper scope.
    
    Bug: 1153440
    Change-Id: I8bf79fa553dfbea5e271b7f06fc6ff0dffb33495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575118
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Oleg Davydov <burunduk@chromium.org>
    Commit-Queue: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836300}

--- chrome/browser/chromeos/app_mode/startup_app_launcher_unittest.cc
+++ chrome/browser/chromeos/app_mode/startup_app_launcher_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/app_mode/kiosk_app_manager.h"
 #include "chrome/browser/chromeos/app_mode/test_kiosk_extension_builder.h"
 #include "chrome/browser/chromeos/extensions/test_external_cache.h"
+#include "chrome/browser/chromeos/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/chromeos/policy/device_local_account.h"
 #include "chrome/browser/chromeos/settings/scoped_cros_settings_test_helper.h"
 #include "chrome/browser/extensions/extension_service.h"
@@ -37,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/settings/cros_settings_names.h"
 #include "components/account_id/account_id.h"
 #include "components/session_manager/core/session_manager.h"
+#include "components/user_manager/scoped_user_manager.h"
 #include "components/version_info/channel.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
@@ -352,6 +354,7 @@ class StartupAppLauncherTest : public extensions::ExtensionServiceTestBase,
 
     extensions::ExtensionServiceTestBase::SetUp();
 
+    InitializeKioskAppUser();
     InitializeEmptyExtensionService();
     external_apps_loader_handler_ = std::make_unique<TestKioskLoaderVisitor>(
         browser_context(), registry(), service());
@@ -555,6 +558,18 @@ class StartupAppLauncherTest : public extensions::ExtensionServiceTestBase,
   }
 
  protected:
+  void InitializeKioskAppUser() {
+    const AccountId kiosk_account_id(
+        AccountId::FromUserEmail(kTestUserAccount));
+    auto fake_user_manager_ =
+        std::make_unique<chromeos::FakeChromeUserManager>();
+    fake_user_manager_->AddKioskAppUser(kiosk_account_id);
+    fake_user_manager_->LoginUser(kiosk_account_id);
+
+    user_manager_enabler_ = std::make_unique<user_manager::ScopedUserManager>(
+        std::move(fake_user_manager_));
+  }
+
   TestAppLaunchDelegate startup_launch_delegate_;
 
   std::unique_ptr<KioskAppLauncher> startup_app_launcher_;
@@ -573,6 +588,8 @@ class StartupAppLauncherTest : public extensions::ExtensionServiceTestBase,
 
   std::unique_ptr<extensions::ExternalProviderImpl> primary_app_provider_;
   std::unique_ptr<extensions::ExternalProviderImpl> secondary_apps_provider_;
+
+  std::unique_ptr<user_manager::ScopedUserManager> user_manager_enabler_;
 
   DISALLOW_COPY_AND_ASSIGN(StartupAppLauncherTest);
 };
