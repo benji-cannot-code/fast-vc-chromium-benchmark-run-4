8aca31d2b2bc9f0ed63474f9d4d3cb42aef88b95
Scott Violet <sky@chromium.org>
2020-12-11 16:55:10 +0000

    downloads/history/safe_browsing: use inclusive names
    This updates a number of call sites to use inclusive terms.
    
    BUG=1105161
    TEST=none, rename only
    
    Change-Id: I366e5087bb5b0b98ceffe055224f01ea5a3fecaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585226
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836143}

--- chrome/browser/safe_browsing/download_protection/download_protection_service_unittest.cc
+++ chrome/browser/safe_browsing/download_protection/download_protection_service_unittest.cc
@@ -2703,7 +2703,7 @@ TEST_F(DownloadProtectionServiceTest,
       default_file_path, alternate_extensions, profile(),
       base::BindOnce(&DownloadProtectionServiceTest::SyncCheckDoneCallback,
                      base::Unretained(this)));
-  ASSERT_TRUE(IsResult(DownloadCheckResult::WHITELISTED_BY_POLICY));
+  ASSERT_TRUE(IsResult(DownloadCheckResult::ALLOWLISTED_BY_POLICY));
 }
 
 TEST_F(DownloadProtectionServiceTest,
@@ -2802,7 +2802,7 @@ TEST_F(DownloadProtectionServiceTest, CheckClientDownloadWhitelistedByPolicy) {
   run_loop.Run();
 
   EXPECT_FALSE(HasClientDownloadRequest());
-  EXPECT_TRUE(IsResult(DownloadCheckResult::WHITELISTED_BY_POLICY));
+  EXPECT_TRUE(IsResult(DownloadCheckResult::ALLOWLISTED_BY_POLICY));
 }
 
 TEST_F(DownloadProtectionServiceTest, CheckOffTheRecordDoesNotSendFeedback) {
@@ -3446,7 +3446,7 @@ TEST_F(DownloadProtectionServiceTest,
   // be posted.
   EXPECT_FALSE(has_result_);
   base::RunLoop().RunUntilIdle();
-  ASSERT_TRUE(IsResult(DownloadCheckResult::WHITELISTED_BY_POLICY));
+  ASSERT_TRUE(IsResult(DownloadCheckResult::ALLOWLISTED_BY_POLICY));
 }
 
 TEST_F(DownloadProtectionServiceTest,
