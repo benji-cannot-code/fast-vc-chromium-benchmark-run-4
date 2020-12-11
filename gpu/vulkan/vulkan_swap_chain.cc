76bd06df53be8ed9cd86ef59cc4313bbf98c9ac6
Peng Huang <penghuang@chromium.org>
2020-12-11 16:22:57 +0000

    Remove LOG(FATAL) in vulkan_swap_chain.cc
    With NVidia GPU, the Xserver crashing and vkCreateSwapchainKHR()
    returning errors happen at same time. Not sure if Xserver crashes cause
    the vkCreateSwapchainKHR() call fail, or Chrome crashes after
    vkCreateSwapchainKHR() returning errors cause Xserver crash. So with
    this change, Chrome GPU process will not crash when
    vkCreateSwapchainKHR() return errors. Instead chrome GPU process will
    handle the error, and consider it as context lost, and then try to tear
    down everything, and then restart the GPU process.
    
    Bug: 1149673
    Change-Id: I9f5e5ca5aaf3f17107e1f9de31fa7837e6489d23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587073
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836137}

--- gpu/vulkan/vulkan_swap_chain.cc
+++ gpu/vulkan/vulkan_swap_chain.cc
@@ -226,7 +226,7 @@ bool VulkanSwapChain::InitializeSwapChain(
   }
 
   if (UNLIKELY(VK_SUCCESS != result)) {
-    LOG(FATAL) << "vkCreateSwapchainKHR() failed: " << result;
+    LOG(DFATAL) << "vkCreateSwapchainKHR() failed: " << result;
     return false;
   }
 
