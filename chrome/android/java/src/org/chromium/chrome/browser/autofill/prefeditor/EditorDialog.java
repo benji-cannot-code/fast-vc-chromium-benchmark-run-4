a779e38cbb851b07a134a279b6ce34036372e738
Rouslan Solomakhin <rouslan@chromium.org>
2020-12-11 15:42:16 +0000

    [Autofill] Increase dropdown touch target
    Before this patch, the touch target of the dropdowns in the card and
    address editors were 24dp tall, which is shorter than the 48dp required
    for accessibility.
    
    This patch adds a 13dp padding on top and bottom of the dropdowns in the
    card and address editor.
    
    Because varying sizes of string values in the dropdown can wrap and
    cause the dropdown height to increase, alignment with text fields on the
    same row is difficult, so dropdowns are now prevented from sharing the
    same line with text fields. This affects the US address format in
    particular, where the "State" dropdown shared a line with the "ZIP Code"
    textfield. Other forms that shared two dropdowns in the row (e.g.,
    expiration month and year in card form) or two textfields in the row are
    not affected.
    
    After this patch, the touch target of the dropdowns in the card and
    address editors is at least 48dp tall.
    
    Bug: 977272
    Change-Id: I9b99f514a7427af7ddf7f0963c724f2cdda0cb0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2586147
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836120}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/prefeditor/EditorDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/prefeditor/EditorDialog.java
@@ -81,7 +81,6 @@ public class EditorDialog
     private final Handler mHandler;
     private final TextView.OnEditorActionListener mEditorActionListener;
     private final int mHalfRowMargin;
-    private final int mDropdownTopPadding;
     private final List<EditorFieldView> mFieldViews;
     private final List<EditText> mEditableTextFields;
     private final List<Spinner> mDropdownFields;
@@ -140,8 +139,6 @@ public class EditorDialog
 
         mHalfRowMargin = activity.getResources().getDimensionPixelSize(
                 R.dimen.editor_dialog_section_large_spacing);
-        mDropdownTopPadding = activity.getResources().getDimensionPixelSize(
-                R.dimen.payments_section_dropdown_top_padding);
         mFieldViews = new ArrayList<>();
         mEditableTextFields = new ArrayList<>();
         mDropdownFields = new ArrayList<>();
@@ -399,6 +396,13 @@ public class EditorDialog
                 if (nextFieldModel.isFullLine()) useFullLine = true;
             }
 
+            // Always keep dropdowns and text fields on different lines because of height
+            // differences.
+            if (!isLastField && !useFullLine
+                    && fieldModel.isDropdownField() != nextFieldModel.isDropdownField()) {
+                useFullLine = true;
+            }
+
             if (useFullLine || isLastField) {
                 addFieldViewToEditor(mDataView, fieldModel);
             } else {
@@ -419,15 +423,6 @@ public class EditorDialog
                 MarginLayoutParamsCompat.setMarginEnd(firstParams, mHalfRowMargin);
                 lastParams.width = 0;
                 lastParams.weight = 1;
-
-                // Align the text field and the dropdown field.
-                if ((fieldModel.isTextField() && nextFieldModel.isDropdownField())
-                        || (nextFieldModel.isTextField() && fieldModel.isDropdownField())) {
-                    LinearLayout.LayoutParams dropdownParams =
-                            fieldModel.isDropdownField() ? firstParams : lastParams;
-                    dropdownParams.topMargin = mDropdownTopPadding;
-                    dropdownParams.bottomMargin = 0;
-                }
 
                 i = i + 1;
             }
