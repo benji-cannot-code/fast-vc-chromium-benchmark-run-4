2c215c577cf50166f0a05a3d5c6ccd89215f84ba
Guillaume Jenkins <gujen@google.com>
2020-12-11 23:26:02 +0000

    [iOS Enterprise] BrowserSignin: hide Recent Tabs promo
    When browser sign-in is disabled by policy, replaces the sign-in promo
    in the "Other devices" section of the Recent Tabs views with a message
    indicating that the feature has been disabled by the managing
    organization / administrator.
    
    Bug: 1155745
    Change-Id: I496e7f7ec53d721748e8a49c9a01228aa9b43548
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575260
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836302}

--- ios/chrome/browser/ui/recent_tabs/recent_tabs_table_view_controller.mm
+++ ios/chrome/browser/ui/recent_tabs/recent_tabs_table_view_controller.mm
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #import "base/numerics/safe_conversions.h"
 #include "base/strings/sys_string_conversions.h"
+#include "components/prefs/pref_service.h"
 #include "components/sessions/core/tab_restore_service.h"
+#include "components/signin/public/base/signin_pref_names.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/sync_sessions/open_tabs_ui_delegate.h"
 #include "components/sync_sessions/session_sync_service.h"
@@ -23,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/drag_and_drop/table_view_url_drag_drop_handler.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/metrics/new_tab_page_uma.h"
+#include "ios/chrome/browser/policy/policy_features.h"
 #include "ios/chrome/browser/sessions/live_tab_context_browser_agent.h"
 #include "ios/chrome/browser/sessions/session_util.h"
 #include "ios/chrome/browser/sync/session_sync_service_factory.h"
@@ -98,6 +101,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeSessionHeader,
   ItemTypeSessionTabData,
   ItemTypeShowFullHistory,
+  ItemTypeSigninDisabled,
 };
 
 // Key for saving whether the Other Device section is collapsed.
@@ -495,7 +499,18 @@ API_AVAILABLE(ios(13.0))
         [self.tableViewModel sectionIsCollapsed:SectionIdentifierOtherDevices];
   }
 
-  if (base::FeatureList::IsEnabled(kIllustratedEmptyStates)) {
+  if (ShouldInstallBrowserSigninPolicyHandler() &&
+      !self.browserState->GetPrefs()->GetBoolean(prefs::kSigninAllowed)) {
+    // If sign-in is disabled, don't show an illustration or a sign-in promo.
+    TableViewTextItem* disabledByOrganizationText =
+        [[TableViewTextItem alloc] initWithType:ItemTypeSigninDisabled];
+    disabledByOrganizationText.text =
+        l10n_util::GetNSString(IDS_IOS_RECENT_TABS_DISABLED_BY_ORGANIZATION);
+    disabledByOrganizationText.textColor =
+        [UIColor colorNamed:kTextSecondaryColor];
+    [self.tableViewModel addItem:disabledByOrganizationText
+         toSectionWithIdentifier:SectionIdentifierOtherDevices];
+  } else if (base::FeatureList::IsEnabled(kIllustratedEmptyStates)) {
     ItemType itemType;
     NSString* itemSubtitle;
     NSString* itemButtonText;
@@ -817,6 +832,7 @@ API_AVAILABLE(ios(13.0))
     case ItemTypeOtherDevicesSyncOff:
     case ItemTypeOtherDevicesNoSessions:
     case ItemTypeOtherDevicesSigninPromo:
+    case ItemTypeSigninDisabled:
       break;
   }
 }
@@ -861,8 +877,10 @@ API_AVAILABLE(ios(13.0))
       itemTypeSelected == ItemTypeSessionTabData) {
     [self loadFaviconForCell:cell indexPath:indexPath];
   }
-  // ItemTypeOtherDevicesNoSessions should not be selectable.
-  if (itemTypeSelected == ItemTypeOtherDevicesNoSessions) {
+  // ItemTypeOtherDevicesNoSessions and ItemTypeSigninDisabled should not be
+  // selectable.
+  if (itemTypeSelected == ItemTypeOtherDevicesNoSessions ||
+      itemTypeSelected == ItemTypeSigninDisabled) {
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
   }
   // Set button action method for ItemTypeOtherDevicesSyncOff.
@@ -998,6 +1016,7 @@ API_AVAILABLE(ios(13.0))
     case ItemTypeOtherDevicesSyncInProgressHeader:
     case ItemTypeSessionHeader:
     case ItemTypeShowFullHistory:
+    case ItemTypeSigninDisabled:
       break;
   }
   return nil;
