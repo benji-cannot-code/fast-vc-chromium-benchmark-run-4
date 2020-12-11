60a72e841be3ee03849f8aebb988d87b060e0099
Benoit Lize <lizeb@chromium.org>
2020-12-11 11:08:55 +0000

    [discardable] Always purge discardable memory on the manager's thread.
    The provided task runner was always bound to the same thread on which
    the manager is created. Make this explicit rather than implicit.
    
    Bug: 1131857
    Change-Id: I1e24d20edf9cd77c2ce904d13e2e66ef559cc183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527045
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836063}

--- components/discardable_memory/client/client_discardable_shared_memory_manager.h
+++ components/discardable_memory/client/client_discardable_shared_memory_manager.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted_delete_on_sequence.h"
 #include "base/memory/unsafe_shared_memory_region.h"
 #include "base/synchronization/lock.h"
+#include "base/threading/thread_checker.h"
 #include "base/trace_event/memory_dump_provider.h"
 #include "components/discardable_memory/common/discardable_memory_export.h"
 #include "components/discardable_memory/common/discardable_shared_memory_heap.h"
@@ -41,9 +42,7 @@ class DISCARDABLE_MEMORY_EXPORT ClientDiscardableSharedMemoryManager
  public:
   ClientDiscardableSharedMemoryManager(
       mojo::PendingRemote<mojom::DiscardableSharedMemoryManager> manager,
-      scoped_refptr<base::SingleThreadTaskRunner> io_task_runner,
-      scoped_refptr<base::SingleThreadTaskRunner> periodic_purge_task_runner =
-          nullptr);
+      scoped_refptr<base::SingleThreadTaskRunner> io_task_runner);
 
   // Overridden from base::DiscardableMemoryAllocator:
   std::unique_ptr<base::DiscardableMemory> AllocateLockedDiscardableMemory(
@@ -78,9 +77,6 @@ class DISCARDABLE_MEMORY_EXPORT ClientDiscardableSharedMemoryManager
       const char* name,
       base::trace_event::ProcessMemoryDump* pmd) const;
 
-  void StartScheduledPurging();
-  void StopScheduledPurging();
-
   struct Statistics {
     size_t total_size;
     size_t freelist_size;
@@ -95,6 +91,13 @@ class DISCARDABLE_MEMORY_EXPORT ClientDiscardableSharedMemoryManager
   static constexpr base::TimeDelta kMinAgeForScheduledPurge =
       base::TimeDelta::FromMinutes(5);
 
+  // The expected cost of purging should be very small (< 1ms), so it can be
+  // scheduled frequently. However, we don't purge memory that has been touched
+  // recently (see: |BackgroundPurge()| and |kMinAgeForScheduledPurge|), so
+  // there is no benefit to scheduling this more than once per minute.
+  static constexpr base::TimeDelta kScheduledPurgeInterval =
+      base::TimeDelta::FromMinutes(1);
+
   // These fields are only protected for testing, they would otherwise be
   // private. Everything else should be either public or private.
  protected:
@@ -103,13 +106,13 @@ class DISCARDABLE_MEMORY_EXPORT ClientDiscardableSharedMemoryManager
   friend class base::DeleteHelper<ClientDiscardableSharedMemoryManager>;
 
   ~ClientDiscardableSharedMemoryManager() override;
-  ClientDiscardableSharedMemoryManager(
-      scoped_refptr<base::SingleThreadTaskRunner> io_task_runner,
-      scoped_refptr<base::SingleThreadTaskRunner> periodic_purge_task_runner);
-  std::unique_ptr<DiscardableSharedMemoryHeap> heap_ GUARDED_BY(lock_);
+  explicit ClientDiscardableSharedMemoryManager(
+      scoped_refptr<base::SingleThreadTaskRunner> io_task_runner);
+
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   mutable base::Lock lock_;
-  std::unique_ptr<base::RepeatingTimer> timer_ GUARDED_BY(lock_);
-  scoped_refptr<base::SingleThreadTaskRunner> periodic_purge_task_runner_;
+  std::unique_ptr<DiscardableSharedMemoryHeap> heap_ GUARDED_BY(lock_);
+  bool is_purge_scheduled_ GUARDED_BY(lock_) = false;
 
  private:
   class DiscardableMemoryImpl : public base::DiscardableMemory {
@@ -179,6 +182,7 @@ class DISCARDABLE_MEMORY_EXPORT ClientDiscardableSharedMemoryManager
   void ReleaseSpan(std::unique_ptr<DiscardableSharedMemoryHeap::Span> span)
       EXCLUSIVE_LOCKS_REQUIRED(lock_);
   base::Lock& GetLock() { return lock_; }
+  size_t GetBytesAllocatedLocked() const EXCLUSIVE_LOCKS_REQUIRED(lock_);
 
   scoped_refptr<base::SingleThreadTaskRunner> io_task_runner_;
   // TODO(penghuang): Switch to SharedRemote when it starts supporting
@@ -196,6 +200,10 @@ class DISCARDABLE_MEMORY_EXPORT ClientDiscardableSharedMemoryManager
   // RenderThreadImpl.
   bool foregrounded_ = false;
 
+  // Whether the scheduled purge feature is enabled.
+  const bool may_schedule_periodic_purge_;
+
+  THREAD_CHECKER(thread_checker_);
   base::WeakPtrFactory<ClientDiscardableSharedMemoryManager> weak_factory_{
       this};
 
