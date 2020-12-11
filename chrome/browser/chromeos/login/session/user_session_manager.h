79b1385746b3fc93dc47611026376e804fe43ff7
Leo Lai <cylai@google.com>
2020-12-11 07:25:23 +0000

    UserSessionManager use TpmmanagerClient to prepare TPM device.
    We are replacing tpm status APis by cryptohome with tpm manager service.
    
    Though the change to the workflow is implemented UserSessionManager is
    supposed to operate almost the same way, before and after this CL.
    Below the behavioral changes are highlighted:
    
    1. TpmIsBeingOwned is removed as a factor from the state machine.
    TpmIsBeingOwned is no longer supported and always returns `false` now.
    Removing it is a trivial change.
    2. As for execution order, `PrepareFinalizeProfile()` is separated
    totally from the preparation of TPM device. The justification comes from
    the fact that `CryptohomeClient::CanAttemptOwnership()` only triggers
    TPM initialization by tpm manager w/o waiting for it to complete; it
    doesn't make sense to just wait for a trigger w/o checking its result.
    Clearing owner password is in best effort and the result is not checked
    even in cases where any remaining owner password dependencies stops the
    password from being cleared.
    3. TPM status checking is made asynchronous after this CL to make sure
    UI thread doesn't get blocked.
    4. Upon failure of calling tpm_manager, `DCHECK` is replaced by logging
    error message. It doesn't make sense to just crash upon the error, and
    in production it doesn't make any difference anyway.
    
    BUG=b:172748724
    BUG=b:172775884
    TEST=unit_tests (filter: *UserSessionManager*)
    TEST=browser_tests (filter: *ExistingUserController*)
    TEST=asan + msan builder trybot passes.
    TEST= perform first login, check if the password exists/gets wiped
    before/after the login.
    
    Change-Id: I66c5ef3faae8c78d9f2cb304ccf59ca7a8f80c01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2560087
    Commit-Queue: Leo Lai <cylai@google.com>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836026}

--- chrome/browser/chromeos/login/session/user_session_manager.h
+++ chrome/browser/chromeos/login/session/user_session_manager.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/chromeos/u2f_notification.h"
 #include "chromeos/dbus/session_manager/session_manager_client.h"
+#include "chromeos/dbus/tpm_manager/tpm_manager.pb.h"
 #include "chromeos/login/auth/authenticator.h"
 #include "chromeos/login/auth/user_context.h"
 #include "components/arc/net/always_on_vpn_manager.h"
@@ -419,13 +420,6 @@ class UserSessionManager
   // Callback to resume profile creation after transferring auth data from
   // the authentication profile.
   void CompleteProfileCreateAfterAuthTransfer(Profile* profile);
-
-  // Asynchronously prepares TPM devices and calls FinalizePrepareProfile on UI
-  // thread.
-  void PrepareTpmDeviceAndFinalizeProfile(Profile* profile);
-
-  // Called on UI thread once Cryptohome operation completes.
-  void OnCryptohomeOperationCompleted(Profile* profile, bool result);
 
   // Finalized profile preparation.
   void FinalizePrepareProfile(Profile* profile);
