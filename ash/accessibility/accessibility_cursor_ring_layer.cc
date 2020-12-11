2718525b660b4af8872c64f82f53ddcdac8324ad
Joel Riley <joelriley@google.com>
2020-12-11 19:47:59 +0000

    Re-land: Move focus rings and highlight layers under accessibility bubbles.
    Original patch: http://crrev.com/c/2568547
    Revert: http://crrev.com/c/2583106
    
    Fix for issue where focus ring appears below switch access panel. Now clients can specify if they want their focus ring to appear above or below accessibility bubble panels. STS will specify they appear below, though other clients will (by default) specify they appear above all panels. This fixes an issue with the switch access focus ring didn't appear above the switch access panel.
    
    Original description:
    
    Move focus rings and highlight layers under accessibility bubbles.
    
    Will allow Select-to-speak panel UI to appear above focus ring and highlight layers. This also avoids the need to create more window containers.
    
    Bug: 1157261, 1143814
    Change-Id: Ibd8dd577ecd4fb5767b628c2eff679a2f6931b9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2582997
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Joel Riley <joelriley@google.com>
    Cr-Commit-Position: refs/heads/master@{#836222}

--- ash/accessibility/accessibility_cursor_ring_layer.cc
+++ ash/accessibility/accessibility_cursor_ring_layer.cc
@@ -51,7 +51,8 @@ void AccessibilityCursorRingLayer::Set(const gfx::Point& location) {
       display::Screen::GetScreen()->GetDisplayMatching(bounds);
   aura::Window* root_window = Shell::GetRootWindowForDisplayId(display.id());
   ::wm::ConvertRectFromScreen(root_window, &bounds);
-  CreateOrUpdateLayer(root_window, "AccessibilityCursorRing", bounds);
+  CreateOrUpdateLayer(root_window, "AccessibilityCursorRing", bounds,
+                      /*stack_at_top=*/true);
 }
 
 void AccessibilityCursorRingLayer::OnPaintLayer(
