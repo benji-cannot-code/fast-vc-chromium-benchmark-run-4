2efeafc31b4ffb4af9036f73cb3f0536370ad07a
Dylan Cutler <dylancutler@google.com>
2020-12-11 14:31:39 +0000

    Get //chrome/test:browser_tests to build if CanonicalCookie's constructor is private.
    This is part of a larger effort to make the generic constructor of
    CanonicalCookie private.
    
    CreateUnsafeCookieForTesting is a factory method that creates a
    test-only cookie which is not guaranteed to be canonical and should not
    be used in production.
    
    Bug: 1102874
    Change-Id: I70bd75b14fa7ca5559a760905b459823b4f0d947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2584644
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/master@{#836100}

--- chrome/browser/browsing_data/browsing_data_remover_browsertest.cc
+++ chrome/browser/browsing_data/browsing_data_remover_browsertest.cc
@@ -275,7 +275,7 @@ std::string GetCookiesTreeModelInfo(const CookieTreeNode* root) {
 // Sets the APISID Gaia cookie, which is monitored by the AccountReconcilor.
 bool SetGaiaCookieForProfile(Profile* profile) {
   GURL google_url = GaiaUrls::GetInstance()->secure_google_url();
-  net::CanonicalCookie cookie(
+  auto cookie = net::CanonicalCookie::CreateUnsafeCookieForTesting(
       "SAPISID", std::string(), "." + google_url.host(), "/", base::Time(),
       base::Time(), base::Time(), true /* secure */, false /* httponly */,
       net::CookieSameSite::NO_RESTRICTION, net::COOKIE_PRIORITY_DEFAULT,
@@ -290,7 +290,7 @@ bool SetGaiaCookieForProfile(Profile* profile) {
   network::mojom::CookieManager* cookie_manager =
       content::BrowserContext::GetDefaultStoragePartition(profile)
           ->GetCookieManagerForBrowserProcess();
-  cookie_manager->SetCanonicalCookie(cookie, google_url,
+  cookie_manager->SetCanonicalCookie(*cookie, google_url,
                                      net::CookieOptions::MakeAllInclusive(),
                                      std::move(callback));
   loop.Run();
--- chrome/browser/sync/test/integration/cookie_helper.cc
+++ chrome/browser/sync/test/integration/cookie_helper.cc
@@ -27,7 +27,7 @@ const char kSigninCookieName[] = "SAPISID";
 
 void AddSigninCookie(Profile* profile) {
   DCHECK(profile);
-  net::CanonicalCookie cookie(
+  auto cookie = net::CanonicalCookie::CreateUnsafeCookieForTesting(
       kSigninCookieName, std::string(), ".google.com", "/",
       /*creation=*/base::Time(),
       /*expires=*/base::Time(), /*last_access=*/base::Time(), /*secure=*/true,
@@ -42,7 +42,7 @@ void AddSigninCookie(Profile* profile) {
 
   base::RunLoop run_loop;
   cookie_manager->SetCanonicalCookie(
-      cookie, net::cookie_util::SimulatedCookieSource(cookie, "https"),
+      *cookie, net::cookie_util::SimulatedCookieSource(*cookie, "https"),
       net::CookieOptions(),
       base::BindLambdaForTesting(
           [&run_loop](net::CookieAccessResult) { run_loop.Quit(); }));
