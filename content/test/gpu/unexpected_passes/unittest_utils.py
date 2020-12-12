fa477ce604f17e60901cc16ab90dbed75fe2d44e
Brian Sheedy <bsheedy@chromium.org>
2020-12-12 01:21:20 +0000

    Add stale removal to unexpected pass finder
    Moves the splitting of expectations based on staleness out of the
    output code and uses it to automatically remove stale expectations from
    the expectation file if specified by the user.
    
    Bug: 998329
    Change-Id: I317ded44447203b8fddc2c6277ed3f5f5b66a0f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585846
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836358}

---
+++ content/test/gpu/unexpected_passes/unittest_utils.py
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Helper methods for unittests."""
+
+from unexpected_passes import data_types
+
+
+def CreateStatsWithPassFails(passes, fails):
+  stats = data_types.BuildStats()
+  for _ in xrange(passes):
+    stats.AddPassedBuild()
+  for i in xrange(fails):
+    stats.AddFailedBuild('build_id%d' % i)
+  return stats
