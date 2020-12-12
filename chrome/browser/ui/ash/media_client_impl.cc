79c048b0fc34afa3db187c0a5e3e0b33e26bb0e2
Jason Lin <lxj@google.com>
2020-12-12 23:37:55 +0000

    Delay MediaClientImpl's subscription to VmCameraMicManager
    ... until the primary profile is ready since VmCameraMicManager is not
    ready until then.
    
    Bug: b/167491603
    Change-Id: Ib2388a95a26ec6c128f2fca0f9c4e5561802bb73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2586286
    Commit-Queue: Jason Lin <lxj@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836457}

--- chrome/browser/ui/ash/media_client_impl.cc
+++ chrome/browser/ui/ash/media_client_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/location.h"
 #include "base/single_thread_task_runner.h"
+#include "base/system/sys_info.h"
 #include "base/task/current_thread.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/chromeos/camera_mic/vm_camera_mic_manager.h"
@@ -141,11 +142,6 @@ MediaClientImpl::MediaClientImpl() {
   MediaCaptureDevicesDispatcher::GetInstance()->AddObserver(this);
   BrowserList::AddObserver(this);
 
-  auto* vm_camera_mic_manager = chromeos::VmCameraMicManager::GetForProfile(
-      ProfileManager::GetPrimaryUserProfile());
-  if (vm_camera_mic_manager)
-    vm_camera_mic_manager->AddObserver(this);
-
   DCHECK(!g_media_client);
   g_media_client = this;
 }
@@ -182,6 +178,18 @@ void MediaClientImpl::InitForTesting(ash::MediaController* controller) {
 
   media_controller_ = controller;
   media_controller_->SetClient(this);
+}
+
+void MediaClientImpl::OnPrimaryUserSessionStarted(Profile* primary_profile) {
+  // Skip when not on real device (i.e. tests and emulator on dev box). The
+  // reason is that some tests cause this function to be run more than once,
+  // which results in a crash.
+  if (base::SysInfo::IsRunningOnChromeOS()) {
+    auto* vm_camera_mic_manager =
+        chromeos::VmCameraMicManager::GetForProfile(primary_profile);
+    if (vm_camera_mic_manager)
+      vm_camera_mic_manager->AddObserver(this);
+  }
 }
 
 void MediaClientImpl::HandleMediaNextTrack() {
