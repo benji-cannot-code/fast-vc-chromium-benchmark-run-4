953e75ab1c4f76d58e7d53b4bdae520df2428705
Yi Gu <yigu@chromium.org>
2020-12-12 00:06:55 +0000

    [CodeHealth] Convert chrome/browser/ui/views/frame from base::Bind and base::Callback to Once/Repeating
    Bug: 1152282
    Change-Id: I77fce1eabfc0652828547d9b59c2517e74fc629f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585815
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836318}

--- chrome/browser/ui/views/frame/test_with_browser_view.cc
+++ chrome/browser/ui/views/frame/test_with_browser_view.cc
@@ -52,7 +52,7 @@ std::unique_ptr<KeyedService> CreateTemplateURLService(
       std::make_unique<ChromeTemplateURLServiceClient>(
           HistoryServiceFactory::GetForProfile(
               profile, ServiceAccessType::EXPLICIT_ACCESS)),
-      base::Closure());
+      base::RepeatingClosure());
 }
 
 std::unique_ptr<KeyedService> CreateAutocompleteClassifier(
