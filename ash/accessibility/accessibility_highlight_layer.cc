b6a90e1757a72ef99dca0d047d551992f2cde1bf
Joel Riley <joelriley@google.com>
2020-12-14 20:42:02 +0000

    Reland "Re-land: Move focus rings and highlight layers under accessibility bubbles."
    Original patch: http://crrev.com/c/2582997
    Revert: http://crrev.com/c/2587604
    
    Previous re-land caused MSAN failures due to uninitialized variable. This re-land has a one-line fix to initialize the value of stacking_order_ in accessibility_focus_ring_layer.h. Otherwise this patch is identical.
    
    Tested with 'linux_chromium_chromeos_msan_rel_ng' try job. With fix, no longer get uninitialized variable failures.
    
    Original change's description:
    > Re-land: Move focus rings and highlight layers under accessibility bubbles.
    >
    > Original patch: http://crrev.com/c/2568547
    > Revert: http://crrev.com/c/2583106
    >
    > Fix for issue where focus ring appears below switch access panel. Now clients can specify if they want their focus ring to appear above or below accessibility bubble panels. STS will specify they appear below, though other clients will (by default) specify they appear above all panels. This fixes an issue with the switch access focus ring didn't appear above the switch access panel.
    >
    > Original description:
    >
    > Move focus rings and highlight layers under accessibility bubbles.
    >
    > Will allow Select-to-speak panel UI to appear above focus ring and highlight layers. This also avoids the need to create more window containers.
    >
    > Bug: 1157261, 1143814
    > Change-Id: Ibd8dd577ecd4fb5767b628c2eff679a2f6931b9c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2582997
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Joel Riley <joelriley@google.com>
    > Cr-Commit-Position: refs/heads/master@{#836222}
    
    Bug: 1157261
    Bug: 1143814
    Change-Id: I445296009d67c9b71d60f5e6b5f17e48b188be71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2588309
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Joel Riley <joelriley@google.com>
    Cr-Commit-Position: refs/heads/master@{#836752}

--- ash/accessibility/accessibility_highlight_layer.cc
+++ ash/accessibility/accessibility_highlight_layer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/accessibility/accessibility_highlight_layer.h"
 
+#include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkPath.h"
@@ -50,8 +51,11 @@ void AccessibilityHighlightLayer::Set(const std::vector<gfx::Rect>& rects,
   display::Display display =
       display::Screen::GetScreen()->GetDisplayMatching(bounds);
   aura::Window* root_window = Shell::GetRootWindowForDisplayId(display.id());
-  ::wm::ConvertRectFromScreen(root_window, &bounds);
-  CreateOrUpdateLayer(root_window, "AccessibilityHighlight", bounds);
+  aura::Window* container = Shell::GetContainer(
+      root_window, kShellWindowId_AccessibilityPanelContainer);
+  ::wm::ConvertRectFromScreen(container, &bounds);
+  CreateOrUpdateLayer(container, "AccessibilityHighlight", bounds,
+                      /*stack_at_top=*/false);
 }
 
 bool AccessibilityHighlightLayer::CanAnimate() const {
