7967a599d499094de3dff273fa614d7f4885b4a8
Victor Costan <pwnall@chromium.org>
2020-12-14 19:18:17 +0000

    IndexedDB: Modernize task posting in IndexedDBQuotaClient.
    * Replace base::PostTask*() calls with calling PostTask*() methods
      directly on TaskRunners.
    
    * Use PostTaskAndReplyWithResult() instead of equivalent implementation
      based on PostTaskAndReply().
    
    * Add comment clarifying that IndexedDBContextImpl::IDBTaskRunner() is a
      thread-safe getter.
    
    * Fix incorrect mapping of failure (success = false) to
      blink::mojom::QuotaStatusCode::kOk in DidDeletedIDBData().
    
    The last improvement may result in elevated error rates while attempting
    to delete IndexedDB data. This is because this CL correctly propagates
    reported errors in a situation where they were ignored before.
    
    Bug: 1016065
    Change-Id: Ibde1154fb1c42dc412a40c0ccbcd3cb6b8e3f627
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2588448
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836701}

--- content/browser/indexed_db/indexed_db_context_impl.h
+++ content/browser/indexed_db/indexed_db_context_impl.h
@@ -147,6 +147,7 @@ class CONTENT_EXPORT IndexedDBContextImpl
 
   int64_t GetOriginDiskUsage(const url::Origin& origin);
 
+  // This getter is thread-safe.
   base::SequencedTaskRunner* IDBTaskRunner();
 
   // Methods called by IndexedDBFactoryImpl or IndexedDBDispatcherHost for
