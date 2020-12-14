ba46bf2c82b684597b07cc210be07973e59afe4d
Jeffrey Kardatzke <jkardatzke@google.com>
2020-12-14 20:07:57 +0000

    Relocate encrypted HEVC mime checking code
    BUG=b:153111783
    TEST=Encrypted HEVC playback works w/ clear testing flag enabled
    
    Change-Id: I08372658b04c2cadf2b6d032e619406ea05516e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587521
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Auto-Submit: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/master@{#836735}

--- media/blink/key_system_config_selector.cc
+++ media/blink/key_system_config_selector.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/logging_override_if_enabled.h"
 #include "media/base/media_permission.h"
 #include "media/base/mime_util.h"
+#include "media/media_buildflags.h"
 #include "third_party/blink/public/platform/url_conversion.h"
 #include "third_party/blink/public/platform/web_media_key_system_configuration.h"
 #include "third_party/blink/public/platform/web_string.h"
@@ -160,6 +161,32 @@ bool IsSupportedMediaType(const std::string& container_mime_type,
 
   std::vector<std::string> codec_vector;
   SplitCodecs(codecs, &codec_vector);
+
+#if BUILDFLAG(ENABLE_PLATFORM_HEVC) && BUILDFLAG(USE_CHROMEOS_PROTECTED_MEDIA)
+  // EME HEVC is supported on CrOS under these build flags, but it is not
+  // supported for clear playback. Remove the HEVC codec strings to avoid asking
+  // IsSupported*MediaFormat() about HEVC. EME support for HEVC profiles
+  // is described via KeySystemProperties::GetSupportedCodecs().
+  // TODO(1156282): Decouple the rest of clear vs EME codec support.
+  if (base::ToLowerASCII(container_mime_type) == "video/mp4" &&
+      !codec_vector.empty()) {
+    auto it = codec_vector.begin();
+    while (it != codec_vector.end()) {
+      VideoCodecProfile profile;
+      uint8_t level_idc;
+      if (ParseHEVCCodecId(*it, &profile, &level_idc))
+        codec_vector.erase(it);
+      else
+        ++it;
+    }
+
+    // Avoid calling IsSupported*MediaFormat() with an empty vector. For
+    // "video/mp4", this will return MaybeSupported, which we would otherwise
+    // consider "false" below.
+    if (codec_vector.empty())
+      return true;
+  }
+#endif
 
   // AesDecryptor decrypts the stream in the demuxer before it reaches the
   // decoder so check whether the media format is supported when clear.
