39470782b0fa43555547248a5b2187d705ed2656
Caroline Rising <corising@chromium.org>
2020-12-14 20:31:10 +0000

    Read later: make the read later button a LabelButton.
    As a label button, the read later button will match the style of the
    other bookmark bar buttons. This change also fixes bugs with the button
    label/icon not updating colors corrently with themes.
    
    Bug: 1109316
    Change-Id: Iebe176f60a87d26abbfdf1c8e01bc4c36f135445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2586242
    Reviewed-by: Connie Wan <connily@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836746}

--- chrome/browser/ui/views/read_later/read_later_button.h
+++ chrome/browser/ui/views/read_later/read_later_button.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_READ_LATER_READ_LATER_BUTTON_H_
 
 #include "chrome/browser/ui/views/bubble/webui_bubble_manager.h"
-#include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "chrome/browser/ui/webui/read_later/read_later_ui.h"
+#include "ui/views/controls/button/label_button.h"
 
 class Browser;
 class WebUIBubbleDialogView;
@@ -16,19 +16,22 @@ class WebUIBubbleDialogView;
 // Button in the bookmarks bar that provides access to the corresponding
 // read later menu.
 // TODO(corising): Handle the the async presentation of the UI bubble.
-class ReadLaterButton : public ToolbarButton {
+class ReadLaterButton : public views::LabelButton {
  public:
   explicit ReadLaterButton(Browser* browser);
   ReadLaterButton(const ReadLaterButton&) = delete;
   ReadLaterButton& operator=(const ReadLaterButton&) = delete;
   ~ReadLaterButton() override;
 
-  // ToolbarButton:
   const char* GetClassName() const override;
-  void UpdateIcon() override;
 
  private:
-  int GetIconSize() const;
+  // LabelButton:
+  std::unique_ptr<views::InkDrop> CreateInkDrop() override;
+  std::unique_ptr<views::InkDropHighlight> CreateInkDropHighlight()
+      const override;
+  SkColor GetInkDropBaseColor() const override;
+  void OnThemeChanged() override;
 
   void ButtonPressed();
 
