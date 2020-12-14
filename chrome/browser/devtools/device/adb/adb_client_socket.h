90f955ad148311af5a548ef47518bcc088fdd819
Leonard Grey <lgrey@chromium.org>
2020-12-14 19:40:06 +0000

    [Code health] Convert callbacks in AdbClientSocket to Once/Repeating
    Bug: 1152274
    Change-Id: I81358a8df4b853654ba031bce0a251ed8445a8ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2586156
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836715}

--- chrome/browser/devtools/device/adb/adb_client_socket.h
+++ chrome/browser/devtools/device/adb/adb_client_socket.h
@@ -15,19 +15,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class AdbClientSocket {
  public:
-  typedef base::Callback<void(int, const std::string&)> CommandCallback;
-  typedef base::Callback<void(int result, std::unique_ptr<net::StreamSocket>)>
+  typedef base::OnceCallback<void(int, const std::string&)> CommandCallback;
+  typedef base::OnceCallback<void(int result,
+                                  std::unique_ptr<net::StreamSocket>)>
       SocketCallback;
 
   static void AdbQuery(int port,
                        const std::string& query,
-                       const CommandCallback& callback);
-
+                       CommandCallback callback);
 
   static void TransportQuery(int port,
                              const std::string& serial,
                              const std::string& socket_name,
-                             const SocketCallback& callback);
+                             SocketCallback callback);
 
  protected:
   explicit AdbClientSocket(int port);
@@ -37,19 +37,19 @@ class AdbClientSocket {
 
   void SendCommand(const std::string& command,
                    bool is_void,
-                   const CommandCallback& callback);
+                   CommandCallback callback);
 
   std::unique_ptr<net::StreamSocket> socket_;
 
  private:
-  void ReadResponse(const CommandCallback& callback, bool is_void, int result);
+  void ReadResponse(CommandCallback callback, bool is_void, int result);
 
-  void OnResponseHeader(const CommandCallback& callback,
+  void OnResponseHeader(CommandCallback callback,
                         bool is_void,
                         scoped_refptr<net::IOBuffer> response_buffer,
                         int result);
 
-  void OnResponseData(const CommandCallback& callback,
+  void OnResponseData(CommandCallback callback,
                       const std::string& response,
                       scoped_refptr<net::IOBuffer> response_buffer,
                       int bytes_left,
--- chrome/browser/devtools/device/tcp_device_provider.cc
+++ chrome/browser/devtools/device/tcp_device_provider.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/devtools/device/adb/adb_client_socket.h"
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -45,7 +44,7 @@ static void RunSocketCallback(
 class ResolveHostAndOpenSocket final : public network::ResolveHostClientBase {
  public:
   ResolveHostAndOpenSocket(const net::HostPortPair& address,
-                           const AdbClientSocket::SocketCallback& callback)
+                           const AndroidDeviceManager::SocketCallback& callback)
       : callback_(callback) {
     mojo::Remote<network::mojom::HostResolver> resolver;
     content::GetUIThreadTaskRunner({})->PostTask(
@@ -93,7 +92,7 @@ class ResolveHostAndOpenSocket final : public network::ResolveHostClientBase {
     delete this;
   }
 
-  AdbClientSocket::SocketCallback callback_;
+  AndroidDeviceManager::SocketCallback callback_;
   mojo::Receiver<network::mojom::ResolveHostClient> receiver_{this};
 };
 
