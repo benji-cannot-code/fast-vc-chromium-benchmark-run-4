e6b62bfd917e8b0fc651c3f83f7f79716ffdc618
Ravjit Singh Uppal <ravjit@chromium.org>
2020-12-14 20:01:30 +0000

    Updated site settings and content settings bubble for one time permissions
    As part of one time permissions for Geolocation on desktop, updated the Content Settings Bubble and Site Settings page to reflect the change.
    
    Bug: 1150347
    Change-Id: I6d13c59315a60fdf64a4ef54d52a2b7882f752a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2544889
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Ravjit Singh Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836728}

--- chrome/browser/ui/content_settings/content_setting_bubble_model.cc
+++ chrome/browser/ui/content_settings/content_setting_bubble_model.cc
@@ -90,6 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::UserMetricsAction;
 using content::WebContents;
 using content_settings::PageSpecificContentSettings;
+using content_settings::SessionModel;
 using content_settings::SETTING_SOURCE_NONE;
 using content_settings::SETTING_SOURCE_USER;
 using content_settings::SettingInfo;
@@ -1229,6 +1230,7 @@ ContentSettingGeolocationBubbleModel::ContentSettingGeolocationBubbleModel(
     : ContentSettingSingleRadioGroup(delegate,
                                      web_contents,
                                      ContentSettingsType::GEOLOCATION) {
+  SetCustomLink();
 #if defined(OS_MAC)
   if (base::FeatureList::IsEnabled(
           ::features::kMacCoreLocationImplementation)) {
@@ -1274,7 +1276,6 @@ void ContentSettingGeolocationBubbleModel::OnManageButtonClicked() {
 void ContentSettingGeolocationBubbleModel::CommitChanges() {
   if (show_system_geolocation_bubble_)
     return;
-
   ContentSettingSingleRadioGroup::CommitChanges();
 }
 
@@ -1292,6 +1293,17 @@ void ContentSettingGeolocationBubbleModel::
   set_radio_group(RadioGroup());
   show_system_geolocation_bubble_ = true;
 #endif  // defined(OS_MAC)
+}
+
+void ContentSettingGeolocationBubbleModel::SetCustomLink() {
+  auto* map = HostContentSettingsMapFactory::GetForProfile(
+      web_contents()->GetBrowserContext());
+  SettingInfo info;
+  const GURL url =
+      web_contents()->GetMainFrame()->GetLastCommittedOrigin().GetURL();
+  map->GetWebsiteSetting(url, url, ContentSettingsType::GEOLOCATION, &info);
+  if (info.session_model == SessionModel::OneTime)
+    set_custom_link(l10n_util::GetStringUTF16(IDS_GEOLOCATION_WILL_ASK_AGAIN));
 }
 
 // ContentSettingSubresourceFilterBubbleModel ----------------------------------
