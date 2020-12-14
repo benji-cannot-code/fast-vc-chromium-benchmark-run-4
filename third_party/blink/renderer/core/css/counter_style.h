c8135d2019574d0dd88c1e1155c2c7361c2eda78
Xiaocheng Hu <xiaochengh@chromium.org>
2020-12-14 23:41:30 +0000

    Implement custom 'pad' values for @counter-style
    Bug: 687225
    Change-Id: Id90bf61c99aabcd464d12f9f43e95ec9bf5d5eb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585676
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836847}

--- third_party/blink/renderer/core/css/counter_style.h
+++ third_party/blink/renderer/core/css/counter_style.h
@@ -74,6 +74,10 @@ class CORE_EXPORT CounterStyle final : public GarbageCollected<CounterStyle> {
   // 'system', 'range' and 'symbols'/'additive-symbols' descriptor values.
   String GenerateInitialRepresentation(int value) const;
 
+  // Uses the fallback counter style to generate a representation for the value.
+  // It may recurse, and if it enters a loop, it uses 'decimal' instead.
+  String GenerateFallbackRepresentation(int value) const;
+
   // The corresponding style rule in CSS.
   Member<const StyleRuleCounterStyle> style_rule_;
 
@@ -100,6 +104,9 @@ class CORE_EXPORT CounterStyle final : public GarbageCollected<CounterStyle> {
 
   String negative_prefix_ = "-";
   String negative_suffix_;
+
+  String pad_symbol_;
+  wtf_size_t pad_length_ = 0;
 
   friend class CounterStyleMapTest;
 };
