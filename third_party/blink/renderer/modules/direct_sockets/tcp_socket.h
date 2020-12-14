9386b12701606fb2b504c9ad56bc68ec36ce90e2
Song Fangzhen <songfangzhen@bytedance.com>
2020-12-14 23:30:37 +0000

    Direct Sockets: Add ReadableStream and WritableStream for TCPSocket.
    Add TCPReadableStreamWrapper and TCPWritableStreamWrapper which
    provide stream implementations for TCPSocket.
    
    TCPReadableWrapper and TCPWritableWrapper watch the mojo datapipe,
    create underlying queue strategy, and provide functions like
    read/write data from/to network.
    
    Unittests are also provided.
    
    Below documents are from Eric Willigers <ericwilligers@chromium.org>.
    Explainer: https://github.com/WICG/raw-sockets/blob/master/docs/explainer.md
    
    Intent to Prototype:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/ARtkaw4e9T4/m/npjeMssPCAAJ
    
    Design doc:
    https://docs.google.com/document/d/1Xa5nFkIWxkL3hZHvDYWPhT8sZvNeFpCUKNuqIwZHxnE/edit?usp=sharing
    
    Bug: 905818
    Change-Id: I8a0da5d9391c543201e5e9620ca493d192166319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563340
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Ke He <kehe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836841}

--- third_party/blink/renderer/modules/direct_sockets/tcp_socket.h
+++ third_party/blink/renderer/modules/direct_sockets/tcp_socket.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/system/data_pipe.h"
 #include "services/network/public/mojom/tcp_socket.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/modules/direct_sockets/tcp_readable_stream_wrapper.h"
+#include "third_party/blink/renderer/modules/direct_sockets/tcp_writable_stream_wrapper.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
@@ -52,6 +54,11 @@ class MODULES_EXPORT TCPSocket final
   // Web-exposed function
   ScriptPromise close(ScriptState*, ExceptionState&);
 
+  ReadableStream* readable() const;
+  WritableStream* writable() const;
+  String remoteAddress() const;
+  uint16_t remotePort() const;
+
   // network::mojom::blink::SocketObserver:
   void OnReadError(int32_t net_error) override;
   void OnWriteError(int32_t net_error) override;
@@ -62,6 +69,9 @@ class MODULES_EXPORT TCPSocket final
  private:
   void OnSocketObserverConnectionError();
 
+  void OnReadableStreamAbort();
+  void OnWritableStreamAbort();
+
   Member<ScriptPromiseResolver> resolver_;
   FrameOrWorkerScheduler::SchedulingAffectingFeatureHandle
       feature_handle_for_scheduler_;
@@ -69,6 +79,11 @@ class MODULES_EXPORT TCPSocket final
   mojo::Remote<network::mojom::blink::TCPConnectedSocket> tcp_socket_;
   mojo::Receiver<network::mojom::blink::SocketObserver>
       socket_observer_receiver_{this};
+
+  Member<TCPReadableStreamWrapper> tcp_readable_stream_wrapper_;
+  Member<TCPWritableStreamWrapper> tcp_writable_stream_wrapper_;
+  base::Optional<net::IPEndPoint> local_addr_;
+  base::Optional<net::IPEndPoint> peer_addr_;
 };
 
 }  // namespace blink
