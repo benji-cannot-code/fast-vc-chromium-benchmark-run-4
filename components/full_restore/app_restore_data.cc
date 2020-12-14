f32a278421881aefe38eee388dd9b29819da4541
Nancy Wang <nancylingwang@chromium.org>
2020-12-14 04:20:55 +0000

    Add the RestoreData class and the AppRestoreData struct.
    The RestoreData class is the full restore data in memory to be written
    to the FullRestoreData file.
    
    There will be some separate CLs to convert RestoreData to JSON format,
    and write the full restore data to a file.
    
    BUG=1146900
    
    Change-Id: I7b51b0ba6e1cbe4f7a4b763a52e81f273a74c8d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2581702
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836523}

---
+++ components/full_restore/app_restore_data.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/full_restore/app_restore_data.h"
+
+namespace full_restore {
+
+AppRestoreData::AppRestoreData() = default;
+AppRestoreData::~AppRestoreData() = default;
+
+}  // namespace full_restore
