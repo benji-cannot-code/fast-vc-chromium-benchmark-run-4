9a264d75eea93ad38a491d6dab39b4cae6dc45ab
Alice Wang <aliceywang@chromium.org>
2020-12-14 09:02:06 +0000

    [Signin][Modularization] Move confirm sync dialog tests to signin.ui module
    This CL moves two confirm sync dialog tests to the junit target of
    c.b.signin.ui module.
    
    Bug: 1156585, 1156588
    Change-Id: I3f54d90707a20ab22671d3f20e8ef1146c784cd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2584977
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836556}

--- chrome/browser/signin/ui/android/junit/src/org/chromium/chrome/browser/signin/ui/ConfirmManagedSyncDataDialogTest.java
+++ chrome/browser/signin/ui/android/junit/src/org/chromium/chrome/browser/signin/ui/ConfirmManagedSyncDataDialogTest.java
@@ -2,7 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-package org.chromium.chrome.browser.signin;
+
+package org.chromium.chrome.browser.signin.ui;
 
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -23,14 +24,9 @@ import org.robolectric.Robolectric;
 import org.robolectric.shadows.ShadowAlertDialog;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
-import org.chromium.chrome.R;
-import org.chromium.chrome.browser.signin.ui.ConfirmManagedSyncDataDialog;
-import org.chromium.chrome.browser.signin.ui.ConfirmSyncDataStateMachineDelegate;
 
 /**
  * Tests for {@link ConfirmManagedSyncDataDialog}.
- * TODO(https://crbug.com/1156588): Modularize this class after moving the
- * {@link ConfirmSyncDataStateMachineDelegate}.
  */
 @RunWith(BaseRobolectricTestRunner.class)
 public class ConfirmManagedSyncDataDialogTest {
