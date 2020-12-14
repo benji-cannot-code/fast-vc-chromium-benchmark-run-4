bb2ade1be7c137ca5fd96337aa4c05ca8db7ee61
Luke Zielinski <lpz@chromium.org>
2020-12-14 17:39:47 +0000

    Roll wpt testharness
    This rolls up to SHA fa8fd1e513f1e8151b79246a7e0ef3cf3d90a127
    
    Change-Id: Idfbd212eb1280686541100f6b4941249c32e2a2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585684
    Auto-Submit: Luke Z <lpz@chromium.org>
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836647}

--- third_party/blink/web_tests/resources/testdriver.js
+++ third_party/blink/web_tests/resources/testdriver.js
@@ -148,6 +148,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         },
 
         /**
+         * Deletes all cookies.
+         *
+         * This matches the behaviour of the {@link
+         * https://w3c.github.io/webdriver/#delete-all-cookies|WebDriver
+         * Delete All Cookies command}.
+         *
+         * @param {WindowProxy} context - Browsing context in which
+         *                                to run the call, or null for the current
+         *                                browsing context.
+         *
+         * @returns {Promise} fulfilled after cookies are deleted, or rejected in
+         *                    the cases the WebDriver command errors
+         */
+        delete_all_cookies: function(context=null) {
+            return window.test_driver_internal.delete_all_cookies(context);
+        },
+
+        /**
          * Send keys to an element
          *
          * This matches the behaviour of the {@link
@@ -457,6 +475,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             return new Promise(function(resolve, reject) {
                 element.addEventListener("click", resolve);
             });
+        },
+
+        delete_all_cookies: function(context=null) {
+            return Promise.reject(new Error("unimplemented"));
         },
 
         send_keys: function(element, keys) {
