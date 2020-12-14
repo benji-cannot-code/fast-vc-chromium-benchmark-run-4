3f9fd503c02494955c5f0cacb5d942b652b0c20f
Alice Wang <aliceywang@chromium.org>
2020-12-14 08:50:04 +0000

    [Signin][Modularization] Move ConfirmSyncDataStateMachine to signin.ui target
    This CL moves ConfirmSyncDataStateMachine to signin.ui target.
    
    Bug: 1156609
    Change-Id: I8cf36f4597c6566fd2f81a1fc896255d90d0277c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2584881
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836555}

--- chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/ConfirmSyncDataStateMachine.java
+++ chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/ConfirmSyncDataStateMachine.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.signin;
+package org.chromium.chrome.browser.signin.ui;
 
 import android.os.Handler;
 import android.text.TextUtils;
@@ -14,9 +14,6 @@ import androidx.annotation.Nullable;
 import org.chromium.base.ThreadUtils;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
-import org.chromium.chrome.browser.signin.ui.ConfirmImportSyncDataDialog;
-import org.chromium.chrome.browser.signin.ui.ConfirmManagedSyncDataDialog;
-import org.chromium.chrome.browser.signin.ui.ConfirmSyncDataStateMachineDelegate;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -64,8 +61,9 @@ public class ConfirmSyncDataStateMachine
 
     /**
      * Callback for completion of the {@link ConfirmSyncDataStateMachine}.
+     * TODO(https://crbug.com/1155123): Change this method to package internal after modularization
      */
-    interface Listener {
+    public interface Listener {
         /**
          * The state machine has completed and the state is done, or all necessary
          * confirmations were given and the sign-in flow can proceed.
