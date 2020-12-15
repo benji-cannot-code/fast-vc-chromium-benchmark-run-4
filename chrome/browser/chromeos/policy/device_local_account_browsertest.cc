6ce4cc8fe515ce3c436b18150cbf7dd21737451a
Ossama Mahmoud <osamafathy@google.com>
2020-12-15 16:40:52 +0000

    OOBE: Migrate ToS screen to MultiStepBehavior
    Bug: 1156668
    Change-Id: I5ab27202a535c829304291ac53dc9545eeb292da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2588909
    Commit-Queue: Ossama Mahmoud <osamafathy@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837120}

--- chrome/browser/chromeos/policy/device_local_account_browsertest.cc
+++ chrome/browser/chromeos/policy/device_local_account_browsertest.cc
@@ -2083,9 +2083,7 @@ IN_PROC_BROWSER_TEST_F(DeviceLocalAccountTest, TermsOfServiceWithLocaleSwitch) {
 
   // Wait for the Terms of Service to finish downloading.
   chromeos::test::OobeJS()
-      .CreateWaiter(GetOobeElementPath({"terms-of-service"}) + ".uiState == " +
-                    base::NumberToString(static_cast<int>(
-                        chromeos::TermsOfServiceScreen::ScreenState::LOADED)))
+      .CreateWaiter(GetOobeElementPath({"terms-of-service"}) + ".isLoaded_()")
       ->Wait();
 
   // Verify that the locale and keyboard layout have been applied.
@@ -2670,9 +2668,7 @@ IN_PROC_BROWSER_TEST_P(TermsOfServiceDownloadTest, TermsOfServiceScreen) {
         ->Wait();
 
     chromeos::test::OobeJS().ExpectTrue(
-        GetOobeElementPath({"terms-of-service"}) + ".uiState == " +
-        base::NumberToString(static_cast<int>(
-            chromeos::TermsOfServiceScreen::ScreenState::ERROR)));
+        GetOobeElementPath({"terms-of-service"}) + ".hasError_()");
 
     chromeos::test::OobeJS().ExpectDisabledPath(
         {"terms-of-service", "acceptButton"});
@@ -2680,9 +2676,7 @@ IN_PROC_BROWSER_TEST_P(TermsOfServiceDownloadTest, TermsOfServiceScreen) {
   }
 
   chromeos::test::OobeJS()
-      .CreateWaiter(GetOobeElementPath({"terms-of-service"}) + ".uiState == " +
-                    base::NumberToString(static_cast<int>(
-                        chromeos::TermsOfServiceScreen::ScreenState::LOADED)))
+      .CreateWaiter(GetOobeElementPath({"terms-of-service"}) + ".isLoaded_()")
       ->Wait();
 
   chromeos::test::OobeJS()
@@ -2721,9 +2715,7 @@ IN_PROC_BROWSER_TEST_P(TermsOfServiceDownloadTest, TermsOfServiceScreen) {
   EXPECT_EQ(terms_of_service, content);
 
   chromeos::test::OobeJS().ExpectFalse(
-      GetOobeElementPath({"terms-of-service"}) + ".uiState == " +
-      base::NumberToString(static_cast<int>(
-          chromeos::TermsOfServiceScreen::ScreenState::ERROR)));
+      GetOobeElementPath({"terms-of-service"}) + ".hasError_()");
 
   chromeos::test::OobeJS().ExpectEnabledPath(
       {"terms-of-service", "acceptButton"});
