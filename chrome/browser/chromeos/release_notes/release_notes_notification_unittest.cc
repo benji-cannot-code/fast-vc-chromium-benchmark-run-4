cf201aa26bf75b11d6504f87a856530cd0fb94ba
Callistus <callistus@google.com>
2020-12-15 23:43:34 +0000

    Add new strings for Release notes notification title and message, and suggestion chip for Help app.
    Bug: b/169897551, b/175501984
    Change-Id: I841c438a40c5399df062ce95dfcf375ee1fd587f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2592227
    Reviewed-by: Rachel Carpenter <carpenterr@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Cr-Commit-Position: refs/heads/master@{#837326}

--- chrome/browser/chromeos/release_notes/release_notes_notification_unittest.cc
+++ chrome/browser/chromeos/release_notes/release_notes_notification_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/release_notes/release_notes_notification.h"
 
 #include "base/feature_list.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/version.h"
 #include "chrome/browser/notifications/notification_display_service_tester.h"
@@ -97,6 +98,8 @@ TEST_F(ReleaseNotesNotificationTest, ShowReleaseNotesNotification) {
   EXPECT_EQ(ui::SubstituteChromeOSDeviceType(
                 IDS_RELEASE_NOTES_DEVICE_SPECIFIC_NOTIFICATION_TITLE),
             GetReleaseNotesNotification().title());
+  EXPECT_EQ("Work better across devices, do things faster, and more",
+            base::UTF16ToASCII(GetReleaseNotesNotification().message()));
   EXPECT_EQ(1, notification_count_);
 }
 
