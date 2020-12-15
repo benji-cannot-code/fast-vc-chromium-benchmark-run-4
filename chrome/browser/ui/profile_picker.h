16513406d8d90fcc6092c7430f1825d2ecc51943
Jan Krcal <jkrcal@chromium.org>
2020-12-15 19:01:59 +0000

    Reland "[Profile picker] Use system profile on startup when showing the picker"
    This is a reland of fa8de98648a97c7739caaba2ab1a3a1f3b818c4d
    
    Original change's description:
    > [Profile picker] Use system profile on startup when showing the picker
    >
    > This CL changes the logic to load the startup profile whenever the
    > profile picker should be loaded. It instead loads the guest profile
    > to indicate the picker should be open. This speeds up showing the picker
    > when the last profile used is heavy to load. The CL also unifies the
    > logic to decide whether to show the new picker (or the old user manager)
    > into one function, GetStartupProfile().
    >
    > There is one behavioral difference: the new code directly opens browser
    > windows if the user had multiple profiles open last time. This is desired
    > because having multiple profiles open simultaneously the last time is a
    > strong signal the user wants to continue this way. Note that having
    > multiple profiles open the last time happens only in limited situations,
    > e.g. via chrome://restart or via session restore (when logging out /
    > switching off the machine with multiple profiles' windows open).
    >
    > Bug: 1150330
    > Change-Id: Iae9744f44bc242e9aaaf8f287cc29e1089326786
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2532460
    > Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#828665}
    
    Bug: 1150330
    Change-Id: I457871e2041eb09eb90fb31f773d095b47aeb462
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547040
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837191}

--- chrome/browser/ui/profile_picker.h
+++ chrome/browser/ui/profile_picker.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class GURL;
 
 namespace base {
-class CommandLine;
 class FilePath;
 }  // namespace base
 
@@ -109,14 +108,11 @@ class ProfilePicker {
   // Overrides the timeout delay for waiting for extended account info.
   static void SetExtendedAccountInfoTimeoutForTesting(base::TimeDelta timeout);
 
-  // Returns whether the profile picker at launch. This can be called on
+  // Returns whether to show profile picker at launch. This can be called on
   // startup or when Chrome is re-opened, e.g. when clicking on the dock icon on
   // MacOS when there are no windows, or from Windows tray icon.
-  // This returns true if this is a new session. Returns false if a specific
-  // profile is passed in the command line, or if some parameters (such as the
-  // URLs to launch) cannot be handled by the picker.
-  static bool ShouldShowAtLaunch(const base::CommandLine& command_line,
-                                 const std::vector<GURL>& urls_to_launch);
+  // This returns true if the user has multiple profiles and has not opted-out.
+  static bool ShouldShowAtLaunch();
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ProfilePicker);
