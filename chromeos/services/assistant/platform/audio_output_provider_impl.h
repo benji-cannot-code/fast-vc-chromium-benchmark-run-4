020246c496eb10885db1dea7e65067d1841bb29a
Jeroen Dhollander <jeroendh@chromium.org>
2020-12-15 00:50:36 +0000

    Revert "Remove external dependencies from |AudioInputImpl|"
    This reverts commit 340ff5b300ef541d17b79a8a69443e8a0dd6d9e2.
    
    Reason for revert: causes crash crbug.com/1158611
    
    Original change's description:
    > Remove external dependencies from |AudioInputImpl|
    >
    > This will allow us to easier move this class to the Libassistant mojm
    > service.
    >
    > All dependencies have been moved to the new class |AudioInputHost|.
    > This class currently talks directly to |AudioInputImpl|, but later
    > it will instead talk to the mojom APIs.
    >
    > Some notes:
    >    - |AudioInputImpl| still depends on
    >      |AssistantClient::Get()->RequestAudioStreamFactory()|, because the
    >      Libassistant mojom service will also use an audio stream factory to
    >      open/close its audio input. In a follow up CL I will introduce a
    >      delegate to sever this dependency.
    >    - There is one behavior change:
    >      |power_manager_client_->NotifyWakeNotification()| was previously
    >      only called when DSP was enabled, and it is now always called (when
    >      Libassistant starts a conversation). I can think of no reason why
    >      this would be an issue.
    >
    > Bug: b/171748795
    > Test: chromeos_unittests, deployed on real hardware with and without DSP.
    > Change-Id: I119521c7a72bf5361019d70240c02ba47a23d6ad
    > Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2582537
    > Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#836710}
    
    TBR=xiaohuic@chromium.org,wutao@chromium.org,meilinw@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,jeroendh@chromium.org
    
    Change-Id: I2525fce21492c0417d0411db99fbb8463583cb02
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b/171748795
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2591771
    Reviewed-by: Jeroen Dhollander <jeroendh@chromium.org>
    Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836891}

--- chromeos/services/assistant/platform/audio_output_provider_impl.h
+++ chromeos/services/assistant/platform/audio_output_provider_impl.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/audio/public/mojom/stream_factory.mojom.h"
 
 namespace chromeos {
+class CrasAudioHandler;
+class PowerManagerClient;
 
 namespace assistant {
 
@@ -33,6 +35,8 @@ class AssistantMediaSession;
 class AudioOutputProviderImpl : public assistant_client::AudioOutputProvider {
  public:
   AudioOutputProviderImpl(
+      PowerManagerClient* power_manager_client,
+      CrasAudioHandler* cras_audio_handler,
       AssistantMediaSession* media_session,
       scoped_refptr<base::SequencedTaskRunner> background_task_runner,
       const std::string& device_id);
--- chromeos/services/assistant/platform_api_impl.h
+++ chromeos/services/assistant/platform_api_impl.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "chromeos/audio/cras_audio_handler.h"
-#include "chromeos/services/assistant/platform/audio_input_host.h"
 #include "chromeos/services/assistant/platform/audio_input_provider_impl.h"
 #include "chromeos/services/assistant/platform/audio_output_provider_impl.h"
 #include "chromeos/services/assistant/platform/file_provider_impl.h"
@@ -103,7 +102,6 @@ class PlatformApiImpl : public CrosPlatformApi,
   FakeAuthProvider auth_provider_;
   FileProviderImpl file_provider_;
   NetworkProviderImpl network_provider_;
-  AudioInputHost audio_input_host_;
   std::unique_ptr<SystemProviderImpl> system_provider_;
   std::string pref_locale_;
 
