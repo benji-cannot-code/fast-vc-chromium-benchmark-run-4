8755be6295b8288bc144a1392804dd946b5bb015
Elizabeth Popova <lizapopova@google.com>
2020-12-15 12:48:35 +0000

    [Autofill] Add parsing for dependent locality
    Prior to this CL, dependent locality was a part of address scheme, but
    was never detected in forms by Autofill. The missing parsing is
    implemented behind kAutofillEnableDependentLocalityParsing flag.
    
    Bug: 1154727
    Change-Id: I1c3869549b0aca9f803c88d733239ed1414abc96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2584843
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/master@{#837058}

--- components/autofill/core/browser/form_data_importer_unittest.cc
+++ components/autofill/core/browser/form_data_importer_unittest.cc
@@ -585,6 +585,10 @@ TEST_P(FormDataImporterTest, ImportStructuredNameAddressProfile) {
 }
 
 TEST_P(FormDataImporterTest, ImportAddressProfiles) {
+  base::test::ScopedFeatureList dependent_locality_feature;
+  dependent_locality_feature.InitAndEnableFeature(
+      features::kAutofillEnableDependentLocalityParsing);
+
   FormData form;
   form.url = GURL("https://wwww.foo.com");
 
@@ -601,6 +605,9 @@ TEST_P(FormDataImporterTest, ImportAddressProfiles) {
   test::CreateTestFormField("Address:", "address1", "21 Laussat St", "text",
                             &field);
   form.fields.push_back(field);
+  test::CreateTestFormField("Neighborhood:", "neighborhood", "Nob Hill", "text",
+                            &field);
+  form.fields.push_back(field);
   test::CreateTestFormField("City:", "city", "San Francisco", "text", &field);
   form.fields.push_back(field);
   test::CreateTestFormField("State:", "state", "California", "text", &field);
@@ -614,8 +621,8 @@ TEST_P(FormDataImporterTest, ImportAddressProfiles) {
   AutofillProfile expected(base::GenerateGUID(), test::kEmptyOrigin);
   test::SetProfileInfo(&expected, "George", nullptr, "Washington",
                        "theprez@gmail.com", nullptr, "21 Laussat St", nullptr,
-                       "San Francisco", "California", "94102", nullptr,
-                       nullptr);
+                       "Nob Hill", "San Francisco", "California", "94102",
+                       nullptr, nullptr);
   const std::vector<AutofillProfile*>& results =
       personal_data_manager_->GetProfiles();
   ASSERT_EQ(1U, results.size());
--- components/autofill/core/browser/form_parsing/address_field.h
+++ components/autofill/core/browser/form_parsing/address_field.h
@@ -53,6 +53,7 @@ class AddressField : public FormField {
   static const int kZipCodeMatchType;
   static const int kCityMatchType;
   static const int kStateMatchType;
+  static const int kDependentLocalityMatchType;
 
   explicit AddressField(LogManager* log_manager);
 
@@ -74,15 +75,19 @@ class AddressField : public FormField {
   bool ParseZipCode(AutofillScanner* scanner,
                     const LanguageCode& page_language);
 
+  bool ParseDependentLocality(AutofillScanner* scanner,
+                              const LanguageCode& page_language);
+
   bool ParseCity(AutofillScanner* scanner, const LanguageCode& page_language);
 
   bool ParseState(AutofillScanner* scanner, const LanguageCode& page_language);
 
   // Parses the current field pointed to by |scanner|, if it exists, and tries
-  // to figure out whether the field's type: city, state, country, zip, or
-  // none of those.
-  bool ParseCityStateCountryZipCode(AutofillScanner* scanner,
-                                    const LanguageCode& page_language);
+  // to determine if the field's type corresponds to one of the following:
+  // dependent locality, city, state, country, zip, or none of those.
+  bool ParseDependentLocalityCityStateCountryZipCode(
+      AutofillScanner* scanner,
+      const LanguageCode& page_language);
 
   // Like ParseFieldSpecifics(), but applies |pattern| against the name and
   // label of the current field separately. If the return value is
@@ -101,6 +106,10 @@ class AddressField : public FormField {
   // RESULT_MATCH_NAME_LABEL, then |scanner| advances and the field is set.
   // Otherwise |scanner| rewinds and the field is cleared.
   ParseNameLabelResult ParseNameAndLabelForZipCode(
+      AutofillScanner* scanner,
+      const LanguageCode& page_language);
+
+  ParseNameLabelResult ParseNameAndLabelForDependentLocality(
       AutofillScanner* scanner,
       const LanguageCode& page_language);
 
@@ -125,6 +134,7 @@ class AddressField : public FormField {
   AutofillField* address3_ = nullptr;
   AutofillField* street_address_ = nullptr;
   AutofillField* apartment_number_ = nullptr;
+  AutofillField* dependent_locality_ = nullptr;
   AutofillField* city_ = nullptr;
   AutofillField* state_ = nullptr;
   AutofillField* zip_ = nullptr;
