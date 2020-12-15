9824260dbf7bc86eb32bff0e221a4f0ba3705d91
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-12-15 18:01:30 +0000

    Add browser-side Managed Configuration API implementation
    This CL adds a new policy ManagedConfigurationPerOrigin, which will
    contain the links to the JSON key-value configuration for applications.
    We also add a new KeyedService, which which is responsible for
    processing of this data and storing it on disk.
    
    Bug: 1141849
    Change-Id: Ieb37c7b9462bdac1b1cd48132af0d3a27f4e51ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2577679
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837155}

---
+++ chrome/browser/device_api/managed_configuration_api_factory.cc
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/device_api/managed_configuration_api_factory.h"
+
+#include "base/memory/singleton.h"
+#include "chrome/browser/device_api/managed_configuration_api.h"
+#include "chrome/browser/profiles/profile.h"
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/pref_registry/pref_registry_syncable.h"
+#include "components/prefs/pref_service_factory.h"
+
+// static
+ManagedConfigurationAPIFactory* ManagedConfigurationAPIFactory::GetInstance() {
+  return base::Singleton<ManagedConfigurationAPIFactory>::get();
+}
+
+// static
+ManagedConfigurationAPI* ManagedConfigurationAPIFactory::GetForProfile(
+    Profile* profile) {
+  return static_cast<ManagedConfigurationAPI*>(
+      GetInstance()->GetServiceForBrowserContext(profile, true));
+}
+
+ManagedConfigurationAPIFactory::ManagedConfigurationAPIFactory()
+    : BrowserContextKeyedServiceFactory(
+          "ManagedConfigurationAPI",
+          BrowserContextDependencyManager::GetInstance()) {}
+
+ManagedConfigurationAPIFactory::~ManagedConfigurationAPIFactory() = default;
+
+KeyedService* ManagedConfigurationAPIFactory::BuildServiceInstanceFor(
+    content::BrowserContext* context) const {
+  Profile* profile(Profile::FromBrowserContext(context));
+  ManagedConfigurationAPI* api = new ManagedConfigurationAPI(profile);
+  return api;
+}
+
+void ManagedConfigurationAPIFactory::RegisterProfilePrefs(
+    user_prefs::PrefRegistrySyncable* registry) {
+  ManagedConfigurationAPI::RegisterProfilePrefs(registry);
+}
+
+bool ManagedConfigurationAPIFactory::ServiceIsCreatedWithBrowserContext()
+    const {
+  return true;
+}
+
+bool ManagedConfigurationAPIFactory::ServiceIsNULLWhileTesting() const {
+  return true;
+}
