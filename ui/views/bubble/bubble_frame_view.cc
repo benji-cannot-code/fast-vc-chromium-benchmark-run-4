e7f1f5fcc2b7a8c04f1d0850d471380a7144f6e7
Wei Li <weili@chromium.org>
2020-12-15 19:08:08 +0000

    Remove NO_SHADOW_OPAQUE_BORDER shadow type
    Ash's touch calibrator view should use NO_ASSETS type if it doesn't
    need the shadow. Otherwise, it should use SMALL_SHADOW (or BIG_SHADOW)
    which has no difference from the current NO_SHADOW_OPAQUE_BORDER shadow
    implementation.
    
    Since that is the last use of NO_SHADOW_OPAQUE_BORDER shadow type,
    now we can completely remove this type and related code.
    
    Bug: 940027
    Change-Id: I8d9e69a76f3fa240947397c5735163db21fc4803
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2591246
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837194}

--- ui/views/bubble/bubble_frame_view.cc
+++ ui/views/bubble/bubble_frame_view.cc
@@ -252,7 +252,6 @@ int BubbleFrameView::NonClientHitTest(const gfx::Point& point) {
 void BubbleFrameView::GetWindowMask(const gfx::Size& size,
                                     SkPath* window_mask) {
   if (bubble_border_->shadow() != BubbleBorder::SMALL_SHADOW &&
-      bubble_border_->shadow() != BubbleBorder::NO_SHADOW_OPAQUE_BORDER &&
       bubble_border_->shadow() != BubbleBorder::NO_ASSETS)
     return;
 
@@ -275,8 +274,7 @@ void BubbleFrameView::GetWindowMask(const gfx::Size& size,
       SkIntToScalar(size.height() - border_insets.bottom() +
                     kBorderStrokeSize)};
 
-  if (bubble_border_->shadow() == BubbleBorder::NO_SHADOW_OPAQUE_BORDER ||
-      bubble_border_->shadow() == BubbleBorder::NO_ASSETS) {
+  if (bubble_border_->shadow() == BubbleBorder::NO_ASSETS) {
     window_mask->addRoundRect(rect, kCornerRadius, kCornerRadius);
   } else {
     static const int kBottomBorderShadowSize = 2;
