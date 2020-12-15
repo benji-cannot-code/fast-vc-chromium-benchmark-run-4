3d54c258bdb16f1688200452b0a304bfb436f549
Victor Costan <pwnall@chromium.org>
2020-12-15 21:29:46 +0000

    Cookie Store: Add thread safety annotations to CookieStoreContext.
    Change-Id: I9e62fcf1a06a8c1513c8c64662bfb4e7ac6f9419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2592927
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837256}

--- content/browser/cookie_store/cookie_store_context.cc
+++ content/browser/cookie_store/cookie_store_context.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/cookie_store/cookie_store_context.h"
 
 #include "base/bind.h"
+#include "base/sequence_checker.h"
 #include "base/task/post_task.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
 #include "content/browser/storage_partition_impl.h"
@@ -20,13 +21,15 @@ namespace content {
 CookieStoreContext::CookieStoreContext()
     : base::RefCountedDeleteOnSequence<CookieStoreContext>(
           base::CreateSingleThreadTaskRunner(
-              {ServiceWorkerContext::GetCoreThreadId()})) {}
+              {ServiceWorkerContext::GetCoreThreadId()})) {
+  DETACH_FROM_SEQUENCE(core_sequence_checker_);
+}
 
 CookieStoreContext::~CookieStoreContext() {
   // The destructor must be called on the service worker core thread, because it
-  // runs cookie_store_manager_'s destructor, and the latter is only accessed on
-  // the core thread.
-  DCHECK_CURRENTLY_ON(ServiceWorkerContext::GetCoreThreadId());
+  // runs `cookie_store_manager_`'s destructor, and the latter is only accessed
+  // on the service worker core thread.
+  DCHECK_CALLED_ON_VALID_SEQUENCE(core_sequence_checker_);
 }
 
 void CookieStoreContext::Initialize(
@@ -127,7 +130,7 @@ void CookieStoreContext::CreateServiceForTesting(
 void CookieStoreContext::InitializeOnCoreThread(
     scoped_refptr<ServiceWorkerContextWrapper> service_worker_context,
     base::OnceCallback<void(bool)> success_callback) {
-  DCHECK_CURRENTLY_ON(ServiceWorkerContext::GetCoreThreadId());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(core_sequence_checker_);
   DCHECK(!cookie_store_manager_) << __func__ << " called more than once";
 
   cookie_store_manager_ =
@@ -138,7 +141,7 @@ void CookieStoreContext::InitializeOnCoreThread(
 void CookieStoreContext::ListenToCookieChangesOnCoreThread(
     mojo::PendingRemote<::network::mojom::CookieManager> cookie_manager_remote,
     base::OnceCallback<void(bool)> success_callback) {
-  DCHECK_CURRENTLY_ON(ServiceWorkerContext::GetCoreThreadId());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(core_sequence_checker_);
   DCHECK(cookie_store_manager_);
 
   cookie_store_manager_->ListenToCookieChanges(std::move(cookie_manager_remote),
@@ -148,7 +151,7 @@ void CookieStoreContext::ListenToCookieChangesOnCoreThread(
 void CookieStoreContext::CreateServiceOnCoreThread(
     const url::Origin& origin,
     mojo::PendingReceiver<blink::mojom::CookieStore> receiver) {
-  DCHECK_CURRENTLY_ON(ServiceWorkerContext::GetCoreThreadId());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(core_sequence_checker_);
   DCHECK(cookie_store_manager_);
 
   cookie_store_manager_->CreateService(std::move(receiver), origin);
