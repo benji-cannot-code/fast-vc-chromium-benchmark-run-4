d725bec1db3b53c0cdf1b3dc38d7875b84565f97
David Maunder <davidjm@chromium.org>
2020-12-15 17:14:06 +0000

    Batch FilePersistedTabDataStorageTest and PersistedTabDataTest
    FilePersistedTabDataStorageTest: 6744 ms -> 2003 ms (70% improvement)
    PersistedTabDataTest: 1621 ms -> 178 ms (89% improvement)
    
    Bug: 1158487
    Change-Id: Ibe1c998aa4269955582c4a3d0548f61f5e939e33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2590344
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837134}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/state/FilePersistedTabDataStorageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/state/FilePersistedTabDataStorageTest.java
@@ -16,6 +16,7 @@ import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.Batch;
 import org.chromium.chrome.browser.tab.state.FilePersistedTabDataStorage.FileSaveRequest;
 import org.chromium.chrome.test.ChromeBrowserTestRule;
 
@@ -26,6 +27,7 @@ import java.util.concurrent.Semaphore;
  * Tests relating to  {@link FilePersistedTabDataStorage}
  */
 @RunWith(BaseJUnit4ClassRunner.class)
+@Batch(Batch.PER_CLASS)
 public class FilePersistedTabDataStorageTest {
     @Rule
     public final ChromeBrowserTestRule mBrowserTestRule = new ChromeBrowserTestRule();
