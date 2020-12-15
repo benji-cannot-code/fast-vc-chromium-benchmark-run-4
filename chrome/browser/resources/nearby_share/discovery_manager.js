208e2dbd8f8b2be1317531b015500af290f4db11
Michael Hansen <hansenmichael@google.com>
2020-12-15 03:50:20 +0000

    [Nearby] Add GetShareType to Attachment.
    Moves the logic for determining the ShareType into the *Attachment
    classes. This cleans up some static casts and also allows this logic to
    be reused when generating the preview for the receive dialog page,
    coming in a subsequent CL.
    
    To avoid a dependency cycle I've moved the ShareType mojo struct
    definition into its own build target, which required importing new mojo
    gen files in a number of places.
    
    Fixed: 1144942
    Change-Id: I7fe151470c9067a918363152d2e0520a08dc0298
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2582523
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#836953}

--- chrome/browser/resources/nearby_share/discovery_manager.js
+++ chrome/browser/resources/nearby_share/discovery_manager.js
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
 import 'chrome://resources/mojo/mojo/public/mojom/base/unguessable_token.mojom-lite.js';
 import './mojo/nearby_share_target_types.mojom-lite.js';
+import './mojo/nearby_share_share_type.mojom-lite.js';
 import './mojo/nearby_share.mojom-lite.js';
 
 /** @type {?nearbyShare.mojom.DiscoveryManagerInterface} */
