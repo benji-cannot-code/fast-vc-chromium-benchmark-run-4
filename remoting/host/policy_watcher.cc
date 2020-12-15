0fcd786f6f0f8d03490b839b3b22a8fb7b9a5078
Joe Downing <joedow@chromium.org>
2020-12-15 16:20:09 +0000

    Update PolicyWatcher to provide platform policy values
    The current PolicyWatcher exposes a set of 'current' policies which
    is actually the default policies with overrides from any platform
    policy which is actually set.  This works fine for driving behavior
    but in some cases we actually want to know whether a policy value
    we receive is the 'default' or if an admin has set it explicitly.
    
    To allow this, the PolicyWatcher will now store the set of platform
    policies on the machine and provide it on request.  I plan to use
    this for logging (to get an idea of policy usage and popularity)
    and for cases where we want to provide a flag from the website to
    the NMH but also want admins to override the value if needed.
    
    Since the PolicyWatcher provides the platform policies now, I renamed
    GetCurrentPolicies() to GetEffectivePolicies() as I think that name
    reflects the fact that it is a merged set of policies.
    
    Change-Id: Id2fd578ac42d925922e8beb5f028374d7bcdcbf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587614
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837116}

--- remoting/host/policy_watcher.cc
+++ remoting/host/policy_watcher.cc
@@ -160,8 +160,12 @@ void PolicyWatcher::StartWatching(
   }
 }
 
-std::unique_ptr<base::DictionaryValue> PolicyWatcher::GetCurrentPolicies() {
-  return old_policies_->CreateDeepCopy();
+std::unique_ptr<base::DictionaryValue> PolicyWatcher::GetEffectivePolicies() {
+  return effective_policies_->CreateDeepCopy();
+}
+
+std::unique_ptr<base::DictionaryValue> PolicyWatcher::GetPlatformPolicies() {
+  return platform_policies_->CreateDeepCopy();
 }
 
 std::unique_ptr<base::DictionaryValue> PolicyWatcher::GetDefaultPolicies() {
@@ -192,7 +196,8 @@ std::unique_ptr<base::DictionaryValue> PolicyWatcher::GetDefaultPolicies() {
 }
 
 void PolicyWatcher::SignalPolicyError() {
-  old_policies_->Clear();
+  effective_policies_->Clear();
+  platform_policies_->Clear();
   policy_error_callback_.Run();
 }
 
@@ -201,7 +206,8 @@ PolicyWatcher::PolicyWatcher(
     std::unique_ptr<policy::PolicyService> owned_policy_service,
     std::unique_ptr<policy::ConfigurationPolicyProvider> owned_policy_provider,
     std::unique_ptr<policy::SchemaRegistry> owned_schema_registry)
-    : old_policies_(new base::DictionaryValue()),
+    : effective_policies_(new base::DictionaryValue()),
+      platform_policies_(new base::DictionaryValue()),
       default_values_(GetDefaultPolicies()),
       policy_service_(policy_service),
       owned_schema_registry_(std::move(owned_schema_registry)),
@@ -304,7 +310,7 @@ PolicyWatcher::StoreNewAndReturnChangedPolicies(
   base::DictionaryValue::Iterator iter(*new_policies);
   while (!iter.IsAtEnd()) {
     base::Value* old_policy;
-    if (!(old_policies_->Get(iter.key(), &old_policy) &&
+    if (!(effective_policies_->Get(iter.key(), &old_policy) &&
           old_policy->Equals(&iter.value()))) {
       changed_policies->Set(iter.key(), iter.value().CreateDeepCopy());
     }
@@ -325,7 +331,7 @@ PolicyWatcher::StoreNewAndReturnChangedPolicies(
   }
 
   // Save the new policies.
-  old_policies_.swap(new_policies);
+  effective_policies_.swap(new_policies);
 
   return changed_policies;
 }
@@ -341,6 +347,8 @@ void PolicyWatcher::OnPolicyUpdated(const policy::PolicyNamespace& ns,
     SignalPolicyError();
     return;
   }
+
+  platform_policies_ = new_policies->CreateDeepCopy();
 
   // Use default values for any missing policies.
   std::unique_ptr<base::DictionaryValue> filled_policies =
