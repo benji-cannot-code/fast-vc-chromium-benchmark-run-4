82bb95105587811b10929d568913079dbc35f210
rbpotter <rbpotter@chromium.org>
2020-12-15 02:00:33 +0000

    WebUI: Remove generated path from SetupWebUIDataSource
    Change-Id: I3a329883d475d30ac4b95d404c8b1abc1b69eb41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2583842
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#836916}

--- chrome/browser/ui/webui/webui_util.h
+++ chrome/browser/ui/webui/webui_util.h
@@ -32,7 +32,6 @@ struct ResourcePath {
 // test loader files.
 void SetupWebUIDataSource(content::WebUIDataSource* source,
                           base::span<const GritResourceMap> resources,
-                          const std::string& generated_path,
                           int default_resource);
 
 // Calls content::WebUIDataSource::AddLocalizedString() in a for-loop for
