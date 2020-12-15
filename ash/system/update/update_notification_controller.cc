e2d0612d83472e32536589b67ecf01161cd66433
James Cook <jamescook@chromium.org>
2020-12-15 21:24:18 +0000

    chromeos: Remove support for Flash update notifications
    Chrome no longer supports Adobe Flash.
    
    I left a few pieces in place because I intend to add a similar
    notification for Lacros updates.
    
    Bug: 1139558
    Change-Id: Ic44d08c514f09f6471cf96e8fa0ac9c61a266041
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593178
    Commit-Queue: James Cook <jamescook@chromium.org>
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837247}

--- ash/system/update/update_notification_controller.cc
+++ ash/system/update/update_notification_controller.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
-#include "build/branding_buildflags.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/message_center/message_center.h"
@@ -153,12 +152,6 @@ base::string16 UpdateNotificationController::GetNotificationMessage() const {
 }
 
 base::string16 UpdateNotificationController::GetNotificationTitle() const {
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  if (model_->update_type() == UpdateType::kFlash) {
-    return l10n_util::GetStringUTF16(
-        IDS_UPDATE_NOTIFICATION_TITLE_FLASH_PLAYER);
-  }
-#endif
   const base::string16 notification_title = model_->notification_title();
   if (!notification_title.empty())
     return notification_title;
