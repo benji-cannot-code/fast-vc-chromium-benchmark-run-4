c5ae7920072c39cd22e59973ceecadc775388c3c
Matt Menke <mmenke@chromium.org>
2020-12-15 17:40:22 +0000

    DomainRelability + NetworkIsolationKeys CL 2
    Wire NetworkIsolationKey through DomainReliabilityContext.
    
    This is the interesting CL of the bunch. To keep things simple, it keeps
    all pending beacons in a single list with a maximum size, even with
    different NetworkIsolationKeys. To upload a report, it selects all
    beacon with the same Key as the first (oldest) beacon and creates a
    report using them. On successful upload, it will remove just those
    beacons, and try to upload again (with backoff), if any beacons are
    left.
    
    This CL also adds some eviction tests, and removes some incorrect
    eviction DCHECKs, which assumed that all beacons currently being
    uploaded could not be evicted. That may be unlikely, but there's
    nothing from preventing it from actually happening, and it may
    be more likely once only beacons with a matching NIK are uploaded
    each time.
    
    Bug: 1138994
    Change-Id: I37d97b679cc0309c5e128c239585555ab097369b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587669
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837146}

--- components/domain_reliability/beacon.h
+++ components/domain_reliability/beacon.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/domain_reliability/domain_reliability_export.h"
 #include "net/base/net_error_details.h"
+#include "net/base/network_isolation_key.h"
 #include "url/gurl.h"
 
 namespace base {
@@ -45,6 +46,10 @@ struct DOMAIN_RELIABILITY_EXPORT DomainReliabilityBeacon {
   // The URL that the beacon is reporting on, if included.
   // The scheme can be non-secure.
   GURL url;
+  // The NetworkIsolationKey associated with the request being reported on. Must
+  // also be used to upload any report. This field does not appear in the
+  // uploaded report.
+  net::NetworkIsolationKey network_isolation_key;
   // The resource name that the beacon is reporting on, if included.
   std::string resource;
   // Status string (e.g. "ok", "dns.nxdomain", "http.403").
