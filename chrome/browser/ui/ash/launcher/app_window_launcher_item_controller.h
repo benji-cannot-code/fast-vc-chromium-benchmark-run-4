86a2d377f777d945460b8c0d4609d822508b031e
Nancy Wang <nancylingwang@chromium.org>
2020-12-15 21:23:12 +0000

    Fix the AppWindowLauncherItemController::GetAppWindow crash issue.
    This CL is the 3rd option to resolve the crash of below call stack:
    AppWindowLauncherItemController::GetAppWindow(aura::Window*, bool)
    AppWindowLauncherItemController::SetActiveWindow(aura::Window*)
    AppWindowLauncherController::OnWindowActivated()
    
    One possible guessing is the AppWindowLauncherItemController object has
    been deleted, but still used by AppServiceAppWindowLauncherController.
    So modify the destruct function, and set the controller as null in
    AppWindowBase.
    
    BUG=1128491
    
    Change-Id: Ied59f857653aa0a136cbe9f903ff79ce5856e34c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2586647
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837244}

--- chrome/browser/ui/ash/launcher/app_window_launcher_item_controller.h
+++ chrome/browser/ui/ash/launcher/app_window_launcher_item_controller.h
@@ -16,10 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
 
-namespace ui {
-class BaseWindow;
-}
-
+class AppWindowBase;
 class ShelfContextMenu;
 
 // This is a ShelfItemDelegate for abstract app windows (extension or ARC).
@@ -31,16 +28,16 @@ class ShelfContextMenu;
 class AppWindowLauncherItemController : public ash::ShelfItemDelegate,
                                         public aura::WindowObserver {
  public:
-  using WindowList = std::list<ui::BaseWindow*>;
+  using WindowList = std::list<AppWindowBase*>;
 
   explicit AppWindowLauncherItemController(const ash::ShelfID& shelf_id);
   ~AppWindowLauncherItemController() override;
 
-  void AddWindow(ui::BaseWindow* window);
-  void RemoveWindow(ui::BaseWindow* window);
+  void AddWindow(AppWindowBase* window);
+  void RemoveWindow(AppWindowBase* window);
 
   void SetActiveWindow(aura::Window* window);
-  ui::BaseWindow* GetAppWindow(aura::Window* window, bool include_hidden);
+  AppWindowBase* GetAppWindow(aura::Window* window, bool include_hidden);
 
   // ash::ShelfItemDelegate overrides:
   AppWindowLauncherItemController* AsAppWindowLauncherItemController() override;
@@ -75,7 +72,7 @@ class AppWindowLauncherItemController : public ash::ShelfItemDelegate,
 
  protected:
   // Returns last active window in the controller or first window.
-  ui::BaseWindow* GetLastActiveWindow();
+  AppWindowBase* GetLastActiveWindow();
 
  private:
   friend class ChromeLauncherControllerTest;
@@ -101,7 +98,7 @@ class AppWindowLauncherItemController : public ash::ShelfItemDelegate,
   // Pointer to the most recently active app window
   // TODO(khmel): Get rid of |last_active_window_| and provide more reliable
   // way to determine active window.
-  ui::BaseWindow* last_active_window_ = nullptr;
+  AppWindowBase* last_active_window_ = nullptr;
 
   // Scoped list of observed windows (for removal on destruction)
   ScopedObserver<aura::Window, aura::WindowObserver> observed_windows_{this};
