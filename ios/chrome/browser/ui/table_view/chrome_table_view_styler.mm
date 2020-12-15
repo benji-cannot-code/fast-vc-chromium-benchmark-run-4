ea33f3edb143b564c82eea47903d1e86a303d520
Ewann <ewannpv@google.com>
2020-12-15 12:34:05 +0000

    [iOS][Settings] Fixes ChromeTableViews background color
    This CL fixes the background color of ChromeTableViews.
    
    Bug: 922511
    Change-Id: I47662d867da44444902de17c40a399c9b20f4399
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593111
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837055}

--- ios/chrome/browser/ui/table_view/chrome_table_view_styler.mm
+++ ios/chrome/browser/ui/table_view/chrome_table_view_styler.mm
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           [UIColor colorNamed:kSecondaryBackgroundColor];
       _cellBackgroundColor = UIColor.cr_secondarySystemGroupedBackgroundColor;
     } else {
-      _tableViewBackgroundColor = UIColor.cr_systemGroupedBackgroundColor;
+      _tableViewBackgroundColor = UIColor.cr_systemBackgroundColor;
     }
   }
   return self;
