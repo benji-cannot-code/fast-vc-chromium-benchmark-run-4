f8ea81fe8089eafc66ca330257a018ab92e95f35
François Doray <fdoray@chromium.org>
2020-12-15 23:00:27 +0000

    Revert "[blink scheduler] Add blink::Thread::ThreadHandle."
    This reverts commit e66dcc06523101fab57200333d198100cb1d99e5.
    
    Reason for revert: Thread handle != thread id on Linux
    crbug.com/1158103
    
    Original change's description:
    > [blink scheduler] Add blink::Thread::ThreadHandle.
    >
    > The thread handle should be used instead of the thread id for thread
    > manipulations. It is available immediately after thread creation,
    > unlike the thread id which is available only after the main function
    > is entered. Also, on Windows, the thread handle remains valid after the
    > thread has exited.
    >
    > This is a prerequisite to remove the wait after compositor thread
    > initialization
    > https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/renderer/platform/scheduler/common/thread.cc;l=96;drc=5b1ca7b1087e13d963bc4058d9406a838fb7f9ca
    >
    > Bug: 1080709
    > Change-Id: Ia0b72aa1b3a86454cffa774131944db87ced0843
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2578172
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Commit-Queue: Kentaro Hara <haraken@chromium.org>
    > Auto-Submit: François Doray <fdoray@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#835544}
    
    TBR=gab@chromium.org,fdoray@chromium.org,haraken@chromium.org,altimin@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1080709, 1158103
    Change-Id: Ib4bd9113ad8a72fe4e9215989e07986d3b357c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593833
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837302}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread.h
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_refptr.h"
 #include "base/single_thread_task_runner.h"
-#include "base/threading/platform_thread.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 
@@ -28,7 +27,6 @@ class PLATFORM_EXPORT MainThread : public Thread {
   // Thread implementation.
   ThreadScheduler* Scheduler() override;
   PlatformThreadId ThreadId() const override;
-  base::PlatformThreadHandle ThreadHandle() const override;
   scoped_refptr<base::SingleThreadTaskRunner> GetTaskRunner() const override;
 
   void AddTaskTimeObserver(base::sequence_manager::TaskTimeObserver*) override;
@@ -36,10 +34,9 @@ class PLATFORM_EXPORT MainThread : public Thread {
       base::sequence_manager::TaskTimeObserver*) override;
 
  private:
-  const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
-  MainThreadSchedulerImpl* const scheduler_;  // Not owned.
-  const PlatformThreadId thread_id_;
-  const base::PlatformThreadHandle thread_handle_;
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+  MainThreadSchedulerImpl* scheduler_;  // Not owned.
+  PlatformThreadId thread_id_;
 };
 
 }  // namespace scheduler
