0937c950bebd9e76e13722f6cde01c390fa7f0a9
Christopher Cameron <ccameron@chromium.org>
2020-12-16 09:56:30 +0000

    Remove unuseble CanvasPixelFormats
    The CanvasPixelFormat should have only user-specifiable formats. At
    some point it ballooned to support BGRA and RGBA and RGBX. Those
    formats now live in CanvasResourceParams as a SkColorType.
    
    Trim down CanvasPixelFormats to just F16 and Uint8. Remove accessors
    that aren't used anymore.
    
    Remove CanvasColorParams::GetNativeCanvasPixelFormat.
    
    Bug: 1157747
    Change-Id: I8b45326a92cd6fdfb1d7e140a3edb1b7862726b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2591711
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837505}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context.cc
@@ -42,7 +42,7 @@ CanvasRenderingContext::CanvasRenderingContext(
     const CanvasContextCreationAttributesCore& attrs)
     : host_(host),
       color_params_(CanvasColorSpace::kSRGB,
-                    CanvasColorParams::GetNativeCanvasPixelFormat(),
+                    CanvasPixelFormat::kUint8,
                     kNonOpaque),
       creation_attributes_(attrs) {
   if (creation_attributes_.pixel_format == kF16CanvasPixelFormatName)
@@ -79,9 +79,7 @@ WTF::String CanvasRenderingContext::PixelFormatAsString() const {
   switch (color_params_.PixelFormat()) {
     case CanvasPixelFormat::kF16:
       return kF16CanvasPixelFormatName;
-    case CanvasPixelFormat::kRGBA8:
-    case CanvasPixelFormat::kBGRA8:
-    case CanvasPixelFormat::kRGBX8:
+    case CanvasPixelFormat::kUint8:
       return kUint8CanvasPixelFormatName;
   };
   CHECK(false);
