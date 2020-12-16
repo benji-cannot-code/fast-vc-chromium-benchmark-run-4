19648f147d3105622e84a8047f832aa581a5a014
Nohemi Fernandez <fernandex@chromium.org>
2020-12-16 09:34:34 +0000

    [iOS] Use signin namespace for utility functions.
    As a follow-up to crrev.com/c/2562778, moves all sign-in utilities to
    the new signin namespace.
    
    Bug: 1157531
    Change-Id: Ic695832a7cd5778e69039b2d3fd1395de3cde50c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593259
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837501}

--- ios/chrome/browser/ui/authentication/signin/user_signin/logging/first_run_signin_logger.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/logging/first_run_signin_logger.mm
@@ -34,7 +34,7 @@ using signin_metrics::RecordSigninUserActionForAccessPoint;
     LogSigninAccessPointStarted(self.accessPoint, self.promoAction);
     RecordSigninUserActionForAccessPoint(self.accessPoint, self.promoAction);
   }
-  SigninRecordVersionSeen();
+  signin::RecordVersionSeen();
 }
 
 @end
