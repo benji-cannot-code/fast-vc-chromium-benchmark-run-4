07e1929b6b150aab99cb546c8d601ca3a4fbcb77
Sonny Sasaka <sonnysasaka@chromium.org>
2020-12-16 00:35:30 +0000

    [Bluetooth] Remove HfpBatteryListener
    Battery reporting via HFP is now consolidated into BlueZ, so there is no
    need to listen to CRAS directly.
    
    BUG=b:172361090,b:166543531
    
    Change-Id: Id0f54323790a5788e09441a32a9a4be77a3d8bb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593821
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837358}

--- ash/power/peripheral_battery_tracker.cc
+++ ash/power/peripheral_battery_tracker.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/power/peripheral_battery_tracker.h"
 
-#include "ash/power/hfp_battery_listener.h"
 #include "ash/power/hid_battery_listener.h"
 #include "base/bind.h"
 #include "base/check.h"
@@ -26,9 +25,9 @@ void PeripheralBatteryTracker::InitializeOnBluetoothReady(
     scoped_refptr<device::BluetoothAdapter> adapter) {
   adapter_ = adapter;
   DCHECK(adapter_.get());
-  hfp_battery_listener_ = std::make_unique<HfpBatteryListener>(adapter_);
   hid_battery_listener_ = std::make_unique<HidBatteryListener>(adapter_);
-  // GATT Battery reporting is handled by device::BluetoothBatteryClient.
+  // GATT and HFP Battery reporting is handled by
+  // device::BluetoothBatteryClient.
 }
 
 }  // namespace ash
