c1ffd7cfb2bc15685d8141c20bf63814ece54ce2
Yulun Wu <yulunwu@chromium.org>
2020-12-16 00:44:38 +0000

    Update app list icon context menu selection themed color.
    Bug: 1134433
    Change-Id: Ie49947cc0b75f4f8f9688b0db2e40f5edb4b791a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2594268
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837362}

--- ash/public/cpp/test/test_app_list_color_provider.h
+++ ash/public/cpp/test/test_app_list_color_provider.h
@@ -51,6 +51,7 @@ class TestAppListColorProvider : public AppListColorProvider {
   SkColor GetFocusRingColor() const override;
   SkColor GetFolderItemFocusRingColor() const override;
   SkColor GetPrimaryIconColor(SkColor default_color) const override;
+  SkColor GetContextMenuHighlightColor(bool is_in_folder) const override;
   float GetFolderBackgrounBlurSigma() const override;
 };
 
