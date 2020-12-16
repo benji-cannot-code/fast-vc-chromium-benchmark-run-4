b0ce3f7a15e31a37e32616b3dcd7625227b66518
Collin Baker <collinbaker@chromium.org>
2020-12-16 23:57:33 +0000

    Replace ThumbnailImage::Observer with Subscriber
    Previously, clients interested in seeing thumbnail updates had to
    derive from ThumbnailImage::Observer and register themselves with
    ThumbnailImage.
    
    Removing ThumbnailTabHelper and ThumbnailImage from the public API and
    instead exposing a global ThumbnailService is a design
    goal. ThumbnailImage::Observer makes this refactor difficult.
    
    This CL uses a callback-based subscriber model instead.
    base::Callbacks replace observer methods. Hints are supplied
    directly on the subscription object. The subscription models loosens
    the coupling between clients and ThumbnailImage.
    
    Clients subscribe by ThumbnailImage::Subscribe(). Subscription can
    later be pushed up easily to the ThumbnailService level by a method
    like ThumbnailService::Subscribe(content::WebContents*).
    
    Bug: 1090038
    Change-Id: I020d848e00331d5f5bbb562f0c53aec5f8ccbc62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593158
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837818}

--- chrome/browser/ui/webui/tab_strip/thumbnail_tracker.cc
+++ chrome/browser/ui/webui/tab_strip/thumbnail_tracker.cc
@@ -16,14 +16,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Handles requests for a given tab's thumbnail and watches for thumbnail
 // updates for the lifetime of the tab.
-class ThumbnailTracker::ContentsData : public content::WebContentsObserver,
-                                       public ThumbnailImage::Observer {
+class ThumbnailTracker::ContentsData : public content::WebContentsObserver {
  public:
   ContentsData(ThumbnailTracker* parent, content::WebContents* contents)
       : content::WebContentsObserver(contents), parent_(parent) {
     thumbnail_ = parent_->thumbnail_getter_.Run(contents);
-    if (thumbnail_)
-      observer_.Add(thumbnail_.get());
+    if (!thumbnail_)
+      return;
+
+    subscription_ = thumbnail_->Subscribe();
+    subscription_->SetCompressedImageCallback(base::BindRepeating(
+        &ContentsData::ThumbnailImageCallback, base::Unretained(this)));
   }
 
   void RequestThumbnail() {
@@ -36,7 +39,7 @@ class ThumbnailTracker::ContentsData : public content::WebContentsObserver,
     // We must un-observe each ThumbnailImage when the WebContents it came from
     // closes.
     if (thumbnail_) {
-      observer_.Remove(thumbnail_.get());
+      subscription_.reset();
       thumbnail_.reset();
     }
 
@@ -44,16 +47,14 @@ class ThumbnailTracker::ContentsData : public content::WebContentsObserver,
     parent_->ContentsClosed(web_contents());
   }
 
-  // ThumbnailImage::Observer:
-  void OnCompressedThumbnailDataAvailable(
-      CompressedThumbnailData thumbnail_image) override {
-    parent_->ThumbnailUpdated(web_contents(), thumbnail_image);
+ private:
+  void ThumbnailImageCallback(CompressedThumbnailData image) {
+    parent_->ThumbnailUpdated(web_contents(), image);
   }
 
- private:
   ThumbnailTracker* parent_;
   scoped_refptr<ThumbnailImage> thumbnail_;
-  ScopedObserver<ThumbnailImage, ThumbnailImage::Observer> observer_{this};
+  std::unique_ptr<ThumbnailImage::Subscription> subscription_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentsData);
 };
