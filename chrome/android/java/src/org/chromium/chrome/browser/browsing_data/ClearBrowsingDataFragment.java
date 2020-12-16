33c4e537237f489209b9ee16a36f08b249023a9f
Hwanseung Lee <hs1217.lee@samsung.com>
2020-12-16 09:33:04 +0000

    Remove History.ClearBrowsingData.TimeSpentInDialog histogram
    this histogram expired since M85 branch out.
    so remove this historam.
    
    Bug: 1088836
    Change-Id: I1b05d0773d1f8d83200ff815eb667c72c057727a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2588676
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Hwanseung Lee <hs1217.lee@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#837500}

--- chrome/android/java/src/org/chromium/chrome/browser/browsing_data/ClearBrowsingDataFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browsing_data/ClearBrowsingDataFragment.java
@@ -10,7 +10,6 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -214,9 +213,6 @@ public abstract class ClearBrowsingDataFragment extends PreferenceFragmentCompat
     // important domains from being cleared.
     private ConfirmImportantSitesDialogFragment mConfirmImportantSitesDialog;
 
-    // Time in ms, when the dialog was created.
-    private long mDialogOpened;
-
     /**
      * @return All available {@link DialogOption} entries.
      */
@@ -328,9 +324,6 @@ public abstract class ClearBrowsingDataFragment extends PreferenceFragmentCompat
         for (@DialogOption Integer option : options) {
             dataTypes.add(getDataType(option));
         }
-
-        RecordHistogram.recordMediumTimesHistogram("History.ClearBrowsingData.TimeSpentInDialog",
-                SystemClock.elapsedRealtime() - mDialogOpened);
 
         final @CookieOrCacheDeletionChoice int choice;
         if (dataTypes.contains(BrowsingDataType.COOKIES)) {
@@ -523,7 +516,6 @@ public abstract class ClearBrowsingDataFragment extends PreferenceFragmentCompat
         if (savedInstanceState != null) {
             mFetcher = savedInstanceState.getParcelable(CLEAR_BROWSING_DATA_FETCHER);
         }
-        mDialogOpened = SystemClock.elapsedRealtime();
         getActivity().setTitle(R.string.clear_browsing_data_title);
         SettingsUtils.addPreferencesFromResource(this, R.xml.clear_browsing_data_preferences_tab);
         List<Integer> options = getDialogOptions();
