2bd038e91a4eafe2cf95f2ee1c91ed10d758ca7a
Side Yilmaz <sideyilmaz@chromium.org>
2020-12-16 23:39:44 +0000

    [DownloadProfile-2] Pass OTRProfileID from DownloadUtils.
    This CL instroduce serialization mechanism for OTRProfileId and passes
    OTRProfileId from |DownloadUtils#showDownloadManager| function via
    reaching profile from WebContents. By this CL;
    - OTRProfileID class has serialization logic to be able to transfer via
    intent and pass to the components level.
    - OTRProfileIDTest class is created.
    
    Bug: 1099577, 1145502
    Change-Id: I2e02b8881025cfb673f269f5de619f68ebfa0d36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2541683
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837809}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadUtils.java
@@ -42,6 +42,7 @@ import org.chromium.chrome.browser.offlinepages.OfflinePageBridge;
 import org.chromium.chrome.browser.offlinepages.OfflinePageOrigin;
 import org.chromium.chrome.browser.offlinepages.OfflinePageUtils;
 import org.chromium.chrome.browser.offlinepages.downloads.OfflinePageDownloadBridge;
+import org.chromium.chrome.browser.profiles.OTRProfileID;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabLaunchType;
@@ -73,6 +74,8 @@ public class DownloadUtils {
 
     private static final String EXTRA_IS_OFF_THE_RECORD =
             "org.chromium.chrome.browser.download.IS_OFF_THE_RECORD";
+    private static final String EXTRA_OTR_PROFILE_ID =
+            "org.chromium.chrome.browser.download.OTR_PROFILE_ID";
     private static final String MIME_TYPE_ZIP = "application/zip";
     private static final String DOCUMENTS_UI_PACKAGE_NAME = "com.android.documentsui";
     public static final String EXTRA_SHOW_PREFETCHED_CONTENT =
@@ -140,7 +143,12 @@ public class DownloadUtils {
             Intent intent = new Intent();
             intent.setClass(appContext, DownloadActivity.class);
             intent.putExtra(EXTRA_SHOW_PREFETCHED_CONTENT, showPrefetchedContent);
-            if (tab != null) intent.putExtra(EXTRA_IS_OFF_THE_RECORD, tab.isIncognito());
+            if (tab != null) {
+                intent.putExtra(EXTRA_IS_OFF_THE_RECORD, tab.isIncognito());
+
+                OTRProfileID id = Profile.fromWebContents(tab.getWebContents()).getOTRProfileID();
+                intent.putExtra(EXTRA_OTR_PROFILE_ID, OTRProfileID.serialize(id));
+            }
             if (activity == null) {
                 // Stands alone in its own task.
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -165,6 +173,15 @@ public class DownloadUtils {
         }
         DownloadMetrics.recordDownloadPageOpen(source);
         return true;
+    }
+
+    /**
+     * @param intent An {@link Intent} instance.
+     * @return The {@link OTRProfileID} that is attached to the given intent.
+     */
+    public static OTRProfileID getOTRProfileIDFromIntent(Intent intent) {
+        String serializedId = IntentUtils.safeGetString(intent.getExtras(), EXTRA_OTR_PROFILE_ID);
+        return OTRProfileID.deserialize(serializedId);
     }
 
     /**
