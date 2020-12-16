b6b17bd1b44025b4c565662a48ae185c71774f47
David Tseng <dtseng@google.com>
2020-12-16 19:23:35 +0000

    Re-enable SelectToSpeakTest.SpeakStatusTray excluding official builds
    On official builds, the unified status tray appears to be inactive; test
    always fails. Re-enable for non-officila builds, which still exercises
    views UI + STS reading.
    
    R=akihiroota@chromium.org
    
    Fixed: 1158879
    Change-Id: I2fc7b4e6bb922c6d60aa819a73ce3a41b3afc003
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595491
    Auto-Submit: David Tseng <dtseng@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837680}

--- chrome/browser/chromeos/accessibility/select_to_speak_browsertest.cc
+++ chrome/browser/chromeos/accessibility/select_to_speak_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/memory/weak_ptr.h"
+#include "build/branding_buildflags.h"
 #include "chrome/browser/chromeos/accessibility/accessibility_manager.h"
 #include "chrome/browser/chromeos/accessibility/speech_monitor.h"
 #include "chrome/browser/profiles/profile.h"
@@ -163,7 +164,13 @@ class SelectToSpeakTestWithLanguageDetection : public SelectToSpeakTest {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(SelectToSpeakTest, DISABLED_SpeakStatusTray) {
+// The status tray is not active on official builds.
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#define MAYBE_SpeakStatusTray DISABLED_SpeakStatusTray
+#else
+#define MAYBE_SpeakStatusTray SpeakStatusTray
+#endif
+IN_PROC_BROWSER_TEST_F(SelectToSpeakTest, MAYBE_SpeakStatusTray) {
   gfx::Rect tray_bounds = ash::Shell::Get()
                               ->GetPrimaryRootWindowController()
                               ->GetStatusAreaWidget()
