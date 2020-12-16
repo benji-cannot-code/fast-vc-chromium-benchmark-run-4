6f376d261534cd5b733e60eb437df17b0c61e0b5
Matt Menke <mmenke@chromium.org>
2020-12-16 19:34:04 +0000

    Make SSLClientSocketVersionTest.CTIsRequiredByExpectCT with partitioning
    Currently, the test fails when all ExpectCT partitioning options is
    enabled, because its call to AddExpectCT() uses an empty
    NetworkIsolationKey, while its other calls use a specific transient key.
    This CL fixes the test to pass in the same transient key.
    
    This CL doesn't modify the test to enable ExpectCT partitioning, since
    that has to be done before the TransportSecurityState is constructed.
    
    Bug: 969893
    Change-Id: Ie36e8d0fc8c10028f57925d393309dd3e1658faa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595628
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837684}

--- net/socket/ssl_client_socket_unittest.cc
+++ net/socket/ssl_client_socket_unittest.cc
@@ -4392,7 +4392,7 @@ TEST_P(SSLClientSocketVersionTest, CTIsRequiredByExpectCT) {
   const base::Time expiry = current_time + base::TimeDelta::FromSeconds(1000);
   transport_security_state_->AddExpectCT(
       host_port_pair().host(), expiry, true /* enforce */,
-      GURL("https://example-report.test"), NetworkIsolationKey());
+      GURL("https://example-report.test"), network_isolation_key);
   MockExpectCTReporter reporter;
   transport_security_state_->SetExpectCTReporter(&reporter);
 
