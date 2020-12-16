3b0ddbad7f85ac42b85dc6ccca8eb16ec392fea1
Xiaohan Wang <xhwang@chromium.org>
2020-12-16 23:09:39 +0000

    Report permission UKM for PERMISSION_PROTECTED_MEDIA_IDENTIFIER
    This was in the original design but missed in the implementation because
    ProtectedMediaIdentifierPermissionContext uses a custom permission
    prompt logic.
    
    This CL updates ProtectedMediaIdentifierPermissionContext to report the
    UKM. We are also able to remove our own ReportPermissionActionUMA()
    since that's covered automatically by the new approach.
    
    In the future, when we use standard permission bubbles for
    ProtectedMediaIdentifierPermissionContext we won't need this anymore.
    
    Manually tested:
    0. Hacked media/blink/key_system_config_selector.cc to always request
       permission.
    1. Build ChromeOS Chrome on Linux.
    2. Start Chrome with the following flags:
       --force-enable-metrics-reporting --metrics-upload-interval=1
    3. Play protected content on Shaka Player, and saw the prompt.
    4. Go to chrome://ukm and chrome://histograms to check results.
       See below.
    
    Without this change:
    
    Histogram: Permissions.Action.ProtectedMedia recorded 1 samples, mean = 2.0 (flags = 0x41)
    0  ...
    2  ------------------------------------------------------------------------O (1 = 100.0%) {0.0%}
    3  ...
    
    With this change:
    
    Permission
    Action	1
    Gesture	2
    PermissionType	21
    PriorDismissals	0
    PriorIgnores	0
    PromptDisposition	0
    Source	0
    
    Histogram: Permissions.Action.ProtectedMedia recorded 2 samples, mean = 0.5 (flags = 0x41)
    0  ------------------------------------------------------------------------O (1 = 50.0%)
    1  ------------------------------------------------------------------------O (1 = 50.0%) {50.0%}
    2  ...
    
    Bug: 1158868
    Test: See above
    Change-Id: Ie852f26550e0f403806e70fb50f4c8897e7e5c11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2594120
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837797}

--- chrome/browser/media/protected_media_identifier_permission_context.cc
+++ chrome/browser/media/protected_media_identifier_permission_context.cc
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/constants/chromeos_switches.h"
 #include "chromeos/dbus/constants/dbus_switches.h"
 #include "chromeos/settings/cros_settings_names.h"
+#include "components/permissions/permission_request_impl.h"
+#include "components/permissions/permission_uma_util.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/user_prefs/user_prefs.h"
 #include "ui/views/widget/widget.h"
@@ -90,7 +92,8 @@ void ProtectedMediaIdentifierPermissionContext::DecidePermission(
       base::BindOnce(&ProtectedMediaIdentifierPermissionContext::
                          OnPlatformVerificationConsentResponse,
                      weak_factory_.GetWeakPtr(), web_contents, id,
-                     requesting_origin, embedding_origin, repeating_callback));
+                     requesting_origin, embedding_origin, user_gesture,
+                     repeating_callback));
 
   // This could happen when the permission is requested from an extension. See
   // http://crbug.com/728534
@@ -215,25 +218,39 @@ bool ProtectedMediaIdentifierPermissionContext::
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 
-static void ReportPermissionActionUMA(permissions::PermissionAction action) {
-  UMA_HISTOGRAM_ENUMERATION("Permissions.Action.ProtectedMedia", action,
-                            permissions::PermissionAction::NUM);
-}
-
 void ProtectedMediaIdentifierPermissionContext::
     OnPlatformVerificationConsentResponse(
         content::WebContents* web_contents,
         const permissions::PermissionRequestID& id,
         const GURL& requesting_origin,
         const GURL& embedding_origin,
+        bool user_gesture,
         permissions::BrowserPermissionCallback callback,
         PlatformVerificationDialog::ConsentResponse response) {
+  // Prepare function to report metrics.
+  auto report_metrics_func = [&](auto permission_action) {
+    // Use base::DoNothing() because we create a PermissionRequest only
+    // so that we can call PermissionUmaUtil::PermissionPromptResolved() below.
+    auto permission_request =
+        std::make_unique<permissions::PermissionRequestImpl>(
+            requesting_origin, ContentSettingsType::PROTECTED_MEDIA_IDENTIFIER,
+            user_gesture,
+            /*permission_decided_callback=*/base::DoNothing(),
+            /*delete_callback=*/base::DoNothing());
+
+    permissions::PermissionUmaUtil::PermissionPromptResolved(
+        {permission_request.get()}, web_contents, permission_action,
+        permissions::PermissionPromptDisposition::CUSTOM_MODAL_DIALOG,
+        /*ui_reason=*/base::nullopt,
+        /*predicted_grant_likelihood=*/base::nullopt);
+  };
+
   // The request may have been canceled. Drop the callback in that case.
   // This can happen if the tab is closed.
   PendingRequestMap::iterator request = pending_requests_.find(web_contents);
   if (request == pending_requests_.end()) {
     VLOG(1) << "Platform verification ignored by user.";
-    ReportPermissionActionUMA(permissions::PermissionAction::IGNORED);
+    report_metrics_func(permissions::PermissionAction::IGNORED);
     return;
   }
 
@@ -247,7 +264,7 @@ void ProtectedMediaIdentifierPermissionContext::
       // This can happen if user clicked "x", or pressed "Esc", or navigated
       // away without closing the tab.
       VLOG(1) << "Platform verification dismissed by user.";
-      ReportPermissionActionUMA(permissions::PermissionAction::DISMISSED);
+      report_metrics_func(permissions::PermissionAction::DISMISSED);
       content_setting = CONTENT_SETTING_ASK;
       persist = false;
       break;
@@ -255,7 +272,7 @@ void ProtectedMediaIdentifierPermissionContext::
       VLOG(1) << "Platform verification accepted by user.";
       base::RecordAction(
           base::UserMetricsAction("PlatformVerificationAccepted"));
-      ReportPermissionActionUMA(permissions::PermissionAction::GRANTED);
+      report_metrics_func(permissions::PermissionAction::GRANTED);
       content_setting = CONTENT_SETTING_ALLOW;
       persist = true;
       break;
@@ -263,7 +280,7 @@ void ProtectedMediaIdentifierPermissionContext::
       VLOG(1) << "Platform verification denied by user.";
       base::RecordAction(
           base::UserMetricsAction("PlatformVerificationRejected"));
-      ReportPermissionActionUMA(permissions::PermissionAction::DENIED);
+      report_metrics_func(permissions::PermissionAction::DENIED);
       content_setting = CONTENT_SETTING_BLOCK;
       persist = true;
       break;
