93d166a81b7d3eb614856ab37b5d6b5365002da9
Noel Gordon <noel@chromium.org>
2020-12-16 02:05:29 +0000

    [filesapp] SWA main.html autogen: import print_function from future
    Followup to CL:2585261. Use idiomatic import of print function: import
    it from the __future__.
    
    Bug: 1113981
    Change-Id: Ib5f7b87c193c9a9de60d25f9e82a4a6e0ff55bc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593848
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837393}

--- chromeos/components/file_manager/resources/gen_main_html.py
+++ chromeos/components/file_manager/resources/gen_main_html.py
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Generate SWA files app main.html from files app main.html"""
 
+from __future__ import print_function
+
 import fileinput
 import optparse
 import os
