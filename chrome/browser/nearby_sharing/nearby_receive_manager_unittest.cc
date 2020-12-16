f4340100a14adfc4b894a978e240a3afd06c8506
Bailey Berro <baileyberro@google.com>
2020-12-16 20:08:35 +0000

    Introduce nearby_share.mojom.RegisterReceiveSurfaceResult
    This change adds the RegisterReceiveSurfaceResult enum which is used to
    provide more granular result information to RegisterReceiveSurface
    callers.
    
    Bug: 1156391
    Change-Id: I6c308e71b85eab1eb52d74a9b8415a0d9231baee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2594177
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837704}

--- chrome/browser/nearby_sharing/nearby_receive_manager_unittest.cc
+++ chrome/browser/nearby_sharing/nearby_receive_manager_unittest.cc
@@ -41,6 +41,8 @@ class NearbyReceiveManagerTest : public testing::Test {
  public:
   using ReceiveSurfaceState = NearbySharingService::ReceiveSurfaceState;
   using StatusCodes = NearbySharingService::StatusCodes;
+  using RegisterReceiveSurfaceResult =
+      nearby_share::mojom::RegisterReceiveSurfaceResult;
 
   NearbyReceiveManagerTest()
       : transfer_metadata_(TransferMetadata::Status::kAwaitingLocalConfirmation,
@@ -111,9 +113,9 @@ class NearbyReceiveManagerTest : public testing::Test {
 
 TEST_F(NearbyReceiveManagerTest, Enter_Exit_Success) {
   ExpectRegister();
-  bool success = false;
-  receive_manager_waiter_.RegisterForegroundReceiveSurface(&success);
-  EXPECT_TRUE(success);
+  RegisterReceiveSurfaceResult result = RegisterReceiveSurfaceResult::kFailure;
+  receive_manager_waiter_.RegisterForegroundReceiveSurface(&result);
+  EXPECT_EQ(RegisterReceiveSurfaceResult::kSuccess, result);
 
   ExpectUnregister();
   bool exited = false;
@@ -124,39 +126,39 @@ TEST_F(NearbyReceiveManagerTest, Enter_Exit_Success) {
 }
 
 TEST_F(NearbyReceiveManagerTest, Enter_Failed) {
-  bool success = true;
+  RegisterReceiveSurfaceResult result = RegisterReceiveSurfaceResult::kSuccess;
   ExpectRegister(StatusCodes::kError);
-  receive_manager_waiter_.RegisterForegroundReceiveSurface(&success);
-  EXPECT_FALSE(success);
+  receive_manager_waiter_.RegisterForegroundReceiveSurface(&result);
+  EXPECT_EQ(RegisterReceiveSurfaceResult::kFailure, result);
   ExpectUnregister();
 }
 
 TEST_F(NearbyReceiveManagerTest, Multiple_Enter_Successful) {
-  bool success = false;
+  RegisterReceiveSurfaceResult result = RegisterReceiveSurfaceResult::kFailure;
   ExpectRegister(StatusCodes::kOk);
-  receive_manager_waiter_.RegisterForegroundReceiveSurface(&success);
+  receive_manager_waiter_.RegisterForegroundReceiveSurface(&result);
   FlushMojoMessages();
-  EXPECT_TRUE(success);
+  EXPECT_EQ(RegisterReceiveSurfaceResult::kSuccess, result);
 
-  success = false;
+  result = RegisterReceiveSurfaceResult::kFailure;
   ExpectRegister(StatusCodes::kOk);
-  receive_manager_waiter_.RegisterForegroundReceiveSurface(&success);
+  receive_manager_waiter_.RegisterForegroundReceiveSurface(&result);
   FlushMojoMessages();
-  EXPECT_TRUE(success);
+  EXPECT_EQ(RegisterReceiveSurfaceResult::kSuccess, result);
 
   ExpectUnregister();
 }
 
 TEST_F(NearbyReceiveManagerTest, Exit_Failed) {
-  bool success = false;
+  RegisterReceiveSurfaceResult result = RegisterReceiveSurfaceResult::kFailure;
   ExpectRegister();
-  receive_manager_waiter_.RegisterForegroundReceiveSurface(&success);
-  EXPECT_TRUE(success);
+  receive_manager_waiter_.RegisterForegroundReceiveSurface(&result);
+  EXPECT_EQ(RegisterReceiveSurfaceResult::kSuccess, result);
 
   ExpectUnregister(StatusCodes::kError);
-  success = false;
-  receive_manager_waiter_.UnregisterForegroundReceiveSurface(&success);
-  EXPECT_FALSE(success);
+  bool exited = false;
+  receive_manager_waiter_.UnregisterForegroundReceiveSurface(&exited);
+  EXPECT_FALSE(exited);
 
   ExpectUnregister();
 }
