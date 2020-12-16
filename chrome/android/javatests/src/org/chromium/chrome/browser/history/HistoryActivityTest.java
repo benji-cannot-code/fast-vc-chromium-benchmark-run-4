8a697cbd47e47aa4c13adde6bac2dfa8e38a1271
Theresa Wellington <twellington@chromium.org>
2020-12-16 17:31:35 +0000

    Re-enable HistoryActivityTest#testOpenSelectedItems
    Change HistoryActivityTest#testOpenSelectedItems and #testOpenItem to
    capture the intents sent when items are clicked, preventing
    ChromeTabbedActivity from actually starting (which is not needed as
    part of the validation in these tests).
    
    BUG=1157189
    
    Change-Id: I46cc42ecdff320944e42a608c142bdc852734344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2594312
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Theresa  <twellington@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837618}

--- chrome/android/javatests/src/org/chromium/chrome/browser/history/HistoryActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/history/HistoryActivityTest.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.history;
 
 import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.Intents.times;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
@@ -14,6 +15,8 @@ import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+import android.app.Activity;
+import android.app.Instrumentation.ActivityResult;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
@@ -38,7 +41,6 @@ import org.chromium.base.test.util.CloseableOnMainThread;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.IntentHandler;
@@ -210,19 +212,23 @@ public class HistoryActivityTest {
     @Test
     @SmallTest
     public void testOpenItem() throws Exception {
+        intending(allOf(hasAction(equalTo(Intent.ACTION_VIEW)), hasData(mItem1.getUrl())))
+                .respondWith(new ActivityResult(Activity.RESULT_OK, null));
+
         clickItem(2);
-        // Match history item open intent twice (once for launcher, once for tab activity)
-        intended(
-            allOf(
-                hasAction(equalTo(Intent.ACTION_VIEW)),
-                hasData(mItem1.getUrl())),
-            times(2));
+
+        intended(allOf(hasAction(equalTo(Intent.ACTION_VIEW)), hasData(mItem1.getUrl())), times(1));
     }
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1157189")
     public void testOpenSelectedItems() throws Exception {
+        // Stub out intent responses to prevent them from actually being sent.
+        intending(allOf(hasAction(equalTo(Intent.ACTION_VIEW)), hasData(mItem1.getUrl())))
+                .respondWith(new ActivityResult(Activity.RESULT_OK, null));
+        intending(allOf(hasAction(equalTo(Intent.ACTION_VIEW)), hasData(mItem2.getUrl())))
+                .respondWith(new ActivityResult(Activity.RESULT_OK, null));
+
         toggleItemSelection(2);
         toggleItemSelection(3);
 
@@ -232,16 +238,8 @@ public class HistoryActivityTest {
                             R.id.selection_mode_open_in_incognito, 0));
         });
 
-        intended(
-            allOf(
-                hasAction(equalTo(Intent.ACTION_VIEW)),
-                hasData(mItem1.getUrl())),
-            times(2));
-        intended(
-            allOf(
-                hasAction(equalTo(Intent.ACTION_VIEW)),
-                hasData(mItem2.getUrl())),
-            times(2));
+        intended(allOf(hasAction(equalTo(Intent.ACTION_VIEW)), hasData(mItem1.getUrl())), times(1));
+        intended(allOf(hasAction(equalTo(Intent.ACTION_VIEW)), hasData(mItem2.getUrl())), times(1));
     }
 
     @Test
