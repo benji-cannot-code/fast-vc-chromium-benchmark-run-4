8c2a6c6c96a1024a5dfa50344e0a0ce046e0db81
Marijn Kruisselbrink <mek@chromium.org>
2020-12-16 01:59:35 +0000

    [FSA] Sanitize descriptions for file types.
    This collapses white space, limits the length of descriptions to 64
    characters, and on windows strips out '*' characters to ensure the
    windows dialog itself will still include the true extension.
    
    This last behavior is triggered by the `keep_extension_visible` flag
    that was added in https://crrev.com/c/2582842 to fix a similar problem
    on Mac.
    
    Tested: Verified that the extension is shown in the pickers shown by https://fsa-tests.glitch.me/accepts.html
    Bug: 1137247
    Change-Id: Ife2344589427dbc1055234026601642113b6a791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2586958
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837391}

--- content/browser/file_system_access/file_system_chooser.cc
+++ content/browser/file_system_access/file_system_chooser.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/i18n/file_util_icu.h"
+#include "base/i18n/rtl.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/post_task.h"
 #include "build/build_config.h"
@@ -20,12 +22,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_client.h"
 #include "net/base/mime_util.h"
+#include "ui/gfx/text_elider.h"
 #include "ui/shell_dialogs/select_file_policy.h"
 #include "ui/shell_dialogs/selected_file_info.h"
 
 namespace content {
 
 namespace {
+
+// The maximum number of unicode code points the description of a file type is
+// allowed to be. Any longer descriptions will be truncated to this length.
+// The exact number here is fairly arbitrary, since font, font size, dialog
+// size and underlying platform all influence how many characters will actually
+// be visible. As such this can be adjusted as needed.
+constexpr int kMaxDescriptionLength = 64;
 
 std::string TypeToString(blink::mojom::ChooseFileSystemEntryType type) {
   switch (type) {
@@ -145,7 +155,16 @@ bool GetFileTypesFromAcceptsOption(
   if (extensions->empty())
     return false;
 
-  *description = option.description;
+  base::string16 sanitized_description = option.description;
+  if (!sanitized_description.empty()) {
+    sanitized_description = base::CollapseWhitespace(
+        sanitized_description, /*trim_sequences_with_line_breaks=*/false);
+    sanitized_description = gfx::TruncateString(
+        sanitized_description, kMaxDescriptionLength, gfx::CHARACTER_BREAK);
+    base::i18n::SanitizeUserSuppliedString(&sanitized_description);
+  }
+  *description = sanitized_description;
+
   return true;
 }
 
