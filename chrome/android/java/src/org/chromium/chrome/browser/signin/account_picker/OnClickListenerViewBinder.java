abd982d9e1880fe2c6057ae16e86dbb03de3551b
Alice Wang <aliceywang@chromium.org>
2020-12-16 09:18:47 +0000

    [WebSignin][Android] Refactor ViewBinder in account_picker MVC
    This CL refactors the ViewBinders in account_picker MVC in two ways:
    
    1/ Make all view binders implements the ViewBinder interface instead of
    using static class;
    2/ Merge IncognitoRowViewBinder and AddAccountRowViewBinder into one
    reusable class.
    
    Bug: 1158373
    Change-Id: I6a1f5442c52e4eb8e2115cd34c140822a494bdfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2589934
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837498}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/account_picker/OnClickListenerViewBinder.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.signin.account_picker;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.modelutil.PropertyModel.ReadableObjectPropertyKey;
+import org.chromium.ui.modelutil.PropertyModelChangeProcessor.ViewBinder;
+
+/**
+ * This class binds an {@link OnClickListener} to a {@link View}.
+ */
+class OnClickListenerViewBinder implements ViewBinder<PropertyModel, View, PropertyKey> {
+    private final ReadableObjectPropertyKey<OnClickListener> mOnClickListenerKey;
+
+    OnClickListenerViewBinder(ReadableObjectPropertyKey<OnClickListener> onClickListenerKey) {
+        mOnClickListenerKey = onClickListenerKey;
+    }
+
+    /**
+     * View binder that sets the {@link OnClickListener} of the view with the corresponding
+     * property in model.
+     */
+    @Override
+    public void bind(PropertyModel model, View view, PropertyKey propertyKey) {
+        assert propertyKey == mOnClickListenerKey : "Unknown  propertyKey: " + propertyKey;
+        view.setOnClickListener(model.get(mOnClickListenerKey));
+    }
+}
