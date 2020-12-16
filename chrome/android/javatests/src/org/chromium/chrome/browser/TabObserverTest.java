556bcb4cb9208e00d78b561d2a5c9f57095021b5
Brandon Wylie <wylieb@chromium.org>
2020-12-16 03:20:28 +0000

    Remove TabObservers by default when Tabs are detached from the window
    Also drops the requirement that every TabObserver care about activity
    detachment events by default. Interested clients can still override this
    method and get custom behavior.
    
    Bug: 1157728, 1146320
    Change-Id: Icb4ca185fb46075b141aa75d8c0ff57a187f5143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585996
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837421}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabObserverTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabObserverTest.java
@@ -17,6 +17,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Restriction;
@@ -104,5 +105,12 @@ public class TabObserverTest {
         // The original tab should be hidden.
         interactabilityHelper.waitForCallback(interactableCallCount);
         assertFalse("Tab should not be interactable.", mTab.isUserInteractable());
+    }
+
+    @Test
+    @SmallTest
+    public void testTabDetach_observerUnregistered() {
+        ThreadUtils.runOnUiThreadBlocking(() -> mTab.updateAttachment(null, null));
+        assertFalse(mTab.hasObserver(mTabObserver));
     }
 }
