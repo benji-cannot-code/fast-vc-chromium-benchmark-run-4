ac7f82ee0a46cfb98b16f7aa034da3b4e5975cdb
Nick Burris <nburris@chromium.org>
2020-12-16 00:28:18 +0000

    Make ExternalAuthUtils a singleton instance
    * Make ExternalAuthUtils provide its own instance instead of callers
      having to use AppHooks. A follow up patch will remove the
      ExternalAuthUtils instance from AppHooks.
    * This is patch 3/5 for providing ExternalAuthUtils as a singleton
      instead of from AppHooks.
    
    Change-Id: I6527d93dbce453626aa0c02f7093d7686f67266a
    Bug: 1154371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587430
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837354}

--- chrome/android/java/src/org/chromium/chrome/browser/TabbedModeTabDelegateFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/TabbedModeTabDelegateFactory.java
@@ -25,6 +25,7 @@ import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.browser_ui.util.BrowserControlsVisibilityDelegate;
 import org.chromium.components.browser_ui.util.ComposedBrowserControlsVisibilityDelegate;
 import org.chromium.components.external_intents.ExternalNavigationHandler;
+import org.chromium.components.externalauth.ExternalAuthUtils;
 
 /**
  * {@link TabDelegateFactory} class to be used in all {@link Tab} instances owned by a
@@ -69,7 +70,7 @@ public class TabbedModeTabDelegateFactory implements TabDelegateFactory {
                         mEphemeralTabCoordinatorSupplier, mContextMenuCopyLinkObserver,
                         mActivity::getSnackbarManager),
                 mShareDelegateSupplier, ChromeContextMenuPopulator.ContextMenuMode.NORMAL,
-                AppHooks.get().getExternalAuthUtils());
+                ExternalAuthUtils.getInstance());
     }
 
     @Override
--- chrome/android/java/src/org/chromium/chrome/browser/provider/ChromeBrowserProviderImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/provider/ChromeBrowserProviderImpl.java
@@ -33,7 +33,6 @@ import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.PostTask;
-import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.database.SQLiteCursor;
 import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
@@ -1134,10 +1133,9 @@ public class ChromeBrowserProviderImpl extends ChromeBrowserProvider.Impl {
     }
 
     private boolean hasPermission(String permission) {
-        boolean isSystemOrGoogleCaller =
-                AppHooks.get().getExternalAuthUtils().isCallerValid(getContext(),
-                        ExternalAuthUtils.FLAG_SHOULD_BE_GOOGLE_SIGNED
-                                | ExternalAuthUtils.FLAG_SHOULD_BE_SYSTEM);
+        boolean isSystemOrGoogleCaller = ExternalAuthUtils.getInstance().isCallerValid(getContext(),
+                ExternalAuthUtils.FLAG_SHOULD_BE_GOOGLE_SIGNED
+                        | ExternalAuthUtils.FLAG_SHOULD_BE_SYSTEM);
 
         if (isSystemOrGoogleCaller) {
             recordPermissionWasGranted("SignaturePassed", permission);
