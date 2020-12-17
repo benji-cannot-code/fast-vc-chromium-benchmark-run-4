033f3fecee99e3082e55e913727b682326d616d1
Darin Fisher <darin@chromium.org>
2020-12-17 16:42:21 +0000

    [Lacros] Unify theme setting w/ expectation on Chrome OS
    Change-Id: I7b378b8a859cb918281134ded63c01ac24d4675f
    Fixed: 1159610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596216
    Commit-Queue: Darin Fisher <darin@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838103}

--- chrome/browser/ui/webui/settings/appearance_handler.cc
+++ chrome/browser/ui/webui/settings/appearance_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/notreached.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/themes/theme_service.h"
@@ -31,7 +32,7 @@ void AppearanceHandler::RegisterMessages() {
                           base::Unretained(this)));
 // TODO(crbug.com/1052397): Revisit the macro expression once build flag switch
 // of lacros-chrome is complete.
-#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if defined(OS_LINUX) && !BUILDFLAG(IS_CHROMEOS_LACROS)
   web_ui()->RegisterMessageCallback(
       "useSystemTheme",
       base::BindRepeating(&AppearanceHandler::HandleUseSystemTheme,
@@ -45,7 +46,7 @@ void AppearanceHandler::HandleUseDefaultTheme(const base::ListValue* args) {
 
 // TODO(crbug.com/1052397): Revisit the macro expression once build flag switch
 // of lacros-chrome is complete.
-#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if defined(OS_LINUX) && !BUILDFLAG(IS_CHROMEOS_LACROS)
 void AppearanceHandler::HandleUseSystemTheme(const base::ListValue* args) {
   if (profile_->IsSupervised())
     NOTREACHED();
