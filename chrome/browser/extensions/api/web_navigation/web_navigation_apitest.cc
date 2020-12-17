fcafee149b8788bb8483033392f0982f13a4ba98
Yi Gu <yigu@chromium.org>
2020-12-17 21:39:05 +0000

    [CodeHealth] Convert c/b/extensions/ from base::Bind and base::Callback to Once/Repeating
    Bug: 1152279
    Change-Id: Ib3ba70a5b7ebb25bed2faada69c48f300b0ff18a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595462
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838245}

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -195,7 +195,7 @@ class WebNavigationApiTest : public ExtensionApiTest {
  public:
   WebNavigationApiTest() {
     embedded_test_server()->RegisterRequestHandler(
-        base::Bind(&HandleTestRequest));
+        base::BindRepeating(&HandleTestRequest));
   }
   ~WebNavigationApiTest() override {}
 
