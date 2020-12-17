f724a6c1fbf860b4e2befa6e81e2055064982ed6
tby <tby@chromium.org>
2020-12-17 00:16:48 +0000

    [Launcher settings] Remove chrome flag
    This expires in 89 and we've launched, so let's remove it.
    
    Bug: 1068851
    Change-Id: I3ae1aab05df1c6badf92d0fa9bb3283ec5e37821
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596277
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837826}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1385,10 +1385,6 @@ const char kKerberosSettingsSectionDescription[] =
     "Enables the Kerberos Section in ChromeOS settings. When disabled, "
     "Kerberos settings will stay under People Section.";
 
-const char kLauncherSettingsSearchName[] = "Launcher settings search";
-const char kLauncherSettingsSearchDescription[] =
-    "Enable search results for OS settings in the launcher.";
-
 const char kLegacyTLSEnforcedName[] =
     "Enforce deprecation of legacy TLS versions";
 const char kLegacyTLSEnforcedDescription[] =
