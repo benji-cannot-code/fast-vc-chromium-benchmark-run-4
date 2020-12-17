612b094c30fc33960d8c54dd47ca3598ea767c39
Hiroki Nakagawa <nhiroki@chromium.org>
2020-12-17 13:28:33 +0000

    NoStatePrefetch: Remove PrerenderHelper::prerender_mode_
    PrerenderHelper::prerender_mode_ is always PrerenderMode::kPrefetchOnly.
    It's not necessary to keep it as the member field.
    
    Bug: 755921
    Change-Id: I7a85359e972ee5be39115e7a3577d303023d3d02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593854
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838054}

--- components/no_state_prefetch/common/prerender_url_loader_throttle.h
+++ components/no_state_prefetch/common/prerender_url_loader_throttle.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "base/timer/timer.h"
 #include "components/no_state_prefetch/common/prerender_canceler.mojom.h"
-#include "components/no_state_prefetch/common/prerender_types.mojom.h"
 #include "net/base/request_priority.h"
 #include "third_party/blink/public/common/loader/url_loader_throttle.h"
 #include "third_party/blink/public/mojom/loader/resource_load_info.mojom-shared.h"
@@ -24,7 +23,6 @@ class PrerenderURLLoaderThrottle
       public base::SupportsWeakPtr<PrerenderURLLoaderThrottle> {
  public:
   PrerenderURLLoaderThrottle(
-      prerender::mojom::PrerenderMode mode,
       const std::string& histogram_prefix,
       mojo::PendingRemote<prerender::mojom::PrerenderCanceler> canceler);
   ~PrerenderURLLoaderThrottle() override;
