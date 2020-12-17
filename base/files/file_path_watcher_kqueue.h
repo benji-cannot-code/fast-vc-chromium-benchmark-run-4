bbded0d810ed2c1c615c1fa509277e7d7914abe6
Greg Thompson <grt@chromium.org>
2020-12-17 00:59:06 +0000

    [Mac] Fix a crash following destruction of a FilePathWatcher.
    FilePathWatcherKQueue::Watch uses a FileDescriptorWatcher, which
    requires that its callers handle spurious runs of callbacks after the
    watch is cancelled.
    
    If this speculative fix resolves the crash, a follow-on CL will revise
    the FileDescriptorWatcher documentation to make this case explicit.
    
    BUG=1156603
    R=fdoray@chromium.org
    
    Change-Id: I7d5da3fb4e58cf30efab21963908b3ff41e9dc71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593379
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837852}

--- base/files/file_path_watcher_kqueue.h
+++ base/files/file_path_watcher_kqueue.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_path_watcher.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 
 namespace base {
 
@@ -121,6 +122,8 @@ class FilePathWatcherKQueue : public FilePathWatcher::PlatformDelegate {
   // Throughout the lifetime of this, OnKQueueReadable() will be called when
   // data is available in |kqueue_|.
   std::unique_ptr<FileDescriptorWatcher::Controller> kqueue_watch_controller_;
+
+  WeakPtrFactory<FilePathWatcherKQueue> weak_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(FilePathWatcherKQueue);
 };
