8490d3b2507bec0c447f92e4ad78285213645abd
Elizabeth Popova <lizapopova@google.com>
2020-12-17 11:29:34 +0000

    [Autofill][Android] Open Autofill-specific help from settings
    Prior to this change, on help icon press in Autofill profiles/payments
    settings the general help article was opened. With this change the
    "Fill out forms automatically" page is shown instead.
    
    Bug: 1157449
    Change-Id: If1ac8016713ed58d35f5e777ba07b29cc9eeb847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2592796
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Cr-Commit-Position: refs/heads/master@{#838019}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/prefeditor/EditorDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/prefeditor/EditorDialog.java
@@ -10,7 +10,6 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -43,8 +42,8 @@ import androidx.core.view.MarginLayoutParamsCompat;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.autofill.AutofillUiUtils;
 import org.chromium.chrome.browser.autofill.settings.CreditCardNumberFormattingTextWatcher;
-import org.chromium.chrome.browser.feedback.HelpAndFeedbackLauncherImpl;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
 import org.chromium.components.browser_ui.widget.AlwaysDismissedDialog;
@@ -77,7 +76,7 @@ public class EditorDialog
 
     private static EditorObserverForTest sObserverForTest;
 
-    private final Context mContext;
+    private final Activity mActivity;
     private final Handler mHandler;
     private final TextView.OnEditorActionListener mEditorActionListener;
     private final int mHalfRowMargin;
@@ -116,7 +115,7 @@ public class EditorDialog
         super(activity, R.style.Theme_Chromium_Fullscreen);
         // Sets transparent background for animating content view.
         getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        mContext = activity;
+        mActivity = activity;
         mHandler = new Handler();
         mIsDismissed = false;
         mEditorActionListener = new TextView.OnEditorActionListener() {
@@ -173,12 +172,6 @@ public class EditorDialog
         getWindow().setAttributes(attributes);
     }
 
-    /** Launches the Autofill help page on top of the current Context and current Profile. */
-    public static void launchAutofillHelpPage(Context context, Profile profile) {
-        HelpAndFeedbackLauncherImpl.getInstance().show((Activity) context,
-                context.getString(R.string.help_context_autofill), profile, null);
-    }
-
     /**
      * Prepares the toolbar for use.
      *
@@ -203,7 +196,7 @@ public class EditorDialog
                     mDeleteRunnable.run();
                     animateOutDialog();
                 } else if (item.getItemId() == R.id.help_menu_id) {
-                    launchAutofillHelpPage(mContext, mProfile);
+                    AutofillUiUtils.launchAutofillHelpPage(mActivity, mProfile);
                 }
                 return true;
             }
@@ -407,7 +400,7 @@ public class EditorDialog
                 addFieldViewToEditor(mDataView, fieldModel);
             } else {
                 // Create a LinearLayout to put it and the next view side by side.
-                LinearLayout rowLayout = new LinearLayout(mContext);
+                LinearLayout rowLayout = new LinearLayout(mActivity);
                 mDataView.addView(rowLayout);
 
                 View firstView = addFieldViewToEditor(rowLayout, fieldModel);
@@ -449,9 +442,9 @@ public class EditorDialog
         View childView = null;
 
         if (fieldModel.getInputTypeHint() == EditorFieldModel.INPUT_TYPE_HINT_ICONS) {
-            childView = new EditorIconsField(mContext, parent, fieldModel).getLayout();
+            childView = new EditorIconsField(mActivity, parent, fieldModel).getLayout();
         } else if (fieldModel.getInputTypeHint() == EditorFieldModel.INPUT_TYPE_HINT_LABEL) {
-            childView = new EditorLabelField(mContext, parent, fieldModel).getLayout();
+            childView = new EditorLabelField(mActivity, parent, fieldModel).getLayout();
         } else if (fieldModel.getInputTypeHint() == EditorFieldModel.INPUT_TYPE_HINT_DROPDOWN) {
             Runnable prepareEditorRunnable = new Runnable() {
                 @Override
@@ -466,7 +459,7 @@ public class EditorDialog
                 }
             };
             EditorDropdownField dropdownView =
-                    new EditorDropdownField(mContext, parent, fieldModel, prepareEditorRunnable);
+                    new EditorDropdownField(mActivity, parent, fieldModel, prepareEditorRunnable);
             mFieldViews.add(dropdownView);
             mDropdownFields.add(dropdownView.getDropdown());
 
@@ -476,7 +469,7 @@ public class EditorDialog
             checkbox.setId(R.id.payments_edit_checkbox);
             checkbox.setText(fieldModel.getLabel());
             checkbox.setChecked(fieldModel.isChecked());
-            checkbox.setMinimumHeight(mContext.getResources().getDimensionPixelSize(
+            checkbox.setMinimumHeight(mActivity.getResources().getDimensionPixelSize(
                     R.dimen.editor_dialog_checkbox_min_height));
             checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
@@ -500,7 +493,7 @@ public class EditorDialog
             }
 
             EditorTextField inputLayout = new EditorTextField(
-                    mContext, fieldModel, mEditorActionListener, filter, formatter);
+                    mActivity, fieldModel, mEditorActionListener, filter, formatter);
             mFieldViews.add(inputLayout);
 
             EditText input = inputLayout.getEditText();
@@ -528,15 +521,15 @@ public class EditorDialog
      */
     public void show(EditorModel editorModel) {
         // If an asynchronous task calls show, while the activity is already finishing, return.
-        if (((Activity) mContext).isFinishing()) return;
+        if (mActivity.isFinishing()) return;
 
         setOnShowListener(this);
         setOnDismissListener(this);
         mEditorModel = editorModel;
-        mLayout = LayoutInflater.from(mContext).inflate(R.layout.payment_request_editor, null);
+        mLayout = LayoutInflater.from(mActivity).inflate(R.layout.payment_request_editor, null);
         setContentView(mLayout);
 
-        mFooter = LayoutInflater.from(mContext).inflate(
+        mFooter = LayoutInflater.from(mActivity).inflate(
                 R.layout.editable_option_editor_footer, null, false);
 
         prepareToolbar();
