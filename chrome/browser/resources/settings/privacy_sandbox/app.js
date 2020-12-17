04e301f7a5b807d6ec1376e3af41c298c1ff543f
Rainhard Findling <rainhard@chromium.org>
2020-12-17 13:02:12 +0000

    Privacy sandbox page: connect toggle button to pref
    Bug: 1152336
    Change-Id: If52332486426aa34d1eae63f87710d6d1066e123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2592804
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838048}

--- chrome/browser/resources/settings/privacy_sandbox/app.js
+++ chrome/browser/resources/settings/privacy_sandbox/app.js
@@ -18,6 +18,13 @@ Polymer({
 
   _template: html`{__html_template__}`,
 
+  properties: {
+    /**
+     * Preferences state.
+     */
+    prefs: Object,
+  },
+
   /** @private */
   onLearnMoreButtonClick_: function() {
     OpenWindowProxyImpl.getInstance().openURL(
