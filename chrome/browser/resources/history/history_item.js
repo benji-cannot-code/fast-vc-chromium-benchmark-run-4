1fc6402bfe9aac16911f3861d3bf8e6b1473f5dd
Dominic Mazzoni <dmazzoni@chromium.org>
2020-12-17 23:53:35 +0000

    History Web UI: add aria descriptions about date headers.
    The table groups sites visited in the same date in a "card", which
    contains a header indicating that date.
    
    We make the information in those headers accessibile using the
    aria-described-by property to link the first and last rows of each
    card with the corresponding date header, so the date is announced
    when navigating the table up or down with the keyboard.
    
    Originally authored by jaragunde@igalia.com on this patch:
    https://chromium-review.googlesource.com/c/chromium/src/+/2563485
    
    Bug: 1100393
    Change-Id: I256ff2c9b04dc060ec769219cf4bfc7dc32664dc
    AX-relnotes: announce date headings in chrome history page.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2594173
    Commit-Queue: Cait Phillips <caitkp@chromium.org>
    Auto-Submit: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838317}

--- chrome/browser/resources/history/history_item.js
+++ chrome/browser/resources/history/history_item.js
@@ -84,6 +84,12 @@ Polymer({
       type: Boolean,
       value: true,
     },
+
+    /** @private */
+    ariaDescribedByForHeading_: {
+      type: String,
+      computed: 'getAriaDescribedByForHeading_(isCardStart, isCardEnd)',
+    },
   },
 
   hostAttributes: {'role': 'row'},
@@ -190,9 +196,25 @@ Polymer({
   getEntrySummary_() {
     const item = this.item;
     return loadTimeData.getStringF(
-        'entrySummary', item.dateTimeOfDay,
+        'entrySummary',
+        this.isCardStart || this.isCardEnd ?
+            this.cardTitle_(
+                this.numberOfItems, item.dateRelativeDay, this.searchTerm) :
+            '',
+        item.dateTimeOfDay,
         item.starred ? loadTimeData.getString('bookmarked') : '', item.title,
         item.domain);
+  },
+
+  /**
+   * The first and last rows of a card have a described-by field pointing to
+   * the date header, to make sure users know if they have jumped between cards
+   * when navigating up or down with the keyboard.
+   * @private
+   * @return {string}
+   */
+  getAriaDescribedByForHeading_() {
+    return this.isCardStart || this.isCardEnd ? 'date-accessed' : '';
   },
 
   /**
