974b9df1696444ece4ef49bb9a164efd54b3e820
Thomas Tellier <tellier@google.com>
2020-12-17 14:51:40 +0000

    [CrOS] Show placeholder even when login screen password is focused
    The placeholder text of the login screen password textfield was previously hidden when the textfield was focused. However, the textfield is focused automatically and it's not possible to loose focus without hitting Tab. Therefore, we now show the placeholder text even when the textfield is focused, as we were doing in older versions, especially since some important messages regarding different auth methods could be displayed through it.
    
    Bug: 1158568
    Change-Id: I9731070d971cd2c8d1779c010ae9e7a1a3634315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595276
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Thomas Tellier <tellier@google.com>
    Cr-Commit-Position: refs/heads/master@{#838070}

--- ash/login/ui/login_password_view.cc
+++ ash/login/ui/login_password_view.cc
@@ -254,7 +254,6 @@ class LoginPasswordView::LoginTextfield : public views::Textfield {
     if (on_blur_closure_)
       on_blur_closure_.Run();
     views::Textfield::OnBlur();
-    SetPlaceholderText(prepared_placeholder_text_);
   }
 
   // views::Textfield:
@@ -262,7 +261,6 @@ class LoginPasswordView::LoginTextfield : public views::Textfield {
     if (on_focus_closure_)
       on_focus_closure_.Run();
     views::Textfield::OnFocus();
-    SetPlaceholderText(base::string16());
   }
 
   void AboutToRequestFocusFromTabTraversal(bool reverse) override {
@@ -271,10 +269,6 @@ class LoginPasswordView::LoginTextfield : public views::Textfield {
 
     if (!GetText().empty())
       SelectAll(/*reversed=*/false);
-  }
-
-  void set_prepared_placeholder_text(const base::string16& text) {
-    prepared_placeholder_text_ = text;
   }
 
   void UpdateFontListAndCursor() {
@@ -322,9 +316,6 @@ class LoginPasswordView::LoginTextfield : public views::Textfield {
   base::RepeatingClosure on_focus_closure_;
   base::RepeatingClosure on_blur_closure_;
   base::RepeatingClosure on_tab_focus_closure_;
-
-  // Placeholder text to set when focus gets lost.
-  base::string16 prepared_placeholder_text_;
 };
 
 class LoginPasswordView::EasyUnlockIcon : public views::Button {
@@ -738,7 +729,7 @@ void LoginPasswordView::Backspace() {
 
 void LoginPasswordView::SetPlaceholderText(
     const base::string16& placeholder_text) {
-  textfield_->set_prepared_placeholder_text(placeholder_text);
+  textfield_->SetPlaceholderText(placeholder_text);
 }
 
 void LoginPasswordView::SetReadOnly(bool read_only) {
