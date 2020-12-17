1bf2c378f9978baeebf9826e97c153600b40a8e9
Greg Thompson <grt@chromium.org>
2020-12-17 12:22:14 +0000

    Revert "Replace ThumbnailImage::Observer with Subscriber"
    This reverts commit e4a4b7a31e2ebafe436239b9f92e59c6a1c9714c.
    
    Reason for revert: https://crbug.com/1159701: very high crash rate on canary channel.
    
    Original change's description:
    > Replace ThumbnailImage::Observer with Subscriber
    >
    > Previously, clients interested in seeing thumbnail updates had to
    > derive from ThumbnailImage::Observer and register themselves with
    > ThumbnailImage.
    >
    > Removing ThumbnailTabHelper and ThumbnailImage from the public API and
    > instead exposing a global ThumbnailService is a design
    > goal. ThumbnailImage::Observer makes this refactor difficult.
    >
    > This CL uses a callback-based subscriber model instead.
    > base::Callbacks replace observer methods. Hints are supplied
    > directly on the subscription object. The subscription models loosens
    > the coupling between clients and ThumbnailImage.
    >
    > Clients subscribe by ThumbnailImage::Subscribe(). Subscription can
    > later be pushed up easily to the ThumbnailService level by a method
    > like ThumbnailService::Subscribe(content::WebContents*).
    >
    > Bug: 1090038
    > Change-Id: I020d848e00331d5f5bbb562f0c53aec5f8ccbc62
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593158
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Reviewed-by: John Lee <johntlee@chromium.org>
    > Commit-Queue: John Lee <johntlee@chromium.org>
    > Auto-Submit: Collin Baker <collinbaker@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#837818}
    
    TBR=dfried@chromium.org,collinbaker@chromium.org,johntlee@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: Ie1e64df4909ce6b11ac1f25788e8681f0a9b2489
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1090038
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595420
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838037}

--- chrome/browser/ui/webui/tab_strip/thumbnail_tracker.cc
+++ chrome/browser/ui/webui/tab_strip/thumbnail_tracker.cc
@@ -16,17 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Handles requests for a given tab's thumbnail and watches for thumbnail
 // updates for the lifetime of the tab.
-class ThumbnailTracker::ContentsData : public content::WebContentsObserver {
+class ThumbnailTracker::ContentsData : public content::WebContentsObserver,
+                                       public ThumbnailImage::Observer {
  public:
   ContentsData(ThumbnailTracker* parent, content::WebContents* contents)
       : content::WebContentsObserver(contents), parent_(parent) {
     thumbnail_ = parent_->thumbnail_getter_.Run(contents);
-    if (!thumbnail_)
-      return;
-
-    subscription_ = thumbnail_->Subscribe();
-    subscription_->SetCompressedImageCallback(base::BindRepeating(
-        &ContentsData::ThumbnailImageCallback, base::Unretained(this)));
+    if (thumbnail_)
+      observer_.Add(thumbnail_.get());
   }
 
   void RequestThumbnail() {
@@ -39,7 +36,7 @@ class ThumbnailTracker::ContentsData : public content::WebContentsObserver {
     // We must un-observe each ThumbnailImage when the WebContents it came from
     // closes.
     if (thumbnail_) {
-      subscription_.reset();
+      observer_.Remove(thumbnail_.get());
       thumbnail_.reset();
     }
 
@@ -47,14 +44,16 @@ class ThumbnailTracker::ContentsData : public content::WebContentsObserver {
     parent_->ContentsClosed(web_contents());
   }
 
- private:
-  void ThumbnailImageCallback(CompressedThumbnailData image) {
-    parent_->ThumbnailUpdated(web_contents(), image);
+  // ThumbnailImage::Observer:
+  void OnCompressedThumbnailDataAvailable(
+      CompressedThumbnailData thumbnail_image) override {
+    parent_->ThumbnailUpdated(web_contents(), thumbnail_image);
   }
 
+ private:
   ThumbnailTracker* parent_;
   scoped_refptr<ThumbnailImage> thumbnail_;
-  std::unique_ptr<ThumbnailImage::Subscription> subscription_;
+  ScopedObserver<ThumbnailImage, ThumbnailImage::Observer> observer_{this};
 
   DISALLOW_COPY_AND_ASSIGN(ContentsData);
 };
