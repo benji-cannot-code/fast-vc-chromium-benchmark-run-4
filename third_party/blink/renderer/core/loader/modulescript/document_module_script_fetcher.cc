5ac8cad3ff77070cb93a81c4c40b3b38b326ba3a
Camillo Bruni <cbruni@chromium.org>
2020-12-17 12:51:51 +0000

    [blink] Enable support for module script streaming
    This CL wires up the module compilation with streaming support.
    Actual script streaming is going to be enabled in a separate CL.
    
    Streaming compilation for module scripts shares most of the
    infrastructure with classic scripts. The ResourceClient has access to
    the ScriptStreamer through the ScriptResource. The underlying
    SourceStream is handed over to V8 where the background compilation task
    can incrementally call GetMoreData.
    
    Currently there finalisation still happens on the main thread by a
    synchronous call in V8ScriptRunner::CompileModule.
    
    - Add ScriptStreamer::TakeFrom helper, used by both modules and classic
      scripts.
    - Move streaming histogram recording to from ClassicPendingScript to
      the ScriptStreamer since it's used by both script types.
    
    Design doc: http://doc/1nYbQWl1gOggmtmHL6Hph83mDe89LhtZj_l3ZvfJJdSg
    
    Bug: 1061857
    Change-Id: I875d914da0aee79d3b13a9a9231537c43e3ea5d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2562847
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838042}

--- third_party/blink/renderer/core/loader/modulescript/document_module_script_fetcher.cc
+++ third_party/blink/renderer/core/loader/modulescript/document_module_script_fetcher.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_streamer.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/loader/resource/script_resource.h"
+#include "third_party/blink/renderer/core/script/pending_script.h"
 #include "third_party/blink/renderer/platform/bindings/parkable_string.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -47,6 +48,19 @@ void DocumentModuleScriptFetcher::NotifyFinished(Resource* resource) {
       return;
     }
   }
+  // Check if we can use the script streamer.
+  ScriptStreamer* streamer;
+  ScriptStreamer::NotStreamingReason not_streamed_reason;
+  std::tie(streamer, not_streamed_reason) =
+      ScriptStreamer::TakeFrom(script_resource);
+
+  ScriptStreamer::RecordStreamingHistogram(ScriptSchedulingType::kAsync,
+                                           streamer, not_streamed_reason);
+
+  TRACE_EVENT_WITH_FLOW1(TRACE_DISABLED_BY_DEFAULT("v8.compile"),
+                         "DocumentModuleScriptFetcher::NotifyFinished", this,
+                         TRACE_EVENT_FLAG_FLOW_IN, "not_streamed_reason",
+                         not_streamed_reason);
   // TODO(crbug.com/1061857): Pass ScriptStreamer to the client here.
   const KURL& url = script_resource->GetResponse().CurrentRequestUrl();
   // Create an external module script where base_url == source_url.
@@ -54,9 +68,8 @@ void DocumentModuleScriptFetcher::NotifyFinished(Resource* resource) {
   client_->NotifyFetchFinishedSuccess(ModuleScriptCreationParams(
       /*source_url=*/url, /*base_url=*/url, module_type,
       script_resource->SourceText(), script_resource->CacheHandler(),
-      script_resource->GetResourceRequest().GetCredentialsMode(),
-      /*script_streamer=*/nullptr,
-      ScriptStreamer::NotStreamingReason::kStreamingDisabled));
+      script_resource->GetResourceRequest().GetCredentialsMode(), streamer,
+      not_streamed_reason));
 }
 
 void DocumentModuleScriptFetcher::Trace(Visitor* visitor) const {
