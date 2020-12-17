3f17e771e4ace968e862100b169f462189ca2c29
Ahmed Mehfooz <amehfooz@chromium.org>
2020-12-17 20:37:36 +0000

    Fix laser pointer icon for light mode
    Bug: 1156228
    Change-Id: Ibdb45ebbd6b54d3316e16844e75ebf4a12560757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2597739
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838210}

--- ash/system/palette/tools/laser_pointer_mode.cc
+++ ash/system/palette/tools/laser_pointer_mode.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
+#include "ash/style/ash_color_provider.h"
 #include "ash/system/palette/palette_ids.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -42,11 +43,17 @@ void LaserPointerMode::OnDisable() {
 }
 
 const gfx::VectorIcon& LaserPointerMode::GetActiveTrayIcon() const {
-  return kPaletteTrayIconLaserPointerIcon;
+  if (AshColorProvider::Get()->IsDarkModeEnabled())
+    return kPaletteTrayIconLaserPointerIcon;
+
+  return kPaletteTrayIconLaserPointerLightModeIcon;
 }
 
 const gfx::VectorIcon& LaserPointerMode::GetPaletteIcon() const {
-  return kPaletteModeLaserPointerIcon;
+  if (AshColorProvider::Get()->IsDarkModeEnabled())
+    return kPaletteModeLaserPointerIcon;
+
+  return kPaletteModeLaserPointerLightModeIcon;
 }
 
 views::View* LaserPointerMode::CreateView() {
