e6100213aca70176dfa73938360c333bc5dfb7a6
Michael Bai <michaelbai@chromium.org>
2020-12-17 19:29:23 +0000

    Autofill: Send the field types to Autofill service
    Send the server and computed types to autofill service when the
    response arrives after the autofill starts.
    
    Add test to cover this case.
    
    Bug: 1151542
    Change-Id: I91ffab1f0bd3ce62fb768ed690e90e2094c4d86d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595792
    Commit-Queue: Michael Bai <michaelbai@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838178}

--- components/autofill/android/provider/java/src/org/chromium/components/autofill/FormFieldData.java
+++ components/autofill/android/provider/java/src/org/chromium/components/autofill/FormFieldData.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.autofill;
 
 import android.graphics.RectF;
+import android.view.autofill.AutofillId;
 
 import androidx.annotation.IntDef;
 import androidx.annotation.VisibleForTesting;
@@ -68,6 +69,7 @@ public class FormFieldData {
     // after the object instantiated.
     private String mServerType;
     private String mComputedType;
+    private AutofillId mAutofillId;
 
     private FormFieldData(String name, String label, String value, String autocompleteAttr,
             boolean shouldAutocomplete, String placeholder, String type, String id,
@@ -171,6 +173,14 @@ public class FormFieldData {
     private void updateAutofillState(boolean autofilled) {
         if (mAutofilled && !autofilled) mPreviouslyAutofilled = true;
         mAutofilled = autofilled;
+    }
+
+    public void setAutofillId(AutofillId id) {
+        mAutofillId = id;
+    }
+
+    public AutofillId getAutofillId() {
+        return mAutofillId;
     }
 
     @CalledByNative
