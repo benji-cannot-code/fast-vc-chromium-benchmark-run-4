5813b5c87d3f61e56b414ad539ab3aeab68de92e
Ken Buchanan <kenrb@chromium.org>
2020-12-17 15:53:49 +0000

    [WebID] Add descriptive console strings for IDP interaction errors
    Right now it is difficult for developers to diagnose any errors when
    they are testing WebID with provider implementations. This adds
    more descriptive console messages to help narrow down any problems.
    
    Bug: 1141125
    Change-Id: I0c13ca6a5c50295292795d96cc7bedc2ee9cf99a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596130
    Reviewed-by: Sam Goto <goto@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838085}

--- content/browser/webid/idp_network_request_manager.cc
+++ content/browser/webid/idp_network_request_manager.cc
@@ -231,7 +231,7 @@ void IdpNetworkRequestManager::OnWellKnownParsed(
     data_decoder::DataDecoder::ValueOrError result) {
   if (!result.value) {
     std::move(idp_well_known_callback_)
-        .Run(FetchStatus::kFetchError, std::string());
+        .Run(FetchStatus::kInvalidResponseError, std::string());
     return;
   }
 
@@ -239,7 +239,7 @@ void IdpNetworkRequestManager::OnWellKnownParsed(
 
   if (!response.is_dict()) {
     std::move(idp_well_known_callback_)
-        .Run(FetchStatus::kFetchError, std::string());
+        .Run(FetchStatus::kInvalidResponseError, std::string());
     return;
   }
 
@@ -247,7 +247,7 @@ void IdpNetworkRequestManager::OnWellKnownParsed(
 
   if (!idp_endpoint || !idp_endpoint->is_string()) {
     std::move(idp_well_known_callback_)
-        .Run(FetchStatus::kFetchError, std::string());
+        .Run(FetchStatus::kInvalidResponseError, std::string());
     return;
   }
 
@@ -279,7 +279,7 @@ void IdpNetworkRequestManager::OnSigninRequestParsed(
     data_decoder::DataDecoder::ValueOrError result) {
   if (!result.value) {
     std::move(signin_request_callback_)
-        .Run(SigninResponse::kSigninError, std::string());
+        .Run(SigninResponse::kInvalidResponseError, std::string());
     return;
   }
 
@@ -287,7 +287,7 @@ void IdpNetworkRequestManager::OnSigninRequestParsed(
 
   if (!response.is_dict()) {
     std::move(signin_request_callback_)
-        .Run(SigninResponse::kSigninError, std::string());
+        .Run(SigninResponse::kInvalidResponseError, std::string());
     return;
   }
 
@@ -303,7 +303,7 @@ void IdpNetworkRequestManager::OnSigninRequestParsed(
   bool both_present = signin_url_present && token_present;
   if (!(signin_url_present || token_present) || both_present) {
     std::move(signin_request_callback_)
-        .Run(SigninResponse::kSigninError, std::string());
+        .Run(SigninResponse::kInvalidResponseError, std::string());
     return;
   }
 
