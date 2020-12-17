23f9326680d09f5950198a0189b6b91fc683d200
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-12-17 17:57:29 +0000

    Add profile DM token reading code to ConnectorsService
    This adds the main code to read the DM token from the profile instead of
    CBCM. The PerProfileConnectorsEnabled feature guards these changes from
    affecting working Connector policies. This change is insufficient to
    test per-profile Connectors since the authentication and reporting code
    still assume the DM token is from the browser.
    
    This adds a tweak to Connector policy test setters to ensure existing
    tests don't break and still use the browser DM token.
    
    Bug: 1148789, 1155585
    Change-Id: I64bb1714667b0b61fdfdbadffbc153328e22f741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2590429
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838139}

--- chrome/browser/enterprise/connectors/connectors_service_unittest.cc
+++ chrome/browser/enterprise/connectors/connectors_service_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile_manager.h"
 #include "components/enterprise/common/proto/connectors.pb.h"
+#include "components/policy/core/common/policy_types.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
@@ -131,6 +132,8 @@ class ConnectorsServiceReportingFeatureTest
 
   const char* pref() const { return ConnectorPref(connector()); }
 
+  const char* scope_pref() const { return ConnectorScopePref(connector()); }
+
   const char* pref_value() const {
     switch (policy_value()) {
       case 1:
@@ -156,8 +159,11 @@ class ConnectorsServiceReportingFeatureTest
 };
 
 TEST_P(ConnectorsServiceReportingFeatureTest, Test) {
-  if (policy_value() != 0)
+  if (policy_value() != 0) {
     profile_->GetPrefs()->Set(pref(), *base::JSONReader::Read(pref_value()));
+    profile_->GetPrefs()->SetInteger(scope_pref(),
+                                     policy::POLICY_SCOPE_MACHINE);
+  }
 
   auto settings = ConnectorsServiceFactory::GetForBrowserContext(profile_)
                       ->GetReportingSettings(connector());
