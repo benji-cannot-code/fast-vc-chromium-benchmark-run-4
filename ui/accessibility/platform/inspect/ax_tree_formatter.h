f1602c3bdce8b4e50fa3c2b46347f52afe439d87
Hans-Filip Elo <helo@vewd.com>
2020-12-17 19:54:05 +0000

    Missing includes in ax_tree_formatter.h
    The file ax_tree_formatter.h does not include <memory> and  <string>
    while they are used there - but still seems to compile with libc++. For
    libstdc++ - this does not compile.
    
    This patch adds the missing includes.
    
    AX-Relnotes: n/a.
    Bug: 957519
    Change-Id: I45f0a474b67c0b26729a24bc9028e75fbfa25729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2597306
    Reviewed-by: Joanmarie Diggs <jdiggs@igalia.com>
    Commit-Queue: Hans-Filip Elo <helo@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#838184}

--- ui/accessibility/platform/inspect/ax_tree_formatter.h
+++ ui/accessibility/platform/inspect/ax_tree_formatter.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_ACCESSIBILITY_PLATFORM_INSPECT_AX_TREE_FORMATTER_H_
 #define UI_ACCESSIBILITY_PLATFORM_INSPECT_AX_TREE_FORMATTER_H_
 
+#include <memory>
+#include <string>
 #include <vector>
 
 #include "ui/accessibility/platform/inspect/ax_inspect.h"
