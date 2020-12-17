c47cddb9c44798be52ca70a296fc4b805b453a76
Xiaocheng Hu <xiaochengh@chromium.org>
2020-12-17 04:28:12 +0000

    Mark non-tree-scoped ListStyleType::ApplyValue() as not reached
    The computed value of list-style-type should always be tree-scoped, so
    this patch marks the non-tree-scoped version not-reached for sanity.
    
    Bug: 687225
    Change-Id: I06e671caf4dced6a92cba90ad6e585aa1c0df2d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596032
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837923}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -4419,7 +4419,7 @@ void ListStyleType::ApplyInherit(StyleResolverState& state) const {
 
 void ListStyleType::ApplyValue(StyleResolverState& state,
                                const CSSValue& value) const {
-  ApplyValue(state, ScopedCSSValue(value, nullptr));
+  NOTREACHED();
 }
 
 void ListStyleType::ApplyValue(StyleResolverState& state,
