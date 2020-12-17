9362136f3d18d42bcc3f29096f35a0dded7aa8cf
danakj <danakj@chromium.org>
2020-12-17 17:42:01 +0000

    Reland: Prevent making invalid SkImageReps
    Original CL: https://chromium-review.googlesource.com/c/chromium/src/+/2572896
    
    Ensure that SkImageRep is always constructed with a valid bitmap.
    Instead of constructing an invalid SkImageRep then throwing it away when
    initializing an SkImage, we add an SkImage::CreateFromBitmap() that will
    just return a null SkImage directly if the bitmap is null/uninitialized.
    
    ImageSkia was creating a null ImageSkiaRep when searching for a
    representation in order to force use of the nearest one, but it also
    uses a boolean value to do the same, so stop making a null ImageSkiaRep.
    
    Then disallow null/empty ImageSkiaRep and also disallow their transport
    over mojo.
    
    Remove a few tests that were creating invalid ImageSkiaReps to test that
    they work - as they are no longer valid.
    
    Have ImageSkia constructed from ImageSkiaOperations check for the
    ImageSkiaRep that they get from their inner (non-null!) ImageSkia, as
    that can still return a null ImageSkiaRep. In that case, the resulting
    ImageSkiaRep must also be null. And we can do that without trying to
    perform operations on the (invalid, null) bitmap inside the null
    ImageSkiaRep.
    
    This fixes the tests included in the CL. We no longer try to serialize
    null or empty ImageSkiaRep since the class itself will not allow it. We
    do test that we will not deserialize such invalid ImageSkiaReps. And a
    test is also added to verify we do not allow non-n32 bitmaps to be
    deserialized in ImageSkiaRep.
    
    R=dcheng@chromium.org, sky@chromium.org
    
    Bug: 1155258
    Change-Id: Ia18360dcd2a59be8c49b7104f01dbdd20bbe6bbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596131
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838132}

--- ui/gfx/nine_image_painter_unittest.cc
+++ ui/gfx/nine_image_painter_unittest.cc
@@ -73,7 +73,7 @@ TEST(NineImagePainterTest, PaintHighDPI) {
 
   float image_scale = 2.f;
 
-  gfx::ImageSkia image(gfx::ImageSkiaRep(src, image_scale));
+  gfx::ImageSkia image = gfx::ImageSkia::CreateFromBitmap(src, image_scale);
   gfx::Insets insets(10, 10, 10, 10);
   gfx::NineImagePainter painter(image, insets);
 
@@ -105,7 +105,7 @@ TEST(NineImagePainterTest, PaintStaysInBounds) {
   src.eraseColor(SK_ColorGREEN);
   src.erase(SK_ColorRED, SkIRect::MakeXYWH(2, 2, 2, 2));
 
-  gfx::ImageSkia image(gfx::ImageSkiaRep(src, 0.0f));
+  gfx::ImageSkia image = gfx::ImageSkia::CreateFrom1xBitmap(src);
   gfx::Insets insets(2, 2, 2, 2);
   gfx::NineImagePainter painter(image, insets);
 
@@ -137,7 +137,7 @@ TEST(NineImagePainterTest, PaintWithBoundOffset) {
   src.eraseColor(SK_ColorRED);
   src.eraseArea(SkIRect::MakeXYWH(1, 1, 8, 8), SK_ColorGREEN);
 
-  gfx::ImageSkia image(gfx::ImageSkiaRep(src, 0.0f));
+  gfx::ImageSkia image = gfx::ImageSkia::CreateFrom1xBitmap(src);
   gfx::Insets insets(1, 1, 1, 1);
   gfx::NineImagePainter painter(image, insets);
 
@@ -169,7 +169,7 @@ TEST(NineImagePainterTest, PaintWithScale) {
 
   float image_scale = 2.f;
 
-  gfx::ImageSkia image(gfx::ImageSkiaRep(src, image_scale));
+  gfx::ImageSkia image = gfx::ImageSkia::CreateFromBitmap(src, image_scale);
   gfx::Insets insets(10, 10, 10, 10);
   gfx::NineImagePainter painter(image, insets);
 
@@ -200,7 +200,7 @@ TEST(NineImagePainterTest, PaintWithNegativeScale) {
 
   float image_scale = 2.f;
 
-  gfx::ImageSkia image(gfx::ImageSkiaRep(src, image_scale));
+  gfx::ImageSkia image = gfx::ImageSkia::CreateFromBitmap(src, image_scale);
   gfx::Insets insets(10, 10, 10, 10);
   gfx::NineImagePainter painter(image, insets);
 
