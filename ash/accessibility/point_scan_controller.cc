02ed9f02d3c82b0ba45186630d9362bc94fbed5b
Shannon Chen <shannc@chromium.org>
2020-12-17 07:16:12 +0000

    [Switch Access] Add padding to layer and fix animation speed
    Add padding to point scan layer so that both lines in range scanning
    have same line width. Adjust scanning speed so that range scanning has
    faster speed than line scanning.
    
    Signed-off-by: Shannon Chen <shannc@chromium.org>
    Bug: 1061537
    Change-Id: I90d8623bcdac226488804682ede16447f18270a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596021
    Commit-Queue: Shannon Chen <shannc@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837960}

--- ash/accessibility/point_scan_controller.cc
+++ ash/accessibility/point_scan_controller.cc
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 // Scanning time in seconds from the start of the screen (offset_start) to the
 // end of the screen (offset_bound).
-constexpr float kHorizontalScanTimeSecs = 20;
-constexpr float kVerticalScanTimeSecs = 20;
+constexpr float kHorizontalScanTimeSecs = 90;
+constexpr float kVerticalScanTimeSecs = 60;
 constexpr float kHorizontalRangeScanTimeSecs = 30;
-constexpr float kVerticalRangeScanTimeSecs = 20;
+constexpr float kVerticalRangeScanTimeSecs = 25;
 constexpr int kDefaultRangeWidthDips = 150;
 constexpr float kDefaultRangeHeightDips = 120;
 
