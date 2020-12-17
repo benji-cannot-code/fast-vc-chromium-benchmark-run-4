53fb85c0c07f776efdaa0ba07e4acc68706739dc
Xiaocheng Hu <xiaochengh@chromium.org>
2020-12-17 02:21:51 +0000

    Change ::content counter() and counters() to use custom counter styles
    This patch switches counter() and counters() to use @counter-style
    rules (including predefined ones) instead of relying on the deprecated
    EListStyleType enum.
    
    Note: The purpose of this patch is to ensure that there's no breakage
    after fully switching counter() and counters() to the new implementation
    for the predefined counter styles. Follow up patches will verify the
    new implementation against custom counter styles and apply fixes, if
    needed.
    
    Bug: 687225
    Change-Id: I981f55dfffff97feac5edfd42b1569fa40de47e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2591873
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#837878}

--- third_party/blink/renderer/core/css/style_engine.h
+++ third_party/blink/renderer/core/css/style_engine.h
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class CounterStyle;
 class CounterStyleMap;
 class CSSFontSelector;
 class CSSStyleSheet;
@@ -380,6 +381,8 @@ class CORE_EXPORT StyleEngine final : public GarbageCollected<StyleEngine>,
   StyleRuleScrollTimeline* FindScrollTimelineRule(const AtomicString& name);
 
   CounterStyleMap* GetUserCounterStyleMap() { return user_counter_style_map_; }
+  const CounterStyle& FindCounterStyleAcrossScopes(const AtomicString&,
+                                                   const TreeScope*) const;
 
   DocumentStyleEnvironmentVariables& EnsureEnvironmentVariables();
 
