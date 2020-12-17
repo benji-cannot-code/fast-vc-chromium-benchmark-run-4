71d8954e089b8c656d69bf929446119096869518
Sebastien Marchand <sebmarchand@chromium.org>
2020-12-17 15:41:16 +0000

    [PM] Don't emit negative freezing votes for visible tabs
    Visible tabs already can't be frozen (see
    https://source.chromium.org/chromium/chromium/src/+/master:content/browser/web_contents/web_contents_impl.cc;l=2656;drc=5d3dd257fdb048822291f570af44ab9116d73e14)
    and non-visible tabs are automatically unfrozen when they become
    visible.
    
    Bug: 1144025
    Change-Id: Ia77bf792324fb4df049026d1073de858b5ed1f3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595455
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838082}

--- chrome/browser/performance_manager/policies/page_freezing_policy.h
+++ chrome/browser/performance_manager/policies/page_freezing_policy.h
@@ -23,7 +23,6 @@ namespace policies {
 // freezing vote is positive.
 //
 // Tabs in one of the following states won't be frozen:
-//   - Visible;
 //   - Audible;
 //   - Capturing video;
 //   - Capturing audio;
@@ -34,6 +33,9 @@ namespace policies {
 //   - Connected to a USB device;
 //   - Holding at least one IndexedDB lock;
 //   - Holding at least one WebLock.
+//
+// Note that visible tabs can't be frozen and tabs that becomes visible are
+// automatically unfrozen, there's no need to track this feature here.
 class PageFreezingPolicy : public GraphOwned,
                            public PageNode::ObserverDefaultImpl,
                            public PageLiveStateObserver {
@@ -53,7 +55,6 @@ class PageFreezingPolicy : public GraphOwned,
  protected:
   // List of states that prevent a tab from being frozen.
   enum class CannotFreezeReason {
-    kVisible,
     kAudible,
     kHoldingIndexedDBLock,
     kHoldingWebLock,
@@ -91,7 +92,6 @@ class PageFreezingPolicy : public GraphOwned,
   // PageNodeObserver implementation:
   void OnPageNodeAdded(const PageNode* page_node) override;
   void OnBeforePageNodeRemoved(const PageNode* page_node) override;
-  void OnIsVisibleChanged(const PageNode* page_node) override;
   void OnIsAudibleChanged(const PageNode* page_node) override;
   void OnPageIsHoldingWebLockChanged(const PageNode* page_node) override;
   void OnPageIsHoldingIndexedDBLockChanged(const PageNode* page_node) override;
