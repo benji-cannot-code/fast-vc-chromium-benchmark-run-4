dfbfdfa8f1e366885f46a7ed40b2793bed17098d
Yuheng Huang <yuhengh@chromium.org>
2020-12-17 19:09:19 +0000

    Tab Search: schedule benchmark on win-10-perf and win-10_laptop_low_end
    Schedule tab search benchmark on Windows 10 since
    most of the failures are fixed.
    
    Bug: 1099917
    Change-Id: I806ac094647f54b0b1bbf4c45b686dd39fa77ef1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595983
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838168}

--- tools/perf/core/bot_platforms.py
+++ tools/perf/core/bot_platforms.py
@@ -351,7 +351,6 @@ _MAC_M1_MINI_2020_BENCHMARK_CONFIGS = PerfSuite([
 
 _WIN_10_BENCHMARK_CONFIGS = PerfSuite(OFFICIAL_BENCHMARK_CONFIGS).Remove([
     'blink_perf.display_locking',
-    'tab_search',
     'v8.runtime_stats.top_25',
 ])
 _WIN_10_EXECUTABLE_CONFIGS = frozenset([
@@ -364,7 +363,6 @@ _WIN_10_EXECUTABLE_CONFIGS = frozenset([
 _WIN_10_LOW_END_BENCHMARK_CONFIGS = PerfSuite(
     OFFICIAL_BENCHMARK_CONFIGS).Remove([
         'blink_perf.display_locking',
-        'tab_search',
     ])
 _WIN_10_LOW_END_HP_CANDIDATE_BENCHMARK_CONFIGS = PerfSuite([
     _GetBenchmarkConfig('v8.browsing_desktop'),
