c82ac4719dda13bbd2c575fd187c2231ec479a62
Xianzhu Wang <wangxianzhu@chromium.org>
2020-12-17 23:52:20 +0000

    Skip unnecessary PaintLayer::ExpandRectForStackingChildren()
    Rename it to ExpandrectForSelfPaintingDescendants() because it doesn't
    only expand for stacking children, but all self-painting descendants
    whose bounds are not included in (clipped) LocalBoundingBox().
    
    Skip it if the layer's layout object is a box, and
    - if it doesn't have self-painting layer descendants;
    - or, if it clips overflow and all descendants are contained.
    
    Bug: 1154746
    Change-Id: If7b392ad836d01aa1e3a5b9be1a12962980c025a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596009
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838316}

--- third_party/blink/renderer/core/paint/paint_layer.h
+++ third_party/blink/renderer/core/paint/paint_layer.h
@@ -464,7 +464,7 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
 
   enum CalculateBoundsOptions {
     // Include clips between this layer and its ancestor layer (inclusive).
-    kIncludeClips = 0x1,
+    kIncludeAncestorClips = 0x1,
     // Include transforms, irrespective of if they are applied via composition
     // or painting.
     kIncludeTransforms = 0x2,
@@ -1161,6 +1161,7 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
 
   // Bounding box in the coordinates of this layer.
   PhysicalRect LocalBoundingBox() const;
+  PhysicalRect ClippedLocalBoundingBox(const PaintLayer& ancestor_layer) const;
 
   bool HasOverflowControls() const;
 
@@ -1291,9 +1292,9 @@ class CORE_EXPORT PaintLayer : public DisplayItemClient {
 
   bool IsTopMostNotAffectedByScrollOf(const PaintLayer* ancestor) const;
 
-  void ExpandRectForStackingChildren(const PaintLayer& composited_layer,
-                                     PhysicalRect& result,
-                                     unsigned options) const;
+  void ExpandRectForSelfPaintingDescendants(const PaintLayer& composited_layer,
+                                            PhysicalRect& result,
+                                            unsigned options) const;
 
   // The return value is in the space of |stackingParent|, if non-null, or
   // |this| otherwise.
