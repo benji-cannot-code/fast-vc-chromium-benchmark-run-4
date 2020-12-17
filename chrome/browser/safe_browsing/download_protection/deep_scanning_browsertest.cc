7cad298c5d43a5c67083473b82d2782ba1ac5fb5
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-12-17 21:06:03 +0000

    Make BinaryUploadService authentication code DMToken-keyed
    This is a necessary step into getting per-profile Connectors to work, as
    some configurations could use different tokens per Connector when some
    are set at the machine scope while others are set at the profile scope.
    
    This also updates |authorization_callbacks_| and
    |pending_validate_data_upload_request_| to use the same mapping pattern
    as |can_upload_enterprise_data_|, this is actually a bug in how that
    was updated to map to Connectors in crrev.com/c/2499082
    
    Bug: 1148789
    Change-Id: Ib216dbfba5ade900c035513b70f2d07176bb87bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593761
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838225}

--- chrome/browser/safe_browsing/download_protection/deep_scanning_browsertest.cc
+++ chrome/browser/safe_browsing/download_protection/deep_scanning_browsertest.cc
@@ -286,7 +286,7 @@ class DownloadDeepScanningBrowserTest
 
   void AuthorizeForDeepScanning() {
     BinaryUploadServiceFactory::GetForProfile(browser()->profile())
-        ->SetAuthForTesting(/*authorized=*/true);
+        ->SetAuthForTesting("dm_token", /*authorized=*/true);
   }
 
  private:
