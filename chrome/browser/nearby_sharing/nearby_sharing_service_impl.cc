252e6d92ebda2459cda85f0752c18ebed74e71bf
Josh Nohle <nohle@google.com>
2020-12-18 01:06:00 +0000

    [Nearby] Adjust high-level transfer metrics to reflect CUJs
    This CL combines the Nearby.Share.Transfer.CompletionStatus and
    Nearby.Share.Transfer.CompletionStatus.NotCompletedReason into
    Nearby.Share.Transfer.FinalStatus[...]. With all of the status codes
    present in a single histogram, we can better compute a success metric
    using a formula, for example, Completed/(Total - Cancelled - Rejected -
    Timeout).  We also add slicing by whether or not the transfer is using
    contact-based sharing. These revisions better align us with the CUJs
    outlined at go/cros-nearby-cujs-sheet.
    
    Manually verified metrics for different sharing scenarios.
    
    Bug: 1105579
    Change-Id: Ia302a014298cefc8f35ea7eb570212708e38ec3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2597750
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838344}

--- chrome/browser/nearby_sharing/nearby_sharing_service_impl.cc
+++ chrome/browser/nearby_sharing/nearby_sharing_service_impl.cc
@@ -2465,8 +2465,9 @@ void NearbySharingServiceImpl::OnIncomingTransferUpdate(
   }
 
   if (metadata.is_final_status()) {
-    RecordNearbyShareTransferCompletionStatusMetric(
-        /*is_incoming=*/true, share_target.type, metadata.status());
+    RecordNearbyShareTransferFinalStatusMetric(
+        /*is_incoming=*/true, share_target.type, metadata.status(),
+        share_target.is_known);
     OnTransferComplete();
     if (metadata.status() != TransferMetadata::Status::kComplete) {
       // For any type of failure, lets make sure any pending files get cleaned
@@ -2501,8 +2502,9 @@ void NearbySharingServiceImpl::OnOutgoingTransferUpdate(
 
   if (metadata.is_final_status()) {
     is_connecting_ = false;
-    RecordNearbyShareTransferCompletionStatusMetric(
-        /*is_incoming=*/false, share_target.type, metadata.status());
+    RecordNearbyShareTransferFinalStatusMetric(
+        /*is_incoming=*/false, share_target.type, metadata.status(),
+        share_target.is_known);
     OnTransferComplete();
   } else if (metadata.status() == TransferMetadata::Status::kMediaDownloading ||
              metadata.status() ==
