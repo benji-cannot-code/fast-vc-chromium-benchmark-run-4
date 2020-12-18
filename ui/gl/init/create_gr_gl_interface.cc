90d57a3a59c3869e51f6286b2bf7f08c98234224
Sunny Sachanandani <sunnyps@chromium.org>
2020-12-18 01:22:34 +0000

    gpu: Skip flush in Skia GL bindings on ARM Mac
    Flushing isn't needed on ARM Macs and consumes significant CPU on some
    benchmarks (see bug).  Flushes were removed in command buffer in
    https://crrev.com/c/2447044, but Skia GL bindings also contain similar
    flush workaround that wasn't removed.  This CL does that.
    
    Bug: 1159599
    Change-Id: I8b6217a8ecb2562b8768d4f85560d889f3e2b12d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2597816
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838353}

--- ui/gl/init/create_gr_gl_interface.cc
+++ ui/gl/init/create_gr_gl_interface.cc
@@ -14,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_version_info.h"
 #include "ui/gl/progress_reporter.h"
 
+#if defined(OS_APPLE)
+#include "base/mac/mac_util.h"
+#endif
+
 namespace gl {
 namespace init {
 
@@ -160,13 +164,17 @@ GrGLFunction<R GR_GL_FUNCTION_TYPE(Args...)> bind_with_flush_on_mac(
     // Conditional may be optimized out because droppable_call is set at compile
     // time.
     if (!droppable_call || !HasInitializedNullDrawGLBindings()) {
-      {
+      // If running on Apple silicon, regardless of the architecture, disable
+      // this workaround.  See https://crbug.com/1131312.
+      static const bool needs_flush =
+          base::mac::GetCPUType() == base::mac::CPUType::kIntel;
+      if (needs_flush) {
         TRACE_EVENT0(
             "gpu", "CreateGrGLInterface - bind_with_flush_on_mac - beforefunc")
         glFlush();
       }
       func(args...);
-      {
+      if (needs_flush) {
         TRACE_EVENT0("gpu",
                      "CreateGrGLInterface - bind_with_flush_on_mac - afterfunc")
         glFlush();
