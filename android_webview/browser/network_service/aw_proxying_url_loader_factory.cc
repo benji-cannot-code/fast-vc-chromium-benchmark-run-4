0a37f534680da62ac969628ed7ba1215844d4e2e
Julie Jeongeun Kim <jkim@igalia.com>
2020-12-18 08:20:42 +0000

    Deprecate ResourceType in //android_webview
    This CL replaces blink::mojom::ResourceType with
    network::mojom::RequestDestination in //android_webview since
    blink::mojom::ResourceType is deprecated[1].
    
    [1] https://crbug.com/960143
    
    Bug: 1059639
    Change-Id: I9bb4817971029420ac30abfd52075f7df2f52edc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596909
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#838432}

--- android_webview/browser/network_service/aw_proxying_url_loader_factory.cc
+++ android_webview/browser/network_service/aw_proxying_url_loader_factory.cc
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_flags.h"
 #include "net/http/http_util.h"
 #include "services/network/public/cpp/resource_request.h"
-#include "third_party/blink/public/mojom/loader/resource_load_info.mojom-shared.h"
+#include "services/network/public/mojom/fetch_api.mojom.h"
 #include "url/gurl.h"
 
 namespace android_webview {
@@ -512,8 +512,7 @@ void InterceptedRequest::OnReceiveResponse(
                        std::move(error_info)));
   }
 
-  if (request_.resource_type ==
-      static_cast<int>(blink::mojom::ResourceType::kMainFrame)) {
+  if (request_.destination == network::mojom::RequestDestination::kDocument) {
     // Check for x-auto-login-header
     HeaderData header_data;
     std::string header_string;
