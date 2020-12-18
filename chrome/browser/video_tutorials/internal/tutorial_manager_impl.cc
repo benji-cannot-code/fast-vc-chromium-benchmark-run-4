53c8dcc6b839872e70bb0cf73eaf93d1d091b75b
Shakti Sahu <shaktisahu@chromium.org>
2020-12-18 03:21:42 +0000

    Video Tutorials : Making summary card a server driven tutorial
    Summary card is shown on NTP after all other tutorials have been clicked
    or dismissed. Currently we use finch param to get the resources to
    create this card. A better solution is to get it from the server as a
    tutorial. The backend will filter it out on a GetTutorials() call.
    
    Bug: 1159249
    Change-Id: I71f78826535a3d13fad41b027aa6e2c645ab3a13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2594442
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838399}

--- chrome/browser/video_tutorials/internal/tutorial_manager_impl.cc
+++ chrome/browser/video_tutorials/internal/tutorial_manager_impl.cc
@@ -14,6 +14,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 
 namespace video_tutorials {
+namespace {
+
+std::vector<Tutorial> FilterTutorials(const std::vector<Tutorial>& tutorials) {
+  std::vector<Tutorial> tutorials_excluding_summary;
+  for (const auto& tutorial : tutorials) {
+    if (tutorial.feature == FeatureType::kSummary)
+      continue;
+    tutorials_excluding_summary.emplace_back(tutorial);
+  }
+
+  return tutorials_excluding_summary;
+}
+
+}  // namespace
 
 TutorialManagerImpl::TutorialManagerImpl(std::unique_ptr<TutorialStore> store,
                                          PrefService* prefs)
@@ -44,7 +58,7 @@ void TutorialManagerImpl::GetTutorials(GetTutorialsCallback callback) {
                            ? preferred_locale.value()
                            : Config::GetDefaultPreferredLocale();
   if (tutorial_group_.has_value() && tutorial_group_->language == locale) {
-    std::move(callback).Run(tutorial_group_->tutorials);
+    std::move(callback).Run(FilterTutorials(tutorial_group_->tutorials));
     return;
   }
 
@@ -118,7 +132,7 @@ void TutorialManagerImpl::OnTutorialsLoaded(
   // We are loading tutorials only for the preferred locale.
   DCHECK(loaded_groups->size() == 1u);
   tutorial_group_ = loaded_groups->front();
-  std::move(callback).Run(tutorial_group_->tutorials);
+  std::move(callback).Run(FilterTutorials(tutorial_group_->tutorials));
 }
 
 void TutorialManagerImpl::SaveGroups(
