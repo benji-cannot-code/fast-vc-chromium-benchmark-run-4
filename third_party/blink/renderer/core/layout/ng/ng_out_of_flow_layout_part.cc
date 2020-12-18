6a94f73526e6d5cac2b8031f31a388d0de38e0b4
Alison Maher <almaher@microsoft.com>
2020-12-18 19:25:32 +0000

    [LayoutNG] Multi-column containing blocks
    Currently, when a multi-column is a containing block for an OOF
    positioned element, the corresponding column is mistaken for the
    containing block, which causes various DCHECK failures. Columns are not
    allowed to be containing blocks, so this change ensures that we skip
    over columns when placing OOF positioned elements.
    
    Bug: 1158706
    Change-Id: Id969b91a8f472d34b4767ffe891a3405770e8443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2597769
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838476}

--- third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.cc
+++ third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.cc
@@ -1009,6 +1009,10 @@ scoped_refptr<const NGLayoutResult> NGOutOfFlowLayoutPart::Layout(
 
 bool NGOutOfFlowLayoutPart::IsContainingBlockForCandidate(
     const NGLogicalOutOfFlowPositionedNode& candidate) {
+  // Column boxes are not allowed to be containing blocks.
+  if (container_builder_->IsFragmentainerBoxType())
+    return false;
+
   EPosition position = candidate.node.Style().GetPosition();
 
   // Candidates whose containing block is inline are always positioned inside
