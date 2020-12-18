15e2c1c0d7afd1a14fb4423f9698dc3018d059f1
Gyuyoung Kim <gyuyoung@igalia.com>
2020-12-18 08:19:12 +0000

    Convert AwViewHostMsg_ShouldOverrideUrlLoading to Mojo
    This CL migrates AwViewHostMsg_ShouldOverrideUrlLoading
    to FrameHost interface. This CL moves the handler of
    the existing IPC message from AwContentsMessageFilter
    to AwRenderViewHostExt.
    
    Bug: 1157131
    Change-Id: I29ab375d5502ab3af465ffcca769dfb3980d5843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2592534
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#838431}

--- android_webview/renderer/aw_content_renderer_client.cc
+++ android_webview/renderer/aw_content_renderer_client.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_thread.h"
 #include "content/public/renderer/render_view.h"
 #include "mojo/public/cpp/bindings/binder_map.h"
+#include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/platform/web_url.h"
@@ -138,10 +139,12 @@ bool AwContentRendererClient::HandleNavigation(
   base::string16 url = request.Url().GetString().Utf16();
   bool has_user_gesture = request.HasUserGesture();
 
-  int render_frame_id = render_frame->GetRoutingID();
-  RenderThread::Get()->Send(new AwViewHostMsg_ShouldOverrideUrlLoading(
-      render_frame_id, url, has_user_gesture, is_redirect, is_main_frame,
-      &ignore_navigation));
+  mojo::AssociatedRemote<mojom::FrameHost> frame_host_remote;
+  render_frame->GetRemoteAssociatedInterfaces()->GetInterface(
+      &frame_host_remote);
+  frame_host_remote->ShouldOverrideUrlLoading(
+      url, has_user_gesture, is_redirect, is_main_frame, &ignore_navigation);
+
   return ignore_navigation;
 }
 
