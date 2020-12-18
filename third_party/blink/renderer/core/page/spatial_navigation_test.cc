0b6f3fda1e8365b6f8e1ff8deb240d33b50c32b2
Morten Stenshorne <mstensho@chromium.org>
2020-12-18 11:30:06 +0000

    Disable 3 "recent" SpatialNavigationTest tests when NG is disabled.
    These have been failing on linux_layout_tests_layout_ng_disabled ever
    since they got added in September:
    https://chromium-review.googlesource.com/c/chromium/src/+/2410039
    
    TBR=fs@opera.com
    
    Bug: 1160211
    Change-Id: Ia77ca4e42c9576ebc4a8bb2bb9b0041700aa4780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2597580
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838443}

--- third_party/blink/renderer/core/page/spatial_navigation_test.cc
+++ third_party/blink/renderer/core/page/spatial_navigation_test.cc
@@ -785,6 +785,9 @@ TEST_F(SpatialNavigationTest, InlineImageLinkWithLineHeight) {
 }
 
 TEST_F(SpatialNavigationTest, InlineImageTextLinkWithLineHeight) {
+  // Fails when LayoutNG is disabled. See crbug.com/1160211
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   LoadAhem();
   SetBodyInnerHTML(
       "<!DOCTYPE html>"
@@ -810,6 +813,9 @@ TEST_F(SpatialNavigationTest, InlineImageTextLinkWithLineHeight) {
 }
 
 TEST_F(SpatialNavigationTest, InlineLinkWithInnerBlock) {
+  // Fails when LayoutNG is disabled. See crbug.com/1160211
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   LoadAhem();
   SetBodyInnerHTML(
       "<!DOCTYPE html>"
@@ -955,6 +961,9 @@ TEST_F(SpatialNavigationTest, NormalizedLineBrokenLink) {
 }
 
 TEST_F(SpatialNavigationTest, NormalizedLineBrokenLinkWithImg) {
+  // Fails when LayoutNG is disabled. See crbug.com/1160211
+  if (!RuntimeEnabledFeatures::LayoutNGEnabled())
+    return;
   LoadAhem();
   SetBodyInnerHTML(
       "<!DOCTYPE html>"
