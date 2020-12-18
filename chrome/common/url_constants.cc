81c42319ace08e7ea1ace24aea08e22b70451d98
Regan Hsu <hsuregan@chromium.org>
2020-12-18 14:19:39 +0000

    [CrOS PhoneHub] Add task continuation disabled summary and toggle.
    When 'Open Tabs' sync type is disabled, the summary below the
    task continuation toggle will tell the user to turn on Chrome
    sync and the toggle will be disabled.
    
    Screenshot:
    https://screenshot.googleplex.com/6bHapjPrYypBWWV
    
    Fixed: 1156302
    Bug: 1106937
    Change-Id: I74bd4de355ef166b797d6eebf6ab1ee90a166f08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2597745
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838453}

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -399,6 +399,9 @@ const char kOemEulaURLPath[] = "oem";
 const char kOnlineEulaURLPath[] =
     "https://policies.google.com/terms/embedded?hl=%s";
 
+const char kPhoneHubLearnMoreLink[] =
+    "https://support.google.com/chromebook?p=phone_hub";
+
 const char kAdditionalToSOnlineURLPath[] =
     "https://www.google.com/intl/%s/chrome/terms/";
 
--- chrome/test/data/webui/settings/chromeos/multidevice_task_continuation_disabled_link_tests.js
+++ chrome/test/data/webui/settings/chromeos/multidevice_task_continuation_disabled_link_tests.js
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import 'chrome://os-settings/chromeos/os_settings.js';
 
 // #import {Router, routes} from 'chrome://os-settings/chromeos/os_settings.js';
-// #import {assertEquals, assertTrue} from '../../chai_assert.js';
+// #import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
+// #import {assert} from 'chrome://resources/js/assert.m.js';
 // #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 // clang-format on
 
@@ -16,8 +17,8 @@ suite('Multidevice', function() {
 
   setup(function() {
     PolymerTest.clearBody();
-    localizedLink =
-        document.createElement('settings-multidevice-wifi-sync-disabled-link');
+    localizedLink = document.createElement(
+        'settings-multidevice-task-continuation-disabled-link');
     document.body.appendChild(localizedLink);
     Polymer.dom.flush();
   });
@@ -46,13 +47,18 @@ suite('Multidevice', function() {
   test('ChromeSyncLink navigates to appropriate route', async () => {
     const chromeSyncLink = localizedLink.$$('#chromeSyncLink');
     chromeSyncLink.click();
-    Polymer.dom.flush();
 
     if (loadTimeData.getBoolean('splitSettingsSyncEnabled')) {
-      assertEquals(
+      await test_util.eventToPromise(
+          'opened-browser-advanced-sync-setting', localizedLink);
+      assertNotEquals(
           settings.Router.getInstance().getCurrentRoute(),
           settings.routes.OS_SYNC);
+      assertNotEquals(
+          settings.Router.getInstance().getCurrentRoute(),
+          settings.routes.SYNC_ADVANCED);
     } else {
+      Polymer.dom.flush();
       assertEquals(
           settings.Router.getInstance().getCurrentRoute(),
           settings.routes.SYNC_ADVANCED);
