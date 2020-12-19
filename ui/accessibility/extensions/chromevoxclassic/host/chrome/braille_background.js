972e395a1a5ef6e276eae3d2611f931d2c09c4c2
David Tseng <dtseng@google.com>
2020-12-19 01:57:39 +0000

    Updates to chromevoxclassic
    TBR=dtseng@chromium.org
    
    Change-Id: I806e8031d315948107885d16f0b58f0430780c1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2598756
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838494}

--- ui/accessibility/extensions/chromevoxclassic/host/chrome/braille_background.js
+++ ui/accessibility/extensions/chromevoxclassic/host/chrome/braille_background.js
@@ -64,7 +64,7 @@ cvox.BrailleBackground = function(opt_displayManagerForTest,
 
 /** @override */
 cvox.BrailleBackground.prototype.write = function(params) {
-  this.setContent_(params, null);
+  // Stubbed out.
 };
 
 
