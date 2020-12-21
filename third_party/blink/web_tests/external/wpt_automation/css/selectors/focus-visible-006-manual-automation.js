6bd0f32d06af6815f60f5f53501b0f4595f8154b
Finnur Thorarinsson <finnur@chromium.org>
2020-12-21 12:41:13 +0000

    Revert "Simulate user inputs in focus-visible tests in css/selectors/"
    This reverts commit 9d0120f3d75be146445be64e770063d03f3049c7.
    
    Reason for revert: 
    
    Very close to 100% failure rate since check-in, on bot "WebKit Linux Leak":
    https://ci.chromium.org/p/chromium/builders/ci/WebKit%20Linux%20Leak/21884
    
    Error message:
    
    Retrying 19 unexpected failures, attempt 1 of 3...
    Sharding tests ...
    Starting 8 workers ...
    [18/19] external/wpt/css/selectors/focus-visible-006.html failed unexpectedly (leak detected: ({"numberOfLiveContextLifecycleStateObservers":[2,4],"numberOfLiveDocuments":[1,2],"numberOfLiveNodes":[4,40],"numberOfLiveResourceFetchers":[1,2],"numberOfLiveResources":[0,5]}))
    stopping content_shell(pid 14917) timed out, killing it
    [19/19] external/wpt/referrer-policy/gen/worker-classic.http-rp/strict-origin-when-cross-origin/worker-classic.http.html failed unexpectedly (test timed out)
    
    
    
    Original change's description:
    > Simulate user inputs in focus-visible tests in css/selectors/
    >
    > Use testdriver Action API to simulate mouse actions in
    > css/selectors/focus-visible-006.html and
    > css/selectors/focus-visible-008.html.
    >
    > Bug: 1145677
    > Change-Id: Id25c6120f1dbd489d4c803ee979e67ad5ef49f8c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551818
    > Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    > Commit-Queue: Lan Wei <lanwei@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#838271}
    
    TBR=lanwei@chromium.org,mustaq@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1145677
    Change-Id: Ie2e39aa960a80dd0ab5b9ec881ae9305c4294760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2599530
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838637}

---
+++ third_party/blink/web_tests/external/wpt_automation/css/selectors/focus-visible-006-manual-automation.js
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importAutomationScript('/pointerevents/pointerevent_common_input.js');
+
+function inject_input() {
+  return mouseClickInTarget("#el");
+};
