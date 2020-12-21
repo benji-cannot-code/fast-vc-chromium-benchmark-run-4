4cbeb34603073e3588839f4f306e1f7f138aa248
Nidhi Jaju <nidhijaju@google.com>
2020-12-21 05:46:52 +0000

    Update Interface Object test expectations for Exposed Streams interfaces
    Following https://chromium-review.googlesource.com/c/chromium/src/+/2570755,
    this CL updates the interface object WPT expectations to remove Streams
    API interface classes that have already been exposed.
    
    Bug: 1093862
    Change-Id: Ibdce3585a8b34b3aeef40e3fae610af733ebdc79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595553
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@google.com>
    Cr-Commit-Position: refs/heads/master@{#838599}

--- third_party/blink/web_tests/external/wpt/workers/semantics/interface-objects/002.worker.js
+++ third_party/blink/web_tests/external/wpt/workers/semantics/interface-objects/002.worker.js
@@ -21,10 +21,6 @@ var unexpected = [
   // https://dom.spec.whatwg.org/
   "DOMImplementation",
   // https://streams.spec.whatwg.org/
-  "ReadableStreamDefaultReader",
-  "ReadableStreamBYOBReader",
-  "ReadableByteStreamController",
-  "WritableStreamDefaultWriter",
   "WritableStreamDefaultController",
   // http://w3c.github.io/IndexedDB/
   "IDBEnvironment",
