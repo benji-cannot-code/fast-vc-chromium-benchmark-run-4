c7ed00ed36cad14ea35eff6f4bdd320d95309834
Bao-Duy Tran <tranbaoduy@chromium.org>
2020-12-21 02:54:20 +0000

    Remove unnecessary "type" field in all VK+IME extension manifests.
    This field is never used anywhere, other than asserting that it must
    always carry value "ime" (!). All parsing and validation is unnecessary.
    The field's existence is unnecessary and may hinder the flexibility for
    future work. It'd better be gone in the spirit of YAGNI "you aren't
    gonna need it". Note that the specs for these manifests have never been
    formally specified or published anywhere. Also, this cleanup is fully
    backward compat even for 3P, because the field is never really used.
    
    Bug: 1134465
    Change-Id: Ied6e1ad30a9ab6f08091a7d31a9c3f3124989a8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2592072
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838589}

--- chrome/common/extensions/api/input_ime/input_components_handler.h
+++ chrome/common/extensions/api/input_ime/input_components_handler.h
@@ -19,12 +19,6 @@ namespace extensions {
 
 class Extension;
 
-enum InputComponentType {
-  INPUT_COMPONENT_TYPE_NONE = -1,
-  INPUT_COMPONENT_TYPE_IME,
-  INPUT_COMPONENT_TYPE_COUNT
-};
-
 struct InputComponentInfo {
   // Define out of line constructor/destructor to please Clang.
   InputComponentInfo();
@@ -32,7 +26,6 @@ struct InputComponentInfo {
   ~InputComponentInfo();
 
   std::string name;
-  InputComponentType type;
   std::string id;
   std::set<std::string> languages;
   std::set<std::string> layouts;
