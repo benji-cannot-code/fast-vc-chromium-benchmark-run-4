54e749a5f0ab6fd7d0cc9ec41341ee2d4453a990
mark a. foltz <mfoltz@chromium.org>
2020-12-21 21:15:33 +0000

    [Media Router] Ensure MediaRouterEnabled() does not change per-profile.
    A runtime change to the kMediaRouterEnabled policy (managed preference) results
    in undefined behavior and crashes.  This is a workaround to ensure that
    MediaRouterEnabled() returns the same value for the lifetime of a profile.
    
    Fixing the Media Router to be enabled/disabled dynamically is possible, but will
    be blocked on extension removal (specifically the MediaRouteProvider mojo
    interface).
    
    Tested by manually enabling/disabling the policy while running Chrome and
    ensuring no crashes or partially-enabled UI states happen.
    
    Bug: 1076831
    Change-Id: I966a58196f92be40aed8771282c27f762102da6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2597752
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838705}

--- chrome/browser/media/router/media_router_feature.cc
+++ chrome/browser/media/router/media_router_feature.cc
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/media/router/media_router_feature.h"
 
+#include <utility>
+
 #include "base/base64.h"
+#include "base/containers/flat_map.h"
 #include "base/feature_list.h"
+#include "base/no_destructor.h"
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
@@ -67,11 +71,22 @@ bool MediaRouterEnabled(content::BrowserContext* context) {
 #endif  // !defined(OFFICIAL_BUILD) && !defined(OS_ANDROID)
 
 #if defined(OS_ANDROID) || BUILDFLAG(ENABLE_EXTENSIONS)
+  static base::NoDestructor<base::flat_map<content::BrowserContext*, bool>>
+      stored_pref_values;
+
+  // If the Media Router was already enabled or disabled for |context|, then it
+  // must remain so.  The Media Router does not support dynamic
+  // enabling/disabling.
+  auto const it = stored_pref_values->find(context);
+  if (it != stored_pref_values->end())
+    return it->second;
+
+  // Check the enterprise policy.
   const PrefService::Preference* pref = GetMediaRouterPref(context);
-  // Only use the pref value if it set from a mandatory policy.
   if (pref->IsManaged() && !pref->IsDefaultValue()) {
-    bool allowed = false;
+    bool allowed;
     CHECK(pref->GetValue()->GetAsBoolean(&allowed));
+    stored_pref_values->insert(std::make_pair(context, allowed));
     return allowed;
   }
 
