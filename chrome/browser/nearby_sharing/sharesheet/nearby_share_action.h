acc54fd4e65f91317a656ce5d95144eb6bf2a308
Michael Hansen <hansenmichael@google.com>
2020-12-21 22:47:54 +0000

    [Nearby] Fix keyboard events in sharesheet UI.
    This updates NearbyShareAction to implement content::WebContentsDelegate
    so that we can handle keyboard events. This was preventing users from
    opening the feedback window.
    
    This change also removes the need to subclass views::WebView, so
    NearbyShareWebView is deleted. I confirmed via manual testing that links
    are still working.
    
    WebViewExample was used as a reference:
    https://source.chromium.org/chromium/chromium/src/+/master:ui/views/examples/webview_example.h;drc=d5caa87ad320cc8ceb307f9cc525b7d4f9604dd1
    
    Fixed: 1133817
    Change-Id: Id9122ac0a92229a1ebbff66232d31d78ef2ddfcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2597900
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838735}

--- chrome/browser/nearby_sharing/sharesheet/nearby_share_action.h
+++ chrome/browser/nearby_sharing/sharesheet/nearby_share_action.h
@@ -8,9 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/sharesheet/share_action.h"
 #include "chrome/browser/ui/webui/nearby_share/nearby_share_dialog_ui.h"
+#include "content/public/browser/web_contents_delegate.h"
+#include "ui/views/controls/webview/unhandled_keyboard_event_handler.h"
+
+namespace views {
+class WebView;
+}  // namespace views
 
 class NearbyShareAction : public sharesheet::ShareAction,
-                          nearby_share::NearbyShareDialogUI::Observer {
+                          nearby_share::NearbyShareDialogUI::Observer,
+                          content::WebContentsDelegate {
  public:
   NearbyShareAction();
   ~NearbyShareAction() override;
@@ -30,9 +37,22 @@ class NearbyShareAction : public sharesheet::ShareAction,
   // nearby_share::NearbyShareDialogUI::Observer:
   void OnClose() override;
 
+  // content::WebContentsDelegate:
+  bool HandleKeyboardEvent(
+      content::WebContents* source,
+      const content::NativeWebKeyboardEvent& event) override;
+  void WebContentsCreated(content::WebContents* source_contents,
+                          int opener_render_process_id,
+                          int opener_render_frame_id,
+                          const std::string& frame_name,
+                          const GURL& target_url,
+                          content::WebContents* new_contents) override;
+
  private:
   sharesheet::SharesheetController* controller_ = nullptr;
   nearby_share::NearbyShareDialogUI* nearby_ui_ = nullptr;
+  views::WebView* web_view_;
+  views::UnhandledKeyboardEventHandler unhandled_keyboard_event_handler_;
 };
 
 #endif  // CHROME_BROWSER_NEARBY_SHARING_SHARESHEET_NEARBY_SHARE_ACTION_H_
