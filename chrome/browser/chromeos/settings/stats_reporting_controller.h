f2eeac30bc881de8e418cd33a8054205278b7a31
Yunke Zhou <yunkez@google.com>
2020-12-21 07:07:58 +0000

    Oobe: Fix issue with metrics client id reset after sign in
    Issue is due to there's a small time difference between StatsReportingController::SetWithService and pref is properly set. We should only propagate the value from service until it is correctly set.
    
    Bug: 1154947
    Change-Id: I977249dd25e1402246b383401c9deef4ea8ebc56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587065
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Cr-Commit-Position: refs/heads/master@{#838606}

--- chrome/browser/chromeos/settings/stats_reporting_controller.h
+++ chrome/browser/chromeos/settings/stats_reporting_controller.h
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/sequence_checker.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/chromeos/settings/device_settings_service.h"
+#include "components/ownership/owner_settings_service.h"
 
 class PrefRegistrySimple;
 class PrefService;
@@ -40,7 +42,8 @@ namespace chromeos {
 // IsEnabled will return the pending value until ownership is taken and the
 // pending value is written - from then on it will return the signed, stored
 // value from CrosSettings.
-class StatsReportingController {
+class StatsReportingController
+    : public ownership::OwnerSettingsService::Observer {
  public:
   // Manage singleton instance.
   static void Initialize(PrefService* local_state);
@@ -69,11 +72,14 @@ class StatsReportingController {
   // ownership.
   void OnOwnershipTaken(ownership::OwnerSettingsService* service);
 
+  // ownership::OwnerSettingsService::Observer implementation:
+  void OnSignedPolicyStored(bool success) override;
+
  private:
   friend class StatsReportingControllerTest;
 
   explicit StatsReportingController(PrefService* local_state);
-  ~StatsReportingController();
+  ~StatsReportingController() override;
 
   // Delegates immediately to SetWithService if |service| is ready, otherwise
   // runs SetWithService asynchronously once |service| is ready.
@@ -125,6 +131,17 @@ class StatsReportingController {
   bool value_notified_to_observers_;
   base::CallbackList<void(void)> callback_list_;
   base::CallbackListSubscription setting_subscription_;
+
+  // Indicates if the setting value is in the process of being set with the
+  // service. There is a small period of time needed between start saving the
+  // value and before the value is stored correctly in the service. We should
+  // not use the setting value from the service if it is still in the process
+  // of being saved.
+  bool is_value_being_set_with_service_ = false;
+
+  base::ScopedObservation<ownership::OwnerSettingsService,
+                          ownership::OwnerSettingsService::Observer>
+      owner_settings_service_observation_{this};
 
   base::WeakPtrFactory<StatsReportingController> weak_factory_{this};
 
