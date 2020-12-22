a5767586e6d2ca829419f1f78bc9802fa76762e7
Jao-ke Chin-Lee <jchinlee@chromium.org>
2020-12-22 23:13:51 +0000

    [media/gpu/vaapi/test] Add ScopedVAConfig and SharedVAContext.
    Previously, VA configuration was tied to VaapiDevice, meaning
    that the VAProfile was static per VaapiDevice. This also
    forced users of decode_test to specify the profile and codec,
    i.e. VP9Profile0, even though the decoder can deduce both.
    
    Separate out ScopedVAConfig and introduce SharedVAContext so that
    decoders can manage this configuration and the configuration can
    manage its own lifetime, allowing decode_test to be used on most
    streams that change profile or resolution mid-stream.
    
    BUG=chromium:1062407,b:175909299
    TEST=./decode_test -video=resolution_change_500frames-vp9.ivf -out-prefix=res_change/frame on nautilus
    
    Change-Id: I32ad5dfab57851166ce31844f6609784fbcaf900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2358989
    Commit-Queue: Jao-ke Chin-Lee <jchinlee@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838985}

--- media/gpu/vaapi/test/vp9_decoder.h
+++ media/gpu/vaapi/test/vp9_decoder.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/filters/ivf_parser.h"
 #include "media/filters/vp9_parser.h"
+#include "media/gpu/vaapi/test/scoped_va_config.h"
+#include "media/gpu/vaapi/test/scoped_va_context.h"
 #include "media/gpu/vaapi/test/shared_va_surface.h"
 #include "media/gpu/vaapi/test/vaapi_device.h"
 #include "media/gpu/vaapi/test/video_decoder.h"
@@ -18,7 +20,8 @@ namespace vaapi_test {
 // A Vp9Decoder decodes VP9-encoded IVF streams using direct libva calls.
 class Vp9Decoder : public VideoDecoder {
  public:
-  Vp9Decoder(std::unique_ptr<IvfParser> ivf_parser, const VaapiDevice& va);
+  Vp9Decoder(std::unique_ptr<IvfParser> ivf_parser,
+             const VaapiDevice& va_device);
   Vp9Decoder(const Vp9Decoder&) = delete;
   Vp9Decoder& operator=(const Vp9Decoder&) = delete;
   ~Vp9Decoder() override;
@@ -42,8 +45,9 @@ class Vp9Decoder : public VideoDecoder {
   const std::unique_ptr<IvfParser> ivf_parser_;
 
   // VA handles.
-  const VaapiDevice& va_;
-  VAContextID context_id_;
+  const VaapiDevice& va_device_;
+  std::unique_ptr<ScopedVAConfig> va_config_;
+  std::unique_ptr<ScopedVAContext> va_context_;
   scoped_refptr<SharedVASurface> last_decoded_surface_;
 
   // VP9-specific data.
