03e0ea590369eb9dbf2c7ced0cb762a07f844c91
Dominic Farolino <dom@chromium.org>
2020-12-22 17:27:07 +0000

    Remove unnecessary content_export #include
    Change-Id: Ie0a095e15948507cd1fa2046d15120097f4bb89f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600541
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838887}

--- content/browser/permissions/permission_controller_impl.cc
+++ content/browser/permissions/permission_controller_impl.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/permissions/permission_controller_impl.h"
 #include "base/bind.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
-#include "content/common/content_export.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/permission_controller_delegate.h"
 #include "content/public/browser/render_frame_host.h"
