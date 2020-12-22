c35497204293e3418a1b319dbff33eed296a0e5a
Ewann <ewannpv@google.com>
2020-12-22 16:09:07 +0000

    [iOS][Settings] Updates Bookrmarks
    This CL updates Bookmarks subViews style according to the kSettingsRefresh feature.
    
    Bug: 922511
    Change-Id: Ib694c91aebb8be704892dd2983775b5a2a1bfd27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2598219
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838875}

--- ios/chrome/browser/ui/bookmarks/bookmark_folder_editor_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_folder_editor_view_controller.mm
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/icons/chrome_icon.h"
 #import "ios/chrome/browser/ui/material_components/utils.h"
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
+#import "ios/chrome/browser/ui/table_view/table_view_utils.h"
+#include "ios/chrome/browser/ui/ui_feature_flags.h"
 #include "ios/chrome/browser/ui/util/rtl_geometry.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
@@ -157,7 +159,10 @@ typedef NS_ENUM(NSInteger, ItemType) {
 - (instancetype)initWithBookmarkModel:(bookmarks::BookmarkModel*)bookmarkModel {
   DCHECK(bookmarkModel);
   DCHECK(bookmarkModel->loaded());
-  self = [super initWithStyle:UITableViewStylePlain];
+  UITableViewStyle style = base::FeatureList::IsEnabled(kSettingsRefresh)
+                               ? ChromeTableViewStyle()
+                               : UITableViewStylePlain;
+  self = [super initWithStyle:style];
   if (self) {
     _bookmarkModel = bookmarkModel;
 
--- ios/chrome/browser/ui/bookmarks/bookmark_folder_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_folder_view_controller.mm
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/bookmarks/cells/bookmark_folder_item.h"
 #import "ios/chrome/browser/ui/icons/chrome_icon.h"
 #import "ios/chrome/browser/ui/material_components/utils.h"
+#import "ios/chrome/browser/ui/table_view/table_view_utils.h"
+#include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/common/ui/colors/UIColor+cr_semantic_colors.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ui/base/l10n/l10n_util_mac.h"
@@ -121,7 +123,11 @@ using bookmarks::BookmarkNode;
   DCHECK(bookmarkModel->loaded());
   DCHECK(browser);
   DCHECK(selectedFolder == NULL || selectedFolder->is_folder());
-  self = [super initWithStyle:UITableViewStylePlain];
+
+  UITableViewStyle style = base::FeatureList::IsEnabled(kSettingsRefresh)
+                               ? ChromeTableViewStyle()
+                               : UITableViewStylePlain;
+  self = [super initWithStyle:style];
   if (self) {
     _browser = browser;
     _allowsCancel = allowsCancel;
