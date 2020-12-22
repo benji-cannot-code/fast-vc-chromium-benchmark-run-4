ed9207e9ad686db5f00654c9853398f8a5fff6eb
Lan Wei <lanwei@chromium.org>
2020-12-22 00:12:51 +0000

    Reland "Simulate user inputs in focus-visible tests in css/selectors/"
    Use testdriver Action API to simulate mouse actions in
    css/selectors/focus-visible-006.html and
    css/selectors/focus-visible-008.html.
    
    TBR=lanwei@chromium.org,mustaq@chromium.org
    
    Bug: 1145677
    Change-Id: I41347fbe7a1fc2c1d88c548b0f2f6ef86ee75154
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600018
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838751}

--- third_party/blink/web_tests/external/wpt_automation/css/selectors/focus-visible-008-manual-automation.js
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-importAutomationScript('/input-events/inputevent_common_input.js');
-
-function inject_input() {
-  return keyDown("Tab").then(() => {
-      return keyDown("Enter");
-  });
-};
