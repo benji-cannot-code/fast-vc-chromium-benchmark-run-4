e716f681cd3042785519c4cf461aac93ad43d8a3
Daniel Cheng <dcheng@chromium.org>
2020-12-22 09:10:14 +0000

    Refactor frame detach helpers to return a bool indicating success.
    Detach() and DetachChildren() now return false when they early return
    due to `this` being reentrantly detached. This matches the signature of
    DetachDocument(); a followup will take advantage of this to simplify
    Frame::Swap() to not require separate DetachDocument() + Detach() calls.
    
    Also reoganize LocalFrame::Detach() to reduce the amount of logic inside
    the reentrancy-safe block.
    
    Bug: 1063150
    Change-Id: If3c19c11ec2f732ec9b234148dc51d4ef314c769
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596880
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838823}

--- third_party/blink/renderer/core/frame/remote_frame.cc
+++ third_party/blink/renderer/core/frame/remote_frame.cc
@@ -225,11 +225,11 @@ void RemoteFrame::Navigate(FrameLoadRequest& frame_request,
                      frame_request.Impression());
 }
 
-void RemoteFrame::DetachImpl(FrameDetachType type) {
+bool RemoteFrame::DetachImpl(FrameDetachType type) {
   PluginScriptForbiddenScope forbid_plugin_destructor_scripting;
-  DetachChildren();
-  if (!Client())
-    return;
+
+  if (!DetachChildren())
+    return false;
 
   // Clean up the frame's view if needed. A remote frame only has a view if
   // the parent is a local frame.
@@ -247,11 +247,12 @@ void RemoteFrame::DetachImpl(FrameDetachType type) {
     SetCcLayer(nullptr, false, false);
   receiver_.reset();
   main_frame_receiver_.reset();
+
+  return true;
 }
 
 bool RemoteFrame::DetachDocument() {
-  DetachChildren();
-  return !!GetPage();
+  return DetachChildren();
 }
 
 void RemoteFrame::CheckCompleted() {
@@ -809,7 +810,7 @@ void RemoteFrame::AdvanceFocus(mojom::blink::FocusType type,
   GetRemoteFrameHostRemote().AdvanceFocus(type, source->GetFrameToken());
 }
 
-void RemoteFrame::DetachChildren() {
+bool RemoteFrame::DetachChildren() {
   using FrameVector = HeapVector<Member<Frame>>;
   FrameVector children_to_detach;
   children_to_detach.ReserveCapacity(Tree().ChildCount());
@@ -818,6 +819,8 @@ void RemoteFrame::DetachChildren() {
     children_to_detach.push_back(child);
   for (const auto& child : children_to_detach)
     child->Detach(FrameDetachType::kRemove);
+
+  return !!Client();
 }
 
 void RemoteFrame::ApplyReplicatedFeaturePolicyHeader() {
