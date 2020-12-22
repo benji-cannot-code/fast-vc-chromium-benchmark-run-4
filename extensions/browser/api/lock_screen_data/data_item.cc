e43ffedf3437719df629a52afbdf9347b59493c5
David Bokan <bokan@chromium.org>
2020-12-22 17:57:43 +0000

    Convert ValueStoreCache to OnceCallback
    The base::Callback parameter in
    ValueStoreCache::RunWithValueStoreForExtension is only ever meant to be
    called once so we convert it and all its callers to base::OnceCallback.
    
    Note, there was an instance in extension_settings_helper.cc where a
    base::BindRepeating was explicitly passed into a method that calls this
    one. This was recently changed and based on usage and the comment on
    RunWithStorage I believe this was made BindRepeating in error so this is
    now a BindOnce.
    
    Bug: 1152268
    Change-Id: I36759ead627c0bd41e19659275bab8052f7388de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595255
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Tim Schumann <tschumann@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838897}

--- extensions/browser/api/lock_screen_data/data_item.cc
+++ extensions/browser/api/lock_screen_data/data_item.cc
@@ -294,10 +294,11 @@ void DataItem::GetRegisteredValuesForExtension(
 
   task_runner->PostTaskAndReply(
       FROM_HERE,
-      base::BindOnce(&ValueStoreCache::RunWithValueStoreForExtension,
-                     base::Unretained(value_store_cache),
-                     base::Bind(&GetRegisteredItems, result_ptr, values_ptr),
-                     extension),
+      base::BindOnce(
+          &ValueStoreCache::RunWithValueStoreForExtension,
+          base::Unretained(value_store_cache),
+          base::BindOnce(&GetRegisteredItems, result_ptr, values_ptr),
+          extension),
       base::BindOnce(&OnGetRegisteredValues, std::move(callback),
                      std::move(result), std::move(values)));
 }
@@ -348,7 +349,8 @@ void DataItem::Register(WriteCallback callback) {
       FROM_HERE,
       base::BindOnce(&ValueStoreCache::RunWithValueStoreForExtension,
                      base::Unretained(value_store_cache_),
-                     base::Bind(&RegisterItem, result_ptr, id()), extension),
+                     base::BindOnce(&RegisterItem, result_ptr, id()),
+                     extension),
       base::BindOnce(&DataItem::OnWriteDone, weak_ptr_factory_.GetWeakPtr(),
                      std::move(callback), std::move(result)));
 }
@@ -368,10 +370,11 @@ void DataItem::Write(const std::vector<char>& data, WriteCallback callback) {
 
   task_runner_->PostTaskAndReply(
       FROM_HERE,
-      base::BindOnce(&ValueStoreCache::RunWithValueStoreForExtension,
-                     base::Unretained(value_store_cache_),
-                     base::Bind(&WriteImpl, result_ptr, id_, data, crypto_key_),
-                     extension),
+      base::BindOnce(
+          &ValueStoreCache::RunWithValueStoreForExtension,
+          base::Unretained(value_store_cache_),
+          base::BindOnce(&WriteImpl, result_ptr, id_, data, crypto_key_),
+          extension),
       base::BindOnce(&DataItem::OnWriteDone, weak_ptr_factory_.GetWeakPtr(),
                      std::move(callback), std::move(result)));
 }
@@ -398,7 +401,7 @@ void DataItem::Read(ReadCallback callback) {
       base::BindOnce(
           &ValueStoreCache::RunWithValueStoreForExtension,
           base::Unretained(value_store_cache_),
-          base::Bind(&ReadImpl, result_ptr, data_ptr, id_, crypto_key_),
+          base::BindOnce(&ReadImpl, result_ptr, data_ptr, id_, crypto_key_),
           extension),
       base::BindOnce(&DataItem::OnReadDone, weak_ptr_factory_.GetWeakPtr(),
                      std::move(callback), std::move(result), std::move(data)));
@@ -420,7 +423,7 @@ void DataItem::Delete(WriteCallback callback) {
       FROM_HERE,
       base::BindOnce(&ValueStoreCache::RunWithValueStoreForExtension,
                      base::Unretained(value_store_cache_),
-                     base::Bind(&DeleteImpl, result_ptr, id_), extension),
+                     base::BindOnce(&DeleteImpl, result_ptr, id_), extension),
       base::BindOnce(&DataItem::OnWriteDone, weak_ptr_factory_.GetWeakPtr(),
                      std::move(callback), std::move(result)));
 }
--- extensions/browser/api/storage/local_value_store_cache.cc
+++ extensions/browser/api/storage/local_value_store_cache.cc
@@ -46,7 +46,7 @@ LocalValueStoreCache::~LocalValueStoreCache() {
 }
 
 void LocalValueStoreCache::RunWithValueStoreForExtension(
-    const StorageCallback& callback,
+    StorageCallback callback,
     scoped_refptr<const Extension> extension) {
   DCHECK(IsOnBackendSequence());
 
@@ -57,9 +57,9 @@ void LocalValueStoreCache::RunWithValueStoreForExtension(
   if (extension->permissions_data()->HasAPIPermission(
           APIPermission::kUnlimitedStorage)) {
     WeakUnlimitedSettingsStorage unlimited_storage(storage);
-    callback.Run(&unlimited_storage);
+    std::move(callback).Run(&unlimited_storage);
   } else {
-    callback.Run(storage);
+    std::move(callback).Run(storage);
   }
 }
 
