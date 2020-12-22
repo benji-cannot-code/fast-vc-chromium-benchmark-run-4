61cb51002446da09e7e645fccaeda05f2c375ac0
Dominic Farolino <dom@chromium.org>
2020-12-22 17:05:41 +0000

    Remove another unnecessary content_export #include
    Change-Id: I651ae81aeb15f4043d298e4363aa49c7550d6ccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600542
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838884}

--- content/public/browser/render_process_host_factory.h
+++ content/public/browser/render_process_host_factory.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_RENDER_PROCESS_HOST_FACTORY_H_
 #define CONTENT_PUBLIC_BROWSER_RENDER_PROCESS_HOST_FACTORY_H_
 
-#include "content/common/content_export.h"
-
 namespace content {
 class BrowserContext;
 class RenderProcessHost;
