df1dc4139c5e422be8f4b2e2a850246de63a7f61
Alice Wang <aliceywang@chromium.org>
2020-12-22 08:18:18 +0000

    [Signin][Android] Move OnClickListenerViewBinder to signin.ui target
    This CL moves OnClickListenerViewBinder to signin.ui target.
    
    Bug: 1150324
    Change-Id: I1f98ddd4eb52de95849a3e5a000119691c6816e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595418
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838814}

--- chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/account_picker/OnClickListenerViewBinder.java
+++ chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/account_picker/OnClickListenerViewBinder.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.signin.account_picker;
+package org.chromium.chrome.browser.signin.ui.account_picker;
 
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,11 +15,16 @@ import org.chromium.ui.modelutil.PropertyModelChangeProcessor.ViewBinder;
 
 /**
  * This class binds an {@link OnClickListener} to a {@link View}.
+ * TODO(crbug/1155123): Change this class to package internal after modularization
  */
-class OnClickListenerViewBinder implements ViewBinder<PropertyModel, View, PropertyKey> {
+public class OnClickListenerViewBinder implements ViewBinder<PropertyModel, View, PropertyKey> {
     private final ReadableObjectPropertyKey<OnClickListener> mOnClickListenerKey;
 
-    OnClickListenerViewBinder(ReadableObjectPropertyKey<OnClickListener> onClickListenerKey) {
+    /**
+     * TODO(crbug/1155123): Change this method to package internal after modularization
+     */
+    public OnClickListenerViewBinder(
+            ReadableObjectPropertyKey<OnClickListener> onClickListenerKey) {
         mOnClickListenerKey = onClickListenerKey;
     }
 
