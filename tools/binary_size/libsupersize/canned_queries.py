20c1d05a991cb84dc5756826c38835e1cb674cc6
Andrew Grieve <agrieve@chromium.org>
2020-12-22 19:15:55 +0000

    SuperSize: Fix mojo categorization in canned queries
    Was searching for "mojo::" within source_path rather than name.
    
    Bug: None
    Change-Id: I1a8171d89a3f2a9a64f9d7bad6254020db1fddd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600026
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838921}

--- tools/binary_size/libsupersize/canned_queries.py
+++ tools/binary_size/libsupersize/canned_queries.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Contains a set of Chrome-specific size queries."""
 
 import logging
+import re
 
 import models
 
@@ -133,7 +134,11 @@ def _CategorizeGenerated(symbols):
       '/protobuf/' in s.object_path or
       s.object_path.endswith('.pbzero.o') or
       s.object_path.endswith('.pb.o'))))
-  symbols = g.Add('Mojo', symbols.WhereSourcePathMatches(r'\bmojom?\b|^mojo::'))
+  mojo_pattern = re.compile(r'\bmojom?\b')
+  symbols = g.Add(
+      'Mojo',
+      symbols.Filter(lambda s: (s.full_name.startswith('mojo::') or mojo_pattern
+                                .search(s.source_path))))
   symbols = g.Add('DevTools', symbols.WhereSourcePathMatches(
       r'\b(?:protocol|devtools)\b'))
   symbols = g.Add('Blink (bindings)', symbols.WherePathMatches(
