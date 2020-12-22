e691ca4ed3965e785d71c96db8c2c4483bfd22ae
Zach Trudo <zatrudo@google.com>
2020-12-22 21:46:15 +0000

    Revert "Switch all IDs from unsigned to signed."
    This reverts commit 7447e48c94a300912a9af5ac1b7a5197fb4acbbd.
    
    Reason for revert: Causing issues server side.
    
    Original change's description:
    > Switch all IDs from unsigned to signed.
    >
    > The purpose is to eliminate incompatibilities with server side
    > code - written in Java - which caused overflows when treating
    > large unsigned numbers as negative signed ones.
    >
    > Change in Proto will be reproduced in server code too, but there
    > is no need to synchronize the two - uint and int in proto are compatible,
    > as long as the numbers are not outside the limits.
    >
    > Bug: b:174509632
    > Change-Id: I996177f2580949a00a35ddc9e83ca18fdecc87e3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596820
    > Reviewed-by: Zach Trudo <zatrudo@google.com>
    > Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#838140}
    
    TBR=chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,zatrudo@google.com,lbaraz@chromium.org
    
    
    Bug: b:174509632
    Change-Id: I1b890e34566d46ada8dda5b4d2acfeff0979dd6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2599925
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Commit-Queue: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/master@{#838967}

--- chrome/browser/policy/messaging_layer/encryption/decryption.cc
+++ chrome/browser/policy/messaging_layer/encryption/decryption.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <limits>
 #include <string>
 
 #include "base/containers/span.h"
@@ -156,8 +155,8 @@ void Decryptor::RecordKeyPair(
               // Assign a random number to be public key id for testing purposes
               // only (in production it will be Java Fingerprint2011 which is
               // 'long').
-              const Encryptor::PublicKeyId public_key_id = base::RandGenerator(
-                  std::numeric_limits<Encryptor::PublicKeyId>::max());
+              Encryptor::PublicKeyId public_key_id;
+              base::RandBytes(&public_key_id, sizeof(public_key_id));
               if (!decryptor->keys_.emplace(public_key_id, key_info).second) {
                 result = Status(error::ALREADY_EXISTS,
                                 base::StrCat({"Public key='", public_key,
