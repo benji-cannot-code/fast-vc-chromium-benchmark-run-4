e96e2947d5088b367c8366704fdf3df02d704932
Jérémie Boulic <jboulic@chromium.org>
2020-12-22 08:08:23 +0000

    Video Player: Add files to load video player as JS modules
    Load the foreground page of the Video Player as JS modules, adding:
    - video_player_module.html
    - main.m.js which replaces video_player_scripts.js
    
    Bug: 1133186
    Change-Id: I44d3732118bec151c5c2f368f6a7123e7c9b3246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2598093
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838811}

--- ui/file_manager/video_player/js/video_player.js
+++ ui/file_manager/video_player/js/video_player.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // clang-format off
+// #import './error_util.m.js';
 // #import {util} from '../../file_manager/common/js/util.m.js';
 // #import {assert} from 'chrome://resources/js/assert.m.js';
 // #import {appUtil} from '../../base/js/app_util.m.js';
