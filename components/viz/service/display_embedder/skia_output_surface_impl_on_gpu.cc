2dc02370dbe6375d542707139b1983adc04cd776
Peng Huang <penghuang@chromium.org>
2020-12-22 18:41:13 +0000

    Fix crash in SkiaOutputDeviceX11 ctor
    Probably the crash is because the window is destroyed, it causes X11
    call failed, and then LOG(FATAL) causes crash. Fix the problem by
    adding SkiaOutputDeviceX11::Create() method, which will return null
    when X11 call fails.
    
    Bug: 1153556
    Change-Id: Id19e51b4fd8f0540125609bb479532199d3a7d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2598582
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838915}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -1266,7 +1266,7 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForVulkan() {
           GetDidSwapBuffersCompleteCallback());
     }
     if (!output_device_) {
-      output_device_ = std::make_unique<SkiaOutputDeviceX11>(
+      output_device_ = SkiaOutputDeviceX11::Create(
           context_state_, dependency_->GetSurfaceHandle(),
           shared_gpu_deps_->memory_tracker(),
           GetDidSwapBuffersCompleteCallback());
@@ -1311,12 +1311,10 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForDawn() {
     // TODO(sgilhuly): Set up a Vulkan swapchain so that Linux can also use
     // SkiaOutputDeviceDawn.
     if (!features::IsUsingOzonePlatform()) {
-      output_device_ = std::make_unique<SkiaOutputDeviceX11>(
+      output_device_ = SkiaOutputDeviceX11::Create(
           context_state_, dependency_->GetSurfaceHandle(),
           shared_gpu_deps_->memory_tracker(),
           GetDidSwapBuffersCompleteCallback());
-    } else {
-      return false;
     }
 #elif defined(OS_WIN)
     std::unique_ptr<SkiaOutputDeviceDawn> output_device =
@@ -1335,7 +1333,7 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForDawn() {
 #endif
   }
 #endif
-  return true;
+  return !!output_device_;
 }
 
 bool SkiaOutputSurfaceImplOnGpu::MakeCurrent(bool need_framebuffer) {
