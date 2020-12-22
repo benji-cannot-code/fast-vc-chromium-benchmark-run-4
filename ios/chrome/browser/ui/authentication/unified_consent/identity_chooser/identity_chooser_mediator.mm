d83addeb1e2ed3fc591c618f08570cecdc9e42d9
Nohemi Fernandez <fernandex@chromium.org>
2020-12-22 16:17:57 +0000

    [iOS] Add handling for invalid accounts in Identity selection UI.
    In certain edge cases, an identity may be invalidated when the user
    selects it to be used as their primary account. Default back to the
    originally selected account in this case.
    
    Bug: 1146459
    Change-Id: Icf014bacea20afa9a7f616ab0bde4fb477709a15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2599803
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838878}

--- ios/chrome/browser/ui/authentication/unified_consent/identity_chooser/identity_chooser_mediator.mm
+++ ios/chrome/browser/ui/authentication/unified_consent/identity_chooser/identity_chooser_mediator.mm
@@ -63,10 +63,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)selectIdentityWithGaiaID:(NSString*)gaiaID {
-  ChromeIdentity* identity = self.chromeIdentityService->GetIdentityWithGaiaID(
+  self.selectedIdentity = self.chromeIdentityService->GetIdentityWithGaiaID(
       base::SysNSStringToUTF8(gaiaID));
-  DCHECK(identity);
-  self.selectedIdentity = identity;
 }
 
 #pragma mark - Private
