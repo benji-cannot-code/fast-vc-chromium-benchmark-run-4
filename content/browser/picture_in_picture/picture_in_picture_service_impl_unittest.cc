47a9145acf3f337df68e181854eb61d8298dcbf8
Mario Sanchez Prada <mario@igalia.com>
2020-12-22 08:54:41 +0000

    Sync removal of mojo remote with destruction of MediaSessionController
    As discussed in [1], with the recent migration of legacy IPC messages
    we could get into a race condition between the point where the mojo
    remote for the media::mojom::MediaPlayer interface associated to a
    specific media player gets removed from the map owned by MWCO, and
    when exactly the MediaSessionController associated to the same media
    player gets destroyed, which is leading to crashes.
    
    To fix this, let's remove the OnMediaDestroyed() legacy IPC message and
    handle that event via the disconnect handler for the mojo remote in MWCO
    associated to the media player being destroyed, and move the logic from
    MWCO::OnMediaDestroyed() in there.
    
    Note that this is also in line with the proposed design in [2], where
    it was already proposed to remove this message in favour of the handler.
    
    [1] https://crrev.com/c/2596148/5#message-b8c571524e046ccf53a53c811da629c98c849a0b
    [2] https://docs.google.com/document/d/1OLMNxLvGkRO6ju_WfHbRMrgaVnW7bsMnW7XlJQI0A2E
    
    TBR=mlamouri@chromium.org
    
    Bug: 1157572,1157239,1157263,1157241,1039252
    Change-Id: I79b9d9cd3e65881db4826b0d058a530d6d249a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596148
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#838819}

--- content/browser/picture_in_picture/picture_in_picture_service_impl_unittest.cc
+++ content/browser/picture_in_picture/picture_in_picture_service_impl_unittest.cc
@@ -113,6 +113,8 @@ class PictureInPictureMediaPlayerReceiver : public media::mojom::MediaPlayer {
     return receiver_.BindNewPipeAndPassRemote();
   }
 
+  mojo::Receiver<media::mojom::MediaPlayer>& receiver() { return receiver_; }
+
   // media::mojom::MediaPlayer implementation.
   void AddMediaPlayerObserver(
       mojo::PendingRemote<media::mojom::MediaPlayerObserver>) override {}
@@ -156,6 +158,8 @@ class PictureInPictureServiceImplTest : public RenderViewHostImplTestHarness {
   BindMediaPlayerReceiverAndPassRemote() {
     return media_player_receiver_.BindMediaPlayerReceiverAndPassRemote();
   }
+
+  void ResetMediaPlayerReceiver() { media_player_receiver_.receiver().reset(); }
 
  private:
   PictureInPictureTestBrowserClient browser_client_;
@@ -222,9 +226,7 @@ TEST_F(PictureInPictureServiceImplTest, MAYBE_EnterPictureInPicture) {
   // Picture-in-Picture media player id should not be reset when the media is
   // destroyed (e.g. video stops playing). This allows the Picture-in-Picture
   // window to continue to control the media.
-  contents()->GetMainFrame()->OnMessageReceived(
-      MediaPlayerDelegateHostMsg_OnMediaDestroyed(
-          contents()->GetMainFrame()->GetRoutingID(), kPlayerVideoOnlyId));
+  ResetMediaPlayerReceiver();
   EXPECT_TRUE(controller->active_session_for_testing());
 }
 
