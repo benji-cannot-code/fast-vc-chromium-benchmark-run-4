3fdddc2efec97117dafd5043f217cbe29fdf86b0
Alice Wang <aliceywang@chromium.org>
2020-12-23 12:04:15 +0000

    [Signin] Make SigninPromoController independent of SigninActivityLauncherImpl
    This CL breaks the dependency of SigninPromoController on
    SigninActivityLauncherImpl to facilitate the modularization of
    SigninPromoController in the future.
    
    Bug: 1159833
    Change-Id: I68edf1c70326984372ab881cf0f661febc8fe7c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600736
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839095}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/settings/SyncPromoPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/settings/SyncPromoPreference.java
@@ -18,6 +18,7 @@ import org.chromium.chrome.browser.firstrun.FirstRunSignInProcessor;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.signin.SigninActivityLauncherImpl;
 import org.chromium.chrome.browser.signin.SigninPromoController;
 import org.chromium.chrome.browser.signin.SigninPromoUtil;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
@@ -166,7 +167,8 @@ public class SyncPromoPreference
         setVisible(true);
 
         if (mSigninPromoController == null) {
-            mSigninPromoController = new SigninPromoController(SigninAccessPoint.SETTINGS);
+            mSigninPromoController = new SigninPromoController(
+                    SigninAccessPoint.SETTINGS, SigninActivityLauncherImpl.get());
         }
 
         notifyChanged();
