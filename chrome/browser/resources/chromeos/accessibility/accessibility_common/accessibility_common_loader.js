2e0e8745e7f76341dd43551a3c16dec87d728f32
David Tseng <dtseng@google.com>
2020-12-23 18:18:54 +0000

    Convert accessibility common to use ES6 modules
    R=dmazzoni@chromium.org, josiahk@chromium.org
    
    AX-Relnotes: n/a
    Test: existing browser_tests
    Change-Id: Ifc2b85ac0903903f8cc526e4a2154121da8346f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2602108
    Reviewed-by: Josiah Krutz <josiahk@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839141}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/accessibility_common_loader.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/accessibility_common_loader.js
@@ -3,11 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {Autoclick} from './autoclick/autoclick.js';
+import {Magnifier} from './magnifier/magnifier.js';
+
 /**
  * Class to manage loading resources depending on which Accessibility features
  * are enabled.
  */
-class AccessibilityCommon {
+export class AccessibilityCommon {
   constructor() {
     /** @private {Autoclick} */
     this.autoclick_ = null;
@@ -87,4 +90,4 @@ class AccessibilityCommon {
 
 InstanceChecker.closeExtraInstances();
 // Initialize the AccessibilityCommon extension.
-var accessibilityCommon = new AccessibilityCommon();
+window.accessibilityCommon = new AccessibilityCommon();
--- chrome/browser/resources/chromeos/accessibility/accessibility_common/magnifier/magnifier.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/magnifier/magnifier.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * Main class for the Chrome OS magnifier.
  */
-class Magnifier {
+export class Magnifier {
   /**
    * @param {!Magnifier.Type} type The type of magnifier in use.
    */
