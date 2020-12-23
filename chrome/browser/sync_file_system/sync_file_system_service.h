842d69a1689ef74edee3561d65162cee030d8248
Ella Ge <eirage@chromium.org>
2020-12-23 02:18:40 +0000

    Convert c/b/ui/webui/sync_file_system_internals/ from base::Bind to Once/Repeating
    Bug: 1152282
    Change-Id: Ic60eb67049d49e55f07314d1728a50b54d99115e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2601221
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839033}

--- chrome/browser/sync_file_system/sync_file_system_service.h
+++ chrome/browser/sync_file_system/sync_file_system_service.h
@@ -53,7 +53,8 @@ class SyncFileSystemService
       public base::SupportsWeakPtr<SyncFileSystemService> {
  public:
   typedef base::Callback<void(const base::ListValue&)> DumpFilesCallback;
-  typedef base::Callback<void(const RemoteFileSyncService::OriginStatusMap&)>
+  typedef base::OnceCallback<void(
+      const RemoteFileSyncService::OriginStatusMap&)>
       ExtensionStatusMapCallback;
 
   // KeyedService implementation.
@@ -63,7 +64,7 @@ class SyncFileSystemService
                         const GURL& app_origin,
                         SyncStatusCallback callback);
 
-  void GetExtensionStatusMap(const ExtensionStatusMapCallback& callback);
+  void GetExtensionStatusMap(ExtensionStatusMapCallback callback);
   void DumpFiles(const GURL& origin, const DumpFilesCallback& callback);
   void DumpDatabase(const DumpFilesCallback& callback);
 
@@ -121,7 +122,7 @@ class SyncFileSystemService
                        std::unique_ptr<base::ListValue> list);
 
   void DidGetExtensionStatusMap(
-      const ExtensionStatusMapCallback& callback,
+      ExtensionStatusMapCallback callback,
       std::unique_ptr<RemoteFileSyncService::OriginStatusMap> status_map);
 
   // Overrides sync_enabled_ setting. This should be called only by tests.
