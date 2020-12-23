6ed4243e29f81f03fdf514d676a10e83581150f2
Xianzhu Wang <wangxianzhu@chromium.org>
2020-12-23 04:23:58 +0000

    [cc] Fix flaky crashing browser_tests on ChromeOS
    The crash happened when we committed from the main thread and the
    high res tiling was dropped because its scale was out of the range
    of min and max scales.
    
    To avoid the crash, remove SanityCheckTilingState() from
    CleanUpTilingsOnActiveLayer() to avoid the DCHECK failure at the
    beginning of UpdateTiles(), and call SanityCheckTilingState() from
    the original callsites after updating the tilings.
    
    Bug: 1160003
    Change-Id: Id9cfb65158dc6c540e2316e4f062fe6299cd1842
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2599471
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839053}

--- cc/layers/picture_layer_impl.cc
+++ cc/layers/picture_layer_impl.cc
@@ -582,6 +582,7 @@ void PictureLayerImpl::AppendQuads(viz::CompositorRenderPass* render_pass,
   // better scheme would be to maintain a tighter visible_layer_rect for the
   // finer tilings.
   CleanUpTilingsOnActiveLayer(last_append_quads_tilings_);
+  SanityCheckTilingState();
 }
 
 bool PictureLayerImpl::UpdateTiles() {
@@ -658,6 +659,8 @@ bool PictureLayerImpl::UpdateTiles() {
       viewport_rect_for_tile_priority_in_content_space_, ideal_contents_scale_,
       current_frame_time_in_seconds, occlusion_in_content_space,
       can_require_tiles_for_activation);
+  DCHECK_GT(tilings_->num_tilings(), 0u);
+  SanityCheckTilingState();
   return updated;
 }
 
@@ -1548,8 +1551,6 @@ void PictureLayerImpl::CleanUpTilingsOnActiveLayer(
   tilings_->CleanUpTilings(min_acceptable_high_res_scale,
                            max_acceptable_high_res_scale, used_tilings,
                            twin_set);
-  DCHECK_GT(tilings_->num_tilings(), 0u);
-  SanityCheckTilingState();
 }
 
 float PictureLayerImpl::MinimumRasterContentsScaleForWillChangeTransform()
