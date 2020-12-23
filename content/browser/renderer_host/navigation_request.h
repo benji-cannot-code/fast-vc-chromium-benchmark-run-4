d62e7ce00b3ceb4dc30579ebc6f67846e3817a83
Nasko Oskov <nasko@chromium.org>
2020-12-23 00:31:35 +0000

    Remove PerNavigationMojoInterface references.
    This CL removes most PerNavigationMojoInterface references from the
    codebase. Most importantly, it removes the
    RenderFrameHostImpl::DidCommitPerNavigationMojoInterfaceNavigation
    which is no longer necessary and is quite long name to read.
    
    Bug: 1020175
    Change-Id: I71f2d4a7e3f9df08e728bfed08ba67b2125d8068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600793
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839012}

--- content/browser/renderer_host/navigation_request.h
+++ content/browser/renderer_host/navigation_request.h
@@ -489,7 +489,7 @@ class CONTENT_EXPORT NavigationRequest
   void ResetStateForSiteInstanceChange();
 
   // Lazily initializes and returns the mojo::NavigationClient interface used
-  // for commit. Only used with PerNavigationMojoInterface enabled.
+  // for commit.
   mojom::NavigationClient* GetCommitNavigationClient();
 
   void set_transition(ui::PageTransition transition) {
@@ -939,12 +939,10 @@ class CONTENT_EXPORT NavigationRequest
   void UpdateCommitNavigationParamsHistory();
 
   // Called when an ongoing renderer-initiated navigation is aborted.
-  // Only used with PerNavigationMojoInterface enabled.
   void OnRendererAbortedNavigation();
 
   // Binds the given error_handler to be called when an interface disconnection
   // happens on the renderer side.
-  // Only used with PerNavigationMojoInterface enabled.
   void HandleInterfaceDisconnection(
       mojo::AssociatedRemote<mojom::NavigationClient>*,
       base::OnceClosure error_handler);
@@ -953,7 +951,6 @@ class CONTENT_EXPORT NavigationRequest
   // disconnection on the renderer side as an AbortNavigation.
   // TODO(ahemery): remove this function when NavigationRequest properly handles
   // interface disconnection in all cases.
-  // Only used with PerNavigationMojoInterface enabled.
   void IgnoreInterfaceDisconnection();
 
   // Inform the RenderProcessHost to no longer expect a navigation.
@@ -1293,7 +1290,6 @@ class CONTENT_EXPORT NavigationRequest
   // The NavigationClient interface used to commit the navigation. For now, this
   // is only used for same-site renderer-initiated navigation.
   // TODO(clamy, ahemery): Extend to all types of navigation.
-  // Only valid when PerNavigationMojoInterface is enabled.
   mojo::AssociatedRemote<mojom::NavigationClient> commit_navigation_client_;
 
   // If set, any redirects to HTTP for this navigation will be upgraded to
