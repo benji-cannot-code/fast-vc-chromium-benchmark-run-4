849b734e21a5be39c93e1263c0b12c9d573217c6
Ella Ge <eirage@chromium.org>
2020-12-23 17:04:54 +0000

    Convert c/b/ui/webui/signin/ from base::Bind to Once/Repeating
    Bug: 1152282
    Change-Id: I1cecfcf7a516e97209fa8edd415be134dab2e0ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2601064
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839130}

--- chrome/browser/ui/webui/signin/user_manager_screen_handler.cc
+++ chrome/browser/ui/webui/signin/user_manager_screen_handler.cc
@@ -442,9 +442,9 @@ void UserManagerScreenHandler::HandleRemoveUser(const base::ListValue* args) {
 
 void UserManagerScreenHandler::HandleLaunchGuest(const base::ListValue* args) {
   if (IsGuestModeEnabled()) {
-    profiles::SwitchToGuestProfile(
-        base::Bind(&UserManagerScreenHandler::OnSwitchToProfileComplete,
-                   weak_ptr_factory_.GetWeakPtr()));
+    profiles::SwitchToGuestProfile(base::BindRepeating(
+        &UserManagerScreenHandler::OnSwitchToProfileComplete,
+        weak_ptr_factory_.GetWeakPtr()));
   } else {
     // The UI should have prevented the user from allowing the selection of
     // guest mode.
@@ -493,8 +493,8 @@ void UserManagerScreenHandler::HandleLaunchUser(const base::ListValue* args) {
 
   profiles::SwitchToProfile(
       *profile_path, false, /* reuse any existing windows */
-      base::Bind(&UserManagerScreenHandler::OnSwitchToProfileComplete,
-                 weak_ptr_factory_.GetWeakPtr()));
+      base::BindRepeating(&UserManagerScreenHandler::OnSwitchToProfileComplete,
+                          weak_ptr_factory_.GetWeakPtr()));
 }
 
 void UserManagerScreenHandler::HandleRemoveUserWarningLoadStats(
@@ -842,8 +842,9 @@ void UserManagerScreenHandler::ReportAuthenticationResult(
   if (success) {
     profiles::SwitchToProfile(
         authenticating_profile_path_, true,
-        base::Bind(&UserManagerScreenHandler::OnSwitchToProfileComplete,
-                   weak_ptr_factory_.GetWeakPtr()));
+        base::BindRepeating(
+            &UserManagerScreenHandler::OnSwitchToProfileComplete,
+            weak_ptr_factory_.GetWeakPtr()));
   } else {
     web_ui()->CallJavascriptFunctionUnsafe(
         "cr.ui.UserManager.showSignInError", base::Value(0),
