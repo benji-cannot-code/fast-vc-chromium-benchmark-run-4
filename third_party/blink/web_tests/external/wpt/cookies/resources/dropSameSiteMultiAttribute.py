1d8b5aa13c3b13e3c3a0f40de88942f4e59607c3
Stephen McGruer <smcgruer@chromium.org>
2020-12-23 18:56:33 +0000

    Manual import of wpt@1c5816e523d00ad3854082245a52f09f7bdb0ff5
    This is a manual copy of an automated import, with the
    tools/ changed backed out due to bug 1161210.
    
    Using wpt-import in Chromium 18a0e912c3e85c24ed355d84c399b6aebe7896af.
    With Chromium commits locally applied on WPT:
    fda9a53d15 "Replace 'dummy' with 'sample' for inclusive language"
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    binji@chromium.org:
      external/wpt/wasm
    bokan@chromium.org:
      external/wpt/scroll-to-text-fragment
    domenic@chromium.org, hiroshige@chromium.org:
      external/wpt/import-maps
    finnur@chromium.org, peter@chromium.org, rayankans@chromium.org:
      external/wpt/contacts
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    garykac@chromium.org, joedow@chromium.org:
      external/wpt/keyboard-lock
    jsbell@chromium.org, mek@chromium.org:
      external/wpt/FileAPI
    mkwst@chromium.org:
      external/wpt/cookies
      external/wpt/trusted-types
    mkwst@chromium.org, andypaicu@chromium.org:
      external/wpt/content-security-policy
    mlamouri@chromium.org:
      external/wpt/mediasession
    npm@chromium.org:
      external/wpt/event-timing
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    
    Bug: 1161210
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Ie906ab1bc036ea00b82032b377823dda97134bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2601738
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#839151}

---
+++ third_party/blink/web_tests/external/wpt/cookies/resources/dropSameSiteMultiAttribute.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from cookies.resources.helpers import makeDropCookie, setNoCacheAndCORSHeaders
+
+def main(request, response):
+    """Respond to `/cookies/resources/dropSameSiteMultiAttribute.py by dropping
+    the cookies set by setSameSiteMultiAttribute.py"""
+    headers = setNoCacheAndCORSHeaders(request, response)
+
+    # Expire the cookies, and return a JSON-encoded success code.
+    headers.append(makeDropCookie(b"samesite_unsupported", True))
+    headers.append(makeDropCookie(b"samesite_unsupported_none", True))
+    headers.append(makeDropCookie(b"samesite_unsupported_lax", False))
+    headers.append(makeDropCookie(b"samesite_unsupported_strict", False))
+    headers.append(makeDropCookie(b"samesite_none_unsupported", True))
+    headers.append(makeDropCookie(b"samesite_lax_unsupported", True))
+    headers.append(makeDropCookie(b"samesite_strict_unsupported", True))
+    headers.append(makeDropCookie(b"samesite_lax_none", True))
+    return headers, b'{"success": true}'
