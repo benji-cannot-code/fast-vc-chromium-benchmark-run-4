40f0ecdb641cb9195d263a4af03b46462b242612
Mario Sanchez Prada <mario@igalia.com>
2020-12-24 18:37:31 +0000

    Migrate MediaPlayerDelegateHostMsg_OnMedia{Playing,Paused} to mojo
    Migrate these legacy IPC messages to the media.mojom.MediaPlayerObserver
    mojo interface, implemented by MediaSessionController in the browser
    process, and update callers in the renderer process.
    
    Relevant design document:
    https://docs.google.com/document/d/1OLMNxLvGkRO6ju_WfHbRMrgaVnW7bsMnW7XlJQI0A2E
    
    Bug: 1039252
    Change-Id: I40b5263f00bb2abe82e234a4e369df8732407d7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2561389
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839275}

--- content/renderer/media/renderer_webmediaplayer_delegate.cc
+++ content/renderer/media/renderer_webmediaplayer_delegate.cc
@@ -120,7 +120,9 @@ void RendererWebMediaPlayerDelegate::DidPlay(int player_id) {
     has_played_video_ = true;
   }
 
-  Send(new MediaPlayerDelegateHostMsg_OnMediaPlaying(routing_id(), player_id));
+  Observer* observer = id_map_.Lookup(player_id);
+  if (observer)
+    observer->OnMediaPlaying();
 
   ScheduleUpdateTask();
 }
@@ -131,8 +133,10 @@ void RendererWebMediaPlayerDelegate::DidPause(int player_id,
            << ")";
   DCHECK(id_map_.Lookup(player_id));
   playing_videos_.erase(player_id);
-  Send(new MediaPlayerDelegateHostMsg_OnMediaPaused(routing_id(), player_id,
-                                                    reached_end_of_stream));
+
+  Observer* observer = id_map_.Lookup(player_id);
+  if (observer)
+    observer->OnMediaPaused(reached_end_of_stream);
 
   // Required to keep background playback statistics up to date.
   ScheduleUpdateTask();
