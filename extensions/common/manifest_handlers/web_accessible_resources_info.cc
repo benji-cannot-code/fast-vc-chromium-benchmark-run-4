7ca090f2db3551d129e02028d146f0b909b794ec
Solomon Kinard <solomonkinard@chromium.org>
2020-12-24 00:59:37 +0000

    [Extensions][web_accessible_resources] Use |extension_ids|.
    Restrict web_accessible_resources based on site |extension_ids| that
    extension developers have specified in manifest.json.
    
    Bug: 1133624
    Change-Id: Ida36fa8f9d1ab3218a4ed2bae44b0c3e34f919cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2519366
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839216}

--- extensions/common/manifest_handlers/web_accessible_resources_info.cc
+++ extensions/common/manifest_handlers/web_accessible_resources_info.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "components/crx_file/id_util.h"
+#include "extensions/common/constants.h"
 #include "extensions/common/error_utils.h"
 #include "extensions/common/extension_id.h"
 #include "extensions/common/manifest.h"
@@ -208,6 +209,10 @@ bool WebAccessibleResourcesInfo::IsResourceWebAccessible(
 
       if (entry.matches.MatchesURL(initiator_url))
         return true;
+      if (initiator_url.SchemeIs(extensions::kExtensionScheme) &&
+          base::Contains(entry.extension_ids, initiator_url.host())) {
+        return true;
+      }
     }
   }
   return false;
