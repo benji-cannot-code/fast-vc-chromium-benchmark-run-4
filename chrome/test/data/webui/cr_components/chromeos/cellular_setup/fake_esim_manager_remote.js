3aec715433cbd19fff4cd7fd69317918a0ad4636
Theo Johnson-kanu <tjohnsonkanu@google.com>
2020-12-28 22:52:56 +0000

    [CrOS cellular] Add error message to remove eSim profile dialog
    - Before this CL remove eSIM profile dialog, did not display any
      meaningful error if remove fails, this CL adds a meaningful error.
    
    Screenshot: https://screenshot.googleplex.com/5JVxg2tdsLued8o.png
    
    Bug: 1154787
    Change-Id: Iee30704914ba582325ea9314e7a1a62736f778ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2602511
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/master@{#839500}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/fake_esim_manager_remote.js
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/fake_esim_manager_remote.js
@@ -82,7 +82,7 @@ cr.define('cellular_setup', function() {
      * @return {Object}
      * @private
      */
-    deferedPromise_() {
+    deferredPromise_() {
       let deferred = {};
       let promise = new Promise(function(resolve, reject) {
         deferred.resolve = resolve;
@@ -105,7 +105,7 @@ cr.define('cellular_setup', function() {
         this.properties_.nickname = nickname;
       }
 
-      this.deferredSetProfileNicknamePromise_ = this.deferedPromise_();
+      this.deferredSetProfileNicknamePromise_ = this.deferredPromise_();
       return this.deferredSetProfileNicknamePromise_.promise;
     }
 
@@ -120,18 +120,26 @@ cr.define('cellular_setup', function() {
 
     /** @override */
     uninstallProfile() {
-      return this.fakeEuicc_.removeProfileForTest(this.properties_.iccid)
-          .then(saved => {
-            if (saved) {
-              return {
-                result:
-                    chromeos.cellularSetup.mojom.ESimOperationResult.kSuccess
-              };
-            }
-            return {
-              result: chromeos.cellularSetup.mojom.ESimOperationResult.kFailure
-            };
-          });
+      this.defferedUninstallProfilePromise_ = this.deferredPromise_();
+      return this.defferedUninstallProfilePromise_.promise;
+    }
+
+    /** @return {Promise<void>} */
+    async resolveUninstallProfilePromise() {
+      if (!this.esimOperationResult_ ||
+          this.esimOperationResult_ ===
+              chromeos.cellularSetup.mojom.ESimOperationResult.kSuccess) {
+        const removeProfileResult =
+            await this.fakeEuicc_.removeProfileForTest(this.properties_.iccid);
+        this.defferedUninstallProfilePromise_.resolve(removeProfileResult);
+        return;
+      }
+
+      this.defferedUninstallProfilePromise_.resolve({
+        result: this.esimOperationResult_ ?
+            this.esimOperationResult_ :
+            chromeos.cellularSetup.mojom.ESimOperationResult.kSuccess
+      });
     }
   }
 
@@ -199,18 +207,25 @@ cr.define('cellular_setup', function() {
      */
     async removeProfileForTest(iccid) {
       const result = [];
-      let resp = false;
+      let profileRemoved = false;
       for (let profile of this.profiles_) {
         const property = await profile.getProperties();
         if (property.properties.iccid === iccid) {
-          resp = true;
+          profileRemoved = true;
           continue;
         }
         result.push(profile);
       }
       this.profiles_ = result;
 
-      return resp;
+      if (profileRemoved) {
+        return {
+          result: chromeos.cellularSetup.mojom.ESimOperationResult.kSuccess
+        };
+      }
+      return {
+        result: chromeos.cellularSetup.mojom.ESimOperationResult.kFailure
+      };
     }
   }
 
