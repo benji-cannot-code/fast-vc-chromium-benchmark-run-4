19ee967083f834eeeb9fda9f9bbf4c1ee913a969
Nicholas Hollingum <hollingum@google.com>
2020-12-29 06:13:07 +0000

    borealis: Allow Expected<T, E> to take a movable E
    This allows E to be something like unique_ptr, where previously it could
    not be.
    
    Bug: b/175360169
    Change-Id: Ibfbb1441763c1892feae988008ad6c2ccb5ff4b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2592244
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Daniel Ng <danielng@google.com>
    Cr-Commit-Position: refs/heads/master@{#839546}

--- chrome/browser/chromeos/borealis/infra/expected_unittest.cc
+++ chrome/browser/chromeos/borealis/infra/expected_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/chromeos/borealis/infra/expected.h"
+#include <memory>
 
 #include "base/bind.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -22,7 +23,7 @@ TEST(ExpectedTest, CanConstructWithExpected) {
 }
 
 TEST(ExpectedTest, CanConstructWithUnexpected) {
-  Expected<int, bool> exp = Unexpected<int>(true);
+  Expected<int, bool> exp = Unexpected<int, bool>(true);
   EXPECT_FALSE(exp);
 }
 
@@ -30,8 +31,28 @@ TEST(ExpectedTest, UnexpectedWorks) {
   Expected<A, B> a{A()};
   EXPECT_FALSE(a.Unexpected());
 
-  Expected<A, B> b = Unexpected<A>(B{});
+  Expected<A, B> b = Unexpected<A, B>(B{});
   EXPECT_TRUE(b.Unexpected());
+}
+
+TEST(ExpectedTest, CanHaveSameType) {
+  Expected<bool, bool> exp{true};
+  EXPECT_FALSE(exp.Unexpected());
+  EXPECT_TRUE(exp.Value());
+
+  Expected<bool, bool> unexp = Unexpected<bool, bool>(true);
+  EXPECT_TRUE(unexp.Unexpected());
+  EXPECT_TRUE(unexp.Error());
+}
+
+TEST(ExpectedTest, CanHaveNonCopyableTypes) {
+  Expected<std::unique_ptr<int>, std::unique_ptr<bool>> exp{
+      std::make_unique<int>(42)};
+  EXPECT_FALSE(exp.Unexpected());
+
+  auto unexp = Unexpected<std::unique_ptr<int>, std::unique_ptr<bool>>(
+      std::make_unique<bool>(true));
+  EXPECT_TRUE(unexp.Unexpected());
 }
 
 TEST(ExpectedTest, GettersReturnCorrectValues) {
@@ -46,7 +67,7 @@ TEST(ExpectedTest, MaybeGettersReturnPointerWhenRight) {
   Expected<A, B> a{A()};
   EXPECT_NE(a.MaybeValue(), nullptr);
 
-  Expected<A, B> b = Unexpected<A>(B{});
+  Expected<A, B> b = Unexpected<A, B>(B{});
   EXPECT_NE(b.MaybeError(), nullptr);
 }
 
@@ -54,7 +75,7 @@ TEST(ExpectedTest, MaybeGettersReturnNullWhenWrong) {
   Expected<A, B> a{A()};
   EXPECT_EQ(a.MaybeError(), nullptr);
 
-  Expected<A, B> b = Unexpected<A>(B{});
+  Expected<A, B> b = Unexpected<A, B>(B{});
   EXPECT_EQ(b.MaybeValue(), nullptr);
 }
 
@@ -71,7 +92,7 @@ TEST(ExpectedTest, HandleCallsCorrectCallback) {
       base::BindOnce(&CallbackFactory<B>::Call, base::Unretained(&b_callback)));
 
   EXPECT_CALL(b_callback, Call).Times(1);
-  Unexpected<A>(B{}).Handle(
+  Unexpected<A, B>(B{}).Handle(
       base::BindOnce(&CallbackFactory<A>::Call, base::Unretained(&a_callback)),
       base::BindOnce(&CallbackFactory<B>::Call, base::Unretained(&b_callback)));
 }
@@ -82,7 +103,7 @@ TEST(ExpectedTest, HandleCanReturn) {
             Exp{A()}.Handle(base::BindOnce([](A&) { return "expected"; }),
                             base::BindOnce([](B&) { return "unexpected"; })));
 
-  EXPECT_EQ("unexpected", Unexpected<A>(B{}).Handle(
+  EXPECT_EQ("unexpected", Exp::Unexpected(B{}).Handle(
                               base::BindOnce([](A&) { return "expected"; }),
                               base::BindOnce([](B&) { return "unexpected"; })));
 }
