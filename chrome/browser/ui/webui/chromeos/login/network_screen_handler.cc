edc69902b16970b9e6204e43a0e4fde9d2f02a51
Kyle Horimoto <khorimoto@chromium.org>
2020-12-29 22:10:28 +0000

    [CrOS Cellular] Use "updatedCellularActivationUi" bool in OOBE
    Before this CL, this loadTimeData boolean was missing in OOBE, resulting
    in a console error. This CL moves it to a shared location which is
    called from both OOBE and settings.
    
    Fixed: 1162251
    Change-Id: I42e9504e903ff3fa26d797b26c8bf19967f022c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2606708
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839647}

--- chrome/browser/ui/webui/chromeos/login/network_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/network_screen_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/demo_mode/demo_setup_controller.h"
 #include "chrome/browser/chromeos/login/screens/network_screen.h"
 #include "chrome/browser/chromeos/login/startup_utils.h"
+#include "chrome/browser/ui/webui/chromeos/cellular_setup/cellular_setup_localized_strings_provider.h"
 #include "chrome/browser/ui/webui/chromeos/login/core_oobe_handler.h"
 #include "chrome/browser/ui/webui/chromeos/network_element_localized_strings_provider.h"
 #include "chrome/grit/generated_resources.h"
@@ -99,10 +100,12 @@ void NetworkScreenHandler::DeclareLocalizedValues(
   builder->Add("offlineDemoSetupListItemName",
                IDS_NETWORK_OFFLINE_DEMO_SETUP_LIST_ITEM_NAME);
   network_element::AddLocalizedValuesToBuilder(builder);
+  cellular_setup::AddLocalizedValuesToBuilder(builder);
 }
 
 void NetworkScreenHandler::GetAdditionalParameters(
     base::DictionaryValue* dict) {
+  cellular_setup::AddNonStringLoadTimeDataToDict(dict);
 }
 
 void NetworkScreenHandler::Initialize() {
