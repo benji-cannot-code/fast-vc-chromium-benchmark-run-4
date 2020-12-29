f138cf2585ba89894db8982993b32f90b7e9bbf2
Monica Basta <msalama@chromium.org>
2020-12-29 17:59:01 +0000

    [ProfilePicker]: Make local profile name editable in place.
    This CL enables editing the local profile name from the picker's main
    view. A hover state is added to guide the user of the possibility of the
    inling editing. On click, the local profile name is editable.
    
    Bug: 1159740
    Change-Id: I2fba0602944b9e7334ee32663c081b5c308428b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2599869
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839597}

--- chrome/browser/resources/signin/profile_picker/manage_profiles_browser_proxy.js
+++ chrome/browser/resources/signin/profile_picker/manage_profiles_browser_proxy.js
@@ -123,6 +123,13 @@ export class ManageProfilesBrowserProxy {
    */
   createProfile(
       profileName, profileColor, avatarUrl, isGeneric, createShortcut) {}
+
+  /**
+   * Sets the local profile name.
+   * @param {string} profilePath
+   * @param {string} profileName
+   */
+  setProfileName(profilePath, profileName) {}
 }
 
 /** @implements {ManageProfilesBrowserProxy} */
@@ -183,6 +190,11 @@ export class ManageProfilesBrowserProxyImpl {
     chrome.send(
         'createProfile',
         [profileName, profileColor, avatarUrl, isGeneric, createShortcut]);
+  }
+
+  /** @override */
+  setProfileName(profilePath, profileName) {
+    chrome.send('setProfileName', [profilePath, profileName]);
   }
 }
 
