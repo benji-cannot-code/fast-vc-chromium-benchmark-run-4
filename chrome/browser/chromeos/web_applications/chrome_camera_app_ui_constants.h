c0bbd8b8cf7102e5b023ec03edd2af8d812b3d44
Kuo Jen Wei <inker@google.com>
2020-12-29 08:39:31 +0000

    CCA: Fine tune window size and letterboxes
    Fine tune the inner window size of 4:3/16:9/minimal to
    788x538/788x428/505x428 px and left/right/bottom line to 44/50/44 px.
    
    Bug: b/172340037
    Test: Manually verify with UX spec.
    Change-Id: Ia5761337cd3c0734bee30e8ba6bcccb0b67c2587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596713
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839556}

--- chrome/browser/chromeos/web_applications/chrome_camera_app_ui_constants.h
+++ chrome/browser/chromeos/web_applications/chrome_camera_app_ui_constants.h
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_WEB_APPLICATIONS_CHROME_CAMERA_APP_UI_CONSTANTS_H_
 #define CHROME_BROWSER_CHROMEOS_WEB_APPLICATIONS_CHROME_CAMERA_APP_UI_CONSTANTS_H_
 
+// All window size number here are referred to inner size excluding top bar.
 constexpr int kChromeCameraAppDefaultWidth = 788;
-constexpr int kChromeCameraAppDefaultHeight = 460;
+constexpr int kChromeCameraAppDefaultHeight = 428;
 constexpr int kChromeCameraAppMinimumWidth = 505;
-constexpr int kChromeCameraAppMinimumHeight = 460;
+constexpr int kChromeCameraAppMinimumHeight = 428;
 
 #endif  // CHROME_BROWSER_CHROMEOS_WEB_APPLICATIONS_CHROME_CAMERA_APP_UI_CONSTANTS_H_
