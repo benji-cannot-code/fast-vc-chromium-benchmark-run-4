3244c868452d2bc1588f61bc60a501b2e4442744
Noel Gordon <noel@chromium.org>
2020-12-29 22:44:10 +0000

    [imageloader] Make chrome://file-manager externally connectable
    Allow chrome://file-manager to connect to the image loader. Note: this
    assumes chrome://file-manager has enabled chrome.runtime.sendMessage.
    
    Since the manifest.json "externally_connectable" permission is used to
    allow chrome://file_manager access, we must now list all the extension
    |ids| that are allowed to connect to the image loader extension in the
    "externally_connectable" permission. The ids are taken from the static
    ImageLoader.ALLOWED_CLIENT_ORIGINS list.
    
    Also add 'chrome://file-manager' to ImageLoader.ALLOWED_CLIENT_ORIGINS
    list, which is still used to allow (or block) connecting sources based
    on their URL origin.
    
    Bug: 1113981, 1123261
    Change-Id: I96b0db1a0155510f95285cda8f5c7c62e6fece9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600433
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839653}

--- ui/file_manager/image_loader/image_loader.js
+++ ui/file_manager/image_loader/image_loader.js
@@ -84,7 +84,6 @@ function ImageLoader() {
   });
 }
 
-
 /**
  * List of extensions allowed to perform image requests.
  *
@@ -95,6 +94,7 @@ ImageLoader.ALLOWED_CLIENT_ORIGINS = [
   'chrome-extension://hhaomjibdihmijegdhdafkllkbggdgoj',  // File Manager
   'chrome-extension://nlkncpkkdoccmpiclbokaimcnedabhhm',  // Gallery
   'chrome-extension://jcgeabjmjgoblfofpppfkcoakmfobdko',  // Video Player
+  'chrome://file-manager',  // File Manager SWA
 ];
 
 /**
