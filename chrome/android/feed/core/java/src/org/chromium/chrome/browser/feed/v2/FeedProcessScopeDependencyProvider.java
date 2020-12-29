2ce46650292a638ceae39675304b3e3eb1c73f77
Clark DuVall <cduvall@chromium.org>
2020-12-29 22:47:41 +0000

    Remove custom library resolver for feed libraries
    This was needed because the libraries were in the feed module, but have
    now been moved to the base module. This will hopefully fix a crash
    loading the libs on some devices.
    
    Bug: 1160654
    Change-Id: Ie33114d6f5963325c096dea9f60e029d7d40e19e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2606187
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839654}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedProcessScopeDependencyProvider.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedProcessScopeDependencyProvider.java
@@ -9,7 +9,6 @@ import android.content.Context;
 
 import androidx.annotation.VisibleForTesting;
 
-import org.chromium.base.BundleUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
@@ -35,7 +34,6 @@ public class FeedProcessScopeDependencyProvider implements ProcessScopeDependenc
 
     private Context mContext;
     private ImageFetchClient mImageFetchClient;
-    private LibraryResolver mLibraryResolver;
 
     @VisibleForTesting
     static PrivacyPreferencesManager sPrivacyPreferencesManagerForTest;
@@ -43,11 +41,6 @@ public class FeedProcessScopeDependencyProvider implements ProcessScopeDependenc
     FeedProcessScopeDependencyProvider() {
         mContext = createFeedContext(ContextUtils.getApplicationContext());
         mImageFetchClient = new FeedImageFetchClient();
-        if (BundleUtils.isIsolatedSplitInstalled(mContext, FEED_SPLIT_NAME)) {
-            mLibraryResolver = (libName) -> {
-                return BundleUtils.getNativeLibraryPath(libName, FEED_SPLIT_NAME);
-            };
-        }
     }
 
     @Override
@@ -110,11 +103,6 @@ public class FeedProcessScopeDependencyProvider implements ProcessScopeDependenc
                 return;
         }
         PostTask.postDelayedTask(traits, task, delayMs);
-    }
-
-    @Override
-    public LibraryResolver getLibraryResolver() {
-        return mLibraryResolver;
     }
 
     @Override
