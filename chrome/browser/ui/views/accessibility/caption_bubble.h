71754e8054519d5669023c7d31780c9939ea1438
Abigail Klein <abigailbklein@google.com>
2020-12-30 20:48:47 +0000

    [Live Caption] Set an accessible window name for the caption bubble.
    Bug: 1055150, 1161040
    Change-Id: I128a2fccdc671a5aab0ad15c3a9e5089ae797168
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2605352
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#839797}

--- chrome/browser/ui/views/accessibility/caption_bubble.h
+++ chrome/browser/ui/views/accessibility/caption_bubble.h
@@ -99,6 +99,7 @@ class CaptionBubble : public views::BubbleDialogDelegateView {
   void OnFocus() override;
   void OnBlur() override;
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
+  base::string16 GetAccessibleWindowTitle() const override;
   void AddedToWidget() override;
 
  private:
