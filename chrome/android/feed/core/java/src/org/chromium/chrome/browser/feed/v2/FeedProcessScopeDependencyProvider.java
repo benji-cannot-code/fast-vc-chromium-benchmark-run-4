03e6919b9c30364ab353788d5d46660db14b3a9a
Clark DuVall <cduvall@chromium.org>
2020-12-30 22:13:49 +0000

    Revert "Remove custom library resolver for feed libraries"
    This reverts commit c2bd53d8d9a0f12e035f6be74c4e4e2ff38704b5.
    
    Reason for revert: This seems to have caused the crash in crbug.com/1143321 to happen again.
    
    Original change's description:
    > Remove custom library resolver for feed libraries
    >
    > This was needed because the libraries were in the feed module, but have
    > now been moved to the base module. This will hopefully fix a crash
    > loading the libs on some devices.
    >
    > Bug: 1160654
    > Change-Id: Ie33114d6f5963325c096dea9f60e029d7d40e19e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2606187
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#839654}
    
    TBR=agrieve@chromium.org,cduvall@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I2f8353a58a6200c9a8ee5475c3b1798c5b4e0f22
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1160654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2607672
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839807}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedProcessScopeDependencyProvider.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedProcessScopeDependencyProvider.java
@@ -9,6 +9,7 @@ import android.content.Context;
 
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.BundleUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
@@ -34,6 +35,7 @@ public class FeedProcessScopeDependencyProvider implements ProcessScopeDependenc
 
     private Context mContext;
     private ImageFetchClient mImageFetchClient;
+    private LibraryResolver mLibraryResolver;
 
     @VisibleForTesting
     static PrivacyPreferencesManager sPrivacyPreferencesManagerForTest;
@@ -41,6 +43,11 @@ public class FeedProcessScopeDependencyProvider implements ProcessScopeDependenc
     FeedProcessScopeDependencyProvider() {
         mContext = createFeedContext(ContextUtils.getApplicationContext());
         mImageFetchClient = new FeedImageFetchClient();
+        if (BundleUtils.isIsolatedSplitInstalled(mContext, FEED_SPLIT_NAME)) {
+            mLibraryResolver = (libName) -> {
+                return BundleUtils.getNativeLibraryPath(libName, FEED_SPLIT_NAME);
+            };
+        }
     }
 
     @Override
@@ -103,6 +110,11 @@ public class FeedProcessScopeDependencyProvider implements ProcessScopeDependenc
                 return;
         }
         PostTask.postDelayedTask(traits, task, delayMs);
+    }
+
+    @Override
+    public LibraryResolver getLibraryResolver() {
+        return mLibraryResolver;
     }
 
     @Override
