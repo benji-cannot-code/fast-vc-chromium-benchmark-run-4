6354209661776b84e6e4b517e37c7c2aabb97f89
Justin DeWitt <dewittj@chromium.org>
2020-12-30 19:23:53 +0000

    Disables failing WebContentsAccessibilityTest.
    Tbr: dmazzoni@chromium.org
    Bug: 1162479
    Change-Id: I41f8dc775e99990c18555463dd814f8411b267b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2607566
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/master@{#839772}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTest.java
@@ -38,6 +38,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
@@ -543,6 +544,7 @@ public class WebContentsAccessibilityTest {
      */
     @Test
     @MediumTest
+    @DisabledTest
     @MinAndroidSdkLevel(Build.VERSION_CODES.M)
     @TargetApi(Build.VERSION_CODES.M)
     public void testMaxContentChangedEventsFired_default() throws Throwable {
@@ -575,6 +577,7 @@ public class WebContentsAccessibilityTest {
      */
     @Test
     @MediumTest
+    @DisabledTest
     @MinAndroidSdkLevel(Build.VERSION_CODES.M)
     @TargetApi(Build.VERSION_CODES.M)
     public void testMaxContentChangedEventsFired_largeLimit() throws Throwable {
