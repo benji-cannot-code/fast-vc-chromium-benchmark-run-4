a144c22ed10c422423b42e7feb2ef1afb928bc60
Chris Lu <thegreenfrog@chromium.org>
2020-12-30 01:54:44 +0000

    [ios] Persist Crash Restore flag
    Add a feature flag to encapsulate logic that will persist the crash
    restore infobar across navigations.
    
    Bug: 1162312
    Change-Id: I70c00d3ebe8d382c4406286da18a49b608c6744e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2606625
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839681}

--- ios/chrome/browser/crash_report/crash_restore_helper.mm
+++ ios/chrome/browser/crash_report/crash_restore_helper.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
@@ -36,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/sessions/session_window_ios.h"
 #import "ios/chrome/browser/ui/main/scene_state.h"
 #import "ios/chrome/browser/ui/main/scene_state_browser_agent.h"
+#include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/multi_window_support.h"
 #include "ios/chrome/browser/web_state_list/web_state_list.h"
 #include "ios/chrome/grit/ios_theme_resources.h"
@@ -142,6 +144,7 @@ class SessionCrashedInfoBarDelegate : public ConfirmInfoBarDelegate {
   base::string16 GetButtonLabel(InfoBarButton button) const override;
   bool Accept() override;
   void InfoBarDismissed() override;
+  bool ShouldExpire(const NavigationDetails& details) const override;
   int GetIconId() const override;
 
   // TimeInterval when the delegate was created.
@@ -214,6 +217,15 @@ void SessionCrashedInfoBarDelegate::InfoBarDismissed() {
   [ConfirmInfobarMetricsRecorder
       recordConfirmInfobarEvent:MobileMessagesConfirmInfobarEvents::Dismissed
           forInfobarConfirmType:InfobarConfirmType::kInfobarConfirmTypeRestore];
+}
+
+bool SessionCrashedInfoBarDelegate::ShouldExpire(
+    const NavigationDetails& details) const {
+  if (base::FeatureList::IsEnabled(kIOSPersistCrashRestore)) {
+    return false;
+  } else {
+    return InfoBarDelegate::ShouldExpire(details);
+  }
 }
 
 int SessionCrashedInfoBarDelegate::GetIconId() const {
