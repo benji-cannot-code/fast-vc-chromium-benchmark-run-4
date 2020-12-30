358e78d6f0cd4a185450ea753d8324b2123cc836
Noel Gordon <noel@chromium.org>
2020-12-30 01:15:11 +0000

    [filesapp] Move SWA volume-related fakes together in the code
    Move the DriveSyncHandler fake definition after the VolumeManager fake
    and all the volume-related fakes.
    
    Bug: 1113981
    Change-Id: Id0999b986230ae32d55baac07a8babaff45dcfe4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2604785
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839676}

--- chromeos/components/file_manager/resources/file_manager_fakes.js
+++ chromeos/components/file_manager/resources/file_manager_fakes.js
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(crbug.com/1113981) rename to file_manager_api_proxy.js or similar.
-
 /** @suppress {checkTypes} */
 window.chrome.extension = {
   inIncognitoContext: false,
@@ -198,30 +196,6 @@ window.FileBrowserBackgroundFull = class extends FileBrowserBackground {
   }
 }
 
-window.DriveSyncHandler = class extends EventTarget {
-  /**
-   * Returns the completed event name.
-   * @return {string}
-   */
-  getCompletedEventName() {}
-
-  /**
-   * Returns whether the Drive sync is currently suppressed or not.
-   * @return {boolean}
-   */
-  isSyncSuppressed() {}
-
-  /**
-   * Shows a notification that Drive sync is disabled on cellular networks.
-   */
-  showDisabledMobileSyncNotification() {}
-
-  /**
-   * @return {boolean} Whether the handler is syncing items or not.
-   */
-  get syncing() {}
-}
-
 window.VolumeManager = class {
   constructor() {
     /**
@@ -406,6 +380,30 @@ window.VolumeInfoListFake = class {
     }
     return -1;
   }
+}
+
+window.DriveSyncHandler = class extends EventTarget {
+  /**
+   * Returns the completed event name.
+   * @return {string}
+   */
+  getCompletedEventName() {}
+
+  /**
+   * Returns whether the Drive sync is currently suppressed or not.
+   * @return {boolean}
+   */
+  isSyncSuppressed() {}
+
+  /**
+   * Shows a notification that Drive sync is disabled on cellular networks.
+   */
+  showDisabledMobileSyncNotification() {}
+
+  /**
+   * @return {boolean} Whether the handler is syncing items or not.
+   */
+  get syncing() {}
 }
 
 window.Crostini = class {
