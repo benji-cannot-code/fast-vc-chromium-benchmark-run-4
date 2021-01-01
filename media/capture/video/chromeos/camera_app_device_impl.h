c481860e6233f90c6469cd448462c1d69d51f582
Wei Lee <wtlee@google.com>
2021-01-01 05:36:29 +0000

    VCD: Close CameraAppDeviceImpl if the connection is down
    So that the connection is recoverable after restarting the cros camera
    service while initializing. Otherwise, CameraAppDeviceBridge will try to
    use the dead one while reconstructing the connection.
    
    Bug: b/176461326
    Test: Restarting cros_camera_service while initializing the VCD and the
    connection is recoverable
    
    Change-Id: I7bd3db68bc3d66de8f7b4406ca6f2a11118437b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2602969
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#839857}

--- media/capture/video/chromeos/camera_app_device_impl.h
+++ media/capture/video/chromeos/camera_app_device_impl.h
@@ -60,7 +60,8 @@ class CAPTURE_EXPORT CameraAppDeviceImpl : public cros::mojom::CameraAppDevice {
       media::mojom::ImageCapture::TakePhotoCallback take_photo_callback);
 
   CameraAppDeviceImpl(const std::string& device_id,
-                      cros::mojom::CameraInfoPtr camera_info);
+                      cros::mojom::CameraInfoPtr camera_info,
+                      base::OnceClosure cleanup_callback);
   ~CameraAppDeviceImpl() override;
 
   // Binds the mojo receiver to this implementation.
@@ -122,6 +123,8 @@ class CAPTURE_EXPORT CameraAppDeviceImpl : public cros::mojom::CameraAppDevice {
  private:
   static void DisableEeNr(ReprocessTask* task);
 
+  void OnMojoConnectionError();
+
   void SetReprocessResultOnMojoThread(SetReprocessOptionCallback callback,
                                       const int32_t status,
                                       media::mojom::BlobPtr blob);
@@ -133,6 +136,9 @@ class CAPTURE_EXPORT CameraAppDeviceImpl : public cros::mojom::CameraAppDevice {
   mojo::ReceiverSet<cros::mojom::CameraAppDevice> receivers_;
 
   cros::mojom::CameraInfoPtr camera_info_;
+
+  // Callback which should be triggered when the connection is down.
+  base::OnceClosure cleanup_callback_;
 
   // It is used to invalidate weak pointer.
   const scoped_refptr<base::SingleThreadTaskRunner> creation_task_runner_;
