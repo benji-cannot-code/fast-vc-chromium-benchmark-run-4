ae2fe25d33d0d9ecf59ada1ec1c012be46ab2c20
Stefan Zager <szager@chromium.org>
2021-01-04 22:09:46 +0000

    Don't let blink compositor set contents_opaque for remote frames
    Currently, the only case where remote frames let the blink compositor
    set contents_opaque for a remote frame is when displaying the sad
    tab graphic for a crashed frame. The sad tab graphic is not opaque,
    and we shouldn't ever set contents_opaque on its layer, regardless of
    the style of its containing <iframe> element.
    
    Bug: 1069546
    
    Change-Id: I04636d2b6cead84d224913563fd6e9bab34069d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2606659
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840045}

--- third_party/blink/renderer/core/frame/web_remote_frame_impl.cc
+++ third_party/blink/renderer/core/frame/web_remote_frame_impl.cc
@@ -253,10 +253,8 @@ WebRemoteFrame* WebRemoteFrameImpl::CreateRemoteChild(
 }
 
 void WebRemoteFrameImpl::SetCcLayer(cc::Layer* layer,
-                                    bool prevent_contents_opaque_changes,
                                     bool is_surface_layer) {
-  GetFrame()->SetCcLayer(layer, prevent_contents_opaque_changes,
-                         is_surface_layer);
+  GetFrame()->SetCcLayer(layer, is_surface_layer);
 }
 
 void WebRemoteFrameImpl::SetCoreFrame(RemoteFrame* frame) {
