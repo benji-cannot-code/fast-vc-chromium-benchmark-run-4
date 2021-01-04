1c67a48636dccffa250299f7e26226435bcd6342
Maksim Sisov <msisov@igalia.com>
2021-01-04 09:03:15 +0000

    Revert "[ozone/wayland] Ensure root_surface is committed last in a frame"
    This reverts commit a489cfcd255094ab876ed01bf86d388c44336c61.
    
    Reason for revert: broke resizing and ui events. see
    https://crbug.com/1162867
    
    Original change's description:
    > [ozone/wayland] Ensure root_surface is committed last in a frame
    >
    > wl_surface.commits of a frame can happen asynchronously due to wl_buffer
    > import delay, frame callbacks, etc. This asynchrony can cause visual
    > glitches when the root_surface commits before its subsurfaces commit.
    >
    > This CL ensures that a root_surface commit always happens after other
    > commits in a frame.
    >
    > Change-Id: I7c83e122c4cdee42e6b8aca74875fa71b6c1ab5a
    > Bug: 1144179
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2547395
    > Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > Reviewed-by: Maksim Sisov (GMT+2) <msisov@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#834327}
    
    TBR=rjkroege@chromium.org,edcourtney@chromium.org,msisov@igalia.com,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,fangzhoug@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1144179, 1162867
    Change-Id: I2cb8547d7953d30754360cc43f4461c1043da5d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2609138
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#840011}

--- ui/ozone/common/features.cc
+++ ui/ozone/common/features.cc
@@ -9,8 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-const base::Feature kWaylandOverlayDelegation{"WaylandOverlayDelegation",
-                                              base::FEATURE_ENABLED_BY_DEFAULT};
+const base::Feature kWaylandOverlayDelegation {
+  "WaylandOverlayDelegation",
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+      base::FEATURE_ENABLED_BY_DEFAULT
+#else
+      base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+};
 
 bool IsWaylandOverlayDelegationEnabled() {
   return base::FeatureList::IsEnabled(kWaylandOverlayDelegation);
