c33a49b98343bab0195804c049c9d58d343cad47
Stephan Hartmann <stha09@googlemail.com>
2021-01-04 14:43:49 +0000

    IWYU: add missing include for std::unique_ptr
    Change-Id: I8f44b6046f38de06644920ee10c3f59e7f18bbd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2602359
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840024}

--- components/performance_manager/public/freezing/freezing.h
+++ components/performance_manager/public/freezing/freezing.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PERFORMANCE_MANAGER_PUBLIC_FREEZING_FREEZING_H_
 #define COMPONENTS_PERFORMANCE_MANAGER_PUBLIC_FREEZING_FREEZING_H_
 
+#include <memory>
+
 #include "components/performance_manager/public/voting/voting.h"
 
 namespace content {
