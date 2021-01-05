0bd568eab4042e6cffb9fd239ec3a0eaa06d2518
Alice Gong <alicego@google.com>
2021-01-05 21:42:51 +0000

    Implement first 2 OAuth2 flows for Box: FindFolder and CreateFolder
    BUG=1157672
    
    Change-Id: Ib31d8dd60486f8634d9477ad82d5d9bbbc095dea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2590428
    Commit-Queue: Alice Gong <alicego@google.com>
    Reviewed-by: Marc-André Decoste <mad@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840319}

---
+++ chrome/browser/enterprise/connectors/file_system/box_api_call_endpoints.cc
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/connectors/file_system/box_api_call_endpoints.h"
+
+namespace enterprise_connectors {
+const char kFileSystemBoxEndpointApi[] = "https://api.box.com/";
+}  // namespace enterprise_connectors
