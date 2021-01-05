d8f1c7cfb5d87e13ac47118c7050ca0d0df65461
Jing Wang <jiwan@chromium.org>
2021-01-05 02:16:33 +0000

    Integrate grammar check into spell check component.
    Implemented a first version of grammar check client, which just throw
    the entire requested text to grammar service and if it returns with any
    suggestion other than the original text, we will mark the whole sentence
    and show the suggestion in context menu.
    
    Design doc of the feature: go/cros-grammar
    Screenshot of the feature:
    https://screenshot.googleplex.com/BqkRhx6BYC2LPiF
    
    This feature is ChromeOS only and is hidden behind a by-default disabled
    flag. When the flag is enabled, and when enhanced spell check is
    enabled, we will call grammar check when enhanced spell checker detects
    no spell errors and display grammar error if we find any.
    
    It is not ideal to always check the entire input and mark the whole
    sentence. The feature is to be improved in the follow-up CLs, but I
    think this is good enough as a first version.
    
    Bug: 1132699
    Test: tested on DUT
    Change-Id: Ie2a8ca99e2f6910e9eef6bb3c7eb5da980a4db92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585245
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840062}

--- chrome/browser/spellchecker/spell_check_host_chrome_impl.h
+++ chrome/browser/spellchecker/spell_check_host_chrome_impl.h
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/spellcheck/browser/spelling_service_client.h"
 #endif
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/input_method/grammar_service_client.h"
+#endif
+
 class SpellcheckCustomDictionary;
 class SpellcheckService;
 class SpellingRequest;
@@ -126,6 +130,17 @@ class SpellCheckHostChromeImpl : public SpellCheckHostImpl {
 #if BUILDFLAG(ENABLE_SPELLING_SERVICE)
   // A JSON-RPC client that calls the remote Spelling service.
   SpellingServiceClient client_;
+#endif
+
+#if defined(OS_CHROMEOS)
+  // Invoked when the on-device grammar service has finished checking the
+  // text of a CallSpellingService request.
+  void CallGrammarServiceDone(
+      CallSpellingServiceCallback callback,
+      bool success,
+      const std::vector<SpellCheckResult>& service_results) const;
+
+  chromeos::GrammarServiceClient grammar_client_;
 #endif
 
 #if BUILDFLAG(USE_RENDERER_SPELLCHECKER)
