921c9ccdd68ecb71379e8e1b2335f5625630f797
Xida Chen <xidachen@chromium.org>
2021-01-05 21:06:00 +0000

    Handle transition keyframes in composite bg color animation
    This CL handles transition keyframes when we composite background
    color animation.
    
    Bug: 1153670
    Change-Id: I9252db7f7344472d8fc9f1866d1f5b2a038dd2f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2589114
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840297}

--- third_party/blink/renderer/core/animation/transition_keyframe.h
+++ third_party/blink/renderer/core/animation/transition_keyframe.h
@@ -79,6 +79,8 @@ class CORE_EXPORT TransitionKeyframe : public Keyframe {
 
     bool IsTransitionPropertySpecificKeyframe() const final { return true; }
 
+    const TypedInterpolationValue* GetValue() const { return value_.get(); }
+
     void Trace(Visitor*) const override;
 
    private:
