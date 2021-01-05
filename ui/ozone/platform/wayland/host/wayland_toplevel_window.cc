abe92ff84e3588c197dcc73c2d4f1bbe65e418dd
Antonio Gomes <tonikitoo@igalia.com>
2021-01-05 12:49:09 +0000

    [ozone/wayland] Relax condition to call PlatformWindowDelegate::OnWindowStateChanged()
    The way the logic is currently implemented prevents the delegate from
    being notified of state changes most of the time. This CL relaxes the
    sanity checks in  WaylandToplevelWindow::HandleSurfaceConfigure(),
    causing DesktopWTHPlatform::OnWindowStateChanged() to get called
    more frequently, but holding now the correct state.
    
    This will allow DesktopWTHPlatform to react on state changes more
    reliably (eg lacros' immersive fullscreen behavior).
    
    Last, this CL changes the logic that sets and unsets fullscreen.
    Previously, UnSetFullscreen() was being called
    for all operations different than "enter fullscreen".
    This is needless and might mess compositors up.
    
    TEST=ozone_unittests adapted accordingly.
    
    BUG=1113900
    R=msisov@igalia.com, nickdiego@igalia.com
    
    Change-Id: Iddffaf5d4f0e20898db0016320935904b9816e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2601219
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#840125}

--- ui/ozone/platform/wayland/host/wayland_toplevel_window.cc
+++ ui/ozone/platform/wayland/host/wayland_toplevel_window.cc
@@ -268,7 +268,6 @@ void WaylandToplevelWindow::HandleSurfaceConfigure(int32_t width,
                                                    bool is_activated) {
   // Store the old state to propagte state changes if Wayland decides to change
   // the state to something else.
-  PlatformWindowState old_state = state_;
   if (state_ == PlatformWindowState::kMinimized && !is_activated) {
     state_ = PlatformWindowState::kMinimized;
   } else if (is_fullscreen) {
@@ -279,7 +278,6 @@ void WaylandToplevelWindow::HandleSurfaceConfigure(int32_t width,
     state_ = PlatformWindowState::kNormal;
   }
 
-  const bool state_changed = old_state != state_;
   const bool is_normal = state_ == PlatformWindowState::kNormal;
 
   // Update state before notifying delegate.
@@ -317,8 +315,8 @@ void WaylandToplevelWindow::HandleSurfaceConfigure(int32_t width,
   SetOrResetRestoredBounds();
   ApplyPendingBounds();
 
-  if (state_changed)
-    delegate()->OnWindowStateChanged(state_);
+  // Notify the delegate about the latest state set.
+  delegate()->OnWindowStateChanged(state_);
 
   if (did_active_change)
     delegate()->OnActivationChanged(is_active_);
@@ -427,22 +425,21 @@ void WaylandToplevelWindow::TriggerStateChanges() {
   if (!shell_surface_)
     return;
 
-  if (state_ == PlatformWindowState::kFullScreen)
-    shell_surface_->SetFullscreen();
-  else
-    shell_surface_->UnSetFullscreen();
-
   // Call UnSetMaximized only if current state is normal. Otherwise, if the
   // current state is fullscreen and the previous is maximized, calling
   // UnSetMaximized may result in wrong restored window position that clients
   // are not allowed to know about.
-  if (state_ == PlatformWindowState::kMaximized)
-    shell_surface_->SetMaximized();
-  else if (state_ == PlatformWindowState::kNormal)
-    shell_surface_->UnSetMaximized();
-
-  if (state_ == PlatformWindowState::kMinimized)
+  if (state_ == PlatformWindowState::kMinimized) {
     shell_surface_->SetMinimized();
+  } else if (state_ == PlatformWindowState::kFullScreen) {
+    shell_surface_->SetFullscreen();
+  } else if (previous_state_ == PlatformWindowState::kFullScreen) {
+    shell_surface_->UnSetFullscreen();
+  } else if (state_ == PlatformWindowState::kMaximized) {
+    shell_surface_->SetMaximized();
+  } else if (state_ == PlatformWindowState::kNormal) {
+    shell_surface_->UnSetMaximized();
+  }
 
   connection()->ScheduleFlush();
 }
