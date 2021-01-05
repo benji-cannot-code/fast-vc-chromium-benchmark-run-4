878b483310f6f35c3d5c8b626455894c9957a840
Maksim Ivanov <emaxx@chromium.org>
2021-01-05 23:12:23 +0000

    Fix use-after-move in //services/device/usb/
    Fix use-after-move (potential) bugs found by the
    "bugprone-use-after-move" clang-tidy check.
    
    The errors are:
    * "'transfer' used after it was moved" (4 times);
    * "'callback' used after it was moved" (2 times).
    
    Bug: 1122844
    Change-Id: I212a2fef9d33aed17e322e599a1c21a86e3eeae0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2601483
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840372}

--- services/device/usb/usb_device_handle_mac.cc
+++ services/device/usb/usb_device_handle_mac.cc
@@ -514,15 +514,16 @@ void UsbDeviceHandleMac::IsochronousTransferIn(
   for (const auto& size : packet_lengths) {
     if (!base::IsValueInRangeForNumericType<uint16_t>(size)) {
       USB_LOG(ERROR) << "Transfer too long.";
-      ReportIsochronousTransferError(std::move(callback), packet_lengths,
-                                     mojom::UsbTransferStatus::TRANSFER_ERROR);
+      ReportIsochronousTransferError(
+          std::move(transfer_data->isochronous_callback), packet_lengths,
+          mojom::UsbTransferStatus::TRANSFER_ERROR);
       return;
     }
     IOUSBIsocFrame frame_entry;
     frame_entry.frReqCount = static_cast<uint16_t>(size);
     frame_list.push_back(frame_entry);
   }
-  transfer->frame_list = frame_list;
+  transfer_data->frame_list = frame_list;
 
   kr = (*interface_interface)
            ->ReadIsochPipeAsync(interface_interface,
@@ -598,15 +599,16 @@ void UsbDeviceHandleMac::IsochronousTransferOut(
   for (const auto& size : packet_lengths) {
     if (!base::IsValueInRangeForNumericType<uint16_t>(size)) {
       USB_LOG(ERROR) << "Transfer too long.";
-      ReportIsochronousTransferError(std::move(callback), packet_lengths,
-                                     mojom::UsbTransferStatus::TRANSFER_ERROR);
+      ReportIsochronousTransferError(
+          std::move(transfer_data->isochronous_callback), packet_lengths,
+          mojom::UsbTransferStatus::TRANSFER_ERROR);
       return;
     }
     IOUSBIsocFrame frame_entry;
     frame_entry.frReqCount = static_cast<uint16_t>(size);
     frame_list.push_back(frame_entry);
   }
-  transfer->frame_list = frame_list;
+  transfer_data->frame_list = frame_list;
 
   kr = (*interface_interface)
            ->WriteIsochPipeAsync(interface_interface,
@@ -775,7 +777,7 @@ void UsbDeviceHandleMac::InterruptIn(
                                     reinterpret_cast<void*>(transfer_data));
   if (kr != kIOReturnSuccess) {
     USB_LOG(ERROR) << "Failed to read from device: " << std::hex << kr;
-    std::move(transfer->generic_callback)
+    std::move(transfer_data->generic_callback)
         .Run(mojom::UsbTransferStatus::TRANSFER_ERROR, buffer, 0);
     transfers_.erase(result.first);
   }
@@ -796,7 +798,7 @@ void UsbDeviceHandleMac::InterruptOut(
                                      reinterpret_cast<void*>(transfer_data));
   if (kr != kIOReturnSuccess) {
     USB_LOG(ERROR) << "Failed to write to device: " << std::hex << kr;
-    std::move(transfer->generic_callback)
+    std::move(transfer_data->generic_callback)
         .Run(mojom::UsbTransferStatus::TRANSFER_ERROR, buffer, 0);
     transfers_.erase(result.first);
   }
