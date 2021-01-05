bd7ad2a968f2c023bb7da4879c5d3eabcb38dc7b
Angela Gyi <angelagyi@google.com>
2021-01-05 01:38:05 +0000

    [Sharesheet] Example ShareAction
    This CL adds a single example of a ShareAction for
    testing the content previews feature on Chrome
    OS devices.
    
    Bug: 1159279
    Change-Id: Ia8e0e5dfbc00ab664468667363ece01f4b71ad1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2592276
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Angela Gyi <angelagyi@google.com>
    Cr-Commit-Position: refs/heads/master@{#840058}

--- chrome/browser/sharesheet/sharesheet_action_cache.cc
+++ chrome/browser/sharesheet/sharesheet_action_cache.cc
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sharesheet/sharesheet_action_cache.h"
 
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/about_flags.h"
+#include "chrome/browser/sharesheet/example_action.h"
 #include "chrome/browser/sharesheet/share_action.h"
 #include "chrome/browser/sharesheet/sharesheet_types.h"
+#include "chrome/common/chrome_features.h"
 #include "ui/gfx/vector_icon_types.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -25,6 +28,19 @@ SharesheetActionCache::SharesheetActionCache() {
     AddShareAction(std::make_unique<NearbyShareAction>());
   }
   AddShareAction(std::make_unique<DriveShareAction>());
+  // Add 9 example actions to show expanded view
+  if (base::FeatureList::IsEnabled(features::kSharesheetContentPreviews)) {
+    AddShareAction(std::make_unique<ExampleAction>());
+    AddShareAction(std::make_unique<ExampleAction>());
+    AddShareAction(std::make_unique<ExampleAction>());
+    AddShareAction(std::make_unique<ExampleAction>());
+    AddShareAction(std::make_unique<ExampleAction>());
+    AddShareAction(std::make_unique<ExampleAction>());
+    AddShareAction(std::make_unique<ExampleAction>());
+    AddShareAction(std::make_unique<ExampleAction>());
+    AddShareAction(std::make_unique<ExampleAction>());
+    AddShareAction(std::make_unique<ExampleAction>());
+  }
 #endif
 }
 
