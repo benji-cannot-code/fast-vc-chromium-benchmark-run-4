8167d496af572021b5dd69a12b17a0d0edabad1a
Hui Yingst <nigi@chromium.org>
2021-01-05 20:59:00 +0000

    Migrate some pp::Rect/pp::Size to gfx::Rect/gfx::Size in pdf/.
    This CL migrates |available_area_|, |document_size_| and FillRect() from
    using pp::Rect/pp::Size to using their gfx:: equivalents in
    OutOfProcessInstance, so that they can be migrated into
    PdfViewPluginBase later.
    
    This CL also changes a constructor of PaintReadyRect(), so that it no
    longer takes pp::Rect type as one of its inputs.
    
    Bug: 1101101
    Change-Id: I40569e34768d81d609d6077c14857581b0198445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2606035
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840288}

--- pdf/paint_ready_rect.cc
+++ pdf/paint_ready_rect.cc
@@ -5,16 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "pdf/paint_ready_rect.h"
 
-#include "pdf/ppapi_migration/geometry_conversions.h"
 #include "ppapi/cpp/image_data.h"
 #include "ppapi/cpp/rect.h"
 
 namespace chrome_pdf {
 
-PaintReadyRect::PaintReadyRect(const pp::Rect& rect,
+PaintReadyRect::PaintReadyRect(const gfx::Rect& rect,
                                const pp::ImageData& image_data,
                                bool flush_now)
-    : rect_(RectFromPPRect(rect)), image_(image_data), flush_now_(flush_now) {}
+    : rect_(rect), image_(image_data), flush_now_(flush_now) {}
 
 PaintReadyRect::PaintReadyRect(const gfx::Rect& rect,
                                const SkBitmap& bitmap,
