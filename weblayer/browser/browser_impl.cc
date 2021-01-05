0f9d6de1b8bea0d993272c8cb04cfde3f1c1c205
Scott Violet <sky@chromium.org>
2021-01-05 22:14:45 +0000

    sessions: makes kOther use timestamps
    Prior to this patch what was the non-snapshotting backend did not use
    timestamps. This is racy, as it means the file is potentially in a bad
    state if WebLayer were to crash before the initial state was written.
    This changes the kOther backend to use time stamps.
    
    This required some changes in the WebLayer to deal with the
    additional files.
    
    BUG=648266
    TEST=covered by tests
    
    Change-Id: I232cdfbf44ddc90b8942048d92028a646d746a8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2605525
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840337}

--- weblayer/browser/browser_impl.cc
+++ weblayer/browser/browser_impl.cc
@@ -395,7 +395,7 @@ std::unique_ptr<Tab> BrowserImpl::RemoveTab(Tab* tab) {
 }
 
 base::FilePath BrowserImpl::GetBrowserPersisterDataPath() {
-  return BuildPathForBrowserPersister(
+  return BuildBasePathForBrowserPersister(
       profile_->GetBrowserPersisterDataBaseDir(), GetPersistenceId());
 }
 
--- weblayer/browser/persistence/browser_persister.cc
+++ weblayer/browser/persistence/browser_persister.cc
@@ -66,8 +66,8 @@ BrowserPersister::BrowserPersister(const base::FilePath& path,
       rebuild_on_next_save_(false),
       crypto_key_(decryption_key) {
   browser_->AddObserver(this);
-  command_storage_manager_->GetCurrentSessionCommands(
-      base::BindOnce(&BrowserPersister::OnGotCurrentSessionCommands,
+  command_storage_manager_->GetLastSessionCommands(
+      base::BindOnce(&BrowserPersister::OnGotLastSessionCommands,
                      weak_factory_.GetWeakPtr()),
       decryption_key);
 }
@@ -274,7 +274,7 @@ void BrowserPersister::ScheduleRebuildOnNextSave() {
   command_storage_manager_->StartSaveTimer();
 }
 
-void BrowserPersister::OnGotCurrentSessionCommands(
+void BrowserPersister::OnGotLastSessionCommands(
     std::vector<std::unique_ptr<sessions::SessionCommand>> commands) {
   ScheduleRebuildOnNextSave();
 
