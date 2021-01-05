0452f8e8fe6fef14c9c386ab5b56c20d8451f1b4
Thomas Guilbert <tguilbert@chromium.org>
2021-01-05 21:18:38 +0000

    Convert LocalToRemoteSyncer::Continuation to OnceCallback
    This CL changes LocalToRemoteSyncer::Continuation from Callback to
    OnceCallback.
    
    Bug: 1152272
    Change-Id: Ie839b5982775429332c48d9c3930151a74f42423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2604677
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840307}

--- chrome/browser/sync_file_system/drive_backend/local_to_remote_syncer.cc
+++ chrome/browser/sync_file_system/drive_backend/local_to_remote_syncer.cc
@@ -254,7 +254,7 @@ void LocalToRemoteSyncer::RunPreflight(std::unique_ptr<SyncTaskToken> token) {
 }
 
 void LocalToRemoteSyncer::MoveToBackground(
-    const Continuation& continuation,
+    Continuation continuation,
     std::unique_ptr<SyncTaskToken> token) {
   std::unique_ptr<TaskBlocker> blocker(new TaskBlocker);
   blocker->app_id = url_.origin().host();
@@ -278,11 +278,11 @@ void LocalToRemoteSyncer::MoveToBackground(
   SyncTaskManager::UpdateTaskBlocker(
       std::move(token), std::move(blocker),
       base::BindOnce(&LocalToRemoteSyncer::ContinueAsBackgroundTask,
-                     weak_ptr_factory_.GetWeakPtr(), continuation));
+                     weak_ptr_factory_.GetWeakPtr(), std::move(continuation)));
 }
 
 void LocalToRemoteSyncer::ContinueAsBackgroundTask(
-    const Continuation& continuation,
+    Continuation continuation,
     std::unique_ptr<SyncTaskToken> token) {
   // The SyncTask runs as a background task beyond this point.
   // Note that any task can run between MoveToBackground() and
@@ -316,7 +316,7 @@ void LocalToRemoteSyncer::ContinueAsBackgroundTask(
     }
   }
 
-  continuation.Run(std::move(token));
+  std::move(continuation).Run(std::move(token));
 }
 
 void LocalToRemoteSyncer::SyncCompleted(std::unique_ptr<SyncTaskToken> token,
