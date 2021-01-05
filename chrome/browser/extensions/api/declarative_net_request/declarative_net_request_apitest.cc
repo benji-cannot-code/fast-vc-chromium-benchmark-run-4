bd99d01fd439328a833fbe443bef9cc5ce90fcbb
David Bertoni <dbertoni@chromium.org>
2021-01-05 19:56:23 +0000

    [Extensions] Re-enable some declarativeNetRequest tests.
    The Service Worker versions of these tests were disabled for being
    flaky. The failures were due to the tests' restarting when the
    extension was reloaded to disable file access. If the test got
    to the point of adding a dynamic rule, that state would be persist
    and the test would fail trying to add that rule again.
    
    The test infrastructure has been fixed and it no longer reloads
    extensions to modify file access. Testing shows these tests are
    not flaky now that they're not restarting.
    
    I also added a ScopedCurrentChannel to a test that uses a feature
    that's only available on the TRUNK channel. Without that, this
    test will fail in other channels.
    
    Bug: 1111240
    Change-Id: I429fab8581d9d503211fd4a385f1f1c69a99736f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2500441
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840255}

--- chrome/browser/extensions/api/declarative_net_request/declarative_net_request_apitest.cc
+++ chrome/browser/extensions/api/declarative_net_request/declarative_net_request_apitest.cc
@@ -11,13 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/threading/thread_restrictions.h"
 #include "content/public/test/browser_test.h"
-#include "extensions/common/scoped_worker_based_extensions_channel.h"
+#include "extensions/common/features/feature_channel.h"
 #include "net/dns/mock_host_resolver.h"
 
 namespace {
 
 using ContextType = extensions::ExtensionApiTest::ContextType;
-using extensions::ScopedWorkerBasedExtensionsChannel;
+using extensions::ScopedCurrentChannel;
 
 class DeclarativeNetRequestAPItest
     : public extensions::ExtensionApiTest,
@@ -47,11 +47,6 @@ class DeclarativeNetRequestAPItest
 
     // Override the path used for loading the extension.
     test_data_dir_ = temp_dir_.GetPath().AppendASCII("declarative_net_request");
-
-    // Service Workers are currently only available on certain channels, so set
-    // the channel for those tests.
-    if (GetParam() == ContextType::kServiceWorker)
-      current_channel_ = std::make_unique<ScopedWorkerBasedExtensionsChannel>();
   }
 
   bool RunTest(const std::string& extension_path) {
@@ -64,8 +59,6 @@ class DeclarativeNetRequestAPItest
 
  private:
   base::ScopedTempDir temp_dir_;
-
-  std::unique_ptr<ScopedWorkerBasedExtensionsChannel> current_channel_;
 };
 
 using DeclarativeNetRequestLazyAPItest = DeclarativeNetRequestAPItest;
@@ -76,8 +69,7 @@ INSTANTIATE_TEST_SUITE_P(PersistentBackground,
 INSTANTIATE_TEST_SUITE_P(EventPage,
                          DeclarativeNetRequestLazyAPItest,
                          ::testing::Values(ContextType::kEventPage));
-// Flaky (https://crbug.com/1111240)
-INSTANTIATE_TEST_SUITE_P(DISABLED_ServiceWorker,
+INSTANTIATE_TEST_SUITE_P(ServiceWorker,
                          DeclarativeNetRequestLazyAPItest,
                          ::testing::Values(ContextType::kServiceWorker));
 
@@ -96,6 +88,10 @@ IN_PROC_BROWSER_TEST_P(DeclarativeNetRequestAPItest, ModifyHeaders) {
 }
 
 IN_PROC_BROWSER_TEST_P(DeclarativeNetRequestLazyAPItest, GetMatchedRules) {
+  // TODO(crbug.com/1043200): This test uses
+  // chrome.declarativeNetRequest.updateSessionRules, which is only available
+  // in the TRUNK channel.
+  ScopedCurrentChannel channel_override(version_info::Channel::UNKNOWN);
   ASSERT_TRUE(RunTest("get_matched_rules")) << message_;
 }
 
