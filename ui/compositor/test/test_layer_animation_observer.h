b5e12bfda8fdcf343cb398e913c2352e807470ac
Thomas Tellier <tellier@google.com>
2021-01-05 17:04:57 +0000

    [CrOS] Introduce switch between unlock and capslock icons on login screen
    On the login and lock screen, the easy unlock icon and the capslock enabled icon share the same space. This CL introduces an animation that switches from one to another when both should be displayed.
    The one that just got enabled is immediately shown. After 2s, a transition is done to the other icon with a fade-out and fade-in effect of 0.5s each. This other icon is shown 2s and then we transition to the first one again. This cycle continues until one of the icon should not be shown.
    
    Bug: 1003885
    Change-Id: I37711de090ba757153e0bfa0e5081c9660a8a525
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587059
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Thomas Tellier <tellier@google.com>
    Cr-Commit-Position: refs/heads/master@{#840184}

--- ui/compositor/test/test_layer_animation_observer.h
+++ ui/compositor/test/test_layer_animation_observer.h
@@ -29,6 +29,7 @@ class TestLayerAnimationObserver : public LayerAnimationObserver {
   void OnLayerAnimationStarted(LayerAnimationSequence* sequence) override;
   void OnLayerAnimationAborted(LayerAnimationSequence* sequence) override;
   void OnLayerAnimationEnded(LayerAnimationSequence* sequence) override;
+  void OnLayerAnimationCycleEnded(LayerAnimationSequence* sequence) override;
   bool RequiresNotificationWhenAnimatorDestroyed() const override;
 
   const LayerAnimationSequence* last_attached_sequence() const {
@@ -68,6 +69,14 @@ class TestLayerAnimationObserver : public LayerAnimationObserver {
   }
 
   int last_ended_sequence_epoch() const { return last_ended_sequence_epoch_; }
+
+  const LayerAnimationSequence* last_cycle_ended_sequence() const {
+    return last_cycle_ended_sequence_;
+  }
+
+  int last_cycle_ended_sequence_epoch() const {
+    return last_cycle_ended_sequence_epoch_;
+  }
 
   const LayerAnimationSequence* last_detached_sequence() const {
     return last_detached_sequence_;
@@ -121,6 +130,9 @@ class TestLayerAnimationObserver : public LayerAnimationObserver {
 
   const LayerAnimationSequence* last_ended_sequence_;
   int last_ended_sequence_epoch_;
+
+  const LayerAnimationSequence* last_cycle_ended_sequence_;
+  int last_cycle_ended_sequence_epoch_;
 
   const LayerAnimationSequence* last_detached_sequence_;
   int last_detached_sequence_epoch_;
