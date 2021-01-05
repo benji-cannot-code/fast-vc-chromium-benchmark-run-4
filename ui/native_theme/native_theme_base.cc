4b148e0b5021ed002d90309a1a399f7c888af18b
Alison Maher <almaher@microsoft.com>
2021-01-05 17:43:42 +0000

    Remove Windows dependency in NativeThemeBase::GetControlColor()
    With CL:2587618, we no longer need to check for a Windows dependency
    in NativeThemeBase::GetControlColor(). Instead, we can call
    InForcedColorsMode(), which in practice is only ever true on
    Windows.
    
    There is work underway to add a devtools emulation feature for Windows
    high contrast to allow developers to test their webpages in forced
    colors mode without a Windows device. This change will likely allow
    for controls to be rendered correctly in this case.
    
    This CL should not result in any functional changes.
    
    Bug: 1107431
    Change-Id: I268e73ea574dd55c535ae3bb031c1d06b84d1d48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2590589
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Ionel Popescu <iopopesc@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#840200}

--- ui/native_theme/native_theme_base.cc
+++ ui/native_theme/native_theme_base.cc
@@ -1608,10 +1608,8 @@ SkColor NativeThemeBase::ControlsBackgroundColorForState(
 
 SkColor NativeThemeBase::GetControlColor(ControlColorId color_id,
                                          ColorScheme color_scheme) const {
-#if defined(OS_WIN)
-  if (UserHasContrastPreference() && features::IsForcedColorsEnabled())
+  if (InForcedColorsMode() && features::IsForcedColorsEnabled())
     return GetHighContrastControlColor(color_id, color_scheme);
-#endif
 
   if(color_scheme == ColorScheme::kDark)
     return GetDarkModeControlColor(color_id);
