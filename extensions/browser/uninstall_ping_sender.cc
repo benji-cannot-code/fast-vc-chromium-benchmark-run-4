71f84723e85530ad2d37f12395a14bdaa3dffdb9
David Bokan <bokan@chromium.org>
2021-01-05 21:04:52 +0000

    Convert UninstallPingSender to RepeatingCallback
    The Filter callback in this class may be called multiple times so this
    should be a RepeatingCallback. We also convert to pass-by-value to
    indicate a transfer of ownership.
    
    No change to call sites as they already use BindRepeating.
    
    Bug: 1152268
    Change-Id: I82df5dcc1a505cac4b778af1495209b3c74f6db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2605875
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840295}

--- extensions/browser/uninstall_ping_sender.cc
+++ extensions/browser/uninstall_ping_sender.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 UninstallPingSender::UninstallPingSender(ExtensionRegistry* registry,
-                                         const Filter& filter)
-    : filter_(filter) {
+                                         Filter filter)
+    : filter_(std::move(filter)) {
   observer_.Add(registry);
 }
 
