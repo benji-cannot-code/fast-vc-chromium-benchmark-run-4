2365b1800cbd83d119d94e7470d7503ae59b76dd
Alex Ilin <alexilin@chromium.org>
2021-01-05 16:55:21 +0000

    [Profile creation] Show GAIA sign-in page in dark mode when enabled
    GAIA supports dark mode by adding a color_scheme=dark query param.
    This CL passes this parameter to the signed-in profile creation flow
    when Chrome is displayed in dark mode.
    
    Bug: 1126913
    Change-Id: I6a2f024797f2571b16f435bc2e1ea4ba060177c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600897
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840179}

--- chrome/browser/ui/views/profiles/profile_picker_view.cc
+++ chrome/browser/ui/views/profiles/profile_picker_view.cc
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "google_apis/gaia/gaia_urls.h"
+#include "net/base/url_util.h"
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/flex_layout.h"
@@ -123,6 +124,14 @@ GURL CreateURLForEntryPoint(ProfilePicker::EntryPoint entry_point) {
     case ProfilePicker::EntryPoint::kProfileMenuAddNewProfile:
       return base_url.Resolve("new-profile");
   }
+}
+
+GURL GetSigninURL() {
+  GURL signin_url = GaiaUrls::GetInstance()->signin_chrome_sync_dice();
+  if (ui::NativeTheme::GetInstanceForNativeUi()->ShouldUseDarkColors()) {
+    signin_url = net::AppendQueryParameter(signin_url, "color_scheme", "dark");
+  }
+  return signin_url;
 }
 
 }  // namespace
@@ -401,8 +410,8 @@ void ProfilePickerView::OnProfileForSigninCreated(
   new_profile_contents_ = content::WebContents::Create(
       content::WebContents::CreateParams(signed_in_profile_being_created_));
   new_profile_contents_->SetDelegate(this);
-  ShowScreen(new_profile_contents_.get(),
-             GaiaUrls::GetInstance()->signin_chrome_sync_dice(),
+
+  ShowScreen(new_profile_contents_.get(), GetSigninURL(),
              /*show_toolbar=*/true);
 }
 
