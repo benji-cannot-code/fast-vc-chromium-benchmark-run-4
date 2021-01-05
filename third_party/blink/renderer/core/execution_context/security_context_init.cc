d43366c7e1678125213ac142bddddcf7c2112f55
Ian Clelland <iclelland@chromium.org>
2021-01-05 22:06:01 +0000

    Remove Document Policy Origin Trial
    The trial is no longer active (Document Policy has shipped) and
    so this configuration is no longer needed.
    
    Bug: 993790
    Change-Id: Iaefea88f07c123a68f742de301dc83160df21465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600034
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Charlie Hu <chenleihu@google.com>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840332}

--- third_party/blink/renderer/core/execution_context/security_context_init.cc
+++ third_party/blink/renderer/core/execution_context/security_context_init.cc
@@ -79,7 +79,7 @@ SecurityContextInit::SecurityContextInit(ExecutionContext* context)
 void SecurityContextInit::ApplyDocumentPolicy(
     DocumentPolicy::ParsedDocumentPolicy& document_policy,
     const String& report_only_document_policy_header) {
-  if (!RuntimeEnabledFeatures::DocumentPolicyEnabled(execution_context_))
+  if (!RuntimeEnabledFeatures::DocumentPolicyEnabled())
     return;
 
   // Because Document-Policy http header is parsed in DocumentLoader,
