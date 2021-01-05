59f2dcb04c1e1d54f50d991d035bf1d231a4f2b2
Gil Dekel <gildekel@chromium.org>
2021-01-05 18:33:56 +0000

    display/ui: Restrict Internal Displays to Preferred Mode
    Most internal panels expose a single native mode, and for those
    internal displays with additional modes, Chrome does not have
    use-cases which require those extra modes.
    
    This CL restrict internal displays to expose only their preferred
    mode to Chrome.
    
    Test: ozone_unittests && display_unittests
    Change-Id: I0f9480846dc7876992110aca0f28e81384a33cb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595621
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Gil Dekel <gildekel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840218}

--- ui/display/manager/configure_displays_task.cc
+++ ui/display/manager/configure_displays_task.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "ui/display/manager/display_util.h"
 #include "ui/display/types/display_configuration_params.h"
+#include "ui/display/types/display_constants.h"
 #include "ui/display/types/display_snapshot.h"
 #include "ui/display/types/native_display_delegate.h"
 
@@ -35,6 +36,12 @@ constexpr int kMaxDisplaysCount = 5;
 // nullptr.
 const DisplayMode* FindNextMode(const DisplaySnapshot& display_state,
                                 const DisplayMode* display_mode) {
+  // Internal displays are restricted to their native mode. We do not attempt to
+  // downgrade their modes upon failure.
+  if (display_state.type() == DISPLAY_CONNECTION_TYPE_INTERNAL) {
+    return nullptr;
+  }
+
   if (!display_mode)
     return nullptr;
 
@@ -50,6 +57,28 @@ const DisplayMode* FindNextMode(const DisplaySnapshot& display_state,
   }
 
   return best_mode;
+}
+
+void LogIfInvalidRequestForInternalDisplay(
+    const DisplayConfigureRequest& request) {
+  if (request.display->type() != DISPLAY_CONNECTION_TYPE_INTERNAL)
+    return;
+
+  if (request.mode == nullptr)
+    return;
+
+  if (request.mode == request.display->native_mode())
+    return;
+
+  LOG(ERROR) << "A mode other than the preferred mode was requested for the "
+                "internal display: preferred="
+             << request.display->native_mode()->ToString()
+             << " vs. requested=" << request.mode->ToString()
+             << ". Current mode="
+             << (request.display->current_mode()
+                     ? request.display->current_mode()->ToString()
+                     : "nullptr (disabled)")
+             << ".";
 }
 
 // Samples used to define buckets used by DisplayResolution enum.
@@ -186,6 +215,8 @@ void ConfigureDisplaysTask::Run() {
 
   std::vector<display::DisplayConfigurationParams> config_requests;
   for (const auto& request : requests_) {
+    LogIfInvalidRequestForInternalDisplay(request);
+
     config_requests.emplace_back(request.display->display_id(), request.origin,
                                  request.mode);
 
