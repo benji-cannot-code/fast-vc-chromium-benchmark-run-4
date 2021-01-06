e4942055154e2fde3c077f8136ac978eaa1e9b53
Mei Liang <meiliang@chromium.org>
2021-01-06 02:32:51 +0000

    Re-enable OverviewListLayoutTest#testCanSwipeClosed
    Fails to reproduce it locally, even with the swarming bots. This CL
    re-enable the test.
    
    Bug: 1127280
    Change-Id: Ibf53ad0a5a0b0d8fda671f45ec7c6b4a2a2f5244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611898
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840451}

--- chrome/android/javatests/src/org/chromium/chrome/browser/accessibility_tab_switcher/OverviewListLayoutTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/accessibility_tab_switcher/OverviewListLayoutTest.java
@@ -27,7 +27,6 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
@@ -196,7 +195,6 @@ public class OverviewListLayoutTest {
     }
 
     @Test
-    @DisabledTest(message = "TODO(crbug.com/1127280): Fix flakiness.")
     @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
     @MediumTest
     @Feature({"Accessibility"})
