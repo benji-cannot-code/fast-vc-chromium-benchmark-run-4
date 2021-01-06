f439d0ad594a94bef1ccba5c80bf8b764ea3a604
Xiaocheng Hu <xiaochengh@chromium.org>
2021-01-06 21:28:58 +0000

    Clamp overflow OL counter values
    Bug: 1161232
    Change-Id: I022b51133370062c9419198f44e2a5723256825e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613364
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840769}

--- third_party/blink/renderer/core/layout/layout_counter.cc
+++ third_party/blink/renderer/core/layout/layout_counter.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/ptr_util.h"
+#include "base/numerics/clamped_math.h"
 #include "third_party/blink/renderer/core/css/counter_style.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/element_traversal.h"
@@ -205,8 +206,8 @@ bool PlanCounter(LayoutObject& object,
         return true;
       }
       if (auto* olist = DynamicTo<HTMLOListElement>(*e)) {
-        value =
-            olist->StartConsideringItemCount() + (olist->IsReversed() ? 1 : -1);
+        value = base::ClampAdd(olist->StartConsideringItemCount(),
+                               olist->IsReversed() ? 1 : -1);
         type_mask = CounterNode::kResetType;
         return true;
       }
