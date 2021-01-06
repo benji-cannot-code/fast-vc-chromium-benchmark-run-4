65212c94ade4909e6bc9cb525ec6274dae77f2bc
Bo Liu <boliu@chromium.org>
2021-01-06 22:09:19 +0000

    Remove GLSurfaceEGLSurfaceControl DumpWithoutCrashing
    This is generating a significant number of crash reports that it's
    affecting ability to evaluate other crashes. There are reports from beta
    already, so going to remove the DumpWithoutCrashing and merge it before
    hitting stable.
    
    Bug: 1142569, 1037760
    Change-Id: Id91b95a385ee91e0e065ec46e5f25a779f6e0c35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2612525
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840788}

--- ui/gl/gl_surface_egl_surface_control.cc
+++ ui/gl/gl_surface_egl_surface_control.cc
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/build_info.h"
 #include "base/android/scoped_hardware_buffer_fence_sync.h"
 #include "base/bind.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/posix/eintr_wrapper.h"
-#include "base/rand_util.h"
 #include "base/strings/strcat.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/trace_event.h"
@@ -686,11 +684,6 @@ void GLSurfaceEGLSurfaceControl::TransactionAckTimeoutManager::
   LOG(ERROR) << "Transaction id " << transaction_id
              << " haven't received any ack from past 5 second which indicates "
                 "it hanged";
-
-  // Hang detection logic here. we want to limit the number of dumps to 10% of
-  // the cases to avoid seeing too many instances in crash report.
-  if (base::RandInt(1, 10) == 1)
-    base::debug::DumpWithoutCrashing();
 }
 
 }  // namespace gl
