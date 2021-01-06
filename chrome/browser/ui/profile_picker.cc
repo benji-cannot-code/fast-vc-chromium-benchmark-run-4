8d5f9254c55c81325799629ac92969b45bf71528
Alex Ilin <alexilin@chromium.org>
2021-01-06 19:02:00 +0000

    Reland "[profiles] The profile picker supports the force signin policy"
    This is a reland of 9b04f2c2bd156cc3fa1bb5aa7a9fa646bdbf5854
    
    Original change's description:
    > [profiles] The profile picker supports the force signin policy
    >
    > This CL removes the restriction that disables the profile picker if the
    > force signin enterprise policy is enabled.
    >
    > Force signin dialogs will be hosted by the profile picker if the
    > corresponding feature is enabled.
    >
    > Bug: 1156096
    > Change-Id: I19ec58afc7a451fbd868eae5e3d8d13899e78719
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595412
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: Alex Ilin <alexilin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#840575}
    
    Bug: 1156096
    Change-Id: I6fa37f9374cbbdfe413d835bc2e858b6374d37f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613147
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840696}

--- chrome/browser/ui/profile_picker.cc
+++ chrome/browser/ui/profile_picker.cc
@@ -55,9 +55,6 @@ ProfilePicker::AvailabilityOnStartup GetAvailabilityOnStartup() {
 bool ProfilePicker::ShouldShowAtLaunch() {
   AvailabilityOnStartup availability_on_startup = GetAvailabilityOnStartup();
 
-  if (signin_util::IsForceSigninEnabled())
-    return false;
-
   if (!base::FeatureList::IsEnabled(features::kNewProfilePicker))
     return false;
 
