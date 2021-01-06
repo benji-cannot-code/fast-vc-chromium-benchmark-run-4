958bcfa9aec428c52e4cfca9d292fe373bab226d
cfredric <cfredric@chromium.org>
2021-01-06 22:11:53 +0000

    Record the size of the party-context on successful SameParty cookie writes and reads.
    Change-Id: Ib071d8444d8fa67409d69cdaaafd75998db2d35a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611965
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840792}

--- net/cookies/cookie_monster.cc
+++ net/cookies/cookie_monster.cc
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/process_memory_dump.h"
+#include "net/base/isolation_info.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "net/base/url_util.h"
 #include "net/cookies/canonical_cookie.h"
@@ -1019,6 +1020,10 @@ void CookieMonster::FilterCookiesWithOptions(
     if (cookie_ptr->IsSameParty()) {
       UMA_HISTOGRAM_BOOLEAN("Cookie.SamePartyReadIncluded.IsHTTP",
                             !options.exclude_httponly());
+      UMA_HISTOGRAM_EXACT_LINEAR(
+          "Cookie.SamePartyReadIncluded.PartyContextSize",
+          options.full_party_context_size(),
+          1 + IsolationInfo::kPartyContextMaxSize);
     }
 
     included_cookies->push_back({*cookie_ptr, access_result});
@@ -1237,7 +1242,11 @@ void CookieMonster::SetCanonicalCookie(std::unique_ptr<CanonicalCookie> cc,
     if (cc->IsSameParty()) {
       UMA_HISTOGRAM_BOOLEAN("Cookie.SamePartySetIncluded.IsHTTP",
                             !options.exclude_httponly());
+      UMA_HISTOGRAM_EXACT_LINEAR("Cookie.SamePartySetIncluded.PartyContextSize",
+                                 options.full_party_context_size(),
+                                 1 + IsolationInfo::kPartyContextMaxSize);
     }
+
     // Realize that we might be setting an expired cookie, and the only point
     // was to delete the cookie which we've already done.
     if (!already_expired) {
