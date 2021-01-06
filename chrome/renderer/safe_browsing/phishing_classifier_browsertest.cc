01b115debc61a7ef4e9da3ca94ac3e0331a1b544
Daniel Rubery <drubery@chromium.org>
2021-01-06 03:50:01 +0000

    Add field is_dom_match to ClientPhishingRequest
    This CL adds a new boolean field is_dom_match to CSD pings. This makes
    it easier to separate out the pings due to the DOM model from the ones
    due to the visual model. In principle, this can be computed by comparing
    the score in the ping to the model threshold, but this process is fairly
    complex due to the potential for models to change on the server-side.
    
    Change-Id: I8685f5795eec41dddb433fe8f6dec5a4f30bee0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2610951
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840467}

--- chrome/renderer/safe_browsing/phishing_classifier_browsertest.cc
+++ chrome/renderer/safe_browsing/phishing_classifier_browsertest.cc
@@ -140,6 +140,7 @@ class PhishingClassifierTest : public ChromeRenderViewTest {
                                   verdict.feature_map(i).value());
     }
     is_phishing_ = verdict.is_phishing();
+    is_dom_match_ = verdict.is_dom_match();
     screenshot_digest_ = verdict.screenshot_digest();
     screenshot_phash_ = verdict.screenshot_phash();
     phash_dimension_size_ = verdict.phash_dimension_size();
@@ -175,6 +176,7 @@ class PhishingClassifierTest : public ChromeRenderViewTest {
   std::string screenshot_digest_;
   std::string screenshot_phash_;
   int phash_dimension_size_;
+  bool is_dom_match_;
 
   // A DiscardableMemoryAllocator is needed for certain Skia operations.
   base::TestDiscardableMemoryAllocator test_allocator_;
@@ -292,6 +294,25 @@ TEST_F(PhishingClassifierTest, TestSendsVisualDigest) {
   EXPECT_FALSE(screenshot_digest_.empty());
 }
 #endif
+
+TEST_F(PhishingClassifierTest, TestPhishingPagesAreDomMatches) {
+  LoadHtml(
+      GURL("http://host.net"),
+      "<html><body><a href=\"http://phishing.com/\">login</a></body></html>");
+  RunPhishingClassifier(&page_text_);
+
+  EXPECT_TRUE(is_phishing_);
+  EXPECT_TRUE(is_dom_match_);
+}
+
+TEST_F(PhishingClassifierTest, TestSafePagesAreNotDomMatches) {
+  LoadHtml(GURL("http://host.net"),
+           "<html><body><a href=\"http://safe.com/\">login</a></body></html>");
+  RunPhishingClassifier(&page_text_);
+
+  EXPECT_FALSE(is_phishing_);
+  EXPECT_FALSE(is_dom_match_);
+}
 
 // TODO(jialiul): Add test to verify that classification only starts on GET
 // method. It seems there is no easy way to simulate a HTTP POST in
