8707b2013d79d55a9a9b994f3c49f210fe4e411e
Stephen McGruer <smcgruer@chromium.org>
2021-01-06 13:37:16 +0000

    Roll wpt tooling.
    This rolls up to SHA d3ce095fcc6b5a85e6056fdebbf939caf2e2719f
    
    This also cherry-picks 0dd83ea79e8505d4f20a633d6750636ad9634300,
    as the upstream version has still not landed.
    
    Bug: None
    Change-Id: I36c86cd06acb2a37e6a0521bc437cac2044db25e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611924
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Commit-Queue: Jason Chase <chasej@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840583}

--- third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/webdriver/webdriver/client.py
+++ third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/webdriver/webdriver/client.py
@@ -505,7 +505,7 @@ class Session(object):
 
         try:
             self.send_command("DELETE", "session/%s" % self.session_id)
-        except error.InvalidSessionIdException:
+        except (OSError, error.InvalidSessionIdException):
             pass
         finally:
             self.session_id = None
