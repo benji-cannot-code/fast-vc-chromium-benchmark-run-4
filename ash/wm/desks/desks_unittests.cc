9c983a2e81e7eaadbab173a42325c510c310e5e5
Morten Stenshorne <mstensho@chromium.org>
2021-01-06 12:40:00 +0000

    Disable DeskTraversalNonTouchpadMetrics due to flakiness.
    TBR=sammiequon@chromium.org
    
    Bug: 1163489
    Change-Id: Id7522dc2da9562b970249b4ab976ccccd31a3ec9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613044
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840567}

--- ash/wm/desks/desks_unittests.cc
+++ ash/wm/desks/desks_unittests.cc
@@ -3706,7 +3706,8 @@ class DesksMockTimeTest : public AshTestBase {
   ~DesksMockTimeTest() override = default;
 };
 
-TEST_F(DesksMockTimeTest, DeskTraversalNonTouchpadMetrics) {
+// crbug.com/1163489
+TEST_F(DesksMockTimeTest, DISABLED_DeskTraversalNonTouchpadMetrics) {
   NewDesk();
   NewDesk();
   NewDesk();
