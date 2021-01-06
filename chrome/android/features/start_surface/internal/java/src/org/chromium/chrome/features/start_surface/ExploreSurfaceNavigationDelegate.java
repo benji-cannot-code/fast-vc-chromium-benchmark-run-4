40d0384f333a907305212b771e6c2a65341c8857
spdonghao <spdonghao@chromium.org>
2021-01-06 01:14:28 +0000

    [Start] Skip overwriting PageTransitionType.
    Related doc:
    https://docs.google.com/document/d/13MZrZWXnkUhR3pdtyBbxytoAe3Jpngy5RZ-Vg4iL8yQ/edit?resourcekey=0-eSxwXfEn4bpV3w_y-b2vpQ#
    
    Bug: 1155757
    Change-Id: I70c38585c2bc41ff7f094d6becbd8335a49e7004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595351
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840396}

--- chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/ExploreSurfaceNavigationDelegate.java
+++ chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/ExploreSurfaceNavigationDelegate.java
@@ -33,8 +33,9 @@ class ExploreSurfaceNavigationDelegate implements NativePageNavigationDelegate {
     @Override
     @Nullable
     public Tab openUrl(int windowOpenDisposition, LoadUrlParams loadUrlParams) {
+        // TODO(crbug.com/1159879): Set PageTransition.AUTO_BOOKMARK here.
         boolean result = ReturnToChromeExperimentsUtil.willHandleLoadUrlFromStartSurface(
-                loadUrlParams.getUrl(), PageTransition.AUTO_BOOKMARK,
+                loadUrlParams.getUrl(), PageTransition.AUTO_TOPLEVEL,
                 windowOpenDisposition == WindowOpenDisposition.OFF_THE_RECORD,
                 mParentTabSupplier.get());
         assert result;
@@ -43,7 +44,8 @@ class ExploreSurfaceNavigationDelegate implements NativePageNavigationDelegate {
 
     @Override
     public void navigateToHelpPage() {
+        // TODO(crbug.com/1159879): Set PageTransition.AUTO_BOOKMARK here.
         openUrl(WindowOpenDisposition.CURRENT_TAB,
-                new LoadUrlParams(NEW_TAB_URL_HELP, PageTransition.AUTO_BOOKMARK));
+                new LoadUrlParams(NEW_TAB_URL_HELP, PageTransition.AUTO_TOPLEVEL));
     }
 }
