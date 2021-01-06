12b7580f37861ca6b713e5bdd0838d5b39c9ca2e
Yunke Zhou <yunkez@google.com>
2021-01-06 06:10:22 +0000

    Reland "Oobe: Fix issue with metrics client id reset after sign in"
    This is a reland of b8af3c0a6921ee80cbc6c85b4c32a3072294c1f2
    
    Original change's description:
    > Oobe: Fix issue with metrics client id reset after sign in
    >
    > Issue is due to there's a small time difference between StatsReportingController::SetWithService and pref is properly set. We should only propagate the value from service until it is correctly set.
    >
    > Bug: 1154947
    > Change-Id: I977249dd25e1402246b383401c9deef4ea8ebc56
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587065
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Commit-Queue: Yunke Zhou <yunkez@google.com>
    > Cr-Commit-Position: refs/heads/master@{#838606}
    
    Bug: 1154947
    Change-Id: I5f61724d871aff31e26411f83ac89b389c2b0c31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2599642
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Cr-Commit-Position: refs/heads/master@{#840498}

--- chrome/browser/chromeos/settings/stats_reporting_controller_unittest.cc
+++ chrome/browser/chromeos/settings/stats_reporting_controller_unittest.cc
@@ -123,10 +123,8 @@ TEST_F(StatsReportingControllerTest, GetAndSet_OwnershipUnknown) {
 
   std::unique_ptr<TestingProfile> user = CreateUser(no_keys);
   StatsReportingController::Get()->SetEnabled(user.get(), true);
-  // A pending value is written in case there is no owner, in which case it will
-  // be written properly when ownership is taken - but we don't read the
-  // pending value, in case there actually is an owner already.
-  EXPECT_FALSE(StatsReportingController::Get()->IsEnabled());
+  // Read from the pending value when ownership is not taken.
+  EXPECT_TRUE(StatsReportingController::Get()->IsEnabled());
   ExpectThatPendingValueIs(true);
   ExpectThatSignedStoredValueIs(false);
 
@@ -181,11 +179,22 @@ TEST_F(StatsReportingControllerTest, GetAndSet_OwnershipTaken) {
   StatsReportingController::Get()->SetEnabled(owner.get(), true);
   EXPECT_TRUE(StatsReportingController::Get()->IsEnabled());
   EXPECT_TRUE(value_at_last_notification_);
+  ExpectThatPendingValueIs(true);
+
+  StatsReportingController::Get()->OnSignedPolicyStored(true);
+  EXPECT_TRUE(StatsReportingController::Get()->IsEnabled());
+  EXPECT_TRUE(value_at_last_notification_);
   ExpectThatPendingValueIsNotSet();
   ExpectThatSignedStoredValueIs(true);
 
   StatsReportingController::Get()->SetEnabled(owner.get(), false);
   EXPECT_FALSE(StatsReportingController::Get()->IsEnabled());
+  EXPECT_FALSE(value_at_last_notification_);
+  ExpectThatPendingValueIs(false);
+
+  StatsReportingController::Get()->OnSignedPolicyStored(true);
+  EXPECT_FALSE(StatsReportingController::Get()->IsEnabled());
+  EXPECT_FALSE(value_at_last_notification_);
   ExpectThatPendingValueIsNotSet();
   ExpectThatSignedStoredValueIs(false);
 }
@@ -222,8 +231,8 @@ TEST_F(StatsReportingControllerTest, SetBeforeOwnershipTaken) {
   // Before device is owned, setting the value means writing a pending value:
   std::unique_ptr<TestingProfile> pre_ownership_user = CreateUser(no_keys);
   StatsReportingController::Get()->SetEnabled(pre_ownership_user.get(), true);
-  EXPECT_FALSE(StatsReportingController::Get()->IsEnabled());
-  EXPECT_FALSE(value_at_last_notification_);
+  EXPECT_TRUE(StatsReportingController::Get()->IsEnabled());
+  EXPECT_TRUE(value_at_last_notification_);
   ExpectThatPendingValueIs(true);
   ExpectThatSignedStoredValueIs(false);
 
@@ -236,6 +245,11 @@ TEST_F(StatsReportingControllerTest, SetBeforeOwnershipTaken) {
   // After device is owned, the value is written to Cros settings.
   StatsReportingController::Get()->OnOwnershipTaken(
       OwnerSettingsServiceChromeOSFactory::GetForBrowserContext(owner.get()));
+  EXPECT_TRUE(StatsReportingController::Get()->IsEnabled());
+  EXPECT_TRUE(value_at_last_notification_);
+  ExpectThatPendingValueIs(true);
+
+  StatsReportingController::Get()->OnSignedPolicyStored(true);
   EXPECT_TRUE(StatsReportingController::Get()->IsEnabled());
   EXPECT_TRUE(value_at_last_notification_);
   ExpectThatPendingValueIsNotSet();
