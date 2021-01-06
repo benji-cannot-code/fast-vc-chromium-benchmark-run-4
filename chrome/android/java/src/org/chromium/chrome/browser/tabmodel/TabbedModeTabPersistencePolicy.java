22e0b88f8bd01d5e0e4a27256d494d1faba59597
David Maunder <davidjm@chromium.org>
2021-01-06 18:55:42 +0000

    Batch MultiInstanceMigrationTest
    Speedup: 6011ms -> 1404ms (76.64% speedup)
    
    Bug: 1163254
    Change-Id: I812fae55b008b331c4ad07d2f0e36d85da221cb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611867
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840691}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabbedModeTabPersistencePolicy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabbedModeTabPersistencePolicy.java
@@ -438,4 +438,9 @@ public class TabbedModeTabPersistencePolicy implements TabPersistencePolicy {
             }
         }
     }
+
+    @VisibleForTesting
+    protected static void resetMigrationTaskForTesting() {
+        sMigrationTask = null;
+    }
 }
