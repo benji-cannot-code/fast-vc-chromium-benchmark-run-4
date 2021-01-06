60020a0c5b42c12b97bd394c014d57b2c9fef5f9
David Tseng <dtseng@google.com>
2021-01-06 03:40:18 +0000

    Support device specific keys in Switch Access
    This change implements the infrastructure for assignment and capture of
    Switch Access keys tied to a specific type of input device (internal,
    usb, bluetooth).
    
    Along the way, it also cleans up unused code, obsoleted preferences.
    
    It does not hook up the final UI for the device type which will be
    handled separately once UX is finalized.
    
    R=josiahk@chromium.org
    
    Bug: 1163226
    Test: preexisting ash_unittests
    Change-Id: I381bbfb32e1de25ab6228cac7b52707170e136ab
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2599923
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Josiah Krutz <josiahk@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/master@{#840466}

--- chrome/browser/resources/settings/chromeos/os_a11y_page/switch_access_action_assignment_dialog.js
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/switch_access_action_assignment_dialog.js
@@ -32,9 +32,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @private
  */
 /* #export */ const actionToPref = {
-  select: 'settings.a11y.switch_access.select.key_codes',
-  next: 'settings.a11y.switch_access.next.key_codes',
-  previous: 'settings.a11y.switch_access.previous.key_codes'
+  select: 'settings.a11y.switch_access.select.device_key_codes',
+  next: 'settings.a11y.switch_access.next.device_key_codes',
+  previous: 'settings.a11y.switch_access.previous.device_key_codes'
+};
+
+/**
+ * Possible device types for Switch Access.
+ * @enum {string}
+ * @private
+ */
+/* #export */ const SwitchAccessDeviceType = {
+  INTERNAL: 'internal',
+  USB: 'usb',
+  BLUETOOTH: 'bluetooth',
+  UNKNOWN: 'unknown'
 };
 
 /**
@@ -46,6 +58,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ADD_ASSIGNMENT: 'add-assignment',
   REMOVE_ASSIGNMENT: 'remove-assignment',
 };
+
+/**
+ * @typedef {!Array<!Object<string, !Array<!SwitchAccessDeviceType>>>}
+ */
+let SwitchAccessKeyAssignmentInfoList;
 
 Polymer({
   is: 'settings-switch-access-action-assignment-dialog',
@@ -77,7 +94,8 @@ Polymer({
 
     /**
      * Assignments for the current action.
-     * @private {Array<string>}
+     * @private {Array<!{key: string, devices:
+     *     !Array<!SwitchAccessDeviceType>}>}
      */
     assignments_: {
       type: Array,
@@ -96,12 +114,17 @@ Polymer({
     /**
      * A dictionary containing all Switch Access key codes (mapped from
      * actions).
-     * @private {{select: !Array<string>, next: !Array<string>, previous:
-     *     !Array<string>}}
+     * Each key code is another object, mapping a stringified key code to a list
+     * of Switch Access device types for that key code.
+     * @private {{
+     *         select: SwitchAccessKeyAssignmentInfoList,
+     *         next: SwitchAccessKeyAssignmentInfoList,
+     *         previous: SwitchAccessKeyAssignmentInfoList
+     * }}
      */
     keyCodes_: {
       type: Object,
-      value: {select: [], next: [], previous: []},
+      value: {select: {}, next: {}, previous: {}},
     },
 
     /**
@@ -220,7 +243,7 @@ Polymer({
 
     // Check for pre-existing assignments in actions other than the current one.
     for (const action of Object.values(SwitchAccessCommand)) {
-      if (!this.keyCodes_[action].includes(event.keyCode)) {
+      if (!this.keyCodes_[action][event.keyCode]) {
         continue;
       }
 
@@ -252,7 +275,12 @@ Polymer({
   handleKeyEventInWaitForConfirmation_(event) {
     if (this.currentKeyCode_ === event.keyCode) {
       // Confirmed.
-      this.keyCodes_[this.action].push(this.currentKeyCode_);
+      // TODO: resolve to specific device type once UI is hooked up;
+      // |event.device| has the Switch Access device type.
+      this.keyCodes_[this.action][this.currentKeyCode_] = [
+        SwitchAccessDeviceType.INTERNAL, SwitchAccessDeviceType.USB,
+        SwitchAccessDeviceType.BLUETOOTH
+      ];
       this.$.switchAccessActionAssignmentDialog.close();
       return;
     }
@@ -277,10 +305,8 @@ Polymer({
     }
 
     // Remove this key code.
-    const index = this.keyCodes_[this.action].indexOf(this.currentKeyCode_);
-    if (index !== -1) {
-      this.keyCodes_[this.action].splice(index, 1);
-    }
+    delete this.keyCodes_[this.action][this.currentKeyCode_];
+
     this.$.switchAccessActionAssignmentDialog.close();
   },
 
@@ -290,21 +316,12 @@ Polymer({
   },
 
   /**
-   * @param {!Object<SwitchAccessCommand, !Array<string>>} value
+   * @param {!Object<SwitchAccessCommand, !Array<!{key: string, devices:
+   *     !Array<SwitchAccessDeviceType>}>>} value
    * @private
    */
   onAssignmentsChanged_(value) {
-    switch (this.action) {
-      case SwitchAccessCommand.SELECT:
-        this.assignments_ = value.select;
-        break;
-      case SwitchAccessCommand.NEXT:
-        this.assignments_ = value.next;
-        break;
-      case SwitchAccessCommand.PREVIOUS:
-        this.assignments_ = value.previous;
-        break;
-    }
+    this.assignments_ = value[this.action];
   },
 
   /**
