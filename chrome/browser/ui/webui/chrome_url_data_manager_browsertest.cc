b96b3f7170333911bed43e0f6eca148e89b2fc81
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-01-06 23:38:45 +0000

    Revert "Clean up the NoTrustedTypesViolation test"
    This reverts commit da4ef76657c1c33dc0c41e9a657143e843cec686.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 840628 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2RhNGVmNzY2NTdjMWMzM2RjMGM0MWU5YTY1NzE0M2U4NDNjZWM2ODYM
    
    Sample Failed Build: https://ci.chromium.org/b/8858824985500220208
    
    Sample Failed Step: browser_tests
    
    Original change's description:
    > Clean up the NoTrustedTypesViolation test
    > 
    > This change clean up unnecessary test cases in the
    > NoTrustedTypesViolation test. Also added few things that makes
    > debugging of the test failure more easier.
    > 
    > Bug: 1115643
    > Change-Id: I12e77b3038046337f8002850f5c28806dd722acd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2610486
    > Reviewed-by: dpapad <dpapad@chromium.org>
    > Commit-Queue: Jun Kokatsu <Jun.Kokatsu@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#840628}
    
    
    Change-Id: Icefeac0f9258222a5deb35ec91e9abfb6ef4ebce
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1115643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613590
    Cr-Commit-Position: refs/heads/master@{#840821}

--- chrome/browser/ui/webui/chrome_url_data_manager_browsertest.cc
+++ chrome/browser/ui/webui/chrome_url_data_manager_browsertest.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/logging.h"
 #include "base/macros.h"
 #include "base/strings/string_piece.h"
 #include "build/build_config.h"
@@ -158,15 +157,9 @@ class ChromeURLDataManagerWebUITrustedTypesTest
 
     ASSERT_TRUE(embedded_test_server()->Start());
     ui_test_utils::NavigateToURL(browser(), GURL(url));
-
-    if (url == "chrome://network-error" || url == "chrome://dino") {
-      // We don't ASSERT_TRUE here because some WebUI pages are
-      // PAGE_TYPE_ERROR by design.
-      content::WaitForLoadStop(content);
-    } else {
-      ASSERT_TRUE(content::WaitForLoadStop(content));
-    }
-
+    // We don't ASSERT_TRUE here because some WebUI pages are by design not
+    // PAGE_TYPE_NORMAL (e.g. chrome://interstitials/ssl).
+    content::WaitForLoadStop(content);
     EXPECT_TRUE(console_observer.messages().empty());
   }
 };
@@ -174,7 +167,6 @@ class ChromeURLDataManagerWebUITrustedTypesTest
 // Verify that there's no Trusted Types violation in chrome://chrome-urls
 IN_PROC_BROWSER_TEST_P(ChromeURLDataManagerWebUITrustedTypesTest,
                        NoTrustedTypesViolation) {
-  LOG(INFO) << "Navigating to " << GetParam();
   CheckTrustedTypesViolation(GetParam());
 }
 
@@ -185,18 +177,23 @@ static constexpr const char* const kChromeUrls[] = {
     // TODO(crbug.com/1114074): DCHECK failure when opening
     // chrome://appcache-internals.
     // "chrome://appcache-internals",
+    "chrome://apps",
     "chrome://autofill-internals",
     "chrome://blob-internals",
     "chrome://bluetooth-internals",
     "chrome://bookmarks",
+    "chrome://browser-switch",
     "chrome://chrome-urls",
     "chrome://components",
+    "chrome://conflicts",
     "chrome://connection-help",
     "chrome://connection-monitoring-detected",
     "chrome://conversion-internals",
     "chrome://crashes",
     "chrome://credits",
     "chrome://device-log",
+    // TODO(crbug.com/1114062): Crash when closing chrome://devices.
+    // "chrome://devices",
     "chrome://dino",
     // TODO(crbug.com/1113446): Test failure due to excessive output.
     // "chrome://discards",
@@ -219,6 +216,7 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://invalidations",
     "chrome://local-state",
     "chrome://management",
+    "chrome://md-user-manager",
     "chrome://media-engagement",
     "chrome://media-feeds",
     "chrome://media-history",
@@ -232,6 +230,7 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://network-errors",
     "chrome://new-tab-page",
     "chrome://newtab",
+    "chrome://notifications-internals",
     "chrome://ntp-tiles-internals",
     "chrome://omnibox",
     "chrome://password-manager-internals",
@@ -249,8 +248,10 @@ static constexpr const char* const kChromeUrls[] = {
     // TODO(crbug.com/1115600): DCHECK failure when opening
     // chrome://signin-dice-web-intercept.
     // "chrome://signin-dice-web-intercept",
+    "chrome://signin-email-confirmation",
     "chrome://signin-internals",
     "chrome://site-engagement",
+    "chrome://snippets-internals",
     "chrome://suggestions",
     // TODO(crbug.com/1099564): Navigating to chrome://sync-confirmation and
     // quickly navigating away cause DCHECK failure.
@@ -270,12 +271,12 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://version",
     "chrome://webrtc-internals",
     "chrome://webrtc-logs",
+    "chrome://welcome",
 #if defined(OS_ANDROID)
     "chrome://explore-sites-internals",
     "chrome://internals/notifications",
     "chrome://internals/query-tiles",
     "chrome://offline-internals",
-    "chrome://snippets-internals",
     "chrome://webapks",
 #endif
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -295,6 +296,7 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://crostini-installer",
     "chrome://cryptohome",
     "chrome://drive-internals",
+    "chrome://first-run",
     "chrome://help-app",
     "chrome://internet-config-dialog",
     "chrome://internet-detail-dialog",
@@ -313,16 +315,10 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://smb-share-dialog",
     "chrome://supervised-user-internals",
     "chrome://sys-internals",
+    // TODO(crbug.com/1115643): DCHECK failure when opening
+    // chrome-untrusted://crosh.
+    // "chrome-untrusted://crosh",
     "chrome-untrusted://terminal",
-#endif
-#if !BUILDFLAG(IS_CHROMEOS_ASH)
-    "chrome://apps",
-    "chrome://browser-switch",
-    "chrome://md-user-manager",
-    "chrome://signin-email-confirmation",
-#endif
-#if defined(OS_WIN)
-    "chrome://conflicts",
 #endif
 };
 
