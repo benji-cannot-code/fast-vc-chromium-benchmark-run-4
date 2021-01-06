e43b95537f0b29b2a87618c270c696e4424cd2fb
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-01-06 21:55:06 +0000

    Revert "[Subresource Filter] Add content settings-based adjustment to WebLayer"
    This reverts commit cd5cde24c35e1c92f9b41a5287dcd4b6e86d766b.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 840681 as the
    culprit for flakes in the build cycles as shown on:
    https://analysis.chromium.org/p/chromium/flake-portal/analysis/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vY2Q1Y2RlMjRjMzVlMWM5MmY5YjQxYTUyODdkY2Q0YjZlODZkNzY2Ygw
    
    Sample Failed Build: https://ci.chromium.org/b/8858820663595122608
    
    Sample Failed Step: weblayer_browsertests
    
    Sample Flaky Test: SubresourceFilterBrowserTest.ContentSettingsAllowlist_DoNotActivate
    
    Original change's description:
    > [Subresource Filter] Add content settings-based adjustment to WebLayer
    > 
    > This CL incorporates Chrome's adjustment of subresource filter page
    > activation decisions based on content settings within WebLayer. To do
    > so, we have the shared SubresourceFilterSafeBrowsingActivationThrottle
    > directly invoke the logic in question rather than having
    > ChromeSubresourceFilterClient do so. That change necessitates some
    > others, also made in this CL:
    > - WebLayer brings up ProfileInteractionManager.
    > - SubresourceFilterClient provides an accessor for
    >   ProfileInteractionManager.
    > - SubresourceFilterSafeBrowsingActivationThrottle calls the adjustment
    >   logic via a new Delegate interface rather than doing so via
    >   SubresourceFilterClient. This interface is implemented by
    >   ProfileInteractionManager in production while allowing for porting of
    >   tests that provide custom (or nil) stub logic.
    > 
    > We also add browsertests of this interaction in //weblayer.
    > 
    > Note that this CL does not do any incorporation of the relevant *UI*
    > for content/site settings of ad blocking in WebLayer; that will be
    > followup work.
    > 
    > Bug: 1116095
    > Change-Id: I394e62f12d5209a4302ffa57de9095c861b59613
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2603574
    > Commit-Queue: Colin Blundell <blundell@chromium.org>
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#840681}
    
    
    Change-Id: Ifda417fda7229cd6c7eca17174b65cdf6d314193
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1116095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611616
    Cr-Commit-Position: refs/heads/master@{#840780}

--- chrome/browser/subresource_filter/chrome_subresource_filter_client.cc
+++ chrome/browser/subresource_filter/chrome_subresource_filter_client.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/subresource_filter/core/common/activation_decision.h"
 #include "components/subresource_filter/core/common/activation_scope.h"
 #include "components/subresource_filter/core/mojom/subresource_filter.mojom.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
@@ -98,6 +99,18 @@ void ChromeSubresourceFilterClient::ShowNotification() {
   }
 }
 
+subresource_filter::mojom::ActivationLevel
+ChromeSubresourceFilterClient::OnPageActivationComputed(
+    content::NavigationHandle* navigation_handle,
+    subresource_filter::mojom::ActivationLevel initial_activation_level,
+    subresource_filter::ActivationDecision* decision) {
+  // TODO(crbug.com/1116095): Once SafeBrowsingActivationThrottle knows about
+  // ProfileInteractionManager, it can invoke ProfileInteractionManager directly
+  // and SubresourceFilterClient::OnPageActivationComputed() can be eliminated.
+  return profile_interaction_manager_->OnPageActivationComputed(
+      navigation_handle, initial_activation_level, decision);
+}
+
 void ChromeSubresourceFilterClient::OnAdsViolationTriggered(
     content::RenderFrameHost* rfh,
     subresource_filter::mojom::AdsViolation triggered_violation) {
@@ -115,11 +128,6 @@ ChromeSubresourceFilterClient::GetSafeBrowsingDatabaseManager() {
       g_browser_process->safe_browsing_service();
   return safe_browsing_service ? safe_browsing_service->database_manager()
                                : nullptr;
-}
-
-subresource_filter::ProfileInteractionManager*
-ChromeSubresourceFilterClient::GetProfileInteractionManager() {
-  return profile_interaction_manager_.get();
 }
 
 void ChromeSubresourceFilterClient::ShowUI(const GURL& url) {
--- components/subresource_filter/content/browser/profile_interaction_manager.h
+++ components/subresource_filter/content/browser/profile_interaction_manager.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SUBRESOURCE_FILTER_CONTENT_BROWSER_PROFILE_INTERACTION_MANAGER_H_
 #define COMPONENTS_SUBRESOURCE_FILTER_CONTENT_BROWSER_PROFILE_INTERACTION_MANAGER_H_
 
-#include "components/subresource_filter/content/browser/subresource_filter_safe_browsing_activation_throttle.h"
 #include "components/subresource_filter/core/common/activation_decision.h"
 #include "components/subresource_filter/core/mojom/subresource_filter.mojom.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -23,9 +22,7 @@ class SubresourceFilterProfileContext;
 // Class that manages interaction between interaction between the
 // per-navigation/per-tab subresource filter objects (i.e., the throttles and
 // throttle manager) and the per-profile objects (e.g., content settings).
-class ProfileInteractionManager
-    : public content::WebContentsObserver,
-      public SubresourceFilterSafeBrowsingActivationThrottle::Delegate {
+class ProfileInteractionManager : public content::WebContentsObserver {
  public:
   ProfileInteractionManager(content::WebContents* web_contents,
                             SubresourceFilterProfileContext* profile_context);
@@ -47,11 +44,19 @@ class ProfileInteractionManager
   void OnAdsViolationTriggered(content::RenderFrameHost* rfh,
                                mojom::AdsViolation triggered_violation);
 
-  // SubresourceFilterSafeBrowsingActivationThrottle::Delegate:
+  // Called when the initial activation decision has been computed by the
+  // safe browsing activation throttle. This object then applies any adjustments
+  // based on relevant state of the Profile (e.g., content settings). Returns
+  // the effective activation for this navigation.
+  //
+  // Note: |decision| is guaranteed to be non-nullptr, and can be modified by
+  // this method if any decision changes.
+  //
+  // Precondition: The navigation must be a main frame navigation.
   mojom::ActivationLevel OnPageActivationComputed(
       content::NavigationHandle* navigation_handle,
       mojom::ActivationLevel initial_activation_level,
-      ActivationDecision* decision) override;
+      ActivationDecision* decision);
 
  private:
   // Unowned and must outlive this object.
