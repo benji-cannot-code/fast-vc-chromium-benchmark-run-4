00ab2545331ad68e3050834ab9bf39b683c7aba1
Gordon Seto <gordonseto@google.com>
2021-01-06 01:11:08 +0000

    [CrOS Settings] Create ESimManagerListenerBehavior.
    Create ESimManagerListenerBehavior, a behavior for observing updates
    to the device's eSIM manager, EUICCs and eSIM profiles. This is needed
    to update the network settings page whenever its eSIM profiles change
    state.
    
    This behavior is required to implement crrev.com/c/2605399, "Install
    pending eSIM profile from network settings."
    
    Bug: 1093185
    Change-Id: Ia996da718e7e3b81ad3fd28ef60d69a54757f84c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611081
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/master@{#840390}

---
+++ ui/webui/resources/cr_components/chromeos/cellular_setup/esim_manager_listener_behavior.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// #import {getESimManagerRemote} from './mojo_interface_provider.m.js';
+
+/**
+ * @fileoverview Polymer behavior for observing ESimManagerObserver
+ * events.
+ */
+
+/** @polymerBehavior */
+/* #export */ const ESimManagerListenerBehavior = {
+  /** @private {?chromeos.cellularSetup.mojom.ESimManagerObserver} */
+  observer_: null,
+
+  /** @override */
+  attached() {
+    this.observer_ =
+        new chromeos.cellularSetup.mojom.ESimManagerObserverReceiver(this);
+    cellular_setup.getESimManagerRemote().addObserver(
+        this.observer_.$.bindNewPipeAndPassRemote());
+  },
+
+  // ESimManagerObserver methods. Override these in the implementation.
+
+  onAvailableEuiccListChanged() {},
+
+  /**
+   * @param {!chromeos.cellularSetup.mojom.EuiccRemote} euicc
+   */
+  onProfileListChanged(euicc) {},
+
+  /**
+   * @param {!chromeos.cellularSetup.mojom.EuiccRemote} euicc
+   */
+  onEuiccChanged(euicc) {},
+
+  /**
+   * @param {!chromeos.cellularSetup.mojom.ESimProfileRemote} profile
+   */
+  onProfileChanged(profile) {},
+};
