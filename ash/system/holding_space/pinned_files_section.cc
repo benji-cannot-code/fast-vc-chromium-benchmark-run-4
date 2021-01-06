c264e85c90b95a9c543ac76bf6eac530ddb7e841
David Black <dmblack@google.com>
2021-01-06 20:32:22 +0000

    Record metric when Files app chip in holding space UI is clicked.
    The Files app chip appears in the pinned files section of holding
    space UI when the user has not yet pinned a file.
    
    Bug: 1163365
    Change-Id: I39412de8f8e40a231ddf1fec81a0b91149ca479b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2612118
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/master@{#840747}

--- ash/system/holding_space/pinned_files_section.cc
+++ ash/system/holding_space/pinned_files_section.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/holding_space/holding_space_constants.h"
 #include "ash/public/cpp/holding_space/holding_space_controller.h"
 #include "ash/public/cpp/holding_space/holding_space_item.h"
+#include "ash/public/cpp/holding_space/holding_space_metrics.h"
 #include "ash/public/cpp/holding_space/holding_space_prefs.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/session/session_controller_impl.h"
@@ -120,6 +121,9 @@ class FilesAppChip : public views::Button {
   }
 
   void OnPressed(const ui::Event& event) {
+    holding_space_metrics::RecordFilesAppChipAction(
+        holding_space_metrics::FilesAppChipAction::kClick);
+
     HoldingSpaceController::Get()->client()->OpenMyFiles(base::DoNothing());
   }
 };
