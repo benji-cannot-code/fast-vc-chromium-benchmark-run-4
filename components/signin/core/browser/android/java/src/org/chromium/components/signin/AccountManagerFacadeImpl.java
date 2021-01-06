d5c46685e98f79ba2a2c2bb262fce7e823ed0fac
Tanmoy Mollik <triploblastic@chromium.org>
2021-01-06 18:16:09 +0000

    [Android] Add a histogram to record number of android accounts on device
    Adds a histogram that records number of android accounts present on
    device even if the user isn't signed in.
    
    Bug: 1025410
    Change-Id: Ide66597ea3da5857e6f7ccd5bec018b54ebc03e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2607306
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840676}

--- components/signin/core/browser/android/java/src/org/chromium/components/signin/AccountManagerFacadeImpl.java
+++ components/signin/core/browser/android/java/src/org/chromium/components/signin/AccountManagerFacadeImpl.java
@@ -449,6 +449,10 @@ public class AccountManagerFacadeImpl implements AccountManagerFacade {
 
         @Override
         protected void onPostExecute(Void v) {
+            // Records number of Android accounts present on device.
+            RecordHistogram.recordExactLinearHistogram(
+                    "Signin.AndroidNumberOfDeviceAccounts", tryGetGoogleAccounts().size(), 50);
+
             for (Runnable callback : mCallbacksWaitingForCachePopulation) {
                 callback.run();
             }
