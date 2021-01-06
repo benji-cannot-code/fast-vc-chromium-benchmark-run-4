301dc948c7d27ff93bd522fcb5003a1a94e73b77
Gauthier Ambard <gambard@chromium.org>
2021-01-06 16:44:08 +0000

    Revert "[iOS] Dismiss unified consent screen from Recent Tabs."
    This reverts commit 55b69ae29c67c21a3c04874ed5cb45b1b60a13dd.
    
    Reason for revert: Breaking iOS 12 FirstRunTestCase 
    
    Original change's description:
    > [iOS] Dismiss unified consent screen from Recent Tabs.
    >
    > Fixes a bug where the unified consent screen is not dismissed when
    > interrupted by a Siri search on the Recent Tabs page.
    >
    > - Performs complete view controller cleanup on interrupt.
    > - Dismisses UnifiedConsentViewController when explicitly required.
    > - Does not explicitly release SigninCoordinator in scene controller,
    > this is already handled in the completion callback.
    >
    > Bug: 1115997
    > Change-Id: Ib4663fe1136a394020020abc76969761c5e0f170
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600835
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#840588}
    
    TBR=gambard@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,fernandex@chromium.org
    
    Change-Id: I09e836432fc7ce4d6091a3cf3795763c12a91fb5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1115997
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2612969
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840633}

--- ios/chrome/browser/ui/authentication/unified_consent/unified_consent_coordinator.mm
+++ ios/chrome/browser/ui/authentication/unified_consent/unified_consent_coordinator.mm
@@ -53,11 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.unifiedConsentMediator start];
 }
 
-- (void)stop {
-  [self.unifiedConsentViewController dismissViewControllerAnimated:YES
-                                                        completion:nil];
-}
-
 - (void)scrollToBottom {
   [self.unifiedConsentViewController scrollToBottom];
 }
