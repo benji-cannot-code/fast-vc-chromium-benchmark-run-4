eaca842b5e91620f17f2b9c95536610fd7a1a9e2
Guido Urdaneta <guidou@chromium.org>
2021-01-06 11:05:31 +0000

    [BreakoutBox] Use weak pointer in MediaStreamVideoTrackUnderlyingSink
    Replace the raw pointer to a PushableMediaStreamVideoSource with
    a weak pointer.
    
    Bug: 1162036
    Change-Id: I9b786cfd3800f8e37daabacf039e6b12e2423f76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2610036
    Auto-Submit: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840551}

--- third_party/blink/renderer/modules/mediastream/media_stream_video_track_underlying_sink.h
+++ third_party/blink/renderer/modules/mediastream/media_stream_video_track_underlying_sink.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class MediaStreamVideoSource;
 class PushableMediaStreamVideoSource;
 
 class MODULES_EXPORT MediaStreamVideoTrackUnderlyingSink
@@ -35,7 +36,7 @@ class MODULES_EXPORT MediaStreamVideoTrackUnderlyingSink
                       ExceptionState& exception_state) override;
 
  private:
-  PushableMediaStreamVideoSource* source_;
+  base::WeakPtr<MediaStreamVideoSource> source_;
 };
 
 }  // namespace blink
