6966814281ccb3a1bc6dad70dba166b7a4c6cbac
Daniel Hosseinian <dhoss@chromium.org>
2021-01-06 22:28:51 +0000

    PDF Viewer Update: Add document properties dialog
    Add a skeleton document properties dialog element containing the fields
    when the document properties will get populated.
    
    Bug: 93619
    Change-Id: Ied7ad9d8301c263cc64e902aa390d86438ac3eeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2605971
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840804}

--- chrome/browser/resources/pdf/pdf_viewer.js
+++ chrome/browser/resources/pdf/pdf_viewer.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './elements/viewer-error-screen.js';
 import './elements/viewer-password-screen.js';
 import './elements/viewer-pdf-toolbar.js';
+import './elements/viewer-properties-dialog.js';
 import './elements/viewer-zoom-toolbar.js';
 import './elements/shared-vars.js';
 // <if expr="chromeos">
@@ -245,6 +246,12 @@ export class PDFViewerElement extends PDFViewerBaseElement {
 
       /** @private */
       printingEnabled_: {
+        type: Boolean,
+        value: false,
+      },
+
+      /** @private */
+      showPropertiesDialog_: {
         type: Boolean,
         value: false,
       },
@@ -744,6 +751,19 @@ export class PDFViewerElement extends PDFViewerBaseElement {
           // Nothing else to do here. The viewport will be updated as a result
           // of a 'resize' event callback.
         });
+  }
+
+  /** @private */
+  onPropertiesClick_() {
+    assert(this.pdfViewerUpdateEnabled_);
+    assert(!this.showPropertiesDialog_);
+    this.showPropertiesDialog_ = true;
+  }
+
+  /** @private */
+  onPropertiesDialogClose_() {
+    assert(this.showPropertiesDialog_);
+    this.showPropertiesDialog_ = false;
   }
 
   /**
