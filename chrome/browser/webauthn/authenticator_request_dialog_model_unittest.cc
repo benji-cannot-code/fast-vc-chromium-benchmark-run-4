33cff2c517185a23bd8e1cadaf78cf62f4d6ac0d
Martin Kreichgauer <martinkr@google.com>
2021-01-06 20:03:38 +0000

    fido: rename 'Touch ID' to 'platform authentictor' in a few places
    To better reflect that it now handles ChromeOS as well.
    
    Also make the has_recognized_platform_authenticator_credential field in
    TransportAvailability a base::Optional to reflect that it will be unset
    if we're not making a GetAssertionRequest with a present platform
    authenticator.
    
    Bug: 1157651
    Change-Id: I2fd7d1e88c237c198250cf2e2c5eb0309d014cf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587559
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840734}

--- chrome/browser/webauthn/authenticator_request_dialog_model_unittest.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_model_unittest.cc
@@ -98,7 +98,7 @@ class AuthenticatorRequestDialogModelTest : public ::testing::Test {
 
 TEST_F(AuthenticatorRequestDialogModelTest, TransportAutoSelection) {
   enum class TransportAvailabilityParam {
-    kHasTouchIdCredential,
+    kHasPlatformCredential,
     kHasWinNativeAuthenticator,
     kHasCableExtension,
   };
@@ -128,7 +128,7 @@ TEST_F(AuthenticatorRequestDialogModelTest, TransportAutoSelection) {
       {RequestType::kGetAssertion,
        {AuthenticatorTransport::kInternal},
        {},
-       {TransportAvailabilityParam::kHasTouchIdCredential},
+       {TransportAvailabilityParam::kHasPlatformCredential},
        Step::kNotStarted},
       {RequestType::kGetAssertion,
        {AuthenticatorTransport::kCloudAssistedBluetoothLowEnergy},
@@ -155,7 +155,7 @@ TEST_F(AuthenticatorRequestDialogModelTest, TransportAutoSelection) {
       {RequestType::kGetAssertion,
        kAllTransports,
        AuthenticatorTransport::kUsbHumanInterfaceDevice,
-       {TransportAvailabilityParam::kHasTouchIdCredential},
+       {TransportAvailabilityParam::kHasPlatformCredential},
        Step::kNotStarted},
 
       // The KeyChain does not contain an allowed Touch ID credential.
@@ -198,13 +198,13 @@ TEST_F(AuthenticatorRequestDialogModelTest, TransportAutoSelection) {
       {RequestType::kGetAssertion,
        {AuthenticatorTransport::kUsbHumanInterfaceDevice},
        base::nullopt,
-       {TransportAvailabilityParam::kHasTouchIdCredential},
+       {TransportAvailabilityParam::kHasPlatformCredential},
        Step::kUsbInsertAndActivate},
       {RequestType::kGetAssertion,
        {AuthenticatorTransport::kUsbHumanInterfaceDevice,
         AuthenticatorTransport::kNearFieldCommunication},
        base::nullopt,
-       {TransportAvailabilityParam::kHasTouchIdCredential},
+       {TransportAvailabilityParam::kHasPlatformCredential},
        Step::kTransportSelection},
 
       // If caBLE is one of the allowed transports, it has second-highest
@@ -288,9 +288,9 @@ TEST_F(AuthenticatorRequestDialogModelTest, TransportAutoSelection) {
     transports_info.request_type = test_case.request_type;
     transports_info.available_transports = test_case.available_transports;
 
-    if (base::Contains(test_case.transport_params,
-                       TransportAvailabilityParam::kHasTouchIdCredential))
-      transports_info.has_recognized_mac_touch_id_credential = true;
+    transports_info.has_recognized_platform_authenticator_credential =
+        base::Contains(test_case.transport_params,
+                       TransportAvailabilityParam::kHasPlatformCredential);
 
     if (base::Contains(
             test_case.transport_params,
