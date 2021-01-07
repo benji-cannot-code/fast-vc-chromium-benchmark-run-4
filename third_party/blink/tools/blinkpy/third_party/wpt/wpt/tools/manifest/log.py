b8999c558ea11e80777bd4c63c358dfd3712a03c
Stephen McGruer <smcgruer@chromium.org>
2021-01-07 15:00:56 +0000

    Roll wpt tooling.
    This rolls up to WPT SHA e04f27b9946175edab6c09421ab5ceaa14b84539
    
    This drops the cherry-pick of 0dd83ea79e8505d4f20a633d6750636ad9634300,
    as the upstream version of that change has now landed.
    
    Bug: 1160108
    Change-Id: I6fdbb8f1453d3b6d70743614e60eaf26f9e2d29a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611551
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Commit-Queue: Jason Chase <chasej@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841054}

--- third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/manifest/log.py
+++ third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/manifest/log.py
@@ -1,16 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
-import sys
 
 logger = logging.getLogger("manifest")
 
-def setup():
+def enable_debug_logging():
     # type: () -> None
     logger.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter(logging.BASIC_FORMAT, None)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
 
 def get_logger():
     # type: () -> logging.Logger
