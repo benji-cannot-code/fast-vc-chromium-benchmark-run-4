4b98b394f4d70d28463986b5dff548216adb5bf6
Mihai Sardarescu <msarda@chromium.org>
2021-01-07 08:03:18 +0000

    Convert c/b/sync_file_system/drive_backend/ to OnPrimaryAccountChanged()
    Methods OnPrimaryAccountSet/Cleared() are deprecated, so this CL changes
    the chrome/browser/sync_file_system/drive_backend/* to override the
    method OnPrimaryAccountChanged().
    
    Bug: 1163126
    Change-Id: Ib619c24eff3778cf658acfef90009e240d7cea09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611088
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Auto-Submit: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840964}

--- chrome/browser/apps/platform_apps/api/sync_file_system/sync_file_system_browsertest.cc
+++ chrome/browser/apps/platform_apps/api/sync_file_system/sync_file_system_browsertest.cc
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/quota/quota_manager.h"
 #include "third_party/leveldatabase/leveldb_chrome.h"
 
+using signin::PrimaryAccountChangeEvent;
+
 namespace sync_file_system {
 
 namespace {
@@ -126,11 +128,6 @@ class SyncFileSystemTest : public extensions::PlatformAppBrowserTest,
 
   void SignIn() {
     identity_test_env_->SetPrimaryAccount(kEmail);
-
-    // It's necessary to invoke this method manually as the observer callback is
-    // not triggered on ChromeOS.
-    sync_engine()->OnPrimaryAccountSet(
-        identity_test_env_->identity_manager()->GetPrimaryAccountInfo());
   }
 
   void SetSyncEnabled(bool enabled) {
@@ -185,8 +182,11 @@ IN_PROC_BROWSER_TEST_F(SyncFileSystemTest, AuthorizationTest) {
   // service.  Wait for the completion and resume the app.
   WaitUntilIdle();
 
-  sync_engine()->OnPrimaryAccountCleared(
-      identity_manager()->GetPrimaryAccountInfo());
+  sync_engine()->OnPrimaryAccountChanged(
+      PrimaryAccountChangeEvent(PrimaryAccountChangeEvent::State(
+                                    identity_manager()->GetPrimaryAccountInfo(),
+                                    signin::ConsentLevel::kSync),
+                                PrimaryAccountChangeEvent::State()));
   foo_created.Reply("resume");
 
   ASSERT_TRUE(bar_created.WaitUntilSatisfied());
@@ -199,8 +199,11 @@ IN_PROC_BROWSER_TEST_F(SyncFileSystemTest, AuthorizationTest) {
   EXPECT_EQ(REMOTE_SERVICE_AUTHENTICATION_REQUIRED,
             sync_engine()->GetCurrentState());
 
-  sync_engine()->OnPrimaryAccountSet(
-      identity_manager()->GetPrimaryAccountInfo());
+  sync_engine()->OnPrimaryAccountChanged(
+      PrimaryAccountChangeEvent(PrimaryAccountChangeEvent::State(),
+                                PrimaryAccountChangeEvent::State(
+                                    identity_manager()->GetPrimaryAccountInfo(),
+                                    signin::ConsentLevel::kSync)));
   WaitUntilIdle();
 
   bar_created.Reply("resume");
