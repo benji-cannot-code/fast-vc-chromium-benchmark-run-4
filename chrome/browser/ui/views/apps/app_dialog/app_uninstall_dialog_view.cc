c8e01a416f9da085bc7abb6725a6adb3009d9a6c
Swapnil <swapnilgupta@google.com>
2021-01-07 10:37:12 +0000

    [Extensions] Use policy-enforced update URL in app uninstall dialog
    This change gives ExtensionManagement control over update URL used when
    updating extensions. Previously only first install was affected, all
    later updates used update URL from the extension manifest. Now update
    URL from the policy can be used if extension management said so.
    This change is split into multiple CLs.
    
    Update URL from policy is handled in
    https://chromium-review.googlesource.com/c/chromium/src/+/2593260.
    
    Bug: b:168097163
    Change-Id: I73c4bc11c1a18bcbadf73bcc611a28cf880517bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593266
    Commit-Queue: Swapnil Gupta <swapnilgupta@google.com>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840998}

--- chrome/browser/ui/views/apps/app_dialog/app_uninstall_dialog_view.cc
+++ chrome/browser/ui/views/apps/app_dialog/app_uninstall_dialog_view.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/extensions/extension_management.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_navigator.h"
@@ -277,7 +278,9 @@ void AppUninstallDialogView::InitializeViewForExtension(
           app_id);
   DCHECK(extension);
 
-  if (extensions::ManifestURL::UpdatesFromGallery(extension)) {
+  extensions::ExtensionManagement* extension_management =
+      extensions::ExtensionManagementFactory::GetForBrowserContext(profile);
+  if (extension_management->UpdatesFromWebstore(*extension)) {
     auto report_abuse_checkbox = std::make_unique<views::Checkbox>(
         l10n_util::GetStringUTF16(IDS_EXTENSION_PROMPT_UNINSTALL_REPORT_ABUSE));
     report_abuse_checkbox->SetMultiLine(true);
