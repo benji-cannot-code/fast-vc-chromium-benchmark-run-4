3969a9dc36d52d54cfc6bb7ed7ec884fbba93b83
Dale Curtis <dalecurtis@chromium.org>
2021-01-07 18:34:06 +0000

    Disable VP9 hardware decoding on Intel GPUs with hybrid decoders.
    We've disabled these on Windows and ChromeOS, so it's no surprise that
    we need to disable them on macOS as well.
    
    R=zmo
    
    Fixed: 1163111
    Change-Id: I406e6ebd1d27da84964e46efb9582ce77f904dac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613560
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841116}

--- media/gpu/mac/vt_video_decode_accelerator_mac.h
+++ media/gpu/mac/vt_video_decode_accelerator_mac.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "base/threading/thread_checker.h"
 #include "base/trace_event/memory_dump_provider.h"
+#include "gpu/config/gpu_driver_bug_workarounds.h"
 #include "media/base/media_log.h"
 #include "media/gpu/gpu_video_decode_accelerator_helpers.h"
 #include "media/gpu/media_gpu_export.h"
@@ -45,6 +46,7 @@ class VTVideoDecodeAccelerator : public VideoDecodeAccelerator,
                                  public base::trace_event::MemoryDumpProvider {
  public:
   VTVideoDecodeAccelerator(const GpuVideoDecodeGLClient& gl_client_,
+                           const gpu::GpuDriverBugWorkarounds& workarounds,
                            MediaLog* media_log);
 
   ~VTVideoDecodeAccelerator() override;
@@ -75,7 +77,8 @@ class VTVideoDecodeAccelerator : public VideoDecodeAccelerator,
               OSStatus status,
               CVImageBufferRef image_buffer);
 
-  static VideoDecodeAccelerator::SupportedProfiles GetSupportedProfiles();
+  static VideoDecodeAccelerator::SupportedProfiles GetSupportedProfiles(
+      const gpu::GpuDriverBugWorkarounds& workarounds);
 
  private:
   // Logged to UMA, so never reuse values. Make sure to update
@@ -215,6 +218,7 @@ class VTVideoDecodeAccelerator : public VideoDecodeAccelerator,
   // GPU thread state.
   //
   const GpuVideoDecodeGLClient gl_client_;
+  const gpu::GpuDriverBugWorkarounds workarounds_;
   MediaLog* media_log_;
 
   VideoDecodeAccelerator::Client* client_ = nullptr;
