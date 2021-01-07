02122824e4b9dd2bdfa3e38a6bb324e7edab223f
Sami Kyostila <skyostil@chromium.org>
2021-01-07 09:31:46 +0000

    sequence_manager: Fix potential crash when sweeping away last task in queue
    If the last delayed task in a queue is cancelled and swept away, we
    ended up comparing an invalid iterator with the end of the queue.
    Depending on where the new end of the queue ends up being, this can
    cause us to try to cancel an invalid task, potentially triggering a
    crash.
    
    Bug: 1155905
    Change-Id: I9df4a2802dbaf3da733f8bde72f394b8fe0b7fab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2612970
    Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#840984}

--- base/task/sequence_manager/task_queue_impl.cc
+++ base/task/sequence_manager/task_queue_impl.cc
@@ -1408,7 +1408,7 @@ void TaskQueueImpl::DelayedIncomingQueue::SweepCancelledTasks(
   // canceled tasks in place.
   bool task_deleted = false;
   auto it = queue_.c.begin();
-  while (it != queue_.c.end()) {
+  while (!queue_.c.empty() && it != queue_.c.end()) {
     // TODO(crbug.com/1155905): Remove after figuring out the cause of the
     // crash.
     sequence_manager->RecordCrashKeys(*it);
@@ -1418,6 +1418,8 @@ void TaskQueueImpl::DelayedIncomingQueue::SweepCancelledTasks(
       *it = std::move(queue_.c.back());
       queue_.c.pop_back();
       task_deleted = true;
+      // Note: if we just removed the last task in the queue, |it| is now
+      // invalid and shouldn't be used.
     } else {
       it++;
     }
