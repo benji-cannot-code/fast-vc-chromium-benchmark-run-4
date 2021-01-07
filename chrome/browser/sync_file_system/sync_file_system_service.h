1e55ec2000ebc0d45baefcd153af201dd2b3ed07
Thomas Guilbert <tguilbert@chromium.org>
2021-01-07 07:02:30 +0000

    Convert sync_callbacks.h to OnceCallback
    This CL converts SyncFileMetadataCallback and SyncFileStatusCallback to
    OnceCallbacks.
    
    It also deletes the unused SyncFileSetCallback.
    
    Bug: 1152272
    Change-Id: Ib85b776c602cdffb871f79ad158f0726c7ed9594
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2606603
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840953}

--- chrome/browser/sync_file_system/sync_file_system_service.h
+++ chrome/browser/sync_file_system/sync_file_system_service.h
@@ -70,7 +70,7 @@ class SyncFileSystemService
 
   // Returns the file |url|'s sync status.
   void GetFileSyncStatus(const storage::FileSystemURL& url,
-                         const SyncFileStatusCallback& callback);
+                         SyncFileStatusCallback callback);
 
   void AddSyncEventObserver(SyncEventObserver* observer);
   void RemoveSyncEventObserver(SyncEventObserver* observer);
@@ -128,7 +128,7 @@ class SyncFileSystemService
   // Overrides sync_enabled_ setting. This should be called only by tests.
   void SetSyncEnabledForTesting(bool enabled);
 
-  void DidGetLocalChangeStatus(const SyncFileStatusCallback& callback,
+  void DidGetLocalChangeStatus(SyncFileStatusCallback callback,
                                SyncStatusCode status,
                                bool has_pending_local_changes);
 
