81895afa27b479c552942cb04e70707ecd376bd5
Song Fangzhen <songfangzhen@bytedance.com>
2021-01-07 02:14:45 +0000

    Direct Sockets: Update PopulateLocalAddr().
    As Glen's suggestion in
    https://chromium-review.googlesource.com/c/chromium/src/+/2603063
    
    We update PopulateLocalAddr() to make this function more readable.
    
    Bug: 905818
    Change-Id: I39f131dcdb20b8990e3018358bc4ba316c59f0a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2612610
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Ke He <kehe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840880}

--- content/browser/direct_sockets/direct_sockets_service_impl.cc
+++ content/browser/direct_sockets/direct_sockets_service_impl.cc
@@ -38,17 +38,19 @@ network::mojom::NetworkContext*& GetNetworkContextForTesting() {
   return network_context;
 }
 
-// Populate |local_addr| from options.
-void PopulateLocalAddr(const blink::mojom::DirectSocketOptions& options,
-                       base::Optional<net::IPEndPoint>& local_addr) {
-  DCHECK(!local_addr);
+// Get local ip address from options.
+base::Optional<net::IPEndPoint> GetLocalAddr(
+    const blink::mojom::DirectSocketOptions& options) {
+  base::Optional<net::IPEndPoint> local_addr = base::nullopt;
   if (!options.local_hostname)
-    return;
+    return local_addr;
 
   net::IPAddress local_address;
   bool success = local_address.AssignFromIPLiteral(*options.local_hostname);
   if (success)
     local_addr = net::IPEndPoint(local_address, options.local_port);
+
+  return local_addr;
 }
 
 }  // namespace
@@ -143,8 +145,7 @@ class DirectSocketsServiceImpl::ResolveHostAndOpenSocket final
     }
 
     DCHECK(resolved_addresses && !resolved_addresses->empty());
-    base::Optional<net::IPEndPoint> local_addr = base::nullopt;
-    PopulateLocalAddr(*options_, local_addr);
+    const base::Optional<net::IPEndPoint> local_addr = GetLocalAddr(*options_);
 
     network::mojom::TCPConnectedSocketOptionsPtr tcp_connected_socket_options =
         network::mojom::TCPConnectedSocketOptions::New();
@@ -181,8 +182,7 @@ class DirectSocketsServiceImpl::ResolveHostAndOpenSocket final
     }
 
     DCHECK(resolved_addresses && !resolved_addresses->empty());
-    base::Optional<net::IPEndPoint> local_addr = base::nullopt;
-    PopulateLocalAddr(*options_, local_addr);
+    base::Optional<net::IPEndPoint> local_addr = GetLocalAddr(*options_);
 
     // TODO(crbug.com/1119620): network_context_->CreateUDPSocket
     // TODO(crbug.com/1119620): Connect(remote_addr, udp_socket_options)
@@ -275,10 +275,10 @@ void DirectSocketsServiceImpl::SetNetworkContextForTesting(
 }
 
 // static
-void DirectSocketsServiceImpl::PopulateLocalAddrForTesting(
-    const blink::mojom::DirectSocketOptions& options,
-    base::Optional<net::IPEndPoint>& local_addr) {
-  PopulateLocalAddr(options, local_addr);
+base::Optional<net::IPEndPoint>
+DirectSocketsServiceImpl::GetLocalAddrForTesting(
+    const blink::mojom::DirectSocketOptions& options) {
+  return GetLocalAddr(options);
 }
 
 void DirectSocketsServiceImpl::RenderFrameDeleted(
