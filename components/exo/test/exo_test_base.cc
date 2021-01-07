513481eeb88baaf9a598ba38ddb9a5a7d06b19e8
Mitsuru Oshima <oshima@chromium.org>
2021-01-07 21:38:32 +0000

    Set window properties when creating widget, not after.
    Optimize ArcOverlayManager so that it observe arc windows only.
    
    Bug: None
    Test: covered by unittest
    Change-Id: Id8272cca58a555689cd12a976a812bc25b4b28b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2610625
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841197}

--- components/exo/test/exo_test_base.cc
+++ components/exo/test/exo_test_base.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/wm_helper_chromeos.h"
 #include "components/viz/service/frame_sinks/frame_sink_manager_impl.h"
 #include "components/viz/service/surfaces/surface_manager.h"
+#include "ui/aura/client/aura_constants.h"
 #include "ui/aura/env.h"
 #include "ui/base/ime/init/input_method_factory.h"
 #include "ui/compositor/test/in_process_context_factory.h"
@@ -20,6 +21,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace exo {
 namespace test {
+namespace {
+
+class TestPropertyResolver : public WMHelper::AppPropertyResolver {
+ public:
+  TestPropertyResolver() = default;
+  TestPropertyResolver(const TestPropertyResolver& other) = delete;
+  TestPropertyResolver& operator=(const TestPropertyResolver& other) = delete;
+  ~TestPropertyResolver() override = default;
+
+  // AppPropertyResolver:
+  void PopulateProperties(
+      const std::string& app_id,
+      const std::string& startup_id,
+      bool for_creation,
+      ui::PropertyHandler& out_properties_container) override {
+    LOG(ERROR) << "AppId=" << app_id;
+    if (app_id == "arc")
+      out_properties_container.SetProperty(aura::client::kAppType,
+                                           (int)ash::AppType::ARC_APP);
+  }
+};
+
+}  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
 // ExoTestBase, public:
@@ -41,6 +65,8 @@ ExoTestBase::~ExoTestBase() = default;
 void ExoTestBase::SetUp() {
   AshTestBase::SetUp();
   wm_helper_ = std::make_unique<WMHelperChromeOS>();
+  wm_helper_->RegisterAppPropertyResolver(
+      base::WrapUnique(new TestPropertyResolver()));
 }
 
 void ExoTestBase::TearDown() {
