d891e0bc4bf1a9bd1a97a383935efb6efcb2c126
David Roger <droger@chromium.org>
2021-01-07 10:32:51 +0000

    [profiles] DiceInterceptedSessionStartupHelper calls multilogin directly
    This CL changes the way DiceInterceptedSessionStartupHelper adds the
    account on the web.
    Previously it was mostly waiting for the reconcilor to trigger, and
    making sure that the process is not stuck (by forcing calls to list
    accounts).
    
    The new behavior is more proactive: when the profile is new, it blocks
    the reconcilor and forces a call to multilogin directly.
    The advantages are:
    - it is easier to control and hopefully it should be less brittle.
    - it should be faster:
      * the previous flow was doing an extra /ListAccounts call after the
        reconciliation, which is now removed
      * the previous flow was using MergeSession while the new flow uses
        multilogin, which should be faster on desktop (no need to fetch an
        access token)
    
    When the profile is not new, it's not possible to simply use multilogin,
    as there may already be accounts in the profile. The reconcilor is still
    used in this case, as it has the logic to handle this.
    
    Note: it would be possible to use MergeSession instead of Multilogin, as
    this would work for all profiles (including existing profiles). However
    MergeSession is now discouraged and thus we avoid adding a new usage
    here.
    
    This CL also improves histograms.
    
    Bug: 1151313
    Change-Id: I9e0f6034abd845047226a3b592ce9a1dabf0c7d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600909
    Commit-Queue: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#840690}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2612845
    Cr-Commit-Position: refs/heads/master@{#840995}

--- chrome/browser/signin/dice_web_signin_interceptor_browsertest.cc
+++ chrome/browser/signin/dice_web_signin_interceptor_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/dice_web_signin_interceptor.h"
 
 #include <map>
+#include <string>
 
 #include "base/memory/weak_ptr.h"
 #include "base/run_loop.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_window.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
 #include "chrome/browser/signin/chrome_signin_client_test_util.h"
+#include "chrome/browser/signin/dice_intercepted_session_startup_helper.h"
 #include "chrome/browser/signin/dice_web_signin_interceptor_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
@@ -39,11 +41,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "components/signin/public/identity_manager/primary_account_mutator.h"
 #include "content/public/test/browser_test.h"
+#include "google_apis/gaia/gaia_urls.h"
 #include "services/network/test/test_url_loader_factory.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
 namespace {
+
+// Fake response for OAuth multilogin.
+const char kMultiloginSuccessResponse[] =
+    R"()]}'
+       {
+         "status": "OK",
+         "cookies":[
+           {
+             "name":"SID",
+             "value":"SID_value",
+             "domain":".google.fr",
+             "path":"/",
+             "isSecure":true,
+             "isHttpOnly":false,
+             "priority":"HIGH",
+             "maxAge":63070000
+           }
+         ]
+       }
+      )";
+
 class FakeDiceWebSigninInterceptorDelegate;
 
 class FakeBubbleHandle : public ScopedDiceWebSigninInterceptionBubbleHandle,
@@ -72,6 +96,7 @@ class FakeDiceWebSigninInterceptorDelegate
                                   SigninInterceptionResult::kAccepted));
     return bubble_handle;
   }
+
   void ShowProfileCustomizationBubble(Browser* browser) override {
     EXPECT_FALSE(customized_browser_)
         << "Customization must be shown only once.";
@@ -79,6 +104,7 @@ class FakeDiceWebSigninInterceptorDelegate
   }
 
   Browser* customized_browser() { return customized_browser_; }
+
   void set_expected_interception_type(
       DiceWebSigninInterceptor::SigninInterceptionType type) {
     expected_interception_type_ = type;
@@ -158,8 +184,18 @@ void CheckHistograms(const base::HistogramTester& histogram_tester,
                                     profile_creation_count);
   histogram_tester.ExpectTotalCount("Signin.Intercept.ProfileSwitchDuration",
                                     profile_switch_count);
-  histogram_tester.ExpectTotalCount("Signin.Intercept.SessionStartupDuration",
-                                    1);
+  histogram_tester.ExpectTotalCount(
+      "Signin.Intercept.SessionStartupDuration.Multilogin",
+      profile_creation_count);
+  histogram_tester.ExpectTotalCount(
+      "Signin.Intercept.SessionStartupDuration.Reconcilor",
+      profile_switch_count);
+  histogram_tester.ExpectUniqueSample(
+      "Signin.Intercept.SessionStartupResult",
+      profile_switch_count
+          ? DiceInterceptedSessionStartupHelper::Result::kReconcilorSuccess
+          : DiceInterceptedSessionStartupHelper::Result::kMultiloginSuccess,
+      1);
 }
 
 }  // namespace
@@ -197,6 +233,7 @@ class DiceWebSigninInterceptorBrowserTest : public InProcessBrowserTest {
   }
 
  private:
+  // InProcessBrowserTest:
   void SetUpOnMainThread() override {
     ASSERT_TRUE(embedded_test_server()->Start());
     identity_test_env_profile_adaptor_ =
@@ -268,12 +305,30 @@ IN_PROC_BROWSER_TEST_F(DiceWebSigninInterceptorBrowserTest, InterceptionTest) {
   DCHECK(account_info.IsValid());
   identity_test_env()->UpdateAccountInfoForAccount(account_info);
 
+  // Instantly return from Gaia calls, to avoid timing out when injecting the
+  // account in the new profile.
+  network::TestURLLoaderFactory* loader_factory = test_url_loader_factory();
+  loader_factory->SetInterceptor(base::BindLambdaForTesting(
+      [loader_factory](const network::ResourceRequest& request) {
+        std::string path = request.url.path();
+        if (path == "/ListAccounts" || path == "/GetCheckConnectionInfo") {
+          loader_factory->AddResponse(request.url.spec(), std::string());
+          return;
+        }
+        if (path == "/oauth/multilogin") {
+          loader_factory->AddResponse(request.url.spec(),
+                                      kMultiloginSuccessResponse);
+          return;
+        }
+      }));
+
   // Add a tab.
   GURL intercepted_url = embedded_test_server()->GetURL("/defaultresponse");
   content::WebContents* web_contents = AddTab(intercepted_url);
   int original_tab_count = browser()->tab_strip_model()->count();
 
   // Do the signin interception.
+  EXPECT_EQ(BrowserList::GetInstance()->size(), 1u);
   Profile* new_profile =
       InterceptAndWaitProfileCreation(web_contents, account_info.account_id);
   ASSERT_TRUE(new_profile);
@@ -284,6 +339,9 @@ IN_PROC_BROWSER_TEST_F(DiceWebSigninInterceptorBrowserTest, InterceptionTest) {
       IdentityManagerFactory::GetForProfile(new_profile);
   EXPECT_TRUE(new_identity_manager->HasAccountWithRefreshToken(
       account_info.account_id));
+
+  IdentityTestEnvironmentProfileAdaptor adaptor(new_profile);
+  adaptor.identity_test_env()->SetAutomaticIssueOfAccessTokens(true);
 
   // Check the profile name.
   ProfileAttributesEntry* entry = nullptr;
@@ -297,15 +355,10 @@ IN_PROC_BROWSER_TEST_F(DiceWebSigninInterceptorBrowserTest, InterceptionTest) {
   EXPECT_TRUE(ThemeServiceFactory::GetForProfile(new_profile)
                   ->UsingAutogeneratedTheme());
 
-  // Add the account to the cookies (simulates the account reconcilor).
-  EXPECT_EQ(BrowserList::GetInstance()->size(), 1u);
-  signin::SetCookieAccounts(new_identity_manager, test_url_loader_factory(),
-                            {{account_info.email, account_info.gaia}});
-
   // A browser has been created for the new profile and the tab was moved there.
-  ASSERT_EQ(BrowserList::GetInstance()->size(), 2u);
-  Browser* added_browser = BrowserList::GetInstance()->get(1);
+  Browser* added_browser = ui_test_utils::WaitForBrowserToOpen();
   ASSERT_TRUE(added_browser);
+  ASSERT_EQ(BrowserList::GetInstance()->size(), 2u);
   EXPECT_EQ(added_browser->profile(), new_profile);
   EXPECT_EQ(browser()->tab_strip_model()->count(), original_tab_count - 1);
   EXPECT_EQ(added_browser->tab_strip_model()->GetActiveWebContents()->GetURL(),
