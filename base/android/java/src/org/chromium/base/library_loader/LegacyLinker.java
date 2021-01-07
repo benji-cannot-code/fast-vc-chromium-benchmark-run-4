c459133c9b8c43a4e03eb3b0825b5db50ff6bd40
Egor Pasko <pasko@chromium.org>
2021-01-07 10:24:33 +0000

    base/android: Make Linker an implementation detail of LibraryLoader
    Restrict Linker functionality to be "package private", do not expose
    the whole Linker API to clients.
    
    Reduce verbosity in code comments about the Linker, and reformulate them
    in terms of RELRO producer/consumer, rather than process type. This way
    the App Zygote can become the RELRO producer without contradicting all
    these explanations.
    
    Remove ChromiumLinkerParams. Previously I thought that in the new
    protocol it would expand, but now I realized that it is better to extend
    the messages that happen later in Linker lifetime.
    
    Extract the bundle exchange between linkers in different processes into
    LibraryLoader's inner class. This looks cleaner because it makes the
    communication protocol handled in one compact place. Before this change
    it was nontrivial to verify that communication is not racy.
    
    The plan is to add functionality to the MultiProcessMessageHandler to
    make it possible to sometimes move the RELRO FD from isolated processes
    into the browser process, without leaking a lot of knowledge about it
    outside of the package o.c.b.library_loader. With this change the
    MultiProcessMessageHandler will make sense to move to another file.
    
    Bug: 1154224
    Change-Id: I2c027961530947a93bca44ce70b8b9dcd25e1b2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2607931
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840993}

--- base/android/java/src/org/chromium/base/library_loader/LegacyLinker.java
+++ base/android/java/src/org/chromium/base/library_loader/LegacyLinker.java
@@ -38,7 +38,7 @@ class LegacyLinker extends Linker {
         ensureInitializedLocked();
         assert mState == State.INITIALIZED; // Only one successful call.
 
-        boolean provideRelro = mInBrowserProcess;
+        boolean produceRelro = mRelroProducer;
         long loadAddress = isFixedAddressPermitted ? mBaseLoadAddress : 0;
 
         String libFilePath = System.mapLibraryName(library);
@@ -51,7 +51,7 @@ class LegacyLinker extends Linker {
         }
         libInfo.mLibFilePath = libFilePath;
 
-        if (provideRelro) {
+        if (produceRelro) {
             if (!nativeCreateSharedRelro(sharedRelRoName, mBaseLoadAddress, libInfo)) {
                 Log.w(TAG, "Could not create shared RELRO for %s at %x", libFilePath,
                         mBaseLoadAddress);
@@ -68,6 +68,7 @@ class LegacyLinker extends Linker {
             useSharedRelrosLocked(mLibInfo);
             mState = State.DONE_PROVIDE_RELRO;
         } else {
+            // Consume RELRO.
             waitForSharedRelrosLocked();
             assert libFilePath.equals(mLibInfo.mLibFilePath);
             useSharedRelrosLocked(mLibInfo);
--- content/public/android/java/src/org/chromium/content/app/ContentChildProcessServiceDelegate.java
+++ content/public/android/java/src/org/chromium/content/app/ContentChildProcessServiceDelegate.java
@@ -21,7 +21,6 @@ import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.MainDex;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.base.library_loader.LibraryLoader;
-import org.chromium.base.library_loader.Linker;
 import org.chromium.base.memory.MemoryPressureUma;
 import org.chromium.base.process_launcher.ChildProcessServiceDelegate;
 import org.chromium.base.task.PostTask;
@@ -35,16 +34,13 @@ import org.chromium.content_public.common.ContentProcessInfo;
 import java.util.List;
 
 /**
- * This implementation of {@link ChildProcessServiceDelegate} loads the native library potentially
- * using the custom linker, provides access to view surfaces.
+ * This implementation of {@link ChildProcessServiceDelegate} loads the native library, provides
+ * access to view surfaces.
  */
 @JNINamespace("content")
 @MainDex
 public class ContentChildProcessServiceDelegate implements ChildProcessServiceDelegate {
     private static final String TAG = "ContentCPSDelegate";
-
-    // Linker-specific parameters for this child process service.
-    private ChromiumLinkerParams mLinkerParams;
 
     private IGpuProcessCallback mGpuCallback;
 
@@ -64,7 +60,7 @@ public class ContentChildProcessServiceDelegate implements ChildProcessServiceDe
 
     @Override
     public void onServiceBound(Intent intent) {
-        mLinkerParams = ChromiumLinkerParams.create(intent.getExtras());
+        LibraryLoader.getInstance().getMediator().takeLoadAddressFromBundle(intent.getExtras());
         LibraryLoader.getInstance().setLibraryProcessType(
                 ChildProcessCreationParamsImpl.getLibraryProcessType(intent.getExtras()));
     }
@@ -79,11 +75,7 @@ public class ContentChildProcessServiceDelegate implements ChildProcessServiceDe
         mCpuFeatures = connectionBundle.getLong(ContentChildProcessConstants.EXTRA_CPU_FEATURES);
         assert mCpuCount > 0;
 
-        if (LibraryLoader.getInstance().useChromiumLinker()
-                && !LibraryLoader.getInstance().isLoadedByZygote()) {
-            Bundle sharedRelros = connectionBundle.getBundle(Linker.EXTRA_LINKER_SHARED_RELROS);
-            if (sharedRelros != null) getLinker().provideSharedRelros(sharedRelros);
-        }
+        LibraryLoader.getInstance().getMediator().takeSharedRelrosFromBundle(connectionBundle);
     }
 
     @Override
@@ -103,12 +95,10 @@ public class ContentChildProcessServiceDelegate implements ChildProcessServiceDe
 
         JNIUtils.enableSelectiveJniRegistration();
 
-        if (LibraryLoader.getInstance().useChromiumLinker()) {
-            assert mLinkerParams != null;
-            getLinker().initServiceProcess(mLinkerParams.mBaseLoadAddress);
-        }
-        LibraryLoader.getInstance().loadNowOverrideApplicationContext(hostContext);
-        LibraryLoader.getInstance().registerRendererProcessHistogram();
+        LibraryLoader libraryLoader = LibraryLoader.getInstance();
+        libraryLoader.getMediator().initInChildProcess();
+        libraryLoader.loadNowOverrideApplicationContext(hostContext);
+        libraryLoader.registerRendererProcessHistogram();
         initializeLibrary();
     }
 
@@ -139,11 +129,6 @@ public class ContentChildProcessServiceDelegate implements ChildProcessServiceDe
     @Override
     public void runMain() {
         ContentMain.start(false);
-    }
-
-    // Return a Linker instance. If testing, the Linker needs special setup.
-    private Linker getLinker() {
-        return Linker.getInstance();
     }
 
     @CalledByNative
