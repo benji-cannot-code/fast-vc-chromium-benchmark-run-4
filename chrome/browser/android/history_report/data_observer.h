f6e5e272b75e0e89d17a4f8d6d42da43b8d64f90
Scott Violet <sky@chromium.org>
2021-01-07 15:21:40 +0000

    android: update DataObserver to remove observer
    DataObserver adds itself as an observer to HistoryService, but never
    removes itself. This also converts to using ScopedObservation.
    
    BUG=1158388
    TEST=none
    
    Change-Id: Ia438428ce0ecd7b8207be5bdc38bd448817300a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2612125
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841061}

--- chrome/browser/android/history_report/data_observer.h
+++ chrome/browser/android/history_report/data_observer.h
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/scoped_observation.h"
 #include "base/time/time.h"
+#include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/browser/bookmark_model_observer.h"
+#include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/history_service_observer.h"
 #include "components/history/core/browser/history_types.h"
 #include "components/history/core/browser/url_row.h"
@@ -77,11 +80,18 @@ class DataObserver : public bookmarks::BookmarkModelObserver,
                       const history::URLRows& changed_urls) override;
   void OnURLsDeleted(history::HistoryService* history_service,
                      const history::DeletionInfo& deletion_info) override;
+  void HistoryServiceBeingDeleted(
+      history::HistoryService* history_service) override;
 
  private:
   void DeleteBookmarks(const std::set<GURL>& removed_urls);
 
-  bookmarks::BookmarkModel* bookmark_model_;
+  base::ScopedObservation<bookmarks::BookmarkModel,
+                          bookmarks::BookmarkModelObserver>
+      scoped_bookmark_model_observer_{this};
+  base::ScopedObservation<history::HistoryService,
+                          history::HistoryServiceObserver>
+      scoped_history_service_observer_{this};
   base::RepeatingCallback<void(void)> data_changed_callback_;
   base::RepeatingCallback<void(void)> data_cleared_callback_;
   base::RepeatingCallback<void(void)> stop_reporting_callback_;
