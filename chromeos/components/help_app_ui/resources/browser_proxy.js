9570a9c6d9e4819100833d7194f1804617e96ac5
Callistus <callistus@google.com>
2021-01-07 23:45:50 +0000

    Fix addOrUpdateSearchIndex to await the LSS async method.
    This is required so that we can record the time taken to initialize the
    search index within the app.
    
    Bug: b/170162419
    Change-Id: Ie629239bcdd8490d60f1a788b93224af4e7ab112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2612312
    Reviewed-by: dstockwell <dstockwell@google.com>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Cr-Commit-Position: refs/heads/master@{#841253}

--- chromeos/components/help_app_ui/resources/browser_proxy.js
+++ chromeos/components/help_app_ui/resources/browser_proxy.js
@@ -103,7 +103,7 @@ guestMessagePipe.registerHandler(
           locale: searchable_item.locale,
         };
       });
-      indexRemote.addOrUpdate(data_to_send);
+      return indexRemote.addOrUpdate(data_to_send);
     });
 
 guestMessagePipe.registerHandler(Message.CLEAR_SEARCH_INDEX, async () => {
