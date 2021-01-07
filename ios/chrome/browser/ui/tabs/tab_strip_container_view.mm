9009786683374af0b675102bc0ef2ddee38f2b1f
Robbie Gibson <rkgibson@google.com>
2021-01-07 17:52:25 +0000

    [iOS][Thumb Strip] Always take tab strip snapshot with clear background
    When smooth scrolling is enabled, the tab strip background is set to
    black to block out the web content behind the tab strip. However, the
    tab strip snapshot should always have a clear background because the
    web content is moved when using the snapshot.
    
    Ths CL overrides the background color of the tab strip to clear when
    taking the snapshot and resets it afterwards.
    
    Fixed: 1163919
    Change-Id: I945183e10633442ccf4f49c05ef6ceae85f3af59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2614786
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#841099}

--- ios/chrome/browser/ui/tabs/tab_strip_container_view.mm
+++ ios/chrome/browser/ui/tabs/tab_strip_container_view.mm
@@ -15,8 +15,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation TabStripContainerView
 
 - (UIView*)screenshotForAnimation {
+  // The tab strip snapshot should have a clear background color. When using
+  // smooth scrolling, the background color is black, because the web content
+  // extends behind the tab strip. Switch out the background color for the
+  // snapshot and restore it afterwards.
+  UIColor* backgroundColor = self.tabStripView.backgroundColor;
+  self.tabStripView.backgroundColor = UIColor.clearColor;
   UIView* tabStripSnapshot =
       [self.tabStripView snapshotViewAfterScreenUpdates:YES];
+  self.tabStripView.backgroundColor = backgroundColor;
   tabStripSnapshot.transform =
       [self adjustTransformForRTL:tabStripSnapshot.transform];
   return tabStripSnapshot;
