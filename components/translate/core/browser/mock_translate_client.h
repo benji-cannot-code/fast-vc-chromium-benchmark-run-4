67b74d3bd0faf7d501938a03340c9b33569e43af
Basia Zimirska <basiaz@google.com>
2021-01-07 23:30:56 +0000

    Unit tests for TranslateInfoBarDelegate
    Bug: 1099438
    Change-Id: I30fe2f19eb721b7de1eba4ce17d8c2d0c7e52bf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2605416
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841243}

--- components/translate/core/browser/mock_translate_client.h
+++ components/translate/core/browser/mock_translate_client.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/infobars/core/infobar.h"
 #include "components/translate/core/browser/translate_client.h"
 #include "components/translate/core/browser/translate_driver.h"
+#include "components/translate/core/browser/translate_infobar_delegate.h"
 #include "components/translate/core/browser/translate_prefs.h"
 #include "components/translate/core/common/language_detection_details.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -41,11 +42,9 @@ class MockTranslateClient : public TranslateClient {
   MOCK_CONST_METHOD0(GetInfobarIconID, int());
 
 #if !defined(USE_AURA)
-  MOCK_CONST_METHOD1(CreateInfoBarMock,
-                     infobars::InfoBar*(TranslateInfoBarDelegate*));
   std::unique_ptr<infobars::InfoBar> CreateInfoBar(
       std::unique_ptr<TranslateInfoBarDelegate> delegate) const {
-    return base::WrapUnique(CreateInfoBarMock(delegate.get()));
+    return std::make_unique<infobars::InfoBar>(std::move(delegate));
   }
 #endif
 
