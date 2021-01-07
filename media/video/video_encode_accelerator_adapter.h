c22600095926f35029a94e311f07e1578439fbc3
Dale Curtis <dalecurtis@chromium.org>
2021-01-07 02:02:06 +0000

    Ensure flush support is properly known before using it.
    VEA flush support isn't known until Initialize() completes, which
    happens by way of RequireBitstreamBuffers(). The old code uses the
    value of |flush_support_| during InitCompleted() despite it not
    being set until after InitCompleted() runs.
    
    The fix is to ensure we set |flush_support_| correctly before we
    use it and to switch to a base::Optional for its value to prevent
    accidental incorrect usage in the future.
    
    R=sandersd
    
    Fixed: 1163673
    Test: New unittest.
    Change-Id: I69cec2f954ffc68773984ab59227b2cd10079820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613670
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840877}

--- media/video/video_encode_accelerator_adapter.h
+++ media/video/video_encode_accelerator_adapter.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/circular_deque.h"
 #include "base/containers/queue.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/optional.h"
 #include "base/synchronization/lock.h"
 #include "media/base/media_export.h"
 #include "media/base/video_encoder.h"
@@ -136,7 +137,7 @@ class MEDIA_EXPORT VideoEncodeAcceleratorAdapter
   scoped_refptr<base::SequencedTaskRunner> callback_task_runner_;
 
   State state_ = State::kNotInitialized;
-  bool flush_support_ = false;
+  base::Optional<bool> flush_support_;
 
   struct PendingEncode {
     PendingEncode();
