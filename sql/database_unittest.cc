472f01af2a9d3fc958c75a795bd20f75201f9607
Victor Costan <pwnall@chromium.org>
2021-01-07 22:18:58 +0000

    sql: Run SQLDatabaseTestExclusiveMode tests.
    Currently, the SQLDatabaseTestExclusiveMode suite is not instantiated,
    so the tests are not run. This CL instantiates the suite.
    
    This problem was discovered by attempting to roll googletest past CL
    315255779, which causes test binaries to fail when they include test
    suites / parametrized tests that are not instantiated, and when they
    include empty test suites.
    
    Bug: 1163396
    Change-Id: Ieb47f8d612c48af04f3d9d05e1873fc437c2f6c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2615579
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841220}

--- sql/database_unittest.cc
+++ sql/database_unittest.cc
@@ -854,13 +854,6 @@ TEST_P(SQLDatabaseTest, Delete) {
   EXPECT_FALSE(GetPathExists(wal_path));
 }
 
-// WAL mode is currently not supported on Fuchsia
-#if !defined(OS_FUCHSIA)
-INSTANTIATE_TEST_SUITE_P(JournalMode, SQLDatabaseTest, testing::Bool());
-#else
-INSTANTIATE_TEST_SUITE_P(JournalMode, SQLDatabaseTest, testing::Values(false));
-#endif
-
 #if defined(OS_POSIX)  // This test operates on POSIX file permissions.
 TEST_P(SQLDatabaseTest, PosixFilePermissions) {
   db().Close();
@@ -1375,5 +1368,18 @@ TEST_P(SQLDatabaseTest, CompileError) {
   }
 #endif  // !defined(OS_ANDROID) && !defined(OS_IOS) && !defined(OS_FUCHSIA)
 }
+
+// WAL mode is currently not supported on Fuchsia.
+#if !defined(OS_FUCHSIA)
+INSTANTIATE_TEST_SUITE_P(JournalMode, SQLDatabaseTest, testing::Bool());
+INSTANTIATE_TEST_SUITE_P(JournalMode,
+                         SQLDatabaseTestExclusiveMode,
+                         testing::Bool());
+#else
+INSTANTIATE_TEST_SUITE_P(JournalMode, SQLDatabaseTest, testing::Values(false));
+INSTANTIATE_TEST_SUITE_P(JournalMode,
+                         SQLDatabaseTestExclusiveMode,
+                         testing::Values(false));
+#endif
 
 }  // namespace sql
