0276016b407907d3634cb9e8d9743a838e981f27
Erik Chen <erikchen@chromium.org>
2021-01-07 03:01:27 +0000

    Remove shell identity extension API.
    The API is unused.
    
    Change-Id: I32b21b8752234505daa52fe920e4322c8485ace1
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2601047
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#840902}

--- extensions/shell/browser/shell_extensions_browser_client.cc
+++ extensions/shell/browser/shell_extensions_browser_client.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/shell/browser/shell_extension_system_factory.h"
 #include "extensions/shell/browser/shell_extension_web_contents_observer.h"
 #include "extensions/shell/browser/shell_extensions_api_client.h"
-#include "extensions/shell/browser/shell_extensions_browser_api_provider.h"
 #include "extensions/shell/browser/shell_navigation_ui_data.h"
 #include "services/network/public/mojom/url_loader.mojom.h"
 
@@ -52,7 +51,6 @@ ShellExtensionsBrowserClient::ShellExtensionsBrowserClient()
   SetCurrentChannel(version_info::Channel::UNKNOWN);
 
   AddAPIProvider(std::make_unique<CoreExtensionsBrowserAPIProvider>());
-  AddAPIProvider(std::make_unique<ShellExtensionsBrowserAPIProvider>());
 }
 
 ShellExtensionsBrowserClient::~ShellExtensionsBrowserClient() {
