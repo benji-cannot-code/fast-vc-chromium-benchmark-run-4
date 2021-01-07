71106342e0a42a96ae27261362249a7a7671a1b5
Joshua Pawlicki <waffles@chromium.org>
2021-01-07 18:16:42 +0000

    Remove dependencies on third_party/adobe/flash
    As far as I can tell, the actual header is no longer used anywhere.
    This completes decoupling the flapper targets from the build tree,
    so they are removed.
    
    Bug: 1064651
    Change-Id: I18996e13c3dbf9df097b3b964e2b0d2113cfef1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613324
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Anthony LaForge <laforge@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841111}

--- chrome/common/chrome_content_client.cc
+++ chrome/common/chrome_content_client.cc
@@ -81,7 +81,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_PLUGINS)
 #include "content/public/common/pepper_plugin_info.h"
-#include "flapper_version.h"  // nogncheck  In SHARED_INTERMEDIATE_DIR.
 #include "ppapi/shared_impl/ppapi_permissions.h"  // nogncheck
 #endif
 
