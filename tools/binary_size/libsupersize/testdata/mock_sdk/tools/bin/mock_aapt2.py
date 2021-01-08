35547eddcc7fcdcbc43af8df2868cdf2cf7002f3
Clark DuVall <cduvall@chromium.org>
2021-01-08 20:55:13 +0000

    Add support for split APKs to supersize
    This allows supersize to analyze split APKs. These show up as child
    containers to the base APK since they are named with
    "<base name>/<split name>".
    
    Example report: https://chrome-supersize.firebaseapp.com/viewer.html?load_url=https%3A%2F%2Fstorage.googleapis.com%2Fchrome-supersize%2Foneoffs%2Fcduvall7.size&group_by=container&type=mx
    Example diff: https://chrome-supersize.firebaseapp.com/viewer.html?load_url=https%3A%2F%2Fstorage.googleapis.com%2Fchrome-supersize%2Foneoffs%2Fcduvall4.sizediff&group_by=container&type=mx&diff_mode=on
    
    Bug: 1143690
    Change-Id: I6ed7d516607e56431cf4c3fb67989d6783520a78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2614123
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841627}

--- tools/binary_size/libsupersize/testdata/mock_sdk/tools/bin/mock_aapt2.py
+++ tools/binary_size/libsupersize/testdata/mock_sdk/tools/bin/mock_aapt2.py
@@ -1,28 +1,23 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright 2018 The Chromium Authors. All rights reserved.
+# Copyright 2020 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 
 import os
 import sys
 import zipfile
 
 
-_SCRIPT_DIR = os.path.dirname(__file__)
-_OUTPUT_FILE = os.path.join(_SCRIPT_DIR, 'apkanalyzer.output')
-
-
 def main():
-  # Without a proguard mapping file, the last argument is the apk_path.
   apk_path = sys.argv[-1]
   assert os.path.exists(apk_path), 'Apk does not exist: {}'.format(apk_path)
   with zipfile.ZipFile(apk_path) as z:
-    if any(n.endswith('.dex') for n in z.namelist()):
-      with open(_OUTPUT_FILE, 'r') as f:
+    try:
+      # The AndroidManifest.xml file will have the aapt2 output, and not XML.
+      with z.open('AndroidManifest.xml') as f:
         sys.stdout.write(f.read())
-    else:
-      sys.stdout.write('P r 0   0       0       <TOTAL>')
+    except KeyError:
+      pass
 
 
 if __name__ == '__main__':
