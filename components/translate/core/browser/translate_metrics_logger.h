32399b70eb40c9ca250b7dff97c0541aed7f607a
Max Curran <curranmax@chromium.org>
2021-01-08 01:03:39 +0000

    Added logging of the new page-load frequency Translate metrics to UKM.
    This CL adds logging of proposed Translate proto to UKM. This proto
    captures a summary of the user's interaction with Translate from the
    beginning of the page load to when the proto is logged. In the event
    that the user backgrounds Chrome and later returns, there may be more
    than one proto logged per page load. In this case the sequence number of
    each proto can be used distinguish the order that the protos were
    logged.
    
    Full design doc: https://docs.google.com/document/d/1dyWh1Xw5VgUA00VA-5PTgKQ6ItziPBnSyeDR8saJ9vM/edit?usp=sharing
    UKM collection review: https://docs.google.com/document/d/1UNVJ2HC35Cy2C6zCb6DdwKFhafURGeV9gDzY5Jm5id8/edit?usp=sharing
    
    Bug: 1114868
    Change-Id: If970d2b656c7355e6a05fa06729978265d9ecb64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2590754
    Commit-Queue: Max Curran <curranmax@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841292}

--- components/translate/core/browser/translate_metrics_logger.h
+++ components/translate/core/browser/translate_metrics_logger.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "components/translate/core/common/translate_errors.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 
 namespace translate {
 
@@ -58,6 +59,8 @@ enum class TriggerDecision {
   kMaxValue = kAutomaticTranslationByPref,
 };
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
 enum class UIInteraction {
   kUninitialized = 0,
   kTranslate = 1,
@@ -89,6 +92,9 @@ class TranslateMetricsLogger {
   // Logs all stored page load metrics. If is_final is |true| then RecordMetrics
   // won't be called again.
   virtual void RecordMetrics(bool is_final) = 0;
+
+  // Sets the UKM source ID for the current page load.
+  virtual void SetUkmSourceId(ukm::SourceId ukm_source_id) = 0;
 
   virtual void LogRankerMetrics(RankerDecision ranker_decision,
                                 uint32_t ranker_version) = 0;
