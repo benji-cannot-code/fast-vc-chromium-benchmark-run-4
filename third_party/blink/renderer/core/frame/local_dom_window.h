6f76112e3695309dd1a18344d165fd318c5565c5
John Delaney <johnidel@chromium.org>
2021-01-08 22:34:55 +0000

    Add window.open interface for impression registration
    What: Add a new param to window.open to register an impression for the
    conversion measurement API.
    
    See https://github.com/WICG/conversion-measurement-api/blob/master/README.md#registering-impressions-for-windowopen-navigations
    for details.
    
    Why:
    Lots of ads perform navigations via JS, making the anchor tag
    conversion API difficult to integrate into existing techstacks.
    
    Note that origin trial features cannot control IDL function overloads
    per https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/renderer/bindings/scripts/v8_interface.py;l=1099?q=v8_interface.py&ss=chromium%2Fchromium%2Fsrc
    
    
    Bug: 1154096
    Change-Id: Ib84bbd224262d35ffde357b01448670f0ae37933
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2566343
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841683}

--- third_party/blink/renderer/core/frame/local_dom_window.h
+++ third_party/blink/renderer/core/frame/local_dom_window.h
@@ -65,6 +65,7 @@ class External;
 class FrameConsole;
 class History;
 class IdleRequestOptions;
+class ImpressionParams;
 class MediaQueryList;
 class MessageEvent;
 class Modulator;
@@ -346,6 +347,13 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
                   const String& url_string,
                   const AtomicString& target,
                   const String& features,
+                  ExceptionState&);
+
+  DOMWindow* open(v8::Isolate*,
+                  const String& url_string,
+                  const AtomicString& target,
+                  const String& features,
+                  const ImpressionParams* impression_params,
                   ExceptionState&);
 
   FrameConsole* GetFrameConsole() const;
--- third_party/blink/renderer/core/page/context_menu_controller.cc
+++ third_party/blink/renderer/core/page/context_menu_controller.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/frame/web_frame_widget_impl.h"
 #include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
+#include "third_party/blink/renderer/core/html/conversion_measurement_parsing.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_input_element.h"
 #include "third_party/blink/renderer/core/html/html_anchor_element.h"
@@ -491,7 +492,7 @@ bool ContextMenuController::ShowContextMenu(LocalFrame* frame,
     data.link_text = anchor->innerText();
 
     if (anchor->HasImpression())
-      data.impression = anchor->GetImpressionForNavigation();
+      data.impression = GetImpressionForAnchor(anchor);
   }
 
   data.input_field_type = ComputeInputFieldType(result);
