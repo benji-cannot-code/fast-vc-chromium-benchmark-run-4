abd829e0757ea722ef99f9196980d9d3151e3871
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-01-08 17:20:29 +0000

    Import wpt@d6c156f2a063659b13b031e8888838b34bf0d4a6
    Using wpt-import in Chromium 9f527ffbc0f5b6b52ecd2abe3713c77ba93d486f.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=foolip@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Ia15a03e178d534e86b487a4ec50a62e46e9f9aad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2615345
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#841544}

--- third_party/blink/web_tests/external/wpt/html/webappapis/atob/base64.any.js
+++ third_party/blink/web_tests/external/wpt/html/webappapis/atob/base64.any.js
@@ -1,13 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<!doctype html>
-<!-- Originally developed by Aryeh Gregor, funded by Google.  Copyright belongs
-to Google. -->
-<title>atob()/btoa() tests</title>
-<meta charset=utf-8>
-<div id=log></div>
-<script src=/resources/testharness.js></script>
-<script src=/resources/testharnessreport.js></script>
-<script>
 /**
  * btoa() as defined by the HTML5 spec, which mostly just references RFC4648.
  */
@@ -161,9 +152,9 @@ function runAtobTests(tests) {
           output = allTests[i][1];
     test(() => {
       if(output === null) {
-        assert_throws_dom("InvalidCharacterError", () => window.atob(input));
+        assert_throws_dom("InvalidCharacterError", () => globalThis.atob(input));
       } else {
-        const result = window.atob(input);
+        const result = globalThis.atob(input);
         for(let ii = 0; ii < output.length; ii++) {
           assert_equals(result.charCodeAt(ii), output[ii]);
         }
@@ -171,4 +162,3 @@ function runAtobTests(tests) {
     }, "atob(" + format_value(input) + ")");
   }
 }
-</script>
