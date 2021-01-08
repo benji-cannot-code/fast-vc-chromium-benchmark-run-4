345e7f0d9dafd5df9dabf640927666f96fe11582
Xing Liu <xingliu@chromium.org>
2021-01-08 02:47:17 +0000

    Read later: Disable the reading list entry point for invalid URL.
    This matches the behavior when the URL is not supported by download.
    
    Bug: 1157808
    Change-Id: I5300adefc2d4f8e90c2345d0aacb38514ba55d11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2616481
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841328}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/read_later/ReadingListUtils.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.read_later;
+
+import android.text.TextUtils;
+
+import org.chromium.components.embedder_support.util.UrlConstants;
+
+/**
+ * Utility functions for reading list feature.
+ */
+public final class ReadingListUtils {
+    /**
+     * @return Whether the URL can be added as reading list article.
+     */
+    public static boolean isReadingListSupported(String url) {
+        if (TextUtils.isEmpty(url)) return false;
+
+        // This should match ReadingListModel::IsUrlSupported(), having a separate function since
+        // the UI may not load native library.
+        return url.startsWith(UrlConstants.HTTP_URL_PREFIX)
+                || url.startsWith(UrlConstants.HTTPS_URL_PREFIX);
+    }
+}
---
+++ chrome/android/junit/src/org/chromium/chrome/browser/read_later/ReadingListUtilsUnitTest.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.read_later;
+
+import android.support.test.filters.SmallTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import org.chromium.base.test.BaseRobolectricTestRunner;
+
+@RunWith(BaseRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+/**
+ * Unit test for {@link ReadingListUtils}.
+ */
+public class ReadingListUtilsUnitTest {
+    @Test
+    @SmallTest
+    public void testIsReadingListSupport() {
+        Assert.assertFalse(ReadingListUtils.isReadingListSupported(null));
+        Assert.assertFalse(ReadingListUtils.isReadingListSupported(""));
+        Assert.assertFalse(ReadingListUtils.isReadingListSupported("chrome://flags"));
+        Assert.assertTrue(ReadingListUtils.isReadingListSupported("http://www.example.com"));
+        Assert.assertTrue(ReadingListUtils.isReadingListSupported("https://www.example.com"));
+    }
+}
