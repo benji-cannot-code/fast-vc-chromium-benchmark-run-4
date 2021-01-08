f21da304ff40553e0a344f842d65f4589bc892cf
Toni Barzic <tbarzic@chromium.org>
2021-01-08 22:47:26 +0000

    Reset holding space model before calling detach observers
    Resets current holding space model before calling holding space
    detached, so HoldingSpaceController::Get()->model() can be used
    test whether a model is currently attached from the observer method.
    
    Also, updates HoldingSpaceTray to cancel previews update when previews
    icon gets hidden.
    
    BUG=1164145
    
    Change-Id: I1f0b50cc52c5b348b0e034e65751b9a0047bd096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2618093
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841691}

--- ash/system/holding_space/holding_space_tray.h
+++ ash/system/holding_space/holding_space_tray.h
@@ -73,6 +73,8 @@ class ASH_EXPORT HoldingSpaceTray : public TrayBackgroundView,
     use_zero_previews_update_delay_ = zero_delay;
   }
 
+  void FirePreviewsUpdateTimerIfRunningForTesting();
+
  private:
   void UpdateVisibility();
 
