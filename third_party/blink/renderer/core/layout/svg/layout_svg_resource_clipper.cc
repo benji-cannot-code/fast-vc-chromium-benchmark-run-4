52eae825a20aa40ed80c0caf4917c46bfac86c86
Fredrik Söderqvist <fs@opera.com>
2021-01-08 15:54:17 +0000

    Restore resource client notifications on layout attach/detach
    We still need to handle cases like when display transitions from 'none'
    to not-'none' since we might need to invalidate paint properties or
    similar in those cases. Unlike previously though we can just call
    MarkAllClientsForInvalidation().
    
    Remove LayoutSVGResourceClipper::WillBeDestroyed() since it should no
    longer be needed.
    
    Bug: 1162093
    Change-Id: Ic0073dd10f0043e11b6ed984b57ac122a8ec37fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2614430
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#841504}

--- third_party/blink/renderer/core/layout/svg/layout_svg_resource_clipper.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_resource_clipper.cc
@@ -306,11 +306,4 @@ void LayoutSVGResourceClipper::StyleDidChange(StyleDifference diff,
   }
 }
 
-void LayoutSVGResourceClipper::WillBeDestroyed() {
-  NOT_DESTROYED();
-  MarkAllClientsForInvalidation(SVGResourceClient::kClipCacheInvalidation |
-                                SVGResourceClient::kPaintInvalidation);
-  LayoutSVGResourceContainer::WillBeDestroyed();
-}
-
 }  // namespace blink
