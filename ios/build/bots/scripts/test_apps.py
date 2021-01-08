67ee5a36df8f7dd5cdbea5cfddf4f9bd375cc490
Zhaoyang Li <zhaoyangli@chromium.org>
2021-01-08 21:54:02 +0000

    [iOS][test runner] Use error log level for GMock.
    This change will prevent "Uninteresting mock function call" GMock log
    spams, which could attribute to 1/3 of log lines in some unit test
    targets and is causing slowness in swarming infra processing logs at
    collecting tasks.
    
    Bug: 1157537
    Change-Id: I1dd2780329ab8568a4582d6ef4e3a5a7d4a68016
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587605
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841664}

--- ios/build/bots/scripts/test_apps.py
+++ ios/build/bots/scripts/test_apps.py
@@ -473,6 +473,8 @@ class DeviceXCTestUnitTestsApp(GTestsApp):
       ]
       self.test_args.append('--gtest_filter=%s' % gtest_filter)
 
+    self.test_args.append('--gmock_verbose=error')
+
     xctestrun_data['TestTargetName'].update(
         {'CommandLineArguments': self.test_args})
 
@@ -587,6 +589,8 @@ class SimulatorXCTestUnitTestsApp(GTestsApp):
           el for el in self.test_args if not el.startswith('--gtest_filter=')
       ]
       self.test_args.append('--gtest_filter=%s' % gtest_filter)
+
+    self.test_args.append('--gmock_verbose=error')
 
     xctestrun_data['TestTargetName'].update(
         {'CommandLineArguments': self.test_args})
