d3e0e91554a2074c42f55ccf70a1d8ccdc393928
Tanmoy Mollik <triploblastic@chromium.org>
2021-01-08 11:42:59 +0000

    Convert components/sync/trusted_vault/* to OnPrimaryAccountChanged
    Method OnPrimaryAccountSet/Cleared and
    OnUnconsentedPrimaryAccountChanged is deprecated. So this cl changes
    these calls to OnPrimaryAccountChanged.
    
    Bug: 1158855
    Change-Id: I0d19f0dad84f128c554ace6942d4ec4e5e96e34b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611740
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841446}

--- components/sync/trusted_vault/trusted_vault_access_token_fetcher_frontend.cc
+++ components/sync/trusted_vault/trusted_vault_access_token_fetcher_frontend.cc
@@ -51,18 +51,8 @@ void TrustedVaultAccessTokenFetcherFrontend::FetchAccessToken(
   }
 }
 
-void TrustedVaultAccessTokenFetcherFrontend::OnPrimaryAccountSet(
-    const CoreAccountInfo& primary_account_info) {
-  UpdatePrimaryAccountIfNeeded();
-}
-
-void TrustedVaultAccessTokenFetcherFrontend::OnPrimaryAccountCleared(
-    const CoreAccountInfo& previous_primary_account_info) {
-  UpdatePrimaryAccountIfNeeded();
-}
-
-void TrustedVaultAccessTokenFetcherFrontend::OnUnconsentedPrimaryAccountChanged(
-    const CoreAccountInfo& unconsented_primary_account_info) {
+void TrustedVaultAccessTokenFetcherFrontend::OnPrimaryAccountChanged(
+    const signin::PrimaryAccountChangeEvent& event) {
   UpdatePrimaryAccountIfNeeded();
 }
 
