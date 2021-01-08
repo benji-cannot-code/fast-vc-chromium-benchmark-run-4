e28a5c74614d685a4d386f596170c2c7e381205b
Jeroen Dhollander <jeroendh@google.com>
2021-01-08 22:20:15 +0000

    Reland "Consolidate interactions with |CrasAudioHandler|."
    This is a reland of ccba8bc4de221a21c072f510d0464a0632d37de2
    
    The problem was that the fixes for the review comments had caused us
    to call AudioDevices::SetAudioDevices() before the constructor of
    |AudioDevices| had finished initializing all variables.
    Unfortunately this only happened on real hardware, and I had not
    retested these review comment fixes on hardware.
    
    Original change's description:
    > Consolidate interactions with |CrasAudioHandler|.
    >
    > Previously 2 places in the code were interacting with |CrasAudioHandler|:
    >    - |PlatformApiImpl| subscribes to device changes, and passes this
    >      information to |AudioInputHost|.
    >    - |AudioInputHost| posts the new hotword model to |CrasAudioHandler|.
    >
    > This made it hard/impossible to split |AudioInputHost| from
    > |PlatformApiImpl|, which is needed to handle audio input in the
    > Libassistant mojom service.
    >
    > To solve this I moved all interactions with |CrasAudioHandler| inside a
    > new class |AudioDevices|, of which an instance is owned by
    > |AudioInputHost|.
    >
    > This also allowed me to add a bunch of new unittests.
    >
    > deployed on real hardware.
    >
    > Bug: b/171748795
    > Test: chromeos_unittests --gtest_filter="Assistant*:Audio*" and manually
    > Change-Id: I8ec9cec6672236f6365345b292efbe828518be69
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2610953
    > Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Auto-Submit: Jeroen Dhollander <jeroendh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#841155}
    
    Bug: b/171748795
    Change-Id: I9b8b970fbd1011ce911bf99961159805a4c0d2a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617568
    Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    Auto-Submit: Jeroen Dhollander <jeroendh@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841677}

--- chromeos/services/assistant/platform_api_impl.h
+++ chromeos/services/assistant/platform_api_impl.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "chromeos/audio/cras_audio_handler.h"
 #include "chromeos/services/assistant/platform/audio_input_host.h"
 #include "chromeos/services/assistant/platform/audio_input_provider_impl.h"
 #include "chromeos/services/assistant/platform/audio_output_provider_impl.h"
@@ -25,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/device/public/mojom/battery_monitor.mojom.h"
 
 namespace chromeos {
+class CrasAudioHandler;
 class PowerManagerClient;
 
 namespace assistant {
@@ -32,8 +32,7 @@ namespace assistant {
 class AssistantMediaSession;
 
 // Platform API required by the voice assistant.
-class PlatformApiImpl : public CrosPlatformApi,
-                        CrasAudioHandler::AudioObserver {
+class PlatformApiImpl : public CrosPlatformApi {
  public:
   PlatformApiImpl(
       AssistantMediaSession* media_session,
@@ -42,7 +41,7 @@ class PlatformApiImpl : public CrosPlatformApi,
       mojo::PendingRemote<device::mojom::BatteryMonitor> battery_monitor,
       scoped_refptr<base::SequencedTaskRunner> main_thread_task_runner,
       scoped_refptr<base::SingleThreadTaskRunner> background_task_runner,
-      std::string pref_locale);
+      const std::string& pref_locale);
   ~PlatformApiImpl() override;
 
   // assistant_client::PlatformApi overrides
@@ -52,9 +51,6 @@ class PlatformApiImpl : public CrosPlatformApi,
   assistant_client::FileProvider& GetFileProvider() override;
   assistant_client::NetworkProvider& GetNetworkProvider() override;
   assistant_client::SystemProvider& GetSystemProvider() override;
-
-  // chromeos::CrasAudioHandler::AudioObserver overrides
-  void OnAudioNodesChanged() override;
 
   // Called when the mic state associated with the interaction is changed.
   void SetMicState(bool mic_open) override;
@@ -105,9 +101,6 @@ class PlatformApiImpl : public CrosPlatformApi,
   NetworkProviderImpl network_provider_;
   AudioInputHost audio_input_host_;
   std::unique_ptr<SystemProviderImpl> system_provider_;
-  std::string pref_locale_;
-
-  CrasAudioHandler* const cras_audio_handler_;
 
   DISALLOW_COPY_AND_ASSIGN(PlatformApiImpl);
 };
