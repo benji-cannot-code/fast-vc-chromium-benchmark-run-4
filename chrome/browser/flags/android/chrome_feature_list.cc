179e985ee2fa0df58e63a0f2db5d6b39237c5089
Kaiyu Chen <kylechenkyc@google.com>
2021-01-08 20:59:21 +0000

    Add GPayAppDynamicUpdate feature flag.
    The feature flag controls the GPay native app integration for dynamic update with web payments on Android.
    The integration for dynamic update is mainly about adding callback service to notify the browser about changes like shipping address.
    Design doc is go/dynamic-update-mweb.
    
    Bug: 1074423
    Change-Id: I608ca5ddd5aa28f2da5672da83eefd0128720992
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617201
    Commit-Queue: Kaiyu Chen <kylechenkyc@google.com>
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841632}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -183,6 +183,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kEphemeralTabUsingBottomSheet,
     &kExploreSites,
     &kFocusOmniboxInIncognitoTabIntents,
+    &kGPayAppDynamicUpdate,
     &kHandleMediaIntents,
     &kHomepagePromoCard,
     &kHorizontalTabSwitcherAndroid,
@@ -508,6 +509,9 @@ const base::Feature kEphemeralTabUsingBottomSheet{
 
 const base::Feature kExploreSites{"ExploreSites",
                                   base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kGPayAppDynamicUpdate{"GPayAppDynamicUpdate",
+                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kHandleMediaIntents{"HandleMediaIntents",
                                         base::FEATURE_ENABLED_BY_DEFAULT};
