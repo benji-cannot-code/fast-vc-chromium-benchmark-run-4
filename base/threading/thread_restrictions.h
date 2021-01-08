21a581382413932a8436449da47e5f0a44b7e47f
Alan Screen <awscreen@chromium.org>
2021-01-08 03:32:12 +0000

    Add FetchCapabilities to print backend service
    Incorporate support for retrieving a printer's basic info, user defined
    papers, and semantic capabilities and defaults from the utility service.
    This combined collection of information is needed when preparing for
    print preview, so it reduces overhead and latency to enable these to all
    be collected with one mojom call instead of needing a chain of calls to
    assemble the data.
    
    Bug: 809738
    Change-Id: I75518dca30f0113cadc7971ba2a87658aca7176b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2578698
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841347}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/macros.h"
 #include "base/threading/hang_watcher.h"
+#include "build/build_config.h"
 
 // -----------------------------------------------------------------------------
 // Usage documentation
@@ -229,6 +230,9 @@ class ScopedIPCSupport;
 }
 namespace printing {
 class LocalPrinterHandlerDefault;
+#if defined(OS_MAC)
+class PrintBackendServiceImpl;
+#endif
 class PrintJobWorker;
 class PrinterQuery;
 }
@@ -400,6 +404,9 @@ class BASE_EXPORT ScopedAllowBlocking {
   friend class module_installer::ScopedAllowModulePakLoad;
   friend class mojo::CoreLibraryInitializer;
   friend class printing::LocalPrinterHandlerDefault;
+#if defined(OS_MAC)
+  friend class printing::PrintBackendServiceImpl;
+#endif
   friend class printing::PrintJobWorker;
   friend class resource_coordinator::TabManagerDelegate;  // crbug.com/778703
   friend class web::WebSubThread;
--- chrome/services/printing/print_backend_service_impl.h
+++ chrome/services/printing/print_backend_service_impl.h
@@ -33,6 +33,10 @@ class PrintBackendServiceImpl : public mojom::PrintBackendService {
       base::OnceCallback<void(const base::Optional<std::string>& printer_name)>;
   using GetPrinterSemanticCapsAndDefaultsCallback = base::OnceCallback<void(
       base::Optional<PrinterSemanticCapsAndDefaults> printer_caps)>;
+  using FetchCapabilitiesCallback = base::OnceCallback<void(
+      base::Optional<PrinterBasicInfo>,
+      base::Optional<PrinterSemanticCapsAndDefaults::Papers>,
+      base::Optional<PrinterSemanticCapsAndDefaults>)>;
 
   // mojom::PrintBackendService implementation:
   void Init(const std::string& locale) override;
@@ -43,6 +47,9 @@ class PrintBackendServiceImpl : public mojom::PrintBackendService {
       const std::string& printer_name,
       mojom::PrintBackendService::GetPrinterSemanticCapsAndDefaultsCallback
           callback) override;
+  void FetchCapabilities(
+      const std::string& printer_name,
+      mojom::PrintBackendService::FetchCapabilitiesCallback callback) override;
 
   scoped_refptr<PrintBackend> print_backend_;
 
