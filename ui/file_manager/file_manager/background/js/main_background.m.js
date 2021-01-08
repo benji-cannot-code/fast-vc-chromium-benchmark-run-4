1c7d91740f3595c9c8d13b70f1d7088a285edf22
Luciano Pacheco <lucmult@chromium.org>
2021-01-08 04:31:39 +0000

    Files app: Add JS module main_background.m.js
    Bug: 1133186
    Change-Id: I0e15ab92a8925677b8d1e1462ddfda585ae44824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2612877
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841362}

---
+++ ui/file_manager/file_manager/background/js/main_background.m.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Main entry point for background page, it replaces the
+ * background_scripts.js.
+ */
+
+import './metrics_start.m.js';
+import '../../../base/js/error_counter.m.js';
+import './background.m.js';
+import './test_util.m.js';
