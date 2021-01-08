5c0d1b512154242c431f1341da847940c2862f7c
Xianzhu Wang <wangxianzhu@chromium.org>
2021-01-08 23:02:42 +0000

    Cleanup for LayoutView overflow clips
    1. Move the code checking requirement of overflow clip for printing
       from PaintPropertyTreeBuilder into LocalFrame::ClipsContent().
    
    2. Combine LocalFrameView::ClipPaintRect() into
       LayoutView::ViewRect(), so that all code paths calling ViewRect()
       (e.g. LayoutView::OverflowClipRect()) can be dev tool viewport
       overriding aware. This means that the overriding viewport is
       reflected in the paint property tree, and
       CompositedLayerMapping::RecomputeInterestRect() no longer needs to
       apply the viewport clip separately.
    
    Change-Id: Ib743a891e4d4fc0749ff799a04ed0f09757e9f5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613388
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841701}

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -1994,6 +1994,9 @@ bool LocalFrame::ClipsContent() const {
   if (GetDocument()->IsPaintingPreview())
     return false;
 
+  if (ShouldUsePrintingLayout())
+    return false;
+
   if (IsMainFrame())
     return GetSettings()->GetMainFrameClipsContent();
   // By default clip to viewport.
