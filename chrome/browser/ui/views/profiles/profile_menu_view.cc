d90e26d4556442f9632118091450b693940a9ffb
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2021-01-08 12:23:59 +0000

    Add metadata to ProfileMenuView classes.
    Bug: None
    Change-Id: I0a0764d6edb19dfb548f05209013b69feaa9b64b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2601360
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Jacobo Aragunde Pérez <jaragunde@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#841451}

--- chrome/browser/ui/views/profiles/profile_menu_view.cc
+++ chrome/browser/ui/views/profiles/profile_menu_view.cc
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/native_theme/native_theme.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/accessibility/view_accessibility.h"
+#include "ui/views/metadata/metadata_impl_macros.h"
 
 namespace {
 
@@ -745,3 +746,7 @@ void ProfileMenuView::BuildProfileManagementFeatureButtons() {
   }
 }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+BEGIN_METADATA(ProfileMenuView, ProfileMenuViewBase)
+ADD_READONLY_PROPERTY_METADATA(gfx::ImageSkia, SyncIcon)
+END_METADATA
