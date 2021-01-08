5a4efb433be659531caa12bd7ca718356896d589
Aya ElAttar <ayaelattar@chromium.org>
2021-01-08 22:07:32 +0000

    DLP: Change DragDropDelegate::OnDragUpdated return type
    - Changed the return type of DragDropDelegate::OnDragUpdated
    to return a struct of DragOperation and DataTransferEndpoint.
    - The returned DataTransferEndpoint should be used in
    DragDropController for allowing/disallowing dropping the
    data according to the policy rules, See crrev.com/c/2595382.
    
    Bug: 1139886
    Change-Id: Ic80f2d55bf1df350d24778f01a43c8cba0ff67cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595411
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841675}

--- ash/drag_drop/drag_drop_controller_unittest.cc
+++ ash/drag_drop/drag_drop_controller_unittest.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/clipboard/clipboard.h"
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
+#include "ui/base/data_transfer_policy/data_transfer_endpoint.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/dragdrop/mojom/drag_drop_types.mojom-shared.h"
 #include "ui/base/dragdrop/os_exchange_data.h"
@@ -257,12 +258,15 @@ class EventTargetTestDelegate : public aura::client::DragDropDelegate {
     EXPECT_EQ(window_, event.target());
     state_ = State::kDragEnteredInvoked;
   }
-  int OnDragUpdated(const ui::DropTargetEvent& event) override {
+  aura::client::DragUpdateInfo OnDragUpdated(
+      const ui::DropTargetEvent& event) override {
     EXPECT_TRUE(State::kDragEnteredInvoked == state_ ||
                 State::kDragUpdateInvoked == state_);
     EXPECT_EQ(window_, event.target());
     state_ = State::kDragUpdateInvoked;
-    return ui::DragDropTypes::DRAG_MOVE;
+    return aura::client::DragUpdateInfo(
+        ui::DragDropTypes::DRAG_MOVE,
+        ui::DataTransferEndpoint(ui::EndpointType::kDefault));
   }
   void OnDragExited() override { ADD_FAILURE(); }
   int OnPerformDrop(const ui::DropTargetEvent& event,
--- ui/aura/client/drag_drop_delegate.h
+++ ui/aura/client/drag_drop_delegate.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/aura/aura_export.h"
 #include "ui/aura/window.h"
+#include "ui/base/data_transfer_policy/data_transfer_endpoint.h"
+#include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/dragdrop/os_exchange_data.h"
 
 namespace ui {
@@ -20,6 +22,18 @@ namespace aura {
 class Window;
 namespace client {
 
+struct AURA_EXPORT DragUpdateInfo {
+  DragUpdateInfo();
+  DragUpdateInfo(int op, ui::DataTransferEndpoint endpoint);
+
+  DragUpdateInfo& operator=(const DragUpdateInfo& update_info);
+
+  // A bitmask of the DragDropTypes::DragOperation supported.
+  int drag_operation = ui::DragDropTypes::DRAG_NONE;
+  // An object representing the destination window.
+  ui::DataTransferEndpoint data_endpoint{ui::EndpointType::kDefault};
+};
+
 // Delegate interface for drag and drop actions on aura::Window.
 class AURA_EXPORT DragDropDelegate {
  public:
@@ -29,10 +43,9 @@ class AURA_EXPORT DragDropDelegate {
   virtual void OnDragEntered(const ui::DropTargetEvent& event) = 0;
 
   // Invoked during a drag and drop session while the mouse is over the window.
-  // This should return a bitmask of the DragDropTypes::DragOperation supported
-  // based on the location of the event. Return 0 to indicate the drop should
-  // not be accepted.
-  virtual int OnDragUpdated(const ui::DropTargetEvent& event) = 0;
+  // This should return DragUpdateInfo object based on the location of the
+  // event.
+  virtual DragUpdateInfo OnDragUpdated(const ui::DropTargetEvent& event) = 0;
 
   // Invoked during a drag and drop session when the mouse exits the window, or
   // when the drag session was canceled and the mouse was over the window.
