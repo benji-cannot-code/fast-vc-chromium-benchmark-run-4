a12bf581b02c953b6b72a481fe88f5d8a55c5537
cfredric <cfredric@chromium.org>
2021-01-09 00:18:01 +0000

    Add UseCounters for SameParty cookie usage.
    This cl adds 3 use counters:
    * one for use of the SameParty attribute;
    * one for when a cookie was excluded solely for SameParty reasons (i.e.
      in a cross-party context);
    * one for when a cookie was included by SameParty but excluded by
      SameSite.
    
    While I'm at it, this cl also:
    * renames the misleadingly-named `excluded_cookie` variable, since the
      cookie could be either included or excluded;
    * renames the EmitSameSiteCookiesDeprecationWarning, since the method
      does more than that;
    * and introduces a helper variable to make the loop body slightly more
      readable.
    
    Change-Id: I388e032651212769c2547ae64b426903ff7be0b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2616422
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841727}

--- net/cookies/canonical_cookie.cc
+++ net/cookies/canonical_cookie.cc
@@ -777,6 +777,7 @@ CookieAccessResult CanonicalCookie::IncludeForRequestURL(
           CookieInclusionStatus::EXCLUDE_SAMEPARTY_CROSS_PARTY_CONTEXT);
       FALLTHROUGH;
     case CookieSamePartyStatus::kEnforceSamePartyInclude: {
+      status.AddWarningReason(CookieInclusionStatus::WARN_TREATED_AS_SAMEPARTY);
       // Remove any SameSite exclusion reasons, since SameParty overrides
       // SameSite.
       DCHECK(!status.HasExclusionReason(
@@ -794,10 +795,23 @@ CookieAccessResult CanonicalCookie::IncludeForRequestURL(
             CookieInclusionStatus::EXCLUDE_SAMESITE_UNSPECIFIED_TREATED_AS_LAX,
         });
       }
+
+      // Update metrics.
+      if (status.HasOnlyExclusionReason(
+              CookieInclusionStatus::EXCLUDE_SAMEPARTY_CROSS_PARTY_CONTEXT) &&
+          included_by_samesite) {
+        status.AddWarningReason(
+            CookieInclusionStatus::WARN_SAMEPARTY_EXCLUSION_OVERRULED_SAMESITE);
+      }
       if (status.IsInclude()) {
         UMA_HISTOGRAM_BOOLEAN(
             "Cookie.SamePartyReadIncluded.InclusionUnderSameSite",
             included_by_samesite);
+        if (!included_by_samesite) {
+          status.AddWarningReason(
+              CookieInclusionStatus::
+                  WARN_SAMEPARTY_INCLUSION_OVERRULED_SAMESITE);
+        }
       }
       break;
     }
@@ -952,6 +966,8 @@ CookieAccessResult CanonicalCookie::IsSetPermittedInContext(
       FALLTHROUGH;
     case CookieSamePartyStatus::kEnforceSamePartyInclude: {
       DCHECK(IsSameParty());
+      access_result.status.AddWarningReason(
+          CookieInclusionStatus::WARN_TREATED_AS_SAMEPARTY);
       // Remove any SameSite exclusion reasons, since SameParty overrides
       // SameSite.
       DCHECK(!access_result.status.HasExclusionReason(
@@ -970,10 +986,23 @@ CookieAccessResult CanonicalCookie::IsSetPermittedInContext(
             CookieInclusionStatus::EXCLUDE_SAMESITE_UNSPECIFIED_TREATED_AS_LAX,
         });
       }
+
+      // Update metrics.
+      if (access_result.status.HasOnlyExclusionReason(
+              CookieInclusionStatus::EXCLUDE_SAMEPARTY_CROSS_PARTY_CONTEXT) &&
+          included_by_samesite) {
+        access_result.status.AddWarningReason(
+            CookieInclusionStatus::WARN_SAMEPARTY_EXCLUSION_OVERRULED_SAMESITE);
+      }
       if (access_result.status.IsInclude()) {
         UMA_HISTOGRAM_BOOLEAN(
             "Cookie.SamePartySetIncluded.InclusionUnderSameSite",
             included_by_samesite);
+        if (!included_by_samesite) {
+          access_result.status.AddWarningReason(
+              CookieInclusionStatus::
+                  WARN_SAMEPARTY_INCLUSION_OVERRULED_SAMESITE);
+        }
       }
       break;
     }
