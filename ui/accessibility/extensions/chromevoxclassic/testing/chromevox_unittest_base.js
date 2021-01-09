03be77aa43613f198cf749ae100219056df77256
David Tseng <dtseng@google.com>
2021-01-09 06:04:19 +0000

    Cleanup accessibility extension helper
    Mostly a follow up to
    https://chromium-review.googlesource.com/c/chromium/src/+/2613726
    
    R=dmazzoni@chromium.org
    
    AX-Relnotes: n/a
    Change-Id: I12ba5e9dc3fba9ac529712190f26ae209528fed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617230
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841795}

--- ui/accessibility/extensions/chromevoxclassic/testing/chromevox_unittest_base.js
+++ ui/accessibility/extensions/chromevoxclassic/testing/chromevox_unittest_base.js
@@ -11,7 +11,9 @@ GEN_INCLUDE([
   '//chrome/browser/resources/chromeos/accessibility/chromevox/testing/' +
       'common.js',
   '//chrome/browser/resources/chromeos/accessibility/common/testing/' +
-      'callback_helper.js'
+      'callback_helper.js',
+  '//chrome/browser/resources/chromeos/accessibility/common/testing/' +
+      'common.js'
 ]);
 
 /**
