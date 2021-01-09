aa28f99ee4fd092b5136667be8c97196ec99fdd4
Dale Curtis <dalecurtis@chromium.org>
2021-01-09 00:14:50 +0000

    Revert "[LanguageDetection] Pass language model to TranslateAgent."
    This reverts commit 5a849c90f8a77a0f202ab018aed59ac8676e441c.
    
    Reason for revert: https://bugs.chromium.org/p/chromium/issues/detail?id=1164560
    
    Original change's description:
    > [LanguageDetection] Pass language model to TranslateAgent.
    >
    > This change provides the file for the language detection model needed
    > in the TranslateAgent.
    >
    > Bug: 1151422
    > Change-Id: I24870758adf8b00ee378ca7ccc55bd45edb164d1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2599580
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: Scott Little <sclittle@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Trevor  Perrier <perrier@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#841678}
    
    TBR=dcheng@chromium.org,jam@chromium.org,sclittle@chromium.org,sophiechang@chromium.org,mcrouse@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,perrier@chromium.org
    
    Change-Id: I7d33ec2acd918141ae310911550e3ffb86f336b8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1151422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2618475
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841723}

--- chrome/renderer/translate/translate_agent_browsertest.cc
+++ chrome/renderer/translate/translate_agent_browsertest.cc
@@ -52,8 +52,6 @@ class FakeContentTranslateDriver
     details_ = details;
     page_level_translation_critiera_met_ = page_level_translation_critiera_met;
   }
-  void GetLanguageDetectionModel(
-      GetLanguageDetectionModelCallback callback) override {}
 
   void ResetNewPageValues() {
     called_new_page_ = false;
