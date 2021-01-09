422ab9a5fbbe7f7f31ee4da5347a1e721c9a20d2
Xiaohui Chen <xiaohuic@google.com>
2021-01-09 04:20:30 +0000

    assistant: remove remaining bloom constants
    Bug: None
    Change-Id: I9eee624092d6ac2b54d242f94dfc53c68121264c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617506
    Auto-Submit: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841783}

--- chromeos/services/assistant/assistant_manager_service_impl.cc
+++ chromeos/services/assistant/assistant_manager_service_impl.cc
@@ -149,9 +149,6 @@ const char* ToTriggerSource(AssistantEntryPoint entry_point) {
       return kEntryPointProactiveSuggestions;
     case AssistantEntryPoint::kLauncherChip:
       return kEntryPointLauncherChip;
-    case AssistantEntryPoint::kBloom:
-      return kEntryPointUnspecified;  // TODO(jeroendh): Use Bloom specific
-                                      // entrypoint
   }
 }
 
--- chromeos/services/assistant/public/cpp/assistant_enums.h
+++ chromeos/services/assistant/public/cpp/assistant_enums.h
@@ -63,8 +63,9 @@ enum class AssistantQuerySource {
   kQuickAnswers = 11,
   kLauncherChip = 12,
   kBetterOnboarding = 13,
-  kBloom = 14,
-  kMaxValue = kBloom,
+  // Deprecated, please do not use.
+  // kBloom = 14,
+  kMaxValue = kBetterOnboarding,
 };
 
 // Enumeration of possible Assistant interaction types.
@@ -128,8 +129,9 @@ enum class AssistantEntryPoint {
   kLauncherSearchBoxIcon = 9,
   kProactiveSuggestions = 10,
   kLauncherChip = 11,
-  kBloom = 12,
-  kMaxValue = kBloom,
+  // Deprecated, please do not reuse
+  // kBloom = 12,
+  kMaxValue = kLauncherChip,
 };
 
 // Enumeration of Assistant exit points. These values are persisted to logs.
