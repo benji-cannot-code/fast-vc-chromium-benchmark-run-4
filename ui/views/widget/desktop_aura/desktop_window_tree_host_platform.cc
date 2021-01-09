4dbcd7cc483c9f6f1187c67d0f2cad700a1c7e30
Minju Kim <mkim@igalia.com>
2021-01-09 04:23:39 +0000

    Make rounded corner of browser frame from NonClientView::GetWindowMask
    crrev.com/c/2578501 introduces
    PlatformWindowDelegate::GetWindowMaskForWindowShape and updates
    window region by the window mask in ozone/wayland.
    This CL is the implementation to make the corner of the browser frame
    rounded from the window mask.
    It is done by XShape in x11, but there is no equivalent extension
    of XShape in wayland.
    
    In this CL,
    1. Introduce WindowShapeUpdater to update |ui::Layer::alpha_shape_|
       for the rounded corner of window shape from
       NonClientView::GetWindowMask.
       - When bounds changes, ui::Layer::alpha_shape_ is updated according
         to the ShapeRects converted from NonClientView::GetWindowMask.
       - UpdateWindowTransparency when ui::Layer::alpha_shape_ is updated.
    
    2. Set transparent:true to the compositor when NonClientView has
       window mask.
       Otherwise, AppendQuadsToFillScreen to always fill the entire
       screen in LayerTreeHostImpl.
    
    Bug: 1126828
    Change-Id: I3a712b8c5f33f9fd32bd2cd5e6ee8664732e3ca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2419056
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: MINJU KIM <mkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#841784}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/corewm/tooltip_aura.h"
 #include "ui/views/widget/desktop_aura/desktop_drag_drop_client_ozone.h"
 #include "ui/views/widget/desktop_aura/desktop_native_widget_aura.h"
+#include "ui/views/widget/desktop_aura/window_shape_updater.h"
 #include "ui/views/widget/widget_aura_utils.h"
 #include "ui/views/window/native_frame_view.h"
 #include "ui/wm/core/window_util.h"
@@ -200,7 +201,13 @@ void DesktopWindowTreeHostPlatform::OnNativeWidgetCreated(
   native_widget_delegate_->OnNativeWidgetCreated();
 }
 
-void DesktopWindowTreeHostPlatform::OnWidgetInitDone() {}
+void DesktopWindowTreeHostPlatform::OnWidgetInitDone() {
+  // WindowShape is updated from ShapeRects transformed from
+  // NonClientView::GetWindowMask. We can guarantee that |NonClientView| is
+  // created OnWidgetInitDone.
+  WindowShapeUpdater::CreateWindowShapeUpdater(
+      this, this->desktop_native_widget_aura());
+}
 
 void DesktopWindowTreeHostPlatform::OnActiveWindowChanged(bool active) {}
 
@@ -438,6 +445,8 @@ gfx::Rect DesktopWindowTreeHostPlatform::GetWorkAreaBoundsInScreen() const {
 
 void DesktopWindowTreeHostPlatform::SetShape(
     std::unique_ptr<Widget::ShapeRects> native_shape) {
+  // TODO(crbug.com/1158733) : When supporting PlatformWindow::SetShape,
+  // Calls ui::Layer::SetAlphaShape and sets |is_shape_explicitly_set_| to true.
   platform_window()->SetShape(std::move(native_shape), GetRootTransform());
 }
 
@@ -560,7 +569,10 @@ bool DesktopWindowTreeHostPlatform::ShouldUseNativeFrame() const {
 }
 
 bool DesktopWindowTreeHostPlatform::ShouldWindowContentsBeTransparent() const {
-  return platform_window()->ShouldWindowContentsBeTransparent();
+  return platform_window()->ShouldWindowContentsBeTransparent() ||
+         const_cast<DesktopWindowTreeHostPlatform*>(this)
+             ->GetWindowMaskForWindowShape(GetBoundsInPixels().size())
+             .has_value();
 }
 
 void DesktopWindowTreeHostPlatform::FrameTypeChanged() {
