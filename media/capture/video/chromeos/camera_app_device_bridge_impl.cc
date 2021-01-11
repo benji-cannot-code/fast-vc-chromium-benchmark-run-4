b3d3ff77104d509f1159fe29ae98b84b61ee3373
Wei Lee <wtlee@google.com>
2021-01-11 10:51:00 +0000

    VCD: Fix the race condition when closing CameraAppDeviceImpl
    After https://crrev.com/c/2602969, |OnDeviceClosed()| might be triggered
    from different threads. But in |OnDeviceClosed()|, it will access
    |camera_app_devices_| and might introduce race condition.
    
    Therefore, to fix it, we can post the task to make sure that
    |OnDeviceClosed()| will be called on the same thread.
    
    And since now we will not call |OnDeviceClosed()| directly and the mojo
    error handler of a receiver set might be triggered more than once, we
    should also handle it properly.
    
    Bug: b/177191321
    Test: Consecutively passes CCAUISettings.swa for 20 times
    
    Change-Id: I93e546a98c410017f17071c97336bfe40dcc7b69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2619247
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841974}

--- media/capture/video/chromeos/camera_app_device_bridge_impl.cc
+++ media/capture/video/chromeos/camera_app_device_bridge_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/command_line.h"
+#include "media/base/bind_to_current_loop.h"
 #include "media/base/media_switches.h"
 #include "media/capture/video/chromeos/public/cros_features.h"
 #include "media/capture/video/chromeos/video_capture_device_chromeos_halv3.h"
@@ -70,8 +71,9 @@ media::CameraAppDeviceImpl* CameraAppDeviceBridgeImpl::CreateCameraAppDevice(
   auto device_info = camera_info_getter_.Run(device_id);
   auto device_impl = std::make_unique<media::CameraAppDeviceImpl>(
       device_id, std::move(device_info),
-      base::BindOnce(&CameraAppDeviceBridgeImpl::OnDeviceClosed,
-                     base::Unretained(this), device_id));
+      media::BindToCurrentLoop(
+          base::BindOnce(&CameraAppDeviceBridgeImpl::OnDeviceClosed,
+                         base::Unretained(this), device_id)));
   auto result = camera_app_devices_.emplace(device_id, std::move(device_impl));
   return result.first->second.get();
 }
