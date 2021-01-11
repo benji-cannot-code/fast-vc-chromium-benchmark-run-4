b135cd554956f8afea07172b737320e522466ecb
Josip Sokcevic <sokcevic@google.com>
2021-01-11 19:14:54 +0000

    Use inclusive parameter names in PRESUBMIT
    Change-Id: Ic48fe52969335c74cef4f7df7258eb9cad556e04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2616523
    Commit-Queue: Josip Sokcevic <sokcevic@google.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Sam Clegg <sbc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842118}

--- build/util/lib/common/PRESUBMIT.py
+++ build/util/lib/common/PRESUBMIT.py
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 def _RunTests(input_api, output_api):
   return (input_api.canned_checks.RunUnitTestsInDirectory(
-          input_api, output_api, '.', whitelist=[r'.+_test.py$']))
+      input_api, output_api, '.', files_to_check=[r'.+_test.py$']))
 
 
 def CheckChangeOnUpload(input_api, output_api):
