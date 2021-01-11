088d70044936c8bd755345a2755f03abc490bdc2
Basia Zimirska <basiaz@google.com>
2021-01-11 19:26:46 +0000

    Adds a few missing kSupportedSpellCheckerLanguages entries
    The problem is that there are some discrepancies between kAcceptLanguages and kSupportedSpellCheckerLanguages for languages that there are dictionaries for. For example, before this change if a user adds "pt" to their languages in settings we don't offer to spellcheck even though we have dictionaries.
    Another issue that this CL solves is duplication of the languages in menu due to omitting country variants (see linked bug).
    
    Bug: 1074284
    Change-Id: I3a308ea1ba17ff912d7aae87e1bd9071bf451225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2606663
    Reviewed-by: Josh Simmons <jds@google.com>
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Cr-Commit-Position: refs/heads/master@{#842126}

--- components/spellcheck/common/spellcheck_common.cc
+++ components/spellcheck/common/spellcheck_common.cc
@@ -38,6 +38,7 @@ static constexpr LanguageRegion kSupportedSpellCheckerLanguages[] = {
     {"cy", "cy-GB"},
     {"da", "da-DK"},
     {"de", "de-DE"},
+    {"de-DE", "de-DE"},
     {"el", "el-GR"},
     {"en-AU", "en-AU"},
     {"en-CA", "en-CA"},
@@ -54,6 +55,7 @@ static constexpr LanguageRegion kSupportedSpellCheckerLanguages[] = {
     {"fa", "fa-IR"},
     {"fo", "fo-FO"},
     {"fr", "fr-FR"},
+    {"fr-FR", "fr-FR"},
     {"he", "he-IL"},
     {"hi", "hi-IN"},
     {"hr", "hr-HR"},
@@ -67,6 +69,7 @@ static constexpr LanguageRegion kSupportedSpellCheckerLanguages[] = {
     {"nb", "nb-NO"},
     {"nl", "nl-NL"},
     {"pl", "pl-PL"},
+    {"pt", "pt-BR"}, // based on kAliasMap from ui/base/l10n/l10n_util.cc
     {"pt-BR", "pt-BR"},
     {"pt-PT", "pt-PT"},
     {"ro", "ro-RO"},
