6129aa17e184f6ede3137ede04ebf51d379de6ef
Sergei Glazunov <glazunov@google.com>
2021-01-11 19:35:54 +0000

    Add option to disable BackupRefPtr in renderers
    Currently, we can't use BackupRefPtr in renderers anyway due to severe
    performance regressions. Therefore, add an option to disable the
    allocator support for the per-allocation reference count slot and save
    memory.
    
    In order to do so, as soon as we can detect whether the current process
    is a renderer, create a new partition with the desired set of flags
    and replace the "temporary" one in the allocator shim. Previously made
    allocations will keep using the old partition.
    
    Bug: 1073933
    Change-Id: Icb0d9359953c3b9b0cdc0c4ced113b4eca7a5c83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2449981
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Sergei Glazunov <glazunov@google.com>
    Cr-Commit-Position: refs/heads/master@{#842138}

--- content/app/content_main_runner_impl.cc
+++ content/app/content_main_runner_impl.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/allocator_extension.h"
 #include "base/allocator/allocator_shim.h"
 #include "base/allocator/buildflags.h"
+#include "base/allocator/partition_allocator/checked_ptr_support.h"
 #include "base/allocator/partition_allocator/partition_alloc_features.h"
 #include "base/at_exit.h"
 #include "base/base_switches.h"
@@ -256,6 +257,19 @@ void EnablePCScanForMallocPartitionsInBrowserProcessIfNeeded() {
           base::features::kPartitionAllocPCScanBrowserOnly)) {
     base::allocator::EnablePCScan();
   }
+#endif
+}
+
+// This function should be executed as early as possible once we can get the
+// command line arguments and determine whether the process needs BRP support.
+// Until that moment, all heap allocations end up in a slower temporary
+// partition with no thread cache and cause heap fragmentation.
+//
+// Furthermore, since the function has to allocate a new partition, it must
+// only run once.
+void ConfigurePartitionRefCountSupportIfNeeded(bool enable_ref_count) {
+#if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC) && DISABLE_REF_COUNT_IN_RENDERER
+  base::allocator::ConfigurePartitionRefCountSupport(enable_ref_count);
 #endif
 }
 
@@ -498,6 +512,9 @@ int RunZygote(ContentMainDelegate* delegate) {
       *base::CommandLine::ForCurrentProcess();
   std::string process_type =
       command_line.GetSwitchValueASCII(switches::kProcessType);
+  // Must run as early as possible. See the definition comment.
+  ConfigurePartitionRefCountSupportIfNeeded(process_type !=
+                                            switches::kRendererProcess);
   ContentClientInitializer::Set(process_type, delegate);
 
   MainFunctionParams main_params(command_line);
@@ -552,6 +569,12 @@ int RunOtherNamedProcessTypeMain(const std::string& process_type,
     {switches::kRendererProcess, RendererMain},
     {switches::kGpuProcess, GpuMain},
   };
+
+  if (process_type != switches::kZygoteProcess) {
+    // Must run as early as possible. See the definition comment.
+    ConfigurePartitionRefCountSupportIfNeeded(process_type !=
+                                              switches::kRendererProcess);
+  }
 
   for (size_t i = 0; i < base::size(kMainFunctions); ++i) {
     if (process_type == kMainFunctions[i].name) {
@@ -912,6 +935,9 @@ int ContentMainRunnerImpl::RunBrowser(MainFunctionParams& main_params,
                        TRACE_EVENT_SCOPE_THREAD);
   if (is_browser_main_loop_started_)
     return -1;
+
+  // Must run as early as possible. See the definition comment.
+  ConfigurePartitionRefCountSupportIfNeeded(/* enable_ref_count = */ true);
 
   bool should_start_minimal_browser = start_minimal_browser;
   if (!mojo_ipc_support_) {
