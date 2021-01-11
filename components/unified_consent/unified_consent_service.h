b6a97a05dd65de20027775f63863bf323f2be26e
Tanmoy Mollik <triploblastic@chromium.org>
2021-01-11 18:38:23 +0000

    Refactor components/unified_consent/* to use OnPrimaryAccountChanged()
    Method OnPrimaryAccountCleared() is deprecated. This cl changes the
    unified_consent code to use OnPrimaryAccountChanged() instead.
    
    Bug: 1158855
    Change-Id: I79f04420e957a95972651851b3dd706e7a9eaa05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622373
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842106}

--- components/unified_consent/unified_consent_service.h
+++ components/unified_consent/unified_consent_service.h
@@ -77,8 +77,8 @@ class UnifiedConsentService
   void Shutdown() override;
 
   // IdentityManager::Observer:
-  void OnPrimaryAccountCleared(
-      const CoreAccountInfo& previous_primary_account_info) override;
+  void OnPrimaryAccountChanged(
+      const signin::PrimaryAccountChangeEvent& event) override;
 
  private:
   friend class UnifiedConsentServiceTest;
