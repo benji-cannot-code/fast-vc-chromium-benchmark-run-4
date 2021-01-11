93c7adbd41c4bc618e506c58f9757ae44b54d3a1
Domenic Denicola <domenic@chromium.org>
2021-01-11 19:30:07 +0000

    Origin-keyed agent clusters: update WPT for the rename
    942aca884980a4dd6c582e3ca1a021769fd18f36 performed the minimal updates
    to the implementation and tests to shift from "origin isolation" to
    "origin-keyed agent clusters". This is part of the followup work to
    fully reflect the rename, in this case focused on the web platform
    tests.
    
    The majority of changes are comment and <title> changes, although some
    assertion messages are also updated.
    
    Note that instead of renaming the top-level directory, we move it under
    html/, since the the specification is now part of the HTML Standard.
    
    Bug: 1158853
    Change-Id: I008bb0b5f8c1f943849dfa5fd24cfc99a552f4dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2616819
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842131}

---
+++ third_party/blink/web_tests/external/wpt/html/browsers/origin/origin-keyed-agent-clusters/getter-special-cases/resources/data-url-test.mjs
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import { insertCustomIframe, testSupportScript } from "./helpers.mjs";
+import { testGetter } from "../../resources/helpers.mjs";
+
+export default () => {
+  promise_setup(() => {
+    return insertCustomIframe(`data:text/html,${testSupportScript}`);
+  });
+
+  // The data: URL iframe has an opaque origin, so it should return true, since
+  // for them site === origin so they are always origin-keyed.
+
+  testGetter(0, true, "data: URL child");
+};
