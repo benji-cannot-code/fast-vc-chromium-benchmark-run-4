75ea70ac848c34ff2d00b0a7c60b177917443f44
Ulan Degenbaev <ulan@chromium.org>
2021-01-11 18:58:08 +0000

    Support dedicated workers in WebMemoryAggregator
    This adds WebMemoryAggregator::VisitWorker for handling dedicated worker.
    It is simpler than the frame case because dedicated workers have the
    same origin as their parent (frame or worker). Additionally, aggregation
    never starts from a dedicated worker.
    
    Bug: 1085129
    Change-Id: If319fce5af3933870822f92bd6f360f524e33c4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2616705
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842111}

--- third_party/blink/renderer/core/timing/measure_memory/measure_memory_controller.cc
+++ third_party/blink/renderer/core/timing/measure_memory/measure_memory_controller.cc
@@ -133,6 +133,8 @@ struct RandomBitGenerator {
 WTF::String ConvertScope(WebMemoryAttribution::Scope scope) {
   using Scope = WebMemoryAttribution::Scope;
   switch (scope) {
+    case Scope::kDedicatedWorker:
+      return "DedicatedWorker";
     case Scope::kWindow:
       return "Window";
     case Scope::kCrossOriginAggregated:
