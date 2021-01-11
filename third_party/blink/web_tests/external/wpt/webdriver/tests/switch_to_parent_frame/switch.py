c694cf27e9c0a273302da1b428caddf02ff2e85c
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-01-11 01:31:17 +0000

    Import wpt@858930151c12292992391e926b5aede66a66b619
    Using wpt-import in Chromium 64c04f0d1ccf9e3db7f33353d92ad13d545bf2a7.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=foolip@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: I970b301e758d2b6042110ae807e402165163dba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2619681
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#841912}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/switch_to_parent_frame/switch.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/switch_to_parent_frame/switch.py
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import pytest
 
-from webdriver import StaleElementReferenceException
+from webdriver import NoSuchElementException
 
 from tests.support.asserts import assert_error, assert_success
 
@@ -67,13 +67,13 @@ def test_switch_from_iframe(session, inline, iframe):
     session.url = inline(iframe("<p>foo"))
     frame_element = session.find.css("iframe", all=False)
     session.switch_frame(frame_element)
-    stale_element = session.find.css("p", all=False)
+    element = session.find.css("p", all=False)
 
     result = switch_to_parent_frame(session)
     assert_success(result)
 
-    with pytest.raises(StaleElementReferenceException):
-        stale_element.text
+    with pytest.raises(NoSuchElementException):
+        element.text
 
 
 def test_switch_from_top_level(session, inline):
