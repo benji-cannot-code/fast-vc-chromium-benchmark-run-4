2dbaa3517a6a87027f26f3c9edd33755e504012f
Jérémie Boulic <jboulic@chromium.org>
2021-01-11 08:35:03 +0000

    Files app: Generate more JS modules in the foreground page
    In //ui/file_manager/file_manager/foreground/js/:
    - drop_effect_and_label.js
    - empty_folder_controller.js
    
    In //ui/file_manager/file_manager/foreground/js/ui/:
    - empty_folder.js
    - files_alert_dialog.js
    
    Bug: 1133186
    Change-Id: I140cc96d9721eb37747216546a5f59fc442342e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2620920
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#841954}

--- ui/file_manager/file_manager/foreground/js/empty_folder_controller.js
+++ ui/file_manager/file_manager/foreground/js/empty_folder_controller.js
@@ -3,10 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// #import {FilesAlertDialog} from './ui/files_alert_dialog.m.js';
+// #import {EmptyFolder} from './ui/empty_folder.m.js';
+// #import {FileListModel} from './file_list_model.m.js';
+// #import {DirectoryModel} from './directory_model.m.js';
+// #import {str, strf, util} from '../../common/js/util.m.js';
+// #import {constants} from './constants.m.js';
+// #import {assert} from 'chrome://resources/js/assert.m.js';
+
 /**
  * Empty folder controller.
  */
-class EmptyFolderController {
+/* #export */ class EmptyFolderController {
   /**
    * @param {!EmptyFolder} emptyFolder Empty folder ui.
    * @param {!DirectoryModel} directoryModel Directory model.
