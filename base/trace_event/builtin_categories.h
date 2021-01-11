fcd3338013529842ed17f483e49298945523088d
Alexander Timin <altimin@chromium.org>
2021-01-11 19:34:15 +0000

    [devtools] Remove devtools.timeline.async category.
    Together with removing the frontend code in crrev.com/c/2617780, remove
    the code emitting the old-style flow events for devtools and remove the
    category.
    
    R=caseq@chromium.org,eseckler@chromium.org
    CC=yangguo@chromium.org
    BUG=1164298
    
    Change-Id: I929c09d8aef5fb07ff64b0aa5960126111f433ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617939
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842137}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -67,7 +67,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("device")                                                            \
   X("devtools")                                                          \
   X("devtools.timeline")                                                 \
-  X("devtools.timeline.async")                                           \
   X("disk_cache")                                                        \
   X("download")                                                          \
   X("download_service")                                                  \
