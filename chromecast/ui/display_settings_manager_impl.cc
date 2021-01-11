466e144c22d21cfee67607b8fd0799b1efeef2d9
Daniel Nicoara <dnicoara@chromium.org>
2021-01-11 17:52:41 +0000

    cast: Update DisplaySettingsManager's use of the NativeDisplayDelegate API
    https://chromium-review.googlesource.com/c/chromium/src/+/2613152
    updated the NativeDisplayDelegate API. Update the usecase to fix the
    build.
    
    Bug: b:177105618
    Test: Build cast_shell internally
    Change-Id: I9234a35d2dcdb6585dd1df6be8f650254eb7c01a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622275
    Commit-Queue: Gil Dekel <gildekel@chromium.org>
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Gil Dekel <gildekel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842084}

--- chromecast/ui/display_settings_manager_impl.cc
+++ chromecast/ui/display_settings_manager_impl.cc
@@ -30,17 +30,6 @@ const float kMinApiBrightness = 0.0f;
 const float kMaxApiBrightness = 1.0f;
 const float kDefaultApiBrightness = kMaxApiBrightness;
 
-#if defined(USE_AURA)
-bool CheckDisplayStatus(const base::flat_map<int64_t, bool>& statuses) {
-  for (const auto& status : statuses) {
-    if (!status.second) {
-      return false;
-    }
-  }
-  return true;
-}
-#endif  // defined(USE_AURA)
-
 }  // namespace
 
 DisplaySettingsManagerImpl::DisplaySettingsManagerImpl(
@@ -110,15 +99,13 @@ void DisplaySettingsManagerImpl::AddReceiver(
 
 void DisplaySettingsManagerImpl::SetScreenPowerOn(PowerToggleCallback callback) {
 #if defined(USE_AURA)
-  display_configurator_->EnableDisplay(
-      base::BindOnce(&CheckDisplayStatus).Then(std::move(callback)));
+  display_configurator_->EnableDisplay(std::move(callback));
 #endif  // defined(USE_AURA)
 }
 
 void DisplaySettingsManagerImpl::SetScreenPowerOff(PowerToggleCallback callback) {
 #if defined(USE_AURA)
-  display_configurator_->DisableDisplay(
-      base::BindOnce(&CheckDisplayStatus).Then(std::move(callback)));
+  display_configurator_->DisableDisplay(std::move(callback));
 #endif  // defined(USE_AURA)
 }
 
