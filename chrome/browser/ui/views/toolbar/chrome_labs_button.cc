d3cf9ce1f572c71d96e9b7d5dde70b5d8e763e2b
Sammie Quon <sammiequon@chromium.org>
2021-01-11 21:18:57 +0000

    Revert "Fix flakiness in ChromeLabsButtonTest.ShowAndHideChromeLabsBubbleOnPress"
    This reverts commit 9cc644774e7f36f2d8d042b8cace33e48a346d1e.
    
    Reason for revert: Breaks ChromeOS builder. See https://crbug.com/1165349.
    
    Original change's description:
    > Fix flakiness in ChromeLabsButtonTest.ShowAndHideChromeLabsBubbleOnPress
    >
    > This test is very flaky, I believe because it relies on global state,
    > instead of explicitly setting up the LabInfo used to populate the labs
    > button. For example, if there were no current labs in SetUpLabs, the
    > test would fail. So this CL explicitly sets up the LabInfo for the labs
    > button. It's a bit messy that it requires a change to the production
    > code, but I didn't see any other way of plumbing that information
    > through to the model.
    >
    > Bug: 956719
    > Change-Id: I8078c211e37fc1eeff50a44043eca760cdc4850a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2618656
    > Reviewed-by: Collin Baker <collinbaker@chromium.org>
    > Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#842150}
    
    TBR=davidbienvenu@chromium.org,collinbaker@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I5cd05c29bb299b97fe8b491d865a9925c642a5f5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 956719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622519
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842174}

--- chrome/browser/ui/views/toolbar/chrome_labs_button.cc
+++ chrome/browser/ui/views/toolbar/chrome_labs_button.cc
@@ -16,8 +16,6 @@ ChromeLabsButton::ChromeLabsButton()
       views::ButtonController::NotifyAction::kOnPress);
 }
 
-ChromeLabsButton::~ChromeLabsButton() = default;
-
 void ChromeLabsButton::UpdateIcon() {
   UpdateIconsWithStandardColors(kChromeLabsIcon);
 }
@@ -26,17 +24,11 @@ const char* ChromeLabsButton::GetClassName() const {
   return "ChromeLabsButton";
 }
 
-void ChromeLabsButton::SetLabInfoForTesting(
-    const std::vector<LabInfo>& test_lab_info) {
-  test_lab_info_ = test_lab_info;
-}
-
 void ChromeLabsButton::ButtonPressed() {
   if (ChromeLabsBubbleView::IsShowing()) {
     ChromeLabsBubbleView::Hide();
     return;
   }
-  std::unique_ptr<ChromeLabsBubbleViewModel> model =
-      std::make_unique<ChromeLabsBubbleViewModel>(test_lab_info_);
-  ChromeLabsBubbleView::Show(this, std::move(model));
+  ChromeLabsBubbleView::Show(this,
+                             std::make_unique<ChromeLabsBubbleViewModel>());
 }
--- chrome/browser/ui/views/toolbar/chrome_labs_button.h
+++ chrome/browser/ui/views/toolbar/chrome_labs_button.h
@@ -6,13 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_TOOLBAR_CHROME_LABS_BUTTON_H_
 #define CHROME_BROWSER_UI_VIEWS_TOOLBAR_CHROME_LABS_BUTTON_H_
 
-#include "chrome/browser/ui/views/toolbar/chrome_labs_bubble_view_model.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 
 class ChromeLabsButton : public ToolbarButton {
  public:
   ChromeLabsButton();
-  ~ChromeLabsButton() override;
 
   // ToolbarButton:
   void UpdateIcon() override;
@@ -20,14 +18,8 @@ class ChromeLabsButton : public ToolbarButton {
   // views::View:
   const char* GetClassName() const override;
 
-  void SetLabInfoForTesting(const std::vector<LabInfo>& test_lab_info);
-
  private:
   void ButtonPressed();
-
-  // Used by tests to customize the LabInfo used to populate the button's menu.
-  // This will be empty in production code.
-  std::vector<LabInfo> test_lab_info_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TOOLBAR_CHROME_LABS_BUTTON_H_
