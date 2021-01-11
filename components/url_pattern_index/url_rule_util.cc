7c861c106314600c13c3e05b538194d7babef61f
Yusuf Ozuysal <yusufo@chromium.org>
2021-01-11 22:59:39 +0000

    Revert "[CodeInclusion] Improve terminology in subresource_filter"
    This reverts commit a28737582433f12d4b52e303b56db9daac1571cd.
    
    Reason for revert: Broke compile on https://ci.chromium.org/ui/p/chromium/builders/ci/Cast%20Audio%20Linux/95633/overview
    
    Original change's description:
    > [CodeInclusion] Improve terminology in subresource_filter
    >
    > Removing all but one case of master/slave/whitelist/blacklist in
    > subresource_filter directories. The remaining case involves a change to
    > safe browsing.
    >
    >
    > Bug: 1160252
    > Change-Id: I6aba50a767072ef4a9abb0bacf463712a27f7058
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2597012
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#842231}
    
    TBR=reillyg@chromium.org,jkarlin@chromium.org,csharrison@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: Ie3140e9aae06c95e265a8367d87975f9ed0eb005
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1160252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622665
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842233}

--- components/url_pattern_index/url_rule_util.cc
+++ components/url_pattern_index/url_rule_util.cc
@@ -154,7 +154,7 @@ std::string DomainOptionsToString(
 std::string FlatUrlRuleToFilterlistString(const flat::UrlRule* flat_rule) {
   std::string out;
 
-  if (flat_rule->options() & url_pattern_index::flat::OptionFlag_IS_ALLOWLIST)
+  if (flat_rule->options() & url_pattern_index::flat::OptionFlag_IS_WHITELIST)
     out += "@@";
 
   out += AnchorToString(flat_rule->anchor_left());
