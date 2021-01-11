7c422e0022c48fcfe39c244d160699b2c92a4bc7
Thomas Tellier <tellier@google.com>
2021-01-11 15:10:15 +0000

    [CrOS] Fix system label button ripple effect on login/lock screen
    Bug: 1160190
    Change-Id: Ib9d2e1ac936137be379e8aa08dd9ac4b34414186
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2601486
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Thomas Tellier <tellier@google.com>
    Cr-Commit-Position: refs/heads/master@{#842037}

--- ash/login/ui/system_label_button.cc
+++ ash/login/ui/system_label_button.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_provider.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
+#include "ui/gfx/color_utils.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/controls/highlight_path_generator.h"
 
@@ -28,6 +29,11 @@ constexpr int kSystemButtonMaxLabelWidthDp =
     kUserInfoBubbleWidth - 2 * kUserInfoBubbleExternalPadding -
     kSystemButtonIconSize - kSystemButtonImageLabelSpacing -
     2 * kSystemButtonBorderRadius;
+
+// Default base layer used for the bubble background, above which the system
+// label button lives.
+constexpr const AshColorProvider::BaseLayerType kBubbleLayerType =
+    AshColorProvider::BaseLayerType::kTransparent80;
 
 SkPath GetSystemButtonHighlightPath(const views::View* view) {
   gfx::Rect rect(view->GetLocalBounds());
@@ -109,8 +115,15 @@ void SystemLabelButton::SetAlertMode(bool alert_mode) {
       alert_mode ? AshColorProvider::ContentLayerType::kButtonLabelColorPrimary
                  : AshColorProvider::ContentLayerType::kButtonLabelColor));
 
+  // In default mode, this won't be the exact resulting color of the button as
+  // neither |background_color_| nor the color bubble below are fully opaque.
+  // Nevertheless, the result is visually satisfying and better than without
+  // applying any background color.
+  SkColor effective_background_color = color_utils::GetResultingPaintColor(
+      background_color_,
+      AshColorProvider::Get()->GetBaseLayerColor(kBubbleLayerType));
   const AshColorProvider::RippleAttributes ripple_attributes =
-      AshColorProvider::Get()->GetRippleAttributes(background_color_);
+      AshColorProvider::Get()->GetRippleAttributes(effective_background_color);
   SetInkDropBaseColor(ripple_attributes.base_color);
   SetInkDropVisibleOpacity(ripple_attributes.inkdrop_opacity);
   SetInkDropHighlightOpacity(ripple_attributes.highlight_opacity);
