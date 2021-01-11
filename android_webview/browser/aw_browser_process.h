1de09dd73344923f6b10c49efd064995f3c086d0
Clark DuVall <cduvall@chromium.org>
2021-01-11 19:03:27 +0000

    Add a synthetic field trial for the WebView APK type
    I needed to fix the IsolatedSplitsSynthetic trial to only log for
    monochrome, and thought it might be useful to have this as well. This
    will let us filter UMA data by the WebView APK type, and can help us
    answer the age old question of how many users have standalone WebView on
    N+.
    
    Bug: 1150162
    Change-Id: I4cc97632f514b0c54b07e205c33dd1c1d3703071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2618625
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842113}

--- android_webview/browser/aw_browser_process.h
+++ android_webview/browser/aw_browser_process.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ANDROID_WEBVIEW_BROWSER_AW_BROWSER_PROCESS_H_
 #define ANDROID_WEBVIEW_BROWSER_AW_BROWSER_PROCESS_H_
 
+#include "android_webview/browser/aw_apk_type.h"
 #include "android_webview/browser/aw_browser_context.h"
 #include "android_webview/browser/aw_feature_list_creator.h"
 #include "android_webview/browser/lifecycle/aw_contents_lifecycle_notifier.h"
@@ -74,6 +75,7 @@ class AwBrowserProcess {
   void PreMainMessageLoopRun();
 
   static void TriggerMinidumpUploading();
+  static ApkType GetApkType();
 
  private:
   void CreateSafeBrowsingUIManager();
