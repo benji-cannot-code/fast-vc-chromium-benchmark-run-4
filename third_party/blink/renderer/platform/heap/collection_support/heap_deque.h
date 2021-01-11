2ef1e4151d95da2c33108ec716f6cddf8af95b7e
Michael Lippautz <mlippautz@chromium.org>
2021-01-11 13:14:26 +0000

    heap: HeapHashCountedSet: Use standard GC types
    Drive-by: IWYU for HeapDeque.
    
    Bug: 1056170
    Change-Id: I0be011fd4701533e24ca9dfbbbc2c046059a4f0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2620563
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842010}

--- third_party/blink/renderer/platform/heap/collection_support/heap_deque.h
+++ third_party/blink/renderer/platform/heap/collection_support/heap_deque.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_COLLECTION_SUPPORT_HEAP_DEQUE_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_COLLECTION_SUPPORT_HEAP_DEQUE_H_
 
+#include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/heap/heap_allocator_impl.h"
 #include "third_party/blink/renderer/platform/heap/visitor.h"
 #include "third_party/blink/renderer/platform/wtf/deque.h"
