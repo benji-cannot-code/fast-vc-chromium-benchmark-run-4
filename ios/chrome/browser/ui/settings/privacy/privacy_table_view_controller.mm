433c7cf02882451f3341eb6afa64f04915f9db28
Stepan Khapugin <stkhapugin@chromium.org>
2021-01-12 16:07:08 +0000

    [iOS][Getaway] Respect the enterprise policy to disable Incognito mode.
    Hides the setting when the policy is enabled. No need to do anything
    else, since when there's no Incognito mode, there's nothing to protect,
    and without any incognito tabs all other code is no-op.
    
    Bug: none
    Change-Id: Iaeb94ab9f9d78a09096d0bbf611a992a19df28b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617882
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842544}

--- ios/chrome/browser/ui/settings/privacy/privacy_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/privacy/privacy_table_view_controller.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/browsing_data/browsing_data_features.h"
 #import "ios/chrome/browser/main/browser.h"
 #include "ios/chrome/browser/pref_names.h"
+#import "ios/chrome/browser/prefs/prefs_util.h"
 #import "ios/chrome/browser/ui/commands/open_new_tab_command.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_cell.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_item.h"
@@ -168,7 +169,10 @@ const char kGoogleServicesSettingsURL[] = "settings://open_google_services";
   [model addItem:[self handoffDetailItem]
       toSectionWithIdentifier:SectionIdentifierWebServices];
 
-  if (base::FeatureList::IsEnabled(kIncognitoAuthentication)) {
+  // Do not show the incognito authentication setting when Incongito mode is
+  // disabled.
+  if (base::FeatureList::IsEnabled(kIncognitoAuthentication) &&
+      !IsIncognitoModeDisabled(_browserState->GetPrefs())) {
     // Incognito authentication item.
     [model addItem:self.incognitoReauthItem
         toSectionWithIdentifier:SectionIdentifierIncognitoAuth];
