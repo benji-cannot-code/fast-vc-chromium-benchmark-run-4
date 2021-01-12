b904cd1174a48e0ccdcf2a33637f2d67132cbc11
Jordan Demeulenaere <jdemeulenaere@google.com>
2021-01-12 11:45:45 +0000

    [Autofill Assistant] Change the height and margins of placeholders.
    This CL changes the height and margins of the details placeholders so
    that it looks better when multiple placeholders are displayed. The
    height and margins are fixed, to optimize the case where we have 2 or 3
    text placeholders displayed [1].
    
    [1] b/174652960#comment14
    
    Bug: b/174652960
    Change-Id: I2e33570dee7767c58ba531b10e250ec50b23e0d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600819
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#842474}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/details/AssistantDetailsViewBinder.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/details/AssistantDetailsViewBinder.java
@@ -22,7 +22,9 @@ import android.media.ThumbnailUtils;
 import android.text.TextUtils;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.StyleRes;
@@ -91,6 +93,8 @@ class AssistantDetailsViewBinder
     private final int mImageHeight;
     private final int mPulseAnimationStartColor;
     private final int mPulseAnimationEndColor;
+    private final int mTextPlaceholdersHeight;
+    private final int mTextPlaceholdersMargin;
 
     private ValueAnimator mPulseAnimation;
     private ImageFetcher mImageFetcher;
@@ -103,6 +107,10 @@ class AssistantDetailsViewBinder
                 R.dimen.autofill_assistant_details_image_size);
         mPulseAnimationStartColor = context.getResources().getColor(R.color.modern_grey_300);
         mPulseAnimationEndColor = context.getResources().getColor(R.color.modern_grey_200);
+        mTextPlaceholdersHeight = context.getResources().getDimensionPixelSize(
+                R.dimen.autofill_assistant_details_text_placeholders_height);
+        mTextPlaceholdersMargin = context.getResources().getDimensionPixelSize(
+                R.dimen.autofill_assistant_details_text_placeholders_margin);
         mImageFetcher = imageFetcher;
     }
 
@@ -174,6 +182,15 @@ class AssistantDetailsViewBinder
         hideIf(viewHolder.mDescriptionLine3View, hideDescriptionLine3);
         hideIfEmpty(viewHolder.mPriceAttributionView);
 
+        // Set the height of the potential placeholders next to the image.
+        setTextHeightAndMargin(viewHolder.mTitleView, placeholders.getShowTitlePlaceholder());
+        setTextHeightAndMargin(viewHolder.mDescriptionLine1View,
+                placeholders.getShowDescriptionLine1Placeholder());
+        setTextHeightAndMargin(viewHolder.mDescriptionLine2View,
+                placeholders.getShowDescriptionLine2Placeholder());
+        setTextHeightAndMargin(viewHolder.mDescriptionLine3View,
+                placeholders.getShowDescriptionLine3Placeholder());
+
         // If no price provided, hide the price view (containing separator, price label, and price).
         viewHolder.mPriceView.setVisibility(
                 details.getTotalPrice().isEmpty() ? View.GONE : View.VISIBLE);
@@ -217,6 +234,22 @@ class AssistantDetailsViewBinder
 
     private void hideIf(TextView view, boolean hide) {
         view.setVisibility(hide ? View.GONE : View.VISIBLE);
+    }
+
+    private void setTextHeightAndMargin(TextView view, boolean isPlaceholder) {
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) view.getLayoutParams();
+
+        if (isPlaceholder) {
+            layoutParams.height = mTextPlaceholdersHeight;
+            layoutParams.topMargin = mTextPlaceholdersMargin;
+            layoutParams.bottomMargin = mTextPlaceholdersMargin;
+        } else {
+            layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            layoutParams.topMargin = 0;
+            layoutParams.bottomMargin = 0;
+        }
+
+        view.setLayoutParams(layoutParams);
     }
 
     private void setTextStyles(AssistantDetails details, ViewHolder viewHolder) {
