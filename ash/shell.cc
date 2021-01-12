146452af545f88cceb3a3f010a34c687784432e4
Sonny Sasaka <sonnysasaka@chromium.org>
2021-01-12 17:40:37 +0000

    [Bluetooth] Remove HidBatteryListener and PeripheralBatteryTracker
    Battery reporting via HID is now consolidated into BlueZ (CL:2593287),
    so there is no need to listen to powerd directly. This only affects
    Bluetooth HID devies and does not affect non-Bluetooth HID battery
    notification.
    
    Bug: b:166543531, b:172361193
    Change-Id: I222677b96463120acdd2f7665d9a3c87df32346e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2594158
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842579}

--- ash/shell.cc
+++ ash/shell.cc
@@ -75,7 +75,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/media/media_notification_controller_impl.h"
 #include "ash/multi_device_setup/multi_device_notification_presenter.h"
 #include "ash/policy/policy_recommendation_restorer.h"
-#include "ash/power/peripheral_battery_tracker.h"
 #include "ash/public/cpp/ash_constants.h"
 #include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/ash_prefs.h"
@@ -945,10 +944,6 @@ void Shell::Init(
 
   peripheral_battery_notifier_ = std::make_unique<PeripheralBatteryNotifier>(
       peripheral_battery_listener_.get());
-  if (base::FeatureList::IsEnabled(
-          chromeos::features::kShowBluetoothDeviceBattery)) {
-    peripheral_battery_tracker_ = std::make_unique<PeripheralBatteryTracker>();
-  }
   power_event_observer_.reset(new PowerEventObserver());
 
   if (features::IsCaptureModeEnabled()) {
