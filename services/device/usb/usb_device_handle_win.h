a544a37c138becf0729826329f3306ac3d856235
Reilly Grant <reillyg@chromium.org>
2021-01-12 04:59:38 +0000

    usb: Expand tests for USB transfer completion order
    This change expands the manual test for transferIn() to send much more
    data and assert that many overlapping transfers will still complete in
    the correct order.
    
    Two changes are necessary to make these tests pass. First, when aborting
    a transfer on device close the ERROR_REQUEST_ABORTED should be mapped to
    an AbortError DOMException. Second, transfers should be tracked in a
    list rather than a map so that iteration order is well defined.
    
    Bug: 1153647
    Change-Id: I248ce4724e391216783a6873809aca43e4690b96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2591928
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: James Hollyer <jameshollyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842380}

--- services/device/usb/usb_device_handle_win.h
+++ services/device/usb/usb_device_handle_win.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_DEVICE_USB_USB_DEVICE_HANDLE_WIN_H_
 #define SERVICES_DEVICE_USB_USB_DEVICE_HANDLE_WIN_H_
 
+#include <list>
 #include <map>
 #include <memory>
 #include <vector>
@@ -207,7 +208,7 @@ class UsbDeviceHandleWin : public UsbDeviceHandle {
 
   std::map<uint8_t, Interface> interfaces_;
   std::map<uint8_t, Endpoint> endpoints_;
-  std::map<Request*, std::unique_ptr<Request>> requests_;
+  std::list<std::unique_ptr<Request>> requests_;
 
   // Control transfers which are waiting for a function handle to be ready.
   std::vector<OpenInterfaceCallback> ep0_ready_callbacks_;
