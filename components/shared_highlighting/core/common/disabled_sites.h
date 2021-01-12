908c9c8ca6f6e3ff91007b1322dfb1bc2373f19e
Tommy Martino <tmartino@google.com>
2021-01-12 20:00:27 +0000

    [SH iOS] Add component code for disabling per-site
    This adds a helper function that platforms can use to determine if the
    current site's URL should prevent the Link to Text feature from being
    offered. This will be called in a follow-up by platform-specific code
    that enables/disables the option. For the time being, we expect the
    number of sites affected to be very small and will reevaluate if the
    list gets longer than ~a dozen.
    
    Eventually we'd like to make this more dynamic (possibly delivering the
    URLs and regexes from a server), but this version is meant to get a very
    quick spot fix out the door.
    
    Bug: 1157981
    Change-Id: I1785459b298d93528415d8735aaa01c4d7a7769e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2587432
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842655}

---
+++ components/shared_highlighting/core/common/disabled_sites.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_DISABLED_SITES_H_
+#define COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_DISABLED_SITES_H_
+
+#include "url/gurl.h"
+
+namespace shared_highlighting {
+
+// Returns true iff Link to Text menu options should be enabled on this page.
+// Uses a blocklist to identify certain sites where personalized or dynamic
+// content make it unlikely that a generated URL will actually work when
+// shared.
+bool ShouldOfferLinkToText(const GURL& url);
+
+}  // namespace shared_highlighting
+
+#endif  // COMPONENTS_SHARED_HIGHLIGHTING_CORE_COMMON_DISABLED_SITES_H_
