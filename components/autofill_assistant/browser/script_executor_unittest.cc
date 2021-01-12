fd8bb5f5273365403e8f5708812574a18672b323
Jordan Demeulenaere <jdemeulenaere@google.com>
2021-01-12 14:57:16 +0000

    [Autofill Assistant] Support showing multiple details.
    This CL adds support for showing multiple details to the bottom sheet,
    instead of only a single one.
    
    Bug: b/174652960
    Change-Id: I8b31f752b0d0cd81a9d7b7f5f3b9b0c8a8cf1b95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2604339
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#842524}

--- components/autofill_assistant/browser/script_executor_unittest.cc
+++ components/autofill_assistant/browser/script_executor_unittest.cc
@@ -383,7 +383,7 @@ TEST_F(ScriptExecutorTest, ClearDetailsWhenFinished) {
 
   delegate_.SetDetails(std::make_unique<Details>());  // empty, but not null
   executor_->Run(&user_data_, executor_callback_.Get());
-  EXPECT_EQ(nullptr, delegate_.GetDetails());
+  EXPECT_THAT(delegate_.GetDetails(), IsEmpty());
 }
 
 TEST_F(ScriptExecutorTest, DontClearDetailsIfOtherActionsAreLeft) {
@@ -403,7 +403,7 @@ TEST_F(ScriptExecutorTest, DontClearDetailsIfOtherActionsAreLeft) {
 
   delegate_.SetDetails(std::make_unique<Details>());  // empty, but not null
   executor_->Run(&user_data_, executor_callback_.Get());
-  EXPECT_NE(nullptr, delegate_.GetDetails());
+  EXPECT_THAT(delegate_.GetDetails(), Not(IsEmpty()));
 }
 
 TEST_F(ScriptExecutorTest, ClearDetailsOnError) {
@@ -415,9 +415,10 @@ TEST_F(ScriptExecutorTest, ClearDetailsOnError) {
       .WillOnce(RunOnceCallback<5>(net::HTTP_UNAUTHORIZED, ""));
   EXPECT_CALL(executor_callback_,
               Run(Field(&ScriptExecutor::Result::success, false)));
+
   delegate_.SetDetails(std::make_unique<Details>());  // empty, but not null
   executor_->Run(&user_data_, executor_callback_.Get());
-  EXPECT_EQ(nullptr, delegate_.GetDetails());
+  EXPECT_THAT(delegate_.GetDetails(), IsEmpty());
 }
 
 TEST_F(ScriptExecutorTest, UpdateScriptStateWhileRunning) {
