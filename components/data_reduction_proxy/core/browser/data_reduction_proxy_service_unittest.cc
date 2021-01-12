c8b88218e35f00999939e35624ea8260c6b2fda2
Sophie Chang <sophiechang@chromium.org>
2021-01-12 17:50:04 +0000

    Remove content dep from components/drp/core/browser
    Bug: 1154790
    Change-Id: If4d9a70b41151d3f465976637a54aa826821bbfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2623717
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842583}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_service_unittest.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_service_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/test/task_environment.h"
 #include "base/time/default_clock.h"
 #include "base/time/time.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client_test_utils.h"
@@ -25,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_switches.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/testing_pref_service.h"
-#include "content/public/test/browser_task_environment.h"
 #include "net/proxy_resolution/proxy_info.h"
 #include "net/proxy_resolution/proxy_resolution_service.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
@@ -53,10 +53,8 @@ class DataReductionProxyServiceTest : public testing::Test {
 
   PrefService* prefs() { return &prefs_; }
 
- protected:
-  content::BrowserTaskEnvironment task_environment_;
-
  private:
+  base::test::TaskEnvironment task_environment_;
   TestingPrefServiceSimple prefs_;
 };
 
