0094bcb6ea41dd522372201f232d677dac151425
Yulun Wu <yulunwu@chromium.org>
2021-01-12 20:49:23 +0000

    Live updates for search box and suggestion chip colors.
    Bug: 1139478
    Change-Id: Ie9179fc42d59def83910475e93ef93c5eef3b496
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622761
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842688}

--- ash/app_list/views/search_box_view.cc
+++ ash/app_list/views/search_box_view.cc
@@ -220,6 +220,13 @@ const char* SearchBoxView::GetClassName() const {
   return "SearchBoxView";
 }
 
+void SearchBoxView::OnThemeChanged() {
+  views::View::OnThemeChanged();
+  SetupAssistantButton();
+  SetupCloseButton();
+  OnWallpaperColorsChanged();
+}
+
 // static
 int SearchBoxView::GetFocusRingSpacing() {
   return kSearchBoxFocusRingWidth + kSearchBoxFocusRingPadding;
