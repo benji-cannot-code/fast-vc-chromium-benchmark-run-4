e10e6d25df23c3d8e907d33949a9355aa4448d5a
Clark DuVall <cduvall@chromium.org>
2021-01-12 04:35:08 +0000

    Bring back preloading of chrome Activity class with isolated splits
    This was reverted because the latest androidx.lifecycle library performs
    thread checks in the ComponentActivity constructor. The constructor
    calls LifecycleRegistry.addObserver(), so we can just override that
    method and make it a no-op when loading on a background thread.
    
    Bug: 1150600, 1160614
    Change-Id: I52e6eb63ce0b4a9c90c002fe34281298b02ef9d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622005
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842373}

--- chrome/android/junit/src/org/chromium/chrome/browser/base/SplitPreloaderTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/base/SplitPreloaderTest.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.base;
 import static com.google.common.truth.Truth.assertThat;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -24,6 +25,7 @@ import org.robolectric.annotation.Config;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.CallbackHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -85,12 +87,23 @@ public class SplitPreloaderTest {
     }
 
     private static class OnCompleteTracker implements SplitPreloader.OnComplete {
+        private SplitContext mBackgroundContext;
         private SplitContext mUiContext;
 
         @Override
-        public void run(Context context) {
+        public void runImmediatelyInBackgroundThread(Context context) {
+            assertNull(mBackgroundContext);
+            mBackgroundContext = (SplitContext) context;
+        }
+
+        @Override
+        public void runInUiThread(Context context) {
             assertNull(mUiContext);
             mUiContext = (SplitContext) context;
+        }
+
+        public SplitContext getBackgroundContext() {
+            return mBackgroundContext;
         }
 
         public SplitContext getUiContext() {
@@ -130,6 +143,8 @@ public class SplitPreloaderTest {
         assertThat(mContext.getBackgroundThreadContextNames()).containsExactly(SPLIT_A);
         assertTrue(tracker.getUiContext().wasCreatedOnUiThread());
         assertEquals(tracker.getUiContext().getName(), SPLIT_A);
+        assertFalse(tracker.getBackgroundContext().wasCreatedOnUiThread());
+        assertEquals(tracker.getBackgroundContext().getName(), SPLIT_A);
     }
 
     @Test
@@ -179,13 +194,29 @@ public class SplitPreloaderTest {
     }
 
     @Test
-    public void testPreload_withOnComplete_splitNotInstalled() {
-        Context[] contextHolder = new Context[1];
-        mPreloader.preload(SPLIT_A, (Context context) -> contextHolder[0] = context);
+    public void testPreload_withOnComplete_splitNotInstalled() throws Exception {
+        Context[] backgroundContextHolder = new Context[1];
+        Context[] uiContextHolder = new Context[1];
+        CallbackHelper helper = new CallbackHelper();
+        mPreloader.preload(SPLIT_A, new SplitPreloader.OnComplete() {
+            @Override
+            public void runImmediatelyInBackgroundThread(Context context) {
+                backgroundContextHolder[0] = context;
+                helper.notifyCalled();
+            }
+
+            @Override
+            public void runInUiThread(Context context) {
+                uiContextHolder[0] = context;
+            }
+        });
+        helper.waitForFirst();
+        assertEquals(backgroundContextHolder[0], mContext);
+
         mPreloader.wait(SPLIT_A);
 
         assertThat(mContext.getUiThreadContextNames()).isEmpty();
         assertThat(mContext.getBackgroundThreadContextNames()).isEmpty();
-        assertEquals(contextHolder[0], mContext);
+        assertEquals(uiContextHolder[0], mContext);
     }
 }
