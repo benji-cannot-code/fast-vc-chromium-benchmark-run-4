3f32a5d52b44d80239b371a965878ed96671e7eb
Corentin Wallez <cwallez@chromium.org>
2021-01-12 04:47:37 +0000

    WebGPU: Use V8GPUIndexFormat directly to avoid one conversion.
    This avoids doing the conversion from WTF::String to an enum once in the
    Blink bindings and once in the WebGPU module.
    
    Bug: chromium:852089
    Change-Id: I8765631c2b0562bdabfc6af246cab0453de63593
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2621754
    Auto-Submit: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842376}

--- third_party/blink/renderer/modules/webgpu/gpu_render_pass_encoder.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_render_pass_encoder.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webgpu/gpu_render_pass_encoder.h"
 
 #include "third_party/blink/renderer/bindings/modules/v8/double_sequence_or_gpu_color_dict.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_index_format.h"
 #include "third_party/blink/renderer/core/typed_arrays/typed_flexible_array_buffer_view.h"
 #include "third_party/blink/renderer/modules/webgpu/dawn_conversions.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_bind_group.h"
@@ -110,12 +111,11 @@ void GPURenderPassEncoder::setScissorRect(uint32_t x,
 }
 
 void GPURenderPassEncoder::setIndexBuffer(GPUBuffer* buffer,
-                                          const WTF::String& format,
+                                          const V8GPUIndexFormat& format,
                                           uint64_t offset,
                                           uint64_t size) {
   GetProcs().renderPassEncoderSetIndexBufferWithFormat(
-      GetHandle(), buffer->GetHandle(), AsDawnEnum<WGPUIndexFormat>(format),
-      offset, size);
+      GetHandle(), buffer->GetHandle(), AsDawnEnum(format), offset, size);
 }
 
 void GPURenderPassEncoder::setVertexBuffer(uint32_t slot,
