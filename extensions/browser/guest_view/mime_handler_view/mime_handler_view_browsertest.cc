18780f1585d32735203d87abbd475e39c1f5bda2
Kevin McNee <mcnee@chromium.org>
2021-01-12 20:21:37 +0000

    Dedupe mime_handler_view test extension id
    Several mime_handler_view tests have a copy of the test extension id.
    We now use the common constant.
    
    Bug: None
    Change-Id: If9c08f486698a1c32fc9c2871a379e474589a2f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2624771
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842666}

--- extensions/browser/guest_view/mime_handler_view/mime_handler_view_browsertest.cc
+++ extensions/browser/guest_view/mime_handler_view/mime_handler_view_browsertest.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h"
 #include "extensions/browser/guest_view/mime_handler_view/test_mime_handler_view_guest.h"
 #include "extensions/browser/process_manager.h"
+#include "extensions/common/constants.h"
 #include "extensions/common/guest_view/extensions_guest_view_messages.h"
 #include "extensions/common/mojom/guest_view.mojom.h"
 #include "extensions/test/result_catcher.h"
@@ -58,9 +59,6 @@ using guest_view::GuestViewManager;
 using guest_view::GuestViewManagerDelegate;
 using guest_view::TestGuestViewManager;
 using guest_view::TestGuestViewManagerFactory;
-
-// The test extension id is set by the key value in the manifest.
-const char kExtensionId[] = "oickdpebdnfbgkcaoklfcdhjniefkcji";
 
 class MimeHandlerViewTest : public extensions::ExtensionApiTest {
  public:
@@ -118,7 +116,8 @@ class MimeHandlerViewTest : public extensions::ExtensionApiTest {
     if (!extension)
       return nullptr;
 
-    CHECK_EQ(std::string(kExtensionId), extension->id());
+    EXPECT_EQ(std::string(extension_misc::kMimeHandlerPrivateTestExtensionId),
+              extension->id());
 
     return extension;
   }
