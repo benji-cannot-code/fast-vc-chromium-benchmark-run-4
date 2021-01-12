9c6fc6eaf8d98f4fe058517b8e65a46b9cfdd566
Tarun Bansal <tbansal@chromium.org>
2021-01-12 00:48:40 +0000

    Delete offline renovations code
    This experiment is no longer being worked on.
    See http://shortn/_YTSn6Rpl6W for more details.
    
    Change-Id: I0fd921cc62b518f6a142402389a6b8b9748b2fb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611836
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842297}

--- chrome/browser/offline_pages/background_loader_offliner.h
+++ chrome/browser/offline_pages/background_loader_offliner.h
@@ -31,7 +31,6 @@ namespace offline_pages {
 
 class OfflinerPolicy;
 class OfflinePageModel;
-class PageRenovationLoader;
 class PageRenovator;
 
 struct RequestStats {
@@ -85,7 +84,6 @@ class BackgroundLoaderOffliner
 
   // BackgroundSnapshotController::Client implementation.
   void StartSnapshot() override;
-  void RunRenovations() override;
 
   void SetBackgroundSnapshotControllerForTest(
       std::unique_ptr<BackgroundSnapshotController> controller);
@@ -170,11 +168,6 @@ class BackgroundLoaderOffliner
   std::unique_ptr<LoadTerminationListener> load_termination_listener_;
   // Whether we are on a low-end device.
   bool is_low_end_device_;
-
-  // PageRenovationLoader must live longer than the PageRenovator.
-  std::unique_ptr<PageRenovationLoader> page_renovation_loader_;
-  // Per-offliner PageRenovator instance.
-  std::unique_ptr<PageRenovator> page_renovator_;
 
   // Save state.
   SaveState save_state_;
