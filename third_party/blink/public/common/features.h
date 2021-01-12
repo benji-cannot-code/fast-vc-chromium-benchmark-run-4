f4cf3fd497b91ce8def1241ed9d7d28982f56a9a
Yao Xiao <yaoxia@chromium.org>
2021-01-12 22:15:41 +0000

    [floc] For the OT, set allows_third_party and make it finch controllable
    We expect doing 3rd-party OT for FLoC, thus setting
    origin_trial_allows_third_party. Also, gate the OT by a base::Feature,
    as this allows us to keep the experimentation under the global usage
    limit.
    
    Bug: 1163394
    Change-Id: Ibaf9554397f8d46fa68c837fe58acc66676a2401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611953
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842729}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -355,6 +355,8 @@ BLINK_COMMON_EXPORT extern const base::Feature kWebRtcDistinctWorkerThread;
 BLINK_COMMON_EXPORT extern const base::Feature
     kSendCnameAliasesToSubresourceFilterFromRenderer;
 
+BLINK_COMMON_EXPORT extern const base::Feature kInterestCohortAPIOriginTrial;
+
 }  // namespace features
 }  // namespace blink
 
