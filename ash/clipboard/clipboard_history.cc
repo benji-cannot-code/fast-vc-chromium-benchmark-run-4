4e4d94826afeae9d90937012d08c42c33ed9b3e2
Alex Newcomer <newcomer@chromium.org>
2021-01-12 00:51:21 +0000

    [multipaste] Add consecutive copy/paste metrics
    PM wants these to measure usefulness of clipboard history.
    
    More consecutive copies or pastes  should mean users are using
    multipaste more.
    
    Bug: 1145445
    Change-Id: I4d6cecb618e8e95f9438a44e8824aa23fbc5e89f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613350
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842298}

--- ash/clipboard/clipboard_history.cc
+++ ash/clipboard/clipboard_history.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/clipboard/clipboard_history_util.h"
 #include "ash/clipboard/clipboard_nudge_controller.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/stl_util.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "ui/base/clipboard/clipboard_monitor.h"
@@ -67,8 +68,6 @@ void ClipboardHistory::OnClipboardDataChanged() {
   if (!ClipboardHistoryUtil::IsEnabledInCurrentMode())
     return;
 
-  // TODO(newcomer): Prevent Clipboard from recording metrics when pausing
-  // observation.
   if (num_pause_ > 0)
     return;
 
@@ -88,6 +87,15 @@ void ClipboardHistory::OnClipboardDataChanged() {
     return;
   }
 
+  // Debounce calls to `OnClipboardOperation()`. Certain surfaces
+  // (Omnibox) may Read/Write to the clipboard multiple times in one user
+  // initiated operation.
+  clipboard_histogram_weak_factory_.InvalidateWeakPtrs();
+  base::SequencedTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(&ClipboardHistory::OnClipboardOperation,
+                                clipboard_histogram_weak_factory_.GetWeakPtr(),
+                                /*copy=*/true));
+
   // We post commit |clipboard_data| at the end of the current task sequence to
   // debounce the case where multiple copies are programmatically performed.
   // Since only the most recent copy will be at the top of the clipboard, the
@@ -101,6 +109,39 @@ void ClipboardHistory::OnClipboardDataChanged() {
       FROM_HERE,
       base::BindOnce(&ClipboardHistory::MaybeCommitData,
                      commit_data_weak_factory_.GetWeakPtr(), *clipboard_data));
+}
+
+void ClipboardHistory::OnClipboardDataRead() {
+  if (num_pause_ > 0)
+    return;
+
+  // Debounce calls to `OnClipboardOperation()`. Certain surfaces
+  // (Omnibox) may Read/Write to the clipboard multiple times in one user
+  // initiated operation.
+  clipboard_histogram_weak_factory_.InvalidateWeakPtrs();
+  base::SequencedTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(&ClipboardHistory::OnClipboardOperation,
+                                clipboard_histogram_weak_factory_.GetWeakPtr(),
+                                /*copy=*/false));
+}
+
+void ClipboardHistory::OnClipboardOperation(bool copy) {
+  if (copy) {
+    consecutive_copies_++;
+    if (consecutive_pastes_ > 0) {
+      base::UmaHistogramCounts100("Ash.Clipboard.ConsecutivePastes",
+                                  consecutive_pastes_);
+      consecutive_pastes_ = 0;
+    }
+    return;
+  }
+
+  consecutive_pastes_++;
+  if (consecutive_copies_ > 0) {
+    base::UmaHistogramCounts100("Ash.Clipboard.ConsecutiveCopies",
+                                consecutive_copies_);
+    consecutive_copies_ = 0;
+  }
 }
 
 base::WeakPtr<ClipboardHistory> ClipboardHistory::GetWeakPtr() {
