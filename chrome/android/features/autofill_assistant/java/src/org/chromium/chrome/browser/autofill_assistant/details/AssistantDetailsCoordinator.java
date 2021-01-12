34a97b6090f1426121c193843633792a583c885e
Jordan Demeulenaere <jdemeulenaere@google.com>
2021-01-12 18:51:08 +0000

    [Autofill Assistant] Group multiple details together.
    This CL merges multiple details into a single block, with the details
    separated by a line. See b/174652960#comment16 for screenshots.
    
    Bug: b/174652960
    Change-Id: Ia562ec600b513a120b503c8f7ffa676124089749
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2611096
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#842612}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/details/AssistantDetailsCoordinator.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/details/AssistantDetailsCoordinator.java
@@ -6,11 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.autofill_assistant.details;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.image_fetcher.ImageFetcher;
 
 /**
@@ -26,6 +32,8 @@ public class AssistantDetailsCoordinator {
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         mView.setLayoutManager(new LinearLayoutManager(
                 context, LinearLayoutManager.VERTICAL, /* reverseLayout= */ false));
+        mView.setBackgroundResource(R.drawable.autofill_assistant_details_bg);
+        mView.addItemDecoration(new DetailsItemDecoration(context));
         AssistantDetailsAdapter adapter = new AssistantDetailsAdapter(context, imageFetcher);
         mView.setAdapter(adapter);
 
@@ -35,9 +43,52 @@ public class AssistantDetailsCoordinator {
                 adapter.setDetails(model.get(AssistantDetailsModel.DETAILS));
             }
         });
+
+        // Observe the details and make this coordinator's view visible only if the details contain
+        // at least one item. That way its margins (set by the AssistantBottomBar Coordinator) will
+        // be considered during layout only if there is at least one details to show.
+        mView.setVisibility(View.GONE);
+        model.addObserver((source, propertyKey) -> {
+            if (propertyKey == AssistantDetailsModel.DETAILS) {
+                int visibility = model.get(AssistantDetailsModel.DETAILS).size() > 0 ? View.VISIBLE
+                                                                                     : View.GONE;
+                if (mView.getVisibility() != visibility) {
+                    mView.setVisibility(visibility);
+                }
+            }
+        });
     }
 
     public RecyclerView getView() {
         return mView;
+    }
+
+    /** A divider that is drawn after each details, except the last one. */
+    private static class DetailsItemDecoration extends RecyclerView.ItemDecoration {
+        private final Drawable mDrawable;
+        private final Rect mBounds = new Rect();
+
+        public DetailsItemDecoration(Context context) {
+            mDrawable = AppCompatResources.getDrawable(
+                    context, R.drawable.autofill_assistant_details_list_divider);
+        }
+
+        @Override
+        public void onDrawOver(Canvas canvas, RecyclerView parent, RecyclerView.State state) {
+            // Note: this implementation is inspired from DividerItemDecoration#drawVertical.
+            int left = parent.getPaddingLeft();
+            int right = parent.getWidth() - parent.getPaddingRight();
+            int childCount = parent.getChildCount();
+
+            // Draw a divider after each child, except the last one.
+            for (int i = 0; i < childCount - 1; ++i) {
+                View child = parent.getChildAt(i);
+                parent.getDecoratedBoundsWithMargins(child, this.mBounds);
+                int bottom = this.mBounds.bottom + Math.round(child.getTranslationY());
+                int top = bottom - this.mDrawable.getIntrinsicHeight();
+                this.mDrawable.setBounds(left, top, right, bottom);
+                this.mDrawable.draw(canvas);
+            }
+        }
     }
 }
