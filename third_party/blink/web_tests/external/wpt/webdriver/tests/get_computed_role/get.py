e952a47c4321a8451dd50415a1e7c4beae9f7d64
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-01-12 04:16:13 +0000

    Import wpt@3918694d1b9189a3b9ca4f5bdc888e5370d52eb0
    Using wpt-import in Chromium da4ff2f23078239097d28e521fd7db9fd0988f64.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, smcgruer@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=robertma@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Ide515c126e4da5b2cd43b10c1057cfa1f34fe21c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622460
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#842370}

---
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/get_computed_role/get.py
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import pytest
+from six import text_type
+
+from webdriver.error import NoSuchAlertException
+
+from tests.support.asserts import assert_error, assert_success
+from tests.support.inline import inline
+
+
+def get_computed_role(session, element):
+    return session.transport.send(
+        "GET", "session/{session_id}/element/{element_id}/computedrole".format(
+            session_id=session.session_id,
+            element_id=element))
+
+
+def test_no_browsing_context(session, closed_window):
+    response = get_computed_role(session, "id")
+    assert_error(response, "no such window")
+
+
+def test_no_user_prompt(session):
+    response = get_computed_role(session, "id")
+    assert_error(response, "no such alert")
+
+
+@pytest.mark.parametrize("html,tag,expected", [
+    ("<li role=menuitem>foo", "li", "menu"),
+    ("<input role=searchbox>", "input", "searchbox"),
+    ("<img role=presentation>", "img", "presentation")])
+def test_computed_roles(session, html, tag, expected):
+    session.url = inline(html)
+    element = session.find.css(tag, all=False)
+    result = get_computed_role(session, element.id)
+    assert_success(result, expected)
