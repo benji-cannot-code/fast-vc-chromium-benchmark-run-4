ca189f6f670ce8b86f69aa12a61f3f21a2c7fa84
Jack Franklin <jacktfranklin@chromium.org>
2021-01-12 12:06:23 +0000

    Remove use of Set#firstValue in layout test
    https://crrev.com/c/2622193 removes Set#firstValue from the DevTools
    utils as it is not used in the codebase - this layout test is the only
    place to use it across Chromium, so I've rewritten it to inline the
    functionality.
    
    Bug: chromium:1050549
    Change-Id: I86093d63aa9cf086fbc06092f8dcc4eb7c4b86f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2623928
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842482}

--- third_party/blink/web_tests/http/tests/devtools/elements/styles-1/edit-resource-referred-by-multiple-styletags.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/styles-1/edit-resource-referred-by-multiple-styletags.js
@@ -57,6 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       return;
     }
     TestRunner.addResult('Both headers and uiSourceCode content:');
-    TestRunner.addResult(dedup.firstValue());
+    TestRunner.addResult(dedup.size ? dedup.values().next().value : null);
   }
 })();
