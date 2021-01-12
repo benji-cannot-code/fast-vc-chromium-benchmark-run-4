7c64a20c75bb5ad35b2a68d7ee806084acaff54f
Marc Treib <treib@chromium.org>
2021-01-12 07:48:20 +0000

    Fix about:flags/#password-change-in-settings in developer builds
    Before this CL, the "field trial name" specified for the
    "PasswordChangeInSettings" feature was different between about_flags.cc
    and the corresponding entry in fieldtrial_testing_config.json. This
    caused a crash if the about:flags entry was enabled in a developer
    build.
    This CL changes about_flags.cc to match the fieldtrial testing config.
    
    Bug: 1108692
    Change-Id: I21112af815aa8e6bca361c1e9cafd17aceeae5ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2599805
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Maxim Kolosovskiy  <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842424}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -6265,7 +6265,7 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_WITH_PARAMS_VALUE_TYPE(
          password_manager::features::kPasswordChangeInSettings,
          kPasswordChangeInSettingsFeatureVariations,
-         "PasswordChangeInSettingsFeatureVariations")},
+         "PasswordChangeInSettings")},
     {"password-scripts-fetching",
      flag_descriptions::kPasswordScriptsFetchingName,
      flag_descriptions::kPasswordScriptsFetchingDescription, kOsAndroid,
