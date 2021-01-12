31d0146fdc0332e4afb095003a84c184bcf276d0
cfredric <cfredric@chromium.org>
2021-01-12 05:56:54 +0000

    Ignore SameParty attribute on cookies whose sites are not in an FPS.
    This brings the implementation into alignment with the final paragraph
    of
    https://github.com/cfredric/sameparty#sameparty-vs-samesite-enforcement-modes,
    which states that if a site is not a member of a First-Party Set, its
    cookies will be subject to SameSite rules, rather than SameParty rules.
    
    Bug: 1143756
    Change-Id: Ie4c9066c874d9e7a42a6d8504f33d4b6e669ff7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613194
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842394}

--- net/cookies/cookie_options.cc
+++ net/cookies/cookie_options.cc
@@ -67,6 +67,7 @@ CookieOptions CookieOptions::MakeAllInclusive() {
   options.set_do_not_update_access_time();
   options.set_same_party_cookie_context_type(
       SamePartyCookieContextType::kSameParty);
+  options.set_is_in_nontrivial_first_party_set(true);
   return options;
 }
 
