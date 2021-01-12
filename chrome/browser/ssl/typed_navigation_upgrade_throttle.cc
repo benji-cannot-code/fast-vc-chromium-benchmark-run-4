c382e1975334a83970ae2c1120a3cc6cdf52d0ff
Mustafa Emre Acer <meacer@chromium.org>
2021-01-12 08:08:35 +0000

    Default typed omnibox navigations to https: Add a UI switch
    This CL adds a switch to chrome://flags to turn the omnibox experiment
    on with 3 and 10 second fallback timeouts.
    
    Bug: 1141691
    Change-Id: I812e06b29f14829d9cf40ac45e5b82935a0a292d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2620462
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842426}

--- chrome/browser/ssl/typed_navigation_upgrade_throttle.cc
+++ chrome/browser/ssl/typed_navigation_upgrade_throttle.cc
@@ -26,13 +26,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Delay in milliseconds before falling back to the HTTP URL.
+// Delay before falling back to the HTTP URL.
 // This can be changed in tests.
 // - If the HTTPS load finishes successfully during this time, the timer is
 //   cleared and no more work is done.
 // - Otherwise, a new navigation to the the fallback HTTP URL is started.
 constexpr base::FeatureParam<base::TimeDelta> kFallbackDelay{
-    &omnibox::kDefaultTypedNavigationsToHttps, "timeout",
+    &omnibox::kDefaultTypedNavigationsToHttps,
+    omnibox::kDefaultTypedNavigationsToHttpsTimeoutParam,
     base::TimeDelta::FromSeconds(3)};
 
 bool IsNavigationUsingHttpsAsDefaultScheme(content::NavigationHandle* handle) {
