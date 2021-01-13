200750971e130967616dece33e62865b3e7616ed
Eugene But <eugenebut@chromium.org>
2021-01-13 17:51:34 +0000

    [ios] Add crashed_after_app_will_terminate synthetic crash reports flag
    According to Stability.iOS.UTE.MobileSessionAppWillTerminateWasReceived
    UMA it's not uncommon to have a UTE after appWillTerminate callback.
    
    Bug: None
    Change-Id: I20b9e581b8dadb11d2b3e8b7f1c770f5ae1df650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627049
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843072}

--- components/previous_session_info/previous_session_info_private.h
+++ components/previous_session_info/previous_session_info_private.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, assign) NSInteger memoryFootprint;
 @property(nonatomic, assign) NSInteger tabCount;
 @property(nonatomic, assign) NSInteger OTRTabCount;
+@property(nonatomic, assign) BOOL applicationWillTerminateWasReceived;
 
 + (void)resetSharedInstanceForTesting;
 
