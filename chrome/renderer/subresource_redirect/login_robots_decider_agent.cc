a9bd519a9a3cd7d085465d0edf839cdc06beca38
rajendrant <rajendrant@chromium.org>
2021-01-13 01:54:28 +0000

    Add browser test for canceled image request
    When image request is cancelled the robots rules request should be
    invalidated correctly. This CL adds a test.
    
    This CL also creates component for subresource_redirect and moves some
    utility functions there. Subsequent CLs could move more independent code
    out of chrome/renderer/subresource_redirect to the component.
    
    Bug: 1160424
    Change-Id: I29b20fe4f7057a1e8778995ea5e1114e36843da8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2619421
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842806}

--- chrome/renderer/subresource_redirect/login_robots_decider_agent.cc
+++ chrome/renderer/subresource_redirect/login_robots_decider_agent.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/subresource_redirect/robots_rules_parser.h"
 #include "chrome/renderer/subresource_redirect/robots_rules_parser_cache.h"
 #include "chrome/renderer/subresource_redirect/subresource_redirect_params.h"
+#include "components/subresource_redirect/common/subresource_redirect_features.h"
 #include "content/public/renderer/render_frame.h"
 
 namespace subresource_redirect {
@@ -53,7 +54,7 @@ LoginRobotsDeciderAgent::LoginRobotsDeciderAgent(
     blink::AssociatedInterfaceRegistry* associated_interfaces,
     content::RenderFrame* render_frame)
     : PublicResourceDeciderAgent(associated_interfaces, render_frame) {
-  DCHECK(IsLoginRobotsCheckedCompressionEnabled());
+  DCHECK(ShouldEnableLoginRobotsCheckedCompression());
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 }
 
@@ -137,7 +138,7 @@ void LoginRobotsDeciderAgent::SetCompressPublicImagesHints(
     mojom::CompressPublicImagesHintsPtr images_hints) {
   // This mojo from browser process should not be called for robots rules based
   // subresource compression on non logged-in pages.
-  DCHECK(IsLoginRobotsCheckedCompressionEnabled());
+  DCHECK(ShouldEnableLoginRobotsCheckedCompression());
   NOTREACHED();
 }
 
---
+++ components/subresource_redirect/common/subresource_redirect_features.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SUBRESOURCE_REDIRECT_COMMON_SUBRESOURCE_REDIRECT_FEATURES_H_
+#define COMPONENTS_SUBRESOURCE_REDIRECT_COMMON_SUBRESOURCE_REDIRECT_FEATURES_H_
+
+namespace subresource_redirect {
+
+// Returns if the public image hints based subresource compression is enabled.
+bool ShouldEnablePublicImageHintsBasedCompression();
+
+// Returns if the login and robots checks based subresource compression is
+// enabled. This compresses non logged-in pages and subresources allowed by
+// robots.txt rules.
+bool ShouldEnableLoginRobotsCheckedCompression();
+
+// Should the subresource be redirected to its compressed version. This returns
+// false if only coverage metrics need to be recorded and actual redirection
+// should not happen.
+bool ShouldCompressRedirectSubresource();
+
+}  // namespace subresource_redirect
+
+#endif  // COMPONENTS_SUBRESOURCE_REDIRECT_COMMON_SUBRESOURCE_REDIRECT_FEATURES_H_
