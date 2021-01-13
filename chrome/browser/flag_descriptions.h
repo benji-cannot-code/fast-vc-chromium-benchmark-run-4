a30d374065d7336ed3cd25c6602e9be5461ea1a2
Katie Dektar <katydek@google.com>
2021-01-13 00:39:40 +0000

    Remove cursor color feature flag.
    This feature was launched in M87.
    
    AX-Relnotes: N/A
    Bug: 1101378
    Change-Id: I0941fdde4d798cfba87d5425a6c7ad253b607efb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2623409
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842799}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2465,9 +2465,6 @@ extern const char kExperimentalAccessibilitySwitchAccessTextDescription[];
 extern const char kSwitchAccessPointScanningName[];
 extern const char kSwitchAccessPointScanningDescription[];
 
-extern const char kExperimentalAccessibilityCursorColorsName[];
-extern const char kExperimentalAccessibilityCursorColorsDescription[];
-
 extern const char kMagnifierNewFocusFollowingName[];
 extern const char kMagnifierNewFocusFollowingDescription[];
 
--- chrome/browser/resources/settings/chromeos/os_a11y_page/manage_a11y_page.js
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/manage_a11y_page.js
@@ -151,15 +151,6 @@ Polymer({
     },
 
     /** @private */
-    shouldShowExperimentalCursorColor_: {
-      type: Boolean,
-      value() {
-        return loadTimeData.getBoolean(
-            'showExperimentalAccessibilityCursorColor');
-      },
-    },
-
-    /** @private */
     isMagnifierPanningImprovementsEnabled_: {
       type: Boolean,
       value() {
