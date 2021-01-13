23b0248975467dc40e108038fdd76c1b2ef18e10
David Black <dmblack@google.com>
2021-01-13 05:38:14 +0000

    Fix holding space in guest sessions.
    Holding space is enabled for guest sessions but previously redirected to
    use original profile instead of OTR profile. This caused:
    
    - Thumbnails to fail to render
    - Opening Files app via util to fail
    
    These failures are due to expectations further downstream that guest
    sessions use OTR profiles.
    
    To address this:
    - Redirect to primary OTR profile in holding space for guest sessions
    - Allow OTR profiles in FileChangeService for guest sessions
    
    Note that FileChangeService was previously *not* created for OTR
    profiles but they will now be allowed if the profile is a guest session.
    
    This CL also modified FileChangeService to *not* create the service w/
    the browser since the service does not need to exist until interacted
    with.
    
    Bug: 1165733, 1164329
    Change-Id: I032aea25171594bdb0666c87564a80f7c0cfe203
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2625799
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Sergei Datsenko <dats@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842893}

--- chrome/browser/chromeos/fileapi/file_change_service_factory.cc
+++ chrome/browser/chromeos/fileapi/file_change_service_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/fileapi/file_change_service_factory.h"
 
 #include "chrome/browser/chromeos/fileapi/file_change_service.h"
+#include "chrome/browser/profiles/profile.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 
 namespace chromeos {
@@ -29,13 +30,24 @@ FileChangeServiceFactory::FileChangeServiceFactory()
 
 FileChangeServiceFactory::~FileChangeServiceFactory() = default;
 
-KeyedService* FileChangeServiceFactory::BuildServiceInstanceFor(
+content::BrowserContext* FileChangeServiceFactory::GetBrowserContextToUse(
     content::BrowserContext* context) const {
-  return new FileChangeService();
+  Profile* const profile = Profile::FromBrowserContext(context);
+
+  // Guest sessions are supported and guest OTR profiles are allowed.
+  if (profile->IsGuestSession())
+    return profile;
+
+  // Don't create the service for OTR profiles outside of guest sessions.
+  return profile->IsOffTheRecord() ? nullptr : profile;
 }
 
-bool FileChangeServiceFactory::ServiceIsCreatedWithBrowserContext() const {
-  return true;
+KeyedService* FileChangeServiceFactory::BuildServiceInstanceFor(
+    content::BrowserContext* context) const {
+  Profile* const profile = Profile::FromBrowserContext(context);
+  if (profile->IsOffTheRecord())
+    CHECK(profile->IsGuestSession());
+  return new FileChangeService();
 }
 
 }  // namespace chromeos
