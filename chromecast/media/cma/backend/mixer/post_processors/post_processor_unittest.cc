2cccb303759968020bf7b520812a6c272f770cbb
Ken MacKay <kmackay@chromium.org>
2021-01-13 20:00:09 +0000

    [Chromecast] Use appropriate epsilon for audio unittests
    The sum of the values being compared can be as large as 2.0, so epsilon
    should be scaled accordingly.
    
    Bug: internal b/177118785
    Change-Id: I6ed39219dddfc5226383e4664937bebc18dcdaed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626925
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Auto-Submit: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843153}

--- chromecast/media/cma/backend/mixer/post_processors/post_processor_unittest.cc
+++ chromecast/media/cma/backend/mixer/post_processors/post_processor_unittest.cc
@@ -22,7 +22,7 @@ namespace post_processor_test {
 
 namespace {
 
-const float kEpsilon = std::numeric_limits<float>::epsilon();
+const float kEpsilon = std::numeric_limits<float>::epsilon() * 2;
 
 // Benchmark parameters.
 const float kTestDurationSec = 1.0;
