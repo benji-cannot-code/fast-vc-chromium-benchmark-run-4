593a62b58b65d885ee39ce8e7b2b12117808c6b3
Vasiliy Telezhnikov <vasilyt@chromium.org>
2021-01-13 17:27:51 +0000

    Add CHECK for consecutive reshape failures
    We suspect that in rare cases some clients trigger some kind of loop
    failures when Reshape fails, generates context loss, but after
    reinitialization it fails again.
    
    This CL adds CHECK for this case to crash and get more info from crash
    reports.
    
    Similar logic that was implemented in SkiaOutputDeviceGL earlier in
    https://crrev.com/c/2416749
    
    Change-Id: Ifa1dca0ee19423f5ba532644ec45cd373e712b47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626771
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843065}

--- components/viz/service/display_embedder/skia_output_device_buffer_queue.cc
+++ components/viz/service/display_embedder/skia_output_device_buffer_queue.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/command_line.h"
+#include "base/debug/alias.h"
 #include "base/feature_list.h"
 #include "build/build_config.h"
 #include "components/viz/common/switches.h"
@@ -24,6 +25,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkSurfaceProps.h"
 #include "ui/gfx/swap_result.h"
 #include "ui/gl/gl_surface.h"
+
+namespace {
+base::TimeTicks g_last_reshape_failure = base::TimeTicks();
+
+NOINLINE void CheckForLoopFailuresBufferQueue() {
+  const auto threshold = base::TimeDelta::FromSeconds(1);
+  auto now = base::TimeTicks::Now();
+  if (!g_last_reshape_failure.is_null() &&
+      now - g_last_reshape_failure > threshold) {
+    CHECK(false);
+  }
+  g_last_reshape_failure = now;
+}
+
+}  // namespace
 
 namespace viz {
 
@@ -481,7 +497,10 @@ bool SkiaOutputDeviceBufferQueue::Reshape(const gfx::Size& size,
   DCHECK(pending_overlay_mailboxes_.empty());
   if (!presenter_->Reshape(size, device_scale_factor, color_space, format,
                            transform)) {
-    DLOG(ERROR) << "Failed to resize.";
+    LOG(ERROR) << "Failed to resize.";
+    CheckForLoopFailuresBufferQueue();
+    // To prevent tail call, so we can see the stack.
+    base::debug::Alias(nullptr);
     return false;
   }
 
@@ -495,7 +514,13 @@ bool SkiaOutputDeviceBufferQueue::Reshape(const gfx::Size& size,
     background_image_is_scheduled_ = false;
   }
 
-  return RecreateImages();
+  bool success = RecreateImages();
+  if (!success) {
+    CheckForLoopFailuresBufferQueue();
+    // To prevent tail call, so we can see the stack.
+    base::debug::Alias(nullptr);
+  }
+  return success;
 }
 
 bool SkiaOutputDeviceBufferQueue::RecreateImages() {
