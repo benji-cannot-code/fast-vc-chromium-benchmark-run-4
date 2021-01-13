c4e261762daec13c77588987213ccac54766f5bd
Oriol Brufau <obrufau@igalia.com>
2021-01-13 17:27:10 +0000

    [editing] Move caret when navigating to a fragment with caret browsing
    When navigating to #foo, the element with id="foo" is scrolled into view
    and is focused if focusable. This patch makes it receive the caret too,
    in case caret browsing is enabled.
    
    Bug: 1011934
    
    TEST=editing/caret/caret-browsing-fragment-anchor.html
    
    Change-Id: I044e6b48545426afdb0c5ee766d4ee3a218aa95b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2463308
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843064}

--- content/web_test/renderer/test_runner.cc
+++ content/web_test/renderer/test_runner.cc
@@ -280,6 +280,7 @@ class TestRunnerBindings : public gin::Wrappable<TestRunnerBindings> {
                                v8::Local<v8::Function> callback);
   void SetBluetoothManualChooser(bool enable);
   void SetCanOpenWindows();
+  void SetCaretBrowsingEnabled();
   void SetColorProfile(const std::string& name,
                        v8::Local<v8::Function> callback);
   void SetCustomPolicyDelegate(gin::Arguments* args);
@@ -670,6 +671,8 @@ gin::ObjectTemplateBuilder TestRunnerBindings::GetObjectTemplateBuilder(
                  &TestRunnerBindings::SetBluetoothManualChooser)
       .SetMethod("setCallCloseOnWebViews", &TestRunnerBindings::NotImplemented)
       .SetMethod("setCanOpenWindows", &TestRunnerBindings::SetCanOpenWindows)
+      .SetMethod("setCaretBrowsingEnabled",
+                 &TestRunnerBindings::SetCaretBrowsingEnabled)
       .SetMethod("setColorProfile", &TestRunnerBindings::SetColorProfile)
       .SetMethod("setCustomPolicyDelegate",
                  &TestRunnerBindings::SetCustomPolicyDelegate)
@@ -1469,6 +1472,13 @@ void TestRunnerBindings::SetCanOpenWindows() {
   if (invalid_)
     return;
   runner_->SetCanOpenWindows();
+}
+
+void TestRunnerBindings::SetCaretBrowsingEnabled() {
+  if (invalid_)
+    return;
+  blink::WebView* web_view = GetWebFrame()->View();
+  web_view->GetSettings()->SetCaretBrowsingEnabled(true);
 }
 
 void TestRunnerBindings::SetImagesAllowed(bool allowed) {
@@ -2302,6 +2312,7 @@ void TestRunner::ResetWebView(WebViewTestProxy* web_view_test_proxy) {
 
   web_view->SetTabKeyCyclesThroughElements(true);
   web_view->GetSettings()->SetHighlightAds(false);
+  web_view->GetSettings()->SetCaretBrowsingEnabled(false);
   web_view->DisableAutoResizeForTesting(gfx::Size());
   web_view->SetScreenOrientationOverrideForTesting(
       fake_screen_orientation_impl_.CurrentOrientationType());
--- third_party/blink/renderer/core/page/scrolling/element_fragment_anchor.cc
+++ third_party/blink/renderer/core/page/scrolling/element_fragment_anchor.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/dom/node.h"
+#include "third_party/blink/renderer/core/editing/frame_selection.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/svg/svg_svg_element.h"
@@ -175,11 +176,27 @@ void ElementFragmentAnchor::ApplyFocusIfNeeded() {
   if (!anchor_node_)
     return;
 
+  frame_->GetDocument()->UpdateStyleAndLayoutTree();
+
+  // If caret browsing is enabled, move the caret to the beginning of the
+  // fragment, or to the first non-inert position after it.
+  if (frame_->IsCaretBrowsingEnabled()) {
+    const Position& pos = Position::FirstPositionInOrBeforeNode(*anchor_node_);
+    if (pos.IsConnected()) {
+      frame_->Selection().SetSelection(
+          SelectionInDOMTree::Builder().Collapse(pos).Build(),
+          SetSelectionOptions::Builder()
+              .SetShouldCloseTyping(true)
+              .SetShouldClearTypingStyle(true)
+              .SetDoNotSetFocus(true)
+              .Build());
+    }
+  }
+
   // If the anchor accepts keyboard focus and fragment scrolling is allowed,
   // move focus there to aid users relying on keyboard navigation.
   // If anchorNode is not focusable or fragment scrolling is not allowed,
   // clear focus, which matches the behavior of other browsers.
-  frame_->GetDocument()->UpdateStyleAndLayoutTree();
   auto* element = DynamicTo<Element>(anchor_node_.Get());
   if (element && element->IsFocusable()) {
     element->focus();
