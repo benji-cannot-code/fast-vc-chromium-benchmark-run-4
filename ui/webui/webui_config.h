e3baca930729864d6bf9bba9ff4a2acce54cb3b5
Stephan Hartmann <stha09@googlemail.com>
2021-01-13 02:18:05 +0000

    IWYU: add missing include for std::unique_ptr
    Change-Id: I70cedfa4d20b09192ba749a0866ac531aeaf0db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2601365
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842841}

--- ui/webui/webui_config.h
+++ ui/webui/webui_config.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_WEBUI_WEBUI_CONFIG_H_
 #define UI_WEBUI_WEBUI_CONFIG_H_
 
+#include <memory>
 #include <string>
 
 #include "base/strings/string_piece.h"
