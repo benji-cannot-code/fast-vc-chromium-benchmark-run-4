05e9897d000b267a2d93a34e976f5286814bf925
Rune Lillesveen <futhark@chromium.org>
2021-01-13 13:36:14 +0000

    Container Queries: Update pseudo elements
    Allow traversal into and update pseudo elements affected by container
    queries.
    
    Bug: 1146097
    Change-Id: I41916286f20bc038089f2f3420d568259a817f62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2624631
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842990}

--- third_party/blink/renderer/core/css/style_recalc.cc
+++ third_party/blink/renderer/core/css/style_recalc.cc
@@ -16,7 +16,8 @@ bool StyleRecalcChange::TraverseChildren(const Element& element) const {
 }
 
 bool StyleRecalcChange::TraversePseudoElements(const Element& element) const {
-  return UpdatePseudoElements() || element.ChildNeedsStyleRecalc();
+  return UpdatePseudoElements() || RecalcContainerQueryDependent() ||
+         element.ChildNeedsStyleRecalc();
 }
 
 bool StyleRecalcChange::TraverseChild(const Node& node) const {
@@ -48,7 +49,12 @@ bool StyleRecalcChange::ShouldRecalcStyleFor(const Node& node) const {
 
 bool StyleRecalcChange::ShouldUpdatePseudoElement(
     const PseudoElement& pseudo_element) const {
-  return UpdatePseudoElements() || pseudo_element.NeedsStyleRecalc();
+  if (UpdatePseudoElements())
+    return true;
+  if (pseudo_element.NeedsStyleRecalc())
+    return true;
+  return RecalcContainerQueryDependent() &&
+         pseudo_element.ComputedStyleRef().DependsOnContainerQueries();
 }
 
 bool StyleRecalcChange::RecalcContainerQueryDependentChildren(
