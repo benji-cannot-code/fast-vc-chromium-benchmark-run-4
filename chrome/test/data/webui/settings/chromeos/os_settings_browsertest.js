3e86d620f025953ec3e8c471dc70a348e3012ff9
Maxim Kolosovskiy <kolos@chromium.org>
2021-01-13 10:14:12 +0000

    Revert "[CrOS cellular] Respond to cellular route params in internet page"
    This reverts commit 9ca977e31508c0d1ef4af96034f74a64a5c320ba.
    
    Reason for revert: I suspect this CL caused a failure https://ci.chromium.org/p/chrome/builders/ci/chromeos-betty-pi-arc-chrome
    
    Two builds: 
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-pi-arc-chrome/11124/overview
    Test 'arc.DragDrop' completed with the following status(es): 'FAILURE'
    
    Test 'arc.DragDrop' had the following logs when run:
    
    ================================================================================
    
    Failed initializing UI Automator
    	at chromiumos/tast/local/bundles/cros/arc.DragDrop (drag_drop.go:93)
    	at chromiumos/tast/internal/planner.runTestWithRoot.func3 (run.go:755)
    	at chromiumos/tast/internal/planner.safeCall.func2 (safe.go:92)
    	at runtime.goexit (asm_amd64.s:1357)
    failed installing /usr/local/share/android-uiautomator-server/app-uiautomator.apk
    	at chromiumos/tast/local/android/ui.installServer (device.go:119)
    	at chromiumos/tast/local/android/ui.NewDevice (device.go:91)
    	at chromiumos/tast/local/arc.(*ARC).NewUIDevice (ui.go:16)
    	at chromiumos/tast/local/bundles/cros/arc.DragDrop (drag_drop.go:91)
    	at chromiumos/tast/internal/planner.runTestWithRoot.func3 (run.go:755)
    	at chromiumos/tast/internal/planner.safeCall.func2 (safe.go:92)
    	at runtime.goexit (asm_amd64.s:1357)
    context deadline exceeded
    	at ???
    
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-pi-arc-chrome/11125/overview
    Test 'chrome_all_tast_tests' completed with the following status(es): 'FAILURE'
    
    Test 'chrome_all_tast_tests' had the following logs when run:
    
    
    
    Original change's description:
    > [CrOS cellular] Respond to cellular route params in internet page
    >
    > Bug: 1163556
    > Change-Id: I196b9c9f73fecb169b53ac3d3ed5635a799b7138
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2625275
    > Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    > Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#842903}
    
    TBR=azeemarshad@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,tjohnsonkanu@google.com
    
    Change-Id: Icd511320cc5b1df0ad54057e372b4d353fc90778
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1163556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626432
    Reviewed-by: Maxim Kolosovskiy  <kolos@chromium.org>
    Commit-Queue: Maxim Kolosovskiy  <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842952}

--- chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
@@ -972,12 +972,7 @@ var OSSettingsInternetPageTest = class extends OSSettingsBrowserTest {
 
   /** @override */
   get featureList() {
-    return {
-      enabled: [
-        'chromeos::features::kOsSettingsDeepLinking',
-        'chromeos::features::kUpdatedCellularActivationUi',
-      ]
-    };
+    return {enabled: ['chromeos::features::kOsSettingsDeepLinking']};
   }
 
   /** @override */
