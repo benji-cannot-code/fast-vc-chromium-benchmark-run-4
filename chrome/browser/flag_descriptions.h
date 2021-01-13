1d87e21e4a5344aa76ce083079dd7846258e116a
Rachel Wong <wrong@chromium.org>
2021-01-13 23:04:07 +0000

    [suggested-content] Remove Chrome flag.
    The suggested content toggle is launched and enabled by default, so we
    can remove its flag.
    
    Bug: 1166057
    Change-Id: I4bbbbf903ddb954a084b1c8e639888a3970c31e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626812
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843221}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1298,9 +1298,6 @@ extern const char kStylusBatteryStatusDescription[];
 extern const char kSystemKeyboardLockName[];
 extern const char kSystemKeyboardLockDescription[];
 
-extern const char kSuggestedContentToggleName[];
-extern const char kSuggestedContentToggleDescription[];
-
 extern const char kSuggestionsWithSubStringMatchName[];
 extern const char kSuggestionsWithSubStringMatchDescription[];
 
