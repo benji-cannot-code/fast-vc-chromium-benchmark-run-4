095f3de51f082b301dc0e8b33464e28ba8c8b629
Adam Langley <agl@chromium.org>
2021-01-13 02:24:11 +0000

    Drop custom P-224 implementation.
    This dates back to when we still used NSS(!) and thus didn't have access
    to group operations for any elliptic curves. We can just use BoringSSL
    now.
    
    Change-Id: I8bc4bc09fdd26feb25a76c17bf716808eb1cb2a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2623123
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#842843}

--- crypto/p224_spake.h
+++ crypto/p224_spake.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/strings/string_piece.h"
-#include "crypto/p224.h"
 #include "crypto/sha2.h"
 
 namespace crypto {
@@ -111,12 +110,14 @@ class CRYPTO_EXPORT P224EncryptedKeyExchange {
                      const std::string& k,
                      uint8_t* out_digest);
 
+  // kScalarBytes is the number of bytes in a P-224 scalar.
+  static constexpr size_t kScalarBytes = 28;
   // x_ is the secret Diffie-Hellman exponent (see paper referenced in .cc
   // file).
-  uint8_t x_[p224::kScalarBytes];
+  uint8_t x_[kScalarBytes];
   // pw_ is SHA256(P(password), P(session))[:28] where P() prepends a uint32_t,
   // big-endian length prefix (see paper referenced in .cc file).
-  uint8_t pw_[p224::kScalarBytes];
+  uint8_t pw_[kScalarBytes];
   // expected_authenticator_ is used to store the hash value expected from the
   // other party.
   uint8_t expected_authenticator_[kSHA256Length];
