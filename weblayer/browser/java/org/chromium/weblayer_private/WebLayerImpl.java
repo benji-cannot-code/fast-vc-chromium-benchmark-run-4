530b838989d70eb143e6a7b684a3be085a85d74f
Clark DuVall <cduvall@chromium.org>
2021-01-14 02:48:55 +0000

    [WebLayer] Warm up WebLayer renderer process on startup
    Chrome, WebView, and CCT all do this, so I think it makes sense for
    WebLayer to do it as well.
    
    Pinpoint run shows time is moved from load to earlier startup (as
    expected) but this is probably a bigger benefit in the field where
    renderer processes can take awhile to start.
    https://pinpoint-dot-chromeperf.appspot.com/job/13506a06d20000
    
    Bug: 1146438
    Change-Id: I6f04a04f5b08a21beea9f0295fa9db3ef30c923f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628032
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843345}

--- weblayer/browser/java/org/chromium/weblayer_private/WebLayerImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/WebLayerImpl.java
@@ -61,6 +61,7 @@ import org.chromium.components.embedder_support.application.FirebaseConfig;
 import org.chromium.components.embedder_support.util.Origin;
 import org.chromium.content_public.browser.BrowserStartupController;
 import org.chromium.content_public.browser.ChildProcessCreationParams;
+import org.chromium.content_public.browser.ChildProcessLauncherHelper;
 import org.chromium.content_public.browser.ContactsPicker;
 import org.chromium.content_public.browser.ContactsPickerListener;
 import org.chromium.content_public.browser.DeviceUtils;
@@ -277,6 +278,7 @@ public final class WebLayerImpl extends IWebLayer.Stub {
         BundleUtils.setIsBundle(ProductConfig.IS_BUNDLE);
 
         setChildProcessCreationParams(appContext, packageInfo.packageName);
+        ChildProcessLauncherHelper.warmUp(appContext, true);
 
         // Creating the Android shared preferences object causes I/O.
         try (StrictModeContext ignored = StrictModeContext.allowDiskWrites()) {
