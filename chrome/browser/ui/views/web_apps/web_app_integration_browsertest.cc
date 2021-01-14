aaa45e6c5a9a6760db75221b137db30ccf5cedd5
Jarryd <jarrydg@chromium.org>
2021-01-14 21:12:08 +0000

    dPWA: Implement testing journey 5.
     * Add test case for journey 5
     * Implement |add_policy_app_internal_windowed| testing action
     * Implement |assert_display_mode_browser_internal| testing action
     * Implement |assert_display_mode_standalone_internal| testing action
     * Add getter for WebAppProvider
    
    Bug: 1145224, 1156676, 1156708, 1156706
    Change-Id: I64f2ce560a78dfb9973cca21d9e21c28dcddf40a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2610045
    Commit-Queue: Jarryd Goodman <jarrydg@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843730}

--- chrome/browser/ui/views/web_apps/web_app_integration_browsertest.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_browsertest.cc
@@ -226,7 +226,9 @@ class WebAppIntegrationBrowserTest
   // alphabetical order.
   void ExecuteAction(const std::string& action_string) {
     if (action_string == "add_policy_app_internal_tabbed") {
-      AddPolicyAppInternalTabbed();
+      AddPolicyAppInternal(base::Value(kDefaultLaunchContainerTabValue));
+    } else if (action_string == "add_policy_app_internal_windowed") {
+      AddPolicyAppInternal(base::Value(kDefaultLaunchContainerWindowValue));
     } else if (action_string == "close_pwa") {
       ClosePWA();
     } else if (action_string == "install_create_shortcut_tabbed") {
@@ -257,6 +259,10 @@ class WebAppIntegrationBrowserTest
       AssertAppInListNotWindowed();
     } else if (action_string == "assert_app_not_in_list") {
       AssertAppNotInList();
+    } else if (action_string == "assert_display_mode_standalone_internal") {
+      AssertDisplayModeStandaloneInternal();
+    } else if (action_string == "assert_display_mode_browser_internal") {
+      AssertDisplayModeBrowserInternal();
     } else if (action_string == "assert_installable") {
       AssertInstallable();
     } else if (action_string == "assert_install_icon_shown") {
@@ -278,7 +284,7 @@ class WebAppIntegrationBrowserTest
   }
 
   // Automated Testing Actions
-  void AddPolicyAppInternalTabbed() {
+  void AddPolicyAppInternal(base::Value default_launch_container) {
     GURL url = GetInstallableAppURL();
     auto* web_app_registrar =
         WebAppProvider::Get(profile())->registrar().AsWebAppRegistrar();
@@ -298,7 +304,7 @@ class WebAppIntegrationBrowserTest
       base::Value item(base::Value::Type::DICTIONARY);
       item.SetKey(kUrlKey, base::Value(url.spec()));
       item.SetKey(kDefaultLaunchContainerKey,
-                  base::Value(kDefaultLaunchContainerTabValue));
+                  std::move(default_launch_container));
       ListPrefUpdate update(profile()->GetPrefs(),
                             prefs::kWebAppInstallForceList);
       update->Append(item.Clone());
@@ -347,7 +353,7 @@ class WebAppIntegrationBrowserTest
   }
 
   void LaunchInternal() {
-    auto* web_app_provider = WebAppProvider::Get(profile());
+    auto* web_app_provider = GetProvider();
     AppRegistrar& app_registrar = web_app_provider->registrar();
     DisplayMode display_mode =
         app_registrar.GetAppEffectiveDisplayMode(app_id_);
@@ -479,6 +485,16 @@ class WebAppIntegrationBrowserTest
     EXPECT_FALSE(display_mode == blink::mojom::DisplayMode::kStandalone);
   }
   void AssertAppNotInList() { EXPECT_FALSE(base::Contains(app_ids_, app_id_)); }
+
+  void AssertDisplayModeStandaloneInternal() {
+    EXPECT_EQ(GetProvider()->registrar().GetAppEffectiveDisplayMode(app_id_),
+              DisplayMode::kStandalone);
+  }
+  void AssertDisplayModeBrowserInternal() {
+    EXPECT_EQ(GetProvider()->registrar().GetAppEffectiveDisplayMode(app_id_),
+              DisplayMode::kBrowser);
+  }
+
   void AssertInstallable() { EXPECT_TRUE(last_navigation_result_.installable); }
 
   void AssertInstallIconShown() {
@@ -533,6 +549,7 @@ class WebAppIntegrationBrowserTest
 
   Profile* profile() { return browser()->profile(); }
   Browser* app_browser() { return app_browser_; }
+  WebAppProvider* GetProvider() { return WebAppProvider::Get(profile()); }
   std::vector<std::string>& testing_actions() { return testing_actions_; }
   PageActionIconView* pwa_install_view() { return pwa_install_view_; }
 
