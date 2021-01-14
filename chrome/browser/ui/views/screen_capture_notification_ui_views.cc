ee3be0ecc5df7a574400251b6e91246f43c95509
Ilya Nikolaevskiy <ilnik@chromium.org>
2021-01-14 17:36:53 +0000

    Minor UI logic changes to prevent a UAF bug when starting tab capture.
    See discussion in crbug 1155426 for details. Changes:
    
    MediaStreamCaptureIndicator::UIDelegate: Ignore multiple calls to
    OnStarted().
    
    TabSharingUIViews: Unconditionally execute clean-up tasks in destructor.
    
    MediaStreamUI and contents::MediaStreamUI: Now has SetStopCallback method which is used to remove the
    stop callback when MediaStreamUI is replaced on switching the tab source.
    
    Bug: 1155426, 1166260
    Change-Id: I0482c82f921fdc2f20931ab83458deec2f2da30c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2629278
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843598}

--- chrome/browser/ui/views/screen_capture_notification_ui_views.cc
+++ chrome/browser/ui/views/screen_capture_notification_ui_views.cc
@@ -83,6 +83,8 @@ class ScreenCaptureNotificationUIViews : public ScreenCaptureNotificationUI,
       base::OnceClosure stop_callback,
       content::MediaStreamUI::SourceCallback source_callback) override;
 
+  void SetStopCallback(base::OnceClosure stop_callback) override;
+
   // views::WidgetDelegateView:
   void DeleteDelegate() override;
   views::ClientView* CreateClientView(views::Widget* widget) override;
@@ -222,6 +224,11 @@ gfx::NativeViewId ScreenCaptureNotificationUIViews::OnStarted(
   widget->SetVisibleOnAllWorkspaces(true);
 
   return 0;
+}
+
+void ScreenCaptureNotificationUIViews::SetStopCallback(
+    base::OnceClosure stop_callback) {
+  stop_callback_ = std::move(stop_callback);
 }
 
 void ScreenCaptureNotificationUIViews::DeleteDelegate() {
