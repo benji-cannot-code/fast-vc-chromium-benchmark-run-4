eb1c53e80f9fad33b54c634f3fc129068b5a7352
Vidhan <vidhanj@google.com>
2021-01-14 10:35:48 +0000

    [Autofill] Removed AutofillDataModel::ShouldSkipFillingOrSuggesting
    AutofillDataModel::ShouldSkipFillingOrSuggesting always returns false
    for the CreditCard whereas AutofillProfile::ShouldSkipFillingOrSuggesting
    overrides it.
    
    With this CL, ShouldSkipFillingOrSuggesting is now a member of only
    AutofillProfile and never called for CreditCard.
    
    Change-Id: Ie86f5cac7eb7f46f2eb8a9193208783484dd0aa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627487
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/master@{#843474}

--- components/autofill/core/browser/data_model/autofill_data_model.cc
+++ components/autofill/core/browser/data_model/autofill_data_model.cc
@@ -88,9 +88,4 @@ AutofillDataModel::ValidityState AutofillDataModel::GetValidityState(
   return AutofillDataModel::UNSUPPORTED;
 }
 
-bool AutofillDataModel::ShouldSkipFillingOrSuggesting(
-    ServerFieldType type) const {
-  return false;
-}
-
 }  // namespace autofill
