a96d79bcaa3f8b026bf3e5f8bd04dc404d46a7b2
Etienne Bergeron <etienneb@chromium.org>
2021-01-14 16:47:54 +0000

    Move UpdateEngagedSites(...) to worker pool
    This CL is moving the computation of engaged sites to the worker pool.
    
    Move the worker model to a single static function to avoid any
    incorrect fields accesses. The worker compute the sites and
    pass it to the main thread.
    
    The callbacks are queued and executed when at the completion of the
    background computation.
    
    Bug: 1157596
    Change-Id: I4021d87fde51d673f78154b6cd5fc94fa71356d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627758
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843572}

--- chrome/browser/lookalikes/lookalike_url_service.h
+++ chrome/browser/lookalikes/lookalike_url_service.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/lookalikes/core/lookalike_url_util.h"
-#include "components/url_formatter/url_formatter.h"
 
 class Profile;
 
@@ -55,18 +54,17 @@ class LookalikeUrlService : public KeyedService {
   void SetClockForTesting(base::Clock* clock);
 
  private:
-  void UpdateEngagedSitesInBackground(
-      scoped_refptr<HostContentSettingsMap> map);
+  void OnUpdateEngagedSitesCompleted(std::vector<DomainInfo> new_engaged_sites);
 
   Profile* profile_;
   base::Clock* clock_;
   base::Time last_engagement_fetch_time_;
-  std::vector<DomainInfo> engaged_sites_;
+  std::vector<DomainInfo> engaged_sites_ GUARDED_BY_CONTEXT(sequence_checker_);
 
   // Indicates that an update to the engaged sites list has been queued. Serves
   // to prevent enqueuing excessive updates.
-  bool update_in_progress_;
-  scoped_refptr<base::SequencedTaskRunner> task_runner_;
+  bool update_in_progress_ = false;
+  std::vector<EngagedSitesCallback> pending_update_complete_callbacks_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
