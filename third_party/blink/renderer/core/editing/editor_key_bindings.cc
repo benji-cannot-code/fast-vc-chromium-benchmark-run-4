0d0b6cf89afd62c4495ae048fd47630ac96c1ac1
Alex Keng <shihken@microsoft.com>
2021-01-14 17:43:35 +0000

    EditContext: enable English typing
    This CL enables English typing for EditContext by redirecting inserted
    text in Editor::HandleEditingKeyboardEvent if EditContext is active.
    
    A test is also added in edit-context.html.
    
    Change-Id: I0c63371385556869ab71652d6857d18d985260d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626528
    Reviewed-by: Anupam Snigdha <snianu@microsoft.com>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Alex Keng <shihken@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#843605}

--- third_party/blink/renderer/core/editing/editor_key_bindings.cc
+++ third_party/blink/renderer/core/editing/editor_key_bindings.cc
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/editing_behavior.h"
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/editing/frame_selection.h"
+#include "third_party/blink/renderer/core/editing/ime/edit_context.h"
+#include "third_party/blink/renderer/core/editing/ime/input_method_controller.h"
 #include "third_party/blink/renderer/core/events/keyboard_event.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
@@ -61,7 +63,20 @@ bool Editor::HandleEditingKeyboardEvent(KeyboardEvent* evt) {
   if (command.Execute(evt))
     return true;
 
-  if (!Behavior().ShouldInsertCharacter(*evt) || !CanEdit())
+  if (!Behavior().ShouldInsertCharacter(*evt))
+    return false;
+
+  // If EditContext is active, redirect text to EditContext, otherwise, send
+  // text to the focused element.
+  auto* edit_context =
+      GetFrame().GetInputMethodController().GetActiveEditContext();
+  if (edit_context) {
+    WebString text(WTF::String(key_event->text));
+    edit_context->InsertText(text);
+    return true;
+  }
+
+  if (!CanEdit())
     return false;
 
   const Element* const focused_element =
