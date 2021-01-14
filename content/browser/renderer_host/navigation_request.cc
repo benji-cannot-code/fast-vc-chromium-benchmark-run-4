8da50283d529015bdfd74eda2c18d49246a6871d
Domenic Denicola <domenic@chromium.org>
2021-01-14 15:35:39 +0000

    Origin-keyed agent clusters: rename in header parser
    942aca884980a4dd6c582e3ca1a021769fd18f36 switched us from parsing the
    Origin-Isolation header to parsing the Origin-Agent-Cluster header.
    However, it left the file/function/field names unchanged. This followup
    aligns them with the new name.
    
    Bug: 1158853
    Change-Id: I1d4c931c9480ba9bd1d952112f7013555abe65d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617466
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843541}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -2099,7 +2099,7 @@ bool NavigationRequest::IsOptInIsolationRequested() {
     return false;
 
   if (base::FeatureList::IsEnabled(features::kOriginIsolationHeader) &&
-      response_head_->parsed_headers->origin_isolation) {
+      response_head_->parsed_headers->origin_agent_cluster) {
     return true;
   }
 
---
+++ services/network/public/cpp/origin_agent_cluster_parser_unittest.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/network/public/cpp/origin_agent_cluster_parser.h"
+
+#include <string>
+#include <vector>
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace network {
+
+TEST(OriginAgentClusterHeaderTest, Parse) {
+  EXPECT_EQ(ParseOriginAgentCluster(""), false);
+
+  EXPECT_EQ(ParseOriginAgentCluster("?1"), true);
+  EXPECT_EQ(ParseOriginAgentCluster("?0"), false);
+
+  EXPECT_EQ(ParseOriginAgentCluster("?1;param"), true);
+  EXPECT_EQ(ParseOriginAgentCluster("?1;param=value"), true);
+  EXPECT_EQ(ParseOriginAgentCluster("?1;param=value;param2=value2"), true);
+
+  EXPECT_EQ(ParseOriginAgentCluster("true"), false);
+  EXPECT_EQ(ParseOriginAgentCluster("\"?1\""), false);
+  EXPECT_EQ(ParseOriginAgentCluster("1"), false);
+  EXPECT_EQ(ParseOriginAgentCluster("?2"), false);
+  EXPECT_EQ(ParseOriginAgentCluster("(?1)"), false);
+}
+
+}  // namespace network
