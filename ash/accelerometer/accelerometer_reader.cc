56b6f396e45e2a4afd9fc42d8887fdd688a5886b
Harvey Yang <chenghaoyang@chromium.org>
2021-01-14 06:30:13 +0000

    accelerometer: USE USE_IIOSERVICE build flag
    BUG=b:168434557
    TEST=unit tests
    
    Change-Id: I896495d19cc5c8ebdc3a696e537b18302644d389
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2624128
    Commit-Queue: Cheng-Hao Yang <chenghaoyang@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843415}

--- ash/accelerometer/accelerometer_reader.cc
+++ ash/accelerometer/accelerometer_reader.cc
@@ -5,26 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/accelerometer/accelerometer_reader.h"
 
-#include <grp.h>
-
-#include "ash/accelerometer/accelerometer_file_reader.h"
-#include "ash/accelerometer/accelerometer_provider_mojo.h"
 #include "ash/shell.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/no_destructor.h"
-#include "base/posix/eintr_wrapper.h"
 #include "base/sequenced_task_runner.h"
 #include "base/task/current_thread.h"
 #include "base/threading/sequenced_task_runner_handle.h"
+#include "chromeos/components/sensors/buildflags.h"
+#if BUILDFLAG(USE_IIOSERVICE)
+#include "ash/accelerometer/accelerometer_provider_mojo.h"
+#else  // !BUILDFLAG(USE_IIOSERVICE)
+#include "ash/accelerometer/accelerometer_file_reader.h"
+#endif  // BUILDFLAG(USE_IIOSERVICE)
 
 namespace ash {
-
-namespace {
-
-// Group name of IIO Service, used to check if IIO Service exists.
-constexpr char kIioServiceGroupName[] = "iioservice";
-
-}  // namespace
 
 // static
 AccelerometerReader* AccelerometerReader::GetInstance() {
@@ -63,17 +57,11 @@ void AccelerometerReader::SetECLidAngleDriverStatusForTesting(
 }
 
 AccelerometerReader::AccelerometerReader() {
-  char buf[1024];
-  struct group result;
-  struct group* resultp;
-
-  if (HANDLE_EINTR(getgrnam_r(kIioServiceGroupName, &result, buf, sizeof(buf),
-                              &resultp)) < 0 ||
-      !resultp) {
-    accelerometer_provider_ = new AccelerometerFileReader();
-  } else {
-    accelerometer_provider_ = new AccelerometerProviderMojo();
-  }
+#if BUILDFLAG(USE_IIOSERVICE)
+  accelerometer_provider_ = new AccelerometerProviderMojo();
+#else   // !BUILDFLAG(USE_IIOSERVICE)
+  accelerometer_provider_ = new AccelerometerFileReader();
+#endif  // BUILDFLAG(USE_IIOSERVICE)
 }
 
 AccelerometerReader::~AccelerometerReader() = default;
