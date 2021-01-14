19aea53764313d0992b91983c7720b5d730583ab
Jordan Demeulenaere <jdemeulenaere@google.com>
2021-01-14 08:41:53 +0000

    [Autofill Assistant] Fix the details placeholder animation.
    This CL fixes the placeholder animation which was not correctly
    supporting multiple details. Now, all details with placeholders
    subscribe to the placeholder animation, which is stopped each time we
    change the details.
    
    This CL also fixes the wrong type of ShowDetailsProto.delay_ms, which
    should be an int32 and not a bool.
    
    Bug: b/174652960
    Change-Id: Ib37479337bfc732d57c2b60ff168c28444e2575f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2625885
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Auto-Submit: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#843439}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/details/AssistantDetailsCoordinator.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/details/AssistantDetailsCoordinator.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.VisibleForTesting;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -24,6 +25,7 @@ import org.chromium.chrome.browser.image_fetcher.ImageFetcher;
  */
 public class AssistantDetailsCoordinator {
     private final RecyclerView mView;
+    private final AssistantDetailsAdapter mAdapter;
 
     public AssistantDetailsCoordinator(
             Context context, AssistantDetailsModel model, ImageFetcher imageFetcher) {
@@ -34,13 +36,13 @@ public class AssistantDetailsCoordinator {
                 context, LinearLayoutManager.VERTICAL, /* reverseLayout= */ false));
         mView.setBackgroundResource(R.drawable.autofill_assistant_details_bg);
         mView.addItemDecoration(new DetailsItemDecoration(context));
-        AssistantDetailsAdapter adapter = new AssistantDetailsAdapter(context, imageFetcher);
-        mView.setAdapter(adapter);
+        mAdapter = new AssistantDetailsAdapter(context, imageFetcher);
+        mView.setAdapter(mAdapter);
 
         // Listen to the model and set the details on the adapter when they change.
         model.addObserver((source, propertyKey) -> {
             if (propertyKey == AssistantDetailsModel.DETAILS) {
-                adapter.setDetails(model.get(AssistantDetailsModel.DETAILS));
+                mAdapter.setDetails(model.get(AssistantDetailsModel.DETAILS));
             }
         });
 
@@ -61,6 +63,11 @@ public class AssistantDetailsCoordinator {
 
     public RecyclerView getView() {
         return mView;
+    }
+
+    @VisibleForTesting
+    public boolean isRunningPlaceholdersAnimationForTesting() {
+        return mAdapter.isRunningPlaceholdersAnimation();
     }
 
     /** A divider that is drawn after each details, except the last one. */
