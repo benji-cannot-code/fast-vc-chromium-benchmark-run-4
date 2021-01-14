2683dd87e67e17c3e921caa5be746c4ede3ccfe0
Daniel Hosseinian <dhoss@chromium.org>
2021-01-14 03:23:29 +0000

    Add pdf_date_fuzzer for chrome_pdf::ParsePdfDate()
    Bug: 93619
    Change-Id: I14ffeabbbfac3321167f735329c0b7b50a2709f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617131
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843357}

---
+++ pdf/pdf_utils/dates_fuzzer.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <string>
+
+#include "base/time/time.h"
+#include "pdf/pdf_utils/dates.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  const std::string input(reinterpret_cast<const char*>(data), size);
+  chrome_pdf::ParsePdfDate(input);
+  return 0;
+}
