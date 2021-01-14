778a6ac342b400b2081daedccc004adcadcadc93
Victor Costan <pwnall@chromium.org>
2021-01-14 08:16:37 +0000

    DevTools: Remove unused include from StorageHandler.
    Bug: 1016065
    Change-Id: I25f113d45ee642759555b7b08d0f24fc38c0f917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628068
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843428}

--- content/browser/devtools/protocol/storage_handler.cc
+++ content/browser/devtools/protocol/storage_handler.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/storage_partition.h"
-#include "storage/browser/quota/quota_client.h"
 #include "storage/browser/quota/quota_manager.h"
 #include "storage/browser/quota/quota_manager_proxy.h"
 #include "storage/browser/quota/quota_override_handle.h"
