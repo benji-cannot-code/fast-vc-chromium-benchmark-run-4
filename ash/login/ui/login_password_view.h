e347740e6643248678eccd692bd716cbb4be48e9
Thomas Tellier <tellier@google.com>
2021-01-14 12:16:54 +0000

    [CrOS] Center password's placeholder text on login/lock screen
    Bug: 1160230
    Change-Id: I1eac02e8a68b83dbd0b4c1cc82b7d6e6ca33b05b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627852
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Thomas Tellier <tellier@google.com>
    Cr-Commit-Position: refs/heads/master@{#843497}

--- ash/login/ui/login_password_view.h
+++ ash/login/ui/login_password_view.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ime/ime_controller_impl.h"
 #include "ash/login/ui/animated_rounded_image_view.h"
 #include "ash/login/ui/login_palette.h"
+#include "ash/login/ui/non_accessible_view.h"
 #include "ash/public/cpp/session/user_info.h"
 #include "base/scoped_observer.h"
 #include "base/strings/string16.h"
@@ -207,6 +208,7 @@ class ASH_EXPORT LoginPasswordView : public views::View,
   LoginTextfield* textfield_ = nullptr;
   ArrowButtonView* submit_button_ = nullptr;
   DisplayPasswordButton* display_password_button_ = nullptr;
+  NonAccessibleView* password_end_space_ = nullptr;
   // Could show either the caps lock icon or the easy unlock icon.
   AlternateIconsView* left_icon_ = nullptr;
   views::ImageView* capslock_icon_ = nullptr;
