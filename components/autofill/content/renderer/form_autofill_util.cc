5cb36a242686f75eb8e4e3e9541aeb649cb5023c
Mason Freed <masonfreed@chromium.org>
2021-01-14 23:24:18 +0000

    Pass through blink::Element::GetIdAttribute to WebElement
    Rather than manually retrieve the "id" attribute, it is cleaner
    and faster to use the existing blink::Element method, which is
    optimized [1].
    
    This should have no behavior changes.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/renderer/core/dom/element.h;l=1326;drc=9f3ae654ee080ec3ccb5c3f85a51f5e880f5f18e
    
    Change-Id: Ic7634eaa8e8057315d83317862ae16428ee15078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628029
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843783}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -1655,10 +1655,8 @@ bool IsWebElementVisible(const blink::WebElement& element) {
 
 base::string16 GetFormIdentifier(const WebFormElement& form) {
   base::string16 identifier = form.GetName().Utf16();
-  static base::NoDestructor<WebString> kId("id");
   if (identifier.empty())
-    identifier = form.GetAttribute(*kId).Utf16();
-
+    identifier = form.GetIdAttribute().Utf16();
   return identifier;
 }
 
@@ -1708,7 +1706,6 @@ void WebFormControlElementToFormField(
   DCHECK(field);
   DCHECK(!element.IsNull());
   static base::NoDestructor<WebString> kAutocomplete("autocomplete");
-  static base::NoDestructor<WebString> kId("id");
   static base::NoDestructor<WebString> kName("name");
   static base::NoDestructor<WebString> kRole("role");
   static base::NoDestructor<WebString> kPlaceholder("placeholder");
@@ -1717,7 +1714,7 @@ void WebFormControlElementToFormField(
   // Save both id and name attributes, if present. If there is only one of them,
   // it will be saved to |name|. See HTMLFormControlElement::nameForAutofill.
   field->name = element.NameForAutofill().Utf16();
-  field->id_attribute = element.GetAttribute(*kId).Utf16();
+  field->id_attribute = element.GetIdAttribute().Utf16();
   field->name_attribute = element.GetAttribute(*kName).Utf16();
   field->unique_renderer_id =
       FieldRendererId(element.UniqueRendererFormControlId());
