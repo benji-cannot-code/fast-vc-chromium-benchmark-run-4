9ff525bd9f25a625ddc7618d24eee369696b9f2c
James Cook <jamescook@chromium.org>
2021-01-14 18:44:52 +0000

    Don't log when the LacrosSupport flag is not enabled
    The "Lacros-chrome is not supported" was printing 7 times in each
    Chrome OS browser_tests test run. This confused some developers, who
    thought their test failures might be caused by lacros. Since the flag
    being off is the common case, don't log.
    
    Bug: none
    Change-Id: I38b46b0a0390d39fae5ca1f095c9131aa25692f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627714
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843636}

--- chrome/browser/chromeos/crosapi/browser_util.cc
+++ chrome/browser/chromeos/crosapi/browser_util.cc
@@ -165,7 +165,7 @@ bool IsLacrosEnabled(Channel channel) {
     return true;
 
   if (!base::FeatureList::IsEnabled(chromeos::features::kLacrosSupport)) {
-    LOG(WARNING) << "Lacros-chrome is not supported";
+    // Don't log here. It's too spammy.
     return false;
   }
 
