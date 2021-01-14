18e88ca672740178abe606908dcaa24450554cfd
Joel Riley <joelriley@google.com>
2021-01-14 21:28:40 +0000

    Fix bug where focus ring disappeared sometimes when quickly navigating.
    Timing issue that can happen when user performs navigation before 'start' event is received. Simplified interruption handler which was putting STS into 'inactive' state when it was not necessary.
    
    Bug: 1165284
    Change-Id: Ia9818b29fcd57e0aaa5584579851ad093d9f89d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622748
    Commit-Queue: Joel Riley <joelriley@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843742}

--- chrome/browser/resources/chromeos/accessibility/select_to_speak/mock_tts.js
+++ chrome/browser/resources/chromeos/accessibility/select_to_speak/mock_tts.js
@@ -38,6 +38,20 @@ var MockTts = function() {
    * @private
    */
   this.options_ = null;
+
+  /**
+   * Whether TTS events will be queued instead of sent immediately.
+   * @private {boolean}
+   */
+  this.waitToSendEvents_ = false;
+
+  /**
+   * TTS engine events waiting to be sent. Each entry in the array is an array
+   * of two elements: [options, event], where |options| is the TTS options
+   * during the time of that event, and |event| is the event isself.
+   * @private {!Array<!Array<!Object>>}
+   */
+  this.pendingEvents_ = [];
 };
 
 MockTts.prototype = {
@@ -48,7 +62,7 @@ MockTts.prototype = {
     this.currentlySpeaking_ = true;
     if (options && options.onEvent) {
       this.options_ = options;
-      this.options_.onEvent({type: 'start', charIndex: 0});
+      this.sendEvent({type: 'start', charIndex: 0});
     }
     if (this.speechCallbackStack_.length > 0) {
       this.speechCallbackStack_.pop()(utterance);
@@ -58,7 +72,7 @@ MockTts.prototype = {
     this.pendingUtterances_ = [];
     this.currentlySpeaking_ = false;
     if (this.options_) {
-      this.options_.onEvent({type: 'end'});
+      this.sendEvent({type: 'end'});
     }
   },
   /**
@@ -69,14 +83,14 @@ MockTts.prototype = {
   speakUntilCharIndex(nextStartIndex) {
     this.currentlySpeaking_ = true;
     if (this.options_) {
-      this.options_.onEvent({type: 'word', charIndex: nextStartIndex});
+      this.sendEvent({type: 'word', charIndex: nextStartIndex});
     }
   },
   stop() {
     this.pendingUtterances_ = [];
     this.currentlySpeaking_ = false;
     if (this.options_) {
-      this.options_.onEvent({type: 'cancelled'});
+      this.sendEvent({type: 'cancelled'});
       this.options_ = null;
     }
   },
@@ -98,5 +112,25 @@ MockTts.prototype = {
   },
   getOptions() {
     return this.options_;
+  },
+  sendEvent(event) {
+    if (!this.options_) {
+      return;
+    }
+    if (this.waitToSendEvents_) {
+      // Queue event if set to wait on events.
+      this.pendingEvents_.push([this.options_, event]);
+      return;
+    }
+    this.options_.onEvent(event);
+  },
+  setWaitToSendEvents(waitToSendEvents) {
+    this.waitToSendEvents_ = waitToSendEvents;
+  },
+  sendPendingEvents() {
+    while (this.pendingEvents_.length > 0) {
+      const [options, event] = this.pendingEvents_.pop();
+      options.onEvent(event);
+    }
   }
 };
