afc17534f71b4823b80be4ebcea15b7948c7162a
Aaron Tagliaboschi <aarontag@chromium.org>
2021-01-14 18:59:35 +0000

    [Client-Hints] Move AddClientHintsIfNecessary to BaseFetchContext
    Make client hints available to other inheritors (i.e.
    WorkerFetchContext). Also a little bit of refactoring. Should be a
    purely mechanical change.
    
    The purpose behind the refactor is to pull out the dependence on
    the Document to the calling site and make everything Document-
    dependent optional, so non-Document-related calls can be made
    (specifically Service Workers initializing their own requests).
    
    Bug: 1151050
    Change-Id: I964585e7a1509b96e8a36ea673be81ff85e6a208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2557225
    Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843650}

--- third_party/blink/renderer/platform/loader/fetch/client_hints_preferences.cc
+++ third_party/blink/renderer/platform/loader/fetch/client_hints_preferences.cc
@@ -35,6 +35,18 @@ void ClientHintsPreferences::UpdateFrom(
   }
 }
 
+void ClientHintsPreferences::CombineWith(
+    const ClientHintsPreferences& preferences) {
+  for (size_t i = 0;
+       i < static_cast<int>(network::mojom::WebClientHintsType::kMaxValue) + 1;
+       ++i) {
+    network::mojom::WebClientHintsType type =
+        static_cast<network::mojom::WebClientHintsType>(i);
+    if (preferences.ShouldSend(type))
+      SetShouldSend(type);
+  }
+}
+
 bool ClientHintsPreferences::UserAgentClientHintEnabled() {
   return RuntimeEnabledFeatures::UserAgentClientHintEnabled() &&
          !base::CommandLine::ForCurrentProcess()->HasSwitch(
