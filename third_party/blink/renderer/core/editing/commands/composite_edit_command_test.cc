95773c271606fea0d61f4aea3bb3ae2baf89560e
Oriol Brufau <obrufau@igalia.com>
2021-01-14 06:01:15 +0000

    [editing] Avoid CHECK failure in CreateMarkupAlgorithm::CreateMarkup
    CompositeEditCommand::MoveParagraphs has a start and end positions, and
    constrains them with Most{Forward,Backward}CaretPosition. The problem
    was that the resulting positions could end up reversed, triggering a
    CHECK failure in CreateMarkup.
    
    This patch changes CompositeEditCommand::MoveParagraphs so that it only
    attempts to contrain the positions if they are different, and also
    changes MostBackwardOrForwardCaretPosition to ensure that the shadow-
    adjusted candidate is not further from the initial position than the
    non-adjusted candidate.
    
    Bug: 1165325
    
    TEST=CompositeEditCommandTest.MoveParagraphContentsToNewBlockWithUAShadowDOM
    TEST=SelectionAdjusterTest.AdjustSelectionTypeWithShadow
    TEST=VisibleSelectionTest.ShadowCrossing
    TEST=VisibleSelectionTest.ShadowNested
    
    Change-Id: Ib52ed472809e15aa551fdfc4ed1fa2323433d4cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627368
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843406}

--- third_party/blink/renderer/core/editing/commands/composite_edit_command_test.cc
+++ third_party/blink/renderer/core/editing/commands/composite_edit_command_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/text.h"
 #include "third_party/blink/renderer/core/editing/frame_selection.h"
 #include "third_party/blink/renderer/core/editing/testing/editing_test_base.h"
+#include "third_party/blink/renderer/core/editing/visible_position.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 
 namespace blink {
@@ -141,6 +142,23 @@ TEST_F(CompositeEditCommandTest,
       "<style>div{-webkit-user-modify:read-only;user-select:none;}</style>"
       "foo",
       body->innerHTML());
+}
+
+TEST_F(CompositeEditCommandTest,
+       MoveParagraphContentsToNewBlockWithUAShadowDOM) {
+  SetBodyContent("<object contenteditable><input></object>");
+  base::RunLoop().RunUntilIdle();
+
+  SampleCommand& sample = *MakeGarbageCollected<SampleCommand>(GetDocument());
+  Element* input = GetDocument().QuerySelector("input");
+  Position pos = Position::BeforeNode(*input);
+  EditingState editing_state;
+
+  // Should not crash
+  sample.MoveParagraphContentsToNewBlockIfNecessary(pos, &editing_state);
+  EXPECT_FALSE(editing_state.IsAborted());
+  EXPECT_EQ("<object contenteditable=\"\"><div><input></div></object>",
+            GetDocument().body()->innerHTML());
 }
 
 TEST_F(CompositeEditCommandTest, InsertNodeOnDisconnectedParent) {
