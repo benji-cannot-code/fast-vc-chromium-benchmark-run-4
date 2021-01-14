e99851b1ba28d646be4ad517bdfc7c12d8775d67
Daniel Murphy <dmurph@chromium.org>
2021-01-14 19:57:48 +0000

    [WebAppProvider] Prevent browser process crash during WebApp migration
    If the user starts chrome by launching the WebApp that is being migrated
    from, then Chrome crashed due to shutting down while the browser object
    is still being used by the migration code. This patch ensures that the
    browser doesn't exit during migration, and only keeps the browser alive
    during the migration itself.
    
    Note: This WebAppMover is not a permanent class, and will be removed
    after the Chat app is successfully migrated.
    
    Design Doc (Googlers only):
    https://docs.google.com/document/d/1GOek_Q4O_jTewHHx0MLnbYXvlTdEKT-KmkPZ5R0VJ3c/edit?resourcekey=0-K35fuzesohP36-qKSGbp0g
    
    Bug: 1165603
    Change-Id: I3a38402513e3dacdd05b5d516b01f371043d5206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622584
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843680}

--- chrome/browser/web_applications/web_app_mover.h
+++ chrome/browser/web_applications/web_app_mover.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "chrome/browser/web_applications/components/install_manager.h"
 #include "chrome/browser/web_applications/components/web_app_id.h"
+#include "components/keep_alive_registry/scoped_keep_alive.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/driver/sync_service_observer.h"
 #include "url/gurl.h"
@@ -100,6 +101,7 @@ class WebAppMover final : public syncer::SyncServiceObserver {
 
   bool new_app_open_as_window_ = false;
   std::vector<AppId> apps_to_uninstall_;
+  std::unique_ptr<ScopedKeepAlive> migration_keep_alive_;
 
   base::ScopedObservation<syncer::SyncService, syncer::SyncServiceObserver>
       sync_observer_{this};
