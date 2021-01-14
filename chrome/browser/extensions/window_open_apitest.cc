29bfc2a758d030302ab822f68772eaf54ad81a50
Nancy Wang <nancylingwang@chromium.org>
2021-01-14 00:33:59 +0000

    Reland "Create standard icons on shelf for non-app icons."
    This reverts commit f112bfc264a901634d86c9ce114504ce8f69369a.
    
    Reason for revert: <INSERT REASONING HERE>
    Fix the browser tests issue.
    
    Original change's description:
    > Revert "Create standard icons on shelf for non-app icons."
    >
    > This reverts commit 201f15f151543d61bbe258957723526b34c06414.
    >
    > Reason for revert: Suspicious about causing a test breakage of WindowOpenApiTest.PopupBlockingExtension.
    > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/22177/overview
    >
    > Original change's description:
    > > Create standard icons on shelf for non-app icons.
    > >
    > > AppService creates standard circle icons for all apps. However, there
    > > are a few cases that icons on shelf are not app icons, e.g.:
    > > 1. Task manager
    > > 2. A few extensions show icons when open a new window, e.g. google
    > > hangouts.
    > >
    > > This CL create standard circle icons for them when they are shown on
    > > shelf.
    > >
    > > Modify TaskManagerView to create standard icon when running in Chrome
    > > OS.
    > >
    > > Modify ChromeLauncherController::OnAppImageUpdated. If the icon is not
    > > loaded by AppServiceAppIconLoader (which loads the standard circle
    > > icons), create standard icons.
    > >
    > > BUG=1162514
    > > BUG=1142373
    > >
    > > Change-Id: Ibfe40c33944a1120ca9c991c4953d0da906ee2e2
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2624273
    > > Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > Reviewed-by: Scott Violet <sky@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#842874}
    >
    > TBR=xiyuan@chromium.org,sky@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,nancylingwang@chromium.org
    >
    > Change-Id: I6e0dbef6a173a179d79e39cebbd4a4f78f15bcfb
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1162514
    > Bug: 1142373
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626821
    > Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    > Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#842947}
    
    TBR=xiyuan@chromium.org,sky@chromium.org,yukishiino@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,nancylingwang@chromium.org
    
    
    Bug: 1162514
    Bug: 1142373
    Change-Id: If55904732c4fd468db2a5f56cece04fc8a937a0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626943
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843283}

--- chrome/browser/extensions/window_open_apitest.cc
+++ chrome/browser/extensions/window_open_apitest.cc
@@ -86,7 +86,7 @@ bool WaitForTabsPopupsApps(Browser* browser,
         browser->tab_strip_model()->count() == num_tabs)
       break;
 
-    content::RunAllPendingInMessageLoop();
+    content::RunAllTasksUntilIdle();
   }
 
   EXPECT_EQ(num_browsers, chrome::GetBrowserCount(browser->profile()));
--- chrome/browser/ui/app_list/app_service/app_service_app_icon_loader.h
+++ chrome/browser/ui/app_list/app_service/app_service_app_icon_loader.h
@@ -23,6 +23,8 @@ class Profile;
 class AppServiceAppIconLoader : public AppIconLoader,
                                 private apps::AppRegistryCache::Observer {
  public:
+  static bool CanLoadImage(Profile* profile, const std::string& id);
+
   AppServiceAppIconLoader(Profile* profile,
                           int resource_size_in_dip,
                           AppIconLoaderDelegate* delegate);
