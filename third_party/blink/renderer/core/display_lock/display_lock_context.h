d76482429567891fe47b9fb22ae6e3f2a8b0fbb6
Vladimir Levin <vmpstr@chromium.org>
2021-01-14 23:01:52 +0000

    content-visibility: Try and restore scroll offset if the element is a scroller.
    This patch tries to restore the scroll offset on a locked element if
    it is a scroller. If the content of the element has changed, then this
    scroll offset may not take effect, but we still try to set it.
    
    R=chrishtr@chromium.org
    
    Bug: 1165856
    Change-Id: Iade948abbb08a30280e25fbff4c2e8eefdf012c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2625627
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843769}

--- third_party/blink/renderer/core/display_lock/display_lock_context.h
+++ third_party/blink/renderer/core/display_lock/display_lock_context.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
+#include "third_party/blink/renderer/core/scroll/scroll_types.h"
 #include "third_party/blink/renderer/core/style/computed_style_base_constants.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cancellable_task.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -329,6 +330,13 @@ class CORE_EXPORT DisplayLockContext final
   // in style adjustment.
   bool CanDirtyStyle() const;
 
+  // When a scroller becomes locked, we store off its current scroll offset, to
+  // avoid losing the offset when the scroller becomes unlocked in the future.
+  // The following functions enable this functionality.
+  void StashScrollOffsetIfAvailable();
+  void RestoreScrollOffsetIfStashed();
+  bool HasStashedScrollOffset() const;
+
   WeakMember<Element> element_;
   WeakMember<Document> document_;
   EContentVisibility state_ = EContentVisibility::kVisible;
@@ -420,6 +428,8 @@ class CORE_EXPORT DisplayLockContext final
   // since we can also force update style outside of this call (via ensure
   // computed style).
   bool set_requested_state_scope_ = false;
+
+  base::Optional<ScrollOffset> stashed_scroll_offset_;
 };
 
 }  // namespace blink
