f124d6450189b5872097eb46dd84bb302d78760f
Mark Yacoub <markyacoub@google.com>
2021-01-14 20:34:20 +0000

    Perform Modeset of all displays in 1 commit.
    Perform 1 drm commit consisting of all displays being modeset/disabled
    together.
    This guarantees that the kernel is aware of the states of all displays
    at the same time, especially in case of inter-dependency.
    
    ScreenManagerTest.{CheckForSecondValidController,CheckMultipleControllersAfterBeingDisabled}
    
    BUG: b/172069469, b/168753412
    TEST: modesetting kohaku with external display,
    Change-Id: I4bb5b253eda793cc5807ab0000cc5e14854dc3d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2551490
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Mark Yacoub <markyacoub@google.com>
    Cr-Commit-Position: refs/heads/master@{#843704}

--- ui/ozone/platform/drm/gpu/crtc_commit_request.h
+++ ui/ozone/platform/drm/gpu/crtc_commit_request.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <xf86drmMode.h>
 
+#include "ui/gfx/geometry/point.h"
 #include "ui/ozone/platform/drm/gpu/drm_device.h"
 #include "ui/ozone/platform/drm/gpu/drm_overlay_plane.h"
 
@@ -32,6 +33,7 @@ class CrtcCommitRequest {
       uint32_t crtc_id,
       uint32_t connector_id,
       drmModeModeInfo mode,
+      gfx::Point origin,
       HardwareDisplayPlaneList* plane_list,
       DrmOverlayPlaneList overlays);
 
@@ -44,6 +46,7 @@ class CrtcCommitRequest {
   uint32_t crtc_id() const { return crtc_id_; }
   uint32_t connector_id() const { return connector_id_; }
   const drmModeModeInfo& mode() const { return mode_; }
+  const gfx::Point& origin() const { return origin_; }
   HardwareDisplayPlaneList* plane_list() const { return plane_list_; }
   const DrmOverlayPlaneList& overlays() const { return overlays_; }
 
@@ -51,6 +54,7 @@ class CrtcCommitRequest {
   CrtcCommitRequest(uint32_t crtc_id,
                     uint32_t connector_id,
                     drmModeModeInfo mode,
+                    gfx::Point origin,
                     HardwareDisplayPlaneList* plane_list,
                     DrmOverlayPlaneList overlays,
                     bool should_enable);
@@ -59,6 +63,7 @@ class CrtcCommitRequest {
   const uint32_t crtc_id_ = 0;
   const uint32_t connector_id_ = 0;
   const drmModeModeInfo mode_ = {};
+  const gfx::Point origin_;
   HardwareDisplayPlaneList* plane_list_ = nullptr;
   const DrmOverlayPlaneList overlays_;
 };
