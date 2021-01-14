4b51ef0973d45b774958d16eebaedd6a0293690d
Swapnil <swapnilgupta@google.com>
2021-01-14 09:38:57 +0000

    [Extensions] Use policy-enforced update URL in assets manager
    This change gives ExtensionManagement control over update URL used when
    updating extensions. Previously only first install was affected, all
    later updates used update URL from the extension manifest. Now update
    URL from the policy can be used if extension management said so.
    This change is split into multiple CLs.
    
    Update URL from policy is handled in
    https://chromium-review.googlesource.com/c/chromium/src/+/2593260.
    
    Bug: b/175786074
    Change-Id: I73580416d039a14abe7b91ecad50f4f8cecc8682
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596259
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Swapnil Gupta <swapnilgupta@google.com>
    Cr-Commit-Position: refs/heads/master@{#843457}

--- chrome/browser/extensions/crx_installer.cc
+++ chrome/browser/extensions/crx_installer.cc
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/preload_check_group.h"
 #include "extensions/browser/requirements_checker.h"
 #include "extensions/common/extension_icon_set.h"
+#include "extensions/common/extension_urls.h"
 #include "extensions/common/file_util.h"
 #include "extensions/common/manifest.h"
 #include "extensions/common/manifest_handlers/kiosk_mode_info.h"
@@ -905,13 +906,22 @@ void CrxInstaller::UpdateCreationFlagsAndCompleteInstall(
   if (withholding_behavior == kWithholdPermissions)
     creation_flags_ |= Extension::WITHHOLD_PERMISSIONS;
 
+  ExtensionManagement* extension_management =
+      ExtensionManagementFactory::GetForBrowserContext(profile());
+  const GURL update_url =
+      extension_management->GetEffectiveUpdateURL(*(extension()));
+  const bool updates_from_webstore_or_empty_update_url =
+      update_url.is_empty() || extension_urls::IsWebstoreUpdateUrl(update_url);
   if (!shared_file_task_runner_->PostTask(
-          FROM_HERE, base::BindOnce(&CrxInstaller::CompleteInstall, this))) {
+          FROM_HERE,
+          base::BindOnce(&CrxInstaller::CompleteInstall, this,
+                         updates_from_webstore_or_empty_update_url))) {
     NOTREACHED();
   }
 }
 
-void CrxInstaller::CompleteInstall() {
+void CrxInstaller::CompleteInstall(
+    bool updates_from_webstore_or_empty_update_url) {
   DCHECK(shared_file_task_runner_->RunsTasksInCurrentSequence());
 
   if (current_version_.IsValid() &&
@@ -929,7 +939,8 @@ void CrxInstaller::CompleteInstall() {
       ExtensionAssetsManager::GetInstance();
   assets_manager->InstallExtension(
       extension(), unpacked_extension_root_, install_directory_, profile(),
-      base::BindOnce(&CrxInstaller::ReloadExtensionAfterInstall, this));
+      base::Bind(&CrxInstaller::ReloadExtensionAfterInstall, this),
+      updates_from_webstore_or_empty_update_url);
 }
 
 void CrxInstaller::ReloadExtensionAfterInstall(
