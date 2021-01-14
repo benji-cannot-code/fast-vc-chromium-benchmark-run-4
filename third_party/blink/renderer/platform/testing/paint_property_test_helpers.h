45b1e34391285eefbef55e6ba32cf43a24cfa5a5
Xianzhu Wang <wangxianzhu@chromium.org>
2021-01-14 17:30:00 +0000

    Revert "Add CullRect::ApplyPaintProperties()"
    This reverts commit 6f895b078d7c02241cb182233416c3958d65c008.
    
    Reason for revert: The reverted CL is supposed not to affect
    production, but it seems to cause crbug.com/1166540.
    
    Original change's description:
    > Add CullRect::ApplyPaintProperties()
    >
    > It will be called to map a cull rect from parent space to local space
    > in cull rect update during PrePaint. The purpose is to mark PaintLayers
    > needing repaint on cull rect changes before paint, instead of checking
    > interest rect / cull rect changes during paint, to avoid setting
    > PaintLayer needing repaint unnecessarily, especially for CAP during
    > composited scrolling.
    >
    > Besides the original features of CullRect::ApplyTransforms() which is
    > used in cull rect mapping for CompositeAfterPaint, it also implements
    > the following features to be on par with CompositedLayerMapping::
    > ReComputeInterestRect():
    >
    > - Considering clips in additional to scroll clips,
    > - Clipping expanded cull rect by scrolling contents rect for composited
    >   scroll translations,
    > - Clamping extreme geometry values,
    > - Adaptive expansion distance for composited transforms.
    >
    > The next steps are:
    > 1. Add CullRectUpdater which updates cull rects during PrePaint when
    >    runtime feature CullRectUpdate is enabled.
    > 2. Use the updated cull rect to replace interest rect in pre-CAP and
    >    cull rect in CAP and pre-CAP, still behind the runtime feature.
    > 3. Enable CullRectUpdate for CAP
    > 4. Enable CullRectUpdate for pre-CAP.
    > 5. Remove old interest rect / cull rect code.
    >
    > Bug: 1046544
    > Change-Id: I6e7e9f85bc0b4640e67de1732744e647b6f7e230
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2620090
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#843304}
    
    TBR=szager@chromium.org,wangxianzhu@chromium.org,pdr@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: Idc3374d3d426b9f571e417b1acbbc18227fdf274
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1046544, 1166540
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628021
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843592}

--- third_party/blink/renderer/platform/testing/paint_property_test_helpers.h
+++ third_party/blink/renderer/platform/testing/paint_property_test_helpers.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/paint/clip_paint_property_node.h"
 #include "third_party/blink/renderer/platform/graphics/paint/effect_paint_property_node.h"
 #include "third_party/blink/renderer/platform/graphics/paint/property_tree_state.h"
-#include "third_party/blink/renderer/platform/graphics/paint/ref_counted_property_tree_state.h"
 #include "third_party/blink/renderer/platform/graphics/paint/transform_paint_property_node.h"
 #include "third_party/blink/renderer/platform/testing/paint_property_test_helpers.h"
 
@@ -24,9 +23,6 @@ inline const ClipPaintPropertyNode& c0() {
 }
 inline const EffectPaintPropertyNode& e0() {
   return EffectPaintPropertyNode::Root();
-}
-inline const ScrollPaintPropertyNode& s0() {
-  return ScrollPaintPropertyNode::Root();
 }
 
 constexpr int c0_id = 1;
@@ -222,28 +218,6 @@ inline scoped_refptr<TransformPaintPropertyNode> CreateScrollTranslation(
   return TransformPaintPropertyNode::Create(parent, std::move(state));
 }
 
-inline scoped_refptr<TransformPaintPropertyNode> CreateScrollTranslation(
-    const TransformPaintPropertyNodeOrAlias& parent,
-    float offset_x,
-    float offset_y,
-    const IntRect& container_rect,
-    const IntSize& contents_size,
-    CompositingReasons compositing_reasons = CompositingReason::kNone) {
-  const auto* parent_scroll_translation = &parent.Unalias();
-  while (!parent_scroll_translation->ScrollNode())
-    parent_scroll_translation = parent_scroll_translation->UnaliasedParent();
-  ScrollPaintPropertyNode::State scroll_state;
-  scroll_state.container_rect = container_rect;
-  scroll_state.contents_size = contents_size;
-  TransformPaintPropertyNode::State translation_state{
-      FloatSize(offset_x, offset_y)};
-  translation_state.direct_compositing_reasons = compositing_reasons;
-  translation_state.scroll = ScrollPaintPropertyNode::Create(
-      *parent_scroll_translation->ScrollNode(), std::move(scroll_state));
-  return TransformPaintPropertyNode::Create(parent,
-                                            std::move(translation_state));
-}
-
 inline scoped_refptr<TransformPaintPropertyNode>
 CreateCompositedScrollTranslation(
     const TransformPaintPropertyNodeOrAlias& parent,
@@ -252,46 +226,6 @@ CreateCompositedScrollTranslation(
     const ScrollPaintPropertyNode& scroll) {
   return CreateScrollTranslation(parent, offset_x, offset_y, scroll,
                                  CompositingReason::kOverflowScrolling);
-}
-
-inline scoped_refptr<TransformPaintPropertyNode>
-CreateCompositedScrollTranslation(
-    const TransformPaintPropertyNodeOrAlias& parent,
-    float offset_x,
-    float offset_y,
-    const IntRect& container_rect,
-    const IntSize& contents_size) {
-  return CreateScrollTranslation(parent, offset_x, offset_y, container_rect,
-                                 contents_size,
-                                 CompositingReason::kOverflowScrolling);
-}
-
-inline RefCountedPropertyTreeState CreateScrollTranslationState(
-    const PropertyTreeState& parent_state,
-    float offset_x,
-    float offset_y,
-    const IntRect& container_rect,
-    const IntSize& contents_size,
-    CompositingReasons compositing_reasons = CompositingReason::kNone) {
-  return RefCountedPropertyTreeState(PropertyTreeState(
-      *CreateScrollTranslation(parent_state.Transform(), offset_x, offset_y,
-                               container_rect, contents_size,
-                               compositing_reasons),
-      *CreateClip(parent_state.Clip(), parent_state.Transform(),
-                  FloatRoundedRect(container_rect)),
-      e0()));
-}
-
-inline RefCountedPropertyTreeState CreateCompositedScrollTranslationState(
-    const PropertyTreeState& parent_state,
-    float offset_x,
-    float offset_y,
-    const IntRect& container_rect,
-    const IntSize& contents_size,
-    CompositingReasons compositing_reasons = CompositingReason::kNone) {
-  return CreateScrollTranslationState(parent_state, offset_x, offset_y,
-                                      container_rect, contents_size,
-                                      CompositingReason::kOverflowScrolling);
 }
 
 inline PropertyTreeState DefaultPaintChunkProperties() {
