703256fb295d1c41f5b6a1da74bd390cb9ac9203
Daniel Rubery <drubery@chromium.org>
2021-01-14 20:11:17 +0000

    Add a Feature for Enterprise CSD server and the functionality
    This CL adds two new base::Features, SafeBrowsingEnterpriseCsd and
    SafeBrowsingDisableConsumerCsdForEnterprise to control
    whether using the Enterprise CSD server is enabled. It also makes two
    behavior changes depending on these flags. When enabled:
    - The consumer download ping is skipped
    - The |csd| field in the upload request is populated with the download
      metadata.
    
    Bug: 1165815
    Change-Id: I00e3cc17ed56eff93803ab50232bb00497667afe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626031
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843689}

--- chrome/browser/safe_browsing/cloud_content_scanning/test_binary_upload_service.h
+++ chrome/browser/safe_browsing/cloud_content_scanning/test_binary_upload_service.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.h"
 #include "chrome/browser/safe_browsing/services_delegate.h"
+#include "components/enterprise/common/proto/connectors.pb.h"
 
 namespace safe_browsing {
 
@@ -23,9 +24,13 @@ class TestBinaryUploadService : public BinaryUploadService {
                    enterprise_connectors::ContentAnalysisResponse response);
 
   bool was_called() { return was_called_; }
+  const enterprise_connectors::ContentAnalysisRequest& last_request() {
+    return last_request_;
+  }
   void ClearWasCalled();
 
  private:
+  enterprise_connectors::ContentAnalysisRequest last_request_;
   Result saved_result_ = Result::UNKNOWN;
   enterprise_connectors::ContentAnalysisResponse saved_response_;
   bool was_called_ = false;
--- chrome/browser/safe_browsing/download_protection/deep_scanning_request.h
+++ chrome/browser/safe_browsing/download_protection/deep_scanning_request.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/connectors/common.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.h"
+#include "chrome/browser/safe_browsing/cloud_content_scanning/file_analysis_request.h"
 #include "chrome/browser/safe_browsing/download_protection/download_protection_util.h"
 #include "components/enterprise/common/proto/connectors.pb.h"
 
@@ -19,6 +20,7 @@ class DownloadItem;
 namespace safe_browsing {
 
 class DownloadProtectionService;
+class DownloadRequestMaker;
 
 // This class encapsulates the process of uploading a file to Safe Browsing for
 // deep scanning and reporting the result.
@@ -62,6 +64,12 @@ class DeepScanningRequest : public download::DownloadItem::Observer {
   void Start();
 
  private:
+  // Callback when the |download_request_maker_| is finished assembling the
+  // download metadata request.
+  void OnDownloadRequestReady(
+      std::unique_ptr<FileAnalysisRequest> deep_scan_request,
+      std::unique_ptr<ClientDownloadRequest> download_request);
+
   // Callbacks for when |binary_upload_service_| finishes uploading.
   void OnScanComplete(BinaryUploadService::Result result,
                       enterprise_connectors::ContentAnalysisResponse response);
@@ -83,7 +91,8 @@ class DeepScanningRequest : public download::DownloadItem::Observer {
   void OpenDownload();
 
   // Populates a request with the appropriate data depending on the used proto.
-  void PrepareRequest(BinaryUploadService::Request* request, Profile* profile);
+  void PrepareRequest(std::unique_ptr<FileAnalysisRequest> deep_scan_request,
+                      Profile* profile);
 
   // The download item to scan. This is unowned, and could become nullptr if the
   // download is destroyed.
@@ -104,6 +113,9 @@ class DeepScanningRequest : public download::DownloadItem::Observer {
 
   // The settings to apply to this scan.
   enterprise_connectors::AnalysisSettings analysis_settings_;
+
+  // Used to assemble the download metadata.
+  std::unique_ptr<DownloadRequestMaker> download_request_maker_;
 
   base::WeakPtrFactory<DeepScanningRequest> weak_ptr_factory_;
 };
