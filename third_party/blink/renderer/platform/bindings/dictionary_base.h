5bf6d6eb43251b2ed615b1f654ff591b08e3bcb4
Michael Lippautz <mlippautz@chromium.org>
2021-01-14 09:58:44 +0000

    heap: More compilation fixes for Oilpan library
    Bug: 1056170
    Change-Id: I2382ff5f7489346e591e3f5466352ff6190e16c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626445
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843464}

--- third_party/blink/renderer/platform/bindings/dictionary_base.h
+++ third_party/blink/renderer/platform/bindings/dictionary_base.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class Visitor;
-
 namespace bindings {
 
 // This class is the base class for all IDL dictionary implementations.  This is
--- third_party/blink/renderer/platform/heap/v8_wrapper/visitor.h
+++ third_party/blink/renderer/platform/heap/v8_wrapper/visitor.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_V8_WRAPPER_VISITOR_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_V8_WRAPPER_VISITOR_H_
 
+#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "v8/include/cppgc/visitor.h"
 
 namespace blink {
