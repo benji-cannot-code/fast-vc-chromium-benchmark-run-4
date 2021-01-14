a4adedf50548dafa37f5c76b0aa9ab436ad64eb9
Daniel Hosseinian <dhoss@chromium.org>
2021-01-14 02:48:24 +0000

    Add a function to parse PDF dates
    The function converts the string format described in section 7.9.4
    "Dates" of the ISO 32000-1 standard to a base::Time, which will be
    necessary when localizing dates for display.
    
    Bug: 93619
    Change-Id: I9c580ca201e6058032778c5d72a1a713478937cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2610624
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843344}

---
+++ pdf/pdf_utils/dates.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PDF_PDF_UTILS_DATES_H_
+#define PDF_PDF_UTILS_DATES_H_
+
+#include "base/strings/string_piece.h"
+
+namespace base {
+class Time;
+}  // namespace base
+
+namespace chrome_pdf {
+
+// Parses a string in the PDF date format (see section 7.9.4 "Dates" of the ISO
+// 32000-1 standard). If `date` cannot be parsed, returns a "null" time (one for
+// which `base::Time::is_null()` returns `true`).
+base::Time ParsePdfDate(base::StringPiece date);
+
+}  // namespace chrome_pdf
+
+#endif  // PDF_PDF_UTILS_DATES_H_
