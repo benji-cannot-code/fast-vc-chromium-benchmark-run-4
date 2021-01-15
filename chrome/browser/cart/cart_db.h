7e43e0d69fe34cabfe463cba3f34bb6e034bbb90
Yue Zhang <yuezhanggg@chromium.org>
2021-01-15 19:31:28 +0000

    [ChromeCart] Add more functions to chrome cart database
    TBR: yusufo@chromium.org
    Bug: 1157892
    Change-Id: I6199dacb72f3e9e9133d28115a7a621a8c0297fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613954
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844185}

--- chrome/browser/cart/cart_db.h
+++ chrome/browser/cart/cart_db.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CART_CART_DB_H_
 #define CHROME_BROWSER_CART_CART_DB_H_
 
+#include "base/callback_helpers.h"
 #include "base/memory/weak_ptr.h"
 
 namespace content {
@@ -21,10 +22,36 @@ class ProfileProtoDB;
 
 class CartDB {
  public:
+  using KeyAndValue = std::pair<std::string, cart_db::ChromeCartContentProto>;
+
+  // Callback which is used when cart content is acquired.
+  using LoadCallback = base::OnceCallback<void(bool, std::vector<KeyAndValue>)>;
+
+  // Used for confirming an operation was completed successfully (e.g.
+  // insert, delete).
+  using OperationCallback = base::OnceCallback<void(bool)>;
+
   explicit CartDB(content::BrowserContext* browser_context);
   CartDB(const CartDB&) = delete;
   CartDB& operator=(const CartDB&) = delete;
   ~CartDB();
+
+  // Load the cart for a domain.
+  void LoadCart(const std::string& domain, LoadCallback callback);
+
+  // Load all carts in the database.
+  void LoadAllCarts(LoadCallback callback);
+
+  // Add a cart to the database.
+  void AddCart(const std::string& domain,
+               const cart_db::ChromeCartContentProto& proto,
+               OperationCallback callback);
+
+  // Delete the cart from certain domain in the database.
+  void DeleteCart(const std::string& domain, OperationCallback callback);
+
+  // Delete all carts in the database.
+  void DeleteAllCarts(OperationCallback callback);
 
  private:
   ProfileProtoDB<cart_db::ChromeCartContentProto>* proto_db_;
