8bdfd8360b561620975dbdf5ad477c8ed04eb499
Pavel Yatsuk <pavely@google.com>
2021-01-15 00:17:41 +0000

    [Messages] Adjust Save Password message description for signed in users
    This CL adjusts Save Password message title and description to reflect
    feedback from privacy team:
    http://doc/1FxSQbCL-zqUsTJaEodEvZ7D8ENxE3lzg5_e7ZEsu8Uk#heading=h.vgwmyj43xgsb
    
    This CL adds strings for both options. Only one is currently used. Once
    we make a decision I'll clean up unused option.
    
    BUG=1166841
    R=twellington@chromium.org
    
    Change-Id: I762c6dbc5e99f9c5fee574321be2750d5d61509f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2630447
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843807}

--- chrome/browser/password_manager/android/save_password_message_delegate.h
+++ chrome/browser/password_manager/android/save_password_message_delegate.h
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/optional.h"
 #include "components/messages/android/message_wrapper.h"
 #include "components/password_manager/core/browser/password_form_manager_for_ui.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
+#include "components/signin/public/identity_manager/account_info.h"
 
 namespace content {
 class WebContents;
@@ -39,7 +41,7 @@ class SavePasswordMessageDelegate {
   void CreateMessage(
       content::WebContents* web_contents,
       std::unique_ptr<password_manager::PasswordFormManagerForUI> form_to_save,
-      bool is_saving_google_account);
+      base::Optional<AccountInfo> account_info);
 
   // Called in response to user clicking "Save" and "Never" buttons.
   void HandleSaveClick();
