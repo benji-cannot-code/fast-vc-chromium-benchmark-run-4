5ae1e08ad504287bdf2f11f34b8e10e4adc94878
Yue Zhang <yuezhanggg@chromium.org>
2021-01-15 19:21:58 +0000

    [ChromeCart] Update dismiss/restore functionalities in CartService
    Instead of using one boolean value in profile pref to record
    dismiss/restore state of cart module, this CL introduces two boolean
    values to record whether the module is temporarily hidden or
    permanently removed.
    
    Bug: 1157892
    Change-Id: I01d95a62fca6466ab893c7be8afbc20b5065ddf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2625816
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844179}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -1104,7 +1104,8 @@ extern const char kSecurityTokenSessionNotificationScheduledDomain[];
 #endif
 
 #if !defined(OS_ANDROID)
-extern const char kCartModuleDismissed[];
+extern const char kCartModuleHidden[];
+extern const char kCartModuleRemoved[];
 #endif
 
 }  // namespace prefs
