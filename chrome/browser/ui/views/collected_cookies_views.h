5898c1036ff0dcd128515209d132bbc153d42ae3
Elly Fong-Jones <ellyjones@google.com>
2021-01-15 19:51:09 +0000

    cbuiv: remove CollectedCookiesViews overrides
    Bug: 1075649
    Change-Id: Ia308f0fdd33e274921b1519c5f1aa539da775360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627777
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844194}

--- chrome/browser/ui/views/collected_cookies_views.h
+++ chrome/browser/ui/views/collected_cookies_views.h
@@ -48,12 +48,6 @@ class CollectedCookiesViews
   // Use BrowserWindow::ShowCollectedCookiesDialog to show.
   static void CreateAndShowForWebContents(content::WebContents* web_contents);
 
-  // views::DialogDelegate:
-  base::string16 GetWindowTitle() const override;
-  ui::ModalType GetModalType() const override;
-  bool ShouldShowCloseButton() const override;
-  void DeleteDelegate() override;
-
   // views::TabbedPaneListener:
   void TabSelectedAt(int index) override;
 
@@ -70,6 +64,9 @@ class CollectedCookiesViews
   explicit CollectedCookiesViews(content::WebContents* web_contents);
 
   void OnDialogClosed();
+
+  // DialogDelegateView:
+  void DeleteDelegate() override;
 
   std::unique_ptr<views::View> CreateAllowedPane();
   std::unique_ptr<views::View> CreateBlockedPane();
