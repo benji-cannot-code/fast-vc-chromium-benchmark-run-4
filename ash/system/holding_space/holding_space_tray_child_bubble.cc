dbca72f0de4a888edbdcf197c7d27ea4ee09ca0c
David Black <dmblack@google.com>
2021-01-15 19:31:51 +0000

    Move selection clearing from section views to child bubbles.
    Child bubbles have padding and child spacing such that there are gaps
    between holding space sections and child bubble edges as well as gaps
    between holding space sections within the same child bubble.
    
    Moving selection clearing to child bubbles instead of section views
    eliminates what the user might otherwise perceive as dead spots.
    
    Bug: 1167248
    Change-Id: I561f2862258189fa9960d6c25de7bd6da124771a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2632808
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#844186}

--- ash/system/holding_space/holding_space_tray_child_bubble.cc
+++ ash/system/holding_space/holding_space_tray_child_bubble.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/holding_space/holding_space_constants.h"
 #include "ash/style/ash_color_provider.h"
+#include "ash/system/holding_space/holding_space_item_view_delegate.h"
 #include "ash/system/holding_space/holding_space_item_views_section.h"
 #include "ash/system/holding_space/holding_space_util.h"
 #include "ash/system/tray/tray_constants.h"
@@ -251,6 +252,16 @@ void HoldingSpaceTrayChildBubble::ChildVisibilityChanged(views::View* child) {
   }
 
   PreferredSizeChanged();
+}
+
+void HoldingSpaceTrayChildBubble::OnGestureEvent(ui::GestureEvent* event) {
+  delegate_->OnHoldingSpaceTrayChildBubbleGestureEvent(*event);
+  views::View::OnGestureEvent(event);
+}
+
+bool HoldingSpaceTrayChildBubble::OnMousePressed(const ui::MouseEvent& event) {
+  delegate_->OnHoldingSpaceTrayChildBubbleMousePressed(event);
+  return true;
 }
 
 void HoldingSpaceTrayChildBubble::MaybeAnimateIn() {
