b2be746fda76db06e54e0e2f20be4d7b5faa640d
Azeem Arshad <azeemarshad@chromium.org>
2021-01-15 23:59:56 +0000

    [CrOS Cellular] Add flag to use external euicc
    This CL adds a flag that when enabled will cause Cellular Setup
    and Settings UI to use the external Euicc instead of the on-board
    Euicc. This is useful for testing with test eSIM cards that can be
    inserted in physical slots and appears as an additional Euicc in
    Hermes.
    
    Loadtime data value will be used in polymer elements in followup CL.
    
    Bug: 1166697, 1093185
    Change-Id: Iccccd2152f588dd7cae9f6d7fdd108795a46f118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2630431
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844292}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2212,6 +2212,9 @@ extern const char kCdmFactoryDaemonDescription[];
 extern const char kCellularUseAttachApnName[];
 extern const char kCellularUseAttachApnDescription[];
 
+extern const char kCellularUseExternalEuiccName[];
+extern const char kCellularUseExternalEuiccDescription[];
+
 extern const char kConnectivityDiagnosticsWebUiName[];
 extern const char kConnectivityDiagnosticsWebUiDescription[];
 
