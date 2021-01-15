c722d49bfa747bbea3a6d02a53c1e3f50b128634
Josh Santana <joshsantana@google.com>
2021-01-15 23:07:29 +0000

    Added Metrics for LanguageSettings.Actions on Browser
    Added UMA histogram metrics for the browser settings UI.
    Mainly affects language_page.js and add_languages_dialog.js. The metrics tracked should now match the android version's implementation here:
    https://uma.googleplex.com/p/chrome/histograms?sid=475eec68a50e5bcf24652249c380d74e
    
    Bug: 1159266
    Change-Id: Id533c550b27ca3630a793131f47258b64a404e53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600455
    Commit-Queue: Joshua Santana <joshsantana@google.com>
    Reviewed-by: Trevor  Perrier <perrier@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: My Nguyen <myy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844275}

--- chrome/browser/resources/settings/languages_page/languages_page.js
+++ chrome/browser/resources/settings/languages_page/languages_page.js
@@ -55,6 +55,8 @@ import {Route, Router} from '../router.m.js';
 import {LanguagesMetricsProxy, LanguagesMetricsProxyImpl, LanguagesPageInteraction} from './languages_metrics_proxy.js';
 // </if>
 
+import {LanguageSettingsActionType, LanguageSettingsMetricsProxy, LanguageSettingsMetricsProxyImpl} from './languages_settings_metrics_proxy.js';
+
 /**
  * @type {number} Millisecond delay that can be used when closing an action
  *      menu to keep it briefly on-screen.
@@ -170,12 +172,20 @@ Polymer({
   // <if expr="chromeos">
   /** @private {?LanguagesMetricsProxy} */
   languagesMetricsProxy_: null,
+  // </if>
+  /** @private {?LanguageSettingsMetricsProxy} */
+  languageSettingsMetricsProxy_: null,
 
   /** @override */
   created() {
+    // <if expr="chromeos">
     this.languagesMetricsProxy_ = LanguagesMetricsProxyImpl.getInstance();
+    // </if>
+    this.languageSettingsMetricsProxy_ =
+        LanguageSettingsMetricsProxyImpl.getInstance();
   },
 
+  // <if expr="chromeos">
   /** @private */
   onOpenChromeOSLanguagesSettingsClick_() {
     const chromeOSLanguagesSettingsPath =
@@ -224,6 +234,8 @@ Polymer({
     // <if expr="chromeos">
     this.languagesMetricsProxy_.recordAddLanguages();
     // </if>
+    this.languageSettingsMetricsProxy_.recordSettingsMetric(
+        LanguageSettingsActionType.CLICK_ON_ADD_LANGUAGE);
     this.showAddLanguagesDialog_ = true;
   },
 
@@ -352,18 +364,24 @@ Polymer({
    * @param {!Event} e
    * @private
    */
-  onTranslateToggleChange_(e) {
-    this.languagesMetricsProxy_.recordToggleTranslate(e.target.checked);
+  onSpellcheckToggleChange_(e) {
+    this.languagesMetricsProxy_.recordToggleSpellCheck(e.target.checked);
   },
+  // </if>
 
   /**
    * @param {!Event} e
    * @private
    */
-  onSpellcheckToggleChange_(e) {
-    this.languagesMetricsProxy_.recordToggleSpellCheck(e.target.checked);
+  onTranslateToggleChange_(e) {
+    // <if expr="chromeos">
+    this.languagesMetricsProxy_.recordToggleTranslate(e.target.checked);
+    // </if>
+    this.languageSettingsMetricsProxy_.recordSettingsMetric(
+        e.target.checked ?
+            LanguageSettingsActionType.ENABLE_TRANSLATE_GLOBALLY :
+            LanguageSettingsActionType.DISABLE_TRANSLATE_GLOBALLY);
   },
-  // </if>
 
   // <if expr="chromeos or is_win">
   /**
@@ -528,9 +546,16 @@ Polymer({
     if (e.target.checked) {
       this.languageHelper.enableTranslateLanguage(
           this.detailLanguage_.language.code);
+
+      this.languageSettingsMetricsProxy_.recordSettingsMetric(
+          LanguageSettingsActionType.ENABLE_TRANSLATE_FOR_SINGLE_LANGUAGE);
+
     } else {
       this.languageHelper.disableTranslateLanguage(
           this.detailLanguage_.language.code);
+
+      this.languageSettingsMetricsProxy_.recordSettingsMetric(
+          LanguageSettingsActionType.DISABLE_TRANSLATE_FOR_SINGLE_LANGUAGE);
     }
     // <if expr="chromeos">
     this.languagesMetricsProxy_.recordTranslateCheckboxChanged(
@@ -559,6 +584,8 @@ Polymer({
   onMoveToTopTap_() {
     /** @type {!CrActionMenuElement} */ (this.$$('#menu').get()).close();
     this.languageHelper.moveLanguageToFront(this.detailLanguage_.language.code);
+    this.languageSettingsMetricsProxy_.recordSettingsMetric(
+        LanguageSettingsActionType.LANGUAGE_LIST_REORDERED);
   },
 
   /**
@@ -569,6 +596,8 @@ Polymer({
     /** @type {!CrActionMenuElement} */ (this.$$('#menu').get()).close();
     this.languageHelper.moveLanguage(
         this.detailLanguage_.language.code, true /* upDirection */);
+    this.languageSettingsMetricsProxy_.recordSettingsMetric(
+        LanguageSettingsActionType.LANGUAGE_LIST_REORDERED);
   },
 
   /**
@@ -579,6 +608,8 @@ Polymer({
     /** @type {!CrActionMenuElement} */ (this.$$('#menu').get()).close();
     this.languageHelper.moveLanguage(
         this.detailLanguage_.language.code, false /* upDirection */);
+    this.languageSettingsMetricsProxy_.recordSettingsMetric(
+        LanguageSettingsActionType.LANGUAGE_LIST_REORDERED);
   },
 
   /**
@@ -588,6 +619,8 @@ Polymer({
   onRemoveLanguageTap_() {
     /** @type {!CrActionMenuElement} */ (this.$$('#menu').get()).close();
     this.languageHelper.disableLanguage(this.detailLanguage_.language.code);
+    this.languageSettingsMetricsProxy_.recordSettingsMetric(
+        LanguageSettingsActionType.LANGUAGE_REMOVED);
   },
 
   // <if expr="not is_macosx">
