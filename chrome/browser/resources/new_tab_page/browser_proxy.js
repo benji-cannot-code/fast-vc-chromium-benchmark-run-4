4a973b59584430ca1e4f125ff0364a05d68ff442
Esmael El-Moslimany <aee@chromium.org>
2021-01-15 22:08:49 +0000

    WebUI NTP: load lazy bundle after timeout is reached
    When creating many new tabs, requestIdleCallback() seems to hang until
    there is a mouse action in the content area. This does not happen in the
    normal use case of opening a single new tab and interacting with it.
    Adding a timeout to requestIdleCallback() works around this issue.
    
    Bug: 1167047
    Change-Id: Ic10d7f75f0be0c6ce9b34992df1a4dc09b9d0e3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2633068
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844254}

--- chrome/browser/resources/new_tab_page/browser_proxy.js
+++ chrome/browser/resources/new_tab_page/browser_proxy.js
@@ -86,7 +86,7 @@ export class BrowserProxy {
    */
   waitForLazyRender() {
     return new Promise((resolve, reject) => {
-      requestIdleCallback(resolve);
+      requestIdleCallback(resolve, {timeout: 500});
     });
   }
 
