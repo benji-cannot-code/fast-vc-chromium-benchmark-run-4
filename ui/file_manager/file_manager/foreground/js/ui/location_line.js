a181543a74651eac99f875d943708a53605d28fd
Luciano Pacheco <lucmult@chromium.org>
2021-01-15 00:52:38 +0000

    Files app: Add JS module for toolbar_controller.js
    Remove the call to Polymer.updateStyles() because this isn't necessary
    anymore and Polymer 3 doesn't expose this function in the Polymer
    object. It was added for crbug.com/740836. I've tested manually without
    this function call and checked the value of  --files-ripple-bg-color in
    the #view-button, this variable changed value from white to black when
    selecting a file like described in the bug.
    
    Bug: 1133186
    Change-Id: Ia955545a60bef11a823b181589a4acaad2e39c06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628712
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843831}

--- ui/file_manager/file_manager/foreground/js/ui/location_line.js
+++ ui/file_manager/file_manager/foreground/js/ui/location_line.js
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {PathComponent} from '../path_component.m.js';
 // #import {NativeEventTarget as EventTarget} from 'chrome://resources/js/cr/event_target.m.js';
 // #import {metrics} from '../../../common/js/metrics.m.js';
-// #import './breadcrumb.m.js';
+// #import {BreadCrumb} from './breadcrumb.m.js';
 // clang-format on
 
 /**
@@ -91,7 +91,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   updateNg_(components) {
     this.components_ = Array.from(components);
 
-    let breadcrumbs = document.querySelector('bread-crumb');
+    let breadcrumbs =
+        /** @type {!BreadCrumb} */ (document.querySelector('bread-crumb'));
     if (!breadcrumbs) {
       breadcrumbs = document.createElement('bread-crumb');
       breadcrumbs.id = 'breadcrumbs';
