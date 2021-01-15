61eee743d2b00de31228e1ce0126a4bb6dc1d8ad
Kaiyu Chen <kylechenkyc@google.com>
2021-01-15 22:48:14 +0000

    Revert "Add GPayAppDynamicUpdate feature flag."
    This reverts commit 768c05c0f0cbb124939dd16c484eb11d7e074a21.
    
    Reason for revert: Should add this GPayAppDynamicUpdate feature flag to components/ instead of chrome/
    
    Original change's description:
    > Add GPayAppDynamicUpdate feature flag.
    >
    > The feature flag controls the GPay native app integration for dynamic update with web payments on Android.
    > The integration for dynamic update is mainly about adding callback service to notify the browser about changes like shipping address.
    > Design doc is go/dynamic-update-mweb.
    >
    > Bug: 1074423
    > Change-Id: I608ca5ddd5aa28f2da5672da83eefd0128720992
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2617201
    > Commit-Queue: Kaiyu Chen <kylechenkyc@google.com>
    > Reviewed-by: Sahel Sharify <sahel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#841632}
    
    TBR=sahel@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,kylechenkyc@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1074423
    Change-Id: I4b80472636a4673e7e96414d4c54fc6555f37bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628490
    Commit-Queue: Kaiyu Chen <kylechenkyc@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844267}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -186,7 +186,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kEphemeralTabUsingBottomSheet,
     &kExploreSites,
     &kFocusOmniboxInIncognitoTabIntents,
-    &kGPayAppDynamicUpdate,
     &kHandleMediaIntents,
     &kHomepagePromoCard,
     &kHorizontalTabSwitcherAndroid,
@@ -514,9 +513,6 @@ const base::Feature kEphemeralTabUsingBottomSheet{
 
 const base::Feature kExploreSites{"ExploreSites",
                                   base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kGPayAppDynamicUpdate{"GPayAppDynamicUpdate",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kHandleMediaIntents{"HandleMediaIntents",
                                         base::FEATURE_ENABLED_BY_DEFAULT};
