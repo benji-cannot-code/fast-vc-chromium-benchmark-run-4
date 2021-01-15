ab0ae5c72a339fda00289b972f75f99806b0214e
Jeff Cyr <jeffcyr@google.com>
2021-01-15 03:45:38 +0000

    Add histograms for Enhanced Safe Browsing CRX allowlist
    Add histograms to count the number enabled and disabled extensions that
    are not present in the Enhanced Safe Browsing CRX allowlist per profile.
    The metrics are separated for all users versus Enhanced Safe Browsing
    users.
    
    Bug: 1153873
    Change-Id: I84f167042fe63299b8da0669c9d117bf136090e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627827
    Commit-Queue: Jeff Cyr <jeffcyr@google.com>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843932}

--- chrome/browser/extensions/installed_loader.cc
+++ chrome/browser/extensions/installed_loader.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/common/url_constants.h"
+#include "extensions/browser/allowlist_state.h"
 #include "extensions/browser/event_router.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
@@ -371,6 +372,8 @@ void InstalledLoader::RecordExtensionsMetrics() {
   int off_store_item_count = 0;
   int web_request_blocking_count = 0;
   int web_request_count = 0;
+  int enabled_not_allowlisted_count = 0;
+  int disabled_not_allowlisted_count = 0;
 
   const ExtensionSet& extensions = extension_registry_->enabled_extensions();
   for (ExtensionSet::const_iterator iter = extensions.begin();
@@ -601,6 +604,12 @@ void InstalledLoader::RecordExtensionsMetrics() {
             num_granted_hosts);
       }
     }
+
+    if (extension_prefs_->GetExtensionAllowlistState(extension->id()) ==
+        ALLOWLIST_NOT_ALLOWLISTED) {
+      // Record the number of not allowlisted enabled extensions.
+      ++enabled_not_allowlisted_count;
+    }
   }
 
   const ExtensionSet& disabled_extensions =
@@ -624,6 +633,12 @@ void InstalledLoader::RecordExtensionsMetrics() {
                                   EXTERNAL_ITEM_NONWEBSTORE_DISABLED,
                                   EXTERNAL_ITEM_MAX_ITEMS);
       }
+    }
+
+    if (extension_prefs_->GetExtensionAllowlistState((*ex)->id()) ==
+        ALLOWLIST_NOT_ALLOWLISTED) {
+      // Record the number of not allowlisted disabled extensions.
+      ++disabled_not_allowlisted_count;
     }
   }
 
@@ -685,6 +700,16 @@ void InstalledLoader::RecordExtensionsMetrics() {
   base::UmaHistogramCounts100("Extensions.WebRequestBlockingCount",
                               web_request_blocking_count);
   base::UmaHistogramCounts100("Extensions.WebRequestCount", web_request_count);
+  base::UmaHistogramCounts100("Extensions.NotAllowlistedEnabled",
+                              enabled_not_allowlisted_count);
+  base::UmaHistogramCounts100("Extensions.NotAllowlistedDisabled",
+                              disabled_not_allowlisted_count);
+  if (safe_browsing::IsEnhancedProtectionEnabled(*profile->GetPrefs())) {
+    base::UmaHistogramCounts100("Extensions.NotAllowlistedEnabledAndEsbUser",
+                                enabled_not_allowlisted_count);
+    base::UmaHistogramCounts100("Extensions.NotAllowlistedDisabledAndEsbUser",
+                                disabled_not_allowlisted_count);
+  }
 }
 
 int InstalledLoader::GetCreationFlags(const ExtensionInfo* info) {
