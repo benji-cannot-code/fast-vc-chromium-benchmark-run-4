ac5c106368e59fd79459da6f6d5a31306b532ec7
Clemens Arbesser <arbesser@google.com>
2021-01-15 11:24:47 +0000

    [Autofill Assistant] Remove android dependency from unittest
    autofill_assistant:browser and its unittests should not depend on
    platform-specific stuff.
    
    Change-Id: I29c798130b0cbd171e87b47ccb3b8cbfceb9685e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631486
    Auto-Submit: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Marian Fechete <marianfe@google.com>
    Reviewed-by: Marian Fechete <marianfe@google.com>
    Cr-Commit-Position: refs/heads/master@{#844032}

--- components/autofill_assistant/browser/autofill_assistant_onboarding_fetcher_unittest.cc
+++ components/autofill_assistant/browser/autofill_assistant_onboarding_fetcher_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill_assistant/browser/autofill_assistant_onboarding_fetcher.h"
 
-#include "base/android/locale_utils.h"
 #include "base/containers/flat_map.h"
 #include "base/test/task_environment.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -72,9 +71,8 @@ TEST_F(AutofillAssistantOnboardingFetcherTest, FetchOnboardingStrings) {
         EXPECT_EQ(map.at("onboarding_text"), "Text");
       });
 
-  fetcher()->FetchOnboardingDefinition("BUY_MOVIE_TICKETS",
-                                       base::android::GetDefaultLocaleString(),
-                                       300, std::move(callback));
+  fetcher()->FetchOnboardingDefinition("BUY_MOVIE_TICKETS", "en-US", 300,
+                                       std::move(callback));
   EXPECT_EQ(1, GetNumberOfPendingRequests());
   SimulateResponse();
   EXPECT_EQ(0, GetNumberOfPendingRequests());
