0e568ec123e3c705db08ff99ac33c0410fcd6e7b
Sammie Quon <sammiequon@chromium.org>
2021-01-15 00:39:25 +0000

    capture_mode: Change snip key to toggle capture mode.
    Add support for the snip key in accelerators and KSV. For the time being,
    both accelerators and KSV will backwards support the old screenshot
    code, resulting in some extra logics in both that will get removed when
    capture mode can no longer be disabled.
    
    Fixed: 1163936
    Test: manual
    Change-Id: I05c32fdf1391397128b5dd2859d38d7508ceedd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628019
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843821}

--- ash/capture_mode/capture_mode_metrics.h
+++ ash/capture_mode/capture_mode_metrics.h
@@ -65,7 +65,8 @@ enum class CaptureModeEntryType {
   kQuickSettings,
   kStylusPalette,
   kPowerMenu,
-  kMaxValue = kPowerMenu,
+  kSnipKey,
+  kMaxValue = kSnipKey,
 };
 
 // Enumeration of quick actions on screenshot notification. Note that these
