3cac60f6fda4ff3563828ca82016b6fc338745a5
Leonid Baraz <lbaraz@chromium.org>
2021-01-15 23:46:49 +0000

    Make seq and gen id int64
    Convert uint64->int64 where necessary.
    Also, when uploading, generate double sequeincing information in
    EncryptedRecord to enable gradual server transition (unsigned one
    will go away later, with only signed one remaining).
    
    Bug: b:174509632
    Bug: b:177677467
    Change-Id: I8deec42dfa4978b5fdf3fbee9ee8e77cb58f0a62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631725
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/master@{#844287}

--- chrome/browser/policy/messaging_layer/upload/record_upload_request_builder.h
+++ chrome/browser/policy/messaging_layer/upload/record_upload_request_builder.h
@@ -31,6 +31,11 @@ namespace reporting {
 //         "generationId": 123456789,
 //         "priority": 1
 //       }
+//       "sequenceInformation": {
+//         "sequencingId": 1,
+//         "generationId": 123456789,
+//         "priority": 1
+//       }
 //     },
 //     {
 //       "encryptedWrappedRecord": "EncryptedMessage",
@@ -43,11 +48,22 @@ namespace reporting {
 //         "generationId": 123456789,
 //         "priority": 1
 //       }
+//       "sequenceInformation": {
+//         "sequencingId": 2,
+//         "generationId": 123456789,
+//         "priority": 1
+//       }
 //     }
 //   ]
 //   "attachEncryptionSettings": true  // optional field
 // }
 // TODO(b/159361496): Periodically add memory and disk space usage.
+//
+// Note that there are two identical sub-records - sequencingInformation and
+// sequenceInformation (sequencingId and generationId in the former are
+// Unsigned, in the later - Signed). This is done temporarily for backwards
+// compatibility with the server.
+// TODO(b/177677467): Remove this duplication once server is fully transitioned.
 //
 // This payload is added to the common payload of all reporting jobs, which
 // includes "device" and "browser" sub-fields:
@@ -92,6 +108,7 @@ class EncryptedRecordDictionaryBuilder {
   base::Optional<base::Value> Build();
 
   static base::StringPiece GetEncryptedWrappedRecordPath();
+  static base::StringPiece GetUnsignedSequencingInformationKeyPath();
   static base::StringPiece GetSequencingInformationKeyPath();
   static base::StringPiece GetEncryptionInfoPath();
 
