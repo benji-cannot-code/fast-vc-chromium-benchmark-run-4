1ff19e7080b27842530867ff5aac1e5d5f6d7a60
Gavin Williams <gavinwill@chromium.org>
2021-01-15 23:24:24 +0000

    scanning: Set selected option when dropdown options change
    This changes fixes a bug where the wrong option in the dropdown is
    selected when the selected scanner is changed. This happens when the new
    scanner has different available options than the previously selected
    scanner.
    
    Bug: 1166808
    Change-Id: I2f81f39358ad6c104e1dc360d429cc14cbe94055
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2633087
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844279}

--- chromeos/components/scanning/resources/resolution_select.js
+++ chromeos/components/scanning/resources/resolution_select.js
@@ -54,19 +54,17 @@ Polymer({
   },
 
   /**
-   * 300 dpi should be the default option if it exists. If not, use the first
-   * resolution in the resolutions array.
-   * @return {string}
+   * Get the index of the default option if it exists. If not, use the index of
+   * the first resolution in the resolutions array.
+   * @return {number}
    * @private
    */
-  getDefaultSelectedResolution_() {
+  getDefaultSelectedResolutionIndex_() {
     const defaultResolutionIndex = this.resolutions.findIndex((resolution) => {
       return this.isDefaultResolution_(resolution);
     });
 
-    return defaultResolutionIndex === -1 ?
-        this.resolutions[0].toString() :
-        this.resolutions[defaultResolutionIndex].toString();
+    return defaultResolutionIndex === -1 ? 0 : defaultResolutionIndex;
   },
 
   /**
@@ -83,7 +81,10 @@ Polymer({
     }
 
     if (this.resolutions.length > 0) {
-      this.selectedResolution = this.getDefaultSelectedResolution_();
+      const selectedResolutionIndex = this.getDefaultSelectedResolutionIndex_();
+      this.selectedResolution =
+          this.resolutions[selectedResolutionIndex].toString();
+      this.$.resolutionSelect.selectedIndex = selectedResolutionIndex;
     }
   },
 
