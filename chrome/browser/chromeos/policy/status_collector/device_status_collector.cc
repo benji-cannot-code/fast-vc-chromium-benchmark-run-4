963ed8ba73c3caa62434fd4dbaf1bd957b688f16
Aya ElAttar <ayaelattar@chromium.org>
2021-01-15 18:11:14 +0000

    Update Callback & Bind in StatusCollector
    Migrated all the usages of base::Callback &
    base::Bind in StatusCollector and its derived
    classes.
    
    Bug: 714018
    Change-Id: Iba62d767ef2adb9cc136262b6edeeff8e0001f11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2630027
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844137}

--- chrome/browser/chromeos/policy/status_collector/device_status_collector.cc
+++ chrome/browser/chromeos/policy/status_collector/device_status_collector.cc
@@ -422,7 +422,7 @@ void FetchGraphicsStatus(
 }
 
 bool ReadAndroidStatus(
-    const policy::StatusCollector::AndroidStatusReceiver& receiver) {
+    policy::StatusCollector::AndroidStatusReceiver receiver) {
   auto* const arc_service_manager = arc::ArcServiceManager::Get();
   if (!arc_service_manager)
     return false;
@@ -434,7 +434,7 @@ bool ReadAndroidStatus(
       ARC_GET_INSTANCE_FOR_METHOD(instance_holder, GetStatus);
   if (!instance)
     return false;
-  instance->GetStatus(receiver);
+  instance->GetStatus(std::move(receiver));
   return true;
 }
 
@@ -675,8 +675,8 @@ class DeviceStatusCollectorState : public StatusCollectorState {
 
   bool FetchAndroidStatus(
       const StatusCollector::AndroidStatusFetcher& android_status_fetcher) {
-    return android_status_fetcher.Run(
-        base::Bind(&DeviceStatusCollectorState::OnAndroidInfoReceived, this));
+    return android_status_fetcher.Run(base::BindOnce(
+        &DeviceStatusCollectorState::OnAndroidInfoReceived, this));
   }
 
   void FetchTpmStatus(
@@ -1349,16 +1349,16 @@ DeviceStatusCollector::DeviceStatusCollector(
   task_runner_ = base::SequencedTaskRunnerHandle::Get();
 
   if (volume_info_fetcher_.is_null())
-    volume_info_fetcher_ = base::Bind(&GetVolumeInfo);
+    volume_info_fetcher_ = base::BindRepeating(&GetVolumeInfo);
 
   if (cpu_statistics_fetcher_.is_null())
-    cpu_statistics_fetcher_ = base::Bind(&ReadCPUStatistics);
+    cpu_statistics_fetcher_ = base::BindRepeating(&ReadCPUStatistics);
 
   if (cpu_temp_fetcher_.is_null())
-    cpu_temp_fetcher_ = base::Bind(&ReadCPUTempInfo);
+    cpu_temp_fetcher_ = base::BindRepeating(&ReadCPUTempInfo);
 
   if (android_status_fetcher_.is_null())
-    android_status_fetcher_ = base::Bind(&ReadAndroidStatus);
+    android_status_fetcher_ = base::BindRepeating(&ReadAndroidStatus);
 
   if (tpm_status_fetcher_.is_null())
     tpm_status_fetcher_ = base::BindRepeating(&ReadTpmStatus);
@@ -1366,8 +1366,10 @@ DeviceStatusCollector::DeviceStatusCollector(
   if (emmc_lifetime_fetcher_.is_null())
     emmc_lifetime_fetcher_ = base::BindRepeating(&ReadDiskLifeTimeEstimation);
 
-  if (stateful_partition_info_fetcher_.is_null())
-    stateful_partition_info_fetcher_ = base::Bind(&ReadStatefulPartitionInfo);
+  if (stateful_partition_info_fetcher_.is_null()) {
+    stateful_partition_info_fetcher_ =
+        base::BindRepeating(&ReadStatefulPartitionInfo);
+  }
 
   if (cros_healthd_data_fetcher_.is_null()) {
     cros_healthd_data_fetcher_ =
@@ -1389,7 +1391,7 @@ DeviceStatusCollector::DeviceStatusCollector(
 
   // Watch for changes to the individual policies that control what the status
   // reports contain.
-  base::Closure callback = base::Bind(
+  base::Closure callback = base::BindRepeating(
       &DeviceStatusCollector::UpdateReportingSettings, base::Unretained(this));
   version_info_subscription_ = cros_settings_->AddSettingsObserver(
       chromeos::kReportDeviceVersionInfo, callback);
