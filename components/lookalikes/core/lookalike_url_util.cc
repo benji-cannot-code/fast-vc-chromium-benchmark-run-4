8b20beb648838ad2042717902a11db2e8f79656e
Etienne Pierre-doray <etiennep@chromium.org>
2021-01-15 16:50:36 +0000

    [jank]: Add lookalike url trace events and uma histogram.
    We're seeing jank in LookalikeUrlService::OnFetchEngagedSites from uma
    sampling profiler. There's a few possible culprits:
    - First call to GetDomainInfo causes icu loading
    - url_formatter::GetSkeletons is expensive and called on many urls
    - IsEngagementAtLeast is expensive and called on many urls
    
    To better understand the problem, this CL adds relevant trace events and
    records count of engaged sites as UMA histogram.
    
    Bug: 1135279
    Change-Id: Ie6ee4ed2dbeeed26e4bbdd72726a0ad48e9bc4bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2615322
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844109}

--- components/lookalikes/core/lookalike_url_util.cc
+++ components/lookalikes/core/lookalike_url_util.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
 #include "base/time/default_clock.h"
+#include "base/trace_event/trace_event.h"
 #include "base/values.h"
 #include "components/lookalikes/core/features.h"
 #include "components/security_interstitials/core/pref_names.h"
@@ -382,6 +383,7 @@ DomainInfo::~DomainInfo() = default;
 DomainInfo::DomainInfo(const DomainInfo&) = default;
 
 DomainInfo GetDomainInfo(const std::string& hostname) {
+  TRACE_EVENT0("navigation", "GetDomainInfo");
   if (net::HostStringIsLocalhost(hostname) ||
       net::IsHostnameNonUnique(hostname)) {
     return DomainInfo(std::string(), std::string(), std::string(),
