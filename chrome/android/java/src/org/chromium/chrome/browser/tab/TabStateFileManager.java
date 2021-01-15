f0938b4f1e251621569b91bd149cdd9b1197d87e
David Maunder <davidjm@chromium.org>
2021-01-15 18:27:58 +0000

    Meausre LoadTime/SaveTime of CriticalPersistedTabData/TabState
    Currently the load time we have includes the time taken on the sequenced
    task runner. Since CriticalPersistedTabData and TabState operations are
    both performed on the sequenced task runner, we block
    CriticalPersistedTabData read over the TabState read. This unfairly
    biases the CriticalPersistedTabData load time metric making it larger
    than it should be. It is better to compare the two independent of the
    sequenced task runner.
    
    Bug: 1166787
    Change-Id: Ibc716f66cf9ec745d741f471bbd372048ef9edb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627730
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844140}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabStateFileManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabStateFileManager.java
@@ -5,12 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tab;
 
+import android.os.SystemClock;
 import android.util.Pair;
 
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Log;
 import org.chromium.base.StreamUtil;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.browser.crypto.CipherFactory;
 import org.chromium.chrome.browser.version.ChromeVersionInfo;
 
@@ -65,7 +67,13 @@ public class TabStateFileManager {
         if (!file.exists()) return null;
 
         // If one of them passed, open the file input stream and read the state contents.
-        return restoreTabState(file, encrypted);
+        long startTime = SystemClock.elapsedRealtime();
+        TabState tabState = restoreTabState(file, encrypted);
+        if (tabState != null) {
+            RecordHistogram.recordTimesHistogram(
+                    "Tabs.TabState.LoadTime", SystemClock.elapsedRealtime() - startTime);
+        }
+        return tabState;
     }
 
     /**
@@ -217,6 +225,7 @@ public class TabStateFileManager {
      */
     public static void saveState(File file, TabState state, boolean encrypted) {
         if (state == null || state.contentsState == null) return;
+        long startTime = SystemClock.elapsedRealtime();
 
         // Create the byte array from contentsState before opening the FileOutputStream, in case
         // contentsState.buffer is an instance of MappedByteBuffer that is mapped to
@@ -256,6 +265,8 @@ public class TabStateFileManager {
             dataOutputStream.writeInt(
                     state.tabLaunchTypeAtCreation != null ? state.tabLaunchTypeAtCreation : -1);
             dataOutputStream.writeInt(state.rootId);
+            RecordHistogram.recordTimesHistogram(
+                    "Tabs.TabState.SaveTime", SystemClock.elapsedRealtime() - startTime);
         } catch (FileNotFoundException e) {
             Log.w(TAG, "FileNotFoundException while attempting to save TabState.");
         } catch (IOException e) {
--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/FilePersistedTabDataStorage.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/FilePersistedTabDataStorage.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tab.state;
 
 import android.content.Context;
+import android.os.StrictMode;
+import android.os.SystemClock;
 
 import androidx.annotation.MainThread;
 import androidx.annotation.VisibleForTesting;
@@ -29,8 +31,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Locale;
-
-import android.os.StrictMode;
 
 /**
  * {@link PersistedTabDataStorage} which uses a file for the storage
@@ -208,9 +208,14 @@ public class FilePersistedTabDataStorage implements PersistedTabDataStorage {
             FileOutputStream outputStream = null;
             boolean success = false;
             try {
+                long startTime = SystemClock.elapsedRealtime();
                 outputStream = new FileOutputStream(mFile);
                 outputStream.write(mData);
                 success = true;
+                RecordHistogram.recordTimesHistogram(
+                        String.format(Locale.US, "Tabs.PersistedTabData.Storage.SaveTime.%s",
+                                getUmaTag()),
+                        SystemClock.elapsedRealtime() - startTime);
             } catch (FileNotFoundException e) {
                 Log.e(TAG,
                         String.format(Locale.ENGLISH,
@@ -333,9 +338,14 @@ public class FilePersistedTabDataStorage implements PersistedTabDataStorage {
             boolean success = false;
             byte[] res = null;
             try {
+                long startTime = SystemClock.elapsedRealtime();
                 AtomicFile atomicFile = new AtomicFile(mFile);
                 res = atomicFile.readFully();
                 success = true;
+                RecordHistogram.recordTimesHistogram(
+                        String.format(Locale.US, "Tabs.PersistedTabData.Storage.LoadTime.%s",
+                                getUmaTag()),
+                        SystemClock.elapsedRealtime() - startTime);
             } catch (FileNotFoundException e) {
                 Log.e(TAG,
                         String.format(Locale.ENGLISH,
