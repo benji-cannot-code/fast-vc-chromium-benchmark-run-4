810116672e36dc112bb53abc4fefccac1267f769
Hui Yingst <nigi@chromium.org>
2021-01-15 00:13:26 +0000

    Migrate CalculateBackgroundParts() to PdfViewPluginBase.
    CalculateBackgroundParts() needs to be migrated to PdfViewPluginBase
    since it can be used identically by both PdfViewWebPlugin and
    OutOfProcessInstance.
    
    This CL migrates `background_parts_` and `available_area_` to
    PdfViewPluginBase and provides accessors and mutators for them, so that
    CalculateBackgroundParts() can be migrated as well.
    
    Bug: 1140629
    Change-Id: I767468a3716bae40c96c8c36b49d6dd2bb16008d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2625787
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843804}

--- pdf/pdf_view_plugin_base.h
+++ pdf/pdf_view_plugin_base.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "pdf/paint_manager.h"
 #include "pdf/pdfium/pdfium_form_filler.h"
+#include "ui/gfx/geometry/rect.h"
 
 namespace chrome_pdf {
 
@@ -39,7 +40,12 @@ class PdfViewPluginBase : public PDFEngine::Client,
                std::vector<gfx::Rect>* pending) override;
 
  protected:
-  // The mininum zoom level allowed.
+  struct BackgroundPart {
+    gfx::Rect location;
+    uint32_t color;
+  };
+
+  // The minimum zoom level allowed.
   static constexpr double kMinZoom = 0.01;
 
   PdfViewPluginBase();
@@ -84,10 +90,25 @@ class PdfViewPluginBase : public PDFEngine::Client,
   // background parts, and notifies the pdf engine.
   virtual void OnGeometryChanged(double old_zoom, float old_device_scale) = 0;
 
+  // Figures out the location of any background rectangles (i.e. those that
+  // aren't painted by the PDF engine).
+  void CalculateBackgroundParts();
+
   // Computes document width/height in device pixels, based on current zoom and
   // device scale
   int GetDocumentPixelWidth() const;
   int GetDocumentPixelHeight() const;
+
+  const std::vector<BackgroundPart>& background_parts() const {
+    return background_parts_;
+  }
+
+  const gfx::Rect& available_area() const { return available_area_; }
+
+  // TODO(https://crbug.com/1140629): Remove mutable_available_area()
+  // once all uses of it in OnGeometryChanged() are migrated to
+  // PdfViewPluginBase.
+  gfx::Rect& mutable_available_area() { return available_area_; }
 
   const gfx::Size& document_size() const { return document_size_; }
   void set_document_size(const gfx::Size& size) { document_size_ = size; }
@@ -129,6 +150,12 @@ class PdfViewPluginBase : public PDFEngine::Client,
  private:
   std::unique_ptr<PDFiumEngine> engine_;
   PaintManager paint_manager_{this};
+
+  std::vector<BackgroundPart> background_parts_;
+
+  // Remaining area, in pixels, to render the pdf in after accounting for
+  // horizontal centering.
+  gfx::Rect available_area_;
 
   // The size of the entire document in pixels (i.e. if each page is 800 pixels
   // high and there are 10 pages, the height will be 8000).
