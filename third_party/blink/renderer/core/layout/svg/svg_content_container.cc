2e306629addc0a6cefcd687d17ce3102cf413787
Fredrik Söderqvist <fs@opera.com>
2021-01-15 10:43:45 +0000

    Fix bounding box propagation for <foreignObject>
    Since the oBB is in unzoomed space but the <foreignObject>
    local-to-parent transform performs adjustment for zoom we need to
    perform correction when propagating it.
    Similarly change LayoutSVGForeignObject::StrokeBoundingBox() to return
    the same bounds as VisualRectInLocalSVGCoordinates() (which is zoomed).
    
    Bug: 1162993
    Change-Id: I430cd7832b37475ef1d139997ef5ac5c0bcc17c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627491
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#844016}

--- third_party/blink/renderer/core/layout/svg/svg_content_container.cc
+++ third_party/blink/renderer/core/layout/svg/svg_content_container.cc
@@ -144,6 +144,16 @@ static bool HasValidBoundingBoxForContainer(const LayoutObject& object) {
   return false;
 }
 
+static FloatRect ObjectBoundsForPropagation(const LayoutObject& object) {
+  FloatRect bounds = object.ObjectBoundingBox();
+  // The local-to-parent transform for <foreignObject> contains a zoom inverse,
+  // so we need to apply zoom to the bounding box that we use for propagation to
+  // be in the correct coordinate space.
+  if (IsA<LayoutSVGForeignObject>(object))
+    bounds.Scale(object.StyleRef().EffectiveZoom());
+  return bounds;
+}
+
 bool SVGContentContainer::UpdateBoundingBoxes(bool& object_bounding_box_valid) {
   object_bounding_box_valid = false;
 
@@ -155,8 +165,9 @@ bool SVGContentContainer::UpdateBoundingBoxes(bool& object_bounding_box_valid) {
     if (!HasValidBoundingBoxForContainer(*current))
       continue;
     const AffineTransform& transform = current->LocalToSVGParentTransform();
-    UpdateObjectBoundingBox(object_bounding_box, object_bounding_box_valid,
-                            transform.MapRect(current->ObjectBoundingBox()));
+    UpdateObjectBoundingBox(
+        object_bounding_box, object_bounding_box_valid,
+        transform.MapRect(ObjectBoundsForPropagation(*current)));
     stroke_bounding_box.Unite(transform.MapRect(current->StrokeBoundingBox()));
   }
 
