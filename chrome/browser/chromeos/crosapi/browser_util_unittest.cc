86d0bdbe2f8da0fb1b57d48f4467e3319314b2ef
James Cook <jamescook@chromium.org>
2021-01-15 22:18:44 +0000

    lacros: Allow LacrosAllowed policy to apply to developer builds
    Clean up some leftover logic from when we disabled lacros in M87 for
    non-google managed users. In particular, don't skip the policy check
    for developer builds with channel unknown. The LacrosAllowed pref
    defaults to true, so this doesn't affect developers who aren't working
    with policy.
    
    Bug: 1135494
    Test: updated unit_tests
    Change-Id: I510ada8de5d266626ba8adef64a7dd33202574a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628592
    Reviewed-by: Igor <igorcov@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844259}

--- chrome/browser/chromeos/crosapi/browser_util_unittest.cc
+++ chrome/browser/chromeos/crosapi/browser_util_unittest.cc
@@ -91,7 +91,10 @@ TEST_F(BrowserUtilTest, ManagedAccountLacrosEnabled) {
       true);
   g_browser_process->local_state()->SetBoolean(prefs::kLacrosAllowed, true);
 
+  EXPECT_TRUE(browser_util::IsLacrosEnabled(Channel::UNKNOWN));
   EXPECT_TRUE(browser_util::IsLacrosEnabled(Channel::CANARY));
+  EXPECT_TRUE(browser_util::IsLacrosEnabled(Channel::DEV));
+  EXPECT_TRUE(browser_util::IsLacrosEnabled(Channel::BETA));
 }
 
 TEST_F(BrowserUtilTest, ManagedAccountLacrosDisabled) {
@@ -102,7 +105,10 @@ TEST_F(BrowserUtilTest, ManagedAccountLacrosDisabled) {
       true);
   g_browser_process->local_state()->SetBoolean(prefs::kLacrosAllowed, false);
 
+  EXPECT_FALSE(browser_util::IsLacrosEnabled(Channel::UNKNOWN));
   EXPECT_FALSE(browser_util::IsLacrosEnabled(Channel::CANARY));
+  EXPECT_FALSE(browser_util::IsLacrosEnabled(Channel::DEV));
+  EXPECT_FALSE(browser_util::IsLacrosEnabled(Channel::BETA));
 }
 
 TEST_F(BrowserUtilTest, BlockedForChildUser) {
