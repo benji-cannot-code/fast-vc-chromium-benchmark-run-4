ce75d1e4dd0be5c82ed6c0699aa0c764edb7a240
Alexey Baskakov <loyso@chromium.org>
2021-01-15 08:12:34 +0000

    dpwa: Implement Web Apps Shortcuts Menu in ChromeOS UI.
    WebAppsChromeOs::GetMenuModel() appends the menu items
    using manifest resources already stored on disk
    (that part is already launched on all OSes)
    
    We override previously introduced
    WebAppsChromeOs::ExecuteContextMenuCommand()
    to launch the web app with url params appended.
    
    We implement ConvertSquareBitmapsToImageSkia helper function
    which converts manifest icons to hi-dpi in-memory representation
    needed by our AppService UI code.
    
    Bug: 1129721
    Change-Id: Ic6aa0777f74db079c91d0f50f173f334d4be862e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2462899
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#843981}

--- chrome/browser/apps/app_service/menu_util.cc
+++ chrome/browser/apps/app_service/menu_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/public/cpp/app_menu_constants.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
@@ -212,6 +213,23 @@ void PopulateItemFromMojoMenuItems(
       NOTREACHED();
       break;
   }
+}
+
+base::StringPiece MenuTypeToString(apps::mojom::MenuType menu_type) {
+  switch (menu_type) {
+    case apps::mojom::MenuType::kShelf:
+      return "shelf";
+    case apps::mojom::MenuType::kAppList:
+      return "applist";
+  }
+}
+
+apps::mojom::MenuType MenuTypeFromString(base::StringPiece menu_type) {
+  if (base::LowerCaseEqualsASCII(menu_type, "shelf"))
+    return apps::mojom::MenuType::kShelf;
+  if (base::LowerCaseEqualsASCII(menu_type, "applist"))
+    return apps::mojom::MenuType::kAppList;
+  return apps::mojom::MenuType::kShelf;
 }
 
 }  // namespace apps
