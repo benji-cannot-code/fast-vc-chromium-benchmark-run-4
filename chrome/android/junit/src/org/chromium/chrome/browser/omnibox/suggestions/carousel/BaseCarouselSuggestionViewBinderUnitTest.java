554f98fcb1de87d831177cc7a261a60e8c6b45ff
Tomasz Wiszkowski <ender@google.com>
2021-01-16 02:11:11 +0000

    Disable header for the Most Visited Tiles.
    This change allows BaseCarouselSuggestion to show or hide the
    header text depending on whether it is desired to show it.
    
    Bug: 1166302, 1145685
    Change-Id: If91da47ab1f121837d6c263dc6d217f2b4452b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2634015
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844337}

--- chrome/android/junit/src/org/chromium/chrome/browser/omnibox/suggestions/carousel/BaseCarouselSuggestionViewBinderUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/omnibox/suggestions/carousel/BaseCarouselSuggestionViewBinderUnitTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox.suggestions.carousel;
 
+import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
@@ -12,6 +13,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import android.content.res.Resources;
 import android.view.View;
 import android.widget.TextView;
 
@@ -23,6 +25,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
 
+import org.chromium.chrome.R;
 import org.chromium.testing.local.LocalRobolectricTestRunner;
 import org.chromium.ui.modelutil.MVCListAdapter.ListItem;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
@@ -39,6 +42,8 @@ import java.util.List;
 @RunWith(LocalRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class BaseCarouselSuggestionViewBinderUnitTest {
+    static final int SUGGESTION_VERTICAL_PADDING = 123;
+
     @Mock
     BaseCarouselSuggestionView mView;
 
@@ -46,10 +51,16 @@ public class BaseCarouselSuggestionViewBinderUnitTest {
     TextView mHeaderTextView;
 
     @Mock
+    View mHeaderView;
+
+    @Mock
     View mItemView;
 
     @Mock
     SimpleRecyclerViewAdapter mAdapter;
+
+    @Mock
+    Resources mResources;
 
     ModelList mTiles;
 
@@ -64,8 +75,13 @@ public class BaseCarouselSuggestionViewBinderUnitTest {
         mTiles = new ModelList();
 
         when(mView.getHeaderTextView()).thenReturn(mHeaderTextView);
+        when(mView.getHeaderView()).thenReturn(mHeaderView);
         when(mView.getAdapter()).thenReturn(mAdapter);
         when(mAdapter.getModelList()).thenReturn(mTiles);
+        when(mView.getResources()).thenReturn(mResources);
+
+        when(mResources.getDimensionPixelSize(eq(R.dimen.omnibox_carousel_suggestion_padding)))
+                .thenReturn(SUGGESTION_VERTICAL_PADDING);
     }
 
     @Test
@@ -110,5 +126,23 @@ public class BaseCarouselSuggestionViewBinderUnitTest {
         Assert.assertEquals(3, mTiles.size());
         mModel.set(BaseCarouselSuggestionViewProperties.TILES, null);
         Assert.assertEquals(0, mTiles.size());
+    }
+
+    @Test
+    public void headerTitle_visibilityChangeAltersTopPadding() {
+        mModel.set(BaseCarouselSuggestionViewProperties.SHOW_TITLE, true);
+        verify(mHeaderView, times(1)).setVisibility(eq(View.VISIBLE));
+        verify(mHeaderView, times(1)).setVisibility(anyInt());
+        verify(mView, times(1))
+                .setPaddingRelative(eq(0), eq(0), eq(0), eq(SUGGESTION_VERTICAL_PADDING));
+        verify(mView, times(1)).setPaddingRelative(anyInt(), anyInt(), anyInt(), anyInt());
+
+        mModel.set(BaseCarouselSuggestionViewProperties.SHOW_TITLE, false);
+        verify(mHeaderView, times(1)).setVisibility(eq(View.GONE));
+        verify(mHeaderView, times(2)).setVisibility(anyInt());
+        verify(mView, times(1))
+                .setPaddingRelative(eq(0), eq(SUGGESTION_VERTICAL_PADDING), eq(0),
+                        eq(SUGGESTION_VERTICAL_PADDING));
+        verify(mView, times(2)).setPaddingRelative(anyInt(), anyInt(), anyInt(), anyInt());
     }
 }
