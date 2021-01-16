edb5a62df73f7c8142206406abc8850a8f97f0ec
Elaine Chien <elainechien@chromium.org>
2021-01-16 00:25:52 +0000

    ChromeLabs: Dynamic refresh for enterprise policy
    Allow Enterprise admins to apply the policy without needing a browser restart.
    
    Bug: 1158427
    Change-Id: I52dc1488eb1c29ec4d93c73169e4523efd838244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2633070
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Connie Wan <connily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844304}

--- chrome/browser/ui/views/toolbar/chrome_labs_bubble_view_unittest.cc
+++ chrome/browser/ui/views/toolbar/chrome_labs_bubble_view_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/about_flags.h"
+#include "chrome/browser/ui/toolbar/chrome_labs_prefs.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/test_with_browser_view.h"
@@ -54,9 +55,12 @@ class ChromeLabsBubbleTest : public TestWithBrowserView {
         {kThirdTestFeatureId, "", "", os_other_than_current,
          FEATURE_VALUE_TYPE(kTestFeature3)}};
     about_flags::testing::SetFeatureEntries(entries);
-    TestWithBrowserView::SetUp();
-    ChromeLabsButton* button = chrome_labs_button();
 
+    TestWithBrowserView::SetUp();
+    profile()->GetPrefs()->SetBoolean(chrome_labs_prefs::kBrowserLabsEnabled,
+                                      true);
+
+    ChromeLabsButton* button = chrome_labs_button();
     CreateTestLabInfo();
     std::unique_ptr<ChromeLabsBubbleViewModel> test_model =
         std::make_unique<ChromeLabsBubbleViewModel>();
--- chrome/browser/ui/views/toolbar/toolbar_view.h
+++ chrome/browser/ui/views/toolbar/toolbar_view.h
@@ -241,6 +241,9 @@ class ToolbarView : public views::AccessiblePaneView,
       const ui::DropTargetEvent& event) override;
   views::View* GetViewForDrop() override;
 
+  // Changes the visibility of the Chrome Labs entry point based on prefs.
+  void OnChromeLabsPrefChanged();
+
   // Loads the images for all the child views.
   void LoadImages();
 
@@ -280,6 +283,9 @@ class ToolbarView : public views::AccessiblePaneView,
 
   Browser* const browser_;
   BrowserView* const browser_view_;
+
+  PrefService* profile_pref_service_;
+  std::unique_ptr<PrefChangeRegistrar> profile_registrar_;
 
   views::FlexLayout* layout_manager_ = nullptr;
 
