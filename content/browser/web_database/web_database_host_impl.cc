5c3f05e36e6854257602cc3be5f45bded2205243
Victor Costan <pwnall@chromium.org>
2021-01-17 23:42:20 +0000

    WebSQL: Move //content/browser implementation to its own directory.
    This CL brings WebSQL in line with other storage features, which have
    their own directories in //content/browser.
    
    Change-Id: I9a8f45015842ce808f82028fd1e5d73dd1e2c786
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2634867
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844411}

--- content/browser/web_database/web_database_host_impl.cc
+++ content/browser/web_database/web_database_host_impl.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/renderer_host/web_database_host_impl.h"
+#include "content/browser/web_database/web_database_host_impl.h"
 
 #include <string>
 #include <utility>
@@ -28,8 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/origin.h"
 
 using storage::DatabaseUtil;
-using storage::VfsBackend;
 using storage::QuotaManager;
+using storage::VfsBackend;
 
 namespace content {
 namespace {
