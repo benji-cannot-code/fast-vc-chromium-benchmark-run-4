31c31989386512ea32fb4ec93d3b5a5ccfc64f42
Joel Hockey <joelhockey@chromium.org>
2021-01-17 23:18:16 +0000

    Remove guest-os-external-protocol flag
    Bug: b/168506505
    Change-Id: Iad19f144b0db599f7450130e0f411d08122c9106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2619120
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844406}

--- chrome/browser/chromeos/external_protocol_dialog.cc
+++ chrome/browser/chromeos/external_protocol_dialog.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/external_protocol_dialog.h"
 
-#include "base/feature_list.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chromeos/arc/intent_helper/arc_external_protocol_dialog.h"
 #include "chrome/browser/chromeos/guest_os/guest_os_external_protocol_handler.h"
@@ -16,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/external_protocol_dialog.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
-#include "chromeos/constants/chromeos_features.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
@@ -45,20 +43,16 @@ void OnArcHandled(const GURL& url,
       tab_util::GetWebContentsByID(render_process_host_id, routing_id);
 
   // Display the standard ExternalProtocolDialog if Guest OS has a handler.
-  if (web_contents && base::FeatureList::IsEnabled(
-                          chromeos::features::kGuestOsExternalProtocol)) {
-    base::Optional<guest_os::GuestOsRegistryService::Registration>
-        registration = guest_os::GetHandler(
-            Profile::FromBrowserContext(web_contents->GetBrowserContext()),
-            url);
-    if (registration) {
-      new ExternalProtocolDialog(web_contents, url,
-                                 base::UTF8ToUTF16(registration->Name()),
-                                 initiating_origin);
-      return;
-    }
+  base::Optional<guest_os::GuestOsRegistryService::Registration> registration =
+      guest_os::GetHandler(
+          Profile::FromBrowserContext(web_contents->GetBrowserContext()), url);
+  if (registration) {
+    new ExternalProtocolDialog(web_contents, url,
+                               base::UTF8ToUTF16(registration->Name()),
+                               initiating_origin);
+  } else {
+    new ExternalProtocolNoHandlersDialog(web_contents, url);
   }
-  new ExternalProtocolNoHandlersDialog(web_contents, url);
 }
 
 }  // namespace
