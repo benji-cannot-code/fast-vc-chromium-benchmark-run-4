6b540b7c44c53a3332903e81e6683cbf65a848c3
Nancy Wang <nancylingwang@chromium.org>
2021-01-18 05:49:28 +0000

    Reland "Add browser tests for AppLaunchHandler."
    This reverts commit b9a6819e0fb356125af500e2d0ebad9f9f085099.
    
    Reason for revert: <INSERT REASONING HERE>
    Fix the browser tests.
    
    Original change's description:
    > Revert "Add browser tests for AppLaunchHandler."
    >
    > This reverts commit dd223e70b542505fdf8970d491bc40a2251571a4.
    >
    > Reason for revert: these tests have been failing constently on MSAN builders
    > https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests
    >
    > Original change's description:
    > > Add browser tests for AppLaunchHandler.
    > >
    > > Add browser tests for AppLaunchHandler to verify the restoration
    > > process.
    > >
    > > BUG=1146900
    > >
    > > Change-Id: Ibeaf262080a24c680718888ee081cc62c5b21a3a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2619245
    > > Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#843901}
    >
    > TBR=xiyuan@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,nancylingwang@chromium.org
    >
    > Change-Id: Icc6cc9ccbf6fcb0759bf2d03efd2d25a31d90d44
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1146900
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2632606
    > Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    > Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#844015}
    
    TBR=xiyuan@chromium.org,apotapchuk@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,nancylingwang@chromium.org
    
    
    Bug: 1146900
    Change-Id: I3b6706aa4dba74e8cf84426cc68cbf065e70748b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2633481
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844442}

--- chrome/browser/chromeos/full_restore/app_launch_handler.cc
+++ chrome/browser/chromeos/full_restore/app_launch_handler.cc
@@ -202,6 +202,7 @@ void AppLaunchHandler::LaunchWebAppOrExtension(
         app_id,
         static_cast<apps::mojom::LaunchContainer>(it.second->container.value()),
         static_cast<WindowOpenDisposition>(it.second->disposition.value()),
+        apps::mojom::AppLaunchSource::kSourceChromeInternal,
         it.second->display_id.value(),
         it.second->file_paths.has_value() ? it.second->file_paths.value()
                                           : std::vector<base::FilePath>{},
