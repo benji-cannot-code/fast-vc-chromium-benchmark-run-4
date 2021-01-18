9c5cfddfed5624ce889cbad4c15649ca65720945
Alice Wang <aliceywang@chromium.org>
2021-01-18 10:09:52 +0000

    [Signin] Migrate string id to CoreAccountId in ChildAccountInfoFetcher
    This CL migrates string id to CoreAccountId in ChildAccountInfoFetcher.
    ChildAccountInfoFetcher.java is moved from components/signin/core to
    components/signin/public to avoid circular dependency.
    
    Bug: 1028580
    Change-Id: Iff6ad1bdf8d18d2cc3afc3b4f2ad6dd984dfb4bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626434
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844474}

--- components/signin/internal/identity_manager/child_account_info_fetcher_android.h
+++ components/signin/internal/identity_manager/child_account_info_fetcher_android.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/android/scoped_java_ref.h"
+#include "components/signin/public/identity_manager/account_info.h"
 #include "google_apis/gaia/core_account_id.h"
 
 class AccountFetcherService;
@@ -25,8 +26,7 @@ class ChildAccountInfoFetcherAndroid {
 
  private:
   ChildAccountInfoFetcherAndroid(AccountFetcherService* service,
-                                 const CoreAccountId& account_id,
-                                 const std::string& account_name);
+                                 const CoreAccountInfo& account_info);
 
   base::android::ScopedJavaGlobalRef<jobject> j_child_account_info_fetcher_;
 
