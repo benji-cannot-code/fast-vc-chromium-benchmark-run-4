b7ba74c523f9104817270e3798c6454eea440d0c
Jonathan Backer <backer@chromium.org>
2021-01-18 22:23:29 +0000

    Remove CpuWritesGpuReadsCached finch flag
    This CL copies https://skia-review.googlesource.com/c/skia/+/312056 and
    uses coherent memory on mobile GPUs, which demonstrated a significant
    CPU saving on Android.
    
    Change-Id: I3ca13695a168e7b998b9ebffddab06d21f10983b
    Bug: 1167904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2636380
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844619}

--- gpu/vulkan/vulkan_util.h
+++ gpu/vulkan/vulkan_util.h
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 
+constexpr uint32_t kVendorARM = 0x13b5;
+constexpr uint32_t kVendorQualcomm = 0x5143;
+constexpr uint32_t kVendorImagination = 0x1010;
+
 struct GPUInfo;
 class VulkanInfo;
 
