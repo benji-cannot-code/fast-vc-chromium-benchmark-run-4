bf4aa3f00855b73e01aa734af2057dc5f345b1de
Colin Blundell <blundell@chromium.org>
2021-01-18 10:52:36 +0000

    [Safe Browsing] Make scopes string for access token fetches public
    This CL moves the scopes string used for safe browsing access token
    fetches into safe_browsing_token_fetcher.h so that the WebLayer
    implementation can reuse it rather than duplicating it.
    
    Bug: 1080748
    Change-Id: I14a028857fe30c70d59454287748704673461f4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627310
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844488}

--- components/safe_browsing/core/browser/sync/safe_browsing_primary_account_token_fetcher.cc
+++ components/safe_browsing/core/browser/sync/safe_browsing_primary_account_token_fetcher.cc
@@ -18,12 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace safe_browsing {
 
-namespace {
-
-const char kAPIScope[] = "https://www.googleapis.com/auth/chrome-safe-browsing";
-
-}  // namespace
-
 SafeBrowsingPrimaryAccountTokenFetcher::SafeBrowsingPrimaryAccountTokenFetcher(
     signin::IdentityManager* identity_manager)
     : identity_manager_(identity_manager),
