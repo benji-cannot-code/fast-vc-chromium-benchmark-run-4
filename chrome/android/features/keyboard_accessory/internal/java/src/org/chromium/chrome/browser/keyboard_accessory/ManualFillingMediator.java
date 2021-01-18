d41386afeb09ce9901f0ea3ed0cdd6d80589bf14
Friedrich Horschig <fhorschig@chromium.org>
2021-01-18 10:44:31 +0000

    [Android] Add traces to keyboard accessory code
    This CL does not affect any behavior.
    
    By adding traces, debugging events of jankiness becomes simpler and it
    allows to figure out which steps that rely on cached data slow down
    once the caching is reduced.
    
    Bug: None
    Change-Id: I2771e85038fd215067bb2a9aa6a2750e80e842c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2633266
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844485}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingMediator.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingMediator.java
@@ -26,6 +26,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.Px;
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.TraceEvent;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.ChromeKeyboardVisibilityDelegate;
@@ -326,6 +327,8 @@ class ManualFillingMediator extends EmptyTabObserver
             onOrientationChange();
             return;
         } else if (property == KEYBOARD_EXTENSION_STATE) {
+            TraceEvent.instant("ManualFillingMediator$KeyboardExtensionState",
+                    getNameForState(mModel.get(KEYBOARD_EXTENSION_STATE)));
             transitionIntoState(mModel.get(KEYBOARD_EXTENSION_STATE));
             return;
         } else if (property == SUPPRESSED_BY_BOTTOM_SHEET) {
@@ -343,9 +346,11 @@ class ManualFillingMediator extends EmptyTabObserver
      */
     private void transitionIntoState(@KeyboardExtensionState int extensionState) {
         if (!meetsStatePreconditions(extensionState)) return;
+        TraceEvent.begin("ManualFillingMediator#transitionIntoState");
         enforceStateProperties(extensionState);
         changeBottomControlSpaceForState(extensionState);
         updateKeyboard(extensionState);
+        TraceEvent.end("ManualFillingMediator#transitionIntoState");
     }
 
     /**
@@ -669,6 +674,24 @@ class ManualFillingMediator extends EmptyTabObserver
 
     private boolean is(@KeyboardExtensionState int state) {
         return mModel.get(KEYBOARD_EXTENSION_STATE) == state;
+    }
+
+    private static String getNameForState(@KeyboardExtensionState int state) {
+        switch (state) {
+            case HIDDEN:
+                return "HIDDEN";
+            case EXTENDING_KEYBOARD:
+                return "EXTENDING_KEYBOARD";
+            case REPLACING_KEYBOARD:
+                return "REPLACING_KEYBOARD";
+            case WAITING_TO_REPLACE:
+                return "WAITING_TO_REPLACE";
+            case FLOATING_BAR:
+                return "FLOATING_BAR";
+            case FLOATING_SHEET:
+                return "FLOATING_SHEET";
+        }
+        return null;
     }
 
     @VisibleForTesting
