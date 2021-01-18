d20d217b690833a9ab82f3b417f7ebede1dd758a
Alan Cutter <alancutter@chromium.org>
2021-01-18 05:44:27 +0000

    Fix default app ordering for Keep web app
    Only the Keep Chrome app was listed, this CL adds the Keep web app ID
    in preparation for Keep's migration over to a web app.
    
    Bug: 1167610
    Change-Id: I69f4cc750d5733f4c4668458b2c0ec42b3649e2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2635243
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844441}

--- chrome/browser/web_applications/components/web_app_id_constants.h
+++ chrome/browser/web_applications/components/web_app_id_constants.h
@@ -37,6 +37,10 @@ constexpr char kGoogleDocsAppId[] = "mpnpojknpmmopombnjdcgaaiekajbnjb";
 constexpr char kGoogleDriveAppId[] = "aghbiahbpaijignceidepookljebhfak";
 
 // Generated as: web_app::GenerateAppIdFromURL(GURL(
+//     "https://keep.google.com/?usp=installed_webapp"))
+constexpr char kGoogleKeepAppId[] = "eilembjdkfgodjkcjnpgpaenohkicgjd";
+
+// Generated as: web_app::GenerateAppIdFromURL(GURL(
 //     "https://www.google.com/maps?force=tt&source=ttpwa"))
 constexpr char kGoogleMapsAppId[] = "mnhkaebcjjhencmpkapnbdaogjamfbcj";
 
