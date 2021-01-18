7de5b1819868f560b6c2e1e0b90db953f7d103b2
Darren Shen <shend@chromium.org>
2021-01-18 00:14:40 +0000

    ime: Fix SetCompositionRange(before, after) to have correct default span
    When calling SetCompositionRange(before, after) without custom underline
    behaviour, we create a default underline across the new composition
    range.
    
    Unfortunately, there is a mistake in the calculation when there is
    selected text. Although the before/after values correctly takes into
    account text selection, the default span doesn't, which causes incorrect
    underlining.
    
    The current code for creating the default span is in the extensions
    bindings, but that part of the code doesn't have access to the text selection.
    So move the default span logic to InputMethodChromeOS, where we have access
    to the text selection.
    
    Bug: b/174631364, b/168070907
    Test: Add unit test for InputMethodChromeOS.
    Change-Id: I5726c8b106904988051079264b4638afcb43f69b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626612
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844414}

--- ui/base/ime/fake_text_input_client.cc
+++ ui/base/ime/fake_text_input_client.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/ime/fake_text_input_client.h"
 
+#include "base/check_op.h"
 #include "build/chromeos_buildflags.h"
 #include "ui/events/event_constants.h"
 #include "ui/gfx/geometry/rect.h"
@@ -18,6 +19,13 @@ FakeTextInputClient::~FakeTextInputClient() = default;
 
 void FakeTextInputClient::set_text_input_type(TextInputType text_input_type) {
   text_input_type_ = text_input_type;
+}
+
+void FakeTextInputClient::SetTextAndSelection(const base::string16& text,
+                                              gfx::Range selection) {
+  DCHECK_LE(selection_.end(), text.length());
+  text_ = text;
+  selection_ = selection;
 }
 
 void FakeTextInputClient::SetCompositionText(
@@ -81,7 +89,8 @@ ui::TextInputClient::FocusReason FakeTextInputClient::GetFocusReason() const {
 }
 
 bool FakeTextInputClient::GetTextRange(gfx::Range* range) const {
-  return false;
+  *range = gfx::Range(0, text_.length());
+  return true;
 }
 
 bool FakeTextInputClient::GetCompositionTextRange(gfx::Range* range) const {
@@ -89,7 +98,8 @@ bool FakeTextInputClient::GetCompositionTextRange(gfx::Range* range) const {
 }
 
 bool FakeTextInputClient::GetEditableSelectionRange(gfx::Range* range) const {
-  return false;
+  *range = selection_;
+  return true;
 }
 
 bool FakeTextInputClient::SetEditableSelectionRange(const gfx::Range& range) {
@@ -137,7 +147,12 @@ bool FakeTextInputClient::ShouldDoLearning() {
 bool FakeTextInputClient::SetCompositionFromExistingText(
     const gfx::Range& range,
     const std::vector<ui::ImeTextSpan>& ui_ime_text_spans) {
-  return false;
+  if (range.start() < 0 || range.end() > text_.length())
+    return false;
+
+  composition_range_ = range;
+  ime_text_spans_ = ui_ime_text_spans;
+  return true;
 }
 #endif
 
