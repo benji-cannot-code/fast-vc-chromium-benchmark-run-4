1b35282a2714d49c4894a76ad106f6d1ff054d54
Wez <wez@chromium.org>
2021-01-18 18:50:20 +0000

    [fuchsia] Bundle locale-specific resources with WebEngine.
    Previously WebEngine bundled only the en-US versions of several sets of
    string resources, and included some resources PAKs which did not appear
    to be used.  This also required WebEngine to use the
    ResourceBundle::InitSharedInstanceWithPakPath(), which is intended only
    for use in testing.
    
    WebEngine now uses ResourceBundle::InitSharedInstanceWithLocale(), and
    bundles the locale resources:
    - A Fuchsia implementation of LoadCommonResources() is provided, which
      mirrors the LoadChromeResources() implementation but loads from a
      "common_resources.pak" file in the bundle, rather than "chrome.pak".
    - //ui's DIR_LOCALES path is overridden in WebEngine to redirect to
      ${DIR_ASSETS}/locales/
    - Locale-specific resources are placed in separate PAK files under
      ${DIR_ASSETS}/locales/.
    
    Additional some unused files are removed from the bundle:
    - Two unused sets of locale-specific resources.
    - The resource PAKs are moved from |data_deps| to |deps|, to prevent
      the associated "info" files from being implicitly pulled into the FAR.
      This will be changed back once the "web_engine" target is migrated off
      of the cr_package_fuchsia() template.
    
    Bug: 1092804, 1164990
    Change-Id: I94bc1185e9005ba987b86a2ff39b921daf158858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626443
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844580}

--- ui/base/resource/resource_bundle_fuchsia.cc
+++ ui/base/resource/resource_bundle_fuchsia.cc
@@ -5,13 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/resource/resource_bundle.h"
 
+#include "base/base_paths.h"
 #include "base/macros.h"
+#include "base/path_service.h"
 #include "ui/gfx/image/image.h"
 
 namespace ui {
 
 void ResourceBundle::LoadCommonResources() {
-  LoadChromeResources();
+  constexpr char kCommonResourcesPakPath[] = "common_resources.pak";
+
+  base::FilePath asset_root;
+  bool result = base::PathService::Get(base::DIR_ASSETS, &asset_root);
+  DCHECK(result);
+
+  AddDataPackFromPath(asset_root.Append(kCommonResourcesPakPath),
+                      ui::SCALE_FACTOR_100P);
 }
 
 gfx::Image& ResourceBundle::GetNativeImageNamed(int resource_id) {
