63851323a4c70ca0728eab065a90882b3b38d22d
Luciano Pacheco <lucmult@chromium.org>
2021-01-18 23:52:19 +0000

    Files app: Rename Command to FilesCommand due to conflict with cr.ui.Command
    When running as JS modules the `cr.ui` namespace is removed causing the
    name conflict.
    
    Bug: 1133186
    Change-Id: Id52659090df89e67f9b0a3cf44d01b4a0747584b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2636076
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844633}

--- ui/file_manager/file_manager/foreground/js/dialog_action_controller.js
+++ ui/file_manager/file_manager/foreground/js/dialog_action_controller.js
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {assert, assertNotReached} from 'chrome://resources/js/assert.m.js';
 // #import {metrics} from '../../common/js/metrics.m.js';
 // #import {$} from 'chrome://resources/js/util.m.js';
-// #import {Command as CrUiCommand} from 'chrome://resources/js/cr/ui/command.m.js';
+// #import {Command} from 'chrome://resources/js/cr/ui/command.m.js';
 // clang-format on
 
 /**
--- ui/file_manager/file_manager/foreground/js/webui_command_extender.js
+++ ui/file_manager/file_manager/foreground/js/webui_command_extender.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // clang-format off
-// #import {Command as CrUiCommand} from 'chrome://resources/js/cr/ui/command.m.js';
+// #import {Command} from 'chrome://resources/js/cr/ui/command.m.js';
 // #import {dispatchPropertyChange} from 'chrome://resources/js/cr.m.js';
 // clang-format on
 
