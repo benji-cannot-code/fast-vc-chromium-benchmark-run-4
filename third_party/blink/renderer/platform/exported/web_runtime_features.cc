69fe036cea853adfa045258964933fa3b1c5a1c4
Cheick <cheickcisse@google.com>
2021-01-18 16:04:22 +0000

    [Blink] Feature flag for Shared Highlighting color change
    This CL adds a feature flag for shared highlighting color change.
    
    Bug: 1166249
    Change-Id: I355710abddc4c2c10dd9d673b5d9a21e30142f25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626270
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/master@{#844534}

--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -346,6 +346,10 @@ void WebRuntimeFeatures::EnableTextFragmentAnchor(bool enable) {
   RuntimeEnabledFeatures::SetTextFragmentIdentifiersEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnableTextFragmentColorChange(bool enable) {
+  RuntimeEnabledFeatures::SetTextFragmentColorChangeEnabled(enable);
+}
+
 void WebRuntimeFeatures::EnablePreciseMemoryInfo(bool enable) {
   RuntimeEnabledFeatures::SetPreciseMemoryInfoEnabled(enable);
 }
