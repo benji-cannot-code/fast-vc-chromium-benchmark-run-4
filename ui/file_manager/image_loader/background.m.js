61847e39398e2ed670c080f9bb5c954fdc09b9b4
Jérémie Boulic <jboulic@chromium.org>
2021-01-19 09:11:58 +0000

    Image loader: Run as JS modules
    Background.m.js replaces background.js and background_scripts.js.
    Update background.html to load the Image Loader as JS modules.
    Optimize_webui to run Image Loader as JS modules.
    
    Tests:
    Load image loader and check that there is no additional error in
    the console.
    Current integration tests pass.
    
    Bug: 1133186
    Change-Id: I3cdf9fd2a59bca080b15c3efad51739a15fa93e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2635384
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844703}

--- ui/file_manager/image_loader/background.m.js
+++ ui/file_manager/image_loader/background.m.js
@@ -1,7 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2013 The Chromium Authors. All rights reserved.
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+/**
+ * @fileoverview Entry point for Image Loader.
+ */
+
+import {ImageLoader} from './image_loader.m.js';
 
 // Load the extension.
 ImageLoader.getInstance();
