1d82ffbd708a4142300740d8ab0e87683f16f2ff
Luciano Pacheco <lucmult@chromium.org>
2021-01-19 06:25:39 +0000

    Files app: Add main.m.js
    This is a preparation to run Files app as JS modules.
    
    Bug: 1133186
    Change-Id: I0447d7c19950c69aa47ecb33f5df35a0fcb330d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2636814
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844679}

---
+++ ui/file_manager/file_manager/foreground/js/main.m.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Start point for Files app.
+ */
+
+import '../../../base/js/error_counter.m.js';
+import './metrics_start.m.js';
+
+import {util} from '../../common/js/util.m.js';
+import {FileManager} from './file_manager.m.js';
+
+
+const fileManager = new FileManager();
+window.fileManager = fileManager;
+
+fileManager.initializeCore();
+
+fileManager.initializeUI(document.body)
+    .then(() => util.testSendMessage('ready'));
