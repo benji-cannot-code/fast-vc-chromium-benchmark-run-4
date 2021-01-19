07105d673d47c17883761c0371e32036689099b1
minch <minch@chromium.org>
2021-01-19 21:37:36 +0000

    bento: Correct corner radius of new desk button
    Bug: 1168255
    Change-Id: Ie849ada4a926d8953c1ac0b6cf4aef6083bb21e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2638105
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844912}

--- ash/wm/desks/zero_state_button.cc
+++ ash/wm/desks/zero_state_button.cc
@@ -33,10 +33,13 @@ constexpr int kZeroStateNewDeskButtonWidth = 36;
 // -----------------------------------------------------------------------------
 // DeskButtonBase:
 
-DeskButtonBase::DeskButtonBase(const base::string16& text)
+DeskButtonBase::DeskButtonBase(const base::string16& text,
+                               int border_corder_radius,
+                               int corner_radius)
     : LabelButton(base::BindRepeating(&DeskButtonBase::OnButtonPressed,
                                       base::Unretained(this)),
-                  text) {
+                  text),
+      corner_radius_(corner_radius) {
   SetPaintToLayer();
   layer()->SetFillsBoundsOpaquely(false);
   SetHorizontalAlignment(gfx::ALIGN_CENTER);
@@ -46,11 +49,11 @@ DeskButtonBase::DeskButtonBase(const base::string16& text)
   SetFocusPainter(nullptr);
   SetFocusBehavior(views::View::FocusBehavior::ACCESSIBLE_ONLY);
 
-  auto border = std::make_unique<WmHighlightItemBorder>(kCornerRadius);
+  auto border = std::make_unique<WmHighlightItemBorder>(border_corder_radius);
   border_ptr_ = border.get();
   SetBorder(std::move(border));
   views::InstallRoundRectHighlightPathGenerator(this, GetInsets(),
-                                                kCornerRadius);
+                                                border_corder_radius);
 
   UpdateBorderState();
 }
@@ -67,7 +70,7 @@ void DeskButtonBase::OnPaintBackground(gfx::Canvas* canvas) {
     flags.setColor(background_color_);
     canvas->DrawRoundRect(gfx::RectF(paint_contents_only_ ? GetContentsBounds()
                                                           : GetLocalBounds()),
-                          kCornerRadius, flags);
+                          corner_radius_, flags);
   }
 }
 
@@ -133,7 +136,9 @@ void DeskButtonBase::UpdateBorderState() {
 // TODO(minch): Show the first desk's current name instead of the default name.
 ZeroStateDefaultDeskButton::ZeroStateDefaultDeskButton(DesksBarView* bar_view)
     : DeskButtonBase(
-          l10n_util::GetStringUTF16(IDS_ASH_DESKS_DESK_1_MINI_VIEW_TITLE)),
+          l10n_util::GetStringUTF16(IDS_ASH_DESKS_DESK_1_MINI_VIEW_TITLE),
+          kCornerRadius,
+          kCornerRadius),
       bar_view_(bar_view) {}
 
 const char* ZeroStateDefaultDeskButton::GetClassName() const {
@@ -162,7 +167,7 @@ void ZeroStateDefaultDeskButton::OnButtonPressed() {
 // ZeroStateNewDeskButton:
 
 ZeroStateNewDeskButton::ZeroStateNewDeskButton()
-    : DeskButtonBase(base::string16()) {
+    : DeskButtonBase(base::string16(), kCornerRadius, kCornerRadius) {
   highlight_on_hover_ = false;
 }
 
