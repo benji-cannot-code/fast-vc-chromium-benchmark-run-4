1b921b943adff3ddec2bd6aab62caf5009853fe4
Nohemi Fernandez <fernandex@chromium.org>
2021-01-19 15:19:42 +0000

    [iOS][MICe] Fix UI crash when signing in to account with Sync password.
    This patch fixes a crash that occurs when a user goes through the
    advanced Sync settings sign-in flow with an account that has a Sync
    encryption password.
    
    Starting from this patch Sync error cards are only displayed after the
    sign-in flow has been completed.
    
    Bug: 1125631
    Change-Id: I877e59e25265e78a4220fbb66b7a3855757dceb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2637575
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844771}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_mediator.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_mediator.mm
@@ -221,9 +221,9 @@ NSString* kGoogleServicesSyncErrorImage = @"google_services_sync_error";
   self.encryptionItem.accessoryType =
       hasDisclosureIndicator ? UITableViewCellAccessoryDisclosureIndicator
                              : UITableViewCellAccessoryNone;
+  [self updateEncryptionItem:NO];
   [model addItem:self.encryptionItem
       toSectionWithIdentifier:AdvancedSettingsSectionIdentifier];
-  [self updateEncryptionItem:NO];
 
   // GoogleActivityControlsItemType.
   TableViewImageItem* googleActivityControlsItem =
@@ -286,7 +286,7 @@ NSString* kGoogleServicesSyncErrorImage = @"google_services_sync_error";
     self.encryptionItem.textColor = UIColor.cr_secondaryLabelColor;
   }
   if (needsUpdate && notifyConsumer) {
-    [self.consumer reloadItem:self.self.encryptionItem];
+    [self.consumer reloadItem:self.encryptionItem];
   }
 }
 
@@ -562,7 +562,8 @@ NSString* kGoogleServicesSyncErrorImage = @"google_services_sync_error";
 // Updates the sync errors section. If |notifyConsumer| is YES, the consumer is
 // notified about model changes.
 - (void)updateSyncErrorsSection:(BOOL)notifyConsumer {
-  if (!base::FeatureList::IsEnabled(signin::kMobileIdentityConsistency)) {
+  if (!base::FeatureList::IsEnabled(signin::kMobileIdentityConsistency) ||
+      !self.syncSetupService->HasFinishedInitialSetup()) {
     return;
   }
   BOOL needsSyncErrorItemsUpdate = [self updateSyncErrorItems];
