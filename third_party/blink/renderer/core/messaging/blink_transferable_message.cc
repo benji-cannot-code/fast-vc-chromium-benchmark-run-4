f163ea1129693eda86f996cb8bae2e24a3c6c768
Maggie Cai <mxcai@chromium.org>
2021-01-19 01:23:22 +0000

    Revert "PostMessage hookup to allow payment request delegation."
    This reverts commit ce17e9266eae1041d5d52f21a057bc5b72c02a44.
    
    Reason for revert: This CL is likely the cause of crash for test
    MessagePortProviderBrowserTest.PostMessage
    in builder Linux MSan Test starting from https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/27063/blamelist
    
    Original change's description:
    > PostMessage hookup to allow payment request delegation.
    >
    > This CL adds fields to postMessage data structures that convey
    > the sender Window's intention to delegate a capability to the
    > receiver Window.  The change is behind two runtime flags: one flag
    > ("CapabilityDelegation") enables the parsing of a new postMessage
    > option for delegation, while the other flag
    > ("CapabilityDelegationPaymentRequest") enables transmission of
    > payment request delegation information to the receiver Window.
    >
    > Bug: 1130558
    > Change-Id: Ifad482bc9629ea69b9bd802d5f0f7df7c24645f5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2568627
    > Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#844548}
    
    TBR=dcheng@chromium.org,flackr@chromium.org,mustaq@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I9a6ac999124984ca5ddff2a64f6c4f4eebd56e97
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1130558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2636098
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844640}

--- third_party/blink/renderer/core/messaging/blink_transferable_message.cc
+++ third_party/blink/renderer/core/messaging/blink_transferable_message.cc
@@ -46,9 +46,6 @@ BlinkTransferableMessage BlinkTransferableMessage::FromMessageEvent(
         user_activation->hasBeenActive(), user_activation->isActive());
   }
 
-  // Capability delegation
-  result.delegate_payment_request = message_event->delegatePaymentRequest();
-
   // Blobs.
   for (const auto& blob : serialized_script_value->BlobDataHandles()) {
     result.message->BlobDataHandles().Set(
@@ -154,7 +151,6 @@ BlinkTransferableMessage BlinkTransferableMessage::FromTransferableMessage(
         message.user_activation->has_been_active,
         message.user_activation->was_active);
   }
-  result.delegate_payment_request = message.delegate_payment_request;
 
   if (!message.array_buffer_contents_array.empty()) {
     SerializedScriptValue::ArrayBufferContentsArray array_buffer_contents_array;
--- third_party/blink/renderer/core/messaging/blink_transferable_message.h
+++ third_party/blink/renderer/core/messaging/blink_transferable_message.h
@@ -40,8 +40,6 @@ struct CORE_EXPORT BlinkTransferableMessage : BlinkCloneableMessage {
 
   mojom::blink::UserActivationSnapshotPtr user_activation;
 
-  bool delegate_payment_request = false;
-
  private:
   DISALLOW_COPY_AND_ASSIGN(BlinkTransferableMessage);
 };
