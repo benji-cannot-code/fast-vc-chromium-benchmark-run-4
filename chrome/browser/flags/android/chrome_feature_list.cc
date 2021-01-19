0b34cfd2548109d741f630ec20d092bd593e0a85
Sophey Dong <sophey@chromium.org>
2021-01-19 18:27:35 +0000

    [QRCode Android] Remove unused duplicate flag.
    Bug: 1152761
    Change-Id: I40ead415dcf134f2119bc63d27a82ce07c6dfb07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2559154
    Reviewed-by: Ramya Nagarajan <ramyan@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844822}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/flags/jni_headers/ChromeFeatureList_jni.h"
 #include "chrome/browser/notifications/chime/android/features.h"
 #include "chrome/browser/performance_hints/performance_hints_features.h"
-#include "chrome/browser/share/features.h"
 #include "chrome/browser/sharing/shared_clipboard/feature_flags.h"
 #include "chrome/browser/video_tutorials/switches.h"
 #include "chrome/browser/webapps/android/features.h"
@@ -210,7 +209,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kShareButtonInTopToolbar,
     &kShareByDefaultInCCT,
     &kSharedClipboardUI,
-    &kSharingQrCodeAndroid,
     &kShoppingAssist,
     &kSpannableInlineAutocomplete,
     &kSpecialLocaleWrapper,
