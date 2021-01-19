077bae9384086120c26313305b29aec5df2306ba
cfredric <cfredric@chromium.org>
2021-01-19 19:51:38 +0000

    Remove unused optional `status` out parameter.
    Change-Id: Id88e7004eb6bb85fcbb8352ee0ff522d88702350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2637397
    Commit-Queue: Richard Coles <torne@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844849}

--- android_webview/browser/cookie_manager.cc
+++ android_webview/browser/cookie_manager.cc
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/cookie_store_factory.h"
 #include "net/cookies/canonical_cookie.h"
 #include "net/cookies/cookie_constants.h"
-#include "net/cookies/cookie_inclusion_status.h"
 #include "net/cookies/cookie_monster.h"
 #include "net/cookies/cookie_options.h"
 #include "net/cookies/cookie_store.h"
@@ -466,10 +465,8 @@ void CookieManager::SetCookieHelper(const GURL& host,
   const GURL& new_host = MaybeFixUpSchemeForSecureCookie(
       host, value, workaround_http_secure_cookies_, &should_allow_cookie);
 
-  net::CookieInclusionStatus status;
-  std::unique_ptr<net::CanonicalCookie> cc(
-      net::CanonicalCookie::Create(new_host, value, base::Time::Now(),
-                                   base::nullopt /* server_time */, &status));
+  std::unique_ptr<net::CanonicalCookie> cc(net::CanonicalCookie::Create(
+      new_host, value, base::Time::Now(), base::nullopt /* server_time */));
 
   if (!cc || !should_allow_cookie) {
     MaybeRunCookieCallback(std::move(callback), false);
