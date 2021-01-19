1c5b5d114e4057271dffd13814e844925a84ab53
Kenichi Ishibashi <bashi@chromium.org>
2021-01-19 09:13:36 +0000

    Add an instrumentation in ServiceWorkerRegistration
    We have a hypothesis that ServiceWorkerRegistration could be
    destructed between ActivateWaitingVersion() and
    ContinueActivation(). This CL adds an instrumentation to confirm
    the hypothesis.
    
    Bug: 1159778
    Change-Id: Id15e4832e6ce82cc5230dc3f907b3e90036e62de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2636007
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844704}

--- content/browser/service_worker/service_worker_registration.cc
+++ content/browser/service_worker/service_worker_registration.cc
@@ -68,6 +68,12 @@ ServiceWorkerRegistration::ServiceWorkerRegistration(
 ServiceWorkerRegistration::~ServiceWorkerRegistration() {
   DCHECK_CURRENTLY_ON(ServiceWorkerContext::GetCoreThreadId());
   DCHECK(!listeners_.might_have_observers());
+
+  // TODO(crbug.com/1159778): Remove once the bug is fixed.
+  CHECK(!in_activate_waiting_version_)
+      << "ServiceWorkerRegistration was destroyed while activating waiting "
+         "version";
+
   if (context_)
     context_->RemoveLiveRegistration(registration_id_);
 }
@@ -481,6 +487,8 @@ void ServiceWorkerRegistration::ActivateWaitingVersion(bool delay) {
   if (activating_version->is_redundant())
     return;  // Activation is no longer relevant.
 
+  in_activate_waiting_version_ = true;
+
   // "5. If exitingWorker is not null,
   if (exiting_version.get()) {
     // Whenever activation happens, evict bfcached controllees.
@@ -518,6 +526,7 @@ void ServiceWorkerRegistration::ActivateWaitingVersion(bool delay) {
   // "10. Queue a task to fire an event named activate..."
   // The browser could be shutting down. To avoid spurious start worker
   // failures, wait a bit before continuing.
+  in_activate_waiting_version_ = false;
   if (delay) {
     task_runner_->PostDelayedTask(
         FROM_HERE,
