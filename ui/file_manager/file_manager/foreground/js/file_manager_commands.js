e0592d36395e135f962a7bce66e6f98f4633c036
Luciano Pacheco <lucmult@chromium.org>
2021-01-19 00:04:51 +0000

    Files app: Remove incorrect .bind(this) and fix cr.ui.Command
    Remove incorrect .bind(this) because rollup fails because `this` is null
    in that context. These callbacks don't use `this` so it's safe to remove
    the binding.
    
    Import webui_command_extender.m.js to add the method setHidden() in
    cr.ui.Command when running as JS modules.
    
    Bug: 1133186
    Change-Id: Iac70f1364a2bf77bfbdf6eb4d3d453786519c2e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2636006
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844634}

--- ui/file_manager/file_manager/foreground/js/file_manager_commands.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands.js
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {metrics} from '../../common/js/metrics.m.js';
 // #import {assert} from 'chrome://resources/js/assert.m.js';
 // #import {Command} from 'chrome://resources/js/cr/ui/command.m.js';
+// #import './webui_command_extender.m.js';
 // clang-format on
 
 
