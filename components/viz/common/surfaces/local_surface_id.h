bdd035fa5b2b10261d2cafbd8a19bf399f45796a
Jonathan Ross <jonross@chromium.org>
2021-01-19 15:56:03 +0000

    Update Viz Flow tracing and Rotation Debug Traces
    With DCHECKs enabled base::HashInts uses a random seed, which is only
    valid in its process. This means that a given viz::LocalSurfaceId would
    have different hashes in different process. Which breaks performing
    Traces with Flow.
    
    Additionally while debugging issue 1060058 I noticed it was difficult to
    know when Viz was blocked on pending surfaces, and when rotations were
    occurring. This change adds a new trace to viz::Surface to list the
    Surface being waited on. As well as adding new traces to LayerTreeHost
    to denote the current viewport rect.
    
    Bug: 1154826
    Change-Id: I60d9f2f195ae6ca45e9eabd101df971462b34224
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2633098
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844783}

--- components/viz/common/surfaces/local_surface_id.h
+++ components/viz/common/surfaces/local_surface_id.h
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <inttypes.h>
 
 #include <iosfwd>
+#include <limits>
 #include <string>
 #include <tuple>
 
-#include "base/hash/hash.h"
 #include "base/unguessable_token.h"
 #include "components/viz/common/viz_common_export.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
@@ -112,11 +112,11 @@ class VIZ_COMMON_EXPORT LocalSurfaceId {
 
   // The |embed_trace_id| is used as the id for trace events associated with
   // embedding this LocalSurfaceId.
-  uint64_t embed_trace_id() const { return hash() << 1; }
+  uint64_t embed_trace_id() const { return persistent_hash() << 1; }
 
   // The |submission_trace_id| is used as the id for trace events associated
   // with submission of a CompositorFrame to a surface with this LocalSurfaceId.
-  uint64_t submission_trace_id() const { return (hash() << 1) | 1; }
+  uint64_t submission_trace_id() const { return (persistent_hash() << 1) | 1; }
 
   bool operator==(const LocalSurfaceId& other) const {
     return parent_sequence_number_ == other.parent_sequence_number_ &&
@@ -128,13 +128,14 @@ class VIZ_COMMON_EXPORT LocalSurfaceId {
     return !(*this == other);
   }
 
-  size_t hash() const {
-    DCHECK(is_valid()) << ToString();
-    return base::HashInts(
-        static_cast<uint64_t>(
-            base::HashInts(parent_sequence_number_, child_sequence_number_)),
-        static_cast<uint64_t>(base::UnguessableTokenHash()(embed_token_)));
-  }
+  // This implementation is fast and appropriate for a hash table lookup.
+  // However the hash differs per process, and is inappropriate for tracing.
+  size_t hash() const;
+
+  // This implementation is slow and not appropriate for a hash table lookup.
+  // However the hash is consistent across processes and can be used for
+  // tracing.
+  size_t persistent_hash() const;
 
   std::string ToString() const;
 
@@ -184,10 +185,6 @@ inline bool operator<=(const LocalSurfaceId& lhs, const LocalSurfaceId& rhs) {
 inline bool operator>=(const LocalSurfaceId& lhs, const LocalSurfaceId& rhs) {
   return !operator<(lhs, rhs);
 }
-
-struct LocalSurfaceIdHash {
-  size_t operator()(const LocalSurfaceId& key) const { return key.hash(); }
-};
 
 }  // namespace viz
 
