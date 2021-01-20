e91d764c3f9bc9d5ba8fa313fe743b67ba2142ac
Jeffrey Young <cowmoo@google.com>
2021-01-20 22:50:52 +0000

    ambient: resume when suspend is done
    This corrects two issues with suspend:
    * Ambient mode does not restart when waking up from suspend
    * Flash of old Ambient mode photo displays when opening lid from suspend
    
    Block Ambient mode from restarting after |SuspendImminent| until
    |SuspendDone| is received. Upon |SuspendDone|, also restart Ambient mode
    lock screen timeout if appropriate.
    
    Start Ambient mode, close lid while active. Reopen lid, wait 7 seconds,
    Ambient mode starts again over lock screen.
    
    BUG=b:177325727
    BUG=b:177692036
    TEST=ash_unittest --gtest_filter="AmbientControllerTest.*Suspend"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I8d52af80ab7dd49538998d0e8831ee3c10d2919a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2625727
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845384}

--- ash/ambient/ambient_controller.cc
+++ ash/ambient/ambient_controller.cc
@@ -271,7 +271,7 @@ void AmbientController::OnAutoShowTimeOut() {
   DCHECK(IsUiHidden(ambient_ui_model_.ui_visibility()));
 
   // Show ambient screen after time out.
-  ambient_ui_model_.SetUiVisibility(AmbientUiVisibility::kShown);
+  ShowUi();
 }
 
 void AmbientController::OnLockStateChanged(bool locked) {
@@ -404,6 +404,15 @@ void AmbientController::SuspendImminent(
   // closing finished.
   CloseAllWidgets(/*immediately=*/true);
   CloseUi();
+  is_suspend_imminent_ = true;
+}
+
+void AmbientController::SuspendDone(base::TimeDelta sleep_duration) {
+  is_suspend_imminent_ = false;
+  // |DismissUI| will restart the lock screen timer if lock screen is active and
+  // if Ambient mode is enabled, so call it when resuming from suspend to
+  // restart Ambient mode if applicable.
+  DismissUI();
 }
 
 void AmbientController::OnAuthScanDone(
@@ -442,6 +451,11 @@ void AmbientController::ShowUi() {
     return;
   }
 
+  if (is_suspend_imminent_) {
+    VLOG(1) << "Do not show UI when suspend imminent";
+    return;
+  }
+
   ambient_ui_model_.SetUiVisibility(AmbientUiVisibility::kShown);
 }
 
@@ -450,6 +464,11 @@ void AmbientController::ShowHiddenUi() {
 
   if (!IsAmbientModeEnabled()) {
     LOG(WARNING) << "Ambient mode is not allowed.";
+    return;
+  }
+
+  if (is_suspend_imminent_) {
+    VLOG(1) << "Do not start hidden UI when suspend imminent";
     return;
   }
 
