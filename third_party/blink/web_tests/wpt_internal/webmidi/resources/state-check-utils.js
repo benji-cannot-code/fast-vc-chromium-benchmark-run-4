9768869c279b7b6ad1d8326b666e118f02006ef3
Ken Rockot <rockot@google.com>
2021-01-20 19:25:54 +0000

    Migrate webmidi WPT to Mojo JS modules
    Bug: 1004256
    Change-Id: Id69210a2c22bd64da82492fae5da41f4eaf0da98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2636851
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845281}

--- third_party/blink/web_tests/wpt_internal/webmidi/resources/state-check-utils.js
+++ third_party/blink/web_tests/wpt_internal/webmidi/resources/state-check-utils.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-async function checkStateTransition(options) {
+export async function checkStateTransition(options) {
     const port = options.port;
     const access = options.access;
     assert_equals(options.initialconnection, port.connection);
