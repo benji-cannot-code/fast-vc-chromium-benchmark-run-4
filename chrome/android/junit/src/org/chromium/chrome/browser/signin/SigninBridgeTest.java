3361a7c33e5e39d756ca66ffe0a830236537dabd
Alice Wang <aliceywang@chromium.org>
2021-01-20 15:30:44 +0000

    [Signin] Move openAccountPickerBottomSheet() to SigninBridge
    This CL moves the method openAccountPickerBottomSheet() from
    SigninUtils to SigninBridge to facilitate modularization.
    
    Bug: 1165772
    Change-Id: Ifc47512cdb0c8b680e16a4fedd49c010a25a7359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631491
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845149}

--- chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninBridgeTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninBridgeTest.java
@@ -28,10 +28,10 @@ import org.chromium.components.signin.metrics.AccountConsistencyPromoAction;
 import org.chromium.ui.base.WindowAndroid;
 
 /**
- * This class tests the method {@link SigninUtils#openAccountPickerBottomSheet}
+ * JUnit tests for the class {@link SigninBridge}.
  */
 @RunWith(BaseRobolectricTestRunner.class)
-public class SigninUtilsAccountPickerTest {
+public class SigninBridgeTest {
     private static final String CONTINUE_URL = "https://test-continue-url.com";
 
     @Rule
@@ -66,7 +66,7 @@ public class SigninUtilsAccountPickerTest {
     @SmallTest
     public void testAccountPickerSuppressedWhenSigninNotAllowed() {
         when(mSigninManagerMock.isSignInAllowed()).thenReturn(false);
-        SigninUtils.openAccountPickerBottomSheet(mWindowAndroidMock, CONTINUE_URL);
+        SigninBridge.openAccountPickerBottomSheet(mWindowAndroidMock, CONTINUE_URL);
         checkHistogramRecording(AccountConsistencyPromoAction.SUPPRESSED_SIGNIN_NOT_ALLOWED);
     }
 
@@ -74,7 +74,7 @@ public class SigninUtilsAccountPickerTest {
     @SmallTest
     public void testAccountPickerSuppressedWhenNoAccountsOnDevice() {
         when(mSigninManagerMock.isSignInAllowed()).thenReturn(true);
-        SigninUtils.openAccountPickerBottomSheet(mWindowAndroidMock, CONTINUE_URL);
+        SigninBridge.openAccountPickerBottomSheet(mWindowAndroidMock, CONTINUE_URL);
         checkHistogramRecording(AccountConsistencyPromoAction.SUPPRESSED_NO_ACCOUNTS);
     }
 
--- chrome/browser/android/signin/signin_bridge.cc
+++ chrome/browser/android/signin/signin_bridge.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
 #include "chrome/android/chrome_jni_headers/SigninBridge_jni.h"
-#include "chrome/android/chrome_jni_headers/SigninUtils_jni.h"
 #include "ui/android/window_android.h"
 
 using base::android::JavaParamRef;
@@ -39,7 +38,7 @@ void SigninBridge::OpenAccountPickerBottomSheet(
     const std::string& continue_url) {
   DCHECK(window);
   JNIEnv* env = base::android::AttachCurrentThread();
-  Java_SigninUtils_openAccountPickerBottomSheet(
+  Java_SigninBridge_openAccountPickerBottomSheet(
       env, window->GetJavaObject(),
       base::android::ConvertUTF8ToJavaString(env, continue_url));
 }
