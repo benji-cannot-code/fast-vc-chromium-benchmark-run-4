def0ae19660b9d9a6420c88effc19f31bcf6fd79
Michael Thiessen <mthiesse@chromium.org>
2021-01-20 04:18:12 +0000

    Implement Homescreen Widget LaunchCauseMetrics
    Implements LaunchCauseMetrics for the Search and Bookmarks widgets.
    
    Bug: 1163961
    Change-Id: I87427a11be245fbb990377cf02da00b408c8fe2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2635447
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845008}

--- chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
@@ -37,6 +37,7 @@ import org.chromium.base.ApplicationStatus;
 import org.chromium.base.Callback;
 import org.chromium.base.ContentUriUtils;
 import org.chromium.base.IntentUtils;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
@@ -55,6 +56,7 @@ import org.chromium.chrome.browser.locale.DefaultSearchEngineDialogHelperUtils;
 import org.chromium.chrome.browser.locale.DefaultSearchEnginePromoDialog;
 import org.chromium.chrome.browser.locale.DefaultSearchEnginePromoDialog.DefaultSearchEnginePromoDialogObserver;
 import org.chromium.chrome.browser.locale.LocaleManager;
+import org.chromium.chrome.browser.metrics.LaunchCauseMetrics;
 import org.chromium.chrome.browser.omnibox.LocationBarCoordinator;
 import org.chromium.chrome.browser.omnibox.OmniboxSuggestionType;
 import org.chromium.chrome.browser.omnibox.UrlBar;
@@ -247,6 +249,10 @@ public class SearchActivityTest {
                 return null;
             }
         }, url);
+        Assert.assertEquals(1,
+                RecordHistogram.getHistogramValueCountForTesting(
+                        LaunchCauseMetrics.LAUNCH_CAUSE_HISTOGRAM,
+                        LaunchCauseMetrics.LaunchCause.HOME_SCREEN_WIDGET));
     }
 
     @Test
@@ -773,6 +779,7 @@ public class SearchActivityTest {
             Criteria.checkThat(tab, Matchers.notNullValue());
             Criteria.checkThat(tab.getUrlString(), Matchers.is(expectedUrl));
         });
+        mActivityTestRule.setActivity(cta);
     }
 
     private void waitForSuggestionType(final SearchActivityLocationBarLayout locationBar,
