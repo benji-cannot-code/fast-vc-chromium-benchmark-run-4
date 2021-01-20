d537e2b40b4c528c1051068d6c8e006f1f59f1ef
Tibor Goldschwendt <tiborg@chromium.org>
2021-01-20 22:49:19 +0000

    [ntp] Add flag to disable the middle slot promo
    Makes the middle slot promo Finch-configurable. Flag is enabled by
    default.
    
    Bug: 1166869
    Change-Id: If6d4e06577f99b635f04fbe406334b66f62210b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2638934
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845381}

--- chrome/browser/resources/new_tab_page/app.js
+++ chrome/browser/resources/new_tab_page/app.js
@@ -198,6 +198,12 @@ class AppElement extends PolymerElement {
       },
 
       /** @private */
+      middleSlotPromoEnabled_: {
+        type: Boolean,
+        value: () => loadTimeData.getBoolean('middleSlotPromoEnabled'),
+      },
+
+      /** @private */
       modulesEnabled_: {
         type: Boolean,
         value: () => loadTimeData.getBoolean('modulesEnabled'),
@@ -518,7 +524,8 @@ class AppElement extends PolymerElement {
    * @private
    */
   computePromoAndModulesLoaded_() {
-    return this.middleSlotPromoLoaded_ &&
+    return (!loadTimeData.getBoolean('middleSlotPromoEnabled') ||
+            this.middleSlotPromoLoaded_) &&
         (!loadTimeData.getBoolean('modulesEnabled') || this.modulesLoaded_);
   }
 
