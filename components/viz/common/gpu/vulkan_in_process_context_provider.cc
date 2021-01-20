7d86735306e388f3fd6cf7641b0a73d5174de7fb
Emircan Uysaler <emircan@chromium.org>
2021-01-20 16:13:52 +0000

    [vulkan] Adjust sync cpu memory limit based on pressure signals
    This CL sets the soft limit at 0 when we are in CRITICAL memory
    pressure. This enforces sync and cleanup in every submit call to
    reduce memory usage spikes.
    
    This CL only affects platforms that have soft limit. That happens
    if --vulkan-sync-cpu-memory-limit-mb flag is set, which is only
    enabled on fuchsia.
    
    Bug: 1166839
    
    Change-Id: I9d38cba2479a8c912d6b1e1624e9f893be8188de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2629573
    Commit-Queue: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845162}

--- components/viz/common/gpu/vulkan_in_process_context_provider.cc
+++ components/viz/common/gpu/vulkan_in_process_context_provider.cc
@@ -18,6 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/gpu/GrDirectContext.h"
 #include "third_party/skia/include/gpu/vk/GrVkExtensions.h"
 
+namespace {
+
+// Setting this limit to 0 practically forces sync at every submit.
+constexpr uint32_t kSyncCpuMemoryLimitAtMemoryPressureCritical = 0;
+
+}  // namespace
+
 namespace viz {
 
 // static
@@ -26,10 +33,12 @@ VulkanInProcessContextProvider::Create(
     gpu::VulkanImplementation* vulkan_implementation,
     uint32_t heap_memory_limit,
     uint32_t sync_cpu_memory_limit,
-    const gpu::GPUInfo* gpu_info) {
+    const gpu::GPUInfo* gpu_info,
+    base::TimeDelta cooldown_duration_at_memory_pressure_critical) {
   scoped_refptr<VulkanInProcessContextProvider> context_provider(
       new VulkanInProcessContextProvider(
-          vulkan_implementation, heap_memory_limit, sync_cpu_memory_limit));
+          vulkan_implementation, heap_memory_limit, sync_cpu_memory_limit,
+          cooldown_duration_at_memory_pressure_critical));
   if (!context_provider->Initialize(gpu_info))
     return nullptr;
   return context_provider;
@@ -38,10 +47,18 @@ VulkanInProcessContextProvider::Create(
 VulkanInProcessContextProvider::VulkanInProcessContextProvider(
     gpu::VulkanImplementation* vulkan_implementation,
     uint32_t heap_memory_limit,
-    uint32_t sync_cpu_memory_limit)
+    uint32_t sync_cpu_memory_limit,
+    base::TimeDelta cooldown_duration_at_memory_pressure_critical)
     : vulkan_implementation_(vulkan_implementation),
       heap_memory_limit_(heap_memory_limit),
-      sync_cpu_memory_limit_(sync_cpu_memory_limit) {}
+      sync_cpu_memory_limit_(sync_cpu_memory_limit),
+      cooldown_duration_at_memory_pressure_critical_(
+          cooldown_duration_at_memory_pressure_critical) {
+  memory_pressure_listener_ = std::make_unique<base::MemoryPressureListener>(
+      FROM_HERE,
+      base::BindRepeating(&VulkanInProcessContextProvider::OnMemoryPressure,
+                          base::Unretained(this)));
+}
 
 VulkanInProcessContextProvider::~VulkanInProcessContextProvider() {
   Destroy();
@@ -173,8 +190,24 @@ void VulkanInProcessContextProvider::EnqueueSecondaryCBPostSubmitTask(
   NOTREACHED();
 }
 
-uint32_t VulkanInProcessContextProvider::GetSyncCpuMemoryLimit() const {
-  return sync_cpu_memory_limit_;
+base::Optional<uint32_t> VulkanInProcessContextProvider::GetSyncCpuMemoryLimit()
+    const {
+  // Return false to indicate that there's no limit.
+  if (!sync_cpu_memory_limit_)
+    return base::Optional<uint32_t>();
+  return base::TimeTicks::Now() < critical_memory_pressure_expiration_time_
+             ? base::Optional<uint32_t>(
+                   kSyncCpuMemoryLimitAtMemoryPressureCritical)
+             : base::Optional<uint32_t>(sync_cpu_memory_limit_);
+}
+
+void VulkanInProcessContextProvider::OnMemoryPressure(
+    base::MemoryPressureListener::MemoryPressureLevel level) {
+  if (level != base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_CRITICAL)
+    return;
+
+  critical_memory_pressure_expiration_time_ =
+      base::TimeTicks::Now() + cooldown_duration_at_memory_pressure_critical_;
 }
 
 }  // namespace viz
