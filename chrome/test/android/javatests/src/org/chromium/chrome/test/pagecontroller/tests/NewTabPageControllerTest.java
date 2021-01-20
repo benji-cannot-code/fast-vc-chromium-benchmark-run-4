e31c9053ca8ae0926009e6d12a0458a9c307965d
Vincent Boisselle <vincb@chromium.org>
2021-01-20 15:55:07 +0000

    Remove unused feed-related control functions in NewTabPageController
    The NTP feed-related tests are in FeedV2NewTabPageTest, which has its own control functions.
    
    Bug: 1167893, 1091640
    Change-Id: I9498d1ce753d0a95ed357ea9f5f707022df8ad29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2635346
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/master@{#845155}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/pagecontroller/tests/NewTabPageControllerTest.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/pagecontroller/tests/NewTabPageControllerTest.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.test.pagecontroller.tests;
 
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import android.os.Build.VERSION_CODES;
@@ -23,7 +22,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.test.pagecontroller.controllers.ntp.ChromeMenu;
 import org.chromium.chrome.test.pagecontroller.controllers.ntp.NewTabPageController;
 import org.chromium.chrome.test.pagecontroller.controllers.urlpage.UrlPage;
@@ -58,14 +56,6 @@ public class NewTabPageControllerTest {
         Assert.assertTrue(mController.isCurrentPageThis());
     }
 
-    @Test
-    @DisabledTest(message = "https://crbug.com/1091640")
-    public void testHideArticles() {
-        boolean isHidden = mController.areArticlesHidden();
-        mController.toggleHideArticles();
-        assertNotEquals(isHidden, mController.areArticlesHidden());
-    }
-
     @LargeTest
     @Test
     @DisableIf.
@@ -73,8 +63,6 @@ public class NewTabPageControllerTest {
     public void testScrollPage() {
         mController.scrollToTop();
         assertTrue(mController.hasScrolledToTop());
-        mController.scrollToBottom();
-        assertTrue(mController.hasScrolledToBottom());
     }
 
     @Test
