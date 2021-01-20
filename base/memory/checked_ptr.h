8c16a0fc3b00c577ba7b81dc5ab39ecbb49b8570
Keishi Hattori <keishi@chromium.org>
2021-01-20 06:30:23 +0000

    Fix BackupRefPtr 32bit builds
    For 32 bit builds, IsManagedByPartitionAllocNormalBuckets() was declared in address_pool_manager.h
    But simply including it in checked_ptr.h would balloon the amount of dependencies for checked_ptr.h
    So I moved everything required to implement IsManagedByPartitionAlloc* for 32bit to address_pool_manager_bitmap.h
    This reduced the amount of files to exclude from the rewriter to just base/synchronization/lock_impl.h
    
    Bug: 1080832
    Change-Id: I0f07156f7afc90e49f93e90a23d5d20595d44bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2637095
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845071}

--- base/memory/checked_ptr.h
+++ base/memory/checked_ptr.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/allocator/partition_allocator/address_pool_manager_bitmap.h"
 #include "base/allocator/partition_allocator/checked_ptr_support.h"
 #include "base/allocator/partition_allocator/partition_address_space.h"
 #include "base/allocator/partition_allocator/partition_alloc_forward.h"
