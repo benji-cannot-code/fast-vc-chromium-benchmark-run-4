1baade37339a41d68564c78a443e22899748cfe6
Victor Costan <pwnall@chromium.org>
2021-01-20 07:27:13 +0000

    WebSQL: Remove OriginInfo::DBInfo.
    DBInfo now holds a single member, the database size. This CL replaces
    the OriginInfo::databases_info map holding DBInfo values with a
    `database_sizes` map.
    
    Change-Id: I62e1e56fec26a9d964190d918ccf917c42f1ddfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2634864
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845083}

--- storage/browser/database/database_quota_client_unittest.cc
+++ storage/browser/database/database_quota_client_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback_helpers.h"
+#include "base/containers/contains.h"
 #include "base/files/file_path.h"
 #include "base/location.h"
 #include "base/memory/scoped_refptr.h"
@@ -108,8 +109,8 @@ class MockDatabaseTracker : public DatabaseTracker {
     }
 
     void AddMockDatabase(const base::string16& name, int size) {
-      EXPECT_TRUE(database_info_.find(name) == database_info_.end());
-      database_info_[name].size = size;
+      EXPECT_FALSE(base::Contains(database_sizes_, name));
+      database_sizes_[name] = size;
       total_size_ += size;
     }
   };
