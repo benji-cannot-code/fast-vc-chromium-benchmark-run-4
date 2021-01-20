be327e881d19a7e19df73f4c5476b3e54322d595
Mike Jackson <mjackson@microsoft.com>
2021-01-20 19:55:04 +0000

    dpwas: Rename mayChangeRunOnOsLoginMode to mayShowRunOnOsLoginMode
    As we look towards adding a enterprise policy, we want to have an
    option to show/hide the menu item, and an option to enabled/disabled
    the menu item.
    
    This CL updates the existing parameter to indicate what the flag
    actually does.
    
    Explainer: https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/master/RunOnLogin/Explainer.md
    I2P: https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/T6d2zqF_jpw/76TP7Bc2DwAJ
    
    Bug: 897302
    Change-Id: I4ef7becd32dd003d8efaab503f0d51a7919d589a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2637329
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Mike Jackson <mjackson@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#845290}

--- chrome/browser/resources/ntp4/page_list_view.js
+++ chrome/browser/resources/ntp4/page_list_view.js
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *            launch_container: number,
  *            launch_type: number,
  *            mayChangeLaunchType: boolean,
- *            mayChangeRunOnOsLoginMode: boolean,
+ *            mayShowRunOnOsLoginMode: boolean,
  *            mayCreateShortcuts: boolean,
  *            mayDisable: boolean,
  *            name: string,
