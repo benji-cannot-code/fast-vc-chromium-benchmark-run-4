33ce886dbbd7243754a7e7fd04cd37d47db62747
Evan Stade <estade@chromium.org>
2021-01-20 18:53:29 +0000

    Move AddToHomescreenDataFetcher to //components/webapps
    The unit test is ported to be run outside of Chrome.
    
    Bug: 1164069
    Change-Id: I8c047e8497cc738ede8c20d5ae43b30089a81357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631626
    Auto-Submit: Evan Stade <estade@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845265}

--- components/webapps/browser/android/add_to_homescreen_data_fetcher.cc
+++ components/webapps/browser/android/add_to_homescreen_data_fetcher.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/android/webapps/add_to_homescreen_data_fetcher.h"
+#include "components/webapps/browser/android/add_to_homescreen_data_fetcher.h"
 
 #include <utility>
 #include <vector>
