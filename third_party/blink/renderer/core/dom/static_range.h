9e7d9aaec2ea74dbc11c1569d408a710c03bb0ef
Jaeyong Bae <jdragon.bae@gmail.com>
2021-01-20 06:40:18 +0000

    [DOM] Implement AbstractRange superclass
    This patch means implementing AbstractRange according to
    specification[1]. The bits shared between StaticRange and Range objects
    are put on a shared superclass named AbstractRange. And, make the failed
    the web test[2] successful.
    
    [1] https://dom.spec.whatwg.org/#interface-abstractrange
    [2] https://github.com/w3c/web-platform-tests/pull/9967
    
    Bug: 820947
    Change-Id: If9ea0b4d9b3f3fa0c30920e0c1972b76da995453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212290
    Commit-Queue: Jaeyong Bae <jdragon.bae@gmail.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845074}

--- third_party/blink/renderer/core/dom/static_range.h
+++ third_party/blink/renderer/core/dom/static_range.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_STATIC_RANGE_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/dom/abstract_range.h"
 #include "third_party/blink/renderer/core/dom/range.h"
 #include "third_party/blink/renderer/core/editing/forward.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
@@ -17,7 +18,7 @@ namespace blink {
 class Document;
 class ExceptionState;
 
-class CORE_EXPORT StaticRange final : public ScriptWrappable {
+class CORE_EXPORT StaticRange final : public AbstractRange {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
@@ -35,21 +36,21 @@ class CORE_EXPORT StaticRange final : public ScriptWrappable {
               Node* end_container,
               unsigned end_offset);
 
-  Node* startContainer() const { return start_container_.Get(); }
+  Node* startContainer() const override { return start_container_.Get(); }
   void setStartContainer(Node* start_container) {
     start_container_ = start_container;
   }
 
-  unsigned startOffset() const { return start_offset_; }
+  unsigned startOffset() const override { return start_offset_; }
   void setStartOffset(unsigned start_offset) { start_offset_ = start_offset; }
 
-  Node* endContainer() const { return end_container_.Get(); }
+  Node* endContainer() const override { return end_container_.Get(); }
   void setEndContainer(Node* end_container) { end_container_ = end_container; }
 
-  unsigned endOffset() const { return end_offset_; }
+  unsigned endOffset() const override { return end_offset_; }
   void setEndOffset(unsigned end_offset) { end_offset_ = end_offset; }
 
-  bool collapsed() const {
+  bool collapsed() const override {
     return start_container_ == end_container_ && start_offset_ == end_offset_;
   }
 
