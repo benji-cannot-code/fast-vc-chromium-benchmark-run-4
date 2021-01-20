6163faa3956117a54b3cbeac0fdbbba3298330cc
Caroline Rising <corising@chromium.org>
2021-01-20 16:02:09 +0000

    Read later: add metrics tracking read/unread counts on Desktop.
    Track two sets of read/unread counts, one when the reading list model
    has loaded and one when building a log for the current session using the
    DesktopPlatformFeaturesMetricsProvider.
    
    Bug: 1163938
    Change-Id: I1b23847e7f209cc2a77b57f0e344d7c9b04c546b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2621914
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845158}

--- chrome/browser/metrics/desktop_platform_features_metrics_provider.cc
+++ chrome/browser/metrics/desktop_platform_features_metrics_provider.cc
@@ -5,7 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/metrics/desktop_platform_features_metrics_provider.h"
 
+#include <vector>
+
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/read_later/reading_list_model_factory.h"
+#include "components/reading_list/core/reading_list_model.h"
+#include "components/reading_list/features/reading_list_switches.h"
 #include "ui/native_theme/native_theme.h"
 
 namespace {
@@ -35,4 +44,20 @@ void DesktopPlatformFeaturesMetricsProvider::ProvideCurrentSessionData(
                                           : DarkModeStatus::kLight;
   }
   UMA_HISTOGRAM_ENUMERATION("Browser.DarkModeStatus", status);
+
+  // Record how many items are in the reading list.
+  if (base::FeatureList::IsEnabled(reading_list::switches::kReadLater)) {
+    std::vector<Profile*> profiles =
+        g_browser_process->profile_manager()->GetLoadedProfiles();
+    for (Profile* profile : profiles) {
+      ReadingListModel* model =
+          ReadingListModelFactory::GetForBrowserContext(profile);
+      if (model && model->loaded()) {
+        UMA_HISTOGRAM_COUNTS_1000("ReadingList.Unread.Count.OnUMAUpload",
+                                  model->unread_size());
+        UMA_HISTOGRAM_COUNTS_1000("ReadingList.Read.Count.OnUMAUpload",
+                                  model->size() - model->unread_size());
+      }
+    }
+  }
 }
