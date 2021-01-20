22428a37f78d3d9240dfcc1b0b0669c158f6c83a
Dan Harrington <harringtond@chromium.org>
2021-01-20 23:37:51 +0000

    Enable lottie capability
    We're no longer blocked on using lottie, see crbug.com/1116501
    I was able to confirm a lottie animation works.
    
    Bug: 1158468
    Change-Id: I05724d6f24630195e8eed0ec985edcc6792e9337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2636848
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845404}

--- components/feed/core/v2/proto_util_unittest.cc
+++ components/feed/core/v2/proto_util_unittest.cc
@@ -59,6 +59,7 @@ TEST(ProtoUtilTest, DefaultCapabilities) {
       request.client_capability(),
       testing::UnorderedElementsAre(
           feedwire::Capability::BASE_UI, feedwire::Capability::REQUEST_SCHEDULE,
+          feedwire::Capability::LOTTIE_ANIMATIONS,
           feedwire::Capability::OPEN_IN_TAB, feedwire::Capability::CARD_MENU,
           feedwire::Capability::DOWNLOAD_LINK,
           feedwire::Capability::INFINITE_FEED,
@@ -100,6 +101,7 @@ TEST(ProtoUtilTest, DisableCapabilitiesWithFinch) {
       request.client_capability(),
       testing::UnorderedElementsAre(
           feedwire::Capability::BASE_UI, feedwire::Capability::REQUEST_SCHEDULE,
+          feedwire::Capability::LOTTIE_ANIMATIONS,
           feedwire::Capability::OPEN_IN_TAB, feedwire::Capability::CARD_MENU,
           feedwire::Capability::DOWNLOAD_LINK,
           feedwire::Capability::DISMISS_COMMAND,
