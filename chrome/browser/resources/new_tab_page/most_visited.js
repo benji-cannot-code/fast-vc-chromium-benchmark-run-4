2bbf7bb278a9cd1bf786ae057f462125172f606a
Esmael El-Moslimany <aee@chromium.org>
2021-01-20 06:12:17 +0000

    WebUI NewTab: most-visited tiles are draggable
    Bug: 1139032
    Change-Id: Icea07c79fbad8bf34664f1ce7ce53380beaadd2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2637262
    Auto-Submit: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845051}

--- chrome/browser/resources/new_tab_page/most_visited.js
+++ chrome/browser/resources/new_tab_page/most_visited.js
@@ -669,6 +669,9 @@ class MostVisitedElement extends PolymerElement {
    * @private
    */
   onDragStart_(e) {
+    if (!this.customLinksEnabled_) {
+      return;
+    }
     // |dataTransfer| is null in tests.
     if (e.dataTransfer) {
       // Remove the ghost image that appears when dragging.
@@ -916,15 +919,6 @@ class MostVisitedElement extends PolymerElement {
     this.pageHandler_.onMostVisitedTilesRendered(
         this.tiles_.slice(0, assert(this.maxVisibleTiles_)),
         BrowserProxy.getInstance().now());
-  }
-
-  /**
-   * @param {boolean} value
-   * @return {string} String representing the given boolean.
-   * @private
-   */
-  booleanToString_(value) {
-    return Boolean(value).toString();
   }
 }
 
