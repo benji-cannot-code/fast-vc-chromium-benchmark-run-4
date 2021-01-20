0dec54f510c05261bbc97ff0fa89828637b9a9a4
Jun Mukai <mukai@chromium.org>
2021-01-20 06:17:35 +0000

    Make autotestPrivate.setClipboardTextData synchronous
    The ChromeOS clipboard used to be synchronously written within
    the browser process, but now it is asynchronously updated
    (see ui/base/clipboard/clipboard_ozone.cc for the details).
    
    Because of that, autotestPrivate.setClipboardTextData may return
    earlier than the clipboard data is actually set occasionally.
    To deal with such case, this CL introduces a clipboard observer
    to ensure that the clipboard is written.
    
    Bug: 1167256
    Test: the new test cases
    Change-Id: I167be3aee090465e30951fbb7a9db688429059c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2634121
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845060}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/rotator/screen_rotation_animator_observer.h"
 #include "base/compiler_specific.h"
 #include "base/optional.h"
+#include "base/scoped_observation.h"
 #include "base/scoped_observer.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/chromeos/printing/cups_printers_manager.h"
@@ -379,14 +380,24 @@ class AutotestPrivateGetClipboardTextDataFunction : public ExtensionFunction {
   ResponseAction Run() override;
 };
 
-class AutotestPrivateSetClipboardTextDataFunction : public ExtensionFunction {
+class AutotestPrivateSetClipboardTextDataFunction
+    : public ExtensionFunction,
+      public ui::ClipboardObserver {
  public:
+  AutotestPrivateSetClipboardTextDataFunction();
+
   DECLARE_EXTENSION_FUNCTION("autotestPrivate.setClipboardTextData",
                              AUTOTESTPRIVATE_SETCLIPBOARDTEXTDATA)
 
  private:
   ~AutotestPrivateSetClipboardTextDataFunction() override;
   ResponseAction Run() override;
+
+  // ui::ClipboardObserver:
+  void OnClipboardDataChanged() override;
+
+  base::ScopedObservation<ui::ClipboardMonitor, ui::ClipboardObserver>
+      observation_{this};
 };
 
 class AutotestPrivateSetCrostiniEnabledFunction : public ExtensionFunction {
