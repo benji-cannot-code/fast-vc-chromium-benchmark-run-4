700aff364863d887f1f977882011fc70b33fa151
Rainhard Findling <rainhard@chromium.org>
2021-01-20 11:09:12 +0000

    Settings: enable PrivacyPage's PrivacySandbox test suite
    Bug: 1152336
    Change-Id: I3ac333ff8fd31838c270dd5e0814c4c017850e71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2639758
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845130}

--- chrome/test/data/webui/settings/privacy_page_test.js
+++ chrome/test/data/webui/settings/privacy_page_test.js
@@ -167,6 +167,7 @@ suite('PrivacySandboxSettingsEnabled', function() {
     page = /** @type {!SettingsPrivacyPageElement} */
         (document.createElement('settings-privacy-page'));
     document.body.appendChild(page);
+    return flushTasks();
   });
 
   test('privacySandboxRowVisible', function() {
