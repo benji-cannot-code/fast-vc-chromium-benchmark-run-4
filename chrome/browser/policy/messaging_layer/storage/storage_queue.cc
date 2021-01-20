86b72a20355012671d3654701e5a83dac29e8a10
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-01-20 18:49:10 +0000

    Revert "Fix bug in reservation monitoring."
    This reverts commit c072c8c746eb099f37190e4c6c5a39c4c947b68e.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 845219 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2MwNzJjOGM3NDZlYjA5OWYzNzE5MGU0YzZjNWEzOWM0Yzk0N2I2OGUM
    
    Sample Failed Build: https://ci.chromium.org/b/8857563414825836112
    
    Sample Failed Step: bf_cache_unit_tests
    
    Original change's description:
    > Fix bug in reservation monitoring.
    > 
    > In ScopedReservation destructor did Reserve instead of Discard!
    > This is now fixed, and unit tests provided for this and other cases.
    > Also, Storage tests now verify that all memory has been released after
    > the storage is reset.
    > 
    > Bug: b:174509632
    > Change-Id: Ic3594f569f53c103d6ae9c6a73bd030c5f25d698
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2638220
    > Commit-Queue: Zach Trudo <zatrudo@google.com>
    > Reviewed-by: Zach Trudo <zatrudo@google.com>
    > Auto-Submit: Leonid Baraz <lbaraz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#845219}
    
    
    Change-Id: I453d14fd867294c134b72a40331ffd98a7a6e12c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b:174509632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2639575
    Cr-Commit-Position: refs/heads/master@{#845259}

--- chrome/browser/policy/messaging_layer/storage/storage_queue.cc
+++ chrome/browser/policy/messaging_layer/storage/storage_queue.cc
@@ -140,8 +140,10 @@ StorageQueue::~StorageQueue() {
 
   // Stop upload timer.
   upload_timer_.AbandonAndStop();
-  // Close all opened files.
-  files_.clear();
+  // CLose all opened files.
+  for (auto& file : files_) {
+    file.second->Close();
+  }
 }
 
 Status StorageQueue::Init() {
@@ -1317,7 +1319,6 @@ StorageQueue::SingleFile::SingleFile(const base::FilePath& filename,
     : filename_(filename), size_(size) {}
 
 StorageQueue::SingleFile::~SingleFile() {
-  Close();
   handle_.reset();
 }
 
@@ -1355,10 +1356,8 @@ void StorageQueue::SingleFile::Close() {
   }
   handle_.reset();
   is_readonly_ = base::nullopt;
-  if (buffer_) {
-    buffer_.reset();
-    GetMemoryResource()->Discard(buffer_size_);
-  }
+  buffer_.reset();
+  GetMemoryResource()->Discard(buffer_size_);
 }
 
 Status StorageQueue::SingleFile::Delete() {
--- chrome/browser/policy/messaging_layer/storage/storage_queue_unittest.cc
+++ chrome/browser/policy/messaging_layer/storage/storage_queue_unittest.cc
@@ -16,9 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/synchronization/waitable_event.h"
 #include "base/test/task_environment.h"
 #include "chrome/browser/policy/messaging_layer/encryption/test_encryption_module.h"
-#include "chrome/browser/policy/messaging_layer/storage/resources/resource_interface.h"
 #include "chrome/browser/policy/messaging_layer/storage/storage_configuration.h"
 #include "chrome/browser/policy/messaging_layer/util/status.h"
 #include "chrome/browser/policy/messaging_layer/util/statusor.h"
@@ -289,8 +289,6 @@ class StorageQueueTest : public ::testing::TestWithParam<size_t> {
   void ResetTestStorageQueue() {
     task_environment_.RunUntilIdle();
     storage_queue_.reset();
-    // Make sure all memory is deallocated.
-    ASSERT_THAT(GetMemoryResource()->GetUsed(), Eq(0u));
   }
 
   void InjectFailures(std::initializer_list<int64_t> seq_numbers) {
