39aa8964c992f0f9fcdad78a1a3d15bf91760359
Sam Goto <goto@chromium.org>
2021-01-20 04:10:23 +0000

    [webid] Allow idp_endpoint to be a relative URL
    The idp_endpoint property today of the .well-known/webid response is now
    taken as an absolute URL, which makes it inconvenient for deployment.
    
    In this CL we resolve the idp_endpoint URL as a relative URL to the file
    that it is contained in, allowing both absolute URLs and relative URLs
    to co-exist.
    
    Bug: 1141125
    Change-Id: I224274634e5eaf7f7d66d044931fe5823f346829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631295
    Auto-Submit: Sam Goto <goto@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#844993}

--- content/browser/webid/idp_network_request_manager.cc
+++ content/browser/webid/idp_network_request_manager.cc
@@ -23,7 +23,6 @@ namespace content {
 namespace {
 // TODO(kenrb): These need to be defined in the explainer or draft spec and
 // referenced here.
-constexpr char kWellKnownFilePath[] = ".well-known/webid";
 
 // Well-known configuration keys.
 constexpr char kIdpEndpointKey[] = "idp_endpoint";
@@ -82,6 +81,9 @@ scoped_refptr<network::SharedURLLoaderFactory> GetUrlLoaderFactory(
 }  // namespace
 
 // static
+constexpr char IdpNetworkRequestManager::kWellKnownFilePath[];
+
+// static
 std::unique_ptr<IdpNetworkRequestManager> IdpNetworkRequestManager::Create(
     const GURL& provider,
     RenderFrameHost* host) {
@@ -106,7 +108,8 @@ void IdpNetworkRequestManager::FetchIDPWellKnown(
   idp_well_known_callback_ = std::move(callback);
 
   const url::Origin& idp_origin = url::Origin::Create(provider_);
-  GURL target_url = idp_origin.GetURL().Resolve(kWellKnownFilePath);
+  GURL target_url =
+      idp_origin.GetURL().Resolve(IdpNetworkRequestManager::kWellKnownFilePath);
 
   net::NetworkTrafficAnnotationTag traffic_annotation =
       CreateTrafficAnnotation();
