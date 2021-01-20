e0028dfd61825271715db2ea95bab26e85dcfdca
Dirk Pranke <dpranke@google.com>
2021-01-20 17:05:06 +0000

    Fix python3 issue in calculate_package_deps.
    This addresses a 2->3 issue w/ print statements in
    calculate_package_deps.py.
    
    Bug: 1112471
    Change-Id: I29e7e778bb8843a14fafea23e40fb754d959b772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2638722
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845183}

--- chrome/installer/linux/debian/calculate_package_deps.py
+++ chrome/installer/linux/debian/calculate_package_deps.py
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 are satisfiable on all supported debian-based distros.
 """
 
+from __future__ import print_function
+
 import argparse
 import json
 import os
@@ -56,7 +58,7 @@ elif arch == 'mips64el':
   cmd.extend(['-l%s/usr/lib/mips64el-linux-gnuabi64' % sysroot,
               '-l%s/lib/mips64el-linux-gnuabi64' % sysroot])
 else:
-  print 'Unsupported architecture ' + arch
+  print('Unsupported architecture ' + arch)
   sys.exit(1)
 cmd.extend(['-l%s/usr/lib' % sysroot, '-O', '-e', binary])
 
@@ -65,8 +67,8 @@ proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
 (stdout, stderr) = proc.communicate()
 exit_code = proc.wait()
 if exit_code != 0:
-  print 'dpkg-shlibdeps failed with exit code ' + str(exit_code)
-  print 'stderr was ' + stderr
+  print('dpkg-shlibdeps failed with exit code ' + str(exit_code))
+  print('stderr was ' + stderr)
   sys.exit(1)
 
 SHLIBS_DEPENDS_PREFIX = 'shlibs:Depends='
@@ -99,9 +101,10 @@ if distro_check:
           dep_satisfiable = True
           break
       if not dep_satisfiable:
-        print >> sys.stderr, (
+        print(
             'Dependency %s not satisfiable on distro %s caused by binary %s' % (
-                interval_set.formatted(), distro, os.path.basename(binary)))
+                interval_set.formatted(), distro, os.path.basename(binary)),
+            file=sys.stderr)
         ret_code = 1
 if ret_code == 0:
   with open(dep_filename, 'w') as dep_file:
