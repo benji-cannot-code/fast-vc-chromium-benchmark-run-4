c4d0fb8378680f71d477d9a592e632e67686baaf
adamta <adamta@google.com>
2021-01-20 23:52:57 +0000

    [iOS] Better handling of content suggestions height changes in NTP
    Uses the content suggestions content height to adjust the feed inset and
    offset correctly. This revealed that the content suggestions height was
    not taking the safe area insets into consideration, so they are now
    handled across the refactored NTP.
    
    Also fixes occasional crash when changing between NTPs by ensuring that
    the fake omnibox doesn't have multiple parent views/VCs at once.
    
    Bug: 1114792
    Change-Id: I458365dc5c494fcc3155cff53e9a368988d83538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2635450
    Commit-Queue: Adam Trudeau-Arcaro <adamta@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845411}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -405,8 +405,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (CGFloat)headerInsetForOverscrollActionsController:
     (OverscrollActionsController*)controller {
+  CGFloat topInset =
+      self.discoverFeedWrapperViewController.view.safeAreaInsets.top;
   return self.contentSuggestionsCoordinator.viewController.collectionView
-      .contentSize.height;
+             .contentSize.height +
+         topInset;
 }
 
 - (CGFloat)headerHeightForOverscrollActionsController:
