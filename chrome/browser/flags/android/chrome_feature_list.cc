8cc9ea5e4cb817d3e6a70ebed443682373a3e55a
Ramya Nagarajan <ramyan@chromium.org>
2021-01-20 21:24:49 +0000

    Enable ChromeSharingHubV15 and ChromeShareQRCode by default.
    Bug: 1098078, 1004336
    Change-Id: Ic237293ceaa635723b6dda215d9b0bb16d1e20f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2637453
    Reviewed-by: Ramya Nagarajan <ramyan@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: Ramya Nagarajan <ramyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845333}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -411,7 +411,7 @@ const base::Feature kChromeShareLongScreenshot{
     "ChromeShareLongScreenshot", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kChromeShareQRCode{"ChromeShareQRCode",
-                                       base::FEATURE_DISABLED_BY_DEFAULT};
+                                       base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kChromeShareScreenshot{"ChromeShareScreenshot",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
@@ -420,7 +420,7 @@ const base::Feature kChromeSharingHub{"ChromeSharingHub",
                                       base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kChromeSharingHubV15{"ChromeSharingHubV15",
-                                         base::FEATURE_DISABLED_BY_DEFAULT};
+                                         base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kChromeStartupDelegate{"ChromeStartupDelegate",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
