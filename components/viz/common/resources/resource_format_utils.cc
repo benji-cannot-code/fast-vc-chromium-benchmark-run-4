60144fa5d25ff83f3852ad0ee12f1c39c5f149ab
Sunny Sachanandani <sunnyps@chromium.org>
2021-01-20 21:46:12 +0000

    viz: Mark RG_16EXT as not supported as a GpuMemoryBuffer format
    https://crrev.com/c/2626100 added RG_16EXT as a viz::ResourceFormat, and
    accidentally marked it as a supported GpuMemoryBuffer format too.  This
    causes crashes on ChromeOS when playing VP9.2 HDR video.
    
    Bug: 1116101, b/177910735
    Change-Id: Iaed4ffefa42bcca4b6bd1885d7e090d9f2782b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2639578
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845342}

--- components/viz/common/resources/resource_format_utils.cc
+++ components/viz/common/resources/resource_format_utils.cc
@@ -355,7 +355,6 @@ bool IsGpuMemoryBufferFormatSupported(ResourceFormat format) {
     case BGRA_8888:
     case RED_8:
     case R16_EXT:
-    case RG16_EXT:
     case RGBA_4444:
     case RGBA_8888:
     case RGBA_1010102:
@@ -371,6 +370,7 @@ bool IsGpuMemoryBufferFormatSupported(ResourceFormat format) {
     case LUMINANCE_F16:
     case BGR_565:
     case RG_88:
+    case RG16_EXT:
     case RGBX_8888:
     case BGRX_8888:
     case YVU_420:
