1a1351fa2b165faa95b58bb5b8d2c486293e6f3e
Ramin Halavati <rhalavati@chromium.org>
2021-01-21 18:11:47 +0000

    Add metrics for signin in Ephemeral Guest profiles.
    "Profile.EphemeralGuest.Signin" metric is recorded when user signs in
    to or signs out from a GAIA account in an ephemeral Guest profile.
    
    Bug: 1167474, 1148695
    Change-Id: Ib0aa6660c2663083b740a6c393243a0058be78c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2632694
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845705}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefs/pref_metrics_service.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
 #include "chrome/browser/profiles/gaia_info_update_service_factory.h"
+#include "chrome/browser/profiles/guest_signin_observer_factory.h"
 #include "chrome/browser/profiles/renderer_updater_factory.h"
 #include "chrome/browser/safe_browsing/certificate_reporting_service_factory.h"
 #include "chrome/browser/search/suggestions/suggestions_service_factory.h"
@@ -239,6 +240,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   AboutSigninInternalsFactory::GetInstance();
 #if !defined(OS_ANDROID)
   AccessContextAuditServiceFactory::GetInstance();
+  GuestSigninObserverFactory::GetInstance();
 #endif
   AccountConsistencyModeManagerFactory::GetInstance();
   AccountInvestigatorFactory::GetInstance();
