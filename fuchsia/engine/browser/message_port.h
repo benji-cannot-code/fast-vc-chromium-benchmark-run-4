e99e26db156117c9de74e072f56453c8e6784d5d
Kevin Marshall <kmarshall@chromium.org>
2021-01-21 22:25:39 +0000

    [fuchsia] Make message_port.h into a private library for WebEngine.
    The MessagePort conversion functions are no longer used outside of
    WebEngine, now that previous clients have migrated to
    use //components/cast/message_port instead.
    
    Remove vestigial dep on now-removed 'message_port' target.
    
    Bug: 1081525
    Change-Id: Ie3629ff1542b96bfe9594cc5dcb1ef15c671c9c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643398
    Auto-Submit: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Sharon Yang <yangsharon@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845821}

--- fuchsia/engine/browser/message_port.h
+++ fuchsia/engine/browser/message_port.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef FUCHSIA_BASE_MESSAGE_PORT_H_
-#define FUCHSIA_BASE_MESSAGE_PORT_H_
+#ifndef FUCHSIA_ENGINE_BROWSER_MESSAGE_PORT_H_
+#define FUCHSIA_ENGINE_BROWSER_MESSAGE_PORT_H_
 
 #include <fuchsia/web/cpp/fidl.h>
 #include <lib/fidl/cpp/interface_handle.h>
@@ -47,4 +47,4 @@ base::Optional<fuchsia::web::WebMessage> FidlWebMessageFromBlink(
 
 }  // namespace cr_fuchsia
 
-#endif  // FUCHSIA_BASE_MESSAGE_PORT_H_
+#endif  // FUCHSIA_ENGINE_BROWSER_MESSAGE_PORT_H_
