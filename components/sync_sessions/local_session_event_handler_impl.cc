19caf57dd224b2d205c3529c3cd1b2b80ca93ec4
Marc Treib <treib@chromium.org>
2021-01-21 17:28:25 +0000

    Extend/expire Sync histograms
    Sync.URLFetchTimedOut is extended by 6 months. I also updated the
    description to point out a (small) caveat with the numbers.
    Sync.SessionTabs is marked as obsolete.
    
    Bug: 1160835
    Change-Id: If5fd2501742197aa0c09fa1794b828705277a03c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2640035
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845687}

--- components/sync_sessions/local_session_event_handler_impl.cc
+++ components/sync_sessions/local_session_event_handler_impl.cc
@@ -11,9 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
-#include "base/metrics/histogram_macros.h"
-#include "base/strings/stringprintf.h"
-#include "build/build_config.h"
 #include "components/sync/model/sync_change.h"
 #include "components/sync/protocol/sync.pb.h"
 #include "components/sync_sessions/switches.h"
@@ -158,11 +155,6 @@ void LocalSessionEventHandlerImpl::AssociateWindows(ReloadTabsOption option,
 
   for (auto& window_iter_pair : windows) {
     const SyncedWindowDelegate* window_delegate = window_iter_pair.second;
-    if (option == RELOAD_TABS) {
-      UMA_HISTOGRAM_COUNTS_1M("Sync.SessionTabs",
-                              window_delegate->GetTabCount());
-    }
-
     // Make sure the window is viewable and is not about to be closed. The
     // viewable window check is necessary because, for example, when a browser
     // is closed the destructor is not necessarily run immediately. This means
