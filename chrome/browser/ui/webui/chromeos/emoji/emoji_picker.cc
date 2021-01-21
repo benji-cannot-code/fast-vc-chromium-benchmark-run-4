ac2b66e9df2d1d97207449049fb93c537072328b
Kenton Lam <kentonlam@google.com>
2021-01-21 04:37:25 +0000

    Create browser tests for chrome://emoji-picker webui.
    Currently, there is basic testing of the <emoji-picker> component
    to make sure that clicking the tab icons correctly activates and
    deactivates the tabs.
    
    Change-Id: I29762bba9dec60fc7260311247df68c44fd22d03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631395
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Kenton Lam <kentonlam@google.com>
    Cr-Commit-Position: refs/heads/master@{#845517}

--- chrome/browser/ui/webui/chromeos/emoji/emoji_picker.cc
+++ chrome/browser/ui/webui/chromeos/emoji/emoji_picker.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/emoji_picker_resources_map.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "ui/resources/grit/webui_generated_resources.h"
 
 #include "chrome/browser/ui/webui/chromeos/emoji/emoji_dialog.h"
 #include "chrome/browser/ui/webui/chromeos/emoji/emoji_handler.h"
@@ -36,6 +37,10 @@ EmojiPicker::EmojiPicker(content::WebUI* web_ui)
       html_source.get(),
       base::make_span(kEmojiPickerResources, kEmojiPickerResourcesSize),
       IDR_EMOJI_PICKER_INDEX_HTML);
+
+  html_source->AddResourcePath("test_loader.js", IDR_WEBUI_JS_TEST_LOADER_JS);
+  html_source->AddResourcePath("test_loader.html",
+                               IDR_WEBUI_HTML_TEST_LOADER_HTML);
 
   // Attach message handler to handle emoji click events.
   web_ui->AddMessageHandler(std::make_unique<EmojiHandler>());
