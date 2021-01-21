13edbd25aa8b390d5834250aac7f7d4a918fe278
Xianzhu Wang <wangxianzhu@chromium.org>
2021-01-21 21:26:43 +0000

    Reland "Cleanup for LayoutView overflow clips"
    This reverts commit 17de31872d0621fe21e4599df29102b471d08225.
    
    Reason for revert: the root cause of crbug.com/1164610 has been fixed
    by https://chromium-review.googlesource.com/c/chromium/src/+/2636883.
    
    This is a clean reland of the original CL.
    
    Original change's description:
    > Revert "Cleanup for LayoutView overflow clips"
    >
    > This reverts commit 49dd6b58b0a58a8f5e45766f10de904710e0b3de.
    >
    > Reason for revert: The scale of the devtool override frame size seems
    > incorrect, causing crbug.com/1164610.
    >
    > Original change's description:
    > > Cleanup for LayoutView overflow clips
    > >
    > > 1. Move the code checking requirement of overflow clip for printing
    > >    from PaintPropertyTreeBuilder into LocalFrame::ClipsContent().
    > >
    > > 2. Combine LocalFrameView::ClipPaintRect() into
    > >    LayoutView::ViewRect(), so that all code paths calling ViewRect()
    > >    (e.g. LayoutView::OverflowClipRect()) can be dev tool viewport
    > >    overriding aware. This means that the overriding viewport is
    > >    reflected in the paint property tree, and
    > >    CompositedLayerMapping::RecomputeInterestRect() no longer needs to
    > >    apply the viewport clip separately.
    > >
    > > Change-Id: Ib743a891e4d4fc0749ff799a04ed0f09757e9f5b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613388
    > > Reviewed-by: Stefan Zager <szager@chromium.org>
    > > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#841701}
    >
    > Bug: 1164610
    >
    > TBR=szager@chromium.org,wangxianzhu@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    >
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Change-Id: I18249c79339935b381ae3a05984d5f139d5194a7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2622514
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Stefan Zager <szager@chromium.org>
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Commit-Queue: Stefan Zager <szager@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#842140}
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    TBR=szager@chromium.org,wangxianzhu@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Bug: 1164610
    Change-Id: I38bd98f55387ac85e441ab3cfa4dc7a310e1c9e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628315
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845804}

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -2042,6 +2042,9 @@ bool LocalFrame::ClipsContent() const {
   if (GetDocument()->IsPaintingPreview())
     return false;
 
+  if (ShouldUsePrintingLayout())
+    return false;
+
   if (IsMainFrame())
     return GetSettings()->GetMainFrameClipsContent();
   // By default clip to viewport.
