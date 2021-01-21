e1dc82a19d63442f92f9f561d8826bd0b65c95d4
Elly Fong-Jones <ellyjones@chromium.org>
2021-01-21 22:45:47 +0000

    Revert "[Controls Refresh] Bug fix for Datalist uses wrong colors for HC black"
    This reverts commit 1935a9b5e94b9b07474ccc9251a0c7ea5b032b3e.
    
    Reason for revert: Strongly suspected cause of crbug.com/1167118
    
    Original change's description:
    > [Controls Refresh] Bug fix for Datalist uses wrong colors for HC black
    > and HC white
    >
    > This CL fixes an issue in which, when in ForcedColors mode, the data
    > list control uses an the default color scheme
    >
    > Bug: 1157185
    > Change-Id: I5103d371fe69226be4ee495f237dd23c874f23f7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2623150
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Commit-Queue: Sam Sebree <sasebree@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#842778}
    
    TBR=ellyjones@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,sasebree@microsoft.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1157185
    Change-Id: I67167385980f3dca8a35e0b72f3ed6b00bc7d647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643459
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845824}

--- chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/platform/ax_platform_node.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/color_palette.h"
+#include "ui/native_theme/native_theme.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/border.h"
 #include "ui/views/bubble/bubble_border.h"
@@ -39,37 +40,23 @@ int AutofillPopupBaseView::GetCornerRadius() {
 }
 
 SkColor AutofillPopupBaseView::GetBackgroundColor() {
-  return GetHighContrastAwareColor(
-      ui::NativeTheme::kColorId_DropdownBackgroundColor,
-      ui::NativeTheme::SystemThemeColor::kButtonFace);
+  return GetNativeTheme()->GetSystemColor(
+      ui::NativeTheme::kColorId_DropdownBackgroundColor);
 }
 
 SkColor AutofillPopupBaseView::GetForegroundColor() {
-  return GetHighContrastAwareColor(
-      ui::NativeTheme::kColorId_DropdownForegroundColor,
-      ui::NativeTheme::SystemThemeColor::kButtonText);
+  return GetNativeTheme()->GetSystemColor(
+      ui::NativeTheme::kColorId_DropdownForegroundColor);
 }
 
 SkColor AutofillPopupBaseView::GetSelectedBackgroundColor() {
-  return GetHighContrastAwareColor(
-      ui::NativeTheme::kColorId_DropdownSelectedBackgroundColor,
-      ui::NativeTheme::SystemThemeColor::kButtonText);
+  return GetNativeTheme()->GetSystemColor(
+      ui::NativeTheme::kColorId_DropdownSelectedBackgroundColor);
 }
 
 SkColor AutofillPopupBaseView::GetSelectedForegroundColor() {
-  return GetHighContrastAwareColor(
-      ui::NativeTheme::kColorId_DropdownSelectedForegroundColor,
-      ui::NativeTheme::SystemThemeColor::kButtonFace);
-}
-
-SkColor AutofillPopupBaseView::GetHighContrastAwareColor(
-    ui::NativeTheme::ColorId non_hc_color,
-    ui::NativeTheme::SystemThemeColor hc_color) {
-  if (ui::NativeTheme::GetInstanceForWeb()->UserHasContrastPreference()) {
-    return *GetNativeTheme()->GetSystemThemeColor(hc_color);
-  } else {
-    return GetNativeTheme()->GetSystemColor(non_hc_color);
-  }
+  return GetNativeTheme()->GetSystemColor(
+      ui::NativeTheme::kColorId_DropdownSelectedForegroundColor);
 }
 
 SkColor AutofillPopupBaseView::GetFooterBackgroundColor() {
