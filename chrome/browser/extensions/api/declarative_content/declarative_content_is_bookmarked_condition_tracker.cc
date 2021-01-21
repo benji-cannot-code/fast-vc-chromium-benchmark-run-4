b0acc64924361bab23337fa48eb74b0168d8891d
Maksim Moskvitin <mmoskvitin@google.com>
2021-01-21 17:28:28 +0000

    [CodeHealth] Convert c/b/e/api/declarative_content to use modern callback
    Bug: 1152279
    Change-Id: I7c0855e4d362934d7273ca44973dd5e0cce62cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2640538
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#845688}

--- chrome/browser/extensions/api/declarative_content/declarative_content_is_bookmarked_condition_tracker.cc
+++ chrome/browser/extensions/api/declarative_content/declarative_content_is_bookmarked_condition_tracker.cc
@@ -36,8 +36,7 @@ bool HasBookmarkAPIPermission(const Extension* extension) {
 //
 
 DeclarativeContentIsBookmarkedPredicate::
-~DeclarativeContentIsBookmarkedPredicate() {
-}
+    ~DeclarativeContentIsBookmarkedPredicate() = default;
 
 bool DeclarativeContentIsBookmarkedPredicate::IsIgnored() const {
   return !HasBookmarkAPIPermission(extension_.get());
@@ -86,20 +85,18 @@ DeclarativeContentIsBookmarkedPredicate(
 //
 
 DeclarativeContentIsBookmarkedConditionTracker::PerWebContentsTracker::
-PerWebContentsTracker(
-    content::WebContents* contents,
-    const RequestEvaluationCallback& request_evaluation,
-    const WebContentsDestroyedCallback& web_contents_destroyed)
+    PerWebContentsTracker(content::WebContents* contents,
+                          RequestEvaluationCallback request_evaluation,
+                          WebContentsDestroyedCallback web_contents_destroyed)
     : WebContentsObserver(contents),
-      request_evaluation_(request_evaluation),
-      web_contents_destroyed_(web_contents_destroyed) {
+      request_evaluation_(std::move(request_evaluation)),
+      web_contents_destroyed_(std::move(web_contents_destroyed)) {
   is_url_bookmarked_ = IsCurrentUrlBookmarked();
   request_evaluation_.Run(web_contents());
 }
 
 DeclarativeContentIsBookmarkedConditionTracker::PerWebContentsTracker::
-~PerWebContentsTracker() {
-}
+    ~PerWebContentsTracker() = default;
 
 void DeclarativeContentIsBookmarkedConditionTracker::PerWebContentsTracker::
 BookmarkAddedForUrl(const GURL& url) {
@@ -139,7 +136,7 @@ IsCurrentUrlBookmarked() {
 
 void DeclarativeContentIsBookmarkedConditionTracker::PerWebContentsTracker::
 WebContentsDestroyed() {
-  web_contents_destroyed_.Run(web_contents());
+  std::move(web_contents_destroyed_).Run(web_contents());
 }
 
 //
@@ -159,8 +156,7 @@ DeclarativeContentIsBookmarkedConditionTracker::
 }
 
 DeclarativeContentIsBookmarkedConditionTracker::
-~DeclarativeContentIsBookmarkedConditionTracker() {
-}
+    ~DeclarativeContentIsBookmarkedConditionTracker() = default;
 
 std::string DeclarativeContentIsBookmarkedConditionTracker::
 GetPredicateApiAttributeName() const {
@@ -191,10 +187,11 @@ void DeclarativeContentIsBookmarkedConditionTracker::TrackForWebContents(
     content::WebContents* contents) {
   per_web_contents_tracker_[contents] = std::make_unique<PerWebContentsTracker>(
       contents,
-      base::Bind(&Delegate::RequestEvaluation, base::Unretained(delegate_)),
-      base::Bind(&DeclarativeContentIsBookmarkedConditionTracker::
-                     DeletePerWebContentsTracker,
-                 base::Unretained(this)));
+      base::BindRepeating(&Delegate::RequestEvaluation,
+                          base::Unretained(delegate_)),
+      base::BindOnce(&DeclarativeContentIsBookmarkedConditionTracker::
+                         DeletePerWebContentsTracker,
+                     base::Unretained(this)));
 }
 
 void DeclarativeContentIsBookmarkedConditionTracker::OnWebContentsNavigation(
