6cd2669ca5e0df2f60aff72a5e2f257a9f778c6b
Maksim Moskvitin <mmoskvitin@google.com>
2021-01-21 17:19:42 +0000

    [CodeHealth] Convert c/b/e/api/braille_display_private to use modern callback
    Note: |connection_| in BrailleControllerImpl never reset and
    CreateBrlapiConnectionFunction never returns nullptr, thus OnceCallback
    can be used.
    
    Bug: 1152279
    Change-Id: I3a7044c7b4143698fb363aa5a4a694156754a0df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2640537
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#845678}

--- chrome/browser/extensions/api/braille_display_private/braille_controller_brlapi.h
+++ chrome/browser/extensions/api/braille_display_private/braille_controller_brlapi.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/callback_forward.h"
 #include "base/files/file_path.h"
 #include "base/files/file_path_watcher.h"
 #include "base/memory/singleton.h"
@@ -43,13 +44,13 @@ class BrailleControllerImpl : public BrailleController {
   ~BrailleControllerImpl() override;
   void TryLoadLibBrlApi();
 
-  typedef base::Callback<std::unique_ptr<BrlapiConnection>()>
-      CreateBrlapiConnectionFunction;
+  using CreateBrlapiConnectionFunction =
+      base::OnceCallback<std::unique_ptr<BrlapiConnection>()>;
 
   // For dependency injection in tests.  Sets the function used to create
   // brlapi connections.
   void SetCreateBrlapiConnectionForTesting(
-      const CreateBrlapiConnectionFunction& callback);
+      CreateBrlapiConnectionFunction callback);
 
   // Makes the controller try to reconnect (if disconnected) as if the brlapi
   // socket directory had changed.
