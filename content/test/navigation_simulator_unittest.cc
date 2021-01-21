77d5e1952c3ab948f1bd242f9670662f0d2d04fe
Todd Sahl <toddsahl@microsoft.com>
2021-01-21 16:32:07 +0000

    Avoid early removal of RFHM::speculative_render_frame_host_
    A change was introduced in http://crrev.com/c/2536794 to clean up
    RenderFrameHostManager::speculative_render_frame_host_ when a
    renderer aborts a NavigationRequest. However, the aborted request
    may or may not have been associated with the speculative RFH, and there
    may remain one or more active NavigationRequests that are associated
    with the speculative RFH.
    
    This change modifies the regressed call site that cleans up the
    speculative RFH to be conditional on there being no remaining
    NavigationRequests that are associated with it.
    
    Bug: 1159916: Crash report - NavigationRequest::OnStartChecksComplete
    Change-Id: Ifebe8361cf08480d170598e7c3fa973b3bb693d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2595806
    Commit-Queue: Todd Sahl <toddsahl@microsoft.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845663}

--- content/test/navigation_simulator_unittest.cc
+++ content/test/navigation_simulator_unittest.cc
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
-#include "base/callback_helpers.h"
 #include "base/macros.h"
-#include "base/memory/ptr_util.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/run_loop.h"
@@ -24,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/test/test_navigation_throttle.h"
 #include "content/public/test/test_navigation_throttle_inserter.h"
+#include "content/test/task_runner_deferring_throttle.h"
 #include "content/test/test_render_frame_host.h"
 #include "content/test/test_render_view_host.h"
 #include "content/test/test_web_contents.h"
@@ -32,48 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace content {
-
-// This class defers a navigation via a no-op async task on the provided task
-// runner.
-class TaskRunnerDeferringThrottle : public NavigationThrottle {
- public:
-  TaskRunnerDeferringThrottle(scoped_refptr<base::TaskRunner> task_runner,
-                              NavigationHandle* handle)
-      : NavigationThrottle(handle), task_runner_(std::move(task_runner)) {}
-  ~TaskRunnerDeferringThrottle() override {}
-
-  static std::unique_ptr<NavigationThrottle> Create(
-      scoped_refptr<base::TaskRunner> task_runner,
-      NavigationHandle* handle) {
-    return base::WrapUnique(
-        new TaskRunnerDeferringThrottle(std::move(task_runner), handle));
-  }
-
-  // NavigationThrottle:
-  ThrottleCheckResult WillStartRequest() override { return DeferToPostTask(); }
-  ThrottleCheckResult WillRedirectRequest() override {
-    return DeferToPostTask();
-  }
-  ThrottleCheckResult WillProcessResponse() override {
-    return DeferToPostTask();
-  }
-  const char* GetNameForLogging() override {
-    return "TaskRunnerDeferringThrottle";
-  }
-
- private:
-  ThrottleCheckResult DeferToPostTask() {
-    task_runner_->PostTaskAndReply(
-        FROM_HERE, base::DoNothing(),
-        base::BindOnce(&TaskRunnerDeferringThrottle::Resume,
-                       weak_factory_.GetWeakPtr()));
-
-    return NavigationThrottle::DEFER;
-  }
-  scoped_refptr<base::TaskRunner> task_runner_;
-  base::WeakPtrFactory<TaskRunnerDeferringThrottle> weak_factory_{this};
-  DISALLOW_COPY_AND_ASSIGN(TaskRunnerDeferringThrottle);
-};
 
 class NavigationSimulatorTest : public RenderViewHostImplTestHarness {};
 
@@ -195,8 +152,11 @@ TEST_F(NavigationSimulatorTest, AutoAdvanceOff) {
   auto task_runner = base::MakeRefCounted<base::TestSimpleTaskRunner>();
   auto* raw_runner = task_runner.get();
   TestNavigationThrottleInserter throttle_inserter(
-      web_contents(), base::BindRepeating(&TaskRunnerDeferringThrottle::Create,
-                                          std::move(task_runner)));
+      web_contents(),
+      base::BindRepeating(&TaskRunnerDeferringThrottle::Create,
+                          std::move(task_runner), true /* defer_start */,
+                          true /* defer_redirect */,
+                          true /* defer_response */));
 
   simulator->Start();
   EXPECT_EQ(1u, raw_runner->NumPendingTasks());
