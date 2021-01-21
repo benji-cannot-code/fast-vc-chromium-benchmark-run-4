5c7ed904ace384a53cd2770f5eeca1608cf795e6
Ahmed Fakhry <afakhry@chromium.org>
2021-01-21 19:12:30 +0000

    capture_mode: Fix video dimensions when display bounds changes
    When recording a window or a partial region, the display can be
    rotated, or its device scale factor can change, leading to the
    DIPs display bounds changing. If we don't propagate this change
    to the recording service, the video capturer will try to fit (i.e.
    center and letter-box) the new frame dimensions into the original
    requested video resolution. This is ok when recording fullscreen,
    but leads to completely broken cropping when recording a region
    (See bug for video demo). It can also lead to a bad quality
    window recording.
    
    This CL fixes this issue by propagating display bounds changes to
    the service to reconfigure the video capturer and encoder if
    necessary (Demo of fix is on the bug).
    
    BUG=1168434
    TEST=Manually, added a new test
    
    Change-Id: I9db5d29bddc217042b007b422b1084c86d8be959
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2641082
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845749}

--- ash/capture_mode/video_recording_watcher.cc
+++ ash/capture_mode/video_recording_watcher.cc
@@ -11,28 +11,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/notreached.h"
 #include "ui/aura/window.h"
+#include "ui/display/screen.h"
 
 namespace ash {
 
 VideoRecordingWatcher::VideoRecordingWatcher(
     CaptureModeController* controller,
     aura::Window* window_being_recorded)
-    : controller_(controller), window_being_recorded_(window_being_recorded) {
+    : controller_(controller),
+      window_being_recorded_(window_being_recorded),
+      recording_source_(controller_->source()) {
   DCHECK(controller_);
   DCHECK(window_being_recorded_);
   DCHECK(controller_->is_recording_in_progress());
 
   if (!window_being_recorded_->IsRootWindow()) {
+    DCHECK_EQ(recording_source_, CaptureModeSource::kWindow);
     non_root_window_capture_request_ =
         window_being_recorded_->MakeWindowCapturable();
   }
 
   window_being_recorded_->AddObserver(this);
+  display::Screen::GetScreen()->AddObserver(this);
 }
 
 VideoRecordingWatcher::~VideoRecordingWatcher() {
   DCHECK(window_being_recorded_);
 
+  display::Screen::GetScreen()->RemoveObserver(this);
   window_being_recorded_->RemoveObserver(this);
 }
 
@@ -58,11 +64,32 @@ void VideoRecordingWatcher::OnWindowRemovingFromRootWindow(
     aura::Window* new_root) {
   DCHECK_EQ(window, window_being_recorded_);
   DCHECK(controller_->is_recording_in_progress());
+  DCHECK_EQ(recording_source_, CaptureModeSource::kWindow);
 
   if (!new_root)
     return;
 
   controller_->OnRecordedWindowChangingRoot(window_being_recorded_, new_root);
+}
+
+void VideoRecordingWatcher::OnDisplayMetricsChanged(
+    const display::Display& display,
+    uint32_t metrics) {
+  if (recording_source_ == CaptureModeSource::kFullscreen)
+    return;
+
+  if (!(metrics & (DISPLAY_METRIC_BOUNDS | DISPLAY_METRIC_ROTATION |
+                   DISPLAY_METRIC_DEVICE_SCALE_FACTOR))) {
+    return;
+  }
+
+  auto* root = window_being_recorded_->GetRootWindow();
+  const int64_t display_id =
+      display::Screen::GetScreen()->GetDisplayNearestWindow(root).id();
+  if (display_id != display.id())
+    return;
+
+  controller_->PushNewRootSizeToRecordingService(root->bounds().size());
 }
 
 }  // namespace ash
