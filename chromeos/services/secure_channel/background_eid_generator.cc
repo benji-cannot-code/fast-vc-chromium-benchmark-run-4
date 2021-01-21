4b593b212519baef5e3a9e879a1e87633fad535a
Josh Nohle <nohle@google.com>
2021-01-21 02:27:24 +0000

    [Secure Channel] Add logging to debug discovery and auth failures
    Log whether or not a remote device is able to be discovered from a given
    advertisement. We also log the candidate remote device's device ID and
    beacon seeds to ensure that the remote device record is not stale. We
    log times in human-readable format.
    
    Bug: 1157278
    Change-Id: I202f9f1a13deba998ddff06dd9ca82b92737abae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2639649
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845475}

--- chromeos/services/secure_channel/background_eid_generator.cc
+++ chromeos/services/secure_channel/background_eid_generator.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstring>
 #include <memory>
 
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/time/default_clock.h"
 #include "base/time/time.h"
@@ -116,7 +117,24 @@ std::string BackgroundEidGenerator::IdentifyRemoteDeviceByAdvertisement(
               return eid.data == service_data_without_flags;
             });
 
-        return eid_it != eids.end();
+        bool success = eid_it != eids.end();
+        std::stringstream ss;
+        ss << "BackgroundEidGenerator::IdentifyRemoteDeviceByAdvertisement: "
+           << (success ? "Identified " : "Failed to identify ")
+           << "the following remote device from advertisement service data 0x"
+           << base::HexEncode(service_data_without_flags.data(),
+                              service_data_without_flags.size())
+           << ": "
+           << "\n  device_name: " << remote_device.name()
+           << "\n  device_id: " << remote_device.GetDeviceId()
+           << "\n  beacon seeds: ";
+        for (const auto& seed : remote_device.beacon_seeds()) {
+          ss << "\n    " << seed;
+        }
+        ss << "\n  eids: " << DataWithTimestamp::ToDebugString(eids);
+        PA_LOG(VERBOSE) << ss.str();
+
+        return success;
       });
 
   // Return empty string if no matching device is found.
