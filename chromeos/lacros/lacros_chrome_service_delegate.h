a5139db8f9dd9937cd8c75e96138f741a586da09
Jenny Zhang <jennyz@chromium.org>
2021-01-21 05:28:19 +0000

    Improve LacrosChromeService Feedback API implementation.
    This cl makes several improvements:
    1. Run callback function of LacrosChromeService api with return
    value on IPC thread instead of UI thread.
    2. Run GetCompressedHistograms in base's thread pool instead of
    UI thread, since it calls functions marking as blocking.
    3. Do not store the callback passed to LacrosChromeServiceDelegateImpl.
    
    Bug: 1167026
    Change-Id: I85ec998517e78bad067f1da49c7cff05e4222539
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2633192
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845529}

--- chromeos/lacros/lacros_chrome_service_delegate.h
+++ chromeos/lacros/lacros_chrome_service_delegate.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/task_runner.h"
 #include "base/values.h"
 
 class GURL;
@@ -42,16 +44,18 @@ class LacrosChromeServiceDelegate {
 
   using GetFeedbackDataCallback = base::OnceCallback<void(base::Value)>;
   // Gets lacros feedback data.
-  virtual void GetFeedbackData(GetFeedbackDataCallback callback) = 0;
+  virtual void GetFeedbackData(
+      scoped_refptr<base::TaskRunner> callback_task_runner,
+      GetFeedbackDataCallback callback) = 0;
 
-  using GetHistogramsCallback = base::OnceCallback<void(const std::string&)>;
   // Gets lacros histograms.
-  virtual void GetHistograms(GetHistogramsCallback callback) = 0;
+  using GetHistogramsCallback = base::OnceCallback<void(const std::string&)>;
+  virtual void GetHistograms(
+      scoped_refptr<base::TaskRunner> callback_task_runner,
+      GetHistogramsCallback callback) = 0;
 
-  using GetActiveTabUrlCallback =
-      base::OnceCallback<void(const base::Optional<GURL>&)>;
   // Gets Url of the active tab if there is any.
-  virtual void GetActiveTabUrl(GetActiveTabUrlCallback callback) = 0;
+  virtual GURL GetActiveTabUrl() = 0;
 };
 
 }  // namespace chromeos
