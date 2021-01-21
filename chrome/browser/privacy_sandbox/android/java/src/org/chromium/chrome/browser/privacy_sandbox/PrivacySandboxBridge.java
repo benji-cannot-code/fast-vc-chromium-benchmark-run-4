e90eb0860ab580b026cd229aebde2d612e0b5726
Andrey Zaytsev <andzaytsev@google.com>
2021-01-21 13:08:52 +0000

    Privacy Sandbox Settings: created a bridge to access prefs
    Bug: 1152351
    Change-Id: I8afa16d818cfe1da71ec0479d9bc9d314346fd0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2637833
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/master@{#845615}

---
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxBridge.java
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.privacy_sandbox;
+
+import org.chromium.base.annotations.NativeMethods;
+
+/** Bridge, providing access to the native-side Privacy Sandbox configuration. */
+public class PrivacySandboxBridge {
+    boolean isPrivacySandboxEnabled() {
+        return PrivacySandboxBridgeJni.get().isPrivacySandboxEnabled();
+    }
+
+    boolean isPrivacySandboxManaged() {
+        return PrivacySandboxBridgeJni.get().isPrivacySandboxManaged();
+    }
+
+    void setPrivacySandboxEnabled(boolean enabled) {
+        PrivacySandboxBridgeJni.get().setPrivacySandboxEnabled(enabled);
+    }
+
+    @NativeMethods
+    interface Natives {
+        boolean isPrivacySandboxEnabled();
+        boolean isPrivacySandboxManaged();
+        void setPrivacySandboxEnabled(boolean enabled);
+    }
+}
---
+++ chrome/browser/privacy_sandbox/android/privacy_sandbox_bridge.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/privacy_sandbox/android/jni_headers/PrivacySandboxBridge_jni.h"
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_settings.h"
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
+#include "chrome/browser/profiles/profile_manager.h"
+
+static jboolean JNI_PrivacySandboxBridge_IsPrivacySandboxEnabled(JNIEnv* env) {
+  return PrivacySandboxSettingsFactory::GetForProfile(
+             ProfileManager::GetActiveUserProfile())
+      ->IsPrivacySandboxEnabled();
+}
+
+static jboolean JNI_PrivacySandboxBridge_IsPrivacySandboxManaged(JNIEnv* env) {
+  return PrivacySandboxSettingsFactory::GetForProfile(
+             ProfileManager::GetActiveUserProfile())
+      ->IsPrivacySandboxManaged();
+}
+
+static void JNI_PrivacySandboxBridge_SetPrivacySandboxEnabled(
+    JNIEnv* env,
+    jboolean enabled) {
+  PrivacySandboxSettingsFactory::GetForProfile(
+      ProfileManager::GetActiveUserProfile())
+      ->SetPrivacySandboxEnabled(enabled);
+}
