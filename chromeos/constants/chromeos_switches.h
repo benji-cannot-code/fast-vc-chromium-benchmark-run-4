e0052cdc68c3d66f7bfc80dc239225a42bbb2702
Nikolai Artemiev <nartemiev@google.com>
2021-01-21 06:12:58 +0000

    Add --form-factor override flag to Chrome
    Allows the --form-factor flag to be used to override the form factor
    value from the LSB release info.
    
    BUG=b:173179861
    TEST=CQ
    
    Change-Id: Ic4dd3eb52dd562d35debac3fea168f686d728e5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631393
    Commit-Queue: Nikolai Artemiev <nartemiev@google.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Nikolai Artemiev <nartemiev@google.com>
    Cr-Commit-Position: refs/heads/master@{#845539}

--- chromeos/constants/chromeos_switches.h
+++ chromeos/constants/chromeos_switches.h
@@ -152,6 +152,8 @@ COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const char kForceLoginManagerInTests[];
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const char kForceSystemCompositorMode[];
+COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
+extern const char kFormFactor[];
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS) extern const char kGuestSession[];
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS) extern const char kGuestWallpaperLarge[];
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS) extern const char kGuestWallpaperSmall[];
--- chromeos/constants/devicetype.cc
+++ chromeos/constants/devicetype.cc
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/strings/string_split.h"
 #include "base/system/sys_info.h"
+#include "chromeos/constants/chromeos_switches.h"
 
 namespace chromeos {
 
@@ -19,20 +21,24 @@ const char kDeviceTypeKey[] = "DEVICETYPE";
 
 DeviceType GetDeviceType() {
   std::string value;
-  if (base::SysInfo::GetLsbReleaseValue(kDeviceTypeKey, &value)) {
-    if (value == "CHROMEBASE")
-      return DeviceType::kChromebase;
-    if (value == "CHROMEBIT")
-      return DeviceType::kChromebit;
-    // Most devices are Chromebooks, so we will also consider reference boards
-    // as chromebooks.
-    if (value == "CHROMEBOOK" || value == "REFERENCE")
-      return DeviceType::kChromebook;
-    if (value == "CHROMEBOX")
-      return DeviceType::kChromebox;
-    LOG(ERROR) << "Unknown device type \"" << value << "\"";
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kFormFactor)) {
+    value = command_line->GetSwitchValueASCII(switches::kFormFactor);
+  } else if (!base::SysInfo::GetLsbReleaseValue(kDeviceTypeKey, &value)) {
+    return DeviceType::kUnknown;
   }
+  if (value == "CHROMEBASE")
+    return DeviceType::kChromebase;
+  if (value == "CHROMEBIT")
+    return DeviceType::kChromebit;
+  // Most devices are Chromebooks, so we will also consider reference boards
+  // as chromebooks.
+  if (value == "CHROMEBOOK" || value == "REFERENCE")
+    return DeviceType::kChromebook;
+  if (value == "CHROMEBOX")
+    return DeviceType::kChromebox;
 
+  LOG(ERROR) << "Unknown device type \"" << value << "\"";
   return DeviceType::kUnknown;
 }
 
