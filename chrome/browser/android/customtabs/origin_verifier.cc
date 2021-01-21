c38cbe20893fed4693b61f2bb864d1c0da0071aa
Peter E Conn <peconn@chromium.org>
2021-01-21 11:01:38 +0000

    🍱 Move browserservice's verification code into a module.
    Change-Id: I8caff95f74faa0ac9509028b357dfcc146980262
    Bug: 1164866
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2637498
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845588}

--- chrome/browser/android/customtabs/origin_verifier.cc
+++ chrome/browser/android/customtabs/origin_verifier.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/bind.h"
-#include "chrome/android/chrome_jni_headers/OriginVerifier_jni.h"
+#include "chrome/browser/android/browserservices/verification/jni_headers/OriginVerifier_jni.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/installable/digital_asset_links/digital_asset_links_handler.h"
 #include "chrome/browser/profiles/profile_android.h"
