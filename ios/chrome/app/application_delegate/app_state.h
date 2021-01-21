cd999dd65fdc214603d97cd93822c63b9b519a16
Mike Dougherty <michaeldo@chromium.org>
2021-01-21 18:49:26 +0000

    Revert "[iOS] Fix Session.TotalDuration metric with multiwindow."
    This reverts commit 8904382c943c42b110ef0dece02f70c71c693acb.
    
    Reason for revert: This CL breaks AppStateTest.resumeSessionWithStartupParameters for iOS 12 devices. See https://ci.chromium.org/ui/p/chrome/builders/ci/iphone-device/15349/blamelist.
    
    Original change's description:
    > [iOS] Fix Session.TotalDuration metric with multiwindow.
    >
    > Track the session as open while there is at least one foreground window.
    > Log this in a dedicated app agent to avoid polluting AppState.
    > The same is done to IOSProfileSessionDurationsService's session
    > start/end events.
    >
    > Bug: 1156586
    > Change-Id: I37586f989039206fbaa90f7ec4046fbf9582e70a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2599870
    > Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    > Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    > Reviewed-by: Mark Pearson <mpearson@chromium.org>
    > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#845653}
    
    TBR=mpearson@chromium.org,marq@chromium.org,msarda@chromium.org,stkhapugin@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: Icd927a3252658933c02c8101067657f3ac7365b2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1156586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643264
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845730}

--- ios/chrome/app/application_delegate/app_state.h
+++ ios/chrome/app/application_delegate/app_state.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-#include <memory>
-
 #import "ios/chrome/app/application_delegate/app_state_agent.h"
 #import "ios/chrome/browser/ui/main/scene_state_observer.h"
 #import "ios/chrome/browser/ui/scoped_ui_blocker/ui_blocker_manager.h"
@@ -26,10 +24,6 @@ class ChromeBrowserState;
 @protocol StartupInformation;
 @protocol TabOpening;
 @protocol TabSwitching;
-
-namespace base {
-class TimeTicks;
-}
 
 @protocol AppStateObserver <NSObject>
 
@@ -100,9 +94,6 @@ initWithBrowserLauncher:(id<BrowserLauncher>)browserLauncher
 // The SceneSession ID for the last session, where the Device doesn't support
 // multiple windows.
 @property(nonatomic, strong) NSString* previousSingleWindowSessionID;
-
-// Timestamp of when a scene was last becoming active. Can be null.
-@property(nonatomic, assign) base::TimeTicks lastTimeInForeground;
 
 // Saves the launchOptions to be used from -newTabFromLaunchOptions. If the
 // application is in background, initialize the browser to basic. If not, launch
