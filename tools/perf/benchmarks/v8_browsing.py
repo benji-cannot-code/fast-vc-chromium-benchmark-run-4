9222e2f84b546bd36132a4bc5ef55639e1f923e0
Tamer Tas <tmrts@chromium.org>
2021-01-21 16:35:43 +0000

    [perf] Add almuthanna as an owner for v8 benchmarks
    R=johnchen@chromium.org,machenbach@chromium.org
    CC=almuthanna@chromium.org,mythria@chromium.org
    
    Bug: 1158374
    Change-Id: I2532943acfd955e9e956a5c3a12ab654a76c2614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2642350
    Auto-Submit: Tamer Tas <tmrts@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845665}

--- tools/perf/benchmarks/v8_browsing.py
+++ tools/perf/benchmarks/v8_browsing.py
@@ -75,9 +75,10 @@ class _V8BrowsingBenchmark(perf_benchmark.PerfBenchmark):
     return options
 
 
-@benchmark.Info(
-    emails=['mythria@chromium.org', 'tmrts@chromium.org'],
-    component='Blink>JavaScript')
+@benchmark.Info(emails=[
+    'mythria@chromium.org', 'tmrts@chromium.org', 'almuthanna@chromium.org'
+],
+                component='Blink>JavaScript')
 class V8DesktopBrowsingBenchmark(
     _V8BrowsingBenchmark):
   PLATFORM = 'desktop'
@@ -117,9 +118,10 @@ class V8MobileBrowsingBenchmark(
     return 'v8.browsing_mobile'
 
 
-@benchmark.Info(
-    emails=['mythria@chromium.org', 'tmrts@chromium.org'],
-    component='Blink>JavaScript')
+@benchmark.Info(emails=[
+    'mythria@chromium.org', 'tmrts@chromium.org', 'almuthanna@chromium.org'
+],
+                component='Blink>JavaScript')
 class V8FutureDesktopBrowsingBenchmark(
     _V8BrowsingBenchmark):
   PLATFORM = 'desktop'
