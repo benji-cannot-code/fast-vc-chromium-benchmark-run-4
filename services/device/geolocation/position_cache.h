980dc39edec8abe5c0ea0cb4f9c60ef70cc0b535
Tomasz Śniatowski <tsniatowski@vewd.com>
2021-01-21 21:57:55 +0000

    geolocation: Add <cstddef> include in position_cache.h
    This header uses size_t  and should include something to get it.
    This happens to work in the default libc++ config, but  can
    cause a build failure in builds with libstdc++.
    
    Change-Id: I1fd3af74bc19b491ae9f4c3529a536f0b93735ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2640578
    Auto-Submit: Tomasz Śniatowski <tsniatowski@vewd.com>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845807}

--- services/device/geolocation/position_cache.h
+++ services/device/geolocation/position_cache.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_DEVICE_GEOLOCATION_POSITION_CACHE_H_
 #define SERVICES_DEVICE_GEOLOCATION_POSITION_CACHE_H_
 
+#include <cstddef>
+
 namespace device {
 namespace mojom {
 class Geoposition;
