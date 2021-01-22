ee591f875bc757c5d954d5ac21cd44a4603dc1f2
Wei Lee <wtlee@google.com>
2021-01-22 17:20:34 +0000

    VCD: Migrate some VLOG to CAMERA_LOG
    The information printed by CAMERA_LOG is visible in feedback report.
    Therefore, migrating some VLOG to CAMERA_LOG in VCD should be helpful
    for debug camera issues.
    
    Bug: b/172344305
    Test: Build successfully
    Change-Id: I6398e8a47714417b50fe8225a3fbc443eb860223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2641689
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846175}

--- media/capture/video/chromeos/camera_hal_dispatcher_impl.cc
+++ media/capture/video/chromeos/camera_hal_dispatcher_impl.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/trace_event/trace_event.h"
+#include "components/device_event_log/device_event_log.h"
 #include "media/base/bind_to_current_loop.h"
 #include "media/capture/video/chromeos/mojom/camera_common.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -267,7 +268,7 @@ CameraHalDispatcherImpl::~CameraHalDispatcherImpl() {
     proxy_thread_.Stop();
   }
   blocking_io_thread_.Stop();
-  VLOG(1) << "CameraHalDispatcherImpl stopped";
+  CAMERA_LOG(EVENT) << "CameraHalDispatcherImpl stopped";
 }
 
 void CameraHalDispatcherImpl::RegisterServer(
@@ -299,7 +300,7 @@ void CameraHalDispatcherImpl::RegisterServerWithToken(
   camera_hal_server_.set_disconnect_handler(
       base::BindOnce(&CameraHalDispatcherImpl::OnCameraHalServerConnectionError,
                      base::Unretained(this)));
-  VLOG(1) << "Camera HAL server registered";
+  CAMERA_LOG(EVENT) << "Camera HAL server registered";
   std::move(callback).Run(
       0, camera_hal_server_callbacks_.BindNewPipeAndPassRemote());
 
@@ -566,7 +567,7 @@ void CameraHalDispatcherImpl::AddClientObserverOnProxyThread(
   }
   client_observers_.insert(std::move(observer));
   std::move(result_callback).Run(0);
-  VLOG(1) << "Camera HAL client registered";
+  CAMERA_LOG(EVENT) << "Camera HAL client registered";
 }
 
 void CameraHalDispatcherImpl::EstablishMojoChannel(
@@ -574,7 +575,7 @@ void CameraHalDispatcherImpl::EstablishMojoChannel(
   DCHECK(proxy_task_runner_->BelongsToCurrentThread());
   mojo::PendingRemote<cros::mojom::CameraModule> camera_module;
   const auto& type = client_observer->GetType();
-  VLOG(1) << "Establishing server channel for " << type;
+  CAMERA_LOG(EVENT) << "Establishing server channel for " << type;
   camera_hal_server_->CreateChannel(
       camera_module.InitWithNewPipeAndPassReceiver(), type);
   client_observer->OnChannelCreated(std::move(camera_module));
@@ -592,7 +593,7 @@ void CameraHalDispatcherImpl::OnPeerConnected(
 void CameraHalDispatcherImpl::OnCameraHalServerConnectionError() {
   DCHECK(proxy_task_runner_->BelongsToCurrentThread());
   base::AutoLock lock(opened_camera_id_map_lock_);
-  VLOG(1) << "Camera HAL server connection lost";
+  CAMERA_LOG(EVENT) << "Camera HAL server connection lost";
   camera_hal_server_.reset();
   camera_hal_server_callbacks_.reset();
   for (auto& opened_camera_id_pair : opened_camera_id_map_) {
@@ -636,7 +637,7 @@ void CameraHalDispatcherImpl::OnCameraHalClientConnectionError(
   auto it = client_observers_.find(client_observer);
   if (it != client_observers_.end()) {
     client_observers_.erase(it);
-    VLOG(1) << "Camera HAL client connection lost";
+    CAMERA_LOG(EVENT) << "Camera HAL client connection lost";
   }
 }
 
