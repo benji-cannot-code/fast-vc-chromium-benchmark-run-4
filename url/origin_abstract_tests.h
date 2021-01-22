d9a16d65de17963327037f1c570c8089e6d18ea4
Lukasz Anforowicz <lukasza@chromium.org>
2021-01-22 03:16:01 +0000

    Sharing IsPotentiallyTrustworthy tests across Blink and "network" layers
    Bug: 1164416
    Change-Id: I3834ffb77ff1ef23f39c8886049b563753ab6cf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2630625
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845905}

--- url/origin_abstract_tests.h
+++ url/origin_abstract_tests.h
@@ -9,8 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <type_traits>
 
+#include "base/containers/contains.h"
+#include "base/optional.h"
 #include "base/strings/string_piece.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+#include "url/origin.h"
+#include "url/scheme_host_port.h"
 #include "url/url_util.h"
 
 namespace url {
@@ -75,6 +80,9 @@ class AbstractOriginTest : public testing::Test {
         "local-std-with-host",
         "local-noaccess-std-with-host",
         "also-local",
+        "sec",
+        "sec-std-with-host",
+        "sec-noaccess",
     };
     for (const char* kScheme : kSchemesToRegister) {
       std::string scheme(kScheme);
@@ -84,6 +92,8 @@ class AbstractOriginTest : public testing::Test {
         AddStandardScheme(kScheme, SchemeType::SCHEME_WITH_HOST);
       if (base::Contains(scheme, "local"))
         AddLocalScheme(kScheme);
+      if (base::Contains(scheme, "sec"))
+        AddSecureScheme(kScheme);
     }
   }
 
@@ -489,6 +499,25 @@ REGISTER_TYPED_TEST_SUITE_P(AbstractOriginTest,
                             TupleOrigins,
                             CustomSchemes_OpaqueOrigins,
                             CustomSchemes_TupleOrigins);
+
+class UrlOriginTestTraits : public virtual OriginTraitsBase<Origin> {
+ public:
+  OriginType CreateOriginFromString(base::StringPiece s) override;
+  OriginType CreateUniqueOpaqueOrigin() override;
+  OriginType CreateWithReferenceOrigin(
+      base::StringPiece url,
+      const OriginType& reference_origin) override;
+  OriginType DeriveNewOpaqueOrigin(const OriginType& reference_origin) override;
+  bool IsOpaque(const OriginType& origin) override;
+  std::string GetScheme(const OriginType& origin) override;
+  std::string GetHost(const OriginType& origin) override;
+  uint16_t GetPort(const OriginType& origin) override;
+  SchemeHostPort GetTupleOrPrecursorTupleIfOpaque(
+      const OriginType& origin) override;
+  bool IsSameOrigin(const OriginType& a, const OriginType& b) override;
+  std::string Serialize(const OriginType& origin) override;
+  bool IsValidUrl(base::StringPiece str) override;
+};
 
 }  // namespace url
 
