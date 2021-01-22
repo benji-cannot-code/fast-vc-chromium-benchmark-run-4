3b6e5c3a256ec07a8bb65c12cdcc2d76b0b26bec
Reilly Grant <reillyg@chromium.org>
2021-01-22 18:09:00 +0000

    Convert callbacks in //chrome/browser/chromeos/lock_screen_apps
    This change converts callbacks from base::Bind and base::Callback to
    Once/Repeating in //chrome/browser/chromeos/lock_screen_apps.
    
    Bug: 1148570
    Change-Id: I85b8e4e83d9f61d3cdc0da02f6650418c1ccccff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2644110
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846204}

--- chrome/browser/chromeos/lock_screen_apps/state_controller.cc
+++ chrome/browser/chromeos/lock_screen_apps/state_controller.cc
@@ -98,10 +98,10 @@ void StateController::FlushTrayActionForTesting() {
 }
 
 void StateController::SetReadyCallbackForTesting(
-    const base::Closure& ready_callback) {
+    base::OnceClosure ready_callback) {
   DCHECK(ready_callback_.is_null());
 
-  ready_callback_ = ready_callback;
+  ready_callback_ = std::move(ready_callback);
 }
 
 void StateController::SetTickClockForTesting(const base::TickClock* clock) {
@@ -266,8 +266,9 @@ void StateController::SetFocusCyclerDelegate(FocusCyclerDelegate* delegate) {
   focus_cycler_delegate_ = delegate;
 
   if (focus_cycler_delegate_ && note_app_window_) {
-    focus_cycler_delegate_->RegisterLockScreenAppFocusHandler(base::Bind(
-        &StateController::FocusAppWindow, weak_ptr_factory_.GetWeakPtr()));
+    focus_cycler_delegate_->RegisterLockScreenAppFocusHandler(
+        base::BindRepeating(&StateController::FocusAppWindow,
+                            weak_ptr_factory_.GetWeakPtr()));
   }
 }
 
@@ -314,8 +315,8 @@ void StateController::OnSessionStateChanged() {
   // and the callback will not be invoked after |app_manager_| goes out of
   // scope.
   app_manager_->Start(
-      base::Bind(&StateController::OnNoteTakingAvailabilityChanged,
-                 base::Unretained(this)));
+      base::BindRepeating(&StateController::OnNoteTakingAvailabilityChanged,
+                          base::Unretained(this)));
   note_app_window_metrics_ =
       std::make_unique<AppWindowMetricsTracker>(tick_clock_);
   lock_screen_data_->SetSessionLocked(true);
@@ -334,8 +335,9 @@ void StateController::OnWindowVisibilityChanged(aura::Window* window,
 
   UpdateLockScreenNoteState(TrayActionState::kActive);
   if (focus_cycler_delegate_) {
-    focus_cycler_delegate_->RegisterLockScreenAppFocusHandler(base::Bind(
-        &StateController::FocusAppWindow, weak_ptr_factory_.GetWeakPtr()));
+    focus_cycler_delegate_->RegisterLockScreenAppFocusHandler(
+        base::BindRepeating(&StateController::FocusAppWindow,
+                            weak_ptr_factory_.GetWeakPtr()));
   }
 }
 
