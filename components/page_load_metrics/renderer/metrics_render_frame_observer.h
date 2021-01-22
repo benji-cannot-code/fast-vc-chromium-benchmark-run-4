ea51c6a7e1bfb7aa859af3217f1543aec6057405
Dave Tapuska <dtapuska@chromium.org>
2021-01-22 21:27:53 +0000

    More WebRect/WebSize removal from Accessibility.
    A few more removals with respect to conversions from WebRect->gfx::Rect.
    
    BUG=738465
    
    Change-Id: Id61dad97afff35df06e242312c833f7e580440e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643623
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846318}

--- components/page_load_metrics/renderer/metrics_render_frame_observer.h
+++ components/page_load_metrics/renderer/metrics_render_frame_observer.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_frame_observer.h"
 #include "third_party/blink/public/common/loader/loading_behavior_flag.h"
 #include "third_party/blink/public/mojom/loader/resource_load_info.mojom-shared.h"
-#include "third_party/blink/public/platform/web_rect.h"
 #include "third_party/blink/public/web/web_local_frame_client.h"
 
 class GURL;
@@ -100,7 +99,7 @@ class MetricsRenderFrameObserver
   void OnAdResourceObserved(int request_id) override;
 
   void OnMainFrameIntersectionChanged(
-      const blink::WebRect& main_frame_intersection) override;
+      const gfx::Rect& main_frame_intersection) override;
   void OnMobileFriendlinessChanged(const blink::MobileFriendliness&) override;
 
   bool SetUpSmoothnessReporting(
--- third_party/blink/renderer/modules/exported/web_ax_object.cc
+++ third_party/blink/renderer/modules/exported/web_ax_object.cc
@@ -476,11 +476,11 @@ bool WebAXObject::CanvasHasFallbackContent() const {
   return private_->CanvasHasFallbackContent();
 }
 
-WebString WebAXObject::ImageDataUrl(const WebSize& max_size) const {
+WebString WebAXObject::ImageDataUrl(const gfx::Size& max_size) const {
   if (IsDetached())
     return WebString();
 
-  return private_->ImageDataUrl(max_size);
+  return private_->ImageDataUrl(IntSize(max_size));
 }
 
 ax::mojom::InvalidState WebAXObject::InvalidState() const {
@@ -544,9 +544,9 @@ WebAXObject WebAXObject::HitTest(const gfx::Point& point) const {
   return WebAXObject();
 }
 
-WebRect WebAXObject::GetBoundsInFrameCoordinates() const {
+gfx::Rect WebAXObject::GetBoundsInFrameCoordinates() const {
   LayoutRect rect = private_->GetBoundsInFrameCoordinates();
-  return WebRect(EnclosingIntRect(rect));
+  return EnclosingIntRect(rect);
 }
 
 WebString WebAXObject::KeyboardShortcut() const {
@@ -1323,7 +1323,7 @@ bool WebAXObject::ScrollToMakeVisible() const {
 }
 
 bool WebAXObject::ScrollToMakeVisibleWithSubFocus(
-    const WebRect& subfocus,
+    const gfx::Rect& subfocus,
     ax::mojom::ScrollAlignment horizontal_scroll_alignment,
     ax::mojom::ScrollAlignment vertical_scroll_alignment,
     ax::mojom::ScrollBehavior scroll_behavior) const {
@@ -1350,7 +1350,7 @@ bool WebAXObject::ScrollToMakeVisibleWithSubFocus(
   blink::mojom::blink::ScrollAlignment blink_vertical_scroll_alignment = {
       visible_vertical_behavior, vertical_behavior, vertical_behavior};
   return private_->RequestScrollToMakeVisibleWithSubFocusAction(
-      subfocus, blink_horizontal_scroll_alignment,
+      IntRect(subfocus), blink_horizontal_scroll_alignment,
       blink_vertical_scroll_alignment);
 }
 
