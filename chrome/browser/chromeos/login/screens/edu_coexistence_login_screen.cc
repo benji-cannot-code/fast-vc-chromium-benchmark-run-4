c8e68632328ea5986492ead7d72020f3fbfd8def
yilkal <yilkal@chromium.org>
2021-01-22 20:47:24 +0000

    Make EDU Coexistence Dialog height consistent with the rest of OOBE
    This cl makes sure that the dialog size and bounds are consistent
    with the rest of onboarding flow.
    
    Bug: 1169786
    Change-Id: Ie433562bf353a287eaa8449cc8e50c27c0bce4b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2645298
    Reviewed-by: Dan S <danan@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846296}

--- chrome/browser/chromeos/login/screens/edu_coexistence_login_screen.cc
+++ chrome/browser/chromeos/login/screens/edu_coexistence_login_screen.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/supervised_user/supervised_user_features.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "chrome/browser/ui/webui/signin/inline_login_dialog_chromeos_onboarding.h"
+#include "ui/aura/window.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace chromeos {
@@ -78,6 +79,8 @@ void EduCoexistenceLoginScreen::ShowImpl() {
 
   dialog_delegate_ =
       std::make_unique<InlineLoginDialogChromeOSOnboarding::Delegate>(dialog);
+  dialog_delegate_->UpdateDialogBounds(
+      oobe_ui->GetNativeView()->GetBoundsInScreen());
 }
 
 void EduCoexistenceLoginScreen::HideImpl() {
