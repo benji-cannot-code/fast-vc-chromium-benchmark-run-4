9ecb148a6e3459c03dc444b821b0f38e9ec1495e
Nektarios Paisios <nektar@chromium.org>
2021-01-22 14:51:19 +0000

    Used "= default" for constructors in AXTreeManagerMap and improved comments
    TBR=dmazzoni@chromium.org, aleventhal@chromium.org
    
    Change-Id: Ief1fff9084e42b284ec733ac344119d326fbe38b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2642633
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846108}

--- ui/accessibility/ax_tree_manager_map.cc
+++ ui/accessibility/ax_tree_manager_map.cc
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-AXTreeManagerMap::AXTreeManagerMap() {}
+AXTreeManagerMap::AXTreeManagerMap() = default;
 
-AXTreeManagerMap::~AXTreeManagerMap() {}
+AXTreeManagerMap::~AXTreeManagerMap() = default;
 
 // static
 AXTreeManagerMap& AXTreeManagerMap::GetInstance() {
