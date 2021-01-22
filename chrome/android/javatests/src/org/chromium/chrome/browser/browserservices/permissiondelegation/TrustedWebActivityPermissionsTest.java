9794a35f88869efb9413d271d173b1b8b525949e
Peter E Conn <peconn@chromium.org>
2021-01-22 08:21:26 +0000

    🏃 Batch TrustedWebActivityPermissionsTest.
    Change-Id: I62e6216cb126a3a34ea23c4a5947bb6211d5e499
    Bug: 1146206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2642152
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846009}

--- chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/permissiondelegation/TrustedWebActivityPermissionsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/permissiondelegation/TrustedWebActivityPermissionsTest.java
@@ -9,6 +9,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import static org.chromium.base.test.util.Batch.PER_CLASS;
+
 import android.support.test.InstrumentationRegistry;
 
 import androidx.test.filters.MediumTest;
@@ -21,6 +23,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.library_loader.LibraryLoader;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.browser.ChromeApplication;
@@ -47,6 +50,7 @@ import java.util.concurrent.TimeoutException;
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 // See: https://crbug.com/1120707
 @DisableIf.Device(type = {UiDisableIf.TABLET})
+@Batch(PER_CLASS)
 public class TrustedWebActivityPermissionsTest {
     @Rule
     public CustomTabActivityTestRule mCustomTabActivityTestRule = new CustomTabActivityTestRule();
@@ -63,6 +67,7 @@ public class TrustedWebActivityPermissionsTest {
 
     @Before
     public void setUp() throws TimeoutException {
+        mCustomTabActivityTestRule.setFinishActivity(true);
         // Native needs to be initialized to start the test server.
         LibraryLoader.getInstance().ensureInitialized();
 
