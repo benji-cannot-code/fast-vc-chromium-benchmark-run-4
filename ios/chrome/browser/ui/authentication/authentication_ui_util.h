5f317f9199804c3028a099be0ae6218f10f6051c
Jérôme Lebel <jlebel@chromium.org>
2021-01-22 13:22:16 +0000

    [iOS] Adding signout dialog for Mice
    Adds signout confirmation dialog for Mice based on this design:
    https://docs.google.com/presentation/d/1mfVdeJXk2hLXjyzOp9TBIx0E7-7jmuoN1-4oB6dYd5I/edit#slide=id.gaea85d524f_2_0
    
    iPad:
    https://drive.google.com/file/d/1UAS0L-ANTaIDEN2FFh2mgAiwKe-ksQ5U
    https://drive.google.com/file/d/1kQJoi6Vqe-xEsbLokmePAlAWU5kNl99T
    https://drive.google.com/file/d/1DUou8tJ1UR54lQtI2Zzi5smJ3aoUbyXX
    
    iPhone:
    https://drive.google.com/file/d/13TPquf7Z8HbdkCDmQhXnyqwP0VQGxwTp
    https://drive.google.com/file/d/1UjLwwkEk4RKnkDfX-7BSLmfqo6Lj1Ewd
    https://drive.google.com/file/d/1aykwVlgT3npD3DQkMCPWJm3hYiMeJfLx
    
    Bug: 1166148
    Change-Id: Ica58a513ee216d79f4994d155a4941ea5fdbe3d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2627401
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846086}

--- ios/chrome/browser/ui/authentication/authentication_ui_util.h
+++ ios/chrome/browser/ui/authentication/authentication_ui_util.h
@@ -9,9 +9,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 #include "base/ios/block_types.h"
+#include "base/strings/string16.h"
 
 @class AlertCoordinator;
+@class ActionSheetCoordinator;
 class Browser;
+
+// Sign-out result, related to SignoutActionSheetCoordinator().
+typedef NS_ENUM(NSUInteger, SignoutActionSheetCoordinatorResult) {
+  // The user canceled the sign-out confirmation dialog.
+  SignoutActionSheetCoordinatorResultCanceled,
+  // The user chose to sign-out and clear their data from the device.
+  SignoutActionSheetCoordinatorResultClearFromDevice,
+  // The user chose to sign-out and keep their data on the device.
+  SignoutActionSheetCoordinatorResultKeepOnDevice,
+};
+
+// Sign-out completion block.
+using SignoutActionSheetCoordinatorCompletion =
+    void (^)(SignoutActionSheetCoordinatorResult result);
+
+// Returns the hosted domain for the primary account.
+base::string16 HostedDomainForPrimaryAccount(Browser* browser);
+
+// Returns ActionSheetCoordinator to ask the sign-out confirmation from the
+// user. The alert sheet dialog choices are based if the primary account is
+// managed account or not, and if the user turned on sync or not.
+// The caller is in charge to start the coordinator.
+// The caller is in charge to sign-out the user and wipe the data from the
+// device according to |signout_completion| value.
+// |view_controller| view controller to present the action sheet.
+// |view| to position the action sheet for iPad.
+// |signout_completion| invoked when the user confirms or cancels sign-out.
+ActionSheetCoordinator* SignoutActionSheetCoordinator(
+    UIViewController* view_controller,
+    Browser* browser,
+    UIView* view,
+    SignoutActionSheetCoordinatorCompletion signout_completion);
 
 // Returns the sign in alert coordinator for |error|. |dismissAction| is called
 // when the dialog is dismissed (the user taps on the Ok button) or cancelled
