ac0f4421bb96eaa492d41740b254eb1503558482
Michael Thiessen <mthiesse@chromium.org>
2021-01-22 02:46:39 +0000

    Add support for new PendingIntent.FLAG_MUTABLE
    See bug for context. FLAG_IMMUTABLE should be used as the default for
    PendingIntents when mutability is not required as it is more secure. A
    mutability flag should be explicitly set for all PendingIntents as when
    unspecified Android makes PendingIntents mutable.
    
    Bug: 1163529
    Change-Id: I28bf2d277c72edaf03a7133a0302326ad2357f00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643099
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#845889}

--- base/android/java/src/org/chromium/base/compat/ApiHelperForM.java
+++ base/android/java/src/org/chromium/base/compat/ApiHelperForM.java
@@ -7,6 +7,7 @@ package org.chromium.base.compat;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkInfo;
@@ -117,5 +118,9 @@ public final class ApiHelperForM {
 
     public static void hideActionMode(ActionMode actionMode, long duration) {
         actionMode.hide(duration);
+    }
+
+    public static int getPendingIntentImmutableFlag() {
+        return PendingIntent.FLAG_IMMUTABLE;
     }
 }
