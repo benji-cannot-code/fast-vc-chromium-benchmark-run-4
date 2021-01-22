4882ba66fa78ea459ae6e3a1c15c4764891c795d
Jesse McKenna <jessemckenna@google.com>
2021-01-22 17:11:52 +0000

    Add metric for potential iframe process reuse
    This change adds a metric to measure how often a navigation occurs
    where the target URL matches a RenderProcessHost that was destroyed in
    the past few seconds.
    
    The new metric, "SiteIsolation.ReusePendingOrCommittedSite.
    TimeSinceReusableProcessDestroyed", emits on every navigation. It
    records the time (in ms) since a RenderProcessHost with a matching
    process lock was destroyed, up to 7 seconds. If no matching host was
    destroyed within the past 7 seconds, it records a sentinel value of 10
    seconds.
    
    Knowing the most common times since reusable processes were destroyed
    will inform a broader experiment to potentially delay subframe process
    shutdown by a few seconds to reduce process churn. Including the
    sentinel value adds information around how frequently reusable
    processes are destroyed, i.e., how much opportunity for process reuse
    exists.
    
    Test plan (see "Metrics" section for details on this metric):
    https://docs.google.com/document/d/11XruVZzYLSkt4se6IV2O4jFOkKs-QbkRB5NMf4mH_hE
    
    Bug: 894253
    Change-Id: Ic15089d1e3b086480942b6afe32525b3c6461895
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2575945
    Reviewed-by: Oliver Li <olivierli@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/master@{#846167}

--- content/browser/child_process_security_policy_impl.cc
+++ content/browser/child_process_security_policy_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/child_process_security_policy_impl.h"
 
 #include <algorithm>
+#include <tuple>
 #include <utility>
 
 #include "base/bind.h"
@@ -235,6 +236,19 @@ bool ProcessLock::operator==(const ProcessLock& rhs) const {
 
 bool ProcessLock::operator!=(const ProcessLock& rhs) const {
   return !(*this == rhs);
+}
+
+bool ProcessLock::operator<(const ProcessLock& rhs) const {
+  const auto this_is_origin_keyed = is_origin_keyed();
+  const auto this_coop_coep_cross_origin_isolated_info =
+      coop_coep_cross_origin_isolated_info();
+  const auto rhs_is_origin_keyed = is_origin_keyed();
+  const auto rhs_coop_coep_cross_origin_isolated_info =
+      coop_coep_cross_origin_isolated_info();
+  return std::tie(lock_url(), this_is_origin_keyed,
+                  this_coop_coep_cross_origin_isolated_info) <
+         std::tie(rhs.lock_url(), rhs_is_origin_keyed,
+                  rhs_coop_coep_cross_origin_isolated_info);
 }
 
 std::string ProcessLock::ToString() const {
