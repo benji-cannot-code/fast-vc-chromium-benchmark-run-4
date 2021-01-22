c8b44cd376cfb6d415642b46d4f79b7c89d9ce58
siashah <siashah@chromium.org>
2021-01-22 23:02:50 +0000

    Sort suggestions based on offer_label
    This would impact the order of autofill suggestions on all platforms.
    
    Bug: 1156916
    Change-Id: Icac331b398a127d21b84ab6b322d20290639ade9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2644244
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846377}

--- components/autofill/core/browser/payments/autofill_offer_manager.cc
+++ components/autofill/core/browser/payments/autofill_offer_manager.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/autofill_offer_data.h"
 #include "components/autofill/core/browser/payments/payments_client.h"
 #include "components/autofill/core/common/autofill_clock.h"
+#include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
@@ -66,6 +67,18 @@ void AutofillOfferManager::UpdateSuggestionsWithOffers(
       suggestion.offer_label =
           l10n_util::GetStringUTF16(IDS_AUTOFILL_OFFERS_CASHBACK);
     }
+  }
+  // Sort the suggestions such that suggestions with offers are shown at the
+  // top.
+  if (base::FeatureList::IsEnabled(
+          features::kAutofillSortSuggestionsBasedOnOfferPresence)) {
+    std::sort(suggestions.begin(), suggestions.end(),
+              [](const Suggestion& a, const Suggestion& b) {
+                if (!a.offer_label.empty() && b.offer_label.empty()) {
+                  return true;
+                }
+                return false;
+              });
   }
 }
 
