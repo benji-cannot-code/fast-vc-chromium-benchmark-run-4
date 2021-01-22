19905d43b3b86db9058115902ed4b1b6f8a89d8b
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-01-22 00:05:59 +0000

    Import wpt@3bb66241b7e3066c6d4ebfca14379e1c8f650385
    Using wpt-import in Chromium bbb681f030c6e936c1a1c0eba0d955cbb1f2a212.
    With Chromium commits locally applied on WPT:
    820c25f5b3 "Prevent underinvalidation of <pattern> with template"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    TBR=lpz@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Ia665a4b9b4e60676b8153d3418d6d7f86112566c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643636
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#845840}

--- third_party/blink/web_tests/external/wpt/resources/testharness.js
+++ third_party/blink/web_tests/external/wpt/resources/testharness.js
@@ -501,11 +501,7 @@ policies and contribution forms [3].
             return new DedicatedWorkerTestEnvironment();
         }
 
-        if (!('location' in global_scope)) {
-            return new ShellTestEnvironment();
-        }
-
-        throw new Error("Unsupported test environment");
+        return new ShellTestEnvironment();
     }
 
     var test_environment = create_test_environment();
