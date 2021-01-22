0517bd4cadbe71164fb5af96a42b6a44956d7a98
Peng Huang <penghuang@chromium.org>
2021-01-22 17:50:53 +0000

    Support EGL_ANGLE_external_context_and_surface in //ui/gl
    This CL adds support for EGL_ANGLE_external_context_and_surface.
    With this extension, it wraps a native EGL context to an ANGLE EGL
    context, so we can use ANGLE on a native EGL context. And call ANGLE
    eglMakeCurrent() with this ANGLE context, will not change the native
    EGL current context.
    And if this external ANGLE context is created with
    EGL_EXTERNAL_CONTEXT_SAVE_STATE_ANGLE. ANGLE will save GL state of
    the native GL when this ANGLE context is made current. And ANGLE will
    restore the saved GL state of the native context, when this ANGLE
    context is released with eglMakeCurrent(EGL_NO_SURFACE, EGL_NO_CONTEXT).
    
    Bug: 1157501
    Change-Id: If7bb23dda18f09866fe33d185ef0f21a78a6ba1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2638194
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846194}

--- ui/gl/gl_surface_egl.cc
+++ ui/gl/gl_surface_egl.cc
@@ -207,6 +207,7 @@ bool g_egl_android_native_fence_sync_supported = false;
 bool g_egl_ext_pixel_format_float_supported = false;
 bool g_egl_angle_feature_control_supported = false;
 bool g_egl_angle_power_preference_supported = false;
+bool g_egl_angle_external_context_and_surface_supported = false;
 EGLGpuSwitchingObserver* g_egl_gpu_switching_observer = nullptr;
 
 constexpr const char kSwapEventTraceCategories[] = "gpu";
@@ -1063,6 +1064,9 @@ bool GLSurfaceEGL::InitializeOneOffCommon() {
   g_egl_angle_power_preference_supported =
       HasEGLExtension("EGL_ANGLE_power_preference");
 
+  g_egl_angle_external_context_and_surface_supported =
+      HasEGLExtension("EGL_ANGLE_external_context_and_surface");
+
   if (g_egl_angle_power_preference_supported) {
     g_egl_gpu_switching_observer = new EGLGpuSwitchingObserver();
     ui::GpuSwitchingManager::GetInstance()->AddObserver(
@@ -1215,7 +1219,11 @@ bool GLSurfaceEGL::IsANGLEPowerPreferenceSupported() {
   return g_egl_angle_power_preference_supported;
 }
 
-GLSurfaceEGL::~GLSurfaceEGL() {}
+bool GLSurfaceEGL::IsANGLEExternalContextAndSurfaceSupported() {
+  return g_egl_angle_external_context_and_surface_supported;
+}
+
+GLSurfaceEGL::~GLSurfaceEGL() = default;
 
 // InitializeDisplay is necessary because the static binding code
 // needs a full Display init before it can query the Display extensions.
