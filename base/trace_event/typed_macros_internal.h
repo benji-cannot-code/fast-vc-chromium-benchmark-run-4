1f0d8c9067c9f71a4461ce0216cda5378bcfc29b
Eric Seckler <eseckler@chromium.org>
2021-01-22 19:21:40 +0000

    tracing: Actually fix track_uuid association for typed events
    Follow-up to crrev.com/c/2644798. The symbol also needs to be exported
    and defined in a .cc.
    
    Bug: 1168791
    Change-Id: I0c7308bfe24846381d3dcad00e8d76f337909634
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2645005
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846241}

--- base/trace_event/typed_macros_internal.h
+++ base/trace_event/typed_macros_internal.h
@@ -60,7 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace trace_event_internal {
 
-static const perfetto::Track kDefaultTrack{};
+extern BASE_EXPORT const perfetto::Track kDefaultTrack;
 
 // Copy of function with the same name from Perfetto client library.
 template <typename T>
