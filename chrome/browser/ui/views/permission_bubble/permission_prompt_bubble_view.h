1333bc2951544fb4d819497da41a721535f69f2e
Bret Sepulveda <bsep@chromium.org>
2021-01-22 12:23:17 +0000

    Refactor PermissionPromptBubbleView to remove visible_requests_.
    The API comment for PermissionPrompt::Delegate::Requests forbids clients
    from storing the pointers to PermissionRequest objects, which is
    violated by PermissionPromptBubbleView::visible_requests_. This patch
    removes that field and recalculates the visible requests in the two
    places it's required.
    
    This also happens to be convenient for further refactoring plans.
    
    Bug: 1110905
    Change-Id: I018b7d9fc00445b05268db9e005377a64bfe138e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2577497
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846068}

--- chrome/browser/ui/views/permission_bubble/permission_prompt_bubble_view.h
+++ chrome/browser/ui/views/permission_bubble/permission_prompt_bubble_view.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/permissions/permission_prompt.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
+namespace permissions {
+enum class RequestType;
+}
+
 class Browser;
 
 // Bubble that prompts the user to grant or deny a permission request from a
@@ -51,9 +55,9 @@ class PermissionPromptBubbleView : public views::BubbleDialogDelegateView {
     bool is_origin;
   };
 
-  std::vector<permissions::PermissionRequest*> GetVisibleRequests();
-  bool ShouldShowPermissionRequest(permissions::PermissionRequest* request);
-  void AddPermissionRequestLine(permissions::PermissionRequest* request);
+  bool ShouldShowRequest(permissions::RequestType type) const;
+  std::vector<permissions::PermissionRequest*> GetVisibleRequests() const;
+  void AddRequestLine(permissions::PermissionRequest* request);
 
   // Returns the origin to be displayed in the permission prompt. May return
   // a non-origin, e.g. extension URLs use the name of the extension.
@@ -72,9 +76,6 @@ class PermissionPromptBubbleView : public views::BubbleDialogDelegateView {
 
   Browser* const browser_;
   permissions::PermissionPrompt::Delegate* const delegate_;
-
-  // List of permission requests that should be visible in the bubble.
-  std::vector<permissions::PermissionRequest*> visible_requests_;
 
   // The requesting domain's name or origin.
   const DisplayNameOrOrigin name_or_origin_;
