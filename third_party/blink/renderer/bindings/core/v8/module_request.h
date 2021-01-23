16199dca6ac3590fe1a222aec55f80b3852e2686
Daniel Clark <daniec@microsoft.com>
2021-01-23 03:06:11 +0000

    Update ModuleRecordTest to handle potential non-'type' assertions
    Update ModuleRecordTest.moduleRequestsWithImportAssertions to handle the
    presence of import assertions other than 'type'.  This change switches
    the test to use ModuleRequest::GetModuleTypeString(), which will produce
    the same result both before and after the assertions filtering is
    removed from V8.  Doing this also removes the dependency on the order of
    the assertions in the FixedArray, which embedders are not supposed to
    depend on and is subject to change.
    
    Note, using GetModuleTypeString in module_record_test.cc required making
    ModuleRequest a CORE_EXPORT.
    
    Bug: 1132413
    Change-Id: Id87c384440a72866cb770cb787d6587da171d90c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2645349
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#846459}

--- third_party/blink/renderer/bindings/core/v8/module_request.h
+++ third_party/blink/renderer/bindings/core/v8/module_request.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_MODULE_REQUEST_H_
 #define THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_MODULE_REQUEST_H_
 
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/wtf/text/text_position.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -27,7 +28,7 @@ struct ImportAssertion {
 // https://tc39.es/proposal-import-assertions/#sec-modulerequest-record
 // Represents a module script's request to import a module given a specifier and
 // list of import assertions.
-struct ModuleRequest {
+struct CORE_EXPORT ModuleRequest {
   String specifier;
   TextPosition position;
   Vector<ImportAssertion> import_assertions;
