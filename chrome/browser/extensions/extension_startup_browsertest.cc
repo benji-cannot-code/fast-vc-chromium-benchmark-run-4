3e25152e265576b881b877c2723fd848559aeb51
Kelvin Jiang <kelvinjiang@chromium.org>
2021-01-23 07:02:54 +0000

    [Extensions] Remove NOTIFICATON_USER_SCRIPTS_UPDATED notification
    Currently, the notification is being used only in test code. Some tests
    can have the notification observer removed entirely as loading an
    extension inside a test now will wait for content scripts to load.
    
    The remaining tests have been converted to use
    UserScriptLoader::Observer. Subsequent patchsets will re-examine these
    tests to see if they could be rewritten to not require the listener.
    
    Bug: 1164483
    Change-Id: Ie971ba607aab2a6aa71fed99b7ecfddfca8d1669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2618551
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846493}

--- chrome/browser/extensions/extension_startup_browsertest.cc
+++ chrome/browser/extensions/extension_startup_browsertest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_util.h"
@@ -32,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/notification_details.h"
-#include "content/public/browser/notification_service.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
@@ -46,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/feature_switch.h"
 #include "extensions/common/manifest.h"
 #include "extensions/common/switches.h"
+#include "extensions/test/test_content_script_load_waiter.h"
 #include "net/base/filename_util.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -153,15 +152,14 @@ class ExtensionStartupTestBase : public InProcessBrowserTest {
             ->extension_service();
     ASSERT_EQ(expect_extensions_enabled, service->extensions_enabled());
 
-    content::WindowedNotificationObserver user_scripts_observer(
-        extensions::NOTIFICATION_USER_SCRIPTS_UPDATED,
-        content::NotificationService::AllSources());
     extensions::ExtensionUserScriptManager* manager =
         extensions::ExtensionSystem::Get(browser()->profile())
             ->extension_user_script_manager();
-    if (!manager->script_loader()->initial_load_complete())
-      user_scripts_observer.Wait();
-    ASSERT_TRUE(manager->script_loader()->initial_load_complete());
+
+    extensions::UserScriptLoader* loader = manager->script_loader();
+    if (!loader->initial_load_complete())
+      extensions::ContentScriptLoadWaiter(loader).Wait();
+    ASSERT_TRUE(loader->initial_load_complete());
   }
 
   void TestInjection(bool expect_css, bool expect_script) {
@@ -207,7 +205,6 @@ class ExtensionStartupTestBase : public InProcessBrowserTest {
   int num_expected_extensions_;
 };
 
-
 // ExtensionsStartupTest
 // Ensures that we can startup the browser with --enable-extensions and some
 // extensions installed and see them run and do basic things.
@@ -241,13 +238,17 @@ IN_PROC_BROWSER_TEST_F(ExtensionsStartupTest, DISABLED_NoFileAccess) {
       extension_list.push_back(it->get());
   }
 
+  extensions::ExtensionUserScriptManager* manager =
+      extensions::ExtensionSystem::Get(browser()->profile())
+          ->extension_user_script_manager();
+
+  extensions::UserScriptLoader* loader = manager->script_loader();
+
   for (size_t i = 0; i < extension_list.size(); ++i) {
-    content::WindowedNotificationObserver user_scripts_observer(
-        extensions::NOTIFICATION_USER_SCRIPTS_UPDATED,
-        content::NotificationService::AllSources());
+    extensions::ContentScriptLoadWaiter waiter(loader);
     extensions::util::SetAllowFileAccess(extension_list[i]->id(),
                                          browser()->profile(), false);
-    user_scripts_observer.Wait();
+    waiter.Wait();
   }
 
   TestInjection(false, false);
