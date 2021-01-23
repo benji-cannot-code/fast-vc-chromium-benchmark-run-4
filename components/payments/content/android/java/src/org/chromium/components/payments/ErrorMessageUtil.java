067874e40779ef97fc1303822beb740cb1ab0652
Liquan (Max) Gu <maxlg@chromium.org>
2021-01-23 23:06:31 +0000

    [GPayTest] Replace ShadowErrorMessageUtil with JniMocker
    ShadowErrorMessageUtil was originally used to stub ErrorMessageUtil
    in PaymentRequestIntegrationTest. However, a shadow was overkilling for
    this use case as we can use a lighter-weight JniMocker instead. This
    CL replaces ShadowErrorMessageUtil with a mock supported by JniMocker.
    
    Bug: 1139465
    Change-Id: Ia5750767c4e8769ae2144d2fd2f67eb4598c492c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2602384
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846542}

--- components/payments/content/android/java/src/org/chromium/components/payments/ErrorMessageUtil.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/ErrorMessageUtil.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.payments;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
 
@@ -28,7 +30,8 @@ public class ErrorMessageUtil {
      * ErrorMessageUtilJni.
      */
     @NativeMethods
-    /* package */ interface Natives {
+    @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
+    public interface Natives {
         String getNotSupportedErrorMessage(String[] methods);
     }
 }
