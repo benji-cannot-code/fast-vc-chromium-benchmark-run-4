f240f34d6110fe61ffa3bd1a72b2a27c4635880d
Christopher Cameron <ccameron@chromium.org>
2021-01-23 19:51:16 +0000

    Revert "Use shared images with DXVA decoder"
    This reverts commit 7bcda0578702513ee033d2cd9306eac9ef8b37ed.
    
    Reason for revert: crbug.com/1168790
    
    This change removed logic which macOS depended on.
    
    Original change's description:
    > Use shared images with DXVA decoder
    >
    > Adds support to bind video textures used for overlays
    > with the shared image system. This change enables
    > overlays to be backed by DXVA decoded output textures
    > by wrapping them in a mailbox per plane.
    >
    > Bug: 1011555
    > Change-Id: Ide149097c1331c0bcb3dc99ae67def8bf4768472
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2485682
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Frank Liberato <liberato@chromium.org>
    > Commit-Queue: Bryan Bernhart <bryan.bernhart@intel.com>
    > Cr-Commit-Position: refs/heads/master@{#842699}
    
    TBR=sunnyps@chromium.org,liberato@chromium.org,bryan.bernhart@intel.com,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1011555, 1168790
    Change-Id: If62cc91646ba9ddd1b712ba8e2aa3777dc0b967a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643411
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846532}

--- media/video/picture.h
+++ media/video/picture.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "gpu/command_buffer/common/mailbox_holder.h"
 #include "media/base/media_export.h"
-#include "media/base/video_frame.h"
 #include "media/base/video_types.h"
 #include "ui/gfx/color_space.h"
 #include "ui/gfx/geometry/rect.h"
@@ -27,7 +26,6 @@ namespace media {
 class MEDIA_EXPORT PictureBuffer {
  public:
   using TextureIds = std::vector<uint32_t>;
-  using TextureSizes = std::vector<gfx::Size>;
 
   PictureBuffer(int32_t id, const gfx::Size& size);
   PictureBuffer(int32_t id,
@@ -43,13 +41,6 @@ class MEDIA_EXPORT PictureBuffer {
                 const gfx::Size& size,
                 const TextureIds& client_texture_ids,
                 const std::vector<gpu::Mailbox>& texture_mailboxes,
-                uint32_t texture_target,
-                VideoPixelFormat pixel_format);
-  PictureBuffer(int32_t id,
-                const gfx::Size& size,
-                const TextureSizes& texture_sizes,
-                const TextureIds& client_texture_ids,
-                const TextureIds& service_texture_ids,
                 uint32_t texture_target,
                 VideoPixelFormat pixel_format);
   PictureBuffer(const PictureBuffer& other);
@@ -74,12 +65,11 @@ class MEDIA_EXPORT PictureBuffer {
 
   VideoPixelFormat pixel_format() const { return pixel_format_; }
 
-  gfx::Size texture_size(size_t plane) const;
+  gpu::Mailbox texture_mailbox(size_t plane) const;
 
  private:
   int32_t id_;
   gfx::Size size_;
-  TextureSizes texture_sizes_;
   TextureIds client_texture_ids_;
   TextureIds service_texture_ids_;
   std::vector<gpu::Mailbox> texture_mailboxes_;
@@ -162,16 +152,11 @@ class MEDIA_EXPORT Picture {
   }
 
   void set_scoped_shared_image(
-      scoped_refptr<ScopedSharedImage> scoped_shared_image,
-      uint32_t plane = 0) {
-    DCHECK(plane < scoped_shared_images_.size());
-    scoped_shared_images_[plane] = scoped_shared_image;
+      scoped_refptr<ScopedSharedImage> scoped_shared_image) {
+    scoped_shared_image_ = scoped_shared_image;
   }
-
-  scoped_refptr<ScopedSharedImage> scoped_shared_image(
-      uint32_t plane = 0) const {
-    DCHECK(plane < scoped_shared_images_.size());
-    return scoped_shared_images_[plane];
+  scoped_refptr<ScopedSharedImage> scoped_shared_image() const {
+    return scoped_shared_image_;
   }
 
  private:
@@ -184,8 +169,7 @@ class MEDIA_EXPORT Picture {
   bool size_changed_;
   bool texture_owner_;
   bool wants_promotion_hint_;
-  std::array<scoped_refptr<ScopedSharedImage>, VideoFrame::kMaxPlanes>
-      scoped_shared_images_;
+  scoped_refptr<ScopedSharedImage> scoped_shared_image_;
 };
 
 }  // namespace media
