2fa4b7a0ef463d091c802a2770770de228ba3a4d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-01-23 13:31:35 +0000

    Import wpt@35077458592d7cf9349840211759aa85f20cd20c
    Using wpt-import in Chromium 9b0e63f98a449d96f3bc28f2062a7e6188fc4e0e.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    binji@chromium.org:
      external/wpt/wasm
    
    NOAUTOREVERT=true
    TBR=robertma@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Ib4c23802d4060b8d31f379d7eb41ac4562b704df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643898
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#846511}

--- third_party/blink/web_tests/external/wpt/wasm/jsapi/table/constructor-reftypes.tentative.any.js
+++ third_party/blink/web_tests/external/wpt/wasm/jsapi/table/constructor-reftypes.tentative.any.js
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: global=window,dedicatedworker,jsshell
 // META: script=assertions.js
-// META: script=/wasm/jsapi/wasm-constants.js
 // META: script=/wasm/jsapi/wasm-module-builder.js
 
 // Test cases for changes to the WebAssembly.Table constructor API that
