3b275d18dd5ea8f4b6f97c179fc20a330e02f992
Mitsuru Oshima <oshima@chromium.org>
2021-01-24 21:20:40 +0000

    Migrate might_have_observers() to empty() in components/{no_state_prefetch|data_use_measurement}
    empty() does not return false positive.
    
    Bug: 1155308
    
    Change-Id: I1fe026fba4f0a135bd400c0bf9de79fc1f0bf838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2646189
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Auto-Submit: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846598}

--- components/no_state_prefetch/renderer/prerender_observer_list.cc
+++ components/no_state_prefetch/renderer/prerender_observer_list.cc
@@ -65,7 +65,7 @@ void PrerenderObserverList::AddObserver(PrerenderObserver* observer) {
 
 bool PrerenderObserverList::RemoveObserver(PrerenderObserver* observer) {
   prerender_observers_.RemoveObserver(observer);
-  return !prerender_observers_.might_have_observers();
+  return prerender_observers_.empty();
 }
 
 void PrerenderObserverList::SetIsPrerendering(bool is_prerendering) {
