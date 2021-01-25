399ca2adf67af0790abe5872eb5aad7aa44fe979
Filip Gorski <fgorski@chromium.org>
2021-01-25 21:13:27 +0000

    [Supplier] Adding hasValue() default method
    This patch also removes an old TODO (bug is fixed) and updates a random
    place in code to test the cost of this approach.
    
    Bug: 1163562
    Change-Id: Ic41cfe3cf5e5bd54cffb4fda99fc8a19ad77e6e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2613130
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846881}

--- base/android/java/src/org/chromium/base/supplier/Supplier.java
+++ base/android/java/src/org/chromium/base/supplier/Supplier.java
@@ -5,17 +5,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.supplier;
 
+import androidx.annotation.Nullable;
+
 /**
  * Based on Java 8's java.util.function.Supplier.
- * Same as Callable<T>, but without a checked Exception.
+ * Similar to Callable<T>, but without a checked Exception and with #hasValue().
  *
  * @param <T> Return type.
  */
 public interface Supplier<T> {
-    // TODO(crbug.com/1034012): Remove once min Android API level reaches 24.
+    /** Returns the value currently held or <code>null</code> when none is held. */
+    @Nullable
+    T get();
 
     /**
-     * Returns a value.
+     * Returns whether the supplier holds a value currently.
+     *
+     * This default implementation should only be used with trivial implementation of #get(), where
+     * the supplier object is accessed. In case it is created on demand, or the Supplier
+     * implementation provides a new one every time, this method must be overridden.
      */
-    T get();
+    default boolean hasValue() {
+        T t = get();
+        assert t == get() : "Value provided by #get() must not change.";
+        return t != null;
+    }
 }
