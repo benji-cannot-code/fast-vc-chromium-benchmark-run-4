69b7250e1d75415e8982caf1706e2a1892f21426
Darwin Huang <huangdarwin@chromium.org>
2021-01-25 21:23:57 +0000

    Revert "Enable integration test for First scroll delay."
    This reverts commit 4613f327ce6c05e7983484af965215ca3dc549cd.
    
    Reason for revert: Test is still flaky on Linux Tester (Ozone X11). 
    
    First Failure here: https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tester%20%28Ozone%20X11%29/4239
    
    More info in bug.
    
    Original change's description:
    > Enable integration test for First scroll delay.
    >
    > The integration test for FSD used to be flaky due to the data loss problem like LCP. We record UKM before the FSD value get sent from the renderer process to the browser process. Now the problem has been solved. It's time to enable the integration test again.
    >
    > Bug: 1098148
    > Change-Id: I29bdba0b0f792cff9e799ec3d1162b295dbc15e1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2645932
    > Reviewed-by: Steve Kobes <skobes@chromium.org>
    > Commit-Queue: Hongbo Song <hbsong@google.com>
    > Cr-Commit-Position: refs/heads/master@{#846772}
    
    TBR=skobes@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,hbsong@google.com
    
    Change-Id: I0bc4052da97b6c8fdaa1ba2f39b108b3f5021e7a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1098148
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648885
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846887}

--- chrome/browser/page_load_metrics/integration_tests/first_scroll_delay_browsertest.cc
+++ chrome/browser/page_load_metrics/integration_tests/first_scroll_delay_browsertest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using ukm::builders::PageLoad;
 
 // http://crbug.com/1098148
-IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, FirstScrollDelay) {
+IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, DISABLED_FirstScrollDelay) {
   LoadHTML(R"HTML(
 <div id="content">
   <p>This is some text</p>
