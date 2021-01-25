9bac448bcabfa95b7c7fbc699c08b62e8c2946a7
Marc Treib <treib@chromium.org>
2021-01-25 17:48:14 +0000

    DataTypeManagerImpl: Call RecordConfigurationStats in one place only
    Before this CL, RecordConfigurationStats() was called from two places:
    Once for types that were already-downloaded, and then again for all
    types including newly-downloaded ones.
    This CL drops the first call - the exact same stats will anyway be
    recorded by the second call. It also adds a test that makes sure the
    stats are still complete.
    
    Note: By itself, this CL doesn't really improve anything much, but it
    unlocks further cleanups.
    
    Bug: 1102837
    Change-Id: I595338c04759c96529dc68c55c9078194e311f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2644812
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846758}

--- components/sync/test/model/fake_model_type_controller_delegate.cc
+++ components/sync/test/model/fake_model_type_controller_delegate.cc
@@ -20,6 +20,11 @@ FakeModelTypeControllerDelegate::FakeModelTypeControllerDelegate(ModelType type)
 
 FakeModelTypeControllerDelegate::~FakeModelTypeControllerDelegate() {}
 
+void FakeModelTypeControllerDelegate::SetModelTypeStateForActivationResponse(
+    const sync_pb::ModelTypeState& model_type_state) {
+  model_type_state_for_activation_response_ = model_type_state;
+}
+
 void FakeModelTypeControllerDelegate::EnableManualModelStart() {
   manual_model_start_enabled_ = true;
 }
@@ -27,8 +32,7 @@ void FakeModelTypeControllerDelegate::EnableManualModelStart() {
 void FakeModelTypeControllerDelegate::SimulateModelStartFinished() {
   DCHECK(manual_model_start_enabled_);
   if (start_callback_) {
-    std::move(start_callback_)
-        .Run(std::make_unique<DataTypeActivationResponse>());
+    std::move(start_callback_).Run(MakeActivationResponse());
   }
 }
 
@@ -70,7 +74,7 @@ void FakeModelTypeControllerDelegate::OnSyncStarting(
     start_callback_ = std::move(callback);
   } else {
     // Trigger completion immediately.
-    std::move(callback).Run(std::make_unique<DataTypeActivationResponse>());
+    std::move(callback).Run(MakeActivationResponse());
   }
 }
 
@@ -96,6 +100,13 @@ void FakeModelTypeControllerDelegate::GetTypeEntitiesCountForDebugging(
 base::WeakPtr<ModelTypeControllerDelegate>
 FakeModelTypeControllerDelegate::GetWeakPtr() {
   return weak_ptr_factory_.GetWeakPtr();
+}
+
+std::unique_ptr<DataTypeActivationResponse>
+FakeModelTypeControllerDelegate::MakeActivationResponse() const {
+  auto response = std::make_unique<DataTypeActivationResponse>();
+  response->model_type_state = model_type_state_for_activation_response_;
+  return response;
 }
 
 }  // namespace syncer
