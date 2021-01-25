b649948577b3afc7f9717a39bed01cd47a792ae9
Mitsuru Oshima <oshima@chromium.org>
2021-01-25 21:06:42 +0000

    Migrate might_have_observers() to empty() in cc/
    empty() does not return false positive.
    
    Bug: 1155308
    
    Change-Id: I6f1ca98957a5d13134ac80ba732f5b0100ed19e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2646706
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846877}

--- cc/animation/element_animations.cc
+++ cc/animation/element_animations.cc
@@ -153,7 +153,7 @@ void ElementAnimations::RemoveKeyframeEffect(KeyframeEffect* keyframe_effect) {
 }
 
 bool ElementAnimations::IsEmpty() const {
-  return !keyframe_effects_list_.might_have_observers();
+  return keyframe_effects_list_.empty();
 }
 
 void ElementAnimations::SetNeedsPushProperties() {
@@ -581,7 +581,7 @@ unsigned int ElementAnimations::CountKeyframesForTesting() const {
 }
 
 KeyframeEffect* ElementAnimations::FirstKeyframeEffectForTesting() const {
-  DCHECK(keyframe_effects_list_.might_have_observers());
+  DCHECK(!keyframe_effects_list_.empty());
   return &*keyframe_effects_list_.begin();
 }
 
