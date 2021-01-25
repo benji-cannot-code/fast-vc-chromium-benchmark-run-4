0c0638f95bf2cde4e2af2181a794164d5be8a56b
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2021-01-25 22:09:20 +0000

    Use ScopedProfileKeepAlive in MediaEngagementBrowserTest
    These tests already hold a ScopedKeepAlive to test their logic without
    deleting the BrowserProcess. Add a ScopedProfileKeepAlive so they don't
    delete the Profile* either, once we start enabling
    DestroyProfileOnBrowserClose by default on CQ.
    
    Bug: 88586
    Change-Id: I16067257c36fab5abc2bd06f2b8d710e3301b247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648166
    Auto-Submit: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846913}

--- chrome/browser/media/media_engagement_browsertest.cc
+++ chrome/browser/media/media_engagement_browsertest.cc
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefetch/no_state_prefetch/prerender_test_utils.h"
 #include "chrome/browser/prefs/session_startup_pref.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_keep_alive_types.h"
+#include "chrome/browser/profiles/scoped_profile_keep_alive.h"
 #include "chrome/browser/sessions/session_restore.h"
 #include "chrome/browser/sessions/session_restore_test_helper.h"
 #include "chrome/browser/ui/browser.h"
@@ -789,6 +791,9 @@ class MediaEngagementSessionRestoreBrowserTest
 
     std::unique_ptr<ScopedKeepAlive> keep_alive(new ScopedKeepAlive(
         KeepAliveOrigin::SESSION_RESTORE, KeepAliveRestartOption::DISABLED));
+    std::unique_ptr<ScopedProfileKeepAlive> profile_keep_alive(
+        new ScopedProfileKeepAlive(profile,
+                                   ProfileKeepAliveOrigin::kBrowserWindow));
     CloseBrowserSynchronously(browser());
 
     chrome::NewEmptyWindow(profile);
