ec5786bc91d570d84567a7ac446402ee36a5c78c
Xi Han <hanxi@google.com>
2021-01-25 18:49:32 +0000

    Disable 2 OopRasterMode/PaintCanvasVideoRendererWithGLTest tests.
    Disable:
    OopRasterMode/PaintCanvasVideoRendererWithGLTest.CopyVideoFrameYUVDataToGLTexture_FlipY/1
    OopRasterMode/PaintCanvasVideoRendererWithGLTest.CopyVideoFrameYUVDataToGLTexture/1
    
    TBR=nazabris@microsoft.com,vasilyt@chromium.org
    
    Bug: 1170392
    Change-Id: I6fa4958562f7c498dc67ebc73523acbfe72b39b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2647631
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846793}

--- media/renderers/paint_canvas_video_renderer_unittest.cc
+++ media/renderers/paint_canvas_video_renderer_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/aligned_memory.h"
 #include "base/sys_byteorder.h"
 #include "base/test/task_environment.h"
+#include "build/build_config.h"
 #include "cc/paint/paint_flags.h"
 #include "cc/paint/skia_paint_canvas.h"
 #include "components/viz/common/gpu/context_provider.h"
@@ -1337,7 +1338,15 @@ INSTANTIATE_TEST_SUITE_P(OopRasterMode,
                          PaintCanvasVideoRendererWithGLTest,
                          testing::Bool());
 
-TEST_P(PaintCanvasVideoRendererWithGLTest, CopyVideoFrameYUVDataToGLTexture) {
+// Disabled on Android (https://crbug.com/1170392).
+#if defined(OS_ANDROID)
+#define MAYBE_CopyVideoFrameYUVDataToGLTexture \
+  DISABLED_CopyVideoFrameYUVDataToGLTexture
+#else
+#define MAYBE_CopyVideoFrameYUVDataToGLTexture CopyVideoFrameYUVDataToGLTexture
+#endif
+TEST_P(PaintCanvasVideoRendererWithGLTest,
+       MAYBE_CopyVideoFrameYUVDataToGLTexture) {
   auto* destination_gl = destination_context_->ContextGL();
   DCHECK(destination_gl);
   GLenum target = GL_TEXTURE_2D;
@@ -1368,8 +1377,16 @@ TEST_P(PaintCanvasVideoRendererWithGLTest, CopyVideoFrameYUVDataToGLTexture) {
   destination_gl->DeleteTextures(1, &texture);
 }
 
+// Disabled on Android (https://crbug.com/1170392).
+#if defined(OS_ANDROID)
+#define MAYBE_CopyVideoFrameYUVDataToGLTexture_FlipY \
+  DISABLED_CopyVideoFrameYUVDataToGLTexture_FlipY
+#else
+#define MAYBE_CopyVideoFrameYUVDataToGLTexture_FlipY \
+  CopyVideoFrameYUVDataToGLTexture_FlipY
+#endif
 TEST_P(PaintCanvasVideoRendererWithGLTest,
-       CopyVideoFrameYUVDataToGLTexture_FlipY) {
+       MAYBE_CopyVideoFrameYUVDataToGLTexture_FlipY) {
   auto* destination_gl = destination_context_->ContextGL();
   DCHECK(destination_gl);
   GLenum target = GL_TEXTURE_2D;
