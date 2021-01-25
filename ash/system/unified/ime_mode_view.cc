6ec974306b3ca0e230ca01e51328dd78d66400f9
Bao-Duy Tran <tranbaoduy@chromium.org>
2021-01-25 05:23:53 +0000

    Add dev flag-guarded colour tweaks for input method shelf indicators.
    These dramatic but unofficial colour tweaks are temporary, to help SWEs
    quickly identify whether the input methods system is in the new Mojo-UIL
    world, as guarded by disabled-by-default kImeMojoDecoder flag. They'll
    be reverted when the migration finishes, before launch.
    
    Bug: 1168354
    Change-Id: I566c442c0f1ec986e291d91f0d927a51f2e276fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2639453
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Leo Zhang <googleo@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Auto-Submit: Bao-Duy Tran <tranbaoduy@chromium.org>
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846625}

--- ash/system/unified/ime_mode_view.cc
+++ ash/system/unified/ime_mode_view.cc
@@ -14,7 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_utils.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
+#include "base/feature_list.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/label.h"
 
@@ -100,6 +103,12 @@ void ImeModeView::Update() {
   label()->SetText(ime_controller->current_ime().short_name);
   label()->SetEnabledColor(
       TrayIconColor(Shell::Get()->session_controller()->GetSessionState()));
+
+  // TODO(crbug.com/1168355): Remove before enabling kImeMojoDecoder by default.
+  if (base::FeatureList::IsEnabled(chromeos::features::kImeMojoDecoder)) {
+    label()->SetEnabledColor(gfx::kGoogleYellow500);
+  }
+
   base::string16 description =
       l10n_util::GetStringFUTF16(IDS_ASH_STATUS_TRAY_INDICATOR_IME_TOOLTIP,
                                  ime_controller->current_ime().name);
