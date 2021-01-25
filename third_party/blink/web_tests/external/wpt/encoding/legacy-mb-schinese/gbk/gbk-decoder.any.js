2e1c6b4ddc008158acf012b738c1bd17b27120a8
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-01-25 13:30:16 +0000

    Import wpt@8f2419328bdfb89fcc2f7368b8948d8cf289fed6
    Using wpt-import in Chromium 7d0adaefb535601b4ea3340595bf7c33c276706d.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/encoding
    
    NOAUTOREVERT=true
    TBR=lpz@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: I1013fc62cf40a2c931fcb84b41946ea12565306f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643809
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#846695}

--- third_party/blink/web_tests/external/wpt/encoding/legacy-mb-schinese/gbk/gbk-decoder.any.js
+++ third_party/blink/web_tests/external/wpt/encoding/legacy-mb-schinese/gbk/gbk-decoder.any.js
@@ -1,8 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<!doctype html>
-<script src=/resources/testharness.js></script>
-<script src=/resources/testharnessreport.js></script>
-<script>
 const gbkPointers = [
     6432, 7533, 7536, 7672, 7673, 7674, 7675, 7676, 7677, 7678, 7679, 7680, 7681, 7682, 7683, 7684,
     23766, 23770, 23771, 23772, 23773, 23774, 23776, 23777, 23778, 23779, 23780, 23781, 23782, 23784, 23785, 23786,
@@ -31,4 +27,3 @@ for (let i = 0; i < gbkPointers.length; i++) {
         assert_equals(decoded, codePoints[i]);
     }, "gbk pointer: " + pointer)
 }
-</script>
