31d4a337ad30a8356dbb110fd3ee459fe4267be4
Josh Simmons <jds@google.com>
2021-01-25 23:46:50 +0000

    Include a configurable string with the Assistant voice search intent.
    Since the Always-mic experiments will be run from the Chrome side, this
    Extra will allow us to split our experimental traffic across one or more
    experiment arms on the Assistant side.
    
    Bug: 1140581
    Change-Id: I425e8e7304410b80ea2822703608afa098307274
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2634054
    Commit-Queue: Josh Simmons <jds@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846944}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -132,6 +132,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kAndroidNightModeTabReparenting,
     &kAndroidPartnerCustomizationPhenotype,
     &kAndroidSearchEngineChoiceNotification,
+    &kAssistantIntentExperimentId,
     &kAssistantIntentPageUrl,
     &kAssistantIntentTranslateInfo,
     &kBentoOffline,
@@ -347,6 +348,9 @@ const base::Feature kAndroidPartnerCustomizationPhenotype{
 
 const base::Feature kAndroidSearchEngineChoiceNotification{
     "AndroidSearchEngineChoiceNotification", base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::Feature kAssistantIntentExperimentId{
+    "AssistantIntentExperimentId", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kAssistantIntentPageUrl{"AssistantIntentPageUrl",
                                             base::FEATURE_DISABLED_BY_DEFAULT};
