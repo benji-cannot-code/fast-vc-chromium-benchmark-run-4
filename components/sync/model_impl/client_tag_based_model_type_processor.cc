a38a03e5184d8730b95b7dc95f370a00970d8ef6
Rushan Suleymanov <rushans@google.com>
2021-01-25 14:04:31 +0000

    [Sync] Make DeviceInfoSyncBridge syncing when all metadata is loaded
    Prior to this CL the DeviceInfoSyncBridge could be in syncing state even
    without local cache GUID and other metadata. This could lead to some
    DCHECK violations (e.g. the one removed at crrev.com/c/2563488).
    This CL shouldn't change behaviour.
    
    Bug: 1023457
    Change-Id: I2bbd5b9481e2f0a3a4f7a7dd4305815967a9ce63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2598914
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#846701}

--- components/sync/model_impl/client_tag_based_model_type_processor.cc
+++ components/sync/model_impl/client_tag_based_model_type_processor.cc
@@ -270,11 +270,13 @@ void ClientTagBasedModelTypeProcessor::ClearAllMetadataAndResetStateImpl(
   }
 }
 
-bool ClientTagBasedModelTypeProcessor::IsTrackingMetadata() {
+bool ClientTagBasedModelTypeProcessor::IsTrackingMetadata() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return entity_tracker_ != nullptr;
 }
 
 std::string ClientTagBasedModelTypeProcessor::TrackedAccountId() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // Returning non-empty here despite !IsTrackingMetadata() has weird semantics,
   // e.g. initial updates are being fetched but we haven't received the response
   // (i.e. prior to exercising MergeSyncData()). Let's be cautious and hide the
@@ -286,6 +288,7 @@ std::string ClientTagBasedModelTypeProcessor::TrackedAccountId() {
 }
 
 std::string ClientTagBasedModelTypeProcessor::TrackedCacheGuid() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // Returning non-empty here despite !IsTrackingMetadata() has weird semantics,
   // e.g. initial updates are being fetched but we haven't received the response
   // (i.e. prior to exercising MergeSyncData()). Let's be cautious and hide the
@@ -297,6 +300,7 @@ std::string ClientTagBasedModelTypeProcessor::TrackedCacheGuid() {
 }
 
 void ClientTagBasedModelTypeProcessor::ReportError(const ModelError& error) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   ReportErrorImpl(error, ErrorSite::kBridgeInitiated);
 }
 
@@ -334,6 +338,7 @@ void ClientTagBasedModelTypeProcessor::ReportErrorImpl(const ModelError& error,
 }
 
 base::Optional<ModelError> ClientTagBasedModelTypeProcessor::GetError() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return model_error_;
 }
 
