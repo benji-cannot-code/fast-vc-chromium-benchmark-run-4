eb7fc960cd9df3e12cc6fa8bfa05363087a9e822
Yuly Novikov <ynovikov@chromium.org>
2021-01-25 19:11:19 +0000

    Report build step as 'interrupted' on Android if there were exceptions
    Otherwise the step is reported as failing,
    but the build is reported as passing.
    
    This follows the same logic as setting UNRELIABLE_RESULTS tags
    for GTest based suites, outputting 'interrupted' in JSON for
    isolated scripts suites on the same condition.
    
    Bug: 1153299
    Change-Id: I3c5cf40cb91f557d2e3e72d8ae11575a885679f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2630546
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846809}

--- build/android/pylib/results/json_results_test.py
+++ build/android/pylib/results/json_results_test.py
@@ -210,7 +210,8 @@ class JsonResultsTest(unittest.TestCase):
     all_results = base_test_result.TestRunResults()
     all_results.AddResult(result)
 
-    results_dict = json_results.GenerateJsonTestResultFormatDict([all_results])
+    results_dict = json_results.GenerateJsonTestResultFormatDict([all_results],
+                                                                 False)
     self.assertEquals(1, len(results_dict['tests']))
     self.assertEquals(1, len(results_dict['tests']['test']))
     self.assertEquals(1, len(results_dict['tests']['test']['package']))
@@ -231,7 +232,8 @@ class JsonResultsTest(unittest.TestCase):
     all_results = base_test_result.TestRunResults()
     all_results.AddResult(result)
 
-    results_dict = json_results.GenerateJsonTestResultFormatDict([all_results])
+    results_dict = json_results.GenerateJsonTestResultFormatDict([all_results],
+                                                                 False)
     self.assertEquals(1, len(results_dict['tests']))
     self.assertEquals(1, len(results_dict['tests']['test']))
     self.assertEquals(1, len(results_dict['tests']['test']['package']))
@@ -263,7 +265,8 @@ class JsonResultsTest(unittest.TestCase):
 
     all_results = [run_results_1, run_results_2]
 
-    results_dict = json_results.GenerateJsonTestResultFormatDict(all_results)
+    results_dict = json_results.GenerateJsonTestResultFormatDict(
+        all_results, False)
     self.assertEquals(1, len(results_dict['tests']))
     self.assertEquals(1, len(results_dict['tests']['test']))
     self.assertEquals(1, len(results_dict['tests']['test']['package']))
