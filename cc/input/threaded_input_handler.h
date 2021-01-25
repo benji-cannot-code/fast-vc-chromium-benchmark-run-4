8935e502236b2f10f29c0d820a953d3b8436342f
Rahul Arakeri <arakeri@microsoft.com>
2021-01-25 19:38:51 +0000

    Fix for Impulse based scrolling track autoscroll.
    This fixes a potential issue with Impulse based scrolling when a track
    autoscroll is initiated. Impulse based scrolls take longer to finish.
    Due to this, the initial pointerdown (that would've initiated the
    track autoscroll) would not yet have finished by the time an linear
    autoscroll kicks off. So when the ScrollbarController tries to add the
    linear autoscroll animation to the KeyframeModel, it leads to a DCHECK
    failure. The fix is to abort the initial scroll offset animation before
    creating a linear autoscroll animation.
    
    Bug: 1169523
    Change-Id: Ic3b65f99c0d71e5e9cc600bfa803f842d83502d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643703
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Rahul Arakeri <arakeri@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#846836}

--- cc/input/threaded_input_handler.h
+++ cc/input/threaded_input_handler.h
@@ -170,6 +170,8 @@ class CC_EXPORT ThreadedInputHandler : public InputHandler,
 
  private:
   FRIEND_TEST_ALL_PREFIXES(ScrollUnifiedLayerTreeHostImplTest,
+                           AbortAnimatedScrollBeforeStartingAutoscroll);
+  FRIEND_TEST_ALL_PREFIXES(ScrollUnifiedLayerTreeHostImplTest,
                            AnimatedScrollYielding);
   FRIEND_TEST_ALL_PREFIXES(ScrollUnifiedLayerTreeHostImplTest,
                            AutoscrollOnDeletedScrollbar);
