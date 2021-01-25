1a5874315eb1530df565c1b12dd44634c49fcf1c
Noel Gordon <noel@chromium.org>
2021-01-25 06:27:35 +0000

    [filesapp] Disable deleting Android special files from context menus
    The file-list context menu disables cut/delete/rename of Android system
    directories, whereas the directory-tree context-menu enables them.
    
    Fix the directory-tree context-menu items: like the file-list it should
    disable cut/delete/rename menu items. Update the directory-tree context
    menu test to match the new behavior [1].
    
    [1] All top-level directories (Android system files) |isRootDirectory|,
    are protected from deletes, and the system /DCIM/Camera directory.
    
    Bug: 1170212
    Change-Id: Ibc8ac54204354450ba666695c1bae407964e95c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2646522
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#846632}

--- ui/file_manager/integration_tests/file_manager/directory_tree_context_menu.js
+++ ui/file_manager/integration_tests/file_manager/directory_tree_context_menu.js
@@ -1217,12 +1217,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       ['#new-folder', false],
     ];
     const folderMenus = [
-      ['#cut', true],
+      ['#cut', false],
       ['#copy', true],
       ['#paste-into-folder', false],
       ['#share-with-linux', true],
-      ['#rename', false],
-      ['#delete', true],
+      ['#delete', false],
       ['#new-folder', true],
     ];
 
