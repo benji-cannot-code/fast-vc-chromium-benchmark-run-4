ad1020deb5f5ab84ec21766a240c4ddfc3e46a4b
Michael Ludwig <michaelludwig@google.com>
2021-01-26 01:58:58 +0000

    Remove unused includes to deprecated skia headers
    This is a follow up https://chromium-review.googlesource.com/c/chromium/src/+/2527768
    as several headers were missed in that CL.
    
    Bug: skia:9310
    Change-Id: Ice6ffe95a530b405d742675725f08c89a8ffe7b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643232
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Michael Ludwig <michaelludwig@google.com>
    Cr-Commit-Position: refs/heads/master@{#846999}

--- cc/layers/layer_impl_unittest.cc
+++ cc/layers/layer_impl_unittest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/trees/tree_synchronizer.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/skia/include/effects/SkBlurImageFilter.h"
 
 namespace cc {
 namespace {
--- cc/paint/paint_filter.h
+++ cc/paint/paint_filter.h
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkImageFilter.h"
 #include "third_party/skia/include/core/SkPoint3.h"
 #include "third_party/skia/include/core/SkRegion.h"
-#include "third_party/skia/include/effects/SkBlurImageFilter.h"
-#include "third_party/skia/include/effects/SkDisplacementMapEffect.h"
-#include "third_party/skia/include/effects/SkDropShadowImageFilter.h"
-#include "third_party/skia/include/effects/SkMatrixConvolutionImageFilter.h"
 
 namespace viz {
 class GLRenderer;
