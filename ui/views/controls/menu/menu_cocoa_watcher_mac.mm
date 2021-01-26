74f6383cc81f59b63e678b3652ac6e1ddfadfb34
Joanmarie Diggs <jdiggs@igalia.com>
2021-01-26 18:02:56 +0000

    Revert two changes related to improving VoiceOver user experience
    This reverts commit a3161cfe381a14feb5d3d65fb72752d6a5b21da6 and commit
    19c5aea14f6c5da8db7d44d8aa6ad280064774e0.
    
    Reasons for revert: crbug.com/1162932, crbug.com/1166574, and some
    additional side effects described in crbug.com/1097769.
    
    Note: The reason that the VoiceOver screen reader was failing to present
    menus being shown and closed is due to the lack of mappings between the
    platform-agnostic/internal accessibility events and the Mac-specific
    accessibility events. It should be possible to address the issue
    reported in crbug.com/1097769 by adding these mappings.
    See crrev.com/c/2642354.
    
    Original changes' descriptions:
    
    > views: activate menus when they are opened by keyboard
    >
    > This change causes menu widgets to be become the active widget if the
    > menu was shown by keyboard. This ensures that the menu widget is
    > announced by screenreaders. There is one special case: editable
    > combobox menus, even if opened by keyboard, never activate since that
    > would pull keyboard focus away from the editable combobox itself.
    >
    > To support that, this change also:
    > * Adds Widget::Predicate, a type for predicates that accept a Widget;
    > * Adds support to MenuCocoaWatcherMac for ignoring certain widget
    >   activations;
    > * Adds an IsNotMenuWidget predicate to MenuController for use with
    >   MenuCocoaWatcherMac
    >
    > Bug: 1097769
    > Change-Id: I4f5289d66428f97e75c38e4e887b8ce9061dada8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2520141
    > Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    > Reviewed-by: Leonard Grey <lgrey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#824630}
    
    and
    
    > views: allow SubmenuView to take focus when menus activate
    >
    > After r824630, some menus become the active widget when opened via
    > keyboard on Mac. This was required for the screenreader to become
    > aware that the user is navigating a menu. However, these widgets
    > were becoming active with no focused view, causing key presses to
    > not be handled. These menus were therefore impossible to either
    > traverse with the arrow keys or close with Escape.
    >
    > Bug: 1097769
    > Change-Id: If47f564a6d3d18df8eaef1e12bd19bc811d93073
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2582822
    > Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    > Reviewed-by: Leonard Grey <lgrey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#835399}
    
    Change-Id: I657434c2064f8ef4b5c1c8f23ad0cfc0fddf4e64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648668
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#847220}

--- ui/views/controls/menu/menu_cocoa_watcher_mac.mm
+++ ui/views/controls/menu/menu_cocoa_watcher_mac.mm
@@ -12,11 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
-MenuCocoaWatcherMac::MenuCocoaWatcherMac(
-    Widget::Predicate activation_is_interesting,
-    base::OnceClosure callback)
-    : activation_is_interesting_(activation_is_interesting),
-      callback_(std::move(callback)) {
+MenuCocoaWatcherMac::MenuCocoaWatcherMac(base::OnceClosure callback)
+    : callback_(std::move(callback)) {
   observer_token_other_menu_ = [[NSNotificationCenter defaultCenter]
       addObserverForName:NSMenuDidBeginTrackingNotification
                   object:nil
@@ -29,11 +26,7 @@ MenuCocoaWatcherMac::MenuCocoaWatcherMac(
                   object:nil
                    queue:nil
               usingBlock:^(NSNotification* notification) {
-                Widget* widget =
-                    Widget::GetWidgetForNativeWindow([NSApp keyWindow]);
-                if (activation_is_interesting_.Run(widget)) {
-                  ExecuteCallback();
-                }
+                ExecuteCallback();
               }];
   observer_token_app_change_ =
       [[[NSWorkspace sharedWorkspace] notificationCenter]
