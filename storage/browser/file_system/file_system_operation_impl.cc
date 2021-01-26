cc0fab51fbfef2e25ab23c313d9fd6364fc2fda9
Victor Costan <pwnall@chromium.org>
2021-01-26 03:27:04 +0000

    Quota: Remove QuotaManagerProxy::quota_manager().
    To prepare for an eventual move of the Quota system to the Storage
    Service, all storage and browser features will interact with
    QuotaManager exclusively via QuotaManagerProxy, which will eventually
    become a mojo interface.
    
    This CL removes the QuotaManagerProxy::quota_manager() getter, so
    features that are handed a QuotaManagerProxy cannot access the
    underlying QuotaManager directly.
    
    Bug: 1168597
    Change-Id: I7dabb3877135665c1929a34c60790589ee3864fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2641984
    Reviewed-by: Jarryd Goodman <jarrydg@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847034}

--- storage/browser/file_system/file_system_operation_impl.cc
+++ storage/browser/file_system/file_system_operation_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/time.h"
 #include "net/base/escape.h"
 #include "net/url_request/url_request.h"
@@ -415,9 +416,8 @@ void FileSystemOperationImpl::GetUsageAndQuotaThenRunTask(
   }
 
   DCHECK(quota_manager_proxy);
-  DCHECK(quota_manager_proxy->quota_manager());
-  quota_manager_proxy->quota_manager()->GetUsageAndQuota(
-      url::Origin::Create(url.origin().GetURL()),
+  quota_manager_proxy->GetUsageAndQuota(
+      base::SequencedTaskRunnerHandle::Get().get(), url.origin(),
       FileSystemTypeToQuotaStorageType(url.type()),
       base::BindOnce(&FileSystemOperationImpl::DidGetUsageAndQuotaAndRunTask,
                      weak_ptr_, std::move(task), std::move(error_callback)));
