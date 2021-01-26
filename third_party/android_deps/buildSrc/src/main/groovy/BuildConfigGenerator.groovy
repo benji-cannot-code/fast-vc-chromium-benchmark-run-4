d6fe5d0027947fd277b4d62336083eee97b36cdd
Peter Kotwicz <pkotwicz@chromium.org>
2021-01-26 22:05:05 +0000

    [Build] Update androidx build.gradle
    This CL:
    - Updates third_party/androidx/build.gradle.template to match androidx
      targets in third_party/android_deps/build.gradle
    - Changes fetch_all.py to make files writable before copying them. This
      is needed by the androidx-packager bot which overwrites files fetched
      from cipd. Files fetched by cipd are read-only.
    - Update BuildConfigGenerator to:
      - Add //build/config/android/rules.gni when creating BUILD.gn
      - Adds ignoreDEPS parameter to make it clearer that
        fetch_all_androidx.py should not update DEPS. This makes
        fetch_all_androidx.py easier to use when testing locally.
    - Makes fetch_all.py not download androidx libraries or update androidx
      DEPS when useDedicatedAndroidxDir gradle parameter is set.
    
    BUG=1064277
    
    Change-Id: Iba3c66c86ae652a1c9428bb64fa0e2b0403ee626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650131
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847336}

--- third_party/android_deps/buildSrc/src/main/groovy/BuildConfigGenerator.groovy
+++ third_party/android_deps/buildSrc/src/main/groovy/BuildConfigGenerator.groovy
@@ -87,6 +87,11 @@ class BuildConfigGenerator extends DefaultTask {
      */
      boolean useDedicatedAndroidxDir
 
+     /**
+      * Whether to ignore DEPS file.
+      */
+     boolean ignoreDEPS
+
     @TaskAction
     void main() {
         skipLicenses = skipLicenses || project.hasProperty("skipLicenses")
@@ -104,6 +109,9 @@ class BuildConfigGenerator extends DefaultTask {
         def downloadTasks = []
         graph.dependencies.values().each { dependency ->
             if (excludeDependency(dependency)) {
+                return
+            }
+            if (useDedicatedAndroidxDir && dependency.id.startsWith("androidx_")) {
                 return
             }
             logger.debug "Processing ${dependency.name}: \n${jsonDump(dependency)}"
@@ -154,8 +162,10 @@ class BuildConfigGenerator extends DefaultTask {
 
         // 3. Generate the root level build files
         updateBuildTargetDeclaration(graph, repositoryPath, normalisedRepoPath)
-        updateDepsDeclaration(graph, cipdBucket, stripFromCipdPath, repositoryPath,
-                              "${normalisedRepoPath}/../../DEPS")
+        if (!ignoreDEPS) {
+            updateDepsDeclaration(graph, cipdBucket, stripFromCipdPath, repositoryPath,
+                                  "${normalisedRepoPath}/../../DEPS")
+        }
         dependencyDirectories.sort { path1, path2 -> return path1.compareTo(path2) }
         updateReadmeReferenceFile(dependencyDirectories,
                                   "${normalisedRepoPath}/additional_readme_paths.json")
@@ -257,6 +267,8 @@ class BuildConfigGenerator extends DefaultTask {
             def matcher = BUILD_GN_GEN_PATTERN.matcher(buildFile.getText())
             if (!matcher.find()) throw new IllegalStateException("BUILD.gn insertion point not found.")
             out = matcher.replaceFirst(out)
+        } else {
+            out = "import(\"//build/config/android/rules.gni\")\n" + out
         }
         buildFile.write(out)
     }
@@ -559,6 +571,9 @@ class BuildConfigGenerator extends DefaultTask {
 
         depGraph.dependencies.values().sort(dependencyComparator).each { dependency ->
             if (excludeDependency(dependency)) {
+                return
+            }
+            if (useDedicatedAndroidxDir && dependency.id.startsWith("androidx_")) {
                 return
             }
             def depPath = "${DOWNLOAD_DIRECTORY_NAME}/${dependency.id}"
