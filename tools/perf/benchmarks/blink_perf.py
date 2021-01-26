9fc0852f44dd91645ec7b5c566d3caee6291d019
Austin Eng <enga@chromium.org>
2021-01-26 16:44:27 +0000

    Activate blink_perf.webgpu and blink_perf.webgl benchmarks
    Bug: 1155348
    Change-Id: I97047f3246bcbc89d420179b269b3f08d6c0ecf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643413
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847191}

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -694,7 +694,7 @@ class BlinkPerfWebGL(_BlinkPerfBenchmark):
 
   @classmethod
   def Name(cls):
-    return 'UNSCHEDULED_blink_perf.webgl'
+    return 'blink_perf.webgl'
 
 
 @benchmark.Info(emails=[
@@ -709,7 +709,7 @@ class BlinkPerfWebGLFastCall(_BlinkPerfBenchmark):
 
   @classmethod
   def Name(cls):
-    return 'UNSCHEDULED_blink_perf.webgl_fast_call'
+    return 'blink_perf.webgl_fast_call'
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs(['--enable-unsafe-fast-js-calls'])
@@ -726,7 +726,7 @@ class BlinkPerfWebGPU(_BlinkPerfBenchmark):
 
   @classmethod
   def Name(cls):
-    return 'UNSCHEDULED_blink_perf.webgpu'
+    return 'blink_perf.webgpu'
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs(['--enable-unsafe-webgpu'])
@@ -744,7 +744,7 @@ class BlinkPerfWebGPUFastCall(_BlinkPerfBenchmark):
 
   @classmethod
   def Name(cls):
-    return 'UNSCHEDULED_blink_perf.webgpu_fast_call'
+    return 'blink_perf.webgpu_fast_call'
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs(
