7b181856bbeb3204da61484f0497dd93e5417b78
rbpotter <rbpotter@chromium.org>
2021-01-26 16:47:23 +0000

    PDF Viewer: Remove old toolbar
    Bug: 1164004
    Change-Id: I2c04d8eb1d1f4a780876312c229d16f6fa32bc83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2642924
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847193}

--- chrome/browser/resources/pdf/pdf_viewer.js
+++ chrome/browser/resources/pdf/pdf_viewer.js
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './elements/viewer-error-screen.js';
 import './elements/viewer-password-dialog.js';
-import './elements/viewer-pdf-toolbar.js';
 import './elements/viewer-properties-dialog.js';
 import './elements/shared-vars.js';
 // <if expr="chromeos">
@@ -361,19 +360,10 @@ export class PDFViewerElement extends PDFViewerBaseElement {
   }
 
   /**
-   * @return {!ViewerPdfToolbarElement}
-   * @private
-   */
-  getToolbar_() {
-    return /** @type {!ViewerPdfToolbarElement} */ (this.$$('#toolbar'));
-  }
-
-  /**
    * @return {!ViewerPdfToolbarNewElement}
    * @private
    */
-  getToolbarNew_() {
-    assert(this.pdfViewerUpdateEnabled_);
+  getToolbar_() {
     return /** @type {!ViewerPdfToolbarNewElement} */ (this.$$('#toolbar'));
   }
 
@@ -429,19 +419,11 @@ export class PDFViewerElement extends PDFViewerBaseElement {
   handleToolbarKeyEvent_(e) {
     if (this.pdfViewerUpdateEnabled_) {
       if (e.key === '\\' && e.ctrlKey) {
-        this.getToolbarNew_().fitToggle();
+        this.getToolbar_().fitToggle();
       }
       // TODO: Add handling for additional relevant hotkeys for the new unified
       // toolbar.
       return;
-    }
-
-    switch (e.key) {
-      case 'g':
-        if (this.toolbarEnabled_ && (e.ctrlKey || e.metaKey) && e.altKey) {
-          this.getToolbar_().selectPageNumber();
-        }
-        return;
     }
   }
 
@@ -912,7 +894,7 @@ export class PDFViewerElement extends PDFViewerBaseElement {
 
   /** @override */
   forceFit(view) {
-    this.getToolbarNew_().forceFit(view);
+    this.getToolbar_().forceFit(view);
   }
 
   /** @override */
