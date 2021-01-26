2f72b0d57dfed44fef4e0119b78eafbf333893f0
Stephen McGruer <smcgruer@chromium.org>
2021-01-26 19:12:38 +0000

    Mark some WPT tests as timeout=long
    A recent change in testharness.js seems to have caused a detectable
    performance regression on the MSAN bots. We intend to follow through on
    this to see if it can be avoided, but for now there are 3 tests that are
    consistently timing out on MSAN that did not previously:
    
    external/wpt/encoding/textdecoder-labels.any.worker.html
    external/wpt/html/canvas/offscreen/text/2d.text.draw.fill.maxWidth.zero.worker.html
    external/wpt/url/url-constructor.any.worker.html
    
    This CL adds 'long' timeouts to each of the tests. Unfortunately the
    canvas test is part of a generated test suite, so far that I added
    support for 'timeout=long' to the worker variant of gentestutils.py...
    and discovered ~20 other test files that were already marked as having a
    long timeout but weren't previously generating the correct code! So they
    unfortunately get bundled in with this.
    
    Bug: 1170588
    Change-Id: I3b471b3a21b4e9f8c126f4e012c7f9949636e4b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648976
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847254}

--- third_party/blink/web_tests/external/wpt/encoding/textdecoder-labels.any.js
+++ third_party/blink/web_tests/external/wpt/encoding/textdecoder-labels.any.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: title=Encoding API: Encoding labels
 // META: script=resources/encodings.js
+// META: timeout=long
 
 var whitespace = [' ', '\t', '\n', '\f', '\r'];
 encodings_table.forEach(function(section) {
