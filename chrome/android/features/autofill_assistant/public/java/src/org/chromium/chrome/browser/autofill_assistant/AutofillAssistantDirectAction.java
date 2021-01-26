3a69db133083c844ac048a614b675f8b8b81c5f9
Andrew Grieve <agrieve@chromium.org>
2021-01-26 05:36:05 +0000

    Android: Remove @DoNotInline now that no longer using dexsplitter
    Bug: 1167891
    Change-Id: I20aeaa3d24b75624b6d28c2ce90020026f7310a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2647626
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847057}

--- chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantDirectAction.java
+++ chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantDirectAction.java
@@ -5,16 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.autofill_assistant;
 
-import org.chromium.base.annotations.DoNotInline;
-
 import java.util.List;
 
 /**
  * Interface for direct action containers.
  */
-// TODO(crbug.com/1145287): DexSplitter has issues with interfaces that have a single implementer
-// inside a DFM.
-@DoNotInline
 interface AutofillAssistantDirectAction {
     /** Returns a list of name and aliases for this direct action. */
     List<String> getNames();
