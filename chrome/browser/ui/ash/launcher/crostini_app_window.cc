7f1452f52fcd902049cc9d1bc00602bf1f5f53e3
James Cook <jamescook@chromium.org>
2021-01-26 19:25:15 +0000

    chromeos: Fix shelf app menu icon size for crostini apps
    CrostiniAppWindow needs to set a smaller icon as the window icon (for
    the shelf menu) and a large icon as the app icon (for alt-tab).
    
    Update views::WidgetDelegate to allow setting independent app and
    window icons. WidgetDelegate already supports retrieving these icons via
    getter methods.
    
    Fixed: 1170455
    Test: added to views_unittests
    Change-Id: Idedb3a8c6d9e24729f32e9ce531e288a752c25fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2647950
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847261}

--- chrome/browser/ui/ash/launcher/crostini_app_window.cc
+++ chrome/browser/ui/ash/launcher/crostini_app_window.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/launcher/crostini_app_window.h"
 
+#include "base/notreached.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/app_service/app_service_app_icon_loader.h"
 #include "extensions/common/constants.h"
@@ -13,16 +14,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget_delegate.h"
 
 namespace {
-constexpr int kIconSize = extension_misc::EXTENSION_ICON_MEDIUM;
+constexpr int kWindowIconSizeDips = extension_misc::EXTENSION_ICON_BITTY;
+constexpr int kAppIconSizeDips = extension_misc::EXTENSION_ICON_MEDIUM;
 }  // namespace
 
 CrostiniAppWindow::CrostiniAppWindow(Profile* profile,
                                      const ash::ShelfID& shelf_id,
                                      views::Widget* widget)
     : AppWindowBase(shelf_id, widget) {
-  app_icon_loader_ =
-      std::make_unique<AppServiceAppIconLoader>(profile, kIconSize, this);
+  app_icon_loader_ = std::make_unique<AppServiceAppIconLoader>(
+      profile, kAppIconSizeDips, this);
   app_icon_loader_->FetchImage(shelf_id.app_id);
+  window_icon_loader_ = std::make_unique<AppServiceAppIconLoader>(
+      profile, kWindowIconSizeDips, this);
+  window_icon_loader_->FetchImage(shelf_id.app_id);
 }
 
 CrostiniAppWindow::~CrostiniAppWindow() = default;
@@ -32,5 +37,12 @@ void CrostiniAppWindow::OnAppImageUpdated(const std::string& app_id,
   if (!widget() || !widget()->widget_delegate())
     return;
 
-  widget()->widget_delegate()->SetIcon(image);
+  static_assert(kWindowIconSizeDips != kAppIconSizeDips, "sizes must differ");
+  if (image.width() == kWindowIconSizeDips) {
+    widget()->widget_delegate()->SetIcon(image);
+  } else if (image.width() == kAppIconSizeDips) {
+    widget()->widget_delegate()->SetAppIcon(image);
+  } else {
+    NOTREACHED();
+  }
 }
