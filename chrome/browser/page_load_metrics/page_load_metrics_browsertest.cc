451b41181fd314a5f689d926c99b05986ef68406
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2021-01-26 19:45:39 +0000

    Use ScopedProfileKeepAlive in PageLoadMetricsBrowserTest
    These tests already hold a ScopedKeepAlive to test their logic without
    deleting the BrowserProcess. Add a ScopedProfileKeepAlive so they don't
    delete the Profile* either, once we start enabling
    DestroyProfileOnBrowserClose by default on CQ.
    
    Bug: 88586
    Change-Id: Ic5a3487ec35dce3a275451edc577aa31b873de8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648246
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Auto-Submit: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847272}

--- chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefetch/no_state_prefetch/prerender_test_utils.h"
 #include "chrome/browser/prefs/session_startup_pref.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_keep_alive_types.h"
+#include "chrome/browser/profiles/scoped_profile_keep_alive.h"
 #include "chrome/browser/sessions/session_restore.h"
 #include "chrome/browser/sessions/session_restore_test_helper.h"
 #include "chrome/browser/sessions/session_service_factory.h"
@@ -1942,8 +1944,10 @@ class SessionRestorePageLoadMetricsBrowserTest
     helper.SetForceBrowserNotAliveWithNoWindows(true);
 #endif
 
-    std::unique_ptr<ScopedKeepAlive> keep_alive(new ScopedKeepAlive(
-        KeepAliveOrigin::SESSION_RESTORE, KeepAliveRestartOption::DISABLED));
+    auto keep_alive = std::make_unique<ScopedKeepAlive>(
+        KeepAliveOrigin::SESSION_RESTORE, KeepAliveRestartOption::DISABLED);
+    auto profile_keep_alive = std::make_unique<ScopedProfileKeepAlive>(
+        profile, ProfileKeepAliveOrigin::kBrowserWindow);
     CloseBrowserSynchronously(browser);
 
     // Create a new window, which should trigger session restore.
