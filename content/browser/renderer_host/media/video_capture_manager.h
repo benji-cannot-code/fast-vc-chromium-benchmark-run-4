f3bf27e3299517e33b4815691c917b4bcb6127b4
Dale Curtis <dalecurtis@chromium.org>
2021-01-26 23:08:54 +0000

    Add metrics for how often screen lock happens with device capture.
    We are evaluating whether we want to disable device capture when
    screen lock occurs. This adds a metric which has an entry for every
    device capture session and marks if a screen lock event occurs.
    
    In this change I replace the expired Media.VideoCaptureManager.Event
    histogram with Media.VideoCaptureManager.DeviceSessionWasLocked.
    
    Bug: 1163105
    Test: Histograms show up when screen is locked.
    Change-Id: I41fc085688ed81d2cba767d2a19bfc8ee73fb394
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648614
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Markus Handell <handellm@google.com>
    Cr-Commit-Position: refs/heads/master@{#847373}

--- content/browser/renderer_host/media/video_capture_manager.h
+++ content/browser/renderer_host/media/video_capture_manager.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/containers/circular_deque.h"
+#include "base/containers/flat_set.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -280,6 +281,9 @@ class CONTENT_EXPORT VideoCaptureManager
   // determine which device to use when ConnectClient() occurs. Used
   // only on the IO thread.
   SessionMap sessions_;
+
+  // A set of sessions that have encountered screen lock.
+  base::flat_set<media::VideoCaptureSessionId> locked_sessions_;
 
   // Currently opened VideoCaptureController instances. The device may or may
   // not be started. This member is only accessed on IO thread.
