43bc763fdf7079bd3737161488f9e04b0bb8a723
Xianzhu Wang <wangxianzhu@chromium.org>
2021-01-26 21:50:11 +0000

    cc::Layer of pre-composited layer should use GraphicsLayer's property state
    When a scroller is composited but doesn't use composited scrolling,
    the main graphics layer of the scroller has the LocalBorderBoxProperties
    state, and the layer contains only a ScrollHitTest paint chunk which has
    the ContentsProperties state. Previously the layer used the
    non-composited scroll node as the scroll state, causing unexpected not
    falling back to the main thread.
    
    This hadn't been a problem before crrev.com/c/2474094 because we didn't
    pass the paint chunks of a GraphicsLayer not needing layerization to
    PaintArtifactCompositor.
    
    Bug: 1169500
    Change-Id: I0fc3a3fa8bd506c348d7d138c6b3080b5e67658d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648834
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847318}

--- third_party/blink/renderer/platform/testing/test_paint_artifact.h
+++ third_party/blink/renderer/platform/testing/test_paint_artifact.h
@@ -95,6 +95,7 @@ class TestPaintArtifact {
   // automatically created client.
   TestPaintArtifact& RectDrawing(const IntRect& bounds, Color color);
   TestPaintArtifact& ScrollHitTest(
+      const IntRect&,
       const TransformPaintPropertyNode* scroll_translation);
 
   TestPaintArtifact& ForeignLayer(scoped_refptr<cc::Layer> layer,
@@ -106,6 +107,7 @@ class TestPaintArtifact {
                                  Color color);
   TestPaintArtifact& ScrollHitTest(
       DisplayItemClient&,
+      const IntRect&,
       const TransformPaintPropertyNode* scroll_translation);
 
   // Sets fake bounds for the last paint chunk. Note that the bounds will be
