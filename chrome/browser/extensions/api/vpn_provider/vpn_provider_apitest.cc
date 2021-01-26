36ebe3fc5fb2fa1953a5f8208cfcc89246ee611e
Maksim Moskvitin <mmoskvitin@google.com>
2021-01-26 11:05:58 +0000

    [CodeHealth] Convert c/b/e/api/vpn_provider to use modern callback
    Bug: 1152279
    Change-Id: I97a1b69f382e73bc69a9c4426a5a1994098ec01c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2642634
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#847111}

--- chrome/browser/extensions/api/vpn_provider/vpn_provider_apitest.cc
+++ chrome/browser/extensions/api/vpn_provider/vpn_provider_apitest.cc
@@ -188,7 +188,7 @@ class VpnProviderApiTest : public extensions::ExtensionApiTest {
   void TriggerInternalRemove() {
     NetworkHandler::Get()->network_configuration_handler()->RemoveConfiguration(
         GetSingleServicePath(), /*remove_confirmer=*/base::nullopt,
-        base::DoNothing(), base::Bind(DoNothingFailureCallback));
+        base::DoNothing(), base::BindOnce(DoNothingFailureCallback));
   }
 
   bool HasService(const std::string& service_path) const {
@@ -351,8 +351,8 @@ IN_PROC_BROWSER_TEST_F(VpnProviderApiTest, ConfigPersistence) {
   NetworkHandler::Get()
       ->network_configuration_handler()
       ->CreateShillConfiguration(properties,
-                                 base::Bind(DoNothingSuccessCallback),
-                                 base::Bind(DoNothingFailureCallback));
+                                 base::BindOnce(DoNothingSuccessCallback),
+                                 base::BindOnce(DoNothingFailureCallback));
   content::RunAllPendingInMessageLoop();
   EXPECT_TRUE(DoesConfigExist(kTestConfig));
 }
