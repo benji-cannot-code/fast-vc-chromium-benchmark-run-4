e45cc474e0a43519fe1cd8692854bf8681e329fe
Nohemi Fernandez <fernandex@chromium.org>
2021-01-26 11:13:29 +0000

    [iOS] Update sign-in promo and infobar to use the user's first name.
    This patch ensures that the sign-in UIs have Chrome cross-platform
    consistency with respect to the naming scheme used for MICE-related
    surfaces.
    
    Screenshots:
    https://drive.google.com/file/d/1xL6a7t2hqiFuJiqEjoQsRqqj3_2aLMuD/
    https://drive.google.com/file/d/1lkLD4rjhq3d8fahAzbPmJEwyP65DVZhb/
    
    Bug: 903393, 1148764
    Change-Id: I99958b7559ff478b6d6930eb5fb0dd3860a89a93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2644885
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847113}

--- ios/chrome/browser/ui/authentication/cells/signin_promo_view_configurator.h
+++ ios/chrome/browser/ui/authentication/cells/signin_promo_view_configurator.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // been added to the device.
 - (instancetype)initWithIdentityPromoViewMode:(IdentityPromoViewMode)viewMode
                                     userEmail:(NSString*)userEmail
-                                 userFullName:(NSString*)userFullName
+                                userGivenName:(NSString*)userGivenName
                                     userImage:(UIImage*)userImage
                                hasCloseButton:(BOOL)hasCloseButton
     NS_DESIGNATED_INITIALIZER;
