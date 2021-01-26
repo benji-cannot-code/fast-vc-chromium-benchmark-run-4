59e7dd8e499dc2aa324e8a70c7d8069dc93142c3
Yeunjoo Choi <ychoi@igalia.com>
2021-01-26 05:41:20 +0000

    Move //chrome/browser/{chromeos => ash}/reset
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1101837, 1164001
    Change-Id: I3dc9da6bec08833b888196dd612e6bbf7c733291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2644544
    Owners-Override: James Cook <jamescook@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847059}

--- chrome/browser/ui/webui/settings/reset_settings_handler.cc
+++ chrome/browser/ui/webui/settings/reset_settings_handler.cc
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/chromeos/reset/metrics.h"
+#include "chrome/browser/ash/reset/metrics.h"
 #include "chrome/common/pref_names.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
