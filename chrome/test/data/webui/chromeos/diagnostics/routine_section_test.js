b9c8d78f49f9ad6f3f1f6b57e88a973892f7acdf
Joon Ahn <joonbug@chromium.org>
2021-01-27 01:09:35 +0000

    diagnostics: inline learn more text
    Learn more text should be inline as per spec
    
    http://screen/zGDXdtHLJpLeKLZ
    
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I1fd8a435539d857cd9cec9b5422ae1e284c7d7c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648195
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Auto-Submit: Joon Ahn <joonbug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847422}

--- chrome/test/data/webui/chromeos/diagnostics/routine_section_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/routine_section_test.js
@@ -430,8 +430,10 @@ export function routineSectionTestSuite() {
 
           // Text is visible saying test succeeded.
           assertFalse(getStatusTextElement().hidden);
-          assertEquals(
-              getStatusTextElement().textContent.trim(), 'Test succeeded');
+          dx_utils.assertElementContainsText(
+              getStatusTextElement(), 'Test succeeded');
+          dx_utils.assertElementContainsText(
+              getStatusTextElement(), 'Learn more');
         });
   });
 
@@ -503,8 +505,10 @@ export function routineSectionTestSuite() {
 
           // Text is visible saying test failed.
           assertFalse(getStatusTextElement().hidden);
-          assertEquals(
-              getStatusTextElement().textContent.trim(), 'Test failed');
+          dx_utils.assertElementContainsText(
+              getStatusTextElement(), 'Test failed');
+          dx_utils.assertElementContainsText(
+              getStatusTextElement(), 'Learn more');
         });
   });
 
