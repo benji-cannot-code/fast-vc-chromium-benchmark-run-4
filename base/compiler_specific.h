f6b0641d5cbd13e446377bece6ac36c626b58255
Bruce Dawson <brucedawson@chromium.org>
2021-01-27 18:07:58 +0000

    Create NO_CODE_FOLDING macro
    base::debug::Alias() can be used to prevent code folding but
    encapsulating this in a macro makes it less cryptic and more obvious.
    This change creates a NO_CODE_FOLDING macro and initially uses it in one
    source file.
    
    This was initially suggested in a comment on crrev.com/c/2607585.
    
    Bug: 1169276
    Change-Id: I4453d9767c33a8199971fa4d55d1f78536816a16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2641565
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847688}

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -66,7 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // To provide the complementary behavior (prevent the annotated function from
 // being omitted) look at NOINLINE. Also note that this doesn't prevent code
 // folding of multiple identical caller functions into a single signature. To
-// prevent code folding, see base::debug::Alias.
+// prevent code folding, see NO_CODE_FOLDING() in base/debug/alias.h.
 // Use like:
 //   void NOT_TAIL_CALLED FooBar();
 #if defined(__clang__) && __has_attribute(not_tail_called)
