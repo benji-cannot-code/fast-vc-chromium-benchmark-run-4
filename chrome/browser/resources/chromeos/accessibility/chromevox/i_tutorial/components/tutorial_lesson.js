dfe3b5dcd0c213a056f6671763953446dc9d3512
Akihiro Ota <akihiroota@chromium.org>
2021-01-27 08:50:50 +0000

    ChromeVox Tutorial: Create separate component for lesson menu.
    The ChromeVox tutorial is implemented as one large polymer
    component. This makes writing tutorials for other accessibility
    extensions difficult; each new tutorial would have to be written from
    scratch and much of the glue code would have to be duplicated.
    
    This change is the third step in creating reusable tutorial
    components. Follow-up changes will do the same for additional
    components.
    
    One other change to note: we rename lessonNum to lessonId
    to avoid confusion with another property, lessonIndex.
    
    This change is a pure refactor and shouldn't cause any change in
    behavior. For more details, please see the following design doc:
    https://docs.google.com/document/d/12vNcRGUkMc
    CSDpYk6Vc-s3uPZNbpra5B2wrMRLVn_ew/edit?usp=sharing
    
    Fixed: 1165443
    AX-Relnotes: N/A
    Test: out/cros/browser_tests --gtest_filter="ChromeVoxTutorialTest.*"
    Change-Id: I1c1c9cfaf98cb5c7d72b3d69a1e6a7fdbbd63bfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650688
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/master@{#847538}

--- chrome/browser/resources/chromeos/accessibility/chromevox/i_tutorial/components/tutorial_lesson.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/i_tutorial/components/tutorial_lesson.js
@@ -25,7 +25,7 @@ export const TutorialLesson = Polymer({
   behaviors: [TutorialCommon],
 
   properties: {
-    lessonNum: {type: Number},
+    lessonId: {type: Number},
 
     title: {type: String},
 
@@ -52,8 +52,8 @@ export const TutorialLesson = Polymer({
     autoInteractive: {type: Boolean, value: false},
 
     // Observed properties.
-
-    activeLessonNum: {type: Number, observer: 'setVisibility'},
+    /** @type {number} */
+    activeLessonId: {type: Number, observer: 'setVisibility'},
   },
 
   /** @override */
@@ -89,7 +89,7 @@ export const TutorialLesson = Polymer({
    * @private
    */
   setVisibility() {
-    if (this.lessonNum === this.activeLessonNum) {
+    if (this.lessonId === this.activeLessonId) {
       this.show();
     } else {
       this.hide();
