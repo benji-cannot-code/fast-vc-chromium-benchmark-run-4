fe54cb8015178ff50b659230cf4b4c0cc6f928b7
Igor Ruvinov <igorruvinov@chromium.org>
2021-01-27 15:50:55 +0000

    Fix clearing of policy conflict override message.
    The messages corresponding to policy conflict overrides were being incorrectly cleared because they are added as type Warning but were being cleared from type Error. This change fixes the message clearing to ensure that the override conflict message is properly cleared after merging.
    
    Bug: 1104043
    Change-Id: Ic079d45ca2667c7443599b2a040b55c3e52c94d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643032
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847632}

--- components/policy/core/common/policy_service_impl_unittest.cc
+++ components/policy/core/common/policy_service_impl_unittest.cc
@@ -1164,8 +1164,6 @@ TEST_F(PolicyServiceTest, DictionaryPoliciesMerging) {
                           POLICY_SOURCE_MERGED, std::move(result), nullptr);
   merged.AddConflictingPolicy(entry_dict_1.DeepCopy());
   merged.AddConflictingPolicy(entry_dict_2.DeepCopy());
-  merged.AddMessage(PolicyMap::MessageType::kWarning,
-                    IDS_POLICY_CONFLICT_DIFF_VALUE);
   expected_chrome.Set(key::kExtensionSettings, std::move(merged));
 
   provider0_.UpdatePolicy(std::move(policy_bundle1));
@@ -1219,8 +1217,6 @@ TEST_F(PolicyServiceTest, ListsPoliciesMerging) {
                           POLICY_SOURCE_MERGED, std::move(result), nullptr);
   merged.AddConflictingPolicy(entry_list_2.DeepCopy());
   merged.AddConflictingPolicy(entry_list_1.DeepCopy());
-  merged.AddMessage(PolicyMap::MessageType::kWarning,
-                    IDS_POLICY_CONFLICT_DIFF_VALUE);
   expected_chrome.Set(key::kExtensionInstallForcelist, std::move(merged));
 
   provider0_.UpdatePolicy(std::move(policy_bundle1));
@@ -1284,8 +1280,6 @@ TEST_F(PolicyServiceTest, GroupPoliciesMergingDisabledForCloudUsers) {
                           POLICY_SOURCE_MERGED, std::move(result), nullptr);
   merged.AddConflictingPolicy(entry_list_2.DeepCopy());
   merged.AddConflictingPolicy(entry_list_1.DeepCopy());
-  merged.AddMessage(PolicyMap::MessageType::kWarning,
-                    IDS_POLICY_CONFLICT_DIFF_VALUE);
   expected_chrome.Set(key::kExtensionInstallForcelist, merged.DeepCopy());
   expected_chrome.Set(key::kExtensionInstallBlocklist, std::move(merged));
   expected_chrome.Set(key::kExtensionInstallAllowlist, std::move(entry_list_3));
@@ -1353,8 +1347,6 @@ TEST_F(PolicyServiceTest, GroupPoliciesMergingEnabled) {
                           POLICY_SOURCE_MERGED, std::move(result), nullptr);
   merged.AddConflictingPolicy(entry_list_2.DeepCopy());
   merged.AddConflictingPolicy(entry_list_1.DeepCopy());
-  merged.AddMessage(PolicyMap::MessageType::kWarning,
-                    IDS_POLICY_CONFLICT_DIFF_VALUE);
   entry_list_3.SetIgnoredByPolicyAtomicGroup();
   expected_chrome.Set(key::kExtensionInstallForcelist, merged.DeepCopy());
   expected_chrome.Set(key::kExtensionInstallBlocklist, std::move(merged));
