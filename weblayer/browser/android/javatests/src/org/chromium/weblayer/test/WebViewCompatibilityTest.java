e1f218322168582bf56118a967a5d1beaeecbe7f
Clark DuVall <cduvall@chromium.org>
2021-01-27 01:28:27 +0000

    [WebLayer] Add another test for WebView/WebLayer in same process
    We had a test loading WebLayer first, this adds another test loading
    WebView first. This extra coverage will be nice to make sure a couple
    perf ideas I have in mind don't break anything.
    
    Bug: 1146438
    Change-Id: I32225a6d79645ed1f0cd881bfb71261c1d5e9804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2651448
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847431}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/WebViewCompatibilityTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/WebViewCompatibilityTest.java
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.weblayer.test;
 
+import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import androidx.test.filters.SmallTest;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +19,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.StrictModeContext;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
+import org.chromium.weblayer.shell.InstrumentationActivity;
 
 /**
  * Tests for compatibility with running WebView and WebLayer in the same process. These tests only
@@ -30,8 +33,33 @@ public class WebViewCompatibilityTest {
 
     @Test
     @SmallTest
-    public void testBothLoadPage() throws Exception {
+    public void testBothLoadPageWebLayerFirst() throws Exception {
         mActivityTestRule.launchShellWithUrl(mActivityTestRule.getTestDataURL("simple_page.html"));
+
+        loadPageWithWebView(mActivityTestRule.getTestDataURL("simple_page2.html"));
+
+        // Make sure WebLayer still loads.
+        mActivityTestRule.navigateAndWait(mActivityTestRule.getTestDataURL("simple_page3.html"));
+    }
+
+    @Test
+    @SmallTest
+    public void testBothLoadPageWebViewFirst() throws Exception {
+        Bundle extras = new Bundle();
+        extras.putBoolean(InstrumentationActivity.EXTRA_CREATE_WEBLAYER, false);
+        InstrumentationActivity activity = mActivityTestRule.launchShell(extras);
+
+        loadPageWithWebView(mActivityTestRule.getTestDataURL("simple_page.html"));
+
+        TestThreadUtils.runOnUiThreadBlocking(
+                () -> activity.loadWebLayerSync(activity.getApplicationContext()));
+        mActivityTestRule.navigateAndWait(mActivityTestRule.getTestDataURL("simple_page2.html"));
+
+        // Make sure WebView still loads.
+        loadPageWithWebView(mActivityTestRule.getTestDataURL("simple_page3.html"));
+    }
+
+    private void loadPageWithWebView(String urlToLoad) throws Exception {
         WebView webView = TestThreadUtils.runOnUiThreadBlocking(() -> {
             // Loading WebView triggers loading from disk.
             try (StrictModeContext ignored = StrictModeContext.allowDiskReads()) {
@@ -43,10 +71,11 @@ public class WebViewCompatibilityTest {
             webView.setWebViewClient(new WebViewClient() {
                 @Override
                 public void onPageFinished(WebView view, String url) {
+                    Assert.assertEquals(url, urlToLoad);
                     callbackHelper.notifyCalled();
                 }
             });
-            webView.loadUrl(mActivityTestRule.getTestDataURL("simple_page2.html"));
+            webView.loadUrl(urlToLoad);
         });
         callbackHelper.waitForFirst();
     }
