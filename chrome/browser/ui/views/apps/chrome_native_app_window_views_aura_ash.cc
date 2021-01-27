d4f7d1ebc4d35fa1563f8105c8c9b0dbfb8295a9
Nancy Wang <nancylingwang@chromium.org>
2021-01-27 04:35:39 +0000

    Save the chrome app launch and window info for full restore.
    Set the session id and the app id as the window properties for chrome
    apps, launched via event.
    
    Save |app_launch_info| to |app_id_to_app_launch_info_| and wait the
    window id/session id from the window, then save app launch info and
    window id together to the full restore data.
    
    BUG=1146900
    
    Change-Id: I0fdf2ce842944a9b668975a7972687b472d8ffc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2649911
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847499}

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ui/base/window_properties.h"
 #include "chromeos/ui/base/window_state_type.h"
 #include "chromeos/ui/frame/immersive/immersive_fullscreen_controller.h"
+#include "components/full_restore/full_restore_utils.h"
 #include "components/session_manager/core/session_manager.h"
 #include "extensions/browser/app_window/app_delegate.h"
 #include "extensions/common/constants.h"
@@ -138,6 +139,11 @@ void ChromeNativeAppWindowViewsAuraAsh::OnBeforeWidgetInit(
     DCHECK_EQ(ui::SHOW_STATE_DEFAULT, init_params->show_state);
     init_params->show_state = ui::SHOW_STATE_MAXIMIZED;
   }
+
+  init_params->init_properties_container.SetProperty(
+      full_restore::kWindowIdKey, app_window()->session_id().id());
+  init_params->init_properties_container.SetProperty(
+      full_restore::kAppIdKey, app_window()->extension_id());
 }
 
 std::unique_ptr<views::NonClientFrameView>
