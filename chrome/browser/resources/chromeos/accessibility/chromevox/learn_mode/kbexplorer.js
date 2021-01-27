00e1f88c35e684c744d6727bea0181a28343f7fe
David Tseng <dtseng@chromium.org>
2021-01-27 03:48:08 +0000

    Update ChromeVox to use fetch instead of synchronous XMLHttpRequest
    After this change, errors similar to:
    Synchronous XMLHttpRequest on the main thread is deprecated because of its detrimental effects to the end user's experience. For more help, check https://xhr.spec.whatwg.org/.
    no longer appear.
    
    Finally, some misc cleanup (removal of unused functions).
    
    R=akihiroota@chromium.org
    
    Test: no errors on startup in ChromeVox
    AX-Relnotes: n/a
    Bug: none
    Change-Id: I59ba033edce18b773864893e3e01283bc97d0b4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648620
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847484}

--- chrome/browser/resources/chromeos/accessibility/chromevox/learn_mode/kbexplorer.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/learn_mode/kbexplorer.js
@@ -49,7 +49,9 @@ KbExplorer = class {
     window.backgroundWindow['BrailleCommandHandler']['setEnabled'](false);
     window.backgroundWindow['GestureCommandHandler']['setEnabled'](false);
 
-    ChromeVoxKbHandler.handlerKeyMap = KeyMap.fromDefaults();
+    (async () => {
+      ChromeVoxKbHandler.handlerKeyMap = await KeyMap.fromDefaults();
+    })();
 
     /** @type {LibLouis.Translator} */
     KbExplorer.currentBrailleTranslator_ =
