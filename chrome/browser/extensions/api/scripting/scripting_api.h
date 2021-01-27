27404df9067e2ef4c41882b74fbdbc6219108511
Devlin Cronin <rdevlin.cronin@chromium.org>
2021-01-27 03:01:49 +0000

    [Extensions] Include frameId in the result list for the scripting API
    Add a new property to the injection result, frameId, for use in the
    scripting.executeScript() API. This allows extensions to determine
    which frame the result came from, even though the results may not be
    in order.
    
    To facilitate this, also create a more detailed struct for the result
    from ScriptExecutor, which includes the per-frame error and URL. In
    the future, these could be used to pass more information to the
    extension (as well as making testing more thorough today).
    
    Add tests for both ScriptExecutor and the end-to-end executeScript
    API.
    
    Bug: 1152910
    Change-Id: Icd57251205e513ae8a632c5e02520c3c003634a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2538164
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847469}

--- chrome/browser/extensions/api/scripting/scripting_api.h
+++ chrome/browser/extensions/api/scripting/scripting_api.h
@@ -10,12 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/extensions/api/scripting.h"
 #include "extensions/browser/extension_function.h"
-
-class GURL;
-
-namespace base {
-class ListValue;
-}
+#include "extensions/browser/script_executor.h"
 
 namespace extensions {
 
@@ -45,9 +40,7 @@ class ScriptingExecuteScriptFunction : public ExtensionFunction {
                std::string* error);
 
   // Invoked when script execution is complete.
-  void OnScriptExecuted(const std::string& error,
-                        const GURL& frame_url,
-                        const base::ListValue& result);
+  void OnScriptExecuted(std::vector<ScriptExecutor::FrameResult> frame_results);
 
   api::scripting::ScriptInjection injection_;
 };
@@ -77,9 +70,7 @@ class ScriptingInsertCSSFunction : public ExtensionFunction {
                std::string* error);
 
   // Called when the CSS insertion is complete.
-  void OnCSSInserted(const std::string& error,
-                     const GURL& frame_url,
-                     const base::ListValue& result);
+  void OnCSSInserted(std::vector<ScriptExecutor::FrameResult> results);
 
   api::scripting::CSSInjection injection_;
 };
