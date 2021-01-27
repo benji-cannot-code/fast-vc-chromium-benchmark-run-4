542b07e2a3bd6bcf640ea0c0526174a95362287e
Michael Lippautz <mlippautz@chromium.org>
2021-01-27 21:10:02 +0000

    heap: Add custom write barriers to Oilpan library
    Invoke the respective HeapConsistency write barriers in V8.
    
    The main idea is that GetWriteBarrierType() provides fast bailout
    checks and only the write barrier uses the TLS accessor for
    ThreadState::Current().
    
    Bug: 1056170
    Change-Id: Ieb2f24cd45c8f7a4fc35f561fd3f0dc21a6a6d26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653029
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847785}

--- third_party/blink/renderer/platform/heap/v8_wrapper/thread_state.h
+++ third_party/blink/renderer/platform/heap/v8_wrapper/thread_state.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/thread_specific.h"
 #include "third_party/blink/renderer/platform/wtf/threading.h"
 #include "v8/include/cppgc/prefinalizer.h"
+#include "v8/include/v8-cppgc.h"
 #include "v8/include/v8.h"
 
 namespace v8 {
