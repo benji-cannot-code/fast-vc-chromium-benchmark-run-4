64c37b55340358cb8100296fcdc06dec07ff4e4d
Ken Rockot <rockot@google.com>
2021-01-27 21:38:29 +0000

    Migrate installedapp tests to WPT+Mojo JS modules
    Mojo JS bindings are now based on modules, and older non-module bindings
    are deprecated.
    
    Modules cannot be loaded from file:// URLs, so these tests need to
    become HTTP tests in order to migrate bindings consumption.
    
    The right place for new HTTP tests is wpt_internal. Hence the move
    there.
    
    This also fixes a bug in the module-based bindings where nullable string
    fields would decode null values as the empty string rather than null.
    
    Bug: 1004256
    Change-Id: I450d606eacfad8b3a15e397d60b41210a971d00a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2651433
    Auto-Submit: Ken Rockot <rockot@google.com>
    Commit-Queue: Bugs Nash <bugsnash@chromium.org>
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847801}

--- third_party/blink/web_tests/wpt_internal/installedapp/resources/installedapp-test-helper.js
+++ third_party/blink/web_tests/wpt_internal/installedapp/resources/installedapp-test-helper.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-'use strict';
+import {InstalledAppProvider, InstalledAppProviderReceiver} from '/gen/third_party/blink/public/mojom/installedapp/installed_app_provider.mojom.m.js';
 
 function assert_relatedapplication_equals(actual, expected, description) {
   assert_equals(actual.platform, expected.platform, description);
@@ -7,7 +7,7 @@ function assert_relatedapplication_equals(actual, expected, description) {
   assert_equals(actual.id, expected.id, description);
 }
 
-function assert_array_relatedapplication_equals(
+export function assert_array_relatedapplication_equals(
     actual, expected, description) {
   assert_equals(actual.length, expected.length, description);
 
@@ -17,12 +17,12 @@ function assert_array_relatedapplication_equals(
 
 class MockInstalledAppProvider {
   constructor(interfaceProvider) {
-    this.bindingSet_ = new mojo.BindingSet(blink.mojom.InstalledAppProvider);
+    this.receiver_ = new InstalledAppProviderReceiver(this);
 
     this.interceptor_ =
-        new MojoInterfaceInterceptor(blink.mojom.InstalledAppProvider.name);
+        new MojoInterfaceInterceptor(InstalledAppProvider.$interfaceName);
     this.interceptor_.oninterfacerequest =
-        e => this.bindingSet_.addBinding(this, e.handle);
+        e => this.receiver_.$.bindHandle(e.handle);
     this.interceptor_.start();
   }
 
@@ -64,7 +64,7 @@ let mockInstalledAppProvider = new MockInstalledAppProvider();
 // pushExpectedCall. It should return a promise, the result of
 // getInstalledRelatedApps().
 // |name| and |properties| are standard testharness arguments.
-function installedapp_test(func, name, properties) {
+export function installedapp_test(func, name, properties) {
   promise_test(t => {
     let mockPromise = mockInstalledAppProvider.init_();
     return Promise.race([func(t, mockInstalledAppProvider), mockPromise]);
