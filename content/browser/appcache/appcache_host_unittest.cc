97b07ea5ca1c93e8dd20d8d1879970ba82884c01
Lukasz Anforowicz <lukasza@chromium.org>
2021-01-27 22:20:47 +0000

    Remove non-url::Origin-based overloads of CanAccessDataForOrigin.
    This CL follows the guidance from //docs/security/origin-vs-url.md
    that strongly suggests using `url::Origin` to represent origins
    and avoiding using `GURL` to represent origins.
    
    Fixed: 1029092
    Change-Id: I273afee2f44c7d27d09314478080e0eed65c1db6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648213
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847820}

--- content/browser/appcache/appcache_host_unittest.cc
+++ content/browser/appcache/appcache_host_unittest.cc
@@ -786,8 +786,8 @@ TEST_F(AppCacheHostTest, SelectCacheAfterProcessCleanup) {
   mojo::Remote<blink::mojom::AppCacheHost> host_remote;
   host.BindReceiver(host_remote.BindNewPipeAndPassReceiver());
 
-  EXPECT_TRUE(
-      security_policy->CanAccessDataForOrigin(kProcessIdForTest, kDocumentURL));
+  EXPECT_TRUE(security_policy->CanAccessDataForOrigin(
+      kProcessIdForTest, url::Origin::Create(kDocumentURL)));
 
   // Destroy the WebContents so the process gets cleaned up.
   web_contents_.reset();
@@ -796,8 +796,8 @@ TEST_F(AppCacheHostTest, SelectCacheAfterProcessCleanup) {
   // Since |host| for kProcessIdForTest is still alive, the corresponding
   // SecurityState in ChildProcessSecurityPolicy should also be kept alive,
   // allowing access for kDocumentURL.
-  EXPECT_TRUE(
-      security_policy->CanAccessDataForOrigin(kProcessIdForTest, kDocumentURL));
+  EXPECT_TRUE(security_policy->CanAccessDataForOrigin(
+      kProcessIdForTest, url::Origin::Create(kDocumentURL)));
 
   // Verify that the document and manifest URLs do not trigger a bad message.
   {
@@ -838,8 +838,8 @@ TEST_F(AppCacheHostTest, ForeignEntryAfterProcessCleanup) {
   mojo::Remote<blink::mojom::AppCacheHost> host_remote;
   host.BindReceiver(host_remote.BindNewPipeAndPassReceiver());
 
-  EXPECT_TRUE(
-      security_policy->CanAccessDataForOrigin(kProcessIdForTest, kDocumentURL));
+  EXPECT_TRUE(security_policy->CanAccessDataForOrigin(
+      kProcessIdForTest, url::Origin::Create(kDocumentURL)));
 
   // Destroy the WebContents so the process gets cleaned up.
   web_contents_.reset();
@@ -848,8 +848,8 @@ TEST_F(AppCacheHostTest, ForeignEntryAfterProcessCleanup) {
   // Since |host| for kProcessIdForTest is still alive, the corresponding
   // SecurityState in ChildProcessSecurityPolicy should also be kept alive,
   // allowing access for kDocumentURL.
-  EXPECT_TRUE(
-      security_policy->CanAccessDataForOrigin(kProcessIdForTest, kDocumentURL));
+  EXPECT_TRUE(security_policy->CanAccessDataForOrigin(
+      kProcessIdForTest, url::Origin::Create(kDocumentURL)));
 
   // Verify that a document URL does not trigger a bad message.
   {
