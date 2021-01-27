5236d271978c81a90c4673f497cc6b2e2c81b940
Daniel Hosseinian <dhoss@chromium.org>
2021-01-27 22:13:08 +0000

    Update WeakPtr example to pass by value
    The example is outdated and violates the Chromium style guide's
    recommendation that "there should rarely be a need to pass smart
    pointers by const ref" [1].
    
    [1] https://chromium.googlesource.com/chromium/src/+/777e130e2fc25c7152479a78c65bdf47d1a65264/styleguide/c++/c++.md#object-ownership-and-calling-conventions
    
    Change-Id: Ibd972cee9770a634456e2aa4469578fb0733ff80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648093
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847817}

--- base/memory/weak_ptr.h
+++ base/memory/weak_ptr.h
@@ -28,13 +28,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 //  class Worker {
 //   public:
-//    static void StartNew(const WeakPtr<Controller>& controller) {
-//      Worker* worker = new Worker(controller);
+//    static void StartNew(WeakPtr<Controller> controller) {
+//      Worker* worker = new Worker(std::move(controller));
 //      // Kick off asynchronous processing...
 //    }
 //   private:
-//    Worker(const WeakPtr<Controller>& controller)
-//        : controller_(controller) {}
+//    Worker(WeakPtr<Controller> controller)
+//        : controller_(std::move(controller)) {}
 //    void DidCompleteAsynchronousProcessing(const Result& result) {
 //      if (controller_)
 //        controller_->WorkComplete(result);
