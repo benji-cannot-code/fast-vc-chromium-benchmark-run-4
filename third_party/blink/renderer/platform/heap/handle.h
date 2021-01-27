ae52d726a0299b8827e534eef2c5d01eac6a750f
Michael Lippautz <mlippautz@chromium.org>
2021-01-27 17:27:35 +0000

    heap: Oilpan library: Allocation for collection backings
    Drive-by: Include and Persistent helper typo fix.
    
    Bug: 1056170
    Change-Id: If37123a53e735a9d6079d0e9cb8b42a31b742015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2652886
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847668}

--- third_party/blink/renderer/platform/heap/handle.h
+++ third_party/blink/renderer/platform/heap/handle.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/heap_allocator.h"
 #include "third_party/blink/renderer/platform/heap/heap_traits.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
+#include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/heap/thread_state.h"
 #include "third_party/blink/renderer/platform/heap/thread_state_scopes.h"
 #include "third_party/blink/renderer/platform/heap/visitor.h"
