01967c54f4d5c3335ced0e1d84c766132968c015
Peter Wen <wnwen@chromium.org>
2021-01-27 18:54:44 +0000

    Android: Document all remaining uses of md5_check
    This concludes our md5_check scrub. The remaining 3 places that use
    md5_check have compelling reasons for keeping the added complexity. It
    is mainly for build speed (dex.py and compile_java.py) and tooling
    performance (app_bundle_utils.py). If eventually those conditions change
    then we can consider removing the md5_check calls.
    
    In general, avoid using md5_check unless there are compelling reasons
    for doing so, since missed input_paths and input_strings can cause hard
    to diagnose and sporatic caching issues.
    
    Bug: 1066943
    Change-Id: Ia034f04b30c60ca7e4741d7964706871e41fc9cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650650
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847710}

--- build/android/gyp/compile_java.py
+++ build/android/gyp/compile_java.py
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import distutils.spawn
-import functools
 import logging
 import multiprocessing
 import optparse
@@ -731,6 +729,8 @@ def main(argv):
       options.warnings_as_errors, options.jar_info_exclude_globs
   ]
 
+  # Keep md5_check since we plan to use its changes feature to implement a build
+  # speed improvement for non-signature compiles: https://crbug.com/1170778
   md5_check.CallAndWriteDepfileIfStale(
       lambda: _OnStaleMd5(options, javac_cmd, javac_args, java_files),
       options,
