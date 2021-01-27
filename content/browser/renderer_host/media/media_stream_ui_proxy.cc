11dc22388c93a50938032fa1d08b3986f52c6370
Scott Violet <sky@chromium.org>
2021-01-27 16:57:39 +0000

    media: add code in hopes of tracking down crash
    WebLayer is seeing a crash that may be indicating
    ProcessStopRequestFromUI() is being called when stop_callback_ is
    null. In looking at the code, I'm a bit skeptical this is actually
    happening. Because of inlining, I can't definitively
    say this is the crash, so instead I'm converting a DCHECK to a
    CHECK and including some additional logging information. I'll remove
    this once resolved
    
    BUG=1140187
    TEST=none
    
    Change-Id: I78fedf73884211069756a73d3afdf11264e9b76c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648720
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847655}

--- content/browser/renderer_host/media/media_stream_ui_proxy.cc
+++ content/browser/renderer_host/media/media_stream_ui_proxy.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/debug/alias.h"
 #include "base/macros.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/browser/renderer_host/render_frame_host_delegate.h"
@@ -294,12 +295,15 @@ void MediaStreamUIProxy::OnStarted(
     WindowIdCallback window_id_callback,
     const std::string& label,
     std::vector<DesktopMediaID> screen_share_ids,
-    MediaStreamUI::StateChangeCallback state_change_callback) {
+    MediaStreamUI::StateChangeCallback state_change_callback,
+    const GURL& url) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
   stop_callback_ = std::move(stop_callback);
   source_callback_ = std::move(source_callback);
   state_change_callback_ = std::move(state_change_callback);
+  last_url_ = url;
+  ++start_count_;
 
   // Owned by the PostTaskAndReply callback.
   gfx::NativeViewId* window_id = new gfx::NativeViewId(0);
@@ -334,8 +338,10 @@ void MediaStreamUIProxy::ProcessAccessRequestResponse(
 
 void MediaStreamUIProxy::ProcessStopRequestFromUI() {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  DCHECK(!stop_callback_.is_null());
-
+  const int start_count_copy = start_count_;
+  base::debug::Alias(&start_count_copy);
+  DEBUG_ALIAS_FOR_GURL(last_url_copy, last_url_);
+  CHECK(stop_callback_);
   std::move(stop_callback_).Run();
 }
 
@@ -454,7 +460,8 @@ void FakeMediaStreamUIProxy::OnStarted(
     WindowIdCallback window_id_callback,
     const std::string& label,
     std::vector<DesktopMediaID> screen_share_ids,
-    MediaStreamUI::StateChangeCallback state_change_callback) {}
+    MediaStreamUI::StateChangeCallback state_change_callback,
+    const GURL& url) {}
 
 void FakeMediaStreamUIProxy::OnDeviceStopped(const std::string& label,
                                              const DesktopMediaID& media_id) {}
