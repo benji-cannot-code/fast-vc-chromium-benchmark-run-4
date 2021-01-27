e40ff9312181e8510c2281dae9b5ffee7de22ffd
Xiaoqian Dai <xdai@chromium.org>
2021-01-27 17:07:14 +0000

    [reland] capture mode: implement customized image capture notification.
    Bug: 1159111
    Change-Id: Ibb554d0e5beca04d3f706db4146e9523430446c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2632390
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#846957}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648581
    Cr-Commit-Position: refs/heads/master@{#847657}

--- ash/public/cpp/external_arc/message_center/arc_notification_content_view_unittest.cc
+++ ash/public/cpp/external_arc/message_center/arc_notification_content_view_unittest.cc
@@ -138,9 +138,8 @@ class ArcNotificationContentViewTest : public AshTestBase {
 
     surface_manager_ = std::make_unique<ArcNotificationSurfaceManagerImpl>();
 
-    message_center::MessageViewFactory::
-        ClearCustomNotificationViewFactoryForTest(
-            kArcNotificationCustomViewType);
+    message_center::MessageViewFactory::ClearCustomNotificationViewFactory(
+        kArcNotificationCustomViewType);
     ArcNotificationManager::SetCustomNotificationViewFactory();
   }
 
@@ -348,7 +347,7 @@ TEST_F(ArcNotificationContentViewTest, CloseButton) {
 TEST_F(ArcNotificationContentViewTest, CloseButtonInMessageCenterView) {
   std::string notification_key("notification id");
 
-  message_center::MessageViewFactory::ClearCustomNotificationViewFactoryForTest(
+  message_center::MessageViewFactory::ClearCustomNotificationViewFactory(
       kArcNotificationCustomViewType);
 
   // Override MessageView factory to capture the created notification view in
