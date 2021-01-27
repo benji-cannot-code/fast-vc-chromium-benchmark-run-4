9d1ab87224d9449df59658b5fa6e8eff58608f52
Robert Kaplow <rkaplow@chromium.org>
2021-01-27 16:51:44 +0000

    Rename and augment Stability histogram to replace the prefs.
    Justification for the omissions of some of the fields:
    
    (incremental)uptime - out of scope for goals of histogram
    3 cros_specific (other_user/kernel/unclean): Waiting to hear back from team
    crash_count_due_to_gms_core_update - spoke to team, likely unneeded
    breakpad - likely unneeded
    debugger - likely unneeded
    from_previous_run - not relevant
    
    Change-Id: I31f292aefd5cd5b91766bd70e6f481f1511671cf
    
    Bug: 1019780
    Change-Id: I31f292aefd5cd5b91766bd70e6f481f1511671cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2639162
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847651}

--- chromecast/browser/metrics/cast_stability_metrics_provider.cc
+++ chromecast/browser/metrics/cast_stability_metrics_provider.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/base/pref_names.h"
 #include "chromecast/metrics/cast_metrics_service_client.h"
 #include "components/metrics/metrics_service.h"
+#include "components/metrics/stability_metrics_helper.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/child_process_data.h"
@@ -155,6 +156,8 @@ void CastStabilityMetricsProvider::BrowserChildProcessCrashed(
     const content::ChildProcessData& data,
     const content::ChildProcessTerminationInfo& info) {
   IncrementPrefValue(prefs::kStabilityChildProcessCrashCount);
+  ::metrics::StabilityMetricsHelper::RecordStabilityEvent(
+      ::metrics::StabilityEventType::kChildProcessCrash);
 }
 
 void CastStabilityMetricsProvider::LogRendererCrash(
@@ -164,6 +167,8 @@ void CastStabilityMetricsProvider::LogRendererCrash(
   if (status == base::TERMINATION_STATUS_PROCESS_CRASHED ||
       status == base::TERMINATION_STATUS_ABNORMAL_TERMINATION) {
     IncrementPrefValue(prefs::kStabilityRendererCrashCount);
+    ::metrics::StabilityMetricsHelper::RecordStabilityEvent(
+        ::metrics::StabilityEventType::kRendererCrash);
 
     base::UmaHistogramSparse("CrashExitCodes.Renderer",
                              MapCrashExitCodeForHistogram(exit_code));
@@ -177,11 +182,15 @@ void CastStabilityMetricsProvider::LogRendererCrash(
                               RENDERER_TYPE_RENDERER, RENDERER_TYPE_COUNT);
   } else if (status == base::TERMINATION_STATUS_LAUNCH_FAILED) {
     IncrementPrefValue(prefs::kStabilityRendererFailedLaunchCount);
+    ::metrics::StabilityMetricsHelper::RecordStabilityEvent(
+        ::metrics::StabilityEventType::kRendererFailedLaunch);
   }
 }
 
 void CastStabilityMetricsProvider::LogRendererHang() {
   IncrementPrefValue(prefs::kStabilityRendererHangCount);
+  ::metrics::StabilityMetricsHelper::RecordStabilityEvent(
+      ::metrics::StabilityEventType::kRendererHang);
 }
 
 void CastStabilityMetricsProvider::IncrementPrefValue(const char* path) {
--- components/metrics/stability_metrics_helper_unittest.cc
+++ components/metrics/stability_metrics_helper_unittest.cc
@@ -48,6 +48,7 @@ class StabilityMetricsHelperTest : public testing::Test {
 
 TEST_F(StabilityMetricsHelperTest, BrowserChildProcessCrashed) {
   StabilityMetricsHelper helper(prefs());
+  base::HistogramTester histogram_tester;
 
   helper.BrowserChildProcessCrashed();
   helper.BrowserChildProcessCrashed();
@@ -63,6 +64,8 @@ TEST_F(StabilityMetricsHelperTest, BrowserChildProcessCrashed) {
       system_profile.stability();
 
   EXPECT_EQ(2, stability.child_process_crash_count());
+  histogram_tester.ExpectUniqueSample(
+      "Stability.Counts2", StabilityEventType::kChildProcessCrash, 2);
 }
 
 TEST_F(StabilityMetricsHelperTest, LogRendererCrash) {
@@ -98,6 +101,12 @@ TEST_F(StabilityMetricsHelperTest, LogRendererCrash) {
   histogram_tester.ExpectUniqueSample("CrashExitCodes.Renderer", 1, 3);
   histogram_tester.ExpectBucketCount("BrowserRenderProcessHost.ChildCrashes",
                                      RENDERER_TYPE_RENDERER, 3);
+  histogram_tester.ExpectBucketCount("Stability.Counts2",
+                                     StabilityEventType::kRendererCrash, 3);
+  histogram_tester.ExpectBucketCount(
+      "Stability.Counts2", StabilityEventType::kRendererFailedLaunch, 1);
+  histogram_tester.ExpectBucketCount("Stability.Counts2",
+                                     StabilityEventType::kExtensionCrash, 0);
 
   // One launch failure each.
   histogram_tester.ExpectBucketCount(
@@ -132,9 +141,17 @@ TEST_F(StabilityMetricsHelperTest, LogRendererCrashEnableExtensions) {
   helper.ProvideStabilityMetrics(&system_profile);
 
   EXPECT_EQ(0, system_profile.stability().renderer_crash_count());
-  EXPECT_EQ(2, system_profile.stability().extension_renderer_crash_count());
   EXPECT_EQ(
       1, system_profile.stability().extension_renderer_failed_launch_count());
+  EXPECT_EQ(2, system_profile.stability().extension_renderer_crash_count());
+
+  histogram_tester.ExpectBucketCount("Stability.Counts2",
+                                     StabilityEventType::kRendererCrash, 0);
+  histogram_tester.ExpectBucketCount(
+      "Stability.Counts2", StabilityEventType::kExtensionRendererFailedLaunch,
+      1);
+  histogram_tester.ExpectBucketCount("Stability.Counts2",
+                                     StabilityEventType::kExtensionCrash, 2);
 
   histogram_tester.ExpectBucketCount(
       "BrowserRenderProcessHost.ChildLaunchFailureCodes", 1, 1);
