0b31ea34ae51f817f4afc4cd237716ebca42ea23
sczs <sczs@chromium.org>
2021-01-27 09:18:27 +0000

    [ios] Stops observing ContentSuggestionsService if DiscoverFeed enabled
    Previously we were early returning in some methods, but it would be
    safer to completely stop observing this.
    
    Bug: 1085419
    Change-Id: I94444c8ed1da8594ec02440e2f9bae46745bb5dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650367
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847549}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
@@ -163,10 +163,10 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
   if (self) {
     _contentSuggestionsEnabled =
         prefService->FindPreference(prefs::kArticlesForYouEnabled);
-    // TODO(crbug.com/1085419): Stop observing this Service once DiscoverFeed is
-    // launched.
-    _suggestionBridge =
-        std::make_unique<ContentSuggestionsServiceBridge>(self, contentService);
+    if (!IsDiscoverFeedEnabled()) {
+      _suggestionBridge = std::make_unique<ContentSuggestionsServiceBridge>(
+          self, contentService);
+    }
     _contentService = contentService;
     _sectionInformationByCategory = [[NSMutableDictionary alloc] init];
 
@@ -448,10 +448,7 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
 - (void)contentSuggestionsService:
             (ntp_snippets::ContentSuggestionsService*)suggestionsService
          newSuggestionsInCategory:(ntp_snippets::Category)category {
-  // Ignore newSuggestionsInCategory if the DiscoverFeed is enabled, if not
-  // these might cause some unecessary section updates and crashes.
-  if (IsDiscoverFeedEnabled())
-    return;
+  DCHECK(!IsDiscoverFeedEnabled());
 
   ContentSuggestionsCategoryWrapper* wrapper =
       [ContentSuggestionsCategoryWrapper wrapperWithCategory:category];
@@ -475,10 +472,7 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
             (ntp_snippets::ContentSuggestionsService*)suggestionsService
                          category:(ntp_snippets::Category)category
                   statusChangedTo:(ntp_snippets::CategoryStatus)status {
-  // Ignore all ContentSuggestionsService if the DiscoverFeed is enabled, if not
-  // these might cause some unecessary section updates and crashes.
-  if (IsDiscoverFeedEnabled())
-    return;
+  DCHECK(!IsDiscoverFeedEnabled());
 
   ContentSuggestionsCategoryWrapper* wrapper =
       [[ContentSuggestionsCategoryWrapper alloc] initWithCategory:category];
@@ -511,6 +505,7 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
             (ntp_snippets::ContentSuggestionsService*)suggestionsService
             suggestionInvalidated:
                 (const ntp_snippets::ContentSuggestion::ID&)suggestion_id {
+  DCHECK(!IsDiscoverFeedEnabled());
   ContentSuggestionsCategoryWrapper* wrapper =
       [[ContentSuggestionsCategoryWrapper alloc]
           initWithCategory:suggestion_id.category()];
@@ -524,6 +519,7 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
 
 - (void)contentSuggestionsServiceFullRefreshRequired:
     (ntp_snippets::ContentSuggestionsService*)suggestionsService {
+  DCHECK(!IsDiscoverFeedEnabled());
   // The UICollectionView -reloadData method is a no-op if it is called at the
   // same time as other collection updates. This full refresh command can come
   // at the same time as other collection update commands. To make sure that it
@@ -538,6 +534,7 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
 
 - (void)contentSuggestionsServiceShutdown:
     (ntp_snippets::ContentSuggestionsService*)suggestionsService {
+  DCHECK(!IsDiscoverFeedEnabled());
   // Update dataSink.
 }
 
