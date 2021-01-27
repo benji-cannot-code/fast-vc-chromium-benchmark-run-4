5d7c689ee52e3a32dd1fe12b0d6e63669ddbc1ad
Maksim Sisov <msisov@igalia.com>
2021-01-27 21:27:50 +0000

    ozone/wayland: remove Pointer::Delegate::OnPointerDestroyed/Created
    These methods mostly result in dependency of WaylandEventSource on
    WaylandPointer.
    
    Thus, remove them and make WaylandPointer reset pointer flags
    stored by WaylandEventSource on destruction + reset pointer focus.
    This will result in adding input emulation with less troubles/workarounds.
    
    Bug: 1134495
    Change-Id: Iffb2ee1859dae5d84a20e6a803c66d30aeeb9106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2652427
    Auto-Submit: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#847795}

--- ui/ozone/platform/wayland/host/wayland_event_source.cc
+++ ui/ozone/platform/wayland/host/wayland_event_source.cc
@@ -125,26 +125,8 @@ uint32_t WaylandEventSource::OnKeyboardKeyEvent(EventType type,
   return DispatchEvent(&event);
 }
 
-void WaylandEventSource::OnPointerCreated(WaylandPointer* pointer) {
-  DCHECK(pointer);
-  pointer_ = pointer;
-}
-
-void WaylandEventSource::OnPointerDestroyed(WaylandPointer* pointer) {
-  DCHECK_EQ(pointer_, pointer);
-
-  // Clear focused window, if any.
-  HandlePointerFocusChange(nullptr);
-
-  ResetPointerFlags();
-  pointer_ = nullptr;
-}
-
 void WaylandEventSource::OnPointerFocusChanged(WaylandWindow* window,
                                                const gfx::PointF& location) {
-  if (!pointer_)
-    return;
-
   // Save new pointer location.
   pointer_location_ = location;
 
@@ -166,9 +148,6 @@ void WaylandEventSource::OnPointerButtonEvent(EventType type,
                                               WaylandWindow* window) {
   DCHECK(type == ET_MOUSE_PRESSED || type == ET_MOUSE_RELEASED);
   DCHECK(HasAnyPointerButtonFlag(changed_button));
-
-  if (!pointer_)
-    return;
 
   auto* prev_focused_window = window_with_pointer_focus_;
   if (window)
@@ -203,6 +182,10 @@ void WaylandEventSource::OnPointerAxisEvent(const gfx::Vector2d& offset) {
   DispatchEvent(&event);
   current_pointer_frame_.dx += offset.x();
   current_pointer_frame_.dy += offset.y();
+}
+
+void WaylandEventSource::OnResetPointerFlags() {
+  ResetPointerFlags();
 }
 
 void WaylandEventSource::OnPointerFrameEvent() {
--- ui/ozone/platform/wayland/host/wayland_event_source.h
+++ ui/ozone/platform/wayland/host/wayland_event_source.h
@@ -91,8 +91,6 @@ class WaylandEventSource : public PlatformEventSource,
                               int device_id) override;
 
   // WaylandPointer::Delegate
-  void OnPointerCreated(WaylandPointer* pointer) override;
-  void OnPointerDestroyed(WaylandPointer* pointer) override;
   void OnPointerFocusChanged(WaylandWindow* window,
                              const gfx::PointF& location) override;
   void OnPointerButtonEvent(EventType evtype,
@@ -103,6 +101,7 @@ class WaylandEventSource : public PlatformEventSource,
   void OnPointerFrameEvent() override;
   void OnPointerAxisSourceEvent(uint32_t axis_source) override;
   void OnPointerAxisStopEvent(uint32_t axis) override;
+  void OnResetPointerFlags() override;
 
   // WaylandTouch::Delegate
   void OnTouchCreated(WaylandTouch* touch) override;
@@ -147,7 +146,6 @@ class WaylandEventSource : public PlatformEventSource,
   WaylandWindowManager* const window_manager_;
 
   // Input device objects. Owned by WaylandConnection.
-  WaylandPointer* pointer_ = nullptr;
   WaylandTouch* touch_ = nullptr;
 
   // Bitmask of EventFlags used to keep track of the the pointer state.
