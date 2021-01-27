6e2fa5aee4241e17465d8492fc477e44b1843e30
Dave Tapuska <dtapuska@chromium.org>
2021-01-27 15:49:40 +0000

    Remove AcceptLanguagesChanged and AcceptLanguages from blink public API.
    Use the RendererPreferences setting API instead of having callbacks
    to fetch them from the RendererPreferences object.
    
    BUG=1155202
    
    Change-Id: Ie2889c92897c29805b87a3864a5588c5c4bcc927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650687
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847630}

--- third_party/blink/renderer/core/exported/web_view_impl.h
+++ third_party/blink/renderer/core/exported/web_view_impl.h
@@ -188,7 +188,6 @@ class CORE_EXPORT WebViewImpl final : public WebView,
   void DidCloseContextMenu() override;
   void CancelPagePopup() override;
   WebPagePopupImpl* GetPagePopup() const override { return page_popup_.get(); }
-  void AcceptLanguagesChanged() override;
   void SetPageFrozen(bool frozen) override;
   WebFrameWidget* MainFrameWidget() override;
   void SetBaseBackgroundColor(SkColor) override;
@@ -547,6 +546,7 @@ class CORE_EXPORT WebViewImpl final : public WebView,
   friend class WebView;  // So WebView::Create can call our constructor
   friend class WTF::RefCounted<WebViewImpl>;
 
+  void AcceptLanguagesChanged();
   void ThemeChanged();
 
   // Update the target url locally and tell the browser that the target URL has
