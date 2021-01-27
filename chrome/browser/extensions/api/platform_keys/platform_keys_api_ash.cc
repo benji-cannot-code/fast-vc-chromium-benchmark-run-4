fcb7ab49c6afd9851c9f260594cc4129ec2d9126
Maksim Moskvitin <mmoskvitin@google.com>
2021-01-27 15:15:19 +0000

    [CodeHealth] Convert c/b/e/api/platform_keys to use modern callback
    Bug: 1152279
    Change-Id: I6da4127dc28da0ec954aa845119a8a5ccdbdb972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2645020
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847617}

--- chrome/browser/extensions/api/platform_keys/platform_keys_api_ash.cc
+++ chrome/browser/extensions/api/platform_keys/platform_keys_api_ash.cc
@@ -223,9 +223,9 @@ PlatformKeysInternalSelectClientCertificatesFunction::Run() {
   service->SelectClientCertificates(
       request, std::move(client_certs), params->details.interactive,
       extension_id(),
-      base::Bind(&PlatformKeysInternalSelectClientCertificatesFunction::
-                     OnSelectedCertificates,
-                 this),
+      base::BindOnce(&PlatformKeysInternalSelectClientCertificatesFunction::
+                         OnSelectedCertificates,
+                     this),
       web_contents);
   return RespondLater();
 }
@@ -303,7 +303,7 @@ ExtensionFunction::ResponseAction PlatformKeysInternalSignFunction::Run() {
         std::string(params->data.begin(), params->data.end()),
         std::string(params->public_key.begin(), params->public_key.end()),
         extension_id(),
-        base::Bind(&PlatformKeysInternalSignFunction::OnSigned, this));
+        base::BindOnce(&PlatformKeysInternalSignFunction::OnSigned, this));
   } else {
     chromeos::platform_keys::HashAlgorithm hash_algorithm;
     if (params->hash_algorithm_name == "SHA-1") {
@@ -334,7 +334,7 @@ ExtensionFunction::ResponseAction PlatformKeysInternalSignFunction::Run() {
         std::string(params->data.begin(), params->data.end()),
         std::string(params->public_key.begin(), params->public_key.end()),
         key_type, hash_algorithm, extension_id(),
-        base::Bind(&PlatformKeysInternalSignFunction::OnSigned, this));
+        base::BindOnce(&PlatformKeysInternalSignFunction::OnSigned, this));
   }
 
   return RespondLater();
@@ -366,9 +366,9 @@ PlatformKeysVerifyTLSServerCertificateFunction::Run() {
   VerifyTrustAPI::GetFactoryInstance()
       ->Get(browser_context())
       ->Verify(std::move(params), extension_id(),
-               base::Bind(&PlatformKeysVerifyTLSServerCertificateFunction::
-                              FinishedVerification,
-                          this));
+               base::BindOnce(&PlatformKeysVerifyTLSServerCertificateFunction::
+                                  FinishedVerification,
+                              this));
 
   return RespondLater();
 }
