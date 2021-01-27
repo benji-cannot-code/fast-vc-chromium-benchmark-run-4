da5d842d50c82dd7da02dc22879c3362c9470921
Trent Begin <tbegin@google.com>
2021-01-27 02:31:33 +0000

    connectivity-diagnostics: add network-health-container element
    The network health container element will be used in both the
    network-diagnostics and network-health-summary component. This change
    factors out the component from routine-group into its own element for
    reuse.
    
    Bug: chromium:1170082
    Change-Id: I9c936da8c9e5554236f6dd899c8b38615db72891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2645672
    Commit-Queue: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847458}

--- ui/webui/resources/cr_components/chromeos/network_health/routine_group.js
+++ ui/webui/resources/cr_components/chromeos/network_health/routine_group.js
@@ -43,7 +43,7 @@ Polymer({
     },
 
     /**
-     * Boolean flag if the routine group is expanded.
+     * Boolean flag if the container is expanded.
      * @private {Boolean}
      */
     expanded: {
@@ -59,15 +59,6 @@ Polymer({
       type: Boolean,
       computed: 'showGroupIcon_(running, expanded)',
     },
-  },
-
-  /**
-   * Returns the correct arrow icon depending on if the routine group is
-   * expanded.
-   * @param {boolean} expanded
-   */
-  getArrowIcon_(expanded) {
-    return expanded ? 'cr:expand-less' : 'cr:expand-more';
   },
 
   /**
@@ -157,14 +148,5 @@ Polymer({
       }
     }
     return false;
-  },
-
-  /**
-   * Helper function to toggle the expanded properties when the routine group
-   * is clicked.
-   * @private
-   */
-  onGroupClick_() {
-    this.set('expanded', !this.expanded);
   },
 });
