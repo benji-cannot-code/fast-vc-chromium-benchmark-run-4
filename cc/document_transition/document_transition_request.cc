50c62ed3c26f6917db18f7afa950c3536ad457fb
Vladimir Levin <vmpstr@chromium.org>
2021-01-27 15:47:37 +0000

    cc: Add ToString for DocumentTransitionRequest.
    This patch adds a ToString() function to document transition request
    which is useful for debugging.
    
    R=kylechar@chromium.org, vasilyt@chromium.org
    
    Bug: 1150461
    Change-Id: I93063f5ed6d4d69cd9393d5e05a88ac1f465d6de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650954
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847628}

--- cc/document_transition/document_transition_request.cc
+++ cc/document_transition/document_transition_request.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/document_transition/document_transition_request.h"
 
 #include <memory>
+#include <sstream>
 #include <utility>
 
 #include "base/callback.h"
@@ -13,6 +14,50 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/compositor_frame_transition_directive.h"
 
 namespace cc {
+namespace {
+
+std::string TypeToString(viz::CompositorFrameTransitionDirective::Type type) {
+  switch (type) {
+    case viz::CompositorFrameTransitionDirective::Type::kSave:
+      return "kSave";
+    case viz::CompositorFrameTransitionDirective::Type::kAnimate:
+      return "kAnimate";
+  }
+  return "<unknown>";
+}
+
+std::string EffectToString(
+    viz::CompositorFrameTransitionDirective::Effect effect) {
+  switch (effect) {
+    case viz::CompositorFrameTransitionDirective::Effect::kNone:
+      return "kNone";
+    case viz::CompositorFrameTransitionDirective::Effect::kCoverDown:
+      return "kCoverDown";
+    case viz::CompositorFrameTransitionDirective::Effect::kCoverLeft:
+      return "kCoverLeft";
+    case viz::CompositorFrameTransitionDirective::Effect::kCoverRight:
+      return "kCoverRight";
+    case viz::CompositorFrameTransitionDirective::Effect::kCoverUp:
+      return "kCoverUp";
+    case viz::CompositorFrameTransitionDirective::Effect::kExplode:
+      return "kExplode";
+    case viz::CompositorFrameTransitionDirective::Effect::kFade:
+      return "kFade";
+    case viz::CompositorFrameTransitionDirective::Effect::kImplode:
+      return "kImplode";
+    case viz::CompositorFrameTransitionDirective::Effect::kRevealDown:
+      return "kRevealDown";
+    case viz::CompositorFrameTransitionDirective::Effect::kRevealLeft:
+      return "kRevealLeft";
+    case viz::CompositorFrameTransitionDirective::Effect::kRevealRight:
+      return "kRevealRight";
+    case viz::CompositorFrameTransitionDirective::Effect::kRevealUp:
+      return "kRevealUp";
+  }
+  return "<unknown>";
+}
+
+}  // namespace
 
 uint32_t DocumentTransitionRequest::s_next_sequence_id_ = 1;
 
@@ -57,6 +102,14 @@ DocumentTransitionRequest::ConstructDirective() const {
           : viz::CompositorFrameTransitionDirective::kMaxDuration;
   return viz::CompositorFrameTransitionDirective(s_next_sequence_id_++, type_,
                                                  effect_, clamped_duration);
+}
+
+std::string DocumentTransitionRequest::ToString() const {
+  std::ostringstream str;
+  str << "[type: " << TypeToString(type_)
+      << " effect: " << EffectToString(effect_)
+      << " duration: " << duration_.InMillisecondsF() << "ms]";
+  return str.str();
 }
 
 }  // namespace cc
--- cc/document_transition/document_transition_request.h
+++ cc/document_transition/document_transition_request.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_DOCUMENT_TRANSITION_DOCUMENT_TRANSITION_REQUEST_H_
 
 #include <memory>
+#include <string>
 #include <utility>
 
 #include "base/callback.h"
@@ -48,6 +49,9 @@ class CC_EXPORT DocumentTransitionRequest {
   // would create a new sequence id for the directive, which means it would be
   // processed again by viz.
   viz::CompositorFrameTransitionDirective ConstructDirective() const;
+
+  // Testing / debugging functionality.
+  std::string ToString() const;
 
  private:
   using Type = viz::CompositorFrameTransitionDirective::Type;
