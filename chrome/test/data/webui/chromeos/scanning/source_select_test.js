3a196ce6a1e416eed5abbef60495afc7803d502c
Gavin Williams <gavinwill@chromium.org>
2021-01-28 21:50:18 +0000

    scanning: Fix Source select Default option
    Default option should not show when other sources are available.
    
    Fixed: 1171867
    Change-Id: I8358bc765038a7374a149013b99b4534e5210ec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2656877
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Auto-Submit: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848273}

--- chrome/test/data/webui/chromeos/scanning/source_select_test.js
+++ chrome/test/data/webui/chromeos/scanning/source_select_test.js
@@ -74,6 +74,7 @@ export function sourceSelectTest() {
     assertEquals(
         getSourceTypeString(secondSource.type),
         select.options[1].textContent.trim());
+    assertTrue(select.options[2].hidden);
     assertEquals(secondSource.name, select.value);
   });
 
