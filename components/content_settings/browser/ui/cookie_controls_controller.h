2a894cc72f230f099575d6003c9875edf697ae14
Andrey Zaytsev <andzaytsev@google.com>
2021-01-28 10:33:42 +0000

    Updated the cookie bubble on Desktop to omit 'third-party' when all cookies are blocked
    Screenshot: https://screenshot.googleplex.com/MXMSg6LJmwATRQD.png
    
    Bug: 1070923
    Change-Id: I61a72fff0eb897a0e548788c342c549970f20919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2645016
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/master@{#848055}

--- components/content_settings/browser/ui/cookie_controls_controller.h
+++ components/content_settings/browser/ui/cookie_controls_controller.h
@@ -45,6 +45,9 @@ class CookieControlsController : content_settings::CookieSettings::Observer {
   // blocking.
   void OnCookieBlockingEnabledForSite(bool block_third_party_cookies);
 
+  // Returns whether first-party cookies are blocked.
+  bool FirstPartyCookiesBlocked();
+
   void AddObserver(CookieControlsView* obs);
   void RemoveObserver(CookieControlsView* obs);
 
