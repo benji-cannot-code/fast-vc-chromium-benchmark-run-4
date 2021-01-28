3e2075e5e228157a2f3b02517c1f85b06ef7ae99
Peter Marshall <petermarshall@chromium.org>
2021-01-28 08:29:30 +0000

    [devtools] Do less work in flaky cpu profile test
    The test was creating a DataGrid with a huge number of nodes,
    which was taking too long to calculate on MSAN.
    
    Fixed: 1169908
    Change-Id: Ic3f56000e7e0e17c9fdc0157a90d2c52bf9a3ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2652690
    Auto-Submit: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848018}

--- third_party/blink/web_tests/http/tests/devtools/profiler/cpu-profiler-bottom-up-large-tree-search.js
+++ third_party/blink/web_tests/http/tests/devtools/profiler/cpu-profiler-bottom-up-large-tree-search.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   TestRunner.addResult(`Tests that search works for large bottom-up view of CPU profile.\n`);
   await TestRunner.loadModule('cpu_profiler_test_runner');
 
-  var nodesCount = 1000;
+  var nodesCount = 200;
   function buildTree(startId, count) {
     // Build a call tree of a chain form: foo1 -> foo2 -> foo3 -> ...
     // This should give a O(n^2) nodes in bottom-up tree.
