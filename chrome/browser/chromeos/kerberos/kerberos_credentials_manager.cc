bfd3d9106c52398459c14bbd355f2b91a534f39e
Felipe Andrade <fsandrade@chromium.org>
2021-01-28 20:02:24 +0000

    Update Kerberos subpage path in the manager
    Update KerberosCredentialsManager to use the correct URL path for
    Kerberos accounts subpage. This path is selected according to the
    Kerberos settings section flag.
    
    Bug: 983041
    Change-Id: I4fa605dfb8bab23d3a1f57b30b9a93e80078c169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653010
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848220}

--- chrome/browser/chromeos/kerberos/kerberos_credentials_manager.cc
+++ chrome/browser/chromeos/kerberos/kerberos_credentials_manager.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/chromeos/constants/routes.mojom.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/dbus/kerberos/kerberos_client.h"
 #include "chromeos/dbus/kerberos/kerberos_service.pb.h"
 #include "chromeos/network/onc/variable_expander.h"
@@ -932,12 +933,18 @@ void KerberosCredentialsManager::NotifyRequiresLoginPassword(
 
 void KerberosCredentialsManager::OnTicketExpiryNotificationClick(
     const std::string& principal_name) {
+  // The correct URL path for Kerberos accounts subpage, according to the
+  // Kerberos settings section flag.
+  const std::string kSubpagePath =
+      chromeos::features::IsKerberosSettingsSectionEnabled()
+          ? chromeos::settings::mojom::kKerberosAccountsV2SubpagePath
+          : chromeos::settings::mojom::kKerberosAccountsSubpagePath;
+
   // TODO(https://crbug.com/952245): Right now, the reauth dialog is tied to the
   // settings. Consider creating a standalone reauth dialog.
   chrome::SettingsWindowManager::GetInstance()->ShowOSSettings(
       primary_profile_,
-      chromeos::settings::mojom::kKerberosAccountsSubpagePath +
-          std::string("?kerberos_reauth=") +
+      kSubpagePath + "?kerberos_reauth=" +
           net::EscapeQueryParamValue(principal_name, false /* use_plus */));
 
   // Close last! |principal_name| is owned by the notification.
