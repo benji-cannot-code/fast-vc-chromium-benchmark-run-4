daf5984b59ddad95cddf9a31763e8d4a26ed8e8c
Fergus Dall <sidereal@google.com>
2021-01-28 02:50:57 +0000

    Mark CrosUsb.NotificationClosed as obsolete
    We decided to let this expire, so remove it.
    
    Bug: 1160806
    Change-Id: I8e6a58d9c7f57d751fda0e53d280ef5799581e2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648525
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847945}

--- chrome/browser/chromeos/usb/cros_usb_detector.cc
+++ chrome/browser/chromeos/usb/cros_usb_detector.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/notification_utils.h"
 #include "base/callback_helpers.h"
 #include "base/files/file_util.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chromeos/crostini/crostini_features.h"
@@ -52,10 +51,6 @@ const char kParallelsShortName[] = "Parallels";
 static CrosUsbDetector* g_cros_usb_detector = nullptr;
 
 const char kNotifierUsb[] = "crosusb.connected";
-
-void RecordNotificationClosure(CrosUsbNotificationClosed disposition) {
-  UMA_HISTOGRAM_ENUMERATION("CrosUsb.NotificationClosed", disposition);
-}
 
 base::string16 ProductLabelFromDevice(
     const device::mojom::UsbDeviceInfoPtr& device_info) {
@@ -171,7 +166,6 @@ class CrosUsbNotificationDelegate
   void Close(bool by_user) override {
     if (by_user)
       disposition_ = chromeos::CrosUsbNotificationClosed::kByUser;
-    RecordNotificationClosure(disposition_);
   }
 
  private:
