dc7ff8690ab998bcd28766d6a7e426ee30f1306c
Josiah K <josiahk@google.com>
2021-01-28 23:59:07 +0000

    [Magnifier] Add pref for screen magnifier mouse panning modes
    This pref will let the user pick one of three mouse panning modes: two new options (centered and continuous) and the current behavior (edge panning) in full screen magnifier.
    
    The default pref value of none is included so we can bifurcate the option for legacy and new magnifier users. Legacy magnifier users will get a consistent experience (edge panning mode), while new magnifier users can get our new desired default experience (to be determined, but possibly continuous panning mode).
    
    (go/magnifier-panning-improvements-design-doc)
    
    AX-Relnotes: N/A.
    Bug: 1144947
    Change-Id: I93e61a1b13ba78c74307519c6a22d44e10bfd8d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648554
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848325}

--- chrome/browser/prefs/pref_service_incognito_allowlist.cc
+++ chrome/browser/prefs/pref_service_incognito_allowlist.cc
@@ -42,6 +42,7 @@ const char* const kPersistentPrefNames[] = {
     ash::prefs::kAccessibilityScreenMagnifierCenterFocus,
     ash::prefs::kAccessibilityScreenMagnifierEnabled,
     ash::prefs::kAccessibilityScreenMagnifierFocusFollowingEnabled,
+    ash::prefs::kAccessibilityScreenMagnifierMousePanningMode,
     ash::prefs::kAccessibilityScreenMagnifierScale,
     ash::prefs::kAccessibilityVirtualKeyboardEnabled,
     ash::prefs::kAccessibilityMonoAudioEnabled,
