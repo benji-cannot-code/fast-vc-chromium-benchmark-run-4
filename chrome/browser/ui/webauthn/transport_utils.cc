7fafe4a66a64d039b657d15af4ea41363d2c0413
Martin Kreichgauer <martinkr@google.com>
2021-01-28 00:55:02 +0000

    webauthn: rename "built-in sensor" to "this device" in UI
    UXW review doc:
    https://docs.google.com/document/d/1INQokRexMfxCoWDvTpY--HUroxm9d8ubJPY_NyJJ9tA/edit
    
    Bug: 1111095
    Change-Id: I6db609fdd9e1546b73836fdbb8ae85e13722c952
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2642926
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#847897}

--- chrome/browser/ui/webauthn/transport_utils.cc
+++ chrome/browser/ui/webauthn/transport_utils.cc
@@ -85,7 +85,7 @@ const gfx::VectorIcon* GetTransportVectorIcon(
     case AuthenticatorTransport::kUsbHumanInterfaceDevice:
       return &vector_icons::kUsbIcon;
     case AuthenticatorTransport::kInternal:
-      return &kFingerprintIcon;
+      return &kLaptopIcon;
     case AuthenticatorTransport::kCloudAssistedBluetoothLowEnergy:
       return &kSmartphoneIcon;
     case AuthenticatorTransport::kAndroidAccessory:
