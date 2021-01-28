dc442747ad3ffd4359695af6e6cee773308b7666
Tal Pressman <talp@chromium.org>
2021-01-28 04:09:40 +0000

    [Prerendering]Set mojo binding policy for gamepad interfaces to kCancel.
    We use kCancel since these interfaces contain sync methods that we don't
    currently support, and we don't think it will effect coverage too much.
    The CL does not contain a test, as kCancel is not yet implemented.
    
    Change-Id: I7d2ab9a21f02589c6f40314ed1b76558718a37a8
    Bug: 1158250
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650003
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Tal Pressman <talp@google.com>
    Cr-Commit-Position: refs/heads/master@{#847963}

--- content/browser/mojo_binder_policy_map_impl.cc
+++ content/browser/mojo_binder_policy_map_impl.cc
@@ -7,11 +7,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "content/public/browser/content_browser_client.h"
+#include "content/public/browser/mojo_binder_policy_map.h"
 #include "content/public/common/content_client.h"
+#include "device/gamepad/public/mojom/gamepad.mojom.h"
 
 namespace content {
 
 namespace {
+
+// Register mojo binder policies for prerendering for content/ interfaces.
+void RegisterContentBinderPoliciesForPrerendering(MojoBinderPolicyMap& map) {
+  map.SetPolicy<device::mojom::GamepadMonitor>(MojoBinderPolicy::kCancel);
+  map.SetPolicy<device::mojom::GamepadHapticsManager>(
+      MojoBinderPolicy::kCancel);
+}
 
 // A singleton class that stores the `MojoBinderPolicyMap` of interfaces which
 // are obtained via `BrowserInterfaceBrowser` for frames.
@@ -20,6 +29,7 @@ namespace {
 class BrowserInterfaceBrokerMojoBinderPolicyMapHolder {
  public:
   BrowserInterfaceBrokerMojoBinderPolicyMapHolder() {
+    RegisterContentBinderPoliciesForPrerendering(map_);
     GetContentClient()->browser()->RegisterMojoBinderPoliciesForPrerendering(
         map_);
   }
