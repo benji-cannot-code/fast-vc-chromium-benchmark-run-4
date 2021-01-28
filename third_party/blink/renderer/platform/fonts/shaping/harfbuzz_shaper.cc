c48be2d765e83ab9c30c8ab8c8470df5b3c852d1
Koji Ishii <kojii@chromium.org>
2021-01-28 17:23:27 +0000

    Default-enable `chws` or `vchw` font feature
    This patch default-enables `chws` or `vchw` font feature.
    
    The OpenType spec for `chws`:
    https://docs.microsoft.com/en-us/typography/opentype/spec/features_ae#tag-chws
    suggests to always enable this feature if the application does
    not support JLREQ "3.1.4 Positioning of Consecutive Opening
    Brackets, Closing Brackets, Commas, Full Stops and Middle Dots"
    https://w3c.github.io/jlreq/?lang=en#positioning_of_consecutive_opening_brackets_closing_brackets_comma_full_stops_and_middle_dots
    and CLREQ "3.1.6.1 Punctuation Adjustment Space":
    https://w3c.github.io/clreq/?lang=en#h-punctuation_adjustment_space
    
    Supporting these features require kerning operation in the
    application, or split puncutuation characters into separate
    runs and apply appropriate features to each run depends on
    used fonts. Using font feature gives advantages such as
    utilizing kerning code in HarfBuzz, and faster shaping.
    
    In "Feature interaction", it also suggests to make it mutually
    exclusive with all other glyph-width features. Among the list
    of such features, this patch makes it exclusive only with
    `halt`, `palt`, and their vertical counterparts. These
    relatively new features are often implemented in GPOS, and
    that they need to be exclusive.
    
    This patch default-enables 'chws' or 'vchw' if no `halt`,
    `palt`, or their vertical counterparts were requested.
    
    Other features are implemented in GSUB in all fonts I
    investigated, and that it is not necessary to check. In
    OpenType community, there's an opinion to allow GPOS to more
    features, while it is known that doing so is not backward
    compatible. The discussion isn't finalized yet, but local
    tests indicate implementing these features in GPOS breaks
    Blink too. If the discussion settled down and such fonts are
    going to appear, we can work on these other features. For more
    details, please refer to OpenType spec:
    https://docs.microsoft.com/en-us/typography/opentype/spec/gpos
    https://docs.microsoft.com/en-us/typography/opentype/spec/gsub
    
    Fonts with `chws` and `vchw` features have landed in AOSP.
    https://android.googlesource.com/platform/external/noto-fonts/+/da4f473c2fc3b24268ead933d093e141e4b32ab9
    
    Change-Id: I15e00357b1925a3c9d52e86e3e1f50c73b46ef5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2626823
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848146}

--- third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper.cc
+++ third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper.cc
@@ -586,7 +586,7 @@ inline RangeData CreateRangeData(const Font* font,
   range_data.buffer = buffer;
   range_data.font = font;
   range_data.text_direction = direction;
-  range_data.font_features.Initialize(*font);
+  range_data.font_features.Initialize(font->GetFontDescription());
   return range_data;
 }
 
