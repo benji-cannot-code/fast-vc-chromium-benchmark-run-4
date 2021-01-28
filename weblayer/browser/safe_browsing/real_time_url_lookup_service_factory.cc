21d7e9bc2b21d0cd77623b00a6ef15caf7142145
Colin Blundell <blundell@chromium.org>
2021-01-28 09:48:09 +0000

    [WebLayer] Add support for Gaia-keyed safe browsing realtime URL lookups
    This CL adds support for Gaia-keyed safe browsing realtime URL lookups
    to WebLayer via a //weblayer-specific implementation of
    SafeBrowsingTokenFetcher that obtains access tokens from the WebLayer
    embedder. That implementation follows the design of the
    SafeBrowsingTokenFetcher implementation that Chrome uses, relying on the
    shared SafeBrowsingTokenFetchTracker helper class to time out access
    token requests after a given delay.
    
    This CL supplies a SafeBrowsingTokenFetcherImpl instance to
    RealTimeUrlLookupService but does not actually enable access token
    fetches in the context of WebLayer, as the specific flow for enabling
    this functionality is still under consideration.
    
    This CL also adds tests of the new functionality:
    - unittests of SafeBrowsingTokenFetcherImpl
    - browsertests that verify that safe browsing access token fetches
      occur/don't occur as part of navigations as expected in various
      configurations of safe browsing.
    
    Bug: 1080748
    Change-Id: If39c59f44dc469fdd6a22ae4dfa5c2d48e03e158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2652466
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848040}

--- weblayer/browser/safe_browsing/real_time_url_lookup_service_factory.cc
+++ weblayer/browser/safe_browsing/real_time_url_lookup_service_factory.cc
@@ -15,7 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "weblayer/browser/browser_context_impl.h"
 #include "weblayer/browser/browser_process.h"
 #include "weblayer/browser/feature_list_creator.h"
+#include "weblayer/browser/profile_impl.h"
 #include "weblayer/browser/safe_browsing/safe_browsing_service.h"
+#include "weblayer/browser/safe_browsing/safe_browsing_token_fetcher_impl.h"
 #include "weblayer/browser/verdict_cache_manager_factory.h"
 
 namespace weblayer {
@@ -54,10 +56,14 @@ KeyedService* RealTimeUrlLookupServiceFactory::BuildServiceInstanceFor(
       // History sync is never enabled in WebLayer.
       base::BindRepeating([]() { return false; }),
       static_cast<BrowserContextImpl*>(context)->pref_service(),
-      // TODO(crbug.com/1080748): Bring up token fetching for URL lookups and
-      // configure the following two parameters accordingly.
-      nullptr /* token_fetcher */,
-      base::BindRepeating([](bool) { return false; }),
+      std::make_unique<SafeBrowsingTokenFetcherImpl>(base::BindRepeating(
+          &ProfileImpl::access_token_fetch_delegate,
+          base::Unretained(ProfileImpl::FromBrowserContext(context)))),
+      // TODO(crbug.com/1171215): Change this to production mechanism for
+      // enabling Gaia-keyed URL lookups once that mechanism is determined.
+      base::BindRepeating(&RealTimeUrlLookupServiceFactory::
+                              access_token_fetches_enabled_for_testing,
+                          base::Unretained(this)),
       safe_browsing::GetProfileManagementStatus(nullptr),
       false /* is_under_advanced_protection */,
       static_cast<BrowserContextImpl*>(context)->IsOffTheRecord(),
