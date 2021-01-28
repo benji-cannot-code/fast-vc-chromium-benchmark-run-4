ee671fc9820387cc9429466d7d5cc192aea51f4d
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-01-28 06:46:12 +0000

    Revert "Net Internals: Modernize JS tests"
    This reverts commit 9b30431c8e7fc56ef1e09b6e33a134f772bf1175.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 847841 as the
    culprit for flakes in the build cycles as shown on:
    https://analysis.chromium.org/p/chromium/flake-portal/analysis/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vOWIzMDQzMWM4ZTdmYzU2ZWYxZTA5YjZlMzNhMTM0Zjc3MmJmMTE3NQw
    
    Sample Failed Build: https://ci.chromium.org/b/8856885146977279104
    
    Sample Failed Step: browser_tests_x11
    
    Sample Flaky Test: NetInternalsDomainSecurityPolicyViewTest.DeleteError
    
    Original change's description:
    > Net Internals: Modernize JS tests
    > 
    > Update the net internals JS tests to be more consistent with current
    > WebUI tests:
    > - Move various utility methods to a separate cr.define() namespace,
    >   instead of defining everything on the NetInternalsTest class.
    > - Update all tests to use mocha
    > - Update TaskQueue to indicate completion using a promise returned from
    >   the run() method.
    > 
    > This is in preparation for updating net internals to use JS modules.
    > 
    > Change-Id: Idace809a542a98d89bdf2d6b9363f17d1a87d9d4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650751
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Reviewed-by: dpapad <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#847841}
    
    
    Change-Id: If5e16e5755c46dfbd5766a593352dbc80b0e4f59
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2654612
    Cr-Commit-Position: refs/heads/master@{#847997}

--- chrome/browser/ui/webui/net_internals/net_internals_ui_browsertest.h
+++ chrome/browser/ui/webui/net_internals/net_internals_ui_browsertest.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/web_ui_browser_test.h"
 
 class NetInternalsTest : public WebUIBrowserTest {
- public:
+ protected:
   NetInternalsTest();
   ~NetInternalsTest() override;
 
