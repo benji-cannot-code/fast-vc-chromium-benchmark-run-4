a6ce27380e2a1e25aa0fcf8d4b3d029e255f032c
Maks Orlovich <morlovich@chromium.org>
2021-01-28 20:07:30 +0000

    Devtools UA-CH emulation: permit omitting brands + version to use
    ... and just let the target fill in with default. That should make it easier for devtools frontend to get proper version for full-version to be emulation when UA string is frozen; doing likewise for brands will permit removing the code to from the frontend that tries to match what Chrome does, including the GREASEing.
    
    Requires as prereq
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2653385
    to avoid trouble.
    
    Bug: 1163935
    Change-Id: I1645dd1247d45af0ec31039935a18af40f26f36a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653686
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848223}

--- third_party/blink/web_tests/http/tests/inspector-protocol/emulation/emulation-user-agent-metadata-override.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/emulation/emulation-user-agent-metadata-override.js
@@ -42,9 +42,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   testRunner.log('navigator.userAgent == ' + await session.evaluate('navigator.userAgent'));
   testRunner.log('brands == ' + await session.evaluate('JSON.stringify(navigator.userAgentData.brands)'));
   testRunner.log('is mobile?' + await session.evaluate('navigator.userAgentData.mobile'));
-  testRunner.log(JSON.stringify(await session.evaluateAsync(
+  testRunner.log(await session.evaluateAsync(
       'navigator.userAgentData.getHighEntropyValues(' +
-          '["platform", "platformVersion", "architecture", "model", "uaFullVersion"])')));
+          '["platform", "platformVersion", "architecture", "model", "uaFullVersion"])'));
   await printHeader('sec-ch-ua');
   await printHeader('sec-ch-ua-full-version');
   await printHeader('sec-ch-ua-arch');
@@ -65,6 +65,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   printHeaderFromList('sec-ch-ua-platform-version', navHeaders);
   printHeaderFromList('sec-ch-ua-mobile', navHeaders);
   printHeaderFromList('sec-ch-ua-model', navHeaders);
+
+  // Tests to make sure that not passing in brand and fullVersion uses defaults
+  testRunner.log('');
+  testRunner.log('Testing defaulting of brand and fullVersion');
+
+  await dp.Emulation.setUserAgentOverride({
+    userAgent: 'Electric Typewriter',
+    userAgentMetadata: {
+      platform: 'Electric Typewriter',
+      platformVersion: '1970',
+      architecture: 'Electronic',
+      model: 'With erase tape',
+      mobile: true
+    }
+  });
+  testRunner.log('navigator.userAgent == ' + await session.evaluate('navigator.userAgent'));
+  testRunner.log('brands == ' + await session.evaluate('JSON.stringify(navigator.userAgentData.brands)'));
+  testRunner.log('is mobile?' + await session.evaluate('navigator.userAgentData.mobile'));
+  testRunner.log(await session.evaluateAsync(
+      'navigator.userAgentData.getHighEntropyValues(' +
+          '["platform", "platformVersion", "architecture", "model", "uaFullVersion"])'));
+  await printHeader('sec-ch-ua');
+  await printHeader('sec-ch-ua-full-version');
+  await printHeader('sec-ch-ua-arch');
+  await printHeader('sec-ch-ua-platform');
+  await printHeader('sec-ch-ua-platform-version');
+  await printHeader('sec-ch-ua-mobile');
+  await printHeader('sec-ch-ua-model');
 
   function printHeaderFromList(name, headers) {
     let logged = false;
