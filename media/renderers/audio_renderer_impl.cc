4ddb35eb7bbee3f4af6f303ead1cfe166ca75703
evliu <evliu@google.com>
2021-01-28 01:44:26 +0000

    Enable live caption for muted audio streams initiated by autoplay if they were ever unmuted
    User feedback from the Live caption Dogfood indicated that users would like muted autoplayed streams captioned even if they were initiated by autoplay. To prevent captioning of muted autoplaying videos in the background, we will caption only muted autoplayed streams that have previously been unmumted.
    
    Bug: 1171018
    Change-Id: I91e5f740358b9be2af1fcacd584a08af3bd78911
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2651454
    Commit-Queue: Evan Liu <evliu@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847914}

--- media/renderers/audio_renderer_impl.cc
+++ media/renderers/audio_renderer_impl.cc
@@ -728,6 +728,7 @@ void AudioRendererImpl::OnWaiting(WaitingReason reason) {
 
 void AudioRendererImpl::SetVolume(float volume) {
   DCHECK(task_runner_->BelongsToCurrentThread());
+    was_unmuted_ = was_unmuted_ || volume != 0;
   if (state_ == kUninitialized || state_ == kInitializing) {
     volume_ = volume;
     return;
@@ -966,7 +967,9 @@ bool AudioRendererImpl::HandleDecodedBuffer_Locked(
       first_packet_timestamp_ = buffer->timestamp();
 
 #if !defined(OS_ANDROID)
-    if (transcribe_audio_callback_ && !(autoplay_initiated_ && volume_ == 0)) {
+    // Do not transcribe muted streams initiated by autoplay if the stream was
+    // never unmuted.
+    if (transcribe_audio_callback_ && !(autoplay_initiated_ && !was_unmuted_)) {
       transcribe_audio_callback_.Run(buffer);
     }
 #endif
