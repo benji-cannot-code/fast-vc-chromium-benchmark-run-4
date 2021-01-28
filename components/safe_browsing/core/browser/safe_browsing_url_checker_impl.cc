e69bc5ccfbd1c671bbedc334b0840e87a2c289e1
Julie Jeongeun Kim <jkim@igalia.com>
2021-01-28 09:50:11 +0000

    Introduce new UMAs with RequestDestination for safe_browsing
    This CL introduces new UMAs with RequestDestination for
    safe_browsing as a follow-up CL of [1]. It adds new UMAs to
    the code where ResourceType is used for safe browsing UMA.
    
    Once RequestDestination histograms are stabilized, the old
    histograms with safe_browsing::ResourceType will be
    deprecated and safe_browsing::ResourceType will be cleaned up.
    
    [1] https://crrev.com/c/2589575
    
    Bug: 1059639
    Change-Id: If79733a5967dadcd510fa690aee534d70fcc689c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2640011
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848041}

--- components/safe_browsing/core/browser/safe_browsing_url_checker_impl.cc
+++ components/safe_browsing/core/browser/safe_browsing_url_checker_impl.cc
@@ -295,6 +295,8 @@ void SafeBrowsingUrlCheckerImpl::OnUrlResult(const GURL& url,
       "SB2.ResourceTypes2.Unsafe",
       safe_browsing::GetResourceTypeFromRequestDestination(
           request_destination_));
+  UMA_HISTOGRAM_ENUMERATION("SB2.RequestDestination.Unsafe",
+                            request_destination_);
 
   security_interstitials::UnsafeResource resource =
       MakeUnsafeResource(url, threat_type, metadata, is_from_real_time_check);
@@ -367,6 +369,8 @@ void SafeBrowsingUrlCheckerImpl::ProcessUrls() {
           "SB2.ResourceTypes2.Skipped",
           safe_browsing::GetResourceTypeFromRequestDestination(
               request_destination_));
+      UMA_HISTOGRAM_ENUMERATION("SB2.RequestDestination.Skipped",
+                                request_destination_);
 
       if (!RunNextCallback(true, false))
         return;
@@ -380,6 +384,8 @@ void SafeBrowsingUrlCheckerImpl::ProcessUrls() {
         "SB2.ResourceTypes2.Checked",
         safe_browsing::GetResourceTypeFromRequestDestination(
             request_destination_));
+    UMA_HISTOGRAM_ENUMERATION("SB2.RequestDestination.Checked",
+                              request_destination_);
 
     SBThreatType threat_type = CheckWebUIUrls(url);
     if (threat_type != safe_browsing::SB_THREAT_TYPE_SAFE) {
@@ -412,6 +418,8 @@ void SafeBrowsingUrlCheckerImpl::ProcessUrls() {
           "SafeBrowsing.RT.ResourceTypes.Checked",
           safe_browsing::GetResourceTypeFromRequestDestination(
               request_destination_));
+      UMA_HISTOGRAM_ENUMERATION("SafeBrowsing.RT.RequestDestinations.Checked",
+                                request_destination_);
       safe_synchronously = false;
       AsyncMatch match =
           can_check_db_
