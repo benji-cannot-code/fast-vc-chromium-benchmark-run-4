a20db7bf09ba11003f542bd182ce09635dc439f5
Ken Rockot <rockot@google.com>
2021-01-28 21:55:44 +0000

    Migrate internal serial WPT to Mojo JS modules
    Bug: 1004256
    Change-Id: I8135f62b8eb7261ac8117eba2d9a51b77c893675
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2657439
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#848275}

--- third_party/blink/web_tests/wpt_internal/serial/serial_getPorts-mojoServiceUnavailable.https.any.js
+++ third_party/blink/web_tests/wpt_internal/serial/serial_getPorts-mojoServiceUnavailable.https.any.js
@@ -2,13 +2,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/resources/test-only-api.js
 
 promise_test(async () => {
-  await loadMojoResources([
-    '/gen/mojo/public/mojom/base/unguessable_token.mojom.js',
-    '/gen/third_party/blink/public/mojom/serial/serial.mojom.js',
-  ]);
+  const {SerialService} = await import(
+      '/gen/third_party/blink/public/mojom/serial/serial.mojom.m.js');
 
   let interceptor =
-      new MojoInterfaceInterceptor(blink.mojom.SerialService.name);
+      new MojoInterfaceInterceptor(SerialService.$interfaceName);
   interceptor.oninterfacerequest = e => e.handle.close();
   interceptor.start();
 
