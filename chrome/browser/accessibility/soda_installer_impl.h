98a7c6cbbd223f30722bfca1f1002989421ec0d0
Amanda Deacon <amandadeacon@chromium.org>
2021-01-28 21:46:16 +0000

    Cache installation path of SODA DLC.
    This change will be part of a collection of WIP changes and is currently hidden behind a feature flag.
    
    The added method GetSodaLibPath is currently for the chromeos-specific implementation only, and we intend it to be called by Chrome OS.
    
    TEST=manually pre-loaded SODA DLC to DUT.
    TEST=hard-coding to trigger modified code blocks.
    TEST=viewed printed output of cached path e.g.:
    TEST=    /run/imageloader/libsoda/package/root
    
    Bug: 1160272
    AX-Relnotes: n/a.
    Change-Id: Ic65e7031acdb3135a84529a5a7c3f1d3b5f119aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643741
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Auto-Submit: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848270}

--- chrome/browser/accessibility/soda_installer_impl.h
+++ chrome/browser/accessibility/soda_installer_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
+#include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observer.h"
 #include "chrome/browser/accessibility/soda_installer.h"
@@ -31,6 +32,9 @@ class SodaInstallerImpl : public SodaInstaller,
   ~SodaInstallerImpl() override;
   SodaInstallerImpl(const SodaInstallerImpl&) = delete;
   SodaInstallerImpl& operator=(const SodaInstallerImpl&) = delete;
+
+  // Currently only implemented in the chromeos-specific subclass.
+  base::FilePath GetSodaLibPath() const override;
 
   // SodaInstaller:
   void InstallSoda(PrefService* prefs) override;
