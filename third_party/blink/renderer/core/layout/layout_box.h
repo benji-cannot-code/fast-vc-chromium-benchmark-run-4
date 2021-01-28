3fbb488be6f125224b61aeba4536c608ed484beb
Felipe Erias <felipeerias@igalia.com>
2021-01-28 05:59:20 +0000

    Do not clip gutters created by scrollbar-gutter
    The gutters created by the scrollbar-gutter property don't occlude
    the content underneath them. Therefore, to prevent missing content
    and ignored events, those gutters should not be clipped out by
    LayoutBox::OverflowClipRect.
    
    In order to do this, ExcludeScrollbars and ComputeScrollbarsInternal
    are optionally able to exclude those gutters from their results.
    
    Web and unit tests are included.
    
    Bug: 710214
    Change-Id: Ib5d70d4783c84eb8c78961f758795cc984f13c9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643687
    Commit-Queue: Felipe Erias Morandeira <felipeerias@gmail.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#847991}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -69,6 +69,11 @@ enum ShouldComputePreferred { kComputeActual, kComputePreferred };
 
 enum ShouldClampToContentBox { kDoNotClampToContentBox, kClampToContentBox };
 
+enum ShouldIncludeScrollbarGutter {
+  kExcludeScrollbarGutter,
+  kIncludeScrollbarGutter
+};
+
 using SnapAreaSet = HashSet<LayoutBox*>;
 
 struct LayoutBoxRareData final : public GarbageCollected<LayoutBoxRareData> {
@@ -2116,7 +2121,8 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
 
   void ExcludeScrollbars(
       PhysicalRect&,
-      OverlayScrollbarClipBehavior = kIgnoreOverlayScrollbarSize) const;
+      OverlayScrollbarClipBehavior = kIgnoreOverlayScrollbarSize,
+      ShouldIncludeScrollbarGutter = kIncludeScrollbarGutter) const;
 
   LayoutUnit ContainingBlockLogicalWidthForPositioned(
       const LayoutBoxModelObject* containing_block,
@@ -2272,7 +2278,8 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
   bool HasScrollbarGutters(ScrollbarOrientation orientation) const;
   NGPhysicalBoxStrut ComputeScrollbarsInternal(
       ShouldClampToContentBox = kDoNotClampToContentBox,
-      OverlayScrollbarClipBehavior = kIgnoreOverlayScrollbarSize) const;
+      OverlayScrollbarClipBehavior = kIgnoreOverlayScrollbarSize,
+      ShouldIncludeScrollbarGutter = kIncludeScrollbarGutter) const;
 
   LayoutUnit FlipForWritingModeInternal(
       LayoutUnit position,
