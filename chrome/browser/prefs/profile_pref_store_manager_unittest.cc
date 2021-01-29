fca88226f5077ed0e23aeb756fec5e0f577b88f7
Alan Screen <awscreen@chromium.org>
2021-01-29 20:52:18 +0000

    prefs: Migrate callbacks from base::Closure
    The stop closure in PrefStoreReadObserver comes from the QuitClosure()
    of base::RunLoop which is only ever invoked once, to exit the loop [1].
    
    This is part of the base::Callback migration.
    
    Context: https://source.chromium.org/chromium/chromium/src/+/master:docs/callback.md;drc=ddc84d470a7d89255b7af81584a09d0cfb3e3cf2;l=40-44
    
    Fixed: 1170964
    Change-Id: I39fdea8d639f776874f34e8b64f3e076fb59a397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2654049
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848697}

--- chrome/browser/prefs/profile_pref_store_manager_unittest.cc
+++ chrome/browser/prefs/profile_pref_store_manager_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
@@ -100,14 +101,14 @@ class PrefStoreReadObserver : public PrefStore::Observer {
   void OnPrefValueChanged(const std::string& key) override {}
 
   void OnInitializationCompleted(bool succeeded) override {
-    if (!stop_waiting_.is_null()) {
+    if (stop_waiting_) {
       std::move(stop_waiting_).Run();
     }
   }
 
  private:
   scoped_refptr<PersistentPrefStore> pref_store_;
-  base::Closure stop_waiting_;
+  base::OnceClosure stop_waiting_;
 
   DISALLOW_COPY_AND_ASSIGN(PrefStoreReadObserver);
 };
