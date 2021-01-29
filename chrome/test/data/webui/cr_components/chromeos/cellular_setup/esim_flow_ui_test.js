1d134be9754eddd0c436f4f9a4c41ae677f37de2
Gordon Seto <gordonseto@google.com>
2021-01-29 21:14:14 +0000

    [CrOS Settings] Enable Done button at end of cellular setup flow.
    Update eSIM and pSIM flows to handle navigateForward() when the forward
    button is a "Done" button.
    
    Bug: 1169816
    Change-Id: I24757b79f346c706ca650d0f375fd14308d5932c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2657029
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848711}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/esim_flow_ui_test.js
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/esim_flow_ui_test.js
@@ -73,6 +73,22 @@ suite('CrComponentsEsimFlowUiTest', function() {
     return confirmationCodeInput;
   }
 
+  async function assertFinalPageAndPressDoneButton(shouldBeShowingError) {
+    assertSelectedPage(cellular_setup.ESimPageName.FINAL, finalPage);
+    assertEquals(!!finalPage.$$('.error'), shouldBeShowingError);
+    assertEquals(
+        cellularSetup.ButtonState.ENABLED, eSimPage.buttonState.forward);
+    assertEquals(eSimPage.forwardButtonLabel, 'Done');
+    let exitCellularSetupEventFired = false;
+    eSimPage.addEventListener('exit-cellular-setup', () => {
+      exitCellularSetupEventFired = true;
+    });
+    eSimPage.navigateForward();
+
+    await flushAsync();
+    assertTrue(exitCellularSetupEventFired);
+  }
+
   suite('No eSIM profiles flow', function() {
     let euicc;
 
@@ -121,7 +137,7 @@ suite('CrComponentsEsimFlowUiTest', function() {
       await flushAsync();
 
       // Should go to final page.
-      assertSelectedPage(cellular_setup.ESimPageName.FINAL, finalPage);
+      await assertFinalPageAndPressDoneButton(false);
     });
 
     test('Valid confirmation code', async function() {
@@ -144,7 +160,7 @@ suite('CrComponentsEsimFlowUiTest', function() {
       await flushAsync();
 
       // Should go to final page.
-      assertSelectedPage(cellular_setup.ESimPageName.FINAL, finalPage);
+      await assertFinalPageAndPressDoneButton(false);
     });
 
     test('Invalid confirmation code', async function() {
@@ -221,8 +237,7 @@ suite('CrComponentsEsimFlowUiTest', function() {
       await flushAsync();
 
       // Should go directly to final page.
-      assertSelectedPage(cellular_setup.ESimPageName.FINAL, finalPage);
-      assertFalse(!!finalPage.$$('.error'));
+      await assertFinalPageAndPressDoneButton(false);
     });
 
     test('Unsuccessful install', async function() {
@@ -236,8 +251,7 @@ suite('CrComponentsEsimFlowUiTest', function() {
       await flushAsync();
 
       // Should go directly to final page.
-      assertSelectedPage(cellular_setup.ESimPageName.FINAL, finalPage);
-      assertTrue(!!finalPage.$$('.error'));
+      await assertFinalPageAndPressDoneButton(true);
     });
 
     test('Valid confirmation code', async function() {
@@ -262,7 +276,7 @@ suite('CrComponentsEsimFlowUiTest', function() {
       await flushAsync();
 
       // Should go to final page.
-      assertSelectedPage(cellular_setup.ESimPageName.FINAL, finalPage);
+      await assertFinalPageAndPressDoneButton(false);
     });
 
     test('Invalid confirmation code', async function() {
@@ -360,7 +374,7 @@ suite('CrComponentsEsimFlowUiTest', function() {
       await flushAsync();
 
       // Should now be at the final page.
-      assertSelectedPage(cellular_setup.ESimPageName.FINAL, finalPage);
+      await assertFinalPageAndPressDoneButton(false);
     });
 
     test(
@@ -426,8 +440,7 @@ suite('CrComponentsEsimFlowUiTest', function() {
       await flushAsync();
 
       // Should now be at the final page.
-      assertSelectedPage(cellular_setup.ESimPageName.FINAL, finalPage);
-      assertFalse(!!finalPage.$$('.error'));
+      await assertFinalPageAndPressDoneButton(false);
     });
 
     test('Select profile with valid confirmation code flow', async function() {
@@ -457,7 +470,7 @@ suite('CrComponentsEsimFlowUiTest', function() {
       await flushAsync();
 
       // Should go to final page.
-      assertSelectedPage(cellular_setup.ESimPageName.FINAL, finalPage);
+      await assertFinalPageAndPressDoneButton(false);
     });
 
     test(
