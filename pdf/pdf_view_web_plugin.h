a7347f18083c442b3fc4014d2869e724f32d3636
Daniel Hosseinian <dhoss@chromium.org>
2021-01-29 19:30:25 +0000

    Expose a scriptable object for the PPAPI-free PDF plugin
    Enable the PDF plugin to receive postMessage() calls from the embedding
    content. A new class, PostMessageReceiver, implements the postMessage()
    API using Gin.
    
    As part of its implementation, it will convert all received messages to
    then forward them to a PdfViewWebPlugin client. The class is exposed
    through the PdfViewWebPlugin's Blink API as a scriptable object.
    
    Bug: 1109796
    Change-Id: Ic639237cb3a231f458f0fb314ca998ab4ca134c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2601240
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848651}

--- pdf/pdf_view_web_plugin.h
+++ pdf/pdf_view_web_plugin.h
@@ -9,9 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/weak_ptr.h"
 #include "pdf/pdf_view_plugin_base.h"
+#include "pdf/post_message_receiver.h"
 #include "pdf/ppapi_migration/url_loader.h"
 #include "third_party/blink/public/web/web_plugin.h"
 #include "third_party/blink/public/web/web_plugin_params.h"
+#include "v8/include/v8.h"
+
+namespace base {
+class Value;
+}  // namespace base
 
 namespace blink {
 class WebPluginContainer;
@@ -22,7 +28,8 @@ namespace chrome_pdf {
 // Skeleton for a `blink::WebPlugin` to replace `OutOfProcessInstance`.
 class PdfViewWebPlugin final : public PdfViewPluginBase,
                                public blink::WebPlugin,
-                               public BlinkUrlLoader::Client {
+                               public BlinkUrlLoader::Client,
+                               public PostMessageReceiver::Client {
  public:
   explicit PdfViewWebPlugin(const blink::WebPluginParams& params);
   PdfViewWebPlugin(const PdfViewWebPlugin& other) = delete;
@@ -32,6 +39,7 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
   bool Initialize(blink::WebPluginContainer* container) override;
   void Destroy() override;
   blink::WebPluginContainer* Container() const override;
+  v8::Local<v8::Object> V8ScriptableObject(v8::Isolate* isolate) override;
   void UpdateAllLifecyclePhases(blink::DocumentUpdateReason reason) override;
   void Paint(cc::PaintCanvas* canvas, const gfx::Rect& rect) override;
   void UpdateGeometry(const gfx::Rect& window_rect,
@@ -120,6 +128,9 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
   std::unique_ptr<blink::WebAssociatedURLLoader> CreateAssociatedURLLoader(
       const blink::WebAssociatedURLLoaderOptions& options) override;
 
+  // PostMessageReceiver::Client:
+  void OnMessage(const base::Value& message) override;
+
  protected:
   // PdfViewPluginBase:
   base::WeakPtr<PdfViewPluginBase> GetWeakPtr() override;
@@ -138,6 +149,8 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
 
   blink::WebPluginParams initial_params_;
   blink::WebPluginContainer* container_ = nullptr;
+
+  v8::Persistent<v8::Object> scriptable_receiver_;
 
   base::WeakPtrFactory<PdfViewWebPlugin> weak_factory_{this};
 };
