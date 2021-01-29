2a01e00498010b0641a3cca8b20fa54bab8169e1
Robert Sesek <rsesek@chromium.org>
2021-01-29 17:08:50 +0000

    Move from mojo::DataPipe to mojo::CreateDataPipe in //services
    The DataPipe constructor has no way to check for errors if creation
    fails, so it CHECKs and crashes. CreateDataPipe offers a way to check
    the return value.
    
    Bug: 944990
    Change-Id: I3729099bb412cfa0abbf12d8cb4bccfe327d71dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2657655
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848567}

--- services/network/url_loader.cc
+++ services/network/url_loader.cc
@@ -2058,10 +2058,13 @@ URLLoader::BlockResponseForCorbResult URLLoader::BlockResponseForCorb() {
   url_loader_client_->OnReceiveResponse(response_->Clone());
 
   // Send empty body to the real URLLoaderClient.
-  mojo::DataPipe empty_data_pipe(kBlockedBodyAllocationSize);
-  empty_data_pipe.producer_handle.reset();
-  url_loader_client_->OnStartLoadingResponseBody(
-      std::move(empty_data_pipe.consumer_handle));
+  mojo::ScopedDataPipeProducerHandle producer_handle;
+  mojo::ScopedDataPipeConsumerHandle consumer_handle;
+  CHECK_EQ(mojo::CreateDataPipe(kBlockedBodyAllocationSize, producer_handle,
+                                consumer_handle),
+           MOJO_RESULT_OK);
+  producer_handle.reset();
+  url_loader_client_->OnStartLoadingResponseBody(std::move(consumer_handle));
 
   // Tell the real URLLoaderClient that the response has been completed.
   bool should_report_corb_blocking =
