8f0bf98c9e6843c42c9d70a02bbb407237166753
Scott Violet <sky@chromium.org>
2021-01-29 17:51:06 +0000

    sessions: enables new backend logic
    This makes it so that if there is an error in writing
    the file, a new file is started. Additionally only
    files that have valid contents (marker) present are
    used.
    
    BUG=648266
    TEST=NONE
    
    Change-Id: I1890e04e061529be46e36e79faf62c41dc6c9894
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2644336
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848596}

--- chrome/browser/sessions/session_service_log.h
+++ chrome/browser/sessions/session_service_log.h
@@ -72,6 +72,9 @@ struct ExitData {
 struct WriteErrorData {
   // Number of write errors that occurred.
   int error_count;
+  // Number of write errors that were unrecoverable. See SessionService for
+  // details on this.
+  int unrecoverable_error_count;
 };
 
 union EventData {
@@ -100,7 +103,8 @@ void LogSessionServiceRestoreEvent(Profile* profile,
                                    int window_count,
                                    int tab_count,
                                    bool encountered_error_reading);
-void LogSessionServiceWriteErrorEvent(Profile* profile);
+void LogSessionServiceWriteErrorEvent(Profile* profile,
+                                      bool unrecoverable_write_error);
 void RemoveLastSessionServiceEventOfType(Profile* profile,
                                          SessionServiceEventLogType type);
 
