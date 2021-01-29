dc5e52078417feef195eb857f35548b1ef340acc
Katie Dektar <katie@chromium.org>
2021-01-29 21:54:16 +0000

    Fix autoclick bubble button icons: regressed in M88
    The left click icon was duplicated for several other icons. Fixes bubble
    by using correct icons for drag, pause, and scroll.
    
    Bug: 1172420
    Test: Tried it. Have screenshots at crbug.com/1172420
    Change-Id: I6a5a9fee5893038c3bfdce79041e413f25b1b5c8
    AX-Relnotes: Fixes autoclick menu bubble icons
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2660018
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848733}

--- ash/system/accessibility/autoclick_menu_view.cc
+++ ash/system/accessibility/autoclick_menu_view.cc
@@ -82,7 +82,7 @@ AutoclickMenuView::AutoclickMenuView(AutoclickEventType type,
                     views::Builder<FloatingMenuButton>()
                         .CopyAddressTo(&drag_button_)
                         .SetID(static_cast<int>(ButtonId::kDragAndDrop))
-                        .SetVectorIcon(kAutoclickLeftClickIcon)
+                        .SetVectorIcon(kAutoclickDragIcon)
                         .SetTooltipText(l10n_util::GetStringUTF16(
                             IDS_ASH_AUTOCLICK_OPTION_DRAG_AND_DROP))
                         .SetCallback(base::BindRepeating(
@@ -92,7 +92,7 @@ AutoclickMenuView::AutoclickMenuView(AutoclickEventType type,
                     views::Builder<FloatingMenuButton>()
                         .CopyAddressTo(&scroll_button_)
                         .SetID(static_cast<int>(ButtonId::kScroll))
-                        .SetVectorIcon(kAutoclickLeftClickIcon)
+                        .SetVectorIcon(kAutoclickScrollIcon)
                         .SetTooltipText(l10n_util::GetStringUTF16(
                             IDS_ASH_AUTOCLICK_OPTION_SCROLL))
                         .SetCallback(base::BindRepeating(
@@ -102,7 +102,7 @@ AutoclickMenuView::AutoclickMenuView(AutoclickEventType type,
                     views::Builder<FloatingMenuButton>()
                         .CopyAddressTo(&pause_button_)
                         .SetID(static_cast<int>(ButtonId::kPause))
-                        .SetVectorIcon(kAutoclickLeftClickIcon)
+                        .SetVectorIcon(kAutoclickPauseIcon)
                         .SetTooltipText(l10n_util::GetStringUTF16(
                             IDS_ASH_AUTOCLICK_OPTION_NO_ACTION))
                         .SetCallback(base::BindRepeating(
