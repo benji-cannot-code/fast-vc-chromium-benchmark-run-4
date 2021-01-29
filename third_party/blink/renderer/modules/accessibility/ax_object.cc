14c027557cc4568f4a06ad39124a0b05b523de37
Aaron Leventhal <aleventhal@google.com>
2021-01-29 20:09:01 +0000

    Prevent script from out-of-range access attempt
    E.g. a web test script that uses the accessibilityController.
    In normal content, this wouldn't be posible.
    
    Bug: 1172115
    Change-Id: Ia8de9eb44d463ce8e6bc7dce4b03739b3a0bf4e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2658129
    Reviewed-by: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848666}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -3311,7 +3311,9 @@ int AXObject::ChildCountIncludingIgnored() const {
 
 AXObject* AXObject::ChildAtIncludingIgnored(int index) const {
   DCHECK_GE(index, 0);
-  DCHECK_LT(index, ChildCountIncludingIgnored());
+  DCHECK_LE(index, ChildCountIncludingIgnored());
+  if (index >= ChildCountIncludingIgnored())
+    return nullptr;
   return ChildrenIncludingIgnored()[index];
 }
 
