c8188ac6d233bf41c6e65168c11186e0240642e7
Albert Chaulk <achaulk@chromium.org>
2021-01-29 16:39:54 +0000

    Upstream chromecast/media/gpu. Current users are all internal
    Bug: b/159906175
    Test: built cast_shell
    Change-Id: Iddeb2f1c069492d021a51308bcd29d2636d202c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653569
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Albert Chaulk <achaulk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848554}

---
+++ chromecast/media/gpu/cast_gpu_factory_dummy.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_MEDIA_GPU_CAST_GPU_FACTORY_DUMMY_H_
+#define CHROMECAST_MEDIA_GPU_CAST_GPU_FACTORY_DUMMY_H_
+
+#include "chromecast/media/gpu/cast_gpu_factory.h"
+
+namespace chromecast {
+
+class CastGpuFactoryDummy : public CastGpuFactory {
+ public:
+  CastGpuFactoryDummy();
+  ~CastGpuFactoryDummy() override;
+  CastGpuFactoryDummy(const CastGpuFactoryDummy&) = delete;
+  CastGpuFactoryDummy& operator=(const CastGpuFactoryDummy&) = delete;
+
+ private:
+  // CastGpuFactory implementation:
+  scoped_refptr<viz::ContextProviderCommandBuffer> CreateOpenGLContextProvider()
+      override;
+  std::unique_ptr<::media::VideoDecoder> CreateVideoDecoder() override;
+  std::unique_ptr<::media::VideoEncodeAccelerator> CreateVideoEncoder()
+      override;
+  gpu::GpuMemoryBufferManager* GpuMemoryBufferManager() override;
+};
+
+}  // namespace chromecast
+
+#endif  // CHROMECAST_MEDIA_GPU_CAST_GPU_FACTORY_DUMMY_H_
