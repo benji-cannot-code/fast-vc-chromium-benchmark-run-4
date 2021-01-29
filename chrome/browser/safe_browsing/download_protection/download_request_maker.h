3ea3c2aa592fbea4585da80d4db41554fbebd1ab
Daniel Rubery <drubery@chromium.org>
2021-01-29 19:43:06 +0000

    Add unit tests for DownloadRequestMaker
    This CL reduces the code duplication between the DownloadItem
    constructor and FileSystemWriteAccess constructor for a
    DownloadRequestMaker, and adds unit tests validating several
    fields are correctly populated.
    
    Change-Id: I5aafe937cd01607080db2149924959bdfc920893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2651086
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848657}

--- chrome/browser/safe_browsing/download_protection/download_request_maker.h
+++ chrome/browser/safe_browsing/download_protection/download_request_maker.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "chrome/browser/safe_browsing/download_protection/file_analyzer.h"
+#include "chrome/browser/safe_browsing/safe_browsing_navigation_observer_manager.h"
 #include "components/download/public/common/download_item.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/safe_browsing/core/proto/csd.pb.h"
@@ -32,13 +33,27 @@ class DownloadRequestMaker {
     GURL referrer;
   };
 
-  DownloadRequestMaker(
+  static std::unique_ptr<DownloadRequestMaker> CreateFromDownloadItem(
       scoped_refptr<BinaryFeatureExtractor> binary_feature_extractor,
       download::DownloadItem* item);
-  DownloadRequestMaker(
+
+  static std::unique_ptr<DownloadRequestMaker> CreateFromFileSystemAccess(
       scoped_refptr<BinaryFeatureExtractor> binary_feature_extractor,
       DownloadProtectionService* service,
       const content::FileSystemAccessWriteItem& item);
+
+  DownloadRequestMaker(
+      scoped_refptr<BinaryFeatureExtractor> binary_feature_extractor,
+      content::BrowserContext* browser_context,
+      TabUrls tab_urls,
+      base::FilePath target_file_path,
+      base::FilePath full_path,
+      GURL source_url,
+      std::string sha256_hash,
+      int64_t length,
+      const std::vector<ClientDownloadRequest::Resource>& resources,
+      bool is_user_initiated,
+      ReferrerChainData* referrer_chain_data);
   ~DownloadRequestMaker();
 
   // Starts filling in fields in the download ping. Will run the callback with
