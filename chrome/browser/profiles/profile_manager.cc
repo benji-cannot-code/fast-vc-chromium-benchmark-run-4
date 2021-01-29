7439f639a90b99e52dead5825cbbb75a65f1c1d9
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2021-01-29 20:40:42 +0000

    Add logging to ProfileManager::Add/RemoveKeepAlive()
    This should help with debugging. By printing the Profile name, the
    origin of the refcount, and the new refcount after the operation, we
    can keep track of ScopedProfileKeepAlive state more easily.
    
    Bug: 88586
    Change-Id: I2f677996678059cb82bf06b30437db5cc5e20387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2658058
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848686}

--- chrome/browser/profiles/profile_manager.cc
+++ chrome/browser/profiles/profile_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <map>
+#include <numeric>
 #include <set>
 #include <string>
 #include <utility>
@@ -381,6 +382,12 @@ void RecordGuestProfileLifetime(const std::string& histogram_name,
                                  100);
 }
 #endif  // !defined(OS_ANDROID)
+
+int GetTotalRefCount(const std::map<ProfileKeepAliveOrigin, int>& keep_alives) {
+  return std::accumulate(
+      keep_alives.begin(), keep_alives.end(), 0,
+      [](int acc, const auto& pair) { return acc + pair.second; });
+}
 
 }  // namespace
 
@@ -1321,7 +1328,15 @@ void ProfileManager::AddKeepAlive(const Profile* profile,
   DCHECK(!profile->IsOffTheRecord());
 
   ProfileInfo* info = GetProfileInfoByPath(profile->GetPath());
-  DCHECK(info);
+  if (!info) {
+    // Can be null in unit tests, when the Profile was not created via
+    // ProfileManager.
+    return;
+  }
+
+  DCHECK_NE(0, GetTotalRefCount(info->keep_alives))
+      << "AddKeepAlive() on a soon-to-be-deleted Profile is not allowed";
+
   info->keep_alives[origin]++;
 
   int& waiting_for_first_browser_window =
@@ -1330,6 +1345,9 @@ void ProfileManager::AddKeepAlive(const Profile* profile,
       waiting_for_first_browser_window != 0) {
     waiting_for_first_browser_window = 0;
   }
+
+  VLOG(1) << "AddKeepAlive(" << profile->GetDebugName() << ", " << origin
+          << "). refcount=" << GetTotalRefCount(info->keep_alives);
 }
 
 void ProfileManager::RemoveKeepAlive(const Profile* profile,
@@ -1342,16 +1360,25 @@ void ProfileManager::RemoveKeepAlive(const Profile* profile,
   DCHECK(!profile->IsOffTheRecord());
 
   ProfileInfo* info = GetProfileInfoByPath(profile->GetPath());
-  DCHECK(info);
+  if (!info) {
+    // Can be null in unit tests, when the Profile was not created via
+    // ProfileManager.
+    return;
+  }
+
   DCHECK(base::Contains(info->keep_alives, origin));
   info->keep_alives[origin]--;
   DCHECK_LE(0, info->keep_alives[origin]);
 
+  int total_refcount = GetTotalRefCount(info->keep_alives);
+  VLOG(1) << "RemoveKeepAlive(" << profile->GetDebugName() << ", " << origin
+          << "). refcount=" << total_refcount;
+
 #if !defined(OS_ANDROID) && !BUILDFLAG(IS_CHROMEOS_ASH)
-  bool is_zero_refcount = base::ranges::all_of(
-      info->keep_alives, [](const auto& entry) { return entry.second == 0; });
-  if (is_zero_refcount)
+  if (total_refcount == 0) {
+    VLOG(1) << "Deleting profile " << profile->GetDebugName();
     RemoveProfile(profile->GetPath());
+  }
 #endif  // !defined(OS_ANDROID) && !BUILDFLAG(IS_CHROMEOS_ASH)
 }
 
