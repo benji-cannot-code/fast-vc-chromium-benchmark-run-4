acbca47b0ed566295f9dcde0fc98d85bda2d5ddf
cfredric <cfredric@chromium.org>
2021-01-29 21:46:22 +0000

    Copy Url proto->string conversion into library to promote sharing.
    This enables other fuzzers to use the URL-to-string conversion code.
    
    Change-Id: If25a4cf317773171be159cbfd6927e6ce263eb09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653666
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848726}

---
+++ testing/libfuzzer/proto/url_proto_converter.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TESTING_LIBFUZZER_PROTO_URL_PROTO_CONVERTER_H_
+#define TESTING_LIBFUZZER_PROTO_URL_PROTO_CONVERTER_H_
+
+#include "testing/libfuzzer/proto/url.pb.h"
+
+namespace url_proto {
+
+// Converts a URL in Protocol Buffer format to a url in string format.
+// Since protobuf is a relatively simple format, fuzzing targets that do not
+// accept protobufs (such as this one) will require code to convert from
+// protobuf to the accepted format (string in this case).
+std::string Convert(const url_proto::Url& url);
+
+}  // namespace url_proto
+
+#endif  // TESTING_LIBFUZZER_PROTO_URL_PROTO_CONVERTER_H_
