40cb56839310a541aaef5d95971f232b7c96fae6
Gordon Seto <gordonseto@google.com>
2021-01-29 22:56:17 +0000

    [CrOS Settings] Add showing selection flow from SystemTrayClient.
    Update SystemTrayClient::ShowSettingsCellularSetupPsimFlow to
    SystemTrayClient::ShowSettingsCellularSetup(bool showPsimFlow) so we can
    navigate to the cellular setup's setup_selection_flow in some cases and
    psim_flow_ui in others.
    
    Add navigating to cellular setup selection flow from cellular setup
    notification.
    
    Bug: 1093185
    Change-Id: Iadb400fb1344416d6f80c2905e59e1573f02d3d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2657396
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/master@{#848766}

--- ash/system/network/cellular_setup_notifier.cc
+++ ash/system/network/cellular_setup_notifier.cc
@@ -8,10 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/ash_pref_names.h"
 #include "ash/public/cpp/network_config_service.h"
 #include "ash/public/cpp/notification_utils.h"
+#include "ash/public/cpp/system_tray_client.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
+#include "ash/system/model/system_tray_model.h"
 #include "base/timer/timer.h"
+#include "components/onc/onc_constants.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -39,7 +42,8 @@ bool DoesCellularDeviceExist(
 }
 
 void OnCellularSetupNotificationClicked() {
-  // TODO(crbug.com/1093185) Handle the notification being clicked.
+  Shell::Get()->system_tray_model()->client()->ShowNetworkCreate(
+      ::onc::network_type::kCellular);
 }
 
 // Returns the value of kCanCellularSetupNotificationBeShown for the last active
--- chrome/browser/ui/ash/system_tray_client.cc
+++ chrome/browser/ui/ash/system_tray_client.cc
@@ -384,6 +384,10 @@ void SystemTrayClient::ShowNetworkConfigure(const std::string& network_id) {
 
 void SystemTrayClient::ShowNetworkCreate(const std::string& type) {
   if (type == ::onc::network_type::kCellular) {
+    if (chromeos::features::IsCellularActivationUiEnabled()) {
+      ShowSettingsCellularSetup(/*show_psim_flow=*/false);
+      return;
+    }
     const chromeos::NetworkState* cellular =
         chromeos::NetworkHandler::Get()
             ->network_state_handler()
@@ -396,11 +400,12 @@ void SystemTrayClient::ShowNetworkCreate(const std::string& type) {
   chromeos::InternetConfigDialog::ShowDialogForNetworkType(type);
 }
 
-void SystemTrayClient::ShowSettingsCellularSetupPsimFlow() {
+void SystemTrayClient::ShowSettingsCellularSetup(bool show_psim_flow) {
   // TODO(crbug.com/1093185) Add metrics action recorder
   std::string page = chromeos::settings::mojom::kCellularNetworksSubpagePath;
   page += "&showCellularSetup=true";
-  page += "&showPsimFlow=true";
+  if (show_psim_flow)
+    page += "&showPsimFlow=true";
   ShowSettingsSubPageForActiveUser(page);
 }
 
@@ -459,7 +464,7 @@ void SystemTrayClient::ShowNetworkSettingsHelper(const std::string& network_id,
     // should open cellular setup dialogs' psim flow if the device has
     // |kUpdatedCellularActivationUi| feature enabled and is a non-activated
     // cellular network
-    ShowSettingsCellularSetupPsimFlow();
+    ShowSettingsCellularSetup(/*show_psim_flow=*/true);
     return;
   }
 
