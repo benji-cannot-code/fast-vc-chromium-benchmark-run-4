3f0d08236d0e3e2ba9df7d4872a7aeb9faf249db
Dominic Battre <battre@chromium.org>
2021-01-29 16:04:58 +0000

    Report KeyChain problems to stderr
    KeyChains problems were only reported for debug builds. As these
    problems happen in the wild, it would be useful to have them always
    available.
    
    Bug: 1171665
    Change-Id: I5635e505f953967868126e5ca5e462f3980722cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2658646
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Auto-Submit: Dominic Battré <battre@chromium.org>
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848532}

--- components/os_crypt/keychain_password_mac.mm
+++ components/os_crypt/keychain_password_mac.mm
@@ -81,6 +81,6 @@ std::string KeychainPassword::GetPassword() const {
     return password;
   }
 
-  OSSTATUS_DLOG(ERROR, error) << "Keychain lookup failed";
+  OSSTATUS_LOG(ERROR, error) << "Keychain lookup failed";
   return std::string();
 }
