1be6850939be1f39615351bb2a550717e86bc006
Richard Knoll <knollr@chromium.org>
2021-01-29 13:40:23 +0000

    Revert "Remove old notifications from database"
    This reverts commit ce5d914fec4b4167540e8b580d74da23ea157bbb.
    
    Reason for revert: Causes lots of TSAN failures due to invalid
    usage of base::test::ScopedFeatureList.
    
    Bug: 1172093
    
    Original change's description:
    > Remove old notifications from database
    >
    > Platforms that do not support notification synchronization (mainly
    > Android) tend to have rather lots of old notifications stored in the
    > database. Ideally we would attempt to properly synchronize on all
    > platforms but that's not always possible. Furthermore some platforms
    > report notifications as visible if the user has actually disabled
    > notification permissions for Chrome which causes the same growth of very
    > old notifications on the device.
    >
    > This will now clean up any notification that we think has been shown for
    > more than 7 days every time we synchronize notifications. This happens
    > on startup on a ThreadPool task runner.
    >
    > Bug: 1170884
    > Change-Id: Icf7b2579d6e174731d55f3961dd13deb17ac1505
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2649983
    > Commit-Queue: Richard Knoll <knollr@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Reviewed-by: Peter Beverloo <peter@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#848198}
    
    TBR=peter@chromium.org,rkaplow@chromium.org,knollr@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I12d57dccbfcbbd724931cb3c456663f055a2ff95
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1170884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2659033
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848495}

--- content/browser/notifications/platform_notification_service_proxy.h
+++ content/browser/notifications/platform_notification_service_proxy.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_NOTIFICATIONS_PLATFORM_NOTIFICATION_SERVICE_PROXY_H_
 
 #include <memory>
-#include <set>
 #include <string>
 
 #include "base/callback_forward.h"
@@ -54,8 +53,8 @@ class CONTENT_EXPORT PlatformNotificationServiceProxy {
   void DisplayNotification(const NotificationDatabaseData& data,
                            DisplayResultCallback callback);
 
-  // Closes the notifications with |notification_ids|.
-  void CloseNotifications(const std::set<std::string>& notification_ids);
+  // Closes the notification with |notification_id|.
+  void CloseNotification(const std::string& notification_id);
 
   // Schedules a notification trigger for |timestamp|.
   void ScheduleTrigger(base::Time timestamp);
@@ -84,9 +83,9 @@ class CONTENT_EXPORT PlatformNotificationServiceProxy {
                              const GURL& service_worker_scope,
                              DisplayResultCallback callback);
 
-  // Actually closes the notifications with |notification_ids|. Must be called
-  // on the UI thread.
-  void DoCloseNotifications(const std::set<std::string>& notification_ids);
+  // Actually closes the notification with |notification_id|. Must be called on
+  // the UI thread.
+  void DoCloseNotification(const std::string& notification_id);
 
   // Actually calls |notification_service_| to schedule a trigger. Must be
   // called on the UI thread.
