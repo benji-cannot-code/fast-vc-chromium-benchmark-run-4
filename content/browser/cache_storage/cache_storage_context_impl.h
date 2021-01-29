8e807f0940b00f606d889d10266a5819f9c4a0bc
Adrienne Walker <enne@chromium.org>
2021-01-29 00:04:28 +0000

    cachestorage: Move CacheStorageQuotaClient off io thread
    As part of moving cache storage to the storage service, the use of
    browser threads will need to be cleaned up.  CacheStorageQuotaClient
    accesses CacheStorageManager so should live on the same task runner.
    
    This is (nearly) the last cross sequence CacheManager() call, so
    CrossSequenceCacheStorageManager can be deleted as a followup.
    
    Bug: 1147642
    Change-Id: I335495e766b6ac611ca5018ad91e64a0566692d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653251
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848331}

--- content/browser/cache_storage/cache_storage_context_impl.h
+++ content/browser/cache_storage/cache_storage_context_impl.h
@@ -118,9 +118,6 @@ class CONTENT_EXPORT CacheStorageContextImpl
 
   void ShutdownOnTaskRunner();
 
-  void CreateQuotaClientsOnIOThread(
-      scoped_refptr<storage::QuotaManagerProxy> quota_manager_proxy);
-
   // Initialized at construction.
   const scoped_refptr<base::SequencedTaskRunner> task_runner_;
 
