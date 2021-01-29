a442c98937cd9908d7eabe52e8e03edcd9448e63
Fredrik Söderqvist <fs@opera.com>
2021-01-29 16:33:55 +0000

    Remove some unused includes from clip_path_clipper.cc
    Change-Id: Id1d6604d73aea401c0f3bf22c319c3b73ef6ae17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2659337
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848549}

--- third_party/blink/renderer/core/paint/clip_path_clipper.cc
+++ third_party/blink/renderer/core/paint/clip_path_clipper.cc
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/paint/clip_path_clipper.h"
 
-#include "third_party/blink/renderer/core/dom/element_traversal.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/layout/layout_inline.h"
 #include "third_party/blink/renderer/core/layout/svg/layout_svg_resource_clipper.h"
-#include "third_party/blink/renderer/core/layout/svg/svg_layout_support.h"
 #include "third_party/blink/renderer/core/layout/svg/svg_resources.h"
 #include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
