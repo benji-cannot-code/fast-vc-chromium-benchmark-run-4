28d456dd8808464f20b219368b3e28153480a5fb
Scott Violet <sky@chromium.org>
2021-01-29 17:25:41 +0000

    weblayer: add minimum weblayer version annotation for testBogusCrashId
    The fix landed in 88, so the minimum weblayer version needs to be
    set.
    
    BUG=1171907
    TEST=test only change
    
    Change-Id: Ic7eae9746d963ba60ece7664ba20e3e3ae139c2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2658608
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Auto-Submit: Scott Violet <sky@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848581}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/CrashReporterTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/CrashReporterTest.java
@@ -121,6 +121,7 @@ public class CrashReporterTest {
         Assert.assertFalse(mCrashSidecar.exists());
     }
 
+    @MinWebLayerVersion(88) // Fix first appeared in 88.
     @Test
     @SmallTest
     public void testBogusCrashId() throws Exception {
