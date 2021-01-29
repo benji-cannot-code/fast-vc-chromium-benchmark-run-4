cc5713d54c03d16c8db9ad2d83b031642c526091
Dave Tapuska <dtapuska@chromium.org>
2021-01-29 20:09:18 +0000

    Move PrintPage handling into frame from WebViewTestProxy.
    Let the RenderViewImpl code call the observers. Move the PrintPage
    code into the ScriptedPrint observer callback.
    
    This is the last override on WebViewTestProxy which makes way for the
    class removal.
    
    BUG=1155202
    
    Change-Id: I9f81b95c5f041e5526b77c0a431cb91cc80d9c53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2657558
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848667}

--- content/web_test/renderer/web_frame_test_proxy.cc
+++ content/web_test/renderer/web_frame_test_proxy.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_frame_widget.h"
 #include "third_party/blink/public/web/web_local_frame.h"
 #include "third_party/blink/public/web/web_plugin_params.h"
+#include "third_party/blink/public/web/web_print_params.h"
 #include "third_party/blink/public/web/web_testing_support.h"
 #include "third_party/blink/public/web/web_view.h"
 
@@ -212,6 +213,18 @@ class TestRenderFrameObserver : public RenderFrameObserver {
       test_runner()->PrintMessage(description +
                                   " - didHandleOnloadEventsForFrame\n");
     }
+  }
+
+  void ScriptedPrint(bool user_initiated) override {
+    // This is using the main frame for the size, but maybe it should be using
+    // the frame's size.
+    blink::WebSize page_size_in_pixels =
+        frame_proxy()->GetLocalRootWebFrameWidget()->Size();
+    if (page_size_in_pixels.IsEmpty())
+      return;
+    blink::WebPrintParams print_params(page_size_in_pixels);
+    render_frame()->GetWebFrame()->PrintBegin(print_params, blink::WebNode());
+    render_frame()->GetWebFrame()->PrintEnd();
   }
 
   WebViewTestProxy* web_view_test_proxy_;
--- content/web_test/renderer/web_view_test_proxy.cc
+++ content/web_test/renderer/web_view_test_proxy.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_url_request.h"
 #include "third_party/blink/public/web/web_frame.h"
 #include "third_party/blink/public/web/web_local_frame.h"
-#include "third_party/blink/public/web/web_print_params.h"
 #include "third_party/blink/public/web/web_settings.h"
 #include "third_party/blink/public/web/web_view.h"
 
@@ -34,18 +33,6 @@ WebViewTestProxy::WebViewTestProxy(AgentSchedulingGroup& agent_scheduling_group,
 
 WebViewTestProxy::~WebViewTestProxy() {
   test_runner_->RemoveRenderView(this);
-}
-
-void WebViewTestProxy::PrintPage(blink::WebLocalFrame* frame) {
-  // This is using the main frame for the size, but maybe it should be using the
-  // frame's size.
-  blink::WebSize page_size_in_pixels =
-      GetMainRenderFrame()->GetLocalRootWebFrameWidget()->Size();
-  if (page_size_in_pixels.IsEmpty())
-    return;
-  blink::WebPrintParams print_params(page_size_in_pixels);
-  frame->PrintBegin(print_params, blink::WebNode());
-  frame->PrintEnd();
 }
 
 void WebViewTestProxy::Reset() {
