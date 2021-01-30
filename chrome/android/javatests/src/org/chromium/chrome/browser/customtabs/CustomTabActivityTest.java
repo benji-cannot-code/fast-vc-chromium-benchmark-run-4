82af622780130c3f291887020be03965fac8519c
Nate Fischer <ntfschr@google.com>
2021-01-30 00:08:42 +0000

    sheriff: disable flaky CCT tests
    No change to logic. This disables the flakiest tests within
    CustomTabActivityTest.
    
    Bug: 1148544
    Test: None
    Change-Id: I4f70f21358f0c35ad0d39b5d2c106216fa6c3649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2659978
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848800}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -1200,6 +1200,7 @@ public class CustomTabActivityTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1148544")
     public void testCreateNewTab() throws Exception {
         launchIntentThatOpensPopup(null);
     }
@@ -1873,6 +1874,7 @@ public class CustomTabActivityTest {
     @Test
     @SmallTest
     @Restriction(RESTRICTION_TYPE_NON_LOW_END_DEVICE)
+    @DisabledTest(message = "https://crbug.com/1148544")
     public void testHiddenTabAndChangingFragmentDontWait() throws Exception {
         startHiddenTabAndChangeFragment(true, false);
     }
@@ -2371,6 +2373,7 @@ public class CustomTabActivityTest {
     @Test
     @SmallTest
     @EnableFeatures({ChromeFeatureList.OMNIBOX_HIDE_VISITS_FROM_CCT})
+    @DisabledTest(message = "https://crbug.com/1148544")
     public void testOmniboxHideVisitsFromCctTransitionAppliesToPopups() throws Exception {
         Bundle extras = new Bundle();
         extras.putBoolean(
@@ -2793,6 +2796,7 @@ public class CustomTabActivityTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1148544")
     public void closeButton_navigatesToLandingPage() throws TimeoutException {
         Context context = InstrumentationRegistry.getInstrumentation()
                 .getTargetContext()
@@ -2821,6 +2825,7 @@ public class CustomTabActivityTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1148544")
     public void closeButton_closesActivityIfNoLandingPage() throws TimeoutException {
         Context context = InstrumentationRegistry.getInstrumentation()
                 .getTargetContext()
