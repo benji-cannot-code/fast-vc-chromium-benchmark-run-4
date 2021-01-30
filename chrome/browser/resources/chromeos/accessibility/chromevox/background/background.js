49bb86be24c8ddc331498d4e8b063e0171d99f63
David Tseng <dtseng@chromium.org>
2021-01-30 02:18:02 +0000

    Transition chromevox Background class to ES6
    This change is a first step to demonstrate how to migrate old style
    Closure files/classes to ES6 modules in ChromeVox, incrementally.
    
    Inparticular, it should compile successfully, pass all test suites, and
    run stable in production.
    
    Finally, modules need to be migrated in breadth-first order in the
    ChromeVox dependency graph starting at any script source. This is
    because ES6 modules can depend on Closure provided classes/files, but
    not vice versa.
    
    In this change, we start with the background script; migrate the
    background.js file with entry points in background.html for ES6 modules
    and the old script.
    
    See point 3 in go/unified-accessibility-component-extensions for more info.
    
    R=akihiroota@chromium.org, dmazzoni@chromium.org
    
    AX-Relnotes: n/a
    Test: compiles, passes closure compile, passes all tests.
    Change-Id: I944be36e6e5f1a57acce96df61e66589974fc226
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2657992
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848853}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/background.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/background.js
@@ -8,44 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * background page.
  */
 
-goog.provide('Background');
-
-goog.require('AutomationPredicate');
-goog.require('AutomationUtil');
-goog.require('BackgroundKeyboardHandler');
-goog.require('BrailleCommandData');
-goog.require('BrailleCommandHandler');
-goog.require('BrailleKeyCommand');
-goog.require('ChromeVoxBackground');
-goog.require('ChromeVoxEditableTextBase');
-goog.require('ChromeVoxState');
-goog.require('CommandHandler');
-goog.require('DesktopAutomationHandler');
-goog.require('DownloadHandler');
-goog.require('ExtensionBridge');
-goog.require('FindHandler');
-goog.require('FocusAutomationHandler');
-goog.require('GestureCommandHandler');
-goog.require('InstanceChecker');
-goog.require('LiveRegions');
-goog.require('LocaleOutputHelper');
-goog.require('MathHandler');
-goog.require('MediaAutomationHandler');
-goog.require('NavBraille');
-goog.require('NextEarcons');
-goog.require('NodeIdentifier');
-goog.require('Notifications');
-goog.require('Output');
-goog.require('Output.EventType');
-goog.require('PanelCommand');
-goog.require('PhoneticData');
-goog.require('RangeAutomationHandler');
-goog.require('UserAnnotationHandler');
-goog.require('constants');
-goog.require('cursors.Cursor');
-
-
-goog.scope(function() {
 const AutomationNode = chrome.automation.AutomationNode;
 const Dir = constants.Dir;
 const EventType = chrome.automation.EventType;
@@ -55,7 +17,7 @@ const StateType = chrome.automation.StateType;
 /**
  * ChromeVox2 background page.
  */
-Background = class extends ChromeVoxState {
+export class Background extends ChromeVoxState {
   constructor() {
     super();
 
@@ -268,6 +230,12 @@ Background = class extends ChromeVoxState {
   }
 
   /**
+   * Navigate to the given range - it both sets the range and outputs it.
+   * @param {!cursors.Range} range The new range.
+   * @param {boolean=} opt_focus Focus the range; defaults to true.
+   * @param {Object=} opt_speechProps Speech properties.
+   * @param {boolean=} opt_shouldSetSelection If true, does set
+   *     the selection.
    * @override
    */
   navigateToRange(range, opt_focus, opt_speechProps, opt_shouldSetSelection) {
@@ -551,9 +519,7 @@ Background = class extends ChromeVoxState {
             .join('|') +
         ')$');
   }
-};
+}
 
 InstanceChecker.closeExtraInstances();
 new Background();
-
-});  // goog.scope
