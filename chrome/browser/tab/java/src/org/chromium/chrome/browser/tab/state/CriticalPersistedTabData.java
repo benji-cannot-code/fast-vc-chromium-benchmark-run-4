72682d368a46dc84b0c4f37822cd0c4dc7a6ccd4
David Maunder <davidjm@chromium.org>
2021-01-30 01:49:56 +0000

    Cleanup CriticalPersistedTabData files when flag is off
    If the user goes from treatment to control, their
    CriticalPersistedTabData files will still exist. If they then go back to
    treatment after some time we will start using their
    CriticalPersistedTabData files which will be out of date. We should
    remove the CriticalPersistedTabData files when they are in control to
    avoid this.
    
    Bug: 1171912
    Change-Id: If05922fc2ccd3572dc8de8fc802c13a0a3049710
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2657795
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848844}

--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/CriticalPersistedTabData.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/CriticalPersistedTabData.java
@@ -394,6 +394,11 @@ public class CriticalPersistedTabData extends PersistedTabData {
         }
     }
 
+    @Override
+    public void delete() {
+        super.delete();
+    }
+
     /**
      * Encapsulates use cases where saving is disabled - as taken from TabPersistentStore.java to
      * ensure feature parity.
@@ -578,7 +583,7 @@ public class CriticalPersistedTabData extends PersistedTabData {
     }
 
     @VisibleForTesting
-    protected void setShouldSaveForTesting(boolean shouldSaveForTesting) {
+    public void setShouldSaveForTesting(boolean shouldSaveForTesting) {
         mShouldSaveForTesting = shouldSaveForTesting;
     }
 }
