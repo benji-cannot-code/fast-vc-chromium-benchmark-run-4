fe4a36304a69836e2f001cffd8341cf58498c3bf
Tomasz Wiszkowski <ender@google.com>
2021-01-30 02:31:29 +0000

    Fix tests failing due to Set#of not being supported.
    Bug: 1128298
    Change-Id: I48c80e4a0d87db8b2066912a96f67c609f1755d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2660199
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848856}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/CachedZeroSuggestionsManagerUnitTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/CachedZeroSuggestionsManagerUnitTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox.suggestions;
 
+import android.util.ArraySet;
 import android.util.SparseArray;
 
 import androidx.test.filters.SmallTest;
@@ -18,7 +19,6 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.UiThreadTest;
 import org.chromium.base.test.util.Batch;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.omnibox.OmniboxSuggestionType;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
@@ -381,7 +381,6 @@ public class CachedZeroSuggestionsManagerUnitTest {
     @Test
     @SmallTest
     @UiThreadTest
-    @DisabledTest(message = "https://crbug.com/1128298")
     public void rejectCacheIfSubtypesAreMalformed() {
         List<AutocompleteMatch> list = Arrays.asList(
                 createSuggestionBuilder(1, OmniboxSuggestionType.SEARCH_SUGGEST_PERSONALIZED)
@@ -397,7 +396,8 @@ public class CachedZeroSuggestionsManagerUnitTest {
 
         // Insert garbage for the Suggestion Subtypes.
         final SharedPreferencesManager manager = SharedPreferencesManager.getInstance();
-        final Set<String> garbageSubtypes = Set.of("invalid");
+        final Set<String> garbageSubtypes = new ArraySet<>();
+        garbageSubtypes.add("invalid");
         manager.writeStringSet(
                 ChromePreferenceKeys.KEY_ZERO_SUGGEST_NATIVE_SUBTYPES_PREFIX.createKey(1),
                 garbageSubtypes);
@@ -409,7 +409,6 @@ public class CachedZeroSuggestionsManagerUnitTest {
     @Test
     @SmallTest
     @UiThreadTest
-    @DisabledTest(message = "https://crbug.com/1128298")
     public void rejectCacheIfSubtypesIncludeNull() {
         List<AutocompleteMatch> list = Arrays.asList(
                 createSuggestionBuilder(1, OmniboxSuggestionType.SEARCH_SUGGEST_PERSONALIZED)
@@ -420,7 +419,8 @@ public class CachedZeroSuggestionsManagerUnitTest {
         CachedZeroSuggestionsManager.saveToCache(dataToCache);
 
         final SharedPreferencesManager manager = SharedPreferencesManager.getInstance();
-        final Set<String> garbageSubtypes = Set.of("null");
+        final Set<String> garbageSubtypes = new ArraySet<>();
+        garbageSubtypes.add("null");
         manager.writeStringSet(
                 ChromePreferenceKeys.KEY_ZERO_SUGGEST_NATIVE_SUBTYPES_PREFIX.createKey(0),
                 garbageSubtypes);
