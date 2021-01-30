a9e468f8f34ad7221e7aad0fc8aa0adffe589e43
Yuta Hijikata <ythjkt@google.com>
2021-01-30 00:57:30 +0000

    Lacros: Preparation for lacros C++ macro flip.
    Lacros chrome is now built as target_os="chromeos" but C++ macros
    OS_LINUX and OS_CHROMEOS has not been flipped. More on
    https://crrev.com/c/2658161.
    
    This ensures that lacros-chrome is consistent after such flip.
    
    Bug: 1171629
    Change-Id: I57490156357cca838435a7b893773035a6286eec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2658704
    Auto-Submit: Yuta Hijikata <ythjkt@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848822}

--- media/video/video_encode_accelerator.cc
+++ media/video/video_encode_accelerator.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 
 namespace media {
 
@@ -154,7 +155,7 @@ bool VideoEncodeAccelerator::IsFlushSupported() {
 }
 
 bool VideoEncodeAccelerator::IsGpuFrameResizeSupported() {
-#if defined(OS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS_ASH)
   // TODO(crbug.com/1166889) Add proper method overrides in
   // MojoVideoEncodeAccelerator and other subclasses that might return true.
   return true;
