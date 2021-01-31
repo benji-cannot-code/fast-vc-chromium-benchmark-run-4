c67ef55b4ef16c5af52237f9062684a837ecf37e
Thomas Guilbert <tguilbert@chromium.org>
2021-01-31 02:28:34 +0000

    Add AudioBuffer::ReadAllFrames and AudioFrame UTs
    This CL adds a media::AudioBus::ReadAllFrames() method, which copies,
    converts and deinterlaces data directly into a std::vector<float*>. This
    allows us to avoid having to convert our media::AudioBuffer into
    media::AudioBus, before writing into our blink::AudioBuffer, when
    constructing blink:AudioFrames.
    
    This CL also adds AudioFrame UTs, and fixes an issue with audio
    MediaStreamProcessor applications: the src and dest buffers were swapped
    when creating an AudioFrame from AudioFrameSerializationData.
    
    Bug: 1171840, 1157608
    Change-Id: I0997a46e164aa5f6c253ccc847434cef8813b75c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2660678
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848901}

--- third_party/blink/renderer/modules/webcodecs/audio_frame.h
+++ third_party/blink/renderer/modules/webcodecs/audio_frame.h
@@ -40,8 +40,6 @@ class MODULES_EXPORT AudioFrame final : public ScriptWrappable {
   void Trace(Visitor*) const override;
 
  private:
-  void CopyDataToInternalBuffer(media::AudioBus* data);
-
   uint64_t timestamp_;
   Member<AudioBuffer> buffer_;
 };
