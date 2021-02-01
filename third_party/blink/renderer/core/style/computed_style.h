f0242b2829c174d695f57f7764d46e546d83a5eb
Philip Rogers <pdr@chromium.org>
2021-02-01 19:16:52 +0000

    [CompositeSVG] Make 3D transforms a compositing trigger
    Web developers frequently use 3D transforms as a compositing trigger,
    even though the 3D component is a no-op in SVG. To maximize the cases
    where CompositeSVG is used, this patch considers 3D transforms in
    addition to the existing triggers such as will-change: transform.
    
    This patch builds on CompositingReasonsFor3DTransform which can now
    return reasons for SVG types. This required setting
    HasTransformRelatedProperty for SVG. Detecting changes in
    CompositingReasonsFor3DTransform is now done directly in ComputedStyle.
    
    Bug: 1171508
    Change-Id: Ib923202b39066d2e4c09041282370f8eb9344670
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2654467
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849203}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -2923,6 +2923,8 @@ class ComputedStyle : public ComputedStyleBase,
   bool DiffNeedsVisualRectUpdate(const ComputedStyle& other) const;
   CORE_EXPORT void UpdatePropertySpecificDifferences(const ComputedStyle& other,
                                                      StyleDifference&) const;
+  bool PotentialCompositingReasonsFor3DTransformChanged(
+      const ComputedStyle& other) const;
 
   bool PropertiesEqual(const Vector<CSSPropertyID>& properties,
                        const ComputedStyle& other) const;
