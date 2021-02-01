fde9cb3a13214abb97bafd2896cac74221f621d9
Pavel Yatsuk <pavely@chromium.org>
2021-02-01 21:21:54 +0000

    Revert "[AW] Support Platform provided trust tokens."
    This reverts commit 956319b6720a8ef6ff4f7ccc13ab208113d0aa98.
    
    Reason for revert: The CL causes test failures. E.g.:
    https://ci.chromium.org/ui/p/chrome/builders/ci/arm-builder-rel/27427/overview
    
    Original change's description:
    > [AW] Support Platform provided trust tokens.
    >
    > Add a method to PlatformServiceBridge to set a factory that creates a
    > LocalTrustTokenFulfiller object. Downstream embedders can override
    > this method.
    >
    > Add logic to call this method during the browser process
    > initialization.
    >
    > Bug: 1170468
    > Change-Id: I34e16382197d06b15e8e8bfa2dd6a94c6c8d3935
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2655917
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#849204}
    
    TBR=ntfschr@chromium.org,nator@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I61e68ba16eaadf72cb175ae188a789168637d5c2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1170468
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2666000
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849263}

--- android_webview/java/src/org/chromium/android_webview/AwBrowserProcess.java
+++ android_webview/java/src/org/chromium/android_webview/AwBrowserProcess.java
@@ -52,7 +52,6 @@ import org.chromium.components.policy.CombinedPolicyProvider;
 import org.chromium.content_public.browser.BrowserStartupController;
 import org.chromium.content_public.browser.ChildProcessCreationParams;
 import org.chromium.content_public.browser.ChildProcessLauncherHelper;
-import org.chromium.content_public.browser.trusttokens.TrustTokenFulfillerManager;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -154,9 +153,6 @@ public final class AwBrowserProcess {
                                 ScopedSysTraceEvent.scoped("AwBrowserProcess.maybeEnable")) {
                     AwSafeBrowsingConfigHelper.maybeEnableSafeBrowsingFromManifest(appContext);
                 }
-
-                TrustTokenFulfillerManager.setFactory(
-                        PlatformServiceBridge.getInstance().getLocalTrustTokenFulfillerFactory());
 
                 try (ScopedSysTraceEvent e2 = ScopedSysTraceEvent.scoped(
                              "AwBrowserProcess.startBrowserProcessesSync")) {
