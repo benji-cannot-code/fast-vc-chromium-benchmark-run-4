f63408d9bbd1bf1b66c91250ceb8c3f7d401b342
Alexander Cooper <alcooper@chromium.org>
2021-02-01 19:38:14 +0000

    [CodeHealth] Update c/b/plugins to modern callback methods
    Updates base::Bind calls in PluginInfoHostImpl and PluginPrefs to match
    the corresponding "Repeating" type of the underlying function being
    called.
    
    Updates PluginINfoHostImplUnittest to take a QuitClosure as a
    OnceClosure to match it's usage.
    
    Fixed: 1170970
    Change-Id: Ifa23e0150587c74b16deb0141c394fb051fdd975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2657771
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849210}

--- chrome/browser/plugins/plugin_info_host_impl_unittest.cc
+++ chrome/browser/plugins/plugin_info_host_impl_unittest.cc
@@ -33,9 +33,9 @@ using testing::Eq;
 
 namespace {
 
-void PluginsLoaded(const base::Closure& callback,
+void PluginsLoaded(base::OnceClosure callback,
                    const std::vector<content::WebPluginInfo>& plugins) {
-  callback.Run();
+  std::move(callback).Run();
 }
 
 class FakePluginServiceFilter : public content::PluginServiceFilter {
