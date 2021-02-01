1bda6ea29030422669ab7f86db18f6797dfb0692
Peter Kasting <pkasting@chromium.org>
2021-02-01 21:28:20 +0000

    Make components_unittests build with string16=std::u16string.
    Commonly, this requires making Windows-API-specific code more consistent
    about using std::wstring instead of assuming it's the same as string16.
    
    Bug: 911896
    Change-Id: I05cc9bb1ff851bcbf9b5a770dfb446f0fc187804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2661802
    Reviewed-by: Satoru Takabayashi <satorux@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Wei Li <weili@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849267}

--- chrome/browser/win/chrome_select_file_dialog_factory.cc
+++ chrome/browser/win/chrome_select_file_dialog_factory.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/win/win_util.h"
 #include "chrome/browser/win/util_win_service.h"
 #include "chrome/services/util_win/public/mojom/util_win.mojom.h"
@@ -33,7 +34,7 @@ class UtilWinHelper {
       const base::FilePath& default_path,
       const std::vector<ui::FileFilterSpec>& filter,
       int file_type_index,
-      const base::string16& default_extension,
+      const std::wstring& default_extension,
       HWND owner,
       ui::OnSelectFileExecutedCallback on_select_file_executed_callback);
 
@@ -44,7 +45,7 @@ class UtilWinHelper {
       const base::FilePath& default_path,
       const std::vector<ui::FileFilterSpec>& filter,
       int file_type_index,
-      const base::string16& default_extension,
+      const std::wstring& default_extension,
       HWND owner,
       ui::OnSelectFileExecutedCallback on_select_file_executed_callback);
 
@@ -75,7 +76,7 @@ void UtilWinHelper::ExecuteSelectFile(
     const base::FilePath& default_path,
     const std::vector<ui::FileFilterSpec>& filter,
     int file_type_index,
-    const base::string16& default_extension,
+    const std::wstring& default_extension,
     HWND owner,
     ui::OnSelectFileExecutedCallback on_select_file_executed_callback) {
   // Self-deleting when the select file operation completes.
@@ -90,7 +91,7 @@ UtilWinHelper::UtilWinHelper(
     const base::FilePath& default_path,
     const std::vector<ui::FileFilterSpec>& filter,
     int file_type_index,
-    const base::string16& default_extension,
+    const std::wstring& default_extension,
     HWND owner,
     ui::OnSelectFileExecutedCallback on_select_file_executed_callback)
     : on_select_file_executed_callback_(
@@ -104,7 +105,7 @@ UtilWinHelper::UtilWinHelper(
 
   remote_util_win_->CallExecuteSelectFile(
       type, base::win::HandleToUint32(owner), title, default_path, filter,
-      file_type_index, default_extension,
+      file_type_index, base::WideToUTF16(default_extension),
       base::BindOnce(&UtilWinHelper::OnSelectFileExecuted,
                      base::Unretained(this)));
 }
@@ -135,7 +136,7 @@ void ExecuteSelectFileImpl(
     const base::FilePath& default_path,
     const std::vector<ui::FileFilterSpec>& filter,
     int file_type_index,
-    const base::string16& default_extension,
+    const std::wstring& default_extension,
     HWND owner,
     ui::OnSelectFileExecutedCallback on_select_file_executed_callback) {
   UtilWinHelper::ExecuteSelectFile(type, title, default_path, filter,
--- ipc/ipc_message_utils.h
+++ ipc/ipc_message_utils.h
@@ -349,7 +349,7 @@ struct ParamTraits<base::string16> {
 
 #if defined(OS_WIN) && defined(BASE_STRING16_IS_STD_U16STRING)
 template <>
-struct ParamTraits<std::wstring> {
+struct COMPONENT_EXPORT(IPC) ParamTraits<std::wstring> {
   typedef std::wstring param_type;
   static void Write(base::Pickle* m, const param_type& p) {
     m->WriteString16(base::AsStringPiece16(p));
@@ -357,7 +357,7 @@ struct ParamTraits<std::wstring> {
   static bool Read(const base::Pickle* m,
                    base::PickleIterator* iter,
                    param_type* r);
-  COMPONENT_EXPORT(IPC) static void Log(const param_type& p, std::string* l);
+  static void Log(const param_type& p, std::string* l);
 };
 #endif
 
