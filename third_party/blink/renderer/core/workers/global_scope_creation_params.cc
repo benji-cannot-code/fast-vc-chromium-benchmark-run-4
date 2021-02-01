2bebf1214560c4e523be7d23c4c428e277ece68e
Minoru Chikamune <chikamune@chromium.org>
2021-02-01 04:04:05 +0000

    [MBI] Migrate CanvasResourceDispatcher to use per-ASG CompositorTaskRunner
    Before this CL, CanvasResourceDispatcher uses per-thread
    CompositorTaskRunner.
    
    After this CL, CanvasResourceDispatcher will use per-ASG (Agent
    Scheduling Group) CompositorTaskRunner.
    
    CanvasResourceDispatcher is used by OffscreenCanvas. OffscreenCanvas
    provides a functionality to draw the Canvas from Worker
    thread. Previously, CanvasResourceDispatcher was accessing the main
    thread’s objects from the worker thread by using
    `Thread::MainThread()->Scheduler()->CompositorTaskRunner()`. But this
    time, we want to use the per-AgentSchedulingGroup compositor task
    runner instead. To plumb per-ASG compositor task runner from main
    thread to worker thread, this CL added
    agent_group_scheduler_compositor_task_runner to WorkerGlobalScope.
    
    Bug: 1105403
    Change-Id: If4a352cd374c5555f94497c09977ee11105ded2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2632110
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#848996}

--- third_party/blink/renderer/core/workers/global_scope_creation_params.cc
+++ third_party/blink/renderer/core/workers/global_scope_creation_params.cc
@@ -42,7 +42,9 @@ GlobalScopeCreationParams::GlobalScopeCreationParams(
     base::UnguessableToken agent_cluster_id,
     ukm::SourceId ukm_source_id,
     const base::Optional<ExecutionContextToken>& parent_context_token,
-    bool parent_cross_origin_isolated_capability)
+    bool parent_cross_origin_isolated_capability,
+    scoped_refptr<base::SingleThreadTaskRunner>
+        agent_group_scheduler_compositor_task_runner)
     : script_url(script_url.Copy()),
       script_type(script_type),
       global_scope_name(global_scope_name.IsolatedCopy()),
@@ -74,7 +76,9 @@ GlobalScopeCreationParams::GlobalScopeCreationParams(
       ukm_source_id(ukm_source_id),
       parent_context_token(parent_context_token),
       parent_cross_origin_isolated_capability(
-          parent_cross_origin_isolated_capability) {
+          parent_cross_origin_isolated_capability),
+      agent_group_scheduler_compositor_task_runner(
+          std::move(agent_group_scheduler_compositor_task_runner)) {
   this->origin_trial_tokens = std::make_unique<Vector<String>>();
   if (origin_trial_tokens) {
     for (const String& token : *origin_trial_tokens)
