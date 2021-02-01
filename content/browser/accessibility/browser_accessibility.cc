e9cc16255f9f367cd3e7fa44e84a0fec75318218
Nektarios Paisios <nektar@chromium.org>
2021-02-01 10:42:03 +0000

    Improved the handling of empty objects in AXPosition
    This patch will need to land before we would be able to merge BrowserAccessibilityPosition
    with AXNodePosition.
    
    Empty objects in AXPosition are defined as nodes in the accessibility tree that
    do not expose any of their content to platform accessibility APIs.
    This situation can occur when, for example, a form control such as a text field or a
    button has no text inside it. Since we still need to treat such controls as character
    and word boundaries, we expose the embedded object character (\xFFFC) in the tree's text representation
    wherever these controls are located.
    The effect of this design decision is that all positions on descendants of
    empty objects should be invalid.
    
    Currently, to detect whether an object is empty, we check (with some exceptions)
    if it has only ignored descendants.
    
    There were two bugs in the current implementation:
    
    1. Sometimes Blink places unignored generic containers inside certain form controls.
    There may be more than one such container.
    ++kTextField (empty)
    ++++kGenericContainer (unignored)
    ++++++kGenericContainer (unignored)
    
    2. The amount of text that a node takes in its parent's hypertext (AKA AXPosition::MaxTextOffsetInParent)
    should be zero if the node is ignored, unless it has unignored descendants:
    
    A) If the node is ignored and it has only ignored descendants, or it has no descendants at all,
    it should not take up any space in its parent's text representation, because we don't want to accidentally turn an unignored
    text position anchored to an ancestor node, to an ignored one when computing the equivalent leaf text position.
    Otherwise, AXPosition's clients would need to always call AXPosition::AsUnignoredPosition() after every move.
    
    B) If the node is ignored but it has unignored descendants, then the space that the node should
    take in its hparent's hypertext should be recursively computed by summing up
    the length of the text of its unignored descendants.
    
    R=dmazzoni@chromium.org, aleventhal@chromium.org, kschmi@microsoft.com
    
    AX-Relnotes: n/a.
    Change-Id: I149d84a5f8435ca69afa599d6ba5392b526c5c2e
    Bug: 1049261,
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2661475
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849048}

--- content/browser/accessibility/browser_accessibility.cc
+++ content/browser/accessibility/browser_accessibility.cc
@@ -1497,6 +1497,10 @@ bool BrowserAccessibility::IsChildOfLeaf() const {
   return node()->IsChildOfLeaf();
 }
 
+bool BrowserAccessibility::IsEmptyLeaf() const {
+  return node()->IsEmptyLeaf();
+}
+
 bool BrowserAccessibility::IsLeaf() const {
   // According to the ARIA and Core-AAM specs:
   // https://w3c.github.io/aria/#button,
--- content/browser/accessibility/browser_accessibility.h
+++ content/browser/accessibility/browser_accessibility.h
@@ -98,7 +98,10 @@ class CONTENT_EXPORT BrowserAccessibility : public ui::AXPlatformNodeDelegate {
 
   bool IsLineBreakObject() const;
 
-  // See AXNode::IsLeaf().
+  // See `AXNode::IsEmptyLeaf()`.
+  bool IsEmptyLeaf() const;
+
+  // See `AXNode::IsLeaf()`.
   bool PlatformIsLeaf() const;
 
   // Returns true if this object can fire events.
