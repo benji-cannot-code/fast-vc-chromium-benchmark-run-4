3849a8c42c38218ce22921df4ca9233ef8e31f09
Caleb Raitto <caraitto@chromium.org>
2021-02-01 22:28:37 +0000

    Add a PlatformBrowserTest to verify the DynamicsCompressor fingerprint.
    This gives us coverage on Android (32-bit and 64-bit builds, on multiple
    OS versions and devices, including Nexus 5, Nexus 5X, Pixel 1 and Pixel
    2) Windows 10, and Linux. For some reason, this test doesn't seem to run
    on Mac on trybots, but perhaps it does on waterfall?
    
    This test uses an original fingerprinting script. To validate the
    effectiveness of this script, I ran this change without the
    anti-fingerprinting mitigations in a trybot run on crrev.com/c/2653828
    -- each OS / device yielded a different fingerprint in that un-mitigated
    run:
    
    Nexus 5 32 bit build: 13.130921687808495
    Nexus 5X 64 bit build: 13.130464374060011
    Ubuntu Linux 16.04: 13.130925446395167
    Win 10: 13.13092414679113
    Pixel 1 64 bit build: 13.130463080802201
    
    Bug: 1145192
    Change-Id: I1c1cbc6bd615f300e080ca476af0f47f613f1730
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2579484
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849293}

---
+++ chrome/test/data/webaudio/dynamics_compressor_fingerprint.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Computes the DynamicsCompressor fingerprint and sends it back to the test via
+// `sendValueToTest`.
+
+async function computeFingerprint() {
+  const context = new OfflineAudioContext(
+      {numberOfChannels: 1, length: 44100, sampleRate: 44100});
+  if (!context) return 0.0;
+  const oscillator = new OscillatorNode(context);
+  const compressor = new DynamicsCompressorNode(context);
+  // For this test, it's ok to mix the stereo compressor output to mono for the
+  // final result.
+  oscillator.connect(compressor).connect(context.destination);
+  oscillator.start(0);
+  const renderedBuffer = await context.startRendering();
+  const chanData = renderedBuffer.getChannelData(0);
+  let result = 0;
+  for (const elem of chanData) {
+    result += elem;
+  }
+  return result;
+};
+
+window.addEventListener('load', async () => {
+  const fingerprint = await computeFingerprint();
+  sendValueToTest(fingerprint);
+});
