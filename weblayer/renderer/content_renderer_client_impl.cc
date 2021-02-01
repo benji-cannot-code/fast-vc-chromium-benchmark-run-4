4bc42901be4d5ce658a87e286eb3fc597845685d
Scott Violet <sky@chromium.org>
2021-02-01 18:09:26 +0000

    weblayer: register android-app scheme
    This way embedders can use android-app in GURLs. My plan is
    to refactor the constant in chrome, but I'm not doing that
    here as I'm going to merge this to 88.
    
    BUG=1172331
    TEST=*AndroidAppReferer
    
    Change-Id: I839fd4aa5f87638a352ab477025a911659cdb0a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2659559
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849161}

--- weblayer/renderer/content_renderer_client_impl.cc
+++ weblayer/renderer/content_renderer_client_impl.cc
@@ -41,6 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_thread.h"
 #include "services/service_manager/public/cpp/local_interface_provider.h"
 #include "third_party/blink/public/platform/web_runtime_features.h"
+#include "third_party/blink/public/platform/web_string.h"
+#include "third_party/blink/public/web/web_security_policy.h"
 #endif
 
 namespace weblayer {
@@ -80,6 +82,10 @@ void ContentRendererClientImpl::RenderThreadStarted() {
     local_interface_provider_ = std::make_unique<SpellcheckInterfaceProvider>();
     spellcheck_ = std::make_unique<SpellCheck>(local_interface_provider_.get());
   }
+  // TODO(sky): refactor. This comes from chrome/common/url_constants.cc's
+  // kAndroidAppScheme.
+  blink::WebSecurityPolicy::RegisterURLSchemeAsAllowedForReferrer(
+      blink::WebString::FromUTF8("android-app"));
 #endif
 
   content::RenderThread* thread = content::RenderThread::Get();
