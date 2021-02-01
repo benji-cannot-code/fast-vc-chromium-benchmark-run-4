a13eda5010598edccf14cf557d0d4b5401303427
Antonio Sartori <antoniosartori@chromium.org>
2021-02-01 09:17:09 +0000

    Initialize ICU in content_security_policy_util_fuzzer
    Bug: 1172755
    Change-Id: Iefb37858d7f6ef48bd92d8e0a852f4dc7490092c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2662775
    Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849035}

--- content/renderer/content_security_policy_util_fuzzer.cc
+++ content/renderer/content_security_policy_util_fuzzer.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/command_line.h"
+#include "base/i18n/icu_util.h"
 #include "base/test/test_timeouts.h"
 #include "content/public/test/blink_test_environment.h"
 #include "content/renderer/content_security_policy_util.h"
@@ -33,6 +34,8 @@ class Environment {
     // is complete, this is for efficiency. We rerun the fuzzer with the same
     // environment as the previous iteration.
     base::AtExitManager at_exit;
+
+    CHECK(base::i18n::InitializeICU());
 
     base::CommandLine::Init(0, nullptr);
 
