c0590b8ea27baceea0667f0247a5b350a687a02c
Roman Aleksandrov <raleksandrov@google.com>
2021-02-01 16:09:58 +0000

    oobe: create simplified oobe dialog for content only hosting.
    Bug: 1168519
    Change-Id: I73154d6470e636c9271517ed2c858e0c2240dd6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2640394
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849130}

--- chrome/browser/resources/chromeos/login/components/oobe_scrollable_behavior/oobe_scrollable_behavior.js
+++ chrome/browser/resources/chromeos/login/components/oobe_scrollable_behavior/oobe_scrollable_behavior.js
@@ -15,7 +15,7 @@ const OobeScrollableBehavior = {
    * Init observers to keep track of the scrollable element size changes.
    */
   initScrollableObservers(scrollableElement, ...sizeChangeObservableElemenets) {
-    if (!scrollableElement) {
+    if (!scrollableElement || this.scrollableElement_) {
       return;
     }
     this.scrollableElement_ = scrollableElement;
@@ -39,6 +39,13 @@ const OobeScrollableBehavior = {
     el.classList.toggle(
         'scrolled-to-bottom',
         el.scrollTop + el.clientHeight >= el.scrollHeight);
+  },
+
+  /**
+   * Scroll to the bottom of scrollalbe element.
+   */
+  scrollToBottom() {
+    this.scrollableElement_.scrollTop = this.scrollableElement_.scrollHeight;
   },
 };
 
