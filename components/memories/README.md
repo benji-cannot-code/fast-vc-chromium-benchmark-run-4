7266dfccc8b1402c6c7fa49ede7c1ab38cd46b87
Tommy Li <tommycli@chromium.org>
2021-02-01 20:01:50 +0000

    [memories] Add components/memories directory
    It also add a base::Feature that our teammates can start guarding their
    WebUI surfaces behind.
    
    Bug: 1171352
    Change-Id: If1873874155618e85ab3d452933e3da1047b9fcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653283
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849228}

---
+++ components/memories/README.md
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# //components/memories
+
+Service implementation for Chrome Memories history clustering feature.
+
+This is implemented as a component to allow //components/omnibox to depend on
+this feature, as well as future iOS compatibility.
---
+++ components/memories/common/memories_features.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/memories/common/memories_features.h"
+
+#include "build/build_config.h"
+
+namespace memories {
+
+// Enables the Chrome Memories history clustering feature.
+const base::Feature kChromeMemories{
+    "ChromeMemories",
+    base::FEATURE_ENABLED_BY_DEFAULT,
+};
+
+}  // namespace memories
