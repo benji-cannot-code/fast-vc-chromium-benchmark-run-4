5b57f744eb793e15e2c76c482faa4077fb73c952
Nate Fischer <ntfschr@chromium.org>
2021-02-01 12:44:53 +0000

    Revert "accessibility: Retrieve character bounds from input search box."
    This reverts commit 9d95c8371b830541e5b5d6d4673ff0417c00bcaf.
    
    Reason for revert: doesn't pass on android-pie-x86-rel bot
    
    Original change's description:
    > accessibility: Retrieve character bounds from input search box.
    >
    > Plain Text fields wrapping their static text and inline text boxes in generic
    > containers, and some, like input type=search, wrap the wrapper as
    > well, and |kGenericContainer| is a generic container for flow content
    > that by itself does not represent anything so when querying
    > inner text, we should query their child nodes instead of the container
    > itself.
    >
    > AX-Relnotes: n/a.
    >
    > Bug: 1167605
    >
    > Change-Id: I98804f6f376fc732c1ba37958ab1f70ca9b2abfb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631978
    > Commit-Queue: HuiWang <wanghui210@huawei.com>
    > Reviewed-by: Bo <boliu@chromium.org>
    > Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#847951}
    
    Bug: 1167605
    Change-Id: Ibba38281dae9de4448560fd28fdaae7751e66abd
    Fixed: 1172485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2660200
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849080}

--- content/browser/accessibility/browser_accessibility_android_unittest.cc
+++ content/browser/accessibility/browser_accessibility_android_unittest.cc
@@ -89,7 +89,7 @@ TEST_F(BrowserAccessibilityAndroidTest, TestRetargetTextOnly) {
 
   std::unique_ptr<BrowserAccessibilityManager> manager(
       BrowserAccessibilityManager::Create(
-          MakeAXTreeUpdate({root, para1, text1}),
+          MakeAXTreeUpdate(root, para1, text1),
           test_browser_accessibility_delegate_.get()));
 
   BrowserAccessibility* root_obj = manager->GetRoot();
@@ -128,7 +128,7 @@ TEST_F(BrowserAccessibilityAndroidTest, TestRetargetHeading) {
 
   std::unique_ptr<BrowserAccessibilityManager> manager(
       BrowserAccessibilityManager::Create(
-          MakeAXTreeUpdate({root, heading1, text1}),
+          MakeAXTreeUpdate(root, heading1, text1),
           test_browser_accessibility_delegate_.get()));
 
   BrowserAccessibility* root_obj = manager->GetRoot();
@@ -168,7 +168,7 @@ TEST_F(BrowserAccessibilityAndroidTest, TestRetargetFocusable) {
 
   std::unique_ptr<BrowserAccessibilityManager> manager(
       BrowserAccessibilityManager::Create(
-          MakeAXTreeUpdate({root, para1, text1}),
+          MakeAXTreeUpdate(root, para1, text1),
           test_browser_accessibility_delegate_.get()));
 
   BrowserAccessibility* root_obj = manager->GetRoot();
@@ -255,9 +255,8 @@ TEST_F(BrowserAccessibilityAndroidTest, TestRetargetInputControl) {
 
   std::unique_ptr<BrowserAccessibilityManager> manager(
       BrowserAccessibilityManager::Create(
-          MakeAXTreeUpdate({root, container, form, label, label_text,
-                            input_time, input_container, input_text, button,
-                            button_text}),
+          MakeAXTreeUpdate(root, container, form, label, label_text, input_time,
+                           input_container, input_text, button, button_text),
           test_browser_accessibility_delegate_.get()));
 
   BrowserAccessibility* root_obj = manager->GetRoot();
