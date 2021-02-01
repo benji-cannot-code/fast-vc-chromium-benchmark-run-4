de20a17ad63a50c84a61c5b007f6e5c5dfd6c6b2
Michael Lippautz <mlippautz@chromium.org>
2021-02-01 12:09:12 +0000

    heap: Oilpan library: Add heap state accessors
    Drive-by: Fix ThreadState ctor for the library.
    
    Bug: 1056170
    Change-Id: Ia45369b1385a16d03818033efc6e1262e78eab08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2663076
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849065}

--- third_party/blink/renderer/platform/heap/v8_wrapper/thread_state.h
+++ third_party/blink/renderer/platform/heap/v8_wrapper/thread_state.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/thread_specific.h"
 #include "third_party/blink/renderer/platform/wtf/threading.h"
 #include "v8-profiler.h"
+#include "v8/include/cppgc/heap-consistency.h"
 #include "v8/include/cppgc/prefinalizer.h"
 #include "v8/include/v8-cppgc.h"
 #include "v8/include/v8.h"
@@ -138,6 +139,12 @@ class ThreadState final {
 
   size_t GcAge() const { return gc_age_; }
 
+  bool InAtomicSweepingPause() const {
+    auto& heap_handle = cpp_heap().GetHeapHandle();
+    return cppgc::subtle::HeapState::IsInAtomicPause(heap_handle) &&
+           cppgc::subtle::HeapState::IsSweeping(heap_handle);
+  }
+
  private:
   // Main-thread ThreadState avoids TLS completely by using a regular global.
   // The object is manually managed and should not rely on global ctor/dtor.
@@ -147,7 +154,7 @@ class ThreadState final {
   static base::LazyInstance<WTF::ThreadSpecific<ThreadState*>>::Leaky
       thread_specific_;
 
-  explicit ThreadState(v8::CppHeap&);
+  explicit ThreadState();
   ~ThreadState();
 
   // Handle is the most frequently accessed field as it is required for
