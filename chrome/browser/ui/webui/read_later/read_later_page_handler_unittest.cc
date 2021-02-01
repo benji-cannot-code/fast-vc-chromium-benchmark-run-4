c394aa70eb8dc14bcf2d24591db76e2af0ed8192
Tom <tluk@chromium.org>
2021-02-01 19:03:58 +0000

    Read Later: Improve ItemsChanged() API
    Currently ItemsChanged() notifies the JS in the renderer that its
    read later entries need to be refreshed which causes the JS to call
    GetReadLaterEntries() to fetch the latest data.
    
    This interaction currently takes 3 hops between the browser and
    renderer process. This CL updates the API such that the current
    lists of read/unread entries are supplied in the ItemsChanged()
    call from the browser process.
    
    Bug: None
    Change-Id: I4ba998e8c3dc109aa30c136feee83124621bde71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2659414
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849193}

--- chrome/browser/ui/webui/read_later/read_later_page_handler_unittest.cc
+++ chrome/browser/ui/webui/read_later/read_later_page_handler_unittest.cc
@@ -44,7 +44,8 @@ class MockPage : public read_later::mojom::Page {
   }
   mojo::Receiver<read_later::mojom::Page> receiver_{this};
 
-  MOCK_METHOD0(ItemsChanged, void());
+  MOCK_METHOD1(ItemsChanged,
+               void(read_later::mojom::ReadLaterEntriesByStatusPtr));
 };
 
 void ExpectNewReadLaterEntry(const read_later::mojom::ReadLaterEntry* entry,
@@ -161,7 +162,7 @@ TEST_F(TestReadLaterPageHandlerTest, OpenSavedEntry) {
 
 TEST_F(TestReadLaterPageHandlerTest, UpdateReadStatus) {
   handler()->UpdateReadStatus(GURL(kTabUrl3), true);
-  EXPECT_CALL(page_, ItemsChanged()).Times(1);
+  EXPECT_CALL(page_, ItemsChanged(testing::_)).Times(1);
 
   // Get Read later entries.
   read_later::mojom::PageHandler::GetReadLaterEntriesCallback callback1 =
@@ -183,7 +184,7 @@ TEST_F(TestReadLaterPageHandlerTest, UpdateReadStatus) {
 
 TEST_F(TestReadLaterPageHandlerTest, RemoveEntry) {
   handler()->RemoveEntry(GURL(kTabUrl3));
-  EXPECT_CALL(page_, ItemsChanged()).Times(1);
+  EXPECT_CALL(page_, ItemsChanged(testing::_)).Times(1);
 
   // Get Read later entries.
   read_later::mojom::PageHandler::GetReadLaterEntriesCallback callback1 =
