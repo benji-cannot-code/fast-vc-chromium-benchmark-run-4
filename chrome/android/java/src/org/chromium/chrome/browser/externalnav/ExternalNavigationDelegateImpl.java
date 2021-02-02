d63169e2e0fa73ba7a260d579a91ded3cf0c73aa
Michael Thiessen <mthiesse@chromium.org>
2021-02-02 20:49:58 +0000

    Ensure Trusted Intent Extras are only added to intents targeting Chrome
    Ensures that intents we add our Trusted Intent Extras to can only
    possibly target Chrome, and not other apps unintentionally.
    
    Bug: 1172513
    Change-Id: Ia7bfd0bbbccb53e6bafdc4c72bc45d526781cb98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2660775
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849740}

--- chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImpl.java
@@ -19,6 +19,7 @@ import androidx.annotation.Nullable;
 import org.chromium.base.ApplicationState;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ContextUtils;
+import org.chromium.base.IntentUtils;
 import org.chromium.base.PackageManagerUtils;
 import org.chromium.chrome.browser.ChromeTabbedActivity2;
 import org.chromium.chrome.browser.IntentHandler;
@@ -86,7 +87,10 @@ public class ExternalNavigationDelegateImpl implements ExternalNavigationDelegat
     }
 
     /**
-     * Determines whether Chrome will be handling the given Intent.
+     * Determines whether Chrome would handle this Intent if fired immediately. Note that this does
+     * not guarantee that Chrome actually will handle the intent, as another app may be installed,
+     * or components may be enabled that provide alternative handlers for this intent before it gets
+     * fired.
      *
      * @param intent            Intent that will be fired.
      * @param matchDefaultOnly  See {@link PackageManager#MATCH_DEFAULT_ONLY}.
@@ -95,12 +99,7 @@ public class ExternalNavigationDelegateImpl implements ExternalNavigationDelegat
     public static boolean willChromeHandleIntent(Intent intent, boolean matchDefaultOnly) {
         Context context = ContextUtils.getApplicationContext();
         // Early-out if the intent targets Chrome.
-        if (context.getPackageName().equals(intent.getPackage())
-                || (intent.getComponent() != null
-                        && context.getPackageName().equals(
-                                intent.getComponent().getPackageName()))) {
-            return true;
-        }
+        if (IntentUtils.intentTargetsSelf(context, intent)) return true;
 
         // Fall back to the more expensive querying of Android when the intent doesn't target
         // Chrome.
