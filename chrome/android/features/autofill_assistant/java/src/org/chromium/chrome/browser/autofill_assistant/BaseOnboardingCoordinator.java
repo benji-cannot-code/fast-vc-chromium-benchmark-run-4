aa1daebb53f6af53a04d76c3932344826633a269
Rahul Goyal <goyalrahul@google.com>
2021-02-02 09:36:56 +0000

    [Autofill Assistant] Added interface for Base Onboarding Coordinator
    Added an interface for public methods of Base Onboarding Coordinator
    
    Bug: b/174445633
    Change-Id: I6a9ddda740eea08b0598ba35ab163de6517322fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2596302
    Commit-Queue: Rahul Goyal <goyalrahul@google.com>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849523}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/BaseOnboardingCoordinator.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/BaseOnboardingCoordinator.java
@@ -40,8 +40,7 @@ import java.util.Map;
  * Autofill Assistant for the first time.
  */
 @JNINamespace("autofill_assistant")
-// TODO(b/174445633): Add a public interface for |show()|, |hide()| and |getOboardingShown()|
-public abstract class BaseOnboardingCoordinator {
+public abstract class BaseOnboardingCoordinator implements OnboardingView {
     private static final String INTENT_IDENTFIER = "INTENT";
     private static final String FETCH_TIMEOUT_IDENTIFIER = "ONBOARDING_FETCH_TIMEOUT_MS";
     private static final String BUY_MOVIE_TICKETS_INTENT = "BUY_MOVIE_TICKET";
@@ -87,6 +86,7 @@ public abstract class BaseOnboardingCoordinator {
      * <p>The {@code targetUrl} is the initial URL Autofill Assistant is being started on. The
      * navigation to that URL is allowed, other navigations will hide Autofill Assistant.
      */
+    @Override
     public void show(Callback<Integer> callback, WebContents webContents, String targetUrl) {
         addWebContentObserver(callback, webContents, targetUrl);
         show(callback);
@@ -126,7 +126,8 @@ public abstract class BaseOnboardingCoordinator {
      * Returns {@code true} if the onboarding has been shown at the beginning when this
      * autofill assistant flow got triggered.
      */
-    boolean getOnboardingShown() {
+    @Override
+    public boolean getOnboardingShown() {
         return mOnboardingShown;
     }
 
@@ -317,7 +318,6 @@ public abstract class BaseOnboardingCoordinator {
     }
 
     abstract ScrollView createViewImpl();
-    abstract void hide();
     abstract void initViewImpl(Callback<Integer> callback);
     abstract void showViewImpl();
 
--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/DialogOnboardingCoordinator.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/DialogOnboardingCoordinator.java
@@ -75,7 +75,7 @@ class DialogOnboardingCoordinator extends BaseOnboardingCoordinator {
     }
 
     @Override
-    void hide() {
+    public void hide() {
         if (mDialog != null) {
             mDialog.cancel();
             mDialog = null;
