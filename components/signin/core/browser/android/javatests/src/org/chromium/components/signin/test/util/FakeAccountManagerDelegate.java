29fede9f88d660d0b1b6d7667158648110d5db15
Alice Wang <aliceywang@chromium.org>
2021-02-02 08:53:18 +0000

    [Signin][Android] Cleanup unnecessary deps of signin_java_test_support
    This CL cleans up unnecessary dependencies of signin_java_test_support.
    
    Bug: 1172172
    Change-Id: I10fed1ea5a80ffe6b0aa5b6db5c3314921059b3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2662995
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849512}

--- components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/util/FakeAccountManagerDelegate.java
+++ components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/util/FakeAccountManagerDelegate.java
@@ -13,8 +13,6 @@ import android.content.Intent;
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 
-import org.junit.Assert;
-
 import org.chromium.base.Callback;
 import org.chromium.base.Log;
 import org.chromium.base.ObserverList;
@@ -146,7 +144,7 @@ public class FakeAccountManagerDelegate implements AccountManagerDelegate {
         ThreadUtils.runOnUiThreadBlocking(() -> {
             synchronized (mAccounts) {
                 boolean added = mAccounts.add(accountHolder);
-                Assert.assertTrue("Account already added", added);
+                assert added : "Account already added";
                 for (AccountsChangeObserver observer : mObservers) {
                     observer.onAccountsChanged();
                 }
@@ -166,7 +164,7 @@ public class FakeAccountManagerDelegate implements AccountManagerDelegate {
         ThreadUtils.runOnUiThreadBlocking(() -> {
             synchronized (mAccounts) {
                 boolean removed = mAccounts.remove(accountHolder);
-                Assert.assertTrue("Can't find account", removed);
+                assert removed : "Can't find account";
                 for (AccountsChangeObserver observer : mObservers) {
                     observer.onAccountsChanged();
                 }
