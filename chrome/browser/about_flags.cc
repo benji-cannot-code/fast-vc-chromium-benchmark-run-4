b07ba249f050705ccca8b5b3e812d1f4cba86f1e
Olivier Yiptong <oyiptong@chromium.org>
2021-02-02 20:47:42 +0000

    FontAccess: Remove FontAccessChooser flag
    This removes the FontAccessChooser flag because it is now obsolete.
    
    Bug: 1173253
    Change-Id: Ie5f93e6280dac110889d79edefb1402b879b8d6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2665760
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Olivier Yiptong <oyiptong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849736}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -5778,10 +5778,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kFontAccessAPIDescription, kOsAll,
      FEATURE_VALUE_TYPE(blink::features::kFontAccess)},
 
-    {"font-access-chooser", flag_descriptions::kFontAccessChooserAPIName,
-     flag_descriptions::kFontAccessChooserAPIDescription, kOsAll,
-     FEATURE_VALUE_TYPE(blink::features::kFontAccessChooser)},
-
     {"mouse-subframe-no-implicit-capture",
      flag_descriptions::kMouseSubframeNoImplicitCaptureName,
      flag_descriptions::kMouseSubframeNoImplicitCaptureDescription, kOsAll,
