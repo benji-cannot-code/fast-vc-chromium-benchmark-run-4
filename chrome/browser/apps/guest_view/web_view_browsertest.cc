d6524a3f2887202b43022b96b0f117825efbe6f1
Ayu Ishii <ayui@chromium.org>
2021-02-02 18:23:15 +0000

    CodeHealth: Convert chrome/browser/apps/guest_view/* from Callback/Bind
    This changes convert uses of base::Bind and base::Callback to
    Once/Repeating. Refer to (https://crbug.com/714018) for details.
    
    Bug: 1170946
    Change-Id: I5bfd1649c3660a656ebe35812b00ebb1d2920595
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2665759
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849673}

--- chrome/browser/apps/guest_view/web_view_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_browsertest.cc
@@ -2772,7 +2772,7 @@ class DownloadManagerWaiter : public content::DownloadManager::Observer {
   }
 
  private:
-  base::Closure quit_closure_;
+  base::OnceClosure quit_closure_;
   bool initialized_;
   content::DownloadManager* download_manager_;
 };
