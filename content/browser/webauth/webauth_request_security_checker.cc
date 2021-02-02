4283b10062cc180584f31fc7f69483b6dc96a53a
Rouslan Solomakhin <rouslan@chromium.org>
2021-02-02 15:13:03 +0000

    [Secure Payment Confirmation] Enable enrolling from an iframe.
    Before this patch, no WebAuthn credential could be created from within a
    cross origin iframe.
    
    This patch adds a boolean field `is_payment_credential_creation` to the
    mojo parameters for making credentials and special cases WebAuthn to
    allow making credentials when that boolean is true. Blink sets that
    boolean to true when the website is creating a payment credential and
    the execution context has the "payment" feature policy enabled and the
    "SecurePaymentConfirmation" feature is enabled.
    
    After this patch, a cross origin iframe with "payment" feature policy
    can create payment credentials in WebAuthn, while all other types of
    credentials are still prohibited from being created in a cross origin
    iframe, regardless of feature policy.
    
    Bug: 1173184
    Change-Id: I852c31babe858b825365d24781b00589b3940d0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2655605
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849597}

--- content/browser/webauth/webauth_request_security_checker.cc
+++ content/browser/webauth/webauth_request_security_checker.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
+#include "components/payments/core/features.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/public/browser/render_frame_host.h"
 #include "device/fido/features.h"
@@ -135,12 +136,16 @@ WebAuthRequestSecurityChecker::ValidateAncestorOrigins(
     RequestType type,
     bool* is_cross_origin) {
   *is_cross_origin = !IsSameOriginWithAncestors(origin);
-  if ((type == RequestType::kMakeCredential ||
+  if ((type != RequestType::kGetAssertion ||
        !base::FeatureList::IsEnabled(
            device::kWebAuthGetAssertionFeaturePolicy) ||
-       !static_cast<RenderFrameHostImpl*>(render_frame_host_)
-            ->IsFeatureEnabled(blink::mojom::FeaturePolicyFeature::
-                                   kPublicKeyCredentialsGet)) &&
+       !render_frame_host_->IsFeatureEnabled(
+           blink::mojom::FeaturePolicyFeature::kPublicKeyCredentialsGet)) &&
+      (type != RequestType::kMakePaymentCredential ||
+       !base::FeatureList::IsEnabled(
+           payments::features::kSecurePaymentConfirmation) ||
+       !render_frame_host_->IsFeatureEnabled(
+           blink::mojom::FeaturePolicyFeature::kPayment)) &&
       *is_cross_origin) {
     return blink::mojom::AuthenticatorStatus::NOT_ALLOWED_ERROR;
   }
