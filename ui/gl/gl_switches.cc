79c8c58a11e16907392fe43137879cce5b17cf54
Peng Huang <penghuang@chromium.org>
2021-02-02 16:53:40 +0000

    Prepare finching ANGLE's Vuklan backend
    * Add crash key egl-display-type which can be used to identify
      EGL implementation and ANGLE's backends (GL, Vulkan, etc)
    * Only enable ANGLE's Vulkan backend finch for Android Q and newer
      devices.
    
    Bug: 1170339
    Change-Id: I191c4dfcd4400996d40651607b32561e64c4d2b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2661716
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849636}

--- ui/gl/gl_switches.cc
+++ ui/gl/gl_switches.cc
@@ -6,6 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_switches.h"
 
 #include "base/stl_util.h"
+#include "build/build_config.h"
+
+#if defined(OS_ANDROID)
+#include "base/android/build_info.h"
+#endif
 
 namespace gl {
 
@@ -234,5 +239,16 @@ const base::Feature kDefaultANGLEVulkan{"DefaultANGLEVulkan",
 // be reliably retrieved with ANGLE backend.
 const base::Feature kTrackCurrentShaders{"TrackCurrentShaders",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
+
+bool IsDefaultANGLEVulkan() {
+#if defined(OS_ANDROID)
+  // No support for devices before Q -- exit before checking feature flags
+  // so that devices are not counted in finch trials.
+  if (base::android::BuildInfo::GetInstance()->sdk_int() <
+      base::android::SDK_VERSION_Q)
+    return false;
+#endif  // defined(OS_ANDROID)
+  return base::FeatureList::IsEnabled(kDefaultANGLEVulkan);
+}
 
 }  // namespace features
