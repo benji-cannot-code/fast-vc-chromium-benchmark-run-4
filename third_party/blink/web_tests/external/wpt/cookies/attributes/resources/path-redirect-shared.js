a4ec5cb3bfaa779c96f3076e60c600820204c847
Mike Taylor <miketaylr@chromium.org>
2021-02-02 15:49:10 +0000

    Rewrite wpt/cookies/http-state/domain-tests.html
    Here's a mapping from the domains the legacy tests expected to their
    current equivalent, as used in the tests:
    
    example.org =>                {{host}}
    home.example.org =>           {{domains[www]}}
    sibling.example.org =>        {{domains[www1]}}
    subdomain.home.example.org => {{domains[www2.www]}}
    
    Here's a mapping from the legacy http-state/domain-tests.html to their
    new location which will have a cookie with some variation of test=N in
    it.
    
    (All found in wpt/cookies/attributes/resources/domain-child.sub.html)
    
    (Apologies to future readers, it can be a little confusing to follow)
    
    domain0001 => test=1
    domain0002 => test=2
    domain0003 => test=3
    domain0004 => test=4
    domain0005 => test=5
    domain0006 => test=6
    domain0007 => test=7
    domain0008 => test=8
    domain0009 => test=9
    domain0010 => test=10
    domain0011 => test=11
    domain0012 => test=12
    domain0013 => test=13
    domain0014 => test=14
    domain0015 => test=15
    domain0016 => test=16
    domain0017 => test=17
    domain0018 => test=18
    domain0019 => test=19
    domain0020 => test=20
    domain0021 => test=21
    domain0022 => test=22
    domain0023 => test=23
    domain0024 => test=24
    domain0025 => test=25
    domain0026 => test=26
    domain0027 => test=27
    domain0028 => test=28
    domain0029 => test=29
    optional-domain0030 => test=30
    domain0031 => Duplicate of domain0025
    domain0032 => (this doesn't exist, don't go hunting for it)
    domain0033 => test=31
    domain0034 => test=32
    domain0035 => test=33
    domain0036 => test=34
    domain0037 => test=35
    domain0038 => test=36
    domain0039 => test=37
    domain0040 => test=38
    domain0041 => test=39
    domain0042 => test=40
    optional-domain0041 => test=41
    optional-domain0042 => test=42
    optional-domain0043 => test=43
    
    And here's some leftovers:
    mozilla0011 => test=26 (wpt/cookies/attributes/invalid.html)
    
    Bug: 1159858
    Change-Id: I7e5d0bb4a0dd2cc77993a3a10ca0a619f763d44c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2654317
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849610}

--- third_party/blink/web_tests/external/wpt/cookies/attributes/resources/path-redirect-shared.js
+++ third_party/blink/web_tests/external/wpt/cookies/attributes/resources/path-redirect-shared.js
@@ -1,8 +1,23 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-window.expireCookie = (cookie) => {
+function expireCookie(cookie) {
   const cookies = Array.isArray(cookie) ? cookie : [cookie];
   for (let c of cookies) {
     document.cookie = c += "; max-age=0";
   }
 }
-window.getCookies = () => document.cookie;
+
+function getCookies() {
+  return document.cookie;
+}
+
+window.addEventListener("message", (e) => {
+  if (e.data == "getCookies") {
+    const cookies = getCookies();
+    e.source.postMessage({"cookies": cookies}, '*');
+  }
+
+  if (typeof e.data == "object" && 'expireCookie' in e.data) {
+    expireCookie(e.data.expireCookie);
+    e.source.postMessage("expired", '*');
+  }
+});
