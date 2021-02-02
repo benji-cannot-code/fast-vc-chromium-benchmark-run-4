ed38a0fe9812b565c1f171d5efe8f34e73ca74d3
Benoit Lize <lizeb@chromium.org>
2021-02-02 18:04:25 +0000

    [base] bits::Align() -> bits::AlignUp().
    to the new call. The old one is still there, as there are many callers
    across the codebase.
    
    bits: :Align() is confusing, and not uniform (the opposite is called
    bits: :AlignDown()). Make naming consistent, and convert base/allocator
    Change-Id: I7f94e9a41665107c109bc120bca56232d1f5cbd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2550796
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849665}

--- base/allocator/partition_allocator/partition_page.cc
+++ base/allocator/partition_allocator/partition_page.cc
@@ -176,7 +176,7 @@ void SlotSpanMetadata<thread_safe>::Decommit(PartitionRoot<thread_safe>* root) {
   size_t size_to_decommit =
 #if defined(OS_WIN)
       // Windows uses lazy commit, thus only provisioned slots are committed.
-      bits::Align(GetProvisionedSize(), SystemPageSize());
+      bits::AlignUp(GetProvisionedSize(), SystemPageSize());
 #else
       bucket->get_bytes_per_span();
 #endif
