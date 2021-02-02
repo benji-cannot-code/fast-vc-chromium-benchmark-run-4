dcc311ba6a8c8d65f72eb594a3bece81aa06fb84
Ian Clelland <iclelland@chromium.org>
2021-02-02 18:35:01 +0000

    Revert "[Secure Payment Confirmation] Enable enrolling from an iframe."
    This reverts commit 3304e4dee3abb86a4692fe22d8cdad6d05d7f85c.
    
    Reason for revert: This is causing a failure on a ChromeOS bot here: https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-dbg/22447
    
    It may be the the check to base::FeatureList::IsEnabled(features::kSecurePaymentConfirmation) is somehow interacting with https://source.chromium.org/chromium/chromium/src/+/master:components/payments/content/payment_app_service.cc;l=38 to cause the feature identity check to fail.
    
    Original change's description:
    > [Secure Payment Confirmation] Enable enrolling from an iframe.
    >
    > Before this patch, no WebAuthn credential could be created from within a
    > cross origin iframe.
    >
    > This patch adds a boolean field `is_payment_credential_creation` to the
    > mojo parameters for making credentials and special cases WebAuthn to
    > allow making credentials when that boolean is true. Blink sets that
    > boolean to true when the website is creating a payment credential and
    > the execution context has the "payment" feature policy enabled and the
    > "SecurePaymentConfirmation" feature is enabled.
    >
    > After this patch, a cross origin iframe with "payment" feature policy
    > can create payment credentials in WebAuthn, while all other types of
    > credentials are still prohibited from being created in a cross origin
    > iframe, regardless of feature policy.
    >
    > Bug: 1173184
    > Change-Id: I852c31babe858b825365d24781b00589b3940d0a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2655605
    > Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#849597}
    
    TBR=kenrb@chromium.org,rouslan@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I3c8181c2115ae6bb703eae6d129fdd284c4fa4f2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1173184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2669027
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849677}

--- content/browser/webauth/webauth_request_security_checker.cc
+++ content/browser/webauth/webauth_request_security_checker.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
-#include "components/payments/core/features.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/public/browser/render_frame_host.h"
 #include "device/fido/features.h"
@@ -136,16 +135,12 @@ WebAuthRequestSecurityChecker::ValidateAncestorOrigins(
     RequestType type,
     bool* is_cross_origin) {
   *is_cross_origin = !IsSameOriginWithAncestors(origin);
-  if ((type != RequestType::kGetAssertion ||
+  if ((type == RequestType::kMakeCredential ||
        !base::FeatureList::IsEnabled(
            device::kWebAuthGetAssertionFeaturePolicy) ||
-       !render_frame_host_->IsFeatureEnabled(
-           blink::mojom::FeaturePolicyFeature::kPublicKeyCredentialsGet)) &&
-      (type != RequestType::kMakePaymentCredential ||
-       !base::FeatureList::IsEnabled(
-           payments::features::kSecurePaymentConfirmation) ||
-       !render_frame_host_->IsFeatureEnabled(
-           blink::mojom::FeaturePolicyFeature::kPayment)) &&
+       !static_cast<RenderFrameHostImpl*>(render_frame_host_)
+            ->IsFeatureEnabled(blink::mojom::FeaturePolicyFeature::
+                                   kPublicKeyCredentialsGet)) &&
       *is_cross_origin) {
     return blink::mojom::AuthenticatorStatus::NOT_ALLOWED_ERROR;
   }
--- third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
@@ -866,8 +866,6 @@ void CreatePublicKeyCredentialForPaymentCredential(
     return;
   }
 
-  mojo_options->is_payment_credential_creation = true;
-
   auto* authenticator =
       CredentialManagerProxy::From(resolver->GetScriptState())->Authenticator();
   authenticator->MakeCredential(
@@ -1165,9 +1163,7 @@ ScriptPromise CredentialsContainer::create(
         FederatedCredential::Create(options->federated(), exception_state));
   } else if (options->hasPayment()) {
     if (RuntimeEnabledFeatures::SecurePaymentConfirmationEnabled(
-            resolver->GetExecutionContext()) &&
-        resolver->GetExecutionContext()->IsFeatureEnabled(
-            mojom::blink::FeaturePolicyFeature::kPayment)) {
+            resolver->GetExecutionContext())) {
       CreatePublicKeyCredentialForPaymentCredential(options->payment(),
                                                     resolver);
     } else {
