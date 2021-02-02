331a6a547ccad823b5aff767c5130e06d5d0a28f
Tsuyoshi Horo <horo@chromium.org>
2021-02-02 04:16:52 +0000

    Revert "Introduce WebBundleChunkedBuffer to avoid inefficient memory copy"
    This reverts commit bb4ff89469ab95e8d8826b27db886fdc321a2b72.
    
    Reason for revert: Unintentionally included content_browser_client.cc 
    
    Original change's description:
    > Introduce WebBundleChunkedBuffer to avoid inefficient memory copy
    >
    > This CL introduces WebBundleChunkedBuffer which keeps the appended bytes
    > as a RefCountedBytes, so we don't need to copy the bytes while creating
    > a DataSource to read the data using a DataPipeProducer.
    >
    > Bug: 1082020
    > Change-Id: Ib70cd5c453450fb488cb7c45027c80483c875204
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2658698
    > Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    > Reviewed-by: Hayato Ito <hayato@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#849429}
    
    TBR=horo@chromium.org,kinuko@chromium.org,hayato@chromium.org,ksakamoto@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I6fd58cb3adba91b3476d146493a1ee28434b489d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1082020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2666649
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849433}

--- services/network/web_bundle_url_loader_factory.cc
+++ services/network/web_bundle_url_loader_factory.cc
@@ -14,13 +14,39 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/system/data_pipe_producer.h"
 #include "net/http/http_status_code.h"
 #include "services/network/public/cpp/cross_origin_read_blocking.h"
-#include "services/network/web_bundle_chunked_buffer.h"
 
 namespace network {
 
 namespace {
 
 constexpr size_t kBlockedBodyAllocationSize = 1;
+
+class PipeDataSource : public mojo::DataPipeProducer::DataSource {
+ public:
+  explicit PipeDataSource(std::vector<uint8_t> data) : data_(std::move(data)) {}
+  uint64_t GetLength() const override { return data_.size(); }
+
+  ReadResult Read(uint64_t uint64_offset, base::span<char> buffer) override {
+    ReadResult result;
+    if (uint64_offset > data_.size()) {
+      result.result = MOJO_RESULT_OUT_OF_RANGE;
+      return result;
+    }
+    size_t offset = base::checked_cast<size_t>(uint64_offset);
+    size_t len = std::min(data_.size() - offset, buffer.size());
+    if (len > 0) {
+      DCHECK_LT(offset, data_.size());
+      memcpy(buffer.data(), &data_[offset], len);
+    }
+    result.bytes_read = len;
+    return result;
+  }
+
+ private:
+  // Since mojo::DataPipeProducer runs in its own sequence, we can't just have
+  // a reference to the buffer in BundleDataSource.
+  std::vector<uint8_t> data_;
+};
 
 void DeleteProducerAndRunCallback(
     std::unique_ptr<mojo::DataPipeProducer> producer,
@@ -250,7 +276,7 @@ class WebBundleURLLoaderFactory::BundleDataSource
                       uint64_t length,
                       ReadToDataPipeCallback callback) {
     TRACE_EVENT0("loading", "BundleDataSource::ReadToDataPipe");
-    if (!finished_loading_ && !buffer_.ContainsAll(offset, length)) {
+    if (!finished_loading_ && offset + length > data_.size()) {
       // Current implementation does not support progressive loading of inner
       // response body.
       PendingReadToDataPipe pending;
@@ -262,17 +288,9 @@ class WebBundleURLLoaderFactory::BundleDataSource
       return;
     }
 
-    auto data_source = buffer_.CreateDataSource(offset, length);
-    if (!data_source) {
-      // When there is no body to send, returns OK here without creating a
-      // DataPipeProducer.
-      std::move(callback).Run(MOJO_RESULT_OK);
-      return;
-    }
-
     auto writer = std::make_unique<mojo::DataPipeProducer>(std::move(producer));
     mojo::DataPipeProducer* raw_writer = writer.get();
-    raw_writer->Write(std::move(data_source),
+    raw_writer->Write(std::make_unique<PipeDataSource>(GetData(offset, length)),
                       base::BindOnce(&DeleteProducerAndRunCallback,
                                      std::move(writer), std::move(callback)));
   }
@@ -280,7 +298,7 @@ class WebBundleURLLoaderFactory::BundleDataSource
   // mojom::BundleDataSource
   void Read(uint64_t offset, uint64_t length, ReadCallback callback) override {
     TRACE_EVENT0("loading", "BundleDataSource::Read");
-    if (!finished_loading_ && !buffer_.ContainsAll(offset, length)) {
+    if (!finished_loading_ && offset + length > data_.size()) {
       PendingRead pending;
       pending.offset = offset;
       pending.length = length;
@@ -288,19 +306,16 @@ class WebBundleURLLoaderFactory::BundleDataSource
       pending_reads_.push_back(std::move(pending));
       return;
     }
-
-    uint64_t out_len = buffer_.GetAvailableLength(offset, length);
-    std::vector<uint8_t> output(base::checked_cast<size_t>(out_len));
-    buffer_.ReadData(offset, out_len, output.data());
-    std::move(callback).Run(std::move(output));
+    std::move(callback).Run(GetData(offset, length));
   }
 
   // mojo::DataPipeDrainer::Client
   void OnDataAvailable(const void* data, size_t num_bytes) override {
     DCHECK(!finished_loading_);
+    const uint8_t* data_uint8 = reinterpret_cast<const uint8_t*>(data);
     // TODO(crbug.com/1082020): Set a threshold for buffer size, so that Network
     // Service does not use memory indefinitely.
-    buffer_.Append(reinterpret_cast<const uint8_t*>(data), num_bytes);
+    data_.insert(data_.end(), data_uint8, data_uint8 + num_bytes);
     ProcessPendingReads();
   }
 
@@ -325,6 +340,18 @@ class WebBundleURLLoaderFactory::BundleDataSource
     }
   }
 
+  std::vector<uint8_t> GetData(uint64_t uint64_offset, uint64_t uint64_length) {
+    size_t offset = base::checked_cast<size_t>(uint64_offset);
+    size_t length = base::checked_cast<size_t>(uint64_length);
+    if (offset >= data_.size())
+      return {};
+    if (length > data_.size() - offset)
+      length = data_.size() - offset;
+
+    std::vector<uint8_t> output(length);
+    memcpy(output.data(), data_.data() + offset, length);
+    return output;
+  }
 
   struct PendingRead {
     uint64_t offset;
@@ -339,7 +366,7 @@ class WebBundleURLLoaderFactory::BundleDataSource
   };
 
   mojo::Receiver<web_package::mojom::BundleDataSource> data_source_receiver_;
-  WebBundleChunkedBuffer buffer_;
+  std::vector<uint8_t> data_;
   std::vector<PendingRead> pending_reads_;
   std::vector<PendingReadToDataPipe> pending_reads_to_data_pipe_;
   bool finished_loading_ = false;
