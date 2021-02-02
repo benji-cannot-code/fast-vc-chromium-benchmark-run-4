450a420b91e82a008e13aeb249439e93f2d5fc26
Daniel Hosseinian <dhoss@chromium.org>
2021-02-02 22:22:10 +0000

    [unseasoned-pdf] Migrate rotation handling
    Migrate from a Pepper OutOfProcessInstance implementation to a common
    PdfViewPluginBase one.
    
    Bug: 1109796
    Change-Id: I1299c6a0bb712a29d8ed2bf9bdfee9b53bc3eddc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2666846
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Hui Yingst <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849792}

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -207,9 +207,6 @@ constexpr char kJSEmailCc[] = "cc";
 constexpr char kJSEmailBcc[] = "bcc";
 constexpr char kJSEmailSubject[] = "subject";
 constexpr char kJSEmailBody[] = "body";
-// Rotation (Page -> Plugin)
-constexpr char kJSRotateClockwiseType[] = "rotateClockwise";
-constexpr char kJSRotateCounterclockwiseType[] = "rotateCounterclockwise";
 // Select all text in the document (Page -> Plugin)
 constexpr char kJSSelectAllType[] = "selectAll";
 // Get the selected text in the document (Page -> Plugin)
@@ -713,10 +710,6 @@ void OutOfProcessInstance::HandleMessage(const pp::Var& message) {
     HandleSaveAttachmentMessage(dict);
   } else if (type == kJSSaveType) {
     HandleSaveMessage(dict);
-  } else if (type == kJSRotateClockwiseType) {
-    RotateClockwise();
-  } else if (type == kJSRotateCounterclockwiseType) {
-    RotateCounterclockwise();
   } else if (type == kJSSelectAllType) {
     engine()->SelectAll();
   } else if (type == kJSResetPrintPreviewModeType) {
