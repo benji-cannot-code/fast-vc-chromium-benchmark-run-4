96c96ab7fa816a54207ff711276eb2b80ec3cde8
Aleks Totic <atotic@chromium.org>
2021-02-02 23:00:18 +0000

    [TablesNG] max-content table sizing + %ge cells
    Currently, %ge cells + table width: max-content
    can result in "too wide table".
    This is because td.width:100% will cause GridInlineMinMax
    to be kTableMaxInlineSize.
    The solution is not to use percentages when tabl is max-content.
    FF does this too.
    
    Bug: 1172498
    Change-Id: I04d4b686917da97856004745003dfe7fe94367a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2660565
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849819}

--- third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm.cc
@@ -107,7 +107,7 @@ LayoutUnit ComputeAssignableTableInlineSize(
   const MinMaxSizes grid_min_max =
       NGTableAlgorithmHelpers::ComputeGridInlineMinMax(
           table, column_constraints, undistributable_space, is_fixed_layout,
-          /* allow_column_percentages */ true,
+          /* is_layout_pass */ true,
           /* skip_collapsed_columns */ false);
 
   // Standard: "used width of the table".
@@ -500,7 +500,7 @@ MinMaxSizesResult NGTableLayoutAlgorithm::ComputeMinMaxSizes(
   const MinMaxSizes grid_min_max =
       NGTableAlgorithmHelpers::ComputeGridInlineMinMax(
           Node(), *column_constraints, undistributable_space, is_fixed_layout,
-          /* allow_column_percentages */ false,
+          /* is_layout_pass */ false,
           /* skip_collapsed_columns */ true);
 
   MinMaxSizes min_max{
