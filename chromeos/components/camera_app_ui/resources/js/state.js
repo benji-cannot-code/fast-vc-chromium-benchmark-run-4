0afbaaf005dfd7af93391c6e94a2ddeb76de91d1
Kuo Jen Wei <inker@google.com>
2021-02-02 03:15:38 +0000

    CCA: Add video profile and bitrate options in expert mode
    Add new profile options and bitrate bar for choosing video encode
    options. The h264 encode level is determined by minimal supported level
    under selected profile and bitrate constraints.
    
    Bug: 151047420
    Test: tast run "camera.CCAUI*"
    
    Change-Id: Ic2eebb89679735298d8d0e4ede25026fecaaeb85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2467776
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849410}

--- chromeos/components/camera_app_ui/resources/js/state.js
+++ chromeos/components/camera_app_ui/resources/js/state.js
@@ -17,6 +17,7 @@ import {
 export const State = {
   CAMERA_CONFIGURING: 'camera-configuring',
   CAMERA_SWITCHING: 'camera-switching',
+  CUSTOM_VIDEO_PARAMETERS: 'custom-video-parameters',
   EXPERT: 'expert',
   FPS_30: 'fps-30',
   FPS_60: 'fps-60',
