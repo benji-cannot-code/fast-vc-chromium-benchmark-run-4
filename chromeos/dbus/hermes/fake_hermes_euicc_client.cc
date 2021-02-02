5a891eea72d8d7e37e5bc859720766671fcffade
Azeem Arshad <azeemarshad@chromium.org>
2021-02-02 21:58:48 +0000

    [CrOS Cellular] Add CellularESimUninstallHandler
    This CL adds a CellularESimUninstallHandler class that handles
    uninstallation of eSIM networks via Hermes and Shill.
    
    This class will be used in ESimProfile class in a followup
    CL.
    
    Bug: 1093185
    Change-Id: Ic1b33253c669b291bcf93ff3cfcdc6ab49ca9040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2660756
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849788}

--- chromeos/dbus/hermes/fake_hermes_euicc_client.cc
+++ chromeos/dbus/hermes/fake_hermes_euicc_client.cc
@@ -418,7 +418,6 @@ void FakeHermesEuiccClient::DoUninstallProfile(
     return;
   }
 
-  RemoveCellularService(carrier_profile_path);
   installed_profiles.erase(it);
   euicc_properties->installed_carrier_profiles().ReplaceValue(
       installed_profiles);
@@ -466,14 +465,6 @@ void FakeHermesEuiccClient::CreateCellularService(
                            service_path);
 }
 
-void FakeHermesEuiccClient::RemoveCellularService(
-    const dbus::ObjectPath& carrier_profile_path) {
-  ShillServiceClient::TestInterface* service_test =
-      ShillServiceClient::Get()->GetTestInterface();
-  service_test->RemoveService(profile_service_path_map_[carrier_profile_path]);
-  profile_service_path_map_.erase(carrier_profile_path);
-}
-
 void FakeHermesEuiccClient::CallNotifyPropertyChanged(
     const dbus::ObjectPath& object_path,
     const std::string& property_name) {
@@ -499,6 +490,7 @@ void FakeHermesEuiccClient::QueueInstalledProfile(
   auto iter = installed_profile_queue_map_.find(euicc_path);
   if (iter != installed_profile_queue_map_.end()) {
     iter->second->push(profile_path);
+    return;
   }
 
   std::unique_ptr<InstalledProfileQueue> installed_profile_queue =
