8b113a5a1ffc544a5859a74409c8624f80e91238
Hazem Ashmawy <hazems@chromium.org>
2021-02-02 18:55:11 +0000

    Componentize BackgroundTaskUpdateScheduler under component_updater
    No change in logic, Just fleshing out BackgroundTaskUpdateScheduler out
    of chrome and move it under components/component_updater/android so that
    WebView can share the implementation with chrome.
    
    Bug: 1170729
    Change-Id: I310501ebc0e13632ec404871ec81d511bf07a77f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2658638
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849687}

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -153,9 +153,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_ANDROID)
-#include "chrome/browser/android/component_updater/background_task_update_scheduler.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/ssl/chrome_security_state_client.h"
+#include "components/component_updater/android/background_task_update_scheduler.h"
 #else
 #include "chrome/browser/devtools/devtools_auto_opener.h"
 #include "chrome/browser/gcm/gcm_product_util.h"
--- components/component_updater/android/java/src/org/chromium/components/component_updater/UpdateScheduler.java
+++ components/component_updater/android/java/src/org/chromium/components/component_updater/UpdateScheduler.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.component_updater;
+package org.chromium.components.component_updater;
 
 import android.os.Build;
 
