e96be9d66003c897ae7f63e91011e887225f3d47
Roman Arora <romanarora@chromium.org>
2021-02-02 18:39:34 +0000

    WebUI Bubble View: Max height based on widget work bounds.
    Bug: 1158092
    Change-Id: I716183f566eff8bbea6c956bcaa95fa2f349b7a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2665750
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849680}

--- chrome/browser/ui/views/bubble/webui_bubble_dialog_view.cc
+++ chrome/browser/ui/views/bubble/webui_bubble_dialog_view.cc
@@ -12,12 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// The min / max size available to the WebBubbleDialogView.
-// These are arbitrary sizes that match those set by ExtensionPopup.
-// TODO(tluk): Determine the correct size constraints for the
-// WebBubbleDialogView.
+// The min size available to the WebBubbleDialogView. These are arbitrary sizes
+// that match those set by ExtensionPopup.
 constexpr gfx::Size kMinSize(25, 25);
-constexpr gfx::Size kMaxSize(800, 600);
 
 }  // namespace
 
@@ -55,7 +52,7 @@ gfx::Size WebUIBubbleDialogView::CalculatePreferredSize() const {
   // Constrain the size to popup min/max.
   gfx::Size preferred_size = BubbleDialogDelegateView::CalculatePreferredSize();
   preferred_size.SetToMax(kMinSize);
-  preferred_size.SetToMin(kMaxSize);
+  preferred_size.SetToMin(GetWidget()->GetWorkAreaBoundsInScreen().size());
   return preferred_size;
 }
 
