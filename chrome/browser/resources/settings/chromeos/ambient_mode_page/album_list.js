f5924c0a70eb1d77316981cccb44eb7e5ebe6d0b
Qasid Sadiq <qasid@google.com>
2021-02-02 07:17:11 +0000

    ambient: Prevent albums from overlapping
    This involves a) Limiting the number of lines both the album title, and
    subtitle can have. The spec seems to indicate it should be 1 and 2
    respectively.
    b) Explicitly specifying item height based on the requirements of
    iron-lists a grid-layout.
    
    The bug is caused by an item having a title or subtitle long
    enough that the album item is larger than the first album item.
    iron-list in grid layout determines the height of each item based on the
    offsetHeight of the first item. If there are variations, weird things
    like this happen.
    
    Because we are hiding long titles, we need to add tooltips
    conditionally. To avoid the paper tooltip being clipped. By another
    album, we need to increase the z-index on the currently hovered grid
    item. The reason is that iron-list calls translate on each item,
    creating a separate stacking context, so setting z-index on the
    paper-tooltip won't change it's z position relative to other grid items.
    Couldn't find a clean way to get callbacks from paper-tooltip on when
    the animation ended and began, so I have to adjust the z-index on mouse
    enter and mouse exit with a delay specified by paper-tooltip.
    
    Screenshot with bug: https://screenshot.googleplex.com/BDtRpmR78qCFG5J.png
    Screenshot with fix: https://screenshot.googleplex.com/7ugoAAbTvvWCDxV.png
    
    Bug: b/176834712
    Test: Added several albums of photos with varying title lengths. And
    tested it by hand. For the subtitle I hardcoded longer and shorter
    lines. Also tried the Art Gallery UI.
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Iedf0698c016ca7f6c5d58c6d51277799e2a94d05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631450
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Qasid Sadiq <qasid@google.com>
    Auto-Submit: Qasid Sadiq <qasid@google.com>
    Cr-Commit-Position: refs/heads/master@{#849484}

--- chrome/browser/resources/settings/chromeos/ambient_mode_page/album_list.js
+++ chrome/browser/resources/settings/chromeos/ambient_mode_page/album_list.js
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'album-list',
 
+  behaviors: [
+    settings.GlobalScrollTargetBehavior,
+  ],
+
   properties: {
     /** @private {!AmbientModeTopicSource} */
     topicSource: {
@@ -27,6 +31,15 @@ Polymer({
       type: Array,
       value: null,
       notify: true,
+    },
+
+    /**
+     * Needed by GlobalScrollTargetBehavior.
+     * @override
+     */
+    subpageRoute: {
+      type: Object,
+      value: settings.routes.AMBIENT_MODE_PHOTOS,
     },
   },
 
