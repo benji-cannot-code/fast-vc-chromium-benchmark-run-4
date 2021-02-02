149b770aeebce75cef1094816fa441666af45e90
Hidehiko Abe <hidehiko@chromium.org>
2021-02-02 03:06:00 +0000

    Support multi crosapi clients.
    This CL introduces BrowserServiceHostAsh, which stores multiple
    Remote<BrowserService> instances.
    
    BUG=1148448
    TEST=Ran on DUT. Tryjob.
    
    Change-Id: I13c12fd52d913c43c909fb80556e8f22b59e3968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2659457
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849402}

--- chrome/browser/chromeos/crosapi/browser_manager.cc
+++ chrome/browser/chromeos/crosapi/browser_manager.cc
@@ -33,7 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/crosapi/browser_loader.h"
+#include "chrome/browser/chromeos/crosapi/browser_service_host_ash.h"
 #include "chrome/browser/chromeos/crosapi/browser_util.h"
+#include "chrome/browser/chromeos/crosapi/crosapi_ash.h"
 #include "chrome/browser/chromeos/crosapi/crosapi_manager.h"
 #include "chrome/browser/chromeos/crosapi/environment_provider.h"
 #include "chrome/browser/chromeos/crosapi/test_mojo_connection_manager.h"
@@ -166,6 +168,14 @@ BrowserManager::BrowserManager(
   if (session_manager::SessionManager::Get())
     session_manager::SessionManager::Get()->AddObserver(this);
 
+  // CrosapiManager may not be initialized on unit testing.
+  if (CrosapiManager::IsInitialized()) {
+    CrosapiManager::Get()
+        ->crosapi_ash()
+        ->browser_service_host_ash()
+        ->AddObserver(this);
+  }
+
   std::string socket_path =
       base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
           chromeos::switches::kLacrosMojoSocketForTesting);
@@ -177,6 +187,13 @@ BrowserManager::BrowserManager(
 }
 
 BrowserManager::~BrowserManager() {
+  if (CrosapiManager::IsInitialized()) {
+    CrosapiManager::Get()
+        ->crosapi_ash()
+        ->browser_service_host_ash()
+        ->RemoveObserver(this);
+  }
+
   // Unregister, just in case the manager is destroyed before
   // OnUserSessionStarted() is called.
   if (session_manager::SessionManager::Get())
@@ -231,38 +248,42 @@ void BrowserManager::NewWindow() {
     return;
   }
 
-  DCHECK(browser_service_.is_connected());
-  browser_service_->NewWindow(base::DoNothing());
+  if (!browser_service_.has_value()) {
+    LOG(ERROR) << "BrowserService was disconnected";
+    return;
+  }
+
+  browser_service_->service->NewWindow(base::DoNothing());
 }
 
 bool BrowserManager::GetFeedbackDataSupported() const {
-  return browser_service_.is_bound() && browser_service_.is_connected() &&
-         browser_service_.version() >= kGetFeedbackDataMinVersion;
+  return browser_service_.has_value() &&
+         browser_service_->interface_version >= kGetFeedbackDataMinVersion;
 }
 
 void BrowserManager::GetFeedbackData(GetFeedbackDataCallback callback) {
   DCHECK(GetFeedbackDataSupported());
-  browser_service_->GetFeedbackData(std::move(callback));
+  browser_service_->service->GetFeedbackData(std::move(callback));
 }
 
 bool BrowserManager::GetHistogramsSupported() const {
-  return browser_service_.is_bound() && browser_service_.is_connected() &&
-         browser_service_.version() >= kGetHistogramsMinVersion;
+  return browser_service_.has_value() &&
+         browser_service_->interface_version >= kGetHistogramsMinVersion;
 }
 
 void BrowserManager::GetHistograms(GetHistogramsCallback callback) {
   DCHECK(GetHistogramsSupported());
-  browser_service_->GetHistograms(std::move(callback));
+  browser_service_->service->GetHistograms(std::move(callback));
 }
 
 bool BrowserManager::GetActiveTabUrlSupported() const {
-  return browser_service_.is_bound() && browser_service_.is_connected() &&
-         browser_service_.version() >= kGetActiveTabUrlMinVersion;
+  return browser_service_.has_value() &&
+         browser_service_->interface_version >= kGetActiveTabUrlMinVersion;
 }
 
 void BrowserManager::GetActiveTabUrl(GetActiveTabUrlCallback callback) {
   DCHECK(GetActiveTabUrlSupported());
-  browser_service_->GetActiveTabUrl(std::move(callback));
+  browser_service_->service->GetActiveTabUrl(std::move(callback));
 }
 
 void BrowserManager::AddObserver(BrowserManagerObserver* observer) {
@@ -272,6 +293,21 @@ void BrowserManager::AddObserver(BrowserManagerObserver* observer) {
 void BrowserManager::RemoveObserver(BrowserManagerObserver* observer) {
   observers_.RemoveObserver(observer);
 }
+
+BrowserManager::BrowserServiceInfo::BrowserServiceInfo(
+    mojo::RemoteSetElementId mojo_id,
+    mojom::BrowserService* service,
+    uint32_t interface_version)
+    : mojo_id(mojo_id),
+      service(service),
+      interface_version(interface_version) {}
+
+BrowserManager::BrowserServiceInfo::BrowserServiceInfo(
+    const BrowserServiceInfo&) = default;
+BrowserManager::BrowserServiceInfo&
+BrowserManager::BrowserServiceInfo::operator=(const BrowserServiceInfo&) =
+    default;
+BrowserManager::BrowserServiceInfo::~BrowserServiceInfo() = default;
 
 void BrowserManager::Start() {
   DCHECK_EQ(state_, State::STOPPED);
@@ -392,15 +428,14 @@ void BrowserManager::StartWithLogFile(base::ScopedFD logfd) {
   base::CommandLine command_line(argv);
   LOG(WARNING) << "Launching lacros with command: "
                << command_line.GetCommandLineString();
+
+  // Prepare to invite lacros-chrome to the Mojo universe of Crosapi.
   mojo::PlatformChannel channel;
   channel.PrepareToPassRemoteEndpoint(&options, &command_line);
-
-  // TODO(crbug.com/1124490): Support multiple mojo connections from lacros.
-  CrosapiManager::Get()->SendInvitation(
+  DCHECK(!crosapi_id_.has_value());
+  crosapi_id_ = CrosapiManager::Get()->SendInvitation(
       environment_provider_.get(), channel.TakeLocalEndpoint(),
       base::BindOnce(&BrowserManager::OnMojoDisconnected,
-                     weak_factory_.GetWeakPtr()),
-      base::BindOnce(&BrowserManager::OnCrosapiConnected,
                      weak_factory_.GetWeakPtr()));
 
   // Create the lacros-chrome subprocess.
@@ -419,12 +454,22 @@ void BrowserManager::StartWithLogFile(base::ScopedFD logfd) {
   channel.RemoteProcessLaunchAttempted();
 }
 
-void BrowserManager::OnCrosapiConnected(
-    mojo::Remote<mojom::BrowserService> browser_service) {
-  DCHECK_EQ(state_, State::STARTING);
+void BrowserManager::OnBrowserServiceConnected(
+    CrosapiId id,
+    mojo::RemoteSetElementId mojo_id,
+    mojom::BrowserService* browser_service,
+    uint32_t browser_service_version) {
+  if (id != crosapi_id_) {
+    // This BrowserService is unrelated to this instance. Skipping.
+    return;
+  }
 
+  DCHECK_EQ(state_, State::STARTING);
   state_ = State::RUNNING;
-  browser_service_ = std::move(browser_service);
+
+  DCHECK(!browser_service_.has_value());
+  browser_service_ =
+      BrowserServiceInfo{mojo_id, browser_service, browser_service_version};
   base::UmaHistogramMediumTimes("ChromeOS.Lacros.StartTime",
                                 base::TimeTicks::Now() - lacros_launch_time_);
   // Set the launch-on-login pref every time lacros-chrome successfully starts,
@@ -434,6 +479,15 @@ void BrowserManager::OnCrosapiConnected(
   LOG(WARNING) << "Connection to lacros-chrome is established.";
 }
 
+void BrowserManager::OnBrowserServiceDisconnected(
+    CrosapiId id,
+    mojo::RemoteSetElementId mojo_id) {
+  // No need to check CrosapiId here, because |mojo_id| is unique within
+  // a process.
+  if (browser_service_.has_value() && browser_service_->mojo_id == mojo_id)
+    browser_service_.reset();
+}
+
 void BrowserManager::OnMojoDisconnected() {
   DCHECK(state_ == State::STARTING || state_ == State::RUNNING);
   LOG(WARNING)
@@ -441,6 +495,7 @@ void BrowserManager::OnMojoDisconnected() {
   state_ = State::TERMINATING;
 
   browser_service_.reset();
+  crosapi_id_.reset();
   base::ThreadPool::PostTaskAndReply(
       FROM_HERE, {base::WithBaseSyncPrimitives()},
       base::BindOnce(&TerminateLacrosChrome, std::move(lacros_process_)),
--- chrome/browser/chromeos/crosapi/crosapi_ash.cc
+++ chrome/browser/chromeos/crosapi/crosapi_ash.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/browser/chromeos/crosapi/browser_manager.h"
+#include "chrome/browser/chromeos/crosapi/browser_service_host_ash.h"
 #include "chrome/browser/chromeos/crosapi/cert_database_ash.h"
 #include "chrome/browser/chromeos/crosapi/clipboard_ash.h"
 #include "chrome/browser/chromeos/crosapi/device_attributes_ash.h"
@@ -44,7 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace crosapi {
 
 CrosapiAsh::CrosapiAsh()
-    : device_attributes_ash_(std::make_unique<DeviceAttributesAsh>()),
+    : browser_service_host_ash_(std::make_unique<BrowserServiceHostAsh>()),
+      device_attributes_ash_(std::make_unique<DeviceAttributesAsh>()),
       file_manager_ash_(std::make_unique<FileManagerAsh>()),
       keystore_service_ash_(std::make_unique<KeystoreServiceAsh>()),
       message_center_ash_(std::make_unique<MessageCenterAsh>()),
@@ -73,8 +75,10 @@ CrosapiAsh::~CrosapiAsh() {
 
 void CrosapiAsh::BindReceiver(
     mojo::PendingReceiver<mojom::Crosapi> pending_receiver,
+    CrosapiId crosapi_id,
     base::OnceClosure disconnect_handler) {
-  mojo::ReceiverId id = receiver_set_.Add(this, std::move(pending_receiver));
+  mojo::ReceiverId id =
+      receiver_set_.Add(this, std::move(pending_receiver), crosapi_id);
   if (!disconnect_handler.is_null())
     disconnect_handler_map_.emplace(id, std::move(disconnect_handler));
 }
