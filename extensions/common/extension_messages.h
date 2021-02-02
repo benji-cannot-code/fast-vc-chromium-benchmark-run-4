e1bc87c2ee44a832a001b1c98950e6b8d636a610
Devlin Cronin <rdevlin.cronin@chromium.org>
2021-02-02 02:56:47 +0000

    [Extensions + IPC] Allow passing a single base::Value result over IPC
    Add IPC::ParamTraits<base::Value> to allow passing a single base::Value
    (instead of a base::ListValue) over IPC. Update
    ExtensionHostMsg_ExecuteCodeFinished to incorporate this.
    
    Bug: None
    Change-Id: Iff39cc07868caab668b2d663effe8e29f1e0ff67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653989
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849389}

--- extensions/common/extension_messages.h
+++ extensions/common/extension_messages.h
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/user_script.h"
 #include "extensions/common/view_type.h"
 #include "ipc/ipc_message_macros.h"
+#include "ipc/ipc_message_utils.h"
 #include "ui/accessibility/ax_param_traits.h"
 #include "url/gurl.h"
 #include "url/origin.h"
@@ -850,7 +851,10 @@ IPC_MESSAGE_ROUTED4(
     int /* request id */,
     std::string /* error; empty implies success */,
     GURL /* URL of the code executed on. May be empty if unsuccessful. */,
-    base::ListValue /* result of the script */)
+    base::Optional<base::Value> /* result of the script, if any. We use a
+                                   base::Optional<> here to differentiate
+                                   between no result (such as in the case of an
+                                   error) and a null result. */)
 
 // Sent from the renderer to the browser to notify that content scripts are
 // running in the renderer that the IPC originated from.
--- extensions/renderer/programmatic_script_injector.h
+++ extensions/renderer/programmatic_script_injector.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "base/optional.h"
 #include "base/values.h"
 #include "extensions/renderer/script_injection.h"
 #include "url/gurl.h"
@@ -78,8 +79,8 @@ class ProgrammaticScriptInjector : public ScriptInjector {
   // is used to provide user-friendly messages.
   std::string origin_for_about_error_;
 
-  // The results of the script execution.
-  base::ListValue results_;
+  // The result of the script execution.
+  base::Optional<base::Value> result_;
 
   // Whether or not this script injection has finished.
   bool finished_;
