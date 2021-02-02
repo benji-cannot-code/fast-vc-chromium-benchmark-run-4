5aaf32d5c17bbdf27cc10e5009b2aa40ff308072
sauski <sauski@google.com>
2021-02-02 18:30:44 +0000

    WebUI Settings: Improve recent_site_permissions_test.js clarity
    CL improves the clarity of the recent_site_permissions_test by avoiding
    strange line break formatting.
    
    Bug: 1127245
    Change-Id: I3fcdad1b81116fa11715c3b194c99bdb936e8ba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2665937
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#849676}

--- chrome/browser/resources/settings/site_settings_page/recent_site_permissions.js
+++ chrome/browser/resources/settings/site_settings_page/recent_site_permissions.js
@@ -216,7 +216,7 @@ Polymer({
    */
   getPermissionsText_({recentPermissions}) {
     // Recently changed permisisons for a site are grouped into three buckets,
-    // each desrcribed by a single sentence.
+    // each described by a single sentence.
     const groupSentences = [
       this.getPermissionGroupText_(
           'Allowed',
