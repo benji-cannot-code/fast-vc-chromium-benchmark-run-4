06f964bf79f2f808216f617b638ae67513884d4f
Brandon Wylie <wylieb@chromium.org>
2021-02-02 19:43:03 +0000

    Deflake an assistant voice search render test
    Bug: 1165682
    Change-Id: I73dde75452a986899782dd884dde24239a2309f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2660696
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849714}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/voice/AssistantVoiceSearchServiceRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/voice/AssistantVoiceSearchServiceRenderTest.java
@@ -28,7 +28,6 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -40,6 +39,7 @@ import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.components.externalauth.ExternalAuthUtils;
+import org.chromium.ui.test.util.DisableAnimationsTestRule;
 
 import java.io.IOException;
 
@@ -56,6 +56,9 @@ public class AssistantVoiceSearchServiceRenderTest {
             ChromeRenderTestRule.Builder.withPublicCorpus().build();
     @Rule
     public MockitoRule mMockitoRule = MockitoJUnit.rule();
+
+    @Rule
+    public DisableAnimationsTestRule mDisableAnimationsTestRule = new DisableAnimationsTestRule();
 
     @Mock
     GSAState mGsaState;
@@ -82,11 +85,10 @@ public class AssistantVoiceSearchServiceRenderTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/1165682")
     @CommandLineFlags.Add({"force-fieldtrial-params=Study.Group:colorful_mic/true"})
     @Feature({"RenderTest"})
     public void testAssistantColorfulMic() throws IOException {
-        mActivityTestRule.loadUrlInNewTab(UrlConstants.NTP_URL, /* incognito= */ false);
+        mActivityTestRule.loadUrl(UrlConstants.NTP_URL);
 
         mRenderTestRule.render(mActivityTestRule.getActivity().findViewById(R.id.ntp_content),
                 "avs_colorful_mic_unfocused_ntp");
@@ -101,7 +103,7 @@ public class AssistantVoiceSearchServiceRenderTest {
     @CommandLineFlags.Add({"force-fieldtrial-params=Study.Group:colorful_mic/false"})
     @Feature({"RenderTest"})
     public void testAssistantMic() throws IOException {
-        mActivityTestRule.loadUrlInNewTab(UrlConstants.NTP_URL, /* incognito= */ false);
+        mActivityTestRule.loadUrl(UrlConstants.NTP_URL);
 
         mRenderTestRule.render(mActivityTestRule.getActivity().findViewById(R.id.ntp_content),
                 "avs__mic_unfocused_ntp");
