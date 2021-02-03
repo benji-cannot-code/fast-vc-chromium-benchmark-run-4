c6a9c99c09f16af39e000a88938ec21d3f9c1564
Robbie Gibson <rkgibson@google.com>
2021-02-03 10:01:08 +0000

    [iOS][Thumb Strip] Prevent floating new tab button closing thumb strip
    The floating new tab button should act more like the grid cell new tab
    button/selecting a tab and not close the thumb strip.
    
    Bug: 1171419
    Change-Id: I8471f5fec3375bee3a0f29ac7a654d4bfbc7b434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2667862
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#850039}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
@@ -1054,7 +1054,7 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   self.plusSignButton = plusSignButton;
   plusSignButton.translatesAutoresizingMaskIntoConstraints = NO;
   [plusSignButton addTarget:self
-                     action:@selector(newTabButtonTapped:)
+                     action:@selector(plusSignButtonTapped:)
            forControlEvents:UIControlEventTouchUpInside];
   [self.view addSubview:plusSignButton];
 
@@ -1618,6 +1618,22 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
     case TabGridPageRegularTabs:
       base::RecordAction(
           base::UserMetricsAction("MobileTabGridCreateRegularTab"));
+      break;
+    case TabGridPageRemoteTabs:
+      // No-op.
+      break;
+  }
+}
+
+- (void)plusSignButtonTapped:(id)sender {
+  switch (self.currentPage) {
+    case TabGridPageRegularTabs:
+      [self.regularTabsDelegate addNewItem];
+      // TODO(crbug.com/1135329): Record when a new regular tab is opened.
+      break;
+    case TabGridPageIncognitoTabs:
+      [self.incognitoTabsDelegate addNewItem];
+      // TODO(crbug.com/1135329): Record when a new incognito tab is opened.
       break;
     case TabGridPageRemoteTabs:
       // No-op.
