3bd84bbaa038a2c71f3392abd12d5147c39adec6
Hirokazu Honda <hiroh@chromium.org>
2021-02-03 03:07:03 +0000

    media/gpu: Remove video_encode_accelerator_unittest
    video_encode_accelerator_unittest is to be replaced with
    video_encode_accelerator_tests and
    video_encode_accelerator_perf_tests. This CL removes the code
    completely.
    
    Bug: b:177615329
    Test: Build
    Change-Id: I8f391e0acf67d339f7f0b839da07edc91f38038c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2631546
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849926}

--- media/gpu/test/video_test_helpers.cc
+++ media/gpu/test/video_test_helpers.cc
@@ -9,10 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_helpers.h"
 #include "base/logging.h"
-#include "base/memory/aligned_memory.h"
 #include "base/memory/ptr_util.h"
 #include "base/stl_util.h"
-#include "base/sys_byteorder.h"
 #include "gpu/ipc/common/gpu_memory_buffer_support.h"
 #include "gpu/ipc/service/gpu_memory_buffer_factory.h"
 #include "media/base/format_utils.h"
