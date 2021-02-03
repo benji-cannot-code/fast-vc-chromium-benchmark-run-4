6b925c555c8e353f3bdb5bb0484ad562c83e12f1
Yuichiro Hanada <yhanada@google.com>
2021-02-03 07:01:53 +0000

    Stop key event propagation from virtual keyboard on password textfield.
    By the special logic in ui::InputMethodChromeOS::DispatchKeyEvent,
    both of InsertChar() and DispatchKeyEventPostIME() are called
    for a key event from virtual keyboard on a password text field.
    The below logic stops a key event propagation
    through DispatchKeyEventPostIME() to prevent from inputting
    two characters.
    
          arc.ChromeVirtualKeyboard{,.unstable} arc.IMESwitchShortcut
          arc.PreIMEKeyEvent
    
    Bug: b:170260630, b:178674749
    Test: VK on password text field on ARC++ generates only one character.
    Test: Physical keyboard works while a11y keyboard shows up
    Test: tast run arc.PhysicalKeyboard{,.unstabe} arc.IMEBlocking
    Change-Id: Ieb4f60c71850bcf6aa7cdcc78649e16f50771624
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2668228
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849997}

--- components/arc/ime/arc_ime_service.cc
+++ components/arc/ime/arc_ime_service.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/ime/chromeos/extension_ime_util.h"
 #include "ui/base/ime/chromeos/input_method_manager.h"
+#include "ui/base/ime/constants.h"
 #include "ui/base/ime/input_method.h"
 #include "ui/base/ime/input_method_delegate.h"
 #include "ui/base/ime/text_input_flags.h"
@@ -44,6 +45,10 @@ namespace arc {
 namespace {
 
 base::Optional<double> g_override_default_device_scale_factor;
+
+bool IsCharacterKeyEvent(const ui::KeyEvent* event) {
+  return !IsControlChar(event) && !ui::IsSystemKeyModifier(event->flags());
+}
 
 class ArcWindowDelegateImpl : public ArcImeService::ArcWindowDelegate {
  public:
@@ -487,7 +492,7 @@ void ArcImeService::InsertChar(const ui::KeyEvent& event) {
     }
   }
 
-  if (!IsControlChar(&event) && !ui::IsSystemKeyModifier(event.flags())) {
+  if (IsCharacterKeyEvent(&event)) {
     has_composition_text_ = false;
     ime_bridge_->SendInsertText(base::string16(1, event.GetText()));
   }
@@ -661,10 +666,27 @@ bool ArcImeService::SetAutocorrectRange(const gfx::Range& range) {
 }
 
 void ArcImeService::OnDispatchingKeyEventPostIME(ui::KeyEvent* event) {
-  if (ShouldEnableKeyEventForwarding() && receiver_->HasCallback()) {
+  if (!ShouldEnableKeyEventForwarding())
+    return;
+
+  if (receiver_->HasCallback()) {
     receiver_->DispatchKeyEventPostIME(event);
     event->SetHandled();
+    return;
   }
+
+  // Do not forward the key event from virtual keyboard if the password text
+  // input is focused. By the special logic in
+  // ui::InputMethodChromeOS::DispatchKeyEvent, both of InsertChar() and
+  // DispatchKeyEventPostIME() are called for a key event from virtual keyboard
+  // on a password text field. The below logic stops a key event propagation
+  // through DispatchKeyEventPostIME() to prevent from inputting two characters.
+  const bool is_password = ime_type_ == ui::TEXT_INPUT_TYPE_PASSWORD;
+  const bool from_vk =
+      event->properties() && (event->properties()->find(ui::kPropertyFromVK) !=
+                              event->properties()->end());
+  if (is_password && from_vk && IsCharacterKeyEvent(event))
+    event->SetHandled();
 }
 
 // static
