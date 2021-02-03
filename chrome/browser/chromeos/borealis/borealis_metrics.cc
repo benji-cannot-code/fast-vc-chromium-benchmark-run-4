5d2d7f870706d58ccbbbe47382e13887bddeb61d
Lucy Qu <lqu@google.com>
2021-02-03 04:05:00 +0000

    Added UMA histogram to count number of VM shutdowns.
    BUG=b:171453025
    TEST=BorealisContextManager
    
    Change-Id: If8dfec672fc73809065a7020d5a00dda6401a714
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2661699
    Commit-Queue: Lucy Qu <lqu@google.com>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Daniel Ng <danielng@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849948}

--- chrome/browser/chromeos/borealis/borealis_metrics.cc
+++ chrome/browser/chromeos/borealis/borealis_metrics.cc
@@ -19,6 +19,8 @@ const char kBorealisStartupNumAttemptsHistogram[] =
 const char kBorealisStartupResultHistogram[] = "Borealis.Startup.Result";
 const char kBorealisStartupOverallTimeHistogram[] =
     "Borealis.Startup.OverallTime";
+const char kBorealisShutdownNumAttemptsHistogram[] =
+    "Borealis.Shutdown.NumAttempts";
 
 void RecordBorealisInstallNumAttemptsHistogram() {
   base::UmaHistogramBoolean(kBorealisInstallNumAttemptsHistogram, true);
@@ -46,6 +48,10 @@ void RecordBorealisStartupResultHistogram(
 
 void RecordBorealisStartupOverallTimeHistogram(base::TimeDelta startup_time) {
   base::UmaHistogramTimes(kBorealisStartupOverallTimeHistogram, startup_time);
+}
+
+void RecordBorealisShutdownNumAttemptsHistogram() {
+  base::UmaHistogramBoolean(kBorealisShutdownNumAttemptsHistogram, true);
 }
 
 }  // namespace borealis
