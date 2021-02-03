944ae1b4cc6ae7a8e42398a89845904025e7649a
Karandeep Bhatia <karandeepb@chromium.org>
2021-02-03 03:44:16 +0000

    Extensions: Cache feature availability for specified keys on Manifest.
    Cache the subset of available and specified manifest keys on the
    Manifest object. This makes availability lookups quicker (just a
    DictionaryValue key/path lookup) instead of the current cost of
    iterating over all parent paths and checking their availability. Expose
    the availabile DictionaryValue on the Manifest object as well and use it
    for auto-generated manifest parsing clients.
    
    The caveat is that this requires a one-time cost of iterating over all
    specified keys in the manifest and checking their availabililty ((this
    should be offset by the faster availabililty lookups) and the memory
    usage of the Manifest object also increases a bit.
    
    BUG=1167276
    
    Change-Id: Iaec2a06b03219159fd21775e2b1f44729740490b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2636686
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Auto-Submit: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849941}

--- chrome/common/extensions/api/omnibox/omnibox_handler.cc
+++ chrome/common/extensions/api/omnibox/omnibox_handler.cc
@@ -38,8 +38,8 @@ OmniboxHandler::~OmniboxHandler() {
 
 bool OmniboxHandler::Parse(Extension* extension, base::string16* error) {
   ManifestKeys manifest_keys;
-  if (!ManifestKeys::ParseFromDictionary(*extension->manifest()->value(),
-                                         &manifest_keys, error)) {
+  if (!ManifestKeys::ParseFromDictionary(
+          extension->manifest()->available_values(), &manifest_keys, error)) {
     return false;
   }
 
