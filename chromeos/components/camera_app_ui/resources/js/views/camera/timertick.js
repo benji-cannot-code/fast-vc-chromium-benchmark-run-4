492a59a3aae95e18013f32605ffc11e736fdb232
Shik Chen <shik@chromium.org>
2021-02-03 07:24:27 +0000

    CCA: Fix type annotations in timer for tsc
    Bug: b/172340451
    Test: Reduced 3 errors in tsc.
    Change-Id: I9f734782cd2bd94a7ba0e8363fb4743744a73781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2670608
    Reviewed-by: Inker Kuo <inker@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850003}

--- chromeos/components/camera_app_ui/resources/js/views/camera/timertick.js
+++ chromeos/components/camera_app_ui/resources/js/views/camera/timertick.js
@@ -10,7 +10,7 @@ import * as state from '../../state.js';
 
 /**
  * Handler to cancel the active running timer-ticks.
- * @type {?function()}
+ * @type {?function(): void}
  */
 let doCancel = null;
 
