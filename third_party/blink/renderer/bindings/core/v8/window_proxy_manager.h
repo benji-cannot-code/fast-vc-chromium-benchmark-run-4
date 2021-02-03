21ed5deb60182dfb3403d1d83bca8b5a2e7c50e5
Daniel Cheng <dcheng@chromium.org>
2021-02-03 05:10:20 +0000

    Crash logging to confirm theory for detached context inconsistency.
    There are some weird crashes where WebLocalFrameImpl is still attached
    but ScreenOrientationController no longer appears to have a DOMWindow
    associated with it. Add some crash logging to confirm if this is
    happening due to forcibly purging v8 memory on Android.
    
    Bug: 1154141
    Change-Id: I1181fd80803629e2c56c2937e20e5d1de9ca07d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2670707
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849971}

--- third_party/blink/renderer/bindings/core/v8/window_proxy_manager.h
+++ third_party/blink/renderer/bindings/core/v8/window_proxy_manager.h
@@ -49,6 +49,10 @@ class WindowProxyManager : public GarbageCollected<WindowProxyManager> {
 
   CORE_EXPORT void ResetIsolatedWorldsForTesting();
 
+  int LifecycleForDebugging() const {
+    return window_proxy_->LifecycleForDebugging();
+  }
+
  protected:
   using IsolatedWorldMap = HeapHashMap<int, Member<WindowProxy>>;
   enum class FrameType { kLocal, kRemote };
