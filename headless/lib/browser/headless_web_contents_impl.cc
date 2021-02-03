1eda1876fd14e5e21b621054e683cedcfd6c8c68
Yuta Hijikata <ythjkt@google.com>
2021-02-03 17:04:38 +0000

    Lacros: Preparation for lacros C++ macro flip.
    Lacros chrome is now built as target_os="chromeos" but C++ macros
    OS_LINUX and OS_CHROMEOS has not been flipped.
    
    This ensures that lacros-chrome is consistent after such flip.
    
    Bug: 1171629
    Change-Id: Ic2024c1b8a60cfca30eab5f8dc1ec631a3a17b83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2671101
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Auto-Submit: Yuta Hijikata <ythjkt@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850162}

--- headless/lib/browser/headless_web_contents_impl.cc
+++ headless/lib/browser/headless_web_contents_impl.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "base/values.h"
 #include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 #include "components/security_state/content/content_utils.h"
 #include "components/security_state/core/security_state.h"
 #include "content/public/browser/browser_thread.h"
@@ -60,7 +61,7 @@ namespace headless {
 namespace {
 
 void UpdatePrefsFromSystemSettings(blink::RendererPreferences* prefs) {
-#if defined(OS_LINUX) || defined(OS_WIN)
+#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS) || defined(OS_WIN)
   content::UpdateFontRendererPreferencesFromSystemSettings(prefs);
 #endif
 
