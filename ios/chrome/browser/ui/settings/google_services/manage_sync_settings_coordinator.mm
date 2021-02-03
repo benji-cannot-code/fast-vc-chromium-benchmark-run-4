132d59570ee261da8065fb04f84e90dc93fb8210
Gauthier Ambard <gambard@chromium.org>
2021-02-03 18:39:11 +0000

    [iOS] Fix Managed Sync TableView style
    When the flag is enabled, it should have the same TableView style as the
    other table views.
    
    Fixed: 1165770
    Change-Id: I2f9c9e6a3f01351745d8f86209f12f5336e93dc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2672741
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850198}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/google_services/sync_error_settings_command_handler.h"
 #import "ios/chrome/browser/ui/settings/sync/sync_encryption_passphrase_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/sync/sync_encryption_table_view_controller.h"
+#import "ios/chrome/browser/ui/table_view/table_view_utils.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity_browser_opener.h"
 #include "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
@@ -103,7 +104,7 @@ using signin_metrics::PromoAction;
   self.mediator.commandHandler = self;
   self.mediator.syncErrorHandler = self;
   self.viewController = [[ManageSyncSettingsTableViewController alloc]
-      initWithStyle:UITableViewStyleGrouped];
+      initWithStyle:ChromeTableViewStyle()];
   self.viewController.serviceDelegate = self.mediator;
   self.viewController.presentationDelegate = self;
   self.viewController.modelDelegate = self.mediator;
--- ios/chrome/browser/ui/settings/safety_check/safety_check_coordinator.mm
+++ ios/chrome/browser/ui/settings/safety_check/safety_check_coordinator.mm
@@ -93,12 +93,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - ChromeCoordinator
 
 - (void)start {
-  UITableViewStyle style = base::FeatureList::IsEnabled(kSettingsRefresh)
-                               ? ChromeTableViewStyle()
-                               : UITableViewStyleGrouped;
-
   SafetyCheckTableViewController* viewController =
-      [[SafetyCheckTableViewController alloc] initWithStyle:style];
+      [[SafetyCheckTableViewController alloc]
+          initWithStyle:ChromeTableViewStyle()];
   self.viewController = viewController;
 
   scoped_refptr<IOSChromePasswordCheckManager> passwordCheckManager =
