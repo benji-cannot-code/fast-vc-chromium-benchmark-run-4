f82a9c0342450e0d9e8d9e6868c96a2729ea6061
Daniel Cheng <dcheng@chromium.org>
2021-02-03 06:40:23 +0000

    Avoid using protected/private RemoteWindowProxy methods.
    Change-Id: I65bb6d1d3ac43b9db70d2fd3854d3e370065f88f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2670882
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849990}

--- third_party/blink/renderer/bindings/core/v8/remote_window_proxy.h
+++ third_party/blink/renderer/bindings/core/v8/remote_window_proxy.h
@@ -41,16 +41,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // Subclass of WindowProxy that only handles RemoteFrame.
-// TODO(dcheng): This class temporarily contains code duplicated from
-// LocalWindowProxy. It will be removed once the global proxy is instantiated
-// using v8::Context::NewRemoteContext().
 class RemoteWindowProxy final : public WindowProxy {
  public:
   RemoteWindowProxy(v8::Isolate*, RemoteFrame&, scoped_refptr<DOMWrapperWorld>);
 
  private:
-  friend class WindowProxyManager;
-
   // WindowProxy overrides:
   void Initialize() override;
   void DisposeContext(Lifecycle next_status, FrameReuseStatus) override;
