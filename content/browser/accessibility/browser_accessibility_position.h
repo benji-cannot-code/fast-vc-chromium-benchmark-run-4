7ccf395207c640a28185e086101fbe64e033e415
Nektarios Paisios <nektar@chromium.org>
2021-02-03 16:45:28 +0000

    Made AXNodePosition work with multiple AX trees
    This work is a prerequicit before merging BrowserAccessibilityPosition and AXNodePosition into
    a single class.
    Until now, BrowserAccessibilityPosition had been able to traverse across multiple accessibility trees
    while AXNodePosition had a limited ability to do that.
    This patch completes the work necessary to enable cross tree traversal in AXNodePosition,
    because BrowserAccessibilityPosition would be deleted soon in a followup patch.
    
    AX-Relnotes: n/a.
    
    R=dmazzoni@chromium.org, aleventhal@chromium.org
    
    Bug: 1049261
    Change-Id: I185e6c599a4c750939fedb7e2494df7b1211c269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2642692
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Auto-Submit: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850148}

--- content/browser/accessibility/browser_accessibility_position.h
+++ content/browser/accessibility/browser_accessibility_position.h
@@ -33,6 +33,7 @@ class CONTENT_EXPORT BrowserAccessibilityPosition
   AXPositionInstance Clone() const override;
 
   base::string16 GetText() const override;
+  ax::mojom::Role GetRole() const override;
   bool IsInLineBreak() const override;
   bool IsInTextObject() const override;
   bool IsInWhiteSpace() const override;
@@ -44,7 +45,7 @@ class CONTENT_EXPORT BrowserAccessibilityPosition
   int AnchorChildCount() const override;
   int AnchorUnignoredChildCount() const override;
   int AnchorIndexInParent() const override;
-  int AnchorSiblingCount() const override;
+  int GetAnchorSiblingCount() const override;
   base::stack<BrowserAccessibility*> GetAncestorAnchors() const override;
   BrowserAccessibility* GetLowestUnignoredAncestor() const override;
   void AnchorParent(AXTreeID* tree_id, ui::AXNodeID* parent_id) const override;
