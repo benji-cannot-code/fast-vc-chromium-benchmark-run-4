6f768fbf1ed5f999470e0b74ab7de61c73158bc5
edchin <edchin@chromium.org>
2021-02-03 19:18:39 +0000

    [ios][PhishGuard] Methods related to recording reuse events
    This CL continues to port over PhishGuard functionality to iOS.
    
    Code in this CL can be compared to the /content counterpart
    (from which it was ported) in //chrome/browser/safe_browsing.
    
    This CL ports over 2 methods related to recording UserEventSpecifics:
    - password reuse detected event
    - result of user's interaction with the reuse warning dialog
    
    This also ports over 2 unit tests for the reuse detected event.
    
    Bug: 1147967
    Change-Id: I2fae9bcb234ce59abab978e7cc3d7f283ce0e2dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2668438
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850223}

--- ios/chrome/browser/safe_browsing/chrome_password_protection_service.h
+++ ios/chrome/browser/safe_browsing/chrome_password_protection_service.h
@@ -10,12 +10,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/password_manager/core/browser/password_reuse_detector.h"
 #import "components/safe_browsing/ios/password_protection/password_protection_service.h"
+#include "components/sync/protocol/gaia_password_reuse.pb.h"
 
 class ChromeBrowserState;
+class GURL;
 class PrefService;
 
 namespace password_manager {
 class PasswordStore;
+}
+
+namespace web {
+class WebState;
 }
 
 namespace safe_browsing {
@@ -119,6 +125,16 @@ class ChromePasswordProtectionService : public PasswordProtectionService {
       RequestOutcome outcome,
       PasswordType password_type,
       const LoginReputationClientResponse* response) override;
+
+  // Records a Chrome Sync event that sync password reuse was detected.
+  void MaybeLogPasswordReuseDetectedEvent(web::WebState* web_state);
+
+  // Records a Chrome Sync event with the result of the user's interaction with
+  // the warning dialog.
+  void MaybeLogPasswordReuseDialogInteraction(
+      int64_t navigation_id,
+      sync_pb::GaiaPasswordReuse::PasswordReuseDialogInteraction::
+          InteractionResult interaction_result);
 
  private:
   password_manager::PasswordStore* GetStoreForReusedCredential(
