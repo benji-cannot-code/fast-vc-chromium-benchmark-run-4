6c7efd93ca9088060737de9dbe9871cf3b6b1ca3
Dana Fried <dfried@chromium.org>
2021-02-03 21:20:57 +0000

    [User Education] put tab groups 'New' badge back behind a targeted flag
    We want to keep experimenting with this badge through M89, but had to
    pull the global 'New' badge flag from the experiment in M89 due to the
    need for other features to use a 'New' badge.
    
    This CL institutes a local badge that gates the display of this specific
    promo badge just in the tab context menu, which the experiment will use
    to gate the display of the badge in M89-90 instead of the global flag
    we can no longer use.
    
    Bug: 1173792
    Change-Id: Iccc52eec0f0b23d4210bc6649025965b880cc1a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2667536
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850283}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1346,6 +1346,9 @@ extern const char kTabGroupsCollapseFreezingDescription[];
 extern const char kTabGroupsFeedbackName[];
 extern const char kTabGroupsFeedbackDescription[];
 
+extern const char kTabGroupsNewBadgePromoName[];
+extern const char kTabGroupsNewBadgePromoDescription[];
+
 extern const char kTabHoverCardsName[];
 extern const char kTabHoverCardsDescription[];
 
