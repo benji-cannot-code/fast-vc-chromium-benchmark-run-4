bb8215e341f6e359f219c1b5c575e0823b918f05
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-02-03 11:33:38 +0000

    [sync] Fix recording of undecryptable data metrics
    Use ModelTypeToHistogramSuffix() rather than ModelTypeToString().
    
    Bug: 1173986
    Change-Id: I33c88c0c3650419250e032d1cbab5aca6bb898ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2671785
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#850061}

--- components/sync/engine/model_type_worker.cc
+++ components/sync/engine/model_type_worker.cc
@@ -580,7 +580,7 @@ void ModelTypeWorker::DecryptStoredEntities() {
     if (newly_found_key.gu_responses_while_should_have_been_known > 0) {
       base::UmaHistogramCounts1000(
           base::StrCat({kTimeUntilEncryptionKeyFoundHistogramPrefix,
-                        ModelTypeToString(GetModelType())}),
+                        ModelTypeToHistogramSuffix(type_)}),
           newly_found_key.gu_responses_while_should_have_been_known);
     }
   }
@@ -734,7 +734,7 @@ void ModelTypeWorker::MaybeDropPendingUpdatesEncryptedWith(
   if (entries_pending_decryption_.size() < updates_before_dropping) {
     base::UmaHistogramCounts1000(
         base::StrCat({kUndecryptablePendingUpdatesDroppedHistogramPrefix,
-                      ModelTypeToString(GetModelType())}),
+                      ModelTypeToHistogramSuffix(type_)}),
         updates_before_dropping - entries_pending_decryption_.size());
   }
 }
