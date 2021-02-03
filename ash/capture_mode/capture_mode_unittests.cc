e8cb333a7df1c1f6662cbb106a88127e3d16f84b
Ahmed Fakhry <afakhry@google.com>
2021-02-03 01:18:02 +0000

    capture_mode: Fix a crash when clearing the window capture.
    The CaptureModeSession clears the input capture from the window
    that had it before the session was started. Some windows may get
    destroyed as a result of losing the capture, for example a window
    created for drag-drop purposes will get destroyed on drag canceled
    as a result of losing capture.
    
    This CL fixes the issue.
    
    BUG=1172425
    TEST=Added new test.
    
    Change-Id: I66eeccdcf7e00ea20eecf462343b4c98ff348405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2668813
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#849873}

--- ash/capture_mode/capture_mode_unittests.cc
+++ ash/capture_mode/capture_mode_unittests.cc
@@ -49,6 +49,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/power/fake_power_manager_client.h"
 #include "chromeos/dbus/power_manager/suspend.pb.h"
 #include "components/account_id/account_id.h"
+#include "ui/aura/client/capture_client.h"
+#include "ui/aura/client/capture_client_observer.h"
 #include "ui/aura/window_tracker.h"
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
@@ -133,6 +135,44 @@ bool IsLayerStackedRightBelow(ui::Layer* layer, ui::Layer* sibling) {
       std::find(children.begin(), children.end(), sibling) - children.begin();
   return sibling_index > 0 && children[sibling_index - 1] == layer;
 }
+
+// Defines a capture client observer, that sets the input capture to the window
+// given to the constructor, and destroys it once capture is lost.
+class TestCaptureClientObserver : public aura::client::CaptureClientObserver {
+ public:
+  explicit TestCaptureClientObserver(std::unique_ptr<aura::Window> window)
+      : window_(std::move(window)) {
+    DCHECK(window_);
+    auto* capture_client =
+        aura::client::GetCaptureClient(window_->GetRootWindow());
+    capture_client->SetCapture(window_.get());
+    capture_client->AddObserver(this);
+  }
+
+  ~TestCaptureClientObserver() override { StopObserving(); }
+
+  // aura::client::CaptureClientObserver:
+  void OnCaptureChanged(aura::Window* lost_capture,
+                        aura::Window* gained_capture) override {
+    if (lost_capture != window_.get())
+      return;
+
+    StopObserving();
+    window_.reset();
+  }
+
+ private:
+  void StopObserving() {
+    if (!window_)
+      return;
+
+    auto* capture_client =
+        aura::client::GetCaptureClient(window_->GetRootWindow());
+    capture_client->RemoveObserver(this);
+  }
+
+  std::unique_ptr<aura::Window> window_;
+};
 
 }  // namespace
 
@@ -452,6 +492,13 @@ TEST_F(CaptureModeTest, StartStop) {
   controller->Stop();
   EXPECT_TRUE(tracker.windows().empty());
   EXPECT_FALSE(controller->IsActive());
+}
+
+// Regression test for https://crbug.com/1172425.
+TEST_F(CaptureModeTest, NoCrashOnClearingCapture) {
+  TestCaptureClientObserver observer(CreateTestWindow(gfx::Rect(200, 200)));
+  auto* controller = StartImageRegionCapture();
+  EXPECT_TRUE(controller->IsActive());
 }
 
 TEST_F(CaptureModeTest, CheckWidgetClosed) {
