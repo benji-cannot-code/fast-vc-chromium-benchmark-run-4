c9c40fe20b2f9c834085740d39d6f747fc8f8050
Aya ElAttar <ayaelattar@chromium.org>
2021-02-03 18:07:56 +0000

    DLP: Add browsertests for clipboard bubble
    - Added browsertests for clipboard bubble in both;
    block & warning modes.
    - Added DEPS file.
    - Updated DIR_METADATA to the right Monorail component.
    
    Bug: 1139884
    Change-Id: Ifc19bcf51dc8c77eea1f3c92d651ad4c4947ab20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2669587
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850188}

--- chrome/browser/chromeos/policy/dlp/dlp_clipboard_notification_helper.h
+++ chrome/browser/chromeos/policy/dlp/dlp_clipboard_notification_helper.h
@@ -53,6 +53,8 @@ class DlpClipboardNotificationHelper : public views::WidgetObserver,
 
   void ResetUserWarnSelection();
 
+  views::Widget* GetWidgetForTesting() { return widget_.get(); }
+
  private:
   virtual void ShowClipboardBlockBubble(const base::string16& text);
   virtual void ShowClipboardBlockToast(const std::string& id,
