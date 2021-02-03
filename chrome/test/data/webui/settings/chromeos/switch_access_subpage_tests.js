002e04d39e8829aae0c812d3983bbb85a784a61a
Theo Johnson-kanu <tjohnsonkanu@google.com>
2021-02-03 04:20:32 +0000

    [CrOS settings] Migrate switch_access_subpage to Polymer3
    Bug: 1045266
    Change-Id: I80ed04c84f8a2ba2ea30b7d07b2c693c154d2499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2669357
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/master@{#849956}

--- chrome/test/data/webui/settings/chromeos/switch_access_subpage_tests.js
+++ chrome/test/data/webui/settings/chromeos/switch_access_subpage_tests.js
@@ -3,6 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
+// #import 'chrome://os-settings/chromeos/lazy_load.js';
+
+// #import {SwitchAccessSubpageBrowserProxyImpl, SwitchAccessSubpageBrowserProxy, routes, Router} from 'chrome://os-settings/chromeos/os_settings.js';
+// #import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+// #import {TestBrowserProxy} from '../../test_browser_proxy.m.js';
+// #import {assertEquals, assertDeepEquals} from '../../chai_assert.js';
+// #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// #import {getDeepActiveElement} from 'chrome://resources/js/util.m.js';
+// #import {waitAfterNextRender} from 'chrome://test/test_util.m.js';
+// clang-format on
+
 /**
  * @implements {SwitchAccessSubpageBrowserProxy}
  */
@@ -220,7 +232,7 @@ suite('ManageAccessibilityPageTests', function() {
       isDeepLinkingEnabled: true,
       showExperimentalAccessibilitySwitchAccessImprovedTextInput: true,
     });
-    prefs = getDefaultPrefs();
+    const prefs = getDefaultPrefs();
     prefs.settings.a11y.switch_access.auto_scan.enabled.value = true;
     initPage(prefs);
 
