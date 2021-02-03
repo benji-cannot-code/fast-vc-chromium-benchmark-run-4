83b00d84dc3f09d6d42e3ec6d917c8442069b955
gogerald <gogerald@google.com>
2021-02-03 18:56:19 +0000

    [Start] Introduce the feature flag
    Bug: 1173160
    Change-Id: I4463f9962f5ac7c2db4dd27d88a28687535c0991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2665149
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850204}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/fullscreen/fullscreen_features.h"
 #import "ios/chrome/browser/ui/infobars/infobar_feature.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_feature.h"
+#import "ios/chrome/browser/ui/start_surface/start_surface_features.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/features.h"
 #import "ios/chrome/browser/ui/table_view/feature_flags.h"
 #import "ios/chrome/browser/ui/toolbar_container/toolbar_container_features.h"
@@ -608,6 +609,9 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kSharedHighlightingUseBlocklistIOSDescription,
      flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(shared_highlighting::kSharedHighlightingUseBlocklist)},
+    {"start-surface", flag_descriptions::kStartSurfaceName,
+     flag_descriptions::kStartSurfaceDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kStartSurface)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
---
+++ ios/chrome/browser/ui/start_surface/start_surface_features.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_START_SURFACE_START_SURFACE_FEATURES_H_
+#define IOS_CHROME_BROWSER_UI_START_SURFACE_START_SURFACE_FEATURES_H_
+
+#include "base/feature_list.h"
+
+// The feature to enable or disable the Start Surface.
+extern const base::Feature kStartSurface;
+
+bool IsStartSurfaceEnabled();
+
+#endif  // IOS_CHROME_BROWSER_UI_START_SURFACE_START_SURFACE_FEATURES_H_.
