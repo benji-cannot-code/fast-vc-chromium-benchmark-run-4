a9059d42c4e5c0a4ff53c06048e02f5fb9ec1e3b
Benoit Lize <lizeb@chromium.org>
2021-02-03 11:55:44 +0000

    [PartitionAlloc] Make sure the thread cache TLS key is always there.
    If ThreadCacheRegistry::PurgeAll() (or DumpStats()) are called before
    any thread cache is created, we try to access the content of a
    non-existent TLS key. This happens when BackupRefPtr initialization is
    delayed, for instance.
    
    Issue found by glazunov@.
    
    Bug: 998048
    Change-Id: I98497f6f8d7a79897dbc2cf02b2116ce4920072e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2653026
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850068}

--- base/allocator/partition_allocator/thread_cache.cc
+++ base/allocator/partition_allocator/thread_cache.cc
@@ -30,6 +30,7 @@ namespace {
 // PartitionRoot can use it.
 static std::atomic<bool> g_has_instance;
 
+static bool g_thread_cache_key_created = false;
 }  // namespace
 
 constexpr base::TimeDelta ThreadCacheRegistry::kPurgeInterval;
@@ -63,6 +64,7 @@ void ThreadCacheRegistry::UnregisterThreadCache(ThreadCache* cache) {
 
 void ThreadCacheRegistry::DumpStats(bool my_thread_only,
                                     ThreadCacheStats* stats) {
+  ThreadCache::EnsureThreadSpecificDataInitialized();
   memset(reinterpret_cast<void*>(stats), 0, sizeof(ThreadCacheStats));
 
   PartitionAutoLock scoped_locker(GetLock());
@@ -107,6 +109,8 @@ void ThreadCacheRegistry::PurgeAll() {
 }
 
 void ThreadCacheRegistry::StartPeriodicPurge() {
+  ThreadCache::EnsureThreadSpecificDataInitialized();
+
   // Can be called several times, don't post multiple tasks.
   if (!has_pending_purge_task_)
     PostDelayedPurgeTask();
@@ -183,11 +187,23 @@ void ThreadCacheRegistry::ResetForTesting() {
 }
 
 // static
-void ThreadCache::Init(PartitionRoot<ThreadSafe>* root) {
-  PA_CHECK(root->buckets[kBucketCount - 1].slot_size == kSizeThreshold);
+void ThreadCache::EnsureThreadSpecificDataInitialized() {
+  // Using the registry lock to protect from concurrent initialization without
+  // adding a special-pupose lock.
+  PartitionAutoLock scoped_locker(ThreadCacheRegistry::Instance().GetLock());
+  if (g_thread_cache_key_created)
+    return;
 
   bool ok = PartitionTlsCreate(&g_thread_cache_key, Delete);
   PA_CHECK(ok);
+  g_thread_cache_key_created = true;
+}
+
+// static
+void ThreadCache::Init(PartitionRoot<ThreadSafe>* root) {
+  PA_CHECK(root->buckets[kBucketCount - 1].slot_size == kSizeThreshold);
+
+  EnsureThreadSpecificDataInitialized();
 
   // Make sure that only one PartitionRoot wants a thread cache.
   bool expected = false;
