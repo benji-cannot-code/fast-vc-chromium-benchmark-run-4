cf54cda12613cad24e4b8541cd73574ecc6d243a
David Roger <droger@chromium.org>
2021-02-03 16:09:23 +0000

    Remove unused enum ValidateMenuItemSelector
    All the uses of this enum were removed in https://crrev.com/c/2661655
    
    Change-Id: I23cb1694a020f21642496082ec7247943e4c9779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2652610
    Commit-Queue: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850130}

--- chrome/browser/ui/cocoa/profiles/profile_menu_controller.mm
+++ chrome/browser/ui/cocoa/profiles/profile_menu_controller.mm
@@ -33,16 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Used in UMA histogram macros, shouldn't be reordered or renumbered
-enum ValidateMenuItemSelector {
-  UNKNOWN_SELECTOR = 0,
-  NEW_PROFILE,
-  EDIT_PROFILE,
-  SWITCH_PROFILE_MENU,
-  SWITCH_PROFILE_DOCK,
-  MAX_VALIDATE_MENU_SELECTOR,
-};
-
 // Check Add Person pref.
 bool IsAddPersonEnabled() {
   PrefService* service = g_browser_process->local_state();
