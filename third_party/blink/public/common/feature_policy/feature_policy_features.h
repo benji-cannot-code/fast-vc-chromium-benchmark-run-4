e659527a7458cf7efe0276540416f7de51844fd0
Charlie Hu <chenleihu@google.com>
2021-02-03 00:24:01 +0000

    [Permissions Policy] Support blockage tracing
    This CL adds the ability to trace the root blockage for a disabled
    feature in certain frame.
    
    Change-Id: Ifd84465312eb4db20d1ecfa6e392f1deb8352dbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2639221
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Cr-Commit-Position: refs/heads/master@{#849856}

--- third_party/blink/public/common/feature_policy/feature_policy_features.h
+++ third_party/blink/public/common/feature_policy/feature_policy_features.h
@@ -13,9 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // The FeaturePolicyFeatureDefault enum defines the default enable state for
-// a feature when neither it nor any parent frame have declared an explicit
-// policy. The three possibilities map directly to Feature Policy Allowlist
-// semantics.
+// a feature when the feature is not declared in iframe 'allow' attribute.
+// See |FeaturePolicy::InheritedValueForFeature| for usage.
+//
+// The 2 possibilities map directly to Feature Policy Allowlist semantics.
 //
 // The default values for each feature are set in GetDefaultFeatureList.
 enum class FeaturePolicyFeatureDefault {
