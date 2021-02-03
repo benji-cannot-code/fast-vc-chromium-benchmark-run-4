da557d1cad82474c3a5f72cea82b03b6d789de93
Samuel Huang <huangs@chromium.org>
2021-02-03 19:51:08 +0000

    Revert "Finish base::Bind -> Once/Repeating migration for media/blink"
    This reverts commit 664ad5e27e2f538b7485aaf73b1d8797e9f57d3d.
    
    Reason for revert: Suspected of causing  BackgroundBehaviorTestInstances/WebMediaPlayerImplBackgroundBehaviorTest.AudioVideo flakes. 
    
    Original change's description:
    > Finish base::Bind -> Once/Repeating migration for media/blink
    >
    > Previous attempt to do this change has led to test flakiness in the past
    > due to the fact that `base::CancelableOnceClosure::IsCancelled()`
    > returns true if the callback is run even if it was not explicitly
    > cancelled.
    >
    > Bug: 1170511
    > Change-Id: I05b28c089ba12d82e78c89f20e430ed6ff685e61
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2669091
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Alex St-Onge <alstonge@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#850153}
    
    TBR=dalecurtis@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,alstonge@chromium.org
    
    Change-Id: Iba4b7547bacb3aaac5186067845d6309fb8eecbc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1170511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673081
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850240}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -3608,8 +3608,7 @@ void WebMediaPlayerImpl::UpdateBackgroundVideoOptimizationState() {
   if (IsHidden()) {
     if (ShouldPausePlaybackWhenHidden()) {
       PauseVideoIfNeeded();
-    } else if (!is_background_status_change_scheduled_) {
-      is_background_status_change_scheduled_ = true;
+    } else if (update_background_status_cb_.IsCancelled()) {
       // Only trigger updates when we don't have one already scheduled.
       update_background_status_cb_.Reset(
           base::Bind(&WebMediaPlayerImpl::DisableVideoTrackIfNeeded,
@@ -3625,7 +3624,6 @@ void WebMediaPlayerImpl::UpdateBackgroundVideoOptimizationState() {
     }
   } else {
     update_background_status_cb_.Cancel();
-    is_background_status_change_scheduled_ = false;
     EnableVideoTrackIfNeeded();
   }
 }
@@ -3664,8 +3662,6 @@ void WebMediaPlayerImpl::EnableVideoTrackIfNeeded() {
 
 void WebMediaPlayerImpl::DisableVideoTrackIfNeeded() {
   DCHECK(IsHidden());
-
-  is_background_status_change_scheduled_ = false;
 
   // Don't change video track while the pipeline is resuming or seeking.
   if (is_pipeline_resuming_ || seeking_)
