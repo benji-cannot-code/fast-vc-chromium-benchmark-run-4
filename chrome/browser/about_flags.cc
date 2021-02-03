f03fed945f25c46e24907d3e3e6a1a4743869a23
Hiroki Nakagawa <nhiroki@chromium.org>
2021-02-03 10:34:52 +0000

    NoStatePrefetch: Rename prerender_field_trial to no_state_prefetch_field_trial
    Change-Id: I78bd57d94c6edc3c66f043a65ead75a6dff0d921
    Bug: 1144577
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2666168
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850047}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -99,7 +99,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/nacl/common/nacl_switches.h"
 #include "components/network_session_configurator/common/network_features.h"
 #include "components/network_session_configurator/common/network_switches.h"
-#include "components/no_state_prefetch/browser/prerender_field_trial.h"
+#include "components/no_state_prefetch/browser/no_state_prefetch_field_trial.h"
 #include "components/ntp_tiles/features.h"
 #include "components/offline_pages/core/offline_page_feature.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
--- components/no_state_prefetch/browser/no_state_prefetch_field_trial.h
+++ components/no_state_prefetch/browser/no_state_prefetch_field_trial.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_NO_STATE_PREFETCH_BROWSER_PRERENDER_FIELD_TRIAL_H_
-#define COMPONENTS_NO_STATE_PREFETCH_BROWSER_PRERENDER_FIELD_TRIAL_H_
+#ifndef COMPONENTS_NO_STATE_PREFETCH_BROWSER_NO_STATE_PREFETCH_FIELD_TRIAL_H_
+#define COMPONENTS_NO_STATE_PREFETCH_BROWSER_NO_STATE_PREFETCH_FIELD_TRIAL_H_
 
 #include "base/feature_list.h"
 
@@ -18,4 +18,4 @@ extern const base::Feature kNavigationPredictorPrefetchHoldback;
 
 }  // namespace prerender
 
-#endif  // COMPONENTS_NO_STATE_PREFETCH_BROWSER_PRERENDER_FIELD_TRIAL_H_
+#endif  // COMPONENTS_NO_STATE_PREFETCH_BROWSER_NO_STATE_PREFETCH_FIELD_TRIAL_H_
