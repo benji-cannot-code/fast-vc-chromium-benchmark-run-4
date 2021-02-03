f24097c03294ab318142ea88d59dfb1a0439bbfa
danakj <danakj@chromium.org>
2021-02-03 17:27:55 +0000

    Remove use of RenderViewCreated and RenderViewReady from fuchsia
    Use RenderFrameCreated which will watch for both current or speculative
    RenderFrame creation, and happens after the renderer's frame is ready.
    
    R=alexmos@chromium.org, wez@chromium.org
    
    Bug: 763548
    Change-Id: Ibe474a8040620f7a75e2bd9942e7b2a0883af3e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2669030
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850175}

--- fuchsia/engine/browser/frame_impl.cc
+++ fuchsia/engine/browser/frame_impl.cc
@@ -28,8 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/permission_controller_delegate.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
-#include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/renderer_preferences_util.h"
@@ -1122,21 +1122,10 @@ void FrameImpl::DidFinishLoad(content::RenderFrameHost* render_frame_host,
   context_->devtools_controller()->OnFrameLoaded(web_contents_.get());
 }
 
-void FrameImpl::RenderViewCreated(content::RenderViewHost* render_view_host) {
-  render_view_host->GetWidget()->GetView()->SetBackgroundColor(
-      SK_AlphaTRANSPARENT);
-}
-
-void FrameImpl::RenderViewReady() {
-  web_contents_->GetRenderViewHost()
-      ->GetWidget()
-      ->GetView()
-      ->SetBackgroundColor(SK_AlphaTRANSPARENT);
-
-  // Setting the background color doesn't necessarily apply it right away, so
-  // request a redraw if there is a view connected to this Frame.
-  if (window_tree_host_)
-    window_tree_host_->compositor()->ScheduleDraw();
+void FrameImpl::RenderFrameCreated(content::RenderFrameHost* frame_host) {
+  // The top-level frame is given a transparent background color.
+  if (frame_host == web_contents()->GetMainFrame())
+    frame_host->GetView()->SetBackgroundColor(SK_AlphaTRANSPARENT);
 }
 
 void FrameImpl::DidFirstVisuallyNonEmptyPaint() {
