30a2fff8a502c8cc955436da55834a9832b0769f
Jana Grill <janagrill@google.com>
2021-02-03 11:24:26 +0000

    Adjust chrome://policy for FlexOrgs
    Remove Display domain from chrome://policy page.
    
    Bug: b:172679657, b:172683878
    Test: Manual
    Change-Id: I28304ba294c0a47c643772f2332da39146bb473c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2656337
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Jana Grill <janagrill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850060}

--- chrome/browser/ui/webui/policy/policy_ui_handler.cc
+++ chrome/browser/ui/webui/policy/policy_ui_handler.cc
@@ -366,7 +366,6 @@ class DeviceCloudPolicyStatusProviderChromeOS
 
  private:
   std::string enterprise_enrollment_domain_;
-  std::string enterprise_display_domain_;
 
   DISALLOW_COPY_AND_ASSIGN(DeviceCloudPolicyStatusProviderChromeOS);
 };
@@ -430,8 +429,7 @@ class DeviceActiveDirectoryPolicyStatusProvider
  public:
   DeviceActiveDirectoryPolicyStatusProvider(
       policy::ActiveDirectoryPolicyManager* policy_manager,
-      const std::string& enterprise_realm,
-      const std::string& enterprise_display_domain);
+      const std::string& enterprise_realm);
 
   ~DeviceActiveDirectoryPolicyStatusProvider() override = default;
 
@@ -440,7 +438,6 @@ class DeviceActiveDirectoryPolicyStatusProvider
 
  private:
   std::string enterprise_realm_;
-  std::string enterprise_display_domain_;
 
   DISALLOW_COPY_AND_ASSIGN(DeviceActiveDirectoryPolicyStatusProvider);
 };
@@ -615,7 +612,6 @@ DeviceCloudPolicyStatusProviderChromeOS::
     : CloudPolicyCoreStatusProvider(
           connector->GetDeviceCloudPolicyManager()->core()) {
   enterprise_enrollment_domain_ = connector->GetEnterpriseEnrollmentDomain();
-  enterprise_display_domain_ = connector->GetEnterpriseDisplayDomain();
 }
 
 DeviceCloudPolicyStatusProviderChromeOS::
@@ -625,7 +621,6 @@ void DeviceCloudPolicyStatusProviderChromeOS::GetStatus(
     base::DictionaryValue* dict) {
   GetStatusFromCore(core_, dict);
   dict->SetString("enterpriseEnrollmentDomain", enterprise_enrollment_domain_);
-  dict->SetString("enterpriseDisplayDomain", enterprise_display_domain_);
   GetOffHoursStatus(dict);
 }
 
@@ -724,17 +719,14 @@ void UserActiveDirectoryPolicyStatusProvider::OnStoreError(
 DeviceActiveDirectoryPolicyStatusProvider::
     DeviceActiveDirectoryPolicyStatusProvider(
         policy::ActiveDirectoryPolicyManager* policy_manager,
-        const std::string& enterprise_realm,
-        const std::string& enterprise_display_domain)
+        const std::string& enterprise_realm)
     : UserActiveDirectoryPolicyStatusProvider(policy_manager, nullptr),
-      enterprise_realm_(enterprise_realm),
-      enterprise_display_domain_(enterprise_display_domain) {}
+      enterprise_realm_(enterprise_realm) {}
 
 void DeviceActiveDirectoryPolicyStatusProvider::GetStatus(
     base::DictionaryValue* dict) {
   UserActiveDirectoryPolicyStatusProvider::GetStatus(dict);
   dict->SetString("enterpriseEnrollmentDomain", enterprise_realm_);
-  dict->SetString("enterpriseDisplayDomain", enterprise_display_domain_);
 }
 
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
@@ -857,7 +849,7 @@ void PolicyUIHandler::RegisterMessages() {
       device_status_provider_ =
           std::make_unique<DeviceActiveDirectoryPolicyStatusProvider>(
               connector->GetDeviceActiveDirectoryPolicyManager(),
-              connector->GetRealm(), connector->GetEnterpriseDisplayDomain());
+              connector->GetRealm());
     } else {
       device_status_provider_ =
           std::make_unique<DeviceCloudPolicyStatusProviderChromeOS>(connector);
