chromium/chromium

commit a72084e865022b09ca41fbc4a571f6dbf3c3bd3f

[css-flex] Ignore items' heights for intrinsic block size calculation
Blink currently makes this flex item 200px tall. Firefox gives it the
correct height of 100px.

<div style="display: flex; flex-direction: column">
  <div style="flex-basis: 100px; height: 200px">
    <div style="height: 100%">

Blink was letting the inner div's height resolve against the flex item's
200px, giving the flex item an automatic minimum size of 200px, even
though the min-content size is supposed to be calculated with the item
having an 'auto' size. Meaning we should ignore the item's 200px height.

To keep the 200px from having an effect, this patch broadens the
definition of ConstraintSpace().IsFixedBlockSizeDefinite. It no longer
has to accompany a FixedBlockSize. It now means that the element is to
be laid out as if it has an auto height.

Flexbox now sets this constraint on items when the flexbox needs the
item's intrinsic block size.

Fixed: 1033476, 607585
Change-Id: I0376e8ec71ef5a6a71989a8bdda63b39718852e8
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2640774
Auto-Submit: David Grogan <dgrogan@chromium.org>
Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
Commit-Queue: David Grogan <dgrogan@chromium.org>
Cr-Commit-Position: refs/heads/master@{#848159}

chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
@@ -771,7 +771,8 @@ TEST_F('OSSettingsDevicePageTest', 'DevicePageTest', () => {
   mocha.grep(assert(device_page_tests.TestNames.DevicePage)).run();
 });
 
-TEST_F('OSSettingsDevicePageTest', 'DisplayTest', () => {
+// Fails after https://chromium-review.googlesource.com/c/chromium/src/+/2640774
+TEST_F('OSSettingsDevicePageTest', 'DISABLED_DisplayTest', () => {
   mocha.grep(assert(device_page_tests.TestNames.Display)).run();
 });
 

third_party/blink/renderer/core/layout/flexible_box_algorithm.cc
@@ -773,13 +773,8 @@ LayoutUnit FlexLayoutAlgorithm::IntrinsicContentBlockSize() const {
 
   if (IsColumnFlow()) {
     LayoutUnit max_size;
-    for (const FlexLine& line : flex_lines_) {
-      // Subtract main_axis_offset to remove border/padding
-      max_size =
-          std::max(line.main_axis_extent_ - line.sum_justify_adjustments_ -
-                       line.main_axis_offset_,
-                   max_size);
-    }
+    for (const FlexLine& line : flex_lines_)
+      max_size = std::max(line.sum_hypothetical_main_size_, max_size);
     return max_size;
   }
 

third_party/blink/renderer/core/layout/ng/custom/ng_custom_layout_algorithm.cc
@@ -9,6 +9,7 @@
 #include "third_party/blink/renderer/bindings/core/v8/v8_intrinsic_sizes_result_options.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/core/layout/geometry/logical_size.h"
 #include "third_party/blink/renderer/core/layout/ng/custom/custom_layout_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/custom/custom_layout_scope.h"
 #include "third_party/blink/renderer/core/layout/ng/custom/layout_worklet.h"
@@ -48,11 +49,18 @@ MinMaxSizesResult NGCustomLayoutAlgorithm::ComputeMinMaxSizes(
 
   bool depends_on_percentage_block_size = false;
   IntrinsicSizesResultOptions* intrinsic_sizes_result_options = nullptr;
-  if (!instance->IntrinsicSizes(
-          ConstraintSpace(), document, Node(),
-          container_builder_.InitialBorderBoxSize(), BorderScrollbarPadding(),
-          input.percentage_resolution_block_size, &scope,
-          &intrinsic_sizes_result_options, &depends_on_percentage_block_size)) {
+  LogicalSize border_box_size{
+      container_builder_.InlineSize(),
+      ComputeBlockSizeForFragment(
+          ConstraintSpace(), Style(), BorderPadding(),
+          CalculateDefaultBlockSize(ConstraintSpace(), Node(),
+                                    BorderScrollbarPadding()),
+          container_builder_.InlineSize())};
+  if (!instance->IntrinsicSizes(ConstraintSpace(), document, Node(),
+                                border_box_size, BorderScrollbarPadding(),
+                                input.percentage_resolution_block_size, &scope,
+                                &intrinsic_sizes_result_options,
+                                &depends_on_percentage_block_size)) {
     // TODO(ikilpatrick): Report this error to the developer.
     return FallbackMinMaxSizes(input);
   }
@@ -94,8 +102,14 @@ scoped_refptr<const NGLayoutResult> NGCustomLayoutAlgorithm::Layout() {
 
   FragmentResultOptions* fragment_result_options = nullptr;
   scoped_refptr<SerializedScriptValue> fragment_result_data;
-  if (!instance->Layout(ConstraintSpace(), document, Node(),
-                        container_builder_.InitialBorderBoxSize(),
+  LogicalSize border_box_size{
+      container_builder_.InlineSize(),
+      ComputeBlockSizeForFragment(
+          ConstraintSpace(), Style(), BorderPadding(),
+          CalculateDefaultBlockSize(ConstraintSpace(), Node(),
+                                    BorderScrollbarPadding()),
+          container_builder_.InlineSize())};
+  if (!instance->Layout(ConstraintSpace(), document, Node(), border_box_size,
                         BorderScrollbarPadding(), &scope,
                         fragment_result_options, &fragment_result_data)) {
     // TODO(ikilpatrick): Report this error to the developer.

third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
@@ -7,6 +7,7 @@
 #include <memory>
 #include "base/optional.h"
 #include "third_party/blink/renderer/core/layout/flexible_box_algorithm.h"
+#include "third_party/blink/renderer/core/layout/geometry/logical_size.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/layout/layout_button.h"
 #include "third_party/blink/renderer/core/layout/layout_flexible_box.h"
@@ -36,7 +37,6 @@ NGFlexLayoutAlgorithm::NGFlexLayoutAlgorithm(
       is_column_(Style().ResolvedIsColumnFlexDirection()),
       is_horizontal_flow_(FlexLayoutAlgorithm::IsHorizontalFlow(Style())),
       is_cross_size_definite_(IsContainerCrossSizeDefinite()) {
-  border_box_size_ = container_builder_.InitialBorderBoxSize();
   child_percentage_size_ = CalculateChildPercentageSize(
       ConstraintSpace(), Node(), ChildAvailableSize());
 
@@ -52,7 +52,7 @@ bool NGFlexLayoutAlgorithm::MainAxisIsInlineAxis(
 
 LayoutUnit NGFlexLayoutAlgorithm::MainAxisContentExtent(
     LayoutUnit sum_hypothetical_main_size) const {
-  if (Style().ResolvedIsColumnFlexDirection()) {
+  if (is_column_) {
     // Even though we only pass border_padding in the third parameter, the
     // return value includes scrollbar, so subtract scrollbar to get content
     // size.
@@ -65,7 +65,7 @@ LayoutUnit NGFlexLayoutAlgorithm::MainAxisContentExtent(
                ConstraintSpace(), Style(), BorderPadding(),
                sum_hypothetical_main_size.ClampNegativeToZero() +
                    border_scrollbar_padding,
-               border_box_size_.inline_size) -
+               container_builder_.InlineSize()) -
            border_scrollbar_padding;
   }
   return ChildAvailableSize().inline_size;
@@ -163,33 +163,15 @@ void NGFlexLayoutAlgorithm::HandleOutOfFlowPositioned(NGBlockNode child) {
 
 bool NGFlexLayoutAlgorithm::IsColumnContainerMainSizeDefinite() const {
   DCHECK(is_column_);
-  // If this flex container is also a flex item, it might have a definite size
-  // imposed on it by its parent flex container.
-  // We can't rely on BlockLengthUnresolvable for this case because that
-  // considers Auto as unresolvable even when the block size is fixed and
-  // definite.
-  if (ConstraintSpace().IsFixedBlockSize() &&
-      !ConstraintSpace().IsFixedBlockSizeIndefinite())
-    return true;
-  Length main_size = Style().LogicalHeight();
-  return !BlockLengthUnresolvable(ConstraintSpace(), main_size,
-                                  LengthResolvePhase::kLayout);
+  return ChildAvailableSize().block_size != kIndefiniteSize;
 }
 
 bool NGFlexLayoutAlgorithm::IsContainerCrossSizeDefinite() const {
   // A column flexbox's cross axis is an inline size, so is definite.
   if (is_column_)
     return true;
-  // If this flex container is also a flex item, it might have a definite size
-  // imposed on it by its parent flex container.
-  // TODO(dgrogan): Removing this check doesn't cause any tests to fail. Remove
-  // it if unneeded or add a test that needs it.
-  if (ConstraintSpace().IsFixedBlockSize() &&
-      !ConstraintSpace().IsFixedBlockSizeIndefinite())
-    return true;
 
-  return !BlockLengthUnresolvable(ConstraintSpace(), Style().LogicalHeight(),
-                                  LengthResolvePhase::kLayout);
+  return ChildAvailableSize().block_size != kIndefiniteSize;
 }
 
 bool NGFlexLayoutAlgorithm::DoesItemStretch(const NGBlockNode& child) const {
@@ -377,8 +359,10 @@ NGConstraintSpace NGFlexLayoutAlgorithm::BuildSpaceForIntrinsicBlockSize(
   // For determining the intrinsic block-size we make %-block-sizes resolve
   // against an indefinite size.
   LogicalSize child_percentage_size = child_percentage_size_;
-  if (is_column_)
+  if (is_column_) {
     child_percentage_size.block_size = kIndefiniteSize;
+    space_builder.SetIsFixedBlockSizeIndefinite(true);
+  }
 
   space_builder.SetAvailableSize(child_available_size);
   space_builder.SetPercentageResolutionSize(child_percentage_size);
@@ -1061,8 +1045,8 @@ scoped_refptr<const NGLayoutResult> NGFlexLayoutAlgorithm::LayoutInternal() {
     main_axis_end_offset = BorderScrollbarPadding().inline_end;
   }
   FlexLine* line;
-  while (
-      (line = algorithm_->ComputeNextFlexLine(border_box_size_.inline_size))) {
+  while ((line = algorithm_->ComputeNextFlexLine(
+              container_builder_.InlineSize()))) {
     line->SetContainerMainInnerSize(
         MainAxisContentExtent(line->sum_hypothetical_main_size_));
     line->FreezeInflexibleItems();
@@ -1207,7 +1191,7 @@ scoped_refptr<const NGLayoutResult> NGFlexLayoutAlgorithm::LayoutInternal() {
                               BorderScrollbarPadding(), intrinsic_block_size);
   LayoutUnit block_size = ComputeBlockSizeForFragment(
       ConstraintSpace(), Style(), BorderPadding(), intrinsic_block_size,
-      border_box_size_.inline_size);
+      container_builder_.InlineSize());
 
   container_builder_.SetIntrinsicBlockSize(intrinsic_block_size);
   container_builder_.SetFragmentsTotalBlockSize(block_size);

third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.h
@@ -86,7 +86,6 @@ class CORE_EXPORT NGFlexLayoutAlgorithm
   const bool is_horizontal_flow_;
   const bool is_cross_size_definite_;
   bool ignore_child_scrollbar_changes_ = false;
-  LogicalSize border_box_size_;
   LogicalSize child_percentage_size_;
   base::Optional<FlexLayoutAlgorithm> algorithm_;
 };

third_party/blink/renderer/core/layout/ng/ng_constraint_space.h
@@ -435,7 +435,18 @@ class CORE_EXPORT NGConstraintSpace final {
 
   bool IsFixedBlockSize() const { return bitfields_.is_fixed_block_size; }
 
-  // Whether a fixed block-size should be considered indefinite.
+  // Whether the block size should be considered indefinite.
+  // The constraint space can have any of the combinations:
+  // (1) !IsFixedBlockSize && !IsFixedBlockSizeIndefinite -- default. no special
+  //     handling needed.
+  // (2) !IsFixedBlockSize && IsFixedBlockSizeIndefinite -- Treat your height as
+  //     indefinite.
+  // (3) IsFixedBlockSize && !IsFixedBlockSizeIndefinite -- You must be this
+  //     size and your children can resolve % block size against it.
+  // (4) IsFixedBlockSize && IsFixedBlockSizeIndefinite -- You must be this
+  //     size but your children canNOT resolve % block size against it.
+  // TODO(dgrogan): This method needs a new name now that #2 above exists.
+  // Either IsBlockSizeIndefinite or ForceBlockSizeToIndefinite.
   bool IsFixedBlockSizeIndefinite() const {
     return bitfields_.is_fixed_block_size_indefinite;
   }

third_party/blink/renderer/core/layout/ng/ng_layout_result_caching_test.cc
@@ -1673,6 +1673,29 @@ TEST_F(NGLayoutResultCachingTest, HitFlexLegacyGrid) {
   EXPECT_NE(result.get(), nullptr);
 }
 
+TEST_F(NGLayoutResultCachingTest, HitFlexDefiniteChange) {
+  SetBodyInnerHTML(R"HTML(
+    <div style="display: flex; flex-direction: column;">
+      <div style="height: 200px;" id=target1>
+        <div style="height: 100px"></div>
+      </div>
+    </div>
+  )HTML");
+
+  auto* target1 = To<LayoutBlock>(GetLayoutObjectByElementId("target1"));
+
+  scoped_refptr<const NGLayoutResult> result1 =
+      target1->GetCachedLayoutResult();
+  scoped_refptr<const NGLayoutResult> measure1 =
+      target1->GetCachedMeasureResult();
+  EXPECT_EQ(measure1->IntrinsicBlockSize(), 100);
+  EXPECT_EQ(result1->PhysicalFragment().Size().height, 200);
+
+  EXPECT_EQ(result1->GetConstraintSpaceForCaching().CacheSlot(),
+            NGCacheSlot::kMeasure);
+  EXPECT_EQ(result1.get(), measure1.get());
+}
+
 TEST_F(NGLayoutResultCachingTest, HitOrthogonalRoot) {
   SetBodyInnerHTML(R"HTML(
     <style>

third_party/blink/renderer/core/layout/ng/ng_layout_utils.cc
@@ -303,17 +303,12 @@ NGLayoutCacheStatus CalculateSizeBasedLayoutCacheStatusWithGeometry(
     // percentages against a fixed block-size.
     // We miss the cache if the %-resolution block-size changes from indefinite
     // to definite (or visa-versa).
-    bool is_new_initial_block_size_indefinite =
-        new_space.IsFixedBlockSize() ? new_space.IsFixedBlockSizeIndefinite()
-                                     : is_initial_block_size_indefinite;
 
     bool is_old_initial_block_size_indefinite =
-        old_space.IsFixedBlockSize()
-            ? old_space.IsFixedBlockSizeIndefinite()
-            : layout_result.IsInitialBlockSizeIndefinite();
+        layout_result.IsInitialBlockSizeIndefinite();
 
     if (is_old_initial_block_size_indefinite !=
-        is_new_initial_block_size_indefinite)
+        is_initial_block_size_indefinite)
       return NGLayoutCacheStatus::kNeedsLayout;
 
     // %-block-size children of table-cells have different behaviour if they
@@ -326,10 +321,10 @@ NGLayoutCacheStatus CalculateSizeBasedLayoutCacheStatusWithGeometry(
     // If our initial block-size is definite, we know that if we change our
     // block-size we'll affect any descendant that depends on the resulting
     // percentage block-size.
-    if (!is_block_size_equal && !is_new_initial_block_size_indefinite)
+    if (!is_block_size_equal && !is_initial_block_size_indefinite)
       return NGLayoutCacheStatus::kNeedsLayout;
 
-    DCHECK(is_block_size_equal || is_new_initial_block_size_indefinite);
+    DCHECK(is_block_size_equal || is_initial_block_size_indefinite);
 
     // At this point we know that either we have the same block-size for our
     // fragment, or our initial block-size was indefinite.
@@ -342,7 +337,7 @@ NGLayoutCacheStatus CalculateSizeBasedLayoutCacheStatusWithGeometry(
     // As we only care about the quirks-mode %-block-size behaviour we remove
     // this false-positive by checking if we have an initial indefinite
     // block-size.
-    if (is_new_initial_block_size_indefinite &&
+    if (is_initial_block_size_indefinite &&
         physical_fragment.DependsOnPercentageBlockSize()) {
       DCHECK(is_old_initial_block_size_indefinite);
       if (new_space.PercentageResolutionBlockSize() !=

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 488000e9b47143117dee9bf8594f24e169616e02

Fixes layout for display alignment UI.
This fixes a bug introduced by a72084e865022b09ca41fbc4a571f6dbf3c3bd3f
where the UI of Settings>Device>Displays>Arrangement would no longer
render correctly due to a change in css precedence.

This also re-enables and fixes the corresponding tests.

Bug: 1033476
Change-Id: Ie1bfce07004c783b9f40e672183fb00680767c77
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673964
Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
Cr-Commit-Position: refs/heads/master@{#850653}

chrome/browser/resources/settings/chromeos/device_page/display.html
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         flex: 1 1;
         height: 300px;
         margin: 10px;
+        min-height: 300px;
       }
 
       .text-area {

chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
@@ -981,8 +981,13 @@ TEST_F('OSSettingsDevicePageTest', 'MAYBE_DevicePageTest', () => {
   mocha.grep(assert(device_page_tests.TestNames.DevicePage)).run();
 });
 
-// Fails after https://chromium-review.googlesource.com/c/chromium/src/+/2640774
-TEST_F('OSSettingsDevicePageTest', 'DISABLED_DisplayTest', () => {
+// Flaky in debug. https://crbug.com/1003483
+GEN('#if !defined(NDEBUG)');
+GEN('#define MAYBE_DisplayTest DISABLED_DisplayTest');
+GEN('#else');
+GEN('#define MAYBE_DisplayTest DisplayTest');
+GEN('#endif');
+TEST_F('OSSettingsDevicePageTest', 'MAYBE_DisplayTest', () => {
   mocha.grep(assert(device_page_tests.TestNames.Display)).run();
 });
 