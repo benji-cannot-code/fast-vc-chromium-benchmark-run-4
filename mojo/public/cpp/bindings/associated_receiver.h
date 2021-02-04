784db3285c25dd8af1d5daed2c6a5e2061767bf7
Daniel Cheng <dcheng@chromium.org>
2021-02-04 19:06:43 +0000

    [mojo] Disallow unbinding an unbound AssociatedReceiver.
    Bug: 1174287
    Change-Id: I9fbedca73110bc73c9d63b9993787950f6bcdb72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2674705
    Commit-Queue: Ken Rockot <rockot@google.com>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#850690}

--- mojo/public/cpp/bindings/associated_receiver.h
+++ mojo/public/cpp/bindings/associated_receiver.h
@@ -216,6 +216,7 @@ class AssociatedReceiver {
   // and the AssociatedRemote will never be able to receive its expected
   // responses.
   PendingAssociatedReceiver<Interface> Unbind() WARN_UNUSED_RESULT {
+    DCHECK(is_bound());
     return PendingAssociatedReceiver<Interface>(binding_.Unbind().PassHandle());
   }
 
