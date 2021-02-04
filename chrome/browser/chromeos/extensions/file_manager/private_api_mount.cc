6491255f02175badf74a24ceda1eb6154c3c7733
Jérémie Boulic <jboulic@chromium.org>
2021-02-04 01:09:00 +0000

    Files app: Notify file tasks when mounting files
    FilesTaskNotifier notifies observers whenever a file is opened through
    the Files App, using chrome.fileManagerPrivate.executeTask. The launcher
    suggestion chip on ChromeOS needs it to know about files
    that have recently been open.
    
    However, when mounting files using the implementation of the Files app
    itself, chrome.fileManagerPrivate.addMount is called instead.
    
    Use NotifyFileTasks to notify file tasks observers about files that
    have just been mounted. An automated integration test will be added in
    a follow-up CL.
    
    system, check that the opened file is recommended in the launcher.
    
    Test: Manual test, open a RAR file and a ZIP file with the new ZIP
    Bug: 1097659
    Change-Id: Iea0d6127dc1b474d066dd6ad0fae4cf8fd40e467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2671719
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850384}

--- chrome/browser/chromeos/extensions/file_manager/private_api_mount.cc
+++ chrome/browser/chromeos/extensions/file_manager/private_api_mount.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "chrome/browser/chromeos/drive/file_system_util.h"
 #include "chrome/browser/chromeos/extensions/file_manager/private_api_util.h"
+#include "chrome/browser/chromeos/file_manager/file_tasks_notifier.h"
 #include "chrome/browser/chromeos/file_manager/fileapi_util.h"
 #include "chrome/browser/chromeos/file_manager/volume_manager.h"
 #include "chrome/browser/chromeos/smb_client/smb_service.h"
@@ -29,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/drive/event_logger.h"
 #include "content/public/browser/browser_thread.h"
 #include "google_apis/drive/task_util.h"
+#include "storage/browser/file_system/file_system_url.h"
 #include "ui/shell_dialogs/selected_file_info.h"
 
 namespace extensions {
@@ -59,6 +61,21 @@ ExtensionFunction::ResponseAction FileManagerPrivateAddMountFunction::Run() {
 
   if (path.empty())
     return RespondNow(Error("Invalid path"));
+
+  if (auto* notifier =
+          file_manager::file_tasks::FileTasksNotifier::GetForProfile(
+              chrome_details_.GetProfile())) {
+    const scoped_refptr<storage::FileSystemContext> file_system_context =
+        file_manager::util::GetFileSystemContextForRenderFrameHost(
+            chrome_details_.GetProfile(), render_frame_host());
+
+    std::vector<storage::FileSystemURL> urls;
+    const storage::FileSystemURL url =
+        file_system_context->CrackURL(GURL(params->source));
+    urls.push_back(url);
+
+    notifier->NotifyFileTasks(urls);
+  }
 
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
