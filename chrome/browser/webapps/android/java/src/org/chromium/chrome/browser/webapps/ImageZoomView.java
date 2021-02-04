2f49c7b5a67e454837c46e2324e814e6b6d7a259
Francois Beaufort <beaufort.francois@gmail.com>
2021-02-04 09:33:14 +0000

    [Android] Add basic screenshot view in bottom-sheet UI for PWA installs
    This CL adds a simple view for showing a single screenshot full screen
    when user taps on it in the bottom-sheet UI for PWA installs. Polishing
    will be done in upcoming CLs.
    
    Screenshots: https://imgur.com/a/JKdg7aJ
    
    Bug: 1146450
    Change-Id: I5706f262fda3a12547a86ccc4b8ed8867d59925d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2667852
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850511}

---
+++ chrome/browser/webapps/android/java/src/org/chromium/chrome/browser/webapps/ImageZoomView.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.webapps;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * UI for the zoomed image view used for screenshots in the bottom-sheet UI for PWA installs.
+ */
+public class ImageZoomView extends AlertDialog {
+    public ImageZoomView(Context context, Bitmap bitmap) {
+        super(context, R.style.Theme_Chromium_Fullscreen);
+
+        View view = LayoutInflater.from(context).inflate(R.layout.image_zoom_view, null);
+        view.setOnClickListener(v -> dismiss());
+        ((ImageView) view.findViewById(R.id.image_zoom)).setImageBitmap(bitmap);
+        setView(view);
+    }
+}
