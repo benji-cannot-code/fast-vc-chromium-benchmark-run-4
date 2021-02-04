4bfac242ef03800d1c9a10744d46671ff0c0f8c4
Michael Thiessen <mthiesse@chromium.org>
2021-02-04 21:58:24 +0000

    Reland: Ensure Trusted Intent Extras are only added to intents targeting Chrome
    Ensures that intents we add our Trusted Intent Extras to can only
    possibly target Chrome, and not other apps unintentionally.
    
    Originally landed in
    https://chromium-review.googlesource.com/c/chromium/src/+/2660775
    
    Bug: 1172513, 1173811
    Change-Id: I16b404a97da1e72ef4d0420f79547c84222cfcfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673716
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850802}

--- chrome/android/javatests/src/org/chromium/chrome/browser/translate/TranslateIntentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/translate/TranslateIntentTest.java
@@ -75,6 +75,8 @@ public class TranslateIntentTest {
         if (targetLanguageCode != null) {
             intent.putExtra(TranslateIntentHandler.EXTRA_TARGET_LANGUAGE_CODE, targetLanguageCode);
         }
+        intent.setClassName(ContextUtils.getApplicationContext(),
+                TranslateIntentHandler.COMPONENT_TRANSLATE_DISPATCHER);
         IntentHandler.addTrustedIntentExtras(intent);
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> mActivityTestRule.getActivity().onNewIntent(intent));
@@ -263,7 +265,7 @@ public class TranslateIntentTest {
         Context context = ContextUtils.getApplicationContext();
 
         final Intent intent = new Intent(TranslateIntentHandler.ACTION_TRANSLATE_TAB);
-        intent.setClassName(context, "com.google.android.apps.chrome.TranslateDispatcher");
+        intent.setClassName(context, TranslateIntentHandler.COMPONENT_TRANSLATE_DISPATCHER);
         intent.putExtra(TranslateIntentHandler.EXTRA_EXPECTED_URL, url);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         // The Activity is already launched so we cannot use startActivityCompletely().
