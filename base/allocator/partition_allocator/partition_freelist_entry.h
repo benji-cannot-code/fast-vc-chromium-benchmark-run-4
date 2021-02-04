6198d18d1057772277d874b398f69e7af7a874fc
Benoit Lize <lizeb@chromium.org>
2021-02-04 12:58:36 +0000

    [PartitionAlloc] Enable freelist hardening on Windows.
    Freelist hardening is disabled on Windows because it was causing
    failures on some post-CQ bots. This was likely due to an actual
    Use-After-Free in the thread cache during process teardown. Now that the
    root cause has been found and addressed, re-enable it.
    
    Note to sheriff: If Windows bots break in range, this is the likely
    culprit, feel free to revert.
    
    Bug: 998048, 1159411
    Change-Id: Ice2c4267adb0f77cf63f323c4ebe95f2c0e6dcf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673824
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850561}

--- base/allocator/partition_allocator/partition_freelist_entry.h
+++ base/allocator/partition_allocator/partition_freelist_entry.h
@@ -18,12 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Enable free list hardening.
 //
 // Disabled on ARM64 Macs, as this crashes very early (crbug.com/1172236).
-// Enabled for DCHECK() builds only on Windows, as this causes issues in some
-// tests.
-//
 // TODO(lizeb): Enable in as many configurations as possible.
-#if !(defined(OS_MAC) && defined(ARCH_CPU_ARM64)) && \
-    (DCHECK_IS_ON() || !defined(OS_WIN))
+#if !(defined(OS_MAC) && defined(ARCH_CPU_ARM64))
 #define PA_HAS_FREELIST_HARDENING
 #endif
 
