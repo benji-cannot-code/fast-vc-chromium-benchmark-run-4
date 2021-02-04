600dafe3ec06329702c51a91d3fe078ce80a966f
Gavin Williams <gavinwill@chromium.org>
2021-02-04 19:45:04 +0000

    scanning: Create scanners loading page
    Show a loading page while searching for scanners. An animated image will
    be added when available.
    
    Next CL will introduce a page to show when no scanners are found.
    
    http://screen/3CUEfL6YiSGN8S5
    
    Bug: 1174137
    Change-Id: I983e9c56b465abe9a348d8ded3870283e4983fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673009
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850712}

---
+++ chromeos/components/scanning/resources/loading_page.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/polymer/v3_0/paper-progress/paper-progress.js';
+
+import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+/**
+ * @fileoverview
+ * 'loading-page' is shown while searching for available scanners.
+ */
+Polymer({
+  is: 'loading-page',
+
+  _template: html`{__html_template__}`,
+
+  behaviors: [I18nBehavior],
+});
