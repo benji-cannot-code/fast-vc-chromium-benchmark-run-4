cff45603dbdfef5e60a6ef6d02b01d9174eeaa2f
Rachel Wong <wrong@chromium.org>
2021-02-04 22:18:40 +0000

    [omnibox] Display the title and details for rich entity results.
    This CL adds logic to handle the display text for non-answer rich entity
    results. The next step will be to download image icons.
    
    To test this CL, enable #omnibox-rich-entities-in-launcher and add the
    following command-line flag:
    --force-variation-ids=t4461027
    
    Bug: 1130372
    Change-Id: Icb60d3870471b44d67bd48359a02561e313d0885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2666308
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850809}

--- ash/app_list/views/search_result_view.cc
+++ ash/app_list/views/search_result_view.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/views/search_result_page_view.h"
 #include "ash/public/cpp/app_list/app_list_color_provider.h"
 #include "ash/public/cpp/app_list/app_list_config.h"
+#include "ash/public/cpp/app_list/app_list_features.h"
 #include "ash/public/cpp/app_list/app_list_switches.h"
 #include "ash/public/cpp/app_list/app_list_types.h"
 #include "base/bind.h"
@@ -137,8 +138,9 @@ void SearchResultView::CreateTitleRenderText() {
 }
 
 void SearchResultView::CreateDetailsRenderText() {
-  // Ensures single line row for omnibox non-url search result.
-  if (result()->is_omnibox_search()) {
+  if (result()->is_omnibox_search() &&
+      !app_list_features::IsOmniboxRichEntitiesEnabled()) {
+    // Ensures single line row for omnibox non-url search result.
     details_text_.reset();
     return;
   }
