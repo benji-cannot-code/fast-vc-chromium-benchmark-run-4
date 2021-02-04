8bfba2e6c33eabd74a20e11958a94520aaa16b17
Ayman Almadhoun <ayman@chromium.org>
2021-02-04 18:40:24 +0000

    Add ProductPriceUpdatePageAnnotation.
    This CL extends PageAnnotationsService by adding a new type to
    represent product price updates generated on the server.
    
    Bug: 1174612
    Change-Id: Icc7b3436526fd5c709bb463decc13000a472bed4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2668671
    Commit-Queue: Ayman Almadhoun <ayman@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850668}

--- chrome/browser/page_annotations/test/android/java/src/org/chromium/chrome/browser/page_annotations/BuyableProductPageAnnotationUnitTest.java
+++ chrome/browser/page_annotations/test/android/java/src/org/chromium/chrome/browser/page_annotations/BuyableProductPageAnnotationUnitTest.java
@@ -20,7 +20,7 @@ import org.chromium.chrome.browser.page_annotations.PageAnnotation.PageAnnotatio
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class BuyableProductPageAnnotationUnitTest {
-    private static final long FAKE_PRICE_MICORS = 10000L;
+    private static final String FAKE_PRICE_MICORS = "10000";
     private static final String FAKE_CURRENCY_CODE = "USD";
 
     @Test
@@ -29,15 +29,15 @@ public class BuyableProductPageAnnotationUnitTest {
                 PageAnnotationsTestUtils.createFakeBuyableProductJson(
                         true, FAKE_PRICE_MICORS, FAKE_CURRENCY_CODE));
         Assert.assertNotNull(annotation);
-        Assert.assertEquals(annotation.getType(), PageAnnotationType.BUYABLE_PRODUCT);
-        Assert.assertEquals(annotation.getCurrencyCode(), FAKE_CURRENCY_CODE);
-        Assert.assertEquals(annotation.getCurrentPriceMicros(), FAKE_PRICE_MICORS);
+        Assert.assertEquals(PageAnnotationType.BUYABLE_PRODUCT, annotation.getType());
+        Assert.assertEquals(FAKE_CURRENCY_CODE, annotation.getCurrencyCode());
+        Assert.assertEquals(10000L, annotation.getCurrentPriceMicros());
     }
 
     @Test
     public void testFromMissingPriceMetadataJson() throws JSONException {
         BuyableProductPageAnnotation annotation = BuyableProductPageAnnotation.fromJson(
-                PageAnnotationsTestUtils.createFakeBuyableProductJson());
+                PageAnnotationsTestUtils.createEmptyBuyableProduct());
         Assert.assertNull(annotation);
     }
 
@@ -51,6 +51,14 @@ public class BuyableProductPageAnnotationUnitTest {
 
     @Test
     public void testFromMissingCurrencyCodeJson() throws JSONException {
+        BuyableProductPageAnnotation annotation = BuyableProductPageAnnotation.fromJson(
+                PageAnnotationsTestUtils.createFakeBuyableProductJson(
+                        true, FAKE_PRICE_MICORS, null));
+        Assert.assertNull(annotation);
+    }
+
+    @Test
+    public void testFromBadPriceAmountJson() throws JSONException {
         BuyableProductPageAnnotation annotation = BuyableProductPageAnnotation.fromJson(
                 PageAnnotationsTestUtils.createFakeBuyableProductJson(
                         true, FAKE_PRICE_MICORS, null));
