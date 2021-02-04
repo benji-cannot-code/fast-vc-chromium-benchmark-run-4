d900b1fe0439bc0469823d26fa266fe1ddf7f349
Ayu Ishii <ayui@chromium.org>
2021-02-04 10:19:59 +0000

    StorageBuckets: Add idlharness test
    This change adds a idlharness test for Storage Buckets API.
    The IDL is tentative and is not generated because there is no
    spec draft yet. This is expected to eventually be replaced by
    a generated IDL file created from the spec.
    
    Bug: 1166018
    Change-Id: I366562636bf62fbd28f4a018a6ab7824f25ae7e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673960
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850522}

---
+++ third_party/blink/web_tests/wpt_internal/storage/buckets/idlharness-worker.https.any.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+idl_test(
+  ['storage-buckets.tentative'],
+  ['html'],
+  async (idl_array, t) => {
+    idl_array.add_objects({
+      BucketManager: ['navigator.storageBuckets'],
+      StorageBucket: []
+    });
+
+    if (self.Window) {
+      idl_array.add_objects({ Navigator: ['navigator'] });
+    } else {
+      idl_array.add_objects({ WorkerNavigator: ['navigator'] });
+    }
+  }
+);
