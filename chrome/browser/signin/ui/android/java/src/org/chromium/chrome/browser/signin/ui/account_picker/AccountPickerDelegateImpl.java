a3d788d113a8d1b361a6fc088b1b23daa165d723
Tanmoy Mollik <triploblastic@chromium.org>
2021-02-04 18:45:27 +0000

    [Signin][Modularization] Move AccountPickerDelegateImpl to signin.ui
    This cl moves AccountPickerDelegateImpl and
    AccountPickerDelegateImplTest to signin.ui.
    
    AccountPickerDelegateTest is renamed to AccountPickerDelegateImplTest.
    
    Bug: 1166153
    Change-Id: Ie50724bb5d16875a26e232501e428e470a96e63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2671961
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850675}

--- chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/account_picker/AccountPickerDelegateImpl.java
+++ chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/account_picker/AccountPickerDelegateImpl.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.signin.account_picker;
+package org.chromium.chrome.browser.signin.ui.account_picker;
 
 import android.accounts.AccountManager;
 import android.app.Activity;
@@ -19,7 +19,6 @@ import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.services.SigninManager;
 import org.chromium.chrome.browser.signin.services.WebSigninBridge;
 import org.chromium.chrome.browser.signin.ui.SigninUtils;
-import org.chromium.chrome.browser.signin.ui.account_picker.AccountPickerDelegate;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.components.signin.AccountManagerFacadeProvider;
 import org.chromium.components.signin.AccountUtils;
--- chrome/browser/signin/ui/android/junit/src/org/chromium/chrome/browser/signin/ui/account_picker/AccountPickerDelegateImplTest.java
+++ chrome/browser/signin/ui/android/junit/src/org/chromium/chrome/browser/signin/ui/account_picker/AccountPickerDelegateImplTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.signin.account_picker;
+package org.chromium.chrome.browser.signin.ui.account_picker;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -54,7 +54,7 @@ import java.lang.ref.WeakReference;
  * This class tests the {@link AccountPickerDelegateImpl}.
  */
 @RunWith(BaseRobolectricTestRunner.class)
-public class AccountPickerDelegateTest {
+public class AccountPickerDelegateImplTest {
     private static final String CONTINUE_URL = "https://test-continue-url.com";
 
     private final FakeAccountManagerFacade mFakeAccountManagerFacade =
