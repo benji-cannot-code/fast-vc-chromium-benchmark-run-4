3d147889a1f19b5f126bc4437c774ef6b170168c
Vladimir Levin <vmpstr@chromium.org>
2021-02-04 21:17:18 +0000

    SET/viz: Issue copy output requests and save results in a saved frame.
    This patch issues copy output requests and stashed them on the saved frame
    for use later.
    
    R=kylechar@chromium.org, vasilyt@chromium.org
    
    Fixed: 1174107
    Change-Id: I8cb61ec133a2f535ccab71b827cae7b46d9190e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673380
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850771}

--- components/viz/service/surfaces/surface_saved_frame.h
+++ components/viz/service/surfaces/surface_saved_frame.h
@@ -6,16 +6,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_SERVICE_SURFACES_SURFACE_SAVED_FRAME_H_
 #define COMPONENTS_VIZ_SERVICE_SURFACES_SURFACE_SAVED_FRAME_H_
 
+#include <memory>
+
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
+#include "components/viz/common/frame_sinks/copy_output_result.h"
 #include "components/viz/common/quads/compositor_frame_transition_directive.h"
+#include "components/viz/common/resources/single_release_callback.h"
 #include "components/viz/service/viz_service_export.h"
 
 namespace viz {
+
+class Surface;
 
 class VIZ_SERVICE_EXPORT SurfaceSavedFrame {
  public:
   explicit SurfaceSavedFrame(
       const CompositorFrameTransitionDirective& directive);
+  ~SurfaceSavedFrame();
 
   // Returns true iff the frame is valid and complete.
   bool IsValid() const;
@@ -23,8 +31,22 @@ class VIZ_SERVICE_EXPORT SurfaceSavedFrame {
   // Returns the animation duration from the saved directive.
   base::TimeDelta animation_duration() const { return directive_.duration(); }
 
+  // Appends copy output requests to the needed render passes in the active
+  // frame.
+  void RequestCopyOfOutput(Surface* surface);
+
+  // For testing functionality that ensures that we have a valid frame.
+  void CompleteSavedFrameForTesting();
+
  private:
+  void NotifyCopyOfOutputComplete(std::unique_ptr<CopyOutputResult> result);
+
   CompositorFrameTransitionDirective directive_;
+
+  CopyOutputResult::TextureResult texture_result_;
+  std::unique_ptr<SingleReleaseCallback> texture_release_callback_;
+
+  base::WeakPtrFactory<SurfaceSavedFrame> weak_factory_{this};
 };
 
 }  // namespace viz
