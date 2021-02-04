26ccd21eb8d73e5302a46b18b9eb62c24c2da71b
Anastasia Helfinstein <anastasi@google.com>
2021-02-04 01:25:21 +0000

    [Switch Access] Add flag for setup guide
    In preparation for the addition of a setup guide, add a flag to hide
    the changes until launch.
    
    Bug: None
    Change-Id: I575714b064f2ee171ce907a9133692f7fad715ce
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2665413
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#850392}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2515,6 +2515,9 @@ extern const char kExperimentalAccessibilitySwitchAccessTextDescription[];
 extern const char kSwitchAccessPointScanningName[];
 extern const char kSwitchAccessPointScanningDescription[];
 
+extern const char kExperimentalAccessibilitySwitchAccessSetupGuideName[];
+extern const char kExperimentalAccessibilitySwitchAccessSetupGuideDescription[];
+
 extern const char kMagnifierNewFocusFollowingName[];
 extern const char kMagnifierNewFocusFollowingDescription[];
 
