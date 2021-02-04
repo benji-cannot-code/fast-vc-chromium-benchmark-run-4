f3dcea52dc4bbb1c1e735c2732267eddbfb95d75
Martin Rodriguez <rodmartin@google.com>
2021-02-04 19:03:19 +0000

    Add Device Trust Service class.
    Initial class code for device trust connector.
    
    Change-Id: Ifa386c362cadc02c4eb5798283028cfb4958d3db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673481
    Commit-Queue: Martin Rodriguez <rodmartin@google.com>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850687}

---
+++ chrome/browser/enterprise/connectors/device_trust/device_trust_service.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/connectors/device_trust/device_trust_service.h"
+#include "chrome/browser/profiles/profile.h"
+
+namespace policy {
+
+DeviceTrustService::DeviceTrustService() = default;
+
+DeviceTrustService::DeviceTrustService(Profile* profile)
+    : profile_(profile), prefs_(profile_->GetPrefs()) {}
+
+DeviceTrustService::~DeviceTrustService() = default;
+
+}  // namespace policy
