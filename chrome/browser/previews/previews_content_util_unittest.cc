f6194fe46fd02c25b26eb4e25430af9f431b2ef7
John Delaney <johnidel@chromium.org>
2021-02-04 21:07:07 +0000

    [CodeInclusion] Update terminology in chrome/browser/previews
    Update usage of "blacklist" -> "blocklist"
    
    Change-Id: I1e1f8f46eb82612552c7761baa1dc2205e8efe1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676284
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850758}

--- chrome/browser/previews/previews_content_util_unittest.cc
+++ chrome/browser/previews/previews_content_util_unittest.cc
@@ -60,7 +60,7 @@ blink::PreviewsState CallDetermineAllowedClientPreviewsState(
 }
 
 // A test implementation of PreviewsDecider that simply returns whether the
-// preview type feature is enabled (ignores ECT and blacklist considerations).
+// preview type feature is enabled (ignores ECT and blocklist considerations).
 class PreviewEnabledPreviewsDecider : public PreviewsDecider {
  public:
   PreviewEnabledPreviewsDecider() {}
