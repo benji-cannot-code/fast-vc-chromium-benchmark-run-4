0bbab28092228cb484f2cdb86bcb13104191748d
Luke Zielinski <lpz@chromium.org>
2021-02-04 19:01:39 +0000

    Roll wpt tooling.
    This rolls up to 3eb2cdae07a8842112e804a59dbd0f9800c64e73.
    
    Change-Id: Ia38ebb29865fc5e5e768a00056af71595b919d88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675842
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Auto-Submit: Luke Z <lpz@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850684}

--- third_party/wpt_tools/wpt/tools/wpt/install.py
+++ third_party/wpt_tools/wpt/tools/wpt/install.py
@@ -3,6 +3,8 @@ import argparse
 from . import browser
 
 latest_channels = {
+    'android_weblayer': 'dev',
+    'android_webview': 'dev',
     'firefox': 'nightly',
     'chrome': 'nightly',
     'chrome_android': 'dev',
