2dd084bbc77bccbc91e8a4618ccaaa9e78dbad5f
Brandon Wylie <wylieb@chromium.org>
2021-02-04 20:45:38 +0000

    Remove ChromeActivity reference from HistoryManagerUtils
    Bug: 1155369
    Change-Id: I81be62498860e19f40d28f3854276fe16b9ca193
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2666746
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850721}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManagerUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManagerUtils.java
@@ -5,15 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.history;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.chrome.browser.IntentHandler;
-import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.content_public.browser.LoadUrlParams;
+import org.chromium.ui.base.DeviceFormFactor;
 
 /**
  * Utility methods for the browsing history manager.
@@ -22,13 +23,14 @@ public class HistoryManagerUtils {
     /**
      * Opens the browsing history manager.
      *
-     * @param activity The {@link ChromeActivity} that owns the {@link HistoryManager}.
+     * @param activity The {@link Activity} that owns the {@link HistoryManager}.
      * @param tab The {@link Tab} to used to display the native page version of the
      *            {@link HistoryManager}.
+     * @param isIncognitoSelected Whether the incognito {@TabModelSelector} is selected.
      */
-    public static void showHistoryManager(ChromeActivity activity, Tab tab) {
+    public static void showHistoryManager(Activity activity, Tab tab, boolean isIncognitoSelected) {
         Context appContext = ContextUtils.getApplicationContext();
-        if (activity.isTablet()) {
+        if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(activity)) {
             // History shows up as a tab on tablets.
             LoadUrlParams params = new LoadUrlParams(UrlConstants.NATIVE_HISTORY_URL);
             tab.loadUrl(params);
@@ -36,8 +38,7 @@ public class HistoryManagerUtils {
             Intent intent = new Intent();
             intent.setClass(appContext, HistoryActivity.class);
             intent.putExtra(IntentHandler.EXTRA_PARENT_COMPONENT, activity.getComponentName());
-            intent.putExtra(IntentHandler.EXTRA_INCOGNITO_MODE,
-                    activity.getTabModelSelector().isIncognitoSelected());
+            intent.putExtra(IntentHandler.EXTRA_INCOGNITO_MODE, isIncognitoSelected);
             activity.startActivity(intent);
         }
     }
