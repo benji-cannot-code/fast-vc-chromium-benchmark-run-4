8c3ebeacc1e3cb71df55c45a8a36bc769634e25b
Nick Diego Yamane <nickdiego@igalia.com>
2021-02-04 20:59:13 +0000

    x11: Remove abort timer leftovers from selection requestor
    Get rid of leftovers from https://crrev.com/c/2622521.
    
    R=thomasanderson@chromium.org
    
    Bug: None
    Change-Id: I6ddb0dea83630d31cd2650f2db313ff7d1736243
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675744
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#850744}

--- ui/base/x/selection_requestor.h
+++ ui/base/x/selection_requestor.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/time/time.h"
-#include "base/timer/timer.h"
 #include "ui/events/platform_event.h"
 #include "ui/gfx/x/event.h"
 
@@ -143,9 +142,6 @@ class COMPONENT_EXPORT(UI_BASE) SelectionRequestor {
   // request is the request for which XConvertSelection() has been
   // called and for which we are waiting for a SelectionNotify response.
   size_t current_request_index_;
-
-  // Used to abort requests if the selection owner takes too long to respond.
-  base::RepeatingTimer abort_timer_;
 
   DISALLOW_COPY_AND_ASSIGN(SelectionRequestor);
 };
