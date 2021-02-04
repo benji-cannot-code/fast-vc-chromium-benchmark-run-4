4e31df6e95f9e3b84b148f1c15fa9c89fa04ea42
Adrienne Walker <enne@chromium.org>
2021-02-04 21:09:27 +0000

    cachestorage: remove direct access to (Chrome)BlobStorageContext
    This is a cleanup of a long-ago change I made to how cache storage got
    initialized with the BlobStorageContext.  As cache storage is moving to
    the storage service, it can't access BlobStorageContext directly
    anymore.
    
    This cleans up the initialization dance to pass in the remote at
    construction time rather than a pointer much later on.  It also moves
    BlobStorageContextWrapper (a sequence-bound refcounted remote) which is
    only used by cache storage into the cache storage directory, as cache
    storage will not be able to access blob_storage from the storage
    service.
    
    This is a reland of:
    https://chromium-review.googlesource.com/c/chromium/src/+/2651182
    
    Bug: 1147642,1172212
    Change-Id: I1241abd435a1ccb316d04dcb706d4820db9eee63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2659226
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Auto-Submit: enne <enne@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850762}

--- content/browser/cache_storage/legacy/legacy_cache_storage.h
+++ content/browser/cache_storage/legacy/legacy_cache_storage.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/services/storage/public/mojom/blob_storage_context.mojom.h"
 #include "components/services/storage/public/mojom/cache_storage_control.mojom.h"
+#include "content/browser/cache_storage/blob_storage_context_wrapper.h"
 #include "content/browser/cache_storage/cache_storage.h"
 #include "content/browser/cache_storage/cache_storage_cache_observer.h"
 #include "content/browser/cache_storage/cache_storage_manager.h"
--- content/browser/cache_storage/legacy/legacy_cache_storage_cache.cc
+++ content/browser/cache_storage/legacy/legacy_cache_storage_cache.cc
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_response_headers.h"
 #include "net/http/http_status_code.h"
 #include "services/network/public/mojom/fetch_api.mojom.h"
-#include "storage/browser/blob/blob_storage_context.h"
 #include "storage/browser/quota/quota_manager_proxy.h"
 #include "storage/common/quota/padding_key.h"
 #include "third_party/blink/public/common/cache_storage/cache_storage_utils.h"
