372b358a4dd895b9b123015021d4d8f2378019ef
Sergey Poromov <poromov@chromium.org>
2021-02-04 20:53:20 +0000

    DLP: Send DLP Files policy to Chrome OS daemon.
    When DataLeakPreventionRulesList policy contains
    Files restrictions, Chrome OS DLP daemon should be
    initialized with these rules to enforce restrictions
    on file accesses.
    
    The CL adds the D-Bus client to talk with the daemon
    via SetDlpFilesPolicy() method and code in DlpRulesManager
    that calls it when needed.
    
    Bug: 1169856
    Test: Configure policy with Files restriction, check that daemon is started and receives the policy.
    Change-Id: Ia2d27d38f03f1b3062d3c79456c8a9ef6ce26a7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2656355
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850734}

--- chrome/browser/chromeos/dbus/dbus_helper.cc
+++ chrome/browser/chromeos/dbus/dbus_helper.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/cups_proxy/cups_proxy_client.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/dlcservice/dlcservice_client.h"
+#include "chromeos/dbus/dlp/dlp_client.h"
 #include "chromeos/dbus/hermes/hermes_clients.h"
 #include "chromeos/dbus/initialize_dbus_client.h"
 #include "chromeos/dbus/ip_peripheral/ip_peripheral_service_client.h"
@@ -86,6 +87,7 @@ void InitializeDBus() {
   InitializeDBusClient<CryptohomeClient>(bus);
   InitializeDBusClient<CupsProxyClient>(bus);
   InitializeDBusClient<DlcserviceClient>(bus);
+  InitializeDBusClient<DlpClient>(bus);
   hermes_clients::Initialize(bus);
   InitializeDBusClient<IpPeripheralServiceClient>(bus);
   InitializeDBusClient<KerberosClient>(bus);
