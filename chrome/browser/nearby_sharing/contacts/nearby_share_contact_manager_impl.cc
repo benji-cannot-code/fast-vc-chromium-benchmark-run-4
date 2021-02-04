553defd21eee871f3fb3a851725bea8a04f5c361
Michael Hansen <hansenmichael@google.com>
2021-02-04 19:46:43 +0000

    Revert "[Nearby] Trigger contacts download more frequently."
    This reverts commit df28d32a808db9f5611135cb67055904f50dbc8d.
    
    Reason for revert: Reverting back to 12 hours due to the fact that this
    will increase server traffic as well as traffic to the Chromebook. It
    was decided that on-demand triggers will be more important than
    increasing the frequency of periodic updates.
    
    Original change's description:
    > [Nearby] Trigger contacts download more frequently.
    >
    > This reduces the period for scheduled contacts download from 12 hours to
    > 1 hour. This will improve consistency of the contact list for the
    > device, reducing the chance of encountering a scenario where a receiver
    > has not updated its certificates when a share is attempted.
    >
    > Related: crrev.com/c/2674322
    >
    > Bug: 1172030
    > Change-Id: Ia95d275bc719b71b9ee632934e52cc6708165caf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673204
    > Commit-Queue: Michael Hansen <hansenmichael@google.com>
    > Reviewed-by: Josh Nohle <nohle@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#850432}
    
    TBR=nohle@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,hansenmichael@google.com
    
    Change-Id: Ida47ed0c033232b2f27cacce58da7f4528e21d91
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1172030
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676266
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/master@{#850715}

--- chrome/browser/nearby_sharing/contacts/nearby_share_contact_manager_impl.cc
+++ chrome/browser/nearby_sharing/contacts/nearby_share_contact_manager_impl.cc
@@ -29,7 +29,7 @@ namespace {
 
 constexpr base::TimeDelta kContactUploadPeriod = base::TimeDelta::FromHours(24);
 constexpr base::TimeDelta kContactDownloadPeriod =
-    base::TimeDelta::FromHours(1);
+    base::TimeDelta::FromHours(12);
 constexpr base::TimeDelta kContactDownloadRpcTimeout =
     base::TimeDelta::FromSeconds(60);
 
