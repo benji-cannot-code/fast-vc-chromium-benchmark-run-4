309ee83908ec8e50b1d7835e6bbd6054e873609d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-02-04 16:19:22 +0000

    Import wpt@d7227af30ff9723db9e615fe38d6dff8792c4f46
    Using wpt-import in Chromium 6bbbdf891a62ef84fb1caee73f3b14a98756f3e9.
    With Chromium commits locally applied on WPT:
    4231e93dbb "CSP: Allow unknown directives in csp attribute"
    6bbbdf891a "Update RTCPeerConnection-relay-canvas  WPT test"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    rego@igalia.com:
      external/wpt/css/selectors
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=weizhong@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: I0fcaffbe86f21b321f08b35cd7a909c077d4a4e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675582
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#850597}

--- third_party/blink/web_tests/external/wpt/fetch/api/resources/utils.js
+++ third_party/blink/web_tests/external/wpt/fetch/api/resources/utils.js
@@ -1,12 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-var inWorker = false;
 var RESOURCES_DIR = "../resources/";
-
-try {
-  inWorker = !(self instanceof Window);
-} catch (e) {
-  inWorker = true;
-}
 
 function dirname(path) {
     return path.replace(/\/[^\/]*$/, '/')
