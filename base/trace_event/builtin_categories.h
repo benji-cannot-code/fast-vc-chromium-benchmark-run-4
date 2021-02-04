461b533eb7dfc2c11f450e07c346abed1df33531
Alex Rudenko <alexrudenko@chromium.org>
2021-02-04 16:23:49 +0000

    Add trace events to InspectorContrast
    This CL adds 3 trace events to InspectorContrast so that it is
    easy to check performance of the contrast calculation when needed.
    
    Change-Id: I64d18b1569962a22e9fff587e470841181ebaa16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675240
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850600}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("content_capture")                                                   \
   X("device")                                                            \
   X("devtools")                                                          \
+  X("devtools.contrast")                                                 \
   X("devtools.timeline")                                                 \
   X("disk_cache")                                                        \
   X("download")                                                          \
