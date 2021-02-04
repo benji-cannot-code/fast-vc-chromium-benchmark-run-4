11bfbb9bcec3b1785c63ee07b2759b3277f8d64f
Yuheng Huang <yuhengh@chromium.org>
2021-02-04 21:33:25 +0000

    Tab Search: measure js heap size per tab in benchmark
    Bug: 1099917
    Change-Id: I9bd4ac0f316cec994e7d65d4d5b006419cc29260
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675080
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850782}

--- tools/perf/page_sets/desktop_ui/tab_search_story.py
+++ tools/perf/page_sets/desktop_ui/tab_search_story.py
@@ -221,7 +221,8 @@ class TabSearchStoryMeasureMemory(TabSearchStory):
         'disabled-by-default-memory-infra')
 
   def GetExtraTracingMetrics(self):
-    return ['memoryMetric']
+    return super(TabSearchStoryMeasureMemory,
+                 self).GetExtraTracingMetrics() + ['memoryMetric']
 
 
 class TabSearchStoryMeasureMemoryBefore(TabSearchStoryMeasureMemory):
@@ -262,6 +263,9 @@ class TabSearchStoryMeasureMemory2TabSearch(TabSearchStoryMeasureMemory):
     tabs = action_runner.tab.browser.tabs
     new_tab = tabs.New()
     new_tab.Navigate(TAB_SEARCH_URL)
+    new_tab.WaitForDocumentReadyStateToBeComplete()
+    new_tab.action_runner.ExecuteJavaScript(MEASURE_JS_MEMORY %
+                                            'used_js_heap_size2')
 
   def InteractWithPage(self, action_runner):
     action_runner.MeasureMemory(deterministic_mode=True)
@@ -272,9 +276,12 @@ class TabSearchStoryMeasureMemory3TabSearch(TabSearchStoryMeasureMemory):
 
   def RunNavigateSteps(self, action_runner):
     tabs = action_runner.tab.browser.tabs
-    for _ in range(2):
+    for i in range(2):
       new_tab = tabs.New()
       new_tab.Navigate(TAB_SEARCH_URL)
+      new_tab.WaitForDocumentReadyStateToBeComplete()
+      new_tab.action_runner.ExecuteJavaScript(
+          MEASURE_JS_MEMORY % ('used_js_heap_size' + str(i + 2)))
 
   def InteractWithPage(self, action_runner):
     action_runner.MeasureMemory(deterministic_mode=True)
