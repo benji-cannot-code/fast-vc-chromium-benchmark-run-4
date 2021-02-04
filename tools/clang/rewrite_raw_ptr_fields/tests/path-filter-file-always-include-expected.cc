087a8c727834d526a6c88eecc95f27b65e6058a4
Keishi Hattori <keishi@chromium.org>
2021-02-04 11:40:48 +0000

    Force include blink common/ dirs in the rewrite_raw_ptr_fields rewrite
    The code in blink/public/common and blink/common dirs must be included in the rewrite as they are used from the browser process.
    
    This CL adds a syntax where a line starting with ! can be used to negate a file marked for exclusion from the rewrite.
    
    Change-Id: I63b302bc4bf2aa6e88394cd1d0c23ea7750516a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2671441
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850544}

---
+++ tools/clang/rewrite_raw_ptr_fields/tests/path-filter-file-always-include-expected.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/memory/checked_ptr.h"
+
+class SomeClass;
+
+struct MyStruct {
+  // Rewrite expected - this file is force included in the rewrite using ! in
+  // tests/paths-to-ignore.txt file.
+  CheckedPtr<SomeClass> ptr_field_;
+};
