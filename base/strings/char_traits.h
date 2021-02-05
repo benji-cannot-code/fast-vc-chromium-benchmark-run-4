7ea2044af93d72ceb724953f99d600ce40f9bbff
Jan Wilken Dörrie <jdoerrie@chromium.org>
2021-02-05 07:26:53 +0000

    [base] Add __builtin_wmemchr work around to CharTraits<wchar_t>
    This change adds a workaround to using __builtin_wmemchr due to
    https://bugs.llvm.org/show_bug.cgi?id=41226.
    
    Bug: 1049498
    Change-Id: Ie89922ca7cfd81bf671532273a8bfc89946f1c8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676066
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851013}

--- base/strings/char_traits.h
+++ base/strings/char_traits.h
@@ -14,6 +14,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
+namespace internal {
+
+template <typename T>
+constexpr const T* ConstexprFind(const T* s, size_t n, T c) {
+  for (; n; --n, ++s) {
+    if (std::char_traits<T>::eq(*s, c))
+      return s;
+  }
+  return nullptr;
+}
+
+}  // namespace internal
+
 // constexpr version of http://en.cppreference.com/w/cpp/string/char_traits.
 // This currently just implements the bits needed to support a (mostly)
 // constexpr StringPiece.
@@ -63,11 +76,7 @@ constexpr size_t CharTraits<T>::length(const T* s) noexcept {
 
 template <typename T>
 constexpr const T* CharTraits<T>::find(const T* s, size_t n, T c) {
-  for (; n; --n, ++s) {
-    if (std::char_traits<T>::eq(*s, c))
-      return s;
-  }
-  return nullptr;
+  return internal::ConstexprFind(s, n, c);
 }
 
 // char and wchar_t specialization of CharTraits that can use clang's constexpr
@@ -103,7 +112,9 @@ struct CharTraits<wchar_t> {
   }
 
   static constexpr const wchar_t* find(const wchar_t* s, size_t n, wchar_t c) {
-    return __builtin_wmemchr(s, c, n);
+    // Note: Due to https://bugs.llvm.org/show_bug.cgi?id=41226 we are not
+    // calling `__builtin_wmemchr` here.
+    return internal::ConstexprFind(s, n, c);
   }
 };
 #endif
