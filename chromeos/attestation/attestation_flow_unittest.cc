6f035f7232988665f4deea4149072e746877666f
Leo Lai <cylai@google.com>
2021-02-05 02:43:23 +0000

    Clarify those tests that verify work-around in attestation flows
    The existing test items that verify the work-around is misleading and
    making readers think that the API is by design. Added comment to avoid
    confusion until we come up with a better way to deal with the illegal
    inputs.
    
    BUG=b:179364923
    TEST=build ok.
    
    Change-Id: Ic7b452d09e7e0cb07405de325b9a9ebb0e77c5fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675782
    Commit-Queue: Leo Lai <cylai@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850930}

--- chromeos/attestation/attestation_flow_unittest.cc
+++ chromeos/attestation/attestation_flow_unittest.cc
@@ -597,8 +597,11 @@ TEST_F(AttestationFlowTest, GetMachineCertificateAlreadyEnrolled) {
                 ->certificate());
 }
 
-// We have the usecase where the machine key is requested with the user account
-// id.
+// There used to be an incidence that a non-empty username are sent when
+// requesting a device key certificate, and we remove the username in the
+// attestation flow process though it is not considered a valid input.
+// TODO(b/179364923): Develop a better API design along with strict assertion
+// instead of silently removing the username.
 TEST_F(AttestationFlowTest, GetMachineCertificateWithUsername) {
   chromeos::AttestationClient::Get()
       ->GetTestInterface()
@@ -917,6 +920,11 @@ TEST_F(AttestationFlowTest, GetCertificate_AlreadyExists) {
   RunUntilIdle();
 }
 
+// There used to be an incidence that a non-empty username are sent when
+// requesting a device key certificate, and we remove the username in the
+// attestation flow process though it is not considered a valid input.
+// TODO(b/179364923): Develop a better API design along with strict assertion
+// instead of silently removing the username.
 TEST_F(AttestationFlowTest, GetCertificate_LookupMachineKeyWithAccountId) {
   chromeos::AttestationClient::Get()
       ->GetTestInterface()
