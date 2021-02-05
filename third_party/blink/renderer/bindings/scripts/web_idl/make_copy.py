b37076e5dec8a61e8bcd7168972a8755ae7b51fa
Wojciech Bielawski <wbielawski@fb.com>
2021-02-05 10:44:36 +0000

    Add bug number to TODO
    According to https://google.github.io/styleguide/cppguide.html#TODO_Comments
    tracking bug is needed.
    
    Bug: 1174969
    Change-Id: I53a6adbe37e49e380ceb61ebb872ea3c5e4500bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675912
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851057}

--- third_party/blink/renderer/bindings/scripts/web_idl/make_copy.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/make_copy.py
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import sys
 
-# TODO: Remove this once Python2 is obsoleted.
+# TODO(crbug.com/1174969): Remove this once Python2 is obsoleted.
 if sys.version_info.major != 2:
     long = int
     basestring = str
