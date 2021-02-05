f379614fe538e1f7cfdd4ac3aeecc1cae425fe1f
Zhiyuan Cai <zhiyuancai@google.com>
2021-02-05 01:21:34 +0000

    Set max width for PriceWelcomeMessageCard
    This change sets the side margin of PriceWelcomeMessageCard in GTS to
    114dp in Landscape mode.
    
    Bug: 1148020
    Change-Id: I642184706d295e81d2a8b87f65363c8e074f6229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2600116
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Zhiyuan Cai <zhiyuancai@google.com>
    Cr-Commit-Position: refs/heads/master@{#850894}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/PriceWelcomeMessageCardView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/PriceWelcomeMessageCardView.java
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tasks.tab_management;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.chrome.browser.tab.state.ShoppingPersistedTabData;
 import org.chromium.chrome.browser.util.ChromeAccessibilityUtil;
@@ -26,6 +29,8 @@ import java.lang.ref.WeakReference;
 class PriceWelcomeMessageCardView extends FrameLayout {
     private static WeakReference<Bitmap> sCloseButtonBitmapWeakRef;
 
+    private final Context mContext;
+    private final int mLandscapeSidePadding;
     private PriceCardView mPriceInfoBox;
     private TextView mTitle;
     private TextView mContent;
@@ -34,6 +39,9 @@ class PriceWelcomeMessageCardView extends FrameLayout {
 
     public PriceWelcomeMessageCardView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
+        mLandscapeSidePadding = (int) context.getResources().getDimension(
+                R.dimen.tab_grid_large_message_side_padding_landscape);
     }
 
     @Override
@@ -54,6 +62,12 @@ class PriceWelcomeMessageCardView extends FrameLayout {
                     Bitmap.createScaledBitmap(bitmap, closeButtonSize, closeButtonSize, true));
         }
         mCloseButton.setImageBitmap(sCloseButtonBitmapWeakRef.get());
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        updateWidthWithOrientation(mContext.getResources().getConfiguration().orientation);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     /**
@@ -109,6 +123,15 @@ class PriceWelcomeMessageCardView extends FrameLayout {
      */
     void setPriceInfoBoxStrings(ShoppingPersistedTabData.PriceDrop priceDrop) {
         mPriceInfoBox.setPriceStrings(priceDrop.price, priceDrop.previousPrice);
+    }
+
+    @VisibleForTesting
+    void updateWidthWithOrientation(int orientation) {
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setPadding(0, 0, 0, 0);
+        } else {
+            setPadding(mLandscapeSidePadding, 0, mLandscapeSidePadding, 0);
+        }
     }
 
     // TODO(crbug.com/1166704): This method has little to do with this view. Move this function to a
