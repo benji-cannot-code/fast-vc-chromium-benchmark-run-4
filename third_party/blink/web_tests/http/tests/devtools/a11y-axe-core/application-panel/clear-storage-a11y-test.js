08e0776d3690152dd4923ac763bfbb907d43e801
Sigurd Schneider <sigurds@chromium.org>
2021-02-05 14:30:56 +0000

    Update/Remove DevTools test
    This removes a test that will become an e2e test as part of
    https://crrev.com/c/2667200
    
    Bug: chromium:945786
    Change-Id: I2451bca08374580865dd2cb2b81337ddd2f587c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676762
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851106}

--- third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/application-panel/clear-storage-a11y-test.js
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/application-panel/clear-storage-a11y-test.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 (async function() {
-  TestRunner.addResult(`Tests accessibility in the Clear Storage view using the axe-core linter.`);
+  TestRunner.addResult('Tests accessibility in the Storage view using the axe-core linter.');
   await TestRunner.loadModule('application_test_runner');
   await TestRunner.loadModule('axe_core_test_runner');
   await ApplicationTestRunner.resetState();
@@ -11,11 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await UI.viewManager.showView('resources');
 
   const parent = UI.panels.resources._sidebar._applicationTreeElement;
-  const clearStorageElement =
-      parent.children().find(child => child.title === ls`Storage`);
+  const clearStorageElement = parent.children().find(child => child.title === ls`Storage`);
   clearStorageElement.select();
-  const clearStorageView = UI.panels.resources.visibleView;
-  TestRunner.addResult('Clear storage view is visible: ' + (clearStorageView instanceof Resources.ClearStorageView));
+  const storageView = UI.panels.resources.visibleView;
+  TestRunner.addResult('Storage view is visible: ' + ApplicationTestRunner.isStorageView(storageView));
 
   async function writeArray() {
     const array = Array(1).fill(0);
@@ -29,6 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   await writeArray();
-  await AxeCoreTestRunner.runValidation(clearStorageView.contentElement);
+  await AxeCoreTestRunner.runValidation(storageView.contentElement);
   TestRunner.completeTest();
 })();
