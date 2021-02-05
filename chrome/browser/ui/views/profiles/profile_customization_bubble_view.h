eb572bab4de805db637f1da442470c0ad8e06da1
David Roger <droger@chromium.org>
2021-02-05 13:56:29 +0000

    [Profiles] F6 can be used to re-focus signin bubbles
    Add the profile bubbles to the hook in
    BrowserView::ActivateFirstInactiveBubbleForAccessibility()
    so that they can be focused by accessibility shortcuts.
    
    The production change is only in
    chrome/browser/ui/views/frame/browser_view.cc
    
    Bug: 1170677
    Change-Id: I750b51aa62ab5feab3eb5bf5ba051566278498cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2663457
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851099}

--- chrome/browser/ui/views/profiles/profile_customization_bubble_view.h
+++ chrome/browser/ui/views/profiles/profile_customization_bubble_view.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_CUSTOMIZATION_BUBBLE_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_CUSTOMIZATION_BUBBLE_VIEW_H_
 
+#include "base/gtest_prod_util.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 #include "ui/views/metadata/metadata_header_macros.h"
 
@@ -28,6 +29,9 @@ class ProfileCustomizationBubbleView : public views::BubbleDialogDelegateView {
   static void CreateBubble(Profile* profile, views::View* anchor_view);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(ProfileBubbleInteractiveUiTest,
+                           CustomizationBubbleFocus);
+
   ProfileCustomizationBubbleView(Profile* profile, views::View* anchor_view);
 
   // Called when the "Done" button is clicked in the inner WebUI.
