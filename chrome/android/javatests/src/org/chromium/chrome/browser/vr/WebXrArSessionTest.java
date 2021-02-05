f12697cb4cb99cf8c7f75b9a600c3d57614cc9e7
Salvador Guerrero <salg@google.com>
2021-02-05 03:00:41 +0000

    Disabled flaky WebXrArSessionTests
    This CL disables the following tests:
    
    WebXrArSessionTest#testArPermissionPersistance
    WebXrArSessionTest#testRepeatedArSessionsSucceed
    
    Because they are flaky on builder "android-10-arm64-rel"
    
    TBR=bsheedy@chromium.org
    
    Bug: 1174857
    Change-Id: I94581004107f59ecc79db95ed00295caff506dfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676967
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Salvador Guerrero <salg@google.com>
    Cr-Commit-Position: refs/heads/master@{#850939}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrArSessionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrArSessionTest.java
@@ -23,6 +23,7 @@ import org.chromium.base.test.params.ParameterAnnotations.UseRunnerDelegate;
 import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.vr.rules.XrActivityRestriction;
@@ -79,6 +80,7 @@ public class WebXrArSessionTest {
      * Tests that consenting causes future attempts to skip the permission prompt as long as no
      * navigation occurs.
      */
+    @DisabledTest(message = "https://crbug.com/1174857")
     @Test
     @MediumTest
     @XrActivityRestriction({XrActivityRestriction.SupportedActivity.ALL})
@@ -106,6 +108,7 @@ public class WebXrArSessionTest {
      * Tests that repeatedly starting and stopping AR sessions does not cause any unexpected
      * behavior. Regression test for https://crbug.com/837894.
      */
+    @DisabledTest(message = "https://crbug.com/1174857")
     @Test
     @MediumTest
     @XrActivityRestriction({XrActivityRestriction.SupportedActivity.ALL})
