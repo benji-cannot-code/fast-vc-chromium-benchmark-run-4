90351ee23c7a549702056a8b8ba3ad47b46023db
David Benjamin <davidben@chromium.org>
2021-02-05 00:51:25 +0000

    Resolve Service Worker redirects based on the response
    We currently resolve Service-Worker-forwarded location headers using the
    request. While this matches Firefox, this does not match the spec or
    Safari's behavior. Instead, the spec says to resolve the location header
    based on the response's URL.
    
    This comes up if the FetchEvent was for /, but the Service Worker
    responded with ev.respondWith(fetch("/foo/", {redirect: "manual"})). In
    that case, a Location: bar.html header would result in /bar.html by our
    version and /foo/bar.html by the spec's version.
    
    Align with the spec. This makes the redirect go where it would have gone
    under {redirect: "follow"}. This has two platform-visible behavior
    changes:
    
    - First, cases like the above will result in a different URL.
    
    - Second, script-constructed Response objects do not have a URL list. If
      the URLs are absolute, this works fine. If they are relative, those
      fetches will now result in a network error. Note Response.redirect()
      internally constructs absolute URLs, so those continue to work. This
      only affects ev.respondWith(new Response(... location: "bar.html"}})).
    
    Both of these changes match Safari.
    
    Note that, as of writing, the Fetch spec describes this behavior in
    terms of a location URL property on the response object. This would
    require computing the location URL earlier and preserving it across many
    layers, including persisting in CacheStorage. See
    https://chromium-review.googlesource.com/c/chromium/src/+/2648648.
    
    Instead, this CL uses the equivalent formulation in
    https://github.com/whatwg/fetch/pull/1149. See also discussion in
    https://github.com/whatwg/fetch/issues/1146.
    
    Bug: 1170379
    Change-Id: Ibb6b12566244fd259029e67787dd7f08edeece9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2665871
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850874}

---
+++ content/test/data/service_worker/fetch_event_invalid_redirect.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+this.onfetch = function(event) {
+  // Redirects are resolved relative to the response's URL list. Synthetic
+  // responses don't have a URL list, so a relative location header should fail
+  // to redirect.
+  event.respondWith(new Response('', {
+    status: 302,
+    headers: {location: 'foo.html'},
+  }));
+};
