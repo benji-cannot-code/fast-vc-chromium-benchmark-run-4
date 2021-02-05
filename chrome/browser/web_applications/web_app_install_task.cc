eb402dbb5a6b97d60fdc28e009163106e93d6455
Eric Willigers <ericwilligers@chromium.org>
2021-02-05 02:28:47 +0000

    WebAppInstallTask: Check for web contents being destroyed
    WebAppInstallTask methods called by WebAppInstallManager
    check the web contents are not being destroyed, before
    making use of the web contents.
    
    Bug: 1169665
    Change-Id: I08470de7687e4c0239ef143ff1791deaa63ce019
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2674562
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850922}

--- chrome/browser/web_applications/web_app_install_task.cc
+++ chrome/browser/web_applications/web_app_install_task.cc
@@ -188,6 +188,9 @@ void WebAppInstallTask::LoadAndInstallWebAppFromManifestWithFallback(
   CheckInstallPreconditions();
 
   Observe(contents);
+  if (ShouldStopInstall())
+    return;
+
   background_installation_ = true;
   install_callback_ = std::move(install_callback);
   install_source_ = install_source;
@@ -273,6 +276,9 @@ void WebAppInstallTask::UpdateWebAppFromInfo(
     InstallManager::OnceInstallCallback callback) {
   CheckInstallPreconditions();
   Observe(web_contents);
+  if (ShouldStopInstall())
+    return;
+
   install_callback_ = std::move(callback);
   background_installation_ = true;
 
@@ -656,6 +662,9 @@ void WebAppInstallTask::InstallWebAppFromInfoRetrieveIcons(
   CheckInstallPreconditions();
 
   Observe(web_contents);
+  if (ShouldStopInstall())
+    return;
+
   install_callback_ = std::move(callback);
   install_source_ = finalize_options.install_source;
   background_installation_ = true;
