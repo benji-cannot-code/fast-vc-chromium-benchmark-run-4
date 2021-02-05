8037bccaa4a0a2cee3abe71ea1fa242fe761530a
Delan Azabani <dazabani@igalia.com>
2021-02-05 03:37:00 +0000

    CSS highlight painting order: independent originating element shadows
    This patch fixes several bugs around ::selection shadow painting.
    
    We now paint the originating element’s shadows for selected text when
    painting the same shadows for the unselected parts, suppressing only
    the text proper in the selected part.
    
    We now suppress the originating element’s shadows when painting the
    ClipOut phase in NGTextPainter::PaintSelectedText, because this is the
    wrong time (and wrong clipping rect) to paint those shadows.
    
    The net result of these first two changes is that originating element
    shadows for all text (including selected text) are now painted without
    erroneous clipping, and always behind any unselected text proper.
    
    We now clear any ShadowList pointer copied from the given style in
    HPU::HighlightPaintingStyle, because the default text-shadow for each
    highlight pseudo should be none, not inherited from the originating
    element. Previously we would paint originating element shadows over
    the ::selection background if HighlightPseudoStyle returns nullptr.
    
    Together these changes should fix bug 474335 entirely, and go a long
    way towards spec-compliant highlight painting (bug 1147859). Relevant
    tests and screenshots (relative to <https://crrev.com/c/2647046>):
    
    • https://wpt.live/css/css-pseudo/highlight-painting-order.html
    • https://bucket.daz.cat/work/igalia/0/6.html?wm=vertical-rl
    • https://bucket.daz.cat/8361fa79820580e0.png (right = patch)
    
    Bug: 1147859, 474335
    Change-Id: I3485b840d3967a75542678ee146a3d6d5c926a77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2649833
    Commit-Queue: Delan Azabani <dazabani@igalia.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850950}

--- third_party/blink/renderer/core/paint/svg_inline_text_box_painter.cc
+++ third_party/blink/renderer/core/paint/svg_inline_text_box_painter.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/paint_timing.h"
 #include "third_party/blink/renderer/core/paint/paint_timing_detector.h"
 #include "third_party/blink/renderer/core/paint/svg_object_painter.h"
+#include "third_party/blink/renderer/core/paint/text_painter_base.h"
 #include "third_party/blink/renderer/core/style/applied_text_decoration.h"
 #include "third_party/blink/renderer/core/style/shadow_list.h"
 #include "third_party/blink/renderer/core/svg/svg_element.h"
@@ -420,8 +421,8 @@ bool SVGInlineTextBoxPainter::SetupTextPaint(
   if (style.TextShadow() &&
       // Text shadows are disabled when printing. http://crbug.com/258321
       !InlineLayoutObject().GetDocument().Printing()) {
-    flags.setLooper(style.TextShadow()->CreateDrawLooper(
-        DrawLooperBuilder::kShadowRespectsAlpha,
+    flags.setLooper(TextPainterBase::CreateDrawLooper(
+        style.TextShadow(), DrawLooperBuilder::kShadowRespectsAlpha,
         style.VisitedDependentColor(GetCSSPropertyColor()),
         style.UsedColorScheme()));
   }
