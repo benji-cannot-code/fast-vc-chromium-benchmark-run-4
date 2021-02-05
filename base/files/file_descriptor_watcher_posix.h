3cfa46549b2ead66b0e25175f47e014fa353add1
Alexander Timin <altimin@chromium.org>
2021-02-05 13:16:32 +0000

    Revert "FileDescriptorWatcher: Make WaitableEvent owned by Watcher."
    This reverts commit 28cf0967c137b3bb428523620a0d585978482f88.
    
    Reason for revert: 
    Suspecting to be the root cause of crbug.com/1175008 and https://ci.chromium.org/ui/p/chromium/builders/ci/linux-ubsan-vptr/1068/test-results?expected=true&exonerated=true&flaky=true
    
    Original change's description:
    > FileDescriptorWatcher: Make WaitableEvent owned by Watcher.
    >
    > Instead of passing WaitableEvent via ScopedClosureRunner, which goesn't
    > guarantee that the Watcher is actually destroyed, pass the reference to
    > WaitableEvent to Watcher and signal it from its constructor.
    >
    > R=​gab@chromium.org
    > BUG=1126253
    >
    > Change-Id: I87ee96c24832f9f8a3a865773451b0b0a8d0c962
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673526
    > Commit-Queue: Alexander Timin <altimin@chromium.org>
    > Reviewed-by: Wez <wez@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#850882}
    
    TBR=wez@chromium.org,gab@chromium.org,altimin@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I95a09454ba151e01efe24393154c6246b8445afa
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1126253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676193
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851091}

--- base/files/file_descriptor_watcher_posix.h
+++ base/files/file_descriptor_watcher_posix.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_pump_for_io.h"
 #include "base/sequence_checker.h"
 #include "base/single_thread_task_runner.h"
-#include "base/synchronization/waitable_event.h"
 
 namespace base {
 
@@ -77,10 +76,6 @@ class BASE_EXPORT FileDescriptorWatcher {
     // task posted to the MessageLoopForIO. This ensures that |watcher_| isn't
     // deleted while it is being used by the MessageLoopForIO.
     std::unique_ptr<Watcher> watcher_;
-
-    // An event for the watcher to notify controller that it's destroyed.
-    // As the |watcher_| is owned by Controller, always outlives the Watcher.
-    base::WaitableEvent on_watcher_destroyed_;
 
     // Validates that the Controller is used on the sequence on which it was
     // instantiated.
