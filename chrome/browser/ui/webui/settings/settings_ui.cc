679418ce12bbaffb68ce46722e41e972d4b68081
sauski <sauski@google.com>
2021-02-05 09:43:19 +0000

    Privacy Sandbox: Only enable if at least one API is available
    To enable the independent launch of the Privacy Sandbox Settings,
    without risking showing a user a UI to control features which are not
    enabled, an additional layering is applied to inspecting feature state.
    
    This additional layer ensures that at least one of the privacy sandbox
    controlled APIs is available.
    
    Bug: 1152336
    Change-Id: I3ae85d61d8a9f22c073fcb4151d66daf350a08fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675903
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#851047}

--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_settings.h"
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/hats/hats_service.h"
@@ -319,10 +321,6 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
       "safetyCheckWeakPasswordsEnabled",
       base::FeatureList::IsEnabled(features::kSafetyCheckWeakPasswords));
 
-  html_source->AddBoolean(
-      "privacySandboxSettingsEnabled",
-      base::FeatureList::IsEnabled(features::kPrivacySandboxSettings));
-
   AddSettingsPageUIHandler(std::make_unique<AboutHandler>(profile));
   AddSettingsPageUIHandler(std::make_unique<ResetSettingsHandler>(profile));
 
@@ -365,10 +363,14 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
                    profile, chrome::FaviconUrlFormat::kFavicon2));
 
   // Privacy Sandbox
-  if (base::FeatureList::IsEnabled(features::kPrivacySandboxSettings)) {
+  bool sandbox_enabled = PrivacySandboxSettingsFactory::GetForProfile(profile)
+                             ->PrivacySandboxSettingsFunctional();
+  html_source->AddBoolean("privacySandboxSettingsEnabled", sandbox_enabled);
+  if (sandbox_enabled) {
     html_source->AddResourcePath(
         "privacySandbox", IDR_SETTINGS_PRIVACY_SANDBOX_PRIVACY_SANDBOX_HTML);
   }
+
   TryShowHatsSurveyWithTimeout();
 }
 
