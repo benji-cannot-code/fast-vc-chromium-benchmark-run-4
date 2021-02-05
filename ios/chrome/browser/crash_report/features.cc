f8ba4357d15faeac9935a6cf00a2f671583bd458
Justin Cohen <justincohen@google.com>
2021-02-05 20:26:02 +0000

    ios: Register synthetic trial for ios-crashpad.
    Because the crashpad flag takes 2 restarts to take effect, register a
    synthetic field try when crashpad is actually running.
    
    Bug: 1022461
    Change-Id: I88d9c42e62494b6d35f9ea736f64b08cf6b7eb8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675645
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851259}

--- ios/chrome/browser/crash_report/features.cc
+++ ios/chrome/browser/crash_report/features.cc
@@ -7,7 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/crash_report/breadcrumbs/features.h"
 
-const base::Feature kCrashpadIOS{"CrashpadIOS",
+// Note the name here is "CrashpadIOSEnabler" and not "CrashpadIOS".  The former
+// is the name of the feature that eventually enables the latter synthetic flag
+// via NSUserDefault syncing.  This is needed to start CrashpadiOS immediately
+// after startup.
+const base::Feature kCrashpadIOS{"CrashpadIOSEnabler",
                                  base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kSyntheticCrashReportsForUte{
