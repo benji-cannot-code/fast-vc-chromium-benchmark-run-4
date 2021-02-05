0abb22964bcd0acfb49cc37fd633b79c500912c9
Nohemi Fernandez <fernandex@chromium.org>
2021-02-05 12:03:37 +0000

    [iOS] Display the sign-in infobar on navigation redirects.
    When a user navigates to Google subdomains via the Omnibox, Chrome
    performs an automatic redirect to AuthLogin before continuing to the
    domain. In this case the infobar is lost before we display it to the
    user. This patch ensures we display the infobar regardless of the
    navigation type.
    
    Bug: 1172240
    Change-Id: If5142e38c112f52f9dcffe8c029bcb01a25afa1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675926
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851078}

--- ios/chrome/browser/ui/authentication/signin_notification_infobar_delegate.h
+++ ios/chrome/browser/ui/authentication/signin_notification_infobar_delegate.h
@@ -52,6 +52,7 @@ class SigninNotificationInfoBarDelegate : public ConfirmInfoBarDelegate {
   gfx::Image GetIcon() const override;
   bool UseIconBackgroundTint() const override;
   bool Accept() override;
+  bool ShouldExpire(const NavigationDetails& details) const override;
 
  private:
   gfx::Image icon_;
