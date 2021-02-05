15d182016ca1cbf00255dcf53bcc4359c7347c75
jchen10 <jie.a.chen@intel.com>
2021-02-05 06:26:31 +0000

    Tighten textureInfoArray in Web IDL
    This enforces it to be required and non-nullable.
    
    Bug: 1142754
    Change-Id: I8f90918070e7366370cf383f1f3a8e64e051552c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676998
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Jie A Chen <jie.a.chen@intel.com>
    Cr-Commit-Position: refs/heads/master@{#851000}

--- third_party/blink/renderer/modules/webgl/webgl_webcodecs_video_frame.cc
+++ third_party/blink/renderer/modules/webgl/webgl_webcodecs_video_frame.cc
@@ -439,7 +439,6 @@ bool WebGLWebCodecsVideoFrame::releaseVideoFrame(
     WebGLWebCodecsVideoFrameHandle* handle,
     ExceptionState& exception_state) {
   DCHECK(handle);
-  DCHECK(handle->hasTextureInfoArray());
   WebGLExtensionScopedContext scoped(this);
   auto* gl = scoped.Context()->ContextGL();
   auto& info_array = handle->textureInfoArray();
