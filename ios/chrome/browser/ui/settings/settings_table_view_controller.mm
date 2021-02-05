9d813038df12206ef5e86f1d4e30ee5911ff5f4a
Gauthier Ambard <gambard@chromium.org>
2021-02-05 16:20:52 +0000

    [iOS][🏢] Fix crash when changing default search engine
    This CL changes the check done when the search engine is changed. If the
    enterprise policy managing the search engine is enabled, the managed
    item should be updated, not the regular one.
    
    Also when the policy changes, the UI should be reloaded.
    
    Fixed: 1172461
    Change-Id: I1136d0c3701a9388ccb2c86e4fa6561a2b6706ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2672583
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851148}

--- ios/chrome/browser/ui/settings/settings_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_table_view_controller.mm
@@ -252,6 +252,7 @@ SyncState GetSyncStateFromBrowserState(ChromeBrowserState* browserState) {
   // Updatable Items.
   TableViewDetailIconItem* _voiceSearchDetailItem;
   TableViewDetailIconItem* _defaultSearchEngineItem;
+  TableViewInfoButtonItem* _managedSearchEngineItem;
   TableViewDetailIconItem* _passwordsDetailItem;
   TableViewDetailIconItem* _autoFillProfileDetailItem;
   TableViewDetailIconItem* _autoFillCreditCardDetailItem;
@@ -347,6 +348,9 @@ SyncState GetSyncStateFromBrowserState(ChromeBrowserState* browserState) {
         autofill::prefs::kAutofillCreditCardEnabled, &_prefChangeRegistrar);
     _prefObserverBridge->ObserveChangesForPreference(
         autofill::prefs::kAutofillProfileEnabled, &_prefChangeRegistrar);
+    _prefObserverBridge->ObserveChangesForPreference(
+        DefaultSearchManager::kDefaultSearchProviderDataPrefName,
+        &_prefChangeRegistrar);
 
     _dispatcher = dispatcher;
 
@@ -739,33 +743,20 @@ SyncState GetSyncStateFromBrowserState(ChromeBrowserState* browserState) {
 }
 
 - (TableViewInfoButtonItem*)managedSearchEngineItem {
-  TableViewInfoButtonItem* managedDefaultSearchEngineItem =
-      [[TableViewInfoButtonItem alloc]
-          initWithType:SettingsItemTypeManagedDefaultSearchEngine];
-  managedDefaultSearchEngineItem.text =
+  _managedSearchEngineItem = [[TableViewInfoButtonItem alloc]
+      initWithType:SettingsItemTypeManagedDefaultSearchEngine];
+  _managedSearchEngineItem.text =
       l10n_util::GetNSString(IDS_IOS_SEARCH_ENGINE_SETTING_TITLE);
-  managedDefaultSearchEngineItem.image =
+  _managedSearchEngineItem.image =
       [UIImage imageNamed:kSettingsSearchEngineImageName];
-  managedDefaultSearchEngineItem.accessibilityHint =
+  _managedSearchEngineItem.accessibilityHint =
       l10n_util::GetNSString(IDS_IOS_TOGGLE_SETTING_MANAGED_ACCESSIBILITY_HINT);
 
-  const base::DictionaryValue* dict = _browserState->GetPrefs()->GetDictionary(
-      DefaultSearchManager::kDefaultSearchProviderDataPrefName);
-  if (dict->FindBoolPath(DefaultSearchManager::kDisabledByPolicy)) {
-    // Default search engine is disabled by policy.
-    managedDefaultSearchEngineItem.statusText =
-        l10n_util::GetNSString(IDS_IOS_SEARCH_ENGINE_SETTING_DISABLED_STATUS);
-  } else {
-    // Default search engine is enabled and set by policy.
-    const std::string* status =
-        dict->FindStringPath(DefaultSearchManager::kShortName);
-    managedDefaultSearchEngineItem.statusText =
-        base::SysUTF8ToNSString(*status);
-  }
+  _managedSearchEngineItem.statusText = [self managedSearchEngineDetailText];
 
-  managedDefaultSearchEngineItem.accessibilityIdentifier =
+  _managedSearchEngineItem.accessibilityIdentifier =
       kSettingsManagedSearchEngineCellId;
-  return managedDefaultSearchEngineItem;
+  return _managedSearchEngineItem;
 }
 
 - (TableViewItem*)passwordsDetailItem {
@@ -1546,6 +1537,21 @@ SyncState GetSyncStateFromBrowserState(ChromeBrowserState* browserState) {
   return NO;
 }
 
+// Returns the text to be displayed by the managed Search Engine item.
+- (NSString*)managedSearchEngineDetailText {
+  const base::DictionaryValue* dict = _browserState->GetPrefs()->GetDictionary(
+      DefaultSearchManager::kDefaultSearchProviderDataPrefName);
+  if (dict->FindBoolPath(DefaultSearchManager::kDisabledByPolicy)) {
+    // Default search engine is disabled by policy.
+    return l10n_util::GetNSString(
+        IDS_IOS_SEARCH_ENGINE_SETTING_DISABLED_STATUS);
+  }
+  // Default search engine is enabled and set by policy.
+  const std::string* status =
+      dict->FindStringPath(DefaultSearchManager::kShortName);
+  return base::SysUTF8ToNSString(*status);
+}
+
 #pragma mark - SigninPresenter
 
 - (void)showSignin:(ShowSigninCommand*)command {
@@ -1663,10 +1669,16 @@ SyncState GetSyncStateFromBrowserState(ChromeBrowserState* browserState) {
 #pragma mark - SearchEngineObserverBridge
 
 - (void)searchEngineChanged {
-  _defaultSearchEngineItem.detailText =
-      base::SysUTF16ToNSString(GetDefaultSearchEngineName(
-          ios::TemplateURLServiceFactory::GetForBrowserState(_browserState)));
-  [self reconfigureCellsForItems:@[ _defaultSearchEngineItem ]];
+  if (_managedSearchEngineItem) {
+    _managedSearchEngineItem.statusText = [self managedSearchEngineDetailText];
+    [self reconfigureCellsForItems:@[ _managedSearchEngineItem ]];
+  } else {
+    // The two items are mutually exclusive.
+    _defaultSearchEngineItem.detailText =
+        base::SysUTF16ToNSString(GetDefaultSearchEngineName(
+            ios::TemplateURLServiceFactory::GetForBrowserState(_browserState)));
+    [self reconfigureCellsForItems:@[ _defaultSearchEngineItem ]];
+  }
 }
 
 #pragma mark ChromeIdentityServiceObserver
@@ -1751,6 +1763,13 @@ SyncState GetSyncStateFromBrowserState(ChromeBrowserState* browserState) {
                                : l10n_util::GetNSString(IDS_IOS_SETTING_OFF);
     _autoFillCreditCardDetailItem.detailText = detailText;
     [self reconfigureCellsForItems:@[ _autoFillCreditCardDetailItem ]];
+  }
+
+  if (preferenceName ==
+      DefaultSearchManager::kDefaultSearchProviderDataPrefName) {
+    // Reloading the data is needed because the item type and its class are
+    // changing.
+    [self reloadData];
   }
 }
 
