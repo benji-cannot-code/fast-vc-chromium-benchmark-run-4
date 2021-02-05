5af53dd244b5bc2094ae99538148c3488d1c692c
Liquan (Max) Gu <maxlg@chromium.org>
2021-02-05 17:23:47 +0000

    [DynamicUpdate] Disable "GPayAppDynamicUpdate" feature on WebLayer
    This CL disables "GPayAppDynamicUpdate" feature on WebLayer because
    the readiness of this feature in WebLayer is different from in Clank.
    
    Bug: 1074423
    Change-Id: I8ea982f5965bd4d027f5c7d6251f59a1ebe7ee44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675824
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851177}

--- weblayer/app/content_main_delegate_impl.cc
+++ weblayer/app/content_main_delegate_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "components/payments/core/features.h"
 #include "components/startup_metric_utils/browser/startup_metric_utils.h"
 #include "content/public/browser/browser_main_runner.h"
 #include "content/public/common/content_features.h"
@@ -176,6 +177,7 @@ bool ContentMainDelegateImpl::BasicStartupComplete(int* exit_code) {
     // TODO(crbug.com/1131021): Support WebOTP Service on WebLayer.
     ::features::kWebOTP,
 #endif
+    payments::features::kGPayAppDynamicUpdate,
   };
 
 #if defined(OS_ANDROID)
