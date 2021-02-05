8a039aa2fa52c9aa8c5c3b4c82cd33c82d774302
Theo Johnson-kanu <tjohnsonkanu@google.com>
2021-02-05 00:32:59 +0000

    [CrOS Network] Migrate to  internet_config_dialog to Polymer3
    - Adds resources file for internet_config_dialog
    - Migrate internet_config_dialog to preprocess_if_expr
    
    Bug: 1090884
    Change-Id: Icad6b2687a3fa144e4e36d754164f4f53477e0d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676362
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850863}

--- chrome/browser/resources/chromeos/internet_config_dialog/internet_config_dialog.js
+++ chrome/browser/resources/chromeos/internet_config_dialog/internet_config_dialog.js
@@ -3,6 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_components/chromeos/network/network_config.m.js';
+import 'chrome://resources/cr_components/chromeos/network/network_icon.m.js';
+import 'chrome://resources/cr_components/chromeos/network/network_shared_css.m.js';
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
+import 'chrome://resources/cr_elements/cr_page_host_style_css.m.js';
+import 'chrome://resources/cr_elements/shared_style_css.m.js';
+import './strings.m.js';
+
+import {assert} from 'chrome://resources/js/assert.m.js';
+import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
 /**
  * @fileoverview
  * 'internet-config-dialog' is used to configure a new or existing network
@@ -11,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 Polymer({
   is: 'internet-config-dialog',
+
+  _template: html`{__html_template__}`,
 
   behaviors: [I18nBehavior],
 
