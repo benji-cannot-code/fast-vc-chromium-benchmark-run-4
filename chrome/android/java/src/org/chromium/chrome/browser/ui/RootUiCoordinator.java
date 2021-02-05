e20d77071f7c6d5997feb452cfaea5f4bcc6d680
Lijin Shen <lazzzis@google.com>
2021-02-05 17:19:45 +0000

    [Messages] Dismiss all messages when root ui coordinator is destroyed
    When tab reparenting is in the process, the ui views, including
    the message container, is recreated. But the timer of message is not
    cancelled, which leads to a crash due to removing a view from
    a destroyed layout. So dismiss all the messages in order
    to cancel the timers.
    
    The support of tab-repareting on message infra will be done in
    the future.
    
    Bug: 1174660
    Change-Id: Ib2f1e734b088fe328916be7799c7b21ab2a1e050
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676936
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/master@{#851175}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -293,6 +293,7 @@ public class RootUiCoordinator
         mActivity.getLayoutManagerSupplier().removeObserver(mLayoutManagerSupplierCallback);
 
         if (mMessageDispatcher != null) {
+            mMessageDispatcher.dismissAllMessages();
             MessagesFactory.detachMessageDispatcher(mMessageDispatcher);
             mMessageDispatcher = null;
         }
