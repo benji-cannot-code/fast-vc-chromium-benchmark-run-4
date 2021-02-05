38575902e590b93ee531efc0f0222760abad145c
Greg Schechter <schechter@google.com>
2021-02-05 19:22:27 +0000

    Allow for control of lens homepage url with a Finch Experiment Param
    Bug: 1168677
    Change-Id: I43aa439f69d23e9bdde1990a8f19894c22f9ec55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676363
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Greg Schechter <schechter@google.com>
    Cr-Commit-Position: refs/heads/master@{#851229}

--- components/lens/lens_features.h
+++ components/lens/lens_features.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_LENS_LENS_FEATURES_H_
 #define COMPONENTS_LENS_LENS_FEATURES_H_
 
+#include <string>
+
 #include "base/feature_list.h"
 
 namespace lens {
@@ -13,6 +15,7 @@ namespace features {
 
 extern const base::Feature kLensStandalone;
 extern int GetMaxPixels();
+extern std::string GetHomepageURL();
 
 }  // namespace features
 }  // namespace lens
