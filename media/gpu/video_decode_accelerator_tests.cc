55c19e0e3cd2cc8627179726db6f7e731b5d7770
Wan-Teh Chang <wtc@google.com>
2021-02-05 12:10:25 +0000

    Allow libgav1 in video_decode_accelerator_tests.cc
    Allow video_decode_accelerator_tests.cc to use either Dav1dVideoDecoder
    or Gav1VideoDecoder. If both dav1d and libgav1 are enabled,
    Dav1dVideoDecoder is used.
    
    Bug: b:176927551
    Change-Id: I6a491b2d1e7d1ce0764834fd743d2f4711182035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676566
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851080}

--- media/gpu/video_decode_accelerator_tests.cc
+++ media/gpu/video_decode_accelerator_tests.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/test_data_util.h"
 #include "media/base/video_decoder_config.h"
 #include "media/base/video_transformation.h"
-#include "media/filters/dav1d_video_decoder.h"
 #include "media/gpu/test/video.h"
 #include "media/gpu/test/video_frame_file_writer.h"
 #include "media/gpu/test/video_frame_validator.h"
@@ -25,7 +24,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/test/video_player/video_player.h"
 #include "media/gpu/test/video_player/video_player_test_environment.h"
 #include "media/gpu/test/video_test_helpers.h"
+#include "media/media_buildflags.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+#if BUILDFLAG(ENABLE_DAV1D_DECODER)
+#include "media/filters/dav1d_video_decoder.h"
+#elif BUILDFLAG(ENABLE_LIBGAV1_DECODER)
+#include "media/filters/gav1_video_decoder.h"
+#endif
 
 namespace media {
 namespace test {
@@ -171,7 +177,11 @@ class VideoDecoderTest : public ::testing::Test {
       LOG(ERROR) << "Frame validation by SSIM is allowed for AV1 streams only";
       return false;
     }
+#if BUILDFLAG(ENABLE_DAV1D_DECODER)
     Dav1dVideoDecoder decoder(
+#elif BUILDFLAG(ENABLE_LIBGAV1_DECODER)
+    Gav1VideoDecoder decoder(
+#endif
         /*media_log=*/nullptr,
         OffloadableVideoDecoder::OffloadState::kOffloaded);
     VideoDecoderConfig decoder_config(
