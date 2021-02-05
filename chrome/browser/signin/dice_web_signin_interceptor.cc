1cebb131c5b80962d80718529d6a42efecc98c86
Ramin Halavati <rhalavati@chromium.org>
2021-02-05 12:20:21 +0000

    Update Dice Web Signin Intercept to check for Guest windows.
    Dice Web Signin Intercept is updated to check for Guest windows instead
    of Guest profiles to decide if a Guest option should be offered or not.
    
    This feature uses Ephemeral Guest profiles. If a Guest window is opened
    and closed on an Ephemeral Guest profile, the profile is marked for
    deletion and will not be used if another Guest window is opened. So in
    the edge case that a Guest profile exist and a Guest window does not
    exist, we are safe to use that Guest profile since it is not used to
    open a window before.
    
    Bug: 1157764
    Change-Id: I1e17844a22d9cdacd5ef73a23925d6de65ffd6d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676187
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851083}

--- chrome/browser/signin/dice_web_signin_interceptor.cc
+++ chrome/browser/signin/dice_web_signin_interceptor.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/passwords/manage_passwords_ui_controller.h"
 #include "chrome/browser/ui/signin/profile_colors_util.h"
 #include "chrome/common/pref_names.h"
@@ -107,7 +108,7 @@ SigninInterceptGuestAvailability GetGuestOptionAvailablity() {
   if (!Profile::IsEphemeralGuestProfileEnabled())
     return SigninInterceptGuestAvailability::kEphemeralGuestDisabled;
 
-  if (ProfileManager::GuestProfileExists())
+  if (BrowserList::GetGuestBrowserCount())
     return SigninInterceptGuestAvailability::kGuestAlreadyOpen;
 
   if (!g_browser_process->local_state()->GetBoolean(
