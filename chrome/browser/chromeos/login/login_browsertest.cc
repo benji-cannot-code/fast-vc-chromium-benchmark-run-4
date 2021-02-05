8dd3d055b93af769388e4061cb772f6245f78dda
Danila Kuzmin <dkuzmin@google.com>
2021-02-05 13:50:22 +0000

    oobe: Offline login availability and navigation fix
    Bug: 1170170
    Change-Id: I126bfd557c4f3325028e26c37a38ae9ff192d863
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2656336
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/master@{#851097}

--- chrome/browser/chromeos/login/login_browsertest.cc
+++ chrome/browser/chromeos/login/login_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "ash/public/cpp/login_screen_test_api.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shell.h"
 #include "ash/system/status_area_widget.h"
@@ -53,6 +54,8 @@ using ::testing::Return;
 
 namespace chromeos {
 namespace {
+
+const test::UIPath kOfflineLoginBackButton = {"offline-login", "backButton"};
 
 class LoginUserTest : public InProcessBrowserTest {
  protected:
@@ -261,6 +264,21 @@ IN_PROC_BROWSER_TEST_F(LoginOfflineManagedTest, FullEmailDontMatchProvided) {
       managed_user_id_.GetUserEmail(), LoginManagerTest::kPassword,
       true /* wait for sign-in */);
   TestSystemTrayIsVisible(false);
+}
+
+IN_PROC_BROWSER_TEST_F(LoginOfflineManagedTest, BackButtonTest) {
+  std::string domain = gaia::ExtractDomainName(managed_user_id_.GetUserEmail());
+
+  ConfigurePolicy(domain);
+
+  OobeScreenWaiter(GaiaView::kScreenId).Wait();
+  offline_login_test_mixin_.GoOffline();
+  offline_login_test_mixin_.InitOfflineLogin(managed_user_id_,
+                                             LoginManagerTest::kPassword);
+
+  test::OobeJS().ClickOnPath(kOfflineLoginBackButton);
+  OobeScreenWaiter(ErrorScreenView::kScreenId).Wait();
+  EXPECT_TRUE(ash::LoginScreenTestApi::IsOobeDialogVisible());
 }
 
 }  // namespace chromeos
