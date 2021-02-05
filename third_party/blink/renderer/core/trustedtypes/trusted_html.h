b43e990d38c4a1d12d4a7e3c705c2ce62a022d39
Stefano Sanfilippo <ssanfilippo@chromium.org>
2021-02-05 16:36:08 +0000

    [Trusted Types] Add toJSON to the trusted type interfaces.
    Addresses https://github.com/w3c/webappsec-trusted-types/issues/306
    
    Change-Id: I595fd60ba68bfe0b5f0122873616e525943f51a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2665936
    Commit-Queue: Stefano Sanfilippo <ssanfilippo@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851153}

--- third_party/blink/renderer/core/trustedtypes/trusted_html.h
+++ third_party/blink/renderer/core/trustedtypes/trusted_html.h
@@ -20,6 +20,7 @@ class CORE_EXPORT TrustedHTML final : public ScriptWrappable {
 
   // TrustedHTML.idl
   const String& toString() const;
+  const String& toJSON() const { return toString(); }
 
  private:
   const String html_;
--- third_party/blink/renderer/core/trustedtypes/trusted_script_url.h
+++ third_party/blink/renderer/core/trustedtypes/trusted_script_url.h
@@ -20,6 +20,7 @@ class CORE_EXPORT TrustedScriptURL final : public ScriptWrappable {
 
   // TrustedScriptURL.idl
   const String& toString() const;
+  const String& toJSON() const { return toString(); }
 
  private:
   const String url_;
