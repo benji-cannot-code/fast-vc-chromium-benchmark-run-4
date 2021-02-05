60b2bc892283ed14f09cd7707525fb55a55a18ba
Venkatesh Srinivas <venkateshs@chromium.org>
2021-02-05 04:48:06 +0000

    atomicops: Remove Acquire_Store
    Acquire_Store was a legacy atomic operation from base/atomicops,
    which stored a value to an AtomicWord. It also implied an 'acquire'
    ordering constraint, which is not part of the C++ atomics model -
    Stores may only have relaxed/release/seq_cst ordering constraints.
    
    Commit bb0303d35b removed the last use outside of third_party; we
    can now retire Acquire_Store! (The code in third_party is in
    tcmalloc and carries its own version of Acquire_Store)
    
    Bug: 420970
    Change-Id: I7c56a9416c57d9d31fc50853ade1ba0df0dbce8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2670190
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Venkatesh Srinivas <venkateshs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#850971}

--- base/atomicops.h
+++ base/atomicops.h
@@ -101,7 +101,6 @@ Atomic32 Release_CompareAndSwap(volatile Atomic32* ptr,
                                 Atomic32 new_value);
 
 void NoBarrier_Store(volatile Atomic32* ptr, Atomic32 value);
-void Acquire_Store(volatile Atomic32* ptr, Atomic32 value);
 void Release_Store(volatile Atomic32* ptr, Atomic32 value);
 
 Atomic32 NoBarrier_Load(volatile const Atomic32* ptr);
@@ -124,7 +123,6 @@ Atomic64 Release_CompareAndSwap(volatile Atomic64* ptr,
                                 Atomic64 old_value,
                                 Atomic64 new_value);
 void NoBarrier_Store(volatile Atomic64* ptr, Atomic64 value);
-void Acquire_Store(volatile Atomic64* ptr, Atomic64 value);
 void Release_Store(volatile Atomic64* ptr, Atomic64 value);
 Atomic64 NoBarrier_Load(volatile const Atomic64* ptr);
 Atomic64 Acquire_Load(volatile const Atomic64* ptr);
