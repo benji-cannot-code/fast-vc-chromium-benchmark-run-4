a1cc3eda7ed9bbb8fcf17d7d5834344d57286d64
Karan Bhatia <karandeepb@chromium.org>
2021-02-05 23:09:00 +0000

    Revert "Convert c/b/r/webui_js_error/ to JS modules."
    This reverts commit 38e0bfea1db821f296eb580699b33ce793a22877.
    
    Reason for revert: Seems to have caused compile failures: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-archive-rel/44905/overview
    
    Original change's description:
    > Convert c/b/r/webui_js_error/ to JS modules.
    >
    > Bug: 1173575
    > Change-Id: I8cf0fe4445c9a606503362a27495e6ee57ac12df
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2678024
    > Auto-Submit: dpapad <dpapad@chromium.org>
    > Commit-Queue: John Lee <johntlee@chromium.org>
    > Reviewed-by: John Lee <johntlee@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#851322}
    
    TBR=dpapad@chromium.org,johntlee@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I11fb5db483162836c0ef030c916e3ace237eb0a1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1173575
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679981
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851325}

--- chrome/browser/resources/webui_js_error/webui_js_error.js
+++ chrome/browser/resources/webui_js_error/webui_js_error.js
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {$} from 'chrome://resources/js/util.m.js';
-
 /**
  * @fileoverview This JavaScript prints an error message, throws uncaught
  * exceptions, and otherwise does erroneous things for testing.
