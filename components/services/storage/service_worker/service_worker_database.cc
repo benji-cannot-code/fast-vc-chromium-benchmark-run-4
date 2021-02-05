14bfd1b01e8a9f80a9f648c49e74b145296a2149
Kenichi Ishibashi <bashi@chromium.org>
2021-02-05 09:21:18 +0000

    Use FilesystemProxy for service worker leveldb env
    So that ServiceWorkerDatabase is property sandboxed when it is
    moved to the Storage Service.
    
    Bug: 1174894
    Change-Id: I6091fde48cea6cf3f21f58c0975742d362c04f03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675547
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851040}

--- components/services/storage/service_worker/service_worker_database.cc
+++ components/services/storage/service_worker/service_worker_database.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "components/services/storage/filesystem_proxy_factory.h"
 #include "components/services/storage/service_worker/service_worker_database.pb.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_database.mojom.h"
 #include "third_party/leveldatabase/env_chromium.h"
@@ -123,7 +124,9 @@ constexpr size_t kWriteBufferSize = 512 * 1024;
 
 class ServiceWorkerEnv : public leveldb_env::ChromiumEnv {
  public:
-  ServiceWorkerEnv() : ChromiumEnv("LevelDBEnv.ServiceWorker") {}
+  ServiceWorkerEnv()
+      : ChromiumEnv("LevelDBEnv.ServiceWorker",
+                    storage::CreateFilesystemProxy()) {}
 
   // Returns a shared instance of ServiceWorkerEnv. This is thread-safe.
   static ServiceWorkerEnv* GetInstance() {
