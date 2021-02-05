3b66070a462eec62ee7a003deffd91a947529e36
Robert Liao <robliao@chromium.org>
2021-02-05 22:34:16 +0000

    Add Instrumentation to Help Debug a Potential Use After Free in Views
    BUG=1162949
    
    Change-Id: Ib69701e2299b4c629b4a8e042628ff4a0a3d97d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2678651
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851313}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -250,6 +250,8 @@ View::~View() {
   // called at the end so observers can examine properties inside
   // OnViewIsDeleting(), for instance.
   ClearProperties();
+
+  life_cycle_state_ = LifeCycleState::kDestroyed;
 }
 
 // Tree operations -------------------------------------------------------------
@@ -1059,6 +1061,8 @@ void View::SchedulePaintInRect(const gfx::Rect& rect) {
 }
 
 void View::Paint(const PaintInfo& parent_paint_info) {
+  CHECK_EQ(life_cycle_state_, LifeCycleState::kAlive);
+
   if (!ShouldPaint())
     return;
 
@@ -1157,6 +1161,8 @@ void View::Paint(const PaintInfo& parent_paint_info) {
     // Delegate painting the contents of the View to the virtual OnPaint method.
     OnPaint(canvas);
   }
+
+  CHECK_EQ(life_cycle_state_, LifeCycleState::kAlive);
 
   // View::Paint() recursion over the subtree.
   PaintChildren(paint_info);
