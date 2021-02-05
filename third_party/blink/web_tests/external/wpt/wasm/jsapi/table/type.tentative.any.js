8f887be2df4ee57bf94a3534ba45c8a64f78b816
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-02-05 01:24:06 +0000

    Import wpt@0aed9ee0f5a91f048dc70c16875d10eec4b77082
    Using wpt-import in Chromium 639e7de89099b3f52bca15feca3b2515e392229a.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    binji@chromium.org:
      external/wpt/wasm
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, philipp.hancke@googlemail.com:
      external/wpt/webrtc
    kojii@chromium.org:
      external/wpt/css/vendor-imports/mozilla/mozilla-central-reftests/variables
    
    NOAUTOREVERT=true
    TBR=weizhong@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Ie4584578ad2e4e0900659b7e1eff759cefc7133c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675749
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#850896}

---
+++ third_party/blink/web_tests/external/wpt/wasm/jsapi/table/type.tentative.any.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,dedicatedworker,jsshell
+// META: script=/wasm/jsapi/assertions.js
+
+function assert_type(argument) {
+    const mytable = new WebAssembly.Table(argument);
+
+    assert_equals(mytable.type.minimum, argument.minimum);
+    assert_equals(mytable.type.maximum, argument.maximum);
+    assert_equals(mytable.type.element, argument.element);
+}
+
+test(() => {
+    assert_type({ "minimum": 0, "element": "funcref"});
+}, "Zero initial, no maximum");
+
+test(() => {
+    assert_type({ "minimum": 5, "element": "funcref" });
+}, "Non-zero initial, no maximum");
+
+test(() => {
+    assert_type({ "minimum": 0, "maximum": 0, "element": "funcref" });
+}, "Zero maximum");
+
+test(() => {
+    assert_type({ "minimum": 0, "maximum": 5, "element": "funcref" });
+}, "None-zero maximum");
