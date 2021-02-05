05c6b882683c43bb78c0bba06270f8f1f49aa435
Maria Kazinova <kazinova@google.com>
2021-02-05 10:37:30 +0000

    Renamed FormFieldData's member field "typed_value" to "user_input".
    This is done to make the naming more precise, as this member field
    contains not only typed values, but also values autofilled on user
    trigger.
    Corresponding FieldDataManager::GetUserTypedValue method was also
    renamed to GetUserInput.
    
    Change-Id: Ied49ed89767ebb4edf595c8c1163cbc0804947f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673409
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/master@{#851056}

--- components/autofill/core/common/mojom/autofill_types_mojom_traits_unittest.cc
+++ components/autofill/core/common/mojom/autofill_types_mojom_traits_unittest.cc
@@ -188,7 +188,7 @@ void ExpectFormFieldData(const FormFieldData& expected,
                          const FormFieldData& passed) {
   EXPECT_TRUE(EquivalentData(expected, passed));
   EXPECT_EQ(expected.value, passed.value);
-  EXPECT_EQ(expected.typed_value, passed.typed_value);
+  EXPECT_EQ(expected.user_input, passed.user_input);
   std::move(closure).Run();
 }
 
@@ -257,7 +257,7 @@ TEST_F(AutofillTypeTraitsTestImpl, PassFormFieldData) {
   input.role = FormFieldData::RoleAttribute::kPresentation;
   input.text_direction = base::i18n::RIGHT_TO_LEFT;
   input.properties_mask = FieldPropertiesFlags::kHadFocus;
-  input.typed_value = base::ASCIIToUTF16("TestTypedValue");
+  input.user_input = base::ASCIIToUTF16("TestTypedValue");
   input.bounds = gfx::RectF(1, 2, 10, 100);
 
   base::RunLoop loop;
@@ -287,7 +287,7 @@ TEST_F(AutofillTypeTraitsTestImpl, PassDataListFormFieldData) {
   input.role = FormFieldData::RoleAttribute::kPresentation;
   input.text_direction = base::i18n::RIGHT_TO_LEFT;
   input.properties_mask = FieldPropertiesFlags::kHadFocus;
-  input.typed_value = base::ASCIIToUTF16("TestTypedValue");
+  input.user_input = base::ASCIIToUTF16("TestTypedValue");
   input.bounds = gfx::RectF(1, 2, 10, 100);
 
   base::RunLoop loop;
