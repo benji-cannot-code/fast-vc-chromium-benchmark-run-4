a61542ad6430c69ec8d577841e76613997add18a
Arthur Eubanks <aeubanks@google.com>
2021-02-05 12:37:55 +0000

    Roll clang llvmorg-12-init-17251-g6de48655-1 : llvmorg-13-init-794-g83e2710e-1.
    Ran `./tools/clang/scripts/upload_revision.py 83e2710eb014c9a7fcb764b0270077e4f00cdab6`.
    
    Bug: 1170449
    Change-Id: I0d7ba13482f5c84a5ec26acad63f7447c50fe2bf
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg
    Cq-Include-Trybots: chromium/try:linux_angle_deqp_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_compile_dbg_32_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:mac-arm64-rel,mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:win-angle-deqp-rel-64
    Cq-Include-Trybots: chromium/try:win-asan,win7-rel,win-angle-deqp-rel-32
    Cq-Include-Trybots: chrome/try:iphone-device,ipad-device
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:win-chrome,win64-chrome,mac-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676316
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/master@{#851087}

--- tools/clang/blink_gc_plugin/BlinkGCPluginConsumer.cpp
+++ tools/clang/blink_gc_plugin/BlinkGCPluginConsumer.cpp
@@ -111,20 +111,6 @@ void BlinkGCPluginConsumer::HandleTranslationUnit(ASTContext& context) {
 
   if (options_.dump_graph) {
     std::error_code err;
-#if !defined(LLVM_FORCE_HEAD_REVISION)
-    // TODO: Make createDefaultOutputFile or a shorter createOutputFile work.
-    json_ = JsonWriter::from(instance_.createOutputFile(
-        "",                                      // OutputPath
-        err,                                     // Errors
-        true,                                    // Binary
-        true,                                    // RemoveFileOnSignal
-        instance_.getFrontendOpts().OutputFile,  // BaseInput
-        "graph.json",                            // Extension
-        false,                                   // UseTemporary
-        false,                                   // CreateMissingDirectories
-        0,                                       // ResultPathName
-        0));                                     // TempPathName
-#else
     SmallString<128> OutputFile(instance_.getFrontendOpts().OutputFile);
     llvm::sys::path::replace_extension(OutputFile, "graph.json");
     json_ = JsonWriter::from(instance_.createOutputFile(
@@ -133,7 +119,6 @@ void BlinkGCPluginConsumer::HandleTranslationUnit(ASTContext& context) {
         true,                                    // RemoveFileOnSignal
         false,                                   // UseTemporary
         false));                                 // CreateMissingDirectories
-#endif
     if (!err && json_) {
       json_->OpenList();
     } else {
