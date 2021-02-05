22f91ef1d4822174b3abee2a283dbb4a749a386d
Francois Beaufort <beaufort.francois@gmail.com>
2021-02-05 09:42:00 +0000

    [Android] Expand PWA install bottom sheet once screenshots are added.
    This CL moves the requested expansion of the PWA install bottom sheet to
    after screenshots are added to the UI.
    
    Bug: 1171732
    Change-Id: I4a3370e45d416e6d82c9132c7632180514ae9f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673531
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851046}

--- chrome/browser/webapps/android/java/src/org/chromium/chrome/browser/webapps/PwaBottomSheetController.java
+++ chrome/browser/webapps/android/java/src/org/chromium/chrome/browser/webapps/PwaBottomSheetController.java
@@ -160,7 +160,6 @@ public class PwaBottomSheetController
      *         requests to.
      * @param windowAndroid The window the UI is associated with.
      * @param webContents The WebContents the UI is associated with.
-     * @param showExpanded Whether to show the UI in expanded mode or not.
      * @param icon The icon of the app represented by the UI.
      * @param isAdaptiveIcon Whether the app icon is adaptive or not.
      * @param title The title of the app represented by the UI.
@@ -168,7 +167,7 @@ public class PwaBottomSheetController
      * @param description The app description.
      */
     public void requestBottomSheetInstaller(long nativePwaBottomSheetController,
-            WindowAndroid windowAndroid, WebContents webContents, boolean showExpanded, Bitmap icon,
+            WindowAndroid windowAndroid, WebContents webContents, Bitmap icon,
             boolean isAdaptiveIcon, String title, String origin, String description) {
         mNativePwaBottomSheetController = nativePwaBottomSheetController;
         mWebContents = webContents;
@@ -199,13 +198,22 @@ public class PwaBottomSheetController
             return;
         }
 
-        if (showExpanded) {
-            mBottomSheetController.expandSheet();
-        }
-
         if (webContents != null) {
             createWebContentsObserver(webContents);
         }
+    }
+
+    /**
+     * Makes a request to expand the Bottom Sheet Installer UI if visible already.
+     * @return Whether the Bottom Sheet Installer UI was expanded.
+     */
+    public boolean expandBottomSheetInstaller() {
+        if (mPwaBottomSheetContent != null
+                && mBottomSheetController.getCurrentSheetContent() == mPwaBottomSheetContent) {
+            mBottomSheetController.expandSheet();
+            return true;
+        }
+        return false;
     }
 
     // onClickListener:
@@ -231,12 +239,7 @@ public class PwaBottomSheetController
      * @param webContents The WebContents the UI is associated with.
      */
     public void requestOrExpandBottomSheetInstaller(WebContents webContents) {
-        if (mPwaBottomSheetContent != null
-                && mBottomSheetController.getCurrentSheetContent() == mPwaBottomSheetContent) {
-            mBottomSheetController.expandSheet();
-            return;
-        }
-
+        if (expandBottomSheetInstaller()) return;
         PwaBottomSheetControllerJni.get().createAndShowBottomSheetInstaller(webContents);
     }
 
