2b986fafc0cfa411f84eefe8754115da0d665c00
Danan S <danan@chromium.org>
2021-02-05 22:20:48 +0000

    Enable the EduCoexistenceV2 flag by default
    Enabling this feature by default required skipping it when other
    OOBE browsertests are running.  Because the EDU Coexistence screen
    is shown  immediately after signin for child users,  it conflicted
    with the expected post-signin state that many OOBE tests expect
    for child users.  Many of these tests wait for the disappearance
    of the OOBE user creation screen.
    
    Instead of special casing all these tests to expect a different
    screen in the event of a child user (which would be irrelevant
    to what they are trying to test), in this CL we always skip
    the EDU Coexistence screen in OOBE tests.
    
    The EDU Coexistence V2 screen itself is
    provided by a web app loaded over the internet (similar to the
    signin pages, which we also skip in these tests).  This makes
    it difficult to test without some kind of web app mock.
    
    In the future, we may consider adding a local mocked EDU
    Coexistence flow server to support a test of the EDU
    Coexistence screen itself separately from the other screens.
    
    
    Bug: 1147681
    Change-Id: I7a551658d8db2931913b789a329b1cdd70504e3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673785
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Dan S <danan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851308}

--- chrome/browser/chromeos/login/test/oobe_base_test.cc
+++ chrome/browser/chromeos/login/test/oobe_base_test.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/ui/webui_login_view.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
+#include "chrome/browser/supervised_user/supervised_user_features.h"
 #include "chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h"
@@ -89,6 +90,14 @@ class GaiaPageEventWaiter : public test::TestConditionWaiter {
 }  // namespace
 
 OobeBaseTest::OobeBaseTest() {
+  // Skip the EDU Coexistence Screen, because many OOBE tests don't expect
+  // the EDU coexistence screen after signin and fail if it isn't disabled.
+  // TODO(https://crbug.com/1175215): Make those tests work with this feature
+  // enabled.
+  scoped_feature_list_.InitWithFeatures(
+      {} /** enabled */,
+      {supervised_users::kEduCoexistenceFlowV2} /** disabled */);
+
   set_exit_when_last_browser_closes(false);
 }
 
