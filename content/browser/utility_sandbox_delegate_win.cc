b2e00fde984746842979f91c12cb382087f8f60c
Alex Gough <ajgo@chromium.org>
2021-02-06 03:42:15 +0000

    Marks CDM service as incompatible with CET.
    widevinecdm.dll is not compliant with CET and will crash when a user
    attempts to play protected content. This adds a workaround to turn off
    CET for the CDM service that hosts widevinecdm.dll. The workaround can
    be removed once widevinecdm.dll has been fixed.
    
    Bug: 1173700
    Tests: mini_installer confirms CET disabled for CDM service
    Change-Id: I38cb30cdc6052675d7678ae2161f7a0036da00cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676392
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851417}

--- content/browser/utility_sandbox_delegate_win.cc
+++ content/browser/utility_sandbox_delegate_win.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/sandboxed_process_launcher_delegate.h"
+#include "media/mojo/mojom/cdm_service.mojom.h"
 #include "sandbox/policy/features.h"
 #include "sandbox/policy/sandbox_type.h"
 #include "sandbox/policy/win/sandbox_win.h"
@@ -239,6 +240,16 @@ bool UtilitySandboxedProcessLauncherDelegate::ShouldUnsandboxedRunInJob() {
   if (utility_sub_type == network::mojom::NetworkService::Name_)
     return true;
   return false;
+}
+
+bool UtilitySandboxedProcessLauncherDelegate::CetCompatible() {
+  auto utility_sub_type =
+      cmd_line_.GetSwitchValueASCII(switches::kUtilitySubType);
+  // TODO(crbug.com/1173700) CDM loads widevinecdm.dll
+  // which is not CET-compliant.
+  if (utility_sub_type == media::mojom::CdmService::Name_)
+    return false;
+  return true;
 }
 
 }  // namespace content
