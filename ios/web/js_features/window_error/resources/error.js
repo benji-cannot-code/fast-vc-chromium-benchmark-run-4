0b392b32ab22659800a732592e285563398bc9d2
Mike Dougherty <michaeldo@chromium.org>
2021-02-06 01:30:18 +0000

    Migrate window error logging to JavaScriptFeature
    Moves error logging to an insolated world on iOS 14 and later. And
    utilizes FeatureScript::ReinjectionBehavior::
    kReinjectOnDocumentRecreation which enables the feature to continue
    functioning if the DOM is replaced for the same window.
    
    Bug: 1042335
    Change-Id: I615050b0aac328b498bb754bd44f6953d512bc72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2677169
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851394}

---
+++ ios/web/js_features/window_error/resources/error.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Error listener to report error details to the native app.
+ */
+
+goog.provide('__crWeb.error');
+
+// Requires __crWeb.common.
+
+/** Beginning of anonymous object */
+(function() {
+
+/**
+ * JavaScript errors are logged on the main application side. The handler is
+ * added ASAP to catch any errors in startup.
+ */
+window.addEventListener('error', function(event) {
+  __gCrWeb.common.sendWebKitMessage('WindowErrorResultHandler',
+      {'filename' : event.filename,
+       'line_number' : event.lineno,
+       'message': event.message.toString()
+      });
+});
+
+}());  // End of anonymous object
--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/web_state/ui/crw_web_view_proxy_impl.h"
 #import "ios/web/web_state/ui/crw_wk_ui_handler.h"
 #import "ios/web/web_state/ui/crw_wk_ui_handler_delegate.h"
-#import "ios/web/web_state/ui/js_window_error_manager.h"
 #import "ios/web/web_state/ui/wk_web_view_configuration_provider.h"
 #import "ios/web/web_state/user_interaction_state.h"
 #import "ios/web/web_state/web_state_impl.h"
@@ -152,9 +151,6 @@ NSString* const kSessionRestoreScriptMessageName = @"session_restore";
 
   // Manager for favicon JavaScript messages.
   std::unique_ptr<web::FaviconManager> _faviconManager;
-
-  // Manager for window.error message.
-  std::unique_ptr<web::JsWindowErrorManager> _jsWindowErrorManager;
 
   // Logger for cookie;.error message.
   std::unique_ptr<web::CookieBlockingErrorLogger> _cookieBlockingErrorLogger;
@@ -314,8 +310,6 @@ typedef void (^ViewportStateCompletion)(const web::PageViewportState*);
         initWithBrowserState:browserState];
     web::FindInPageManagerImpl::CreateForWebState(_webStateImpl);
     _faviconManager = std::make_unique<web::FaviconManager>(_webStateImpl);
-    _jsWindowErrorManager =
-        std::make_unique<web::JsWindowErrorManager>(_webStateImpl);
     _cookieBlockingErrorLogger =
         std::make_unique<web::CookieBlockingErrorLogger>(_webStateImpl);
 
