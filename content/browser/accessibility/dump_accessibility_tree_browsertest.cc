79254b6adf1be89747460494ef92490c0b8d873b
Nico Weber <thakis@chromium.org>
2021-02-06 00:21:43 +0000

    Revert "Disable flaky ReusedMapChangeMapName test"
    This reverts commit 6ddd4adeb452e8636dcbf4439851a1d381c34f25.
    
    Reason for revert: Prerequisite for reverting  https://chromium-review.googlesource.com/c/chromium/src/+/2666110
    
    Original change's description:
    > Disable flaky ReusedMapChangeMapName test
    >
    > Bug: 1175205
    > Change-Id: I6ba1cd47647b86b309f7166a80faee007c97c318
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679663
    > Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    > Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    > Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#851286}
    
    TBR=karandeepb@chromium.org,aleventhal@chromium.org,dlibby@microsoft.com,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,dlibby@chromium.org
    
    Change-Id: If38b224ccdbcb255760e4447ecb7b395bd35745c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1175205
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679625
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#851363}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -2857,8 +2857,7 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, ReusedMapChangeUsemap) {
   RunRegressionTest(FILE_PATH_LITERAL("reused-map-change-usemap.html"));
 }
 
-IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
-                       DISABLED_ReusedMapChangeMapName) {
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, ReusedMapChangeMapName) {
   RunRegressionTest(FILE_PATH_LITERAL("reused-map-change-map-name.html"));
 }
 
