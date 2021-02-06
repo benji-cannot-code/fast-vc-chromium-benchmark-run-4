394db406ac25f6295dcb679bce82704051fbf0b3
Michael Lippautz <mlippautz@chromium.org>
2021-02-06 01:16:30 +0000

    WebGPU: Fix include to allow building with Oilpan library
    MakeGarbageCollected<> requires all types of all fields to be present.
    
    Bug: 1056170
    Change-Id: I100e0ce0f0a96ba597d279f7d2fb25ee8e03459a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679483
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851380}

--- third_party/blink/renderer/modules/webgpu/gpu_swap_chain.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_swap_chain.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webgpu/gpu_canvas_context.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_device.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_texture.h"
+#include "third_party/blink/renderer/platform/graphics/gpu/webgpu_mailbox_texture.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 
 namespace blink {
