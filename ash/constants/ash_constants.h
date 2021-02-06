73c64dec3bc56fcb22499b0c1436161ad46fcc15
Henrique Ferreiro <hferreiro@igalia.com>
2021-02-06 07:13:56 +0000

    Move //chromeos/constants to //ash/constants
    Moving the actual target was the only thing left to do.
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    No-Presubmit: true
    Bug: 1164001
    Change-Id: I7b9dfc39c236e41d28b27def6a14910dbe8f3834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2666133
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Owners-Override: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851442}

--- ash/constants/ash_constants.h
+++ ash/constants/ash_constants.h
@@ -13,13 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::FilePath::CharType kDriveCacheDirname[];
-COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::FilePath::CharType kNssCertDbPath[];
-COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::FilePath::CharType kNssDirPath[];
-COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::FilePath::CharType kNssKeyDbPath[];
 
 }  // namespace chromeos
