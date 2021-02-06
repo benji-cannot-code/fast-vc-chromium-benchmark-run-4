2783fe6c56351193a792255cf8a9091ea1fd9e1c
Josh Nohle <nohle@google.com>
2021-02-06 04:16:36 +0000

    [Nearby] Process pending connection requests on device removal/change
    Fail pending connect-to-service-insecurely requests if either the device
    has been removed or the device changed with no RSSI. Before this CL,
    some pending requests were never resolved, which is deadly because of
    the synchronous nature of the adapter's ConnectToServiceInsecurely mojo
    method.
    
    Manually verified fix b/179370277 for Chromebook<->Chromebook and
    Chromebook<->Phone Nearby Share.
    
    Bug: 1175326, b/179370277
    Change-Id: I3cb4f9637fcb2e195b16dfbaf570a1f64e703a10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676983
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851423}

--- device/bluetooth/adapter.cc
+++ device/bluetooth/adapter.cc
@@ -273,6 +273,18 @@ void Adapter::DeviceAdded(device::BluetoothAdapter* adapter,
 
 void Adapter::DeviceChanged(device::BluetoothAdapter* adapter,
                             device::BluetoothDevice* device) {
+  const std::string& address = device->GetAddress();
+  base::Optional<int8_t> rssi = device->GetInquiryRSSI();
+  auto it = pending_connect_to_service_args_.begin();
+  while (it != pending_connect_to_service_args_.end()) {
+    if (address == std::get<0>(*it) && !rssi) {
+      std::move(std::get<2>(*it)).Run(/*result=*/nullptr);
+      it = pending_connect_to_service_args_.erase(it);
+    } else {
+      ++it;
+    }
+  }
+
   auto device_info = Device::ConstructDeviceInfoStruct(device);
   for (auto& observer : observers_)
     observer->DeviceChanged(device_info->Clone());
@@ -280,6 +292,17 @@ void Adapter::DeviceChanged(device::BluetoothAdapter* adapter,
 
 void Adapter::DeviceRemoved(device::BluetoothAdapter* adapter,
                             device::BluetoothDevice* device) {
+  const std::string& address = device->GetAddress();
+  auto it = pending_connect_to_service_args_.begin();
+  while (it != pending_connect_to_service_args_.end()) {
+    if (address == std::get<0>(*it)) {
+      std::move(std::get<2>(*it)).Run(/*result=*/nullptr);
+      it = pending_connect_to_service_args_.erase(it);
+    } else {
+      ++it;
+    }
+  }
+
   auto device_info = Device::ConstructDeviceInfoStruct(device);
   for (auto& observer : observers_)
     observer->DeviceRemoved(device_info->Clone());
@@ -296,7 +319,6 @@ void Adapter::GattServicesDiscovered(device::BluetoothAdapter* adapter,
     return;
 
   const std::string& address = device->GetAddress();
-
   auto it = pending_connect_to_service_args_.begin();
   while (it != pending_connect_to_service_args_.end()) {
     if (address == std::get<0>(*it)) {
@@ -319,7 +341,8 @@ void Adapter::OnDeviceFetchedForInsecureServiceConnection(
     const device::BluetoothUUID& service_uuid,
     ConnectToServiceInsecurelyCallback callback,
     device::BluetoothDevice* device) {
-  if (device->IsConnected() && !device->IsGattServicesDiscoveryComplete()) {
+  if (!device->IsPaired() && device->IsConnected() &&
+      !device->IsGattServicesDiscoveryComplete()) {
     // This provided device is most likely a result of calling ConnectDevice():
     // it's connected, but the remote device's services are still being probed
     // (IsGattServicesDiscoveryComplete() refers to all services, not just GATT
