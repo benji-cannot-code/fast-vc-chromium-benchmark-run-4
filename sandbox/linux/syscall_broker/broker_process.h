1412cda244613dbea8dfc955b01a8ac066e339b1
Dan Sanders <sandersd@chromium.org>
2021-02-06 13:26:11 +0000

    [Code health] Enable gn check for //sandbox/linux
    Bug: 1159029
    Change-Id: I8456458f9abc638d741a1192f184b795b544987f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2666706
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Auto-Submit: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Erik Staab <estaab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851453}

--- sandbox/linux/syscall_broker/broker_process.h
+++ sandbox/linux/syscall_broker/broker_process.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/pickle.h"
 #include "base/process/process.h"
-#include "sandbox/linux/bpf_dsl/trap_registry.h"
 #include "sandbox/linux/syscall_broker/broker_command.h"
 #include "sandbox/linux/syscall_broker/broker_permission_list.h"
 #include "sandbox/sandbox_export.h"
