01d89b22612177485ff3e53f75814e5158741258
Amanda Deacon <amandadeacon@chromium.org>
2021-02-07 23:02:37 +0000

    [Live Caption] Implement (chromeos-specific) UninstallSoda.
    1) Creates a new UninstallSoda method in SodaInstaller.
    2) Implements the chromeos-specific version.
    
    N.B. The non-chromeos-specific implementation is deferred for now, pending discussion re: how the current uninstallation code is working.
    
    This is part of a collection of WIP changes hidden behind feature flag/s.
    
    TEST= manually loaded DLCs to DUT and confirmed that a call to
    TEST= UninstallSoda succeeds (with no errors returned by the uninstall
    TEST= callback).
    
    Bug: 1055150
    Change-Id: I62c272eb34b87c0f1a715602ab6378a5ab9d5070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676223
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851554}

--- chrome/browser/accessibility/soda_installer_impl.cc
+++ chrome/browser/accessibility/soda_installer_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/feature_list.h"
 #include "base/no_destructor.h"
+#include "base/notreached.h"
 #include "base/numerics/ranges.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/component_updater/soda_component_installer.h"
@@ -107,6 +108,12 @@ void SodaInstallerImpl::InstallLanguage(PrefService* prefs) {
 bool SodaInstallerImpl::IsSodaInstalled() const {
   DCHECK(base::FeatureList::IsEnabled(media::kUseSodaForLiveCaption));
   return soda_binary_installed_ && language_installed_;
+}
+
+void SodaInstallerImpl::UninstallSoda(PrefService* global_prefs) {
+  // TODO(crbug.com/1055150): Refactor uninstallation code from elsewhere to
+  // here.
+  NOTREACHED();
 }
 
 void SodaInstallerImpl::OnEvent(Events event, const std::string& id) {
