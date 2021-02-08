8bd4e385b5f9b15fba3d2a0622d9c87d8ab6cabb
Dominik Röttsches <drott@chromium.org>
2021-02-08 11:12:35 +0000

    Revert "ServiceWorker: Throttle EmbeddedWorkerInstanceClient StopWorker calls."
    This reverts commit a25e1ae74424b13d418941baa92a2e0609e9c9aa.
    
    Reason for revert: Suspected to cause crashes on Webkit Linux Leak builder, see https://bugs.chromium.org/p/chromium/issues/detail?id=1175689
    
    Original change's description:
    > ServiceWorker: Throttle EmbeddedWorkerInstanceClient StopWorker calls.
    >
    > We are seeing rapid, repeated calls to EmbeddedWorkerInstanceClient
    > StopWorker() in crbug.com/1152793 causing excessive CPU usage.  This
    > attempts to mitigate the issue be throttling these calls to at most
    > one per second.
    >
    > Bug: 1152793
    > Change-Id: Ic8e0c862509e05a95421230e9a6b4ecc04cd272a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2672660
    > Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    > Reviewed-by: Brian White <bcwhite@chromium.org>
    > Auto-Submit: Ben Kelly <wanderview@chromium.org>
    > Commit-Queue: Brian White <bcwhite@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#851264}
    
    TBR=falken@chromium.org,bcwhite@chromium.org,shimazu@chromium.org,wanderview@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1152793
    Change-Id: I9b90d708ef3dec979fb3f4acb0774e4058fc36a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2681933
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851657}

--- content/browser/service_worker/embedded_worker_instance.cc
+++ content/browser/service_worker/embedded_worker_instance.cc
@@ -229,8 +229,6 @@ class EmbeddedWorkerInstance::WorkerProcessHandle {
   DISALLOW_COPY_AND_ASSIGN(WorkerProcessHandle);
 };
 
-constexpr base::TimeDelta EmbeddedWorkerInstance::kThrottledStopWorkerThreshold;
-
 // Info that is recorded as UMA on OnStarted().
 struct EmbeddedWorkerInstance::StartInfo {
   StartInfo(bool is_installed,
@@ -496,7 +494,7 @@ void EmbeddedWorkerInstance::Stop() {
     return;
   }
 
-  ThrottledStopWorker();
+  client_->StopWorker();
   status_ = EmbeddedWorkerStatus::STOPPING;
   for (auto& observer : listener_list_)
     observer.OnStopping();
@@ -1140,36 +1138,6 @@ void EmbeddedWorkerInstance::BindCacheStorageInternal() {
                        std::move(coep_reporter_remote), std::move(receiver)));
   }
   pending_cache_storage_receivers_.clear();
-}
-
-void EmbeddedWorkerInstance::ThrottledStopWorker() {
-  static_assert(
-      kThrottledStopWorkerThreshold < ServiceWorkerVersion::kStopWorkerTimeout,
-      "StopWorker throttle threshold should be less than expected "
-      "stop timeout.");
-
-  base::TimeDelta elapsed = base::Time::Now() - last_stop_time_;
-  bool needs_delay = elapsed < kThrottledStopWorkerThreshold;
-
-  UMA_HISTOGRAM_BOOLEAN("ServiceWorker.ThrottledStopWorker.NeedsDelay",
-                        needs_delay);
-
-  if (!needs_delay) {
-    StopWorkerInternal();
-    return;
-  }
-
-  if (stop_timer_.IsRunning())
-    return;
-
-  stop_timer_.Start(FROM_HERE, (kThrottledStopWorkerThreshold - elapsed),
-                    base::BindOnce(&EmbeddedWorkerInstance::StopWorkerInternal,
-                                   base::Unretained(this)));
-}
-
-void EmbeddedWorkerInstance::StopWorkerInternal() {
-  last_stop_time_ = base::Time::Now();
-  client_->StopWorker();
 }
 
 }  // namespace content
