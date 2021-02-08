a9aa891bb52c4b8f283b110061d24c01c865cd34
Claude van der Merwe <cvandermerwe@google.com>
2021-02-08 02:12:27 +0000

    Migrate os_apps_page to Polymer 3
    Bug: 1045266
    Change-Id: I80fc26ba0bbbf56145d0e7a645f156cb59ac9e3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2665254
    Commit-Queue: Claude van der Merwe <cvandermerwe@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851570}

--- chrome/browser/resources/settings/chromeos/os_apps_page/android_apps_browser_proxy.js
+++ chrome/browser/resources/settings/chromeos/os_apps_page/android_apps_browser_proxy.js
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
+// #import {addSingletonGetter} from 'chrome://resources/js/cr.m.js';
+// clang-format on
+
 /**
  * @fileoverview A helper object used by the "Google Play Store" (ARC) section
  * to retrieve information about android apps.
@@ -18,11 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * }}
  * @see chrome/browser/ui/webui/settings/chromeos/android_apps_handler.cc
  */
-let AndroidAppsInfo;
+/* #export */ let AndroidAppsInfo;
 
 cr.define('settings', function() {
   /** @interface */
-  class AndroidAppsBrowserProxy {
+  /* #export */ class AndroidAppsBrowserProxy {
     requestAndroidAppsInfo() {}
 
     /**
@@ -35,7 +39,7 @@ cr.define('settings', function() {
   /**
    * @implements {settings.AndroidAppsBrowserProxy}
    */
-  class AndroidAppsBrowserProxyImpl {
+  /* #export */ class AndroidAppsBrowserProxyImpl {
     /** @override */
     requestAndroidAppsInfo() {
       chrome.send('requestAndroidAppsInfo');
--- chrome/test/data/webui/settings/chromeos/apps_page_test.js
+++ chrome/test/data/webui/settings/chromeos/apps_page_test.js
@@ -3,6 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
+// #import 'chrome://os-settings/chromeos/os_settings.js';
+
+// #import {AndroidAppsBrowserProxyImpl, Router, routes} from 'chrome://os-settings/chromeos/os_settings.js';
+// #import {TestAndroidAppsBrowserProxy} from './test_android_apps_browser_proxy.m.js';
+// #import {flush} from'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// #import {waitAfterNextRender} from 'chrome://test/test_util.m.js';
+// #import {getDeepActiveElement} from 'chrome://resources/js/util.m.js';
+// clang-format on
+
 /** @type {?OsSettingsAppsPageElement} */
 let appsPage = null;
 
