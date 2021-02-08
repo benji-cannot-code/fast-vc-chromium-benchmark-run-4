bd9368c46630185ea2d8db2ee82073bd890f721b
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2021-02-08 19:14:29 +0000

    Reland "mojo-js: Add flush() to receivers"
    This is a reland of 656d52a1317ad0a00f7c03dab775fcfa9b8ce32f
    
    The CL was reverted due to http/tests/mojo/bindings-lite-sw.https.html
    timing out. Per crbug.com/1174822 there doesn't seem to be anything
    wrong with the tests; they just take a long time.
    
    This reland changes the number of calls to didFlush() from 10000
    to 1000 to avoid making the test longer.
    
    Original change's description:
    > mojo-js: Add flush() to receivers
    >
    > Adds a method to Receivers to flush calls"
    >
    > receiver.$.flush();
    >
    > or
    >
    > callbackRouter.$.flush();
    >
    > Bug: 955171
    > Change-Id: Idce46c7dca140c42e390495752cc324d84113eed
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2672542
    > Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Cr-Commit-Position: refs/heads/master@{#850484}
    
    Bug: 955171
    Change-Id: I3647d9f2782bde48c18b1578ec13264d4c6a9148
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679074
    Auto-Submit: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#851811}

--- mojo/public/js/interface_support.js
+++ mojo/public/js/interface_support.js
@@ -1033,6 +1033,16 @@ mojo.internal.interfaceSupport.InterfaceReceiverHelperInternal = class {
     return this.connectionErrorEventRouter_;
   }
 
+  /**
+   * @return {!Promise}
+   * @export
+   */
+  async flush() {
+    for (let endpoint of this.endpoints_) {
+      await endpoint.flushForTesting();
+    }
+  }
+
   /** @override */
   onMessageReceived(endpoint, header, buffer, handles) {
     if (header.flags & mojo.internal.kMessageFlagIsResponse)
@@ -1141,6 +1151,14 @@ mojo.internal.interfaceSupport.InterfaceReceiverHelper = class {
   /** @export */
   close() {
     this.helper_internal_.closeBindings();
+  }
+
+  /**
+   * @return {!Promise}
+   * @export
+   */
+  flush() {
+    return this.helper_internal_.flush();
   }
 }
 
