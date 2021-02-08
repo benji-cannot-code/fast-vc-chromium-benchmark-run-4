f71d622128030c502e44515f010fc99bb12dd510
Jon Mann <jonmann@chromium.org>
2021-02-08 21:54:12 +0000

    Wi-Fi Sync: Prevent crash when removing a network before sync is ready.
    This change also prevents a crash if the map of pending updates is
    modified while it's being iterated over by making a copy.
    
    Bug: 1163357
    Change-Id: I66ec872d326a4dc1d69b5ef49b2abc5464408101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676803
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851911}

--- chromeos/components/sync_wifi/wifi_configuration_bridge_unittest.cc
+++ chromeos/components/sync_wifi/wifi_configuration_bridge_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/callback_helpers.h"
 #include "base/containers/flat_map.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
@@ -222,6 +223,25 @@ class WifiConfigurationBridgeTest : public testing::Test {
         }));
     loop.Run();
     return data;
+  }
+
+  // This can only be called before InitializeSyncStore().
+  void PresaveSyncedNetwork(const WifiConfigurationSpecifics& proto) {
+    std::unique_ptr<syncer::ModelTypeStore::WriteBatch> batch =
+        store_->CreateWriteBatch();
+    std::string storage_key =
+        NetworkIdentifier::FromProto(proto).SerializeToString();
+    batch->WriteData(storage_key, proto.SerializeAsString());
+
+    base::RunLoop run_loop;
+    store_->CommitWriteBatch(
+        std::move(batch),
+        base::BindLambdaForTesting(
+            [&](const base::Optional<syncer::ModelError>& error) {
+              EXPECT_FALSE(error);
+              run_loop.Quit();
+            }));
+    run_loop.Run();
   }
 
   syncer::MockModelTypeChangeProcessor* processor() { return &mock_processor_; }
@@ -634,6 +654,26 @@ TEST_F(WifiConfigurationBridgeTest, LocalRemove) {
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(storage_key, meow_network_id().SerializeToString());
   histogram_tester.ExpectTotalCount(kTotalCountHistogram, 1);
+}
+
+TEST_F(WifiConfigurationBridgeTest, LocalRemoved_BeforeInit) {
+  WifiConfigurationSpecifics meow_local =
+      GenerateTestWifiSpecifics(meow_network_id(), kSyncPsk, /*timestamp=*/100);
+  std::string guid = meow_network_id().SerializeToString();
+  local_network_collector()->AddNetwork(meow_local);
+  PresaveSyncedNetwork(meow_local);
+  bridge()->OnBeforeConfigurationRemoved("service_path", guid);
+
+  EXPECT_CALL(*processor(), Delete(_, _)).Times(0);
+  bridge()->OnConfigurationRemoved("service_path", guid);
+  base::RunLoop().RunUntilIdle();
+
+  timer_factory()->FireAll();
+  base::RunLoop().RunUntilIdle();
+
+  EXPECT_CALL(*processor(), Delete(_, _)).Times(1);
+  InitializeSyncStore();
+  base::RunLoop().RunUntilIdle();
 }
 
 }  // namespace
