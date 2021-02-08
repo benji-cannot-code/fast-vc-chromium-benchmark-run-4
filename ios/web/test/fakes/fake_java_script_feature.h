d50b749496be9a5a3af54fd91a0ce1e1a2b9ea79
Mike Dougherty <michaeldo@chromium.org>
2021-02-08 20:14:02 +0000

    Add JavaScriptFeature inttest for ReinjectionBehavior
    Adds tests to ensure that the reinjection behavior of a
    JavaScriptFeature works as expected.
    
    In order to support these tests, add a
    JavaScriptFeature::CallJavaScriptFunction function with a callback to
    return the value of the executed script.
    
    Bug: 1042335
    Change-Id: Ie1f5f24539c690856b0cb522c8a37dd9afb6506e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2681656
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851849}

--- ios/web/test/fakes/fake_java_script_feature.h
+++ ios/web/test/fakes/fake_java_script_feature.h
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include <base/values.h>
+#include "base/callback.h"
+#include "base/values.h"
 #import "ios/web/public/js_messaging/java_script_feature.h"
 
 @class WKScriptMessage;
@@ -42,6 +43,10 @@ class FakeJavaScriptFeature : public JavaScriptFeature {
   // |parameters| in |web_frame|.
   void ReplyWithPostMessage(WebFrame* web_frame,
                             const std::vector<base::Value>& parameters);
+
+  // Returns the number of errors received
+  void GetErrorCount(WebFrame* web_frame,
+                     base::OnceCallback<void(const base::Value*)> callback);
 
   BrowserState* last_received_browser_state() const {
     return last_received_browser_state_;
