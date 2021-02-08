5f68947d78484680545616a55e95da875be79232
Kunihiko Sakamoto <ksakamoto@chromium.org>
2021-02-08 05:02:08 +0000

    Revert "Privacy Sandbox Android: updated logic for showing the snackbar in Cookie settings"
    This reverts commit dafb2babbf80c4de4cb00d726d5028639ca9b337.
    
    Reason for revert: Needed to revert crrev.com/c/2675903; see
    crbug.com/1175621.
    
    Original change's description:
    > Privacy Sandbox Android: updated logic for showing the snackbar in Cookie settings
    >
    > Bug: 1152351
    > Change-Id: Ic1218fea02ce81ff13d1b8648b2529ea1c56925d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676767
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    > Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    > Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    > Cr-Commit-Position: refs/heads/master@{#851201}
    
    TBR=dullweber@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,andzaytsev@google.com,sauski@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1152351,1175621
    Change-Id: Ibf307e821f779f9aeb770c3025536b46b920b091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2681341
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#851592}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxBridge.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxBridge.java
@@ -9,10 +9,6 @@ import org.chromium.base.annotations.NativeMethods;
 
 /** Bridge, providing access to the native-side Privacy Sandbox configuration. */
 public class PrivacySandboxBridge {
-    public static boolean isPrivacySandboxFunctional() {
-        return PrivacySandboxBridgeJni.get().isPrivacySandboxFunctional();
-    }
-
     public static boolean isPrivacySandboxEnabled() {
         return PrivacySandboxBridgeJni.get().isPrivacySandboxEnabled();
     }
@@ -27,7 +23,6 @@ public class PrivacySandboxBridge {
 
     @NativeMethods
     interface Natives {
-        boolean isPrivacySandboxFunctional();
         boolean isPrivacySandboxEnabled();
         boolean isPrivacySandboxManaged();
         void setPrivacySandboxEnabled(boolean enabled);
