f39d40cd71b4e7ffc1e90492e524d8f527bb68af
Ayu Ishii <ayui@chromium.org>
2021-02-08 17:54:26 +0000

    CodeHealth: Convert ppapi/proxy/* from base::Bind/base::Callback
    This changes convert uses of base::Bind and base::Callback to
    Once/Repeating. Refer to (https://crbug.com/714018) for details.
    
    Bug: 1007816
    Change-Id: I1398e4fabc3abecc36bd7de385397d46579ec554
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2669454
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851774}

--- ppapi/proxy/ppapi_proxy_test.cc
+++ ppapi/proxy/ppapi_proxy_test.cc
@@ -95,10 +95,10 @@ void TearDownRemoteHarness(ProxyTestHarnessBase* harness,
   harness_torn_down->Signal();
 }
 
-void RunTaskOnRemoteHarness(const base::Closure& task,
+void RunTaskOnRemoteHarness(base::OnceClosure task,
                             base::WaitableEvent* task_complete) {
- task.Run();
- task_complete->Signal();
+  std::move(task).Run();
+  task_complete->Signal();
 }
 
 }  // namespace
@@ -588,15 +588,15 @@ void TwoWayTest::TearDown() {
   io_thread_.Stop();
 }
 
-void TwoWayTest::PostTaskOnRemoteHarness(const base::Closure& task) {
+void TwoWayTest::PostTaskOnRemoteHarness(base::OnceClosure task) {
   base::WaitableEvent task_complete(
       base::WaitableEvent::ResetPolicy::MANUAL,
       base::WaitableEvent::InitialState::NOT_SIGNALED);
   plugin_thread_.task_runner()->PostTask(
-      FROM_HERE, base::BindOnce(&RunTaskOnRemoteHarness, task, &task_complete));
+      FROM_HERE,
+      base::BindOnce(&RunTaskOnRemoteHarness, std::move(task), &task_complete));
   task_complete.Wait();
 }
-
 
 }  // namespace proxy
 }  // namespace ppapi
