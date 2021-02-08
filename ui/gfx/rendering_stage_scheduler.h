8cfa797211c2e155432e7bb5cb1b77b7e952f99f
Stephan Hartmann <stha09@googlemail.com>
2021-02-08 20:47:12 +0000

    IWYU: add missing include for std::unique_ptr
    Bug: None
    Change-Id: I86445b69402d31adf4f616eeb8ecea20ba97fe67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679524
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851872}

--- ui/gfx/rendering_stage_scheduler.h
+++ ui/gfx/rendering_stage_scheduler.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_RENDERING_STAGE_SCHEDULER_H_
 #define UI_GFX_RENDERING_STAGE_SCHEDULER_H_
 
+#include <memory>
 #include <vector>
 
 #include "base/threading/platform_thread.h"
