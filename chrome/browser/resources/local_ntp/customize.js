1f6a8232e6d09910bfff22c964fc899cb0d3556c
Nathan Naze <nnaze@google.com>
2021-02-08 17:14:08 +0000

    Add proper JSDoc @license tags and SPDX lines
    This is needed for Copybara ingestion for go/thirdpartyjavascript
    
    Change-Id: I6740fdd34795e1c8f00342ced109a127c02a9684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2680028
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Auto-Submit: Nathan Naze <nnaze@google.com>
    Cr-Commit-Position: refs/heads/master@{#851734}

--- chrome/browser/resources/local_ntp/customize.js
+++ chrome/browser/resources/local_ntp/customize.js
@@ -1,7 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/**
+ * @license
+ * Copyright 2018 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
 'use strict';
 
--- chrome/browser/resources/local_ntp/most_visited_single.js
+++ chrome/browser/resources/local_ntp/most_visited_single.js
@@ -1,7 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/* Copyright 2015 The Chromium Authors. All rights reserved.
+/**
+ * @license
+ * Copyright 2015 The Chromium Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file. */
+ * found in the LICENSE file.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
 // Single iframe for NTP tiles.
 
