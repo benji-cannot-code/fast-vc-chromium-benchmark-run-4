f30abdd770ffe9ff40e1ab6cf3f9c88f26972818
Gang Wu <gangwu@chromium.org>
2021-02-08 20:17:01 +0000

    Add the histogram to record tab matching time
    Add the histogram to record tab matching time when the Omnibox offer
    suggestions.
    We are adding "switch to tab" feature to Android, but there is a
    regression on the histogram "Omnibox.QueryTime2.0", the time increase
    30% at 99th percentiles. more details at
    https://docs.google.com/document/d/1RGtDnu8abCMJ7pEmHYQ7d61ipzkyyvJOqZuc0SJVBS8/edit
    Since "switch to tab" does need some extra works for tab matching, we
    need to know how much time to do tab matching on other platform, and how
    much we should expected "Omnibox.QueryTime2.0" increase, we add this
    histogram.
    
    
    Bug: 1170390
    Change-Id: Ie259a0945169e2d353d191d76b0afde0131c8568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2647792
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#851852}

--- components/omnibox/browser/autocomplete_result.cc
+++ components/omnibox/browser/autocomplete_result.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
@@ -497,6 +498,7 @@ void AutocompleteResult::AttachPedalsToMatches(
 void AutocompleteResult::ConvertOpenTabMatches(
     AutocompleteProviderClient* client,
     const AutocompleteInput* input) {
+  base::TimeTicks start_time = base::TimeTicks::Now();
   for (auto& match : matches_) {
     // If already converted this match, don't re-search through open tabs and
     // possibly re-change the description.
@@ -507,6 +509,11 @@ void AutocompleteResult::ConvertOpenTabMatches(
       match.has_tab_match = true;
     }
   }
+
+  base::TimeDelta time_delta = base::TimeTicks::Now() - start_time;
+  UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
+      "Omnibox.TabMatchTime", time_delta, base::TimeDelta::FromMicroseconds(1),
+      base::TimeDelta::FromMilliseconds(5), 50);
 }
 
 bool AutocompleteResult::HasCopiedMatches() const {
