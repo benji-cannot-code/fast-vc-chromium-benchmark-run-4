f04a995bf0c6841a3ab51014ea5ad47ec6fe8e72
Ella Ge <eirage@chromium.org>
2021-02-08 17:41:21 +0000

    Remove ChromeActivity usage in WebApkServiceClient
    This CL changes it to check activity.isFinishing() and
    activity.isDestroyed() directly instead of using
    activity.isActivityFinishingOrDestroyed(). Therefore,
    ChromeActivity usage in WebApkServiceClient and
    TwaFinishHandler can be removed.
    
    Bug: 1155804
    Change-Id: Iaa0d64078b08af508c82788365b2da615cb06fca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679010
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851770}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/trustedwebactivityui/TwaFinishHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/trustedwebactivityui/TwaFinishHandler.java
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.browserservices.trustedwebactivityui;
 
+import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 
-import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.browserservices.BrowserServicesIntentDataProvider;
 import org.chromium.chrome.browser.customtabs.CustomTabsConnection;
 import org.chromium.chrome.browser.dependency_injection.ActivityScope;
@@ -25,15 +25,15 @@ public class TwaFinishHandler {
     private static final String FINISH_TASK_COMMAND_NAME = "finishAndRemoveTask";
     private static final String SUCCESS_KEY = "success";
 
-    private final ChromeActivity<?> mActivity;
+    private final Activity mActivity;
     private final BrowserServicesIntentDataProvider mIntentDataProvider;
     private final CustomTabsConnection mConnection;
 
     private boolean mShouldAttemptFinishingTask;
 
     @Inject
-    public TwaFinishHandler(ChromeActivity<?> activity,
-            BrowserServicesIntentDataProvider intentDataProvider, CustomTabsConnection connection) {
+    public TwaFinishHandler(Activity activity, BrowserServicesIntentDataProvider intentDataProvider,
+            CustomTabsConnection connection) {
         mActivity = activity;
         mIntentDataProvider = intentDataProvider;
         mConnection = connection;
