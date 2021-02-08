52503b188076a070ba92b77c6fefb1c8f854e6bb
Rachel Wong <wrong@chromium.org>
2021-02-08 06:21:41 +0000

    Revert "[Structured metrics] Use storage protos for key data."
    This reverts commit a791cb700420daadf5b2e36b3f5c9cf50a144c85.
    
    Reason for revert: This CL broke chromium build.
    
    Original change's description:
    > [Structured metrics] Use storage protos for key data.
    >
    > This uses the protos and persistent proto class added in previous CLs
    > to change how the KeyData class does its storage. This is essentially
    > a rewrite of the KeyData class to make it much simpler, with secondary
    > effects on the SturcturedMetricsProvider class and histogram code.
    >
    > Note that there are two unit tests that now fail in
    > structured_metrics_provider_unittest, these test integration between
    > the provider and key data. I've disabled these two tests for now,
    > because the next CL updates the provider to use protos as well and fixes
    > the integration issues.
    >
    > There's also an old test for migrating key data format which is no
    > longer relevant, as we're making a breaking change here.
    >
    > Bug: 1148168
    > Change-Id: I7ffc00576aca61a84d3028840ac1c721bdba3636
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2639131
    > Commit-Queue: Tony Yeoman <tby@chromium.org>
    > Reviewed-by: Rachel Wong <wrong@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#850884}
    
    TBR=tby@chromium.org,wrong@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1148168
    Change-Id: I1e8aaeebf219288c7aa0d2491454c418f3520456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679088
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851608}

--- components/metrics/structured/structured_metrics_provider.cc
+++ components/metrics/structured/structured_metrics_provider.cc
@@ -88,8 +88,6 @@ int StructuredMetricsProvider::kMaxEventsPerUpload = 100;
 
 char StructuredMetricsProvider::kStorageFileName[] = "structured_metrics.json";
 
-char StructuredMetricsProvider::kStorageDirectory[] = "structured_metrics";
-
 StructuredMetricsProvider::StructuredMetricsProvider() = default;
 
 StructuredMetricsProvider::~StructuredMetricsProvider() {
@@ -209,8 +207,8 @@ void StructuredMetricsProvider::OnRecord(const EventBase& event) {
   // ID that will eventually be used as the profile_event_id of this event.
   base::Value event_value(base::Value::Type::DICTIONARY);
   event_value.SetStringKey("name", base::NumberToString(event.name_hash()));
-  event_value.SetStringKey(
-      "id", base::NumberToString(key_data_->Id(event.project_name_hash())));
+  event_value.SetStringKey("id", base::NumberToString(key_data_->UserProjectId(
+                                     event.project_name_hash())));
   event_value.SetKey("metrics", std::move(metrics));
 
   // Add the event to |storage_|.
@@ -237,11 +235,7 @@ void StructuredMetricsProvider::OnInitializationCompleted(const bool success) {
   if (!success)
     return;
   DCHECK(!storage_->ReadOnly());
-  // TODO(crbug.com/1148168): Replace storage_directory with a member once we
-  // use it for StructuredMetricsProvider as well as KeyData.
-  const base::FilePath storage_directory(kStorageDirectory);
-  key_data_ = std::make_unique<KeyData>(storage_directory.Append("keys"),
-                                        base::DoNothing());
+  key_data_ = std::make_unique<internal::KeyData>(storage_.get());
   initialized_ = true;
 }
 
