faed424458d7efe1acd0e8e06cfbeb2f1a5e7c4d
Ioana Pandele <ioanap@chromium.org>
2021-02-08 11:16:18 +0000

    [PwdEditAndroid] Pass credential from settings to the edit bridge
    Bug: 1170289
    Change-Id: I703b4d456403edc7ebcc9ec91b150067020c9feb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675971
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851658}

--- chrome/browser/password_entry_edit/android/internal/java/src/org/chromium/chrome/browser/password_entry_edit/CredentialEditBridge.java
+++ chrome/browser/password_entry_edit/android/internal/java/src/org/chromium/chrome/browser/password_entry_edit/CredentialEditBridge.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.password_entry_edit;
 import android.content.Context;
 
 import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.NativeMethods;
 import org.chromium.components.browser_ui.settings.SettingsLauncher;
 
 /**
@@ -30,7 +31,18 @@ class CredentialEditBridge {
     }
 
     @CalledByNative
+    void setCredential(String displayUrlOrAppName, String username, String password,
+            String displayFederationOrigin) {
+        // TODO(crbug.com/1170289): Pass the credential data to the UI to be displayed.
+    }
+
+    @CalledByNative
     void destroy() {
         mNativeCredentialEditBridge = 0;
+    }
+
+    @NativeMethods
+    interface Natives {
+        void getCredential(long nativeCredentialEditBridge);
     }
 }
