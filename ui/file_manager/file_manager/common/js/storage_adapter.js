9214cf933884805953e85983693682dfad8b6264
Alex Danilo <adanilo@chromium.org>
2021-02-08 06:25:17 +0000

    Files SWA: Add persistence for chrome.storage adapter
    Uses localStorage to store and retrieve data that is produced and
    consumed by the 'chrome.storage' adapter.
    
    Note: the initial load of data from localStorage is deferred until it
    is first used to avoid blocking the main thread when instantiating the
    StorageAdapter class.
    
    Bug: 1113981
    Change-Id: Iab3d031c7ecf235537d400fe5da0583cf0a194e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2674603
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851609}

--- ui/file_manager/file_manager/common/js/storage_adapter.js
+++ ui/file_manager/file_manager/common/js/storage_adapter.js
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @suppress {checkTypes} */
 /* #export */ class StorageAdapter {
   constructor() {
-    this.onChanged = {
-      addListener() {},
-    };
+    /** @private {boolean} */
+    this.storageHasBeenLoaded_ = false;
+
+    this.onChanged = {addListener() {}};
 
     this.sync = {
       /** @private {!Object} store */
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       get(keys, callback) {
         const inKeys = Array.isArray(keys) ? keys : [keys];
         const result = {};
+        chrome.storage.loadStorageIfNeeded_();
         inKeys.forEach(key => {
           if (key in chrome.storage.sync.store_) {
             result[key] = chrome.storage.sync.store_[key];
@@ -35,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         for (const key in items) {
           chrome.storage.sync.store_[key] = items[key];
         }
+        chrome.storage.flushIntoLocalStorage_('sync');
         if (opt_callback) {
           setTimeout(opt_callback);
         }
@@ -51,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       get(keys, callback) {
         const inKeys = Array.isArray(keys) ? keys : [keys];
         const result = {};
+        chrome.storage.loadStorageIfNeeded_();
         inKeys.forEach(key => {
           if (key in chrome.storage.local.store_) {
             result[key] = chrome.storage.local.store_[key];
@@ -66,10 +70,46 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         for (const key in items) {
           chrome.storage.local.store_[key] = items[key];
         }
+        chrome.storage.flushIntoLocalStorage_('local');
         if (opt_callback) {
           setTimeout(opt_callback);
         }
       }
     };
+  }
+
+  /**
+   * If localStorage hasn't been loaded, read it and populate the
+   * 'sync' and 'local' stores so they can be read.
+   * @private
+   */
+  loadStorageIfNeeded_() {
+    if (this.storageHasBeenLoaded_ === false) {
+      const localData = window.localStorage.getItem('local');
+      if (localData) {
+        this.local.store_ = JSON.parse(localData);
+      }
+      const syncData = window.localStorage.getItem('sync');
+      if (syncData) {
+        this.sync.store_ = JSON.parse(syncData);
+      }
+      // Only do this once.
+      this.storageHasBeenLoaded_ = true;
+    }
+  }
+
+  /**
+   * Write out the 'sync' and 'local' stores into localStorage.
+   * @param {string} which Which store key we're writing to.
+   * @private
+   */
+  flushIntoLocalStorage_(which) {
+    let storeObjectString = null;
+    if (which === 'local') {
+      storeObjectString = JSON.stringify(this.local.store_);
+    } else {
+      storeObjectString = JSON.stringify(this.sync.store_);
+    }
+    window.localStorage.setItem(which, storeObjectString);
   }
 }
