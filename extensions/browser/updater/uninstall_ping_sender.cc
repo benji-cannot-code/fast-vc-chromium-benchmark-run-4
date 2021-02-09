e0e1f98855789be3fe5ce2fc254664ca4da747da
Rushan Suleymanov <rushans@google.com>
2021-02-09 08:48:37 +0000

    Enable gn check for //extensions/browser/updater
    UninstallPingSender is also moved to extensions/browser/updater because
    it deals with only UpdateService.
    
    ScopedObserver is replaced by ScopedObservation to pass presubmit check.
    
    Bug: 1158985
    Change-Id: Ia16b46e160d79c6c4cbedee628f1291495de78fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2674928
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#852126}

--- extensions/browser/updater/uninstall_ping_sender.cc
+++ extensions/browser/updater/uninstall_ping_sender.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "extensions/browser/uninstall_ping_sender.h"
+#include "extensions/browser/updater/uninstall_ping_sender.h"
 
 #include "base/version.h"
 #include "extensions/browser/updater/update_service.h"
@@ -13,7 +13,7 @@ namespace extensions {
 UninstallPingSender::UninstallPingSender(ExtensionRegistry* registry,
                                          Filter filter)
     : filter_(std::move(filter)) {
-  observer_.Add(registry);
+  observer_.Observe(registry);
 }
 
 UninstallPingSender::~UninstallPingSender() {}
