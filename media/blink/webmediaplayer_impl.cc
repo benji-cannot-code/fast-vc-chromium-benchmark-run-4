16499a931665beed1d5a8c7bf6f48a150538b018
Mario Sanchez Prada <mario@igalia.com>
2021-02-09 12:53:06 +0000

    Migrate MediaPlayerDelegateHostMsg_OnAudioOutputSinkChanged to mojo
    Migrate this legacy IPC message to the media.mojom.MediaPlayerObserver
    mojo interface, implemented by MediaSessionController in the browser
    process, and update callers in the renderer process.
    
    Relevant design document:
    https://docs.google.com/document/d/1OLMNxLvGkRO6ju_WfHbRMrgaVnW7bsMnW7XlJQI0A2E
    
    Bug: 1039252
    Change-Id: I6166aee0ebb589ae9bd2709ccdb4937ae99ac066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650145
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852166}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -1064,7 +1064,7 @@ void WebMediaPlayerImpl::OnRequestPictureInPicture() {
   DCHECK(bridge_->GetSurfaceId().is_valid());
 }
 
-void WebMediaPlayerImpl::SetSinkId(
+bool WebMediaPlayerImpl::SetSinkId(
     const blink::WebString& sink_id,
     blink::WebSetSinkIdCompleteCallback completion_callback) {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
@@ -1076,7 +1076,7 @@ void WebMediaPlayerImpl::SetSinkId(
   media_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&SetSinkIdOnMediaThread, audio_source_provider_,
                                 sink_id_utf8, std::move(callback)));
-  delegate_->DidAudioOutputSinkChange(delegate_id_, sink_id_utf8);
+  return true;
 }
 
 STATIC_ASSERT_ENUM(WebMediaPlayer::kPreloadNone, MultibufferDataSource::NONE);
--- third_party/blink/renderer/core/html/media/html_media_element_test.cc
+++ third_party/blink/renderer/core/html/media/html_media_element_test.cc
@@ -156,6 +156,8 @@ class MockMediaPlayerObserverReceiverForTesting
 
   void OnPictureInPictureAvailabilityChanged(bool available) override {}
 
+  void OnAudioOutputSinkChanged(const WTF::String& hashed_device_id) override {}
+
   void OnAudioOutputSinkChangingDisabled() override {}
 
   void OnBufferUnderflow() override {
