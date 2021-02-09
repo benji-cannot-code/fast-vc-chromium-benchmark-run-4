7b6e939b4ad7ba0d6e1150f9cb2e2032635e5909
Josh Nohle <nohle@google.com>
2021-02-09 02:01:11 +0000

    [Nearby] Refactor processing of insecure service connection requests
    This is a pure refactor of the changes made in
    https://crrev.com/c/2676983. We consolidate the iteration of the
    pending request list to a helper function.
    
    Manually tested Chromebook-to-Chromebook for the following scenarios
    suggested in https://crrev.com/c/2676983:
    
    * Chromebooks never paired before; discovered via high-viz
    * Chromebooks never paired before; discovered via BLE
    * Chromebooks have paired before; discovered via high-viz
    * Chromebooks have paired before; discovered via BLE
    
    Tested using slightly modified repro steps from b/179370277:
    
    0.  Put Chromebooks in appropriate visibilities and remove pairing if
        desired using bluetoothctl
    1.  Start discovery on sender
    2.  (High-viz only) Put receiver into high-visibility mode
    3.  Notice sender discovers receiver
    4a. (High-viz only) Turn off high-visibility mode on receiver, or
    4b. (Contact-based or high-viz) lock the screen of the receiver
    5.  Select stale receiver share target on sender and click "Next"
    6.  Cancel or wait for error on sender
    7.  Unlock screen of receiver if necessary
    8.  Try to share again
    
    Bug: 1175326, b/179370277
    Change-Id: I3c732ec886c0bbc3f54ee876f1610f13d04487a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683263
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852027}

--- device/bluetooth/adapter.cc
+++ device/bluetooth/adapter.cc
@@ -273,16 +273,12 @@ void Adapter::DeviceAdded(device::BluetoothAdapter* adapter,
 
 void Adapter::DeviceChanged(device::BluetoothAdapter* adapter,
                             device::BluetoothDevice* device) {
-  const std::string& address = device->GetAddress();
-  base::Optional<int8_t> rssi = device->GetInquiryRSSI();
-  auto it = pending_connect_to_service_args_.begin();
-  while (it != pending_connect_to_service_args_.end()) {
-    if (address == std::get<0>(*it) && !rssi) {
-      std::move(std::get<2>(*it)).Run(/*result=*/nullptr);
-      it = pending_connect_to_service_args_.erase(it);
-    } else {
-      ++it;
-    }
+  // Because paired Bluetooth devices never fire device-removed events, we also
+  // consider a null RSSI indicative of a device no longer being discoverable.
+  // In this scenario, we fail any pending connection requests.
+  if (!device->GetInquiryRSSI()) {
+    ProcessPendingInsecureServiceConnectionRequest(device->GetAddress(),
+                                                   /*device=*/nullptr);
   }
 
   auto device_info = Device::ConstructDeviceInfoStruct(device);
@@ -292,16 +288,8 @@ void Adapter::DeviceChanged(device::BluetoothAdapter* adapter,
 
 void Adapter::DeviceRemoved(device::BluetoothAdapter* adapter,
                             device::BluetoothDevice* device) {
-  const std::string& address = device->GetAddress();
-  auto it = pending_connect_to_service_args_.begin();
-  while (it != pending_connect_to_service_args_.end()) {
-    if (address == std::get<0>(*it)) {
-      std::move(std::get<2>(*it)).Run(/*result=*/nullptr);
-      it = pending_connect_to_service_args_.erase(it);
-    } else {
-      ++it;
-    }
-  }
+  ProcessPendingInsecureServiceConnectionRequest(device->GetAddress(),
+                                                 /*device=*/nullptr);
 
   auto device_info = Device::ConstructDeviceInfoStruct(device);
   for (auto& observer : observers_)
@@ -315,20 +303,9 @@ void Adapter::GattServicesDiscovered(device::BluetoothAdapter* adapter,
   // resolved. Once service probing for a device within a cached request (in
   // |pending_connect_to_service_args_|) concludes, attempt socket creation
   // again via OnDeviceFetchedForInsecureServiceConnection().
-  if (!device->IsGattServicesDiscoveryComplete())
-    return;
-
-  const std::string& address = device->GetAddress();
-  auto it = pending_connect_to_service_args_.begin();
-  while (it != pending_connect_to_service_args_.end()) {
-    if (address == std::get<0>(*it)) {
-      OnDeviceFetchedForInsecureServiceConnection(
-          /*service_uuid=*/std::get<1>(*it),
-          /*callback=*/std::move(std::get<2>(*it)), device);
-      it = pending_connect_to_service_args_.erase(it);
-    } else {
-      ++it;
-    }
+  if (device->IsGattServicesDiscoveryComplete()) {
+    ProcessPendingInsecureServiceConnectionRequest(device->GetAddress(),
+                                                   device);
   }
 }
 
@@ -341,6 +318,11 @@ void Adapter::OnDeviceFetchedForInsecureServiceConnection(
     const device::BluetoothUUID& service_uuid,
     ConnectToServiceInsecurelyCallback callback,
     device::BluetoothDevice* device) {
+  if (!device) {
+    std::move(callback).Run(/*result=*/nullptr);
+    return;
+  }
+
   if (!device->IsPaired() && device->IsConnected() &&
       !device->IsGattServicesDiscoveryComplete()) {
     // This provided device is most likely a result of calling ConnectDevice():
@@ -361,6 +343,22 @@ void Adapter::OnDeviceFetchedForInsecureServiceConnection(
                      weak_ptr_factory_.GetWeakPtr(), copyable_callback),
       base::BindOnce(&Adapter::OnConnectToServiceError,
                      weak_ptr_factory_.GetWeakPtr(), copyable_callback));
+}
+
+void Adapter::ProcessPendingInsecureServiceConnectionRequest(
+    const std::string& address,
+    device::BluetoothDevice* device) {
+  auto it = pending_connect_to_service_args_.begin();
+  while (it != pending_connect_to_service_args_.end()) {
+    if (address == std::get<0>(*it)) {
+      OnDeviceFetchedForInsecureServiceConnection(
+          /*service_uuid=*/std::get<1>(*it),
+          /*callback=*/std::move(std::get<2>(*it)), device);
+      it = pending_connect_to_service_args_.erase(it);
+    } else {
+      ++it;
+    }
+  }
 }
 
 void Adapter::OnGattConnected(
