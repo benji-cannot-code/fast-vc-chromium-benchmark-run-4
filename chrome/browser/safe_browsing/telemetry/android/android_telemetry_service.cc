dca4ee31318bdaffea8c1599b4be62ade3094de6
Xinghui Lu <xinghuilu@chromium.org>
2021-02-09 08:25:10 +0000

    Log whether the mime type is apk on apk download reports.
    We have switched the apk check from mime type to file extension. We'd
    also like to know how many of these files actually set apk as their
    mime type.
    
    Metrics added:
    SafeBrowsing.AndroidTelemetry.ApkDownload.IsMimeTypeApk
    
    Bug: 1173145
    Change-Id: I83b3b8cd38ea291ebb98758f3d8a79738f609009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2680126
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852120}

--- chrome/browser/safe_browsing/telemetry/android/android_telemetry_service.cc
+++ chrome/browser/safe_browsing/telemetry/android/android_telemetry_service.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "chrome/browser/download/simple_download_manager_coordinator_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
@@ -39,6 +40,8 @@ namespace safe_browsing {
 namespace {
 // File suffix for APKs.
 const base::FilePath::CharType kApkSuffix[] = FILE_PATH_LITERAL(".apk");
+// MIME-type for APKs.
+const char kApkMimeType[] = "application/vnd.android.package-archive";
 
 // The number of user gestures to trace back for the referrer chain.
 const int kAndroidTelemetryUserGestureLimit = 2;
@@ -112,6 +115,9 @@ void AndroidTelemetryService::OnDownloadUpdated(download::DownloadItem* item) {
   DCHECK(item->GetTargetFilePath().MatchesExtension(kApkSuffix));
 
   if (item->GetState() == download::DownloadItem::COMPLETE) {
+    base::UmaHistogramBoolean(
+        "SafeBrowsing.AndroidTelemetry.ApkDownload.IsMimeTypeApk",
+        (item->GetMimeType() == kApkMimeType));
     // Download completed. Send report.
     std::unique_ptr<ClientSafeBrowsingReportRequest> report = GetReport(item);
     MaybeSendApkDownloadReport(
@@ -134,7 +140,6 @@ bool AndroidTelemetryService::CanSendPing(download::DownloadItem* item) {
     // how often people download non-APK files.
     return false;
   }
-  // TODO(crbug.com/1173145): Add a metric to check if the MIME type is APK.
 
   if (!IsSafeBrowsingEnabled(*GetPrefs())) {
     RecordApkDownloadTelemetryOutcome(
