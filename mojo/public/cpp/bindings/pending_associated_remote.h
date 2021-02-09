ed9841bb869490b9c6e56c87d18a5fe53db1d22a
Daniel Cheng <dcheng@chromium.org>
2021-02-09 19:46:58 +0000

    Implement test binding helpers for Associated{Receiver,Remote}
    These helpers are similar to the existing ones for binding associated
    receivers/remotes to a dedicated endpoint, but can be used with an
    already-created pending associated receiver/remote.
    
    Bug: 1173504
    Change-Id: I94c6e3fbc4263445133e036a556cfb025c2a9e18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2670090
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#852308}

--- mojo/public/cpp/bindings/pending_associated_remote.h
+++ mojo/public/cpp/bindings/pending_associated_remote.h
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "build/build_config.h"
 #include "mojo/public/cpp/bindings/associated_interface_ptr_info.h"
+#include "mojo/public/cpp/bindings/lib/multiplex_router.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "mojo/public/cpp/bindings/scoped_interface_endpoint_handle.h"
+#include "mojo/public/cpp/system/message_pipe.h"
 
 namespace mojo {
 
@@ -89,6 +91,31 @@ class PendingAssociatedRemote {
         &handle_, &receiver_handle);
     set_version(0);
     return PendingAssociatedReceiver<Interface>(std::move(receiver_handle));
+  }
+
+  // Associates this endpoint with a dedicated message pipe. This allows the
+  // entangled AssociatedReceiver/AssociatedRemote endpoints to be used without
+  // ever being associated with any other mojom interfaces.
+  //
+  // Needless to say, messages sent between the two entangled endpoints will not
+  // be ordered with respect to any other mojom interfaces. This is generally
+  // useful for ignoring calls on an associated remote or for binding associated
+  // endpoints in tests.
+  void EnableUnassociatedUsage() {
+    DCHECK(is_valid());
+
+    MessagePipe pipe;
+    scoped_refptr<internal::MultiplexRouter> router0 =
+        new internal::MultiplexRouter(
+            std::move(pipe.handle0), internal::MultiplexRouter::MULTI_INTERFACE,
+            false, base::SequencedTaskRunnerHandle::Get());
+    scoped_refptr<internal::MultiplexRouter> router1 =
+        new internal::MultiplexRouter(
+            std::move(pipe.handle1), internal::MultiplexRouter::MULTI_INTERFACE,
+            true, base::SequencedTaskRunnerHandle::Get());
+
+    InterfaceId id = router1->AssociateInterface(PassHandle());
+    set_handle(router0->CreateLocalEndpointHandle(id));
   }
 
  private:
