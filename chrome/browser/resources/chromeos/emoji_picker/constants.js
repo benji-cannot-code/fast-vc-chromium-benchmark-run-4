a16d840db7822ab22e55d484b4b87ffa81ddd4e2
Kenton Lam <kentonlam@google.com>
2021-02-09 23:34:22 +0000

    Change emoji picker to have 9 columns and tweak dialog size.
    Change-Id: Icf06e4bdf1e63027ef5388843cac4f5866b2c0dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2681465
    Commit-Queue: Kenton Lam <kentonlam@google.com>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852385}

--- chrome/browser/resources/chromeos/emoji_picker/constants.js
+++ chrome/browser/resources/chromeos/emoji_picker/constants.js
@@ -4,10 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // height and width should match the dialog size in EmojiPickerDialog.
-export const EMOJI_PICKER_HEIGHT = 343;
-export const EMOJI_PICKER_WIDTH = 280;
+export const EMOJI_PICKER_HEIGHT = 390;
+export const EMOJI_PICKER_WIDTH = 340;
 export const EMOJI_SIZE = 32;
-export const EMOJI_PER_ROW = 7;
+export const EMOJI_PER_ROW = 9;
 
 export const EMOJI_PICKER_HEIGHT_PX = `${EMOJI_PICKER_HEIGHT}px`;
 export const EMOJI_PICKER_WIDTH_PX = `${EMOJI_PICKER_WIDTH}px`;
