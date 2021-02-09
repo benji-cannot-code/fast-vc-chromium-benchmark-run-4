d2657522af26562261e741b488a0b3c871a75601
Yulun Wu <yulunwu@chromium.org>
2021-02-09 22:43:28 +0000

    Remove classic shelf.
    Classic shelf is no longer needed and we should use dark mode shelf
    before enabling the dark/light flag.
    
    Bug: 1146464
    Change-Id: Ie8abc2ff6442ab5d9196f92a59c7866886b23880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683088
    Reviewed-by: Min Chen <minch@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852377}

--- ash/shelf/back_button.cc
+++ ash/shelf/back_button.cc
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_focus_cycler.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
-#include "ash/style/default_color_constants.h"
-#include "ash/style/default_colors.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
 #include "base/metrics/user_metrics.h"
@@ -45,9 +43,9 @@ void BackButton::PaintButtonContents(gfx::Canvas* canvas) {
   // Use PaintButtonContents instead of SetImage so the icon gets drawn at
   // |GetCenterPoint| coordinates instead of always in the center.
   gfx::ImageSkia img = CreateVectorIcon(
-      kShelfBackIcon, DeprecatedGetContentLayerColor(
-                          AshColorProvider::ContentLayerType::kButtonIconColor,
-                          kShelfButtonColor));
+      kShelfBackIcon,
+      AshColorProvider::Get()->GetContentLayerColor(
+          AshColorProvider::ContentLayerType::kButtonIconColor));
   canvas->DrawImageInt(img, GetCenterPoint().x() - img.width() / 2,
                        GetCenterPoint().y() - img.height() / 2);
 }
