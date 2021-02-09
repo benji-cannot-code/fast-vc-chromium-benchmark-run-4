e0a7f0492dea6324751f587f2f22f0ea9da9ccac
Yoav Weiss <yoavweiss@chromium.org>
2021-02-09 01:31:50 +0000

    Correct comment on ResourceLoadingLog activation
    In order to get ResourceLoadingLog to output logs to the comment line, one needs the enable-logging
    CLI argument enabled, on top of the ones mentioned in the comment on ResourceLoadingLog.
    This CL fixes the comment in that file to reflect that.
    
    Change-Id: If3f70fd8591e4339db75747dbe5f146d690928a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679497
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852013}

--- third_party/blink/renderer/platform/loader/fetch/resource_loading_log.h
+++ third_party/blink/renderer/platform/loader/fetch/resource_loading_log.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if DCHECK_IS_ON()
 // We can see logs with |--v=N| or |--vmodule=ResourceLoadingLog=N| where N is a
-// verbose level.
+// verbose level, as well as the |--enable-logging=stderr| CLI argument.
 #define RESOURCE_LOADING_DVLOG(verbose_level) \
   LAZY_STREAM(                                \
       VLOG_STREAM(verbose_level),             \
