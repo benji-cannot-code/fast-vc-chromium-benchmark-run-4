07f2752ba3647034ca7a74a748d94c7d4979fecf
Aya ElAttar <ayaelattar@chromium.org>
2021-02-09 20:43:33 +0000

    DLP: Split clipboard and DnD notifiers
    - Moved ClipboardBubble out to a separate .h/.cc
    - Split the notification helper to:
      -- DataTransferDlpNotifier as a base class
      -- DlpClipboardNotifier for clipboard notifications
      -- DlpDndNotifier for Drag-n-Drop notifications
    
    Bug: 1175682
    Change-Id: I6c7aa822b40ec2f642ec588dafbedcd103e4a5d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679384
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852341}

--- chrome/browser/chromeos/policy/dlp/data_transfer_dlp_controller.h
+++ chrome/browser/chromeos/policy/dlp/data_transfer_dlp_controller.h
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_POLICY_DLP_DATA_TRANSFER_DLP_CONTROLLER_H_
 
 #include "base/strings/string16.h"
-#include "chrome/browser/chromeos/policy/dlp/dlp_clipboard_notification_helper.h"
+#include "chrome/browser/chromeos/policy/dlp/dlp_clipboard_notifier.h"
+#include "chrome/browser/chromeos/policy/dlp/dlp_drag_drop_notifier.h"
 #include "ui/base/data_transfer_policy/data_transfer_policy_controller.h"
 
 namespace ui {
@@ -56,8 +57,13 @@ class DataTransferDlpController : public ui::DataTransferPolicyController {
   virtual bool ShouldProceedOnWarn(
       const ui::DataTransferEndpoint* const data_dst);
 
+  virtual void NotifyBlockedDrop(
+      const ui::DataTransferEndpoint* const data_src,
+      const ui::DataTransferEndpoint* const data_dst);
+
   const DlpRulesManager& dlp_rules_manager_;
-  DlpClipboardNotificationHelper helper_;
+  DlpClipboardNotifier clipboard_notifier_;
+  DlpDragDropNotifier drag_drop_notifier_;
 };
 
 }  // namespace policy
