4845eb1279cb16e7a55700b523501201f8647ddf
paulzhchen <paulzchen@google.com>
2021-02-09 05:43:05 +0000

    [Eche SWA] Adding flags for Eche SWA.
    Adding the Eche feature flag to the chrome://flags UI.
    
    AboutFlagsTest.EveryFlagHasMetadata.
    
    Bug: b/179441407
    Test: Pass AboutFlagsHistogramTest.CheckHistograms,
    Change-Id: I5544bcf53431420b719e54e464e1fa926d4bcb4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675553
    Commit-Queue: Paulz Chen <paulzchen@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852093}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2488,6 +2488,9 @@ extern const char kAudioPlayerJsModulesDescription[];
 extern const char kVideoPlayerJsModulesName[];
 extern const char kVideoPlayerJsModulesDescription[];
 
+extern const char kEcheSWAName[];
+extern const char kEcheSWADescription[];
+
 extern const char kEnableSuggestedFilesName[];
 extern const char kEnableSuggestedFilesDescription[];
 
