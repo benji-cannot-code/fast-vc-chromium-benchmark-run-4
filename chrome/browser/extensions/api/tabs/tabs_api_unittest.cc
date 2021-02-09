a74bd7d8e8609194451313b2face3d871a52d1bd
David Bienvenu <davidbienvenu@chromium.org>
2021-02-09 04:59:49 +0000

    tests: Add ScopedDlpContentManagerForTesting
    Some tests were calling SetDlpContentManagerForTesting without calling
    ResetDlpContentManagerForTesting. This could lead to UAF and crashes,
    in tests.
    
    Bug: 956719
    Change-Id: I851e4b7c36ca6d0d9147fa03964fca0eeba53bd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679227
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852083}

--- chrome/browser/extensions/api/tabs/tabs_api_unittest.cc
+++ chrome/browser/extensions/api/tabs/tabs_api_unittest.cc
@@ -1075,7 +1075,7 @@ TEST_F(TabsApiUnitTest, ScreenshotsRestricted) {
 
   // Setup Data Leak Prevention restriction.
   policy::MockDlpContentManager mock_dlp_content_manager;
-  policy::DlpContentManager::SetDlpContentManagerForTesting(
+  policy::ScopedDlpContentManagerForTesting scoped_dlp_content_manager_(
       &mock_dlp_content_manager);
   EXPECT_CALL(mock_dlp_content_manager, IsScreenshotRestricted(testing::_))
       .Times(1)
@@ -1088,7 +1088,6 @@ TEST_F(TabsApiUnitTest, ScreenshotsRestricted) {
 
   // Clean up.
   browser()->tab_strip_model()->CloseAllTabs();
-  policy::DlpContentManager::ResetDlpContentManagerForTesting();
 }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
--- chrome/browser/media/webrtc/desktop_capture_access_handler_unittest.cc
+++ chrome/browser/media/webrtc/desktop_capture_access_handler_unittest.cc
@@ -376,7 +376,7 @@ TEST_F(DesktopCaptureAccessHandlerTest, ScreenCaptureAccessSuccess) {
 TEST_F(DesktopCaptureAccessHandlerTest, ScreenCaptureAccessDlpRestricted) {
   // Setup Data Leak Prevention restriction.
   policy::MockDlpContentManager mock_dlp_content_manager;
-  policy::DlpContentManager::SetDlpContentManagerForTesting(
+  policy::ScopedDlpContentManagerForTesting scoped_dlp_content_manager_(
       &mock_dlp_content_manager);
   EXPECT_CALL(mock_dlp_content_manager, IsScreenCaptureRestricted(testing::_))
       .Times(1)
@@ -405,13 +405,12 @@ TEST_F(DesktopCaptureAccessHandlerTest, ScreenCaptureAccessDlpRestricted) {
 
   EXPECT_EQ(blink::mojom::MediaStreamRequestResult::PERMISSION_DENIED, result);
   EXPECT_EQ(0u, devices.size());
-  policy::DlpContentManager::ResetDlpContentManagerForTesting();
 }
 
 TEST_F(DesktopCaptureAccessHandlerTest, GenerateStreamDlpRestricted) {
   // Setup Data Leak Prevention restriction.
   policy::MockDlpContentManager mock_dlp_content_manager;
-  policy::DlpContentManager::SetDlpContentManagerForTesting(
+  policy::ScopedDlpContentManagerForTesting scoped_dlp_content_manager_(
       &mock_dlp_content_manager);
   EXPECT_CALL(mock_dlp_content_manager, IsScreenCaptureRestricted(testing::_))
       .Times(1)
@@ -435,6 +434,5 @@ TEST_F(DesktopCaptureAccessHandlerTest, GenerateStreamDlpRestricted) {
 
   EXPECT_EQ(blink::mojom::MediaStreamRequestResult::PERMISSION_DENIED, result);
   EXPECT_EQ(0u, devices.size());
-  policy::DlpContentManager::ResetDlpContentManagerForTesting();
 }
 #endif
