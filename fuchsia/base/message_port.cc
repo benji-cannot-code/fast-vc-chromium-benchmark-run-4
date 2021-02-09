34197415ed002358e1c984f9a09ae26fafcaf938
Kevin Marshall <kmarshall@chromium.org>
2021-02-09 01:40:06 +0000

    Revert "[fuchsia] Make message_port.h into a private library for WebEngine."
    This reverts commit 6e7efcd9f2287143d71f1bf0d625803226bf8bdc.
    
    Reason for revert: Breaks dependent code in another repository.
    
    Original change's description:
    > [fuchsia] Make message_port.h into a private library for WebEngine.
    >
    > The MessagePort conversion functions are no longer used outside of
    > WebEngine, now that previous clients have migrated to
    > use //components/cast/message_port instead.
    >
    > Remove vestigial dep on now-removed 'message_port' target.
    >
    > Bug: 1081525
    > Change-Id: Ie3629ff1542b96bfe9594cc5dcb1ef15c671c9c7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643398
    > Auto-Submit: Kevin Marshall <kmarshall@chromium.org>
    > Reviewed-by: Sharon Yang <yangsharon@chromium.org>
    > Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#845821}
    
    TBR=kmarshall@chromium.org,yangsharon@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1081525
    Change-Id: I86ced1db00286f4c40ab9d9338d8c48ffc26b2b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683400
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Sharon Yang <yangsharon@chromium.org>
    Auto-Submit: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852018}

--- fuchsia/base/message_port.cc
+++ fuchsia/base/message_port.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "fuchsia/engine/browser/message_port.h"
+#include "fuchsia/base/message_port.h"
 
 #include <stdint.h>
 
