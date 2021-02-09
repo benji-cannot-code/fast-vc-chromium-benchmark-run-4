c51b9b6f82967a3d976fd0ca5435a1959de7742e
Asami Doi <asamidoi@chromium.org>
2021-02-09 07:47:07 +0000

    Revert "[ios] Use a fixed session identifier if multiple scenes is not supported"
    This reverts commit 92b4838cda71a9a0ec6199dd8d436018e764721f.
    
    Reason for revert: SmokeTestCase, SyncFakeServerTestCase, SafeBrowsingTestCase, and etc. tests fail. This is a speculative revert but the CL is only one that updates iOS code in regression range. I'm sorry if I'm wrong and please reland it if the code doesn't have any problems.
    
    Build log: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-simulator-noncq/15628/overview
    
    Original change's description:
    > [ios] Use a fixed session identifier if multiple scenes is not supported
    >
    > For devices that do not support multiple scenes (devices running version
    > of iOS older than 13.0, phones or old iPads) use a constant identifier
    > instead of `nil` or `-persistentIdentifier` of UISceneSession (when iOS
    > version is 13.0).
    >
    > The reason for using a fixed identifier is that if the device runs on
    > iOS 13.0 or higher, the swipe gesture is sometimes identified as either
    > a gesture to kill the app or to close the current window. If the former
    > happen, on next startup the value of `-persistentIdentifier` will be
    > different, leading to loss of the session (tabs) and snapshots.
    >
    > To fix this there are two possible strategy, of either having separate
    > code path for devices that support multiple scenes and for the others,
    > or to have a single code path but with a fixed identifier for devices
    > that do not support multiple windows.
    >
    > The latter option (fixed identifier) is simpler as it can be limited to
    > a single location (`-sceneSessionID` in SceneState) if we ensure that
    > the value can never be the one returned by `-persistentIdentifier`. As
    > this value is an UUID, it is relatively easy to select a constant that
    > will not conflict (selected constant is {SyntheticIdentifier}).
    >
    > To avoid losing data when the user update Chrome, some migration code
    > is also present to move the session (tabs) and snapshots from the old
    > location to the new one if needed. Determining the previous location
    > for those files can be a bit tricky as the location was changed when
    > multi-window support was added (and enabled in M87).
    >
    > Bug: 1165798, 1117317, 1138219
    > Change-Id: I6bbd66651ff9beb5c99138afb57225501a922950
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2644813
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    > Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#851707}
    
    TBR=sdefresne@chromium.org,holte@chromium.org,stkhapugin@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: Ie16826e078daca3133538ec95c620dc56f121991
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1165798
    Bug: 1117317
    Bug: 1138219
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683445
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852112}

--- ios/chrome/browser/snapshots/snapshot_browser_agent.h
+++ ios/chrome/browser/snapshots/snapshot_browser_agent.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/main/browser_user_data.h"
 #import "ios/chrome/browser/web_state_list/web_state_list_observer.h"
 
+namespace base {
+class FilePath;
+}
 @class SnapshotCache;
 
 // Associates a SnapshotCache to a Browser.
@@ -23,13 +26,10 @@ class SnapshotBrowserAgent : BrowserObserver,
   SnapshotBrowserAgent();
   ~SnapshotBrowserAgent() override;
 
-  SnapshotBrowserAgent(const SnapshotBrowserAgent&) = delete;
-  SnapshotBrowserAgent& operator=(const SnapshotBrowserAgent&) = delete;
-
   // Set a session identification string that will be used to locate the
   // snapshots directory. Setting this more than once on the same agent is
   // probably a programming error.
-  void SetSessionID(NSString* session_identifier);
+  void SetSessionID(const std::string& session_identifier);
 
   // Maintains the snapshots storage including purging unused images and
   // performing any necessary migrations.
@@ -70,12 +70,17 @@ class SnapshotBrowserAgent : BrowserObserver,
   // Purges the snapshots folder of unused snapshots.
   void PurgeUnusedSnapshots();
 
+  // Returns the storage folder for snapshots.
+  base::FilePath GetStoragePath();
+
   // Returns the Tab IDs of all the WebStates in the Browser.
   NSSet<NSString*>* GetTabIDs();
 
-  __strong SnapshotCache* snapshot_cache_;
+  Browser* browser_;               // unowned.
+  SnapshotCache* snapshot_cache_;  // strong, owned.
 
-  Browser* browser_ = nullptr;
+  // Session identifier for this agent.
+  std::string session_identifier_;
 };
 
 #endif  // IOS_CHROME_BROWSER_SNAPSHOTS_SNAPSHOT_BROWSER_AGENT_H_
--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator_unittest.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator_unittest.mm
@@ -70,7 +70,7 @@ namespace {
 void AddAgentsToBrowser(Browser* browser, SceneState* scene_state) {
   SnapshotBrowserAgent::CreateForBrowser(browser);
   SnapshotBrowserAgent::FromBrowser(browser)->SetSessionID(
-      [[NSUUID UUID] UUIDString]);
+      base::SysNSStringToUTF8([[NSUUID UUID] UUIDString]));
   SceneStateBrowserAgent::CreateForBrowser(browser, scene_state);
 }
 
