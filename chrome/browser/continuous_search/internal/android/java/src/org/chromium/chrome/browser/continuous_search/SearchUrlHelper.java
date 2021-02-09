df94893cf27984e2e972b8f639d4e80d64b3d378
Yashar Dabiran <yashard@chromium.org>
2021-02-09 17:28:01 +0000

    [CSN] Change session detection behaviour for BackNavigationTabObserver
    There is a chance URL parameters change as user navigates back to SRP. This CL changes BackNavigationTabObserver's behaviour to only check the URL query parameter instead of the whole URL to detect back navigation entries.
    
    Change-Id: I03d063ee47991cf87b8b648442fafe9d73451cd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679802
    Commit-Queue: Yashar Dabiran <yashard@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852233}

--- chrome/browser/continuous_search/internal/android/java/src/org/chromium/chrome/browser/continuous_search/SearchUrlHelper.java
+++ chrome/browser/continuous_search/internal/android/java/src/org/chromium/chrome/browser/continuous_search/SearchUrlHelper.java
@@ -28,15 +28,6 @@ public class SearchUrlHelper {
     }
 
     /**
-     * Checks whether the provided url represents a valid Google search url.
-     * @param url the url to check.
-     * @return true if the url satisfies the criteria.
-     */
-    public static boolean isSrpUrl(GURL url) {
-        return SearchUrlHelperJni.get().isSrpUrl(url);
-    }
-
-    /**
      * Gets the query of the provided url if it is a SRP URL and shows the "All" or "News" tab
      * results.
      * @param url The url to try to extract the query from.
@@ -49,7 +40,6 @@ public class SearchUrlHelper {
     @NativeMethods
     interface Natives {
         boolean isGoogleDomainUrl(GURL url);
-        boolean isSrpUrl(GURL url);
         String getQueryIfValidSrpUrl(GURL url);
     }
 }
