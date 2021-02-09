0f61bc8a0ab3933f1296d5af60ef5dcf5f9a7bf4
Gauthier Ambard <gambard@chromium.org>
2021-02-09 18:18:05 +0000

    [iOS]🏢 Open new window in the forced mode
    If the incognito mode is forced on or off by a policy, this policy
    should be followed when opening a new window.
    This CL makes sure that the newly opened window is opening in the
    correct mode.
    
    It also updates the bookmark context menu code.
    
    Fixed: 1175063
    Change-Id: I3e5919d1c45ff6b0912d501aa5b18f461edc26f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675914
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852257}

--- ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
@@ -1789,6 +1789,8 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
                            BookmarkHomeViewController* strongSelf = weakSelf;
                            if (!strongSelf)
                              return;
+                           if ([strongSelf isIncognitoForced])
+                             return;
                            auto nodes = [strongSelf editNodes];
                            [strongSelf openAllURLs:GetUrlsToOpen(nodes)
                                        inIncognito:NO
@@ -1802,6 +1804,8 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
                          action:^{
                            BookmarkHomeViewController* strongSelf = weakSelf;
                            if (!strongSelf)
+                             return;
+                           if (![strongSelf isIncognitoAvailable])
                              return;
                            auto nodes = [strongSelf editNodes];
                            [strongSelf openAllURLs:GetUrlsToOpen(nodes)
@@ -1865,6 +1869,8 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
   titleString = GetNSString(IDS_IOS_CONTENT_CONTEXT_OPENLINKNEWTAB);
   [coordinator addItemWithTitle:titleString
                          action:^{
+                           if ([weakSelf isIncognitoForced])
+                             return;
                            [weakSelf openAllURLs:{nodeURL}
                                      inIncognito:NO
                                           newTab:YES];
@@ -1883,13 +1889,14 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
     };
     [coordinator addItemWithTitle:titleString
                            action:action
-                            style:UIAlertActionStyleDefault
-                          enabled:![self isIncognitoForced]];
+                            style:UIAlertActionStyleDefault];
   }
 
   titleString = GetNSString(IDS_IOS_CONTENT_CONTEXT_OPENLINKNEWINCOGNITOTAB);
   [coordinator addItemWithTitle:titleString
                          action:^{
+                           if (![weakSelf isIncognitoAvailable])
+                             return;
                            [weakSelf openAllURLs:{nodeURL}
                                      inIncognito:YES
                                           newTab:YES];
@@ -2356,6 +2363,8 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
           [[NSMutableArray alloc] init];
 
       UIAction* openAction = [actionFactory actionToOpenInNewTabWithBlock:^{
+        if ([weakSelf isIncognitoForced])
+          return;
         [weakSelf openAllURLs:{nodeURL} inIncognito:NO newTab:YES];
       }];
       if ([self isIncognitoForced]) {
@@ -2365,6 +2374,8 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
 
       UIAction* openInIncognito =
           [actionFactory actionToOpenInNewIncognitoTabWithBlock:^{
+            if (![weakSelf isIncognitoAvailable])
+              return;
             [weakSelf openAllURLs:{nodeURL} inIncognito:YES newTab:YES];
           }];
       if (![self isIncognitoAvailable]) {
@@ -2373,13 +2384,11 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
       [menuElements addObject:openInIncognito];
 
       if (base::ios::IsMultipleScenesSupported()) {
-        UIAction* openInWindow = [actionFactory
-            actionToOpenInNewWindowWithURL:nodeURL
-                            activityOrigin:WindowActivityBookmarksOrigin];
-        if ([self isIncognitoForced]) {
-          openInWindow.attributes = UIMenuElementAttributesDisabled;
-        }
-        [menuElements addObject:openInWindow];
+        [menuElements
+            addObject:[actionFactory
+                          actionToOpenInNewWindowWithURL:nodeURL
+                                          activityOrigin:
+                                              WindowActivityBookmarksOrigin]];
       }
 
       [menuElements addObject:[actionFactory actionToCopyURL:nodeURL]];
