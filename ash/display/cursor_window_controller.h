ca76ef8186b7f777c7de56e4eb490f9faf18d42f
Ahmed Fakhry <afakhry@chromium.org>
2021-02-09 18:33:17 +0000

    capture_mode: Implement an efficient way of recording the cursor
    So far, the cursor was captured by enabling the
    software-composited cursor while recording is in
    progress. This works for both fullscreen, and
    partial region captures, but doesn't work when
    recording a window (since the cursor widget is
    not in the recorded window layer subtree).
    
    Moreover, the software cursor has other
    disadvantages: It's very slow, and moving it
    causes damage resulting in more submitted
    compositor frames. The FrameSinkVideoCapturer
    has a maximum number of in-flight video frames,
    so moving the cursor too much will cause hitting
    that limit, and result in a janky video.
    
    This CL changes the mechanism of recording the
    cursor to a more efficient way using the
    FrameSinkVideoCaptureOverlay, which can be used
    to blit the cursor bitmap on top of the video
    frames when they get generated.
    
    BUG=1165700
    TEST=Manually, Added new tests at ash_unittests --gtest_filter=*CursorOverlay*
    
    Change-Id: I700b7934f5c43ff8dac3ebdf73296f29017102f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2670711
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852262}

--- ash/display/cursor_window_controller.h
+++ ash/display/cursor_window_controller.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/public/cpp/ash_constants.h"
 #include "base/macros.h"
+#include "base/observer_list.h"
+#include "base/observer_list_types.h"
 #include "ui/aura/window.h"
 #include "ui/base/cursor/cursor.h"
 #include "ui/base/cursor/cursor_size.h"
@@ -29,12 +31,23 @@ class CursorWindowDelegate;
 // When cursor compositing is enabled, just draw the cursor as-is.
 class ASH_EXPORT CursorWindowController {
  public:
+  class Observer : public base::CheckedObserver {
+   public:
+    virtual void OnCursorCompositingStateChanged(bool enabled) = 0;
+
+   protected:
+    ~Observer() override = default;
+  };
+
   CursorWindowController();
   ~CursorWindowController();
 
   bool is_cursor_compositing_enabled() const {
     return is_cursor_compositing_enabled_;
   }
+
+  void AddObserver(Observer* observer);
+  void RemoveObserver(Observer* observer);
 
   void SetLargeCursorSizeInDip(int large_cursor_size_in_dip);
   void SetCursorColor(SkColor cursor_color);
@@ -85,6 +98,8 @@ class ASH_EXPORT CursorWindowController {
   SkBitmap GetAdjustedBitmap(const gfx::ImageSkiaRep& image_rep) const;
 
   const gfx::ImageSkia& GetCursorImageForTest() const;
+
+  base::ObserverList<Observer> observers_;
 
   aura::Window* container_ = nullptr;
 
