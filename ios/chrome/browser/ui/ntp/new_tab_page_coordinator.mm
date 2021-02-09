e8f05e1678ae04f788aa7f45ffab610f49c21d12
Robbie Gibson <rkgibson@google.com>
2021-02-09 14:50:05 +0000

    [iOS][Thumb Strip] Fix NTP feed feature targeting condition
    This CL is a follow up to crrev.com/c/2678941. In that CL, one of the
    comments pointed out that the targeting condition for a feature was
    incorrect. Instead of testing for refactored NTP, the check should be
    for refactored NTP and feed enabled. This fixes that.
    
    Bug: 1171362
    Change-Id: I5bdd35a574dd8629c50d9c52d969734a9cac9e14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2684409
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852186}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -312,7 +312,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stopScrolling {
-  if (IsRefactoredNTP()) {
+  if ([self isNTPRefactoredAndFeedVisible]) {
     [self.ntpViewController stopScrolling];
   } else {
     [self.contentSuggestionsCoordinator stopScrolling];
