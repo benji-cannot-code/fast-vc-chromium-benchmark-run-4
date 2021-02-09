bf820b4cdce755e7c5e056c300cc6cdb33235bce
Jeroen Dhollander <jeroendh@google.com>
2021-02-09 18:45:37 +0000

    Use StructTraits for AndroidAppInfo
    This eliminates us having to convert to and from the mojom equivalent
    type.
    
    Bug: b/179729019
    Test: chromeos_unittests --gtest_filter="Assistant*"
    Change-Id: Icdfbfa9f6dd753e0d237652c6e319082b9a8e3c8
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683131
    Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852267}

--- chromeos/services/libassistant/display_controller.cc
+++ chromeos/services/libassistant/display_controller.cc
@@ -15,29 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace libassistant {
 
-namespace {
-
-assistant::AndroidAppInfo ToAndroidAppInfo(
-    const mojom::AndroidAppInfoPtr& app) {
-  assistant::AndroidAppInfo result;
-  result.package_name = app->package_name;
-  result.version = app->version;
-  result.localized_app_name = app->localized_app_name;
-  return result;
-}
-
-std::vector<assistant::AndroidAppInfo> ToAndroidAppInfoList(
-    const std::vector<mojom::AndroidAppInfoPtr>& apps) {
-  std::vector<assistant::AndroidAppInfo> result;
-
-  for (const auto& app : apps)
-    result.push_back(ToAndroidAppInfo(app));
-
-  return result;
-}
-
-}  // namespace
-
 class DisplayController::EventObserver : public DisplayConnectionObserver {
  public:
   explicit EventObserver(DisplayController* parent) : parent_(parent) {}
@@ -86,8 +63,8 @@ void DisplayController::SetRelatedInfoEnabled(bool enabled) {
 }
 
 void DisplayController::SetAndroidAppList(
-    std::vector<mojom::AndroidAppInfoPtr> apps) {
-  display_connection_->OnAndroidAppListRefreshed(ToAndroidAppInfoList(apps));
+    const std::vector<::chromeos::assistant::AndroidAppInfo>& apps) {
+  display_connection_->OnAndroidAppListRefreshed(apps);
 }
 
 void DisplayController::OnAssistantManagerCreated(
