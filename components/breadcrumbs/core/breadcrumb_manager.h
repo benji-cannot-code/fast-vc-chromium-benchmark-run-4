80158fc81c19a46b60b28715a7c96cc313100022
Jesse McKenna <jessemckenna@google.com>
2021-02-09 17:39:19 +0000

    Cleanup in BreadcrumbManager
    This change makes the following cleanup changes to BreadcrumbManager:
    * make deleted copy constructor and copy assignment operator public
    * remove unused #includes
    * add missing #include <utility> (for std::pair)
    * reorder class methods per style guide ("constructors and assignment
      operators, destructor, all other methods")
    
    Change-Id: I43f8f120d43520680be324e5dc15d4f6fd0669ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679120
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/master@{#852239}

--- components/breadcrumbs/core/breadcrumb_manager.h
+++ components/breadcrumbs/core/breadcrumb_manager.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_BREADCRUMBS_CORE_BREADCRUMB_MANAGER_H_
 
 #include <list>
-#include <memory>
 #include <string>
+#include <utility>
 
 #include "base/observer_list.h"
 #include "base/time/time.h"
@@ -24,6 +24,11 @@ class BreadcrumbManagerObserver;
 // stale data.
 class BreadcrumbManager {
  public:
+  BreadcrumbManager();
+  BreadcrumbManager(const BreadcrumbManager&) = delete;
+  BreadcrumbManager& operator=(const BreadcrumbManager&) = delete;
+  ~BreadcrumbManager();
+
   // Returns the number of collected breadcrumb events which are still relevant.
   // Note: This method may drop old events so the value can change even when no
   // new events have been added, but time has passed.
@@ -46,13 +51,7 @@ class BreadcrumbManager {
   void AddObserver(BreadcrumbManagerObserver* observer);
   void RemoveObserver(BreadcrumbManagerObserver* observer);
 
-  BreadcrumbManager();
-  ~BreadcrumbManager();
-
  private:
-  BreadcrumbManager(const BreadcrumbManager&) = delete;
-  BreadcrumbManager& operator=(const BreadcrumbManager&) = delete;
-
   // Drops events which are considered stale. Note that stale events are not
   // guaranteed to be removed. Explicitly, stale events will be retained while
   // newer events are limited.
