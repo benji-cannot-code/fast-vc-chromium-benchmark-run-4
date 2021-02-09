3a457ca950c127a6fa141f5d3e43457a2ccc8dfa
Qasid Sadiq <qasid@google.com>
2021-02-09 00:18:18 +0000

    ambient: Fix flaky line clamping tests.
    The line height for UI is inherited from os_settings_ui and declared in
    cr-page-host-style at 20. These tests run outside of that context, so
    without a line height specified, it defaults to 24, and can vary based
    on the text content.
    
    So, to make sure we are testing the right clamping, read line clamp from
    computed style.
    Also, fix/explicitly specify the line-height. We need to specify this
    here, because album-item needs to have an explicit height (see iron-list
    grid). If the line-height changes, it may no longer fit correctly in
    this element. There are alternatives to this, but inherited styles
    affecting size don't work with statically sized items like iron-list
    grid items. So, isolating this conflict and making it explicit is the
    way to go.
    
    My favorite alternative was to call getComputedStyle(item).lineHeight.
    And use that to calculate the the item height. The problem is,
    that is not available, until attached is called. And the iron-list
    containing this renders the whole grid on attached. Polymer "cannot
    guarantee that local DOM children have their attached callback called
    before their parent's attached callback." And iron-list doesn't have a
    public API to trigger a redraw. So this can't work.
    
    Test: Tested by hand on atlas and for i in {1..100};
    do testing/xvfb.py ./out/Default/browser_tests
    --gtest_filter="*OSSettingsAmbientModePhotosPageTest*"; done
    
    gtest_filter="*OSSettingsAmbientModePhotosPageTest*"
    Bug: 1173526
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I6c70f86e2299fbeefb22c32e84ae6e1f65724030
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673970
    Commit-Queue: Qasid Sadiq <qasid@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Auto-Submit: Qasid Sadiq <qasid@google.com>
    Cr-Commit-Position: refs/heads/master@{#851970}

--- chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_browsertest.js
@@ -370,8 +370,7 @@ var OSSettingsAmbientModePhotosPageTest = class extends OSSettingsBrowserTest {
   }
 };
 
-// TODO(https://crbug.com/1173526): Reenable flaky test.
-TEST_F('OSSettingsAmbientModePhotosPageTest', 'DISABLED_AllJsTests', () => {
+TEST_F('OSSettingsAmbientModePhotosPageTest', 'MAYBE_AllJsTests', () => {
   mocha.run();
 });
 
