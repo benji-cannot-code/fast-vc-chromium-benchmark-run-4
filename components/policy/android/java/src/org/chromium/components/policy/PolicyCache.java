28259fde2742765582082c644ab4bd67c40e8643
Owen Min <zmin@chromium.org>
2021-02-09 15:01:13 +0000

    Create PolicyCacheUpdater JNI interface
    The interface listens to the PolicyService update event, filters out
    policies with error, un-released or ignored and passes it to Java library
    to cache. Only necessary policies will be cached, BrowserSignin is the
    first one and many will follow.
    
    Also update PolicyMap and PolicyErrorMap so that we can check policy
    error without ResourceBundle ready.
    
    Bug: 1127469
    Change-Id: Ifc3fd939f5639ff6bcd920618ae17eda0f6f3328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675586
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852188}

--- components/policy/android/java/src/org/chromium/components/policy/PolicyCache.java
+++ components/policy/android/java/src/org/chromium/components/policy/PolicyCache.java
@@ -223,4 +223,9 @@ public class PolicyCache {
     static void resetForTesting() {
         sPolicyCache = null;
     }
+
+    @VisibleForTesting
+    public void setReadableForTesting(boolean readable) {
+        mReadable = readable;
+    }
 }
