d7adc3a9e2cbb9df85dd1eeb9cf0c3632fd08c15
Robbie McElrath <rmcelrath@chromium.org>
2021-02-09 22:37:50 +0000

    [WebLayer] Limit DarkModeTest to M90+
    This limits DarkModeTest to M90+, which is the version the feature
    landed in.
    
    Bug: 1176303
    Change-Id: Ie7d874b8cad2ecfbb0ca4f9e3a0511279383cd64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2685604
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852372}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/DarkModeTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/DarkModeTest.java
@@ -45,6 +45,7 @@ public class DarkModeTest {
 
     @Test
     @SmallTest
+    @MinWebLayerVersion(90)
     public void testDarkModeWithWebThemeDarkening() throws Exception {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         mActivity = mActivityTestRule.launchShell(new Bundle());
@@ -55,6 +56,7 @@ public class DarkModeTest {
 
     @Test
     @SmallTest
+    @MinWebLayerVersion(90)
     public void testDarkModeWithUserAgentDarkening() throws Exception {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         mActivity = mActivityTestRule.launchShell(new Bundle());
@@ -65,6 +67,7 @@ public class DarkModeTest {
 
     @Test
     @SmallTest
+    @MinWebLayerVersion(90)
     public void testDarkModeWithPreferWebThemeDarkening() throws Exception {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         mActivity = mActivityTestRule.launchShell(new Bundle());
@@ -75,6 +78,7 @@ public class DarkModeTest {
 
     @Test
     @SmallTest
+    @MinWebLayerVersion(90)
     public void testLightModeWithWebThemeDarkening() throws Exception {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         mActivity = mActivityTestRule.launchShell(new Bundle());
@@ -85,6 +89,7 @@ public class DarkModeTest {
 
     @Test
     @SmallTest
+    @MinWebLayerVersion(90)
     public void testLightModeWithUserAgentDarkening() throws Exception {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         mActivity = mActivityTestRule.launchShell(new Bundle());
@@ -95,6 +100,7 @@ public class DarkModeTest {
 
     @Test
     @SmallTest
+    @MinWebLayerVersion(90)
     public void testLightModeWithPreferWebThemeDarkening() throws Exception {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         mActivity = mActivityTestRule.launchShell(new Bundle());
