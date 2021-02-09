a9d01741b1bd765439992503d6fccfb07b25f82d
Eric Seckler <eseckler@chromium.org>
2021-02-09 20:58:52 +0000

    PowerMode: Add loading voter to RenderFrameImpl
    Instruments FrameSchedulerImpl with a voter for the loading PowerMode in
    the renderer process.
    
    Internal design doc: http://go/chromium-process-power-scheduler
    (sorry, no public docs yet.)
    
    Bug: 1166695
    Change-Id: I62bc8d52700c9f7ba9e8c7cb29d3f7a9b6d27b76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673039
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Auto-Submit: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852345}

--- third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.cc
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequence_manager/lazy_now.h"
 #include "base/time/time.h"
 #include "base/trace_event/blame_context.h"
+#include "components/power_scheduler/power_mode.h"
+#include "components/power_scheduler/power_mode_arbiter.h"
+#include "components/power_scheduler/power_mode_voter.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/scheduler/web_scheduler_tracked_feature.h"
 #include "third_party/blink/public/platform/blame_context.h"
@@ -191,7 +194,10 @@ FrameSchedulerImpl::FrameSchedulerImpl(
       waiting_for_meaningful_paint_(true,
                                     "FrameScheduler.WaitingForMeaningfulPaint",
                                     &tracing_controller_,
-                                    YesNoStateToString) {
+                                    YesNoStateToString),
+      loading_power_mode_voter_(
+          power_scheduler::PowerModeArbiter::GetInstance()->NewVoter(
+              "PowerModeVoter.Loading")) {
   frame_task_queue_controller_.reset(
       new FrameTaskQueueController(main_thread_scheduler_, this, this));
 }
@@ -612,9 +618,14 @@ void FrameSchedulerImpl::DidCommitProvisionalLoad(
   bool is_same_document = navigation_type == NavigationType::kSameDocument;
 
   if (!is_same_document) {
+    loading_power_mode_voter_->VoteFor(power_scheduler::PowerMode::kLoading);
+    loading_power_mode_voter_->ResetVoteAfterTimeout(
+        power_scheduler::PowerModeVoter::kLoadingTimeout);
+
     waiting_for_contentful_paint_ = true;
     waiting_for_meaningful_paint_ = true;
   }
+
   if (is_main_frame && !is_same_document) {
     task_time_ = base::TimeDelta();
     // Ignore result here, based on the assumption that
@@ -942,6 +953,8 @@ void FrameSchedulerImpl::OnLoad() {
     // update.
     main_thread_scheduler_->OnMainFrameLoad(*this);
   }
+
+  loading_power_mode_voter_->VoteFor(power_scheduler::PowerMode::kIdle);
 }
 
 bool FrameSchedulerImpl::IsWaitingForContentfulPaint() const {
--- third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.h
+++ third_party/blink/renderer/platform/scheduler/main_thread/frame_scheduler_impl.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/task/sequence_manager/task_queue.h"
 #include "base/trace_event/trace_event.h"
+#include "components/power_scheduler/power_mode_voter.h"
 #include "net/base/request_priority.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "third_party/blink/public/platform/task_type.h"
@@ -372,6 +373,8 @@ class PLATFORM_EXPORT FrameSchedulerImpl : public FrameScheduler,
       waiting_for_contentful_paint_;
   TraceableState<bool, TracingCategoryName::kInfo>
       waiting_for_meaningful_paint_;
+
+  std::unique_ptr<power_scheduler::PowerModeVoter> loading_power_mode_voter_;
 
   // TODO(altimin): Remove after we have have 1:1 relationship between frames
   // and documents.
