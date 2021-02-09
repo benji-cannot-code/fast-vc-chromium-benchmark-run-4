2d33369579096eed1752709922bb9c1bbac9d84d
Kelvin Jiang <kelvinjiang@chromium.org>
2021-02-09 00:04:14 +0000

    Revert "[CSN] Refactor UI source directory"
    This reverts commit e02868afd69363fe673fac6cf74370b856991a7b.
    
    Reason for revert: Broke the tree with a compile failure, and saw that the compile failure mentioned continuous_search and this was the only CL matching that in the regression range
    
    Original change's description:
    > [CSN] Refactor UI source directory
    >
    > This refactors the UI source directory in preparation for integrating it
    > with Chrome. A follow-up CL will implement a 'container' element that
    > will host the UI.
    >
    > Bug: 1175121
    > Change-Id: I271f0b9914ebd873290efa36f3c74c591fda3dec
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2678541
    > Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#851940}
    
    TBR=yfriedman@chromium.org,mthiesse@chromium.org,mahmoudi@chromium.org,ckitagawa@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,yashard@chromium.org
    
    Change-Id: I9f91250693067c8197de9e91775b0c5bb5c6ddef
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1175121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683289
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851956}

--- chrome/browser/continuous_search/android/java/org/chromium/chrome/browser/continuous_search/ContinuousSearchTabHelper.java
+++ chrome/browser/continuous_search/android/java/org/chromium/chrome/browser/continuous_search/ContinuousSearchTabHelper.java
@@ -27,6 +27,7 @@ public class ContinuousSearchTabHelper {
         if (!ChromeFeatureList.isEnabled(ChromeFeatureList.CONTINUOUS_SEARCH)) return;
 
         SearchResultUserData.createForTab(tab);
+        SearchResultListCoordinator.createForTab(tab);
         new ContinuousSearchTabObserver(tab);
     }
 }
