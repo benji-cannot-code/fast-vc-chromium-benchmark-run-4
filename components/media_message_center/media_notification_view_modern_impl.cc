a516698b599c58864854b58bb8784f18a1290d58
Peter Kasting <pkasting@chromium.org>
2021-02-09 16:22:10 +0000

    Fix uninitialized variable.
    This is causing build failures on the MSAN bots.
    
    Bug: 1159562
    Change-Id: I2ed274313bd6de4ce07ffdb869b387b485a432d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683459
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852212}

--- components/media_message_center/media_notification_view_modern_impl.cc
+++ components/media_message_center/media_notification_view_modern_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/canvas.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/gfx/font.h"
 #include "ui/gfx/font_list.h"
 #include "ui/gfx/paint_vector_icon.h"
@@ -73,7 +74,7 @@ class MediaArtworkView : public views::ImageView {
   void OnPaint(gfx::Canvas* canvas) override;
 
  private:
-  SkColor vignette_color_;
+  SkColor vignette_color_ = gfx::kPlaceholderColor;
   float corner_radius_;
 };
 
