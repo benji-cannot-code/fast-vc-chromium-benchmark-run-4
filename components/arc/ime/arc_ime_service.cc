0e286f0d13ebd0fadb69e61eb36b85dff0b17e6f
Yuichiro Hanada <yhanada@google.com>
2021-02-09 02:58:54 +0000

    Stop key event propagation from virtual keyboard.
    This is a retry of crrev.com/c/2681342.
    By the special logic in ui::InputMethodChromeOS::DispatchKeyEvent, both
    of InserChar() and DispatchKeyEventPostIME() are called for a key event
    from virtual keyboard.
    To preveing from inputting two characters by one key event, key events
    which are sent via InsertChar() should not be dispatched to the later
    stages.
    
    Bug: b:170260630, b:178674749, b:179050423
    Test: manual - confirm all linked issues are fixed.
    Test: arc.ChromeVirtualKeyboard{,.unstable}, arc.PreIMEKeyEvent
    Change-Id: I4b8b589b0b70820a8825385477c385f59f8dc957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2682357
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852047}

--- components/arc/ime/arc_ime_service.cc
+++ components/arc/ime/arc_ime_service.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/ime/chromeos/extension_ime_util.h"
 #include "ui/base/ime/chromeos/input_method_manager.h"
+#include "ui/base/ime/constants.h"
 #include "ui/base/ime/input_method.h"
 #include "ui/base/ime/input_method_delegate.h"
 #include "ui/base/ime/text_input_flags.h"
@@ -44,6 +45,17 @@ namespace arc {
 namespace {
 
 base::Optional<double> g_override_default_device_scale_factor;
+
+// Return true when a rich text editing is available on a text field with the
+// given type.
+bool IsTextInputActive(ui::TextInputType type) {
+  return type != ui::TEXT_INPUT_TYPE_NONE && type != ui::TEXT_INPUT_TYPE_NULL;
+}
+
+// Return true if the given key event generats a visible character.
+bool IsCharacterKeyEvent(const ui::KeyEvent* event) {
+  return !IsControlChar(event) && !ui::IsSystemKeyModifier(event->flags());
+}
 
 class ArcWindowDelegateImpl : public ArcImeService::ArcWindowDelegate {
  public:
@@ -462,12 +474,10 @@ void ArcImeService::InsertChar(const ui::KeyEvent& event) {
     return;
 
   // According to the document in text_input_client.h, InsertChar() is called
-  // even when the text input type is NONE. We ignore such events, since for
-  // ARC we are only interested in the event as a method of text input.
-  if (ime_type_ == ui::TEXT_INPUT_TYPE_NONE ||
-      ime_type_ == ui::TEXT_INPUT_TYPE_NULL) {
+  // even when the text editing is not available. We ignore such events, since
+  // for ARC we are only interested in the event as a method of text input.
+  if (!IsTextInputActive(ime_type_))
     return;
-  }
 
   InvalidateSurroundingTextAndSelectionRange();
 
@@ -487,7 +497,7 @@ void ArcImeService::InsertChar(const ui::KeyEvent& event) {
     }
   }
 
-  if (!IsControlChar(&event) && !ui::IsSystemKeyModifier(event.flags())) {
+  if (IsCharacterKeyEvent(&event)) {
     has_composition_text_ = false;
     ime_bridge_->SendInsertText(base::string16(1, event.GetText()));
   }
@@ -661,10 +671,26 @@ bool ArcImeService::SetAutocorrectRange(const gfx::Range& range) {
 }
 
 void ArcImeService::OnDispatchingKeyEventPostIME(ui::KeyEvent* event) {
-  if (ShouldEnableKeyEventForwarding() && receiver_->HasCallback()) {
+  if (!ShouldEnableKeyEventForwarding())
+    return;
+
+  if (receiver_->HasCallback()) {
     receiver_->DispatchKeyEventPostIME(event);
     event->SetHandled();
+    return;
   }
+
+  // Do not forward the key event from virtual keyboard if it's sent via
+  // InsertChar(). By the special logic in
+  // ui::InputMethodChromeOS::DispatchKeyEvent, both of InsertChar() and
+  // DispatchKeyEventPostIME() are called for a key event injected by the
+  // virtual keyboard. The below logic stops key event propagation through
+  // DispatchKeyEventPostIME() to prevent from inputting two characters.
+  const bool from_vk =
+      event->properties() && (event->properties()->find(ui::kPropertyFromVK) !=
+                              event->properties()->end());
+  if (from_vk && IsCharacterKeyEvent(event) && IsTextInputActive(ime_type_))
+    event->SetHandled();
 }
 
 // static
