06b996632e44a4111e43bf98de44c23cb2b0ae6d
Claude van der Merwe <cvandermerwe@google.com>
2021-02-09 19:30:50 +0000

    Migrate network_ui to Polymer 3
    Fixed: 1111852
    Change-Id: Ib2e8b15232e3f7853a7daea42b8e0d16f27f2f4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2678667
    Commit-Queue: Claude van der Merwe <cvandermerwe@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852294}

--- chrome/browser/resources/chromeos/network_ui/network_logs_ui.js
+++ chrome/browser/resources/chromeos/network_ui/network_logs_ui.js
@@ -3,6 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
+import 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.m.js';
+import 'chrome://resources/cr_elements/cr_radio_group/cr_radio_group.m.js';
+import 'chrome://resources/cr_elements/shared_style_css.m.js';
+
+import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {NetworkUIBrowserProxy, NetworkUIBrowserProxyImpl} from './network_ui_browser_proxy.js';
+
 /**
  * @fileoverview
  * Polymer element for UI controlling the storing of system logs.
@@ -10,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 Polymer({
   is: 'network-logs-ui',
+
+  _template: html`{__html_template__}`,
 
   behaviors: [I18nBehavior],
 
@@ -71,8 +84,8 @@ Polymer({
 
   observers: ['onShillDebuggingChanged_(shillDebugging_)'],
 
-  /** @type {!network_ui.NetworkUIBrowserProxy} */
-  browserProxy_: network_ui.NetworkUIBrowserProxyImpl.getInstance(),
+  /** @type {!NetworkUIBrowserProxy} */
+  browserProxy_: NetworkUIBrowserProxyImpl.getInstance(),
 
   /** @override */
   attached() {},
--- chrome/browser/resources/chromeos/network_ui/network_ui.js
+++ chrome/browser/resources/chromeos/network_ui/network_ui.js
@@ -3,6 +3,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_components/chromeos/network/network_select.m.js';
+import 'chrome://resources/cr_components/chromeos/network_health/network_diagnostics.m.js';
+import 'chrome://resources/cr_components/chromeos/network_health/network_health_summary.m.js';
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/cr_elements/cr_input/cr_input.m.js';
+import 'chrome://resources/cr_elements/cr_tabs/cr_tabs.m.js';
+import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
+import 'chrome://resources/mojo/chromeos/services/network_config/public/mojom/cros_network_config.mojom-lite.js';
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/polymer/v3_0/iron-pages/iron-pages.js';
+import './strings.m.js';
+import './network_state_ui.js';
+import './network_logs_ui.js';
+
+import {OncMojo} from 'chrome://resources/cr_components/chromeos/network/onc_mojo.m.js';
+import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {NetworkUIBrowserProxy, NetworkUIBrowserProxyImpl} from './network_ui_browser_proxy.js';
+
+
 /**
  * @fileoverview
  * Polymer element network debugging UI.
@@ -10,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 Polymer({
   is: 'network-ui',
+
+  _template: html`{__html_template__}`,
 
   behaviors: [I18nBehavior],
 
@@ -50,8 +72,8 @@ Polymer({
   /** @type {?chromeos.networkConfig.mojom.CrosNetworkConfigRemote} */
   networkConfig_: null,
 
-  /** @type {!network_ui.NetworkUIBrowserProxy} */
-  browserProxy_: network_ui.NetworkUIBrowserProxyImpl.getInstance(),
+  /** @type {!NetworkUIBrowserProxy} */
+  browserProxy_: NetworkUIBrowserProxyImpl.getInstance(),
 
   /** @override */
   attached() {
