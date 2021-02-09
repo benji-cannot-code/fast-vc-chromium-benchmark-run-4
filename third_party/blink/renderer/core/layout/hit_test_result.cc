80b092f759c2749b0253da35ba9ae7cbdc8f1740
Koji Ishii <kojii@chromium.org>
2021-02-09 20:24:50 +0000

    Ensure PrePaintClean before PositionForPoint
    This patch ensures PrePaintClean before |MouseEventManager|
    calls |SelectionController|, which in turn will call
    |LayoutObject::PositionForPoint|.
    
    |LayoutObject::PositionForPoint| requires PrePaintClean when
    NG block fragmented, since r849502 <crrev.com/c/2637678>. The
    plan is to ensure PrePaintClean even when it is not block
    fragmented to avoid complicated issues, and this patch is the
    first part of the effort.
    
    |LayoutView::HitTest| already forces |PrePaintClean|, but
    |MouseEventManager| forces |UpdateStyleAndLayout| multiple
    times because changing focus and running JS event handlers
    can modify DOM and/or styles. It is likely that the document
    is not |PrePaintClean| when it calls |SelectionController|
    for that reason.
    
    Bug: 1150362
    Change-Id: I88a2ca2587bc39d0f9eb147818ad39ef0c0dfe78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679061
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852333}

--- third_party/blink/renderer/core/layout/hit_test_result.cc
+++ third_party/blink/renderer/core/layout/hit_test_result.cc
@@ -165,6 +165,9 @@ PositionWithAffinity HitTestResult::GetPosition() const {
   const Node* node = inner_possibly_pseudo_node_;
   if (!node)
     return PositionWithAffinity();
+  // |LayoutObject::PositionForPoint()| requires |kPrePaintClean|.
+  DCHECK_GE(node->GetDocument().Lifecycle().GetState(),
+            DocumentLifecycle::kPrePaintClean);
   LayoutObject* layout_object = node->GetLayoutObject();
   if (!layout_object)
     return PositionWithAffinity();
@@ -194,6 +197,9 @@ PositionWithAffinity HitTestResult::GetPositionForInnerNodeOrImageMapImage()
     node = InnerNodeOrImageMapImage();
   if (!node)
     return PositionWithAffinity();
+  // |LayoutObject::PositionForPoint()| requires |kPrePaintClean|.
+  DCHECK_GE(node->GetDocument().Lifecycle().GetState(),
+            DocumentLifecycle::kPrePaintClean);
   LayoutObject* layout_object = node->GetLayoutObject();
   if (!layout_object)
     return PositionWithAffinity();
