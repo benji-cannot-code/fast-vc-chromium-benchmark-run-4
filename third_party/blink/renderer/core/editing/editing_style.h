3fae97f285365e55a6af4824437473fc1f1fe852
Yoshifumi Inoue <yosin@chromium.org>
2021-02-09 08:14:30 +0000

    Make EditingStyle to handle text-align with direction
    This patch changes |EditingStyle| to resolve "start" and "end" of
    "text-align" based on the direction of the element applied to
    "text-align" instead of "direction" CSS property in |EditingStyle|.
    
    Bug: 957952
    Change-Id: I38eddec03e56423adb96f47e88555380e3983ea1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675554
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852115}

--- third_party/blink/renderer/core/editing/editing_style.h
+++ third_party/blink/renderer/core/editing/editing_style.h
@@ -154,6 +154,7 @@ class CORE_EXPORT EditingStyle final : public GarbageCollected<EditingStyle> {
   float FontSizeDelta() const { return font_size_delta_; }
   bool HasFontSizeDelta() const { return font_size_delta_ != kNoFontDelta; }
 
+  CSSValueID GetProperty(CSSPropertyID) const;
   void SetProperty(CSSPropertyID,
                    const String& value,
                    bool important,
@@ -172,6 +173,7 @@ class CORE_EXPORT EditingStyle final : public GarbageCollected<EditingStyle> {
                                           CSSComputedStyleDeclaration*);
   void ExtractFontSizeDelta();
   EditingTriState TriStateOfStyle(CSSStyleDeclaration* style_to_compare,
+                                  Node* node,
                                   ShouldIgnoreTextOnlyProperties,
                                   SecureContextMode) const;
   bool ConflictsWithInlineStyleOfElement(
@@ -181,6 +183,10 @@ class CORE_EXPORT EditingStyle final : public GarbageCollected<EditingStyle> {
   void MergeStyle(const CSSPropertyValueSet*, CSSPropertyOverrideMode);
 
   Member<MutableCSSPropertyValueSet> mutable_style_;
+  // This |EditingStyle| is constructed from |node_|. |node_| is null when
+  // this |EditingStyle| is constructed from |CSSPropertyValueSet*| or
+  // |CSSPropertyID|.
+  Member<Node> node_;
   bool is_monospace_font_ = false;
   float font_size_delta_ = kNoFontDelta;
   bool is_vertical_align_ = false;
