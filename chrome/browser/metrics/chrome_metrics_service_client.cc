b8eec28d41227e35c10e146e7a2ebf64ac376df3
Brian Malcolm <bmalcolm@chromium.org>
2021-02-09 00:15:48 +0000

    Add UserTypeByDeviceType UMA
    These metrics are a count of the types of primary user logins for various
    device types (unmanaged user on managed device, managed user on unmanaged
    device, managed guest session on education device, etc).
    
    BUG=b:178732873
    
    Change-Id: I268ca415d1e04cee258d7fd4b83615f96d6fcafb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673641
    Commit-Queue: Brian Malcolm <bmalcolm@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#851965}

--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/https_engagement_metrics_provider.h"
 #include "chrome/browser/metrics/metrics_reporting_state.h"
 #include "chrome/browser/metrics/network_quality_estimator_provider_impl.h"
+#include "chrome/browser/metrics/usertype_by_devicetype_metrics_provider.h"
 #include "chrome/browser/privacy_budget/privacy_budget_prefs.h"
 #include "chrome/browser/privacy_budget/privacy_budget_ukm_entry_filter.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -764,6 +765,12 @@ void ChromeMetricsServiceClient::RegisterMetricsServiceProviders() {
   if (base::FeatureList::IsEnabled(chromeos::kFamilyLinkUserMetricsProvider)) {
     metrics_service_->RegisterMetricsProvider(
         std::make_unique<FamilyLinkUserMetricsProvider>());
+  }
+
+  if (base::FeatureList::IsEnabled(
+          ::features::kUserTypeByDeviceTypeMetricsProvider)) {
+    metrics_service_->RegisterMetricsProvider(
+        std::make_unique<UserTypeByDeviceTypeMetricsProvider>());
   }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
