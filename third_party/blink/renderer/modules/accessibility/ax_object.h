29345cdb70d7d05475bbceb70bae0b0c5a00848e
Meredith Lane <meredithl@chromium.org>
2021-02-10 02:49:26 +0000

    [AXOnionSoup] move Element/HTML attribute serialization to AXObject
    This patch has no functional difference, it is just a refactor.
    
    AX-Relnotes: N/A
    Bug: 1068668
    Change-Id: Ia16a4f984e76830477ed68a454aea9850db48140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2682325
    Commit-Queue: Meredith Lane <meredithl@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852431}

--- third_party/blink/renderer/modules/accessibility/ax_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_object.h
@@ -1311,6 +1311,8 @@ class MODULES_EXPORT AXObject : public GarbageCollected<AXObject> {
   void SerializeTableAttributes(ui::AXNodeData* node_data);
   void SerializeListAttributes(ui::AXNodeData* node_data);
   void SerializeScrollAttributes(ui::AXNodeData* node_data);
+  void SerializeElementAttributes(ui::AXNodeData* dst);
+  void SerializeHTMLAttributes(ui::AXNodeData* dst);
 
   // Serialization implemented in specific subclasses.
   virtual void SerializeMarkerAttributes(ui::AXNodeData* node_data) const;
