f2f8d9de9cff2f7cfd145409a9279b0c9e0b6270
Elly Fong-Jones <ellyjones@chromium.org>
2021-02-10 20:32:26 +0000

    cbuiv: record metrics for window naming
    This will help us figure out how often users find this feature, since
    we have some questions about discoverability, and given that they have
    found it how often they decide to use it.
    
    Specifically, we log:
    * Browser.AnyWindowHasName at every metrics upload, to get a DAU/MAU
      count
    * Action WindowNaming_DialogShown
    * Actions WindowNaming_{Clear,Set} depending on what the user does with
      the dialog
    
    Bug: 1096586
    Change-Id: I6d9f0b7363bab299dfedb2a655d2cf32e80db06e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683831
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852779}

--- chrome/browser/metrics/desktop_platform_features_metrics_provider.cc
+++ chrome/browser/metrics/desktop_platform_features_metrics_provider.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/read_later/reading_list_model_factory.h"
 #include "components/reading_list/core/reading_list_model.h"
 #include "components/reading_list/features/reading_list_switches.h"
@@ -27,6 +29,14 @@ enum class DarkModeStatus {
   kDark = 2,
   kMaxValue = kDark,
 };
+
+bool AnyBrowserWindowHasName() {
+  for (auto* browser : *BrowserList::GetInstance()) {
+    if (!browser->user_title().empty())
+      return true;
+  }
+  return false;
+}
 
 }  // namespace
 
@@ -60,4 +70,6 @@ void DesktopPlatformFeaturesMetricsProvider::ProvideCurrentSessionData(
       }
     }
   }
+
+  UMA_HISTOGRAM_BOOLEAN("Browser.AnyWindowHasName", AnyBrowserWindowHasName());
 }
