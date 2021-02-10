01e7294db37110aed370a95c99be20df93a5fec7
Bartek Nowierski <bartekn@chromium.org>
2021-02-10 13:01:11 +0000

    [PA] Split GigaCage into: supports BRP vs. doesn't
    So far GigaCage was split into normal buckets that support BRP and
    direct map that doesn't. Normal buckets not supporting BRP were
    allocated outside of GigaCage entirely, which led to confusing code.
    This CL lumps the 2nd and 3rd category together, thus splitting GigaCage
    into "supports BRP" vs. "doesn't support BRP" pools.
    
    Note that pool selection doesn't depend on USE_BACKUP_REF_PTR (it could
    but for stability better not), but it does depend on
    ENABLE_RUNTIME_BACKUP_REF_PTR_CONTROL.
    
    Most of this CL is renaming, but non-trivial changes include:
    1. AllocNewSuperPage() now chooses pool at run-time.
    2. UsesGigaCage() no longer checks allow_ref_count (in BRP case), hence
       is replaced with features::IsPartitionAllocGigaCageEnabled().
    3. FreeNoHooks() can now always enable GigaCage pool belonging check.
    4. Functions IsWithinSuperPagePayload, GetSlotStartInSuperPage,
       QuarantineBitmapFromPointer, PartitionAllocGetSlotSpanForSizeQuery
       and PartitionRoot::FromPointer lost their ability to check if the
       passed pointer was allocated from normal buckets.
    
    Bug: 1073933
    Change-Id: I063eca084b98f988b3c2649e29f39b1b5f00e08d
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2671383
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852604}

--- base/allocator/partition_allocator/partition_root.cc
+++ base/allocator/partition_allocator/partition_root.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/allocator/partition_allocator/partition_root.h"
 
+#include "base/allocator/partition_allocator/address_pool_manager_bitmap.h"
 #include "base/allocator/partition_allocator/oom.h"
 #include "base/allocator/partition_allocator/page_allocator.h"
+#include "base/allocator/partition_allocator/partition_address_space.h"
 #include "base/allocator/partition_allocator/partition_alloc_check.h"
 #include "base/allocator/partition_allocator/partition_alloc_features.h"
 #include "base/allocator/partition_allocator/partition_bucket.h"
@@ -387,10 +389,8 @@ static void PartitionDumpBucketStats(
 }
 
 #if DCHECK_IS_ON()
-void DCheckIfManagedByPartitionAllocNormalBuckets(const void* ptr) {
-  if (features::IsPartitionAllocGigaCageEnabled()) {
-    PA_DCHECK(IsManagedByPartitionAllocNormalBuckets(ptr));
-  }
+void DCheckIfManagedByPartitionAllocBRPPool(const void* ptr) {
+  PA_DCHECK(IsManagedByPartitionAllocBRPPool(ptr));
 }
 #endif
 
--- base/allocator/partition_allocator/pcscan_unittest.cc
+++ base/allocator/partition_allocator/pcscan_unittest.cc
@@ -24,7 +24,7 @@ class PCScanTest : public testing::Test {
     allocator_.init({PartitionOptions::Alignment::kRegular,
                      PartitionOptions::ThreadCache::kDisabled,
                      PartitionOptions::Quarantine::kAllowed,
-                     PartitionOptions::RefCount::kDisabled});
+                     PartitionOptions::RefCount::kEnabled});
     PCScan<ThreadSafe>::Instance().RegisterScannableRoot(allocator_.root());
   }
   ~PCScanTest() override {
@@ -171,8 +171,8 @@ template <typename SourceList, typename ValueList>
 void TestDanglingReference(PCScanTest& test,
                            SourceList* source,
                            ValueList* value) {
-  auto* value_root = ThreadSafePartitionRoot::FromPointerInNormalBucketPool(
-      reinterpret_cast<char*>(value));
+  auto* value_root =
+      ThreadSafePartitionRoot::FromPointer(reinterpret_cast<char*>(value));
   {
     // Free |value| and leave the dangling reference in |source|.
     ValueList::Destroy(*value_root, value);
@@ -316,11 +316,11 @@ TEST_F(PCScanTest, DanglingInterPartitionReference) {
   ThreadSafePartitionRoot source_root({PartitionOptions::Alignment::kRegular,
                                        PartitionOptions::ThreadCache::kDisabled,
                                        PartitionOptions::Quarantine::kAllowed,
-                                       PartitionOptions::RefCount::kDisabled});
+                                       PartitionOptions::RefCount::kEnabled});
   ThreadSafePartitionRoot value_root({PartitionOptions::Alignment::kRegular,
                                       PartitionOptions::ThreadCache::kDisabled,
                                       PartitionOptions::Quarantine::kAllowed,
-                                      PartitionOptions::RefCount::kDisabled});
+                                      PartitionOptions::RefCount::kEnabled});
 
   PCScan<ThreadSafe>::Instance().RegisterScannableRoot(&source_root);
   PCScan<ThreadSafe>::Instance().RegisterScannableRoot(&value_root);
@@ -339,11 +339,11 @@ TEST_F(PCScanTest, DanglingReferenceToNonScannablePartition) {
   ThreadSafePartitionRoot source_root({PartitionOptions::Alignment::kRegular,
                                        PartitionOptions::ThreadCache::kDisabled,
                                        PartitionOptions::Quarantine::kAllowed,
-                                       PartitionOptions::RefCount::kDisabled});
+                                       PartitionOptions::RefCount::kEnabled});
   ThreadSafePartitionRoot value_root({PartitionOptions::Alignment::kRegular,
                                       PartitionOptions::ThreadCache::kDisabled,
                                       PartitionOptions::Quarantine::kAllowed,
-                                      PartitionOptions::RefCount::kDisabled});
+                                      PartitionOptions::RefCount::kEnabled});
 
   PCScan<ThreadSafe>::Instance().RegisterScannableRoot(&source_root);
   PCScan<ThreadSafe>::Instance().RegisterNonScannableRoot(&value_root);
@@ -363,12 +363,12 @@ TEST_F(PCScanTest, DanglingReferenceFromNonScannablePartition) {
       {PartitionOptions::Alignment::kRegular,
        PartitionOptions::ThreadCache::kDisabled,
        base::PartitionOptions::Quarantine::kAllowed,
-       PartitionOptions::RefCount::kDisabled});
+       PartitionOptions::RefCount::kEnabled});
   ThreadSafePartitionRoot value_root(
       {PartitionOptions::Alignment::kRegular,
        PartitionOptions::ThreadCache::kDisabled,
        base::PartitionOptions::Quarantine::kAllowed,
-       PartitionOptions::RefCount::kDisabled});
+       PartitionOptions::RefCount::kEnabled});
 
   PCScan<ThreadSafe>::Instance().RegisterNonScannableRoot(&source_root);
   PCScan<ThreadSafe>::Instance().RegisterScannableRoot(&value_root);
