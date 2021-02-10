bae78b067a75c523aea4c8c27b8eecd426e35210
Mohamed Amir Yosef <mamir@chromium.org>
2021-02-10 20:22:01 +0000

    [Autofill] Save address dialog buttons invoke the callback
    This CL wires the Accept/Cancel buttons of the Save Address Profile
    dialog to the callback that saves the address profile.
    
    Bug: 1167060
    Change-Id: Ifbf073f3a2635f00fb6c5d16238fff081d8c481b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2643290
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852769}

--- chrome/browser/ui/autofill/save_address_profile_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/save_address_profile_bubble_controller_impl.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/autofill/save_address_profile_bubble_controller.h"
+#include "chrome/browser/ui/autofill/save_address_profile_bubble_controller_impl.h"
 
 #include "chrome/browser/ui/autofill/autofill_bubble_handler.h"
 #include "chrome/browser/ui/browser.h"
@@ -13,21 +13,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-SaveAddressProfileBubbleController::SaveAddressProfileBubbleController(
+SaveAddressProfileBubbleControllerImpl::SaveAddressProfileBubbleControllerImpl(
     content::WebContents* web_contents)
     : AutofillBubbleControllerBase(web_contents) {
   DCHECK(base::FeatureList::IsEnabled(
       features::kAutofillAddressProfileSavePrompt));
 }
 
-SaveAddressProfileBubbleController::~SaveAddressProfileBubbleController() =
-    default;
+SaveAddressProfileBubbleControllerImpl::
+    ~SaveAddressProfileBubbleControllerImpl() = default;
 
-base::string16 SaveAddressProfileBubbleController::GetWindowTitle() const {
-  return base::string16();
-}
-
-void SaveAddressProfileBubbleController::OfferSave(
+void SaveAddressProfileBubbleControllerImpl::OfferSave(
     const AutofillProfile& profile,
     AutofillClient::AddressProfileSavePromptCallback
         address_profile_save_prompt_callback) {
@@ -40,16 +36,29 @@ void SaveAddressProfileBubbleController::OfferSave(
   Show();
 }
 
-void SaveAddressProfileBubbleController::OnBubbleClosed() {
+base::string16 SaveAddressProfileBubbleControllerImpl::GetWindowTitle() const {
+  return base::string16();
+}
+
+void SaveAddressProfileBubbleControllerImpl::OnUserDecision(
+    AutofillClient::SaveAddressProfileOfferUserDecision decision) {
+  set_bubble_view(nullptr);
+
+  std::move(address_profile_save_prompt_callback_)
+      .Run(decision, address_profile_);
+}
+
+void SaveAddressProfileBubbleControllerImpl::OnBubbleClosed() {
   set_bubble_view(nullptr);
   UpdatePageActionIcon();
 }
 
-PageActionIconType SaveAddressProfileBubbleController::GetPageActionIconType() {
+PageActionIconType
+SaveAddressProfileBubbleControllerImpl::GetPageActionIconType() {
   return PageActionIconType::kSaveCard;
 }
 
-void SaveAddressProfileBubbleController::DoShowBubble() {
+void SaveAddressProfileBubbleControllerImpl::DoShowBubble() {
   DCHECK(!bubble_view());
   Browser* browser = chrome::FindBrowserWithWebContents(web_contents());
   set_bubble_view(
@@ -60,6 +69,6 @@ void SaveAddressProfileBubbleController::DoShowBubble() {
   DCHECK(bubble_view());
 }
 
-WEB_CONTENTS_USER_DATA_KEY_IMPL(SaveAddressProfileBubbleController)
+WEB_CONTENTS_USER_DATA_KEY_IMPL(SaveAddressProfileBubbleControllerImpl)
 
 }  // namespace autofill
--- chrome/browser/ui/autofill/save_address_profile_bubble_controller_impl_browsertest.cc
+++ chrome/browser/ui/autofill/save_address_profile_bubble_controller_impl_browsertest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/autofill/save_address_profile_bubble_controller.h"
+#include "chrome/browser/ui/autofill/save_address_profile_bubble_controller_impl.h"
 
 #include "chrome/browser/ui/autofill/chrome_autofill_client.h"
 #include "chrome/browser/ui/browser.h"
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-class SaveAddressProfileBubbleControllerTest : public DialogBrowserTest {
+class SaveAddressProfileBubbleControllerImplTest : public DialogBrowserTest {
  public:
-  SaveAddressProfileBubbleControllerTest() {
+  SaveAddressProfileBubbleControllerImplTest() {
     feature_list_.InitAndEnableFeature(
         features::kAutofillAddressProfileSavePrompt);
   }
@@ -35,29 +35,30 @@ class SaveAddressProfileBubbleControllerTest : public DialogBrowserTest {
     autofill_client->ConfirmSaveAddressProfile(test::GetFullProfile(),
                                                base::DoNothing());
     controller_ =
-        SaveAddressProfileBubbleController::FromWebContents(web_contents);
+        SaveAddressProfileBubbleControllerImpl::FromWebContents(web_contents);
     DCHECK(controller_);
   }
 
-  SaveAddressProfileBubbleController* controller() { return controller_; }
+  SaveAddressProfileBubbleControllerImpl* controller() { return controller_; }
 
  private:
-  SaveAddressProfileBubbleController* controller_ = nullptr;
+  SaveAddressProfileBubbleControllerImpl* controller_ = nullptr;
   base::test::ScopedFeatureList feature_list_;
 };
 
-IN_PROC_BROWSER_TEST_F(SaveAddressProfileBubbleControllerTest, InvokeUi_Save) {
+IN_PROC_BROWSER_TEST_F(SaveAddressProfileBubbleControllerImplTest,
+                       InvokeUi_Save) {
   ShowAndVerifyUi();
 }
 
-IN_PROC_BROWSER_TEST_F(SaveAddressProfileBubbleControllerTest,
+IN_PROC_BROWSER_TEST_F(SaveAddressProfileBubbleControllerImplTest,
                        InvokeUi_SaveCloseThenReopen) {
   ShowAndVerifyUi();
   controller()->OnBubbleClosed();
   ShowAndVerifyUi();
 }
 
-IN_PROC_BROWSER_TEST_F(SaveAddressProfileBubbleControllerTest,
+IN_PROC_BROWSER_TEST_F(SaveAddressProfileBubbleControllerImplTest,
                        CloseTabWhileBubbleIsOpen) {
   ShowAndVerifyUi();
   content::WebContents* tab =
