422be3051bcf4868a1142540fea6bc1a59060b47
Trent Begin <tbegin@google.com>
2021-02-10 06:06:26 +0000

    connectivity-diagnostics: add IP address fields to network health
    This change adds the network device's IP addresses to the network health
    summary. Each IP address is represented as a string. The network's ipv4
    address is a single field, while the ipv6 field is an array.
    
    Bug: chromium:1170082
    Change-Id: I36d43766bf202d4745b7b954c7a3c3f0f4f27826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2660197
    Commit-Queue: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852510}

--- chrome/browser/chromeos/system_logs/network_health_source.cc
+++ chrome/browser/chromeos/system_logs/network_health_source.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
 #include "chrome/browser/chromeos/net/network_health/network_health_service.h"
 #include "chromeos/network/network_event_log.h"
 #include "content/public/browser/browser_thread.h"
@@ -42,6 +43,17 @@ std::string GetFormattedString(
                    : "N/A")
            << "\n";
     output << "MAC Address: " << net->mac_address.value_or("N/A") << "\n";
+
+    // Automatic PII scrubbing does not work for IP addresses so manually scrub
+    // them.
+    if (!scrub) {
+      output << "IPV4 Address: " << net->ipv4_address.value_or("N/A") << "\n";
+      output << "IPV6 Addresses: "
+             << (net->ipv6_addresses.size()
+                     ? base::JoinString(net->ipv6_addresses, ", ")
+                     : "N/A");
+    }
+
     output << "\n";
   }
   return output.str();
