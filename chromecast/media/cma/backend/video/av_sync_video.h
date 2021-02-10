be6ad7819d4dff8e0d68e829eebf45222803f26d
Ken MacKay <kmackay@chromium.org>
2021-02-10 00:56:13 +0000

    [Chromecast] Factor AV sync rate adjustments into a separate class
    Thus allowing unit tests.
    
    Bug: internal b/178250485
    Change-Id: I255762a8d68d56c2ae02838268f09193dc6e276b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2685610
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852402}

--- chromecast/media/cma/backend/video/av_sync_video.h
+++ chromecast/media/cma/backend/video/av_sync_video.h
@@ -19,6 +19,7 @@ class WeightedMovingLinearRegression;
 
 namespace media {
 class MediaPipelineBackendForMixer;
+class RateAdjuster;
 
 class AvSyncVideo : public AvSync {
  public:
@@ -41,7 +42,9 @@ class AvSyncVideo : public AvSync {
   int GetVideoFrameRate();
 
   void HardCorrection(int64_t apts, int64_t desired_apts_timestamp);
-  void AudioRateUpkeep(int64_t error, int64_t timestamp);
+  double ChangeAudioRate(double desired_clock_rate,
+                         double error_slope,
+                         double current_error);
 
   void FlushAudioPts();
   void FlushVideoPts();
@@ -52,6 +55,7 @@ class AvSyncVideo : public AvSync {
 
   std::unique_ptr<WeightedMovingLinearRegression> apts_error_;
   std::unique_ptr<WeightedMovingLinearRegression> video_pts_;
+  std::unique_ptr<RateAdjuster> audio_rate_adjuster_;
 
   // This is the audio playback rate propagated from SetPlaybackRate, which is
   // exposed to the user to speed up or slow down their playback.
@@ -66,10 +70,6 @@ class AvSyncVideo : public AvSync {
 
   int64_t last_apts_value_ = INT64_MIN;
   int64_t last_apts_timestamp_ = INT64_MIN;
-
-  int64_t clock_rate_start_timestamp_ = INT64_MIN;
-  double clock_rate_error_base_ = 0.0;
-  int64_t apts_error_start_timestamp_ = INT64_MIN;
 
   DISALLOW_COPY_AND_ASSIGN(AvSyncVideo);
 };
