d7ba138215de4ff93edda3d63970c03bcdc8db9f
Mike Dougherty <michaeldo@chromium.org>
2021-02-10 21:39:54 +0000

    [iOS] Update JavaScriptFeature to only allow one script message handler
    Although, the JavaScriptFeature class exposes support for multiple
    script message handlers, the handlers are stored in a map within the
    JavaScriptContentWorld keyed by the feature. This means that only the
    last registered handler will be properly retained.
    
    Since features do not currently need more than a single handler, this CL
    modifies the API to only allow for one handler name to be monitored.
    
    Bug: 1042335
    Change-Id: If731a2ccf0915423cc5e3679507593df60582c79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679309
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852809}

--- ios/web/js_features/context_menu/context_menu_java_script_feature.h
+++ ios/web/js_features/context_menu/context_menu_java_script_feature.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <string>
-#include <vector>
 
 #include "base/callback.h"
 #include "base/supports_user_data.h"
@@ -46,7 +45,7 @@ class ContextMenuJavaScriptFeature : public JavaScriptFeature,
                          ElementDetailsCallback callback);
 
   // JavaScriptFeature:
-  std::vector<std::string> GetScriptMessageHandlerNames() const override;
+  base::Optional<std::string> GetScriptMessageHandlerName() const override;
   void ScriptMessageReceived(BrowserState* browser_state,
                              WKScriptMessage* message) override;
 
