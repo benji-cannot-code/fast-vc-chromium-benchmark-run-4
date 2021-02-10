638a8f64f981408c1dfd1606eea5b8408258f11f
Alan Screen <awscreen@chromium.org>
2021-02-10 08:56:16 +0000

    Flush PPD buffer before seek
    This is a speculative fix for the PPD parsing crashes sometimes seen
    on macOS.
    
    The previous methods for parsing PPDs involved multiple file open and
    close operations.  The act of closing before reopening ensured that the
    contents of writing out the PPD capabilities string was flushed out to
    the file before that was read back in.
    
    While the Unix description of fseek() [1] indicates that a call to
    fseek() with unwritten data will cause such data to be written to the
    file, there is no such similar guarantee stated for the BSD version
    of fseek(). [2]
    
    Include an explicit fflush() after writing out the data and prior to
    seeking to the beginning of file to read it back by the CUPS PPD parsing
    routine.  This is to rule out the possibility of the underlying macOS
    BSD file stream routines as the possible reason for how data might have
    gotten corrupted and led to crashes during the subsequent parsing.
    
    [1] https://pubs.opengroup.org/onlinepubs/7908799/xsh/fseek.html
    [2] https://man.openbsd.org/fseek.3
    
    Bug: 1166792
    Change-Id: If7f39a8479a70707fcd47d28fcc6b0718c13b9ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2686593
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852550}

--- printing/backend/cups_helper.cc
+++ printing/backend/cups_helper.cc
@@ -603,6 +603,8 @@ bool ParsePpdCapabilities(cups_dest_t* dest,
     return false;
   }
 
+  if (fflush(ppd_file.handle()))
+    return false;
   if (fseek(ppd_file.handle(), 0, SEEK_SET) != 0)
     return false;
 
