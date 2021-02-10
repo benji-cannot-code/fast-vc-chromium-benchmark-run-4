554c64a2b690afd4d0bd72cb7baf315c515165c0
danakj <danakj@chromium.org>
2021-02-10 15:24:38 +0000

    Remove WebContentsObserver::RenderViewCreated().
    This was deprecated and usage has been moved to RenderFrameCreated.
    
    R=avi@chromium.org
    
    Bug: 763548
    Change-Id: I0f439f7381e1a152bde3fe5d449df14bdd64952c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2686147
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852638}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -6779,8 +6779,6 @@ void WebContentsImpl::RenderViewCreated(RenderViewHost* render_view_host) {
       Source<WebContents>(this),
       Details<RenderViewHost>(render_view_host));
 
-  observers_.NotifyObservers(&WebContentsObserver::RenderViewCreated,
-                             render_view_host);
   if (delegate_)
     RenderFrameDevToolsAgentHost::WebContentsCreated(this);
 }
