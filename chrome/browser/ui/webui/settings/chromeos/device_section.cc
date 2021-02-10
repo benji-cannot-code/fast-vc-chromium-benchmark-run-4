d420695e31c9a3b4f9db2b313517e2df4e3127a2
Kyle Horimoto <khorimoto@chromium.org>
2021-02-10 04:47:45 +0000

    [CrOS Settings] Only show "mouse scroll" search results when available
    Before this CL, we would show settings search results for mouse scroll
    acceleration even when the setting was unavailable (due to its flag
    being disabled). This CL only shows these results when the flag is on.
    
    Fixed: 1159964
    Change-Id: I2a3d068cdb68334abbe364fa8a246a690c7e340b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2686390
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852481}

--- chrome/browser/ui/webui/settings/chromeos/device_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/device_section.cc
@@ -199,6 +199,18 @@ GetTouchpadScrollAccelerationSearchConcepts() {
   return *tags;
 }
 
+const std::vector<SearchConcept>& GetMouseScrollAccelerationSearchConcepts() {
+  static const base::NoDestructor<std::vector<SearchConcept>> tags({
+      {IDS_OS_SETTINGS_TAG_MOUSE_SCROLL_ACCELERATION,
+       mojom::kPointersSubpagePath,
+       mojom::SearchResultIcon::kMouse,
+       mojom::SearchResultDefaultRank::kMedium,
+       mojom::SearchResultType::kSetting,
+       {.setting = mojom::Setting::kMouseScrollAcceleration}},
+  });
+  return *tags;
+}
+
 const std::vector<SearchConcept>& GetMouseSearchConcepts() {
   static const base::NoDestructor<std::vector<SearchConcept>> tags({
       {IDS_OS_SETTINGS_TAG_MOUSE_ACCELERATION,
@@ -231,12 +243,6 @@ const std::vector<SearchConcept>& GetMouseSearchConcepts() {
        mojom::SearchResultDefaultRank::kMedium,
        mojom::SearchResultType::kSubpage,
        {.subpage = mojom::Subpage::kPointers}},
-      {IDS_OS_SETTINGS_TAG_MOUSE_SCROLL_ACCELERATION,
-       mojom::kPointersSubpagePath,
-       mojom::SearchResultIcon::kMouse,
-       mojom::SearchResultDefaultRank::kMedium,
-       mojom::SearchResultType::kSetting,
-       {.setting = mojom::Setting::kMouseScrollAcceleration}},
   });
   return *tags;
 }
@@ -471,6 +477,11 @@ const std::vector<SearchConcept>& GetPowerWithLaptopLidSearchConcepts() {
         SearchConcept::kAltTagEnd}},
   });
   return *tags;
+}
+
+bool AreScrollSettingsAllowed() {
+  return base::FeatureList::IsEnabled(
+      ::chromeos::features::kAllowScrollSettings);
 }
 
 bool IsUnifiedDesktopAvailable() {
@@ -1021,11 +1032,14 @@ void DeviceSection::TouchpadExists(bool exists) {
 
 void DeviceSection::MouseExists(bool exists) {
   SearchTagRegistry::ScopedTagUpdater updater = registry()->StartUpdate();
+  updater.RemoveSearchTags(GetMouseSearchConcepts());
+  updater.RemoveSearchTags(GetMouseScrollAccelerationSearchConcepts());
 
-  if (exists)
+  if (exists) {
     updater.AddSearchTags(GetMouseSearchConcepts());
-  else
-    updater.RemoveSearchTags(GetMouseSearchConcepts());
+    if (AreScrollSettingsAllowed())
+      updater.AddSearchTags(GetMouseScrollAccelerationSearchConcepts());
+  }
 }
 
 void DeviceSection::PointingStickExists(bool exists) {
@@ -1214,9 +1228,7 @@ void DeviceSection::AddDevicePointersStrings(
   html_source->AddBoolean(
       "allowDisableMouseAcceleration",
       base::FeatureList::IsEnabled(::features::kAllowDisableMouseAcceleration));
-  html_source->AddBoolean(
-      "allowScrollSettings",
-      base::FeatureList::IsEnabled(::chromeos::features::kAllowScrollSettings));
+  html_source->AddBoolean("allowScrollSettings", AreScrollSettingsAllowed());
   html_source->AddBoolean(
       "separatePointingStickSettings",
       base::FeatureList::IsEnabled(::features::kSeparatePointingStickSettings));
