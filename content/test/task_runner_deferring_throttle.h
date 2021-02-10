274a347c673d975898b24b566d9797c19693e60e
danakj <danakj@chromium.org>
2021-02-10 20:20:38 +0000

    Do not report ReadyToCommitNavigation without a live renderer frame.
    Doing so would have WebContentsObservers attempt to interact with a
    frame that does not exist, and trying to use mojo bindings that do not
    exist would result in a crash.
    
    R=avi@chromium.org
    
    Bug: 1176055
    Change-Id: I9b054b1112ede721f04654008780b6e29b5c0529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2687659
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852768}

--- content/test/task_runner_deferring_throttle.h
+++ content/test/task_runner_deferring_throttle.h
@@ -35,6 +35,10 @@ class TaskRunnerDeferringThrottle : public NavigationThrottle {
   ThrottleCheckResult WillProcessResponse() override;
   const char* GetNameForLogging() override;
 
+ protected:
+  // Tests may override this to change how/when Resume() is called.
+  virtual void AsyncResume();
+
  private:
   ThrottleCheckResult DeferToPostTask();
 
