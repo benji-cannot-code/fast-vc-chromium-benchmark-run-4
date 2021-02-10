chromium/chromium

commit 30c1a362748552108893775d73faa28c7c563784

Make views::LabelButton smarter about preferred size when eliding.
Before this patch, Label uses un-elided text to determine the preferred
width. In the case where it's laid out with less than the preferred
width space, and it elides, it may end up using less than its allotted
width, leaving empty space after the trailing ellipsis.

With this change, it will "give back" that trailing space during the
preferred size calculation step, allowing LabelButton to shrink to chop
off some otherwise useless horizontal space.

Bug: 1169204
Change-Id: I96ebdbe1063c8eabe1bb81b5cb754761fd9003db
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2644346
Commit-Queue: Evan Stade <estade@chromium.org>
Reviewed-by: Peter Kasting <pkasting@chromium.org>
Cr-Commit-Position: refs/heads/master@{#849431}

ui/views/controls/label.cc
@@ -451,6 +451,13 @@ void Label::SetMaximumWidth(int max_width) {
   OnPropertyChanged(&max_width_, kPropertyEffectsPreferredSizeChanged);
 }
 
+void Label::SetMaximumWidthSingleLine(int max_width) {
+  DCHECK(!GetMultiLine());
+  max_width_single_line_ = max_width;
+  OnPropertyChanged(&max_width_single_line_,
+                    kPropertyEffectsPreferredSizeChanged);
+}
+
 bool Label::GetCollapseWhenHidden() const {
   return collapse_when_hidden_;
 }
@@ -1102,6 +1109,17 @@ gfx::Size Label::GetTextSize() const {
   gfx::Size size;
   if (GetText().empty()) {
     size = gfx::Size(0, GetLineHeight());
+  } else if (max_width_single_line_ > 0) {
+    DCHECK(!GetMultiLine());
+    // Enable eliding during text width calculation. This allows the RenderText
+    // to report an accurate width given the constraints and how it determines
+    // to elide the text. If we simply clamp the width to the max after the
+    // fact, then there may be some empty space left over *after* an ellipsis.
+    full_text_->SetElideBehavior(elide_behavior_);
+    full_text_->SetDisplayRect(
+        gfx::Rect(0, 0, max_width_single_line_ - GetInsets().width(), 0));
+    size = full_text_->GetStringSize();
+    full_text_->SetElideBehavior(gfx::NO_ELIDE);
   } else {
     // Cancel the display rect of |full_text_|. The display rect may be
     // specified in GetHeightForWidth(), and specifying empty Rect cancels

ui/views/controls/label.h
@@ -187,6 +187,10 @@ class VIEWS_EXPORT Label : public View,
   int GetMaxLines() const;
   void SetMaxLines(int max_lines);
 
+  // If single-line, a non-zero value will help determine the amount of space
+  // needed *after* elision, which may be less than the passed |max_width|.
+  void SetMaximumWidthSingleLine(int max_width);
+
   // Returns the number of lines required to render all text. The actual number
   // of rendered lines might be limited by |max_lines_| which elides the rest.
   size_t GetRequiredLines() const;
@@ -460,7 +464,10 @@ class VIEWS_EXPORT Label : public View,
   // Whether to collapse the label when it's not visible.
   bool collapse_when_hidden_ = false;
   int fixed_width_ = 0;
+  // This is used only for multi-line mode.
   int max_width_ = 0;
+  // This is used in single-line mode.
+  int max_width_single_line_ = 0;
 
   std::unique_ptr<SelectionController> selection_controller_;
 

ui/views/controls/button/label_button.cc
@@ -246,6 +246,9 @@ gfx::Size LabelButton::CalculatePreferredSize() const {
   // Account for the label only when the button is not shrinking down to hide
   // the label entirely.
   if (!shrinking_down_label_) {
+    if (!label_->GetMultiLine() && max_size_.width() > 0)
+      label_->SetMaximumWidthSingleLine(max_size_.width() - size.width());
+
     const gfx::Size preferred_label_size = label_->GetPreferredSize();
     size.Enlarge(preferred_label_size.width(), 0);
     size.SetToMax(

ui/views/controls/button/label_button_unittest.cc
@@ -5,6 +5,7 @@
 #include "ui/views/controls/button/label_button.h"
 
 #include <algorithm>
+#include <string>
 #include <utility>
 
 #include "base/command_line.h"
@@ -193,7 +194,9 @@ TEST_F(LabelButtonTest, Label) {
   // Clamp the size to a maximum value.
   button_->SetText(long_text);
   button_->SetMaxSize(gfx::Size(short_text_width, 1));
-  EXPECT_EQ(button_->GetPreferredSize(), gfx::Size(short_text_width, 1));
+  const gfx::Size preferred_size = button_->GetPreferredSize();
+  EXPECT_LE(preferred_size.width(), short_text_width);
+  EXPECT_EQ(1, preferred_size.height());
 
   // Clamp the size to a minimum value.
   button_->SetText(short_text);
@@ -201,6 +204,48 @@ TEST_F(LabelButtonTest, Label) {
   button_->SetMinSize(gfx::Size(long_text_width, font_list.GetHeight() * 2));
   EXPECT_EQ(button_->GetPreferredSize(),
             gfx::Size(long_text_width, font_list.GetHeight() * 2));
+}
+
+// Tests LabelButton's usage of SetMaximumWidthSingleLine.
+TEST_F(LabelButtonTest, LabelPreferredSizeWithMaxWidth) {
+  const std::string text_cases[] = {
+      {"The"},
+      {"The quick"},
+      {"The quick brown"},
+      {"The quick brown fox"},
+      {"The quick brown fox jumps"},
+      {"The quick brown fox jumps over"},
+      {"The quick brown fox jumps over the"},
+      {"The quick brown fox jumps over the lazy"},
+      {"The quick brown fox jumps over the lazy dog"},
+  };
+
+  const int width_cases[] = {
+      10, 30, 50, 70, 90, 110, 130, 170, 200, 500,
+  };
+
+  for (bool set_image = false; button_->GetImage(Button::STATE_NORMAL).isNull();
+       set_image = true) {
+    if (set_image)
+      button_->SetImage(Button::STATE_NORMAL, CreateTestImage(16, 16));
+
+    bool preferred_size_is_sometimes_narrower_than_max = false;
+
+    for (size_t i = 0; i < base::size(text_cases); ++i) {
+      for (size_t j = 0; j < base::size(width_cases); ++j) {
+        button_->SetText(ASCIIToUTF16(text_cases[i]));
+        button_->SetMaxSize(gfx::Size(width_cases[j], 30));
+
+        const gfx::Size preferred_size = button_->GetPreferredSize();
+        EXPECT_LE(preferred_size.width(), width_cases[j]);
+
+        if (preferred_size.width() < width_cases[j])
+          preferred_size_is_sometimes_narrower_than_max = true;
+      }
+    }
+
+    EXPECT_TRUE(preferred_size_is_sometimes_narrower_than_max);
+  }
 }
 
 TEST_F(LabelButtonTest, LabelShrinkDown) {

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 981bced75fd9a76542d7472d1ba797a62ff176b5

Fix views::Label text elision performance regression.
Performance of bookmark bar labels was regressed by
30c1a362748552108893775d73faa28c7c563784.

Bug: 1176231
Change-Id: If28b8a3b35c933640dbfb941487810e1da3f7a79
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2685603
Commit-Queue: Peter Kasting <pkasting@chromium.org>
Reviewed-by: Peter Kasting <pkasting@chromium.org>
Cr-Commit-Position: refs/heads/master@{#852630}

ui/views/controls/label.cc
@@ -405,6 +405,7 @@ void Label::SetElideBehavior(gfx::ElideBehavior elide_behavior) {
   if (elide_behavior_ == elide_behavior)
     return;
   elide_behavior_ = elide_behavior;
+  UpdateFullTextElideBehavior();
   ClearDisplayText();
   OnPropertyChanged(&elide_behavior_, kPropertyEffectsPreferredSizeChanged);
 }
@@ -454,7 +455,9 @@ void Label::SetMaximumWidth(int max_width) {
 
 void Label::SetMaximumWidthSingleLine(int max_width) {
   DCHECK(!GetMultiLine());
+
   max_width_single_line_ = max_width;
+  UpdateFullTextElideBehavior();
   OnPropertyChanged(&max_width_single_line_,
                     kPropertyEffectsPreferredSizeChanged);
 }
@@ -1074,9 +1077,7 @@ void Label::Init(const base::string16& text,
   full_text_->SetCursorEnabled(false);
   full_text_->SetWordWrapBehavior(gfx::TRUNCATE_LONG_WORDS);
   full_text_->SetMinLineHeight(GetLineHeight());
-  // NOTE: |full_text_| should not be elided at all. This is used to keep
-  // some properties and to compute the size of the string.
-  full_text_->SetElideBehavior(gfx::NO_ELIDE);
+  UpdateFullTextElideBehavior();
   full_text_->SetDirectionalityMode(directionality_mode);
 
   SetText(text);
@@ -1116,11 +1117,9 @@ gfx::Size Label::GetTextSize() const {
     // to report an accurate width given the constraints and how it determines
     // to elide the text. If we simply clamp the width to the max after the
     // fact, then there may be some empty space left over *after* an ellipsis.
-    full_text_->SetElideBehavior(elide_behavior_);
     full_text_->SetDisplayRect(
         gfx::Rect(0, 0, max_width_single_line_ - GetInsets().width(), 0));
     size = full_text_->GetStringSize();
-    full_text_->SetElideBehavior(gfx::NO_ELIDE);
   } else {
     // Cancel the display rect of |full_text_|. The display rect may be
     // specified in GetHeightForWidth(), and specifying empty Rect cancels
@@ -1233,6 +1232,13 @@ void Label::BuildContextMenuContents() {
   context_menu_contents_.AddItemWithStringId(MenuCommands::kCopy, IDS_APP_COPY);
   context_menu_contents_.AddItemWithStringId(MenuCommands::kSelectAll,
                                              IDS_APP_SELECT_ALL);
+}
+
+void Label::UpdateFullTextElideBehavior() {
+  // In single line mode when a max width has been set, |full_text_| uses
+  // elision to properly calculate the text size. Otherwise, it is not elided.
+  full_text_->SetElideBehavior(max_width_single_line_ > 0 ? elide_behavior_
+                                                          : gfx::NO_ELIDE);
 }
 
 BEGIN_METADATA(Label, View)

ui/views/controls/label.h
@@ -424,6 +424,9 @@ class VIEWS_EXPORT Label : public View,
   // Builds |context_menu_contents_|.
   void BuildContextMenuContents();
 
+  // Updates the elide behavior used by |full_text_|.
+  void UpdateFullTextElideBehavior();
+
   int text_context_;
   int text_style_;
   base::Optional<int> line_height_;