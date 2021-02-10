699a1031e5e414f1b6957b81fd9d4a707068d17b
Lan Wei <lanwei@chromium.org>
2021-02-10 22:15:19 +0000

    Fix the Pointerevents IDL WPT test
    Right now, the pointerevents IDL WPT test has errors of calling
    "setPointerCapture", "releasePointerCapture" and "hasPointerCapture"
    functions on document, because these three functions are defined in
    "Element", and "Document" is not a subclass of "Element". That is why we
    see the errors, such as "FAIL Element interface: document must inherit
    property "setPointerCapture(long)" with the proper type assert_inherits:
    property "setPointerCapture" not found in prototype chain".
    
    I fixed it by changing the "document" to "document.body", which is an
    element.
    
    Bug: 1163370
    Change-Id: I2435466e76fee05235d135e6181a566c898a5855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2685361
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852833}

--- third_party/blink/web_tests/external/wpt/pointerevents/idlharness.window.js
+++ third_party/blink/web_tests/external/wpt/pointerevents/idlharness.window.js
@@ -13,7 +13,7 @@ idl_test(
   idl_array => {
     idl_array.add_objects({
       Document: ['document'],
-      Element: ['document'],
+      Element: ['document.body'],
       Window: ['window'],
       Navigator: ['navigator'],
       PointerEvent: ['new PointerEvent("type")']
