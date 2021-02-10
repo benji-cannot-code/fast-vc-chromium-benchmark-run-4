095bfe93d9f7fe8404a7fb47fdb5b676a981d3cb
Yeunjoo Choi <ychoi@igalia.com>
2021-02-10 23:23:19 +0000

    Reland "Move //chrome/browser/{chromeos => ash}/system"
    This is a reland of 612daee53a0c8a3aa91881e5eb799e752f0586c0
    
    Original change's description:
    > Move //chrome/browser/{chromeos => ash}/system
    >
    > This CL is part of the Chrome OS source code directory migration:
    > https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    >
    > Bug: 1164001
    > Change-Id: I93e71672b03a73da5b193d43553f6f7ad6ad0fce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683654
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#852435}
    
    Bug: 1164001
    Change-Id: I971360a29e9e383c93ae1eccda1dc0c0e86a5d91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2686058
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: sebsg <sebsg@chromium.org>
    Auto-Submit: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852870}

--- chrome/browser/chromeos/crostini/crosvm_process_list.h
+++ chrome/browser/chromeos/crostini/crosvm_process_list.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_map>
 
 #include "base/files/file_path.h"
-#include "chrome/browser/chromeos/system/procfs_util.h"
+#include "chrome/browser/ash/system/procfs_util.h"
 
 namespace crostini {
 
