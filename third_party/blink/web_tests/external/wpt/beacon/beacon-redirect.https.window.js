4695b00fa20e3601046b7367224e4cfdde6bd111
Yutaka Hirano <yhirano@chromium.org>
2021-02-10 06:51:04 +0000

    Reorganize and augment beacon wpt
     - Run tests on secure contexts. I'm planning to use the sec-fetch-mode
       header once WebKit supports it, which is only available on secure
       contexts.
     - Replace html files with window.js files.
     - Merge many beacon-basic-* files into beacon-basic.https.window.js.
       This is doable because the payload size limit is per context, not
       per page.
     - Merge beacon-readablestream.js and beacon-error.sub.window.js into
       beacon-basic.https.window.js.
     - Merge beacon-preflight-failure.sub.window.js into
       beacon-cors.https.window.js.
     - Add more cross origin tests in beacon-cors.https.window.js.
     - Slim down beacon-common.sub.js.
    
    Also remove some test files in http/tests/sendbeacon, because they are
    covered by wpt/beacon.
    
    Bug: 720303
    Change-Id: Id65c70d290753428d5d17bc8e9e783969ff8bc16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2677465
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852515}

---
+++ third_party/blink/web_tests/external/wpt/beacon/beacon-redirect.https.window.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: timeout=long
+// META: script=/common/get-host-info.sub.js
+// META: script=/common/utils.js
+// META: script=beacon-common.sub.js
+
+'use strict';
+
+const {ORIGIN} = get_host_info();
+
+// Execute each sample test per redirect status code.
+// Note that status codes 307 and 308 are the only codes that will maintain POST
+// data through a redirect.
+for (const status of [307, 308]) {
+  for (const type of [STRING, ARRAYBUFFER, FORM, BLOB]) {
+    parallelPromiseTest(async (t) => {
+      const iframe = document.createElement('iframe');
+      document.body.appendChild(iframe);
+      t.add_cleanup(() => iframe.remove());
+
+      const payload = makePayload(SMALL, type);
+      const id = token();
+      const destination =
+          `${ORIGIN}/beacon/resources/beacon.py?cmd=store&id=${id}`;
+      const url = `${ORIGIN}/common/redirect.py` +
+          `?status=${status}&location=${encodeURIComponent(destination)}`;
+
+      assert_true(iframe.contentWindow.navigator.sendBeacon(url, payload));
+      iframe.remove();
+
+      await waitForResult(id);
+    }, `cross-origin, CORS-safelisted: status = ${status}, type = ${type}`);
+  }
+};
+
+done();
