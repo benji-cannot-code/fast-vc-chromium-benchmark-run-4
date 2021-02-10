3cf8615f120a128c78a43caefd1e95c34300c31c
Sebastien Marchand <sebmarchand@chromium.org>
2021-02-10 16:58:22 +0000

    [battery] Power UKMs - track the time spent playing fullscreen video
    Bug: 1153193
    Change-Id: I22125e0d484b594b0054f372120047f404d7dead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679293
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Oliver Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852678}

--- chrome/browser/metrics/usage_scenario/tab_usage_scenario_tracker.h
+++ chrome/browser/metrics/usage_scenario/tab_usage_scenario_tracker.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/tab_stats/tab_stats_observer.h"
 #include "chrome/browser/metrics/usage_scenario/usage_scenario_tracker.h"
 #include "content/public/browser/visibility.h"
+#include "ui/display/display_observer.h"
 
 namespace content {
 class WebContents;
@@ -19,7 +20,8 @@ namespace metrics {
 
 // Used to relay information gathered from TabStatsTracker to
 // UsageScenarioDataStore. No information is stored in this class.
-class TabUsageScenarioTracker : public TabStatsObserver {
+class TabUsageScenarioTracker : public TabStatsObserver,
+                                public display::DisplayObserver {
  public:
   // This class will not own |usage_scenario_data_store| so it needs to be
   // outlived by it.
@@ -36,6 +38,12 @@ class TabUsageScenarioTracker : public TabStatsObserver {
   void OnTabVisibilityChanged(content::WebContents* web_contents,
                               content::Visibility visibility) override;
   void OnTabInteraction(content::WebContents* web_contents) override;
+  void OnMediaEffectivelyFullscreenChanged(content::WebContents* web_contents,
+                                           bool is_fullscreen) override;
+
+  // display::DisplayObserver:
+  void OnDisplayAdded(const display::Display& new_display) override;
+  void OnDisplayRemoved(const display::Display& new_display) override;
 
   // Testing trampolines:
   void OnTabAddedForTesting(content::WebContents* web_contents,
@@ -53,6 +61,9 @@ class TabUsageScenarioTracker : public TabStatsObserver {
 
   // Keep track of the visible web-contents.
   base::flat_set<content::WebContents*> visible_contents_;
+
+  // Indicates if there's a media currently playing fullscreen.
+  bool media_playing_fullscreen_ = false;
 
   // Used to verify that all access to |usage_scenario_data_store_| goes through
   // the same sequence as the one that created this object.
