87fe28d82b1a32a2e7e61463e8bf38936f13e97c
Michael Bai <michaelbai@chromium.org>
2021-02-10 22:11:52 +0000

    Autofill: Add query result delay by command line
    Add command line to delay propagating the query result for testing
    
    This helps Android autofill service to test the code path that
    predication arrives after autofill starts.
    
    Test: Manally add command line and verify it works.
    
    Bug: 1169367, 1176816
    Change-Id: I8daded92fe26921d410cfa1b2fdea0134309ea44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2686480
    Commit-Queue: Michael Bai <michaelbai@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852831}

--- components/autofill/core/browser/autofill_handler.h
+++ components/autofill/core/browser/autofill_handler.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/cancelable_callback.h"
 #include "base/compiler_specific.h"
 #include "base/scoped_observation.h"
 #include "base/time/time.h"
@@ -336,6 +337,9 @@ class AutofillHandler
   // |form_structures|.
   void OnFormsParsed(const std::vector<const FormData*>& forms);
 
+  void PropagateAutofillPredictionsToDriver(
+      const std::vector<FormStructure*>& forms);
+
   // Provides driver-level context to the shared code of the component. Must
   // outlive this object.
   AutofillDriver* const driver_;
@@ -369,6 +373,10 @@ class AutofillHandler
 
   // Tracks whether or not rich query encoding is enabled for this client.
   const bool is_rich_query_enabled_ = false;
+
+  // Task to delay propagate the query result to driver for testing.
+  base::CancelableOnceCallback<void(const std::vector<FormStructure*>&)>
+      query_result_delay_task_;
 
   // Will be not null only for |SaveCardBubbleViewsFullFormBrowserTest|.
   ObserverForTest* observer_for_testing_ = nullptr;
