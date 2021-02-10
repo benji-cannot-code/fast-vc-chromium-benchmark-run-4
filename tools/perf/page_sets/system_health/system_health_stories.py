1422a50db1500155605bb8a67644176496102066
Tamer Tas <tmrts@chromium.org>
2021-02-10 15:31:09 +0000

    [perf] Create a system health benchmark for benchmarking PCScan
    R=johnchen@chromium.org
    CC=mlippautz@chromium.org,bikineev@chromium.org
    
    Bug: v8:11180
    Change-Id: I30e3f25694826aeef6146d7de900cbdb2b77c127
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2565577
    Auto-Submit: Tamer Tas <tmrts@chromium.org>
    Commit-Queue: Tamer Tas <tmrts@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852643}

--- tools/perf/page_sets/system_health/system_health_stories.py
+++ tools/perf/page_sets/system_health/system_health_stories.py
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import os
 
-from page_sets.system_health import chrome_stories
+from page_sets.system_health import browsing_stories, chrome_stories
 from page_sets.system_health import platforms
 from page_sets.system_health import story_tags
 from page_sets.system_health import system_health_story
@@ -99,3 +99,11 @@ def IterAllSystemHealthStoryClasses():
       top_level_dir=os.path.dirname(start_dir),
       base_class=system_health_story.SystemHealthStory).iteritems()):
     yield cls
+
+
+class SystemHealthPCScanStorySet(story.StorySet):
+  """A story set containing stories for benchmarking PCScan feature."""
+
+  def __init__(self, take_memory_measurement=False):
+    super(SystemHealthPCScanStorySet, self).__init__()
+    self.AddStory(browsing_stories.CnnStory2020(self, take_memory_measurement))
