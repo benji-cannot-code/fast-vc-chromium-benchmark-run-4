86083a5b7fc77d8e8233c59fbb143f43df4917a1
Mason Freed <masonfreed@chromium.org>
2021-02-10 22:32:24 +0000

    Implement the beginnings of "light dismiss" behavior for <popup>
    With this CL, clicking outside a popup will dismiss that popup,
    along with all others in the popup stack. Clicking within a
    popup will preserve that popup in the visible state, but will
    hide any popups above that popup in the stack.
    
    Bug: 1168738
    Change-Id: Ibb8fc90ba19929a04d746fbf092001f546fb1e75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683092
    Auto-Submit: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Ionel Popescu <iopopesc@microsoft.com>
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852844}

--- third_party/blink/renderer/core/html/html_popup_element.h
+++ third_party/blink/renderer/core/html/html_popup_element.h
@@ -35,6 +35,10 @@ class HTMLPopupElement final : public HTMLElement {
   void ScheduleHideEvent();
   void MarkStyleDirty();
 
+  void PushNewPopupElement(HTMLPopupElement*);
+  void PopPopupElement(HTMLPopupElement*);
+  HTMLPopupElement* TopmostPopupElement();
+
   bool open_;
 };
 
