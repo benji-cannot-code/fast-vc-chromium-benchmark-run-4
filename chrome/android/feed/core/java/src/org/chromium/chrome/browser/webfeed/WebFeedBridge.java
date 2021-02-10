1074418896028fa8db79cf8d0898dc9f7ef60781
Sophey Dong <sophey@chromium.org>
2021-02-10 03:31:00 +0000

    [WebFeed] Add skeleton bridge class for WebFeed.
    Also add a Following string to the menu.
    
    Bug: 1152592
    Change-Id: I9407c64b419c16a9a7c423708d2da3bd95d3c8fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2674947
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852455}

---
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/webfeed/WebFeedBridge.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.webfeed;
+
+import org.chromium.url.GURL;
+
+/**
+ * Collection of methods used across Web Feed follow entry points.
+ */
+public class WebFeedBridge {
+    /**
+     * Returns whether the given {@link GURL} is followed.
+     */
+    public boolean isFollowed(GURL url) {
+        return false;
+    }
+}
