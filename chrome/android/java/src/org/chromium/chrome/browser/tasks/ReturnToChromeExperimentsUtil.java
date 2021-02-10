7f775d53aa3a4a68c56feede33b42928ffa69dc5
Xi Han <hanxi@google.com>
2021-02-10 16:13:12 +0000

    [Start] Fix URL for new tab created with Omnibox focused.
    In CL https://crrev.com/c/2635448, we set an empty URL to the new tab
    created when focusing on Omnibox is enabled. However, this breaks
    features like AutoComplete which treats chrome://newtab differently. In
    this CL, we modify LocationBarModel#getCurrentUrl(), so it will return
    chrome://newtab in this scenario. We can't set the URL of the newly
    created tab directly to be chrome://newtab. This is because a NTP will
    be created rather than an empty tab.
    Before this CL:
    https://drive.google.com/file/d/1pk11KJfSx1lBcjwDgy0ZMMA7nwJ83lxi/view?usp=sharing
    After this CL:
    https://drive.google.com/file/d/1jfaftwdfxVPQOqjkMa0ogNMFbHbS83nQ/view?usp=sharing
    
    Bug: 1164996
    Change-Id: I6b69fc196130fe641bc8590708864dcf4ff4f70a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676207
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852655}

--- chrome/android/java/src/org/chromium/chrome/browser/tasks/ReturnToChromeExperimentsUtil.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tasks/ReturnToChromeExperimentsUtil.java
@@ -246,6 +246,7 @@ public final class ReturnToChromeExperimentsUtil {
         if (focusOnOmnibox && tab != null) {
             tab.addObserver(observer);
             StartSurfaceUserData.setFocusOnOmnibox(tab, true);
+            StartSurfaceUserData.setCreatedAsNtp(tab);
         }
 
         if (params.getTransitionType() == PageTransition.AUTO_BOOKMARK) {
