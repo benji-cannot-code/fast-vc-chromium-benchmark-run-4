e50b8a3cd5b3d9daf3e389eec4fdcbe4b55b52d1
Jesse Schettler <jschettler@chromium.org>
2021-02-10 01:22:25 +0000

    arc: Resize NativeView-based CCT modal dialogs
    Update ArcCustomTabModalDialogHost to notify modal dialogs when the
    WebContents changes size. This ensures that modal dialogs hosted by
    NativeView-based Custom Tabs are resized when the Custom Tab is resized.
    
    Bug: b:141468069
    Test: Print from and Android app and resize the app window
    Change-Id: I3f6fbfb772f26a79c23c94ebcb2c2548a60d4eac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2686093
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852406}

--- chrome/browser/ui/ash/arc_custom_tab_modal_dialog_host.cc
+++ chrome/browser/ui/ash/arc_custom_tab_modal_dialog_host.cc
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 ArcCustomTabModalDialogHost::ArcCustomTabModalDialogHost(
     std::unique_ptr<arc::CustomTab> custom_tab,
     content::WebContents* web_contents)
-    : custom_tab_(std::move(custom_tab)), web_contents_(web_contents) {
+    : WebContentsObserver(web_contents),
+      custom_tab_(std::move(custom_tab)),
+      web_contents_(web_contents) {
   // Attach any required WebContents helpers. Browser tabs automatically get
   // them attached in TabHelpers::AttachTabHelpers.
   web_modal::WebContentsModalDialogManager::CreateForWebContents(web_contents_);
@@ -25,7 +27,15 @@ ArcCustomTabModalDialogHost::ArcCustomTabModalDialogHost(
       ->SetDelegate(this);
 }
 
-ArcCustomTabModalDialogHost::~ArcCustomTabModalDialogHost() = default;
+ArcCustomTabModalDialogHost::~ArcCustomTabModalDialogHost() {
+  for (auto& observer : observer_list_)
+    observer.OnHostDestroying();
+}
+
+void ArcCustomTabModalDialogHost::MainFrameWasResized(bool width_changed) {
+  for (auto& observer : observer_list_)
+    observer.OnPositionRequiresUpdate();
+}
 
 web_modal::WebContentsModalDialogHost*
 ArcCustomTabModalDialogHost::GetWebContentsModalDialogHost() {
@@ -46,7 +56,11 @@ gfx::Size ArcCustomTabModalDialogHost::GetMaximumDialogSize() {
 }
 
 void ArcCustomTabModalDialogHost::AddObserver(
-    web_modal::ModalDialogHostObserver* observer) {}
+    web_modal::ModalDialogHostObserver* observer) {
+  observer_list_.AddObserver(observer);
+}
 
 void ArcCustomTabModalDialogHost::RemoveObserver(
-    web_modal::ModalDialogHostObserver* observer) {}
+    web_modal::ModalDialogHostObserver* observer) {
+  observer_list_.RemoveObserver(observer);
+}
