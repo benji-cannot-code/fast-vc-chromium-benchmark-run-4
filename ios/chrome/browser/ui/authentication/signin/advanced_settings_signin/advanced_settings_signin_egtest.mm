cc260e93c4bf7eb3318c8e606a49e9ba6bccd8c4
Jérôme Lebel <jlebel@chromium.org>
2021-02-10 12:52:16 +0000

    [iOS] Fix EG flakness in AdvancedSettingsSigninTestCase
    testInterruptSigninFromTabSwitcherFromAdvancedSigninSettings fails with
    iPhone 8 Plus when 3 tests run before.
    
    The following methods:
    
    +[SigninEarlGreyUI scrollToPrimarySignInButtonInRecentTabs]
    +[SigninEarlGreyUI collapseRecentlyClosedTabsIfSigninPromoNotVisible]
    
    are replaced by:
    
    // Opens the recent tabs and tap in the primary sign-in button.
    +[SigninEarlGreyUI tapPrimarySignInButtonInRecentTabs]
    // Opens the tab switcher and tap in the primary sign-in button.
    +[SigninEarlGreyUI tapPrimarySignInButtonInTabSwitcher]
    
    Fixed: 1131479
    Change-Id: I556ab4fbb06ba10b84642cfc7152b6c8cc89ba78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2684900
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852603}

--- ios/chrome/browser/ui/authentication/signin/advanced_settings_signin/advanced_settings_signin_egtest.mm
+++ ios/chrome/browser/ui/authentication/signin/advanced_settings_signin/advanced_settings_signin_egtest.mm
@@ -139,10 +139,7 @@ const NSTimeInterval kSyncOperationTimeout = 5.0;
   FakeChromeIdentity* fakeIdentity = [SigninEarlGrey fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:fakeIdentity];
 
-  [ChromeEarlGreyUI openToolsMenu];
-  [ChromeEarlGreyUI
-      tapToolsMenuButton:chrome_test_util::RecentTabsMenuButton()];
-  [SigninEarlGreyUI scrollToPrimarySignInButtonInRecentTabs];
+  [SigninEarlGreyUI tapPrimarySignInButtonInRecentTabs];
   [SigninEarlGreyUI tapSettingsLink];
   [ChromeEarlGreyUI waitForAppToIdle];
 
@@ -159,14 +156,7 @@ const NSTimeInterval kSyncOperationTimeout = 5.0;
   FakeChromeIdentity* fakeIdentity = [SigninEarlGrey fakeIdentity1];
   [SigninEarlGrey addFakeIdentity:fakeIdentity];
 
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::ShowTabsButton()]
-      performAction:grey_tap()];
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::
-                                          TabGridOtherDevicesPanelButton()]
-      performAction:grey_tap()];
-
-  [SigninEarlGreyUI collapseRecentlyClosedTabsIfSigninPromoNotVisible];
-  [SigninEarlGreyUI scrollToPrimarySignInButtonInRecentTabs];
+  [SigninEarlGreyUI tapPrimarySignInButtonInTabSwitcher];
   [SigninEarlGreyUI tapSettingsLink];
   [ChromeEarlGreyUI waitForAppToIdle];
 
