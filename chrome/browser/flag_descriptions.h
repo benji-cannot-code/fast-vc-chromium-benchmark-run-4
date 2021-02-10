2a4dbce6bafcc9d26687f0e604ae01cb5b686587
Mustafa Camurcu <camurcu@google.com>
2021-02-10 19:00:40 +0000

    arc-r: Implement USAP for ARC-R
    This creates a flag that enables Android feature
    Unspecialized Application Processes which creates a pool of
    processes that are used for launching applications so that the
    Zygote process doesn't have to wake.
    It can only be enabled on high memory devices (>4MB).
    
    BUG=b:176926293
    TEST=Manually launched app and saw correct logs being printed.
    TEST=components_unittests --gtest_filter="*ArcVmClientAdapterUsapProfileTest.Profile*" --ozone-platform=headless
    
    Change-Id: I45844e94d839340b1bfdd1199c94bd55165f4ad0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2655006
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Mustafa Camurcu <camurcu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852734}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2207,6 +2207,9 @@ extern const char kArcNativeBridge64BitSupportExperimentDescription[];
 extern const char kArcUseHighMemoryDalvikProfileName[];
 extern const char kArcUseHighMemoryDalvikProfileDesc[];
 
+extern const char kArcEnableUsapName[];
+extern const char kArcEnableUsapDesc[];
+
 extern const char kArcUsbHostName[];
 extern const char kArcUsbHostDescription[];
 
