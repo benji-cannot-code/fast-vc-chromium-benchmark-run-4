031510d60e258483392028e2f8e4f9865e5f4f88
David Black <dmblack@google.com>
2021-02-10 19:13:25 +0000

    Fix label mask when closing holding space bubble.
    When a holding space item chip view is hovered over, it shows a pin
    toggle and masks out its label to avoid overlap. This is accomplished
    via layer mask.
    
    When the holding space bubble is closed, it fades out. Unfortunately
    the mask layer for the holding space item chip view is immediately
    destroyed so the label and pin toggle in the holding space item chip
    view overlap as the UI fades away.
    
    This CL addresses the issue by manually masking label when needed
    instead of relying on a mask layer.
    
    
    Bug: 1176536
    Change-Id: If3d3a349be723bf3776eccc68c0ce171ca0fa0a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2686071
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852739}

--- ash/system/holding_space/holding_space_item_chip_view.h
+++ ash/system/holding_space/holding_space_item_chip_view.h
@@ -13,10 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/holding_space/holding_space_item_view.h"
 #include "ui/views/metadata/metadata_header_macros.h"
 
-namespace ui {
-class LayerOwner;
-}  // namespace ui
-
 namespace views {
 class Label;
 }  // namespace views
@@ -45,13 +41,16 @@ class ASH_EXPORT HoldingSpaceItemChipView : public HoldingSpaceItemView {
   void OnHoldingSpaceItemUpdated(const HoldingSpaceItem* item) override;
   void OnPinVisiblityChanged(bool pin_visible) override;
 
+  // Invoked during `label_`'s paint sequence to paint its optional mask. Note
+  // that `label_` is only masked when `pin_` is visible to avoid overlapping.
+  void OnPaintLabelMask(gfx::Canvas* canvas);
+
   void UpdateImage();
 
-  std::unique_ptr<ui::LayerOwner> label_mask_layer_owner_;
-
+  // Owned by view hierarchy.
   RoundedImageView* image_ = nullptr;
   views::Label* label_ = nullptr;
-  views::View* label_and_pin_button_container_ = nullptr;
+  views::View* pin_ = nullptr;
 
   base::CallbackListSubscription image_subscription_;
 };
--- ash/system/holding_space/holding_space_util.cc
+++ ash/system/holding_space/holding_space_util.cc
@@ -76,9 +76,7 @@ void AnimateOut(views::View* view,
             observer);
 }
 
-std::unique_ptr<views::Label> CreateLabel(LabelStyle style,
-                                          const base::string16& text) {
-  auto label = std::make_unique<views::Label>(text);
+void ApplyStyle(views::Label* label, LabelStyle style) {
   label->SetAutoColorReadabilityEnabled(false);
   label->SetEnabledColor(AshColorProvider::Get()->GetContentLayerColor(
       AshColorProvider::ContentLayerType::kTextColorPrimary));
@@ -101,7 +99,12 @@ std::unique_ptr<views::Label> CreateLabel(LabelStyle style,
                                        gfx::Font::Weight::MEDIUM));
       break;
   }
+}
 
+std::unique_ptr<views::Label> CreateLabel(LabelStyle style,
+                                          const base::string16& text) {
+  auto label = std::make_unique<views::Label>(text);
+  ApplyStyle(label.get(), style);
   return label;
 }
 
