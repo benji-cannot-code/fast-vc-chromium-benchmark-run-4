59b1a91b84cc0317fa68a797afc72496275a462e
Theresa Sullivan <twellington@chromium.org>
2021-02-10 02:07:15 +0000

    🤠 Disable flaky EarlyTraceEventTest#testCanRecordEvent
    Bug: 1173968
    Change-Id: I4218265aaecda0503ddf848ad4168ab1f27e2b8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683557
    Owners-Override: Theresa  <twellington@chromium.org>
    Auto-Submit: Theresa  <twellington@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852418}

--- base/android/javatests/src/org/chromium/base/EarlyTraceEventTest.java
+++ base/android/javatests/src/org/chromium/base/EarlyTraceEventTest.java
@@ -19,6 +19,7 @@ import org.chromium.base.EarlyTraceEvent.AsyncEvent;
 import org.chromium.base.EarlyTraceEvent.Event;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 
 import java.util.ArrayList;
@@ -54,6 +55,7 @@ public class EarlyTraceEventTest {
     @Test
     @SmallTest
     @Feature({"Android-AppBase"})
+    @DisabledTest(message = "https://crbug.com/1173968")
     public void testCanRecordEvent() {
         EarlyTraceEvent.enable();
         long myThreadId = Process.myTid();
