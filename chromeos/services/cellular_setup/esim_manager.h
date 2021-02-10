1472072f6d0e963d5d661a966fca6c13de9d6e34
Azeem Arshad <azeemarshad@chromium.org>
2021-02-10 19:52:47 +0000

    [CrOS Cellular] Add Inhibitor in Euicc mojo methods
    This CL wraps profile installation and request pending
    profiles methods in Euicc objects with inhibitor logic.
    
    Bug: 1093185
    Change-Id: Iadd45c967fe00aadc3323c8b51cd96f5c63b4804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2685301
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852758}

--- chromeos/services/cellular_setup/esim_manager.h
+++ chromeos/services/cellular_setup/esim_manager.h
@@ -23,6 +23,7 @@ class ObjectPath;
 namespace chromeos {
 
 class CellularESimUninstallHandler;
+class CellularInhibitor;
 
 namespace cellular_setup {
 
@@ -40,7 +41,8 @@ class ESimManager : public mojom::ESimManager,
  public:
   ESimManager();
   ESimManager(CellularESimProfileHandler* cellular_esim_profile_handler,
-              CellularESimUninstallHandler* cellular_esim_uninstall_handler);
+              CellularESimUninstallHandler* cellular_esim_uninstall_handler,
+              CellularInhibitor* cellular_inhibitor);
   ESimManager(const ESimManager&) = delete;
   ESimManager& operator=(const ESimManager&) = delete;
   ~ESimManager() override;
@@ -73,6 +75,8 @@ class ESimManager : public mojom::ESimManager,
     return cellular_esim_uninstall_handler_;
   }
 
+  CellularInhibitor* cellular_inhibitor() { return cellular_inhibitor_; }
+
  private:
   void UpdateAvailableEuiccs();
   // Removes Euicc objects in |available_euiiccs_| that are not in
@@ -85,6 +89,7 @@ class ESimManager : public mojom::ESimManager,
 
   CellularESimProfileHandler* cellular_esim_profile_handler_;
   CellularESimUninstallHandler* cellular_esim_uninstall_handler_;
+  CellularInhibitor* cellular_inhibitor_;
   std::vector<std::unique_ptr<Euicc>> available_euiccs_;
   mojo::RemoteSet<mojom::ESimManagerObserver> observers_;
   mojo::ReceiverSet<mojom::ESimManager> receivers_;
