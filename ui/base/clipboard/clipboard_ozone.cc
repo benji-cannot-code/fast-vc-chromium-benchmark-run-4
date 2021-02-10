db662d2a94c0752fd4a17ec1ffb530e344f9cbd9
Joel Hockey <joelhockey@chromium.org>
2021-02-10 20:25:28 +0000

    Ozone StubPlatformClipboard takes ownership after offer
    The stub clipboard is changed to take and keep ownership of buffers
    passed in OfferClipboardData(). This allows ClipboardOzone to return
    any existing data in ReadClipboardDataAndWait().
    
    It is fine for stub to keep ownership forever and always return the
    latest internal data since the reason we are using the stub is that we
    couldn't use the platform clipboard.
    
    This change is required for unittests in crrev.com/c/2646768.
    
    Bug: 1175483
    Change-Id: I6e156e665d1ed9239b97ff181bb4d36f9146a71a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2684142
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852772}

--- ui/base/clipboard/clipboard_ozone.cc
+++ ui/base/clipboard/clipboard_ozone.cc
@@ -45,7 +45,10 @@ constexpr base::TimeDelta kRequestTimeout = base::TimeDelta::FromSeconds(10);
 
 // Depending on the backend, the platform clipboard may or may not be
 // available.  Should it be absent, we provide a dummy one.  It always calls
-// back immediately with empty data, and denies ownership of any buffer.
+// back immediately with empty data. It starts without ownership of any buffers
+// but will take and keep ownership after a call to OfferClipboardData(). By
+// taking ownership, we allow ClipboardOzone to return existing data in
+// ReadClipboardDataAndWait().
 class StubPlatformClipboard : public PlatformClipboard {
  public:
   StubPlatformClipboard() = default;
@@ -56,6 +59,7 @@ class StubPlatformClipboard : public PlatformClipboard {
       ClipboardBuffer buffer,
       const PlatformClipboard::DataMap& data_map,
       PlatformClipboard::OfferDataClosure callback) override {
+    is_owner_[buffer] = true;
     std::move(callback).Run();
   }
   void RequestClipboardData(
@@ -70,10 +74,15 @@ class StubPlatformClipboard : public PlatformClipboard {
       PlatformClipboard::GetMimeTypesClosure callback) override {
     std::move(callback).Run({});
   }
-  bool IsSelectionOwner(ClipboardBuffer buffer) override { return false; }
+  bool IsSelectionOwner(ClipboardBuffer buffer) override {
+    return is_owner_[buffer];
+  }
   void SetSequenceNumberUpdateCb(
       PlatformClipboard::SequenceNumberUpdateCb cb) override {}
   bool IsSelectionBufferAvailable() const override { return false; }
+
+ private:
+  base::flat_map<ClipboardBuffer, bool> is_owner_;
 };
 
 }  // namespace
