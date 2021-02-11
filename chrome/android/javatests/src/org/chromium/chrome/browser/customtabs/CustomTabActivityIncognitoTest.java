3862df9b30e5823fbd9ac811bc78c6f557bf329c
Rohit Agarwal <roagarwal@chromium.org>
2021-02-11 18:19:11 +0000

    Make IncognitoCustomTabIntentDataProvider#isIncognito as single source of truth for Incognito CCT.
    IncognitoCustomTabIntentDataProvider#isValidIncognitoIntent which is a
    static method that acts as the primary body that verifies who can fire
    Incognito CCT. If all checks pass we create the
    IncognitoCustomTabIntentDataProvider instance.
    
    In all places of the custom tabs codebase we rely on this
    BrowserServicesIntentDataProvider to tell if a CCT is Incognito or not
    "except" in CustomTabActivityTabController#takeWebContents that is
    responsible for creating WebContents either for IncognitoCCT or a
    RegularCCT. There we instead relied on
    CustomTabIncognitoManager#isEnabledIncognitoCCT which delegates the
    check to IncognitoCustomTabIntentDataProvider but also adds "one" more
    extra check on Feature flag which is a problem.
    
    1P App experiments override the IncognitoCustomTabIntentDataProvider#
    isValidIncognitoIntent (to circumvent the feature check which was
    disabled in M88) that gives the IncognitoCCT UI but due to
    CustomTabIncognitoManager#isEnabledIncognitoCCT returning false we used
    a primary OTR profile instead.
    
    Primary OTR profile because before we enabled the np-OTR profile support
    for Incognito CCT, payments were using Incognito CCT but with primary
    OTR profile.
    
    
    This CL:
    
    a) Makes IncognitoCustomTabIntentDataProvider#isIncognito the
    primary source of truth.
    
    b) Remove the notion of primary OTR completely for Incognito CCT as
    payments don't rely on them since M86.
    
    c) Also adds non primary OTR profile check in tests.
    
    Bug: 1177349
    Change-Id: I668a1f38694b3a87088fdde7515f818e8ba4d20d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690628
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Side Yilmaz <sideyilmaz@google.com>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853189}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityIncognitoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityIncognitoTest.java
@@ -16,6 +16,7 @@ import static org.junit.Assert.assertTrue;
 
 import static org.chromium.chrome.browser.customtabs.CustomTabsTestUtils.addActionButtonToIntent;
 import static org.chromium.chrome.browser.customtabs.CustomTabsTestUtils.createTestBitmap;
+import static org.chromium.chrome.browser.customtabs.IncognitoCustomTabIntentDataProvider.EXTRA_FORCE_ENABLE_FOR_EXPERIMENT;
 
 import android.annotation.TargetApi;
 import android.app.NotificationManager;
@@ -165,6 +166,15 @@ public class CustomTabActivityIncognitoTest {
         return mCustomTabActivityTestRule.getActivity();
     }
 
+    private void assertProfileUsedIsNonPrimary() throws TimeoutException {
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            Profile profile = Profile.fromWebContents(
+                    mCustomTabActivityTestRule.getActivity().getCurrentWebContents());
+            assertTrue(profile.isOffTheRecord());
+            assertFalse(profile.isPrimaryOTRProfile());
+        });
+    }
+
     @Test
     @MediumTest
     @Features.EnableFeatures({ChromeFeatureList.CCT_INCOGNITO})
@@ -172,6 +182,7 @@ public class CustomTabActivityIncognitoTest {
         Intent intent = createMinimalIncognitoCustomTabIntent();
         CustomTabActivity activity = launchIncognitoCustomTab(intent);
         assertTrue(activity.getActivityTab().isIncognito());
+        assertProfileUsedIsNonPrimary();
     }
 
     @Test
@@ -246,10 +257,10 @@ public class CustomTabActivityIncognitoTest {
     @Features.DisableFeatures({ChromeFeatureList.CCT_INCOGNITO})
     public void canLaunchFirstPartyIncognitoWithExtraWhenDisabled() throws Exception {
         Intent intent = createMinimalIncognitoCustomTabIntent();
-        intent.putExtra(
-                IncognitoCustomTabIntentDataProvider.EXTRA_FORCE_ENABLE_FOR_EXPERIMENT, true);
+        intent.putExtra(EXTRA_FORCE_ENABLE_FOR_EXPERIMENT, true);
         CustomTabActivity activity = launchIncognitoCustomTab(intent);
         assertTrue(activity.getActivityTab().isIncognito());
+        assertProfileUsedIsNonPrimary();
     }
 
     @Test
