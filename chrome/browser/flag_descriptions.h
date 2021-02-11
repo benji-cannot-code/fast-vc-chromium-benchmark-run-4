228fd56f643cb8428a26f8d60a6b6097dfe7817f
Cheick Cisse <cheickcisse@google.com>
2021-02-11 02:23:26 +0000

    Feature flag for link to text UI revamp
    This CL adds a feature flag for the link to text UI revamp on android.
    
    Bug: 1176278
    Change-Id: Ie1f586d4a0571891a77f01da319cac458a5e12dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2684934
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852949}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1523,6 +1523,9 @@ extern const char kEnableVulkanDescription[];
 extern const char kSharedHighlightingUseBlocklistName[];
 extern const char kSharedHighlightingUseBlocklistDescription[];
 
+extern const char kPreemtiveLinkToTextGenerationName[];
+extern const char kPreemtiveLinkToTextGenerationDescription[];
+
 // Android --------------------------------------------------------------------
 
 #if defined(OS_ANDROID)
--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -102,6 +102,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &features::kMetricsSettingsAndroid,
     &features::kNetworkServiceInProcess,
     &features::kPredictivePrefetchingAllowedOnAllConnectionTypes,
+    &features::kPreemtiveLinkToTextGeneration,
     &features::kPrivacyReorderedAndroid,
     &features::kPrivacySandboxSettings,
     &features::kPrioritizeBootstrapTasks,
