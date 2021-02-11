df5d39dd57a38457a26ed677a6d38abbe183cf17
Aaron Colwell <acolwell@chromium.org>
2021-02-11 21:51:52 +0000

    Move all SiteURL and lock URL computations to SiteInfo.
    This change moves all site URL and lock URL generation out of
    SiteInstanceImpl and into SiteInfo. This allows all details related
    to the creation of these URLs to be hidden from other classes. Tests
    were updated to use the new methods. ProcessLock creation was modified
    slightly to make it easier to hide threading concerns related to
    ProcessLocks and their related SiteInfos. A test was also added to verify
    that ProcessLocks created on different threads match even though the
    underlying SiteInfos do no. This should help prevent this behavior from
    accidentally regressing in the future. There should not be any
    user visible behavior changes introduced by this patch. It is primarily
    just moving code around and renaming things.
    
    Bug: 1085275
    Change-Id: I46794960f2bb669c2e6e2750e139dc25cd03ae84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2680068
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Aaron Colwell <acolwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853243}

--- content/browser/webui/web_ui_navigation_browsertest.cc
+++ content/browser/webui/web_ui_navigation_browsertest.cc
@@ -50,7 +50,7 @@ content::mojom::OpenURLParamsPtr CreateOpenURLParams(const GURL& url) {
 
 bool DoesURLRequireDedicatedProcess(const IsolationContext& isolation_context,
                                     const GURL& url) {
-  return SiteInstanceImpl::ComputeSiteInfoForTesting(isolation_context, url)
+  return SiteInfo::CreateForTesting(isolation_context, url)
       .RequiresDedicatedProcess(isolation_context);
 }
 
