8bd6d2e76010727a5fc941313a389e6035491e21
Morten Stenshorne <mstensho@chromium.org>
2021-02-11 19:48:47 +0000

    Don't fall back the entire document to legacy at table part.
    When detecting that some subtree needs legacy fallback, we'd end up
    falling the entire document back to legacy layout, instead of just some
    subtree. This happened with LayoutNGTable enabled. It is true that
    tables need some additional marking, but limit this to the bare minimum,
    i.e. only mark ancestors until we find the containing table.
    
    This fixes two virtual/layout_ng_block_frag/ bugs. Tables are not
    fragmentable in NG block fragmentation, so if we have a multicol
    container with a table inside, we need to fall back the entire
    fragmentation context to legacy layout. But we don't need the ancestry
    of that multicol container to do the same. It can be laid out by NG.
    The two tests showed minor text rendering differences when rendered in
    legacy (the test) and NG (the ref), and therefore failed.
    
    Bug: 958381
    Change-Id: I33d2270928c889512e8cbd8b18ade15d346ea985
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2687617
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853213}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -282,7 +282,13 @@ bool IsRootEditableElementWithCounting(const Element& element) {
 bool DefinitelyNewFormattingContext(const Node& node,
                                     const ComputedStyle& style) {
   auto display = style.Display();
-  if (display == EDisplay::kInline || display == EDisplay::kContents)
+  if (display == EDisplay::kInline || display == EDisplay::kContents ||
+      display == EDisplay::kTableRowGroup ||
+      display == EDisplay::kTableHeaderGroup ||
+      display == EDisplay::kTableFooterGroup ||
+      display == EDisplay::kTableRow ||
+      display == EDisplay::kTableColumnGroup ||
+      display == EDisplay::kTableColumn)
     return false;
   // ::marker may establish a formatting context but still have some dependency
   // on the originating list item, so return false.
@@ -4240,12 +4246,15 @@ void Element::UpdateForceLegacyLayout(const ComputedStyle& new_style,
 
 void Element::ForceLegacyLayoutInFormattingContext(
     const ComputedStyle& new_style) {
-  // TableNG requires that table elements are either all NG, or all Legacy.
-  bool needs_traverse_to_table =
-      RuntimeEnabledFeatures::LayoutNGTableEnabled() &&
-      new_style.IsDisplayTableType();
-  bool found_bfc = DefinitelyNewFormattingContext(*this, new_style);
-  for (Element* ancestor = this; !found_bfc || needs_traverse_to_table;) {
+  bool found_fc = DefinitelyNewFormattingContext(*this, new_style);
+  if (found_fc && RuntimeEnabledFeatures::LayoutNGTableEnabled()) {
+    // TableNG requires that table elements are either all NG, or all Legacy. We
+    // need to mark all the way up to the containing table.
+    found_fc = new_style.Display() != EDisplay::kTableCell &&
+               new_style.Display() != EDisplay::kTableCaption;
+  }
+
+  for (Element* ancestor = this; !found_fc;) {
     ancestor =
         DynamicTo<Element>(LayoutTreeBuilderTraversal::Parent(*ancestor));
     if (!ancestor || ancestor->ShouldForceLegacyLayout())
@@ -4253,16 +4262,7 @@ void Element::ForceLegacyLayoutInFormattingContext(
     const ComputedStyle* style = ancestor->GetComputedStyle();
     if (style->Display() == EDisplay::kNone)
       break;
-    found_bfc = found_bfc || DefinitelyNewFormattingContext(*ancestor, *style);
-    if (found_bfc && !needs_traverse_to_table) {
-      needs_traverse_to_table =
-          RuntimeEnabledFeatures::LayoutNGTableEnabled() &&
-          style->IsDisplayTableType();
-    }
-    if (needs_traverse_to_table) {
-      if (style->IsDisplayTableBox())
-        needs_traverse_to_table = false;
-    }
+    found_fc = found_fc || DefinitelyNewFormattingContext(*ancestor, *style);
     ancestor->SetShouldForceLegacyLayoutForChild(true);
     ancestor->SetNeedsReattachLayoutTree();
   }
