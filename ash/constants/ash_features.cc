bd96c4361602f8b7a8a1a6315ec64268e650044b
Jimmy Gong <jimmyxgong@google.com>
2021-02-11 01:32:18 +0000

    Pciguard: Add EnablePciGuardUi flag
    This flag gates whether or not the PciGuard-related UI will display,
    which includes the Settings toggle and notifications.
    
    Bug: 1175987
    Change-Id: Ic831ab5f4509af35f3e29f67f291adba9a27d072
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683406
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852932}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -297,6 +297,10 @@ const base::Feature kEnableLocalSearchService{"EnableLocalSearchService",
 const base::Feature kEnableOobeChromeVoxHint{"EnableOobeChromeVoxHint",
                                              base::FEATURE_ENABLED_BY_DEFAULT};
 
+// Enables toggling Pciguard settings through Settings UI.
+const base::Feature kEnablePciguardUi{"EnablePciguardUi",
+                                      base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Enables Device End Of Lifetime warning notifications.
 const base::Feature kEolWarningNotifications{"EolWarningNotifications",
                                              base::FEATURE_ENABLED_BY_DEFAULT};
@@ -834,6 +838,10 @@ bool IsClipboardHistoryContextMenuNudgeEnabled() {
 
 bool IsImprovedKeyboardShortcutsEnabled() {
   return base::FeatureList::IsEnabled(kImprovedKeyboardShortcuts);
+}
+
+bool IsPciguardUiEnabled() {
+  return base::FeatureList::IsEnabled(kEnablePciguardUi);
 }
 
 bool IsPhoneHubEnabled() {
