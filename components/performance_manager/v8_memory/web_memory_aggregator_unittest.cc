1d0b5bd784c18e31bf68a7fe21131df023813ab7
Alexander Timin <altimin@chromium.org>
2021-02-11 19:07:37 +0000

    [tracing] Move mojo tracing to use perfetto::TracedValue.
    Migrate autogenerated mojo C++ bindings from
    base::trace_event::TracedValue to perfetto::TracedValue and implement
    WriteIntoTracedValue for them (both for structs and for enums).
    
    As the perfetto::TracedValue takes care of the composeability, remove
    the existing complex logic to support argument tracing from mojo.
    
    R=rockot@google.com,nasko@chromium.org
    BUG=1137154
    
    Change-Id: I2cd0e58e50e4cf30c6da1c768abe25742c8abca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679383
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853206}

--- components/performance_manager/v8_memory/web_memory_aggregator_unittest.cc
+++ components/performance_manager/v8_memory/web_memory_aggregator_unittest.cc
@@ -74,11 +74,8 @@ mojom::WebMemoryMeasurementPtr CreateExpectedMemoryMeasurement(
   return expected_measurement;
 }
 
-// Abuse Mojo's trace integration to serialize a measurement to sorted JSON for
-// string comparison. This gives failure messages that include the full
-// measurement in JSON format and is easier than comparing every field of
-// nested Mojo messages individually.
-std::string MeasurementToJSON(
+// Clone and sort the measurement for easier comparison.
+mojom::WebMemoryMeasurementPtr NormalizeMeasurement(
     const mojom::WebMemoryMeasurementPtr& measurement) {
   // Sort all arrays.
   auto canonical_measurement = measurement->Clone();
@@ -89,10 +86,7 @@ std::string MeasurementToJSON(
   std::sort(canonical_measurement->breakdown.begin(),
             canonical_measurement->breakdown.end());
 
-  // Convert to JSON string.
-  base::trace_event::TracedValueJSON json_value;
-  canonical_measurement->AsValueInto(&json_value);
-  return json_value.ToJSON();
+  return canonical_measurement;
 }
 
 }  // namespace
@@ -170,8 +164,8 @@ TEST_F(WebMemoryAggregatorTest, CreateBreakdownEntry) {
                                 AttributionScope::kWindow,
                                 /*expected_url=*/"", attribute, attribute),
     });
-    EXPECT_EQ(MeasurementToJSON(measurement),
-              MeasurementToJSON(expected_result));
+    EXPECT_EQ(NormalizeMeasurement(measurement),
+              NormalizeMeasurement(expected_result));
   }
 }
 
@@ -185,7 +179,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateSingleFrame) {
   });
   WebMemoryAggregator aggregator(main_frame);
   auto result = aggregator.AggregateMeasureMemoryResult();
-  EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+  EXPECT_EQ(NormalizeMeasurement(result),
+            NormalizeMeasurement(expected_result));
 }
 
 TEST_F(WebMemoryAggregatorTest, AggregateSingleSiteMultiFrame) {
@@ -204,7 +199,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateSingleSiteMultiFrame) {
                               "redirect.html?target=iframe.html"),
   });
   auto result = aggregator.AggregateMeasureMemoryResult();
-  EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+  EXPECT_EQ(NormalizeMeasurement(result),
+            NormalizeMeasurement(expected_result));
 }
 
 TEST_F(WebMemoryAggregatorTest, AggregateCrossOrigin) {
@@ -242,7 +238,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateCrossOrigin) {
                               "https://foo.com/iframe1"),
   });
   auto result = aggregator.AggregateMeasureMemoryResult();
-  EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+  EXPECT_EQ(NormalizeMeasurement(result),
+            NormalizeMeasurement(expected_result));
   worker->RemoveClientFrame(child_frame);
 }
 
@@ -325,7 +322,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateNestedCrossOrigin) {
                               "https://example.com/empty_frame"),
   });
   auto result = aggregator.AggregateMeasureMemoryResult();
-  EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+  EXPECT_EQ(NormalizeMeasurement(result),
+            NormalizeMeasurement(expected_result));
 }
 
 TEST_F(WebMemoryAggregatorTest, AggregateSameOriginAboutBlank) {
@@ -339,7 +337,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateSameOriginAboutBlank) {
   });
   WebMemoryAggregator aggregator(main_frame);
   auto result = aggregator.AggregateMeasureMemoryResult();
-  EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+  EXPECT_EQ(NormalizeMeasurement(result),
+            NormalizeMeasurement(expected_result));
 }
 
 TEST_F(WebMemoryAggregatorTest, SkipCrossOriginAboutBlank) {
@@ -356,7 +355,8 @@ TEST_F(WebMemoryAggregatorTest, SkipCrossOriginAboutBlank) {
   });
   WebMemoryAggregator aggregator(main_frame);
   auto result = aggregator.AggregateMeasureMemoryResult();
-  EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+  EXPECT_EQ(NormalizeMeasurement(result),
+            NormalizeMeasurement(expected_result));
 }
 
 TEST_F(WebMemoryAggregatorTest, AggregateWindowOpener) {
@@ -392,7 +392,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateWindowOpener) {
                               base::nullopt, "example-id3"),
   });
   auto result = aggregator.AggregateMeasureMemoryResult();
-  EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+  EXPECT_EQ(NormalizeMeasurement(result),
+            NormalizeMeasurement(expected_result));
 
   {
     WebMemoryAggregator aggregator(cross_site_child);
@@ -405,8 +406,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateWindowOpener) {
             base::nullopt),
     });
     auto cross_site_result = aggregator.AggregateMeasureMemoryResult();
-    EXPECT_EQ(MeasurementToJSON(cross_site_result),
-              MeasurementToJSON(expected_cross_site_result));
+    EXPECT_EQ(NormalizeMeasurement(cross_site_result),
+              NormalizeMeasurement(expected_cross_site_result));
   }
 
   {
@@ -418,8 +419,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateWindowOpener) {
                                 base::nullopt, base::nullopt),
     });
     auto cross_site_result = aggregator.AggregateMeasureMemoryResult();
-    EXPECT_EQ(MeasurementToJSON(cross_site_result),
-              MeasurementToJSON(expected_cross_site_result));
+    EXPECT_EQ(NormalizeMeasurement(cross_site_result),
+              NormalizeMeasurement(expected_cross_site_result));
   }
 }
 
@@ -438,7 +439,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateProvisionalWindowOpener) {
                               "https://example.com/"),
   });
   auto result = aggregator.AggregateMeasureMemoryResult();
-  EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+  EXPECT_EQ(NormalizeMeasurement(result),
+            NormalizeMeasurement(expected_result));
 }
 
 TEST_F(WebMemoryAggregatorTest, AggregateSameOriginWorker) {
@@ -465,7 +467,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateSameOriginWorker) {
                               "https://example.com/worker2", "example-id"),
   });
   auto result = aggregator.AggregateMeasureMemoryResult();
-  EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+  EXPECT_EQ(NormalizeMeasurement(result),
+            NormalizeMeasurement(expected_result));
   worker2->RemoveClientWorker(worker1);
   worker1->RemoveClientFrame(child_frame);
 }
@@ -490,7 +493,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateCrossOriginWorker) {
                               base::nullopt, "example-id"),
   });
   auto result = aggregator.AggregateMeasureMemoryResult();
-  EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+  EXPECT_EQ(NormalizeMeasurement(result),
+            NormalizeMeasurement(expected_result));
   worker2->RemoveClientWorker(worker1);
   worker1->RemoveClientFrame(child_frame);
 }
@@ -532,7 +536,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateCrossOriginCallers) {
                                 "https://a.com/popup2"),
     });
     auto result = aggregator.AggregateMeasureMemoryResult();
-    EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+    EXPECT_EQ(NormalizeMeasurement(result),
+              NormalizeMeasurement(expected_result));
   }
 
   {
@@ -552,7 +557,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateCrossOriginCallers) {
                                 base::nullopt, "c_com_iframe2"),
     });
     auto result = aggregator.AggregateMeasureMemoryResult();
-    EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+    EXPECT_EQ(NormalizeMeasurement(result),
+              NormalizeMeasurement(expected_result));
   }
 
   {
@@ -566,7 +572,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateCrossOriginCallers) {
                                 "https://c.com/iframe2"),
     });
     auto result = aggregator.AggregateMeasureMemoryResult();
-    EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+    EXPECT_EQ(NormalizeMeasurement(result),
+              NormalizeMeasurement(expected_result));
   }
 }
 
@@ -583,7 +590,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateCrossProcessCallers) {
                                 base::nullopt, "b_com_iframe"),
     });
     auto result = aggregator.AggregateMeasureMemoryResult();
-    EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+    EXPECT_EQ(NormalizeMeasurement(result),
+              NormalizeMeasurement(expected_result));
   }
 
   {
@@ -595,7 +603,8 @@ TEST_F(WebMemoryAggregatorTest, AggregateCrossProcessCallers) {
                                 "https://b.com/iframe"),
     });
     auto result = aggregator.AggregateMeasureMemoryResult();
-    EXPECT_EQ(MeasurementToJSON(result), MeasurementToJSON(expected_result));
+    EXPECT_EQ(NormalizeMeasurement(result),
+              NormalizeMeasurement(expected_result));
   }
 }
 
--- mojo/public/cpp/bindings/struct_ptr.h
+++ mojo/public/cpp/bindings/struct_ptr.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "mojo/public/cpp/bindings/lib/hash_util.h"
 #include "mojo/public/cpp/bindings/type_converter.h"
+#include "third_party/perfetto/include/perfetto/tracing/traced_value.h"
 
 namespace mojo {
 namespace internal {
@@ -108,6 +109,13 @@ class StructPtr {
   }
 
   explicit operator bool() const { return !is_null(); }
+
+  // If T is serialisable into trace, StructPtr<T> is also serialisable.
+  template <class U = S>
+  perfetto::check_traced_value_support_t<U> WriteIntoTracedValue(
+      perfetto::TracedValue context) const {
+    perfetto::WriteIntoTracedValue(std::move(context), ptr_);
+  }
 
  private:
   friend class internal::StructPtrWTFHelper<Struct>;
@@ -202,6 +210,17 @@ class InlinedStructPtr {
   }
 
   explicit operator bool() const { return !is_null(); }
+
+  // If T is serialisable into trace, StructPtr<T> is also serialisable.
+  template <class U = S>
+  perfetto::check_traced_value_support_t<U> WriteIntoTracedValue(
+      perfetto::TracedValue context) const {
+    if (is_null()) {
+      std::move(context).WritePointer(nullptr);
+      return;
+    }
+    perfetto::WriteIntoTracedValue(std::move(context), value_);
+  }
 
  private:
   friend class internal::InlinedStructPtrWTFHelper<Struct>;
