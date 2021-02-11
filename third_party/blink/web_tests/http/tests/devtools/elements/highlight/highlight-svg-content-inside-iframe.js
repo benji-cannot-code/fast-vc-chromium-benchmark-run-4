fdce4ae0538fa3115b8efabed8ef01646f45a02d
Alex Rudenko <alexrudenko@chromium.org>
2021-02-11 12:26:51 +0000

    Fix invocation of ElementsTestRunner.dumpInspectorHighlightJSON in a test
    The method does not accept the 3rd parameter.
    
    Bug: 1173995
    Change-Id: I67e133872d55cff236a40d20c3854826fa7c2bae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2687061
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853089}

--- third_party/blink/web_tests/http/tests/devtools/elements/highlight/highlight-svg-content-inside-iframe.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/highlight/highlight-svg-content-inside-iframe.js
@@ -32,5 +32,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       </div>
     `);
 
-  ElementsTestRunner.dumpInspectorHighlightJSON('svg-rect', TestRunner.completeTest.bind(TestRunner), 'svg-iframe');
+  ElementsTestRunner.dumpInspectorHighlightJSON('svg-rect', TestRunner.completeTest.bind(TestRunner));
 })();
