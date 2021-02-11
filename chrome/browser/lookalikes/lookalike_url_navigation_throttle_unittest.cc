aa4018a38e877cb537b3cf7f1bb97f664351180e
Mustafa Emre Acer <meacer@google.com>
2021-02-11 16:48:13 +0000

    Lookalikes: Add digital asset links support
    This CL adds the initial version of Digital Asset Links support to
    lookalike checks. The goal of this feature is to allow site owners
    to allowlist lookalike sites they own.
    
    With this feature, we now attempt to fetch DAL manifests when a
    lookalike URL is detected. Assume that the lookalike heuristics match
    https://lookalike-site.com to https://target-site.com. The feature works
    as follows:
    
    1. The lookalike manifest is fetched from
    https://lookalike-site.com/.well-known/assetlinks.json. If the fetch
    fails, the interstitial is shown.
    2. An entry with "lookalikes/allowlist" relationship is searched in the
    manifest with a value of "https://target-site.com". If not found, the
    interstitial is shown.
    3. If a valid entry is found, the target manifest is fetched from
    https://target-site.com/.well-known/assetlinks.json. If the fetch fails,
    the interstitial is shown.
    4. An entry with "lookalikes/allowlist" relationship is searched in the
    manifest with a value of "https://lookalike-site.com". If not found, the
    interstitial is shown.
    5. If a valid entry is found, the sites are assumed to be co-owned, so
    the interstitial isn't shown.
    
    Future work:
    - This CL currently waits indefinitely for the manifest fetches. Future
    CLs will limit the waiting time.
    - Metrics need to be added.
    - More edge cases should be added for manifest URLs that redirect or end
    up with an SSL errors.
    - Caching of manifests or validation results need to be implemented.
    - The actual relationship type to use in the manifest is TBD. Currently
    it's "lookalikes/allowlist".
    
    Bug: 1175385
    Change-Id: I29f6c00a6fbf6e583c99af4950342df61b178d18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2680131
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853148}

--- chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/lookalikes/lookalike_url_navigation_throttle.h"
 
+#include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
