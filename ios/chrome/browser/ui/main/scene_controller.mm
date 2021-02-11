6d81c89d4bbd93ab0f03003ca0eb256a037b238f
Guillaume Jenkins <gujen@google.com>
2021-02-11 00:39:36 +0000

    [iOS Enterprise] BrowserSignin: Add metrics
    Adds metrics to the force sign-out flow when the user is signed out due
    to the BrowserSignin policy being set to 0 (sign-in disabled): one that
    records when the user was already signed in and is automatically signed
    out, and one that records when the sign-in UI is automatically
    dismissed.
    
    Bug: 1155745
    Change-Id: I4636f011a6cdbac7093ab3b9c2d071c08a19db0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675906
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#852893}

--- ios/chrome/browser/ui/main/scene_controller.mm
+++ ios/chrome/browser/ui/main/scene_controller.mm
@@ -1287,6 +1287,8 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
     if (!service->IsAuthenticated()) {
       return;
     }
+    UMA_HISTOGRAM_BOOLEAN("Enterprise.BrowserSigninIOS.SignedOutByPolicy",
+                          true);
     // Sign the user out, but keep synced data (bookmarks, passwords, etc)
     // locally to be consistent with the policy's behavior on other platforms.
     service->SignOut(
@@ -1307,6 +1309,8 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
 
   if (self.signinCoordinator) {
     [self interruptSigninCoordinatorAnimated:YES completion:signOut];
+    UMA_HISTOGRAM_BOOLEAN(
+        "Enterprise.BrowserSigninIOS.SignInInterruptedByPolicy", true);
   } else {
     signOut();
   }
