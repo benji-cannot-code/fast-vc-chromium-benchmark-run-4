1d6847728d543ddd2ceb1233f8aff5f87df25768
Illia Klimov <elklm@google.com>
2021-02-11 23:27:24 +0000

    Remove ContentSettingsType::PLUGINS.
    Flash / PLUGINS has been deprecated in M87 and almost entirely removed
    in M88. It was not possible to remove ContentSettingsType::PLUGINS entry
    because it was used in a filter to display Flash warning messages.
    In M90 warning messages will be removed, hence
    ContentSettingsType::PLUGINS can be removed as well.
    
    
    Bug: 1149878
    Change-Id: I168bf780538a62d138e684338cd02cf78c8fe44e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2671327
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853276}

--- chrome/browser/extensions/api/content_settings/content_settings_api.cc
+++ chrome/browser/extensions/api/content_settings/content_settings_api.cc
@@ -64,11 +64,6 @@ bool RemoveContentType(base::ListValue* args,
   // We remove the ContentSettingsType parameter since this is added by the
   // renderer, and is not part of the JSON schema.
   args->Remove(0, nullptr);
-  // PLUGINS have been deprecated, so ignore requests for removing them.
-  if (content_type_str == "plugins") {
-    *content_type = ContentSettingsType::DEPRECATED_PLUGINS;
-    return true;
-  }
   *content_type =
       extensions::content_settings_helpers::StringToContentSettingsType(
           content_type_str);
@@ -86,12 +81,6 @@ ContentSettingsContentSettingClearFunction::Run() {
 
   std::unique_ptr<Clear::Params> params(Clear::Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params.get());
-
-  if (content_type == ContentSettingsType::DEPRECATED_PLUGINS) {
-    return RespondNow(
-        Error(content_settings_api_constants::
-                  kSettingPluginContentSettingsClearIsDisallowed));
-  }
 
   ExtensionPrefsScope scope = kExtensionPrefsScopeRegular;
   bool incognito = false;
@@ -127,10 +116,6 @@ ContentSettingsContentSettingGetFunction::Run() {
   std::unique_ptr<Get::Params> params(Get::Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params.get());
 
-  if (content_type == ContentSettingsType::DEPRECATED_PLUGINS) {
-    return RespondNow(Error(content_settings_api_constants::
-                                kSettingPluginContentSettingsGetIsDisallowed));
-  }
 
   GURL primary_url(params->details.primary_url);
   if (!primary_url.is_valid()) {
@@ -199,12 +184,6 @@ ContentSettingsContentSettingSetFunction::Run() {
 
   std::unique_ptr<Set::Params> params(Set::Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params.get());
-
-  // PLUGINS have been deprecated.
-  if (content_type == ContentSettingsType::DEPRECATED_PLUGINS) {
-    return RespondNow(Error(content_settings_api_constants::
-                                kSettingPluginContentSettingsIsDisallowed));
-  }
 
   std::string primary_error;
   ContentSettingsPattern primary_pattern =
@@ -329,10 +308,6 @@ ExtensionFunction::ResponseAction
 ContentSettingsContentSettingGetResourceIdentifiersFunction::Run() {
   ContentSettingsType content_type;
   EXTENSION_FUNCTION_VALIDATE(RemoveContentType(args_.get(), &content_type));
-
-  if (content_type != ContentSettingsType::DEPRECATED_PLUGINS) {
-    return RespondNow(NoArguments());
-  }
 
 #if BUILDFLAG(ENABLE_PLUGINS)
   return RespondNow(
