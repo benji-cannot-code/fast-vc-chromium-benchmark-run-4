9fc5f7549d9453c06d901ba0835a377385b2d3d9
Sammie Quon <sammiequon@chromium.org>
2021-02-11 02:40:58 +0000

    full_restore: Write to file when window changes.
    Calls the full restore helper when window state, bounds, etc. changes.
    Removes FullRestoreWindowManager in favour of calling SaveWindow
    directly from WindowState. This was suggested by oshima-san earlier
    but makes a lot of sense after more prototyping.
    
    Also adds saving when the MRU order changes. Indexes saved will be in
    reverse order of the MRU list since that is how we expect them to be
    read.
    
    Saving during window add/destroy is handled in app launch, so the test
    in this suite that was formerly there is removed.
    
    Bug: 1176024
    Test: manual, added tests
    Change-Id: I925bf70883c5261ba2a1a36f9c26aac17b4c0e7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2684626
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852957}

--- ash/wm/window_state.cc
+++ ash/wm/window_state.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/focus_cycler.h"
 #include "ash/metrics/pip_uma.h"
 #include "ash/public/cpp/app_types.h"
+#include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/public/cpp/window_animation_types.h"
 #include "ash/public/cpp/window_properties.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wm/collision_detection/collision_detection_utils.h"
 #include "ash/wm/default_state.h"
+#include "ash/wm/full_restore/full_restore_controller.h"
 #include "ash/wm/pip/pip_positioner.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/window_animations.h"
@@ -174,6 +176,16 @@ void ReportAshPipAndroidPipUseTime(base::TimeDelta duration) {
   UMA_HISTOGRAM_CUSTOM_TIMES(kAshPipAndroidPipUseTimeHistogramName, duration,
                              base::TimeDelta::FromSeconds(1),
                              base::TimeDelta::FromHours(10), 50);
+}
+
+// Notifies the full restore controller to write to file.
+void SaveWindowForFullRestore(WindowState* window_state) {
+  if (!features::IsFullRestoreEnabled())
+    return;
+
+  auto* controller = FullRestoreController::Get();
+  if (controller)
+    controller->SaveWindow(window_state);
 }
 
 }  // namespace
@@ -525,6 +537,7 @@ void WindowState::OnCompleteDrag(const gfx::PointF& location) {
   DCHECK(drag_details_);
   if (delegate_)
     delegate_->OnDragFinished(/*canceled=*/false, location);
+  SaveWindowForFullRestore(this);
 }
 
 void WindowState::OnRevertDrag(const gfx::PointF& location) {
@@ -669,6 +682,7 @@ void WindowState::NotifyPostStateTypeChange(
   for (auto& observer : observer_list_)
     observer.OnPostWindowStateTypeChange(this, old_window_state_type);
   OnPostPipStateChange(old_window_state_type);
+  SaveWindowForFullRestore(this);
 }
 
 void WindowState::OnPostPipStateChange(WindowStateType old_window_state_type) {
@@ -911,6 +925,16 @@ void WindowState::OnWindowPropertyChanged(aura::Window* window,
     }
     return;
   }
+  if (key == aura::client::kWindowWorkspaceKey) {
+    // Save the window for full restore purposes unless
+    // |ignore_property_change_| is true. Note that moving windows across
+    // displays will also trigger a property change, even if the value stays the
+    // same, so we do not need to save the window when it changes root windows
+    // (OnWindowAddedToRootWindow).
+    if (!ignore_property_change_)
+      SaveWindowForFullRestore(this);
+    return;
+  }
 
   // The shelf visibility should be updated if kHideShelfWhenFullscreenKey or
   // kImmersiveIsActive change - these property affect the shelf behavior, and
@@ -958,6 +982,9 @@ void WindowState::OnWindowBoundsChanged(aura::Window* window,
       window_->GetProperty(ash::kWindowManagerManagesOpacityKey)) {
     window_->SetOpaqueRegionsForOcclusion({gfx::Rect(new_bounds.size())});
   }
+
+  if (reason != ui::PropertyChangeReason::FROM_ANIMATION && !is_dragged())
+    SaveWindowForFullRestore(this);
 }
 
 }  // namespace ash
