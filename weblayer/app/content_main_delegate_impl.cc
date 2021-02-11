40c48bb84f973c0bcbe4cc1c8c1ce5d6ffbc22ff
Robbie McElrath <rmcelrath@chromium.org>
2021-02-11 01:04:21 +0000

    [WebLayer] Disable portals in WL
    Portals aren't currently implemented in WebLayer, so we shouldn't be
    exposing them to web content.
    
    Bug: 1174856
    Change-Id: Ie816dbc3fa3ee6c0beed7dff5337587b4258f68d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2686344
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852917}

--- weblayer/app/content_main_delegate_impl.cc
+++ weblayer/app/content_main_delegate_impl.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/url_constants.h"
 #include "media/base/media_switches.h"
 #include "services/network/public/cpp/features.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/web_runtime_features.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
@@ -167,6 +168,8 @@ bool ContentMainDelegateImpl::BasicStartupComplete(int* exit_code) {
     ::features::kPeriodicBackgroundSync,
     // TODO(crbug.com/1131017): Support SurfaceViews on WebLayer.
     media::kOverlayFullscreenVideo,
+    // TODO(crbug.com/1174856): Support Portals.
+    blink::features::kPortals,
 #if defined(OS_ANDROID)
     // TODO(crbug.com/1131016): Support Picture in Picture API on WebLayer.
     media::kPictureInPictureAPI,
