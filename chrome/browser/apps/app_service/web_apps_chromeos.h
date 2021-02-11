d37cc26892cf138df719a04c63fb9c1154e5c534
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-02-11 14:05:10 +0000

    Revert "Add browser tests to verify the media access function."
    This reverts commit 6e48422fc5b860f5c584e681b3c7fe98c6919242.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 853053 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzZlNDg0MjJmYzViODYwZjVjNTg0ZTY4MWIzYzdmZTk4YzY5MTkyNDIM
    
    Sample Failed Build: https://ci.chromium.org/b/8855592894708499408
    
    Sample Failed Step: browser_tests
    
    Original change's description:
    > Add browser tests to verify the media access function.
    > 
    > When the web contents are destroyed before OnRequestUpdate is invoked,
    > web_contents can't be found in OnRequestUpdate, and it could cause the
    > media accessing status not correctly updated.
    > 
    > Add AppWebContentsData to observe WebContents if there is a media
    > accessing for the web contents. When the web contents are destroyed,
    > modify the media accessing status.
    > 
    > BUG=1172777
    > 
    > Change-Id: Idbf96538edb48d0a063e0f8f37eef171cb464d8c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679071
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#853053}
    
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    BUG=1172777
    
    Change-Id: I38cf1fe2f5cc12b38f68f7660c1e74a59bb124e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690646
    Cr-Commit-Position: refs/heads/master@{#853097}

--- chrome/browser/apps/app_service/web_apps_chromeos.h
+++ chrome/browser/apps/app_service/web_apps_chromeos.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/scoped_observer.h"
 #include "chrome/browser/apps/app_service/app_notifications.h"
-#include "chrome/browser/apps/app_service/app_web_contents_data.h"
 #include "chrome/browser/apps/app_service/icon_key_util.h"
 #include "chrome/browser/apps/app_service/media_requests.h"
 #include "chrome/browser/apps/app_service/paused_apps.h"
@@ -42,8 +41,7 @@ namespace apps {
 class WebAppsChromeOs : public WebAppsBase,
                         public ArcAppListPrefs::Observer,
                         public NotificationDisplayService::Observer,
-                        public MediaCaptureDevicesDispatcher::Observer,
-                        public AppWebContentsData::Client {
+                        public MediaCaptureDevicesDispatcher::Observer {
  public:
   WebAppsChromeOs(const mojo::Remote<apps::mojom::AppService>& app_service,
                   Profile* profile,
@@ -116,9 +114,6 @@ class WebAppsChromeOs : public WebAppsBase,
                        int render_frame_id,
                        blink::mojom::MediaStreamType stream_type,
                        const content::MediaRequestState state) override;
-
-  // AppWebContentsData::Observer:
-  void OnWebContentsDestroyed(content::WebContents* contents) override;
 
   // NotificationDisplayService::Observer overrides.
   void OnNotificationDisplayed(
