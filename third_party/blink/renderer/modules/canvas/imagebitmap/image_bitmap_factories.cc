37b6fa53a99a42d13e05edb79c4625bf76455f8f
Dale Curtis <dalecurtis@chromium.org>
2021-02-11 04:06:08 +0000

    Move window.createImageBitmap from /core/ to /modules/
    This allows other modules types to extend the ImageBitmapSource
    union; e.g., adding VideoFrame to things ImageBitmaps can be
    created from.
    
    Bug: 1175907
    Change-Id: Ibac3276b191c0248302abd42a1e8db51431c3530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2685525
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#852986}

--- third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_factories.cc
+++ third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_factories.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "third_party/blink/renderer/core/imagebitmap/image_bitmap_factories.h"
+#include "third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_factories.h"
 
 #include <memory>
 #include <utility>
