006739598f76340786eadf93697d647435988213
Philipp Hancke <philipp.hancke@googlemail.com>
2021-02-11 17:27:36 +0000

    [webrtc-internals] add audio concealment percentage stats
    calculated as delta(concealedSamples) / delta(totalSamplesReceived)
    
    BUG=webrtc:11640
    
    Change-Id: I9a6f6a3977d34eee700198163f9b17793e233f2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690846
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853166}

--- content/browser/resources/media/stats_rates_calculator.js
+++ content/browser/resources/media/stats_rates_calculator.js
@@ -497,7 +497,10 @@ class StatsRatesCalculator {
               'framesDecoded', 'interFrameDelay'),
           totalSamplesReceived:
               new RateCalculator('totalSamplesReceived', 'timestamp'),
-          concealedSamples: new RateCalculator('concealedSamples', 'timestamp'),
+          concealedSamples: [
+            new RateCalculator('concealedSamples', 'timestamp'),
+            new RateCalculator('concealedSamples', 'totalSamplesReceived'),
+          ],
           silentConcealedSamples:
               new RateCalculator('silentConcealedSamples', 'timestamp'),
           insertedSamplesForDeceleration:
