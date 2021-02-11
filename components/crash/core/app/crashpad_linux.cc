db8e4ec3d2b3d3e0e0f7a937283d8ad9ae64b161
Andreas Haas <ahaas@chromium.org>
2021-02-11 10:19:31 +0000

    [v8][wasm] Cleanup the conditions for registering trap handlers
    The conditions on when and how to register WebAssembly trap handlers
    were confusing and even caused bugs, see crbug.com/1172341. With this
    CL we clean up the registration code. First we handle the options for
    official builds that use Crashpad or Breakpad. Then we handle debugging
    cases, and finally the case where there is no signal handler already
    and V8 should set up its own signal handler.
    
    To set up the the conditions properly I had to move the kEnableCrashpad
    flag out of components/crash to avoid the violation of include rules.
    
    R=mark@chromium.org, jperaza@chromium.org
    
    Bug: chromium:1172341
    Change-Id: I0d38e46762a1d3de64fad9bcc3319130123e7ca8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683985
    Reviewed-by: Joshua Peraza <jperaza@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853044}

--- components/crash/core/app/crashpad_linux.cc
+++ components/crash/core/app/crashpad_linux.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <limits>
 
+#include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/path_service.h"
@@ -52,12 +53,9 @@ void SetFirstChanceExceptionHandler(bool (*handler)(int, siginfo_t*, void*)) {
       FirstChanceHandlerHelper);
 }
 
-// TODO(jperaza): Remove kEnableCrashpad and IsCrashpadEnabled() when Crashpad
-// is fully enabled on Linux.
-const char kEnableCrashpad[] = "enable-crashpad";
-
 bool IsCrashpadEnabled() {
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(kEnableCrashpad);
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      ::switches::kEnableCrashpad);
 }
 
 bool GetHandlerSocket(int* fd, pid_t* pid) {
