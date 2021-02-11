f2f715ed3862dd8dd79835e27dfda25dcf734e90
Alan Screen <awscreen@chromium.org>
2021-02-11 10:44:29 +0000

    Add EnumeratePrinters to print backend service
    Incorporate support for retrieving the list of installed printers from
    the utility service.  This is needed to support populating the list of
    destinations in print preview.
    
    Bug: 809738
    Change-Id: Ia01f725b199cf259a87af4afdf4ab9d7e09bd16a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673677
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853066}

--- chrome/services/printing/print_backend_service_impl.cc
+++ chrome/services/printing/print_backend_service_impl.cc
@@ -33,6 +33,25 @@ void PrintBackendServiceImpl::Init(const std::string& locale) {
   print_backend_ = PrintBackend::CreateInstance(locale);
 }
 
+void PrintBackendServiceImpl::EnumeratePrinters(
+    mojom::PrintBackendService::EnumeratePrintersCallback callback) {
+  if (!print_backend_) {
+    DLOG(ERROR)
+        << "Print backend instance has not been initialized for locale.";
+    std::move(callback).Run(base::nullopt);
+    return;
+  }
+
+  PrinterList printer_list;
+  if (!print_backend_->EnumeratePrinters(&printer_list)) {
+    DLOG(ERROR) << "EnumeratePrinters failed, last error is "
+                << logging::GetLastSystemErrorCode();
+    std::move(callback).Run(base::nullopt);
+    return;
+  }
+  std::move(callback).Run(std::move(printer_list));
+}
+
 void PrintBackendServiceImpl::GetDefaultPrinterName(
     mojom::PrintBackendService::GetDefaultPrinterNameCallback callback) {
   if (!print_backend_) {
@@ -49,6 +68,8 @@ void PrintBackendServiceImpl::GetPrinterSemanticCapsAndDefaults(
     mojom::PrintBackendService::GetPrinterSemanticCapsAndDefaultsCallback
         callback) {
   if (!print_backend_) {
+    DLOG(ERROR)
+        << "Print backend instance has not been initialized for locale.";
     std::move(callback).Run(base::nullopt);
     return;
   }
@@ -69,6 +90,8 @@ void PrintBackendServiceImpl::FetchCapabilities(
     const std::string& printer_name,
     mojom::PrintBackendService::FetchCapabilitiesCallback callback) {
   if (!print_backend_) {
+    DLOG(ERROR)
+        << "Print backend instance has not been initialized for locale.";
     std::move(callback).Run(base::nullopt, base::nullopt, base::nullopt);
     return;
   }
