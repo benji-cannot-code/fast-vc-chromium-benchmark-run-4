7fde7f363ee9b877a6cdafb27b3590dbeb416bd7
Henrique Ferreiro <hferreiro@igalia.com>
2021-02-12 22:33:15 +0000

    Make sure the different drag&drop types match each other values
    This CL adds static_asserts to verify that
    ui::DragDropTypes::DragOperation, ui::mojom::DragOperation and
    blink::DragOperationsMask values match. These will be removed once those
    types are merged.
    
    Bug: 1093536
    Change-Id: I98b6a941e4dec024ba8e043c274bb1cac77ebe2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690577
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#853694}

---
+++ third_party/blink/common/page/drag_operation.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/public/common/page/drag_operation.h"
+
+#include "ui/base/dragdrop/mojom/drag_drop_types.mojom-shared.h"
+
+namespace blink {
+
+using ::ui::mojom::DragOperation;
+
+// Ensure that the blink::DragOperationsMask enum values stay in sync with
+// ui::mojom::DragOperation.
+#define STATIC_ASSERT_ENUM(a, b)                            \
+  static_assert(static_cast<int>(a) == static_cast<int>(b), \
+                "enum mismatch: " #a)
+STATIC_ASSERT_ENUM(kDragOperationNone, DragOperation::kNone);
+STATIC_ASSERT_ENUM(kDragOperationCopy, DragOperation::kCopy);
+STATIC_ASSERT_ENUM(kDragOperationLink, DragOperation::kLink);
+STATIC_ASSERT_ENUM(kDragOperationMove, DragOperation::kMove);
+
+}  // namespace blink
---
+++ ui/base/dragdrop/drag_drop_types.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/base/dragdrop/drag_drop_types.h"
+
+#include "ui/base/dragdrop/mojom/drag_drop_types.mojom.h"
+
+namespace ui {
+
+using mojom::DragOperation;
+
+// Ensure that the DragDropTypes::DragOperation enum values stay in sync with
+// mojom::DragOperation.
+#define STATIC_ASSERT_ENUM(a, b)                            \
+  static_assert(static_cast<int>(a) == static_cast<int>(b), \
+                "enum mismatch: " #a)
+STATIC_ASSERT_ENUM(DragDropTypes::DRAG_NONE, DragOperation::kNone);
+STATIC_ASSERT_ENUM(DragDropTypes::DRAG_COPY, DragOperation::kCopy);
+STATIC_ASSERT_ENUM(DragDropTypes::DRAG_LINK, DragOperation::kLink);
+STATIC_ASSERT_ENUM(DragDropTypes::DRAG_MOVE, DragOperation::kMove);
+
+}  // namespace ui
