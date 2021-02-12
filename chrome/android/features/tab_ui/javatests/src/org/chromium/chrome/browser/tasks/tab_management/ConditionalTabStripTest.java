7d434918c2dc40a46d637b19afc5fca7e98f7452
Brandon Wylie <wylieb@chromium.org>
2021-02-12 00:16:56 +0000

    [Sheriff] Disable failing tests in ConditionalTabStripTest
    TBR=meiliang@chromium.org
    
    Bug: 1177383
    Change-Id: I7c8b77dc1c29f0b1f6ef26ea57fcd938eda71c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2689887
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853305}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/ConditionalTabStripTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/ConditionalTabStripTest.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tasks.tab_management;
 
-import static android.os.Build.VERSION_CODES.M;
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.longClick;
@@ -37,6 +35,7 @@ import static org.chromium.chrome.browser.tasks.tab_management.TabUiTestHelper.v
 
 import android.content.Context;
 import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.support.test.InstrumentationRegistry;
 import android.view.View;
 import android.widget.ListView;
@@ -171,8 +170,10 @@ public class ConditionalTabStripTest {
 
     @Test
     @MediumTest
-    @DisableIf.Build(sdk_is_less_than = M, message = "crbug.com/1081832")
-    public void testStrip_updateWithAddition() throws Exception {
+    @DisableIf.Build(sdk_is_less_than = VERSION_CODES.O,
+            message = "Failing or flaky on N & P, see crbug.com/1177383 & crbug.com/1081832")
+    public void
+    testStrip_updateWithAddition() throws Exception {
         ChromeTabbedActivity cta = sActivityTestRule.getActivity();
         verifyHidingStrip();
 
@@ -446,7 +447,10 @@ public class ConditionalTabStripTest {
 
     @Test
     @MediumTest
-    public void testStrip_enabled_notExpired() throws Exception {
+    @DisableIf.Build(sdk_is_greater_than = VERSION_CODES.M, sdk_is_less_than = VERSION_CODES.O,
+            message = "Failing on N, see crbug.com/1177383")
+    public void
+    testStrip_enabled_notExpired() throws Exception {
         ChromeTabbedActivity cta = sActivityTestRule.getActivity();
         for (int i = 0; i < 3; i++) {
             createBlankPageWithLaunchType(cta, false, TabLaunchType.FROM_CHROME_UI);
