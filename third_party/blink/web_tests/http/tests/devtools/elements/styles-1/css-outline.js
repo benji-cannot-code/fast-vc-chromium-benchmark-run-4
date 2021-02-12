470d4527270979ae66b674cab64c03ff678a9a48
Tim van der Lippe <tvanderlippe@chromium.org>
2021-02-12 13:39:35 +0000

    Use legacy import loader for formatter
    formatter/module.json will be removed soon, which means we need to
    import the formatter legacy file dynamically in the test.
    
    This CL requires https://crrev.com/c/2687500 to land first.
    
    R=aerotwist@chromium.org
    
    Bug: 1127902
    Change-Id: I72ccb6a2b0407ec0c8fe06dc74384d809332b2c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2687101
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853497}

--- third_party/blink/web_tests/http/tests/devtools/elements/styles-1/css-outline.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/styles-1/css-outline.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 (async function() {
   TestRunner.addResult(`The test verifies the CSS outline functionality.\n`);
-  await TestRunner.loadModule('formatter');
+  await TestRunner.loadLegacyModule('sources');
   await TestRunner.showPanel('elements');
   await TestRunner.loadHTML(`
 <style id="styler">
