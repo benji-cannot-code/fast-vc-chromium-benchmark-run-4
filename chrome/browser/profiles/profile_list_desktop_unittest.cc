1ff7550d813ea407b9d15547773dd5e99e7714ba
Alex Ilin <alexilin@chromium.org>
2021-02-12 17:29:47 +0000

    [profiles] DCHECK that omitted profiles are always ephemeral
    Since the is_omitted attribute is stored in-memory only, we can actually
    DCHECK that only ephemeral profiles can be omitted.
    
    A non-ephemeral omitted profile can persist on disk without being
    visible to the user. We want to prevent that from happening.
    
    Change-Id: Ib695c012466148d4d46464134b29a7d95562da4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690852
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853569}

--- chrome/browser/profiles/profile_list_desktop_unittest.cc
+++ chrome/browser/profiles/profile_list_desktop_unittest.cc
@@ -84,6 +84,7 @@ class ProfileListDesktopTest : public testing::Test {
     ProfileAttributesEntry* entry =
         storage->GetProfileAttributesWithPath(profile_path);
     ASSERT_NE(entry, nullptr);
+    entry->SetIsEphemeral(true);
     entry->SetIsOmitted(true);
   }
 
