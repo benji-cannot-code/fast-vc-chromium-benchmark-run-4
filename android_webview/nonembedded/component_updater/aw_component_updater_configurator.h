c4218cf46348e860762c172561c7399536ace871
Hazem Ashmawy <hazems@chromium.org>
2021-02-12 01:31:58 +0000

    [AW] Add component updater service config class
    Add component updater service configurator class. The
    configurator instance isn't complete yet; it doesn't have
    PrefService nor NetworkFetcherFactory.
    
    This is adapted from
    //chrome/browser/component_updater/chrome_component_updater_configurator*
    
    This is CL#1 in a series of CLs to init and run component update
    service in nonembedded webview.
    Design doc: http://go/wv-component-updater
    
    Bug: 1171770
    Test: out/bin/run_android_webview_unittests
    Change-Id: I98937bd80c49491bd1a3accbf7dc71eee9c449f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2685159
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853343}

---
+++ android_webview/nonembedded/component_updater/aw_component_updater_configurator.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ANDROID_WEBVIEW_NONEMBEDDED_COMPONENT_UPDATER_AW_COMPONENT_UPDATER_CONFIGURATOR_H_
+#define ANDROID_WEBVIEW_NONEMBEDDED_COMPONENT_UPDATER_AW_COMPONENT_UPDATER_CONFIGURATOR_H_
+
+#include "base/memory/scoped_refptr.h"
+#include "components/update_client/configurator.h"
+
+class PrefService;
+
+namespace base {
+class CommandLine;
+}
+
+namespace android_webview {
+
+scoped_refptr<update_client::Configurator> MakeAwComponentUpdaterConfigurator(
+    const base::CommandLine* cmdline,
+    PrefService* pref_service);
+
+}  // namespace android_webview
+
+#endif  // ANDROID_WEBVIEW_NONEMBEDDED_COMPONENT_UPDATER_AW_COMPONENT_UPDATER_CONFIGURATOR_H_
