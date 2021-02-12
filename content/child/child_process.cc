0c78220a55fde292d8719d0e56df528785eaebe8
Eric Seckler <eseckler@chromium.org>
2021-02-12 15:58:54 +0000

    PowerMode: Add charging voter to arbiter
    Since //base can't depend on //components, the charging voter is
    implemented as a PowerObserver of base's PowerMonitor and owned by
    the arbiter.
    
    Internal design doc: http://go/chromium-process-power-scheduler
    (sorry, no public docs yet.)
    
    Bug: 1166695
    Change-Id: I79cf2ee6607504edfd2f294ef824a668b6ed884d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690387
    Auto-Submit: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853538}

--- content/child/child_process.cc
+++ content/child/child_process.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_local.h"
 #include "build/build_config.h"
 #include "build/config/compiler/compiler_buildflags.h"
-#include "components/power_scheduler/power_mode_arbiter.h"
 #include "content/child/child_thread_impl.h"
 #include "content/common/android/cpu_time_metrics.h"
 #include "content/common/mojo_core_library_support.h"
@@ -90,8 +89,6 @@ ChildProcess::ChildProcess(base::ThreadPriority io_thread_priority,
   }
 
   tracing::InitTracingPostThreadPoolStartAndFeatureList();
-
-  power_scheduler::PowerModeArbiter::GetInstance()->OnThreadPoolAvailable();
 
 #if defined(OS_ANDROID)
   SetupCpuTimeMetrics();
