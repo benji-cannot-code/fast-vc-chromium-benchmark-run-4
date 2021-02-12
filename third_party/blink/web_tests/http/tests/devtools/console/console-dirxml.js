5798b3a5e602d15718a8a63fe95a4bb9c1f847d0
Andres Olivares <andoli@chromium.org>
2021-02-12 14:35:41 +0000

    Add call to show elements panel in layout tests
    These calls make it possible to import the elements module *before* the Renderer extension declared
    in elements/ (https://crrev.com/c/2687697) is invoked in the test environment. 
    
    Dynamically importing the module when the extension is invoked is a pattern that is failing for this
    particular extension and that has failed for other extensions as well. We should investigate to
    determine why this is the case and implement a definitive solution.
    
    Bug: 1134103
    Change-Id: Ic7788c230189b25efb3145482c7c29122a88c56a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690031
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853513}

--- third_party/blink/web_tests/http/tests/devtools/console/console-dirxml.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-dirxml.js
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         console.dirxml([document, fragment, document.createElement("span")]);
     }
   `);
+  await TestRunner.showPanel('elements');
 
   runtime.loadModulePromise('elements').then(function() {
     TestRunner.evaluateInPage('logToConsole()', onLoggedToConsole);
--- third_party/blink/web_tests/http/tests/devtools/console/console-xml-document.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-xml-document.js
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     var danglingNode = document.implementation.createDocument("", "books");
     console.dirxml(danglingNode.createElement("Book"));
   `);
+  await TestRunner.showPanel('elements');
 
   // Warm up elements renderer.
   runtime.loadModulePromise('elements').then(function() {
