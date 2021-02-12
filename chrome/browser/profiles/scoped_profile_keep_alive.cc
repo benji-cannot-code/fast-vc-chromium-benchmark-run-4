6be6ff6aa31374ddadfcb4d289d917cb0e2de4b0
Kevin Marshall <kmarshall@chromium.org>
2021-02-12 21:18:08 +0000

    Revert "Post RemoveKeepAlive() to a task"
    This reverts commit 590e82d754ee1123caa0ac4d00f0970cccfe5cf2.
    
    Reason for revert: Breaks Mac tests (crbug/1177956)
    
    Bug: 1177956
    
    Original change's description:
    > Post RemoveKeepAlive() to a task
    >
    > The object that owns ScopedProfileKeepAlive might be owned by Profile,
    > in which case decrementing the refcount immediately can cause UAF bugs.
    > Post RemoveKeepAlive() to a task to avoid this.
    >
    > Bug: 88586
    > Change-Id: Id33e9638d9ea0c1f0f08722c2af16548ab1f5255
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2678668
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#853548}
    
    TBR=msarda@chromium.org,qinmin@chromium.org,nicolaso@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I4d0ce7c8987259f9b8c066eec10d2dad39b76d85
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 88586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693630
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#853667}

--- chrome/browser/profiles/scoped_profile_keep_alive.cc
+++ chrome/browser/profiles/scoped_profile_keep_alive.cc
@@ -8,13 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_keep_alive_types.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "content/public/browser/browser_task_traits.h"
-#include "content/public/browser/browser_thread.h"
 
 ScopedProfileKeepAlive::ScopedProfileKeepAlive(const Profile* profile,
                                                ProfileKeepAliveOrigin origin)
     : profile_(profile), origin_(origin) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   DCHECK(profile_);
   // |profile_manager| can be nullptr in tests.
   auto* profile_manager = g_browser_process->profile_manager();
@@ -23,24 +20,9 @@ ScopedProfileKeepAlive::ScopedProfileKeepAlive(const Profile* profile,
 }
 
 ScopedProfileKeepAlive::~ScopedProfileKeepAlive() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  // The object that owns ScopedProfileKeepAlive might be owned by Profile,
-  // in which case triggering ~Profile() from here causes UAF bugs. Post
-  // RemoveKeepAlive() to a task to avoid this.
-  content::GetUIThreadTaskRunner({})->PostTask(
-      FROM_HERE,
-      base::BindOnce(&ScopedProfileKeepAlive::RemoveKeepAliveOnUIThread,
-                     profile_, origin_));
-}
-
-// static
-void ScopedProfileKeepAlive::RemoveKeepAliveOnUIThread(
-    const Profile* profile,
-    ProfileKeepAliveOrigin origin) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   // |profile_manager| could be nullptr if this is called during shutdown, e.g.
-  // for system/guest profiles or in tests.
+  // for system/guest profiles.
   auto* profile_manager = g_browser_process->profile_manager();
   if (profile_manager)
-    profile_manager->RemoveKeepAlive(profile, origin);
+    profile_manager->RemoveKeepAlive(profile_, origin_);
 }
