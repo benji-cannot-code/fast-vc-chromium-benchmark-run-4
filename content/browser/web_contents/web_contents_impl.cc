e0e4f4f0e189355e7024464e56e25e2c93b1841b
danakj <danakj@chromium.org>
2021-02-12 16:52:55 +0000

    Inform devtools when a primary main frame is created in a renderer.
    Previously this informed when a RenderView was created in a renderer,
    which is also when the main frame was created. Now, RenderViews exist
    as well for proxies, but the first one would have always been a main
    frame. We are removing the RenderViewCreated code path, and all
    observers are already gone. This explicit notification to devtools
    is moved to RenderFrameCreated.
    
    Note that we also adjust the call from SetDelegate, as devtools does
    not wish to be told about the WebContents until the renderer is live,
    also demonstrated in RenderFrameDevToolsAgentHost::AddAllAgentHosts().
    So we only tell devtools in SetDelegate if the renderer frame is live,
    otherwise deferring that call until RenderFrameCreated.
    
    R=avi@chromium.org, yangguo@chromium.org
    
    Bug: 489664, 763548
    Change-Id: I487add6fe7d256764a7f064992145ca8ccd242ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2689565
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853549}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1219,8 +1219,11 @@ void WebContentsImpl::SetDelegate(WebContentsDelegate* delegate) {
     // Ensure the visible RVH reflects the new delegate's preferences.
     if (view_)
       view_->SetOverscrollControllerEnabled(CanOverscrollContent());
-    if (GetRenderViewHost())
-      RenderFrameDevToolsAgentHost::WebContentsCreated(this);
+
+    // RenderFrameDevToolsAgentHost should not be told about the main renderer
+    // frame until/unless there is a `delegate_`.
+    if (GetMainFrame()->IsRenderFrameLive())
+      RenderFrameDevToolsAgentHost::WebContentsMainFrameCreated(this);
   }
 }
 
@@ -6314,6 +6317,15 @@ void WebContentsImpl::RenderFrameCreated(RenderFrameHost* render_frame_host) {
         view_->Focus();
       }
     }
+
+    // RenderFrameDevToolsAgentHost should not be told about the main renderer
+    // frame until/unless there is a `delegate_`.
+    if (delegate_) {
+      // TODO(crbug.com/1164280): Under MPArch, with multiple frame trees in a
+      // WebContents, this is intended to just notify about the main frame of
+      // the root page.
+      RenderFrameDevToolsAgentHost::WebContentsMainFrameCreated(this);
+    }
   }
 
   observers_.NotifyObservers(&WebContentsObserver::RenderFrameCreated,
@@ -6790,7 +6802,6 @@ void WebContentsImpl::RenderViewCreated(RenderViewHost* render_view_host) {
                         static_cast<void*>(render_view_host));
   if (delegate_) {
     view_->SetOverscrollControllerEnabled(CanOverscrollContent());
-    RenderFrameDevToolsAgentHost::WebContentsCreated(this);
   }
 }
 
