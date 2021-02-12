a947cbcbb2940b19e7f5270ac17f79c0f3ebd8f9
Olivier Yiptong <oyiptong@chromium.org>
2021-02-12 19:08:59 +0000

    Compute Pressure: Feature flag to gate the API
    This is a feature flag that gates access to the Compute Pressure API.
    This API provides developers access to CPU performance information.
    
    Bug: 1067627
    Change-Id: If1b3017f4b2a9326a50fe0ca5b1ccffe08787497
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2691667
    Commit-Queue: enne <enne@chromium.org>
    Auto-Submit: Olivier Yiptong <oyiptong@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853606}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -353,6 +353,7 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
           {"BlockHTMLParserOnStyleSheets",
            blink::features::kBlockHTMLParserOnStyleSheets},
           {"COLRV1Fonts", blink::features::kCOLRV1Fonts},
+          {"ComputePressure", blink::features::kComputePressure},
           {"CSSColorSchemeUARendering", features::kCSSColorSchemeUARendering},
           {"DeclarativeShadowDOM", blink::features::kDeclarativeShadowDOM},
           {"FeaturePolicyForClientHints",
