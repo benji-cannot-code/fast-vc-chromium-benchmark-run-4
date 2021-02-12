afcaa32470179b694342f8ff6e6dff9bca97ddd6
Luca Hunkeler <hluca@google.com>
2021-02-12 11:48:14 +0000

    [Autofill Assistant] Add support for website screenshots
    When Autofill Assistant is in an error state, the content of the webpage
    is more important to determine what went wrong than the content of the
    bottomsheet. For this reason clicking the feedback chip should
    attach the screenshot of the webpage.
    Clicking the send feedback option in the header will still send the
    bottom sheet screenshot.
    
    Bug: b/178809950
    Change-Id: I20f223f4a086f2a2f80f6ddc09b1f9dc531ed470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2644618
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853476}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiController.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiController.java
@@ -20,6 +20,7 @@ import org.chromium.chrome.browser.autofill_assistant.carousel.AssistantChip;
 import org.chromium.chrome.browser.autofill_assistant.metrics.DropOutReason;
 import org.chromium.chrome.browser.autofill_assistant.onboarding.BaseOnboardingCoordinator;
 import org.chromium.chrome.browser.customtabs.CustomTabActivity;
+import org.chromium.chrome.browser.feedback.ScreenshotMode;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.ui.TabObscuringHandler;
@@ -290,8 +291,8 @@ public class AutofillAssistantUiController {
     }
 
     @CalledByNative
-    private void showFeedback(String debugContext) {
-        mCoordinator.showFeedback(debugContext);
+    private void showFeedback(String debugContext, @ScreenshotMode int screenshotMode) {
+        mCoordinator.showFeedback(debugContext, screenshotMode);
     }
 
     @CalledByNative
--- chrome/browser/android/autofill_assistant/ui_controller_android.cc
+++ chrome/browser/android/autofill_assistant/ui_controller_android.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/autofill_assistant/client_android.h"
 #include "chrome/browser/android/autofill_assistant/generic_ui_root_controller_android.h"
 #include "chrome/browser/android/autofill_assistant/ui_controller_android_utils.h"
+#include "chrome/browser/android/feedback/screenshot_mode.h"
 #include "chrome/browser/autofill/android/personal_data_manager_android.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
@@ -66,6 +67,7 @@ using base::android::AttachCurrentThread;
 using base::android::ConvertUTF8ToJavaString;
 using base::android::JavaParamRef;
 using base::android::JavaRef;
+using chrome::android::ScreenshotMode;
 
 namespace autofill_assistant {
 
@@ -532,15 +534,12 @@ void UiControllerAndroid::SetSpinPoodle(bool enabled) {
   header_model_->SetSpinPoodle(enabled);
 }
 
-void UiControllerAndroid::OnFeedbackButtonClicked() {
+void UiControllerAndroid::OnHeaderFeedbackButtonClicked() {
   JNIEnv* env = AttachCurrentThread();
   Java_AutofillAssistantUiController_showFeedback(
       env, java_object_,
-      ConvertUTF8ToJavaString(env, ui_delegate_->GetDebugContext()));
-}
-
-void UiControllerAndroid::OnFeedbackFormRequested() {
-  OnFeedbackButtonClicked();
+      ConvertUTF8ToJavaString(env, ui_delegate_->GetDebugContext()),
+      ScreenshotMode::COMPOSITOR);
 }
 
 void UiControllerAndroid::OnViewEvent(const EventHandler::EventKey& key) {
@@ -885,7 +884,8 @@ void UiControllerAndroid::OnFeedbackButtonClicked(
   // directly stop.
   Java_AutofillAssistantUiController_showFeedback(
       env, java_object_,
-      ConvertUTF8ToJavaString(env, ui_delegate_->GetDebugContext()));
+      ConvertUTF8ToJavaString(env, ui_delegate_->GetDebugContext()),
+      ScreenshotMode::COMPOSITOR);
 
   OnUserActionSelected(env, jcaller, index);
 }
