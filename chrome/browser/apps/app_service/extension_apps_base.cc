f84160ec810873940fdf956d6d14f015689070ca
Nancy Wang <nancylingwang@chromium.org>
2021-02-12 17:48:41 +0000

    Add launch source for full restore.
    This is the follow up for CL:2682204, adding the launch source for the
    app launching from the full restore service during the system init
    phase.
    
    BUG=1146900
    
    Change-Id: I9183426905fd99ef22872f10dec631c9eb3731d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690766
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/master@{#853577}

--- chrome/browser/apps/app_service/extension_apps_base.cc
+++ chrome/browser/apps/app_service/extension_apps_base.cc
@@ -116,6 +116,7 @@ ash::ShelfLaunchSource ConvertLaunchSource(
     case apps::mojom::LaunchSource::kFromArc:
     case apps::mojom::LaunchSource::kFromSharesheet:
     case apps::mojom::LaunchSource::kFromReleaseNotesNotification:
+    case apps::mojom::LaunchSource::kFromFullRestore:
       return ash::LAUNCH_FROM_UNKNOWN;
   }
 }
@@ -436,6 +437,7 @@ void ExtensionAppsBase::Launch(const std::string& app_id,
     case apps::mojom::LaunchSource::kFromArc:
     case apps::mojom::LaunchSource::kFromSharesheet:
     case apps::mojom::LaunchSource::kFromReleaseNotesNotification:
+    case apps::mojom::LaunchSource::kFromFullRestore:
       break;
   }
 
