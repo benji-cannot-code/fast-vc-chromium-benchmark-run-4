2753d976dda67e436571ab81e0cc4c5f0e2ecca9
Ilya Nikolaevskiy <ilnik@chromium.org>
2021-02-12 14:00:19 +0000

    Move SharedMemoryPool to be reusable
    This change moves VideoEncodeAcceleratorAdapter::SharedMemoryPool
    to a separate file, makes it thread safe and allows change of region
    size on-the-fly.
    
    GpuEncodeAcceleratorFactories was removed, now
    UnsafeShareMemoryRegion::Create() is called directly.
    
    If the requested size is increased, free regions are purged.
    
    Now the pool returns a SharedMemoryHandle which holds the memory and
    returns it to the pool when destroyed.
    
    Bug: 1120900
    Change-Id: I9ab2ba13e1a451bb4beff8ea16e2e0076add7c1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2682654
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853503}

--- media/video/video_encode_accelerator_adapter.h
+++ media/video/video_encode_accelerator_adapter.h
@@ -10,11 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "base/containers/circular_deque.h"
+#include "base/containers/flat_map.h"
 #include "base/containers/queue.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/optional.h"
 #include "base/synchronization/lock.h"
 #include "media/base/media_export.h"
+#include "media/base/shared_memory_pool.h"
 #include "media/base/video_encoder.h"
 #include "media/video/video_encode_accelerator.h"
 #include "ui/gfx/geometry/size.h"
@@ -74,7 +76,6 @@ class MEDIA_EXPORT VideoEncodeAcceleratorAdapter
   static void DestroyAsync(std::unique_ptr<VideoEncodeAcceleratorAdapter> self);
 
  private:
-  class SharedMemoryPool;
   enum class State {
     kNotInitialized,
     kWaitingForFirstFrame,
@@ -116,6 +117,9 @@ class MEDIA_EXPORT VideoEncodeAcceleratorAdapter
 
   scoped_refptr<SharedMemoryPool> output_pool_;
   scoped_refptr<SharedMemoryPool> input_pool_;
+  std::unique_ptr<SharedMemoryPool::SharedMemoryHandle> output_handle_holder_;
+  size_t input_buffer_size_;
+
   std::unique_ptr<VideoEncodeAccelerator> accelerator_;
   GpuVideoAcceleratorFactories* gpu_factories_;
 
