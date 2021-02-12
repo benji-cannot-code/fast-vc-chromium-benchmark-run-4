aa5f8ac6f77529a27a30d58f980a28cfdb6a6a6f
dpapad <dpapad@chromium.org>
2021-02-12 23:07:17 +0000

    Settings: Fix line-height of minimum font size preview text.
    Fixed: 1177657
    Change-Id: I27c1f64dc9c834ea8ea8d50f4ae3dcde75628732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2691935
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853707}

--- chrome/browser/resources/settings/appearance_page/appearance_fonts_page.js
+++ chrome/browser/resources/settings/appearance_page/appearance_fonts_page.js
@@ -123,6 +123,6 @@ Polymer({
 
   /** @private */
   onMinimumSizeChange_() {
-    this.$.minimumSizeSample.hidden = this.computeMinimumFontSize_() <= 0;
+    this.$.minimumSizeFontPreview.hidden = this.computeMinimumFontSize_() <= 0;
   },
 });
