830ece0316ffb6470fb0bbf6104e02170d3c4e0a
Benji Fischman <befischm@microsoft.com>
2021-02-12 03:00:59 +0000

    Refactor normalization logic on chrome://histograms page.
    Normalization was causing bar lengths to be incorrect as described in crbug/996424.
    This CL adds deterministic unit tests for the code updates in histograms and sparse_histograms.
    All Histograms should display consistent and reproducible ascii graphs that scale as appropriate to data volume and screen size.
    
    Bug: 996424
    Change-Id: I8fc1df701962aa11e7558e46d17d88a56b88438b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2661033
    Commit-Queue: Benjamin Fischman <befischm@microsoft.com>
    Auto-Submit: Benjamin Fischman <befischm@microsoft.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853372}

--- base/metrics/histogram.h
+++ base/metrics/histogram.h
@@ -262,9 +262,6 @@ class BASE_EXPORT Histogram : public HistogramBase {
   // Method to override to skip the display of the i'th bucket if it's empty.
   virtual bool PrintEmptyBucket(uint32_t index) const;
 
-  // Get normalized size, relative to the ranges(i).
-  virtual double GetBucketSize(Count current, uint32_t i) const;
-
   // Return a string description of what goes in a given bucket.
   // Most commonly this is the numeric value, but in derived classes it may
   // be a name (or string description) given to the bucket.
@@ -276,6 +273,7 @@ class BASE_EXPORT Histogram : public HistogramBase {
   FRIEND_TEST_ALL_PREFIXES(HistogramTest, BoundsTest);
   FRIEND_TEST_ALL_PREFIXES(HistogramTest, BucketPlacementTest);
   FRIEND_TEST_ALL_PREFIXES(HistogramTest, CorruptSampleCounts);
+  FRIEND_TEST_ALL_PREFIXES(HistogramTest, GetPeakBucketSize);
 
   friend class StatisticsRecorder;  // To allow it to delete duplicates.
   friend class StatisticsRecorderTest;
@@ -421,8 +419,6 @@ class BASE_EXPORT LinearHistogram : public Histogram {
                   const DelayedPersistentAllocation& logged_counts,
                   HistogramSamples::Metadata* meta,
                   HistogramSamples::Metadata* logged_meta);
-
-  double GetBucketSize(Count current, uint32_t i) const override;
 
   // If we have a description for a bucket, then return that.  Otherwise
   // let parent class provide a (numeric) description.
@@ -598,8 +594,6 @@ class BASE_EXPORT CustomHistogram : public Histogram {
 
   // HistogramBase implementation:
   void SerializeInfoImpl(base::Pickle* pickle) const override;
-
-  double GetBucketSize(Count current, uint32_t i) const override;
 
  private:
   friend BASE_EXPORT HistogramBase* DeserializeHistogramInfo(
