7b564a3ca1ff9e51b9f7c812b2c4a0aa86a124ba
Mugdha Lakhani <nator@chromium.org>
2021-02-12 17:26:16 +0000

    [AW] Create a WebViewApkProcess, PrefService.
    Create a class to hold global state for webview_apk process, and add
    logic to create a PrefService. This'll be used by component updater
    to store some persisted data.
    
    Bug: 1173887
    Change-Id: I7a23f200d068dfe09a8f945f9254eb0b0dd88c8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675929
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853566}

--- android_webview/nonembedded/webview_apk_application.cc
+++ android_webview/nonembedded/webview_apk_application.cc
@@ -6,14 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/nonembedded/nonembedded_jni_headers/WebViewApkApplication_jni.h"
 
 #include "android_webview/common/aw_resource_bundle.h"
+#include "android_webview/nonembedded/webview_apk_process.h"
 #include "base/android/base_jni_onload.h"
 #include "base/check.h"
 
 namespace android_webview {
 
-void JNI_WebViewApkApplication_InitializePakResources(JNIEnv* env) {
+void JNI_WebViewApkApplication_InitializeGlobalsAndResources(JNIEnv* env) {
   CHECK(base::android::OnJNIOnLoadInit());
   InitIcuAndResourceBundleBrowserSide();
+  WebViewApkProcess::GetInstance();
 }
 
 }  // namespace android_webview
