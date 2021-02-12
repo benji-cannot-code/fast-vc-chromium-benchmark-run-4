55887dad5fde5cb32915032d3125612bdd218fee
Andres Olivares <andoli@chromium.org>
2021-02-12 15:20:05 +0000

    Add call to show sources panel in layout tests
    These tests fail because with our work on migrating module.json
    extensions (https://crbug.com/1134103) we have altered the availability
    of the sources panel at certain points of the tests' implementations. We
    manually add the sources panel to the global scope by calling
    TestRunner.showPanel('sources') before the tests are implemented to fix
    this.
    
    Bug: 1177568
    Change-Id: I1d7e3d1151899a9d7f8b247595c8aaa0eeb9dfba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690561
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853523}

--- third_party/blink/web_tests/http/tests/devtools/console/console-error-on-call-frame.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-error-on-call-frame.js
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       debugger;
     }
   `);
+  await TestRunner.showPanel('sources');
 
   SourcesTestRunner.startDebuggerTest(step1);
 
