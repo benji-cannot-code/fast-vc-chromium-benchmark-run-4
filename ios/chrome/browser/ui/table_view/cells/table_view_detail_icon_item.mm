242606ae549853e1f8bf2a933f291a5cadeae47b
Ewann <ewannpv@google.com>
2021-02-12 14:50:27 +0000

    [iOS][Settings] Fixes trailing content
    The trailing content padding of various cells is not the same.
    This CL globalizes this padding.
    
    Bug: 1177233, 922511
    Change-Id: Iecaf2005f88e894a52f7954b5fa569887b0e9bb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690348
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853514}

--- ios/chrome/browser/ui/table_view/cells/table_view_detail_icon_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_detail_icon_item.mm
@@ -21,8 +21,6 @@ namespace {
 // Proportion of Cell's textLabel/detailTextLabel. This guarantees that the
 // textLabel occupies 75% of the row space and detailTextLabel occupies 25%.
 const CGFloat kCellLabelsWidthProportion = 3.0f;
-// Padding used between the detail text and the AccessoryType.
-const CGFloat kDetailTextTrailingPadding = 6;
 
 }  // namespace
 
@@ -167,7 +165,7 @@ const CGFloat kDetailTextTrailingPadding = 6;
       // Text labels.
       [_textStackView.trailingAnchor
           constraintEqualToAnchor:contentView.trailingAnchor
-                         constant:-kDetailTextTrailingPadding],
+                         constant:-kTableViewTrailingContentPadding],
       _iconHiddenConstraint,
 
       // Leading constraint for |customSeparator|.
