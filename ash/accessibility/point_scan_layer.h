9ee1ee0a8a350c7a34fadc881c80f4c0cca82051
Anastasia Helfinstein <anastasi@google.com>
2021-02-12 00:18:17 +0000

    [Switch Access] Refactor point_scan_layer.cc
    This change does a few things to clarify and simplify the point scan
    layer code. Specifically:
    
    - Adds constructor parameters to represent the layer's orientation
      (horizontal or vertical) and size (range or line)
    - Removes ad-hoc booleans representing the orientation and size
    - Combines four similar methods into a single Start() method
    - Removes duplicate instance of bounds_ field
    
    AX-Relnotes: n/a.
    Bug: 1061537
    Change-Id: I4911eefc4cb1ca291ffe8e7731055aa935f84846
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2689218
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/master@{#853306}

--- ash/accessibility/point_scan_layer.h
+++ ash/accessibility/point_scan_layer.h
@@ -9,27 +9,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accessibility/accessibility_layer.h"
 #include "ash/accessibility/point_scan_layer_animation_info.h"
 #include "ash/ash_export.h"
+#include "ui/compositor/layer.h"
 
 namespace ash {
 
 class PointScanLayer : public AccessibilityLayer {
  public:
-  explicit PointScanLayer(AccessibilityLayerDelegate* delegate);
+  enum Orientation {
+    HORIZONTAL = 0,
+    VERTICAL,
+  };
+
+  enum Type {
+    LINE = 0,
+    RANGE,
+  };
+
+  explicit PointScanLayer(AccessibilityLayerDelegate* delegate,
+                          Orientation orientation,
+                          Type type);
   ~PointScanLayer() override = default;
 
   PointScanLayer(const PointScanLayer&) = delete;
   PointScanLayer& operator=(const PointScanLayer&) = delete;
 
-  // Begins sweeping a line horizontally across the screen, for the user to pick
-  // an x-coordinate.
-  void StartHorizontalRangeScanning();
-  void StartHorizontalScanning();
-  void StartVerticalRangeScanning();
-  void StartVerticalScanning();
+  void Start();
   void Pause();
 
   bool IsMoving() const;
-  gfx::Rect bounds() const { return bounds_; }
+  gfx::Rect bounds() { return layer()->bounds(); }
 
   // AccessibilityLayer overrides:
   bool CanAnimate() const override;
@@ -46,17 +54,10 @@ class PointScanLayer : public AccessibilityLayer {
     gfx::Point end;
   };
 
-  // The bounds within which we are scanning.
-  gfx::Rect bounds_;
-
-  // The line currently being drawn.
+  Orientation orientation_;
+  Type type_;
   Line line_;
-
   bool is_moving_ = false;
-
-  bool is_range_scan_ = false;
-
-  bool is_horizontal_range_ = false;
 };
 
 }  // namespace ash
