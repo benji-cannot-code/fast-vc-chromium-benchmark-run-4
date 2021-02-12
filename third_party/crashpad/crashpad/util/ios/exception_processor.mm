b5bc951efcbcdc0b84409a2490c28e3aad3aaadd
Justin Cohen <justincohen@google.com>
2021-02-12 13:37:31 +0000

    Update Crashpad to adf40ba77cd633257e3957b6671e70cc1e0a0c50
    adf40ba77cd6 ios: Handle libunwind.h header changes
    
    Change-Id: I6ee69a4af1f07b8e033ed19f43649212b7ea7b47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2691470
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853495}

--- third_party/crashpad/crashpad/util/ios/exception_processor.mm
+++ third_party/crashpad/crashpad/util/ios/exception_processor.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "util/ios/exception_processor.h"
 
+#include <Availability.h>
 #import <Foundation/Foundation.h>
 #include <TargetConditionals.h>
 #include <cxxabi.h>
@@ -175,8 +176,13 @@ id ObjcExceptionPreprocessor(id exception) {
     }
 
     // Check to see if the handler is really an exception handler.
-    __personality_routine p =
-        reinterpret_cast<__personality_routine>(frame_info.handler);
+#if defined(__IPHONE_14_5) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_14_5
+    using personality_routine = _Unwind_Personality_Fn;
+#else
+    using personality_routine = __personality_routine;
+#endif
+    personality_routine p =
+        reinterpret_cast<personality_routine>(frame_info.handler);
 
     // From 10.15.0 libunwind-35.4/src/UnwindLevel1.c.
     _Unwind_Reason_Code personalityResult = (*p)(
