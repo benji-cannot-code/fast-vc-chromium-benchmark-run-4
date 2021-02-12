eca00012be196b4cf6270bc14062ae7648f085b2
Maciek Slusarczyk <mslus@chromium.org>
2021-02-12 18:22:16 +0000

    Histogram for ChromeOS offline login.
    Introduces a new histogram to track offline login attempts when user
    pods are hidden on the login screen.
    
    Bug: b:149562898
    Change-Id: If69c129458122947b7899500f2b44e071042b156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679746
    Commit-Queue: Maciek Slusarczyk <mslus@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853589}

--- chrome/browser/chromeos/login/screens/offline_login_screen.cc
+++ chrome/browser/chromeos/login/screens/offline_login_screen.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/screens/offline_login_screen.h"
 
 #include "base/bind.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/time/default_clock.h"
 #include "base/time/time.h"
 #include "chrome/browser/browser_process.h"
@@ -35,10 +36,24 @@ constexpr char kUserActionCancel[] = "cancel";
 constexpr const base::TimeDelta kIdleTimeDelta =
     base::TimeDelta::FromMinutes(3);
 
+// These values should not be renumbered and numeric values should never
+// be reused. This must be kept in sync with ChromeOSHiddenUserPodsOfflineLogin
+// in tools/metrics/histogram/enums.xml
+enum class OfflineLoginEvent {
+  kOfflineLoginEnabled = 0,
+  kOfflineLoginBlockedByTimeLimit = 1,
+  kOfflineLoginBlockedByInvalidToken = 2,
+  kMaxValue = kOfflineLoginBlockedByInvalidToken,
+};
+
 inline std::string GetEnterpriseDomainManager() {
   policy::BrowserPolicyConnectorChromeOS* connector =
       g_browser_process->platform_part()->browser_policy_connector_chromeos();
   return connector->GetEnterpriseDomainManager();
+}
+
+void RecordEvent(OfflineLoginEvent event) {
+  base::UmaHistogramEnumeration("Login.OfflineLoginWithHiddenUserPods", event);
 }
 
 }  // namespace
@@ -174,8 +189,10 @@ void OfflineLoginScreen::HandleEmailSubmitted(const std::string& email) {
   }
 
   if (offline_limit_expired) {
+    RecordEvent(OfflineLoginEvent::kOfflineLoginBlockedByTimeLimit);
     view_->ShowOnlineRequiredDialog();
   } else {
+    RecordEvent(OfflineLoginEvent::kOfflineLoginEnabled);
     view_->ShowPasswordPage();
   }
 }
