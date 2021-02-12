9384b33593c7f875d4223dfa56f7ab66b3b8fa8f
Leonard Grey <lgrey@chromium.org>
2021-02-12 19:19:03 +0000

    Commander: introduce WindowCommandSource
    This adds commands to switch windows and merge the current window into
    another (same-profiled) window.
    
    Individual windows will also appear as nouns if the user types more
    than two characters. Currently, executing the command will switch to
    the window; I experimented with giving switch and merge options but I
    think we're not there yet with UI for three phase commands (i.e.
    [window 1] > merge with window > [window 2]
    
    Bug: 1014639
    Change-Id: Iacf3e3dc1ead8b9eca658efc7209e5d64fd657f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2691546
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853612}

--- chrome/browser/resources/commander/app.js
+++ chrome/browser/resources/commander/app.js
@@ -93,8 +93,6 @@ export class CommanderAppElement extends PolymerElement {
       this.browserProxy_.promptCancelled();
       this.promptText_ = null;
       this.$.input.value = this.savedInput_;
-      e.preventDefault();
-      this.onInput_();
     }
   }
 
@@ -122,7 +120,6 @@ export class CommanderAppElement extends PolymerElement {
       this.promptText_ = viewModel.promptText || null;
       this.savedInput_ = this.$.input.value;
       this.$.input.value = '';
-      this.onInput_();
     }
   }
 
