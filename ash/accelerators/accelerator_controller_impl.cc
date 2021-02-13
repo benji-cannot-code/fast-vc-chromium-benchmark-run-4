86fbf0d874a7b6649180420b1e7c1686b694e2ce
Cattalyya Nuengsigkapian <cattalyya@chromium.org>
2021-02-13 00:07:48 +0000

    Bento: AltTabMode: Support ChromeVox and update keyboard nav
    # Keyboard Navigation (to follow the updates in the spec)
    - Clicking a button during keyboard nav resets to focus window.
    - Track alt-tab slider focus in the slider class to ensure the
    consistency between the focus visibility and the focused component.
    - If there's no window, pressing the Down arrow key does nothing.
    
    # ChromeVox a11y support
    - Announce alt-tab mode, target window, and directional cue via
    ChromeVox for keyboard navigation and button clicking.
    - Disable the target window focus during alt-tab mode switch to
    avoid repetitive target window announcement.
    
    # NIT bug fixing
    - no recent items zero size
    
    Bug: 1161247
    Test: Manual test and `ash_unittests gtest_filter=ModeSelectionWindowCycleControllerTest.TabSliderButtonsKeyboardNavigationAfterClick`
    
    Change-Id: I2c4b91d1ff0985758dd7fdfffb2ff997592f4426
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2687776
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Cattalyya Nuengsigkapian <cattalyya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853735}

--- ash/accelerators/accelerator_controller_impl.cc
+++ ash/accelerators/accelerator_controller_impl.cc
@@ -286,7 +286,7 @@ void HandleCycleBackwardMRU(const ui::Accelerator& accelerator) {
     base::RecordAction(base::UserMetricsAction("Accel_PrevWindow_Tab"));
 
   Shell::Get()->window_cycle_controller()->HandleCycleWindow(
-      WindowCycleController::BACKWARD);
+      WindowCycleController::WindowCyclingDirection::kBackward);
 }
 
 void HandleCycleForwardMRU(const ui::Accelerator& accelerator) {
@@ -294,7 +294,7 @@ void HandleCycleForwardMRU(const ui::Accelerator& accelerator) {
     base::RecordAction(base::UserMetricsAction("Accel_NextWindow_Tab"));
 
   Shell::Get()->window_cycle_controller()->HandleCycleWindow(
-      WindowCycleController::FORWARD);
+      WindowCycleController::WindowCyclingDirection::kForward);
 }
 
 void HandleActivateDesk(const ui::Accelerator& accelerator) {
