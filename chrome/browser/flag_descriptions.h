e236ee54a232087b6c7258ff2d220fbf1e153249
Andre Le <leandre@chromium.org>
2021-02-13 22:00:13 +0000

    Scalable Status Area: Add a flag in chrome://flags
    Add flag for Scalable Status Area feature.
    
    BUG=1161557
    
    Change-Id: If50316d3628c0135305e54d70a0a84881884a9cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2688321
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853858}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2696,6 +2696,9 @@ extern const char kReleaseNotesNotificationDescription[];
 extern const char kReleaseNotesNotificationAllChannelsName[];
 extern const char kReleaseNotesNotificationAllChannelsDescription[];
 
+extern const char kScalableStatusAreaName[];
+extern const char kScalableStatusAreaDescription[];
+
 extern const char kScanningUIName[];
 extern const char kScanningUIDescription[];
 
