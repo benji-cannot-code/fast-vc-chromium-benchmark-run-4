0fe8df19dc4f3f3eb7ea488938bafba104f32c79
Joel Hockey <joelhockey@chromium.org>
2021-02-13 17:52:49 +0000

    Add ui::FileInfo fuzzer
    Bug: 1175483
    Change-Id: Id5c7d7dccbb6ccd4978ef1ea62a5ff8913aaa957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2694286
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853845}

---
+++ ui/base/clipboard/file_info_fuzzer.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/strings/string_piece.h"
+#include "ui/base/clipboard/file_info.h"
+
+namespace ui {
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  base::StringPiece data_piece(reinterpret_cast<const char*>(data), size);
+  std::vector<ui::FileInfo> files = ui::URIListToFileInfos(data_piece);
+  return 0;
+}
+
+}  // namespace ui
