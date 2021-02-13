9868d0a8e298038293aceb6120dade00a46a2585
Juliet Levesque <julietlevesque@google.com>
2021-02-13 00:18:07 +0000

    [OS Settings] Migrate os_settings_page to Polymer3.
    Migrate os_settings_page to Polymer3 and write os_settings_page_test and
    rename os_settings_page_test to os_settings_ui_test to reflect more
    accurately the testing.
    
    Bug: 1045266
    Change-Id: Id021d22afa86b14dd7c709fb0bfb45fd717435bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2691186
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853741}

--- chrome/browser/resources/settings/chromeos/os_settings_page/os_settings_page.js
+++ chrome/browser/resources/settings/chromeos/os_settings_page/os_settings_page.js
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview
  * 'os-settings-page' is the settings page containing the actual OS settings.
  */
-(function() {
-'use strict';
 
 Polymer({
   is: 'os-settings-page',
@@ -372,4 +370,3 @@ Polymer({
     return bool.toString();
   },
 });
-})();
