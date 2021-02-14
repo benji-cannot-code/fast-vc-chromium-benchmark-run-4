75d5b1ac82ac2a39cca196fc45163a16f5694042
Alexander Timin <altimin@chromium.org>
2021-02-14 20:24:43 +0000

    [tracing] Add TracedValue support to WTF::String.
    Add WTF::String::WriteIntoTracedValue method and allow WTF::String to be
    passed directly to TRACE_EVENT arguments.
    
    R=haraken@chromium.org
    BUG=1137154
    
    Cq-Include-Trybots: luci.chromium.try:linux-perfetto-rel
    Change-Id: I10d29dc29726940ba982489122bbce473d26a898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690542
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Auto-Submit: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853922}

--- third_party/blink/renderer/platform/wtf/text/wtf_string.h
+++ third_party/blink/renderer/platform/wtf/text/wtf_string.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/string_view.h"
 #include "third_party/blink/renderer/platform/wtf/wtf_export.h"
 #include "third_party/blink/renderer/platform/wtf/wtf_size_t.h"
+#include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
 
 #ifdef __OBJC__
 #include <objc/objc.h>
@@ -557,6 +558,8 @@ class WTF_EXPORT String {
   // For use in the debugger.
   void Show() const;
 #endif
+
+  void WriteIntoTracedValue(perfetto::TracedValue context) const;
 
  private:
   friend struct HashTraits<String>;
