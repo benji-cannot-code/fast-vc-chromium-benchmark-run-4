508bd25e179e0a23cc3eaf495e28f860408cf420
Francois Beaufort <beaufort.francois@gmail.com>
2021-02-15 12:04:44 +0000

    [Android] Show PWA bottom sheet UI instead of A2S prompt when deferred
    This CL makes sure the PWA bottom sheet UI is shown instead of the
    regular Add-To-HomeScreen prompt when the appropriate flag is turned on
    and the prompt was deferred programmatically by the website.
    
    Bug: 1169587
    Change-Id: I691defb3c6a9eca7e5ca519a183184e8f483b4b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2673493
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854007}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -197,6 +197,7 @@ import org.chromium.components.sync.PassphraseType;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.components.webapk.lib.client.WebApkValidator;
 import org.chromium.components.webapps.AddToHomescreenCoordinator;
+import org.chromium.components.webapps.InstallTrigger;
 import org.chromium.components.webxr.ArDelegate;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.ScreenOrientationProvider;
@@ -2263,7 +2264,8 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
                 PwaBottomSheetController controller =
                         PwaBottomSheetControllerProvider.from(getWindowAndroid());
                 if (controller != null) {
-                    controller.requestOrExpandBottomSheetInstaller(currentTab.getWebContents());
+                    controller.requestOrExpandBottomSheetInstaller(
+                            currentTab.getWebContents(), InstallTrigger.MENU);
                     return true;
                 }
             }
