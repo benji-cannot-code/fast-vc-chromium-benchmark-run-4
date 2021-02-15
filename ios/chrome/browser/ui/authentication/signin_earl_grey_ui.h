710be82bb921e6a92ba65fe66196ed6e60ccc861
Jérôme Lebel <jlebel@chromium.org>
2021-02-15 12:15:08 +0000

    [iOS] Fix a typo in a comment in signin_earl_grey_ui.h
    Fix a type in signin_earl_grey_ui.h. Changing:
    // Calls +[SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity syncing:YES].
    to:
    // Calls +[SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity enableSync:YES].
    
    Related to: crrev.com/c/2684899
    
    Change-Id: Idc9c1c9095605e84930af4ef8578fb36c7dd5d10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2689777
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854008}

--- ios/chrome/browser/ui/authentication/signin_earl_grey_ui.h
+++ ios/chrome/browser/ui/authentication/signin_earl_grey_ui.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Test methods that perform sign in actions on Chrome UI.
 @interface SigninEarlGreyUI : NSObject
 
-// Calls +[SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity syncing:YES].
+// Calls +[SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity enableSync:YES].
 + (void)signinWithFakeIdentity:(FakeChromeIdentity*)fakeIdentity;
 
 // Signs the account for |fakeIdentity| into Chrome through the Settings screen,
