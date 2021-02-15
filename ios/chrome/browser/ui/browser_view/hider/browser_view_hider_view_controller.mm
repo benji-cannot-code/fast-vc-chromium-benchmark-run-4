7d886734f7d00b9ee0610c8d492590775d124299
Robbie Gibson <rkgibson@google.com>
2021-02-15 17:04:20 +0000

    [iOS][Thumb Strip] Change hider view to secondary_background_color
    As mentioned in the bug, the thumb strip browser hider view should
    have a different background color.
    
    Fixed: 1171425
    Change-Id: I33fc6eb88efcba29b0b2a986d0a8a1831040d310
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2695378
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854070}

--- ios/chrome/browser/ui/browser_view/hider/browser_view_hider_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/hider/browser_view_hider_view_controller.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/toolbar/public/toolbar_constants.h"
 #import "ios/chrome/browser/ui/util/layout_guide_names.h"
 #import "ios/chrome/browser/ui/util/named_guide.h"
+#import "ios/chrome/common/ui/colors/dynamic_color_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 
@@ -30,7 +31,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  self.view.backgroundColor = [UIColor colorNamed:kBackgroundDarkColor];
+  if (@available(iOS 13, *)) {
+    // TODO(crbug.com/981889): When iOS 12 is dropped, only the next line is
+    // needed for styling. Every other check for |incognitoStyle| can be
+    // removed, as well as the incognito specific assets.
+    self.view.overrideUserInterfaceStyle = UIUserInterfaceStyleDark;
+  }
+  UIColor* backgroundColor = color::DarkModeDynamicColor(
+      [UIColor colorNamed:kSecondaryBackgroundColor], true,
+      [UIColor colorNamed:kSecondaryBackgroundDarkColor]);
+  self.view.backgroundColor = backgroundColor;
   self.view.accessibilityIdentifier = @"BrowserViewHiderView";
   self.view.layer.cornerRadius = kTopCornerRadius;
   self.view.hidden = YES;
