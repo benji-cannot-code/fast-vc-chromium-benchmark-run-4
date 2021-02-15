8542818767b0eeed6c2eb83c4b05011cc221bddb
Kunihiko Sakamoto <ksakamoto@chromium.org>
2021-02-15 02:32:23 +0000

    Clean up error handling code in WebBundleURLLoaderFactory
    This is in preparation of crbug.com/1176493 and makes it easier to add
    more error conditions.
    
    Pure refactoring, no behavior changes.
    
    Bug: 1176493
    Change-Id: I47b62b774e5f0345d20a6dc969a7ef6bce21a0c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690768
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#853937}

--- services/network/web_bundle_url_loader_factory.h
+++ services/network/web_bundle_url_loader_factory.h
@@ -42,6 +42,9 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) WebBundleURLLoaderFactory {
   base::WeakPtr<WebBundleURLLoaderFactory> GetWeakPtr() const;
 
   void SetBundleStream(mojo::ScopedDataPipeConsumerHandle body);
+  void ReportErrorAndCancelPendingLoaders(SubresourceWebBundleLoadResult result,
+                                          mojom::WebBundleErrorType error,
+                                          const std::string& message);
   mojo::PendingRemote<mojom::URLLoaderClient> WrapURLLoaderClient(
       mojo::PendingRemote<mojom::URLLoaderClient> wrapped);
 
@@ -54,6 +57,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) WebBundleURLLoaderFactory {
   class BundleDataSource;
   class URLLoader;
 
+  bool HasError() const;
   void StartLoad(URLLoader* loader);
   void OnMetadataParsed(web_package::mojom::BundleMetadataPtr metadata,
                         web_package::mojom::BundleMetadataParseErrorPtr error);
@@ -62,7 +66,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) WebBundleURLLoaderFactory {
                         web_package::mojom::BundleResponseParseErrorPtr error);
   void OnMemoryQuotaExceeded();
   void OnDataCompleted();
-  void MaybeRecordLoadResult();
+  void MaybeRecordLoadResult(SubresourceWebBundleLoadResult result);
 
   GURL bundle_url_;
   mojo::Remote<mojom::WebBundleHandle> web_bundle_handle_;
@@ -72,10 +76,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) WebBundleURLLoaderFactory {
   std::unique_ptr<BundleDataSource> source_;
   mojo::Remote<web_package::mojom::WebBundleParser> parser_;
   web_package::mojom::BundleMetadataPtr metadata_;
-  web_package::mojom::BundleMetadataParseErrorPtr metadata_error_;
-  bool quota_exceeded_error_ = false;
+  base::Optional<SubresourceWebBundleLoadResult> load_result_;
   bool data_completed_ = false;
-  bool load_result_recorded_ = false;
   std::vector<base::WeakPtr<URLLoader>> pending_loaders_;
 
   base::WeakPtrFactory<WebBundleURLLoaderFactory> weak_ptr_factory_{this};
