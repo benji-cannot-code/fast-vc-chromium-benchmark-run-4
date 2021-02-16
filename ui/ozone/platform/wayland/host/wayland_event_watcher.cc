b7542be67ecea8ca2ba32fc71b03771e343c2951
Stephan Hartmann <stha09@googlemail.com>
2021-02-16 16:05:25 +0000

    IWYU: add cstring for std::strerror
    Bug: 1173630
    Change-Id: If9245585c7456b34474055d30e4fcb5e2671fd27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2697090
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#854284}

--- ui/ozone/platform/wayland/host/wayland_event_watcher.cc
+++ ui/ozone/platform/wayland/host/wayland_event_watcher.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/ozone/platform/wayland/host/wayland_event_watcher.h"
 
+#include <cstring>
+
 #include "base/bind.h"
 #include "base/check.h"
 #include "base/logging.h"
