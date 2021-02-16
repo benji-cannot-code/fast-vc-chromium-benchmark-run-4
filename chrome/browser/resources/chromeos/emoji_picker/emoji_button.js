46dffbadc7ee10de3b98b92030bc79d41a03d8c9
Kenton Lam <kentonlam@google.com>
2021-02-16 04:38:58 +0000

    Implement search of emoji names and keywords for emoji-picker.
    Pressing Enter from the search box focuses the first result or inserts the first result if it is the only one.
    The search results can be navigated with tab or the up/down arrow keys.
    Search of names and keywords is implemented using fuse.js.
    
    Also moved emoji-picker event handlers to be registered afterNextRender to marginally improve first load performance.
    
    Bug: 1152237
    Change-Id: Ic9addde3b1f267170124b30fc5aa6b5e5e1dccf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2689984
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Kenton Lam <kentonlam@google.com>
    Cr-Commit-Position: refs/heads/master@{#854153}

--- chrome/browser/resources/chromeos/emoji_picker/emoji_button.js
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_button.js
@@ -36,6 +36,10 @@ export class EmojiButton extends PolymerElement {
     super();
   }
 
+  getButton() {
+    return this.$.button;
+  }
+
   focusButton(options) {
     this.$.button.focus(options);
   }
