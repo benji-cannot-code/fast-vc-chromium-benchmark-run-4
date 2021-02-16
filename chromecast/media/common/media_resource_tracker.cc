664d36c063e52100489aa786eabb3bafd443035f
kylechar <kylechar@chromium.org>
2021-02-16 18:48:11 +0000

    Convert src/chromecast to use base::BindPostTask()
    This CL switches src/chromecast to use base::BindPostTask() instead of
    reimplementing the same functionality. The major difference for the task
    runner trampoline is that BindPostTask() will ensure the callbacks are
    destroyed on the specified task runner unlike the existing
    implementation.
    
    Leave the helper functions BindToCurrentThread() and
    BindToCurrentSequence() as those don't exist in base/ but update them to
    forward the location of the caller.
    
    Bug: 1140582
    Change-Id: I0bfc89f7751224e8c8d5166dd4597bc318020686
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2697447
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: Luke Halliwell (slow) <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854363}

--- chromecast/media/common/media_resource_tracker.cc
+++ chromecast/media/common/media_resource_tracker.cc
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/media/common/media_resource_tracker.h"
 
 #include "base/bind.h"
+#include "base/bind_post_task.h"
 #include "base/callback_helpers.h"
 #include "base/command_line.h"
-#include "chromecast/base/bind_to_task_runner.h"
+#include "base/single_thread_task_runner.h"
 #include "chromecast/public/cast_media_shlib.h"
 #include "chromecast/public/volume_control.h"
 
@@ -102,7 +103,7 @@ void MediaResourceTracker::MaybeCallFinalizeOnMediaThread(
   DCHECK(!finalize_completion_cb_);
 
   finalize_completion_cb_ =
-      BindToTaskRunner(ui_task_runner_, std::move(completion_cb));
+      base::BindPostTask(ui_task_runner_, std::move(completion_cb));
   if (!media_lib_initialized_) {
     if (finalize_completion_cb_)
       std::move(finalize_completion_cb_).Run();
