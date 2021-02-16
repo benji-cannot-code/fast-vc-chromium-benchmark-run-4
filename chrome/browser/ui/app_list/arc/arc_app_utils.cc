b7d12426b3f581ce86ffe0c0496513619195c2ef
Tim Sergeant <tsergeant@chromium.org>
2021-02-16 05:01:29 +0000

    Add default app order entry for YouTube Music WebAPK
    This Web APK entry will appear as a promise icon while waiting for the
    PWA to be preloaded through PAI.
    
    Bug: 1126247
    Change-Id: Idf585b4e3781c8f81ac5ca8e971e266d668ccaa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2684515
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854157}

--- chrome/browser/ui/app_list/arc/arc_app_utils.cc
+++ chrome/browser/ui/app_list/arc/arc_app_utils.cc
@@ -238,6 +238,7 @@ const char kPlayStoreAppId[] = "cnbgggchhmkkdmeppjobngjoejnihlei";
 const char kSettingsAppId[] = "mconboelelhjpkbdhhiijkgcimoangdj";
 const char kYoutubeAppId[] = "aniolghapcdkoolpkffememnhpphmjkl";
 const char kYoutubeMusicAppId[] = "hpdkdmlckojaocbedhffglopeafcgggc";
+const char kYoutubeMusicWebApkAppId[] = "jcmmigapnpnikbmnjknhcoageaeinihi";
 
 bool ShouldShowInLauncher(const std::string& app_id) {
   for (auto* const id : kAppIdsHiddenInLauncher) {
