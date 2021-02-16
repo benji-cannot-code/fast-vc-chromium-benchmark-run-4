2870cd1954a052e299cd65c8963cd437943f16f0
Elly Fong-Jones <ellyjones@chromium.org>
2021-02-16 18:09:42 +0000

    Revert "Disable flaky ProtocolHandlerApiTest.Registration"
    This reverts commit b43bcbd8e78c2baaf9fd9bfacc939f73306e43dd.
    
    Reason for revert: speculative actual fix for the test landed as r853130 :)
    
    Original change's description:
    > Disable flaky ProtocolHandlerApiTest.Registration
    >
    > Disabling ProtocolHandlerApiTest.Registration on Mac due to flakiness.
    >
    > Bug: 1177254
    > Change-Id: I9f6b8a67e22a0ee643acb57eeb09c95d3b9d5ba1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690388
    > Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    > Reviewed-by: Thiemo Nagel <tnagel@chromium.org>
    > Commit-Queue: Thiemo Nagel <tnagel@chromium.org>
    > Owners-Override: Thiemo Nagel <tnagel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#853093}
    
    TBR=ellyjones@chromium.org,tnagel@chromium.org,harrisonsean@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1177254
    Change-Id: Ife158b7fb7f078124fb73e7cb42c1c23e328cabc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693169
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Thiemo Nagel <tnagel@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854335}

--- chrome/browser/extensions/protocol_handler_apitest.cc
+++ chrome/browser/extensions/protocol_handler_apitest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "build/build_config.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry_factory.h"
 #include "chrome/browser/extensions/extension_apitest.h"
@@ -55,17 +54,10 @@ class ProtocolHandlerChangeWaiter : public ProtocolHandlerRegistry::Observer {
   base::RunLoop run_loop_;
 };
 
-// Flaky on Mac.  https://crbug.com/1177254
-#if defined(OS_MAC)
-#define MAYBE_Registration DISABLED_Registration
-#else
-#define MAYBE_Registration Registration
-#endif
-
 // This test verifies correct registration of protocol handlers using HTML5's
 // registerProtocolHandler in extension context and its validation with relaxed
 // security checks.
-IN_PROC_BROWSER_TEST_F(ProtocolHandlerApiTest, MAYBE_Registration) {
+IN_PROC_BROWSER_TEST_F(ProtocolHandlerApiTest, Registration) {
   ASSERT_TRUE(StartEmbeddedTestServer());
 
   // Initialize listener and result catcher before the test page is loaded to
