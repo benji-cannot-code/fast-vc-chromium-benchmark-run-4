b7bd34c2b520294123e916e4ba4b4514421494db
Alice Wang <aliceywang@chromium.org>
2021-02-16 16:52:37 +0000

    [Android] Fix child account status setup during the sign-in
    This CL fixes the bug that child account status did not propagate to
    Profile when users sign in and adds two render tests for the account
    management screen in child account mode.
    
    Bug: 1045525
    Change-Id: I9ed41c6d604694550afe182c61bc22710da6036b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2614424
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854301}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/AccountManagementFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/AccountManagementFragmentTest.java
@@ -17,6 +17,7 @@ import static org.hamcrest.CoreMatchers.is;
 
 import static org.chromium.chrome.test.util.ViewUtils.onViewWaiting;
 
+import android.accounts.Account;
 import android.view.View;
 
 import androidx.test.filters.MediumTest;
@@ -30,6 +31,7 @@ import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -43,12 +45,28 @@ import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.chrome.test.util.browser.signin.AccountManagerTestRule;
+import org.chromium.components.signin.ChildAccountStatus;
 import org.chromium.components.signin.identitymanager.ConsentLevel;
+import org.chromium.components.signin.test.util.FakeAccountManagerFacade;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 
+/**
+ * Tests {@link AccountManagementFragment}.
+ */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 public class AccountManagementFragmentTest {
+    private static final String CHILD_ACCOUNT_EMAIL = "child@gmail.com";
+
+    private final FakeAccountManagerFacade mFakeFacade = new FakeAccountManagerFacade(null) {
+        @Override
+        public void checkChildAccountStatus(Account account, ChildAccountStatusListener listener) {
+            listener.onStatusReady(CHILD_ACCOUNT_EMAIL.equals(account.name)
+                            ? ChildAccountStatus.REGULAR_CHILD
+                            : ChildAccountStatus.NOT_CHILD);
+        }
+    };
+
     public final SettingsActivityTestRule<AccountManagementFragment> mSettingsActivityTestRule =
             new SettingsActivityTestRule<>(AccountManagementFragment.class);
 
@@ -62,7 +80,8 @@ public class AccountManagementFragmentTest {
             RuleChain.outerRule(mActivityTestRule).around(mSettingsActivityTestRule);
 
     @Rule
-    public final AccountManagerTestRule mAccountManagerTestRule = new AccountManagerTestRule();
+    public final AccountManagerTestRule mAccountManagerTestRule =
+            new AccountManagerTestRule(mFakeFacade);
 
     @Rule
     public final ChromeRenderTestRule mRenderTestRule =
@@ -108,6 +127,36 @@ public class AccountManagementFragmentTest {
         View view = mSettingsActivityTestRule.getFragment().getView();
         onViewWaiting(allOf(is(view), isDisplayed()));
         mRenderTestRule.render(view, "account_management_fragment_signed_in_account_on_top");
+    }
+
+    @Test
+    @MediumTest
+    @Feature("RenderTest")
+    @Features.DisableFeatures(ChromeFeatureList.MOBILE_IDENTITY_CONSISTENCY)
+    public void testAccountManagementViewForChildAccountLegacy() throws Exception {
+        mAccountManagerTestRule.addAccountAndWaitForSeeding(CHILD_ACCOUNT_EMAIL);
+        final Profile profile = TestThreadUtils.runOnUiThreadBlockingNoException(
+                Profile::getLastUsedRegularProfile);
+        CriteriaHelper.pollUiThread(profile::isChild);
+        mSettingsActivityTestRule.startSettingsActivity();
+        View view = mSettingsActivityTestRule.getFragment().getView();
+        onViewWaiting(allOf(is(view), isDisplayed()));
+        mRenderTestRule.render(view, "account_management_fragment_for_child_account_legacy");
+    }
+
+    @Test
+    @MediumTest
+    @Feature("RenderTest")
+    @Features.EnableFeatures(ChromeFeatureList.MOBILE_IDENTITY_CONSISTENCY)
+    public void testAccountManagementViewForChildAccount() throws Exception {
+        mAccountManagerTestRule.addAccountAndWaitForSeeding(CHILD_ACCOUNT_EMAIL);
+        final Profile profile = TestThreadUtils.runOnUiThreadBlockingNoException(
+                Profile::getLastUsedRegularProfile);
+        CriteriaHelper.pollUiThread(profile::isChild);
+        mSettingsActivityTestRule.startSettingsActivity();
+        View view = mSettingsActivityTestRule.getFragment().getView();
+        onViewWaiting(allOf(is(view), isDisplayed()));
+        mRenderTestRule.render(view, "account_management_fragment_for_child_account");
     }
 
     @Test
