18659862a5e1cdebe3467d3b3edc8dd146f72954
Robbie Gibson <rkgibson@google.com>
2021-02-16 15:02:25 +0000

    [iOS][Thumb Strip] Close thumb strip when focusing omnibox
    This CL adds a new command, ThumbStripCommands, which is handled by the
    ThumbStripCoordinator. This allows arbitrary objects to close the thumb
    strip without needing a reference to the actual PanHandler.
    
    One small change is that the bug says the thumb strip should be closed
    when the omnibox is tapped, but I implemented this closing whenever the
    omnibox is focused for any reason.
    
    Fixed: 1171366
    Change-Id: Id77c26f7e10db78981dd4f96a2fb70926a26f474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2684576
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854263}

--- ios/chrome/browser/ui/omnibox/omnibox_coordinator.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_coordinator.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
 #import "ios/chrome/browser/ui/commands/load_query_commands.h"
 #import "ios/chrome/browser/ui/commands/omnibox_commands.h"
+#import "ios/chrome/browser/ui/commands/thumb_strip_commands.h"
 #import "ios/chrome/browser/ui/location_bar/location_bar_constants.h"
 #import "ios/chrome/browser/ui/omnibox/keyboard_assist/omnibox_assistive_keyboard_delegate.h"
 #import "ios/chrome/browser/ui/omnibox/keyboard_assist/omnibox_assistive_keyboard_views.h"
@@ -131,6 +132,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)focusOmnibox {
   if (![self.textField isFirstResponder]) {
     base::RecordAction(base::UserMetricsAction("MobileOmniboxFocused"));
+
+    // Thumb strip is not necessarily active, so only close it if it is active.
+    if ([self.browser->GetCommandDispatcher()
+            dispatchingForProtocol:@protocol(ThumbStripCommands)]) {
+      id<ThumbStripCommands> thumbStripHandler = HandlerForProtocol(
+          self.browser->GetCommandDispatcher(), ThumbStripCommands);
+      [thumbStripHandler closeThumbStrip];
+    }
 
     // In multiwindow context, -becomeFirstRepsonder is not enough to get the
     // keyboard input. The window will not automatically become key. Make it key
