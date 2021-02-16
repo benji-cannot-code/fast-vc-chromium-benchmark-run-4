6c107038ddf7d3f0967ba593f52961319efea732
Juanmi Huertas <juanmihd@chromium.org>
2021-02-16 23:15:29 +0000

    Fixing issue with same thread capturestream and offscreencanvas
    The compositor_sink will not respect the same frequency as when it is
    being called when in the same thread. This is particularly damaging when
    trying to capturestream a video, and Request a new frame for every RaF.
    
    This CL adds a shortcut when in the same thread and in capturestream
    mode, so it forces the Dispatcher to begin a new fream when it is being
    requested.
    
    Bug: 1144872
    Change-Id: I9516093e60efca73fff8ae768f43ceef7cd7f501
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2689553
    Auto-Submit: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854495}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.h
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.h
@@ -150,6 +150,8 @@ class CORE_EXPORT HTMLCanvasElement final
   // Used for canvas capture.
   void AddListener(CanvasDrawListener*);
   void RemoveListener(CanvasDrawListener*);
+  // Derived from OffscreenCanvasPlaceholder.
+  bool HasCanvasCapture() const final { return !listeners_.IsEmpty(); }
 
   // Used for rendering
   void DidDraw(const FloatRect&) override;
