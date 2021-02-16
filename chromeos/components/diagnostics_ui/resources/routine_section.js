576a8e20f18af09c30b24e81bd6720e23a162b5c
Joon Ahn <joonbug@chromium.org>
2021-02-16 22:33:40 +0000

    diagnostics: hide help center links pre-login
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: Idaaf2da3e7c7e2a5420edbabf42922c000402a3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2697747
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854475}

--- chromeos/components/diagnostics_ui/resources/routine_section.js
+++ chromeos/components/diagnostics_ui/resources/routine_section.js
@@ -116,6 +116,12 @@ Polymer({
       type: String,
       value: '',
     },
+
+    /** @private {boolean} */
+    isLoggedIn_: {
+      type: Boolean,
+      value: loadTimeData.getBoolean('isLoggedIn'),
+    },
   },
 
   observers: ['routineStatusChanged_(executionStatus_, currentTestName_)'],
@@ -217,7 +223,7 @@ Polymer({
 
   /** @protected */
   isLearnMoreHidden_() {
-    return this.isPowerRoutine === false ||
+    return this.isPowerRoutine === false || this.isLoggedIn_ === false ||
         this.executionStatus_ !== ExecutionProgress.kCompleted;
   },
 
