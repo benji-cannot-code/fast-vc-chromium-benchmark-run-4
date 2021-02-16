4531b519623129a5a96717c5b8a3040291c8a123
Richard Knoll <knollr@chromium.org>
2021-02-16 11:58:55 +0000

    Move remaining notification fake classes
    This moves the remaining fake classes into the reusable utils file so we
    can use them from the new notification service.
    
    Bug: 1146412
    Change-Id: Id23c3f33abaca748d9609604b976c7fed520e91a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2692921
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854228}

--- chrome/services/mac_notifications/public/cpp/notification_test_utils_mac.mm
+++ chrome/services/mac_notifications/public/cpp/notification_test_utils_mac.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/services/mac_notifications/public/cpp/notification_test_utils_mac.h"
 
+#include "base/mac/scoped_nsobject.h"
+
 @implementation FakeUNNotification
 @synthesize request = _request;
 - (void)dealloc {
@@ -21,4 +23,101 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [_actionIdentifier release];
   [super dealloc];
 }
+@end
+
+@implementation FakeUNNotificationSettings
+@synthesize alertStyle = _alertStyle;
+@synthesize authorizationStatus = _authorizationStatus;
+@end
+
+@implementation FakeUNUserNotificationCenter {
+  base::scoped_nsobject<FakeUNNotificationSettings> _settings;
+  base::scoped_nsobject<NSMutableDictionary> _notifications;
+  base::scoped_nsobject<NSSet<UNNotificationCategory*>> _categories;
+}
+
+- (instancetype)init {
+  if ((self = [super init])) {
+    _settings.reset([[FakeUNNotificationSettings alloc] init]);
+    _notifications.reset([[NSMutableDictionary alloc] init]);
+    _categories.reset([[NSSet alloc] init]);
+  }
+  return self;
+}
+
+- (void)setDelegate:(id<UNUserNotificationCenterDelegate>)delegate {
+}
+
+- (void)removeAllDeliveredNotifications {
+  [_notifications removeAllObjects];
+}
+
+- (void)setNotificationCategories:(NSSet<UNNotificationCategory*>*)categories {
+  _categories.reset([categories copy]);
+}
+
+- (void)replaceContentForRequestWithIdentifier:(NSString*)requestIdentifier
+                            replacementContent:
+                                (UNMutableNotificationContent*)content
+                             completionHandler:
+                                 (void (^)(NSError* _Nullable error))
+                                     notificationDelivered {
+  UNNotificationRequest* request =
+      [UNNotificationRequest requestWithIdentifier:requestIdentifier
+                                           content:content
+                                           trigger:nil];
+  base::scoped_nsobject<FakeUNNotification> notification(
+      [[FakeUNNotification alloc] init]);
+  [notification setRequest:request];
+  [_notifications setObject:notification forKey:[request identifier]];
+  notificationDelivered(/*error=*/nil);
+}
+
+- (void)addNotificationRequest:(UNNotificationRequest*)request
+         withCompletionHandler:(void (^)(NSError* error))completionHandler {
+  base::scoped_nsobject<FakeUNNotification> notification(
+      [[FakeUNNotification alloc] init]);
+  [notification setRequest:request];
+  [_notifications setObject:notification forKey:[request identifier]];
+  completionHandler(/*error=*/nil);
+}
+
+- (void)getDeliveredNotificationsWithCompletionHandler:
+    (void (^)(NSArray<UNNotification*>* notifications))completionHandler {
+  completionHandler([_notifications allValues]);
+}
+
+- (void)getNotificationCategoriesWithCompletionHandler:
+    (void (^)(NSSet<UNNotificationCategory*>* categories))completionHandler {
+  completionHandler([[_categories copy] autorelease]);
+}
+
+- (void)requestAuthorizationWithOptions:(UNAuthorizationOptions)options
+                      completionHandler:(void (^)(BOOL granted, NSError* error))
+                                            completionHandler {
+  completionHandler(/*granted=*/YES, /*error=*/nil);
+}
+
+- (void)removeDeliveredNotificationsWithIdentifiers:
+    (NSArray<NSString*>*)identifiers {
+  [_notifications removeObjectsForKeys:identifiers];
+}
+
+- (void)getNotificationSettingsWithCompletionHandler:
+    (void (^)(UNNotificationSettings* settings))completionHandler {
+  completionHandler(static_cast<UNNotificationSettings*>(_settings.get()));
+}
+
+- (FakeUNNotificationSettings*)settings {
+  return _settings.get();
+}
+
+- (NSArray<UNNotification*>* _Nonnull)notifications {
+  return [_notifications allValues];
+}
+
+- (NSSet<UNNotificationCategory*>* _Nonnull)categories {
+  return _categories.get();
+}
+
 @end
