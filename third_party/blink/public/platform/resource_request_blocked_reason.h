39d6d8a38448fca6675c1d18924b1abded8b97a7
Simon Zünd <szuend@chromium.org>
2021-02-16 08:32:12 +0000

    [devtools] Mark conversion registration requests as successful
    Conversion registration requests are redirects to a well-known
    conversion registration endpoint. The redirect request itself is
    always blocked should a conversion be registered.
    
    This CL adds a new blocked reason to mark such requests and
    reports them as successful to DevTools (Network.loadingFinished)
    instead of as a failure (Network.loadingFailed).
    
    R=sigurds@chromium.org
    
    Bug: chromium:1174422
    Change-Id: If8478fad898aa5c38ed92e66492e0ad1e09e4f2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676188
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854185}

--- third_party/blink/public/platform/resource_request_blocked_reason.h
+++ third_party/blink/public/platform/resource_request_blocked_reason.h
@@ -23,6 +23,7 @@ enum class ResourceRequestBlockedReason {
   kCorpNotSameOrigin,
   kCorpNotSameOriginAfterDefaultedToSameOriginByCoep,
   kCorpNotSameSite,
+  kConversionRequest,
 };
 }  // namespace blink
 
--- third_party/blink/renderer/platform/loader/fetch/resource_error.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_error.cc
@@ -302,6 +302,9 @@ String DescriptionForBlockedByClientOrResponse(int error, int extended_error) {
     case ResourceRequestBlockedReason::kCorpNotSameSite:
       detail = "NotSameSite";
       break;
+    case ResourceRequestBlockedReason::kConversionRequest:
+      detail = "ConversionRequest";
+      break;
   }
   return WebString::FromASCII(net::ErrorToString(error) + "." + detail);
 }
