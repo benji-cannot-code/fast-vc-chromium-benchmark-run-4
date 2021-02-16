24ba9398b5629620aacafec6b835354df7c5b6e0
Alexander Hendrich <hendrich@chromium.org>
2021-02-16 13:57:28 +0000

    Disable LocationIconViewTest.ShouldNotRecreateInkDropNeedlessly on Mac
    Bug: 1178786
    Change-Id: I31bb51bfd821de6f3613b8bef2413113bb1a4530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2696375
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Owners-Override: Alexander Hendrich <hendrich@chromium.org>
    Auto-Submit: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854249}

--- chrome/browser/ui/views/location_bar/location_icon_view_unittest.cc
+++ chrome/browser/ui/views/location_bar/location_icon_view_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/test/views/chrome_views_test_base.h"
 #include "components/omnibox/browser/location_bar_model.h"
 #include "components/omnibox/browser/test_location_bar_model.h"
@@ -152,7 +153,15 @@ TEST_F(LocationIconViewTest, ShouldNotAnimateWarningToDangerous) {
 // Whenever InkDropMode is set a new InkDrop is created, which will reset any
 // animations on the drop, so we should only set the InkDropMode when it has
 // actually changed.
-TEST_F(LocationIconViewTest, ShouldNotRecreateInkDropNeedlessly) {
+// TODO(https://crbug.com/1178786) flaky on mac
+#if defined(OS_MAC)
+#define MAYBE_ShouldNotRecreateInkDropNeedlessly \
+  DISABLED_ShouldNotRecreateInkDropNeedlessly
+#else
+#define MAYBE_ShouldNotRecreateInkDropNeedlessly \
+  ShouldNotRecreateInkDropNeedlessly
+#endif
+TEST_F(LocationIconViewTest, MAYBE_ShouldNotRecreateInkDropNeedlessly) {
   delegate()->set_is_editing_or_empty(false);
   view()->Update(false);
 
