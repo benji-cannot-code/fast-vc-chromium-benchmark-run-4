1153e68ac1477941254c3ab001080a881d4d9fc0
Christopher Cameron <ccameron@chromium.org>
2021-02-16 23:14:20 +0000

    DesktopCaptureMacV2: Avoid kFullDesktopScreenId
    DesktopCaptureDevice support a sentinel kFullDesktopScreenId, which
    stitches all displays together into a single screenshot. This feature
    has not been implemented by DesktopCaptureDeviceMac. Detect it and avoid
    it.
    
    Also, move the logic saying "modes X Y and Z aren't supported by
    DesktopCaptureDeviceMac into CreateDesktopCaptureDeviceMac, so it can
    indicate a lack of support by returning nullptr.
    
    Bug: 1178360
    Change-Id: Id4a20be88e35c57dbebc692025476a66a1d48c60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2694177
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854493}

--- content/browser/media/capture/desktop_capture_device_mac.cc
+++ content/browser/media/capture/desktop_capture_device_mac.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/threading/thread_checker.h"
 #include "media/capture/video/video_capture_device.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_capture_types.h"
 #include "ui/gfx/native_widget_types.h"
 
 namespace content {
@@ -183,7 +184,19 @@ class DesktopCaptureDeviceMac : public media::VideoCaptureDevice {
 
 std::unique_ptr<media::VideoCaptureDevice> CreateDesktopCaptureDeviceMac(
     const DesktopMediaID& source) {
-  CHECK_EQ(source.type, DesktopMediaID::TYPE_SCREEN);
+  // DesktopCaptureDeviceMac supports only TYPE_SCREEN.
+  // https://crbug.com/1176900
+  if (source.type != DesktopMediaID::TYPE_SCREEN)
+    return nullptr;
+
+  // DesktopCaptureDeviceMac only supports a single display at a time. It
+  // will not stitch desktops together.
+  // https://crbug.com/1178360
+  if (source.id == webrtc::kFullDesktopScreenId ||
+      source.id == webrtc::kInvalidScreenId) {
+    return nullptr;
+  }
+
   IncrementDesktopCaptureCounter(SCREEN_CAPTURER_CREATED);
   IncrementDesktopCaptureCounter(source.audio_share
                                      ? SCREEN_CAPTURER_CREATED_WITH_AUDIO
