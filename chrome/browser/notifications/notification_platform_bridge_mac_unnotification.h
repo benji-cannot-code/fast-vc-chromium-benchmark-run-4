2efe1cbb7a895fc624629c19c3de46d1fe1092fe
Richard Knoll <knollr@chromium.org>
2021-02-16 12:39:43 +0000

    Create NotificationCategoryManager for macOS notifications
    This manages the set of categories for the UNUserNotificationCenter on
    macOS. Categories contain all action buttons for a notification and are
    reused between notifications with the same buttons.
    
    Bug: 1170731
    Change-Id: I9c70bedcb72f9967d61cb77a1f67076e644c5b98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693004
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854238}

--- chrome/browser/notifications/notification_platform_bridge_mac_unnotification.h
+++ chrome/browser/notifications/notification_platform_bridge_mac_unnotification.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/notification_common.h"
 #include "chrome/browser/notifications/notification_image_retainer.h"
 #include "chrome/browser/notifications/notification_platform_bridge.h"
+#include "chrome/services/mac_notifications/public/cpp/notification_category_manager.h"
 
 @class UNNotificationBuilder;
 @class UNNotificationCenterDelegate;
@@ -60,6 +61,9 @@ class API_AVAILABLE(macosx(10.14)) NotificationPlatformBridgeMacUNNotification
   // Remove the closed alert notification.
   void DoCloseAlert(Profile* profile, const std::string& notification_id);
 
+  // Called when a notification has been closed from the notification center.
+  void OnNotificationClosed(std::string notification_id);
+
   // Process notification request that got delivered successfully.
   void DeliveredSuccessfully(
       const std::string& notification_id,
@@ -98,14 +102,6 @@ class API_AVAILABLE(macosx(10.14)) NotificationPlatformBridgeMacUNNotification
   // An object that keeps temp files alive long enough for macOS to pick up.
   NotificationImageRetainer image_retainer_;
 
-  // An object that carries the categories for the notifications.
-  base::scoped_nsobject<NSMutableSet> categories_;
-
-  // An object that maps a notification to the category it's carrying, this is
-  // used to update categories for notifications being redelivered to avoid
-  // multiple categories for the same notification.
-  base::scoped_nsobject<NSMutableDictionary> delivered_categories_;
-
   // An object that carries the delivered notifications to compare with the
   // actual delivered notifications in the notification center.
   base::scoped_nsobject<NSMutableDictionary> delivered_notifications_;
@@ -113,6 +109,9 @@ class API_AVAILABLE(macosx(10.14)) NotificationPlatformBridgeMacUNNotification
   // An object used to synchronize the notifications by polling over them every
   // |kSynchronizationInterval| minutes.
   base::RepeatingTimer synchronize_displayed_notifications_timer_;
+
+  // Category manager for action buttons.
+  NotificationCategoryManager category_manager_;
 
   base::WeakPtrFactory<NotificationPlatformBridgeMacUNNotification>
       weak_factory_{this};
