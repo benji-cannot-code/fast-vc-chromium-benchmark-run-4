8523d1ddd57772936a1ed2a34b70e868de7fb240
Daniel Cheng <dcheng@chromium.org>
2021-02-16 23:32:38 +0000

    [mojo] Add [Default] attribute for extensible enums.
    Extensible enums skip all validation. However, this means that the
    receiver can see values for an extensible enum that lie outside the
    valid range if the sender is using a newer version of the enumeration.
    
    To avoid that, define a `[Default]` attribute that may be specified on
    an enumerator value. When bindings receives an unknown value, instead,
    it will map it to the enumerator value specified as `[Default]`. This
    allows unknown values to be funnelled into one well-defined handling
    point and will eventually be required for all extensible enums.
    
    Bug: 731893
    Change-Id: Iee00aad9587ce142db418a797c2d830362ed7822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2528922
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854514}

--- mojo/public/cpp/bindings/lib/bindings_internal.h
+++ mojo/public/cpp/bindings/lib/bindings_internal.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <functional>
 #include <type_traits>
+#include <utility>
 
+#include "base/template_util.h"
 #include "mojo/public/cpp/bindings/enum_traits.h"
 #include "mojo/public/cpp/bindings/interface_id.h"
 #include "mojo/public/cpp/bindings/lib/template_util.h"
@@ -335,6 +337,23 @@ T ConvertEnumValue(MojomType input) {
   bool result = EnumTraits<MojomType, T>::FromMojom(input, &output);
   DCHECK(result);
   return output;
+}
+
+template <typename MojomType, typename SFINAE = void>
+struct EnumKnownValueTraits {
+  static MojomType ToKnownValue(MojomType in) { return in; }
+};
+
+template <typename MojomType>
+struct EnumKnownValueTraits<
+    MojomType,
+    base::void_t<decltype(ToKnownEnumValue(std::declval<MojomType>()))>> {
+  static MojomType ToKnownValue(MojomType in) { return ToKnownEnumValue(in); }
+};
+
+template <typename MojomType>
+MojomType ToKnownEnumValueHelper(MojomType in) {
+  return EnumKnownValueTraits<MojomType>::ToKnownValue(in);
 }
 
 }  // namespace internal
--- mojo/public/js/lib/codec.js
+++ mojo/public/js/lib/codec.js
@@ -876,7 +876,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   Enum.prototype.encodedSize = 4;
 
   Enum.prototype.decode = function(decoder) {
-    return decoder.readInt32();
+    let value = decoder.readInt32();
+    return cls.toKnownEnumValue(value);
   };
 
   Enum.prototype.encode = function(encoder, val) {
