ccecacc60a816f9af73c4f392f228755be54ec22
Benoit Lize <lizeb@chromium.org>
2021-02-16 18:41:13 +0000

    [mojo] Don't use aligned allocations for Channels.
    malloc() always provides the required alignment, don't use
    base::AlignedAlloc() needlessly, which depending on the memory
    allocator, may be less efficient, and at least has more overhead inside
    Chromium.
    
    Change-Id: Ic11bd5a271db1ec233831e4ffa9a7d0e8b862181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2695578
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854359}

--- mojo/core/channel.cc
+++ mojo/core/channel.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include <algorithm>
+#include <cstddef>
 #include <limits>
 #include <utility>
 
@@ -59,9 +60,11 @@ const size_t kMaxUnusedReadBufferCapacity = 4096;
 // Fuchsia: The zx_channel_write() API supports up to 64 handles.
 const size_t kMaxAttachedHandles = 64;
 
+static_assert(alignof(std::max_align_t) >= kChannelMessageAlignment, "");
 Channel::AlignedBuffer MakeAlignedBuffer(size_t size) {
-  return Channel::AlignedBuffer(
-      static_cast<char*>(base::AlignedAlloc(size, kChannelMessageAlignment)));
+  // No need to call base::AlignedAlloc() since malloc() already had the proper
+  // alignment.
+  return Channel::AlignedBuffer(static_cast<char*>(malloc(size)));
 }
 
 }  // namespace
--- mojo/core/channel_linux.cc
+++ mojo/core/channel_linux.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_pump_for_io.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/posix/eintr_wrapper.h"
+#include "base/process/process_metrics.h"
 #include "base/task_runner.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
