8f7c8f8d48ccf30b1f7402de2a0728f96b2b9b13
Peter Kasting <pkasting@chromium.org>
2021-02-16 18:42:18 +0000

    Get cloud_print_unittests to build with std::u16string.
    Bug: 911896
    Change-Id: Idef19d86e082d234cd98fee4823f27bd49dbbe38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693298
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854360}

--- cloud_print/virtual_driver/win/virtual_driver_helpers.cc
+++ cloud_print/virtual_driver/win/virtual_driver_helpers.cc
@@ -15,17 +15,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cloud_print {
 
-void DisplayWindowsMessage(HWND hwnd,
-                           HRESULT hr,
-                           const base::string16& caption) {
+void DisplayWindowsMessage(HWND hwnd, HRESULT hr, const std::wstring& caption) {
   ::MessageBox(hwnd, GetErrorMessage(hr).c_str(), caption.c_str(), MB_OK);
 }
 
-base::string16 GetPortMonitorDllName() {
+std::wstring GetPortMonitorDllName() {
   if (IsSystem64Bit()) {
-    return base::string16(L"gcp_portmon64.dll");
+    return std::wstring(L"gcp_portmon64.dll");
   } else {
-    return base::string16(L"gcp_portmon.dll");
+    return std::wstring(L"gcp_portmon.dll");
   }
 }
 
