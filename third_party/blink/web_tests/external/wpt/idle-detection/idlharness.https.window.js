075e6f982875b3205a1030c68f530f0733cfb924
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-02-16 00:05:11 +0000

    Import wpt@f2dd4d76f32ee16a61b5b914c9bff3f7f2114e66
    Using wpt-import in Chromium cdde2b915f6fc8ecf944075466b4c6711c5238bd.
    With Chromium commits locally applied on WPT:
    7735b6d589 "Simulate user inputs in wpt/touch-events/multi-touch-interfaces.html"
    d4ec46070d "Move COOP tests to new reporting endpoint"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    TBR=robertma@google.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: I249c4cd5cccd16cb6c73d093c8ffcfac6dc4f4b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693482
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#854095}

--- third_party/blink/web_tests/external/wpt/idle-detection/idlharness.https.window.js
+++ third_party/blink/web_tests/external/wpt/idle-detection/idlharness.https.window.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 idl_test(
-    ['idle-detection.tentative'],
+    ['idle-detection'],
     ['dom', 'html'],
     async (idl_array, t) => {
       await test_driver.set_permission({ name: 'idle-detection' }, 'granted', false);
