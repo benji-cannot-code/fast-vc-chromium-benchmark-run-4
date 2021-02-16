a28a28052a11f6560d4a9934631757b92c894a8a
Nohemi Fernandez <fernandex@chromium.org>
2021-02-16 10:36:59 +0000

    Reland "Verify the consent level when fetching access tokens."
    This reverts commit 0074f1a367f8d577de5bd14218fef8592795e364.
    
    Reason for revert: The original test failures in AccessTokenFetcherTest
    were due to difference between pre and post-submit configurations.
    
    Explicitly run death tests only when the flag #DCHECK_IS_ON is enabled.
    
    Change-Id: I4913f9418da2aaba32a59e77564610fff7b8a8bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2692540
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854211}

--- components/signin/public/identity_manager/access_token_fetcher.h
+++ components/signin/public/identity_manager/access_token_fetcher.h
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/scoped_observation.h"
+#include "components/signin/internal/identity_manager/primary_account_manager.h"
 #include "components/signin/internal/identity_manager/profile_oauth2_token_service.h"
 #include "components/signin/internal/identity_manager/profile_oauth2_token_service_observer.h"
+#include "components/signin/public/identity_manager/consent_level.h"
 #include "components/signin/public/identity_manager/scope_set.h"
 #include "google_apis/gaia/core_account_id.h"
 #include "google_apis/gaia/oauth2_access_token_manager.h"
@@ -166,6 +168,7 @@ class AccessTokenFetcher : public ProfileOAuth2TokenServiceObserver,
   AccessTokenFetcher(const CoreAccountId& account_id,
                      const std::string& oauth_consumer_name,
                      ProfileOAuth2TokenService* token_service,
+                     PrimaryAccountManager* primary_account_manager,
                      const ScopeSet& scopes,
                      TokenCallback callback,
                      Mode mode);
@@ -179,6 +182,7 @@ class AccessTokenFetcher : public ProfileOAuth2TokenServiceObserver,
       const CoreAccountId& account_id,
       const std::string& oauth_consumer_name,
       ProfileOAuth2TokenService* token_service,
+      PrimaryAccountManager* primary_account_manager,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
       const ScopeSet& scopes,
       TokenCallback callback,
@@ -192,6 +196,7 @@ class AccessTokenFetcher : public ProfileOAuth2TokenServiceObserver,
                      const std::string client_secret,
                      const std::string& oauth_consumer_name,
                      ProfileOAuth2TokenService* token_service,
+                     PrimaryAccountManager* primary_account_manager,
                      const ScopeSet& scopes,
                      TokenCallback callback,
                      Mode mode);
@@ -205,6 +210,7 @@ class AccessTokenFetcher : public ProfileOAuth2TokenServiceObserver,
       const std::string client_secret,
       const std::string& oauth_consumer_name,
       ProfileOAuth2TokenService* token_service,
+      PrimaryAccountManager* primary_account_manager,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
       const ScopeSet& scopes,
       TokenCallback callback,
@@ -213,6 +219,10 @@ class AccessTokenFetcher : public ProfileOAuth2TokenServiceObserver,
   // Returns true iff a refresh token is available for |account_id_|. Should
   // only be called in mode |kWaitUntilAvailable|.
   bool IsRefreshTokenAvailable() const;
+
+  // Verifies that the client has the appropriate level of user consent for all
+  // of the requested scopes.
+  void VerifyScopeAccess();
 
   void StartAccessTokenRequest();
 
@@ -237,6 +247,9 @@ class AccessTokenFetcher : public ProfileOAuth2TokenServiceObserver,
   const std::string client_id_;
   const std::string client_secret_;
   ProfileOAuth2TokenService* token_service_;
+  // Suppress unused typedef warnings in some compiler builds when DCHECK is
+  // disabled.
+  PrimaryAccountManager* primary_account_manager_ ALLOW_UNUSED_TYPE;
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
   const ScopeSet scopes_;
   const Mode mode_;
--- components/signin/public/identity_manager/identity_manager.cc
+++ components/signin/public/identity_manager/identity_manager.cc
@@ -134,9 +134,9 @@ IdentityManager::CreateAccessTokenFetcherForAccount(
     const ScopeSet& scopes,
     AccessTokenFetcher::TokenCallback callback,
     AccessTokenFetcher::Mode mode) {
-  return std::make_unique<AccessTokenFetcher>(account_id, oauth_consumer_name,
-                                              token_service_.get(), scopes,
-                                              std::move(callback), mode);
+  return std::make_unique<AccessTokenFetcher>(
+      account_id, oauth_consumer_name, token_service_.get(),
+      primary_account_manager_.get(), scopes, std::move(callback), mode);
 }
 
 std::unique_ptr<AccessTokenFetcher>
@@ -148,8 +148,9 @@ IdentityManager::CreateAccessTokenFetcherForAccount(
     AccessTokenFetcher::TokenCallback callback,
     AccessTokenFetcher::Mode mode) {
   return std::make_unique<AccessTokenFetcher>(
-      account_id, oauth_consumer_name, token_service_.get(), url_loader_factory,
-      scopes, std::move(callback), mode);
+      account_id, oauth_consumer_name, token_service_.get(),
+      primary_account_manager_.get(), url_loader_factory, scopes,
+      std::move(callback), mode);
 }
 
 std::unique_ptr<AccessTokenFetcher>
@@ -163,7 +164,8 @@ IdentityManager::CreateAccessTokenFetcherForClient(
     AccessTokenFetcher::Mode mode) {
   return std::make_unique<AccessTokenFetcher>(
       account_id, client_id, client_secret, oauth_consumer_name,
-      token_service_.get(), scopes, std::move(callback), mode);
+      token_service_.get(), primary_account_manager_.get(), scopes,
+      std::move(callback), mode);
 }
 
 void IdentityManager::RemoveAccessTokenFromCache(
