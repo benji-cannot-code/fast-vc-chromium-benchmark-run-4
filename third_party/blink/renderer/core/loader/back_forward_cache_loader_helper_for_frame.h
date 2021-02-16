817320a0f5dcc92c2c92db8b2ca7043b256bbcae
Omer Katz <omerkatz@chromium.org>
2021-02-16 11:22:59 +0000

    Loader: Fix more includes
    No-try: true
    Bug: 1056170
    Change-Id: I7314a32a5a08e517fe3324868fc06b96b812db94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2696575
    Auto-Submit: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854216}

--- third_party/blink/renderer/core/loader/back_forward_cache_loader_helper_for_frame.h
+++ third_party/blink/renderer/core/loader/back_forward_cache_loader_helper_for_frame.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/mojom/frame/back_forward_cache_controller.mojom-blink.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
-#include "third_party/blink/renderer/platform/heap/impl/member.h"
+#include "third_party/blink/renderer/platform/heap/member.h"
 #include "third_party/blink/renderer/platform/loader/fetch/back_forward_cache_loader_helper.h"
 
 namespace blink {
