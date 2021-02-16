2edd69c5de2b29109e605b4f3cd99f64d49cba51
Francois Beaufort <beaufort.francois@gmail.com>
2021-02-16 04:48:59 +0000

    [Android] Show adaptive icon in bottom-sheet UI for PWA installs
    This CL makes sure the maskable icon (adaptive) is padded/cropped
    properly when shown in the bottom-sheet UI for PWA installs.
    
    Screenshots:
    - Before: https://i.imgur.com/DNeAhy6.jpg
    - After: https://i.imgur.com/gvoY8LG.jpg
    
    Bug: 1178399
    Change-Id: Ie92ad623c90c3e81decbcffbf5f5888e65789c27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693243
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#854155}

--- chrome/browser/webapps/android/java/src/org/chromium/chrome/browser/webapps/PwaInstallBottomSheetView.java
+++ chrome/browser/webapps/android/java/src/org/chromium/chrome/browser/webapps/PwaInstallBottomSheetView.java
@@ -5,18 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webapps;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
-import android.graphics.drawable.Icon;
-import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.chromium.components.webapps.WebappsIconUtils;
 
 /**
  * The view portion of the PWA Install bottom sheet.
@@ -86,18 +85,12 @@ public class PwaInstallBottomSheetView {
 
     void setIcon(Bitmap icon, boolean isAdaptive) {
         ImageView imageView = mToolbarView.findViewById(R.id.app_icon);
-        if (isAdaptive && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            setAdaptiveIcon(imageView, icon);
+        if (isAdaptive && WebappsIconUtils.doesAndroidSupportMaskableIcons()) {
+            imageView.setImageBitmap(WebappsIconUtils.generateAdaptiveIconBitmap(icon));
         } else {
-            assert !isAdaptive : "Adaptive icons should not be provided pre-Android O.";
             imageView.setImageBitmap(icon);
         }
         imageView.setVisibility(View.VISIBLE);
-    }
-
-    @TargetApi(Build.VERSION_CODES.O)
-    private void setAdaptiveIcon(ImageView imageView, Bitmap icon) {
-        imageView.setImageIcon(Icon.createWithAdaptiveBitmap(icon));
     }
 
     void setCanSubmit(boolean canSubmit) {
