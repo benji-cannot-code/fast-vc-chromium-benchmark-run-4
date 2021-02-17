2153730590cf500fd8ea61dda0fe3d36d3cdf7c3
Charlie Hu <chenleihu@google.com>
2021-02-17 16:29:15 +0000

    [Permissions Policy] Add permissions policy to devtools protocol
    This CL adds a new command |getPermissionsPolicyState| on devtools
    protocol Page object.
    
    Design Doc (https://docs.google.com/document/d/1cNTksArJEDP_eV8aHTTJUDsHGIZwTpW5NQCP2QL-GCc/edit?usp=sharing&resourcekey=0-G5-mhHycLxpg-oad4fe76w)
    
    Bug: 1158827
    Change-Id: I78206d92d3d308d3146aee11489c7580b0a26430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2593889
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854801}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/permissions-policy.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startURL(
+      'https://example.test:8443/inspector-protocol/resources/permissions-policy-page.php',
+      'Verifies that we can successfully retrieve permissions policy state in frame tree');
+
+  await dp.Page.enable();
+  const frameId = (await dp.Page.getResourceTree()).result.frameTree.frame.id;
+  const result = (await dp.Page.getPermissionsPolicyState({frameId})).result;
+  const states = result.states
+    .sort((a, b) => a.feature.localeCompare(b.feature))
+    .filter(state => !state.allowed);
+  testRunner.log(states);
+  testRunner.log((await dp.Page.getPermissionsPolicyState({frameId: 'bad-id'})));
+  testRunner.completeTest();
+});
