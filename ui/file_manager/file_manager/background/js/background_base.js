7b9886225c7a682f49826351d869525ee4f0c4a6
Alex Danilo <adanilo@chromium.org>
2021-02-17 22:31:45 +0000

    Files SWA: Stop loading string data in background base
    class BackgroundBaseImpl loads string data from fileManagerPrivate which
    is not needed for the SWA case.
    
    Stop the load for the SWA File Manager.
    
    Bug: 1113981
    Change-Id: I652e38565d8cde95e238a3a8d2b774bff055f99c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2698913
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854983}

--- ui/file_manager/file_manager/background/js/background_base.js
+++ ui/file_manager/file_manager/background/js/background_base.js
@@ -25,16 +25,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     this.dialogs = {};
 
     // Initializes the strings. This needs for the volume manager.
-    this.initializationPromise_ = new Promise((fulfill, reject) => {
-      chrome.fileManagerPrivate.getStrings(stringData => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError.message);
-          return;
-        }
-        loadTimeData.data = assert(stringData);
-        fulfill(stringData);
+    if (!window.isSWA) {
+      this.initializationPromise_ = new Promise((fulfill, reject) => {
+        chrome.fileManagerPrivate.getStrings(stringData => {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+            return;
+          }
+          loadTimeData.data = assert(stringData);
+          fulfill(stringData);
+        });
       });
-    });
+    }
 
     /** @private {?LaunchHandler} */
     this.launchHandler_ = null;
