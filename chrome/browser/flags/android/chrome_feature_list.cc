b710ffcc84317c3df5f81b87dd9ec0abb347f7b9
Ella Ge <eirage@chromium.org>
2021-02-17 15:25:52 +0000

    [Launchpad] Add feature flag
    Adds a feature flag for launchpad.
    
    Bug: 1178919
    Change-Id: Ia5559275935daf7e336ccf6901b2dba5a208836c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2697430
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854777}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -134,6 +134,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kAssistantIntentExperimentId,
     &kAssistantIntentPageUrl,
     &kAssistantIntentTranslateInfo,
+    &kAppLaunchpad,
     &kBentoOffline,
     &kCastDeviceFilter,
     &kCloseTabSuggestions,
@@ -353,6 +354,9 @@ const base::Feature kAssistantIntentPageUrl{"AssistantIntentPageUrl",
 
 const base::Feature kAssistantIntentTranslateInfo{
     "AssistantIntentTranslateInfo", base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kAppLaunchpad{"AppLaunchpad",
+                                  base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kBackgroundTaskComponentUpdate{
     "BackgroundTaskComponentUpdate", base::FEATURE_DISABLED_BY_DEFAULT};
