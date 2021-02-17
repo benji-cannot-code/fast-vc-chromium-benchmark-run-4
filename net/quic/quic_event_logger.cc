5787a7aab7e5110c2719046b5738ad3699989db0
Renjie Tang <renjietang@chromium.org>
2021-02-17 01:40:01 +0000

    In QUIC netlog, print string literal for error code in STOP_SENDING.
    https://source.chromium.org/chromium/chromium/src/+/master:third_party/catapult/netlog_viewer/netlog_viewer/log_view_painter.js;l=411?q=quic_rst_stream_error&ss=chromium%2Fchromium%2Fsrc does the tostring part.
    
    Change-Id: I0d7dd8af1c0d13f655202f732fd512862550f0f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2699008
    Auto-Submit: Renjie Tang <renjietang@chromium.org>
    Commit-Queue: Renjie Tang <renjietang@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854573}

--- net/quic/quic_event_logger.cc
+++ net/quic/quic_event_logger.cc
@@ -378,7 +378,7 @@ base::Value NetLogQuicStopSendingFrameParams(
     const quic::QuicStopSendingFrame* frame) {
   base::Value dict(base::Value::Type::DICTIONARY);
   dict.SetIntKey("stream_id", frame->stream_id);
-  dict.SetIntKey("error_code", frame->error_code);
+  dict.SetIntKey("quic_rst_stream_error", frame->error_code);
   return dict;
 }
 
