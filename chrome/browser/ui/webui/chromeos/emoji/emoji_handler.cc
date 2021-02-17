1b15a0584bf0db54ac681007dc227b0839d304a9
John Palmer <jopalmer@chromium.org>
2021-02-17 00:10:23 +0000

    make emoji-picker window a singleton
    BUG=1177608
    
    Change-Id: I41a3382fe72d22b84d4f9f592b9aead4d8558158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690671
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854534}

--- chrome/browser/ui/webui/chromeos/emoji/emoji_handler.cc
+++ chrome/browser/ui/webui/chromeos/emoji/emoji_handler.cc
@@ -35,6 +35,7 @@ void EmojiHandler::HandleInsertEmoji(const base::ListValue* args) {
   // Hide emoji picker window to restore focus to original text field
   if (EmojiPickerDialog::window) {
     EmojiPickerDialog::window->Hide();
+    EmojiPickerDialog::window = nullptr;
   }
 
   ui::InputMethod* input_method =
