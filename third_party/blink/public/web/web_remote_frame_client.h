67d28e1ef12a7d9dd686a2637942a2c3441f12ad
Antonio Sartori <antoniosartori@chromium.org>
2021-02-17 10:23:10 +0000

    Send PolicyContainer keep alive for remote frame navigations
    This is a follow up to https://crrev.com/c/2648489 to send a
    PolicyContainerHost keep alive handle also for navigations of remote
    frames.
    
    It also fixes a null point dereference in Blink in the
    RemoteFrame::Navigate path, which I found out while writing a test for
    this patch.
    
    Bug: 1130587
    Change-Id: I6a0ecbbd35b804fa981a2ff500827487daa905b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690394
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854706}

--- third_party/blink/public/web/web_remote_frame_client.h
+++ third_party/blink/public/web/web_remote_frame_client.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 #include "third_party/blink/public/mojom/blob/blob_url_store.mojom-shared.h"
 #include "third_party/blink/public/mojom/frame/lifecycle.mojom-shared.h"
+#include "third_party/blink/public/mojom/frame/policy_container.mojom-forward.h"
 #include "third_party/blink/public/mojom/input/focus_type.mojom-shared.h"
 #include "third_party/blink/public/platform/cross_variant_mojo_util.h"
 #include "third_party/blink/public/platform/web_impression.h"
@@ -36,14 +37,17 @@ class WebRemoteFrameClient {
   // A remote frame was asked to start a navigation.
   virtual void Navigate(
       const WebURLRequest& request,
-      blink::WebLocalFrame* initiator_frame,
       bool should_replace_current_entry,
       bool is_opener_navigation,
       bool initiator_frame_has_download_sandbox_flag,
       bool blocking_downloads_in_sandbox_enabled,
       bool initiator_frame_is_ad,
       CrossVariantMojoRemote<mojom::BlobURLTokenInterfaceBase> blob_url_token,
-      const base::Optional<WebImpression>& impression) {}
+      const base::Optional<WebImpression>& impression,
+      const base::UnguessableToken* initiator_frame_token,
+      CrossVariantMojoRemote<
+          blink::mojom::PolicyContainerHostKeepAliveHandleInterfaceBase>
+          initiator_policy_container_keep_alive_handle) {}
 
   virtual void WillSynchronizeVisualProperties(
       bool capture_sequence_number_changed,
