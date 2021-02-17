6dd86d1b9284673b4beed57ef96db4ea239b1e87
Rainhard Findling <rainhard@chromium.org>
2021-02-17 10:09:58 +0000

    Privacy sandbox: update cookies page toast strings
    Update the strings of the privacy sandbox info toast shown on the
    cookies page.
    
    Bug: 1152336
    Change-Id: I5b660e1bcc877bb6be906cd4235c9c4a620f841a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693617
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854700}

--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -1446,6 +1446,10 @@ void AddPrivacySandboxStrings(content::WebUIDataSource* html_source,
        IDS_SETTINGS_PRIVACY_SANDBOX_TRIALS_ENABLED},
       {"privacySandboxTrialsDisabled",
        IDS_SETTINGS_PRIVACY_SANDBOX_TRIALS_DISABLED},
+      {"privacySandboxCookiesDialog",
+       IDS_SETTINGS_PRIVACY_SANDBOX_COOKIES_DIALOG},
+      {"privacySandboxCookiesDialogMore",
+       IDS_SETTINGS_PRIVACY_SANDBOX_COOKIES_DIALOG_MORE},
   };
   html_source->AddLocalizedStrings(kLocalizedStrings);
 
