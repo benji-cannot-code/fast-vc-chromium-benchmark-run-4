d3c77aa5e4d425b15ca881b72af5bfdae432e0be
Xiqi Ruan <xiqiruan@chromium.org>
2021-02-17 00:04:26 +0000

    metrics: Add parental control metrics for Family Link user
    Records the parental control metrics of time limit
    control for Chrome OS Family Link user. These metrics
    are reported at the beginning of the first active session
    on Chrome OS each day.
    
    Bug: 1152622, 1166241
    Change-Id: Idf52f283cf477d3d21bc6bae32d6db0ca3caca67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2527723
    Reviewed-by: Toby Huang <tobyhuang@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854529}

--- chrome/browser/chromeos/child_accounts/child_user_service.h
+++ chrome/browser/chromeos/child_accounts/child_user_service.h
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_CHILD_ACCOUNTS_CHILD_USER_SERVICE_H_
 
 #include <memory>
+#include <set>
 #include <string>
 
+#include "chrome/browser/chromeos/child_accounts/family_user_parental_control_metrics.h"
 #include "chrome/browser/chromeos/child_accounts/time_limits/app_activity_report_interface.h"
 #include "chrome/browser/chromeos/child_accounts/time_limits/app_time_limit_interface.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -94,6 +96,12 @@ class ChildUserService : public KeyedService,
   // Should only be called if |features::kPerAppTimeLimits| and
   // |features::kWebTimeLimits| features are enabled.
   base::TimeDelta GetWebTimeLimit() const;
+
+  // Checks whether app time limit and chrome app time limit are enabled and
+  // inserts the enabled types to `enabled_policies`.
+  void GetEnabledAppTimeLimitPolicies(
+      std::set<FamilyUserParentalControlMetrics::TimeLimitPolicyType>*
+          enabled_policies);
 
  private:
   // KeyedService:
--- chrome/browser/chromeos/child_accounts/family_user_metrics_service.h
+++ chrome/browser/chromeos/child_accounts/family_user_metrics_service.h
@@ -19,7 +19,7 @@ class PrefService;
 
 namespace base {
 class Time;
-}
+}  // namespace base
 
 namespace content {
 class BrowserContext;
