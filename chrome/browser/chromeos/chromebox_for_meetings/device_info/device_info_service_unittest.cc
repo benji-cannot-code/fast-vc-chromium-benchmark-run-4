f5455e87c0b81d3b79c75d30c1fe5f88c5288923
Kyle Williams <kdgwill@google.com>
2021-02-17 14:14:52 +0000

    chromebox_for_meetings: Update Mojoms
    Update mojoms to match changes in CrOS
    
    Bug: b:179382408
    Test:xvfb-run ./out/Default/unit_tests --single-process-tests \
    --gtest_filter=CfmDeviceInfoServiceTest.*
    
    Change-Id: Iaf3ee5ffaaf947867acf4b02f75867e2e7a630a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2692606
    Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    Auto-Submit: Kyle Williams <kdgwill@chromium.org>
    Reviewed-by: Paul Moy <pmoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854753}

--- chrome/browser/chromeos/chromebox_for_meetings/device_info/device_info_service_unittest.cc
+++ chrome/browser/chromeos/chromebox_for_meetings/device_info/device_info_service_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/system/sys_info.h"
 #include "base/test/bind.h"
 #include "base/test/mock_callback.h"
 #include "base/test/task_environment.h"
@@ -37,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace cfm {
 namespace {
+
+constexpr char kReleaseVersion[] = "13671.0.2020";
 
 class CfmDeviceInfoServiceTest : public ::testing::Test {
  public:
@@ -184,13 +187,19 @@ TEST_F(CfmDeviceInfoServiceTest, TestPolicyInfo) {
 
 TEST_F(CfmDeviceInfoServiceTest, TestSysInfo) {
   base::RunLoop run_loop;
+
+  base::SysInfo::SetChromeOSVersionInfoForTest(
+      base::StringPrintf("CHROMEOS_RELEASE_VERSION=%s\n", kReleaseVersion),
+      base::Time::Now());
+
   const auto& details_remote = GetDeviceInfoRemote();
   run_loop.RunUntilIdle();
 
   base::RunLoop mojo_loop;
   details_remote->GetSysInfo(
       base::BindLambdaForTesting([&](mojom::SysInfoPtr info_ptr) {
-        ASSERT_GE(info_ptr->num_proc, 1);
+        ASSERT_FALSE(info_ptr.is_null());
+        EXPECT_EQ(info_ptr->release_version, kReleaseVersion);
         mojo_loop.Quit();
       }));
   mojo_loop.Run();
