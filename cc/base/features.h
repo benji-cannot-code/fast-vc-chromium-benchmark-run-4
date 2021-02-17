b19e4bee56129a1eb7b8447d986c8165645d6619
Chris Blume <cblume@chromium.org>
2021-02-17 18:21:01 +0000

    Change animated image behavior
    The current animated image behavior is to catch up when it falls behind.
    This CL changes that behavior behind a Finch flag to resume where it
    left off.
    
    Bug: 1094716,1050197
    Change-Id: Ibe574185b84a1705bde5b5c077f772d4a1ea1afd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2410157
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854859}

--- cc/base/features.h
+++ cc/base/features.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 
+CC_BASE_EXPORT extern const base::Feature kAnimatedImageResume;
 CC_BASE_EXPORT extern const base::Feature kImpulseScrollAnimations;
 CC_BASE_EXPORT extern const base::Feature kSynchronizedScrolling;
 
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -75,6 +75,9 @@ extern const char kAllowInsecureLocalhostDescription[];
 extern const char kAllowSyncXHRInPageDismissalName[];
 extern const char kAllowSyncXHRInPageDismissalDescription[];
 
+extern const char kAnimatedImageResumeName[];
+extern const char kAnimatedImageResumeDescription[];
+
 extern const char kAriaElementReflectionName[];
 extern const char kAriaElementReflectionDescription[];
 
