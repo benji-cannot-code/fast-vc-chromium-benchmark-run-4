0856b724b8a45c4075e77286618a0e9b6c0d62c5
Xiaocheng Hu <xiaochengh@chromium.org>
2021-02-17 23:03:19 +0000

    [advance-override] Remove the stale advance-override implementation
    We've implemented two variants so far:
    1. advance-override: adds a fixed amount to the advance of each
       character
    2. advance-proportional-override: scales the advance of each character
       by a fixed factor
    
    Now that the CSSWG has standardized the second variant and named it
    'advance-override' [1], this patch removes the implementation of the
    first variant. A follow up patch will rename the second variant to
    'advance-override'.
    
    [1] https://github.com/w3c/csswg-drafts/pull/5991
    
    Bug: 1137633
    Change-Id: I65124a238614c3c95ab1b1f1aaf6b616c6070f39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702291
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854997}

--- third_party/blink/renderer/core/css/font_face.cc
+++ third_party/blink/renderer/core/css/font_face.cc
@@ -208,8 +208,6 @@ FontFace* FontFace::Create(Document* document,
                                       AtRuleDescriptorID::DescentOverride) &&
       font_face->SetPropertyFromStyle(properties,
                                       AtRuleDescriptorID::LineGapOverride) &&
-      font_face->SetPropertyFromStyle(properties,
-                                      AtRuleDescriptorID::AdvanceOverride) &&
       font_face->SetPropertyFromStyle(
           properties, AtRuleDescriptorID::AdvanceProportionalOverride) &&
       font_face->GetFontSelectionCapabilities().IsValid() &&
@@ -422,9 +420,6 @@ bool FontFace::SetPropertyValue(const CSSValue* value,
       break;
     case AtRuleDescriptorID::LineGapOverride:
       line_gap_override_ = ConvertFontMetricOverrideValue(value);
-      break;
-    case AtRuleDescriptorID::AdvanceOverride:
-      advance_override_ = value;
       break;
     case AtRuleDescriptorID::AdvanceProportionalOverride:
       advance_proportional_override_ = ConvertFontMetricOverrideValue(value);
@@ -871,7 +866,6 @@ void FontFace::Trace(Visitor* visitor) const {
   visitor->Trace(ascent_override_);
   visitor->Trace(descent_override_);
   visitor->Trace(line_gap_override_);
-  visitor->Trace(advance_override_);
   visitor->Trace(advance_proportional_override_);
   visitor->Trace(error_);
   visitor->Trace(loaded_property_);
@@ -913,10 +907,6 @@ FontMetricsOverride FontFace::GetFontMetricsOverride() const {
   if (line_gap_override_) {
     result.line_gap_override =
         To<CSSPrimitiveValue>(*line_gap_override_).GetFloatValue() / 100;
-  }
-  if (advance_override_) {
-    result.advance_override =
-        To<CSSPrimitiveValue>(*advance_override_).GetFloatValue();
   }
   if (advance_proportional_override_) {
     result.advance_proportional_override =
