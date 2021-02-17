7a7ff9cac5b769132229651071f5547abad62c9a
Peter Kasting <pkasting@chromium.org>
2021-02-17 23:21:17 +0000

    Revert "Add drawImage() and createImageBitmap() support for blink::VideoFrame."
    This reverts commit 27830868be3b06b19761c7ca4507a8e77ab1f535.
    
    Reason for revert: New tests SEGVing on several linux bots
    
    Original change's description:
    > Add drawImage() and createImageBitmap() support for blink::VideoFrame.
    >
    > This makes blink::VideoFrame a CanvasImageSource for drawImage() and
    > wires up the existing ImageBitmapSource implementation to
    > createImageBitmap() to deprecate VideoFrame.createImageBitmap().
    >
    > A deprecation notice is added for VideoFrame.createImageBitmap(); the
    > method will be removed prior to shipping WebCodecs.
    >
    > Bug: 1175907
    > Test: New (moved from internal) WPT tests.
    >
    > Change-Id: I549d996c9b16a8d0a1dd5293b5bee29f61cac8f3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2680955
    > Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Dan Sanders <sandersd@chromium.org>
    > Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#854938}
    
    Bug: 1175907
    Change-Id: I41b3cc7e6d76e4c80131d8cad2299774e4ebec95
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702757
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#855005}

--- third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_factories.cc
+++ third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_factories.cc
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h"
 #include "third_party/blink/renderer/core/svg/svg_image_element.h"
 #include "third_party/blink/renderer/core/workers/worker_global_scope.h"
-#include "third_party/blink/renderer/modules/webcodecs/video_frame.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/graphics/unaccelerated_static_bitmap_image.h"
 #include "third_party/blink/renderer/platform/instrumentation/histogram.h"
@@ -74,8 +73,7 @@ enum CreateImageBitmapSource {
   kCreateImageBitmapSourceHTMLVideoElement = 5,
   kCreateImageBitmapSourceOffscreenCanvas = 6,
   kCreateImageBitmapSourceSVGImageElement = 7,
-  kCreateImageBitmapSourceVideoFrame = 8,
-  kMaxValue = kCreateImageBitmapSourceVideoFrame,
+  kMaxValue = kCreateImageBitmapSourceSVGImageElement,
 };
 
 }  // namespace
@@ -123,11 +121,6 @@ static inline ImageBitmapSource* ToImageBitmapSourceInternal(
     UMA_HISTOGRAM_ENUMERATION("Blink.Canvas.CreateImageBitmapSource",
                               kCreateImageBitmapSourceOffscreenCanvas);
     return value.GetAsOffscreenCanvas();
-  }
-  if (value.IsVideoFrame()) {
-    UMA_HISTOGRAM_ENUMERATION("Blink.Canvas.CreateImageBitmapSource",
-                              kCreateImageBitmapSourceVideoFrame);
-    return value.GetAsVideoFrame();
   }
   NOTREACHED();
   return nullptr;
