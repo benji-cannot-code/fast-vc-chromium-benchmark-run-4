70ceda5e30ae829ac83f253f80de90fc7c3c4b14
Roman Sorokin <rsorokin@chromium.org>
2021-02-17 19:58:31 +0000

    [CrOS] OOBE: Add powerwash-confirmation step for the powerwash screen
    In debug overlay.
    
    Bug: 1110478
    Change-Id: Ic04afd03b1c48f620fa07d2cecf0d4a7dba4ac2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2700635
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/master@{#854908}

--- chrome/browser/resources/chromeos/login/debug/debug.js
+++ chrome/browser/resources/chromeos/login/debug/debug.js
@@ -485,6 +485,13 @@ cr.define('cr.ui.login.debug', function() {
             screen.setIsTpmFirmwareUpdateAvailable(true);
           },
         },
+        {
+          id: 'powerwash-confirmation',
+          trigger: (screen) => {
+            screen.reset();
+            screen.setShouldShowConfirmationDialog(true);
+          },
+        },
       ],
     },
     {
