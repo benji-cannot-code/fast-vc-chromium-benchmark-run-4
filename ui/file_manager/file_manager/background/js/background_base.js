4f549e274aebf072720f469b08a12cbafc09a0cf
Alex Danilo <adanilo@chromium.org>
2021-02-17 22:28:59 +0000

    Files SWA: Inhibit runtime listeners
    The SWA version of file manager doesn't need to listen for
    chrome.runtime launch and restart events.
    
    Remove those listener attachment calls for SWA File Manager.
    
    Bug: 1113981
    Change-Id: Ic766931bdff18b414210c9da66248dae60fe697a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2699171
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854982}

--- ui/file_manager/file_manager/background/js/background_base.js
+++ ui/file_manager/file_manager/background/js/background_base.js
@@ -40,8 +40,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     this.launchHandler_ = null;
 
     // Initialize handlers.
-    chrome.app.runtime.onLaunched.addListener(this.onLaunched_.bind(this));
-    chrome.app.runtime.onRestarted.addListener(this.onRestarted_.bind(this));
+    if (!window.isSWA) {
+      chrome.app.runtime.onLaunched.addListener(this.onLaunched_.bind(this));
+      chrome.app.runtime.onRestarted.addListener(this.onRestarted_.bind(this));
+    }
   }
 
   /**
