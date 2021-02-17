98100e2ad71b5cb425dde211bda718afcd59ad00
Michael Lippautz <mlippautz@chromium.org>
2021-02-17 16:57:21 +0000

    heap: Oilpan library: Avoid TLS in write barrier when possible
    Bug: 1056170
    Change-Id: I3056702841adbeeff2d654fe2b69e2c4a1a23bc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2698111
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854817}

--- third_party/blink/renderer/platform/heap/v8_wrapper/unified_heap_marking_visitor.h
+++ third_party/blink/renderer/platform/heap/v8_wrapper/unified_heap_marking_visitor.h
@@ -23,10 +23,10 @@ class PLATFORM_EXPORT UnifiedHeapMarkingVisitor final {
     v8::JSHeapConsistency::WriteBarrierParams params;
     if (v8::JSHeapConsistency::GetWriteBarrierType(
             ref, params, []() -> cppgc::HeapHandle& {
-              return ThreadState::Current()->cpp_heap().GetHeapHandle();
+              return ThreadState::Current()->heap_handle();
             }) == v8::JSHeapConsistency::WriteBarrierType::kMarking) {
       v8::JSHeapConsistency::DijkstraMarkingBarrier(
-          params, ThreadState::Current()->cpp_heap().GetHeapHandle(), ref);
+          params, ThreadState::Current()->heap_handle(), ref);
     }
   }
 
@@ -38,11 +38,10 @@ class PLATFORM_EXPORT UnifiedHeapMarkingVisitor final {
     if (v8::JSHeapConsistency::GetWriteBarrierType(
             wrapper, kV8DOMWrapperObjectIndex, wrappable, params,
             []() -> cppgc::HeapHandle& {
-              return ThreadState::Current()->cpp_heap().GetHeapHandle();
+              return ThreadState::Current()->heap_handle();
             }) == v8::JSHeapConsistency::WriteBarrierType::kMarking) {
       v8::JSHeapConsistency::DijkstraMarkingBarrier(
-          params, ThreadState::Current()->cpp_heap().GetHeapHandle(),
-          wrappable);
+          params, ThreadState::Current()->heap_handle(), wrappable);
     }
   }
 };
