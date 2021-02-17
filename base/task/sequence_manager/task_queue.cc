9e7f14ad90c7c528c41268f5e7346a7f46d68757
Jeremy Leconte <jleconte@google.com>
2021-02-17 15:54:26 +0000

    Add missing include perfetto/tracing/traced_value.h.
    Issue has been introduced with https://crrev.com/c/2679435.
    It is currently blocking Chromium Roll in WebRTC.
    
    TBR=altimin@chromium.org
    
    Bug: 1137154
    Change-Id: I0136c88fbeee5a9277666ffea81ccdf6abc27157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2696568
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854787}

--- base/task/sequence_manager/task_queue.cc
+++ base/task/sequence_manager/task_queue.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "base/threading/thread_checker_impl.h"
 #include "base/time/time.h"
+#include "third_party/perfetto/include/perfetto/tracing/traced_value.h"
 
 namespace base {
 namespace sequence_manager {
