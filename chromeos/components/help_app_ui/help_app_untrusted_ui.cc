5bc5eff8b585f95fe4220b89c2ae2c90d2118985
dpapad <dpapad@chromium.org>
2021-02-17 16:18:22 +0000

    Leverage WebUIDataSource::AddResourcePaths where possible.
    Previously this was not possible because AddResourcePaths resided as a
    webui::AddResourcePathsBulk utility method in c/b/ui/webui/webui_util.h
    which could not be a dependency of content/ or chromeos/components/.
    
    Bug: 1176299
    Change-Id: I710c1dc93e576df52cb27d3bd55e86bc67663c63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2691275
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854792}

--- chromeos/components/help_app_ui/help_app_untrusted_ui.cc
+++ chromeos/components/help_app_ui/help_app_untrusted_ui.cc
@@ -33,10 +33,8 @@ content::WebUIDataSource* CreateHelpAppUntrustedDataSource(
   source->DisableTrustedTypesCSP();
 
   // Add all resources from chromeos_media_app_bundle.pak.
-  for (size_t i = 0; i < kChromeosHelpAppBundleResourcesSize; i++) {
-    source->AddResourcePath(kChromeosHelpAppBundleResources[i].path,
-                            kChromeosHelpAppBundleResources[i].id);
-  }
+  source->AddResourcePaths(base::make_span(
+      kChromeosHelpAppBundleResources, kChromeosHelpAppBundleResourcesSize));
 
   // Add device and feature flags.
   delegate->PopulateLoadTimeData(source);
