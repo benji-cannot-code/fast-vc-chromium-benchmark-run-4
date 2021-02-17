b13862a1c771c6b8fbae1d4208615b70b1cb96df
Brandon Jones <bajones@chromium.org>
2021-02-17 21:30:50 +0000

    Implement GPUObjectBase
    Implements the GPUObjectBase mixin on every interface the spec says
    includes it. This change does not actually push any of the label changes
    to Dawn.
    
    Bug: 1177469
    Change-Id: I697bbe3dc491eccbe0575513b8b0a2b438885c02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2699072
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#854928}

--- third_party/blink/renderer/modules/webgpu/dawn_object.h
+++ third_party/blink/renderer/modules/webgpu/dawn_object.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/dawn_control_client_holder.h"
 #include "third_party/blink/renderer/platform/heap/visitor.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 #define DAWN_OBJECTS                          \
   X(BindGroup, bindGroup)                     \
@@ -83,8 +84,13 @@ class DawnObjectBase {
   // Flush commands up until now on this object's parent device immediately.
   void FlushNow();
 
+  // GPUObjectBase mixin implementation
+  const String& label() const { return label_; }
+  void setLabel(const String& value);
+
  private:
   scoped_refptr<DawnControlClientHolder> dawn_control_client_;
+  String label_;
 };
 
 class DawnObjectImpl : public ScriptWrappable, public DawnObjectBase {
--- third_party/blink/renderer/modules/webgpu/gpu_compute_pipeline.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_compute_pipeline.cc
@@ -50,9 +50,11 @@ GPUComputePipeline* GPUComputePipeline::Create(
   WGPUComputePipelineDescriptor dawn_desc =
       AsDawnType(webgpu_desc, &label, &computeStageDescriptor);
 
-  return MakeGarbageCollected<GPUComputePipeline>(
+  GPUComputePipeline* pipeline = MakeGarbageCollected<GPUComputePipeline>(
       device, device->GetProcs().deviceCreateComputePipeline(
                   device->GetHandle(), &dawn_desc));
+  pipeline->setLabel(webgpu_desc->label());
+  return pipeline;
 }
 
 GPUComputePipeline::GPUComputePipeline(GPUDevice* device,
