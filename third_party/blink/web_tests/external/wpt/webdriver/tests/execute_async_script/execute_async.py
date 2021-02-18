d170f1a5b9e7fba67242bc06a8fe30029bbad0c0
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-02-18 03:14:13 +0000

    Import wpt@806ae8875bea1b8380081b0da8fd416337ea447f
    Using wpt-import in Chromium 46e037f83169189cd14b025395e2cabf842957bd.
    With Chromium commits locally applied on WPT:
    b721d6830d "Allow fragmentainers with zero block-size."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    mcasas@chromium.org:
      external/wpt/mediacapture-record
    rego@igalia.com:
      external/wpt/css/selectors
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: I6a84d42887bb440cab8ae587b57244e27d965b18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702773
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855101}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/execute_async_script/execute_async.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/execute_async_script/execute_async.py
@@ -1,9 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import pytest
 
+from webdriver.error import NoSuchAlertException
 from webdriver.transport import Response
 
 from tests.support.asserts import assert_error, assert_success
+from tests.support.sync import Poll
 
 
 def execute_async_script(session, script, args=None):
@@ -51,9 +53,16 @@ def test_abort_by_user_prompt_twice(session, dialog_type):
 
     session.alert.accept()
 
-    # The first alert has been accepted by the user prompt handler, the second one remains.
-    # FIXME: this is how browsers currently work, but the spec should clarify if this is the
-    #        expected behavior, see https://github.com/w3c/webdriver/issues/1153.
-    assert session.alert.text == "Bye"
+    # The first alert has been accepted by the user prompt handler, the second
+    # alert will still be opened because the current step isn't aborted.
+    wait = Poll(
+        session,
+        timeout=5,
+        message="Second alert has not been opened",
+        ignored_exceptions=NoSuchAlertException
+    )
+    text = wait.until(lambda s: s.alert.text)
+
+    assert text == "Bye"
 
     session.alert.accept()
