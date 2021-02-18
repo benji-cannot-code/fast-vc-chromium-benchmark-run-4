14f3f263d3e35f832b9e7b628099eacf7d94a0c7
Fredrik Söderqvist <fs@opera.com>
2021-02-18 18:37:36 +0000

    Don't access initial style if no element in ParseColorOrCurrentColor
    When we don't have a HTMLCanvasElement we may be on another thread, so
    we shouldn't access ComputedStyle::InitialStyle(). Return a constant in
    that case instead.
    
    Drop the extra null check before the call to CurrentColor() because it
    already checks this (and produce the same value).
    
    Bug: 1179321, 1179322, 1179359
    Change-Id: I44a14c940f8da12921c82e7814ed83944b5e69a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704578
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855354}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_style.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_style.cc
@@ -74,7 +74,9 @@ static Color CurrentColor(HTMLCanvasElement* canvas) {
 }
 
 static mojom::blink::ColorScheme ColorScheme(HTMLCanvasElement* canvas) {
-  if (canvas && canvas->isConnected()) {
+  if (!canvas)
+    return mojom::blink::ColorScheme::kLight;
+  if (canvas->isConnected()) {
     if (auto* style = canvas->GetComputedStyle())
       return style->UsedColorScheme();
   }
@@ -92,7 +94,7 @@ bool ParseColorOrCurrentColor(Color& parsed_color,
     case kParsedSystemColor:
       return true;
     case kParsedCurrentColor:
-      parsed_color = canvas ? CurrentColor(canvas) : Color::kBlack;
+      parsed_color = CurrentColor(canvas);
       return true;
     case kParseFailed:
       return false;
