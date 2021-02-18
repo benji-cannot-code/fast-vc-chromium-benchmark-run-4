58adb7e1c02bfe06b6ac9752023ca8d7e831eb30
Bartek Nowierski <bartekn@chromium.org>
2021-02-18 11:27:00 +0000

    [PA][BRP] Add a guard page for direct map on 64-bit too
    This eliminates the risk that pointers immediately past a direct-map
    allocation (not using BackupRefCount) will fall into normal bucket pool
    (thus believing that BackupRefCount is used, and trying to manipulate
    the ref-count that isn't there... a guard page is there).
    
    As an added bonus, this makes code more consistent between
    architectures, at a cost of one reserved (but not committed) page.
    
    Bug: 1073933
    Change-Id: I88b8b8c0a3270034f0264ebb13277de3fe7ee1f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2699441
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855207}

--- base/allocator/partition_allocator/partition_alloc_unittest.cc
+++ base/allocator/partition_allocator/partition_alloc_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_freelist_entry.h"
 #include "base/allocator/partition_allocator/partition_page.h"
 #include "base/allocator/partition_allocator/partition_ref_count.h"
+#include "base/allocator/partition_allocator/partition_root.h"
 #include "base/bits.h"
 #include "base/logging.h"
 #include "base/partition_alloc_buildflags.h"
@@ -2835,10 +2836,8 @@ TEST_F(PartitionAllocTest, MAYBE_Bookkeeping) {
     size_t aligned_size = bits::AlignUp(huge_size, SystemPageSize());
     ptr = root.Alloc(huge_size - kExtraAllocSize, type_name);
     expected_committed_size += aligned_size;
-    size_t surrounding_pages_size = PartitionPageSize();
-#if !defined(PA_HAS_64_BITS_POINTERS)
-    surrounding_pages_size += SystemPageSize();
-#endif
+    size_t surrounding_pages_size =
+        PartitionRoot<ThreadSafe>::GetDirectMapMetadataAndGuardPagesSize();
     size_t alignment = PageAllocationGranularity();
 #if defined(PA_HAS_64_BITS_POINTERS)
     if (root.UsesGigaCage())
