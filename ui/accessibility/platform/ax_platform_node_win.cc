bfa6668c9bfb96cf23762e6c81dfecb3a1b9ed47
Daniel Libby <dlibby@microsoft.com>
2021-02-18 02:10:22 +0000

    Return stable runtime ids for UIA
    Certain sites like Word Online will move a contenteditable div around
    per-paragraph - this is where the caret/insertion point lives. When this
    element moves between subtrees, the corresponding operations in Blink
    create a new node to be serialized, along with a new id. When Narrator
    sees this, along with focus bouncing to the 'new' node, it thinks focus
    lands in a different editable node than the one that previously had
    focus and subsequently announces "Editing." instead of reading the
    contents of the next line/character/etc. (depending on how the
    navigation took place).
    
    To alleviate this, we should return a stable runtime id, which allows
    identity checks on Narrator's side to detect these scenarios, and
    continue announcing the right content. The DOM unique id is already
    serialized as part of AXNodeData, but this in not unique across
    different instances of Blink. However, the frame id does provide this
    disambiguation, so prepend this to the runtime id (luckily the return
    type of SAFEARRAY allows us this flexibility).
    
    R=kschmi@microsoft.com
    
    Bug: 1178053
    Change-Id: Ib7e647763aca6d9b3dcd7ccd2bebbbb6be4bd4ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2692356
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#855078}

--- ui/accessibility/platform/ax_platform_node_win.cc
+++ ui/accessibility/platform/ax_platform_node_win.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node_position.h"
 #include "ui/accessibility/ax_role_properties.h"
 #include "ui/accessibility/ax_tree_data.h"
+#include "ui/accessibility/ax_tree_id_registry.h"
 #include "ui/accessibility/platform/ax_fragment_root_win.h"
 #include "ui/accessibility/platform/ax_platform_node_delegate.h"
 #include "ui/accessibility/platform/ax_platform_node_delegate_utils_win.h"
@@ -3921,8 +3922,26 @@ IFACEMETHODIMP AXPlatformNodeWin::Navigate(
 
 void AXPlatformNodeWin::GetRuntimeIdArray(
     AXPlatformNodeWin::RuntimeIdArray& runtime_id) {
+  const AXNodeData& data = GetData();
+  int dom_id;
   runtime_id[0] = UiaAppendRuntimeId;
-  runtime_id[1] = GetUniqueId();
+
+  // The combination of tree/frame id and Blink (DOM) id is unique and gives
+  // nodes stable ids across layouts/tree movement. If there's a valid tree
+  // id, use that, otherwise fall back to the globally unique id.
+  AXTreeID tree_id = GetDelegate()->GetTreeData().tree_id;
+  if (data.GetIntAttribute(ax::mojom::IntAttribute::kDOMNodeId, &dom_id) &&
+      tree_id != AXTreeIDUnknown()) {
+    AXTreeIDRegistry::FrameID frame_id =
+        AXTreeIDRegistry::GetInstance()->GetFrameID(tree_id);
+    runtime_id[1] = frame_id.first;
+    runtime_id[2] = frame_id.second;
+    runtime_id[3] = dom_id;
+  } else {
+    runtime_id[1] = 0;
+    runtime_id[2] = 0;
+    runtime_id[3] = GetUniqueId();
+  }
 }
 
 IFACEMETHODIMP AXPlatformNodeWin::GetRuntimeId(SAFEARRAY** runtime_id) {
