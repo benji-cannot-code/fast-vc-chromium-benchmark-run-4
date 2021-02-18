3e4cc998cf84164902462ec7a501a1fb27f8f645
Aaron Colwell <acolwell@chromium.org>
2021-02-18 17:14:35 +0000

    Remove parameters from CreateTestRenderView().
    All of the callers are passing the exact same parameter values so they
    aren't actually needed. We can just hardcode the values inside the
    function itself.
    
    Bug: 1015882
    Change-Id: I6258e9dd23260ce0d4569a02326d6cfd1a75321f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702520
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Aaron Colwell <acolwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855316}

--- chrome/browser/ui/zoom/zoom_controller_unittest.cc
+++ chrome/browser/ui/zoom/zoom_controller_unittest.cc
@@ -31,8 +31,7 @@ class ZoomControllerTest : public ChromeRenderViewHostTestHarness {
 
     // This call is needed so that the RenderViewHost reports being alive. This
     // is only important for tests that call ZoomController::SetZoomLevel().
-    content::RenderViewHostTester::For(rvh())->CreateTestRenderView(
-        base::nullopt, MSG_ROUTING_NONE, false);
+    content::RenderViewHostTester::For(rvh())->CreateTestRenderView();
   }
 
   void TearDown() override {
