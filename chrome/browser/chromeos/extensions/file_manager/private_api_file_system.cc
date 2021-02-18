1dd16c0204ae4492360a5645a85e7811711ae2c1
Bo Majewski <majewski@chromium.org>
2021-02-18 00:29:19 +0000

    [Files SWA]: White space fix.
    To bring this file in line with mega-patch.
    
    Bug: 1113981
    Change-Id: I28b8ff295e769cbdf74a4f46fe0a4b20a586942d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703191
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855039}

--- chrome/browser/chromeos/extensions/file_manager/private_api_file_system.cc
+++ chrome/browser/chromeos/extensions/file_manager/private_api_file_system.cc
@@ -152,9 +152,8 @@ void NotifyCopyProgress(
   file_manager::EventRouter* event_router =
       GetEventRouterByProfileId(profile_id);
   if (event_router) {
-    event_router->OnCopyProgress(
-        operation_id, type,
-        source_url.ToGURL(), destination_url.ToGURL(), size);
+    event_router->OnCopyProgress(operation_id, type, source_url.ToGURL(),
+                                 destination_url.ToGURL(), size);
   }
 }
 
@@ -185,9 +184,8 @@ void NotifyCopyCompletion(
   file_manager::EventRouter* event_router =
       GetEventRouterByProfileId(profile_id);
   if (event_router)
-    event_router->OnCopyCompleted(
-        operation_id,
-        source_url.ToGURL(), destination_url.ToGURL(), error);
+    event_router->OnCopyCompleted(operation_id, source_url.ToGURL(),
+                                  destination_url.ToGURL(), error);
 }
 
 // Callback invoked upon completion of Copy() (regardless of succeeded or
@@ -376,8 +374,7 @@ FileManagerPrivateEnableExternalFileSchemeFunction::Run() {
 }
 
 FileManagerPrivateGrantAccessFunction::FileManagerPrivateGrantAccessFunction()
-    : chrome_details_(this) {
-}
+    : chrome_details_(this) {}
 
 ExtensionFunction::ResponseAction FileManagerPrivateGrantAccessFunction::Run() {
   using extensions::api::file_manager_private::GrantAccess::Params;
@@ -580,8 +577,8 @@ FileManagerPrivateGetSizeStatsFunction::Run() {
   const std::unique_ptr<Params> params(Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params);
 
-  using file_manager::VolumeManager;
   using file_manager::Volume;
+  using file_manager::VolumeManager;
   const ChromeExtensionFunctionDetails chrome_details(this);
   VolumeManager* const volume_manager =
       VolumeManager::Get(chrome_details.GetProfile());
@@ -688,8 +685,8 @@ FileManagerPrivateFormatVolumeFunction::Run() {
   const std::unique_ptr<Params> params(Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params);
 
-  using file_manager::VolumeManager;
   using file_manager::Volume;
+  using file_manager::VolumeManager;
   const ChromeExtensionFunctionDetails chrome_details(this);
   VolumeManager* const volume_manager =
       VolumeManager::Get(chrome_details.GetProfile());
@@ -751,8 +748,8 @@ FileManagerPrivateRenameVolumeFunction::Run() {
   const std::unique_ptr<Params> params(Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params);
 
-  using file_manager::VolumeManager;
   using file_manager::Volume;
+  using file_manager::VolumeManager;
   const ChromeExtensionFunctionDetails chrome_details(this);
   VolumeManager* const volume_manager =
       VolumeManager::Get(chrome_details.GetProfile());
@@ -1110,16 +1107,15 @@ void FileManagerPrivateInternalResolveIsolatedEntriesFunction::
 
 FileManagerPrivateInternalComputeChecksumFunction::
     FileManagerPrivateInternalComputeChecksumFunction()
-    : digester_(new drive::util::FileStreamMd5Digester()) {
-}
+    : digester_(new drive::util::FileStreamMd5Digester()) {}
 
 FileManagerPrivateInternalComputeChecksumFunction::
     ~FileManagerPrivateInternalComputeChecksumFunction() = default;
 
 ExtensionFunction::ResponseAction
 FileManagerPrivateInternalComputeChecksumFunction::Run() {
-  using extensions::api::file_manager_private_internal::ComputeChecksum::Params;
   using drive::util::FileStreamMd5Digester;
+  using extensions::api::file_manager_private_internal::ComputeChecksum::Params;
   const std::unique_ptr<Params> params(Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params);
 
