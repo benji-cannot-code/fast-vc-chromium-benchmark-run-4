f22c9ac789677b9b596277546df9bd5f4f45ca73
Keren Zhu <kerenzhu@chromium.org>
2021-02-18 20:31:05 +0000

    Add ClassProperty to metadata
    Add ADD_CLASS_PROPERTY_METADATA to declare an ui::ClassProperty on a
    View class. Declared class property can be inspect in UI Devtools.
    
    Bug: 1173664
    Change-Id: I2d506ded441cbe3ad76e3f9e3a810460f039c8f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703113
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855405}

--- ui/views/metadata/metadata_macros_internal.h
+++ ui/views/metadata/metadata_macros_internal.h
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
 #define METADATA_PROPERTY_TYPE_INTERNAL(property_type, property_name, ...) \
-  views::metadata::ClassPropertyMetaData<                                  \
+  views::metadata::ObjectPropertyMetaData<                                 \
       ViewClass, property_type, decltype(&ViewClass::Set##property_name),  \
       &ViewClass::Set##property_name,                                      \
       decltype(std::declval<ViewClass>().Get##property_name()),            \
@@ -61,10 +61,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define METADATA_READONLY_PROPERTY_TYPE_INTERNAL(property_type, property_name, \
                                                  ...)                          \
-  views::metadata::ClassPropertyReadOnlyMetaData<                              \
+  views::metadata::ObjectPropertyReadOnlyMetaData<                             \
       ViewClass, property_type,                                                \
       decltype(std::declval<ViewClass>().Get##property_name()),                \
       &ViewClass::Get##property_name, ##__VA_ARGS__>
+
+#define METADATA_CLASS_PROPERTY_TYPE_INTERNAL(property_type, property_key, \
+                                              ...)                         \
+  views::metadata::ClassPropertyMetaData<decltype(property_key),           \
+                                         property_type, ##__VA_ARGS__>
 
 #define BEGIN_METADATA_INTERNAL(qualified_class_name, metadata_class_name,   \
                                 parent_class_name)                           \
