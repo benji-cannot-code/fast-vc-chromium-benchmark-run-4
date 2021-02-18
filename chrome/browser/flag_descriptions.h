80c2849ca6c527aebeb94c93c48ceed61bedf6ce
Lijin Shen <lazzzis@google.com>
2021-02-18 01:59:06 +0000

    [Messages] Add a feature flag for popup blocked messages
    This CL adds a feature flag:
    - MessagesForAndroidPopupBlocked will control whether popup blocked
      uses Infobars or Messages UI.
    
    
    Bug: 1179462
    Change-Id: I0f821172df34211e63037f59d5147aff46b873a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702724
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/master@{#855073}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1729,6 +1729,9 @@ extern const char kMessagesForAndroidInfrastructureDescription[];
 extern const char kMessagesForAndroidPasswordsName[];
 extern const char kMessagesForAndroidPasswordsDescription[];
 
+extern const char kMessagesForAndroidPopupBlockedName[];
+extern const char kMessagesForAndroidPopupBlockedDescription[];
+
 extern const char kOfflineIndicatorAlwaysHttpProbeName[];
 extern const char kOfflineIndicatorAlwaysHttpProbeDescription[];
 
