318c46410b8bac17ebe868886217bcff0b66d2b1
Bailey Berro <baileyberro@google.com>
2021-02-18 18:20:36 +0000

    Diagnostics: Use wake lock to prevent sleep during Memory Routine
    This change modifies the Memory Test to acquire a wake lock. The lock is
    released when any of the following conditions are met:
    - Memory routine completes successfully
    - Memory routine is cancelled
    - Diagnostics app is closed during memory routine.
    
    Bug: 1128204
    Change-Id: I92224b1dbdd3ff60122753864c90674f07ab02d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2698054
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Auto-Submit: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855347}

--- chromeos/components/diagnostics_ui/backend/system_routine_controller.h
+++ chromeos/components/diagnostics_ui/backend/system_routine_controller.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/data_decoder/public/cpp/data_decoder.h"
+#include "services/device/public/mojom/wake_lock.mojom.h"
+#include "services/device/public/mojom/wake_lock_provider.mojom.h"
 
 namespace base {
 class OneShotTimer;
@@ -54,6 +56,11 @@ class SystemRoutineController : public mojom::SystemRoutineController {
 
   void BindInterface(
       mojo::PendingReceiver<mojom::SystemRoutineController> pending_receiver);
+
+  void SetWakeLockProviderForTesting(
+      mojo::Remote<device::mojom::WakeLockProvider> provider) {
+    wake_lock_provider_ = std::move(provider);
+  }
 
  private:
   void OnAvailableRoutinesFetched(
@@ -122,6 +129,10 @@ class SystemRoutineController : public mojom::SystemRoutineController {
 
   bool IsLoggingEnabled() const;
 
+  void AcquireWakeLock();
+
+  void ReleaseWakeLock();
+
   RoutineLog* routine_log_ptr_ = nullptr;  // Not Owned.
 
   // Keeps track of the id created by CrosHealthd for the currently running
@@ -143,6 +154,12 @@ class SystemRoutineController : public mojom::SystemRoutineController {
       diagnostics_service_;
 
   mojo::Receiver<mojom::SystemRoutineController> receiver_{this};
+
+  // `wake_lock_` is used to prevent the device from sleeping during the
+  // memory test.
+  mojo::Remote<device::mojom::WakeLock> wake_lock_;
+
+  mojo::Remote<device::mojom::WakeLockProvider> wake_lock_provider_;
 
   base::WeakPtrFactory<SystemRoutineController> weak_factory_{this};
 };
