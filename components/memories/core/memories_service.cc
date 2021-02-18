218945805a7fd6157a3d3431ce6a37a9f713f888
Tommy Li <tommycli@chromium.org>
2021-02-18 00:20:47 +0000

    [memories] Stash visit data to MemoriesService in-memory vector
    This CL hooks up the UKM data collection observer to stash visits into
    MemoryService as an in-memory vector, for now.
    
    We will use this vector of visits to generate memories.
    
    Bug: 1171352
    Change-Id: I9ac3def263b6d311f7cf7323f65e45ac61e25bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2697985
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855035}

--- components/memories/core/memories_service.cc
+++ components/memories/core/memories_service.cc
@@ -4,11 +4,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/memories/core/memories_service.h"
+#include "base/feature_list.h"
+#include "components/memories/core/memories_features.h"
 
 namespace memories {
+
+MemoriesService::MemoriesService() = default;
 
 MemoriesService::~MemoriesService() = default;
 
 void MemoriesService::Shutdown() {}
+
+void MemoriesService::AddVisit(const GURL& url,
+                               const base::Time& time,
+                               const VisitContextSignals& context_signals) {
+  if (!base::FeatureList::IsEnabled(memories::kChromeMemories))
+    return;
+
+  MemoriesVisit visit;
+  visit.url = url;
+  visit.visit_time = time;
+  visit.context_signals = context_signals;
+
+  // TODO(tommycli/manukh): It sure seems like we need to get the History
+  // visit_id. Probably we need to plumb the navigation ID from the caller of
+  // this function and ask HistoryService.
+  visits_.push_back(visit);
+
+  // TODO(tommycli/manukh): Persist to History, and take out of in-memory.
+}
 
 }  // namespace memories
--- components/memories/core/memories_service.h
+++ components/memories/core/memories_service.h
@@ -6,21 +6,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_MEMORIES_CORE_MEMORIES_SERVICE_H_
 #define COMPONENTS_MEMORIES_CORE_MEMORIES_SERVICE_H_
 
+#include <vector>
+
 #include "base/macros.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/memories/core/visit_data.h"
 
 namespace memories {
 
 // This Service is the API for UIs to fetch Chrome Memories.
 class MemoriesService : public KeyedService {
  public:
-  MemoriesService() = default;
+  MemoriesService();
   ~MemoriesService() override;
 
   // KeyedService:
   void Shutdown() override;
 
+  // Adds a visit to the memories service for processing into History and
+  // memory generation.
+  void AddVisit(const GURL& url,
+                const base::Time& time,
+                const VisitContextSignals& context_signals);
+
  private:
+  // If the Memories flag is enabled, this contains all the visits in-memory
+  // during the Profile lifetime.
+  // TODO(tommycli): Hide this better behind a new debug flag.
+  std::vector<MemoriesVisit> visits_;
+
   DISALLOW_COPY_AND_ASSIGN(MemoriesService);
 };
 
