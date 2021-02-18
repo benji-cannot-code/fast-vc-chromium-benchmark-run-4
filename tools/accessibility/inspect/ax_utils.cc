641f6874de2d044f1204cb7c732ba4581feb2e52
Alexander Surkov <asurkov@igalia.com>
2021-02-18 19:31:31 +0000

    ax_dump_tools: support Microsoft Edge in ax_dump_tree
    Bug: 1177438
    Change-Id: Id0c4887a1fd625d16242899992b6e49525945233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2689368
    Reviewed-by: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855383}

--- tools/accessibility/inspect/ax_utils.cc
+++ tools/accessibility/inspect/ax_utils.cc
@@ -11,6 +11,7 @@ char kActiveTabSwitch[] = "active-tab";
 char kChromeSwitch[] = "chrome";
 char kChromiumSwitch[] = "chromium";
 char kFirefoxSwitch[] = "firefox";
+char kEdgeSwitch[] = "edge";
 char kPatternSwitch[] = "pattern";
 char kSafariSwitch[] = "safari";
 
@@ -23,8 +24,13 @@ void PrintHelpForTreeSelectors() {
   printf("  pre-defined application selectors to dump accessible tree for:\n");
   printf("    --chrome\tChrome browser\n");
   printf("    --chromium\tChromium browser\n");
+#if defined(WINDOWS)
+  printf("    --edge\tEdge browser\n");
+#endif
   printf("    --firefox\tFirefox browser\n");
+#if defined(MACOS)
   printf("    --safari\tSafari browser\n");
+#endif
   printf("    --active-tab\tActive tab of a choosen browser\n");
 }
 
@@ -35,6 +41,8 @@ AXTreeSelector TreeSelectorFromCommandLine(
     selectors = AXTreeSelector::Chrome;
   } else if (command_line->HasSwitch(kChromiumSwitch)) {
     selectors = AXTreeSelector::Chromium;
+  } else if (command_line->HasSwitch(kEdgeSwitch)) {
+    selectors = AXTreeSelector::Edge;
   } else if (command_line->HasSwitch(kFirefoxSwitch)) {
     selectors = AXTreeSelector::Firefox;
   } else if (command_line->HasSwitch(kSafariSwitch)) {
