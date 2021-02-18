614564b61c199edda1814edddfe4d48d329976ab
Yeunjoo Choi <ychoi@igalia.com>
2021-02-18 09:38:14 +0000

    Move //chrome/browser/{chromeos => ash}/system_logs
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: I49c134ed7918b28187e86284458af85f44888572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703039
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#855178}

--- chrome/browser/feedback/system_logs/about_system_logs_fetcher.cc
+++ chrome/browser/feedback/system_logs/about_system_logs_fetcher.cc
@@ -14,14 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/feedback/system_logs/system_logs_fetcher.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/chromeos/system_logs/command_line_log_source.h"
-#include "chrome/browser/chromeos/system_logs/dbus_log_source.h"
-#include "chrome/browser/chromeos/system_logs/debug_daemon_log_source.h"
-#include "chrome/browser/chromeos/system_logs/device_event_log_source.h"
-#include "chrome/browser/chromeos/system_logs/network_health_source.h"
-#include "chrome/browser/chromeos/system_logs/shill_log_source.h"
-#include "chrome/browser/chromeos/system_logs/touch_log_source.h"
-#include "chrome/browser/chromeos/system_logs/ui_hierarchy_log_source.h"
+#include "chrome/browser/ash/system_logs/command_line_log_source.h"
+#include "chrome/browser/ash/system_logs/dbus_log_source.h"
+#include "chrome/browser/ash/system_logs/debug_daemon_log_source.h"
+#include "chrome/browser/ash/system_logs/device_event_log_source.h"
+#include "chrome/browser/ash/system_logs/network_health_source.h"
+#include "chrome/browser/ash/system_logs/shill_log_source.h"
+#include "chrome/browser/ash/system_logs/touch_log_source.h"
+#include "chrome/browser/ash/system_logs/ui_hierarchy_log_source.h"
 #endif
 
 namespace system_logs {
