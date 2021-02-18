05bfc6cb6d594fcc4cf5f7a9e65e297c6fc630f8
Michael Thiessen <mthiesse@chromium.org>
2021-02-18 14:09:19 +0000

    Set PendingIntent mutability for TracingNotificationServiceImpl
    We only look at the action for these PendingIntents, so they can be
    marked as Immutable.
    
    Bug: 1163529
    Change-Id: Ib586435b866e723ec5cc784f6066111f9988f7ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702394
    Auto-Submit: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855241}

--- chrome/android/java/src/org/chromium/chrome/browser/tracing/TracingNotificationServiceImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tracing/TracingNotificationServiceImpl.java
@@ -9,6 +9,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
+import org.chromium.base.IntentUtils;
 import org.chromium.base.task.PostTask;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
 
@@ -34,7 +35,9 @@ public class TracingNotificationServiceImpl extends TracingNotificationService.I
     public static PendingIntent getStopRecordingIntent(Context context) {
         Intent intent = new Intent(context, TracingNotificationService.class);
         intent.setAction(ACTION_STOP_RECORDING);
-        return PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return PendingIntent.getService(context, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+                        | IntentUtils.getPendingIntentMutabilityFlag(false));
     }
 
     /**
@@ -46,7 +49,9 @@ public class TracingNotificationServiceImpl extends TracingNotificationService.I
     public static PendingIntent getDiscardTraceIntent(Context context) {
         Intent intent = new Intent(context, TracingNotificationService.class);
         intent.setAction(ACTION_DISCARD_TRACE);
-        return PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return PendingIntent.getService(context, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+                        | IntentUtils.getPendingIntentMutabilityFlag(false));
     }
 
     /**
@@ -58,7 +63,9 @@ public class TracingNotificationServiceImpl extends TracingNotificationService.I
     public static PendingIntent getShareTraceIntent(Context context) {
         Intent intent = new Intent(context, TracingNotificationService.class);
         intent.setAction(ACTION_SHARE_TRACE);
-        return PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return PendingIntent.getService(context, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+                        | IntentUtils.getPendingIntentMutabilityFlag(false));
     }
 
     @Override
