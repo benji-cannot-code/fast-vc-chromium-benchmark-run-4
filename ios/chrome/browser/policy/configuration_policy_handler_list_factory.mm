22118765afe8d6fa688895e7e105e008ea3b2323
Vincent Boisselle <vincb@google.com>
2021-02-18 21:56:12 +0000

    [iOS Enterprise] Add the UrlKeyedAnonymizedDataCollectionEnabled policy
    Tested: see demo https://drive.google.com/file/d/1YASMHuJo-sWDYXUDFZpVazWO8DkR5g2M/view?usp=sharing&resourcekey=0-Y99KzvGBrgNB5VZ6-uYfhA
      * We can see the policy activated in chrome://policy
      * The corresponding setting is disabled and only shows the value along
        the learn more icon.
      * Also works when UKM is disabled through the policy (false).
      * Learn more redirects to chrome://management
      * The toggle is available again when removing the policy
    
    Bug: 1176939
    Change-Id: I82548242e21e282476d1dd1dbe7e138d77dc6be1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2687661
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855454}

--- ios/chrome/browser/policy/configuration_policy_handler_list_factory.mm
+++ ios/chrome/browser/policy/configuration_policy_handler_list_factory.mm
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/search_engines/default_search_policy_handler.h"
 #include "components/translate/core/browser/translate_pref_names.h"
+#include "components/unified_consent/pref_names.h"
 #include "components/variations/pref_names.h"
 #include "components/variations/service/variations_service.h"
 #include "ios/chrome/browser/policy/browser_signin_policy_handler.h"
@@ -95,6 +96,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { policy::key::kURLAllowlist,
     policy::policy_prefs::kUrlAllowlist,
     base::Value::Type::LIST},
+  { policy::key::kUrlKeyedAnonymizedDataCollectionEnabled,
+    unified_consent::prefs::kUrlKeyedAnonymizedDataCollectionEnabled,
+    base::Value::Type::BOOLEAN },
 };
 // clang-format on
 
