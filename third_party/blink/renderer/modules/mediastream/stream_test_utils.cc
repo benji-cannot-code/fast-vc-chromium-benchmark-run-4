cc2f1d5184cfce3efaa01f35f8e87dda3ac316c9
Omer Katz <omerkatz@chromium.org>
2021-02-18 10:59:26 +0000

    [BreakoutBox] Fix includes
    Should not include files under heap/impl/ and heap/v8_wrapper/ directly.
    Use their counterparts under heap/ instead.
    
    No-try: true
    Bug: 1056170
    Change-Id: I4469ab8e66edb68ae4cd1470c8f412eab9a36ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703516
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855201}

--- third_party/blink/renderer/modules/mediastream/stream_test_utils.cc
+++ third_party/blink/renderer/modules/mediastream/stream_test_utils.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/mediastream/media_stream_track.h"
 #include "third_party/blink/renderer/modules/mediastream/media_stream_video_track.h"
 #include "third_party/blink/renderer/modules/mediastream/mock_media_stream_video_source.h"
-#include "third_party/blink/renderer/platform/heap/impl/heap.h"
+#include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/mediastream/media_stream_source.h"
 
 namespace blink {
