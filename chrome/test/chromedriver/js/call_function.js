374c854d010899498c5ac96296d9fbfc67ebdcae
Shengfa Lin <shengfa@google.com>
2021-02-18 22:26:40 +0000

    Revert "[chromedriver] Distinguish between stale element and no element"
    This reverts commit f55edca9c6dd7a6fa9057c7cc734b8e2c6f48a2f.
    
    Reason for revert: Broke wpt tests (https://bugs.chromium.org/p/chromium/issues/detail?id=1168659#c5)
    
    Original change's description:
    > [chromedriver] Distinguish between stale element and no element
    >
    > Add a new NO_SUCH_ELEMENT code in call_function.js, and throw
    > the new error when the key is missing in the cache.
    > Modify test to reflect the change.
    >
    > Bug: chromedriver:3742
    > Change-Id: Ie09fd6cf5a3004b03db08dd6c81ae7f19adf2474
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2700793
    > Reviewed-by: John Chen <johnchen@chromium.org>
    > Commit-Queue: Shengfa Lin <shengfa@google.com>
    > Cr-Commit-Position: refs/heads/master@{#855037}
    
    Bug: chromedriver:3742, chromium:1168659
    Change-Id: I06fd39097717c475a23909f3e5573deb337df907
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704534
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Shengfa Lin <shengfa@google.com>
    Cr-Commit-Position: refs/heads/master@{#855474}

--- chrome/test/chromedriver/js/call_function.js
+++ chrome/test/chromedriver/js/call_function.js
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @enum {number}
  */
 var StatusCode = {
-  NO_SUCH_ELEMENT: 7,
   STALE_ELEMENT_REFERENCE: 10,
   JAVA_SCRIPT_ERROR: 17,
 };
@@ -112,15 +111,10 @@ CacheWithUUID.prototype = {
    */
   retrieveItem: function(id) {
     var item = this.cache_[id];
-    if (!item)
-       throw newError('id (' + id + ') not seen before',
-                     StatusCode.NO_SUCH_ELEMENT);
-
-    if (!isNodeReachable(item))
-      throw newError('element is not attached to the page document',
-                     StatusCode.STALE_ELEMENT_REFERENCE);
-
-    return item;
+    if (item && isNodeReachable(item))
+      return item;
+    throw newError('element is not attached to the page document',
+                   StatusCode.STALE_ELEMENT_REFERENCE);
   },
 
 };
@@ -163,15 +157,10 @@ Cache.prototype = {
    */
   retrieveItem: function(id) {
     var item = this.cache_[id];
-    if (!item)
-       throw newError('id (' + id + ') not seen before',
-                     StatusCode.NO_SUCH_ELEMENT);
-
-    if (!isNodeReachable(item))
-      throw newError('element is not attached to the page document',
-                     StatusCode.STALE_ELEMENT_REFERENCE);
-
-    return item;
+    if (item && isNodeReachable(item))
+      return item;
+    throw newError('element is not attached to the page document',
+                   StatusCode.STALE_ELEMENT_REFERENCE);
   },
 
 };
