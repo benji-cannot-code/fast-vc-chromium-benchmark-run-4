7d594ad1fc6ac2c6ce5f1b76dd3564d4d0e3f88c
Kyle Horimoto <khorimoto@chromium.org>
2021-02-18 04:37:17 +0000

    [CrOS Settings] Add Polymer3 test for Users page
    Was missed when originally migrating the People page.
    
    Bug: 1045266
    Change-Id: I6bbfd18bc0ce74e7bdb649028d407d0e32ca26c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702499
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855128}

--- chrome/test/data/webui/settings/chromeos/user_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/user_page_tests.js
@@ -4,6 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // clang-format off
+// #import 'chrome://os-settings/chromeos/os_settings.js';
+
+// #import {FakeUsersPrivate} from './fake_users_private.m.js';
+// #import {Router, routes} from 'chrome://os-settings/chromeos/os_settings.js';
 // #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 // #import {getDeepActiveElement} from 'chrome://resources/js/util.m.js';
 // #import {waitAfterNextRender} from 'chrome://test/test_util.m.js';
