7985a2ec4fecdf748b1ee6f63c8d40bbb67bb625
Andre Le <leandre@chromium.org>
2021-02-18 21:03:00 +0000

    Scalable Status Area: Don't show capslock notification as popup.
    Lower the priority of the capslock notification since we already show an
    icon in tray.
    
    BUG=1161557
    
    Change-Id: I156ccf29e46fcf8ae5bb4eaf867f0dd69a31c750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705338
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855426}

--- ash/system/caps_lock_notification_controller.cc
+++ ash/system/caps_lock_notification_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/caps_lock_notification_controller.h"
 
 #include "ash/accessibility/accessibility_controller_impl.h"
+#include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/notification_utils.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/session/session_controller_impl.h"
@@ -45,6 +46,13 @@ std::unique_ptr<Notification> CreateNotification() {
       kNotificationCapslockIcon,
       message_center::SystemNotificationWarningLevel::NORMAL);
   notification->set_pinned(true);
+
+  if (ash::features::IsScalableStatusAreaEnabled()) {
+    // Set the priority to low to prevent the notification showing as a popup
+    // because we already show an icon in tray for this in the feature.
+    notification->set_priority(message_center::LOW_PRIORITY);
+  }
+
   return notification;
 }
 
