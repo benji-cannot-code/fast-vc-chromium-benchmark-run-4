dacbe9fd2e1d07146d8b7f5037a34605f9df6d7c
Meilin Wang <meilinw@google.com>
2021-02-18 18:01:45 +0000

    [LibassistantV2] Update |AssistantManagerServiceImpl|.
    Minor refactoring on logic of |AssistantManagerServiceImpl| registering
    itself as an |AssistantActionObserver|, since |CrosActionModule| now
    stores multiple observers.
    
    This change is required for migrating |CrosActionModule| to Libassistant Mojom service on a per method basis.
    
    Bug: b/178397990
    Test: unittested, and manually tested
    Change-Id: I0ed62345b72a27ef6f117cdb5f9b86aca5ed2d42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693868
    Commit-Queue: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855338}

--- chromeos/services/assistant/assistant_manager_service_impl.h
+++ chromeos/services/assistant/assistant_manager_service_impl.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/thread.h"
+#include "chromeos/assistant/internal/action/assistant_action_observer.h"
 #include "chromeos/assistant/internal/action/cros_action_module.h"
 #include "chromeos/assistant/internal/internal_util.h"
 #include "chromeos/services/assistant/assistant_manager_service.h"
@@ -330,6 +331,9 @@ class COMPONENT_EXPORT(ASSISTANT_SERVICE) AssistantManagerServiceImpl
       scoped_app_list_event_subscriber_{this};
   base::ObserverList<CommunicationErrorObserver> error_observers_;
   base::ObserverList<AssistantManagerService::StateObserver> state_observers_;
+  base::ScopedObservation<action::CrosActionModule,
+                          action::AssistantActionObserver>
+      scoped_action_observer_{this};
 
   base::WeakPtrFactory<AssistantManagerServiceImpl> weak_factory_;
 
