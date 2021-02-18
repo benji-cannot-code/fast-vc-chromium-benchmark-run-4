f864ca90fd2a6f507156e91506e75db45a3a9bcc
Dan Harrington <harringtond@chromium.org>
2021-02-18 00:08:21 +0000

    web feeds: make ClearAllTask work for web feed stream
    Bug: 1152592
    Change-Id: I6a2ffd47ca4cf2320969fb5fd91f67dd42f3947a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2697976
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855031}

--- components/feed/core/v2/feed_stream.cc
+++ components/feed/core/v2/feed_stream.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
@@ -165,7 +166,7 @@ LocalActionId FeedStream::Metadata::GetNextActionId() {
   return LocalActionId(id);
 }
 
-FeedStream::Stream::Stream() : type(kInterestStream) {}
+FeedStream::Stream::Stream() = default;
 FeedStream::Stream::~Stream() = default;
 
 FeedStream::FeedStream(RefreshTaskScheduler* refresh_task_scheduler,
@@ -547,6 +548,12 @@ void FeedStream::ForceRefreshForDebuggingTask() {
   UnloadModel(kInterestStream);
   store_->ClearStreamData(kInterestStream, base::DoNothing());
   TriggerStreamLoad(kInterestStream);
+
+  if (base::FeatureList::IsEnabled(kWebFeed)) {
+    UnloadModel(kWebFeedStream);
+    store_->ClearStreamData(kWebFeedStream, base::DoNothing());
+    TriggerStreamLoad(kWebFeedStream);
+  }
 }
 
 std::string FeedStream::DumpStateForDebugging() {
@@ -585,12 +592,6 @@ base::Time FeedStream::GetLastFetchTime() {
   if (fetch_time > base::Time::Now())
     return base::Time();
   return fetch_time;
-}
-
-bool FeedStream::HasSurfaceAttached() const {
-  // TODO(crbug/1152592): Make ClearAll() work with multiple streams.
-  const Stream* stream = FindStream(kInterestStream);
-  return stream && stream->surface_updater->HasSurfaceAttached();
 }
 
 void FeedStream::LoadModelForTesting(const StreamType& stream_type,
@@ -824,6 +825,12 @@ void FeedStream::FinishClearAll() {
   prefs::SetExperiments({}, *profile_prefs_);
   metadata_.Populate(feedstore::Metadata());
   delegate_->ClearAll();
+
+  for (auto& item : streams_) {
+    if (item.second.surface_updater->HasSurfaceAttached()) {
+      TriggerStreamLoad(item.second.type);
+    }
+  }
 }
 
 ImageFetchId FeedStream::FetchImage(
@@ -890,6 +897,13 @@ void FeedStream::UnloadModel(const StreamType& stream_type) {
   stream->surface_updater->SetModel(nullptr);
   stream->model.reset();
 }
+
+void FeedStream::UnloadModels() {
+  for (auto& item : streams_) {
+    UnloadModel(item.second.type);
+  }
+}
+
 void FeedStream::ReportOpenAction(const StreamType& stream_type,
                                   const std::string& slice_id) {
   Stream& stream = GetStream(stream_type);
