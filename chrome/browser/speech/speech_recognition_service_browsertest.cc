eafe645001b39e2e3521299e25887300d6827340
evliu <evliu@google.com>
2021-02-18 01:04:39 +0000

    Update speech recognition unit and browser tests
    This CL updates the speech recognition service browser tests and SODA client unit tests.
    
    Bug: 1179383
    Change-Id: I4352b5a328cd0fb44eab598d7aed4f1fa3512623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2701692
    Reviewed-by: Rob Schonberger <robsc@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#855053}

--- chrome/browser/speech/speech_recognition_service_browsertest.cc
+++ chrome/browser/speech/speech_recognition_service_browsertest.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/speech/speech_recognition_service.h"
+#include "chrome/browser/speech/chrome_speech_recognition_service.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/services/speech/speech_recognition_recognizer_impl.h"
@@ -98,7 +98,7 @@ void SpeechRecognitionServiceTest::LaunchService() {
   // Launch the Speech Recognition service.
   auto* browser_context =
       static_cast<content::BrowserContext*>(browser()->profile());
-  auto* service = new SpeechRecognitionService(browser_context);
+  auto* service = new ChromeSpeechRecognitionService(browser_context);
 
   mojo::PendingReceiver<media::mojom::SpeechRecognitionContext>
       speech_recognition_context_receiver =
@@ -137,6 +137,9 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionServiceTest, RecognizePhrase) {
       prefs::kSodaEnUsConfigPath,
       test_data_dir_.Append(base::FilePath(kSodaResourcesDir))
           .Append(kSodaLanguagePackRelativePath));
+
+  PrefService* profile_prefs = browser()->profile()->GetPrefs();
+  profile_prefs->SetBoolean(prefs::kLiveCaptionEnabled, true);
   LaunchService();
 
   std::string buffer;
