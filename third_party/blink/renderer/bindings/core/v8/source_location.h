48889624410386500d2e65cb731bd04282ca97f4
Alexander Timin <altimin@chromium.org>
2021-02-18 04:06:11 +0000

    [tracing] Add DEVTOOLS_TIMELINE_TRACE_EVENT_INSTANT.
    In preparation of migrating inspector trace events en masse to use
    perfetto::TracedValue, add a helper macro
    DEVTOOLS_TIMELINE_TRACE_EVENT_INSTANT to hide the implementation details
    (creating a lambda and a bunch of boilerplate) and use it to convert a
    single event as a no-op.
    
    This patch does not change the format the resulting data, just makes the
    code more readable and writing process more efficient.
    
    R=caseq@chromium.org
    BUG=1137154
    
    Cq-Include-Trybots: luci.chromium.try:linux-perfetto-rel
    Change-Id: I8511c1397f7ec9582e4388d32d682b112410ec6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693609
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855115}

--- third_party/blink/renderer/bindings/core/v8/source_location.h
+++ third_party/blink/renderer/bindings/core/v8/source_location.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+#include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
 
 namespace blink {
 
@@ -59,10 +60,16 @@ class CORE_EXPORT SourceLocation {
     return std::move(stack_trace_);
   }
 
+  bool HasStackTrace() const { return !!stack_trace_; }
+
   // Safe to pass between threads, drops async chain in stack trace.
   std::unique_ptr<SourceLocation> Clone() const;
 
+  void WriteIntoTracedValue(perfetto::TracedValue context) const;
+
   // No-op when stack trace is unknown.
+  // TODO(altimin): Replace all usages of `ToTracedValue` with
+  // `WriteIntoTracedValue` and remove this method.
   void ToTracedValue(TracedValue*, const char* name) const;
 
   // Could be null string when stack trace is unknown.
