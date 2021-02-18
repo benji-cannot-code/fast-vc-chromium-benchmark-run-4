92a7d7c8de3d3200b68e3848a154cb00c82144fe
John Chen <johnchen@chromium.org>
2021-02-18 09:45:32 +0000

    [Benchmark] Schedule system_health.pcscan benchmark
    Bug: v8:11180
    Change-Id: Icb3d3f16d3fd661dab355cb6e039aa65b599013b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2701431
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: Tamer Tas <tmrts@chromium.org>
    Commit-Queue: Tamer Tas <tmrts@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855181}

--- tools/perf/benchmarks/system_health.py
+++ tools/perf/benchmarks/system_health.py
@@ -302,7 +302,7 @@ class PCScanSystemHealthBenchmark(perf_benchmark.PerfBenchmark):
 
   @classmethod
   def Name(cls):
-    return 'UNSCHEDULED_system_health.pcscan'
+    return 'system_health.pcscan'
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs(
