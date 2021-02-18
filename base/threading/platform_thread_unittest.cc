c71d8c356abab1adc1b3a44857ff39c553e8c058
Olga Sharonova <olka@chromium.org>
2021-02-18 17:17:38 +0000

    Reland "Enabling OptimizedRealtimeThreadingMac by default on Mac (% OS 10.14)"
    This reverts commit df4bdcd8e8eb1c830070e73c1e6c4f738d9d4d09.
    
    Reason for revert: Fixed the tests; go/rubber-stamper-user-guide.
    
    CQ_INCLUDE_TRYBOTS=luci.chromium.try:mac_chromium_10.14_rel_ng
    
    Original change's description:
    > Revert "Enabling OptimizedRealtimeThreadingMac by default on Mac (% OS 10.14)"
    >
    > This reverts commit ee40d10a5ea16add6eb4ab64a0022c3c2c154633.
    >
    > Reason for revert:
    > https://ci.chromium.org/p/chromium/builders/ci/Mac10.14%20Tests/13550
    >
    > deterministic failures [caused step to fail]:
    > RealtimePlatformThreadTest/RealtimePlatformThreadTest.RealtimeAudioConfigMac/32
    > RealtimePlatformThreadTest/RealtimePlatformThreadTest.RealtimeAudioConfigMac/33
    > RealtimePlatformThreadTest/RealtimePlatformThreadTest.RealtimeAudioConfigMac/38
    > RealtimePlatformThreadTest/RealtimePlatformThreadTest.RealtimeAudioConfigMac/39
    > RealtimePlatformThreadTest/RealtimePlatformThreadTest.RealtimeAudioConfigMac/44
    > RealtimePlatformThreadTest/RealtimePlatformThreadTest.RealtimeAudioConfigMac/45
    > RealtimePlatformThreadTest/RealtimePlatformThreadTest.RealtimeAudioConfigMac/50
    > RealtimePlatformThreadTest/RealtimePlatformThreadTest.RealtimeAudioConfigMac/51
    > RealtimePlatformThreadTest/RealtimePlatformThreadTest.RealtimeAudioConfigMac/56
    > RealtimePlatformThreadTest/RealtimePlatformThreadTest.RealtimeAudioConfigMac/57
    >
    >
    > Original change's description:
    > > Enabling OptimizedRealtimeThreadingMac by default on Mac (% OS 10.14)
    > >
    > > see go/mac-rt-thread-finch
    > >
    > > Keeping feature parameters in place, as there were some concerns
    > > regarding choppy audio reports which we'll continue to monitor.
    > >
    > > Bug: 1132225,1112952
    > > Change-Id: Iab3fea5db2867f0dae950f0e4a457fc480e46870
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2693582
    > > Commit-Queue: Olga Sharonova <olka@chromium.org>
    > > Reviewed-by: Markus Handell <handellm@google.com>
    > > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#854936}
    >
    > Bug: 1132225
    > Bug: 1112952
    > Change-Id: I543c686aa140698ba939a372e5aac07db969f7e3
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703552
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Johann Koenig <johannkoenig@google.com>
    > Cr-Commit-Position: refs/heads/master@{#855087}
    
    Bug: 1132225
    Bug: 1112952
    Change-Id: I384daed74f411912c58a5da6fb3c94bca88e6454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703963
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855317}

--- base/threading/platform_thread_unittest.cc
+++ base/threading/platform_thread_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <mach/mach.h>
 #include <mach/mach_time.h>
 #include <mach/thread_policy.h>
+#include "base/mac/mac_util.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/time/time.h"
 #endif
@@ -464,6 +465,9 @@ class RealtimeTestThread : public FunctionTestThread {
     mach_timebase_info(&tb_info);
 
     if (FeatureList::IsEnabled(kOptimizedRealtimeThreadingMac) &&
+#if defined(OS_MAC)
+        !mac::IsOS10_14() &&  // Should not be applied on 10.14.
+#endif
         !realtime_period_.is_zero()) {
       uint32_t abs_realtime_period = saturated_cast<uint32_t>(
           realtime_period_.InNanoseconds() *
