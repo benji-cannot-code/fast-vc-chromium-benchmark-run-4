9465759fb6b5e36fc0ef11bbc4148903eecae733
Frank Li <frankli@microsoft.com>
2021-02-18 20:25:15 +0000

    media: rename media/mojo/clients/windows as media/mojo/clients/win
    This CL is to rename windows folder as win folder under
    media/mojo/clients.
    
    Explainer: https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/master/Media/MFCdm/explainer.md
    
    Media Foundation (MF) based Renderer and Content Decryption Modules (CDMs):
    https://docs.google.com/document/d/1kXMdmfsSKU3tcwTxG-wFU-MaEA6sN3by45PCGBV8zJU
    
    Sandbox of Media Foundation (MF) based Content Decryption Modules:
    https://docs.google.com/document/d/19Y4Js5v3BlzA5uSuiVTvcvPNIOwmxcMSFJWtuc1A-w8/edit#heading=h.iqvhsrml3gl9
    
    Bug: 999747
    Change-Id: I40d151f5e1d240d2e130bcf724a62ab83ab7172d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2698575
    Commit-Queue: Frank Li <frankli@microsoft.com>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855400}

--- media/mojo/clients/win/media_foundation_renderer_client.cc
+++ media/mojo/clients/win/media_foundation_renderer_client.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "media/mojo/clients/windows/media_foundation_renderer_client.h"
+#include "media/mojo/clients/win/media_foundation_renderer_client.h"
 
 #include "base/callback_helpers.h"
 #include "base/metrics/histogram_functions.h"
--- media/mojo/clients/win/media_foundation_renderer_client.h
+++ media/mojo/clients/win/media_foundation_renderer_client.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_MOJO_CLIENTS_WINDOWS_MEDIA_FOUNDATION_RENDERER_CLIENT_H_
-#define MEDIA_MOJO_CLIENTS_WINDOWS_MEDIA_FOUNDATION_RENDERER_CLIENT_H_
+#ifndef MEDIA_MOJO_CLIENTS_WIN_MEDIA_FOUNDATION_RENDERER_CLIENT_H_
+#define MEDIA_MOJO_CLIENTS_WIN_MEDIA_FOUNDATION_RENDERER_CLIENT_H_
 
 #include "base/callback.h"
 #include "base/macros.h"
@@ -150,4 +150,4 @@ class MediaFoundationRendererClient
 
 }  // namespace media
 
-#endif  // MEDIA_MOJO_CLIENTS_WINDOWS_MEDIA_FOUNDATION_RENDERER_CLIENT_H_
+#endif  // MEDIA_MOJO_CLIENTS_WIN_MEDIA_FOUNDATION_RENDERER_CLIENT_H_
