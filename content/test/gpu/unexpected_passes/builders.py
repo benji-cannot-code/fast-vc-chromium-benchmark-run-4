a5ce48f5c96cc50a6ace376d747cdcec41dad8e1
Brian Sheedy <bsheedy@chromium.org>
2021-02-19 00:52:50 +0000

    Improve GPU unexpected pass finder performance
    Improves the GPU unexpected pass finder's performance when working with
    large amounts of data by properly parallelizing the querying and
    processing code instead of only using a thread pool (which isn't
    actually multithreaded). This results in a ~130% speedup when run on
    against the WebGL test suites with 100 samples.
    
    Bug: 1175852
    Change-Id: I80f6d42977f8a6cc777bb56559bbb8f6f45b3854
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704952
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855556}

--- content/test/gpu/unexpected_passes/builders.py
+++ content/test/gpu/unexpected_passes/builders.py
@@ -7,9 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import fnmatch
 import json
 import logging
-import multiprocessing
 import os
 import subprocess
+
+from unexpected_passes import multiprocessing_utils
 
 TESTING_BUILDBOT_DIR = os.path.realpath(
     os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'testing',
@@ -155,7 +156,7 @@ def GetTryBuilders(ci_builders):
   mirrored_builders = set()
   no_output_builders = set()
 
-  pool = _GetPool()
+  pool = multiprocessing_utils.GetProcessPool()
   results = pool.map(_GetMirroredBuildersForCiBuilder, ci_builders)
   for (builders, found_mirror) in results:
     if found_mirror:
@@ -215,12 +216,6 @@ def _GetMirroredBuildersForCiBuilder(ci_builder):
     logging.debug('Got mirrored builder for %s: %s', ci_builder, split[1])
     mirrored_builders.add(split[1])
   return mirrored_builders, True
-
-
-def _GetPool():
-  # Split out for testing - multiprocessing very much dislikes mocking/monkey
-  # patching functions that it's trying to use.
-  return multiprocessing.Pool()
 
 
 def _GetBuildbucketOutputForCiBuilder(ci_builder):
---
+++ content/test/gpu/unexpected_passes/multiprocessing_utils.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from pathos import pools
+
+
+def GetProcessPool():
+  """Returns a pathos.pools.ProcessPool instance.
+
+  Split out for ease of unittesting since pathos can still run into pickling
+  issues with MagicMocks used in tests.
+
+  Returns:
+    A pathos.pools.ProcessPool instance.
+  """
+  return pools.ProcessPool()
