c79105eff5592493e51cc52fe5d04e6de52529e8
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-02-19 15:30:52 +0000

    Revert "viz: Add a path for the SurfaceAnimationManager to ref/unref resources."
    This reverts commit 3d716fad77576d2f750e45a2831b0da59efcff5a.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 855697 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzNkNzE2ZmFkNzc1NzZkMmY3NTBlNDVhMjgzMWIwZGE1OWVmY2ZmNWEM
    
    Sample Failed Build: https://ci.chromium.org/b/8854856387897522016
    
    Sample Failed Step: compile
    
    Original change's description:
    > viz: Add a path for the SurfaceAnimationManager to ref/unref resources.
    > 
    > This patch ensures that we only handle non-restricted range resource
    > ids in the surface resource holder.
    > 
    > It also adds a part which passes the resources to surface animation
    > manager, and manages ref/unreffing resources in the restricted range.
    > 
    > R=kylechar@chromium.org, vasilyt@chromium.org
    > 
    > Bug: 1174110
    > Change-Id: Iaaef2997f6a5e7483604d4201558a77d0a6fab4d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702392
    > Commit-Queue: vmpstr <vmpstr@chromium.org>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: kylechar <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#855697}
    
    
    Change-Id: If4cbe1451b395248108380afac83f98e6b3d6fc3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1174110
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704985
    Cr-Commit-Position: refs/heads/master@{#855698}

--- components/viz/service/transitions/surface_animation_manager.cc
+++ components/viz/service/transitions/surface_animation_manager.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/time/time.h"
-#include "components/viz/common/resources/returned_resource.h"
-#include "components/viz/common/resources/transferable_resource.h"
 #include "components/viz/service/surfaces/surface_saved_frame_storage.h"
 
 namespace viz {
@@ -136,26 +134,6 @@ double SurfaceAnimationManager::CalculateAnimationProgress() const {
 
 void SurfaceAnimationManager::InterpolateFrame(Surface* surface) {
   // TODO(vmpstr): Do the interpolation and saving things back to surface.
-}
-
-void SurfaceAnimationManager::RefResources(
-    const std::vector<TransferableResource>& resources) {
-  if (transferable_resource_tracker_.is_empty())
-    return;
-  for (const auto& resource : resources) {
-    if (resource.id >= kVizReservedRangeStartId)
-      transferable_resource_tracker_.RefResource(resource.id);
-  }
-}
-
-void SurfaceAnimationManager::UnrefResources(
-    const std::vector<ReturnedResource>& resources) {
-  if (transferable_resource_tracker_.is_empty())
-    return;
-  for (const auto& resource : resources) {
-    if (resource.id >= kVizReservedRangeStartId)
-      transferable_resource_tracker_.UnrefResource(resource.id);
-  }
 }
 
 }  // namespace viz
