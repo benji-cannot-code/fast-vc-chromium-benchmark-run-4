56044efd1633bcfc39ee7c1f5fe46a89e74dc2bd
Clemens Arbesser <arbesser@google.com>
2021-02-19 17:15:04 +0000

    [Autofill Assistant] Split onboarding factory methods.
    It makes the code and self-documenting and easier to read.
    
    Bug: b/179648654
    Change-Id: Id473fe2d2b0e7822d0ac7daf9131570b42b5be7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2684416
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855718}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantActionHandlerImpl.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantActionHandlerImpl.java
@@ -88,9 +88,9 @@ class AutofillAssistantActionHandlerImpl implements AutofillAssistantActionHandl
             String experimentIds, Bundle arguments, Callback<Boolean> callback) {
         Map<String, String> parameters = toArgumentMap(arguments);
         BaseOnboardingCoordinator coordinator =
-                OnboardingCoordinatorFactory.createOnboardingCoordinator(
-                        /* isDialogOnboardingEnabled = */ false, experimentIds, parameters,
-                        mContext, mBottomSheetController, mBrowserControls, mCompositorViewHolder);
+                OnboardingCoordinatorFactory.createBottomSheetOnboardingCoordinator(experimentIds,
+                        parameters, mContext, mBottomSheetController, mBrowserControls,
+                        mCompositorViewHolder);
         coordinator.show(result -> {
             coordinator.hide();
             callback.onResult(result == AssistantOnboardingResult.ACCEPTED);
@@ -114,10 +114,9 @@ class AutofillAssistantActionHandlerImpl implements AutofillAssistantActionHandl
 
         if (!AutofillAssistantPreferencesUtil.isAutofillOnboardingAccepted()) {
             BaseOnboardingCoordinator coordinator =
-                    OnboardingCoordinatorFactory.createOnboardingCoordinator(
-                            /* isDialogOnboardingEnabled = */ false, experimentIds, argumentMap,
-                            mContext, mBottomSheetController, mBrowserControls,
-                            mCompositorViewHolder);
+                    OnboardingCoordinatorFactory.createBottomSheetOnboardingCoordinator(
+                            experimentIds, argumentMap, mContext, mBottomSheetController,
+                            mBrowserControls, mCompositorViewHolder);
             coordinator.show(result -> {
                 if (result != AssistantOnboardingResult.ACCEPTED) {
                     coordinator.hide();
--- chrome/browser/android/autofill_assistant/onboarding_coordinator_bridge.cc
+++ chrome/browser/android/autofill_assistant/onboarding_coordinator_bridge.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/android/autofill_assistant/onboarding_coordinator_bridge.h"
-
+#include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
+#include "base/android/scoped_java_ref.h"
 #include "base/containers/flat_map.h"
 #include "chrome/android/features/autofill_assistant/jni_headers/BaseOnboardingCoordinator_jni.h"
 #include "chrome/browser/android/autofill_assistant/onboarding_fetcher_factory.h"
