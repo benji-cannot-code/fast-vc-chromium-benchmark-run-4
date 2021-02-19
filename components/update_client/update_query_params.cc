c15c8e6a24df88b3985203c02e054df5ab7e48cf
Mugdha Lakhani <nator@chromium.org>
2021-02-19 23:46:52 +0000

    [AW] Add WEBVIEW ProdID to component updater.
    Bug: 1176894
    Test: android_webview_unittests
    Change-Id: I56fbf9070c434cc76f95bad0a3b07604f1793229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704671
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Auto-Submit: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855992}

--- components/update_client/update_query_params.cc
+++ components/update_client/update_query_params.cc
@@ -67,9 +67,11 @@ const char kArch[] =
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 const char kChrome[] = "chrome";
 const char kCrx[] = "chromecrx";
+const char kWebView[] = "googleandroidwebview";
 #else
 const char kChrome[] = "chromium";
 const char kCrx[] = "chromiumcrx";
+const char kWebView[] = "androidwebview";
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
 UpdateQueryParamsDelegate* g_delegate = nullptr;
@@ -92,6 +94,8 @@ const char* UpdateQueryParams::GetProdIdString(UpdateQueryParams::ProdId prod) {
       return kChrome;
     case UpdateQueryParams::CRX:
       return kCrx;
+    case UpdateQueryParams::WEBVIEW:
+      return kWebView;
   }
   return kUnknown;
 }
