ad14013f515eba607162a1f8fb2c43b516a2ae1b
Ryan Hansberry <hansberry@chromium.org>
2021-02-19 00:05:33 +0000

    [CrOS PhoneHub] Ensure endpoint_id is valid UTF8.
    PhoneHub's strategy, prior to this CL, of generating its own injected
    endpoint ID was problematic because it interpreted random bytes as a
    string. This usually lead to printing endpoint IDs into logs which are
    non-UTF8, and that leads to crbug.com/1174859.
    
    This CL aligns PhoneHub's endpoint ID generation logic to be more in
    line with Nearby Connections' logic [1]: take the first N chars of a
    Base64-encoded random byte array.
    
    1) https://source.corp.google.com/piper///depot/google3/location/nearby/connections/core/internal/client_proxy.cc;l=37
    
    Bug: 1174859
    Change-Id: I974ddafc469d1134847da702a40c4e5ec4ba558d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705981
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855523}

--- chrome/browser/chromeos/secure_channel/nearby_endpoint_finder_impl.cc
+++ chrome/browser/chromeos/secure_channel/nearby_endpoint_finder_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/secure_channel/nearby_endpoint_finder_impl.h"
 
+#include "base/base64.h"
 #include "base/memory/ptr_util.h"
 #include "base/rand_util.h"
 #include "chrome/browser/chromeos/secure_channel/util/histogram_util.h"
@@ -33,9 +34,24 @@ void OnStopDiscoveryDestructorResult(Status status) {
     PA_LOG(WARNING) << "Failed to stop discovery as part of destructor";
 }
 
-std::vector<uint8_t> GenerateEndpointInfo() {
-  std::string endpoint_info = base::RandBytesAsString(kEndpointInfoLength);
+std::vector<uint8_t> GenerateRandomByteArray(size_t length) {
+  std::string endpoint_info = base::RandBytesAsString(length);
   return std::vector<uint8_t>(endpoint_info.begin(), endpoint_info.end());
+}
+
+std::string GenerateEndpointId() {
+  // Generate a random array of bytes; as long as it is of size of at least
+  // 3/4 kEndpointInfoLength, the final substring of the Base64-encoded array
+  // will be of size kEndpointInfoLength.
+  std::vector<uint8_t> raw_endpoint_info =
+      GenerateRandomByteArray(kEndpointIdLength);
+
+  // Return the first kEndpointIdLength characters of the Base64-encoded string.
+  return base::Base64Encode(raw_endpoint_info).substr(0, kEndpointIdLength);
+}
+
+std::vector<uint8_t> GenerateEndpointInfo() {
+  return GenerateRandomByteArray(kEndpointInfoLength);
 }
 
 }  // namespace
@@ -62,7 +78,7 @@ NearbyEndpointFinderImpl::NearbyEndpointFinderImpl(
         location::nearby::connections::mojom::NearbyConnections>&
         nearby_connections)
     : nearby_connections_(nearby_connections),
-      endpoint_id_(base::RandBytesAsString(kEndpointIdLength)),
+      endpoint_id_(GenerateEndpointId()),
       endpoint_info_(GenerateEndpointInfo()) {}
 
 NearbyEndpointFinderImpl::~NearbyEndpointFinderImpl() {
