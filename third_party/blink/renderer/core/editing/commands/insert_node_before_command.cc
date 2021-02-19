a0f00155db36e1d65e63ba78d3fc52098259729d
Oriol Brufau <obrufau@igalia.com>
2021-02-19 04:23:10 +0000

    [editing] Abort InsertNodeBeforeCommand in case of exception
    InsertNodeBeforeCommand::DoApply was calling ContainerNode::InsertBefore
    with IGNORE_EXCEPTION_FOR_TESTING as the ExceptionState, therefore
    silently ignoring any exceptions.
    
    This was problematic in case of FormatBlockCommand, which relies on
    InsertNodeBeforeCommand in order to wrap an existing element inside a
    container. However, this can't work if the element is the root element.
    So the silent failure was breaking some assumptions of the code.
    
    This patch makes InsertNodeBeforeCommand::DoApply abort the editing
    command if ContainerNode::InsertBefore had an exception, so that
    FormatBlockCommand can detect the problem and return early.
    
    Bug: 1172656
    
    TEST=ApplyBlockElementCommandTest.FormatBlockWithDirectChildrenOfRoot
    
    Change-Id: I57519cfcf0beb8239e4f8ab6e889c65ce5d60772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2698472
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#855631}

--- third_party/blink/renderer/core/editing/commands/insert_node_before_command.cc
+++ third_party/blink/renderer/core/editing/commands/insert_node_before_command.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/editing/commands/insert_node_before_command.h"
 
+#include "third_party/blink/renderer/core/editing/commands/editing_state.h"
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 
@@ -51,7 +52,7 @@ InsertNodeBeforeCommand::InsertNodeBeforeCommand(
       << ref_child_->parentNode();
 }
 
-void InsertNodeBeforeCommand::DoApply(EditingState*) {
+void InsertNodeBeforeCommand::DoApply(EditingState* editing_state) {
   ContainerNode* parent = ref_child_->parentNode();
   GetDocument().UpdateStyleAndLayoutTree();
   if (!parent || (should_assume_content_is_always_editable_ ==
@@ -60,8 +61,9 @@ void InsertNodeBeforeCommand::DoApply(EditingState*) {
     return;
   DCHECK(HasEditableStyle(*parent)) << parent;
 
-  parent->InsertBefore(insert_child_.Get(), ref_child_.Get(),
-                       IGNORE_EXCEPTION_FOR_TESTING);
+  DummyExceptionStateForTesting exception_state;
+  parent->InsertBefore(insert_child_.Get(), ref_child_.Get(), exception_state);
+  ABORT_EDITING_COMMAND_IF(exception_state.HadException());
 }
 
 void InsertNodeBeforeCommand::DoUnapply() {
