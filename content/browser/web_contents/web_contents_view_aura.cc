4fa3eb9134ac2b8ac76be023553946fd403c2ac2
John Delaney <johnidel@chromium.org>
2021-02-19 22:47:12 +0000

    Fix potential null dereference in WebDragSourceAura
    Fixes a potential null dereference when RenderProcessGone and
    WebContentsDestroyed are both invoked on WebDragSourceAura.
    
    Prior to this using a WebContentsObserver, this used the
    disconnected notification which fired at most once per source avoiding
    this issue.
    
    Bug: 1180290
    Change-Id: I2fb8952c63052942be323ed58b455546b7112030
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705645
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855965}

--- content/browser/web_contents/web_contents_view_aura.cc
+++ content/browser/web_contents/web_contents_view_aura.cc
@@ -132,6 +132,9 @@ class WebDragSourceAura : public content::WebContentsObserver {
   void WebContentsDestroyed() override { CancelDrag(); }
 
   void CancelDrag() {
+    if (!window_)
+      return;
+
     // Cancel the drag if it is still in progress.
     aura::client::DragDropClient* dnd_client =
         aura::client::GetDragDropClient(window_->GetRootWindow());
@@ -145,7 +148,6 @@ class WebDragSourceAura : public content::WebContentsObserver {
 
  private:
   aura::Window* window_;
-  NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(WebDragSourceAura);
 };
