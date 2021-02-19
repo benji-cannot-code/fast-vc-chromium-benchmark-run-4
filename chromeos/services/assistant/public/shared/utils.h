67a13ae0ede9de7fcfbbd241da98ffe9be7c0c78
Jeroen Dhollander <jeroendh@google.com>
2021-02-19 22:01:27 +0000

    Remove unused include in chromeos/services/assistant/public/shared/utils.h
    Bug: b/180449465
    Test: compiled
    Change-Id: Iacee9bbc21a97aeaf19a5d06513350f5b99c1bd1
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2707135
    Auto-Submit: Jeroen Dhollander <jeroendh@chromium.org>
    Reviewed-by: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855929}

--- chromeos/services/assistant/public/shared/utils.h
+++ chromeos/services/assistant/public/shared/utils.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/component_export.h"
-// TODO(b/180449465): Remove this include
-#include "chromeos/services/libassistant/public/cpp/android_app_info.h"
 
 namespace chromeos {
 namespace assistant {
