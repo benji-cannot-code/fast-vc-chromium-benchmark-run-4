3fa2d4cbd15b582629f0fd3493c58809f021c93b
Peter Kasting <pkasting@chromium.org>
2021-02-19 21:43:28 +0000

    Get already-fixed targets to build with std::u16string again.
    Bug: 911896
    Change-Id: If213710dc7370f76df2671424985b6b5ed548df4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2694916
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855919}

--- content/browser/accessibility/accessibility_tree_formatter_utils_win.cc
+++ content/browser/accessibility/accessibility_tree_formatter_utils_win.cc
@@ -700,7 +700,7 @@ CONTENT_EXPORT HWND GetHwndForProcess(base::ProcessId pid) {
 }
 
 struct HWNDSearchInfo {
-  base::string16 title;
+  std::wstring title;
   HWND hwnd;
 };
 
@@ -710,13 +710,13 @@ BOOL CALLBACK MatchWindow(HWND hwnd, LPARAM lParam) {
     return TRUE;
   }
 
-  base::string16 title(num_chars + 1, '\0');
+  std::wstring title(num_chars + 1, '\0');
   if (!::GetWindowText(hwnd, &title.front(), title.size())) {
     return TRUE;
   }
 
   auto* info = reinterpret_cast<HWNDSearchInfo*>(lParam);
-  if (title.find(info->title) != std::string::npos) {
+  if (title.find(info->title) != std::wstring::npos) {
     info->hwnd = hwnd;
     return FALSE;
   }
