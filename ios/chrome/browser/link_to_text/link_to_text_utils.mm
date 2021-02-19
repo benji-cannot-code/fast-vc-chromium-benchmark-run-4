5d95d3f66a0f3e8a29b2b404822b15f359d8bd36
Tommy Martino <tmartino@google.com>
2021-02-19 19:02:43 +0000

    [SH iOS] Integrate JS timeout
    Most of the work for this is covered in the third_party lib (which has
    its DEPS entry updated in this CL); the Chromium-side changes are to
    catch the new error that can be thrown by that lib, and log it.
    
    Bug: 1179377
    
    Change-Id: I4bec0d864ca66dd6cfb40e8896b0fadc9ba25671
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2701932
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Auto-Submit: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855801}

--- ios/chrome/browser/link_to_text/link_to_text_utils.mm
+++ ios/chrome/browser/link_to_text/link_to_text_utils.mm
@@ -53,6 +53,9 @@ shared_highlighting::LinkGenerationError OutcomeToError(
     case LinkGenerationOutcome::kAmbiguous:
       return LinkGenerationError::kContextExhausted;
       break;
+    case LinkGenerationOutcome::kTimeout:
+      return LinkGenerationError::kTimeout;
+      break;
     case LinkGenerationOutcome::kSuccess:
       // kSuccess is not supposed to happen, as it is not an error.
       NOTREACHED();
