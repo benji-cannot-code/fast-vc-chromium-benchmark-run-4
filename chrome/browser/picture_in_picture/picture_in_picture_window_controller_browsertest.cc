80d7c24e7ef8e8b95e6ea77dfa73a8a2ae807729
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2021-02-19 18:39:11 +0000

    Add PictureInPictureWindowControllerBrowserTest.ControlsVisibility
    Bring back MoveMouseOverOverlayWindow() and use it in a controls
    visibility test so issues like one introduced by crrev.com/c/2682527
    don't slip in this easily in the future.
    
    Bug: 1179907,1180090
    Change-Id: I9521e913841022b7bd7197bdcea64d86c3c8ed66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705723
    Commit-Queue: Wojciech Dzierżanowski <wdzierzanowski@opera.com>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#855783}

--- chrome/browser/picture_in_picture/picture_in_picture_window_controller_browsertest.cc
+++ chrome/browser/picture_in_picture/picture_in_picture_window_controller_browsertest.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/web_preferences/web_preferences.h"
 #include "ui/compositor/test/draw_waiter_for_test.h"
 #include "ui/display/display_switches.h"
+#include "ui/events/base_event_utils.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/view_observer.h"
@@ -273,6 +274,14 @@ class PictureInPictureWindowControllerBrowserTest
       ASSERT_EQ(IsOverlayWindowControlVisible(control), expected_visible);
   }
 
+  void MoveMouseOverOverlayWindow() {
+    auto* const window = GetOverlayWindow();
+    const gfx::Point p(window->GetBounds().origin());
+    ui::MouseEvent moved_over(ui::ET_MOUSE_MOVED, p, p, ui::EventTimeForNow(),
+                              ui::EF_NONE, ui::EF_NONE);
+    window->OnMouseEvent(&moved_over);
+  }
+
  private:
   content::PictureInPictureWindowController* pip_window_controller_ = nullptr;
   MockPictureInPictureWindowController mock_controller_;
@@ -313,6 +322,16 @@ IN_PROC_BROWSER_TEST_F(PictureInPictureWindowControllerBrowserTest,
   EXPECT_TRUE(platform_util::IsWindowActive(native_window));
 #endif
 #endif
+}
+
+IN_PROC_BROWSER_TEST_F(PictureInPictureWindowControllerBrowserTest,
+                       ControlsVisibility) {
+  LoadTabAndEnterPictureInPicture(
+      browser(), base::FilePath(kPictureInPictureWindowSizePage));
+
+  EXPECT_FALSE(GetOverlayWindow()->AreControlsVisible());
+  MoveMouseOverOverlayWindow();
+  EXPECT_TRUE(GetOverlayWindow()->AreControlsVisible());
 }
 
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
