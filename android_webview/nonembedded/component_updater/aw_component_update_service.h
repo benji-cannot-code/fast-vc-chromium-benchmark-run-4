c9baa824f4a722607024afb5749db6aa65a50646
Hazem Ashmawy <hazems@chromium.org>
2021-02-19 21:16:20 +0000

    [AW] Add custom AwComponentInstallerPolicyDelegate
    Add AwComponentInstallerPolicyDelegate which provides custom WebView
    implementation for the callbacks used by ComponentUpdateService. It
    acts as a bridge to communicate between the native
    ComponentUpdateService and ComponentProviderService.
    
    To register a component, typically a component author would extend
    the component installer policy with a custom policy for webview that
    delegate callbacks to WebView delegate.
    
    This is CL#3 to implement and enable component updater in WebView. See
    design doc for more details http://go/wv-component-updater
    
    Bug: 1171768
    Test: out/bin/run_android_webview_unittests
    Test: manually launch the AwComponentUpdateService and notice debug logs
    Change-Id: Ifbab14852473d5d7f9c0f4fd0b3a163efa4db33a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2675909
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855896}

--- android_webview/nonembedded/component_updater/aw_component_update_service.h
+++ android_webview/nonembedded/component_updater/aw_component_update_service.h
@@ -7,14 +7,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ANDROID_WEBVIEW_NONEMBEDDED_COMPONENT_UPDATER_AW_COMPONENT_UPDATE_SERVICE_H_
 
 #include <memory>
+#include <string>
 
 #include "base/no_destructor.h"
+
+namespace base {
+class FilePath;
+class Version;
+}  // namespace base
 
 namespace component_updater {
 class ComponentUpdateService;
 }  // namespace component_updater
 
 namespace android_webview {
+
+class MockAwComponentUpdateService;
 
 // Native-side implementation of the AwComponentUpdateService, it initializes
 // ComponentUpdateService and registers components.
@@ -23,14 +31,21 @@ class AwComponentUpdateService {
   static AwComponentUpdateService* GetInstance();
   void MaybeStartComponentUpdateService();
 
+  virtual bool NotifyNewVersion(const std::string& component_id,
+                                const base::FilePath& install_dir,
+                                const base::Version& version);
+
  private:
   friend base::NoDestructor<AwComponentUpdateService>;
+  friend MockAwComponentUpdateService;
 
   AwComponentUpdateService();
-  ~AwComponentUpdateService();
+  virtual ~AwComponentUpdateService();
 
   std::unique_ptr<component_updater::ComponentUpdateService> cus_;
 };
+
+void SetAwComponentUpdateServiceForTesting(AwComponentUpdateService* service);
 
 }  // namespace android_webview
 
