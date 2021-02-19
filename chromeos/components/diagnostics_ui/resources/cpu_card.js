1c3d6d768afd59431140e1576cdaba17ae1c7d50
Joon Ahn <joonbug@chromium.org>
2021-02-19 22:28:00 +0000

    diagnostics: routine section remaining time text
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: Ife57a6867a54bcf06d33abd629cbafad512a7f68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702278
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855950}

--- chromeos/components/diagnostics_ui/resources/cpu_card.js
+++ chromeos/components/diagnostics_ui/resources/cpu_card.js
@@ -161,4 +161,10 @@ Polymer({
         'currentCpuSpeedText',
         this.convertKhzToGhz_(this.cpuUsage_.scalingCurrentFrequencyKhz));
   },
+
+  /** @protected */
+  getEstimateRuntimeInMinutes_() {
+    // Each routine runs for a minute
+    return this.routines_.length;
+  },
 });
