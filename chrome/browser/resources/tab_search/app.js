cc5dd902793dbce799b762594385a9dd1a8f1ade
Tom <tluk@chromium.org>
2021-02-19 23:32:08 +0000

    Tab Search: Reset search text and selected state when hiding
    This CL ensures that when Tab Search is hidden the search text and
    selected state are restored to ensure Tab Search starts from a clean
    slate when a cached WebContents is used for re-invocation.
    
    Bug: 1177048
    Change-Id: Ie3f6ca630f575a81679b7137b1cf8ef60041d655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703146
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855986}

--- chrome/browser/resources/tab_search/app.js
+++ chrome/browser/resources/tab_search/app.js
@@ -109,6 +109,8 @@ export class TabSearchAppElement extends PolymerElement {
       // Refresh Tab Search's tab data when transitioning into a visible state.
       if (document.visibilityState === 'visible') {
         this.updateTabs_();
+      } else {
+        this.onDocumentHidden_();
       }
     };
   }
@@ -168,6 +170,12 @@ export class TabSearchAppElement extends PolymerElement {
 
     document.removeEventListener(
         'visibilitychange', this.visibilityChangedListener_);
+  }
+
+  /** @private */
+  onDocumentHidden_() {
+    (this.$.tabsList).selected = NO_SELECTION;
+    this.$.searchField.setValue('');
   }
 
   /** @private */
@@ -477,9 +485,14 @@ export class TabSearchAppElement extends PolymerElement {
     this.searchResultText_ = this.getA11ySearchResultText_();
   }
 
-  /** return {!Tab} */
+  /** @return {!Tab} */
   getSelectedTab_() {
     return this.filteredOpenTabs_[this.getSelectedIndex()].tab;
+  }
+
+  /** @return {string} */
+  getSearchTextForTesting() {
+    return this.searchText_;
   }
 }
 
