832b0e5a48ce42e443e4d703310e74a0b1f2ca08
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2021-02-19 20:11:28 +0000

    Cancel ExtensionUninstallDialog when Profile* is deleted
    With the DestroyProfileOnBrowserClose flag, the Profile* can get
    deleted while the dialog is still visible. Listen for the
    OnProfileWillBeDestroyed() event, so the dialog can close itself if
    this happens.
    
    Bug: 88586
    Change-Id: I8774e5ac726225b4e97560967206b9cc7afc2c4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2699270
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855858}

--- chrome/browser/extensions/extension_uninstall_dialog.h
+++ chrome/browser/extensions/extension_uninstall_dialog.h
@@ -10,11 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "base/strings/string16.h"
 #include "base/threading/thread_checker.h"
 #include "chrome/browser/extensions/chrome_app_icon.h"
 #include "chrome/browser/extensions/chrome_app_icon_delegate.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_observer.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_registry_observer.h"
 #include "extensions/browser/uninstall_reason.h"
@@ -23,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 class NativeWindowTracker;
-class Profile;
 
 namespace extensions {
 class Extension;
@@ -31,7 +32,8 @@ class Extension;
 class ExtensionUninstallDialog
     : public base::SupportsWeakPtr<ExtensionUninstallDialog>,
       public ChromeAppIconDelegate,
-      public ExtensionRegistryObserver {
+      public ExtensionRegistryObserver,
+      public ProfileObserver {
  public:
   // Implement this callback to handle checking for the dialog's header message.
   using OnWillShowCallback =
@@ -139,17 +141,15 @@ class ExtensionUninstallDialog
                               const Extension* extension,
                               UninstallReason reason) override;
 
+  // ProfileObserver:
+  void OnProfileWillBeDestroyed(Profile* profile) override;
+
   // Displays the prompt. This should only be called after loading the icon.
   // The implementations of this method are platform-specific.
   virtual void Show() = 0;
 
-  // Returns true if a checkbox for reporting abuse should be shown.
-  bool ShouldShowReportAbuseCheckbox() const;
-
-  // Returns true if a checkbox for removing associated data should be shown.
-  bool ShouldShowRemoveDataCheckbox() const;
-
-  Profile* const profile_;
+  // Resets to nullptr when the Profile is deleted.
+  Profile* profile_;
 
   // The dialog's parent window.
   gfx::NativeWindow parent_;
@@ -158,7 +158,7 @@ class ExtensionUninstallDialog
   Delegate* delegate_;
 
   // The extension we are showing the dialog for.
-  scoped_refptr<const Extension> extension_;
+  scoped_refptr<const Extension> extension_ = nullptr;
 
   // The extension triggering the dialog if the dialog was shown by
   // chrome.management.uninstall.
@@ -172,9 +172,17 @@ class ExtensionUninstallDialog
   // Indicates that dialog was shown.
   bool dialog_shown_ = false;
 
+  // True if a checkbox for reporting abuse is shown.
+  bool show_report_abuse_checkbox_ = false;
+
+  // True if a checkbox for removing associated data is shown.
+  bool show_remove_data_checkbox_ = false;
+
   UninstallReason uninstall_reason_ = UNINSTALL_REASON_FOR_TESTING;
 
-  ScopedObserver<ExtensionRegistry, ExtensionRegistryObserver> observer_{this};
+  base::ScopedObservation<ExtensionRegistry, ExtensionRegistryObserver>
+      registry_observation_{this};
+  base::ScopedObservation<Profile, ProfileObserver> profile_observation_{this};
 
   base::ThreadChecker thread_checker_;
 
