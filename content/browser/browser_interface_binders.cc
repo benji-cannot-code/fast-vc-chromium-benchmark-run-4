7b53ef33f583f7e0b9933a945caab8f566aea12d
Tal Pressman <talp@chromium.org>
2021-02-19 06:09:04 +0000

    Reland "[VideoCapture] Make VideoCaptureHost per frame instead of per process"
    This is a reland of crrev.com/c/2632614 by guidou@, fixing the renderer
    crashes reported in crbug.com/116866. The failing bot was run manually
    to verify the fix.
    
    Original change description:
    Prior to this CL, there was a VideoCaptureHost mojo object per renderer
    process.  This CL makes it per frame, which is preferred.
    
    This CL keeps the mojo client (VideoCaptureImpl and
    WebVideoCaptureImplManager) in platform since they do not really need
    to know about frames, only about a browser interface broker.
    LocalVideoCaptureClientImpl is moved to modules/mediastream, where it
    can know about frames and can pass the corresponding BIB to the video.
    The other user of the video-capture client is pepper, which already
    knew about frames.
    
    
    Bug: 1167900, 1168666, 1126758
    Cq-Include-Trybots: luci.chrome.try:chromeos-betty-pi-arc-chrome
    Change-Id: I854ce0d833ac2db4f9ac8853d3549627c33dd0ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703551
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Tal Pressman <talp@google.com>
    Cr-Commit-Position: refs/heads/master@{#855642}

--- content/browser/browser_interface_binders.cc
+++ content/browser/browser_interface_binders.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/file_utilities_host_impl.h"
 #include "content/browser/renderer_host/media/media_devices_dispatcher_host.h"
 #include "content/browser/renderer_host/media/media_stream_dispatcher_host.h"
+#include "content/browser/renderer_host/media/video_capture_host.h"
 #include "content/browser/renderer_host/raw_clipboard_host_impl.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/renderer_host/render_process_host_impl.h"
@@ -68,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/buildflags/buildflags.h"
 #include "device/vr/public/mojom/vr_service.mojom.h"
 #include "media/capture/mojom/image_capture.mojom.h"
+#include "media/capture/mojom/video_capture.mojom.h"
 #include "media/mojo/mojom/interface_factory.mojom-forward.h"
 #include "media/mojo/mojom/media_metrics_provider.mojom.h"
 #include "media/mojo/mojom/media_player.mojom.h"
@@ -743,6 +745,12 @@ void PopulateFrameBinders(RenderFrameHostImpl* host, mojo::BinderMap* map) {
     map->Add<blink::mojom::MediaStreamDispatcherHost>(
         base::BindRepeating(&MediaStreamDispatcherHost::Create,
                             host->GetProcess()->GetID(), host->GetRoutingID(),
+                            base::Unretained(media_stream_manager)),
+        GetIOThreadTaskRunner({}));
+
+    map->Add<media::mojom::VideoCaptureHost>(
+        base::BindRepeating(&VideoCaptureHost::Create,
+                            host->GetProcess()->GetID(),
                             base::Unretained(media_stream_manager)),
         GetIOThreadTaskRunner({}));
   }
