a412251b0a857319aaafea063ebb431a9263fb4e
Li Lin <llin@chromium.org>
2021-02-19 18:22:23 +0000

    Update projector feature pod button toggle state.
    Bug: 1175934
    Test: manual tests
    Change-Id: I554ec611c6930450e3fe2e5fc9faf9779ee955f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703494
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855769}

--- ash/projector/projector_feature_pod_controller.h
+++ ash/projector/projector_feature_pod_controller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_PROJECTOR_PROJECTOR_FEATURE_POD_CONTROLLER_H_
 #define ASH_PROJECTOR_PROJECTOR_FEATURE_POD_CONTROLLER_H_
 
+#include "ash/projector/model/projector_ui_model.h"
 #include "ash/system/unified/feature_pod_controller_base.h"
 
 namespace ash {
@@ -13,7 +14,8 @@ namespace ash {
 class UnifiedSystemTrayController;
 
 // Controller of a feature pod button that toggles Projector tools.
-class ProjectorFeaturePodController : public FeaturePodControllerBase {
+class ProjectorFeaturePodController : public FeaturePodControllerBase,
+                                      public ProjectorUiModelObserver {
  public:
   explicit ProjectorFeaturePodController(
       UnifiedSystemTrayController* controller);
@@ -26,6 +28,9 @@ class ProjectorFeaturePodController : public FeaturePodControllerBase {
   FeaturePodButton* CreateButton() override;
   void OnIconPressed() override;
   SystemTrayItemUmaType GetUmaType() const override;
+
+  // ProjectorUiModelObserver:
+  void OnProjectorBarStateChanged(bool enabled) override;
 
  private:
   UnifiedSystemTrayController* const tray_controller_;
