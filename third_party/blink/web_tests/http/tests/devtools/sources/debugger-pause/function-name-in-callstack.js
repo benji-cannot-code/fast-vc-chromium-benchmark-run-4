ce509325c2e830e41f102feefa3cca9e926c78c4
Benedikt Meurer <bmeurer@chromium.org>
2021-02-19 13:15:58 +0000

    Rebaseline and reenable tests after "displayName" removal.
    Bug: chromium:1177685
    Change-Id: I9abb1b893943f9bded10f22dbc67bdc5104fb5ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705381
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#855683}

--- third_party/blink/web_tests/http/tests/devtools/sources/debugger-pause/function-name-in-callstack.js
+++ third_party/blink/web_tests/http/tests/devtools/sources/debugger-pause/function-name-in-callstack.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 (async function() {
   TestRunner.addResult(
-      `Tests that callFrames on pause contains function name taking into account displayName and Function.name.\n`);
+      `Tests that callFrames on pause contains function name taking into account Function.name (and ignoring displayName).\n`);
   await TestRunner.loadModule('sources_test_runner');
   await TestRunner.showPanel('sources');
   await TestRunner.evaluateInPagePromise(`
