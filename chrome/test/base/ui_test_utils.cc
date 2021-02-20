a390b19aa5604215d87770f3f5d3b00ee4d0e67d
Alexander Timin <altimin@chromium.org>
2021-02-20 21:18:21 +0000

    Make //chrome's NavigateToURL* methods return RFH instead of RPH.
    Return newly-navigated-to-RenderFrameHost, which is more flexible than
    returning RenderProcessHost, as it's easy to get RenderProcessHost from
    RenderFrameHost, but not vice versa.
    
    R=avi@chromium.org
    
    Change-Id: Ia830dc53174255262665c17f0f8dfd68d70d8af2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2709585
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856116}

--- chrome/test/base/ui_test_utils.cc
+++ chrome/test/base/ui_test_utils.cc
@@ -216,13 +216,13 @@ void NavigateToURLWithPost(Browser* browser, const GURL& url) {
   NavigateToURL(&params);
 }
 
-content::RenderProcessHost* NavigateToURL(Browser* browser, const GURL& url) {
+content::RenderFrameHost* NavigateToURL(Browser* browser, const GURL& url) {
   return NavigateToURLWithDisposition(browser, url,
                                       WindowOpenDisposition::CURRENT_TAB,
                                       BROWSER_TEST_WAIT_FOR_LOAD_STOP);
 }
 
-content::RenderProcessHost*
+content::RenderFrameHost*
 NavigateToURLWithDispositionBlockUntilNavigationsComplete(
     Browser* browser,
     const GURL& url,
@@ -271,13 +271,13 @@ NavigateToURLWithDispositionBlockUntilNavigationsComplete(
   }
   if (disposition == WindowOpenDisposition::CURRENT_TAB) {
     same_tab_observer.Wait();
-    return web_contents->GetMainFrame()->GetProcess();
+    return web_contents->GetMainFrame();
   } else if (web_contents) {
     content::TestNavigationObserver observer(
         web_contents, number_of_navigations,
         content::MessageLoopRunner::QuitMode::DEFERRED);
     observer.Wait();
-    return web_contents->GetMainFrame()->GetProcess();
+    return web_contents->GetMainFrame();
   }
   EXPECT_TRUE(web_contents)
       << " Unable to wait for navigation to \"" << url.spec() << "\""
@@ -285,7 +285,7 @@ NavigateToURLWithDispositionBlockUntilNavigationsComplete(
   return nullptr;
 }
 
-content::RenderProcessHost* NavigateToURLWithDisposition(
+content::RenderFrameHost* NavigateToURLWithDisposition(
     Browser* browser,
     const GURL& url,
     WindowOpenDisposition disposition,
@@ -294,7 +294,7 @@ content::RenderProcessHost* NavigateToURLWithDisposition(
       browser, url, 1, disposition, browser_test_flags);
 }
 
-content::RenderProcessHost* NavigateToURLBlockUntilNavigationsComplete(
+content::RenderFrameHost* NavigateToURLBlockUntilNavigationsComplete(
     Browser* browser,
     const GURL& url,
     int number_of_navigations) {
