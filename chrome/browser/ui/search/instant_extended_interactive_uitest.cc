3418a3054c9db28d97af3b982b445e22e1485836
Alexander Timin <altimin@chromium.org>
2021-02-20 19:17:18 +0000

    Remove NOTIFICATION_NAV_ENTRY_COMMITTED from tests.
    R=avi@chromium.org
    BUG=1174760
    
    Change-Id: I70365a33265f75e2da71ffd8477e1f59388b330f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2709843
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856109}

--- chrome/browser/ui/search/instant_extended_interactive_uitest.cc
+++ chrome/browser/ui/search/instant_extended_interactive_uitest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "content/public/test/test_navigation_observer.h"
 #include "content/public/test/test_utils.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -87,11 +88,10 @@ class InstantExtendedTest : public InProcessBrowserTest,
   }
 
   void PressEnterAndWaitForNavigation() {
-    content::WindowedNotificationObserver nav_observer(
-        content::NOTIFICATION_NAV_ENTRY_COMMITTED,
-        content::NotificationService::AllSources());
+    content::TestNavigationObserver observer(
+        browser()->tab_strip_model()->GetActiveWebContents(), 1);
     browser()->window()->GetLocationBar()->AcceptInput();
-    nav_observer.Wait();
+    observer.WaitForNavigationFinished();
   }
 
   void PressEnterAndWaitForFrameLoad() {
