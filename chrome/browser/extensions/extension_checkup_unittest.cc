9dece4fc4ce8ee9d4d2cdc2433ae38aa48ebd2ec
David Bertoni <dbertoni@chromium.org>
2021-02-20 01:07:55 +0000

    [Extensions] Fix and re-enable ExtensionCheckupTest tests.
    These tests have been flaky because there is a race condition
    between the ScopedFeatureList they use and ExtensionServiceTestBase's
    BrowserTaskEnvironment, which will try to access the ScopedFeatureList
    after it's been destroyed. Since ExtensionServiceTestBase already
    has a ScopedFeatureList with the proper lifetime, the fix is to use
    that rather than one in ExtensionCheckupTest.
    
    This fix passed 3000 iterations on all of the bots without any flakes.
    
    Bug: 1163169, 1163232, 1163813, 1163917
    Change-Id: I4dea8ac27b7881723db2379bf9e7a7c2582e446e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2707447
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856026}

--- chrome/browser/extensions/extension_checkup_unittest.cc
+++ chrome/browser/extensions/extension_checkup_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_checkup.h"
 
-#include "base/test/scoped_feature_list.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_service_test_base.h"
@@ -22,7 +21,7 @@ class ExtensionCheckupTest : public ExtensionServiceTestBase,
   ~ExtensionCheckupTest() override {}
 
   void SetUp() override {
-    scoped_feature_list_.InitAndEnableFeatureWithParameters(
+    feature_list_.InitAndEnableFeatureWithParameters(
         extensions_features::kExtensionsCheckup,
         {{extensions_features::kExtensionsCheckupEntryPointParameter,
           GetParam()}});
@@ -73,36 +72,15 @@ class ExtensionCheckupTest : public ExtensionServiceTestBase,
       EXPECT_FALSE(ShouldShowExtensionsCheckupPromo(browser_context()));
   }
 
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
-
   DISALLOW_COPY_AND_ASSIGN(ExtensionCheckupTest);
 };
 
-// Checkup is not shown if no extensions are installed.
-// Flaky on TSAN: https://crbug.com/1163813
-#if defined(THREAD_SANITIZER)
-#define MAYBE_NoInstalledExtensions DISABLED_NoInstalledExtensions
-#else
-#define MAYBE_NoInstalledExtensions NoInstalledExtensions
-#endif
-TEST_P(ExtensionCheckupTest, MAYBE_NoInstalledExtensions) {
+TEST_P(ExtensionCheckupTest, NoInstalledExtensions) {
   VerifyNonExperimentCheckupDisabled();
   EXPECT_FALSE(ShouldShowExperimentCheckup());
 }
 
-// Checkup is not shown if the only extensions installed are policy
-// installed, component extensions, or installed by default.
-//
-// Flaky on various Linux bots.  http://crbug.com/1163917
-// TODO(crbug.com/1052397): Revisit once build flag switch of lacros-chrome is
-// complete.
-#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
-#define MAYBE_NoUserInstalledExtensions DISABLED_NoUserInstalledExtensions
-#else
-#define MAYBE_NoUserInstalledExtensions NoUserInstalledExtensions
-#endif
-TEST_P(ExtensionCheckupTest, MAYBE_NoUserInstalledExtensions) {
+TEST_P(ExtensionCheckupTest, NoUserInstalledExtensions) {
   AddExemptExtensions();
   VerifyNonExperimentCheckupDisabled();
   EXPECT_FALSE(ShouldShowExperimentCheckup());
