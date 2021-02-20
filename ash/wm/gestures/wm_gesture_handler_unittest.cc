e683c5f5bf655f930e11e131c211836835e0312b
Judy Wang <shidi@chromium.org>
2021-02-20 01:01:57 +0000

    ChromeOS: Disable touchpad finger swipe in the "Locked Mode"
    Test: Manual Test and Unit Test.
    
    Bug: 1175358
    Change-Id: I2f838fe742d62a81c9f37d1ad5d7c21d23e242ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2706644
    Commit-Queue: Judy Wang <shidi@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856021}

--- ash/wm/gestures/wm_gesture_handler_unittest.cc
+++ ash/wm/gestures/wm_gesture_handler_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/root_window_desk_switch_animator_test_api.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/overview/overview_test_util.h"
+#include "ash/wm/screen_pinning_controller.h"
 #include "ash/wm/window_cycle/window_cycle_controller.h"
 #include "ash/wm/window_cycle/window_cycle_list.h"
 #include "ash/wm/window_util.h"
@@ -288,6 +289,25 @@ TEST_F(WmGestureHandlerTest, EnterOverviewOnScrollEnd) {
                               GetOffsetY(-10), num_fingers);
   GetEventGenerator()->Dispatch(&fling_start);
   EXPECT_TRUE(InOverviewSession());
+}
+
+// Test switch desk is disabled when screen is pinned.
+TEST_F(WmGestureHandlerTest, LockedModeNoSwitchDesk) {
+  auto* desk_controller = DesksController::Get();
+  desk_controller->NewDesk(DesksCreationRemovalSource::kButton);
+  ASSERT_EQ(2u, desk_controller->desks().size());
+  ASSERT_EQ(desk_controller->desks()[0].get(), desk_controller->active_desk());
+
+  // Pin a window to current desk.
+  aura::Window* w1 = CreateTestWindowInShellWithId(0);
+  wm::ActivateWindow(w1);
+  window_util::PinWindow(w1, /*trusted=*/false);
+  EXPECT_TRUE(Shell::Get()->screen_pinning_controller()->IsPinned());
+
+  // Tests that scrolling right won't switch desks when screen is pinned.
+  const float long_scroll = WmGestureHandler::kHorizontalThresholdDp;
+  Scroll(long_scroll, 0.f, kNumFingersForDesksSwitch);
+  EXPECT_EQ(desk_controller->desks()[0].get(), desk_controller->active_desk());
 }
 
 using DesksGestureHandlerTest = WmGestureHandlerTest;
