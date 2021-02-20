a70c130feeb76ef0bfad35e9febf756b7c34c675
Dana Fried <dfried@chromium.org>
2021-02-20 01:39:42 +0000

    More general fix for tabstrip layout bugs when adding/removing tabs
    The common denominator in all of these issues is calling
    AnimateToIdealBounds() without invalidating the layout. However,
    AnimateToIdealBounds() implicitly changes the result of
    CalculatePreferredSize() because it checks IsAnimating(). To fix this
    problem, AnimateToIdealBounds() now explicitly calls
    PreferredSizeChanged().
    
    Bug: 1177186
    Change-Id: I0f0d8f3dc8d58b90127a76cbbb1000b8f932e840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2689330
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856035}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1314,10 +1314,6 @@ void TabStrip::AddTabAt(int model_index, TabRendererData data, bool is_active) {
   SwapLayoutIfNecessary();
   UpdateAccessibleTabIndices();
 
-  // Need to ensure an old cached preferred size isn't used before the first
-  // tick of the animation. See crbug.com/1172910.
-  PreferredSizeChanged();
-
   for (TabStripObserver& observer : observers_)
     observer.OnTabAdded(model_index);
 
@@ -2793,6 +2789,12 @@ void TabStrip::AnimateToIdealBounds() {
             base::BindRepeating(&TabStrip::OnTabSlotAnimationProgressed,
                                 base::Unretained(this))));
   }
+
+  // Because the preferred size of the tabstrip depends on the IsAnimating()
+  // condition, but starting an animation  doesn't necessarily invalidate the
+  // existing preferred size and layout (which may now be incorrect), we need to
+  // signal this explicitly.
+  PreferredSizeChanged();
 }
 
 void TabStrip::SnapToIdealBounds() {
