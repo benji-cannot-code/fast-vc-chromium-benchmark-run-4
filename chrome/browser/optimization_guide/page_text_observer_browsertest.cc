ab58ba42d75cc73506aa2bd348635c756211df27
Robert Ogden <robertogden@chromium.org>
2021-02-20 04:58:58 +0000

    Reduce OptGuide Text Observer Browstertest flakes
    These tests are flaky since there's not always enough time inbetween
    commit and first layout for the text dump request to make it to the
    renderer.
    A render-blocking JS is added which is slowed by the test server,
    which helps the race.
    
    Bug: 1179653
    Change-Id: If2dfc3133b295b82add9a4f3a92b2a9368460290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2707683
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856065}

--- chrome/browser/optimization_guide/page_text_observer_browsertest.cc
+++ chrome/browser/optimization_guide/page_text_observer_browsertest.cc
@@ -21,7 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_base.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
+#include "net/http/http_status_code.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "net/test/embedded_test_server/http_request.h"
+#include "net/test/embedded_test_server/http_response.h"
 
 namespace optimization_guide {
 
@@ -92,6 +95,14 @@ class PageTextObserverBrowserTest : public InProcessBrowserTest {
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
     InProcessBrowserTest::SetUpOnMainThread();
+
+    embedded_test_server()->RegisterRequestHandler(
+        base::BindRepeating(&PageTextObserverBrowserTest::HandleSlowRequest,
+                            base::Unretained(this)));
+    embedded_test_server()->ServeFilesFromSourceDirectory(
+        "chrome/test/data/optimization_guide");
+
+    ASSERT_TRUE(embedded_test_server()->Start());
   }
 
   content::WebContents* web_contents() {
@@ -101,12 +112,28 @@ class PageTextObserverBrowserTest : public InProcessBrowserTest {
   PageTextObserver* observer() {
     return PageTextObserver::FromWebContents(web_contents());
   }
+
+ private:
+  // This script is render blocking in the HTML, but is intentionally slow. This
+  // provides important time between commit and first layout for any text dump
+  // requests to make it to the renderer, reducing flakes.
+  std::unique_ptr<net::test_server::HttpResponse> HandleSlowRequest(
+      const net::test_server::HttpRequest& request) {
+    std::string path_value;
+    if (request.GetURL().path() == "/slow-first-layout.js") {
+      std::unique_ptr<net::test_server::DelayedHttpResponse> resp =
+          std::make_unique<net::test_server::DelayedHttpResponse>(
+              base::TimeDelta::FromMilliseconds(500));
+      resp->set_code(net::HTTP_OK);
+      resp->set_content_type("application/javascript");
+      resp->set_content(std::string());
+      return resp;
+    }
+    return nullptr;
+  }
 };
 
 IN_PROC_BROWSER_TEST_F(PageTextObserverBrowserTest, SimpleCase) {
-  embedded_test_server()->ServeFilesFromSourceDirectory(
-      "chrome/test/data/optimization_guide");
-  ASSERT_TRUE(embedded_test_server()->Start());
   PageTextObserver::CreateForWebContents(web_contents());
   ASSERT_TRUE(observer());
 
@@ -125,9 +152,6 @@ IN_PROC_BROWSER_TEST_F(PageTextObserverBrowserTest, SimpleCase) {
 }
 
 IN_PROC_BROWSER_TEST_F(PageTextObserverBrowserTest, FirstLayoutAndOnLoad) {
-  embedded_test_server()->ServeFilesFromSourceDirectory(
-      "chrome/test/data/optimization_guide");
-  ASSERT_TRUE(embedded_test_server()->Start());
   PageTextObserver::CreateForWebContents(web_contents());
   ASSERT_TRUE(observer());
 
