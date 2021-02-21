2766d9f3b6fb7435414c0e275389a826c20a2c3e
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-02-21 23:21:53 +0000

    Revert "[Nearby] Make SendMessageExpress single use"
    This reverts commit d76f8843392c212b127408d0713c75718f98b95b.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 856048 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2Q3NmY4ODQzMzkyYzIxMmIxMjc0MDhkMDcxM2M3NTcxOGY5OGI5NWIM
    
    Sample Failed Build: https://ci.chromium.org/b/8854786629476449440
    
    Sample Failed Step: unit_tests
    
    Original change's description:
    > [Nearby] Make SendMessageExpress single use
    > 
    > WebRtcSignalingMessenger::SendMessage() did not allow for overlapping
    > calls because under the hood it relied on state in a single instance of
    > SendMessageExpress.
    > 
    > This change makes each WebRtcSignalingMessenger::SendMessage() call use
    > a unique SendMessageExpress instance that is tied to the life of the
    > mojo call and cleaned up when complete.
    > 
    > See: https://crbug.com/1180403 for a more involved future refactor.
    > 
    > Change-Id: I54037e371c1a8e6ff61975f12d5b07838dca2e3d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2706629
    > Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    > Reviewed-by: Josh Nohle <nohle@chromium.org>
    > Commit-Queue: James Vecore <vecore@google.com>
    > Cr-Commit-Position: refs/heads/master@{#856048}
    
    
    Change-Id: Id6065967196d7d89dfffa04b09a6c5f1686cd274
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2709754
    Cr-Commit-Position: refs/heads/master@{#856143}

--- chrome/browser/nearby_sharing/instantmessaging/send_message_express.h
+++ chrome/browser/nearby_sharing/instantmessaging/send_message_express.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/nearby_sharing/instantmessaging/token_fetcher.h"
 
 namespace chrome_browser_nearby_sharing_instantmessaging {
 class SendMessageExpressRequest;
@@ -24,21 +23,15 @@ class SharedURLLoaderFactory;
 class SimpleURLLoader;
 }  // namespace network
 
-namespace signin {
-class IdentityManager;
-}  // namespace signin
+class TokenFetcher;
 
-// Sends messages using the Instant Messaging API over HTTP. This object is
-// intended to service exactly one request to SendMessage per object. The
-// WebRtcSignalingMessenger that creates this object may choose to clean it up
-// after the SuccessCallback is invoked so no interaction with the |this|
-// pointer should happen after that point.
+// Sends messages using the Instant Messaging API over HTTP.
 class SendMessageExpress {
  public:
   using SuccessCallback = base::OnceCallback<void(bool success)>;
 
   SendMessageExpress(
-      signin::IdentityManager* identity_manager,
+      TokenFetcher* token_fetcher,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
   ~SendMessageExpress();
 
@@ -57,7 +50,7 @@ class SendMessageExpress {
       SuccessCallback callback,
       std::unique_ptr<std::string> response_body);
 
-  TokenFetcher token_fetcher_;
+  TokenFetcher* token_fetcher_;
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
   base::WeakPtrFactory<SendMessageExpress> weak_ptr_factory_{this};
 };
