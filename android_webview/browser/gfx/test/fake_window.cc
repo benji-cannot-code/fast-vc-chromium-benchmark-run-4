7178179355e332e3d5360ab9da7eb9f2f311f6b3
Vasiliy Telezhnikov <vasilyt@chromium.org>
2021-02-22 14:31:15 +0000

    Fix BrowserViewRenderer tests
    This CL fixes few issues with the test:
    * Switches client_id in FrameSinkId to 1 (to be compatible with viz)
    * Set device scale to 1.0 (otherwise CompositorFrames are invalid)
    * Makes render_thread in FakeWindow global, because webview expects
      it to be global.
    
    Bug: 1180688
    Change-Id: Icbd02c52d06576a7ffe7577b3110c3e2d2c9efc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2710948
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856266}

--- android_webview/browser/gfx/test/fake_window.cc
+++ android_webview/browser/gfx/test/fake_window.cc
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/init/gl_factory.h"
 
 namespace android_webview {
+namespace {
+std::unique_ptr<base::Thread> g_render_thread;
+}
 
 class FakeWindow::ScopedMakeCurrent {
  public:
@@ -70,8 +73,6 @@ FakeWindow::~FakeWindow() {
                                   base::Unretained(this), &completion));
     completion.Wait();
   }
-
-  render_thread_.reset();
 }
 
 void FakeWindow::Detach() {
@@ -170,13 +171,17 @@ void FakeWindow::CheckCurrentlyOnUIThread() {
 
 void FakeWindow::CreateRenderThreadIfNeeded() {
   CheckCurrentlyOnUIThread();
-  if (render_thread_) {
-    DCHECK(render_thread_loop_);
+  if (render_thread_loop_) {
+    DCHECK(g_render_thread);
     return;
   }
-  render_thread_.reset(new base::Thread("TestRenderThread"));
-  render_thread_->Start();
-  render_thread_loop_ = render_thread_->task_runner();
+
+  if (!g_render_thread) {
+    g_render_thread = std::make_unique<base::Thread>("TestRenderThread");
+    g_render_thread->Start();
+  }
+
+  render_thread_loop_ = g_render_thread->task_runner();
   rt_checker_.DetachFromSequence();
 
   base::WaitableEvent completion(
--- android_webview/browser/gfx/test/fake_window.h
+++ android_webview/browser/gfx/test/fake_window.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_surface.h"
 
 namespace base {
-class Thread;
 class WaitableEvent;
 }
 
@@ -89,7 +88,6 @@ class FakeWindow {
   base::SequenceChecker ui_checker_;
 
   // Render thread members.
-  std::unique_ptr<base::Thread> render_thread_;
   base::SequenceChecker rt_checker_;
   scoped_refptr<base::SingleThreadTaskRunner> render_thread_loop_;
   scoped_refptr<gl::GLSurface> surface_;
