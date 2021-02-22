88e4bdfd64df7cbf390c9a46aec1e1e0f69a0d2d
Chris Hamilton <chrisha@chromium.org>
2021-02-22 19:51:06 +0000

    Migrate media/blink to use strongly typed frame tokens.
    BUG=1096617
    
    Change-Id: Id37a147b2b7d9107db9cf52e33daa0c10e10042a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713423
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856384}

--- media/blink/webmediaplayer_impl_unittest.cc
+++ media/blink/webmediaplayer_impl_unittest.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/public/platform/media/webmediaplayer_delegate.h"
 #include "third_party/blink/public/platform/scheduler/web_thread_scheduler.h"
 #include "third_party/blink/public/platform/web_fullscreen_video_status.h"
@@ -327,12 +328,12 @@ class WebMediaPlayerImplTest
                                          /*opener=*/nullptr,
                                          mojo::NullAssociatedReceiver(),
                                          *agent_group_scheduler)),
-        web_local_frame_(blink::WebLocalFrame::CreateMainFrame(
-            web_view_,
-            &web_frame_client_,
-            nullptr,
-            base::UnguessableToken::Create(),
-            nullptr)),
+        web_local_frame_(
+            blink::WebLocalFrame::CreateMainFrame(web_view_,
+                                                  &web_frame_client_,
+                                                  nullptr,
+                                                  blink::LocalFrameToken(),
+                                                  nullptr)),
         context_provider_(viz::TestContextProvider::Create()),
         audio_parameters_(TestAudioParameters::Normal()),
         memory_dump_manager_(
