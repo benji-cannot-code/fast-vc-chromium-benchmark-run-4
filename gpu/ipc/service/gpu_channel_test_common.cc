0896f829f83d0d2aa60df19356110dad1f0df143
Vasiliy Telezhnikov <vasilyt@chromium.org>
2021-02-22 14:27:47 +0000

    Make gpu_unittests work with SkiaRenderer
    because it doesn't implement enough gl functions. For tests that uses
    stub implementation we don't care about time of context, so this CL
    switches to GrContext::MakeMock in this case.
    
    GrContext: :MakeGL can't create context if we use stub gl implementation
    Change-Id: I20f416c8190c253b3117aecb9c8950d6b9cf9a74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2711406
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856264}

--- gpu/ipc/service/gpu_channel_test_common.cc
+++ gpu/ipc/service/gpu_channel_test_common.cc
@@ -6,11 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/service/gpu_channel_test_common.h"
 
 #include "base/memory/unsafe_shared_memory_region.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/memory_dump_manager.h"
-#include "components/viz/common/features.h"
 #include "gpu/command_buffer/common/activity_flags.h"
 #include "gpu/command_buffer/service/scheduler.h"
 #include "gpu/command_buffer/service/shared_image_manager.h"
@@ -83,9 +81,6 @@ GpuChannelTestCommon::GpuChannelTestCommon(
   // We need GL bindings to actually initialize command buffers.
   if (use_stub_bindings) {
     gl::GLSurfaceTestSupport::InitializeOneOffWithStubBindings();
-    // GrContext cannot be created with stub bindings.
-    scoped_feature_list_ = std::make_unique<base::test::ScopedFeatureList>();
-    scoped_feature_list_->InitAndDisableFeature(features::kUseSkiaRenderer);
   } else {
     gl::GLSurfaceTestSupport::InitializeOneOff();
   }
