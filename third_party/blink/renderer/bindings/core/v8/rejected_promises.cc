1c93ed73938bf8abe3289df71422c88561d6ba99
Daniel Cheng <dcheng@chromium.org>
2021-02-22 02:27:28 +0000

    [blink] Remove WTF::Passed() usage in bindings.
    The WTF::Passed() implementation allows unsafe callbacks to be created.
    In Blink, its usage should largely be unnecessary. WTF::Bind() creates a
    OnceCallback, so std::move() works fine. WTF::BindRepeating() creates a
    repeating callback, but using WTF::Passed() is undesirable, since the
    underlying callback usually ends up being a OnceCallback in disguise.
    
    Bug: 1180103
    Change-Id: Iaa15e47b8f4882a4681f5a8089352fab8be4732a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2711474
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856161}

--- third_party/blink/renderer/bindings/core/v8/rejected_promises.cc
+++ third_party/blink/renderer/bindings/core/v8/rejected_promises.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/rejected_promises.h"
 
 #include <memory>
+#include <utility>
 
 #include "base/memory/ptr_util.h"
 #include "third_party/blink/public/platform/platform.h"
@@ -215,7 +216,7 @@ void RejectedPromises::HandlerAdded(v8::PromiseRejectMessage data) {
       context->GetTaskRunner(TaskType::kDOMManipulation)
           ->PostTask(FROM_HERE, WTF::Bind(&RejectedPromises::RevokeNow,
                                           scoped_refptr<RejectedPromises>(this),
-                                          WTF::Passed(std::move(message))));
+                                          std::move(message)));
       reported_as_errors_.EraseAt(i);
       return;
     }
@@ -245,7 +246,7 @@ void RejectedPromises::ProcessQueue() {
     kv.key->GetTaskRunner(blink::TaskType::kDOMManipulation)
         ->PostTask(FROM_HERE, WTF::Bind(&RejectedPromises::ProcessQueueNow,
                                         scoped_refptr<RejectedPromises>(this),
-                                        WTF::Passed(std::move(kv.value))));
+                                        std::move(kv.value)));
   }
 }
 
--- third_party/blink/renderer/bindings/core/v8/script_streamer.cc
+++ third_party/blink/renderer/bindings/core/v8/script_streamer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_streamer.h"
 
 #include <memory>
+#include <utility>
 
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
@@ -103,9 +104,9 @@ class SourceStream : public v8::ScriptCompiler::ExternalSourceStream {
           memcpy(copy_for_resource.get(), buffer, num_bytes);
           PostCrossThreadTask(
               *loading_task_runner_, FROM_HERE,
-              CrossThreadBindOnce(
-                  NotifyClientDidReceiveData, response_body_loader_client_,
-                  WTF::Passed(std::move(copy_for_resource)), num_bytes));
+              CrossThreadBindOnce(NotifyClientDidReceiveData,
+                                  response_body_loader_client_,
+                                  std::move(copy_for_resource), num_bytes));
 
           result = data_pipe_->EndReadData(num_bytes);
           CHECK_EQ(result, MOJO_RESULT_OK);
@@ -629,7 +630,7 @@ bool ScriptStreamer::TryStartStreamingTask() {
   worker_pool::PostTask(
       FROM_HERE, {base::TaskPriority::USER_BLOCKING, base::MayBlock()},
       CrossThreadBindOnce(RunScriptStreamingTask,
-                          WTF::Passed(std::move(script_streaming_task)),
+                          std::move(script_streaming_task),
                           WrapCrossThreadPersistent(this),
                           WTF::CrossThreadUnretained(stream_)));
 
