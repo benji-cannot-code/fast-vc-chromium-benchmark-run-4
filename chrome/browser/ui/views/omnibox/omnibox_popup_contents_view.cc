dd445600c1f2aa304111a44932a70dbcb54f315c
Andrew Xu <andrewxu@chromium.org>
2021-02-22 19:28:08 +0000

    [View] Rename SetMouseHandler() to SetMouseAndGestureHandler()
    Now RootView::SetMouseHandler() and View::SetMouseHandler() always
    modify the gesture handler stored in the RootView instance. Hence,
    the current function name is misleading. This CL renames
    SetMouseHandler() to SetMouseAndGestureHandler(). No logical change
    should be introduced by this CL.
    
    Bug: 1163282
    Change-Id: I9ffa80c7ca64a28ec57877a54ace467834c971fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702717
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856369}

--- chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
@@ -443,7 +443,7 @@ void OmniboxPopupContentsView::OnMatchIconUpdated(size_t match_index) {
 }
 
 void OmniboxPopupContentsView::OnDragCanceled() {
-  SetMouseHandler(nullptr);
+  SetMouseAndGestureHandler(nullptr);
 }
 
 bool OmniboxPopupContentsView::OnMouseDragged(const ui::MouseEvent& event) {
@@ -455,7 +455,7 @@ bool OmniboxPopupContentsView::OnMouseDragged(const ui::MouseEvent& event) {
   // If the drag event is over the bounds of one of the result views, pass
   // control to that view.
   if (HasMatchAt(index)) {
-    SetMouseHandler(result_view_at(index));
+    SetMouseAndGestureHandler(result_view_at(index));
     return false;
   }
 
