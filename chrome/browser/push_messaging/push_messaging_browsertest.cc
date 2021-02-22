120053502fd8d899cf45c9319556d02eeaeec828
Illia Klimov <elklm@google.com>
2021-02-22 23:28:57 +0000

    Enable quiet UI notification prompt phases 3 and 4 in fieldtrial_testing_config.json.
    TBR=rayankans@chromium.org
    
    Bug: 1180197
    Change-Id: I303866ec3572caca3123bcdd4aeefee7d6dd1a46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705395
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856493}

--- chrome/browser/push_messaging/push_messaging_browsertest.cc
+++ chrome/browser/push_messaging/push_messaging_browsertest.cc
@@ -1691,10 +1691,6 @@ class PushMessagingBrowserTestWithAbusiveOriginPermissionRevocation
 IN_PROC_BROWSER_TEST_F(
     PushMessagingBrowserTestWithAbusiveOriginPermissionRevocation,
     PushEventPermissionRevoked) {
-  AddToPreloadDataBlocklist(https_server()->GetURL("/").GetOrigin(),
-                            SiteReputation::ABUSIVE_CONTENT);
-  AddToSafeBrowsingBlocklist(https_server()->GetURL("/").GetOrigin());
-
   ASSERT_NO_FATAL_FAILURE(SubscribeSuccessfully());
   PushMessagingAppIdentifier app_identifier =
       GetAppIdentifierForServiceWorkerRegistration(0LL);
@@ -1703,6 +1699,11 @@ IN_PROC_BROWSER_TEST_F(
   std::string script_result;
   ASSERT_TRUE(RunScript("isControlled()", &script_result));
   ASSERT_EQ("true - is controlled", script_result);
+
+  // Add an origin to blocking lists after service worker is registered.
+  AddToPreloadDataBlocklist(https_server()->GetURL("/").GetOrigin(),
+                            SiteReputation::ABUSIVE_CONTENT);
+  AddToSafeBrowsingBlocklist(https_server()->GetURL("/").GetOrigin());
 
   gcm::IncomingMessage message;
   message.sender_id = GetTestApplicationServerKey();
