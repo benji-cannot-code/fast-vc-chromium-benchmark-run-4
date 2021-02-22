d136802605c389c76126932ce221d2eefe83d42c
Josh Nohle <nohle@google.com>
2021-02-22 21:32:00 +0000

    [Nearby] Add metrics for Nearby Share contact data
    Add metrics to track data about user contact list downloads from the
    Nearby server. We track success rates for the HTTP calls, measure the
    download times, and the analyze the distribution of contact types--for
    example, Google vs. device contacts, reachable vs. unreachable contacts,
    percentage selected for sharing, etc.
    
    Manually verified that the histograms are emitted to as expected.
    
    Bug: 1105579
    Change-Id: I873f301e8f453026c01d461a454920d80d908b93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2707138
    Reviewed-by: James Vecore <vecore@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856448}

--- chrome/browser/nearby_sharing/contacts/nearby_share_contact_downloader.cc
+++ chrome/browser/nearby_sharing/contacts/nearby_share_contact_downloader.cc
@@ -9,20 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 
-namespace {
-
-void RecordSuccessMetrics(
-    const std::vector<nearbyshare::proto::ContactRecord>& contacts) {
-  // TODO(https://crbug.com/1105579): Record a success/failure histogram value.
-  // TODO(https://crbug.com/1105579): Record total number of contacts returned.
-}
-
-void RecordFailureMetrics() {
-  // TODO(https://crbug.com/1105579): Record a success/failure histogram value.
-}
-
-}  // namespace
-
 NearbyShareContactDownloader::NearbyShareContactDownloader(
     const std::string& device_id,
     SuccessCallback success_callback,
@@ -45,7 +31,6 @@ void NearbyShareContactDownloader::Succeed(
     uint32_t num_unreachable_contacts_filtered_out) {
   DCHECK(was_run_);
   DCHECK(success_callback_);
-  RecordSuccessMetrics(contacts);
 
   std::move(success_callback_)
       .Run(std::move(contacts), num_unreachable_contacts_filtered_out);
@@ -54,7 +39,6 @@ void NearbyShareContactDownloader::Succeed(
 void NearbyShareContactDownloader::Fail() {
   DCHECK(was_run_);
   DCHECK(failure_callback_);
-  RecordFailureMetrics();
 
   std::move(failure_callback_).Run();
 }
