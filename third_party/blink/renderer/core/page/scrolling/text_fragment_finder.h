6f3187ad3cbb880743220b2e62912174b9bea02c
Gayane Petrosyan <gayane@chromium.org>
2021-02-22 20:41:48 +0000

    [SH-Blink] Refactor TextFragmentFinder to run in batches
    This is a prep work for running FindMatch in batches. Refactoring
    TextFragmentFinder so that each step can be run as a separate task.
    
    Bug: 1177503, 930156
    Change-Id: I8ed505e0e934b61d2cf08754c544fb17e8c217c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2689225
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856412}

--- third_party/blink/renderer/core/page/scrolling/text_fragment_finder.h
+++ third_party/blink/renderer/core/page/scrolling/text_fragment_finder.h
@@ -6,8 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_PAGE_SCROLLING_TEXT_FRAGMENT_FINDER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAGE_SCROLLING_TEXT_FRAGMENT_FINDER_H_
 
+#include "base/callback.h"
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/editing/ephemeral_range.h"
 #include "third_party/blink/renderer/core/editing/forward.h"
+#include "third_party/blink/renderer/core/editing/position.h"
+#include "third_party/blink/renderer/core/editing/position_iterator.h"
 #include "third_party/blink/renderer/core/page/scrolling/text_fragment_anchor_metrics.h"
 #include "third_party/blink/renderer/core/page/scrolling/text_fragment_selector.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -19,7 +23,8 @@ class Document;
 // This is a helper class to TextFragmentAnchor. It's responsible for actually
 // performing the text search for the anchor and returning the results to the
 // anchor.
-class CORE_EXPORT TextFragmentFinder final {
+class CORE_EXPORT TextFragmentFinder final
+    : public GarbageCollected<TextFragmentFinder> {
  public:
   class Client {
    public:
@@ -36,19 +41,80 @@ class CORE_EXPORT TextFragmentFinder final {
                                          const PositionInFlatTree& end);
 
   // Client must outlive the finder.
-  TextFragmentFinder(Client& client, const TextFragmentSelector& selector);
+  TextFragmentFinder(Client& client,
+                     const TextFragmentSelector& selector,
+                     Document* document);
   ~TextFragmentFinder() = default;
 
   // Begins searching in the given top-level document.
-  void FindMatch(Document& document);
+  void FindMatch();
+
+  void Trace(Visitor*) const;
 
  private:
+  // Used for tracking what should be the next match stage.
+  enum SelectorMatchStep {
+    kMatchPrefix,
+    kMatchTextStart,
+    kMatchTextEnd,
+    kMatchSuffix
+  };
+
+  void FindMatchFromPosition(PositionInFlatTree search_start);
+
+  void OnFindMatchInRangeComplete(String search_text,
+                                  Range* range,
+                                  bool word_start_bounded,
+                                  bool word_end_bounded,
+                                  const EphemeralRangeInFlatTree& match);
+
+  void FindMatchInRange(String search_text,
+                        Range* range,
+                        bool word_start_bounded,
+                        bool word_end_bounded);
+
+  void FindPrefix();
+  void FindTextStart();
+  void FindTextEnd();
+  void FindSuffix();
+
+  void OnPrefixMatchComplete(EphemeralRangeInFlatTree match);
+  void OnTextStartMatchComplete(EphemeralRangeInFlatTree match);
+  void OnTextEndMatchComplete(EphemeralRangeInFlatTree match);
+  void OnSuffixMatchComplete(EphemeralRangeInFlatTree match);
+
+  void GoToStep(SelectorMatchStep step);
+
+  void OnMatchComplete();
+
+  void SetPotentialMatch(EphemeralRangeInFlatTree range);
+  void SetPrefixMatch(EphemeralRangeInFlatTree range);
+
   Client& client_;
   const TextFragmentSelector selector_;
+  Member<Document> document_;
+  SelectorMatchStep step_ = kMatchPrefix;
 
-  EphemeralRangeInFlatTree FindMatchFromPosition(
-      Document& document,
-      PositionInFlatTree search_start);
+  // Start positions for the next text end |FindTask|, this is separate as the
+  // search for end might move the position, which should be discarded.
+  PositionInFlatTree range_end_search_start_;
+
+  // Successful match after |FindMatchFromPosition| first run.
+  Member<Range> first_match_;
+  // Current match after |FindMatchFromPosition| run. See
+  // https://wicg.github.io/scroll-to-text-fragment/#ref-for-range-collapsed:~:text=Let-,potentialMatch
+  Member<Range> potential_match_;
+  // Used for text start match, the presence will change how the text start is
+  // matched. See
+  // https://wicg.github.io/scroll-to-text-fragment/#ref-for-range-collapsed:~:text=Let-,prefixMatch
+  Member<Range> prefix_match_;
+  // Range for the current match task, including |prefix_match_|,
+  // |potential_match_| and |suffix_match|. See
+  // https://wicg.github.io/scroll-to-text-fragment/#ref-for-range-collapsed:~:text=Let-,searchRange
+  Member<Range> search_range_;
+  // Range used for search for |potential_match_|.
+  // https://wicg.github.io/scroll-to-text-fragment/#ref-for-range-collapsed:~:text=Let-,matchRange
+  Member<Range> match_range_;
 };
 
 }  // namespace blink
