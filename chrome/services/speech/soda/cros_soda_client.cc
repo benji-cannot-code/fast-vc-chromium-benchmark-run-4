952ed164c415d047bc98019f6e6dbadcf55f33e2
Rob Schonberger <robsc@chromium.org>
2021-02-22 22:18:42 +0000

    Update to use BindMachineLearningService to allow threaded exec.
    This modifies execution to bind an ML service first, which allows for execution from any sequence.
    
    TEST=Compiles happily
    
    BUG=b:160550533
    
    Change-Id: I56c1ae9253f237b40b5bffe6522c3de09e25aa61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703813
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856474}

--- chrome/services/speech/soda/cros_soda_client.cc
+++ chrome/services/speech/soda/cros_soda_client.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/services/speech/soda/cros_soda_client.h"
 #include "base/run_loop.h"
 #include "chromeos/services/machine_learning/public/cpp/service_connection.h"
+#include "chromeos/services/machine_learning/public/mojom/machine_learning_service.mojom.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
 namespace soda {
@@ -38,23 +39,23 @@ void CrosSodaClient::Reset(
   }
   soda_recognizer_.reset();
   soda_client_.reset();
+  ml_service_.reset();
   is_initialized_ = true;
   auto mojom_config = chromeos::machine_learning::mojom::SodaConfig::New();
   mojom_config->channel_count = channel_count_;
   mojom_config->sample_rate = sample_rate_;
-
   chromeos::machine_learning::ServiceConnection::GetInstance()
-      ->GetMachineLearningService()
-      .LoadSpeechRecognizer(
-          std::move(mojom_config), soda_client_.BindNewPipeAndPassRemote(),
-          soda_recognizer_.BindNewPipeAndPassReceiver(),
-          base::BindOnce(
-              [](chromeos::machine_learning::mojom::LoadModelResult result) {
-                if (result !=
-                    chromeos::machine_learning::mojom::LoadModelResult::OK) {
-                  LOG(DFATAL) << "Could not load recognizer, error: " << result;
-                }
-              }));
+      ->BindMachineLearningService(ml_service_.BindNewPipeAndPassReceiver());
+  ml_service_->LoadSpeechRecognizer(
+      std::move(mojom_config), soda_client_.BindNewPipeAndPassRemote(),
+      soda_recognizer_.BindNewPipeAndPassReceiver(),
+      base::BindOnce(
+          [](chromeos::machine_learning::mojom::LoadModelResult result) {
+            if (result !=
+                chromeos::machine_learning::mojom::LoadModelResult::OK) {
+              LOG(DFATAL) << "Could not load recognizer, error: " << result;
+            }
+          }));
 
   callback_ = callback;
   // Ensure this one is started.
--- chrome/services/speech/soda/cros_soda_client.h
+++ chrome/services/speech/soda/cros_soda_client.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
+#include "chromeos/services/machine_learning/public/mojom/machine_learning_service.mojom.h"
 #include "chromeos/services/machine_learning/public/mojom/soda.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -55,6 +56,8 @@ class CrosSodaClient : public chromeos::machine_learning::mojom::SodaClient {
   mojo::Remote<chromeos::machine_learning::mojom::SodaRecognizer>
       soda_recognizer_;
   mojo::Receiver<chromeos::machine_learning::mojom::SodaClient> soda_client_;
+  mojo::Remote<chromeos::machine_learning::mojom::MachineLearningService>
+      ml_service_;
 };
 }  // namespace soda
 #endif  // CHROME_SERVICES_SPEECH_SODA_CROS_SODA_CLIENT_H_
