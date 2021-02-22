3ccb35ad5e889f6f70453879e7fb26b3d1277f1f
Michael Thiessen <mthiesse@chromium.org>
2021-02-22 18:39:54 +0000

    Ensure only trusted Intents can set a Tab ID
    This is a preventative measure. I'm not sure what kind of stuff
    external apps could do using new tab intents, but if they could somehow
    guess an AsyncTabCreationParams id they could do a bunch of weird and
    probably insecure things.
    
    I think this intent extra was originally intended to be Chrome-only,
    but we've got lots of paths into the tab creator now and many of them
    don't look for trusted intent extras, so this change protects against
    third party intents mucking with tab IDs in a more robust way.
    
    Bug: 1166870
    Change-Id: Iaebf8a5a71044c8207e1b5b67d3de903742889f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2707665
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856340}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityTabFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityTabFactory.java
@@ -11,7 +11,6 @@ import android.util.Pair;
 import androidx.annotation.Nullable;
 
 import org.chromium.base.Callback;
-import org.chromium.base.IntentUtils;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.app.tabmodel.AsyncTabParamsManagerSingleton;
@@ -131,10 +130,8 @@ public class CustomTabActivityTabFactory {
     public Tab createTab(
             WebContents webContents, TabDelegateFactory delegateFactory, Callback<Tab> action) {
         Intent intent = mIntentDataProvider.getIntent();
-        int assignedTabId =
-                IntentUtils.safeGetIntExtra(intent, IntentHandler.EXTRA_TAB_ID, Tab.INVALID_TAB_ID);
         return new TabBuilder()
-                .setId(assignedTabId)
+                .setId(IntentHandler.getTabId(intent))
                 .setIncognito(mIntentDataProvider.isIncognito())
                 .setWindow(mActivityWindowAndroid.get())
                 .setLaunchType(TabLaunchType.FROM_EXTERNAL_APP)
