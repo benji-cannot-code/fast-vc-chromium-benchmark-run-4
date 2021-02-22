2601d947d5c832bc328c069a49dacd37238bb862
Peter E Conn <peconn@chromium.org>
2021-02-22 10:28:51 +0000

    💸 Enable Site Isolation for Digital Goods Payments.
    When the DigitalGoodsImpl is created, we request that the current URL
    is isolated.
    
    Bug: 1163803
    Change-Id: I045dda3cc585bdcba71aa897693d0b4956d07f68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2666334
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856228}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/digitalgoods/SiteIsolator.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.browserservices.digitalgoods;
+
+import org.chromium.base.annotations.NativeMethods;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.url.GURL;
+
+/**
+ * This class calls into native to request that a give tab starts site isolation.
+ */
+public class SiteIsolator {
+    private SiteIsolator() {}
+
+    public static void startIsolatingSite(GURL url) {
+        Profile profile = Profile.getLastUsedRegularProfile();
+        SiteIsolatorJni.get().startIsolatingSite(profile, url);
+    }
+
+    @NativeMethods
+    interface Natives {
+        void startIsolatingSite(Profile profile, GURL url);
+    }
+}
