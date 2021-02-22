4196c34be7fb5ec9990aa74ed16458042ad23835
shivanigithub <shivanisha@chromium.org>
2021-02-22 19:24:30 +0000

    Add a flag for http cache partitioning
    Split cache is going to be fully launched so keep the about flag entry
    around for the first few releases for a workaround in case of an issue.
    This flag should expire after a few releases when the change is stable
    and we are not seeing issues reported.
    
    Change-Id: Ie5703f149129455a665f34dc1670998055b377f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2712722
    Commit-Queue: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856365}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1322,6 +1322,9 @@ extern const char kWebOTPCrossDeviceDescription[];
 extern const char kSpeculativeServiceWorkerStartOnQueryInputName[];
 extern const char kSpeculativeServiceWorkerStartOnQueryInputDescription[];
 
+extern const char kSplitCacheByNetworkIsolationKeyName[];
+extern const char kSplitCacheByNetworkIsolationKeyDescription[];
+
 extern const char kStopInBackgroundName[];
 extern const char kStopInBackgroundDescription[];
 
