eba8ef12ea26926fa2b2305b472513cc88888e96
Nohemi Fernandez <fernandex@chromium.org>
2021-02-22 12:44:54 +0000

    [iOS][MICE] Remove interrupted sync state infobar after sign-in flow.
    If a user chooses to disable Sync during the sign-in flow Chrome will
    display an infobar to remind the user that a disabled Sync is an
    intermediary state. This will no longer be the case in MICE and so
    we remove the infobar.
    
    Bug: 1167164
    Change-Id: Idf3a7f3a2d1942ba6193db15d424b20a263e6dfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705169
    Auto-Submit: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856243}

--- ios/chrome/browser/ui/settings/sync/utils/sync_util.mm
+++ ios/chrome/browser/ui/settings/sync/utils/sync_util.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_macros.h"
 #include "components/infobars/core/infobar_manager.h"
+#include "components/signin/public/base/account_consistency_method.h"
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/infobars/infobar_manager_impl.h"
@@ -204,6 +205,14 @@ bool DisplaySyncErrors(ChromeBrowserState* browser_state,
       loggedErrorState = SYNC_UNRECOVERABLE_ERROR;
       break;
     case SyncSetupService::kSyncSettingsNotConfirmed:
+      // Do not display sync not confirmed infobar if the user is in the MICE
+      // sign-in flow, since disabling Sync during sign-in is considered a
+      // valid end state.
+      // TODO(crbug.com/1084941): the kSyncSettingsNotConfirmed case should be
+      // removed following the launch of MICE.
+      if (signin::IsMobileIdentityConsistencyEnabled()) {
+        return false;
+      }
       loggedErrorState = SYNC_SYNC_SETTINGS_NOT_CONFIRMED;
       break;
   }
