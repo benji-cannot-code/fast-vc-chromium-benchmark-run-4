28c28c0152aeb01db2d5bdabacdee00e95fd0c19
Regan Hsu <hsuregan@google.com>
2021-02-23 22:32:37 +0000

    [CrOS Network] Fix CrPolicyNetworkBehaviorMojoTests for HTML outlaw.
    The importHTML statements will be banned in
    https://chromium-review.googlesource.com/c/chromium/src/+/2458206,
    so they are removed here to prevent test failure.
    
    Fixed: 1180692
    Change-Id: I3008f3b8f2f7a3dca1d70ca4cc05eb915198ca58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713260
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856884}

--- chrome/test/data/webui/cr_components/chromeos/network/cr_policy_network_behavior_mojo_tests.js
+++ chrome/test/data/webui/cr_components/chromeos/network/cr_policy_network_behavior_mojo_tests.js
@@ -16,9 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 suite('CrPolicyNetworkBehaviorMojo', function() {
   suiteSetup(async () => {
-    await PolymerTest.importHtml('chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.html');
-    await PolymerTest.importHtml('chrome://resources/mojo/chromeos/services/network_config/public/mojom/cros_network_config.mojom.html');
-
     Polymer({
       is: 'test-behavior',
 
