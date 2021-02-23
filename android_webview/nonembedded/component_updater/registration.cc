767fecf3f94ae9bf4437baec162e133c18561dac
Mugdha Lakhani <nator@chromium.org>
2021-02-23 09:23:28 +0000

    [AW][OriginTrials] Register Origin trials component.
    Registers origin trials component for update with component updater
    during AwComponentUpdateService startup.
    
    Bug: 1171772
    Change-Id: I3116546ad9f8f18b15f4b0fad3dda537311189bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2712543
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856602}

--- android_webview/nonembedded/component_updater/registration.cc
+++ android_webview/nonembedded/component_updater/registration.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "android_webview/nonembedded/component_updater/aw_component_installer_policy_delegate.h"
+#include "android_webview/nonembedded/component_updater/installer_policies/aw_origin_trials_component_installer.h"
 #include "android_webview/nonembedded/component_updater/installer_policies/aw_trust_token_key_commitments_component_installer_policy.h"
 #include "base/callback.h"
+#include "base/check.h"
 #include "base/memory/scoped_refptr.h"
 #include "components/component_updater/component_installer.h"
 
@@ -17,10 +19,10 @@ namespace android_webview {
 
 void RegisterComponentsForUpdate(
     component_updater::ComponentUpdateService* component_update_service) {
-  base::MakeRefCounted<component_updater::ComponentInstaller>(
-      std::make_unique<AwTrustTokenKeyCommitmentsComponentInstallerPolicy>(
-          std::make_unique<AwComponentInstallerPolicyDelegate>()))
-      ->Register(component_update_service, base::OnceClosure());
+  DCHECK(component_update_service);
+
+  RegisterOriginTrialsComponent(component_update_service);
+  RegisterTrustTokensComponent(component_update_service);
 }
 
 }  // namespace android_webview
