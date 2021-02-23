a5df88afe4bd5059da9ef249b01060fb4249ee90
Jan Wilken Dörrie <jdoerrie@chromium.org>
2021-02-23 08:56:18 +0000

    Revert "[Controls Refresh Android] Fix thin border for form controls: button, input, progress, etc."
    This reverts commit c50c499441f7b448d55def02d9db9c47f243900d.
    
    Reason for revert: Suspect for https://crbug.com/1181215
    
    Original change's description:
    > [Controls Refresh Android] Fix thin border for form controls: button, input, progress, etc.
    >
    > Prior to this CL, form controls' border does not respect zoom and
    > DSF if they are painted with native theme. This resulted in inconsistent
    > border on highDPI display between controls. Form controls that were
    > painted in blink, <meter> and multi-select, have a consistent border
    > appearance on highDPI with other elements like div {border: 1px solid}.
    > While other controls--button, input, progressbar--appear to have a
    > thin border on highDPI display.
    >
    > This CL fixes this issue by passing zoom level from blink to native
    > theme and applying the zoom to form controls' border. A decision is made
    > by Dev and UX to apply this fix to android only. We felt a general fix
    > for all platform introduces side effects that needs further analysis,
    > follow-up crbug.com/1179509.
    >
    >
    >
    > Bug: 1160104
    > Change-Id: Ied25eca8b39375dbc00de9d09132439787006c3c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2694095
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Yu Han <yuzhehan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#856492}
    
    Bug: 1160104
    Change-Id: I9e53150f09900fe085c16ca7761a8bb1af87af4a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2712759
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#856594}

--- ui/native_theme/native_theme.h
+++ ui/native_theme/native_theme.h
@@ -220,7 +220,6 @@ class NATIVE_THEME_EXPORT NativeTheme {
     SkColor arrow_color;
     SkColor background_color;
     int classic_state;  // Used on Windows when uxtheme is not available.
-    float zoom;
   };
 
   struct MenuBackgroundExtraParams {
@@ -234,7 +233,6 @@ class NATIVE_THEME_EXPORT NativeTheme {
     int value_rect_y;
     int value_rect_width;
     int value_rect_height;
-    float zoom;
   };
 
   struct ScrollbarArrowExtraParams {
@@ -301,7 +299,6 @@ class NATIVE_THEME_EXPORT NativeTheme {
     int classic_state;  // Used on Windows when uxtheme is not available.
     bool has_border;
     bool auto_complete_active;
-    float zoom;
   };
 
   struct TrackbarExtraParams {
