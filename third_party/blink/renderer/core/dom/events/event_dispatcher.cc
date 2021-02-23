12db69ffffbbd7313522d9184fcf7b26417590c7
Nicolás Peña Moreno <npm@chromium.org>
2021-02-23 22:47:44 +0000

    Set hadRecentInput when change event occurs
    Currently if the select element is chosen from a popup, hadRecentInput
    isn't set because the pointer events are handled by the popup, which has
    a different LocalFrameView. Thus we need to depend on the change event
    directly in order to set hadRecentInput.
    
    Bug: 1120282
    Change-Id: I5260e442513a534abb8d7ddaf2591f6209219d96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716045
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856893}

--- third_party/blink/renderer/core/dom/events/event_dispatcher.cc
+++ third_party/blink/renderer/core/dom/events/event_dispatcher.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/inspector/inspector_trace_events.h"
+#include "third_party/blink/renderer/core/layout/layout_shift_tracker.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/page/spatial_navigation_controller.h"
 #include "third_party/blink/renderer/core/timing/event_timing.h"
@@ -160,6 +161,10 @@ DispatchEventResult EventDispatcher::Dispatch() {
   LocalFrame* frame = node_->GetDocument().GetFrame();
   if (frame && frame->DomWindow())
     eventTiming = EventTiming::Create(frame->DomWindow(), *event_);
+  if (event_->type() == event_type_names::kChange && event_->isTrusted() &&
+      view_) {
+    view_->GetLayoutShiftTracker().NotifyChangeEvent();
+  }
   event_->GetEventPath().EnsureWindowEventContext();
 
   const bool is_click =
