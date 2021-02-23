dc803fc15007216e72500651636fffeebc94cf49
Martin Šrámek <msramek@chromium.org>
2021-02-23 12:35:14 +0000

    Add the chrome.privacy.websites.privacySandboxEnabled extensions API
    This is a simple boolean API binding directly into a boolean preference.
    
    Bug: 1154374
    Change-Id: I9f702191abdf9f79b2090a32e309c3951cba9741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2563468
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856645}

--- chrome/browser/extensions/api/preference/preference_api.cc
+++ chrome/browser/extensions/api/preference/preference_api.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/embedder_support/pref_names.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_service.h"
+#include "components/privacy_sandbox/privacy_sandbox_prefs.h"
 #include "components/proxy_config/proxy_config_pref_names.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/spellcheck/browser/pref_names.h"
@@ -116,6 +117,8 @@ const PrefMappingEntry kPrefMapping[] = {
     {"spellingServiceEnabled", spellcheck::prefs::kSpellCheckUseSpellingService,
      APIPermission::kPrivacy, APIPermission::kPrivacy},
     {"thirdPartyCookiesAllowed", prefs::kCookieControlsMode,
+     APIPermission::kPrivacy, APIPermission::kPrivacy},
+    {"privacySandboxEnabled", prefs::kPrivacySandboxApisEnabled,
      APIPermission::kPrivacy, APIPermission::kPrivacy},
     {"translationServiceEnabled", prefs::kOfferTranslateEnabled,
      APIPermission::kPrivacy, APIPermission::kPrivacy},
--- chrome/test/data/extensions/api_test/preference/standard/test.js
+++ chrome/test/data/extensions/api_test/preference/standard/test.js
@@ -22,6 +22,7 @@ var preferences_to_test = [
     root: chrome.privacy.websites,
     preferences: {
       thirdPartyCookiesAllowed: false,
+      privacySandboxEnabled: true,
       hyperlinkAuditingEnabled: false,
       referrersEnabled: false,
       doNotTrackEnabled: false,
