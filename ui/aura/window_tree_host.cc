76b9578fc9984726dbb785fa7e31425ad4ce4918
yjliu <yjliu@chromium.org>
2021-02-23 23:56:22 +0000

    Fix: WTH OnCompositingShuttingDown() not called properly
    Upon the destruction of Compositor, its observer WindowTreeHost was
    removed before OnCompositingShuttingDown() was called, rendering this
    function meaningless.
    
    Bug: n/a
    Change-Id: Iaf4eeb232b979e281ce723e0087ad52c6c084fd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2715663
    Auto-Submit: Jun Liu <yjliu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jun Liu <yjliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856922}

--- ui/aura/window_tree_host.cc
+++ ui/aura/window_tree_host.cc
@@ -560,8 +560,4 @@ void WindowTreeHost::OnCompositingChildResizing(ui::Compositor* compositor) {
   holding_pointer_moves_ = true;
 }
 
-void WindowTreeHost::OnCompositingShuttingDown(ui::Compositor* compositor) {
-  compositor->RemoveObserver(this);
-}
-
 }  // namespace aura
