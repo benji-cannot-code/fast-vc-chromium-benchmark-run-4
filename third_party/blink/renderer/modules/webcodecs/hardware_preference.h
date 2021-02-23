ee68376b9704813e5e80ebe4e396aa8ef150cc54
Thomas Guilbert <tguilbert@chromium.org>
2021-02-23 00:13:16 +0000

    Reland "Add HardwarePreference to VideoDecoder"
    This reverts commit 08f5cd663e6cd209ef73895ef6a6ddbd558a4d0c, itself
    reverting 6ac070494159d0c6810557c87904595ebcdc54bd.
    
    The original commit did not remove some file references from the
    generated_in_modules.gni file, which caused some build breaks.
    
    Bug: 1164013
    Change-Id: I5e98e6f21a74869d42c381c7a60e3c699edbaf62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713521
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856519}

---
+++ third_party/blink/renderer/modules/webcodecs/hardware_preference.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBCODECS_HARDWARE_PREFERENCE_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBCODECS_HARDWARE_PREFERENCE_H_
+
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+
+namespace blink {
+
+enum class HardwarePreference { kAllow, kDeny, kRequire };
+
+HardwarePreference StringToHardwarePreference(const String& value);
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_WEBCODECS_HARDWARE_PREFERENCE_H_
