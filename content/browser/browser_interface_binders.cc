2895eff084a86808c79272ff4dbf4bef3d391837
Honglin Yu <honglinyu@chromium.org>
2021-02-23 13:14:33 +0000

    Add the backend code for blink handwriting API
    This is the browser side backend of blink handwriting API:
    Discourse post:
    https://discourse.wicg.io/t/proposal-handwriting-recognition-api/4935
    Intent to prototype:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/VXUq1UY4m7Y
    Explainer:
    https://github.com/WICG/handwriting-recognition/blob/main/explainer.md
    Chrome Status:
    https://chromestatus.com/feature/5263213807534080
    
    Currently, it is only truly supported on CrOS by piping the request
    to mlservice.
    
    Bug: 1166910
    Test: combined with the other CLs in the chain, it works on betty.
    
    Change-Id: I1fa68b90e277236236910c041c9afc3a75f0e344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2680896
    Commit-Queue: Honglin Yu <honglinyu@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856651}

--- content/browser/browser_interface_binders.cc
+++ content/browser/browser_interface_binders.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/feature_observer.h"
 #include "content/browser/federated_learning/floc_service_impl.h"
 #include "content/browser/gpu/gpu_process_host.h"
+#include "content/browser/handwriting/handwriting_recognition_service_factory.h"
 #include "content/browser/image_capture/image_capture_impl.h"
 #include "content/browser/keyboard_lock/keyboard_lock_service_impl.h"
 #include "content/browser/loader/content_security_notifier.h"
@@ -709,6 +710,9 @@ void PopulateFrameBinders(RenderFrameHostImpl* host, mojo::BinderMap* map) {
 
   map->Add<payments::mojom::PaymentManager>(base::BindRepeating(
       &RenderFrameHostImpl::CreatePaymentManager, base::Unretained(host)));
+
+  map->Add<handwriting::mojom::HandwritingRecognitionService>(
+      base::BindRepeating(&CreateHandwritingRecognitionService));
 
   map->Add<blink::mojom::WebBluetoothService>(base::BindRepeating(
       &RenderFrameHostImpl::CreateWebBluetoothService, base::Unretained(host)));
