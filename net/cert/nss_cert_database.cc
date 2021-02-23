15bbc06db70fef25923e9869e68f36d7f61ca731
Pavol Marko <pmarko@google.com>
2021-02-23 17:32:29 +0000

    cros: Ensure cert is on public slot before modifying trust
    On Chrome OS, before modifying trust settings of a certificate, ensure
    that it is on the public slot. If not, copy it to the public slot.
    This works together with CL:2566763 to make NSS put trust objects on
    the public slot.
    
    Bug: 1132030
    Test: On Chrome OS, apply CL:2566763 and CL:2567878 then change trust of
    built-in certificates on chrome://settings/certificates .
    
    Change-Id: Ieb4516f33ad28d6b87eb38b5fc01ce6b6364d711
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2561386
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856741}

--- net/cert/nss_cert_database.cc
+++ net/cert/nss_cert_database.cc
@@ -180,6 +180,16 @@ void NSSCertDatabase::ListModules(std::vector<crypto::ScopedPK11Slot>* modules,
   }
 }
 
+bool NSSCertDatabase::SetCertTrust(CERTCertificate* cert,
+                                   CertType type,
+                                   TrustBits trust_bits) {
+  bool success = psm::SetCertTrust(cert, type, trust_bits);
+  if (success)
+    NotifyObserversCertDBChanged();
+
+  return success;
+}
+
 int NSSCertDatabase::ImportFromPKCS12(
     PK11SlotInfo* slot_info,
     const std::string& data,
@@ -325,16 +335,6 @@ NSSCertDatabase::TrustBits NSSCertDatabase::GetCertTrust(
     default:
       return TRUST_DEFAULT;
   }
-}
-
-bool NSSCertDatabase::SetCertTrust(CERTCertificate* cert,
-                                   CertType type,
-                                   TrustBits trust_bits) {
-  bool success = psm::SetCertTrust(cert, type, trust_bits);
-  if (success)
-    NotifyObserversCertDBChanged();
-
-  return success;
 }
 
 bool NSSCertDatabase::DeleteCertAndKey(CERTCertificate* cert) {
