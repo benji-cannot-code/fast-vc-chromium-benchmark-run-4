15f59c3bcfcb197294b59ee7859be417d9082f93
Mugdha Lakhani <nator@chromium.org>
2021-02-23 10:27:19 +0000

    [AW] Add a NetworkFetcher, NetworkFetcherFactory.
    Component updater code expects a NetworkFetcherFactory, which can
    create a NetworkFetcher for downloading dynamic config. The default
    implementation relies on use of network service which can't be used by
    the WebView non-embedded process. Create a Webview-specific one.
    
    A test has been added to verify the logic. To be able to mock out calls
    to native, the test has to be a junit test.
    
    Bug: 1174140
    Change-Id: Iee270087794f1d98ce681f8319d6d5e70a124d42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2679694
    Auto-Submit: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856608}

---
+++ android_webview/nonembedded/net/network_task.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "android_webview/nonembedded/net/network_task.h"
+
+#include <utility>
+
+#include "android_webview/nonembedded/net/download_file_task.h"
+#include "android_webview/nonembedded/net/network_impl.h"
+#include "base/sequenced_task_runner.h"
+
+namespace android_webview {
+
+NetworkTask::NetworkTask(
+    update_client::NetworkFetcher::ResponseStartedCallback
+        response_started_callback,
+    update_client::NetworkFetcher::ProgressCallback progress_callback)
+    : response_started_callback_(std::move(response_started_callback)),
+      progress_callback_(std::move(progress_callback)) {}
+
+NetworkTask::~NetworkTask() = default;
+
+}  // namespace android_webview
