b1e4978526d8206c69ef7638e1194f9bc6a8228f
Oksana Zhuravlova <oksamyt@chromium.org>
2021-02-23 21:15:39 +0000

    Notify ProcessVisibilityTracker in renderer process
    This change updates RenderThreadImpl::SetProcessState to notify
    ProcessVisibilityTracker about the new visibility state.
    
    Bug: 1177542
    Change-Id: I4ece736877fc8d836d7c3d73e00ca5cb0cf5c679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704730
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856843}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/buildflags.h"
 #include "content/common/content_constants_internal.h"
 #include "content/common/frame_messages.h"
+#include "content/common/process_visibility_tracker.h"
 #include "content/public/common/content_constants.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_paths.h"
@@ -1347,7 +1348,15 @@ void RenderThreadImpl::SetProcessState(
   }
 
   if (visible_state != visible_state_) {
-    if (visible_state == mojom::RenderProcessVisibleState::kVisible)
+    bool is_visible =
+        visible_state == mojom::RenderProcessVisibleState::kVisible;
+
+    if (!IsInBrowserProcess()) {
+      ProcessVisibilityTracker::GetInstance()->OnProcessVisibilityChanged(
+          is_visible);
+    }
+
+    if (is_visible)
       OnRendererVisible();
     else
       OnRendererHidden();
