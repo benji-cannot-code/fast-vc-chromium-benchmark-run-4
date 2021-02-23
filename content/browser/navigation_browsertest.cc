608a5086ab6c53222f5d94103b1faa0a0d0ee167
Aaron Colwell <acolwell@chromium.org>
2021-02-23 19:14:48 +0000

    Fix tests to pass in kProcessSharingWithStrictSiteInstances mode.
    Minor changes to a few tests that were failing in the default process
    mode. One test behavior was dependent on the RenderFrameHost changing
    so the code was updated to match the comment that stated this. A few
    other tests had expectations that depended on the default SiteInstance
    mode so the test was reworked to make that explicit.
    
    Bug: 1158650
    Change-Id: I68603cbcbca9f85868ae82bf474dd96da8dae161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713055
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Aaron Colwell <acolwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856785}

--- content/browser/navigation_browsertest.cc
+++ content/browser/navigation_browsertest.cc
@@ -4657,10 +4657,10 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTest,
       main_frame->child_at(0)->current_frame_host();
   RenderFrameHostImpl* grandchild_frame =
       child_frame->child_at(0)->current_frame_host();
-  if (AreAllSitesIsolatedForTesting()) {
-    EXPECT_NE(main_frame->GetSiteInstance(), child_frame->GetSiteInstance());
-  } else {
+  if (AreDefaultSiteInstancesEnabled()) {
     EXPECT_EQ(main_frame->GetSiteInstance(), child_frame->GetSiteInstance());
+  } else {
+    EXPECT_NE(main_frame->GetSiteInstance(), child_frame->GetSiteInstance());
   }
   EXPECT_EQ(main_frame->GetSiteInstance(), grandchild_frame->GetSiteInstance());
   EXPECT_EQ(main_frame->GetLastCommittedOrigin(),
@@ -4707,10 +4707,10 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTest,
       main_frame->child_at(0)->current_frame_host();
   RenderFrameHostImpl* grandchild_frame =
       child_frame->child_at(0)->current_frame_host();
-  if (AreAllSitesIsolatedForTesting()) {
-    EXPECT_NE(main_frame->GetSiteInstance(), child_frame->GetSiteInstance());
-  } else {
+  if (AreDefaultSiteInstancesEnabled()) {
     EXPECT_EQ(main_frame->GetSiteInstance(), child_frame->GetSiteInstance());
+  } else {
+    EXPECT_NE(main_frame->GetSiteInstance(), child_frame->GetSiteInstance());
   }
   EXPECT_EQ(child_frame->GetSiteInstance(),
             grandchild_frame->GetSiteInstance());
@@ -4751,10 +4751,10 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTest, TopToAboutBlank_CrossSite) {
       shell()->web_contents()->GetMainFrame());
   RenderFrameHostImpl* child_frame =
       main_frame->child_at(0)->current_frame_host();
-  if (AreAllSitesIsolatedForTesting()) {
-    EXPECT_NE(main_frame->GetSiteInstance(), child_frame->GetSiteInstance());
-  } else {
+  if (AreDefaultSiteInstancesEnabled()) {
     EXPECT_EQ(main_frame->GetSiteInstance(), child_frame->GetSiteInstance());
+  } else {
+    EXPECT_NE(main_frame->GetSiteInstance(), child_frame->GetSiteInstance());
   }
   url::Origin a_origin =
       url::Origin::Create(embedded_test_server()->GetURL("a.com", "/"));
