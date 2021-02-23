025e5437d8957b220410437f7286444016c8d9a5
Leonid Baraz <lbaraz@chromium.org>
2021-02-23 18:49:31 +0000

    Rid InitializingContext of sequential semantics.
    That semantics is not used anyway, and later on there will be a single
    callback to pass to the client, which will do a series of callbacks depending on the
    caller.
    
    Bug: b:180417849
    Change-Id: Ieb572c273d5b5e00d566a1a1d63127e5719fe557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713897
    Auto-Submit: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856777}

--- chrome/browser/policy/messaging_layer/public/report_queue_unittest.cc
+++ chrome/browser/policy/messaging_layer/public/report_queue_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/optional.h"
-#include "base/synchronization/waitable_event.h"
 #include "base/test/task_environment.h"
 #include "base/values.h"
 #include "chrome/browser/policy/messaging_layer/proto/test.pb.h"
