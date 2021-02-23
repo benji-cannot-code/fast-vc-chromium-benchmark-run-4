783dc5a979b85dfcd928286641fd61d9def36f92
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2021-02-23 22:46:42 +0000

    [ChromeCart] De-flake CommerceHintAgentTest tests
    Bug: 1179241, 1164236
    Change-Id: I361fa1165cedd31b4306f8834f1ad2c4ca6886d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2709665
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856892}

--- chrome/renderer/cart/commerce_hint_agent.h
+++ chrome/renderer/cart/commerce_hint_agent.h
@@ -40,6 +40,7 @@ class CommerceHintAgent
   static std::string ExtractButtonText(const blink::WebFormElement& form);
 
  private:
+  GURL starting_url_;
   base::WeakPtrFactory<CommerceHintAgent> weak_factory_{this};
 
   // content::RenderFrameObserver overrides
@@ -48,6 +49,7 @@ class CommerceHintAgent
   void DidStartNavigation(
       const GURL& url,
       base::Optional<blink::WebNavigationType> navigation_type) override;
+  void DidCommitProvisionalLoad(ui::PageTransition transition) override;
   void DidFinishLoad() override;
   void WillSubmitForm(const blink::WebFormElement& form) override;
   void DidObserveLayoutShift(double score, bool after_input_or_scroll) override;
--- chrome/renderer/cart/commerce_hint_agent_browsertest.cc
+++ chrome/renderer/cart/commerce_hint_agent_browsertest.cc
@@ -169,8 +169,7 @@ class CommerceHintAgentTest : public PlatformBrowserTest {
   bool satisfied_;
 };
 
-// TODO(crbug/1179241): Deflake this test.
-IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, DISABLED_AddToCartByURL) {
+IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, AddToCartByURL) {
   // For add-to-cart by URL, normally a URL in that domain has already been
   // committed.
   NavigateToURL("https://www.walmart.com/");
@@ -199,8 +198,7 @@ IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, VisitCart) {
   WaitForCartCount(kExpectedExample);
 }
 
-// TODO(crbug/1179241): Deflake this test.
-IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, DISABLED_VisitCheckout) {
+IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, VisitCheckout) {
   service_->AddCart(kMockExample, kMockExampleProto);
   WaitForCartCount(kExpectedExample);
 
@@ -209,8 +207,7 @@ IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, DISABLED_VisitCheckout) {
   WaitForCartCount(kEmptyExpected);
 }
 
-// TODO(crbug/1179241): Deflake this test.
-IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, DISABLED_PurchaseByURL) {
+IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, PurchaseByURL) {
   service_->AddCart(kMockAmazon, kMockAmazonProto);
   WaitForCartCount(kExpectedAmazon);
 
