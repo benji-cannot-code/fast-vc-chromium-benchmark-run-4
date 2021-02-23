ad94d5c2a4363679bb7d0630844e8e3baf3c635a
Mei Liang <meiliang@chromium.org>
2021-02-23 00:35:50 +0000

    [Sherrif] Revert "CSP: Only check 'wasm-eval' on supported schemes"
    This reverts commit 52b8e69367f6b28277af4766bcafdfba2efc24c9.
    
    Reason for revert: Test failed on all L bots. https://bugs.chromium.org/p/chromium/issues/detail?id=1181093
    
    Original change's description:
    > CSP: Only check 'wasm-eval' on supported schemes
    >
    > The Content Security Policy keyword 'wasm-eval' is only supported on
    > extensions. In the Blink CSP code, we were parsing the keyword only
    > for supported schemes, but checking it always. This was the cause of
    > confusing error messages, which this CL fixes.
    >
    > Fixed: 1169592
    > Bug: 1169592
    > Change-Id: Ib3b4724f3e2efeeee3c9439a6439091248babd69
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702174
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#855768}
    
    Bug: 1169592
    Change-Id: Iccb9ff19185cf66362f57a67cbf0b5c7ebf1e8c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713943
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856531}

--- third_party/blink/renderer/core/frame/csp/csp_directive_list.h
+++ third_party/blink/renderer/core/frame/csp/csp_directive_list.h
@@ -103,7 +103,7 @@ bool CSPDirectiveListAllowEval(
     const String& content);
 
 CORE_EXPORT
-bool CSPDirectiveListAllowWasmCodeGeneration(
+bool CSPDirectiveListAllowWasmEval(
     const network::mojom::blink::ContentSecurityPolicy& csp,
     ContentSecurityPolicy* policy,
     ReportingDisposition reporting_disposition,
