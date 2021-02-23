8e80d6fd6fd5e99338b99ece0d96f68eb4344adb
Patrick Monette <pmonette@chromium.org>
2021-02-23 22:11:27 +0000

    [Battery] Add VideoCaptureEventProvider
    This class provides video capture events to the
    UsageScenarioDataStore.
    
    Bug: 1153193
    Change-Id: I0c579a39282b2e43bf69c7638c968a345b96fdb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2706893
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856872}

--- chrome/browser/metrics/usage_scenario/usage_scenario_tracker.cc
+++ chrome/browser/metrics/usage_scenario/usage_scenario_tracker.cc
@@ -5,5 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/metrics/usage_scenario/usage_scenario_tracker.h"
 
-UsageScenarioTracker::UsageScenarioTracker() = default;
+UsageScenarioTracker::UsageScenarioTracker()
+    : video_capture_event_provider_(&data_store_) {}
+
 UsageScenarioTracker::~UsageScenarioTracker() = default;
--- chrome/browser/metrics/usage_scenario/usage_scenario_tracker.h
+++ chrome/browser/metrics/usage_scenario/usage_scenario_tracker.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/sequence_checker.h"
 #include "chrome/browser/metrics/usage_scenario/usage_scenario_data_store.h"
+#include "chrome/browser/metrics/usage_scenario/video_capture_event_provider.h"
 
 // Registers as an observer to various components to maintain a
 // UsageScenarioDataStore.
@@ -25,6 +26,9 @@ class UsageScenarioTracker {
   UsageScenarioDataStoreImpl data_store_;
 
   // TODO(crbug.com/1153193): Add the events providers for the data store.
+
+  // Tracks tabs capturing video.
+  VideoCaptureEventProvider video_capture_event_provider_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 };
