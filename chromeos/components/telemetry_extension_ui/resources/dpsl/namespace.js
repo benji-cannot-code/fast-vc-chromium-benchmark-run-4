9e79f91e10f59d8ce86e8219f300afe29bbe01de
Mahmoud Gawad <mgawad@google.com>
2021-02-23 21:19:15 +0000

    [DPSL] Introduce dpsl.telemetry.* APIs
    Introduce dpsl.telemetry.* APIs that are defined in [1].
    
    Introduce dpsl.* data types that define input/output of aforementioned
    APIs.
    
    Add dpsl_types.js file that include the aforementioned data types. The
    reason behind not defining those types in types.js is that types.js
    would need to know about "dpsl." namespace, which is defined in
    interface_definitions.js. This interface also defines "chromeos."
    namespace. This would override the mojo APIs exposed in the "chromeos."
    namespace.
    
    [1] go/dd-cros-dpsl-v2
    
    Bug: b:179135561
    Test: added browser tests
    Change-Id: Iade9e5dbd7c1a64d31bb1d77d088e41b727557e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690541
    Commit-Queue: Mahmoud Gawad <mgawad@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/master@{#856845}

--- chromeos/components/telemetry_extension_ui/resources/dpsl/namespace.js
+++ chromeos/components/telemetry_extension_ui/resources/dpsl/namespace.js
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Namespace for DPSL APIs.
  */
 var dpsl = {};
+dpsl.telemetry = null;
 dpsl.diagnostics = null;
 dpsl.internal = {};
 dpsl.internal.messagePipe =
