ba633b15c74b0c76559456a0127b415784e79c23
David Jean <djean@chromium.org>
2021-02-23 08:42:53 +0000

    [ios] Fixed plugin placeholder test failure
    These were hidden behind previously flaky tests.
    These test are only if only PluginPlaceholderTest are
    run, otherwise the first test that runs sets a different
    message for plugin not supported.
    
    Change-Id: I0a75e030e0d2d3e34594f105bb4b297e7b39ba65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2712542
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856591}

--- ios/web/plugin_placeholder_inttest.mm
+++ ios/web/plugin_placeholder_inttest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ios/testing/embedded_test_server_handlers.h"
+#import "ios/web/js_messaging/java_script_feature_util_impl.h"
 #import "ios/web/public/test/fakes/fake_web_client.h"
 #import "ios/web/public/test/navigation_test_util.h"
 #import "ios/web/public/test/web_test_with_web_state.h"
@@ -36,6 +37,16 @@ class PluginPlaceholderTest : public WebTestWithWebState {
     FakeWebClient* web_client = static_cast<FakeWebClient*>(GetWebClient());
     web_client->SetPluginNotSupportedText(
         base::UTF8ToUTF16(kPluginNotSupportedText));
+  }
+
+  void SetUp() override {
+    WebTestWithWebState::SetUp();
+    web::java_script_features::ResetPluginPlaceholderJavaScriptFeature();
+  }
+
+  void TearDown() override {
+    web::java_script_features::ResetPluginPlaceholderJavaScriptFeature();
+    WebTestWithWebState::TearDown();
   }
 
   // Sets up |server_| with |html| as response content.
