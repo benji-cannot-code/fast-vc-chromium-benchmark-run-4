3269e6f0a9399d8466f0afa386f1966fba104f62
Rob Schonberger <robsc@chromium.org>
2021-02-23 22:19:32 +0000

    [Live captions] Add forgotten include.
    This adds an include for file paths as is needed.
    
    TEST=All work compiles, but not tested end to end.
    
    Bug: 1055150
    Change-Id: I6d18dec58dfc9d7fc388f4885d1c21d449979f85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716543
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Auto-Submit: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#856878}

--- chrome/services/speech/cros_speech_recognition_recognizer_impl.h
+++ chrome/services/speech/cros_speech_recognition_recognizer_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/services/speech/cloud_speech_recognition_client.h"
 #include "chrome/services/speech/speech_recognition_recognizer_impl.h"
