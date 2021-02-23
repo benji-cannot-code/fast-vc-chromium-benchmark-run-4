809e1604b8b70fae26b4fee589cafbb0a377b150
Christopher Cameron <ccameron@chromium.org>
2021-02-23 10:59:33 +0000

    FrameSinkCapture/macOS: Add a CGWindowIDs to viz::FrameSinkId map
    Windows for capture are identified by their CGWindowID, which is a
    global (not restricted to a single app or process) identifier for the
    window.
    
    Chrome-compositor-based capture needs viz::FrameSinkId to identify the
    content to capture.
    
    Add a remote_cocoa::ScopedCGWindowID that can be used to query the
    corresponding viz::FrameSinkId. This class will be expanded to include
    observers to be notified when the object will be destroyed.
    
    Create this object in the reply to the mojo InitializeCompositor mojo
    method. This is done in this location (as opposed to the CreateWindow
    mojo method) for convenience -- we are guaranteed that the compositor
    will have been initialized when the callback returns, so we can
    immediately create the mapping.
    
    Bug: 1179098
    Change-Id: I83355f32aee170db5f6e1260107526235d79c6de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2707696
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856614}

--- ui/views/cocoa/native_widget_mac_ns_window_host.h
+++ ui/views/cocoa/native_widget_mac_ns_window_host.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/remote_cocoa/app_shim/native_widget_ns_window_host_helper.h"
 #include "components/remote_cocoa/app_shim/ns_view_ids.h"
 #include "components/remote_cocoa/browser/application_host.h"
+#include "components/remote_cocoa/browser/scoped_cg_window_id.h"
 #include "components/remote_cocoa/common/native_widget_ns_window.mojom.h"
 #include "components/remote_cocoa/common/native_widget_ns_window_host.mojom.h"
 #include "mojo/public/cpp/bindings/associated_receiver.h"
@@ -457,6 +458,7 @@ class VIEWS_EXPORT NativeWidgetMacNSWindowHost
   gfx::Rect window_bounds_before_fullscreen_;
 
   std::unique_ptr<ui::RecyclableCompositorMac> compositor_;
+  std::unique_ptr<remote_cocoa::ScopedCGWindowID> scoped_cg_window_id_;
 
   // Properties used by Set/GetNativeWindowProperty.
   std::map<std::string, void*> native_window_properties_;
