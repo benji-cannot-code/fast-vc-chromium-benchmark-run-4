80fd61a7392dfe2a0b13f2348bd8781c7985a6e9
David Jean <djean@chromium.org>
2021-02-23 14:09:08 +0000

    [ios] Add app-side monitor for ephemeral views in eg tests
    This CL is part of the effort to reduce test flakiness.
    
    Some views, like snackbars, are ephemeral and can have already
    disappeared when the client side starts waiting on them to do so.
    
    This adds a app-side monitor that can be put up before requesting
    the execution of the action that triggers this ephemeral view,
    and that monitors the appearance of that view. The monitor
    can then be check if the expected view was seen.
    
    Change-Id: Ia54b211d3e0879a4c2ade6738175dbb0c2c34408
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705084
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856660}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -573,6 +573,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // perspective.
 + (NSString*)pasteboardURLSpec;
 
+#pragma mark - Watcher utilities
+
+// Starts monitoring for buttons (based on traits) with the given
+// (accessibility) |labels|. Monitoring will stop once all are found, or if
+// timeout expires. If a previous set is currently being watched for it gets
+// replaced with this set. Note that timeout is best effort and can be a bit
+// longer than specified. This method returns immediately.
++ (void)watchForButtonsWithLabels:(NSArray<NSString*>*)labels
+                          timeout:(NSTimeInterval)timeout;
+
+// Returns YES is the button with given (accessibility) |label| was observed at
+// some point since |watchForButtonsWithLabels:timeout:| was called.
++ (BOOL)watcherDetectedButtonWithLabel:(NSString*)label;
+
+// Clear the watcher list, stopping monitoring.
++ (void)stopWatcher;
+
 @end
 
 #endif  // IOS_CHROME_TEST_EARL_GREY_CHROME_EARL_GREY_APP_INTERFACE_H_
