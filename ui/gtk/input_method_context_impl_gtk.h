1a1ca01161803f015782ce6d081a28649f19d851
Tom Anderson <thomasanderson@chromium.org>
2021-02-23 20:04:26 +0000

    [GTK4] Fix build errors in GtkEventLoopX11 and InputMethodContextImplGtk
    BUG=876586
    R=nickdiego
    
    Change-Id: Iff561d2f2098448828c9ac7e31c5d17999fa0f44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704813
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856813}

--- ui/gtk/input_method_context_impl_gtk.h
+++ ui/gtk/input_method_context_impl_gtk.h
@@ -12,9 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/glib/glib_signal.h"
 #include "ui/base/ime/linux/linux_input_method_context.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gtk/gtk_buildflags.h"
 
-typedef struct _GdkWindow GdkWindow;
 typedef struct _GtkIMContext GtkIMContext;
+
+#if BUILDFLAG(GTK_VERSION) == 3
+using GdkWindow = struct _GdkWindow;
+#else
+using GdkWindow = struct _GdkSurface;
+#endif
 
 namespace gtk {
 
