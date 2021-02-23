f47242843db8771c04d239ce058f5aa0afc4a11b
Vasilii Sukhanov <vasilii@chromium.org>
2021-02-23 12:26:23 +0000

    Reenable PopupShownAutomaticallyAndPasswordErased with additional logs.
    It's unclear from the optimized stack where the process got stuck.
    
    Bug: 1168150
    Change-Id: Ic0d513b0ca45083bb45b1ea8a471f6a0b430d1bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2712949
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856641}

--- chrome/browser/password_manager/password_generation_interactive_uitest.cc
+++ chrome/browser/password_manager/password_generation_interactive_uitest.cc
@@ -69,6 +69,8 @@ class TestPopupObserver : public PasswordGenerationPopupObserver {
   void WaitForStatus(GenerationPopup status) {
     if (status == popup_showing_)
       return;
+    SCOPED_TRACE(::testing::Message()
+                 << "WaitForStatus " << static_cast<int>(status));
     base::RunLoop run_loop;
     run_loop_ = &run_loop;
     run_loop_->Run();
@@ -77,6 +79,7 @@ class TestPopupObserver : public PasswordGenerationPopupObserver {
 
   // Waits until the popup is either shown or hidden.
   void WaitForStatusChange() {
+    SCOPED_TRACE(::testing::Message() << "WaitForStatusChange");
     base::RunLoop run_loop;
     run_loop_ = &run_loop;
     run_loop_->Run();
@@ -242,9 +245,8 @@ IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
       1);
 }
 
-// TODO(crbug.com/1177135) Re-enable test
 IN_PROC_BROWSER_TEST_F(PasswordGenerationInteractiveTest,
-                       DISABLED_PopupShownAutomaticallyAndPasswordErased) {
+                       PopupShownAutomaticallyAndPasswordErased) {
   FocusPasswordField();
   EXPECT_TRUE(GenerationPopupShowing());
   SendKeyToPopup(ui::VKEY_DOWN);
