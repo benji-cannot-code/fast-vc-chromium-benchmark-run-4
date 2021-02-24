6ef1d2bbbe5770d7d352826762a7255051b3d56c
Daniel Cheng <dcheng@chromium.org>
2021-02-24 21:19:14 +0000

    remoting: Change Jingle session reply callback to base::OnceCallback
    The motivation here is to eliminate the anti-pattern of combining
    base::BindRepeating() + base::Passed(), as the resulting callback should
    typically only be run once.
    
    Bug: 1180750
    Change-Id: Idada44b02769e71a51f71d727726cae4ce8fcfe1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716858
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857353}

--- remoting/protocol/jingle_session.h
+++ remoting/protocol/jingle_session.h
@@ -48,8 +48,7 @@ class JingleSession : public Session {
  private:
   friend class JingleSessionManager;
 
-  typedef base::RepeatingCallback<void(JingleMessageReply::ErrorType)>
-      ReplyCallback;
+  using ReplyCallback = base::OnceCallback<void(JingleMessageReply::ErrorType)>;
 
   explicit JingleSession(JingleSessionManager* session_manager);
 
@@ -87,22 +86,22 @@ class JingleSession : public Session {
   // messages.
   void OnIncomingMessage(const std::string& id,
                          std::unique_ptr<JingleMessage> message,
-                         const ReplyCallback& reply_callback);
+                         ReplyCallback reply_callback);
 
   // Called by OnIncomingMessage() to process the incoming Jingle messages
   // in the same order that they are sent.
   void ProcessIncomingMessage(std::unique_ptr<JingleMessage> message,
-                              const ReplyCallback& reply_callback);
+                              ReplyCallback reply_callback);
 
   // Message handlers for incoming messages.
   void OnAccept(std::unique_ptr<JingleMessage> message,
-                const ReplyCallback& reply_callback);
+                ReplyCallback reply_callback);
   void OnSessionInfo(std::unique_ptr<JingleMessage> message,
-                     const ReplyCallback& reply_callback);
+                     ReplyCallback reply_callback);
   void OnTransportInfo(std::unique_ptr<JingleMessage> message,
-                       const ReplyCallback& reply_callback);
+                       ReplyCallback reply_callback);
   void OnTerminate(std::unique_ptr<JingleMessage> message,
-                   const ReplyCallback& reply_callback);
+                   ReplyCallback reply_callback);
 
   // Called from OnAccept() to initialize session config.
   bool InitializeConfigFromDescription(const ContentDescription* description);
@@ -161,7 +160,7 @@ class JingleSession : public Session {
     PendingMessage();
     PendingMessage(PendingMessage&& moved);
     PendingMessage(std::unique_ptr<JingleMessage> message,
-                   const ReplyCallback& reply_callback);
+                   ReplyCallback reply_callback);
     ~PendingMessage();
     PendingMessage& operator=(PendingMessage&& moved);
     std::unique_ptr<JingleMessage> message;
