d8373b41d979cdbcf4d108d0bf792ca52c6c4a58
David Jean <djean@chromium.org>
2021-02-24 15:53:59 +0000

    [ios] disable StorageTest.WriteIntoNewStorageAndUploadWithKeyUpdate for ios
    Causes crashes on ipad devices bots
    
    Bug: 1181652
    Change-Id: If88c9ba563e8c4460179d84f92a76dd261ff0044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716565
    Auto-Submit: David Jean <djean@chromium.org>
    Commit-Queue: Mike Pinkerton <pinkerton@chromium.org>
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857187}

--- components/reporting/storage/storage_unittest.cc
+++ components/reporting/storage/storage_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
+#include "build/build_config.h"
 #include "components/reporting/encryption/decryption.h"
 #include "components/reporting/encryption/encryption.h"
 #include "components/reporting/encryption/test_encryption_module.h"
@@ -745,6 +746,9 @@ TEST_P(StorageTest, WriteIntoNewStorageAndUpload) {
   task_environment_.FastForwardBy(base::TimeDelta::FromSeconds(1));
 }
 
+#if !defined(OS_IOS)
+// This test is failing iOS ipad device bots.
+// http://crbug.com/1181652
 TEST_P(StorageTest, WriteIntoNewStorageAndUploadWithKeyUpdate) {
   // Run the test only when encryption is enabled.
   if (!is_encryption_enabled()) {
@@ -803,6 +807,7 @@ TEST_P(StorageTest, WriteIntoNewStorageAndUploadWithKeyUpdate) {
   // Trigger upload with key update after a long wait.
   EXPECT_OK(storage_->Flush(MANUAL_BATCH));
 }
+#endif
 
 TEST_P(StorageTest, WriteIntoNewStorageReopenWriteMoreAndUpload) {
   CreateTestStorageOrDie(BuildTestStorageOptions());
