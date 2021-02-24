1fc948aff966bc31b14264b22059afa1cade6500
Jeroen Dhollander <jeroendh@google.com>
2021-02-24 22:45:23 +0000

    Move AssistantTimer to libassistant structs target
    |AssistantTimer| will be used in the Libassistant mojom API in a
    follow-up CL, so this CL moves it together with all other structures
    used in this mojom API.
    
    Bug: b/180661729
    Test: chromeos_unittests "Assistant*"
    Change-Id: Id43cb69143e5abbca4f2f03a2330955cb4cf1c20
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2708203
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857400}

---
+++ chromeos/services/libassistant/public/cpp/assistant_timer.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/libassistant/public/cpp/assistant_timer.h"
+
+namespace chromeos {
+namespace assistant {
+
+AssistantTimer::AssistantTimer() = default;
+AssistantTimer::AssistantTimer(const AssistantTimer&) = default;
+AssistantTimer& AssistantTimer::operator=(const AssistantTimer&) = default;
+AssistantTimer::~AssistantTimer() = default;
+
+bool AssistantTimer::IsEqualInLibAssistantTo(
+    const AssistantTimer& other) const {
+  return id == other.id && label == other.label &&
+         fire_time == other.fire_time &&
+         original_duration == other.original_duration && state == other.state;
+}
+
+}  // namespace assistant
+}  // namespace chromeos
