5d8112c347d6e02d8bbab3c07fb060ccb15b313a
Sophie Chang <sophiechang@chromium.org>
2021-02-24 18:36:27 +0000

    Record a local histogram when a model download has started
    This will be used by integration tests as a signal to determine how long
    to wait for the download
    
    Change-Id: Idd270f76b8f8dccdba06bec0e62733cc6b19d045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716729
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857269}

--- chrome/browser/optimization_guide/prediction/prediction_model_download_client.cc
+++ chrome/browser/optimization_guide/prediction/prediction_model_download_client.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/optimization_guide/prediction/prediction_model_download_client.h"
 
 #include "base/bind.h"
+#include "base/metrics/histogram_macros_local.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
@@ -62,6 +63,16 @@ void PredictionModelDownloadClient::OnServiceUnavailable() {
       GetPredictionModelDownloadManager();
   if (download_manager)
     download_manager->OnDownloadServiceUnavailable();
+}
+
+void PredictionModelDownloadClient::OnDownloadStarted(
+    const std::string& guid,
+    const std::vector<GURL>& url_chain,
+    const scoped_refptr<const net::HttpResponseHeaders>& headers) {
+  // Do not remove. This is a hook used by integration tests that test
+  // client-server interaction.
+  LOCAL_HISTOGRAM_BOOLEAN(
+      "OptimizationGuide.PredictionModelDownloadClient.DownloadStarted", true);
 }
 
 void PredictionModelDownloadClient::OnDownloadFailed(
