02da6cb6e49e46cc6d4ee1b90d656dd85b4f3de1
Sammie Quon <sammiequon@chromium.org>
2021-02-24 18:53:47 +0000

    desks: Use the same padding for hit wall animation and touchpad swipe.
    As decided by UX, used 10% of root window width.
    
    Fixed: 1163662
    Test: Manual
    Change-Id: Idd7836d0ead5e756b398a672064a1090d31b450c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2699543
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857282}

--- ash/wm/desks/root_window_desk_switch_animator.cc
+++ ash/wm/desks/root_window_desk_switch_animator.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/screen_util.h"
 #include "ash/wm/desks/desk.h"
+#include "ash/wm/desks/desks_constants.h"
 #include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/desks/desks_util.h"
 #include "base/auto_reset.h"
@@ -123,8 +124,7 @@ std::string GetScreenshotLayerName(int index) {
 // units. Convert these units so that what is considered a full touchpad swipe
 // shifts the animation layer one entire desk length.
 float TouchpadToXTranslation(float touchpad_x, int desk_length) {
-  return desk_length * touchpad_x /
-         RootWindowDeskSwitchAnimator::kTouchpadSwipeLengthForDeskChange;
+  return desk_length * touchpad_x / kTouchpadSwipeLengthForDeskChange;
 }
 
 }  // namespace
