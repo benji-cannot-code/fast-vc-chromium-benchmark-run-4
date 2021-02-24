ae56dc2826e131378c9043762d8125ba0f877480
Moe Ahmadi <mahmadi@chromium.org>
2021-02-24 18:07:24 +0000

    [memories] Memories Mojom definition
    See go/memory-anatomy for details.
    
    Fixed: 1179064
    Change-Id: Ic5a97f86cec66b38a2efc452fa0a537dc25a4fbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2699036
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857248}

--- chrome/browser/resources/memories/browser_proxy.js
+++ chrome/browser/resources/memories/browser_proxy.js
@@ -4,6 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
+import 'chrome://resources/mojo/mojo/public/mojom/base/big_buffer.mojom-lite.js';
+import 'chrome://resources/mojo/mojo/public/mojom/base/string16.mojom-lite.js';
+import 'chrome://resources/mojo/mojo/public/mojom/base/time.mojom-lite.js';
+import 'chrome://resources/mojo/mojo/public/mojom/base/token.mojom-lite.js';
+import 'chrome://resources/mojo/mojo/public/mojom/base/unguessable_token.mojom-lite.js';
+import 'chrome://resources/mojo/url/mojom/url.mojom-lite.js';
 
 import './memories_definition.mojom-lite.js';
 import './memories_api.mojom-lite.js';
