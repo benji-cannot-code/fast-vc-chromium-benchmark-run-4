c4729bba808189f0227e1a3ddf60d615045a5dc3
Anupam Snigdha <snianu@microsoft.com>
2021-02-24 07:44:24 +0000

    Remove kZeroWidthNonJoinerCharacter as grapheme break.
    The fix for crbug.com/1027695 added zwnj as grapheme cluster break
    in order to let |InsertText| command position the |VisiblePosition|
    canonicalization in between zwnj and another character which should
    be part of one grapheme cluster as mentioned in UAX#29. This is no
    longer needed because the fix for crrev.com/c/2619002 handles these
    scenarios without VisiblePosition canonicalization.
    This patch also makes grapheme cluster algorithm align with UAX#29
    as mentioned in https://unicode.org/reports/tr29/#GB9.
    
    Bug: 1027695
    Change-Id: Ibd103b86d61bb2007404e93e881c9cf5269af128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713906
    Auto-Submit: Anupam Snigdha <snianu@microsoft.com>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#857056}

--- third_party/blink/renderer/core/editing/state_machines/state_machine_util_test.cc
+++ third_party/blink/renderer/core/editing/state_machines/state_machine_util_test.cc
@@ -169,10 +169,4 @@ TEST(StateMachineUtilTest, IsGraphmeBreak_IndicSyllabicCategoryVirama) {
   EXPECT_TRUE(IsGraphemeBreak(kTamilVirama, kDevangariKa));
 }
 
-TEST(StateMachineUtilTest, IsGraphmeBreak_ZWNJSequecne) {
-  // U+200C (kZeroWidthNonJoinerCharacter)
-  EXPECT_TRUE(IsGraphemeBreak('a', WTF::unicode::kZeroWidthNonJoinerCharacter));
-  EXPECT_TRUE(IsGraphemeBreak(WTF::unicode::kZeroWidthNonJoinerCharacter, 'a'));
-}
-
 }  // namespace blink
