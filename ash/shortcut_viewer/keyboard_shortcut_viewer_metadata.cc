6f872c684af5230469802219566565579f396ec3
Kenton Lam <kentonlam@google.com>
2021-02-24 03:11:17 +0000

    Implement Search+Shift+Space shortcut for emoji picker.
    Shortcut is enabled regardless of emoji-picker flag. That should be fine
    because there always is an emoji picker, it will just be the old one.
    
    shortcut_viewer_strings_grd screenshot: https://storage.cloud.google.com/chromium-translation-screenshots/25b92cfbad31b120e5ea3231a73b29f18ae8802b
    
    Bug: 1152237
    Change-Id: I17ee2c2c18068582a5017ec581bebaaf0429d7e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2651926
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856990}

--- ash/shortcut_viewer/keyboard_shortcut_viewer_metadata.cc
+++ ash/shortcut_viewer/keyboard_shortcut_viewer_metadata.cc
@@ -1448,6 +1448,13 @@ const std::vector<KeyboardShortcutItem>& GetKeyboardShortcutItemList() {
        {},
        // |accelerator_ids|
        {{ui::VKEY_PRIVACY_SCREEN_TOGGLE, ui::EF_NONE}}},
+
+      {// |categories|
+       {ShortcutCategory::kTextEditing},
+       IDS_KSV_DESCRIPTION_SHOW_EMOJI_PICKER,
+       {},
+       // |accelerator_ids|
+       {{ui::VKEY_SPACE, ui::EF_SHIFT_DOWN | ui::EF_COMMAND_DOWN}}},
   });
 
   static bool is_initialized = false;
