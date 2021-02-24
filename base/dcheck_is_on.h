53d60dbdc8de5ee30ad41d411c5220be88089ba5
Benoit Lize <lizeb@chromium.org>
2021-02-24 15:23:40 +0000

    [base] Introduce EXPENSIVE_DCHECK_IS_ON(), and use it in PartitionAlloc.
    PartitionAlloc has some very expensive DCHECK()s, such as memset()-ing
    memory at malloc() and free() time. Given that the allocator is very
    performance-sensitive, this can create unacceptable performance in some
    cases, such as canary builds with DCHECK()s enabled.
    
    To mitigate that and still keep good coverage, introduce a buildflag to
    optionally disable some DCHECK()s, and use it for the memset() calls in
    PartitionAlloc.
    
    Bug: 998048
    Change-Id: I5f20429da060b0f91421446c00f18898d4481892
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2698250
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857172}

--- base/dcheck_is_on.h
+++ base/dcheck_is_on.h
@@ -6,10 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_DCHECK_IS_ON_H_
 #define BASE_DCHECK_IS_ON_H_
 
+#include "base/debug/debugging_buildflags.h"
+
 #if defined(NDEBUG) && !defined(DCHECK_ALWAYS_ON)
 #define DCHECK_IS_ON() false
 #else
 #define DCHECK_IS_ON() true
+#endif
+
+#if BUILDFLAG(EXPENSIVE_DCHECKS_ARE_ON)
+#define EXPENSIVE_DCHECKS_ARE_ON() true
+#else
+#define EXPENSIVE_DCHECKS_ARE_ON() false
 #endif
 
 #endif  // BASE_DCHECK_IS_ON_H_
