01fb8d7ad307120995d01fb26ba3b731c2d0130b
Elad Alon <eladalon@chromium.org>
2021-02-24 17:05:29 +0000

    Put getCurrentBrowsingContextMedia behind an origin trial
    Previously, this was behind a runtime flag. Instead, we want it
    behind an origin trial.
    
    Intent-to-experiment: https://groups.google.com/a/chromium.org/g/blink-dev/c/j70Xe2xSh1s/m/XeQ5UIr8AgAJ
    
    Bug: 1136942
    Change-Id: Ic42fd6444dd8db42a4bd8fdcae3f1ac1367f875e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2712823
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857222}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -249,8 +249,6 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
     {wf::EnableForcedColors, features::kForcedColors},
     {wf::EnableFractionalScrollOffsets, features::kFractionalScrollOffsets},
     {wf::EnableGenericSensorExtraClasses, features::kGenericSensorExtraClasses},
-    {wf::EnableGetCurrentBrowsingContextMedia,
-     blink::features::kRTCGetCurrentBrowsingContextMedia},
 #if defined(OS_ANDROID)
     {wf::EnableGetDisplayMedia, features::kUserMediaScreenCapturing},
 #endif
