2a0782b26d62d76f88ca65ec522a9c50e8645fcb
dpapad <dpapad@chromium.org>
2021-02-24 21:09:11 +0000

    WebUI: Remove cr_toolbar Polymer2 version, check-in Polymer3.
    Bug: 1173575
    Change-Id: I59d994b33931a2199b93f74aeec019ebcb9a54ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717457
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857347}

--- chrome/browser/resources/chromeos/emulator/device_emulator_pages.js
+++ chrome/browser/resources/chromeos/emulator/device_emulator_pages.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_drawer/cr_drawer.m.js';
-import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.m.js';
+import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
 import 'chrome://resources/polymer/v3_0/iron-flex-layout/iron-flex-layout-classes.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import 'chrome://resources/polymer/v3_0/iron-selector/iron-selector.js';
--- chrome/browser/resources/history/history_toolbar.js
+++ chrome/browser/resources/history/history_toolbar.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_lazy_render/cr_lazy_render.m.js';
-import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.m.js';
+import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
 import './shared_style.js';
 import './strings.js';
 
