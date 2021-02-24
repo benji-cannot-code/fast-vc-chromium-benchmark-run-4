3a3c678e7e4ba001753fec1708595dadeab21d1a
Colin Blundell <blundell@chromium.org>
2021-02-24 14:12:00 +0000

    [WebLayer] Instantiate AdsResourceTracker in the renderer
    This renderer-side object is necessary for browser-side page load
    metrics observation to get information about ads resources, which is in
    turn relied on by Chrome's ads page loads metrics observation and
    interventions. This CL paves the way for the near-term porting of those
    features to WebLayer by instantiating AdsResourceTracker and verifying
    via a browsertest that information about ads resources makes it to
    WebLayer's browser-side page load metrics observation. The browsertest
    is adapted from that of //chrome, stripped down to just what is
    necessary for this initial functionality; we'll expand it as we bring
    more of //chrome's functionality to //weblayer.
    
    Bug: 1116095
    Change-Id: Ibd84202f4fdcbbf333f072ac42d66b190cafea3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2712902
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857150}

--- weblayer/renderer/content_renderer_client_impl.cc
+++ weblayer/renderer/content_renderer_client_impl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/no_state_prefetch/renderer/prerender_render_frame_observer.h"
 #include "components/no_state_prefetch/renderer/prerender_utils.h"
 #include "components/page_load_metrics/renderer/metrics_render_frame_observer.h"
+#include "components/subresource_filter/content/renderer/ad_resource_tracker.h"
 #include "components/subresource_filter/content/renderer/subresource_filter_agent.h"
 #include "components/subresource_filter/content/renderer/unverified_ruleset_dealer.h"
 #include "components/subresource_filter/core/common/common_features.h"
@@ -121,13 +122,17 @@ void ContentRendererClientImpl::RenderFrameCreated(
   if (weblayer_observer_)
     agent->SetContentSettingRules(weblayer_observer_->content_setting_rules());
 
-  new page_load_metrics::MetricsRenderFrameObserver(render_frame);
+  auto* metrics_render_frame_observer =
+      new page_load_metrics::MetricsRenderFrameObserver(render_frame);
 
-  // TODO(crbug.com/1116095): Bring up AdResourceTracker?
+  auto ad_resource_tracker =
+      std::make_unique<subresource_filter::AdResourceTracker>();
+  metrics_render_frame_observer->SetAdResourceTracker(
+      ad_resource_tracker.get());
   auto* subresource_filter_agent =
       new subresource_filter::SubresourceFilterAgent(
           render_frame, subresource_filter_ruleset_dealer_.get(),
-          /*ad_resource_tracker=*/nullptr);
+          std::move(ad_resource_tracker));
   subresource_filter_agent->Initialize();
 
 #if defined(OS_ANDROID)
