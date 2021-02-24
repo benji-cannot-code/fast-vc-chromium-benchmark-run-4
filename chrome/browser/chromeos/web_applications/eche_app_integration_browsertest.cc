8a43bffd5fef0c7ad411c3f0f86cda805392c19c
paulzhchen <paulzchen@google.com>
2021-02-24 07:43:20 +0000

    [Eche SWA] Change Display mode to Minimal UI.
    Minimal UI provides the end-user with some means to access a
    minimal set of UI elements for controlling navigation, reload and back.
    
    Bug: b/180860155
    Test: Pass EcheAppIntegrationTest.MinimalUi
    Change-Id: I0946ba05d6bcdd5c10a89949a4aeb3c0bed5f065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717585
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    Commit-Queue: Paulz Chen <paulzchen@google.com>
    Cr-Commit-Position: refs/heads/master@{#857054}

--- chrome/browser/chromeos/web_applications/eche_app_integration_browsertest.cc
+++ chrome/browser/chromeos/web_applications/eche_app_integration_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
 #include "chrome/browser/web_applications/system_web_app_manager.h"
 #include "chromeos/components/eche_app_ui/url_constants.h"
 #include "content/public/test/browser_test.h"
@@ -65,6 +66,17 @@ IN_PROC_BROWSER_TEST_P(EcheAppIntegrationTest,
       BrowserView::GetBrowserViewForBrowser(browser);
   EXPECT_FALSE(browser_view->CanResize());
   EXPECT_FALSE(browser_view->CanMaximize());
+}
+
+IN_PROC_BROWSER_TEST_P(EcheAppIntegrationTest, MinimalUi) {
+  WaitForTestSystemAppInstall();
+  Browser* browser;
+  LaunchApp(web_app::SystemAppType::ECHE, &browser);
+  BrowserView* const browser_view =
+      BrowserView::GetBrowserViewForBrowser(browser);
+  ToolbarButtonProvider* provider = browser_view->toolbar_button_provider();
+  EXPECT_TRUE(provider->GetBackButton());
+  EXPECT_TRUE(provider->GetReloadButton());
 }
 
 INSTANTIATE_SYSTEM_WEB_APP_MANAGER_TEST_SUITE_REGULAR_PROFILE_P(
