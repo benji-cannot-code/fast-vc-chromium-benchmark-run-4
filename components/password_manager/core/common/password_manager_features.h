e47ad933ab9670f84437ef0acd30a412e38a9da0
Maria Kazinova <kazinova@google.com>
2021-02-24 11:10:11 +0000

    [iOS,Passwords] Added a feature & a flag for manual password generation.
    Bug: 1181254
    Change-Id: I2e41de44c9ea72e17d1dbf34ed3eef5cf98422f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2715188
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857100}

--- components/password_manager/core/common/password_manager_features.h
+++ components/password_manager/core/common/password_manager_features.h
@@ -22,6 +22,7 @@ extern const base::Feature kBiometricTouchToFill;
 extern const base::Feature kChangePasswordAffiliationInfo;
 extern const base::Feature kEditPasswordsInSettings;
 extern const base::Feature kDetectFormSubmissionOnFormClear;
+extern const base::Feature kEnableManualPasswordGeneration;
 extern const base::Feature kEnableMovingMultiplePasswordsToAccount;
 extern const base::Feature kEnableOverwritingPlaceholderUsernames;
 extern const base::Feature kEnablePasswordsAccountStorage;
