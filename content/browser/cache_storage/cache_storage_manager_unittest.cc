245409ab9abfcdd84ca8263e915b887d8d21cfb9
Ben Kelly <wanderview@chromium.org>
2021-02-24 21:57:50 +0000

    CacheStorage: De-flake and re-enable '*WithOldIndex' unit tests.
    This removes an unnecessary expectation from the test that tried to
    assert the index was "fresh" at a certain point in the test.
    Unfortunately due to the way disk_cache writes files asynchronously
    under the hood we can't really assert this.  Later on the test forces
    timestamps to make sure the desired condition is tested properly.
    
    Bug: 1170905,760687
    Change-Id: I724df9db32023c260d39692c7d54dcfec385cb6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717754
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Auto-Submit: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857381}

--- content/browser/cache_storage/cache_storage_manager_unittest.cc
+++ content/browser/cache_storage/cache_storage_manager_unittest.cc
@@ -1707,9 +1707,7 @@ TEST_P(CacheStorageManagerTestP, GetAllOriginsUsageDifferentOwners) {
   }
 }
 
-// TODO(crbug.com/760687): Flaky on Fuchsia.
-// TODO(crbug.com/1170905): Failing on multiple platforms.
-TEST_F(CacheStorageManagerTest, DISABLED_GetAllOriginsUsageWithOldIndex) {
+TEST_F(CacheStorageManagerTest, GetAllOriginsUsageWithOldIndex) {
   // Write a single value (V1) to the cache.
   const GURL kFooURL = origin1_.GetURL().Resolve("foo");
   const std::string kCacheName = "foo";
@@ -1728,7 +1726,6 @@ TEST_F(CacheStorageManagerTest, DISABLED_GetAllOriginsUsageWithOldIndex) {
   DestroyStorageManager();
 
   // Save a copy of the V1 index.
-  EXPECT_TRUE(IsIndexFileCurrent(storage_dir));
   base::FilePath index_path = storage_dir.AppendASCII("index.txt");
   EXPECT_TRUE(base::PathExists(index_path));
   base::FilePath backup_index_path = storage_dir.AppendASCII("index.txt.bak");
@@ -1788,9 +1785,7 @@ TEST_F(CacheStorageManagerTest, DISABLED_GetAllOriginsUsageWithOldIndex) {
   EXPECT_FALSE(usage[0]->last_modified.is_null());
 }
 
-// TODO(crbug.com/760687): Flaky on Fuchsia.
-// TODO(crbug.com/1170905): Failing on multiple platforms.
-TEST_F(CacheStorageManagerTest, DISABLED_GetOriginSizeWithOldIndex) {
+TEST_F(CacheStorageManagerTest, GetOriginSizeWithOldIndex) {
   // Write a single value (V1) to the cache.
   const GURL kFooURL = origin1_.GetURL().Resolve("foo");
   const std::string kCacheName = "foo";
@@ -1809,7 +1804,6 @@ TEST_F(CacheStorageManagerTest, DISABLED_GetOriginSizeWithOldIndex) {
   DestroyStorageManager();
 
   // Save a copy of the V1 index.
-  EXPECT_TRUE(IsIndexFileCurrent(storage_dir));
   base::FilePath index_path = storage_dir.AppendASCII("index.txt");
   EXPECT_TRUE(base::PathExists(index_path));
   base::FilePath backup_index_path = storage_dir.AppendASCII("index.txt.bak");
