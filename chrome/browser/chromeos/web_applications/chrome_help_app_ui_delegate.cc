9de7aef4af2b338cbf46962f8ab1658c336ca098
Zufeng Wang <zufeng@google.com>
2021-02-24 07:45:32 +0000

    Add device-help-content-id to help app loadTimeData
    As per go/cros-device-help-content-id, the value comes from the chrome
    flag --device-help-content-id.
    
    Tested manually in Ozone.
    
    Bug: b/180348654
    Change-Id: Ib25886e89b09e40ba44f7ec6e53234ed7da7ff99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2712314
    Reviewed-by: dstockwell <dstockwell@google.com>
    Commit-Queue: Zufeng Wang <zufeng@google.com>
    Cr-Commit-Position: refs/heads/master@{#857058}

--- chrome/browser/chromeos/web_applications/chrome_help_app_ui_delegate.cc
+++ chrome/browser/chromeos/web_applications/chrome_help_app_ui_delegate.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "ash/public/cpp/assistant/assistant_state.h"
 #include "ash/public/cpp/tablet_mode.h"
+#include "base/command_line.h"
 #include "base/system/sys_info.h"
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
 #include "chrome/browser/ash/assistant/assistant_util.h"
@@ -79,6 +80,9 @@ void ChromeHelpAppUIDelegate::PopulateLoadTimeData(
   source->AddString("customizationId", customization_id);
   source->AddString("deviceName", ui::GetChromeOSDeviceName());
   source->AddString("hwid", hwid);
+  source->AddString("deviceHelpContentId",
+                    base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+                        "device-help-content-id"));
 
   // Add any features that have been enabled.
   source->AddBoolean("HelpAppReleaseNotes", true);
