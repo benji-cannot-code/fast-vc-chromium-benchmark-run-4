e716c6f035a0429afb49d5094c6d86624cc96670
Richard Chui <richui@chromium.org>
2021-02-24 02:54:45 +0000

    Capture Mode: use Env event handler
    When ephemeral UI is present, capture mode was not handling events (such
    as ENTER and ESC). This is based on the priority and order the event
    handlers are added to the list. This CL bumps the priority up and uses
    the Env event handler instead of the Shell one in order to move its
    position higher up in the targets list.
    
    Bug: 1166391
    Test: manual
    Change-Id: I3db050ff374ddb0883e88b197af81dcb4bd232ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2715649
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856962}

--- ash/capture_mode/capture_mode_session.cc
+++ ash/capture_mode/capture_mode_session.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "cc/paint/paint_flags.h"
 #include "ui/aura/client/capture_client.h"
+#include "ui/aura/env.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tracker.h"
 #include "ui/base/cursor/cursor_factory.h"
@@ -552,7 +553,8 @@ CaptureModeSession::CaptureModeSession(CaptureModeController* controller)
   display::Screen::GetScreen()->AddObserver(this);
   // Our event handling code assumes the capture bar widget has been initialized
   // already. So we start handling events after everything has been setup.
-  Shell::Get()->AddPreTargetHandler(this);
+  aura::Env::GetInstance()->AddPreTargetHandler(
+      this, ui::EventTarget::Priority::kSystem);
 
   capture_mode_util::TriggerAccessibilityAlert(l10n_util::GetStringFUTF8(
       IDS_ASH_SCREEN_CAPTURE_ALERT_OPEN,
@@ -565,7 +567,7 @@ CaptureModeSession::CaptureModeSession(CaptureModeController* controller)
 }
 
 CaptureModeSession::~CaptureModeSession() {
-  Shell::Get()->RemovePreTargetHandler(this);
+  aura::Env::GetInstance()->RemovePreTargetHandler(this);
   display::Screen::GetScreen()->RemoveObserver(this);
   current_root_->RemoveObserver(this);
   TabletModeController::Get()->RemoveObserver(this);
