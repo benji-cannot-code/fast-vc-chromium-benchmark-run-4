0523d8e17208434d5351a00359854f870c9b174b
Mirko Bonadei <mbonadei@chromium.org>
2021-02-24 11:44:47 +0000

    Roll abseil_revision b315753c0b..998805a4c7
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/b315753c0b..998805a4c7
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/b315753c0b..998805a4c7
    
    No BUILD.gn and .def changes.
    
    Bug: None
    Change-Id: Iddd58731e182f2440f7b039bdcf69f84e293fe43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717307
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857110}

--- third_party/abseil-cpp/absl/base/attributes.h
+++ third_party/abseil-cpp/absl/base/attributes.h
@@ -679,4 +679,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ABSL_CONST_INIT
 #endif  // ABSL_HAVE_CPP_ATTRIBUTE(clang::require_constant_initialization)
 
+// ABSL_ATTRIBUTE_PURE_FUNCTION
+//
+// ABSL_ATTRIBUTE_PURE_FUNCTION is used to annotate declarations of "pure"
+// functions. A function is pure if its return value is only a function of its
+// arguments. The pure attribute prohibits a function from modifying the state
+// of the program that is observable by means other than inspecting the
+// function's return value. Declaring such functions with the pure attribute
+// allows the compiler to avoid emitting some calls in repeated invocations of
+// the function with the same argument values.
+//
+// Example:
+//
+//  ABSL_ATTRIBUTE_PURE_FUNCTION int64_t ToInt64Milliseconds(Duration d);
+#if ABSL_HAVE_CPP_ATTRIBUTE(gnu::pure)
+#define ABSL_ATTRIBUTE_PURE_FUNCTION [[gnu::pure]]
+#elif ABSL_HAVE_ATTRIBUTE(pure)
+#define ABSL_ATTRIBUTE_PURE_FUNCTION __attribute__((pure))
+#else
+#define ABSL_ATTRIBUTE_PURE_FUNCTION
+#endif
+
 #endif  // ABSL_BASE_ATTRIBUTES_H_
