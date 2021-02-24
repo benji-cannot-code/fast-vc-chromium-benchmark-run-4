d79e1d4dab783717d4eb61cbeb4987d2aafc10e6
David Dorwin <ddorwin@chromium.org>
2021-02-24 02:58:10 +0000

    [fuchsia] Enable Partial Site Isolation
    Adds preloaded origins to be isolated when Site Isolation is enabled
    but Strict Site Isolation is not - as determined by
    UseDedicatedProcessesForAllSites().
    
          whether "site-per-process" is disabled in the WebEngine config.
          default configuration and one renderer with "site-per-process"
          disabled. (This is the same as before this change.)
    
    Fixed: 1179087
    Test: https://mail.google.com results in two renderers regardless of
    Test: A site with many ad iframes results in multiple renderers in the
    Change-Id: Ic17b4ed992135bb850d6ee8fa3fbb584e05856b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713940
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#856968}

--- fuchsia/engine/browser/web_engine_browser_context.cc
+++ fuchsia/engine/browser/web_engine_browser_context.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/system/sys_info.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/keyed_service/core/simple_key_map.h"
+#include "components/site_isolation/site_isolation_policy.h"
 #include "components/strings/grit/components_locale_settings.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -93,6 +94,10 @@ WebEngineBrowserContext::WebEngineBrowserContext(bool force_incognito)
   simple_factory_key_ =
       std::make_unique<SimpleFactoryKey>(GetPath(), IsOffTheRecord());
   SimpleKeyMap::GetInstance()->Associate(this, simple_factory_key_.get());
+
+  // TODO(crbug.com/1181156): Should apply any persisted isolated origins here.
+  // However, since WebEngine does not persist any, that would currently be a
+  // no-op.
 }
 
 WebEngineBrowserContext::~WebEngineBrowserContext() {
