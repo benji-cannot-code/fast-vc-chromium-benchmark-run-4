78aa7a7a2edd1f25f6aab9734ec7e7e5f5141228
Antonio Gomes <tonikitoo@igalia.com>
2021-02-24 18:30:49 +0000

    [lacros] Implement SnapControllerLacros
    This CL implements "can snap", "show snap preview" and "commit snap"
    actions, on  SnapControllerLacros.
    The original implementation residing at [1] was stubbed out as part of
    the WM integration bring up (crbug.com/1113900).
    
    The implementation makes use of the ui::WaylandExtension path [2]
    in order to communicate with Ozone/Wayland.
    
    On Exo-side, the CL extends AuraShell with two new requests,
    "show snap preview" and "commit snap".
    
    [1] //chrome/browser/lacros/snap_controller_lacros.cc|h
    [2] //ui/platform_window/extensions/wayland_extension.cc|h
    [3] //components/exo/wayland/protocol/aura-shell.xml
    
    TEST = On Lacros window, long press on the maximize button
           (using mouse or touch).
    EXPECTED = A snap UI should appear (left arrow and right arrow)
               and allow the window to snap left/right
    
    BUG=1141701, 1143926, 1173834
    
    Change-Id: I1f49d1f24aa157a691e4856677eb849e174585b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2701852
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#857266}

--- components/exo/sub_surface.h
+++ components/exo/sub_surface.h
@@ -63,6 +63,12 @@ class SubSurface : public SurfaceDelegate, public SurfaceObserver {
   void OnActivationRequested() override {}
   void OnNewOutputAdded() override {}
   void OnSetServerStartResize() override {}
+  void ShowSnapPreviewToLeft() override {}
+  void ShowSnapPreviewToRight() override {}
+  void HideSnapPreview() override {}
+  void SetSnappedToLeft() override {}
+  void SetSnappedToRight() override {}
+  void UnsetSnap() override {}
 
   // Overridden from SurfaceObserver:
   void OnSurfaceDestroying(Surface* surface) override;
