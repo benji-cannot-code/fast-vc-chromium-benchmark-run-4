dd9f18b79daf1597266176897c310349bdd909bc
Elizabeth Popova <lizapopova@google.com>
2021-02-24 16:19:37 +0000

    [Autofill] Flatten address_profiles folder
    Moved all files one level up to have all autofill related files under
    the same directory.
    
    Change-Id: I37d11b1bc1b93652990a667f9115dd2b231a78d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717825
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Cr-Commit-Position: refs/heads/master@{#857202}

--- components/autofill/core/browser/form_data_importer.cc
+++ components/autofill/core/browser/form_data_importer.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
-#include "components/autofill/core/browser/address_profiles/address_profile_save_manager.h"
+#include "components/autofill/core/browser/address_profile_save_manager.h"
 #include "components/autofill/core/browser/autofill_client.h"
 #include "components/autofill/core/browser/autofill_metrics.h"
 #include "components/autofill/core/browser/autofill_type.h"
