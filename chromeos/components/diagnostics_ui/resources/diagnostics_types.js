d07c83a540e564c9504a4f4e279c5b82a293c362
Joon Ahn <joonbug@chromium.org>
2021-02-24 20:05:38 +0000

    diagnostics: remove obsolete TODOs
    Bug: n/a
    Change-Id: Ib56edfe229a831eec3f07549e1c670ad99b868cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717536
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Auto-Submit: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857319}

--- chromeos/components/diagnostics_ui/resources/diagnostics_types.js
+++ chromeos/components/diagnostics_ui/resources/diagnostics_types.js
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @fileoverview
  * Type aliases for the mojo API.
- *
- * TODO(zentaro): When the fake API is replaced by mojo these can be
- * re-aliased to the corresponding mojo types, or replaced by them.
  */
 
 /**
