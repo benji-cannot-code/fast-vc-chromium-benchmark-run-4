74116e806bdcfa1a1a97f37ffd7ce4d4e1b92192
Jimmy Gong <jimmyxgong@google.com>
2021-02-25 03:04:42 +0000

    Pciguard: Add Typecd D-bus skeleton client
    Adds a skeleton implementation of the Typecd D-bus client.
    Once the actual D-bus service is implemented, will update the
    client.
    
    Bug: 1175987
    Change-Id: Iea589da69ac4958fd45df49abe3543f84626b6ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2698494
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857512}

---
+++ chromeos/dbus/typecd/typecd_client.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_DBUS_TYPECD_TYPECD_CLIENT_H_
+#define CHROMEOS_DBUS_TYPECD_TYPECD_CLIENT_H_
+
+#include "base/component_export.h"
+
+namespace dbus {
+class Bus;
+}
+
+namespace chromeos {
+
+// TypecdClient is responsible receiving D-bus signals from the TypeCDaemon
+// service. The TypeCDaemon is the underlying service that informs us whenever
+// a new Thunderbolt/USB4 peripheral has been plugged in.
+class COMPONENT_EXPORT(TYPECD) TypecdClient {
+ public:
+  // Creates and initializes a global instance. |bus| must not be null.
+  static void Initialize(dbus::Bus* bus);
+
+  // Creates and initializes a fake global instance if not already created.
+  static void InitializeFake();
+
+  // Destroys the global instance.
+  static void Shutdown();
+
+  // Returns the global instance which may be null if not initialized.
+  static TypecdClient* Get();
+
+ protected:
+  // Initialize/Shutdown should be used instead.
+  TypecdClient();
+
+  TypecdClient(const TypecdClient&) = delete;
+  TypecdClient& operator=(const TypecdClient&) = delete;
+  virtual ~TypecdClient();
+};
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_DBUS_TYPECD_TYPECD_CLIENT_H_
