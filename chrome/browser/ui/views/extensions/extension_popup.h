43778bf1df853a98352ab2d2ff28e7eba2e69bf4
Vasiliy Telezhnikov <vasilyt@chromium.org>
2021-02-25 19:29:21 +0000

    Remove EXTENSION_HOST_VIEW_SHOULD_CLOSE from ExtensionPopup
    This CL replaces NOTIFICATION_EXTENSION_HOST_VIEW_SHOULD_CLOSE usage in
    ExtensionPopup with ExtensionHostObserver.
    
    Bug: 1174742
    Change-Id: Ia385517ed2fc83afbe1f9fadefe27d151c92d151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703094
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857703}

--- chrome/browser/ui/views/extensions/extension_popup.h
+++ chrome/browser/ui/views/extensions/extension_popup.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/devtools_agent_host_observer.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "extensions/browser/extension_host.h"
+#include "extensions/browser/extension_host_observer.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_registry_observer.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
@@ -48,7 +50,8 @@ class ExtensionPopup : public views::BubbleDialogDelegateView,
                        public extensions::ExtensionRegistryObserver,
                        public content::NotificationObserver,
                        public TabStripModelObserver,
-                       public content::DevToolsAgentHostObserver {
+                       public content::DevToolsAgentHostObserver,
+                       public extensions::ExtensionHostObserver {
  public:
   METADATA_HEADER(ExtensionPopup);
 
@@ -122,6 +125,9 @@ class ExtensionPopup : public views::BubbleDialogDelegateView,
   void DevToolsAgentHostDetached(
       content::DevToolsAgentHost* agent_host) override;
 
+  // extensions::ExtensionHostObserver:
+  void OnExtensionHostShouldClose(extensions::ExtensionHost* host) override;
+
  private:
   ExtensionPopup(std::unique_ptr<extensions::ExtensionViewHost> host,
                  views::View* anchor_view,
@@ -138,6 +144,10 @@ class ExtensionPopup : public views::BubbleDialogDelegateView,
   std::unique_ptr<extensions::ExtensionViewHost> host_;
 
   ExtensionViewViews* extension_view_;
+
+  base::ScopedObservation<extensions::ExtensionHost,
+                          extensions::ExtensionHostObserver>
+      extension_host_observation_{this};
 
   base::ScopedObservation<extensions::ExtensionRegistry,
                           extensions::ExtensionRegistryObserver>
