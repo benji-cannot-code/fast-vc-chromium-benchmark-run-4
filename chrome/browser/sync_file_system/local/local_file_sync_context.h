9e928ad00c0cddcd14b2b96dcf3caaf80caf4e28
Maggie Cai <mxcai@chromium.org>
2021-02-25 12:50:12 +0000

    [Code health] Convert Closure to OnceClosure for url_syncable_callback and done_callback.
    This CL converts url_syncable_callback and done_callback from
    base::Closure to base::OnceClosure in local_file_sync_context.
    
    BUG=1152272
    
    Change-Id: If3d214c56e458d2574db80eda7b8aba6eb5908a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2718206
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857632}

--- chrome/browser/sync_file_system/local/local_file_sync_context.h
+++ chrome/browser/sync_file_system/local/local_file_sync_context.h
@@ -105,7 +105,7 @@ class LocalFileSyncContext
   // This method must be called on UI thread.
   void ClearChangesForURL(storage::FileSystemContext* file_system_context,
                           const storage::FileSystemURL& url,
-                          const base::Closure& done_callback);
+                          base::OnceClosure done_callback);
 
   // Finalizes SnapshotSync, which must have been started by
   // PrepareForSync with SYNC_SNAPSHOT.
@@ -123,7 +123,7 @@ class LocalFileSyncContext
   void FinalizeExclusiveSync(storage::FileSystemContext* file_system_context,
                              const storage::FileSystemURL& url,
                              bool clear_local_changes,
-                             const base::Closure& done_callback);
+                             base::OnceClosure done_callback);
 
   // Prepares for sync |url| by disabling writes on |url|.
   // If the target |url| is being written and cannot start sync it
@@ -155,7 +155,7 @@ class LocalFileSyncContext
   //
   // This method must be called on UI thread.
   void RegisterURLForWaitingSync(const storage::FileSystemURL& url,
-                                 const base::Closure& on_syncable_callback);
+                                 base::OnceClosure on_syncable_callback);
 
   // Applies a remote change.
   // This method must be called on UI thread.
@@ -354,7 +354,7 @@ class LocalFileSyncContext
   // A URL and associated callback waiting for sync is enabled.
   // Accessed only on IO thread.
   storage::FileSystemURL url_waiting_sync_on_io_;
-  base::Closure url_syncable_callback_;
+  base::OnceClosure url_syncable_callback_;
 
   // Used only on IO thread for available changes notifications.
   base::Time last_notified_changes_;
