39795488ed28a38e3d97b9572ccdfc835eb7a084
Torne (Richard Coles) <torne@google.com>
2021-02-25 21:57:07 +0000

    Disable OutOfMemoryDeathTest on x86 Android.
    Various tests in OutOfMemoryDeathTest are failing on x86 Android bots;
    disable until it's resolved.
    
    Bug: 1181112
    Change-Id: I9cfde31c5a4ebea04e6ad74baae1e6338aad9c71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720422
    Auto-Submit: Richard Coles <torne@chromium.org>
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857862}

--- base/process/memory_unittest.cc
+++ base/process/memory_unittest.cc
@@ -174,6 +174,8 @@ class OutOfMemoryDeathTest : public OutOfMemoryTest {
 #endif
 };
 
+// Failing on x86 android, crbug.com/1181112
+#if !defined(OS_ANDROID) || !defined(ARCH_CPU_X86)
 TEST_F(OutOfMemoryDeathTest, New) {
   ASSERT_OOM_DEATH({
     SetUpInDeathAssert();
@@ -215,6 +217,7 @@ TEST_F(OutOfMemoryDeathTest, AlignedAlloc) {
     value_ = base::AlignedAlloc(test_size_, 8);
   });
 }
+#endif
 
 // POSIX does not define an aligned realloc function.
 #if defined(OS_WIN)
