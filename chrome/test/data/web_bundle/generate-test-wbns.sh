df6e1a5e279672884a711dcafad28e08bf796cef
Tsuyoshi Horo <horo@chromium.org>
2021-02-25 04:13:57 +0000

    Don't translate the urn scheme URL in WebBundle
    Bug: 1181626, 1082020
    
    Change-Id: I6b591c32433ba46882a0742b924a0c18c8b66f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717122
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857532}

---
+++ chrome/test/data/web_bundle/generate-test-wbns.sh
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+
+if ! command -v gen-bundle > /dev/null 2>&1; then
+    echo "gen-bundle is not installed. Please run:"
+    echo "  go get -u github.com/WICG/webpackage/go/bundle/cmd/..."
+    echo '  export PATH=$PATH:$(go env GOPATH)/bin'
+    exit 1
+fi
+
+# Generate a WBN which includes urn:uuid resources.
+gen-bundle \
+  -version b1 \
+  -har urn-handler-test.har \
+  -primaryURL urn:uuid:be40faa5-b581-42af-9c0c-fd9cb3b8a7a0 \
+  -o urn-handler-test.wbn
