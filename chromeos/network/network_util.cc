14f60964f69fec27e8289ab4925fefb29752d833
Azeem Arshad <azeemarshad@chromium.org>
2021-02-25 19:37:26 +0000

    [CrOS Cellular] Add SIMInfo.is_primary field in cros_network_config.
    This CL adds SIMInfo.is_primary field in cros_network_config API
    that indicates whether a slot is currently active or not.
    Device properties do not expose active iccid, so this allows us
    to determine what iccid is currently active. This is needed to
    show SIM Lock UI for the currently active SIM.
    
    Bug: 1093185
    Change-Id: I987f5265b01388395b4ffa748f8086a3a7db5d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716859
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857718}

--- chromeos/network/network_util.cc
+++ chromeos/network/network_util.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/onc/onc_signature.h"
 #include "chromeos/network/onc/onc_translation_tables.h"
 #include "chromeos/network/onc/onc_translator.h"
+#include "components/device_event_log/device_event_log.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
 namespace chromeos {
@@ -186,10 +187,9 @@ bool ParseCellularSIMSlotInfo(
     if (iccid)
       sim_slot_info.iccid = *iccid;
 
-    const std::string* primary =
-        value.FindStringKey(shill::kSIMSlotInfoPrimary);
-    if (primary)
-      sim_slot_info.primary = *primary;
+    base::Optional<bool> primary =
+        value.FindBoolKey(shill::kSIMSlotInfoPrimary);
+    sim_slot_info.primary = primary.has_value() ? *primary : false;
 
     sim_slot_infos->push_back(sim_slot_info);
   }
