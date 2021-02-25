58031f80a9a28294ea686241a072e3d11960a2e2
Eric Orth <ericorth@chromium.org>
2021-02-25 19:54:04 +0000

    Block MockHostResolver from using system or default resolvers
    Added a new param onto HostResolverProc to block such resolutions. If
    set, the HostResolverProc will not try to setup the global
    `default_proc_` as a fallback, and if a request is not handled by any
    proc, it will crash rather than use the system resolver as a final
    fallback.  MockHostResolver now always sets this new parameter.
    
    Also gave MockHostResolver a new param to optionally not add a
    "catchall" rule that would attempt to resolve any non-rule-matching
    requests to 127.0.0.1. It's not ideal mocking behavior to silently
    allow unexpected interactions, and the catchall is slightly buggy and
    doesn't catch everything anyway. I would make the new param the default,
    but I found a large number of existing tests relying on the catchall
    behavior (see crbug.com/1182263).
    
    Also fix some PacFileDeciderQuickCheckTests that were mistakenly
    making queries that fallback to the system resolver that would become
    (correctly recognized as) broken with these MockHostResolver
    improvements.
    
    Fixed: 1181196,1181780
    Change-Id: Iacb9d8852ccbcf02c62332dd5881917ac0e51d5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716425
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857747}

--- net/dns/mock_host_resolver.h
+++ net/dns/mock_host_resolver.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/thread_checker.h"
+#include "net/base/address_family.h"
 #include "net/base/completion_once_callback.h"
 #include "net/base/network_isolation_key.h"
 #include "net/dns/host_resolver.h"
@@ -71,7 +72,9 @@ int ParseAddressList(const std::string& host_list,
 // re-map one hostname to another as well.
 //
 // By default, MockHostResolvers include a single rule that maps all hosts to
-// 127.0.0.1.
+// 127.0.0.1. Disable by setting `require_matching_rule` to true.
+// TODO(crbug.com/1182263): Fixup any tests relying on the current default and
+// switch to make `require_matching_rule` be the default.
 //
 // Separate rules are used for separate HostResolverSource (eg
 // HostResolverSource::SYSTEM for requests that should only be resolved using
@@ -242,7 +245,9 @@ class MockHostResolverBase
   // If > 0, |cache_invalidation_num| is the number of times a cached entry can
   // be read before it invalidates itself. Useful to force cache expiration
   // scenarios.
-  explicit MockHostResolverBase(bool use_caching, int cache_invalidation_num);
+  explicit MockHostResolverBase(bool use_caching,
+                                int cache_invalidation_num,
+                                bool require_matching_rule);
 
   // Handle resolution for |request|. Expected to be called only the RequestImpl
   // object itself.
@@ -304,10 +309,11 @@ class MockHostResolverBase
 
 class MockHostResolver : public MockHostResolverBase {
  public:
-  MockHostResolver()
+  explicit MockHostResolver(bool require_matching_rule = false)
       : MockHostResolverBase(false /*use_caching*/,
-                             0 /* cache_invalidation_num */) {}
-  ~MockHostResolver() override {}
+                             0 /* cache_invalidation_num */,
+                             require_matching_rule) {}
+  ~MockHostResolver() override = default;
 };
 
 // Same as MockHostResolver, except internally it uses a host-cache.
@@ -320,9 +326,12 @@ class MockCachingHostResolver : public MockHostResolverBase {
   // If > 0, |cache_invalidation_num| is the number of times a cached entry can
   // be read before it invalidates itself. Useful to force cache expiration
   // scenarios.
-  explicit MockCachingHostResolver(int cache_invalidation_num = 0)
-      : MockHostResolverBase(true /*use_caching*/, cache_invalidation_num) {}
-  ~MockCachingHostResolver() override {}
+  explicit MockCachingHostResolver(int cache_invalidation_num = 0,
+                                   bool require_matching_rule = false)
+      : MockHostResolverBase(true /*use_caching*/,
+                             cache_invalidation_num,
+                             require_matching_rule) {}
+  ~MockCachingHostResolver() override = default;
 };
 
 // Factory that will always create and return Mock(Caching)HostResolvers.
@@ -366,7 +375,10 @@ class MockHostResolverFactory : public HostResolver::Factory {
 // added or removed on any thread.
 class RuleBasedHostResolverProc : public HostResolverProc {
  public:
-  explicit RuleBasedHostResolverProc(HostResolverProc* previous);
+  // If `allow_fallback` is false, no Proc fallback is allowed except to
+  // `previous`.
+  explicit RuleBasedHostResolverProc(HostResolverProc* previous,
+                                     bool allow_fallback = true);
 
   // Any hostname matching the given pattern will be replaced with the given
   // |ip_literal|.
@@ -413,10 +425,14 @@ class RuleBasedHostResolverProc : public HostResolverProc {
   void AllowDirectLookup(const std::string& host);
 
   // Simulate a lookup failure for |host| (it also can be a pattern).
-  void AddSimulatedFailure(const std::string& host);
+  void AddSimulatedFailure(
+      const std::string& host,
+      HostResolverFlags flags = HOST_RESOLVER_LOOPBACK_ONLY);
 
   // Simulate a lookup timeout failure for |host| (it also can be a pattern).
-  void AddSimulatedTimeoutFailure(const std::string& host);
+  void AddSimulatedTimeoutFailure(
+      const std::string& host,
+      HostResolverFlags flags = HOST_RESOLVER_LOOPBACK_ONLY);
 
   // Deletes all the rules that have been added.
   void ClearRules();
