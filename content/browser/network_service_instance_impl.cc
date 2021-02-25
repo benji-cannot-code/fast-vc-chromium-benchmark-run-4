2a082675e9d8b6295de15198dd6601e3fce821e1
David Dorwin <ddorwin@chromium.org>
2021-02-25 23:15:44 +0000

    [fuchsia] Restore the sandbox for WebEngine's network process
    crrev.com/c/2657852 changed the default network process sandbox type
    to kNoSandbox for most platforms. On Fuchsia, this caused the network
    process to not be sandboxed and thus have its own job. Also, since
    WebEngine uses the sandbox type for identifying child processes, the
    package path was shown instead of the base filename and process type.
    
    Also proactively apply a similar fix for the audio service process.
    
    Fixed: 1177912
    Test: Load a web page in WebEngine and run `ps` on the device. Confirm "web_engine_exe:network" is listed and in its own job.
    Change-Id: Ie81c22bd8f1cc17034ae498594a387fe9b5c634c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2719096
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857902}

--- content/browser/network_service_instance_impl.cc
+++ content/browser/network_service_instance_impl.cc
@@ -640,7 +640,7 @@ void SetCertVerifierServiceFactoryForTesting(
 }
 
 bool IsNetworkSandboxEnabled() {
-#if defined(OS_MAC)
+#if defined(OS_MAC) || defined(OS_FUCHSIA)
   return true;
 #else
 #if defined(OS_WIN)
@@ -659,7 +659,7 @@ bool IsNetworkSandboxEnabled() {
 #endif  // defined(OS_WIN)
   return base::FeatureList::IsEnabled(
       sandbox::policy::features::kNetworkServiceSandbox);
-#endif  // defined(OS_MAC)
+#endif  // defined(OS_MAC) || defined(OS_FUCHSIA)
 }
 
 }  // namespace content
