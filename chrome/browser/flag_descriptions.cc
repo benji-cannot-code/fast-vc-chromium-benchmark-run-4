571e94df3c0a06acdb6e77b15f07509519a55112
Viktor Semeniuk <vsemeniuk@google.com>
2021-02-25 12:51:22 +0000

    Adding flag for filling passwords across affiliated websites
    Bug: 1181304
    Change-Id: Ife31e5e877157d9eedc05ff0ff53ce3819fb92a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2715026
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857634}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1212,6 +1212,12 @@ const char kFileHandlingAPIDescription[] =
     "Enables the file handling API, allowing websites to register as file "
     "handlers.";
 
+const char kFillingAcrossAffiliatedWebsitesName[] =
+    "Fill passwords across affiliated websites.";
+const char kFillingAcrossAffiliatedWebsitesDescription[] =
+    "Enables filling password on a website when there is saved "
+    "password on affiliated website.";
+
 const char kFillOnAccountSelectName[] = "Fill passwords on account selection";
 const char kFillOnAccountSelectDescription[] =
     "Filling of passwords when an account is explicitly selected by the user "
