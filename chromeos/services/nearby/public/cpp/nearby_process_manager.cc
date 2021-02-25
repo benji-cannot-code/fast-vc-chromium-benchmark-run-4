3bc161747d79c83681e48e0529bf426907988325
Shane Fitzpatrick <shanefitz@google.com>
2021-02-25 03:54:51 +0000

    [Nearby] Record more granular disconnect reason in nearby_connector_impl
    Previously we were only recording 'crash' every time the nearby process
    stopped.  We have recently exposed a more granular reason for why the
    process stopped, and this change uses that to determine what
    values should be recorded (other than 'crash').
    
    Fixed: 1180848
    Change-Id: Idccc7f231494c23c6c62390de9de193560590344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717240
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857518}

---
+++ chromeos/services/nearby/public/cpp/nearby_process_manager.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/nearby/public/cpp/nearby_process_manager.h"
+
+namespace chromeos {
+namespace nearby {
+
+std::ostream& operator<<(
+    std::ostream& os,
+    const NearbyProcessManager::NearbyProcessShutdownReason& reason) {
+  switch (reason) {
+    case NearbyProcessManager::NearbyProcessShutdownReason::kNormal:
+      return os << "Normal";
+    case NearbyProcessManager::NearbyProcessShutdownReason::kCrash:
+      return os << "Crash";
+    case NearbyProcessManager::NearbyProcessShutdownReason::
+        kMojoPipeDisconnection:
+      return os << "Mojo Pipe Disconnection";
+  }
+}
+
+}  // namespace nearby
+}  // namespace chromeos
