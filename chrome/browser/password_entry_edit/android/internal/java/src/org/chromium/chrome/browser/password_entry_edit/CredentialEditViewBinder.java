480c8d1fc059d6733a8d2f312bd9bcff161ebcb8
Ioana Pandele <ioanap@chromium.org>
2021-02-25 22:39:26 +0000

    [PwdEditAndroid] Add validation for username and password
    Errors should be shown when:
    - The password field is left empty.
    - Attempting to modify a username to an already existing saved username
    for the same site.
    
    The "Done" button should be disabled if there is an error.
    
    Bug: 1175785
    Change-Id: Idcc6125a40dc0f329cdd76651f09edbdd5a53bdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720225
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857887}

--- chrome/browser/password_entry_edit/android/internal/java/src/org/chromium/chrome/browser/password_entry_edit/CredentialEditViewBinder.java
+++ chrome/browser/password_entry_edit/android/internal/java/src/org/chromium/chrome/browser/password_entry_edit/CredentialEditViewBinder.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.password_entry_edit;
 
+import static org.chromium.chrome.browser.password_entry_edit.CredentialEditProperties.DUPLICATE_USERNAME_ERROR;
+import static org.chromium.chrome.browser.password_entry_edit.CredentialEditProperties.EMPTY_PASSWORD_ERROR;
 import static org.chromium.chrome.browser.password_entry_edit.CredentialEditProperties.FEDERATION_ORIGIN;
 import static org.chromium.chrome.browser.password_entry_edit.CredentialEditProperties.PASSWORD;
 import static org.chromium.chrome.browser.password_entry_edit.CredentialEditProperties.PASSWORD_VISIBLE;
@@ -32,12 +34,16 @@ class CredentialEditViewBinder {
             // layout is in place.
         } else if (propertyKey == USERNAME) {
             fragmentView.setUsername(model.get(USERNAME));
+        } else if (propertyKey == DUPLICATE_USERNAME_ERROR) {
+            fragmentView.changeUsernameError(model.get(DUPLICATE_USERNAME_ERROR));
         } else if (propertyKey == PASSWORD_VISIBLE) {
             fragmentView.changePasswordVisibility(model.get(PASSWORD_VISIBLE));
         } else if (propertyKey == PASSWORD) {
             if (model.get(FEDERATION_ORIGIN).isEmpty()) {
                 fragmentView.setPassword(model.get(PASSWORD));
             }
+        } else if (propertyKey == EMPTY_PASSWORD_ERROR) {
+            fragmentView.changePasswordError(model.get(EMPTY_PASSWORD_ERROR));
         } else if (propertyKey == UI_DISMISSED_BY_NATIVE) {
             fragmentView.dismiss();
         } else {
