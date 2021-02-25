1464489d218edb04fd250130775f2c91cb54f401
Yu Han <yuzhehan@chromium.org>
2021-02-25 22:11:28 +0000

    Reland "[Controls Refresh Android] Fix thin border for form controls: button, input, progress, etc."
    This is a reland of c50c499441f7b448d55def02d9db9c47f243900d
    
    The reason for the revert was that this CL causes failures on some of
    the testers bots. They have been addressed in the following:
    1. linux-lacros-tester-rel.  Added BUILDFLAG(IS_CHROMEOS_LACROS) check
       to ensure the correct expected PNGs are compared.
    2. Mac10.11-Mac10.14 tester bots failure. This was due to additional
       text that resulted in too much difference for pixel match. This CL
       removed most of the text and bump up the error limits on Mac.
       Note: mac_chromium_[10.11 | 10.12]_rel_ng are not due to this CL but
       other systemic failures.
       In addition, added a check to prevent running dark mode tests on
       MacOS <= 10.14.
    3. Win7 Tests: Failure caused by font rendering diff between Win7 and
       Win10. Prevent test Button from running in Win7.
    
    Original change's description:
    > [Controls Refresh Android] Fix thin border for form controls: button, input, progress, etc.
    >
    > Prior to this CL, form controls' border does not respect zoom and
    > DSF if they are painted with native theme. This resulted in inconsistent
    > border on highDPI display between controls. Form controls that were
    > painted in blink, <meter> and multi-select, have a consistent border
    > appearance on highDPI with other elements like div {border: 1px solid}.
    > While other controls--button, input, progressbar--appear to have a
    > thin border on highDPI display.
    >
    > This CL fixes this issue by passing zoom level from blink to native
    > theme and applying the zoom to form controls' border. A decision is made
    > by Dev and UX to apply this fix to android only. We felt a general fix
    > for all platform introduces side effects that needs further analysis,
    > follow-up crbug.com/1179509.
    >
    >
    >
    > Bug: 1160104
    > Change-Id: Ied25eca8b39375dbc00de9d09132439787006c3c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2694095
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Yu Han <yuzhehan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#856492}
    
    Bug: 1160104 1181215
    Change-Id: I85cc2441b2370a90303f34498ae8502034f6ed37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2714445
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Yu Han <yuzhehan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857878}

--- third_party/blink/public/platform/web_theme_engine.h
+++ third_party/blink/public/platform/web_theme_engine.h
@@ -128,6 +128,7 @@ class WebThemeEngine {
     SkColor background_color;
     bool has_border;
     bool auto_complete_active;
+    float zoom;
   };
 
   // Extra parameters for PartMenuList
@@ -140,6 +141,7 @@ class WebThemeEngine {
     SkColor arrow_color;
     SkColor background_color;
     bool fill_content_area;
+    float zoom;
   };
 
   // Extra parameters for PartSliderTrack and PartSliderThumb
@@ -165,6 +167,7 @@ class WebThemeEngine {
     int value_rect_y;
     int value_rect_width;
     int value_rect_height;
+    float zoom;
   };
 
   // Extra parameters for scrollbar thumb. Used only for overlay scrollbars.
