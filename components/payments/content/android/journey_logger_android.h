8536a3a10883ffed52184a63c3aede437806cb69
Liquan (Max) Gu <maxlg@chromium.org>
2021-02-25 20:11:15 +0000

    [Events] Hide EVENT_AVAILABLE_METHOD_*
    Motivation:
    Since we are going to introduce Event2 bitfields to JourneyLogger,
    exposing Event and Event2 to the browser side would be confusing because
    they are implementation details. So we need to hide the Event class
    from the browser side.
    
    Changes:
    Refactored "Event.EVENT_AVAILABLE_METHOD_*" events so that the browser
    side would not reference it.
    
    Bug: 1180398
    Change-Id: I5715f22acea0a7aacc5e4e529d1f329c0f097ae2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2708883
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857775}

--- components/payments/content/android/journey_logger_android.h
+++ components/payments/content/android/journey_logger_android.h
@@ -43,6 +43,9 @@ class JourneyLoggerAndroid {
   void SetSelectedMethod(JNIEnv* env,
                          const base::android::JavaParamRef<jobject>& jcaller,
                          jint jPaymentMethodCategory);
+  void SetAvailableMethod(JNIEnv* env,
+                          const base::android::JavaParamRef<jobject>& jcaller,
+                          jint jPaymentMethodCategory);
   void SetRequestedInformation(
       JNIEnv* env,
       const base::android::JavaParamRef<jobject>& jcaller,
