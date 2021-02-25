fc50c32f15900e1557ab36e4e13f38c872d9fdba
Michael Checo <michaelcheco@google.com>
2021-02-25 21:39:39 +0000

    Diagnostics: Hide TBD marketing name
    Screenshot: http://shortn/_8zac4VxQku
    
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I36620911da67caf615bc6269e5cab1e726a251af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720662
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857851}

--- chrome/test/data/webui/chromeos/diagnostics/overview_card_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/overview_card_test.js
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://diagnostics/overview_card.js';
 
 import {SystemInfo} from 'chrome://diagnostics/diagnostics_types.js';
-import {fakeSystemInfo} from 'chrome://diagnostics/fake_data.js';
+import {fakeSystemInfo, fakeSystemInfoWithTBD} from 'chrome://diagnostics/fake_data.js';
 import {FakeSystemDataProvider} from 'chrome://diagnostics/fake_system_data_provider.js';
 import {getSystemDataProvider, setSystemDataProviderForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
-import {flushTasks} from '../../test_util.m.js';
+import {flushTasks, isVisible} from '../../test_util.m.js';
 
 import * as dx_utils from './diagnostics_test_utils.js';
 
@@ -62,6 +62,13 @@ export function overviewCardTestSuite() {
       dx_utils.assertElementContainsText(
           overviewElement.$$('#deviceInfo'),
           fakeSystemInfo.versionInfo.milestoneVersion);
+    });
+  });
+
+  test('TBDMarketingNameHidden', () => {
+    return initializeOverviewCard(fakeSystemInfoWithTBD).then(() => {
+      assertFalse(isVisible(
+          /** @type {!HTMLElement} */ (overviewElement.$$('#marketingName'))));
     });
   });
 }
