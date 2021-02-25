d0b8d6f6958f50672b625ac0890b8335e695e1fc
Monica Basta <msalama@chromium.org>
2021-02-25 19:40:50 +0000

    [Settings]: Remove the code of the old design of manage profiles page.
    The profile picker is replacing the current user manager for M90. As a
    consequence the new local profile creation flow will be the only UI used for
    profile creation. Newly created profiles will have the option to use the
    generic colored avatar as well as a theme color. Therefore, using the new
    design for the manage profile is required for M90.
    
    Bug: 1014322
    Change-Id: Ide8c4550b61a251daee33d8493104948749c51b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2718188
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857724}

--- chrome/browser/ui/webui/settings/settings_manage_profile_handler.cc
+++ chrome/browser/ui/webui/settings/settings_manage_profile_handler.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/signin/profile_colors_util.h"
-#include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
@@ -152,13 +151,11 @@ std::unique_ptr<base::ListValue> ManageProfileHandler::GetAvailableIcons() {
       profiles::GetCustomProfileAvatarIconsAndLabels(selected_avatar_idx));
 
   if (entry->GetSigninState() == SigninState::kNotSignedIn) {
-    if (base::FeatureList::IsEnabled(features::kNewProfilePicker)) {
-      ProfileThemeColors colors = entry->GetProfileThemeColors();
-      auto generic_avatar_info = profiles::GetDefaultProfileAvatarIconAndLabel(
-          colors.default_avatar_fill_color, colors.default_avatar_stroke_color,
-          selected_avatar_idx == profiles::GetPlaceholderAvatarIndex());
-      avatars->Insert(0, std::move(generic_avatar_info));
-    }
+    ProfileThemeColors colors = entry->GetProfileThemeColors();
+    auto generic_avatar_info = profiles::GetDefaultProfileAvatarIconAndLabel(
+        colors.default_avatar_fill_color, colors.default_avatar_stroke_color,
+        selected_avatar_idx == profiles::GetPlaceholderAvatarIndex());
+    avatars->Insert(0, std::move(generic_avatar_info));
     return avatars;
   }
 
