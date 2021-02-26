ddf061fe7bb6323e9cd559405e07c8a944950999
John Delaney <johnidel@chromium.org>
2021-02-26 19:41:29 +0000

    Set initiator_process_id in LoadUrlParams for opener_supressed navs
    What:
    Properly set initiator_process_id for browser initiated navigations
    which handled by the CreateWindow + opener_supressed code path.
    
    Why:
    This is currently unset, causing initiator frame lookup to fail
    for the Conversion Measurement API when an impression is attached
    to these navigations.
    
    initiator_frame_tokens were properly set on these in
    https://crrev.com/c/2600016
    
    This was not covered in existing test suites because
    ImpressionDeclarationBrowsertest only verifies that impressions
    are attached for these navigations, not that the initiator is also
    valid. This failure case is now covered by a new ConversionsBrowserTest
    test.
    
    Bug: 1155577
    Change-Id: I19273842437c832a40f11f2ef8a58d585fd894c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2718535
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858224}

--- content/browser/navigation_browsertest.cc
+++ content/browser/navigation_browsertest.cc
@@ -1082,6 +1082,7 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTest,
   RenderFrameHost* initial_rfh = current_frame_host();
   url::Origin initial_origin = initial_rfh->GetLastCommittedOrigin();
   blink::LocalFrameToken initiator_frame_token = initial_rfh->GetFrameToken();
+  int initiator_process_id = initial_rfh->GetProcess()->GetID();
 
   // Simulate clicking on a cross-site link which has rel="noopener".
   {
@@ -1112,6 +1113,7 @@ IN_PROC_BROWSER_TEST_F(NavigationBrowserTest,
     EXPECT_TRUE(observer.last_initiator_frame_token().has_value());
     EXPECT_EQ(initiator_frame_token,
               observer.last_initiator_frame_token().value());
+    EXPECT_EQ(initiator_process_id, observer.last_initiator_process_id());
   }
 }
 
