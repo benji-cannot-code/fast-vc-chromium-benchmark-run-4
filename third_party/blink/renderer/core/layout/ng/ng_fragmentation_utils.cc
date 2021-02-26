45b218a66def3a046566f81ba9a3ac85b3568d75
Morten Stenshorne <mstensho@chromium.org>
2021-02-26 14:27:35 +0000

    Avoid fragmentainer overflow from children of monolithic content.
    Checking if the border box of a monolithic element can fit in the
    current fragmentainer isn't enough. Children may overflow its border
    box, and it's this size that we need to consider when determining
    whether a monolithic element can fit in the current fragmentainer, or if
    we have to push it to the next one.
    
    Had to add a work-around to
    fast/multicol/vertical-lr/balancing/balance-unbreakable.html to prevent
    the (monolithic) IMG element (with alt text) in the test from messing
    up. The broken image symbol got incorrectly aligned and therefore
    incorrectly contributed to overflow. Reported crbug.com/1178743 for
    this. Use a real image as a work-around. My first attempt was to switch
    the IMG to writing-mode:horizontal-tb, but that messes up legacy column
    balancing. Oh well...
    
    Bug: 1178742
    Change-Id: Iffbe55d53f32003ae58b574bb6a38745bb92d42e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2696632
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858109}

--- third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.cc
+++ third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
 // At a class A break point [1], the break value with the highest precedence
 // wins. If the two values have the same precedence (e.g. "left" and "right"),
 // the value specified on a latter object wins.
@@ -55,6 +56,25 @@ inline int FragmentainerBreakPrecedence(EBreakBetween break_value) {
       return 6;
   }
 }
+
+// Return layout overflow block-size that's not clipped (or simply the
+// block-size if it *is* clipped).
+LayoutUnit BlockAxisLayoutOverflow(const NGPhysicalFragment& fragment,
+                                   WritingDirectionMode writing_direction) {
+  const auto* box = DynamicTo<NGPhysicalBoxFragment>(fragment);
+  if (box && box->HasNonVisibleOverflow()) {
+    OverflowClipAxes block_axis =
+        writing_direction.IsHorizontal() ? kOverflowClipY : kOverflowClipX;
+    if (box->GetOverflowClipAxes() & block_axis)
+      box = nullptr;
+  }
+  PhysicalRect rect = fragment.LocalRect();
+  if (box)
+    rect.UniteEvenIfEmpty(box->LayoutOverflow());
+  return writing_direction.IsHorizontal() ? rect.Bottom() : rect.Right();
+}
+
+}  // anonymous namespace
 
 EBreakBetween JoinFragmentainerBreakValues(EBreakBetween first_value,
                                            EBreakBetween second_value) {
@@ -640,7 +660,10 @@ bool MovePastBreakpoint(const NGConstraintSpace& space,
         builder->SetBreakAppeal(appeal_inside);
       return true;
     }
-  } else if (refuse_break_before || fragment.BlockSize() <= space_left) {
+  } else if (refuse_break_before ||
+             BlockAxisLayoutOverflow(physical_fragment,
+                                     space.GetWritingDirection()) <=
+                 space_left) {
     // The child either fits, or we are not allowed to break. So we can move
     // past this breakpoint.
     if (child.IsBlock() && builder) {
