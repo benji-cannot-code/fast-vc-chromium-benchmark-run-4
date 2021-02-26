281df30b989a092ba8c318327a4e052fe96be94b
Peter Kasting <pkasting@chromium.org>
2021-02-26 23:05:06 +0000

    Remove "throbber light" color and use "throbber spinning" instead.
    This is only used on standalone windows that show a throbber, e.g. a
    page that's been saved as an application so it opens as a new window
    with a window icon and title.
    
    I believe this dates from the original blue opaque frame, and on that
    frame, had good contrast.  However, subsequent changes and additions
    have made this very difficult to see.  Using the normal color instead is
    more visible, and simpler to boot.
    
    Bug: none
    Change-Id: I1c3e02b88cc454b835dcad98c8a1e125aef200ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2724651
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858325}

--- ui/native_theme/native_theme_color_id.h
+++ ui/native_theme/native_theme_color_id.h
@@ -154,7 +154,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   /* Colors for the material spinner (aka throbber). */                        \
   OP(kColorId_ThrobberSpinningColor),                                          \
   OP(kColorId_ThrobberWaitingColor),                                           \
-  OP(kColorId_ThrobberLightColor),                                             \
   /* Colors for Bubble Border */                                               \
   OP(kColorId_BubbleBorder),                                                   \
   /* Colors for Footnote Container. */                                         \
