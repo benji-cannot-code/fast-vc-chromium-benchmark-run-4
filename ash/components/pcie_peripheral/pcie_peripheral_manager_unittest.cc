b99186c6733a0166828cb8aba85ceeb68104a330
Jimmy Gong <jimmyxgong@google.com>
2021-02-26 01:24:53 +0000

    pciguard: Observe Typecd Dbus events in PciePeripheralManager
    - Have PciePeripheralManager start observing the TypecdClient D-Bus
      client.
    - This will now start displaying notifications based on the signals
      received from Typecd.
    
    TBR=dcheng@chromium.org, dpranke@google.com
    
    TBR list:
    dcheng@ for +base in DEPS
    dpranke@ for +testing in DEPS
    
    Bug: 1175987
    Test: ash_unittest
    Change-Id: I18899efa82b7f4aaa9d4560dea1606a4a0b29829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2715669
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857951}

--- ash/components/pcie_peripheral/pcie_peripheral_manager_unittest.cc
+++ ash/components/pcie_peripheral/pcie_peripheral_manager_unittest.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "chromeos/dbus/typecd/fake_typecd_client.h"
+#include "chromeos/dbus/typecd/typecd_client.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
@@ -54,8 +56,15 @@ class PciePeripheralManagerTest : public testing::Test {
 
   // testing::Test:
   void SetUp() override {
-    PciePeripheralManager::Initialize(/*is_guest_session=*/false,
-                                      /*is_pci_tunneling_allowed=*/false);
+    chromeos::TypecdClient::InitializeFake();
+    fake_typecd_client_ =
+        static_cast<chromeos::FakeTypecdClient*>(chromeos::TypecdClient::Get());
+  }
+
+  void InitializeManager(bool is_guest_session,
+                         bool is_pcie_tunneling_allowed) {
+    PciePeripheralManager::Initialize(is_guest_session,
+                                      is_pcie_tunneling_allowed);
     manager_ = PciePeripheralManager::Get();
 
     manager_->AddObserver(&fake_observer_);
@@ -64,6 +73,11 @@ class PciePeripheralManagerTest : public testing::Test {
   void TearDown() override {
     manager_->RemoveObserver(&fake_observer_);
     PciePeripheralManager::Shutdown();
+    chromeos::TypecdClient::Shutdown();
+  }
+
+  chromeos::FakeTypecdClient* fake_typecd_client() {
+    return fake_typecd_client_;
   }
 
   size_t GetNumLimitedPerformanceObserverCalls() {
@@ -79,16 +93,98 @@ class PciePeripheralManagerTest : public testing::Test {
   }
 
  private:
+  chromeos::FakeTypecdClient* fake_typecd_client_;
   PciePeripheralManager* manager_ = nullptr;
   FakeObserver fake_observer_;
 };
 
-// TODO(jimmyxgong): Add more tests once the necessary D-bus clients are
-// available.
 TEST_F(PciePeripheralManagerTest, InitialTest) {
+  InitializeManager(/*is_guest_profile=*/false,
+                    /*is_pcie_tunneling_allowed=*/false);
   EXPECT_EQ(0u, GetNumLimitedPerformanceObserverCalls());
   EXPECT_EQ(0u, GetNumGuestModeNotificationObserverCalls());
   EXPECT_FALSE(GetIsCurrentGuestDeviceTbtOnly());
+}
+
+TEST_F(PciePeripheralManagerTest, LimitedPerformanceNotification) {
+  InitializeManager(/*is_guest_profile=*/false,
+                    /*is_pcie_tunneling_allowed=*/false);
+
+  EXPECT_EQ(0u, GetNumLimitedPerformanceObserverCalls());
+  EXPECT_EQ(0u, GetNumGuestModeNotificationObserverCalls());
+  EXPECT_FALSE(GetIsCurrentGuestDeviceTbtOnly());
+
+  // Simulate emitting D-Bus signal.
+  fake_typecd_client()->EmitThunderboltDeviceConnectedSignal(
+      /*is_thunderbolt_only=*/false);
+  // pcie tunneling is not allowed and a alt-mode device has been plugged in.
+  // Expect the notification observer to be called.
+  EXPECT_EQ(1u, GetNumLimitedPerformanceObserverCalls());
+  EXPECT_EQ(0u, GetNumGuestModeNotificationObserverCalls());
+  EXPECT_FALSE(GetIsCurrentGuestDeviceTbtOnly());
+}
+
+TEST_F(PciePeripheralManagerTest, NoNotificationShown) {
+  InitializeManager(/*is_guest_profile=*/false,
+                    /*is_pcie_tunneling_allowed=*/true);
+
+  EXPECT_EQ(0u, GetNumLimitedPerformanceObserverCalls());
+  EXPECT_EQ(0u, GetNumGuestModeNotificationObserverCalls());
+  EXPECT_FALSE(GetIsCurrentGuestDeviceTbtOnly());
+
+  // Simulate emitting D-Bus signal.
+  fake_typecd_client()->EmitThunderboltDeviceConnectedSignal(
+      /*is_thunderbolt_only=*/false);
+  // Pcie tunneling allowed, we do not show any notifications for this case.
+  EXPECT_EQ(0u, GetNumLimitedPerformanceObserverCalls());
+  EXPECT_EQ(0u, GetNumGuestModeNotificationObserverCalls());
+  EXPECT_FALSE(GetIsCurrentGuestDeviceTbtOnly());
+
+  // Simulate emitting a new D-Bus signal, this time with |is_thunderbolt_only|
+  // set to true.
+  fake_typecd_client()->EmitThunderboltDeviceConnectedSignal(
+      /*is_thunderbolt_only=*/true);
+  // Pcie tunneling allowed, we do not show any notifications for this case.
+  EXPECT_EQ(0u, GetNumLimitedPerformanceObserverCalls());
+  EXPECT_EQ(0u, GetNumGuestModeNotificationObserverCalls());
+  // No observer was called, therefore don't expect this to be updated.
+  EXPECT_FALSE(GetIsCurrentGuestDeviceTbtOnly());
+}
+
+TEST_F(PciePeripheralManagerTest, GuestNotificationLimitedPerformance) {
+  InitializeManager(/*is_guest_profile=*/true,
+                    /*is_pcie_tunneling_allowed=*/false);
+
+  EXPECT_EQ(0u, GetNumLimitedPerformanceObserverCalls());
+  EXPECT_EQ(0u, GetNumGuestModeNotificationObserverCalls());
+  EXPECT_FALSE(GetIsCurrentGuestDeviceTbtOnly());
+
+  // Simulate emitting D-Bus signal.
+  fake_typecd_client()->EmitThunderboltDeviceConnectedSignal(
+      /*is_thunderbolt_only=*/false);
+  // Pcie tunneling not allowed and user is in guest session. The device
+  // supports an alt-mode, expect the notification observer to be called.
+  EXPECT_EQ(0u, GetNumLimitedPerformanceObserverCalls());
+  EXPECT_EQ(1u, GetNumGuestModeNotificationObserverCalls());
+  EXPECT_FALSE(GetIsCurrentGuestDeviceTbtOnly());
+}
+
+TEST_F(PciePeripheralManagerTest, GuestNotificationRestricted) {
+  InitializeManager(/*is_guest_profile=*/true,
+                    /*is_pcie_tunneling_allowed=*/false);
+
+  EXPECT_EQ(0u, GetNumLimitedPerformanceObserverCalls());
+  EXPECT_EQ(0u, GetNumGuestModeNotificationObserverCalls());
+  EXPECT_FALSE(GetIsCurrentGuestDeviceTbtOnly());
+
+  // Simulate emitting D-Bus signal.
+  fake_typecd_client()->EmitThunderboltDeviceConnectedSignal(
+      /*is_thunderbolt_only=*/true);
+  // Pcie tunneling not allowed and user is in guest session. The device
+  // does not support alt-mode, expect the notification observer to be called.
+  EXPECT_EQ(0u, GetNumLimitedPerformanceObserverCalls());
+  EXPECT_EQ(1u, GetNumGuestModeNotificationObserverCalls());
+  EXPECT_TRUE(GetIsCurrentGuestDeviceTbtOnly());
 }
 
 }  // namespace ash
