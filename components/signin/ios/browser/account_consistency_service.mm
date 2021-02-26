cae2f333b92b2f123d3a82d2bf7099e40f848845
Nohemi Fernandez <fernandex@chromium.org>
2021-02-26 09:20:14 +0000

    [iOS] Add time constraint to Gaia cookie refresh v2 logic.
    In our initial metrics we have noticed that there are some situations in
    which redirects to accounts.google.com are being processed on the order
    of 100s per client. In order to circumvent the potential problem of
    leaving users in an infinite loop we simulate a one-hour sleep call
    following the first time accounts.google.com is processed in the
    context of Gaia cookie restoration.
    
    Note that if the first call to restore Gaia cookies is unsuccessful the
    user will be left in a signed-out state on the web.
    
    Bug: 1181747
    Change-Id: Ib127a39d091fb5ee13a4104c355a79bd58cfa4e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2718115
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858060}

--- components/signin/ios/browser/account_consistency_service.mm
+++ components/signin/ios/browser/account_consistency_service.mm
@@ -272,12 +272,13 @@ void AccountConsistencyService::AccountConsistencyHandler::ShouldAllowResponse(
           DLOG_IF(ERROR,
                   !params.continue_url.empty() && !continue_url.is_valid())
               << "Invalid continuation URL: \"" << continue_url << "\"";
-          identity_manager_->GetAccountsCookieMutator()
-              ->ForceTriggerOnCookieChange();
-          account_consistency_service_->AddCookieRestoreCallback(
-              base::BindOnce(&AccountConsistencyHandler::NavigateToURL,
-                             weak_ptr_factory_.GetWeakPtr(), continue_url));
-          return;
+          if (account_consistency_service_->RestoreGaiaCookies(base::BindOnce(
+                  &AccountConsistencyHandler::NavigateToURL,
+                  weak_ptr_factory_.GetWeakPtr(), continue_url))) {
+            // Continue URL will be processed in a callback once Gaia cookies
+            // have been restored.
+            return;
+          }
         }
       }
       if (params.show_consistency_promo) {
@@ -387,6 +388,22 @@ AccountConsistencyService::AccountConsistencyService(
 
 AccountConsistencyService::~AccountConsistencyService() {}
 
+BOOL AccountConsistencyService::RestoreGaiaCookies(
+    base::OnceClosure cookies_restored_callback) {
+  // Only processes a single restoration attempt for a given amount of time to
+  // avoid redirect loops.
+  if (last_gaia_cookie_update_time_.is_null() ||
+      base::Time::Now() - last_gaia_cookie_update_time_ <
+          GetDelayThresholdToUpdateGaiaCookie()) {
+    gaia_cookies_restored_callbacks_.push_back(
+        std::move(cookies_restored_callback));
+    identity_manager_->GetAccountsCookieMutator()->ForceTriggerOnCookieChange();
+    last_gaia_cookie_update_time_ = base::Time::Now();
+    return YES;
+  }
+  return NO;
+}
+
 void AccountConsistencyService::SetWebStateHandler(
     web::WebState* web_state,
     id<ManageAccountsDelegate> delegate) {
@@ -408,12 +425,6 @@ void AccountConsistencyService::RemoveWebStateHandler(
   handlers_map_.erase(iter);
 
   web_state->RemoveObserver(handler.get());
-}
-
-void AccountConsistencyService::AddCookieRestoreCallback(
-    base::OnceClosure cookies_restored_callback) {
-  gaia_cookies_restored_callbacks_.push_back(
-      std::move(cookies_restored_callback));
 }
 
 void AccountConsistencyService::SetGaiaCookiesIfDeleted(
@@ -462,7 +473,8 @@ void AccountConsistencyService::TriggerGaiaCookieChangeIfDeleted(
 
   // Re-generate cookie to ensure that the user is properly signed in.
   identity_manager_->GetAccountsCookieMutator()->ForceTriggerOnCookieChange();
-  AddCookieRestoreCallback(std::move(cookies_restored_callback));
+  gaia_cookies_restored_callbacks_.push_back(
+      std::move(cookies_restored_callback));
 }
 
 void AccountConsistencyService::RemoveAllChromeConnectedCookies(
