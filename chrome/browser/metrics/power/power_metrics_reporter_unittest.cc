193e819b800ea91e79f2c6b8d06c6a1f4ab741bc
Sebastien Marchand <sebmarchand@chromium.org>
2021-02-26 01:33:07 +0000

    [battery] Record the time spent capturing video
    Bug: 1153193
    Change-Id: Idcc0c434bcc70bdf123df61ae62dfc2a37565415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717013
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857958}

--- chrome/browser/metrics/power/power_metrics_reporter_unittest.cc
+++ chrome/browser/metrics/power/power_metrics_reporter_unittest.cc
@@ -163,6 +163,8 @@ TEST_F(PowerMetricsReporterUnitTest, UKMs) {
       base::TimeDelta::FromSeconds(++fake_value);
   fake_interval_data.time_since_last_user_interaction_with_browser =
       base::TimeDelta::FromSeconds(++fake_value);
+  fake_interval_data.time_capturing_video =
+      base::TimeDelta::FromSeconds(++fake_value);
 
   task_environment_.FastForwardBy(kExpectedMetricsCollectionInterval);
   // Pretend that the battery has dropped by 50% in 2 minutes, for a rate of
@@ -249,6 +251,10 @@ TEST_F(PowerMetricsReporterUnitTest, UKMs) {
       ukm::GetExponentialBucketMinForUserTiming(
           fake_interval_data.time_since_last_user_interaction_with_browser
               .InSeconds()));
+  test_ukm_recorder_.ExpectEntryMetric(
+      entries[0], UkmEntry::kVideoCaptureSecondsName,
+      ukm::GetExponentialBucketMinForUserTiming(
+          fake_interval_data.time_capturing_video.InSeconds()));
 
   histogram_tester_.ExpectUniqueSample(kBatteryDischargeRateHistogramName, 2500,
                                        1);
