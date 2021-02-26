64638c97b639a5970c3ee8cfd8d02692178c3d73
mcrouse <mcrouse@google.com>
2021-02-26 21:59:09 +0000

    [LangID] Add chrome flag to allow easy local testing of the new model.
    This adds an about flag for language detection that relies on TFLite.
    
    Bug: 1182477
    Change-Id: I5e569c2dc83141ad7366d6a712c35e8dc48c8664
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2721126
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858295}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1409,6 +1409,9 @@ extern const char kTabOutlinesInLowContrastThemesDescription[];
 extern const char kTextFragmentColorChangeName[];
 extern const char kTextFragmentColorChangeDescription[];
 
+extern const char kTFLiteLanguageDetectionName[];
+extern const char kTFLiteLanguageDetectionDescription[];
+
 extern const char kTintCompositedContentName[];
 extern const char kTintCompositedContentDescription[];
 
