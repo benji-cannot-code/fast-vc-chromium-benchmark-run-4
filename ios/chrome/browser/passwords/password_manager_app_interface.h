ef84030166030a0336dbeff2bd3841df28dc320e
Fabio Tirelo <ftirelo@chromium.org>
2021-02-26 17:01:08 +0000

    Revert "[ios][PhishGuard] Add CachedVerdict methods and egtest"
    This reverts commit 6b36a2a88ab082230261201520142f746691895c.
    
    Reason for revert: speculative revert due to failing eg tests; this is the only CL that seems to have any effect on those tests for the failing range (https://chromium.googlesource.com/chromium/src/+log/95badd61a39a4bec041a8fc6bbb1f55994ff5452..62e38c0a1136e7af3d54139cf42f94ddf8401ada)
    
    Example build: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-simulator-full-configs/29969/overview
    
    Original change's description:
    > [ios][PhishGuard] Add CachedVerdict methods and egtest
    >
    > * Adds methods that set and retrieve cached verdicts.
    > * Adds an integration test that tests everything from the JS injection,
    > to password protection logic, to presentation of the PhishGuard UI.
    >
    > Bug: 1147967
    > Change-Id: I283f00111a88584e3f214b147084f2bd34cd0401
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717288
    > Commit-Queue: edchin <edchin@chromium.org>
    > Reviewed-by: Javier Flores <javierrobles@chromium.org>
    > Reviewed-by: Ali Juma <ajuma@chromium.org>
    > Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    > Reviewed-by: Maria Kazinova <kazinova@google.com>
    > Cr-Commit-Position: refs/heads/master@{#858047}
    
    Bug: 1147967
    Change-Id: I1501f1e7f626700712b8d21651204baf92aa2c74
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2723331
    Auto-Submit: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#858159}

--- ios/chrome/browser/passwords/password_manager_app_interface.h
+++ ios/chrome/browser/passwords/password_manager_app_interface.h
@@ -12,12 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Stores a credential to the password store.
 + (NSError*)storeCredentialWithUsername:(NSString*)username
-                               password:(NSString*)password
-                                    URL:(NSURL*)URL;
-
-// Stores a credential to the password store. Associates the current WebState's
-// last committed URL with the credential.
-+ (NSError*)storeCredentialWithUsername:(NSString*)username
                                password:(NSString*)password;
 
 // Clears any credentials that were stored during a test run.
--- ios/chrome/browser/passwords/password_manager_app_interface.mm
+++ ios/chrome/browser/passwords/password_manager_app_interface.mm
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/app/tab_test_util.h"
 #import "ios/testing/nserror_util.h"
 #import "ios/web/public/web_state.h"
-#include "net/base/mac/url_conversions.h"
-#include "url/gurl.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -56,8 +54,7 @@ class PasswordStoreConsumerHelper : public PasswordStoreConsumer {
 @implementation PasswordManagerAppInterface
 
 + (NSError*)storeCredentialWithUsername:(NSString*)username
-                               password:(NSString*)password
-                                    URL:(NSURL*)URL {
+                               password:(NSString*)password {
   // Obtain a PasswordStore.
   scoped_refptr<password_manager::PasswordStore> passwordStore =
       IOSChromePasswordStoreFactory::GetForBrowserState(
@@ -73,21 +70,13 @@ class PasswordStoreConsumerHelper : public PasswordStoreConsumer {
   password_manager::PasswordForm passwordCredentialForm;
   passwordCredentialForm.username_value = base::SysNSStringToUTF16(username);
   passwordCredentialForm.password_value = base::SysNSStringToUTF16(password);
-  passwordCredentialForm.url = net::GURLWithNSURL(URL).GetOrigin();
+  passwordCredentialForm.url =
+      chrome_test_util::GetCurrentWebState()->GetLastCommittedURL().GetOrigin();
   passwordCredentialForm.signon_realm = passwordCredentialForm.url.spec();
   passwordCredentialForm.scheme = password_manager::PasswordForm::Scheme::kHtml;
   passwordStore->AddLogin(passwordCredentialForm);
 
   return nil;
-}
-
-+ (NSError*)storeCredentialWithUsername:(NSString*)username
-                               password:(NSString*)password {
-  NSURL* URL = net::NSURLWithGURL(
-      chrome_test_util::GetCurrentWebState()->GetLastCommittedURL());
-  return [PasswordManagerAppInterface storeCredentialWithUsername:username
-                                                         password:password
-                                                              URL:URL];
 }
 
 + (void)clearCredentials {
