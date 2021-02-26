4c85095f42cea1c3eb02cfe645a12920f8c67930
Xianzhu Wang <wangxianzhu@chromium.org>
2021-02-26 01:28:54 +0000

    Fix NG table collapsed border pixel-snapping and clipping
    This fixes the following issues:
    
    - We missed pixel-snapping of the collapsed borders.
      Add ObjectPainterBase::DrawBoxSide() which accepts IntRect instead of
      float parameters. Previously if the float parameters were not
      pixel-snapped, they would be converted to int by truncating which
      implicitly snapped to whole pixels incorrectly.
    
    - We missed pixel-snapping and shrinking by border outsets for the clip
      when painting background for layered table parts. Combine the clip
      logic for cell background and other table part background with
      correct pixel-snapping and clip rect, with a test.
    
    Bug: 1180773
    Change-Id: I81a967b3de08c116eed97a08e0f608a966088f40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717524
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857955}

--- third_party/blink/renderer/core/paint/object_painter_base.h
+++ third_party/blink/renderer/core/paint/object_painter_base.h
@@ -7,13 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_OBJECT_PAINTER_BASE_H_
 
 #include "third_party/blink/renderer/core/style/computed_style_constants.h"
+#include "third_party/blink/renderer/platform/geometry/int_rect.h"
+#include "third_party/blink/renderer/platform/graphics/color.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
 
 class ComputedStyle;
-class Color;
 class GraphicsContext;
 struct PaintInfo;
 struct PhysicalRect;
@@ -25,6 +26,20 @@ class ObjectPainterBase {
   STACK_ALLOCATED();
 
  public:
+  static void DrawBoxSide(GraphicsContext& context,
+                          const IntRect& snapped_edge_rect,
+                          BoxSide side,
+                          Color color,
+                          EBorderStyle style) {
+    DrawLineForBoxSide(context, snapped_edge_rect.X(), snapped_edge_rect.Y(),
+                       snapped_edge_rect.MaxX(), snapped_edge_rect.MaxY(), side,
+                       color, style, 0, 0, true);
+  }
+
+  // TODO(wangxianzhu): The float parameters are truncated to int in the
+  // function, which implicitly snaps to whole pixels incorrectly. We should
+  // always use the above function. For now the only outside caller is
+  // BoxBorderPainter::PaintOneBorderSide().
   static void DrawLineForBoxSide(GraphicsContext&,
                                  float x1,
                                  float y1,
@@ -33,8 +48,8 @@ class ObjectPainterBase {
                                  BoxSide,
                                  Color,
                                  EBorderStyle,
-                                 int adjbw1,
-                                 int adjbw2,
+                                 int adjacent_edge_width1,
+                                 int adjacent_edge_width2,
                                  bool antialias = false);
 
  protected:
