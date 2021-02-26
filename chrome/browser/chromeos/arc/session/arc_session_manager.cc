8ab41a53c06e24f96b5e69bedf1cdecf0602096e
Jiewei Qian <qjw@chromium.org>
2021-02-26 04:49:19 +0000

    Revert "Replace arc::LaunchAppWithIntent with AppService LaunchAppWithIntent."
    This reverts commit 0bad159a1eff79f0cfcdc9ba0ec335bbf0346f0e.
    
    Reason for revert: Failure on linux-chromeos-chrome
    
    Test failed: All/ArcAppLauncherForDefaultAppTest.AppLauncherForDefaultApps/1
    
    See: https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/12398/overview
    
    Original change's description:
    > Replace arc::LaunchAppWithIntent with AppService LaunchAppWithIntent.
    >
    > This CL replaces arc::LaunchAppWithIntent with the AppService
    > interface LaunchAppWithIntent, so that:
    > 1. We can have 1 central place to add app launching metrics(UMA)
    > 2. We have 1 central place to record the app launching information for
    > the full restore project.
    >
    > Modify ArcAppLauncher to call the AppService interface
    > LaunchAppWithIntent to launch the app, and observe AppRegistryCache to
    > keep stack the app status.
    >
    > Add CreateIntentFromStartType to generate intent using activity and
    > start type.
    >
    > Modify mojom to add start type to the intent mojom structure.
    >
    > Modify unit tests to verify ArcAppLauncher.
    >
    > BUG=1146900
    > BUG=1154891
    >
    > Change-Id: I761189bdd75266701b4b2697500626049dfd1057
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2711227
    > Reviewed-by: Long Cheng <lgcheng@google.com>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Maggie Cai <mxcai@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#857975}
    
    Bug: 1146900
    Bug: 1154891
    Change-Id: I82e9445270c488966a69e1c20a4592eea3b20483
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2719696
    Auto-Submit: Jiewei Qian  <qjw@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#858028}

--- chrome/browser/chromeos/arc/session/arc_session_manager.cc
+++ chrome/browser/chromeos/arc/session/arc_session_manager.cc
@@ -67,7 +67,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/session/arc_session_runner.h"
 #include "components/arc/session/arc_supervision_transition.h"
 #include "components/prefs/pref_service.h"
-#include "components/services/app_service/public/cpp/intent_util.h"
 #include "components/session_manager/core/session_manager.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/browser_thread.h"
@@ -684,10 +683,10 @@ void ArcSessionManager::OnProvisioningFinished(
             prefs->GetBoolean(prefs::kArcProvisioningInitiatedFromOobe))) {
       playstore_launcher_ = std::make_unique<ArcAppLauncher>(
           profile_, kPlayStoreAppId,
-          apps_util::CreateIntentForActivity(kPlayStoreActivity,
-                                             kInitialStartParam),
+          GetLaunchIntent(kPlayStorePackage, kPlayStoreActivity,
+                          {kInitialStartParam}),
           false /* deferred_launch_allowed */, display::kInvalidDisplayId,
-          apps::mojom::LaunchSource::kFromChromeInternal);
+          arc::UserInteractionType::NOT_USER_INITIATED);
     }
 
     prefs->ClearPref(prefs::kArcProvisioningInitiatedFromOobe);
