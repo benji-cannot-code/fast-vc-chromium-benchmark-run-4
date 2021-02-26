4707b1e311e61c8ff71842c0402eee36be04e9c9
Tarun Bansal <tbansal@chromium.org>
2021-02-26 02:29:07 +0000

    Delete DeferAllScript logic from //blink.
    This can be deleted since we've decided to turn-down the
    intervention.
    
    This CL manually reverts previous chromium CLs:
    https://chromium-review.googlesource.com/c/chromium/src/+/1625269
    https://chromium-review.googlesource.com/c/chromium/src/+/1785137
    https://chromium-review.googlesource.com/c/chromium/src/+/1707129
    
    Change-Id: Id4a74198dbd3e208e7c985734d2fc536f0b5d084
    Bug: 1175799,1167769
    TBR: twellington@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2691539
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857981}

--- third_party/blink/renderer/core/html/parser/html_document_parser.cc
+++ third_party/blink/renderer/core/html/parser/html_document_parser.cc
@@ -553,9 +553,6 @@ void HTMLDocumentParser::PrepareToStopParsing() {
   if (IsDetached())
     return;
 
-  if (script_runner_)
-    script_runner_->RecordMetricsAtParseEnd();
-
   AttemptToRunDeferredScriptsAndEnd();
 }
 
