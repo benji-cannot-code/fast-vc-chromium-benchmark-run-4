391f33ee4b803bd520f0028a33c6803439139f25
Jeremy Roman <jbroman@chromium.org>
2021-02-26 22:29:55 +0000

    Mark scoped_refptr with [[clang::trivial_abi]].
    We already use the experimental libc++ ABI, which does the same to
    std::unique_ptr and std::shared_ptr.
    
    This means that files that don't build with the Windows ABI may
    need additional includes (because the destructor runs in the callee's
    frame) and the order of construction and destruction of arguments may
    not be well-nested. In some cases this also surfaces UAF bugs that
    were previously masked by optimization.
    
    This CL includes the additional needed includes, and the test fixes
    have been committed separately.
    
    This allows scoped_refptr and trivial wrappers of it to be passed in
    registers instead of on the stack, which leads to about 140 kB of APK
    size reduction on Android. It may also have a slight performance
    advantage, though this hasn't been validated.
    
    Bug: 1019548
    Change-Id: I01ca86d2cabb54aed9c51438732506fa87d17a63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2714247
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858306}

--- components/signin/internal/identity_manager/profile_oauth2_token_service_delegate_android.cc
+++ components/signin/internal/identity_manager/profile_oauth2_token_service_delegate_android.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/account_info.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "google_apis/gaia/oauth2_access_token_fetcher.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
 
 using base::android::AttachCurrentThread;
 using base::android::ConvertJavaStringToUTF8;
--- remoting/host/keyboard_layout_monitor_mac.cc
+++ remoting/host/keyboard_layout_monitor_mac.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_cftyperef.h"
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
+#include "base/single_thread_task_runner.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "remoting/proto/control.pb.h"
