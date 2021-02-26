cbe198c7693ef5740b9dc1c3538ec6875e780695
Cammie Smith Barnes <cammie@chromium.org>
2021-02-26 17:45:03 +0000

    PageLoadMetrics: Add PLMMemoryTrackerFactory to Weblayer and Webview.
    Previously, only //chrome had an implementation of
    PageLoadMetricsMemoryTrackerFactory, so the implementation of
    PageLoadMetricsEmbedder::GetMemoryTrackerForBrowserContext in both
    Weblayer and Webview was trivial.
    
    This CL adds a PageLoadMetricsMemoryTrackerFactory
    implementation to each of //weblayer and //android_webview and
    provides nontrivial implementations of
    PageLoadMetricsEmbedder::GetMemoryTrackerForBrowserContext, so that
    memory tracking functionality is available.
    
    Bug: 1180353
    Change-Id: Ib6a84f09a0a2fb2d961f03f880c1221ec09709d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2708024
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858179}

--- android_webview/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ android_webview/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/page_load_metrics/page_load_metrics_initialize.h"
 
+#include "android_webview/browser/page_load_metrics/aw_page_load_metrics_memory_tracker_factory.h"
 #include "base/macros.h"
 #include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
 #include "components/page_load_metrics/browser/page_load_metrics_embedder_base.h"
+#include "components/page_load_metrics/browser/page_load_metrics_memory_tracker.h"
 
 namespace content {
 class BrowserContext;
@@ -73,7 +75,11 @@ bool PageLoadMetricsEmbedder::IsExtensionUrl(const GURL& url) {
 page_load_metrics::PageLoadMetricsMemoryTracker*
 PageLoadMetricsEmbedder::GetMemoryTrackerForBrowserContext(
     content::BrowserContext* browser_context) {
-  return nullptr;
+  if (!base::FeatureList::IsEnabled(features::kV8PerFrameMemoryMonitoring))
+    return nullptr;
+
+  return AwPageLoadMetricsMemoryTrackerFactory::GetForBrowserContext(
+      browser_context);
 }
 
 }  // namespace
