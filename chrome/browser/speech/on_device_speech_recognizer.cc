380ca1bfdc6fa3ff9f566f7bb3d733858875f55d
Katie Dektar <katie@chromium.org>
2021-02-26 05:32:58 +0000

    Rename SodaSpeechRecognizer to OnDeviceSpeechRecognizer.
    SpeechRecognitionService authors aim to avoid leaky abstractions
    by not using the term SODA outside of SODA client. Continue that
    by renaming this to reflect that it's happening on-device.
    
    Bug: 1173135
    Change-Id: I578bc27306ea804993e8b7f9d8e0e360f6de6fe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2719240
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858033}

--- chrome/browser/speech/on_device_speech_recognizer.cc
+++ chrome/browser/speech/on_device_speech_recognizer.cc
@@ -3,22 +3,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/speech/soda_speech_recognizer.h"
+#include "chrome/browser/speech/on_device_speech_recognizer.h"
 
 #include "chrome/browser/speech/speech_recognizer_delegate.h"
 
-bool SodaSpeechRecognizer::IsSodaSpeechRecognizerAvailable() {
+bool OnDeviceSpeechRecognizer::IsOnDeviceSpeechRecognizerAvailable() {
   return false;
 }
 
-SodaSpeechRecognizer::SodaSpeechRecognizer(
+OnDeviceSpeechRecognizer::OnDeviceSpeechRecognizer(
     const base::WeakPtr<SpeechRecognizerDelegate>& delegate)
     : SpeechRecognizer(delegate) {}
 
-SodaSpeechRecognizer::~SodaSpeechRecognizer() {
+OnDeviceSpeechRecognizer::~OnDeviceSpeechRecognizer() {
   Stop();
 }
 
-void SodaSpeechRecognizer::Start() {}
+void OnDeviceSpeechRecognizer::Start() {}
 
-void SodaSpeechRecognizer::Stop() {}
+void OnDeviceSpeechRecognizer::Stop() {}
