daf4286cdb51295e72aa78dc799da8e8757be316
Side Yilmaz <sideyilmaz@chromium.org>
2021-02-26 09:39:56 +0000

    Adding Download.Start.PerProfileType metric.
    This CL adds metric to record download start activity for each platform
    by using BrowserProfileType enum.
    
    Also this CL replace histogram macros with histogram functions to
    prevent unnecessary caching.
    
    Bug: 1179196
    Change-Id: Id4d2187444ccc4cf28f683b45f2dc4ae8b782be7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702255
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858065}

--- chrome/browser/download/download_stats.h
+++ chrome/browser/download/download_stats.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "chrome/browser/download/download_prompt_status.h"
+#include "chrome/browser/profiles/profile.h"
 #include "components/download/public/common/download_danger_type.h"
 #include "components/download/public/common/download_path_reservation_tracker.h"
 
@@ -154,6 +155,8 @@ enum class DownloadShelfDragEvent {
 };
 
 void RecordDownloadShelfDragEvent(DownloadShelfDragEvent drag_event);
+
+void RecordDownloadStartPerProfileType(Profile* profile);
 
 #ifdef OS_ANDROID
 // Records whether the download dialog is shown to the user.
