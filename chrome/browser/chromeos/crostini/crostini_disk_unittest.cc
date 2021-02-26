fb858a28ace7081afc63de9a4030b51c1c4f5f4d
Joel Hockey <joelhockey@chromium.org>
2021-02-26 07:47:23 +0000

    Change EnsureVmRunning() to be RestartOption.start_vm_only
    Simplify the code to start only the VM without also starting the
    container. Rather than having an observer which calls abort, we can take
    a RestartOption and control the flow from within CrostiniRestarter.
    
    Observers now watch all restarters that run up to and including the
    restarter they are called on.  This ensures that observers will see
    all states until their restarter completes.Previously if a full
    restarter ran after a vm-only restarter, the observer would stop
    seeing states after the vm starts.
    
    This now means that RestartObservers may see states multiple times,
    and out of order.
    
    Change-Id: Ia338ef738487f4efbb5e011ce17f9d1e0c7ac7f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2719988
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858050}

--- chrome/browser/chromeos/crostini/crostini_disk_unittest.cc
+++ chrome/browser/chromeos/crostini/crostini_disk_unittest.cc
@@ -59,11 +59,13 @@ class CrostiniDiskTestDbus : public CrostiniDiskTest {
 
   void SetUp() override {
     profile_ = std::make_unique<TestingProfile>();
+    test_helper_ = std::make_unique<CrostiniTestHelper>(profile_.get());
     CrostiniManager::GetForProfile(profile_.get())
-        ->AddRunningVmForTesting("vm_name");
+        ->set_skip_restart_for_testing();
   }
 
   void TearDown() override {
+    test_helper_.reset();
     profile_.reset();
     chromeos::DBusThreadManager::Shutdown();
   }
@@ -93,6 +95,7 @@ class CrostiniDiskTestDbus : public CrostiniDiskTest {
   chromeos::FakeConciergeClient* fake_concierge_client_;
 
   std::unique_ptr<TestingProfile> profile_;
+  std::unique_ptr<CrostiniTestHelper> test_helper_;
 };
 
 TEST_F(CrostiniDiskTest, NonResizeableDiskReturnsEarly) {
