844e0a6ea266becb1ff7a8a85853003c5934d16b
Robert Sesek <rsesek@chromium.org>
2021-02-26 22:19:47 +0000

    mac: Enable process-wide CPU mitigations where available.
    The existing mitigation flag is per-thread, so this offers stronger
    protections. The thread-specific mitigation will remain enabled in
    order to support older macOS versions. The new process-wide flag does
    not conflict with it (task takes precedence over thread).
    
    The posix_spawnattr option is used instead of the libproc API, because
    the latter requires permitting (allow system-sched (target self)) in the
    sandbox, which would grant additional capabilities beyond the CPU
    mitigations control.
    
    Bug: 1129602
    Change-Id: I876f2aae3a3e26a622c67ad181efbdfd91fa8b76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2722913
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858301}

--- content/browser/ppapi_plugin_process_host.cc
+++ content/browser/ppapi_plugin_process_host.cc
@@ -126,6 +126,7 @@ class PpapiPluginSandboxedProcessLauncherDelegate
 
 #if defined(OS_MAC)
   bool DisclaimResponsibility() override { return true; }
+  bool EnableCpuSecurityMitigations() override { return true; }
 #endif
 
  private:
