70ac4fe15f7a014136410aa891bcdc7b399f2998
Kyle Horimoto <khorimoto@chromium.org>
2021-02-26 21:10:37 +0000

    [CrOS Cellular] Ensure mobile data subpage network list is shown
    Before this change, we'd only show the cellular-specific network list if
    there was at least one active network. This meant that if there were no
    networks (e.g., if the user was opening settings to try to install their
    first eSIM profile), the section would be hidden with no clear way to
    add the profile. This CL shows the cellular network list regardless of
    whether there already was a network.
    
    Fixed: 1182406
    Change-Id: Id0882c9c07fed0162387be441ba99a9a20c50e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720941
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858270}

--- chrome/test/data/webui/settings/chromeos/internet_subpage_tests.js
+++ chrome/test/data/webui/settings/chromeos/internet_subpage_tests.js
@@ -64,18 +64,28 @@ suite('InternetSubpage', function() {
     internetSubpage.deviceState = mojoApi_.getDeviceStateForTest(type);
   }
 
-  function setCellularNetworks() {
+  /**
+   * @param {!Array<!chromeos.networkConfig.mojom.NetworkStateProperties>=}
+   *     opt_networks Networks to set. If left undefined, default networks will
+   *     be set.
+   */
+  function addCellularNetworks(opt_networks) {
     const mojom = chromeos.networkConfig.mojom;
-    mojoApi_.setNetworkTypeEnabledState(mojom.NetworkType.kTether);
-    setNetworksForTest(mojom.NetworkType.kCellular, [
+
+    const networks = opt_networks || [
       OncMojo.getDefaultNetworkState(mojom.NetworkType.kCellular, 'cellular1'),
       OncMojo.getDefaultNetworkState(mojom.NetworkType.kTether, 'tether1'),
       OncMojo.getDefaultNetworkState(mojom.NetworkType.kTether, 'tether2'),
-    ]);
+    ];
+
+    mojoApi_.setNetworkTypeEnabledState(mojom.NetworkType.kTether);
+    setNetworksForTest(mojom.NetworkType.kCellular, networks);
     internetSubpage.tetherDeviceState = {
       type: mojom.NetworkType.kTether,
       deviceState: mojom.DeviceStateType.kEnabled
     };
+    internetSubpage.cellularDeviceState =
+        mojoApi_.getDeviceStateForTest(mojom.NetworkType.kCellular);
   }
 
   function initSubpage(isUpdatedCellularUiEnabled) {
@@ -222,7 +232,7 @@ suite('InternetSubpage', function() {
         function() {
           initSubpage(false /* isUpdatedCellularUiEnabled */);
           const mojom = chromeos.networkConfig.mojom;
-          setCellularNetworks();
+          addCellularNetworks();
           return flushAsync().then(() => {
             assertEquals(3, internetSubpage.networkStateList_.length);
             const toggle = internetSubpage.$$('#deviceEnabledButton');
@@ -242,7 +252,7 @@ suite('InternetSubpage', function() {
         function() {
           initSubpage(true /* isUpdatedCellularUiEnabled */);
           const mojom = chromeos.networkConfig.mojom;
-          setCellularNetworks();
+          addCellularNetworks();
           return flushAsync().then(() => {
             assertEquals(3, internetSubpage.networkStateList_.length);
             const toggle = internetSubpage.$$('#deviceEnabledButton');
@@ -254,6 +264,19 @@ suite('InternetSubpage', function() {
             assertEquals(3, cellularNetworkList.networks.length);
             const tetherToggle = internetSubpage.$$('#tetherEnabledButton');
             assertFalse(!!tetherToggle);
+          });
+        });
+
+    // Regression test for https://crbug.com/1182406.
+    test(
+        'Cellular subpage with no networks w/ updatedCellularActivationUi flag',
+        function() {
+          initSubpage(true /* isUpdatedCellularUiEnabled */);
+          addCellularNetworks([] /* networks */);
+          return flushAsync().then(() => {
+            const cellularNetworkList =
+                internetSubpage.$$('#cellularNetworkList');
+            assertTrue(!!cellularNetworkList);
           });
         });
 
