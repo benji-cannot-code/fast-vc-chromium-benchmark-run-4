cb88c8ddc6857a7d2b35bcfa80e2e32b11985fcc
Regan Hsu <hsuregan@google.com>
2021-02-26 00:03:43 +0000

    [CrOS Settings] Show all IMEs in Polymer3 add-input-methods-dialog.
    Previously, not all IMEs were showing in the add-input-methods-dialog in
    Polymer3 but not Polymer2.
    
    This is likely due to CrScrollableBehavior misbehaving in Polymer3
    when there are 2 iron-lists with 2 different scroll targets inside a
    CrDialog.
    
    This CL removes the scroll targets and the CrScrollableBehavior in
    add-input-methods-dialog, which also aren't needed when the correct
    CSS rules are applied.
    
    Tested in RTL and LTR languages - in Polymer2 and Polymer3.
    
    Fixed: 1182172
    Change-Id: Ia883d35930923600029c1b85ad38107dda69b5ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720700
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#857922}

--- chrome/browser/resources/settings/chromeos/os_languages_page/add_input_methods_dialog.js
+++ chrome/browser/resources/settings/chromeos/os_languages_page/add_input_methods_dialog.js
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'os-settings-add-input-methods-dialog',
 
-  behaviors: [
-    CrScrollableBehavior,
-  ],
-
   properties: {
     /** @type {!LanguagesModel|undefined} */
     languages: Object,
