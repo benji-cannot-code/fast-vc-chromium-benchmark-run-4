078a0204e074bf8f8d79949ebdb8c947b6cf7990
Matt Reynolds <mattreynolds@google.com>
2021-02-27 01:29:26 +0000

    hid: Populate HID device bus type on Linux/ChromeOS
    Previously, the bus type was always set to USB. This CL
    parses the BUS ID member of the HID_ID tuple to determine
    how the device is connected. Only USB and Bluetooth are
    currently recognized. Devices that use an unrecognized
    BUS ID will use the newly-added kHIDBusTypeUnknown enum
    value.
    
    Bug: 443335
    Change-Id: I2ebcceb1046df9b3ecc51ffb02394927c35d9d47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2683173
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858370}

--- services/device/hid/hid_service_linux.cc
+++ services/device/hid/hid_service_linux.cc
@@ -173,6 +173,20 @@ const char* GetPhysicalDeviceId(udev_device* hidraw_device) {
   return hid_sysfs_path;
 }
 
+// Convert from a Linux |bus_id| (defined in linux/input.h) to a
+// mojom::HidBusType.
+mojom::HidBusType BusTypeFromLinuxBusId(uint16_t bus_id) {
+  switch (bus_id) {
+    case BUS_USB:
+      return mojom::HidBusType::kHIDBusTypeUSB;
+    case BUS_BLUETOOTH:
+      return mojom::HidBusType::kHIDBusTypeBluetooth;
+    default:
+      break;
+  }
+  return mojom::HidBusType::kHIDBusTypeUnknown;
+}
+
 }  // namespace
 
 struct HidServiceLinux::ConnectParams {
@@ -252,6 +266,12 @@ class HidServiceLinux::BlockingTaskRunnerHelper : public UdevWatcher::Observer {
       return;
 
     uint32_t int_property = 0;
+    if (!HexStringToUInt(base::StringPiece(parts[0]), &int_property) ||
+        int_property > std::numeric_limits<uint16_t>::max()) {
+      return;
+    }
+    auto bus_type = BusTypeFromLinuxBusId(int_property);
+
     if (!HexStringToUInt(base::StringPiece(parts[1]), &int_property) ||
         int_property > std::numeric_limits<uint16_t>::max()) {
       return;
@@ -292,9 +312,7 @@ class HidServiceLinux::BlockingTaskRunnerHelper : public UdevWatcher::Observer {
 
     auto device_info = base::MakeRefCounted<HidDeviceInfo>(
         platform_device_id, physical_device_id, vendor_id, product_id,
-        product_name, serial_number,
-        // TODO(reillyg): Detect Bluetooth. crbug.com/443335
-        mojom::HidBusType::kHIDBusTypeUSB,
+        product_name, serial_number, bus_type,
         std::vector<uint8_t>(report_descriptor_str.begin(),
                              report_descriptor_str.end()),
         device_node);
