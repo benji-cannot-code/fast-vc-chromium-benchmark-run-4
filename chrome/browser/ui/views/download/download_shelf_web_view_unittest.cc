3cd5d3627d10d1019100eb68b636b5f6a3b55379
Roman Arora <romanarora@chromium.org>
2021-02-27 00:25:12 +0000

    Download Bar: Initial feature flag commit
    Add a placeholder WebUI download shelf web view implementation.
    To enable: --enable-features=WebUIDownloadShelf
    
    Bug: 1182529
    Change-Id: I15f83e50c7e62bfed8c88e9015376d06cb29c41c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716514
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858347}

---
+++ chrome/browser/ui/views/download/download_shelf_web_view_unittest.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/download/download_shelf_web_view.h"
+
+#include "chrome/test/base/browser_with_test_window_test.h"
+#include "chrome/test/views/chrome_test_widget.h"
+
+using DownloadShelfWebViewTest = BrowserWithTestWindowTest;
+
+TEST_F(DownloadShelfWebViewTest, BorderSet) {
+  views::Widget::InitParams params(views::Widget::InitParams::TYPE_CONTROL);
+  params.context = GetContext();
+  params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
+  ChromeTestWidget widget;
+  widget.Init(std::move(params));
+  DownloadShelfWebView* view =
+      widget.SetContentsView(std::make_unique<DownloadShelfWebView>(browser()));
+  EXPECT_NE(view->GetBorder(), nullptr);
+}
