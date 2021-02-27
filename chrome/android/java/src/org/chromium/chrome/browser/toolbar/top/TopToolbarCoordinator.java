84ab57d631f87115d1450efe584c2d98b321940f
spdonghao <spdonghao@chromium.org>
2021-02-27 00:45:51 +0000

    Remove public toolbar container view visibility setting.
    This CL does two things:
    1. Replaces setToolbarContainerVisibility() with changing
    mToolbarLayout's visibility in TopToolbarCoordinator.java.
    2. With enterAlways in layout_scrollFlags of top_toolbar_placeholder in
    tasks_view_layout.xml, top_toolbar_placeholder is shown as a white patch
    when scrolling up, which is not needed. We only need it to be snap
    scrolled. So this CL removes enterAlways.
    Please see demo:
    https://drive.google.com/file/d/1vYYsHDOtAJvJGsrfoe_yrdNB9ivzYXWM/view?usp=sharing&resourcekey=0-jB9YjOh3biwh3dNH7DbIcA
    
    Bug: 1136111
    Change-Id: I1ea8043451166cae64c8f01aef13f0a0df8197df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2716432
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858355}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/TopToolbarCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/TopToolbarCoordinator.java
@@ -643,7 +643,7 @@ public class TopToolbarCoordinator implements Toolbar {
             return;
         }
         mStartSurfaceToolbarCoordinator.onStartSurfaceStateChanged(newState, requestToShow);
-        updateToolbarContainerVisibility();
+        updateToolbarLayoutVisibility();
     }
 
     /**
@@ -653,16 +653,15 @@ public class TopToolbarCoordinator implements Toolbar {
     public void onStartSurfaceHeaderOffsetChanged(int verticalOffset) {
         if (mStartSurfaceToolbarCoordinator != null) {
             mStartSurfaceToolbarCoordinator.onStartSurfaceHeaderOffsetChanged(verticalOffset);
-            updateToolbarContainerVisibility();
+            updateToolbarLayoutVisibility();
         }
     }
 
-    private void updateToolbarContainerVisibility() {
+    private void updateToolbarLayoutVisibility() {
         if (mStartSurfaceToolbarCoordinator != null) {
-            boolean shouldHideToolbarContainer =
-                    mStartSurfaceToolbarCoordinator.shouldHideToolbarContainer(getHeight());
-            mControlContainer.setToolbarContainerVisibility(
-                    shouldHideToolbarContainer ? View.INVISIBLE : View.VISIBLE);
+            boolean shouldHideToolbarLayout =
+                    mStartSurfaceToolbarCoordinator.shouldHideToolbarLayout(getHeight());
+            mToolbarLayout.setVisibility(shouldHideToolbarLayout ? View.INVISIBLE : View.VISIBLE);
         }
     }
 
