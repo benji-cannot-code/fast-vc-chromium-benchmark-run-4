17cd713307bf4f4aa70f992e2bd3a969e86ad066
Yu Han <yuzhehan@chromium.org>
2021-03-01 08:13:54 +0000

    Reland 2 [Controls Refresh Android] Fix thin border for form controls: button, input, progress, etc.""
    This is a reland of 33bfc24cdea05733d794710c418d218c8e6ee7c9
    
    There was another issue with form_controls_browsertest on tester bot,
    https://ci.chromium.org/p/chromium/builders/ci/android-pie-x86-rel.
    These has been fixed by increasing the error parameters on the
    fuzzy comparators. I've checked all other android bots to ensure
    none others have failures.
    
    Since this bot is run in luci.chromium.ci, I can't schedule it.
    However, I ran android-pie-x86-rel on luci.chromium.try to ensure
    these form controls tests don't fail.
    
    
    Original change's description:
    > Reland "[Controls Refresh Android] Fix thin border for form controls: button, input, progress, etc."
    >
    > This is a reland of c50c499441f7b448d55def02d9db9c47f243900d
    >
    > The reason for the revert was that this CL causes failures on some of
    > the testers bots. They have been addressed in the following:
    > 1. linux-lacros-tester-rel.  Added BUILDFLAG(IS_CHROMEOS_LACROS) check
    >    to ensure the correct expected PNGs are compared.
    > 2. Mac10.11-Mac10.14 tester bots failure. This was due to additional
    >    text that resulted in too much difference for pixel match. This CL
    >    removed most of the text and bump up the error limits on Mac.
    >    Note: mac_chromium_[10.11 | 10.12]_rel_ng are not due to this CL but
    >    other systemic failures.
    >    In addition, added a check to prevent running dark mode tests on
    >    MacOS <= 10.14.
    > 3. Win7 Tests: Failure caused by font rendering diff between Win7 and
    >    Win10. Prevent test Button from running in Win7.
    >
    > Original change's description:
    > > [Controls Refresh Android] Fix thin border for form controls: button, input, progress, etc.
    > >
    > > Prior to this CL, form controls' border does not respect zoom and
    > > DSF if they are painted with native theme. This resulted in inconsistent
    > > border on highDPI display between controls. Form controls that were
    > > painted in blink, <meter> and multi-select, have a consistent border
    > > appearance on highDPI with other elements like div {border: 1px solid}.
    > > While other controls--button, input, progressbar--appear to have a
    > > thin border on highDPI display.
    > >
    > > This CL fixes this issue by passing zoom level from blink to native
    > > theme and applying the zoom to form controls' border. A decision is made
    > > by Dev and UX to apply this fix to android only. We felt a general fix
    > > for all platform introduces side effects that needs further analysis,
    > > follow-up crbug.com/1179509.
    > >
    > >
    > >
    > > Bug: 1160104
    > > Change-Id: Ied25eca8b39375dbc00de9d09132439787006c3c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2694095
    > > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > > Reviewed-by: Avi Drissman <avi@chromium.org>
    > > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > > Commit-Queue: Yu Han <yuzhehan@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#856492}
    >
    > Bug: 1160104 1181215
    > Change-Id: I85cc2441b2370a90303f34498ae8502034f6ed37
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2714445
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Yu Han <yuzhehan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#857878}
    
    Bug: 1160104 1181215 1182645
    Change-Id: I3e7805ff62633910413a7d37abe62233c3d5abdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2723613
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Yu Han <yuzhehan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858490}

---
+++ content/browser/form_controls_browsertest_mac.mm
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/form_controls_browsertest_mac.h"
+
+bool MacOSVersionSupportsDarkMode() {
+  if (@available(macOS 10.14, *))
+    return true;
+  return false;
+}
