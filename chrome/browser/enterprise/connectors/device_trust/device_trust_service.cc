c043cede55f7dc6c7aa84d0a34c29dc169000165
Martin Rodriguez <rodmartin@google.com>
2021-03-01 11:15:24 +0000

    Add logic for Device Trust - Key pair
    Add a key pair generation for Device Trust Connector. This key pair
    will be storage in user prefs. For the private key, an ecryptd version
    is stored using `OSCrypt`.
    Method for exporting public key in PEM format is also added.
    
    Bug: b/172933342
    Change-Id: I98baa8f9c64a5dc3ef6f19ff9c068f310c1d8b69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2648048
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Martin Rodriguez <rodmartin@google.com>
    Cr-Commit-Position: refs/heads/master@{#858520}

--- chrome/browser/enterprise/connectors/device_trust/device_trust_service.cc
+++ chrome/browser/enterprise/connectors/device_trust/device_trust_service.cc
@@ -4,14 +4,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/enterprise/connectors/device_trust/device_trust_service.h"
+
+#include "base/base64.h"
+#include "base/logging.h"
+#include "base/values.h"
+#include "chrome/browser/enterprise/connectors/connectors_prefs.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/pref_names.h"
 
 namespace policy {
 
 DeviceTrustService::DeviceTrustService() = default;
 
 DeviceTrustService::DeviceTrustService(Profile* profile)
-    : profile_(profile), prefs_(profile_->GetPrefs()) {}
+    : profile_(profile), prefs_(profile_->GetPrefs()) {
+  if (prefs_->FindPreference(
+          enterprise_connectors::kContextAwareAccessSignalsAllowlistPref) &&
+      prefs_->HasPrefPath(
+          enterprise_connectors::kContextAwareAccessSignalsAllowlistPref))
+    key_pair_ = std::make_unique<DeviceTrustKeyPair>(profile_);
+}
 
 DeviceTrustService::~DeviceTrustService() = default;
 
--- chrome/browser/enterprise/connectors/device_trust/device_trust_service.h
+++ chrome/browser/enterprise/connectors/device_trust/device_trust_service.h
@@ -6,7 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_DEVICE_TRUST_SERVICE_H_
 #define CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_DEVICE_TRUST_SERVICE_H_
 
+#include "chrome/browser/enterprise/connectors/device_trust/device_trust_key_pair.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/policy/core/browser/configuration_policy_handler.h"
+#include "components/prefs/pref_registry_simple.h"
+#include "components/prefs/pref_service.h"
 
 class Profile;
 class PrefService;
@@ -27,6 +31,7 @@ class DeviceTrustService : public KeyedService {
 
   Profile* profile_;
   PrefService* prefs_;
+  std::unique_ptr<DeviceTrustKeyPair> key_pair_;
 };
 
 }  // namespace policy
