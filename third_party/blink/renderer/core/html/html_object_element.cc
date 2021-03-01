9c610927bb8db4e1acb02148cd9e75a89f60ec1e
Mason Freed <masonfreed@chromium.org>
2021-03-01 19:04:28 +0000

    Remove "usemap" attribute from <object>
    This previously just made the <object> element interactive, but
    no actual usemap-type behavior was implemented. See [1] and [2]
    for the effort to remove <object usemap> from the spec.
    
    If this CL causes a site bug, please make sure to file a bug
    on crbug.com/new and cc me.
    
    [1] https://github.com/whatwg/html/pull/6283
    [2] https://github.com/web-platform-tests/wpt/pull/27184
    
    Fixed: 1182910
    Change-Id: I0c7317300b38636cd1144ec776f680a5cd1d6b13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2725443
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858634}

--- third_party/blink/renderer/core/html/html_object_element.cc
+++ third_party/blink/renderer/core/html/html_object_element.cc
@@ -302,8 +302,6 @@ void HTMLObjectElement::ChildrenChanged(const ChildrenChange& change) {
 bool HTMLObjectElement::IsURLAttribute(const Attribute& attribute) const {
   return attribute.GetName() == html_names::kCodebaseAttr ||
          attribute.GetName() == html_names::kDataAttr ||
-         (attribute.GetName() == html_names::kUsemapAttr &&
-          attribute.Value()[0] != '#') ||
          HTMLPlugInElement::IsURLAttribute(attribute);
 }
 
@@ -403,10 +401,6 @@ void HTMLObjectElement::DidMoveToNewDocument(Document& old_document) {
 
 HTMLFormElement* HTMLObjectElement::formOwner() const {
   return ListedElement::Form();
-}
-
-bool HTMLObjectElement::IsInteractiveContent() const {
-  return FastHasAttribute(html_names::kUsemapAttr);
 }
 
 bool HTMLObjectElement::UseFallbackContent() const {
