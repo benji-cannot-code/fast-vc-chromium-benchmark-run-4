946574de5d3eae562eaac530bf0f88a780785d38
Lambros Lambrou <lambroslambrou@chromium.org>
2021-03-01 05:46:30 +0000

    Reland "[remoting] Add GN switch for WebRTC debug logs."
    This is a reland of f790c4cee9ed4cac32c8141bc64d33f18602b61d
    
    Original change's description:
    > [remoting] Add GN switch for WebRTC debug logs.
    >
    > In the host process, the WebRTC "VERBOSE1" logs are extremely spammy,
    > and they cannot be filtered separately from "INFO" messages using --v or
    > --vmodule switches (the third_party/webrtc_overrides implementation sets
    > them to the same log verbosity level).
    >
    > To make these logs optional and easy to enable, this CL provides a
    > GN argument "remoting_webrtc_verbose_logging" with a default value of
    > false. Because of the huge amount of logs generated, this is set to
    > false even for Debug builds.
    >
    > This CL also replaces AppendSwitch(..) with the corrected call to
    > AppendSwitchASCII(). The former, when passed "key=value", would set a
    > command-line switch literally named "key=value", whose value was an
    > empty string. So the WebRTC debug logs were never enabled even in Debug
    > builds.
    >
    > Change-Id: I2a3b5c0130f4bb004b1ebf7433fc5097513b6559
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2725024
    > Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    > Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#858408}
    
    Change-Id: Id3f54cc9cb6a24bc2214e6d64e3ef485ac591516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2726819
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858477}

--- remoting/host/logging_mac.cc
+++ remoting/host/logging_mac.cc
@@ -89,6 +89,8 @@ bool LogMessageToAsl(
 }  // namespace
 
 void InitHostLogging() {
+  InitHostLoggingCommon();
+
   // Write logs to the system debug log.
   logging::LoggingSettings settings;
   settings.logging_dest =
