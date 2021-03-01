a75c7ff23a841c383f0cd4b9c907db417e56535f
jlebel <jlebel@chromium.org>
2021-03-01 14:38:54 +0000

    [iOS] Reset PasswordCheckObserverBridge from SettingsTableViewController
    Reset _passwordCheckObserver in -[SettingsTableViewController
    settingsWillBeDismissed]. This is to make sure the observer is removed
    before IOSChromePasswordCheckManager is deallocated.
    
    Related to: crrev.com/c/2357989
    
    Fixed: 1182793
    Change-Id: Ia257d82f9c3c530b95ec92e2b244e02e7e349f78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2723472
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858553}

--- ios/chrome/browser/passwords/ios_chrome_password_check_manager_unittest.mm
+++ ios/chrome/browser/passwords/ios_chrome_password_check_manager_unittest.mm
@@ -296,6 +296,7 @@ TEST_F(IOSChromePasswordCheckManagerTest,
       kExampleCom, kUsername1, base::TimeDelta::FromMinutes(1),
       InsecureType::kPhished));
   RunUntilIdle();
+  manager().RemoveObserver(&observer);
 }
 
 // Tests whether adding and removing an observer works as expected.
@@ -319,6 +320,7 @@ TEST_F(IOSChromePasswordCheckManagerTest, NotifyObserversAboutStateChanges) {
   static_cast<BulkLeakCheckServiceInterface::Observer*>(&manager())
       ->OnStateChanged(BulkLeakCheckServiceInterface::State::kRunning);
   RunUntilIdle();
+  manager().RemoveObserver(&observer);
 }
 
 // Tests password deleted.
@@ -445,4 +447,5 @@ TEST_F(IOSChromePasswordCheckManagerTest, CheckFinishedWithDelay) {
   EXPECT_CALL(observer, PasswordCheckStatusChanged(PasswordCheckState::kIdle))
       .Times(1);
   FastForwardBy(base::TimeDelta::FromSeconds(1));
+  manager().RemoveObserver(&observer);
 }
