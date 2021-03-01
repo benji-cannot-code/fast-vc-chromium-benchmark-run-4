8d3d0e7b1cc0561b2f0fd45fb8055e8c38630b2e
Sky Malice <skym@chromium.org>
2021-03-01 22:22:06 +0000

    Revert "[Start] Set empty tab UserData before the Tab is loading its URL"
    This reverts commit d7f2431b9612b39320fee7fbf359f662380ae180.
    
    Reason for revert: Failing test-n-phone see https://ci.chromium.org/ui/p/chrome/builders/ci/test-n-phone/12240/overview
    
    Original change's description:
    > [Start] Set empty tab UserData before the Tab is loading its URL
    >
    > When OMNIBOX_FOCUSED_ON_NEW_TAB is enabled, the Start surface with the
    > singe Tab variation will create an empty Tab, but the Omnibox isn't
    > focused. The bug was caused by setting the focus-on-omnibox UserData is
    > too late. When ToolbarManager checks its value, it hasn't been set. In
    > this CL, we add an API OverviewNTPCreator#onTabWillLoadUrl() which
    > allows the caller of createNewTab() update the UserData right after Tab
    > is created, and before its URL loading.
    >
    > Bug: 1164996
    > Change-Id: I32e4743c1d24cdaf769fde004a1b094a1698a461
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720423
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    > Commit-Queue: Xi Han <hanxi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#858576}
    
    Bug: 1164996
    Change-Id: Ib450dd2bb6df16a77e195adcdc66c440d1dbc0a6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2727958
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858726}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1727,11 +1727,6 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
                     }
                     return shouldShowStart;
                 }
-
-                @Override
-                public void onTabWillLoadUrl(Tab tab, String url) {
-                    StartSurfaceConfiguration.maySetUserDataForEmptyTab(tab, url);
-                }
             };
         }
         return Pair.create(new ChromeTabCreator(this, getWindowAndroid(), getStartupTabPreloader(),
