bc23c23e579080b597e874e07a9d31ba79c8d58b
Hayato Ito <hayato@chromium.org>
2021-03-01 09:13:46 +0000

    Revert "Reland: arc::LaunchAppWithIntent with AppService LaunchAppWithIntent.""
    This reverts commit e88f966947318eb75b71981f38880a7bb543d679.
    
    Reason for revert: http://crbug.com/1183256
    
    the following tests are consistently failing on "linux-chromeos-dbg":
    
    All/ArcAppLauncherForDefaultAppTest.AppLauncherForDefaultApps/1
    All/ArcAppLauncherForDefaultAppTest.AppLauncherForDefaultApps/0
    
    Original change's description:
    > Reland: arc::LaunchAppWithIntent with AppService LaunchAppWithIntent."
    >
    > This CL replaces arc::LaunchAppWithIntent with the AppService interface
    > LaunchAppWithIntent, so that:
    > 1. We can have 1 central place to add app launching metrics(UMA)
    > 2. We have 1 central place to record the app launching information for
    > the full restore project.
    >
    > Modify ArcAppLauncher to call the AppService interface
    > LaunchAppWithIntent to launch the app, and observe AppRegistryCache to
    > keep stack the app status.
    >
    > Add CreateIntentFromStartType to generate intent using activity and
    > start type.
    >
    > Modify mojom to add start type to the intent mojom structure.
    >
    > Modify unit tests to verify ArcAppLauncher.
    >
    > Fix the test issue. ArcAppLauncher calls the AppService mojom launch
    > interface to launch the apps, so adding FlushMojoCallsForAppService
    > before checking the result.
    >
    > Original change's description:
    > > > Replace arc::LaunchAppWithIntent with AppService LaunchAppWithIntent.
    > > >
    > > > BUG=1146900
    > > > BUG=1154891
    > > >
    > > > Change-Id: I761189bdd75266701b4b2697500626049dfd1057
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2711227
    > > > Reviewed-by: Long Cheng <lgcheng@google.com>
    > > > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > > > Reviewed-by: Maggie Cai <mxcai@chromium.org>
    > > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > > Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#857975}
    > >
    > > Bug: 1146900
    > > Bug: 1154891
    > > Change-Id: I82e9445270c488966a69e1c20a4592eea3b20483
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2719696
    > > Auto-Submit: Jiewei Qian  <qjw@chromium.org>
    > > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Cr-Commit-Position: refs/heads/master@{#858028}
    >
    > Bug: 1146900
    > Bug: 1154891
    > Change-Id: I03a002a329e4abc5610fd7ff29c718d9f112d712
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2719946
    > Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#858473}
    
    Bug: 1146900
    Bug: 1154891
    Change-Id: Ie30365a322b685e87e16a685aa8d9a54950a76fb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2726021
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858497}

--- chrome/browser/apps/app_service/intent_util.cc
+++ chrome/browser/apps/app_service/intent_util.cc
@@ -19,7 +19,6 @@ namespace {
 
 constexpr char kIntentExtraText[] = "android.intent.extra.TEXT";
 constexpr char kIntentExtraSubject[] = "android.intent.extra.SUBJECT";
-constexpr char kIntentExtraStartType[] = "org.chromium.arc.start_type";
 
 const char* GetArcIntentAction(const std::string& action) {
   if (action == apps_util::kIntentActionMain) {
@@ -70,10 +69,6 @@ base::flat_map<std::string, std::string> CreateArcIntentExtras(
     extras.insert(
         std::make_pair(kIntentExtraSubject, intent->share_title.value()));
   }
-  if (intent->start_type.has_value()) {
-    extras.insert(
-        std::make_pair(kIntentExtraStartType, intent->start_type.value()));
-  }
   return extras;
 }
 
@@ -93,8 +88,7 @@ arc::mojom::IntentInfoPtr CreateArcIntent(
   if (intent->url.has_value()) {
     arc_intent->data = intent->url->spec();
   }
-  if (intent->share_text.has_value() || intent->share_title.has_value() ||
-      intent->start_type.has_value()) {
+  if (intent->share_text.has_value() || intent->share_title.has_value()) {
     arc_intent->extras = CreateArcIntentExtras(intent);
   }
   return arc_intent;
