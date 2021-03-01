77eebbb2cc60b2fc6b5e37e93b3230e907db0444
Rouslan Solomakhin <rouslan@chromium.org>
2021-03-01 22:08:01 +0000

    [Web Payment] Prohibit CSP violations in payment method names.
    Bug: 1013080
    Change-Id: I4912e789da57ff107d28624d238603f3e80f766b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720162
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858721}

--- third_party/blink/renderer/modules/payments/payment_request.cc
+++ third_party/blink/renderer/modules/payments/payment_request.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/dom/events/event_queue.h"
 #include "third_party/blink/renderer/core/event_type_names.h"
+#include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/frame_owner.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
@@ -700,6 +701,16 @@ void ValidateAndConvertPaymentMethodData(
             "or \"requestPayerPhone\" options.");
         return;
       }
+    }
+
+    KURL url(payment_method_data->supportedMethod());
+    if (url.IsValid() &&
+        !execution_context.GetContentSecurityPolicy()->AllowConnectToSource(
+            url, url, RedirectStatus::kNoRedirect)) {
+      exception_state.ThrowRangeError(
+          payment_method_data->supportedMethod() +
+          " payment method identifier violates Content Security Policy.");
+      return;
     }
 
     method_names.insert(payment_method_data->supportedMethod());
