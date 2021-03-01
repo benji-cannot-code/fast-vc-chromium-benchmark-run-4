385a672955337e5b5129c2118b5cf69a879129e8
Xianzhu Wang <wangxianzhu@chromium.org>
2021-03-01 21:11:25 +0000

    Check for null/inactive/no-current-time for animation->timeline()
    I can't reproduce the bug, and don't understand the code and why my CL
    (in cc) was blamed for the bug, but the bug clearly indicates that
    animation->timeline() was null when the crash happened.
    
    Also moved the DCHECK conditions into 'if' because the conditions seem
    to happen in real world.
    
    Bug: 1182531
    Change-Id: I781a2bee270249200aecbca9966babea0c665f05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2722976
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858690}

--- third_party/blink/renderer/core/animation/pending_animations.cc
+++ third_party/blink/renderer/core/animation/pending_animations.cc
@@ -165,8 +165,7 @@ void PendingAnimations::NotifyCompositorAnimationStarted(
       waiting_for_compositor_animation_start_.push_back(animation);
       continue;
     }
-    if (animation->timeline() &&
-        !animation->timeline()->IsMonotonicallyIncreasing()) {
+    if (!animation->timeline()->IsMonotonicallyIncreasing()) {
       animation->NotifyReady(
           animation->timeline()->CurrentTime().value_or(AnimationTimeDelta()));
     } else {
@@ -201,9 +200,8 @@ void PendingAnimations::FlushWaitingNonCompositedAnimations() {
   for (auto& animation : animations) {
     if (animation->HasActiveAnimationsOnCompositor()) {
       waiting_for_compositor_animation_start_.push_back(animation);
-    } else {
-      DCHECK(animation->timeline()->IsActive() &&
-             animation->timeline()->CurrentTime());
+    } else if (animation->timeline() && animation->timeline()->IsActive() &&
+               animation->timeline()->CurrentTime().has_value()) {
       animation->NotifyReady(animation->timeline()->CurrentTime().value());
     }
   }
