c2cfbdf0c043d4dea6245a6c55c8158f4d2d0a9c
Jan Wilken Dörrie <jdoerrie@chromium.org>
2021-03-01 22:23:57 +0000

    [base] Change string16 to std::u16string everywhere
    This change modifies base::string16 to be an alias of std::u16string
    on all platforms. base::char16, blink::WebUChar and the STRING16_LITERAL
    macro are modified accordingly.
    
    Bug: 911896
    Change-Id: I15aec7b9f50288291219426857e8a1b5d93ad852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1776026
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858728}

--- base/strings/string_piece.h
+++ base/strings/string_piece.h
@@ -80,8 +80,6 @@ BASE_EXPORT size_t find_last_not_of(StringPiece16 self,
                                     StringPiece16 s,
                                     size_t pos);
 
-// Overloads for WStringPiece in case it is not the same type as StringPiece16.
-#if !defined(WCHAR_T_IS_UTF16)
 BASE_EXPORT size_t find(WStringPiece self, WStringPiece s, size_t pos);
 BASE_EXPORT size_t rfind(WStringPiece self, WStringPiece s, size_t pos);
 BASE_EXPORT size_t find_first_of(WStringPiece self, WStringPiece s, size_t pos);
@@ -92,7 +90,6 @@ BASE_EXPORT size_t find_last_of(WStringPiece self, WStringPiece s, size_t pos);
 BASE_EXPORT size_t find_last_not_of(WStringPiece self,
                                     WStringPiece s,
                                     size_t pos);
-#endif
 
 }  // namespace internal
 
@@ -566,10 +563,7 @@ constexpr bool operator>=(std::common_type_t<BasicStringPiece<StringT>> lhs,
 
 BASE_EXPORT std::ostream& operator<<(std::ostream& o, StringPiece piece);
 BASE_EXPORT std::ostream& operator<<(std::ostream& o, StringPiece16 piece);
-
-#if !defined(WCHAR_T_IS_UTF16)
 BASE_EXPORT std::ostream& operator<<(std::ostream& o, WStringPiece piece);
-#endif
 
 // Hashing ---------------------------------------------------------------------
 
