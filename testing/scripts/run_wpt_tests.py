ab3bfc1403eef88fc7f983df3fd63894ae48cd18
Luke Zielinski <lpz@chromium.org>
2021-03-02 16:56:53 +0000

    Update processing order of metadata builder.
    It now does the following:
    1. copies checked-in metadata to the output directory
    2. [optionally] loads baselines and looks for subtest fails or harness
    errors.
    3. loads combined statuses from Expectations files in standard blinkpy
    order.
    
    Normally, data from #2 and #3 are merged together and clobber whatever
    was copied in #1. The exception is Skips which overwrite any other
    statuses found in #2 and #3. This is called "translated" metadata.
    
    However, after this change, if an Expectation is annotated with
    'wpt_use_checked_in_metadata' in #3 then the statuses from #2 and #3
    are discarded and #1 is used. This is called "checked-in" metadata.
    
    This CL also annotates all tests from identity and input bots to use the
    'wpt_use_checked_in_metadata' annotation. This uncovered a couple cases
    where TestExpectations was covering test failures, and we generated
    real metadata for these tests.
    
    Additionally, if we ever use "translated" metadata, we always tell
    wptrunner to ignore subtest results, because the translation is unable
    to capture this level of granularity.
    
    Bug: 1157848
    Change-Id: Ibcabb3a304822d406457515bfe026003e44b62f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2692751
    Commit-Queue: Luke Z <lpz@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859023}

--- testing/scripts/run_wpt_tests.py
+++ testing/scripts/run_wpt_tests.py
@@ -157,7 +157,6 @@ class WPTTestAdapter(wpt_common.BaseWptScriptAdapter):
             "--checked-in-metadata-dir",
             WPT_CHECKED_IN_METADATA_DIR,
             "--no-process-baselines",
-            "--no-handle-annotations"
         ])
 
 
