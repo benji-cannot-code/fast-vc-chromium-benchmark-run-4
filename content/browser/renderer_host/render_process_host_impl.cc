cc613c0b353772a6fc3c4a26c78a38f6f614fdc4
Rakina Zata Amni <rakina@chromium.org>
2021-03-02 02:04:35 +0000

    Plumb kDisableBackForwardCache command line to the renderer
    Without this, IsBackForwardCacheDisabledByCommandLine() will always
    return false when called by the renderer (e.g when setting Blink's
    BackForwardCache flag). This CL also changes calls to
    DeviceHasEnoughMemoryForBackForwardCache() to call
    IsBackForwardCacheEnabled() instead which accounts for the command line
    flag.
    
    Bug: 511340
    Change-Id: I16d3240a72f4e98e167fc6c2c05f1af4d84b3054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717847
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858828}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -3233,6 +3233,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableYUVImageDecoding,
     switches::kDisableAXMenuList,
     switches::kDisableAcceleratedVideoDecode,
+    switches::kDisableBackForwardCache,
     switches::kDisableBackgroundTimerThrottling,
     switches::kDisableBestEffortTasks,
     switches::kDisableBreakpad,
