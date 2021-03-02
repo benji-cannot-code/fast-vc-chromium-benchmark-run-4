ac8f050e170d4360bb97ad9a4fbf4a23a5724f4d
Stephan Hartmann <stha09@googlemail.com>
2021-03-02 09:24:40 +0000

    IWYU: include missing cstring for strlen
    Bug: None
    Change-Id: Ib38cbcb18f57a3eb8a29cb205fda920412addd63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2727717
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#858938}

--- components/translate/core/language_detection/ngram_hash_ops_utils.cc
+++ components/translate/core/language_detection/ngram_hash_ops_utils.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/translate/core/language_detection/ngram_hash_ops_utils.h"
 
+#include <cstring>
 #include <vector>
 
 #include "third_party/utf/src/include/utf.h"
