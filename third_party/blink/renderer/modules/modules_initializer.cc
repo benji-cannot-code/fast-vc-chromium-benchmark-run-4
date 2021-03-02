fedbe700f93df0dca5d8701a2ee01cd464397a6e
Omer Katz <omerkatz@chromium.org>
2021-03-02 09:43:35 +0000

    Fix includes
    The CL in [1] broke the build in [2].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2682205
    [2] https://ci.chromium.org/p/chromium/builders/ci/linux-blink-v8-oilpan
    
    No-try: true
    Bug: 1126758, 1178670
    Change-Id: I40bd175bc749f722511da4640664278145d998ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2727334
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858943}

--- third_party/blink/renderer/modules/modules_initializer.cc
+++ third_party/blink/renderer/modules/modules_initializer.cc
@@ -96,7 +96,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webgpu/gpu_canvas_context.h"
 #include "third_party/blink/renderer/modules/worklet/animation_and_paint_worklet_thread.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
-#include "third_party/blink/renderer/platform/heap/impl/persistent.h"
+#include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/mojo/mojo_helper.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/widget/frame_widget.h"
