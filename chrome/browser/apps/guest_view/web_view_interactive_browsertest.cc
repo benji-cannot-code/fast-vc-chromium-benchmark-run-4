d6890deb10031f296dabdd7ba5a5dad4f2baaf0c
Sven Zheng <svenzheng@google.com>
2021-03-02 23:36:22 +0000

    Fix WebViewInteractiveTests which caused swarming timed out
    When the condition can't meet, this loop will never end.
    This caused bot to use all the remaining time to run the test.
    See
    https://ci.chromium.org/p/chromium/builders/try/win10_chromium_x64_rel_ng/780745
    https://ci.chromium.org/p/chromium/builders/try/win10_chromium_x64_rel_ng/780783
    
    Bug: 1176000
    Change-Id: I24c63c200a3cdf1effdfd1adac047ecb3145d735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2729756
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859173}

--- chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
@@ -361,7 +361,11 @@ class WebViewInteractiveTest : public extensions::PlatformAppBrowserTest {
 
     ~PopupCreatedObserver() = default;
 
-    void Wait() {
+    void Wait(int wait_retry_left = 10) {
+      if (wait_retry_left <= 0) {
+        LOG(ERROR) << "Wait failed";
+        return;
+      }
       if (CountWidgets() == initial_widget_count_ + 1 &&
           last_render_widget_host_->GetView()->GetNativeView()) {
         gfx::Rect popup_bounds =
@@ -375,7 +379,7 @@ class WebViewInteractiveTest : public extensions::PlatformAppBrowserTest {
 
       // If we haven't seen any new widget or we get 0 size widget, we need to
       // schedule waiting.
-      ScheduleWait();
+      ScheduleWait(wait_retry_left - 1);
 
       if (!message_loop_.get()) {
         message_loop_ = new content::MessageLoopRunner;
@@ -391,10 +395,11 @@ class WebViewInteractiveTest : public extensions::PlatformAppBrowserTest {
     }
 
    private:
-    void ScheduleWait() {
+    void ScheduleWait(int wait_retry_left) {
       base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
           FROM_HERE,
-          base::BindOnce(&PopupCreatedObserver::Wait, base::Unretained(this)),
+          base::BindOnce(&PopupCreatedObserver::Wait, base::Unretained(this),
+                         wait_retry_left),
           base::TimeDelta::FromMilliseconds(200));
     }
 
