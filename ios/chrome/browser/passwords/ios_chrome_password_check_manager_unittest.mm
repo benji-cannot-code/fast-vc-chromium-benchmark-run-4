259a5b5afa5a5676dca6f4004f9155d9e6058d48
jlebel <jlebel@chromium.org>
2021-03-02 13:44:12 +0000

    [iOS] Remove extra lines in IOSChromePasswordCheckManagerTest
    Removing extra |manager().RemoveObserver(&observer);| added in
     * NotifyObserversAboutCompromisedCredentialChanges
     * NotifyObserversAboutStateChanges
    
    Related to: crrev.com/c/2727327
    
    Bug: 1182793
    Change-Id: I0b7a53e3cfcac4a5388a875636f6d15cb17cf63a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2727700
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858978}

--- ios/chrome/browser/passwords/ios_chrome_password_check_manager_unittest.mm
+++ ios/chrome/browser/passwords/ios_chrome_password_check_manager_unittest.mm
@@ -296,7 +296,6 @@ TEST_F(IOSChromePasswordCheckManagerTest,
       kExampleCom, kUsername1, base::TimeDelta::FromMinutes(1),
       InsecureType::kPhished));
   RunUntilIdle();
-  manager().RemoveObserver(&observer);
 }
 
 // Tests whether adding and removing an observer works as expected.
@@ -320,7 +319,6 @@ TEST_F(IOSChromePasswordCheckManagerTest, NotifyObserversAboutStateChanges) {
   static_cast<BulkLeakCheckServiceInterface::Observer*>(&manager())
       ->OnStateChanged(BulkLeakCheckServiceInterface::State::kRunning);
   RunUntilIdle();
-  manager().RemoveObserver(&observer);
 }
 
 // Tests password deleted.
