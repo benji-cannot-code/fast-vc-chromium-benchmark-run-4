e00d3e7d98eea2215b9cca03f6a514d93a8f4842
Peter Kasting <pkasting@chromium.org>
2021-03-02 00:15:58 +0000

    Rework overlay scrollbar colors to build in alpha.
    This simplifies computations, fixes a small issue where the stroke alpha
    was incorrect for the views version (0.6 * 0.7 = 0.42 instead of the
    intended 0.5), and will allow correcting the colors for dark mode.
    
    Bug: none
    Change-Id: I914d8798b0a95151fb6be8e0e0247efbd0689f45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2724172
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858783}

--- ui/native_theme/native_theme_color_id.h
+++ ui/native_theme/native_theme_color_id.h
@@ -83,8 +83,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   OP(kColorId_LinkDisabled),                                                   \
   OP(kColorId_LinkEnabled),                                                    \
   OP(kColorId_LinkPressed),                                                    \
-  OP(kColorId_OverlayScrollbarThumbBackground),                                \
-  OP(kColorId_OverlayScrollbarThumbForeground),                                \
+  /* Overlay scrollbar */                                                      \
+  OP(kColorId_OverlayScrollbarThumbFill),                                      \
+  OP(kColorId_OverlayScrollbarThumbHoveredFill),                               \
+  OP(kColorId_OverlayScrollbarThumbHoveredStroke),                             \
+  OP(kColorId_OverlayScrollbarThumbStroke),                                    \
   /* Notification view */                                                      \
   OP(kColorId_NotificationDefaultBackground),                                  \
   OP(kColorId_NotificationActionsRowBackground),                               \
