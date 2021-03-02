f92f91a41cee01f9326e1857d85038bf8d332241
John Palmer <jopalmer@chromium.org>
2021-03-02 02:03:54 +0000

    Add some metrics to autocorrect to understand why the revert rate is so low
    Revert rate through assistive autocorrect is super low https://uma.googleplex.com/p/chrome/timeline_v2?sid=52fdc328068673df85327d94825fc030
    
    
    Add some metrics to try to understand what people are doing.
    
    BUG=b/180249516
    
    Change-Id: Id8ccb786e5b46f5718eb42b20ea3e63da261f54d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703297
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858827}

--- chrome/browser/chromeos/input_method/autocorrect_manager.cc
+++ chrome/browser/chromeos/input_method/autocorrect_manager.cc
@@ -30,7 +30,9 @@ enum class AutocorrectActions {
   kUnderlined = 1,
   kReverted = 2,
   kUserAcceptedAutocorrect = 3,
-  kMaxValue = kUserAcceptedAutocorrect,
+  kUserActionClearedUnderline = 4,
+  kUserExitedTextFieldWithUnderline = 5,
+  kMaxValue = kUserExitedTextFieldWithUnderline,
 };
 
 bool IsCurrentInputMethodExperimentalMultilingual() {
@@ -87,7 +89,9 @@ void AutocorrectManager::HandleAutocorrect(gfx::Range autocorrect_range,
 
   original_text_ = original_text;
   key_presses_until_underline_hide_ = kKeysUntilAutocorrectWindowHides;
-  ClearUnderline();
+  if (!input_context->GetAutocorrectRange().is_empty()) {
+    ClearUnderline();
+  }
 
   input_context->SetAutocorrectRange(autocorrect_range);
   LogAssistiveAutocorrectAction(AutocorrectActions::kUnderlined);
@@ -132,6 +136,9 @@ void AutocorrectManager::ClearUnderline() {
   if (input_context && !input_context->GetAutocorrectRange().is_empty()) {
     input_context->SetAutocorrectRange(gfx::Range());
     LogAssistiveAutocorrectAction(AutocorrectActions::kUserAcceptedAutocorrect);
+  } else {
+    LogAssistiveAutocorrectAction(
+        AutocorrectActions::kUserActionClearedUnderline);
   }
 }
 
@@ -174,6 +181,12 @@ void AutocorrectManager::OnSurroundingTextChanged(const base::string16& text,
 }
 
 void AutocorrectManager::OnFocus(int context_id) {
+  if (key_presses_until_underline_hide_ > 0) {
+    // TODO(b/149796494): move this to onblur()
+    LogAssistiveAutocorrectAction(
+        AutocorrectActions::kUserExitedTextFieldWithUnderline);
+    key_presses_until_underline_hide_ = -1;
+  }
   context_id_ = context_id;
 }
 
