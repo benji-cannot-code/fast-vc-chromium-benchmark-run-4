8d31ddbc95b88389033e89e10b31f2cdd48850f4
Benoit Lize <lizeb@chromium.org>
2021-03-02 12:51:41 +0000

    [PartitionAlloc] Report thread cache hit rate with PA-E.
    It was previously only reported for the FastMalloc partition when
    PartitionAlloc is not malloc. Move the reporting code to
    malloc_dump_provider.cc to report it more broadly.
    
    Bug: 998048
    Change-Id: Ia1cd422ff76dca811383074b205e62cb55da6997
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2722001
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: ssid <ssid@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858968}

--- base/trace_event/malloc_dump_provider.cc
+++ base/trace_event/malloc_dump_provider.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/allocator_extension.h"
 #include "base/allocator/buildflags.h"
 #include "base/debug/profiler.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "base/trace_event/traced_value.h"
 #include "build/build_config.h"
@@ -76,13 +77,13 @@ void ReportPartitionAllocStats(ProcessMemoryDump* pmd, bool detailed) {
   if (allocator_dumper.stats().has_thread_cache) {
     const auto& stats = allocator_dumper.stats().all_thread_caches_stats;
     auto* thread_cache_dump = pmd->CreateAllocatorDump("malloc/thread_cache");
-    ReportPartitionAllocThreadCacheStats(thread_cache_dump, stats);
+    ReportPartitionAllocThreadCacheStats(thread_cache_dump, stats, "");
     const auto& main_thread_stats =
         allocator_dumper.stats().current_thread_cache_stats;
     auto* main_thread_cache_dump =
         pmd->CreateAllocatorDump("malloc/thread_cache/main_thread");
     ReportPartitionAllocThreadCacheStats(main_thread_cache_dump,
-                                         main_thread_stats);
+                                         main_thread_stats, ".MainThread");
   }
 
   // Not reported in UMA, detailed dumps only.
@@ -241,7 +242,8 @@ void MallocDumpProvider::DisableMetrics() {
 
 #if BUILDFLAG(USE_PARTITION_ALLOC)
 void ReportPartitionAllocThreadCacheStats(MemoryAllocatorDump* dump,
-                                          const ThreadCacheStats& stats) {
+                                          const ThreadCacheStats& stats,
+                                          const std::string& metrics_suffix) {
   dump->AddScalar("alloc_count", "scalar", stats.alloc_count);
   dump->AddScalar("alloc_hits", "scalar", stats.alloc_hits);
   dump->AddScalar("alloc_misses", "scalar", stats.alloc_misses);
@@ -257,6 +259,19 @@ void ReportPartitionAllocThreadCacheStats(MemoryAllocatorDump* dump,
 
   dump->AddScalar("size", "bytes", stats.bucket_total_memory);
   dump->AddScalar("metadata_overhead", "bytes", stats.metadata_overhead);
+
+  if (stats.alloc_count) {
+    int hit_rate_percent =
+        static_cast<int>((100 * stats.alloc_hits) / stats.alloc_count);
+    base::UmaHistogramPercentage(
+        "Memory.PartitionAlloc.ThreadCache.HitRate" + metrics_suffix,
+        hit_rate_percent);
+    int batch_fill_rate_percent =
+        static_cast<int>((100 * stats.batch_fill_count) / stats.alloc_count);
+    base::UmaHistogramPercentage(
+        "Memory.PartitionAlloc.ThreadCache.BatchFillRate" + metrics_suffix,
+        batch_fill_rate_percent);
+  }
 }
 #endif  // BUILDFLAG(USE_PARTITION_ALLOC)
 
