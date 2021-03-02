da3a5ad13bfc87afd2600109ddc8524fe4ebaf65
Joel Hockey <joelhockey@chromium.org>
2021-03-02 10:12:16 +0000

    Remove expired MediaNotificationsCounter flag
    Bug: 1111881
    Change-Id: I99a9ff2ce81254a3fde523bf39fbfef1e778dd3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2725409
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#858946}

--- ash/system/message_center/message_center_utils_unittest.cc
+++ ash/system/message_center/message_center_utils_unittest.cc
@@ -6,11 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/message_center/message_center_utils.h"
 
 #include "ash/media/media_notification_constants.h"
-#include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/vm_camera_mic_constants.h"
 #include "ash/test/ash_test_base.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/scoped_feature_list.h"
 #include "ui/message_center/message_center.h"
 
 namespace ash {
@@ -34,44 +32,14 @@ void AddNotification(const std::string& notification_id,
 
 }  // namespace
 
-class MessageCenterUtilsTest : public AshTestBase,
-                               public testing::WithParamInterface<bool> {
- public:
-  MessageCenterUtilsTest() = default;
-  MessageCenterUtilsTest(const MessageCenterUtilsTest&) = delete;
-  MessageCenterUtilsTest& operator=(const MessageCenterUtilsTest&) = delete;
-  ~MessageCenterUtilsTest() override = default;
+using MessageCenterUtilsTest = AshTestBase;
 
-  // AshTestBase:
-  void SetUp() override {
-    AshTestBase::SetUp();
-    scoped_feature_list_.InitWithFeatureState(
-        features::kMediaNotificationsCounter,
-        IsMediaNotificationsCounterEnabled());
-  }
-
-  bool IsMediaNotificationsCounterEnabled() { return GetParam(); }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
-};
-
-INSTANTIATE_TEST_SUITE_P(
-    All,
-    MessageCenterUtilsTest,
-    testing::Bool() /* IsMediaNotificationsCounterEnabled() */);
-
-TEST_P(MessageCenterUtilsTest, TotalNotificationCount) {
+TEST_F(MessageCenterUtilsTest, TotalNotificationCount) {
   EXPECT_EQ(0u, GetNotificationCount());
 
   // VM camera/mic notifications are ignored by the counter.
   AddNotification("0", kVmCameraMicNotifierId);
   EXPECT_EQ(0u, GetNotificationCount());
-
-  AddNotification("1", kMediaSessionNotifierId);
-  // Counter should ignore media notifications when feature is enabled.
-  size_t expected_count = IsMediaNotificationsCounterEnabled() ? 0u : 1u;
-  EXPECT_EQ(expected_count, GetNotificationCount());
 }
 
 }  // namespace message_center_utils
