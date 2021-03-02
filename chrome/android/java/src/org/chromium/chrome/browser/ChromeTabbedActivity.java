2c7727c85a68bf17f53a99fc50b8ca6ae375960d
Yashar Dabiran <yashard@chromium.org>
2021-03-02 21:25:57 +0000

    [Paint Preview] Remove PaintPreviewWindowAndroidHelper class
    This CL removes the helper class in StartupPaintPreviewHelper that kept
    the activity-specific variables and moves all the variables to
    StartupPaintPreviewHelper instead.
    UnownedUserDataSupplier is used to supply the StartupPaintPreviewHelper
    objects per activity.
    
    Bug: 1157482
    Change-Id: If0bd603f051ae42a56b3f786e60cd2c8ad45ba62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2584367
    Commit-Queue: Yashar Dabiran <yashard@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859125}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -52,6 +52,7 @@ import org.chromium.base.supplier.OneShotCallback;
 import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.base.supplier.OneshotSupplierImpl;
 import org.chromium.base.supplier.Supplier;
+import org.chromium.base.supplier.UnownedUserDataSupplier;
 import org.chromium.base.task.PostTask;
 import org.chromium.cc.input.BrowserControlsState;
 import org.chromium.chrome.R;
@@ -116,6 +117,7 @@ import org.chromium.chrome.browser.night_mode.WebContentsDarkModeController;
 import org.chromium.chrome.browser.ntp.NewTabPageUma;
 import org.chromium.chrome.browser.omnibox.OmniboxFocusReason;
 import org.chromium.chrome.browser.paint_preview.StartupPaintPreviewHelper;
+import org.chromium.chrome.browser.paint_preview.StartupPaintPreviewHelperSupplier;
 import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -299,6 +301,9 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
     private boolean mOverviewShownOnStart;
 
     private NextTabPolicySupplier mNextTabPolicySupplier;
+
+    private final UnownedUserDataSupplier<StartupPaintPreviewHelper>
+            mStartupPaintPreviewHelperSupplier = new StartupPaintPreviewHelperSupplier();
 
     private final OneshotSupplierImpl<LayoutStateProvider> mLayoutStateProviderOneshotSupplier =
             new OneshotSupplierImpl<>();
@@ -1513,6 +1518,8 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
         supportRequestWindowFeature(Window.FEATURE_ACTION_MODE_OVERLAY);
 
         IncognitoTabHostRegistry.getInstance().register(mIncognitoTabHost);
+
+        mStartupPaintPreviewHelperSupplier.attach(getWindowAndroid().getUnownedUserDataHost());
     }
 
     @Override
@@ -1587,14 +1594,17 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
 
         mInactivityTracker = new ChromeInactivityTracker(
                 ChromePreferenceKeys.TABBED_ACTIVITY_LAST_BACKGROUNDED_TIME_MS_PREF);
+
         assert getActivityTabStartupMetricsTracker() != null;
-        StartupPaintPreviewHelper.initialize(getWindowAndroid(), getOnCreateTimestampMs(),
-                getBrowserControlsManager(), getTabModelSelector(),
-                shouldShowTabSwitcherOnStart(), () -> {
-                    return getToolbarManager() == null
-                            ? null
-                            : getToolbarManager().getProgressBarCoordinator();
-                }, getActivityTabStartupMetricsTracker()::pagePreviewRendered);
+        if (StartupPaintPreviewHelper.isEnabled()) {
+            mStartupPaintPreviewHelperSupplier.set(new StartupPaintPreviewHelper(getWindowAndroid(),
+                    getOnCreateTimestampMs(), getBrowserControlsManager(), getTabModelSelector(),
+                    shouldShowTabSwitcherOnStart(), () -> {
+                        return getToolbarManager() == null
+                                ? null
+                                : getToolbarManager().getProgressBarCoordinator();
+                    }, getActivityTabStartupMetricsTracker()::pagePreviewRendered));
+        }
     }
 
     @Override
@@ -2238,6 +2248,10 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
 
         if (mStartSurfaceSupplier.get() != null) {
             mStartSurfaceSupplier.get().destroy();
+        }
+
+        if (mStartupPaintPreviewHelperSupplier != null) {
+            mStartupPaintPreviewHelperSupplier.destroy();
         }
 
         IncognitoTabHostRegistry.getInstance().unregister(mIncognitoTabHost);
