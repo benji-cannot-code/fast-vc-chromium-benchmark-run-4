1536b225fb9b3abce6ee16d40ece6a86c62ed788
Eric Seckler <eseckler@chromium.org>
2021-03-02 20:20:54 +0000

    tracing: Extract a superclass for tracing unit tests
    In an attempt to deflake some of the tracing tests, this superclass
    tries to set up and clean up PerfettoTracedProcess and the client
    library task runner consistently.
    
    This doesn't cover unit tests for ConsumerHost, which requires a
    more complicated threading setup.
    
    Also addresses another flake in TEDS's startup tracing test.
    
    Bug: 1166230
    Change-Id: I4c82d4a97d3be293e8461871a3a97ac3e660c1d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2729046
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859098}

--- services/tracing/tracing_service_unittest.cc
+++ services/tracing/tracing_service_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
-#include "base/test/task_environment.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/threading/thread.h"
 #include "build/build_config.h"
@@ -42,34 +41,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/perfetto/protos/perfetto/trace/trace_packet.pbzero.h"
 
 namespace tracing {
-namespace {
 
-RebindableTaskRunner* GetPerfettoTaskRunner() {
-  static base::NoDestructor<RebindableTaskRunner> task_runner;
-  return task_runner.get();
-}
-
-}  // namespace
-
-class TracingServiceTest : public testing::Test {
+class TracingServiceTest : public TracingUnitTest {
  public:
-  TracingServiceTest() : service_(&perfetto_service_) {
-    // Since Perfetto's platform backend can only be initialized once in a
-    // process, we give it a task runner that can outlive the per-test task
-    // environment.
-    auto* perfetto_task_runner = GetPerfettoTaskRunner();
-    auto* perfetto_platform =
-        PerfettoTracedProcess::Get()->perfetto_platform_for_testing();
-    if (!perfetto_platform->did_start_task_runner())
-      perfetto_platform->StartTaskRunner(perfetto_task_runner);
-    perfetto_task_runner->set_task_runner(base::ThreadTaskRunnerHandle::Get());
+  TracingServiceTest() : service_(&perfetto_service_) {}
 
-    // Also tell PerfettoTracedProcess to use the current task environment.
-    PerfettoTracedProcess::ResetTaskRunnerForTesting(
-        base::ThreadTaskRunnerHandle::Get());
+  void SetUp() override {
+    TracingUnitTest::SetUp();
     perfetto_service()->SetActiveServicePidsInitialized();
   }
-  ~TracingServiceTest() override = default;
+
+  void TearDown() override { TracingUnitTest::TearDown(); }
 
  protected:
   PerfettoService* perfetto_service() { return &perfetto_service_; }
@@ -131,7 +113,6 @@ class TracingServiceTest : public testing::Test {
   }
 
  private:
-  base::test::TaskEnvironment task_environment_;
   PerfettoService perfetto_service_;
   TracingService service_;
 
