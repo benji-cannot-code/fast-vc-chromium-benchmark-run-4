252dac56bf8409b29de21d049cda7677357bd462
Adam Langley <agl@chromium.org>
2021-03-02 00:37:17 +0000

    webauthn: require U2F signatures be valid ASN.1
    U2F public keys are always ECDSA P-256, so we know what they should look
    like. Reject any that are ill-formed.
    
    Some security keys are generating invalid signatures (ASN.1 INTEGER
    encoding is usually the culprit) and such signatures will now be
    rejected. But they would have been rejected by sites that are parsing
    ASN.1 correctly anyway. Also, these bugs generally happen for about
    1/128 assertions so retrying will solve the problem.
    
    BUG=1179077
    
    Change-Id: Icb03a68fb60a3288459b723d777ce2aa08542b38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2698353
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#858802}

--- device/fido/authenticator_get_assertion_response.cc
+++ device/fido/authenticator_get_assertion_response.cc
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "components/cbor/values.h"
 #include "components/cbor/writer.h"
+#include "components/device_event_log/device_event_log.h"
 #include "device/fido/authenticator_data.h"
 #include "device/fido/fido_parsing_utils.h"
+#include "third_party/boringssl/src/include/openssl/ecdsa.h"
 
 namespace device {
 
@@ -50,6 +52,15 @@ AuthenticatorGetAssertionResponse::CreateFromU2fSignResponse(
 
   auto signature =
       fido_parsing_utils::Materialize(u2f_data.subspan(kSignatureIndex));
+
+  bssl::UniquePtr<ECDSA_SIG> parsed_sig(
+      ECDSA_SIG_from_bytes(signature.data(), signature.size()));
+  if (!parsed_sig) {
+    FIDO_LOG(ERROR)
+        << "Rejecting U2F assertion response with invalid signature";
+    return base::nullopt;
+  }
+
   AuthenticatorGetAssertionResponse response(std::move(authenticator_data),
                                              std::move(signature));
   response.SetCredential(PublicKeyCredentialDescriptor(
