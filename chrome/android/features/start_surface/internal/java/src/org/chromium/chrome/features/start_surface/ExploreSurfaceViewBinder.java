8cb54c08a73dedb5f222528a49ec67937792afef
Xi Han <hanxi@google.com>
2021-03-03 23:05:08 +0000

    [Start] Fix java.lang.NullPointerException at FeedStream.getView(FeedStream.java)
    Bug: 1184388
    Change-Id: I0e61f83bb433df5d60b50dea8f0d39d4b86f6cb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2733269
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859568}

--- chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/ExploreSurfaceViewBinder.java
+++ chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/ExploreSurfaceViewBinder.java
@@ -19,6 +19,7 @@ import android.widget.FrameLayout;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.chromium.chrome.browser.feed.FeedSurfaceCoordinator;
 import org.chromium.ui.UiUtils;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -84,10 +85,10 @@ class ExploreSurfaceViewBinder {
     }
 
     private static void resetScrollPosition(PropertyModel model) {
-        if (model.get(FEED_SURFACE_COORDINATOR) == null) return;
+        FeedSurfaceCoordinator feedLoadingCoordinator = model.get(FEED_SURFACE_COORDINATOR);
+        if (feedLoadingCoordinator == null || feedLoadingCoordinator.getStream() == null) return;
 
-        RecyclerView feedStreamView =
-                (RecyclerView) model.get(FEED_SURFACE_COORDINATOR).getStream().getView();
+        RecyclerView feedStreamView = (RecyclerView) feedLoadingCoordinator.getStream().getView();
         if (feedStreamView != null) {
             feedStreamView.scrollToPosition(0);
         }
