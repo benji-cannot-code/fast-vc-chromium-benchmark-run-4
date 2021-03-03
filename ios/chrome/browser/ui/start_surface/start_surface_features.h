0bbe99a2ca7f0dee753abc3d82390a93c622ff5f
Chris Lu <thegreenfrog@chromium.org>
2021-03-03 18:43:08 +0000

    [ios] Add Bling Start Experiment Util methods and implement one group
    These util methods will be useful to check for experiment group
    assignment. Group Variations are added to the chrome://flags
    dropdown options.
    
    Content Suggestions hide shortcuts group is implemented in this CL.
    
    Bug: 1173160
    Change-Id: I31a0ee05237526223780022b2ecb89160845d961
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2702282
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859463}

--- ios/chrome/browser/ui/start_surface/start_surface_features.h
+++ ios/chrome/browser/ui/start_surface/start_surface_features.h
@@ -8,6 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 
+// Start Surface experiment params.
+extern const char kStartSurfaceHideShortcutsParam[];
+extern const char kStartSurfaceShrinkLogoParam[];
+extern const char kStartSurfaceReturnToRecentTabParam[];
+
 // The feature to enable or disable the Start Surface.
 extern const base::Feature kStartSurface;
 
@@ -20,5 +25,16 @@ bool IsStartSurfaceEnabled();
 
 // Returns the inactive duration to show the Start Surface.
 double GetReturnToStartSurfaceDuration();
+
+// Returns true if the shortcuts should be hidden on NTP for the Start Surface
+bool ShouldHideShortcutsForStartSurface();
+
+// Returns true if the Google logo should be shrunk on NTP for the Start
+// Surface.
+bool ShouldShrinkLogoForStartSurface();
+
+// Returns true if the most recent tab tile should be created on NTP for the
+// Start Surface.
+bool ShouldShowReturnToMostRecentTabForStartSurface();
 
 #endif  // IOS_CHROME_BROWSER_UI_START_SURFACE_START_SURFACE_FEATURES_H_.
