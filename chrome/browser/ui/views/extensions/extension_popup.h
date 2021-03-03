29c6152d9f56958761e7e1ccde0797cb856b00c1
Alex Ilin <alexilin@chromium.org>
2021-03-03 11:57:44 +0000

    [CodeHealth] Remove NOTIFICATION_LOAD_COMPLETED_MAIN_FRAME in ExtensionPopup
    The notification system is deprecated.
    
    ExtensionPopup listens for NOTIFICATION_LOAD_COMPLETED_MAIN_FRAME coming
    from particular WebContents. This can be directly replaced with
    observing this WebContents via
    WebContentsObserver::DocumentOnLoadCompletedInMainFrame().
    
    Bug: 1174761
    Change-Id: I062b00b5062d2c135f85ad072b8ba6ec20cc14a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2727824
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859326}

--- chrome/browser/ui/views/extensions/extension_popup.h
+++ chrome/browser/ui/views/extensions/extension_popup.h
@@ -14,8 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/views/extensions/extension_view_views.h"
 #include "content/public/browser/devtools_agent_host_observer.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extension_host_observer.h"
 #include "extensions/browser/extension_registry.h"
@@ -48,7 +47,7 @@ class ExtensionPopup : public views::BubbleDialogDelegateView,
 #endif
                        public ExtensionViewViews::Container,
                        public extensions::ExtensionRegistryObserver,
-                       public content::NotificationObserver,
+                       public content::WebContentsObserver,
                        public TabStripModelObserver,
                        public content::DevToolsAgentHostObserver,
                        public extensions::ExtensionHostObserver {
@@ -108,10 +107,8 @@ class ExtensionPopup : public views::BubbleDialogDelegateView,
                            const extensions::Extension* extension,
                            extensions::UnloadedExtensionReason reason) override;
 
-  // content::NotificationObserver:
-  void Observe(int type,
-               const content::NotificationSource& source,
-               const content::NotificationDetails& details) override;
+  // content::WebContentsObserver:
+  void DocumentOnLoadCompletedInMainFrame() override;
 
   // TabStripModelObserver:
   void OnTabStripModelChanged(
@@ -154,8 +151,6 @@ class ExtensionPopup : public views::BubbleDialogDelegateView,
       extension_registry_observation_{this};
 
   ShowAction show_action_;
-
-  content::NotificationRegistrar registrar_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSION_POPUP_H_
