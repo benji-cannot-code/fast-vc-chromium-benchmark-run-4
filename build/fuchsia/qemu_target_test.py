ab85024f8c9abe35a881aeef66a929a8b021ef14
Stephen Roe <steveroe@google.com>
2021-03-03 00:34:04 +0000

    [fuchsia] Change /usr/bin/python references to /usr/bin/python2.
    The /usr/bin/python link to /usr/bin/python2 will be removed
    from gLinux/Rodete.
    
    Bug: 1182059
    Change-Id: I33fe8623533b88fee0609309b1ec5cf00e21170f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2726746
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859192}

--- build/fuchsia/qemu_target_test.py
+++ build/fuchsia/qemu_target_test.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/python
+#!/usr/bin/python2
 # Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -56,4 +56,4 @@ with qemu_target.QemuTarget(tmpdir, 'x64') as target:
 
 
   if __name__ == '__main__':
-      unittest.main()
+    unittest.main()
