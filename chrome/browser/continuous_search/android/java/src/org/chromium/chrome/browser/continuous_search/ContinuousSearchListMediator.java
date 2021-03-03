992b9d4ec65d2f4621049dbc24423dae5957d786
ckitagawa <ckitagawa@chromium.org>
2021-03-03 18:50:15 +0000

    [CSN] Update Observer Logic
    Updates the Observer to
    - Invalidate earlier.
    - Specify whether a page is a SRP page in the update.
    - More permissive SRP detection.
    
    This does not solve the redirect issue which requires more discussion.
    
    Bug: 1183495
    Change-Id: Ic540ef2c7ecaf47e122e04f8c8abaad2967656cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2727517
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859468}

--- chrome/browser/continuous_search/android/java/src/org/chromium/chrome/browser/continuous_search/ContinuousSearchListMediator.java
+++ chrome/browser/continuous_search/android/java/src/org/chromium/chrome/browser/continuous_search/ContinuousSearchListMediator.java
@@ -24,6 +24,7 @@ class ContinuousSearchListMediator implements SearchResultUserDataObserver, Call
     private final ModelList mModelList;
     private final Callback<Boolean> mSetLayoutVisibility;
     private Tab mCurrentTab;
+    private boolean mOnSrp;
     private SearchResultUserData mCurrentSearchUserData;
 
     ContinuousSearchListMediator(ModelList modelList, Callback<Boolean> setLayoutVisibility) {
@@ -64,10 +65,11 @@ class ContinuousSearchListMediator implements SearchResultUserDataObserver, Call
     public void onInvalidate() {
         mModelList.clear();
         mSetLayoutVisibility.onResult(false);
+        mOnSrp = false;
     }
 
     @Override
-    public void onUpdate(SearchResultMetadata metadata, GURL currentUrl) {
+    public void onUpdate(SearchResultMetadata metadata) {
         mModelList.clear();
 
         for (SearchResultGroup group : metadata.getGroups()) {
@@ -81,11 +83,11 @@ class ContinuousSearchListMediator implements SearchResultUserDataObserver, Call
                         itemType, generateListItem(result.getTitle(), result.getUrl())));
             }
         }
-        mSetLayoutVisibility.onResult(mModelList.size() > 0);
     }
 
     @Override
-    public void onUrlChanged(GURL currentUrl) {
+    public void onUrlChanged(GURL currentUrl, boolean onSrp) {
+        mOnSrp = onSrp;
         for (ListItem listItem : mModelList) {
             if (listItem.type == ListItemType.GROUP_LABEL) continue;
 
@@ -93,6 +95,7 @@ class ContinuousSearchListMediator implements SearchResultUserDataObserver, Call
                     && currentUrl.equals(listItem.model.get(ContinuousSearchListProperties.URL));
             listItem.model.set(ContinuousSearchListProperties.IS_SELECTED, isSelected);
         }
+        mSetLayoutVisibility.onResult(mModelList.size() > 0 && !mOnSrp);
     }
 
     private PropertyModel generateListItem(String text, GURL url) {
--- chrome/browser/continuous_search/internal/android/java/src/org/chromium/chrome/browser/continuous_search/ContinuousSearchTabObserver.java
+++ chrome/browser/continuous_search/internal/android/java/src/org/chromium/chrome/browser/continuous_search/ContinuousSearchTabObserver.java
@@ -22,6 +22,12 @@ public class ContinuousSearchTabObserver extends EmptyTabObserver implements Sea
     }
 
     @Override
+    public void onPageLoadStarted(Tab tab, GURL url) {
+        SearchResultUserData searchResultUserData = SearchResultUserData.getOrCreateForTab(tab);
+        searchResultUserData.updateCurrentUrl(url);
+    }
+
+    @Override
     public void onPageLoadFinished(Tab tab, GURL url) {
         SearchResultUserData searchResultUserData = SearchResultUserData.getOrCreateForTab(tab);
         searchResultUserData.updateCurrentUrl(url);
