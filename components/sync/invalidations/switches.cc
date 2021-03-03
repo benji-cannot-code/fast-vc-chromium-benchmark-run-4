5a60b548ca4db31fccbf68c546fef81981708487
Rushan Suleymanov <rushans@google.com>
2021-03-03 12:26:03 +0000

    [Sync] Enabled SyncSendInterestedDataTypes by default
    This CL also slightly updates a SendTabToSelfUtilTest iOS unit test to
    prevent creating real services.
    
    Bug: 1178784
    Change-Id: I97bbe93d39a1aaf5fb8089b67905424534033fd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720121
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#859332}

--- components/sync/invalidations/switches.cc
+++ components/sync/invalidations/switches.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace switches {
 
 const base::Feature kSyncSendInterestedDataTypes = {
-    "SyncSendInterestedDataTypes", base::FEATURE_DISABLED_BY_DEFAULT};
+    "SyncSendInterestedDataTypes", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kUseSyncInvalidations = {"UseSyncInvalidations",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
