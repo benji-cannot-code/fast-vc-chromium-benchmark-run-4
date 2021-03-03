031668c10c287e3436643b7af3b575c25dcd486c
Nancy Wang <nancylingwang@chromium.org>
2021-03-03 02:09:58 +0000

    Save the ARC app launch info to the full restore file.
    Add the FullRestoreArcTaskHandler class to observe ArcAppListPrefs, and
    call FullRestoreSaveHandler to update the ARC app launch info when a
    task is created or destroyed.
    
    Modify FullRestoreSaveHandler to save the task id as the window id. When
    the task id is destroyed, remove the record for the task id.
    
    TODO: Handle the ARC app launch with intent.
    
    BUG=1146900
    
    Change-Id: I8bb859f094e642cad6a5fd66ccf6d7bcf36c0865
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2725903
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859226}

--- chrome/browser/chromeos/arc/session/arc_service_launcher.cc
+++ chrome/browser/chromeos/arc/session/arc_service_launcher.cc
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/user_session/arc_user_session_service.h"
 #include "chrome/browser/chromeos/arc/video/gpu_arc_video_service_host.h"
 #include "chrome/browser/chromeos/arc/wallpaper/arc_wallpaper_service.h"
+#include "chrome/browser/chromeos/full_restore/full_restore_arc_task_handler.h"
 #include "chrome/browser/chromeos/login/startup_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/arc/arc_usb_host_permission_manager.h"
@@ -253,6 +254,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   CertStoreService::GetForBrowserContext(profile);
   apps::ArcAppsFactory::GetForProfile(profile);
   chromeos::ApkWebAppService::Get(profile);
+  chromeos::full_restore::FullRestoreArcTaskHandler::GetForProfile(profile);
 
   // ARC Container-only services.
   if (!arc::IsArcVmEnabled()) {
