e8e90234c2259fa1a9f88937da15cc9307747409
Preethi Mohan <preethim@google.com>
2021-03-03 02:03:48 +0000

    flag specific support in rebaseline-cl.
    Added the option --flag-specific=<foo>.
    Currently supporting only 'highdpi' for flag-specific.
    New experimental highdpi builder is added which gets triggered with this option.
    Results from the new builder are fetched and rebaselined under
    third_party/blink/web_tests/flag-specific/<foo>/. Other builders are ignored with this option.
    
    Rebaselined the highdpi tests using these changes.
    
    Bug: 1068400
    
    Change-Id: I5638eb225398159caa06963c9ef5f3991b1a7a3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2545863
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Preethi Mohan <preethim@google.com>
    Cr-Commit-Position: refs/heads/master@{#859222}

--- third_party/blink/tools/blinkpy/tool/commands/rebaseline_test.py
+++ third_party/blink/tools/blinkpy/tool/commands/rebaseline_test.py
@@ -23,6 +23,7 @@ class RebaselineTest(AbstractRebaseliningCommand):
             self.build_number_option,
             self.step_name_option,
             self.results_directory_option,
+            self.flag_specific_option,
         ])
 
     def execute(self, options, args, tool):
@@ -44,16 +45,21 @@ class RebaselineTest(AbstractRebaseliningCommand):
             options.builder)
         test_name = options.test
         for suffix in self._baseline_suffix_list:
-            self._rebaseline_test(
-                port_name, test_name, suffix, results_url,
-                self._tool.builders.is_wpt_builder(options.builder))
+            self._rebaseline_test(port_name,
+                                  test_name,
+                                  suffix,
+                                  results_url,
+                                  self._tool.builders.is_wpt_builder(
+                                      options.builder),
+                                  options=options)
 
     def _rebaseline_test(self,
                          port_name,
                          test_name,
                          suffix,
                          results_url,
-                         is_wpt=False):
+                         is_wpt=False,
+                         options=None):
         """Downloads a baseline file and saves it to the filesystem.
 
         Args:
@@ -64,13 +70,16 @@ class RebaselineTest(AbstractRebaseliningCommand):
                 test name and results_url this determines what file to download.
             results_url: Base URL to download the actual result from.
             is_wpt: (Optional, default to False) Whether this is a WPT builder.
+            options: (Optional, default to None) An object with the command line options.
         """
-        port = self._tool.port_factory.get(port_name)
+        port = self._tool.port_factory.get(port_name, options)
 
         # TODO(crbug.com/1154085): Undo this special case when we have WPT bots
         # on more ports.
         if is_wpt:
             baseline_directory = port.web_tests_dir()
+        elif options and options.flag_specific:
+            baseline_directory = port.baseline_flag_specific_dir()
         else:
             baseline_directory = port.baseline_version_dir()
 
--- third_party/blink/tools/blinkpy/web_tests/builder_list_unittest.py
+++ third_party/blink/tools/blinkpy/web_tests/builder_list_unittest.py
@@ -90,6 +90,12 @@ class BuilderListTest(unittest.TestCase):
                 'master': "luci",
                 'has_webdriver_tests': True
             },
+            'Flag Specific': {
+                'port_name': 'port-c',
+                'specifiers': ['C', 'Release'],
+                'flag_specific': 'highdpi',
+                "is_try_builder": True
+            },
         })
 
     def test_constructor_validates_list(self):
@@ -108,6 +114,7 @@ class BuilderListTest(unittest.TestCase):
             'CQ Try A',
             'CQ Try B',
             'CQ Try C',
+            'Flag Specific',
             'Try A',
             'Try B',
             'some-wpt-bot',
@@ -131,6 +138,12 @@ class BuilderListTest(unittest.TestCase):
         self.assertEqual(
             ['CQ Try A', 'CQ Try B', 'CQ Try C'],
             builders.all_cq_try_builder_names())
+
+    def test_all_flag_specific_builder_names(self):
+        builders = self.sample_builder_list()
+        self.assertEqual(['Flag Specific'],
+                         builders.all_flag_specific_try_builder_names(
+                             flag_specific="highdpi"))
 
     def test_all_port_names(self):
         builders = self.sample_builder_list()
