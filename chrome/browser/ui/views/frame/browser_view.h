896857b147a8b9f19e34210844fb37a9c0ebc747
Keren Zhu <kerenzhu@chromium.org>
2021-03-03 17:48:33 +0000

    Fix color access in cbui/webui
    Remove most usages of the static NativeTheme::GetInstanceForNativeUi()
    by searching for the NativeTheme of a BrowserView or a Widget
    containing the WebContents of the WebUI.
    
    There're two remaining cases:
    - When the singleton NativeTheme is used in a observer and there is no
      associating WebContents, e.g. in a KeyedService.
    - In tests where the WebContents may not be contained in a Browser or a
      Widget.
    
    Bug: 1056916
    Change-Id: I388e1308d141c4979c8f792f5ce3dddc8a27c4d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720899
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859440}

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -78,11 +78,12 @@ class TopControlsSlideControllerTest;
 class WebContentsCloseHandler;
 class WebUITabStripContainerView;
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 namespace ui {
+class NativeTheme;
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 class ThroughputTracker;
-}
 #endif
+}  // namespace ui
 
 namespace version_info {
 enum class Channel;
@@ -349,6 +350,7 @@ class BrowserView : public BrowserWindow,
                                 float ratio) override;
   bool DoBrowserControlsShrinkRendererSize(
       const content::WebContents* contents) const override;
+  ui::NativeTheme* GetNativeTheme() override;
   int GetTopControlsHeight() const override;
   void SetTopControlsGestureScrollInProgress(bool in_progress) override;
   StatusBubble* GetStatusBubble() override;
--- chrome/browser/ui/webui/ntp/ntp_resource_cache.h
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_change_registrar.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "content/public/browser/web_contents.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/native_theme/native_theme_observer.h"
 
@@ -56,7 +57,9 @@ class NTPResourceCache : public content::NotificationObserver,
 
   base::RefCountedMemory* GetNewTabGuestHTML();
   base::RefCountedMemory* GetNewTabHTML(WindowType win_type);
-  base::RefCountedMemory* GetNewTabCSS(WindowType win_type);
+  base::RefCountedMemory* GetNewTabCSS(
+      WindowType win_type,
+      const content::WebContents::Getter wc_getter);
 
   // content::NotificationObserver:
   void Observe(int type,
@@ -106,10 +109,10 @@ class NTPResourceCache : public content::NotificationObserver,
   bool NewTabHTMLNeedsRefresh();
 
   void CreateNewTabHTML();
-  void CreateNewTabCSS();
+  void CreateNewTabCSS(const content::WebContents::Getter wc_getter);
 
   void CreateNewTabIncognitoHTML();
-  void CreateNewTabIncognitoCSS();
+  void CreateNewTabIncognitoCSS(const content::WebContents::Getter wc_getter);
 
   scoped_refptr<base::RefCountedString> CreateNewTabGuestHTML(
       const GuestNTPInfo& guest_ntp_info);
