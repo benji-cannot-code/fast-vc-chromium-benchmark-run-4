a86735d877bf0c4b439a67c0200483365ede46da
Greg Thompson <grt@chromium.org>
2021-03-03 16:43:26 +0000

    Use only Chrome's version for NaCl cache invalidation.
    NaClValidationCache doesn't require the other bits of info included in
    chrome::GetVersionString, so return only the raw version number from
    NaClBrowserDelegateImpl's implementation of GetVersionString.
    
    Bug: 1177739
    Change-Id: I9be4b9709e5254268bf05cee1e9644b0c9e3b222
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2690792
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Mark Seaborn <mseaborn@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859398}

--- chrome/browser/nacl_host/nacl_browser_delegate_impl.cc
+++ chrome/browser/nacl_host/nacl_browser_delegate_impl.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/renderer_host/pepper/chrome_browser_pepper_host_factory.h"
-#include "chrome/common/channel_info.h"
+#include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_paths_internal.h"
 #include "chrome/common/logging_chrome.h"
@@ -96,7 +96,7 @@ bool NaClBrowserDelegateImpl::GetUserDirectory(base::FilePath* user_dir) {
 }
 
 std::string NaClBrowserDelegateImpl::GetVersionString() const {
-  return chrome::GetVersionString();
+  return chrome::kChromeVersion;
 }
 
 ppapi::host::HostFactory* NaClBrowserDelegateImpl::CreatePpapiHostFactory(
