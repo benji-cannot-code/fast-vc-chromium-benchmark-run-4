ca26d3ff23e240e0ba51e3b8f4f5726a59bdf5ea
Julie Jeongeun Kim <jkim@igalia.com>
2021-03-03 07:50:49 +0000

    Converts ExtensionMsg_ShouldSuspend to mojo
    This CL converts ExtensionMsg_ShouldSuspend to ShouldSuspend()
    in mojom::ShouldSuspend. Since ShouldSuspend() replies to the
    browser with a callback, it also removes
    ExtensionHostMsg_ShouldSuspendAck.
    
    Bug: 1180866
    Change-Id: I6413995347d56b1663a2c7aac1dce83a1c0cacf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2722112
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859287}

--- extensions/browser/extension_message_filter.cc
+++ extensions/browser/extension_message_filter.cc
@@ -102,7 +102,6 @@ void ExtensionMessageFilter::OverrideThreadForMessage(
     case ExtensionHostMsg_RemoveLazyServiceWorkerListener::ID:
     case ExtensionHostMsg_AddFilteredListener::ID:
     case ExtensionHostMsg_RemoveFilteredListener::ID:
-    case ExtensionHostMsg_ShouldSuspendAck::ID:
     case ExtensionHostMsg_TransferBlobsAck::ID:
     case ExtensionHostMsg_WakeEventPage::ID:
     case ExtensionHostMsg_OpenChannelToExtension::ID:
@@ -141,8 +140,6 @@ bool ExtensionMessageFilter::OnMessageReceived(const IPC::Message& message) {
                         OnExtensionAddFilteredListener)
     IPC_MESSAGE_HANDLER(ExtensionHostMsg_RemoveFilteredListener,
                         OnExtensionRemoveFilteredListener)
-    IPC_MESSAGE_HANDLER(ExtensionHostMsg_ShouldSuspendAck,
-                        OnExtensionShouldSuspendAck)
     IPC_MESSAGE_HANDLER(ExtensionHostMsg_TransferBlobsAck,
                         OnExtensionTransferBlobsAck)
     IPC_MESSAGE_HANDLER(ExtensionHostMsg_WakeEventPage,
@@ -304,16 +301,6 @@ void ExtensionMessageFilter::OnExtensionRemoveFilteredListener(
 
   GetEventRouter()->RemoveFilteredEventListener(
       event_name, process, extension_id, sw_identifier, filter, lazy);
-}
-
-void ExtensionMessageFilter::OnExtensionShouldSuspendAck(
-     const std::string& extension_id, int sequence_id) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  if (!browser_context_)
-    return;
-
-  ProcessManager::Get(browser_context_)
-      ->OnShouldSuspendAck(extension_id, sequence_id);
 }
 
 void ExtensionMessageFilter::OnExtensionTransferBlobsAck(
