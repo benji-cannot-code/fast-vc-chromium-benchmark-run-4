cf4acf300be27c96064ac7af501a7f0549e55b7d
Michael Hansen <hansenmichael@google.com>
2021-03-03 03:37:58 +0000

    [Nearby] Extend timeout for ReceiveMessagesExpress.
    Extends the network timeout for the URL request used in
    ReceiveMessagesExpress to 60s. Because the typical bandwidth
    upgrade takes more than the previous timeout of 2.5s, there were often a
    number of timeouts and retries before a successful p2p connection could
    be established. This change improves the success rate of individual
    upgrade attempts.
    
    The WebRTC medium already has timeouts for the sending and receiving
    flows ([1] and [2]) so this timeout is just a failsafe.
    
    This also adds a timer so that if we don't receive "fast path ready"
    from Tachyon within 2.5s, we fail and close the stream.
    
    1) https://source.chromium.org/chromium/chromium/src/+/master:third_party/nearby/src/cpp/core/internal/mediums/webrtc.cc;l=139;drc=133dddeccbeb3c1dcc7c69cf433a62d921f0bbf9
    2) https://source.chromium.org/chromium/chromium/src/+/master:third_party/nearby/src/cpp/core/internal/mediums/webrtc.cc;l=313;drc=133dddeccbeb3c1dcc7c69cf433a62d921f0bbf9
    
    Change-Id: I045598ebee0eb30d4d84dbbb799263b637ebbb0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2724976
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/master@{#859249}

--- chrome/browser/nearby_sharing/instantmessaging/receive_messages_express.cc
+++ chrome/browser/nearby_sharing/instantmessaging/receive_messages_express.cc
@@ -26,6 +26,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+const base::TimeDelta kFastPathReadyTimeout =
+    base::TimeDelta::FromMilliseconds(2500);
+
+// Timeout for the receive messages stream, from when the stream first opens.
+// This timeout applies to the Tachyon signaling process, so once we establish
+// the peer-to-peer connection this stream and timeout will be canceled. There
+// are other timeouts in the WebRTC medium that will cancel the signaling
+// process sooner than 60s, so this is just a failsafe to make sure we clean up
+// the ReceiveMessagesExpress if something goes wrong.
+const base::TimeDelta kStreamTimeout = base::TimeDelta::FromSeconds(60);
+
 // TODO(crbug.com/1123164) - Add nearby sharing policy when available.
 const net::NetworkTrafficAnnotationTag kTrafficAnnotation =
     net::DefineNetworkTrafficAnnotation("receive_messages_express", R"(
@@ -142,6 +153,8 @@ ReceiveMessagesExpress::~ReceiveMessagesExpress() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   NS_LOG(VERBOSE) << __func__ << ": Receive messages session going down";
 
+  fast_path_ready_timeout_timer_.Stop();
+
   if (start_receiving_messages_callback_) {
     std::move(start_receiving_messages_callback_)
         .Run(false, mojo::NullRemote());
@@ -201,14 +214,30 @@ void ReceiveMessagesExpress::DoStartReceivingMessages(
 
   url_loader_ = network::SimpleURLLoader::Create(std::move(resource_request),
                                                  kTrafficAnnotation);
-  url_loader_->SetTimeoutDuration(kNetworkTimeout);
+  url_loader_->SetTimeoutDuration(kStreamTimeout);
   url_loader_->AttachStringForUpload(request.SerializeAsString(),
                                      "application/x-protobuf");
   url_loader_->DownloadAsStream(url_loader_factory_.get(), this);
+
+  // We are safe to use base::Unretained() here because if
+  // ReceiveMessagesExpress is destroyed the timer will go out of scope first
+  // which will cancel it.
+  fast_path_ready_timeout_timer_.Start(
+      FROM_HERE, kFastPathReadyTimeout,
+      base::BindOnce(&ReceiveMessagesExpress::OnFastPathReadyTimeout,
+                     base::Unretained(this)));
+}
+
+void ReceiveMessagesExpress::OnFastPathReadyTimeout() {
+  NS_LOG(WARNING) << __func__;
+  FailSessionAndDestruct("Timeout before receiving fast path ready");
+  // |this| will be destroyed here.
+  return;
 }
 
 void ReceiveMessagesExpress::StopReceivingMessages() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  fast_path_ready_timeout_timer_.Stop();
 
   // Cancel any pending calls into this object.
   weak_ptr_factory_.InvalidateWeakPtrs();
@@ -238,6 +267,7 @@ void ReceiveMessagesExpress::OnDataReceived(base::StringPiece data,
 
 void ReceiveMessagesExpress::OnComplete(bool success) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  fast_path_ready_timeout_timer_.Stop();
   base::Optional<NearbyShareHttpStatus> http_status =
       HttpStatusFromUrlLoader(url_loader_.get());
 
@@ -268,6 +298,7 @@ void ReceiveMessagesExpress::OnRetry(base::OnceClosure start_retry) {
 
 void ReceiveMessagesExpress::OnFastPathReady() {
   NS_LOG(VERBOSE) << __func__;
+  fast_path_ready_timeout_timer_.Stop();
   if (start_receiving_messages_callback_) {
     LogReceiveResult(/*success=*/true, /*http_status=*/base::nullopt);
     std::move(start_receiving_messages_callback_)
--- chrome/browser/nearby_sharing/instantmessaging/receive_messages_express.h
+++ chrome/browser/nearby_sharing/instantmessaging/receive_messages_express.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
+#include "base/timer/timer.h"
 #include "chrome/browser/nearby_sharing/instantmessaging/stream_parser.h"
 #include "chrome/browser/nearby_sharing/instantmessaging/token_fetcher.h"
 #include "chromeos/services/nearby/public/mojom/webrtc_signaling_messenger.mojom.h"
@@ -75,6 +76,8 @@ class ReceiveMessagesExpress : public sharing::mojom::ReceiveMessagesSession,
           ReceiveMessagesExpressRequest& request,
       const std::string& oauth_token);
 
+  void OnFastPathReadyTimeout();
+
   // network::SimpleURLLoaderStreamConsumer:
   void OnDataReceived(base::StringPiece string_piece,
                       base::OnceClosure resume) override;
@@ -99,6 +102,7 @@ class ReceiveMessagesExpress : public sharing::mojom::ReceiveMessagesSession,
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
   std::unique_ptr<network::SimpleURLLoader> url_loader_;
   StreamParser stream_parser_;
+  base::OneShotTimer fast_path_ready_timeout_timer_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
