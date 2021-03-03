2547e502b9a591a8bfda0b888841293aa0012cda
Richard Knoll <knollr@chromium.org>
2021-03-03 21:31:10 +0000

    Use PendingIntentProvider for notification actions
    The web notification settings button did not specify the required
    mutability flags on Android S. This now changes the interface of
    NotificationBuilderBase to use PendingIntentProvider instead of plain
    PendingIntents so we can make sure that we always set the expected
    flags.
    
    Bug: 1163529
    Change-Id: I887823aac9bd344974af37e2300b53439a1a84cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2725521
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859542}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/CustomNotificationBuilder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/CustomNotificationBuilder.java
@@ -217,7 +217,7 @@ public class CustomNotificationBuilder extends NotificationBuilderBase {
             }
 
             view.setTextViewText(R.id.button, action.title);
-            view.setOnClickPendingIntent(R.id.button, action.intent);
+            view.setOnClickPendingIntent(R.id.button, action.intent.getPendingIntent());
             bigView.addView(R.id.buttons, view);
         }
     }
@@ -230,7 +230,7 @@ public class CustomNotificationBuilder extends NotificationBuilderBase {
             bigView.setViewPadding(R.id.origin, leftPadding, 0, rightPadding, 0);
             return;
         }
-        bigView.setOnClickPendingIntent(R.id.origin, mSettingsAction.intent);
+        bigView.setOnClickPendingIntent(R.id.origin, mSettingsAction.intent.getPendingIntent());
         bigView.setInt(R.id.origin_settings_icon, "setColorFilter", BUTTON_ICON_COLOR_MATERIAL);
     }
 
