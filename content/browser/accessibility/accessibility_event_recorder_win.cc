849c745d2854eb5b9ccf98d65c9f858089c31865
Alexander Surkov <asurkov@igalia.com>
2021-03-03 17:11:01 +0000

    ax_dump tools: warn on multiple application matches to dump on Windows
    Bug: 1177438
    Change-Id: I46a8cf0a17e4fea2a317f116ecea55cdc7b1c7c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705859
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Reviewed-by: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859423}

--- content/browser/accessibility/accessibility_event_recorder_win.cc
+++ content/browser/accessibility/accessibility_event_recorder_win.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 using ui::AccessibilityEventToString;
+using ui::GetHWNDBySelector;
 using ui::IAccessible2StateToString;
 using ui::IAccessibleRoleToString;
 using ui::IAccessibleStateToString;
