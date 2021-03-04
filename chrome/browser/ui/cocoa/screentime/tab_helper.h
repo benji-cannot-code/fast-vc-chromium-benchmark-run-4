560a44bf8d1c825b0b53366718e2eeffbaf27e6e
Elly Fong-Jones <ellyjones@chromium.org>
2021-03-04 23:12:45 +0000

    mac: never record Screentime state in Incognito/Guest
    It would defeat the leave-no-trace purpose of these features.
    
    Bug: 1184820
    Change-Id: Icfd34c38e0d337b7fec9dfa478a83a4777aa5aa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737362
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860000}

--- chrome/browser/ui/cocoa/screentime/tab_helper.h
+++ chrome/browser/ui/cocoa/screentime/tab_helper.h
@@ -8,8 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/feature_list.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
+
+class Profile;
 
 namespace content {
 class WebContents;
@@ -25,8 +28,10 @@ class WebpageController;
 class TabHelper : public content::WebContentsObserver,
                   public content::WebContentsUserData<TabHelper> {
  public:
+  static const base::Feature kScreenTime;
+
   static void UseFakeWebpageControllerForTesting();
-  static bool IsScreentimeEnabled();
+  static bool IsScreentimeEnabledForProfile(Profile* profile);
 
   TabHelper(content::WebContents* contents);
   ~TabHelper() override;
