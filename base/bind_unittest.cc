e70efecdd0ed9801377f0767e16bc69d09be5590
Daniel Cheng <dcheng@chromium.org>
2021-03-04 13:18:04 +0000

    Move implementation details of base::Bind{Once,Repeating} into bind_internal.h
    Bug: none
    Change-Id: Iba83208d74e7bc65f03c6d45c318c410bc42a96c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2727737
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859741}

--- base/bind_unittest.cc
+++ base/bind_unittest.cc
@@ -827,8 +827,9 @@ struct RepeatingTestConfig {
   using ClosureType = RepeatingClosure;
 
   template <typename F, typename... Args>
-  static CallbackType<MakeUnboundRunType<F, Args...>>
-  Bind(F&& f, Args&&... args) {
+  static CallbackType<internal::MakeUnboundRunType<F, Args...>> Bind(
+      F&& f,
+      Args&&... args) {
     return BindRepeating(std::forward<F>(f), std::forward<Args>(args)...);
   }
 };
@@ -839,8 +840,9 @@ struct OnceTestConfig {
   using ClosureType = OnceClosure;
 
   template <typename F, typename... Args>
-  static CallbackType<MakeUnboundRunType<F, Args...>>
-  Bind(F&& f, Args&&... args) {
+  static CallbackType<internal::MakeUnboundRunType<F, Args...>> Bind(
+      F&& f,
+      Args&&... args) {
     return BindOnce(std::forward<F>(f), std::forward<Args>(args)...);
   }
 };
