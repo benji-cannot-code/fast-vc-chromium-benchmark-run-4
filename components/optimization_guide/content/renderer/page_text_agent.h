feaa6aeddbcd2a9aa5ade316629fde6369d10c47
Robert Ogden <robertogden@chromium.org>
2021-03-04 01:56:34 +0000

    OptGuide PageTextAgent should use a receiver set
    Bug: 1163244
    Change-Id: Ifa1523545ac52ab25c4f805f7437e78103bc7875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2733480
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Auto-Submit: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859619}

--- components/optimization_guide/content/renderer/page_text_agent.h
+++ components/optimization_guide/content/renderer/page_text_agent.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "components/optimization_guide/content/mojom/page_text_service.mojom.h"
 #include "content/public/renderer/render_frame_observer_tracker.h"
-#include "mojo/public/cpp/bindings/associated_receiver.h"
+#include "mojo/public/cpp/bindings/associated_receiver_set.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "third_party/blink/public/web/web_meaningful_layout.h"
 
@@ -70,7 +70,7 @@ class PageTextAgent
                 mojo::PendingRemote<mojom::PageTextConsumer>>;
   std::map<mojom::TextDumpEvent, RequestAndConsumer> requests_by_event_;
 
-  mojo::AssociatedReceiver<mojom::PageTextService> receiver_{this};
+  mojo::AssociatedReceiverSet<mojom::PageTextService> receivers_;
 
   base::WeakPtrFactory<PageTextAgent> weak_factory_{this};
 };
