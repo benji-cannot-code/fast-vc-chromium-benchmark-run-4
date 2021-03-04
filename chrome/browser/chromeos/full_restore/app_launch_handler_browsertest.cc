65c26a7a08264ea401f1e12e94d69b2852925eee
François Doray <fdoray@chromium.org>
2021-03-04 15:36:13 +0000

    Revert "full_restore: Speculative fix for flaky test."
    This reverts commit e0115b80f680fbe4bad039939e46e2afd971fd4d.
    
    Reason for revert: The fix did not work.
    
    Original change's description:
    > full_restore: Speculative fix for flaky test.
    >
    > From the stack, looks like there was an issue requesting a screenshot
    > from the compositor. Was unable to repro locally, so this is just
    > speculative. Gives some time between creating desks and creates one less
    > desk.
    >
    > Bug: 1184193
    > Test: browser_tests AppLaunchHandlerBrowserTest.RestoreChromeApp
    > Change-Id: I3866a6a09e4eeeb5406426abe4c9ecc3a12b24f3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732779
    > Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#859617}
    
    Bug: 1184193, 1174588
    Change-Id: Idc7c336e973554460d93816a475395aa75bccb1c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2736235
    Auto-Submit: François Doray <fdoray@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#859775}

--- chrome/browser/chromeos/full_restore/app_launch_handler_browsertest.cc
+++ chrome/browser/chromeos/full_restore/app_launch_handler_browsertest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/autotest_desks_api.h"
-#include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
@@ -414,12 +413,10 @@ IN_PROC_BROWSER_TEST_F(AppLaunchHandlerBrowserTest, WindowProperties) {
 }
 
 IN_PROC_BROWSER_TEST_F(AppLaunchHandlerBrowserTest, RestoreChromeApp) {
-  // Have 3 desks total.
+  // Have 4 desks total.
   ash::AutotestDesksApi().CreateNewDesk();
-  base::RunLoop().RunUntilIdle();
-
   ash::AutotestDesksApi().CreateNewDesk();
-  base::RunLoop().RunUntilIdle();
+  ash::AutotestDesksApi().CreateNewDesk();
 
   ::full_restore::SetActiveProfilePath(profile()->GetPath());
 
