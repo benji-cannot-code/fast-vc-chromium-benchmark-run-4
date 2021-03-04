59e5433c2d1b7cbffa350643d25dc7a9f2bea07c
Sammie Quon <sammiequon@chromium.org>
2021-03-04 01:45:00 +0000

    full_restore: Speculative fix for flaky test.
    From the stack, looks like there was an issue requesting a screenshot
    from the compositor. Was unable to repro locally, so this is just
    speculative. Gives some time between creating desks and creates one less
    desk.
    
    Bug: 1184193
    Test: browser_tests AppLaunchHandlerBrowserTest.RestoreChromeApp
    Change-Id: I3866a6a09e4eeeb5406426abe4c9ecc3a12b24f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732779
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859617}

--- chrome/browser/chromeos/full_restore/app_launch_handler_browsertest.cc
+++ chrome/browser/chromeos/full_restore/app_launch_handler_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/autotest_desks_api.h"
+#include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
@@ -413,10 +414,12 @@ IN_PROC_BROWSER_TEST_F(AppLaunchHandlerBrowserTest, WindowProperties) {
 }
 
 IN_PROC_BROWSER_TEST_F(AppLaunchHandlerBrowserTest, RestoreChromeApp) {
-  // Have 4 desks total.
+  // Have 3 desks total.
   ash::AutotestDesksApi().CreateNewDesk();
+  base::RunLoop().RunUntilIdle();
+
   ash::AutotestDesksApi().CreateNewDesk();
-  ash::AutotestDesksApi().CreateNewDesk();
+  base::RunLoop().RunUntilIdle();
 
   ::full_restore::SetActiveProfilePath(profile()->GetPath());
 
