58ab25ca86dfb3e9853898e5ba36d8ab37547af8
Tanmoy Mollik <triploblastic@chromium.org>
2021-03-04 18:01:56 +0000

    [Signin] Remove components/signin/core/browser/android/
    This cl merges components/signin/core/browser/android with
    components/signin/public/android and removes the former folder.
    
    Bug: 1173958
    Change-Id: Ieae813953530c068ec72e0121e08d7c33e771b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720120
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859831}

--- components/signin/public/android/java/src/org/chromium/components/signin/PatternMatcher.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/PatternMatcher.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.components.signin.util;
+package org.chromium.components.signin;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +12,7 @@ import java.util.List;
  * Encapsulates simple wildcard pattern-matching.
  * See {@link PatternMatcher#PatternMatcher(String)} for the format description.
  */
-public class PatternMatcher {
+class PatternMatcher {
     /** Encapsulates information about illegal pattern. */
     public static class IllegalPatternException extends Exception {
         private IllegalPatternException(String message, String pattern) {
@@ -36,7 +36,7 @@ public class PatternMatcher {
      *
      * @param pattern the pattern to match strings against.
      */
-    public PatternMatcher(String pattern) throws IllegalPatternException {
+    PatternMatcher(String pattern) throws IllegalPatternException {
         // Split pattern by * wildcards and un-escape.
         boolean escape = false;
         StringBuilder currentChunk = new StringBuilder();
@@ -66,7 +66,7 @@ public class PatternMatcher {
      * @param string the string to match against the pattern.
      * @return whether the whole string matches the pattern.
      */
-    public boolean matches(String string) {
+    boolean matches(String string) {
         // No wildcards, the whole string should match the pattern.
         if (mChunks.size() == 1) {
             return string.equals(mChunks.get(0));
