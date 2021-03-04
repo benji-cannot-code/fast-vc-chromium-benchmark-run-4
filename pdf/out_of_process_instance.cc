0b59f48a44287aeb73af6153b1ab972d1b5fbe15
Daniel Hosseinian <dhoss@chromium.org>
2021-03-04 22:32:46 +0000

    [unseasoned-pdf] Migrate Beep()
    Migrate from a Pepper OutOfProcessInstance implementation to a common
    PdfViewPluginBase one.
    
    Bug: 1109796
    Change-Id: Ib4692debf6470746ce6ac8e39892d594aca51d0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2736955
    Auto-Submit: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Hui Yingst <nigi@chromium.org>
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859979}

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -90,8 +90,6 @@ constexpr char kType[] = "type";
 // Name of identifier field passed from JS to the plugin and back, to associate
 // Page->Plugin messages to Plugin->Page responses.
 constexpr char kJSMessageId[] = "messageId";
-// Beep message arguments. (Plugin -> Page).
-constexpr char kJSBeepType[] = "beep";
 // Document print preview loaded (Plugin -> Page)
 constexpr char kJSPreviewLoadedType[] = "printPreviewLoaded";
 // Metadata (Plugin -> Page)
@@ -1089,12 +1087,6 @@ void OutOfProcessInstance::SaveToFile(const std::string& token) {
   engine()->KillFormFocus();
   ConsumeSaveToken(token);
   pp::PDF::SaveAs(this);
-}
-
-void OutOfProcessInstance::Beep() {
-  pp::VarDictionary message;
-  message.Set(pp::Var(kType), pp::Var(kJSBeepType));
-  PostMessage(message);
 }
 
 void OutOfProcessInstance::Alert(const std::string& message) {
