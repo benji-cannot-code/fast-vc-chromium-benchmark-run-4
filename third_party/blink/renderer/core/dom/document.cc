f8cb3ec1d34b26bd287b50cf1f3194172930c9ba
Nate Chapin <japhet@chromium.org>
2021-03-04 01:26:04 +0000

    Revert "Remove DumpWithoutCrashing() in Document::BeginLifecycleUpdatesIfRenderingReady()"
    This reverts commit 45bf243c598985e0502e4e5ad4d7433b2cea7053.
    
    Reason for revert: M90 has branched, and we want this logging back on trunk.
    
    Original change's description:
    > Remove DumpWithoutCrashing() in Document::BeginLifecycleUpdatesIfRenderingReady()
    >
    > This commit will be reverted and the DumpWithoutCrashing() restored
    > after the M90 branch.
    >
    > Bug: 1178215
    > Change-Id: I800a77242dfb9303bf014789450a26dd1e2283a7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713052
    > Auto-Submit: Nate Chapin <japhet@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Nate Chapin <japhet@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#856744}
    
    Bug: 1178215
    Change-Id: Iac4fb2f4310ae9bad8981bb0d8e086013ad10eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2733615
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859610}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -6934,6 +6934,9 @@ void Document::BeginLifecycleUpdatesIfRenderingReady() {
   // Speculative fix for https://crbug.com/1171891
   if (auto* view = View()) {
     view->BeginLifecycleUpdates();
+  } else {
+    NOTREACHED();
+    base::debug::DumpWithoutCrashing();
   }
 }
 
