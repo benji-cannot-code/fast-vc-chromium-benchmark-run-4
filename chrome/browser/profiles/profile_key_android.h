65fe538ef0872b79f2dd4aa8ad89abbf8fc17b60
Sky Malice <skym@chromium.org>
2021-03-04 18:38:13 +0000

    Add SimpleFactoryKey support to Java with Handle pattern.
    While this functionality is not used yet, it will be needed before
    various background task scheduler clients (downloads) can be moved to
    //components.
    
    The approach taken to have ProfileKey extend a handle class allows it
    to mirror Profile and BrowserContextHandle's relationship, see
    https://crrev.com/c/2130674 .
    
    Bug: 1087482
    Change-Id: Ie00ddafa6a21b15a8adeaceab80f6fe2a1a4fcf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2585384
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859849}

--- chrome/browser/profiles/profile_key_android.h
+++ chrome/browser/profiles/profile_key_android.h
@@ -32,6 +32,8 @@ class ProfileKeyAndroid {
   // Whether this profile is off the record.
   jboolean IsOffTheRecord(JNIEnv* env);
 
+  jlong GetSimpleFactoryKeyPointer(JNIEnv* env);
+
   base::android::ScopedJavaLocalRef<jobject> GetJavaObject();
 
  private:
---
+++ components/embedder_support/android/java/src/org/chromium/components/embedder_support/simple_factory_key/SimpleFactoryKeyHandle.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.embedder_support.simple_factory_key;
+
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.JNINamespace;
+
+/** An interface that provides access to a native SimpleFactoryKey. */
+@JNINamespace("simple_factory_key")
+public interface SimpleFactoryKeyHandle {
+    /** @return A pointer to the native SimpleFactoryKey that this object wraps. */
+    @CalledByNative
+    long getNativeSimpleFactoryKeyPointer();
+}
