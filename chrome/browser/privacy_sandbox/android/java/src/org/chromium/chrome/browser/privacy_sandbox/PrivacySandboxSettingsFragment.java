a9ca757d6243d5390e954168495ca07ff3f0f6fc
Andrey Zaytsev <andzaytsev@google.com>
2021-03-04 16:40:30 +0000

    Privacy Sandbox Android: updated the help page URL to be retrieved from the feature param
    Bug: 1152351, 1184701
    Change-Id: I8826b08737930a750bce59451d9ce4aeeb3a744b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2735816
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859797}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsFragment.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsFragment.java
@@ -22,6 +22,7 @@ import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
 import org.chromium.base.IntentUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.settings.ChromeManagedPreferenceDelegate;
 import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
@@ -35,8 +36,9 @@ import org.chromium.ui.widget.ChromeBulletSpan;
  */
 public class PrivacySandboxSettingsFragment
         extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
-    public static final String PRIVACY_SANDBOX_URL =
-            "https://www.google.com/chrome/privacy-sandbox";
+    public static final String PRIVACY_SANDBOX_DEFAULT_URL =
+            "https://web.dev/digging-into-the-privacy-sandbox/";
+    public static final String EXPERIMENT_URL_PARAM = "website-url";
     // Key for the argument with which the PrivacySandbox fragment will be launched. The value for
     // this argument should be part of the PrivacySandboxReferrer enum, which contains all points of
     // entry to the Privacy Sandbox UI.
@@ -70,7 +72,7 @@ public class PrivacySandboxSettingsFragment
                         getContext().getString(R.string.privacy_sandbox_description),
                         new SpanInfo("<link>", "</link>",
                                 new NoUnderlineClickableSpan(getContext().getResources(),
-                                        (widget) -> openUrlInCct(PRIVACY_SANDBOX_URL)))));
+                                        (widget) -> openUrlInCct(getPrivacySandboxUrl())))));
         // Format the toggle description, which has bullet points.
         findPreference(TOGGLE_DESCRIPTION_PREFERENCE)
                 .setSummary(SpanApplier.applySpans(
@@ -121,7 +123,7 @@ public class PrivacySandboxSettingsFragment
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.menu_id_targeted_help) {
             // Action for the question mark button.
-            openUrlInCct(PRIVACY_SANDBOX_URL);
+            openUrlInCct(getPrivacySandboxUrl());
             return true;
         }
         return false;
@@ -142,6 +144,14 @@ public class PrivacySandboxSettingsFragment
             if (!TOGGLE_PREFERENCE.equals(preference.getKey())) return false;
             return PrivacySandboxBridge.isPrivacySandboxManaged();
         };
+    }
+
+    private String getPrivacySandboxUrl() {
+        // Get the URL from Finch, if defined.
+        String url = ChromeFeatureList.getFieldTrialParamByFeature(
+                ChromeFeatureList.PRIVACY_SANDBOX_SETTINGS, EXPERIMENT_URL_PARAM);
+        if (url == null || url.isEmpty()) return PRIVACY_SANDBOX_DEFAULT_URL;
+        return url;
     }
 
     private void openUrlInCct(String url) {
