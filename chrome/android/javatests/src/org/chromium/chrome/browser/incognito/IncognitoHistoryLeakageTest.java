a5278645ae57122ff44dd25324820cbdc3372014
Shakti Sahu <shaktisahu@chromium.org>
2021-03-04 20:38:16 +0000

    Disable flaky test IncognitoHistoryLeakageTest
    Disabled flaky test IncognitoHistoryLeakageTest#testBrowsingHistoryDoNotLeakFromIncognitoTabbedActivity on Android M.
    
    TBR=roagarwal@chromium.org
    
    Bug: 1184759
    Change-Id: Ica962a76b9dd8e1432a399fda750bb6b3a2caa87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737414
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859915}

--- chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoHistoryLeakageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoHistoryLeakageTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import android.content.Intent;
+import android.os.Build.VERSION_CODES;
 import android.support.test.InstrumentationRegistry;
 
 import androidx.test.filters.LargeTest;
@@ -28,6 +29,7 @@ import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.browser.customtabs.CustomTabsTestUtils;
 import org.chromium.chrome.browser.customtabs.IncognitoCustomTabActivityTestRule;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -124,7 +126,10 @@ public class IncognitoHistoryLeakageTest {
 
     @Test
     @LargeTest
-    public void testBrowsingHistoryDoNotLeakFromIncognitoTabbedActivity() throws TimeoutException {
+    @DisableIf.Build(message = "Flaky on Android M, see https://crbug.com/1184759",
+            sdk_is_greater_than = VERSION_CODES.LOLLIPOP_MR1, sdk_is_less_than = VERSION_CODES.N)
+    public void
+    testBrowsingHistoryDoNotLeakFromIncognitoTabbedActivity() throws TimeoutException {
         mChromeActivityTestRule.startMainActivityOnBlankPage();
         mChromeActivityTestRule.loadUrlInNewTab(mTestPage1, /*incognito=*/true);
         List<HistoryItem> historyEntriesOfIncognitoMode =
