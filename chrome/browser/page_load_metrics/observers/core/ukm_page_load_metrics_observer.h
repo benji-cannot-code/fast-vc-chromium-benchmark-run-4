d057c2136c9d36a6386be51bcd8070db034eb33e
Tommy Li <tommycli@chromium.org>
2021-03-04 04:12:35 +0000

    [memories] Add PageLoad.DurationSinceLastVisitSeconds UKM signal
    This CL adds the UKM signal, using the new History API to enable its
    collection.
    
    Collection Review:
    http://go/project-delorean-design-doc#heading=h.u8jhw46p4v17
    
    Bug: 1171352
    Change-Id: Ie6a836e58b2c3a27027939632ee4628320a96b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2724841
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Tom McKee <tommckee@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859642}

--- chrome/browser/page_load_metrics/observers/core/ukm_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/core/ukm_page_load_metrics_observer.h
@@ -7,8 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_PAGE_LOAD_METRICS_OBSERVERS_CORE_UKM_PAGE_LOAD_METRICS_OBSERVER_H_
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/optional.h"
+#include "base/task/cancelable_task_tracker.h"
 #include "base/time/time.h"
+#include "components/history/core/browser/history_types.h"
 #include "components/memories/core/visit_data.h"
 #include "components/page_load_metrics/browser/page_load_metrics_event.h"
 #include "components/page_load_metrics/browser/page_load_metrics_observer.h"
@@ -121,6 +124,9 @@ class UkmPageLoadMetricsObserver
   virtual bool IsOfflinePreview(content::WebContents* web_contents) const;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(UkmPageLoadMetricsObserverTest,
+                           DurationSinceLastVisitSeconds);
+
   void RecordNavigationTimingMetrics();
 
   // Records page load timing related metrics available in PageLoadTiming, such
@@ -194,6 +200,9 @@ class UkmPageLoadMetricsObserver
   // Records a score from the SiteEngagementService. Called when the page
   // becomes hidden, or at the end of the session if the page is never hidden.
   void RecordSiteEngagement() const;
+
+  // Callback for HistoryService::GetLastVisitToURL.
+  void OnURLLastVisitResult(history::HistoryLastVisitResult result);
 
   // Guaranteed to be non-null during the lifetime of |this|.
   network::NetworkQualityTracker* network_quality_tracker_;
@@ -291,12 +300,21 @@ class UkmPageLoadMetricsObserver
 
   base::ReadOnlySharedMemoryMapping ukm_smoothness_data_;
 
+  // The time that the navigation started. Used to ensure we fetch the previous
+  // last visit time, and not this one.
+  base::Time navigation_start_for_history_;
   // Collected by this observer during the page lifetime. Shipped to UKM and
   // History. Also save the URL and commit timestamp to align with History.
   GURL committed_url_;
   // Meant to correspond with the timestamp recorded in HistoryService.
   base::Time committed_history_timestamp_;
   memories::VisitContextSignals memories_signals_;
+
+  // Task tracker for calls for the history service.
+  base::CancelableTaskTracker task_tracker_;
+
+  // Used to discard callbacks that come back after the page lifetime is over.
+  base::WeakPtrFactory<UkmPageLoadMetricsObserver> weak_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(UkmPageLoadMetricsObserver);
 };
