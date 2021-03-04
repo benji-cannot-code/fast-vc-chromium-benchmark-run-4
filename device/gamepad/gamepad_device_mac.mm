64221be87a1c9e2dc050682096187bb89de09683
James Hollyer <jameshollyer@google.com>
2021-03-04 06:26:30 +0000

    Correctly map macOS Bluetooth joystick buttons for Series X Controller
    New support was just added for the Xbox Series X Controller on macOS
    over Bluetooth however that patch failed to properly map the joystick
    click buttons. This patch fixes that issue simply by mapping them to
    the proper buttons.
    
    Bug: 1170887
    Change-Id: I2ea338df8a650dea92187183e71b9f126d1e4159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2650905
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: James Hollyer <jameshollyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859663}

--- device/gamepad/gamepad_device_mac.mm
+++ device/gamepad/gamepad_device_mac.mm
@@ -36,6 +36,7 @@ const uint16_t kPowerUsageNumber = 0x30;
 const uint16_t kSearchUsageNumber = 0x0221;
 const uint16_t kHomeUsageNumber = 0x0223;
 const uint16_t kBackUsageNumber = 0x0224;
+const uint16_t kRecordUsageNumber = 0xb2;
 
 const int kRumbleMagnitudeMax = 10000;
 
@@ -53,6 +54,7 @@ struct SpecialUsages {
     // Back.
     {kConsumerUsagePage, kHomeUsageNumber},
     {kConsumerUsagePage, kBackUsageNumber},
+    {kConsumerUsagePage, kRecordUsageNumber},
 };
 const size_t kSpecialUsagesLen = base::size(kSpecialUsages);
 
