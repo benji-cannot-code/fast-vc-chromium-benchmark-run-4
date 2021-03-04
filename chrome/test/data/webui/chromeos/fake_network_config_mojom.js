ccb62a37eba160b35f7b30f0ebe7368de0076ea8
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-03-04 01:11:16 +0000

    [CrOS cellular] Move sim lock dialogs to its own UI Element
    Sim lock dialogs will be used in multiple UI elements, the entire
    functionality of network_siminfo is not required for all elements. This
    CL extracts the dialogs into a UI element of its own.
    
    Bug: 1093185,1184463
    Change-Id: I7b1f48337f5fa78ddc1ec91e1f6173643b212781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713722
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859601}

--- chrome/test/data/webui/chromeos/fake_network_config_mojom.js
+++ chrome/test/data/webui/chromeos/fake_network_config_mojom.js
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  // #import {PromiseResolver} from 'chrome://resources/js/promise_resolver.m.js';
  // clang-format on
 
+// Default cellular pin, used when locking/unlocking cellular profiles.
+/* #export */ const DEFAULT_CELLULAR_PIN = '1111';
+
 // TODO(stevenjb): Include cros_network_config.mojom.js and extend
 // CrosNetworkConfigInterface
 /* #export */ class FakeNetworkConfig {
@@ -45,6 +48,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
      * @type {!Array<!chromeos.networkConfig.mojom.CrosNetworkConfigObserver>
      */
     this.observers_ = [];
+
+    /**
+     * When updating or changing cellular pin, |testPin| is used to store
+     * the updated pin, if not set DEFAULT_CELLULAR_PIN is used to check pin
+     * value in |setCellularSimState()|
+     * @type {string}
+     */
+    this.testPin = '';
 
     this.resetForTest();
   }
@@ -93,7 +104,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     ['getNetworkState', 'getNetworkStateList', 'getDeviceStateList',
      'getManagedProperties', 'setNetworkTypeEnabledState', 'requestNetworkScan',
      'getGlobalPolicy', 'getVpnProviders', 'getNetworkCertificates',
-     'setProperties']
+     'setProperties', 'setCellularSimState']
         .forEach((methodName) => {
           this.resolverMap_.set(methodName, new PromiseResolver());
         });
@@ -324,6 +335,59 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       }
       this.methodCalled('getManagedProperties');
       resolve({result: result || null});
+    });
+  }
+
+  /**
+   * @param {!chromeos.networkConfig.mojom.CellularSimState} cellularSimState
+   * @return {!Promise<{success: boolean}>}
+   */
+  setCellularSimState(cellularSimState) {
+    return new Promise(resolve => {
+      const completeSetCellularSimState = (success) => {
+        this.methodCalled('setCellularSimState');
+        this.onDeviceStateListChanged();
+        resolve({success: success});
+      };
+
+      // This is only called by cellular networks.
+      const type = chromeos.networkConfig.mojom.NetworkType.kCellular;
+      let deviceState = this.deviceStates_.get(type);
+      let simLockStatus = deviceState.simLockStatus;
+      const pin = this.testPin ? this.testPin : DEFAULT_CELLULAR_PIN;
+
+      // If the correct pin is entered.
+      if (cellularSimState.currentPinOrPuk === pin) {
+        if (cellularSimState.newPin) {
+          // Set new pin.
+          this.testPin = cellularSimState.newPin;
+          completeSetCellularSimState(/*success*/ true);
+          return;
+        }
+
+        // toggling lock status.
+        simLockStatus.lockEnabled = !simLockStatus.lockEnabled;
+        deviceState.simLockStatus = simLockStatus;
+        this.deviceStates_.set(type, deviceState);
+        completeSetCellularSimState(/*success*/ true);
+        return;
+      }
+
+      // Wrong pin entered.
+      if (simLockStatus.retriesLeft > 1) {
+        // If there is more than one retries left.
+        simLockStatus.retriesLeft--;
+        deviceState.simLockStatus = simLockStatus;
+        this.deviceStates_.set(type, deviceState);
+        completeSetCellularSimState(/*success*/ false);
+        return;
+      }
+
+      // No retried left.
+      simLockStatus = {lockEnabled: true, lockType: 'sim-puk', retriesLeft: 0};
+      deviceState.simLockStatus = simLockStatus;
+      this.deviceStates_.set(type, deviceState);
+      completeSetCellularSimState(/*success*/ false);
     });
   }
 
