579a828590e09033c7446cdbc7ed96fd435492a1
Alexander Surkov <asurkov@igalia.com>
2021-03-04 00:26:30 +0000

    ax_dump tools: remove powershell util scripts on Windows
    These script are redundant now: now ax_dump_tree/events have native
    support of targeting applications, for example,
    ax_dump_tree --firefox will dump Firefox accessible tree.
    
    Bug: 1177438
    Change-Id: Iea2eb8eb33d258357bcba8830b2f8ea0467c8edc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2718445
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#859587}

--- tools/accessibility/inspect/ff-tree.ps1
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Powershell script to dump entire accessibility tree for Firefox, all windows + tabs.
-$hwnd = Get-Process | Where-Object {$_.ProcessName -eq 'firefox'} | where MainWindowTitle -like "*Firefox" | select MainWindowHandle -ExpandProperty MainWindowHandle | Out-String
-$hwnd_arg = "--window=" + $hwnd
-$exe = ".\ax_dump_tree.exe"
-& $exe $hwnd_arg
