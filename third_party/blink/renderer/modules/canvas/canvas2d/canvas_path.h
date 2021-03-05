43c1ee494147838c5e0795b612fb92b90f957869
Justin Novosad <junov@chromium.org>
2021-03-05 01:49:01 +0000

    Apply NoAllocDirectCall to some 2D canvas methods.
    This change takes care of all the "freebies" where NoAllocDirectCall
    can be used without changing anything to the blink code or the
    bindings generator.  More work on the bindings code is needed to
    support methods with overloads, methods that use the RaisesException
    IDL attribute, and methods that receive non-base type objects as
    arguments.
    
    BUG=1182245
    
    Change-Id: Ie67d6d5b0e6b76a2635d25446136ab5d2aea0bf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2719962
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860068}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_path.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_path.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/bindings/modules/v8/double_or_dom_point.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/bindings/no_alloc_direct_call_host.h"
 #include "third_party/blink/renderer/platform/graphics/path.h"
 #include "third_party/blink/renderer/platform/transforms/affine_transform.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -41,7 +42,7 @@ namespace blink {
 
 class ExceptionState;
 
-class MODULES_EXPORT CanvasPath {
+class MODULES_EXPORT CanvasPath : public NoAllocDirectCallHost {
   DISALLOW_NEW();
 
  public:
