7ac918dcc48af7a9cd08fbed3bccf93b52281531
Caroline Rising <corising@chromium.org>
2021-03-05 19:30:23 +0000

    Clean up tab hover card feature flag and references.
    This feature has been launched on all platforms since M87.
    
    Bug: 910739
    Change-Id: Idd71d4d25f084f24e9078bfe4853078b83d57967
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2739381
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860314}

--- chrome/browser/ui/views/tabs/tab_hover_card_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/tabs/tab_hover_card_bubble_view_browsertest.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/browser.h"
@@ -12,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
-#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/tabs/tab.h"
 #include "chrome/browser/ui/views/tabs/tab_close_button.h"
@@ -36,7 +34,6 @@ class TabHoverCardBubbleViewBrowserTest : public DialogBrowserTest {
       : animation_mode_reset_(gfx::AnimationTestApi::SetRichAnimationRenderMode(
             gfx::Animation::RichAnimationRenderMode::FORCE_DISABLED)) {
     TabHoverCardController::disable_animations_for_testing_ = true;
-    scoped_feature_list_.InitAndEnableFeature(features::kTabHoverCards);
   }
   TabHoverCardBubbleViewBrowserTest(const TabHoverCardBubbleViewBrowserTest&) =
       delete;
@@ -98,8 +95,6 @@ class TabHoverCardBubbleViewBrowserTest : public DialogBrowserTest {
  private:
   std::unique_ptr<base::AutoReset<gfx::Animation::RichAnimationRenderMode>>
       animation_mode_reset_;
-
-  base::test::ScopedFeatureList scoped_feature_list_;
 
   TabStrip* tab_strip_ = nullptr;
 };
