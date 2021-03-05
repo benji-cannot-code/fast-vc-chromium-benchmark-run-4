e18bb92bc6e4d6970c9a75a046c9861cf73274e9
Hui Yingst <nigi@chromium.org>
2021-03-05 19:55:33 +0000

    [unseasoned-pdf] Migrate GetFileNameFromUrl() to //pdf/ui.
    Migrate static method OutOfProcessInstance::GetFileNameFromUrl() to
    //pdf/ui and rename it to GetFileNameForSaveFromUrl() since it's a
    utility function for supporting the PDF Viewer's saving documents
    feature. This will allow SaveToBuffer() to be migrated to
    PdfViewPluginBase later.
    
    Bug: 1184342
    Change-Id: I2ce12a57aae92356fc2b34274337c8a7e73cc74d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2734237
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860323}

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/chromeos_buildflags.h"
 #include "net/base/escape.h"
-#include "net/base/filename_util.h"
 #include "pdf/accessibility.h"
 #include "pdf/accessibility_structs.h"
 #include "pdf/document_attachment_info.h"
@@ -47,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "pdf/ppapi_migration/url_loader.h"
 #include "pdf/ppapi_migration/value_conversions.h"
 #include "pdf/thumbnail.h"
+#include "pdf/ui/file_name.h"
 #include "pdf/ui/format_page_size.h"
 #include "ppapi/c/dev/ppb_cursor_control_dev.h"
 #include "ppapi/c/pp_errors.h"
@@ -75,7 +75,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/vector2d.h"
-#include "url/gurl.h"
 
 namespace chrome_pdf {
 
@@ -1043,7 +1042,7 @@ void OutOfProcessInstance::SaveToBuffer(const std::string& token) {
   pp::VarDictionary message;
   message.Set(kType, kJSSaveDataType);
   message.Set(kJSToken, pp::Var(token));
-  message.Set(kJSFileName, pp::Var(GetFileNameFromUrl(GetURL())));
+  message.Set(kJSFileName, pp::Var(GetFileNameForSaveFromUrl(GetURL())));
   // This will be overwritten if the save is successful.
   message.Set(kJSDataToSave, pp::Var(pp::Var::Null()));
 
@@ -1220,17 +1219,6 @@ void OutOfProcessInstance::RotateClockwise() {
 
 void OutOfProcessInstance::RotateCounterclockwise() {
   engine()->RotateCounterclockwise();
-}
-
-// static
-std::string OutOfProcessInstance::GetFileNameFromUrl(const std::string& url) {
-  // Generate a file name. Unfortunately, MIME type can't be provided, since it
-  // requires IO.
-  base::string16 file_name = net::GetSuggestedFilename(
-      GURL(url), /*content_disposition=*/std::string(),
-      /*referrer_charset=*/std::string(), /*suggested_name=*/std::string(),
-      /*mime_type=*/std::string(), /*default_name=*/std::string());
-  return base::UTF16ToUTF8(file_name);
 }
 
 void OutOfProcessInstance::HandleGetThumbnailMessage(
---
+++ pdf/ui/file_name.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PDF_UI_FILE_NAME_H_
+#define PDF_UI_FILE_NAME_H_
+
+#include <string>
+
+namespace chrome_pdf {
+
+// Creates a file name for saving a PDF file, given the source URL.
+std::string GetFileNameForSaveFromUrl(const std::string& url);
+
+}  // namespace chrome_pdf
+
+#endif  // PDF_UI_FILE_NAME_H_
