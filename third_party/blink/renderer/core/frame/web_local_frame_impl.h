376866d075bb072fac1abf9e6949d4be128e59a1
Dave Tapuska <dtapuska@chromium.org>
2021-03-05 22:54:01 +0000

    Convert WebSize to gfx::Size for WebLocalFrame::DocumentSize.
    Use the gfx::Size type for returning the document size.
    
    BUG=738465
    
    Change-Id: I45895d8ccec494e32077985f77667b0ce4a28fb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2739921
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Owners-Override: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860406}

--- third_party/blink/renderer/core/frame/web_local_frame_impl.h
+++ third_party/blink/renderer/core/frame/web_local_frame_impl.h
@@ -286,7 +286,7 @@ class CORE_EXPORT WebLocalFrameImpl final
   void DeprecatedStopLoading() override;
   WebSize GetScrollOffset() const override;
   void SetScrollOffset(const WebSize&) override;
-  WebSize DocumentSize() const override;
+  gfx::Size DocumentSize() const override;
   bool HasVisibleContent() const override;
   gfx::Rect VisibleContentRect() const override;
   void DispatchBeforePrintEvent(
