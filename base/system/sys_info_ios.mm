77cf9fc04e420ed9d7448715d686018747e7f73f
Guillaume Jenkins <gujen@google.com>
2021-03-05 00:42:16 +0000

    [iOS SysInfo] Fix iOS CPU architecture info
    On iOS devices, the current SysInfo::OperatingSystemArchitecture()
    returns the device model identifier (e.g. "iPhone6,11") instead of
    the intended CPU architecture (e.g. "arm64"). This is causing problems
    in the Enterprise reporting infrastructure.
    
    This CL uses the build config to return an appropriate CPU architecture
    string on iOS. This CL also changes previous users of this function, who
    relied on the erroneous behavior, to use SysInfo::HardwareModelName()
    instead.
    
    Bug: 1180366, 370104
    Change-Id: I56d0b39dc8734b5e05e75c1c5fd8c9db06710da9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2723012
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#860035}

--- base/system/sys_info_ios.mm
+++ base/system/sys_info_ios.mm
@@ -18,7 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process_metrics.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
+#include "build/build_config.h"
 
 namespace base {
 
@@ -88,6 +90,21 @@ void SysInfo::OperatingSystemVersionNumbers(int32_t* major_version,
 }
 
 // static
+std::string SysInfo::OperatingSystemArchitecture() {
+#if defined(ARCH_CPU_X86)
+  return "x86";
+#elif defined(ARCH_CPU_X86_64)
+  return "x86_64";
+#elif defined(ARCH_CPU_ARMEL)
+  return "arm";
+#elif defined(ARCH_CPU_ARM64)
+  return "arm64";
+#else
+#error Unsupported CPU architecture
+#endif
+}
+
+// static
 std::string SysInfo::GetIOSBuildNumber() {
   int mib[2] = {CTL_KERN, KERN_OSVERSION};
   unsigned int namelen = sizeof(mib) / sizeof(mib[0]);
@@ -134,7 +151,21 @@ std::string SysInfo::HardwareModelName() {
 #if TARGET_OS_SIMULATOR
   // On the simulator, "hw.machine" returns "i386" or "x86_64" which doesn't
   // match the expected format, so supply a fake string here.
-  return "Simulator1,1";
+  const char* model = getenv("SIMULATOR_MODEL_IDENTIFIER");
+  if (model == nullptr) {
+    switch ([[UIDevice currentDevice] userInterfaceIdiom]) {
+      case UIUserInterfaceIdiomPhone:
+        model = "iPhone";
+        break;
+      case UIUserInterfaceIdiomPad:
+        model = "iPad";
+        break;
+      default:
+        model = "Unknown";
+        break;
+    }
+  }
+  return base::StringPrintf("iOS Simulator (%s)", model);
 #else
   // Note: This uses "hw.machine" instead of "hw.model" like the Mac code,
   // because "hw.model" doesn't always return the right string on some devices.
