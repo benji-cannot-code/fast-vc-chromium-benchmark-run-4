e643b90671e05a06ca541d8bbe695105e4e2fd31
Cammie Smith Barnes <cammie@chromium.org>
2021-03-05 21:16:53 +0000

    AdMetrics: Add queue to fix flaky AdsMemoryMeasurementBrowserTest.
    AdsMemoryMeasurementBrowserTest.SingleAdFrame_MaxMemoryBytesRecorded
    is still failing flakily on Chrome OS ASAN and linux-chromeos-dbg
    after the refactor (http://crrev.com/c/2706680) that allowed for
    waiting directly for the same memory measurements that are used in the
    test's histograms.
    
    The bug turned out to be a race condition where sometimes the first
    memory measurements for the mainframe were not received by the
    AdsPageLoadMetricsObserver because the navigation had not yet finished
    and so there was not yet a `committed_load_` in the
    MetricsWebContentsObserver to forward the memory updates.
    Hence, sometimes the first updates received by APLMO were negative,
    leading to a max memory usage of 0 (the default initial value).
    
    This CL adds a std::queue of memory updates to the
    MetricsWebContentsObserver to accumulate the updates in the absence of
    a `committed_load_`.
    MetricsWebContentsObserver::HandleCommittedNavigationForTrackedLoad
    then flushes the queue and forwards any waiting updates.
    
    In the
    AdsMemoryMeasurementBrowserTest.SingleAdFrame_MaxMemoryBytesRecorded,
    this CL also adds a navigation to "about:blank" before the navigation
    to cross_site_iframe_factory.html so that we can grab the mainframe's
    routing ID and use it to add a memory update expectation to
    PageLoadMetricsTestWaiter *before* the navigation to
    cross_site_iframe_factory.html instead of afterwards.
    
    Moreover, the unit test with a similar name that was mistakenly
    disabled instead of the browser test is herein re-enabled.
    
    Bug: 1177094,1184366
    Change-Id: I8b6e790da52a9476f59eb832f3a7df60c239ecee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2721119
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860362}

--- components/page_load_metrics/browser/metrics_web_contents_observer.h
+++ components/page_load_metrics/browser/metrics_web_contents_observer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
+#include <queue>
 #include <vector>
 
 #include "base/macros.h"
@@ -308,6 +309,11 @@ class MetricsWebContentsObserver
   std::vector<std::unique_ptr<PageLoadTracker>> aborted_provisional_loads_;
 
   std::unique_ptr<PageLoadTracker> committed_load_;
+
+  // Memory updates that are accumulated while there is no `committed_load_`.
+  // Will be sent in HandleCommittedNavigationForTrackedLoad, unless the
+  // render process is gone and/or web contents is destroyed.
+  std::queue<std::vector<MemoryUpdate>> queued_memory_updates_;
 
   // This is currently set only for the main frame.
   base::ReadOnlySharedMemoryRegion ukm_smoothness_data_;
