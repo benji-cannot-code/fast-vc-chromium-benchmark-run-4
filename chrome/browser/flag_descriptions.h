fb1c5b6284f6a4997164cd291330200a825ef94e
Wez <wez@chromium.org>
2021-03-05 20:32:30 +0000

    Revert "Remove overlay scrollbar about:flag"
    This reverts commit f4218e1a4f4da040ecd95b119f2fcb0aa8132d23.
    
    Reason for revert: Overlay scrollbars have reduced usability with some
    content, and for some users. Restore the about:flags option to let them
    opt-out of overlay-scrollbars until more accessible scrollbars are
    properly supported.
    
    Bug: 801671
    
    Original change's description:
    > Remove overlay scrollbar about:flag
    >
    > Overlay scrollbars are enabled based on platform, though without a user
    > option (in Chrome UI), so the runtime feature enabling/disabling them
    > must remain. However, the about:flag has been expired since M76 and can
    > be removed.
    >
    > Change-Id: Id9269637369e393556937cb2b231646eed202e16
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2602895
    > Commit-Queue: David Bokan <bokan@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#840785}
    
    Change-Id: I7c24fb7e22c41a111f8b14df3fac205c7d6b3f66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2739606
    Commit-Queue: Wez <wez@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860344}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1069,6 +1069,9 @@ extern const char kOsSettingsDeepLinkingDescription[];
 extern const char kEnableDeJellyName[];
 extern const char kEnableDeJellyDescription[];
 
+extern const char kOverlayScrollbarsName[];
+extern const char kOverlayScrollbarsDescription[];
+
 extern const char kOverlayStrategiesName[];
 extern const char kOverlayStrategiesDescription[];
 extern const char kOverlayStrategiesDefault[];
