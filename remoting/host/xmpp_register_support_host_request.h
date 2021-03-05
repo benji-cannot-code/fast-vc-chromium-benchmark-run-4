6f16858d284b4b6f58d1da8c899cf9210932c19d
Meredith Lane <meredithl@chromium.org>
2021-03-05 05:10:40 +0000

    Revert "Enable gn check for //remoting/host"
    This reverts commit 210c862cfcc0309835e51f8f9aa2484c4120413b.
    
    Reason for revert: Suspected culprit for win compile failure
    
    https://ci.chromium.org/p/chrome/builders/ci/win-chrome/19464
    
    Original change's description:
    > Enable gn check for //remoting/host
    >
    > Fixed: 1159016
    > Change-Id: Ic6df45c41edd9da4ac3a56c9b60008f14b140f63
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2718573
    > Commit-Queue: Joey Arhar <jarhar@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Erik Staab <estaab@chromium.org>
    > Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    > Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#860108}
    
    Change-Id: I5377149ab1c6775f6341342f46015c1cac209a9a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2738999
    Auto-Submit: Meredith Lane <meredithl@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#860128}

--- remoting/host/xmpp_register_support_host_request.h
+++ remoting/host/xmpp_register_support_host_request.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/register_support_host_request.h"
 #include "remoting/protocol/errors.h"
 #include "remoting/signaling/signal_strategy.h"
+#include "testing/gtest/include/gtest/gtest_prod.h"
 
 namespace jingle_xmpp {
 class XmlElement;
