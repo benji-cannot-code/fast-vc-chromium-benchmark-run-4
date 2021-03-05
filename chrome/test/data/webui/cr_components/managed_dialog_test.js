4895665cb6b91ae284ffb47e66613e0199708330
Igor Ruvinov <igorruvinov@chromium.org>
2021-03-05 18:06:53 +0000

    Convert managed dialog to a cr_component
    Convert the managed dialog settings element to a cr_component as it will be used in the customize_theme component (bug 1173533), which is also a cr_component.
    
    Bug: 1173533
    Change-Id: I073eb14f1a34340a8090aafed8f1db32f54435d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2733230
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860287}

--- chrome/test/data/webui/cr_components/managed_dialog_test.js
+++ chrome/test/data/webui/cr_components/managed_dialog_test.js
@@ -5,11 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /** @fileoverview Test suite for managed-dialog. */
 
-import 'chrome://settings/lazy_load.js';
+import 'chrome://resources/cr_components/managed_dialog/managed_dialog.js';
 
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {assertFalse, assertNotEquals, assertTrue} from '../chai_assert.js';
 
-suite('SettingsManagedDialog', function() {
+suite('ManagedDialogTest', function() {
+  suiteSetup(function() {
+    loadTimeData.data = {};
+  });
+
   setup(function() {
     document.body.innerHTML = '';
   });
@@ -22,7 +27,15 @@ suite('SettingsManagedDialog', function() {
    * @return {Element}
    */
   function createManagedDialog(title, body) {
-    const dialog = document.createElement('settings-managed-dialog');
+    loadTimeData.overrideValues({
+      title,
+      body,
+      controlledSettingPolicy: '',
+      ok: 'OK',
+      close: 'Close',
+    });
+
+    const dialog = document.createElement('managed-dialog');
     dialog.title = title;
     dialog.body = body;
     document.body.appendChild(dialog);
@@ -37,12 +50,6 @@ suite('SettingsManagedDialog', function() {
     assertTrue(dialog.$$('cr-dialog').open);
     assertTrue(dialog.shadowRoot.textContent.includes(title));
     assertTrue(dialog.shadowRoot.textContent.includes(body));
-  });
-
-  test('DialogDismiss', function() {
-    const dialog = createManagedDialog('', '');
-    assertNotEquals('none', getComputedStyle(dialog).display);
-    assertTrue(dialog.$$('cr-dialog').open);
 
     // Click OK button to dismiss dialog
     dialog.$$('.action-button').click();
