c72084cf284f1f920795e78ed1e863d4a349a99b
John Chen <johnchen@chromium.org>
2021-03-05 17:47:08 +0000

    [Perf] Update Pixel4 to use Trichrome bundle
    Update android-pixel4-perf to use android-trichrome-bundle,
    and android-pixel4_webview-perf to use
    android-webview-trichrome-google-bundle.
    
    Bug: 1175768
    Change-Id: I203bab4057bc6470d768f9acc370db7038a7826f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737515
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860279}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -628,7 +628,7 @@ BUILDERS = {
         'tests': [{
             'isolate': 'performance_webview_test_suite',
         }],
-        'platform': 'android-webview-trichrome-google',
+        'platform': 'android-webview-trichrome-google-bundle',
         'dimension': {
             'pool': 'chrome.tests.perf-webview',
             'os': 'Android',
@@ -654,7 +654,7 @@ BUILDERS = {
         'tests': [{
             'isolate': 'performance_test_suite',
         }],
-        'platform': 'android-chrome-64-bundle',
+        'platform': 'android-trichrome-bundle',
         'dimension': {
             'pool': 'chrome.tests.perf',
             'os': 'Android',
