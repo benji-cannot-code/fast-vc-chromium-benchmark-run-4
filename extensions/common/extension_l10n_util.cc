f141b8aa9f916510d1b253d877d27a880a470309
Solomon Kinard <solomonkinard@chromium.org>
2021-03-05 23:11:25 +0000

    Extensions: ContentVerifier: Skip message catalog content verification
    Introduce case-insensitive comparison of message catalog paths and
    canonical locale candidates. Prior to this change, lowercase paths
    might be content verified during installation causing disabling of the
    extension in addition to the presence of the repair button.
    
    Bug: 1171233
    
    Change-Id: Iec00db2c938924f49b74ceb0e545cd10f580b868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2739257
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860413}

--- extensions/common/extension_l10n_util.cc
+++ extensions/common/extension_l10n_util.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/message_bundle.h"
+#include "extensions/common/utils/base_string.h"
 #include "third_party/icu/source/common/unicode/uloc.h"
 #include "third_party/zlib/google/compression_utils.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -544,13 +545,7 @@ bool ShouldSkipValidation(const base::FilePath& locales_path,
   // On case-insensitive file systems we will load messages by matching them
   // with locale names (see LoadMessageCatalogs). Reversed comparison must still
   // work here, when we match locale name with file name.
-  auto find_iter = std::find_if(all_locales.begin(), all_locales.end(),
-                                [subdir](const std::string& locale) {
-                                  return base::CompareCaseInsensitiveASCII(
-                                             subdir, locale) == 0;
-                                });
-
-  if (find_iter == all_locales.end())
+  if (!extensions::ContainsStringIgnoreCaseASCII(all_locales, subdir))
     return true;
 
   return false;
