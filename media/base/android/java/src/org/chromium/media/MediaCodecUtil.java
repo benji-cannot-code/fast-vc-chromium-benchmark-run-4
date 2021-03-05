770006f2a857e07fb059fd94f2906c4d2123d175
chenjiadong <chenjiadong@huawei.com>
2021-03-05 03:54:52 +0000

    Android: Enable WebRTC H264 HW Encoder for Hisilicon
    Bug: 1182608
    
    Change-Id: I805b7b368e45252130f0907661846957322f5fcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2721393
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860110}

--- media/base/android/java/src/org/chromium/media/MediaCodecUtil.java
+++ media/base/android/java/src/org/chromium/media/MediaCodecUtil.java
@@ -490,7 +490,7 @@ class MediaCodecUtil {
 
     // List of supported HW encoders.
     @IntDef({HWEncoder.QcomVp8, HWEncoder.QcomH264, HWEncoder.ExynosVp8, HWEncoder.ExynosH264,
-            HWEncoder.MediatekH264})
+            HWEncoder.MediatekH264, HWEncoder.HisiH264})
     @Retention(RetentionPolicy.SOURCE)
     public @interface HWEncoder {
         int QcomVp8 = 0;
@@ -498,7 +498,8 @@ class MediaCodecUtil {
         int ExynosVp8 = 2;
         int ExynosH264 = 3;
         int MediatekH264 = 4;
-        int NUM_ENTRIES = 5;
+        int HisiH264 = 5;
+        int NUM_ENTRIES = 6;
     }
 
     private static String getMimeForHWEncoder(@HWEncoder int decoder) {
@@ -509,6 +510,7 @@ class MediaCodecUtil {
             case HWEncoder.QcomH264:
             case HWEncoder.ExynosH264:
             case HWEncoder.MediatekH264:
+            case HWEncoder.HisiH264:
                 return MimeTypes.VIDEO_H264;
         }
         return "";
@@ -524,6 +526,8 @@ class MediaCodecUtil {
                 return "OMX.Exynos.";
             case HWEncoder.MediatekH264:
                 return "OMX.MTK.";
+            case HWEncoder.HisiH264:
+                return "OMX.hisi.";
         }
         return "";
     }
@@ -538,6 +542,8 @@ class MediaCodecUtil {
                 return Build.VERSION_CODES.M;
             case HWEncoder.MediatekH264:
                 return Build.VERSION_CODES.O_MR1;
+            case HWEncoder.HisiH264:
+                return Build.VERSION_CODES.N;
         }
         return -1;
     }
@@ -551,6 +557,7 @@ class MediaCodecUtil {
                 return BitrateAdjuster.Type.NO_ADJUSTMENT;
             case HWEncoder.ExynosH264:
             case HWEncoder.MediatekH264:
+            case HWEncoder.HisiH264:
                 return BitrateAdjuster.Type.FRAMERATE_ADJUSTMENT;
         }
         throw new IllegalArgumentException("Invalid HWEncoder decoder parameter.");
