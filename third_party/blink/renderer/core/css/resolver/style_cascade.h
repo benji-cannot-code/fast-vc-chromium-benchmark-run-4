c0218078cb3a6dcb47a4151292493633468df723
Anders Hartvoll Ruud <andruud@chromium.org>
2021-03-05 08:46:39 +0000

    Make custom properties that are IACVT guaranteed-invalid
    Per recent spec change [1], custom properties that are invalid at
    computed-value time (IACVT) shall become guaranteed-invalid [2]
    if the custom property supports guaranteed-invalid values.
    
    [1] https://github.com/w3c/csswg-drafts/pull/6006
    [2] https://drafts.csswg.org/css-variables/#guaranteed-invalid-value
    
    Fixed: 1110188
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/0xrbzYe_vxU/m/7bsL76n9CgAJ
    Change-Id: Ibfbb0c641abaf29f099e082115d5f04f77637840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2734621
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860161}

--- third_party/blink/renderer/core/css/resolver/style_cascade.h
+++ third_party/blink/renderer/core/css/resolver/style_cascade.h
@@ -341,7 +341,6 @@ class CORE_EXPORT StyleCascade {
   void CountUse(WebFeature);
   void MaybeUseCountRevert(const CSSValue&);
   void MaybeUseCountSummaryDisplayBlock();
-  void MaybeUseCountInvalidVariableUnset(const CustomProperty&);
 
   StyleResolverState& state_;
   MatchResult match_result_;
