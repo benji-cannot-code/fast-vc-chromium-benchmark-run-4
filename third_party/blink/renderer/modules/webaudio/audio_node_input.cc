b2eb88f0459a53328295b6458fc1fd1f1e18be77
Raymond Toy <rtoy@chromium.org>
2021-03-05 01:40:49 +0000

    Get render quantum frames in DeferredTaskHandler (Part 2/N)
    In this CL, we convert the simpler cases of replacing
    audio_utilities::kRenderQuantumFrames with
    GetDeferredTaskHandler().RenderQuantumFrames().
    
    No user-visible changes.
    
    Bug: 988121
    Change-Id: I337ee7cdff6fd8a149fa7beefd01e791d4c97b93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2715424
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860054}

--- third_party/blink/renderer/modules/webaudio/audio_node_input.cc
+++ third_party/blink/renderer/modules/webaudio/audio_node_input.cc
@@ -40,7 +40,7 @@ AudioNodeInput::AudioNodeInput(AudioHandler& handler)
       handler_(handler) {
   // Set to mono by default.
   internal_summing_bus_ =
-      AudioBus::Create(1, audio_utilities::kRenderQuantumFrames);
+      AudioBus::Create(1, GetDeferredTaskHandler().RenderQuantumFrames());
 }
 
 AudioNodeInput::~AudioNodeInput() {
@@ -61,7 +61,7 @@ void AudioNodeInput::UpdateInternalBus() {
     return;
 
   internal_summing_bus_ = AudioBus::Create(
-      number_of_input_channels, audio_utilities::kRenderQuantumFrames);
+      number_of_input_channels, GetDeferredTaskHandler().RenderQuantumFrames());
 }
 
 unsigned AudioNodeInput::NumberOfChannels() const {
