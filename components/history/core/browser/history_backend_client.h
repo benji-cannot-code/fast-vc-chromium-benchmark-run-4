77e6269b05d6def8b7dc6d2951fba8a1dfdd050d
Michael Thiessen <mthiesse@chromium.org>
2021-03-05 18:45:52 +0000

    Stub out ChromeBrowserProvider
    ChromeBrowserProvider should never have been supported past Android M,
    when Android deprecated and started always rejecting the BOOKMARKS
    permissions, but some OEMs have continued granting these permissions.
    
    The initial reason for implementing ChromeBrowserProvider no longer
    exists, so there's no reason to continue exposing Chrome's bookmarks to
    external apps.
    
    This change replaces the implementation with a stub implementation that
    returns an empty (but valid) cursor for queries, and no-ops
    update/delete requests.
    
    It's possible that not returning a valid Uri from Insert could break
    existing apps, but WRITE usage is extremely low, and Chrome could
    already return null or throw exceptions from this API, so apps should
    be handling that already.
    
    Bug: 582250
    Change-Id: I177dcd5149f0cf001a51783973de2ddd72d1a7ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2727641
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860297}

--- components/history/core/browser/history_backend_client.h
+++ components/history/core/browser/history_backend_client.h
@@ -10,21 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/strings/string16.h"
-#include "build/build_config.h"
 #include "url/gurl.h"
 
-namespace base {
-class FilePath;
-}
-
-namespace favicon {
-class FaviconDatabase;
-}
-
 namespace history {
-
-class HistoryBackend;
-class HistoryDatabase;
 
 struct URLAndTitle {
   GURL url;
@@ -46,19 +34,6 @@ class HistoryBackendClient {
   // Returns whether |url| should be considered web-safe (see
   // content::ChildProcessSecurityPolicy).
   virtual bool IsWebSafe(const GURL& url) = 0;
-
-#if defined(OS_ANDROID)
-  // Called upon initialization of the HistoryBackend.
-  virtual void OnHistoryBackendInitialized(
-      HistoryBackend* history_backend,
-      HistoryDatabase* history_database,
-      favicon::FaviconDatabase* favicon_database,
-      const base::FilePath& history_dir) = 0;
-
-  // Called upon destruction of the HistoryBackend.
-  virtual void OnHistoryBackendDestroyed(HistoryBackend* history_backend,
-                                         const base::FilePath& history_dir) = 0;
-#endif  // defined(OS_ANDROID)
 
  private:
   DISALLOW_COPY_AND_ASSIGN(HistoryBackendClient);
