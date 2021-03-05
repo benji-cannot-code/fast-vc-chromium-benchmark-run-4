3c5b604687e87654d2243915e52004a8f276ecbe
Andrew Grieve <agrieve@chromium.org>
2021-03-05 21:22:24 +0000

    Android: Reset CachingUmaRecorder between tests
    The CachingUmaRecorder contains global state, and so should be reset
    between tests. This was found in a test batching refactor that caused
    the size limit assert to be hit:
    
    "Histogram exceeded sample cache size limit"
    https://ci.chromium.org/ui/p/chromium/builders/try/android-lollipop-arm-rel/254533
    
    Bug: 1183540
    Change-Id: I4e4b318ed126c335faac9757689f49d5f304c38a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2739242
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860364}

--- base/android/java/src/org/chromium/base/metrics/UmaRecorderHolder.java
+++ base/android/java/src/org/chromium/base/metrics/UmaRecorderHolder.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.metrics;
 
-import androidx.annotation.VisibleForTesting;
-
 /** Holds the {@link CachingUmaRecorder} used by {@link RecordHistogram}. */
 public class UmaRecorderHolder {
     private UmaRecorderHolder() {}
@@ -16,6 +14,9 @@ public class UmaRecorderHolder {
 
     /** Allow calling onLibraryLoaded() */
     private static boolean sAllowNativeUmaRecorder = true;
+
+    /** Whether onLibraryLoaded() was called. */
+    private static boolean sNativeInitialized;
 
     /** Returns the held {@link UmaRecorder}. */
     public static UmaRecorder get() {
@@ -52,16 +53,20 @@ public class UmaRecorderHolder {
      */
     public static void onLibraryLoaded() {
         assert sAllowNativeUmaRecorder : "Setting NativeUmaRecorder is not allowed";
+        assert !sNativeInitialized;
+        sNativeInitialized = true;
         sRecorder.setDelegate(new NativeUmaRecorder());
     }
 
     /**
-     * Tests may need to disable metrics. The value should be reset after the test done, to avoid
-     * carrying over state to unrelated tests.
-     *
-     * @deprecated This method does nothing.
+     * Reset globals for tests.
      */
-    @VisibleForTesting
-    @Deprecated
-    public static void setDisabledForTests(boolean disabled) {}
+    public static void resetForTesting() {
+        // Prevent hitting cache size limits from tests running without ever switching to the native
+        // recorder. Also guards against tests that use setNonNativeDelegate() to inject a mock from
+        // forgetting to reset it.
+        if (!sNativeInitialized) {
+            sRecorder = new CachingUmaRecorder();
+        }
+    }
 }
