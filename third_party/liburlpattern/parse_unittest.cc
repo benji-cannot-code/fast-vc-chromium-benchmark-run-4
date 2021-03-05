7e441539893a086ed7d4837eb518b7fec4dc3b4b
Ben Kelly <wanderview@chromium.org>
2021-03-05 22:42:17 +0000

    URLPattern: Canonicalize pattern encoding.
    This CL adds an encoding callback to liburlpattern::Parse().  The
    parse will invoke the given callback for plaintext parts of the pattern
    to validate and encode the characters.  This callback mechanism is then
    used to apply the chromium url canonicalization code for each component
    pattern.
    
    There are a couple of behaviors in the canonicalizer that do not play
    well with this approach that the CL works around:
    
    1. The port canonicalizer will replace an exact default port with the
       empty string.  Since the liburlpattern::Parse() callback is invoked
       for partial values this CL instead implements this canoncilization
       separately before pattern compilation.
    2. The URL canonicalizer will prepend a leading `/` character if there
       isn't one.  Again, this behavior does not make sense when operating
       on partial values.  Therefore this CL exposes the internal partial
       path canonicalization routine so that we can use it in URLPattern.
    
    In addition, this CL removes a DCHECK from url's DoPartialPath() that
    asserted there was always a character preceding a dot.  The DCHECK has
    had a runtime check checking the same behavior since 2013 so it seems
    safe to remove the DCHECK.  And in this case we want to be able to
    run the canonicalize partial paths that do start with dots.
    
    The CL adds a number of additional WPT test cases validating the new
    canonicalization behavior.
    
    The behavior in this test has been discussed in this spec issue:
    
    https://github.com/WICG/urlpattern/issues/33
    
    Bug: 1141510
    Change-Id: I388be5d0cc57b125d44465b283050df5ed0b5321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720702
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860399}

--- third_party/liburlpattern/parse_unittest.cc
+++ third_party/liburlpattern/parse_unittest.cc
@@ -5,13 +5,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/liburlpattern/parse.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/strings/str_format.h"
 #include "third_party/liburlpattern/pattern.h"
 
 namespace liburlpattern {
 
+absl::StatusOr<std::string> PassThrough(absl::string_view input) {
+  return std::string(input);
+}
+
+absl::StatusOr<std::string> ToUpper(absl::string_view input) {
+  std::string output;
+  std::transform(input.begin(), input.end(), std::back_inserter(output),
+                 [](unsigned char c) { return std::toupper(c); });
+  return output;
+}
+
 void RunParseTest(absl::string_view pattern,
-                  absl::StatusOr<std::vector<Part>> expected) {
-  auto result = Parse(pattern);
+                  absl::StatusOr<std::vector<Part>> expected,
+                  EncodeCallback callback = PassThrough) {
+  auto result = Parse(pattern, std::move(callback));
   ASSERT_EQ(result.ok(), expected.ok())
       << "parse status '" << result.status() << "' for: " << pattern;
   if (!expected.ok()) {
@@ -39,8 +52,11 @@ TEST(ParseTest, EmptyPattern) {
   RunParseTest("", std::vector<Part>());
 }
 
-TEST(ParseTest, InvalidChar) {
-  RunParseTest("/foo/ßar", absl::InvalidArgumentError("Invalid character"));
+TEST(ParseTest, EncoderCallback) {
+  std::vector<Part> expected_parts = {
+      Part(PartType::kFixed, "/FOO/BAR", Modifier::kNone),
+  };
+  RunParseTest("/foo/bar", expected_parts, ToUpper);
 }
 
 TEST(ParseTest, Fixed) {
