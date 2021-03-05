1010bf5f1ea1b65cfdc256bfd193a12c840fd906
Stepan Khapugin <stkhapugin@chromium.org>
2021-03-05 15:24:24 +0000

    [iOS][Getaway] UI tweaks.
    1. Hide the incognito logo in tab switcher blocking UI
    2. Tweak colors and increase blur levels.
    3. Add highlighted state to buttons on the blocking UI.
    
    Fixed: 1181339, 1181333
    Change-Id: I83d374ce4e822fb6b8c0a98d9c6cc12753e6c5e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732097
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860242}

--- ios/chrome/browser/ui/incognito_reauth/incognito_reauth_view.h
+++ ios/chrome/browser/ui/incognito_reauth/incognito_reauth_view.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong, readonly) UIButton* authenticateButton;
 // The button to go to the tab switcher.
 @property(nonatomic, strong, readonly) UIButton* tabSwitcherButton;
+// The image view with the incognito logo.
+@property(nonatomic, strong, readonly) UIView* logoView;
 
 @end
 
