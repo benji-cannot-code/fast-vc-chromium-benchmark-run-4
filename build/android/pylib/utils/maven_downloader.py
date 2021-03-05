3d83615e5a7bad6918050a60a6880d0fad7b8a3c
Meredith Lane <meredithl@chromium.org>
2021-03-05 01:20:18 +0000

    Revert "Use vpython for scripts and tests in //build/android that use devil."
    This reverts commit 0acf7c880d0b3550601dd1b0416597d16eac0876.
    
    Reason for revert: Suspected cause of tree closer on Android ASAN (dbg): https://ci.chromium.org/p/chromium/builders/ci/Android%20ASAN%20(dbg)%20(reclient)/1?
    
    
    Original change's description:
    > Use vpython for scripts and tests in //build/android that use devil.
    >
    > Bug: 1007101
    > Change-Id: Ie150922237bf15cdfaad5468b351d0d5563a98eb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2620679
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Reviewed-by: Ben Pastene <bpastene@chromium.org>
    > Commit-Queue: John Budorick <jbudorick@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#859912}
    
    Bug: 1007101
    Change-Id: I7c79aa6ffea3ce28e1547237004c0a8b4dff4332
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2738036
    Auto-Submit: Meredith Lane <meredithl@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Meredith Lane <meredithl@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Meredith Lane <meredithl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860053}

--- build/android/pylib/utils/maven_downloader.py
+++ build/android/pylib/utils/maven_downloader.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env vpython
+#!/usr/bin/env python
 # Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -8,10 +8,7 @@ import errno
 import logging
 import os
 import shutil
-import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
-import devil_chromium  # pylint: disable=unused-import
 from devil.utils import cmd_helper
 from devil.utils import parallelizer
 
