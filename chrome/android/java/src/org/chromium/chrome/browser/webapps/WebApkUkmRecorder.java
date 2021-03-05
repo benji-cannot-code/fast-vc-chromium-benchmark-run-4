316f81970cd65ea54a57b940488634b8be58e4b2
Ella Ge <eirage@chromium.org>
2021-03-05 01:08:30 +0000

    Change WebApkDistributor package to browserservices/intents
    The generated java class is moved to browserservices/intents module, but
    package name was not changed accordingly.
    
    Bug: 1178911
    Change-Id: I85560880381a3a40750c885b450bd9ef0553478e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732513
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860051}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUkmRecorder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUkmRecorder.java
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 package org.chromium.chrome.browser.webapps;
+
 import org.chromium.base.annotations.NativeMethods;
+import org.chromium.chrome.browser.browserservices.intents.WebApkDistributor;
 
 /**
  * A class to record User Keyed Metrics relevant to WebAPKs. This
