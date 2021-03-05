0086add5da808c258ed30467fcd439a67c85f3db
Eric Willigers <ericwilligers@chromium.org>
2021-03-05 02:10:00 +0000

    Web Share: Limit size of shared title/text/url
    The title/text/url fields are now limited in size:
    title - 16KiB
    text - 1MiB desktop, 160KiB Android
    url - 16KiB
    
    These were chosen after testing on different platforms
    (Android, Chrome OS, Windows).
    
    We already place limits on file sizes.
    
    The spec does not specify limits.
    https://w3c.github.io/web-share/#sharedata-dictionary
    
    
    Bug: 1183108
    Change-Id: If12a66d5a0ba16c17f8da5606f5c74eec708bbd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2730367
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860083}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webshare/WebShareTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webshare/WebShareTest.java
@@ -55,6 +55,7 @@ public class WebShareTest {
     private static final String TEST_FILE_OGG = "/content/test/data/android/webshare-ogg.html";
     private static final String TEST_FILE_MANY = "/content/test/data/android/webshare-many.html";
     private static final String TEST_FILE_LARGE = "/content/test/data/android/webshare-large.html";
+    private static final String TEST_LONG_TEXT = "/content/test/data/android/webshare-long.html";
 
     private EmbeddedTestServer mTestServer;
 
@@ -282,6 +283,22 @@ public class WebShareTest {
     @Feature({"WebShare"})
     public void testWebShareLarge() throws Exception {
         mActivityTestRule.loadUrl(mTestServer.getURL(TEST_FILE_LARGE));
+        // Click (instead of directly calling the JavaScript function) to simulate a user gesture.
+        TouchCommon.singleClickView(mTab.getView());
+        Assert.assertEquals("Fail: NotAllowedError: "
+                        + "Failed to execute 'share' on 'Navigator': Permission denied",
+                mUpdateWaiter.waitForUpdate());
+    }
+
+    /**
+     * Verify WebShare fails if share of long text is called from a user gesture.
+     * @throws Exception
+     */
+    @Test
+    @MediumTest
+    @Feature({"WebShare"})
+    public void testWebShareLongText() throws Exception {
+        mActivityTestRule.loadUrl(mTestServer.getURL(TEST_LONG_TEXT));
         // Click (instead of directly calling the JavaScript function) to simulate a user gesture.
         TouchCommon.singleClickView(mTab.getView());
         Assert.assertEquals("Fail: NotAllowedError: "
