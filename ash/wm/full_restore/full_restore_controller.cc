7915a10c06c0fda1c0ebd58b2046f06b3d03120d
Sammie Quon <sammiequon@chromium.org>
2021-03-05 19:45:59 +0000

    full_restore: Save display id to restore file.
    Fixed: 1177774
    Test: manually checked restore file
    Change-Id: Ibda68daff9b82acb144321beb12ffd4fc8a116d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737792
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Jeremy Chinsen <chinsenj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860319}

--- ash/wm/full_restore/full_restore_controller.cc
+++ ash/wm/full_restore/full_restore_controller.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/full_restore/full_restore_utils.h"
 #include "components/prefs/pref_service.h"
 #include "ui/aura/client/aura_constants.h"
+#include "ui/display/display.h"
+#include "ui/display/screen.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
 
@@ -179,6 +181,8 @@ void FullRestoreController::SaveWindowImpl(
   window_info.restore_bounds = window_state->GetRestoreBoundsInScreen();
   window_info.current_bounds = window->GetBoundsInScreen();
   window_info.window_state_type = window_state->GetStateType();
+  window_info.display_id =
+      display::Screen::GetScreen()->GetDisplayNearestWindow(window).id();
   full_restore::SaveWindowInfo(window_info);
 
   if (g_save_window_callback_for_testing)
