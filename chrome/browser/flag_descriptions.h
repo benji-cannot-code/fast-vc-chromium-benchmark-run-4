bccd4cd2ba5bda0f1d4cbf777ff554904f28c922
Rohit Agarwal <roagarwal@chromium.org>
2021-03-05 12:25:47 +0000

    Add Incognito brand consistency for desktop feature flag.
    This CL only adds a feature flag IncognitoBrandConsistencyForDesktop
    without any logic.
    
    The feature flag would help to create an Incognito brand consistency
    across different desktop platforms.
    
    UX: go/incognito-brand
    
    Bug: 1183343
    Change-Id: Id325b6cc00a7b3e804b18ed254e4833935d28280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2729276
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860201}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -202,6 +202,9 @@ extern const char kAutoScreenBrightnessDescription[];
 extern const char kAvatarToolbarButtonName[];
 extern const char kAvatarToolbarButtonDescription[];
 
+extern const char kIncognitoBrandConsistencyForDesktopName[];
+extern const char kIncognitoBrandConsistencyForDesktopDescription[];
+
 extern const char kBackForwardCacheName[];
 extern const char kBackForwardCacheDescription[];
 
