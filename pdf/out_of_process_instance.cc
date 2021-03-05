5413dcac59ce522a916017458bc4c8c7386df9c9
Daniel Hosseinian <dhoss@chromium.org>
2021-03-05 01:53:35 +0000

    [unseasoned-pdf] Migrate DocumentFocusChanged()
    Migrate from a Pepper OutOfProcessInstance implementation to a common
    PdfViewPluginBase one.
    
    Bug: 1109796
    Change-Id: I0bab7159a5d6a491a8a1e88a8d87076df4933ea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737798
    Auto-Submit: Daniel Hosseinian <dhoss@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Reviewed-by: Hui Yingst <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860076}

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -141,10 +141,6 @@ constexpr char kJSSetIsEditingType[] = "setIsEditing";
 constexpr char kJSFieldFocusType[] = "formFocusChange";
 constexpr char kJSFieldFocus[] = "focused";
 
-// Notify when document is focused (Plugin -> Page)
-constexpr char kJSDocumentFocusChangedType[] = "documentFocusChanged";
-constexpr char kJSDocumentHasFocus[] = "hasFocus";
-
 // Request the thumbnail image for a particular page (Page -> Plugin)
 constexpr char kJSGetThumbnailType[] = "getThumbnail";
 constexpr char kJSGetThumbnailPage[] = "page";
@@ -1568,13 +1564,6 @@ void OutOfProcessInstance::EnteredEditMode() {
 
   pp::VarDictionary message;
   message.Set(kType, kJSSetIsEditingType);
-  PostMessage(message);
-}
-
-void OutOfProcessInstance::DocumentFocusChanged(bool document_has_focus) {
-  pp::VarDictionary message;
-  message.Set(pp::Var(kType), pp::Var(kJSDocumentFocusChangedType));
-  message.Set(pp::Var(kJSDocumentHasFocus), pp::Var(document_has_focus));
   PostMessage(message);
 }
 
