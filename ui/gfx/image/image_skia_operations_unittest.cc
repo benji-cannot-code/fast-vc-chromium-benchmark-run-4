8f291691153ebe42248cb3a96d784bfe26d55b84
danakj <danakj@chromium.org>
2021-03-05 21:27:04 +0000

    Handle resize bitmap operation failing.
    If it fails, make a null ImageSkiaRep instead of passing a null bitmap
    and trying to make a valid ImageSkiaRep with it, which will CHECK() on
    an incorrect color type.
    
    R=sky@chromium.org
    
    Bug: 1184688
    Change-Id: Idc6e6a4239db75ce91b3a73dc49f3d28a549a131
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2735999
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860368}

---
+++ ui/gfx/image/image_skia_operations_unittest.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gfx/image/image_skia_operations.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gfx/image/image_skia.h"
+
+namespace gfx {
+namespace {
+
+TEST(ImageSkiaOperationsTest, ResizeFailure) {
+  ImageSkia image(ImageSkiaRep(gfx::Size(10, 10), 1.f));
+
+  // Try to resize to empty. This isn't a valid resize and fails gracefully.
+  ImageSkia resized = ImageSkiaOperations::CreateResizedImage(
+      image, skia::ImageOperations::RESIZE_BEST, gfx::Size());
+  EXPECT_TRUE(resized.GetRepresentation(1.0f).is_null());
+}
+
+}  // namespace
+}  // namespace gfx
