a1218e5530714016ae39b25c2a4e886b696ad158
Tanya Gupta <tgupta@chromium.org>
2021-03-05 01:40:55 +0000

    [LongScreenshots] Fixed nullptr exception
    Change-Id: I4cb8ece55cc9b7c43aa25f2ca2325f1ca581a414
    Bug: 1183931,1153969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737959
    Reviewed-by: Travis Skare <skare@chromium.org>
    Commit-Queue: Travis Skare <skare@chromium.org>
    Commit-Queue: Tanya Gupta <tgupta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860055}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/long_screenshots/LongScreenshotsMediator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/long_screenshots/LongScreenshotsMediator.java
@@ -94,7 +94,11 @@ public class LongScreenshotsMediator implements LongScreenshotsEntry.EntryListen
             return;
         }
 
-        mPendingEntry = mEntryManager.getNextEntry(mCurrentEntry.getId());
+        LongScreenshotsEntry newEntry = mEntryManager.getNextEntry(mCurrentEntry.getId());
+        if (newEntry == null) {
+            return;
+        }
+        mPendingEntry = newEntry;
         mPendingEntry.setListener(this);
 
         // Next entry is already generated/available.
@@ -111,7 +115,11 @@ public class LongScreenshotsMediator implements LongScreenshotsEntry.EntryListen
             return;
         }
 
-        mPendingEntry = mEntryManager.getPreviousEntry(mCurrentEntry.getId());
+        LongScreenshotsEntry newEntry = mEntryManager.getPreviousEntry(mCurrentEntry.getId());
+        if (newEntry == null) {
+            return;
+        }
+        mPendingEntry = newEntry;
         mPendingEntry.setListener(this);
 
         // Next entry is already generated/available.
