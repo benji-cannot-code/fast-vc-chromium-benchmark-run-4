abe2202acfc070d1487c5c86950fc704fe13e57b
Azeem Arshad <azeemarshad@chromium.org>
2021-03-05 23:19:21 +0000

    [CrOS Cellular] Add eid and iccid to network state.
    This CL exposes the Cellular ICCID and EID properties
    in the CellularStateProperties. Currently, EID and ICCID
    are only available in managed properties. This makes it
    very cumbersome for some UI operations since we need to
    make additional getManagedProperties calls to get these
    values.
    
    Bug: 1184749
    Change-Id: I335521e7c9f787df44d0826f578b18d50fd9ac60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732965
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860418}

--- ui/webui/resources/cr_components/chromeos/network/onc_mojo.js
+++ ui/webui/resources/cr_components/chromeos/network/onc_mojo.js
@@ -623,6 +623,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     switch (type) {
       case mojom.NetworkType.kCellular:
         result.typeState.cellular = {
+          iccid: '',
+          eid: '',
           activationState: mojom.ActivationStateType.kUnknown,
           networkTechnology: '',
           roaming: false,
