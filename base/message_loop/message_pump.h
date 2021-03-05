1c83040b2828f98d6c5b77c9f45ab9deb7aae018
Gabriel Charette <gab@chromium.org>
2021-03-05 22:17:22 +0000

    [base] Notify MessagePumpDelegate of work happening OnLibevent
    OnLibeventNotification being statically invoked, give it knowledge of
    the ongoing Run() via a RunState struct. Similar to the concept already
    used in MessagePumpWin (align that implementation as much as possible
    for consistency). This isn't common across all MessagePumps and as such
    minor code duplication is better than moving to message_pump.h. This is
    not the first instance of minor code duplication between various pump
    impls.
    
    R=fdoray@chromium.org
    
    Bug: 1184812
    Change-Id: Id007ae79ade85a37b5b1add6639529becddaf284
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737575
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860395}

--- base/message_loop/message_pump.h
+++ base/message_loop/message_pump.h
@@ -80,6 +80,8 @@ class BASE_EXPORT MessagePump {
 
     class ScopedDoNativeWork {
      public:
+      ScopedDoNativeWork() : outer_(nullptr) {}
+
       ~ScopedDoNativeWork() {
         if (outer_)
           outer_->OnEndNativeWork();
