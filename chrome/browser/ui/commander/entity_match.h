8b773f051222324f504101c486218c1a1fcce676
Leonard Grey <lgrey@chromium.org>
2021-03-05 21:13:46 +0000

    Commander: add tab group matching infra
    Bug: 1014639
    Change-Id: I21008fd2397b478fe5a535ecd2c0ab0e5313f637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2738596
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860359}

--- chrome/browser/ui/commander/entity_match.h
+++ chrome/browser/ui/commander/entity_match.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string16.h"
 #include "chrome/browser/ui/commander/command_source.h"
+#include "components/tab_groups/tab_group_id.h"
 #include "ui/gfx/range/range.h"
 
 class Browser;
@@ -33,6 +34,24 @@ struct WindowMatch {
   std::vector<gfx::Range> matched_ranges;
   double score;
 };
+// Intermediate result type for tab groups that are eligible to be
+// presented to the user as an option for a particular command.
+struct GroupMatch {
+  GroupMatch(tab_groups::TabGroupId group,
+             const base::string16& title,
+             double score);
+  ~GroupMatch();
+
+  GroupMatch(GroupMatch&& other);
+  GroupMatch& operator=(GroupMatch&& other);
+
+  std::unique_ptr<CommandItem> ToCommandItem() const;
+
+  tab_groups::TabGroupId group;
+  base::string16 title;
+  std::vector<gfx::Range> matched_ranges;
+  double score;
+};
 
 // Returns browser windows whose titles fuzzy match `input`. If input is empty,
 // returns all eligible browser windows with score reflecting MRU order.
@@ -41,6 +60,14 @@ struct WindowMatch {
 std::vector<WindowMatch> WindowsMatchingInput(const Browser* browser_to_exclude,
                                               const base::string16& input,
                                               bool match_profile = false);
+
+// Returns tab groups in `browser` whose titles fuzzy match `input`. If input is
+// empty, returns all groups in an arbitrary order. If `group_to_exclude` is
+// set, it is excluded from the list.
+std::vector<GroupMatch> GroupsMatchingInput(
+    const Browser* browser,
+    const base::string16& input,
+    base::Optional<tab_groups::TabGroupId> group_to_exclude = base::nullopt);
 }  // namespace commander
 
 #endif  // CHROME_BROWSER_UI_COMMANDER_ENTITY_MATCH_H_
