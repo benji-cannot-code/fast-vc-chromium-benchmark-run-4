f5a8d63d7ab972096def5ca95e114e64fdf6ae7b
Roger Tawa <rogerta@chromium.org>
2021-03-06 00:04:50 +0000

    For enterprise file system connector, match file and page URL.
    Bug: 1174642, 1176354
    Change-Id: I8b851179cc672de852e7638870a51524de4b0c43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737815
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Alice Gong <alicego@google.com>
    Cr-Commit-Position: refs/heads/master@{#860436}

--- chrome/browser/enterprise/connectors/file_system/service_settings.cc
+++ chrome/browser/enterprise/connectors/file_system/service_settings.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/enterprise/connectors/service_provider_config.h"
 #include "components/policy/core/browser/url_util.h"
 
-constexpr char kWildcardMimeType[] = "*";
-
 namespace enterprise_connectors {
 
 FileSystemServiceSettings::FileSystemServiceSettings(
