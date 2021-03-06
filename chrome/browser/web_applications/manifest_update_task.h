307627ab0937e7ecc568be4588411ef21acee722
Daniel Murphy <dmurph@chromium.org>
2021-03-06 02:21:09 +0000

    [WebAppProvider] Disabling manifest updating of the icon.
    This disables updating of web app icons unless there were no icons
    stored for the given webapp. This does NOT disable icon updating of the
    app shortcuts, that should still operate normally.
    
    Design Doc:
    https://docs.google.com/document/d/1n1qJkRIm3PDrM-dw5-0JKlLN1gh-tnWfY7rJzkBFa5o/edit#
    
    R=cmp@chromium.org
    
    Bug: 1182485
    Change-Id: I52f0360d8c178289f45bbd7735e8cc1b98f46c24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2721219
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860479}

--- chrome/browser/web_applications/manifest_update_task.h
+++ chrome/browser/web_applications/manifest_update_task.h
@@ -99,6 +99,7 @@ class ManifestUpdateTask final
     kPendingIconDownload,
     kPendingIconReadFromDisk,
     kPendingWindowsClosed,
+    kPendingMaybeReadExistingIcons,
     kPendingInstallation,
   };
 
@@ -116,6 +117,7 @@ class ManifestUpdateTask final
       const ShortcutsMenuIconBitmaps& disk_shortcuts_menu_icons) const;
   void UpdateAfterWindowsClose();
   void OnAllAppWindowsClosed();
+  void OnExistingIconsRead(IconBitmaps icon_bitmaps);
   void OnInstallationComplete(
       const AppId& app_id,
       InstallResultCode code);
