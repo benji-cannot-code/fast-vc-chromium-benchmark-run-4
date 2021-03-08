25a2258dd04f6d3d6032cb83613fe192bfe23a9f
Sylvain Defresne <sdefresne@chromium.org>
2021-03-08 11:44:47 +0000

    [ios] Remove obsolete gn variables
    Remove `ios_enable_multi_window` and `ios_enable_scene_startup` gn
    variables as they are always set to `true` and other values are no
    longer supported (they cause DCHECK when running the app on iOS 13
    or higher).
    
    Bug: none
    Change-Id: Icdf85d07b492f6a1e69f9e6e3bccb034d217f949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2738976
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860679}

--- ios/chrome/app/main_application_delegate.mm
+++ ios/chrome/app/main_application_delegate.mm
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/chrome_overlay_window.h"
 #import "ios/chrome/app/main_application_delegate_testing.h"
 #import "ios/chrome/app/main_controller.h"
-#include "ios/chrome/app/multi_window_buildflags.h"
 #import "ios/chrome/browser/ui/main/scene_controller.h"
 #import "ios/chrome/browser/ui/main/scene_delegate.h"
 #import "ios/chrome/browser/ui/main/scene_state.h"
@@ -236,7 +235,6 @@ const int kMainIntentCheckDelay = 1;
   [_memoryHelper handleMemoryPressure];
 }
 
-#if BUILDFLAG(IOS_MULTIWINDOW_ENABLED)
 - (void)application:(UIApplication*)application
     didDiscardSceneSessions:(NSSet<UISceneSession*>*)sceneSessions
     API_AVAILABLE(ios(13)) {
@@ -245,7 +243,6 @@ const int kMainIntentCheckDelay = 1;
       ->ApplicationDidDiscardSceneSessions(sceneSessions);
   [_appState application:application didDiscardSceneSessions:sceneSessions];
 }
-#endif  // BUILDFLAG(IOS_MULTIWINDOW_ENABLED)
 
 #pragma mark - Scenes lifecycle
 
