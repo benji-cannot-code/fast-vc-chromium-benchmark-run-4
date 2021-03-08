b28ee35c91b43aacb268b62395f8948742523ba1
bttk <bttk@chromium.org>
2021-03-08 20:36:11 +0000

    Record adaptive toolbar button metrics
    Add histograms to record which button variant is shown and which variant
    was clicked:
     - Android.AdaptiveToolbarButton.SessionVariant
     - Android.AdaptiveToolbarButton.Clicked
    
    Bug: 1182330
    Change-Id: I822d915f0ac729575878edc94039f80f5521bb46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720949
    Commit-Queue: bttk <bttk@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860838}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/adaptive/AdaptiveToolbarButtonControllerTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/adaptive/AdaptiveToolbarButtonControllerTest.java
@@ -9,6 +9,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import android.view.View;
+
 import androidx.test.filters.SmallTest;
 
 import org.junit.Assert;
@@ -20,11 +22,14 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.annotation.Config;
 
+import org.chromium.base.metrics.test.ShadowRecordHistogram;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.toolbar.ButtonData;
+import org.chromium.chrome.browser.toolbar.ButtonData.ButtonSpec;
 import org.chromium.chrome.browser.toolbar.ButtonDataImpl;
 import org.chromium.chrome.browser.toolbar.ButtonDataProvider;
 import org.chromium.chrome.browser.toolbar.ButtonDataProvider.ButtonDataObserver;
@@ -33,6 +38,7 @@ import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
 
 /** Unit tests for the {@code MagicToolbarButtonController} */
+@Config(manifest = Config.NONE, shadows = {ShadowRecordHistogram.class})
 @RunWith(BaseRobolectricTestRunner.class)
 public class AdaptiveToolbarButtonControllerTest {
     @Rule
@@ -50,7 +56,12 @@ public class AdaptiveToolbarButtonControllerTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        mButtonData = new ButtonDataImpl(true, null, null, 0, false, null, true);
+        ShadowRecordHistogram.reset();
+        AdaptiveToolbarFeatures.clearParsedParamsForTesting();
+        mButtonData = new ButtonDataImpl(
+                /*canShow=*/true, /*drawable=*/null, Mockito.mock(View.OnClickListener.class),
+                /*contentDescriptionResId=*/0, /*supportsTinting=*/false,
+                /*iphCommandBuilder=*/null, /*isEnabled=*/true);
     }
 
     @Test
@@ -99,12 +110,14 @@ public class AdaptiveToolbarButtonControllerTest {
     @EnableFeatures({ChromeFeatureList.ADAPTIVE_BUTTON_IN_TOP_TOOLBAR})
     public void testGet_alwaysShare() {
         AdaptiveToolbarFeatures.MODE_PARAM.setForTesting(AdaptiveToolbarFeatures.ALWAYS_SHARE);
+        mButtonData.setButtonSpec(makeButtonSpec(AdaptiveToolbarButtonVariant.SHARE));
+
         when(mShareButtonController.get(any())).thenReturn(mButtonData);
 
         AdaptiveToolbarButtonController adaptiveToolbarButtonController = buildController();
         ButtonData buttonData = adaptiveToolbarButtonController.get(mTab);
 
-        Assert.assertSame(mButtonData, buttonData);
+        Assert.assertEquals(101, buttonData.getButtonSpec().getContentDescriptionResId());
     }
 
     @Test
@@ -112,12 +125,48 @@ public class AdaptiveToolbarButtonControllerTest {
     @EnableFeatures({ChromeFeatureList.ADAPTIVE_BUTTON_IN_TOP_TOOLBAR})
     public void testGet_alwaysVoice() {
         AdaptiveToolbarFeatures.MODE_PARAM.setForTesting(AdaptiveToolbarFeatures.ALWAYS_VOICE);
+        mButtonData.setButtonSpec(makeButtonSpec(AdaptiveToolbarButtonVariant.VOICE));
+
         when(mVoiceToolbarButtonController.get(any())).thenReturn(mButtonData);
 
         AdaptiveToolbarButtonController adaptiveToolbarButtonController = buildController();
         ButtonData buttonData = adaptiveToolbarButtonController.get(mTab);
 
-        Assert.assertSame(mButtonData, buttonData);
+        Assert.assertEquals(101, buttonData.getButtonSpec().getContentDescriptionResId());
+    }
+
+    @Test
+    @SmallTest
+    @EnableFeatures({ChromeFeatureList.ADAPTIVE_BUTTON_IN_TOP_TOOLBAR})
+    public void testMetrics() {
+        AdaptiveToolbarFeatures.MODE_PARAM.setForTesting(AdaptiveToolbarFeatures.ALWAYS_SHARE);
+        mButtonData.setCanShow(true);
+        mButtonData.setEnabled(true);
+        mButtonData.setButtonSpec(makeButtonSpec(AdaptiveToolbarButtonVariant.SHARE));
+        when(mShareButtonController.get(any())).thenReturn(mButtonData);
+
+        AdaptiveToolbarButtonController adaptiveToolbarButtonController = buildController();
+        ButtonData buttonData = adaptiveToolbarButtonController.get(mTab);
+
+        Assert.assertEquals(1,
+                ShadowRecordHistogram.getHistogramValueCountForTesting(
+                        "Android.AdaptiveToolbarButton.SessionVariant",
+                        AdaptiveToolbarButtonVariant.SHARE));
+        Assert.assertEquals(1,
+                ShadowRecordHistogram.getHistogramTotalCountForTesting(
+                        "Android.AdaptiveToolbarButton.SessionVariant"));
+
+        View view = Mockito.mock(View.class);
+        buttonData.getButtonSpec().getOnClickListener().onClick(view);
+        buttonData.getButtonSpec().getOnClickListener().onClick(view);
+
+        Assert.assertEquals(2,
+                ShadowRecordHistogram.getHistogramValueCountForTesting(
+                        "Android.AdaptiveToolbarButton.Clicked",
+                        AdaptiveToolbarButtonVariant.SHARE));
+        Assert.assertEquals(2,
+                ShadowRecordHistogram.getHistogramTotalCountForTesting(
+                        "Android.AdaptiveToolbarButton.Clicked"));
     }
 
     @Test
@@ -164,5 +213,11 @@ public class AdaptiveToolbarButtonControllerTest {
         adaptiveToolbarButtonController.addButtonVariant(
                 AdaptiveToolbarButtonVariant.VOICE, mVoiceToolbarButtonController);
         return adaptiveToolbarButtonController;
+    }
+
+    private static ButtonSpec makeButtonSpec(@AdaptiveToolbarButtonVariant int variant) {
+        return new ButtonSpec(/*drawable=*/null, Mockito.mock(View.OnClickListener.class),
+                /*contentDescriptionResId=*/101, /*supportsTinting=*/false,
+                /*iphCommandBuilder=*/null, variant);
     }
 }
