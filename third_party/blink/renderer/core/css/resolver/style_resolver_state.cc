1f963526d5c0297d714bdcc7cd3980b7426da274
Anders Hartvoll Ruud <andruud@chromium.org>
2021-03-08 14:04:34 +0000

    Revert "Remove remnants of PseudoElementStyleRequest"
    This reverts commit 9439269b918b8b1e917d4e79facbf5b4db81223e.
    
    Reason for revert: Investigating crash.corp breakage.
    
    Original change's description:
    > Remove remnants of PseudoElementStyleRequest
    >
    > Change-Id: I03342cc8d2ebdcaf5d2f2274c416040846f3f895
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732092
    > Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#860559}
    
    Change-Id: I60bda000732542faf1c401f474d8f225ef641c2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2741127
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860701}

--- third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
@@ -39,7 +39,7 @@ StyleResolverState::StyleResolverState(
     Document& document,
     Element& element,
     PseudoElement* pseudo_element,
-    StyleRequest::RequestType pseudo_request_type,
+    PseudoElementStyleRequest::RequestType pseudo_request_type,
     ElementType element_type,
     const ComputedStyle* parent_style,
     const ComputedStyle* layout_parent_style)
@@ -76,7 +76,7 @@ StyleResolverState::StyleResolverState(Document& document,
     : StyleResolverState(document,
                          element,
                          nullptr /* pseudo_element */,
-                         StyleRequest::kForRenderer,
+                         PseudoElementStyleRequest::kForRenderer,
                          ElementType::kElement,
                          parent_style,
                          layout_parent_style) {}
@@ -85,7 +85,7 @@ StyleResolverState::StyleResolverState(
     Document& document,
     Element& element,
     PseudoId pseudo_id,
-    StyleRequest::RequestType pseudo_request_type,
+    PseudoElementStyleRequest::RequestType pseudo_request_type,
     const ComputedStyle* parent_style,
     const ComputedStyle* layout_parent_style)
     : StyleResolverState(document,
@@ -125,7 +125,7 @@ void StyleResolverState::SetStyle(scoped_refptr<ComputedStyle> style) {
 
 scoped_refptr<ComputedStyle> StyleResolverState::TakeStyle() {
   if (had_no_matched_properties_ &&
-      pseudo_request_type_ == StyleRequest::kForRenderer) {
+      pseudo_request_type_ == PseudoElementStyleRequest::kForRenderer) {
     return nullptr;
   }
   return std::move(style_);
@@ -163,7 +163,7 @@ void StyleResolverState::SetLayoutParentStyle(
 }
 
 void StyleResolverState::LoadPendingResources() {
-  if (pseudo_request_type_ == StyleRequest::kForComputedStyle ||
+  if (pseudo_request_type_ == PseudoElementStyleRequest::kForComputedStyle ||
       (ParentStyle() && ParentStyle()->IsEnsuredInDisplayNone()) ||
       (StyleRef().Display() == EDisplay::kNone &&
        !GetElement().LayoutObjectIsNeeded(StyleRef())) ||
--- third_party/blink/renderer/core/css/resolver/style_resolver_state.h
+++ third_party/blink/renderer/core/css/resolver/style_resolver_state.h
@@ -61,7 +61,7 @@ class CORE_EXPORT StyleResolverState {
   StyleResolverState(Document&,
                      Element&,
                      PseudoId,
-                     StyleRequest::RequestType,
+                     PseudoElementStyleRequest::RequestType,
                      const ComputedStyle* parent_style,
                      const ComputedStyle* layout_parent_style);
   StyleResolverState(Document&, Element&, const StyleRequest&);
@@ -186,7 +186,7 @@ class CORE_EXPORT StyleResolverState {
   StyleResolverState(Document&,
                      Element&,
                      PseudoElement*,
-                     StyleRequest::RequestType,
+                     PseudoElementStyleRequest::RequestType,
                      ElementType,
                      const ComputedStyle* parent_style,
                      const ComputedStyle* layout_parent_style);
@@ -212,7 +212,7 @@ class CORE_EXPORT StyleResolverState {
 
   CSSAnimationUpdate animation_update_;
   bool is_animation_interpolation_map_ready_ = false;
-  StyleRequest::RequestType pseudo_request_type_;
+  PseudoElementStyleRequest::RequestType pseudo_request_type_;
 
   FontBuilder font_builder_;
 
