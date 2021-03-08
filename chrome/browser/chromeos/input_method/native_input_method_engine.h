d20eeadf00ee172b3db08258199e8663bb5bcf10
Darren Shen <shend@chromium.org>
2021-03-08 04:08:26 +0000

    ime: Only enable system PK when autocorrect prefs is enabled.
    Right now, system PK will always be launched when the flag is enabled.
    To maintain the current behaviour, system PK should look at the
    PK autocorrect setting. If the user doesn't want autocorrect, then
    we don't need to launch IME service at all.
    
    Add a check for the PK autocorrect prefs when activating an IME.
    If the user is using an FST language, but autocorrect is disabled,
    we will just reset the IME service connection. Functions that forward
    events to IME service need to check if the IME service connection
    is active or not.
    
    Also add a Prefs observer so that when the user changes the setting,
    IME service is activated/deactivated immediately.
    
    Bug: b/181077907
    Change-Id: I83342a973e058296538e8e7cb80ddde7e801f259
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717152
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860621}

--- chrome/browser/chromeos/input_method/native_input_method_engine.h
+++ chrome/browser/chromeos/input_method/native_input_method_engine.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/input_method/input_method_engine.h"
 #include "chrome/browser/ui/ash/keyboard/chrome_keyboard_controller_client.h"
 #include "chromeos/services/ime/public/mojom/input_engine.mojom-forward.h"
+#include "components/prefs/pref_change_registrar.h"
+#include "components/prefs/pref_service.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
 namespace chromeos {
@@ -72,6 +74,7 @@ class NativeInputMethodEngine
     // migration. It will be removed when the official extension is completely
     // migrated.
     ImeObserver(
+        PrefService* prefs,
         std::unique_ptr<InputMethodEngineBase::Observer> ime_base_observer,
         std::unique_ptr<AssistiveSuggester> assistive_suggester,
         std::unique_ptr<AutocorrectManager> autocorrect_manager);
@@ -142,7 +145,7 @@ class NativeInputMethodEngine
     void FlushForTesting();
 
     // Returns whether this is connected to the input engine.
-    bool IsConnectedForTesting() const { return active_engine_id_.has_value(); }
+    bool IsConnectedForTesting() const { return remote_to_engine_.is_bound(); }
 
    private:
     // Called when this is connected to the input engine. |bound| indicates
@@ -158,6 +161,8 @@ class NativeInputMethodEngine
         ui::IMEEngineHandlerInterface::KeyEventDoneCallback callback,
         ime::mojom::KeypressResponseForRulebasedPtr response);
 
+    PrefService* prefs_ = nullptr;
+
     std::unique_ptr<InputMethodEngineBase::Observer> ime_base_observer_;
     mojo::Remote<ime::mojom::InputEngineManager> remote_manager_;
     mojo::Receiver<ime::mojom::InputChannel> receiver_from_engine_;
@@ -170,11 +175,15 @@ class NativeInputMethodEngine
 
   ImeObserver* GetNativeObserver() const;
 
+  void OnInputMethodPrefsChanged();
+
   AssistiveSuggester* assistive_suggester_ = nullptr;
   AutocorrectManager* autocorrect_manager_ = nullptr;
   base::ScopedObservation<ChromeKeyboardControllerClient,
                           ChromeKeyboardControllerClient::Observer>
       chrome_keyboard_controller_client_observer_{this};
+
+  PrefChangeRegistrar pref_change_registrar_;
 };
 
 }  // namespace chromeos
--- chrome/browser/chromeos/input_method/native_input_method_engine_unittest.cc
+++ chrome/browser/chromeos/input_method/native_input_method_engine_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "chrome/browser/chromeos/input_method/stub_input_method_engine_observer.h"
 #include "chrome/browser/ui/ash/keyboard/chrome_keyboard_controller_client_test_helper.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chromeos/services/ime/mock_input_channel.h"
 #include "chromeos/services/ime/public/mojom/input_engine.mojom.h"
@@ -36,6 +37,16 @@ using testing::NiceMock;
 using testing::StrictMock;
 
 constexpr char kEngineIdUs[] = "xkb:us::eng";
+
+void SetPhysicalTypingAutocorrectEnabled(TestingProfile& profile,
+                                         bool enabled) {
+  base::Value input_method_setting(base::Value::Type::DICTIONARY);
+  input_method_setting.SetPath(
+      std::string(kEngineIdUs) + ".physicalKeyboardAutoCorrectionLevel",
+      base::Value(enabled ? 1 : 0));
+  profile.GetPrefs()->Set(prefs::kLanguageInputMethodSpecificSettings,
+                          input_method_setting);
+}
 
 class TestInputEngineManager : public ime::mojom::InputEngineManager {
  public:
@@ -108,12 +119,66 @@ class NativeInputMethodEngineTest : public ::testing::Test {
       keyboard_controller_client_test_helper_;
 };
 
-TEST_F(NativeInputMethodEngineTest, EnableCallsRightMojoFunctions) {
+TEST_F(NativeInputMethodEngineTest, DoesNotLaunchImeServiceIfAutocorrectIsOff) {
+  TestingProfile testing_profile;
+  SetPhysicalTypingAutocorrectEnabled(testing_profile, false);
+
   testing::StrictMock<ime::MockInputChannel> mock_input_channel;
   input_method::InputMethodManager::Initialize(
       new TestInputMethodManager(&mock_input_channel));
   NativeInputMethodEngine engine;
+  engine.Initialize(std::make_unique<StubInputMethodEngineObserver>(),
+                    /*extension_id=*/"", &testing_profile);
+
+  engine.Enable(kEngineIdUs);
+  EXPECT_FALSE(engine.IsConnectedForTesting());
+
+  InputMethodManager::Shutdown();
+}
+
+TEST_F(NativeInputMethodEngineTest, LaunchesImeServiceIfAutocorrectIsOn) {
   TestingProfile testing_profile;
+  SetPhysicalTypingAutocorrectEnabled(testing_profile, true);
+
+  testing::StrictMock<ime::MockInputChannel> mock_input_channel;
+  input_method::InputMethodManager::Initialize(
+      new TestInputMethodManager(&mock_input_channel));
+  NativeInputMethodEngine engine;
+  engine.Initialize(std::make_unique<StubInputMethodEngineObserver>(),
+                    /*extension_id=*/"", &testing_profile);
+
+  engine.Enable(kEngineIdUs);
+  EXPECT_TRUE(engine.IsConnectedForTesting());
+
+  InputMethodManager::Shutdown();
+}
+
+TEST_F(NativeInputMethodEngineTest, TogglesImeServiceWhenAutocorrectChanges) {
+  TestingProfile testing_profile;
+  testing::StrictMock<ime::MockInputChannel> mock_input_channel;
+  input_method::InputMethodManager::Initialize(
+      new TestInputMethodManager(&mock_input_channel));
+  NativeInputMethodEngine engine;
+  engine.Initialize(std::make_unique<StubInputMethodEngineObserver>(),
+                    /*extension_id=*/"", &testing_profile);
+  engine.Enable(kEngineIdUs);
+
+  SetPhysicalTypingAutocorrectEnabled(testing_profile, true);
+  EXPECT_TRUE(engine.IsConnectedForTesting());
+  SetPhysicalTypingAutocorrectEnabled(testing_profile, false);
+  EXPECT_FALSE(engine.IsConnectedForTesting());
+
+  InputMethodManager::Shutdown();
+}
+
+TEST_F(NativeInputMethodEngineTest, EnableCallsRightMojoFunctions) {
+  TestingProfile testing_profile;
+  SetPhysicalTypingAutocorrectEnabled(testing_profile, true);
+
+  testing::StrictMock<ime::MockInputChannel> mock_input_channel;
+  input_method::InputMethodManager::Initialize(
+      new TestInputMethodManager(&mock_input_channel));
+  NativeInputMethodEngine engine;
   engine.Initialize(std::make_unique<StubInputMethodEngineObserver>(),
                     /*extension_id=*/"", &testing_profile);
 
@@ -126,11 +191,13 @@ TEST_F(NativeInputMethodEngineTest, EnableCallsRightMojoFunctions) {
 }
 
 TEST_F(NativeInputMethodEngineTest, FocusCallsRightMojoFunctions) {
+  TestingProfile testing_profile;
+  SetPhysicalTypingAutocorrectEnabled(testing_profile, true);
+
   testing::StrictMock<ime::MockInputChannel> mock_input_channel;
   input_method::InputMethodManager::Initialize(
       new TestInputMethodManager(&mock_input_channel));
   NativeInputMethodEngine engine;
-  TestingProfile testing_profile;
   engine.Initialize(std::make_unique<StubInputMethodEngineObserver>(),
                     /*extension_id=*/"", &testing_profile);
 
@@ -156,12 +223,14 @@ TEST_F(NativeInputMethodEngineTest, FocusCallsRightMojoFunctions) {
 }
 
 TEST_F(NativeInputMethodEngineTest, HandleAutocorrectChangesAutocorrectRange) {
+  TestingProfile testing_profile;
+  SetPhysicalTypingAutocorrectEnabled(testing_profile, true);
+
   testing::NiceMock<ime::MockInputChannel> mock_input_channel;
   mojo::Remote<ime::mojom::InputChannel> remote;
   input_method::InputMethodManager::Initialize(
       new TestInputMethodManager(&mock_input_channel, &remote));
   NativeInputMethodEngine engine;
-  TestingProfile testing_profile;
   engine.Initialize(std::make_unique<StubInputMethodEngineObserver>(),
                     /*extension_id=*/"", &testing_profile);
   ui::IMEEngineHandlerInterface::InputContext input_context(
