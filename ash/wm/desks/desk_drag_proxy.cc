2965a18acd16c0b3dba6bae1554dc0272bf10425
Xiaodan Zhu <zxdan@chromium.org>
2021-03-08 21:09:17 +0000

    Fix the behaviors when a desk is snapping back.
    This CL fixs the wrong behaviors of dragging a desk while another
    desk is snapping back.
    
    The wrong behaviors are due to the old function call logic.
    
    For mouse event, new logic is fixed as below:
    
    - Add OnMousePressed in DeskPreviewView to call HandlePressEvent in
    the DesksBarView.
    - HandlePressEvent calls InitDragDesk to initialize a drag proxy.
    - InitDragDesk ends current drag and estimate the initial offset for
    the new drag but don't create the drag proxy widget.
    - OnMouseDragged in DesksPreviewView calls HandleDragEvent in the
    DesksBarView.
    - HandleDragEvent calls StartDragDesk if drag proxy is initialized.
    If the drag has already started, then call ContinueDragDesk.
    - OnMouseReleased in DesksPreviewView calls HandleReleaseEvent in
    DesksBarView.
    - HandleReleaseEvent calls EndDragDesk if the drag is started.
    
    For gesture event, the function call changes as follow:
    
    - ET_GESTURE_LONG_PRESS in DesksPreviewView calls
    HandleLongPressEvent in the DesksBarView to initialize and start
    a drag.
    
    Bug: 1181089
    Change-Id: Ibfae484963523cd32971cb66e29416bf9b2ae159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2730508
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860855}

--- ash/wm/desks/desk_drag_proxy.cc
+++ ash/wm/desks/desk_drag_proxy.cc
@@ -37,7 +37,25 @@ DeskDragProxy::DeskDragProxy(DesksBarView* desks_bar_view,
     : desks_bar_view_(desks_bar_view),
       drag_view_(drag_view),
       drag_preview_size_(drag_view->GetPreviewBoundsInScreen().size()),
-      init_offset_(init_offset) {
+      init_offset_(init_offset) {}
+
+DeskDragProxy::~DeskDragProxy() = default;
+
+void DeskDragProxy::OnImplicitAnimationsCompleted() {
+  DCHECK(desks_bar_view_);
+
+  state_ = State::kEnded;
+
+  // |this| is destroyed here.
+  desks_bar_view_->FinalizeDragDesk();
+}
+
+gfx::Point DeskDragProxy::GetPositionInScreen() const {
+  return drag_widget_->GetWindowBoundsInScreen().origin();
+}
+
+void DeskDragProxy::InitAndScaleAndMoveTo(
+    const gfx::PointF& location_in_screen) {
   DCHECK(drag_view_);
 
   aura::Window* root_window =
@@ -56,23 +74,8 @@ DeskDragProxy::DeskDragProxy(DesksBarView* desks_bar_view,
   drag_widget_->SetBounds(drag_view_->GetPreviewBoundsInScreen());
 
   drag_widget_->Show();
-}
 
-DeskDragProxy::~DeskDragProxy() = default;
-
-void DeskDragProxy::OnImplicitAnimationsCompleted() {
-  DCHECK(desks_bar_view_);
-
-  desks_bar_view_->FinalizeDragDesk();
-}
-
-gfx::Point DeskDragProxy::GetPositionInScreen() const {
-  return drag_widget_->GetWindowBoundsInScreen().origin();
-}
-
-void DeskDragProxy::ScaleAndMoveTo(const gfx::PointF& location_in_screen) {
   ui::Layer* layer = drag_widget_->GetLayer();
-
   // Perform and animate scaling.
   gfx::Transform scale_transform;
   scale_transform.Scale(kDragProxyScale, kDragProxyScale);
@@ -87,6 +90,8 @@ void DeskDragProxy::ScaleAndMoveTo(const gfx::PointF& location_in_screen) {
 
   // Perform Moving.
   DragTo(location_in_screen);
+
+  state_ = State::kStarted;
 }
 
 void DeskDragProxy::DragTo(const gfx::PointF& location_in_screen) {
@@ -96,12 +101,7 @@ void DeskDragProxy::DragTo(const gfx::PointF& location_in_screen) {
 }
 
 void DeskDragProxy::SnapBackToDragView() {
-  ui::Layer* layer = drag_widget_->GetLayer();
-
-  // Do not snap back again if the proxy is already doing it.
-  if (layer->GetAnimator()->is_animating() &&
-      layer->GetTargetTransform().IsIdentity())
-    return;
+  DCHECK_NE(state_, State::kSnappingBack);
 
   // Cache proxy's bounds and drag view's bounds.
   gfx::RectF scaled_proxy_bounds(drag_widget_->GetWindowBoundsInScreen());
@@ -111,6 +111,7 @@ void DeskDragProxy::SnapBackToDragView() {
   // Set bounds of drag view to drag proxy.
   drag_widget_->SetBounds(drag_view_bounds);
 
+  ui::Layer* layer = drag_widget_->GetLayer();
   // Animate snapping back.
   layer->SetTransform(gfx::TransformBetweenRects(gfx::RectF(drag_view_bounds),
                                                  scaled_proxy_bounds));
@@ -119,6 +120,8 @@ void DeskDragProxy::SnapBackToDragView() {
   settings.SetTweenType(gfx::Tween::ACCEL_LIN_DECEL_60);
   settings.AddObserver(this);
   layer->SetTransform(gfx::Transform());
+
+  state_ = State::kSnappingBack;
 }
 
 }  // namespace ash
