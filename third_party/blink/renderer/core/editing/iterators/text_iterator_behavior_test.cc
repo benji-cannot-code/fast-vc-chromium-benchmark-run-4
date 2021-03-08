f21f531d30da3f29b39b6b34e941169bf9c96e35
Yoshifumi Inoue <yosin@chromium.org>
2021-03-08 07:48:09 +0000

    Get rid of unused functions TextIteratorBehavior::{Set,}ForInnerText()
    This patch gets rid of unused functions |TextIteratorBehavior::
    {Set,}ForInnerText()| for improving code health, because we don't use
    |TextIterator| when we implement |Element#innerText| not using
    |TextIterator|[1].
    
    
    [1] http://crrev.com/c/1114673 Implement Element#innerText to conform
    the spec
    
    Change-Id: Ic4423c1665a1e2d27323b5faecad1e86cc54896c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2738910
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860644}

--- third_party/blink/renderer/core/editing/iterators/text_iterator_behavior_test.cc
+++ third_party/blink/renderer/core/editing/iterators/text_iterator_behavior_test.cc
@@ -12,21 +12,11 @@ namespace blink {
 TEST(TextIteratorBehaviorTest, Basic) {
   EXPECT_TRUE(TextIteratorBehavior() == TextIteratorBehavior());
   EXPECT_FALSE(TextIteratorBehavior() != TextIteratorBehavior());
-  EXPECT_NE(TextIteratorBehavior(),
-            TextIteratorBehavior::Builder().SetForInnerText(true).Build());
-  EXPECT_NE(TextIteratorBehavior::Builder().SetForInnerText(true).Build(),
-            TextIteratorBehavior());
-  EXPECT_NE(TextIteratorBehavior::Builder().SetForInnerText(true).Build(),
-            TextIteratorBehavior::Builder().SetEmitsImageAltText(true).Build());
-  EXPECT_NE(TextIteratorBehavior::Builder().SetEmitsImageAltText(true).Build(),
-            TextIteratorBehavior::Builder().SetForInnerText(true).Build());
   EXPECT_EQ(TextIteratorBehavior::Builder()
-                .SetForInnerText(true)
                 .SetEmitsImageAltText(true)
                 .Build(),
             TextIteratorBehavior::Builder()
                 .SetEmitsImageAltText(true)
-                .SetForInnerText(true)
                 .Build());
 }
 
@@ -67,10 +57,6 @@ TEST(TextIteratorBehaviorTest, Values) {
                   .SetExcludeAutofilledValue(true)
                   .Build()
                   .ExcludeAutofilledValue());
-  EXPECT_TRUE(TextIteratorBehavior::Builder()
-                  .SetForInnerText(true)
-                  .Build()
-                  .ForInnerText());
   EXPECT_TRUE(TextIteratorBehavior::Builder()
                   .SetForSelectionToString(true)
                   .Build()
