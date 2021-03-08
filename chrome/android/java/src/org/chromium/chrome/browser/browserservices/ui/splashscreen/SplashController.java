021910260e27f64d8b268cefcd65f29ae8e86bd4
Ella Ge <eirage@chromium.org>
2021-03-08 21:44:16 +0000

    Remove ChromeActivity usge from SplashController
    Change #isActivityFinishingOrDestroyed to use the helper in
    LifecycleDispatcher.
    Others use the generic Activity
    
    Bug: 1155353
    Change-Id: I853fc0ad6fe77e515de902083beb7fcff32f60ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2738599
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860873}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/ui/splashscreen/SplashController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/ui/splashscreen/SplashController.java
@@ -21,7 +21,6 @@ import androidx.annotation.VisibleForTesting;
 import org.chromium.base.ObserverList;
 import org.chromium.base.TraceEvent;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.browserservices.trustedwebactivityui.TwaFinishHandler;
 import org.chromium.chrome.browser.compositor.CompositorViewHolder;
 import org.chromium.chrome.browser.customtabs.BaseCustomTabActivity;
@@ -87,7 +86,7 @@ public class SplashController
 
     private static final String TAG = "SplashController";
 
-    private final ChromeActivity<?> mActivity;
+    private final Activity mActivity;
     private final ActivityLifecycleDispatcher mLifecycleDispatcher;
     private final TabObserverRegistrar mTabObserverRegistrar;
     private final TwaFinishHandler mFinishHandler;
@@ -125,8 +124,7 @@ public class SplashController
     private ObserverList<SplashscreenObserver> mObservers;
 
     @Inject
-    public SplashController(ChromeActivity<?> activity,
-            ActivityLifecycleDispatcher lifecycleDispatcher,
+    public SplashController(Activity activity, ActivityLifecycleDispatcher lifecycleDispatcher,
             TabObserverRegistrar tabObserverRegistrar,
             CustomTabOrientationController orientationController, TwaFinishHandler finishHandler,
             CustomTabActivityTabProvider tabProvider,
@@ -301,7 +299,7 @@ public class SplashController
 
     /** Hides the splash screen. */
     private void hideSplash(final Tab tab, boolean loadFailed) {
-        if (mActivity.isActivityFinishingOrDestroyed()) {
+        if (mLifecycleDispatcher.isActivityFinishingOrDestroyed()) {
             return;
         }
 
