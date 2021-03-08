017927bd73d35387c13eb4d3099c9fe98538ec92
Mandy Chen <mandy.chen@microsoft.com>
2021-03-08 22:39:09 +0000

    Refactor UpdateOsHook to allow unit testing
    Extract existing update code into functions to allow unit testing. Added
    corresponding test.
    
    Bug: 1072058
    Change-Id: Id020a9345d8f4ef045c1425d704659f6edda5531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2730043
    Commit-Queue: Mandy Chen <mandy.chen@microsoft.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860895}

--- chrome/browser/web_applications/components/os_integration_manager.h
+++ chrome/browser/web_applications/components/os_integration_manager.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/callback_forward.h"
 #include "base/memory/weak_ptr.h"
+#include "base/strings/string_piece_forward.h"
 #include "chrome/browser/web_applications/components/app_registrar.h"
 #include "chrome/browser/web_applications/components/app_shortcut_manager.h"
 #include "chrome/browser/web_applications/components/file_handler_manager.h"
@@ -211,6 +212,9 @@ class OsIntegrationManager {
   virtual void UnregisterWebAppOsUninstallation(const AppId& app_id);
 
   // Update:
+  virtual void UpdateShortcuts(const AppId& app_id, base::StringPiece old_name);
+  virtual void UpdateShortcutsMenu(const AppId& app_id,
+                                   const WebApplicationInfo& web_app_info);
   virtual void UpdateUrlHandlers(const AppId& app_id);
 
   // Utility methods:
