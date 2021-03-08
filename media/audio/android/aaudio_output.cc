de4ac971bfc96c50dfa9d02a2b675ce8a2d99cc8
Hongchan Choi <hongchan@chromium.org>
2021-03-08 18:08:35 +0000

    Use AAUDIO_PERFORMANCE_MODE_LOW_LATENCY for EXACT_MS latency case.
    When AudioContext is created with a double value of `latencyHint`,
    AudioLatency::LATENCY_EXACT_MS setting is used. However, this latency
    profile is not handled properly in aaudio_output.cc, so it defaults
    to `AAUDIO_PERFORMANCE_MODE_NONE`. This default mode makes the
    callback interval very unstable and irregular, and it causes the
    Web Audio renderer to glitch.
    
    This CL simply adds one line to handle `LATENCY_EXACT_MS` to set
    up the AAudio performance mode properly.
    
    Bug: 1158444
    Test: The repro case does not glitch anymore in Pixel devices.
    Change-Id: Ib24e64809546cd2373f767b268b47fbbad230e0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2699228
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860771}

--- media/audio/android/aaudio_output.cc
+++ media/audio/android/aaudio_output.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/android/aaudio_output.h"
 
 #include "base/logging.h"
+#include "base/trace_event/trace_event.h"
 #include "media/audio/android/aaudio_stubs.h"
 #include "media/audio/android/audio_manager_android.h"
 #include "media/base/audio_bus.h"
@@ -44,6 +45,7 @@ AAudioOutputStream::AAudioOutputStream(AudioManagerAndroid* manager,
 
   if (AudioManagerAndroid::SupportsPerformanceModeForOutput()) {
     switch (params.latency_tag()) {
+      case AudioLatency::LATENCY_EXACT_MS:
       case AudioLatency::LATENCY_INTERACTIVE:
       case AudioLatency::LATENCY_RTC:
         performance_mode_ = AAUDIO_PERFORMANCE_MODE_LOW_LATENCY;
@@ -55,6 +57,12 @@ AAudioOutputStream::AAudioOutputStream(AudioManagerAndroid* manager,
         performance_mode_ = AAUDIO_PERFORMANCE_MODE_NONE;
     }
   }
+
+  TRACE_EVENT2("audio", "AAudioOutputStream::AAudioOutputStream",
+               "AAUDIO_PERFORMANCE_MODE_LOW_LATENCY",
+               performance_mode_ == AAUDIO_PERFORMANCE_MODE_LOW_LATENCY
+                   ? "true" : "false",
+               "frames_per_buffer", params.frames_per_buffer());
 }
 
 AAudioOutputStream::~AAudioOutputStream() {
@@ -101,6 +109,10 @@ bool AAudioOutputStream::Open() {
   AAudioStream_setBufferSizeInFrames(aaudio_stream_, sizeRequested);
 
   audio_bus_ = AudioBus::Create(params_);
+
+  TRACE_EVENT2("audio", "AAudioOutputStream::Open",
+               "params_", params_.AsHumanReadableString(),
+               "requested BufferSizeInFrames", sizeRequested);
 
   return true;
 }
