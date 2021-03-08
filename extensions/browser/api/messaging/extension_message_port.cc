bd66c49ad61752788be1191c2a068164c953bbae
Marc Treib <treib@chromium.org>
2021-03-08 15:09:32 +0000

    Revert "Close messaging channel if its worker shuts down without any receivers."
    This reverts commit ab424bb4fa0c3b093573b97e6a64993dc984673b.
    
    Reason for revert: The newly-added WorkerShutsDownWhileNativeMessagePortIsOpen test crashes, see crbug.com/1185742
    
    Original change's description:
    > Close messaging channel if its worker shuts down without any receivers.
    >
    > Similar to existing RenderFrameHost shutdown scenario, this CL makes
    > the service worker specific ExtensionMessagePort (created via
    > ExtensionMessagePort::CreateForEndpoint) observe worker unregistering
    > via ProcessManagerObserver. On worker shutdown,
    > EMP::OnServiceWorkerUnregistered closes the channel if the port doesn’t
    > have any receivers.
    > This was especially problematic for ports from worker to native message
    > hosts, as keeping the channel open would allow the native hosts to keep
    > replying to the (invalid) worker endpoint, causing a browser crash.
    >
    > This CL adds a test that ensures that channel count is reduced as worker shuts down.
    >
    > This CL also test specific exposes channel count method to
    > MessageService.
    >
    > Bug: 1176400
    > Test: See bug description for test case
    > Change-Id: Ie73a419d94894d51204ec4ff0405fadc0a1d3148
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2734264
    > Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    > Reviewed-by: David Bertoni <dbertoni@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#860453}
    
    Bug: 1176400, 1185742
    Change-Id: I007ad968314113fe8f79881ef33155b109e99e06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2742518
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860717}

--- extensions/browser/api/messaging/extension_message_port.cc
+++ extensions/browser/api/messaging/extension_message_port.cc
@@ -152,11 +152,9 @@ ExtensionMessagePort::ExtensionMessagePort(
 ExtensionMessagePort::ExtensionMessagePort(
     base::WeakPtr<ChannelDelegate> channel_delegate,
     const PortId& port_id,
-    const ExtensionId& extension_id,
     content::BrowserContext* browser_context)
     : weak_channel_delegate_(channel_delegate),
       port_id_(port_id),
-      extension_id_(extension_id),
       browser_context_(browser_context) {}
 
 // static
@@ -175,9 +173,7 @@ std::unique_ptr<ExtensionMessagePort> ExtensionMessagePort::CreateForEndpoint(
   // NOTE: We don't want all the workers within the extension, so we cannot
   // reuse other constructor from above.
   std::unique_ptr<ExtensionMessagePort> port(new ExtensionMessagePort(
-      channel_delegate, port_id, extension_id, endpoint.browser_context()));
-  port->frame_tracker_ = std::make_unique<FrameTracker>(port.get());
-  port->frame_tracker_->TrackExtensionProcessFrames();
+      channel_delegate, port_id, endpoint.browser_context()));
   port->RegisterWorker(endpoint.GetWorkerId());
   return port;
 }
--- extensions/browser/api/messaging/message_service.h
+++ extensions/browser/api/messaging/message_service.h
@@ -118,9 +118,6 @@ class MessageService : public BrowserContextKeyedAPI,
                  const PortContext& port_context,
                  bool force_close);
 
-  // Returns the number of open channels for test.
-  size_t GetChannelCountForTest() { return channels_.size(); }
-
   base::WeakPtr<MessagePort::ChannelDelegate> GetChannelDelegate() {
     return weak_factory_.GetWeakPtr();
   }
