24297126562a601d7623ae6c74f09adaba6403c2
David Benjamin <davidben@chromium.org>
2021-03-08 23:39:37 +0000

    Roll src/third_party/boringssl/src f6bd54efb..dfe0b01b3
    https://boringssl.googlesource.com/boringssl/+log/f6bd54efbcafcf4625ce99b5f702dc4850b0ca50..dfe0b01b3eb3773d16a099cb1f900f9ca48fc842
    
    The following commits have update notes:
      d4fdb7bae Make the X509_VAL structure opaque.
      a1d1a6758 Remove some remnants of TLS 1.3 downgrade carveouts.
      e9315d5f8 Remove X509_REQ_to_X509.
      eb5640bc4 Check the inner and outer CRL signature algorithms match.
      a3aeea7c7 Remove block_mask from EVP_CIPHER_CTX.
    
    This requires adding some more wrappers to wincrypt_shim.h because it
    turns out the OpenSSL PKCS7_SIGNER_INFO type conflicts with another
    Windows macro. (We don't get a choice in the name of the type. It's
    constrained by OpenSSL compatibility.)
    
    Bug: none
    Change-Id: I3c9ada72d3bd224a964a05d338045be3c77c575a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2733600
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860926}

--- base/win/wincrypt_shim.h
+++ base/win/wincrypt_shim.h
@@ -16,10 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Undefine the macros which conflict with OpenSSL and define replacements. See
 // http://msdn.microsoft.com/en-us/library/windows/desktop/aa378145(v=vs.85).aspx
+#undef PKCS7_SIGNER_INFO
 #undef X509_CERT_PAIR
 #undef X509_EXTENSIONS
 #undef X509_NAME
 
+#define WINCRYPT_PKCS7_SIGNER_INFO ((LPCSTR)500)
 #define WINCRYPT_X509_CERT_PAIR ((LPCSTR)53)
 #define WINCRYPT_X509_EXTENSIONS ((LPCSTR)5)
 #define WINCRYPT_X509_NAME ((LPCSTR)7)
