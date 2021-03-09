ada22df6e92a31f4e7e29af55d5a96848a1f4666
jlebel <jlebel@chromium.org>
2021-03-09 19:11:15 +0000

    [iOS] Add ChildBottomSheetViewController
    The layout fitting size of the child bottom view controller is now
    returned by the child controller itself,  instead of asking
    ConsistencyPromoSigninCoordinator.
    
    Add ChildBottomSheetViewController protocol:
    - (CGFloat)layoutFittingHeightForWidth:(CGFloat)width;
    
    Bug: 1151288
    Change-Id: I29bb07b770127b580743e4a8d430f45ec493f167
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732427
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861237}

--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/bottom_sheet/bottom_sheet_navigation_controller.h
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/bottom_sheet/bottom_sheet_navigation_controller.h
@@ -18,12 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)bottomSheetNavigationControllerDidDisappear:
     (BottomSheetNavigationController*)viewController;
 
-// Returns the desired height for |viewController| to fit. The height needs to
-// include safe area insets.
-- (CGFloat)layoutFittingHeightForViewController:
-               (UIViewController*)viewController
-                                          width:(CGFloat)width;
-
 @end
 
 // Navigation controller presented from the bottom. The pushed view controllers
@@ -32,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The view is automatically sized according to the last child view controller.
 // This class works with BottomSheetPresentationController and
 // BottomSheetSlideTransitionAnimator.
+// Child view controller are required to implement
+// ChildBottomSheetViewController protocol.
 @interface BottomSheetNavigationController : UINavigationController
 
 // Presentation delegate.
--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
@@ -118,14 +118,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                    showAdvancedSettingsSignin:NO];
 }
 
-- (CGFloat)layoutFittingHeightForViewController:
-               (UIViewController*)viewController
-                                          width:(CGFloat)width {
-  // Needs implementation.
-  NOTIMPLEMENTED();
-  return 0;
-}
-
 #pragma mark - UIViewControllerAnimatedTransitioning
 
 - (id<UIViewControllerAnimatedTransitioning>)
