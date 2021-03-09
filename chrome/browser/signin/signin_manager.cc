7155fa6c215b1ee6b39dee9a2134653af97fc348
Tanmoy Mollik <triploblastic@chromium.org>
2021-03-09 16:47:19 +0000

    [Signin] Update unconsented primary account set logic in SigninManager
    This cl updates the logic used for setting unconsented primary account
    in SigninManager and fixes a test.
    
    Bug: 1176695
    Change-Id: I20c228f43a8407504b84ebf5e0f97a3f6c8c959b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2741124
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861177}

--- chrome/browser/signin/signin_manager.cc
+++ chrome/browser/signin/signin_manager.cc
@@ -25,11 +25,23 @@ SigninManager::~SigninManager() {
 void SigninManager::UpdateUnconsentedPrimaryAccount() {
   base::Optional<CoreAccountInfo> account =
       ComputeUnconsentedPrimaryAccountInfo();
-  if (!account)
-    return;
 
-  identity_manager_->GetPrimaryAccountMutator()->SetUnconsentedPrimaryAccount(
-      account->account_id);
+  DCHECK(!account || !account->IsEmpty());
+  if (account) {
+    if (identity_manager_->GetPrimaryAccountInfo(
+            signin::ConsentLevel::kNotRequired) != account) {
+      DCHECK(
+          !identity_manager_->HasPrimaryAccount(signin::ConsentLevel::kSync));
+      identity_manager_->GetPrimaryAccountMutator()
+          ->SetUnconsentedPrimaryAccount(account->account_id);
+    }
+  } else if (identity_manager_->HasPrimaryAccount(
+                 signin::ConsentLevel::kNotRequired)) {
+    DCHECK(!identity_manager_->HasPrimaryAccount(signin::ConsentLevel::kSync));
+    identity_manager_->GetPrimaryAccountMutator()->ClearPrimaryAccount(
+        signin_metrics::USER_DELETED_ACCOUNT_COOKIES,
+        signin_metrics::SignoutDelete::IGNORE_METRIC);
+  }
 }
 
 base::Optional<CoreAccountInfo>
@@ -54,7 +66,7 @@ SigninManager::ComputeUnconsentedPrimaryAccountInfo() const {
     // in cookies if it exists and has a refresh token.
     if (cookie_accounts.empty()) {
       // Cookies are empty, the UPA is empty.
-      return CoreAccountInfo();
+      return base::nullopt;
     }
 
     base::Optional<AccountInfo> account_info =
@@ -68,7 +80,7 @@ SigninManager::ComputeUnconsentedPrimaryAccountInfo() const {
         identity_manager_->HasAccountWithRefreshTokenInPersistentErrorState(
             account_info->account_id);
 
-    return error_state ? AccountInfo() : account_info;
+    return error_state ? base::nullopt : account_info;
   }
 
   if (!identity_manager_->HasPrimaryAccount(signin::ConsentLevel::kNotRequired))
@@ -84,26 +96,27 @@ SigninManager::ComputeUnconsentedPrimaryAccountInfo() const {
       !identity_manager_->HasAccountWithRefreshToken(current_account)) {
     // Tokens are loaded, but the current UPA doesn't have a refresh token.
     // Clear the current UPA.
-    return CoreAccountInfo();
+    return base::nullopt;
   }
 
   if (!are_refresh_tokens_loaded &&
       unconsented_primary_account_revoked_during_load_) {
     // Tokens are not loaded, but the current UPA's refresh token has been
     // revoked. Clear the current UPA.
-    return CoreAccountInfo();
+    return base::nullopt;
   }
 
   if (cookie_info.accounts_are_fresh) {
     if (cookie_accounts.empty() || cookie_accounts[0].id != current_account) {
       // The current UPA is not the first in fresh cookies. It needs to be
       // cleared.
-      return CoreAccountInfo();
+      return base::nullopt;
     }
   }
 
-  // No indication that the current UPA is invalid, return no op.
-  return base::nullopt;
+  // No indication that the current UPA is invalid, return current UPA.
+  return identity_manager_->GetPrimaryAccountInfo(
+      signin::ConsentLevel::kNotRequired);
 }
 
 // signin::IdentityManager::Observer implementation.
