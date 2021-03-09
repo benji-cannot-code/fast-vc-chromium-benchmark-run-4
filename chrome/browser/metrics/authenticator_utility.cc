fa9ff1c8c28a20a03a7e38f0570eedbf63662678
Ken Buchanan <kenrb@chromium.org>
2021-03-09 23:14:53 +0000

    [WebAuthn] Add startup metric for IsUVPAA on Android
    https://crrev.com/791045 added a startup metric to check whether the
    IsUserVerifyingPlatformAuthenticatorAvailable JS method returns true
    on the host system. Android was omitted at that time, but the proxy
    metrics for estimating the prevalence of availability are not clear
    so it is useful to add for Android as well.
    
    Bug: 1104229
    Change-Id: I483e0e85292536b7b0f1ce8844bf35835481687b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2723074
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861335}

--- chrome/browser/metrics/authenticator_utility.cc
+++ chrome/browser/metrics/authenticator_utility.cc
@@ -21,6 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/is_uvpaa.h"
 
+#if defined(OS_ANDROID)
+#include "base/android/jni_android.h"
+#include "chrome/android/chrome_jni_headers/IsUvpaaHelper_jni.h"
+#endif
+
 #if defined(OS_MAC)
 #include "device/fido/mac/authenticator.h"
 #endif
@@ -75,12 +80,9 @@ void ReportUVPlatformAuthenticatorAvailabilityMainThreadMac() {
                      ChromeAuthenticatorRequestDelegate::
                          TouchIdAuthenticatorConfigForProfile(profile)));
 }
-#endif
+#endif  // defined(OS_MAC)
 
 void ReportUVPlatformAuthenticatorAvailability() {
-  // This only reports metrics for desktop platforms. For mobile devices, the
-  // platform version is an exact proxy for whether a platform authenticator
-  // can be used.
 #if defined(OS_MAC)
   DCHECK(!content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
   // IsUVPAA() is prone to crashes/hangs on macOS. Downsample metric collection
@@ -101,7 +103,24 @@ void ReportUVPlatformAuthenticatorAvailability() {
       base::BindOnce(&ReportAvailability));
 #elif BUILDFLAG(IS_CHROMEOS_ASH)
   // TODO(crbug.com/1181426): Reenable the IsUVPAA() startup metric on CrOS.
+#elif defined(OS_ANDROID)
+  JNIEnv* env = base::android::AttachCurrentThread();
+
+  ::webauth::Java_IsUvpaaHelper_isUserVerifyingPlatformAuthenticatorAvailable(
+      env);
 #endif
 }
 
 }  // namespace authenticator_utility
+
+#if defined(OS_ANDROID)
+
+namespace webauth {
+
+void JNI_IsUvpaaHelper_OnIsUvpaaComplete(JNIEnv* env, jboolean available) {
+  authenticator_utility::ReportAvailability(static_cast<bool>(available));
+}
+
+}  // namespace webauth
+
+#endif  // defined(OS_ANDROID)
