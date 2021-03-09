da3fde21efaeda7bfffb718c4ac3d6bc97256d8c
Yutaka Hirano <yhirano@chromium.org>
2021-03-09 09:31:55 +0000

    Remove obsolete Net.DataPipeUseFor* histograms
    Also remove NetworkService::ReportMetrics which was used only by them.
    
    Bug: 1138291, 1138292
    Change-Id: I075ed3b67a873c658c6b643468db390f6c42d65a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2744897
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861079}

--- services/network/websocket.cc
+++ services/network/websocket.cc
@@ -212,7 +212,6 @@ void WebSocket::WebSocketEventHandler::OnAddChannelResponse(
     impl_->Reset();
     return;
   }
-  impl_->data_pipe_use_tracker_.Activate();
   const MojoResult mojo_result = impl_->writable_watcher_.Watch(
       impl_->writable_.get(), MOJO_HANDLE_SIGNAL_WRITABLE,
       MOJO_WATCH_CONDITION_SATISFIED,
@@ -413,7 +412,6 @@ WebSocket::WebSocket(
     mojo::PendingRemote<mojom::TrustedHeaderClient> header_client,
     base::Optional<WebSocketThrottler::PendingConnection>
         pending_connection_tracker,
-    DataPipeUseTracker data_pipe_use_tracker,
     base::TimeDelta delay)
     : factory_(factory),
       url_loader_network_observer_(std::move(url_loader_network_observer)),
@@ -433,7 +431,6 @@ WebSocket::WebSocket(
       readable_watcher_(FROM_HERE,
                         mojo::SimpleWatcher::ArmingPolicy::MANUAL,
                         base::ThreadTaskRunnerHandle::Get()),
-      data_pipe_use_tracker_(std::move(data_pipe_use_tracker)),
       reassemble_short_messages_(base::FeatureList::IsEnabled(
           network::features::kWebSocketReassembleShortMessages)) {
   DCHECK(handshake_client_);
@@ -880,7 +877,6 @@ void WebSocket::Reset() {
   auth_handler_.reset();
   header_client_.reset();
   receiver_.reset();
-  data_pipe_use_tracker_.Reset();
 
   // net::WebSocketChannel requires that we delete it at this point.
   channel_.reset();
