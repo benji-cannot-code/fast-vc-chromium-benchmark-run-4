1e6d3fcfea671faddfd011da21a282b11d5b9d1f
Siyu An <siyua@chromium.org>
2021-03-09 02:22:25 +0000

    [Offer notification] M90 - Add metrics for the offer notification bubble
    Bug: 1093057
    Change-Id: Iaae4f0d04e5f816c423625a16164774c1162d07c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2730724
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#860992}

--- chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/page_action/page_action_icon_type.h"
+#include "components/autofill/core/browser/autofill_metrics.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/navigation_handle.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -75,7 +76,32 @@ void OfferNotificationBubbleControllerImpl::OnBubbleClosed(
     PaymentsBubbleClosedReason closed_reason) {
   set_bubble_view(nullptr);
   UpdatePageActionIcon();
-  // TODO(crbug.com/1093057): Add logging metrics.
+
+  // Log bubble result according to the closed reason.
+  AutofillMetrics::OfferNotificationBubbleResultMetric metric;
+  switch (closed_reason) {
+    case PaymentsBubbleClosedReason::kAccepted:
+      metric = AutofillMetrics::OfferNotificationBubbleResultMetric::
+          OFFER_NOTIFICATION_BUBBLE_ACKNOWLEDGED;
+      break;
+    case PaymentsBubbleClosedReason::kClosed:
+      metric = AutofillMetrics::OfferNotificationBubbleResultMetric::
+          OFFER_NOTIFICATION_BUBBLE_CLOSED;
+      break;
+    case PaymentsBubbleClosedReason::kNotInteracted:
+      metric = AutofillMetrics::OfferNotificationBubbleResultMetric::
+          OFFER_NOTIFICATION_BUBBLE_NOT_INTERACTED;
+      break;
+    case PaymentsBubbleClosedReason::kLostFocus:
+      metric = AutofillMetrics::OfferNotificationBubbleResultMetric::
+          OFFER_NOTIFICATION_BUBBLE_LOST_FOCUS;
+      break;
+    default:
+      NOTREACHED();
+      return;
+  }
+  AutofillMetrics::LogOfferNotificationBubbleResultMetric(metric,
+                                                          is_user_gesture_);
 }
 
 void OfferNotificationBubbleControllerImpl::ShowOfferNotificationIfApplicable(
@@ -144,7 +170,7 @@ void OfferNotificationBubbleControllerImpl::DoShowBubble() {
   if (observer_for_testing_)
     observer_for_testing_->OnBubbleShown();
 
-  // TODO(crbug.com/1093057): Add logging metrics.
+  AutofillMetrics::LogOfferNotificationBubbleOfferMetric(is_user_gesture_);
 }
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(OfferNotificationBubbleControllerImpl)
