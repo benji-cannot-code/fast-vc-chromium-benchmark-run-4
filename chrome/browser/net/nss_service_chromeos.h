e5a889d93c65d796ce0e77e1ff486ba8789acbd9
Matt Menke <mmenke@chromium.org>
2021-03-09 19:22:52 +0000

    Create service to manage per-profile NSS configuration on ChromeOS Ash.
    This CL moves Ash NSS configuration from ProfileIOData to
    NssServiceChromeOS, a BrowserContextKeyedService. In followup CLs,
    this class will own the NSSCertDatabaseChromeOSManager instead of
    the ResourceContext.
    
    Bug: 1018972
    Change-Id: I15ad9f277f8d90f0d33e8f78ece2b54dfd2c282e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2743974
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861238}

---
+++ chrome/browser/net/nss_service_chromeos.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_NET_NSS_SERVICE_CHROMEOS_H_
+#define CHROME_BROWSER_NET_NSS_SERVICE_CHROMEOS_H_
+
+#include "components/keyed_service/core/keyed_service.h"
+
+class Profile;
+
+// ChromeOS Ash service that manages initialization of NSS and the per-profile
+// certificate database.
+class NssServiceChromeOS : public KeyedService {
+ public:
+  explicit NssServiceChromeOS(Profile* profile);
+  NssServiceChromeOS(const NssServiceChromeOS&) = delete;
+  NssServiceChromeOS& operator=(const NssServiceChromeOS&) = delete;
+  ~NssServiceChromeOS() override;
+
+ private:
+  // TODO(https://cbug.com/1018972):  Move ownership of
+  // NSSCertDatabaseChromeOSManager to this class.
+};
+
+#endif  // CHROME_BROWSER_NET_NSS_SERVICE_CHROMEOS_H_
