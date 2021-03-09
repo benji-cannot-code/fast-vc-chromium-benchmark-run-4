16f0a20e008b2ec28374bec7fd1e8e125e2b6c25
dpapad <dpapad@chromium.org>
2021-03-09 11:56:23 +0000

    Settings: Check-in remaining Polymer3 files under privacy_page/
    Bug: 1184052
    Change-Id: I95c667d4552ac835f94ffb6f8f775c47a8958901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2738928
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861116}

--- chrome/browser/resources/settings/privacy_page/personalization_options.js
+++ chrome/browser/resources/settings/privacy_page/personalization_options.js
@@ -8,10 +8,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * 'personalization-options' contains several toggles related to
  * personalizations.
  */
-(function() {
+import '//resources/cr_elements/cr_button/cr_button.m.js';
+import '//resources/cr_elements/cr_toggle/cr_toggle.m.js';
+import '../controls/settings_toggle_button.m.js';
+import '../people_page/signout_dialog.m.js';
+import '../prefs/prefs.m.js';
+import '../settings_shared_css.m.js';
+// <if expr="not chromeos">
+import '//resources/cr_elements/cr_toast/cr_toast.m.js';
+// </if>
+
+import {WebUIListenerBehavior} from '//resources/js/web_ui_listener_behavior.m.js';
+import {html, Polymer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {loadTimeData} from '../i18n_setup.js';
+import {LifetimeBrowserProxyImpl} from '../lifetime_browser_proxy.m.js';
+import {StatusAction, SyncStatus} from '../people_page/sync_browser_proxy.m.js';
+import {PrefsBehavior} from '../prefs/prefs_behavior.m.js';
+
+import {MetricsReporting, PrivacyPageBrowserProxy, PrivacyPageBrowserProxyImpl} from './privacy_page_browser_proxy.js';
 
 Polymer({
   is: 'settings-personalization-options',
+
+  _template: html`{__html_template__}`,
 
   behaviors: [
     PrefsBehavior,
@@ -32,7 +52,7 @@ Polymer({
      */
     pageVisibility: Object,
 
-    /** @type {settings.SyncStatus} */
+    /** @type {SyncStatus} */
     syncStatus: Object,
 
     // <if expr="_google_chrome and not chromeos">
@@ -63,7 +83,7 @@ Polymer({
     },
   },
 
-  /** @private {?settings.PrivacyPageBrowserProxy} */
+  /** @private {?PrivacyPageBrowserProxy} */
   browserProxy_: null,
 
   /**
@@ -76,7 +96,7 @@ Polymer({
 
   /** @override */
   ready() {
-    this.browserProxy_ = settings.PrivacyPageBrowserProxyImpl.getInstance();
+    this.browserProxy_ = PrivacyPageBrowserProxyImpl.getInstance();
 
     // <if expr="_google_chrome and not chromeos">
     const setMetricsReportingPref = this.setMetricsReportingPref_.bind(this);
@@ -117,7 +137,7 @@ Polymer({
   },
 
   /**
-   * @param {!settings.MetricsReporting} metricsReporting
+   * @param {!MetricsReporting} metricsReporting
    * @private
    */
   setMetricsReportingPref_(metricsReporting) {
@@ -177,7 +197,7 @@ Polymer({
   shouldShowDriveSuggest_() {
     return loadTimeData.getBoolean('driveSuggestAvailable') &&
         !!this.syncStatus && !!this.syncStatus.signedIn &&
-        this.syncStatus.statusAction !== settings.StatusAction.REAUTHENTICATE;
+        this.syncStatus.statusAction !== StatusAction.REAUTHENTICATE;
   },
 
   /** @private */
@@ -214,7 +234,6 @@ Polymer({
    */
   onRestartTap_(e) {
     e.stopPropagation();
-    settings.LifetimeBrowserProxyImpl.getInstance().restart();
+    LifetimeBrowserProxyImpl.getInstance().restart();
   },
 });
-})();
