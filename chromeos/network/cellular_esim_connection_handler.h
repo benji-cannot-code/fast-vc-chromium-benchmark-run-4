de682aefd4baa006a04e1f1a35a27a23032812b1
Azeem Arshad <azeemarshad@chromium.org>
2021-03-09 21:27:03 +0000

    [CrOS Cellular] Remove uninhibit step in ESimConnectionHandler
    This CL removes the explicit uninhibit step and uninhibit
    on error logic since uninhibit can happen automatically
    when the lock is destroyed. This prepares ESimConnectionHandler
    so that inhibit_locks can also be passed in for re-use in
    new requests.
    
    Unittests were not updated since this doesn't change any
    underlying behavior but only removes the an explicit step
    that's not needed anymore.
    
    Bug: 1186337
    Change-Id: Ie14f1a69ca514b1651fc5f3e2c6efdfef66ea0d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2733923
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861295}

--- chromeos/network/cellular_esim_connection_handler.h
+++ chromeos/network/cellular_esim_connection_handler.h
@@ -66,6 +66,7 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) CellularESimConnectionHandler
                               network_handler::ErrorCallback error_callback);
     ~ConnectionRequestMetadata();
 
+    std::unique_ptr<CellularInhibitor::InhibitLock> inhibit_lock;
     std::string service_path;
     base::OnceClosure success_callback;
     network_handler::ErrorCallback error_callback;
@@ -78,7 +79,6 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) CellularESimConnectionHandler
     kRequestingProfilesBeforeEnabling,
     kEnablingProfile,
     kRequestingProfilesAfterEnabling,
-    kUninhibitingScans,
     kWaitingForConnectable
   };
   friend std::ostream& operator<<(std::ostream& stream,
@@ -120,8 +120,6 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) CellularESimConnectionHandler
 
   ConnectionState state_ = ConnectionState::kIdle;
   base::queue<std::unique_ptr<ConnectionRequestMetadata>> request_queue_;
-
-  std::unique_ptr<CellularInhibitor::InhibitLock> inhibit_lock_;
 
   base::WeakPtrFactory<CellularESimConnectionHandler> weak_ptr_factory_{this};
 };
