dad0aff05703290827bf4ca6cacac7cf659654bf
Wei Lee <wtlee@google.com>
2021-03-09 04:56:07 +0000

    Remove logs when installing camera app as SWA
    Bug: b/172343409
    Test: None
    Change-Id: Id45973a4a4445a53d05c63b98a833ffcff15b52f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2739019
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861029}

--- chrome/browser/web_applications/system_web_app_manager.cc
+++ chrome/browser/web_applications/system_web_app_manager.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
-#include "components/device_event_log/device_event_log.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
@@ -138,9 +137,6 @@ base::flat_map<SystemAppType, SystemAppInfo> CreateSystemWebApps(
         OriginTrialsMap({{GetOrigin("chrome://camera-app"), {"FileHandling"}}});
     infos.at(SystemAppType::CAMERA).capture_navigations = true;
 
-    // TODO(b/172343409): Remove this log when the camera app is fully migrated
-    // to SWA.
-    CAMERA_LOG(EVENT) << "Install the camera app as SWA";
     // Minimum height +32 for top bar height.
     infos.at(SystemAppType::CAMERA).minimum_window_size = {
         kChromeCameraAppMinimumWidth, kChromeCameraAppMinimumHeight + 32};
