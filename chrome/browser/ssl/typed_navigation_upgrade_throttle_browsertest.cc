27f72d6aadb4362e21e860c7e098e74c0f3d8112
Mustafa Emre Acer <meacer@google.com>
2021-03-09 21:00:20 +0000

    Trim https from autocomplete when defaulting typed navigations to https
    Presently, when the user enters a hostname without a scheme (e.g.
    example.com), the Default Typed Omnibox Navigations to HTTPS feature
    causes the first autocomplete suggestion to display the scheme
    (https://example.com).
    
    This CL trims the scheme so that upgraded navigations don't show
    the scheme in the autocomplete suggestion.
    
    Bug: 1184872
    Change-Id: Ib1fe63b9ed22b388bcb0ccc70917c39576277397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737638
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861277}

--- chrome/browser/ssl/typed_navigation_upgrade_throttle_browsertest.cc
+++ chrome/browser/ssl/typed_navigation_upgrade_throttle_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/omnibox/browser/omnibox_edit_model.h"
+#include "components/omnibox/browser/omnibox_popup_model.h"
 #include "components/omnibox/common/omnibox_features.h"
 #include "components/security_interstitials/content/ssl_error_handler.h"
 #include "content/public/browser/web_contents.h"
@@ -321,11 +322,32 @@ class TypedNavigationUpgradeThrottleBrowserTest
   }
 
  private:
+  void WaitForAutocompleteControllerDone() {
+    AutocompleteController* controller =
+        omnibox()->model()->autocomplete_controller();
+    ASSERT_TRUE(controller);
+
+    if (controller->done())
+      return;
+
+    ui_test_utils::WaitForAutocompleteDone(browser());
+    ASSERT_TRUE(controller->done());
+  }
+
   void TypeUrlAndCheckNavigation(const std::string& url_without_scheme,
                                  const base::HistogramTester& histograms,
                                  NavigationExpectation expectation,
                                  size_t num_expected_navigations) {
     SetOmniboxText(url_without_scheme);
+    // Regression check for crbug.com/1184872: The first autocomplete result
+    // should be the same as the typed text, without a scheme.
+    OmniboxPopupModel* popup_model = omnibox()->model()->popup_model();
+    ASSERT_TRUE(popup_model);
+    WaitForAutocompleteControllerDone();
+    ASSERT_TRUE(popup_model->IsOpen());
+    EXPECT_EQ(base::UTF8ToUTF16(url_without_scheme),
+              popup_model->result().match_at(0).fill_into_edit);
+
     PressEnterAndWaitForNavigations(num_expected_navigations);
 
     ui_test_utils::HistoryEnumerator enumerator(browser()->profile());
