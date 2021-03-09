65e27bf011877708b1dc02c2bd896f2104022d66
John Lee <johntlee@chromium.org>
2021-03-09 21:23:17 +0000

    WebUI: Update hover, active, and focused states for cr-icon-buttons
    This CL updates the visual states when a cr-icon-button or a row-like
    element containing a button is hovered, focused, or active.
    
    https://imgur.com/a/NRUusZZ
    
    A follow-up CL will be sent to make Read Later and Tab Search UIs more
    consistent with the new UI. For now, this CL attempts to keep the
    more custom changes for those UIs in tact.
    
    Bug: 1150154
    Change-Id: Ic3997ddc37e0b148f1d2e4b9466db98f0d4c3e39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704731
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861293}

--- ui/webui/resources/cr_elements/cr_icon_button/cr_icon_button.js
+++ ui/webui/resources/cr_elements/cr_icon_button/cr_icon_button.js
@@ -44,10 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'cr-icon-button',
 
-  behaviors: [
-    Polymer.PaperRippleBehavior,
-  ],
-
   properties: {
     disabled: {
       type: Boolean,
@@ -70,21 +66,9 @@ Polymer({
       reflectToAttribute: true,
     },
 
-    noRippleOnFocus: {
-      type: Boolean,
-      value: false,
-    },
-
     /** @private */
     multipleIcons_: {
       type: Boolean,
-      reflectToAttribute: true,
-    },
-
-    /** @private */
-    rippleShowing_: {
-      type: Boolean,
-      value: false,
       reflectToAttribute: true,
     },
   },
@@ -98,12 +82,8 @@ Polymer({
   listeners: {
     blur: 'onBlur_',
     click: 'onClick_',
-    down: 'showRipple_',
-    focus: 'onFocus_',
     keydown: 'onKeyDown_',
     keyup: 'onKeyUp_',
-    pointerdown: 'ensureRipple',
-    up: 'hideRipple_',
   },
 
   /**
@@ -116,22 +96,6 @@ Polymer({
    * @private {boolean}
    */
   spaceKeyDown_: false,
-
-  /** @private */
-  hideRipple_() {
-    if (this.hasRipple()) {
-      this.getRipple().clear();
-      this.rippleShowing_ = false;
-    }
-  },
-
-  /** @private */
-  showRipple_() {
-    if (!this.noink && !this.disabled) {
-      this.getRipple().showAndHoldDown();
-      this.rippleShowing_ = true;
-    }
-  },
 
   /**
    * @param {boolean} newValue
@@ -162,19 +126,8 @@ Polymer({
   },
 
   /** @private */
-  onFocus_() {
-    if (!this.noRippleOnFocus) {
-      this.showRipple_();
-    }
-  },
-
-  /** @private */
   onBlur_() {
     this.spaceKeyDown_ = false;
-
-    if (!this.noRippleOnFocus) {
-      this.hideRipple_();
-    }
   },
 
   /**
@@ -204,14 +157,6 @@ Polymer({
             .forEach(child => child.setAttribute('role', 'none'));
       }
     });
-    if (!this.hasRipple()) {
-      return;
-    }
-    if (icons.length > 1) {
-      this.getRipple().classList.remove('circle');
-    } else {
-      this.getRipple().classList.add('circle');
-    }
   },
 
   /**
@@ -250,18 +195,6 @@ Polymer({
       this.spaceKeyDown_ = false;
       this.click();
     }
-  },
-
-  // customize the element's ripple
-  _createRipple() {
-    this._rippleContainer = this.$.icon;
-    const ripple = Polymer.PaperRippleBehavior._createRipple();
-    ripple.id = 'ink';
-    ripple.setAttribute('recenters', '');
-    if (!(this.ironIcon || '').includes(',')) {
-      ripple.classList.add('circle');
-    }
-    return ripple;
   },
 });
 /* #ignore */ console.warn('crbug/1173575, non-JS module files deprecated.');
