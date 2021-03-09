15a676895c3ab779503512b1e18c5e02310fe5b1
John Abd-El-Malek <jam@chromium.org>
2021-03-09 22:56:56 +0000

    Fix use after free in WebLayer Shell on desktop at shutdown.
    Bug: 1181368
    Change-Id: I010b4692c3a4c8a2c72fc2e2146951415e7b6ea1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2745457
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861329}

--- weblayer/shell/browser/shell.cc
+++ weblayer/shell/browser/shell.cc
@@ -40,10 +40,6 @@ Shell::Shell(std::unique_ptr<Browser> browser)
     tab()->AddObserver(this);
     tab()->GetNavigationController()->AddObserver(this);
 #if !defined(OS_ANDROID)  // Android does this in Java.
-
-    // TODO: how will tests work with this on android? can we get to the
-    // concrete type?
-
     static_cast<TabImpl*>(tab())->profile()->SetDownloadDelegate(this);
 #endif
   }
@@ -53,6 +49,9 @@ Shell::~Shell() {
   if (tab()) {
     tab()->GetNavigationController()->RemoveObserver(this);
     tab()->RemoveObserver(this);
+#if !defined(OS_ANDROID)  // Android does this in Java.
+    static_cast<TabImpl*>(tab())->profile()->SetDownloadDelegate(nullptr);
+#endif
   }
   PlatformCleanUp();
 
