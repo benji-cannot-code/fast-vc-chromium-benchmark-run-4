672d69fc825bc9dcd6f1f407b3a8c0707933429f
Henrik Boström <hbos@chromium.org>
2021-03-09 11:24:34 +0000

    WebRtcVideoFrameAdapter: Move SharedResource mock to a separate file.
    While moving I discovered that all MOCK methods were not based on
    virtual methods. This, for example, prevents EXPECT_CALL with
    CreateFrame() which I need to do in a follow-up CL. So I made all MOCK
    methods virtual.
    
    In order not to break a test that depends on the mock providing a
    non-mock implementation of CreateFrame and CreateTemporaryFrame, I
    modified the mock to allow expecting these to be called with the real
    implementation.
    
    Bug: webrtc:12469
    Change-Id: I192b5de27c064320fa01428ee3eec5ee9530a250
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2739517
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861110}

--- third_party/blink/renderer/platform/webrtc/webrtc_video_frame_adapter_test.cc
+++ third_party/blink/renderer/platform/webrtc/webrtc_video_frame_adapter_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/renderer/platform/testing/video_frame_utils.h"
+#include "third_party/blink/renderer/platform/webrtc/testing/mock_webrtc_video_frame_adapter_shared_resources.h"
 #include "third_party/webrtc/api/video/video_frame_buffer.h"
 #include "third_party/webrtc/rtc_base/ref_counted_object.h"
 #include "ui/gfx/gpu_memory_buffer.h"
@@ -20,42 +21,6 @@ using ::testing::_;
 using ::testing::Return;
 
 namespace blink {
-
-class MockSharedResources : public WebRtcVideoFrameAdapter::SharedResources {
- public:
-  MockSharedResources() : WebRtcVideoFrameAdapter::SharedResources(nullptr) {}
-
-  MOCK_METHOD(scoped_refptr<media::VideoFrame>,
-              CreateFrame,
-              (media::VideoPixelFormat format,
-               const gfx::Size& coded_size,
-               const gfx::Rect& visible_rect,
-               const gfx::Size& natural_size,
-               base::TimeDelta timestamp));
-
-  MOCK_METHOD(scoped_refptr<media::VideoFrame>,
-              CreateTemporaryFrame,
-              (media::VideoPixelFormat format,
-               const gfx::Size& coded_size,
-               const gfx::Rect& visible_rect,
-               const gfx::Size& natural_size,
-               base::TimeDelta timestamp));
-
-  MOCK_METHOD(scoped_refptr<viz::RasterContextProvider>,
-              GetRasterContextProvider,
-              ());
-
-  MOCK_METHOD(scoped_refptr<media::VideoFrame>,
-              ConstructVideoFrameFromTexture,
-              (scoped_refptr<media::VideoFrame> source_frame));
-
-  MOCK_METHOD(scoped_refptr<media::VideoFrame>,
-              ConstructVideoFrameFromGpu,
-              (scoped_refptr<media::VideoFrame> source_frame));
-
- private:
-  friend class base::RefCountedThreadSafe<MockSharedResources>;
-};
 
 class WebRtcVideoFrameAdapterParamTest
     : public ::testing::TestWithParam<
@@ -384,6 +349,8 @@ TEST(WebRtcVideoFrameAdapterTest, ConvertsTextureFrameWithSharedResources) {
          kCodedSize.GetArea() * 4);
 
   // Should call texture conversion.
+  resources->ExpectCreateFrameWithRealImplementation();
+  resources->ExpectCreateTemporaryFrameWithRealImplementation();
   EXPECT_CALL(*resources, ConstructVideoFrameFromTexture(_))
       .WillOnce(Return(memory_frame));
 
