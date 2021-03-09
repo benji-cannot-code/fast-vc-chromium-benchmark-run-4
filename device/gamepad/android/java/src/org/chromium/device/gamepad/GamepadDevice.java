fd06bca72d30d29acaa928dd4e4e98d57014c779
Md Jobed Hossain <jobed.h@samsung.com>
2021-03-09 05:53:51 +0000

    Include vendor/product ID in Gamepad.id string
    Chrome typically includes the vendor and product ID as hexadecimal
    values within the Gamepad.id string to aid developers in identifying
    controller models. Historically, these IDs were omitted from the ID
    string on Android because they were not available pre-KitKat.
    
    Chrome 72 discontinued support for Android Jelly Bean. Now that we
    don't need to maintain consistent IDs between old and new versions of
    Android, let's add the device IDs and mapping info to the product name
    string using the same format as on desktop Chrome.
    
    Bug: 1175926
    Change-Id: I03b004d6cbca3fc7f8efa34a3d36b2c61d22fb5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2734880
    Commit-Queue: Jobed H <jobed.h@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861039}

--- device/gamepad/android/java/src/org/chromium/device/gamepad/GamepadDevice.java
+++ device/gamepad/android/java/src/org/chromium/device/gamepad/GamepadDevice.java
@@ -45,6 +45,11 @@ class GamepadDevice {
     private int mDeviceId;
     // The index of the gamepad in the Navigator.
     private int mDeviceIndex;
+    // The vendor ID of the gamepad, or zero if the gamepad does not have a vendor ID.
+    private int mDeviceVendorId;
+    // The product ID of the gamepad, or zero if the gamepad does not have a product ID.
+    private int mDeviceProductId;
+
     // Last time the data for this gamepad was updated.
     private long mTimestamp;
 
@@ -76,6 +81,8 @@ class GamepadDevice {
         mDeviceIndex = index;
         mDeviceId = inputDevice.getId();
         mDeviceName = inputDevice.getName();
+        mDeviceVendorId = inputDevice.getVendorId();
+        mDeviceProductId = inputDevice.getProductId();
         mTimestamp = SystemClock.uptimeMillis();
         // Get axis ids and initialize axes values.
         final List<MotionRange> ranges = inputDevice.getMotionRanges();
@@ -127,6 +134,22 @@ class GamepadDevice {
      */
     public String getName() {
         return mDeviceName;
+    }
+
+    /**
+     * @return Vendor Id of the gamepad device.
+     * It can be zero if gamepad doesn't have a vendor ID.
+     */
+    public int getVendorId() {
+        return mDeviceVendorId;
+    }
+
+    /**
+     * @return The product ID of the gamepad.
+     * It can be zero if gamepad doesn't have a product ID.
+     */
+    public int getProductId() {
+        return mDeviceProductId;
     }
 
     /**
