1fad65e1c9cd6809d6862d08f61cc46cb24365b8
Nancy Wang <nancylingwang@chromium.org>
2021-03-09 03:33:08 +0000

    Use ScopedObservation in ApkWebAppService.
    This CL modifies the current observers in ApkWebAppService to use
    base::ScopedObservation.
    
    BUG=1168254
    
    Change-Id: Ia01b192cb02d96418b31cd37659d70660c8a1e48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2744761
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861013}

--- chrome/browser/ash/apps/apk_web_app_service.cc
+++ chrome/browser/ash/apps/apk_web_app_service.cc
@@ -84,11 +84,11 @@ ApkWebAppService::ApkWebAppService(Profile* profile) : profile_(profile) {
   // Can be null in tests.
   arc_app_list_prefs_ = ArcAppListPrefs::Get(profile);
   if (arc_app_list_prefs_)
-    arc_app_list_prefs_->AddObserver(this);
+    arc_app_list_prefs_observer_.Observe(arc_app_list_prefs_);
 
   provider_ = web_app::WebAppProvider::Get(profile);
   DCHECK(provider_);
-  registrar_observer_.Add(&provider_->registrar());
+  registrar_observer_.Observe(&provider_->registrar());
 }
 
 ApkWebAppService::~ApkWebAppService() = default;
@@ -243,10 +243,8 @@ void ApkWebAppService::UpdateShelfPin(
 
 void ApkWebAppService::Shutdown() {
   // Can be null in tests.
-  if (arc_app_list_prefs_) {
-    arc_app_list_prefs_->RemoveObserver(this);
+  if (arc_app_list_prefs_)
     arc_app_list_prefs_ = nullptr;
-  }
 }
 
 void ApkWebAppService::OnPackageInstalled(
--- chrome/browser/ash/apps/apk_web_app_service.h
+++ chrome/browser/ash/apps/apk_web_app_service.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/ash/apps/apk_web_app_installer.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/web_applications/components/app_registrar.h"
@@ -113,8 +113,11 @@ class ApkWebAppService : public KeyedService,
   ArcAppListPrefs* arc_app_list_prefs_;
   web_app::WebAppProvider* provider_;
 
-  ScopedObserver<web_app::AppRegistrar, web_app::AppRegistrarObserver>
+  base::ScopedObservation<web_app::AppRegistrar, web_app::AppRegistrarObserver>
       registrar_observer_{this};
+
+  base::ScopedObservation<ArcAppListPrefs, ArcAppListPrefs::Observer>
+      arc_app_list_prefs_observer_{this};
 
   // Must go last.
   base::WeakPtrFactory<ApkWebAppService> weak_ptr_factory_{this};
