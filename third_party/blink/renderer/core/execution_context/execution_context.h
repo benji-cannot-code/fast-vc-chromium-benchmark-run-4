bd2423bba5fdae8681f6808f22d3deaaa846d14e
Caleb Raitto <caraitto@chromium.org>
2021-03-09 21:17:24 +0000

    Add a mechanism for detecting if running in RequestAnimationFrame.
    The intent is to avoid collecting identifiability study metrics when in
    a RequestAnimationFrame for APIs like canvas.
    
    Bug: 973801
    Change-Id: I12c4cd53206430c20f6900d28f05a1861818c9fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2736781
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861291}

--- third_party/blink/renderer/core/execution_context/execution_context.h
+++ third_party/blink/renderer/core/execution_context/execution_context.h
@@ -408,6 +408,30 @@ class CORE_EXPORT ExecutionContext : public Supplementable<ExecutionContext>,
 
   void FileSharedArrayBufferCreationIssue();
 
+  bool IsInRequestAnimationFrame() const {
+    return is_in_request_animation_frame_;
+  }
+
+  // For use by FrameRequestCallbackCollection::ExecuteFrameCallbacks();
+  // IsInRequestAnimationFrame() for the corresponding ExecutionContext will
+  // return true while this instance exists.
+  class ScopedRequestAnimationFrameStatus {
+    STACK_ALLOCATED();
+
+   public:
+    explicit ScopedRequestAnimationFrameStatus(ExecutionContext* context)
+        : context_(context) {
+      DCHECK(!context_->is_in_request_animation_frame_);
+      context_->is_in_request_animation_frame_ = true;
+    }
+    ~ScopedRequestAnimationFrameStatus() {
+      context_->is_in_request_animation_frame_ = false;
+    }
+
+   private:
+    ExecutionContext* context_;
+  };
+
  protected:
   explicit ExecutionContext(v8::Isolate* isolate, Agent*);
   ExecutionContext(const ExecutionContext&) = delete;
@@ -447,6 +471,8 @@ class CORE_EXPORT ExecutionContext : public Supplementable<ExecutionContext>,
 
   bool has_filed_shared_array_buffer_transfer_issue_ = false;
   bool has_filed_shared_array_buffer_creation_issue_ = false;
+
+  bool is_in_request_animation_frame_ = false;
 
   Member<PublicURLManager> public_url_manager_;
 
