d6fb12657a2b5c7c5a7104b109f692ba04cbceb4
Richard Knoll <knollr@chromium.org>
2021-03-10 22:36:10 +0000

    Log notification build success on Android
    This adds a metric to track how often building a notification on Android
    succeeds or fails. This will be used as a regression metric to roll out
    the NotificationCompat.Builder for Web Notifications.
    
    Bug: 697104
    Change-Id: Id823d248e14acab6a5df1fc5d38a948905a81482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2748236
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861740}

--- components/browser_ui/notifications/android/java/src/org/chromium/components/browser_ui/notifications/NotificationWrapperStandardBuilder.java
+++ components/browser_ui/notifications/android/java/src/org/chromium/components/browser_ui/notifications/NotificationWrapperStandardBuilder.java
@@ -20,6 +20,7 @@ import android.widget.RemoteViews;
 import androidx.core.app.NotificationCompat;
 
 import org.chromium.base.Log;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.components.browser_ui.notifications.channels.ChannelsInitializer;
 
 /**
@@ -355,7 +356,14 @@ public class NotificationWrapperStandardBuilder implements NotificationWrapperBu
 
     @Override
     public Notification build() {
-        return mBuilder.build();
+        boolean success = false;
+        try {
+            Notification notification = mBuilder.build();
+            success = true;
+            return notification;
+        } finally {
+            RecordHistogram.recordBooleanHistogram("Notifications.Android.Build", success);
+        }
     }
 
     @Override
