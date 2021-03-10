d02dc307164ad5c4c603ac660e7458b85f24079a
Henrik Boström <hbos@chromium.org>
2021-03-10 09:34:48 +0000

    Remove standalone RTCIceTransport constructor.
    The only supported use case for constructing an RTCIceTransport as a
    standalone object, i.e. that is not associated and constructed by an
    RTCPeerConnection, is for use with P2P QUIC. However P2P QUIC with
    ICE was an experimental feature that was removed in M88.
    
    As part of further cleanup we're now also removing the standalone
    constructor. See also https://crbug.com/1106907 for more reasons why we
    don't want to continue to maintain this code.
    
    Bug: chromium:1106907
    Change-Id: Ide384ed4db6ca53ef71e82022b868dded052a17d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2744038
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Tommi <tommi@chromium.org>
    Reviewed-by: Steve Anton <steveanton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861491}

--- third_party/blink/renderer/modules/peerconnection/rtc_ice_transport.h
+++ third_party/blink/renderer/modules/peerconnection/rtc_ice_transport.h
@@ -57,7 +57,6 @@ class MODULES_EXPORT RTCIceTransport final
     kDisposed,
   };
 
-  static RTCIceTransport* Create(ExecutionContext* context);
   static RTCIceTransport* Create(
       ExecutionContext* context,
       rtc::scoped_refptr<webrtc::IceTransportInterface> ice_transport_channel,
