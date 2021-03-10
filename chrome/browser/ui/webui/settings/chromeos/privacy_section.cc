f488eb82ba862a6bec0b9043d847a7b3fa530f7d
Anastasiia N <anastasiian@chromium.org>
2021-03-10 16:11:37 +0000

    cros: Hide Lock screen section for Guest mode
    Hide Lock screen and Manage other people sections in "Security and
    Privacy" for Guest sessions.
    
    Bug: 1185818
    Change-Id: I18394fdb1cb8fb00827aca685b78294ceb93e27b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2745376
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861573}

--- chrome/browser/ui/webui/settings/chromeos/privacy_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/privacy_section.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "build/branding_buildflags.h"
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_utils.h"
+#include "chrome/browser/ui/webui/settings/chromeos/os_settings_features_util.h"
 #include "chrome/browser/ui/webui/settings/chromeos/peripheral_data_access_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/search/search_tag_registry.h"
 #include "chrome/browser/ui/webui/settings/shared_settings_localized_strings_provider.h"
@@ -45,7 +46,8 @@ const std::vector<SearchConcept>& GetPrivacySearchConcepts() {
          {.section = mojom::Section::kPrivacyAndSecurity}},
     });
 
-    if (chromeos::features::IsAccountManagementFlowsV2Enabled()) {
+    if (chromeos::features::IsAccountManagementFlowsV2Enabled() &&
+        !features::IsGuestModeActive()) {
       all_tags.insert(
           all_tags.end(),
           {{IDS_OS_SETTINGS_TAG_GUEST_BROWSING,
@@ -186,7 +188,7 @@ PrivacySection::PrivacySection(Profile* profile,
 
   // Fingerprint search tags are added if necessary. Remove fingerprint search
   // tags update dynamically during a user session.
-  if (AreFingerprintSettingsAllowed() &&
+  if (!features::IsGuestModeActive() && AreFingerprintSettingsAllowed() &&
       chromeos::features::IsAccountManagementFlowsV2Enabled()) {
     updater.AddSearchTags(GetFingerprintSearchConcepts());
 
