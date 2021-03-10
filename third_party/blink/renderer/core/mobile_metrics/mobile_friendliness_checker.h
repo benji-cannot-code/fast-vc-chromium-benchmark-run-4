bbe5f2f68f747752d7d26a17d01371599930d228
Michael Lippautz <mlippautz@chromium.org>
2021-03-10 23:37:48 +0000

    MobileFriendlinessChecker: Fix includes and fwd decl
    Fix includes and forward declarations for Oilpan library which is not
    yet built on the main waterfall.
    
    Bug: 1056170
    Change-Id: Ibeedcfb63b7b39496668d69cf4c418e682b3f2b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2749239
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861767}

--- third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.h
+++ third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.h
@@ -8,12 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/mobile_metrics/mobile_friendliness.h"
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
+#include "third_party/blink/renderer/platform/heap/visitor.h"
 
 namespace blink {
 class LocalFrameView;
 class LayoutObject;
-class Visitor;
 struct ViewportDescription;
 
 CORE_EXPORT extern const base::Feature kBadTapTargetsRatio;
