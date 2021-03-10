3bc4c4af9ce631ce4edd426379aa2e0550bcb9a6
Brandon Wylie <wylieb@chromium.org>
2021-03-10 16:50:33 +0000

    Remove ChromeActivity reference from AndroidPaymentAppFinder
    Bug: 1155788
    Change-Id: I46bebe1e46a3d3580313b0ddc75277000368c71a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2733258
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861593}

--- chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorSupplier.java
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorSupplier.java
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tabmodel;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.chromium.base.UnownedUserDataKey;
 import org.chromium.base.supplier.ObservableSupplier;
+import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.UnownedUserDataSupplier;
 import org.chromium.ui.base.WindowAndroid;
 
@@ -17,14 +20,23 @@ import org.chromium.ui.base.WindowAndroid;
 public class TabModelSelectorSupplier extends UnownedUserDataSupplier<TabModelSelector> {
     private static final UnownedUserDataKey<TabModelSelectorSupplier> KEY =
             new UnownedUserDataKey<TabModelSelectorSupplier>(TabModelSelectorSupplier.class);
+    private static ObservableSupplierImpl<TabModelSelector> sInstanceForTesting;
 
     /** Return {@link TabModelSelector} supplier associated with the given {@link WindowAndroid}. */
     public static ObservableSupplier<TabModelSelector> from(WindowAndroid windowAndroid) {
+        if (sInstanceForTesting != null) return sInstanceForTesting;
         return KEY.retrieveDataFromHost(windowAndroid.getUnownedUserDataHost());
     }
 
     /** Constructs a TabModelSelectorSupplier and attaches it to the {@link WindowAndroid} */
     public TabModelSelectorSupplier() {
         super(KEY);
+    }
+
+    /** Sets an instance for testing. */
+    @VisibleForTesting
+    public static void setInstanceForTesting(TabModelSelector tabModelSelector) {
+        sInstanceForTesting = new ObservableSupplierImpl<>();
+        sInstanceForTesting.set(tabModelSelector);
     }
 }
