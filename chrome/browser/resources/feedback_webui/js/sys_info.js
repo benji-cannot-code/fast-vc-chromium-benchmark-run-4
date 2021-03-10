714db62f76564a93b3ad5fd9b02f5b357fd89072
Xiangdong Kong <Xiangdongkong@google.com>
2021-03-10 21:16:19 +0000

    [feedback] implemented the sendFeedbackReport JS function
    This is for feedback migration.
    
    Bug: 1167223
    Change-Id: I4742a4534c918e7d22144c8e7c0e73b2d607f093
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737519
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861671}

--- chrome/browser/resources/feedback_webui/js/sys_info.js
+++ chrome/browser/resources/feedback_webui/js/sys_info.js
@@ -228,6 +228,6 @@ function createTable(systemInfo) {
  * Initializes the page when the window is loaded.
  */
 window.onload = function() {
-  // TODO(crbug.com/1167223): Implement this.
-  // getFullSystemInfo(createTable);
+  // The getFullSystemInfo promise is set from the parent page
+  getFullSystemInfo().then(createTable);
 };
