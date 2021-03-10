70866625e8dc4ebc1608a5ee5b35310f3a6212b3
Mike Reed <reed@google.com>
2021-03-10 18:34:42 +0000

    Stop calling deprecated getFlags
    See https://skia-review.googlesource.com/c/skia/+/382702
    
    Change-Id: I3c2208c85846b6f523246f8ac0548205584c2359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2749175
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Mike Reed <reed@google.com>
    Cr-Commit-Position: refs/heads/master@{#861630}

--- cc/paint/paint_flags.cc
+++ cc/paint/paint_flags.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 static bool affects_alpha(const SkColorFilter* cf) {
-  return cf && !(cf->getFlags() & SkColorFilter::kAlphaUnchanged_Flag);
+  return cf && !cf->isAlphaUnchanged();
 }
 
 }  // namespace
