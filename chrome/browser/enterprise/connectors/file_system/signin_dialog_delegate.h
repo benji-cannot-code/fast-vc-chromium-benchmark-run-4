ad502349e5503fadc992bf11c484b644fb3f0e28
Alice Gong <alicego@google.com>
2021-03-10 18:31:21 +0000

    SetCancelCallback() in FileSystemSigninDelegate.
    To allow users to cancel sign in, in case of retry loop, add
    SetCancelCallback() in FileSystemSigninDelegate that sends a
    REQUEST_CANCELLED back to FileSystemRenameHandler.
    
    BUG=1158490
    
    Change-Id: I795862c768789e0af4f2fa2fcbc8eb8c63861552
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2743114
    Commit-Queue: Alice Gong <alicego@google.com>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861628}

--- chrome/browser/enterprise/connectors/file_system/signin_dialog_delegate.h
+++ chrome/browser/enterprise/connectors/file_system/signin_dialog_delegate.h
@@ -66,6 +66,8 @@ class FileSystemSigninDialogDelegate
   void DeleteDelegate() override;
   views::View* GetInitiallyFocusedView() override;
 
+  void OnCancellation();
+
   // content::WebContentsObserver:
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
@@ -78,7 +80,7 @@ class FileSystemSigninDialogDelegate
   std::unique_ptr<views::WebView> web_view_;
   std::unique_ptr<OAuth2AccessTokenFetcherImpl> token_fetcher_;
   AuthorizationCompletedCallback callback_;
-  base::WeakPtrFactory<FileSystemSigninDialogDelegate> factory_{this};
+  base::WeakPtrFactory<FileSystemSigninDialogDelegate> weak_factory_{this};
 };
 
 }  // namespace enterprise_connectors
