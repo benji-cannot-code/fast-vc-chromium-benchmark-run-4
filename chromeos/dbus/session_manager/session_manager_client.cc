df22b00309351a251c7a04b831a7ed430ec75128
Victor-Gabriel Savu <vsavu@google.com>
2021-03-10 21:34:29 +0000

    histograms: Remove Enterprise.RetrievePolicyResponse
    Remove the code emitting Enterprise.RetrievePolicyResponse and
    obsolete the policy.
    
    Bug: chromium:1039330
    Change-Id: Ic7f08a1e933b38fc7e43281f8afd080ab0ea60c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747383
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861701}

--- chromeos/dbus/session_manager/session_manager_client.cc
+++ chromeos/dbus/session_manager/session_manager_client.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/read_only_shared_memory_region.h"
 #include "base/memory/writable_shared_memory_region.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/optional.h"
 #include "base/path_service.h"
 #include "base/strings/string_util.h"
@@ -77,30 +76,6 @@ RetrievePolicyResponseType GetPolicyResponseTypeByError(
     return RetrievePolicyResponseType::POLICY_ENCODE_ERROR;
   }
   return RetrievePolicyResponseType::OTHER_ERROR;
-}
-
-// Logs UMA stat for retrieve policy request, corresponding to D-Bus method name
-// used.
-void LogPolicyResponseUma(login_manager::PolicyAccountType account_type,
-                          RetrievePolicyResponseType response) {
-  switch (account_type) {
-    case login_manager::ACCOUNT_TYPE_DEVICE:
-      UMA_HISTOGRAM_ENUMERATION("Enterprise.RetrievePolicyResponse.Device",
-                                response, RetrievePolicyResponseType::COUNT);
-      break;
-    case login_manager::ACCOUNT_TYPE_DEVICE_LOCAL_ACCOUNT:
-      UMA_HISTOGRAM_ENUMERATION(
-          "Enterprise.RetrievePolicyResponse.DeviceLocalAccount", response,
-          RetrievePolicyResponseType::COUNT);
-      break;
-    case login_manager::ACCOUNT_TYPE_USER:
-      UMA_HISTOGRAM_ENUMERATION("Enterprise.RetrievePolicyResponse.User",
-                                response, RetrievePolicyResponseType::COUNT);
-      break;
-    default:
-      NOTREACHED();
-      break;
-  }
 }
 
 // Creates a PolicyDescriptor object to store/retrieve Chrome policy.
@@ -780,7 +755,6 @@ class SessionManagerClientImpl : public SessionManagerClient {
     } else {
       policy_out->clear();
     }
-    LogPolicyResponseUma(descriptor.account_type(), result);
     return result;
   }
 
@@ -930,7 +904,6 @@ class SessionManagerClientImpl : public SessionManagerClient {
     if (!response) {
       RetrievePolicyResponseType response_type =
           GetPolicyResponseTypeByError(error ? error->GetErrorName() : "");
-      LogPolicyResponseUma(account_type, response_type);
       std::move(callback).Run(response_type, std::string());
       return;
     }
@@ -938,7 +911,6 @@ class SessionManagerClientImpl : public SessionManagerClient {
     dbus::MessageReader reader(response);
     std::string proto_blob;
     ExtractPolicyResponseString(account_type, response, &proto_blob);
-    LogPolicyResponseUma(account_type, RetrievePolicyResponseType::SUCCESS);
     std::move(callback).Run(RetrievePolicyResponseType::SUCCESS, proto_blob);
   }
 
