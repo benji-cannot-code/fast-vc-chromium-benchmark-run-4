c94616a4dc99f502406bf0b48a17aa1495c4babe
Tommy Li <tommycli@chromium.org>
2021-03-10 22:19:49 +0000

    [memories] Add HistoryClustersTabHelper as peer to HistoryTabHelper
    We will need to use HistoryClustersTabHelper (in coordination with
    HistoryTabHelper) so that Memories uses History's concept of a visit
    instead of UKM's.
    
    This adds the tab helper and some functions that HistoryTabHelper
    calls. Further implementation is needed.
    
    Bug: 1171352, 1184923
    Change-Id: Id41e1359d6120f73e46035063698a85ad351b68d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747056
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861729}

--- chrome/browser/history/history_tab_helper.cc
+++ chrome/browser/history/history_tab_helper.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "build/build_config.h"
 #include "chrome/browser/history/history_service_factory.h"
+#include "chrome/browser/history_clusters/history_clusters_tab_helper.h"
 #include "chrome/browser/prefetch/no_state_prefetch/no_state_prefetch_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/history/content/browser/history_context_helper.h"
@@ -190,7 +191,19 @@ void HistoryTabHelper::DidFinishNavigation(
     return;
 #endif
 
+  HistoryClustersTabHelper* clusters_tab_helper =
+      HistoryClustersTabHelper::FromWebContents(web_contents());
+  if (clusters_tab_helper) {
+    clusters_tab_helper->WillUpdateHistoryForNavigation(navigation_handle,
+                                                        add_page_args);
+  }
+
   UpdateHistoryForNavigation(add_page_args);
+
+  if (clusters_tab_helper) {
+    clusters_tab_helper->DidUpdateHistoryForNavigation(navigation_handle,
+                                                       add_page_args);
+  }
 }
 
 // We update history upon the associated WebContents becoming the top level
--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/file_system_access/file_system_access_tab_helper.h"
 #include "chrome/browser/history/history_tab_helper.h"
 #include "chrome/browser/history/top_sites_factory.h"
+#include "chrome/browser/history_clusters/history_clusters_tab_helper.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/lite_video/lite_video_observer.h"
 #include "chrome/browser/login_detection/login_detection_tab_helper.h"
@@ -271,6 +272,7 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
   history::WebContentsTopSitesObserver::CreateForWebContents(
       web_contents, TopSitesFactory::GetForProfile(profile).get());
   HistoryTabHelper::CreateForWebContents(web_contents);
+  HistoryClustersTabHelper::CreateForWebContents(web_contents);
   InfoBarService::CreateForWebContents(web_contents);
   webapps::InstallableManager::CreateForWebContents(web_contents);
   PrefetchProxyTabHelper::CreateForWebContents(web_contents);
