da8df5dad7a97e1ee5fd00ae2cba1b6a719c0565
David Bienvenu <davidbienvenu@chromium.org>
2021-03-10 17:21:45 +0000

    unit_tests: Fix flakiness in some extensions tests
    Some tests flake because QuotaManagerImpl::RegisterClient gets called
    after QuotaManagerImpl::LazyInitialize. One cause is
    StoragePartitionImplMap::Get, which kicks off a chain of async calls
    that eventually call QuotaManagerImpl::RegisterClient. If test
    teardown triggers a call to LazyInitialize before the chain completes,
    the test will check fail. This CL forces an initialization of the
    extension storage partition map before we shutdown, which should fix
    the flakes.
    
    When 1182630 is fixed, it should be possible to roll this CL back.
    
    Bug: 956719, 1169093
    Change-Id: I9f1ef636d46f291e36ff2e47582ae89cabede934
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2744031
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861602}

--- chrome/browser/ui/toolbar/toolbar_actions_model_unittest.cc
+++ chrome/browser/ui/toolbar/toolbar_actions_model_unittest.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/toolbar/test_toolbar_action_view_controller.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/test/base/testing_profile.h"
 #include "components/crx_file/id_util.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/prefs/scoped_user_pref_update.h"
@@ -40,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
+#include "extensions/browser/extension_util.h"
 #include "extensions/browser/pref_names.h"
 #include "extensions/browser/test_extension_registry_observer.h"
 #include "extensions/browser/uninstall_reason.h"
@@ -258,6 +260,13 @@ testing::AssertionResult ToolbarActionsModelUnitTest::AddExtension(
     return testing::AssertionFailure()
            << "Failed to install extension: " << extension->name();
   }
+  // Make sure RegisterClient calls for storage are finished to avoid flaky
+  // crashes in QuotaManagerImpl::RegisterClient.
+  // TODO(crbug.com/1182630) : Remove this when 1182630 is fixed.
+  extensions::util::GetStoragePartitionForExtensionId(extension->id(),
+                                                      profile());
+  task_environment()->RunUntilIdle();
+
   return testing::AssertionSuccess();
 }
 
