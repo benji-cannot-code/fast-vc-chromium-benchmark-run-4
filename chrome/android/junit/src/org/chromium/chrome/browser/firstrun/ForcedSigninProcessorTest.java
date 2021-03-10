dabf23b9b13f2c4bfffe189284b427e87b77f857
Alice Wang <aliceywang@chromium.org>
2021-03-10 07:21:16 +0000

    [Android][Signin] Use CoreAccountInfo in ForcedSigninProcessor
    This CL uses CoreAccountInfo in ForcedSigninProcessor for sign-in.
    
    Bug: 1002056
    Change-Id: I6dff79d487f6be1bb30510219d73beffdc640f91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2744146
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861457}

--- chrome/android/junit/src/org/chromium/chrome/browser/firstrun/ForcedSigninProcessorTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/firstrun/ForcedSigninProcessorTest.java
@@ -24,7 +24,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.robolectric.annotation.Config;
 
+import org.chromium.base.task.test.CustomShadowAsyncTask;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.SyncFirstSetupCompleteSource;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -35,6 +37,7 @@ import org.chromium.chrome.browser.sync.ProfileSyncService;
 import org.chromium.chrome.test.util.browser.signin.AccountManagerTestRule;
 import org.chromium.components.signin.AccountUtils;
 import org.chromium.components.signin.ChildAccountStatus;
+import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.identitymanager.IdentityManager;
 import org.chromium.components.signin.metrics.SigninAccessPoint;
 import org.chromium.components.signin.test.util.FakeAccountManagerFacade;
@@ -43,6 +46,7 @@ import org.chromium.components.signin.test.util.FakeAccountManagerFacade;
  * JUnit tests for {@link ForcedSigninProcessor}.
  */
 @RunWith(BaseRobolectricTestRunner.class)
+@Config(shadows = {CustomShadowAsyncTask.class})
 public class ForcedSigninProcessorTest {
     private static final Account CHILD_ACCOUNT =
             AccountUtils.createAccountFromName("child.account@gmail.com");
@@ -92,7 +96,7 @@ public class ForcedSigninProcessorTest {
             return null;
         })
                 .when(mSigninManagerMock)
-                .signinAndEnableSync(anyInt(), any(Account.class), notNull());
+                .signinAndEnableSync(anyInt(), any(CoreAccountInfo.class), notNull());
     }
 
     @Test
@@ -120,21 +124,21 @@ public class ForcedSigninProcessorTest {
         ForcedSigninProcessor.start();
         verify(mSigninManagerMock).onFirstRunCheckDone();
         verify(mSigninManagerMock, never())
-                .signinAndEnableSync(anyInt(), any(Account.class), any());
+                .signinAndEnableSync(anyInt(), any(CoreAccountInfo.class), any());
         verify(mProfileSyncServiceMock, never())
                 .setFirstSetupComplete(SyncFirstSetupCompleteSource.BASIC_FLOW);
     }
 
     @Test
     public void testStartWhenSigninAllowed() {
-        mAccountManagerTestRule.addAccount(CHILD_ACCOUNT);
+        CoreAccountInfo childAccount = mAccountManagerTestRule.addAccount(CHILD_ACCOUNT);
         when(mSigninManagerMock.isSignInAllowed()).thenReturn(true);
 
         ForcedSigninProcessor.start();
         verify(mSigninManagerMock).onFirstRunCheckDone();
         verify(mSigninManagerMock)
                 .signinAndEnableSync(
-                        eq(SigninAccessPoint.FORCED_SIGNIN), eq(CHILD_ACCOUNT), notNull());
+                        eq(SigninAccessPoint.FORCED_SIGNIN), eq(childAccount), notNull());
         verify(mProfileSyncServiceMock)
                 .setFirstSetupComplete(SyncFirstSetupCompleteSource.BASIC_FLOW);
     }
