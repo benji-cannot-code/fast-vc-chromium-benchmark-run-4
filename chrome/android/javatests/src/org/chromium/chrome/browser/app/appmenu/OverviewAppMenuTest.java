1e58f6b48f3d6fd7408aae747a8a10190b52ff35
Gang Wu <gangwu@chromium.org>
2021-03-10 21:28:41 +0000

    Reland "App Menu phase 1 and 2 clean up"
    This reload update 2 things from original one.
    1. Update render tests' gold name for the icon row.
    2. Update the preview test. The test will fail on small screen devices
    because the preview menu item was moved to the bottom of the app menu
    and it might be off the screen.
    
    This is a reland of d2a9fdb050db740257748be46272da959af7270a
    
    Original change's description:
    > App Menu phase 1 and 2 clean up
    >
    > Remove phase 1 and 2 flags, and enable icons and regroups by default.
    > This CL delete main_menu.xml and rename main_menu_regroup.xml to
    > main_menu.xml.
    >
    > Change-Id: I039288475f46e851b9ce40207a324e41d61eacf5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2740310
    > Commit-Queue: Weilun Shi <sweilun@chromium.org>
    > Reviewed-by: Weilun Shi <sweilun@chromium.org>
    > Reviewed-by: Theresa  <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#860943}
    
    Change-Id: Id24d531b069706999d035d6e9083e8b863171de5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2746058
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861692}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/OverviewAppMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/OverviewAppMenuTest.java
@@ -144,9 +144,10 @@ public class OverviewAppMenuTest {
                 int itemId = item.getItemId();
                 assertTrue(itemId == R.id.new_tab_menu_id
                         || itemId == R.id.new_incognito_tab_menu_id
-                        || itemId == R.id.all_bookmarks_menu_id
-                        || itemId == R.id.recent_tabs_menu_id || itemId == R.id.open_history_menu_id
-                        || itemId == R.id.downloads_menu_id || itemId == R.id.close_all_tabs_menu_id
+                        || itemId == R.id.divider_line_id || itemId == R.id.open_history_menu_id
+                        || itemId == R.id.downloads_menu_id || itemId == R.id.all_bookmarks_menu_id
+                        || itemId == R.id.recent_tabs_menu_id
+                        || itemId == R.id.close_all_tabs_menu_id
                         || itemId == R.id.close_all_incognito_tabs_menu_id
                         || itemId == R.id.menu_group_tabs || itemId == R.id.track_prices_row_menu_id
                         || itemId == R.id.preferences_id);
@@ -159,7 +160,7 @@ public class OverviewAppMenuTest {
                 checkedMenuItems++;
             }
         }
-        assertThat(checkedMenuItems, equalTo(11));
+        assertThat(checkedMenuItems, equalTo(13));
     }
 
     @Test
@@ -182,9 +183,10 @@ public class OverviewAppMenuTest {
                 int itemId = item.getItemId();
                 assertTrue(itemId == R.id.new_tab_menu_id
                         || itemId == R.id.new_incognito_tab_menu_id
-                        || itemId == R.id.all_bookmarks_menu_id
-                        || itemId == R.id.recent_tabs_menu_id || itemId == R.id.open_history_menu_id
-                        || itemId == R.id.downloads_menu_id || itemId == R.id.close_all_tabs_menu_id
+                        || itemId == R.id.divider_line_id || itemId == R.id.open_history_menu_id
+                        || itemId == R.id.downloads_menu_id || itemId == R.id.all_bookmarks_menu_id
+                        || itemId == R.id.recent_tabs_menu_id
+                        || itemId == R.id.close_all_tabs_menu_id
                         || itemId == R.id.close_all_incognito_tabs_menu_id
                         || itemId == R.id.menu_group_tabs || itemId == R.id.track_prices_row_menu_id
                         || itemId == R.id.preferences_id);
@@ -197,7 +199,7 @@ public class OverviewAppMenuTest {
                 checkedMenuItems++;
             }
         }
-        assertThat(checkedMenuItems, equalTo(11));
+        assertThat(checkedMenuItems, equalTo(13));
     }
 
     @Test
