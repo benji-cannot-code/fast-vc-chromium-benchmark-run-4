b085d83af50f624eec9ab72081898daab7c210dd
François Beaufort <beaufort.francois@gmail.com>
2021-03-10 21:23:56 +0000

    System Media Controls: Stop scrubber from moving while paused on macOS
    This CL fixes a bug in the Now Playing widget on macOS where the
    scrubber was continuing to count forward even when media was paused. It
    does so by storing most recently received playback status and position
    and update those in a debounce timer as the Now Playing widget doesn't
    support well when info are updated multiple times in a row.
    
    Bug: 1112756
    Change-Id: I9e20b76d6f63d18f378402ae6ddf617faec84781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747965
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#861683}

--- components/system_media_controls/mac/now_playing_info_center_delegate_cocoa.mm
+++ components/system_media_controls/mac/now_playing_info_center_delegate_cocoa.mm
@@ -41,45 +41,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)setPlaybackState:(MPNowPlayingPlaybackState)state {
   [MPNowPlayingInfoCenter defaultCenter].playbackState = state;
-  [self updateNowPlayingInfo];
 }
 
 - (void)setTitle:(NSString*)title {
   [_nowPlayingInfo setObject:title forKey:MPMediaItemPropertyTitle];
-  [self updateNowPlayingInfo];
 }
 
 - (void)setArtist:(NSString*)artist {
   [_nowPlayingInfo setObject:artist forKey:MPMediaItemPropertyArtist];
-  [self updateNowPlayingInfo];
 }
 
 - (void)setAlbum:(NSString*)album {
   [_nowPlayingInfo setObject:album forKey:MPMediaItemPropertyAlbumTitle];
-  [self updateNowPlayingInfo];
 }
 
 - (void)setPlaybackRate:(NSNumber*)rate {
   [_nowPlayingInfo setObject:rate forKey:MPNowPlayingInfoPropertyPlaybackRate];
-  [self updateNowPlayingInfo];
 }
 
 - (void)setCurrentPlaybackDate:(NSDate*)date {
   [_nowPlayingInfo setObject:date
                       forKey:MPNowPlayingInfoPropertyCurrentPlaybackDate];
-  [self updateNowPlayingInfo];
 }
 
 - (void)setElapsedPlaybackTime:(NSNumber*)time {
   [_nowPlayingInfo setObject:time
                       forKey:MPNowPlayingInfoPropertyElapsedPlaybackTime];
-  [self updateNowPlayingInfo];
 }
 
 - (void)setDuration:(NSNumber*)duration {
   [_nowPlayingInfo setObject:duration
                       forKey:MPMediaItemPropertyPlaybackDuration];
-  [self updateNowPlayingInfo];
 }
 
 - (void)clearMetadata {
