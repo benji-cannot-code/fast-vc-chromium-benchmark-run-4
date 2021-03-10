1b037869741c3e5783ef808d8a3a9e3fd11f836f
Gordon Seto <gordonseto@google.com>
2021-03-10 18:04:35 +0000

    [CrOS Settings] Hide technology badge in OOBE.
    Hide the cellular technology badge in the OOBE network selection screen.
    
    Bug: 929907
    Change-Id: Ib8fd1b5044cabe6631a940ee5092ae04890a83b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2743088
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/master@{#861614}

--- chrome/browser/resources/chromeos/login/components/network_select_login.js
+++ chrome/browser/resources/chromeos/login/components/network_select_login.js
@@ -66,10 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
        */
       showTechnologyBadge_: {
         type: Boolean,
-        value() {
-          return loadTimeData.valueExists('showTechnologyBadge') &&
-              loadTimeData.getBoolean('showTechnologyBadge');
-        }
+        value: false,
       }
     },
 
--- chrome/browser/ui/webui/chromeos/login/core_oobe_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/core_oobe_handler.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/public/ash_interfaces.h"
-#include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/event_rewriter_controller.h"
 #include "ash/public/cpp/shelf_config.h"
 #include "ash/public/cpp/tablet_mode.h"
@@ -117,8 +116,6 @@ void CoreOobeHandler::GetAdditionalParameters(base::DictionaryValue* dict) {
                base::Value(ash::TabletMode::Get()->InTabletMode()));
   dict->SetKey("isDemoModeEnabled",
                base::Value(DemoSetupController::IsDemoModeAllowed()));
-  dict->SetKey("showTechnologyBadge",
-               base::Value(!ash::features::IsSeparateNetworkIconsEnabled()));
   dict->SetKey("newLayoutEnabled",
                base::Value(features::IsNewOobeLayoutEnabled()));
 }
