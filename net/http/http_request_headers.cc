e70217952257d209c0da8ef6b204519250f5a951
Sebastien Marchand <sebmarchand@chromium.org>
2021-03-10 02:42:39 +0000

    Don't freeze tabs that set the Cache-Control: no-store header
    Tabs that have the "Cache-Control: no-store" header should not be frozen
    because it could introduce a small privacy regression when returning to
    the tab after it has been frozen in background for a long time (e.g. the
    content of a banking tab could be visible for a few frames before the
    site realizing that the user should have been logged out a while ago).
    
    This requires exposing the last HTTP response headers in
    RenderFrameHost.
    
    Bug: 1144025
    Change-Id: I98f717b489e4bfed70ad9aa88a727497c0a3c04a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737035
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861397}

--- net/http/http_request_headers.cc
+++ net/http/http_request_headers.cc
@@ -54,6 +54,8 @@ const char HttpRequestHeaders::kReferer[] = "Referer";
 const char HttpRequestHeaders::kTransferEncoding[] = "Transfer-Encoding";
 const char HttpRequestHeaders::kUserAgent[] = "User-Agent";
 
+const char HttpRequestHeaders::kNoStoreDirective[] = "no-store";
+
 HttpRequestHeaders::HeaderKeyValuePair::HeaderKeyValuePair() = default;
 
 HttpRequestHeaders::HeaderKeyValuePair::HeaderKeyValuePair(
