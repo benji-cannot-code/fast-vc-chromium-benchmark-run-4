072bf4cc73eacef056dd333218830d11cb2d9abd
Gabriel Szlechtman <gabrielsz@google.com>
2021-03-10 21:43:18 +0000

    Refactor kHappinessTrackingSystem out of HatsFinchHelper
    Bug: b/177999743
    Change-Id: I88fe9aeb00980d225a363402747cfe8ab9e8ade9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747154
    Commit-Queue: Gabriel Szlechtman <gabrielsz@google.com>
    Reviewed-by: Malay Keshav <malaykeshav@chromium.org>
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861711}

--- chrome/browser/chromeos/hats/hats_finch_helper.h
+++ chrome/browser/chromeos/hats/hats_finch_helper.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/time/time.h"
+#include "chrome/common/chrome_features.h"
 
 class Profile;
 
@@ -20,9 +21,9 @@ namespace chromeos {
 // information related to the hats finch experiment.
 class HatsFinchHelper {
  public:
-  static std::string GetTriggerID();
+  static std::string GetTriggerID(const base::Feature& feature);
 
-  explicit HatsFinchHelper(Profile* profile);
+  explicit HatsFinchHelper(Profile* profile, const base::Feature& feature);
   ~HatsFinchHelper();
 
   bool IsDeviceSelectedForCurrentCycle() const {
@@ -46,7 +47,7 @@ class HatsFinchHelper {
 
   // Loads all the param values from the finch seed and initializes the member
   // variables.
-  void LoadFinchParamValues();
+  void LoadFinchParamValues(const base::Feature& feature);
 
   // Returns true if the survey cycle that was active most recently has passed
   // its end date.
