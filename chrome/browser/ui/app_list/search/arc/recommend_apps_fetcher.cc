9778bbc9b06f3b114208b34d81ae50752dd3385a
Thanh Nguyen <thanhdng@chromium.org>
2021-03-10 00:34:08 +0000

    [D42] Adding classes to fetch apps recommendation
    This CL adds some classes which help:
    1. Get apps recommendation from an HTTP endpoint
    2. Parse the response
    3. Send the response to its client.
    
    Bug: 1179489
    Change-Id: Iac100a808a22f72eb1ce37e6005a9085ff6dca2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2703040
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861358}

---
+++ chrome/browser/ui/app_list/search/arc/recommend_apps_fetcher.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/app_list/search/arc/recommend_apps_fetcher.h"
+
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/app_list/search/arc/recommend_apps_fetcher_impl.h"
+#include "content/public/browser/storage_partition.h"
+
+namespace app_list {
+
+// static
+std::unique_ptr<RecommendAppsFetcher> RecommendAppsFetcher::Create(
+    RecommendAppsFetcherDelegate* delegate) {
+  return std::make_unique<RecommendAppsFetcherImpl>(
+      delegate, content::BrowserContext::GetDefaultStoragePartition(
+                    ProfileManager::GetActiveUserProfile())
+                    ->GetURLLoaderFactoryForBrowserProcess()
+                    .get());
+}
+
+}  // namespace app_list
