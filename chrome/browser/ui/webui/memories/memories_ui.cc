6bbf3ed757645c1bf0d978adb02ecffa293a3fc4
Moe Ahmadi <mahmadi@chromium.org>
2021-03-10 22:29:25 +0000

    [memories] Memories landing page (part 1)
    Memories result + empty memory card
    
    screenshot/F4aAMyCmemjAKj5
    
    Bug: 1180970
    Change-Id: I46317f9cb3dc7746afa0f46c372d9d8fe3db1e4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2744287
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861738}

--- chrome/browser/ui/webui/memories/memories_ui.cc
+++ chrome/browser/ui/webui/memories/memories_ui.cc
@@ -6,12 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/memories/memories_ui.h"
 
 #include "base/logging.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/webui/favicon_source.h"
 #include "chrome/browser/ui/webui/memories/memories_handler.h"
+#include "chrome/browser/ui/webui/sanitized_image_source.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/memories_resources.h"
 #include "chrome/grit/memories_resources_map.h"
+#include "components/favicon_base/favicon_url_parser.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -20,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-content::WebUIDataSource* CreateAndSetupWebUIDataSource() {
+content::WebUIDataSource* CreateAndSetupWebUIDataSource(Profile* profile) {
   content::WebUIDataSource* source =
       content::WebUIDataSource::Create(chrome::kChromeUIMemoriesHost);
 
@@ -28,6 +32,10 @@ content::WebUIDataSource* CreateAndSetupWebUIDataSource() {
       {"title", IDS_MEMORIES_PAGE_TITLE},
   };
   source->AddLocalizedStrings(kStrings);
+
+  // TODO(crbug.com/1173908): Replace these with localized strings.
+  source->AddString("memoryTitleDescription",
+                    base::UTF8ToUTF16("Based on previous web activity"));
 
   webui::SetupWebUIDataSource(
       source, base::make_span(kMemoriesResources, kMemoriesResourcesSize),
@@ -45,7 +53,7 @@ MemoriesUI::MemoriesUI(content::WebUI* web_ui)
   DCHECK(profile_);
   DCHECK(web_contents_);
 
-  auto* source = CreateAndSetupWebUIDataSource();
+  auto* source = CreateAndSetupWebUIDataSource(profile_);
   content::WebUIDataSource::Add(profile_, source);
 }
 
