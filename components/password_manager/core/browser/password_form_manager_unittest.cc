6978fc42088c7dc88a698b1576999c36d1c4c5f8
Christos Froussios <cfroussios@chromium.org>
2021-03-10 10:22:48 +0000

    [Autofill] Enable AutofillMetadataUploads by default
    The experiment is at 50% stable. Full rollout will follow.
    
    Bug: 1183712
    Change-Id: I9564bf36768f837b2b3f7a4517fbd5d7da8e92f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2728995
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861508}

--- components/password_manager/core/browser/password_form_manager_unittest.cc
+++ components/password_manager/core/browser/password_form_manager_unittest.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_download_manager.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/form_structure.h"
+#include "components/autofill/core/browser/randomized_encoder.h"
+#include "components/autofill/core/common/autofill_prefs.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill/core/common/form_field_data.h"
 #include "components/autofill/core/common/gaia_id_hash.h"
@@ -299,6 +301,11 @@ class PasswordFormManagerTest : public testing::Test,
         prefs::kProfileStoreDateLastUsedForFilling, base::Time());
     pref_service_.registry()->RegisterTimePref(
         prefs::kAccountStoreDateLastUsedForFilling, base::Time());
+    pref_service_.registry()->RegisterBooleanPref(
+        autofill::RandomizedEncoder::kUrlKeyedAnonymizedDataCollectionEnabled,
+        true);
+    pref_service_.registry()->RegisterStringPref(
+        autofill::prefs::kAutofillUploadEncodingSeed, "seed");
 
     form_manager_->set_wait_for_server_predictions_for_filling(true);
 
