b83f86c6cbff67dbd38306daf3c06952754cf684
Michael Lippautz <mlippautz@chromium.org>
2021-03-11 23:36:21 +0000

    heap: Oilpan library: Fix includes for HeapDeque
    Make VectorTraits for HeapDeque available through heap_deque.h
    
    No-try: true
    Bug: 1056170
    Change-Id: If66d37472ff080c156ab45aeee7203004f195101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2754507
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862190}

--- third_party/blink/renderer/platform/heap/collection_support/heap_deque.h
+++ third_party/blink/renderer/platform/heap/collection_support/heap_deque.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_COLLECTION_SUPPORT_HEAP_DEQUE_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_COLLECTION_SUPPORT_HEAP_DEQUE_H_
 
+// Include heap_vector.h to also make general VectorTraits available.
+#include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/heap/heap_allocator_impl.h"
 #include "third_party/blink/renderer/platform/heap/visitor.h"
@@ -55,5 +57,19 @@ class HeapDeque final : public GarbageCollected<HeapDeque<T>>,
 };
 
 }  // namespace blink
+
+namespace WTF {
+
+template <typename T>
+struct VectorTraits<blink::HeapDeque<T>>
+    : VectorTraitsBase<blink::HeapDeque<T>> {
+  STATIC_ONLY(VectorTraits);
+  static const bool kNeedsDestruction = false;
+  static const bool kCanInitializeWithMemset = true;
+  static const bool kCanClearUnusedSlotsWithMemset = true;
+  static const bool kCanMoveWithMemcpy = true;
+};
+
+}  // namespace WTF
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_HEAP_COLLECTION_SUPPORT_HEAP_DEQUE_H_
