c84869422b219c21f023e3b1bdf7799daff6eb69
Lily Chen <chlily@chromium.org>
2021-03-11 16:02:02 +0000

    Add UseCounter and UKM features for Lax-allowing-unsafe cookies
    This will measure the usage of cookies without an explicit SameSite
    attribute, which are sent on requests with unsafe methods due to being
    less than 2 minutes old.
    
    Bug: 990439
    Change-Id: I3c561756f2a9da1f47bf2a414faf9c6869ba4232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2749455
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861985}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -194,6 +194,7 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kAVIFImage,
           WebFeature::kGetDisplayMedia,
           WebFeature::kGetCurrentBrowsingContextMedia,
+          WebFeature::kLaxAllowingUnsafeCookies,
       }));
   return *opt_in_features;
 }
