2990cb9232ec2a79098e25739855be909d039d34
Alex Ilin <alexilin@chromium.org>
2021-03-11 15:36:59 +0000

    [signin] Refactor SigninErrorUI to accept SigninUIError
    A new SigninUIError class was recently created for passing an error
    that should be displayed in SigninErrorUI.
    
    This CL finishes plumbing of SigninUIError through all the callers of
    LoginUIService::DisplayLoginResult().
    
    Bug: 1133189
    Change-Id: I87fdc10a66f0410e3f213227f8a36b650e11f717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2743779
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861979}

--- chrome/browser/ui/webui/signin/login_ui_service.h
+++ chrome/browser/ui/webui/signin/login_ui_service.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/strings/string16.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/ui/webui/signin/signin_ui_error.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 class Browser;
@@ -89,11 +90,9 @@ class LoginUIService : public KeyedService {
                                 const std::string& email_hint);
 
   // Displays login results. This is either the Modal Signin Error dialog if
-  // |error_message| is a non-empty string, or the User Menu with a blue header
-  // toast otherwise.
-  virtual void DisplayLoginResult(Browser* browser,
-                                  const base::string16& error_message,
-                                  const base::string16& email);
+  // |error.message()| is a non-empty string, or the User Menu with a blue
+  // header toast otherwise.
+  virtual void DisplayLoginResult(Browser* browser, const SigninUIError& error);
 
   // Set the profile blocking modal error dialog message.
   virtual void SetProfileBlockingErrorMessage();
@@ -102,25 +101,23 @@ class LoginUIService : public KeyedService {
   // error message.
   bool IsDisplayingProfileBlockedErrorMessage() const;
 
-  // Gets the last login result set through |DisplayLoginResult|.
-  const base::string16& GetLastLoginResult() const;
-
-  // Gets the last email used for signing in when a signin error occured; set
-  // through |DisplayLoginResult|.
-  const base::string16& GetLastLoginErrorEmail() const;
+#if !BUILDFLAG(IS_CHROMEOS_ASH)
+  // Gets the last error set through |DisplayLoginResult|.
+  const SigninUIError& GetLastLoginError() const;
+#endif
 
  private:
   // Weak pointers to the recently opened UIs, with the most recent in front.
   std::list<LoginUI*> ui_list_;
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
   Profile* profile_;
+  SigninUIError last_login_error_ = SigninUIError::Ok();
 #endif
 
   // List of observers.
   base::ObserverList<Observer>::Unchecked observer_list_;
 
-  base::string16 last_login_result_;
-  base::string16 last_login_error_email_;
+  // TODO(https://crbug.com/1133189): merge into SigninUIError.
   bool is_displaying_profile_blocking_error_message_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(LoginUIService);
--- chrome/browser/ui/webui/signin/signin_ui_error.h
+++ chrome/browser/ui/webui/signin/signin_ui_error.h
@@ -10,6 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/strings/string16.h"
+#include "build/build_config.h"
+
+#if defined(OS_WIN)
+#include "chrome/credential_provider/common/gcp_strings.h"
+#endif
+
+class GoogleServiceAuthError;
 
 // Holds different sign-in error types along with error messages for displaying
 // in the UI.
@@ -24,6 +31,8 @@ class SigninUIError {
     kWrongReauthAccount,
     kAccountAlreadyUsedByAnotherProfile,
     kProfileWasUsedByAnotherAccount,
+    kFromGoogleServiceAuthError,
+    kFromCredentialProviderUiExitCode,
   };
 
   // Following static functions construct a `SigninUIError` with a corresponding
@@ -40,6 +49,14 @@ class SigninUIError {
   static SigninUIError ProfileWasUsedByAnotherAccount(
       const std::string& email,
       const std::string& last_email);
+  static SigninUIError FromGoogleServiceAuthError(
+      const std::string& email,
+      const GoogleServiceAuthError& error);
+#if defined(OS_WIN)
+  static SigninUIError FromCredentialProviderUiExitCode(
+      const std::string& email,
+      credential_provider::UiExitCodes exit_code);
+#endif
 
   SigninUIError(const SigninUIError& other);
   SigninUIError& operator=(const SigninUIError& other);
@@ -51,9 +68,15 @@ class SigninUIError {
   const base::string16& email() const;
   const base::string16& message() const;
 
-  // Should be called only if `type()` == //
+  // Should be called only if `type()` ==
   // `Type::kAccountAlreadyUsedByAnotherProfile`.
   const base::FilePath& another_profile_path() const;
+
+#if defined(OS_WIN)
+  // Should be called only if `type()` ==
+  // `Type::kFromCredentialProviderUiExitCode`.
+  credential_provider::UiExitCodes credential_provider_exit_code() const;
+#endif
 
   bool operator==(const SigninUIError& other) const;
   bool operator!=(const SigninUIError& other) const;
@@ -68,8 +91,14 @@ class SigninUIError {
   base::string16 email_;
   base::string16 message_;
 
-  // Defined only for Type::kAccountAlreadyUsedByAnotherProfile
+  // Defined only for Type::kAccountAlreadyUsedByAnotherProfile.
   base::FilePath another_profile_path_;
+
+#if defined(OS_WIN)
+  // Defined only for Type::kFromCredentialProviderUiExitCode.
+  credential_provider::UiExitCodes credential_provider_exit_code_ =
+      credential_provider::UiExitCodes::kUiecSuccess;
+#endif
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_SIGNIN_SIGNIN_UI_ERROR_H_
