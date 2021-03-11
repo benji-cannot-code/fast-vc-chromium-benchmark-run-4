72ebc79647b5ff342c902fd93050f2493da3ee6d
Greg Thompson <grt@chromium.org>
2021-03-11 17:29:13 +0000

    Surface extended stable in enterprise enterprise_management::BrowserReport.
    Clients that follow the extended stable update channel report this via
    the "is_extended_stable_channel" field in BrowserReport messages.
    
    Bug: 1185621
    Change-Id: I58d43c8a0cb4542bd8dc1ca20cb4f21c8aba46d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2749298
    Commit-Queue: Owen Min <zmin@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862016}

--- components/enterprise/browser/reporting/browser_report_generator.h
+++ components/enterprise/browser/reporting/browser_report_generator.h
@@ -32,6 +32,7 @@ class BrowserReportGenerator {
 
     virtual std::string GetExecutablePath() = 0;
     virtual version_info::Channel GetChannel() = 0;
+    virtual bool IsExtendedStableChannel() = 0;
     virtual void GenerateBuildStateInfo(
         enterprise_management::BrowserReport* report) = 0;
     virtual void GenerateProfileInfo(
