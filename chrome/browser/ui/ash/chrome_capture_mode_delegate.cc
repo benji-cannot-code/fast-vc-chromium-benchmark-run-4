47fb99b996b23abbf6c0b8edc7b0b91d2165bb37
Judy Wang <shidi@chromium.org>
2021-03-11 22:50:29 +0000

    ChromeOS: Re-enable screenshot during OOBE.
    Re-enable taking full screenshot in login screen.
    File will be saved to temporary location.
    
    Bug: 1180454
    Test: Manual tested on DUT.
    
    Change-Id: Ibdf464c1d0a575d5c5e968c4d425244b815207ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2733256
    Commit-Queue: Judy Wang <shidi@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862170}

--- chrome/browser/ui/ash/chrome_capture_mode_delegate.cc
+++ chrome/browser/ui/ash/chrome_capture_mode_delegate.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/services/recording/public/mojom/recording_service.mojom.h"
 #include "base/check.h"
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/i18n/time_formatting.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
@@ -73,11 +74,16 @@ void ChromeCaptureModeDelegate::InterruptVideoRecordingIfAny() {
     std::move(interrupt_video_recording_callback_).Run();
 }
 
-base::FilePath ChromeCaptureModeDelegate::GetActiveUserDownloadsDir() const {
-  DCHECK(chromeos::LoginState::Get()->IsUserLoggedIn());
-  DownloadPrefs* download_prefs =
-      DownloadPrefs::FromBrowserContext(ProfileManager::GetActiveUserProfile());
-  return download_prefs->DownloadPath();
+base::FilePath ChromeCaptureModeDelegate::GetScreenCaptureDir() const {
+  if (chromeos::LoginState::Get()->IsUserLoggedIn()) {
+    DownloadPrefs* download_prefs = DownloadPrefs::FromBrowserContext(
+        ProfileManager::GetActiveUserProfile());
+    return download_prefs->DownloadPath();
+  }
+  base::FilePath tmp_dir;
+  if (!base::GetTempDir(&tmp_dir))
+    LOG(ERROR) << "Failed to find temporary directory.";
+  return tmp_dir;
 }
 
 void ChromeCaptureModeDelegate::ShowScreenCaptureItemInFolder(
