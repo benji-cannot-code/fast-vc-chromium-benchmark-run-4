2a7636d5a25cfc44b17740ec7c51ccfe2c2d60ff
rbpotter <rbpotter@chromium.org>
2021-03-11 19:19:33 +0000

    Sync internals: fix CL breakup issue
    Function in index.html got prematurely renamed when the large original
    CL was broken up.
    
    Define the function with the new name.
    
    Bug: 1173575
    Change-Id: Ia74fca966d397d94e6a09406fbe9672d9d27239b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2752531
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Auto-Submit: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862076}

--- components/sync/driver/resources/about.js
+++ components/sync/driver/resources/about.js
@@ -236,3 +236,5 @@ cr.define('chrome.sync.about_tab', function() {
 
 document.addEventListener(
     'DOMContentLoaded', chrome.sync.about_tab.onLoad, false);
+
+window.addAboutExpandListener = chrome.sync.about_tab.addExpandListener;
