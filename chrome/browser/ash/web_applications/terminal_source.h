3817bb3bddecaefdd5662f7a3c7c9988dbc3332b
Joel Hockey <joelhockey@chromium.org>
2021-03-11 04:51:34 +0000

    Run crosh from terminal.html
    Now that crosh is using terminal settings page, we must load crosh with
    the same set of defaults for all prefs.  The simplest way to do this is
    to run crosh out of terminal.html.
    
    When terminal.html and the associated JS detects that it is running as
    chrome-untrusted://crosh, it will call
    terminalPrivate.openTerminalProcess() which opens crosh rather than
    terminalPrivate.openVmshellProcess() which opens vmshell.
    
    Terminal is configured in _api_features.json to not have access to
    openTerminalProcess() which can open crosh.
    
    Terminal uses chrome.tabs to set tabs as non discardable so they wont
    be dumped on low memory, and hterm uses chrome.windows during init
    when chrome.tabs is available.  So crosh needs to also be given access
    to these apis.
    
    Change-Id: Idd9826ac2d4db905589c91775b0be9182d8db716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747315
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Jason Lin <lxj@google.com>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861858}

--- chrome/browser/ash/web_applications/terminal_source.h
+++ chrome/browser/ash/web_applications/terminal_source.h
@@ -28,9 +28,7 @@ class TerminalSource : public content::URLDataSource {
   ~TerminalSource() override;
 
  private:
-  explicit TerminalSource(Profile* profile,
-                          std::string source,
-                          std::string default_file);
+  explicit TerminalSource(Profile* profile, std::string source);
 
   // content::URLDataSource:
   std::string GetSource() override;
@@ -49,7 +47,6 @@ class TerminalSource : public content::URLDataSource {
 
   Profile* profile_;
   std::string source_;
-  std::string default_file_;
   ui::TemplateReplacements replacements_;
 
   DISALLOW_COPY_AND_ASSIGN(TerminalSource);
