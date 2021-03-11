cfca60163a2fcaeec331df51386294f43fa264ab
Victor-Gabriel Savu <vsavu@google.com>
2021-03-11 11:22:37 +0000

    Remove histogram_tester_ from heartbeat_scheduler_unittest.cc
    Bug: None
    Change-Id: Ic678e97fde0e786bcd2e91c54b4d658ecf83f5a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2752627
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Victor-Gabriel Savu <vsavu@google.com>
    Cr-Commit-Position: refs/heads/master@{#861935}

--- chrome/browser/chromeos/policy/heartbeat_scheduler_unittest.cc
+++ chrome/browser/chromeos/policy/heartbeat_scheduler_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/gmock_move_support.h"
-#include "base/test/metrics/histogram_tester.h"
 #include "base/test/test_simple_task_runner.h"
 #include "chrome/browser/ash/settings/scoped_testing_cros_settings.h"
 #include "chrome/browser/ash/settings/stub_cros_settings_provider.h"
@@ -182,8 +181,6 @@ class HeartbeatSchedulerTest : public testing::Test {
 
   // The HeartbeatScheduler instance under test.
   policy::HeartbeatScheduler scheduler_;
-
-  base::HistogramTester histogram_tester_;
 };
 
 TEST_F(HeartbeatSchedulerTest, Basic) {
