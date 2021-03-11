165ad726cb6deca3a57111a2a4cff482402cd8a5
Takumi Fujimoto <takumif@chromium.org>
2021-03-11 00:20:50 +0000

    Hide Meet from CrOS system tray's Cast options when feature is disabled
    The backend still needs to provide Meet sinks even when the feature is
    disabled, so we hide it in the system tray UI.
    
    Bug: 1186410
    Change-Id: I57b57da6133bcd1e2fcfab08c8e960664704b1b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747694
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: John Williams <jrw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861772}

--- chrome/browser/ui/ash/cast_config_controller_media_router.cc
+++ chrome/browser/ui/ash/cast_config_controller_media_router.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/media_router/browser/media_router_factory.h"
 #include "components/media_router/browser/media_routes_observer.h"
 #include "components/media_router/browser/media_sinks_observer.h"
+#include "components/media_router/common/media_sink.h"
 #include "components/media_router/common/media_source.h"
 #include "components/user_manager/user_manager.h"
 
@@ -204,6 +205,13 @@ void CastConfigControllerMediaRouter::RequestDeviceRefresh() {
   devices_.clear();
 
   for (const media_router::MediaSink& sink : device_cache()->sinks()) {
+    // TODO(crbug.com/1154342): Remove this if-statement once the toolbar's Cast
+    // dialog no longer needs Meet sinks and they are disabled in the backend.
+    if (sink.IsMaybeCloudSink() &&
+        !base::FeatureList::IsEnabled(
+            media_router::kCastToMeetingFromCastDialog)) {
+      continue;
+    }
     ash::SinkAndRoute device;
     device.sink.id = sink.id();
     device.sink.name = sink.name();
