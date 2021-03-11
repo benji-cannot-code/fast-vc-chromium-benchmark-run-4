2f827475c1d97a2b16eb2c2c8c847cd52d510882
Katie Dektar <katie@chromium.org>
2021-03-11 07:37:11 +0000

    Reland "Dictation can use on-device speech recognition behind a flag."
    This reverts commit d7ed08e7015a572769eb10c432ac8a632860062c.
    Flaky tests have been fixed by using a FakeSpeechRecognitionManager.
    Earlier tests were passing before the SpeechRecognitionManager returned
    an error asynchronously. Changes to make Dictation start-up async
    exposed these errors. Using a FakeSpeechRecognitionManager avoids errors.
    Some minor improvements have been made to FakeSpeechRecognitionManager
    to make it more similar in behavior to SpeechRecognitionManagerImpl
    and to remove flakes with using a closure on two different threads.
    
    Original change description:
    > Dictation can use on-device speech recognition behind a flag.
    >
    > Fleshes out implementation of OnDeviceSpeechRecognizer and allows
    > Dictation to use OnDeviceSpeechRecognizer behind a flag, when it
    > is available.
    >
    > Includes some clean-up to SpeechRecognizerStatus to ensure the
    > statuses make sense for on-device and network speech. Removes
    > unused state SPEECH_RECOGNIZER_STOPPING in SpeechRecognizerStatus
    > which was not used by NetworkSpeechRecognizer. Dictation and
    > Projector are the only users of SpeechRecognizerStatus.
    >
    > Improves NetworkSpeechRecognizer test to test status update
    > changes, and updates FakeSpeechRecognitionManager to send a more
    > complete set of callbacks during fake listening. Modifies
    > SpeechRecognitionAPITest to not succeed immediately after speech
    > started, as it now receives this callback before the speech callback.
    >
    > Bug: 1173135
    > AX-Relnotes: N/A
    > Test: Existing browser-test and manual testing.
    
    Change-Id: Ia46eb5b38619941e237b3b0262352038940e1902
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2745652
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861886}

--- chrome/test/data/extensions/platform_apps/web_view/speech_recognition_api/guest.js
+++ chrome/test/data/extensions/platform_apps/web_view/speech_recognition_api/guest.js
@@ -21,7 +21,6 @@ var runSpeechRecognitionAPI = function() {
     succeeded = true;
     LOG('r.onstart');
     notifyEmbedder(['recognition', 'onstart', '']);
-    r.abort();
   };
   r.onerror = function() {
     LOG('r.onerror');
--- content/public/test/fake_speech_recognition_manager.h
+++ content/public/test/fake_speech_recognition_manager.h
@@ -44,6 +44,9 @@ class FakeSpeechRecognitionManager : public SpeechRecognitionManager,
   }
 
   void WaitForRecognitionStarted();
+  void WaitForRecognitionEnded();
+  void OnRecognitionStarted();
+  void OnRecognitionEnded();
 
   void SetFakeResult(const std::string& result);
 
@@ -91,6 +94,7 @@ class FakeSpeechRecognitionManager : public SpeechRecognitionManager,
   bool did_cancel_all_;
   bool should_send_fake_response_;
   base::OnceClosure recognition_started_closure_;
+  base::OnceClosure recognition_ended_closure_;
   SpeechRecognitionManagerDelegate* delegate_;  // Not owned.
 
   DISALLOW_COPY_AND_ASSIGN(FakeSpeechRecognitionManager);
