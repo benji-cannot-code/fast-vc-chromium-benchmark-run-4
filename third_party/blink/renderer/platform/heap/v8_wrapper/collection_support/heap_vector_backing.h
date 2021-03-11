942cc6183f428d8488e7159f5345c06d26800094
Michael Lippautz <mlippautz@chromium.org>
2021-03-11 23:55:20 +0000

    heap: Oilpan library: Add include to enable container annotations
    Container annotations are only available when including the proper
    file from wTF. Without this include, the annotations will not be
    adjusted and ASAN will crash as the GC reads from poisoned memory.
    
    No-try: true
    Bug: 1056170
    Change-Id: I2ffedec7177eef91638f1e1e2068d3fd8ef29673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2753929
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862200}

--- third_party/blink/renderer/platform/heap/v8_wrapper/collection_support/heap_vector_backing.h
+++ third_party/blink/renderer/platform/heap/v8_wrapper/collection_support/heap_vector_backing.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/thread_state.h"
 #include "third_party/blink/renderer/platform/heap/trace_traits.h"
 #include "third_party/blink/renderer/platform/wtf/conditional_destructor.h"
+#include "third_party/blink/renderer/platform/wtf/container_annotations.h"
 #include "third_party/blink/renderer/platform/wtf/type_traits.h"
 #include "third_party/blink/renderer/platform/wtf/vector_traits.h"
 #include "v8/include/cppgc/custom-space.h"
