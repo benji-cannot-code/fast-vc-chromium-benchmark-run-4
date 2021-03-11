d8facbc500eb25c2e951bbce9dde9de0abcd982e
Joon Ahn <joonbug@chromium.org>
2021-03-11 17:47:18 +0000

    diagnostics: record missing system info for visibility in jslogs for tast
    Bug: n/a
    Test: n/a
    Change-Id: Id68f511d158e2901f240aec3eab19947420daa23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2745982
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Auto-Submit: Joon Ahn <joonbug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862029}

--- chromeos/components/diagnostics_ui/resources/diagnostics_app.js
+++ chromeos/components/diagnostics_ui/resources/diagnostics_app.js
@@ -91,6 +91,7 @@ Polymer({
     this.systemDataProvider_.getSystemInfo().then((result) => {
       this.onSystemInfoReceived_(result.systemInfo);
     });
+    setTimeout(() => this.recordLateSystemInfo_(), 3000);
   },
 
   /**
@@ -100,6 +101,15 @@ Polymer({
   onSystemInfoReceived_(systemInfo) {
     this.systemInfoReceived_ = true;
     this.showBatteryStatusCard_ = systemInfo.deviceCapabilities.hasBattery;
+  },
+
+  /**
+   * @private
+   */
+  recordLateSystemInfo_() {
+    if (!this.systemInfoReceived_) {
+      console.warn('system info not received within three seconds.');
+    }
   },
 
   /** @protected */
