09d7000eb019f4f8857b9c2006f51a6e9c699432
Kramer Ge <fangzhoug@chromium.org>
2021-03-11 00:23:05 +0000

    Revert "Prerender: Update Document#prerendering on activation"
    This reverts commit 513bdf4e63d05cd9e79649c177338337a027be1c.
    
    Reason for revert: Potential suspect of increased flakiness in crbug.com/1114780
    
    Original change's description:
    > Prerender: Update Document#prerendering on activation
    >
    > This CL notifies the page being prerendered in Blink of activation to
    > update Document#prerendering and dispatch the prerenderingchange event.
    >
    > The draft spec requires the state update and the event dispatch must run
    > before deferred features run. However, this CL doesn't guarantee it yet.
    > This will be fixed by follow-up CLs.
    >
    > This fixes the existing test that checks the state is correctly updated
    > in the top-level frame. Follow-up CLs will add tests for nested frames.
    >
    > Design Doc:
    > https://docs.google.com/document/d/1_l1LDUALf8PbZIz5y_UYNGZmYCMKNpePDLBHn-gTsTM/edit?usp=sharing
    >
    > Change-Id: Ibd9a9e306fe431d75c35eb2d6a6702e4b60c3a39
    > Bug: 1169032, 1183320
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2728955
    > Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#860659}
    
    Bug: 1169032
    Bug: 1183320
    Change-Id: I63d9d9388a86bdfba39e4ff21a53377aea5634a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747680
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861776}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -3105,10 +3105,6 @@ void WebViewImpl::SetInsidePortal(bool inside_portal) {
     web_widget_->SetIsNestedMainFrameWidget(inside_portal);
 }
 
-void WebViewImpl::ActivatePrerender() {
-  GetPage()->ActivateForPrerendering();
-}
-
 void WebViewImpl::RegisterRendererPreferenceWatcher(
     CrossVariantMojoRemote<mojom::RendererPreferenceWatcherInterfaceBase>
         watcher) {
