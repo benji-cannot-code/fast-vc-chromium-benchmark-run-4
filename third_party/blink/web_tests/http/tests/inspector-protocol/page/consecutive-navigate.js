0c4d8384ee436cd4d2012d62d604a563bd563524
Adithya Srinivasan <adithyas@chromium.org>
2021-03-11 18:45:04 +0000

    DevTools: Use navigation_token instead of frame_token in PageHandler
    We currently index pending DevTools initiated navigations with the
    frame_token, but it makes more sense semantically to index it by
    navigation_token which is unique per navigation.
    
    Change-Id: I0c569894e488c7097e2bf2c78cdcbe1c28115bad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2739235
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862060}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/consecutive-navigate.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank(
+      `Tests that calling Page.navigate while a navigation is running cancels the previous navigation`);
+
+  await dp.Page.enable();
+  const navigatePromise1 = dp.Page.navigate({ url: testRunner.url('../resources/test-page.html') });
+  const navigatePromise2 = dp.Page.navigate({ url: testRunner.url('../resources/image.html') });
+
+  testRunner.log(await navigatePromise1);
+  testRunner.log(await navigatePromise2);
+
+  await dp.Fetch.enable();
+  const navigatePromise3 = dp.Page.navigate({ url: testRunner.url('../resources/test-page.html')});
+  await dp.Fetch.onceRequestPaused();
+  const navigatePromise4 = dp.Page.navigate({ url: testRunner.url('../resources/final.html')});
+  await dp.Fetch.disable();
+
+  testRunner.log(await navigatePromise3);
+  testRunner.log(await navigatePromise4);
+
+  testRunner.completeTest();
+})
