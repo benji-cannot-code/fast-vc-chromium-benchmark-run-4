91e1885b226515a445d699778095f9d71914df17
Joe Mason <joenotcharles@google.com>
2021-03-11 23:11:23 +0000

    [PM] Move V8DetailedMemoryDecorator out of the public header
    Also move the *AnySeq classes into their own header. A followup will
    move the classes into separate implementation files matching the header
    files.
    
    R=chrisha
    
    Bug: 1168558
    Change-Id: If6e6514d5306c93121934927c39fe5dfc0d637ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747156
    Auto-Submit: Joe Mason <joenotcharles@chromium.org>
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862179}

--- components/performance_manager/v8_memory/v8_detailed_memory_unittest.cc
+++ components/performance_manager/v8_memory/v8_detailed_memory_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/performance_manager/public/v8_memory/v8_detailed_memory.h"
+#include "components/performance_manager/public/v8_memory/v8_detailed_memory_any_seq.h"
 
 #include <memory>
 #include <tuple>
--- components/performance_manager/v8_memory/v8_memory_test_helpers.h
+++ components/performance_manager/v8_memory/v8_memory_test_helpers.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_PERFORMANCE_MANAGER_V8_MEMORY_V8_MEMORY_TEST_HELPERS_H_
 
 #include "components/performance_manager/public/v8_memory/v8_detailed_memory.h"
+#include "components/performance_manager/v8_memory/v8_detailed_memory_decorator.h"
 
 #include <string>
 #include <vector>
