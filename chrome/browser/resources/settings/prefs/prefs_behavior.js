5185683995091d7b9520572919fc25576f5f4feb
dpapad <dpapad@chromium.org>
2021-03-11 22:40:43 +0000

    Settings: Check-in remaining Polymer3 files under prefs/ part1.
    Bug: 1184052
    Change-Id: I7d1d29dfbcb5dc86903a7fbd6f0c2304a1d4c492
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2746216
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862164}

--- chrome/browser/resources/settings/prefs/prefs_behavior.js
+++ chrome/browser/resources/settings/prefs/prefs_behavior.js
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 // clang-format off
-// #import {assert} from 'chrome://resources/js/assert.m.js';
+import {assert} from 'chrome://resources/js/assert.m.js';
 // clang-format on
 
 /** @polymerBehavior */
-/* #export */ const PrefsBehavior = {
+export const PrefsBehavior = {
   properties: {
     /** Preferences state. */
     prefs: {
