d3b0e826df98503e2f017e8a2d27d045b6d7ab91
Manas Verma <manasverma@google.com>
2021-03-11 22:59:33 +0000

    Move authenticator_traits.h to contents/browser/public
    Bug: 1094445
    Change-Id: Ib472d67361efa7b746ff9244da03bf4b036fbf60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704644
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Manas Verma <manasverma@google.com>
    Cr-Commit-Position: refs/heads/master@{#862175}

--- chrome/browser/autofill/android/internal_authenticator_android.cc
+++ chrome/browser/autofill/android/internal_authenticator_android.cc
@@ -15,10 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "chrome/android/chrome_jni_headers/AuthenticatorImpl_jni.h"
 #include "components/payments/content/android/byte_buffer_helper.h"
-#include "content/browser/webauth/authenticator_mojom_traits.h"
 #include "content/public/browser/web_contents.h"
 #include "mojo/public/cpp/base/time_mojom_traits.h"
 #include "mojo/public/mojom/base/time.mojom.h"
+#include "third_party/blink/public/mojom/authenticator_mojom_traits.h"
 #include "third_party/blink/public/mojom/webauthn/authenticator.mojom-blink.h"
 
 using base::android::AttachCurrentThread;
