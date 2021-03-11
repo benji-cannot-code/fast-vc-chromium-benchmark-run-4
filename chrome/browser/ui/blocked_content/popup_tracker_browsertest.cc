99c24084d29c80723a2f4860085ed08bf63e10bf
Roman Sorokin <rsorokin@chromium.org>
2021-03-11 12:34:53 +0000

    [Sheriff] Skip *PopupRedirectsTwice_RedirectCountTwo* test
    On Linux Wayland configuration.
    
    Bug: 1179235
    Change-Id: Ife6c078203fc090eb6dc5b3de3eec829248c2c63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2752294
    Owners-Override: Roman Sorokin [CET] <rsorokin@chromium.org>
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861946}

--- chrome/browser/ui/blocked_content/popup_tracker_browsertest.cc
+++ chrome/browser/ui/blocked_content/popup_tracker_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/command_line.h"
 #include "base/path_service.h"
 #include "base/supports_user_data.h"
 #include "base/test/metrics/histogram_tester.h"
@@ -40,6 +41,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/keycodes/dom/dom_key.h"
 #include "ui/events/keycodes/keyboard_codes.h"
+
+#if defined(OS_LINUX)
+#include "ui/ozone/public/ozone_switches.h"
+#endif
 
 namespace {
 const char kPopupFirstDocumentEngagement[] =
@@ -624,6 +629,18 @@ IN_PROC_BROWSER_TEST_F(PopupTrackerBrowserTest,
 #endif
 IN_PROC_BROWSER_TEST_F(PopupTrackerBrowserTest,
                        MAYBE_PopupRedirectsTwice_RedirectCountTwo) {
+#if defined(OS_LINUX)
+  {
+    auto* command_line = base::CommandLine::ForCurrentProcess();
+    if (command_line->HasSwitch(switches::kOzonePlatform) &&
+        command_line->GetSwitchValueASCII(switches::kOzonePlatform) ==
+            "wayland") {
+      // TODO(crbug.com/1179235): Test is flaky on Linux Wayland configuration.
+      GTEST_SKIP() << "Flaky on Linux Wayland";
+    }
+  }
+#endif
+
   const GURL first_url = embedded_test_server()->GetURL("/title1.html");
   ui_test_utils::NavigateToURL(browser(), first_url);
 
