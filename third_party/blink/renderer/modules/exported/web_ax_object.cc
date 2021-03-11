3ef725cc0d256d94b761c4146423aa478425becc
Aaron Leventhal <aleventhal@google.com>
2021-03-11 19:44:18 +0000

    Speculative fix for new PerformAction() crash
    Bug: 1186943
    Change-Id: Ie064ca2f4d4305d78800b263bb346459b8daf7dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2753268
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862086}

--- third_party/blink/renderer/modules/exported/web_ax_object.cc
+++ third_party/blink/renderer/modules/exported/web_ax_object.cc
@@ -531,6 +531,9 @@ WebAXObject WebAXObject::HitTest(const gfx::Point& point) const {
     return WebAXObject();
   }
 
+  if (IsDetached())
+    return WebAXObject();  // Updating lifecycle could detach object.
+
   AXObject* hit = private_->AccessibilityHitTest(contents_point);
 
   if (hit)
@@ -593,6 +596,9 @@ bool WebAXObject::PerformAction(const ui::AXActionData& action_data) const {
 
   document->View()->UpdateAllLifecyclePhasesExceptPaint(
       DocumentUpdateReason::kAccessibility);
+
+  if (IsDetached())
+    return false;  // Updating lifecycle could detach object.
 
   ScopedActionAnnotator annotater(private_.Get());
   return private_->PerformAction(action_data);
