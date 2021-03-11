f457e9424166234cf1a79f93353db8513a097982
Anqing Zhao <anqing@google.com>
2021-03-11 12:11:20 +0000

    Set correct application locale for MGS (Managed Guest Session)
    Currently, the application locale of MGS is decided by login session. If
    user changes it to another language before login. It doesn't change
    accordingly.
    
    We need to check and set the locale before all applications are
    initialized. Otherwise they may apply different language preference
    (like Google search website).
    
    Bug: 1055979, 1102112
    Change-Id: Iecf8393860e39edd7240e25134fb17c1c139613d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2470083
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#861942}

--- chrome/browser/ash/login/screens/locale_switch_screen.cc
+++ chrome/browser/ash/login/screens/locale_switch_screen.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/webui/chromeos/login/locale_switch_screen_handler.h"
-#include "chrome/common/pref_names.h"
 #include "components/language/core/browser/pref_names.h"
 #include "components/language/core/common/locale_util.h"
 #include "components/prefs/pref_service.h"
@@ -68,12 +67,8 @@ bool LocaleSwitchScreen::MaybeSkip(WizardContext* wizard_context) {
     return false;
   }
 
-  // Only switch language if logging into a public account with the Terms of
-  // Service have been specified through policy. Otherwise advance to the
-  // session immediately.
-  if (user_manager::UserManager::Get()->IsLoggedInAsPublicAccount() &&
-      ProfileManager::GetActiveUserProfile()->GetPrefs()->IsManagedPreference(
-          prefs::kTermsOfServiceURL)) {
+  // Switch language if logging into a public account.
+  if (user_manager::UserManager::Get()->IsLoggedInAsPublicAccount()) {
     return false;
   }
 
