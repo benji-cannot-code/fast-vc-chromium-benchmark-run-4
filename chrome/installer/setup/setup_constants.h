e735fddd83ffd077568cf38be59059cfc52005be
Yann Dago <ydago@chromium.org>
2021-03-12 17:06:30 +0000

    Installer: Framework to handle downgrades after a breaking change
    Write the latest breaking installer version in the registry after every
    install. Perform a post-downgrade cleanup using the most recent
    installer. The cleanup is done in the finalizing stages of a regular
    update or when finalizing an in-use update after renaming the
    executables.
    
    Bug: 1121914
    Change-Id: Icca60b01d2d3859742b7458dd29a105d9ad63aa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2672922
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862446}

--- chrome/installer/setup/setup_constants.h
+++ chrome/installer/setup/setup_constants.h
@@ -43,6 +43,9 @@ extern const char kSetDisplayVersionProduct[];
 extern const char kSetDisplayVersionValue[];
 extern const char kUserExperiment[];
 
+extern const char kCleanupForDowngradeOperation[];
+extern const char kCleanupForDowngradeVersion[];
+
 }  // namespace switches
 
 }  // namespace installer
