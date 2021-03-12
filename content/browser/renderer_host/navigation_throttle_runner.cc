ba1bcac02f1fa855bb6bec13bc0bbee72a5aefc4
Takashi Toyoshima <toyoshim@chromium.org>
2021-03-12 13:15:55 +0000

    Prerender: Defer cross-origin subframe loading
    This patch adds PrerenderingSubframeNavigationThrottole
    that monitors subframe navigation and defer cross-origin
    requests or redirects during the prerendering phase.
    
    This patch also modifies document.cc so that it ignores
    activation event if the document was created for the
    initial about:blank. This will happen on loading iframes.
    This change may fix the flakiness issue of 1185965.
    
    Change-Id: I870df69799b5a1215e061721b89179916147708b
    Bug: 1186209, 1185965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2744794
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862388}

--- content/browser/renderer_host/navigation_throttle_runner.cc
+++ content/browser/renderer_host/navigation_throttle_runner.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/devtools/devtools_instrumentation.h"
 #include "content/browser/portal/portal_navigation_throttle.h"
 #include "content/browser/prerender/prerender_navigation_throttle.h"
+#include "content/browser/prerender/prerender_subframe_navigation_throttle.h"
 #include "content/browser/renderer_host/ancestor_throttle.h"
 #include "content/browser/renderer_host/back_forward_cache_throttle.h"
 #include "content/browser/renderer_host/blocked_scheme_navigation_throttle.h"
@@ -127,6 +128,10 @@ void NavigationThrottleRunner::RegisterNavigationThrottles() {
 
   // Block certain requests that are not permitted for prerendering.
   AddThrottle(PrerenderNavigationThrottle::MaybeCreateThrottleFor(request));
+
+  // Defer cross-origin subframe loading during prerendering state.
+  AddThrottle(
+      PrerenderSubframeNavigationThrottle::MaybeCreateThrottleFor(request));
 
   // Intercept federated identity requests.
   AddThrottle(FederatedAuthNavigationThrottle::MaybeCreateThrottleFor(request));
