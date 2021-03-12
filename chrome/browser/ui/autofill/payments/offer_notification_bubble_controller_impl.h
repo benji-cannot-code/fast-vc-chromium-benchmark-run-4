a94472505a8d52c35b1f74f6ec3c069e0676ec13
Tim Schumann <tschumann@chromium.org>
2021-03-12 12:58:13 +0000

    Revert "[Offer notification] Fix bubble being rendered on the wrong content"
    This reverts commit a6173e357e6fac1b32aed9831a02121d0041906c.
    
    Reason for revert: Appears to have broken some tests on Mac:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.14%20Tests/13986/overview
    
    Original change's description:
    > [Offer notification] Fix bubble being rendered on the wrong content
    >
    > Fix the issue that bubble is rendered even if the page is not being
    > active (opened via right click, Open link in new tab).
    >
    > The issue was because we didn't check if the controller's web_content
    > is the active web content.
    >
    > Added a new check. With the fix, when open a new tab and if the website
    > is also the merchant's, do not trigger the bubble on the original tab
    > (and on the new tab), but only update the offer icon in the new tab.
    >
    > Manually tested in local build. Screenrecord in bug comment#30
    >
    > [NOTE] there is one missing part after the fix, which will be
    > followed-up in M91. Refer to crbug.com/1187190
    >
    > Bug: 1093057
    > Change-Id: I14de069677dfb6fcc1fb632291a75b59d07b6d5f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2744159
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Reviewed-by: Jared Saul <jsaul@google.com>
    > Commit-Queue: Siyu An <siyua@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#862141}
    
    Bug: 1093057
    Change-Id: I119e8c8121f8f05f99d33b75f6feae7102b1fede
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2756053
    Auto-Submit: Tim Schumann <tschumann@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#862386}

--- chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.h
+++ chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.h
@@ -71,8 +71,8 @@ class OfferNotificationBubbleControllerImpl
       OfferNotificationBubbleControllerImpl>;
   friend class OfferNotificationBubbleViewsTestBase;
 
-  // Returns whether the web content associated with this controller is active.
-  bool IsWebContentsActive();
+  // Updates the visibility of the icon as per IsIconVisible().
+  void UpdateOfferIcon();
 
   // For testing.
   void SetEventObserverForTesting(ObserverForTest* observer) {
