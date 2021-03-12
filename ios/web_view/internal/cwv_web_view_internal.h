79d75f02c4a9db8528858b073630d37567cf8c6d
John Wu <jzw@chromium.org>
2021-03-12 02:57:10 +0000

    Allow customization of input accessory view
    - test that it only overrides if non nil
    - move some protocol conformance to internal header
    
    Bug: 1115587
    Change-Id: Ice94de30315587f81a4b18aed04f46b978a09901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2751586
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862271}

--- ios/web_view/internal/cwv_web_view_internal.h
+++ ios/web_view/internal/cwv_web_view_internal.h
@@ -8,13 +8,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web_view/public/cwv_web_view.h"
 
+#import "ios/web/common/crw_input_view_provider.h"
+#import "ios/web/public/web_state_delegate_bridge.h"
+#import "ios/web/public/web_state_observer_bridge.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
 namespace web {
 class WebState;
 }
 
-@interface CWVWebView ()
+@interface CWVWebView () <CRWResponderInputView,
+                          CRWWebStateDelegate,
+                          CRWWebStateObserver>
 
 // Returns CWVWebView which corresponds to the given web state.
 // It causes an assertion failure if the web state has no corresponding
