f20f5c349b3a859943e9a28c2584d5f889ada1f4
Katie Dektar <katie@chromium.org>
2021-03-12 02:59:56 +0000

    Send AudioFetcherSource's errors to SpeechRecognitionRecognizerClient
    AX-Relnotes: N/A
    Bug: 1173135
    Test: Ran SpeechRecognitionServiceTest locally
    Change-Id: I46392416c99c35afda1776fecc94e4a1fe6a8404
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2752965
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#862273}

--- chrome/services/speech/speech_recognition_recognizer_impl.cc
+++ chrome/services/speech/speech_recognition_recognizer_impl.cc
@@ -120,7 +120,7 @@ SpeechRecognitionRecognizerImpl::SpeechRecognitionRecognizerImpl(
     DCHECK(base::PathExists(binary_path));
     soda_client_ = std::make_unique<::soda::SodaClient>(binary_path);
     if (!soda_client_->BinaryLoadedSuccessfully()) {
-      client_remote_->OnSpeechRecognitionError();
+      OnSpeechRecognitionError();
     }
   } else {
     cloud_client_ = std::make_unique<CloudSpeechRecognitionClient>(
@@ -169,6 +169,10 @@ void SpeechRecognitionRecognizerImpl::SendAudioToSpeechRecognitionService(
 
   // OK, everything is verified, let's send the audio.
   SendAudioToSpeechRecognitionServiceInternal(std::move(buffer));
+}
+
+void SpeechRecognitionRecognizerImpl::OnSpeechRecognitionError() {
+  client_remote_->OnSpeechRecognitionError();
 }
 
 void SpeechRecognitionRecognizerImpl::
