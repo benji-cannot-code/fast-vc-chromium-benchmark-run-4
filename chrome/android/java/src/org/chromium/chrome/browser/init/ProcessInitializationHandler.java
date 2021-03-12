6d3f79896858a916863624027bd398609b7a42ed
Eric Seckler <eseckler@chromium.org>
2021-03-12 16:31:19 +0000

    PowerMonitor: Initialize earlier on Android
    Currently, the PowerMonitor's Java side is initialized during deferred
    startup. Because the PowerMonitor starts out in "charging" state, this
    results in O(30 seconds) after startup during which it indicates
    "charging" and the true state isn't known.
    
    For the PowerScheduler [1], this means it will not apply any power
    optimizations during this time, because it (incorrectly) believes the
    device to be charging.
    
    This patch moves the initialization of the Java side into an earlier
    startup phase, namely after the native library is initialized.
    
    [1] http://go/chromium-process-power-scheduler
    
    Bug: 1166695
    Change-Id: Ie3f07a29a5ea5cb81b87478ba900d03686090938
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2756216
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862429}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -20,7 +20,6 @@ import androidx.annotation.WorkerThread;
 import org.chromium.base.CommandLine;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
-import org.chromium.base.PowerMonitor;
 import org.chromium.base.SysUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.TraceEvent;
@@ -307,8 +306,6 @@ public class ProcessInitializationHandler {
                                         UrlUtilities.isNTPUrl(homepageUrl), homepageUrl);
                             }
                         });
-
-                PowerMonitor.create();
 
                 ShareImageFileUtils.clearSharedImages();
 
