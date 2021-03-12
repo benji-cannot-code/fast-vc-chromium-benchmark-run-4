5d7661d53724e55ed2c8e364c81311606202975b
Dave Tapuska <dtapuska@chromium.org>
2021-03-12 18:04:42 +0000

    Remove renderer frame id for calculating variation headers.
    Pass the top frame origin in to WebURLRequestExtraData so we don't
    need to pass the render_frame_id along.
    
    BUG=1173710
    
    Change-Id: I22ee55931d8ae73b04ab6870309d2c8a762e4e9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747118
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862458}

--- third_party/blink/public/platform/web_url_request_extra_data.h
+++ third_party/blink/public/platform/web_url_request_extra_data.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/platform/web_vector.h"
 #include "ui/base/page_transition_types.h"
+#include "url/origin.h"
 
 namespace network {
 struct ResourceRequest;
@@ -38,6 +39,14 @@ class BLINK_PLATFORM_EXPORT WebURLRequestExtraData
   void set_transition_type(ui::PageTransition transition_type) {
     transition_type_ = transition_type;
   }
+
+  // Set the top origin. Only applicable for frames.
+  void set_top_frame_origin(const url::Origin& origin) {
+    top_frame_origin_ = origin;
+  }
+
+  // The origin of the topmost frame. Only applicable for frames.
+  const url::Origin& top_frame_origin() { return top_frame_origin_; }
 
   // The request is for a prefetch-only client (i.e. running NoStatePrefetch)
   // and should use LOAD_PREFETCH network flags.
@@ -98,6 +107,9 @@ class BLINK_PLATFORM_EXPORT WebURLRequestExtraData
   WebVector<std::unique_ptr<URLLoaderThrottle>> url_loader_throttles_;
   scoped_refptr<WebFrameRequestBlocker> frame_request_blocker_;
   bool allow_cross_origin_auth_prompt_ = false;
+
+  // The origin of the top most frame. Only applicable for frames.
+  url::Origin top_frame_origin_;
 };
 
 }  // namespace blink
