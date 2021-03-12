db2ec445e33cdc342c3e2c7edae72c96b263980d
Tal Pressman <talp@chromium.org>
2021-03-12 07:41:41 +0000

    Create WTF::SequenceBound to specialize the one in base/.
    Blink requires some care when binding objects across threads, that can
    cause some issues when attempting to use base::SequenceBound there. This
    CL introduces a specialization of SequenceBound in WTF, that
    uses Blink's stricter primitives for binding objects and posting tasks.
    
    Bug: 1182140
    Change-Id: Icc8eeb4c88cf0cb93b0627593b1ef9c90d1b76d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732274
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Tal Pressman <talp@google.com>
    Cr-Commit-Position: refs/heads/master@{#862332}

---
+++ third_party/blink/renderer/platform/wtf/sequence_bound_test.cc
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/platform/wtf/sequence_bound.h"
+
+#include "base/single_thread_task_runner.h"
+#include "base/task/post_task.h"
+#include "base/task/thread_pool.h"
+#include "base/test/task_environment.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace WTF {
+namespace {
+struct Foo {
+  Foo() = default;
+  void Bar(int) {}
+  int Baz() { return 42; }
+};
+}  // namespace
+
+class SequenceBoundTest : public testing::Test {
+ protected:
+  base::test::TaskEnvironment task_environment_;
+};
+
+TEST_F(SequenceBoundTest, CanInstantiate) {
+  SequenceBound<Foo> sequence_bound(
+      base::CreateSingleThreadTaskRunner({base::ThreadPool()}));
+
+  sequence_bound.AsyncCall(&Foo::Bar).WithArgs(5);
+  sequence_bound.PostTaskWithThisObject(FROM_HERE,
+                                        CrossThreadBindOnce([](Foo* foo) {}));
+}
+
+}  // namespace WTF
