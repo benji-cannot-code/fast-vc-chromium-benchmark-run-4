81880c73d4785210d669bffdcd433db19ce9c8ec
Tom Anderson <thomasanderson@chromium.org>
2021-03-12 20:42:38 +0000

    Fix uninitialized x11::Input::XIEventMask variables for XISelectEvents()
    R=lambroslambrou
    
    Change-Id: I462f1385307a8425e2b789fbb4b56a11a50ea61a
    Bug: 1186861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757287
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862509}

--- ui/base/x/x11_user_input_monitor.cc
+++ ui/base/x/x11_user_input_monitor.cc
@@ -80,7 +80,7 @@ void XUserInputMonitor::StartMonitor(WriteKeyPressCallback& callback) {
   connection_->xinput().XIQueryVersion(
       {x11::Input::major_version, x11::Input::minor_version});
 
-  x11::Input::XIEventMask mask;
+  x11::Input::XIEventMask mask{};
   SetXinputMask(&mask, x11::Input::RawDeviceEvent::RawKeyPress);
   SetXinputMask(&mask, x11::Input::RawDeviceEvent::RawKeyRelease);
   connection_->xinput().XISelectEvents(
