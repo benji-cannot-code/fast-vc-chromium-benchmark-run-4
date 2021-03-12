d3df7770c7ce5b4cee78f8d14a203b584f3cfaca
Xianzhu Wang <wangxianzhu@chromium.org>
2021-03-12 06:03:33 +0000

    Use the common ancestor property tree state in overlap testing
    Previously we mapped visual rects of pending layers to
    PropertyTreeState::Root() and checked their intersection in
    overlap testing. If two overlapping layers had a common ancestor clip
    making one or both empty in the root state, we treated them as not
    overlapping. This required us to always do PaintArtifactCompositor
    update on scroll.
    
    Now use the common ancestor property tree state instead, to avoid
    always PaintArtifactCompositor update on scroll. We still need the
    update when there are viewport constrained objects or a non-stacking-
    context scroller contains stacked descendants. The logic is moved into
    PaintPropertyTreeBuilder because we need the PaintLayer's descendant
    dependent flags to be ready.
    
    Bug: 1182892
    Change-Id: Ia9b40c4a1b63227c9b69bc3c93ee7d405577d3a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2747178
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862319}

--- third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h
+++ third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h
@@ -306,7 +306,8 @@ class PLATFORM_EXPORT PaintArtifactCompositor final
 
     std::unique_ptr<JSONObject> ToJSON() const;
 
-    FloatRect VisualRectForOverlapTesting() const;
+    FloatRect VisualRectForOverlapTesting(
+        const PropertyTreeState& ancestor_state) const;
 
     bool MayDrawContent() const;
 
