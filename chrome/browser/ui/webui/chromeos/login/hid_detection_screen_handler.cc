99516bc4f0b01a0b247622dfb803d2323797bdcf
Ossama Mahmoud <osamafathy@google.com>
2021-03-12 14:25:39 +0000

    OOBE: Add test API for HID Detection Screen
    Add testing utils for tast tests to emulate the connection of devices
    in the HID Detection Screen which will allow to test users behaviors
    on the screen instead of skipping it.
    
    Bug: 1179226
    Change-Id: If70427d8a265475c98ffec2b622cb4c497fa982c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2752158
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Ossama Mahmoud <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/master@{#862399}

--- chrome/browser/ui/webui/chromeos/login/hid_detection_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/hid_detection_screen_handler.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "components/login/localized_values_builder.h"
 #include "components/prefs/pref_service.h"
+#include "services/device/public/mojom/input_service.mojom.h"
 
 namespace chromeos {
 
@@ -145,6 +146,28 @@ void HIDDetectionScreenHandler::DeclareLocalizedValues(
   builder->Add("oobeModalDialogClose", IDS_CHROMEOS_OOBE_CLOSE_DIALOG);
   builder->Add("hidDetectionTouchscreenDetected",
                IDS_HID_DETECTION_DETECTED_TOUCHSCREEN);
+}
+
+void HIDDetectionScreenHandler::DeclareJSCallbacks() {
+  AddCallback(
+      "HIDDetectionScreen.emulateDevicesConnectedForTesting",
+      &HIDDetectionScreenHandler::HandleEmulateDevicesConnectedForTesting);
+}
+
+void HIDDetectionScreenHandler::HandleEmulateDevicesConnectedForTesting() {
+  auto mouse = device::mojom::InputDeviceInfo::New();
+  mouse->id = "fake_mouse";
+  mouse->subsystem = device::mojom::InputDeviceSubsystem::SUBSYSTEM_INPUT;
+  mouse->type = device::mojom::InputDeviceType::TYPE_USB;
+  mouse->is_mouse = true;
+  screen_->InputDeviceAddedForTesting(std::move(mouse));
+
+  auto keyboard = device::mojom::InputDeviceInfo::New();
+  keyboard->id = "fake_keyboard";
+  keyboard->subsystem = device::mojom::InputDeviceSubsystem::SUBSYSTEM_INPUT;
+  keyboard->type = device::mojom::InputDeviceType::TYPE_USB;
+  keyboard->is_keyboard = true;
+  screen_->InputDeviceAddedForTesting(std::move(keyboard));
 }
 
 void HIDDetectionScreenHandler::Initialize() {
