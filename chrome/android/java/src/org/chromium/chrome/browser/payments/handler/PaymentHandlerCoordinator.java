96bb96a5223ee830b2f8565f507827366293a820
Liquan (Max) Gu <maxlg@chromium.org>
2021-03-12 21:30:35 +0000

    Prevent WebContents leak in PaymentHandler
    Motivation:
    In PaymentHandlerCoordinator, when
    BottomSheetController#requestShowContent() failed, the coordinator quits
    without cleaning up the PaymentHandlerView that's saved in
    BottomSheetController. Now that the view retains the
    a WebContents, this causes the leak of WebContents. In the case of
    BottomSheet being suppressed, payment request service should be closed
    immediately instead of waiting until BottomSheet's being unsuppressed.
    This is to keep the PaymentHandler API simple.
    
    Bug: 1176138
    Change-Id: I264f61e357735f19141cbbd98aea259c1214f9ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2748820
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862536}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/handler/PaymentHandlerCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/handler/PaymentHandlerCoordinator.java
@@ -115,7 +115,12 @@ public class PaymentHandlerCoordinator {
             mPaymentHandlerWebContents.destroy();
         };
         boolean isShowSuccess = bottomSheetController.requestShowContent(view, /*animate=*/true);
-        return isShowSuccess ? mPaymentHandlerWebContents : null;
+        if (!isShowSuccess) {
+            mHider.run();
+            mHider = null;
+            return null;
+        }
+        return mPaymentHandlerWebContents;
     }
 
     private void initializeWebContents(
