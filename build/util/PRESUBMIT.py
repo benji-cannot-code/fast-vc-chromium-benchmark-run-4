7de2bd883592ddf9150456f6de41f67b3dd99e0c
Nate Fischer <ntfschr@chromium.org>
2021-03-12 05:49:34 +0000

    Code inclusion: update terms in build/{toolchain,util}
    No change to logic. This replaces "whitelist" and "blacklist" with more
    appropriate terminology in build/toolchain/ and build/util/.
    
    Test: N/A
    Change-Id: I7930d915eab19342685953b9ac035bf023a0861d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2745868
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862318}

--- build/util/PRESUBMIT.py
+++ build/util/PRESUBMIT.py
@@ -7,7 +7,7 @@ import re
 """Presubmit for build/util"""
 
 
-def _GetBlacklist(input_api):
+def _GetFilesToSkip(input_api):
   files_to_skip = []
   affected_files = input_api.change.AffectedFiles()
   version_script_change = next(
@@ -31,7 +31,7 @@ def _GetBlacklist(input_api):
 
 def _GetPythonUnitTests(input_api, output_api):
   # No need to test if files are unchanged
-  files_to_skip = _GetBlacklist(input_api)
+  files_to_skip = _GetFilesToSkip(input_api)
 
   return input_api.canned_checks.GetUnitTestsRecursively(
       input_api,
