b15857d70c285e4c39ccc6c52ce17a0c8039aa76
Nicolas Dubus <nicodubus@fb.com>
2021-03-12 18:26:46 +0000

    [blink] Fire samplebufferfull event on the Profiler object.
     - Fire samplebufferfull event on the Profiler object
    when buffer fills up
     - Server (v8) side CR:
    https://chromium-review.googlesource.com/c/v8/v8/+/2652206
     - Includes web performance tests, verifying delegate function
    invoked
    
    Bug: 956688
    Change-Id: I7685c95d04fd68ef661011f4b0e7cf079e7dd2b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2649617
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Nicolas Dubus <nicodubus@fb.com>
    Cr-Commit-Position: refs/heads/master@{#862465}

--- third_party/blink/renderer/core/timing/profiler.cc
+++ third_party/blink/renderer/core/timing/profiler.cc
@@ -29,6 +29,14 @@ void Profiler::DisposeAsync() {
   }
 }
 
+const AtomicString& Profiler::InterfaceName() const {
+  return event_target_names::kProfiler;
+}
+
+ExecutionContext* Profiler::GetExecutionContext() const {
+  return ExecutionContext::From(script_state_);
+}
+
 ScriptPromise Profiler::stop(ScriptState* script_state) {
   auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
   ScriptPromise promise = resolver->Promise();
