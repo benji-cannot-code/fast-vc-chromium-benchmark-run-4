fb355b5f8f665f560dafa3e18375d5a20fa53e9f
Elly Fong-Jones <ellyjones@chromium.org>
2021-03-12 21:46:27 +0000

    views: asynchronously drop ViewsDelegate ref during menu cancel
    Dropping the ViewsDelegate reference can cause browser teardown to
    start on the same stack, which in turn can cause synchronous window
    closes, synchronous view deletions, and even synchronous deletion
    of the menu's delegate. This creates an untenable lifecycle situation
    in which parts of the object graph might be deallocated any time
    after dropping the last ref to the ViewsDelegate. There are some
    defensive checks against this in MenuController, but they are not
    sufficient (see e.g. the linked bug) and can't be made sufficient.
    
    Instead of that, post a task to drop the last reference to the
    ViewsDelegate, ensuring that the entire menu cancellation can
    complete before browser teardown might begin. That allows
    removing some of the defensive code.
    
    Bug: 1183440
    Change-Id: Ia8f950ddc41400dfe11656d583a4cdc16610c922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2756977
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862544}

--- ui/views/controls/menu/menu_runner_unittest.cc
+++ ui/views/controls/menu/menu_runner_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/bind.h"
 #include "base/test/simple_test_tick_clock.h"
 #include "build/build_config.h"
 #include "ui/base/ui_base_types.h"
@@ -577,9 +578,12 @@ TEST_F(MenuRunnerDestructionTest, MenuRunnerDestroyedDuringReleaseRef) {
   menu_runner->RunMenuAt(owner(), nullptr, gfx::Rect(),
                          MenuAnchorPosition::kTopLeft, 0);
 
-  test_views_delegate()->set_release_ref_callback(base::BindRepeating(
-      [](internal::MenuRunnerImpl* menu_runner) { menu_runner->Release(); },
-      base::Unretained(menu_runner)));
+  base::RunLoop run_loop;
+  test_views_delegate()->set_release_ref_callback(
+      base::BindLambdaForTesting([&]() {
+        run_loop.Quit();
+        menu_runner->Release();
+      }));
 
   base::WeakPtr<internal::MenuRunnerImpl> ref(MenuRunnerAsWeakPtr(menu_runner));
   MenuControllerTestApi menu_controller;
@@ -587,8 +591,9 @@ TEST_F(MenuRunnerDestructionTest, MenuRunnerDestroyedDuringReleaseRef) {
   // |menu_runner| simulating device shutdown.
   menu_controller.controller()->Cancel(MenuController::ExitType::kAll);
   // Both the |menu_runner| and |menu_controller| should have been deleted.
-  EXPECT_EQ(nullptr, ref);
   EXPECT_EQ(nullptr, menu_controller.controller());
+  run_loop.Run();
+  EXPECT_EQ(nullptr, ref);
 }
 
 TEST_F(MenuRunnerImplTest, FocusOnMenuClose) {
