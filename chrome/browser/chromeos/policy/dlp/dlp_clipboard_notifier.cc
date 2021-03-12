fe22d474b846427b706e97856f4989ecbc95a85a
Aya ElAttar <ayaelattar@chromium.org>
2021-03-12 09:00:17 +0000

    Increase unittests coverage for DlpClipboardNotifier
    Bug: 1187166
    Change-Id: I9e2a34b01a715e9e09c6152269175f52666a7cb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2752883
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862350}

--- chrome/browser/chromeos/policy/dlp/dlp_clipboard_notifier.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_clipboard_notifier.cc
@@ -156,7 +156,6 @@ void DlpClipboardNotifier::WarnOnPaste(
                     l10n_util::GetStringUTF16(IDS_POLICY_DLP_ANDROID_APPS)));
       return;
     }
-    DCHECK(!data_dst->IsUrlType());
   }
 
   auto proceed_cb =
@@ -207,6 +206,11 @@ bool DlpClipboardNotifier::DidUserApproveDst(
 bool DlpClipboardNotifier::DidUserCancelDst(
     const ui::DataTransferEndpoint* const data_dst) {
   return HasEndpoint(cancelled_dsts_, data_dst);
+}
+
+void DlpClipboardNotifier::SetBlinkPasteCallbackForTesting(
+    base::OnceCallback<void(bool)> paste_cb) {
+  blink_paste_cb_ = std::move(paste_cb);
 }
 
 void DlpClipboardNotifier::ProceedPressed(
@@ -267,6 +271,7 @@ void DlpClipboardNotifier::OnWidgetDestroyed(views::Widget* widget) {
 }
 
 void DlpClipboardNotifier::WebContentsDestroyed() {
+  std::move(blink_paste_cb_);
   CloseWidget(widget_.get(), views::Widget::ClosedReason::kUnspecified);
 }
 
