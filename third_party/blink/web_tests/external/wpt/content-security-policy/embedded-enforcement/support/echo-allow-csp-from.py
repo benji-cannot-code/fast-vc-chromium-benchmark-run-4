eec153b9f4133d11f5d5aa769349eb997d9874d7
Antonio Sartori <antoniosartori@chromium.org>
2021-03-12 14:57:50 +0000

    Remove trailing spaces from CSP WPTs
    Let's enforce no trailing spaces from now on for Content Security
    Policies WPTs.
    
    Change-Id: I2f870e0914135d5143e63cf0ced9f4f211d1418e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2756253
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862409}

--- third_party/blink/web_tests/external/wpt/content-security-policy/embedded-enforcement/support/echo-allow-csp-from.py
+++ third_party/blink/web_tests/external/wpt/content-security-policy/embedded-enforcement/support/echo-allow-csp-from.py
@@ -27,7 +27,7 @@ def main(request, response):
             background-color: maroon;
         }
     </style>
-    <script nonce="abc"> 
+    <script nonce="abc">
         var response = {};
         response["id"] = "%s";
         response["loaded"] = true;
