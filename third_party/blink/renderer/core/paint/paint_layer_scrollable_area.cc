39d295208a5d6f379163f38af1d06c8366023c96
Stefan Zager <szager@chromium.org>
2021-03-12 01:19:13 +0000

    Decomposite ScrollableArea in hidden LocalFrameView
    For other compositing reasons, we don't create composited layers in
    a hidden (offscreen) LocalFrameView; this applies the same logic to
    scrollers.  This hopefully fixes a long-standing bug where a hidden
    iframe will decomposite, but an iframe embedded within the iframe
    will still get composited layers for a ScrollableArea. When this
    happens, the composited layers are orphaned, because they can't be
    attached to an ancestor composited layer in the parent iframe.
    
    This CL also makes it so we uniformly use
    PaintLayerCompositor::InCompositingMode to check whether an iframe
    has composited layers. Previously, we checked
    PaintLayerCompositor::RootGraphicsLayer(), but that was a hack to
    avoid the fact that InCompositingMode() and RootGraphicsLayer() were
    not always consistent. That inconsistency was fixed in:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/2733389
    
    Finally, this moves CanBeComposited() from PaintLayerCompositor to
    PaintLayer, because it is based on PaintLayer properties.
    
    Bug: 1133662,1092673,1038959
    Change-Id: Ifc11a0eef322e872b941ffa7b0b4049e754fd232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2751335
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862234}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
@@ -2563,6 +2563,9 @@ bool PaintLayerScrollableArea::ComputeNeedsCompositedScrollingInternal(
   DCHECK_EQ(background_paint_location_if_composited,
             GetLayoutBox()->ComputeBackgroundPaintLocationIfComposited());
 
+  if (!Layer()->GetLayoutObject().GetFrameView()->IsVisible())
+    return false;
+
   if (CompositingReasonFinder::RequiresCompositingForRootScroller(*layer_))
     return true;
 
