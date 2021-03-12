cda8eb82843889a14cf5c73a2feef085578418fc
vidorteg <vidorteg@microsoft.com>
2021-03-12 17:15:02 +0000

    [Loc] Removing Localization V1 remnants. (ls calls on the backend)
    Cleaning more code from localization v1. In this case a missing ls
    usage in an a11y test
    
    Bug: 1136655
    Change-Id: I8563158b90ab11b11592025ae155e32db0b5993b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2755182
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Vidal Diazleal <vidorteg@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#862451}

--- third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/application-panel/clear-storage-a11y-test.js
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/application-panel/clear-storage-a11y-test.js
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await UI.viewManager.showView('resources');
 
   const parent = UI.panels.resources._sidebar._applicationTreeElement;
-  const clearStorageElement = parent.children().find(child => child.title === ls`Storage`);
+  const clearStorageElement = parent.children().find(child => child.title === 'Storage');
   clearStorageElement.select();
   const storageView = UI.panels.resources.visibleView;
   TestRunner.addResult('Storage view is visible: ' + ApplicationTestRunner.isStorageView(storageView));
