d331fa310188fad768cefd747666a9633bc7711d
Peter Kasting <pkasting@chromium.org>
2021-03-13 01:33:41 +0000

    Add a highlighted style toggle to the tabbed pane example.
    Bug: none
    Change-Id: I2629e2a2d130ebb557ea4fc564bb32a86036f98d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757170
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862628}

--- ui/views/examples/tabbed_pane_example.h
+++ ui/views/examples/tabbed_pane_example.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/examples/example_base.h"
 
 namespace views {
+class Button;
+
 namespace examples {
 
 // A TabbedPane example tests adding and selecting tabs.
@@ -33,15 +35,21 @@ class VIEWS_EXAMPLES_EXPORT TabbedPaneExample : public ExampleBase,
   // TabbedPaneListener:
   void TabSelectedAt(int index) override;
 
-  void CreateTabbedPane(View* container, TabbedPane::Orientation orientation);
+  void CreateTabbedPane(View* container,
+                        TabbedPane::Orientation orientation,
+                        TabbedPane::TabStripStyle style);
   void PrintCurrentStatus();
   void SwapLayout();
+  void ToggleHighlighted();
   void AddTab(const std::u16string& label);
   void AddAt();
   void SelectAt();
 
   // The tabbed pane to be tested.
   TabbedPane* tabbed_pane_;
+
+  // The button that toggles highlighted style.
+  Button* toggle_highlighted_;
 };
 
 }  // namespace examples
