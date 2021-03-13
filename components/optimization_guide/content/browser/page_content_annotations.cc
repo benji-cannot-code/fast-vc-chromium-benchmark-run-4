0e4104e957296c8271d6d1638b4ecb75ba093db3
Sophie Chang <sophiechang@chromium.org>
2021-03-13 01:32:31 +0000

    Postprocess output coming out of page topics model
    Bug: 1177102
    Change-Id: I22a2bba9ca87d8bef38b6ac725826267736fd143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2754129
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862626}

--- components/optimization_guide/content/browser/page_content_annotations.cc
+++ components/optimization_guide/content/browser/page_content_annotations.cc
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/content/browser/page_content_annotations.h"
 
 PageContentAnnotations::PageContentAnnotations(
-    const std::map<int, float>& categories,
-    float sensitivity)
-    : categories_(categories), sensitivity_(sensitivity) {}
+    const std::vector<std::pair<int, float>>& categories,
+    float floc_protected_score)
+    : categories_(categories), floc_protected_score_(floc_protected_score) {}
 
 PageContentAnnotations::~PageContentAnnotations() = default;
 PageContentAnnotations::PageContentAnnotations(
--- components/optimization_guide/content/browser/page_content_annotations_model_manager.h
+++ components/optimization_guide/content/browser/page_content_annotations_model_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/optimization_guide/content/browser/bert_model_executor.h"
 #include "components/optimization_guide/content/browser/page_content_annotations.h"
+#include "components/optimization_guide/proto/page_topics_model_metadata.pb.h"
 
 namespace optimization_guide {
 
@@ -42,7 +43,14 @@ class PageContentAnnotationsModelManager {
   // Invoked when the page topics model has finished executing.
   void OnPageTopicsModelExecutionCompleted(
       PageContentAnnotatedCallback callback,
+      const proto::PageTopicsModelMetadata& model_metadata,
       const base::Optional<std::vector<tflite::task::core::Category>>& output);
+
+  // Converts |model_output| into PageContentAnnotations based on
+  // |model_metadata|.
+  PageContentAnnotations GetPageContentAnnotationsFromModelOutput(
+      const proto::PageTopicsModelMetadata& model_metadata,
+      const std::vector<tflite::task::core::Category>& model_output) const;
 
   // The model executor responsible for executing the page topics model.
   std::unique_ptr<BertModelExecutor> page_topics_model_executor_;
