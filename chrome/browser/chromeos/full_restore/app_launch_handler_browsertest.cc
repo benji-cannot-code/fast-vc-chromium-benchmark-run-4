744fe7e39876df061fd2614869ab57bcb4d0ba61
Sammie Quon <sammiequon@chromium.org>
2021-03-13 02:20:17 +0000

    full_restore: Add create and remove desks to app launch test.
    This will allow us to do a more end-to-end testing with regards to
    desks. This was previously landed, but reverted for flakiness. The
    stack trace shows the remove desk animations sometimes hits an error.
    This CL manually removes the desks, which wait until the full desk
    animation is finished before removing the next desk. The wait can help
    with the viz error.
    
    Bug: 1184193
    Test: none
    Change-Id: I54a647bd04595fb5d7e086f308d1f3ce87d56649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2742876
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Auto-Submit: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862639}

--- chrome/browser/chromeos/full_restore/app_launch_handler_browsertest.cc
+++ chrome/browser/chromeos/full_restore/app_launch_handler_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/public/cpp/ash_features.h"
+#include "ash/public/cpp/autotest_desks_api.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/wm_event.h"
 #include "base/test/scoped_feature_list.h"
@@ -418,6 +419,11 @@ IN_PROC_BROWSER_TEST_F(AppLaunchHandlerBrowserTest, WindowProperties) {
 }
 
 IN_PROC_BROWSER_TEST_F(AppLaunchHandlerBrowserTest, RestoreChromeApp) {
+  // Have 4 desks total.
+  ash::AutotestDesksApi().CreateNewDesk();
+  ash::AutotestDesksApi().CreateNewDesk();
+  ash::AutotestDesksApi().CreateNewDesk();
+
   ::full_restore::SetActiveProfilePath(profile()->GetPath());
 
   // Create the restore data.
@@ -455,6 +461,7 @@ IN_PROC_BROWSER_TEST_F(AppLaunchHandlerBrowserTest, RestoreChromeApp) {
   int32_t* index = window->GetProperty(::full_restore::kActivationIndexKey);
   ASSERT_TRUE(index);
   EXPECT_EQ(kActivationIndex, *index);
+  EXPECT_EQ(kDeskId, window->GetProperty(aura::client::kWindowWorkspaceKey));
 
   // Windows created from full restore are not activated. They will become
   // activatable after a post task is run.
@@ -469,6 +476,14 @@ IN_PROC_BROWSER_TEST_F(AppLaunchHandlerBrowserTest, RestoreChromeApp) {
   // Close the window.
   CloseAppWindow(app_window);
   ASSERT_FALSE(::full_restore::GetWindowInfo(restore_window_id));
+
+  // Remove the added desks.
+  while (true) {
+    base::RunLoop run_loop;
+    if (!ash::AutotestDesksApi().RemoveActiveDesk(run_loop.QuitClosure()))
+      break;
+    run_loop.Run();
+  }
 }
 
 IN_PROC_BROWSER_TEST_F(AppLaunchHandlerBrowserTest,
