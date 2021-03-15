1e03567c9bec14d91acf1f65093ccbac0250b7e3
John L Chen <zuan@chromium.org>
2021-03-15 21:29:23 +0000

    initial commit of UserDataAuth
    UserDataAuth is the new DBus interface of cryptohomed. This change adds
    the DBus client that would be used to talk to cryptohomed through the
    new UserDataAuth DBus interface.
    
    Only one method is implemented to ensure everything goes smoothly. Other
    methods are following in later CLs.
    
    BUG=b:181787676
    TEST=Build OK && Unit test passes.
    
    Change-Id: Iaa8d6d5dd00c7d34fc88053eec06d24b4cf32bf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732808
    Commit-Queue: John L Chen <zuan@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862969}

---
+++ chromeos/dbus/userdataauth/fake_userdataauth_client.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/dbus/userdataauth/fake_userdataauth_client.h"
+
+#include "base/notreached.h"
+
+namespace chromeos {
+
+FakeUserDataAuthClient::FakeUserDataAuthClient() = default;
+
+FakeUserDataAuthClient::~FakeUserDataAuthClient() = default;
+
+void FakeUserDataAuthClient::IsMounted(
+    const ::user_data_auth::IsMountedRequest& request,
+    IsMountedCallback callback) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace chromeos
