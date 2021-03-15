8f9bf0e732b14a350c4713a5b5166be823a873cc
Sky Malice <skym@chromium.org>
2021-03-15 16:52:21 +0000

    Add BrowserContextHandleResolverFactory.
    Bug: 1087482
    Change-Id: I5b529e039693ebacfd4d2ce2e43aeaf444bf9b02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2661686
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862855}

---
+++ components/embedder_support/android/java/src/org/chromium/components/embedder_support/browser_context/PartitionResolverSupplier.java
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.embedder_support.browser_context;
+
+import org.chromium.base.supplier.OneshotSupplierImpl;
+
+/**
+ * Shared access point for components to fetch the singleton embedder specific implementation of
+ * {@link PartitionResolver}. Should be set sometime early in app lifecycle.
+ */
+public final class PartitionResolverSupplier {
+    // The impl has thread checks and asserts set it only called once. Do not allow raw access to
+    // the provider to ensure assert in get accessor is always run.
+    private static final OneshotSupplierImpl<PartitionResolver> sOneshotSupplierImpl =
+            new OneshotSupplierImpl<>();
+
+    private PartitionResolverSupplier() {}
+
+    /**
+     * Sets the given singleton resolver. Should only be called once.
+     * @param resolver Singleton resolver to use across the app.
+     */
+    public static void setInstance(PartitionResolver resolver) {
+        sOneshotSupplierImpl.set(resolver);
+    }
+
+    /**
+     * Fetches the previously set singleton resolver. Should only be called after being set.
+     * @return The singleton resolver.
+     */
+    public static PartitionResolver getInstance() {
+        assert sOneshotSupplierImpl.get() != null;
+        return sOneshotSupplierImpl.get();
+    }
+}
