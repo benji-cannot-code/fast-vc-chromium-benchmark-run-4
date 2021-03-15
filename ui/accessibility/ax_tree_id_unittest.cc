5cb03b659391c7643ab0058c851ae3504c242bd7
Erik Chen <erikchen@chromium.org>
2021-03-15 23:47:39 +0000

    Fix AXTreeID conversions.
    Prior to this CL, converting an AXTreeID to a token and back to an
    AXTreeID would return a different result. This did not appear to be
    intentional behavior, so this CL fixes that in preparation for AXTreeID
    serialization for Lacros.
    
    AX-Relnotes: n/a.
    Bug: 1185764
    Change-Id: I7dd91eb92f16046a612ffd56be49bcd267e67a17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2743824
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863043}

---
+++ ui/accessibility/ax_tree_id_unittest.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/accessibility/ax_tree_id.h"
+
+#include "base/unguessable_token.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace ui {
+
+TEST(AXTreeIDTest, ToStringFromString) {
+  AXTreeID tree_id = AXTreeID::CreateNewAXTreeID();
+  AXTreeID new_tree_id = AXTreeID::FromString(tree_id.ToString());
+  ASSERT_EQ(tree_id, new_tree_id);
+}
+
+TEST(AXTreeIDTest, ToTokenFromToken) {
+  AXTreeID tree_id = AXTreeID::CreateNewAXTreeID();
+  AXTreeID new_tree_id = AXTreeID::FromToken(tree_id.token().value());
+  ASSERT_EQ(tree_id, new_tree_id);
+}
+
+}  // namespace ui
