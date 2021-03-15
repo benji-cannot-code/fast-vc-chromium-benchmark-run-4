9e604ce2c913f4f18db9e6909a2e0f892e01174c
Joon Ahn <joonbug@chromium.org>
2021-03-15 23:33:12 +0000

    diagnostics: fix saved session log corrupt data.
    - cast uint8_t's, otherwise it prints as char code
    - print true/false for boolean value (instead of 0/1)
    
    Bug: 1188138
    Test: components_unitests --filter=*TelemetryLog*
    Change-Id: I3731802c48ad3e19d3dc125e00b633d44c6fab9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2762802
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863037}

--- chromeos/components/diagnostics_ui/backend/telemetry_log.cc
+++ chromeos/components/diagnostics_ui/backend/telemetry_log.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sstream>
 #include <utility>
 
+#include "base/strings/string_number_conversions.h"
+
 namespace chromeos {
 namespace diagnostics {
 namespace {
@@ -48,9 +50,9 @@ const char kMemoryUsageFreeMemoryTitle[] = "Free Memory (kib): ";
 
 // CpuUsage constants:
 const char kCpuUsageSectionName[] = "--- Cpu Usage ---";
-const char kCpuUsageUserTitle[] = "Usage User: ";
-const char kCpuUsageSystemTitle[] = "Usage System: ";
-const char kCpuUsageFreeTitle[] = "Usage Free: ";
+const char kCpuUsageUserTitle[] = "Usage User (%): ";
+const char kCpuUsageSystemTitle[] = "Usage System (%): ";
+const char kCpuUsageFreeTitle[] = "Usage Free (%): ";
 const char kCpuUsageAvgTempTitle[] = "Avg Temp (C): ";
 const char kCpuUsageScalingFrequencyTitle[] =
     "Current scaled frequency (kHz): ";
@@ -101,8 +103,10 @@ std::string TelemetryLog::GetContents() const {
            << kSystemInfoMilestoneVersionTitle
            << latest_system_info_->version_info->milestone_version << kNewline
            << kSystemInfoHasBatteryTitle
-           << latest_system_info_->device_capabilities->has_battery << kNewline
-           << kNewline;
+           << ((latest_system_info_->device_capabilities->has_battery)
+                   ? "true"
+                   : "false")
+           << kNewline << kNewline;
   }
   if (latest_battery_charge_status_) {
     output << kBatteryChargeStatusSectionName << kNewline
@@ -127,8 +131,9 @@ std::string TelemetryLog::GetContents() const {
            << kNewline << kBatteryHealthCycleCountTitle
            << latest_battery_health_->cycle_count << kNewline
            << kBatteryHealthWearPercentageTitle
-           << latest_battery_health_->battery_wear_percentage << kNewline
-           << kNewline;
+           << base::NumberToString(
+                  latest_battery_health_->battery_wear_percentage)
+           << kNewline << kNewline;
   }
   if (latest_memory_usage_) {
     output << kMemoryUsageSectionName << kNewline
@@ -141,11 +146,12 @@ std::string TelemetryLog::GetContents() const {
   }
   if (latest_cpu_usage_) {
     output << kCpuUsageSectionName << kNewline << kCpuUsageUserTitle
-           << latest_cpu_usage_->percent_usage_user << kNewline
-           << kCpuUsageSystemTitle << latest_cpu_usage_->percent_usage_system
+           << base::NumberToString(latest_cpu_usage_->percent_usage_user)
+           << kNewline << kCpuUsageSystemTitle
+           << base::NumberToString(latest_cpu_usage_->percent_usage_system)
            << kNewline << kCpuUsageFreeTitle
-           << latest_cpu_usage_->percent_usage_free << kNewline
-           << kCpuUsageAvgTempTitle
+           << base::NumberToString(latest_cpu_usage_->percent_usage_free)
+           << kNewline << kCpuUsageAvgTempTitle
            << latest_cpu_usage_->average_cpu_temp_celsius << kNewline
            << kCpuUsageScalingFrequencyTitle
            << latest_cpu_usage_->scaling_current_frequency_khz << kNewline
