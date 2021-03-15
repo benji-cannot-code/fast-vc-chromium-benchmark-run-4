54e3c65e78cc37163398ac5132f93e9b81a0aa0e
Anders Hartvoll Ruud <andruud@chromium.org>
2021-03-15 22:07:12 +0000

    Revert "Make custom properties that are IACVT guaranteed-invalid"
    This reverts commit 020915b1d8c067bbe8cb9f828ff3130975428ad3, and
    d928546e829a5d6a67e90d453a8d89ed7ae844eb.
    
    Additionally, it removes the baseline for
    var-reference-registered-properties-cycles.html, and re-enables the unit
    test StyleCascade.RevertToCycleInKeyframe.
    
    Change-Id: I2f3d6e0ad7df675598c0e71640deac73c8d6ad67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2759698
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862987}

--- third_party/blink/renderer/core/css/resolver/style_cascade_test.cc
+++ third_party/blink/renderer/core/css/resolver/style_cascade_test.cc
@@ -1061,19 +1061,6 @@ TEST_F(StyleCascadeTest, RegisteredCycle) {
   cascade.Add("--b", "var(--a)");
   cascade.Apply();
 
-  EXPECT_EQ("0px", cascade.ComputedValue("--a"));
-  EXPECT_EQ("0px", cascade.ComputedValue("--b"));
-}
-
-TEST_F(StyleCascadeTest, UniversalSyntaxCycle) {
-  RegisterProperty(GetDocument(), "--a", "*", "foo", false);
-  RegisterProperty(GetDocument(), "--b", "*", "bar", false);
-
-  TestCascade cascade(GetDocument());
-  cascade.Add("--a", "var(--b)");
-  cascade.Add("--b", "var(--a)");
-  cascade.Apply();
-
   EXPECT_FALSE(cascade.ComputedValue("--a"));
   EXPECT_FALSE(cascade.ComputedValue("--b"));
 }
@@ -1086,11 +1073,11 @@ TEST_F(StyleCascadeTest, PartiallyRegisteredCycle) {
   cascade.Add("--b", "var(--a)");
   cascade.Apply();
 
-  EXPECT_EQ("0px", cascade.ComputedValue("--a"));
+  EXPECT_FALSE(cascade.ComputedValue("--a"));
   EXPECT_FALSE(cascade.ComputedValue("--b"));
 }
 
-TEST_F(StyleCascadeTest, ReferencedRegisteredCycle) {
+TEST_F(StyleCascadeTest, FallbackTriggeredByRegisteredCycle) {
   RegisterProperty(GetDocument(), "--a", "<length>", "0px", false);
   RegisterProperty(GetDocument(), "--b", "<length>", "0px", false);
 
@@ -1103,10 +1090,10 @@ TEST_F(StyleCascadeTest, ReferencedRegisteredCycle) {
   cascade.Add("--d", "var(--b,2px)");
   cascade.Apply();
 
-  EXPECT_EQ("0px", cascade.ComputedValue("--a"));
-  EXPECT_EQ("0px", cascade.ComputedValue("--b"));
-  EXPECT_EQ("0px", cascade.ComputedValue("--c"));
-  EXPECT_EQ("0px", cascade.ComputedValue("--d"));
+  EXPECT_FALSE(cascade.ComputedValue("--a"));
+  EXPECT_FALSE(cascade.ComputedValue("--b"));
+  EXPECT_EQ("1px", cascade.ComputedValue("--c"));
+  EXPECT_EQ("2px", cascade.ComputedValue("--d"));
 }
 
 TEST_F(StyleCascadeTest, CycleStillInvalidWithFallback) {
@@ -1269,7 +1256,7 @@ TEST_F(StyleCascadeTest, EmUnitCycle) {
   cascade.Add("--x", "10em");
   cascade.Apply();
 
-  EXPECT_EQ("0px", cascade.ComputedValue("--x"));
+  EXPECT_FALSE(cascade.ComputedValue("--x"));
 }
 
 TEST_F(StyleCascadeTest, SubstitutingEmCycles) {
@@ -1282,8 +1269,8 @@ TEST_F(StyleCascadeTest, SubstitutingEmCycles) {
   cascade.Add("--z", "var(--x,1px)");
   cascade.Apply();
 
-  EXPECT_EQ("0px", cascade.ComputedValue("--y"));
-  EXPECT_EQ("0px", cascade.ComputedValue("--z"));
+  EXPECT_FALSE(cascade.ComputedValue("--y"));
+  EXPECT_EQ("1px", cascade.ComputedValue("--z"));
 }
 
 TEST_F(StyleCascadeTest, RemUnit) {
@@ -1332,7 +1319,7 @@ TEST_F(StyleCascadeTest, RemUnitInRootFontSizeCycle) {
   cascade.Add("--x", "1rem");
   cascade.Apply();
 
-  EXPECT_EQ("0px", cascade.ComputedValue("--x"));
+  EXPECT_FALSE(cascade.ComputedValue("--x"));
 }
 
 TEST_F(StyleCascadeTest, RemUnitInRootFontSizeNonCycle) {
@@ -1839,8 +1826,7 @@ TEST_F(StyleCascadeTest, RevertInKeyframeResponsive) {
   EXPECT_EQ("40px", cascade.ComputedValue("margin-left"));
 }
 
-// TODO(crbug.com/1185745): Temporarily disabled
-TEST_F(StyleCascadeTest, DISABLED_RevertToCycleInKeyframe) {
+TEST_F(StyleCascadeTest, RevertToCycleInKeyframe) {
   RegisterProperty(GetDocument(), "--x", "<length>", "0px", false);
 
   AppendSheet(R"HTML(
