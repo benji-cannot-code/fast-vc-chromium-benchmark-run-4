92fa3fe10724ccb00bc10551eb810e29308b3029
Tim van der Lippe <tvanderlippe@chromium.org>
2021-03-15 16:50:03 +0000

    Load legacy modules in DevTools layout tests 2/n
    R=aerotwist@chromium.org
    
    Bug: 1131500
    Change-Id: I044bd896c2e34d1581faa0c3aa6d4a8f6a43400e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2756274
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862853}

--- third_party/blink/web_tests/http/tests/devtools/tracing/timeline-js/timeline-js-line-level-profile.js
+++ third_party/blink/web_tests/http/tests/devtools/tracing/timeline-js/timeline-js-line-level-profile.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function() {
   TestRunner.addResult(`Tests that a line-level CPU profile is shown in the text editor.\n`);
   await TestRunner.loadModule('sources_test_runner');
+  await TestRunner.loadLegacyModule("perf_ui");
   await TestRunner.showPanel('timeline');
   await TestRunner.showPanel('sources');
   await TestRunner.addScriptTag('../resources/empty.js');
--- third_party/blink/web_tests/http/tests/devtools/tracing/timeline-misc/timeline-window-filter.js
+++ third_party/blink/web_tests/http/tests/devtools/tracing/timeline-misc/timeline-window-filter.js
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   TestRunner.addResult(`Tests the Timeline window filter.`);
   TestRunner.addResult(`It applies different ranges to the OverviewGrid and expects that current view reflects the change.\n`);
   await TestRunner.loadModule('performance_test_runner');
+  await TestRunner.loadLegacyModule("perf_ui");
   await TestRunner.showPanel('timeline');
 
   var timeline = UI.panels.timeline;
