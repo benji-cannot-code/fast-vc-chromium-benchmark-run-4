55213040a28a4162a1a74281ce046ffc1117f16f
Varun Khaneja <vakh@chromium.org>
2021-03-15 23:08:34 +0000

    Use histogram function instead of macro to log UMA.
    Using the macro caused the same metric to be logged multiple times
    even though the metric name argument being passed to the macro was
    different each time.
    
    Also, post to |task_runner_| just once for all renames, instead
    of posting individually for each file rename.
    
    Bug: 1126611
    Change-Id: I04815da1d1e2fb8ea8bb9db82f8f011c00299809
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2755179
    Auto-Submit: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863024}

--- components/safe_browsing/core/db/v4_local_database_manager.cc
+++ components/safe_browsing/core/db/v4_local_database_manager.cc
@@ -301,7 +301,9 @@ V4LocalDatabaseManager::V4LocalDatabaseManager(
                        : base::ThreadPool::CreateSequencedTaskRunner(
                              {base::MayBlock(),
                               base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN})) {
-  RenameOldStoreFiles();
+  task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(&V4LocalDatabaseManager::RenameOldStoreFiles,
+                                weak_factory_.GetWeakPtr()));
 
   DCHECK(!base_path_.empty());
   DCHECK(!list_infos_.empty());
@@ -936,7 +938,6 @@ void V4LocalDatabaseManager::RenameOldStoreFiles() {
     const base::StringPiece& new_name = pair.second;
 
     const base::FilePath old_store_path = base_path_.AppendASCII(old_name);
-
     // Is the old filename also being used for a valid V4Store?
     auto it = std::find_if(
         std::begin(list_infos_), std::end(list_infos_),
@@ -968,9 +969,7 @@ void V4LocalDatabaseManager::RenameOldStoreFiles() {
       continue;
     }
 
-    task_runner_->PostTask(
-        FROM_HERE, base::BindOnce(&V4LocalDatabaseManager::RenameStoreFile,
-                                  old_store_path, new_store_path));
+    RenameStoreFile(old_store_path, new_store_path);
   }
 }
 
@@ -980,7 +979,7 @@ void V4LocalDatabaseManager::RenameStoreFile(const base::FilePath& old_path,
   base::File::Error error = base::File::FILE_OK;
   base::ReplaceFile(old_path, new_path, &error);
 
-  UMA_HISTOGRAM_ENUMERATION(
+  base::UmaHistogramExactLinear(
       "SafeBrowsing.V4Store.RenameStatus" + GetUmaSuffixForStore(new_path),
       -error, -base::File::FILE_ERROR_MAX);
 }
