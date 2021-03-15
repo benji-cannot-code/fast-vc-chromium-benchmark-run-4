97dc7659395c24e6b2772237c8bed73d16f3bf29
Kurt Catti-Schmidt (SCHMIDT) <kschmi@microsoft.com>
2021-03-15 21:20:55 +0000

    [GridNG] Baseline alignment
    This is a refactor of Ian's excellent prototype for baseline support
    @ https://chromium-review.googlesource.com/c/chromium/src/+/2694509
    
    Some follow-up is necessary in the mapping between directions and
    the actual baselines (see TODO's in GetLogicalBaseline and
    CanParticipateInBaselineAlignment), but this is the behavior the
    tests are expecting.
    
    There's one new crash in TestExpecations
    (grid-row-axis-self-baseline-synthesized-003.html) that will be
    addressed in the aforementioned follow-up. The layout space is
    indeterminate, so it requires a second pass.
    
    Bug: 1045599
    Change-Id: I8a8deea21218ae4747401f1a3f20f3738cf0db7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2714323
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Ethan Jimenez <ethavar@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862963}

--- third_party/blink/renderer/core/layout/ng/ng_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_fragment.h
@@ -37,6 +37,9 @@ class CORE_EXPORT NGFragment {
     return physical_fragment_.Size().ConvertToLogical(
         writing_direction_.GetWritingMode());
   }
+  WritingDirectionMode GetWritingDirection() const {
+    return writing_direction_;
+  }
 
  protected:
   const NGPhysicalFragment& physical_fragment_;
