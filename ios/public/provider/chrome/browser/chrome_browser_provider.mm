a16f447117b4d5322276cda5fc27b6dae0e52b06
Justin Cohen <justincohen@google.com>
2021-03-15 21:14:31 +0000

    ios: Experiment with iOS_TBA native session restore.
    Per https://trac.webkit.org/changeset/273283, iOS_TBA may contain
    native session restore.  Repurpose a similar experiment for something
    we can use to test this feature with internal WebKit builds.
    
    Bug: 1174560
    
    Change-Id: Ibcf8d4644201f485136a423abbe85bddd6661f29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732510
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862957}

--- ios/public/provider/chrome/browser/chrome_browser_provider.mm
+++ ios/public/provider/chrome/browser/chrome_browser_provider.mm
@@ -84,12 +84,6 @@ bool ChromeBrowserProvider::MightBlockUrlDuringRestore() {
   return false;
 }
 
-bool ChromeBrowserProvider::ShouldBlockUrlDuringRestore(
-    const GURL& url,
-    web::WebState* web_state) {
-  return false;
-}
-
 UITextField* ChromeBrowserProvider::CreateStyledTextField() const {
   return nil;
 }
