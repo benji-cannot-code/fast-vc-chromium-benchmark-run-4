1668e1067551afbfbec67ee3df5172a96cdfaa40
Ayu Ishii <ayui@chromium.org>
2021-03-15 23:59:50 +0000

    StorageBuckets: Remove title policy
    Per feedback from early TAG review,
    Remove `title` policy from StorageBucketOptions.
    
    https://github.com/WICG/storage-buckets/issues/27
    
    Bug: 1107553
    Change-Id: Id5c4affc64943569af888be6d0e1e28986fd3f6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757169
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863050}

--- third_party/blink/renderer/modules/buckets/storage_bucket_manager.cc
+++ third_party/blink/renderer/modules/buckets/storage_bucket_manager.cc
@@ -44,9 +44,6 @@ mojom::blink::BucketPoliciesPtr ToMojoBucketPolicies(
     const StorageBucketOptions* options) {
   auto policies = mojom::blink::BucketPolicies::New();
   policies->persisted = options->persisted();
-  policies->title = (options->hasTitle() && !options->title().IsEmpty())
-                        ? options->title()
-                        : "";
   policies->quota = options->hasQuotaNonNull()
                         ? options->quotaNonNull()
                         : mojom::blink::kNoQuotaPolicyValue;
