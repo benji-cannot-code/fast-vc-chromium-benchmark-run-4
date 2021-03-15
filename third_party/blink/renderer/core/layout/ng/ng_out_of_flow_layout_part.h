78d77c5c8bf45300744d5949102d00df4c3e9c8f
Alison Maher <almaher@microsoft.com>
2021-03-15 22:29:41 +0000

    [LayoutNG] Nested multicol inside an abspos
    This change makes sure that any inner multicols with pending OOF
    descendants that are nested inside an OOF positioned element are
    properly propagated and handled. For example:
      multicol
        relpos
          abspos
            multicol
              relpos
                abspos
    
    Bug: 1079031
    Change-Id: Ia23234b3d9adf1e3c850b41174d5cab7bd4043c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757113
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863000}

--- third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.h
+++ third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.h
@@ -86,7 +86,6 @@ class CORE_EXPORT NGOutOfFlowLayoutPart {
     LogicalRect rect;
   };
 
-  // TODO(almaher): Move this to the multicol algorithm in upcoming refactor.
   // This stores the information needed to update a multicol child inside an
   // existing multicol fragment. This is used during nested fragmentation of an
   // OOF positioned element.
@@ -170,6 +169,7 @@ class CORE_EXPORT NGOutOfFlowLayoutPart {
                         const LayoutBox* only_layout,
                         HashSet<const LayoutObject*>* placed_objects);
 
+  void HandleMulticolsWithPendingOOFs(NGBoxFragmentBuilder* container_builder);
   void LayoutOOFsInMulticol(const NGBlockNode& multicol);
 
   // Layout the OOF nodes that are descendants of a fragmentation context root.
