74bfa1065f14ca05ddbc3463cf9b43e98c5f303a
kylechar <kylechar@chromium.org>
2021-03-15 15:56:03 +0000

    Add DisallowSyncCall() in GPU process
    Viz and GPU code rely heavily on messages arriving in order. Incoming
    sync mojo IPCs can be handled out of order with respect to async IPCs if
    the receiver is bound to a thread that is waiting on the response to an
    outgoing sync mojo IPC.
    
    Also both viz and GPU main threads are on the critical display path and
    blocking either thread should be avoided anyways.
    
    There doesn't appear to be any sync mojo IPCs sent from the GPU process
    today thankfully. Add SyncCallRestrictions::DisallowSyncCall() when
    starting the GPU process to ensure that none get added.
    
    Fixed: 1187572
    Change-Id: I8dbbebde24f0140d37b421c6a2688080fd005eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2753828
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862826}

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/service/gpu_init.h"
 #include "gpu/ipc/service/gpu_watchdog_thread.h"
 #include "media/gpu/buildflags.h"
+#include "mojo/public/cpp/bindings/sync_call_restrictions.h"
 #include "services/tracing/public/cpp/trace_startup.h"
 #include "third_party/angle/src/gpu_info_util/SystemInfo.h"
 #include "ui/base/ui_base_features.h"
@@ -232,6 +233,13 @@ int GpuMain(const MainFunctionParams& parameters) {
     bool success = gpu_preferences.FromSwitchValue(value);
     CHECK(success);
   }
+
+  // Disallow sending sync IPCs from the GPU process, in particular CrGpuMain
+  // and VizCompositorThreads. Incoming sync IPCs can be received out of order
+  // when waiting on response to an outgoing sync IPC. Both viz and gpu
+  // interfaces rely on receiving messages in order so this message reordering
+  // would break things.
+  mojo::SyncCallRestrictions::DisallowSyncCall();
 
   if (gpu_preferences.gpu_startup_dialog)
     WaitForDebugger("Gpu");
