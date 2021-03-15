f547570f48c09b0fb6598554e264303cf27384a1
Cathy Li <chili@chromium.org>
2021-03-15 23:55:10 +0000

    [Webfeed]: Connect sectionheaderlist to view supporting multiple headers.
    Note that minimal styling work has been done. More to come in a later CL.
    
    Bug: 1152592
    Change-Id: I5df9a754ee985685b4af14bab248d48d98ec69c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2729758
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863048}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/OnSectionHeaderSelectedListener.java
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ntp.snippets;
+
+/** Callback for when a section header is selected. */
+public interface OnSectionHeaderSelectedListener {
+    /**
+     * Callback for when a header tab is selected.
+     * @param index the index of the tab selected.
+     */
+    void onSectionHeaderSelected(int index);
+}
