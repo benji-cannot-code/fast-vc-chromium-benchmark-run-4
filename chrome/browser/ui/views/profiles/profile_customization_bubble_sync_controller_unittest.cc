6a3f5c751651c09733ad8b7ec558816d0275a068
Jan Krcal <jkrcal@chromium.org>
2021-03-15 22:38:52 +0000

    [Profile customization] Skip bubble if extension theme is set to install
    This CL fixes an important case for the OnThemeSyncStarted notification
    in ThemeSyncableService: if merging sync data resulted in installing a
    new theme extension, this bit is returned in the notification.
    
    This is important for the (only) observer to be able to skip the profile
    customization bubble if an extension theme was synced down (the observer
    cannot tell it from the state of ThemeService because theme installation
    is async and the default theme is used while the extension is being
    installed).
    
    This CL sticks to the original promise: notifying when theme sync
    starts. For the customization bubble, this can result in rare false
    negatives (when the installation of the theme fails). This outcome
    is non-problematic and thus does not justify extra complexity of
    waiting for the theme to actually install (the complexity comes from
    the fact that ThemeSyncableService does not get notified when the
    installation of the theme fails).
    
    Bug: 1187496
    Change-Id: Ibd19e4a3c15e77ababe8ae1644d9859df5ad1c4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2760039
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863004}

--- chrome/browser/ui/views/profiles/profile_customization_bubble_sync_controller_unittest.cc
+++ chrome/browser/ui/views/profiles/profile_customization_bubble_sync_controller_unittest.cc
@@ -84,8 +84,12 @@ class ProfileCustomizationBubbleSyncControllerTest : public testing::Test {
     fake_theme_service_.DoSetTheme(nullptr, false);
   }
 
-  void NotifyOnSyncStarted() {
-    theme_syncable_service_.NotifyOnSyncStartedForTesting();
+  void NotifyOnSyncStarted(bool waiting_for_extension_installation = false) {
+    theme_syncable_service_.NotifyOnSyncStartedForTesting(
+        waiting_for_extension_installation
+            ? ThemeSyncableService::ThemeSyncState::
+                  kWaitingForExtensionInstallation
+            : ThemeSyncableService::ThemeSyncState::kApplied);
   }
 
  protected:
@@ -138,6 +142,16 @@ TEST_F(ProfileCustomizationBubbleSyncControllerTest,
   ApplyColorAndShowBubbleWhenNoValueSynced(show_bubble.Get());
   SetSyncedProfileTheme();
   NotifyOnSyncStarted();
+  histogram_tester_.ExpectTotalCount("Profile.SyncCustomizationBubbleDelay", 1);
+}
+
+TEST_F(ProfileCustomizationBubbleSyncControllerTest,
+       ShouldNotShowWhenSyncGetsCustomThemeToInstall) {
+  base::MockCallback<base::OnceCallback<void(bool)>> show_bubble;
+  EXPECT_CALL(show_bubble, Run(false));
+
+  ApplyColorAndShowBubbleWhenNoValueSynced(show_bubble.Get());
+  NotifyOnSyncStarted(/*waiting_for_extension_installation=*/true);
   histogram_tester_.ExpectTotalCount("Profile.SyncCustomizationBubbleDelay", 1);
 }
 
