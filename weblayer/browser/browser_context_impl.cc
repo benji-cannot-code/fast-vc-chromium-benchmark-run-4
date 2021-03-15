745670f33c97149b6e346397c6d73b5ecc849f53
Colin Blundell <blundell@chromium.org>
2021-03-15 16:03:59 +0000

    [WebLayer] Enable heavy ad interventions
    This CL enables heavy ad interventions in WebLayer via:
    - Bringup of HeavyAdService
    - Proper initialization of HeavyAdService on BrowserContext creation
    - Passing of HeavyAdService instances to AdsPageLoadMetricsObserver
    
    This CL also adds a representative subset of //chrome's heavy ad
    intervention browsertests to //weblayer to test the integration.
    
    We will add clearing of the heavy ad blocklist data on browsing data
    removal in a followup CL.
    
    Bug: 1116095
    Change-Id: I2f182475f011cab09a18f1f05a373e7e76d3c897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2753147
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862828}

--- weblayer/browser/browser_context_impl.cc
+++ weblayer/browser/browser_context_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/download/public/common/in_progress_download_manager.h"
 #include "components/embedder_support/pref_names.h"
+#include "components/heavy_ad_intervention/heavy_ad_service.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/language/core/browser/language_prefs.h"
 #include "components/payments/core/payment_prefs.h"
@@ -43,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "weblayer/browser/browsing_data_remover_delegate_factory.h"
 #include "weblayer/browser/client_hints_factory.h"
 #include "weblayer/browser/default_search_engine.h"
+#include "weblayer/browser/heavy_ad_service_factory.h"
 #include "weblayer/browser/permissions/permission_manager_factory.h"
 #include "weblayer/browser/stateful_ssl_host_state_delegate_factory.h"
 #include "weblayer/public/common/switches.h"
@@ -105,6 +107,13 @@ BrowserContextImpl::BrowserContextImpl(ProfileImpl* profile_impl,
 
   BrowserContextDependencyManager::GetInstance()->CreateBrowserContextServices(
       this);
+
+  auto* heavy_ad_service = HeavyAdServiceFactory::GetForBrowserContext(this);
+  if (IsOffTheRecord()) {
+    heavy_ad_service->InitializeOffTheRecord();
+  } else {
+    heavy_ad_service->Initialize(GetPath());
+  }
 
   site_isolation::SiteIsolationPolicy::ApplyPersistedIsolatedOrigins(this);
 
