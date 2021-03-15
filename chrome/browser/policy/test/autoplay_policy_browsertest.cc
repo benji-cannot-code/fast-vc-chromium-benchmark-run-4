6b876bab1dcd753339459cf9ecd7df7f0e0e8ef6
Mateusz Malon <mmalon@opera.com>
2021-03-15 08:33:50 +0000

    Update autoplay_policy preference after every navigation
    As autoplay_policy preference is URL dependent it should be updated
    after each navigation.
    
    Bug: 1185719
    Change-Id: I3b37b91092909743ad620083330d9133da043f00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2745146
    Commit-Queue: Mateusz Malon <mmalon@opera.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862744}

--- chrome/browser/policy/test/autoplay_policy_browsertest.cc
+++ chrome/browser/policy/test/autoplay_policy_browsertest.cc
@@ -92,7 +92,7 @@ IN_PROC_BROWSER_TEST_F(AutoplayPolicyTest, AutoplayAllowedByPolicy) {
   UpdateProviderPolicy(policies);
 
   // Check that autoplay was allowed by policy.
-  GetWebContents()->OnWebPreferencesChanged();
+  NavigateToTestPage();
   EXPECT_TRUE(TryAutoplay(GetMainFrame()));
   EXPECT_TRUE(TryAutoplay(GetChildFrame()));
 }
@@ -114,7 +114,7 @@ IN_PROC_BROWSER_TEST_F(AutoplayPolicyTest, AutoplayWhitelist_Allowed) {
   UpdateProviderPolicy(policies);
 
   // Check that autoplay was allowed by policy.
-  GetWebContents()->OnWebPreferencesChanged();
+  NavigateToTestPage();
   EXPECT_TRUE(TryAutoplay(GetMainFrame()));
   EXPECT_TRUE(TryAutoplay(GetChildFrame()));
 }
@@ -136,7 +136,7 @@ IN_PROC_BROWSER_TEST_F(AutoplayPolicyTest, AutoplayWhitelist_PatternAllowed) {
   UpdateProviderPolicy(policies);
 
   // Check that autoplay was allowed by policy.
-  GetWebContents()->OnWebPreferencesChanged();
+  NavigateToTestPage();
   EXPECT_TRUE(TryAutoplay(GetMainFrame()));
   EXPECT_TRUE(TryAutoplay(GetChildFrame()));
 }
@@ -158,7 +158,7 @@ IN_PROC_BROWSER_TEST_F(AutoplayPolicyTest, AutoplayWhitelist_Missing) {
   UpdateProviderPolicy(policies);
 
   // Check that autoplay was not allowed.
-  GetWebContents()->OnWebPreferencesChanged();
+  NavigateToTestPage();
   EXPECT_FALSE(TryAutoplay(GetMainFrame()));
   EXPECT_FALSE(TryAutoplay(GetChildFrame()));
 }
@@ -176,7 +176,7 @@ IN_PROC_BROWSER_TEST_F(AutoplayPolicyTest, AutoplayDeniedByPolicy) {
   UpdateProviderPolicy(policies);
 
   // Check that autoplay was not allowed by policy.
-  GetWebContents()->OnWebPreferencesChanged();
+  NavigateToTestPage();
   EXPECT_FALSE(TryAutoplay(GetMainFrame()));
   EXPECT_FALSE(TryAutoplay(GetChildFrame()));
 
@@ -189,7 +189,7 @@ IN_PROC_BROWSER_TEST_F(AutoplayPolicyTest, AutoplayDeniedByPolicy) {
   UpdateProviderPolicy(policies);
 
   // Check that autoplay was not allowed.
-  GetWebContents()->OnWebPreferencesChanged();
+  NavigateToTestPage();
   EXPECT_FALSE(TryAutoplay(GetMainFrame()));
   EXPECT_FALSE(TryAutoplay(GetChildFrame()));
 }
@@ -207,7 +207,7 @@ IN_PROC_BROWSER_TEST_F(AutoplayPolicyTest, AutoplayDeniedAllowedWithURL) {
   UpdateProviderPolicy(policies);
 
   // Check that autoplay was not allowed by policy.
-  GetWebContents()->OnWebPreferencesChanged();
+  NavigateToTestPage();
   EXPECT_FALSE(TryAutoplay(GetMainFrame()));
   EXPECT_FALSE(TryAutoplay(GetChildFrame()));
 
@@ -220,7 +220,7 @@ IN_PROC_BROWSER_TEST_F(AutoplayPolicyTest, AutoplayDeniedAllowedWithURL) {
   UpdateProviderPolicy(policies);
 
   // Check that autoplay was allowed by policy.
-  GetWebContents()->OnWebPreferencesChanged();
+  NavigateToTestPage();
   EXPECT_TRUE(TryAutoplay(GetMainFrame()));
   EXPECT_TRUE(TryAutoplay(GetChildFrame()));
 }
@@ -240,7 +240,7 @@ IN_PROC_BROWSER_TEST_F(AutoplayPolicyTest,
   UpdateProviderPolicy(policies);
 
   // Check that autoplay was not allowed by policy.
-  GetWebContents()->OnWebPreferencesChanged();
+  NavigateToTestPage();
   EXPECT_FALSE(TryAutoplay(GetMainFrame()));
   EXPECT_FALSE(TryAutoplay(GetChildFrame()));
 
@@ -253,7 +253,7 @@ IN_PROC_BROWSER_TEST_F(AutoplayPolicyTest,
   UpdateProviderPolicy(policies);
 
   // Check that autoplay was allowed by policy.
-  GetWebContents()->OnWebPreferencesChanged();
+  NavigateToTestPage();
   EXPECT_TRUE(TryAutoplay(GetMainFrame()));
   EXPECT_TRUE(TryAutoplay(GetChildFrame()));
 }
