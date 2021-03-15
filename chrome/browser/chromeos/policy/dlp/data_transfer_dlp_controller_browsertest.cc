6ca4cf55a568f2a53f9009139aada5ff0dbac845
Aya ElAttar <ayaelattar@chromium.org>
2021-03-15 09:11:40 +0000

    Add unittests for DlpDataTransferNotifier
    Bug: 1187287
    Change-Id: Ida8afa429e629891bc17480008880f827a59e02c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2754048
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862750}

--- chrome/browser/chromeos/policy/dlp/data_transfer_dlp_controller_browsertest.cc
+++ chrome/browser/chromeos/policy/dlp/data_transfer_dlp_controller_browsertest.cc
@@ -52,7 +52,7 @@ constexpr char kExampleUrl[] = "https://example.com";
 
 class FakeClipboardNotifier : public DlpClipboardNotifier {
  public:
-  views::Widget* GetWidget() { return GetWidgetForTesting(); }
+  views::Widget* GetWidget() { return widget_.get(); }
 
   void ProceedPressed(const ui::DataTransferEndpoint& data_dst) {
     DlpClipboardNotifier::ProceedPressed(data_dst, GetWidget());
