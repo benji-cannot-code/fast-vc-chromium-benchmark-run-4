158fd50bb08fe58241aa5f99c4b98dd05011acd3
Sigurdur Asgeirsson <siggi@chromium.org>
2021-03-15 19:29:50 +0000

    ScopedObserver IWYU fixes.
    Fix compile errors due to ScopedObserver retirement.
    
    Bug: 1145565
    Change-Id: I144de8ad67acdfbcb158771f3417e0223139bf24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2761039
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#862916}

--- chrome/browser/ash/login/app_mode/kiosk_browsertest.cc
+++ chrome/browser/ash/login/app_mode/kiosk_browsertest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
+#include "base/scoped_observer.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
--- chrome/browser/ash/login/webview_login_browsertest.cc
+++ chrome/browser/ash/login/webview_login_browsertest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/run_loop.h"
+#include "base/scoped_observer.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
