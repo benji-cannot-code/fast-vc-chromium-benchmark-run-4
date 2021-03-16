0e08950c91bc9ba1d43c26373907652533129a27
Richard Knoll <knollr@chromium.org>
2021-03-16 19:58:00 +0000

    Enable NotificationCompat.Builder in tests
    We will start enabling this for users so let's enable it in tests so
    we're sure it continues to work as expected. Also updates expectations
    in one test to work with and without the flag. Once we enable the flag
    by default we'll remove the @EnableFeatures annotations and flag checks.
    
    Bug: 697104
    Change-Id: I92ba784d2d3ca14d45a0b3058011d74eb88d73df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2756218
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863419}

--- chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/TrustedWebActivityClientTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/TrustedWebActivityClientTest.java
@@ -39,7 +39,7 @@ import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.notifications.NotificationUmaTracker;
 import org.chromium.chrome.browser.notifications.StandardNotificationBuilder;
 import org.chromium.chrome.test.util.browser.Features;
-import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
+import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
 import org.chromium.components.embedder_support.util.Origin;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
 
@@ -65,7 +65,7 @@ import java.util.concurrent.TimeoutException;
  * 4. This sends a Message to ResponseHandler in this class.
  */
 @RunWith(BaseJUnit4ClassRunner.class)
-@DisableFeatures(ChromeFeatureList.USE_NOTIFICATION_COMPAT_BUILDER)
+@EnableFeatures(ChromeFeatureList.USE_NOTIFICATION_COMPAT_BUILDER)
 public class TrustedWebActivityClientTest {
     private static final Uri SCOPE = Uri.parse("https://www.example.com/notifications");
     private static final Origin ORIGIN = Origin.create(SCOPE);
