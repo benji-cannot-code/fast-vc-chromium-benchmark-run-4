eb72f4ea57b3c9cbade46af0b421a02e616bbd01
Dominique Fauteux-Chapleau <domfc@chromium.org>
2021-03-16 17:27:31 +0000

    Add --reporting-connector-url switch support
    This will allow an easier override of this URL than a custom Chrome
    build for dev purposes.
    
    Bug: 1164403
    Change-Id: I11b020d730a34dcb042095fa6ea856e65dcdaa29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2753413
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863339}

--- chrome/browser/enterprise/connectors/reporting/reporting_service_settings.cc
+++ chrome/browser/enterprise/connectors/reporting/reporting_service_settings.cc
@@ -5,11 +5,39 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/enterprise/connectors/reporting/reporting_service_settings.h"
 
+#include "base/command_line.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/enterprise/connectors/service_provider_config.h"
 #include "chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.h"
+#include "chrome/browser/policy/chrome_browser_policy_connector.h"
 #include "components/policy/core/browser/url_util.h"
 
 namespace enterprise_connectors {
+
+namespace {
+
+constexpr char kReportingConnectorUrlFlag[] = "reporting-connector-url";
+
+base::Optional<GURL> GetUrlOverride() {
+  // Ignore this flag on Stable and Beta to avoid abuse.
+  if (!g_browser_process || !g_browser_process->browser_policy_connector()
+                                 ->IsCommandLineSwitchSupported()) {
+    return base::nullopt;
+  }
+
+  base::CommandLine* cmd = base::CommandLine::ForCurrentProcess();
+  if (cmd->HasSwitch(kReportingConnectorUrlFlag)) {
+    GURL url = GURL(cmd->GetSwitchValueASCII(kReportingConnectorUrlFlag));
+    if (url.is_valid())
+      return url;
+    else
+      VLOG(1) << "--reporting-connector-url is set to an invalid URL";
+  }
+
+  return base::nullopt;
+}
+
+}  // namespace
 
 ReportingServiceSettings::ReportingServiceSettings(
     const base::Value& settings_value,
@@ -58,7 +86,8 @@ ReportingServiceSettings::GetReportingSettings() const {
 
   ReportingSettings settings;
 
-  settings.reporting_url = GURL(service_provider_->reporting_url());
+  settings.reporting_url =
+      GetUrlOverride().value_or(GURL(service_provider_->reporting_url()));
   DCHECK(settings.reporting_url.is_valid());
 
   settings.enabled_event_names.insert(enabled_event_names_.begin(),
