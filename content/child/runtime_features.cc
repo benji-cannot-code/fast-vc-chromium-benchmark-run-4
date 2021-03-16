c9c5dd6b3f7587a2f3ad9ca8d37f53b76d2274b9
Tommy Steimel <steimel@chromium.org>
2021-03-16 18:59:07 +0000

    PiP 1.5: Expose video conferencing actions and state setters in Blink
    This CL adds to the Media Session Blink API to expose the new video
    conferencing MediaSessionActions (e.g. mutemicrophone) as well as
    methods to set the current microphone and camera states.
    
    Design doc: https://docs.google.com/document/d/1KDpWqg9LcnuQ5TQDBK45BrbkyfuziolZ0m7Wmt6xPnU/edit?usp=sharing&resourcekey=0-I0o3GayIn9Q-2LN7EB1l8w
    
    Bug: 1178939
    Change-Id: Icdea3602bbea2809b9622132664beb80e52b5ff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2698773
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863385}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -267,6 +267,7 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
     {wf::EnableMediaEngagementBypassAutoplayPolicies,
      media::kMediaEngagementBypassAutoplayPolicies},
     {wf::EnableMediaFeeds, media::kMediaFeeds},
+    {wf::EnableMediaSessionWebRTC, media::kMediaSessionWebRTC},
     {wf::EnableMouseSubframeNoImplicitCapture,
      features::kMouseSubframeNoImplicitCapture},
     {wf::EnableNeverSlowMode, features::kNeverSlowMode},
