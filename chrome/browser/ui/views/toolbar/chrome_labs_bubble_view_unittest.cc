d1e190db1303f1b9dede1d1061f82f26eb7e9fba
Elaine Chien <elainechien@chromium.org>
2021-03-16 00:16:55 +0000

    Chrome Labs: Add support for translations and translate experiments
    Bug: 1145666
    Change-Id: I8b76e2c90266b95cb6267b624ed911f970c461c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2744641
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#863062}

--- chrome/browser/ui/views/toolbar/chrome_labs_bubble_view_unittest.cc
+++ chrome/browser/ui/views/toolbar/chrome_labs_bubble_view_unittest.cc
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 const char kFirstTestFeatureId[] = "feature-1";
-const char kSecondTestFeatureId[] = "feature-2";
+const char kTestFeatureWithVariationId[] = "feature-2";
 const char kThirdTestFeatureId[] = "feature-3";
 const char kExpiredFlagTestFeatureId[] = "expired-feature";
 
@@ -55,7 +55,7 @@ class ChromeLabsBubbleTest : public TestWithBrowserView {
             {{kFirstTestFeatureId, "", "",
               flags_ui::FlagsState::GetCurrentPlatform(),
               FEATURE_VALUE_TYPE(kTestFeature1)},
-             {kSecondTestFeatureId, "", "",
+             {kTestFeatureWithVariationId, "", "",
               flags_ui::FlagsState::GetCurrentPlatform(),
               FEATURE_WITH_PARAMS_VALUE_TYPE(kTestFeature2,
                                              kTestVariations2,
@@ -165,9 +165,13 @@ class ChromeLabsBubbleTest : public TestWithBrowserView {
         LabInfo(kFirstTestFeatureId, base::ASCIIToUTF16(""),
                 base::ASCIIToUTF16(""), "", version_info::Channel::STABLE));
 
+    std::vector<std::u16string> variation_descriptions = {
+        base::ASCIIToUTF16("Description")};
+
     test_feature_info.emplace_back(
-        LabInfo(kSecondTestFeatureId, base::ASCIIToUTF16(""),
-                base::ASCIIToUTF16(""), "", version_info::Channel::STABLE));
+        LabInfo(kTestFeatureWithVariationId, base::ASCIIToUTF16(""),
+                base::ASCIIToUTF16(""), "", version_info::Channel::STABLE,
+                variation_descriptions));
 
     test_feature_info.emplace_back(
         LabInfo(kThirdTestFeatureId, base::ASCIIToUTF16(""),
