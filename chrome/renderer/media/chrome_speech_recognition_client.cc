a5a0fcb91d8819d51091bb25ced96e0335209f7d
evliu <evliu@google.com>
2021-03-16 01:42:39 +0000

    Move ChromeSpeechRecognitionClient::Reset to the media thread
    The ChromeSpeechRecognitionClient properties are currently read from the Media thread via IsSpeechRecognitionAvailable() but written from the main thread via Reset(), resulting in a data race condition. This CL binds the Reset() call to the Media thread.
    
    Bug: 1187788
    Change-Id: I25e550701b7d8c5ca044b02c7e7b6b8f34bb108e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2762403
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#863094}

--- chrome/renderer/media/chrome_speech_recognition_client.cc
+++ chrome/renderer/media/chrome_speech_recognition_client.cc
@@ -118,8 +118,8 @@ void ChromeSpeechRecognitionClient::SpeechRecognitionAvailabilityChanged(
     bool is_speech_recognition_available) {
   if (is_speech_recognition_available) {
     initialize_callback_.Run();
-  } else {
-    Reset();
+  } else if (reset_callback_) {
+    reset_callback_.Run();
   }
 }
 
@@ -150,6 +150,10 @@ void ChromeSpeechRecognitionClient::Initialize() {
     base::UmaHistogramBoolean("Accessibility.LiveCaption.WebsiteBlocked",
                               is_website_blocked_);
   }
+
+  // Bind the call to Reset() to the Media thread.
+  reset_callback_ = media::BindToCurrentLoop(base::BindRepeating(
+      &ChromeSpeechRecognitionClient::Reset, weak_factory_.GetWeakPtr()));
 
   speech_recognition_context_.set_disconnect_handler(media::BindToCurrentLoop(
       base::BindOnce(&ChromeSpeechRecognitionClient::OnRecognizerDisconnected,
