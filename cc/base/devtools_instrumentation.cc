194f5f25d19df190652d4843caf768494c85ed3a
Mingjing Zhang <mjzhang@chromium.org>
2021-03-16 18:10:11 +0000

    Use frame presentation timestamp as frame boundaries in DevTools
    In this CL, DevTools are now notified of the timestamp of the each
    frame's presentation and will use that timestamp to visualize frame
    boundaries in the Frames timeline. This change will make the duration
    of each frame more accurately aligned with refresh rate.
    
    This CL requires https://crrev.com/c/2760960 to populate the change
    in DevTools. If used without CL2760960, the current CL will not
    cause observable change in the Frame timeline in DevTools.
    
    Bug: 1185876
    Change-Id: I6239164dc0941c76323ad147bfe5ba909748b83c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2743914
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863359}

--- cc/base/devtools_instrumentation.cc
+++ cc/base/devtools_instrumentation.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/base/devtools_instrumentation.h"
 
+#include <string>
+
 namespace cc {
 namespace devtools_instrumentation {
 namespace {
@@ -37,6 +39,7 @@ const char kFrameId[] = "frameId";
 const char kLayerId[] = "layerId";
 const char kLayerTreeId[] = "layerTreeId";
 const char kPixelRefId[] = "pixelRefId";
+const char kPresentationTimestamp[] = "presentationTimestamp";
 
 const char kImageUploadTask[] = "ImageUploadTask";
 const char kImageDecodeTask[] = "ImageDecodeTask";
