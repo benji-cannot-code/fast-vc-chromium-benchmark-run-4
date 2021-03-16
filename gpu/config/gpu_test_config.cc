0ff82bef4b8847553a2d0009f8832971056bf2ec
Zhenyao Mo <zmo@chromium.org>
2021-03-16 01:08:38 +0000

    Fix Mac BigSur OS version issue in GPUTestConfig
    BigSur includes all 11.*, not just 11.0.
    
    TEST=gpu_unittests on Mac FYI arm64 Release
    
    Bug: 1187607
    Change-Id: Ice76183dfb7735362353304ecc7dc2566c0aa7ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2762642
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863079}

--- gpu/config/gpu_test_config.cc
+++ gpu/config/gpu_test_config.cc
@@ -83,11 +83,7 @@ GPUTestConfig::OS GetCurrentOS() {
       }
       break;
     case 11:
-      switch (minor_version) {
-        case 0:
-          return GPUTestConfig::kOsMacBigSur;
-      }
-      break;
+      return GPUTestConfig::kOsMacBigSur;
   }
   return GPUTestConfig::kOsUnknown;
 #elif defined(OS_ANDROID)
