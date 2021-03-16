d7ae6e68dbb756c2b812b120d79515a8c9e680ce
Anton Bikineev <bikineev@chromium.org>
2021-03-16 19:32:12 +0000

    PCScan: Prepare PCScan infra for safepoints
    The CL does a bit of refactoring and preparation for mutator safepoints:
    1) Introduces new mutator scopes: clear and overall;
    2) Introduces the PCScan state variable, which will be needed for
    mutators to check whether safepoint can be entered;
    3) Makes scope counters atomic;
    4) Moves survived_qurantined_size and swept_size to StatsCollector;
    5) Moves figuring out SIMD support out of the scanning routine to avoid
    reentrant allocations;
    6) Removes filtering out super pages that have empty quarantine tables,
    since it's no longer needed with the CardTable;
    7) Moves collector scopes out of ScanPartition, ClearQuarantine and
    SweepQuarantine functions;
    
    Bug: 1129751
    Change-Id: I779ae32f512d2d5a80039492f3084236a76f4c70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2764363
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863400}

--- base/allocator/partition_allocator/pcscan.h
+++ base/allocator/partition_allocator/pcscan.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace internal {
 
+class PCScanTask;
+
 [[noreturn]] BASE_EXPORT NOINLINE NOT_TAIL_CALLED void DoubleFreeAttempt();
 
 // PCScan (Probabilistic Conservative Scanning) is the algorithm that eliminates
@@ -75,9 +77,7 @@ class BASE_EXPORT PCScan final {
   void PerformScanIfNeeded(InvocationMode invocation_mode);
 
   // Checks if there is a PCScan task currently in progress.
-  ALWAYS_INLINE bool IsInProgress() const {
-    return in_progress_.load(std::memory_order_relaxed);
-  }
+  ALWAYS_INLINE bool IsInProgress() const;
 
   // Sets process name (used for histograms). |name| must be a string literal.
   void SetProcessName(const char* name);
@@ -85,8 +85,8 @@ class BASE_EXPORT PCScan final {
   void ClearRootsForTesting();
 
  private:
-  class PCScanTask;
   class PCScanThread;
+  friend class PCScanTask;
   friend class PCScanTest;
 
   class QuarantineData final {
@@ -118,6 +118,17 @@ class BASE_EXPORT PCScan final {
     size_t last_size_ = 0;
   };
 
+  enum class State : uint8_t {
+    // PCScan task is not scheduled.
+    kNotRunning,
+    // PCScan task is being started and about to be scheduled.
+    kScheduled,
+    // PCScan task is scheduled and can be scanning (or clearing).
+    kScanning,
+    // PCScan task is sweeping or finalizing.
+    kSweepingAndFinishing
+  };
+
   inline constexpr PCScan();
 
   // Performs scanning unconditionally.
@@ -127,7 +138,7 @@ class BASE_EXPORT PCScan final {
   static PCScan instance_ PA_CONSTINIT;
 
   QuarantineData quarantine_data_{};
-  std::atomic<bool> in_progress_{false};
+  std::atomic<State> state_{State::kNotRunning};
 };
 
 // To please Chromium's clang plugin.
@@ -141,6 +152,10 @@ ALWAYS_INLINE bool PCScan::QuarantineData::Account(size_t size) {
 // To please Chromium's clang plugin.
 constexpr PCScan::PCScan() = default;
 
+ALWAYS_INLINE bool PCScan::IsInProgress() const {
+  return state_.load(std::memory_order_relaxed) != State::kNotRunning;
+}
+
 ALWAYS_INLINE void PCScan::MoveToQuarantine(void* ptr, size_t slot_size) {
   auto* quarantine = QuarantineBitmapFromPointer(QuarantineBitmapType::kMutator,
                                                  quarantine_data_.epoch(), ptr);
@@ -152,7 +167,7 @@ ALWAYS_INLINE void PCScan::MoveToQuarantine(void* ptr, size_t slot_size) {
   const bool is_limit_reached = quarantine_data_.Account(slot_size);
   if (UNLIKELY(is_limit_reached)) {
     // Perform a quick check if another scan is already in progress.
-    if (in_progress_.load(std::memory_order_relaxed))
+    if (IsInProgress())
       return;
     // Avoid blocking the current thread for regular scans.
     PerformScan(InvocationMode::kNonBlocking);
