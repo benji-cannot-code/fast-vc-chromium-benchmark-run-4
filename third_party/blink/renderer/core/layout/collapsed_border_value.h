6ca823e3a6dfdce7db27606324ccc3c634161c98
Fredrik Söderqvist <fs@opera.com>
2021-03-16 16:03:03 +0000

    Reduce BorderValue <-> border-width impedance mismatch
    BorderValue stores a limited (reduced precision) version of a
    LayoutUnit. Construction of a BorderValue however requires a float,
    which means that - since border-widths are stored as LayoutUnit (in
    ComputedStyle) - we do LayoutUnit -> float -> (reduced) LayoutUnit.
    
    Instead make the BorderValue constructor take a LayoutUnit and simplify
    the clamping code based on that. Encapsulate BorderValue a little more
    by exposing the max value as a LayoutUnit and make the constants related
    to the representation private.
    
    Since the related class CollapsedBorderValue use a similar storage
    model, apply similar logic to it as well. Also make it use constructor
    delegation to avoid duplicating some of its construction logic.
    
    Change-Id: I1866a89cab38fd1c132a3fe0ba35dba7fdfb2a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757471
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#863306}

--- third_party/blink/renderer/core/layout/collapsed_border_value.h
+++ third_party/blink/renderer/core/layout/collapsed_border_value.h
@@ -28,14 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/style/border_value.h"
-#include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/platform/geometry/layout_rect.h"
 #include "third_party/blink/renderer/platform/graphics/color.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
 namespace blink {
-
-class ComputedStyle;
 
 enum EBorderPrecedence {
   kBorderPrecedenceOff,
@@ -62,7 +59,7 @@ class CORE_EXPORT CollapsedBorderValue {
                        const Color&,
                        EBorderPrecedence);
   CollapsedBorderValue(EBorderStyle style,
-                       const float width,
+                       const LayoutUnit width,
                        const Color&,
                        EBorderPrecedence);
 
