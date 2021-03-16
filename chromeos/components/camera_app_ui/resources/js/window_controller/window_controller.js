fee4b59a3d7dbd775854fdc88b15c4914e223526
Wei Lee <wtlee@google.com>
2021-03-16 05:33:04 +0000

    [CCA] Remove deprecated JS modules
    We can remove deprecated modules once the usage in tests are already
    removed. (https://crrev.com/c/2742258)
    
    Bug: b/172343409
    Test: Build successfully
    Change-Id: I06d69ba8b0439b9d0ff75c4bc0854f6a5ac31fe6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2742259
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863155}

--- chromeos/components/camera_app_ui/resources/js/window_controller/window_controller.js
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2021 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// This file should be removed once the usage in tests are updated.
-
-export {windowController} from '../window_controller.js';
