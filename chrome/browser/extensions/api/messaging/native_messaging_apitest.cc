4b8879856a6e356ffe9a7e7714e39ef8f72cecff
Joe Downing <joedow@chromium.org>
2021-03-16 18:22:08 +0000

    Revert "Implement built-in NativeMessageHost support for Lacros"
    This reverts commit 0a9f40b91643900de956abc381a40e9a91f44bda.
    
    Reason for revert: Breaks Lacros codesearch bots -> crbug.com/1188541
    
    Original change's description:
    > Implement built-in NativeMessageHost support for Lacros
    >
    > Per previous discussions, it looks like we will want to support built-in
    > NativeMessageHosts in Lacros (such as the CRD remote support host).
    > I'm working on implementing a Lacros-friendly version of the CRD host
    > and the first step is getting the extensions API code updated so the
    > website client can request a host instance.
    >
    > While looking through the extensions API code, I found that it was being
    > configured the same was as the Linux browser.  I think this is due to
    > the is_chromeos_{ash|lacros} bifurcation work and not a conscious
    > decision as there isn't, AFAIK, a way to install an arbitrary NMH on
    > Lacros.
    >
    > This CL updates the extensions API to mimic the version which is built
    > into the monolithic chrome binary for chromeos which uses a hard-coded
    > registry of built-in NMHs which can be instantiated.
    >
    > I've included an empty CRD NMH impl to demonstrate usage and to build on
    > in future CLs.
    >
    > Bug: 1181824
    > Change-Id: I9093a801631f11382ad4e2dcc057fb3162a7d14e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713941
    > Commit-Queue: Joe Downing <joedow@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Tommy Martino <tmartino@chromium.org>
    > Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#863092}
    
    Bug: 1181824
    Change-Id: Ib9812441cadbe1af5a4c7ef3a225dbf2f9accf09
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2765224
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#863366}

--- chrome/browser/extensions/api/messaging/native_messaging_apitest.cc
+++ chrome/browser/extensions/api/messaging/native_messaging_apitest.cc
@@ -84,7 +84,7 @@ IN_PROC_BROWSER_TEST_P(NativeMessagingApiTest,
   ASSERT_TRUE(RunTest("native_messaging_connect")) << message_;
 }
 
-#if !BUILDFLAG(IS_CHROMEOS_ASH) && !BUILDFLAG(IS_CHROMEOS_LACROS)
+#if !BUILDFLAG(IS_CHROMEOS_ASH)
 
 class TestProcessManagerObserver : public ProcessManagerObserver {
  public:
@@ -438,7 +438,7 @@ IN_PROC_BROWSER_TEST_F(NativeMessagingLaunchBackgroundModeApiTest,
   ASSERT_NO_FATAL_FAILURE(TestKeepAliveStateObserver().WaitForNoKeepAlive());
 }
 
-#endif  // !BUILDFLAG(IS_CHROMEOS_ASH) && !BUILDFLAG(IS_CHROMEOS_LACROS)
+#endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
 
 }  // namespace
 }  // namespace extensions
