f52b905fef25e975e227281ffa234d7ed7cb2af5
Philipp Hancke <philipp.hancke@googlemail.com>
2021-03-16 19:57:50 +0000

    show insertableStreams in webrtc-internals page
    insertableStreams is a chrome feature so not part of the
    WebRTC RTCConfiguration. Note: the serialization format
    of the configuration is not true JSON as it has string
    values without quotes.
    
    BUG=None
    
    Change-Id: I46147d5947413df7281df9adb446c47cc2cd3b08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2760439
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#863418}

--- third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.cc
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.cc
@@ -324,11 +324,12 @@ String SerializeSdpSemantics(webrtc::SdpSemantics sdp_semantics) {
     default:
       NOTREACHED();
   }
-  return sdp_semantics_str;
+  return "\"" + sdp_semantics_str + "\"";
 }
 
 String SerializeConfiguration(
-    const webrtc::PeerConnectionInterface::RTCConfiguration& config) {
+    const webrtc::PeerConnectionInterface::RTCConfiguration& config,
+    bool usesInsertableStreams) {
   StringBuilder result;
   // TODO(hbos): Add serialization of certificate.
   result.Append("{ iceServers: ");
@@ -341,9 +342,12 @@ String SerializeConfiguration(
   result.Append(SerializeRtcpMuxPolicy(config.rtcp_mux_policy));
   result.Append(", iceCandidatePoolSize: ");
   result.AppendNumber(config.ice_candidate_pool_size);
-  result.Append(", sdpSemantics: \"");
+  result.Append(", sdpSemantics: ");
   result.Append(SerializeSdpSemantics(config.sdp_semantics));
-  result.Append("\" }");
+  if (usesInsertableStreams) {
+    result.Append(", encodedInsertableStreams: true");
+  }
+  result.Append(" }");
   return result.ToString();
 }
 
@@ -835,7 +839,11 @@ void PeerConnectionTracker::RegisterPeerConnection(
   auto info = blink::mojom::blink::PeerConnectionInfo::New();
 
   info->lid = GetNextLocalID();
-  info->rtc_configuration = SerializeConfiguration(config);
+  bool usesInsertableStreams =
+      pc_handler->force_encoded_audio_insertable_streams() &&
+      pc_handler->force_encoded_video_insertable_streams();
+  info->rtc_configuration =
+      SerializeConfiguration(config, usesInsertableStreams);
 
   info->constraints = SerializeMediaConstraints(constraints);
   if (frame)
@@ -951,8 +959,12 @@ void PeerConnectionTracker::TrackSetConfiguration(
   if (id == -1)
     return;
 
-  SendPeerConnectionUpdate(id, "setConfiguration",
-                           SerializeConfiguration(config));
+  bool usesInsertableStreams =
+      pc_handler->force_encoded_audio_insertable_streams() &&
+      pc_handler->force_encoded_video_insertable_streams();
+  SendPeerConnectionUpdate(
+      id, "setConfiguration",
+      SerializeConfiguration(config, usesInsertableStreams));
 }
 
 void PeerConnectionTracker::TrackAddIceCandidate(
