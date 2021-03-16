2ef6d78c90dd849cf63204a8fcddaa9b5db0fe78
Azeem Arshad <azeemarshad@chromium.org>
2021-03-16 18:26:29 +0000

    [CrOS Settings] Remove unnecessary error logs in internet detail page.
    This CL removes error logs for a case that's expected to
    occur in practice and is being handled accordingly.
    
    There are a few other similar error logs in internet
    detail page. These are not removed since they seem to
    be logging real error conditions.
    
    Fixed: 1188329
    Change-Id: I7a47d167726031260698796bebb7d25acc0c9029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2762404
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863372}

--- chrome/browser/resources/settings/chromeos/internet_page/internet_detail_page.js
+++ chrome/browser/resources/settings/chromeos/internet_page/internet_detail_page.js
@@ -794,7 +794,7 @@ Polymer({
     }
 
     if (!properties) {
-      console.error('Details page: GUID no longer exists: ' + this.guid);
+      // Close the page if the network was removed and no longer exists.
       this.close();
       return;
     }
