3e06e2c693c347769e3fbab54bbc5c9afc40d284
Caroline Rising <corising@chromium.org>
2021-03-16 15:35:59 +0000

    Tab groups: make the group editor bubble offset the arrow if the preferred anchoring is outside window bounds.
    By default, when a bubble is set to adjust if offscreen it will be
    mirrored. This cl sets the tab group editor bubble to offset instead
    which will keep it flush with the edge of the window until there is
    space for it to fit in the correct anchored location.
    
    Bug: 1169566
    Change-Id: I44251ae9cde14575b1f91112742fcfb5b46ffc39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2762179
    Reviewed-by: Charlene Yan <cyan@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863286}

--- chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/range/range.h"
 #include "ui/native_theme/native_theme.h"
+#include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/controls/label.h"
@@ -69,6 +70,9 @@ views::Widget* TabGroupEditorBubbleView::Show(
   views::Widget* const widget =
       BubbleDialogDelegateView::CreateBubble(tab_group_editor_bubble_view);
   tab_group_editor_bubble_view->set_adjust_if_offscreen(true);
+  tab_group_editor_bubble_view->GetBubbleFrameView()
+      ->SetPreferredArrowAdjustment(
+          views::BubbleFrameView::PreferredArrowAdjustment::kOffset);
   tab_group_editor_bubble_view->SizeToContents();
   widget->Show();
   return widget;
