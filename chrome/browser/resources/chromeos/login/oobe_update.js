61d5c76e9e66eb7e1b648a9baac8cabdddc26240
Renato Silva <rrsilva@google.com>
2021-03-16 14:26:21 +0000

    OOBE - Serve behaviors separately
    Serve LoginScreenBehavior and MultiStepBehavior under a dedicated URL.
    Additionally add the steps to build the modularized version of these
    files and closure compilation rules.
    
    
    Bug: 1184731
    Change-Id: Idbb6651ebf30e9a1df62039466ebf884e727ede9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2742714
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/master@{#863263}

--- chrome/browser/resources/chromeos/login/oobe_update.js
+++ chrome/browser/resources/chromeos/login/oobe_update.js
@@ -40,7 +40,11 @@ const UIState = {
 Polymer({
   is: 'oobe-update-element',
 
-  behaviors: [OobeI18nBehavior, MultiStepBehavior, LoginScreenBehavior],
+  behaviors: [
+    OobeI18nBehavior,
+    MultiStepBehavior,
+    LoginScreenBehavior
+  ],
 
   EXTERNAL_API: [
     'setCancelUpdateShortcutEnabled',
