7bece9ca765eb99bbc90cd2165e2f01a85e5405b
Gayane Petrosyan <gayane@chromium.org>
2021-03-16 23:58:57 +0000

    Timeout task should run only if selector is requested.
    For blocklist and iframe cases selector is not requested, hence the
    remote is not connected. Therefore when timeout tasks runs and tries
    to cancel the remote it crashes.
    
    Bug: 1188564
    Change-Id: I0be6610d03545af09d6483f4b902fd520efa1ba8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2765903
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863541}

--- chrome/browser/renderer_context_menu/copy_link_to_text_menu_observer.cc
+++ chrome/browser/renderer_context_menu/copy_link_to_text_menu_observer.cc
@@ -64,14 +64,8 @@ void CopyLinkToTextMenuObserver::InitMenu(
       IDC_CONTENT_CONTEXT_COPYLINKTOTEXT,
       l10n_util::GetStringUTF16(IDS_CONTENT_CONTEXT_COPYLINKTOTEXT));
 
-  if (ShouldPreemptivelyGenerateLink()) {
+  if (ShouldPreemptivelyGenerateLink())
     RequestLinkGeneration();
-    base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
-        FROM_HERE,
-        base::BindOnce(&CopyLinkToTextMenuObserver::Timeout,
-                       weak_ptr_factory_.GetWeakPtr()),
-        kTimeoutMs);
-  }
 }
 
 bool CopyLinkToTextMenuObserver::IsCommandIdSupported(int command_id) {
@@ -171,6 +165,11 @@ void CopyLinkToTextMenuObserver::RequestLinkGeneration() {
   remote_->RequestSelector(base::BindOnce(
       &CopyLinkToTextMenuObserver::OnRequestLinkGenerationCompleted,
       weak_ptr_factory_.GetWeakPtr()));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&CopyLinkToTextMenuObserver::Timeout,
+                     weak_ptr_factory_.GetWeakPtr()),
+      kTimeoutMs);
 }
 
 void CopyLinkToTextMenuObserver::CopyLinkToClipboard() {
@@ -181,6 +180,8 @@ void CopyLinkToTextMenuObserver::CopyLinkToClipboard() {
 
 void CopyLinkToTextMenuObserver::Timeout() {
   DCHECK(ShouldPreemptivelyGenerateLink());
+  DCHECK(remote_.is_bound());
+  DCHECK(remote_.is_connected());
   if (generated_link_.has_value())
     return;
   remote_->Cancel();
