d407f4f09570b599d7a8cdeb5535f021528d8a6b
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2021-03-16 08:31:14 +0000

    Remove alert behavior from MessageBoxView.
    MessageBoxView is a reusable component found inside other dialogs, it
    may contain a message, prompt and buttons.
    
    It exposed the alert role and emitted an alert event, but that does
    not seem necessary, since it is meant to be contained in other
    dialogs that may or may not expose alert behavior.
    
    What's more, this seemed to be confusing some ATs, which ignored the
    contents of the message box when announcing the parent dialog.
    
    Removing the alert role and event from the message box fixes the
    missing announcement of dialog contents in JavaScript alert and
    onbeforeunload dialogs, on Windows/NVDA.
    
    Bug: 779501, 1164927, 1166370
    Change-Id: I59003b538f27eec9230f55141ba2c7265f944923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2726236
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Jacobo Aragunde Pérez <jaragunde@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#863193}

--- ui/views/controls/message_box_view.h
+++ ui/views/controls/message_box_view.h
@@ -72,9 +72,6 @@ class VIEWS_EXPORT MessageBoxView : public View {
   // Sets the text and the callback of the link. |text| must be non-empty.
   void SetLink(const std::u16string& text, Link::ClickedCallback callback);
 
-  // View:
-  void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
-
   void SetInterRowVerticalSpacing(int spacing);
   void SetMessageWidth(int width);
 
