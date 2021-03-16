e226004fef01292fd233ebaa2fb56526c1a7aa81
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2021-03-16 18:36:47 +0000

    Tweak CaptionControllerBrowserTest for DestroyProfileOnBrowserClose
    Some changes in ProfileManager require tweaking this test, so it
    passes when DestroyProfileOnBrowserClose is enabled.
    
    Bug: 88586
    Change-Id: If3718301cc3ee43ad3bf5f21c679d09be3624ad3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2762177
    Auto-Submit: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#863375}

--- chrome/browser/accessibility/caption_controller_browsertest.cc
+++ chrome/browser/accessibility/caption_controller_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/ranges/ranges.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/accessibility/caption_controller_factory.h"
@@ -14,8 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_keep_alive_types.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_window.h"
+#include "chrome/browser/profiles/scoped_profile_keep_alive.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/caption_bubble_controller.h"
@@ -321,6 +324,7 @@ IN_PROC_BROWSER_TEST_F(CaptionControllerTest, OnBrowserRemoved) {
     //
     // TODO(crbug.com/88586): Remove the other branch once
     // DestroyProfileOnBrowserClose becomes the default.
+    base::RunLoop().RunUntilIdle();
     std::vector<Profile*> loaded_profiles =
         g_browser_process->profile_manager()->GetLoadedProfiles();
     auto it = base::ranges::find(loaded_profiles, profile);
@@ -591,6 +595,13 @@ IN_PROC_BROWSER_TEST_F(CaptionControllerTest,
   Browser* browser2 = CreateBrowser(profile2);
   CaptionController* controller1 = GetControllerForProfile(profile1);
   CaptionController* controller2 = GetControllerForProfile(profile2);
+
+  // TODO(crbug.com/88586): Remove this test when the
+  // DestroyProfileOnBrowserClose flag is removed.
+  ScopedProfileKeepAlive profile1_keep_alive(
+      profile1, ProfileKeepAliveOrigin::kBrowserWindow);
+  ScopedProfileKeepAlive profile2_keep_alive(
+      profile2, ProfileKeepAliveOrigin::kBrowserWindow);
 
   // Enable live caption on both profiles.
   SetLiveCaptionEnabled(true);
