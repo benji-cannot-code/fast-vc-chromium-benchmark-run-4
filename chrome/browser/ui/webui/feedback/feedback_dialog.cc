0b7ee26fa95bc5c8546798173a64b9ab5f5e9f66
Xiangdong Kong <Xiangdongkong@google.com>
2021-03-17 18:06:12 +0000

    [feedback] use WebDialog's window frame
    The Feedback WebUI App now uses the title, minimize button, and close
    button of the web dialog.
    
    Bug: 1167223
    Change-Id: Ie29516c770fd0d23410213b75b1aa4bb0952b942
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2765147
    Auto-Submit: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/master@{#863856}

--- chrome/browser/ui/webui/feedback/feedback_dialog.cc
+++ chrome/browser/ui/webui/feedback/feedback_dialog.cc
@@ -106,11 +106,11 @@ void FeedbackDialog::OnCloseContents(WebContents* source,
 }
 
 bool FeedbackDialog::ShouldShowDialogTitle() const {
-  return false;
+  return true;
 }
 
 bool FeedbackDialog::ShouldShowCloseButton() const {
-  return false;
+  return true;
 }
 
 ui::WebDialogDelegate::FrameKind FeedbackDialog::GetWebDialogFrameKind() const {
