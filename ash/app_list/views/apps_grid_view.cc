9dec949b2030eb6009b1eda758b5e2723eda640d
Ana Salazar <anasalazar@chromium.org>
2021-03-17 19:34:04 +0000

    [drag-spec]: Fix item view state after exiting folder
    When the apps grid view detects a drag into one item, if it is not a
    folder, we set the state as UI_STATE_DROPPING_IN_FOLDER to give visual
    affordance of an attempted folder. If the user drags the item outside
    of the attempted folder, the folder should return to either
    UI_STATE_NORMAL with the correct scale depending on the state it should
    be handling if the apps grid view is in cardified state.
    
    Bug: 1114817
    Change-Id: I06ed8064bc83278b9972e2480af5a0746887f420
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757834
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863906}

--- ash/app_list/views/apps_grid_view.cc
+++ ash/app_list/views/apps_grid_view.cc
@@ -884,7 +884,7 @@ void AppsGridView::EndDrag(bool cancel) {
   if (cardified_state_) {
     // Temporarily set to cardified UI State so it animates back to its position
     // smoothly with all other icons.
-    released_drag_view->SetCardifyUIState();
+    released_drag_view->EnterCardifyState();
     // Compensate drag_source_bounds for the translation of the items_container
     // during AnimateCardifiedState().
     gfx::Point start_position = items_container_->origin();
@@ -2350,9 +2350,9 @@ void AppsGridView::AnimateCardifiedState() {
     current_bounds.Offset(translate_offset);
 
     if (cardified_state_)
-      entry_view->SetCardifyUIState();
+      entry_view->EnterCardifyState();
     else
-      entry_view->SetNormalUIState();
+      entry_view->ExitCardifyState();
 
     gfx::Rect target_bounds(view_model_.ideal_bounds(i));
     entry_view->SetBoundsRect(target_bounds);
