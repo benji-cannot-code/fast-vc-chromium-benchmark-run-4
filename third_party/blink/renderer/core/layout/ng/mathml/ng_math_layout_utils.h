62dda3e86917cf13541417528bf7cb4ce8bc7649
Ian Kilpatrick <ikilpatrick@chromium.org>
2021-03-17 19:40:13 +0000

    [layout] Introduce ComputeMinAndMaxContentContributionForMathChild
    As above. There should be no behaviour change.
    
    Change-Id: Ic72cd98cab2b40ff6fce003f28c694882d4adf61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2762165
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863913}

--- third_party/blink/renderer/core/layout/ng/mathml/ng_math_layout_utils.h
+++ third_party/blink/renderer/core/layout/ng/mathml/ng_math_layout_utils.h
@@ -13,6 +13,7 @@ namespace blink {
 
 struct LogicalSize;
 struct MinMaxSizes;
+struct MinMaxSizesResult;
 class NGBlockNode;
 class NGConstraintSpace;
 class NGLayoutInputNode;
@@ -24,6 +25,12 @@ NGConstraintSpace CreateConstraintSpaceForMathChild(
     const LogicalSize& child_available_size,
     const NGConstraintSpace& parent_constraint_space,
     const NGLayoutInputNode&);
+
+MinMaxSizesResult ComputeMinAndMaxContentContributionForMathChild(
+    const ComputedStyle& parent_style,
+    const NGConstraintSpace& parent_constraint_space,
+    const NGBlockNode& child,
+    LayoutUnit child_available_block_size);
 
 NGLayoutInputNode FirstChildInFlow(const NGBlockNode&);
 NGLayoutInputNode NextSiblingInFlow(const NGBlockNode&);
