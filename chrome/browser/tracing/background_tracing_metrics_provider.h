3e292a2509857343f34e66047c354347deb1368e
ssid <ssid@chromium.org>
2021-03-17 10:20:31 +0000

    Add cpu and gpu system profile to trace logs
    Bug: b/165403997
    Change-Id: I367a9f9037ec56899adbfb517efa94e8f5f0784f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2762612
    Commit-Queue: ssid <ssid@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863691}

--- chrome/browser/tracing/background_tracing_metrics_provider.h
+++ chrome/browser/tracing/background_tracing_metrics_provider.h
@@ -7,13 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_TRACING_BACKGROUND_TRACING_METRICS_PROVIDER_H_
 
 #include <memory>
+#include <vector>
 
 #include "build/build_config.h"
 #include "components/metrics/metrics_provider.h"
-
-#if defined(OS_WIN)
-#include "chrome/browser/metrics/antivirus_metrics_provider_win.h"
-#endif  // defined(OS_WIN)
 
 namespace tracing {
 
@@ -43,9 +40,13 @@ class BackgroundTracingMetricsProvider : public metrics::MetricsProvider {
       base::HistogramSnapshotManager* snapshot_manager) override;
 
  private:
+  std::vector<std::unique_ptr<metrics::MetricsProvider>>
+      system_profile_providers_;
 #if defined(OS_WIN)
-  std::unique_ptr<AntiVirusMetricsProvider> av_metrics_provider_;
-#endif  // defined(OS_WIN)
+  // owned by |system_profile_providers_|.
+  MetricsProvider* av_metrics_provider_ = nullptr;
+#endif
+
   DISALLOW_COPY_AND_ASSIGN(BackgroundTracingMetricsProvider);
 };
 
