0f99a69ee50e52d8eb2978c16aff657f485e6e25
Tim Sergeant <tsergeant@chromium.org>
2021-03-17 03:59:50 +0000

    Web Apps: Allow external web apps to be placed in the OEM folder
    This adds a new JSON field ("oem_installed") to the external web apps
    structure on Chrome OS, which is used to indicate that an app was
    installed by the device OEM and should be placed in the OEM folder in
    the launcher.
    
    This mimics an existing feature available for OEM-installed Extensions
    and ARC apps.
    
    BUG=1184576
    
    Change-Id: If30adfc403c9a710a22ce0446b00264c5043930e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2758401
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863625}

--- chrome/browser/web_applications/extensions/bookmark_app_registrar.h
+++ chrome/browser/web_applications/extensions/bookmark_app_registrar.h
@@ -39,6 +39,7 @@ class BookmarkAppRegistrar : public web_app::AppRegistrar,
   bool IsInstalled(const web_app::AppId& app_id) const override;
   bool IsLocallyInstalled(const web_app::AppId& app_id) const override;
   bool WasInstalledByUser(const web_app::AppId& app_id) const override;
+  bool WasInstalledByOem(const web_app::AppId& app_id) const override;
   int CountUserInstalledApps() const override;
   std::string GetAppShortName(const web_app::AppId& app_id) const override;
   std::string GetAppDescription(const web_app::AppId& app_id) const override;
