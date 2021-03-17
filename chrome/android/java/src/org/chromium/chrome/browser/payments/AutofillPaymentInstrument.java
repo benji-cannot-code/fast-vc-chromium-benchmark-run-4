98be56e397309923a74c2ba502591dbe4100cb9a
Brandon Wylie <wylieb@chromium.org>
2021-03-17 00:01:38 +0000

    Remove ChromeActivity reference from AutofillPaymentInstrument
    Bug: 1155788
    Change-Id: I70c112ff0e476e3b729fc5847056c9fe9d1817ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2733097
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863544}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/AutofillPaymentInstrument.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/AutofillPaymentInstrument.java
@@ -13,8 +13,8 @@ import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 import androidx.appcompat.content.res.AppCompatResources;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.autofill.PersonalDataManager;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.CreditCard;
@@ -96,7 +96,7 @@ public class AutofillPaymentInstrument
         mIsEditable = true;
         mMethodName = methodName;
 
-        Context context = ChromeActivity.fromWebContents(mWebContents);
+        Context context = ContextUtils.getApplicationContext();
         if (context == null) return;
 
         if (card.getIssuerIconDrawableId() != 0) {
@@ -327,7 +327,7 @@ public class AutofillPaymentInstrument
         mMethodName = methodName;
         mBillingAddress = billingAddress;
 
-        Context context = ChromeActivity.fromWebContents(mWebContents);
+        Context context = ContextUtils.getApplicationContext();
         if (context == null) return;
 
         updateIdentifierLabelsAndIcon(card.getGUID(), card.getObfuscatedNumber(), card.getName(),
