c085868f45ebdd9350b0f056bf127b3c6760154c
Dominik Röttsches <drott@chromium.org>
2021-03-17 08:43:56 +0000

    Record emoji glyph run success/brokenness
    FontMatchingMetrics thankfully already implements the logic for
    recording font related statistics for the lifetime of a Document or
    WorkerGlobalContext. Borrow this infrastructure for tracking during
    shaping what the percentage of successfully shaped to unsuccessfully
    shape emoji clusters is, based on the assumption that each emoji and
    emoji sequence should show as one non-tofu glyph.
    
    Needed for a limited time for tracking whether switching to retrieving
    the emoji font "Noto Color Emoji" from GMSCore (see issue 1142444) moves
    the needle and can increase the number of successfully shaped emoji and
    lead to better glyph coverage.
    
    Bug: 1186694
    Change-Id: Id821b00fbb44363c9d072a4bd0ef9c9c5980f6aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2756276
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863681}

--- third_party/blink/renderer/platform/testing/font_test_helpers.cc
+++ third_party/blink/renderer/platform/testing/font_test_helpers.cc
@@ -57,7 +57,6 @@ class TestFontSelector : public FontSelector {
 
   unsigned Version() const override { return 0; }
   void FontCacheInvalidated() override {}
-  void ReportNotDefGlyph() const override {}
   void ReportSuccessfulFontFamilyMatch(
       const AtomicString& font_family_name) override {}
   void ReportFailedFontFamilyMatch(
@@ -81,6 +80,8 @@ class TestFontSelector : public FontSelector {
   void ReportLastResortFallbackFontLookup(
       const FontDescription& font_description,
       SimpleFontData* resulting_font_data) override {}
+  void ReportNotDefGlyph() const override {}
+  void ReportEmojiSegmentGlyphCoverage(unsigned, unsigned) override {}
   ExecutionContext* GetExecutionContext() const override { return nullptr; }
   FontFaceCache* GetFontFaceCache() override { return nullptr; }
 
