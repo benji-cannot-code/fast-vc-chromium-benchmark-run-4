501933f564f810723de6b0c904bb63404b305c2b
Kent Tamura <tkent@chromium.org>
2021-03-17 06:16:07 +0000

    Text Control NG: Fix a regression of placeholder position for input[type=number]
    For text controls with decoration elements such as input[type=number]
    and input[type=search], placeholder box width should be same as
    editing-view-port's width.
    
    This CL ports the following line in |LayoutTextControlSingleLine::
    UpdateLayout()|:
      placeholder_box->SetOverrideLogicalWidth(inner_editor_logical_width);
    
    Bug: 1187471
    Change-Id: I983a2071a2e202e418a1281d78c3abd2266c2b60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2763403
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863658}

--- third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
@@ -177,6 +177,7 @@ class CORE_EXPORT NGPhysicalFragment
            !layout_object_->IsTableCellLegacy();
   }
 
+  bool IsTextControlContainer() const;
   bool IsTextControlPlaceholder() const;
 
   // Return true if this fragment is a container established by a fieldset
