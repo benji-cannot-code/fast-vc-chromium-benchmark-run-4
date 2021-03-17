a4c9f51459f100c3738180cc6dd59deb9665d645
Tim van der Lippe <tvanderlippe@chromium.org>
2021-03-17 17:21:59 +0000

    Add missing load calls for browser_debugger
    These tests were relying on the BrowserDebugger global to exist, but
    weren't loading the module.
    
    R=aerotwist@chromium.org
    
    Bug: 1187573
    Change-Id: Iee6ebdbd5e07f97f46311951b2cfa0d6bdfb92c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2767226
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863827}

--- third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/sources/event-listener-breakpoints-a11y-test.js
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/sources/event-listener-breakpoints-a11y-test.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function () {
   await TestRunner.loadModule('axe_core_test_runner');
   await TestRunner.showPanel('sources');
+  await TestRunner.loadLegacyModule('browser_debugger');
 
   await UI.viewManager.showView('sources.eventListenerBreakpoints');
   const eventListenerWidget = self.runtime.sharedInstance(
--- third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/sources/xhr-breakpoints-sidebar-a11y-test.js
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/sources/xhr-breakpoints-sidebar-a11y-test.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function () {
   await TestRunner.loadModule('axe_core_test_runner');
   await TestRunner.showPanel('sources');
+  await TestRunner.loadLegacyModule('browser_debugger');
 
   // this rule causes false negatives due to axe not handling the shadow DOM properly
   const noRequiredParent = {'aria-required-parent': {enabled: false}};
