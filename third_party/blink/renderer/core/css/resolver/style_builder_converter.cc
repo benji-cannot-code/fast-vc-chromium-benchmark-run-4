e14d2cca63f2acd5ed7015bf35df4739cf6e965a
Rune Lillesveen <futhark@chromium.org>
2021-03-17 13:35:06 +0000

    Use light color-scheme constant instead of via InitialStyle
    In preparation for removing the InitialStyle() singleton from
    ComputedStyle.
    
    Use actually used color-scheme in test.
    
    Bug: 929098
    Change-Id: I132c2d98ed826bfeee079a75af898a434b5da25e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2764616
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863739}

--- third_party/blink/renderer/core/css/resolver/style_builder_converter.cc
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.cc
@@ -1416,7 +1416,7 @@ ShadowData StyleBuilderConverter::ConvertShadow(
             break;
           default:
             color = StyleColor(StyleColor::ColorFromKeyword(
-                value_id, ComputedStyle::InitialStyle().UsedColorScheme()));
+                value_id, mojom::blink::ColorScheme::kLight));
         }
       }
     }
