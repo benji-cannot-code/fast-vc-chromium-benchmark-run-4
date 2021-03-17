f15f1f4dc839be06839554103c13d7d43fe6275c
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2021-03-17 03:07:00 +0000

    Revert "[Switch Access] Change Point Scanning Speed based on pref"
    This reverts commit 693b1b0f80aea32b134022e622ef5c11fe63017b.
    
    Reason for revert: 
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 863344 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzY5M2IxYjBmODBhZWEzMmIxMzQwMjJlNjIyZWY1YzExZmU2MzAxN2IM
    
    Sample Failed Build: https://ci.chromium.org/b/8852564450228368240
    
    Sample Failed Step: browser_tests
    
    Original change's description:
    > [Switch Access] Change Point Scanning Speed based on pref
    >
    > Allow Switch Access Point Scanning Speed to be determined by pref
    > (which will be later be controllable via slider in Chrome OS
    > Switch Access Settings)
    >
    > AX-Relnotes: N/A
    > Fixed: 1181913
    > Change-Id: I726acab9cc54b5d3dc45458019edb4c2a24598de
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2717874
    > Reviewed-by: Josiah Krutz <josiahk@google.com>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    > Commit-Queue: Josiah Krutz <josiahk@google.com>
    > Cr-Commit-Position: refs/heads/master@{#863344}
    
    Change-Id: I9a15ae1a3f104c83614ad4c635b75c7614ad123b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2764086
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Owners-Override: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#863613}

--- ash/accessibility/point_scan_controller.h
+++ ash/accessibility/point_scan_controller.h
@@ -27,8 +27,6 @@ class ASH_EXPORT PointScanController : public AccessibilityLayerDelegate {
   PointScanController& operator=(const PointScanController&) = delete;
 
   enum class PointScanState {
-    // Point scanning is not scanning.
-    kOff,
     // Point scanning is currently range scanning horizontally.
     kHorizontalRangeScanning,
     // Point scanning is currently scanning horizontally.
@@ -37,6 +35,8 @@ class ASH_EXPORT PointScanController : public AccessibilityLayerDelegate {
     kVerticalRangeScanning,
     // Point scanning is currently scanning vertically.
     kVerticalScanning,
+    // Point scanning is not scanning.
+    kOff,
   };
 
   // Starts point scanning, by sweeping a range across the screen and waiting
@@ -51,7 +51,6 @@ class ASH_EXPORT PointScanController : public AccessibilityLayerDelegate {
   void ResetAnimation();
   base::Optional<gfx::PointF> OnPointSelect();
   bool IsPointScanEnabled();
-  void SetSpeedDipsPerSecond(int speed_dips_per_second);
 
  private:
   // AccessibilityLayerDelegate implementation:
@@ -71,7 +70,7 @@ class ASH_EXPORT PointScanController : public AccessibilityLayerDelegate {
   PointScanLayerAnimationInfo vertical_line_layer_info_;
   std::unique_ptr<PointScanLayer> vertical_line_layer_;
 
-  PointScanState state_ = PointScanState::kOff;
+  PointScanState state_;
 };
 
 }  // namespace ash
