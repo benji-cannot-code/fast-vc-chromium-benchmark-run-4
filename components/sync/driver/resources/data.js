c3927d0768f142bde9cac0954040e7416d1c6798
rbpotter <rbpotter@chromium.org>
2021-03-17 05:58:47 +0000

    Sync internals: Migrate to JS modules
    - Migrate page to JS modules
    - Use FireWebUIListener and event listeners instead of calling a
      function and then re-dispatching all the events from a <div>.
    
    Bug: 1173575
    Change-Id: If553ef5dd588368aaedcbd37cce1b673b52c09ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2728322
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863655}

--- components/sync/driver/resources/data.js
+++ components/sync/driver/resources/data.js
@@ -3,7 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-(function() {
+import {addWebUIListener, removeWebUIListener} from 'chrome://resources/js/cr.m.js';
+import {$} from 'chrome://resources/js/util.m.js';
+
+import {aboutInfo} from './about.js';
+import {getAllNodes, requestIncludeSpecificsInitialState, requestListOfTypes} from './chrome_sync.js';
+import {log} from './sync_log.js';
+
 const dumpToTextButton = $('dump-to-text');
 const dataDump = $('data-dump');
 dumpToTextButton.addEventListener('click', function(event) {
@@ -13,21 +19,14 @@ dumpToTextButton.addEventListener('click', function(event) {
   data += '======\n';
   data += 'Status\n';
   data += '======\n';
-  data += JSON.stringify(chrome.sync.aboutInfo, null, 2);
-  data += '\n';
-  data += '\n';
-
-  data += '=============\n';
-  data += 'Notifications\n';
-  data += '=============\n';
-  data += JSON.stringify(chrome.sync.notifications, null, 2);
+  data += JSON.stringify(aboutInfo, null, 2);
   data += '\n';
   data += '\n';
 
   data += '===\n';
   data += 'Log\n';
   data += '===\n';
-  data += JSON.stringify(chrome.sync.log.entries, null, 2);
+  data += JSON.stringify(log.entries, null, 2);
   data += '\n';
 
   dataDump.textContent = data;
@@ -140,8 +139,8 @@ function triggerDataDownload(nodesMap) {
   let output = [makeDateUserAgentHeader()];
   output.push('=====');
 
-  const aboutInfo = JSON.stringify(chrome.sync.aboutInfo, null, 2);
-  output.push(aboutInfo);
+  const aboutInfoString = JSON.stringify(aboutInfo, null, 2);
+  output.push(aboutInfoString);
 
   // Filter out non-selected types.
   const selectedTypesNodes = nodesMap.filter(function(x) {
@@ -184,33 +183,31 @@ function createTypesCheckboxes(types) {
   });
 }
 
-function onReceivedListOfTypes(e) {
-  const types = e.details.types;
+let listOfTypesListener = null;
+
+function onReceivedListOfTypes(response) {
+  const types = response.types;
   types.sort();
   createTypesCheckboxes(types);
-  chrome.sync.events.removeEventListener(
-      'onReceivedListOfTypes',
-      onReceivedListOfTypes);
+  removeWebUIListener(listOfTypesListener);
 }
 
-function onReceivedIncludeSpecificsInitialState(e) {
-  $('capture-specifics').checked = e.details.includeSpecifics;
+function onReceivedIncludeSpecificsInitialState(response) {
+  $('capture-specifics').checked = response.includeSpecifics;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  chrome.sync.events.addEventListener(
-      'onReceivedListOfTypes',
-      onReceivedListOfTypes);
-  chrome.sync.requestListOfTypes();
+  listOfTypesListener =
+      addWebUIListener('onReceivedListOfTypes', onReceivedListOfTypes);
+  requestListOfTypes();
 
-  chrome.sync.events.addEventListener(
-    'onReceivedIncludeSpecificsInitialState',
-    onReceivedIncludeSpecificsInitialState);
-  chrome.sync.requestIncludeSpecificsInitialState();
+  addWebUIListener(
+      'onReceivedIncludeSpecificsInitialState',
+      onReceivedIncludeSpecificsInitialState);
+  requestIncludeSpecificsInitialState();
 });
 
 const dumpToFileLink = $('dump-to-file');
 dumpToFileLink.addEventListener('click', function(event) {
-  chrome.sync.getAllNodes(triggerDataDownload);
+  getAllNodes(triggerDataDownload);
 });
-})();
--- components/sync/driver/resources/user_events.js
+++ components/sync/driver/resources/user_events.js
@@ -3,23 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-cr.define('chrome.sync.user_events', function() {
-  function write() {
-    chrome.sync.writeUserEvent(
-      $('event-time-usec-input').value,
-      $('navigation-id-input').value);
-  }
+import {$} from 'chrome://resources/js/util.m.js';
+import {writeUserEvent} from './chrome_sync.js';
 
-  function onLoad() {
-    $('create-event-button').addEventListener('click', write);
-  }
+function write() {
+  writeUserEvent(
+      $('event-time-usec-input').value, $('navigation-id-input').value);
+}
 
-  return {
-    onLoad: onLoad
-  };
-});
-
-document.addEventListener(
-    'DOMContentLoaded',
-    chrome.sync.user_events.onLoad,
-    false);
+document.addEventListener('DOMContentLoaded', () => {
+  $('create-event-button').addEventListener('click', write);
+}, false);
