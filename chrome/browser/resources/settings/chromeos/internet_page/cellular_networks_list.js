4d64a15ebbb9e629662f93569a1718b070068b7c
Azeem Arshad <azeemarshad@chromium.org>
2021-03-17 02:22:19 +0000

    [CrOS Cellular] Remove managedProperties call in networks list
    This CL modifies cellular-networks-list component so that
    it uses eid from the network state object to differentiate
    eSIM vs pSIM networks. This avoids making an additional
    getManagedProperties call which could fail in certain cases
    e.g. if the SIM is locked.
    
    Fixed: 1188963
    Change-Id: I1c9603e7d85bc31d55270d68c21782ece3442a44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2765611
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863603}

--- chrome/browser/resources/settings/chromeos/internet_page/cellular_networks_list.js
+++ chrome/browser/resources/settings/chromeos/internet_page/cellular_networks_list.js
@@ -278,7 +278,7 @@ Polymer({
   /**
    * @private
    */
-  async onNetworksListChanged_() {
+  onNetworksListChanged_() {
     const mojom = chromeos.networkConfig.mojom;
 
     const pSimNetworks = [];
@@ -291,16 +291,7 @@ Polymer({
         continue;
       }
 
-      const managedPropertiesResponse =
-          await this.networkConfig_.getManagedProperties(network.guid);
-      if (!managedPropertiesResponse || !managedPropertiesResponse.result) {
-        console.error(
-            'Unable to get managed properties for network. guid=',
-            network.guid);
-        continue;
-      }
-
-      if (managedPropertiesResponse.result.typeProperties.cellular.eid) {
+      if (network.typeState.cellular && network.typeState.cellular.eid) {
         eSimNetworks.push(network);
       } else {
         pSimNetworks.push(network);
