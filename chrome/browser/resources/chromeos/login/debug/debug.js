e7f4f837ea5f5d0f89053d7bdeb0ee8a8601591f
Ossama Mahmoud <osamafathy@google.com>
2021-03-17 20:24:11 +0000

    OOBE: Fix A11y button placement on marketing-opt-in
    Bug: 1186353
    Change-Id: I52b6c858723ecd842c31ebfe0851b1d65382424e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2767302
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Ossama Mahmoud <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/master@{#863939}

--- chrome/browser/resources/chromeos/login/debug/debug.js
+++ chrome/browser/resources/chromeos/login/debug/debug.js
@@ -944,6 +944,9 @@ cr.define('cr.ui.login.debug', function() {
             optInDefaultState: true,
             legalFooterVisibility: false,
           },
+          trigger: (screen) => {
+            screen.updateA11ySettingsButtonVisibility(false);
+          },
         },
         {
           id: 'NoOptionToSubscribe',
@@ -952,6 +955,9 @@ cr.define('cr.ui.login.debug', function() {
             optInDefaultState: false,
             legalFooterVisibility: false,
           },
+          trigger: (screen) => {
+            screen.updateA11ySettingsButtonVisibility(false);
+          },
         },
         {
           id: 'WithLegalFooter',
@@ -959,6 +965,20 @@ cr.define('cr.ui.login.debug', function() {
             optInVisibility: true,
             optInDefaultState: true,
             legalFooterVisibility: true,
+          },
+          trigger: (screen) => {
+            screen.updateA11ySettingsButtonVisibility(false);
+          },
+        },
+        {
+          id: 'WithAceessibilityButton',
+          data: {
+            optInVisibility: true,
+            optInDefaultState: true,
+            legalFooterVisibility: true,
+          },
+          trigger: (screen) => {
+            screen.updateA11ySettingsButtonVisibility(true);
           },
         },
       ],
