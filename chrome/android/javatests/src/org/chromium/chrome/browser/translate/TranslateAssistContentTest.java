1ed70721cdb584530186cedcc979258f4d22b10e
Theresa Sullivan <twellington@chromium.org>
2021-03-17 23:22:40 +0000

    🤠 Disable flaky TranslateAssistContentTest#testAssistContentNonTranslatePage
    Bug: 1189385
    Change-Id: Iba1ef203836fb325b284c3eda9d1a38359da18da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2770141
    Commit-Queue: Theresa  <twellington@chromium.org>
    Commit-Queue: Megan Jablonski <megjablon@chromium.org>
    Commit-Queue: Josh Simmons <jds@google.com>
    Auto-Submit: Theresa  <twellington@chromium.org>
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Reviewed-by: Josh Simmons <jds@google.com>
    Cr-Commit-Position: refs/heads/master@{#863989}

--- chrome/android/javatests/src/org/chromium/chrome/browser/translate/TranslateAssistContentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/translate/TranslateAssistContentTest.java
@@ -19,6 +19,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -151,6 +152,7 @@ public class TranslateAssistContentTest {
     @MediumTest
     @Restriction(Restriction.RESTRICTION_TYPE_INTERNET)
     @Features.EnableFeatures({ChromeFeatureList.TRANSLATE_ASSIST_CONTENT})
+    @DisabledTest(message = "https://crbug.com/1189385")
     public void testAssistContentNonTranslatePage()
             throws TimeoutException, ExecutionException, JSONException {
         if (shouldSkipDueToNetworkService()) return;
