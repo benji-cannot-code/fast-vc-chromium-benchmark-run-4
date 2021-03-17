5d4729cb5e4344fa0af6ecf6dc0aba6762d12fbd
Polina Bondarenko <pbond@google.com>
2021-03-17 16:00:33 +0000

    Add reason to RestartJob
    Add extra parameter |reason| to SessionManagerClient::RestartJob to be
    passed to corresponding D-Bus method. The reason indicates whether
    a user session needs to be restarted (for guest sessions only) or not
    (for headless browser).
    
    BUG=b:181662602
    TEST=compile
    
    Change-Id: If853495a90ab8cba2c214d91853f2805d892c59a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757187
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Polina Bondarenko <pbond@chromium.org>
    Auto-Submit: Polina Bondarenko <pbond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863781}

--- chrome/browser/ash/login/session/user_session_manager.cc
+++ chrome/browser/ash/login/session/user_session_manager.cc
@@ -598,7 +598,7 @@ void UserSessionManager::CompleteGuestSessionLogin(const GURL& start_url) {
         base::CommandLine::StringVector());
   }
 
-  RestartChrome(command_line);
+  RestartChrome(command_line, RestartChromeReason::kGuest);
 }
 
 scoped_refptr<Authenticator> UserSessionManager::CreateAuthenticator(
