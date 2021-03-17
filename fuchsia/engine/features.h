cf4a6f9f3191e9b168db51409ceff3bc8210e545
Wez <wez@chromium.org>
2021-03-17 17:28:52 +0000

    [fuchsia] Disable memory-pressure handling in Renderers by default.
    Delivering memory pressure signals to renderers triggers a pathological
    interaction between release of resources by Skia, and use of those
    resources by the display compositor. This results in graphical glitches
    (usually flickering of some UX elements) when under pressure.
    
    Temporarily disable handling of pressure by default, pending a fix to
    the compositor pipeline.
    
    Bug: 1189208
    Bug: 1175858, 1173685
    Bug: b/181245064
    Change-Id: I661604951606fbe01e70714911be290b18f70c2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2767284
    Commit-Queue: Wez <wez@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863832}

--- fuchsia/engine/features.h
+++ fuchsia/engine/features.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace features {
 
 constexpr base::Feature kHandleMemoryPressureInRenderer{
-    "HandleMemoryPressureInRenderer", base::FEATURE_ENABLED_BY_DEFAULT};
+    "HandleMemoryPressureInRenderer", base::FEATURE_DISABLED_BY_DEFAULT};
 
 }  // namespace features
 
