7a0d53854aaaeb14ec9d18ff6dd3535f541a9cbf
Hiroki Nakagawa <nhiroki@chromium.org>
2021-03-17 08:20:35 +0000

    Prerender: Delay releasing mojo capability control until prerenderingchange event
    This CL delays the timing to release the mojo capability control in the
    browser process. Releasing the mojo capability control corresponds to
    running the "post-prerendering activation steps list" defined in the
    draft spec:
    https://jeremyroman.github.io/alternate-loading-modes/#document-post-prerendering-activation-steps-list
    
    This change makes it possible to write tests for deferred features using
    the web-exposed features (i.e., `document.prerendering` and the
    `prerenderingchange` event)
    
    > Details
    
    Before this CL, the mojo capability control was released in the browser
    process before the prerenderingchange event was dispatched in the Blink.
    This violated the spec steps in the Activate algorithm. The step 3 and
    step 4 could be reversed.
    https://jeremyroman.github.io/alternate-loading-modes/#prerendering-browsing-context-activate
    
      1. Set bc's loading mode to "default".
      2. Let doc be bc’s active document.
      3. Fire an event named prerenderingchange at doc.
      4. For each steps in doc's post-prerendering activation steps list:
        1. Run steps.
    
    After this CL, to obey the spec steps, the mojo capability control is
    released after the browser process (RenderFrameHostImpl) receives a
    DidActivateForPrerendering message sent by the Blink (LocalFrame) after
    the prerenderingchange event is dispatched.
    
    Design Doc:
    https://docs.google.com/document/d/1_l1LDUALf8PbZIz5y_UYNGZmYCMKNpePDLBHn-gTsTM/edit?usp=sharing
    
    > Future plans
    
    This causes unfavorable period between when RenderFrameHostImpl gets
    activated and when RenderFrameHostImpl receives the
    DidActivateForPrerendering message. The BrowserInterfaceBrokerImpl may
    receive a mojo request that causes cancellation of prerendering during
    the period, while the frame host already gets activated. This will be
    supported by introducing the "mode" of the BrowserInterfaceBrokerImpl
    in follow-up CLs. See the discussion in the design doc for details.
    
    Change-Id: I52c33d5350733cfb8d990312d665e36d97970fec
    Bug: 1183320, 1186796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2738911
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#863673}

--- third_party/blink/renderer/core/testing/fake_local_frame_host.cc
+++ third_party/blink/renderer/core/testing/fake_local_frame_host.cc
@@ -223,6 +223,8 @@ void FakeLocalFrameHost::DidAddMessageToConsole(
 
 void FakeLocalFrameHost::FrameSizeChanged(const gfx::Size& frame_size) {}
 
+void FakeLocalFrameHost::DidActivateForPrerendering() {}
+
 void FakeLocalFrameHost::BindFrameHostReceiver(
     mojo::ScopedInterfaceEndpointHandle handle) {
   receiver_.Bind(mojo::PendingAssociatedReceiver<mojom::blink::LocalFrameHost>(
