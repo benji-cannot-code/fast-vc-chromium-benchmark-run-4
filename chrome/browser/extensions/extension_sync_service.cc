19a150cfdf13674eaaf581b162904531eccd9346
Phillis Tang <phillis@chromium.org>
2021-03-18 21:56:19 +0000

    Reland "DPWA: stop applying sync from extension"
    This is a reland for
    https://chromium-review.googlesource.com/c/chromium/src/+/2725194
    after the test being deflaked here
    https://chromium-review.googlesource.com/c/chromium/src/+/2747054
    
    Stop applying sync entity changes via extension sync service for bookmark
    apps since web apps syncing are managed by WebAppSyncBridge now.
    Also update sync_test to allow setup_sync for individual clients .
    
    
    
    Bug: 1173739, 1184371
    Change-Id: Ic9179abe255bbb94980a22c04670c5b1b3e1fc53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2765054
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864437}

--- chrome/browser/extensions/extension_sync_service.cc
+++ chrome/browser/extensions/extension_sync_service.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/web_app_provider_base.h"
 #include "chrome/browser/web_applications/components/web_application_info.h"
 #include "chrome/common/buildflags.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "components/sync/model/sync_change.h"
 #include "components/sync/model/sync_error_factory.h"
@@ -475,8 +476,11 @@ void ExtensionSyncService::ApplySyncData(
     }
 
     if (!extension_sync_data.bookmark_app_url().empty()) {
-      // Handles creating and updating the bookmark app.
-      ApplyBookmarkAppSyncData(extension_sync_data);
+      // Handles creating and updating the bookmark app only if
+      // kSyncBookmarkApps is enabled. Bookmark apps have been migrated to web
+      // apps and are now handled by WebAppSyncBridge.
+      if (base::FeatureList::IsEnabled(features::kSyncBookmarkApps))
+        ApplyBookmarkAppSyncData(extension_sync_data);
       return;
     }
   }
