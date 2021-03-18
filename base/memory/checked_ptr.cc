5d2ee145238859aa547e51763baae29035bf2432
Sergei Glazunov <glazunov@google.com>
2021-03-18 14:09:21 +0000

    [BRP] Add super-page blocklist to 32-bit GigaCage
    This CL addresses an issue caused by the fact that on 32-bit systems a
    memory address can change its state from "outside GigaCage" to
    "inside GigaCage" over time. Consider the following snippet:
    
    ```
    CheckedPtr<void> checked_ptr = mmap();
    munmap(checked_ptr);
    void* ptr = malloc();
    checked_ptr = nullptr;
    ```
    
    When |checked_ptr| is being initialized, the address doesn't pass the
    GigaCage check, so the constructor doesn't attempt to modify the
    reference count. However, the |malloc()| call may coincidentally lead to
    PA reusing the same memory region that was freed by |munmap()| for a new
    super-page in GigaCage. As a result, the assignment operation in the
    last line may incorrectly assume that |checked_ptr| points to a valid
    GigaCage allocation and corrupt memory while trying to manipulate the
    non-existent reference count field.
    
    The CL makes PA store the number of active |CheckedPtr| objects per each
    *potential* super-page region and forbid the creation a super-page in a
    region where the number is non-zero.
    
    In addition, update |BackupRefPtrImpl::Advance()| to support addresses
    that lie on the boundary between GigaCage and non-GigaCage memory.
    
    Bug: 1073933, 1173851
    Change-Id: Ib6cfc9078f95c5943dcfa92cd226efa132e3a0ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2696420
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Sergei Glazunov <glazunov@google.com>
    Cr-Commit-Position: refs/heads/master@{#864220}

--- base/memory/checked_ptr.cc
+++ base/memory/checked_ptr.cc
@@ -41,6 +41,11 @@ bool BackupRefPtrImpl::IsPointeeAlive(void* ptr) {
   return PartitionRefCountPointer(slot_start)->IsAlive();
 }
 
+bool BackupRefPtrImpl::IsValidDelta(void* ptr, ptrdiff_t delta) {
+  DCHECK(features::IsPartitionAllocGigaCageEnabled());
+  return PartitionAllocIsValidPtrDelta(ptr, delta);
+}
+
 }  // namespace internal
 
 }  // namespace base
