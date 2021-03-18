5e5ac09d65a8a92152d0bd6957decc66c1fdbf90
Joe DeBlasio <jdeblasio@chromium.org>
2021-03-18 15:57:46 +0000

    [Lookalikes] Add TriggeredByInitialUrl to Lookalikes UKM
    This CL adds a boolean to the UKM recorded for lookalikes that indicates
    whether the URL that triggered the heuristic was the first URL in the
    redirect chain (initial_url) or the final URL in the chain
    (committed_url). Without this change, we just made educated guesses.
    Privacy approved this addition in go/lookalike-ukm?disco=AAAALpzpUVk .
    
    Presently, iOS checks ALL urls in the chain, not just first and last.
    Rather than solve that bug here, this CL just sets TriggeredByInitialUrl
    to false always on iOS and adds a TODO to fix that in the future.
    
    Bug: 1133598
    Change-Id: I2a6c4854e3d6f28e19bb6b03c6a6187d7c49e7e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2765941
    Reviewed-by: Livvie Lin <livvielin@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864260}

--- chrome/browser/lookalikes/lookalike_url_navigation_throttle_browsertest.cc
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle_browsertest.cc
@@ -483,6 +483,7 @@ IN_PROC_BROWSER_TEST_P(LookalikeUrlNavigationThrottleBrowserTest,
 
   CheckUkm({kNavigatedUrl}, "MatchType",
            LookalikeUrlMatchType::kSkeletonMatchTop500);
+  CheckUkm({kNavigatedUrl}, "TriggeredByInitialUrl", false);
 }
 
 // Navigate to a domain that would trigger the warning, but doesn't because it
@@ -528,6 +529,7 @@ IN_PROC_BROWSER_TEST_P(LookalikeUrlNavigationThrottleBrowserTest,
   }
   CheckUkm({kNavigatedUrl}, "MatchType",
            LookalikeUrlMatchType::kTargetEmbedding);
+  CheckUkm({kNavigatedUrl}, "TriggeredByInitialUrl", false);
 }
 
 // Same as TargetEmbedding_TopDomain_Match, but has a redirect where the first
@@ -561,6 +563,7 @@ IN_PROC_BROWSER_TEST_P(LookalikeUrlNavigationThrottleBrowserTest,
         NavigationSuggestionEvent::kMatchTargetEmbedding);
   }
   CheckUkm({kLastUrl}, "MatchType", LookalikeUrlMatchType::kTargetEmbedding);
+  CheckUkm({kLastUrl}, "TriggeredByInitialUrl", true);
 }
 
 // Target embedding should not trigger on allowlisted embedder domains.
