4cbb54ce1abf5cea91634c04a2216282a7629a92
Pâris MEULEMAN <pmeuleman@chromium.org>
2021-03-18 17:17:23 +0000

    Move WPT COOP/reporting/resources to COOP/resources
    Move the dispatcher/executor/redirect files that are used by both
    reporting and non-reporting tests to the parent folder.
    Try-access.js is not added by default to the executor anymore, and
    is instead included following an order built with
    resources/common.js's add_script_and_trigger_onload.
    
    Change-Id: I3ac36b2dd0a9e677d3eb2334b657f122a4cf17ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2771578
    Auto-Submit: Pâris Meuleman <pmeuleman@chromium.org>
    Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864297}

--- third_party/blink/web_tests/external/wpt/html/cross-origin-opener-policy/reporting/resources/reporting-common.js
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-opener-policy/reporting/resources/reporting-common.js
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
-const directory = "/html/cross-origin-opener-policy/reporting/resources";
+const directory = "/html/cross-origin-opener-policy/resources";
 const executor_path = directory + "/executor.html?pipe=";
 const coep_header = '|header(Cross-Origin-Embedder-Policy,require-corp)';
 
--- third_party/blink/web_tests/external/wpt/html/cross-origin-opener-policy/reporting/resources/test-access-property.js
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-opener-policy/reporting/resources/test-access-property.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-const directory = "/html/cross-origin-opener-policy/reporting";
+const directory = "/html/cross-origin-opener-policy";
 const executor_path = directory + "/resources/executor.html?pipe=";
 const coep_header = '|header(Cross-Origin-Embedder-Policy,require-corp)';
 
