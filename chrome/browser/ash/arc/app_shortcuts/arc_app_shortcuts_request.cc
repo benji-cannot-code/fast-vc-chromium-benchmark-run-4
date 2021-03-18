f8296caa01a612efca4f364dee6eb4c3be46b5cd
Alexey Baskakov <loyso@chromium.org>
2021-03-18 03:12:58 +0000

    AppService: Extract common constant kAppShortcutIconSizeDip.
    This is a follow up for the JumpLists CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/2462899
    
    The constant is now unified and shared across Arc and Web Apps.
    
    A publisher creates context menu shortcuts and handles the execution
    of context menu command kPublisherCommand.
    
    Bug: 1140356
    Change-Id: I13972a11ccb5bb4c4b842dda7f297e7103b73061
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2763447
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Auto-Submit: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864094}

--- chrome/browser/ash/arc/app_shortcuts/arc_app_shortcuts_request.cc
+++ chrome/browser/ash/arc/app_shortcuts/arc_app_shortcuts_request.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/apps/app_service/app_icon_factory.h"
+#include "chrome/browser/apps/app_service/menu_util.h"
 #include "chrome/browser/chromeos/arc/icon_decode_request.h"
 #include "chrome/common/chrome_features.h"
 #include "components/arc/arc_service_manager.h"
@@ -71,7 +72,6 @@ void ArcAppShortcutsRequest::OnGetAppShortcutItems(
       base::BindOnce(&ArcAppShortcutsRequest::OnAllIconDecodeRequestsDone,
                      base::Unretained(this)));
 
-  constexpr int kAppShortcutIconSize = 32;
   for (const auto& shortcut_item_ptr : shortcut_items) {
     ArcAppShortcutItem item;
     item.shortcut_id = shortcut_item_ptr->shortcut_id;
@@ -82,7 +82,7 @@ void ArcAppShortcutsRequest::OnGetAppShortcutItems(
 
     if (base::FeatureList::IsEnabled(features::kAppServiceAdaptiveIcon)) {
       apps::ArcRawIconPngDataToImageSkia(
-          std::move(shortcut_item_ptr->icon), kAppShortcutIconSize,
+          std::move(shortcut_item_ptr->icon), apps::kAppShortcutIconSizeDip,
           base::BindOnce(&ArcAppShortcutsRequest::OnSingleIconDecodeRequestDone,
                          weak_ptr_factory_.GetWeakPtr(), items_->size() - 1));
       continue;
@@ -95,7 +95,7 @@ void ArcAppShortcutsRequest::OnGetAppShortcutItems(
       icon_decode_requests_.emplace_back(std::make_unique<IconDecodeRequest>(
           base::BindOnce(&ArcAppShortcutsRequest::OnSingleIconDecodeRequestDone,
                          weak_ptr_factory_.GetWeakPtr(), items_->size() - 1),
-          kAppShortcutIconSize));
+          apps::kAppShortcutIconSizeDip));
       icon_decode_requests_.back()->StartWithOptions(
           shortcut_item_ptr->icon_png);
       continue;
@@ -104,7 +104,7 @@ void ArcAppShortcutsRequest::OnGetAppShortcutItems(
     icon_decode_requests_.emplace_back(std::make_unique<IconDecodeRequest>(
         base::BindOnce(&ArcAppShortcutsRequest::OnSingleIconDecodeRequestDone,
                        weak_ptr_factory_.GetWeakPtr(), items_->size() - 1),
-        kAppShortcutIconSize));
+        apps::kAppShortcutIconSizeDip));
     icon_decode_requests_.back()->StartWithOptions(
         shortcut_item_ptr->icon->icon_png_data.value());
   }
