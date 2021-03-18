665771551fbd367597b4faff790f6a26520a1987
Andy Paicu <andypaicu@chromium.org>
2021-03-18 23:25:11 +0000

    Gate CPSS on standard safe browsing and add updated obfuscating logic
    Bug: 2696419
    Change-Id: I8418e81024eb7d2fb292fad8c7cd8d0add2ca94c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2705388
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Ravjit Singh Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864461}

--- components/permissions/prediction_service/prediction_service_common.cc
+++ components/permissions/prediction_service/prediction_service_common.cc
@@ -13,14 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace permissions {
 ClientFeatures_Platform GetCurrentPlatformProto() {
-#if defined(OS_WIN)
-  return permissions::ClientFeatures_Platform_PLATFORM_WINDOWS;
-#elif defined(OS_LINUX) || defined(OS_CHROMEOS)
-  return permissions::ClientFeatures_Platform_PLATFORM_LINUX;
+#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS) || \
+    defined(OS_MAC)
+  return permissions::ClientFeatures_Platform_PLATFORM_DESKTOP;
 #elif defined(OS_ANDROID) || defined(OS_FUCHSIA)
-  return permissions::ClientFeatures_Platform_PLATFORM_ANDROID;
-#elif defined(OS_MAC)
-  return permissions::ClientFeatures_Platform_PLATFORM_MAC_OS;
+  return permissions::ClientFeatures_Platform_PLATFORM_MOBILE;
 #else
   return permissions::ClientFeatures_Platform_PLATFORM_UNSPECIFIED;
 #endif
