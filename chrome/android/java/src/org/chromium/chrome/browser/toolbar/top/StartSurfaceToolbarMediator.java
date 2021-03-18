0a9346894cdf33a772a38a6abe9526417c6cfc61
spdonghao <spdonghao@chromium.org>
2021-03-18 17:38:43 +0000

    [Start] Hide home button on tab switcher page when homepage is customized.
    Bug: 1185318
    Change-Id: Id5f8eea6714b3ad078104f2b0c031ff4c390f288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2745583
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864316}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/StartSurfaceToolbarMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/StartSurfaceToolbarMediator.java
@@ -75,6 +75,7 @@ class StartSurfaceToolbarMediator {
     @StartSurfaceState
     private int mOverviewModeState;
     private boolean mIsGoogleSearchEngine;
+    private boolean mShouldShowStartSurfaceAsHomepage;
 
     private CallbackController mCallbackController = new CallbackController();
     private float mNonIncognitoHomepageTranslationY;
@@ -87,6 +88,7 @@ class StartSurfaceToolbarMediator {
             ObservableSupplier<Boolean> identityDiscStateSupplier,
             Supplier<ButtonData> identityDiscButtonSupplier,
             ObservableSupplier<Boolean> homepageEnabledSupplier,
+            ObservableSupplier<Boolean> startSurfaceAsHomepageSupplier,
             ObservableSupplier<Boolean> homepageManagedByPolicySupplier,
             OnClickListener homeButtonOnClickHandler,
             boolean shouldShowTabSwitcherButtonOnHomepage) {
@@ -108,6 +110,11 @@ class StartSurfaceToolbarMediator {
             mPropertyModel.set(
                     HOMEPAGE_MANAGED_BY_POLICY_SUPPLIER, homepageManagedByPolicySupplier);
             mPropertyModel.set(HOME_BUTTON_CLICK_HANDLER, homeButtonOnClickHandler);
+            startSurfaceAsHomepageSupplier.addObserver(
+                    mCallbackController.makeCancelable((showStartSurfaceAsHomepage) -> {
+                        mShouldShowStartSurfaceAsHomepage = showStartSurfaceAsHomepage;
+                        updateHomeButtonVisibility();
+                    }));
         }
         mShouldShowTabSwitcherButtonOnHomepage = shouldShowTabSwitcherButtonOnHomepage;
     }
@@ -339,9 +346,11 @@ class StartSurfaceToolbarMediator {
                 || mOverviewModeState == StartSurfaceState.SHOWN_TABSWITCHER_TASKS_ONLY
                 || mOverviewModeState == StartSurfaceState.SHOWN_TABSWITCHER_OMNIBOX_ONLY
                 || mOverviewModeState == StartSurfaceState.SHOWN_TABSWITCHER_TRENDY_TERMS;
+        // If start surface is not shown as the homepage, home button shouldn't be shown on tab
+        // switcher page.
         mPropertyModel.set(HOME_BUTTON_IS_VISIBLE,
                 isShownTabswitcherState && !mPropertyModel.get(IS_INCOGNITO)
-                        && mShowHomeButtonOnTabSwitcher);
+                        && mShowHomeButtonOnTabSwitcher && mShouldShowStartSurfaceAsHomepage);
     }
 
     private void updateTabSwitcherButtonVisibility() {
