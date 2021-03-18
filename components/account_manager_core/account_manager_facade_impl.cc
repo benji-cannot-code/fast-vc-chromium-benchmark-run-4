818939079c08e9b888dbb9b712da4fe5ece57c17
Kushagra Sinha <sinhak@chromium.org>
2021-03-18 19:33:17 +0000

    Lacros: Delete AccountManagerFacade::IsInitialized
    This method is no longer needed because after
    https://crrev.com/c/2762117 clients need not care about the
    initialization status of Account Manager before calling it.
    Hence, transitively, clients of AccountManagerFacade will not need to
    care about it either.
    
    Additionally, remove the flag in AccountManagerFacadeImpl that tracked
    the initialization status of Account Manager and repurpose
    `IsInitialized` method for tracking AccountManagerFacade's
    internal initialization status.
    
    Bug: 1167183
    Test: components_unittests
    Change-Id: I60a1d3109ee7e9bdcceab8380347b94a24e44e96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2764444
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864378}

--- components/account_manager_core/account_manager_facade_impl.cc
+++ components/account_manager_core/account_manager_facade_impl.cc
@@ -93,10 +93,6 @@ AccountManagerFacadeImpl::AccountManagerFacadeImpl(
 
 AccountManagerFacadeImpl::~AccountManagerFacadeImpl() = default;
 
-bool AccountManagerFacadeImpl::IsInitialized() {
-  return is_remote_initialized_;
-}
-
 void AccountManagerFacadeImpl::AddObserver(Observer* observer) {
   observer_list_.AddObserver(observer);
 }
@@ -198,16 +194,8 @@ void AccountManagerFacadeImpl::OnReceiverReceived(
   receiver_ =
       std::make_unique<mojo::Receiver<crosapi::mojom::AccountManagerObserver>>(
           this, std::move(receiver));
-  // At this point (|receiver_| exists), we are subscribed to Account Manager.
+  // At this point (`receiver_` exists), we are subscribed to Account Manager.
 
-  account_manager_remote_->IsInitialized(base::BindOnce(
-      &AccountManagerFacadeImpl::OnInitialized, weak_factory_.GetWeakPtr()));
-}
-
-void AccountManagerFacadeImpl::OnInitialized(bool is_initialized) {
-  if (is_initialized)
-    is_remote_initialized_ = true;
-  // else: We will receive a notification in |OnTokenUpserted|.
   FinishInitSequenceIfNotAlreadyFinished();
 }
 
@@ -237,12 +225,6 @@ void AccountManagerFacadeImpl::FinishAddAccount(
 
 void AccountManagerFacadeImpl::OnTokenUpserted(
     crosapi::mojom::AccountPtr account) {
-  is_remote_initialized_ = true;
-  // |OnTokenUpserted| may be invoked before |OnInitialized|. Invoking
-  // initialization sequence callbacks before observers makes sure observers
-  // aren't confused by the call order.
-  FinishInitSequenceIfNotAlreadyFinished();
-
   base::Optional<Account> maybe_account = FromMojoAccount(account);
   if (!maybe_account) {
     LOG(WARNING) << "Can't unmarshal account of type: "
@@ -282,9 +264,11 @@ void AccountManagerFacadeImpl::GetPersistentErrorInternal(
 }
 
 void AccountManagerFacadeImpl::FinishInitSequenceIfNotAlreadyFinished() {
-  if (is_initialization_sequence_finished_)
+  if (is_initialized_) {
     return;
-  is_initialization_sequence_finished_ = true;
+  }
+
+  is_initialized_ = true;
   for (auto& cb : initialization_callbacks_) {
     std::move(cb).Run();
   }
@@ -293,11 +277,15 @@ void AccountManagerFacadeImpl::FinishInitSequenceIfNotAlreadyFinished() {
 
 void AccountManagerFacadeImpl::RunAfterInitializationSequence(
     base::OnceClosure closure) {
-  if (!is_initialization_sequence_finished_) {
+  if (!is_initialized_) {
     initialization_callbacks_.emplace_back(std::move(closure));
   } else {
     std::move(closure).Run();
   }
+}
+
+bool AccountManagerFacadeImpl::IsInitialized() {
+  return is_initialized_;
 }
 
 void AccountManagerFacadeImpl::FlushMojoForTesting() {
