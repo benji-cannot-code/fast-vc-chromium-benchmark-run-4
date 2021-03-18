eca5605a8291e88ac4f184ec8a23a515e5113b87
David Bertoni <dbertoni@chromium.org>
2021-03-18 05:45:46 +0000

    [Extensions] Fix regression in RulesCacheDelegate::UpdateRules.
    RulesCacheDelegate::UpdateRules will crash if an update task is
    posted and the extension is uninstalled before the task runs. This
    fixes the bug, which is a regression of crbug.com/517492.
    
    This CL also removes a workaround that was added to a
    declarativeContent API test to fix flaky failures as a result of
    this bug.
    
    Bug: 517492, 1013457, 1184472
    Change-Id: I769224907c4ec7b44a7d945a612bbeacf893ffdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2760501
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864128}

--- chrome/browser/extensions/api/declarative_content/declarative_content_apitest.cc
+++ chrome/browser/extensions/api/declarative_content/declarative_content_apitest.cc
@@ -1014,10 +1014,6 @@ IN_PROC_BROWSER_TEST_F(DeclarativeContentApiTest,
   EXPECT_EQ("add_rule",
             ExecuteScriptInBackgroundPage(extension->id(), kAddTestRule));
 
-  // Wait for declarative rules to be added.
-  content::BrowserContext::GetDefaultStoragePartition(profile())
-      ->FlushNetworkInterfaceForTesting();
-
   ExtensionService* extension_service = extensions::ExtensionSystem::Get(
       browser()->profile())->extension_service();
 
