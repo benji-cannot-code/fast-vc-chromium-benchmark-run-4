ca446d2a4123e2456687bde5e44ec2805cc2cb0e
Matt Menke <mmenke@chromium.org>
2021-03-18 00:05:13 +0000

    Remove ProfileIOData's call to initialize NssServiceChromeOS.
    NssServiceChromeOS is now created on first use, rather than when
    ProfileIOData is initialized (which happens on first request for the
    ResourceContext).
    
    Fixed: 1018972
    Change-Id: Ia5f00f1b166f8b2d9651a9ffafd2fdf8d503dbaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2751650
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864015}

--- chrome/browser/profiles/profile_io_data.cc
+++ chrome/browser/profiles/profile_io_data.cc
@@ -26,20 +26,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/constants.h"
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/net/nss_service_chromeos_factory.h"
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
-
 using content::BrowserThread;
 
 void ProfileIOData::InitializeOnUIThread(Profile* profile) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  // TODO(https://crbug.com/1018972): Remove this line once NSS initialization
-  // no longer depends on the ResourceContext.
-  NssServiceChromeOSFactory::GetForContext(profile);
-#endif  // #if BUILDFLAG(IS_CHROMEOS_ASH)
   // Make sure the ResourceContext is initialized.  It's unclear if this is
   // still needed.
   content::BrowserContext::EnsureResourceContextInitialized(profile);
