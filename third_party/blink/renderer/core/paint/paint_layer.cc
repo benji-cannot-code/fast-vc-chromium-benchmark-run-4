f4e370874bf0a052fcb56eb8b616c93b2efef31e
Felipe Erias <felipeerias@igalia.com>
2021-03-18 15:37:41 +0000

    Don't create unneeded layers for scrollbar-gutter: force
    This CL prevents the creation of unnecessary layers for elements
    that use scrollbar-gutter's "force" keyword.
    
    Now those layers are created only when the element's style uses
    custom scrollbars, because a PaintLayerScrollableArea is required
    in order to calculate their thickness.
    
    When the element uses default scrollbars, their thickness can be
    obtained directly from the page's scrollbar theme.
    
    Bug: 710214
    Change-Id: Ia319e5601745247189be58da8b1e992a34a17c99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2738895
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Felipe Erias <felipeerias@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#864254}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -1684,10 +1684,12 @@ bool PaintLayer::RequiresScrollableArea() const {
   // PaintLayerScrollableArea.
   if (GetLayoutBox()->CanResize())
     return true;
-  // When scrollbar-gutter is "force" we need a PaintLayerScrollableArea
-  // in order to calculate the size of scrollbar gutters.
-  if (GetLayoutObject().StyleRef().IsScrollbarGutterForce())
+  // With custom scrollbars, we need a PaintLayerScrollableArea
+  // so we can calculate the size of scrollbar gutters.
+  if (GetLayoutObject().StyleRef().IsScrollbarGutterForce() &&
+      GetLayoutObject().StyleRef().HasPseudoElementStyle(kPseudoIdScrollbar)) {
     return true;
+  }
   return false;
 }
 
