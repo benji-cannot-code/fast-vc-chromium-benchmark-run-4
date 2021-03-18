e5c5e30ccbe36b703a631a420c9392bf6142915c
Yuki Shiino <yukishiino@chromium.org>
2021-03-18 10:16:51 +0000

    sheriff: Disable PageTextObserverSingleProcessBrowserTest.SameProcessIframe
    Bug: 1189556
    Change-Id: I2cbe7eb8926f727239969c76b5d3cc78dad1b7f3
    Tbr: sophiechang@chromium.org, mcrouse@chromium.org, tbansal@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2772059
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864176}

--- chrome/browser/optimization_guide/page_text_observer_browsertest.cc
+++ chrome/browser/optimization_guide/page_text_observer_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -335,8 +336,14 @@ class PageTextObserverSingleProcessBrowserTest
   }
 };
 
+#if defined(OS_MAC)
+// https://crbug.com/1189556
+#define MAYBE_SameProcessIframe DISABLED_SameProcessIframe
+#else
+#define MAYBE_SameProcessIframe SameProcessIframe
+#endif
 IN_PROC_BROWSER_TEST_F(PageTextObserverSingleProcessBrowserTest,
-                       SameProcessIframe) {
+                       MAYBE_SameProcessIframe) {
   PageTextObserver::CreateForWebContents(web_contents());
   ASSERT_TRUE(observer());
 
