1421933b52ef27c281a53e5aeebf1cd872cc4e3f
Christian Dullweber <dullweber@chromium.org>
2021-03-18 17:53:36 +0000

    CookieSettings: Add test for dedicated worker storage
    Add tests to verify that CookieSettings are applied to IndexedDb,
    CacheStorage and StorageFoundation in dedicated workers.
    
    Bug: 1188536
    Change-Id: Ic3a49851318e231e3e05c1e93f1f8e9c707f42b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2773038
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864330}

--- chrome/browser/net/storage_test_utils.cc
+++ chrome/browser/net/storage_test_utils.cc
@@ -11,10 +11,11 @@ namespace storage {
 namespace test {
 
 const std::vector<std::string> kStorageTypes{
-    "Cookie",           "LocalStorage",           "FileSystem",
-    "FileSystemAccess", "SessionStorage",         "IndexedDb",
-    "WebSql",           "CacheStorage",           "ServiceWorker",
-    "CookieStore",      "WorkerFileSystemAccess", "StorageFoundation"};
+    "Cookie",           "LocalStorage",      "FileSystem",
+    "FileSystemAccess", "SessionStorage",    "IndexedDb",
+    "WebSql",           "CacheStorage",      "ServiceWorker",
+    "CookieStore",      "StorageFoundation", "WorkerFileSystemAccess",
+    "WorkerIndexedDb",  "WorkerCacheStorage", "WorkerStorageFoundation"};
 
 const std::vector<std::string> kCrossTabCommunicationTypes{
     "SharedWorker",
@@ -53,7 +54,7 @@ void SetStorageForFrame(content::RenderFrameHost* frame) {
     bool data = false;
     EXPECT_TRUE(content::ExecuteScriptAndExtractBool(
         frame, "set" + data_type + "()", &data));
-    EXPECT_TRUE(data) << data_type;
+    EXPECT_TRUE(data) << "SetStorageForFrame for " << data_type;
   }
 }
 
@@ -62,7 +63,7 @@ void ExpectStorageForFrame(content::RenderFrameHost* frame, bool expected) {
     bool data = false;
     EXPECT_TRUE(content::ExecuteScriptAndExtractBool(
         frame, "has" + data_type + "();", &data));
-    EXPECT_EQ(expected, data) << data_type;
+    EXPECT_EQ(expected, data) << "ExpectStorageForFrame for " << data_type;
   }
 }
 
