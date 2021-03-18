beeed6459933f627e7e3d2142dfee0d9e2267939
Xida Chen <xidachen@chromium.org>
2021-03-18 15:16:27 +0000

    Mark AutofillAssistantOverlayIntegrationTest#testShowCastOnIFrameElementInScrollIFrame flaky
    TBR=engedy@chromium.org
    NOTRY=true
    
    Bug: 1182103
    Change-Id: If87e31b2248aaf72cf395c31a7c5858843c6e3d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2773201
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Owners-Override: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864243}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantOverlayIntegrationTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantOverlayIntegrationTest.java
@@ -34,6 +34,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.autofill_assistant.proto.ActionProto;
 import org.chromium.chrome.browser.autofill_assistant.proto.BitmapDrawableProto;
@@ -306,6 +307,7 @@ public class AutofillAssistantOverlayIntegrationTest {
      */
     @Test
     @MediumTest
+    @FlakyTest(message = "https://crbug.com/1182103")
     public void testShowCastOnIFrameElementInScrollIFrame() throws Exception {
         SelectorProto element =
                 (SelectorProto) SelectorProto.newBuilder()
