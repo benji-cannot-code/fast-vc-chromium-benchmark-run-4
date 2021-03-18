5e409392db4fae74d986300d0a1b733d0ad87aea
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-03-18 23:24:08 +0000

    [CrOS cellular] Add tooltip when sim lock toggle is disabled
    Follow up CL, adds tooltip to disabled sim lock toggle button.
    
    https://screenshot.googleplex.com/8etWJDMLjYp72xu.png
    
    Bug: 1093185
    Change-Id: I5846c5b786008cbec1d52b7f47515f9fbe3e49e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2753959
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864460}

--- chrome/test/data/webui/cr_components/chromeos/network/network_siminfo_test.js
+++ chrome/test/data/webui/cr_components/chromeos/network/network_siminfo_test.js
@@ -109,8 +109,10 @@ suite('NetworkSiminfoTest', function() {
       async function() {
         let changePinButton = simInfo.$$('#changePinButton');
         let simLockButton = simInfo.$$('#simLockButton');
+        let simLockButtonTooltip = simInfo.$$('#inActiveSimLockTooltip');
         assertFalse(changePinButton.hidden);
         assertFalse(simLockButton.disabled);
+        assertFalse(!!simLockButtonTooltip);
 
         // Trigger device state change
         simInfo.deviceState =
@@ -119,7 +121,12 @@ suite('NetworkSiminfoTest', function() {
 
         changePinButton = simInfo.$$('#changePinButton');
         simLockButton = simInfo.$$('#simLockButton');
+        simLockButtonTooltip = simInfo.$$('#inActiveSimLockTooltip');
+
+        assertTrue(!!simLockButtonTooltip);
+
         assertTrue(changePinButton.hidden);
         assertTrue(simLockButton.disabled);
+        assertFalse(simLockButtonTooltip.hidden);
       });
 });
