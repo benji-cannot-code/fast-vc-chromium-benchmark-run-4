0e2ccd9cde9c13abd69bb43d69c5c695522d9770
Sigurdur Asgeirsson <siggi@chromium.org>
2021-03-18 22:41:00 +0000

    Add a feature flag to enable ludicrous timer slack on Mac.
    This will be enabled by finch experiment in due course. This is a
    "lightspeed" experiment, which will never be enabled beyond canary.
    
    Bug: 1181297
    Change-Id: I9e168f483e2384472bfb0de3a00a244b7c3c6adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2764545
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864449}

---
+++ base/message_loop/timer_slack.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/message_loop/timer_slack.h"
+
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+
+namespace base {
+namespace features {
+
+constexpr base::Feature kLudicrousTimerSlack{"LudicrousTimerSlack",
+                                             base::FEATURE_DISABLED_BY_DEFAULT};
+
+namespace {
+
+constexpr base::FeatureParam<base::TimeDelta> kSlackValueMs{
+    &kLudicrousTimerSlack, "slack_ms",
+    // 1.5 seconds default slack for this ludicrous experiment.
+    base::TimeDelta::FromMilliseconds(1500)};
+
+}  // namespace
+}  // namespace features
+
+bool IsLudicrousTimerSlackEnabled() {
+  return base::FeatureList::IsEnabled(base::features::kLudicrousTimerSlack);
+}
+
+base::TimeDelta GetLudicrousTimerSlack() {
+  return features::kSlackValueMs.Get();
+}
+
+}  // namespace base
