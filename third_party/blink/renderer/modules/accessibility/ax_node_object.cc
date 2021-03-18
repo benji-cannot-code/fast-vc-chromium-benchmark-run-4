22484a854a64c61786732d54f108e34eb71ca6d7
Aaron Leventhal <aleventhal@google.com>
2021-03-18 19:06:00 +0000

    Do not truncate descendants of editable nodes for ARIA leafiness
    Bug: 1178325,1187096
    Change-Id: Ia325fb7ee5691020bec7412ffdceb5373d5059dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2753046
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864365}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -4023,6 +4023,12 @@ bool AXNodeObject::CanHaveChildren() const {
       break;
   }
 
+  // Allow editable roots to expose any children they might have, complying
+  // with browser-side expectations that editable controls have children
+  // containing the actual text content.
+  if (IsEditable() && !IsRichlyEditable())
+    return true;
+
   switch (AriaRoleAttribute()) {
     case ax::mojom::blink::Role::kImage:
       return false;
