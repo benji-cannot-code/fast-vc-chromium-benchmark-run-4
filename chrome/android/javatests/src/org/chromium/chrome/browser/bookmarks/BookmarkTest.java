dc97a406e427f86b58fb085dfe8ce630de0a3f61
Balazs Engedy <engedy@chromium.org>
2021-03-18 17:43:11 +0000

    Mark another BookmarkTest as flaky.
    Mark BookmarkTest#testReadingListFolderShownMultipleUnreadPages as flaky.
    
    Bug: 1189666
    Change-Id: If30ed8a46f3fb4e91cfa6459663644e6c32f2e4f
    TBR: xingliu@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2773122
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Owners-Override: Balazs Engedy <engedy@chromium.org>
    Auto-Submit: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864319}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
@@ -1726,6 +1726,7 @@ public class BookmarkTest {
     @Test
     @SmallTest
     @Features.EnableFeatures({ChromeFeatureList.READ_LATER})
+    @FlakyTest(message = "https://crbug.com/1189666")
     public void testReadingListFolderShownMultipleUnreadPages() throws Exception {
         BookmarkPromoHeader.forcePromoStateForTests(PromoState.PROMO_NONE);
         openBookmarkManager();
