ac68c0dc43c22e8ddf98f6b3e608ccab02d90cf1
Michael Ludwig <michaelludwig@google.com>
2021-03-18 18:59:22 +0000

    Explicitly reject paint ops with non-finite bounds
    Skia's quickReject() assumes the bounds are sorted and finite.
    SkCanvas checks for non-finite bounds before calling quickReject(), so
    this updates PaintOp::QuickRejectDraw to do the same.
    
    Bug: 1186392
    Change-Id: Ia622111e98afb98b4c142cdafbe71dbb52573658
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2773199
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Cr-Commit-Position: refs/heads/master@{#864360}

--- cc/paint/paint_op_buffer.cc
+++ cc/paint/paint_op_buffer.cc
@@ -2391,6 +2391,8 @@ bool PaintOp::QuickRejectDraw(const PaintOp* op, const SkCanvas* canvas) {
   SkRect rect;
   if (!PaintOp::GetBounds(op, &rect))
     return false;
+  if (!rect.isFinite())
+    return true;
 
   if (op->IsPaintOpWithFlags()) {
     SkPaint paint = static_cast<const PaintOpWithFlags*>(op)->flags.ToSkPaint();
