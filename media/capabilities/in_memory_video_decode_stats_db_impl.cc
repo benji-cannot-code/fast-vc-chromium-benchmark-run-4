68b86b32f2022b633713581fd65843e37567a442
Chris Cunningham <chcunningham@chromium.org>
2021-03-19 04:36:20 +0000

    VideoDecodeStatsDB: Remove UAF checks
    The checks were added to track down https://crbug.com/865321, which is
    now fixed.
    
    While the UAF is gone, one of the less important CHECKs is now being hit
    on Android (see bug). The assumptions in the CHECK were important to the
    UAF detection strategy, but aren't required for proper functioning of
    the stats DB. We'll try to get to the bottom of what changed in the bug
    comments.
    
    Bug: 1189111
    Change-Id: I3ec275cbc82b360fc8b744a2a8794bda23197972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2770059
    Auto-Submit: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Will Cassella <cassew@google.com>
    Reviewed-by: Will Cassella <cassew@google.com>
    Cr-Commit-Position: refs/heads/master@{#864562}

--- media/capabilities/in_memory_video_decode_stats_db_impl.cc
+++ media/capabilities/in_memory_video_decode_stats_db_impl.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/task/post_task.h"
 #include "media/base/bind_to_current_loop.h"
-#include "media/capabilities/video_decode_stats_db_impl.h"
 #include "media/capabilities/video_decode_stats_db_provider.h"
 
 namespace media {
@@ -30,9 +29,6 @@ InMemoryVideoDecodeStatsDBImpl::InMemoryVideoDecodeStatsDBImpl(
 
 InMemoryVideoDecodeStatsDBImpl::~InMemoryVideoDecodeStatsDBImpl() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  if (seed_db_)
-    seed_db_->set_dependent_db(nullptr);
 }
 
 void InMemoryVideoDecodeStatsDBImpl::Initialize(InitializeCB init_cb) {
@@ -43,7 +39,7 @@ void InMemoryVideoDecodeStatsDBImpl::Initialize(InitializeCB init_cb) {
   // Tracking down crbug.com/1114128. Suspect we're double initializing somehow,
   // so this should show who the crash at the time of the second initialize
   // call.
-  CHECK(!seed_db_) << __func__ << " Already have a seed_db_?";
+  DCHECK(!seed_db_) << __func__ << " Already have a seed_db_?";
 
   // Fetch an *initialized* seed DB.
   if (seed_db_provider_) {
@@ -66,12 +62,8 @@ void InMemoryVideoDecodeStatsDBImpl::OnGotSeedDB(InitializeCB init_cb,
   DVLOG(2) << __func__ << (db ? " has" : " null") << " seed db";
 
   db_init_ = true;
-
-  CHECK(!seed_db_) << __func__ << " Already have a seed_db_?";
+  DCHECK(!seed_db_) << __func__ << " Already have a seed_db_?";
   seed_db_ = db;
-
-  if (seed_db_)
-    seed_db_->set_dependent_db(this);
 
   // Hard coding success = true. There are rare cases (e.g. disk corruption)
   // where an incognito profile may fail to acquire a reference to the base
