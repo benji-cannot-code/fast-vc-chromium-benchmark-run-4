da28afea34ea35bdf8c92c5d0e6f50173577f732
Chris Thompson <cthomp@chromium.org>
2021-03-19 01:40:20 +0000

    Remove support for TLS1.0/1.1 in SSLVersionMin policy
    Also updates the LegacyTLSInterstitialTest.PolicyOverridesInterstitial
    into a negative test PolicyDoesNotOverrideInterstitial, and updates
    CertificateProviderApiMockedExtensionTest tests.
    
    Bug: 1155619
    
    Change-Id: I55ee0775bf1a83f1275a1dfd1ab6d9e3be698b3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2765737
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864518}

--- chrome/browser/ssl/ssl_config_service_manager_pref.cc
+++ chrome/browser/ssl/ssl_config_service_manager_pref.cc
@@ -75,14 +75,6 @@ std::vector<uint16_t> ParseCipherSuites(
 // false if the string is not recognized.
 bool SSLProtocolVersionFromString(const std::string& version_str,
                                   network::mojom::SSLVersion* version) {
-  if (version_str == switches::kSSLVersionTLSv1) {
-    *version = network::mojom::SSLVersion::kTLS1;
-    return true;
-  }
-  if (version_str == switches::kSSLVersionTLSv11) {
-    *version = network::mojom::SSLVersion::kTLS11;
-    return true;
-  }
   if (version_str == switches::kSSLVersionTLSv12) {
     *version = network::mojom::SSLVersion::kTLS12;
     return true;
@@ -261,8 +253,7 @@ SSLConfigServiceManagerPref::GetSSLConfigFromPrefs() const {
   }
 
   network::mojom::SSLVersion version_max;
-  if (SSLProtocolVersionFromString(version_max_str, &version_max) &&
-      version_max >= network::mojom::SSLVersion::kTLS12) {
+  if (SSLProtocolVersionFromString(version_max_str, &version_max)) {
     config->version_max = version_max;
   }
 
