9a46fd1026182c2b69428fd71721179dcfd52610
Kenichi Ishibashi <bashi@chromium.org>
2021-03-19 01:26:07 +0000

    Make URLLoader call OnReceiveEarlyHints()
    H2/QUIC net stack can plumb 103 Early Hints responses now. This CL
    plumbs Early Hints from URLLoader to URLLoaderClient.
    
    This CL also drops `request_time` from mojom::EarlyHints because it
    isn't used. We can add it later if we need it.
    
    Bug: 671310
    Change-Id: I9e75da28f1ac8f57cc7b35864bf88be435db39fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2770999
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864512}

--- services/network/public/cpp/parsed_headers.cc
+++ services/network/public/cpp/parsed_headers.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace network {
 
 mojom::ParsedHeadersPtr PopulateParsedHeaders(
-    const scoped_refptr<net::HttpResponseHeaders>& headers,
+    const net::HttpResponseHeaders* headers,
     const GURL& url) {
   auto parsed_headers = mojom::ParsedHeaders::New();
   if (!headers)
