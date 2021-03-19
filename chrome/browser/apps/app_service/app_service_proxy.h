76b9ae8cc583af713304c4aca3f3e2a1a6bae24b
Maggie Cai <mxcai@chromium.org>
2021-03-19 02:07:03 +0000

    [Lacros] Created a skeleton Lacros Web Apps publisher.
    This CL creates a skeleton Lacros Web Apps publisher to work as a proxy
    publisher in ash-chrome for web apps from lacros-chrome.
    
    BUG=1144877
    
    Change-Id: I562bf209e008aeed8e1c177c66f3db051d794b58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2748554
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864525}

--- chrome/browser/apps/app_service/app_service_proxy.h
+++ chrome/browser/apps/app_service/app_service_proxy.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/built_in_chromeos_apps.h"
 #include "chrome/browser/apps/app_service/crostini_apps.h"
 #include "chrome/browser/apps/app_service/extension_apps_chromeos.h"
+#include "chrome/browser/apps/app_service/lacros_web_apps.h"
 #include "chrome/browser/apps/app_service/plugin_vm_apps.h"
 #include "chrome/browser/apps/app_service/web_apps_chromeos.h"
 #include "components/services/app_service/public/cpp/instance_registry.h"
@@ -440,6 +441,7 @@ class AppServiceProxy : public KeyedService,
   std::unique_ptr<LacrosApps> lacros_apps_;
   std::unique_ptr<WebAppsChromeOs> web_apps_;
   std::unique_ptr<BorealisApps> borealis_apps_;
+  std::unique_ptr<LacrosWebApps> lacros_web_apps_;
 
   bool arc_is_registered_ = false;
 
