01158f52e318ea4243bcbc7e2b0efb4afb3aadb1
amraboelkher <amraboelkher@chromium.org>
2021-03-19 17:38:12 +0000

    private_membership: Support error handling of empty PSM responses
    This CL is doing the following:
    - updating the library version up to cr/325344392 which is
    adding the error handling for the empty PSM responses
    - it is modifying the local patches into one that contains all required
    headers for git which will facilitate dealing easily with merge
    conflicts in the future
    - updating the version in README.chromium to point to the cr/325344392
    
    BUG=chromium:1188900
    
    Change-Id: I380985af36032e10bbb32d871184070a65ea58de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2772422
    Reviewed-by: Amr Aboelkher <amraboelkher@chromium.org>
    Reviewed-by: Amr Aboelkher <amraboelkher@google.com>
    Commit-Queue: Amr Aboelkher <amraboelkher@google.com>
    Cr-Commit-Position: refs/heads/master@{#864693}

--- third_party/private_membership/src/internal/crypto_utils.cc
+++ third_party/private_membership/src/internal/crypto_utils.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/private_membership/src/internal/crypto_utils.h"
 
-#include "absl/strings/str_cat.h"
 #include "third_party/private_membership/src/internal/aes_ctr_256_with_fixed_iv.h"
 #include "third_party/private_membership/src/internal/id_utils.h"
 #include "third_party/shell-encryption/src/status_macros.h"
--- third_party/private_membership/src/internal/crypto_utils.h
+++ third_party/private_membership/src/internal/crypto_utils.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_PRIVATE_MEMBERSHIP_SRC_INTERNAL_CRYPTO_UTILS_H_
 #define THIRD_PARTY_PRIVATE_MEMBERSHIP_SRC_INTERNAL_CRYPTO_UTILS_H_
 
+#include "absl/strings/str_cat.h"
 #include "third_party/private-join-and-compute/src/crypto/context.h"
 #include "third_party/private-join-and-compute/src/crypto/ec_commutative_cipher.h"
 #include "third_party/private_membership/src/private_membership.pb.h"
