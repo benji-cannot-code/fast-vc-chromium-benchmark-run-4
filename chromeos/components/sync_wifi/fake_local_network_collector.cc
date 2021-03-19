ab99c91ced9592dcadf57fc27bc6e07bc404421f
Jon Mann <jonmann@chromium.org>
2021-03-19 19:47:16 +0000

    Wi-Fi Sync: Re-enable autoconnect for networks affected by b/180854680
    This is a temporary change to re-enable autoconnect for all networks
    which came from Chrome Sync with autoconnect unset or unspecified in
    the proto.  This is a one time fix on login.
    
    Bug: b/180854680
    Change-Id: I6821496ee1accf9ab0590897ddc68892e3c8cf8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2753817
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864763}

--- chromeos/components/sync_wifi/fake_local_network_collector.cc
+++ chromeos/components/sync_wifi/fake_local_network_collector.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/components/sync_wifi/fake_local_network_collector.h"
 
+#include "base/callback.h"
 #include "components/sync/protocol/wifi_configuration_specifics.pb.h"
 
 namespace chromeos {
@@ -55,6 +56,18 @@ void FakeLocalNetworkCollector::ClearNetworks() {
 
 void FakeLocalNetworkCollector::SetNetworkMetadataStore(
     base::WeakPtr<NetworkMetadataStore> network_metadata_store) {}
+
+void FakeLocalNetworkCollector::FixAutoconnect(
+    std::vector<sync_pb::WifiConfigurationSpecifics> protos,
+    base::OnceCallback<void()> success_callback) {
+  has_fixed_autoconnect_ = true;
+  std::move(success_callback).Run();
+}
+
+void FakeLocalNetworkCollector::ExecuteAfterNetworksLoaded(
+    base::OnceCallback<void()> callback) {
+  std::move(callback).Run();
+}
 
 }  // namespace sync_wifi
 
