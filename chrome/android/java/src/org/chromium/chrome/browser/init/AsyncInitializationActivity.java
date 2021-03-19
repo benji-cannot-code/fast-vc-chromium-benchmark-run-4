f7ad005a9d6e00f5607cdf09040022adaec4fcf4
Michael Thiessen <mthiesse@chromium.org>
2021-03-19 15:02:48 +0000

    Re-write FROM_HISTORY intents to always be MAIN intents.
    When an Activity self-finishes without removing its task, and is then
    launched from the recents/overview menu, it's launched with its base
    intent, which could be a View intent if that's what initially launched
    the Activity. This could lead to unexpectedly launching URLs extra
    times, firing app redirects multiple times, etc.
    
    To avoid this, we can just re-write incoming FROM_HISTORY intents to
    look like the intents Android sends us when we don't self-finish and
    are resumed from history after being killed (MAIN Intents with the
    FROM_HISTORY flag).
    
    Bug: 945700
    Change-Id: Iaff7e60c1228b664d1bcdd2a78a889ba30cd65ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2768958
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864650}

--- chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
@@ -315,7 +315,7 @@ public abstract class AsyncInitializationActivity extends ChromeBaseAppCompatAct
 
     private final void onCreateInternal(Bundle savedInstanceState) {
         initializeStartupMetrics();
-        setIntent(validateIntent(getIntent()));
+        setIntent(IntentHandler.rewriteFromHistoryIntent(getIntent()));
 
         @LaunchIntentDispatcher.Action
         int dispatchAction = maybeDispatchLaunchIntent(getIntent(), savedInstanceState);
@@ -448,14 +448,6 @@ public abstract class AsyncInitializationActivity extends ChromeBaseAppCompatAct
      */
     protected boolean isStartedUpCorrectly(Intent intent) {
         return true;
-    }
-
-    /**
-     * Validates the intent that started this activity.
-     * @return The validated intent.
-     */
-    protected Intent validateIntent(final Intent intent) {
-        return intent;
     }
 
     /**
