158526c82be7b1c1b7e3352f37109c84a7e8a4cb
Matt Reynolds <mattreynolds@google.com>
2021-03-19 23:37:55 +0000

    gamepad: Map Share button for XSX (BT) on Android
    The Xbox Series X controller added a new Share button below the
    Xbox button which sends the HID usage Consumer/Record when
    connected over Bluetooth. Android remaps this to the
    KEYCODE_MEDIA_RECORD key code which is not recognized as a
    gamepad button.
    
    This CL adds a mapping function for Xbox Series X that maps the
    Share button at button index 17.
    
    Also reorders the vendorId and productId parameters to
    GamepadMappings.getMappings to follow the typical order.
    
    Bug: 1147578
    Change-Id: I7e81d3fe067981531c3fa024305bfd2968c85b51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774231
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: James Hollyer <jameshollyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864858}

---
+++ device/gamepad/android/junit/src/org/chromium/device/gamepad/GamepadDeviceTest.java
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.device.gamepad;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.Feature;
+
+/**
+ * Verify no regressions in gamepad mappings.
+ */
+@RunWith(BaseRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class GamepadDeviceTest {
+    @Test
+    @Feature({"Gamepad"})
+    public void testRelevantKeycodesAreSorted() {
+        for (int i = 0; i < GamepadDevice.RELEVANT_KEYCODES.length - 1; ++i) {
+            Assert.assertTrue(
+                    GamepadDevice.RELEVANT_KEYCODES[i] < GamepadDevice.RELEVANT_KEYCODES[i + 1]);
+        }
+    }
+}
