ef3803ff7b692ad8763743b9adc5e1ae125c97b6
Matt Falkenhagen <falken@chromium.org>
2021-03-19 05:31:27 +0000

    navigation: Add trace event for IsInactiveAndDisallowActivation.
    To help with debugging via traces.
    
    Bug: 1043616
    
    Change-Id: I8beb8e64787b4c0a475000b6216bdf0f8ab7c921
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2771398
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864570}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -4589,6 +4589,10 @@ void RenderFrameHostImpl::SendAccessibilityEventsToManager(
 }
 
 bool RenderFrameHostImpl::IsInactiveAndDisallowActivation() {
+  TRACE_EVENT1("navigation",
+               "RenderFrameHostImpl::IsInactiveAndDisallowActivation",
+               "render_frame_host", this);
+
   switch (lifecycle_state_) {
     case LifecycleState::kRunningUnloadHandlers:
     case LifecycleState::kReadyToBeDeleted:
