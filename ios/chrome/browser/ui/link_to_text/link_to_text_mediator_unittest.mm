339a5c74bc62f710ba339b8e35c97fc4f8757924
Mike Dougherty <michaeldo@chromium.org>
2021-03-19 03:02:37 +0000

    [iOS] Do not pass ownership of faked responses to FakeWebFrame
    The response for a call to a WebFrame can be returned to the application
    after the WebFrame has been deconstructed. Change
    FakeWebFrame::AddJsResultForFunctionCall to take a point to the
    base::Value result instead of taking ownership of the result value.
    
    This was identified on the ios-asan bot. Ex:
    https://ci.chromium.org/ui/p/chromium/builders/ci/ios-asan/13433/overview
    
    Change-Id: I41cc0bf6217dfe7f41bf4d8fb8109adabee376c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2761957
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864543}

--- ios/chrome/browser/ui/link_to_text/link_to_text_mediator_unittest.mm
+++ ios/chrome/browser/ui/link_to_text/link_to_text_mediator_unittest.mm
@@ -114,10 +114,8 @@ class LinkToTextMediatorTest : public PlatformTest {
                                                 consumer:mocked_consumer_];
   }
 
-  void SetLinkToTextResponse(std::unique_ptr<base::Value> value,
-                             CGFloat zoom_scale) {
-    main_frame_->AddJsResultForFunctionCall(std::move(value),
-                                            kJavaScriptFunctionName);
+  void SetLinkToTextResponse(base::Value* value, CGFloat zoom_scale) {
+    main_frame_->AddJsResultForFunctionCall(value, kJavaScriptFunctionName);
 
     fake_scroll_view_.contentInset =
         UIEdgeInsetsMake(kFakeTopInset, kFakeLeftInset, 0, 0);
@@ -238,7 +236,7 @@ TEST_F(LinkToTextMediatorTest, HandleLinkToTextSelectionTriggersCommandNoZoom) {
 
   std::unique_ptr<base::Value> fake_response =
       CreateSuccessResponse(kTestQuote, selection_rect);
-  SetLinkToTextResponse(std::move(fake_response), zoom);
+  SetLinkToTextResponse(fake_response.get(), zoom);
 
   __block BOOL callback_invoked = NO;
 
@@ -283,7 +281,7 @@ TEST_F(LinkToTextMediatorTest,
 
   std::unique_ptr<base::Value> fake_response =
       CreateSuccessResponse(kTestQuote, selection_rect);
-  SetLinkToTextResponse(std::move(fake_response), zoom);
+  SetLinkToTextResponse(fake_response.get(), zoom);
 
   __block BOOL callback_invoked = NO;
 
@@ -323,7 +321,7 @@ TEST_F(LinkToTextMediatorTest, LinkGenerationError) {
 
   std::unique_ptr<base::Value> error_response =
       CreateErrorResponse(LinkGenerationOutcome::kInvalidSelection);
-  SetLinkToTextResponse(std::move(error_response), /*zoom=*/1.0);
+  SetLinkToTextResponse(error_response.get(), /*zoom=*/1.0);
 
   __block BOOL callback_invoked = NO;
   [[[mocked_consumer_ expect] andDo:^(NSInvocation*) {
@@ -356,7 +354,7 @@ TEST_F(LinkToTextMediatorTest, EmptyResponseLinkGenerationError) {
   base::HistogramTester histogram_tester;
 
   std::unique_ptr<base::Value> empty_response = std::make_unique<base::Value>();
-  SetLinkToTextResponse(std::move(empty_response), /*zoom=*/1.0);
+  SetLinkToTextResponse(empty_response.get(), /*zoom=*/1.0);
 
   __block BOOL callback_invoked = NO;
   [[[mocked_consumer_ expect] andDo:^(NSInvocation*) {
@@ -391,7 +389,7 @@ TEST_F(LinkToTextMediatorTest, BadResponseLinkGenerationError) {
   std::unique_ptr<base::Value> malformed_response =
       std::make_unique<base::Value>(base::Value::Type::DICTIONARY);
   malformed_response->SetStringKey("somethingElse", "abc");
-  SetLinkToTextResponse(std::move(malformed_response), /*zoom=*/1.0);
+  SetLinkToTextResponse(malformed_response.get(), /*zoom=*/1.0);
 
   __block BOOL callback_invoked = NO;
   [[[mocked_consumer_ expect] andDo:^(NSInvocation*) {
@@ -425,7 +423,7 @@ TEST_F(LinkToTextMediatorTest, StringResponseLinkGenerationError) {
 
   std::unique_ptr<base::Value> string_response =
       std::make_unique<base::Value>("someValue");
-  SetLinkToTextResponse(std::move(string_response), /*zoom=*/1.0);
+  SetLinkToTextResponse(string_response.get(), /*zoom=*/1.0);
 
   __block BOOL callback_invoked = NO;
   [[[mocked_consumer_ expect] andDo:^(NSInvocation*) {
@@ -459,7 +457,7 @@ TEST_F(LinkToTextMediatorTest, LinkGenerationSuccessButNoPayload) {
 
   std::unique_ptr<base::Value> success_response =
       CreateErrorResponse(LinkGenerationOutcome::kSuccess);
-  SetLinkToTextResponse(std::move(success_response), /*zoom=*/1.0);
+  SetLinkToTextResponse(success_response.get(), /*zoom=*/1.0);
 
   __block BOOL callback_invoked = NO;
   [[[mocked_consumer_ expect] andDo:^(NSInvocation*) {
@@ -495,7 +493,7 @@ TEST_F(LinkToTextMediatorTest, LinkGenerationTimeout) {
   // will simply invoke the callback with nullptr (due to a timeout).
   std::unique_ptr<base::Value> success_response =
       CreateErrorResponse(LinkGenerationOutcome::kSuccess);
-  SetLinkToTextResponse(std::move(success_response), /*zoom=*/1.0);
+  SetLinkToTextResponse(success_response.get(), /*zoom=*/1.0);
 
   main_frame_->set_force_timeout(true);
 
@@ -538,7 +536,7 @@ TEST_F(LinkToTextMediatorTest, WithHttpsAndCanonicalUrl) {
       CreateSuccessResponse(kTestQuote, selection_rect);
   std::string canonical_url = "https://www.example.com/";
   SetCanonicalUrl(fake_response.get(), canonical_url);
-  SetLinkToTextResponse(std::move(fake_response), zoom);
+  SetLinkToTextResponse(fake_response.get(), zoom);
 
   __block BOOL callback_invoked = NO;
 
@@ -576,7 +574,7 @@ TEST_F(LinkToTextMediatorTest, NotHttpsAndCanonicalUrl) {
       CreateSuccessResponse(kTestQuote, selection_rect);
   std::string canonical_url = "https://www.example.com/";
   SetCanonicalUrl(fake_response.get(), canonical_url);
-  SetLinkToTextResponse(std::move(fake_response), zoom);
+  SetLinkToTextResponse(fake_response.get(), zoom);
 
   __block BOOL callback_invoked = NO;
 
--- ios/web/public/test/fakes/fake_web_frame.h
+++ ios/web/public/test/fakes/fake_web_frame.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_WEB_PUBLIC_TEST_FAKES_FAKE_WEB_FRAME_H_
 
 #include <map>
-#include <memory>
 #include <vector>
 
 #include "ios/web/public/js_messaging/web_frame.h"
@@ -70,7 +69,9 @@ class FakeWebFrame : public WebFrame {
 
   // Sets |js_result| that will be passed into callback for |name| function
   // call. The same result will be pass regardless of call arguments.
-  void AddJsResultForFunctionCall(std::unique_ptr<base::Value> js_result,
+  // NOTE: The caller is responsible for keeping |js_result| alive for as
+  // long as this instance lives.
+  void AddJsResultForFunctionCall(base::Value* js_result,
                                   const std::string& function_name);
 
   void set_force_timeout(bool force_timeout) { force_timeout_ = force_timeout; }
@@ -85,7 +86,7 @@ class FakeWebFrame : public WebFrame {
   // Map holding values to be passed in CallJavaScriptFunction() callback. Keyed
   // by JavaScript function |name| expected to be passed into
   // CallJavaScriptFunction().
-  std::map<std::string, std::unique_ptr<base::Value>> result_map_;
+  std::map<std::string, base::Value*> result_map_;
   // The frame identifier which uniquely identifies this frame across the
   // application's lifetime.
   std::string frame_id_;
