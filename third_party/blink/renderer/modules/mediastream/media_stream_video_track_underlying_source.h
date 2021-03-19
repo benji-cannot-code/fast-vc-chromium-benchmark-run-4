06b1e1748feec6d136d9cc531a0d3bb84ff020bc
Michael Lippautz <mlippautz@chromium.org>
2021-03-19 11:12:31 +0000

    heap: Fix include reaching directly into heap impl
    No-try: true
    Bug: 1056170
    Change-Id: I77046d1fad4fdc51339e4a9a2df45e68904b6303
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2772972
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864614}

--- third_party/blink/renderer/modules/mediastream/media_stream_video_track_underlying_source.h
+++ third_party/blink/renderer/modules/mediastream/media_stream_video_track_underlying_source.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/streams/readable_stream_transferring_optimizer.h"
 #include "third_party/blink/renderer/modules/mediastream/frame_queue_underlying_source.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
-#include "third_party/blink/renderer/platform/heap/impl/garbage_collected.h"
+#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
 namespace blink {
 
