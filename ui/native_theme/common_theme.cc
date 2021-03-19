4497289688da89644baaa3bdbbb3592152b20c0f
Peter Kasting <pkasting@chromium.org>
2021-03-19 17:46:15 +0000

    Reland "Add remaining native theme colors to color pipeline."
    This is a reland of e6168e2a880e149cdf6dde73afae5f755d41fbd1
    
    Original change's description:
    > Add remaining native theme colors to color pipeline.
    >
    > Bug: 1061127
    > Change-Id: I043ae7682a98ef8056f305661def3ca5378158f7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2754290
    > Commit-Queue: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#863969}
    
    Bug: 1061127
    Change-Id: I4c0e5c087884e9b4bbbd3262be191e98f7006edd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774928
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864695}

--- ui/native_theme/common_theme.cc
+++ ui/native_theme/common_theme.cc
@@ -322,13 +322,8 @@ SkColor GetDefaultColor(NativeTheme::ColorId color_id,
           accent, (color_id == kInitial) ? 0x4D : gfx::kGoogleGreyAlpha200);
     }
 
-    // Message Center
-    case NativeTheme::kColorId_MessageCenterSmallImageMaskForeground:
-      return SK_ColorWHITE;
-    case NativeTheme::kColorId_MessageCenterSmallImageMaskBackground:
-      return SkColorSetRGB(0xa3, 0xa3, 0xa3);
-
     // Notification
+    case NativeTheme::kColorId_MessageCenterSmallImageMaskForeground:
     case NativeTheme::kColorId_NotificationBackground:
       return base_theme->GetUnprocessedSystemColor(
           NativeTheme::kColorId_WindowBackground, color_scheme);
@@ -350,6 +345,7 @@ SkColor GetDefaultColor(NativeTheme::ColorId color_id,
       return base_theme->GetUnprocessedSystemColor(
           NativeTheme::kColorId_HighlightedMenuItemBackgroundColor,
           color_scheme);
+    case NativeTheme::kColorId_MessageCenterSmallImageMaskBackground:
     case NativeTheme::kColorId_NotificationDefaultAccentColor:
       return base_theme->GetUnprocessedSystemColor(
           NativeTheme::kColorId_LabelSecondaryColor, color_scheme);
