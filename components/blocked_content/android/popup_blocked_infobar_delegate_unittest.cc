70098ce4839419457ae79b6a9661aad411a623c6
Lijin Shen <lazzzis@google.com>
2021-03-19 22:04:30 +0000

    [Messages] Introduce popup blocked message
    1. Migration from popup blocked infobar to message
    2. Some refactor of shared logic between popup blocked infobar and
    message
    
    Bug: 1179462
    Change-Id: I52183ea621b78e0962a598a4e74717b2d57021b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2713707
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/master@{#864810}

--- components/blocked_content/android/popup_blocked_infobar_delegate_unittest.cc
+++ components/blocked_content/android/popup_blocked_infobar_delegate_unittest.cc
@@ -42,6 +42,11 @@ class TestInfoBarManager : public infobars::ContentInfoBarManager {
 class PopupBlockedInfoBarDelegateTest
     : public content::RenderViewHostTestHarness {
  public:
+  PopupBlockedInfoBarDelegateTest() : content::RenderViewHostTestHarness() {
+    // Make sure the SafeBrowsingTriggeredPopupBlocker is not created.
+    feature_list_.InitAndDisableFeature(kAbusiveExperienceEnforce);
+  }
+
   ~PopupBlockedInfoBarDelegateTest() override {
     settings_map_->ShutdownOnUIThread();
   }
@@ -49,8 +54,6 @@ class PopupBlockedInfoBarDelegateTest
   // content::RenderViewHostTestHarness:
   void SetUp() override {
     content::RenderViewHostTestHarness::SetUp();
-    // Make sure the SafeBrowsingTriggeredPopupBlocker is not created.
-    feature_list_.InitAndDisableFeature(kAbusiveExperienceEnforce);
 
     HostContentSettingsMap::RegisterProfilePrefs(pref_service_.registry());
     settings_map_ = base::MakeRefCounted<HostContentSettingsMap>(
