02675e817e6c255897dae05f182c978d2052129e
Miyoung Shin <myid.shin@igalia.com>
2021-03-19 23:18:33 +0000

    Use DEFINE_STATIC_LOCAL for V8DetailedMemoryReporterImpl
    This CL uses DEFINE_STATIC_LOCAL for V8DetailedMemoryReporterImpl that has
    a process-wide receiver instead of MakeSelfOwnedReceiver<T>.
    
    Bug: 1176590
    Change-Id: Iabbbf1bcf3e5d359fc13fe3cfa22bfcbd75c587b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2772791
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#864848}

--- third_party/blink/renderer/controller/performance_manager/v8_detailed_memory_reporter_impl.h
+++ third_party/blink/renderer/controller/performance_manager/v8_detailed_memory_reporter_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CONTROLLER_PERFORMANCE_MANAGER_V8_DETAILED_MEMORY_REPORTER_IMPL_H_
 
 #include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/blink/public/mojom/performance_manager/v8_detailed_memory_reporter.mojom-blink.h"
 #include "third_party/blink/renderer/controller/controller_export.h"
 
@@ -16,10 +17,13 @@ namespace blink {
 class CONTROLLER_EXPORT V8DetailedMemoryReporterImpl
     : public mojom::blink::V8DetailedMemoryReporter {
  public:
-  static void Create(
+  static void Bind(
       mojo::PendingReceiver<mojom::blink::V8DetailedMemoryReporter> receiver);
 
   void GetV8MemoryUsage(Mode mode, GetV8MemoryUsageCallback callback) override;
+
+ private:
+  mojo::Receiver<mojom::blink::V8DetailedMemoryReporter> receiver_{this};
 };
 
 }  // namespace blink
