d4e132c72ee440c12c8f9f3a9fb905689616963a
Dirk Pranke <dpranke@google.com>
2021-03-19 20:28:28 +0000

    Fix py2->3 issues in //remoting/tools/zip2msi.py.
    This CL updates a few `print` statements that don't work when
    built under Python3, and removes some calls to .iteritems().
    
    Bug: 1112471
    Cq-Include-Trybots: luci.chrome.try:win-chrome
    Change-Id: I7343df1d93addb0d86a8428ffda71ff0f35575a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774226
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Owners-Override: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864780}

--- remoting/tools/zip2msi.py
+++ remoting/tools/zip2msi.py
@@ -46,6 +46,8 @@ The copies of the script can be out of sync so make sure that a newer version is
 compatible with the older ones when updating the script.
 """
 
+from __future__ import print_function
+
 import copy
 import json
 from optparse import OptionParser
@@ -64,8 +66,8 @@ def UnpackZip(target, source):
     target_file = os.path.normpath(os.path.join(target, f))
     # Sanity check to make sure .zip uses relative paths.
     if os.path.commonprefix([target_file, target]) != target:
-      print "Failed to unpack '%s': '%s' is not under '%s'" % (
-          source, target_file, target)
+      print("Failed to unpack '%s': '%s' is not under '%s'" % (
+          source, target_file, target))
       return 1
 
     # Create intermediate directories.
@@ -94,7 +96,7 @@ def Merge(left, right):
   if isinstance(left, dict):
     if isinstance(right, dict):
       retval = copy.copy(left)
-      for key, value in right.iteritems():
+      for key, value in right.items():
         if key in retval:
           retval[key] = Merge(retval[key], value)
         else:
@@ -160,7 +162,7 @@ def GenerateCommandLine(tool, source, dest, parameters):
   switches = [os.path.join(wix_path, tool), '-nologo']
 
   # Append the list of defines and extensions to the command line switches.
-  for name, value in params.get('defines', {}).iteritems():
+  for name, value in params.get('defines', {}).items():
     switches.append('-d%s=%s' % (name, value))
 
   for ext in params.get('extensions', []):
@@ -184,10 +186,10 @@ def Run(args):
       command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   out, _ = popen.communicate()
   if popen.returncode:
-    print command
+    print(command)
     for line in out.splitlines():
-      print line
-    print '%s returned %d' % (args[0], popen.returncode)
+      print(line)
+    print('%s returned %d' % (args[0], popen.returncode))
   return popen.returncode
 
 
@@ -215,11 +217,11 @@ def GenerateMsi(target, source, parameters):
   f.close()
 
   if 'source' not in parameters:
-    print 'The source .wxs is not specified'
+    print('The source .wxs is not specified')
     return 1
 
   if 'bind_path' not in parameters:
-    print 'The binding path is not specified'
+    print('The binding path is not specified')
     return 1
 
   wxs = os.path.join(source_dir, parameters['source'])
@@ -234,7 +236,7 @@ def GenerateMsi(target, source, parameters):
   elif target_arch == 'x64':
     arch_param = 'x64'
   else:
-    print 'Invalid target_arch parameter value'
+    print('Invalid target_arch parameter value')
     return 1
 
   # Add the architecture to candle-specific parameters.
