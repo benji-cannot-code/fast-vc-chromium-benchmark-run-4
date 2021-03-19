4ac1c4a16604690ef0e6ddd5bf005d92678cbe56
Martin Kreichgauer <martinkr@google.com>
2021-03-19 22:27:45 +0000

    fido: include a pinUvAuthToken when checking exclude lists
    When probing for credentials on a makeCredential exclude list, we may
    send a number of silent getAssertion requests that each test for the
    existence of a subset of the credential IDs (credential list batching).
    These silent probing requests currently never include a pinUvAuthToken
    parameter, even if prior to probing we obtained a |pin::TokenResponse|
    from the authenticator. This leads to the authenticator not disclosing
    the existence of credentials with credProtect level "uvRequired" during
    probing.
    
    Another possible issue is that a CTAP 2.1 authenticator may actually
    clear all of its pinUvAuthToken state whenever it receives a request
    without a pinUvAuthToken parameter. So if exclude list batching occurs
    after obtaining a token, the authenticator would then fail to validate
    that token during the actual makeCrential request.
    
    This change plumbs the |pin::TokenResponse| via
    |CtapMakeCredentialRequest| into |MakeCredentialTask|, where a proper
    auth token value for the GetAssertion probing requests is crafted.
    
    (GetAssertion allowList probing does not have the same issue because
    |GetAssertionTask| copies the pinAuth parameter from the original to the
    probing request.)
    
    Thanks to John Bradley for reporting this issue.
    
    Fixed: 1166657
    Change-Id: I843c226480b1b5a56f894f018228f4ec03c03ca4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774295
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864825}

--- device/fido/virtual_fido_device.h
+++ device/fido/virtual_fido_device.h
@@ -86,6 +86,7 @@ class COMPONENT_EXPORT(DEVICE_FIDO) VirtualFidoDevice : public FidoDevice {
   // authenticator device.
   struct COMPONENT_EXPORT(DEVICE_FIDO) RegistrationData {
     RegistrationData();
+    explicit RegistrationData(const std::string& rp_id);
     RegistrationData(
         std::unique_ptr<PrivateKey> private_key,
         base::span<const uint8_t, kRpIdHashLength> application_parameter,
@@ -96,7 +97,7 @@ class COMPONENT_EXPORT(DEVICE_FIDO) VirtualFidoDevice : public FidoDevice {
 
     ~RegistrationData();
 
-    std::unique_ptr<PrivateKey> private_key;
+    std::unique_ptr<PrivateKey> private_key = PrivateKey::FreshP256Key();
     std::array<uint8_t, kRpIdHashLength> application_parameter;
     uint32_t counter = 0;
     bool is_resident = false;
@@ -250,6 +251,11 @@ class COMPONENT_EXPORT(DEVICE_FIDO) VirtualFidoDevice : public FidoDevice {
 
     FidoTransportProtocol transport =
         FidoTransportProtocol::kUsbHumanInterfaceDevice;
+
+    // Adds a new credential to the authenticator. Returns true on success,
+    // false if there already exists a credential with the given ID.
+    bool InjectRegistration(base::span<const uint8_t> credential_id,
+                            RegistrationData registration);
 
     // Adds a registration for the specified credential ID with the application
     // parameter set to be valid for the given relying party ID (which would
