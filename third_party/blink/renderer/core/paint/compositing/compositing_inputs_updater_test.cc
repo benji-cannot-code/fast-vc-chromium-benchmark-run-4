5faf23763d16d72b59712e0146687e25ee3e9844
Xianzhu Wang <wangxianzhu@chromium.org>
2021-03-20 18:00:13 +0000

    Ensure DescendantHasDirectOrScrollingCompositingReason
    Previously when an element with strict containment was added a
    compositing reason, DescendantHasDirectOrScrollingCompositingReason
    was not set on ancestors because the wrong condition, causing
    DCHECK failure and missing compositing assignment of the layer and
    its ancestors.
    
    Bug: 1181456
    Change-Id: I9cbf786e7a93817f05a9871e523be831cf56f6e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774268
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#864961}

--- third_party/blink/renderer/core/paint/compositing/compositing_inputs_updater_test.cc
+++ third_party/blink/renderer/core/paint/compositing/compositing_inputs_updater_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/paint/compositing/compositing_inputs_updater.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/core/paint/compositing/paint_layer_compositor.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
@@ -205,6 +206,33 @@ TEST_F(CompositingInputsUpdaterTest, LayoutContainmentLayer) {
   EXPECT_EQ(parent, child->NearestContainedLayoutLayer());
   EXPECT_EQ(grandchild, grandchild->NearestContainedLayoutLayer());
   EXPECT_EQ(grandchild, greatgrandchild->NearestContainedLayoutLayer());
+}
+
+TEST_F(CompositingInputsUpdaterTest,
+       DescendantHasDirectOrScrollingCompositingReason) {
+  SetBodyInnerHTML("<div id='target' style='contain: strict'>TARGET</div>");
+
+  PaintLayer* target = GetPaintLayerByElementId("target");
+  EXPECT_FALSE(target->HasCompositedLayerMapping());
+  EXPECT_FALSE(GetLayoutView()
+                   .Layer()
+                   ->DescendantHasDirectOrScrollingCompositingReason());
+
+  // Add compositing reason on target, which should schedule compositing inputs
+  // update using |target| as the CompositingInputsRoot.
+  GetDocument().getElementById("target")->setAttribute(
+      html_names::kStyleAttr, "contain: strict; will-change: opacity");
+  GetDocument().View()->UpdateLifecycleToLayoutClean(
+      DocumentUpdateReason::kTest);
+  EXPECT_TRUE(target->NeedsCompositingInputsUpdate());
+  EXPECT_FALSE(GetLayoutView().Layer()->ChildNeedsCompositingInputsUpdate());
+  EXPECT_EQ(target, GetLayoutView().Compositor()->GetCompositingInputsRoot());
+
+  UpdateAllLifecyclePhasesForTest();
+  EXPECT_TRUE(target->HasCompositedLayerMapping());
+  EXPECT_TRUE(GetLayoutView()
+                  .Layer()
+                  ->DescendantHasDirectOrScrollingCompositingReason());
 }
 
 }  // namespace blink
