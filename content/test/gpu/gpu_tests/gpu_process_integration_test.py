8674090d3bdb1a131389e06602f7f3bde20039f4
James Darpinian <jdarpinian@chromium.org>
2021-03-22 23:08:43 +0000

    Increase timeout for loading about:gpu
    For some reason about:gpu is taking a long time to load on Nvidia
    mac debug. We should diagnose it, but in the meantime we can
    increase the timeout to prevent broken test runs.
    
    Bug: 1189426
    Change-Id: I585543a216f34db901941add965cd4403b80206b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2773490
    Auto-Submit: James Darpinian <jdarpinian@chromium.org>
    Commit-Queue: James Darpinian <jdarpinian@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865409}

--- content/test/gpu/gpu_tests/gpu_process_integration_test.py
+++ content/test/gpu/gpu_tests/gpu_process_integration_test.py
@@ -12,6 +12,8 @@ from gpu_tests import common_browser_args as cba
 from gpu_tests import gpu_integration_test
 from gpu_tests import path_util
 
+_GPU_PAGE_TIMEOUT = 30
+
 data_path = os.path.join(path_util.GetChromiumSrcDir(), 'content', 'test',
                          'data')
 
@@ -44,7 +46,8 @@ def _GetBrowserBridgeProperty(tab, path):
   """The GPU WebUI uses JS modules and may not have initialized the global
     browserBridge object by the time we can start injecting JavaScript. This
     ensures we don't have that problem."""
-  tab.WaitForJavaScriptCondition('window.gpuPagePopulated', timeout=10)
+  tab.WaitForJavaScriptCondition('window.gpuPagePopulated',
+                                 timeout=_GPU_PAGE_TIMEOUT)
   return tab.EvaluateJavaScript('browserBridge.' + path)
 
 
@@ -277,7 +280,8 @@ class GpuProcessIntegrationTest(gpu_integration_test.GpuIntegrationTest):
     self.RestartBrowserIfNecessaryWithArgs(
         ['--use_gpu_driver_workaround_for_testing'])
     self._Navigate(test_path)
-    self.tab.WaitForJavaScriptCondition('window.gpuPagePopulated', timeout=10)
+    self.tab.WaitForJavaScriptCondition('window.gpuPagePopulated',
+                                        timeout=_GPU_PAGE_TIMEOUT)
     self._ValidateDriverBugWorkarounds('use_gpu_driver_workaround_for_testing',
                                        None)
 
@@ -333,7 +337,8 @@ class GpuProcessIntegrationTest(gpu_integration_test.GpuIntegrationTest):
     self.RestartBrowserIfNecessaryWithArgs([])
     self._Navigate(test_path)
     self._VerifyGpuProcessPresent()
-    self.tab.WaitForJavaScriptCondition('window.gpuPagePopulated', timeout=10)
+    self.tab.WaitForJavaScriptCondition('window.gpuPagePopulated',
+                                        timeout=_GPU_PAGE_TIMEOUT)
     recorded_workarounds, recorded_disabled_gl_extensions = (
         self._CompareAndCaptureDriverBugWorkarounds())
     # Relaunch the browser enabling test group 1 with entry 215, where
@@ -346,7 +351,8 @@ class GpuProcessIntegrationTest(gpu_integration_test.GpuIntegrationTest):
     self.RestartBrowserIfNecessaryWithArgs(additional_args)
     self._Navigate(test_path)
     self._VerifyGpuProcessPresent()
-    self.tab.WaitForJavaScriptCondition('window.gpuPagePopulated', timeout=10)
+    self.tab.WaitForJavaScriptCondition('window.gpuPagePopulated',
+                                        timeout=_GPU_PAGE_TIMEOUT)
     new_workarounds, new_disabled_gl_extensions = (
         self._CompareAndCaptureDriverBugWorkarounds())
     diff = set(recorded_workarounds).symmetric_difference(new_workarounds)
@@ -407,7 +413,8 @@ class GpuProcessIntegrationTest(gpu_integration_test.GpuIntegrationTest):
         '--use_gpu_driver_workaround_for_testing=0'
     ])
     self._Navigate(test_path)
-    self.tab.WaitForJavaScriptCondition('window.gpuPagePopulated', timeout=10)
+    self.tab.WaitForJavaScriptCondition('window.gpuPagePopulated',
+                                        timeout=_GPU_PAGE_TIMEOUT)
     workarounds, _ = (self._CompareAndCaptureDriverBugWorkarounds())
     if 'use_gpu_driver_workaround_for_testing' in workarounds:
       self.fail('use_gpu_driver_workaround_for_testing erroneously present')
