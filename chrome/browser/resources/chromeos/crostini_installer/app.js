a0fb4e099124dd286cafa3e297b149ceb22de243
Fergus Dall <sidereal@google.com>
2021-03-22 01:06:56 +0000

    crostini_installer: Add a new error screen
    Switching to DLC introduces a new failure mode for the installer when
    the OS image is either out of date, or has a pending update. In this
    case we show a new error screen telling the user to update their
    device and try again, with a button to open the settings app to the
    about page.
    
    Bug: 953544
    Change-Id: I5e00376d99ce0ee5d118abfb31248d50eee8c34b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2772514
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/master@{#865013}

--- chrome/browser/resources/chromeos/crostini_installer/app.js
+++ chrome/browser/resources/chromeos/crostini_installer/app.js
@@ -30,13 +30,13 @@ const State = {
   CONFIGURE: 'configure',
   INSTALLING: 'installing',
   ERROR: 'error',
-  ERROR_NO_RETRY: 'error_no_retry',
   CANCELING: 'canceling',
 };
 
 const MAX_USERNAME_LENGTH = 32;
 const InstallerState = crostini.mojom.InstallerState;
 const InstallerError = crostini.mojom.InstallerError;
+const NoDiskSpaceError = 'no_disk_space';
 
 const UNAVAILABLE_USERNAMES = [
   'root',
@@ -81,6 +81,12 @@ Polymer({
     state_: {
       type: String,
       value: State.PROMPT,
+    },
+
+    /** @private */
+    error_: {
+      type: String,
+      value: InstallerError.kNone,
     },
 
     /** @private */
@@ -177,6 +183,7 @@ Polymer({
         } else {
           assert(this.state_ === State.INSTALLING);
           this.errorMessage_ = this.getErrorMessage_(error);
+          this.error_ = error;
           this.state_ = State.ERROR;
         }
       }),
@@ -243,7 +250,8 @@ Polymer({
           .catch(() => {
             this.errorMessage_ =
                 loadTimeData.getString('minimumFreeSpaceUnmetError');
-            this.state_ = State.ERROR_NO_RETRY;
+            this.error_ = NoDiskSpaceError;
+            this.state_ = State.ERROR;
           })
           .finally(() => {
             this.onNextButtonClickIsRunning_ = false;
@@ -253,7 +261,7 @@ Polymer({
 
   /** @private */
   onInstallButtonClick_() {
-    assert(this.showInstallButton_(this.state_));
+    assert(this.showInstallButton_(this.state_, this.error_));
     var diskSize = 0;
     if (loadTimeData.getBoolean('diskResizingEnabled')) {
       if (this.showDiskSlider_) {
@@ -266,6 +274,11 @@ Polymer({
     this.installerProgress_ = 0;
     this.state_ = State.INSTALLING;
     BrowserProxy.getInstance().handler.install(diskSize, this.username_);
+  },
+
+  /** @private */
+  onSettingsButtonClick_() {
+    window.open('chrome://os-settings/help');
   },
 
   /**
@@ -297,7 +310,6 @@ Polymer({
         BrowserProxy.getInstance().handler.cancel();
         break;
       case State.ERROR:
-      case State.ERROR_NO_RETRY:
         this.closePage_();
         break;
       case State.CANCELING:
@@ -319,7 +331,7 @@ Polymer({
    * @returns {string}
    * @private
    */
-  getTitle_(state) {
+  getTitle_(state, error) {
     let titleId;
     switch (state) {
       case State.PROMPT:
@@ -330,8 +342,11 @@ Polymer({
         titleId = 'installingTitle';
         break;
       case State.ERROR:
-      case State.ERROR_NO_RETRY:
-        titleId = 'errorTitle';
+        if (error == InstallerError.kNeedUpdate) {
+          titleId = 'needUpdateTitle';
+        } else {
+          titleId = 'errorTitle';
+        }
         break;
       case State.CANCELING:
         titleId = 'cancelingTitle';
@@ -354,11 +369,14 @@ Polymer({
 
   /**
    * @param {State} state
+   * @param {string} error
    * @returns {boolean}
    * @private
    */
-  showInstallButton_(state) {
-    return state === State.CONFIGURE || state === State.ERROR;
+  showInstallButton_(state, error) {
+    return state === State.CONFIGURE ||
+        (state === State.ERROR && error != NoDiskSpaceError &&
+         error != InstallerError.kNeedUpdate);
   },
 
   /**
@@ -382,6 +400,16 @@ Polymer({
    */
   showNextButton_(state) {
     return state === State.PROMPT;
+  },
+
+  /**
+   * @param {State} state
+   * @param {string} error
+   * @returns {boolean}
+   * @private
+   */
+  showSettingsButton_(state, error) {
+    return state === State.ERROR && error == InstallerError.kNeedUpdate;
   },
 
   /**
@@ -460,11 +488,10 @@ Polymer({
     let messageId = null;
     switch (error) {
       case InstallerError.kErrorLoadingTermina:
-      case InstallerError.kNeedReboot:
-        // TODO(sidereal) kNeedReboot should really have it's own error message,
-        // but that has to go through UX. Previously these failures would have
-        // been reported as kErrorLoadingTermina so use that message for now.
         messageId = 'loadTerminaError';
+        break;
+      case InstallerError.kNeedUpdate:
+        messageId = 'needUpdateError';
         break;
       case InstallerError.kErrorCreatingDiskImage:
         messageId = 'createDiskImageError';
@@ -553,7 +580,7 @@ Polymer({
 
   /** @private */
   showErrorMessage_(state) {
-    return state === State.ERROR || state === State.ERROR_NO_RETRY;
+    return state === State.ERROR;
   },
 
   /** @private */
