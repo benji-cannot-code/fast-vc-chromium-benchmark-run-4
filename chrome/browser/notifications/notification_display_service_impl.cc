0941fdb5034e9fe9021255003bd45fe228277eaf
Richard Knoll <knollr@chromium.org>
2021-03-22 19:27:36 +0000

    Log the result of loading a profile for notifications
    This adds a UMA metric to track if loading the profile from a
    notification action is successful or not. Only logged when we're using a
    system notification bridge, so not on Chrome OS and not when we use
    Chrome's own notifications.
    
    Bug: None
    Change-Id: Ibb357b3036134b8f558a838c85777e2a8cc8a48f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2776011
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865300}

--- chrome/browser/notifications/notification_display_service_impl.cc
+++ chrome/browser/notifications/notification_display_service_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "build/build_config.h"
 #include "build/buildflag.h"
 #include "build/chromeos_buildflags.h"
@@ -277,9 +278,9 @@ void NotificationDisplayServiceImpl::ProfileLoadedCallback(
     const base::Optional<std::u16string>& reply,
     const base::Optional<bool>& by_user,
     Profile* profile) {
+  base::UmaHistogramBoolean("Notifications.LoadProfileResult",
+                            profile != nullptr);
   if (!profile) {
-    // TODO(miguelg): Add UMA for this condition.
-    // Perhaps propagate this through PersistentNotificationStatus.
     LOG(WARNING) << "Profile not loaded correctly";
     return;
   }
