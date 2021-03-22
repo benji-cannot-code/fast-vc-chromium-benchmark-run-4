3e52a5706724b151920d5d89013e962eb1787e0a
Wei Lee <wtlee@chromium.org>
2021-03-22 04:58:15 +0000

    Reland "Lacros: Enable CroS camera stack on Lacros"
    This reverts commit 10f5f1578506483608c9726006eaa976dc405a11.
    
    The original CL is reverted since it breaks:
    
    BrowserTest:
      WebRtcVideoCaptureServiceEnumerationBrowserTest
          .AddingAndRemovingVirtualDeviceTriggersMediaElementOnDeviceChange
    
    UnitTest:
      services_unittests
      content_unittests
    
    The browser test is known flaky on Chrome OS, but it is only disabled for
    Ash-Chrome. Therefore, this CL disables it for Lacros-Chrome as well.
    
    For those unit tests, they crashed since there is no
    LacrosChromeServiceImpl initialized. It seems that the Lacros service
    might be unavailable under unit tests. Therefore, we remove the DCHECK
    and fallback to the legacy path if Lacros service is unavailable.
    
    Original change's description:
    > Revert "Lacros: Enable CroS camera stack on Lacros"
    >
    > This reverts commit dbe828606837dd908549719c51f8b4b3ae39bf55.
    >
    > Reason for revert: Failed on linux-lacros-tester-rel.
    > See https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-tester-rel/8033/test-results?q=
    >
    > Original change's description:
    > > Lacros: Enable CroS camera stack on Lacros
    > >
    > > This CL implements the interface between Lacros-Chrome and Ash-Chrome so
    > > that the video capture request from Lacros-Chrome can be forwarded to
    > > Ash-Chrome and eventually reaches the VideoCaptureDevice in Ash-Chrome.
    > >
    > > Bug: b/175168296
    > > Test: Manually
    > >
    > > Change-Id: I81d0042cfe250232d056034d338ab2d2ec8da4da
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2719683
    > > Auto-Submit: Wei Lee <wtlee@chromium.org>
    > > Reviewed-by: Scott Violet <sky@chromium.org>
    > > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > > Commit-Queue: Wei Lee <wtlee@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#863134}
    >
    > Bug: b/175168296
    > Change-Id: I0dae1c8774b8eccf0e00c8bc99dff733397524b1
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2763268
    > Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/master@{#863164}
    
    Bug: b/175168296
    Change-Id: Iee3b477cae74cfd6c27b0af8df9152f507bbf1a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2763448
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865036}

--- chrome/browser/ash/crosapi/browser_util.cc
+++ chrome/browser/ash/crosapi/browser_util.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_manager.h"
 #include "components/user_manager/user_type.h"
 #include "components/version_info/channel.h"
+#include "media/capture/mojom/video_capture.mojom.h"
 #include "mojo/public/cpp/platform/platform_channel.h"
 #include "mojo/public/cpp/system/invitation.h"
 
@@ -233,7 +234,7 @@ bool IsLacrosWindow(const aura::Window* window) {
 
 base::flat_map<base::Token, uint32_t> GetInterfaceVersions() {
   static_assert(
-      crosapi::mojom::Crosapi::Version_ == 19,
+      crosapi::mojom::Crosapi::Version_ == 20,
       "if you add a new crosapi, please add it to the version map here");
   InterfaceVersions versions;
   AddVersion<chromeos::sensors::mojom::SensorHalClient>(&versions);
@@ -258,6 +259,7 @@ base::flat_map<base::Token, uint32_t> GetInterfaceVersions() {
   AddVersion<crosapi::mojom::TaskManager>(&versions);
   AddVersion<crosapi::mojom::TestController>(&versions);
   AddVersion<crosapi::mojom::UrlHandler>(&versions);
+  AddVersion<crosapi::mojom::VideoCaptureDeviceFactory>(&versions);
   AddVersion<device::mojom::HidConnection>(&versions);
   AddVersion<device::mojom::HidManager>(&versions);
   AddVersion<media_session::mojom::MediaControllerManager>(&versions);
