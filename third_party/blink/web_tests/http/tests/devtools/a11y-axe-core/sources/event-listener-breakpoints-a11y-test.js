df28cee9700c55dd3486f7951de58d88908fbb55
Tim van der Lippe <tvanderlippe@chromium.org>
2021-03-22 15:25:36 +0000

    Inline usage of runtime.sharedInstance
    R=aerotwist@chromium.org
    
    Bug: 1127902
    Change-Id: I960d8ca04caefa5a5b8a8c990fbdc1ef4432ab40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2778395
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865170}

--- third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/sources/event-listener-breakpoints-a11y-test.js
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/sources/event-listener-breakpoints-a11y-test.js
@@ -9,8 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.loadLegacyModule('browser_debugger');
 
   await UI.viewManager.showView('sources.eventListenerBreakpoints');
-  const eventListenerWidget = self.runtime.sharedInstance(
-      BrowserDebugger.EventListenerBreakpointsSidebarPane);
+  const eventListenerWidget = BrowserDebugger.EventListenerBreakpointsSidebarPane.instance();
   TestRunner.addResult('Setting event listener breakpoints.');
   const {checkbox, element} = eventListenerWidget._categories.get('Animation');
   element.revealAndSelect();
