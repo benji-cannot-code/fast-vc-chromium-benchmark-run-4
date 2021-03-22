27c6af2cc66b4d3630678021a4af48890875f575
Sigurd Schneider <sigurds@chromium.org>
2021-03-22 14:31:01 +0000

    Re-enable tests
    Bug: chromium:1184201
    Change-Id: Iad9a0482c1110a6dd2d7d3a1829ebe24f10e22da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2775741
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865151}

--- chrome/browser/devtools/devtools_browsertest.cc
+++ chrome/browser/devtools/devtools_browsertest.cc
@@ -2305,15 +2305,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsTest,
   content::WebUIControllerFactory::UnregisterFactoryForTesting(&test_factory);
 }
 
-#if defined(MEMORY_SANITIZER)
-// Flaking on MSan runs: crbug.com/1184201
-#define MAYBE_TestRawHeadersWithRedirectAndHSTS \
-  DISABLED_TestRawHeadersWithRedirectAndHSTS
-#else
-#define MAYBE_TestRawHeadersWithRedirectAndHSTS \
-  TestRawHeadersWithRedirectAndHSTS
-#endif
-IN_PROC_BROWSER_TEST_F(DevToolsTest, MAYBE_TestRawHeadersWithRedirectAndHSTS) {
+IN_PROC_BROWSER_TEST_F(DevToolsTest, TestRawHeadersWithRedirectAndHSTS) {
   net::EmbeddedTestServer https_test_server(
       net::EmbeddedTestServer::TYPE_HTTPS);
   https_test_server.SetSSLConfig(
@@ -2399,14 +2391,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsTest, MAYBE_TestOpenInNewTabFilter) {
   CloseDevToolsWindow();
 }
 
-#if defined(MEMORY_SANITIZER)
-// Flaking on MSan runs: crbug.com/1184201
-#define MAYBE_LoadNetworkResourceForFrontend \
-  DISABLED_LoadNetworkResourceForFrontend
-#else
-#define MAYBE_LoadNetworkResourceForFrontend LoadNetworkResourceForFrontend
-#endif
-IN_PROC_BROWSER_TEST_F(DevToolsTest, MAYBE_LoadNetworkResourceForFrontend) {
+IN_PROC_BROWSER_TEST_F(DevToolsTest, LoadNetworkResourceForFrontend) {
   std::string file_url =
       "file://" + base::PathService::CheckedGet(base::DIR_SOURCE_ROOT)
                       .AppendASCII("content/test/data/devtools/navigation.html")
