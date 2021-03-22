b24c310e8d025c0eb3f3cb76809f406ba67d5d05
Xiaohan Wang <xhwang@chromium.org>
2021-03-22 21:48:46 +0000

    media: MediaFoundationCdm requires MediaFoundationRenderer
    This is a follow up of
    https://chromium-review.googlesource.com/c/chromium/src/+/2720409.
    
    Update MediaFoundationCdm to indicate that it requires
    MediaFoundationRenderer.
    
    Bug: 999747
    Change-Id: I8ee439cd314c4a536f19137f27377e05c6009bbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2776396
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865372}

--- media/cdm/win/media_foundation_cdm.h
+++ media/cdm/win/media_foundation_cdm.h
@@ -58,6 +58,7 @@ class MEDIA_EXPORT MediaFoundationCdm : public ContentDecryptionModule,
   CdmContext* GetCdmContext() final;
 
   // CdmContext implementation.
+  bool RequiresMediaFoundationRenderer() final;
   bool GetMediaFoundationCdmProxy(
       GetMediaFoundationCdmProxyCB get_mf_cdm_proxy_cb) final;
 
