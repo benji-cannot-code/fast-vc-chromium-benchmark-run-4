7678caabd46f5c08394732021567f91f7e8303fa
Anton Bikineev <bikineev@chromium.org>
2021-03-22 17:13:12 +0000

    PCScan: Temporarily disable safepoints.
    TLS on Android is emulated with the runtime lib, which can allocate and
    thereby cause reentrancy.
    
    This also attempts to fix OOM for mojo::Message::Channels.
    
    Bug: 1190621,1190649,1190764
    Change-Id: Icb2526e4e0a5a67dc98a003f40c63e33c55a5f1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2772977
    Auto-Submit: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865216}

--- base/allocator/partition_allocator/starscan/pcscan_unittest.cc
+++ base/allocator/partition_allocator/starscan/pcscan_unittest.cc
@@ -411,6 +411,7 @@ TEST_F(PCScanTest, DoubleFree) {
 }
 #endif
 
+#if !PCSCAN_DISABLE_SAFEPOINTS
 namespace {
 template <typename SourceList, typename ValueList>
 void TestDanglingReferenceWithSafepoint(PCScanTest& test,
@@ -469,6 +470,7 @@ TEST_F(PCScanTest, Safepoint) {
 
   TestDanglingReferenceWithSafepoint(*this, source, value);
 }
+#endif  // PCSCAN_DISABLE_SAFEPOINTS
 
 }  // namespace internal
 }  // namespace base
