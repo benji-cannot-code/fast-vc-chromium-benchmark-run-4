75c07d4a7cc1bab175a387ffe1e0fcbedd8c5905
Peter Kotwicz <pkotwicz@chromium.org>
2021-03-22 17:21:10 +0000

    Disable failing TabModelImplTest.hasOtherRelatedTabs_detectMergedTabs
    BUG=1190854
    
    Owners-Override: True
    Change-Id: Ia69fa7b3e2d01ac6242483a7be17e938b51d58fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2778631
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865224}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelImplTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelImplTest.java
@@ -158,6 +158,7 @@ public class TabModelImplTest {
     @Test
     @SmallTest
     @EnableFeatures({ChromeFeatureList.TAB_GROUPS_ANDROID})
+    @DisabledTest(message = "https://crbug.com/1190854")
     public void hasOtherRelatedTabs_detectMergedTabs() throws Exception {
         createTabs(3, false, mTestUrl);
 
