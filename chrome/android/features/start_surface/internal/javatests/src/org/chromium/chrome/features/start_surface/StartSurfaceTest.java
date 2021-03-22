2dd22588e8e9117b3dfe11ff446b6d7ef27261ca
Xi Han <hanxi@google.com>
2021-03-22 14:29:08 +0000

    [Start] Disable testOmnibox_FocusedOnNewTabInSingleSurface(V2).* on emulators.
    1. Disable testOmnibox_FocusedOnNewTabInSingleSurface__* on M emulators;
    2. Disable testOmnibox_FocusedOnNewTabInSingleSurfaceV2__* on all
       emulators.
    
    Bug: 1170553
    Change-Id: I3b811ed4b6a1ff3c07211cd90c4f02daef589f98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2777813
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865148}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
@@ -1724,8 +1724,11 @@ public class StartSurfaceTest {
     @LargeTest
     @Feature({"StartSurface"})
     @DisableIf.Build(sdk_is_less_than = M, message = "https://crbug.com/1170553")
+    @DisableIf.Build(sdk_is_less_than = N, supported_abis_includes = "x86",
+            message = "https://crbug.com/1170553")
     @CommandLineFlags.Add({BASE_PARAMS + "/single/omnibox_focused_on_new_tab/true"})
-    public void testOmnibox_FocusedOnNewTabInSingleSurface() {
+    public void
+    testOmnibox_FocusedOnNewTabInSingleSurface() {
         if (!mImmediateReturn) {
             pressHomePageButton();
         }
@@ -1796,6 +1799,7 @@ public class StartSurfaceTest {
     @LargeTest
     @Feature({"StartSurface"})
     @DisableIf.Build(sdk_is_less_than = M, message = "https://crbug.com/1170553")
+    @DisableIf.Build(supported_abis_includes = "x86", message = "https://crbug.com/1170553")
     // clang-format off
     @CommandLineFlags.Add({BASE_PARAMS + "/single/show_last_active_tab_only/true" +
             "/exclude_mv_tiles/true/omnibox_focused_on_new_tab/true"})
