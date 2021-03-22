9930bb09e45453b4f0a0c06658686bd785b7dcf6
Fabrice de Gans-Riberi <fdegans@chromium.org>
2021-03-22 22:28:27 +0000

    Convert use of base::CallbackList in //net
    The last remaining use was for an obersver style callback list. It has
    been converted to a RepeatingCallbackList.
    
    Bug: 1113007
    Change-Id: I3cc9ead0ee87a4beb5ddd24c5f734c281871f342
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2779702
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Auto-Submit: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865391}

--- net/cookies/cookie_monster_change_dispatcher.h
+++ net/cookies/cookie_monster_change_dispatcher.h
@@ -30,7 +30,7 @@ class CookieMonster;
 class CookieMonsterChangeDispatcher : public CookieChangeDispatcher {
  public:
   using CookieChangeCallbackList =
-      base::CallbackList<void(const CookieChangeInfo&)>;
+      base::RepeatingCallbackList<void(const CookieChangeInfo&)>;
 
   // Expects |cookie_monster| to outlive this.
   explicit CookieMonsterChangeDispatcher(const CookieMonster* cookie_monster);
