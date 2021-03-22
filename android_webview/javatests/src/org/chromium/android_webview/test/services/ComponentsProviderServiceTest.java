4106953e461fafd9c7a722787e0eaaeb8ee6ca76
Laís Minchillo <laisminchillo@chromium.org>
2021-03-22 22:51:26 +0000

    [AW] ComponentsProviderService: schedule updater
    In ComponentsProviderService, schedule a periodic job for
    AwComponentUpdateService if there isn't already a scheduled
    job for this.
    
    Add a test that the service is scheduled in ComponentsProviderService
    onCreate().
    
    Bug: 1179005
    Test: run_webview_instrumentation_test_apk -f "*ComponentsProviderServiceTest*"
    Change-Id: I2f131de80e9629fdca5f30e659048406696cf219
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2704556
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Laís Minchillo <laisminchillo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865403}

--- android_webview/javatests/src/org/chromium/android_webview/test/services/ComponentsProviderServiceTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/services/ComponentsProviderServiceTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test.services;
 
+import android.app.job.JobScheduler;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -27,6 +28,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.FileUtils;
 import org.chromium.base.PathUtils;
 import org.chromium.base.test.util.Batch;
+import org.chromium.components.background_task_scheduler.TaskIds;
 import org.chromium.components.component_updater.IComponentsProviderService;
 
 import java.io.File;
@@ -190,6 +192,21 @@ public class ComponentsProviderServiceTest {
 
             Assert.assertTrue("Service didn't create directory " + sDirectory.getAbsolutePath(),
                     sDirectory.exists());
+        }
+
+        @Test
+        @SmallTest
+        public void testOnCreateSchedulesUpdater() throws Exception {
+            JobScheduler jobScheduler =
+                    (JobScheduler) ContextUtils.getApplicationContext().getSystemService(
+                            Context.JOB_SCHEDULER_SERVICE);
+            jobScheduler.cancelAll();
+
+            mService.onCreate();
+
+            Assert.assertTrue("Service should schedule updater job",
+                    ComponentsProviderService.isJobScheduled(
+                            jobScheduler, TaskIds.WEBVIEW_COMPONENT_UPDATE_JOB_ID));
         }
 
         @Test
--- android_webview/junit/src/org/chromium/android_webview/robolectric/common/services/ServiceNamesTest.java
+++ android_webview/junit/src/org/chromium/android_webview/robolectric/common/services/ServiceNamesTest.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
 
 import org.chromium.android_webview.common.services.ServiceNames;
+import org.chromium.android_webview.nonembedded.AwComponentUpdateService;
 import org.chromium.android_webview.services.AwMinidumpUploadJobService;
 import org.chromium.android_webview.services.ComponentsProviderService;
 import org.chromium.android_webview.services.CrashReceiverService;
@@ -45,6 +46,8 @@ public class ServiceNamesTest {
                 ServiceNames.VARIATIONS_SEED_SERVER);
         Assert.assertEquals("Incorrect class name constant",
                 ComponentsProviderService.class.getName(),
-                ServiceNames.AW_COMPONENTS_PROVIDER_SERVICE);
+                ServiceNames.COMPONENTS_PROVIDER_SERVICE);
+        Assert.assertEquals("Incorrect class name constant",
+                AwComponentUpdateService.class.getName(), ServiceNames.AW_COMPONENT_UPDATE_SERVICE);
     }
 }
