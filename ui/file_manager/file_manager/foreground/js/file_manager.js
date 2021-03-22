9b6c85263cb37ad16496cc4605f6753236547f5c
Alex Danilo <adanilo@chromium.org>
2021-03-22 05:19:34 +0000

    Files app: Enable optimize_webui=false to build JS as debug
    Enables the individual modules for:
    1. Foreground page.
    2. The deferred elements (secondary surfaces).
    3. The Shared Worker (used for the Content Metadata Provider).
    4. The background page.
    
    NOTE: The path for the metadata_dispatcher.m.js was missing the part:
    `metadata/` in the `resource_path_rewrites` and is fixed here because
    the path in the rollup has to match the path in the disk.
    
    NB: This is CL:2676023 by lucmult@ reapplied to the main branch.
    
    Bug: 1030998
    Change-Id: I509e35d3d4c620fbd4ec4a2fd322aeb055e617cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2777474
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865038}

--- ui/file_manager/file_manager/foreground/js/file_manager.js
+++ ui/file_manager/file_manager/foreground/js/file_manager.js
@@ -1045,7 +1045,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     if (util.isFilesJsModulesEnabled()) {
       ContentMetadataProvider.configure(
-          'foreground/js/metadata_dispatcher.m.js', /*isModule=*/ true);
+          'foreground/js/metadata/metadata_dispatcher.m.js',
+          /*isModule=*/ true);
     } else if (window.isSWA) {
       ContentMetadataProvider.configure(
           'foreground/js/metadata/metadata_dispatcher.js');
