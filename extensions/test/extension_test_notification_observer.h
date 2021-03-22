ece67b89086f29cde13bde4313bfe0e16f691382
Fabrice de Gans-Riberi <fdegans@chromium.org>
2021-03-22 22:46:31 +0000

    Convert use of base::CallbackList in //extensions
    The last remaining use was for an obersver style closure list. It has
    been converted to a RepeatingClosureList.
    
    Bug: 1113007
    Change-Id: I24a2a5c70ce3316dfcf48c930e842e0c278bc4bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2779637
    Auto-Submit: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865401}

--- extensions/test/extension_test_notification_observer.h
+++ extensions/test/extension_test_notification_observer.h
@@ -86,7 +86,7 @@ class ExtensionTestNotificationObserver : public content::NotificationObserver,
 
     // Notified any time an Add()ed notification is received.
     // The details of the notification are dropped.
-    base::CallbackList<void()>& callback_list() { return callback_list_; }
+    base::RepeatingClosureList& closure_list() { return closure_list_; }
 
    private:
     class ForwardingWebContentsObserver;
@@ -104,7 +104,7 @@ class ExtensionTestNotificationObserver : public content::NotificationObserver,
     void WebContentsDestroyed(content::WebContents* web_contents);
 
     content::NotificationRegistrar notification_registrar_;
-    base::CallbackList<void()> callback_list_;
+    base::RepeatingClosureList closure_list_;
     ScopedObserver<extensions::ProcessManager,
                    extensions::ProcessManagerObserver>
         process_manager_observer_{this};
