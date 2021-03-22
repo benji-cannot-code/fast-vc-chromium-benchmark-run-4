aa536f06164ed7fd236e20417d0ce4cdfcee885f
Ioana Pandele <ioanap@chromium.org>
2021-03-22 14:50:17 +0000

    Revert "Add Google News to SystemFeaturesDisableList policy"
    This reverts commit a04fa7dd9cdebe7b4e6478bdf27cdf140c26df3e.
    
    Reason for revert: Speculative revert. This might have caused failures in:
    virtual/unified-autoplay/external/wpt/feature-policy/feature-policy-frame-policy-timing.https.sub.html
    
    and
    
    benchmarks.system_health_smoke_test.SystemHealthBenchmarkSmokeTest.system_health.memory_desktop/browse:news:cnn:2021
    
    Original change's description:
    > Add Google News to SystemFeaturesDisableList policy
    >
    > Bug: 1142785
    > Change-Id: I5ab311615bc74e5693a902b491fc3138b5104566
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2767322
    > Commit-Queue: Sanja Perisic <sanjaperisic@chromium.org>
    > Reviewed-by: Anqing Zhao <anqing@chromium.org>
    > Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#865107}
    
    Bug: 1142785, 1190850
    Change-Id: Ib1682a5876f0352efd322f3bf7ebed01d87005a6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2778396
    Auto-Submit: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#865156}

--- chrome/browser/chromeos/policy/system_features_disable_list_policy_handler.cc
+++ chrome/browser/chromeos/policy/system_features_disable_list_policy_handler.cc
@@ -22,7 +22,6 @@ const char kOsSettingsFeature[] = "os_settings";
 const char kScanningFeature[] = "scanning";
 const char kWebStoreFeature[] = "web_store";
 const char kCanvasFeature[] = "canvas";
-const char kGoogleNewsFeature[] = "google_news";
 
 const char kBlockedDisableMode[] = "blocked";
 const char kHiddenDisableMode[] = "hidden";
@@ -48,8 +47,6 @@ SystemFeature SystemFeaturesDisableListPolicyHandler::GetSystemFeatureFromAppId(
     const std::string& app_id) {
   if (app_id == web_app::kCanvasAppId)
     return SystemFeature::kCanvas;
-  if (app_id == web_app::kGoogleNewsAppId)
-    return SystemFeature::kGoogleNews;
   return SystemFeature::kUnknownSystemFeature;
 }
 
@@ -96,8 +93,6 @@ SystemFeature SystemFeaturesDisableListPolicyHandler::ConvertToEnum(
     return SystemFeature::kWebStore;
   if (system_feature == kCanvasFeature)
     return SystemFeature::kCanvas;
-  if (system_feature == kGoogleNewsFeature)
-    return SystemFeature::kGoogleNews;
 
   LOG(ERROR) << "Unsupported system feature: " << system_feature;
   return kUnknownSystemFeature;
