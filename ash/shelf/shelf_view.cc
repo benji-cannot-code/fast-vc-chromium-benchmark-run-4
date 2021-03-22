5f3ca8c84346dc594d55ca02f646219ea3f06fc1
Hidehiko Abe <hidehiko@chromium.org>
2021-03-22 17:28:05 +0000

    Add TYPE_UNPINNED_BROWSER_SHORTCUT.
    For Lacros Primary, we'll unpin the browser shortcut item on the shelf.
    This CL adds a new type of shelf items for it.
    Note that it is not used yet, but follow CL will.
    
    BUG=1188020
    TEST=Ran ash_unittests.
    
    Change-Id: I23dcd5453f69611a96d18686d0a57ad52805736f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774847
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865230}

--- ash/shelf/shelf_view.cc
+++ ash/shelf/shelf_view.cc
@@ -221,6 +221,7 @@ bool ShelfButtonIsInDrag(const ShelfItemType item_type,
     case TYPE_PINNED_APP:
     case TYPE_BROWSER_SHORTCUT:
     case TYPE_APP:
+    case TYPE_UNPINNED_BROWSER_SHORTCUT:
       return static_cast<const ShelfAppButton*>(item_view)->state() &
              ShelfAppButton::STATE_DRAGGING;
     case TYPE_DIALOG:
@@ -742,6 +743,7 @@ void ShelfView::ButtonPressed(views::Button* sender,
     case TYPE_PINNED_APP:
     case TYPE_BROWSER_SHORTCUT:
     case TYPE_APP:
+    case TYPE_UNPINNED_BROWSER_SHORTCUT:
       Shell::Get()->metrics()->RecordUserMetricsAction(
           UMA_LAUNCHER_CLICK_ON_APP);
       break;
@@ -1037,6 +1039,7 @@ views::View* ShelfView::CreateViewForItem(const ShelfItem& item) {
     case TYPE_PINNED_APP:
     case TYPE_BROWSER_SHORTCUT:
     case TYPE_APP:
+    case TYPE_UNPINNED_BROWSER_SHORTCUT:
     case TYPE_DIALOG: {
       ShelfAppButton* button = new ShelfAppButton(
           this, shelf_button_delegate_ ? shelf_button_delegate_ : this);
@@ -1793,7 +1796,8 @@ bool ShelfView::CanDragAcrossSeparator(views::View* drag_view) const {
 
   DCHECK(drag_view);
   // The dragged item is not allowed to be unpinned if |drag_view| is pinned by
-  // policy, dragged from app list, or its item type is TYPE_BROWSER_SHORTCUT.
+  // policy, dragged from app list, or its item type is TYPE_BROWSER_SHORTCUT
+  // or TYPE_UNPINNED_BROWSER_SHORTCUT.
   // Therefore, the |drag_view| can not be dragged across the separator.
   bool can_change_pin_state =
       ShelfItemForView(drag_view)->type == TYPE_PINNED_APP ||
@@ -2129,6 +2133,7 @@ void ShelfView::ShelfItemChanged(int model_index, const ShelfItem& old_item) {
     case TYPE_PINNED_APP:
     case TYPE_BROWSER_SHORTCUT:
     case TYPE_APP:
+    case TYPE_UNPINNED_BROWSER_SHORTCUT:
     case TYPE_DIALOG: {
       CHECK_EQ(ShelfAppButton::kViewClassName, view->GetClassName());
       ShelfAppButton* button = static_cast<ShelfAppButton*>(view);
