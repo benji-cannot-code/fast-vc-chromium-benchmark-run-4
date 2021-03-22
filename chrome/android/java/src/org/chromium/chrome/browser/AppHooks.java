a74e559b5e5db67b47f92d4e441f935d66ef6dc9
Jinsuk Kim <jinsukkim@chromium.org>
2021-03-22 01:36:14 +0000

    Android: New modularize build target for c.b.lens
    Adds dummy build targets for chrome.browser.lens package to be
    migrated.
    
    This is patch 1/7 for moving the package from AppHooks to its
    own build rule-based link.
    
    Bug: 1188049
    Change-Id: I0811d8c69c08e0a18616c171cb2b79d66a7c58ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2759575
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865018}

--- chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
+++ chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
@@ -64,6 +64,9 @@ import java.util.List;
 public abstract class AppHooks {
     private static AppHooksImpl sInstance;
 
+    @Nullable
+    private LensController mLensController;
+
     /**
      * Sets a mocked instance for testing.
      */
@@ -167,8 +170,18 @@ public abstract class AppHooks {
         return new InstantAppsHandler();
     }
 
-    public LensController createLensController() {
+    /** Creates a new instance of LensController. */
+    protected LensController createLensController() {
         return new LensController();
+    }
+
+    /**
+     * Return LensController. Create a new one if not available.
+     **/
+    public LensController getLensController() {
+        if (mLensController == null) mLensController = createLensController();
+
+        return mLensController;
     }
 
     /**
