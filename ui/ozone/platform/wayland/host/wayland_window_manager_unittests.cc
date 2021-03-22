5022112e32609b959ec3d71924d464780af3cc7e
Maksim Sisov <msisov@igalia.com>
2021-03-22 10:00:20 +0000

    ozone/wayland: prefer output with largest scale
    Before this CL, outputs were stored into flat_set that
    were sorted, which resulted in usage of a display that has the
    lowest id (the first element was used). This didn't look right
    and could result in weird output when user drags the window
    between two display with different dpi (the surface is 2x
    size, but the contents are rendered at 1x size).
    
    To fix the issue, the outputs are now stored into std::vector
    as sorting is not required. Secondly, WaylandWindow has a concept
    of a preferred output. That is, WaylandWindow now prefers an output
    that has a largest scale. If there are >2 outputs with the same
    scale value, the very first entered output with largest scale is
    used. If scale is the same, the very first entered output is returned.
    
    Bug: 1190141
    Change-Id: I3d5fe4bf17d11f17736e80c927f9ce78c16ab3c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2775567
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865089}

--- ui/ozone/platform/wayland/host/wayland_window_manager_unittests.cc
+++ ui/ozone/platform/wayland/host/wayland_window_manager_unittests.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/ozone/platform/wayland/host/wayland_output.h"
 #include "ui/ozone/platform/wayland/test/mock_pointer.h"
 #include "ui/ozone/platform/wayland/test/mock_surface.h"
 #include "ui/ozone/platform/wayland/test/test_keyboard.h"
@@ -185,10 +186,10 @@ TEST_P(WaylandWindowManagerTest, GetWindowsOnOutput) {
 
   Sync();
 
-  auto entered_outputs_window1 = window1->entered_outputs_ids();
+  auto entered_outputs_window1 = window1->entered_outputs();
   EXPECT_EQ(1u, entered_outputs_window1.size());
 
-  uint32_t output_id = *entered_outputs_window1.begin();
+  uint32_t output_id = (*entered_outputs_window1.begin())->output_id();
 
   auto windows_on_output = manager_->GetWindowsOnOutput(output_id);
   EXPECT_EQ(1u, windows_on_output.size());
