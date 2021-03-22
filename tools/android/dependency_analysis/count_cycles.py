10ebc83b6639811e35a346d3f2e1915e86f0af23
Henrique Nakashima <hnakashima@chromium.org>
2021-03-22 22:17:26 +0000

    [Lorenz] diff_graphs.py prints added and removed package cycles
    Example output (shortened):
    
    $ tools/android/dependency_analysis/diff_graphs.py -b 2021-02-22_23-15_monochrome_public_bundle.json -a 2021-02-23_22-38_monochrome_public_bundle.json --package-cycles 4
    Total Java class count: +5 (8483 -> 8488)
    Total Java package count: +0 (693 -> 693)
    
    Java classes added (+) and removed (-):
    + [   1] org.chromium.android_webview.nonembedded.DownloadFileTask
    ...
    + [   9] org.chromium.viz.mojom.ResourceId
    - [   1] org.chromium.blink.mojom.ClipboardFile
    ...
    - [   4] org.chromium.chrome.browser.password_check.helper.PasswordCheckReauthenticationHelper
    
    Java packages - no changes
    
    Total Java package cycle count (up to size 4): -308 (16471 -> 16163)
    
    Java package cycles added (+) and removed (-):
    - [   1] .c.b.app > .c.b.app.appmenu > .c.b.compositor.layouts > .c.b.native_page > .c.b.app
    - [   2] .c.b.app > .c.b.app.appmenu > .c.b.tasks.tab_management > .c.b.native_page > .c.b.app
    - [   3] .c.b.app > .c.b.app.flags > .c.b.tasks.tab_management > .c.b.native_page > .c.b.app
    - [   4] .c.b.app > .c.b.app.tabmodel > .c.b.compositor.layouts > .c.b.native_page > .c.b.app
    ...
    - [ 308] .chrome.browser > .chrome.features.start_surface > .c.b.toolbar > .c.b.omnibox > .chrome.browser
    
    
    Bug: 1166855
    Change-Id: I2e20417370e8ab786f46510595da4c56dc164ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720820
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865382}

--- tools/android/dependency_analysis/count_cycles.py
+++ tools/android/dependency_analysis/count_cycles.py
@@ -8,10 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import argparse
 import collections
 
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 import serialization
 import graph
+
+
+Cycle = Tuple[graph.Node, ...]
 
 
 def find_cycles_from_node(
@@ -102,7 +105,7 @@ def find_cycles_from_node(
 
 
 def find_cycles(base_graph: graph.Graph,
-                max_cycle_length: int) -> List[List[List[graph.Node]]]:
+                max_cycle_length: int) -> List[List[Cycle]]:
     """Finds all cycles in the graph within a certain length.
 
     The algorithm is as such: Number the nodes arbitrarily. For i from 0 to
@@ -133,7 +136,13 @@ def find_cycles(base_graph: graph.Graph,
                                                   node_to_id)
         for cycle_length, cycle_list in enumerate(start_node_cycles):
             cycles[cycle_length].extend(cycle_list)
-    return cycles
+
+    # Convert cycles to be tuples of nodes, so the cycles are hashable and
+    # immutable.
+    immutable_cycles = []
+    for cycle_list in cycles:
+        immutable_cycles.append([tuple(cycle) for cycle in cycle_list])
+    return immutable_cycles
 
 
 def main():
