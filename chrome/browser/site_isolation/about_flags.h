69116c4c46e6ea52cce63eba3553bc29936410d4
Mattias Nissler <mnissler@chromium.org>
2021-03-22 15:50:46 +0000

    Use SetFeatureFlagsForUser when applying flags.
    On Chrome OS, Chrome asks session_manager to apply feature flags on
    restart. Previously the flags would be passed as raw command line
    switches, but session_manager now maintains a separate set of feature
    flags that it passes in encoded form in a command line switch. This
    change adjusts Chrome to make use of that facility and deletes a bunch
    of support code for the legacy Chrome OS approach.
    
    BUG=chromium:1073940
    TEST=Existing tests (e.g. SiteIsolationFlagHandlingTest), additional manual testing
    
    Change-Id: I311960e2fad45136b3cc14a1fc99581b0d2d9001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2762738
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Mattias Nissler <mnissler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865178}

---
+++ chrome/browser/site_isolation/about_flags.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SITE_ISOLATION_ABOUT_FLAGS_H_
+#define CHROME_BROWSER_SITE_ISOLATION_ABOUT_FLAGS_H_
+
+#include <string>
+
+#include "base/strings/strcat.h"
+
+namespace about_flags {
+
+constexpr char kSiteIsolationTrialOptOutInternalName[] =
+    "site-isolation-trial-opt-out";
+
+inline std::string SiteIsolationTrialOptOutChoiceEnabled() {
+  return base::StrCat({kSiteIsolationTrialOptOutInternalName, "@1"});
+}
+
+}  // namespace about_flags
+
+#endif  // CHROME_BROWSER_SITE_ISOLATION_ABOUT_FLAGS_H_
