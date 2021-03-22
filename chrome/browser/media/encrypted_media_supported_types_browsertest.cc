982ff39b5695af32f7bdb590e5d16297bfba49ea
Xiaohan Wang <xhwang@chromium.org>
2021-03-22 21:23:52 +0000

    media: Remove kWidevineAv1* features
    These were added when adding AV1 support to the Widevine CDM around
    Chrome M78. Remove these features as we don't need them any more.
    
    Bug: 953507
    Change-Id: I5922ec8bda289e48a5a413ac7aa81b114dc8be22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2776577
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865359}

--- chrome/browser/media/encrypted_media_supported_types_browsertest.cc
+++ chrome/browser/media/encrypted_media_supported_types_browsertest.cc
@@ -470,9 +470,7 @@ class EncryptedMediaSupportedTypesExternalClearKeyNotEnabledTest
 class EncryptedMediaSupportedTypesWidevineTest
     : public EncryptedMediaSupportedTypesTest {
  protected:
-  EncryptedMediaSupportedTypesWidevineTest() {
-    scoped_feature_list_.InitAndEnableFeature(media::kWidevineAv1);
-  }
+  EncryptedMediaSupportedTypesWidevineTest() = default;
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
     EncryptedMediaSupportedTypesTest::SetUpCommandLine(command_line);
