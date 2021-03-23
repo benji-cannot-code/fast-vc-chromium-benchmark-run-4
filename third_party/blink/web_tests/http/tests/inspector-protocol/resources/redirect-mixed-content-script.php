dfad36d22be9ace764289a213196656b45c1a65c
Simon Zünd <szuend@chromium.org>
2021-03-23 10:07:49 +0000

    [devtools] Add regression test for mixed content issues via redirect
    This CL adds an inspector-protocol test verifying that the
    AffectedRequest is included in Mixed Content Issues stemming from
    server redirects that originate from the renderer:
    
      1) Initiate script load from renderer (e.g. via document.write ing
         a <script> tag) from a HTTPS origin
      2) Server redirects to a HTTP origin
      3) Renderer tries to load script via HTTP origin
    
    The Mixed Content issue filed in step 3 does currently not include
    the requestId. The ResourceRequest created for redirecting, and that
    is used for the Mixed Content check is transparent to the network
    agent. The two "requestWillBeSent" Network events are filed with the
    same "requestId", namely the ResourceRequest created in step 1.
    
    Please note that this CL doesn't actually include a fix and the test
    needs to be rebaselined once the requestId is actually included.
    
    R=sigurds@chromium.org
    
    Bug: chromium:1190808
    Change-Id: Ic72d49ddf06d468cdb9639a25da7e024ed4b81ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780260
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865576}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/resources/redirect-mixed-content-script.php
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header('HTTP/1.1 302 Found');
+header('Location: http://devtools.test:8000/inspector-protocol/resources/empty.js');
+?>
