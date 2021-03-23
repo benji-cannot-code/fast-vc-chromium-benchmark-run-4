9913c5fa9292caf2cb36942369f767aafc0d1365
Colin Blundell <blundell@chromium.org>
2021-03-23 08:25:07 +0000

    [WebLayer] Bring up browsertests of mobile ad density intervention
    This CL ports //chrome's browsertests of the Android-specific ad density
    intervention to //weblayer. As part of this, we move helper functions
    out of the //chrome browsertest into the //components-level shared util
    file.
    
    The browsertests work out of the box in WebLayer.
    
    Bug: 1116095
    Change-Id: Ic0df545ba40a87bf6dd5eeb29f524984ddda350b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2760437
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865551}

--- weblayer/browser/android/ad_density_intervention_browsertest.cc
+++ weblayer/browser/android/ad_density_intervention_browsertest.cc
@@ -1,22 +1,18 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2020 The Chromium Authors. All rights reserved.
+// Copyright 2021 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include <memory>
 
 #include "base/test/scoped_feature_list.h"
-#include "build/build_config.h"
-#include "chrome/browser/infobars/infobar_service.h"
-#include "chrome/browser/subresource_filter/subresource_filter_browser_test_harness.h"
-#include "chrome/common/chrome_features.h"
-#include "chrome/test/base/chrome_test_utils.h"
 #include "components/infobars/core/infobar.h"
 #include "components/infobars/core/infobar_delegate.h"
 #include "components/infobars/core/infobar_manager.h"
 #include "components/page_load_metrics/browser/observers/ad_metrics/ad_intervention_browser_test_utils.h"
 #include "components/page_load_metrics/browser/page_load_metrics_test_waiter.h"
 #include "components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.h"
+#include "components/subresource_filter/core/browser/subresource_filter_features.h"
 #include "components/subresource_filter/core/common/common_features.h"
 #include "components/subresource_filter/core/common/test_ruleset_utils.h"
 #include "components/subresource_filter/core/mojom/subresource_filter.mojom.h"
@@ -28,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/widget/screen_info.h"
 #include "ui/gfx/geometry/rect.h"
 #include "url/gurl.h"
+#include "weblayer/browser/infobar_service.h"
+#include "weblayer/test/subresource_filter_browser_test_harness.h"
+
+namespace weblayer {
 
 namespace {
 
@@ -36,8 +36,7 @@ const char kAdsInterventionRecordedHistogram[] =
 
 }  // namespace
 
-class AdDensityViolationBrowserTest
-    : public subresource_filter::SubresourceFilterBrowserTest {
+class AdDensityViolationBrowserTest : public SubresourceFilterBrowserTest {
  public:
   AdDensityViolationBrowserTest() = default;
 
@@ -48,7 +47,7 @@ class AdDensityViolationBrowserTest
     std::vector<base::Feature> disabled = {};
 
     feature_list_.InitWithFeatures(enabled, disabled);
-    subresource_filter::SubresourceFilterBrowserTest::SetUp();
+    SubresourceFilterBrowserTest::SetUp();
   }
 
   void SetUpOnMainThread() override {
@@ -67,23 +66,21 @@ IN_PROC_BROWSER_TEST_F(
   base::HistogramTester histogram_tester;
   ukm::TestAutoSetUkmRecorder ukm_recorder;
 
-  content::WebContents* web_contents =
-      chrome_test_utils::GetActiveWebContents(this);
   auto waiter = std::make_unique<page_load_metrics::PageLoadMetricsTestWaiter>(
-      web_contents);
+      web_contents());
   const GURL url(embedded_test_server()->GetURL(
       "a.com", "/ads_observer/blank_with_adiframe_writer.html"));
 
   waiter->SetMainFrameIntersectionExpectation();
-  EXPECT_TRUE(content::NavigateToURL(web_contents, url));
+  EXPECT_TRUE(content::NavigateToURL(web_contents(), url));
   waiter->Wait();
 
-  int document_height = GetDocumentHeight(web_contents);
+  int document_height = GetDocumentHeight(web_contents());
 
   int frame_width = 100;  // Ad density by height is independent of frame width.
   int frame_height = document_height * 0.45;
 
-  CreateAndWaitForIframeAtRect(web_contents, waiter.get(),
+  CreateAndWaitForIframeAtRect(web_contents(), waiter.get(),
                                embedded_test_server(),
                                gfx::Rect(0, 0, frame_width, frame_height));
 
@@ -91,13 +88,14 @@ IN_PROC_BROWSER_TEST_F(
   // for the next page load.
   waiter.reset();
 
-  EXPECT_TRUE(content::NavigateToURL(web_contents, url));
+  EXPECT_TRUE(content::NavigateToURL(web_contents(), url));
 
   // blank_with_adiframe_writer loads a script tagged as an ad, verify it is not
   // loaded and the subresource filter UI for ad blocking is shown.
-  EXPECT_FALSE(WasParsedScriptElementLoaded(web_contents->GetMainFrame()));
-  EXPECT_EQ(InfoBarService::FromWebContents(web_contents)->infobar_count(), 1u);
-  EXPECT_EQ(InfoBarService::FromWebContents(web_contents)
+  EXPECT_FALSE(WasParsedScriptElementLoaded(web_contents()->GetMainFrame()));
+  EXPECT_EQ(InfoBarService::FromWebContents(web_contents())->infobar_count(),
+            1u);
+  EXPECT_EQ(InfoBarService::FromWebContents(web_contents())
                 ->infobar_at(0)
                 ->delegate()
                 ->GetIdentifier(),
@@ -115,23 +113,21 @@ IN_PROC_BROWSER_TEST_F(
   base::HistogramTester histogram_tester;
   ukm::TestAutoSetUkmRecorder ukm_recorder;
 
-  content::WebContents* web_contents =
-      chrome_test_utils::GetActiveWebContents(this);
   auto waiter = std::make_unique<page_load_metrics::PageLoadMetricsTestWaiter>(
-      web_contents);
+      web_contents());
   const GURL url(embedded_test_server()->GetURL(
       "a.com", "/ads_observer/blank_with_adiframe_writer.html"));
 
   waiter->SetMainFrameIntersectionExpectation();
-  EXPECT_TRUE(content::NavigateToURL(web_contents, url));
+  EXPECT_TRUE(content::NavigateToURL(web_contents(), url));
   waiter->Wait();
 
-  int document_height = GetDocumentHeight(web_contents);
+  int document_height = GetDocumentHeight(web_contents());
 
   int frame_width = 100;  // Ad density by height is independent of frame width.
   int frame_height = document_height * 0.25;
 
-  CreateAndWaitForIframeAtRect(web_contents, waiter.get(),
+  CreateAndWaitForIframeAtRect(web_contents(), waiter.get(),
                                embedded_test_server(),
                                gfx::Rect(0, 0, frame_width, frame_height));
 
@@ -139,21 +135,22 @@ IN_PROC_BROWSER_TEST_F(
   // for the next page load.
   waiter.reset();
 
-  EXPECT_TRUE(content::NavigateToURL(web_contents, url));
+  EXPECT_TRUE(content::NavigateToURL(web_contents(), url));
 
   // blank_with_adiframe_writer loads a script tagged as an ad, verify it is
   // loaded as ads are not blocked and the subresource filter UI is not
   // shown.
-  EXPECT_TRUE(WasParsedScriptElementLoaded(web_contents->GetMainFrame()));
+  EXPECT_TRUE(WasParsedScriptElementLoaded(web_contents()->GetMainFrame()));
 
   // No ads blocked infobar should be shown as we have not triggered the
   // intervention.
-  EXPECT_EQ(InfoBarService::FromWebContents(web_contents)->infobar_count(), 0u);
+  EXPECT_EQ(InfoBarService::FromWebContents(web_contents())->infobar_count(),
+            0u);
   histogram_tester.ExpectTotalCount(kAdsInterventionRecordedHistogram, 0);
 }
 
 class AdDensityViolationBrowserTestWithoutEnforcement
-    : public subresource_filter::SubresourceFilterBrowserTest {
+    : public SubresourceFilterBrowserTest {
  public:
   AdDensityViolationBrowserTestWithoutEnforcement() = default;
 
@@ -163,7 +160,7 @@ class AdDensityViolationBrowserTestWithoutEnforcement
         subresource_filter::kAdsInterventionsEnforced};
 
     feature_list_.InitWithFeatures(enabled, disabled);
-    subresource_filter::SubresourceFilterBrowserTest::SetUp();
+    SubresourceFilterBrowserTest::SetUp();
   }
 
   void SetUpOnMainThread() override {
@@ -182,23 +179,21 @@ IN_PROC_BROWSER_TEST_F(
   base::HistogramTester histogram_tester;
   ukm::TestAutoSetUkmRecorder ukm_recorder;
 
-  content::WebContents* web_contents =
-      chrome_test_utils::GetActiveWebContents(this);
   auto waiter = std::make_unique<page_load_metrics::PageLoadMetricsTestWaiter>(
-      web_contents);
+      web_contents());
   const GURL url(embedded_test_server()->GetURL(
       "a.com", "/ads_observer/blank_with_adiframe_writer.html"));
 
   waiter->SetMainFrameIntersectionExpectation();
-  EXPECT_TRUE(content::NavigateToURL(web_contents, url));
+  EXPECT_TRUE(content::NavigateToURL(web_contents(), url));
   waiter->Wait();
 
-  int document_height = GetDocumentHeight(web_contents);
+  int document_height = GetDocumentHeight(web_contents());
 
   int frame_width = 100;  // Ad density by height is independent of frame width.
   int frame_height = document_height * 0.45;
 
-  CreateAndWaitForIframeAtRect(web_contents, waiter.get(),
+  CreateAndWaitForIframeAtRect(web_contents(), waiter.get(),
                                embedded_test_server(),
                                gfx::Rect(0, 0, frame_width, frame_height));
 
@@ -206,18 +201,21 @@ IN_PROC_BROWSER_TEST_F(
   // for the next page load.
   waiter.reset();
 
-  EXPECT_TRUE(content::NavigateToURL(web_contents, url));
+  EXPECT_TRUE(content::NavigateToURL(web_contents(), url));
 
   // We are not enforcing ad blocking on ads violations, site should load
   // as expected without subresource filter UI.
-  EXPECT_TRUE(WasParsedScriptElementLoaded(web_contents->GetMainFrame()));
+  EXPECT_TRUE(WasParsedScriptElementLoaded(web_contents()->GetMainFrame()));
 
   // No ads blocked infobar should be shown as we have not triggered the
   // intervention.
-  EXPECT_EQ(InfoBarService::FromWebContents(web_contents)->infobar_count(), 0u);
+  EXPECT_EQ(InfoBarService::FromWebContents(web_contents())->infobar_count(),
+            0u);
   histogram_tester.ExpectBucketCount(
       kAdsInterventionRecordedHistogram,
       static_cast<int>(subresource_filter::mojom::AdsViolation::
                            kMobileAdDensityByHeightAbove30),
       1);
 }
+
+}  // namespace weblayer
