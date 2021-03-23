1e8c3583a54dae6af56018d5ddcc6ea2242a0b52
Peter Kotwicz <pkotwicz@chromium.org>
2021-03-23 08:48:30 +0000

    Disable flaky SingleClientDeviceInfoSyncTest.*TransportOnly
    BUG=1191225
    
    Owners-Override: True
    Change-Id: Ib61c31a0641641161d4021830a7c6e913bb2b2f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780339
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#865554}

--- chrome/browser/sync/test/integration/single_client_device_info_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_device_info_sync_test.cc
@@ -119,6 +119,9 @@ IN_PROC_BROWSER_TEST_F(SingleClientDeviceInfoSyncTest, DownloadRemoteDevices) {
                             HasCacheGuid(CacheGuidForSuffix(2))}));
 }
 
+// CommitLocalDevice_TransportOnly and DownloadRemoteDevices_TransportOnly are
+// flaky on Android.
+#if !defined(OS_ANDROID)
 IN_PROC_BROWSER_TEST_F(SingleClientDeviceInfoSyncTest,
                        CommitLocalDevice_TransportOnly) {
   ASSERT_TRUE(SetupClients());
@@ -171,6 +174,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientDeviceInfoSyncTest,
               IsSupersetOf({HasCacheGuid(CacheGuidForSuffix(1)),
                             HasCacheGuid(CacheGuidForSuffix(2))}));
 }
+#endif  // !defined(OS_ANDROID)
 
 IN_PROC_BROWSER_TEST_F(SingleClientDeviceInfoSyncTest,
                        ShouldSetTheOnlyClientFlag) {
