5bc4e230fa9440894f2bd1913035cf3367c83db9
Jinsuk Kim <jinsukkim@chromium.org>
2021-03-23 02:49:13 +0000

    Android: Move lens classes to the modularized build target
    Completes the modularization of c.b.lens.
    
    This is patch 3/7 for moving the package from AppHooks to its
    own build rule-based link.
    
    Bug: 1188049
    Change-Id: I3b629a114352a71576f28e83027789dd529eefc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2759814
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865493}

--- chrome/browser/lens/java/src/org/chromium/chrome/browser/lens/LensController.java
+++ chrome/browser/lens/java/src/org/chromium/chrome/browser/lens/LensController.java
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 package org.chromium.chrome.browser.lens;
+
 import android.content.Intent;
 import android.net.Uri;
 
@@ -12,9 +13,9 @@ import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 
 import org.chromium.base.Callback;
-import org.chromium.chrome.R;
 import org.chromium.chrome.browser.contextmenu.ChipRenderParams;
 import org.chromium.ui.base.WindowAndroid;
+
 /**
  * A class which manages communication with the Lens SDK.
  */
