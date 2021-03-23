bd9c84b4bf43a448825d889e99833ab6799e7295
Justin Novosad <junov@chromium.org>
2021-03-23 02:21:13 +0000

    2D canvas roundRect: add radius validation
    New behaviour:
    * Fail silently if a radius value is non-finite.
    * Throw an IndexSizeError if a radius value is negative.
    
    Bug: 1190228
    Change-Id: Id4d836c942832cb6339327ce6e6be3a59ad3102e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2778791
    Auto-Submit: Justin Novosad <junov@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865476}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_path.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_path.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_CANVAS_CANVAS2D_CANVAS_PATH_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_CANVAS_CANVAS2D_CANVAS_PATH_H_
 
-#include "third_party/blink/renderer/bindings/modules/v8/double_or_dom_point.h"
+#include "third_party/blink/renderer/bindings/modules/v8/unrestricted_double_or_dom_point.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/no_alloc_direct_call_host.h"
 #include "third_party/blink/renderer/platform/graphics/path.h"
@@ -91,7 +91,7 @@ class MODULES_EXPORT CanvasPath : public NoAllocDirectCallHost {
                  double double_y,
                  double double_width,
                  double double_height,
-                 const HeapVector<DoubleOrDOMPoint, 0> radii,
+                 const HeapVector<UnrestrictedDoubleOrDOMPoint, 0> radii,
                  ExceptionState&);
 
   virtual bool IsTransformInvertible() const { return true; }
