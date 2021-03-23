d8449baa53ffd487df5289df86987988d182409c
Henrique Ferreiro <hferreiro@igalia.com>
2021-03-23 14:38:34 +0000

    Migrate chrome/browser/{chromeos => ash}/arc [2/9]
    bluetooth/, boot_phase_monitor/, cast_receiver/, enterprise/,
    extensions/ and file_system_watcher/ are moved to
    chrome/browser/ash/arc/.
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: I495f2cf5987dee0401ddf1c8070d24d32fbf5b6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2760040
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Auto-Submit: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865623}

--- chrome/browser/chromeos/arc/arc_support_host.h
+++ chrome/browser/chromeos/arc/arc_support_host.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
-#include "chrome/browser/chromeos/arc/extensions/arc_support_message_host.h"
+#include "chrome/browser/ash/arc/extensions/arc_support_message_host.h"
 #include "extensions/browser/api/messaging/native_message_host.h"
 #include "ui/display/display_observer.h"
 #include "url/gurl.h"
--- chrome/browser/chromeos/arc/instance_throttle/arc_instance_throttle.cc
+++ chrome/browser/chromeos/arc/instance_throttle/arc_instance_throttle.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
-#include "chrome/browser/chromeos/arc/boot_phase_monitor/arc_boot_phase_monitor_bridge.h"
+#include "chrome/browser/ash/arc/boot_phase_monitor/arc_boot_phase_monitor_bridge.h"
 #include "chrome/browser/chromeos/arc/instance_throttle/arc_active_window_throttle_observer.h"
 #include "chrome/browser/chromeos/arc/instance_throttle/arc_app_launch_throttle_observer.h"
 #include "chrome/browser/chromeos/arc/instance_throttle/arc_boot_phase_throttle_observer.h"
