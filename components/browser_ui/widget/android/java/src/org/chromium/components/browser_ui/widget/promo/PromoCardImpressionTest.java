fdf8468c5b25421a53a677fbc0a9a1655da2f987
Wenyu Fu <wenyufu@google.com>
2021-03-23 22:13:08 +0000

    Disable flaky PromoCardImpressionTest#testImpression_Card_Hide
    The test appears flakiness when the impression counter is increasing
    when the promo is not shown.
    
    This issue is not locally reproducible. Disable the test for now.
    
    Change-Id: I4741b42911b7f1b5e492afccace1e172f623020b
    Bug: 1181859
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2776050
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Owners-Override: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865843}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/promo/PromoCardImpressionTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/promo/PromoCardImpressionTest.java
@@ -24,6 +24,7 @@ import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.test.util.DisableAnimationsTestRule;
@@ -112,6 +113,7 @@ public class PromoCardImpressionTest {
 
     @Test
     @SmallTest
+    @FlakyTest(message = "https://crbug.com/1181859")
     public void testImpression_Card_Hide() throws TimeoutException {
         int initCount = mPromoSeenCallback.getCallCount();
         setUpPromoCard(false, true);
