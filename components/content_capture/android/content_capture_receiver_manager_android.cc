99921837ef902410139ab439bfc54e760ee1803b
Shimi Zhang <ctzsm@chromium.org>
2021-03-23 19:52:12 +0000

    ContentCapture: Handle top margin
    WebLayer and Chrome has a top margin for the top control, we need to
    add the top margin to the rects when the margin presents.
    
    Bug: 1174566
    
    Change-Id: Ib65c4ae3eda26632637fc39b31920ee47981ebaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2671382
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Michael Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865774}

--- components/content_capture/android/content_capture_receiver_manager_android.cc
+++ components/content_capture/android/content_capture_receiver_manager_android.cc
@@ -48,24 +48,27 @@ namespace {
 ScopedJavaLocalRef<jobject> ToJavaObjectOfContentCaptureData(
     JNIEnv* env,
     const ContentCaptureData& data,
-    const JavaRef<jobject>& parent) {
+    const JavaRef<jobject>& parent,
+    int offset_y) {
   ScopedJavaLocalRef<jstring> jvalue =
       ConvertUTF16ToJavaString(env, data.value);
   ScopedJavaLocalRef<jobject> jdata =
       Java_ContentCaptureData_createContentCaptureData(
-          env, parent, data.id, jvalue, data.bounds.x(), data.bounds.y(),
-          data.bounds.width(), data.bounds.height());
+          env, parent, data.id, jvalue, data.bounds.x(),
+          data.bounds.y() + offset_y, data.bounds.width(),
+          data.bounds.height());
   if (jdata.is_null())
     return jdata;
   for (const auto& child : data.children) {
-    ToJavaObjectOfContentCaptureData(env, child, jdata);
+    ToJavaObjectOfContentCaptureData(env, child, jdata, offset_y);
   }
   return jdata;
 }
 
 ScopedJavaLocalRef<jobject> ToJavaObjectOfContentCaptureFrame(
     JNIEnv* env,
-    const ContentCaptureFrame& data) {
+    const ContentCaptureFrame& data,
+    int offset_y) {
   ScopedJavaLocalRef<jstring> jurl = ConvertUTF16ToJavaString(env, data.url);
   ScopedJavaLocalRef<jstring> jtitle;
   if (!data.title.empty())
@@ -73,19 +76,20 @@ ScopedJavaLocalRef<jobject> ToJavaObjectOfContentCaptureFrame(
 
   ScopedJavaLocalRef<jobject> jdata =
       Java_ContentCaptureFrame_createContentCaptureFrame(
-          env, data.id, jurl, data.bounds.x(), data.bounds.y(),
+          env, data.id, jurl, data.bounds.x(), data.bounds.y() + offset_y,
           data.bounds.width(), data.bounds.height(), jtitle);
   if (jdata.is_null())
     return jdata;
   for (const auto& child : data.children) {
-    ToJavaObjectOfContentCaptureData(env, child, jdata);
+    ToJavaObjectOfContentCaptureData(env, child, jdata, offset_y);
   }
   return jdata;
 }
 
 ScopedJavaLocalRef<jobjectArray> ToJavaArrayOfContentCaptureFrame(
     JNIEnv* env,
-    const ContentCaptureSession& session) {
+    const ContentCaptureSession& session,
+    int offset_y) {
   ScopedJavaLocalRef<jclass> object_clazz =
       base::android::GetClass(env, "java/lang/Object");
   jobjectArray joa =
@@ -94,7 +98,7 @@ ScopedJavaLocalRef<jobjectArray> ToJavaArrayOfContentCaptureFrame(
 
   for (size_t i = 0; i < session.size(); ++i) {
     ScopedJavaLocalRef<jobject> item =
-        ToJavaObjectOfContentCaptureFrame(env, session[i]);
+        ToJavaObjectOfContentCaptureFrame(env, session[i], offset_y);
     env->SetObjectArrayElement(joa, i, item.obj());
   }
   return ScopedJavaLocalRef<jobjectArray>(env, joa);
@@ -127,13 +131,15 @@ void ContentCaptureReceiverManagerAndroid::DidCaptureContent(
   JNIEnv* env = AttachCurrentThread();
   DCHECK(java_ref_.obj());
 
+  const int offset_y = Java_ContentCaptureReceiverManager_getOffsetY(
+      env, java_ref_, web_contents()->GetJavaWebContents());
   ScopedJavaLocalRef<jobject> jdata =
-      ToJavaObjectOfContentCaptureFrame(env, data);
+      ToJavaObjectOfContentCaptureFrame(env, data, offset_y);
   if (jdata.is_null())
     return;
   Java_ContentCaptureReceiverManager_didCaptureContent(
-      env, java_ref_, ToJavaArrayOfContentCaptureFrame(env, parent_session),
-      jdata);
+      env, java_ref_,
+      ToJavaArrayOfContentCaptureFrame(env, parent_session, offset_y), jdata);
 }
 
 void ContentCaptureReceiverManagerAndroid::DidUpdateContent(
@@ -142,13 +148,15 @@ void ContentCaptureReceiverManagerAndroid::DidUpdateContent(
   JNIEnv* env = AttachCurrentThread();
   DCHECK(java_ref_.obj());
 
+  const int offset_y = Java_ContentCaptureReceiverManager_getOffsetY(
+      env, java_ref_, web_contents()->GetJavaWebContents());
   ScopedJavaLocalRef<jobject> jdata =
-      ToJavaObjectOfContentCaptureFrame(env, data);
+      ToJavaObjectOfContentCaptureFrame(env, data, offset_y);
   if (jdata.is_null())
     return;
   Java_ContentCaptureReceiverManager_didUpdateContent(
-      env, java_ref_, ToJavaArrayOfContentCaptureFrame(env, parent_session),
-      jdata);
+      env, java_ref_,
+      ToJavaArrayOfContentCaptureFrame(env, parent_session, offset_y), jdata);
 }
 
 void ContentCaptureReceiverManagerAndroid::DidRemoveContent(
@@ -156,8 +164,10 @@ void ContentCaptureReceiverManagerAndroid::DidRemoveContent(
     const std::vector<int64_t>& data) {
   JNIEnv* env = AttachCurrentThread();
   DCHECK(java_ref_.obj());
+  const int offset_y = Java_ContentCaptureReceiverManager_getOffsetY(
+      env, java_ref_, web_contents()->GetJavaWebContents());
   Java_ContentCaptureReceiverManager_didRemoveContent(
-      env, java_ref_, ToJavaArrayOfContentCaptureFrame(env, session),
+      env, java_ref_, ToJavaArrayOfContentCaptureFrame(env, session, offset_y),
       ToJavaLongArray(env, data));
 }
 
@@ -165,16 +175,20 @@ void ContentCaptureReceiverManagerAndroid::DidRemoveSession(
     const ContentCaptureSession& session) {
   JNIEnv* env = AttachCurrentThread();
   DCHECK(java_ref_.obj());
+  const int offset_y = Java_ContentCaptureReceiverManager_getOffsetY(
+      env, java_ref_, web_contents()->GetJavaWebContents());
   Java_ContentCaptureReceiverManager_didRemoveSession(
-      env, java_ref_, ToJavaArrayOfContentCaptureFrame(env, session));
+      env, java_ref_, ToJavaArrayOfContentCaptureFrame(env, session, offset_y));
 }
 
 void ContentCaptureReceiverManagerAndroid::DidUpdateTitle(
     const ContentCaptureFrame& main_frame) {
   JNIEnv* env = AttachCurrentThread();
   DCHECK(java_ref_.obj());
+  const int offset_y = Java_ContentCaptureReceiverManager_getOffsetY(
+      env, java_ref_, web_contents()->GetJavaWebContents());
   ScopedJavaLocalRef<jobject> jdata =
-      ToJavaObjectOfContentCaptureFrame(env, main_frame);
+      ToJavaObjectOfContentCaptureFrame(env, main_frame, offset_y);
   if (jdata.is_null())
     return;
   Java_ContentCaptureReceiverManager_didUpdateTitle(env, java_ref_, jdata);
