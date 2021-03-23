880b5e0f4824b62281f68e5b9c510d46c72bfc98
Maria Kazinova <kazinova@google.com>
2021-03-23 13:16:06 +0000

    [Passwords] Cleaned up PasswordFormManager::DoesManage* methods.
    Deleted the original implementation of DoesManage method and replaced
    it with DoesManageAccordingToRendererId, since this version should be
    used now on all platforms.
    
    Change-Id: I7782a836f13a75b1a0e020bb71479bb84e717fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2778257
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/master@{#865610}

--- components/password_manager/core/browser/password_manager.cc
+++ components/password_manager/core/browser/password_manager.cc
@@ -127,7 +127,7 @@ PasswordFormManager* FindMatchedManagerByRendererId(
     const std::vector<std::unique_ptr<PasswordFormManager>>& form_managers,
     const PasswordManagerDriver* driver) {
   for (const auto& form_manager : form_managers) {
-    if (form_manager->DoesManageAccordingToRendererId(form_renderer_id, driver))
+    if (form_manager->DoesManage(form_renderer_id, driver))
       return form_manager.get();
   }
   return nullptr;
@@ -786,7 +786,7 @@ void PasswordManager::OnPasswordFormRemoved(
     if (driver && !manager->GetDriver())
       manager->SetDriver(driver->AsWeakPtr());
     // Find a form with corresponding renderer id.
-    if (manager->DoesManageAccordingToRendererId(form_id, driver)) {
+    if (manager->DoesManage(form_id, driver)) {
       DetectPotentialSubmission(manager.get(), field_data_manager, driver);
       return;
     }
@@ -1194,7 +1194,7 @@ PasswordFormManager* PasswordManager::GetMatchedManager(
     if (driver && !form_manager->GetDriver())
       form_manager->SetDriver(driver->AsWeakPtr());
 #endif
-    if (form_manager->DoesManageAccordingToRendererId(form_id, driver))
+    if (form_manager->DoesManage(form_id, driver))
       return form_manager.get();
   }
   return nullptr;
