1f6e19f8a7d0823d31ec6450ec2cac17fc31419c
Ramin Halavati <rhalavati@chromium.org>
2021-03-23 14:39:46 +0000

    Add random GUID to generated OTRProfileIDs.
    To reduce the chance of collision between a generated OTRProfileID
    through OTRProfileID::CreateUnique and an id created by a constant or
    serialized text, the sequencer added to the end of OTRProfileIDs is
    replaced with a randomly generated GUID.
    
    Bug: 1187430
    Change-Id: I8ed1de60e18b97f1ff5e06465a1e43d8aeb7832d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780653
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865626}

--- chrome/browser/profiles/profile.cc
+++ chrome/browser/profiles/profile.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/files/file_path.h"
+#include "base/guid.h"
 #include "base/path_service.h"
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
@@ -102,13 +103,11 @@ const Profile::OTRProfileID Profile::OTRProfileID::PrimaryID() {
 }
 
 // static
-int Profile::OTRProfileID::first_unused_index_ = 0;
-
-// static
 Profile::OTRProfileID Profile::OTRProfileID::CreateUnique(
     const std::string& profile_id_prefix) {
-  return OTRProfileID(base::StringPrintf("%s-%i", profile_id_prefix.c_str(),
-                                         first_unused_index_++));
+  return OTRProfileID(base::StringPrintf(
+      "%s-%s", profile_id_prefix.c_str(),
+      base::GUID::GenerateRandomV4().AsLowercaseString().c_str()));
 }
 
 // static
