c6914764cb50fe065f7907446d8db85719928921
David Bienvenu <davidbienvenu@chromium.org>
2021-03-23 14:29:48 +0000

    unit_tests: fix tsan flake in ChromePasswordManagerClientTest
    Fixes a tsan data race on the command line by moving the command line
    handling to test SetUp, and making the test a TEST_P bool.
    
    Also fixes a lint error about multiple include of the same header.
    
    Bug: 956719
    Change-Id: Ifed9e4f2b6ffcaafc018d8665db36328fb36f7ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2775863
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865622}

--- chrome/browser/password_manager/chrome_password_manager_client_unittest.cc
+++ chrome/browser/password_manager/chrome_password_manager_client_unittest.cc
@@ -83,9 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "chrome/browser/autofill/manual_filling_controller_impl.h"
-#include "chrome/browser/autofill/mock_address_accessory_controller.h"
 #include "chrome/browser/autofill/mock_credit_card_accessory_controller.h"
-#include "chrome/browser/autofill/mock_manual_filling_view.h"
 #include "chrome/browser/password_manager/android/password_accessory_controller_impl.h"
 #include "chrome/browser/password_manager/android/password_generation_controller.h"
 #endif  // defined(OS_ANDROID)
@@ -459,14 +457,28 @@ TEST_F(ChromePasswordManagerClientTest,
   EXPECT_FALSE(client->IsFillingFallbackEnabled(kUrlOn));
 }
 
-TEST_F(ChromePasswordManagerClientTest, SavingDependsOnAutomation) {
-  // Test that saving passwords UI is disabled for automated tests.
+class ChromePasswordManagerClientAutomatedTest
+    : public ChromePasswordManagerClientTest,
+      public testing::WithParamInterface<bool> {
+ public:
+  ChromePasswordManagerClientAutomatedTest() {
+    if (GetParam()) {
+      base::CommandLine::ForCurrentProcess()->AppendSwitch(
+          switches::kEnableAutomation);
+    }
+  }
+};
+
+INSTANTIATE_TEST_SUITE_P(AutomatedTestPasswordHandling,
+                         ChromePasswordManagerClientAutomatedTest,
+                         testing::Bool());
+
+TEST_P(ChromePasswordManagerClientAutomatedTest, SavingDependsOnAutomation) {
+  // Test that saving passwords UI is disabled for automated tests,
+  // and enabled for non-automated tests.
   ChromePasswordManagerClient* client = GetClient();
   const GURL kUrlOn("https://accounts.google.com");
-  EXPECT_TRUE(client->IsSavingAndFillingEnabled(kUrlOn));
-  base::CommandLine::ForCurrentProcess()->AppendSwitch(
-      switches::kEnableAutomation);
-  EXPECT_FALSE(client->IsSavingAndFillingEnabled(kUrlOn));
+  EXPECT_NE(client->IsSavingAndFillingEnabled(kUrlOn), GetParam());
 }
 
 // Check that password manager is disabled on about:blank pages.
