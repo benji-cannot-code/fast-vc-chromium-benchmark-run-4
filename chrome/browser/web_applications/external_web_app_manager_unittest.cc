85ecf3ac1ddf79a9dfbf643166aa07f1d106e013
Tim Sergeant <tsergeant@chromium.org>
2021-03-23 03:43:22 +0000

    Web apps: Allow loading device-specific web apps from a subdirectory
    This CL reads the command line switch --extra-web-apps-dir and loads web
    apps from the specified subdirectory of the regular external web apps
    configuration directory.
    
    This allows a single Chrome OS system image to have device-specific apps
    for multiple devices, selecting the correct set at runtime based on the
    switch value provided by login_manager.
    
    Bug: 1184581
    Change-Id: I21fb76fc8ee93954d8be500bb4190d73e04e5249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2777433
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865505}

--- chrome/browser/web_applications/external_web_app_manager_unittest.cc
+++ chrome/browser/web_applications/external_web_app_manager_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_command_line.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_path_override.h"
 #include "build/chromeos_buildflags.h"
@@ -35,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/constants/ash_switches.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "components/user_manager/scoped_user_manager.h"
@@ -464,6 +466,30 @@ TEST_F(ExternalWebAppManagerTest, NonPrimaryProfile) {
   VerifySetOfApps(CreateProfile().get(),
                   {GURL(kAppAllUrl), GURL(kAppUnmanagedUrl)});
 }
+
+TEST_F(ExternalWebAppManagerTest, ExtraWebApps) {
+  // The extra_web_apps directory contains two JSON files in different named
+  // subdirectories. The --extra-web-apps-dir switch should control which
+  // directory apps are loaded from.
+  base::test::ScopedCommandLine command_line;
+  command_line.GetProcessCommandLine()->AppendSwitchASCII(
+      chromeos::switches::kExtraWebAppsDir, "model1");
+
+  const auto app_infos = LoadApps("extra_web_apps");
+  EXPECT_EQ(1u, app_infos.size());
+  ExpectHistograms(/*enabled=*/1, /*disabled=*/0, /*errors=*/0);
+}
+
+TEST_F(ExternalWebAppManagerTest, ExtraWebAppsNoMatchingDirectory) {
+  base::test::ScopedCommandLine command_line;
+  command_line.GetProcessCommandLine()->AppendSwitchASCII(
+      chromeos::switches::kExtraWebAppsDir, "model3");
+
+  const auto app_infos = LoadApps("extra_web_apps");
+  EXPECT_EQ(0u, app_infos.size());
+  ExpectHistograms(/*enabled=*/0, /*disabled=*/0, /*errors=*/0);
+}
+
 #else   // BUILDFLAG(IS_CHROMEOS_ASH)
 // No app is expected for non-ChromeOS builds.
 TEST_F(ExternalWebAppManagerTest, NoApp) {
