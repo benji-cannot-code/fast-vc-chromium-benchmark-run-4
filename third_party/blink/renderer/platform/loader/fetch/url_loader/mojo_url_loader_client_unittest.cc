844cd83b18f69acac869cd2c138cf2307fa511cd
Yuzu Saijo <yuzus@chromium.org>
2021-03-23 11:10:12 +0000

    [bfcache] Use pure virtual functions for BackForwardCacheLoaderHelper
    This CL refactors BackForwardCacheLoaderHelper class so that all of its
    functions (except for Trace, which cannot be pure virtual because of
    blink-gc rules) are pure virtual.
    
    Change-Id: I977860f6f52e0a1ca6e4b59d880c689d8fb4dae8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2763594
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865585}

--- third_party/blink/renderer/platform/loader/fetch/url_loader/mojo_url_loader_client_unittest.cc
+++ third_party/blink/renderer/platform/loader/fetch/url_loader/mojo_url_loader_client_unittest.cc
@@ -174,6 +174,12 @@ std::string GetRequestPeerContextBody(
 class TestBackForwardCacheLoaderHelper : public BackForwardCacheLoaderHelper {
  public:
   TestBackForwardCacheLoaderHelper() = default;
+  void EvictFromBackForwardCache(
+      mojom::RendererEvictionReason reason) override {}
+
+  void DidBufferLoadWhileInBackForwardCache(size_t num_bytes) override {}
+
+  void Detach() override {}
 
   bool CanContinueBufferingWhileInBackForwardCache() const override {
     return true;
