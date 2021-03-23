3935e8b0a87ff7d6e6e9d1c8cdfea1107f4af187
Kenichi Ishibashi <bashi@chromium.org>
2021-03-23 02:19:06 +0000

    Disallow empty header lists in QuicChromiumClientStream
    According to the IETF draft, all responses must include the ":status"
    pseudo-header field [1]. This means that we should treat an empty header
    list as a protocol error. With this CL QuicChromiumClientStream
    disallows an empty header list because ParseHeaderStatusCode() will fail
    for an empty header list.
    
    [1] https://datatracker.ietf.org/doc/html/draft-ietf-quic-http-34#section-4.1.1.1
    
    Bug: 1096414
    Change-Id: Ibfbd9b5507517bafca96467fbe7696ec45f21a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2772688
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Reviewed-by: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865473}

--- net/quic/quic_chromium_client_session_test.cc
+++ net/quic/quic_chromium_client_session_test.cc
@@ -2174,6 +2174,10 @@ TEST_P(QuicChromiumClientSessionTest, ResetOnEmptyResponseHeaders) {
   if (VersionUsesHttp3(version_.transport_version)) {
     quic_data.AddWrite(ASYNC,
                        client_maker_.MakeInitialSettingsPacket(packet_num++));
+    quic_data.AddWrite(ASYNC, client_maker_.MakeRstPacket(
+                                  packet_num++, true,
+                                  GetNthClientInitiatedBidirectionalStreamId(0),
+                                  quic::QUIC_STREAM_GENERAL_PROTOCOL_ERROR));
   } else {
     // In case of Google QUIC, QuicSpdyStream resets the stream.
     quic_data.AddWrite(ASYNC, client_maker_.MakeRstPacket(
@@ -2217,7 +2221,7 @@ TEST_P(QuicChromiumClientSessionTest, ResetOnEmptyResponseHeaders) {
     spdy::Http2HeaderBlock header_block;
     int rv = stream_handle->ReadInitialHeaders(&header_block,
                                                CompletionOnceCallback());
-    EXPECT_THAT(rv, IsError(ERR_INVALID_RESPONSE));
+    EXPECT_THAT(rv, IsError(net::ERR_QUIC_PROTOCOL_ERROR));
   }
 
   base::RunLoop().RunUntilIdle();
