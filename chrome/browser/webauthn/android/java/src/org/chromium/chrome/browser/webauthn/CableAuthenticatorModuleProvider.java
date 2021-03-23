1fc9f3dbed792e6437f599efc26f6318c48ab8ac
Adam Langley <agl@chromium.org>
2021-03-23 15:23:13 +0000

    webauthn: drop “installing” string for caBLEv2.
    This string appears for a few hundred milliseconds for me on WiFi. Too
    fast to fully read and abrupt when it disappears. Remove it.
    
    This also removes the failure message. We might want to put that back in
    a future change but I don't want to hold this up for now.
    
    BUG=1002262
    
    Change-Id: I85a12a39e14e994782aabca5c2491b1ce0914e7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2779954
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#865638}

--- chrome/browser/webauthn/android/java/src/org/chromium/chrome/browser/webauthn/CableAuthenticatorModuleProvider.java
+++ chrome/browser/webauthn/android/java/src/org/chromium/chrome/browser/webauthn/CableAuthenticatorModuleProvider.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webauthn;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -13,11 +12,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import org.chromium.base.Log;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.chrome.modules.cablev2_authenticator.Cablev2AuthenticatorModule;
@@ -32,6 +31,9 @@ import org.chromium.chrome.modules.cablev2_authenticator.Cablev2AuthenticatorMod
  * settings UI, while {@link ModuleInstallUi} assumes that the UI does in a {@link Tab}.
  */
 public class CableAuthenticatorModuleProvider extends Fragment {
+    // TAG is subject to a 20 character limit.
+    private static final String TAG = "CableAuthModuleProv";
+
     // NETWORK_CONTEXT_KEY is the key under which a pointer to a NetworkContext
     // is passed (as a long) in the arguments {@link Bundle} to the {@link
     // Fragment} in the module.
@@ -43,35 +45,23 @@ public class CableAuthenticatorModuleProvider extends Fragment {
             "org.chromium.chrome.modules.cablev2_authenticator.ActivityClassName";
     private static final String ACTIVITY_CLASS_NAME =
             "org.chromium.chrome.browser.webauth.authenticator.CableAuthenticatorActivity";
-    private TextView mStatus;
 
     @Override
-    @SuppressLint("SetTextI18n")
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final Context context = getContext();
 
-        // This UI is a placeholder for development, has not been reviewed by
-        // UX, and thus just uses untranslated strings for now.
-        getActivity().setTitle("Installing");
-
-        mStatus = new TextView(context);
-        mStatus.setPadding(0, 60, 0, 60);
-
         LinearLayout layout = new LinearLayout(context);
         layout.setOrientation(LinearLayout.VERTICAL);
         layout.setGravity(Gravity.CENTER_HORIZONTAL);
-        layout.addView(mStatus,
-                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT));
 
         if (Cablev2AuthenticatorModule.isInstalled()) {
             showModule();
         } else {
-            mStatus.setText("Installing security key functionality…");
             Cablev2AuthenticatorModule.install((success) -> {
                 if (!success) {
-                    mStatus.setText("Failed to install.");
+                    Log.e(TAG, "Failed to install caBLE DFM");
+                    getActivity().finish();
                     return;
                 }
                 showModule();
@@ -81,10 +71,7 @@ public class CableAuthenticatorModuleProvider extends Fragment {
         return layout;
     }
 
-    @SuppressLint("SetTextI18n")
     private void showModule() {
-        mStatus.setText("Installed.");
-
         FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
         Fragment fragment = Cablev2AuthenticatorModule.getImpl().getFragment();
         Bundle arguments = getArguments();
