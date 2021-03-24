8ad0fae3c28a394226edf253ed1a91a821c4eb05
Aaron Krajeski <aaronhk@chromium.org>
2021-03-24 20:08:40 +0000

    Reland "Create canvasFilter class and interface"
    Using the NewCanvas2DAPI flag on an idl file caused MSAN failures because
    that flag was not explicitly initialized on the WebPreferences
    constructor. This CL solves that problem
    https://chromium-review.googlesource.com/c/chromium/src/+/2783641
    I'll also run this CL through the MSan bots to confirm.
    
    This is a reland of 5d00b2d8b1b3a3431ce093a990524a8b25a2bcac
    It was reverted here https://chromium-review.googlesource.com/c/chromium/src/+/2775779
    
    Original change's description:
    > Create canvasFilter class and interface
    >
    > Create a "mongo-db" like interface for filters. Implement blur, but only
    > blur.
    >
    > Bug: 1169216
    > Change-Id: I0dc045fa70f0d1e443cf4f8ff53632056cca6924
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2741994
    > Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    > Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > Reviewed-by: Xida Chen <xidachen@chromium.org>
    > Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    > Reviewed-by: Yi Xu <yiyix@chromium.org>
    > Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#864863}
    
    Bug: 1169216
    Change-Id: I6e5badf27d41f6dbdc1759ac518d502d1983ebd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780336
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866266}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/parser/css_parser.h"
 #include "third_party/blink/renderer/core/html/canvas/text_metrics.h"
 #include "third_party/blink/renderer/core/html/media/html_video_element.h"
+#include "third_party/blink/renderer/modules/canvas/canvas2d/canvas_filter.h"
 #include "third_party/blink/renderer/modules/canvas/canvas2d/canvas_pattern.h"
 #include "third_party/blink/renderer/modules/canvas/canvas2d/path_2d.h"
 #include "third_party/blink/renderer/platform/geometry/float_quad.h"
@@ -468,29 +469,37 @@ void BaseRenderingContext2D::setGlobalCompositeOperation(
   ModifiableState().SetGlobalComposite(sk_blend_mode);
 }
 
-void BaseRenderingContext2D::filter(StringOrObject& filter) const {
-  filter.SetString(GetState().UnparsedFilter());
+void BaseRenderingContext2D::filter(StringOrCanvasFilter& filter) const {
+  if (GetState().GetCanvasFilter())
+    filter.SetCanvasFilter(GetState().GetCanvasFilter());
+  else
+    filter.SetString(GetState().UnparsedCSSFilter());
 }
 
 void BaseRenderingContext2D::setFilter(
     const ExecutionContext* execution_context,
-    StringOrObject input) {
+    StringOrCanvasFilter input) {
   if (input.IsString()) {
     String filter_string = input.GetAsString();
 
-    if (filter_string == GetState().UnparsedFilter())
+    if (!GetState().GetCanvasFilter() &&
+        filter_string == GetState().UnparsedCSSFilter())
       return;
 
-    const CSSValue* filter_value = CSSParser::ParseSingleValue(
+    const CSSValue* css_value = CSSParser::ParseSingleValue(
         CSSPropertyID::kFilter, filter_string,
         MakeGarbageCollected<CSSParserContext>(
             kHTMLStandardMode, execution_context->GetSecureContextMode()));
 
-    if (!filter_value || filter_value->IsCSSWideKeyword())
+    if (!css_value || css_value->IsCSSWideKeyword())
       return;
 
-    ModifiableState().SetUnparsedFilter(filter_string);
-    ModifiableState().SetFilter(filter_value);
+    ModifiableState().SetUnparsedCSSFilter(filter_string);
+    ModifiableState().SetCSSFilter(css_value);
+    SnapshotStateForFilter();
+  } else if (input.IsCanvasFilter() &&
+             RuntimeEnabledFeatures::NewCanvas2DAPIEnabled()) {
+    ModifiableState().SetCanvasFilter(input.GetAsCanvasFilter());
     SnapshotStateForFilter();
   }
 }
