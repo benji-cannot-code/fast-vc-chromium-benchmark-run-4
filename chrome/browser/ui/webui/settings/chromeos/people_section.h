415d520a2d37366033bc0b7930ef00611a5c182a
Anastasiia N <anastasiian@chromium.org>
2021-03-24 09:44:21 +0000

    cros: Observe AccountManagerFacade in PeopleSection
    Bug: 1117472
    Change-Id: I60c2a29b9ddc00b137010f9a0850f3fe248f8327
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2772607
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866043}

--- chrome/browser/ui/webui/settings/chromeos/people_section.h
+++ chrome/browser/ui/webui/settings/chromeos/people_section.h
@@ -8,20 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/account_manager/account_manager.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/kerberos/kerberos_credentials_manager.h"
 #include "chrome/browser/ui/webui/settings/chromeos/os_settings_section.h"
 #include "components/account_manager_core/account.h"
+#include "components/account_manager_core/account_manager_facade.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/sync/driver/sync_service_observer.h"
 
 class PrefService;
 class Profile;
 class SupervisedUserService;
-
-namespace account_manager {
-class AccountManagerFacade;
-}
 
 namespace content {
 class WebUIDataSource;
@@ -48,7 +46,7 @@ class SearchTagRegistry;
 // they are allowed by policy/flags. Different sets of Sync tags are shown
 // depending on whether the feature is enabed or disabled.
 class PeopleSection : public OsSettingsSection,
-                      public AccountManager::Observer,
+                      public account_manager::AccountManagerFacade::Observer,
                       public syncer::SyncServiceObserver,
                       public KerberosCredentialsManager::Observer {
  public:
@@ -72,8 +70,8 @@ class PeopleSection : public OsSettingsSection,
   bool LogMetric(mojom::Setting setting, base::Value& value) const override;
   void RegisterHierarchy(HierarchyGenerator* generator) const override;
 
-  // AccountManager::Observer:
-  void OnTokenUpserted(const ::account_manager::Account& account) override;
+  // AccountManagerFacade::Observer:
+  void OnAccountUpserted(const ::account_manager::Account& account) override;
   void OnAccountRemoved(const ::account_manager::Account& account) override;
 
   // syncer::SyncServiceObserver:
@@ -96,6 +94,13 @@ class PeopleSection : public OsSettingsSection,
   signin::IdentityManager* identity_manager_;
   PrefService* pref_service_;
   PrefChangeRegistrar fingerprint_pref_change_registrar_;
+
+  // An observer for `AccountManagerFacade`. Automatically deregisters when
+  // `this` is destructed.
+  base::ScopedObservation<account_manager::AccountManagerFacade,
+                          account_manager::AccountManagerFacade::Observer>
+      account_manager_facade_observation_{this};
+
   base::WeakPtrFactory<PeopleSection> weak_factory_{this};
 };
 
