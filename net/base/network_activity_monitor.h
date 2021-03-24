a27501390c91c24dea99f736a8c95d3bacbba895
Francois Doray <fdoray@chromium.org>
2021-03-24 15:06:50 +0000

    [net] Remove unused methods NetworkActivityMonitor::GetTimeSinceLast(Received|Sent).
    Bug: 1189805
    Change-Id: Icfdaab2037370d99a249e319fb344c3ce0c1b4c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2773982
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866117}

--- net/base/network_activity_monitor.h
+++ net/base/network_activity_monitor.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/lazy_instance.h"
 #include "base/synchronization/lock.h"
-#include "base/time/time.h"
 #include "net/base/net_export.h"
 
 namespace net {
@@ -46,9 +45,6 @@ class NET_EXPORT_PRIVATE NetworkActivityMonitor {
   uint64_t GetBytesReceived() const;
   uint64_t GetBytesSent() const;
 
-  base::TimeDelta GetTimeSinceLastReceived() const;
-  base::TimeDelta GetTimeSinceLastSent() const;
-
  private:
   friend class test::NetworkActivityMonitorPeer;
 
@@ -61,9 +57,6 @@ class NET_EXPORT_PRIVATE NetworkActivityMonitor {
 
   uint64_t bytes_received_;
   uint64_t bytes_sent_;
-
-  base::TimeTicks last_received_ticks_;
-  base::TimeTicks last_sent_ticks_;
 };
 
 }  // namespace net
