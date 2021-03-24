00422fda03feb90e38a161cdc36bd6f74f4b6401
Brian Geffon <bgeffon@chromium.org>
2021-03-24 15:21:34 +0000

    Mojo: Channel Linux Detect support for EFD_ZERO_ON_WAKE
    We've added a new flag to eventfd(2) called EFD_ZERO_ON_WAKE. This
    flag helps in situations where the value stored in the eventfd(2) is
    irrelevant, such as for notification only schemes. For more information
    on this kernel change see [1].
    
    This change detects if the kernel supports this flag, and if it's also
    enabled by our feature parameter then we will use it.
    
    [1] https://lkml.org/lkml/2021/3/10/1465
    
    Bug: b:173022729
    Change-Id: If0d91199bd54426a2c27deaf5e876d061675af94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782844
    Commit-Queue: Brian Geffon <bgeffon@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#866122}

--- mojo/core/embedder/features.h
+++ mojo/core/embedder/features.h
@@ -17,6 +17,7 @@ namespace core {
 #if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_ANDROID)
 extern const base::Feature kMojoLinuxChannelSharedMem;
 extern const base::FeatureParam<int> kMojoLinuxChannelSharedMemPages;
+extern const base::FeatureParam<bool> kMojoLinuxChannelSharedMemEfdZeroOnWake;
 #endif  // defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_ANDROID)
 
 extern const base::Feature kMojoPosixUseWritev;
