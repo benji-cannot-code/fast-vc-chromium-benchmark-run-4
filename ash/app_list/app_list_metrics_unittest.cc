830e6ce1564352d7bb45aa0b052189a19d5d6996
Yulun Wu <yulunwu@chromium.org>
2021-03-24 22:27:59 +0000

    Change app list test model name indexing.
    Reland "Fix coordinates used to reparent items from folder to root grid view."
    
    This reverts commit 311ecb9816d141e8fec9306732193cfa22349bdf.
    
    Reason for revert: Fixed Tests
    
    Original change's description:
    > Revert "Fix coordinates used to reparent items from folder to root grid view."
    >
    > This reverts commit a9eb65c56ac3ded3965de57df892b750f4db784f.
    >
    > Reason for revert:
    >
    > Findit (https://goo.gl/kROfz5) identified CL at revision 864589 as the
    > culprit for failures in the build cycles as shown on:
    > https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2E5ZWI2NWM1NmFjM2RlZDM5NjVkZTU3ZGY4OTJiNzUwZjRkYjc4NGYM
    >
    > Sample Failed Build: https://ci.chromium.org/b/8852336657216064128
    >
    > Sample Failed Step: ash_unittests
    >
    > Original change's description:
    > > Fix coordinates used to reparent items from folder to root grid view.
    > >
    > > This CL fixes a bug where items on non-zero'th pages of app list folder
    > > views would have offset drag and drop coordinates when reparenting to
    > > the root level apps grid view. This CL also updates how we populate
    > > the app list test model as the previous model would have duplicate
    > > app item IDs when creating items in both the root level apps grid and a
    > > folder's apps grid.
    > >
    > > Note that PopulatedAppListTest.ScreenRotationDuringFolderItemDrag in
    > > had a bug where Item 2, Item 3, and Item 4 are duplicated in the folder
    > > and the root apps grid view.
    > >
    > > Bug: 1101762
    > > Change-Id: I5739a09a515d753916e65fec2b777bb332da9f22
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2771138
    > > Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    > > Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#864589}
    >
    >
    > Change-Id: I2de771b350f6ac125fdcb4ba8d6d182ec3f2109a
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1101762
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2773670
    > Cr-Commit-Position: refs/heads/master@{#864648}
    
    Bug: 1101762
    Change-Id: I8b13f1bde753246fb2f0defb688216ddd2092af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2777076
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866326}

--- ash/app_list/app_list_metrics_unittest.cc
+++ ash/app_list/app_list_metrics_unittest.cc
@@ -86,9 +86,6 @@ class AppListAppLaunchedMetricTest : public AshTestBase {
 
     search_model_ = Shell::Get()->app_list_controller()->GetSearchModel();
 
-    app_list_test_model_ = static_cast<test::AppListTestModel*>(
-        Shell::Get()->app_list_controller()->GetModel());
-
     shelf_test_api_ = std::make_unique<ShelfViewTestAPI>(
         GetPrimaryShelf()->GetShelfViewForTesting());
   }
@@ -194,7 +191,11 @@ class AppListAppLaunchedMetricTest : public AshTestBase {
 
   void PopulateAndLaunchAppInGrid() {
     // Populate apps in the root app grid.
-    app_list_test_model_->PopulateApps(4);
+    AppListModel* model = Shell::Get()->app_list_controller()->GetModel();
+    model->AddItem(std::make_unique<AppListItem>("item 0"));
+    model->AddItem(std::make_unique<AppListItem>("item 1"));
+    model->AddItem(std::make_unique<AppListItem>("item 2"));
+    model->AddItem(std::make_unique<AppListItem>("item 3"));
 
     AppListView::TestApi test_api(
         Shell::Get()->app_list_controller()->presenter()->GetView());
@@ -208,7 +209,6 @@ class AppListAppLaunchedMetricTest : public AshTestBase {
 
  private:
   SearchModel* search_model_ = nullptr;
-  test::AppListTestModel* app_list_test_model_ = nullptr;
   std::unique_ptr<ShelfViewTestAPI> shelf_test_api_;
 
   DISALLOW_COPY_AND_ASSIGN(AppListAppLaunchedMetricTest);
