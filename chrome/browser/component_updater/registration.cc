c46d1b2c695065c3a4aa58189325ea7a18c1981d
Jeffrey Cohen <jeffreycohen@google.com>
2021-03-24 20:16:51 +0000

    [DesktopSharingHub] add new component installer
    Bug: 1186831
    Change-Id: I45218e12fa162b9817f3d01155bb2602822d94dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782257
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866274}

--- chrome/browser/component_updater/registration.cc
+++ chrome/browser/component_updater/registration.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/component_updater/chrome_origin_trials_component_installer.h"
 #include "chrome/browser/component_updater/crl_set_component_installer.h"
 #include "chrome/browser/component_updater/crowd_deny_component_installer.h"
+#include "chrome/browser/component_updater/desktop_sharing_hub_component_installer.h"
 #include "chrome/browser/component_updater/file_type_policies_component_installer.h"
 #include "chrome/browser/component_updater/first_party_sets_component_installer.h"
 #include "chrome/browser/component_updater/floc_component_installer.h"
@@ -139,6 +140,8 @@ void RegisterComponentsForUpdate(bool is_off_the_record_profile,
   }
   RegisterSSLErrorAssistantComponent(cus);
   RegisterFileTypePoliciesComponent(cus);
+  RegisterDesktopSharingHubComponent(cus);
+
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
   // CRLSetFetcher attempts to load a CRL set from either the local disk or
   // network.
