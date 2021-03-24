420fc6ee65006f9cb7e3a8aef5e05e1305eb501d
Dirk Pranke <dpranke@google.com>
2021-03-24 06:52:40 +0000

    Update '#!/usr/bin/python' shebangs to '#!/usr/bin/env python'.
    Python2 has been considered obsolete since 2020-01-01. Some Linux
    distributions are increasingly moving away from installing a
    Python2 binary in `/usr/bin/python`, which means that any
    Unix shebang lines that refer to it will stop working.
    
    This CL changes (almost) all of the shebang lines found in
    chromium/src to use `#!/usr/bin/env python` instead, which is
    more portable (it allows you to have your own Python as long as
    it is simply somewhere in $PATH). There are a few cases where
    I did not update the changes because they were in third_party
    code.
    
    The actual command used to generate this CL was:
    
    git grep -l '^#!\s*/usr/bin/python$' -- \*.py | egrep -v 'external/wpt|third_party/(abseil-cpp|android_platform|libxml|closure_compiler|google_input_tools|libxml|mako|opus|protobuf|wpt_tools|zxcvbn-cpp)' | xargs perl -pi -e 's-^#!/usr/bin/python-#!/usr/bin/env python-'
    
    Bug: 1191100
    Change-Id: Ifca134a1e24d56b25114c6f70a173d71fec8e6d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780719
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Owners-Override: Kentaro Hara <haraken@chromium.org>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866002}

--- chrome/test/data/third_party/kraken/make-hosted.py
+++ chrome/test/data/third_party/kraken/make-hosted.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # Copyright (C) 2007 Apple Inc.  All rights reserved.
 # Copyright (C) 2010 Mozilla Foundation
