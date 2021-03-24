eb8562bb9f20f73407330f429b5c9e881bd68446
Ewann <ewannpv@google.com>
2021-03-24 10:04:05 +0000

    [iOS][Settings] Removes background of Recents Tabs sync cell
    This CL removes the default background color of the illustrated sync
    cell presented in the Recent Tabs.
    
    Bug: 922511
    Change-Id: I08b5c5a3db9681fda623f8ef205a35656b181a86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780262
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866046}

--- ios/chrome/browser/ui/table_view/cells/table_view_illustrated_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_illustrated_item.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/foundation_util.h"
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
+#include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -69,6 +70,9 @@ const CGFloat kButtonCornerRadius = 8.0;
     [cell.button setTitle:self.buttonText forState:UIControlStateNormal];
   } else {
     cell.button.hidden = YES;
+  }
+  if (base::FeatureList::IsEnabled(kSettingsRefresh)) {
+    cell.backgroundColor = nil;
   }
 
   if (styler.cellTitleColor) {
