5dc77967f756569cc81c1113c57ebdf37a8c4414
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-03-24 07:23:21 +0000

    [CrOS cellular] Add sim locked dialogs to network_list_item
    This CL adds an unlock button to network_list_item which are locked
    when clicked opens and unlock dialog from sim locked dialogs.
    
    Screenshots: https://screenshot.googleplex.com/6ofCLuNG9BoHJbE.png
                 https://screenshot.googleplex.com/LAGnENxQcnRfibj.png
    
    Bug: 1093185,1184464
    Change-Id: Iaa41a3fd9f8d203222dab2c969d4ed2f2fdf4ebb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2720068
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866008}

--- ui/webui/resources/cr_components/chromeos/network/network_list_item.js
+++ ui/webui/resources/cr_components/chromeos/network/network_list_item.js
@@ -124,6 +124,12 @@ Polymer({
         return loadTimeData.getBoolean('updatedCellularActivationUi');
       }
     },
+
+    /**@private {boolean} */
+    isCellularUnlockDialogOpen_: {
+      type: Boolean,
+      value: false,
+    },
   },
 
   /** @override */
@@ -428,8 +434,15 @@ Polymer({
       if (this.isCellularNetworkScanning_()) {
         return this.i18n('networkListItemScanning');
       }
-      if (this.networkState.typeState.cellular.simLocked) {
+
+      if (this.networkState.typeState.cellular.simLocked &&
+          !this.isUpdatedCellularUiEnabled_) {
         return this.i18n('networkListItemSimCardLocked');
+      }
+
+      if (this.networkState.typeState.cellular.simLocked &&
+          this.isUpdatedCellularUiEnabled_) {
+        return this.i18n('networkListItemUpdatedCellularSimCardLocked');
       }
     }
 
@@ -443,6 +456,21 @@ Polymer({
       return this.i18n('networkListItemConnecting');
     }
     return '';
+  },
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getNetworkStateTextClass_() {
+    const mojom = chromeos.networkConfig.mojom;
+    if (this.networkState &&
+        this.networkState.type === mojom.NetworkType.kCellular &&
+        this.networkState.typeState.cellular.simLocked &&
+        this.isUpdatedCellularUiEnabled_) {
+      return 'locked';
+    }
+    return 'cr-secondary-text';
   },
 
   /**
@@ -468,7 +496,7 @@ Polymer({
    * @private
    */
   isSubpageButtonVisible_(networkState, showButtons) {
-    return !!networkState && showButtons;
+    return !!networkState && showButtons && !this.shouldShowUnlockButton_();
   },
 
   /**
@@ -516,6 +544,8 @@ Polymer({
       this.fireShowDetails_(event);
     } else if (this.isESimPendingProfile_) {
       this.onInstallButtonClick_(event);
+    } else if (this.shouldShowUnlockButton_()) {
+      this.onUnlockButtonClick_();
     } else if (this.item.hasOwnProperty('customItemName')) {
       this.fire('custom-item-selected', this.item);
     } else {
@@ -622,5 +652,30 @@ Polymer({
     return this.deviceState.simInfos.some(simInfo => {
       return simInfo.iccid === iccid && simInfo.isPrimary;
     });
+  },
+
+  /** @private */
+  onUnlockButtonClick_() {
+    this.isCellularUnlockDialogOpen_ = true;
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  shouldShowUnlockButton_() {
+    if (!this.networkState || !this.networkState.typeState.cellular ||
+        !this.isUpdatedCellularUiEnabled_) {
+      return false;
+    }
+    return this.networkState.typeState.cellular.simLocked;
+  },
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getUnlockBtnA11yLabel_() {
+    return this.i18n('networkListItemUnlockA11YLabel', this.getItemName_());
   }
 });
