0cc1bbc8a5cd5ed9c7c2e485fa44d32e507b4679
Jan Wilken Dörrie <jdoerrie@chromium.org>
2021-03-24 20:05:53 +0000

    Revert "[base] Add __builtin_wmemchr work around to CharTraits<wchar_t>"
    This reverts commit 63a8515ff8c841dc4398a59fda3e5fe7cf8f4fa5.
    
    Reason for revert: Fixed in upstream clang [1] and rolled in [2].
    [1] https://github.com/llvm/llvm-project/commit/f5352dd
    [2] https://crrev.com/c/2767238
    
    Original change's description:
    > [base] Add __builtin_wmemchr work around to CharTraits<wchar_t>
    >
    > This change adds a workaround to using __builtin_wmemchr due to
    > https://bugs.llvm.org/show_bug.cgi?id=41226.
    >
    > Bug: 1049498
    > Change-Id: Ie89922ca7cfd81bf671532273a8bfc89946f1c8c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2676066
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#851013}
    
    Bug: 1049498
    Change-Id: Ib299268aa2e18f638e2297c06e68d96689e30f95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2712882
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866262}

--- base/strings/char_traits.h
+++ base/strings/char_traits.h
@@ -14,19 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-namespace internal {
-
-template <typename T>
-constexpr const T* ConstexprFind(const T* s, size_t n, T c) {
-  for (; n; --n, ++s) {
-    if (std::char_traits<T>::eq(*s, c))
-      return s;
-  }
-  return nullptr;
-}
-
-}  // namespace internal
-
 // constexpr version of http://en.cppreference.com/w/cpp/string/char_traits.
 // This currently just implements the bits needed to support a (mostly)
 // constexpr StringPiece.
@@ -76,7 +63,11 @@ constexpr size_t CharTraits<T>::length(const T* s) noexcept {
 
 template <typename T>
 constexpr const T* CharTraits<T>::find(const T* s, size_t n, T c) {
-  return internal::ConstexprFind(s, n, c);
+  for (; n; --n, ++s) {
+    if (std::char_traits<T>::eq(*s, c))
+      return s;
+  }
+  return nullptr;
 }
 
 // char and wchar_t specialization of CharTraits that can use clang's constexpr
@@ -112,9 +103,7 @@ struct CharTraits<wchar_t> {
   }
 
   static constexpr const wchar_t* find(const wchar_t* s, size_t n, wchar_t c) {
-    // Note: Due to https://bugs.llvm.org/show_bug.cgi?id=41226 we are not
-    // calling `__builtin_wmemchr` here.
-    return internal::ConstexprFind(s, n, c);
+    return __builtin_wmemchr(s, c, n);
   }
 };
 #endif
