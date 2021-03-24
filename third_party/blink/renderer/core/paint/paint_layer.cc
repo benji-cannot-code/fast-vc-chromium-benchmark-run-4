e6677867531262f9bd0134079fe0d5b22823e647
Chris Harrelson <chrishtr@chromium.org>
2021-03-24 18:38:31 +0000

    Apply scroll offsets of intermediate scrollers for overlap testing.
    Bug: 1191476
    
    Change-Id: I55ca161ae2df09338661ba7f481734dcfbac372e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2783901
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866206}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -1187,7 +1187,7 @@ const IntRect PaintLayer::ClippedAbsoluteBoundingBox() const {
 const IntRect PaintLayer::UnclippedAbsoluteBoundingBox() const {
   return EnclosingIntRect(GetLayoutObject().LocalToAbsoluteRect(
       LocalBoundingBoxForCompositingOverlapTest(),
-      kUseGeometryMapperMode | kIgnoreScrollOffset));
+      kUseGeometryMapperMode | kIgnoreScrollOffsetOfAncestor));
 }
 
 void PaintLayer::SetNeedsCompositingInputsUpdateInternal() {
