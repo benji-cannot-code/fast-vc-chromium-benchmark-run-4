591bda5325461d6ab731a34ef343efa7a14f25d4
Sammie Quon <sammiequon@chromium.org>
2021-03-24 18:41:00 +0000

    capture_mode: Add animation for capture button when switch phases.
    Bug: 1188843
    Test: manual
    Change-Id: I58844d0a051c7379ec978ee7d86c3b3524b346b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2766024
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866208}

--- ash/capture_mode/capture_mode_session.cc
+++ ash/capture_mode/capture_mode_session.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/cursor/cursor_util.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/compositor/layer.h"
+#include "ui/compositor/layer_animation_element.h"
 #include "ui/compositor/layer_type.h"
 #include "ui/compositor/paint_recorder.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
@@ -126,7 +127,7 @@ constexpr int kCaptureRegionMinimumPaddingDp = 16;
 // Animation parameters needed when countdown starts.
 // The animation duration that the label fades out and scales down before count
 // down starts.
-constexpr base::TimeDelta kCaptureLabelAnimationDuration =
+constexpr base::TimeDelta kCaptureLabelCountdownStartDuration =
     base::TimeDelta::FromMilliseconds(267);
 // The animation duration that the capture bar fades out before count down
 // starts.
@@ -139,6 +140,17 @@ constexpr base::TimeDelta kCaptureShieldFadeOutDuration =
 // If there is no text message was showing when count down starts, the label
 // widget will shrink down from 120% -> 100% and fade in.
 constexpr float kLabelScaleUpOnCountdown = 1.2;
+
+// The animation duration that the label fades out and scales up when going from
+// the selection phase to the fine tune phase.
+constexpr base::TimeDelta kCaptureLabelRegionPhaseChangeDuration =
+    base::TimeDelta::FromMilliseconds(167);
+// The delay before the label fades out and scales up.
+constexpr base::TimeDelta kCaptureLabelRegionPhaseChangeDelay =
+    base::TimeDelta::FromMilliseconds(67);
+// When going from the select region phase to the fine tune phase, the label
+// widget will scale up from 80% -> 100%.
+constexpr float kLabelScaleDownOnPhaseChange = 0.8;
 
 // Animation parameters for capture bar overlapping the user capture region.
 // The default animation duration for opacity changes to the capture bar.
@@ -546,7 +558,7 @@ void CaptureModeSession::Initialize() {
   if (Shell::Get()->accessibility_controller()->spoken_feedback().enabled())
     focus_cycler_->AdvanceFocus(/*reverse=*/false);
 
-  UpdateCaptureLabelWidget();
+  UpdateCaptureLabelWidget(CaptureLabelAnimation::kNone);
   RefreshStackingOrder(parent);
 
   UpdateCursor(display::Screen::GetScreen()->GetCursorScreenPoint(),
@@ -629,7 +641,7 @@ void CaptureModeSession::OnCaptureSourceChanged(CaptureModeSource new_source) {
   capture_mode_bar_view_->OnCaptureSourceChanged(new_source);
   UpdateDimensionsLabelWidget(/*is_resizing=*/false);
   layer()->SchedulePaint(layer()->bounds());
-  UpdateCaptureLabelWidget();
+  UpdateCaptureLabelWidget(CaptureLabelAnimation::kNone);
   UpdateCursor(display::Screen::GetScreen()->GetCursorScreenPoint(),
                /*is_touch=*/false);
 
@@ -642,7 +654,7 @@ void CaptureModeSession::OnCaptureSourceChanged(CaptureModeSource new_source) {
 
 void CaptureModeSession::OnCaptureTypeChanged(CaptureModeType new_type) {
   capture_mode_bar_view_->OnCaptureTypeChanged(new_type);
-  UpdateCaptureLabelWidget();
+  UpdateCaptureLabelWidget(CaptureLabelAnimation::kNone);
   UpdateCursor(display::Screen::GetScreen()->GetCursorScreenPoint(),
                /*is_touch=*/false);
 
@@ -697,7 +709,7 @@ void CaptureModeSession::StartCountDown(
   CaptureLabelView* label_view =
       static_cast<CaptureLabelView*>(capture_label_widget_->GetContentsView());
   label_view->StartCountDown(std::move(countdown_finished_callback));
-  UpdateCaptureLabelWidgetBounds(/*animate=*/true);
+  UpdateCaptureLabelWidgetBounds(CaptureLabelAnimation::kCountdownStart);
 
   UpdateCursor(display::Screen::GetScreen()->GetCursorScreenPoint(),
                /*is_touch=*/false);
@@ -817,13 +829,13 @@ void CaptureModeSession::OnTouchEvent(ui::TouchEvent* event) {
 }
 
 void CaptureModeSession::OnTabletModeStarted() {
-  UpdateCaptureLabelWidget();
+  UpdateCaptureLabelWidget(CaptureLabelAnimation::kNone);
   UpdateCursor(display::Screen::GetScreen()->GetCursorScreenPoint(),
                /*is_touch=*/false);
 }
 
 void CaptureModeSession::OnTabletModeEnded() {
-  UpdateCaptureLabelWidget();
+  UpdateCaptureLabelWidget(CaptureLabelAnimation::kNone);
   UpdateCursor(display::Screen::GetScreen()->GetCursorScreenPoint(),
                /*is_touch=*/false);
 }
@@ -864,7 +876,7 @@ void CaptureModeSession::OnDisplayMetricsChanged(
   capture_mode_bar_widget_->SetBounds(
       CaptureModeBarView::GetBounds(current_root_));
   if (capture_label_widget_)
-    UpdateCaptureLabelWidget();
+    UpdateCaptureLabelWidget(CaptureLabelAnimation::kNone);
   layer()->SchedulePaint(layer()->bounds());
 }
 
@@ -1316,7 +1328,7 @@ void CaptureModeSession::OnLocatedEventReleased(
 
   // After first release event, we advance to the next phase.
   is_selecting_region_ = false;
-  UpdateCaptureLabelWidget();
+  UpdateCaptureLabelWidget(CaptureLabelAnimation::kRegionPhaseChange);
 }
 
 void CaptureModeSession::UpdateCaptureRegion(
@@ -1337,7 +1349,7 @@ void CaptureModeSession::UpdateCaptureRegion(
 
   controller_->SetUserCaptureRegion(new_capture_region, by_user);
   UpdateDimensionsLabelWidget(is_resizing);
-  UpdateCaptureLabelWidget();
+  UpdateCaptureLabelWidget(CaptureLabelAnimation::kNone);
 }
 
 void CaptureModeSession::UpdateDimensionsLabelWidget(bool is_resizing) {
@@ -1467,7 +1479,8 @@ std::vector<gfx::Point> CaptureModeSession::GetAnchorPointsForPosition(
   return anchor_points;
 }
 
-void CaptureModeSession::UpdateCaptureLabelWidget() {
+void CaptureModeSession::UpdateCaptureLabelWidget(
+    CaptureLabelAnimation animation_type) {
   if (!capture_label_widget_) {
     capture_label_widget_ = std::make_unique<views::Widget>();
     auto* parent = GetParentContainer(current_root_);
@@ -1481,12 +1494,13 @@ void CaptureModeSession::UpdateCaptureLabelWidget() {
   CaptureLabelView* label_view =
       static_cast<CaptureLabelView*>(capture_label_widget_->GetContentsView());
   label_view->UpdateIconAndText();
-  UpdateCaptureLabelWidgetBounds(/*animate=*/false);
+  UpdateCaptureLabelWidgetBounds(animation_type);
 
   focus_cycler_->OnCaptureLabelWidgetUpdated();
 }
 
-void CaptureModeSession::UpdateCaptureLabelWidgetBounds(bool animate) {
+void CaptureModeSession::UpdateCaptureLabelWidgetBounds(
+    CaptureLabelAnimation animation_type) {
   DCHECK(capture_label_widget_);
 
   const gfx::Rect bounds = CalculateCaptureLabelWidgetBounds();
@@ -1495,20 +1509,45 @@ void CaptureModeSession::UpdateCaptureLabelWidgetBounds(bool animate) {
   if (old_bounds == bounds)
     return;
 
-  if (!animate) {
+  if (animation_type == CaptureLabelAnimation::kNone) {
     capture_label_widget_->SetBounds(bounds);
     return;
   }
 
   ui::Layer* layer = capture_label_widget_->GetLayer();
+  ui::LayerAnimator* animator = layer->GetAnimator();
+
+  if (animation_type == CaptureLabelAnimation::kRegionPhaseChange) {
+    capture_label_widget_->SetBounds(bounds);
+    const gfx::Point center_point = bounds.CenterPoint();
+    layer->SetTransform(
+        gfx::GetScaleTransform(gfx::Point(center_point.x() - bounds.x(),
+                                          center_point.y() - bounds.y()),
+                               kLabelScaleDownOnPhaseChange));
+    layer->SetOpacity(0.f);
+
+    ui::ScopedLayerAnimationSettings settings(animator);
+    settings.SetTransitionDuration(kCaptureLabelRegionPhaseChangeDuration);
+    settings.SetTweenType(gfx::Tween::ACCEL_LIN_DECEL_100);
+    settings.SetPreemptionStrategy(ui::LayerAnimator::ENQUEUE_NEW_ANIMATION);
+    animator->SchedulePauseForProperties(
+        kCaptureLabelRegionPhaseChangeDelay,
+        ui::LayerAnimationElement::TRANSFORM |
+            ui::LayerAnimationElement::OPACITY);
+    layer->SetTransform(gfx::Transform());
+    layer->SetOpacity(1.f);
+    return;
+  }
+
+  DCHECK_EQ(CaptureLabelAnimation::kCountdownStart, animation_type);
   if (!old_bounds.IsEmpty()) {
     // This happens if there is a label or a label button showing when count
     // down starts. In this case we'll do a bounds change animation.
-    ui::ScopedLayerAnimationSettings settings(layer->GetAnimator());
+    ui::ScopedLayerAnimationSettings settings(animator);
     settings.SetTweenType(gfx::Tween::LINEAR_OUT_SLOW_IN);
     settings.SetPreemptionStrategy(
         ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET);
-    settings.SetTransitionDuration(kCaptureLabelAnimationDuration);
+    settings.SetTransitionDuration(kCaptureLabelCountdownStartDuration);
     capture_label_widget_->SetBounds(bounds);
   } else {
     // This happens when no text message was showing when count down starts, in
@@ -1522,8 +1561,8 @@ void CaptureModeSession::UpdateCaptureLabelWidgetBounds(bool animate) {
     layer->SetOpacity(0.f);
 
     // Fade in.
-    ui::ScopedLayerAnimationSettings settings(layer->GetAnimator());
-    settings.SetTransitionDuration(kCaptureLabelAnimationDuration);
+    ui::ScopedLayerAnimationSettings settings(animator);
+    settings.SetTransitionDuration(kCaptureLabelCountdownStartDuration);
     settings.SetTweenType(gfx::Tween::LINEAR);
     settings.SetPreemptionStrategy(
         ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET);
@@ -1709,7 +1748,7 @@ void CaptureModeSession::MaybeChangeRoot(aura::Window* new_root) {
   // bounds, moving it onto the correct display, but will early return if the
   // region is already empty.
   if (controller_->user_capture_region().IsEmpty())
-    UpdateCaptureLabelWidgetBounds(/*animate=*/false);
+    UpdateCaptureLabelWidgetBounds(CaptureLabelAnimation::kNone);
 
   // Start with a new region when we switch displays.
   is_selecting_region_ = true;
