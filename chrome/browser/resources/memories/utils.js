9e0ec097e0166fd7530b9d04e8fbc2e10541954c
Moe Ahmadi <mahmadi@chromium.org>
2021-03-24 03:23:59 +0000

    [memories] Memories landing page (part 3)
    Related tab groups and bookmarks section of the memory card
    
    screenshot/BC46BmffK375p7i
    
    Bug: 1180970
    Change-Id: I35b29fe8578854bed389a9120bb76212afc63090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2745786
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#865970}

--- chrome/browser/resources/memories/utils.js
+++ chrome/browser/resources/memories/utils.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {String16} from 'chrome://resources/mojo/mojo/public/mojom/base/string16.mojom-webui.js';
+import {Url} from 'chrome://resources/mojo/url/mojom/url.mojom-webui.js';
 
 /**
  * @fileoverview This file provides shared utility functions used by the custom
@@ -17,4 +18,12 @@ import {String16} from 'chrome://resources/mojo/mojo/public/mojom/base/string16.
  */
 export function decodeMojoString16(str) {
   return str ? str.data.map(ch => String.fromCodePoint(ch)).join('') : '';
+}
+
+/**
+ * @param {!Url} url
+ * @return {string} The domain name of the URL without the leading 'www.'.
+ */
+export function getHostnameFromUrl(url) {
+  return new URL(url.url).hostname.replace(/^(www\.)/, '');
 }
