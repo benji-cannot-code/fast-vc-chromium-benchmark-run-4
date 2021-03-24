6eac4f061373c873c39d9b1ca0e0021671e98074
Simon Zünd <szuend@chromium.org>
2021-03-24 12:49:14 +0000

    [devtools] Change how cookies are injected for HAR web test
    The DevTools CL https://crrev.com/c/2782548 removes frontend side
    request cookie parsing. The HAR web test relies on that parsing step
    though, so it has to be rewritten to inject the custom cookies
    into `includedRequestCookies`.
    
    R=sigurds@chromium.org
    
    Bug: chromium:1192000
    Change-Id: Ie1e1a2cad2bd841c902bd369f1a86a60fd05de2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784600
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866085}

--- third_party/blink/web_tests/http/tests/devtools/resource-har-conversion.js
+++ third_party/blink/web_tests/http/tests/devtools/resource-har-conversion.js
@@ -18,15 +18,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   `);
 
   function findRequestByURL(url) {
-    const requests = NetworkTestRunner.networkRequests();
-    for (var i = 0; i < requests.length; ++i) {
-      if (url.test(requests[i].url()))
-        return requests[i];
-    }
+    return NetworkTestRunner.networkRequests().find(request => url.test(request.url()));
   }
 
   function addCookieHeadersToRequest(request) {
-    request.setRequestHeaders([{name: 'Cookie', value: 'a=b; $Path=/path; $Domain=example.com; a1=b1\nc1=d1'}]);
+    const c1 = new SDK.Cookie('a', 'b');
+    c1.addAttribute('path', '/path');
+    c1.addAttribute('domain', 'example.com');
+    request._includedRequestCookies = [
+      c1,
+      new SDK.Cookie('a1', 'b1'),
+      new SDK.Cookie('c1', 'd1'),
+    ];
 
     request.responseHeaders = [{
       name: 'Set-Cookie',
