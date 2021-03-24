cd2f29299e945db5648955e5633039be28d5bbf4
Michael Thiessen <mthiesse@chromium.org>
2021-03-24 16:11:17 +0000

    Increase poll for UrlOverridingTest#testRedirectionFromIntentWarm
    The android-pie-x86-rel bot is crazy slow and sometimes times out on
    this poll.
    
    Change-Id: I38acf6617b1f40e6dda502d7d0fcd166059962db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782724
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866139}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -468,8 +468,9 @@ public class UrlOverridingTest {
                 ChromeTabbedActivity.class, Stage.CREATED, () -> context.startActivity(intent));
         mActivityTestRule.setActivity(activity);
 
-        CriteriaHelper.pollUiThread(
-                () -> Criteria.checkThat(mActivityMonitor.getHits(), Matchers.is(1)));
+        CriteriaHelper.pollUiThread(() -> {
+            Criteria.checkThat(mActivityMonitor.getHits(), Matchers.is(1));
+        }, 10000L, CriteriaHelper.DEFAULT_POLLING_INTERVAL);
         CriteriaHelper.pollUiThread(
                 () -> AsyncInitializationActivity.wasMoveTaskToBackInterceptedForTesting());
     }
@@ -488,8 +489,9 @@ public class UrlOverridingTest {
         AsyncInitializationActivity.interceptMoveTaskToBackForTesting();
         context.startActivity(intent);
 
-        CriteriaHelper.pollUiThread(
-                () -> Criteria.checkThat(mActivityMonitor.getHits(), Matchers.is(1)));
+        CriteriaHelper.pollUiThread(() -> {
+            Criteria.checkThat(mActivityMonitor.getHits(), Matchers.is(1));
+        }, 10000L, CriteriaHelper.DEFAULT_POLLING_INTERVAL);
         CriteriaHelper.pollUiThread(
                 () -> AsyncInitializationActivity.wasMoveTaskToBackInterceptedForTesting());
     }
