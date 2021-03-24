10e574abd9e92c9973a020a63944257911ae4333
Stephen Chenney <schenney@chromium.org>
2021-03-24 23:37:14 +0000

    [Paint Holding] De-flake CommandsApiTest.ContinuePropagation
    The test is very flaky on ChromeOS testers. So allow
    early input.
    
    Bug: 1191635
    Change-Id: Ia9b3963725225343ee219ba03101ec49f3ef6dd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785385
    Auto-Submit: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866358}

--- chrome/browser/extensions/extension_keybinding_apitest.cc
+++ chrome/browser/extensions/extension_keybinding_apitest.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/test/result_catcher.h"
 #include "extensions/test/test_extension_dir.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "third_party/blink/public/common/switches.h"
 
 using content::WebContents;
 
@@ -246,6 +247,14 @@ class CommandsApiTest : public ExtensionApiTest {
     waiter.WaitForActivation();
     ASSERT_TRUE(browser()->window()->IsActive());
 #endif
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    ExtensionApiTest::SetUpCommandLine(command_line);
+    // Some builders are flaky due to slower loading interacting with
+    // deferred commits. This primarily impacts chromeos for the test
+    // CommandsApiTest.ContinuePropagation.
+    command_line->AppendSwitch(blink::switches::kAllowPreCommitInput);
   }
 
  protected:
