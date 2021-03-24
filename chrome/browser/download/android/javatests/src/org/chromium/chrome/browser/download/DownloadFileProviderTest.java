350fd1d706306edfd3d79bc3d32624bced27fa1a
David Maunder <davidjm@chromium.org>
2021-03-24 15:38:14 +0000

    Batch DownloadFileProviderTest
    Speedup 5745ms -> 151ms (97% improvement)
    
    Bug: 1191086
    Change-Id: I53a7c88b724b9f05381b93da477fcef8c213e6f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2779635
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866128}

--- chrome/browser/download/android/javatests/src/org/chromium/chrome/browser/download/DownloadFileProviderTest.java
+++ chrome/browser/download/android/javatests/src/org/chromium/chrome/browser/download/DownloadFileProviderTest.java
@@ -13,17 +13,16 @@ import androidx.test.filters.SmallTest;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.download.DownloadDirectoryProvider.SecondaryStorageInfo;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -33,6 +32,7 @@ import java.util.List;
  * Test content URI can be generated correctly by {@link DownloadFileProvider}.
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
+@Batch(Batch.UNIT_TESTS)
 public class DownloadFileProviderTest {
     private static final String PRIMARY_STORAGE_DOWNLOAD_DIRECTORY_PATH =
             "/storage/emulated/1234/Download";
@@ -51,9 +51,6 @@ public class DownloadFileProviderTest {
 
     private static final String EXTERNAL_SD_CARD_DOWNLOAD_PATH =
             EXTERNAL_SD_CARD_DOWNLOAD_DIRECTORY_PATH + "/app-wise-release.apk";
-
-    @Rule
-    public final ChromeTabbedActivityTestRule mTestRule = new ChromeTabbedActivityTestRule();
 
     @Mock
     private DownloadDirectoryProvider.Delegate mMockDirectoryDelegate;
