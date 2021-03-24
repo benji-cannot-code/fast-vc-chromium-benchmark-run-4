d9ad41ce42abd3e4022ba92111c3fb3462eac30b
Xiaodan Zhu <zxdan@chromium.org>
2021-03-24 18:46:52 +0000

    Scroll desks bar when drag a desk on the arrow buttons.
    This CL implemented behaviors below:
    - Scroll the desk bar every 1 second when the user long presses on
    the arrow button by mouse.
    - Scroll the desk bar every 1 second when the user long presses on
    the arrow button by gesture.
    - Scroll the desk bar every 1 second when the user drags a desk on
    the arrow button.
    - Add scroll animation.
    
    The effects are shown in the videos in crbug.
    
          DesksBentoTest.ScrollBarByDraggedDesk
    
    Test: DesksBentoTest.ContinueScrollBar
    Bug: 1175272
    Change-Id: I5a5004ea6c7481a9367c8686024873b267342d20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757618
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866211}

--- ash/wm/desks/scroll_arrow_button.h
+++ ash/wm/desks/scroll_arrow_button.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WM_DESKS_SCROLL_ARROW_BUTTON_H_
 
 #include "ash/ash_export.h"
+#include "base/timer/timer.h"
 #include "ui/views/controls/button/button.h"
 
 namespace ash {
@@ -19,21 +20,34 @@ class DesksBarView;
 // customize the icon's layout.
 class ASH_EXPORT ScrollArrowButton : public views::Button {
  public:
-  ScrollArrowButton(PressedCallback callback,
+  ScrollArrowButton(base::RepeatingClosure on_scroll,
                     bool is_left_arrow,
                     DesksBarView* bar_view);
   ScrollArrowButton(const ScrollArrowButton&) = delete;
   ScrollArrowButton& operator=(const ScrollArrowButton&) = delete;
-  ~ScrollArrowButton() override = default;
+  ~ScrollArrowButton() override;
 
   // views::Button:
   void PaintButtonContents(gfx::Canvas* canvas) override;
   void OnThemeChanged() override;
   const char* GetClassName() const override;
 
+  // Called when a desk is being dragged and hovering on the button.
+  void OnDeskHoverStart();
+  // Called when the desk is dropped or leaves the button.
+  void OnDeskHoverEnd();
+
  private:
+  void OnStateChanged();
+
+  // The callback of bar scroll method.
+  base::RepeatingClosure on_scroll_;
+  // The subscription of button state change callback.
+  base::CallbackListSubscription state_change_subscription_;
   const bool is_left_arrow_;
   DesksBarView* const bar_view_;  // Not owned.
+  // If the button is kept pressed, trigger scroll every one second.
+  base::RepeatingTimer timer_;
 };
 
 }  // namespace ash
