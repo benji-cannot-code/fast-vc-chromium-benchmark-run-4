b5d20c5558520bb754d9e978f10bea11866795cb
Gavin Williams <gavinwill@chromium.org>
2021-03-24 21:08:46 +0000

    scanning: Add Edit button to open Media app
    Add a button row on the Done page for opening the Media app.
    
    Screenshot: http://screen/5Yqz56mpBauCimk
    
    Bug: 1185408
    Change-Id: Ie7c8b5bbd771f3be599a16790fef3d65819c87bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2775472
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866302}

--- chromeos/components/scanning/scanning_ui.cc
+++ chromeos/components/scanning/scanning_ui.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "ash/constants/ash_features.h"
 #include "base/containers/span.h"
+#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "chromeos/components/scanning/mojom/scanning.mojom.h"
 #include "chromeos/components/scanning/scanning_app_delegate.h"
@@ -58,6 +60,7 @@ void AddScanningAppStrings(content::WebUIDataSource* html_source) {
       {"colorOptionText", IDS_SCANNING_APP_COLOR_OPTION_TEXT},
       {"defaultSourceOptionText", IDS_SCANNING_APP_DEFAULT_SOURCE_OPTION_TEXT},
       {"doneButtonText", IDS_SCANNING_APP_DONE_BUTTON_TEXT},
+      {"editButtonLabel", IDS_SCANNING_APP_EDIT_BUTTON_LABEL},
       {"fileNotFoundToastText", IDS_SCANNING_APP_FILE_NOT_FOUND_TOAST_TEXT},
       {"fileTypeDropdownLabel", IDS_SCANNING_APP_FILE_TYPE_DROPDOWN_LABEL},
       {"fitToScanAreaOptionText",
@@ -112,6 +115,12 @@ void AddScanningAppPluralStrings(ScanningHandler* handler) {
     handler->AddStringToPluralMap(str.name, str.id);
 }
 
+void AddFeatureFlags(content::WebUIDataSource* html_source) {
+  html_source->AddBoolean(
+      "scanAppMediaLinkEnabled",
+      base::FeatureList::IsEnabled(chromeos::features::kScanAppMediaLink));
+}
+
 }  // namespace
 
 ScanningUI::ScanningUI(
@@ -136,6 +145,8 @@ ScanningUI::ScanningUI(
                                IDR_SCANNING_MOJO_LITE_JS);
   html_source->AddResourcePath("file_path.mojom-lite.js",
                                IDR_SCANNING_APP_FILE_PATH_MOJO_LITE_JS);
+
+  AddFeatureFlags(html_source.get());
 
   AddScanningAppStrings(html_source.get());
 
