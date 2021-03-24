e90b7d000b489a8c33866a8f85ca36fa2cd363a4
Daniel Hosseinian <dhoss@chromium.org>
2021-03-24 23:23:22 +0000

    [unseasoned-pdf] Migrate `full_`
    Rename `full_` to the more descriptive `full_frame_`. Migrate it from
    OutOfProcessInstance to PdfViewPluginBase and provide an accessor and
    a mutator, so that it can be shared by both PdfViewWebPlugin
    and OutOfProcessInstance.
    
    Bug: 1140629
    Change-Id: Ic7c90148a77e3bf6ce3faa95680d3b9159febc32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785541
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866352}

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -505,14 +505,6 @@ bool OutOfProcessInstance::Init(uint32_t argc,
   CHECK(base::StartsWith(document_url_piece, kChromeExtension) ||
         is_print_preview_);
 
-  // Check if the plugin is full frame. This is passed in from JS.
-  for (uint32_t i = 0; i < argc; ++i) {
-    if (strcmp(argn[i], "full-frame") == 0) {
-      full_ = true;
-      break;
-    }
-  }
-
   // Allow the plugin to handle find requests.
   SetPluginToHandleFindRequests();
 
@@ -534,6 +526,8 @@ bool OutOfProcessInstance::Init(uint32_t argc,
       top_level_url = argv[i];
     } else if (strcmp(argn[i], "headers") == 0) {
       headers = argv[i];
+    } else if (strcmp(argn[i], "full-frame") == 0) {
+      set_full_frame(true);
     } else if (strcmp(argn[i], "background-color") == 0) {
       SkColor background_color;
       if (!base::StringToUint(argv[i], &background_color))
@@ -1003,7 +997,7 @@ void OutOfProcessInstance::FormDidOpen(int32_t result) {
 }
 
 std::unique_ptr<UrlLoader> OutOfProcessInstance::CreateUrlLoader() {
-  if (full_) {
+  if (full_frame()) {
     if (!did_call_start_loading_) {
       did_call_start_loading_ = true;
       pp::PDF::DidStartLoading(this);
@@ -1071,7 +1065,7 @@ void OutOfProcessInstance::DocumentLoadComplete() {
   if (accessibility_state() == AccessibilityState::kPending)
     LoadAccessibility();
 
-  if (!full_)
+  if (!full_frame())
     return;
 
   if (did_call_start_loading_) {
@@ -1292,7 +1286,7 @@ void OutOfProcessInstance::DocumentHasUnsupportedFeature(
   }
 
   // Since we use an info bar, only do this for full frame plugins..
-  if (!full_)
+  if (!full_frame())
     return;
 
   if (told_browser_about_unsupported_feature_)
