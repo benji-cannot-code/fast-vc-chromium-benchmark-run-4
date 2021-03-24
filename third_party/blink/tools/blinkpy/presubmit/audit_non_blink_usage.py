3eeb43ee35db7a7a3ff8a18b7b1993fe42be20c9
Juanmi <juanmihd@chromium.org>
2021-03-24 21:49:19 +0000

    Adding TransferToImageBitmap to WebGPU
    This CL adds the implementation for TransferToImageBitmap in WebGPU.
    TransferToImageBitmap is used from OffscreenCanvas to produce an
    ImageBitmap that can be passed to other context to be presented there.
    The canonical example would be to use an ImageBitmapRenderingContext,
    that would TrasnferFromImageBitmap and receive the ImageBitmap generated
    by TransferToImageBitmap in the OffscreenCanvas.
    
    The test will be added directly to WebGPU CTS.
    
    There is a bug in ImageBitmapRenderingContext about using the
    TransferFromImageBitmap with a WebGPU created bitmap:
    https://bugs.chromium.org/p/chromium/issues/detail?id=1190990
    There is a CL already in progress that would fix this issue,
    https://chromium-review.googlesource.com/c/chromium/src/+/2778696
    but that does not block this CL, but leaving it here for record.
    
    Change-Id: I5404934079336b4303d0449ac412072b29cf0e55
    Bug: 1191081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2765253
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Auto-Submit: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/master@{#866310}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -907,6 +907,7 @@ _CONFIG = [
             'media::PIXEL_FORMAT_Y16',
             'media::VideoFrame',
             'viz::RasterContextProvider',
+            'viz::TransferableResource',
         ],
     },
     {
