ff595ebc27b1ecb68f771cc0375ee1436d9e0773
Katie Dektar <katie@chromium.org>
2021-03-24 19:35:25 +0000

    Add continuous listening for dictation behind a flag.
    This allows dictation to keep listening after speech
    is finalized, which means users can dictate more than
    one sentence at a time.
    
    For the network speech case, after 5 seconds without speech
    from either start or finalized speech, Dictation will
    automatically turn off.
    
    Ensures the appropriate spacing between recognized speech
    phrases.
    
    without --enable-experimental-accessibility-dictation-listening,
    and added new browser_tests coverage.
    
    Bug: 1186859
    Change-Id: I779da0534dc43c8d1984aaf7f6405ea0a304f645
    Test: Tried with and without OnDeviceSpeechRecognizer, with and
    AX-Relnotes: N/A (behind flag)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2779848
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866236}

--- chrome/browser/ash/accessibility/dictation.cc
+++ chrome/browser/ash/accessibility/dictation.cc
@@ -78,7 +78,7 @@ bool Dictation::OnToggleDictation() {
     DictationOff();
     return false;
   }
-
+  has_committed_text_ = false;
   if (OnDeviceSpeechRecognizer::IsOnDeviceSpeechRecognizerAvailable() &&
       switches::IsExperimentalAccessibilityDictationOfflineEnabled()) {
     // On-device recognition is behind a flag and then only available if
@@ -97,11 +97,22 @@ bool Dictation::OnToggleDictation() {
 }
 
 void Dictation::OnSpeechResult(
-    const std::u16string& query,
+    const std::u16string& transcription,
     bool is_final,
     const base::Optional<SpeechRecognizerDelegate::TranscriptTiming>&
         word_offsets) {
-  composition_->text = query;
+  // If the first character of text isn't a space, add a space before it.
+  // NetworkSpeechRecognizer adds the preceding space but
+  // OnDeviceSpeechRecognizer does not. This is also done in
+  // CaptionBubbleModel::CommitPartialText.
+  // TODO(crbug.com/1055150): This feature is launching for English first.
+  // Make sure spacing is correct for all languages.
+  if (has_committed_text_ && transcription.size() > 0 &&
+      transcription.compare(0, 1, u" ") != 0) {
+    composition_->text = u" " + transcription;
+  } else {
+    composition_->text = transcription;
+  }
 
   if (!is_final) {
     // If ChromeVox is enabled, we don't want to show intermediate results
@@ -113,7 +124,12 @@ void Dictation::OnSpeechResult(
       input_context->UpdateCompositionText(*composition_, 0, true);
     return;
   }
-  DictationOff();
+  if (switches::IsExperimentalAccessibilityDictationListeningEnabled()) {
+    CommitCurrentText();
+  } else {
+    // Turn off after finalized speech.
+    DictationOff();
+  }
 }
 
 void Dictation::OnSpeechSoundLevelChanged(int16_t level) {}
@@ -158,17 +174,9 @@ void Dictation::DictationOff() {
   if (!speech_recognizer_)
     return;
 
+  CommitCurrentText();
   if (!composition_->text.empty()) {
     audio::SoundsManager::Get()->Play(static_cast<int>(Sound::kDictationEnd));
-
-    ui::IMEInputContextHandlerInterface* input_context = GetInputContext();
-    if (input_context) {
-      input_context->CommitText(
-          composition_->text,
-          ui::TextInputClient::InsertTextCursorBehavior::kMoveCursorAfterText);
-    }
-
-    composition_->text = std::u16string();
   } else {
     audio::SoundsManager::Get()->Play(
         static_cast<int>(Sound::kDictationCancel));
@@ -178,6 +186,21 @@ void Dictation::DictationOff() {
       AccessibilityNotificationType::kToggleDictation, false /* enabled */);
   AccessibilityManager::Get()->NotifyAccessibilityStatusChanged(details);
   speech_recognizer_.reset();
+}
+
+void Dictation::CommitCurrentText() {
+  if (composition_->text.empty()) {
+    return;
+  }
+  has_committed_text_ = true;
+  ui::IMEInputContextHandlerInterface* input_context = GetInputContext();
+  if (input_context) {
+    input_context->CommitText(
+        composition_->text,
+        ui::TextInputClient::InsertTextCursorBehavior::kMoveCursorAfterText);
+  }
+
+  composition_->text = std::u16string();
 }
 
 }  // namespace ash
