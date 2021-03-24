20cdd95b938ecceb5892abf953604d2991601a92
Sebastien Marchand <sebmarchand@chromium.org>
2021-03-24 17:07:54 +0000

    [battery] Add the visibility time for the origin associated w the event
    This metric has been approved in the UKM privacy document: https://docs.google.com/document/d/18cX0Aug0YuUAfW7v3eOeDzilS1IzcnCoFp616zzI_Vo/edit?disco=AAAAIB3G5tc
    
    Bug: 1187223
    Change-Id: I9ddb954527a787da738baf2819bc10f0f245b349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2754607
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866169}

--- chrome/browser/metrics/power/power_metrics_reporter_unittest.cc
+++ chrome/browser/metrics/power/power_metrics_reporter_unittest.cc
@@ -177,6 +177,8 @@ TEST_F(PowerMetricsReporterUnitTest, UKMs) {
       base::TimeDelta::FromSeconds(++fake_value);
   fake_interval_data.time_playing_audio =
       base::TimeDelta::FromSeconds(++fake_value);
+  fake_interval_data.longest_visible_origin_duration =
+      base::TimeDelta::FromSeconds(++fake_value);
 
   task_environment_.FastForwardBy(kExpectedMetricsCollectionInterval);
   // Pretend that the battery has dropped by 50% in 2 minutes, for a rate of
@@ -273,6 +275,10 @@ TEST_F(PowerMetricsReporterUnitTest, UKMs) {
       entries[0], UkmEntry::kPlayingAudioSecondsName,
       PowerMetricsReporter::GetBucketForSampleForTesting(
           fake_interval_data.time_playing_audio));
+  test_ukm_recorder_.ExpectEntryMetric(
+      entries[0], UkmEntry::kOriginVisibilityTimeSecondsName,
+      PowerMetricsReporter::GetBucketForSampleForTesting(
+          fake_interval_data.longest_visible_origin_duration));
 
   histogram_tester_.ExpectUniqueSample(kBatteryDischargeRateHistogramName, 2500,
                                        1);
