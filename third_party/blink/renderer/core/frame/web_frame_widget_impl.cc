a33c2e6909422364b54ee4bc26b054a1028db2ba
Stephen Nusko <nuskos@chromium.org>
2021-03-24 11:00:23 +0000

    Increase InputTargetClient interface to user input blocking priority.
    Introduce a new TaskType priority that maps to kInput queue which is a
    high priority queue for tasks which are input related. While a Target
    result is requested in the browser all input events are queued and thus
    if the RendererMain is busy and we don't reply to the InputTargetClient
    request we will block all input until it gets a change to run.
    
    We introduce a new TaskType of kInternalInputBlocking and bind the
    InputTargetClient mojo interface to it, which maps to the kInput queue
    which uses a high priority task runner.
    
    Bug: b:182568202
    Change-Id: Ib46d226ad0704b08b891eab31f16414c7c58be66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774064
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866056}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/trees/layer_tree_host.h"
 #include "cc/trees/swap_promise.h"
 #include "cc/trees/ukm_manager.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/input/input_handler.mojom-blink.h"
 #include "third_party/blink/public/mojom/input/touch_event.mojom-blink.h"
 #include "third_party/blink/public/platform/platform.h"
@@ -521,9 +522,13 @@ void WebFrameWidgetImpl::BindWidgetCompositor(
 void WebFrameWidgetImpl::BindInputTargetClient(
     mojo::PendingReceiver<viz::mojom::blink::InputTargetClient> receiver) {
   DCHECK(!input_target_receiver_.is_bound());
-  input_target_receiver_.Bind(
-      std::move(receiver),
-      local_root_->GetTaskRunner(TaskType::kInternalDefault));
+  TaskType priority = TaskType::kInternalDefault;
+  if (base::FeatureList::IsEnabled(
+          blink::features::kInputTargetClientHighPriority)) {
+    priority = TaskType::kInternalInputBlocking;
+  }
+  input_target_receiver_.Bind(std::move(receiver),
+                              local_root_->GetTaskRunner(priority));
 }
 
 void WebFrameWidgetImpl::FrameSinkIdAt(const gfx::PointF& point,
--- third_party/blink/renderer/platform/scheduler/worker/worker_scheduler.cc
+++ third_party/blink/renderer/platform/scheduler/worker/worker_scheduler.cc
@@ -218,6 +218,7 @@ scoped_refptr<base::SingleThreadTaskRunner> WorkerScheduler::GetTaskRunner(
     case TaskType::kInternalFrameLifecycleControl:
     case TaskType::kInternalFindInPage:
     case TaskType::kInternalHighPriorityLocalFrame:
+    case TaskType::kInternalInputBlocking:
     case TaskType::kMainThreadTaskQueueIPCTracking:
     case TaskType::kCount:
       NOTREACHED();
