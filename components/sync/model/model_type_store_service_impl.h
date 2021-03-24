5961c089405b0adddd4132ebc3e9cacfbfa4b061
Mikel Astiz <mastiz@chromium.org>
2021-03-24 12:27:59 +0000

    [sync] Fix DCHECK failures during ModelTypeStoreBackend destruction
    This is a follow-up to crrev.com/c/2443710 which tried to avoid
    ModelTypeStoreBackend being destroyed on the UI thread and running
    into blocking waits.
    
    Despite the patch above, in rare cases, ModelTypeStoreBackend can be
    destroyed on arbitrary sequences, leading to test flakiness and -in
    theory- DCHECK failures in the wild.
    
    This is because the class is refcounted and, as per API contract in
    ModelTypeStoreService::GetStoreFactory(), references to it don't have
    lifetime constraints and in particular may outlive keyed service
    MoldeTypeStoreServiceImpl.
    
    In such rare cases, the last reference released could be the factory
    RepeatingCallback itself or some task posted to the UI thread, which
    means ModelTypeStoreBackend gets destroyed on the UI thread. To avoid
    blocking it, this patch ensures that a task is posted to the backend
    sequence when destroying leveldb::DB.
    
    Change-Id: If2ac9a0d020fd0caf86ec2bd41e8b701ab6d7549
    Bug: 1190187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2778399
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866077}

--- components/sync/model/model_type_store_service_impl.h
+++ components/sync/model/model_type_store_service_impl.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/files/file_path.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/sequenced_task_runner.h"
 #include "components/sync/model/model_type_store.h"
@@ -45,12 +44,11 @@ class ModelTypeStoreServiceImpl : public ModelTypeStoreService {
   // The backend sequence or thread.
   const scoped_refptr<base::SequencedTaskRunner> backend_task_runner_;
 
-  // Constructed in the UI thread, used and destroyed in |backend_task_runner_|.
-  scoped_refptr<ModelTypeStoreBackend> store_backend_;
+  // Constructed on the UI thread, used on |backend_task_runner_| and destroyed
+  // on any sequence.
+  const scoped_refptr<ModelTypeStoreBackend> store_backend_;
 
   SEQUENCE_CHECKER(ui_sequence_checker_);
-
-  base::WeakPtrFactory<ModelTypeStoreServiceImpl> weak_ptr_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(ModelTypeStoreServiceImpl);
 };
