bef051e156e17fe2f4c62710323c4347c2446408
Gauthier Ambard <gambard@chromium.org>
2021-03-24 16:37:01 +0000

    [iOS] Remove omnibox geolocation classes now unused
    This CL removes the Location manager, the provider and all the classes
    related to the XGeo header.
    
    Bug: 1178627
    Change-Id: I4939ed3fccf67a9b77f6e6f6e6a532c6ec715d77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2707106
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866158}

--- ios/chrome/browser/geolocation/omnibox_geolocation_controller.mm
+++ ios/chrome/browser/geolocation/omnibox_geolocation_controller.mm
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/version_info/version_info.h"
 #include "ios/chrome/app/tests_hook.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
-#import "ios/chrome/browser/geolocation/CLLocation+OmniboxGeolocation.h"
-#import "ios/chrome/browser/geolocation/CLLocation+XGeoHeader.h"
 #import "ios/chrome/browser/geolocation/omnibox_geolocation_authorization_alert.h"
 #import "ios/chrome/browser/geolocation/omnibox_geolocation_config.h"
 #import "ios/chrome/browser/geolocation/omnibox_geolocation_controller+Testing.h"
--- ios/public/provider/chrome/browser/chrome_browser_provider.mm
+++ ios/public/provider/chrome/browser/chrome_browser_provider.mm
@@ -67,11 +67,6 @@ ChromeBrowserProvider::GetChromeTrustedVaultService() {
   return nullptr;
 }
 
-GeolocationUpdaterProvider*
-ChromeBrowserProvider::GetGeolocationUpdaterProvider() {
-  return nullptr;
-}
-
 std::string ChromeBrowserProvider::GetRiskData() {
   return std::string();
 }
