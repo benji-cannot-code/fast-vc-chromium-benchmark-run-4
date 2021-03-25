c937176e2950ff8b101f2da70465739550707c9c
Sammie Quon <sammiequon@chromium.org>
2021-03-25 19:15:44 +0000

    full_restore: ARC apps can be restored part 1.
    ARC apps will save to file. Modifies ModifyWidgetParams so that it can
    be used in a follow up CL in components/exo/. Prototype of follow up CL
    at crrev.com/c/2782068.
    
    Bug: 1190711, 1190714
    Test: none
    Change-Id: I37e02cfa970e0f7002594953a0f5fe00c404333b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2786164
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866698}

--- components/full_restore/full_restore_read_handler.h
+++ components/full_restore/full_restore_read_handler.h
@@ -24,6 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_observer.h"
 #include "ui/views/widget/widget.h"
 
+namespace chromeos {
+namespace full_restore {
+class AppLaunchHandlerBrowserTest;
+}
+}
+
 namespace full_restore {
 
 class FullRestoreFileHandler;
@@ -95,10 +101,6 @@ class COMPONENT_EXPORT(FULL_RESTORE) FullRestoreReadHandler
                                             const std::string& app_id,
                                             int32_t restore_window_id);
 
-  // Gets the window information for |restore_window_id| for browser windows and
-  // Chrome app windows only. This interface can't be used for Arc app windows.
-  std::unique_ptr<WindowInfo> GetWindowInfo(int32_t restore_window_id);
-
   // Gets the window information for |window|.
   std::unique_ptr<WindowInfo> GetWindowInfo(aura::Window* window);
 
@@ -112,9 +114,9 @@ class COMPONENT_EXPORT(FULL_RESTORE) FullRestoreReadHandler
   // first, and OnTaskCreated is called later..
   int32_t GetArcRestoreWindowId(int32_t task_id);
 
-  // Modifies |out_params| based on the window info associated with
-  // |restore_window_id|.
-  void ModifyWidgetParams(int32_t restore_window_id,
+  // Modifies `out_params` based on the window info associated with
+  // `restore_window_id`. Returns true if `out_params` was modified.
+  bool ModifyWidgetParams(int32_t restore_window_id,
                           views::Widget::InitParams* out_params);
 
   // Generates the ARC session id (1,000,000,001 - INT_MAX) for restored ARC
@@ -126,7 +128,12 @@ class COMPONENT_EXPORT(FULL_RESTORE) FullRestoreReadHandler
   void SetArcSessionIdForWindowId(int32_t arc_session_id, int32_t window_id);
 
  private:
+  friend class ::chromeos::full_restore::AppLaunchHandlerBrowserTest;
   friend class FullRestoreReadHandlerTestApi;
+
+  // Gets the window information for |restore_window_id| for browser windows and
+  // Chrome app windows only. This interface can't be used for ARC app windows.
+  std::unique_ptr<WindowInfo> GetWindowInfo(int32_t restore_window_id);
 
   // Invoked when reading the restore data from |profile_path| is finished, and
   // calls |callback| to notify that the reading operation is done.
