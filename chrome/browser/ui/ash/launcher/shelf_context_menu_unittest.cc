c9a2f2a0134b89934ff69d7241a93fcd260a283e
Glen Robertson <glenrob@chromium.org>
2021-03-25 14:30:28 +0000

    Add test util method AwaitStartWebAppProvider for common case.
    Unit tests (especially outside of web_apps code) often need a web app
    provider running with default subsystems. Added a simple util method to
    do that, including ensuring the startup finishes before we continue with
    the test to avoid flakes (or RunUntilIdle calls).
    
    Includes using a TestSystemWebAppManager to avoid installing a bunch of
    default apps on start().
    
    Also moves the test util methods into a test namespace so they're more
    distinguishable from local methods.
    
    In prep for adding another usage.
    
    Change-Id: I42c242b8f6040dea60392e80821bdeb9ac12aff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2781231
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866598}

--- chrome/browser/ui/ash/launcher/shelf_context_menu_unittest.cc
+++ chrome/browser/ui/ash/launcher/shelf_context_menu_unittest.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/shell_surface_util.h"
 #include "components/prefs/pref_service.h"
 #include "components/session_manager/core/session_manager.h"
+#include "content/public/common/content_switches.h"
 #include "ui/aura/window_event_dispatcher.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/display/display.h"
@@ -102,6 +103,9 @@ class ShelfContextMenuTest : public ChromeAshTestBase {
 
     base::CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kDisableDefaultApps);
+    // switches::kTestType is needed before SystemWebAppManager construction
+    // to skip adding the default SWAs.
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(switches::kTestType);
 
     profile_ = std::make_unique<TestingProfile>();
 
@@ -112,8 +116,7 @@ class ShelfContextMenuTest : public ChromeAshTestBase {
         base::CommandLine::ForCurrentProcess(), base::FilePath(), false);
     extension_service_->Init();
 
-    ConfigureWebAppProvider();
-    web_app::TestWebAppProvider::Get(profile())->Start();
+    web_app::test::AwaitStartWebAppProviderAndSubsystems(profile());
 
     crostini_helper_ = std::make_unique<CrostiniTestHelper>(profile());
     crostini_helper_->ReInitializeAppServiceIntegration();
@@ -227,15 +230,6 @@ class ShelfContextMenuTest : public ChromeAshTestBase {
   }
 
  private:
-  void ConfigureWebAppProvider() {
-    auto system_web_app_manager =
-        std::make_unique<web_app::TestSystemWebAppManager>(profile());
-
-    auto* provider = web_app::TestWebAppProvider::Get(profile());
-    provider->SetSystemWebAppManager(std::move(system_web_app_manager));
-    provider->SetRunSubsystemStartupTasks(true);
-  }
-
   base::test::ScopedCommandLine scoped_command_line_;
   std::unique_ptr<TestingProfile> profile_;
   std::unique_ptr<CrostiniTestHelper> crostini_helper_;
@@ -711,8 +705,8 @@ TEST_F(ShelfContextMenuTest, WebApp) {
   constexpr char kWebAppName[] = "WebApp1";
 
   app_service_test().FlushMojoCalls();
-  const web_app::AppId app_id =
-      web_app::InstallDummyWebApp(profile(), kWebAppName, GURL(kWebAppUrl));
+  const web_app::AppId app_id = web_app::test::InstallDummyWebApp(
+      profile(), kWebAppName, GURL(kWebAppUrl));
 
   controller()->PinAppWithID(app_id);
   const ash::ShelfItem* item = controller()->GetItem(ash::ShelfID(app_id));
