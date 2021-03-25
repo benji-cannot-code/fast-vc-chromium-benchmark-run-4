255d342b61e4411343a81553b323bd40a6b3701b
Nick Diego Yamane <nickdiego@igalia.com>
2021-03-25 19:15:49 +0000

    clipboard: ozone: Remove DataMap param from RequestClipboardData function
    DataMap pointer parameter in PlatformClipboard::RequestClipboardData
    function is unneeded and makes it difficult to properly implement nested
    requests support (ie: async re-entrancy). The resulting data is already
    returned asynchronously through the read callback parameter, so drop it
    and update implementations and call sites accordingly.
    
    R=dcheng@chromium.org, msisov@igalia.com
    
    Bug: 1165466
    Test: ozone_unittests --gtest_filter='*/WaylandClipboardTest.*'
    Change-Id: I07e15bb679ca0878957347a4b319fd7bff5f1227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780076
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#866699}

--- ui/base/clipboard/clipboard_ozone.cc
+++ ui/base/clipboard/clipboard_ozone.cc
@@ -62,7 +62,6 @@ class StubPlatformClipboard : public PlatformClipboard {
   void RequestClipboardData(
       ClipboardBuffer buffer,
       const std::string& mime_type,
-      PlatformClipboard::DataMap* data_map,
       PlatformClipboard::RequestDataClosure callback) override {
     std::move(callback).Run({});
   }
@@ -211,10 +210,6 @@ class ClipboardOzone::AsyncClipboardOzone {
         std::move(quit_closure_).Run();
     }
 
-    void FinishWithOptional(const base::Optional<Result>& result) {
-      Finish(result.value_or(Result{}));
-    }
-
     base::WeakPtr<Request<Result>> GetWeakPtr() {
       return weak_factory_.GetWeakPtr();
     }
@@ -251,10 +246,9 @@ class ClipboardOzone::AsyncClipboardOzone {
                                const std::string& mime_type) {
     using ReadRequest = Request<PlatformClipboard::Data>;
     ReadRequest request;
-    PlatformClipboard::DataMap data_map;
     platform_clipboard_->RequestClipboardData(
-        buffer, mime_type, &data_map,
-        base::BindOnce(&ReadRequest::FinishWithOptional, request.GetWeakPtr()));
+        buffer, mime_type,
+        base::BindOnce(&ReadRequest::Finish, request.GetWeakPtr()));
     return request.TakeResultSync().release();
   }
 
