35f1a70a3c8664e5d926c8a53dee5b1a288b0bb9
Etienne Bergeron <etienneb@chromium.org>
2021-03-25 16:06:32 +0000

    Ensures ObserverListThreadSafe TLS code is unused
    R=fdoray@chromium.org
    
    Bug: 1192296
    Change-Id: I8029fdf4dc76435d599b524aacab5c5d48f18079
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785923
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866635}

--- base/observer_list_threadsafe.h
+++ base/observer_list_threadsafe.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_export.h"
 #include "base/bind.h"
 #include "base/check_op.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/lazy_instance.h"
 #include "base/location.h"
 #include "base/memory/ref_counted.h"
@@ -133,6 +134,13 @@ class ObserverListThreadSafe : public internal::ObserverListThreadSafeBase {
       const NotificationDataBase* current_notification =
           tls_current_notification_.Get().Get();
       if (current_notification && current_notification->observer_list == this) {
+        // TODO(http://crbug.com/1192296): This code is temporary added to
+        // ensure that TLS code is not used by any observer. Remove this code
+        // after the experiment.
+        if (!dump_already_reported_) {
+          debug::DumpWithoutCrashing();
+          dump_already_reported_ = true;
+        }
         const NotificationData* notification_data =
             static_cast<const NotificationData*>(current_notification);
         task_runner->PostTask(
@@ -284,6 +292,7 @@ class ObserverListThreadSafe : public internal::ObserverListThreadSafeBase {
 
   mutable Lock lock_;
 
+  bool dump_already_reported_ GUARDED_BY(lock_) = false;
   size_t observer_id_counter_ GUARDED_BY(lock_) = 0;
 
   struct ObserverTaskRunnerInfo {
