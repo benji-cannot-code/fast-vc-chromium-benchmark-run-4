29bf8c13d682337cf539ad302c15be0eacde51d8
Travis Skare <skare@chromium.org>
2021-03-25 01:57:28 +0000

    [LongScreenshots] Proceed to screenshot actions when accepting dialog.
    Note: We'll still need to populate the final bitmap later.
    
    Bug: 1153969
    Change-Id: Ib52d7f854c5a66dc3b70e9a0188d090bc002ba5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785722
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Commit-Queue: Travis Skare <skare@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866404}

--- chrome/browser/share/android/javatests/src/org/chromium/chrome/browser/share/long_screenshots/LongScreenshotsCoordinatorTest.java
+++ chrome/browser/share/android/javatests/src/org/chromium/chrome/browser/share/long_screenshots/LongScreenshotsCoordinatorTest.java
@@ -17,6 +17,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
 
@@ -86,6 +87,6 @@ public class LongScreenshotsCoordinatorTest {
     @Test
     public void testCaptureScreenshotInitialEntrySuccess() {
         mCoordinator.captureScreenshot();
-        verify(mMediator, times(1)).displayInitialScreenshot();
+        verify(mMediator, times(1)).capture(Mockito.isA(Runnable.class));
     }
 }
