ee938c05c64dcb2d7666601e04a0c49d6c76799e
Owen Min <zmin@chromium.org>
2021-03-25 19:04:30 +0000

    Add a parameter for real time extension request feature
    The new parameter allows us switch between DM server partial reporting
    and the messaging layer.
    
    Also bump the flag expire date and update its owner.
    
    Bug: 1192292
    Change-Id: I516b0a712f750f6b554bc221098c380d6fc4d538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785622
    Commit-Queue: Owen Min <zmin@chromium.org>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Reviewed-by: Parastoo Geranmayeh <parastoog@google.com>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866687}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1543,6 +1543,17 @@ const FeatureEntry::FeatureVariation kNtpRepeatableQueriesVariations[] = {
 };
 #endif  // !defined(OS_ANDROID)
 
+#if !defined(OS_ANDROID)
+const FeatureEntry::FeatureParam kEnterpriseRealtimeExtensionRequestParam[] = {
+    {"with_erp", "false"},
+    {"with_erp", "true"}};
+const FeatureEntry::FeatureVariation
+    kEnterpriseRealtimeExtensionRequestVariation[] = {
+        {"with encrypted reporting pipeline",
+         kEnterpriseRealtimeExtensionRequestParam,
+         base::size(kEnterpriseRealtimeExtensionRequestParam), nullptr}};
+#endif  // !defined(OS_ANDROID)
+
 #if defined(OS_ANDROID)
 const FeatureEntry::FeatureParam kTranslateForceTriggerOnEnglishHeuristic[] = {
     {language::kOverrideModelKey, language::kOverrideModelHeuristicValue},
@@ -5716,7 +5727,10 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnterpriseRealtimeExtensionRequestName,
      flag_descriptions::kEnterpriseRealtimeExtensionRequestDescription,
      kOsDesktop,
-     FEATURE_VALUE_TYPE(features::kEnterpriseRealtimeExtensionRequest)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(
+         features::kEnterpriseRealtimeExtensionRequest,
+         kEnterpriseRealtimeExtensionRequestVariation,
+         "EnterpriseRealtimeExtensionRequest")},
 #endif  // !defined(OS_ANDROID)
 
     {"enable-unsafe-webgpu", flag_descriptions::kUnsafeWebGPUName,
