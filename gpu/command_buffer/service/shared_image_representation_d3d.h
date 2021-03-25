0b1edc3f3e22746972d09ac41a95563d9aeb89d4
Bryan Bernhart <bryan.bernhart@intel.com>
2021-03-25 03:40:37 +0000

    Cache Dawn external image on AssociateMailbox
    Allows the D3D backing to use a Dawn texture without re-opening the
    shared handle to the D3D12 resource. Re-opening the shared handle per SI
    should be avoided since it requires a syscall and possibly a GPU memory
    operation.
    
    However this approach has a draw-back: it requires the same WGPUDevice
    will be used between ProduceDawn calls, which may not always be the case.
    
    Change-Id: Ic92e4668dcf91e04044d5ca56a79e510a980d518
    Bug: 1147184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2709243
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Bryan Bernhart <bryan.bernhart@intel.com>
    Cr-Commit-Position: refs/heads/master@{#866440}

--- gpu/command_buffer/service/shared_image_representation_d3d.h
+++ gpu/command_buffer/service/shared_image_representation_d3d.h
@@ -48,10 +48,12 @@ class SharedImageRepresentationGLTexturePassthroughD3D
 #if BUILDFLAG(USE_DAWN)
 class SharedImageRepresentationDawnD3D : public SharedImageRepresentationDawn {
  public:
-  SharedImageRepresentationDawnD3D(SharedImageManager* manager,
-                                   SharedImageBacking* backing,
-                                   MemoryTypeTracker* tracker,
-                                   WGPUDevice device);
+  SharedImageRepresentationDawnD3D(
+      SharedImageManager* manager,
+      SharedImageBacking* backing,
+      MemoryTypeTracker* tracker,
+      WGPUDevice device,
+      dawn_native::d3d12::ExternalImageDXGI* external_image);
 
   ~SharedImageRepresentationDawnD3D() override;
 
@@ -61,6 +63,7 @@ class SharedImageRepresentationDawnD3D : public SharedImageRepresentationDawn {
  private:
   WGPUDevice device_;
   WGPUTexture texture_ = nullptr;
+  dawn_native::d3d12::ExternalImageDXGI* external_image_ = nullptr;
 
   // TODO(cwallez@chromium.org): Load procs only once when the factory is
   // created and pass a pointer to them around?
