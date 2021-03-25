b9713700fa962d909a703d21387199a3401b744b
Benoit Lize <lizeb@chromium.org>
2021-03-25 19:21:30 +0000

    [PartitionAlloc] Only use large thread cache size in renderers.
    Renderers perform a lot of large-ish allocations in
    performance-sensitive contexts, such as "zone" allocation in V8 for
    instance. To improve performance, the cache limits were raised in
    https://chromium-review.googlesource.com/c/chromium/src/+/2781727, which
    improved performance, but regressed memory footprint
    significantly (crbug.com/1191887).
    
    This CL introduces per-process controls on thread cache limits: they
    stay low in most processes, except for renderers. According to
    pinpoint (https://pinpoint-dot-chromeperf.appspot.com/job/12851809d20000),
    this lowers private footprint significantly (3.6%) in a loading memory
    benchmark.
    
    Bug: 1169157, 1191887
    Change-Id: If3d2ac4bf46dedc78f4f4b6fea8e7cc1fbfe40cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782326
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866709}

--- base/allocator/partition_allocator/thread_cache.h
+++ base/allocator/partition_allocator/thread_cache.h
@@ -29,6 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PA_THREAD_CACHE_SUPPORTED
 #endif
 
+// Supported on Linux 64 bits only for now, partly to see the impact on
+// performance and memory usage on bots, as this increases memory footprint.
+#if defined(OS_LINUX) && defined(ARCH_CPU_64_BITS)
+#define PA_THREAD_CACHE_LARGE_ALLOCATIONS
+#endif
+
 namespace base {
 
 namespace internal {
@@ -118,6 +124,11 @@ constexpr ThreadCacheRegistry::ThreadCacheRegistry() = default;
 
 ALWAYS_INLINE static constexpr int ConstexprLog2(size_t n) {
   return n < 1 ? -1 : (n < 2 ? 0 : (1 + ConstexprLog2(n >> 1)));
+}
+
+ALWAYS_INLINE static constexpr uint16_t BucketIndexForSize(size_t size) {
+  return ((ConstexprLog2(size) - kMinBucketedOrder + 1)
+          << kNumBucketsPerOrderBits);
 }
 
 #if DCHECK_IS_ON()
@@ -224,24 +235,27 @@ class BASE_EXPORT ThreadCache {
     return buckets_[index].count;
   }
 
+  // Sets the maximum size of allocations that may be cached by the thread
+  // cache. This applies to all threads. However, the maximum size is bounded by
+  // |kLargeSizeThreshold|.
+  static void SetLargestCachedSize(size_t size);
+
   // TODO(lizeb): Once we have periodic purge, lower the ratio.
   static constexpr uint16_t kBatchFillRatio = 8;
   static constexpr float kDefaultMultiplier = 2.;
   static constexpr uint8_t kSmallBucketBaseCount = 64;
 
-#if defined(OS_LINUX) && defined(ARCH_CPU_64_BITS)
-  // TODO((lizeb): Tune this better. Right now this is set to a high value on
-  // Linux x86_64, partly to see the impact on performance and memory usage on
-  // bots.
-  //
+  // When trying to conserve memory, set the thread cache limit to this.
+  static constexpr size_t kDefaultSizeThreshold = 512;
+#if defined(PA_THREAD_CACHE_LARGE_ALLOCATIONS)
   // 32kiB is chosen here as from local experiments, "zone" allocation in
   // V8 is performance-sensitive, and zones can (and do) grow up to 32kiB for
   // each individual allocation.
-  static constexpr size_t kSizeThreshold = 1 << 15;
+  static constexpr size_t kLargeSizeThreshold = 1 << 15;
 #else
-  // TODO(lizeb): Optimize the threshold.
-  static constexpr size_t kSizeThreshold = 512;
-#endif
+  static constexpr size_t kLargeSizeThreshold = kDefaultSizeThreshold;
+#endif  // defined(PA_THREAD_CACHE_LARGE_ALLOCATIONS)
+  static_assert(kDefaultSizeThreshold <= kLargeSizeThreshold, "");
 
  private:
   struct Bucket {
@@ -269,9 +283,7 @@ class BASE_EXPORT ThreadCache {
                               float multiplier);
 
   static constexpr uint16_t kBucketCount =
-      ((ConstexprLog2(kSizeThreshold) - kMinBucketedOrder + 1)
-       << kNumBucketsPerOrderBits) +
-      1;
+      BucketIndexForSize(kLargeSizeThreshold) + 1;
   static_assert(
       kBucketCount < kNumBuckets,
       "Cannot have more cached buckets than what the allocator supports");
@@ -290,6 +302,12 @@ class BASE_EXPORT ThreadCache {
   static constexpr uintptr_t kTombstoneMask = ~kTombstone;
 
   static uint8_t global_limits_[kBucketCount];
+  // Index of the largest active bucket. Not all processes/platforms will use
+  // all buckets, as using larger buckets increases the memory footprint.
+  //
+  // TODO(lizeb): Investigate making this per-thread rather than static, to
+  // improve locality, and open the door to per-thread settings.
+  static uint16_t largest_active_bucket_index_;
 
   Bucket buckets_[kBucketCount];
   std::atomic<bool> should_purge_;
@@ -313,9 +331,12 @@ class BASE_EXPORT ThreadCache {
   FRIEND_TEST_ALL_PREFIXES(ThreadCacheTest, RecordStats);
   FRIEND_TEST_ALL_PREFIXES(ThreadCacheTest, ThreadCacheRegistry);
   FRIEND_TEST_ALL_PREFIXES(ThreadCacheTest, MultipleThreadCachesAccounting);
-  FRIEND_TEST_ALL_PREFIXES(ThreadCacheTest, DynamicThreshold);
-  FRIEND_TEST_ALL_PREFIXES(ThreadCacheTest, DynamicThresholdClamping);
-  FRIEND_TEST_ALL_PREFIXES(ThreadCacheTest, DynamicThresholdMultipleThreads);
+  FRIEND_TEST_ALL_PREFIXES(ThreadCacheTest, DynamicCountPerBucket);
+  FRIEND_TEST_ALL_PREFIXES(ThreadCacheTest, DynamicCountPerBucketClamping);
+  FRIEND_TEST_ALL_PREFIXES(ThreadCacheTest,
+                           DynamicCountPerBucketMultipleThreads);
+  FRIEND_TEST_ALL_PREFIXES(ThreadCacheTest, DynamicSizeThreshold);
+  FRIEND_TEST_ALL_PREFIXES(ThreadCacheTest, DynamicSizeThresholdPurge);
 };
 
 ALWAYS_INLINE bool ThreadCache::MaybePutInCache(void* slot_start,
@@ -323,7 +344,7 @@ ALWAYS_INLINE bool ThreadCache::MaybePutInCache(void* slot_start,
   PA_REENTRANCY_GUARD(is_in_thread_cache_);
   INCREMENT_COUNTER(stats_.cache_fill_count);
 
-  if (UNLIKELY(bucket_index >= kBucketCount)) {
+  if (UNLIKELY(bucket_index > largest_active_bucket_index_)) {
     INCREMENT_COUNTER(stats_.cache_fill_misses);
     return false;
   }
@@ -360,7 +381,7 @@ ALWAYS_INLINE void* ThreadCache::GetFromCache(size_t bucket_index,
   PA_REENTRANCY_GUARD(is_in_thread_cache_);
   INCREMENT_COUNTER(stats_.alloc_count);
   // Only handle "small" allocations.
-  if (UNLIKELY(bucket_index >= kBucketCount)) {
+  if (UNLIKELY(bucket_index > largest_active_bucket_index_)) {
     INCREMENT_COUNTER(stats_.alloc_miss_too_large);
     INCREMENT_COUNTER(stats_.alloc_misses);
     return nullptr;
