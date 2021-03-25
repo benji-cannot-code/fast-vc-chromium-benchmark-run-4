3d9e6d99d50f396cc8f6f524947ad758d9ed756e
Friedrich Horschig <fhorschig@chromium.org>
2021-03-25 11:31:59 +0000

    [Android][TTF] Batch and reenable tests
    This CL enables a formerly flaky test (it was deflaked together with all
    others 3 weeks after it was disabled but we forgot to remove the
    annotation).
    Mainly, it allows batching for all ViewTests.
    
    Bug: 1012221
    Change-Id: I5ab6501ea6ed3acdc8870d02fc11e01a49bd34d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784887
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866542}

--- chrome/browser/touch_to_fill/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillIntegrationTest.java
+++ chrome/browser/touch_to_fill/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillIntegrationTest.java
@@ -33,9 +33,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.ScalableTimeout;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.touch_to_fill.data.Credential;
@@ -55,6 +55,7 @@ import java.util.Collections;
  * end up rendering a View.
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
+@Batch(Batch.PER_CLASS)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 public class TouchToFillIntegrationTest {
     private static final String EXAMPLE_URL = "https://www.example.xyz";
@@ -91,7 +92,6 @@ public class TouchToFillIntegrationTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/1012221")
     public void testClickingSuggestionsTriggersCallback() {
         runOnUiThreadBlocking(() -> {
             mTouchToFill.showCredentials(EXAMPLE_URL, true, Collections.singletonList(ANA));
