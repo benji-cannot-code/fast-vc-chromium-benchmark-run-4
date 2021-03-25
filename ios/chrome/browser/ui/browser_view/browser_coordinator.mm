30860c4ba4bfb5a569441de32693d2802acfb71d
Gauthier Ambard <gambard@chromium.org>
2021-03-25 10:55:08 +0000

    [iOS] Exit fullscreen when sharing the page
    In some cases (for example when the Share Link is failing) the page can
    be shared while being in fullscreen.
    On iPad this leads to a broken UI where the share menu is pointing to
    where the Share button should be if the toolbar is present.
    Exit fullscreen in all cases to avoid this. Exiting on iPhone is also
    good as it is displaying the omnibox, making sure the user knows which
    page they are sharing.
    
    Bug: 1147507
    Change-Id: Ia331dd304830d0340e6539fa6c2fadf85f272e70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2739722
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866536}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/download/pass_kit_coordinator.h"
 #import "ios/chrome/browser/ui/find_bar/find_bar_controller_ios.h"
 #import "ios/chrome/browser/ui/find_bar/find_bar_coordinator.h"
+#import "ios/chrome/browser/ui/fullscreen/fullscreen_controller.h"
 #import "ios/chrome/browser/ui/incognito_reauth/incognito_reauth_mediator.h"
 #import "ios/chrome/browser/ui/incognito_reauth/incognito_reauth_scene_agent.h"
 #import "ios/chrome/browser/ui/infobars/infobar_feature.h"
@@ -526,6 +527,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)sharePage {
   ActivityParams* params = [[ActivityParams alloc]
       initWithScenario:ActivityScenario::TabShareButton];
+
+  // Exit fullscreen if needed to make sure that share button is visible.
+  FullscreenController::FromBrowser(self.browser)->ExitFullscreen();
 
   self.sharingCoordinator = [[SharingCoordinator alloc]
       initWithBaseViewController:self.viewController
