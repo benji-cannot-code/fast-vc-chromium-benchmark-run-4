22dc25dece0efa77adef1457125c256bff10615e
Yeunjoo Choi <ychoi@igalia.com>
2021-03-25 11:51:31 +0000

    Change namespace chromeos => ash for authpolicy_helper_unittest.cc
    Code in authpolicy_helper_unittest.cc is a part of the ash-chrome, so
    namespace is changed to ash from chromeos.
    
    Also, typo in the filename authpolicy_helper.unittest.cc. is fixed to
    authpolicy_helper_unittest.cc.
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: Ib653c827eb6ce1360c414c641c4c01c4a32f069a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784341
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#866548}

--- chrome/browser/ash/authpolicy/authpolicy_helper_unittest.cc
+++ chrome/browser/ash/authpolicy/authpolicy_helper_unittest.cc
@@ -11,12 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/tpm/stub_install_attributes.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace chromeos {
+namespace ash {
 namespace {
 
 constexpr char kDMToken[] = "dm_token";
 
-class MockAuthPolicyClient : public FakeAuthPolicyClient {
+class MockAuthPolicyClient : public chromeos::FakeAuthPolicyClient {
  public:
   MockAuthPolicyClient() { SetStarted(true); }
   ~MockAuthPolicyClient() override = default;
@@ -57,10 +57,10 @@ class MockAuthPolicyClient : public FakeAuthPolicyClient {
 
 // Check that helper calls RefreshDevicePolicy after JoinAdDomain.
 TEST(AuthPolicyHelper, JoinFollowedByRefreshDevicePolicy) {
-  ScopedStubInstallAttributes scoped_stub_install_attributes;
+  chromeos::ScopedStubInstallAttributes scoped_stub_install_attributes;
 
   auto* mock_client = new MockAuthPolicyClient;
-  CryptohomeClient::InitializeFake();
+  chromeos::CryptohomeClient::InitializeFake();
 
   AuthPolicyHelper helper;
   helper.set_dm_token(kDMToken);
@@ -74,8 +74,8 @@ TEST(AuthPolicyHelper, JoinFollowedByRefreshDevicePolicy) {
                       }));
   mock_client->CheckExpectations();
 
-  CryptohomeClient::Shutdown();
-  AuthPolicyClient::Shutdown();
+  chromeos::CryptohomeClient::Shutdown();
+  chromeos::AuthPolicyClient::Shutdown();
 }
 
-}  // namespace chromeos
+}  // namespace ash
