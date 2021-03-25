ff052e8f964781d9817fd4fbcdc0ac2b2ed2c8d5
Takashi Sakamoto <tasak@google.com>
2021-03-25 15:02:29 +0000

    Put PartitionRefCount in the previous slot or metadata.
    When putting PartitionRefCount in the previous slot, the ref count and the slot may be in different system pages. This will happen when the slot start address is SystemPage-size aligned. So when slot_start is SystemPage-size aligned, put PartitionRefCount in SuperPage metadata. Otherwise, put PartitionRefCount in the previous slot. This looks like a combination of ref-count  bitmap idea and in-slot ref-count idea. Since we need only kSuperPageSize / SystemPageSize * sizeof(PartitionRefCount), the refcount bitmap will be smaller than or equal to 1 SystemPage.
    
    Change-Id: I4cb08011330833165bdf85da5e043ac9573815ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2783963
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#866608}

--- base/allocator/partition_allocator/partition_freelist_entry.h
+++ base/allocator/partition_allocator/partition_freelist_entry.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/allocator/partition_allocator/partition_alloc_constants.h"
+#include "base/allocator/partition_allocator/partition_ref_count.h"
 #include "base/compiler_specific.h"
 #include "base/dcheck_is_on.h"
 #include "base/immediate_crash.h"
@@ -19,7 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Disabled on ARM64 Macs, as this crashes very early (crbug.com/1172236).
 // TODO(lizeb): Enable in as many configurations as possible.
-#if !(defined(OS_MAC) && defined(ARCH_CPU_ARM64))
+// Disabled when putting refcount in the previous slot.
+#if !(defined(OS_MAC) && defined(ARCH_CPU_ARM64)) && \
+    !BUILDFLAG(REF_COUNT_AT_END_OF_ALLOCATION)
 #define PA_HAS_FREELIST_HARDENING
 #endif
 
@@ -39,8 +42,24 @@ namespace {
 struct EncodedPartitionFreelistEntry;
 
 #if defined(PA_HAS_FREELIST_HARDENING)
-static_assert((1 << kMinBucketedOrder) >= 2 * sizeof(void*),
+static_assert(kSmallestBucket >= 2 * sizeof(void*),
               "Need enough space for two pointers in freelist entries");
+#endif
+
+#if BUILDFLAG(REF_COUNT_AT_END_OF_ALLOCATION)
+constexpr size_t kMinimalBucketSizeWithRefCount =
+    (1 + sizeof(PartitionRefCount) + kSmallestBucket - 1) &
+    ~(kSmallestBucket - 1);
+#if defined(PA_HAS_FREELIST_HARDENING)
+static_assert(
+    kMinimalBucketSizeWithRefCount >=
+        sizeof(PartitionRefCount) + 2 * sizeof(void*),
+    "Need enough space for two pointer and one refcount in freelist entries");
+#else
+static_assert(
+    kMinimalBucketSizeWithRefCount >= sizeof(PartitionRefCount) + sizeof(void*),
+    "Need enough space for one pointer and one refcount in freelist entries");
+#endif
 #endif
 
 // Freelist entries are encoded for security reasons. See
