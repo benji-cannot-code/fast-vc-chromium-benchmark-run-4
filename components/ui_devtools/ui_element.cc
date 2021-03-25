04f82b697396860e9e012841fc98769145216380
Tim Sergeant <tsergeant@chromium.org>
2021-03-25 07:28:00 +0000

    Revert "UI Devtools: add dispatchMouseEvent API"
    This reverts commit 3bc0d647e82c033a2cd23293a20512bee20f417a.
    
    Reason for revert: Reverting CL which FindIt identified as a culprit here: https://crrev.com/c/2786345
    
    Original change's description:
    > UI Devtools: add dispatchMouseEvent API
    >
    > This CL is part of the effort to support interact with native UI
    > for Desktop UI benchmark
    >
    > Doc: https://docs.google.com/document/d/1ui3QM3tgtDL4FvpEto-vQc-LLduQH40W-eBqbttBxmI
    > Related CL: https://chromium-review.googlesource.com/c/chromium/src/+/2646447
    >
    > Change-Id: I25e2410a646c65624222012476af8b81b7c39f7a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2778872
    > Reviewed-by: Wei Li <weili@chromium.org>
    > Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#866412}
    
    Change-Id: Ib8760aedc0eef86a39bdca5d413fbe2bfe244f7a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784354
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Owners-Override: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866478}

--- components/ui_devtools/ui_element.cc
+++ components/ui_devtools/ui_element.cc
@@ -151,8 +151,4 @@ std::vector<UIElement::Source> UIElement::GetSources() {
   return sources_;
 }
 
-bool UIElement::DispatchMouseEvent(protocol::DOM::MouseEvent* event) {
-  return false;
-}
-
 }  // namespace ui_devtools
