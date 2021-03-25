a7c89202703032e1bd23de2db57f8d2b7c83c430
Jinsuk Kim <jinsukkim@chromium.org>
2021-03-25 13:48:07 +0000

    PageInfo: Helper class ChromePageInfo
    ChromePageInfo internally adopts a few default objects like
    OfflinePageUtils.TabOfflinePageLoadUrlDelegate to show page info popup.
    It was devised to remove Omnibox's dependency on c.b.page_info, such as:
    - ChromePageInfoControllerDelegate
    - ChromePermissionParamsListBuilderDelegate
    
    It also replaces PageInfoController.show() in many places in Chrome,
    helps make the callsites much less verbose.
    
    Bug: 1171512
    Change-Id: I20330a2a0ad9deef883981e98db716d01347a427
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784203
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866584}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
@@ -28,6 +28,7 @@ import org.chromium.chrome.browser.lifecycle.Destroyable;
 import org.chromium.chrome.browser.lifecycle.NativeInitObserver;
 import org.chromium.chrome.browser.locale.LocaleManager;
 import org.chromium.chrome.browser.omnibox.status.StatusCoordinator;
+import org.chromium.chrome.browser.omnibox.status.StatusCoordinator.PageInfoAction;
 import org.chromium.chrome.browser.omnibox.status.StatusView;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteCoordinator;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteDelegate;
@@ -112,6 +113,7 @@ public final class LocationBarCoordinator implements LocationBar, NativeInitObse
      * @param backKeyBehavior Delegate that allows customization of back key behavior.
      * @param searchEngineLogoUtils Utils to query the state of the search engine logos feature.
      * @param launchAssistanceSettingsAction Runnable launching settings for voice assistance.
+     * @param pageInfoAction Displays page info popup.
      */
     public LocationBarCoordinator(View locationBarLayout, View autocompleteAnchorView,
             ObservableSupplier<Profile> profileObservableSupplier,
@@ -124,7 +126,8 @@ public final class LocationBarCoordinator implements LocationBar, NativeInitObse
             ActivityLifecycleDispatcher activityLifecycleDispatcher,
             OverrideUrlLoadingDelegate overrideUrlLoadingDelegate,
             BackKeyBehaviorDelegate backKeyBehavior, SearchEngineLogoUtils searchEngineLogoUtils,
-            @NonNull Runnable launchAssistanceSettingsAction) {
+            @NonNull Runnable launchAssistanceSettingsAction,
+            @NonNull PageInfoAction pageInfoAction) {
         mLocationBarLayout = (LocationBarLayout) locationBarLayout;
         mWindowDelegate = windowDelegate;
         mWindowAndroid = windowAndroid;
@@ -152,7 +155,7 @@ public final class LocationBarCoordinator implements LocationBar, NativeInitObse
         mStatusCoordinator = new StatusCoordinator(isTablet(), statusView, mUrlCoordinator,
                 incognitoStateProvider, modalDialogManagerSupplier, locationBarDataProvider,
                 mTemplateUrlServiceSupplier, searchEngineLogoUtils, profileObservableSupplier,
-                windowAndroid);
+                windowAndroid, pageInfoAction);
         mLocationBarMediator.setCoordinators(
                 mUrlCoordinator, mAutocompleteCoordinator, mStatusCoordinator);
 
