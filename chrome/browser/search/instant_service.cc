a216e851dc0b735c63ea641ece7ffa3b8d603d70
Esmael El-Moslimany <aee@chromium.org>
2021-03-25 03:39:09 +0000

    WebUI: register theme source in InstantService (temp fix)
    Bug: 1192394
    Change-Id: I6e788492aa782f200b5530436bf0bf9ac1906745
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2786079
    Auto-Submit: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866438}

--- chrome/browser/search/instant_service.cc
+++ chrome/browser/search/instant_service.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/browser/ui/omnibox/omnibox_theme.h"
 #include "chrome/browser/ui/webui/favicon_source.h"
+#include "chrome/browser/ui/webui/theme_source.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/search/search.mojom.h"
@@ -227,6 +228,13 @@ InstantService::InstantService(Profile* profile)
 
   // Listen for theme installation.
   ThemeServiceFactory::GetForProfile(profile_)->AddObserver(this);
+
+  // TODO(crbug.com/1192394): multiple WebUI pages depend on the theme source
+  // without adding it themselves. This is not causing an issue because the
+  // theme source is being added here. The source should be added where it is
+  // used and then the following can be removed.
+  content::URLDataSource::Add(profile_,
+                              std::make_unique<ThemeSource>(profile_));
 
   // Set up the data sources that Instant uses on the NTP.
   content::URLDataSource::Add(
