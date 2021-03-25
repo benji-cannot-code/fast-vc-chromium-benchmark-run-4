cb6d4fa2b3ae1504afa05aeb552516bda08ba518
Sigurd Schneider <sigurds@chromium.org>
2021-03-25 09:05:41 +0000

    Update layout test to sniff new UISouceCode.addMessage API
    Bug: chromium:1189086
    Change-Id: Ia2cc8bb094aedb8e8aca704d799af7153322e807
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784827
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866515}

--- third_party/blink/web_tests/http/tests/devtools/console/console-link-to-snippet.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-link-to-snippet.js
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.loadModule('console'); await TestRunner.loadTestModule('console_test_runner');
   await TestRunner.showPanel('console');
 
-  TestRunner.addSniffer(Workspace.UISourceCode.prototype, 'addLineMessage', dumpLineMessage, true);
+  TestRunner.addSniffer(
+      Workspace.UISourceCode.prototype, 'addMessage', dumpLineMessage, true);
 
   TestRunner.runTestSuite([
     function testConsoleLogAndReturnMessageLocation(next) {
@@ -73,8 +74,10 @@ console.error(null)`)
     return Common.Revealer.reveal(uiSourceCode).then(() => uiSourceCode);
   }
 
-  function dumpLineMessage(level, text, lineNumber, columnNumber) {
-    TestRunner.addResult(`Line Message was added: ${this.url()} ${level} '${text}':${lineNumber}:${columnNumber}`);
+  function dumpLineMessage(message) {
+    TestRunner.addResult(`Line Message was added: ${this.url()} ${
+        message.level()} '${message.text()}':${message.lineNumber()}:${
+        message.columnNumber()}`);
   }
 
   function runSelectedSnippet() {
