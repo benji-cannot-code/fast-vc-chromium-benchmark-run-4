7046cd8d9739b42ebc5d0b3a65e4cf3afe7e963a
Elizabeth Popova <lizapopova@google.com>
2021-03-25 11:11:26 +0000

    [Autofill][Android] Add nickname field to settings/CardEditor
    Added an optional card nickname field similar to the one currently
    displayed in Settings [1]. The behavior is replicated except the
    validation: it is performed on focus loss and not on every input change
    to align with general validation pattern. It also required adding new
    functionality to prefeditor lib: showing length counter ("cur/max")
    below the focused field.
    
    This change is a prerequisite for switching to settings/CardEditor UI in
    Autofill Settings and currently affects only Autofill Assistant usage.
    
    [1]https://source.chromium.org/chromium/chromium/src/+/master:chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillLocalCardEditor.java;l=73
    
    Bug: 1176929
    Change-Id: I3119985f3941dec02d7b7a3696c8b0344182f6c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2778392
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Cr-Commit-Position: refs/heads/master@{#866538}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/prefeditor/EditorFieldModel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/prefeditor/EditorFieldModel.java
@@ -63,7 +63,7 @@ public class EditorFieldModel {
     private static final int INPUT_TYPE_HINT_MIN_INCLUSIVE = 0;
 
     /** Text input with no special formatting rules, e.g., a city, a suburb, or a company name. */
-    private static final int INPUT_TYPE_HINT_NONE = 0;
+    public static final int INPUT_TYPE_HINT_NONE = 0;
 
     /** Indicates a phone field. */
     public static final int INPUT_TYPE_HINT_PHONE = 1;
@@ -114,6 +114,9 @@ public class EditorFieldModel {
 
     private static final int INPUT_TYPE_HINT_MAX_EXCLUSIVE = 13;
 
+    /* Indicates that the length counter is disabled. */
+    public static final int LENGTH_COUNTER_LIMIT_NONE = 0;
+
     private final int mInputTypeHint;
 
     @Nullable
@@ -163,6 +166,7 @@ public class EditorFieldModel {
     private int mActionIconDescriptionForAccessibility;
     private boolean mIsFullLine = true;
     private boolean mPlusIconIsDisplayed;
+    private int mLengthCounterLimit = LENGTH_COUNTER_LIMIT_NONE;
 
     /**
      * Constructs a label to show in the editor. This can be, for example, description of a server
@@ -287,7 +291,7 @@ public class EditorFieldModel {
     /**
      * Constructs a text input field model.
      *
-     * @param inputTypeHint        The type of input. For example, INPUT_TYPE_HINT_PHONE.
+     * @param inputTypeHint        The type of input. For example, {@link INPUT_TYPE_HINT_PHONE}.
      * @param label                The human-readable label for user to understand the type of data
      *                             that should be entered into this field.
      * @param suggestions          Optional set of values to suggest to the user.
@@ -298,6 +302,9 @@ public class EditorFieldModel {
      *                             cannot leave this field empty.
      * @param invalidErrorMessage  The optional error message that indicates to the user that the
      *                             value they have entered is not valid.
+     * @param lengthCounterLimit   The maximum number of characters to be allowed by the length
+     *                             counter ("cur/max") shown below the focused field. Use
+     *                             {@link LENGTH_COUNTER_LIMIT_NONE} to disable the counter.
      * @param value                Optional initial value of this field.
      */
     public static EditorFieldModel createTextInput(int inputTypeHint, CharSequence label,
@@ -305,7 +312,7 @@ public class EditorFieldModel {
             @Nullable EditorFieldValidator validator,
             @Nullable EditorValueIconGenerator valueIconGenerator,
             @Nullable CharSequence requiredErrorMessage, @Nullable CharSequence invalidErrorMessage,
-            @Nullable CharSequence value) {
+            int lengthCounterLimit, @Nullable CharSequence value) {
         assert label != null;
         EditorFieldModel result = new EditorFieldModel(inputTypeHint);
         assert result.isTextField();
@@ -317,6 +324,7 @@ public class EditorFieldModel {
         result.mRequiredErrorMessage = requiredErrorMessage;
         result.mLabel = label;
         result.mValue = value;
+        result.mLengthCounterLimit = Math.max(lengthCounterLimit, LENGTH_COUNTER_LIMIT_NONE);
         return result;
     }
 
@@ -665,5 +673,32 @@ public class EditorFieldModel {
      */
     public void setIsFullLine(boolean isFullLine) {
         mIsFullLine = isFullLine;
+    }
+
+    /**
+     * @return The maximum number of characters allowed by the length counter ("cur/max") shown
+     * below the focused field. If the value equals {@link LENGTH_COUNTER_LIMIT_NONE}, the counter
+     * is not shown, which is the default behavior.
+     */
+    public int getLengthCounterLimit() {
+        return mLengthCounterLimit;
+    }
+
+    /**
+     * Sets the maximum number of characters to be allowed by the length counter ("cur/max") shown
+     * below the focused field. If the value equals {@link LENGTH_COUNTER_LIMIT_NONE} or any
+     * nonpositive value, the counter won't be shown, which is the default behavior.
+     *
+     * @param lengthCounterLimit The maximum number of characters for the length counter.
+     */
+    public void setLengthCounterLimit(int lengthCounterLimit) {
+        mLengthCounterLimit = Math.max(lengthCounterLimit, LENGTH_COUNTER_LIMIT_NONE);
+    }
+
+    /**
+     * @return whether the length counter should be shown below the focused field.
+     */
+    public boolean hasLengthCounter() {
+        return mLengthCounterLimit != EditorFieldModel.LENGTH_COUNTER_LIMIT_NONE;
     }
 }
--- chrome/android/java/src/org/chromium/chrome/browser/payments/AddressEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/AddressEditor.java
@@ -282,7 +282,7 @@ public class AddressEditor
                     mPhoneNumbers, mPhoneFormatter, mPhoneValidator, null /* valueIconGenerator */,
                     requiredErrorMessage,
                     mContext.getString(R.string.payments_phone_invalid_validation_message),
-                    null /* value */);
+                    EditorFieldModel.LENGTH_COUNTER_LIMIT_NONE, null /* value */);
         }
 
         // Phone number field is cached, so its value needs to be updated for every new profile
@@ -298,7 +298,7 @@ public class AddressEditor
                         null /* suggestions */, null /* formatter */, null /* validator */,
                         null /* valueIconGenerator */, null /* requiredErrorMessage */,
                         mContext.getString(R.string.payments_email_invalid_validation_message),
-                        null /* value */);
+                        EditorFieldModel.LENGTH_COUNTER_LIMIT_NONE, null /* value */);
             }
             // Retrieve and set the email address field.
             mEmailField.setValue(mProfile.getEmailAddress());
