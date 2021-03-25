89a1d5d4c9fedf07d045497530097e54f3817ea3
dpapad <dpapad@chromium.org>
2021-03-25 20:57:58 +0000

    Add ts_library() GN rule to support TypeScript in WebUI.
    The new ts_library() rule does the following:
    
     - Generates tsconfig.json file in the corresponding gen/ subfolder.
     - Invokes tsc (TypeScript compiler) using the generated tsconfig.json
     - Generates a manifest file to be used with generate_grd()
     - Handles dependencies between different projects, where a "project"
       in this context is a folder with its own tsconfig.json file.
     - Supports custom URL mappings, such as chrome://resources/ URLs
     - Allows passing a root_dir parameter to support cases where
       preprocess_if_expr() to be run before ts_library().
    
    Bug: 1189595
    Change-Id: I613515d27e826eb76b07c68bde83844ad4d951fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2763769
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866776}

---
+++ tools/typescript/tests/project2/bar.ts
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {foo} from '../project1/foo.js';
+import {num} from 'chrome://some-other-source/foo.js';
+
+function doNothing(): void {
+  console.log(foo());
+  console.log(num());
+}
+
+doNothing();
