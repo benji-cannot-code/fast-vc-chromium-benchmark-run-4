cabc999999ea29b4c69f53d2229bd185e2c21f3a
Hassan Talat <hatalat@microsoft.com>
2021-03-25 19:28:21 +0000

    Reland "WebShare: Implement SharingServicePicker"
    This is a reland of 07700ae66eaf1099dd60ba52644ecc5fed1a9ea7
    
    Original change's description:
    > WebShare: Implement SharingServicePicker
    >
    > This CL completes the implementation of webshare on mac.
    > The UX review is being tracked here: crbug.com/1162971
    >
    > To test manually:
    > -launch chromium with flag 'WebShare'
    > -go to https://w3c.github.io/web-share/demos/share-files.html
    > -optionally, add a file to the form
    > -click on Share
    > -the NSSharingServicePicker should popup in the top-middle,
    > overlapping the client and non-client area
    >
    > Bug: 1144920
    > Change-Id: Ia28ad87fac82a9704c07122b57a144ce5aa5086e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2668974
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    > Reviewed-by: Leonard Grey <lgrey@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#859624}
    
    Bug: 1144920
    Change-Id: Ied9df60b65fc19c366f9c0e038028563df976a5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737234
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Hassan Talat <hatalat@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#866720}

---
+++ content/app_shim_remote_cocoa/sharing_service_picker.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_APP_SHIM_REMOTE_COCOA_SHARING_SERVICE_PICKER_H_
+#define CONTENT_APP_SHIM_REMOTE_COCOA_SHARING_SERVICE_PICKER_H_
+
+#include <string>
+
+#include <AppKit/AppKit.h>
+
+#include "content/common/render_widget_host_ns_view.mojom.h"
+
+namespace remote_cocoa {
+
+void ShowSharingServicePickerForView(
+    NSView* owning_view,
+    const std::string& title,
+    const std::string& text,
+    const std::string& url,
+    const std::vector<std::string>& file_paths,
+    mojom::RenderWidgetHostNSView::ShowSharingServicePickerCallback callback);
+
+}  // namespace remote_cocoa
+
+#endif  // CONTENT_APP_SHIM_REMOTE_COCOA_SHARING_SERVICE_PICKER_H_
