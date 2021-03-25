65f3a25ce637560ff337ea05e845a7b88aedfcaa
Andres Calderon Jaramillo <andrescj@chromium.org>
2021-03-25 22:31:12 +0000

    Add API to process the next N pending tasks.
    This CL adds the TestMockTimeTaskRunner::ProcessNextNTasks() method to
    allow client classes to finely control when to run tasks, possibly one
    by one.
    
    The motivating use case is to allow fuzzers to use multiple task runners
    and control the order in which tasks are run in order to find bugs that
    are caused by scheduling order.
    
    Bug: b:183643168
    Test: Added unit tests for the new API
    Change-Id: I341e48bd694cca6cc84e7c1048c98e3be41c2cc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2787211
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866811}

--- base/test/test_mock_time_task_runner.cc
+++ base/test/test_mock_time_task_runner.cc
@@ -189,7 +189,7 @@ void TestMockTimeTaskRunner::FastForwardBy(TimeDelta delta) {
   DCHECK_GE(delta, TimeDelta());
 
   const TimeTicks original_now_ticks = NowTicks();
-  ProcessAllTasksNoLaterThan(delta);
+  ProcessTasksNoLaterThan(delta);
   ForwardClocksUntilTickTime(original_now_ticks + delta);
 }
 
@@ -204,12 +204,17 @@ void TestMockTimeTaskRunner::AdvanceWallClock(TimeDelta delta) {
 
 void TestMockTimeTaskRunner::RunUntilIdle() {
   DCHECK(thread_checker_.CalledOnValidThread());
-  ProcessAllTasksNoLaterThan(TimeDelta());
+  ProcessTasksNoLaterThan(TimeDelta());
+}
+
+void TestMockTimeTaskRunner::ProcessNextNTasks(int n) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  ProcessTasksNoLaterThan(TimeDelta::Max(), n);
 }
 
 void TestMockTimeTaskRunner::FastForwardUntilNoTasksRemain() {
   DCHECK(thread_checker_.CalledOnValidThread());
-  ProcessAllTasksNoLaterThan(TimeDelta::Max());
+  ProcessTasksNoLaterThan(TimeDelta::Max());
 }
 
 void TestMockTimeTaskRunner::ClearPendingTasks() {
@@ -339,7 +344,8 @@ void TestMockTimeTaskRunner::OnAfterTaskRun() {
   // Empty default implementation.
 }
 
-void TestMockTimeTaskRunner::ProcessAllTasksNoLaterThan(TimeDelta max_delta) {
+void TestMockTimeTaskRunner::ProcessTasksNoLaterThan(TimeDelta max_delta,
+                                                     int limit) {
   DCHECK(thread_checker_.CalledOnValidThread());
   DCHECK_GE(max_delta, TimeDelta());
 
@@ -352,7 +358,7 @@ void TestMockTimeTaskRunner::ProcessAllTasksNoLaterThan(TimeDelta max_delta) {
   }
 
   const TimeTicks original_now_ticks = NowTicks();
-  while (!quit_run_loop_) {
+  for (int i = 0; !quit_run_loop_ && (limit < 0 || i < limit); i++) {
     OnBeforeSelectingTask();
     TestPendingTask task_info;
     if (!DequeueNextTask(original_now_ticks, max_delta, &task_info))
