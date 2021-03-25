c4d6e6e069b11496d3b8d87d7d9ff0fc8cd17941
Sebastien Marchand <sebmarchand@chromium.org>
2021-03-25 19:37:24 +0000

    Revert "Don't freeze tabs that set the Cache-Control: no-store header"
    This reverts commit d6c81d251d2599bccf5b6df4606bf20d77efbdb6.
    
    Reason for revert: It's not needed, cache-controls headers shouldn't be used to guide freezing decisions.
    
    Original change's description:
    > Don't freeze tabs that set the Cache-Control: no-store header
    >
    > Tabs that have the "Cache-Control: no-store" header should not be frozen
    > because it could introduce a small privacy regression when returning to
    > the tab after it has been frozen in background for a long time (e.g. the
    > content of a banking tab could be visible for a few frames before the
    > site realizing that the user should have been logged out a while ago).
    >
    > This requires exposing the last HTTP response headers in
    > RenderFrameHost.
    >
    > Bug: 1144025
    > Change-Id: I98f717b489e4bfed70ad9aa88a727497c0a3c04a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737035
    > Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    > Reviewed-by: Brian White <bcwhite@chromium.org>
    > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Reviewed-by: François Doray <fdoray@chromium.org>
    > Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#861397}
    
    Bug: 1144025
    Change-Id: I1d050f8431ae0445bd745ed07ee7de5c84ea1c1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785143
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866736}

--- net/http/http_request_headers.cc
+++ net/http/http_request_headers.cc
@@ -54,8 +54,6 @@ const char HttpRequestHeaders::kReferer[] = "Referer";
 const char HttpRequestHeaders::kTransferEncoding[] = "Transfer-Encoding";
 const char HttpRequestHeaders::kUserAgent[] = "User-Agent";
 
-const char HttpRequestHeaders::kNoStoreDirective[] = "no-store";
-
 HttpRequestHeaders::HeaderKeyValuePair::HeaderKeyValuePair() = default;
 
 HttpRequestHeaders::HeaderKeyValuePair::HeaderKeyValuePair(
