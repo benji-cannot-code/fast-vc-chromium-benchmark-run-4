87d58db5467e08dec2f3c56820bc483dd05fab0e
rajendrant <rajendrant@chromium.org>
2021-03-25 04:09:22 +0000

    ImageCompression: Disable login robots based compression for low memory
    Site-isolation's preloaded list and password entered sites list are
    tracked only for >2GB devices. This CL adds a finch param to disable
    image compression for such low memory devices.
    
    Bug: 1190665
    Change-Id: I6eafbb9f2c9e0a218bb3444af95c9511ff1c378a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785298
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866447}

--- components/subresource_redirect/common/subresource_redirect_features.cc
+++ components/subresource_redirect/common/subresource_redirect_features.cc
@@ -6,11 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/subresource_redirect/common/subresource_redirect_features.h"
 
 #include "base/metrics/field_trial_params.h"
+#include "base/system/sys_info.h"
 #include "third_party/blink/public/common/features.h"
 
 namespace subresource_redirect {
 
 namespace {
+
+// This default low memory threshold is chosen as the default threshold for
+// Android in
+// SiteIsolationPolicy::ShouldDisableSiteIsolationDueToMemoryThreshold(). This
+// is the threshold for enabling the site-isolation preloaded list and
+// populating the password entered sites. So, the subresource redirect feature
+// should not compress the devices below this threshold.
+constexpr int kDefaultLowMemoryThresholdMb = 1900;
 
 bool IsSubresourceRedirectEnabled() {
   return base::FeatureList::IsEnabled(blink::features::kSubresourceRedirect);
@@ -37,6 +46,15 @@ bool ShouldEnableLoginRobotsCheckedCompression() {
   // Only one of the public image hints or login and robots based image
   // compression should be active.
   DCHECK(!is_enabled || !ShouldEnablePublicImageHintsBasedCompression());
+  if (is_enabled && !base::GetFieldTrialParamByFeatureAsBool(
+                        blink::features::kSubresourceRedirect,
+                        "enable_login_robots_for_low_memory", false)) {
+    return base::SysInfo::AmountOfPhysicalMemoryMB() >
+           base::GetFieldTrialParamByFeatureAsInt(
+               blink::features::kSubresourceRedirect,
+               "login_robots_low_memory_threshold_mb",
+               kDefaultLowMemoryThresholdMb);
+  }
   return is_enabled;
 }
 
