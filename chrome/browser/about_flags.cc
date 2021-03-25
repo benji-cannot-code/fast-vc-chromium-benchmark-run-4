f7872e6a95942356663fa671a487c10d2d3d2986
Rob Schonberger <robsc@chromium.org>
2021-03-25 05:26:59 +0000

    Add a flag on chrome://flags to enable SODA for live captions.
    This allows users to enable/disable soda at will via chrome://flags
    
    Bug: b:181827839
    Change-Id: Ib64478f1e59033f05c3062da9ff7238751bfb383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732741
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#866461}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -5074,6 +5074,10 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableAccessibilityLiveCaptionName,
      flag_descriptions::kEnableAccessibilityLiveCaptionDescription, kOsDesktop,
      FEATURE_VALUE_TYPE(media::kLiveCaption)},
+    {"enable-accessibility-live-caption-soda",
+     flag_descriptions::kEnableAccessibilityLiveCaptionSodaName,
+     flag_descriptions::kEnableAccessibilityLiveCaptionSodaDescription,
+     kOsDesktop, FEATURE_VALUE_TYPE(media::kUseSodaForLiveCaption)},
 #endif  // !defined(OS_ANDROID)
 
     {"enable-accessibility-object-model",
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1966,6 +1966,9 @@ extern const char kAllowAllSitesToInitiateMirroringDescription[];
 extern const char kEnableAccessibilityLiveCaptionName[];
 extern const char kEnableAccessibilityLiveCaptionDescription[];
 
+extern const char kEnableAccessibilityLiveCaptionSodaName[];
+extern const char kEnableAccessibilityLiveCaptionSodaDescription[];
+
 extern const char kCastMediaRouteProviderName[];
 extern const char kCastMediaRouteProviderDescription[];
 
