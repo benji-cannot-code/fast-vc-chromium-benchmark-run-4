78cf55d83b8fa4e1978e38570bf7158509f72a2b
Yao Xiao <yaoxia@chromium.org>
2021-03-25 03:11:00 +0000

    Add the content_annotations field to URLResult
    Expose the content_annotations in URLResult. Move
    VisitContentAnnotations from history_types.h to url_row.h to avoid
    circular dependency.
    
    Bug: 1177102
    Change-Id: I6920b0516404f2c1205408ac91c3a363fecf947e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782201
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866432}

--- components/optimization_guide/content/browser/page_content_annotations_model_manager.h
+++ components/optimization_guide/content/browser/page_content_annotations_model_manager.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_PAGE_CONTENT_ANNOTATIONS_MODEL_MANAGER_H_
 #define COMPONENTS_OPTIMIZATION_GUIDE_CONTENT_BROWSER_PAGE_CONTENT_ANNOTATIONS_MODEL_MANAGER_H_
 
-#include "components/history/core/browser/history_types.h"
+#include "components/history/core/browser/url_row.h"
 #include "components/optimization_guide/content/browser/bert_model_executor.h"
 #include "components/optimization_guide/proto/page_topics_model_metadata.pb.h"
 
--- components/optimization_guide/content/browser/page_content_annotations_service.h
+++ components/optimization_guide/content/browser/page_content_annotations_service.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/weak_ptr.h"
-#include "components/history/core/browser/history_types.h"
+#include "components/history/core/browser/url_row.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/optimization_guide/machine_learning_tflite_buildflags.h"
 #include "url/gurl.h"
