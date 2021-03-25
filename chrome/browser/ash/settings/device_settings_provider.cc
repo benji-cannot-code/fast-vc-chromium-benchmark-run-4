09baddb92d9c1d468364ef40dbc20c335d34769f
Timothy Loh <timloh@chromium.org>
2021-03-25 07:31:41 +0000

    Revert "Stop supporting Plugin VM device license key"
    This reverts commit 1cb9f0f336952e6261ce5a6c05a5d59271421744.
    
    Reason for revert: Breaking uprev 
    
    Original change's description:
    > Stop supporting Plugin VM device license key
    >
    > This has been superseded by ByteBot licensing, and has caused some
    > confusion by users who had previously added such a key, as the policy
    > is no longer available in the admin console (excl managedchrome).
    >
    > Bug: b/178895204
    > Change-Id: I8c1715a2d7db9a5618c14e8f9805b831ab9e3f21
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2766562
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Jason Lin <lxj@google.com>
    > Commit-Queue: Timothy Loh <timloh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#865901}
    
    Bug: b/178895204, 1192299
    Change-Id: Ie09228479700abf1ed668ca1ec58651f8571168e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784676
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Owners-Override: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866480}

--- chrome/browser/ash/settings/device_settings_provider.cc
+++ chrome/browser/ash/settings/device_settings_provider.cc
@@ -113,6 +113,7 @@ const char* const kKnownSettings[] = {
     kLoginAuthenticationBehavior,
     kLoginVideoCaptureAllowedUrls,
     kPluginVmAllowed,
+    kPluginVmLicenseKey,
     kPolicyMissingMitigationMode,
     kRebootOnShutdown,
     kReleaseChannel,
@@ -899,6 +900,15 @@ void DecodeGenericPolicies(const em::ChromeDeviceSettingsProto& policy,
     if (container.has_plugin_vm_allowed()) {
       new_values_cache->SetValue(kPluginVmAllowed,
                                  base::Value(container.plugin_vm_allowed()));
+    }
+  }
+
+  if (policy.has_plugin_vm_license_key()) {
+    const em::PluginVmLicenseKeyProto& container(
+        policy.plugin_vm_license_key());
+    if (container.has_plugin_vm_license_key()) {
+      new_values_cache->SetValue(
+          kPluginVmLicenseKey, base::Value(container.plugin_vm_license_key()));
     }
   }
 
