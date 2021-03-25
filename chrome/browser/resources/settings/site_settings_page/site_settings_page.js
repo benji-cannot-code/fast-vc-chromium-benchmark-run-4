fcd090d99b1660e63bd7b1d421eb642a8bc7829f
Sean Harrison <harrisonsean@chromium.org>
2021-03-25 18:47:48 +0000

    [WebUI][Settings] Add string switching
    Add string switching for newly redesigned pages on
    chrome://settings/content based on contentsettingsredesign flag.
    
    Bug: 1113642
    Change-Id: Idff89785e12be656ded0fb8ca4e75187e19018c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784889
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866657}

--- chrome/browser/resources/settings/site_settings_page/site_settings_page.js
+++ chrome/browser/resources/settings/site_settings_page/site_settings_page.js
@@ -94,8 +94,10 @@ function getCategoryItemMap() {
       id: Id.BLUETOOTH_DEVICES,
       label: 'siteSettingsBluetoothDevices',
       icon: 'settings:bluetooth',
-      enabledLabel: 'siteSettingsBluetoothDevicesAsk',
-      disabledLabel: 'siteSettingsBluetoothDevicesBlock',
+      enabledLabel: redesignEnabled ? 'siteSettingsBluetoothDevicesAllowed' :
+                                      'siteSettingsBluetoothDevicesAsk',
+      disabledLabel: redesignEnabled ? 'siteSettingsBluetoothDevicesBlocked' :
+                                       'siteSettingsBluetoothDevicesBlock',
       shouldShow: () =>
           loadTimeData.getBoolean('enableWebBluetoothNewPermissionsBackend'),
     },
@@ -161,8 +163,10 @@ function getCategoryItemMap() {
       id: Id.IDLE_DETECTION,
       label: 'siteSettingsIdleDetection',
       icon: 'settings:devices',
-      enabledLabel: 'siteSettingsIdleDetectionAsk',
-      disabledLabel: 'siteSettingsIdleDetectionBlock',
+      enabledLabel: redesignEnabled ? 'siteSettingsHidDevicesAllowed' :
+                                      'siteSettingsIdleDetectionAsk',
+      disabledLabel: redesignEnabled ? 'siteSettingsHidDevicesBlocked' :
+                                       'siteSettingsIdleDetectionBlock',
     },
     {
       route: routes.SITE_SETTINGS_IMAGES,
@@ -226,8 +230,10 @@ function getCategoryItemMap() {
       id: Id.FONT_ACCESS,
       label: 'fonts',
       icon: 'settings:font-access',
-      enabledLabel: 'siteSettingsFontAccessAsk',
-      disabledLabel: 'siteSettingsFontAccessBlock',
+      enabledLabel: redesignEnabled ? 'siteSettingsFontsAllowed' :
+                                      'siteSettingsFontAccessAsk',
+      disabledLabel: redesignEnabled ? 'siteSettingsFontsBlocked' :
+                                       'siteSettingsFontAccessBlock',
       shouldShow: () =>
           loadTimeData.getBoolean('enableFontAccessContentSetting'),
     },
@@ -256,6 +262,8 @@ function getCategoryItemMap() {
       id: 'pdfDocuments',
       label: 'siteSettingsPdfDocuments',
       icon: 'settings:pdf',
+      enabledLabel: redesignEnabled ? 'siteSettingsPdfsAllowed' : '',
+      disabledLabel: redesignEnabled ? 'siteSettingsPdfsBlocked' : '',
     },
     {
       route: routes.SITE_SETTINGS_POPUPS,
@@ -272,14 +280,20 @@ function getCategoryItemMap() {
       id: Id.PROTECTED_CONTENT,
       label: 'siteSettingsProtectedContent',
       icon: 'settings:protected-content',
+      enabledLabel: redesignEnabled ? 'siteSettingsProtectedContentAllowed' :
+                                      '',
+      disabledLabel: redesignEnabled ? 'siteSettingsProtectedContentBlocked' :
+                                       '',
     },
     {
       route: routes.SITE_SETTINGS_HANDLERS,
       id: Id.PROTOCOL_HANDLERS,
       label: 'siteSettingsHandlers',
       icon: 'settings:protocol-handler',
-      enabledLabel: 'siteSettingsHandlersAsk',
-      disabledLabel: 'siteSettingsHandlersBlocked',
+      enabledLabel: redesignEnabled ? 'siteSettingsProtocolHandlersAllowed' :
+                                      'siteSettingsHandlersAsk',
+      disabledLabel: redesignEnabled ? 'siteSettingsProtocolHandlersBlocked' :
+                                       'siteSettingsHandlersBlocked',
       shouldShow: () => !loadTimeData.getBoolean('isGuest'),
     },
     {
