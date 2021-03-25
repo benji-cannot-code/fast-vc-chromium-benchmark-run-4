01de94925febb3973d44110e8e982303e9625c0e
Rakina Zata Amni <rakina@chromium.org>
2021-03-25 07:58:44 +0000

    Add flag to enable freezing of frame-associated task queues when KeepActive is true
    Currently when the PageScheduler's KeepActive() is true, we won't freeze
    non-throttleable task queues, which might cause problems for bfcached
    pages, which expects its frame-associated task queues to be frozen.
    
    This CL adds the FreezeWhileKeepActive flag, which when enabled will
    allow non-throttleable frame-associated task queues to be frozen when a
    page is frozen (for bfcache and other cases), even when KeepActive is
    true. Also adds tests to ensure service worker (which sets KeepActive
    to true) still works when the flag is enabled, even if it's in the same
    process as a bfcached page.
    
    Bug: 1179590
    Change-Id: I200a3ccdcbc46916b77ceadf23f47a0beefd51fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2772515
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866490}

---
+++ content/test/data/service_worker/hello.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+self.addEventListener('fetch', (event) => {
+  // Responds to requests containing "hello_sw".
+  if (event.request.url.includes('hello_sw')) {
+    event.respondWith(
+      fetch('hello-from-sw.txt').then((response) => {
+        return response;
+      }));
+  }
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data == 'postMessage from the page') {
+    event.source.postMessage('postMessage from the service worker');
+  }
+});
