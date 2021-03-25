1089d34615edcf4a35ab1be00470adf907f617bf
Dale Curtis <dalecurtis@chromium.org>
2021-03-25 21:28:30 +0000

    Switch ImageDecoder over to new ImageTrack semantics.
    This moves frameCount and repetitionCount onto the track properties
    and removes selectTrack() in favor of "track.selected = true". The
    tracks list is modeled after Audio/VideoTrackList, but for now does
    not include any events.
    
    This also hardens the 'animated' status on a track so that it won't
    change after metadata has been retrieved. repetitionCount is switched
    to a 'unrestricted float' so 'Infinity' can be be accurately signaled.
    
    R=chcunningham
    
    Bug: 1182435, 1183456
    Test: New and updated unittests and wpt tests.
    Change-Id: Ibf8363e6f80d998202d42f745180812953681da9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2783220
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866786}

--- third_party/blink/renderer/modules/webcodecs/image_decoder_external.h
+++ third_party/blink/renderer/modules/webcodecs/image_decoder_external.h
@@ -25,7 +25,7 @@ class ScriptState;
 class ImageDecodeOptions;
 class ImageDecoderInit;
 class ImageDecodeResult;
-class ImageTrackExternal;
+class ImageTrackList;
 class ReadableStreamBytesConsumer;
 class ScriptPromiseResolver;
 class SegmentReader;
@@ -47,19 +47,14 @@ class MODULES_EXPORT ImageDecoderExternal final
 
   static ScriptPromise isTypeSupported(ScriptState*, String type);
 
-  using ImageTrackList = HeapVector<Member<ImageTrackExternal>>;
-
   // image_decoder.idl implementation.
   ScriptPromise decode(const ImageDecodeOptions* options = nullptr);
   ScriptPromise decodeMetadata();
-  void selectTrack(uint32_t track_id, ExceptionState&);
-  uint32_t frameCount() const;
-  String type() const;
-  uint32_t repetitionCount() const;
-  bool complete() const;
-  const ImageTrackList tracks() const;
   void reset(DOMException* exception = nullptr);
   void close();
+  String type() const;
+  bool complete() const;
+  ImageTrackList& tracks() const;
 
   // BytesConsumer::Client implementation.
   void OnStateChange() override;
@@ -74,6 +69,9 @@ class MODULES_EXPORT ImageDecoderExternal final
   // ScriptWrappable override.
   bool HasPendingActivity() const override;
 
+  // Called by ImageTrack to change the current track.
+  void UpdateSelectedTrack();
+
  private:
   void CreateImageDecoder();
 
@@ -84,10 +82,6 @@ class MODULES_EXPORT ImageDecoderExternal final
   // Returns false if the decoder was constructed with an ArrayBuffer or
   // ArrayBufferView that has since been neutered.
   bool HasValidEncodedData() const;
-
-  // Helpers for creating common exceptions for rejecting promises.
-  DOMException* CreateUnsupportedImageTypeException() const;
-  DOMException* CreateClosedException() const;
 
   void AbortPendingDecodes(DOMException* exception);
 
@@ -106,9 +100,12 @@ class MODULES_EXPORT ImageDecoderExternal final
   ColorBehavior color_behavior_ = ColorBehavior::Tag();
   SkISize desired_size_;
 
-  // Copy of |preferAnimation| from |init_data_|. Will be modified based on
-  // calls to selectTrack().
+  // Copy of |preferAnimation| from |init_data_|.
   base::Optional<bool> prefer_animation_;
+
+  // Currently configured AnimationOption for |decoder_|.
+  ImageDecoder::AnimationOption animation_option_ =
+      ImageDecoder::AnimationOption::kUnspecified;
 
   bool data_complete_ = false;
 
@@ -116,10 +113,7 @@ class MODULES_EXPORT ImageDecoderExternal final
 
   std::unique_ptr<ImageDecoder> decoder_;
   String mime_type_;
-  uint32_t frame_count_ = 0u;
-  uint32_t repetition_count_ = 0u;
-  base::Optional<uint32_t> selected_track_id_;
-  ImageTrackList tracks_;
+  Member<ImageTrackList> tracks_;
 
   // Pending decode() requests.
   struct DecodeRequest : public GarbageCollected<DecodeRequest> {
