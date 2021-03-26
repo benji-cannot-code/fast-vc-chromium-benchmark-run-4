0a4105cda5e6881ff66853d684cae398c6f65fb0
Eugene Zemtsov <eugene@chromium.org>
2021-03-26 01:55:19 +0000

    Include both Lollipop SDK versions into the condition
    Everything below Marshmallow is either Lollipop or not supported.
    Bug: 1175322
    
    Change-Id: I24a5237a1c6a1b2d1624b49796e90fe56bfb591f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2787521
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866890}

--- media/gpu/android/codec_allocator_unittest.cc
+++ media/gpu/android/codec_allocator_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/android/build_info.h"
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/check.h"
@@ -393,7 +394,13 @@ TEST_F(CodecAllocatorTest, LowResolutionGetsSoftware) {
                      base::Unretained(this), run_loop.QuitClosure()),
       std::move(config));
 
-  EXPECT_CALL(*this, OnCodecCreated(CodecType::kSoftware));
+  bool lollipop = base::android::BuildInfo::GetInstance()->sdk_int() <
+                  base::android::SDK_VERSION_MARSHMALLOW;
+  if (lollipop)
+    EXPECT_CALL(*this, OnCodecCreated(CodecType::kAny));
+  else
+    EXPECT_CALL(*this, OnCodecCreated(CodecType::kSoftware));
+
   run_loop.Run();
 }
 
