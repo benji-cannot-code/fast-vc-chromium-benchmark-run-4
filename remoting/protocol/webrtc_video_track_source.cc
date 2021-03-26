26efc5110cf5ab504f74324b0dd1edb642fad0ea
Lambros Lambrou <lambroslambrou@chromium.org>
2021-03-26 00:07:13 +0000

    [remoting host] Move VideoTrackSource into header/source files.
    This removes "Dummy" from the class name, and moves it to separate
    header and source files. Eventually this class will have some
    non-trivial implementation, with the move away from "internal source"
    encoding.
    
    Bug: 1192865
    Change-Id: I1a08b158ce50e26871003210bee901e17982b2f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2787671
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866863}

---
+++ remoting/protocol/webrtc_video_track_source.cc
@@ -0,0 +1,56 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/protocol/webrtc_video_track_source.h"
+
+namespace remoting {
+namespace protocol {
+
+WebrtcVideoTrackSource::WebrtcVideoTrackSource() = default;
+WebrtcVideoTrackSource::~WebrtcVideoTrackSource() = default;
+
+webrtc::MediaSourceInterface::SourceState WebrtcVideoTrackSource::state()
+    const {
+  return kLive;
+}
+
+bool WebrtcVideoTrackSource::remote() const {
+  return false;
+}
+
+bool WebrtcVideoTrackSource::is_screencast() const {
+  return true;
+}
+
+absl::optional<bool> WebrtcVideoTrackSource::needs_denoising() const {
+  return absl::nullopt;
+}
+
+bool WebrtcVideoTrackSource::GetStats(
+    webrtc::VideoTrackSourceInterface::Stats* stats) {
+  return false;
+}
+
+void WebrtcVideoTrackSource::AddOrUpdateSink(
+    rtc::VideoSinkInterface<webrtc::VideoFrame>* sink,
+    const rtc::VideoSinkWants& wants) {}
+
+void WebrtcVideoTrackSource::RemoveSink(
+    rtc::VideoSinkInterface<webrtc::VideoFrame>* sink) {}
+
+bool WebrtcVideoTrackSource::SupportsEncodedOutput() const {
+  return false;
+}
+
+void WebrtcVideoTrackSource::GenerateKeyFrame() {}
+
+void WebrtcVideoTrackSource::AddEncodedSink(
+    rtc::VideoSinkInterface<webrtc::RecordableEncodedFrame>* sink) {}
+
+void WebrtcVideoTrackSource::RemoveEncodedSink(
+    rtc::VideoSinkInterface<webrtc::RecordableEncodedFrame>* sink) {}
+
+}  // namespace protocol
+}  // namespace remoting
