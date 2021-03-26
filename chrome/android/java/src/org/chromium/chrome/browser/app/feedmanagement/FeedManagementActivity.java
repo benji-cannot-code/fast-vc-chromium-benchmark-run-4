4e326b98e489efce2f2706f1dd7977c8474b1d66
Pete Williamson <petewil@chromium.org>
2021-03-26 01:02:15 +0000

    Add the Feed management interstitial menu.
    For web feed management, we are taking all the manage items out of
    the discover "gear" menu, and putting them into a new interstitial
    menu activity.  This changelist defines the new activity and the
    core code files to implement it. The actual implementation will be
    in a following changelist (to keep the individual changes smaller).
    
    This is a redo of
    https://chromium-review.googlesource.com/c/chromium/src/+/2740208/,
    which got corrupted in a merge conflict.
    
    Bug: 1188903
    Change-Id: I6905b487d894f8489cc3e54dbaf69bc7c1d0603d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2766186
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#866876}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/app/feedmanagement/FeedManagementActivity.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.app.feedmanagement;
+
+import android.os.Bundle;
+
+import org.chromium.chrome.browser.SnackbarActivity;
+import org.chromium.chrome.browser.feed.feedmanagement.FeedManagementCoordinator;
+
+/**
+ * Activity for managing feed and webfeed settings on the new tab page.
+ */
+public class FeedManagementActivity extends SnackbarActivity {
+    private static final String TAG = "FeedManagementActivity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FeedManagementCoordinator coordinator = new FeedManagementCoordinator(this);
+        setContentView(coordinator.getView());
+    }
+}
