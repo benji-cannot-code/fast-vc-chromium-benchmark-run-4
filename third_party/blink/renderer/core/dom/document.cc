5ddb1981b956814c43bc6d9f9e0f2a0fa2811bc9
Stefan Zager <szager@chromium.org>
2021-03-28 02:19:08 +0000

    Check for document->View() before doing scroll restoration
    Bug: 1191378
    Change-Id: Iee1a228c59cbe1bc8c8857d6eef6420c95db45a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2785357
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867044}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -2559,6 +2559,7 @@ void Document::UpdateStyleAndLayoutForNode(const Node* node,
 }
 
 void Document::ApplyScrollRestorationLogic() {
+  DCHECK(View());
   // This function in not re-entrant. However, the places that invoke this are
   // re-entrant. Specifically, UpdateStyleAndLayout() calls this, which in turn
   // can do a find-in-page for the scroll-to-text feature, which can cause
@@ -2704,7 +2705,8 @@ void Document::UpdateStyleAndLayout(DocumentUpdateReason reason) {
   if (Lifecycle().GetState() < DocumentLifecycle::kLayoutClean)
     Lifecycle().AdvanceTo(DocumentLifecycle::kLayoutClean);
 
-  ApplyScrollRestorationLogic();
+  if (frame_view)
+    ApplyScrollRestorationLogic();
 
   if (LocalFrameView* frame_view_anchored = View())
     frame_view_anchored->PerformScrollAnchoringAdjustments();
