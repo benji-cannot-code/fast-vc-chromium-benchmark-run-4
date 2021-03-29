c21917ca399182b86820e37acb131352a56f81d2
Carlos Caballero <carlscab@google.com>
2021-03-29 10:43:31 +0000

    [mparch] Remove unnecessary use of WebContentsObserver::FrameDeleted
    Clean up the page_load_metrics code to a point where uses of
    FrameDeleted only require the rfh->GetFrameTreeNodeId() in preparation
    for an API change that will pass int frame_tree_node_id as a param to
    WebContentsObserver::FrameDeleted.
    
    Bug: 1188721
    Change-Id: Id99a9d1a82eb399b5ef44aa31342f90bcb7d97fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2778322
    Reviewed-by: Tom McKee <tommckee@google.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#867160}

--- components/page_load_metrics/browser/page_load_metrics_memory_tracker.cc
+++ components/page_load_metrics/browser/page_load_metrics_memory_tracker.cc
@@ -130,7 +130,7 @@ void PageLoadMetricsMemoryTracker::OnV8MemoryMeasurementAvailable(
   }
 }
 
-void PageLoadMetricsMemoryTracker::OnFrameDeleted(
+void PageLoadMetricsMemoryTracker::OnRenderFrameDeleted(
     content::RenderFrameHost* render_frame_host,
     MetricsWebContentsObserver* observer) {
   DCHECK(render_frame_host);
