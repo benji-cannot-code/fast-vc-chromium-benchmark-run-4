8bb1e3cef8215f6ffe96ac35015c969759228d11
Anton Bikineev <bikineev@chromium.org>
2021-03-29 01:12:59 +0000

    Revert "PCScan: Fix race in scan scheduling heuristic to avoid useless scans."
    Reason for revert: Race between finishing scanning and free(), which
    causes PCScan to stop running:
    1) scanner sets thread_limit_;
    2) mutator calls free(large_object) (or a bunch of free() calls);
    3) scanner finishes scanning;
    4) subsequent free() calls never schedule PCScan, since at that point
    trigger_limit_ is always < 0 (until underflows).
    
    Original change's description:
    > Instead of comparing the scan limit with the current quarantine size,
    > we decrement the limit. The first deallocation that makes it negative
    > triggers the scan. With that the state is atomic.
    >
    > Bug: 1129751
    > Change-Id: I50516866913984ad69041efc269471870f4097a7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774979
    > Commit-Queue: Hannes Payer <hpayer@chromium.org>
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#866187}
    
    Bug: 1129751
    Change-Id: I5cf19ddd50aa0d17bb6da8ad6005f4f16f06f772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2789471
    Auto-Submit: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867097}

--- base/allocator/partition_allocator/starscan/pcscan.cc
+++ base/allocator/partition_allocator/starscan/pcscan.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <array>
-#include <atomic>
 #include <condition_variable>
 #include <mutex>
 #include <numeric>
@@ -1359,7 +1358,7 @@ void PCScanTask::SweepQuarantine() {
 
 void PCScanTask::FinishScanner() {
   stats_.ReportTracesAndHists();
-  LogStats(stats_.swept_size(), pcscan_.quarantine_data_.quarantine_size(),
+  LogStats(stats_.swept_size(), pcscan_.quarantine_data_.last_size(),
            stats_.survived_quarantine_size());
 
   const size_t total_pa_heap_size =
@@ -1497,10 +1496,10 @@ class PCScan::PCScanThread final {
   TaskHandle posted_task_;
 };
 
-constexpr intptr_t PCScan::QuarantineData::kQuarantineSizeMinLimit;
+constexpr size_t PCScan::QuarantineData::kQuarantineSizeMinLimit;
 
 void PCScan::QuarantineData::ResetAndAdvanceEpoch() {
-  quarantine_size_ = limit_ - trigger_limit_.load(std::memory_order_relaxed);
+  last_size_ = current_size_.exchange(0, std::memory_order_relaxed);
   epoch_.fetch_add(1, std::memory_order_relaxed);
 }
 
@@ -1508,15 +1507,10 @@ void PCScan::QuarantineData::GrowLimitIfNeeded(size_t heap_size) {
   static constexpr double kQuarantineSizeFraction = 0.1;
   // |heap_size| includes the current quarantine size, we intentionally leave
   // some slack till hitting the limit.
-  limit_ = std::max(kQuarantineSizeMinLimit,
-                    static_cast<intptr_t>(kQuarantineSizeFraction * heap_size));
-  // Force a new scan on the next deallocation if the too many deallocations
-  // accumulated during the current scan.
-  const intptr_t old_trigger_limit =
-      trigger_limit_.fetch_add(limit_, std::memory_order_relaxed);
-  if (old_trigger_limit + limit_ < 0) {
-    trigger_limit_.store(0, std::memory_order_relaxed);
-  }
+  size_limit_.store(
+      std::max(kQuarantineSizeMinLimit,
+               static_cast<size_t>(kQuarantineSizeFraction * heap_size)),
+      std::memory_order_relaxed);
 }
 
 void PCScan::PerformScan(InvocationMode invocation_mode) {
