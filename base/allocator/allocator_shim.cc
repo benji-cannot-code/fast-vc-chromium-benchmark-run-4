c22af08d9d928ad80ec983231ead57598fcfa4bb
Yuki Shiino <yukishiino@chromium.org>
2021-03-29 10:57:56 +0000

    base/allocator: The first step of PA-E for macOS
    When PA-E is enabled on macOS, adds a new malloc zone and makes
    it the default zone.  The zone functions use the allocator shim.
    
    Bug: 1121427
    Change-Id: I81714e36830c0658c8855b04795b657a2d536e8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2777988
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867163}

--- base/allocator/allocator_shim.cc
+++ base/allocator/allocator_shim.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <malloc/malloc.h>
 
 #include "base/allocator/allocator_interception_mac.h"
+#include "base/mac/mach_logging.h"
 #endif
 
 // No calls to malloc / new in this file. They would would cause re-entrancy of
@@ -339,7 +340,11 @@ ALWAYS_INLINE void ShimAlignedFree(void* address, void* context) {
 // On Windows we use plain link-time overriding of the CRT symbols.
 #include "base/allocator/allocator_shim_override_ucrt_symbols_win.h"
 #elif defined(OS_APPLE)
+#if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+#include "base/allocator/allocator_shim_override_mac_default_zone.h"
+#else  // BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 #include "base/allocator/allocator_shim_override_mac_symbols.h"
+#endif  // BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 #else
 #include "base/allocator/allocator_shim_override_libc_symbols.h"
 #endif
@@ -372,7 +377,11 @@ ALWAYS_INLINE void ShimAlignedFree(void* address, void* context) {
 #if defined(OS_APPLE)
 namespace base {
 namespace allocator {
+
 void InitializeAllocatorShim() {
+#if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+  AddMacMallocZoneAsDefaultZone();
+#else   // BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
   // Prepares the default dispatch. After the intercepted malloc calls have
   // traversed the shim this will route them to the default malloc zone.
   InitializeDefaultDispatchToMacAllocator();
@@ -382,7 +391,9 @@ void InitializeAllocatorShim() {
   // This replaces the default malloc zone, causing calls to malloc & friends
   // from the codebase to be routed to ShimMalloc() above.
   base::allocator::ReplaceFunctionsForStoredZones(&functions);
+#endif  // BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 }
+
 }  // namespace allocator
 }  // namespace base
 #endif
