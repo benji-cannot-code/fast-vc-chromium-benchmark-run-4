b1e4db94f7e28c3e5d454b6c365300a42dec31d0
Sreeja Kamishetty <sreejakshetty@chromium.org>
2021-03-29 16:26:13 +0000

    [content] Introduce LifecycleStateImpl::kPendingCommit
    This CL adds a new LifecycleStateImpl::kPendingCommit to the
    RenderFrameHostImpl::LifecycleStateImpl to represent the state of the
    RenderFrameHost after the navigation commits in the renderer and is
    waiting to commit in the browser process.
    
    kPendingCommit Doc:
    https://docs.google.com/document/d/1ZcPSHVtVmvlmNdodHtHgjStQngs4ZJLFVGcIFdfCGjE/edit?usp=sharing
    
    BUG=1182291
    
    Change-Id: I77e105853b60d7f9494a285174c264018763bb94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2719235
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867233}

--- content/browser/renderer_host/render_frame_host_manager.cc
+++ content/browser/renderer_host/render_frame_host_manager.cc
@@ -3365,7 +3365,10 @@ std::unique_ptr<RenderFrameHostImpl> RenderFrameHostManager::SetRenderFrameHost(
   // - kPrerendering: starts unloading.
 
   // The lifecycle state of the new RenderFrameHost is either:
-  // - kSpeculative: for regular navigation.
+  // - kSpeculative: for early-commit navigations (see
+  //   https://crbug.com/1072817) and when attaching an inner delegate (when
+  //   embedding one WebContents inside another).
+  // - kPendingCommit: for regular cross-RenderFrameHost navigations.
   // - kBackForwardCache: for BackForwardCache restore navigation.
   // - kPrerendering: for a prerender activation navigation.
   // It should become kActive in the primary frame tree and kPrerendering for
@@ -3381,7 +3384,7 @@ std::unique_ptr<RenderFrameHostImpl> RenderFrameHostManager::SetRenderFrameHost(
   if (render_frame_host_) {
     if (frame_tree->is_prerendering()) {
       if (render_frame_host_->lifecycle_state() ==
-          LifecycleStateImpl::kSpeculative)
+          LifecycleStateImpl::kPendingCommit)
         render_frame_host_->SetLifecycleStateToPrerendering();
     } else {
       if (render_frame_host_->lifecycle_state() != LifecycleStateImpl::kActive)
