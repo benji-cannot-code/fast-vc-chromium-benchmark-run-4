a9b69e9a9a2d2cc301b9a4c202e3df30b881163e
Aaron Leventhal <aleventhal@google.com>
2021-03-29 22:43:44 +0000

    Use correct index for active option in open menulist
    AXMenuList needs to use the current active index, not the one from
    before the popup was opened.
    
    This regressed in CL:2728870, and wasn't caught because the tests
    are disabled because of flakiness.
    
    Reenabling the tests in CL:2787386.
    
    Bug: 1189283
    Change-Id: Ide7a529e996102043e75a0c6aa64509ffdd32506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2788731
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867312}

--- third_party/blink/renderer/core/html/forms/html_select_element.cc
+++ third_party/blink/renderer/core/html/forms/html_select_element.cc
@@ -1275,7 +1275,8 @@ void HTMLSelectElement::SetIndexToSelectOnCancel(int list_index) {
   select_type_->UpdateTextStyleAndContent();
 }
 
-HTMLOptionElement* HTMLSelectElement::OptionToBeShownForTesting() const {
+HTMLOptionElement* HTMLSelectElement::OptionToBeShown() const {
+  DCHECK(!IsMultiple());
   return select_type_->OptionToBeShown();
 }
 
