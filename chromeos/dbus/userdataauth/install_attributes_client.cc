5fd8d70133c451f5f2fd9d5d2fcec4e876e3d3d8
John L Chen <zuan@chromium.org>
2021-03-29 09:27:58 +0000

    Fix InstallAttributesClient to use the correct dbus interface
    BUG=b:181787676
    TEST=Build OK && Unit test passes
    
    Change-Id: I630b5fe007d3acad66f601a44c8e819c14972053
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2788828
    Commit-Queue: John L Chen <zuan@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867150}

--- chromeos/dbus/userdataauth/install_attributes_client.cc
+++ chromeos/dbus/userdataauth/install_attributes_client.cc
@@ -131,7 +131,7 @@ class InstallAttributesClientImpl : public InstallAttributesClient {
       const ::user_data_auth::InstallAttributesGetRequest& request) override {
     return BlockingCallProtoMethod<::user_data_auth::InstallAttributesGetReply>(
         ::user_data_auth::kInstallAttributesGet,
-        ::user_data_auth::kCryptohomeMiscInterface, request);
+        ::user_data_auth::kInstallAttributesInterface, request);
   }
 
   base::Optional<::user_data_auth::InstallAttributesSetReply>
@@ -139,7 +139,7 @@ class InstallAttributesClientImpl : public InstallAttributesClient {
       const ::user_data_auth::InstallAttributesSetRequest& request) override {
     return BlockingCallProtoMethod<::user_data_auth::InstallAttributesSetReply>(
         ::user_data_auth::kInstallAttributesSet,
-        ::user_data_auth::kCryptohomeMiscInterface, request);
+        ::user_data_auth::kInstallAttributesInterface, request);
   }
 
   base::Optional<::user_data_auth::InstallAttributesFinalizeReply>
@@ -149,7 +149,7 @@ class InstallAttributesClientImpl : public InstallAttributesClient {
     return BlockingCallProtoMethod<
         ::user_data_auth::InstallAttributesFinalizeReply>(
         ::user_data_auth::kInstallAttributesFinalize,
-        ::user_data_auth::kCryptohomeMiscInterface, request);
+        ::user_data_auth::kInstallAttributesInterface, request);
   }
 
   base::Optional<::user_data_auth::InstallAttributesGetStatusReply>
@@ -159,7 +159,7 @@ class InstallAttributesClientImpl : public InstallAttributesClient {
     return BlockingCallProtoMethod<
         ::user_data_auth::InstallAttributesGetStatusReply>(
         ::user_data_auth::kInstallAttributesGetStatus,
-        ::user_data_auth::kCryptohomeMiscInterface, request);
+        ::user_data_auth::kInstallAttributesInterface, request);
   }
 
  private:
