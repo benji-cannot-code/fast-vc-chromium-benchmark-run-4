afd52fd12ab19a41ff73bc58549854f2e42ecfa7
Ghazale Hosseinabadi <ghazale@google.com>
2021-03-29 15:27:58 +0000

    Revert "base::Feature insead of RuntimeEnabledFeature for ModuleServiceWorker"
    This CL reverts commit cd5646731b212ecd76c11dc12e8b1b521616b58f. Since
    RuntimeEnabledFeatures::ModuleServiceWorker is set to stable, browser
    tests can pass without commit cd5646731b212ecd76c11dc12e8b1b521616b58f
    and commit cd5646731b212ecd76c11dc12e8b1b521616b58f is no longer needed.
    
    Bug: 1189313
    Change-Id: Ieb15460226cff0b65983399c0e4198b9efd72ebb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2788904
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867204}

--- third_party/blink/renderer/modules/service_worker/service_worker_container.cc
+++ third_party/blink/renderer/modules/service_worker/service_worker_container.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/optional.h"
-#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_error_type.mojom-blink.h"
 #include "third_party/blink/public/platform/web_fetch_client_settings_object.h"
 #include "third_party/blink/public/platform/web_string.h"
@@ -224,7 +223,7 @@ ScriptPromise ServiceWorkerContainer::registerServiceWorker(
   // TODO(crbug.com/824647): Remove this check after module loading for
   // ServiceWorker is enabled by default.
   if (options->type() == "module" &&
-      !base::FeatureList::IsEnabled(features::kModuleServiceWorker)) {
+      !RuntimeEnabledFeatures::ModuleServiceWorkerEnabled()) {
     resolver->Reject(MakeGarbageCollected<DOMException>(
         DOMExceptionCode::kNotSupportedError,
         "type 'module' in RegistrationOptions is not implemented yet."
