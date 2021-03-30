416a4dfd882b4d13f45faf30c2c6866f800f1d26
Nancy Wang <nancylingwang@chromium.org>
2021-03-30 08:17:21 +0000

    Reland: arc::LaunchAppWithIntent with AppService LaunchAppWithIntent.
    This CL replaces arc::LaunchAppWithIntent with the AppService interface
    LaunchAppWithIntent, so that:
    1. We can have 1 central place to add app launching metrics(UMA)
    2. We have 1 central place to record the app launching information for
    the full restore project.
    
    Modify ArcAppLauncher to call the AppService interface
    LaunchAppWithIntent to launch the app, and observe AppRegistryCache to
    keep stack the app status.
    
    Add CreateIntentFromStartType to generate intent using activity and
    start type.
    
    Modify mojom to add start type to the intent mojom structure.
    
    Modify unit tests to verify ArcAppLauncher.
    
    Fix the test issue. Change the DCHECK to check whether app_launched_ is
    true to prevent duplicate launching.
    
    Resolve optin failures in Tast Tests. Add
    "android.intent.category.LAUNCHER" to categories for launch intent.
    
    Bug: b/182497089
    Bug: 1146900
    Bug: 1154891
    
    Change-Id: I9519bc9a415b8bd4fd9b63e667b69160d9d77f4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2789075
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Cr-Commit-Position: refs/heads/master@{#867512}

--- chrome/browser/ui/app_list/arc/arc_app_launcher.h
+++ chrome/browser/ui/app_list/arc/arc_app_launcher.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_utils.h"
 #include "components/arc/metrics/arc_metrics_constants.h"
+#include "components/services/app_service/public/cpp/app_registry_cache.h"
+#include "components/services/app_service/public/mojom/types.mojom.h"
 
 namespace content {
 class BrowserContext;
@@ -22,14 +24,15 @@ class BrowserContext;
 
 // Helper class for deferred ARC app launching in case app is not ready on the
 // moment of request.
-class ArcAppLauncher : public ArcAppListPrefs::Observer {
+class ArcAppLauncher : public ArcAppListPrefs::Observer,
+                       public apps::AppRegistryCache::Observer {
  public:
   ArcAppLauncher(content::BrowserContext* context,
                  const std::string& app_id,
-                 const base::Optional<std::string>& launch_intent,
+                 apps::mojom::IntentPtr launch_intent,
                  bool deferred_launch_allowed,
                  int64_t display_id,
-                 arc::UserInteractionType interaction);
+                 apps::mojom::LaunchSource launch_source);
   ~ArcAppLauncher() override;
 
   bool app_launched() const { return app_launched_; }
@@ -40,9 +43,15 @@ class ArcAppLauncher : public ArcAppListPrefs::Observer {
   void OnAppStatesChanged(const std::string& app_id,
                           const ArcAppListPrefs::AppInfo& app_info) override;
 
+  // apps::AppRegistryCache::Observer overrides:
+  void OnAppUpdate(const apps::AppUpdate& update) override;
+  void OnAppRegistryCacheWillBeDestroyed(
+      apps::AppRegistryCache* cache) override;
+
  private:
   bool MaybeLaunchApp(const std::string& app_id,
-                      const ArcAppListPrefs::AppInfo& app_info);
+                      const ArcAppListPrefs::AppInfo& app_info,
+                      apps::mojom::Readiness readiness);
 
   // Unowned pointer.
   content::BrowserContext* context_;
@@ -50,7 +59,7 @@ class ArcAppLauncher : public ArcAppListPrefs::Observer {
   const std::string app_id_;
   // Optional intent to launch the app. If not set then app is started default
   // way.
-  const base::Optional<std::string> launch_intent_;
+  apps::mojom::IntentPtr launch_intent_;
   // If it is set to true that means app is allowed to launch in deferred mode
   // once it is registered, regardless it is ready or not. Otherwise app is
   // launched when it becomes ready.
@@ -60,7 +69,7 @@ class ArcAppLauncher : public ArcAppListPrefs::Observer {
   // Flag indicating that ARC app was launched.
   bool app_launched_ = false;
   // Enum that indicates what type of metric to record to UMA on launch.
-  arc::UserInteractionType interaction_;
+  apps::mojom::LaunchSource launch_source_;
 
   DISALLOW_COPY_AND_ASSIGN(ArcAppLauncher);
 };
