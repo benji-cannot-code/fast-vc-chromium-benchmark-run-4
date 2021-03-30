44642ee758a675340ebc12c9a4a6ef194c608f97
Vasilii Sukhanov <vasilii@chromium.org>
2021-03-30 09:00:13 +0000

    Revert "Add temporary logging for debugging the test."
    This reverts commit 952a24fea83cfc227d3065dc462d0d2da20fc020.
    
    Reason for revert: Enough logs collected.
    
    Original change's description:
    > Add temporary logging for debugging the test.
    >
    > CredentialManagerBrowserTest.UpdatingPSLMatchedCredentialCreatesSecondEntry
    > is flaky. The logs are to be removed in a day or two.
    >
    > Bug: 1136691
    > Change-Id: I1b8258fbdc3e5aa3811d2b06ccdeee0210b11f76
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791363
    > Auto-Submit: Vasilii Sukhanov <vasilii@chromium.org>
    > Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    > Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#867201}
    
    Bug: 1136691
    Change-Id: I40e976d338438b730e5746276542e6be3ed76fd9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791430
    Auto-Submit: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#867527}

--- third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
@@ -1170,8 +1170,6 @@ ScriptPromise CredentialsContainer::store(ScriptState* script_state,
     return promise;
   }
 
-  // Temporary debugging for UpdatingPSLMatchedCredentialCreatesSecondEntry test
-  VLOG(0) << "CredentialsContainer::store";
   auto* credential_manager =
       CredentialManagerProxy::From(script_state)->CredentialManager();
   credential_manager->Store(
