5164b2e2078dcfb336bb25eae938654d00322cfe
Xiaoqian Dai <xdai@chromium.org>
2021-03-30 15:57:11 +0000

    Use MockTime in BackGestureContextualNudgeControllerTestA11yPrefs tests instead.
    Bug: 1190416
    Change-Id: Id78ffaa3871933d418db834b890815f3228e97b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2793010
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867632}

--- ash/test/ash_test_base.h
+++ ash/test/ash_test_base.h
@@ -306,8 +306,10 @@ class AshTestBase : public testing::Test {
 
 class NoSessionAshTestBase : public AshTestBase {
  public:
-  NoSessionAshTestBase() { set_start_session(false); }
-  ~NoSessionAshTestBase() override {}
+  NoSessionAshTestBase();
+  explicit NoSessionAshTestBase(
+      base::test::TaskEnvironment::TimeSource time_source);
+  ~NoSessionAshTestBase() override;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(NoSessionAshTestBase);
