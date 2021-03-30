7e8e8a0a34e8ab2fe24a36ba339fb40ce35c0f38
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2021-03-30 19:37:37 +0000

    [iOS][Default Promo] Virtual imlplementation for tailored branded images
    Bug: 1191726
    Change-Id: I9c6a882739d48e0bba49cb702d13e4573188b622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2792920
    Commit-Queue: Javier Flores <javierrobles@chromium.org>
    Auto-Submit: Javier Flores <javierrobles@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867725}

--- ios/public/provider/chrome/browser/images/branded_image_provider.mm
+++ ios/public/provider/chrome/browser/images/branded_image_provider.mm
@@ -51,3 +51,11 @@ UIImage* BrandedImageProvider::GetDownloadGoogleDriveImage() {
 UIImage* BrandedImageProvider::GetOmniboxAnswerIcon() {
   return nil;
 }
+
+UIImage* BrandedImageProvider::GetAllTabsPromoImage() {
+  return nil;
+}
+
+UIImage* BrandedImageProvider::GetMadeForIOSPromoImage() {
+  return nil;
+}
