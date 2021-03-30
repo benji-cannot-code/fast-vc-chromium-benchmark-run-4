54507c5e06a4448d8b2f42261ad92c5bc22bd36d
Mason Freed <masonf@chromium.org>
2021-03-30 22:47:33 +0000

    Implement new behavior for invoking ("popup") elements
    The previous code assumed all potential invoking elements formed part
    of the ancestor chain. With recent clarifications, only the element
    that actually invoked the popup should count as an invoking element.
    
    Bug: 1168738
    Change-Id: I246de43b69770793d848c714ebda4aa9b316aaa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2795412
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Ionel Popescu <iopopesc@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#867845}

--- third_party/blink/renderer/core/html/html_popup_element.h
+++ third_party/blink/renderer/core/html/html_popup_element.h
@@ -31,8 +31,9 @@ class HTMLPopupElement final : public HTMLElement {
 
   Element* AnchorElement() const;
 
-  void AddInvoker(Element* invoker);
-  void RemoveInvoker(Element* invoker);
+  // This is used by invoking elements (which have a "popup" attribute)
+  // to invoke the popup.
+  void Invoke(Element* invoker);
 
   static void HandleLightDismiss(const Event&);
 
@@ -49,7 +50,7 @@ class HTMLPopupElement final : public HTMLElement {
   static const HTMLPopupElement* NearestOpenAncestralPopup(Node*);
 
   bool open_;
-  HeapHashSet<Member<Element>> invokers_;
+  WeakMember<Element> invoker_;
 };
 
 }  // namespace blink
