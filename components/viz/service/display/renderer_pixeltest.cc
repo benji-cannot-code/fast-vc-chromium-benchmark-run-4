a104a5afd05443743512794c25ccd58786c98890
kylechar <kylechar@chromium.org>
2021-03-30 21:13:05 +0000

    Try enabling previously flaky RenderPixelTests
    RenderPixelTest FastPassSaturateFilter and FastPassColorFilterAlpha were
    disabled for being flaky about a year ago. I tested locally with a tsan
    build and was unable to reproduce any flake. Try re-enabling the tests
    and see if flake reoccurs.
    
    Bug: 1044841
    Change-Id: Ie02a403ff0949cbd07057cf688cb927141316ed3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2795443
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867788}

--- components/viz/service/display/renderer_pixeltest.cc
+++ components/viz/service/display/renderer_pixeltest.cc
@@ -2104,8 +2104,7 @@ TEST_P(VideoRendererPixelTest, TwoColorY16Rect) {
       cc::FuzzyPixelOffByOneComparator(true)));
 }
 
-// TODO(https://crbug.com/1044841): Flaky, especially on Linux/TSAN and Fuchsia.
-TEST_P(RendererPixelTest, DISABLED_FastPassColorFilterAlpha) {
+TEST_P(RendererPixelTest, FastPassColorFilterAlpha) {
   gfx::Rect viewport_rect(this->device_viewport_size_);
 
   AggregatedRenderPassId root_pass_id{1};
@@ -2182,8 +2181,7 @@ TEST_P(RendererPixelTest, DISABLED_FastPassColorFilterAlpha) {
       FuzzyForSkiaOnlyPixelComparator(renderer_type())));
 }
 
-// TODO(https://crbug.com/1044841): Flaky, especially on Linux/TSAN and Fuchsia.
-TEST_P(RendererPixelTest, DISABLED_FastPassSaturateFilter) {
+TEST_P(RendererPixelTest, FastPassSaturateFilter) {
   gfx::Rect viewport_rect(this->device_viewport_size_);
 
   AggregatedRenderPassId root_pass_id{1};
