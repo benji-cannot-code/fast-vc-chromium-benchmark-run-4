68ef9ced15dbfb1a79069119f0e7995ad6250801
Pavel Yatsuk <pavely@google.com>
2021-03-30 20:16:58 +0000

    [Messages] Support for both, displaying a menu and direct callback to native from secondary button
    This CL introduces a logic that allows feature code to either display
    a single item menu in response to secondary button or directly invoke
    a callback that later can trigger modal dialog.
    
    BUG=1163302
    R=lazzzis@chromium.org
    
    Change-Id: I2d79dc96ee6b101cfcfe243dd26395a53ad8c4d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2787760
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867752}

--- components/messages/android/internal/java/src/org/chromium/components/messages/MessageBannerView.java
+++ components/messages/android/internal/java/src/org/chromium/components/messages/MessageBannerView.java
@@ -37,7 +37,7 @@ public class MessageBannerView extends BoundedLinearLayout {
     private TextView mPrimaryButton;
     private ListMenuButton mSecondaryButton;
     private View mDivider;
-    private String mSecondaryActionText;
+    private String mSecondaryButtonMenuText;
     private Runnable mSecondaryActionCallback;
     private SwipeGestureListener mSwipeGestureDetector;
 
@@ -54,7 +54,7 @@ public class MessageBannerView extends BoundedLinearLayout {
         mIconView = findViewById(R.id.message_icon);
         mSecondaryButton = findViewById(R.id.message_secondary_button);
         mDivider = findViewById(R.id.message_divider);
-        mSecondaryButton.setOnClickListener((View v) -> { displayMenu(); });
+        mSecondaryButton.setOnClickListener((View v) -> { handleSecondaryButtonClick(); });
     }
 
     void setTitle(String title) {
@@ -89,8 +89,8 @@ public class MessageBannerView extends BoundedLinearLayout {
         mSecondaryActionCallback = callback;
     }
 
-    void setSecondaryActionText(String text) {
-        mSecondaryActionText = text;
+    void setSecondaryButtonMenuText(String text) {
+        mSecondaryButtonMenuText = text;
     }
 
     void setSecondaryIconContentDescription(String description) {
@@ -101,13 +101,17 @@ public class MessageBannerView extends BoundedLinearLayout {
         mSwipeGestureDetector = new MessageSwipeGestureListener(getContext(), handler);
     }
 
-    // TODO(crbug.com/1163302): For the M88 experiment we decided to display single item menu in
-    // response to the tap on secondary button. The code below implements this logic. Past M88 it
-    // will be replaced with modal dialog driven from the feature code.
-    void displayMenu() {
+    void handleSecondaryButtonClick() {
+        if (mSecondaryButtonMenuText == null) {
+            if (mSecondaryActionCallback != null) {
+                mSecondaryActionCallback.run();
+            }
+            return;
+        }
+
         final PropertyModel menuItemPropertyModel =
                 new PropertyModel.Builder(ListMenuItemProperties.ALL_KEYS)
-                        .with(ListMenuItemProperties.TITLE, mSecondaryActionText)
+                        .with(ListMenuItemProperties.TITLE, mSecondaryButtonMenuText)
                         .with(ListMenuItemProperties.ENABLED, true)
                         .build();
 
