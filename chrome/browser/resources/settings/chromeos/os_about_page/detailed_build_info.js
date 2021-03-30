ffb9efaab6fc410be2d3f9c9cfde30411674fbf5
Regan Hsu <hsuregan@google.com>
2021-03-30 06:34:25 +0000

    [CrOS Settings] Show different EOL subtitle string for Managed users.
    The AUE date on the settings page is not an accurate reflection of
    updates for managed users since the updates for these users are
    controlled by the administrator. The date is removed for managed
    users and replaced with a general statement indicating that updates
    are managed by the domain admin.
    
    Screenshot:
    go/tscreen/a36854b58ec16bca714593f37925205344a483ba
    
    Mock:
    https://screenshot.googleplex.com/4sSG7oxeipAGrdC.png
    
    Fixed: 1193843
    Change-Id: I544e308ac5f1eba5c9b29a3fe122942e2252eb4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2792925
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867492}

--- chrome/browser/resources/settings/chromeos/os_about_page/detailed_build_info.js
+++ chrome/browser/resources/settings/chromeos/os_about_page/detailed_build_info.js
@@ -57,10 +57,29 @@ Polymer({
     },
 
     /** @private */
+    shouldHideEolInfo_: {
+      type: Boolean,
+      computed: 'computeShouldHideEolInfo_(eolMessageWithMonthAndYear)',
+    },
+
+    /** @private */
     isHostnameSettingEnabled_: {
       type: Boolean,
       value() {
         return loadTimeData.getBoolean('isHostnameSettingEnabled');
+      },
+      readOnly: true,
+    },
+
+    /**
+     * Whether the browser/ChromeOS is managed by their organization
+     * through enterprise policies.
+     * @private
+     */
+    isManaged_: {
+      type: Boolean,
+      value() {
+        return loadTimeData.getBoolean('isManaged');
       },
       readOnly: true,
     },
@@ -93,6 +112,14 @@ Polymer({
     }
 
     this.attemptDeepLink();
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  computeShouldHideEolInfo_() {
+    return this.isManaged_ || !this.eolMessageWithMonthAndYear;
   },
 
   /** @private */
