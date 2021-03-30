42e34cc882a82bbf7f0aa464ac4c03b01885611e
Samuel Huang <huangs@chromium.org>
2021-03-30 22:16:58 +0000

    Reland "[Lacros] Impose Public Session restrictions for chrome.webRequest."
    This reverts commit 62be0fb4c48afc07279d18bb4f4bf1e8db3acc2f.
    
    Reason for reland: Failed tests have been fixed. 
    
    Original change's description:
    > Revert "[Lacros] Impose Public Session restrictions for chrome.webRequest."
    >
    > This reverts commit 30c9bc8705e36d7299895013a676cdc4c80eeff8.
    >
    > Reason for revert: This is likely causing WebViewAPITest and ExtensionWebRequestPermissionsTest failures.
    >
    > Original change's description:
    > > [Lacros] Impose Public Session restrictions for chrome.webRequest.
    > >
    > > ChromeOS uses LoginState to impose restrictions from Public Session for
    > > the chrome.webRequest extension API. Specifically, this uses
    > > LoginState::ArePublicSessionRestrictionsEnabled().
    > >
    > > This CL has Lacros apply the same restriction. Instead of using
    > > LogicState, SessionType in BrowserInitParams is used. To hide
    > > implementation differences from Ash, the logic is extracted to helper
    > > function ArePublicSessionRestrictionsEnabled().
    > >
    > > This CL also adds ScopedLacrosChromeServiceTestHelper to simplify
    > > instantiating LacrosChromeServiceImpl in unit tests. Usage is added to:
    > > * ExtensionServiceTestBase
    > > * WebRequestRulesRegistryTest
    > > This is a more wide-ranging change that affects the ongoing effort to
    > > have Lacros support Ash extension API.
    > >
    > > Bug: 1188688
    > > Change-Id: Ied1140545068ebba8010f6d22f6261430d5ed38a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2764644
    > > Commit-Queue: Samuel Huang <huangs@chromium.org>
    > > Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    > > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#865385}
    >
    > Bug: 1188688
    > Change-Id: I113b223e0ec791b8f40d04d7fd025efe054e6fca
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2781764
    > Auto-Submit: Samuel Huang <huangs@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/master@{#865631}
    
    Bug: 1188688
    Change-Id: I99941adf31e49716d8953579660134ce094f2ba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782491
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867825}

--- chrome/browser/extensions/extension_service_test_base.h
+++ chrome/browser/extensions/extension_service_test_base.h
@@ -33,6 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/settings/scoped_cros_settings_test_helper.h"
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chromeos/lacros/scoped_lacros_chrome_service_test_helper.h"
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+
 class Profile;
 class TestingProfile;
 
@@ -215,6 +219,11 @@ class ExtensionServiceTestBase : public testing::Test {
   ash::ScopedCrosSettingsTestHelper cros_settings_test_helper_;
   ash::ScopedTestUserManager test_user_manager_;
 #endif
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  chromeos::ScopedLacrosChromeServiceTestHelper
+      scoped_lacros_chrome_service_test_helper_;
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 
   // An override that ignores CRX3 publisher signatures.
   SandboxedUnpacker::ScopedVerifierFormatOverrideForTest
--- extensions/browser/api/web_request/web_request_api_helpers.cc
+++ extensions/browser/api/web_request/web_request_api_helpers.cc
@@ -53,6 +53,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/features.h"
 #include "url/url_constants.h"
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "chromeos/login/login_state/login_state.h"
+#endif
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chromeos/crosapi/mojom/crosapi.mojom.h"  // nogncheck
+#include "chromeos/lacros/lacros_chrome_service_impl.h"
+#endif
+
 // TODO(battre): move all static functions into an anonymous namespace at the
 // top of this file.
 
@@ -1733,6 +1742,20 @@ bool ShouldHideRequestHeader(content::BrowserContext* browser_context,
 bool ShouldHideResponseHeader(int extra_info_spec, const std::string& name) {
   return !(extra_info_spec & ExtraInfoSpec::EXTRA_HEADERS) &&
          base::LowerCaseEqualsASCII(name, "set-cookie");
+}
+
+bool ArePublicSessionRestrictionsEnabled() {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  if (chromeos::LoginState::IsInitialized()) {
+    return chromeos::LoginState::Get()->ArePublicSessionRestrictionsEnabled();
+  }
+#elif BUILDFLAG(IS_CHROMEOS_LACROS)
+  DCHECK(chromeos::LacrosChromeServiceImpl::Get());
+  return chromeos::LacrosChromeServiceImpl::Get()
+             ->init_params()
+             ->session_type == crosapi::mojom::SessionType::kPublicSession;
+#endif
+  return false;
 }
 
 }  // namespace extension_web_request_api_helpers
