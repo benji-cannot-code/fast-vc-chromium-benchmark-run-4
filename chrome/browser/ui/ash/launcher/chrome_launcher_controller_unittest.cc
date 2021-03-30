6b571359bff46a7cdb638b64eb8b8a10cab0f343
Ian Clelland <iclelland@chromium.org>
2021-03-30 19:14:18 +0000

    Revert "Reland "Add test util method AwaitStartWebAppProvider for common case.""
    This reverts commit f5afb821e8cc015d7bf716f2e7ccac5db221b6af.
    
    Reason for revert: Reverting the reland :(
    
    The tests:
    AppListSyncableServiceTest.AddPageBreakItems
    AppListSyncableServiceTest.PruneRedundantPageBreakItems
    AppSearchProviderTest.FilterDuplicate
    CrostiniAppTest.AppInstallation
    WebAppBuilderTest.WebAppList
    
    All started failing intermittently after this reland.
    
    See build 13126 to 13138 on
    https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome
    
    
    Original change's description:
    > Reland "Add test util method AwaitStartWebAppProvider for common case."
    >
    > Switch to using TestSystemWebAppManager (as in previous patchset of OCL)
    > instead of flags that are indirect and must be set before profile is
    > created.
    >
    > OCL: crrev.com/c/2781231
    > Revert: crrev.com/c/2787312
    >
    > Original change's description:
    > > Add test util method AwaitStartWebAppProvider for common case.
    > >
    > > Unit tests (especially outside of web_apps code) often need a web app
    > > provider running with default subsystems. Added a simple util method to
    > > do that, including ensuring the startup finishes before we continue with
    > > the test to avoid flakes (or RunUntilIdle calls).
    > >
    > > Includes using a TestSystemWebAppManager to avoid installing a bunch of
    > > default apps on start().
    > >
    > > Also moves the test util methods into a test namespace so they're more
    > > distinguishable from local methods.
    > >
    > > In prep for adding another usage.
    >
    > Change-Id: Ia138122805b5ac69099f6739a181abb2b75abe89
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2789998
    > Commit-Queue: Glen Robertson <glenrob@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#867341}
    
    Change-Id: Ibf900df3672d42e5e999a29c56827705824defb7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2795751
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Owners-Override: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867718}

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller_unittest.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller_unittest.cc
@@ -5082,7 +5082,13 @@ class ChromeLauncherControllerWebAppTest : public ChromeLauncherControllerTest {
   ~ChromeLauncherControllerWebAppTest() override = default;
 
   void MaybeStartWebAppProvider() override {
-    web_app::test::AwaitStartWebAppProviderAndSubsystems(profile());
+    auto system_web_app_manager =
+        std::make_unique<web_app::TestSystemWebAppManager>(profile());
+
+    auto* provider = web_app::TestWebAppProvider::Get(profile());
+    provider->SetSystemWebAppManager(std::move(system_web_app_manager));
+    provider->SetRunSubsystemStartupTasks(true);
+    provider->Start();
   }
 };
 
@@ -5093,8 +5099,8 @@ TEST_F(ChromeLauncherControllerWebAppTest, WebAppPinRunUnpinClose) {
 
   InitLauncherController();
 
-  const web_app::AppId app_id = web_app::test::InstallDummyWebApp(
-      profile(), kWebAppName, GURL(kWebAppUrl));
+  const web_app::AppId app_id =
+      web_app::InstallDummyWebApp(profile(), kWebAppName, GURL(kWebAppUrl));
   base::RunLoop().RunUntilIdle();
 
   // The model should only contain the browser shortcut item.
