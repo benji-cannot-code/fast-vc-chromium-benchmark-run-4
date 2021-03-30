22c3dcb3cf1c059fe036c998133016cf2ed9164d
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2021-03-30 15:47:31 +0000

    Do not treat null tabs as interactable for geolocation.
    This makes Chrome's GeolocationPermissionContextDelegate consistent
    with WebLayer's, and with Chrome's and WebLayer's
    NfcPermissionContextDelegate.
    
    Change-Id: I4f06a848e3fe75d678ef2e634f512c81d616d7ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794365
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867621}

--- chrome/browser/geolocation/geolocation_permission_context_delegate_android.cc
+++ chrome/browser/geolocation/geolocation_permission_context_delegate_android.cc
@@ -55,7 +55,7 @@ bool GeolocationPermissionContextDelegateAndroid::DecidePermission(
 bool GeolocationPermissionContextDelegateAndroid::IsInteractable(
     content::WebContents* web_contents) {
   TabAndroid* tab = TabAndroid::FromWebContents(web_contents);
-  return !tab || tab->IsUserInteractable();
+  return tab && tab->IsUserInteractable();
 }
 
 PrefService* GeolocationPermissionContextDelegateAndroid::GetPrefs(
