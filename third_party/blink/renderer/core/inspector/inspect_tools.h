4686fb1ed0c86e1ba2f2035fa4c29dc5969186c9
Ian Clelland <iclelland@chromium.org>
2021-03-30 13:21:31 +0000

    Revert "Add an overlay to visualise scroll snapping"
    This reverts commit c0b0c484f08d1bd8f37801dc7e7a5b6694f13a88.
    
    Reason for revert: New test fails on some Mac CI bots.
    The first failures were seen at
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.12%20Tests/42669
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.13%20Tests/36227
    
    as well as subsequent builds on those two bots.
    
    Original change's description:
    > Add an overlay to visualise scroll snapping
    >
    > This CL adds a new overlay to visualise CSS scroll snapping. The overlay
    > is implemented as a persistent one and one can enable or disable it
    > using scroll-snap adorners in DevTools. The implementation is similar
    > to other persistent overlays such as grid and flexbox overlays.
    >
    > Depends on: crrev.com/c/2762146, crrev.com/c/2759704
    > Design doc: https://docs.google.com/document/d/1P1AmuxVpPuSMerPpK4XuWpqKGYqGlvN_G5g5ZQJwetY
    >
    > Screenshot: https://i.imgur.com/SZ6zkVm.png
    > Bug: chromium:862450
    > Change-Id: I2d713761a641a5fd98edaf627a05253cb1566565
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2752877
    > Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#867539}
    
    Bug: chromium:862450
    Change-Id: I8ede444bd52241ad0a479aeb46cc71a96f9290a9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2792188
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Owners-Override: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867580}

--- third_party/blink/renderer/core/inspector/inspect_tools.h
+++ third_party/blink/renderer/core/inspector/inspect_tools.h
@@ -143,24 +143,19 @@ class SourceOrderTool : public InspectTool {
 
 // -----------------------------------------------------------------------------
 
-using GridConfigs = Vector<
+using GirdConfigs = Vector<
     std::pair<Member<Node>, std::unique_ptr<InspectorGridHighlightConfig>>>;
 using FlexContainerConfigs =
     Vector<std::pair<Member<Node>,
                      std::unique_ptr<InspectorFlexContainerHighlightConfig>>>;
-using ScrollSnapConfigs = Vector<
-    std::pair<Member<Node>,
-              std::unique_ptr<InspectorScrollSnapContainerHighlightConfig>>>;
-
 class PersistentTool : public InspectTool {
   using InspectTool::InspectTool;
 
  public:
   void Draw(float scale) override;
   bool IsEmpty();
-  void SetGridConfigs(GridConfigs);
+  void SetGridConfigs(GirdConfigs);
   void SetFlexContainerConfigs(FlexContainerConfigs);
-  void SetScrollSnapConfigs(ScrollSnapConfigs);
 
   std::unique_ptr<protocol::DictionaryValue> GetGridInspectorHighlightsAsJson()
       const;
@@ -171,9 +166,8 @@ class PersistentTool : public InspectTool {
   bool HideOnHideHighlight() override;
   String GetOverlayName() override;
 
-  GridConfigs grid_node_highlights_;
+  GirdConfigs grid_node_highlights_;
   FlexContainerConfigs flex_container_configs_;
-  ScrollSnapConfigs scroll_snap_configs_;
   DISALLOW_COPY_AND_ASSIGN(PersistentTool);
 };
 
