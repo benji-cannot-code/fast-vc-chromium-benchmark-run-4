0743e45d09f0fbcca8e58b1cb2be390be1d28b11
Victor Vasiliev <vasilvv@chromium.org>
2021-03-30 03:39:14 +0000

    Roll src/net/third_party/quiche/src/ 4cf427b9d..f0bc77d7b (26 commits)
    https://quiche.googlesource.com/quiche.git/+log/4cf427b9d36e..f0bc77d7b5f4
    
    $ git log 4cf427b9d..f0bc77d7b --date=short --no-merges --format='%ad %ae %s'
    2021-03-25 danzh Add connection option kRVCM to enable reverse path validation upon new client address on server side.
    2021-03-25 bnc Remove Http2StrAppend, Http2StringPrintf, Http2HexEscape, Http2Hex from platform.
    2021-03-25 bnc Remove unused SpdyMemSlice.
    2021-03-25 bnc Resize HttpDecoder::buffer_ when buffering data.
    2021-03-24 quiche-dev Initial implementation of a Chromium-based Http2Adapter.
    2021-03-24 fayang Add two QUIC_BUGs to debug b/182571515.
    2021-03-24 quiche-dev Removes some virtual methods from Http2Adapter.
    2021-03-24 vasilvv Unify the way in which streams are marked unblocked after being converted from the pending status.
    2021-03-24 bnc Remove SpdyStrAppend, SpdyHexEncodeUInt32AndTrim, SpdyStringPieceCaseHash, and SpdyStringPieceCaseEq from spdy/platform.
    2021-03-24 bnc Remove unused HostIsLittleEndian(), fix comment.
    2021-03-24 quiche-dev For convenience, adds a matcher that considers only frame types.
    2021-03-24 quiche-dev Minor API changes to Http2Adapter and friends to ease implementation.
    2021-03-23 danzh Set chrome_value of --gfe2_reloadable_flag_quic_send_path_response2 to true. This value was set to false to unblock code merge, but didn't flip back afterwards.
    2021-03-23 bnc Remove HpackHeaderTable::GetByIndex().
    2021-03-23 haoyuewang Internal QUICHE change
    2021-03-23 haoyuewang Internal QUICHE change
    2021-03-23 bnc Use std::vector instead of std::deque for HPACK and QPACK static table.
    2021-03-23 bnc Move string in HpackDecoderDynamicTable::Insert().
    2021-03-23 bnc Remove HpackEntry::InsertionIndex().
    2021-03-23 quiche-dev Fix a couple small incompatibilities with header modules, and update the comment around where they're disabled to reflect that test macros, not logging macros, are the outstanding concern.
    2021-03-23 ianswett Deprecate --gfe2_reloadable_flag_quic_stateless_reset.
    2021-03-23 quiche-dev Adds a MockHttp2Visitor, for use in tests.
    2021-03-23 quiche-dev Ports a test utility from //gfe/gfe2/test_tools to //third_party/http2/adapter.
    2021-03-23 quiche-dev Remove V2 family of GFE_BUG (and friends).
    2021-03-22 quiche-dev Deprecate send_quic_fallback_server_config_on_leto_error
    2021-03-22 quiche-dev Moves //gfe/gfe2/http2:window_manager to //third_party/http2/adapter.
    
    Created with:
      roll-dep src/net/third_party/quiche/src
    
    R=dschinazi@chromium.org
    
    Change-Id: I5b10d320c239059a0565b2d0bf691de9879f0be9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791941
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867447}

--- net/quic/quic_event_logger.cc
+++ net/quic/quic_event_logger.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/quic/quic_event_logger.h"
 
+#include "base/strings/string_number_conversions.h"
 #include "net/cert/x509_certificate.h"
 #include "net/log/net_log_values.h"
 #include "net/quic/address_utils.h"
