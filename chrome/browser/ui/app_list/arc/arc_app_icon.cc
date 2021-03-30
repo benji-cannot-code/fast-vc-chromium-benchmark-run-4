81c67fd44630eebe80fb1de588700400d01921d3
Nancy Wang <nancylingwang@chromium.org>
2021-03-30 05:13:24 +0000

    Reusing a shared image decoder process for ARC icon loading.
    Generate a static DataDecoder to share the image decoder process for ARC
    icon loading process, based on the comments from
    https://bugs.chromium.org/p/chromium/issues/detail?id=1185184#c27
    https://bugs.chromium.org/p/chromium/issues/detail?id=1185184#c35
    
    BUG=1185184
    
    Change-Id: I5d4837fd5aa6467fc84d7bf07224a2a6f0128b97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2788168
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867478}

--- chrome/browser/ui/app_list/arc/arc_app_icon.cc
+++ chrome/browser/ui/app_list/arc/arc_app_icon.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/macros.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/no_destructor.h"
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/image_decoder/image_decoder.h"
@@ -27,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/component_extension_resources.h"
 #include "content/public/browser/browser_thread.h"
 #include "extensions/grit/extensions_browser_resources.h"
+#include "services/data_decoder/public/cpp/data_decoder.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/geometry/size.h"
@@ -36,6 +38,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 bool disable_safe_decoding_for_testing = false;
+
+data_decoder::DataDecoder& GetDataDecoder() {
+  static base::NoDestructor<data_decoder::DataDecoder> data_decoder;
+  return *data_decoder;
+}
 
 }  // namespace
 
@@ -169,7 +176,8 @@ ArcAppIcon::DecodeRequest::DecodeRequest(
     bool retain_padding,
     gfx::ImageSkia& image_skia,
     std::map<ui::ScaleFactor, base::Time>& incomplete_scale_factors)
-    : host_(host),
+    : ImageRequest(&GetDataDecoder()),
+      host_(host),
       descriptor_(descriptor),
       resize_allowed_(resize_allowed),
       retain_padding_(retain_padding),
