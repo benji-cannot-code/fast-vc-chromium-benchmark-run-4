e5cdf9c68eaeb977815987159b2804ec4dabdcb2
Sinan Sahin <sinansahin@google.com>
2021-03-30 20:10:21 +0000

    [Offline indicator v2] Fix invalid top controls min-height offsets
    If the top controls height and min-height changed with animation and
    the change in height was bigger than the min-height, the top controls
    min-height offset would get an invalid value that is greater than the
    min-height itself.
    
    This CL clamps the min-height offset to the animation [start, end] range
    to fix the issue.
    
    Bug: 1184902
    Change-Id: I0f5ea7fef6cd90ef18d48d476e5a21d5acca07c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2783357
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#867748}

--- cc/input/browser_controls_offset_manager.h
+++ cc/input/browser_controls_offset_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_INPUT_BROWSER_CONTROLS_OFFSET_MANAGER_H_
 
 #include <memory>
+#include <utility>
 
 #include "base/time/time.h"
 #include "cc/input/browser_controls_state.h"
@@ -118,6 +119,8 @@ class CC_EXPORT BrowserControlsOffsetManager {
   void InitAnimationForHeightChange(Animation* animation,
                                     float start_ratio,
                                     float stop_ratio);
+  void SetTopMinHeightOffsetAnimationRange(float from, float to);
+  void SetBottomMinHeightOffsetAnimationRange(float from, float to);
 
   // The client manages the lifecycle of this.
   BrowserControlsOffsetManagerClient* client_;
@@ -156,6 +159,15 @@ class CC_EXPORT BrowserControlsOffsetManager {
   // Current top/bottom controls min-height.
   float top_controls_min_height_offset_;
   float bottom_controls_min_height_offset_;
+
+  // Minimum and maximum values |top_controls_min_height_offset_| can take
+  // during the current min-height change animation.
+  base::Optional<std::pair<float, float>>
+      top_min_height_offset_animation_range_;
+  // Minimum and maximum values |bottom_controls_min_height_offset_| can take
+  // during the current min-height change animation.
+  base::Optional<std::pair<float, float>>
+      bottom_min_height_offset_animation_range_;
 
   // Class that holds and manages the state of the controls animations.
   class Animation {
