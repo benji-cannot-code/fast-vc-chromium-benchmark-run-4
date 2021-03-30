a1ddd1dd94f963fae0e6c429e083be72b08c3feb
Jiawei Li <lijiawei@chromium.org>
2021-03-30 00:15:53 +0000

    [chromecast] Ensures bindings are injected for main frame
    Bindings scripts injection change:
    
    1. Removed OnPageLoading() from |ReadyToCommitNavigation|.
    2. Now |CastWebContentsImpl| will inject the bindings scripts while
       ReadyToCommitNavigation is invoked. Previously, BindingsManagerCast
       listens on PageState::LOADING and that could fail in corner case.
       (e.g. Multiple same LOADING events are ignored due to CastWebContents's
       lifecycle management logic, thus BindingsManagerCast does not request
       to inject required bindings scripts.)
    
    PageState::LOADED logic change:
    To make PageState::LOADED actually means the completion of navigations and
    main frame's document resource is fully loaded. This CL does:
    
    1. We only send out PageState::LOADED once and only once for a
       successful page loading.
    2. Added an |active_navigation_| member to |CastWebContentsImpl| to
       indicate the main frame has pending navigation.
    3. On |DidFinishLoad|, if there is the pending navigation, we will wait
       that being completed first (i.e. waiting another/more DidFinishLoad).
    
    Merge-With: eureka-internal/552843
    
    Bug: Internal b/175623201, b/152813213
    Test: CastWebContentsBrowserTest
    Change-Id: I304db9b0897ae2fb55aa96f80162571b3e3ed863
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2759810
    Commit-Queue: Jiawei Li <lijiawei@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867356}

--- chromecast/browser/cast_web_contents_impl.h
+++ chromecast/browser/cast_web_contents_impl.h
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/blink/public/mojom/favicon/favicon_url.mojom-forward.h"
 
+namespace content {
+class NavigationHandle;
+}  // namespace content
+
 namespace chromecast {
 
 namespace shell {
@@ -70,7 +74,6 @@ class CastWebContentsImpl : public CastWebContents,
   void EnableBackgroundVideoPlayback(bool enabled) override;
   on_load_script_injector::OnLoadScriptInjectorHost<std::string>*
   script_injector() override;
-  void InjectScriptsIntoMainFrame() override;
   void PostMessageToMainFrame(
       const std::string& target_origin,
       const std::string& data,
@@ -165,7 +168,12 @@ class CastWebContentsImpl : public CastWebContents,
   bool is_websql_enabled_;
   bool is_mixer_audio_enabled_;
   base::TimeTicks start_loading_ticks_;
+
+  // True once the main frame finishes loading and there are no outstanding
+  // navigations.
   bool main_frame_loaded_;
+  content::NavigationHandle* active_navigation_ = nullptr;
+
   bool closing_;
   bool stopped_;
   bool stop_notified_;
