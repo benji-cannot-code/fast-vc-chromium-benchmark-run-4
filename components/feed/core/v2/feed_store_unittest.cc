237a46e049ed63b61989e82258011b9141346e56
Takashi Sakamoto <tasak@google.com>
2021-03-30 17:38:38 +0000

    Fix flaky FeedStoreTest.OverwriteStream and OverwriteStreamWebFeed failure.
    The tests expects kTestTimeEpoch == base::Time::UnixEpoch().
    Since FeedApiTest::SetUp() set kTestTimeEpoch to be Now(), FeedApiTest::TearDown() must reset kTestTimeEpoch to be base::Time::UnixEpoch().
    
    Change-Id: Icbb942e454601f71d3c37c0e2f19eb83436654b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2777954
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867668}

--- components/feed/core/v2/feed_store_unittest.cc
+++ components/feed/core/v2/feed_store_unittest.cc
@@ -125,6 +125,8 @@ TEST_F(FeedStoreTest, InitFailure) {
 TEST_F(FeedStoreTest, OverwriteStream) {
   MakeFeedStore({});
   CallbackReceiver<bool> receiver;
+  // TODO(harringtond): find a long term fix for assumptions about
+  // kTestTimeEpoch value.
   store_->OverwriteStream(kForYouStream, MakeTypicalInitialModelState(),
                           receiver.Bind());
   fake_db_->UpdateCallback(true);
@@ -244,6 +246,8 @@ TEST_F(FeedStoreTest, OverwriteStream) {
 TEST_F(FeedStoreTest, OverwriteStreamWebFeed) {
   MakeFeedStore({});
   CallbackReceiver<bool> receiver;
+  // TODO(harringtond): find a long term fix for assumptions about
+  // kTestTimeEpoch value.
   store_->OverwriteStream(kWebFeedStream, MakeTypicalInitialModelState(),
                           receiver.Bind());
   fake_db_->UpdateCallback(true);
