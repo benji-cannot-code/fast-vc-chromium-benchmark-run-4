28496fba2c61bf3deb02431d6d61c22fa0ca76ce
Evan Stade <estade@chromium.org>
2021-03-30 20:48:32 +0000

    WebLayer: allow routing of console.log to adb logs, via manifest entry.
    Android Chrome disables this by default but allows it to be enabled
    via a feature flag. This manifest entry is mostly equivalent to the
    flag, but is more ergonomic (can be used on Android user builds) and
    allows us to enable by default for weblayer shell and weblayer tests.
    
    Bug: none
    Change-Id: I081dbb953f78c6c1ba2c9ab11d8b89a85c0a983c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794818
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867780}

--- weblayer/browser/background_fetch/background_fetch_delegate_factory.cc
+++ weblayer/browser/background_fetch/background_fetch_delegate_factory.cc
@@ -14,8 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "weblayer/browser/host_content_settings_map_factory.h"
 
 #if defined(OS_ANDROID)
-#include "base/android/jni_android.h"
-#include "weblayer/browser/java/jni/BackgroundFetchHelper_jni.h"
+#include "weblayer/browser/android/application_info_helper.h"
 #endif
 
 namespace weblayer {
@@ -23,8 +22,9 @@ namespace weblayer {
 // static
 bool BackgroundFetchDelegateFactory::IsEnabled() {
 #if defined(OS_ANDROID)
-  static bool enabled = Java_BackgroundFetchHelper_isEnabled(
-      base::android::AttachCurrentThread());
+  static bool enabled = GetApplicationMetadataAsBoolean(
+      "org.chromium.weblayer.ENABLE_BACKGROUND_FETCH",
+      /*defaultValue=*/false);
   return enabled;
 #else
   return true;
