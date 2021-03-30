b5d1c857bcb0c4b51dc6dc89e0b6caf1b35edeb8
Akihiro Ota <akihiroota@chromium.org>
2021-03-30 22:09:07 +0000

    ChromeVox Tutorial: Miscellaneous fixes to support touch tutorial.
    This change does two things to support the newly added touch lessons:
    1. Adds touch-specific header descriptions. These are read when main
    headers in the tutorial UI are focused. E.g. when focusing the main
    tutorial header, ChromeVox will read "Swipe left or right with one
    finger to browse topics."
    2. Adds ability to hide/show main menu buttons depending on the
    associated interaction medium. This is important because we don't want
    to expose entry points into keyboard-specific lessons if we are on
    a touch device.
    
    Fixed: 1183896
    AX-Relnotes: N/A
    Change-Id: I74716aab480f619cb0f42ab5643a7dc5d68d5ae8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2783144
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/master@{#867822}

--- chrome/browser/resources/chromeos/accessibility/common/tutorial/constants.js
+++ chrome/browser/resources/chromeos/accessibility/common/tutorial/constants.js
@@ -55,7 +55,8 @@ export let LessonData;
  * Defines a type for data used to generate main menu buttons.
  * @typedef {{
  *    title: string,
- *    curriculum: Curriculum
+ *    curriculum: Curriculum,
+ *    medium: InteractionMedium,
  * }}
  */
 export let MainMenuButtonData;
