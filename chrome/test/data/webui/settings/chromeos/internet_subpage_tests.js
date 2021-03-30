8a770f0e4e90a810cfa871a07b67eab02935a7e8
Claude van der Merwe <cvandermerwe@google.com>
2021-03-30 13:47:37 +0000

    [eSim] Show banner when device is inhibited
    This CL adds cellular-banner to the internet-subpage. The
    banner is shown when the device is inhibited.
    
    Screenshot: https://screenshot.googleplex.com/66rnpVzezTvAwd7
    
    Bug: 1093185
    Change-Id: I8306412e0446e4a4683f34704ebd2981a2ec46b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782211
    Commit-Queue: Claude van der Merwe <cvandermerwe@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867588}

--- chrome/test/data/webui/settings/chromeos/internet_subpage_tests.js
+++ chrome/test/data/webui/settings/chromeos/internet_subpage_tests.js
@@ -218,6 +218,10 @@ suite('InternetSubpage', function() {
       };
 
       return flushAsync().then(async () => {
+        // Banner should not exist if device is not inhibited.
+        const cellularBanner = internetSubpage.$$('cellular-banner');
+        assertFalse(!!cellularBanner);
+
         const addCellularButton = internetSubpage.$$('#addCellularButton');
         assertTrue(!!addCellularButton);
 
@@ -229,7 +233,7 @@ suite('InternetSubpage', function() {
     });
 
     test(
-        'Device inhibited disables toggle and add cellular network',
+        'Device inhibited disables toggle, add cellular and shows banner',
         async () => {
           initSubpage(true);
           const mojom = chromeos.networkConfig.mojom;
@@ -256,6 +260,9 @@ suite('InternetSubpage', function() {
               internetSubpage.$$('#deviceEnabledButton');
           assertTrue(!!deviceEnabledButton);
           assertTrue(deviceEnabledButton.disabled);
+
+          const cellularBanner = internetSubpage.$$('cellular-banner');
+          assertTrue(!!cellularBanner);
         });
 
     test(
