db3c799754e2ed875138691df4c06560d6089e3f
Mohsen Izadi <mohsen@chromium.org>
2021-03-30 15:41:04 +0000

    Remove unnecessary sort in LatencyTracker
    LatencyTracker sorts its list of latency infos before reporting metrics
    on them. This has been added to accommodate calculation of average lag
    metrics (r618576). However, average lag tracking has been decoupled from
    latency info and LatencyTracker (r785975) since, so there is no more
    need for the sort.
    
    Bug: 1192116
    Change-Id: Icde52dce029469f1c3f9725a2f7aa85fa9c229c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782767
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867610}

--- ui/latency/latency_tracker.cc
+++ ui/latency/latency_tracker.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/latency/latency_tracker.h"
 
-#include <algorithm>
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
@@ -51,10 +50,6 @@ bool IsInertialScroll(const LatencyInfo& latency) {
   return latency.source_event_type() == ui::SourceEventType::INERTIAL;
 }
 
-bool LatencyTraceIdCompare(const LatencyInfo& i, const LatencyInfo& j) {
-  return i.trace_id() < j.trace_id();
-}
-
 }  // namespace
 
 LatencyTracker::LatencyTracker() = default;
@@ -63,42 +58,35 @@ LatencyTracker::~LatencyTracker() = default;
 void LatencyTracker::OnGpuSwapBuffersCompleted(
     std::vector<ui::LatencyInfo> latency_info,
     bool top_controls_visible_height_changed) {
-  // Sort latency_info as they can be in incorrect order.
-  std::sort(latency_info.begin(), latency_info.end(), LatencyTraceIdCompare);
-  for (const auto& latency : latency_info)
-    OnGpuSwapBuffersCompleted(latency, top_controls_visible_height_changed);
-}
+  for (const auto& latency : latency_info) {
+    base::TimeTicks gpu_swap_end_timestamp;
+    if (!latency.FindLatency(INPUT_EVENT_LATENCY_FRAME_SWAP_COMPONENT,
+                             &gpu_swap_end_timestamp)) {
+      continue;
+    }
 
-void LatencyTracker::OnGpuSwapBuffersCompleted(
-    const LatencyInfo& latency,
-    bool top_controls_visible_height_changed) {
-  base::TimeTicks gpu_swap_end_timestamp;
-  if (!latency.FindLatency(INPUT_EVENT_LATENCY_FRAME_SWAP_COMPONENT,
-                           &gpu_swap_end_timestamp)) {
-    return;
-  }
+    base::TimeTicks gpu_swap_begin_timestamp;
+    bool found_component = latency.FindLatency(
+        ui::INPUT_EVENT_GPU_SWAP_BUFFER_COMPONENT, &gpu_swap_begin_timestamp);
+    DCHECK_AND_RETURN_ON_FAIL(found_component);
 
-  base::TimeTicks gpu_swap_begin_timestamp;
-  bool found_component = latency.FindLatency(
-      ui::INPUT_EVENT_GPU_SWAP_BUFFER_COMPONENT, &gpu_swap_begin_timestamp);
-  DCHECK_AND_RETURN_ON_FAIL(found_component);
+    if (!latency.FindLatency(ui::INPUT_EVENT_LATENCY_BEGIN_RWH_COMPONENT,
+                             nullptr)) {
+      continue;
+    }
 
-  if (!latency.FindLatency(ui::INPUT_EVENT_LATENCY_BEGIN_RWH_COMPONENT,
-                           nullptr)) {
-    return;
-  }
-
-  ui::SourceEventType source_event_type = latency.source_event_type();
-  if (source_event_type == ui::SourceEventType::WHEEL ||
-      source_event_type == ui::SourceEventType::MOUSE ||
-      source_event_type == ui::SourceEventType::TOUCH ||
-      source_event_type == ui::SourceEventType::INERTIAL ||
-      source_event_type == ui::SourceEventType::KEY_PRESS ||
-      source_event_type == ui::SourceEventType::TOUCHPAD ||
-      source_event_type == ui::SourceEventType::SCROLLBAR) {
-    ComputeEndToEndLatencyHistograms(gpu_swap_begin_timestamp,
-                                     gpu_swap_end_timestamp, latency,
-                                     top_controls_visible_height_changed);
+    ui::SourceEventType source_event_type = latency.source_event_type();
+    if (source_event_type == ui::SourceEventType::WHEEL ||
+        source_event_type == ui::SourceEventType::MOUSE ||
+        source_event_type == ui::SourceEventType::TOUCH ||
+        source_event_type == ui::SourceEventType::INERTIAL ||
+        source_event_type == ui::SourceEventType::KEY_PRESS ||
+        source_event_type == ui::SourceEventType::TOUCHPAD ||
+        source_event_type == ui::SourceEventType::SCROLLBAR) {
+      ComputeEndToEndLatencyHistograms(gpu_swap_begin_timestamp,
+                                       gpu_swap_end_timestamp, latency,
+                                       top_controls_visible_height_changed);
+    }
   }
 }
 
