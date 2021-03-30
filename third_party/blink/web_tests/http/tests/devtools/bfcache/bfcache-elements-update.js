5f28a8681bea585998654be5e92a2a09bcfd38f1
Tim van der Lippe <tvanderlippe@chromium.org>
2021-03-30 15:39:56 +0000

    Use loadTestModule in BFCache
    R=szuend@chromium.org
    
    Bug: 1187573
    Change-Id: I57ade7c5b8c263c73aa93014b4f4dd2ad7e41cae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794385
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867608}

--- third_party/blink/web_tests/http/tests/devtools/bfcache/bfcache-elements-update.js
+++ third_party/blink/web_tests/http/tests/devtools/bfcache/bfcache-elements-update.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 (async function() {
   TestRunner.addResult(`Tests that elements tab updates after a bf-cache navigation.\n`);
-  await TestRunner.loadModule('elements_test_runner');
+  await TestRunner.loadTestModule('elements_test_runner');
   await TestRunner.showPanel('elements');
 
   // Save time on style updates.
