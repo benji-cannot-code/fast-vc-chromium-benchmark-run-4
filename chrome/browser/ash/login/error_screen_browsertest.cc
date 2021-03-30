73d6f8ca9dd6fb31b9a0cf84fa2e201141cb8f86
Akihiro Ota <akihiroota@chromium.org>
2021-03-30 22:14:37 +0000

    Reland "ChromeVox hint: Give hint after 20 seconds of idling"
    This is a reland of 4066cc8a39ad8e958aba630bb8e58c6eebf71bb7
    
    Original change's description:
    > ChromeVox hint: Give hint after 20 seconds of idling
    >
    > Currently, the ChromeVox hint is given 20 seconds after the OOBE
    > welcome screen is shown. The only way to cancel the timer and prevent
    > the hint is to click one of the three on-screen buttons. This leads to
    > many people receiving the hint when they don't need it.
    >
    > After discussion with the UI/UX team about the timing of the
    > announcement, we have decided to give the ChromeVox hint after 20
    > seconds of idling. This will help us make a more informed decision
    > about giving the ChromeVox hint, as opposed to the naive approach of
    > giving it after 20 seconds.
    >
    > --gtest_filter="WelcomeScreenChromeVoxHint*.*"
    > Also ran manual tests to ensure the hint is given 20s after idle.
    > seconds of idling, as opposed to 20 seconds after device boot up.
    >
    > Fixed: 1186502
    > Test: out/cros/browser_tests
    > Change-Id: I38eca6b0f7d6c66776bcd2e8e62730250efa0a43
    > AX-Relnotes: The ChromeVox announcement in OOBE now fires after 20
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2752922
    > Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    > Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#866300}
    
    Change-Id: I20aa454ba7ce816897458fdfe30c9bdebcd6d97e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2792064
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867824}

--- chrome/browser/ash/login/error_screen_browsertest.cc
+++ chrome/browser/ash/login/error_screen_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ash/constants/ash_switches.h"
 #include "ash/public/cpp/login_screen_test_api.h"
 #include "base/run_loop.h"
 #include "base/strings/strcat.h"
@@ -77,6 +78,12 @@ class NetworkErrorScreenTest : public InProcessBrowserTest {
 
     ShowLoginWizard(WelcomeView::kScreenId);
     OobeScreenWaiter(WelcomeView::kScreenId).Wait();
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    command_line->AppendSwitch(
+        chromeos::switches::kDisableOOBEChromeVoxHintTimerForTesting);
+    InProcessBrowserTest::SetUpCommandLine(command_line);
   }
 
   void ShowErrorScreenWithNetworkList() {
