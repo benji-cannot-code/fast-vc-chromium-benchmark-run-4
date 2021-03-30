b3e2ce6e8e7e49129a4f66d5031ef70cf1d6c808
Yoav Weiss <yoavweiss@chromium.org>
2021-03-30 18:38:16 +0000

    [devtools-cwv] Set preloads to non-blocking
    Currently markup based preloads are considered blocking [1].
    This CL makes sure that they're considered non-blocking, along with
    dynamically added preloads.
    
    [1] https://github.com/GoogleChrome/lighthouse/issues/2065#issuecomment-781672424
    
    Change-Id: Ie11b67be1992751342f16bb3aee62b519b9582ff
    
    Bug: 1193760
    Change-Id: Ie11b67be1992751342f16bb3aee62b519b9582ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791427
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867694}

--- third_party/blink/web_tests/inspector-protocol/timeline/render-blocking-style-frame.js
+++ third_party/blink/web_tests/inspector-protocol/timeline/render-blocking-style-frame.js
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
   // The number includes the frame, the 4 CSS files it loads directly, and the
   // one imported by them.
-  const numberOfURLs = 10;
+  const numberOfURLs = 11;
 
   var {page, session, dp} = await testRunner.startHTML(`
       <head></head>
