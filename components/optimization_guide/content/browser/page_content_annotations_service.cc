e172f685c30ec9f29bb3b18ab860f91d6343fb66
Sophie Chang <sophiechang@chromium.org>
2021-03-30 23:31:56 +0000

    Add feature param for whether to write content annotations to History
    Bug: 1177102
    Change-Id: I260644a593b4207c7c903764279323b8662adc7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2795971
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867867}

--- components/optimization_guide/content/browser/page_content_annotations_service.cc
+++ components/optimization_guide/content/browser/page_content_annotations_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/history_service.h"
 #include "components/optimization_guide/content/browser/optimization_guide_decider.h"
 #include "components/optimization_guide/core/optimization_guide_enums.h"
+#include "components/optimization_guide/core/optimization_guide_features.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 
@@ -66,6 +67,9 @@ void PageContentAnnotationsService::OnPageContentAnnotated(
       "OptimizationGuide.PageContentAnnotationsService.ContentAnnotated",
       content_annotations.has_value());
   if (!content_annotations)
+    return;
+
+  if (!features::ShouldWriteContentAnnotationsToHistoryService())
     return;
 
   history_service_->QueryURL(
