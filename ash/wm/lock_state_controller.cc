d7145c2e9112e04370f64d7f1899501068fd5206
Mitsuru Oshima <oshima@chromium.org>
2021-03-30 02:00:22 +0000

    Revert "Remove duplicated wallpaper update"
    This reverts commit 1273243b4d964132f38cb171e847e68f7d0650cc.
    
    Reason for revert: it caused visual side effect. i'll look into different approach, or better solution.
    
    Original change's description:
    > Remove duplicated wallpaper update
    >
    > This is already handled in WallpaperControllerImpl[1]
    >
    > [1] https://source.chromium.org/chromium/chromium/src/+/master:ash/wallpaper/wallpaper_controller_impl.cc;drc=06c5a3395c5f87161a7f3954f42668dec927345a;l=1433
    >
    > Bug: 1187766
    > Change-Id: I6a877f4be26be4467b406e76d844a0622f639c3e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2756944
    > Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#863168}
    
    Bug: 1187766
    Change-Id: Ia6af8d73a46726d23b14fbef6fcd65112cda09ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2786184
    Auto-Submit: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867411}

--- ash/wm/lock_state_controller.cc
+++ ash/wm/lock_state_controller.cc
@@ -444,6 +444,7 @@ void LockStateController::PostLockAnimationFinished() {
 }
 
 void LockStateController::UnlockAnimationAfterUIDestroyedFinished() {
+  Shell::Get()->wallpaper_controller()->UpdateWallpaperBlurForLockState(false);
   RestoreUnlockedProperties();
 }
 
