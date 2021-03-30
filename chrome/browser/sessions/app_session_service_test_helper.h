83f2d17cba1ddc8cb7feffe40fdccdd201da6945
Stanley Hon <stahon@microsoft.com>
2021-03-30 01:39:09 +0000

    Add AppSessionService but DO NOT enable PWA restore.
    This change adds AppSessionService but NOT the hooks required to have it be
    utilized for PWA restoration.
    
    Chrome OS is buildflag'd out, so there will be no changes on Chrome OS.
    
    AppSessionServiceFactory assists in the creation of AppSessionService.
    
    AppSessionService is used throughout the code to keep bookkeeping for
    apps and browsers separate.
    
    This change includes unit_tests.
    
    How to run unit_tests:
    
    1. autoninja unit_tests
    2. unit_tests --gtest_filter=*AppSessionService*
    
    Bug: 938759
    Change-Id: I7c170b1a3b66fa845213528dc5757ffea09df94a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2721002
    Commit-Queue: Stanley Hon <stahon@microsoft.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867402}

---
+++ chrome/browser/sessions/app_session_service_test_helper.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SESSIONS_APP_SESSION_SERVICE_TEST_HELPER_H_
+#define CHROME_BROWSER_SESSIONS_APP_SESSION_SERVICE_TEST_HELPER_H_
+
+#include "chrome/browser/sessions/session_service_base_test_helper.h"
+
+class Profile;
+class AppSessionService;
+
+class AppSessionServiceTestHelper : public SessionServiceBaseTestHelper {
+ public:
+  AppSessionServiceTestHelper();
+  explicit AppSessionServiceTestHelper(Profile* profile);
+  explicit AppSessionServiceTestHelper(AppSessionService* service);
+  AppSessionServiceTestHelper(const AppSessionServiceTestHelper&) = delete;
+  AppSessionServiceTestHelper& operator=(const AppSessionServiceTestHelper&) =
+      delete;
+  ~AppSessionServiceTestHelper() = default;
+
+  void SetService(AppSessionService* service);
+  AppSessionService* service() { return app_session_service_; }
+
+ private:
+  AppSessionService* app_session_service_;
+};
+
+#endif  // CHROME_BROWSER_SESSIONS_APP_SESSION_SERVICE_TEST_HELPER_H_
