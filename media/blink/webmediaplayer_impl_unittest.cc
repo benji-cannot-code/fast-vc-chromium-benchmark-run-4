aa27744f90534e346969a0a5f19e5051de06af84
Dale Curtis <dalecurtis@chromium.org>
2021-03-30 19:14:14 +0000

    Enable WebMediaPlayerImplBackgroundBehaviorTest.AudioVideo tests.
    These should be fixed on ToT
    
    R=cassew
    
    Fixed: 1177112
    Change-Id: I49438557e7f0b71911dd2711c8ee83878e4468fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2795227
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Will Cassella <cassew@google.com>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Will Cassella <cassew@google.com>
    Cr-Commit-Position: refs/heads/master@{#867717}

--- media/blink/webmediaplayer_impl_unittest.cc
+++ media/blink/webmediaplayer_impl_unittest.cc
@@ -2327,8 +2327,7 @@ TEST_P(WebMediaPlayerImplBackgroundBehaviorTest, VideoOnly) {
   EXPECT_EQ(should_pause, ShouldPausePlaybackWhenHidden());
 }
 
-// TODO(crbug.com/1177112) Re-enable test
-TEST_P(WebMediaPlayerImplBackgroundBehaviorTest, DISABLED_AudioVideo) {
+TEST_P(WebMediaPlayerImplBackgroundBehaviorTest, AudioVideo) {
   SetMetadata(true, true);
 
   // Optimization requirements are the same for all platforms.
