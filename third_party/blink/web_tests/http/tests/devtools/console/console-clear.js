5f18f59e27f8c0998940eec09778b0a029e5f743
Tim van der Lippe <tvanderlippe@chromium.org>
2021-03-30 10:18:14 +0000

    Use loadTestModule in last layout tests
    These are the last three tests that were still using loadModule for
    test_runner modules.
    
    R=aerotwist@chromium.org
    
    Bug: 1187573
    Change-Id: Ida261b4e1e11dfff15a4ae0d40ac621c9642fa0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791407
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867547}

--- third_party/blink/web_tests/http/tests/devtools/console/console-clear.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-clear.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   TestRunner.addResult("Tests that console is cleared upon requestClearMessages call.\n");
 
   await TestRunner.showPanel("console");
-  await TestRunner.loadModule("console_test_runner");
+  await TestRunner.loadTestModule("console_test_runner");
 
   await TestRunner.evaluateInPagePromise(`
     console.log("one");
