03f4427688fede6457ebeeeaef49ced3c3be10fa
Patrick Monette <pmonette@chromium.org>
2021-03-31 01:10:14 +0000

    WebRtcEventLogger: Pass GlobalFrameRoutingId by value
    GlobalFrameRoutingId is a small struct containing only 2 ints which
    means it is likely more efficient to pass by value.
    
    But more importantly, this means that parameter passing will be
    consistent between WebRtcEventLogger and WebRTCInternals. This is
    needed because in a subsequent CL, they will both implement the
    same observer interface.
    
    Change 5/7
    
    Bug: 1191026
    Change-Id: Id01486c8ac8ed86ed14cf277c7900ba0b336b57b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2770839
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#867915}

--- chrome/browser/media/webrtc/webrtc_event_log_manager.cc
+++ chrome/browser/media/webrtc/webrtc_event_log_manager.cc
@@ -232,19 +232,19 @@ void WebRtcEventLogManager::DisableForBrowserContext(
 }
 
 void WebRtcEventLogManager::OnPeerConnectionAdded(
-    const content::GlobalFrameRoutingId& frame_id,
+    content::GlobalFrameRoutingId frame_id,
     int lid) {
   OnPeerConnectionAdded(frame_id, lid, base::NullCallback());
 }
 
 void WebRtcEventLogManager::OnPeerConnectionRemoved(
-    const content::GlobalFrameRoutingId& frame_id,
+    content::GlobalFrameRoutingId frame_id,
     int lid) {
   OnPeerConnectionRemoved(frame_id, lid, base::NullCallback());
 }
 
 void WebRtcEventLogManager::OnPeerConnectionUpdated(
-    const content::GlobalFrameRoutingId& frame_id,
+    content::GlobalFrameRoutingId frame_id,
     int lid,
     const std::string& type,
     const std::string& value) {
@@ -255,7 +255,7 @@ void WebRtcEventLogManager::OnPeerConnectionUpdated(
 }
 
 void WebRtcEventLogManager::OnPeerConnectionSessionIdSet(
-    const content::GlobalFrameRoutingId& frame_id,
+    content::GlobalFrameRoutingId frame_id,
     int lid,
     const std::string& session_id) {
   OnPeerConnectionSessionIdSet(frame_id, lid, session_id, base::NullCallback());
@@ -271,7 +271,7 @@ void WebRtcEventLogManager::DisableLocalLogging() {
 }
 
 void WebRtcEventLogManager::OnWebRtcEventLogWrite(
-    const content::GlobalFrameRoutingId& frame_id,
+    content::GlobalFrameRoutingId frame_id,
     int lid,
     const std::string& message) {
   OnWebRtcEventLogWrite(frame_id, lid, message, base::NullCallback());
@@ -472,7 +472,7 @@ void WebRtcEventLogManager::RenderProcessHostExitedDestroyed(
 }
 
 void WebRtcEventLogManager::OnPeerConnectionAdded(
-    const content::GlobalFrameRoutingId& frame_id,
+    content::GlobalFrameRoutingId frame_id,
     int lid,
     base::OnceCallback<void(bool)> reply) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
@@ -509,7 +509,7 @@ void WebRtcEventLogManager::OnPeerConnectionAdded(
 }
 
 void WebRtcEventLogManager::OnPeerConnectionRemoved(
-    const content::GlobalFrameRoutingId& frame_id,
+    content::GlobalFrameRoutingId frame_id,
     int lid,
     base::OnceCallback<void(bool)> reply) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
@@ -535,7 +535,7 @@ void WebRtcEventLogManager::OnPeerConnectionRemoved(
 }
 
 void WebRtcEventLogManager::OnPeerConnectionStopped(
-    const content::GlobalFrameRoutingId& frame_id,
+    content::GlobalFrameRoutingId frame_id,
     int lid,
     base::OnceCallback<void(bool)> reply) {
   // From the logger's perspective, we treat stopping a peer connection the
@@ -545,7 +545,7 @@ void WebRtcEventLogManager::OnPeerConnectionStopped(
 }
 
 void WebRtcEventLogManager::OnPeerConnectionSessionIdSet(
-    const content::GlobalFrameRoutingId& frame_id,
+    content::GlobalFrameRoutingId frame_id,
     int lid,
     const std::string& session_id,
     base::OnceCallback<void(bool)> reply) {
@@ -606,7 +606,7 @@ void WebRtcEventLogManager::DisableLocalLogging(
 }
 
 void WebRtcEventLogManager::OnWebRtcEventLogWrite(
-    const content::GlobalFrameRoutingId& frame_id,
+    content::GlobalFrameRoutingId frame_id,
     int lid,
     const std::string& message,
     base::OnceCallback<void(std::pair<bool, bool>)> reply) {
