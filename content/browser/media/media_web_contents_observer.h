3bb0f8a05f120fc36d271577cad6018a7ed6e31a
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2021-03-31 10:15:26 +0000

    Create MediaPlayer and set up MediaPlayerObserver simultaneously
    Creating a MediaPlayer will now automatically initialize an initial
    MediaPlayerObserver instance so that the remote end can receive
    notifications since the very first moment the player is created.
    
    This replaces an earlier workaround made for OnMediaMetadataChanged()
    specifically.
    
    The new browser test CanvasCaptureControlledByMediaSession verifies the
    desired behavior.
    
    Bug: 1191332
    Change-Id: I849ef9f9ef10beedb69906f6aeea284f6f7846b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2781733
    Commit-Queue: Wojciech Dzierżanowski <wdzierzanowski@opera.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#868035}

--- content/browser/media/media_web_contents_observer.h
+++ content/browser/media/media_web_contents_observer.h
@@ -126,13 +126,6 @@ class CONTENT_EXPORT MediaWebContentsObserver : public WebContentsObserver {
       mojo::PendingAssociatedReceiver<media::mojom::MediaPlayerHost>
           player_receiver);
 
-  // Communicates with the MediaSessionControllerManager to find or create (if
-  // needed) a MediaSessionController identified by |player_id|, in order to
-  // bind its mojo remote for media::mojom::MediaPlayer.
-  void OnMediaPlayerAdded(
-      mojo::PendingAssociatedRemote<media::mojom::MediaPlayer> player_remote,
-      MediaPlayerId player_id);
-
   // Called by the WebContents when a tab has been closed but may still be
   // available for "undo" -- indicates that all media players (even audio only
   // players typically allowed background audio) bound to this WebContents must
@@ -146,8 +139,6 @@ class CONTENT_EXPORT MediaWebContentsObserver : public WebContentsObserver {
 
  private:
   class PlayerInfo;
-  friend class PlayerInfo;
-
   using PlayerInfoMap =
       base::flat_map<MediaPlayerId, std::unique_ptr<PlayerInfo>>;
 
@@ -169,6 +160,8 @@ class CONTENT_EXPORT MediaWebContentsObserver : public WebContentsObserver {
     // media::mojom::MediaPlayerHost implementation.
     void OnMediaPlayerAdded(
         mojo::PendingAssociatedRemote<media::mojom::MediaPlayer> media_player,
+        mojo::PendingAssociatedReceiver<media::mojom::MediaPlayerObserver>
+            media_player_observer,
         int32_t player_id) override;
 
    private:
@@ -187,8 +180,9 @@ class CONTENT_EXPORT MediaWebContentsObserver : public WebContentsObserver {
     ~MediaPlayerObserverHostImpl() override;
 
     // Used to bind the receiver via the BrowserInterfaceBroker.
-    mojo::PendingAssociatedRemote<media::mojom::MediaPlayerObserver>
-    BindMediaPlayerObserverReceiverAndPassRemote();
+    void BindMediaPlayerObserverReceiver(
+        mojo::PendingAssociatedReceiver<media::mojom::MediaPlayerObserver>
+            media_player_observer);
 
     // media::mojom::MediaPlayerObserver implementation.
     void OnMediaPlaying() override;
@@ -225,6 +219,15 @@ class CONTENT_EXPORT MediaWebContentsObserver : public WebContentsObserver {
   using MediaPlayerRemotesMap =
       base::flat_map<MediaPlayerId,
                      mojo::AssociatedRemote<media::mojom::MediaPlayer>>;
+
+  // Communicates with the MediaSessionControllerManager to find or create (if
+  // needed) a MediaSessionController identified by |player_id|, in order to
+  // bind its mojo remote for media::mojom::MediaPlayer.
+  void OnMediaPlayerAdded(
+      mojo::PendingAssociatedRemote<media::mojom::MediaPlayer> player_remote,
+      mojo::PendingAssociatedReceiver<media::mojom::MediaPlayerObserver>
+          media_player_observer,
+      MediaPlayerId player_id);
 
   // Returns the PlayerInfo associated with |id|, or nullptr if no such
   // PlayerInfo exists.
