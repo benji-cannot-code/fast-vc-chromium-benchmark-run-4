8fc4771fed74c1a76d32442cca1fe129bcf2b9be
Carlos Caballero <carlscab@google.com>
2021-03-31 16:00:28 +0000

    [mparch] Get rid of WCO::FrameDeleted use
    The RenderFrameHost instance will only be added to
    pending_render_frame_hosts_ if IsRenderFrameCreated() thus we can listen
    to RenderFrameDeleted for cleanup.
    
    
    Bug: 1188721
    Change-Id: Iaeed4141fd1c20c32841eb9be3b45eae0575b809
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2742520
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#868110}

--- components/safe_browsing/content/browser/threat_details.h
+++ components/safe_browsing/content/browser/threat_details.h
@@ -107,7 +107,7 @@ class ThreatDetails : public content::WebContentsObserver {
   void OnRedirectionCollectionReady();
 
   // WebContentsObserver implementation:
-  void FrameDeleted(content::RenderFrameHost* render_frame_host) override;
+  void RenderFrameDeleted(content::RenderFrameHost* render_frame_host) override;
   void RenderFrameHostChanged(content::RenderFrameHost* old_host,
                               content::RenderFrameHost* new_host) override;
 
