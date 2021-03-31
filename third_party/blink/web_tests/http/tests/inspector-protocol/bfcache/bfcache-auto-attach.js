4d700c6fc8a8b5005d1c277943f158ea96d8b772
Adithya Srinivasan <adithyas@chromium.org>
2021-03-31 15:07:37 +0000

    DevTools: Fix TargetAutoAttacher::UpdateFrames
    This fixes an existing bug where we don't attach targets for OOPIFs
    after a BFCache navigation.
    
    UpdateFrames can be called when a navigation is ready to commit
    (ReadyToCommitNavigation). At that point, render_frame_host_ is pending
    commit and its FTN points to a different (the current) RFH. This CL
    ensures we traverse |render_frame_host_|'s children directly instead of
    traversing its FTN's current RFH's children.
    
    Bug: 1191438
    Change-Id: Id4097b5cebeca5cae0d7b06854f302de27276d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782720
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868092}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/bfcache/bfcache-auto-attach.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startURL(
+    'http://localhost:8000/inspector-protocol/bfcache/resources/page-with-oopif.html',
+    'Tests that target for oopif is attached after BFCache navigation');
+  await dp.Page.enable();
+
+  let attachedToTargetPromise = dp.Target.onceAttachedToTarget();
+  await dp.Target.setAutoAttach({ autoAttach: true, waitForDebuggerOnStart: false, flatten: true });
+  let targetInfo = (await attachedToTargetPromise).params.targetInfo;
+  testRunner.log('OOPIF attached: ' + targetInfo.url);
+
+  const detachedFromTargetPromise = dp.Target.onceDetachedFromTarget();
+  await session.navigate('http://devtools.oopif.test:8000/inspector-protocol/resources/test-page.html');
+  await detachedFromTargetPromise;
+  testRunner.log('detached OOPIF');
+
+  attachedToTargetPromise = dp.Target.onceAttachedToTarget();
+  await session.evaluate('window.history.back()');
+  targetInfo = (await attachedToTargetPromise).params.targetInfo;
+  testRunner.log('OOPIF attached after BFCache navigation: ' + targetInfo.url);
+
+  testRunner.completeTest();
+});
