4686db966b93528c1d6613e000c91cff6b1cf5bc
ckitagawa <ckitagawa@chromium.org>
2021-03-31 19:05:53 +0000

    [CSN] Title Validator
    Adds validation to titles which
    - Replaces control characters with whitespace.
    - Collapses/trims whitespace.
    - Restricts length to 4096 chars.
    
    Eventually we should move this somewhere it can be shared and used
    throughout any title handling code in the codebase it may also be worth
    adapting it for WTF::String.
    
    This implementation is based on a hybrid of blink::Document
    CanonicalizedTitle and base::CollapseWhitespaceT.
    
    Bug: 1194567
    Change-Id: Ie1058daf7db41ad3f9e42593d97842614a0a0e8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2795464
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868209}

--- components/continuous_search/browser/search_result_extractor_client.cc
+++ components/continuous_search/browser/search_result_extractor_client.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/continuous_search/browser/search_result_extractor_client.h"
 
+#include "base/strings/utf_string_conversions.h"
+#include "components/continuous_search/common/title_validator.h"
 #include "components/google/core/common/google_util.h"
 #include "content/public/browser/web_contents.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
@@ -74,6 +76,14 @@ void SearchResultExtractorClient::RequestDataCallbackAdapter(
     std::move(callback).Run(SearchResultExtractorClientStatus::kUnexpectedUrl,
                             mojom::CategoryResults::New());
     return;
+  }
+
+  // Validate all the returned titles (URL already needs to be valid for mojom).
+  for (mojom::ResultGroupPtr& group : results->groups) {
+    for (mojom::SearchResultPtr& result : group->results) {
+      result->title =
+          base::UTF16ToUTF8(ValidateTitle(base::UTF8ToUTF16(result->title)));
+    }
   }
 
   // `url` and transitively `document_url` should always be a search URL. If
