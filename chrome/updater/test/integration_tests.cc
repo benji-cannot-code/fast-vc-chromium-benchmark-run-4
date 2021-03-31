0d0d84f4c8c2be7e90497e46f8f51b48f91f2373
Allie Zhao <allieyzhao@google.com>
2021-03-31 11:48:55 +0000

    Enable tests for IntegrationTestCommandsSystem on bots for mac.
    Bug:1186817
    
    Change-Id: If68b5859c6c86daf1a4d2fe753f90845f37032a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791859
    Commit-Queue: Allie Zhao <allieyzhao@google.com>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868052}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -45,9 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace updater {
 namespace test {
 
-// TODO(crbug.com/1096654): Enable tests for IntegrationTestCommandsSystem on
-// bots that support passwordless sudo.
-
 // TODO(crbug.com/1096654): Enable for system integration tests for Win.
 
 class IntegrationTest : public ::testing::Test {
