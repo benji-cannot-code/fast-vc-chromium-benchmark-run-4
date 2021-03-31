ace2086f28747547a6a56f2eaef660888177d9b3
Ramin Halavati <rhalavati@chromium.org>
2021-03-31 13:39:08 +0000

    Ensure Ephemeral Guest profiles do not fetch DICe access tokens.
    Ephemeral Guest profiles use DICe only for signin transfer from regular
    profiles and are not supposed to fetch access tokens for any other
    activity.
    
    Bug: 1173953
    Change-Id: I281b3628c9e0b92a1b0a5563127873b7895e3d3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784624
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868073}

--- components/signin/public/identity_manager/identity_manager.h
+++ components/signin/public/identity_manager/identity_manager.h
@@ -382,6 +382,8 @@ class IdentityManager : public KeyedService,
     ash::AccountManager* ash_account_manager = nullptr;
 #endif
 
+    bool allow_access_token_fetch = true;
+
     InitParameters();
     InitParameters(InitParameters&&);
     ~InitParameters();
@@ -697,6 +699,7 @@ class IdentityManager : public KeyedService,
   ash::AccountManager* ash_account_manager_ = nullptr;
 #endif
 
+  const bool allow_access_token_fetch_;
   DISALLOW_COPY_AND_ASSIGN(IdentityManager);
 };
 
