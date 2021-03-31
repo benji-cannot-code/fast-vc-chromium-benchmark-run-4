3cebae3f55e913d3f8e475a2a535e67647dd5b97
Miyoung Shin <myid.shin@igalia.com>
2021-03-31 01:54:30 +0000

    Use DEFINE_STATIC_LOCAL for OomInterventionImpl
    This CL uses DEFINE_STATIC_LOCAL for OomInterventionImpl that has
    a process-wide receiver instead of MakeSelfOwnedReceiver<T>.
    
    Bug: 1176590
    Change-Id: Iffa906aadb29d3063efc3b7751ba28198a219cb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2772672
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#867929}

--- third_party/blink/renderer/controller/oom_intervention_impl.h
+++ third_party/blink/renderer/controller/oom_intervention_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/common/oom_intervention/oom_intervention_types.h"
 #include "third_party/blink/public/mojom/oom_intervention/oom_intervention.mojom-blink.h"
@@ -28,11 +29,13 @@ class CONTROLLER_EXPORT OomInterventionImpl
     : public mojom::blink::OomIntervention,
       public MemoryUsageMonitor::Observer {
  public:
-  static void Create(
+  static void Bind(
       mojo::PendingReceiver<mojom::blink::OomIntervention> receiver);
 
   OomInterventionImpl();
   ~OomInterventionImpl() override;
+
+  void Reset();
 
   // mojom::blink::OomIntervention:
   void StartDetection(
@@ -73,6 +76,7 @@ class CONTROLLER_EXPORT OomInterventionImpl
   OomInterventionMetrics metrics_at_intervention_;
   int number_of_report_needed_ = 0;
   TaskRunnerTimer<OomInterventionImpl> delayed_report_timer_;
+  mojo::Receiver<mojom::blink::OomIntervention> receiver_{this};
 };
 
 }  // namespace blink
