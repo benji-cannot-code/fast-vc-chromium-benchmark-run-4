f97bc4df6b6dae1bd8d8875ac1a8025d078bfcb7
nedenwang <nedenwang@tencent.com>
2021-03-31 03:17:19 +0000

    [AW] Reset LocalMainFrame remote object if not speculative RFH created
    Bug: 1193439
    Change-Id: I30cf5e71c81b90881db72b8d2102fe54cb416b22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791044
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: cathie chen <cathiechen@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#867948}

--- android_webview/browser/renderer_host/aw_render_view_host_ext.h
+++ android_webview/browser/renderer_host/aw_render_view_host_ext.h
@@ -38,7 +38,6 @@ class AwRenderViewHostExtClient {
 class AwRenderViewHostExt : public content::WebContentsObserver,
                             mojom::FrameHost {
  public:
-
   // To send receive messages to a RenderView we take the WebContents instance,
   // as it internally handles RenderViewHost instances changing underneath us.
   AwRenderViewHostExt(
@@ -80,6 +79,8 @@ class AwRenderViewHostExt : public content::WebContentsObserver,
  private:
   // content::WebContentsObserver implementation.
   void RenderFrameCreated(content::RenderFrameHost* frame_host) override;
+  void RenderFrameHostChanged(content::RenderFrameHost* old_host,
+                              content::RenderFrameHost* new_host) override;
   void DidStartNavigation(
       content::NavigationHandle* navigation_handle) override;
   void DidFinishNavigation(
@@ -98,6 +99,8 @@ class AwRenderViewHostExt : public content::WebContentsObserver,
       ShouldOverrideUrlLoadingCallback callback) override;
 
   bool IsRenderViewReady() const;
+
+  void ResetLocalMainFrameRemote(content::RenderFrameHost* frame_host);
 
   AwRenderViewHostExtClient* client_;
 
