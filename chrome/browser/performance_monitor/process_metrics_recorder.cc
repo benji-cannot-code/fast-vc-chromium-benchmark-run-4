c22a77720fff5507146448cc3649bc47efd05b33
Francois Doray <fdoray@chromium.org>
2021-03-31 23:22:52 +0000

    Remove obsolete histogram PerformanceMonitor.HighCPU.*.
    Fixed: 1190968
    Change-Id: I84080075c7e264eaaf5fd78bb15068add290fa30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798312
    Auto-Submit: François Doray <fdoray@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868304}

--- chrome/browser/performance_monitor/process_metrics_recorder.cc
+++ chrome/browser/performance_monitor/process_metrics_recorder.cc
@@ -12,10 +12,6 @@ namespace performance_monitor {
 
 namespace {
 
-// If a process is consistently above this CPU utilization percentage over time,
-// we consider it as high and may take action.
-const float kHighCPUUtilizationThreshold = 90.0f;
-
 // CPU usage metrics are provided to this class as a double in the
 // [0.0, number of cores * 100.0] range. The CPU usage is usually below 1%, so
 // the histograms are reported with a 1/10000 granularity to make analyzing the
@@ -56,12 +52,6 @@ void ProcessMetricsRecorder::OnMetricsSampled(
           "PerformanceMonitor.AverageCPU2.BrowserProcess",
           metrics.cpu_usage * kCPUUsageFactor, kCPUUsageHistogramMin,
           kCPUUsageHistogramMax, kCPUUsageHistogramBucketCount);
-      // If CPU usage has consistently been above our threshold,
-      // we *may* have an issue.
-      if (metrics.cpu_usage > kHighCPUUtilizationThreshold) {
-        base::UmaHistogramBoolean("PerformanceMonitor.HighCPU.BrowserProcess",
-                                  true);
-      }
 #if defined(OS_WIN)
       base::UmaHistogramCustomCounts(
           "PerformanceMonitor.AverageDisk.BrowserProcess", metrics.disk_usage,
@@ -89,10 +79,6 @@ void ProcessMetricsRecorder::OnMetricsSampled(
           "PerformanceMonitor.AverageCPU2.RendererProcess2",
           metrics.cpu_usage * kCPUUsageFactor, kCPUUsageHistogramMin,
           kCPUUsageHistogramMax, kCPUUsageHistogramBucketCount);
-      if (metrics.cpu_usage > kHighCPUUtilizationThreshold) {
-        base::UmaHistogramBoolean("PerformanceMonitor.HighCPU.RendererProcess",
-                                  true);
-      }
 #if defined(OS_MAC) || defined(OS_LINUX) || defined(OS_CHROMEOS) || \
     defined(OS_AIX)
       base::UmaHistogramCounts10000(
@@ -115,9 +101,6 @@ void ProcessMetricsRecorder::OnMetricsSampled(
           "PerformanceMonitor.AverageCPU2.GPUProcess",
           metrics.cpu_usage * kCPUUsageFactor, kCPUUsageHistogramMin,
           kCPUUsageHistogramMax, kCPUUsageHistogramBucketCount);
-      if (metrics.cpu_usage > kHighCPUUtilizationThreshold)
-        base::UmaHistogramBoolean("PerformanceMonitor.HighCPU.GPUProcess",
-                                  true);
 #if defined(OS_MAC) || defined(OS_LINUX) || defined(OS_CHROMEOS) || \
     defined(OS_AIX)
       base::UmaHistogramCounts10000("PerformanceMonitor.IdleWakeups.GPUProcess",
@@ -138,9 +121,6 @@ void ProcessMetricsRecorder::OnMetricsSampled(
           "PerformanceMonitor.AverageCPU2.PPAPIProcess",
           metrics.cpu_usage * kCPUUsageFactor, kCPUUsageHistogramMin,
           kCPUUsageHistogramMax, kCPUUsageHistogramBucketCount);
-      if (metrics.cpu_usage > kHighCPUUtilizationThreshold)
-        base::UmaHistogramBoolean("PerformanceMonitor.HighCPU.PPAPIProcess",
-                                  true);
       break;
     case content::PROCESS_TYPE_UTILITY:
       base::UmaHistogramCustomCounts(
@@ -163,21 +143,12 @@ void ProcessMetricsRecorder::OnMetricsSampled(
           "PerformanceMonitor.AverageCPU2.RendererExtensionPersistentProcess",
           metrics.cpu_usage * kCPUUsageFactor, kCPUUsageHistogramMin,
           kCPUUsageHistogramMax, kCPUUsageHistogramBucketCount);
-      if (metrics.cpu_usage > kHighCPUUtilizationThreshold) {
-        base::UmaHistogramBoolean(
-            "PerformanceMonitor.HighCPU.RendererExtensionPersistentProcess",
-            true);
-      }
       break;
     case kProcessSubtypeExtensionEvent:
       base::UmaHistogramCustomCounts(
           "PerformanceMonitor.AverageCPU2.RendererExtensionEventProcess",
           metrics.cpu_usage * kCPUUsageFactor, kCPUUsageHistogramMin,
           kCPUUsageHistogramMax, kCPUUsageHistogramBucketCount);
-      if (metrics.cpu_usage > kHighCPUUtilizationThreshold) {
-        base::UmaHistogramBoolean(
-            "PerformanceMonitor.HighCPU.RendererExtensionEventProcess", true);
-      }
       break;
     case kProcessSubtypeNetworkProcess:
       base::UmaHistogramCustomCounts(
