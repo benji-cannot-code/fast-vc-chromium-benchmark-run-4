996d983338e1bdd9331a151091203f9d8905add6
Tom Anderson <thomasanderson@chromium.org>
2021-03-31 21:52:18 +0000

    [GTK] Convert build checks to runtime checks for GtkCssContext
    R=nickdiego
    
    Bug: 1192861
    Change-Id: I973b71a11d0a1d958b5e3f0031e253379c2bd0b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2793011
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#868269}

--- ui/gtk/gtk_compat.cc
+++ ui/gtk/gtk_compat.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gtk/gtk_compat.h"
 
 #include <dlfcn.h>
-#include <gtk/gtk.h>
 
 #include "base/check.h"
 #include "base/compiler_specific.h"
--- ui/gtk/gtk_compat.h
+++ ui/gtk/gtk_compat.h
@@ -6,10 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GTK_GTK_COMPAT_H_
 #define UI_GTK_GTK_COMPAT_H_
 
+#include <gdk/gdk.h>
+#include <gtk/gtk.h>
+
 #include <string>
 #include <vector>
 
 #include "base/component_export.h"
+#include "ui/gtk/gtk_types.h"
+extern "C" {
+#include "ui/gtk/gtk.sigs"
+}
 
 namespace gtk {
 
