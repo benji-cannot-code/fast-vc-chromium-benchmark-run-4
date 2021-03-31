278e3e92de0f408cf5bf8051bac734bef7f51df0
Daniel Libby <dlibby@microsoft.com>
2021-03-31 23:25:09 +0000

    Fix hang in CreatePreviousAnchorPosition
    After CL:2702380 we can now end up in scenarios where we try to get the
    previous anchor position from a descendant of an node marked as
    embedded object character. Since these are considered leaf nodes, they
    will hand out null positions, which causes CreatePreviousAnchorPosition
    to infinitely loop when trying to traverse the tree.
    
    If the parent is a leaf, then we just return it instead. Also added
    some fairly conservative CHECKs to immediately crash instead of
    hanging.
    
    R=kschmi@microsoft.com, nektar@chromium.org
    
    Bug: 1192344
    Change-Id: I27e7d3c32cd30d8d1d9d79298872b6c62b85381e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2786132
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#868308}

--- ui/accessibility/ax_position.h
+++ ui/accessibility/ax_position.h
@@ -4304,9 +4304,13 @@ class AXPosition {
     if (parent_position->IsNullPosition())
       return parent_position;
 
-    // If there is no previous sibling, move up to the parent.
+    // If there is no previous sibling, or the parent itself is a leaf, move up
+    // to the parent. The parent can be a leaf if we start with a tree position
+    // that is a descendant of a node that is an empty control represented by
+    // an "object replacement character" (see
+    // `IsEmptyObjectReplacedByCharacter()`).
     const int index_in_parent = current_position->AnchorIndexInParent();
-    if (index_in_parent <= 0) {
+    if (index_in_parent <= 0 || parent_position->IsLeaf()) {
       if (abort_predicate.Run(*current_position, *parent_position,
                               AXMoveType::kAncestor,
                               AXMoveDirection::kPreviousInTree)) {
@@ -4326,6 +4330,7 @@ class AXPosition {
       return CreateNullPosition();
     }
 
+    CHECK(!rightmost_leaf->IsNullPosition());
     while (!rightmost_leaf->IsLeaf()) {
       parent_position = std::move(rightmost_leaf);
       rightmost_leaf = parent_position->CreateChildPositionAt(
@@ -4337,6 +4342,7 @@ class AXPosition {
                               AXMoveDirection::kPreviousInTree)) {
         return CreateNullPosition();
       }
+      CHECK(!rightmost_leaf->IsNullPosition());
     }
     return rightmost_leaf;
   }
