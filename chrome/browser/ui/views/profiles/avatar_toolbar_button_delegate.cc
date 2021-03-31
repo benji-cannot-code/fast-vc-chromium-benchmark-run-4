08912ec8dfbba65980ffdde54669c7df6a760ea8
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-03-31 12:37:47 +0000

    Remove AvatarButtonErrorController
    This class offers little and convoluted functionality.
    It calls OnAvatarErrorChanged() to notify AvatarToolbarButtonDelegate
    of the existence of sync errors, but it doesn't expose what the error
    is, merely a HasAvatarError() bool. So AvatarToolbarButtonDelegate must
    call sync_ui_util::GetAvatarSyncErrorType(). One would imagine the
    implementation of HasAvatarError() also calls this getter, but it's
    worse: HasAvatarError() duplicates parts of the implementation of
    GetAvatarSyncErrorType(). As a minor extra, AvatarButtonErrorController
    is a bad name because the class doesn't control anything.
    
    This CL moves the minimal functionality of observing sync errors
    to AvatarToolbarButtonDelegate, getting rid of the problematic class.
    
    Bug: 1191411
    Change-Id: I92eadaf1ae933de5fb9b7fbe89b6da3f079d0141
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794508
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868064}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button_delegate.cc
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button_delegate.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/sync_ui_util.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "components/signin/public/identity_manager/consent_level.h"
+#include "components/sync/driver/profile_sync_service.h"
 #include "ui/base/resource/resource_bundle.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -96,28 +97,25 @@ bool IsGuest(Profile* profile) {
 
 }  // namespace
 
-AvatarToolbarButtonDelegate::AvatarToolbarButtonDelegate() = default;
-
-AvatarToolbarButtonDelegate::~AvatarToolbarButtonDelegate() {
-  BrowserList::RemoveObserver(this);
-}
-
-void AvatarToolbarButtonDelegate::Init(AvatarToolbarButton* button,
-                                       Profile* profile) {
-  avatar_toolbar_button_ = button;
-  profile_ = profile;
-  error_controller_ =
-      std::make_unique<AvatarButtonErrorController>(this, profile_);
+AvatarToolbarButtonDelegate::AvatarToolbarButtonDelegate(
+    AvatarToolbarButton* button,
+    Profile* profile)
+    : avatar_toolbar_button_(button),
+      profile_(profile),
+      last_avatar_error_(sync_ui_util::GetAvatarSyncErrorType(profile)) {
   profile_observation_.Observe(&GetProfileAttributesStorage());
+
+  if (auto* sync_service = ProfileSyncServiceFactory::GetForProfile(profile_))
+    sync_service_observation_.Observe(sync_service);
+
   AvatarToolbarButton::State state = GetState();
   if (state == AvatarToolbarButton::State::kIncognitoProfile ||
       state == AvatarToolbarButton::State::kGuestSession) {
     BrowserList::AddObserver(this);
-  } else if (state != AvatarToolbarButton::State::kGuestSession) {
+  } else {
     signin::IdentityManager* identity_manager =
         IdentityManagerFactory::GetForProfile(profile_);
     identity_manager_observation_.Observe(identity_manager);
-
     if (identity_manager->AreRefreshTokensLoaded())
       OnRefreshTokensLoaded();
   }
@@ -131,6 +129,10 @@ void AvatarToolbarButtonDelegate::Init(AvatarToolbarButton* button,
         state == AvatarToolbarButton::State::kIncognitoProfile);
   }
 #endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
+}
+
+AvatarToolbarButtonDelegate::~AvatarToolbarButtonDelegate() {
+  BrowserList::RemoveObserver(this);
 }
 
 std::u16string AvatarToolbarButtonDelegate::GetProfileName() const {
@@ -197,27 +199,25 @@ AvatarToolbarButton::State AvatarToolbarButtonDelegate::GetState() const {
     return AvatarToolbarButton::State::kAnimatedUserIdentity;
   }
 
-  if (identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSync) &&
-      ProfileSyncServiceFactory::IsSyncAllowed(profile_) &&
-      error_controller_->HasAvatarError()) {
-    const sync_ui_util::AvatarSyncErrorType error =
-        sync_ui_util::GetAvatarSyncErrorType(profile_);
-
-    // When DICE is enabled and the error is an auth error, the sync-paused
-    // icon is shown.
-    if (AccountConsistencyModeManager::IsDiceEnabledForProfile(profile_) &&
-        error == sync_ui_util::AUTH_ERROR) {
-      return AvatarToolbarButton::State::kSyncPaused;
-    }
-
-    if (error == sync_ui_util::TRUSTED_VAULT_KEY_MISSING_FOR_PASSWORDS_ERROR) {
-      return AvatarToolbarButton::State::kPasswordsOnlySyncError;
-    }
-
-    return AvatarToolbarButton::State::kSyncError;
+  if (!ProfileSyncServiceFactory::IsSyncAllowed(profile_) ||
+      !identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSync)) {
+    return AvatarToolbarButton::State::kNormal;
   }
 
-  return AvatarToolbarButton::State::kNormal;
+  // Show any existing sync errors.
+  const sync_ui_util::AvatarSyncErrorType error =
+      sync_ui_util::GetAvatarSyncErrorType(profile_);
+  if (error == sync_ui_util::AUTH_ERROR &&
+      AccountConsistencyModeManager::IsDiceEnabledForProfile(profile_)) {
+    return AvatarToolbarButton::State::kSyncPaused;
+  }
+
+  if (error == sync_ui_util::TRUSTED_VAULT_KEY_MISSING_FOR_PASSWORDS_ERROR)
+    return AvatarToolbarButton::State::kPasswordsOnlySyncError;
+
+  return error == sync_ui_util::NO_SYNC_ERROR
+             ? AvatarToolbarButton::State::kNormal
+             : AvatarToolbarButton::State::kSyncError;
 }
 
 void AvatarToolbarButtonDelegate::ShowHighlightAnimation() {
@@ -373,7 +373,13 @@ void AvatarToolbarButtonDelegate::OnExtendedAccountInfoRemoved(
   avatar_toolbar_button_->UpdateIcon();
 }
 
-void AvatarToolbarButtonDelegate::OnAvatarErrorChanged() {
+void AvatarToolbarButtonDelegate::OnStateChanged(syncer::SyncService*) {
+  sync_ui_util::AvatarSyncErrorType error =
+      sync_ui_util::GetAvatarSyncErrorType(profile_);
+  if (last_avatar_error_ == error)
+    return;
+
+  last_avatar_error_ = error;
   avatar_toolbar_button_->UpdateIcon();
   avatar_toolbar_button_->UpdateText();
 }
