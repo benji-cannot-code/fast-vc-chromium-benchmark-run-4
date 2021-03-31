84ccca1c526228c3dd2d0f3817f9a1145667aea9
David Jean <djean@chromium.org>
2021-03-31 14:14:23 +0000

    [ios] Fix flaky test caused by default browser promo
    The default browser promo causes message drop down to be dismiss
    too early, causing some multiwindow tests to fail.
    
    Bug: 1183471
    Change-Id: I3892a53ca548f29bdcfc2835a9b0a8da438cf840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794949
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868081}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -74,6 +74,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Programmatically dismisses settings screen.
 + (void)dismissSettings;
 
+// Disables default browser promo. If a test needs to check a message drop down
+// in a second window, this needs to be disabled or the popup will kill the
+// message.
++ (void)disableDefaultBrowserPromo;
+
 #pragma mark - Tab Utilities (EG2)
 
 // Selects tab with given index in current mode (incognito or main
