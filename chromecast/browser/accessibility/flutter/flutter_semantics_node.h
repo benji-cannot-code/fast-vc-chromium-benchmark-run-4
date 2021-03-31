744c823eae484997f4d33aeff24b12db67467bb2
Lei Zhang <thestig@chromium.org>
2021-03-31 21:43:47 +0000

    Do not treat headers from Chromium checkout as system header.
    Consistently use quotes instead of angle brackets when including header
    files that are found in a Chromium checkout.
    
    Change-Id: Ib180cbb7dd8ca2031f31597e061e00dc3fa62a31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2787968
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Randy Rossi <rmrossi@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868259}

--- chromecast/browser/accessibility/flutter/flutter_semantics_node.h
+++ chromecast/browser/accessibility/flutter/flutter_semantics_node.h
@@ -6,10 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMECAST_BROWSER_ACCESSIBILITY_FLUTTER_FLUTTER_SEMANTICS_NODE_H_
 #define CHROMECAST_BROWSER_ACCESSIBILITY_FLUTTER_FLUTTER_SEMANTICS_NODE_H_
 
-#include <ui/gfx/geometry/rect.h>
-
 #include <string>
 #include <vector>
+
+#include "ui/gfx/geometry/rect.h"
 
 namespace ui {
 struct AXNodeData;
--- remoting/client/cursor_shape_stub_proxy.cc
+++ remoting/client/cursor_shape_stub_proxy.cc
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <remoting/client/cursor_shape_stub_proxy.h>
+#include "remoting/client/cursor_shape_stub_proxy.h"
+
 #include "base/bind.h"
 #include "base/location.h"
 #include "remoting/proto/control.pb.h"
