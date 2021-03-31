fbec0eb05c04ab9cb83ad58e2e1bf435b1e818a6
ckitagawa <ckitagawa@chromium.org>
2021-03-31 18:26:20 +0000

    [Paint Preview] Add test coverage for image provider
    Adds test coverage for
    https://chromium-review.googlesource.com/c/chromium/src/+/2792185
    
    This adds a cc::PaintImage to the cc::PaintRecord which adds coverage
    for the ImageProvider implementation.
    
    Bug: 1193756
    Change-Id: If9a10e4ce5bd53f435dc03628ac5ece3f963e834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2795161
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868188}

--- components/paint_preview/renderer/paint_preview_recorder_utils_unittest.cc
+++ components/paint_preview/renderer/paint_preview_recorder_utils_unittest.cc
@@ -12,9 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/files/file.h"
 #include "base/files/scoped_temp_dir.h"
+#include "base/memory/discardable_memory_allocator.h"
 #include "base/memory/read_only_shared_memory_region.h"
 #include "base/notreached.h"
 #include "base/optional.h"
+#include "base/test/test_discardable_memory_allocator.h"
 #include "base/unguessable_token.h"
 #include "cc/paint/paint_canvas.h"
 #include "cc/paint/paint_flags.h"
@@ -211,10 +213,23 @@ class PaintPreviewRecorderUtilsSerializeAsSkPictureTest
 
  protected:
   void SetUp() override {
+    base::DiscardableMemoryAllocator::SetInstance(&test_allocator_);
+
     canvas = recorder.beginRecording(dimensions.width(), dimensions.width());
     cc::PaintFlags flags;
     canvas->drawRect(SkRect::MakeWH(dimensions.width(), dimensions.height()),
                      flags);
+    SkBitmap bitmap;
+    bitmap.allocN32Pixels(dimensions.width(), dimensions.height());
+    {
+      SkCanvas sk_canvas(bitmap);
+      sk_canvas.drawColor(SK_ColorRED);
+    }
+    canvas->drawImage(cc::PaintImage::CreateFromBitmap(bitmap), 0, 0);
+  }
+
+  void TearDown() override {
+    base::DiscardableMemoryAllocator::SetInstance(nullptr);
   }
 
   base::Optional<SerializedRecording> SerializeAsSkPicture(
@@ -264,6 +279,9 @@ class PaintPreviewRecorderUtilsSerializeAsSkPictureTest
 
   // Valid after SetUp() until SerializeAsSkPicture() is called.
   cc::PaintCanvas* canvas{};
+
+ private:
+  base::TestDiscardableMemoryAllocator test_allocator_;
 };
 
 TEST_P(PaintPreviewRecorderUtilsSerializeAsSkPictureTest, Roundtrip) {
@@ -291,6 +309,12 @@ TEST_P(PaintPreviewRecorderUtilsSerializeAsSkPictureTest, Roundtrip) {
     result->ctx.erase(content_id);
   }
   EXPECT_TRUE(result->ctx.empty());
+
+  SkBitmap bitmap;
+  bitmap.allocN32Pixels(dimensions.width(), dimensions.height());
+  SkCanvas sk_canvas(bitmap);
+  sk_canvas.drawPicture(result->skp);
+  EXPECT_EQ(bitmap.getColor(10, 10), SK_ColorRED);
 }
 
 TEST_P(PaintPreviewRecorderUtilsSerializeAsSkPictureTest, FailIfExceedMaxSize) {
