5201b9800fb755c7cb1eceff828fe29acda051f5
Gordon Seto <gordonseto@google.com>
2021-03-31 18:15:05 +0000

    [CrOS Settings] Disable internet detail overflow menu when inhibited.
    Disable internet-detail-menu when device is inhibited.
    
    Bug: 1093185
    Change-Id: Ifa405f511f8b8a46b336b55ace76734636f33425
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794739
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868183}

--- chrome/browser/resources/settings/chromeos/internet_page/internet_detail_menu.js
+++ chrome/browser/resources/settings/chromeos/internet_page/internet_detail_menu.js
@@ -17,11 +17,25 @@ Polymer({
   ],
 
   properties: {
+    /**
+     * Device state for the network type.
+     * @type {!OncMojo.DeviceStateProperties|undefined}
+     */
+    deviceState: Object,
+
     /** @private {?OncMojo.NetworkStateProperties} */
     networkState_: {
       type: Object,
       value: null,
-    }
+    },
+
+    /** @private */
+    isUpdatedCellularUiEnabled_: {
+      type: Boolean,
+      value() {
+        return loadTimeData.getBoolean('updatedCellularActivationUi');
+      }
+    },
   },
 
   /**
@@ -32,7 +46,7 @@ Polymer({
    */
   currentRouteChanged(route, oldRoute) {
     if (route !== settings.routes.NETWORK_DETAIL ||
-        !loadTimeData.getBoolean('updatedCellularActivationUi')) {
+        !this.isUpdatedCellularUiEnabled_) {
       return;
     }
     this.networkState_ = null;
@@ -75,6 +89,17 @@ Polymer({
    */
   shouldShowDotsMenuButton_() {
     return !!this.networkState_;
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  isDotsMenuButtonDisabled_() {
+    if (!this.deviceState || !this.isUpdatedCellularUiEnabled_) {
+      return false;
+    }
+    return OncMojo.deviceIsInhibited(this.deviceState);
   },
 
   /**
