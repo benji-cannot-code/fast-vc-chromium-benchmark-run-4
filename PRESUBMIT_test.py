2cc14f75d343902d5dee1ee96593f94165c91a77
seanmccullough <seanmccullough@google.com>
2021-04-01 18:49:52 +0000

    [chromium/src PRESUBMIT] add failsafe for inclusive language check
    If the exempt directories file is missing, just log and continue.
    
    Bug: 1194996
    Change-Id: Ibd2bad1c9a4f3f485fb7933dfea7a630400a3654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2800492
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Sean McCullough <seanmccullough@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868614}

--- PRESUBMIT_test.py
+++ PRESUBMIT_test.py
@@ -3941,6 +3941,14 @@ class CheckDeprecationOfPreferencesTest(unittest.TestCase):
 
 
 class InclusiveLanguageCheckTest(unittest.TestCase):
+  def testMissingExemptFile(self):
+    input_api = MockInputApi()
+    input_api.change.RepositoryRoot = lambda: ''
+
+    input_api.files = []
+    errors = PRESUBMIT.CheckInclusiveLanguage(input_api, MockOutputApi())
+    self.assertEqual(0, len(errors))
+
   def testBlockedTerms(self):
     input_api = MockInputApi()
     input_api.change.RepositoryRoot = lambda: ''
