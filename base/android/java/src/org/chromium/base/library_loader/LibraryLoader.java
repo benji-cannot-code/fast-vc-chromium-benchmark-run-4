1150a15d38534d6c41cf6f5b7717ee7ede69cb5e
Peter Kotwicz <pkotwicz@chromium.org>
2021-04-01 22:11:13 +0000

    Change org.chromium.base.NativeLibraries package to build
    This CL changes the package of org.chromium.base.NativeLibraries to
    org.chromium.build. This better reflects the location of the GN
    build rule to generate NativeLibraries.java
    
    BUG=1189911
    
    Change-Id: Id600a2749d9d6ed02ad00dc7b596a29f84ba4300
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2790522
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Owners-Override: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868696}

--- base/android/java/src/org/chromium/base/library_loader/LibraryLoader.java
+++ base/android/java/src/org/chromium/base/library_loader/LibraryLoader.java
@@ -36,6 +36,7 @@ import org.chromium.base.compat.ApiHelperForM;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.UmaRecorderHolder;
 import org.chromium.build.BuildConfig;
+import org.chromium.build.NativeLibraries;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
