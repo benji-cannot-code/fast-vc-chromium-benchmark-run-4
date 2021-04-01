67f464c98f338e7ce79ed615690c112e0f0feed0
hscham <hscham@chromium.org>
2021-04-01 02:10:13 +0000

    cros-healthd: Use new mojo binding types
    To keep the cros_healthd.mojom file in sync with that in Chrome OS.
    
    Bug: chromium:1179608
    Test: autoninja -C out/Default chrome
    Change-Id: I7b3342cffbb96bee8f37e9fd28376b37c85a2681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784415
    Reviewed-by: Paul Moy <pmoy@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Grace Cham <hscham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868381}

--- chromeos/dbus/cros_healthd/fake_cros_healthd_service.h
+++ chromeos/dbus/cros_healthd/fake_cros_healthd_service.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/cros_healthd/public/mojom/cros_healthd_probe.mojom.h"
 #include "chromeos/services/network_health/public/mojom/network_diagnostics.mojom.h"
 #include "chromeos/services/network_health/public/mojom/network_health.mojom.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -51,10 +52,13 @@ class FakeCrosHealthdService final
   ~FakeCrosHealthdService() override;
 
   // CrosHealthdServiceFactory overrides:
-  void GetProbeService(mojom::CrosHealthdProbeServiceRequest service) override;
+  void GetProbeService(
+      mojo::PendingReceiver<mojom::CrosHealthdProbeService> service) override;
   void GetDiagnosticsService(
-      mojom::CrosHealthdDiagnosticsServiceRequest service) override;
-  void GetEventService(mojom::CrosHealthdEventServiceRequest service) override;
+      mojo::PendingReceiver<mojom::CrosHealthdDiagnosticsService> service)
+      override;
+  void GetEventService(
+      mojo::PendingReceiver<mojom::CrosHealthdEventService> service) override;
   void SendNetworkHealthService(
       mojo::PendingRemote<chromeos::network_health::mojom::NetworkHealthService>
           remote) override;
@@ -63,7 +67,7 @@ class FakeCrosHealthdService final
           chromeos::network_diagnostics::mojom::NetworkDiagnosticsRoutines>
           network_diagnostics_routines) override;
   void GetSystemService(
-      mojom::CrosHealthdSystemServiceRequest service) override;
+      mojo::PendingReceiver<mojom::CrosHealthdSystemService> service) override;
 
   // CrosHealthdDiagnosticsService overrides:
   void GetAvailableRoutines(GetAvailableRoutinesCallback callback) override;
@@ -134,9 +138,12 @@ class FakeCrosHealthdService final
 
   // CrosHealthdEventService overrides:
   void AddBluetoothObserver(
-      mojom::CrosHealthdBluetoothObserverPtr observer) override;
-  void AddLidObserver(mojom::CrosHealthdLidObserverPtr observer) override;
-  void AddPowerObserver(mojom::CrosHealthdPowerObserverPtr observer) override;
+      mojo::PendingRemote<mojom::CrosHealthdBluetoothObserver> observer)
+      override;
+  void AddLidObserver(
+      mojo::PendingRemote<mojom::CrosHealthdLidObserver> observer) override;
+  void AddPowerObserver(
+      mojo::PendingRemote<mojom::CrosHealthdPowerObserver> observer) override;
   void AddNetworkObserver(
       mojo::PendingRemote<
           chromeos::network_health::mojom::NetworkEventsObserver> observer)
