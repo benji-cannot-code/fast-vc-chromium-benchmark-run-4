7c69b30a50d59f2c48511eb44d5ca63c69b4d1a8
Noel Gordon <noel@chromium.org>
2021-04-01 03:13:10 +0000

    Revert "Implement zcr_extended_keyboard::peek_key."
    This reverts commit e3739ab81ef28ded794e41c3533fd68b324e1a07.
    
    Reason for revert:
    
    Tree closing compile failure per
    
    https://ci.chromium.org/p/chromium/builders/ci/WebKit%20Linux%20MSAN
    
    Original change's description:
    > Implement zcr_extended_keyboard::peek_key.
    >
    > BUG=1180433
    > TEST=Ran exo_unittests.
    >
    > Change-Id: I787963980a795b41fb52b6d2690d427e66d9286d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2796852
    > Reviewed-by: Jun Mukai <mukai@chromium.org>
    > Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#868389}
    
    Bug: 1180433
    Change-Id: I0c179a57e69d086e3b46bac3a293ae934532ba49
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2799111
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868393}

--- components/exo/seat.h
+++ components/exo/seat.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "build/chromeos_buildflags.h"
 #include "components/exo/data_source_observer.h"
-#include "components/exo/key_state.h"
 #include "ui/aura/client/drag_drop_delegate.h"
 #include "ui/aura/client/focus_change_observer.h"
 #include "ui/base/clipboard/clipboard_observer.h"
@@ -71,7 +70,7 @@ class Seat : public aura::client::FocusChangeObserver,
   virtual Surface* GetFocusedSurface();
 
   // Returns currently pressed keys.
-  const base::flat_map<ui::DomCode, KeyState>& pressed_keys() const {
+  const base::flat_map<ui::DomCode, ui::DomCode>& pressed_keys() const {
     return pressed_keys_;
   }
 
@@ -178,7 +177,7 @@ class Seat : public aura::client::FocusChangeObserver,
   // The platform code is the key in this map as it represents the physical
   // key that was pressed. The value is a potentially rewritten code that the
   // physical key press generated.
-  base::flat_map<ui::DomCode, KeyState> pressed_keys_;
+  base::flat_map<ui::DomCode, ui::DomCode> pressed_keys_;
   ui::DomCode physical_code_for_currently_processing_event_ = ui::DomCode::NONE;
 
   // Data source being used as a clipboard content.
