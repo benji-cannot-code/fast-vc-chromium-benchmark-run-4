0a4bf70dbcb9b3559582055698cb28f93e2c0d33
Adam Langley <agl@chromium.org>
2021-04-01 22:51:32 +0000

    webauthn: tidy up getting platform credential status a little.
    Change-Id: I5280e514de4ed722d561ac1e6a0bde7539edc940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791764
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#868711}

--- device/fido/get_assertion_request_handler.h
+++ device/fido/get_assertion_request_handler.h
@@ -87,8 +87,8 @@ class COMPONENT_EXPORT(DEVICE_FIDO) GetAssertionRequestHandler
   void DispatchRequest(FidoAuthenticator* authenticator) override;
   void AuthenticatorRemoved(FidoDiscoveryBase* discovery,
                             FidoAuthenticator* authenticator) override;
-  void FillHasRecognizedPlatformCredential(
-      base::OnceCallback<void()> done_callback) override;
+  void GetPlatformCredentialStatus(
+      FidoAuthenticator* platform_authenticator) override;
 
   // AuthTokenRequester::Delegate:
   void AuthenticatorSelectedForPINUVAuthToken(
@@ -130,10 +130,6 @@ class COMPONENT_EXPORT(DEVICE_FIDO) GetAssertionRequestHandler
           blobs);
   void OnWriteLargeBlob(FidoAuthenticator* authenticator,
                         CtapDeviceResponseCode status);
-  void OnHasPlatformCredential(
-      base::OnceCallback<void()> done_callback,
-      std::vector<PublicKeyCredentialUserEntity> credentials,
-      bool has_credential);
 
   CompletionCallback completion_callback_;
   State state_ = State::kWaitingForTouch;
