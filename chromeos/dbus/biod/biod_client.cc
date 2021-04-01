7e477e0fcb43feca85f357ddd98192bf8b26edfd
Polina Bondarenko <pbond@google.com>
2021-04-01 09:55:11 +0000

    chromeos: disable BIOD input in headless
    Disable BIOD input when headless ozone platform is enabled by
    ArcDataSnapshotdManager.
    
    BUG=b:180684680
    TEST=set DeviceArcDataSnapshotHours policy, see that BIOD is disable \
         while the snapshot is generated.
    
    Change-Id: Ie8b9eb81458629eec047826a5fc5c42ddf88c678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2797192
    Commit-Queue: Polina Bondarenko <pbond@chromium.org>
    Auto-Submit: Polina Bondarenko <pbond@chromium.org>
    Reviewed-by: Edman Anjos <edman@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868471}

--- chromeos/dbus/biod/biod_client.cc
+++ chromeos/dbus/biod/biod_client.cc
@@ -12,11 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback_helpers.h"
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/strings/stringprintf.h"
 #include "chromeos/dbus/biod/fake_biod_client.h"
 #include "chromeos/dbus/biod/messages.pb.h"
+#include "chromeos/dbus/constants/dbus_switches.h"
 #include "dbus/bus.h"
 #include "dbus/message.h"
 #include "dbus/object_path.h"
@@ -423,8 +425,12 @@ BiodClient::~BiodClient() {
 
 // static
 void BiodClient::Initialize(dbus::Bus* bus) {
-  DCHECK(bus);
-  (new BiodClientImpl())->Init(bus);
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(switches::kBiodFake)) {
+    BiodClient::InitializeFake();
+  } else {
+    DCHECK(bus);
+    (new BiodClientImpl())->Init(bus);
+  }
 }
 
 // static
