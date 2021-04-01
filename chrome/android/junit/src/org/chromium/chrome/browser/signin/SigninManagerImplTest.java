9a45f45a501df39334deba7ab4a4e81489a4cb7d
Alice Wang <aliceywang@chromium.org>
2021-04-01 07:56:49 +0000

    [Signin][Android] Remove isOperationInProgress() from SigninManager API
    This CL removes the method isOperationInProgress() from SigninManager
    API as this is an implementation detail that we do not need to expose
    to users. The only usage of it in SigninHelper is also redundant as
    the check is already done in runAfterOperationInProgress().
    
    Bug: 1152460
    Change-Id: I59b01bec3dd8b2cebe8b96d023e180dc66feafd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794512
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868448}

--- chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninManagerImplTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninManagerImplTest.java
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.signin;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -371,7 +369,6 @@ public class SigninManagerImplTest {
 
     @Test
     public void callbackNotifiedWhenNoOperationIsInProgress() {
-        assertFalse(mSigninManager.isOperationInProgress());
         AtomicInteger callCount = new AtomicInteger(0);
 
         mSigninManager.runAfterOperationInProgress(callCount::incrementAndGet);
@@ -389,13 +386,11 @@ public class SigninManagerImplTest {
                 .clearPrimaryAccount(anyInt(), anyInt());
 
         mSigninManager.signOut(SignoutReason.SIGNOUT_TEST);
-        assertTrue(mSigninManager.isOperationInProgress());
         AtomicInteger callCount = new AtomicInteger(0);
         mSigninManager.runAfterOperationInProgress(callCount::incrementAndGet);
         assertEquals(0, callCount.get());
 
         mSigninManager.finishSignOut();
-        assertFalse(mSigninManager.isOperationInProgress());
         assertEquals(1, callCount.get());
     }
 
@@ -415,13 +410,12 @@ public class SigninManagerImplTest {
         mSigninManager.onFirstRunCheckDone(); // Allow sign-in.
 
         mSigninManager.signinAndEnableSync(SigninAccessPoint.UNKNOWN, ACCOUNT_INFO, null);
-        assertTrue(mSigninManager.isOperationInProgress());
+
         AtomicInteger callCount = new AtomicInteger(0);
         mSigninManager.runAfterOperationInProgress(callCount::incrementAndGet);
         assertEquals(0, callCount.get());
 
         mSigninManager.finishSignInAfterPolicyEnforced();
-        assertFalse(mSigninManager.isOperationInProgress());
         assertEquals(1, callCount.get());
     }
 
