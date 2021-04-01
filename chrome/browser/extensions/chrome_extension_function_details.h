5536db7fdbc62d7ecd68ef1c41bba394db0d0b3a
Devlin Cronin <rdevlin.cronin@chromium.org>
2021-04-01 23:21:14 +0000

    [Extensions] Mark ChromeExtensionFunctionDetails::GetProfile deprecated
    Mark the ChromeExtensionFunctionDetails::GetProfile() method as
    deprecated. This function only returns Profile::FromBrowserContext(),
    which the caller could easily call themselves. Its use doesn't
    warrant the extra redirection, dependence on
    ChromeExtensionFunctionDetails, or questions of whether it would
    behave differently than simply returning the Profile from the
    BrowserContext.
    
    Bug: 1194753
    Change-Id: I40f0ec38b15446cc5490911538a9472cbb7e622d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798026
    Auto-Submit: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868719}

--- chrome/browser/extensions/chrome_extension_function_details.h
+++ chrome/browser/extensions/chrome_extension_function_details.h
@@ -23,6 +23,8 @@ class ChromeExtensionFunctionDetails {
   explicit ChromeExtensionFunctionDetails(ExtensionFunction* function);
   ~ChromeExtensionFunctionDetails();
 
+  // DEPRECATED. Instead, use Profile::FromBrowserContext() directly.
+  // See https://crbug.com/1194753.
   Profile* GetProfile() const;
 
   // Gets the "current" browser, if any.
