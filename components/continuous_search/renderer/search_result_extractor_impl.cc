fa92dbc6b55f2a95fb2e330d2ddb04b6eb776895
ckitagawa <ckitagawa@chromium.org>
2021-04-01 17:07:15 +0000

    [CSN] Use UTF16 for titles
    Followup to
    https://chromium-review.googlesource.com/c/chromium/src/+/2795464
    which removes the need to convert back and forth from UTF8 for title
    validation.
    
    Bug: 1194567
    Change-Id: Ide8c8a72a62dff9ed70922ca2e2b7a81a1542411
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2797612
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868570}

--- components/continuous_search/renderer/search_result_extractor_impl.cc
+++ components/continuous_search/renderer/search_result_extractor_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "components/continuous_search/common/title_validator.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_registry.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/web/web_document.h"
@@ -87,9 +88,7 @@ bool ExtractResultCards(blink::WebElement node, mojom::ResultGroupPtr& group) {
     }
     auto result = mojom::SearchResult::New();
     result->link = url;
-    std::u16string trimmed_title;
-    base::TrimWhitespace(title, base::TRIM_ALL, &trimmed_title);
-    result->title = base::UTF16ToUTF8(trimmed_title);
+    result->title = ValidateTitle(title);
     group->results.push_back(std::move(result));
 
     had_results = true;
