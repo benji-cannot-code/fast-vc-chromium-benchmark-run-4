18b3f83307f8969249873fbb4b509cfbcf651a31
Shane Fitzpatrick <shanefitz@google.com>
2021-04-01 15:51:25 +0000

    [Nearby] Explicitly call StopDiscovery on confirmation page close
    We currently have a race condition that can cause the notification
    manager to not receive transfer updates and thus no success/failure
    notification is sent. This occurs most frequently with small files
    (<100KB).
    
    This race condition occurs because the DiscoveryManager unregisters
    itself as a foreground surface in it's destructor.  This destructor is
    called when the page is destroyed. The page can be destroyed during the
    transfer, and if the transfer is small enough it can conclude before the
    destructor has run.  In that event, the DiscoveryManager is still
    registered as a foreground surface and swallows the events that should be going to the notification manager.
    
    This CL makes the change to explicitly call StopDiscovery (and thus UnregisterSendSurface) on the DiscoveryManager when the confirmation page is about to close, and closes the page when that method has returned.
    
    This change brings the bug repro rate down to 0%, even on files as small
    as 1 byte. I smoke tested transfers of various sizes and found no
    regressions.
    
    Fixed: 1137586
    Change-Id: I4e140620a9d57d0ca22cee035ebe9c9c03b6c2d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794876
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/master@{#868535}

--- chrome/test/data/webui/nearby_share/fake_mojo_interfaces.js
+++ chrome/test/data/webui/nearby_share/fake_mojo_interfaces.js
@@ -46,6 +46,7 @@ export class FakeDiscoveryManagerRemote extends TestBrowserProxy {
       'getPayloadPreview',
       'selectShareTarget',
       'startDiscovery',
+      'stopDiscovery',
       'addDiscoveryObserver',
     ]);
 
@@ -90,6 +91,10 @@ export class FakeDiscoveryManagerRemote extends TestBrowserProxy {
   async startDiscovery(listener) {
     this.methodCalled('startDiscovery', listener);
     return {result: this.startDiscoveryResult};
+  }
+
+  async stopDiscovery() {
+    this.methodCalled('stopDiscovery');
   }
 
   /**
