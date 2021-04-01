c104f65bc28b08739664b2454ba814c181b4fa78
Hiroki Nakagawa <nhiroki@chromium.org>
2021-04-01 08:35:52 +0000

    NoStatePrefetch: Rename PrerenderData to NoStatePrefetchData
    Bug: 1144577
    Change-Id: I6e3d7176946edbdc5aef7e3c5d51631a68981584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791286
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868454}

--- components/no_state_prefetch/browser/no_state_prefetch_handle.h
+++ components/no_state_prefetch/browser/no_state_prefetch_handle.h
@@ -76,7 +76,7 @@ class NoStatePrefetchHandle : public NoStatePrefetchContents::Observer {
   friend class NoStatePrefetchManager;
 
   explicit NoStatePrefetchHandle(
-      NoStatePrefetchManager::PrerenderData* prerender_data);
+      NoStatePrefetchManager::NoStatePrefetchData* prefetch_data);
 
   // From NoStatePrefetchContents::Observer:
   void OnPrefetchStop(
@@ -86,7 +86,7 @@ class NoStatePrefetchHandle : public NoStatePrefetchContents::Observer {
 
   Observer* observer_;
 
-  base::WeakPtr<NoStatePrefetchManager::PrerenderData> prerender_data_;
+  base::WeakPtr<NoStatePrefetchManager::NoStatePrefetchData> prefetch_data_;
   base::WeakPtrFactory<NoStatePrefetchHandle> weak_ptr_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(NoStatePrefetchHandle);
