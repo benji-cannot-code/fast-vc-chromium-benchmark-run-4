7416511c35dcac22f60c75de4ff556d04fbe337c
alanlxl <alanlxl@chromium.org>
2021-04-01 00:05:47 +0000

    Remove the temp fixture of HandwritingRecognizer
    Now the web extension can use the top-level ml-service interface, we
    don't need the HandwritingRecognizerRequestor anymore.
    
    Bug: 1076172, 916760
    Test: None
    Change-Id: I18af8fa3e2dd142b23e7d1188e87fdece8920481
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794350
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868338}

--- chrome/browser/extensions/chrome_extensions_browser_interface_binders.cc
+++ chrome/browser/extensions/chrome_extensions_browser_interface_binders.cc
@@ -38,9 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 #include "chromeos/services/ime/public/mojom/input_engine.mojom.h"
-#include "chromeos/services/machine_learning/public/cpp/handwriting_recognizer_manager.h"
 #include "chromeos/services/machine_learning/public/cpp/service_connection.h"
-#include "chromeos/services/machine_learning/public/mojom/handwriting_recognizer_requestor.mojom.h"
 #include "ui/base/ime/chromeos/extension_ime_util.h"
 #include "ui/base/ime/chromeos/input_method_manager.h"
 #endif
@@ -65,16 +63,6 @@ void BindInputEngineManager(
     mojo::PendingReceiver<chromeos::ime::mojom::InputEngineManager> receiver) {
   chromeos::input_method::InputMethodManager::Get()->ConnectInputEngineManager(
       std::move(receiver));
-}
-
-void BindHandwritingRecognizerRequestor(
-    content::RenderFrameHost* render_frame_host,
-    mojo::PendingReceiver<
-        chromeos::machine_learning::mojom::HandwritingRecognizerRequestor>
-        receiver) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  chromeos::machine_learning::HandwritingRecognizerManager::GetInstance()
-      ->AddReceiver(std::move(receiver));
 }
 
 void BindMachineLearningService(
@@ -132,9 +120,6 @@ void PopulateChromeFrameBindersForExtension(
   if (extension->id() == chromeos::extension_ime_util::kXkbExtensionId) {
     binder_map->Add<chromeos::ime::mojom::InputEngineManager>(
         base::BindRepeating(&BindInputEngineManager));
-    binder_map->Add<
-        chromeos::machine_learning::mojom::HandwritingRecognizerRequestor>(
-        base::BindRepeating(&BindHandwritingRecognizerRequestor));
     binder_map->Add<chromeos::machine_learning::mojom::MachineLearningService>(
         base::BindRepeating(&BindMachineLearningService));
   }
