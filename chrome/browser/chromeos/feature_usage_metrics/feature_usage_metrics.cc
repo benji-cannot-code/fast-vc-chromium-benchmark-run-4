4716675b7d1ade29ef2f58ee3a422d51faf6198e
Roman Sorokin <rsorokin@chromium.org>
2021-04-01 19:22:18 +0000

    [CrOS] Add README.md for feature_usage_metrics
    Follow-up to https://crrev.com/c/2596263
    
    Bug: 1162260
    Change-Id: Ic6ee4524f58ac21d352b8e89cd70cacf2655331c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2799535
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868632}

--- chrome/browser/chromeos/feature_usage_metrics/feature_usage_metrics.cc
+++ chrome/browser/chromeos/feature_usage_metrics/feature_usage_metrics.cc
@@ -95,7 +95,7 @@ void FeatureUsageMetrics::RegisterPref(PrefRegistrySimple* registry,
 }
 
 void FeatureUsageMetrics::RecordUsage(bool success) const {
-  Event e = success ? Event::kUsedWithSucess : Event::kUsedWithFailure;
+  Event e = success ? Event::kUsedWithSuccess : Event::kUsedWithFailure;
   base::UmaHistogramEnumeration(histogram_name_, e);
 }
 
