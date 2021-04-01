b9cb4a1d5d98d7d932fb4beed0007e9a783846e6
Maksim Sisov <msisov@igalia.com>
2021-04-01 18:39:51 +0000

    ozone/wayland: PlatformWindowType::kPopup are not menu windows
    These windows do not require parent windows and are rather used
    as notifications. Thus, create them as toplevel windows.
    
    Bug: 1191446
    Change-Id: Ia9d91ca2d99867b85694c91d3566c3966061f55e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791369
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#868607}

--- ui/ozone/platform/wayland/host/wayland_screen_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_screen_unittest.cc
@@ -271,7 +271,7 @@ TEST_P(WaylandScreenTest, GetAcceleratedWidgetAtScreenPoint) {
       CreateWaylandWindowWithProperties(
           gfx::Rect(window_->GetBounds().width() - 10,
                     window_->GetBounds().height() - 10, 100, 100),
-          PlatformWindowType::kPopup, window_->GetWidget(), &delegate);
+          PlatformWindowType::kMenu, window_->GetWidget(), &delegate);
 
   Sync();
 
@@ -539,7 +539,7 @@ TEST_P(WaylandScreenTest, GetCursorScreenPoint) {
       CreateWaylandWindowWithProperties(
           gfx::Rect(second_window_bounds.width() - 10,
                     second_window_bounds.height() - 10, 10, 20),
-          PlatformWindowType::kPopup, second_window->GetWidget(), &delegate);
+          PlatformWindowType::kMenu, second_window->GetWidget(), &delegate);
 
   Sync();
 
@@ -586,7 +586,7 @@ TEST_P(WaylandScreenTest, GetCursorScreenPoint) {
       CreateWaylandWindowWithProperties(
           gfx::Rect(menu_window_bounds.x() + menu_window_bounds.width(),
                     menu_window_bounds.y() + 2, 10, 20),
-          PlatformWindowType::kPopup, second_window->GetWidget(), &delegate);
+          PlatformWindowType::kMenu, second_window->GetWidget(), &delegate);
 
   Sync();
 
