b353aeaad4fb5a6a222325c335f44df17a47e3c0
Moritz Firsching <firsching@google.com>
2021-04-01 04:51:50 +0000

    Add libjxl library and its dependencies.
    Make the new "libjxl" library available in gn build system and
    include its new dependencies (google/highway).
    
    This library is now included in the build from the
    blink_platform_unittests_sources target so it is built in this
    commit. Since it is only in the unittest it won't be included in
    chrome binary with this commit.
    
    This library will be conditionally included behind a flag in a
    follow up commit in preparation to support jxl Content-Encoding and
    image/jxl codec.
    
    Co-Authored-By: deymo@chromium.org
    Co-Authored-By: lode@google.com
    
    Bug: 1178058
    Change-Id: Ia5a87f7de05f77fa611553b464fefec42a2e92ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2745343
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Moritz Firsching <firsching@google.com>
    Cr-Commit-Position: refs/heads/master@{#868408}

---
+++ third_party/libjxl/gen_headers/jxl/jxl_export.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_LIBJXL_GEN_HEADERS_JXL_JXL_EXPORT_H_
+#define THIRD_PARTY_LIBJXL_GEN_HEADERS_JXL_JXL_EXPORT_H_
+
+#define JXL_EXPORT
+
+#endif  // THIRD_PARTY_LIBJXL_GEN_HEADERS_JXL_JXL_EXPORT_H_
