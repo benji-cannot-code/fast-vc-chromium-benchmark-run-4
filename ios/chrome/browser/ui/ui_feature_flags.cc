7168a3da66939012c3c93b23cb5d151bc66f0e7a
Tina Wang <tinazwang@google.com>
2021-04-01 17:14:23 +0000

    🐻 [Pandora] Create a flag for FRE UI module
    Create a flag for FRE UI module. This feature flag guards the FRE UI
    module and it will be used for experimenting new FRE UI module before
    the Chrome startup refractory(crbug.com/1178533) is done.
    
    Bug: 1194809
    Change-Id: I0136b7d76a4a3d544eb1387bdd75268f0b4e7e64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798539
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868573}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -29,6 +29,9 @@ const base::Feature kIllustratedEmptyStates{"IllustratedEmptyStates",
 const base::Feature kSharedHighlightingIOS{"SharedHighlightingIOS",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kEnableFREUIModuleIOS{"EnableFREUIModuleIOS",
+                                          base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kEnableFullPageScreenshot{
     "EnableFullPageScreenshot", base::FEATURE_DISABLED_BY_DEFAULT};
 
