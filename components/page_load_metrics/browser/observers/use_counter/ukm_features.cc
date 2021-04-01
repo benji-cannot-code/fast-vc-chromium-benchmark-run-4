2b43150ec272ea7c84ccec9bc6f3982c7a96b819
Nick Burris <nburris@chromium.org>
2021-04-01 21:58:21 +0000

    Record CSP violations in payment method names.
    This is a partial revert of crrev.com/c/2720162 due to web compat issues
    that came up. This reverts the behavior of prohibiting CSP violations in
    payment method names, and instead adds a use counter so we can measure
    the potential breakage while we work on a solution.
    
    Bug: 1013080
    Change-Id: I42f926fe55b225414d2e8d6fef9a182f34b90951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2797836
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868693}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -196,6 +196,7 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kGetCurrentBrowsingContextMedia,
           WebFeature::kLaxAllowingUnsafeCookies,
           WebFeature::kOpenWebDatabaseThirdPartyContext,
+          WebFeature::kPaymentRequestCSPViolation,
       }));
   return *opt_in_features;
 }
