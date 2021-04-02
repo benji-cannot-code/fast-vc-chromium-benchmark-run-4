9947e23e4384119e3c49054b2a7eebc0370830c1
Caroline Rising <corising@chromium.org>
2021-04-02 19:55:50 +0000

    Read later/side panel: open reading list item in active tab when side panel is enabled.
    Bug: 1194692
    Change-Id: Id6daff57351cc19f3cb6f05d42bfde9d7547752c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2802512
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868949}

--- chrome/browser/ui/webui/read_later/read_later_page_handler.cc
+++ chrome/browser/ui/webui/read_later/read_later_page_handler.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/read_later/reading_list_model_factory.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/webui/read_later/read_later_ui.h"
 #include "chrome/common/webui_url_constants.h"
 #include "components/reading_list/core/reading_list_entry.h"
@@ -88,8 +89,10 @@ void ReadLaterPageHandler::OpenSavedEntry(const GURL& url) {
 
   // Open in active tab if the user is on the NTP.
   WindowOpenDisposition open_location =
-      IsActiveTabNTP(browser) ? WindowOpenDisposition::CURRENT_TAB
-                              : WindowOpenDisposition::NEW_FOREGROUND_TAB;
+      IsActiveTabNTP(browser) ||
+              base::FeatureList::IsEnabled(features::kSidePanel)
+          ? WindowOpenDisposition::CURRENT_TAB
+          : WindowOpenDisposition::NEW_FOREGROUND_TAB;
 
   content::OpenURLParams params(url, content::Referrer(), open_location,
                                 ui::PAGE_TRANSITION_AUTO_BOOKMARK, false);
