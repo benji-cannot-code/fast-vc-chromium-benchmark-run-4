38b5319f164d2973a945703e98a5faff441d1892
Xinghui Lu <xinghuilu@chromium.org>
2021-04-02 21:33:13 +0000

    Sanitize referrer chain in real time URL requests.
    If subresource URLs cannot be sent due to user consent restriction,
    the subframe URLs should be removed in referrer chains.
    
    Bug: 1161342
    Change-Id: I989dc6d3cfac8f833180965a97ab6f41897e2d8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798251
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868981}

--- components/safe_browsing/core/realtime/url_lookup_service_base.h
+++ components/safe_browsing/core/realtime/url_lookup_service_base.h
@@ -39,6 +39,9 @@ using RTLookupRequestCallback =
 using RTLookupResponseCallback =
     base::OnceCallback<void(bool, bool, std::unique_ptr<RTLookupResponse>)>;
 
+using ReferrerChain =
+    google::protobuf::RepeatedPtrField<safe_browsing::ReferrerChainEntry>;
+
 class VerdictCacheManager;
 class ReferrerChainProvider;
 
@@ -112,6 +115,9 @@ class RealTimeUrlLookupServiceBase : public KeyedService {
  private:
   using PendingRTLookupRequests =
       base::flat_map<network::SimpleURLLoader*, RTLookupResponseCallback>;
+
+  // Removes non-mainframe URLs due to user consent restriction.
+  static void SanitizeReferrerChainEntries(ReferrerChain* referrer_chain);
 
   // Returns the endpoint that the URL lookup will be sent to.
   virtual GURL GetRealTimeLookupUrl() const = 0;
