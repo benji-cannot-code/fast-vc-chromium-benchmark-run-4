e80ceb2f4f5fb8d10553956af8df14b23174e616
Mitsuru Oshima <oshima@chromium.org>
2021-04-02 18:55:02 +0000

    Fix flaky LayerUtilTset
    Check if the callback is valid on the thread The copy request callback can be called on non UI thread during shutdown.
    
    Also set the animation speed to normal and the animation advance longer.
    
    Bug: 1177129
    Change-Id: I0412c5b16472605527863fdc3a5597241b27b9e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2801853
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868933}

--- ash/utility/layer_util_unittest.cc
+++ ash/utility/layer_util_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/layer.h"
+#include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/compositor/test/test_compositor_host.h"
 #include "ui/compositor/test/test_context_factories.h"
 #include "ui/gfx/geometry/rect.h"
@@ -64,8 +65,10 @@ class LayerUtilTest : public testing::Test {
 
 }  // namespace
 
-// TODO(crbug.com/1177129) Re-enable test
-TEST_F(LayerUtilTest, DISABLED_CopyContentToExistingLayer) {
+TEST_F(LayerUtilTest, CopyContentToExistingLayer) {
+  ui::ScopedAnimationDurationScaleMode non_zero(
+      ui::ScopedAnimationDurationScaleMode::NORMAL_DURATION);
+
   ui::Layer layer1;
   layer1.SetBounds(gfx::Rect(100, 100));
   root_layer()->Add(&layer1);
@@ -101,7 +104,7 @@ TEST_F(LayerUtilTest, DISABLED_CopyContentToExistingLayer) {
     cancelable.Cancel();
 
     GenerateOneFrame();
-    Advance(base::TimeDelta::FromMilliseconds(16));
+    Advance(base::TimeDelta::FromMilliseconds(1000));
     EXPECT_FALSE(called);
   }
 }
