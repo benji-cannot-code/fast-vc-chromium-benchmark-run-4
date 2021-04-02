2078f43e92faf81519b044517463307798fcc142
Erik Chen <erikchen@chromium.org>
2021-04-02 21:03:31 +0000

    lacros: Refactor initial browser action to be an enum.
    This CL switches the previous set of bools to an enum. This is in
    preparation for a new enum that is mutually exclusive with the previous
    bools, which would allow the formation of a syntactically correct but
    semantically incorrect IPC message.
    
    Bug: 1194187
    Change-Id: I9a3e4566da82b0aef3b1dccd5c348747c1c6c52b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2796013
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868971}

--- chrome/browser/ash/crosapi/browser_manager.cc
+++ chrome/browser/ash/crosapi/browser_manager.cc
@@ -242,8 +242,8 @@ void BrowserManager::SetLoadCompleteCallback(LoadCompleteCallback callback) {
 
 void BrowserManager::NewWindow(bool incognito) {
   auto result = MaybeStart(
-      incognito ? browser_util::InitialBrowserAction::kOpenIncognitoWindow
-                : browser_util::InitialBrowserAction::kOpenWindow);
+      incognito ? mojom::InitialBrowserAction::kOpenIncognitoWindow
+                : mojom::InitialBrowserAction::kUseStartupPreference);
   if (result != MaybeStartResult::kRunning)
     return;
 
@@ -255,7 +255,7 @@ void BrowserManager::NewWindow(bool incognito) {
 }
 
 void BrowserManager::NewTab() {
-  auto result = MaybeStart(browser_util::InitialBrowserAction::kOpenWindow);
+  auto result = MaybeStart(mojom::InitialBrowserAction::kUseStartupPreference);
   if (result != MaybeStartResult::kRunning)
     return;
 
@@ -267,8 +267,7 @@ void BrowserManager::NewTab() {
 }
 
 void BrowserManager::RestoreTab() {
-  auto result =
-      MaybeStart(browser_util::InitialBrowserAction::kRestoreLastSession);
+  auto result = MaybeStart(mojom::InitialBrowserAction::kRestoreLastSession);
   if (result != MaybeStartResult::kRunning)
     return;
 
@@ -364,7 +363,7 @@ BrowserManager::BrowserServiceInfo::operator=(const BrowserServiceInfo&) =
 BrowserManager::BrowserServiceInfo::~BrowserServiceInfo() = default;
 
 BrowserManager::MaybeStartResult BrowserManager::MaybeStart(
-    browser_util::InitialBrowserAction initial_browser_action) {
+    mojom::InitialBrowserAction initial_browser_action) {
   if (!browser_util::IsLacrosEnabled())
     return MaybeStartResult::kNotStarted;
 
@@ -416,8 +415,7 @@ BrowserManager::MaybeStartResult BrowserManager::MaybeStart(
   return MaybeStartResult::kRunning;
 }
 
-void BrowserManager::Start(
-    browser_util::InitialBrowserAction initial_browser_action) {
+void BrowserManager::Start(mojom::InitialBrowserAction initial_browser_action) {
   DCHECK_EQ(state_, State::STOPPED);
   DCHECK(!lacros_path_.empty());
   // Ensure we're not trying to open a window before the shelf is initialized.
@@ -432,7 +430,7 @@ void BrowserManager::Start(
 }
 
 void BrowserManager::StartWithLogFile(
-    browser_util::InitialBrowserAction initial_browser_action,
+    mojom::InitialBrowserAction initial_browser_action,
     base::ScopedFD logfd) {
   DCHECK_EQ(state_, State::CREATING_LOG_FILE);
 
@@ -689,7 +687,7 @@ void BrowserManager::OnLoadComplete(const base::FilePath& path) {
   }
 
   if (state_ == State::STOPPED && GetLaunchOnLoginPref())
-    Start(browser_util::InitialBrowserAction::kOpenWindow);
+    Start(mojom::InitialBrowserAction::kUseStartupPreference);
 }
 
 void BrowserManager::SetDeviceAccountPolicy(const std::string& policy_blob) {
@@ -720,7 +718,7 @@ void BrowserManager::LaunchForKeepAliveIfNecessary() {
     // TODO(https://crbug.com/1194187): Call start with a different initial
     // browser action. We need this even though UnlauchForKeepAlive is not
     // implemented for unit testing.
-    Start(browser_util::InitialBrowserAction::kOpenIncognitoWindow);
+    Start(mojom::InitialBrowserAction::kOpenIncognitoWindow);
   }
 }
 
