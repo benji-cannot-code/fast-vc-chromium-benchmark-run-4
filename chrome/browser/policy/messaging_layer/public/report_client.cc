54a45e71b1288c0eb2b36d5689224974a9e3d829
Leonid Baraz <lbaraz@chromium.org>
2021-04-02 05:39:57 +0000

    Extend ReportClient to select backend.
    Depending on features set, it will choose local or missive storage
    module and whether or not to have uploader capability.
    
    Bug: 1174889
    Change-Id: Iaa066e671c59094fe0348a2b72f29ef99603b716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798028
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Auto-Submit: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/master@{#868810}

--- chrome/browser/policy/messaging_layer/public/report_client.cc
+++ chrome/browser/policy/messaging_layer/public/report_client.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/task/thread_pool.h"
 #include "base/threading/sequence_bound.h"
-#include "build/chromeos_buildflags.h"
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/policy/messaging_layer/public/report_queue_impl.h"
 #include "chrome/browser/policy/messaging_layer/util/get_cloud_policy_client.h"
@@ -38,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/reporting/storage/storage_module.h"
 #include "components/reporting/storage/storage_module_interface.h"
 #include "components/reporting/storage/storage_uploader_interface.h"
+#include "components/reporting/storage_selector/storage_selector.h"
 #include "components/reporting/util/status.h"
 #include "components/reporting/util/status_macros.h"
 #include "components/reporting/util/statusor.h"
@@ -199,7 +199,14 @@ ReportingClient::ClientInitializingContext::~ClientInitializingContext() =
     default;
 
 void ReportingClient::ClientInitializingContext::OnStart() {
-  // CloudPolicyClient requires posting to the main UI thread.
+  if (!StorageSelector::is_uploader_required()) {
+    // Uploading is disabled, proceed with no CloudPolicyClient.
+    ConfigureStorageModule();
+    return;
+  }
+
+  // Uploading is enabled, it will need CloudPolicyClient, which requires
+  // posting to the main UI thread for getting access to it.
   content::GetUIThreadTaskRunner({})->PostTask(
       FROM_HERE,
       base::BindOnce(
@@ -230,20 +237,17 @@ void ReportingClient::ClientInitializingContext::OnCloudPolicyClientConfigured(
 }
 
 void ReportingClient::ClientInitializingContext::ConfigureStorageModule() {
+  // Storage location in the local file system (if local storage is enabled).
   base::FilePath user_data_dir;
   if (!base::PathService::Get(chrome::DIR_USER_DATA, &user_data_dir)) {
     Complete(
         Status(error::FAILED_PRECONDITION, "Could not retrieve base path"));
     return;
   }
-
   base::FilePath reporting_path = user_data_dir.Append(kReportingDirectory);
-  StorageModule::Create(
-      StorageOptions()
-          .set_directory(reporting_path)
-          .set_signature_verification_public_key(
-              SignatureVerifier::VerificationKey()),
-      std::move(async_start_upload_cb_), EncryptionModule::Create(),
+
+  StorageSelector::CreateStorageModule(
+      reporting_path, std::move(async_start_upload_cb_),
       base::BindOnce(&ClientInitializingContext::OnStorageModuleConfigured,
                      base::Unretained(this)));
 }
@@ -258,6 +262,12 @@ void ReportingClient::ClientInitializingContext::OnStorageModuleConfigured(
   }
 
   storage_ = storage_result.ValueOrDie();
+  if (!cloud_policy_client_) {
+    // No policy client - no uploader needed (uploads will not be forwarded).
+    Complete(Status::StatusOK());
+    return;
+  }
+
   UploadClient::Create(
       cloud_policy_client_,
       base::BindRepeating(&StorageModuleInterface::ReportSuccess, storage_),
