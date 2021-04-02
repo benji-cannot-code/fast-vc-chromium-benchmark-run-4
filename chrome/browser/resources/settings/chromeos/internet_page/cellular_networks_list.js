e6512da947f7f94cce2db998e59693cabbf42173
Gordon Seto <gordonseto@google.com>
2021-04-02 23:56:24 +0000

    [CrOS Settings] Hide pSIM section when no pSIM slot.
    Hide the pSIM section in cellular-networks-list if the number of pSIM
    slots on the device is zero.
    
    Fixed: 1184912
    Change-Id: I94b29e7203281b3e752ea0a785dba8ff20d9600b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2802885
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/master@{#869017}

--- chrome/browser/resources/settings/chromeos/internet_page/cellular_networks_list.js
+++ chrome/browser/resources/settings/chromeos/internet_page/cellular_networks_list.js
@@ -327,6 +327,16 @@ Polymer({
   },
 
   /**
+   * @param {!OncMojo.DeviceStateProperties|undefined} deviceState
+   * @returns {boolean}
+   * @private
+   */
+  shouldShowPSimSection_(deviceState) {
+    const {pSimSlots} = getSimSlotCount(deviceState);
+    return pSimSlots > 0;
+  },
+
+  /**
    * @param {!settings.MultiDevicePageContentData} newData
    * @private
    */
--- chrome/test/data/webui/settings/chromeos/cellular_networks_list_test.js
+++ chrome/test/data/webui/settings/chromeos/cellular_networks_list_test.js
@@ -59,6 +59,18 @@ suite('CellularNetworksList', function() {
     cellularNetworkList.networks = networks;
   }
 
+  function addPSimSlot() {
+    cellularNetworkList.set('deviceState.simInfos', [{
+                              iccid: '',
+                            }]);
+    return flushAsync();
+  }
+
+  function removePSimSlot() {
+    cellularNetworkList.set('deviceState.simInfos', []);
+    return flushAsync();
+  }
+
   function flushAsync() {
     Polymer.dom.flush();
     // Use setTimeout to wait for the next macrotask.
@@ -86,6 +98,7 @@ suite('CellularNetworksList', function() {
     ]);
 
     eSimManagerRemote.addEuiccForTest(2);
+    addPSimSlot();
 
     await flushAsync();
 
@@ -112,7 +125,7 @@ suite('CellularNetworksList', function() {
         setNetworksForTest(mojom.NetworkType.kCellular, [
           OncMojo.getDefaultNetworkState(mojom.NetworkType.kTether, 'tether1'),
         ]);
-        Polymer.dom.flush();
+        addPSimSlot();
 
         await flushAsync();
         const esimNoNetworkAnchor =
@@ -197,6 +210,21 @@ suite('CellularNetworksList', function() {
     const esimNetworkList = cellularNetworkList.$$('#esimNetworkList');
 
     assertFalse(!!esimNetworkList);
+  });
+
+  test('Hide pSIM section when no pSIM slots', async () => {
+    init();
+    setNetworksForTest(mojom.NetworkType.kCellular, [
+      OncMojo.getDefaultNetworkState(mojom.NetworkType.kTether, 'tether1'),
+    ]);
+    await flushAsync();
+    assertFalse(!!cellularNetworkList.$$('#pSimNoNetworkFound'));
+
+    addPSimSlot();
+    assertTrue(!!cellularNetworkList.$$('#pSimNoNetworkFound'));
+
+    removePSimSlot();
+    assertFalse(!!cellularNetworkList.$$('#pSimNoNetworkFound'));
   });
 
   test('Hide instant tethering section when not enabled', async () => {
