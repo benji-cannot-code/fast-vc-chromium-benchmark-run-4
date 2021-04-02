32ea14d1ea8fa3cd3dfd0b9551b1afe29486c5c4
Tarun Bansal <tbansal@chromium.org>
2021-04-02 21:12:59 +0000

    Change network requirement for model downloads
    Bug: 1195423
    Change-Id: I40ae25986bcd81bc21a50a7f02996d6a7dd4666b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2802918
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868974}

--- chrome/browser/optimization_guide/prediction/prediction_model_download_manager_unittest.cc
+++ chrome/browser/optimization_guide/prediction/prediction_model_download_manager_unittest.cc
@@ -292,7 +292,7 @@ TEST_F(PredictionModelDownloadManagerTest, StartDownloadRestrictedDownloading) {
       download_params.scheduling_params.battery_requirements,
       download::SchedulingParams::BatteryRequirements::BATTERY_INSENSITIVE);
   EXPECT_EQ(download_params.scheduling_params.network_requirements,
-            download::SchedulingParams::NetworkRequirements::OPTIMISTIC);
+            download::SchedulingParams::NetworkRequirements::NONE);
 
   // Now invoke start callback.
   std::move(download_params.callback)
