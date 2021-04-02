42dc438338b5e6a81d475292635ca00dd1a2eb0b
Justin Donnelly <jdonnelly@chromium.org>
2021-04-02 17:00:23 +0000

    [omnibox] Add a chrome://flags entry for the Pedals defaut color icon.
    Bug: 893183
    Change-Id: If3b300dd90987c0d76978c4f1f83cce7ded2f9d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2801463
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868875}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2069,6 +2069,9 @@ extern const char kOmniboxPedalSuggestionsDescription[];
 extern const char kOmniboxPedalsBatch2Name[];
 extern const char kOmniboxPedalsBatch2Description[];
 
+extern const char kOmniboxPedalsDefaultIconColoredName[];
+extern const char kOmniboxPedalsDefaultIconColoredDescription[];
+
 extern const char kOmniboxKeywordSearchButtonName[];
 extern const char kOmniboxKeywordSearchButtonDescription[];
 
