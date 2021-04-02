d9e646b80b2367c6aa20bb2e8371f7a29601c56f
Zentaro Kavanagh <zentaro@chromium.org>
2021-04-02 17:09:15 +0000

    Accelerators: Make new/remove desks use actual shortcut
    - Behind flag for improved shortcuts
    - Since F-Key rewrites were removed the shortcuts for virtual
      desks need to be updated
    - They were nominally Search+Shift+[MINUS|PLUS] but due to event
      rewriting they were actually implemented as Shift+[F11|F12]
    - Registers "real" shortcut when kImprovedKeyboardShortcuts is
      enabled
    - Does not register F-Key version when enabled
    - Add tests for both directions of the feature flag
    
    Bug: 1174326
    Test: ash_unittests --gtest_filter=Accelerator*Test.*
    Change-Id: I2ba398ebdfecb54f09a06b736331176635bc4893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2800747
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868891}

--- ash/accelerators/accelerator_controller_impl.cc
+++ ash/accelerators/accelerator_controller_impl.cc
@@ -1854,7 +1854,10 @@ void AcceleratorControllerImpl::Init() {
 
   RegisterAccelerators(kAcceleratorData, kAcceleratorDataLength);
 
-  if (::features::IsNewShortcutMappingEnabled()) {
+  if (::features::IsImprovedKeyboardShortcutsEnabled()) {
+    RegisterAccelerators(kEnableWithPositionalAcceleratorsData,
+                         kEnableWithPositionalAcceleratorsDataLength);
+  } else if (::features::IsNewShortcutMappingEnabled()) {
     RegisterAccelerators(kEnableWithNewMappingAcceleratorData,
                          kEnableWithNewMappingAcceleratorDataLength);
   } else {
