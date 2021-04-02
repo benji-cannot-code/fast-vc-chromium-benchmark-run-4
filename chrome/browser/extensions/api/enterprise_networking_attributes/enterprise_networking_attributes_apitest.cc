1f307fed96f70caca862f7a6df484fab1759ebf1
John L Chen <zuan@chromium.org>
2021-04-02 08:54:42 +0000

    Adding missing includes for shill's dbus constants
    BUG=b:184234270
    TEST=Build OK && Unit/browser test passes
    
    Change-Id: I61586fbfeb34068928f53f4b48fdef3bd71ed428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2800332
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: John L Chen <zuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868843}

--- chrome/browser/extensions/api/enterprise_networking_attributes/enterprise_networking_attributes_apitest.cc
+++ chrome/browser/extensions/api/enterprise_networking_attributes/enterprise_networking_attributes_apitest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/shill/shill_profile_client.h"
 #include "chromeos/dbus/shill/shill_service_client.h"
 #include "content/public/test/browser_test.h"
+#include "third_party/cros_system_api/dbus/shill/dbus-constants.h"
 #include "url/gurl.h"
 
 namespace {
