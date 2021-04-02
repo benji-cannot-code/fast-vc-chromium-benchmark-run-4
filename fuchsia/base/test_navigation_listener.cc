a1508a7c2f48180778c7170f6a151610f8dc280c
Wez <wez@chromium.org>
2021-04-02 20:34:31 +0000

    [fuchsia] Clean up TestNavigationListener.
    Fix the TestNavigationListener::RunUntilNavigationStateMatches() API to
    only spin the RunLoop once, so that failing tests timeout properly.
    
    Also fix the order of definition of some of the class member functions.
    
    Change-Id: I7d31f833730c1f0dadb4ec0cbb801d9f1eb08588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2799720
    Auto-Submit: Wez <wez@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868965}

--- fuchsia/base/test_navigation_listener.cc
+++ fuchsia/base/test_navigation_listener.cc
@@ -16,19 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fuchsia/base/mem_buffer_util.h"
 
 namespace cr_fuchsia {
-namespace {
-
-void QuitRunLoopAndRunCallback(
-    base::OnceClosure quit_run_loop_closure,
-    TestNavigationListener::BeforeAckCallback before_ack_callback,
-    const fuchsia::web::NavigationState& change,
-    fuchsia::web::NavigationEventListener::OnNavigationStateChangedCallback
-        ack_callback) {
-  std::move(quit_run_loop_closure).Run();
-  before_ack_callback.Run(change, std::move(ack_callback));
-}
-
-}  // namespace
 
 TestNavigationListener::TestNavigationListener() {
   // Set up the default acknowledgement handling behavior.
@@ -42,13 +29,17 @@ void TestNavigationListener::RunUntilNavigationStateMatches(
   DCHECK(before_ack_);
 
   // Spin the runloop until the expected conditions are met.
-  while (!AllFieldsMatch(expected_state)) {
-    base::RunLoop run_loop;
-    base::AutoReset<BeforeAckCallback> callback_setter(
-        &before_ack_, base::BindRepeating(&QuitRunLoopAndRunCallback,
-                                          run_loop.QuitClosure(), before_ack_));
-    run_loop.Run();
-  }
+  if (AllFieldsMatch(expected_state))
+    return;
+
+  base::RunLoop run_loop;
+  base::AutoReset<BeforeAckCallback> callback_setter(
+      &before_ack_,
+      base::BindRepeating(&TestNavigationListener::QuitLoopIfAllFieldsMatch,
+                          base::Unretained(this),
+                          base::Unretained(&expected_state),
+                          run_loop.QuitClosure(), before_ack_));
+  run_loop.Run();
 }
 
 void TestNavigationListener::RunUntilLoaded() {
@@ -105,6 +96,16 @@ void TestNavigationListener::RunUntilUrlTitleBackForwardEquals(
   state.set_can_go_back(expected_can_go_back);
   state.set_can_go_forward(expected_can_go_forward);
   RunUntilNavigationStateMatches(state);
+}
+
+void TestNavigationListener::SetBeforeAckHook(BeforeAckCallback send_ack_cb) {
+  if (send_ack_cb) {
+    before_ack_ = send_ack_cb;
+  } else {
+    before_ack_ = base::BindRepeating(
+        [](const fuchsia::web::NavigationState&,
+           OnNavigationStateChangedCallback callback) { callback(); });
+  }
 }
 
 void TestNavigationListener::OnNavigationStateChanged(
@@ -173,16 +174,6 @@ void TestNavigationListener::OnNavigationStateChanged(
   before_ack_.Run(change, std::move(callback));
 }
 
-void TestNavigationListener::SetBeforeAckHook(BeforeAckCallback send_ack_cb) {
-  if (send_ack_cb) {
-    before_ack_ = send_ack_cb;
-  } else {
-    before_ack_ = base::BindRepeating(
-        [](const fuchsia::web::NavigationState&,
-           OnNavigationStateChangedCallback callback) { callback(); });
-  }
-}
-
 bool TestNavigationListener::AllFieldsMatch(
     const fuchsia::web::NavigationState& expected) {
   if (expected.has_url() &&
@@ -221,6 +212,18 @@ bool TestNavigationListener::AllFieldsMatch(
   }
 
   return true;
+}
+
+void TestNavigationListener::QuitLoopIfAllFieldsMatch(
+    const fuchsia::web::NavigationState* expected_state,
+    base::RepeatingClosure quit_run_loop_closure,
+    TestNavigationListener::BeforeAckCallback before_ack_callback,
+    const fuchsia::web::NavigationState& change,
+    fuchsia::web::NavigationEventListener::OnNavigationStateChangedCallback
+        ack_callback) {
+  if (AllFieldsMatch(*expected_state))
+    quit_run_loop_closure.Run();
+  before_ack_callback.Run(change, std::move(ack_callback));
 }
 
 }  // namespace cr_fuchsia
