0554aeaba81562da8513a794d25c5c64175c1ed2
dpapad <dpapad@chromium.org>
2021-04-02 22:04:46 +0000

    Auto-generate grd file for chrome://internals/notifications
    Bug: 1132403
    Change-Id: I3372194b41947ea04d2d2f831080db14c791643b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2796638
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868990}

--- chrome/browser/ui/webui/internals/internals_ui.cc
+++ chrome/browser/ui/webui/internals/internals_ui.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/dev_ui_browser_resources.h"
+#include "chrome/grit/internals_resources.h"
+#include "chrome/grit/internals_resources_map.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_controller.h"
 #include "content/public/browser/web_ui_data_source.h"
@@ -16,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/internals/query_tiles/query_tiles_internals_ui_message_handler.h"
 #else
 #include "chrome/browser/ui/webui/internals/web_app/web_app_internals_page_handler_impl.h"
-#include "chrome/grit/internals_resources.h"
-#include "chrome/grit/internals_resources_map.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #endif  // defined(OS_ANDROID)
 
@@ -25,18 +25,23 @@ InternalsUI::InternalsUI(content::WebUI* web_ui)
     : ui::MojoWebUIController(web_ui, /*enable_chrome_send=*/true) {
   profile_ = Profile::FromWebUI(web_ui);
   source_ = content::WebUIDataSource::Create(chrome::kChromeUIInternalsHost);
+  source_->AddResourcePaths(
+      base::make_span(kInternalsResources, kInternalsResourcesSize));
 
   // Add your sub-URL internals WebUI here.
   // Keep this set of sub-URLs in sync with |kChromeInternalsPathURLs|.
 #if defined(OS_ANDROID)
   // chrome://internals/notifications
-  AddNotificationsInternals(web_ui);
+  source_->AddResourcePath(
+      "notifications",
+      IDR_NOTIFICATIONS_INTERNALS_NOTIFICATIONS_INTERNALS_HTML);
+  web_ui->AddMessageHandler(
+      std::make_unique<NotificationsInternalsUIMessageHandler>(profile_));
+
   // chrome://internals/query-tiles
   if (!profile_->IsOffTheRecord())
     AddQueryTilesInternals(web_ui);
 #else
-  source_->AddResourcePaths(
-      base::make_span(kInternalsResources, kInternalsResourcesSize));
   source_->AddResourcePath("hello-ts", IDR_HELLO_TS_HELLO_TS_HTML);
 
   // chrome://internals/web-app
@@ -49,17 +54,6 @@ InternalsUI::InternalsUI(content::WebUI* web_ui)
 InternalsUI::~InternalsUI() = default;
 
 #if defined(OS_ANDROID)
-void InternalsUI::AddNotificationsInternals(content::WebUI* web_ui) {
-  source_->AddResourcePath("notifications_internals.js",
-                           IDR_NOTIFICATIONS_INTERNALS_JS);
-  source_->AddResourcePath("notifications_internals_browser_proxy.js",
-                           IDR_NOTIFICATIONS_INTERNALS_BROWSER_PROXY_JS);
-  source_->AddResourcePath("notifications", IDR_NOTIFICATIONS_INTERNALS_HTML);
-
-  web_ui->AddMessageHandler(
-      std::make_unique<NotificationsInternalsUIMessageHandler>(profile_));
-}
-
 void InternalsUI::AddQueryTilesInternals(content::WebUI* web_ui) {
   source_->AddResourcePath("query_tiles_internals.js",
                            IDR_QUERY_TILES_INTERNALS_JS);
