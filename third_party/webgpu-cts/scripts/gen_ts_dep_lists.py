5ea99c917f7b8d37d7b0cb63a1d45297ae3c1ce1
Austin Eng <enga@chromium.org>
2021-04-02 21:25:49 +0000

    Reland "WebGPU CTS: Remove manually compiled JS sources"
    This is a reland of fec81b1196b92685c8f3ac66aa36e63c81420004
    It updates third_party/webgpu-cts/BUILD.gn to have a single
    action for compiling the CTS, instead of two actions where
    the second action overwrites a file from the first.
    
    Bug: chromium:1186320
    
    Original change's description:
    > WebGPU CTS: Remove manually compiled JS sources
    >
    > Typescript sources are transpiled into /gen during the build.
    > wpt_internal/webgpu/cts.html loads the generated JS files from /gen
    > during test runs.
    >
    > Bug: chromium:1186320
    > Change-Id: Ie9d1ba977017bcfebc1b41960eb8055874845617
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2787749
    > Commit-Queue: Austin Eng <enga@chromium.org>
    > Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    > Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#868818}
    
    Bug: chromium:1186320
    Change-Id: I5797dbcf0100741dfd38d315c95230922276736d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2802759
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#868980}

--- third_party/webgpu-cts/scripts/gen_ts_dep_lists.py
+++ third_party/webgpu-cts/scripts/gen_ts_dep_lists.py
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
 import sys
 
-from compile_src import run_tsc_ignore_errors
+from tsc_ignore_errors import run_tsc_ignore_errors
 
 third_party_dir = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
