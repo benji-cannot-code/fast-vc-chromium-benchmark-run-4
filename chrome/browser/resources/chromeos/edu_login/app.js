6abdfa3baf47428e6577e9116d2a48e4faaca4c0
Toby Huang <tobyhuang@chromium.org>
2021-04-03 00:44:17 +0000

    Metrics: Deprecate histogram AccountManager.EduCoexistence.FlowResult
    Remove UMA metrics code rendered obsolete after V2 EduCoexistence.
    This metric has been replaced by AccountManager.EduCoexistenceV2.
    {InSessionFlowResult, OnboardingFlowResult} in V2.
    
    Bug: 1169358
    Change-Id: Ifaff2162271ef7163566d6dfee8989163cb3f1dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798964
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869030}

--- chrome/browser/resources/chromeos/edu_login/app.js
+++ chrome/browser/resources/chromeos/edu_login/app.js
@@ -15,7 +15,7 @@ import {assert} from 'chrome://resources/js/assert.m.js';
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {EduAccountLoginBrowserProxyImpl} from './browser_proxy.js';
-import {EduCoexistenceFlowResult, EduLoginErrorType, EduLoginParams, ParentAccount} from './edu_login_util.js';
+import {EduLoginErrorType, EduLoginParams, ParentAccount} from './edu_login_util.js';
 
 /** @enum {string} */
 const Steps = {
@@ -28,15 +28,6 @@ const Steps = {
 
 /** @type {!Array<Steps>} */
 const stepsArray = Object.values(Steps);
-
-/** @type {!Map<!Steps, !EduCoexistenceFlowResult>} */
-const stepToFlowResultMap = new Map([
-  [Steps.PARENTS, EduCoexistenceFlowResult.PARENTS_LIST_SCREEN],
-  [Steps.PARENT_SIGNIN, EduCoexistenceFlowResult.PARENT_PASSWORD_SCREEN],
-  [Steps.COEXISTENCE_INFO, EduCoexistenceFlowResult.PARENT_INFO_SCREEN1],
-  [Steps.PARENT_INFO, EduCoexistenceFlowResult.PARENT_INFO_SCREEN2],
-  [Steps.EDU_LOGIN, EduCoexistenceFlowResult.EDU_ACCOUNT_LOGIN_SCREEN],
-]);
 
 Polymer({
   is: 'edu-login-app',
@@ -97,7 +88,6 @@ Polymer({
   /** @override */
   ready() {
     this.switchViewAtIndex_(this.stepIndex_);
-    this.updateEduCoexistenceFlowResult_(this.stepIndex_);
   },
 
   /**
@@ -108,7 +98,6 @@ Polymer({
     assert(this.stepIndex_ < stepsArray.length - 1);
     ++this.stepIndex_;
     this.switchViewAtIndex_(this.stepIndex_);
-    this.updateEduCoexistenceFlowResult_(this.stepIndex_);
   },
 
   /**
@@ -119,7 +108,6 @@ Polymer({
     assert(this.stepIndex_ > 0);
     --this.stepIndex_;
     this.switchViewAtIndex_(this.stepIndex_);
-    this.updateEduCoexistenceFlowResult_(this.stepIndex_);
   },
 
   /**
@@ -130,19 +118,6 @@ Polymer({
   switchViewAtIndex_(index) {
     /** @type {CrViewManagerElement} */ (this.$.viewManager)
         .switchView(stepsArray[index]);
-  },
-
-  /**
-   * Sends new value to update EduCoexistenceFlowResult.
-   * @param {number} index of the step shown.
-   * @private
-   */
-  updateEduCoexistenceFlowResult_(index) {
-    /** @type {EduCoexistenceFlowResult} */
-    const result = stepToFlowResultMap.get(stepsArray[index]);
-    assert(result !== undefined);
-    EduAccountLoginBrowserProxyImpl.getInstance()
-        .updateEduCoexistenceFlowResult(result);
   },
 
   /**
