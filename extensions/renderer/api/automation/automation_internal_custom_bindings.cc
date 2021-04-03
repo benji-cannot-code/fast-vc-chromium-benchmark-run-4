f3050939709c227d548c8df5e5b0d0ca21151c83
Dominic Mazzoni <dmazzoni@chromium.org>
2021-04-03 00:23:31 +0000

    Suppress a noisy log statement in non-debug builds.
    Bug: 1167762
    Change-Id: I93f6eb59489562838b273b86ed010a9d4a8ac598
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2803572
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869026}

--- extensions/renderer/api/automation/automation_internal_custom_bindings.cc
+++ extensions/renderer/api/automation/automation_internal_custom_bindings.cc
@@ -2354,7 +2354,7 @@ void AutomationInternalCustomBindings::OnAccessibilityEvents(
   }
 
   if (!tree_wrapper->OnAccessibilityEvents(event_bundle, is_active_profile)) {
-    LOG(ERROR) << tree_wrapper->tree()->error();
+    DLOG(ERROR) << tree_wrapper->tree()->error();
     base::ListValue args;
     args.AppendString(tree_id.ToString());
     bindings_system_->DispatchEventInContext(
