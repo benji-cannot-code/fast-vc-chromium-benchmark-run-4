3d5d33d89465e4a4485b9a719f596fcb1d1fa70e
David Tseng <dtseng@google.com>
2021-04-05 00:47:12 +0000

    Save the last focused window in AXAuraObjCache
    In Lacros Chrome, some root windows'  focus client return nullptr when asked for their focused window when one is expected. In
    contrast, for Ash Chrome, root windows always contain a focus client with a focused window.
    
    This change makes AXAuraObjCache save the last focused window, which it
    receives as part of being an FocusChangeObserver . The cache observes
    all incoming root windows it creates as part of OnRootWindowObjCreated.
    
    R=dmazzoni@chromium.org
    
    Bug: 1185764
    Test: all exo and non-exo shell surfaces: (Lacros, ARC++) and (ordinary non-Lacros browser windows).
    Change-Id: Id035564e90d7c49dd8bf003007c76f35a9e14872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2801464
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869072}

--- ui/views/accessibility/ax_aura_obj_cache.h
+++ ui/views/accessibility/ax_aura_obj_cache.h
@@ -159,6 +159,8 @@ class VIEWS_EXPORT AXAuraObjCache : public aura::client::FocusChangeObserver {
 
   std::set<aura::Window*> root_windows_;
 
+  aura::Window* last_focused_window_ = nullptr;
+
   views::Widget* focused_widget_for_testing_ = nullptr;
 };
 
