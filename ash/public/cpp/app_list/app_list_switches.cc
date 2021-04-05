2197bdf1241c82a0b2d0f11703a0bcd7ae7ce944
James Cook <jamescook@chromium.org>
2021-04-05 21:01:48 +0000

    chromeos: Delete unused app list switches
    Most are unreferenced in the code.
    
    --disable-app-list-dismiss-on-blur is used in theory, but I don't think
    anyone sets it. It's not referenced in Chrome OS source or google3
    source. The commit that added it said it was for Linux and Windows:
    https://codereview.chromium.org/661003003
    
    Bug: 942564
    Change-Id: Ia607c0961fd0244faf982902159e48776a843ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2806303
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869271}

--- ash/public/cpp/app_list/app_list_switches.cc
+++ ash/public/cpp/app_list/app_list_switches.cc
@@ -5,22 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/app_list/app_list_switches.h"
 
-#include "base/command_line.h"
-
 namespace ash {
 namespace switches {
-
-// Specifies the chrome-extension:// URL for the contents of an additional page
-// added to the app launcher.
-const char kCustomLauncherPage[] = "custom-launcher-page";
-
-// If set, the app list will not be dismissed when it loses focus. This is
-// useful when testing the app list or a custom launcher page. It can still be
-// dismissed via the other methods (like the Esc key).
-const char kDisableAppListDismissOnBlur[] = "disable-app-list-dismiss-on-blur";
-
-// If set, the app list will be enabled as if enabled from CWS.
-const char kEnableAppList[] = "enable-app-list";
 
 // If set, the CrOSActionRecorder will be enabled which will record some user
 // actions on device.
@@ -36,11 +22,6 @@ const char kCrOSActionRecorderWithoutHash[] = "log-without-hash";
 // Disable structured metrics logging of cros actions.
 const char kCrOSActionRecorderStructuredDisabled[] =
     "structured-metrics-disabled";
-
-bool ShouldNotDismissOnBlur() {
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      kDisableAppListDismissOnBlur);
-}
 
 }  // namespace switches
 }  // namespace ash
