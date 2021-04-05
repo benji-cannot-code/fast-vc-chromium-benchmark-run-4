2dc24263acadb4940795e5d3879136829c69f844
dpapad <dpapad@chromium.org>
2021-04-05 18:30:18 +0000

    WebUI: Check-in Polymer3 version of search_highlight_style_css.js.
    Bug: 1184053
    Change-Id: I5911829383d38144b65b35e7bdb16dfa81c61a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2805556
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869209}

--- ui/webui/resources/cr_elements/search_highlight_style_css.js
+++ ui/webui/resources/cr_elements/search_highlight_style_css.js
@@ -3,16 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// <if expr="chromeos">
-import '//resources/cr_elements/chromeos/cros_color_overrides.m.js';
-// </if>
-import '//resources/cr_elements/search_highlight_style_css.js';
-import '//resources/cr_elements/shared_vars_css.m.js';
-import '//resources/cr_elements/shared_style_css.m.js';
-import './settings_vars_css.js';
-
+import './shared_vars_css.m.js';
 const template = document.createElement('template');
 template.innerHTML = `
-<dom-module id="settings-shared">{__html_template__}</dom-module>
+<dom-module id="search-highlight-style" assetpath="chrome://resources/">
+{__html_template__}
+</dom-module>
 `;
 document.body.appendChild(template.content.cloneNode(true));
