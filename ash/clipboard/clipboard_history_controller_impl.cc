794112af5ddea545ae4591c15879449911c1e952
Andrew Xu <andrewxu@chromium.org>
2021-04-05 21:11:10 +0000

    An api to query whether a rect is in any display
    This CL creates an api to check whether a rect in screen coordinates is
    contained by any display bounds. No visible difference should be
    introduced by this CL.
    
    Bug: 1165302
    Change-Id: Ic8950f7135a52fc77afbd59e23dec0acc38a436d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2806223
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869275}

--- ash/clipboard/clipboard_history_controller_impl.cc
+++ ash/clipboard/clipboard_history_controller_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/clipboard/clipboard_nudge_controller.h"
 #include "ash/clipboard/scoped_clipboard_history_pause_impl.h"
 #include "ash/constants/ash_features.h"
+#include "ash/display/display_util.h"
 #include "ash/public/cpp/clipboard_image_model_factory.h"
 #include "ash/public/cpp/window_tree_host_lookup.h"
 #include "ash/resources/vector_icons/vector_icons.h"
@@ -40,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/webui/web_ui_util.h"
-#include "ui/display/screen.h"
 #include "ui/events/event.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
@@ -60,16 +60,6 @@ ui::ClipboardNonBacked* GetClipboard() {
   auto* clipboard = ui::ClipboardNonBacked::GetForCurrentThread();
   DCHECK(clipboard);
   return clipboard;
-}
-
-bool IsRectContainedByAnyDisplay(const gfx::Rect& rect) {
-  const std::vector<display::Display>& displays =
-      display::Screen::GetScreen()->GetAllDisplays();
-  for (const auto& display : displays) {
-    if (display.bounds().Contains(rect))
-      return true;
-  }
-  return false;
 }
 
 }  // namespace
