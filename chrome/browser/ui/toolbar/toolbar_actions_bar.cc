a56ea61ea859cac7b4885b409c08495d1bb6ac4d
Devlin Cronin <rdevlin.cronin@chromium.org>
2021-04-05 20:55:00 +0000

    [Extensions UI] Remove unused ToolbarActionsBarObserver
    It had no usage. Remove it.
    
    Bug: None
    Change-Id: I053bb0d0ec9f858a55d7617ed658432d2723e9be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2803483
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Auto-Submit: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869269}

--- chrome/browser/ui/toolbar/toolbar_actions_bar.cc
+++ chrome/browser/ui/toolbar/toolbar_actions_bar.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/toolbar/toolbar_action_view_controller.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_bar_delegate.h"
-#include "chrome/browser/ui/toolbar/toolbar_actions_bar_observer.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/pref_names.h"
 #include "components/crx_file/id_util.h"
@@ -126,9 +125,6 @@ ToolbarActionsBar::~ToolbarActionsBar() {
   // Make sure we don't listen to any more model changes during
   // ToolbarActionsBar destruction.
   model_observation_.Reset();
-
-  for (ToolbarActionsBarObserver& observer : observers_)
-    observer.OnToolbarActionsBarDestroyed();
 }
 
 // static
@@ -433,8 +429,6 @@ void ToolbarActionsBar::OnDragEnded() {
 
   DCHECK(is_drag_in_progress());
   index_of_dragged_item_.reset();
-  for (ToolbarActionsBarObserver& observer : observers_)
-    observer.OnToolbarActionDragDone();
 }
 
 void ToolbarActionsBar::OnDragDrop(int dragged_index,
@@ -464,11 +458,6 @@ const base::Optional<size_t> ToolbarActionsBar::IndexOfDraggedItem() const {
 }
 
 void ToolbarActionsBar::OnAnimationEnded() {
-  // Notify the observers now, since showing a bubble or popup could potentially
-  // cause another animation to start.
-  for (ToolbarActionsBarObserver& observer : observers_)
-    observer.OnToolbarActionsBarAnimationEnded();
-
   // Check if we were waiting for animation to complete to either show a
   // message bubble, or to show a popup.
   if (pending_bubble_controller_) {
@@ -565,14 +554,6 @@ void ToolbarActionsBar::HideActivePopup() {
   if (popup_owner_)
     popup_owner_->HidePopup();
   DCHECK(!popup_owner_);
-}
-
-void ToolbarActionsBar::AddObserver(ToolbarActionsBarObserver* observer) {
-  observers_.AddObserver(observer);
-}
-
-void ToolbarActionsBar::RemoveObserver(ToolbarActionsBarObserver* observer) {
-  observers_.RemoveObserver(observer);
 }
 
 void ToolbarActionsBar::ShowToolbarActionBubble(
--- chrome/browser/ui/toolbar/toolbar_actions_bar.h
+++ chrome/browser/ui/toolbar/toolbar_actions_bar.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/observer_list.h"
 #include "base/optional.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/ui/extensions/extensions_container.h"
@@ -30,7 +29,6 @@ class PrefRegistrySyncable;
 
 class BrowserWindow;
 class ToolbarActionsBarDelegate;
-class ToolbarActionsBarObserver;
 class ToolbarActionViewController;
 
 // A platform-independent version of the container for toolbar actions,
@@ -189,10 +187,6 @@ class ToolbarActionsBar : public ExtensionsContainer,
 
   // Called when the active bubble is closed.
   void OnBubbleClosed();
-
-  // Add or remove an observer.
-  void AddObserver(ToolbarActionsBarObserver* observer);
-  void RemoveObserver(ToolbarActionsBarObserver* observer);
 
   // Returns the underlying toolbar actions, but does not order them. Primarily
   // for use in testing.
@@ -366,8 +360,6 @@ class ToolbarActionsBar : public ExtensionsContainer,
   // The index of the action currently being dragged, or |base::nullopt| if
   // no drag is in progress.
   base::Optional<size_t> index_of_dragged_item_;
-
-  base::ObserverList<ToolbarActionsBarObserver>::Unchecked observers_;
 
   base::WeakPtrFactory<ToolbarActionsBar> weak_ptr_factory_{this};
 
