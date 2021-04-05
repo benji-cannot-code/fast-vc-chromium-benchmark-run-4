b356f32a2fabd168db25351a1ba5184d663a95b4
Elly Fong-Jones <ellyjones@chromium.org>
2021-04-05 18:24:57 +0000

    sheriff: disable HarfBuzzShaperTest.EmojiPercentage on macOS 11
    Bug: 1194323
    Change-Id: Ie6013a0d360164ec2a8e64b3910bf6323fc74b6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2805862
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Owners-Override: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869207}

--- third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper_test.cc
+++ third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper_test.cc
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/build_info.h"
 #endif
 
+#if defined(OS_MAC)
+#include "base/mac/mac_util.h"
+#endif
+
 using testing::ElementsAre;
 
 namespace blink {
@@ -1842,6 +1846,10 @@ TEST_F(HarfBuzzShaperTest, ShapeVerticalWithSubpixelPositionIsRounded) {
 }
 
 TEST_F(HarfBuzzShaperTest, EmojiPercentage) {
+#if defined(OS_MAC)
+  if (base::mac::IsOS11())
+    GTEST_SKIP() << "Broken on macOS 11: https://crbug.com/1194323";
+#endif
   // This test relies on Noto Color Emoji from the third_party directory to not
   // contain sequences and single codepoint emoji from Unicode 13 and 13.1 such
   // as:
