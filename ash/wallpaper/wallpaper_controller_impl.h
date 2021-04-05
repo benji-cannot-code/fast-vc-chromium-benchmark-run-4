3162e50b868111a62d908480b2405661966de2f3
Qasid Sadiq <qasid@google.com>
2021-04-05 18:56:03 +0000

    wallpaper: we can't use chrome.storage, use PrefService instead.
    WallpaperInfo is currently stored using chrome.storage, and redundantly
    PrefService in wallpaper_controller_impl, behind
    [Get|Set]UserWallpaperInfo.
    
    chrome.storage was used to sync info across devices. Let's handle
    syncing in the wallpaper_controller through profile prefs.
    
    Migrate to the new prefs in whenever a new profile pref service becomes
    active, and the migration hasn't already happened.
    
    We have two kinds of wallpaperInfo storage.
    kSyncedWallpaperInfo in profile Prefs.
    kLocalWallpaperInfo in local state.
    
    The wallpaper type determines if it belongs in kLocalWallpaperInfo or
    kSyncedWallpaperInfo.
    If a wallpaper is syncable, it is stored in kSyncedWallpaperInfo. The
    info in local state is what is read to display a wallpaper. The info in
    profile prefs is used to sync to other devices and hold onto the info,
    while the actual image becomes available.
    
    Tests: Added wallpaper_controller_unittest.cc tests. testing/xvfb.py
    ./out/Debug/ash_unittests  --gtest_filter="*WallpaperControllerTest*"
    Used logs to verify the data being migrated is the
    correct data. And tested by hand, turning on the feature flag will
    disable syncing in the old app, and enable it in the new place.
    You can see online syncing (as long as the wallpaper is already
    downloaded), and default syncing.
    
    Bug: b/178016374
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ia4132a753598907bddf5e673652a70af1a3ecc11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2714427
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Qasid Sadiq <qasid@google.com>
    Auto-Submit: Qasid Sadiq <qasid@google.com>
    Cr-Commit-Position: refs/heads/master@{#869221}

--- ash/wallpaper/wallpaper_controller_impl.h
+++ ash/wallpaper/wallpaper_controller_impl.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/scoped_observation.h"
 #include "base/timer/timer.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "ui/compositor/compositor_lock.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/native_theme/native_theme.h"
@@ -86,10 +87,17 @@ class ASH_EXPORT WallpaperControllerImpl
   static const char kLargeWallpaperSubDir[];
   static const char kOriginalWallpaperSubDir[];
 
+  // Names of nodes with wallpaper info in |kUserWallpaperInfo| dictionary.
+  static const char kNewWallpaperDateNodeName[];
+  static const char kNewWallpaperLayoutNodeName[];
+  static const char kNewWallpaperLocationNodeName[];
+  static const char kNewWallpaperTypeNodeName[];
+
   explicit WallpaperControllerImpl(PrefService* local_state);
   ~WallpaperControllerImpl() override;
 
   static void RegisterLocalStatePrefs(PrefRegistrySimple* registry);
+  static void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
   // Returns the maximum size of all displays combined in native
   // resolutions.  Note that this isn't the bounds of the display who
@@ -301,6 +309,7 @@ class ASH_EXPORT WallpaperControllerImpl
 
   // SessionObserver:
   void OnSessionStateChanged(session_manager::SessionState state) override;
+  void OnActiveUserPrefServiceChanged(PrefService* pref_service) override;
 
   // TabletModeObserver:
   void OnTabletModeStarted() override;
@@ -332,6 +341,9 @@ class ASH_EXPORT WallpaperControllerImpl
 
   // Proxy to private ReloadWallpaper().
   void ReloadWallpaperForTesting(bool clear_cache);
+
+  // Needed when logoff is simulated in testing.
+  void ClearPrefChangeObserverForTesting();
 
   void set_bypass_decode_for_testing() { bypass_decode_for_testing_ = true; }
 
@@ -531,6 +543,17 @@ class ASH_EXPORT WallpaperControllerImpl
   // This is used when we want to change wallpaper dimming.
   void RepaintWallpaper();
 
+  bool SetLocalWallpaperInfo(const AccountId& account_id,
+                             const WallpaperInfo& info);
+  bool GetLocalWallpaperInfo(const AccountId& account_id,
+                             WallpaperInfo* info) const;
+  void OnPrefChanged();
+  void HandleWallpaperInfoSyncedIn(const AccountId& account_id,
+                                   WallpaperInfo info);
+  void OnAttemptSetOnlineWallpaper(WallpaperInfo info, bool success);
+
+  constexpr bool IsWallpaperTypeSyncable(WallpaperType type);
+
   bool locked_ = false;
 
   WallpaperMode wallpaper_mode_ = WALLPAPER_NONE;
@@ -601,6 +624,8 @@ class ASH_EXPORT WallpaperControllerImpl
       theme_observation_{this};
 
   std::unique_ptr<ui::CompositorLock> compositor_lock_;
+
+  std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
 
   // A non-empty value indicates the current wallpaper is in preview mode, which
   // expects either |ConfirmPreviewWallpaper| or |CancelPreviewWallpaper| to be
