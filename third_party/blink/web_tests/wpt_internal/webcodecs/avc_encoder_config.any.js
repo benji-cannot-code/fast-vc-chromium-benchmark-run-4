420b5742c0504506fc95e40b46cb2943cceb2d27
Eugene Zemtsov <eugene@chromium.org>
2021-04-05 06:51:10 +0000

    webcodecs: Stop closing frames in Audio|VideoEncoder.encode()
    Change-Id: I0281c1bdd836a94f968bf8fdf4fe8d09392845c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2803617
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869112}

--- third_party/blink/web_tests/wpt_internal/webcodecs/avc_encoder_config.any.js
+++ third_party/blink/web_tests/wpt_internal/webcodecs/avc_encoder_config.any.js
@@ -12,6 +12,7 @@ async function configureAndEncode(encoder, config) {
 
   let frame = await createFrame(defaultWidth, defaultHeight, ++frameNumber);
   encoder.encode(frame, { keyFrame : true });
+  frame.close();
   return encoder.flush()
 }
 
