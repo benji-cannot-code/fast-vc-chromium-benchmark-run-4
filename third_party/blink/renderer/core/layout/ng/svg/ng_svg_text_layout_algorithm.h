1a0aa65d0145150a341b31b3ce2bd0be4c9b2a5b
Kent Tamura <tkent@chromium.org>
2021-04-05 08:55:36 +0000

    SVG Text NG: Implement "3. Resolve character positioning"
    https://svgwg.org/svg2-draft/text.html#TextLayoutAlgorithm
    
    This CL has no behavior changes. New code is behind a runtime flag.
    
    Bug: 1179585
    Change-Id: I10bb6c8565705345e3d72d5aff33567829adc9f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2797679
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869126}

--- third_party/blink/renderer/core/layout/ng/svg/ng_svg_text_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/svg/ng_svg_text_layout_algorithm.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+struct SVGCharacterData;
+
 class NGSVGTextLayoutAlgorithm {
   STACK_ALLOCATED();
 
@@ -24,6 +26,12 @@ class NGSVGTextLayoutAlgorithm {
   // Returns false if we should skip the following steps.
   bool Setup(wtf_size_t approximate_count);
   void SetFlags(const NGFragmentItemsBuilder::ItemWithOffsetList& items);
+  void ResolveCharacterPositioning(
+      const LayoutObject& layout_object,
+      const NGFragmentItemsBuilder::ItemWithOffsetList& items,
+      bool in_text_path,
+      wtf_size_t& index,
+      Vector<SVGCharacterData>& resolve);
 
   NGInlineNode inline_node_;
 
