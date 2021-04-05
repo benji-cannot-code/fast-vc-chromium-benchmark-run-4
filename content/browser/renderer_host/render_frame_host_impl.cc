063c244e04af1159652c67b7f051a800f22abcb8
Rakina Zata Amni <rakina@chromium.org>
2021-04-05 22:19:29 +0000

    Add DCHECK_NE(net::OK, error_code) for failed navigation commits
    This ensures all error page commits should have error code != net::OK.
    
    Change-Id: I517111fe223fd72bf03e8ee30c75f34998fd07ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2804118
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869307}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -9426,6 +9426,7 @@ void RenderFrameHostImpl::SendCommitFailedNavigation(
     blink::mojom::PolicyContainerPtr policy_container) {
   DCHECK(navigation_client && navigation_request);
   DCHECK_NE(GURL(), common_params->url);
+  DCHECK_NE(net::OK, error_code);
   navigation_client->CommitFailedNavigation(
       std::move(common_params), std::move(commit_params),
       has_stale_copy_in_cache, error_code, extended_error_code,
