d486fae66052d2428ccaf46a6371dcc894489fb5
Lei Zhang <thestig@chromium.org>
2021-04-05 19:55:09 +0000

    Remove GCC/MSVC workarounds in gpu/.
    Bug: 1053958
    Change-Id: I21465bba12d8778e2b1d5279ef3fdf104bf62a1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2803040
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869248}

--- gpu/command_buffer/client/implementation_base.cc
+++ gpu/command_buffer/client/implementation_base.cc
@@ -21,10 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 
-#if !defined(_MSC_VER)
 const uint32_t ImplementationBase::kMaxSizeOfSimpleResult;
 const uint32_t ImplementationBase::kStartingOffset;
-#endif
 
 ImplementationBase::ImplementationBase(CommandBufferHelper* helper,
                                        TransferBufferInterface* transfer_buffer,
