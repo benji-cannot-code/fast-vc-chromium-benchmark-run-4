bacc32717b89834377cbadb58ea94d78e63e515d
Caroline Rising <corising@chromium.org>
2021-04-05 17:13:59 +0000

    Read later: only update reading list button visibility due to pref changes if the button exists.
    There is a potential crash here if a user updates the
    kShowReadingListInBookmarkBar pref on a device where the reading list is
    enabled then the pref is synced to a device where the reading list was
    not enabled but the pref syncing caused
    OnReadingListVisibilityPrefChanged to be called, which assumes the
    reading list button exists.
    
    Bug: 1195695, 1192117
    Change-Id: Ic004bd12df299c549d1cd75fc032e026e6224c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2803812
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869181}

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
@@ -1462,10 +1462,13 @@ void BookmarkBarView::Init() {
       base::BindRepeating(
           &BookmarkBarView::OnAppsPageShortcutVisibilityPrefChanged,
           base::Unretained(this)));
-  profile_pref_registrar_.Add(
-      bookmarks::prefs::kShowReadingListInBookmarkBar,
-      base::BindRepeating(&BookmarkBarView::OnReadingListVisibilityPrefChanged,
-                          base::Unretained(this)));
+  if (read_later_button_) {
+    profile_pref_registrar_.Add(
+        bookmarks::prefs::kShowReadingListInBookmarkBar,
+        base::BindRepeating(
+            &BookmarkBarView::OnReadingListVisibilityPrefChanged,
+            base::Unretained(this)));
+  }
   profile_pref_registrar_.Add(
       bookmarks::prefs::kShowManagedBookmarksInBookmarkBar,
       base::BindRepeating(&BookmarkBarView::OnShowManagedBookmarksPrefChanged,
@@ -2020,6 +2023,7 @@ void BookmarkBarView::OnAppsPageShortcutVisibilityPrefChanged() {
 }
 
 void BookmarkBarView::OnReadingListVisibilityPrefChanged() {
+  DCHECK(read_later_button_);
   bool visible =
       chrome::ShouldShowReadingListInBookmarkBar(browser_->profile());
   if (read_later_button_->GetVisible() == visible)
