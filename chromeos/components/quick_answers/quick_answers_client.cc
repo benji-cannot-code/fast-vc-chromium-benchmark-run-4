3b1b44d560dbf27c606110a9cb44c6c76ca18c34
Yue Li <updowndota@google.com>
2021-04-05 18:32:34 +0000

    Quick Answers: Add request text length histograms
    Bug: None
    Test: Manual Test
    Change-Id: Id355cf93d5c9cfdefeaadd8a4de8a0a47bee2249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798542
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869211}

--- chromeos/components/quick_answers/quick_answers_client.cc
+++ chromeos/components/quick_answers/quick_answers_client.cc
@@ -198,6 +198,9 @@ void QuickAnswersClient::IntentGeneratorCallback(
     }
   }
 
+  RecordRequestTextLength(intent_info.intent_type,
+                          quick_answers_request.selected_text.length());
+
   if (!skip_fetch)
     FetchQuickAnswers(processed_request);
 }
