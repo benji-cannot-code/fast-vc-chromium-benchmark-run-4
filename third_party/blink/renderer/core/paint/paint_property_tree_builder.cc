428ae7b7b21f60b484eb1d76daf6c8a2edf3f512
Xianzhu Wang <wangxianzhu@chromium.org>
2021-04-05 00:51:30 +0000

    Expand bounds of mask layer by 1 pixel
    This is similar to crrev.com/c/2786129 which is for synthesized clip
    mask layer, to ensure the mask fully covers the masked contents
    regardless of computation errors in later stages.
    
    To make that work, we also need to decomposite the MaskClip on the mask
    layer, otherwise the expanded pixels would be clipped off and would
    have no effect.
    
    Bug: 1171601
    Change-Id: I3198f228de1a0aa8baf2b8ed3c670ceabdeb57a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2787695
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869079}

--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -1245,10 +1245,6 @@ void FragmentPaintPropertyTreeBuilder::UpdateEffect() {
       if (has_mask_based_clip_path)
         clip_path_clip = fragment_data_.ClipPathBoundingBox();
 
-      const auto* output_clip = EffectCanUseCurrentClipAsOutputClip()
-                                    ? context_.current.clip
-                                    : nullptr;
-
       if (mask_clip || clip_path_clip) {
         IntRect combined_clip = mask_clip ? *mask_clip : *clip_path_clip;
         if (mask_clip && clip_path_clip)
@@ -1258,7 +1254,6 @@ void FragmentPaintPropertyTreeBuilder::UpdateEffect() {
             *context_.current.clip,
             ClipPaintPropertyNode::State(context_.current.transform,
                                          FloatRoundedRect(combined_clip))));
-        output_clip = properties_->MaskClip();
       } else {
         OnClearClip(properties_->ClearMaskClip());
       }
@@ -1268,6 +1263,10 @@ void FragmentPaintPropertyTreeBuilder::UpdateEffect() {
         mask_compositor_element_id =
             GetCompositorElementId(CompositorElementIdNamespace::kEffectMask);
       }
+
+      const auto* output_clip = EffectCanUseCurrentClipAsOutputClip()
+                                    ? context_.current.clip
+                                    : nullptr;
 
       EffectPaintPropertyNode::State state;
       state.local_transform_space = context_.current.transform;
