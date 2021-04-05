a480014ade3235a1d6fe8e089347f47d6811607e
Ghazale Hosseinabadi <ghazale@google.com>
2021-04-05 22:10:01 +0000

    Set top-level module script request for SWs to same-origin mode
    Top-level module script request for service workers should be set to
    same-origin mode. Link to the spec discussion issue can be found at
    https://github.com/whatwg/html/issues/6544.
    
    Bug: 1194571
    Change-Id: I8247669d23ef84fa879f743b699c6e11ac49bb50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2797663
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869300}

---
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/test-request-mode-worker.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Add a unique UUID per request to induce service worker script update.
+// Time stamp: %UUID%
+
+// The server injects the request headers here as a JSON string.
+const headersAsJson = `%HEADERS%`;
+const headers = JSON.parse(headersAsJson);
+
+self.addEventListener('message', async (e) => {
+  e.source.postMessage(headers);
+});
