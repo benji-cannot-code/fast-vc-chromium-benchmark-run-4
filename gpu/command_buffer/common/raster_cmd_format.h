412a5d7cffb65423df1267bf9a09f957e052b5ce
Ken Rockot <rockot@google.com>
2021-04-05 21:42:06 +0000

    Remove unused bitfield_helpers.h
    Just some dead code.
    
    Bug: None
    Change-Id: I3f77a031f6bcb8c9f8a35ef09ec1a3d7c2f946b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2806245
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#869283}

--- gpu/command_buffer/common/raster_cmd_format.h
+++ gpu/command_buffer/common/raster_cmd_format.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/atomicops.h"
 #include "base/macros.h"
 #include "components/viz/common/resources/resource_format.h"
-#include "gpu/command_buffer/common/bitfield_helpers.h"
 #include "gpu/command_buffer/common/cmd_buffer_common.h"
 #include "gpu/command_buffer/common/common_cmd_format.h"
 #include "gpu/command_buffer/common/constants.h"
