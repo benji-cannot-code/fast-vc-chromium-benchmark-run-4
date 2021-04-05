d479886d68a59ab28cbc48eb6fe691d8d668032b
Victor Vasiliev <vasilvv@chromium.org>
2021-04-05 22:25:29 +0000

    Abstract QuicTransportClient API into WebTransportClient
    Bug: 1193408, 1193409
    Change-Id: I6399de2a6fbf63baf0a917b7e1384907a8395d39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2804811
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869310}

--- net/quic/quic_transport_error.h
+++ net/quic/quic_transport_error.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
+// TODO(crbug.com/1193409): rename this class to WebTransportError.
 struct NET_EXPORT QuicTransportError {
   QuicTransportError() = default;
   QuicTransportError(int net_error,
