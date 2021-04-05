a1e24301cae1c9087538789e1a833da49c907037
Chris Blume <cblume@chromium.org>
2021-04-05 21:20:26 +0000

    [mac] Put hardware-protected video in a overlay
    We would like to prevent capture of protected content. For MacOS,
    we want to put protected content on its own overlay so we can
    prevent just the capture of that protected content.
    
    This CL puts hardware protected content in its own overlay.
    
    Bug: 1026703
    Change-Id: I2f1ea32e47cad7d57c794e4f4bb60b0348a0fe89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2792263
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869277}

--- components/viz/service/display/ca_layer_overlay.h
+++ components/viz/service/display/ca_layer_overlay.h
@@ -97,7 +97,7 @@ class VIZ_SERVICE_EXPORT CALayerOverlayProcessor {
 
   bool AreClipSettingsValid(const CALayerOverlay& ca_layer_overlay,
                             CALayerOverlayList* ca_layer_overlay_list) const;
-  void PutHDRContentInSeparateOverlay(
+  void PutForcedOverlayContentIntoOverlays(
       DisplayResourceProvider* resource_provider,
       AggregatedRenderPass* render_pass,
       const gfx::RectF& display_rect,
@@ -114,6 +114,20 @@ class VIZ_SERVICE_EXPORT CALayerOverlayProcessor {
       DisplayResourceProvider* resource_provider,
       const gfx::RectF& display_rect,
       const QuadList& quad_list,
+      const base::flat_map<AggregatedRenderPassId, cc::FilterOperations*>&
+          render_pass_filters,
+      const base::flat_map<AggregatedRenderPassId, cc::FilterOperations*>&
+          render_pass_backdrop_filters,
+      CALayerOverlayList* ca_layer_overlays) const;
+
+ private:
+  // Returns whether future candidate quads should be considered
+  bool PutQuadInSeparateOverlay(
+      QuadList::Iterator at,
+      DisplayResourceProvider* resource_provider,
+      AggregatedRenderPass* render_pass,
+      const gfx::RectF& display_rect,
+      const DrawQuad* quad,
       const base::flat_map<AggregatedRenderPassId, cc::FilterOperations*>&
           render_pass_filters,
       const base::flat_map<AggregatedRenderPassId, cc::FilterOperations*>&
