0240edc57dc81a593b05574ab0e6b14d3e34ee41
Alexandre Courbot <acourbot@chromium.org>
2021-04-05 00:41:36 +0000

    media/gpu/v4l2: add VP9 stateless accelerator for 5.4 kernel
    Add a VP9 accelerator based on the latest (but not final) upstream
    proposal that has been merged in the 5.4 tree.
    
    This accelerator is specific to Chromium-OS and should be replaced by
    the final upstream one when it lands.
    
    BUG=b:161415741
    BUG=b:167469080
    TEST=VDAtest passes with VP9 input on Asurada.
    
    Change-Id: I408534f53d1f23bf7f22b0ba4a3b7532d437d08f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2434185
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Auto-Submit: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869062}

--- media/gpu/v4l2/v4l2_slice_video_decode_accelerator.cc
+++ media/gpu/v4l2/v4l2_slice_video_decode_accelerator.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/v4l2/v4l2_vda_helpers.h"
 #include "media/gpu/v4l2/v4l2_vp8_accelerator.h"
 #include "media/gpu/v4l2/v4l2_vp8_accelerator_legacy.h"
+#include "media/gpu/v4l2/v4l2_vp9_accelerator_chromium.h"
 #include "media/gpu/v4l2/v4l2_vp9_accelerator_legacy.h"
 #include "ui/gfx/native_pixmap_handle.h"
 #include "ui/gl/gl_context.h"
@@ -321,9 +322,15 @@ bool V4L2SliceVideoDecodeAccelerator::Initialize(const Config& config,
     }
   } else if (video_profile_ >= VP9PROFILE_MIN &&
              video_profile_ <= VP9PROFILE_MAX) {
-    decoder_ = std::make_unique<VP9Decoder>(
-        std::make_unique<V4L2LegacyVP9Accelerator>(this, device_.get()),
-        video_profile_);
+    if (supports_requests_) {
+      decoder_ = std::make_unique<VP9Decoder>(
+          std::make_unique<V4L2ChromiumVP9Accelerator>(this, device_.get()),
+          video_profile_);
+    } else {
+      decoder_ = std::make_unique<VP9Decoder>(
+          std::make_unique<V4L2LegacyVP9Accelerator>(this, device_.get()),
+          video_profile_);
+    }
   } else {
     NOTREACHED() << "Unsupported profile " << GetProfileName(video_profile_);
     return false;
