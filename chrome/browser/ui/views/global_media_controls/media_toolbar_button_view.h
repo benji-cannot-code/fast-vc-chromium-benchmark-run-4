60b4af727980e85c50a73d417972b19047477f0c
Muyao Xu <muyaoxu@google.com>
2021-04-05 16:29:25 +0000

    [Zenith] Show the Zenith toolbar button before the dialog opens
    The Zenith dialog may be positioned before the Zenith toolbar button
    when the dialog is first opened from an in-page cast button and the
    toolbar button has never been visible.
    
    This CL always shows the Zenith toolbar button before it opens the
    Zenith dialog for presentation request.
    
    Bug: b/182935298, 1194222
    Change-Id: Idf770f0cd1938d0ab3d8a4fa37c30e547b5f8b9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2800748
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869172}

--- chrome/browser/ui/views/global_media_controls/media_toolbar_button_view.h
+++ chrome/browser/ui/views/global_media_controls/media_toolbar_button_view.h
@@ -38,6 +38,10 @@ class MediaToolbarButtonView : public ToolbarButton,
   void Enable() override;
   void Disable() override;
 
+  MediaToolbarButtonController* media_toolbar_button_controller() {
+    return controller_.get();
+  }
+
  private:
   void ButtonPressed();
 
