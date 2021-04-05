88bc0bfd7dd87832754c7a07fee87ee70ea43170
Yeunjoo Choi <ychoi@igalia.com>
2021-04-05 03:22:43 +0000

    Move //chrome/browser/{chromeos => ash}/camera_mic
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Change-Id: Ibc6f63e7f181a55a9b8ca7874ca7cd9682e8d805
    Bug: 1164001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2801611
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#869098}

--- chrome/browser/ui/ash/media_client_impl.h
+++ chrome/browser/ui/ash/media_client_impl.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
-#include "chrome/browser/chromeos/camera_mic/vm_camera_mic_manager.h"
+#include "chrome/browser/ash/camera_mic/vm_camera_mic_manager.h"
 #include "chrome/browser/media/webrtc/media_capture_devices_dispatcher.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "media/capture/video/chromeos/camera_hal_dispatcher_impl.h"
