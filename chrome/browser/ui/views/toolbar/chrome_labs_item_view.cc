6f33948dde8639116049367aa27b3d3d114d7cdd
Elaine Chien <elainechien@chromium.org>
2021-04-05 19:33:08 +0000

    ChromeLabs: More accessibility
    This CL Enable navigation with arrow keys, changes text style and
    overrides the accessible role for the Chrome Labs button so that what
    the JAWS screen reader announces is consistent with other Toolbar buttons.
    (See Accessibility review in bug for more context)
    
    Bug: 1145666
    Change-Id: Ief2921a89a9253ee0da8a13fee70f5d7814a1c45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2803681
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869237}

--- chrome/browser/ui/views/toolbar/chrome_labs_item_view.cc
+++ chrome/browser/ui/views/toolbar/chrome_labs_item_view.cc
@@ -114,6 +114,7 @@ ChromeLabsItemView::ChromeLabsItemView(
                    .CopyAddressTo(&experiment_name)
                    .SetTextContext(views::style::CONTEXT_DIALOG_BODY_TEXT)
                    .SetText(lab.visible_name)
+                   .SetTextStyle(ChromeTextStyle::STYLE_EMPHASIZED)
                    .SetHorizontalAlignment(gfx::ALIGN_LEFT)
                    .Build());
   AddChildView(
@@ -121,7 +122,7 @@ ChromeLabsItemView::ChromeLabsItemView(
           .CopyAddressTo(&experiment_description)
           .SetText(lab.visible_description)
           .SetTextContext(ChromeTextContext::CONTEXT_DIALOG_BODY_TEXT_SMALL)
-          .SetTextStyle(views::style::STYLE_SECONDARY)
+          .SetTextStyle(views::style::STYLE_PRIMARY)
           .SetMultiLine(true)
           .SetHorizontalAlignment(gfx::ALIGN_LEFT)
           .SetProperty(views::kFlexBehaviorKey,
