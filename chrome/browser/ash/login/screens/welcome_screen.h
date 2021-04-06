0a8f9bafab6d85fca0262285078f9dc89315a3a4
Akihiro Ota <akihiroota@chromium.org>
2021-04-06 20:57:36 +0000

    Reland "ChromeVox hint: Give hint after 20 seconds of idling"
    This is a reland of https://www.crrev.com/c/2792064
    
    Original change's description:
    > ChromeVox hint: Give hint after 20 seconds of idling
    >
    > Currently, the ChromeVox hint is given 20 seconds after the OOBE
    > welcome screen is shown. The only way to cancel the timer and prevent
    > the hint is to click one of the three on-screen buttons. This leads to
    > many people receiving the hint when they don't need it.
    >
    > After discussion with the UI/UX team about the timing of the
    > announcement, we have decided to give the ChromeVox hint after 20
    > seconds of idling. This will help us make a more informed decision
    > about giving the ChromeVox hint, as opposed to the naive approach of
    > giving it after 20 seconds.
    >
    > --gtest_filter="WelcomeScreenChromeVoxHint*.*"
    > Also ran manual tests to ensure the hint is given 20s after idle.
    > seconds of idling, as opposed to 20 seconds after device boot up.
    >
    > Fixed: 1186502
    > Test: out/cros/browser_tests
    > Change-Id: I38eca6b0f7d6c66776bcd2e8e62730250efa0a43
    > AX-Relnotes: The ChromeVox announcement in OOBE now fires after 20
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2752922
    > Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    > Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#866300}
    
    Change-Id: I140256a003f61e6fe821110accfeef3c2db9af91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798079
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869690}

--- chrome/browser/ash/login/screens/welcome_screen.h
+++ chrome/browser/ash/login/screens/welcome_screen.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/base/locale_util.h"
 #include "chrome/browser/ash/login/demo_mode/demo_mode_detector.h"
 #include "chrome/browser/ash/login/screens/base_screen.h"
+#include "chrome/browser/ash/login/screens/chromevox_hint/chromevox_hint_detector.h"
 #include "chrome/browser/ash/login/wizard_context.h"
 #include "ui/base/ime/chromeos/input_method_manager.h"
 
@@ -31,7 +32,8 @@ class WelcomeView;
 
 class WelcomeScreen : public BaseScreen,
                       public input_method::InputMethodManager::Observer,
-                      public DemoModeDetector::Observer {
+                      public DemoModeDetector::Observer,
+                      public ChromeVoxHintDetector::Observer {
  public:
   using TView = WelcomeView;
 
@@ -107,14 +109,9 @@ class WelcomeScreen : public BaseScreen,
     return &(context()->configuration);
   }
 
-  void CancelChromeVoxHintTimer();
-  void GiveChromeVoxHintForTesting();
-  bool GetChromeVoxHintTimerCancelledForTesting() {
-    return chromevox_hint_timer_cancelled_for_testing_;
-  }
-  bool GetChromeVoxHintTimerActivatedForTesting() {
-    return chromevox_hint_timer_activated_;
-  }
+  // ChromeVox hint.
+  void CancelChromeVoxHintIdleDetection();
+  ChromeVoxHintDetector* GetChromeVoxHintDetectorForTesting();
 
  protected:
   // Exposes exit callback to test overrides.
@@ -126,6 +123,9 @@ class WelcomeScreen : public BaseScreen,
   void HideImpl() override;
   void OnUserAction(const std::string& action_id) override;
   bool HandleAccelerator(ash::LoginAcceleratorAction action) override;
+
+  // ChromeVoxHintDetector::Observer:
+  void OnShouldGiveChromeVoxHint() override;
 
   // InputMethodManager::Observer:
   void InputMethodChanged(input_method::InputMethodManager* manager,
@@ -164,14 +164,12 @@ class WelcomeScreen : public BaseScreen,
   void NotifyLocaleChange();
   void OnLocaleChangeResult(ash::LocaleNotificationResult result);
 
-  // ChromeVox hint.
-  void StartChromeVoxHintTimer();
-  void GiveChromeVoxHint();
-
   WelcomeView* view_ = nullptr;
   ScreenExitCallback exit_callback_;
 
   std::unique_ptr<DemoModeDetector> demo_mode_detector_;
+
+  std::unique_ptr<ChromeVoxHintDetector> chromevox_hint_detector_;
 
   std::string input_method_;
   std::string timezone_;
@@ -185,12 +183,6 @@ class WelcomeScreen : public BaseScreen,
   std::string selected_language_code_;
 
   base::ObserverList<Observer>::Unchecked observers_;
-
-  base::OneShotTimer chromevox_hint_timer_;
-
-  bool chromevox_hint_timer_activated_ = false;
-
-  bool chromevox_hint_timer_cancelled_for_testing_ = false;
 
   base::WeakPtrFactory<WelcomeScreen> weak_factory_{this};
 
