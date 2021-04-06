14fde2d29752b0f4963bc9a90a67fd623d66fc58
Alice Wang <aliceywang@chromium.org>
2021-04-06 09:26:10 +0000

    [Signin][Android] Migrate the deprecate seeding method to the safer version
    This CL migrates the deprecated seeding method to the safer version
    onAccountsChanged(). The latter posts the seeding task to the end of
    the pending task queue when there is already a seeding in progress.
    
    Bug: 1185712
    Change-Id: I73ac3d96d920bf8afac25eb7aad7c24498585d67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2802332
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869445}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManagerImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManagerImpl.java
@@ -678,7 +678,7 @@ class SigninManagerImpl implements IdentityManager.Observer, SigninManager {
             SigninManagerImplJni.get().wipeGoogleServiceWorkerCaches(
                     mNativeSigninManagerAndroid, wipeDataCallback);
         }
-        mAccountTrackerService.invalidateAccountSeedStatus(true);
+        mAccountTrackerService.onAccountsChanged();
     }
 
     @VisibleForTesting
