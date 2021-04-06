10d4352321090a1b198d110b8637d073a37fa87c
Orin Jaworski <orinj@google.com>
2021-04-06 22:57:08 +0000

    Add activation by URL query parameter to settings safety check
    This CL adds an automatic activation feature to
    chrome://settings/safetyCheck so that a "Run Safety Check" omnibox
    Pedal can save users from having to press the check button manually by
    referencing `chrome://settings/safetyCheck?activateSafetyCheck`.
    
    Bug: 893183
    Change-Id: I34b17b97db8746d4dac8c23d04e62407c67f7c0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2795502
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#869731}

--- chrome/browser/resources/settings/settings_routes.js
+++ chrome/browser/resources/settings/settings_routes.js
@@ -39,6 +39,7 @@ import {Route} from './router.js';
  *   PRIVACY: !Route,
  *   RESET: !Route,
  *   RESET_DIALOG: !Route,
+ *   SAFETY_CHECK: !Route,
  *   SEARCH: !Route,
  *   SEARCH_ENGINES: !Route,
  *   SECURITY: !Route,
