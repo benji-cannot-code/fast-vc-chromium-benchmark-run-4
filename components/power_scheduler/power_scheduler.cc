b867dd099f05610c0f4642f0f54faa85ee2e1ec0
Sami Kyostila <skyostil@chromium.org>
2021-04-06 12:17:19 +0000

    power_scheduler: Also throttle in background mode
    Also enable the idle throttling policy in the background power mode (in
    addition to the idle power mode). While Android automatically limits the
    CPU impact of activities in the background, there are other cases where
    the power scheduler determines the process to be in the background power
    mode, for example, when an iframe has been scrolled offscreen. Because
    of this we can potentially save more power by extending idle throttling
    to cover these scenarios.
    
    Bug: 1166695
    Change-Id: I8b840254ecde6b43be1b4f2da91152d8775eb9eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2797420
    Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869482}

--- components/power_scheduler/power_scheduler.cc
+++ components/power_scheduler/power_scheduler.cc
@@ -19,10 +19,11 @@ namespace {
 
 perfetto::StaticString TraceEventNameForAffinityMode(
     base::CpuAffinityMode affinity) {
-  if (affinity == base::CpuAffinityMode::kDefault)
+  if (affinity == base::CpuAffinityMode::kDefault) {
     return "ApplyCpuAffinityModeDefault";
-  else if (affinity == base::CpuAffinityMode::kLittleCoresOnly)
+  } else if (affinity == base::CpuAffinityMode::kLittleCoresOnly) {
     return "ApplyCpuAffinityModeLittleCoresOnly";
+  }
   return "ApplyCpuAffinityModeUnknown";
 }
 
@@ -133,7 +134,8 @@ void PowerScheduler::ApplyPolicy() {
   auto new_affinity = base::CpuAffinityMode::kDefault;
   if (current_policy_ == SchedulingPolicy::kLittleCoresOnly ||
       (current_policy_ == SchedulingPolicy::kThrottleIdle &&
-       current_power_mode_ == power_scheduler::PowerMode::kIdle)) {
+       (current_power_mode_ == power_scheduler::PowerMode::kIdle ||
+        current_power_mode_ == power_scheduler::PowerMode::kBackground))) {
     new_affinity = base::CpuAffinityMode::kLittleCoresOnly;
   }
 
