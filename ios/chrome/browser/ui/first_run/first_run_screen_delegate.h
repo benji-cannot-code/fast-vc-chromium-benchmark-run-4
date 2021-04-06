fc2a5899d73bcd35b2a9d9cdfe999c62946b7634
Tina Wang <tinazwang@google.com>
2021-04-06 19:11:40 +0000

    🐻 [Pandora] Create basic FRECoordinator
    Add new classes for FRE module interface.
    
    Bug: 1186760,1189798
    Change-Id: I5c7e33380986ff005d5ebd5691bfd1bd567ec531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2795983
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869652}

---
+++ ios/chrome/browser/ui/first_run/first_run_screen_delegate.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_FIRST_RUN_FIRST_RUN_SCREEN_DELEGATE_H_
+#define IOS_CHROME_BROWSER_UI_FIRST_RUN_FIRST_RUN_SCREEN_DELEGATE_H_
+
+// The delegate for transferring between screens.
+@protocol FirstRunScreenDelegate <NSObject>
+
+// Called when one screen finished presenting.
+- (void)willFinishPresenting;
+
+// Called when user want to skip all screens after.
+- (void)skipAll;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_FIRST_RUN_FIRST_RUN_SCREEN_DELEGATE_H_
