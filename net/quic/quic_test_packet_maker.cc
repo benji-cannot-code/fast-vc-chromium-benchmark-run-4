44a0c6d9256173d4d53a641c9572f54e582fdf3f
Victor Vasiliev <vasilvv@chromium.org>
2021-04-06 06:58:52 +0000

    Roll src/net/third_party/quiche/src/ 91b48f5f3..c2638cc1d (8 commits)
    https://quiche.googlesource.com/quiche.git/+log/91b48f5f3c28..c2638cc1d6a6
    
    $ git log 91b48f5f3..c2638cc1d --date=short --no-merges --format='%ad %ae %s'
    2021-04-05 fayang In QUIC, do not try to write in the mid of packet processing.
    2021-04-05 ianswett Fix a bug in BBRv2 bw_lo experimental modes where the second ACK would cause the pacing rate to suddenly drop.  Also limits reduction of bandwidth_lo to 30% in a single round at the end of a round.
    2021-04-05 fayang Internal QUICHE change
    2021-04-05 dschinazi Deprecate quic_stateless_reset_faster_random
    2021-04-05 bnc Improve HpackEntry class constructors.
    2021-04-05 vasilvv In WebTransport, make FIN be a return of Read() method rather than a visitor call.
    2021-04-05 dschinazi Allow selecting QUIC version from command line in quic_client_interop_test
    2021-04-05 wub Move B2LO to Bbr2Sender::ApplyConnectionOptions, this allows it to be set in uStreamer experiments.
    
    Created with:
      roll-dep src/net/third_party/quiche/src
    
    Change-Id: I1c601db75f37c29b62057714a22efc03043f0650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2805309
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869413}

--- net/quic/quic_test_packet_maker.cc
+++ net/quic/quic_test_packet_maker.cc
@@ -1113,7 +1113,7 @@ std::unique_ptr<quic::QuicEncryptedPacket>
 QuicTestPacketMaker::MakeStatelessResetPacket() {
   auto connection_id = quic::test::TestConnectionId();
   return quic::QuicFramer::BuildIetfStatelessResetPacket(
-      connection_id, quic::QuicFramer::GetMinStatelessResetPacketLength(),
+      connection_id, quic::QuicFramer::GetMinStatelessResetPacketLength() + 1,
       quic::QuicUtils::GenerateStatelessResetToken(connection_id));
 }
 
