6e6f587f1aeae2bda1ab66be8d160e3474237c92
Kuo Jen Wei <inker@google.com>
2021-04-06 09:11:01 +0000

    media/gpu/video_encode_accelerator_perf_tests: Add bitrate options
    Add |--bitrate| video_encode_accelerator_perf_tests for measuring SSIM,
    PSNR changes with respect to bitrate.
    
    Bug: b:182713338
    Test: tast -verbose run <DUT> "video.EncodeAccelPerf*
    
    Change-Id: Ice5ec87e7c259a6ea224db75b3903d891a865d94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2770943
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869440}

--- media/gpu/test/video_encoder/video_encoder_test_environment.h
+++ media/gpu/test/video_encoder/video_encoder_test_environment.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/files/file_path.h"
+#include "base/optional.h"
 #include "media/base/video_codecs.h"
 #include "media/gpu/test/video_test_environment.h"
 
@@ -42,6 +43,7 @@ class VideoEncoderTestEnvironment : public VideoTestEnvironment {
       const std::string& codec,
       size_t num_temporal_layers,
       bool output_bitstream,
+      base::Optional<uint32_t> output_bitrate,
       const FrameOutputConfig& frame_output_config = FrameOutputConfig());
 
   ~VideoEncoderTestEnvironment() override;
--- media/gpu/video_encode_accelerator_tests.cc
+++ media/gpu/video_encode_accelerator_tests.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/optional.h"
 #include "base/strings/string_number_conversions.h"
 #include "media/base/media_util.h"
 #include "media/base/test_data_util.h"
@@ -722,7 +723,7 @@ int main(int argc, char** argv) {
       media::test::VideoEncoderTestEnvironment::Create(
           video_path, video_metadata_path, enable_bitstream_validator,
           output_folder, codec, num_temporal_layers, output_bitstream,
-          frame_output_config);
+          /*output_bitrate=*/base::nullopt, frame_output_config);
 
   if (!test_environment)
     return EXIT_FAILURE;
