dc266bf1209ad89a4dc7b3c797c25e903875ad93
Jon Mann <jonmann@chromium.org>
2021-04-06 21:00:56 +0000

    Log a metric to track the last connection for hidden networks.
    Bug: 1175894
    Change-Id: I771cee4656de342542d4be287b1e0891ec183867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2800047
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869695}

--- chromeos/network/network_metadata_store.cc
+++ chromeos/network/network_metadata_store.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/location.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "chromeos/network/network_configuration_handler.h"
@@ -84,7 +85,7 @@ NetworkMetadataStore::NetworkMetadataStore(
   if (network_configuration_handler_) {
     network_configuration_handler_->AddObserver(this);
   }
-  if (network_state_handler_ && !HasFixedHiddenNetworks()) {
+  if (network_state_handler_) {
     network_state_handler_->AddObserver(this, FROM_HERE);
   }
   if (LoginState::IsInitialized()) {
@@ -112,17 +113,19 @@ void NetworkMetadataStore::LoggedInStateChanged() {
 }
 
 void NetworkMetadataStore::NetworkListChanged() {
-  if (HasFixedHiddenNetworks()) {
-    return;
-  }
-
   // Ensure that user networks have been loaded from Shill before querying.
   if (!network_state_handler_->IsProfileNetworksLoaded()) {
+    has_profile_loaded_ = false;
     return;
   }
 
-  profile_pref_service_->SetBoolean(kHasFixedHiddenNetworks, true);
+  if (has_profile_loaded_) {
+    return;
+  }
+
+  has_profile_loaded_ = true;
   FixSyncedHiddenNetworks();
+  LogHiddenNetworkAge();
 }
 
 void NetworkMetadataStore::OwnSharedNetworksOnFirstUserLogin() {
@@ -154,6 +157,10 @@ void NetworkMetadataStore::OwnSharedNetworksOnFirstUserLogin() {
 }
 
 void NetworkMetadataStore::FixSyncedHiddenNetworks() {
+  if (HasFixedHiddenNetworks()) {
+    return;
+  }
+
   NetworkStateHandler::NetworkStateList networks;
   network_state_handler_->GetNetworkListByType(
       NetworkTypePattern::WiFi(), /*configured_only=*/true,
@@ -175,6 +182,29 @@ void NetworkMetadataStore::FixSyncedHiddenNetworks() {
         base::DoNothing::Once(),
         base::BindOnce(&NetworkMetadataStore::OnDisableHiddenError,
                        weak_ptr_factory_.GetWeakPtr()));
+  }
+  profile_pref_service_->SetBoolean(kHasFixedHiddenNetworks, true);
+}
+
+void NetworkMetadataStore::LogHiddenNetworkAge() {
+  NetworkStateHandler::NetworkStateList networks;
+  network_state_handler_->GetNetworkListByType(
+      NetworkTypePattern::WiFi(), /*configured_only=*/true,
+      /*visible_only=*/false, /*limit=*/0, &networks);
+
+  for (const chromeos::NetworkState* network : networks) {
+    if (!network->hidden_ssid()) {
+      continue;
+    }
+    base::TimeDelta timestamp = GetLastConnectedTimestamp(network->guid());
+    if (!timestamp.is_zero()) {
+      int days = base::Time::Now().ToDeltaSinceWindowsEpoch().InDays() -
+                 timestamp.InDays();
+      base::UmaHistogramCounts10000("Network.Shill.WiFi.Hidden.LastConnected",
+                                    days);
+    }
+    base::UmaHistogramBoolean("Network.Shill.WiFi.Hidden.EverConnected",
+                              !timestamp.is_zero());
   }
 }
 
