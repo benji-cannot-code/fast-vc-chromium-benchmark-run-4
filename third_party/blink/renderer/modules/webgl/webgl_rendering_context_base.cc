3022c2c5b684de990efe3be4864a86e3143119f3
Kenneth Russell <kbr@chromium.org>
2021-04-06 21:23:00 +0000

    Guard against context loss during multisample resolve.
    In rare situations on macOS, due to GPU switching, the context may be
    lost while resolving the default multisampled back buffer. Guard
    against this and add a test for it. The test unfortunately does not
    reliably catch the failure, but always passes with the fix.
    
    Bug: 1186582
    Change-Id: Ic97a2bfbe7546fa194c48678b26dcfe22432487e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2807179
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: James Darpinian <jdarpinian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869712}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -622,7 +622,11 @@ scoped_refptr<StaticBitmapImage> WebGLRenderingContextBase::GetImage() {
     return nullptr;
 
   ScopedFramebufferRestorer fbo_restorer(this);
-  GetDrawingBuffer()->ResolveAndBindForReadAndDraw();
+  // In rare situations on macOS the drawing buffer can be destroyed
+  // during the resolve process, specifically during automatic
+  // graphics switching. Guard against this.
+  if (!GetDrawingBuffer()->ResolveAndBindForReadAndDraw())
+    return nullptr;
   // Use the drawing buffer size here instead of the canvas size to ensure that
   // sizing is consistent. The forced downsizing logic in Reshape() can lead to
   // the drawing buffer being smaller than the canvas size.
@@ -1657,7 +1661,11 @@ bool WebGLRenderingContextBase::PaintRenderingResultsToCanvas(
   ScopedTexture2DRestorer restorer(this);
   ScopedFramebufferRestorer fbo_restorer(this);
 
-  GetDrawingBuffer()->ResolveAndBindForReadAndDraw();
+  // In rare situations on macOS the drawing buffer can be destroyed
+  // during the resolve process, specifically during automatic
+  // graphics switching. Guard against this.
+  if (!GetDrawingBuffer()->ResolveAndBindForReadAndDraw())
+    return false;
   if (!CopyRenderingResultsFromDrawingBuffer(Host()->ResourceProvider(),
                                              source_buffer)) {
     // Currently, CopyRenderingResultsFromDrawingBuffer is expected to always
@@ -1728,7 +1736,11 @@ sk_sp<SkData> WebGLRenderingContextBase::PaintRenderingResultsToDataArray(
   if (isContextLost())
     return nullptr;
   ClearIfComposited(kClearCallerOther);
-  GetDrawingBuffer()->ResolveAndBindForReadAndDraw();
+  // In rare situations on macOS the drawing buffer can be destroyed
+  // during the resolve process, specifically during automatic
+  // graphics switching. Guard against this.
+  if (!GetDrawingBuffer()->ResolveAndBindForReadAndDraw())
+    return nullptr;
   ScopedFramebufferRestorer restorer(this);
   return GetDrawingBuffer()->PaintRenderingResultsToDataArray(source_buffer);
 }
