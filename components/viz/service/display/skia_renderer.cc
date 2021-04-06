89e77d7ddd66c95dabb23156a81777d5005c2c84
Brian Osman <brianosman@google.com>
2021-04-06 17:54:46 +0000

    Update deprecated SkSL syntax in SkiaRenderer
    'uniform shader' has been the preferred syntax for some time.
    Support for 'in shader' is going away, so migrate this file.
    
    Bug: skia:11374
    Change-Id: I5b2bfbadcfc793f4419fdb37e8fa778d582ab3f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2803332
    Auto-Submit: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Cr-Commit-Position: refs/heads/master@{#869612}

--- components/viz/service/display/skia_renderer.cc
+++ components/viz/service/display/skia_renderer.cc
@@ -2297,7 +2297,7 @@ sk_sp<SkColorFilter> SkiaRenderer::GetColorSpaceConversionFilter(
     const char* hdr = R"(
 uniform half offset;
 uniform half multiplier;
-in shader child;
+uniform shader child;
 
 half4 main() {
   half4 color = sample(child);
