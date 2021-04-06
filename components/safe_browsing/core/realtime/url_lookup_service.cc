cf7f14e2452ad6c14c52801cc41cfebbc5c1ea8b
Xinghui Lu <xinghuilu@chromium.org>
2021-04-06 07:45:48 +0000

    Make user gesture limit configurable for real time URL check referrers.
    Make the length of the referrer chain configurable by a field trial
    param SafeBrowsingRealTimeUrlLookupReferrerLengthParam. The default
    value is set to 2.
    
    Bug: 1161342
    Change-Id: Ieb60813e624176adb49139e9234074efd09040f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798555
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869421}

--- components/safe_browsing/core/realtime/url_lookup_service.cc
+++ components/safe_browsing/core/realtime/url_lookup_service.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/realtime/url_lookup_service.h"
 
 #include "base/base64url.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/strcat.h"
@@ -28,6 +29,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+
+namespace {
+
+constexpr char kRealTimeUrlLookupReferrerLengthParam[] =
+    "SafeBrowsingRealTimeUrlLookupReferrerLengthParam";
+constexpr int kDefaultRealTimeUrlLookupReferrerLength = 2;
+
+}  // namespace
 
 namespace safe_browsing {
 
@@ -92,6 +101,12 @@ bool RealTimeUrlLookupService::CanPerformFullURLLookupWithToken() const {
 
 bool RealTimeUrlLookupService::CanAttachReferrerChain() const {
   return base::FeatureList::IsEnabled(kRealTimeUrlLookupReferrerChain);
+}
+
+int RealTimeUrlLookupService::GetReferrerUserGestureLimit() const {
+  return base::GetFieldTrialParamByFeatureAsInt(
+      kRealTimeUrlLookupReferrerChain, kRealTimeUrlLookupReferrerLengthParam,
+      kDefaultRealTimeUrlLookupReferrerLength);
 }
 
 bool RealTimeUrlLookupService::CanCheckSubresourceURL() const {
