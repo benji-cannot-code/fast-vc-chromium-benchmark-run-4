9fd91b5ba4d1a158f09a77a4ce7d29d3bff7c7b7
Takashi Toyoshima <toyoshim@chromium.org>
2021-04-06 10:06:02 +0000

    Prerender: Cleanup on PrerenderHost's initiator_origin
    This patch stop passing `initiator_origin` as a constructor argument.
    Originally we assumed that the `initiator_origin` may be changed on
    StartPrerendering(). But with this patch, we don't call
    CreateAndStartHost() in PrerenderProcessor if the `initiator_origin`
    is changed.
    `initiator_origin` can be obtained via RenderFrameHost instead.
    
    Bug: 1187364
    Change-Id: I50514cfae28756209245f82b53ef3604aef4759a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2789831
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869455}

--- content/browser/prerender/prerender_host_registry.cc
+++ content/browser/prerender/prerender_host_registry.cc
@@ -38,7 +38,6 @@ void PrerenderHostRegistry::RemoveObserver(Observer* observer) {
 
 int PrerenderHostRegistry::CreateAndStartHost(
     blink::mojom::PrerenderAttributesPtr attributes,
-    const url::Origin& initiator_origin,
     RenderFrameHostImpl& initiator_render_frame_host) {
   DCHECK(attributes);
 
@@ -59,16 +58,17 @@ int PrerenderHostRegistry::CreateAndStartHost(
 
   // Ignore prerendering requests for the same URL.
   const GURL prerendering_url = attributes->url;
-  TRACE_EVENT2("navigation", "PrerenderHostRegistry::CreateAndStartHost",
-               "attributes", attributes, "initiator_origin",
-               initiator_origin.GetURL().spec());
+  TRACE_EVENT2(
+      "navigation", "PrerenderHostRegistry::CreateAndStartHost", "attributes",
+      attributes, "initiator_origin",
+      initiator_render_frame_host.GetLastCommittedOrigin().GetURL().spec());
 
   auto found = frame_tree_node_id_by_url_.find(prerendering_url);
   if (found != frame_tree_node_id_by_url_.end())
     return found->second;
 
   auto prerender_host = std::make_unique<PrerenderHost>(
-      std::move(attributes), initiator_origin, initiator_render_frame_host);
+      std::move(attributes), initiator_render_frame_host);
   const int frame_tree_node_id = prerender_host->frame_tree_node_id();
 
   CHECK(!base::Contains(prerender_host_by_frame_tree_node_id_,
