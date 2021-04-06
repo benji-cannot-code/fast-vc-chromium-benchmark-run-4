2b673ef00f7764e59ff67939f19e0a7364f48fc2
Hidehiko Abe <hidehiko@chromium.org>
2021-04-06 07:27:40 +0000

    Consolidate Sync and Async events from IME.
    With this CL, sync Commit event and async Commit event
    generates same key events to the client.
    
    Specifically, if NeedInsertChar() becomes true,
    Actual key event's key code, rather than 229 (VKEY_PROCESSKEY) is
    used.
    
    Also, along with OnCommit() method update, OnPreeditChanged/End
    is also updated to be consistent with sync cases.
    However, practically it should be no visible changes,
    because NeedInsertChar() will return false for those cases
    always.
    
    BUG=1180433
    TEST=Ran linux chrome locally. ui_base_unittests.
    
    Change-Id: I3327500cff51d1eb7250e367e7e527094cc1e682
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2801575
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869417}

--- ui/base/ime/linux/input_method_auralinux.h
+++ ui/base/ime/linux/input_method_auralinux.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/component_export.h"
-#include "base/macros.h"
+#include "base/optional.h"
 #include "ui/base/ime/composition_text.h"
 #include "ui/base/ime/input_method_base.h"
 #include "ui/base/ime/linux/linux_input_method_context.h"
@@ -24,6 +24,8 @@ class COMPONENT_EXPORT(UI_BASE_IME_LINUX) InputMethodAuraLinux
       public LinuxInputMethodContextDelegate {
  public:
   explicit InputMethodAuraLinux(internal::InputMethodDelegate* delegate);
+  InputMethodAuraLinux(const InputMethodAuraLinux&) = delete;
+  InputMethodAuraLinux& operator=(const InputMethodAuraLinux&) = delete;
   ~InputMethodAuraLinux() override;
 
   LinuxInputMethodContext* GetContextForTesting(bool is_simple);
@@ -50,17 +52,27 @@ class COMPONENT_EXPORT(UI_BASE_IME_LINUX) InputMethodAuraLinux
                                 TextInputClient* focused) override;
 
  private:
+  // Continues to dispatch the ET_KEY_PRESSED event to the client.
+  // This needs to be called "before" committing the result string or
+  // the composition string.
+  ui::EventDispatchDetails DispatchImeFilteredKeyPressEvent(
+      ui::KeyEvent* event);
   enum class CommitResult {
     kSuccess,          // Successfully committed at least one character.
     kNoCommitString,   // No available string to commit.
     kTargetDestroyed,  // Target was destroyed during the commit.
   };
   CommitResult MaybeCommitResult(bool filtered, const KeyEvent& event);
-  bool MaybeUpdateComposition();
+  bool MaybeUpdateComposition(bool text_committed);
 
+  // Shared implementation of OnPreeditChanged and OnPreeditEnd.
+  // |force_update_client| is designed to dispatch key event/update
+  // the client's composition string, specifically for async-mode case.
+  void OnPreeditUpdate(const ui::CompositionText& composition_text,
+                       bool force_update_client);
   void ConfirmCompositionText();
   bool HasInputMethodResult();
-  bool NeedInsertChar() const;
+  bool NeedInsertChar(const std::u16string& result_text) const;
   ui::EventDispatchDetails SendFakeProcessKeyEvent(ui::KeyEvent* event) const
       WARN_UNUSED_RESULT;
   void UpdateContextFocusState();
@@ -69,6 +81,10 @@ class COMPONENT_EXPORT(UI_BASE_IME_LINUX) InputMethodAuraLinux
 
   std::unique_ptr<LinuxInputMethodContext> context_;
   std::unique_ptr<LinuxInputMethodContext> context_simple_;
+
+  // The last key event that IME is probably in process in
+  // async-mode.
+  base::Optional<ui::KeyEvent> ime_filtered_key_event_;
 
   std::u16string result_text_;
 
@@ -91,8 +107,6 @@ class COMPONENT_EXPORT(UI_BASE_IME_LINUX) InputMethodAuraLinux
 
   // Used for making callbacks.
   base::WeakPtrFactory<InputMethodAuraLinux> weak_ptr_factory_{this};
-
-  DISALLOW_COPY_AND_ASSIGN(InputMethodAuraLinux);
 };
 
 }  // namespace ui
