d625fbb5eb6a77fa435bb56d3e8cd3107668e6fd
Ryan Hansberry <hansberry@chromium.org>
2021-04-06 23:04:37 +0000

    Revert "[SmartLock] Add temporary CHECKs and logs for crash crbug.com/974410."
    This reverts commit 6417df157e35147aacc1cdc22dc5423a3ac22e03.
    
    Reason for revert: <No longer needed>
    
    Original change's description:
    > [SmartLock] Add temporary CHECKs and logs for crash crbug.com/974410.
    >
    > Bug: 974410
    >
    > Change-Id: Id477df66b6b17f1612eaf0aaca19ce7ab1776cc3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2707488
    > Auto-Submit: Ryan Hansberry <hansberry@chromium.org>
    > Commit-Queue: James Vecore <vecore@google.com>
    > Reviewed-by: James Vecore <vecore@google.com>
    > Cr-Commit-Position: refs/heads/master@{#855975}
    
    Bug: 974410
    Change-Id: Ie14784429c202f5845a8cd6a04403d87241daeae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2808779
    Reviewed-by: James Vecore <vecore@google.com>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869744}

--- chromeos/components/proximity_auth/messenger_impl.h
+++ chromeos/components/proximity_auth/messenger_impl.h
@@ -30,7 +30,7 @@ class MessengerImpl : public Messenger,
   // Messages are relayed over the provided |channel|.
   //
   // The messenger begins observing messages as soon as it is constructed.
-  explicit MessengerImpl(
+  MessengerImpl(
       std::unique_ptr<chromeos::secure_channel::ClientChannel> channel);
   ~MessengerImpl() override;
 
