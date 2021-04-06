c50c820bfc7f797cc1d1a4061d87b5387cb69f2f
Gavin Williams <gavinwill@chromium.org>
2021-04-06 23:30:35 +0000

    scanning: Create flag for Scan app sticky settings
    Bug: 1194636
    Change-Id: I03a9c00ca88dfe4fa9976b5f3d720432a5855826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2801315
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Auto-Submit: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869762}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2733,6 +2733,9 @@ extern const char kScalableStatusAreaDescription[];
 extern const char kScanAppMediaLinkName[];
 extern const char kScanAppMediaLinkDescription[];
 
+extern const char kScanAppStickySettingsName[];
+extern const char kScanAppStickySettingsDescription[];
+
 extern const char kSchedulerConfigurationName[];
 extern const char kSchedulerConfigurationDescription[];
 extern const char kSchedulerConfigurationConservative[];
