c70a47d7ac41bb62cd5a04fb36963d6aa1635dc9
Etienne Bergeron <etienneb@chromium.org>
2021-04-06 20:23:56 +0000

    Re-enable flaky test caused by PowerMonitor
    This CL is changing the unittest code to use a Mock PowerMonitor
    to avoid any risk of flakiness. Previously, the PowerMonitorSource
    was not removed and can cause other tests that requires
    PowerMonitor to fail.
    
    
    R=fdoray@chromium.org
    
    Bug: 921687, 1188692
    Change-Id: I2d889bf5f288852dbe5dd9d6e86d6d42f178a27d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2800192
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869683}

--- chrome/browser/extensions/extension_protocols_unittest.cc
+++ chrome/browser/extensions/extension_protocols_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/files/file_util.h"
+#include "base/optional.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/stl_util.h"
@@ -293,10 +294,8 @@ class ExtensionProtocolsTestBase : public testing::Test {
                             : testing_profile_.get();
   }
 
-  void SimulateSystemSuspendForRequests() {
-    power_monitor_source_ = new base::PowerMonitorTestSource();
-    base::PowerMonitor::Initialize(
-        std::unique_ptr<base::PowerMonitorSource>(power_monitor_source_));
+  void EnableSimulationOfSystemSuspendForRequests() {
+    power_monitor_source_.emplace();
   }
 
   void AddExtensionAndPerformResourceLoad(const ExtensionId& extension_id) {
@@ -350,9 +349,12 @@ class ExtensionProtocolsTestBase : public testing::Test {
         CreateResourceRequest("GET", destination, url), client.CreateRemote(),
         net::MutableNetworkTrafficAnnotationTag(TRAFFIC_ANNOTATION_FOR_TESTS));
 
+    // If `power_monitor_source_` is set, simulates power suspend and resume
+    // notifications. These notifications are posted tasks that will be executed
+    // by `client.RunUntilComplete()`.
     if (power_monitor_source_) {
-      power_monitor_source_->GenerateSuspendEvent();
-      power_monitor_source_->GenerateResumeEvent();
+      power_monitor_source_->Suspend();
+      power_monitor_source_->Resume();
     }
 
     client.RunUntilComplete();
@@ -380,8 +382,8 @@ class ExtensionProtocolsTestBase : public testing::Test {
   const bool force_incognito_;
   const ukm::SourceIdObj test_ukm_id_;
 
-  // |power_monitor_source_| is owned by the global PowerMonitor.
-  base::PowerMonitorTestSource* power_monitor_source_ = nullptr;
+  base::Optional<base::test::ScopedPowerMonitorTestSource>
+      power_monitor_source_;
 };
 
 class ExtensionProtocolsTest : public ExtensionProtocolsTestBase {
@@ -831,17 +833,9 @@ TEST_F(ExtensionProtocolsTest, MimeTypesForKnownFiles) {
   }
 }
 
-#if defined(OS_WIN)
-#define MAYBE_ExtensionRequestsNotAborted DISABLED_ExtensionRequestsNotAborted
-#else
-#define MAYBE_ExtensionRequestsNotAborted ExtensionRequestsNotAborted
-#endif
 // Tests that requests for extension resources (including the generated
 // background page) are not aborted on system suspend.
-//
-// Flaky on Windows.
-// TODO(https://crbug.com/921687): Investigate and fix.
-TEST_F(ExtensionProtocolsTest, MAYBE_ExtensionRequestsNotAborted) {
+TEST_F(ExtensionProtocolsTest, ExtensionRequestsNotAborted) {
   // Register a non-incognito extension protocol handler.
   SetProtocolHandler(false);
 
@@ -853,7 +847,7 @@ TEST_F(ExtensionProtocolsTest, MAYBE_ExtensionRequestsNotAborted) {
       &error);
   ASSERT_TRUE(extension.get()) << error;
 
-  SimulateSystemSuspendForRequests();
+  EnableSimulationOfSystemSuspendForRequests();
 
   // Request the generated background page. Ensure the request completes
   // successfully.
