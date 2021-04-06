9c9a06583bc79c5d9cc67dfbac1275ace6a64f07
Viktor Semeniuk <vsemeniuk@google.com>
2021-04-06 10:15:12 +0000

    Removed ProcessLoginsChanged
    This change removes ProcessLoginsChanged and moves logic for insecure
    credentials removal on password update to login_database.
    
    Bug: 1194029
    Change-Id: Ic7858d028de7e25fa0f7a048ce67ae2270b5beaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784825
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#869459}

--- components/password_manager/core/browser/password_store.cc
+++ components/password_manager/core/browser/password_store.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/android_affiliation/affiliated_match_helper.h"
 #include "components/password_manager/core/browser/field_info_table.h"
 #include "components/password_manager/core/browser/insecure_credentials_consumer.h"
-#include "components/password_manager/core/browser/insecure_credentials_observer.h"
 #include "components/password_manager/core/browser/insecure_credentials_table.h"
 #include "components/password_manager/core/browser/password_form.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
@@ -770,20 +769,6 @@ void PasswordStore::NotifyLoginsChanged(
 
     if (reuse_detector_)
       reuse_detector_->OnLoginsChanged(changes);
-
-    ProcessLoginsChanged(
-        changes,
-        base::BindRepeating(
-            [](scoped_refptr<PasswordStore> store,
-               const std::string& signon_realm, const std::u16string& username,
-               RemoveInsecureCredentialsReason reason) {
-              auto callback =
-                  base::BindOnce(&PasswordStore::RemoveInsecureCredentialsImpl,
-                                 store, signon_realm, username, reason);
-              store->InvokeAndNotifyAboutInsecureCredentialsChange(
-                  std::move(callback));
-            },
-            scoped_refptr<PasswordStore>(this)));
   }
 }
 
