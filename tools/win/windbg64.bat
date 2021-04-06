3bc208f669c0fa6ee9f3aa1a0b9d0f223909fe0c
Bruce Dawson <brucedawson@chromium.org>
2021-04-06 03:58:13 +0000

    Add windbg32.bat and windbg64.bat
    Running windbg from the command line can be helpful as a way to specify
    command-line arguments such as -o but it is unwieldy due to the varying
    locations for the SDK install. These batch files find the SDK and launch
    either the x86 or x64 versions of windbg. They also print some very
    brief suggestions on command-line arguments.
    
    As an example, instead of running this command:
    
        "third_party\depot_tools\win_toolchain\vs_files\20d5f2553f\Windows Kits\10\Debuggers\x64\windbg.exe" -g -G -o out\Default\chrome.exe
    
    a developer can run:
    
        tools\win\windbg64 -g -G -o out\Default\chrome.exe
    
    Change-Id: Ib615e97b3281b64a5cba67b85936c0b384a422bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794820
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869384}

---
+++ tools/win/windbg64.bat
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+@echo off
+
+REM Copyright 2021 The Chromium Authors. All rights reserved.
+REM Use of this source code is governed by a BSD-style license that can be
+REM found in the LICENSE file.
+
+REM Helper batch file to launch windbg, either from a local install or from the
+REM packaged Chromium toolchain.
+REM There are windbg32.bat and windbg64.bat flavors of this batch file - keep
+REM them synchronized when making changes.
+
+@setlocal
+
+REM Print helpful messages and set win_sdk_dir
+call %~dp0windbg_common.bat
+
+@echo on
+"%win_sdk_dir%\Debuggers\x64\windbg.exe" %*
