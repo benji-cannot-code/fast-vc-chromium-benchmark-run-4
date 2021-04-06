726b971c69b58aa554acf95091ee3e011dd2f1b8
Alexander Cooper <alcooper@chromium.org>
2021-04-06 21:05:34 +0000

    Make ARCompositor composite DOM overlay content
    Updates the ARCompositor to be able to appropriately composite any DOM
    content as part of its submitted CompositorFrame. Note that this
    requires some additional setup, as we need to ensure that we are now
    placed in front of any DOM content when this compositing is occurring.
    In addition to this, we need to "parent" the DOM content to ourselves
    in viz (note that content may have multiple parents). We also need to
    ensure that we get an updated SurfaceId after everything has settled
    out from fullscreening the content. Unfortunately, we don't seem to
    get the Fullscreen callback from the webcontents observer, and there
    may be other cases that cause the SurfaceId to change, so we query
    for it on each frame as a best-effort. (The SurfaceId shouldn't be
    changing every frame).
    
    Bug: 1182864
    Change-Id: Idf11b1c56144218f723df950c7b0912fd7532ffb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794817
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869701}

--- chrome/android/java/src/org/chromium/chrome/browser/vr/ArCompositorDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr/ArCompositorDelegateImpl.java
@@ -28,8 +28,8 @@ public class ArCompositorDelegateImpl implements ArCompositorDelegate {
     }
 
     @Override
-    public void setOverlayImmersiveArMode(boolean enabled) {
-        mCompositorView.setOverlayImmersiveArMode(enabled);
+    public void setOverlayImmersiveArMode(boolean enabled, boolean domSurfaceNeedsConfiguring) {
+        mCompositorView.setOverlayImmersiveArMode(enabled, domSurfaceNeedsConfiguring);
     }
 
     @Override
