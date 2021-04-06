3a512733594072eb14c4ca5266d4ba40c40a5b5c
Camillo Bruni <cbruni@chromium.org>
2021-04-06 14:56:42 +0000

    [benchmark] Enable blinkResourceMetric for v8.browsing* benchmarks
    Enable blinkResourceMetric as additional sanity checks on top of the
    console.error metric that is already enabled.
    
    This CL enables the metric on the v8.browsing stories as a first
    test-ground. We will then expand this to the system_health benchmarks.
    
    Bug: 1178198
    Change-Id: I7f267e185b0743d9801b375c1613b64cc0ec1db0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2799635
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869528}

--- tools/perf/benchmarks/v8_helper.py
+++ tools/perf/benchmarks/v8_helper.py
@@ -42,6 +42,7 @@ def AugmentOptionsForV8Metrics(options, enable_runtime_call_stats=True):
       'v8.console',
       'webkit.console',
       # Blink categories.
+      'blink',
       'blink_gc',
       'partition_alloc',
       # Needed for the metric reported by page.
@@ -62,6 +63,7 @@ def AugmentOptionsForV8Metrics(options, enable_runtime_call_stats=True):
 
   metrics = [
       'blinkGcMetric',
+      'blinkResourceMetric',
       'consoleErrorMetric',
       'expectedQueueingTimeMetric',
       'gcMetric',
