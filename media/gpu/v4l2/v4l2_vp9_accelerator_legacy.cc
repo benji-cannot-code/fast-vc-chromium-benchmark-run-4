93484f31b4d901d7119eacf95df297ae7b3480a7
Alexandre Courbot <acourbot@chromium.org>
2021-04-06 03:23:27 +0000

    media/gpu/v4l2: directly include legacy vp9 controls definition
    The upstream videodev2.h does not include these non-standard controls.
    Include it ourselves so we can remove the inclusion from videodev2.h
    safely.
    
    BUG=b:161415741
    TEST=Chromium builds for arm-generic.
    
    Change-Id: Ibcd36e0f87397ae443a702ebf3f6909f629f3cfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2802231
    Auto-Submit: Alexandre Courbot <acourbot@chromium.org>
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869377}

--- media/gpu/v4l2/v4l2_vp9_accelerator_legacy.cc
+++ media/gpu/v4l2/v4l2_vp9_accelerator_legacy.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <linux/videodev2.h>
 #include <string.h>
 
+#include <linux/media/vp9-ctrls-legacy.h>
+
 #include "base/logging.h"
 #include "base/stl_util.h"
 #include "media/gpu/macros.h"
