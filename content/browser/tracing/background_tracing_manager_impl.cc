0c6f79f3e99d1dcda1cc689ce715ae8a6352581d
Chris Hamilton <chrisha@chromium.org>
2021-04-07 19:48:58 +0000

    Use new CONTENT_TRIGGER for performance.mark triggers.
    For better integration into the Perfetto pipeline this uses a new named
    trigger enum for content-triggers. The actual name of the content
    trigger will be plumbed through as additional metadata.
    
    BUG=1181774
    
    Change-Id: I5f52ab4807305ad5e9c678ac31936f383ada8def
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809794
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870149}

--- content/browser/tracing/background_tracing_manager_impl.cc
+++ content/browser/tracing/background_tracing_manager_impl.cc
@@ -56,6 +56,10 @@ const char kBackgroundTracingUploadUrl[] = "upload_url";
 }  // namespace
 
 // static
+const char BackgroundTracingManager::kContentTriggerConfig[] =
+    "content-trigger-config";
+
+// static
 BackgroundTracingManager* BackgroundTracingManager::GetInstance() {
   return BackgroundTracingManagerImpl::GetInstance();
 }
