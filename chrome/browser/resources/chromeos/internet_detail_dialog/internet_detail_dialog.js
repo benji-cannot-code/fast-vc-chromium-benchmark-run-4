f2a57ab3117a230864e3b4e96359beb5d0bbf780
Gordon Seto <gordonseto@google.com>
2021-04-07 18:51:01 +0000

    [CrOS Settings] Disable internet-detail-dialog when inhibited.
    Added disabled property to internet-detail-dialog that is set to true
    when the device is inhibited.
    
    Bug: 1093185
    Change-Id: Iac54e62889a6c9ab7d9a073c8fb05f660eb8dda6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809445
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/master@{#870124}

--- chrome/browser/resources/chromeos/internet_detail_dialog/internet_detail_dialog.js
+++ chrome/browser/resources/chromeos/internet_detail_dialog/internet_detail_dialog.js
@@ -87,6 +87,16 @@ Polymer({
           managedProperties_.*)`,
     },
 
+    /**
+     * When true, all inputs that allow state to be changed (e.g., toggles,
+     * inputs) are disabled.
+     */
+    disabled_: {
+      type: Boolean,
+      value: false,
+      computed: 'computeDisabled_(deviceState_.*)'
+    },
+
     /** @private */
     isUpdatedCellularUiEnabled_: {
       type: Boolean,
@@ -455,6 +465,9 @@ Polymer({
    * @private
    */
   enableConnectDisconnect_(managedProperties) {
+    if (this.disabled_) {
+      return false;
+    }
     if (!this.showConnectDisconnect_(managedProperties)) {
       return false;
     }
@@ -562,7 +575,8 @@ Polymer({
    */
   hasVisibleFields_(fields) {
     return fields.some((field) => {
-      const value = this.get(field, this.managedProperties_);
+      const key = OncMojo.getManagedPropertyKey(field);
+      const value = this.get(key, this.managedProperties_);
       return value !== undefined && value !== '';
     });
   },
@@ -624,5 +638,23 @@ Polymer({
         OncMojo.managedPropertiesToNetworkState(this.managedProperties_);
     assert(networkState);
     return isActiveSim(networkState, this.deviceState_);
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  computeDisabled_() {
+    if (!this.isUpdatedCellularUiEnabled_) {
+      return false;
+    }
+    if (!this.deviceState_ ||
+        this.deviceState_.type !==
+            chromeos.networkConfig.mojom.NetworkType.kCellular) {
+      return false;
+    }
+    // If this is a cellular device and inhibited, state cannot be changed, so
+    // the dialog's inputs should be disabled.
+    return OncMojo.deviceIsInhibited(this.deviceState_);
   }
 });
