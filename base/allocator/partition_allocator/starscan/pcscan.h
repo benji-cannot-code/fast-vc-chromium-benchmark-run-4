4f2a0cbe4b403b5fcfdc328c6c5d4abe35b5e9e9
Anton Bikineev <bikineev@chromium.org>
2021-04-07 02:05:27 +0000

    PCScan: Temporarily disable safepoints due to crashes
    Enabling safepoints led to a new crash in RunSSE3. Temporarily disable
    them to cut M91 Dev RC.
    
    Bug: 1196458
    Change-Id: I5cf30d0e6b8cf881a388672cd9003eab8f7f7fb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809174
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869830}

--- base/allocator/partition_allocator/starscan/pcscan.h
+++ base/allocator/partition_allocator/starscan/pcscan.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #endif
 
-#define PCSCAN_DISABLE_SAFEPOINTS 0
+#define PCSCAN_DISABLE_SAFEPOINTS 1
 
 namespace base {
 namespace internal {
