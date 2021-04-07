12081b3c5e4c5c6e098739d7d9162aefd683b679
Abigail Klein <abigailbklein@google.com>
2021-04-07 03:47:35 +0000

    [Live Caption] Remove extra paragraph node in CaptionBubbleLabel.
    The CaptionBubbleLabel's accessibility tree is:
    document
    +staticText
    +staticText ... 1 staticText node per visual line
    
    Before this change, it was:
    document
    +paragraph
    ++staticText
    ++staticText ... 1 staticText node per visual line
    
    Bug: 1055150, 1191091
    Change-Id: I3a26f004b7525aa08a43e5188f14832952b186e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809094
    Reviewed-by: Chris Hall <chrishall@chromium.org>
    Commit-Queue: Chris Hall <chrishall@chromium.org>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Auto-Submit: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#869866}

--- chrome/browser/ui/views/accessibility/caption_bubble_controller_views_browsertest.cc
+++ chrome/browser/ui/views/accessibility/caption_bubble_controller_views_browsertest.cc
@@ -195,19 +195,6 @@ class CaptionBubbleControllerViewsTest : public InProcessBrowserTest {
     return ax_document->GetCustomData();
   }
 
-  size_t GetNumberAXParagraphNodes() {
-    auto& ax_document =
-        GetLabel()->GetViewAccessibility().virtual_children()[0];
-    return ax_document->children().size();
-  }
-
-  ui::AXNodeData GetAXParagraphNodeData() {
-    auto& ax_document =
-        GetLabel()->GetViewAccessibility().virtual_children()[0];
-    auto& ax_paragraph = ax_document->children()[0];
-    return ax_paragraph->GetCustomData();
-  }
-
   std::vector<ui::AXNodeData> GetAXLinesNodeData() {
     std::vector<ui::AXNodeData> node_datas;
     views::Label* label = GetLabel();
@@ -215,8 +202,7 @@ class CaptionBubbleControllerViewsTest : public InProcessBrowserTest {
       return node_datas;
     auto& ax_document =
         GetLabel()->GetViewAccessibility().virtual_children()[0];
-    auto& ax_paragraph = ax_document->children()[0];
-    auto& ax_lines = ax_paragraph->children();
+    auto& ax_lines = ax_document->children();
     for (auto& ax_line : ax_lines) {
       node_datas.push_back(ax_line->GetCustomData());
     }
@@ -1065,11 +1051,7 @@ IN_PROC_BROWSER_TEST_F(CaptionBubbleControllerViewsTest, AccessibleTextSetUp) {
   EXPECT_EQ(ax::mojom::Restriction::kReadOnly,
             GetAXDocumentNodeData().GetRestriction());
 
-  // There is 1 paragraph node in the paragraph.
-  EXPECT_EQ(1u, GetNumberAXParagraphNodes());
-  EXPECT_EQ(ax::mojom::Role::kParagraph, GetAXParagraphNodeData().role);
-
-  // There is 1 staticText node in the paragraph.
+  // There is 1 staticText node in the document.
   EXPECT_EQ(1u, GetAXLinesNodeData().size());
   EXPECT_EQ(ax::mojom::Role::kStaticText, GetAXLinesNodeData()[0].role);
   EXPECT_EQ("Capybaras are the world's largest rodents.",
