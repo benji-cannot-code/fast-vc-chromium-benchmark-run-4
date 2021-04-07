ad5e485976c96097e87ca13f6fcc44e4d5f83d5a
Lambros Lambrou <lambroslambrou@chromium.org>
2021-04-07 23:39:28 +0000

    [remoting host] Add WebrtcVideoFrameAdapter class.
    This adapter will be used in the standard encoding pipeline, to
    send captured DesktopFrame instances to the registered VideoSink which
    accepts webrtc::VideoFrame instances. Setting type kNative will ensure
    that the encoder receives the same VideoFrame with the adapter, so it
    can extract the DesktopFrame to encode it.
    
    Bug: 1192865
    Change-Id: Iaab3f363d5367c1987d5bccc183a1cac124c7e42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798962
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870268}

---
+++ remoting/protocol/webrtc_video_frame_adapter.cc
@@ -0,0 +1,59 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/protocol/webrtc_video_frame_adapter.h"
+
+#include <utility>
+
+#include "base/notreached.h"
+#include "third_party/webrtc/rtc_base/ref_counted_object.h"
+
+namespace remoting {
+namespace protocol {
+
+WebrtcVideoFrameAdapter::WebrtcVideoFrameAdapter(
+    std::unique_ptr<webrtc::DesktopFrame> frame)
+    : frame_(std::move(frame)), frame_size_(frame_->size()) {}
+
+WebrtcVideoFrameAdapter::~WebrtcVideoFrameAdapter() = default;
+
+// static
+webrtc::VideoFrame WebrtcVideoFrameAdapter::CreateVideoFrame(
+    std::unique_ptr<webrtc::DesktopFrame> desktop_frame) {
+  rtc::scoped_refptr<WebrtcVideoFrameAdapter> adapter =
+      new rtc::RefCountedObject<WebrtcVideoFrameAdapter>(
+          std::move(desktop_frame));
+  return webrtc::VideoFrame::Builder().set_video_frame_buffer(adapter).build();
+}
+
+std::unique_ptr<webrtc::DesktopFrame>
+WebrtcVideoFrameAdapter::TakeDesktopFrame() {
+  return std::move(frame_);
+}
+
+webrtc::VideoFrameBuffer::Type WebrtcVideoFrameAdapter::type() const {
+  return Type::kNative;
+}
+
+int WebrtcVideoFrameAdapter::width() const {
+  return frame_size_.width();
+}
+
+int WebrtcVideoFrameAdapter::height() const {
+  return frame_size_.height();
+}
+
+rtc::scoped_refptr<webrtc::I420BufferInterface>
+WebrtcVideoFrameAdapter::ToI420() {
+  // Strictly speaking all adapters must implement ToI420(), so that if the
+  // external encoder fails, an internal libvpx could be used. But the
+  // remoting encoder already uses libvpx, so there's no reason for fallback to
+  // happen.
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+}  // namespace protocol
+}  // namespace remoting
