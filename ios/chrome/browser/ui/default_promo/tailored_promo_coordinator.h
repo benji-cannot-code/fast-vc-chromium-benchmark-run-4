91214ae4ba2bf42d4528b41728e8bed02a7fd83a
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2021-04-07 17:44:36 +0000

    [iOS][Default Promo] Add tailored coordinator & view controller
    Files forked from current browser modal fullscreen promo.
    
    Bug: 1191736, 1191727
    Change-Id: I408212710e1b0c1cc7b69fdc3fc906a0ea336452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2810173
    Commit-Queue: Javier Flores <javierrobles@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870092}

---
+++ ios/chrome/browser/ui/default_promo/tailored_promo_coordinator.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_DEFAULT_PROMO_TAILORED_PROMO_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_UI_DEFAULT_PROMO_TAILORED_PROMO_COORDINATOR_H_
+
+#import "ios/chrome/browser/ui/coordinators/chrome_coordinator.h"
+
+#import "ios/chrome/browser/ui/default_promo/default_browser_promo_commands.h"
+
+// Coordinator for a fullscreen modal default browser promo.
+@interface TailoredPromoCoordinator : ChromeCoordinator
+
+// Handler for all actions of this coordinator.
+@property(nonatomic, weak) id<DefaultBrowserPromoCommands> handler;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_DEFAULT_PROMO_TAILORED_PROMO_COORDINATOR_H_
