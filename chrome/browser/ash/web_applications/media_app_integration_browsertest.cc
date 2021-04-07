0ac19f73a41cff41b43b3b55b60f3a8f1d44231d
Patti <patricialor@chromium.org>
2021-04-07 02:22:01 +0000

    Revert "Enable SharedArrayBuffer for MediaAppIntegrationTest"
    This reverts commit b7f50ea5d69ffde40994477c4a5eeacc6c84fc9f.
    
    Reason for revert: https://crrev.com/c/2763523 has landed, which adds COOP/COEP headers for Media app web UI. This change should no longer be necessary.
    
    Original change's description:
    > Enable SharedArrayBuffer for MediaAppIntegrationTest
    >
    > - Enables SABs so the LoadsInkForImageAnnotation will pass. We
    >   probably need to rework the test, and perhaps the Ink app as well.
    >
    > Bug: chromium:1195678,chromium:1194557
    > Change-Id: I1864580f852f8fc4778d9e901c804f62c2c41858
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2805568
    > Commit-Queue: Trent Apted <tapted@chromium.org>
    > Reviewed-by: Trent Apted <tapted@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#869268}
    
    Bug: chromium:1195678
    Bug: chromium:1194557
    Change-Id: I0c35bad9687d2ad51f26cd66e4b5f6f3324dc504
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809993
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Commit-Queue: Patti <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869839}

--- chrome/browser/ash/web_applications/media_app_integration_browsertest.cc
+++ chrome/browser/ash/web_applications/media_app_integration_browsertest.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/components/media_app_ui/test/media_app_ui_browsertest.h"
 #include "chromeos/components/media_app_ui/url_constants.h"
 #include "components/crash/content/browser/error_reporting/mock_crash_endpoint.h"
-#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/entry_info.h"
@@ -72,10 +71,7 @@ constexpr char kDomExceptionScript[] =
 class MediaAppIntegrationTest : public SystemWebAppIntegrationTest {
  public:
   MediaAppIntegrationTest() {
-    // LoadsInkForImageAnnotation needs SharedArrayBuffer.
-    // TODO(crbug.com/1144104) Migrate the Ink app to be cross-origin isolated.
-    scoped_feature_list_.InitWithFeatures(
-        {chromeos::features::kMediaApp, features::kSharedArrayBuffer}, {});
+    scoped_feature_list_.InitWithFeatures({chromeos::features::kMediaApp}, {});
   }
 
  private:
