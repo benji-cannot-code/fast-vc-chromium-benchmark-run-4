895c3cfc4f942481a85ab7d652f05796d21bed20
Hiroshige Hayashizaki <hiroshige@chromium.org>
2021-04-07 00:03:32 +0000

    Revert "Added accessibility event on Android for when dialogs are opened"
    This reverts commit bf7a0e8e74f9eaa0fe7fafe7509e15f310c8c235.
    
    Reason for revert: Newly added tests are failing on
    Builder Win 7 Tests x64 (1). crbug.com/1196471
    
    Original change's description:
    > Added accessibility event on Android for when dialogs are opened
    >
    > This CL expands accessibility support for modal dialogs.
    >
    > With the current implementation, when a site opens a modal dialog,
    > Chrome alerts AT of a window content change, but does not include
    > any further information such as dialog title/text.
    >
    > With this CL we update to now send a TYPE_WINDOW_STATE_CHANGED event
    > specifying that a new pane has appeared, and we add text for the
    > title of that pane.
    >
    > To populate the text, we look for the aria-labelledby nodes for the
    > dialog or alertdialog in question, and concatenate all inner texts.
    > Some pages use aria-describedby, which we check when no labels are
    > available. And if neither labels nor describedby are available, we
    > default to a simple message of: "dialog opened."
    >
    > This CL applies to role="dialog" and role="alertdialog" elements.
    >
    > AX-Relnotes: role="dialog" and role="alertdialog" elements on Android now proactively send their title to AT when opened.
    > Bug: 1179314
    > Change-Id: I3c41b131f981bc649c1dce24bf52556e8fd85ac1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2708619
    > Reviewed-by: Mark Schillaci <mschillaci@google.com>
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Reviewed-by: danakj <danakj@chromium.org>
    > Commit-Queue: Mark Schillaci <mschillaci@google.com>
    > Cr-Commit-Position: refs/heads/master@{#869619}
    
    Bug: 1179314, 1196471
    Change-Id: I4ec69e80c7ea77f5bb60c05ae73f4b97e9022d62
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2808602
    Auto-Submit: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Owners-Override: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869775}

--- content/browser/accessibility/browser_accessibility_android.h
+++ content/browser/accessibility/browser_accessibility_android.h
@@ -95,8 +95,6 @@ class CONTENT_EXPORT BrowserAccessibilityAndroid : public BrowserAccessibility {
 
   std::string GetRoleString() const;
 
-  std::u16string GetDialogModalMessageText() const;
-
   std::u16string GetContentInvalidErrorMessage() const;
 
   std::u16string GetStateDescription() const;
