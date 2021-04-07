e5b5565633f3fe62ac43bf268aab671ccb43a858
John Wu <jzw@chromium.org>
2021-04-07 18:55:03 +0000

    Add missing pref to ios/web_view
    to register this pref to avoid a DCHECK.
    
    features: :kAutofillMetadataUploads is enabled by default now so we need
    Change-Id: I328375946db893c8979fa78ee185f72c70488f49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2803621
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870127}

--- ios/web_view/internal/web_view_browser_state.mm
+++ ios/web_view/internal/web_view_browser_state.mm
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync_device_info/device_info_prefs.h"
 #include "components/translate/core/browser/translate_pref_names.h"
 #include "components/translate/core/browser/translate_prefs.h"
+#include "components/unified_consent/unified_consent_service.h"
 #include "ios/web/public/thread/web_task_traits.h"
 #include "ios/web/public/thread/web_thread.h"
 #import "ios/web/public/web_state.h"
@@ -175,6 +176,7 @@ void WebViewBrowserState::RegisterPrefs(
   syncer::SyncPrefs::RegisterProfilePrefs(pref_registry);
   syncer::DeviceInfoPrefs::RegisterProfilePrefs(pref_registry);
   safe_browsing::RegisterProfilePrefs(pref_registry);
+  unified_consent::UnifiedConsentService::RegisterPrefs(pref_registry);
 
   // Instantiate all factories to setup dependency graph for pref registration.
   WebViewLanguageModelManagerFactory::GetInstance();
