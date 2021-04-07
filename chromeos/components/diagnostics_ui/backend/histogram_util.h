19c30379cc9c7c12679ed20e0bf7b90d11eec15c
Michael Checo <michaelcheco@google.com>
2021-04-07 21:53:43 +0000

    Diagnostics: Wire up networking routines
    Bug: 1191678
    Test: ./out/Default/chromeos_components_unittests
    Change-Id: Idc4bf16cfb00a66662f0766f977f77b3bfd89770
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2786141
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870224}

--- chromeos/components/diagnostics_ui/backend/histogram_util.h
+++ chromeos/components/diagnostics_ui/backend/histogram_util.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_COMPONENTS_DIAGNOSTICS_UI_BACKEND_HISTOGRAM_UTIL_H_
 #define CHROMEOS_COMPONENTS_DIAGNOSTICS_UI_BACKEND_HISTOGRAM_UTIL_H_
 
+#include <stddef.h>
 #include <cstdint>
 
 #include "chromeos/components/diagnostics_ui/mojom/system_routine_controller.mojom.h"
