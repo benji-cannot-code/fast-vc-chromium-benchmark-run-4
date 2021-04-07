317abd1dc8de8e2c8e0a5e8bb23e0f40afd22264
Francois Doray <fdoray@chromium.org>
2021-04-07 02:32:30 +0000

    Remove obsolete histograms PerformanceMonitor.AverageDisk*.
    These histograms are expired and not need for our current
    investigations.
    
    Fixed: 1089250, 1190967
    Change-Id: I9a1558d4f508b0464c988ce4f6328239790eba08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798000
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#869845}

--- base/process/process_metrics.cc
+++ base/process/process_metrics.cc
@@ -18,6 +18,8 @@ namespace base {
 
 namespace {
 
+#if defined(OS_APPLE) || defined(OS_LINUX) || defined(OS_CHROMEOS) || \
+    defined(OS_AIX)
 int CalculateEventsPerSecond(uint64_t event_count,
                              uint64_t* last_event_count,
                              base::TimeTicks* last_calculated) {
@@ -35,6 +37,7 @@ int CalculateEventsPerSecond(uint64_t event_count,
   *last_event_count = event_count;
   return events_per_second;
 }
+#endif
 
 }  // namespace
 
@@ -153,12 +156,5 @@ uint64_t ProcessMetrics::GetCumulativeDiskUsageInBytes() {
   return 0;
 }
 #endif
-
-uint64_t ProcessMetrics::GetDiskUsageBytesPerSecond() {
-  uint64_t cumulative_disk_usage = GetCumulativeDiskUsageInBytes();
-  return CalculateEventsPerSecond(cumulative_disk_usage,
-                                  &last_cumulative_disk_usage_,
-                                  &last_disk_usage_time_);
-}
 
 }  // namespace base
