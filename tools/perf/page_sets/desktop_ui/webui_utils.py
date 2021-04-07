b14170a0a828755e68cef016c857e841d55b8242
Yuheng Huang <yuhengh@chromium.org>
2021-04-07 19:18:27 +0000

    WebUITabStrip: Implement benchmark story
    tools/perf/run_benchmark run desktop_ui --browser-executable=out/Default/chrome --story-filter=webui_tab_strip:top10:loading:2020
    
    Usage: 
    Change-Id: I78f01c9b17433303ac5f33aae60ebc3c99c3d2f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2628169
    Reviewed-by: John Chen <johnchen@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870135}

---
+++ tools/perf/page_sets/desktop_ui/webui_utils.py
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import py_utils
+from telemetry.internal.actions.action_runner import ActionRunner
+
+
+def Inspect(browser, url):
+  # Wait for url to be inspectable.
+  browser.supports_inspecting_webui = True
+  tabs = browser.tabs
+  py_utils.WaitFor(lambda: any([True for tab in tabs if tab.url == url]), 10)
+  # Wait for url to load.
+  tab = next(iter([tab for tab in tabs if tab.url == url]))
+  action_runner = ActionRunner(tab)  # Recreate action_runner.
+  tab.WaitForDocumentReadyStateToBeComplete()
+  return action_runner
