407411afc53bf5847cff6af76399bf099455f81d
Stephan Hartmann <stha09@googlemail.com>
2021-04-07 06:32:37 +0000

    GCC: fix ternary operator in gtk_util
    Ternary operator requires that operands after ? have
    same type, but is std::nullptr_t and ScopedGObject<GtkWidget>.
    Use ScopedGObject<GtkWidget>() for std::nullptr_t instead.
    
    Bug: 819294
    Change-Id: Ia5669cc97744e4cc361844c3e5f476fba46ab90e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2804680
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#869894}

--- ui/gtk/gtk_util.cc
+++ ui/gtk/gtk_util.cc
@@ -286,7 +286,7 @@ GtkCssContext::operator GtkStyleContext*() {
 GtkCssContext GtkCssContext::GetParent() {
   if (GtkCheckVersion(4)) {
     return GtkCssContext(WrapGObject(gtk_widget_get_parent(widget_)),
-                         root_ == widget_ ? nullptr : root_);
+                         root_ == widget_ ? ScopedGObject<GtkWidget>() : root_);
   }
   return GtkCssContext(WrapGObject(gtk_style_context_get_parent(context_)));
 }
