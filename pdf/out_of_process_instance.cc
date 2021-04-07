8feae8af8a4adfe8595c81edfa9eb9074d001064
Hui Yingst <nigi@chromium.org>
2021-04-07 20:28:10 +0000

    [unseasoned-pdf] Represent the plugin rectangle with `plugin_rect_`.
    In PdfViewPluginBase, replace `plugin_size_` and `plugin_offset_` with
    a gfx::Rect member `plugin_rect_`, which represents the plugin rectangle
    in pixels with the device scale applied. Since the previous
    `plugin_offset_` was not scaled by device, revert the scaling operation
    on plugin_rect_.origin() before using it.
    
    Change-Id: I723e3b5e248e63ec71c284208e289eb3f82d3aae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809134
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870180}

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -1090,7 +1090,7 @@ void OutOfProcessInstance::HandleResetPrintPreviewModeMessage(
   engine()->SetGrayscale(dict.Get(pp::Var(kJSPrintPreviewGrayscale)).AsBool());
   engine()->New(GetURL().c_str(), /*headers=*/nullptr);
 
-  paint_manager().InvalidateRect(gfx::Rect(plugin_size()));
+  paint_manager().InvalidateRect(gfx::Rect(plugin_rect().size()));
 }
 
 void OutOfProcessInstance::HandleSaveAttachmentMessage(
