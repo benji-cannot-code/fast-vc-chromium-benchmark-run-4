d5b3d13c495b1c8c833f6bfc4ff4a881bf407d07
Dirk Pranke <dpranke@google.com>
2021-04-07 02:11:22 +0000

    Fix a py3 build determinism issue in the idl compiler.
    Object ids become non-deterministic in Python3, which means
    that things that use them like sets and dicts can have
    unpredictable orderings. Anything that relies on deterministic
    output needs to handle that.
    
    This changes fixes one such issue in the Blink IDL compiler.
    
    Bug: 1195213, 1194277
    Change-Id: Ibc0757f7d7cc379e878dba5125dca1639bb6c699
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2808780
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/master@{#869834}

--- third_party/blink/renderer/bindings/tests/results/core/test_dictionary.h
+++ third_party/blink/renderer/bindings/tests/results/core/test_dictionary.h
@@ -39,11 +39,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class Element;
 class EventTarget;
 class InternalDictionary;
-class TestObject;
 class TestInterfaceImplementation;
-class Element;
+class TestObject;
 
 class CORE_EXPORT TestDictionary : public IDLDictionaryBase {
  public:
