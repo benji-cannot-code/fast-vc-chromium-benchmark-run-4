b6784d49558b1ec02c063b0b63be7fa354cec18f
Ayu Ishii <ayui@chromium.org>
2021-04-07 20:55:30 +0000

    CodeHealth: Remove use of Value::GetAsBoolean
    This change removes the use of Value::GetAsBoolean
    and replaces with Value::is_bool(), Value::GetBool()
    
    Bug: 1187003
    Change-Id: I2f178f0051ec25d3d0df199e33d6ea373ca89499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2810980
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870194}

--- extensions/browser/api/system_display/system_display_apitest.cc
+++ extensions/browser/api/system_display/system_display_apitest.cc
@@ -476,9 +476,8 @@ IN_PROC_BROWSER_TEST_F(SystemDisplayApiTest, ShowNativeTouchCalibration) {
           show_native_calibration.get(), "[\"" + id + "\"]",
           browser_context()));
 
-  bool callback_result;
-  ASSERT_TRUE(result->GetAsBoolean(&callback_result));
-  ASSERT_TRUE(callback_result);
+  ASSERT_TRUE(result->is_bool());
+  EXPECT_TRUE(result->GetBool());
 }
 
 IN_PROC_BROWSER_TEST_F(SystemDisplayApiTest, SetMirrorMode) {
