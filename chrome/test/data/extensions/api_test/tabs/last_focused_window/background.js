56e55e8efc09748314176a13f3c707c1e3aa7fca
Ghazale Hosseinabadi <ghazale@google.com>
2021-04-07 18:15:22 +0000

    [Extensions] Add WINDOWS_GETLASTFOCUSED API test coverage
    This CL adds test coverage for WINDOWS_GETLASTFOCUSED API.
    
    Bug: 1194182
    Change-Id: Ib56cc155d956912ebc7226fce4dc646a379f0c62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2804712
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870107}

---
+++ chrome/test/data/extensions/api_test/tabs/last_focused_window/background.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function getSelectedPopup() {
+    chrome.windows.create(
+        {type: 'popup', url: 'about:blank', focused: true},
+        chrome.test.callbackPass(function(win) {
+      chrome.windows.getLastFocused(
+          chrome.test.callbackPass(function(lastFocusedWindowData) {
+        chrome.test.assertTrue(lastFocusedWindowData.id == win.id);
+      }));
+    }));
+  }
+]);
