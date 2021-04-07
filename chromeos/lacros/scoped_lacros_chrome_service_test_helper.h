3efe5af91c49c6822fab7eae92707421e44db62e
Antonio Gomes <tonikitoo@igalia.com>
2021-04-07 17:43:22 +0000

    buildfix: lacros is_component_build=true
    ld.lld: error: undefined symbol:
      chromeos::ScopedLacrosChromeServiceTestHelper::~ScopedLacrosChromeServiceTestHelper()
    >>> referenced by webrequest_rules_registry_unittest.cc:100
    >>> referenced by extension_service_test_base.cc:149
    (...)
    
    BUG=NONE
    R=jamescook@chromium.org
    
    Change-Id: I54f9b3b702e0103c463b45da812f6660816a9ae6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2810958
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870091}

--- chromeos/lacros/scoped_lacros_chrome_service_test_helper.h
+++ chromeos/lacros/scoped_lacros_chrome_service_test_helper.h
@@ -8,13 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/component_export.h"
+
 namespace chromeos {
 
 class LacrosChromeServiceImpl;
 
 // Helper for teststo instantiate LacrosChromeServiceImpl. This should only be
 // used for unit tests, not browser tests.
-class ScopedLacrosChromeServiceTestHelper {
+class COMPONENT_EXPORT(CHROMEOS_LACROS) ScopedLacrosChromeServiceTestHelper {
  public:
   ScopedLacrosChromeServiceTestHelper();
   virtual ~ScopedLacrosChromeServiceTestHelper();
