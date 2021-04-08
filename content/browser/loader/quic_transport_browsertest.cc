b1871535946844e9b55b880bd8678bf48caa01ab
Victor Vasiliev <vasilvv@chromium.org>
2021-04-08 10:39:29 +0000

    Support WebTransport over HTTP/3.
    This adds support for WebTransport over HTTP/3 within a dedicated QUIC
    connection (no pooling supported).
    
    Where possible, QuicTransport code is kept side-by-side for now, even
    though we do intend to remove it later.  The end-to-end browser test is
    directly migrated as it is non-trivial to duplicate it.
    
    Change-Id: I3c430fc4ca31c38b43735e92c1d392e74c06739d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2811842
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870446}

--- content/browser/loader/quic_transport_browsertest.cc
+++ content/browser/loader/quic_transport_browsertest.cc
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/ip_endpoint.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/third_party/quiche/src/quic/test_tools/crypto_test_utils.h"
+#include "net/third_party/quiche/src/quic/test_tools/quic_test_backend.h"
+#include "net/tools/quic/quic_simple_server.h"
 #include "net/tools/quic/quic_transport_simple_server.h"
 #include "services/network/public/cpp/network_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -50,9 +52,8 @@ class QuicTransportSimpleServerWithThread final {
   ~QuicTransportSimpleServerWithThread() {
     io_thread_->task_runner()->PostTask(
         FROM_HERE,
-        base::BindOnce(
-            [](std::unique_ptr<net::QuicTransportSimpleServer> server) {},
-            std::move(server_)));
+        base::BindOnce([](std::unique_ptr<net::QuicSimpleServer> server) {},
+                       std::move(server_)));
 
     base::ScopedAllowBaseSyncPrimitivesForTesting allow_wait_for_thread_join;
     io_thread_.reset();
@@ -71,11 +72,16 @@ class QuicTransportSimpleServerWithThread final {
     net::IPEndPoint server_address;
     io_thread_->task_runner()->PostTask(
         FROM_HERE, base::BindLambdaForTesting([&]() {
-          server_ = std::make_unique<net::QuicTransportSimpleServer>(
-              /*port=*/0, origins_,
-              quic::test::crypto_test_utils::ProofSourceForTesting());
-          const auto result = server_->Start();
-          CHECK_EQ(EXIT_SUCCESS, result);
+          backend_ = std::make_unique<quic::test::QuicTestBackend>();
+          backend_->set_enable_webtransport(true);
+          server_ = std::make_unique<net::QuicSimpleServer>(
+              quic::test::crypto_test_utils::ProofSourceForTesting(),
+              quic::QuicConfig(), quic::QuicCryptoServerConfig::ConfigOptions(),
+              quic::AllSupportedVersions(), backend_.get());
+          bool result = server_->CreateUDPSocketAndListen(
+              quic::QuicSocketAddress(quic::QuicSocketAddress(
+                  quic::QuicIpAddress::Any6(), /*port=*/0)));
+          CHECK(result);
           server_address = server_->server_address();
           event.Signal();
         }));
@@ -89,10 +95,12 @@ class QuicTransportSimpleServerWithThread final {
   const std::vector<url::Origin> origins_;
   net::IPEndPoint server_address_;
 
-  std::unique_ptr<net::QuicTransportSimpleServer> server_;
+  std::unique_ptr<quic::test::QuicTestBackend> backend_;
+  std::unique_ptr<net::QuicSimpleServer> server_;
   std::unique_ptr<base::Thread> io_thread_;
 };
 
+// TODO(vasilvv): Rename this to WebTransportBrowserTest.
 class QuicTransportBrowserTest : public ContentBrowserTest {
  public:
   QuicTransportBrowserTest() : server_({}) {
@@ -148,10 +156,10 @@ IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, Echo) {
   ASSERT_TRUE(ExecuteScript(
       shell(), base::StringPrintf(R"JS(
     async function run() {
-      const transport = new QuicTransport('quic-transport://localhost:%d/echo');
+      const transport = new WebTransport('https://localhost:%d/echo');
 
-      const writer = transport.sendDatagrams().getWriter();
-      const reader = transport.receiveDatagrams().getReader();
+      const writer = transport.datagramWritable.getWriter();
+      const reader = transport.datagramReadable.getReader();
 
       const data = new Uint8Array([65, 66, 67]);
       const id = setInterval(() => {
@@ -187,7 +195,7 @@ IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, EchoViaWebTransport) {
   ASSERT_TRUE(ExecuteScript(
       shell(), base::StringPrintf(R"JS(
     async function run() {
-      const transport = new WebTransport('quic-transport://localhost:%d/echo');
+      const transport = new WebTransport('https://localhost:%d/echo');
 
       const writer = transport.datagramWritable.getWriter();
       const reader = transport.datagramReadable.getReader();
@@ -216,7 +224,7 @@ IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, EchoViaWebTransport) {
   ASSERT_TRUE(WaitForTitle(u"PASS", {u"FAIL"}));
 }
 
-IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, ClientIndicationFailure) {
+IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, NonexistentResource) {
   ASSERT_TRUE(embedded_test_server()->Start());
   ASSERT_TRUE(
       NavigateToURL(shell(), embedded_test_server()->GetURL("/title2.html")));
@@ -228,17 +236,14 @@ IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, ClientIndicationFailure) {
     async function run() {
       // The client indication fails because there is no resource /X
       // on the server.
-      const transport = new QuicTransport('quic-transport:localhost:%d/X');
-
-      // Client indication is NOT part of handshake.
-      await transport.ready;
+      const transport = new WebTransport('https://localhost:%d/X');
 
       try {
-        await transport.closed;
+        await transport.ready;
       } catch (e) {
         return;
       }
-      throw Error('closed should be rejected');
+      throw Error('ready should be rejected');
     }
 
     run().then(() => { document.title = 'PASS'; },
@@ -259,11 +264,11 @@ IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, CreateSendStream) {
   ASSERT_TRUE(ExecuteScript(
       shell(), base::StringPrintf(R"JS(
     async function run() {
-      const transport = new QuicTransport('quic-transport://localhost:%d/echo');
+      const transport = new WebTransport('https://localhost:%d/echo');
 
       await transport.ready;
 
-      const sendStream = await transport.createSendStream();
+      const sendStream = await transport.createUnidirectionalStream();
       const writer = sendStream.writable.getWriter();
       await writer.write(new Uint8Array([65, 66, 67]));
       await writer.close();
@@ -278,6 +283,7 @@ IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, CreateSendStream) {
 }
 
 // ReceiveStream is flaky: crbug.com/1140193
+// TODO(vasilvv): change from QuicTransport to WebTransport when re-enabling.
 #define MAYBE_ReceiveStream DISABLED_ReceiveStream
 IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, MAYBE_ReceiveStream) {
   ASSERT_TRUE(embedded_test_server()->Start());
@@ -289,7 +295,7 @@ IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, MAYBE_ReceiveStream) {
   ASSERT_TRUE(ExecuteScript(
       shell(), base::StringPrintf(R"JS(
     async function run() {
-      const transport = new QuicTransport('quic-transport://localhost:%d/echo');
+      const transport = new QuicTransport('https://localhost:%d/echo');
 
       await transport.ready;
 
@@ -339,7 +345,7 @@ IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, BidirectionalStream) {
   ASSERT_TRUE(ExecuteScript(
       shell(), base::StringPrintf(R"JS(
     async function run() {
-      const transport = new QuicTransport('quic-transport://localhost:%d/echo');
+      const transport = new WebTransport('https://localhost:%d/echo');
 
       await transport.ready;
 
@@ -387,8 +393,8 @@ IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, CertificateFingerprint) {
       shell(), base::StringPrintf(R"JS(
     async function run() {
       // The connection fails because the fingerprint does not match.
-      const transport = new QuicTransport(
-          'quic-transport://localhost:%d/echo', {
+      const transport = new WebTransport(
+          'https://localhost:%d/echo', {
             serverCertificateFingerprints: [
               {
                 algorithm: "sha-256",
@@ -434,8 +440,57 @@ IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest, ReceiveBidirectionalStream) {
   ASSERT_TRUE(ExecuteScript(
       shell(), base::StringPrintf(R"JS(
     async function run() {
+      const transport = new WebTransport(
+        'https://localhost:%d/echo');
+
+      await transport.ready;
+
+      const streams = transport.incomingBidirectionalStreams;
+      const reader = streams.getReader();
+      const {value, done} = await reader.read();
+      if (done) {
+        throw new Error('bidirectional streams should not be closed');
+      }
+      await testBidiStream(value);
+    }
+
+    async function testBidiStream(bidiStream) {
+      // Consume the initial "hello" that is sent by the server.
+      const writer = bidiStream.writable.getWriter();
+      const reader = bidiStream.readable.getReader();
+      await writer.write(new TextEncoder().encode('hello'));
+      const {value: valueAsBinary, done: done0} = await reader.read();
+      if (done0) {
+        throw new Error('at least one read should happen');
+      }
+      const valueAsString = new TextDecoder().decode(valueAsBinary);
+      if (valueAsString !== 'hello') {
+        throw new Error(`expected 'hello', got '${valueAsString}'`);
+      }
+    }
+
+    run().then(() => { document.title = 'PASS'; },
+               (e) => { console.log(e); document.title = 'FAIL'; });
+)JS",
+                                  server_.server_address().port())));
+
+  ASSERT_TRUE(WaitForTitle(u"PASS", {u"FAIL"}));
+}
+
+// TODO(vasilvv): re-add /receive-bidirectional and re-enable the test.
+IN_PROC_BROWSER_TEST_F(QuicTransportBrowserTest,
+                       DISABLED_ReceiveBidirectionalStreamOld) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  ASSERT_TRUE(
+      NavigateToURL(shell(), embedded_test_server()->GetURL("/title2.html")));
+
+  ASSERT_TRUE(WaitForTitle(u"Title Of Awesomeness"));
+
+  ASSERT_TRUE(ExecuteScript(
+      shell(), base::StringPrintf(R"JS(
+    async function run() {
       const transport = new QuicTransport(
-        'quic-transport://localhost:%d/receive-bidirectional');
+        'https://localhost:%d/receive-bidirectional');
 
       await transport.ready;
 
--- net/quic/web_transport_client.cc
+++ net/quic/web_transport_client.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/quic/web_transport_client.h"
 
+#include "net/quic/dedicated_web_transport_http3_client.h"
 #include "net/quic/quic_transport_client.h"
 
 namespace net {
@@ -20,7 +21,7 @@ class FailedWebTransportClient : public WebTransportClient {
                ErrorToString(net_error),
                /*safe_to_report_details=*/true),
         visitor_(visitor) {}
-  void Connect() override { visitor_->OnError(); }
+  void Connect() override { visitor_->OnConnectionFailed(); }
 
   quic::WebTransportSession* session() override { return nullptr; }
   const QuicTransportError& error() const override { return error_; }
@@ -53,6 +54,14 @@ std::unique_ptr<WebTransportClient> CreateWebTransportClient(
           ERR_DISALLOWED_URL_SCHEME, visitor);
     }
     return std::make_unique<QuicTransportClient>(
+        url, origin, visitor, isolation_key, context, parameters);
+  }
+  if (url.scheme() == url::kHttpsScheme) {
+    if (!parameters.enable_web_transport_http3) {
+      return std::make_unique<FailedWebTransportClient>(
+          ERR_DISALLOWED_URL_SCHEME, visitor);
+    }
+    return std::make_unique<DedicatedWebTransportHttp3Client>(
         url, origin, visitor, isolation_key, context, parameters);
   }
 
