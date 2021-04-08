d8e55fc9fcee599e4d04aa41b8b968552545dc67
Sylvain Defresne <sdefresne@chromium.org>
2021-04-08 17:46:23 +0000

    [ios] Fix crashes when opening a tab in background
    When opening a tab in background, UrlLoadingBrowserAgent requests
    its delegate to play an animation and schedule a block to complete
    the WebState creation.
    
    The block scheduled captured `this` implicitly which is unsafe as
    UrlLoadingBrowserAgent is a C++ class thus not reference-counted.
    If the object is destroyed while the animation is playing, then
    the block would try to dereference a dangling pointer leading to
    undefined behaviour (due to use-after-free).
    
    Refactor the code to move the WebState creation and insertion to
    an helper method and use a WeakPtr<UrlLoadingBrowserAgent> to get
    access to the UrlLoadingBrowserAgent* in the block. The method is
    passed a base::Optional<void*> to check whether the active tab
    has changed while the animation was playing out, and if it has,
    the opener-opened relationship is not created (to reproduce the
    old behaviour, though it is a bit broken).
    
    Bug: 1195826
    Change-Id: Ibf61f2c815741162ccc100e3f2621b8d71ae5e06
    Fixed: 1195826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814660
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870602}

--- ios/chrome/browser/url_loading/url_loading_browser_agent.h
+++ ios/chrome/browser/url_loading/url_loading_browser_agent.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#include "base/memory/weak_ptr.h"
 #import "ios/chrome/browser/main/browser_user_data.h"
 
 class SceneUrlLoadingService;
@@ -60,11 +61,21 @@ class UrlLoadingBrowserAgent : public BrowserUserData<UrlLoadingBrowserAgent> {
   // Loads a url based on |params| in a new tab.
   virtual void LoadUrlInNewTab(const UrlLoadParams& params);
 
+  // Helper function implementing the creation and insertion of the new tab
+  // for LoadUrlInNewTab(). It is split to a separate function as it can be
+  // called asynchronously if the tab is opened in a background (and moving
+  // it to a separate function makes it safer not to capture state that can
+  // become invalid when creating the asynchronous task).
+  void LoadUrlInNewTabImpl(const UrlLoadParams& params,
+                           base::Optional<void*> hint);
+
   __weak id<URLLoadingDelegate> delegate_;
   Browser* browser_;
   UrlLoadingNotifierBrowserAgent* notifier_ = nullptr;
   UrlLoadingBrowserAgent* incognito_loader_ = nullptr;
   SceneUrlLoadingService* scene_service_ = nullptr;
+
+  base::WeakPtrFactory<UrlLoadingBrowserAgent> weak_ptr_factory_{this};
 };
 
 #endif  // IOS_CHROME_BROWSER_URL_LOADING_URL_LOADING_BROWSER_AGENT_H_
