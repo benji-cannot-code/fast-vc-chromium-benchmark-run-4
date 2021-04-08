44a9a3ff03d4a3e00ed91c9efb09bb8788c7dd40
Muyao Xu <muyaoxu@google.com>
2021-04-08 16:35:54 +0000

    [Zenith] Show non-mirroring sessions from in the Zenith dialog
    This CL shows all media routes except tab/desktop/file mirroring in
    the Zenith dialog. Some routes do not have a generic controller (i.e.
    DIAL and site-initiated mirroring routes) and the cast notifications
    will not show the media controllers UI.
    
    If there are only non-mirroring sessions, the Cast toolbar button will
    be hidden.
    
    Bug: b/182924319, 1196337
    Change-Id: I27eb24d34b32279e1c08b7593e9b84fcbb73c38a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2807942
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870566}

--- chrome/browser/ui/global_media_controls/cast_media_notification_producer.cc
+++ chrome/browser/ui/global_media_controls/cast_media_notification_producer.cc
@@ -14,10 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 bool ShouldHideNotification(const media_router::MediaRoute& route) {
-  if (!route.for_display() ||
-      route.controller_type() != media_router::RouteControllerType::kGeneric) {
+  // TODO(crbug.com/1195382): Display multizone group route.
+  // Hide a route if it's not for display or it's a mirroring route.
+  if (!route.for_display() || route.media_source().IsTabMirroringSource() ||
+      route.media_source().IsDesktopMirroringSource() ||
+      route.media_source().IsLocalFileSource()) {
     return true;
   }
+
   if (!route.media_source().IsCastPresentationUrl()) {
     return false;
   }
