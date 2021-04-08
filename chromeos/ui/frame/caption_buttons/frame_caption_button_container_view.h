30e06b722cefed2a854492f5107faf5102c050e3
Allen Bauer <kylixrd@chromium.org>
2021-04-08 00:05:16 +0000

    Inherit from FrameCaptionButtonContainerView from BoxLayoutView.
    Bug: 1130082
    Change-Id: I830b581c1c3a599e34bb571d42816bc4397661b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2808752
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870285}

--- chromeos/ui/frame/caption_buttons/frame_caption_button_container_view.h
+++ chromeos/ui/frame/caption_buttons/frame_caption_button_container_view.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ui/frame/caption_buttons/frame_size_button_delegate.h"
 #include "chromeos/ui/frame/caption_buttons/snap_controller.h"
 #include "ui/views/animation/animation_delegate_views.h"
+#include "ui/views/layout/box_layout_view.h"
 #include "ui/views/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
 #include "ui/views/window/frame_caption_button.h"
@@ -36,7 +37,7 @@ namespace chromeos {
 // is in //ash because it needs ash test support (AshTestBase and its
 // utilities).
 class COMPONENT_EXPORT(CHROMEOS_UI_FRAME) FrameCaptionButtonContainerView
-    : public views::View,
+    : public views::BoxLayoutView,
       public FrameSizeButtonDelegate,
       public views::AnimationDelegateViews {
  public:
