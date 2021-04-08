6af0eef458e4fcf8bfa6d0239dc428a7c49838b6
Alice Wang <aliceywang@chromium.org>
2021-04-08 07:01:24 +0000

    [Signin][Android] Use seeded account id in SyncConsentFragment
    This CL uses the seeded account id directly to record sync consent
    instead of fetching it again via AccountManagerFacade.
    
    Bug: 831257
    Change-Id: I1fe2f8e1c752795423f4488c0182250ccac9d34e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2810760
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870399}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SyncConsentFragmentBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SyncConsentFragmentBase.java
@@ -29,7 +29,6 @@ import androidx.fragment.app.FragmentTransaction;
 import org.chromium.base.Log;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
-import org.chromium.base.task.AsyncTask;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.consent_auditor.ConsentAuditorFeature;
 import org.chromium.chrome.browser.preferences.Pref;
@@ -56,7 +55,8 @@ import org.chromium.components.signin.AccountsChangeObserver;
 import org.chromium.components.signin.ChildAccountStatus;
 import org.chromium.components.signin.GmsAvailabilityException;
 import org.chromium.components.signin.GmsJustUpdatedException;
-import org.chromium.components.signin.base.CoreAccountId;
+import org.chromium.components.signin.base.CoreAccountInfo;
+import org.chromium.components.signin.identitymanager.AccountInfoService;
 import org.chromium.components.signin.metrics.SigninAccessPoint;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.ui.text.NoUnderlineClickableSpan;
@@ -387,10 +387,7 @@ public abstract class SyncConsentFragmentBase
         mIsSigninInProgress = true;
         mRecordUndoSignin = false;
         RecordUserAction.record("Signin_Signin_WithDefaultSyncSettings");
-
-        // Record the fact that the user consented to the consent text by clicking on a button
-        recordConsent((TextView) button);
-        seedAccountsAndSignin(false);
+        seedAccountsAndSignin(false, button);
     }
 
     private void onAddAccountButtonClicked(View button) {
@@ -402,10 +399,7 @@ public abstract class SyncConsentFragmentBase
         if (!areControlsEnabled()) return;
         mIsSigninInProgress = true;
         RecordUserAction.record("Signin_Signin_WithAdvancedSyncSettings");
-
-        // Record the fact that the user consented to the consent text by clicking on a link
-        recordConsent((TextView) view);
-        seedAccountsAndSignin(true);
+        seedAccountsAndSignin(true, view);
     }
 
     /**
@@ -418,13 +412,18 @@ public abstract class SyncConsentFragmentBase
         return !mAccountSelectionPending && !mIsSigninInProgress && !mHasGmsError;
     }
 
-    private void seedAccountsAndSignin(boolean settingsClicked) {
+    private void seedAccountsAndSignin(boolean settingsClicked, View confirmationView) {
         // Ensure that the AccountTrackerService has a fully up to date GAIA id <-> email mapping,
         // as this is needed for the previous account check.
         final long seedingStartTime = SystemClock.elapsedRealtime();
         IdentityServicesProvider.get()
                 .getAccountTrackerService(Profile.getLastUsedRegularProfile())
                 .seedAccountsIfNeeded(() -> {
+                    final CoreAccountInfo accountInfo =
+                            AccountInfoService.get().getAccountInfoByEmail(mSelectedAccountName);
+                    assert accountInfo != null : "The seeded CoreAccountInfo shouldn't be null";
+                    mConsentTextTracker.recordConsent(accountInfo.getId(),
+                            ConsentAuditorFeature.CHROME_SYNC, (TextView) confirmationView, mView);
                     RecordHistogram.recordTimesHistogram(
                             "Signin.AndroidAccountSigninViewSeedingTime",
                             SystemClock.elapsedRealtime() - seedingStartTime);
@@ -458,26 +457,6 @@ public abstract class SyncConsentFragmentBase
                         mIsSigninInProgress = false;
                     }
                 });
-    }
-
-    /**
-     * Records the Sync consent.
-     * @param confirmationView The view that the user clicked when consenting.
-     */
-    private void recordConsent(TextView confirmationView) {
-        // TODO(crbug.com/831257): Provide the account id synchronously from AccountManagerFacade.
-        new AsyncTask<String>() {
-            @Override
-            public String doInBackground() {
-                return mAccountManagerFacade.getAccountGaiaId(mSelectedAccountName);
-            }
-
-            @Override
-            public void onPostExecute(String accountId) {
-                mConsentTextTracker.recordConsent(new CoreAccountId(accountId),
-                        ConsentAuditorFeature.CHROME_SYNC, confirmationView, mView);
-            }
-        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     private void showAccountPicker() {
