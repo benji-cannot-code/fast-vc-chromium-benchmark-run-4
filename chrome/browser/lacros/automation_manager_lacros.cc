95e5b53b63426239d38fa4036bb28aca622d6075
David Tseng <dtseng@google.com>
2021-04-08 20:57:27 +0000

    Add AutomationFactory to crosapi
    AutomationFactory makes clear the relationship of Lacros to Ash when
    using the automation api by binding remote and receiver in one
    BindAutomationTreeSource method.
    
    Bug: 1185764
    Test: manual
    
    Change-Id: Ie1a69907a6ca864b9d858a6529c73e3506a47a0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2810221
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870695}

--- chrome/browser/lacros/automation_manager_lacros.cc
+++ chrome/browser/lacros/automation_manager_lacros.cc
@@ -16,11 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 AutomationManagerLacros::AutomationManagerLacros() {
   chromeos::LacrosChromeServiceImpl* impl =
       chromeos::LacrosChromeServiceImpl::Get();
-  if (!impl->IsAvailable<crosapi::mojom::Automation>())
+  if (!impl->IsAvailable<crosapi::mojom::AutomationFactory>())
     return;
-  id_ = base::UnguessableToken::Create();
-  impl->GetRemote<crosapi::mojom::Automation>()->RegisterAutomationClient(
-      receiver_.BindNewPipeAndPassRemote(), id_);
+
+  impl->GetRemote<crosapi::mojom::AutomationFactory>()->BindAutomation(
+      automation_client_receiver_.BindNewPipeAndPassRemote(),
+      automation_remote_.BindNewPipeAndPassReceiver());
 
   extensions::AutomationEventRouter::GetInstance()->RegisterRemoteRouter(this);
 }
@@ -28,7 +29,7 @@ AutomationManagerLacros::AutomationManagerLacros() {
 AutomationManagerLacros::~AutomationManagerLacros() {
   chromeos::LacrosChromeServiceImpl* impl =
       chromeos::LacrosChromeServiceImpl::Get();
-  if (!impl->IsAvailable<crosapi::mojom::Automation>())
+  if (!impl->IsAvailable<crosapi::mojom::AutomationFactory>())
     return;
 
   extensions::AutomationEventRouter::GetInstance()->RegisterRemoteRouter(
@@ -40,6 +41,9 @@ void AutomationManagerLacros::DispatchAccessibilityEvents(
     std::vector<ui::AXTreeUpdate> updates,
     const gfx::Point& mouse_location,
     std::vector<ui::AXEvent> events) {
+  if (!automation_remote_)
+    return;
+
   ExtensionMsg_AccessibilityEventBundleParams event_bundle;
   event_bundle.tree_id = tree_id;
   event_bundle.updates = std::move(updates);
@@ -49,9 +53,9 @@ void AutomationManagerLacros::DispatchAccessibilityEvents(
   IPC::ParamTraits<ExtensionMsg_AccessibilityEventBundleParams>::Write(
       &pickle, event_bundle);
   std::string result(static_cast<const char*>(pickle.data()), pickle.size());
-  chromeos::LacrosChromeServiceImpl::Get()
-      ->GetRemote<crosapi::mojom::Automation>()
-      ->ReceiveEventPrototype(std::move(result), false, id_, std::string());
+  automation_remote_->ReceiveEventPrototype(std::move(result), false,
+                                            base::UnguessableToken::Create(),
+                                            std::string());
 }
 
 void AutomationManagerLacros::DispatchAccessibilityLocationChange(
@@ -65,9 +69,10 @@ void AutomationManagerLacros::DispatchTreeDestroyedEvent(
   if (!tree_id.token())
     return;
 
-  chromeos::LacrosChromeServiceImpl::Get()
-      ->GetRemote<crosapi::mojom::Automation>()
-      ->DispatchTreeDestroyedEvent(*(tree_id.token()));
+  if (!automation_remote_)
+    return;
+
+  automation_remote_->DispatchTreeDestroyedEvent(*(tree_id.token()));
 }
 
 void AutomationManagerLacros::DispatchActionResult(
