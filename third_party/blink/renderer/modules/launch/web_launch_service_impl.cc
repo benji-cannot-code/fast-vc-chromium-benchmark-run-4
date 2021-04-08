af25db916c410a804eaf1b07ca3b6364c7c19d64
Darwin Huang <huangdarwin@chromium.org>
2021-04-08 00:48:56 +0000

    File Handling: Add use counter to measure usage.
    This is especially important in origin trials, to ensure folks are
    actually using the API (but also perhaps that not too many people
    are using an experimental API).
    
    This is also therefore required for origin trial:
    https://chromium.googlesource.com/chromium/src/+/master/docs/origin_trials_integration.md#web-feature-counting
    
    Bug: 829689
    Change-Id: I65f308da6bba3c770391d42d52b5697c08528380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2808680
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870304}

--- third_party/blink/renderer/modules/launch/web_launch_service_impl.cc
+++ third_party/blink/renderer/modules/launch/web_launch_service_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/modules/launch/dom_window_launch_queue.h"
+#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 
 namespace blink {
 // static
@@ -62,6 +63,8 @@ void WebLaunchServiceImpl::SetLaunchFiles(
         std::move(entry), GetSupplementable()->GetExecutionContext()));
   }
 
+  UseCounter::Count(GetSupplementable()->GetExecutionContext(),
+                    WebFeature::kFileHandlingLaunch);
   DOMWindowLaunchQueue::UpdateLaunchFiles(GetSupplementable(),
                                           std::move(files));
 }
