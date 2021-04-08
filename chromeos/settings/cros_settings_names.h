c49aa1c189cb4e859324d01508dd1522753e0b37
Yun-Hao Chung <howardchung@chromium.org>
2021-04-08 18:06:50 +0000

    Reland "Add chrome policy DeviceAllowedBluetoothServices"
    This is a reland of a704ab0f9203edfb3bde6a2c267335495ce91af9. Original
    CL had an issue where the policy description had mismatched quotes.
    
    Original change's description:
    > Add chrome policy DeviceAllowedBluetoothServices
    >
    > This adds the policy definition and the handler for
    > DeviceAllowedBluetoothServices. Unit tests for device_settings_provider
    > and policy_handler are also implemented.
    >
    > BUG=1182826
    > TEST=build chrome and unit_tests. Confirm newly added tests
    > are passed.
    >
    > Change-Id: I8aee99d4fbf524bc97d0ac387a32b1b5909dd425
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2793682
    > Commit-Queue: Yun-Hao Chung <howardchung@chromium.org>
    > Reviewed-by: Pavol Marko <pmarko@chromium.org>
    > Reviewed-by: Anqing Zhao <anqing@chromium.org>
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#870389}
    
    Bug: 1182826
    Change-Id: Id23d4cf860c513d5b9ca4f1b073fe58aa0319329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2815160
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Ben Mason <benmason@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870609}

--- chromeos/settings/cros_settings_names.h
+++ chromeos/settings/cros_settings_names.h
@@ -290,6 +290,9 @@ extern const char kUsbDetachableAllowlistKeyPid[];
 
 COMPONENT_EXPORT(CHROMEOS_SETTINGS)
 extern const char kDevicePeripheralDataAccessEnabled[];
+
+COMPONENT_EXPORT(CHROMEOS_SETTINGS)
+extern const char kDeviceAllowedBluetoothServices[];
 }  // namespace chromeos
 
 // TODO(https://crbug.com/1164001): remove when migrated to ash/components/.
@@ -325,6 +328,7 @@ using ::chromeos::kAttestationForContentProtectionEnabled;
 using ::chromeos::kBorealisAllowedForDevice;
 using ::chromeos::kCastReceiverName;
 using ::chromeos::kCrosSettingsPrefix;
+using ::chromeos::kDeviceAllowedBluetoothServices;
 using ::chromeos::kDeviceAttestationEnabled;
 using ::chromeos::kDeviceAutoUpdateTimeRestrictions;
 using ::chromeos::kDeviceChannelDowngradeBehavior;
