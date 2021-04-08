c6639e58fe6f92e44a06e8f6c61aea9dbf9d0c1f
Matthew Mourgos <mmourgos@chromium.org>
2021-04-08 22:06:33 +0000

    CrOS: Calculate notification badge color using a singleton instance
    Previously, calculating and caching the notification badge color based
    on the app id all relied on code in the ChromeLauncherController. This
    has problems when a badge color needs to be calculated and recorded
    before the ChromeLauncherController has been initialized.
    
    This change takes the notification badge color calculation/caching
    and moves it into a base::NoDestructor<> singleton instance. This
    allows the functionality to get initialized once it is needed, and
    it can still be used between the ChromeLauncherController and the
    ChromeAppListItem.
    
    Bug: 1132117
    Change-Id: Iaf2dcb8fbf676bc3fe93a3d02e053e00b43cbe60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2808679
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870724}

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/launcher/shelf_spinner_controller.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_window_manager_helper.h"
+#include "chrome/browser/ui/ash/notification_badge_color_cache.h"
 #include "chrome/browser/ui/ash/session_controller_client_impl.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -102,7 +103,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/display/types/display_constants.h"
-#include "ui/gfx/color_analysis.h"
 #include "ui/resources/grit/ui_resources.h"
 
 using extension_misc::kChromeAppId;
@@ -134,32 +134,6 @@ std::string GetCrostiniAppIdFromContents(content::WebContents* web_contents) {
       crostini::CrostiniAppIdFromAppName(browser->app_name());
   return app_id_opt.value_or("");
 }
-
-// Uses the icon image to calculate the light vibrant color to be used for
-// the notification indicator.
-base::Optional<SkColor> CalculateNotificationBadgeColor(gfx::ImageSkia image) {
-  const SkBitmap* source = image.bitmap();
-  if (!source || source->empty() || source->isNull())
-    return base::nullopt;
-
-  std::vector<color_utils::ColorProfile> color_profiles;
-  color_profiles.push_back(color_utils::ColorProfile(
-      color_utils::LumaRange::LIGHT, color_utils::SaturationRange::VIBRANT));
-
-  std::vector<color_utils::Swatch> best_swatches =
-      color_utils::CalculateProminentColorsOfBitmap(
-          *source, color_profiles, nullptr /* bitmap region */,
-          color_utils::ColorSwatchFilter());
-
-  // If the best swatch color is transparent, then
-  // CalculateProminentColorsOfBitmap() failed to find a suitable color.
-  if (best_swatches.empty() || best_swatches[0].color == SK_ColorTRANSPARENT)
-    return base::nullopt;
-
-  return best_swatches[0].color;
-}
-
-constexpr SkColor kDefaultIndicatorColor = SK_ColorWHITE;
 
 }  // namespace
 
@@ -495,7 +469,8 @@ void ChromeLauncherController::SetLauncherItemImage(
     ash::ShelfItem new_item = *item;
     new_item.image = image;
     new_item.notification_badge_color =
-        CalculateNotificationBadgeColorForApp(new_item.id.app_id, image);
+        ash::NotificationBadgeColorCache::GetInstance().GetBadgeColorForApp(
+            new_item.id.app_id, image);
     model_->Set(model_->ItemIndexByID(shelf_id), new_item);
   }
 }
@@ -915,19 +890,6 @@ void ChromeLauncherController::DoShowAppInfoFlow(Profile* profile,
   }
 }
 
-SkColor ChromeLauncherController::CalculateNotificationBadgeColorForApp(
-    const std::string& app_id,
-    const gfx::ImageSkia& icon) {
-  AppIdBadgeColor::const_iterator it = app_id_badge_color_map_.find(app_id);
-  if (it != app_id_badge_color_map_.end())
-    return it->second;
-
-  SkColor notification_color =
-      CalculateNotificationBadgeColor(icon).value_or(kDefaultIndicatorColor);
-  app_id_badge_color_map_[app_id] = notification_color;
-  return notification_color;
-}
-
 ///////////////////////////////////////////////////////////////////////////////
 // LauncherAppUpdater::Delegate:
 
@@ -1058,7 +1020,8 @@ void ChromeLauncherController::OnAppImageUpdated(const std::string& app_id,
         is_standard_icon ? image : apps::CreateStandardIconImage(image);
     shelf_spinner_controller_->MaybeApplySpinningEffect(app_id, &item.image);
     item.notification_badge_color =
-        CalculateNotificationBadgeColorForApp(app_id, image);
+        ash::NotificationBadgeColorCache::GetInstance().GetBadgeColorForApp(
+            app_id, image);
     model_->Set(index, item);
     // It's possible we're waiting on more than one item, so don't break.
   }
@@ -1340,8 +1303,8 @@ void ChromeLauncherController::CreateBrowserShortcutLauncherItem(bool pinned) {
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
   browser_shortcut.image = *rb.GetImageSkiaNamed(IDR_CHROME_APP_ICON_192);
   browser_shortcut.notification_badge_color =
-      CalculateNotificationBadgeColorForApp(kChromeAppId,
-                                            browser_shortcut.image);
+      ash::NotificationBadgeColorCache::GetInstance().GetBadgeColorForApp(
+          kChromeAppId, browser_shortcut.image);
   browser_shortcut.title = l10n_util::GetStringUTF16(IDS_PRODUCT_NAME);
   // Set the delegate first to avoid constructing another one in ShelfItemAdded.
   model_->SetShelfItemDelegate(
