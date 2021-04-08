88fc69d5425d356f7da8deaa6f02aec6d7a950c6
Tommy Steimel <steimel@chromium.org>
2021-04-08 18:38:36 +0000

    PiP 1.5: Label back to tab button with origin and center it
    This CL makes the back to tab button on the PiP window a LabelButton
    that contains the origin of the tab. It also moves the back to tab
    button to the center of the window.
    
    This also fixes a bug in the PiP window where the position of buttons
    was wrong when there were 3 buttons and the middle wasn't the play
    button.
    
    Bug: 1196066
    Change-Id: I5b036ad7b9a91e9220aa1dfa80505c202996f31b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2804366
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870626}

--- chrome/browser/ui/views/overlay/constants.h
+++ chrome/browser/ui/views/overlay/constants.h
@@ -10,4 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 constexpr SkColor kPipWindowIconColor = SK_ColorWHITE;
 
+constexpr SkColor kPipWindowTextColor = SK_ColorWHITE;
+
 #endif  // CHROME_BROWSER_UI_VIEWS_OVERLAY_CONSTANTS_H_
