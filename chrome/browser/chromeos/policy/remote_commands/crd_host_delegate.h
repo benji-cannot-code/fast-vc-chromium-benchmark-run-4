366c4f4435bb0c7727af6569087c5f481b525366
Jeroen Dhollander <jeroendh@google.com>
2021-04-08 11:12:45 +0000

    Cleanup CRDHostDelegate: Introduce OAuthTokenFetcher
    This new helper class combines all logic related to fetching the
    OAuth token, making the code easier to understand.
    
    Bug: b/183937151
    Test: unit_tests --gtest_filter="*CRC*" and deployed on real HW.
    Change-Id: Ie1a1ce9208f526bd100f84aa5f3a7b9775e8a446
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791381
    Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870456}

--- chrome/browser/chromeos/policy/remote_commands/crd_host_delegate.h
+++ chrome/browser/chromeos/policy/remote_commands/crd_host_delegate.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chromeos/policy/remote_commands/device_command_start_crd_session_job.h"
 #include "extensions/browser/api/messaging/native_message_host.h"
-#include "google_apis/gaia/oauth2_access_token_manager.h"
 
 class Profile;
 
@@ -23,14 +22,15 @@ namespace policy {
 
 // An implementation of the |DeviceCommandStartCRDSessionJob::Delegate|.
 class CRDHostDelegate : public DeviceCommandStartCRDSessionJob::Delegate,
-                        public OAuth2AccessTokenManager::Consumer,
                         public extensions::NativeMessageHost::Client {
  public:
   CRDHostDelegate();
   ~CRDHostDelegate() override;
 
  private:
-  // DeviceCommandScreenshotJob::Delegate:
+  class OAuthTokenFetcher;
+
+  // DeviceCommandStartCRDSessionJob::Delegate:
   bool HasActiveSession() const override;
   void TerminateSession(base::OnceClosure callback) override;
   bool AreServicesReady() const override;
@@ -44,13 +44,6 @@ class CRDHostDelegate : public DeviceCommandStartCRDSessionJob::Delegate,
       bool terminate_upon_input,
       DeviceCommandStartCRDSessionJob::AccessCodeCallback success_callback,
       DeviceCommandStartCRDSessionJob::ErrorCallback error_callback) override;
-
-  // OAuth2AccessTokenManager::Consumer:
-  void OnGetTokenSuccess(
-      const OAuth2AccessTokenManager::Request* request,
-      const OAuth2AccessTokenConsumer::TokenResponse& token_response) override;
-  void OnGetTokenFailure(const OAuth2AccessTokenManager::Request* request,
-                         const GoogleServiceAuthError& error) override;
 
   // extensions::NativeMessageHost::Client:
   // Invoked when native host sends a message
@@ -78,11 +71,11 @@ class CRDHostDelegate : public DeviceCommandStartCRDSessionJob::Delegate,
 
   Profile* GetKioskProfile() const;
 
-  DeviceCommandStartCRDSessionJob::OAuthTokenCallback oauth_success_callback_;
+  std::unique_ptr<OAuthTokenFetcher> oauth_token_fetcher_;
+
   DeviceCommandStartCRDSessionJob::AccessCodeCallback code_success_callback_;
   DeviceCommandStartCRDSessionJob::ErrorCallback error_callback_;
 
-  std::unique_ptr<OAuth2AccessTokenManager::Request> oauth_request_;
   std::unique_ptr<extensions::NativeMessageHost> host_;
 
   // Filled structure with parameters for "connect" message.
