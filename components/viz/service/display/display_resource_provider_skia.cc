7002da046f4d2efb978cb7f69668c05439bb0ad7
Vasiliy Telezhnikov <vasilyt@chromium.org>
2021-04-08 14:59:14 +0000

    Remove DidLoseContextProvider from DRP
    After display compositor was moved to gpu process it's no longer
    possible to lose context provider and the code is not used.
    
    Bug: 1176023
    Change-Id: If3ffea85c587a62c02c1d87e2bf73bfcd31fbc71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2811298
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870518}

--- components/viz/service/display/display_resource_provider_skia.cc
+++ components/viz/service/display/display_resource_provider_skia.cc
@@ -61,7 +61,6 @@ DisplayResourceProviderSkia::DeleteAndReturnUnusedResourcesToChildImpl(
     DCHECK(child_info.child_to_parent_map.count(child_id));
     DCHECK(resource.is_gpu_resource_type());
 
-    bool is_lost = lost_context_provider_;
     auto can_delete = CanDeleteNow(child_info, resource, style);
     if (can_delete == CanDeleteNowResult::kNo) {
       // Defer this resource deletion.
@@ -69,7 +68,7 @@ DisplayResourceProviderSkia::DeleteAndReturnUnusedResourcesToChildImpl(
       continue;
     }
 
-    is_lost = is_lost || can_delete == CanDeleteNowResult::kYesButLoseResource;
+    const bool is_lost = can_delete == CanDeleteNowResult::kYesButLoseResource;
 
     to_return.emplace_back(child_id, resource.sync_token(),
                            resource.imported_count, is_lost);
