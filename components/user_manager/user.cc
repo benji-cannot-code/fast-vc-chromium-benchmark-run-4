ddc5619f8c2ea4df0831f84c2067d9953083a97c
Toby Huang <tobyhuang@chromium.org>
2021-04-08 15:23:11 +0000

    LSU: Remove CreateSupervisedUser() and SupervisedUser class
    Remove CreateSupervisedUser() because it is not referenced anywhere,
    and remove the SupervisedUser class because it refers to deprecated
    legacy supervised users.
    
    Bug: 1155729
    Change-Id: I0f0bddaeeb5da881d8e77b84d0a37a3993532e14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2810752
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870527}

--- components/user_manager/user.cc
+++ components/user_manager/user.cc
@@ -144,19 +144,6 @@ class WebKioskAppUser : public DeviceLocalAccountUserBase {
   DISALLOW_COPY_AND_ASSIGN(WebKioskAppUser);
 };
 
-class SupervisedUser : public User {
- public:
-  explicit SupervisedUser(const AccountId& account_id);
-  ~SupervisedUser() override;
-
-  // Overridden from User:
-  UserType GetType() const override;
-  std::string display_email() const override;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(SupervisedUser);
-};
-
 class PublicAccountUser : public DeviceLocalAccountUserBase {
  public:
   explicit PublicAccountUser(const AccountId& account_id);
@@ -341,10 +328,6 @@ User* User::CreateWebKioskAppUser(const AccountId& web_kiosk_account_id) {
   return new WebKioskAppUser(web_kiosk_account_id);
 }
 
-User* User::CreateSupervisedUser(const AccountId& account_id) {
-  return new SupervisedUser(account_id);
-}
-
 User* User::CreatePublicAccountUser(const AccountId& account_id,
                                     bool is_using_saml) {
   User* user = new PublicAccountUser(account_id);
@@ -508,21 +491,6 @@ WebKioskAppUser::~WebKioskAppUser() {}
 
 UserType WebKioskAppUser::GetType() const {
   return user_manager::USER_TYPE_WEB_KIOSK_APP;
-}
-
-SupervisedUser::SupervisedUser(const AccountId& account_id) : User(account_id) {
-  set_can_lock(true);
-}
-
-SupervisedUser::~SupervisedUser() {
-}
-
-UserType SupervisedUser::GetType() const {
-  return user_manager::USER_TYPE_SUPERVISED_DEPRECATED;
-}
-
-std::string SupervisedUser::display_email() const {
-  return base::UTF16ToUTF8(display_name());
 }
 
 PublicAccountUser::PublicAccountUser(const AccountId& account_id)
