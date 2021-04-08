4d0c3f75b7c552dfa543f2521f67eb84a85b3070
rbpotter <rbpotter@chromium.org>
2021-04-08 19:27:40 +0000

    Crashes UI: Update to JS modules
    Also fixing some WebUI style issues:
    - Use addWebUIListener instead of relying on global JS functions
    - Class names should use dashes, not camelCase
    - Removing flattenhtml, registering CSS file and SVG on the data source
      directly.
    
    Bug: 1173575
    Change-Id: Ib1bf1e924eba7cca2c64e454794b1926b1d44ffd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2812686
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870648}

--- components/crash/core/browser/resources/crashes.js
+++ components/crash/core/browser/resources/crashes.js
@@ -3,6 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// <if expr="is_ios">
+import 'chrome://resources/js/ios/web_ui.js';
+// </if>
+
+import 'chrome://resources/js/action_link.js';
+import './strings.m.js';
+import {addWebUIListener} from 'chrome://resources/js/cr.m.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+import {$} from 'chrome://resources/js/util.m.js';
+
 /* Id for tracking automatic refresh of crash list.  */
 let refreshCrashListId = undefined;
 
@@ -15,17 +25,24 @@ function requestCrashes() {
 
 /**
  * Callback from backend with the list of crashes. Builds the UI.
- * @param {boolean} enabled Whether or not crash reporting is enabled.
- * @param {boolean} dynamicBackend Whether the crash backend is dynamic.
- * @param {boolean} manualUploads Whether the manual uploads are supported.
- * @param {array} crashes The list of crashes.
- * @param {string} version The browser version.
- * @param {string} os The OS name and version.
- * @param {boolean} isGoogleAccount whether primary account is internal.
+ * @param {!{enabled: boolean,
+ *           dynamicBackend: boolean,
+ *           manualUploads: boolean,
+ *           crashes: !Array,
+ *           version: string,
+ *           os: string,
+ *           isGoogleAccount: boolean,
+ *           }} result
  */
-function updateCrashList(
-    enabled, dynamicBackend, manualUploads,
-    crashes, version, os, isGoogleAccount) {
+function updateCrashList({
+  enabled,
+  dynamicBackend,
+  manualUploads,
+  crashes,
+  version,
+  os,
+  isGoogleAccount
+}) {
   $('crashesCount').textContent = loadTimeData.getStringF(
       'crashCountFormat', crashes.length.toLocaleString());
 
@@ -37,7 +54,7 @@ function updateCrashList(
   const template = crashList.getElementsByTagName('template')[0];
 
   // Clear any previous list.
-  crashList.querySelectorAll('.crashRow').forEach((elm) => elm.remove());
+  crashList.querySelectorAll('.crash-row').forEach((elm) => elm.remove());
 
   const productName = loadTimeData.getString('shortProductName');
 
@@ -49,18 +66,18 @@ function updateCrashList(
 
     const crashRow = template.content.cloneNode(true);
     if (crash.state !== 'uploaded') {
-      crashRow.querySelector('.crashRow').classList.add('notUploaded');
+      crashRow.querySelector('.crash-row').classList.add('not-uploaded');
     }
 
     const uploaded = crash.state === 'uploaded';
 
     // Some clients do not distinguish between capture time and upload time,
     // so use the latter if the former is not available.
-    crashRow.querySelector('.captureTime').textContent =
+    crashRow.querySelector('.capture-time').textContent =
         loadTimeData.getStringF(
             'crashCaptureTimeFormat',
             crash.capture_time || crash.upload_time || '');
-    crashRow.querySelector('.localId .value').textContent = crash.local_id;
+    crashRow.querySelector('.local-id .value').textContent = crash.local_id;
 
     let stateText = '';
     switch (crash.state) {
@@ -81,10 +98,10 @@ function updateCrashList(
     }
     crashRow.querySelector('.status .value').textContent = stateText;
 
-    const uploadId = crashRow.querySelector('.uploadId');
-    const uploadTime = crashRow.querySelector('.uploadTime');
-    const sendNowButton = crashRow.querySelector('.sendNow');
-    const fileBugButton = crashRow.querySelector('.fileBug');
+    const uploadId = crashRow.querySelector('.upload-id');
+    const uploadTime = crashRow.querySelector('.upload-time');
+    const sendNowButton = crashRow.querySelector('.send-now');
+    const fileBugButton = crashRow.querySelector('.file-bug');
     if (uploaded) {
       const uploadIdValue = uploadId.querySelector('.value');
       if (isGoogleAccount) {
@@ -116,7 +133,7 @@ function updateCrashList(
       };
     }
 
-    const fileSize = crashRow.querySelector('.fileSize');
+    const fileSize = crashRow.querySelector('.file-size');
     if (crash.file_size === '') {
       fileSize.remove();
     } else {
@@ -191,10 +208,11 @@ function requestCrashUpload() {
  * @param {Event} The DOM event for onclick.
  */
 function toggleDevDetails(e) {
-  $('crashList').classList.toggle('showingDevDetails', e.target.checked);
+  $('crashList').classList.toggle('showing-dev-details', e.target.checked);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  addWebUIListener('update-crash-list', updateCrashList);
   $('uploadCrashes').onclick = requestCrashUpload;
   $('showDevDetails').onclick = toggleDevDetails;
   requestCrashes();
