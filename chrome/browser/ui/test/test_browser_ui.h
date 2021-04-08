06dd6f595af46e6e5e4299087a1e57a94239919d
Bret Sepulveda <bsep@chromium.org>
2021-04-08 12:22:02 +0000

    Revert "Enabled pixel tests for permission chip"
    This reverts commit ba9bdd73d1e1a94e347e5c99375fa557631aee4a.
    
    Reason for revert: Multiple permissions-related browser tests failing on MSAN, see for example https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/27998/overview
    
    Original change's description:
    > Enabled pixel tests for permission chip
    >
    > TestBrowserUi supports verifying pixel UI for dialogs (windows) only.
    > In this CL I added method that can verify UI for a view
    > (BrowserSkiaGoldPixelDiff supports taking screenshots and comparing
    > views). I also used this new methods in browser test for permission chip
    > button in the omnibox.
    >
    > To ensure that correct screenshot is taken each time (with chip expanded
    > and both icon and text shown), I added logic to force expand the chip
    > for tests.
    >
    > Also, I added PermissionPromptBubbleViewBrowserTest to the filter file,
    > it wasn't there before and pixel comparison wasn't performed for it.
    >
    > Change-Id: I4c08f6bd5d98ea0c9f645f51fd2290f50e718806
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780669
    > Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    > Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    > Cr-Commit-Position: refs/heads/master@{#870420}
    
    Change-Id: Iaacfbb188211928f0ffd9801136644fbd4f69ac7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814603
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Owners-Override: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870479}

--- chrome/browser/ui/test/test_browser_ui.h
+++ chrome/browser/ui/test/test_browser_ui.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 class Widget;
-class View;
 }  // namespace views
 
 namespace ui {
@@ -99,11 +98,6 @@ class TestBrowserUi {
 #if defined(OS_WIN) || (defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS))
   // Can be called by VerifyUi() to ensure pixel correctness.
   bool VerifyPixelUi(views::Widget* widget,
-                     const std::string& screenshot_prefix,
-                     const std::string& screenshot_name);
-
-  // Can be called by VerifyUi() to ensure pixel correctness.
-  bool VerifyPixelUi(views::View* view,
                      const std::string& screenshot_prefix,
                      const std::string& screenshot_name);
 
