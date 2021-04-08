ff45e5dd52602875c6a6b5a5bf451786bdfe11a6
wutao <wutao@chromium.org>
2021-04-08 21:33:00 +0000

    assistant: Add web view top padding
    Adding top padding to Assistant Settings so that the avatar doesn't look
    cropped in overview mode.
    
    Bug: b/148406521
    Test: manual
    Change-Id: Ib8dcb4bfc860eeb04d73ecdbc8ef0cee863fd8c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2812684
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870708}

--- ash/assistant/ui/assistant_web_container_view.cc
+++ ash/assistant/ui/assistant_web_container_view.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
 #include "ui/views/background.h"
+#include "ui/views/border.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/metadata/metadata_impl_macros.h"
 #include "ui/views/window/caption_button_layout_constants.h"
@@ -82,6 +83,9 @@ void AssistantWebContainerView::DidStopLoading() {
 
   contents_view_->SetPreferredSize(GetPreferredSize());
   contents_view_ptr_ = AddChildView(std::move(contents_view_));
+  constexpr int kTopPaddingDip = 8;
+  contents_view_ptr_->SetBorder(
+      views::CreateEmptyBorder(kTopPaddingDip, 0, 0, 0));
   SetFocusBehavior(FocusBehavior::ALWAYS);
 }
 
