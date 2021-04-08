ecc1d2375afb97d05a8b30902e3a1d6464536eae
Anatoliy Potapchuk <apotapchuk@chromium.org>
2021-04-08 21:28:01 +0000

    Update WPT tests for Managed Configuration API
    To ensure that tests pass on other platforms, we remove testharness.js and
    testharnessreport.js .
    
    Bug: 1141849
    Change-Id: I5815821dc4bcc3fb353f9811842b8f875a336c62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2815304
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870706}

--- third_party/blink/web_tests/external/wpt/managed/managed-config-error.https.window.js
+++ third_party/blink/web_tests/external/wpt/managed/managed-config-error.https.window.js
@@ -1,7 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META: script=/resources/testharness.js
-// META: script=/resources/testharnessreport.js
-
 promise_test(t => {
   return promise_rejects_js(
       t, TypeError, navigator.managed.getManagedConfiguration(-1));
