a540e9fc75626657ed73d79079c4818962092df3
Evan Stade <estade@chromium.org>
2021-04-08 20:06:26 +0000

    [Code health] Remove NOTIFICATION_BROWSER_OPENED
    ... and some other notifications that were used in
    AppShimTerminationManagerImpl.
    
    Bug: 1174784,1174777,1174776
    Change-Id: Id27c63bae389f758d1b4f08ff6364ed045abdd6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2815161
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870668}

--- chrome/browser/profiles/profile_manager.cc
+++ chrome/browser/profiles/profile_manager.cc
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/download_core_service.h"
 #include "chrome/browser/download/download_core_service_factory.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
-#include "chrome/browser/lifetime/browser_shutdown.h"
 #include "chrome/browser/lite_video/lite_video_keyed_service.h"
 #include "chrome/browser/lite_video/lite_video_keyed_service_factory.h"
 #include "chrome/browser/navigation_predictor/navigation_predictor_keyed_service_factory.h"
