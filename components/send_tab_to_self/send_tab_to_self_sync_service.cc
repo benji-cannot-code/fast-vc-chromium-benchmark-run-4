779e649099913fb249c2b857684a159d5c47729e
Elly Fong-Jones <ellyjones@chromium.org>
2021-04-08 22:07:32 +0000

    stts: add SendTabToSelfUseFakeBackend feature
    This feature causes STTS to use a fake model, which uses hardcoded
    test data instead of data from the sync feature; this allows
    developing STTS-related UI without configuring sync or signin or
    meeting the various other prerequisites.
    
    Bug: None
    Change-Id: Ie08de8036dbb0117e4e8714f8515950158bf0257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2815946
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Travis Skare <skare@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870725}

--- components/send_tab_to_self/send_tab_to_self_sync_service.cc
+++ components/send_tab_to_self/send_tab_to_self_sync_service.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/time/default_clock.h"
 #include "components/history/core/browser/history_service.h"
+#include "components/send_tab_to_self/fake_send_tab_to_self_model.h"
+#include "components/send_tab_to_self/features.h"
 #include "components/send_tab_to_self/send_tab_to_self_bridge.h"
 #include "components/send_tab_to_self/send_tab_to_self_model.h"
 #include "components/sync/base/report_unrecoverable_error.h"
@@ -26,6 +28,12 @@ SendTabToSelfSyncService::SendTabToSelfSyncService(
     syncer::OnceModelTypeStoreFactory create_store_callback,
     history::HistoryService* history_service,
     syncer::DeviceInfoTracker* device_info_tracker) {
+  if (base::FeatureList::IsEnabled(kSendTabToSelfUseFakeBackend))
+    fake_model_ = std::make_unique<FakeSendTabToSelfModel>();
+
+  // Even when using a fake model, it's still necessary to construct a real
+  // bridge here, so that all the sync helper objects (the ModelTypeSyncBridge
+  // and friends) are created.
   bridge_ = std::make_unique<send_tab_to_self::SendTabToSelfBridge>(
       std::make_unique<syncer::ClientTagBasedModelTypeProcessor>(
           syncer::SEND_TAB_TO_SELF,
@@ -37,7 +45,8 @@ SendTabToSelfSyncService::SendTabToSelfSyncService(
 SendTabToSelfSyncService::~SendTabToSelfSyncService() = default;
 
 SendTabToSelfModel* SendTabToSelfSyncService::GetSendTabToSelfModel() {
-  return bridge_.get();
+  return fake_model_ ? static_cast<SendTabToSelfModel*>(fake_model_.get())
+                     : static_cast<SendTabToSelfModel*>(bridge_.get());
 }
 
 base::WeakPtr<syncer::ModelTypeControllerDelegate>
