d9e12adfa4e920c822441c513de978a1d862d2ca
Hui Yingst <nigi@chromium.org>
2021-04-08 17:27:33 +0000

    [unseasoned-pdf] Make `sent_message_` private.
    Make `sent_message_` a private member of FakePdfViewPluginBase and
    provide a public accessor for it.
    
    Change-Id: I68cabfc6bdc7bc2821a06850d94801ac9fa4ffed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2812506
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870595}

--- pdf/pdf_view_plugin_base_unittest.cc
+++ pdf/pdf_view_plugin_base_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2020 The Chromium Authors. All rights reserved.
+// Copyright 2021 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -123,6 +123,9 @@ class FakePdfViewPluginBase : public PdfViewPluginBase {
 
   MOCK_METHOD(void, UserMetricsRecordAction, (const std::string&), (override));
 
+  const base::Value& sent_message() const { return sent_message_; }
+
+ private:
   base::Value sent_message_;
 };
 
@@ -141,7 +144,7 @@ TEST_F(PdfViewPluginBaseTest, ConsumeSaveToken) {
   expected_message.SetStringKey("type", "consumeSaveToken");
   expected_message.SetStringKey("token", kTokenString);
 
-  EXPECT_EQ(expected_message, fake_plugin_.sent_message_);
+  EXPECT_EQ(expected_message, fake_plugin_.sent_message());
 }
 
 TEST_F(PdfViewPluginBaseTest, HandleSetBackgroundColorMessage) {
