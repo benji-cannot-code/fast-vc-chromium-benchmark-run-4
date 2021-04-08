4c6f1e1393e7a3b6f432968e553b02ae7eea5de2
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2021-04-08 17:27:27 +0000

    [iOS][Default Promo] Add tailored promo utils
    Bug: 1191733
    Change-Id: Ic8bee209504137abe68b871cb5f74a15d2941957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809905
    Commit-Queue: Javier Flores <javierrobles@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#870594}

--- ios/chrome/browser/ui/default_promo/tailored_promo_view_controller.h
+++ ios/chrome/browser/ui/default_promo/tailored_promo_view_controller.h
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
 
-@interface TailoredPromoViewController : ConfirmationAlertViewController
+#import "ios/chrome/browser/ui/default_promo/tailored_promo_util.h"
+
+@interface TailoredPromoViewController
+    : ConfirmationAlertViewController <TailoredPromoConsumer>
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_DEFAULT_PROMO_TAILORED_PROMO_VIEW_CONTROLLER_H_
