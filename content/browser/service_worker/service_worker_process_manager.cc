37fbc5eb5ec9aa6b4184025c96dc141a9f292563
James MacLean <wjmaclean@chromium.org>
2021-04-09 01:04:21 +0000

    Revert "Experiment: Add KeepAlive source histogram to crash keys."
    This reverts commit 26294842ee02653bfab5bd4db1becfd26fd35537.
    
    Reason for revert: This shouldn't live past the branch, and we have the data we need.
    
    Original change's description:
    > Experiment: Add KeepAlive source histogram to crash keys.
    >
    > This re-lands the experiment first landed in
    > https://chromium-review.googlesource.com/c/chromium/src/+/2705030 in
    > an effort to discover the source of new no-BrowsingInstanceID
    > failures.
    >
    > This CL will be reverted once the data has been logged.
    >
    > Bug: 1148542
    > Change-Id: Ie387c8678fea9d13326bcc7360b9b8898523fda4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2799974
    > Commit-Queue: James MacLean <wjmaclean@chromium.org>
    > Reviewed-by: Charlie Reis <creis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#868659}
    
    Bug: 1148542
    Change-Id: Id5d9b3a3623373bcbaf8105e1b1b179df42234e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2815263
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870794}

--- content/browser/service_worker/service_worker_process_manager.cc
+++ content/browser/service_worker/service_worker_process_manager.cc
@@ -67,10 +67,8 @@ void ServiceWorkerProcessManager::Shutdown() {
     for (const auto& it : worker_process_map_) {
       if (it.second->HasProcess()) {
         RenderProcessHost* process = it.second->GetProcess();
-        if (!process->IsKeepAliveRefCountDisabled()) {
-          process->DecrementKeepAliveRefCount(
-              RenderProcessHost::KeepAliveSource::KEEP_ALIVE_SERVICE_WORKER);
-        }
+        if (!process->IsKeepAliveRefCountDisabled())
+          process->DecrementKeepAliveRefCount();
       }
     }
   }
@@ -164,8 +162,7 @@ ServiceWorkerProcessManager::AllocateWorkerProcess(
 
   worker_process_map_.emplace(embedded_worker_id, std::move(site_instance));
   if (!rph->IsKeepAliveRefCountDisabled())
-    rph->IncrementKeepAliveRefCount(
-        RenderProcessHost::KeepAliveSource::KEEP_ALIVE_SERVICE_WORKER);
+    rph->IncrementKeepAliveRefCount();
   out_info->process_id = rph->GetID();
   out_info->start_situation = start_situation;
   return blink::ServiceWorkerStatusCode::kOk;
@@ -194,10 +191,8 @@ void ServiceWorkerProcessManager::ReleaseWorkerProcess(int embedded_worker_id) {
 
   if (it->second->HasProcess()) {
     RenderProcessHost* process = it->second->GetProcess();
-    if (!process->IsKeepAliveRefCountDisabled()) {
-      process->DecrementKeepAliveRefCount(
-          RenderProcessHost::KeepAliveSource::KEEP_ALIVE_SERVICE_WORKER);
-    }
+    if (!process->IsKeepAliveRefCountDisabled())
+      process->DecrementKeepAliveRefCount();
   }
   worker_process_map_.erase(it);
 }
