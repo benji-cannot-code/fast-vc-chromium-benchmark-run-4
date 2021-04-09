ccf7d12e310fac9a362bf4d96d1f8e2630943df1
Emilia Paz <emiliapaz@chromium.org>
2021-04-09 22:14:16 +0000

    [Extensions] SessionStorageManager: remove value from storage
    Removes existent values from storage. Returns changes that will allow
    SettingsFunction to dispatch onChanged event from the
    RemoveFunction.
    
    For each key/value pair, we have the following behaviors in Remove():
      - existent key: change only with old value and key
      - non existent key: no change
    
    Bug: 1185226
    Change-Id: I8df81f04e570dfd310933b3fe662ab3a36941ec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2795520
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871135}

--- extensions/browser/api/storage/session_storage_manager.h
+++ extensions/browser/api/storage/session_storage_manager.h
@@ -68,6 +68,16 @@ class SessionStorageManager : public base::SupportsUserData::Data {
            std::map<std::string, base::Value> values,
            std::vector<ValueChange>& changes);
 
+  // Removes multiple keys for the given `extension_id`.
+  void Remove(const ExtensionId& extension_id,
+              const std::vector<std::string>& keys,
+              std::vector<ValueChange>& changes);
+
+  // Removes a key for the given `extension_id`.
+  void Remove(const ExtensionId& extension_id,
+              const std::string& key,
+              std::vector<ValueChange>& changes);
+
  private:
   struct SessionValue {
     SessionValue(base::Value value, size_t size);
@@ -94,6 +104,10 @@ class SessionStorageManager : public base::SupportsUserData::Data {
     // if a change occurs.
     bool Set(std::map<std::string, base::Value> input_values,
              std::vector<ValueChange>& changes);
+
+    // Removes multiple keys from the storage.
+    void Remove(const std::vector<std::string>& keys,
+                std::vector<ValueChange>& changes);
 
    private:
     // Returns the updated usage for the input values and adds them as session
