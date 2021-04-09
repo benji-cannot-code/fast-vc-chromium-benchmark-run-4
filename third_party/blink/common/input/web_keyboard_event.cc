ff7dfde0e63065c9d926a003f4f8c50726d6f6f3
Daniel Hosseinian <dhoss@chromium.org>
2021-04-09 20:48:22 +0000

    [unseasoned-pdf] Add a function to convert to Blink keyboard events
    Extend the implementation of GetWebInputEvent() to convert keyboard
    events.
    
    Meanwhile, provide a deﬁnition without an initializer for
    `blink::WebKeyboardEvent::kTextLengthCap` to resolve a linker issue
    caused by std::min taking a reference to the static const.
    
    Bug: 1191817
    Change-Id: Ie6f044e6939187cd2ba89b846ea6bdd80a90b5e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2816447
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871101}

--- third_party/blink/common/input/web_keyboard_event.cc
+++ third_party/blink/common/input/web_keyboard_event.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+const size_t WebKeyboardEvent::kTextLengthCap;
+
 std::unique_ptr<WebInputEvent> WebKeyboardEvent::Clone() const {
   return std::make_unique<WebKeyboardEvent>(*this);
 }
