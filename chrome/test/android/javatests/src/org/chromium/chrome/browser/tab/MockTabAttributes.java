c5697963dd46819108e1eed718ba247447f71ecb
David Maunder <davidjm@chromium.org>
2021-04-09 21:37:23 +0000

    Deflake testUndoCloseAllTabsWritesTabListFile
    Bug: 1017732
    Change-Id: Ieec644e45dbeab65bc740b8375ca4bb5857ff81f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2682498
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871117}

---
+++ chrome/test/android/javatests/src/org/chromium/chrome/browser/tab/MockTabAttributes.java
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tab;
+
+import org.chromium.base.UserData;
+
+/**
+ * Attributes associated with a {@link MockTab}
+ */
+public class MockTabAttributes implements UserData {
+    public final boolean restoredFromDisk;
+
+    /**
+     * @param restoredFromDisk if {@link MockTab} was restored from disk
+     */
+    public MockTabAttributes(boolean restoredFromDisk) {
+        this.restoredFromDisk = restoredFromDisk;
+    }
+
+    @Override
+    public void destroy() {}
+}
