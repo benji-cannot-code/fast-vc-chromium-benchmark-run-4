602f9fb515ada23222ef37738892bfc067d392ac
Devlin Cronin <rdevlin.cronin@chromium.org>
2021-04-09 02:03:04 +0000

    [Extensions] Fully remove ChromeExtensionFunctionDetails::GetProfile()
    ChromeExtensionFunctionDetails::GetProfile() is deprecated, because it
    doesn't provide any utility over using Profile::FromBrowserContext()
    directly.  Additionally, it frequently introduced otherwise-unnecessary
    dependencies and instantiations of the ChromeExtensionFunctionDetails
    object.
    
    All usages of the function have been updated; remove the function
    completely.
    
    Bug: 1194753
    Change-Id: Ia1a8e307bebb6b506706686722625b23e3f77ead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2804359
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870813}

--- chrome/browser/extensions/chrome_extension_function_details.h
+++ chrome/browser/extensions/chrome_extension_function_details.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/native_widget_types.h"
 
 class Browser;
-class Profile;
 class ExtensionFunction;
 
 // Provides Chrome-specific details to ExtensionFunction
@@ -22,10 +21,6 @@ class ChromeExtensionFunctionDetails {
   // instance.
   explicit ChromeExtensionFunctionDetails(ExtensionFunction* function);
   ~ChromeExtensionFunctionDetails();
-
-  // DEPRECATED. Instead, use Profile::FromBrowserContext() directly.
-  // See https://crbug.com/1194753.
-  Profile* GetProfile() const;
 
   // Gets the "current" browser, if any.
   //
