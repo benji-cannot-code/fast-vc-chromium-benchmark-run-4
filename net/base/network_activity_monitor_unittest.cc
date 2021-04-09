365c51d03d7c45d2fe8c95b48fef9e498af25e9e
Francois Doray <fdoray@chromium.org>
2021-04-09 16:15:37 +0000

    [net] Use an atomic counter to track received bytes.
    With this CL, a global atomic counter is used to track bytes received
    through //net's UDP and TCP sockets. This replaces the previous
    lock-protected counter in a singleton object managed by LazyInstance.
    
    An atomic counter is faster than a lock-protected counter:
    https://chromeperf.appspot.com/report?sid=82ed117e0b1ef424ee3388019db813d17497700f688268d4cc7ccbbc0c72548c
    
    A global avoids the memory barriers introduced by LazyInstance:
    https://source.chromium.org/chromium/chromium/src/+/master:base/lazy_instance_helpers.h;l=58-95;drc=59b18de9525c94e2ee27748059d110c56aea32e9
    
    We make these performance optimizations in preparation for
    updating the counter every time bytes are received by UDPSocketPosix,
    instead of updating on a timer.
    
    Bug: 1189805
    Change-Id: I8fd5da67f2a086352c60c7f6223d72ad87167d21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2774071
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Auto-Submit: François Doray <fdoray@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870991}

--- net/base/network_activity_monitor_unittest.cc
+++ net/base/network_activity_monitor_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/synchronization/lock.h"
 #include "base/threading/thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -20,48 +19,25 @@ namespace net {
 
 namespace test {
 
-class NetworkActivityMonitorPeer {
- public:
-  static void ResetMonitor() {
-    NetworkActivityMonitor* monitor = NetworkActivityMonitor::GetInstance();
-    base::AutoLock lock(monitor->lock_);
-    monitor->bytes_received_ = 0;
-  }
-};
-
-
 class NetworkActivityMontiorTest : public testing::Test {
  public:
   NetworkActivityMontiorTest() {
-    NetworkActivityMonitorPeer::ResetMonitor();
+    activity_monitor::ResetBytesReceivedForTesting();
   }
 };
 
-TEST_F(NetworkActivityMontiorTest, GetInstance) {
-  NetworkActivityMonitor* monitor = NetworkActivityMonitor::GetInstance();
-  EXPECT_TRUE(monitor != nullptr);
-  EXPECT_TRUE(monitor == NetworkActivityMonitor::GetInstance());
-}
-
 TEST_F(NetworkActivityMontiorTest, BytesReceived) {
-  NetworkActivityMonitor* monitor = NetworkActivityMonitor::GetInstance();
-
-  EXPECT_EQ(0u, monitor->GetBytesReceived());
+  EXPECT_EQ(0u, activity_monitor::GetBytesReceived());
 
   uint64_t bytes = 12345;
-  monitor->IncrementBytesReceived(bytes);
-  EXPECT_EQ(bytes, monitor->GetBytesReceived());
+  activity_monitor::IncrementBytesReceived(bytes);
+  EXPECT_EQ(bytes, activity_monitor::GetBytesReceived());
 }
 
 namespace {
 
 void VerifyBytesReceivedIsMultipleOf(uint64_t bytes) {
-  EXPECT_EQ(0u,
-            NetworkActivityMonitor::GetInstance()->GetBytesReceived() % bytes);
-}
-
-void IncrementBytesReceived(uint64_t bytes) {
-  NetworkActivityMonitor::GetInstance()->IncrementBytesReceived(bytes);
+  EXPECT_EQ(0u, activity_monitor::GetBytesReceived() % bytes);
 }
 
 }  // namespace
@@ -78,7 +54,8 @@ TEST_F(NetworkActivityMontiorTest, Threading) {
   for (size_t i = 0; i < num_increments; ++i) {
     size_t thread_num = i % threads.size();
     threads[thread_num]->task_runner()->PostTask(
-        FROM_HERE, base::BindOnce(&IncrementBytesReceived, bytes_received));
+        FROM_HERE, base::BindOnce(&activity_monitor::IncrementBytesReceived,
+                                  bytes_received));
     threads[thread_num]->task_runner()->PostTask(
         FROM_HERE,
         base::BindOnce(&VerifyBytesReceivedIsMultipleOf, bytes_received));
@@ -86,8 +63,8 @@ TEST_F(NetworkActivityMontiorTest, Threading) {
 
   threads.clear();
 
-  NetworkActivityMonitor* monitor = NetworkActivityMonitor::GetInstance();
-  EXPECT_EQ(num_increments * bytes_received, monitor->GetBytesReceived());
+  EXPECT_EQ(num_increments * bytes_received,
+            activity_monitor::GetBytesReceived());
 }
 
 }  // namespace test
