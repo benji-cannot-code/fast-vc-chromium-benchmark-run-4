9916df530a2aabd85b75e1484ed950a871ff30a4
Kuo Jen Wei <inker@google.com>
2021-04-09 09:55:22 +0000

    CCA: Polish hold PTZ button behavior
    Bug: b/183661327
    Test: Manually by touch, mouse, keydown hold buttons.
    
    Change-Id: Ic1fba4234a37866365e32b5d68522cf619dc83a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2793244
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#870911}

--- chromeos/components/camera_app_ui/resources/js/async_job_queue.js
+++ chromeos/components/camera_app_ui/resources/js/async_job_queue.js
@@ -11,7 +11,16 @@ export class AsyncJobQueue {
    * @public
    */
   constructor() {
+    /**
+     * @private {!Promise}
+     */
     this.promise_ = Promise.resolve();
+
+    /**
+     * Flag for canceling all future jobs.
+     * @private {boolean}
+     */
+    this.clearing_ = false;
   }
 
   /**
@@ -20,7 +29,12 @@ export class AsyncJobQueue {
    * @return {!Promise} Resolved when the job is finished.
    */
   push(job) {
-    this.promise_ = this.promise_.then(() => job());
+    this.promise_ = this.promise_.then(() => {
+      if (this.clearing_) {
+        return;
+      }
+      return job();
+    });
     return this.promise_;
   }
 
@@ -30,5 +44,14 @@ export class AsyncJobQueue {
    */
   async flush() {
     await this.promise_;
+  }
+
+  /**
+   * Clears all not-yet-scheduled jobs and waits for current job finished.
+   */
+  async clear() {
+    this.clearing_ = true;
+    await this.flush();
+    this.clearing_ = false;
   }
 }
