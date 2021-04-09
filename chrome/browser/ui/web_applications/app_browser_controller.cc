dda1112c47584abbcc73595e86629a541cd004ef
paulzhchen <paulzchen@google.com>
2021-04-09 03:26:43 +0000

    [Eche SWA] Aim for a 16:9 portrait aspect ratio and match the CrOS display pixel density.
    1. Remove default window size(480x640) for the Eche SWA.
    2. Eche App has a default bounds is always 16:9 portrait aspect ratio.
    3. The minimum bounds is 240x240.
    
    Bug: b/183678895
    Test: pass browser_tests --gtest_filter=All/EcheAppIntegrationTest*
    Change-Id: I3f928393090f4f4533b64524b432c0c6fd38af74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809717
    Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Paulz Chen <paulzchen@google.com>
    Cr-Commit-Position: refs/heads/master@{#870836}

--- chrome/browser/ui/web_applications/app_browser_controller.cc
+++ chrome/browser/ui/web_applications/app_browser_controller.cc
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/favicon_size.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/resize_utils.h"
 #include "ui/gfx/image/image_skia.h"
 #include "url/gurl.h"
 #include "url/origin.h"
@@ -97,6 +98,24 @@ SkColor GetAltColor(SkColor color) {
       .color;
 }
 
+gfx::Rect GetDefaultBoundsForEche() {
+  // Ensures the Eche bounds is always 16:9 portrait aspect ratio and not more
+  // than half of the windows.
+  const float aspect_ratio = 16.0f / 9.0f;
+  const gfx::Size min_size(240, 240);
+
+  gfx::Rect bounds =
+      display::Screen::GetScreen()->GetDisplayForNewWindows().work_area();
+  const float bounds_aspect_ratio = bounds.width() / bounds.height();
+  const bool is_landscape = (bounds_aspect_ratio >= 1);
+  auto new_width = is_landscape ? (bounds.height() / 2) : bounds.width() / 2;
+  if (min_size.width() > new_width) {
+    new_width = min_size.width();
+  }
+  bounds.ClampToCenteredSize(gfx::Size(new_width, new_width * aspect_ratio));
+  return bounds;
+}
+
 }  // namespace
 
 namespace web_app {
@@ -110,7 +129,6 @@ constexpr gfx::Size HELP_DEFAULT_SIZE(960, 600);
 constexpr gfx::Size CAMERA_WINDOW_DEFAULT_SIZE(kChromeCameraAppDefaultWidth,
                                                kChromeCameraAppDefaultHeight +
                                                    32);
-constexpr gfx::Size ECHE_DEFAULT_SIZE(480, 640);
 }  // namespace
 
 // static
@@ -402,10 +420,7 @@ gfx::Rect AppBrowserController::GetDefaultBounds() const {
     bounds.ClampToCenteredSize(CAMERA_WINDOW_DEFAULT_SIZE);
     return bounds;
   } else if (system_app_type_ == SystemAppType::ECHE) {
-    gfx::Rect bounds =
-        display::Screen::GetScreen()->GetDisplayForNewWindows().work_area();
-    bounds.ClampToCenteredSize(ECHE_DEFAULT_SIZE);
-    return bounds;
+    return GetDefaultBoundsForEche();
   }
   return gfx::Rect();
 }
