e3315bdc922017235501d1fac8c5c1b218ca2bbc
Michael Thiessen <mthiesse@chromium.org>
2021-04-09 21:26:59 +0000

    Avoid unnecessary JNI hops in UrlUtilities
    Since these functions just test a scheme against a list, there's no
    reason to call into c++ and do a bunch of string conversions for them.
    
    Bug: 783819
    Change-Id: Icc6ea381106aab1fe89fb00aa7575e9a16bcac1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798393
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871111}

--- components/external_intents/android/java/src/org/chromium/components/external_intents/ExternalNavigationHandler.java
+++ components/external_intents/android/java/src/org/chromium/components/external_intents/ExternalNavigationHandler.java
@@ -563,10 +563,8 @@ public class ExternalNavigationHandler {
         // TODO(https://crbug.com/783819): Pass a GURL into this function.
         GURL alternateUrl = new GURL(alternateUri);
 
-        // TODO(https://crbug.com/783819): Convert UrlUtilities functions to GURL.
-        boolean isPrimaryUrlValid =
-                (primaryUrl != null) ? UrlUtilities.isAcceptedScheme(primaryUrl.getSpec()) : false;
-        boolean isAlternateUrlValid = UrlUtilities.isAcceptedScheme(alternateUrl.getSpec());
+        boolean isPrimaryUrlValid = UrlUtilities.isAcceptedScheme(primaryUrl);
+        boolean isAlternateUrlValid = UrlUtilities.isAcceptedScheme(alternateUrl);
 
         if (!isPrimaryUrlValid && !isAlternateUrlValid) return false;
 
@@ -1154,7 +1152,7 @@ public class ExternalNavigationHandler {
             return OverrideUrlLoadingResult.forNoOverride();
         }
 
-        boolean isExternalProtocol = !UrlUtilities.isAcceptedScheme(params.getUrl().getSpec());
+        boolean isExternalProtocol = !UrlUtilities.isAcceptedScheme(params.getUrl());
 
         if (isInternalPdfDownload(isExternalProtocol, params)) {
             return OverrideUrlLoadingResult.forNoOverride();
