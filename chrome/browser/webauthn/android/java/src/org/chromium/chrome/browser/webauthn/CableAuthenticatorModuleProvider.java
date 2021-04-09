3b62af94d4e19b8d115788b85b3a2bc1552eb00e
Adam Langley <agl@chromium.org>
2021-04-09 17:26:36 +0000

    webauthn: immediately show notification on mobile
    This change removes support for establishing the caBLE connection in the
    background on mobile. I suspect there's a good chance that some of this
    will come back in the future but, for right now, it's not what we're
    aiming at so drop the complexity and leave the git history of the old
    design should we need to use it in the future.
    
    BUG=1002262
    
    Change-Id: I8ce1e8d5f71279541d7b3d45d90d884cb4cef58a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2808038
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#871018}

--- chrome/browser/webauthn/android/java/src/org/chromium/chrome/browser/webauthn/CableAuthenticatorModuleProvider.java
+++ chrome/browser/webauthn/android/java/src/org/chromium/chrome/browser/webauthn/CableAuthenticatorModuleProvider.java
@@ -41,8 +41,6 @@ public class CableAuthenticatorModuleProvider extends Fragment {
             "org.chromium.chrome.modules.cablev2_authenticator.NetworkContext";
     private static final String REGISTRATION_KEY =
             "org.chromium.chrome.modules.cablev2_authenticator.Registration";
-    private static final String ACTIVITY_CLASS_NAME_KEY =
-            "org.chromium.chrome.modules.cablev2_authenticator.ActivityClassName";
     private static final String ACTIVITY_CLASS_NAME =
             "org.chromium.chrome.browser.webauth.authenticator.CableAuthenticatorActivity";
     private static final String SECRET_KEY =
@@ -84,7 +82,6 @@ public class CableAuthenticatorModuleProvider extends Fragment {
                 CableAuthenticatorModuleProviderJni.get().getSystemNetworkContext());
         arguments.putLong(
                 REGISTRATION_KEY, CableAuthenticatorModuleProviderJni.get().getRegistration());
-        arguments.putString(ACTIVITY_CLASS_NAME_KEY, ACTIVITY_CLASS_NAME);
         arguments.putByteArray(SECRET_KEY, CableAuthenticatorModuleProviderJni.get().getSecret());
         fragment.setArguments(arguments);
         transaction.replace(getId(), fragment);
