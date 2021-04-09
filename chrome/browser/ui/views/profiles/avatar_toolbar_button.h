abb2e68a20b72dfd861d2f9b851803c2a79ba44c
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-04-09 09:06:35 +0000

    Remove AvatarToolbarButton::State::kGenericProfile
    The only difference between kGenericProfile and kNormal is the tooltip
    text shown when hovering the avatar button: for kGenericProfile it's
    "Current user", for kNormal it's the profile name.
    
    However,
    a) The profile name already has a good default ("You").
    b) IsGenericProfile() seems half broken. It checks that there is 1
    profile and that profile has the default icon. This means if the user:
    	- Changes their profile name, they will still see "Generic
    	user" (the change happens only on restart because no bserver
    	code is implemented).
    	- Changes their icon, they will start seeing the profile name,
    	for no apparent reason.
    
    This CL removes kGenericProfile. States that previously returned this
    value will now return kNormal, or kAnimatedUserIdentity (the identity
    pill). We also use the occasion to remove one leftover file from
    crrev.com/c/2794508.
    
    Bug: 1191411
    Change-Id: If25dcca20453514db119fea26f81ce4b9f5bd376
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2810305
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#870897}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button.h
@@ -28,7 +28,6 @@ class AvatarToolbarButton : public ToolbarButton,
   enum class State {
     kIncognitoProfile,
     kGuestSession,
-    kGenericProfile,
     kAnimatedUserIdentity,
     kSyncPaused,
     kSyncError,
