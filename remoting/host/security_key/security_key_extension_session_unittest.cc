0fcc884761597b6a94802ef01a0d82fe14832340
Yuwei Huang <yuweih@chromium.org>
2021-04-10 02:30:25 +0000

    Revert "[remoting host] Add OpenUrl support to HostControlDispatcher"
    This reverts commit 079a464e065875adce4a8cd84c20d4caf44dd303.
    
    Reason for revert: We will introduce a new data channel for remote open URL, so this change can be reverted now.
    
    Original change's description:
    > [remoting host] Add OpenUrl support to HostControlDispatcher
    >
    > This CL adds an OpenUrl method to HostControlDispatcher, which will send
    > a OpenUrlRequest to the client. In future CLs, this method will be
    > called by a standalone binary (something like `remoting-client-open`)
    > via mojo that communicates back to the main host process.
    >
    > BUG=b:183135000
    >
    > Change-Id: I935ecb0d8f5ec22d75ebc3ad640b732331e76c65
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2792533
    > Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#867421}
    
    Bug: b:183135000,1197765
    Change-Id: I2863bcc79407d9f50cb06c0315c1495ed8242d5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2819541
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871211}

--- remoting/host/security_key/security_key_extension_session_unittest.cc
+++ remoting/host/security_key/security_key_extension_session_unittest.cc
@@ -67,7 +67,6 @@ class TestClientStub : public protocol::ClientStub {
   void DeliverHostMessage(const protocol::ExtensionMessage& message) override;
   void SetVideoLayout(const protocol::VideoLayout& layout) override;
   void SetTransportInfo(const protocol::TransportInfo& transport_info) override;
-  void OpenUrl(const protocol::OpenUrlRequest& open_url_request) override;
 
   // protocol::ClipboardStub implementation.
   void InjectClipboardEvent(const protocol::ClipboardEvent& event) override;
@@ -109,9 +108,6 @@ void TestClientStub::SetVideoLayout(const protocol::VideoLayout& layout) {}
 
 void TestClientStub::SetTransportInfo(
     const protocol::TransportInfo& transport_info) {}
-
-void TestClientStub::OpenUrl(const protocol::OpenUrlRequest& open_url_request) {
-}
 
 void TestClientStub::InjectClipboardEvent(
     const protocol::ClipboardEvent& event) {}
--- remoting/protocol/client_stub.h
+++ remoting/protocol/client_stub.h
@@ -21,7 +21,6 @@ namespace protocol {
 
 class Capabilities;
 class ExtensionMessage;
-class OpenUrlRequest;
 class PairingResponse;
 class TransportInfo;
 class VideoLayout;
@@ -47,9 +46,6 @@ class ClientStub : public ClipboardStub,
 
   // Passes the host's transport info to the client.
   virtual void SetTransportInfo(const TransportInfo& transport_info) = 0;
-
-  // Opens an URL on the client.
-  virtual void OpenUrl(const OpenUrlRequest& open_url_request) = 0;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ClientStub);
