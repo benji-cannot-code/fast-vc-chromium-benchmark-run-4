d5ad3255a94565b42d58b40a1ae146c67e2e0647
Ionel Popescu <iopopesc@microsoft.com>
2021-04-10 01:51:33 +0000

    [SelectMenu] Add basic support for positioning <selectmenu>'s popup.
    The <selectmenu>'s popup position is determined by:
    - vertically: where there is more space available
    - horizontally: if there is enough space (<selectmenu>'s width for now)
    or there is more space, the popup is going to be left aligned; if not
    it is going to be right aligned;
    
    For now, since the anchored positioning shceme is not yet provided,
    the position is provided by overriding the computed style for
    the applicable top/left/right/bottom properties.
    
    Bug: 1121840
    Change-Id: I7d75e62d52595b0efa5e7295ab8f8151499c5857
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2812432
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#871204}

--- third_party/blink/renderer/core/html/html_popup_element.h
+++ third_party/blink/renderer/core/html/html_popup_element.h
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class ComputedStyle;
 class Document;
+class HTMLSelectMenuElement;
 
 // The HTMLPopupElement implements the <popup> HTML element. The popup element
 // can be used to construct a topmost popup dialog. This feature is still
@@ -37,11 +39,22 @@ class HTMLPopupElement final : public HTMLElement {
 
   static void HandleLightDismiss(const Event&);
 
+  // TODO(crbug.com/1197720): The popup position should be provided by the new
+  // anchored positioning scheme.
+  void SetNeedsRepositioningForSelectMenu(bool flag);
+  bool NeedsRepositioningForSelectMenu() const;
+  void SetOwnerSelectMenuElement(HTMLSelectMenuElement*);
+  ComputedStyle* CustomStyleForLayoutObject(const StyleRecalcContext&) final;
+
   void Trace(Visitor*) const override;
 
  private:
   void ScheduleHideEvent();
   void MarkStyleDirty();
+
+  // TODO(crbug.com/1197720): The popup position should be provided by the new
+  // anchored positioning scheme.
+  void AdjustPopupPositionForSelectMenu(ComputedStyle&);
 
   void PushNewPopupElement(HTMLPopupElement*);
   void PopPopupElement(HTMLPopupElement*);
@@ -51,6 +64,9 @@ class HTMLPopupElement final : public HTMLElement {
 
   bool open_;
   WeakMember<Element> invoker_;
+
+  bool needs_repositioning_for_select_menu_;
+  WeakMember<HTMLSelectMenuElement> owner_select_menu_element_;
 };
 
 }  // namespace blink
