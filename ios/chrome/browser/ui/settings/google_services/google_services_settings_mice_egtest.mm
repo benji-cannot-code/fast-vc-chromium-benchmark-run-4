d7cbfb265d9cb5c5c64f13618b0ea27b32e84e28
Nohemi Fernandez <fernandex@chromium.org>
2021-04-10 10:50:47 +0000

    [iOS][MICE] Add sign-out logic for "Allow Chrome sign-in" feature.
    Allows the user to select their data retention strategy when signing
    out using the "Allow Chrome sign-in" option.
    
    Screenshots:
    https://drive.google.com/file/d/1UZkC7jn5O2DdzBJoFdS4PHnaSM9NvjaO
    https://drive.google.com/file/d/18574_9yIQ9fH7piBngx2ELpRxHFpJPrP
    https://drive.google.com/file/d/1dNpzDoTmcPWgCOxqlcqKOIQYw7dqB4-z
    
    Change-Id: I2cbe95ecfcaea8993c5a6e088314881ec2bfce3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809402
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871247}

--- ios/chrome/browser/ui/settings/google_services/google_services_settings_mice_egtest.mm
+++ ios/chrome/browser/ui/settings/google_services/google_services_settings_mice_egtest.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/signin/public/base/account_consistency_method.h"
 #include "components/signin/public/base/signin_pref_names.h"
+#include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui.h"
 #import "ios/chrome/browser/ui/settings/google_services/google_services_settings_constants.h"
@@ -12,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/grit/ios_chromium_strings.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_actions.h"
+#import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
@@ -23,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using l10n_util::GetNSString;
+using chrome_test_util::ButtonWithAccessibilityLabelId;
 using chrome_test_util::PrimarySignInButton;
 using chrome_test_util::SettingsMenuBackButton;
 using chrome_test_util::StaticTextWithAccessibilityLabelId;
@@ -34,6 +37,7 @@ id<GREYMatcher> GoogleServicesSettingsButton() {
                     grey_sufficientlyVisible(),
                     grey_accessibilityID(kSettingsGoogleServicesCellId), nil);
 }
+
 }  // namespace
 
 // Integration tests using the Google services settings screen with
@@ -66,6 +70,14 @@ id<GREYMatcher> GoogleServicesSettingsButton() {
                                    /*is_toggled_on=*/YES,
                                    /*enabled=*/YES)]
       performAction:chrome_test_util::TurnSettingsSwitchOn(NO)];
+  [[EarlGrey
+      selectElementWithMatcher:ButtonWithAccessibilityLabelId(
+                                   IDS_IOS_SIGNOUT_DIALOG_SIGN_OUT_BUTTON)]
+      performAction:grey_tap()];
+  [[EarlGrey
+      selectElementWithMatcher:ButtonWithAccessibilityLabelId(
+                                   IDS_IOS_SIGNOUT_DIALOG_CLEAR_DATA_BUTTON)]
+      performAction:grey_tap()];
 
   // Verify that the user is signed out and sign-in is disabled.
   [[EarlGrey selectElementWithMatcher:SettingsMenuBackButton()]
