ba7b9046c90b18d02554ba54f211fcb3e6b713a0
Tom Anderson <thomasanderson@chromium.org>
2021-04-12 20:56:37 +0000

    Extract gtk_compat to a separate target for X11 and Wayland delegates
    R=nickdiego
    
    Bug: 1192861
    Change-Id: I066f400171f65887c03adda173ad95f05938312c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2818912
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871628}

--- ui/gtk/x/gtk_event_loop_x11.h
+++ ui/gtk/x/gtk_event_loop_x11.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/glib/glib_integers.h"
 #include "ui/base/glib/glib_signal.h"
-#include "ui/gtk/gtk_buildflags.h"
+#include "ui/gtk/gtk_compat.h"
 
 namespace ui {
 
