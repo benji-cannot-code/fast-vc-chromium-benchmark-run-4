5b2ecd9799fd2489a5073fc8f9820cfc1314b21a
Yusuke Sato <yusukes@chromium.org>
2021-04-12 21:24:34 +0000

    arc: Add arc_sharesheet_bridge_unittest.cc for better test coverage
    BUG=None
    TEST=try
    
    Change-Id: I85799d4e0eeaeb185040a32ea687596d893cc5ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2818271
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871642}

--- chrome/browser/ash/arc/sharesheet/arc_sharesheet_bridge.cc
+++ chrome/browser/ash/arc/sharesheet/arc_sharesheet_bridge.cc
@@ -45,6 +45,12 @@ ArcSharesheetBridge* ArcSharesheetBridge::GetForBrowserContext(
   return ArcSharesheetBridgeFactory::GetForBrowserContext(context);
 }
 
+// static
+ArcSharesheetBridge* ArcSharesheetBridge::GetForBrowserContextForTesting(
+    content::BrowserContext* context) {
+  return ArcSharesheetBridgeFactory::GetForBrowserContextForTesting(context);
+}
+
 ArcSharesheetBridge::ArcSharesheetBridge(content::BrowserContext* context,
                                          ArcBridgeService* bridge_service)
     : arc_bridge_service_(bridge_service),
