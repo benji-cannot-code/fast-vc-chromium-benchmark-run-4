ebcc29f3eb5ebf73d5af63c206911f81848db1a7
Moe Ahmadi <mahmadi@chromium.org>
2021-04-12 21:55:11 +0000

    [memories] Fixes compile error
    This is a follow up to crrev.com/c/2812429. It makes the WeakPtrFactory
    member variable available in official builds.
    
    Bug: 1198292, 1179069
    Change-Id: Ib612f2d61b4165cf1d7a4e9d48a4b32ed930fcff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822322
    Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871652}

--- chrome/browser/ui/webui/memories/memories_handler.h
+++ chrome/browser/ui/webui/memories/memories_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/webui/memories/memories.mojom.h"
 #include "components/history_clusters/core/memories.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -17,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 
 #if !defined(OFFICIAL_BUILD)
-#include "base/memory/weak_ptr.h"
 #include "base/task/cancelable_task_tracker.h"
 #endif
 
@@ -62,6 +62,7 @@ class MemoriesHandler : public memories::mojom::PageHandler {
       base::OnceCallback<void(std::vector<memories::mojom::MemoryPtr>)>;
   void OnHistoryQueryResults(MemoriesQueryResultsCallback callback,
                              history::QueryResults results);
+  base::CancelableTaskTracker history_task_tracker_;
 #endif
 
   Profile* profile_;
@@ -70,10 +71,7 @@ class MemoriesHandler : public memories::mojom::PageHandler {
   mojo::Remote<memories::mojom::Page> page_;
   mojo::Receiver<memories::mojom::PageHandler> page_handler_;
 
-#if !defined(OFFICIAL_BUILD)
-  base::CancelableTaskTracker history_task_tracker_;
   base::WeakPtrFactory<MemoriesHandler> weak_ptr_factory_{this};
-#endif
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_MEMORIES_MEMORIES_HANDLER_H_
