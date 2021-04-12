6ace5db5e37fd28cbefe6803203d981cc7dbbd2e
Lukasz Anforowicz <lukasza@chromium.org>
2021-04-12 20:09:21 +0000

    Browser-initited "about:blank" navigations should commit opaque origin.
    A browser-initiated "about:blank" navigation will not have a
    `requestor_origin_` in `blink::DocumentLoader::CommitNavigation`.
    Before this CL, we would incorrectly inherit the origin from the
    "opener" (i.e. the `owner_document`) instead of committing an
    opaque/unique origin.  After this CL, we will correctly ignore
    `owner_document_` if `requestor_origin_` is missing.
    
    To correctly handle the initial empty document (where
    `requestor_origin_` is also missing) the CL extracts
    DocumentLoader::CalculateOwnerFrame, where the 3 cases (initial empty
    document, about:blank, about:srcdoc) can be explicitly, separately
    handled.
    
    Bug: 1190088
    Change-Id: Ie668b988828b0c3ba055482af3f1cdb646e98cb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2782064
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871609}

--- third_party/blink/renderer/core/loader/document_loader.h
+++ third_party/blink/renderer/core/loader/document_loader.h
@@ -87,6 +87,7 @@ class ApplicationCacheHostForFrame;
 class ContentSecurityPolicy;
 class Document;
 class DocumentParser;
+class Frame;
 class FrameLoader;
 class HistoryItem;
 class LocalDOMWindow;
@@ -369,6 +370,7 @@ class CORE_EXPORT DocumentLoader : public GarbageCollected<DocumentLoader>,
   Member<MHTMLArchive> archive_;
 
  private:
+  Frame* CalculateOwnerFrame();
   scoped_refptr<SecurityOrigin> CalculateOrigin(Document* owner_document);
   void InitializeWindow(Document* owner_document);
   void DidInstallNewDocument(Document*);
