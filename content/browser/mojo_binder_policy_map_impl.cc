269dde0ebd2b9964ed03275a4d7cba44f0d97e1a
Miyoung Shin <myid.shin@igalia.com>
2021-04-12 07:13:43 +0000

    Prerender: Grant Access to Storage Foundation API for Prerendering
    This CL grants Storage Foundation API interface and adds a web tests
    to ensure that prerendering pages can access Storage Foundation API.
    
    Bug: 1197021
    Change-Id: Ia1113ec42e1e83faf11546e7184ccb73b1079840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814731
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871394}

--- content/browser/mojo_binder_policy_map_impl.cc
+++ content/browser/mojo_binder_policy_map_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/cache_storage/cache_storage.mojom.h"
 #include "third_party/blink/public/mojom/clipboard/clipboard.mojom.h"
 #include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom.h"
+#include "third_party/blink/public/mojom/native_io/native_io.mojom.h"
 
 namespace content {
 
@@ -39,6 +40,7 @@ void RegisterContentBinderPoliciesForSameOriginPrerendering(
 
   map.SetPolicy<blink::mojom::CacheStorage>(MojoBinderPolicy::kGrant);
   map.SetPolicy<blink::mojom::IDBFactory>(MojoBinderPolicy::kGrant);
+  map.SetPolicy<blink::mojom::NativeIOHost>(MojoBinderPolicy::kGrant);
   map.SetPolicy<network::mojom::RestrictedCookieManager>(
       MojoBinderPolicy::kGrant);
 }
