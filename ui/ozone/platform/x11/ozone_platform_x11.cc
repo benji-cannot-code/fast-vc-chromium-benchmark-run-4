197625fef81fa80264add4bb082d9bd860383de3
Nick Diego Yamane <nickdiego@igalia.com>
2021-04-12 12:33:15 +0000

    ozone/x11: Remove unused xkbcommon init code
    ...which got somehow checked in by mistake months ago [1]. This reverts
    it, which on Linux Desktop should be fine as keyboard input handling
    (including layout switching) is handled at GTK Input Method impl.
    Though, keep the TODO for further investigation, if needed, eg. in
    chromeos-linux builds.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2044778
    
    Bug: 987939
    Change-Id: I2e21899702f5d3ea550c3eb4e5b5f04bb15566b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2819915
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#871447}

--- ui/ozone/platform/x11/ozone_platform_x11.cc
+++ ui/ozone/platform/x11/ozone_platform_x11.cc
@@ -63,13 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gtk/x/gtk_ui_delegate_x11.h"  // nogncheck
 #endif
 
-#if BUILDFLAG(USE_XKBCOMMON)
-#include "ui/events/ozone/layout/xkb/xkb_evdev_codes.h"
-#include "ui/events/ozone/layout/xkb/xkb_keyboard_layout_engine.h"
-#else
-#include "ui/events/ozone/layout/stub/stub_keyboard_layout_engine.h"
-#endif
-
 namespace ui {
 
 namespace {
@@ -78,9 +71,7 @@ namespace {
 class OzonePlatformX11 : public OzonePlatform,
                          public ui::OSExchangeDataProviderFactoryOzone {
  public:
-  OzonePlatformX11() {
-    SetInstance(this);
-  }
+  OzonePlatformX11() { SetInstance(this); }
 
   ~OzonePlatformX11() override = default;
 
@@ -228,14 +219,8 @@ class OzonePlatformX11 : public OzonePlatform,
     cursor_factory_ = std::make_unique<X11CursorFactory>();
     gpu_platform_support_host_.reset(CreateStubGpuPlatformSupportHost());
 
-#if BUILDFLAG(USE_XKBCOMMON)
-    keyboard_layout_engine_ =
-        std::make_unique<XkbKeyboardLayoutEngine>(xkb_evdev_code_converter_);
-    // TODO(nickdiego): debugging..
+    // TODO(crbug.com/987939): Support XKB.
     keyboard_layout_engine_ = std::make_unique<StubKeyboardLayoutEngine>();
-#else
-    keyboard_layout_engine_ = std::make_unique<StubKeyboardLayoutEngine>();
-#endif
     KeyboardLayoutEngineManager::SetKeyboardLayoutEngine(
         keyboard_layout_engine_.get());
 
@@ -309,10 +294,6 @@ class OzonePlatformX11 : public OzonePlatform,
   }
 
   bool common_initialized_ = false;
-
-#if BUILDFLAG(USE_XKBCOMMON)
-  XkbEvdevCodes xkb_evdev_code_converter_;
-#endif
 
   // Objects in the UI process.
   std::unique_ptr<KeyboardLayoutEngine> keyboard_layout_engine_;
