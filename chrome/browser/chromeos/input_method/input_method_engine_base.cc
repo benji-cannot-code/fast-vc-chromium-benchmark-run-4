ae1992726bab52b4079e15d8962cfdc506ef3264
Darren Shen <shend@chromium.org>
2021-04-12 02:10:26 +0000

    ime: Don't reset prefs listener on reset.
    InputMethodEngineBase::Reset is misleading. It is called whenever
    composition is cancelled by the system (e.g. by moving the cursor or
    switching focus). It does not mean that the InputMethodEngineBase is
    actually 'reset' to an initial state.
    
    Hence, we must not reset prefs listener in Reset, because we need to
    continue listening for prefs changes while the IME is active.
    
    Bug: b/181077907
    Change-Id: I3e6da8eda97f783d1c69790a2993f780b4b48fff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2812629
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871345}

--- chrome/browser/chromeos/input_method/input_method_engine_base.cc
+++ chrome/browser/chromeos/input_method/input_method_engine_base.cc
@@ -128,9 +128,6 @@ void InputMethodEngineBase::Disable() {
 
 void InputMethodEngineBase::Reset() {
   observer_->OnReset(active_component_id_);
-  if (pref_change_registrar_) {
-    pref_change_registrar_.reset();
-  }
 }
 
 void InputMethodEngineBase::ProcessKeyEvent(const ui::KeyEvent& key_event,
