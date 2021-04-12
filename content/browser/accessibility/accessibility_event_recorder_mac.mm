4ef481d0ea5c07310d4f37f33ad079d94388052e
Alexey Baskakov <loyso@chromium.org>
2021-04-12 05:04:49 +0000

    Revert "Fire menu accessibility events for ARIA menus on macOS and Linux"
    This reverts commit 904c488c8f082f5dc993d45682d23d01b3416c65.
    
    Reason for revert: Fails Win tests.
    https://ci.chromium.org/ui/p/chromium/builders/ci/win-asan/18192 
    
    Original change's description:
    > Fire menu accessibility events for ARIA menus on macOS and Linux
    >
    > When an ARIA menu is opened or closed, we should be firing accessibility
    > events on macOS and Linux.
    >
    > BrowserAccessibilityManagerWin was firing menu-related platform events
    > in response to subtree creation and node destruction. Move this logic
    > to BrowserAccessibilityManager and the AXEventGenerator so that it can
    > be used on all platforms. Also fire the new MENU_POPUP events when the
    > ignored state is changed to show/hide an ARIA menu.
    >
    > Ax-Relnotes: Assistive technologies on macOS and Linux will be notified
    > when an ARIA menu is opened or closed and can use this information to
    > update state and/or present the change.
    >
    > Bug: 1171433
    >
    > Change-Id: I5203259ed106578601ae5e1ee26b043f11240b9b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2642354
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#871061}
    
    Bug: 1171433
    Change-Id: I3a2a74d2dc6aad610c0a48247208725f6aee4f49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2820574
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Owners-Override: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871367}

--- content/browser/accessibility/accessibility_event_recorder_mac.mm
+++ content/browser/accessibility/accessibility_event_recorder_mac.mm
@@ -71,8 +71,6 @@ AccessibilityEventRecorderMac::AccessibilityEventRecorderMac(
   AddNotification(@"AXMenuItemSelected");
   AddNotification(@"AXRowCollapsed");
   AddNotification(@"AXRowExpanded");
-  AddNotification((NSString*)kAXMenuClosedNotification);
-  AddNotification((NSString*)kAXMenuOpenedNotification);
   AddNotification(NSAccessibilityFocusedUIElementChangedNotification);
   AddNotification(NSAccessibilityRowCollapsedNotification);
   AddNotification(NSAccessibilityRowCountChangedNotification);
