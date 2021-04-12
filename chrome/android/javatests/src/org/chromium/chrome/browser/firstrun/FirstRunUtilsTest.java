cbba86ef2ad20cc6dbc5d95b4362f1e21658821c
Alice Wang <aliceywang@chromium.org>
2021-04-12 15:48:43 +0000

    [Android][Test] Refactor auth token management in test infrastructure
    This CL refactors the auth token management in the test infrastructure
    by removing the redundant fields and simplifying the update logic.
    
    Bug: 1194581
    Change-Id: I5ad67c30a553c2ce18c948a05307a40a58e2690b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2807283
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871483}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunUtilsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunUtilsTest.java
@@ -67,7 +67,7 @@ public class FirstRunUtilsTest {
     }
 
     private void addTestAccount() {
-        mAccountManager.addAccount(AccountHolder.builder("dummy@gmail.com").build());
+        mAccountManager.addAccount(AccountHolder.createFromEmail("dummy@gmail.com"));
     }
 
     @Test
