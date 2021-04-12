1ab6563c7e7b3b88c1231c7f80e8e22b1deac481
Dan McArdle <dmcardle@google.com>
2021-04-12 15:54:20 +0000

    [BackoffEntrySerializer] Simplify GetReleaseTime() property in fuzzer
    The TestDeserialize side of net_backoff_entry_serializer_fuzzer tests
    the property that the original BackoffEntry's release time is less than
    or equal to the reparsed BackoffEntry's release time.
    
    The attached Clusterfuzz bug revealed that this property was a little
    too simplistic because it did not account for the difference between the
    parse time and serialize time.
    
    When SerializeToValue() computes the new absolute release time, it first
    computes the entry's backoff duration as the entry's release time minus
    the entry's "now" time, then adds that to the |time_now| parameter. The
    difference between the entry's "now" time and |time_now| was responsible
    for the release-time property test failing.
    
    Rather than reversing the time translation that SerializeToValue() does,
    this commit standardizes on the |translator.parse_time()| time (for the
    TestDeserialize side of the fuzzer). As a result, we do not have to
    change the spelling of the property.
    
    Bug: 1195476
    Change-Id: I10e871336230c145ac96df2f251ffab46be3dd17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2817623
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871486}

--- net/base/backoff_entry_serializer_fuzzer.cc
+++ net/base/backoff_entry_serializer_fuzzer.cc
@@ -39,6 +39,10 @@ class ProtoTranslator {
     return base::Time() +
            base::TimeDelta::FromMicroseconds(input_.parse_time());
   }
+  base::TimeTicks parse_time_ticks() const {
+    return base::TimeTicks() +
+           base::TimeDelta::FromMicroseconds(input_.parse_time());
+  }
   base::Time serialize_time() const {
     return base::Time() +
            base::TimeDelta::FromMicroseconds(input_.serialize_time());
@@ -96,7 +100,7 @@ void TestDeserialize(const ProtoTranslator& translator) {
   BackoffEntry::Policy policy = translator.policy();
 
   MockClock clock;
-  clock.SetNow(translator.now_ticks());
+  clock.SetNow(translator.parse_time_ticks());
 
   // Attempt to deserialize a BackoffEntry.
   std::unique_ptr<BackoffEntry> entry =
