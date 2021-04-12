0bd3e7234db107f9040fd6bc813b5f88cf4c3e5a
Michael Checo <michaelcheco@google.com>
2021-04-12 15:41:17 +0000

    Diagnostics: Add NetworkHealthProvider skeleton
    Bug: 1197335
    Test: ./out/Default/chromeos_components_unittests --gtest_filter=NetworkHealthProvider*
    Change-Id: I70540f96de73be245c91a70908686f8b3ad26ebd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2816220
    Auto-Submit: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871480}

---
+++ chromeos/components/diagnostics_ui/backend/network_health_provider.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_COMPONENTS_DIAGNOSTICS_UI_BACKEND_NETWORK_HEALTH_PROVIDER_H_
+#define CHROMEOS_COMPONENTS_DIAGNOSTICS_UI_BACKEND_NETWORK_HEALTH_PROVIDER_H_
+
+namespace chromeos {
+namespace diagnostics {
+
+class NetworkHealthProvider {
+ public:
+  NetworkHealthProvider();
+
+  NetworkHealthProvider(const NetworkHealthProvider&) = delete;
+  NetworkHealthProvider& operator=(const NetworkHealthProvider&) = delete;
+
+  ~NetworkHealthProvider();
+};
+
+}  // namespace diagnostics
+}  // namespace chromeos
+
+#endif  // CHROMEOS_COMPONENTS_DIAGNOSTICS_UI_BACKEND_NETWORK_HEALTH_PROVIDER_H_
