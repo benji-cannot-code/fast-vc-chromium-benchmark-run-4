dda37430db3ef346af1a1178feb6a2c6b8ca3677
Charlene Yan <cyan@chromium.org>
2021-04-12 01:46:25 +0000

    [Read Later] Add IPH trigger for reading list entry point discovery.
    Bug: 1175979
    Change-Id: I09dc9fc39098fbf84953ef4d67e248f86840bf4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2753233
    Commit-Queue: Charlene Yan <cyan@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871340}

--- chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
@@ -365,6 +365,15 @@ void BrowserTabStripController::CloseTab(int model_index) {
   model_->CloseWebContentsAt(model_index,
                              TabStripModel::CLOSE_USER_GESTURE |
                              TabStripModel::CLOSE_CREATE_HISTORICAL_TAB);
+
+  // Try to show reading list IPH if needed.
+  if (tabstrip_->GetTabCount() >= 7) {
+    feature_engagement_tracker_->NotifyEvent(
+        feature_engagement::events::kClosedTabWithEightOrMore);
+
+    browser_view_->feature_promo_controller()->MaybeShowPromo(
+        feature_engagement::kIPHReadingListEntryPointFeature);
+  }
 }
 
 void BrowserTabStripController::AddTabToGroup(
@@ -795,7 +804,6 @@ void BrowserTabStripController::AddTab(WebContents* contents,
 
   tabstrip_->AddTabAt(index, TabRendererData::FromTabInModel(model_, index),
                       is_active);
-
   // Try to show tab groups IPH if needed.
   if (tabstrip_->GetTabCount() >= 6) {
     feature_engagement_tracker_->NotifyEvent(
@@ -803,9 +811,6 @@ void BrowserTabStripController::AddTab(WebContents* contents,
 
     browser_view_->feature_promo_controller()->MaybeShowPromo(
         feature_engagement::kIPHDesktopTabGroupsNewGroupFeature);
-
-    browser_view_->feature_promo_controller()->MaybeShowPromo(
-        feature_engagement::kIPHReadingListEntryPointFeature);
   }
 }
 
