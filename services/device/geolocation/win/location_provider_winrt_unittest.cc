405f40a46fa7555a887d0aaf57eee57eff781463
Robert Liao <robliao@chromium.org>
2021-04-13 21:32:14 +0000

    Remove base::win::RoInitialize from LocationProviderWinrtTest
    base::win::RoInitialize is banned due to improper use.
    
    BUG=1197722
    
    Change-Id: Ie96f819d3b9502b6585b46aef67f21bfdc5fbc88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2823876
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872118}

--- services/device/geolocation/win/location_provider_winrt_unittest.cc
+++ services/device/geolocation/win/location_provider_winrt_unittest.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/device/geolocation/win/location_provider_winrt.h"
 
+#include "base/optional.h"
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
-#include "base/win/core_winrt_util.h"
+#include "base/win/scoped_winrt_initializer.h"
+#include "base/win/windows_version.h"
 #include "services/device/geolocation/win/fake_geocoordinate_winrt.h"
 #include "services/device/geolocation/win/fake_geolocator_winrt.h"
 #include "services/device/public/cpp/geolocation/geoposition.h"
@@ -81,17 +83,20 @@ class TestingLocationProviderWinrt : public LocationProviderWinrt {
 };
 
 class LocationProviderWinrtTest : public testing::Test {
- public:
-  static void SetUpTestSuite() {
-    base::win::RoInitialize(RO_INIT_TYPE::RO_INIT_MULTITHREADED);
-  }
-
  protected:
   LocationProviderWinrtTest()
       : observer_(
             std::make_unique<MockLocationObserver>(run_loop_.QuitClosure())),
         callback_(base::BindRepeating(&MockLocationObserver::OnLocationUpdate,
                                       base::Unretained(observer_.get()))) {}
+
+  void SetUp() override {
+    if (base::win::GetVersion() < base::win::Version::WIN8)
+      GTEST_SKIP();
+
+    winrt_initializer_.emplace();
+    ASSERT_TRUE(winrt_initializer_->Succeeded());
+  }
 
   void InitializeProvider(
       PositionStatus position_status = PositionStatus::PositionStatus_Ready) {
@@ -113,6 +118,7 @@ class LocationProviderWinrtTest : public testing::Test {
 
   base::test::TaskEnvironment task_environment_;
   base::RunLoop run_loop_;
+  base::Optional<base::win::ScopedWinrtInitializer> winrt_initializer_;
   const std::unique_ptr<MockLocationObserver> observer_;
   const LocationProvider::LocationProviderUpdateCallback callback_;
   std::unique_ptr<TestingLocationProviderWinrt> provider_;
