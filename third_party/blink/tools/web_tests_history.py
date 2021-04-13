82a7a20ee1be5e0e2bc081f0813561bc08ed3eab
Preethi Mohan <preethim@google.com>
2021-04-13 17:46:21 +0000

    Remove version check.
    Bug: 1197679
    Change-Id: Ia85933e489397618ff6bfb7d890bb8580914bd44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822669
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Preethi Mohan <preethim@google.com>
    Cr-Commit-Position: refs/heads/master@{#872009}

--- third_party/blink/tools/web_tests_history.py
+++ third_party/blink/tools/web_tests_history.py
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import sys
 
-from blinkpy.common import version_check  # pylint: disable=unused-import
 from blinkpy.web_tests.web_tests_history import main
 
 if __name__ == '__main__':
