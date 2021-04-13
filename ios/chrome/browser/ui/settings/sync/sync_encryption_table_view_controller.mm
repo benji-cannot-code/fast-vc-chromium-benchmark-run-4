422af0f8a904121bdb39f7ad23a690f325fe30b5
Nohemi Fernandez <fernandex@chromium.org>
2021-04-13 16:34:58 +0000

    [iOS] Remove Sync observer on shutdown in sync encryption.
    Settings dismissal occurs before the shutdown notification is sent to
    ProfileSyncService observers. Ensure that Chrome stops observing Sync
    for this use case.
    
    Bug: 1137311
    Change-Id: I035e4d41865f3ef40a29e34958454b0244e044c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2818023
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871970}

--- ios/chrome/browser/ui/settings/sync/sync_encryption_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/sync/sync_encryption_table_view_controller.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/main/browser.h"
 #include "ios/chrome/browser/sync/profile_sync_service_factory.h"
 #import "ios/chrome/browser/sync/sync_observer_bridge.h"
+#import "ios/chrome/browser/ui/settings/settings_controller_protocol.h"
 #import "ios/chrome/browser/ui/settings/sync/sync_create_passphrase_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/sync/sync_encryption_passphrase_table_view_controller.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_cells_constants.h"
@@ -54,7 +55,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
 
 }  // namespace
 
-@interface SyncEncryptionTableViewController () <SyncObserverModelBridge> {
+@interface SyncEncryptionTableViewController () <SyncObserverModelBridge,
+                                                 SettingsControllerProtocol> {
   std::unique_ptr<SyncObserverBridge> _syncObserver;
   BOOL _isUsingSecondaryPassphrase;
 }
@@ -162,6 +164,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
 
 - (void)tableView:(UITableView*)tableView
     didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
+  DCHECK(self.browser) << "tableView:didSelectRowAtIndexPath called after "
+                          "-settingsWillBeDismissed";
   DCHECK_EQ(indexPath.section,
             [self.tableViewModel
                 sectionForSectionIdentifier:SectionIdentifierEncryption]);
@@ -195,9 +199,26 @@ typedef NS_ENUM(NSInteger, ItemType) {
   [tableView deselectRowAtIndexPath:indexPath animated:NO];
 }
 
+#pragma mark - SettingsControllerProtocol callbacks
+
+- (void)reportDismissalUserAction {
+  NOTREACHED();
+}
+
+- (void)reportBackUserAction {
+  NOTREACHED();
+}
+
+- (void)settingsWillBeDismissed {
+  _syncObserver.reset();
+  _browser = nil;
+}
+
 #pragma mark SyncObserverModelBridge
 
 - (void)onSyncStateChanged {
+  DCHECK(self.browser)
+      << "onSyncStateChanged called after -settingsWillBeDismissed";
   ChromeBrowserState* browserState = self.browser->GetBrowserState();
   syncer::SyncService* service =
       ProfileSyncServiceFactory::GetForBrowserState(browserState);
