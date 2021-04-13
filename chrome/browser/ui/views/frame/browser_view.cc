28b315fca98c29852c9a1c702e5726503937f5db
Leonard Grey <lgrey@chromium.org>
2021-04-13 18:07:58 +0000

    Reland "Hide download shelf in fullscreen on Aura"
    This is a reland of 8178d2c405a5e024965c19d1607ffd11ec36a632
    
    Fix is in r871964
    
    Original change's description:
    > Hide download shelf in fullscreen on Aura
    >
    > I think we can probably do better on unifying the Mac/Aura fullscreen
    > lifecycle but that requires more thinking and I've left this unfixed
    > too long.
    >
    > Bug: 1135060
    > Change-Id: Ide8976f39103dfe481a1aea356fcafd7909d2f16
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2815841
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Commit-Queue: Leonard Grey <lgrey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#870734}
    
    Bug: 1135060
    Change-Id: I8f50006086a0956ebd44cde5e2d3966c8b4cf441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822893
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872018}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1431,7 +1431,6 @@ void BrowserView::FullscreenStateChanging() {
           ? GetExclusiveAccessManager()->GetExclusiveAccessExitBubbleType()
           : EXCLUSIVE_ACCESS_BUBBLE_TYPE_NONE,
       display::kInvalidDisplayId);
-  frame_->GetFrameView()->OnFullscreenStateChanged();
 }
 
 void BrowserView::FullscreenStateChanged() {
@@ -3314,6 +3313,7 @@ void BrowserView::ProcessFullscreen(bool fullscreen,
   // Undo our anti-jankiness hacks and force a re-layout.
   in_process_fullscreen_ = false;
   ToolbarSizeChanged(false);
+  frame_->GetFrameView()->OnFullscreenStateChanged();
 }
 
 bool BrowserView::ShouldUseImmersiveFullscreenForUrl(const GURL& url) const {
