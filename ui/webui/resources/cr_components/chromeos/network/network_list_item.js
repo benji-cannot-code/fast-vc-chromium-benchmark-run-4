84c16af1e99e713dd6e8f3257f6083956fd4dbd0
Gordon Seto <gordonseto@google.com>
2021-04-13 23:04:31 +0000

    [CrOS Settings] Open pSIM activation flow from 'Activate' button.
    Add click listener to 'Activate' button for pending activation
    network-list-items that opens the pSIM activation flow when clicked.
    
    Bug: 1196507
    Change-Id: I0860abf02b2d9ee46da392b11af0548822331027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2823585
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872157}

--- ui/webui/resources/cr_components/chromeos/network/network_list_item.js
+++ ui/webui/resources/cr_components/chromeos/network/network_list_item.js
@@ -656,6 +656,8 @@ Polymer({
       this.onUnlockButtonClick_();
     } else if (this.item && this.item.hasOwnProperty('customItemName')) {
       this.fire('custom-item-selected', this.item);
+    } else if (this.isPSimPendingActivationNetwork_) {
+      this.fireShowDetails_(event);
     } else {
       this.fire('selected', this.item);
       this.focusRequested_ = true;
@@ -797,6 +799,17 @@ Polymer({
    */
   getActivateBtnA11yLabel_() {
     return this.i18n('networkListItemActivateA11yLabel', this.getItemName_());
+  },
+
+  /**
+   * @param {!Event} event
+   * @private
+   */
+  onActivateButtonClick_(event) {
+    this.fire(
+        'show-cellular-setup',
+        {pageName: cellularSetup.CellularSetupPageName.PSIM_FLOW_UI});
+    event.stopPropagation();
   },
 
   /**
