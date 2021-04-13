2fa4c5f314c101df23c335ce2144fc76379e51be
Peter Kotwicz <pkotwicz@chromium.org>
2021-04-13 17:42:12 +0000

    Guard Value::WriteIntoTracedValue() with BUILDFLAG(ENABLE_BASE_TRACING)
    This fixes the webrtc standalone build which uses //base but sets
    enable_base_tracing=false
    This broke as a result of
    https://chromium-review.googlesource.com/c/chromium/src/+/2786753
    
    WebRTC bug:12634
    
    Change-Id: I806e620bdf993dd0b8587ae3b258cbb6871d7882
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2821754
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872008}

--- base/values.h
+++ base/values.h
@@ -579,8 +579,10 @@ class BASE_EXPORT Value {
   // Serializes to a string for logging and debug purposes.
   std::string DebugString() const;
 
+#if BUILDFLAG(ENABLE_BASE_TRACING)
   // Write this object into a trace.
   void WriteIntoTracedValue(perfetto::TracedValue) const;
+#endif  // BUILDFLAG(ENABLE_BASE_TRACING)
 
  protected:
   // Checked convenience accessors for dict and list.
