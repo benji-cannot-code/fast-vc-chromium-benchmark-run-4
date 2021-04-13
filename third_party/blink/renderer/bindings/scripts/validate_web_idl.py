500b3be312bae777ae0cb3a7b3dbc74cd2d3f288
Canon Mukai <canonmukai@google.com>
2021-04-13 10:46:30 +0000

    validator: Implement a framework of IDL Validator
    Currently, there is no system to check if the IDL files follow the rules
    described in Web IDL (https://heycam.github.io/webidl),
    so this CL implements it.
    
    Design Doc:
    https://docs.google.com/document/d/1T9hCySxnTBQPEFmc48E0i9BUZuriVOv3mvHCOLZQJOo
    
    Diagram in Design Doc:
    https://docs.google.com/document/d/1T9hCySxnTBQPEFmc48E0i9BUZuriVOv3mvHCOLZQJOo/edit#heading=h.t5qcecbha5kb
    
    Bug: 1198370
    Change-Id: Ifbc59115869635f573f86ce22efca9b5d2f13dd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2805691
    Commit-Queue: Canon Mukai <canonmukai@google.com>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871886}

---
+++ third_party/blink/renderer/bindings/scripts/validate_web_idl.py
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+Runs the validator of Web IDL files using web_idl.Database.
+"""
+
+import optparse
+
+import validator
+import validator.rules
+import web_idl
+
+
+def parse_options():
+    parser = optparse.OptionParser()
+    parser.add_option("--web_idl_database",
+                      type="string",
+                      help="filepath of the input database")
+    options, args = parser.parse_args()
+
+    opt_name = "web_idl_database"
+    if getattr(options, opt_name) is None:
+        parser.error("--{} is a required option.".format(opt_name))
+
+    return options, args
+
+
+def main():
+    options, args = parse_options()
+
+    # Register rules
+    rule_store = validator.RuleStore()
+    validator.rules.register_all_rules(rule_store)
+
+    # Validate
+    database = web_idl.file_io.read_pickle_file(options.web_idl_database)
+    validator_instance = validator.Validator(database)
+    validator_instance.execute(rule_store)
+
+
+if __name__ == '__main__':
+    main()
---
+++ third_party/blink/renderer/bindings/scripts/validator/rules/__init__.py
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from . import rules_function_like
+
+
+def register_all_rules(rule_store):
+    rules_function_like.register_rules(rule_store)
