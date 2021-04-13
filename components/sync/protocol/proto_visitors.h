835166b27458111852b50c5fe9cf74d953054663
Siyu An <siyua@chromium.org>
2021-04-13 00:24:38 +0000

    [VCN] Extend proto WalletMaskedCreditCard with fields for vcn
    Design doc: go/vcn-chrome-sync-dd (google only).
    
    Bug: 1196021
    Change-Id: I3de56264ee5b6bf626690fe8416c3f65c4345dfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2806563
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#871712}

--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -1091,6 +1091,8 @@ VISIT_PROTO_FIELDS(const sync_pb::WalletMaskedCreditCard& proto) {
   VISIT(nickname);
   VISIT(card_issuer);
   VISIT(instrument_id);
+  VISIT_ENUM(virtual_card_enrollment_state);
+  VISIT(card_art_url);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::WalletMetadataSpecifics& proto) {
