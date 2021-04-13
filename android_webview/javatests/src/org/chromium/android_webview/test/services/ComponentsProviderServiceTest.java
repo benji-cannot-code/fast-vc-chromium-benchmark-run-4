3637f66b6c04193a5aca44646edfbd534d5c9b31
Laís Minchillo <laisminchillo@chromium.org>
2021-04-13 18:12:18 +0000

    [AW] ComponentUpdater: record UMA of directory size
    Record UMA data for component updater directory size. This is logged once
    after AwComponentUpdateService finishes an update job. Add a method in
    FileUtils to recursively calculate file/directory size and a test in
    FileUtilsTest.
    
    Modify ComponentsProviderServiceTest to write data to test files and
    assert that their size is not zero.
    
    Manually tested that metrics are logged when AwComponentUpdateService
    finishes an update job. Automated tests will be implemented in a
    follow-up CL.
    
    Bug: 1171817
    Test: Manual
    Change-Id: I9b68e7cccb6abd132d038862e04cd2279aa89ae2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791432
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Laís Minchillo <laisminchillo@chromium.org>
    Auto-Submit: Laís Minchillo <laisminchillo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872023}

--- android_webview/javatests/src/org/chromium/android_webview/test/services/ComponentsProviderServiceTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/services/ComponentsProviderServiceTest.java
@@ -32,6 +32,7 @@ import org.chromium.components.background_task_scheduler.TaskIds;
 import org.chromium.components.component_updater.IComponentsProviderService;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -250,6 +251,15 @@ public class ComponentsProviderServiceTest {
         final File file = new File(
                 versionDirectory, getComponentTestFileName(componentId, sequenceNumber, version));
         Assert.assertTrue("Failed to create file " + file.getAbsolutePath(), file.createNewFile());
+
+        FileWriter writer = new FileWriter(file);
+        for (int i = 0; i < 100; i++) {
+            writer.write("Adding some data to file...\n");
+        }
+        writer.close();
+
+        Assert.assertTrue("File " + file.getName() + " should not have size 0",
+                FileUtils.getFileSizeBytes(file) > 0);
     }
 
     private static String getComponentTestFileName(
