a87663fcc390086a29f77ab30149ea95275d5410
Patrick Monette <pmonette@chromium.org>
2021-04-13 21:33:20 +0000

    Fix crash in PerProfileWorkerTaskTracker::CreateWorkerTask()
    Because service worker notifications can arrive asynchronously, their
    render process host could already have been deleted.
    
    With this change, those service workers are ignored to avoid calling
    into a dead RPH.
    
    One of the goals with this CL is to be minimal enough so that it can be
    easily reverted when service worker notifications are fixed to arrive
    synchronously.
    
    Fixed: 1191930
    Change-Id: I34b04386136e3bcace3535c730a68fdab4a3da1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2823169
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872119}

--- chrome/browser/task_manager/providers/per_profile_worker_task_tracker.h
+++ chrome/browser/task_manager/providers/per_profile_worker_task_tracker.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/containers/flat_map.h"
+#include "base/containers/flat_set.h"
 #include "base/scoped_observer.h"
 #include "chrome/browser/task_manager/providers/task.h"
 #include "content/public/browser/dedicated_worker_service.h"
@@ -18,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/tokens/tokens.h"
 
 class Profile;
+
+namespace content {
+class RenderProcessHost;
+}
 
 namespace task_manager {
 
@@ -85,7 +90,7 @@ class PerProfileWorkerTaskTracker
   void CreateWorkerTask(
       const WorkerId& worker_id,
       Task::Type task_type,
-      int worker_process_id,
+      content::RenderProcessHost* worker_process_host,
       base::flat_map<WorkerId, std::unique_ptr<WorkerTask>>* out_worker_tasks);
 
   // Deletes an existing WorkerTask from |out_worker_tasks| and notifies
@@ -134,6 +139,11 @@ class PerProfileWorkerTaskTracker
 
   base::flat_map<int64_t /*version_id*/, std::unique_ptr<WorkerTask>>
       service_worker_tasks_;
+
+  // Because service worker notifications are asynchronous, it is possible to
+  // be notified of the creation of a service worker after its render process
+  // was deleted. Those workers are ignored.
+  base::flat_set<int64_t /*version_id*/> ignored_service_worker_;
 };
 
 }  // namespace task_manager
