95c148be0caffaa85c03520326bdfa78bc22245c
Shik Chen <shik@chromium.org>
2021-04-13 08:18:18 +0000

    CCA: Remove unnecessary async/await for localStorage
    The async/await semantic was added to be compatible with the legacy
    chrome.storage API, which is already removed. This CL removes that
    unnecessary compatibility hack.
    
    Bug: b:185181901
    Test: Pass Closure compiler and CCAUIStress.real
    Change-Id: Iee863cf6dd20b67cec8924369af600b3554a61d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2821739
    Commit-Queue: Shik Chen <shik@chromium.org>
    Auto-Submit: Shik Chen <shik@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871841}

--- chromeos/components/camera_app_ui/resources/js/main.js
+++ chromeos/components/camera_app_ui/resources/js/main.js
@@ -161,8 +161,8 @@ export class App {
    * @private
    */
   setupToggles_() {
-    localStorage.get({expert: false})
-        .then((values) => state.set(state.State.EXPERT, values['expert']));
+    const values = localStorage.get({expert: false});
+    state.set(state.State.EXPERT, values['expert']);
     dom.getAll('input', HTMLInputElement).forEach((element) => {
       element.addEventListener('keypress', (event) => {
         const e = assertInstanceof(event, KeyboardEvent);
@@ -198,10 +198,8 @@ export class App {
       });
       if (element.dataset['key'] !== undefined) {
         // Restore the previously saved state on startup.
-        localStorage.get(payload(element))
-            .then(
-                (values) => util.toggleChecked(
-                    element, values[element.dataset['key']]));
+        const values = localStorage.get(payload(element));
+        util.toggleChecked(element, values[element.dataset['key']]);
       }
     });
   }
--- chromeos/components/camera_app_ui/resources/js/models/local_storage.js
+++ chromeos/components/camera_app_ui/resources/js/models/local_storage.js
@@ -4,10 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
+ * TODO(b/185181901): Simplify the interface.
  * @param {(string|!Array<string>|!Object)} keys
- * @return {!Promise<!Object>}
+ * @return {!Object}
  */
-export async function get(keys) {
+export function get(keys) {
   let result = {};
   let sanitizedKeys = [];
   if (typeof keys === 'string') {
@@ -39,9 +40,8 @@ export async function get(keys) {
 
 /**
  * @param {!Object<string>} items
- * @return {!Promise}
  */
-export async function set(items) {
+export function set(items) {
   for (const [key, val] of Object.entries(items)) {
     window.localStorage.setItem(key, JSON.stringify(val));
   }
@@ -49,9 +49,8 @@ export async function set(items) {
 
 /**
  * @param {(string|!Array<string>)} items
- * @return {!Promise}
  */
-export async function remove(items) {
+export function remove(items) {
   if (typeof items === 'string') {
     items = [items];
   }
@@ -61,8 +60,8 @@ export async function remove(items) {
 }
 
 /**
- * @return {!Promise}
+ * Clears all the items in the local storage.
  */
-export async function clear() {
+export function clear() {
   window.localStorage.clear();
 }
