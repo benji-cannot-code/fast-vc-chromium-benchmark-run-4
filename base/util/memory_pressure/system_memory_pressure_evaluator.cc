91f85e2f8a64ad3b2fae36639d86344332d2da2c
Sonny Rao <sonnyrao@chromium.org>
2021-04-13 20:39:07 +0000

    Enable tab discarder for LaCros
    This enabled the existing code added for discard on Linux as a very
    basic tab discarder for LaCros.
    
    Bug: internal b/184846650
    Change-Id: I5c54d0445f4122bfc2e2478673e1660ef607e62e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822474
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Sonny Rao <sonnyrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872095}

--- base/util/memory_pressure/system_memory_pressure_evaluator.cc
+++ base/util/memory_pressure/system_memory_pressure_evaluator.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif defined(OS_WIN)
 #include "base/util/memory_pressure/system_memory_pressure_evaluator_win.h"
 #include "base/win/windows_version.h"
+#elif BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "base/util/memory_pressure/system_memory_pressure_evaluator_linux.h"
 #endif
 
 namespace util {
@@ -45,6 +47,9 @@ SystemMemoryPressureEvaluator::CreateDefaultSystemEvaluator(
     evaluator->CreateOSSignalPressureEvaluator(monitor->CreateVoter());
   }
   return evaluator;
+#elif BUILDFLAG(IS_CHROMEOS_LACROS)
+  return std::make_unique<util::os_linux::SystemMemoryPressureEvaluator>(
+      monitor->CreateVoter());
 #endif
   return nullptr;
 }
