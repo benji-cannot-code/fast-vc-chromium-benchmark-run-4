3630925eacc8ddcef193436f519e414cd0e1055f
Victor Vianna <victorvianna@google.com>
2021-04-13 12:58:14 +0000

    Revert "Remove base::win::RoInitialize from LocationProviderWinrtTest"
    This reverts commit 76bf5bccf42f7159ef2333b5282e61b0dee4f99e.
    
    Reason for revert: Causes failure on Win7 bots because ScopedWinrtInitializer is supposed to be used on Win8+
    See crbug.com/1198507
    
    Original change's description:
    > Remove base::win::RoInitialize from LocationProviderWinrtTest
    >
    > base::win::RoInitialize is banned due to improper use.
    >
    > BUG=1197722
    >
    > Change-Id: Ie11f8666c7901e5b791d35c35de1a2b6de1b072e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822310
    > Commit-Queue: Robert Liao <robliao@chromium.org>
    > Auto-Submit: Robert Liao <robliao@chromium.org>
    > Reviewed-by: Miguel Casas <mcasas@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#871655}
    
    Bug: 1197722
    Change-Id: Icf2b4554dfa8cabb45044ad6ffa5fe3157ed4be6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2821320
    Owners-Override: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#871914}

--- services/device/geolocation/win/location_provider_winrt_unittest.cc
+++ services/device/geolocation/win/location_provider_winrt_unittest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
-#include "base/win/scoped_winrt_initializer.h"
+#include "base/win/core_winrt_util.h"
 #include "services/device/geolocation/win/fake_geocoordinate_winrt.h"
 #include "services/device/geolocation/win/fake_geolocator_winrt.h"
 #include "services/device/public/cpp/geolocation/geoposition.h"
@@ -81,15 +81,17 @@ class TestingLocationProviderWinrt : public LocationProviderWinrt {
 };
 
 class LocationProviderWinrtTest : public testing::Test {
+ public:
+  static void SetUpTestSuite() {
+    base::win::RoInitialize(RO_INIT_TYPE::RO_INIT_MULTITHREADED);
+  }
+
  protected:
   LocationProviderWinrtTest()
       : observer_(
             std::make_unique<MockLocationObserver>(run_loop_.QuitClosure())),
         callback_(base::BindRepeating(&MockLocationObserver::OnLocationUpdate,
-                                      base::Unretained(observer_.get()))) {
-    if (!winrt_initializer_.Succeeded())
-      ADD_FAILURE() << "WinRT initialization failed.";
-  }
+                                      base::Unretained(observer_.get()))) {}
 
   void InitializeProvider(
       PositionStatus position_status = PositionStatus::PositionStatus_Ready) {
@@ -109,7 +111,6 @@ class LocationProviderWinrtTest : public testing::Test {
     provider_->SetUpdateCallback(callback_);
   }
 
-  base::win::ScopedWinrtInitializer winrt_initializer_;
   base::test::TaskEnvironment task_environment_;
   base::RunLoop run_loop_;
   const std::unique_ptr<MockLocationObserver> observer_;
