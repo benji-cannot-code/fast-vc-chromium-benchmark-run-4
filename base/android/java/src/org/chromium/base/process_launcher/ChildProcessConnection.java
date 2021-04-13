1cb150e22266f6d067a1b1cc494548e2764919e1
Bo Liu <boliu@chromium.org>
2021-04-13 23:26:22 +0000

    Unbind WaiveCpu with strong/moderate binding
    Bind WaiveCpu binding only if the regular strong and moderate
    bindings are unbound. This is just to ensure that adding
    BIND_NOT_FOREGROUND does not lower the cpu priority in this
    situation.
    
    This also has the side effect of only creating 2 bindings
    instead of 3 on start up, which is probably what the perf
    bot measured.
    
    Note the logic is not centralized but spread out because we
    always want to bind the new binding before unbinding an
    existing one.
    
    Bug: 1163920
    Bug: 1192885
    Change-Id: Ie845f4fcb03f105ead8b9b273a9553bf01208d2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2790303
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872169}

--- base/android/java/src/org/chromium/base/process_launcher/ChildProcessConnection.java
+++ base/android/java/src/org/chromium/base/process_launcher/ChildProcessConnection.java
@@ -787,6 +787,9 @@ public class ChildProcessConnection {
         }
         if (mStrongBindingCount == 0) {
             mStrongBinding.bindServiceConnection();
+            if (mModerateWaiveCpuBinding.isBound()) {
+                mModerateWaiveCpuBinding.unbindServiceConnection();
+            }
             updateBindingState();
         }
         mStrongBindingCount++;
@@ -800,6 +803,10 @@ public class ChildProcessConnection {
         assert mStrongBindingCount > 0;
         mStrongBindingCount--;
         if (mStrongBindingCount == 0) {
+            if (mModerateWaiveCpuBindingCount > 0 && !mModerateWaiveCpuBinding.isBound()
+                    && !mModerateBinding.isBound()) {
+                mModerateWaiveCpuBinding.bindServiceConnection();
+            }
             mStrongBinding.unbindServiceConnection();
             updateBindingState();
         }
@@ -823,7 +830,8 @@ public class ChildProcessConnection {
             return;
         }
         if (waiveCpuPriority) {
-            if (mModerateWaiveCpuBindingCount == 0) {
+            if (mModerateWaiveCpuBindingCount == 0 && !mStrongBinding.isBound()
+                    && !mModerateBinding.isBound()) {
                 mModerateWaiveCpuBinding.bindServiceConnection();
                 updateBindingState();
             }
@@ -832,6 +840,9 @@ public class ChildProcessConnection {
         }
         if (mModerateBindingCount == 0) {
             mModerateBinding.bindServiceConnection();
+            if (mModerateWaiveCpuBinding.isBound()) {
+                mModerateWaiveCpuBinding.unbindServiceConnection();
+            }
             updateBindingState();
         }
         mModerateBindingCount++;
@@ -848,7 +859,7 @@ public class ChildProcessConnection {
         if (waiveCpuPriority) {
             assert mModerateWaiveCpuBindingCount > 0;
             mModerateWaiveCpuBindingCount--;
-            if (mModerateWaiveCpuBindingCount == 0) {
+            if (mModerateWaiveCpuBindingCount == 0 && mModerateWaiveCpuBinding.isBound()) {
                 mModerateWaiveCpuBinding.unbindServiceConnection();
                 updateBindingState();
             }
@@ -857,6 +868,10 @@ public class ChildProcessConnection {
         assert mModerateBindingCount > 0;
         mModerateBindingCount--;
         if (mModerateBindingCount == 0) {
+            if (mModerateWaiveCpuBindingCount > 0 && !mModerateWaiveCpuBinding.isBound()
+                    && !mStrongBinding.isBound()) {
+                mModerateWaiveCpuBinding.bindServiceConnection();
+            }
             mModerateBinding.unbindServiceConnection();
             updateBindingState();
         }
