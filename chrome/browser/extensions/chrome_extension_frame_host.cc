3fa5e9e24dcabddd2c94b07ba70c59b0cb49fbe6
Julie Jeongeun Kim <jkim@igalia.com>
2021-04-13 02:42:03 +0000

    Reland "Convert ExtensionHostMsg_Request to mojom.LocalFrameHost message"
    This is a reland of 8eba1ef76426a3efb1340ceabfb6516f77162db0
    
    The problem was that it calls ResponseCallback with no
    Type::LIST value when it has an error.
    The updated CL adds a helper function for handling
    ResponseCallback when it has an error in order to call it
    with an empty Type::LIST value consistently. It also
    introduces ExtensionFrameHostBrowserTest to test the error
    case.
    
    Original change's description:
    > Convert ExtensionHostMsg_Request to mojom.LocalFrameHost message
    >
    > This CL converts ExtensionHostMsg_Request to
    > extensions::mojom::LocalFrameHost::Request() and the reply of it
    > is sent back to the renderer through the RequestCallback instead
    > of ExtensionMsg_Response.
    >
    > Bug: 1183518
    > Change-Id: Idc9faf41c8d5159fcd2b6617f4e45de621ed14f3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784353
    > Commit-Queue: Julie Kim <jkim@igalia.com>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Reviewed-by: Sam McNally <sammc@chromium.org>
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#869478}
    
    Bug: 1183518
    Change-Id: I2ba0585317099d755b1c14ff3f179e572fb6eb74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814121
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#871762}

--- chrome/browser/extensions/chrome_extension_frame_host.cc
+++ chrome/browser/extensions/chrome_extension_frame_host.cc
@@ -15,7 +15,7 @@ namespace extensions {
 
 ChromeExtensionFrameHost::ChromeExtensionFrameHost(
     content::WebContents* web_contents)
-    : ExtensionFrameHost(web_contents), web_contents_(web_contents) {}
+    : ExtensionFrameHost(web_contents) {}
 
 ChromeExtensionFrameHost::~ChromeExtensionFrameHost() = default;
 
@@ -25,7 +25,7 @@ void ChromeExtensionFrameHost::RequestScriptInjectionPermission(
     mojom::RunLocation run_location,
     RequestScriptInjectionPermissionCallback callback) {
   ExtensionActionRunner* runner =
-      ExtensionActionRunner::GetForWebContents(web_contents_);
+      ExtensionActionRunner::GetForWebContents(web_contents());
   if (!runner) {
     std::move(callback).Run(false);
     return;
@@ -38,7 +38,7 @@ void ChromeExtensionFrameHost::GetAppInstallState(
     const GURL& requestor_url,
     GetAppInstallStateCallback callback) {
   ExtensionRegistry* registry =
-      ExtensionRegistry::Get(web_contents_->GetBrowserContext());
+      ExtensionRegistry::Get(web_contents()->GetBrowserContext());
   const ExtensionSet& extensions = registry->enabled_extensions();
   const ExtensionSet& disabled_extensions = registry->disabled_extensions();
 
--- extensions/browser/extension_frame_host.cc
+++ extensions/browser/extension_frame_host.cc
@@ -7,10 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "content/public/browser/render_process_host.h"
+#include "extensions/browser/extension_function_dispatcher.h"
+#include "extensions/browser/extension_web_contents_observer.h"
+
 namespace extensions {
 
 ExtensionFrameHost::ExtensionFrameHost(content::WebContents* web_contents)
-    : receivers_(web_contents, this) {}
+    : web_contents_(web_contents), receivers_(web_contents, this) {}
 
 ExtensionFrameHost::~ExtensionFrameHost() = default;
 
@@ -26,6 +30,16 @@ void ExtensionFrameHost::GetAppInstallState(
     const GURL& requestor_url,
     GetAppInstallStateCallback callback) {
   std::move(callback).Run(std::string());
+}
+
+void ExtensionFrameHost::Request(mojom::RequestParamsPtr params,
+                                 RequestCallback callback) {
+  content::RenderFrameHost* render_frame_host =
+      receivers_.GetCurrentTargetFrame();
+  ExtensionWebContentsObserver::GetForWebContents(web_contents_)
+      ->dispatcher()
+      ->Dispatch(std::move(params), render_frame_host,
+                 render_frame_host->GetProcess()->GetID(), std::move(callback));
 }
 
 }  // namespace extensions
