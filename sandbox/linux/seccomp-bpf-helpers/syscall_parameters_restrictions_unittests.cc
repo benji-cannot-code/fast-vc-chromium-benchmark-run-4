f6a5951ca5fe73d9f2ac31b2814a722ca0397bde
Matthew Denton <mpdenton@chromium.org>
2021-04-13 02:49:23 +0000

    Linux sandbox syscall broker: use struct kernel_stat
    The struct stat used in libc is different (in size and field ordering)
    from the structure assumed by the Linux kernel. So, when emulating
    system calls, we need to use the struct definition the kernel expects.
    
    This CL adds linux_stat.h that includes definitions of the different
    kernel structs.
    
    Change-Id: I53cad35c2251dff0f6b7ea77528cfa58ef3cab4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780876
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871767}

--- sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions_unittests.cc
+++ sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions_unittests.cc
@@ -38,10 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/linux/system_headers/linux_time.h"
 #include "sandbox/linux/tests/unit_tests.h"
 
-#if !defined(OS_ANDROID)
-#include "third_party/lss/linux_syscall_support.h"  // for MAKE_PROCESS_CPUCLOCK
-#endif
-
 namespace sandbox {
 
 namespace {
--- sandbox/linux/services/syscall_wrappers.cc
+++ sandbox/linux/services/syscall_wrappers.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "sandbox/linux/services/syscall_wrappers.h"
 
+#include <fcntl.h>
 #include <pthread.h>
 #include <sched.h>
 #include <setjmp.h>
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "sandbox/linux/system_headers/capability.h"
 #include "sandbox/linux/system_headers/linux_signal.h"
+#include "sandbox/linux/system_headers/linux_stat.h"
 #include "sandbox/linux/system_headers/linux_syscalls.h"
 
 namespace sandbox {
@@ -218,7 +220,7 @@ asm(
 #undef STR
 #undef XSTR
 
-#endif
+#endif  // defined(ARCH_CPU_X86_FAMILY)
 
 int sys_sigaction(int signum,
                   const struct sigaction* act,
@@ -242,7 +244,7 @@ int sys_sigaction(int signum,
 #error "Unsupported architecture."
 #endif
     }
-#endif
+#endif  // defined(ARCH_CPU_X86_FAMILY)
   }
 
   LinuxSigAction linux_oldact = {};
@@ -260,6 +262,23 @@ int sys_sigaction(int signum,
   return result;
 }
 
-#endif  // defined(MEMORY_SANITIZER)
+#endif  // !defined(OS_NACL_NONSFI)
+
+int sys_stat(const char* path, struct kernel_stat* stat_buf) {
+#if !defined(__NR_stat)
+  return syscall(__NR_newfstatat, AT_FDCWD, path, stat_buf, 0);
+#else
+  return syscall(__NR_stat, path, stat_buf);
+#endif
+}
+
+int sys_lstat(const char* path, struct kernel_stat* stat_buf) {
+#if !defined(__NR_lstat)
+  return syscall(__NR_newfstatat, AT_FDCWD, path, stat_buf,
+                 AT_SYMLINK_NOFOLLOW);
+#else
+  return syscall(__NR_lstat, path, stat_buf);
+#endif
+}
 
 }  // namespace sandbox
