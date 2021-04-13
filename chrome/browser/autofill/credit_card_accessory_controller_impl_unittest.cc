248137ffda614e43b8581d828b7be130dfadb8d3
Friedrich Horschig <fhorschig@chromium.org>
2021-04-13 12:32:43 +0000

    [Android][MFill] Notify observers of the credit card sheet
    This CL is a noop.
    
    It adds a second path (still unused) where a registered observer gets
    notified about sheet changes. (A couple of tests ensure this call
    actually happens but the wiring to the manual filling controller happens
    in a follow-up.)
    
    Bug: 1169167
    Change-Id: I63c7b63622db6f7ff953034b526688786d11fa92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814260
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#871906}

--- chrome/browser/autofill/credit_card_accessory_controller_impl_unittest.cc
+++ chrome/browser/autofill/credit_card_accessory_controller_impl_unittest.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autofill/credit_card_accessory_controller_impl.h"
 
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/mock_callback.h"
+#include "chrome/browser/autofill/accessory_controller.h"
 #include "chrome/browser/autofill/mock_manual_filling_controller.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_profile.h"
@@ -26,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using testing::_;
 using testing::SaveArg;
 using testing::SaveArgPointee;
+using IsFillingSourceAvailable = AccessoryController::IsFillingSourceAvailable;
 
 constexpr char kExampleSite[] = "https://example.com";
 const std::u16string kFirstTwelveDigits = u"411111111111";
@@ -155,6 +158,8 @@ class CreditCardAccessoryControllerTest
   MockAutocompleteHistoryManager history_;
   testing::NiceMock<MockManualFillingController> mock_mf_controller_;
   TestAutofillManager af_manager_;
+  base::MockCallback<AccessoryController::FillingSourceObserver>
+      filling_source_observer_;
 };
 
 TEST_F(CreditCardAccessoryControllerTest, RefreshSuggestions) {
@@ -221,10 +226,16 @@ TEST_F(CreditCardAccessoryControllerTest, PreventsFillingInsecureContexts) {
 }
 
 TEST_F(CreditCardAccessoryControllerTest, ServerCardUnmask) {
+  // TODO(crbug.com/1169167): Move this into setup once controllers don't push
+  // updated sheets proactively anymore.
+  controller()->RegisterFillingSourceObserver(filling_source_observer_.Get());
+
   autofill::CreditCard card = test::GetMaskedServerCard();
   data_manager_.AddCreditCard(card);
   data_manager_.AddCreditCard(test::GetCreditCard());
 
+  EXPECT_CALL(filling_source_observer_,
+              Run(controller(), IsFillingSourceAvailable(true)));
   ASSERT_TRUE(controller());
   controller()->RefreshSuggestions();
 
