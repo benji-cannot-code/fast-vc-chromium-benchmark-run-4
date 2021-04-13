cf515d3a226a86b0af97fb371158737339b9652f
Gordon Seto <gordonseto@google.com>
2021-04-13 23:50:49 +0000

    [CrOS Cellular] Remove extraneous showConfigurableSections_ checks.
    Remove extra showConfigurableSections_ checks in internet-detail-page
    and internet-detail-dialog that would hide Configure, Forget, etc.
    buttons.
    Fix internet-detail-dialog error where no managedProperties_ would
    cause showConnect_ and showDisconnect_ to error. This bug was not
    visible before this change as these methods were behind
    showConfigurableSections_ which always checks for defined
    managedProperties_.
    
    Bug: 1198625
    Change-Id: Ia64201a99480c63399d2a99d1f5ae6a6f03d8bb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2824587
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872184}

--- chrome/browser/resources/chromeos/internet_detail_dialog/internet_detail_dialog.js
+++ chrome/browser/resources/chromeos/internet_detail_dialog/internet_detail_dialog.js
@@ -413,7 +413,8 @@ Polymer({
   },
 
   /**
-   * @param {!chromeos.networkConfig.mojom.ManagedProperties} managedProperties
+   * @param {!chromeos.networkConfig.mojom.ManagedProperties|undefined}
+   *     managedProperties
    * @return {boolean}
    * @private
    */
@@ -423,11 +424,15 @@ Polymer({
   },
 
   /**
-   * @param {!chromeos.networkConfig.mojom.ManagedProperties} managedProperties
+   * @param {!chromeos.networkConfig.mojom.ManagedProperties|undefined}
+   *     managedProperties
    * @return {boolean}
    * @private
    */
   showConnect_(managedProperties) {
+    if (!managedProperties) {
+      return false;
+    }
     return managedProperties.connectable &&
         managedProperties.type !=
         chromeos.networkConfig.mojom.NetworkType.kEthernet &&
@@ -436,11 +441,15 @@ Polymer({
   },
 
   /**
-   * @param {!chromeos.networkConfig.mojom.ManagedProperties} managedProperties
+   * @param {!chromeos.networkConfig.mojom.ManagedProperties|undefined}
+   *     managedProperties
    * @return {boolean}
    * @private
    */
   showDisconnect_(managedProperties) {
+    if (!managedProperties) {
+      return false;
+    }
     return managedProperties.type !=
         chromeos.networkConfig.mojom.NetworkType.kEthernet &&
         managedProperties.connectionState !=
