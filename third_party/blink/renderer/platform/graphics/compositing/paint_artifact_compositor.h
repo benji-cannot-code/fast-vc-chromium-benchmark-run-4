7aea93ffc6f7494b2bdc44860b0465f4ec192cf7
Philip Rogers <pdr@chromium.org>
2021-04-13 18:32:01 +0000

    [CompositeAfterPaint] Ensure scrollbar thumb opacity animations repaint
    This patch issues the same call to cc::Layer::SetNeedsDisplay in
    ScrollableArea::SetScrollbarNeedsPaintInvalidation for
    CompositeAfterPaint as is done pre-CompositeAfterPaint.
    
    Blink paints scrollbars with a ScrollbarDisplayItem which leads to
    PaintArtifactCompositor creating and storing a cc::ScrollbarLayerBase.
    The scrollbar track/thumb/etc for painted overlay scrollbars are
    actually painted outside of blink, similar to foreign layers, via
    PaintedOverlayScrollbarLayer::Update. To match the efficiency of
    pre-CompositeAfterPaint, we directly mark the layer as needing an
    update rather than forcing blink to paint, run PAC::Update, etc.
    
    Bug: 1192894
    Change-Id: I1cb7a07b9957dccc302d1c6516c561fee6e55d22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2819622
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872035}

--- third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h
+++ third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h
@@ -254,6 +254,8 @@ class PLATFORM_EXPORT PaintArtifactCompositor final
 
   size_t ApproximateUnsharedMemoryUsage() const;
 
+  void SetScrollbarNeedsDisplay(CompositorElementId element_id);
+
  private:
   // A pending layer is a collection of paint chunks that will end up in
   // the same cc::Layer.
@@ -429,6 +431,8 @@ class PLATFORM_EXPORT PaintArtifactCompositor final
       const PendingLayer* previous_layer) const;
 
   void UpdateDebugInfo() const;
+
+  cc::ScrollbarLayerBase* ScrollbarLayer(CompositorElementId);
 
   // For notifying blink of composited scrolling.
   base::WeakPtr<CompositorScrollCallbacks> scroll_callbacks_;
