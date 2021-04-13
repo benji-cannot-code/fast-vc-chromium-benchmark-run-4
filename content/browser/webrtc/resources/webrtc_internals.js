823d953b281bb56cf7acb1a80ae6a317543e1552
Evan Shrubsole <eshr@google.com>
2021-04-13 09:36:42 +0000

    [webrtc-internals] Export changeToLegacyGetStats
    This not be exported caused the webrtc-internals perf browser tests to fail.
    
    Bug: 12660
    Change-Id: I724333113138a18e1910b01e4ca465ae5163c841
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2821438
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Evan Shrubsole <eshr@google.com>
    Cr-Commit-Position: refs/heads/master@{#871866}

--- content/browser/webrtc/resources/webrtc_internals.js
+++ content/browser/webrtc/resources/webrtc_internals.js
@@ -223,6 +223,7 @@ function changeToLegacyGetStats() {
   selectElement.value = currentGetStatsMethod;
   requestStats();
 }
+window.changeToLegacyGetStats = changeToLegacyGetStats;
 
 /**
  * A helper function for getting a peer connection element id.
