4167980b7a63d39ea866370d9b78d0c053e3a6d3
Alex Keng <shihken@microsoft.com>
2021-04-13 08:45:20 +0000

    EditContext: add beforeinput(insertText) event
    This CL adds beforeinput(insertText) event (which is cancelable)
    to English typing.
    
    A test is also added.
    
    
    Bug: 999184
    Change-Id: I26867f91d6ececda9f5b177996a47de8011ee4b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2792915
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Anupam Snigdha <snianu@microsoft.com>
    Commit-Queue: Alex Keng <shihken@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#871846}

--- third_party/blink/renderer/core/editing/editor_key_bindings.cc
+++ third_party/blink/renderer/core/editing/editor_key_bindings.cc
@@ -70,6 +70,11 @@ bool Editor::HandleEditingKeyboardEvent(KeyboardEvent* evt) {
   auto* edit_context =
       GetFrame().GetInputMethodController().GetActiveEditContext();
   if (edit_context) {
+    if (DispatchBeforeInputInsertText(evt->target()->ToNode(),
+                                      key_event->text) !=
+        DispatchEventResult::kNotCanceled)
+      return true;
+
     WebString text(WTF::String(key_event->text));
     edit_context->InsertText(text);
     return true;
