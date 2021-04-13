f004e37a876fd8ee56e2eaba9c73859f911eca6f
Guillaume Jenkins <gujen@google.com>
2021-04-13 15:37:16 +0000

    [iOS FRE] Add new FRE shared view controller
    Creates the base view controller that will be used by the various new
    FRE screens. Also adds an entry to Showcase that uses this new view
    controller for a sample screen.
    
    Note that this only addresses the vertical iPhone scenario. Constraints
    for other layouts, and dynamically applying the correct ones, will come
    in a future change.
    
    Bug: 1186762
    Change-Id: I432b735256abb2b6bb5c89016bd0518844b06a30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2797393
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#871946}

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -174,6 +174,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       kClassForInstantiationKey : @"SCDefaultBrowserFullscreenPromoCoordinator",
       showcase::kUseCaseKey : @"Default Browser Fullscreen Promo UI",
     },
+    @{
+      showcase::kClassForDisplayKey : @"SCFirstRunHeroScreenViewController",
+      showcase::
+      kClassForInstantiationKey : @"SCFirstRunHeroScreenViewController",
+      showcase::kUseCaseKey : @"New First Run Experience example hero screen",
+    },
   ];
 }
 
---
+++ ios/showcase/first_run/sc_first_run_hero_screen_view_controller.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_SHOWCASE_FIRST_RUN_SC_FIRST_RUN_HERO_SCREEN_VIEW_CONTROLLER_H_
+#define IOS_SHOWCASE_FIRST_RUN_SC_FIRST_RUN_HERO_SCREEN_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/ui/first_run/first_run_screen_view_controller.h"
+
+// A view controller to showcase an example hero screen for the new first-run
+// experience.
+@interface SCFirstRunHeroScreenViewController : FirstRunScreenViewController
+
+@end
+
+#endif  // IOS_SHOWCASE_FIRST_RUN_SC_FIRST_RUN_HERO_SCREEN_VIEW_CONTROLLER_H_
