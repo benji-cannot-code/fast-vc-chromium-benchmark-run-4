3fd3cfa6f7943f7244a086ca21bdca807975e00f
Tina Wang <tinazwang@google.com>
2021-04-14 23:25:34 +0000

    🐻 [Pandora] Introduce Sign-in screen module
    Create a skeleton of the MVC component, with an empty VC and basic
    coordinator and mediator functions.
    
    Bug: 1186765
    Change-Id: If113a36f096db77ce6203f3b8934f6615d63a31a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2821048
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872612}

---
+++ ios/chrome/browser/ui/first_run/signin_screen_consumer.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_FIRST_RUN_SIGNIN_SCREEN_CONSUMER_H_
+#define IOS_CHROME_BROWSER_UI_FIRST_RUN_SIGNIN_SCREEN_CONSUMER_H_
+
+#import <Foundation/Foundation.h>
+
+// Handles sign-in screen UI updates.
+@protocol SigninScreenConsumer <NSObject>
+
+// Sets user image when there is a known account.
+- (void)setUserImage:(UIImage*)userImage;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_FIRST_RUN_SIGNIN_SCREEN_CONSUMER_H_
---
+++ ios/chrome/browser/ui/first_run/signin_screen_view_controller.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_FIRST_RUN_SIGNIN_SCREEN_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_FIRST_RUN_SIGNIN_SCREEN_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/ui/first_run/signin_screen_consumer.h"
+
+// Delegate of sign-in screen view controller.
+@protocol SigninScreenViewControllerDelegate <NSObject>
+
+// Called when the user taps to see the account picker.
+- (void)showAccountPicker;
+
+@end
+
+// View controller of sign-in screen.
+@interface SigninScreenViewController : UIViewController <SigninScreenConsumer>
+
+@property(nonatomic, weak) id<SigninScreenViewControllerDelegate> delegate;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_FIRST_RUN_SIGNIN_SCREEN_VIEW_CONTROLLER_H_
