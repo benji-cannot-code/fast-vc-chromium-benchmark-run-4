c1419dadf80d3aa1b18f695acbf18deca02b4f68
sauski <sauski@google.com>
2021-04-14 19:02:43 +0000

    [WebUI][Settings] Notification content page string
    Make the sub-string on the chrome://settings/content page respect the
    tri-state nature of the setting.
    
    Bug: 1113642
    Change-Id: Id36a4c85b92a4ff735afdb6a0037b5f8c408b935
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791425
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#872542}

--- chrome/test/data/webui/settings/site_settings_page_test.js
+++ chrome/test/data/webui/settings/site_settings_page_test.js
@@ -5,11 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // clang-format off
 import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {ContentSetting, defaultSettingLabel, SiteSettingsPrefsBrowserProxyImpl} from 'chrome://settings/lazy_load.js';
+import {ContentSetting, defaultSettingLabel, NotificationSetting, SiteSettingsPrefsBrowserProxyImpl} from 'chrome://settings/lazy_load.js';
 
 import {assertEquals, assertTrue} from '../chai_assert.js';
-import {eventToPromise, isChildVisible} from '../test_util.m.js';
+import {eventToPromise,flushTasks, isChildVisible} from '../test_util.m.js';
 
 import {TestSiteSettingsPrefsBrowserProxy} from './test_site_settings_prefs_browser_proxy.js';
 
@@ -32,6 +33,14 @@ suite('SiteSettingsPage', function() {
     document.body.innerHTML = '';
     page = /** @type {!SettingsSiteSettingsPageElement} */ (
         document.createElement('settings-site-settings-page'));
+    page.prefs = {
+      generated: {
+        notification: {
+          type: chrome.settingsPrivate.PrefType.NUMBER,
+          value: NotificationSetting.ASK,
+        },
+      },
+    };
     document.body.appendChild(page);
     flush();
   }
@@ -69,6 +78,56 @@ suite('SiteSettingsPage', function() {
 
     webUIListenerCallback('cookieSettingDescriptionChanged', testLabels[1]);
     assertEquals(testLabels[1], cookiesLinkRow.subLabel);
+  });
+
+  test('NotificationsLinkRowSublabel_RedesignDisabled', async function() {
+    loadTimeData.overrideValues({
+      enableContentSettingsRedesign: false,
+    });
+
+    const notificationsLinkRow = /** @type {!CrLinkRowElement} */ (
+        page.$$('#basicPermissionsList').$$('#notifications'));
+
+    page.set('prefs.generated.notification.value', NotificationSetting.ASK);
+    await flushTasks();
+    assertEquals(
+        loadTimeData.getString('siteSettingsAskBeforeSending'),
+        notificationsLinkRow.subLabel);
+
+    page.set('prefs.generated.notification.value', NotificationSetting.BLOCK);
+    await flushTasks();
+    assertEquals(
+        loadTimeData.getString('siteSettingsBlocked'),
+        notificationsLinkRow.subLabel);
+  });
+
+  test('NotificationsLinkRowSublabel_RedesignEnabled', async function() {
+    loadTimeData.overrideValues({
+      enableContentSettingsRedesign: true,
+    });
+
+    const notificationsLinkRow = /** @type {!CrLinkRowElement} */ (
+        page.$$('#basicPermissionsList').$$('#notifications'));
+
+    page.set(
+        'prefs.generated.notification.value',
+        NotificationSetting.QUIETER_MESSAGING);
+    await flushTasks();
+    assertEquals(
+        loadTimeData.getString('siteSettingsNotificationsPartial'),
+        notificationsLinkRow.subLabel);
+
+    page.set('prefs.generated.notification.value', NotificationSetting.ASK);
+    await flushTasks();
+    assertEquals(
+        loadTimeData.getString('siteSettingsNotificationsAllowed'),
+        notificationsLinkRow.subLabel);
+
+    page.set('prefs.generated.notification.value', NotificationSetting.BLOCK);
+    await flushTasks();
+    assertEquals(
+        loadTimeData.getString('siteSettingsNotificationsBlocked'),
+        notificationsLinkRow.subLabel);
   });
 
   test('ProtectedContentRow', function() {
