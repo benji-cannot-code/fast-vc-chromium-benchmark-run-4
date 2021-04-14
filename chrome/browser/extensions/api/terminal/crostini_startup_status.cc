1032614f240d3c9116b00dc94ba3831dfdab543d
David Munro <davidmunro@google.com>
2021-04-14 04:41:51 +0000

    crostini terminal: Fix AppLaunch metric and split by type
    For GUI apps crostini_util is responsible for launching Crostini and
    going through Cicerone to launch the application. For the terminal
    however, the terminal itself does so. Accordingly, we let the terminal
    emit app launch metrics for the terminal and crostini_util does so for
    GUI apps. This fixes a bug where crostini_util would always consider
    launching the terminal to succeed.
    Additionally, add new metrics to break down app launch success rates by
    terminal vs GUI and extend the AppLaunch metric to track all terminal
    launches instead of just those from the launcher and files app.
    
    Bug: 1188992
    Test: Unit, browser, deploy and check histograms
    Change-Id: I26e0fa52fa21ba0c25af0d7caf9ba5037d0c8d67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2783542
    Commit-Queue: David Munro <davidmunro@google.com>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Jason Lin <lxj@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872279}

--- chrome/browser/extensions/api/terminal/crostini_startup_status.cc
+++ chrome/browser/extensions/api/terminal/crostini_startup_status.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/system/sys_info.h"
 #include "base/time/time.h"
+#include "chrome/browser/chromeos/crostini/crostini_util.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/dbus/util/version_loader.h"
 #include "components/version_info/version_info.h"
@@ -54,6 +55,8 @@ CrostiniStartupStatus::~CrostiniStartupStatus() = default;
 
 void CrostiniStartupStatus::OnCrostiniRestarted(
     crostini::CrostiniResult result) {
+  crostini::RecordAppLaunchResultHistogram(
+      crostini::CrostiniAppLaunchAppType::kTerminal, result);
   if (result != crostini::CrostiniResult::SUCCESS) {
     PrintAfterStage(
         kColor1RedBright,
@@ -87,7 +90,6 @@ void CrostiniStartupStatus::ShowProgressAtInterval() {
 }
 
 void CrostiniStartupStatus::OnStageStarted(InstallerState stage) {
-  stage_ = stage;
   stage_index_ = static_cast<int>(stage) + 1;
   if (!verbose_) {
     return;
