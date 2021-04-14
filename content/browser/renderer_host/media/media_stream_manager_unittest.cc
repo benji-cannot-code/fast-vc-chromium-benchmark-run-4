261fc44d55f838bd40633509520a413da950e2c3
Guido Urdaneta <guidou@chromium.org>
2021-04-14 21:59:55 +0000

    [MediaStream] Close audio streams when the session is closed
    This CL makes audio tracks end when an audio session is closed
    on the browser process.
    This change ensures behavior consistency with video tracks.
    Prior to this CL, there were cases where a video track ended but
    an audio track in the same situation did not (e.g., when a device
    is suspended/goes to sleep).
    Unlike for video tracks, the AudioInputStreamManager object that
    manage sessions in the browser process has no way to directly
    stop streams. So, to achieve the same effect, we use the stop
    callback in the audio case.
    
    Bug: 1198358
    Change-Id: I8ba87c579023cb415efc4cb45bc06ba50c506387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2824268
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872585}

--- content/browser/renderer_host/media/media_stream_manager_unittest.cc
+++ content/browser/renderer_host/media/media_stream_manager_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
+#include "base/test/mock_callback.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "content/browser/renderer_host/media/media_stream_manager.h"
@@ -259,7 +260,8 @@ class MediaStreamManagerTest : public ::testing::Test {
     MediaStreamManager::GenerateStreamCallback generate_stream_callback =
         base::BindOnce(GenerateStreamCallback, &run_loop_, request_audio,
                        true /* request_video */, &audio_device, &video_device);
-    MediaStreamManager::DeviceStoppedCallback stopped_callback;
+    base::MockCallback<MediaStreamManager::DeviceStoppedCallback>
+        stopped_callback;
     MediaStreamManager::DeviceChangedCallback changed_callback;
     MediaStreamManager::DeviceRequestStateChangeCallback
         request_state_change_callback;
@@ -287,7 +289,7 @@ class MediaStreamManagerTest : public ::testing::Test {
         StreamSelectionInfo::New(
             blink::mojom::StreamSelectionStrategy::SEARCH_BY_DEVICE_ID,
             base::nullopt),
-        std::move(generate_stream_callback), std::move(stopped_callback),
+        std::move(generate_stream_callback), stopped_callback.Get(),
         std::move(changed_callback), std::move(request_state_change_callback));
     run_loop_.Run();
 
@@ -311,9 +313,14 @@ class MediaStreamManagerTest : public ::testing::Test {
           OnMediaRequestStateChanged(
               _, _, _, _, blink::mojom::MediaStreamType::DISPLAY_AUDIO_CAPTURE,
               MEDIA_REQUEST_STATE_CLOSING));
+      blink::MediaStreamDevice device;
+      EXPECT_CALL(stopped_callback, Run(_, _))
+          .WillOnce(testing::SaveArg<1>(&device));
       media_stream_manager_->StopStreamDevice(
           render_process_id, render_frame_id, requester_id, audio_device.id,
           audio_device.session_id());
+      EXPECT_EQ(device.type,
+                blink::mojom::MediaStreamType::DISPLAY_AUDIO_CAPTURE);
     }
   }
 
