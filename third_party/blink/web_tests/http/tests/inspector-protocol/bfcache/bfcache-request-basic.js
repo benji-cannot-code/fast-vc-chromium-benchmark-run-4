5230d236094f778302e05fbb72e7b34e3ca2244c
Sigurd Schneider <sigurds@chromium.org>
2021-04-14 06:05:55 +0000

    Report navigation type in CDP Page.frameNavigated event
    Change-Id: I48659b627177572e36a9c6b50f0ec4c5cbedcac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784921
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872295}

--- third_party/blink/web_tests/http/tests/inspector-protocol/bfcache/bfcache-request-basic.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/bfcache/bfcache-request-basic.js
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Navigate back - should use back-forward cache.
   session.evaluate('window.history.back()');
-  await dp.Page.onceFrameNavigated();
+  const frameNavigated = await dp.Page.onceFrameNavigated();
+  testRunner.log(frameNavigated.params.type);
   testRunner.completeTest();
 });
