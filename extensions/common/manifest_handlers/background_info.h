c8cb7b63be1418ba95e75ecfac5c9a50b316cba2
Ghazale Hosseinabadi <ghazale@google.com>
2021-04-14 05:24:35 +0000

    [Extensions] Enable extensions to register module service workers
    This CL enables extension developers to use module service workers.
    Service worker type is set to classic by default. For a module
    service worker, background.type should be set to module in the
    manifest file.
    
    Bug: 1194681
    Change-Id: Ic788782cd5d3bf3d84353a627142f39450171193
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809321
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872291}

--- extensions/common/manifest_handlers/background_info.h
+++ extensions/common/manifest_handlers/background_info.h
@@ -18,6 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
+enum class BackgroundServiceWorkerType {
+  kClassic,
+  kModule,
+};
+
 class BackgroundInfo : public Extension::ManifestData {
  public:
   BackgroundInfo();
@@ -27,6 +32,8 @@ class BackgroundInfo : public Extension::ManifestData {
   static const std::vector<std::string>& GetBackgroundScripts(
       const Extension* extension);
   static const std::string& GetBackgroundServiceWorkerScript(
+      const Extension* extension);
+  static BackgroundServiceWorkerType GetBackgroundServiceWorkerType(
       const Extension* extension);
   static bool HasBackgroundPage(const Extension* extension);
   static bool HasPersistentBackgroundPage(const Extension* extension);
@@ -76,6 +83,9 @@ class BackgroundInfo : public Extension::ManifestData {
 
   // Optional service worker based background script.
   base::Optional<std::string> background_service_worker_script_;
+
+  // Optional service worker based background type.
+  base::Optional<BackgroundServiceWorkerType> background_service_worker_type_;
 
   // True if the background page should stay loaded forever; false if it should
   // load on-demand (when it needs to handle an event). Defaults to true.
