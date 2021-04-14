9763ecdb87331ea1c22267c8d955652b2c0d88be
ckitagawa <ckitagawa@chromium.org>
2021-04-14 00:22:36 +0000

    [Paint Preview] Limit image size independently of capture size
    Presently, the max capture size of an SkPicture is also used to
    determine the max decoded image size for image discarding logic. This
    was fine when all clients tolerated discarding images. However, there
    are now some clients (tall screenshots) where this is not acceptable
    behavior. Tall screenshots should still be able to impose a limit on
    total capture size to avoid OOMs, but that feature cannot tolerate image
    discarding as the screenshot should be full fidelity if it succeeds.
    
    Additionally, this intentionally fails the serialization when images
    stop serializing if they would exceed the total permitted capture size
    and the client is not tolerant to image discarding. This differs subtly
    from the prior behavior which could succeed if the rest of the
    SkPicture + the images that did successfully serialize fit within the
    size limit.
    
    Bug: 1198304
    Change-Id: I4fba33f9345a789256cbe5fe42ddbfeaf616d898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2821637
    Reviewed-by: Yashar Dabiran <yashard@chromium.org>
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872203}

--- components/paint_preview/browser/paint_preview_base_service_unittest.cc
+++ components/paint_preview/browser/paint_preview_base_service_unittest.cc
@@ -179,7 +179,8 @@ class PaintPreviewBaseServiceTest
       RecordingPersistence persistence,
       gfx::Rect clip_rect,
       bool capture_links,
-      size_t max_per_capture_size) {
+      size_t max_per_capture_size,
+      uint64_t max_decoded_image_size_bytes) {
     PaintPreviewBaseService::CaptureParams capture_params;
     capture_params.web_contents = web_contents;
     capture_params.root_dir = root_dir;
@@ -187,6 +188,7 @@ class PaintPreviewBaseServiceTest
     capture_params.clip_rect = clip_rect;
     capture_params.capture_links = capture_links;
     capture_params.max_per_capture_size = max_per_capture_size;
+    capture_params.max_decoded_image_size_bytes = max_decoded_image_size_bytes;
     return capture_params;
   }
 
@@ -201,6 +203,7 @@ TEST_P(PaintPreviewBaseServiceTest, CaptureMainFrame) {
   params->clip_rect = gfx::Rect(0, 0, 0, 0);
   params->is_main_frame = true;
   params->max_capture_size = 50;
+  params->max_decoded_image_size_bytes = 1000;
   recorder.SetExpectedParams(std::move(params));
   auto response = mojom::PaintPreviewCaptureResponse::New();
   response->embedding_token = base::nullopt;
@@ -219,7 +222,7 @@ TEST_P(PaintPreviewBaseServiceTest, CaptureMainFrame) {
   base::RunLoop loop;
   service->CapturePaintPreview(
       CreateCaptureParams(web_contents(), &path, GetParam(),
-                          gfx::Rect(0, 0, 0, 0), true, 50),
+                          gfx::Rect(0, 0, 0, 0), true, 50, 1000),
       base::BindOnce(
           [](base::OnceClosure quit_closure,
              PaintPreviewBaseService::CaptureStatus expected_status,
@@ -287,7 +290,8 @@ TEST_P(PaintPreviewBaseServiceTest, CaptureFailed) {
   base::RunLoop loop;
   service->CapturePaintPreview(
       CreateCaptureParams(web_contents(), &path, GetParam(),
-                          gfx::Rect(0, 0, 0, 0), true, 0),
+                          gfx::Rect(0, 0, 0, 0), true, 0,
+                          std::numeric_limits<uint64_t>::max()),
       base::BindOnce(
           [](base::OnceClosure quit_closure,
              PaintPreviewBaseService::CaptureStatus expected_status,
@@ -323,7 +327,8 @@ TEST_P(PaintPreviewBaseServiceTest, CaptureDisallowed) {
   base::RunLoop loop;
   service->CapturePaintPreview(
       CreateCaptureParams(web_contents(), &path, GetParam(),
-                          gfx::Rect(0, 0, 0, 0), true, 0),
+                          gfx::Rect(0, 0, 0, 0), true, 0,
+                          std::numeric_limits<uint64_t>::max()),
       base::BindOnce(
           [](base::OnceClosure quit_closure,
              PaintPreviewBaseService::CaptureStatus expected_status,
--- components/paint_preview/browser/paint_preview_client.cc
+++ components/paint_preview/browser/paint_preview_client.cc
@@ -120,6 +120,8 @@ mojom::PaintPreviewCaptureParamsPtr CreateRecordingRequestParams(
   mojo_params->is_main_frame = capture_params.is_main_frame;
   mojo_params->file = std::move(file);
   mojo_params->max_capture_size = capture_params.max_capture_size;
+  mojo_params->max_decoded_image_size_bytes =
+      capture_params.max_decoded_image_size_bytes;
   return mojo_params;
 }
 
@@ -314,6 +316,8 @@ void PaintPreviewClient::CapturePaintPreview(
   document_data.accepted_tokens = CreateAcceptedTokenList(render_frame_host);
   document_data.capture_links = params.inner.capture_links;
   document_data.max_per_capture_size = params.inner.max_capture_size;
+  document_data.max_decoded_image_size_bytes =
+      params.inner.max_decoded_image_size_bytes;
   all_document_data_.insert(
       {params.inner.document_guid, std::move(document_data)});
   TRACE_EVENT_NESTABLE_ASYNC_BEGIN0(
@@ -338,6 +342,7 @@ void PaintPreviewClient::CaptureSubframePaintPreview(
   params.is_main_frame = false;
   params.capture_links = it->second.capture_links;
   params.max_capture_size = it->second.max_per_capture_size;
+  params.max_decoded_image_size_bytes = it->second.max_decoded_image_size_bytes;
   CapturePaintPreviewInternal(params, render_subframe_host);
 }
 
