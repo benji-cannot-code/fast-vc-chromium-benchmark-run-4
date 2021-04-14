c4e7ca49c269b98e6045dfaad1a6b4b2c78defd4
Dominic Mazzoni <dmazzoni@chromium.org>
2021-04-14 16:06:42 +0000

    Cache data about running Android accessibility services
    Move the code that iterates over running accessibility
    services to BrowserAccessibilityState, and optimize it
    by having it only iterate once and then cache the
    results until the set of running accessibility services
    changes.
    
    Bug: 931492
    AX-Relnotes: N/A
    Change-Id: Ieb569a5a3458e75671b511e3cb953fed42efed62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2824677
    Reviewed-by: Dan Elphick <delphick@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872461}

--- content/browser/accessibility/web_contents_accessibility_android.cc
+++ content/browser/accessibility/web_contents_accessibility_android.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/ranges.h"
 #include "content/browser/accessibility/browser_accessibility_android.h"
 #include "content/browser/accessibility/browser_accessibility_manager_android.h"
-#include "content/browser/accessibility/browser_accessibility_state_impl.h"
+#include "content/browser/accessibility/browser_accessibility_state_impl_android.h"
 #include "content/browser/accessibility/one_shot_accessibility_tree_search.h"
 #include "content/browser/android/render_widget_host_connector.h"
 #include "content/browser/renderer_host/render_widget_host_view_android.h"
@@ -1535,9 +1535,11 @@ void WebContentsAccessibilityAndroid::CollectStats() {
   if (obj.is_null())
     return;
 
-  int event_type_mask =
-      Java_WebContentsAccessibilityImpl_getAccessibilityServiceEventTypeMask(
-          env, obj);
+  BrowserAccessibilityStateImplAndroid* accessibility_state =
+      static_cast<BrowserAccessibilityStateImplAndroid*>(
+          BrowserAccessibilityStateImpl::GetInstance());
+
+  int event_type_mask = accessibility_state->GetServiceEventTypeMask();
   EVENT_TYPE_HISTOGRAM(event_type_mask, ANNOUNCEMENT);
   EVENT_TYPE_HISTOGRAM(event_type_mask, ASSIST_READING_CONTEXT);
   EVENT_TYPE_HISTOGRAM(event_type_mask, GESTURE_DETECTION_END);
@@ -1565,9 +1567,7 @@ void WebContentsAccessibilityAndroid::CollectStats() {
   EVENT_TYPE_HISTOGRAM(event_type_mask, WINDOW_CONTENT_CHANGED);
   EVENT_TYPE_HISTOGRAM(event_type_mask, WINDOW_STATE_CHANGED);
 
-  int feedback_type_mask =
-      Java_WebContentsAccessibilityImpl_getAccessibilityServiceFeedbackTypeMask(
-          env, obj);
+  int feedback_type_mask = accessibility_state->GetServiceFeedbackTypeMask();
   FEEDBACK_TYPE_HISTOGRAM(feedback_type_mask, SPOKEN);
   FEEDBACK_TYPE_HISTOGRAM(feedback_type_mask, HAPTIC);
   FEEDBACK_TYPE_HISTOGRAM(feedback_type_mask, AUDIBLE);
@@ -1575,9 +1575,7 @@ void WebContentsAccessibilityAndroid::CollectStats() {
   FEEDBACK_TYPE_HISTOGRAM(feedback_type_mask, GENERIC);
   FEEDBACK_TYPE_HISTOGRAM(feedback_type_mask, BRAILLE);
 
-  int flags_mask =
-      Java_WebContentsAccessibilityImpl_getAccessibilityServiceFlagsMask(env,
-                                                                         obj);
+  int flags_mask = accessibility_state->GetServiceFlagsMask();
   FLAGS_HISTOGRAM(flags_mask, INCLUDE_NOT_IMPORTANT_VIEWS);
   FLAGS_HISTOGRAM(flags_mask, REQUEST_TOUCH_EXPLORATION_MODE);
   FLAGS_HISTOGRAM(flags_mask, REQUEST_ENHANCED_WEB_ACCESSIBILITY);
@@ -1586,9 +1584,7 @@ void WebContentsAccessibilityAndroid::CollectStats() {
   FLAGS_HISTOGRAM(flags_mask, RETRIEVE_INTERACTIVE_WINDOWS);
   FLAGS_HISTOGRAM(flags_mask, FORCE_DIRECT_BOOT_AWARE);
 
-  int capabilities_mask =
-      Java_WebContentsAccessibilityImpl_getAccessibilityServiceCapabilitiesMask(
-          env, obj);
+  int capabilities_mask = accessibility_state->GetServiceCapabilitiesMask();
   CAPABILITY_TYPE_HISTOGRAM(capabilities_mask, CAN_RETRIEVE_WINDOW_CONTENT);
   CAPABILITY_TYPE_HISTOGRAM(capabilities_mask, CAN_REQUEST_TOUCH_EXPLORATION);
   CAPABILITY_TYPE_HISTOGRAM(capabilities_mask,
@@ -1597,8 +1593,7 @@ void WebContentsAccessibilityAndroid::CollectStats() {
   CAPABILITY_TYPE_HISTOGRAM(capabilities_mask, CAN_CONTROL_MAGNIFICATION);
   CAPABILITY_TYPE_HISTOGRAM(capabilities_mask, CAN_PERFORM_GESTURES);
 
-  auto service_ids =
-      Java_WebContentsAccessibilityImpl_getAccessibilityServiceIds(env, obj);
+  auto service_ids = accessibility_state->GetServiceIds();
   jsize len = env->GetArrayLength(service_ids.obj());
   bool has_assistive_tech = false;
   bool has_password_manager = false;
