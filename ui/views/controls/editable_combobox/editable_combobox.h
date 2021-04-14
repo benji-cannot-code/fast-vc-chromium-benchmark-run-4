c3a57a6795f6395639f41597533066c49e94ed04
Mohamed Amir Yosef <mamir@chromium.org>
2021-04-14 17:20:15 +0000

    [UI] Properly Support filter-on-edit for EditableCombobox with icons.
    Support for icons in EditableCombobox has been recently added in
    https://crrev.com/c/2810645.
    However, the filter-on-edit feature isn't compatible with icons.
    This CL adds such comptaiblitly.
    Precisely:
    
    Before this CL:
    The dropdown menu icons didn't respect the filtered list of items and
    were displaying the icons in the original model before filtering
    
    After this CL:
    The icons in the filtered model are displayed instead.
    
    In order to test this behavior, SimpleComboboxModel is also being
    modified to support icons. This will allow testing the new behavior.
    In addition, follow up CLs will make use of SimpleComboboxModel
    instead of home-grown combobox models.
    
    Bug: 1167060
    Change-Id: Ic20a5d4a45db1724e0ebb57b90aca5c35e2e9d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2811540
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872499}

--- ui/views/controls/editable_combobox/editable_combobox.h
+++ ui/views/controls/editable_combobox/editable_combobox.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/scoped_observation.h"
 #include "build/build_config.h"
+#include "ui/base/models/image_model.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/views/controls/textfield/textfield_controller.h"
 #include "ui/views/layout/animating_layout_manager.h"
@@ -109,6 +110,7 @@ class VIEWS_EXPORT EditableCombobox
   ui::ComboboxModel* GetComboboxModelForTest() { return combobox_model_.get(); }
   int GetItemCountForTest();
   std::u16string GetItemForTest(int index);
+  ui::ImageModel GetIconForTest(int index);
   MenuRunner* GetMenuRunnerForTest() { return menu_runner_.get(); }
   Textfield* GetTextfieldForTest() { return textfield_; }
 
