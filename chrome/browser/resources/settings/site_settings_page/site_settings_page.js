c61a64712f1ed4fb721a77f007861a21f3f24865
Sean Harrison <harrisonsean@chromium.org>
2021-04-14 15:13:41 +0000

    [WebUI][Settings] Fix content settings lists and erroneous strings
    The HID strings have been swapped for the correct device use strings
    on chrome://settings/content.
    
    The content settings list has been updated to include all supported
    settings so that all settings behave properly after the redesign.
    
    Bug: 1198096, 1198101
    Change-Id: I3afd0a14b25258a4b9e5705f27d96e01184cc8b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2825452
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872437}

--- chrome/browser/resources/settings/site_settings_page/site_settings_page.js
+++ chrome/browser/resources/settings/site_settings_page/site_settings_page.js
@@ -163,9 +163,9 @@ function getCategoryItemMap() {
       id: Id.IDLE_DETECTION,
       label: 'siteSettingsIdleDetection',
       icon: 'settings:devices',
-      enabledLabel: redesignEnabled ? 'siteSettingsHidDevicesAllowed' :
+      enabledLabel: redesignEnabled ? 'siteSettingsDeviceUseAllowed' :
                                       'siteSettingsIdleDetectionAsk',
-      disabledLabel: redesignEnabled ? 'siteSettingsHidDevicesBlocked' :
+      disabledLabel: redesignEnabled ? 'siteSettingsDeviceUseBlocked' :
                                        'siteSettingsIdleDetectionBlock',
     },
     {
