d1d9dcee945560db8df5dc541f7147792444066c
Vikas Soni <vikassoni@chromium.org>
2021-04-14 19:30:17 +0000

    [Dr-Dc]:Gpu Scheduler changes to support multiple threads.
    Dr-Dc a.k.a direct-rendering display compositor enables use of
    2 gpu threads instead of 1 gpu main thread.
    
    This change is to update gpu scheduler to handle multiple gpu threads.
    Primary changes are :
    1. A sequence can be created on any thread.
    2. Each thread will have its own priority queue to schedule sequences
    created on that thread.
    3. GpuInProcessThreadService::CreateSequence() can be called from viz
    thread so make sure its on the gpu thread.
    4. Update all scheduler unit tests as well as all affected unit tests
    due to scheduler changes.
    
    This change should not affect any functional or performance changes
    to current/legacy features.
    
    Bug: 1186278
    Change-Id: Icfa9277b907a59b744e2598f2d057c3389616932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2748416
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872554}

--- components/viz/service/display_embedder/skia_output_surface_dependency_impl.cc
+++ components/viz/service/display_embedder/skia_output_surface_dependency_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/viz/service/gl/gpu_service_impl.h"
 #include "gpu/command_buffer/service/scheduler.h"
+#include "gpu/ipc/command_buffer_task_executor.h"
 #include "gpu/ipc/gpu_task_scheduler_helper.h"
 #include "gpu/ipc/scheduler_sequence.h"
 #include "gpu/ipc/service/image_transport_surface.h"
@@ -22,8 +23,10 @@ namespace viz {
 
 SkiaOutputSurfaceDependencyImpl::SkiaOutputSurfaceDependencyImpl(
     GpuServiceImpl* gpu_service_impl,
+    gpu::CommandBufferTaskExecutor* gpu_task_executor,
     gpu::SurfaceHandle surface_handle)
     : gpu_service_impl_(gpu_service_impl),
+      gpu_task_executor_(gpu_task_executor),
       surface_handle_(surface_handle),
       client_thread_task_runner_(base::ThreadTaskRunnerHandle::Get()) {}
 
@@ -31,8 +34,8 @@ SkiaOutputSurfaceDependencyImpl::~SkiaOutputSurfaceDependencyImpl() = default;
 
 std::unique_ptr<gpu::SingleTaskSequence>
 SkiaOutputSurfaceDependencyImpl::CreateSequence() {
-  return std::make_unique<gpu::SchedulerSequence>(
-      gpu_service_impl_->GetGpuScheduler());
+  // Using the |gpu_task_executor_| to create sequence on the gpu main thread.
+  return gpu_task_executor_->CreateSequence();
 }
 
 gpu::SharedImageManager*
