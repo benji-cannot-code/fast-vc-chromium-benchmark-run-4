db9154c03be86385b3dd2862472d6aa9e83d914e
mlcui <mlcui@google.com>
2021-04-14 22:58:58 +0000

    Remove all remaining references to osLanguages/{details,inputMethods}
    As the routes were fully removed in WebUI (see [1] and [2]), we can
    safely remove all remaining references to them in C++ and Mojo files.
    
    [1]: https://crrev.com/c/2822274
    [2]: https://crrev.com/c/2796860
    
    Bug: 1097328
    Change-Id: Ia2d24637477a01a0f00b97dfa3367344ad45a20d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822403
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/master@{#872602}

--- chrome/browser/ui/webui/settings/chromeos/languages_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/languages_section.cc
@@ -464,13 +464,6 @@ void LanguagesSection::RegisterHierarchy(HierarchyGenerator* generator) const {
       mojom::SearchResultIcon::kGlobe, mojom::SearchResultDefaultRank::kMedium,
       mojom::kEditDictionarySubpagePath);
 
-  // Languages and input details.
-  generator->RegisterTopLevelSubpage(
-      IDS_OS_SETTINGS_LANGUAGES_AND_INPUT_PAGE_TITLE,
-      mojom::Subpage::kLanguagesAndInputDetails,
-      mojom::SearchResultIcon::kGlobe, mojom::SearchResultDefaultRank::kMedium,
-      mojom::kLanguagesAndInputDetailsSubpagePath);
-
   generator->RegisterNestedSetting(mojom::Setting::kAddLanguage,
                                    mojom::Subpage::kLanguages);
   generator->RegisterNestedSetting(mojom::Setting::kShowInputOptionsInShelf,
@@ -482,14 +475,6 @@ void LanguagesSection::RegisterHierarchy(HierarchyGenerator* generator) const {
       mojom::Subpage::kInputMethodOptions, mojom::Subpage::kInput,
       mojom::SearchResultIcon::kGlobe, mojom::SearchResultDefaultRank::kMedium,
       mojom::kInputMethodOptionsSubpagePath);
-
-  // Manage input methods.
-  generator->RegisterNestedSubpage(
-      IDS_SETTINGS_LANGUAGES_MANAGE_INPUT_METHODS_TITLE,
-      mojom::Subpage::kManageInputMethods,
-      mojom::Subpage::kLanguagesAndInputDetails,
-      mojom::SearchResultIcon::kGlobe, mojom::SearchResultDefaultRank::kMedium,
-      mojom::kManageInputMethodsSubpagePath);
 
   // Smart inputs.
   generator->RegisterTopLevelSubpage(
