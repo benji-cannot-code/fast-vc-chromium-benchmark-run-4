c70cd60ab7028ce37707e58295e0d0a42f1c734d
Sergey Ulanov <sergeyu@chromium.org>
2021-04-14 16:04:39 +0000

    Add command line flag to set GPU watchdog timeout
    Added --gpu-watchdog-timeout-seconds flag that allows to override GPU
    watchdog timeout.
    Also
    1. Restored the timeout used on Fuchsia to the default value of 15s.
    2. Updated blink web tests to set the new flag to 60s on Fuchsia.
    
    Bug: 1185119
    Change-Id: I98c30cbe4e74d021bf35c6a8543da34e1b45443a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2823729
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872460}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -301,6 +301,7 @@ static const char* const kSwitchNames[] = {
 #endif
     switches::kGpuBlocklistTestGroup,
     switches::kGpuDriverBugListTestGroup,
+    switches::kGpuWatchdogTimeoutSeconds,
     switches::kUseCmdDecoder,
     switches::kForceVideoOverlays,
 #if defined(OS_ANDROID)
