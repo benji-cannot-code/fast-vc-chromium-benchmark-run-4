0da3dee97eb97317fd43a024a360a23da52e3b9c
Rushan Suleymanov <rushans@google.com>
2021-04-14 08:04:18 +0000

    [Sync] Do not use UpdatedProgressMarkerChecker in SyncSetup
    Sync integration tests rely on UpdatedProgressMarkerChecker to verify
    that all local changes have been committed. However, the checker relies
    on self invalidations to verify that the last commit had no entities.
    
    In some cases when invalidations are disabled for some data types (e.g.
    for commit-only data types) it may happen that there won't be an
    additional sync cycle and the checker will wait forever. It leads to
    test flakiness.
    
    This CL replaces UpdatedProgressMarkerChecker with a new checker which
    verifies that there are no new changes after the last sync cycle. It
    changes behaviour in the way that the last sync cycle may has not been
    completed after SyncSetup() call (this cycle should have empty commit
    request). But this behaviour is a bit closer to the real scenarios.
    
    The next step may be turning off self invalidations in tests which don't
    use UpdatedProgressMarkerChecker explicitly.
    
    Bug: 1188034
    Change-Id: I5b5713f137aa6c35b88bb5cc6893e7e50e0d171a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2818158
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872321}

--- chrome/browser/sync/test/integration/sync_test.cc
+++ chrome/browser/sync/test/integration/sync_test.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/sync/sync_invalidations_service_factory.h"
+#include "chrome/browser/sync/test/integration/committed_all_nudged_changes_checker.h"
 #include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
 #include "chrome/browser/sync/test/integration/single_client_status_change_checker.h"
 #include "chrome/browser/sync/test/integration/sync_datatype_helper.h"
@@ -310,11 +311,6 @@ SyncTest::SyncTest(TestType test_type)
       server_type_(SERVER_TYPE_UNDECIDED),
       previous_profile_(nullptr),
       num_clients_(-1) {
-  // TODO(crbug.com/1153297): This is a workaround to mitigate flakiness. Remove
-  // once the issue is resolved.
-  feature_list_.InitAndDisableFeature(
-      federated_learning::kFlocIdComputedEventLogging);
-
   sync_datatype_helper::AssociateWithTest(this);
   switch (test_type_) {
     case SINGLE_CLIENT: {
@@ -934,9 +930,11 @@ void SyncTest::SetupSyncInternal(SetupSyncMode setup_mode) {
         client->AwaitSyncSetupCompletion();
         break;
       case WAIT_FOR_COMMITS_TO_COMPLETE:
+        // TODO(crbug.com/1188034): remove the DCHECK.
         DCHECK(TestUsesSelfNotifications())
             << "We need that for the UpdatedProgressMarkerChecker";
-        UpdatedProgressMarkerChecker checker(GetSyncService(client_index));
+        client->AwaitSyncSetupCompletion();
+        CommittedAllNudgedChangesChecker checker(GetSyncService(client_index));
         checker.Wait();
         break;
     }
