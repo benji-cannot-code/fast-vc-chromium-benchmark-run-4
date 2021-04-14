ad4f4554b5424a39519457386ecb67da5400d3d1
Joel Hockey <joelhockey@chromium.org>
2021-04-14 06:12:39 +0000

    Replace UILockBubble with SubtleNotificationView used by chrome
    Reusing code to guarantee that we get the same visual look.
    One particular difference is to have the box around the accelerator
    name in the string "Press and hold [Esc] to exit full screen".
    
    Bug: 1198400
    Change-Id: I532e49d6c3d0a2b3c4dbce82c6ccd6125eeb90fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2823430
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Mitsuru Oshima (Slow - Gardening) <oshima@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872297}

--- components/fullscreen_control/subtle_notification_view.h
+++ components/fullscreen_control/subtle_notification_view.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_SUBTLE_NOTIFICATION_VIEW_H_
-#define CHROME_BROWSER_UI_VIEWS_SUBTLE_NOTIFICATION_VIEW_H_
+#ifndef COMPONENTS_FULLSCREEN_CONTROL_SUBTLE_NOTIFICATION_VIEW_H_
+#define COMPONENTS_FULLSCREEN_CONTROL_SUBTLE_NOTIFICATION_VIEW_H_
 
 #include <memory>
 #include <string>
@@ -49,4 +49,4 @@ class SubtleNotificationView : public views::View {
   InstructionView* instruction_view_;
 };
 
-#endif  // CHROME_BROWSER_UI_VIEWS_SUBTLE_NOTIFICATION_VIEW_H_
+#endif  // COMPONENTS_FULLSCREEN_CONTROL_SUBTLE_NOTIFICATION_VIEW_H_
