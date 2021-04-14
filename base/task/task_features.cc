52a2111696a9cb28dbdfe52d20b9b024a817067c
Olivier Li <olivierli@chromium.org>
2021-04-14 01:42:17 +0000

    Cleanup experiment code for AllTasksUserBlocking trial
    Bug: 757022
    Change-Id: I68eef97865145c3b1460f3defec95adae1c150fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2815840
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872233}

--- base/task/task_features.cc
+++ base/task/task_features.cc
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-const Feature kAllTasksUserBlocking{"AllTasksUserBlocking",
-                                    FEATURE_DISABLED_BY_DEFAULT};
-
 const Feature kNoDetachBelowInitialCapacity = {
     "NoDetachBelowInitialCapacity", base::FEATURE_DISABLED_BY_DEFAULT};
 
