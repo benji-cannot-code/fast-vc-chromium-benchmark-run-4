286a6cef0f9131ec832e5c42076828077730ebfc
Elly Fong-Jones <ellyjones@chromium.org>
2021-04-14 14:41:28 +0000

    extensions: mark icons as presentational
    These icons all have the same alt text ("Extension Icon" or
    "App Icon"). There's no point reading this text for a screenreader
    user - knowing that there is an icon there that they can't see has
    no value. It should just be marked with alt="" so it's skipped
    altogether, since it is purely presentational.
    
    This change affects the icons on both chrome://extensions/shortcuts
    and chrome://extensions.
    
    Bug: 1191700,1191692
    Change-Id: Id6f49c1e7f5af9a9192616ee9ed9e73979940113
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2818838
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872425}

--- chrome/browser/resources/extensions/kiosk_dialog.js
+++ chrome/browser/resources/extensions/kiosk_dialog.js
@@ -15,7 +15,6 @@ import {assert} from 'chrome://resources/js/assert.m.js';
 import {WebUIListenerBehavior} from 'chrome://resources/js/web_ui_listener_behavior.m.js';
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {ItemBehavior} from './item_behavior.js';
 import {KioskApp, KioskAppSettings, KioskBrowserProxy, KioskBrowserProxyImpl} from './kiosk_browser_proxy.js';
 
 
@@ -24,7 +23,7 @@ Polymer({
 
   _template: html`{__html_template__}`,
 
-  behaviors: [WebUIListenerBehavior, ItemBehavior],
+  behaviors: [WebUIListenerBehavior],
 
   properties: {
     /** @private {?string} */
