70627221d57408bb6cd86cf5e00c294eba6c3a8e
Renato Silva <rrsilva@google.com>
2021-04-14 18:25:00 +0000

    OOBE - ARC Terms of Service - Prevent crashes
    Prevent the browser from crashing when trying to load the
    ARC terms of service caused by trying to execute JS on a non-
    existing renderer.
    
    Bug: 1174691, 1180291
    Change-Id: I94bb8b0a86a7db271b0a74579039388b56fbf611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814926
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/master@{#872528}

--- chrome/browser/ui/webui/chromeos/login/arc_terms_of_service_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/arc_terms_of_service_screen_handler.cc
@@ -84,7 +84,12 @@ void ArcTermsOfServiceScreenHandler::MaybeLoadPlayStoreToS(
   if (!ignore_network_state && !default_network)
     return;
   const std::string country_code = base::CountryCodeForCurrentTimezone();
-  CallJS("login.ArcTermsOfServiceScreen.loadPlayStoreToS", country_code);
+  // TODO(crbug.com/1180291) - Remove once OOBE JS calls are fixed.
+  if (IsJavascriptAllowed()) {
+    CallJS("login.ArcTermsOfServiceScreen.loadPlayStoreToS", country_code);
+  } else {
+    LOG(ERROR) << "Silently dropping MaybeLoadPlayStoreToS request.";
+  }
 }
 
 void ArcTermsOfServiceScreenHandler::OnCurrentScreenChanged(
@@ -297,6 +302,7 @@ void ArcTermsOfServiceScreenHandler::Bind(ArcTermsOfServiceScreen* screen) {
 }
 
 void ArcTermsOfServiceScreenHandler::StartNetworkAndTimeZoneObserving() {
+  // TODO(crbug.com/1180291) - Clean up work. Fix this logic.
   if (network_time_zone_observing_)
     return;
 
