5c162b097bf580d22e6c93b481a42c1db3860af5
Moe Ahmadi <mahmadi@chromium.org>
2021-04-15 01:03:39 +0000

    [memories] Lazily load more Memories via infinite scrolling
    This CL introduces lazy loading of Memories (i.e., paging) via infinite
    scrolling in the memories landing page. Through which, this CL also
    defines the MemoriesService API as it pertains to querying the Memories.
    This API will be fully implemented in the follow up CLs.
    
    In order to test lazy loading of the Memories, MemoriesHandler now
    creates and returns multiple mock Memories, one for each day.
    
    Bug: 1180970, 1184884, 1184883
    Change-Id: I6cab71159bce7e36ff35dfbbf75ffc653907fc27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2816225
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872625}

--- components/history_clusters/core/memories_service.cc
+++ components/history_clusters/core/memories_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "components/history_clusters/core/memories_features.h"
+#include "components/history_clusters/core/memories_remote_model_helper.h"
 
 namespace history_clusters {
 
@@ -52,9 +53,10 @@ void MemoriesService::CompleteVisitIfReady(int64_t nav_id) {
   }
 }
 
-void MemoriesService::QueryMemories(const std::string& query,
-                                    MemoriesCallback callback) {
-  remote_model_helper_->GetMemories(visits_, std::move(callback));
+void MemoriesService::QueryMemories(mojom::QueryParamsPtr query_params,
+                                    QueryMemoriesCallback callback) {
+  remote_model_helper_->GetMemories(
+      visits_, base::BindOnce(std::move(callback), nullptr));
 }
 
 }  // namespace history_clusters
