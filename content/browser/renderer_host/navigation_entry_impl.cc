3f9e34acd98ca7cdd43f564f07ad7a84fa2cae1a
Rakina Zata Amni <rakina@chromium.org>
2021-04-15 03:35:12 +0000

    Stop using url_is_unreachable to check for error pages
    Currently we use DidCommitProvisionalLoadParams' url_is_unreachable to
    check if a navigation was for an error page in various places. This is
    actually wrong in some cases, e.g. loadDataWithBaseURL navigations will
    result in url_is_unreachable == true as well, even if the navigation
    succeeded.
    
    This CL changes the checks to check the value of the net error code
    saved in the NavigationRequest instead, which will only be true for
    actual error pages navigations. This CL also found that
    loadDataWithBaseURL navigations depend on the previous behavior in some
    cases e.g. saving committed origin, and adds TODO to remove these cases.
    
    See: https://groups.google.com/a/google.com/g/chrome-navigation/c/x4qaF1dmszI/m/66DUFhoQCgAJ
    
    Bug: 1131832, 1198406
    Change-Id: I04b27a47bcbe46bafe6fd852929bce0a935a6b6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814168
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872694}

--- content/browser/renderer_host/navigation_entry_impl.cc
+++ content/browser/renderer_host/navigation_entry_impl.cc
@@ -78,7 +78,7 @@ void RecursivelyGenerateFrameEntries(
       nullptr, GURL(state.url_string.value_or(std::u16string())),
       // TODO(nasko): Supply valid origin once the value is persisted across
       // session restore.
-      nullptr /* origin */,
+      base::nullopt /* origin */,
       Referrer(GURL(state.referrer.value_or(std::u16string())),
                state.referrer_policy),
       state.initiator_origin, std::vector<GURL>(),
@@ -334,7 +334,7 @@ NavigationEntryImpl::NavigationEntryImpl(
               std::move(instance),
               nullptr,
               url,
-              nullptr /* origin */,
+              base::nullopt /* origin */,
               referrer,
               initiator_origin,
               std::vector<GURL>(),
@@ -929,9 +929,8 @@ void NavigationEntryImpl::AddOrUpdateFrameEntry(
   // or unique name.
   auto frame_entry = base::MakeRefCounted<FrameNavigationEntry>(
       unique_name, item_sequence_number, document_sequence_number,
-      site_instance, std::move(source_site_instance), url,
-      base::OptionalOrNullptr(origin), referrer, initiator_origin,
-      redirect_chain, page_state, method, post_id,
+      site_instance, std::move(source_site_instance), url, origin, referrer,
+      initiator_origin, redirect_chain, page_state, method, post_id,
       std::move(blob_url_loader_factory), std::move(web_bundle_navigation_info),
       std::move(subresource_web_bundle_navigation_info),
       std::move(policy_container_policies));
