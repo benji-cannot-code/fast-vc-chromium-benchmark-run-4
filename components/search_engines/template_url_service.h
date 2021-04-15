ea48ec97ba240929d26924e4e6818941c9823bdb
Lei Zhang <thestig@chromium.org>
2021-04-15 19:04:42 +0000

    Do not initialize scoped_refptr to nullptr.
    scoped_refptr already starts out with a nullptr by default.
    This CL is mechanically generated with these 2 commands:
    
    git grep -l ' scoped_refptr.* = nullptr;$'
    sed -i 's/ scoped_refptr\([^*]*\) = nullptr;$/ scoped_refptr\1;/'
    
    Change-Id: Ia43e4e11a24eac7bb91c7c3554350dd5db146765
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827313
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872983}

--- components/search_engines/template_url_service.h
+++ components/search_engines/template_url_service.h
@@ -691,7 +691,7 @@ class TemplateURLService : public WebDataServiceConsumer,
 
   // ---------- Dependencies on other components ------------------------------
   // Service used to store entries.
-  scoped_refptr<KeywordWebDataService> web_data_service_ = nullptr;
+  scoped_refptr<KeywordWebDataService> web_data_service_;
 
   std::unique_ptr<TemplateURLServiceClient> client_;
 
