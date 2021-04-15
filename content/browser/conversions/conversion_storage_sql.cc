796f42fbae33e4e2eeb45898a3b8c1542976f4b1
Nan Lin <linnan@chromium.org>
2021-04-15 17:27:57 +0000

    Measure number of conversions and impressions deleted per clear operation
    What: This CL adds metrics to measure the number of conversions and impressions which can never be attributed due to being deleted by data clearing. We can calculate % impressions deleted by comparing this metrics to Conversions.RegisteredImpressionsPerPage.
    
    Bug: 1193709
    Change-Id: I9bd0ec6523c3f5e9a7b8dc49acd1b85f10105fad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822028
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872925}

--- content/browser/conversions/conversion_storage_sql.cc
+++ content/browser/conversions/conversion_storage_sql.cc
@@ -76,6 +76,16 @@ void RecordInitializationStatus(const ConversionStorageSql::InitStatus status) {
                                 ConversionStorageSql::InitStatus::kMaxValue);
 }
 
+void RecordImpressionsDeleted(int count) {
+  UMA_HISTOGRAM_COUNTS_1000(
+      "Conversions.ImpressionsDeletedInDataClearOperation", count);
+}
+
+void RecordReportsDeleted(int count) {
+  UMA_HISTOGRAM_COUNTS_1000("Conversions.ReportsDeletedInDataClearOperation",
+                            count);
+}
+
 }  // namespace
 
 // static
@@ -553,6 +563,9 @@ void ConversionStorageSql::ClearData(
       return;
   }
 
+  int num_conversions_deleted =
+      static_cast<int>(conversion_ids_to_delete.size());
+
   // Careful! At this point we can still have some vestigial entries in the DB.
   // For example, if an impression has two conversions, and one conversion is
   // deleted, the above logic will delete the impression as well, leaving the
@@ -567,6 +580,8 @@ void ConversionStorageSql::ClearData(
     delete_vestigial_statement.BindInt64(0, impression_id);
     if (!delete_vestigial_statement.Run())
       return;
+
+    num_conversions_deleted += db_->GetLastChangeCount();
   }
 
   if (!rate_limit_table_.ClearDataForImpressionIds(
@@ -577,7 +592,12 @@ void ConversionStorageSql::ClearData(
                                                     delete_end, filter))
     return;
 
-  transaction.Commit();
+  if (!transaction.Commit())
+    return;
+
+  RecordImpressionsDeleted(
+      static_cast<int>(unique_impression_ids_to_delete.size()));
+  RecordReportsDeleted(num_conversions_deleted);
 }
 
 void ConversionStorageSql::ClearAllDataInRange(base::Time delete_begin,
@@ -642,6 +662,8 @@ void ConversionStorageSql::ClearAllDataInRange(base::Time delete_begin,
   if (!delete_conversions_statement.Run())
     return;
 
+  int num_conversions_deleted = db_->GetLastChangeCount();
+
   if (!rate_limit_table_.ClearDataForImpressionIds(db_.get(),
                                                    impression_ids_to_delete))
     return;
@@ -650,7 +672,11 @@ void ConversionStorageSql::ClearAllDataInRange(base::Time delete_begin,
                                              delete_end))
     return;
 
-  transaction.Commit();
+  if (!transaction.Commit())
+    return;
+
+  RecordImpressionsDeleted(static_cast<int>(impression_ids_to_delete.size()));
+  RecordReportsDeleted(num_conversions_deleted);
 }
 
 void ConversionStorageSql::ClearAllDataAllTime() {
@@ -665,11 +691,22 @@ void ConversionStorageSql::ClearAllDataAllTime() {
       db_->GetCachedStatement(SQL_FROM_HERE, kDeleteAllImpressionsSql));
   if (!delete_all_conversions_statement.Run())
     return;
+
+  int num_conversions_deleted = db_->GetLastChangeCount();
+
   if (!delete_all_impressions_statement.Run())
     return;
+
+  int num_impressions_deleted = db_->GetLastChangeCount();
+
   if (!rate_limit_table_.ClearAllDataAllTime(db_.get()))
     return;
-  transaction.Commit();
+
+  if (!transaction.Commit())
+    return;
+
+  RecordImpressionsDeleted(num_impressions_deleted);
+  RecordReportsDeleted(num_conversions_deleted);
 }
 
 bool ConversionStorageSql::HasCapacityForStoringImpression(
