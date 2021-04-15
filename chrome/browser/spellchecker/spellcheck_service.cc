ee0c6e3cc90cb58e7257bc95b3d4dbe83f47bc04
mlcui <mlcui@google.com>
2021-04-15 23:41:16 +0000

    OSSettingsLanguages: Fix changing Accept-Languages modifying spellcheck
    We already removed the Accept-Languages hook in languages.js for LSV2
    Update 2, but there is another one in SpellcheckService (removed in this
    CL).
    
    Fixed: 1198391
    Change-Id: Icb70b27adb7dae17c1b1ba572a14e9ff094468c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822394
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/master@{#873106}

--- chrome/browser/spellchecker/spellcheck_service.cc
+++ chrome/browser/spellchecker/spellcheck_service.cc
@@ -45,6 +45,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "ui/base/l10n/l10n_util.h"
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/constants/ash_features.h"
+#endif
+
 #if defined(OS_WIN) && BUILDFLAG(USE_BROWSER_SPELLCHECKER)
 #include "base/task/post_task.h"
 #include "base/task/task_traits.h"
@@ -856,6 +860,13 @@ void SpellcheckService::OnUseSpellingServiceChanged() {
 }
 
 void SpellcheckService::OnAcceptLanguagesChanged() {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // Accept-Languages and spell check are decoupled in LSV2 Update 2.
+  if (base::FeatureList::IsEnabled(ash::features::kLanguageSettingsUpdate2)) {
+    return;
+  }
+#endif
+
   std::vector<std::string> accept_languages = GetNormalizedAcceptLanguages();
 
   StringListPrefMember dictionaries_pref;
