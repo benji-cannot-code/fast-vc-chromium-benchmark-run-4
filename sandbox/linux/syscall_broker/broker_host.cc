e61099740c62888bab10eb6df147819f0fabc77c
Matthew Denton <mpdenton@chromium.org>
2021-04-15 13:52:34 +0000

    Reland "Linux sandbox syscall broker: use struct kernel_stat"
    This reverts commit 23030dc650cdfa22631f25bef937905f27f06a2c.
    
    Original change's description:
    > Revert "Linux sandbox syscall broker: use struct kernel_stat"
    >
    > This reverts commit 784b0fcd8a3ca6bcd3acb9cfd624ec9cbbac2789.
    >
    > Reason for revert: Causing failure in 
    > Step "sandbox_linux_unittests" failing on builder "Linux ChromiumOS MSan Tests"
    > See crbug.com/1198480
    >
    > Original change's description:
    > > Linux sandbox syscall broker: use struct kernel_stat
    > >
    > > The struct stat used in libc is different (in size and field ordering)
    > > from the structure assumed by the Linux kernel. So, when emulating
    > > system calls, we need to use the struct definition the kernel expects.
    > >
    > > This CL adds linux_stat.h that includes definitions of the different
    > > kernel structs.
    > >
    > > Change-Id: I53cad35c2251dff0f6b7ea77528cfa58ef3cab4a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2780876
    > > Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    > > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#871767}
    >
    > Change-Id: Icbec38f2103c8424dec79ab1870b97c3e83f9361
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2821812
    > Auto-Submit: Victor Vianna <victorvianna@google.com>
    > Owners-Override: Victor Vianna <victorvianna@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/master@{#871882}
    
    Change-Id: I1f39bb5242961474def594ff7dbea52009f2cee4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2824115
    Auto-Submit: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872812}

--- sandbox/linux/syscall_broker/broker_host.cc
+++ sandbox/linux/syscall_broker/broker_host.cc
@@ -21,9 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_file.h"
 #include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
+#include "sandbox/linux/services/syscall_wrappers.h"
 #include "sandbox/linux/syscall_broker/broker_command.h"
 #include "sandbox/linux/syscall_broker/broker_permission_list.h"
 #include "sandbox/linux/syscall_broker/broker_simple_message.h"
+#include "sandbox/linux/system_headers/linux_stat.h"
 #include "sandbox/linux/system_headers/linux_syscalls.h"
 
 namespace sandbox {
@@ -194,10 +196,12 @@ void StatFileForIPC(const BrokerCommandSet& allowed_command_set,
     RAW_CHECK(reply->AddIntToMessage(-permission_list.denied_errno()));
     return;
   }
+
   if (command_type == COMMAND_STAT) {
-    struct stat sb;
-    int sts =
-        follow_links ? stat(file_to_access, &sb) : lstat(file_to_access, &sb);
+    struct kernel_stat sb;
+
+    int sts = follow_links ? sandbox::sys_stat(file_to_access, &sb)
+                           : sandbox::sys_lstat(file_to_access, &sb);
     if (sts < 0) {
       RAW_CHECK(reply->AddIntToMessage(-errno));
       return;
@@ -206,10 +210,12 @@ void StatFileForIPC(const BrokerCommandSet& allowed_command_set,
     RAW_CHECK(
         reply->AddDataToMessage(reinterpret_cast<char*>(&sb), sizeof(sb)));
   } else {
+#if defined(__NR_fstatat64)
     DCHECK(command_type == COMMAND_STAT64);
-    struct stat64 sb;
-    int sts = follow_links ? stat64(file_to_access, &sb)
-                           : lstat64(file_to_access, &sb);
+    struct kernel_stat64 sb;
+
+    int sts = sandbox::sys_fstatat64(AT_FDCWD, file_to_access, &sb,
+                                     follow_links ? 0 : AT_SYMLINK_NOFOLLOW);
     if (sts < 0) {
       RAW_CHECK(reply->AddIntToMessage(-errno));
       return;
@@ -217,6 +223,11 @@ void StatFileForIPC(const BrokerCommandSet& allowed_command_set,
     RAW_CHECK(reply->AddIntToMessage(0));
     RAW_CHECK(
         reply->AddDataToMessage(reinterpret_cast<char*>(&sb), sizeof(sb)));
+#else  // defined(__NR_fstatat64)
+    // We should not reach here on 64-bit systems, as the *stat*64() are only
+    // necessary on 32-bit.
+    RAW_CHECK(false);
+#endif
   }
 }
 
--- sandbox/linux/system_headers/linux_time.h
+++ sandbox/linux/system_headers/linux_time.h
@@ -12,6 +12,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CPUCLOCK_CLOCK_MASK 3
 #endif
 
+#if !defined(CPUCLOCK_PROF)
+#define CPUCLOCK_PROF 0
+#endif
+
+#if !defined(CPUCLOCK_VIRT)
+#define CPUCLOCK_VIRT 1
+#endif
+
+#if !defined(CPUCLOCK_SCHED)
+#define CPUCLOCK_SCHED 2
+#endif
+
+#if !defined(CPUCLOCK_PERTHREAD_MASK)
+#define CPUCLOCK_PERTHREAD_MASK 4
+#endif
+
+#if !defined(MAKE_PROCESS_CPUCLOCK)
+#define MAKE_PROCESS_CPUCLOCK(pid, clock) \
+  ((int)(~(unsigned)(pid) << 3) | (int)(clock))
+#endif
+
+#if !defined(MAKE_THREAD_CPUCLOCK)
+#define MAKE_THREAD_CPUCLOCK(tid, clock) \
+  ((int)(~(unsigned)(tid) << 3) | (int)((clock) | CPUCLOCK_PERTHREAD_MASK))
+#endif
+
 #if !defined(CLOCKFD)
 #define CLOCKFD 3
 #endif
