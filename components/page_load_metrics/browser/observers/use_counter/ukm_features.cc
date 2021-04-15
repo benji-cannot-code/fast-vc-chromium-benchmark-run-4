b2b09a0287c7727f2218f0dbf785e27a24095bcd
Lily Chen <chlily@chromium.org>
2021-04-15 19:10:21 +0000

    Add UseCounter and UKM for Persistent File System in 3rd-party contexts
    Bug: 1198643
    Change-Id: Icdfc736080c3a768cb4569c9bec246415ceccc25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2824229
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872988}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -197,6 +197,7 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kLaxAllowingUnsafeCookies,
           WebFeature::kOpenWebDatabaseThirdPartyContext,
           WebFeature::kPaymentRequestCSPViolation,
+          WebFeature::kRequestedFileSystemPersistentThirdPartyContext,
       }));
   return *opt_in_features;
 }
