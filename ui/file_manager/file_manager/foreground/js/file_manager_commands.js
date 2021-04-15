a623f36f4bef4b0bee109274448ba3fc1623f13d
Josh Simmons <simmonsjosh@google.com>
2021-04-15 05:04:41 +0000

    Files app: Update refresh button browser tests
    The (now default) unified media views reuse Files.app's "Recents"
    functionality, which (in the UI) is represented by a FakeEntry for
    each root (Image, Video, Audio). These do not have a Volume
    associated with them and thus the existing tests to check for the
    presence of a "refresh" button on these roots are no longer valid.
    
    Updated the browser tests to ensure that the refresh button is shown
    under the correct conditions.
    
    Bug: 1198420
    Test: browser_tests --gtest_filter="*toolbarRefreshButton*"
    Change-Id: If33d17e82a562a2b77a206a472c1fe769f413ed2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2825974
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Josh Simmons <simmonsjosh@google.com>
    Cr-Commit-Position: refs/heads/master@{#872719}

--- ui/file_manager/file_manager/foreground/js/file_manager_commands.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands.js
@@ -2906,7 +2906,7 @@ CommandHandler.COMMANDS_['refresh'] = new class extends FilesCommand {
 };
 
 /**
- * Refreshes the currently selected directory.
+ * Sets the system wallpaper to the selected file.
  */
 CommandHandler.COMMANDS_['set-wallpaper'] = new class extends FilesCommand {
   execute(event, fileManager) {
