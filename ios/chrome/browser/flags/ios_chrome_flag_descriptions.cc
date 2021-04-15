1d1b3b13d051f55250b00f8a88404371292757a2
Elly Fong-Jones <ellyjones@chromium.org>
2021-04-15 21:01:25 +0000

    flags: remove autofill-cache-query-responses
    This change removes this flag (which expired in 72) and the
    backing feature, as well as the test coverage for disabling
    the feature.
    
    Bug: 953690
    Change-Id: I86f2613be6ecd18f254bdb3a466de9f6057422d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2828699
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873043}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.cc
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.cc
@@ -12,12 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace flag_descriptions {
 
-const char kAutofillCacheQueryResponsesName[] =
-    "Cache Autofill Query Responses";
-const char kAutofillCacheQueryResponsesDescription[] =
-    "When enabled, autofill will cache the responses it receives from the "
-    "crowd-sourced field type prediction server.";
-
 const char kAutofillCreditCardUploadName[] =
     "Offers uploading Autofilled credit cards";
 const char kAutofillCreditCardUploadDescription[] =
