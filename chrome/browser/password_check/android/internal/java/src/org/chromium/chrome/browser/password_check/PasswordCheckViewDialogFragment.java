c80842d1212ae43cbbe0debe53cf290bfe2a7a77
Ioana Pandele <ioanap@chromium.org>
2021-04-15 10:04:59 +0000

    [Passwords] Disable screenshots in the view password dialog
    This dialog is shown after re-authentication, when the user chooses
    to view a compromised password.
    
    Bug: 1195561
    Change-Id: I836d529d71975e9d29ffa7a7003fdcd9a7cb38e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2825433
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872793}

--- chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckViewDialogFragment.java
+++ chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckViewDialogFragment.java
@@ -9,9 +9,11 @@ import android.app.Dialog;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.view.WindowManager.LayoutParams;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -37,6 +39,7 @@ public class PasswordCheckViewDialogFragment extends PasswordCheckDialogFragment
      */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        getActivity().getWindow().setFlags(LayoutParams.FLAG_SECURE, LayoutParams.FLAG_SECURE);
         View dialogContent = getActivity().getLayoutInflater().inflate(
                 R.layout.password_check_view_credential_dialog, null);
         TextView passwordView = dialogContent.findViewById(R.id.view_dialog_compromised_password);
@@ -71,5 +74,11 @@ public class PasswordCheckViewDialogFragment extends PasswordCheckDialogFragment
             // ensure the user goes through reauth again.
             dismiss();
         }
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        getActivity().getWindow().clearFlags(LayoutParams.FLAG_SECURE);
     }
 }
