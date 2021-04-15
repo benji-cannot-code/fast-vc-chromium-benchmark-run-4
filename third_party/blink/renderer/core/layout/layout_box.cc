8187d059b9ea7779704a6927e0c726158a69b246
Koji Ishii <kojii@chromium.org>
2021-04-15 08:46:13 +0000

    Add DowncastTraits<LayoutNGFieldset>
    This patch has no behavior changes.
    
    Bug: 1144203
    Change-Id: I3a27c7bb80899a537f15bfbb87fd5a7894a41b6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827832
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872773}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -82,6 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_box_strut.h"
 #include "third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h"
+#include "third_party/blink/renderer/core/layout/ng/layout_ng_fieldset.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_box_fragment_builder.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_constraint_space.h"
@@ -7346,9 +7347,9 @@ void LayoutBox::RecalcFragmentsVisualOverflow() {
   // of |this| in the box tree, but it is a child of the fieldset container in
   // the fragment tree. Make sure it is recalculated.
   if (UNLIKELY(IsAnonymous() && HasSelfPaintingLayer())) {
-    const auto* parent = DynamicTo<LayoutBlock>(Parent());
-    if (parent && parent->IsLayoutNGFieldset()) {
-      if (LayoutBox* legend = LayoutFieldset::FindInFlowLegend(*parent))
+    const auto* fieldset = DynamicTo<LayoutNGFieldset>(Parent());
+    if (UNLIKELY(fieldset)) {
+      if (LayoutBox* legend = LayoutFieldset::FindInFlowLegend(*fieldset))
         legend->RecalcFragmentsVisualOverflow();
     }
   }
