c33b3f9bc5cafab87ad73158cc5f5201ddcd25be
Elly Fong-Jones <ellyjones@chromium.org>
2021-04-15 14:54:43 +0000

    flags: remove touchpad-overscroll-history-navigation
    This flag expired in M76. The backing feature still seems to be used,
    mostly to allow for unit-testing both the enabled and disabled paths
    on all platforms.
    
    Bug: 953690
    Change-Id: Iba4cc475b1e2d9b39c35df66bbb3cceb0fd5213b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2826676
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872863}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1433,9 +1433,6 @@ extern const char kTouchSelectionStrategyDescription[];
 extern const char kTouchSelectionStrategyCharacter[];
 extern const char kTouchSelectionStrategyDirection[];
 
-extern const char kTouchpadOverscrollHistoryNavigationName[];
-extern const char kTouchpadOverscrollHistoryNavigationDescription[];
-
 extern const char kTraceUploadUrlName[];
 extern const char kTraceUploadUrlDescription[];
 extern const char kTraceUploadUrlChoiceOther[];
