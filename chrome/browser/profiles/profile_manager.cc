3cc268ed6e433af951d1c6dd093259f3b74a6b98
Ramin Halavati <rhalavati@chromium.org>
2021-04-15 14:47:12 +0000

    Merge session duration service class for Incognito and regular profiles.
    |AndroidProfileSessionDurationsService| records session duration for
    regular profiles and |AndroidIncognitoSessionDurationsService| for
    Incognito profiles.
    The two classes (and their factories) are merged into
    |AndroidSessionDurationService| to simplify the code and removing the
    need that upstream classes know the implementation details.
    
    Bug: 1186118
    Change-Id: I9cf5020f38eb75470bc418474334b37cbd2b34d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791385
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872850}

--- chrome/browser/profiles/profile_manager.cc
+++ chrome/browser/profiles/profile_manager.cc
@@ -130,9 +130,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/supervised_user/supervised_user_service_factory.h"
 #endif
 
-#if defined(OS_ANDROID)
-#include "chrome/browser/android/metrics/android_profile_session_durations_service_factory.h"
-#else
+#if !defined(OS_ANDROID)
 #include "chrome/browser/accessibility/caption_controller.h"
 #include "chrome/browser/accessibility/caption_controller_factory.h"
 #include "chrome/browser/browsing_data/chrome_browsing_data_lifetime_manager.h"
@@ -1544,12 +1542,10 @@ void ProfileManager::DoFinalInitForServices(Profile* profile,
   // because ProfileSyncService needs the URL context getter.
   UnifiedConsentServiceFactory::GetForProfile(profile);
 
-#if defined(OS_ANDROID)
-  AndroidProfileSessionDurationsServiceFactory::GetForProfile(profile);
-#elif BUILDFLAG(IS_CHROMEOS_ASH)  // !OS_ANDROID && IS_CHROMEOS_ASH
+#if BUILDFLAG(IS_CHROMEOS_ASH)
   if (!chromeos::ProfileHelper::IsSigninProfile(profile))
     captions::CaptionControllerFactory::GetForProfile(profile)->Init();
-#else                             // !OS_ANDROID && !IS_CHROMEOS_ASH
+#elif !defined(OS_ANDROID)  // !OS_ANDROID && !IS_CHROMEOS_ASH
   captions::CaptionControllerFactory::GetForProfile(profile)->Init();
 #endif
 
