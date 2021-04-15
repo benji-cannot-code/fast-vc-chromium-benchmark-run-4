f42bd36a905c72967a058e5c1f107e26f0613110
Viktor Semeniuk <vsemeniuk@google.com>
2021-04-15 14:49:06 +0000

    Notifying InsecureCredentialsObserver on password update
    InsecureCredentialsObserver should be notified when password value was
    changed.
    
    Bug: 1197520
    Change-Id: I8c4cf7c51b474427d409e920b90627c9f8fa5f8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2825454
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#872853}

--- components/password_manager/core/browser/password_store.cc
+++ components/password_manager/core/browser/password_store.cc
@@ -770,6 +770,12 @@ void PasswordStore::NotifyLoginsChanged(
     if (reuse_detector_)
       reuse_detector_->OnLoginsChanged(changes);
   }
+
+  if (base::ranges::any_of(changes, [](const auto& change) {
+        return change.insecure_credentials_changed();
+      })) {
+    NotifyInsecureCredentialsChanged();
+  }
 }
 
 void PasswordStore::NotifyInsecureCredentialsChanged() {
--- components/password_manager/core/browser/password_store_change.h
+++ components/password_manager/core/browser/password_store_change.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace password_manager {
 
+using InsecureCredentialsChanged =
+    base::StrongAlias<class InsecureCredentialsChangedTag, bool>;
+
 class PasswordStoreChange {
  public:
   // This is used in enums.xml. Please keep order the same.
@@ -27,7 +30,9 @@ class PasswordStoreChange {
   PasswordStoreChange(Type type,
                       PasswordForm form,
                       FormPrimaryKey primary_key,
-                      bool password_changed);
+                      bool password_changed,
+                      InsecureCredentialsChanged insecure_changed =
+                          InsecureCredentialsChanged(false));
 
   PasswordStoreChange(const PasswordStoreChange& other);
   PasswordStoreChange(PasswordStoreChange&& other);
@@ -37,8 +42,11 @@ class PasswordStoreChange {
 
   Type type() const { return type_; }
   const PasswordForm& form() const { return form_; }
-  FormPrimaryKey primary_key() const { return FormPrimaryKey(primary_key_); }
+  FormPrimaryKey primary_key() const { return primary_key_; }
   bool password_changed() const { return password_changed_; }
+  InsecureCredentialsChanged insecure_credentials_changed() const {
+    return insecure_credentials_changed_;
+  }
 
   bool operator==(const PasswordStoreChange& other) const;
 
@@ -48,6 +56,8 @@ class PasswordStoreChange {
   // The corresponding primary key in the database for this password.
   FormPrimaryKey primary_key_{-1};
   bool password_changed_ = false;
+  // Whether change affected insecure credentials.
+  InsecureCredentialsChanged insecure_credentials_changed_{false};
 };
 
 typedef std::vector<PasswordStoreChange> PasswordStoreChangeList;
