4af6b16798c32d01bfb25dba19c09f273399d993
Aaron Krajeski <aaronhk@chromium.org>
2021-04-15 15:17:03 +0000

    Add getLocalToDevice to paintcanvas
    getLocalToDevice returns an SkM44, whereas the previously used
    getTotalMatrix returns the deprecated SkMatrix.
    
    Thought this was a fix for crbug.com/1195276, turns out it is not.
    It's good to move away from the deprecated SkMatrix when we can though.
    
    Bug: 1155544
    Change-Id: I95e62697a0a2e154fcd4e0c2f352044cc65f8219
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2815441
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872870}

--- cc/paint/record_paint_canvas.h
+++ cc/paint/record_paint_canvas.h
@@ -109,6 +109,7 @@ class CC_PAINT_EXPORT RecordPaintCanvas : public PaintCanvas {
 
   bool isClipEmpty() const override;
   SkMatrix getTotalMatrix() const override;
+  SkM44 getLocalToDevice() const override;
 
   void Annotate(AnnotationType type,
                 const SkRect& rect,
--- cc/paint/skia_paint_canvas.h
+++ cc/paint/skia_paint_canvas.h
@@ -131,6 +131,7 @@ class CC_PAINT_EXPORT SkiaPaintCanvas final : public PaintCanvas {
 
   bool isClipEmpty() const override;
   SkMatrix getTotalMatrix() const override;
+  SkM44 getLocalToDevice() const override;
 
   void Annotate(AnnotationType type,
                 const SkRect& rect,
