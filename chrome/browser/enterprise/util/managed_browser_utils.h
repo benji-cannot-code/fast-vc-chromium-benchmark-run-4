09d48bdde04a31453010074d3fa4644211a02192
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2021-04-15 23:56:32 +0000

    Add ClientCertificateFetcher to be used in the enterprise reporting API
    Bug: 1194759
    Change-Id: Ibec0eaf3f3506b493ee12d2d65f39faf4259ead4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822237
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873111}

--- chrome/browser/enterprise/util/managed_browser_utils.h
+++ chrome/browser/enterprise/util/managed_browser_utils.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "net/ssl/client_cert_identity.h"
+
+class GURL;
 class Profile;
 
 namespace chrome {
@@ -22,6 +25,14 @@ bool HasBrowserPoliciesApplied(Profile* profile);
 // Extracts the domain from provided |email| if it's an email address and
 // returns an empty string, otherwise.
 std::string GetDomainFromEmail(const std::string& email);
+
+// Attempts to auto-select a client certificate according to the value of
+// |ContentSettingsType::AUTO_SELECT_CERTIFICATE| content setting for
+// |requesting_url|. If no certificate was auto-selected, returns nullptr.
+std::unique_ptr<net::ClientCertIdentity> AutoSelectCertificate(
+    Profile* profile,
+    const GURL& requesting_url,
+    net::ClientCertIdentityList& client_certs);
 
 }  // namespace enterprise_util
 }  // namespace chrome
