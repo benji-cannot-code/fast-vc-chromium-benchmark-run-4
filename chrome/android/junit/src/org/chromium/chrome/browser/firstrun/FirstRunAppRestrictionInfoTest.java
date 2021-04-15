7494782ec9461c3f862eed722cc0e511be4a38f9
Alice Wang <aliceywang@chromium.org>
2021-04-15 15:34:01 +0000

    [Android][Test] Migrate CustomShadowUserManager to robolectric shadow
    This CL migrates the CustomShadowUserManager used in first run tests
    to robolectric ShadowUserManager and removes the custom shadow.
    
    Bug: 779568
    Change-Id: Ia13e09f43e056b02d8da0e279b0a5430a4f7b5ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2825902
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872874}

--- chrome/android/junit/src/org/chromium/chrome/browser/firstrun/FirstRunAppRestrictionInfoTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/firstrun/FirstRunAppRestrictionInfoTest.java
@@ -22,6 +22,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowUserManager;
 
 import org.chromium.base.CommandLine;
 import org.chromium.base.ContextUtils;
@@ -32,7 +33,6 @@ import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.components.policy.PolicySwitches;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
-import org.chromium.testing.local.CustomShadowUserManager;
 
 import java.util.Arrays;
 import java.util.List;
@@ -43,8 +43,7 @@ import java.util.concurrent.TimeoutException;
  */
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE,
-        shadows = {ShadowRecordHistogram.class, ShadowPostTask.class,
-                CustomShadowUserManager.class})
+        shadows = {ShadowRecordHistogram.class, ShadowPostTask.class, ShadowUserManager.class})
 public class FirstRunAppRestrictionInfoTest {
     private static final List<String> HISTOGRAM_NAMES =
             Arrays.asList("Enterprise.FirstRun.AppRestrictionLoadTime",
@@ -90,7 +89,7 @@ public class FirstRunAppRestrictionInfoTest {
 
         Context context = ContextUtils.getApplicationContext();
         UserManager userManager = (UserManager) context.getSystemService(Context.USER_SERVICE);
-        CustomShadowUserManager shadowUserManager = (CustomShadowUserManager) shadowOf(userManager);
+        ShadowUserManager shadowUserManager = shadowOf(userManager);
         shadowUserManager.setApplicationRestrictions(context.getPackageName(), mMockBundle);
     }
 
