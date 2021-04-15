8678b0233df386b8bef91153b6532d9b168d24a8
Pete Williamson <petewil@chromium.org>
2021-04-15 16:04:15 +0000

    Code Framework for the Follow Management activity.
    For the Web Feed, we need an activity to allow the user to manage what
    they are following.  This sets up the activity without any of the
    business logic, and starts the activity when we press the right
    menu option.
    
    This is the first in a set of changelists.  Follow on changelists will
    1) Get actual data to display in the follow management activity
    2) Put the data into a recycler view in the follow management activity.
    
    Bug: 1197286
    Change-Id: I61268c4b91202e7e9fb0e0a743d12d1ee150b31d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2815656
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872885}

--- chrome/android/java/src/org/chromium/chrome/browser/app/feedmanagement/FeedManagementActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/feedmanagement/FeedManagementActivity.java
@@ -5,22 +5,41 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.app.feedmanagement;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import org.chromium.base.Log;
 import org.chromium.chrome.browser.SnackbarActivity;
+import org.chromium.chrome.browser.app.followmanagement.FollowManagementActivity;
 import org.chromium.chrome.browser.feed.feedmanagement.FeedManagementCoordinator;
+import org.chromium.chrome.browser.feed.feedmanagement.FeedManagementMediator;
 
 /**
  * Activity for managing feed and webfeed settings on the new tab page.
  */
-public class FeedManagementActivity extends SnackbarActivity {
-    private static final String TAG = "FeedManagementActivity";
+public class FeedManagementActivity
+        extends SnackbarActivity implements FeedManagementMediator.FollowManagementLauncher {
+    private static final String TAG = "FeedMActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        FeedManagementCoordinator coordinator = new FeedManagementCoordinator(this);
+        FeedManagementCoordinator coordinator = new FeedManagementCoordinator(this, this);
         setContentView(coordinator.getView());
+    }
+
+    // FollowManagementLauncher method.
+    @Override
+    public void launch(Context context) {
+        try {
+            // Launch a new activity for the following management page.
+            Intent intent = new Intent(context, FollowManagementActivity.class);
+            Log.d(TAG, "Launching follow management activity.");
+            context.startActivity(intent);
+        } catch (Exception e) {
+            Log.d(TAG, "Failed to launch activity " + e);
+        }
     }
 }
--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/feedmanagement/FeedManagementCoordinator.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/feedmanagement/FeedManagementCoordinator.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ListView;
 
+import org.chromium.chrome.browser.feed.feedmanagement.FeedManagementMediator.FollowManagementLauncher;
 import org.chromium.chrome.browser.feed.webfeed.R;
 import org.chromium.ui.modelutil.LayoutViewBuilder;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
@@ -22,7 +23,8 @@ public class FeedManagementCoordinator {
     private FeedManagementMediator mMediator;
     private final View mView;
 
-    public FeedManagementCoordinator(Context context) {
+    public FeedManagementCoordinator(
+            Context context, FollowManagementLauncher followManagementLauncher) {
         ModelList listItems = new ModelList();
 
         // Once this is attached to the ListView, there is no need to hold a reference to it.
@@ -36,7 +38,7 @@ public class FeedManagementCoordinator {
         ListView listView = (ListView) mView.findViewById(R.id.feed_management_menu);
         listView.setAdapter(adapter);
 
-        mMediator = new FeedManagementMediator(context, listItems);
+        mMediator = new FeedManagementMediator(context, listItems, followManagementLauncher);
     }
 
     public View getView() {
