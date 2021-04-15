ee81ecb421867cf80ab305b266fc2f8dd25f2966
Jérémie Boulic <jboulic@chromium.org>
2021-04-15 01:08:15 +0000

    [Files app] Update mount error visual signal on invalid mount path
    Bug: b:185136879, chromium:912236
    Change-Id: I6954c653a31cf960d88047b3171449b6584c71bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2825309
    Auto-Submit: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872631}

--- ui/file_manager/file_manager/foreground/js/file_tasks.js
+++ ui/file_manager/file_manager/foreground/js/file_tasks.js
@@ -948,7 +948,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       const item = new ProgressCenterItem();
       item.id = 'Cannot mount: ' + url;
       item.type = ProgressItemType.MOUNT_ARCHIVE;
-      item.message = strf('ARCHIVE_MOUNT_FAILED', filename);
+      const msgId = error === VolumeManagerCommon.VolumeError.INVALID_PATH ?
+          'ARCHIVE_MOUNT_INVALID_PATH' :
+          'ARCHIVE_MOUNT_FAILED';
+      item.message = strf(msgId, filename);
       item.state = ProgressItemState.ERROR;
       this.progressCenter_.updateItem(item);
 
