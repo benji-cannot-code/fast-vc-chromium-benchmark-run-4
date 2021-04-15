14c3a26a53884d176ceb8c05f2cd1466ff2efd80
Yeunjoo Choi <ychoi@igalia.com>
2021-04-15 13:54:42 +0000

    Move //chrome/browser/{chromeos => ash}/display
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: I014d95736e59400410eb2b1a80dd974acdf2690d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2823319
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#872815}

--- chrome/browser/ash/display/quirks_manager_delegate_impl.h
+++ chrome/browser/ash/display/quirks_manager_delegate_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROMEOS_DISPLAY_QUIRKS_MANAGER_DELEGATE_IMPL_H_
-#define CHROME_BROWSER_CHROMEOS_DISPLAY_QUIRKS_MANAGER_DELEGATE_IMPL_H_
+#ifndef CHROME_BROWSER_ASH_DISPLAY_QUIRKS_MANAGER_DELEGATE_IMPL_H_
+#define CHROME_BROWSER_ASH_DISPLAY_QUIRKS_MANAGER_DELEGATE_IMPL_H_
 
 #include "base/macros.h"
 #include "components/quirks/quirks_manager.h"
@@ -30,4 +30,4 @@ class QuirksManagerDelegateImpl : public QuirksManager::Delegate {
 
 }  // namespace quirks
 
-#endif  // CHROME_BROWSER_CHROMEOS_DISPLAY_QUIRKS_MANAGER_DELEGATE_IMPL_H_
+#endif  // CHROME_BROWSER_ASH_DISPLAY_QUIRKS_MANAGER_DELEGATE_IMPL_H_
