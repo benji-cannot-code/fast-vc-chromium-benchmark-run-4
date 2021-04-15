a68d587d895ad8db5581b3d3fc7f9d88277ac7ce
Owen Min <zmin@chromium.org>
2021-04-15 18:31:40 +0000

    Improve error message for extension request policy
    When CloudReportingEnabled policy is not enabled,
    CloudExtensionRequestEnabled policy will now show an error message
    indicates that it's ignored.
    
    Also add a new API to the PolicyErrorMap so that it supports error
    message with two replacement strings.
    
    Change-Id: I1ec961af6ae21357c84920b769ab5f3f84b20e7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2824049
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872963}

--- chrome/browser/enterprise/reporting/extension_request/extension_request_policy_handler.cc
+++ chrome/browser/enterprise/reporting/extension_request/extension_request_policy_handler.cc
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/values.h"
 #include "chrome/common/pref_names.h"
+#include "components/policy/core/browser/policy_error_map.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/policy_constants.h"
 #include "components/prefs/pref_value_map.h"
+#include "components/strings/grit/components_strings.h"
 
 namespace enterprise_reporting {
 
@@ -20,16 +22,29 @@ ExtensionRequestPolicyHandler::ExtensionRequestPolicyHandler()
 
 ExtensionRequestPolicyHandler::~ExtensionRequestPolicyHandler() = default;
 
-void ExtensionRequestPolicyHandler::ApplyPolicySettings(
+bool ExtensionRequestPolicyHandler::CheckPolicySettings(
     const policy::PolicyMap& policies,
-    PrefValueMap* prefs) {
+    policy::PolicyErrorMap* errors) {
+  if (!policies.GetValue(policy_name()))
+    return true;
+  if (!TypeCheckingPolicyHandler::CheckPolicySettings(policies, errors))
+    return false;
   const base::Value* cloud_reporting_policy_value =
       policies.GetValue(policy::key::kCloudReportingEnabled);
   if (!cloud_reporting_policy_value ||
       !cloud_reporting_policy_value->is_bool() ||
-      !cloud_reporting_policy_value->GetBool())
-    return;
+      !cloud_reporting_policy_value->GetBool()) {
+    errors->AddError(policy_name(), IDS_POLICY_DEPENDENCY_ERROR,
+                     policy::key::kCloudReportingEnabled, "Enabled");
+    return false;
+  }
 
+  return true;
+}
+
+void ExtensionRequestPolicyHandler::ApplyPolicySettings(
+    const policy::PolicyMap& policies,
+    PrefValueMap* prefs) {
   const base::Value* extension_request_policy_value =
       policies.GetValue(policy_name());
 
