6ae03a93c4c5fd15730cc3d37318a341a3f8883b
Mike Nichols <mikenichols@chromium.org>
2021-04-15 22:53:24 +0000

    install-build-deps: Update python2 handling for image building.
    Local development is based on rolling Debian whereas the image builders
    utilize Ubuntu, which differs in package names for python2 packages.
    This should allow for local execution to remain working while working
    for image builders.
    
    Bug: b:185514009
    Change-Id: I03f10953cfae1d7735735f68323626c206ac54e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2828749
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Mike Nichols <mikenichols@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873087}

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -205,7 +205,6 @@ dev_list="\
   patch
   perl
   pkg-config
-  python2-dev
   python-setuptools
   rpm
   ruby
@@ -220,9 +219,9 @@ dev_list="\
 "
 
 if package_exists python-is-python2; then
-  dev_list="${dev_list} python-is-python2"
+  dev_list="${dev_list} python-is-python2 python2-dev"
 else
-  dev_list="${dev_list} python"
+  dev_list="${dev_list} python python-dev"
 fi
 
 # 64-bit systems need a minimum set of 32-bit compat packages for the pre-built
@@ -341,7 +340,6 @@ backwards_compatible_list="\
   libgtk2.0-dev
   mesa-common-dev
   msttcorefonts
-  python-dev
   ttf-dejavu-core
   ttf-indic-fonts
   ttf-kochi-gothic
@@ -349,6 +347,10 @@ backwards_compatible_list="\
   ttf-mscorefonts-installer
   xfonts-mathml
 "
+if package_exists python-is-python2; then
+  backwards_compatible_list="${backwards_compatible_list} python-dev"
+fi
+
 case $distro_codename in
   trusty)
     backwards_compatible_list+=" \
