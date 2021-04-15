1616bdcc482b5afe2b6e29e4127b74e198881ce1
Joey Arhar <jarhar@chromium.org>
2021-04-15 01:59:04 +0000

    Update offsetParent behavior in shadow trees
    New behavior for offsetParent in shadow trees was discussed in [1] and
    [2]. This lead to a chromium patch [3] which changed the behavior. After
    [3] landed, the desired behavior in the discussions in [1] and [2]
    seemed to have changed. Unfortunately, the author of [3] was no longer
    working on chromium. This new behavior was then added to the spec and
    landed in webkit [4] and firefox [5], and a WPT was added for it [6].
    
    This patch implements the new behavior to follow suit with webkit and
    firefox based on the WPT in [6]. Unfortunately, there are several tests
    which are either internal to chromium or are only passing in chromium
    which appear to oppose this new behavior and will have to be updated or
    removed:
    - external/wpt/css/css-contain/content-visibility/content-visibility-035.html
    - external/wpt/css/css-contain/content-visibility/content-visibility-044.html
    - fast/dom/shadow/offset-parent-does-not-leak-ua-shadow.html
    - shadow-dom/offsetParent.html
    For shadow-dom/offsetParent.html, I verified that firefox and safari
    both currently fail the same tests which this patch does.
    
    [1] https://github.com/WICG/webcomponents/issues/497
    [2] https://github.com/w3c/csswg-drafts/issues/159
    [3] https://chromium.googlesource.com/chromium/src/+/18d455ee833f6a30dcbe2755380861eb75cd9f6f
    [4] https://trac.webkit.org/changeset/239313/webkit
    [5] https://bugzilla.mozilla.org/show_bug.cgi?id=1514074
    [6] https://wpt.fyi/results/shadow-dom/offsetParent-across-shadow-boundaries.html
    
    Fixed: 920069
    Change-Id: I168edc5ad0e4fcb92d0c4a440623f2424b14a988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2775208
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872658}

--- third_party/blink/renderer/core/dom/node.h
+++ third_party/blink/renderer/core/dom/node.h
@@ -643,6 +643,7 @@ class CORE_EXPORT Node : public EventTarget {
   unsigned CountChildren() const;
 
   bool IsDescendantOf(const Node*) const;
+  bool IsDescendantOrShadowDescendantOf(const Node*) const;
   bool contains(const Node*) const;
   // https://dom.spec.whatwg.org/#concept-shadow-including-inclusive-ancestor
   bool IsShadowIncludingInclusiveAncestorOf(const Node&) const;
