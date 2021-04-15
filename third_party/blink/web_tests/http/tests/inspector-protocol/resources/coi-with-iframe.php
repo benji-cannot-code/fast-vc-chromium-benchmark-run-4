8e62d38e35f095a1423e9fc06e9008f2154e8f99
Sigurd Schneider <sigurds@chromium.org>
2021-04-15 06:13:59 +0000

    Sent attachedToTarget events to all connected CDP sessions
    This CL makes sure all connected CDP sessions receive the
    attachedToTarget event. Previously only one of the sessions
    received that event (the one that ended up creating the
    RFDAH). This CL also allows all connected sessions to
    block on the debugger, i.e. each session now has to call
    Runtime.runIfWaitingForDebugger to resume the navigation.
    Previously only the session that created the RFDAH was
    able to resume the navigation.
    
    Bug: chromium:1197428, chromium:1196618
    Change-Id: Ic990b0cbb83d2f1c8e685f84ba79909f3b5edf80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814927
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872741}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/resources/coi-with-iframe.php
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header('Cross-Origin-Embedder-Policy: require-corp');
+header('Cross-Origin-Resource-Policy: cross-origin');
+header('Cross-Origin-Opener-Policy: cross-origin');
+?>
+
+<html>
+<iframe id="iframe">
+<iframe>
+</html>
---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/resources/corp-iframe.php
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header('Cross-Origin-Embedder-Policy: require-corp');
+header('Cross-Origin-Resource-Policy: cross-origin');
+?>
+
+<html>
+Corp iframe
+</html>
