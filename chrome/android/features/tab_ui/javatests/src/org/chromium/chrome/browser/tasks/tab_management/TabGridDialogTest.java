62ebe25424c1ea38b1a893cade6c8048c8b9a9bb
Matt Jones <mdjones@chromium.org>
2021-04-15 14:45:50 +0000

    Support BottomSheet widget in WebLayer
    This patch adds BottomSheet support to WebLayer which includes a basic
    scrim (currently without system UI tinting support). The only UI in
    WebLayer that the sheet needs to interact with is the
    ModalDialogManager which is handled in the BrowserViewController.
    
    Bug: 1197089
    Binary-Size: Adding a component previously not in WebLayer
    Change-Id: I034c2e22a26f2680524225c2d86ef1ab994683a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2806690
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Matt Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872847}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
@@ -1068,7 +1068,8 @@ public class TabGridDialogTest {
             return;
         }
         @ColorInt
-        int scrimDefaultColor = ApiCompatibilityUtils.getColor(resources, R.color.black_alpha_65);
+        int scrimDefaultColor =
+                ApiCompatibilityUtils.getColor(resources, R.color.default_scrim_color);
         @ColorInt
         int navigationBarColor =
                 ApiCompatibilityUtils.getColor(resources, R.color.bottom_system_nav_color);
