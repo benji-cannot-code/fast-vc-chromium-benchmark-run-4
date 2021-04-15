1c5705479f7e79cfc6f28f70a624714f181071dd
Shimi Zhang <ctzsm@chromium.org>
2021-04-15 17:28:11 +0000

    aw: Delay GpuServiceWebView initialization in unittest code
    GpuServiceWebView::GetInstance() is depending on FeatureList, but when
    we are calling GpuServiceWebView::GetInstance() in main(), the
    FeatureList haven't been initialized yet, so are we encountering a null
    pointer dereference.
    
    We can delay the initialization of GpuServiceWebView when we actually
    need it to avoid this issue.
    
    Bug: 1188282
    Change-Id: I4707d8ee150b1d1b175b54fa29a0b916546dbe15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827406
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872926}

--- android_webview/lib/webview_tests.cc
+++ android_webview/lib/webview_tests.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "android_webview/browser/gfx/gpu_service_webview.h"
 #include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/test/test_suite.h"
@@ -21,7 +20,6 @@ int main(int argc, char** argv) {
                                   ",VizForWebViewDefault");
 
   gl::GLSurfaceTestSupport::InitializeNoExtensionsOneOff();
-  android_webview::GpuServiceWebView::GetInstance();
   base::TestSuite test_suite(argc, argv);
   mojo::core::Init();
   return test_suite.Run();
