420f7269da32337e23a9d86628629f3f67f6adcf
Azeem Arshad <azeemarshad@chromium.org>
2021-04-15 19:00:45 +0000

    [CrOS Cellular] Wait for device before refreshing euicc.
    This CL fixes issue with CellularESimProfileHandler attempting
    to refresh new EUICCs before a cellular device exists. This causes
    inhibit to fail. Fixed this by checking for cellular device before
    starting refresh. Also added a refresh call on DeviceStateList
    change so that EUICCs will be updated once the device appears.
    
    Bug: 1197577
    Change-Id: I5806d756d6533c1a1ac5bb92bb603aa264009183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827502
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#872981}

--- chromeos/network/cellular_esim_profile_handler.cc
+++ chromeos/network/cellular_esim_profile_handler.cc
@@ -25,6 +25,7 @@ void CellularESimProfileHandler::Init(
   HermesManagerClient::Get()->AddObserver(this);
   HermesEuiccClient::Get()->AddObserver(this);
   HermesProfileClient::Get()->AddObserver(this);
+  InitInternal();
 }
 
 void CellularESimProfileHandler::RefreshProfileList(
