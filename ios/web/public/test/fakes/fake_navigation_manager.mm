7f4cb46df8ba0aaf56e9cec38de04e505fc3d10b
Ali Juma <ajuma@chromium.org>
2021-04-16 16:05:25 +0000

    [iOS] Remove the transient item concept from NavigationManager
    Transient navigation items are no longer used anywhere, since there
    are no longer any non-committed interstitials.
    
    This CL removes all methods related to transient items from
    NavigationManager and its subclasses.
    
    Change-Id: I05995e9add0676f8596d358270f8b59ad5e5d5e3
    Bug: 1197618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2829394
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873320}

--- ios/web/public/test/fakes/fake_navigation_manager.mm
+++ ios/web/public/test/fakes/fake_navigation_manager.mm
@@ -57,11 +57,6 @@ void FakeNavigationManager::SetPendingItem(NavigationItem* item) {
   pending_item_ = item;
 }
 
-web::NavigationItem* FakeNavigationManager::GetTransientItem() const {
-  NOTREACHED();
-  return nullptr;
-}
-
 void FakeNavigationManager::DiscardNonCommittedItems() {
   SetPendingItem(nullptr);
 }
