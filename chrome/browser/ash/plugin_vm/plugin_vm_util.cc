47e3ae92e0135a958bbf456a87ae5f16e0a478aa
Timothy Loh <timloh@chromium.org>
2021-04-16 07:10:28 +0000

    Revert "Reland: Stop supporting Plugin VM device license key"
    This reverts commit b7ccf4ce807d775fde613e6908cdc58258d1531a.
    
    Reason for revert: Breaks Chrome uprev (pita.Suspend failing)
    
    While we can boot the VM the first time, it seems like after suspending we run into a (license?) error trying to resume.
    
    Bug: 1199401
    
    Original change's description:
    > Reland: Stop supporting Plugin VM device license key
    >
    > This has been superseded by ByteBot licensing, and has caused some
    > confusion by users who had previously added such a key, as the policy
    > is no longer available in the admin console (excl managedchrome).
    >
    > This CL was previously landed at https://crrev.com/c/2766562 and then
    > reverted as it broke our tast tests. The fake policy setter now also
    > makes the installer skip the license check, and we now continue sending
    > the fake license key in the PluginVmService (I suspect this is no longer
    > necessary, but will make the change separately).
    >
    > Change-Id: I7ec1e94a27758c7291b1d49ccce1f1da3f7436a2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2789091
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: Jason Lin <lxj@google.com>
    > Commit-Queue: Timothy Loh <timloh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#872266}
    
    Change-Id: I44f359eafb3510fc4bfd255b512c9d2b1e1dce5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830772
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Owners-Override: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873223}

--- chrome/browser/ash/plugin_vm/plugin_vm_util.cc
+++ chrome/browser/ash/plugin_vm/plugin_vm_util.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_drive_image_download_service.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_features.h"
-#include "chrome/browser/ash/plugin_vm/plugin_vm_installer_factory.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_manager.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_manager_factory.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_pref_names.h"
@@ -40,9 +39,19 @@ const char kChromeOSBaseDirectoryDisplayText[] = "Network \u203a ChromeOS";
 
 namespace {
 
-static std::string& MutableFakeLicenseKey() {
+std::string& GetFakeLicenseKey() {
   static base::NoDestructor<std::string> license_key;
   return *license_key;
+}
+
+base::RepeatingClosureList& GetFakeLicenceKeyListeners() {
+  static base::NoDestructor<base::RepeatingClosureList> instance;
+  return *instance;
+}
+
+std::string& GetFakeUserId() {
+  static base::NoDestructor<std::string> user_id;
+  return *user_id;
 }
 
 }  // namespace
@@ -62,6 +71,17 @@ bool IsPluginVmAppWindow(const aura::Window* window) {
   return *app_id == "org.chromium.plugin_vm_ui";
 }
 
+std::string GetPluginVmLicenseKey() {
+  if (FakeLicenseKeyIsSet())
+    return GetFakeLicenseKey();
+  std::string plugin_vm_license_key;
+  if (!ash::CrosSettings::Get()->GetString(chromeos::kPluginVmLicenseKey,
+                                           &plugin_vm_license_key)) {
+    return std::string();
+  }
+  return plugin_vm_license_key;
+}
+
 std::string GetPluginVmUserIdForProfile(const Profile* profile) {
   DCHECK(profile);
   return profile->GetPrefs()->GetString(plugin_vm::prefs::kPluginVmUserId);
@@ -76,17 +96,19 @@ void SetFakePluginVmPolicy(Profile* profile,
   base::DictionaryValue* dict = update.Get();
   dict->SetPath("url", base::Value(image_url));
   dict->SetPath("hash", base::Value(image_hash));
-  MutableFakeLicenseKey() = license_key;
-  plugin_vm::PluginVmInstallerFactory::GetForProfile(profile)
-      ->SkipLicenseCheckForTesting();  // IN-TEST
-}
 
-std::string GetFakeLicenseKey() {
-  return MutableFakeLicenseKey();
+  GetFakeLicenseKey() = license_key;
+
+  GetFakeLicenceKeyListeners().Notify();
+  GetFakeUserId() = "FAKE_USER_ID";
 }
 
 bool FakeLicenseKeyIsSet() {
-  return !MutableFakeLicenseKey().empty();
+  return !GetFakeLicenseKey().empty();
+}
+
+bool FakeUserIdIsSet() {
+  return !GetFakeUserId().empty();
 }
 
 void RemoveDriveDownloadDirectoryIfExists() {
@@ -152,6 +174,13 @@ PluginVmPolicySubscription::PluginVmPolicySubscription(
                           base::Unretained(this)));
   device_allowed_subscription_ = cros_settings->AddSettingsObserver(
       chromeos::kPluginVmAllowed,
+      base::BindRepeating(&PluginVmPolicySubscription::OnPolicyChanged,
+                          base::Unretained(this)));
+  license_subscription_ = cros_settings->AddSettingsObserver(
+      chromeos::kPluginVmLicenseKey,
+      base::BindRepeating(&PluginVmPolicySubscription::OnPolicyChanged,
+                          base::Unretained(this)));
+  fake_license_subscription_ = GetFakeLicenceKeyListeners().Add(
       base::BindRepeating(&PluginVmPolicySubscription::OnPolicyChanged,
                           base::Unretained(this)));
 
