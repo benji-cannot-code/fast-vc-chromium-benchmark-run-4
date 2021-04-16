94550eca1eaf0aabb0baa02cc5e69d0b9aeda7dc
Hirthanan Subenderan <hirthanan@google.com>
2021-04-16 20:27:00 +0000

    Retrieve UMA consent for a given device.
    This method is needed before updating the new "full_hardware_class"
    field to our DeviceInfoSpecifics proto. We must check whether the user
    has given UMA consent before sending hardware class information apart of
    the Chrome Sync ping.
    
    Currently, the UMA consent can be retireved once the browser process
    begins by calling IsMetricsAndCrashReportingEnabled() in
    ChromeMetricsServiceAccessor class.
    
    Bug: 1188978
    Change-Id: I154de47c2acf1c6d5e2e44e13291a55f31bd8436
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2816348
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Auto-Submit: Hirthanan Subenderan <hirthanan@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873452}

---
+++ chrome/browser/sync/device_info_sync_client_impl.h
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SYNC_DEVICE_INFO_SYNC_CLIENT_IMPL_H_
+#define CHROME_BROWSER_SYNC_DEVICE_INFO_SYNC_CLIENT_IMPL_H_
+
+#include "base/macros.h"
+#include "base/optional.h"
+
+#include "components/sync_device_info/device_info_sync_client.h"
+
+class Profile;
+
+namespace browser_sync {
+
+class DeviceInfoSyncClientImpl : public syncer::DeviceInfoSyncClient {
+ public:
+  explicit DeviceInfoSyncClientImpl(Profile* profile);
+
+  ~DeviceInfoSyncClientImpl() override;
+
+  // syncer::DeviceInfoSyncClient:
+  std::string GetSigninScopedDeviceId() const override;
+
+  // syncer::DeviceInfoSyncClient:
+  bool GetSendTabToSelfReceivingEnabled() const override;
+
+  // syncer::DeviceInfoSyncClient:
+  base::Optional<syncer::DeviceInfo::SharingInfo> GetLocalSharingInfo()
+      const override;
+
+  // syncer::DeviceInfoSyncClient:
+  base::Optional<std::string> GetFCMRegistrationToken() const override;
+
+  // syncer::DeviceInfoSyncClient:
+  base::Optional<syncer::ModelTypeSet> GetInterestedDataTypes() const override;
+
+  // syncer::DeviceInfoSyncClient:
+  base::Optional<syncer::DeviceInfo::PhoneAsASecurityKeyInfo>
+  GetPhoneAsASecurityKeyInfo() const override;
+
+  // syncer::DeviceInfoSyncClient:
+  bool IsUmaEnabledOnCrOSDevice() const override;
+
+ private:
+  Profile* const profile_;
+
+  DISALLOW_COPY_AND_ASSIGN(DeviceInfoSyncClientImpl);
+};
+
+}  // namespace browser_sync
+
+#endif  // CHROME_BROWSER_SYNC_DEVICE_INFO_SYNC_CLIENT_IMPL_H_
