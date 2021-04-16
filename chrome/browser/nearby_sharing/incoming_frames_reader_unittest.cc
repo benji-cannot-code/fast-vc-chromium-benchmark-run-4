2ec0b7ac12b22944bdde756ae6b2d57509e34891
Claude van der Merwe <cvandermerwe@google.com>
2021-04-16 02:02:15 +0000

    [Nearby] Don't close connection in incoming frames reader
    This CL calls Done in IncomingFramesReader if the ReadFrame times
    out. This ensures that the ReadFrame callback is called when the
    ReadFrame times out so that the appropriate metric can get logged
    on the client.
    
    Before this change the connection would get closed which would
    rely on the client having a disconnect listener and as a byproduct
    always logging the kUnexpectedDisconnection if a disconnection
    listener was present.
    
    The change was manually verified by timing out the ReadFrame at
    all stages of incoming and outgoing transfers.
    
    The following metrics are now logged when:
    1. Timing out on first two ReadFrames kPairedKeyVerificationFailed
    is logged on both send and receive
    2. Timing out on third frame kFailedToReadOutgoingConnectionResponse
    is logged for outgoing transfers and kInvalidIntroductionFrame is
    logged for incoming transfers.
    
    If we want to log the frequency of frame reader timeouts we can
    log a separate metric in IncomingFramesReader
    
    Bug: 1193837
    Change-Id: I85f567d37cd1e73c9ce1e0e15c51d3c7315ef423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2824228
    Commit-Queue: Claude van der Merwe <cvandermerwe@google.com>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#873149}

--- chrome/browser/nearby_sharing/incoming_frames_reader_unittest.cc
+++ chrome/browser/nearby_sharing/incoming_frames_reader_unittest.cc
@@ -114,6 +114,12 @@ TEST_F(IncomingFramesReaderTest, ReadTimedOut) {
           }),
       kTimeout);
   run_loop.Run();
+
+  // Ensure that the OnDataReadFromConnection callback is not run since the read
+  // timed out.
+  EXPECT_FALSE(connection().has_read_callback_been_run());
+  // Ensure that the IncomingFramesReader does not close the connection.
+  EXPECT_FALSE(connection().IsClosed());
 }
 
 TEST_F(IncomingFramesReaderTest, ReadAnyFrameSuccessful) {
