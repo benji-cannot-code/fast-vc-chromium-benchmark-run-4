02fbf764474d82489af765326fb527f00eae8b00
Ian Kilpatrick <ikilpatrick@chromium.org>
2021-04-16 18:41:37 +0000

    [GridNG] Re-compute the grid if %-rows, %-gaps, or flex-rows are present
    This patch adds logic to rerun calculating the grid, if any percentage
    rows, gaps, or flexible rows are present.
    
    They re-resolve against the block-size calculated in the first pass.
    
    Bug: 1045599
    Change-Id: I3187ab53309616e83238ad47073ef670cdf3cd79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2791846
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ethan Jimenez <ethavar@microsoft.com>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#873391}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
@@ -209,6 +209,11 @@ class CORE_EXPORT NGGridLayoutAlgorithm
   struct SetGeometry {
     Vector<SetOffsetData> sets;
     LayoutUnit gutter_size;
+
+    LayoutUnit FinalGutterSize() const {
+      DCHECK_GT(sets.size(), 0u);
+      return (sets.size() == 1) ? LayoutUnit() : gutter_size;
+    }
   };
 
   // Typically we pass around both the column, and row geometry together.
@@ -344,8 +349,7 @@ class CORE_EXPORT NGGridLayoutAlgorithm
       const NGGridLayoutAlgorithmTrackCollection& track_collection) const;
 
   // Gets the row or column gap of the grid.
-  LayoutUnit GridGap(GridTrackSizingDirection track_direction,
-                     LayoutUnit available_size = kIndefiniteSize) const;
+  LayoutUnit GridGap(GridTrackSizingDirection track_direction) const;
 
   LayoutUnit DetermineFreeSpace(
       SizingConstraint sizing_constraint,
