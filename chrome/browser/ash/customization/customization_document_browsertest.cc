8c20ac0468186707932b83d7cd1e8f742e4323f3
Yeunjoo Choi <ychoi@igalia.com>
2021-04-16 13:18:19 +0000

    Move //chrome/browser/{chromeos => ash}/customization
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: I4c63c798e28fdff40578b95ab088b78c98c1dd16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831035
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#873277}

--- chrome/browser/ash/customization/customization_document_browsertest.cc
+++ chrome/browser/ash/customization/customization_document_browsertest.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/threading/thread_restrictions.h"
 #include "chrome/browser/ash/base/locale_util.h"
-#include "chrome/browser/chromeos/customization/customization_document.h"
+#include "chrome/browser/ash/customization/customization_document.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/webui/chromeos/login/l10n_util.h"
 #include "chrome/test/base/in_process_browser_test.h"
