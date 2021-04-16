0410dc4dad1b2bb651e5fae0cb6c286f29f27aa2
Daniel Cheng <dcheng@chromium.org>
2021-04-16 05:22:07 +0000

    [blink] Remove Page::PageClients.
    Prior to splitting up from WebKit, PageClients encapsulated much more
    state. Since it's only used to pass ChromeClient now, just remove it and
    have the callers pass ChromeClient directly.
    
    [1] https://trac.webkit.org/browser/webkit/trunk/Source/WebCore/page/PageConfiguration.h?rev=274192
    
    Change-Id: I6181be28daf9bcd04154e28b54dbb2701d118754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830458
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873202}

--- third_party/blink/renderer/core/loader/empty_clients.cc
+++ third_party/blink/renderer/core/loader/empty_clients.cc
@@ -47,10 +47,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void FillWithEmptyClients(Page::PageClients& page_clients) {
-  DEFINE_STATIC_LOCAL(Persistent<ChromeClient>, dummy_chrome_client,
+ChromeClient& GetStaticEmptyChromeClientInstance() {
+  DEFINE_STATIC_LOCAL(Persistent<ChromeClient>, chrome_client,
                       (MakeGarbageCollected<EmptyChromeClient>()));
-  page_clients.chrome_client = dummy_chrome_client;
+  return *chrome_client;
 }
 
 class EmptyPopupMenu : public PopupMenu {
