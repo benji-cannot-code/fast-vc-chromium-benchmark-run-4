6ded891da8c78b7f9dc934e4501405538f301ab0
Wenyu Fu <wenyufu@google.com>
2021-04-16 19:29:14 +0000

    [FirstRun] Respect uma check box state when FRE is recreated
    If for any reason the ToS Fragment is re-created (e.g. user hit
    backpress), respect the state of Uma Checkbox for the user had chosen
    before.
    
    Change-Id: Ib0571e8be4200e4e543f471715d60ef1bac6b05d
    Bug: 1197584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2818532
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873419}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/ToSAndUMAFirstRunFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/ToSAndUMAFirstRunFragment.java
@@ -24,6 +24,7 @@ import androidx.fragment.app.Fragment;
 
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.privacy.settings.PrivacyPreferencesManagerImpl;
 import org.chromium.chrome.browser.version.ChromeVersionInfo;
 import org.chromium.components.signin.ChildAccountStatus;
 import org.chromium.ui.text.NoUnderlineClickableSpan;
@@ -88,7 +89,7 @@ public class ToSAndUMAFirstRunFragment extends Fragment implements FirstRunFragm
 
         mAcceptButton.setOnClickListener((v) -> onTosButtonClicked());
 
-        mSendReportCheckBox.setChecked(FirstRunActivity.DEFAULT_METRICS_AND_CRASH_REPORTING);
+        mSendReportCheckBox.setChecked(getUmaCheckBoxInitialState());
         if (!canShowUmaCheckBox()) {
             mSendReportCheckBox.setVisibility(View.GONE);
         }
@@ -183,6 +184,7 @@ public class ToSAndUMAFirstRunFragment extends Fragment implements FirstRunFragm
         assert !isWaitingForNativeAndPolicyInit();
 
         setSpinnerVisible(false);
+        mSendReportCheckBox.setChecked(getUmaCheckBoxInitialState());
     }
 
     private void onTosButtonClicked() {
@@ -225,6 +227,15 @@ public class ToSAndUMAFirstRunFragment extends Fragment implements FirstRunFragm
 
     private boolean isWaitingForNativeAndPolicyInit() {
         return !mNativeInitialized || getPageDelegate().getPolicyLoadListener().get() == null;
+    }
+
+    private boolean getUmaCheckBoxInitialState() {
+        // The shared preference behind PrivacyPreferencesManagerImpl#isMetricsUploadPermitted is
+        // set after ToS is accepted. If ToS is not accepted yet, use the default value.
+        return FirstRunUtils.didAcceptTermsOfService()
+                ? PrivacyPreferencesManagerImpl.getInstance()
+                          .isUsageAndCrashReportingPermittedByUser()
+                : FirstRunActivity.DEFAULT_METRICS_AND_CRASH_REPORTING;
     }
 
     // Exposed methods for ToSAndUMACCTFirstRunFragment
