088edc7871d861ebbad82c12e5a061e4e36bedae
Michael Ludwig <michaelludwig@google.com>
2021-04-16 17:46:24 +0000

    Pass override PaintFlags and base CTM to PaintOp::Serialize
    This adds arguments to PaintOp::Serialize for the flags_to_serialize
    and original_ctm. These change regularly within the serialization of
    a PaintOpBuffer but are currently stored on SerializeOptions. This
    requires serialization to make copies of these options and prevents
    SerializeOptions from being constant for the entire invocation.
    
    This just adds the function parameters and updates call sites to
    pass through the parameters already stored in SerializeOptions
    (except unit tests, which use the appropriate values directly). In
    a follow-up CL, the fields will be removed from SerializeOptions
    entirely and PaintOpBufferSerializer will track the necessary
    data more succinctly.
    
    Bug: 1194701
    Change-Id: I26bf5fa43bb9578c5a62f1dbafa9d35ded81d6eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2800135
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873370}

--- cc/paint/paint_op_buffer_serializer.h
+++ cc/paint/paint_op_buffer_serializer.h
@@ -19,7 +19,9 @@ class CC_PAINT_EXPORT PaintOpBufferSerializer {
  public:
   using SerializeCallback =
       base::RepeatingCallback<size_t(const PaintOp*,
-                                     const PaintOp::SerializeOptions&)>;
+                                     const PaintOp::SerializeOptions&,
+                                     const PaintFlags*,
+                                     const SkM44&)>;
 
   PaintOpBufferSerializer(SerializeCallback serialize_cb,
                           ImageProvider* image_provider,
@@ -138,7 +140,9 @@ class CC_PAINT_EXPORT SimpleBufferSerializer : public PaintOpBufferSerializer {
 
  private:
   size_t SerializeToMemory(const PaintOp* op,
-                           const PaintOp::SerializeOptions& options);
+                           const PaintOp::SerializeOptions& options,
+                           const PaintFlags* flags_to_serialize,
+                           const SkM44& original_ctm);
 
   void* memory_;
   const size_t total_;
