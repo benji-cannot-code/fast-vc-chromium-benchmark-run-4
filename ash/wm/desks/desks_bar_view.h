ccbe1edb9d0f6134881f57750964b001aeadd45f
minch <minch@chromium.org>
2021-04-16 23:51:41 +0000

    bento_v1: Update the logic to show the gradients
    Current logic to show the gradients,
    - Show the both sides gradients during scroll.
    - Show the start/end gradient only in last page with LTR/RTL layout.
    - Show the end/start gradient if there are contents beyond the
      right/left of the scroll view's visible bounds.
    
    Adjust the desks bar's scroll position on scroll ends to make sure no
    desk preview is cropped at the scroll bar's start position.
    
    Bug: 1188139
    Change-Id: Ib0c9016edab494707e8a8f43593080bb71261765
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2802612
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873496}

--- ash/wm/desks/desks_bar_view.h
+++ ash/wm/desks/desks_bar_view.h
@@ -154,6 +154,7 @@ class ASH_EXPORT DesksBarView : public views::View,
 
   // views::ScrollView::Observer:
   void OnContentsScrolled() override;
+  void OnContentsScrollEnded() override;
 
   // This is called on initialization, creating a new desk through the
   // NewDeskButton or ExpandedStateNewDeskButton, or expanding from zero state
@@ -179,6 +180,9 @@ class ASH_EXPORT DesksBarView : public views::View,
   const DeskMiniView* GetDragDeskMiniViewForTesting() const {
     return drag_view_;
   }
+
+  bool IsLeftGradientVisibleForTesting() const;
+  bool IsRightGradientVisibleForTesting() const;
 
  private:
   friend class DesksBarScrollViewLayout;
@@ -223,6 +227,10 @@ class ASH_EXPORT DesksBarView : public views::View,
   // will be clipped and can not be seen.
   void ScrollToPreviousPage();
   void ScrollToNextPage();
+
+  // Gets the adjusted scroll position based on |position| to make sure no desk
+  // preview is cropped at the start position of the scrollable bar.
+  int GetAdjustedUncroppedScrollPosition(int position) const;
 
   // A view that shows a dark gary transparent background that can be animated
   // when the very first mini_views are created.
--- ui/views/animation/scroll_animator.cc
+++ ui/views/animation/scroll_animator.cc
@@ -64,6 +64,7 @@ void ScrollAnimator::Stop() {
 
 void ScrollAnimator::AnimationEnded(const gfx::Animation* animation) {
   Stop();
+  delegate_->OnFlingScrollEnded();
 }
 
 void ScrollAnimator::AnimationProgressed(const gfx::Animation* animation) {
