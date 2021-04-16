6b798ca9afaa4162606736b2267549f6271273b3
adamta <adamta@google.com>
2021-04-16 20:18:23 +0000

    [iOS] Set header synchronizer to NTP mediator in refactored NTP
    Header Synchronizer was not assigned to NTP mediator in the refactored
    NTP, so it could not get the pinned offset before tapping the omnibox.
    
    Bug: 1192575
    Change-Id: I2c3c54a3d03f406c0e0acef8e9b860f6cc660f5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2826205
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Adam Trudeau-Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/master@{#873450}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -256,6 +256,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.ntpViewController.headerController =
         self.contentSuggestionsCoordinator.headerController;
     self.ntpMediator.primaryViewController = self.ntpViewController;
+    self.ntpMediator.headerCollectionInteractionHandler =
+        self.headerSynchronizer;
   }
 
   base::RecordAction(base::UserMetricsAction("MobileNTPShowMostVisited"));
