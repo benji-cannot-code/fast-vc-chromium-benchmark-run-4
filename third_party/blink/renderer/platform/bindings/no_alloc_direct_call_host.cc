13de41b2eb7ee183f2919d81b14f24bfa17e9afa
Justin Novosad <junov@chromium.org>
2021-04-16 18:36:14 +0000

    Remove fallback mechanism from NoAllocDirectCallHost
    The NoAllocDirectCall fallback mechanism was identified as a risk for
    stability and security due to how easy it is to violate the idempotence
    requirement in the blink-side method implementations. Since we do not
    absolutely need fallback support for deploying [NoAllocDirectCall], we
    can remove it.
    
    Bug: 1198320
    Change-Id: I43f0c808afa39850c18b0b52ef5f9f860965c059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830831
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Auto-Submit: Justin Novosad <junov@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873387}

--- third_party/blink/renderer/platform/bindings/no_alloc_direct_call_host.cc
+++ third_party/blink/renderer/platform/bindings/no_alloc_direct_call_host.cc
@@ -8,11 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 void NoAllocDirectCallHost::PostDeferrableAction(DeferrableAction&& action) {
-  // This should never be called after NoAllocFallbackForAllocation
-#if DCHECK_IS_ON()
-  DCHECK(can_post_deferrable_actions_);
-#endif
-
   if (IsInFastMode()) {
     deferred_actions_.push_back(std::move(action));
     callback_options_->fallback = true;
@@ -20,23 +15,6 @@ void NoAllocDirectCallHost::PostDeferrableAction(DeferrableAction&& action) {
     // In slow mode, action is executed immediately.
     std::move(action).Run();
   }
-}
-
-bool NoAllocDirectCallHost::NoAllocFallbackForAllocation() {
-  if (IsInFastMode()) {
-    // Discarding deferred actions means that FlushDeferredActions will return
-    // false, which will result in the fallback bindings code path not taking
-    // the early exit and re-calling the API implementation with allocations
-    // allowed.
-    deferred_actions_.clear();
-    callback_options_->fallback = true;
-#if DCHECK_IS_ON()
-    can_post_deferrable_actions_ = false;
-#endif
-    return true;
-  }
-  // Already in the fallback code path.
-  return false;
 }
 
 void NoAllocDirectCallHost::FlushDeferredActions() {
