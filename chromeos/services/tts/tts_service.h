12ebe907b94875ec62f91af9a68f737cb935abb7
David Tseng <dtseng@google.com>
2021-04-16 23:57:16 +0000

    Simplify TtsStreamFactory receiver handling with ReceiverSet
    R=dmazzoni@chromium.org
    
    Change-Id: I33b357befd46556c1a0eb1923e112256ce95825b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830120
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873507}

--- chromeos/services/tts/tts_service.h
+++ chromeos/services/tts/tts_service.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "library_loaders/libchrometts.h"
 #include "media/base/audio_renderer_sink.h"
 #include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
 namespace audio {
@@ -59,13 +60,9 @@ class TtsService : public mojom::TtsService,
   // Maybe exit this process.
   void MaybeExit();
 
-  mojo::Receiver<mojom::TtsStreamFactory>* tts_stream_factory_for_testing() {
-    return &tts_stream_factory_;
-  }
-
-  std::queue<mojo::PendingReceiver<mojom::TtsStreamFactory>>&
-  pending_tts_stream_factory_receivers_for_testing() {
-    return pending_tts_stream_factory_receivers_;
+  mojo::ReceiverSet<mojom::TtsStreamFactory>*
+  tts_stream_factory_receivers_for_testing() {
+    return &tts_stream_factory_receivers_;
   }
 
   void set_keep_process_alive_for_testing(bool value) {
@@ -98,9 +95,6 @@ class TtsService : public mojom::TtsService,
   void StopLocked(bool clear_buffers = true)
       EXCLUSIVE_LOCKS_REQUIRED(state_lock_);
 
-  // Satisfies any pending tts stream factory receivers.
-  void ProcessPendingTtsStreamFactories();
-
   // Do any processing (e.g. sending start/end events) on buffers that have just
   // been rendered on the audio thread.
   void ProcessRenderedBuffers();
@@ -108,12 +102,8 @@ class TtsService : public mojom::TtsService,
   // Connection to tts in the browser.
   mojo::Receiver<mojom::TtsService> service_receiver_;
 
-  // Factory creating various types of streams.
-  mojo::Receiver<mojom::TtsStreamFactory> tts_stream_factory_;
-
-  // A list of pending component extension requesting a tts stream factory.
-  std::queue<mojo::PendingReceiver<mojom::TtsStreamFactory>>
-      pending_tts_stream_factory_receivers_;
+  // A list of active component extension tts stream factory connections.
+  mojo::ReceiverSet<mojom::TtsStreamFactory> tts_stream_factory_receivers_;
 
   std::unique_ptr<GoogleTtsStream> google_tts_stream_;
 
