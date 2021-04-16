1cc6427dfc6cd7d8369027d30dc6d52b8ad0e84e
Asami Doi <asamidoi@chromium.org>
2021-04-16 09:49:58 +0000

    Prerender: rename files under web_tests/wpt_internal/prerender
    This CL just renames file under web_tests/wpt_internal/prerender
    to align with the following suggestion.
    > File names should use `snake-case`, but preserve the case of any embedded API
    names. For example, prefer `document-createElement.html` to
    `document-create-element.html`.
    https://source.chromium.org/chromium/chromium/src/+/master:docs/testing/web_tests_tips.md;l=165
    
    No behavior change.
    
    Bug: 1126305
    Change-Id: Ia42d6eb5864d232564239465368a84f44862b25e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831197
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873240}

--- third_party/blink/web_tests/wpt_internal/prerender/resources/utils.js
+++ third_party/blink/web_tests/wpt_internal/prerender/resources/utils.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-const STORE_URL = '/wpt_internal/prerender/resources/key_value_store.py';
+const STORE_URL = '/wpt_internal/prerender/resources/key-value-store.py';
 
 // Starts prerendering for `url`.
 function startPrerendering(url) {
