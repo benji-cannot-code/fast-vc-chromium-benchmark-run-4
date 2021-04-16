cfa12c8b070d8c4a20eba5d4125fbe68ef5a1ff0
Miyoung Shin <myid.shin@igalia.com>
2021-04-16 09:59:08 +0000

    Reland "Fix the wrong direction with disabling CSSPseudoDir flag"
    This is a reland of 5743763d4ddfdaf16db6cd35d721b50584fcc24f,
    which was reverted in crrev.com/c/2830777. The build failure was
    a linking issue caused by not including headers explicitly.
    
    Original change's description:
    > Fix the wrong direction with disabling CSSPseudoDir flag
    >
    > This CL fixes the wrong direction in style caused by blocked
    > recalculating the directionality of dir=auto when children are changed.
    >
    > Bug: 1199070
    > Change-Id: I780e8dce9ff0cc4ade56250c85bd7ac6c86df781
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2825939
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Reviewed-by: Mason Freed <masonf@chromium.org>
    > Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#873124}
    
    Bug: 1199070
    Change-Id: Ic75d659add03bf135b1589e8d16b55f841ea79be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831325
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#873242}

--- third_party/blink/renderer/core/html/html_element_test.cc
+++ third_party/blink/renderer/core/html/html_element_test.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_element.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
+#include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
 
 namespace blink {
@@ -245,6 +247,20 @@ TEST_F(HTMLElementTest,
             GetDocument().getElementById("box"));
   EXPECT_FALSE(
       GetDocument().GetPage()->Animator().has_inline_style_mutation_for_test());
+}
+
+TEST_F(HTMLElementTest, DirAutoByChildChanged) {
+  ScopedCSSPseudoDirForTest scoped_feature(false);
+
+  SetBodyInnerHTML("<div id='target' dir='auto'></div>");
+  auto* element = GetDocument().getElementById("target");
+  element->setTextContent(u"\u05D1");
+  UpdateAllLifecyclePhasesForTest();
+  EXPECT_EQ(element->GetComputedStyle()->Direction(), TextDirection::kRtl);
+
+  element->RemoveChildren();
+  UpdateAllLifecyclePhasesForTest();
+  EXPECT_EQ(element->GetComputedStyle()->Direction(), TextDirection::kLtr);
 }
 
 }  // namespace blink
