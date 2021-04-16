9107e822f5084f56672edb90671e3325f14b8c8f
Lei Zhang <thestig@chromium.org>
2021-04-16 18:11:54 +0000

    Clean up includes in discardable_memory_utils.cc.
    There are many includes left over from https://crrev.com/713010 that are
    no longer needed. Plus they were in the wrong order and sometimes
    incorrectly used angle brackets instead of quotes.
    
    Change-Id: I0daa4ff515fd4f4f83cfbf2bec4f97499c4bb9ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798537
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873379}

--- content/renderer/discardable_memory_utils.cc
+++ content/renderer/discardable_memory_utils.cc
@@ -3,26 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "content/renderer/discardable_memory_utils.h"
+
 #include <utility>
 
-#include "base/command_line.h"
 #include "base/memory/discardable_memory.h"
 #include "content/child/child_process.h"
 #include "content/child/child_thread_impl.h"
-#include "content/public/common/content_switches.h"
-#include "content/renderer/discardable_memory_utils.h"
-
-#if defined(OS_POSIX)
-#include <sys/mman.h>
-#include <sys/utsname.h>
-
-#include "base/memory/madv_free_discardable_memory_allocator_posix.h"
-#include "base/memory/madv_free_discardable_memory_posix.h"
-#endif
-
-#if defined(OS_ANDROID)
-#include <third_party/ashmem/ashmem.h>
-#endif
 
 namespace content {
 
