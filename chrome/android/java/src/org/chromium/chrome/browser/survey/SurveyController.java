99afb5d3191532d479fa2d73488202b9e3a76210
Wenyu Fu <wenyufu@google.com>
2021-04-16 21:09:51 +0000

    [Survey] Add test to download and show survey infobar
    Add test to a different file, as it introduce a lot of shadows.
    
    Change-Id: I9e08ffcb98b9a2cc890e2afcf1c9e8f2f29f961e
    Bug: 1198362
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822986
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873475}

--- chrome/android/java/src/org/chromium/chrome/browser/survey/SurveyController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/survey/SurveyController.java
@@ -8,6 +8,8 @@ package org.chromium.chrome.browser.survey;
 import android.app.Activity;
 import android.content.Context;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.chromium.chrome.browser.AppHooks;
 
 /**
@@ -26,6 +28,11 @@ public class SurveyController {
             sInstance = AppHooks.get().createSurveyController();
         }
         return sInstance;
+    }
+
+    @VisibleForTesting
+    static void setInstanceForTesting(SurveyController testInstance) {
+        sInstance = testInstance;
     }
 
     /**
