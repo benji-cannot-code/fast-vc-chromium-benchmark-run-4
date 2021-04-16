87e3304c714581bb497419cd54cce783a36d4f3f
Steven Bingler <bingler@chromium.org>
2021-04-16 00:23:42 +0000

    Create StorageKey class
    Create a new class to abstract away the key used by storage APIs.
    It's currently modeled after the key used by the Service Worker DB and
    we may need to modify it and/or modify other APIs to match.
    
    This CL just creates the class, subsequent CLs will begin using it.
    
    Bug: 1199077
    Change-Id: Iad870c62516b8ebfba4084e69c67adff31279b4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827130
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873121}

---
+++ components/services/storage/public/cpp/storage_key.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/services/storage/public/cpp/storage_key.h"
+
+#include "url/gurl.h"
+
+namespace storage {
+
+StorageKey StorageKey::Deserialize(const std::string& in) {
+  return StorageKey(url::Origin::Create(GURL(in)));
+}
+
+std::string StorageKey::Serialize() const {
+  DCHECK(!opaque());
+  return origin_.GetURL().spec();
+}
+
+bool operator==(const StorageKey& lhs, const StorageKey& rhs) {
+  return lhs.origin_ == rhs.origin_;
+}
+
+bool operator!=(const StorageKey& lhs, const StorageKey& rhs) {
+  return !(lhs == rhs);
+}
+
+}  // namespace storage
