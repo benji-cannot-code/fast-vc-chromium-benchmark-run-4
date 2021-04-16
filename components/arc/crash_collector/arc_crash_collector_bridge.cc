ff25b7280e44c73f66717202ffc2395d1460a2d4
Ryo Hashimoto <hashimoto@chromium.org>
2021-04-16 02:32:18 +0000

    arc: Stop executing crash_collector with --arc_is_arcvm
    This switch has no effect after CL:2810105
    
    BUG=chromium:1196588
    TEST=tast run "arc.AppCrash*" "arc.CxxCrash*" "arc.KernelCrash*"
    
    Change-Id: I4be585c62ea348464358795c148893a73a7b3c01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2823032
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873158}

--- components/arc/crash_collector/arc_crash_collector_bridge.cc
+++ components/arc/crash_collector/arc_crash_collector_bridge.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
-#include "components/arc/arc_util.h"
 #include "components/arc/session/arc_bridge_service.h"
 #include "mojo/public/cpp/system/platform_handle.h"
 
@@ -183,9 +182,6 @@ std::vector<std::string> ArcCrashCollectorBridge::CreateCrashReporterArgs() {
   };
   if (fingerprint_)
     args.push_back("--arc_fingerprint=" + fingerprint_.value());
-
-  if (arc::IsArcVmEnabled())
-    args.emplace_back("--arc_is_arcvm");
 
   return args;
 }
