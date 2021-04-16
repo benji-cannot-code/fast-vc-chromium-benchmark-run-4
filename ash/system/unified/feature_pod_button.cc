899e7a82bcde8b15090993c6e9cbfb4c0ba210e3
Andre Le <leandre@chromium.org>
2021-04-16 18:58:15 +0000

    Quick Settings: Allow feature pods to have 2-line title.
    If a feature pod has no sub-label/description, allowing it to have 2
    lines in title label to avoid text eliding.
    
    Fixed: 1195876
    Change-Id: I24ca0f5bb16519b123de248284f7cd934f27eadf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814000
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873405}

--- ash/system/unified/feature_pod_button.cc
+++ ash/system/unified/feature_pod_button.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
+#include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_popup_utils.h"
 #include "ash/system/unified/feature_pod_controller_base.h"
 #include "ui/accessibility/ax_enums.mojom.h"
@@ -173,6 +174,8 @@ FeaturePodLabelButton::FeaturePodLabelButton(PressedCallback callback)
   GetViewAccessibility().OverrideIsLeaf(true);
 
   label_->SetLineHeight(kUnifiedFeaturePodLabelLineHeight);
+  label_->SetMultiLine(true);
+  label_->SetMaxLines(kUnifiedFeaturePodLabelMaxLines);
   ConfigureFeaturePodLabel(label_, kUnifiedFeaturePodLabelLineHeight,
                            kUnifiedFeaturePodLabelFontSize);
   ConfigureFeaturePodLabel(sub_label_, kUnifiedFeaturePodSubLabelLineHeight,
@@ -285,7 +288,8 @@ const std::u16string& FeaturePodLabelButton::GetLabelText() const {
 
 void FeaturePodLabelButton::SetSubLabel(const std::u16string& sub_label) {
   sub_label_->SetText(sub_label);
-  sub_label_->SetVisible(true);
+  sub_label_->SetVisible(!sub_label.empty());
+  label_->SetMultiLine(sub_label.empty());
   InvalidateLayout();
 }
 
