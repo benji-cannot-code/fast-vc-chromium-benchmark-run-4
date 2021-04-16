31b6acfcc7f11d6b6460b52746e7960621493d74
Kinuko Yasuda <kinuko@chromium.org>
2021-04-16 07:13:10 +0000

    Revert "wtf: Reinstate thread check in DEFINE_STATIC_LOCAL."
    This reverts commit 0eb4dfcd81ee47b38d3c878d6c5056256034c3fe.
    
    Reason for revert: broke tests by several assertion hits on Mac
    
    The right approach would be to fix the callsite and reland this as enabling this assertion itself is great.
    
    Tests:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.15%20Tests%20(dbg)/303/test-results?q=
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.15%20Tests%20(dbg)/306/test-results?q=
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.15%20Tests%20(dbg)/307/test-results?q=
    
    Original change's description:
    > wtf: Reinstate thread check in DEFINE_STATIC_LOCAL.
    >
    > DEFINE_STATIC_LOCAL() has a built-in assert that makes sure the variable
    > is created on a non-racy manner. Unfortunately, this has not been
    > working since the required WTF::WillCreateThread() call was accidentally
    > removed.
    >
    > This CL reenables the check by reintroducing a call to
    > WillCreateThread() when a child thread is about to be created.
    >
    > Bug: 1074302
    > Change-Id: Ia2ce3d344d885eca32300b36aa23586af06f242e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2777626
    > Commit-Queue: Kentaro Hara <haraken@chromium.org>
    > Auto-Submit: Yuta Kitamura <yutak@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#872293}
    
    Bug: 1074302
    Change-Id: If318ffdf505e2db1c8d47f8b36e80d0035a6b461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830181
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Owners-Override: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873224}

--- third_party/blink/renderer/platform/scheduler/common/thread.cc
+++ third_party/blink/renderer/platform/scheduler/common/thread.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 #include "third_party/blink/renderer/platform/wtf/thread_specific.h"
-#include "third_party/blink/renderer/platform/wtf/threading.h"
 
 #if defined(OS_WIN)
 #include <windows.h>
@@ -81,9 +80,6 @@ ThreadCreationParams& ThreadCreationParams::SetSupportsGC(bool gc_enabled) {
 
 std::unique_ptr<Thread> Thread::CreateThread(
     const ThreadCreationParams& params) {
-#if DCHECK_IS_ON()
-  WTF::WillCreateThread();
-#endif
   auto thread = std::make_unique<scheduler::WorkerThread>(params);
   thread->Init();
   return std::move(thread);
