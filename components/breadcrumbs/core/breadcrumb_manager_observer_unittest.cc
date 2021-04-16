99f39743c0502b94acd5df6b4f9ed4709b5671f5
Jesse McKenna <jessemckenna@google.com>
2021-04-16 23:59:24 +0000

    breadcrumbs: move BreadcrumbManagerObserver test
    This change moves breadcrumb_manager_observer_unittest.cc to the new
    breadcrumbs component. The class it tests has already been migrated.
    
    Bug: 1163762
    Change-Id: I4e3382e3bbab1c346ffe4182a117ce5de0bf1a8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2829793
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/master@{#873510}

--- components/breadcrumbs/core/breadcrumb_manager_observer_unittest.cc
+++ components/breadcrumbs/core/breadcrumb_manager_observer_unittest.cc
@@ -5,17 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/breadcrumbs/core/breadcrumb_manager_observer.h"
 
+#include <string>
+
+#include "base/test/task_environment.h"
+#include "base/time/time.h"
 #include "components/breadcrumbs/core/breadcrumb_manager.h"
-#import "ios/web/public/test/web_task_environment.h"
+#include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
+namespace breadcrumbs {
 
 namespace {
-class FakeBreadcrumbManagerObserver
-    : public breadcrumbs::BreadcrumbManagerObserver {
+
+class FakeBreadcrumbManagerObserver : public BreadcrumbManagerObserver {
  public:
   FakeBreadcrumbManagerObserver() {}
   ~FakeBreadcrumbManagerObserver() override = default;
@@ -25,23 +27,23 @@ class FakeBreadcrumbManagerObserver
       const FakeBreadcrumbManagerObserver&) = delete;
 
   // BreadcrumbManagerObserver
-  void EventAdded(breadcrumbs::BreadcrumbManager* manager,
+  void EventAdded(BreadcrumbManager* manager,
                   const std::string& event) override {
     event_added_last_received_manager_ = manager;
     event_added_last_received_event_ = event;
   }
 
-  void OldEventsRemoved(breadcrumbs::BreadcrumbManager* manager) override {
+  void OldEventsRemoved(BreadcrumbManager* manager) override {
     old_events_removed_last_received_manager_ = manager;
   }
 
-  breadcrumbs::BreadcrumbManager* event_added_last_received_manager_ = nullptr;
+  BreadcrumbManager* event_added_last_received_manager_ = nullptr;
   std::string event_added_last_received_event_;
 
-  breadcrumbs::BreadcrumbManager* old_events_removed_last_received_manager_ =
-      nullptr;
+  BreadcrumbManager* old_events_removed_last_received_manager_ = nullptr;
 };
-}
+
+}  // namespace
 
 class BreadcrumbManagerObserverTest : public PlatformTest {
  protected:
@@ -51,11 +53,10 @@ class BreadcrumbManagerObserverTest : public PlatformTest {
     manager_.RemoveObserver(&observer_);
   }
 
-  web::WebTaskEnvironment task_env_{
-      web::WebTaskEnvironment::Options::DEFAULT,
+  base::test::TaskEnvironment task_env_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
 
-  breadcrumbs::BreadcrumbManager manager_;
+  BreadcrumbManager manager_;
   FakeBreadcrumbManagerObserver observer_;
 };
 
@@ -88,3 +89,5 @@ TEST_F(BreadcrumbManagerObserverTest, OldEventsRemoved) {
 
   EXPECT_EQ(&manager_, observer_.old_events_removed_last_received_manager_);
 }
+
+}  // namespace breadcrumbs
