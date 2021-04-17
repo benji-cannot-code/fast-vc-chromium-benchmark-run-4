3b5113944eccef6a725acb9ac7539e8110a88dfa
Minoru Chikamune <chikamune@chromium.org>
2021-04-17 07:13:40 +0000

    CodeHealth: Remove use of Value::GetAsInteger
    This change removes the use of Value::GetAsInteger and replaces with
    Value::is_int(), Value::GetInt(), Value::GetIfInt().
    
    Bug: 1187004
    Change-Id: Idbaa231784ab0ab3e1e9c0797e0467ab8dfa7f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830665
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873626}

--- components/prefs/pref_service_unittest.cc
+++ components/prefs/pref_service_unittest.cc
@@ -159,9 +159,8 @@ TEST(PrefServiceTest, GetValueChangedType) {
   const base::Value* value = pref->GetValue();
   ASSERT_TRUE(value);
   EXPECT_EQ(base::Value::Type::INTEGER, value->type());
-  int actual_int_value = -1;
-  EXPECT_TRUE(value->GetAsInteger(&actual_int_value));
-  EXPECT_EQ(kTestValue, actual_int_value);
+  ASSERT_TRUE(value->is_int());
+  EXPECT_EQ(kTestValue, value->GetInt());
 }
 
 TEST(PrefServiceTest, GetValueAndGetRecommendedValue) {
@@ -179,9 +178,8 @@ TEST(PrefServiceTest, GetValueAndGetRecommendedValue) {
   const base::Value* value = pref->GetValue();
   ASSERT_TRUE(value);
   EXPECT_EQ(base::Value::Type::INTEGER, value->type());
-  int actual_int_value = -1;
-  EXPECT_TRUE(value->GetAsInteger(&actual_int_value));
-  EXPECT_EQ(kDefaultValue, actual_int_value);
+  ASSERT_TRUE(value->is_int());
+  EXPECT_EQ(kDefaultValue, value->GetInt());
 
   // Check that GetRecommendedValue() returns no value.
   value = pref->GetRecommendedValue();
@@ -194,9 +192,8 @@ TEST(PrefServiceTest, GetValueAndGetRecommendedValue) {
   value = pref->GetValue();
   ASSERT_TRUE(value);
   EXPECT_EQ(base::Value::Type::INTEGER, value->type());
-  actual_int_value = -1;
-  EXPECT_TRUE(value->GetAsInteger(&actual_int_value));
-  EXPECT_EQ(kUserValue, actual_int_value);
+  ASSERT_TRUE(value->is_int());
+  EXPECT_EQ(kUserValue, value->GetInt());
 
   // Check that GetRecommendedValue() returns no value.
   value = pref->GetRecommendedValue();
@@ -210,17 +207,15 @@ TEST(PrefServiceTest, GetValueAndGetRecommendedValue) {
   value = pref->GetValue();
   ASSERT_TRUE(value);
   EXPECT_EQ(base::Value::Type::INTEGER, value->type());
-  actual_int_value = -1;
-  EXPECT_TRUE(value->GetAsInteger(&actual_int_value));
-  EXPECT_EQ(kUserValue, actual_int_value);
+  ASSERT_TRUE(value->is_int());
+  EXPECT_EQ(kUserValue, value->GetInt());
 
   // Check that GetRecommendedValue() returns the recommended value.
   value = pref->GetRecommendedValue();
   ASSERT_TRUE(value);
   EXPECT_EQ(base::Value::Type::INTEGER, value->type());
-  actual_int_value = -1;
-  EXPECT_TRUE(value->GetAsInteger(&actual_int_value));
-  EXPECT_EQ(kRecommendedValue, actual_int_value);
+  ASSERT_TRUE(value->is_int());
+  EXPECT_EQ(kRecommendedValue, value->GetInt());
 
   // Remove the user-set value.
   prefs.RemoveUserPref(kPrefName);
@@ -229,17 +224,15 @@ TEST(PrefServiceTest, GetValueAndGetRecommendedValue) {
   value = pref->GetValue();
   ASSERT_TRUE(value);
   EXPECT_EQ(base::Value::Type::INTEGER, value->type());
-  actual_int_value = -1;
-  EXPECT_TRUE(value->GetAsInteger(&actual_int_value));
-  EXPECT_EQ(kRecommendedValue, actual_int_value);
+  ASSERT_TRUE(value->is_int());
+  EXPECT_EQ(kRecommendedValue, value->GetInt());
 
   // Check that GetRecommendedValue() returns the recommended value.
   value = pref->GetRecommendedValue();
   ASSERT_TRUE(value);
   EXPECT_EQ(base::Value::Type::INTEGER, value->type());
-  actual_int_value = -1;
-  EXPECT_TRUE(value->GetAsInteger(&actual_int_value));
-  EXPECT_EQ(kRecommendedValue, actual_int_value);
+  ASSERT_TRUE(value->is_int());
+  EXPECT_EQ(kRecommendedValue, value->GetInt());
 }
 
 TEST(PrefServiceTest, SetTimeValue_RegularTime) {
