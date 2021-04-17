d8bdeaf3cde58667097ec061aaf808be7f39f1d8
Shengsong Tan <sstan@google.com>
2021-04-17 15:10:01 +0000

    full_restore: Introduce Observer for ArcWindowHandler
    This observer is mainly used to notify the shell surface (window),
    which are managed by ArcWindowHandler the AppInstance is connected. So
    that the window should start syncing their state and bounds back to
    ARC to keep consistancy.
    
    BUG=b:177405617
    
    Change-Id: I9e86cfdecaae7fc7ac3b33c28052b1003f80f338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831328
    Commit-Queue: Shengsong Tan <sstan@google.com>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873645}

--- chrome/browser/chromeos/full_restore/arc_window_handler.h
+++ chrome/browser/chromeos/full_restore/arc_window_handler.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_FULL_RESTORE_ARC_WINDOW_HANDLER_H_
 #define CHROME_BROWSER_CHROMEOS_FULL_RESTORE_ARC_WINDOW_HANDLER_H_
 
+#include "base/observer_list.h"
+#include "base/observer_list_types.h"
 #include "components/exo/client_controlled_shell_surface.h"
 #include "components/exo/wm_helper.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
@@ -50,21 +52,33 @@ class ArcWindowHandler {
     ShellSurfaceMap* session_id_map_;
   };
 
+  // This class is used to notify observers that AppInstance is connected.
+  class Observer : public base::CheckedObserver {
+   public:
+    // Observer for app instance connection ready.
+    virtual void OnAppInstanceConnected() {}
+
+   protected:
+    ~Observer() override = default;
+  };
+
  public:
   ArcWindowHandler();
   ArcWindowHandler(const ArcWindowHandler&) = delete;
   ArcWindowHandler& operator=(const ArcWindowHandler&) = delete;
   ~ArcWindowHandler();
 
+  void AddObserver(Observer* observer);
+  void RemoveObserver(Observer* observer);
+  bool HasObserver(Observer* observer);
+
   void OnAppInstanceConnected();
 
  private:
-  // The ghost windows would not send window info to app instance if the
-  // instance had not connected yet.
-  bool app_instance_connected_ = false;
-
   // Map window session id to ClientControlledShellSurface.
   ShellSurfaceMap session_id_to_shell_surface_;
+
+  base::ObserverList<Observer> observer_list_;
 };
 
 }  // namespace full_restore
