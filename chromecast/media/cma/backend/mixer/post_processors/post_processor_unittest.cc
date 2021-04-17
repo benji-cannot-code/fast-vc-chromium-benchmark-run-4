8e6556bc52b4fd740b03138a6b923ed809b7afec
Jin Chen <jinjiechen@google.com>
2021-04-17 02:30:14 +0000

    [chromecast] Fix post processor unittest
    * Allows linking to use utility functions without instantiating the
    parameterized test.
    
    Bug: internal b/184766385
    Test: CQ, build chromecast
    Change-Id: Iaae06caf7bcf0851f7cfdca78543a1b959902684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2832524
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: JinJie Chen <jinjiechen@google.com>
    Cr-Commit-Position: refs/heads/master@{#873592}

--- chromecast/media/cma/backend/mixer/post_processors/post_processor_unittest.cc
+++ chromecast/media/cma/backend/mixer/post_processors/post_processor_unittest.cc
@@ -349,6 +349,8 @@ INSTANTIATE_TEST_SUITE_P(SampleRates,
                          PostProcessorTest,
                          ::testing::Values(44100, 48000));
 
+GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(PostProcessorTest);
+
 }  // namespace post_processor_test
 }  // namespace media
 }  // namespace chromecast
