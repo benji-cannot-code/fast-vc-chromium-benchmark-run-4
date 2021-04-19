b023514ff13a744e5295669b08dc3d89e2e82f09
Tanmoy Mollik <triploblastic@chromium.org>
2021-04-19 13:53:35 +0000

    [Android][Test] Replace MockitoAnnotations.initMocks() in signin/ tests
    This cl replaces MockitoAnnotations.initMocks() with
    MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS) for tests in
    signin/
    
    Bug: 1186674
    Change-Id: I9c8038becb684574f637e3d3abec30aa8091b18e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2822151
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873789}

--- chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninBridgeTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninBridgeTest.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.signin;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
 
 import androidx.test.filters.SmallTest;
 
@@ -18,6 +17,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.quality.Strictness;
 import org.robolectric.annotation.Config;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
@@ -62,6 +64,9 @@ public class SigninBridgeTest {
     @Rule
     public final AccountManagerTestRule mAccountManagerTestRule = new AccountManagerTestRule();
 
+    @Rule
+    public final MockitoRule mMockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
+
     @Mock
     private Profile mProfileMock;
 
@@ -79,7 +84,6 @@ public class SigninBridgeTest {
 
     @Before
     public void setUp() {
-        initMocks(this);
         UmaRecorderHolder.setNonNativeDelegate(mUmaRecorderMock);
         Profile.setLastUsedProfileForTesting(mProfileMock);
         IdentityServicesProvider.setInstanceForTests(mIdentityServicesProviderMock);
