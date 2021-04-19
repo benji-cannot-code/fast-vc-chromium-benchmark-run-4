ff5e47885d0862f67dd1b691e67716933cfd0aa9
Scott Haseley <shaseley@chromium.org>
2021-04-19 18:25:03 +0000

    [scheduling APIs] Tasks scheduled with an aborted signal should use AbortError
    The postTask result promise was rejected with a NotSupportedError rather
    than an AbortError when an aborted signal was passed to postTask. This
    removes the RejectPromiseImmediately() helper function that hardcoded
    the exception code, and uses the appropriate code at the callsite.
    
    Bug: 979017
    Change-Id: I8b3dbf3e27a44372f986c04b471c95ccd646279c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2832335
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873884}

--- third_party/blink/renderer/modules/scheduler/dom_scheduler.cc
+++ third_party/blink/renderer/modules/scheduler/dom_scheduler.cc
@@ -19,18 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-namespace {
-
-static ScriptPromise RejectPromiseImmediately(ExceptionState& exception_state) {
-  // The bindings layer implicitly converts thrown exceptions in
-  // promise-returning functions to promise rejections.
-  exception_state.ThrowDOMException(DOMExceptionCode::kNotSupportedError,
-                                    "Current window is detached");
-  return ScriptPromise();
-}
-
-}  // namespace
-
 const char DOMScheduler::kSupplementName[] = "DOMScheduler";
 
 DOMScheduler* DOMScheduler::scheduler(LocalDOMWindow& window) {
@@ -71,10 +59,18 @@ ScriptPromise DOMScheduler::postTask(
     V8SchedulerPostTaskCallback* callback_function,
     SchedulerPostTaskOptions* options,
     ExceptionState& exception_state) {
-  if (!GetExecutionContext() || GetExecutionContext()->IsContextDestroyed())
-    return RejectPromiseImmediately(exception_state);
-  if (options->signal() && options->signal()->aborted())
-    return RejectPromiseImmediately(exception_state);
+  if (!GetExecutionContext() || GetExecutionContext()->IsContextDestroyed()) {
+    // The bindings layer implicitly converts thrown exceptions in
+    // promise-returning functions to promise rejections.
+    exception_state.ThrowDOMException(DOMExceptionCode::kNotSupportedError,
+                                      "Current window is detached");
+    return ScriptPromise();
+  }
+  if (options->signal() && options->signal()->aborted()) {
+    exception_state.ThrowDOMException(DOMExceptionCode::kAbortError,
+                                      "The task was aborted");
+    return ScriptPromise();
+  }
 
   // Always honor the priority and the task signal if given.
   DOMTaskSignal* task_signal = nullptr;
