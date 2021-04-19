54e85ae8bf14c13faa475ac874feda21df495b01
Aaron Leventhal <aleventhal@google.com>
2021-04-19 18:42:52 +0000

    Simplify AXObject role name code
    AXObject only needs a list of ARIA roles. For all other roles, we
    can get the string from mojo via |str << role|.
    
    While doing this, I found that we were supporting an ARIA role of
    "text" that does not exist (may have been proposed at one point).
    
    Added some comments explaining what kReverseRoles is for. Perhaps
    it should be called kSynonymousRoles? There were a couple of
    unnecessary entries in there as well, since they were already
    in kAriaRoles.
    
    Bug: None
    Change-Id: Ic90d9c885e67dab051c96929de9986b5a4fca012
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830373
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873890}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -3140,8 +3140,8 @@ const AtomicString& AXObjectCacheImpl::ComputedRoleForNode(Node* node) {
 
   AXObject* obj = GetOrCreate(node);
   if (!obj)
-    return AXObject::RoleName(ax::mojom::Role::kUnknown);
-  return AXObject::RoleName(obj->RoleValue());
+    return AXObject::ARIARoleName(ax::mojom::blink::Role::kUnknown);
+  return AXObject::ARIARoleName(obj->RoleValue());
 }
 
 String AXObjectCacheImpl::ComputedNameForNode(Node* node) {
