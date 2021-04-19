87ea2387e9bc51f8a71a2133b3219d0b2df4e44d
Allen Bauer <kylixrd@chromium.org>
2021-04-19 16:48:09 +0000

    Use the default fill layout rather than the layout manager directly.
    Bug: 1159116
    Change-Id: I19462425325f22ec43c88e9441766817ffe286a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2832216
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873839}

--- chrome/browser/ui/views/location_bar/permission_chip.cc
+++ chrome/browser/ui/views/location_bar/permission_chip.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/button/button_controller.h"
-#include "ui/views/layout/fill_layout.h"
 #include "ui/views/metadata/metadata_impl_macros.h"
 #include "ui/views/widget/widget.h"
 
@@ -67,7 +66,7 @@ class BubbleButtonController : public views::ButtonController {
 };
 
 PermissionChip::PermissionChip(Browser* browser) : browser_(browser) {
-  SetLayoutManager(std::make_unique<views::FillLayout>());
+  SetUseDefaultFillLayout(true);
   SetVisible(false);
 
   chip_button_ =
