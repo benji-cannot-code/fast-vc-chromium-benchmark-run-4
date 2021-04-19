3f14573e3382bbdc0f347dfc7a2f3be85443b51b
Brian Sheedy <bsheedy@chromium.org>
2021-04-19 22:50:17 +0000

    Restrict GPU pass results to main branch
    Restricts the data queried by the GPU unexpected pass finder to only
    the chromium:[ci|try] realm. This prevents any data from release
    branches being pulled in, which is not useful for determining stale
    expectations on the main branch/ToT.
    
    Bug: 1175850
    Change-Id: Ibcb1529bf8cd2e5f3e92c66c8c7abc46584f0294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2837349
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874009}

--- content/test/gpu/unexpected_passes/queries.py
+++ content/test/gpu/unexpected_passes/queries.py
@@ -61,6 +61,7 @@ WITH
       FROM `luci-resultdb.chromium.gpu_{builder_type}_test_results` tr
       WHERE
         status != "SKIP"
+        AND exported.realm = "chromium:{builder_type}"
         AND STRUCT("builder", @builder_name) IN UNNEST(variant)
         {test_filter_clause}
       GROUP BY exported.id
@@ -106,6 +107,7 @@ WITH
       FROM `luci-resultdb.chromium.gpu_{builder_type}_test_results` tr
       WHERE
         status != "SKIP"
+        AND exported.realm = "chromium:{builder_type}"
         AND STRUCT("builder", @builder_name) IN UNNEST(variant)
         AND REGEXP_CONTAINS(
           test_id,
