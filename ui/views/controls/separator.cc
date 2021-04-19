eab2436eb282acdabc4b52992abdcd08f267f834
James Cook <jamescook@chromium.org>
2021-04-19 17:54:12 +0000

    views: Remove unnecessary includes from separator.cc
    In particular ax_enums.mojom.h, which transitively includes a lot of
    mojo stuff.
    
    Bug: 922875
    Change-Id: I13428107d77360f13380b65958bbd326ce914bf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2832082
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873869}

--- ui/views/controls/separator.cc
+++ ui/views/controls/separator.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "ui/accessibility/ax_enums.mojom.h"
-#include "ui/accessibility/ax_node_data.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/scoped_canvas.h"
 #include "ui/native_theme/native_theme.h"
