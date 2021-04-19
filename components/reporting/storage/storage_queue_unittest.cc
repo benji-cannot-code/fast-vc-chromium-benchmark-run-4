66c74476e52889750373afdd2984ba917b4fc376
Kevin McNee <mcnee@chromium.org>
2021-04-19 19:57:15 +0000

    Disable several StorageQueueTests on windows for flakiness
    Bug: 1195296, 1194943, 1194626
    Change-Id: I6d756f83d47f7c0bcc456b562aeaf26ab36cb12b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2836568
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Owners-Override: Kevin McNee <mcnee@google.com>
    Owners-Override: Mike Wasserman <msw@google.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873922}

--- components/reporting/storage/storage_queue_unittest.cc
+++ components/reporting/storage/storage_queue_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
+#include "build/build_config.h"
 #include "components/reporting/encryption/test_encryption_module.h"
 #include "components/reporting/proto/record.pb.h"
 #include "components/reporting/storage/resources/resource_interface.h"
@@ -376,7 +377,15 @@ constexpr std::array<const char*, 3> kData = {"Rec1111", "Rec222", "Rec33"};
 constexpr std::array<const char*, 3> kMoreData = {"More1111", "More222",
                                                   "More33"};
 
-TEST_P(StorageQueueTest, WriteIntoNewStorageQueueAndReopen) {
+// TODO(1195296): Flaky on windows.
+#if defined(OS_WIN)
+#define MAYBE_WriteIntoNewStorageQueueAndReopen \
+  DISABLED_WriteIntoNewStorageQueueAndReopen
+#else
+#define MAYBE_WriteIntoNewStorageQueueAndReopen \
+  WriteIntoNewStorageQueueAndReopen
+#endif
+TEST_P(StorageQueueTest, MAYBE_WriteIntoNewStorageQueueAndReopen) {
   EXPECT_CALL(set_mock_uploader_expectations_, Call(NotNull())).Times(0);
   CreateTestStorageQueueOrDie(BuildStorageQueueOptionsPeriodic());
   WriteStringOrDie(kData[0]);
@@ -388,7 +397,15 @@ TEST_P(StorageQueueTest, WriteIntoNewStorageQueueAndReopen) {
   CreateTestStorageQueueOrDie(BuildStorageQueueOptionsPeriodic());
 }
 
-TEST_P(StorageQueueTest, WriteIntoNewStorageQueueReopenAndWriteMore) {
+// TODO(1194943): Flaky on windows.
+#if defined(OS_WIN)
+#define MAYBE_WriteIntoNewStorageQueueReopenAndWriteMore \
+  DISABLED_WriteIntoNewStorageQueueReopenAndWriteMore
+#else
+#define MAYBE_WriteIntoNewStorageQueueReopenAndWriteMore \
+  WriteIntoNewStorageQueueReopenAndWriteMore
+#endif
+TEST_P(StorageQueueTest, MAYBE_WriteIntoNewStorageQueueReopenAndWriteMore) {
   EXPECT_CALL(set_mock_uploader_expectations_, Call(NotNull())).Times(0);
   CreateTestStorageQueueOrDie(BuildStorageQueueOptionsPeriodic());
   WriteStringOrDie(kData[0]);
@@ -522,8 +539,16 @@ TEST_P(StorageQueueTest,
   task_environment_.FastForwardBy(base::TimeDelta::FromSeconds(1));
 }
 
+// TODO(1194626): Flaky on windows.
+#if defined(OS_WIN)
+#define MAYBE_WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload \
+  DISABLED_WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload
+#else
+#define MAYBE_WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload \
+  WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload
+#endif
 TEST_P(StorageQueueTest,
-       WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload) {
+       MAYBE_WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload) {
   CreateTestStorageQueueOrDie(BuildStorageQueueOptionsPeriodic());
   WriteStringOrDie(kData[0]);
   WriteStringOrDie(kData[1]);
