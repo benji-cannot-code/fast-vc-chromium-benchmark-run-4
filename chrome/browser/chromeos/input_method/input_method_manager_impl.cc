a51b631a1ba28d17c8d38a8ffb2bfcfeec5a7fc9
Lei Zhang <thestig@chromium.org>
2021-04-19 21:32:06 +0000

    Remove includes for some redundant C/C++ system headers.
    No need for stdint.h and cstdint. Same for stddef.h and cstddef. In one
    case, remove both stdint.h. and cstdint, as they are not needed.
    
    Change-Id: I8a40237372a45c5ca548359931c56841e6ae92e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830299
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873981}

--- chrome/browser/chromeos/input_method/input_method_manager_impl.cc
+++ chrome/browser/chromeos/input_method/input_method_manager_impl.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <algorithm>  // std::find
-#include <cstdint>
 #include <memory>
 #include <set>
 #include <sstream>
