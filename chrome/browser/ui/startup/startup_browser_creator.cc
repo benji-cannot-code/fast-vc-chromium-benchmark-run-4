b8dc45c3f7ddf45ae84838562006718dbbb4051f
Daniel Cheng <dcheng@chromium.org>
2021-04-19 17:21:50 +0000

    Use base::StringTokenizer::token_piece() when std::string is not needed.
    base::StringTokenizer::token() creates a new std::string each time. Many
    callers do not even need a std::string, so just update them to use
    token_piece() instead.
    
    This CL was uploaded by git cl split.
    
    R=tmartino@chromium.org
    
    Bug: 1199547
    Change-Id: Ib258b6cbfa08d5465af6fd182a21bc59ffe37d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2832865
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873855}

--- chrome/browser/ui/startup/startup_browser_creator.cc
+++ chrome/browser/ui/startup/startup_browser_creator.cc
@@ -1243,7 +1243,7 @@ bool StartupBrowserCreator::ProcessLoadApps(
     return false;
 
   base::FilePath app_absolute_dir =
-      base::MakeAbsoluteFilePath(base::FilePath(tokenizer.token()));
+      base::MakeAbsoluteFilePath(base::FilePath(tokenizer.token_piece()));
   if (!apps::AppLoadService::Get(profile)->LoadAndLaunch(
           app_absolute_dir, command_line, cur_dir)) {
     return false;
@@ -1251,7 +1251,7 @@ bool StartupBrowserCreator::ProcessLoadApps(
 
   while (tokenizer.GetNext()) {
     app_absolute_dir =
-        base::MakeAbsoluteFilePath(base::FilePath(tokenizer.token()));
+        base::MakeAbsoluteFilePath(base::FilePath(tokenizer.token_piece()));
 
     if (!apps::AppLoadService::Get(profile)->Load(app_absolute_dir)) {
       return false;
