94b2395ac3718ae15a5b54aa52523503afe46123
Xida Chen <xidachen@chromium.org>
2021-04-19 14:08:56 +0000

    [composite-bgcolor-animation] Completely decouple paint & compositing
    blink::Animation class has a | supplemental_failure_reasons_| bit,
    it is set during the paint stage and used to determine whether a
    background-color animation is compositable or not during the compositing
    stage. This bit needs to be carefully maintained, otherwise it
    could causes problems in the future, such as using the stale value,
    set it in wrong places, etc.
    
    This CL address the problem. Basically that bit is set during
    the paint stage by doing some pre-checks in BackgroundColorPaintWorklet.
    What we can do is to do all those pre-checks again during the
    CheckCanStartAnimationOnCompositor. It is a bit inefficient, but
    will prevent bugs in the future.
    
    Note that this is a code health CL, no behavior change is
    expected.
    
    There are some extra notes about this CL captured here:
    https://docs.google.com/document/d/1TQBd_nqPovjKrKl7Ldre1Eu8SZpquhoEIG0_Yp-LDXQ/edit
    
    Bug: 1193757
    Change-Id: I00c504593ba04c4d1cb32543b3616089326f52d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2820669
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#873792}

--- third_party/blink/renderer/core/css/background_color_paint_image_generator.h
+++ third_party/blink/renderer/core/css/background_color_paint_image_generator.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class Animation;
+class Element;
 class Image;
 class LocalFrame;
 class Node;
@@ -45,6 +47,8 @@ class CORE_EXPORT BackgroundColorPaintImageGenerator
       Vector<Color>* animated_colors,
       Vector<double>* offsets,
       base::Optional<double>* progress) = 0;
+
+  virtual Animation* GetAnimationIfCompositable(const Element* element) = 0;
 };
 
 }  // namespace blink
