39345e33257fefe895dcbce416d4529abf086a53
Peter Boström <pbos@chromium.org>
2021-04-19 23:07:28 +0000

    Don't toggle read-later bookmarks-bar w/ SidePanel
    This makes MaybeShowBookmarkBarForReadLater a no-op when kSidePanel is
    enabled. It also skips logging
    ReadingList.BookmarkBarState.OnFirstAddToReadingList which was used to
    monitor how often this toggle would trigger for users.
    
    Bug: 1199126
    Change-Id: I3bcb8711ac46bce468dd47b7fac8571b88353d32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2832119
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874019}

--- chrome/browser/ui/browser_commands.cc
+++ chrome/browser/ui/browser_commands.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
@@ -68,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_group.h"
 #include "chrome/browser/ui/tabs/tab_group_model.h"
 #include "chrome/browser/ui/translate/translate_bubble_view_state_transition.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/user_education/feature_promo_controller.h"
 #include "chrome/browser/ui/user_education/reopen_tab_in_product_help.h"
 #include "chrome/browser/ui/user_education/reopen_tab_in_product_help_factory.h"
@@ -1149,6 +1151,8 @@ bool IsCurrentTabUnreadInReadLater(Browser* browser) {
 
 void MaybeShowBookmarkBarForReadLater(Browser* browser) {
 #if !defined(OS_ANDROID)
+  if (base::FeatureList::IsEnabled(features::kSidePanel))
+    return;
   PrefService* pref_service = browser->profile()->GetPrefs();
   if (pref_service &&
       !pref_service->GetBoolean(
@@ -1158,6 +1162,7 @@ void MaybeShowBookmarkBarForReadLater(Browser* browser) {
     base::UmaHistogramEnumeration(
         "ReadingList.BookmarkBarState.OnFirstAddToReadingList",
         browser->bookmark_bar_state());
+
     if (browser->bookmark_bar_state() == BookmarkBar::HIDDEN)
       ToggleBookmarkBar(browser);
   }
