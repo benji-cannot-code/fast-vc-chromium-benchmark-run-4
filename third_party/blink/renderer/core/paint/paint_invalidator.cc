dbdaafca47f607ea78a162a0c2357e1b51d14fda
Michael Lippautz <mlippautz@chromium.org>
2021-04-20 21:52:52 +0000

    PaintTreeWalk: Avoid temporary allocations
    PaintTreeWalk is a stack-scoped object that accumulates a significant
    amount of temporary objects:
    - Avoid dynamic allocation of PaintTreeWalk itself;
    - Eagerly freeing the backing of context_storage_ which may accumulate
      several MB floating garbage;
    - Revert PaintPropertyTreeBuilderContext to an inlined object with
      Trace() method to avoid another temporary allocation;
    
    Bug: 1197697
    Change-Id: I6b5abc1e63b1919b5b816791f0eec2f5db75acf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831589
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Yuki Yamada <yukiy@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874446}

--- third_party/blink/renderer/core/paint/paint_invalidator.cc
+++ third_party/blink/renderer/core/paint/paint_invalidator.cc
@@ -32,21 +32,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void PaintInvalidatorContext::ParentContextAccessor::Trace(
-    Visitor* visitor) const {
-  visitor->Trace(tree_walk_);
-}
-
 void PaintInvalidatorContext::Trace(Visitor* visitor) const {
   visitor->Trace(parent_context_accessor_);
   visitor->Trace(directly_composited_container);
   visitor->Trace(directly_composited_container_for_stacked_contents);
   visitor->Trace(painting_layer);
   visitor->Trace(fragment_data);
-}
-
-void PaintInvalidator::Trace(Visitor* visitor) const {
-  visitor->Trace(pending_delayed_paint_invalidations_);
 }
 
 const PaintInvalidatorContext*
