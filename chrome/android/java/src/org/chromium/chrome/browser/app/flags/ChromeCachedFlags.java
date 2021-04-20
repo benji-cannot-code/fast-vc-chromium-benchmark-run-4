d6c13a65a39fb9237771d2e2512145f908065a5e
Ayman Almadhoun <ayman@chromium.org>
2021-04-20 01:36:36 +0000

    Add a dedicated feature for Merchant Viewer
    This CL adds a new feature for merchant viewer and creates MerchantViewerConfig will host the various config values for the experience.
    
    Change-Id: I924b18675bbf9d3580d1dfb99e31c7a04c8881f8
    Bug: 1199990
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831084
    Commit-Queue: Ayman Almadhoun <ayman@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874086}

--- chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
@@ -15,6 +15,7 @@ import org.chromium.chrome.browser.flags.CachedFeatureFlags;
 import org.chromium.chrome.browser.flags.CachedFieldTrialParameter;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.lens.LensFeature;
+import org.chromium.chrome.browser.merchant_viewer.MerchantViewerConfig;
 import org.chromium.chrome.browser.page_annotations.PageAnnotationsServiceConfig;
 import org.chromium.chrome.browser.paint_preview.StartupPaintPreviewHelper;
 import org.chromium.chrome.browser.subscriptions.CommerceSubscriptionsServiceConfig;
@@ -114,6 +115,7 @@ public class ChromeCachedFlags {
                 LensFeature.ENABLE_LENS_CAMERA_ASSISTED_SEARCH_ON_TABLET,
                 LensFeature.SEARCH_BOX_START_VARIANT_LENS_CAMERA_ASSISTED_SEARCH,
                 LensFeature.MIN_AGSA_VERSION_LENS_CAMERA_ASSISTED_SEARCH,
+                MerchantViewerConfig.DEFAULT_TRUST_SIGNALS_MESSAGE_DELAY,
                 PageAnnotationsServiceConfig.PAGE_ANNOTATIONS_BASE_URL,
                 ReturnToChromeExperimentsUtil.TAB_SWITCHER_ON_RETURN_MS,
                 ShoppingPersistedTabData.TIME_TO_LIVE_MS,
