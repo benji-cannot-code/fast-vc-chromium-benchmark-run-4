ab192cc533a959ee0211e6f4956e4c645046f4c2
Dan Elphick <delphick@chromium.org>
2021-04-20 10:23:47 +0000

    tracing: Increase size of SMB for PosixSystemProducer
    Increase the size of the SMB when tracing with Android to match the
    Chrome default (4MiB instead of 256KiB).
    
    Change-Id: I985d348402a3a9ae39cad04ff3dfc4b3ce11d806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2835847
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874203}

--- services/tracing/public/cpp/perfetto/posix_system_producer.cc
+++ services/tracing/public/cpp/perfetto/posix_system_producer.cc
@@ -418,7 +418,8 @@ void PosixSystemProducer::ConnectSocket() {
   if (!SandboxForbidsSocketConnection()) {
     auto service = perfetto::ProducerIPCClient::Connect(
         socket_name_.c_str(), this, std::move(producer_name), task_runner(),
-        perfetto::TracingService::ProducerSMBScrapingMode::kEnabled);
+        perfetto::TracingService::ProducerSMBScrapingMode::kEnabled,
+        kSMBSizeBytes, kSMBPageSizeBytes);
 
     base::AutoLock lock(lock_);
     services_.push_back(std::move(service));
@@ -449,7 +450,8 @@ void PosixSystemProducer::ConnectSocket() {
             perfetto::ipc::Client::ConnArgs(
                 perfetto::base::ScopedFile(file.TakePlatformFile())),
             self.get(), std::move(producer_name), self->task_runner(),
-            perfetto::TracingService::ProducerSMBScrapingMode::kEnabled);
+            perfetto::TracingService::ProducerSMBScrapingMode::kEnabled,
+            kSMBSizeBytes, kSMBPageSizeBytes);
 
         base::AutoLock lock(self->lock_);
         self->services_.push_back(std::move(service));
