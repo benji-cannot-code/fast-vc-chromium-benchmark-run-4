a6275b3396e6e938e498d7c1b02352394318d172
David Tseng <dtseng@chromium.org>
2021-04-20 14:14:22 +0000

    Revert "[sheriff] Disabling SpokenFeedbackAppListTest.AppListReordering on CrOS"
    This reverts commit 1db20db0a152e1b2f38eb7ea0373a142a871f686.
    
    Reason for revert: <INSERT REASONING HERE>
    Root cause was found and reverted
    https://chromium.googlesource.com/chromium/src/+/14dc3fc233553aa66cc00db0ffa54e72b2b701b1
    
    Original change's description:
    > [sheriff] Disabling SpokenFeedbackAppListTest.AppListReordering on CrOS
    >
    > times out
    >
    > Bug: 1200264
    > Change-Id: I0a64e7aac7482605af0459137ab75ed6ec3870e9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2835866
    > Reviewed-by: Katie Dektar <katie@chromium.org>
    > Commit-Queue: Olga Sharonova <olka@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#874179}
    
    Bug: 1200264
    Change-Id: Ife88c9748a42e8e05a1f8ccab06690c5e3e9e0ab
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2840049
    Auto-Submit: David Tseng <dtseng@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#874244}

--- chrome/browser/ash/accessibility/spoken_feedback_app_list_browsertest.cc
+++ chrome/browser/ash/accessibility/spoken_feedback_app_list_browsertest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
-#include "build/build_config.h"
 #include "chrome/browser/ash/accessibility/spoken_feedback_browsertest.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
@@ -563,13 +562,7 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackAppListTest, AppListFoldering) {
 // well for braille users. The preferred way to handle this is to actually
 // change focus as the user navigates, and to have each object's
 // accessible name describe its position. (See crbug.com/1098495)
-// Times out on Linux CrOS, http://crbug.com/1200264.
-#if defined(OS_CHROMEOS)
-#define MAYBE_AppListReordering DiSABLED_AppListReordering
-#else
-#define MAYBE_AppListReordering AppListReordering
-#endif
-IN_PROC_BROWSER_TEST_P(SpokenFeedbackAppListTest, MAYBE_AppListReordering) {
+IN_PROC_BROWSER_TEST_P(SpokenFeedbackAppListTest, AppListReordering) {
   // Add 7 apps.
   PopulateApps(22);
 
