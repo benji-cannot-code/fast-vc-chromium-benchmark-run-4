d216f0a546f52f8cf749b4986da7240a2eefb053
Matt Menke <mmenke@chromium.org>
2021-04-20 16:44:40 +0000

    FLEDGE: Block access to Date() API.
    Block access to the built-in Date() ECMAScript APIs in worklets, to
    mitigate Spectre-style sidechannel attacks.
    
    PrevWin times are now given in seconds that have passed since the
    win, instead of as dates.
    
    Also includes minor cleanups in bidder_worklet_unittests.cc
    
    Bug: 1186444
    Change-Id: I5b7605d96b4765b2f833b01faea3e25bbbfd73bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827455
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874295}

--- content/services/auction_worklet/auction_runner.h
+++ content/services/auction_worklet/auction_runner.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/logging.h"
 #include "base/optional.h"
+#include "base/time/time.h"
 #include "content/services/auction_worklet/auction_v8_helper.h"
 #include "content/services/auction_worklet/bidder_worklet.h"
 #include "content/services/auction_worklet/public/mojom/auction_worklet_service.mojom.h"
@@ -133,6 +134,9 @@ class AuctionRunner {
   // State for the bidding phase.
   int outstanding_bids_;  // number of bids for which we're waiting on a fetch.
   std::vector<BidState> bid_states_;  // parallel to `bidders_`.
+  // The time the auction started. Use a single base time for all Worklets, to
+  // present a more consistent view of the universe.
+  const base::Time auction_start_time_ = base::Time::Now();
 
   // State for the scoring phase.
   std::unique_ptr<SellerWorklet> seller_worklet_;
