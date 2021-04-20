29207fc3ed4c86cb147e547320b86352d657e86f
Patrick Monette <pmonette@chromium.org>
2021-04-20 18:16:30 +0000

    Simplify the usage of timers in /storage/browser
    R=mek@chromium.org
    
    Change-Id: I18eaddef9b4b2682595d9a36958bcf7556940645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2829392
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874339}

--- storage/browser/file_system/file_system_usage_cache.h
+++ storage/browser/file_system/file_system_usage_cache.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
-#include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/timer/timer.h"
 
@@ -101,8 +100,6 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) FileSystemUsageCache {
   std::map<base::FilePath, std::vector<uint8_t>> incognito_usages_;
 
   std::map<base::FilePath, std::unique_ptr<base::File>> cache_files_;
-
-  base::WeakPtrFactory<FileSystemUsageCache> weak_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(FileSystemUsageCache);
 };
