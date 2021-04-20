3e3b1a816ae94578812d5dc927fe2ce4b442b614
Mark Yacoub <markyacoub@google.com>
2021-04-20 20:44:13 +0000

    drm/ozone: Change modeset interface to pass planes list
    Change the modeset interfaces to take an overlay planes list instead of
    a single primary plane to modeset with.
    This allows us to extend it later to modeset with multiple planes
    (Primary + Overlay).
    
    BUG=b:185899132
    TEST=HardwareDisplayControllerTest.*
    
    Change-Id: Ia5e37b7f3ac8ee02c8d06687a78561a265e29e8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2840166
    Commit-Queue: Mark Yacoub <markyacoub@google.com>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874418}

--- ui/ozone/platform/drm/gpu/screen_manager.h
+++ ui/ozone/platform/drm/gpu/screen_manager.h
@@ -132,7 +132,7 @@ class ScreenManager {
       uint32_t connector,
       const gfx::Point& origin,
       const drmModeModeInfo& mode,
-      const DrmOverlayPlane& primary);
+      const DrmOverlayPlaneList& modeset_planes);
 
   // Configures a display controller to be disabled. The display controller is
   // identified by |crtc|. Controller modeset props are added into
@@ -164,20 +164,20 @@ class ScreenManager {
       const scoped_refptr<DrmDevice>& drm,
       const gfx::Rect& bounds);
 
-  DrmOverlayPlane GetModesetBuffer(HardwareDisplayController* controller,
-                                   const gfx::Rect& bounds,
-                                   const std::vector<uint64_t>& modifiers,
-                                   bool is_testing);
+  DrmOverlayPlaneList GetModesetPlanes(HardwareDisplayController* controller,
+                                       const gfx::Rect& bounds,
+                                       const std::vector<uint64_t>& modifiers,
+                                       bool is_testing);
 
   // Gets props for modesetting the |controller| using |origin| and |mode|.
   void GetModesetControllerProps(CommitRequest* commit_request,
                                  HardwareDisplayController* controller,
                                  const gfx::Point& origin,
                                  const drmModeModeInfo& mode,
-                                 const DrmOverlayPlane& primary);
+                                 const DrmOverlayPlaneList& modeset_planes);
   void GetEnableControllerProps(CommitRequest* commit_request,
                                 HardwareDisplayController* controller,
-                                const DrmOverlayPlane& primary);
+                                const DrmOverlayPlaneList& modeset_planes);
 
   DrmWindow* FindWindowAt(const gfx::Rect& bounds) const;
 
