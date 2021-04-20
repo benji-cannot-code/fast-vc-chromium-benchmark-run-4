eddcd6ae93269d566b329579644538779ce4b2a4
Adam Langley <agl@chromium.org>
2021-04-20 21:17:00 +0000

    webauthn: don't advertise phone as a security key if no screen lock.
    GMSCore fails all FIDO requests if a screen lock isn't configured on the
    device. Therefore a phone shouldn't advertise itself in Sync in that
    case.
    
    Since I'm not sure about the cost of checking this (i.e. it's probably
    an RPC call), don't do it during startup. Instead wait until Sync is
    preparing to publish device-info before checking this. Also, don't
    register an FCM token until we have checked all the requirements in
    order to save on traffic to the FCM service.
    
    Lastly, because someone could remove a screen lock after Sync has
    published, catch this case and show an error. For now there's not a
    specific string but there probably will be in the future.
    
    BUG=1002262
    
    Change-Id: I001bcbc1588d2ef929946d63915c9e47306fedc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2838227
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874434}

--- chrome/browser/webauthn/android/java/src/org/chromium/chrome/browser/webauthn/CableAuthenticatorModuleProvider.java
+++ chrome/browser/webauthn/android/java/src/org/chromium/chrome/browser/webauthn/CableAuthenticatorModuleProvider.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.webauthn;
 
+import android.annotation.TargetApi;
+import android.app.KeyguardManager;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.os.Build;
@@ -18,6 +20,7 @@ import android.widget.LinearLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.NativeMethods;
@@ -123,8 +126,24 @@ public class CableAuthenticatorModuleProvider extends Fragment {
 
     @CalledByNative
     public static boolean canDeviceSupportCable() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
-                && BluetoothAdapter.getDefaultAdapter() != null;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N
+                || BluetoothAdapter.getDefaultAdapter() == null) {
+            return false;
+        }
+
+        // GMSCore will immediately fail all requests if a screenlock
+        // isn't configured.
+        return hasScreenLockConfigured();
+    }
+
+    // canDeviceSupportCable has checked that the system is >= N (API level 24)
+    // before calling this function.
+    @TargetApi(24)
+    private static boolean hasScreenLockConfigured() {
+        KeyguardManager km =
+                (KeyguardManager) ContextUtils.getApplicationContext().getSystemService(
+                        Context.KEYGUARD_SERVICE);
+        return km.isDeviceSecure();
     }
 
     @NativeMethods
