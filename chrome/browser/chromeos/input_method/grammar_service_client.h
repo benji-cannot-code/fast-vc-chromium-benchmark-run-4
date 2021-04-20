5ce4676b2bcff870212989ece63e6324676619f7
Jing Wang <jiwan@chromium.org>
2021-04-20 00:37:46 +0000

    Trim grammar query text when it is too long.
    The sentence end logic is from http://google3/research/language_modeling/sentence_explorer/text_utils.cc?l=1137&rcl=364069326,
    Because we are running on English only, I simplified it.
    
    Also fix the offsets to UTF16 offsets.
    
    Also added tests.
    
    TEST: passed unit test.
    Bug: 1132699
    Change-Id: I9f14bbf6cee7fddc1416a2a9fc4294f66deac802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2777512
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874055}

--- chrome/browser/chromeos/input_method/grammar_service_client.h
+++ chrome/browser/chromeos/input_method/grammar_service_client.h
@@ -63,7 +63,8 @@ class GrammarServiceClient {
  private:
   // Parse the result returned from grammar check service.
   void ParseGrammarCheckerResult(
-      const std::u16string& text,
+      const std::string& query_text,
+      const uint32_t query_offset,
       TextCheckCompleteCallback callback,
       chromeos::machine_learning::mojom::GrammarCheckerResultPtr result) const;
 
