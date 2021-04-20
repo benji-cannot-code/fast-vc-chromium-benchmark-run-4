0b468669125078bfec37900447ecc704613f010a
Peter Boström <pbos@chromium.org>
2021-04-20 16:42:23 +0000

    Add View::GetLayoutProvider() stub
    This analogy to GetThemeProvider() adds a stub for adding non-global
    LayoutProviders.
    
    Bug: 1200584
    Change-Id: I82ad94ae414bb9367bdead593d2028a852d8af23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2837522
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874292}

--- ui/views/view.h
+++ ui/views/view.h
@@ -85,6 +85,7 @@ class ContextMenuController;
 class DragController;
 class FocusManager;
 class FocusTraversable;
+class LayoutProvider;
 class ScrollView;
 class ViewAccessibility;
 class ViewMaskLayer;
@@ -897,6 +898,9 @@ class VIEWS_EXPORT View : public ui::LayerDelegate,
 
   // Get the theme provider from the parent widget.
   const ui::ThemeProvider* GetThemeProvider() const;
+
+  // Get the layout provider for the View.
+  const LayoutProvider* GetLayoutProvider() const;
 
   // Returns the ColorProvider from the ColorProviderManager.
   ui::ColorProvider* GetColorProvider() {
