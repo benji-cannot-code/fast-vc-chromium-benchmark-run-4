f2548628ec0a91f0372cb3a5d4054b1628871064
Minoru Chikamune <chikamune@chromium.org>
2021-04-20 16:48:50 +0000

    Revert "[MBI] Migrate CanvasResourceDispatcher to use per-ASG CompositorTaskRunner"
    This reverts commit 7db085bb33261e6f4b1057f907f4a6b8943cb9f2.
    
    Reason for revert: Can't draw to offscreen canvas from a
    SharedWorker (Error Code 11 Aw Snap)
    
    Original change's description:
    > [MBI] Migrate CanvasResourceDispatcher to use per-ASG CompositorTaskRunner
    >
    > Before this CL, CanvasResourceDispatcher uses per-thread
    > CompositorTaskRunner.
    >
    > After this CL, CanvasResourceDispatcher will use per-ASG (Agent
    > Scheduling Group) CompositorTaskRunner.
    >
    > CanvasResourceDispatcher is used by OffscreenCanvas. OffscreenCanvas
    > provides a functionality to draw the Canvas from Worker
    > thread. Previously, CanvasResourceDispatcher was accessing the main
    > thread’s objects from the worker thread by using
    > `Thread::MainThread()->Scheduler()->CompositorTaskRunner()`. But this
    > time, we want to use the per-AgentSchedulingGroup compositor task
    > runner instead. To plumb per-ASG compositor task runner from main
    > thread to worker thread, this CL added
    > agent_group_scheduler_compositor_task_runner to WorkerGlobalScope.
    >
    > Bug: 1105403
    > Change-Id: If4a352cd374c5555f94497c09977ee11105ded2a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2632110
    > Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#848996}
    
    Bug: 1200058, 1105403
    Change-Id: I595ccf6b660f295210684f07097296da009d1346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2838729
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874297}

--- third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher_test.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher_test.cc
@@ -50,7 +50,6 @@ class MockCanvasResourceDispatcher : public CanvasResourceDispatcher {
  public:
   MockCanvasResourceDispatcher()
       : CanvasResourceDispatcher(nullptr /* client */,
-                                 base::ThreadTaskRunnerHandle::Get(),
                                  kClientId,
                                  kSinkId,
                                  0 /* placeholder_canvas_id* */,
