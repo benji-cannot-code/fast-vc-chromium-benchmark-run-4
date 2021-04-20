b630c9d9fcab818cc9d4ad5b42df9630014fdbe3
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-04-20 11:35:43 +0000

    [CrOS cellular] Expand and collapse qrcode detection video
    This CL adds an expand and collapse transition for qrcode detention
    video container.
    
    GIF: https://imgur.com/Fz1cCgy
    
    Bug: 1195952
    Change-Id: I98fc25bc5887238251f92ad54454aabdb45d964e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2832495
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/master@{#874214}

--- ui/webui/resources/cr_components/chromeos/cellular_setup/activation_code_page.js
+++ ui/webui/resources/cr_components/chromeos/cellular_setup/activation_code_page.js
@@ -112,6 +112,15 @@ Polymer({
       type: Object,
       value: null,
     },
+
+    /**
+     * If true, video is expanded.
+     */
+    expanded_: {
+      type: Boolean,
+      value: false,
+      reflectToAttribute: true,
+    }
   },
 
   /**
@@ -429,6 +438,14 @@ Polymer({
         this.stopStream_(this.stream_);
       });
     }
+
+    if (this.state_ === PageState.SCANNING_USER_FACING ||
+        this.state_ === PageState.SCANNING_ENVIRONMENT_FACING) {
+      this.expanded_ = true;
+      return;
+    }
+
+    this.expanded_ = false;
   },
 
   /**
@@ -516,5 +533,5 @@ Polymer({
    */
   shouldActivationCodeInputBeInvalid_(state) {
     return state === PageState.MANUAL_ENTRY_INSTALL_FAILURE;
-  }
+  },
 });
