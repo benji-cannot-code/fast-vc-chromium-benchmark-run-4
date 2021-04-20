4fded23d69290eae2a545cd57ac9bc4d5d62a483
Fergal Daly <fergal@chromium.org>
2021-04-20 09:01:39 +0000

    Revert "ozone/wayland: watch fd on a dedicated thread."
    This reverts commit 8e81fabe307827e222edc1f559922fdd6ba8b40b.
    
    Reason for revert: breaks BrowserActionInteractiveTest.TestOpenPopup
    
    
    Original change's description:
    > ozone/wayland: watch fd on a dedicated thread.
    >
    > Whenever user closes a popup window (or any additional window),
    > the browser process can get stuck waiting for the gpu side to
    > close. But the gpu thread can also get stuck because of destroying
    > the wl_egl_surface. That is, internally, Wayland EGL implementation
    > (it's something driver vendors provide) can try to also read events
    > from the wl_display's fd and call wl_display_prepare_read, and
    > then try to read the events. But it won't be possible as
    > wl_display_read_events will block for them, because Ozone/Wayland
    > has already prepared to read, but it doesn't read, because
    > it waits for another thread to complete.
    >
    > Thus, the most correct approach is to use a dedicated non-blocking
    > thread that always watches the wayland fd and dispatches events
    > on an original thread. Also, use a non-default event queue to avoid
    > flushing events that may not belong to us (a toolkit or egl
    > implementation uses a default queue). See also
    > https://wayland.freedesktop.org/docs/html/apb.html#Client-classwl__display_1a40039c1169b153269a3dc0796a54ddb0
    > that explains this.
    >
    > Bug: 1117463
    > Change-Id: I33fa21f84bb26592e97327c3f0a9c926868ad214
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814598
    > Commit-Queue: Maksim Sisov <msisov@igalia.com>
    > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#873303}
    
    Bug: 1117463,1200725
    Change-Id: Iaa289889be8c17e745cf970399e4e5011b190a00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2837972
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Fergal Daly <fergal@google.com>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874194}

--- ui/ozone/platform/wayland/common/wayland_object.h
+++ ui/ozone/platform/wayland/common/wayland_object.h
@@ -27,7 +27,6 @@ struct wl_data_device;
 struct wl_data_offer;
 struct wl_data_source;
 struct wl_drm;
-struct wl_event_queue;
 struct wl_keyboard;
 struct wl_output;
 struct wl_pointer;
@@ -42,7 +41,6 @@ struct wl_surface;
 struct wl_touch;
 struct wp_presentation;
 struct wp_presentation_feedback;
-struct wl_proxy;
 struct wp_viewport;
 struct wp_viewporter;
 struct xdg_wm_base;
@@ -193,12 +191,6 @@ struct ObjectTraits<wl_drm> {
 };
 
 template <>
-struct ObjectTraits<wl_event_queue> {
-  static const wl_interface* interface;
-  static void (*deleter)(wl_event_queue*);
-};
-
-template <>
 struct ObjectTraits<wl_display> {
   static const wl_interface* interface;
   static void (*deleter)(wl_display*);
@@ -286,13 +278,6 @@ template <>
 struct ObjectTraits<wp_presentation_feedback> {
   static const wl_interface* interface;
   static void (*deleter)(wp_presentation_feedback*);
-};
-
-template <>
-struct ObjectTraits<wl_proxy> {
-  // Interface is null for proxy.
-  static const wl_interface* interface;
-  static void (*deleter)(void*);
 };
 
 template <>
@@ -484,7 +469,6 @@ class Object : public std::unique_ptr<T, Deleter> {
 
 template <typename T>
 wl::Object<T> Bind(wl_registry* registry, uint32_t name, uint32_t version) {
-  DCHECK(ObjectTraits<T>::interface);
   return wl::Object<T>(wl::bind_registry<T>(
       registry, name, ObjectTraits<T>::interface, version));
 }
