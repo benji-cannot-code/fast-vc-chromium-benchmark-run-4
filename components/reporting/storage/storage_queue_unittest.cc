654577efa3c8e7516d290d8d1e2c39968ad96381
Leonid Baraz <lbaraz@chromium.org>
2021-04-20 21:42:18 +0000

    Stabilize storage tests after on-thread destructor
    After crrev.com/c/2787772 storage queue destructor is running
    asynchronously on a thread, and so the caller must wait for all
    threads to idle in order to be sure everything is released.
    
    Re-enabling previously flaky tests too.
    
    Verification: running the test as
    tools/run-swarmed.py out/win components_unittests             \
           --gtest_filter="*/Storage*Test.*" --gtest_repeat=100
    
    No failures detected:
     https://chromium-swarm.appspot.com/task?id=5309b80d78577110
    
    as opposed to the baseline failing:
     https://chromium-swarm.appspot.com/task?id=5309d17dccfef810
    
    Bug: 1195296, 1194943, 1194626
    Change-Id: I3af8d52ff21331b8f2e153401fb4d12f0ec62068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2841643
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/master@{#874443}

--- components/reporting/storage/storage_queue_unittest.cc
+++ components/reporting/storage/storage_queue_unittest.cc
@@ -270,7 +270,6 @@ class StorageQueueTest : public ::testing::TestWithParam<size_t> {
 
   void TearDown() override {
     ResetTestStorageQueue();
-    task_environment_.RunUntilIdle();
     // Make sure all memory is deallocated.
     ASSERT_THAT(GetMemoryResource()->GetUsed(), Eq(0u));
     // Make sure all disk is not reserved (files remain, but Storage is not
@@ -301,8 +300,12 @@ class StorageQueueTest : public ::testing::TestWithParam<size_t> {
   }
 
   void ResetTestStorageQueue() {
+    // Let everything ongoing to finish.
     task_environment_.RunUntilIdle();
     storage_queue_.reset();
+    // StorageQueue is destructed on a thread,
+    // so we need to wait for it to destruct.
+    task_environment_.RunUntilIdle();
   }
 
   void InjectFailures(std::initializer_list<int64_t> sequencing_ids) {
@@ -377,15 +380,7 @@ constexpr std::array<const char*, 3> kData = {"Rec1111", "Rec222", "Rec33"};
 constexpr std::array<const char*, 3> kMoreData = {"More1111", "More222",
                                                   "More33"};
 
-// TODO(1195296): Flaky on windows.
-#if defined(OS_WIN)
-#define MAYBE_WriteIntoNewStorageQueueAndReopen \
-  DISABLED_WriteIntoNewStorageQueueAndReopen
-#else
-#define MAYBE_WriteIntoNewStorageQueueAndReopen \
-  WriteIntoNewStorageQueueAndReopen
-#endif
-TEST_P(StorageQueueTest, MAYBE_WriteIntoNewStorageQueueAndReopen) {
+TEST_P(StorageQueueTest, WriteIntoNewStorageQueueAndReopen) {
   EXPECT_CALL(set_mock_uploader_expectations_, Call(NotNull())).Times(0);
   CreateTestStorageQueueOrDie(BuildStorageQueueOptionsPeriodic());
   WriteStringOrDie(kData[0]);
@@ -397,15 +392,7 @@ TEST_P(StorageQueueTest, MAYBE_WriteIntoNewStorageQueueAndReopen) {
   CreateTestStorageQueueOrDie(BuildStorageQueueOptionsPeriodic());
 }
 
-// TODO(1194943): Flaky on windows.
-#if defined(OS_WIN)
-#define MAYBE_WriteIntoNewStorageQueueReopenAndWriteMore \
-  DISABLED_WriteIntoNewStorageQueueReopenAndWriteMore
-#else
-#define MAYBE_WriteIntoNewStorageQueueReopenAndWriteMore \
-  WriteIntoNewStorageQueueReopenAndWriteMore
-#endif
-TEST_P(StorageQueueTest, MAYBE_WriteIntoNewStorageQueueReopenAndWriteMore) {
+TEST_P(StorageQueueTest, WriteIntoNewStorageQueueReopenAndWriteMore) {
   EXPECT_CALL(set_mock_uploader_expectations_, Call(NotNull())).Times(0);
   CreateTestStorageQueueOrDie(BuildStorageQueueOptionsPeriodic());
   WriteStringOrDie(kData[0]);
@@ -539,16 +526,8 @@ TEST_P(StorageQueueTest,
   task_environment_.FastForwardBy(base::TimeDelta::FromSeconds(1));
 }
 
-// TODO(1194626): Flaky on windows.
-#if defined(OS_WIN)
-#define MAYBE_WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload \
-  DISABLED_WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload
-#else
-#define MAYBE_WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload \
-  WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload
-#endif
 TEST_P(StorageQueueTest,
-       MAYBE_WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload) {
+       WriteIntoNewStorageQueueReopenWithMissingDataWriteMoreAndUpload) {
   CreateTestStorageQueueOrDie(BuildStorageQueueOptionsPeriodic());
   WriteStringOrDie(kData[0]);
   WriteStringOrDie(kData[1]);
