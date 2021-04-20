2fffa1f0d896383e54d37eca395055a2f42cb053
David Munro <davidmunro@google.com>
2021-04-20 00:40:06 +0000

    crostini: Terminal launch metrics cover until vsh bind
    Instead of reporting success/failure after launching Crostini, report
    them after binding into the container so we have coverage of the full
    flow.
    
    Bug: 1188992
    Test: Browser tests, deploy and check histograms
    Change-Id: Ifbd1b77dd2c0a57a664c63389366956bcbed051c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831156
    Commit-Queue: David Munro <davidmunro@google.com>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874058}

--- chrome/browser/extensions/api/terminal/terminal_private_api.h
+++ chrome/browser/extensions/api/terminal/terminal_private_api.h
@@ -66,18 +66,22 @@ class TerminalPrivateOpenTerminalProcessFunction : public ExtensionFunction {
       base::CommandLine cmdline,
       crostini::CrostiniResult result);
 
-  void OpenVmshellProcess(const std::string& user_id_hash,
-                          base::CommandLine cmdline);
+  void OpenVmshellProcess(
+      const std::string& user_id_hash,
+      base::CommandLine cmdline,
+      std::unique_ptr<CrostiniStartupStatus> startup_status);
 
   void OnGetVshSession(const std::string& user_id_hash,
                        base::CommandLine cmdline,
                        const std::string& terminal_id,
+                       std::unique_ptr<CrostiniStartupStatus> startup_status,
                        bool success,
                        const std::string& failure_reason,
                        int32_t container_shell_pid);
 
   void OpenProcess(const std::string& user_id_hash,
-                   base::CommandLine cmdline);
+                   base::CommandLine cmdline,
+                   std::unique_ptr<CrostiniStartupStatus> startup_status);
 
   using ProcessOutputCallback =
       base::RepeatingCallback<void(const std::string& terminal_id,
@@ -85,10 +89,12 @@ class TerminalPrivateOpenTerminalProcessFunction : public ExtensionFunction {
                                    const std::string& output)>;
   using OpenProcessCallback =
       base::OnceCallback<void(bool success, const std::string& terminal_id)>;
-  void OpenOnRegistryTaskRunner(ProcessOutputCallback output_callback,
-                                OpenProcessCallback callback,
-                                base::CommandLine cmdline,
-                                const std::string& user_id_hash);
+  void OpenOnRegistryTaskRunner(
+      ProcessOutputCallback output_callback,
+      OpenProcessCallback callback,
+      base::CommandLine cmdline,
+      const std::string& user_id_hash,
+      std::unique_ptr<CrostiniStartupStatus> startup_status);
   void RespondOnUIThread(bool success, const std::string& terminal_id);
 };
 
