ac35fc21c7a67bcba57026bdbdd7a538ea8796b7
dpapad <dpapad@chromium.org>
2021-04-20 22:16:25 +0000

    WebUI: Use an intermediate tsconfig file in ts_definitions().
    This is in preparation of generating .d.ts files for class-based
    PolymerElement subclasses, which requires dealing with path mappings.
    
    Bug: 1189595
    Change-Id: I0315ad7fa419d40fac0410127d2fa7c6917b9c55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2825410
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874456}

--- tools/typescript/ts_definitions.py
+++ tools/typescript/ts_definitions.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import argparse
+import json
 import os
 import re
 import sys
@@ -17,14 +18,38 @@ import node
 import node_modules
 
 
+_TSCONFIG_BASE = 'tsconfig_definitions_base.json'
+_TSCONFIG_GEN = 'tsconfig_definitions.json'
+
+
+def _write_tsconfig_json(gen_dir, tsconfig):
+  if not os.path.exists(gen_dir):
+    os.makedirs(gen_dir)
+
+  with open(os.path.join(gen_dir, _TSCONFIG_GEN), 'w') as generated_tsconfig:
+    json.dump(tsconfig, generated_tsconfig, indent=2)
+  return
+
+
 def main(argv):
   parser = argparse.ArgumentParser()
+  parser.add_argument('--gen_dir', required=True)
   parser.add_argument('--out_dir', required=True)
   parser.add_argument('--root_dir', required=True)
   parser.add_argument('--js_files', nargs='*', required=True)
   args = parser.parse_args(argv)
 
-  js_files = [os.path.join(args.root_dir, f) for f in args.js_files]
+  with open(os.path.join(_HERE_DIR, _TSCONFIG_BASE)) as root_tsconfig:
+    tsconfig = json.loads(root_tsconfig.read())
+
+  root_dir = os.path.relpath(args.root_dir, args.gen_dir)
+  out_dir = os.path.relpath(args.out_dir, args.gen_dir)
+
+  tsconfig['files'] = [os.path.join(root_dir, f) for f in args.js_files]
+  tsconfig['compilerOptions']['rootDir'] = root_dir
+  tsconfig['compilerOptions']['outDir'] = out_dir
+
+  _write_tsconfig_json(args.gen_dir, tsconfig)
 
   if (args.root_dir == args.out_dir):
     # Delete .d.ts files if they already exist, otherwise TypeScript compiler
@@ -36,17 +61,9 @@ def main(argv):
         os.remove(to_delete)
 
   node.RunNode([
-      node_modules.PathToTypescript(),
-      '--declaration',
-      '--allowJs',
-      '--emitDeclarationOnly',
-      '--removeComments',
-      '--noResolve',
-      '--rootDir',
-      args.root_dir,
-      '--outDir',
-      args.out_dir,
-  ] + js_files)
+      node_modules.PathToTypescript(), '--project',
+      os.path.join(args.gen_dir, _TSCONFIG_GEN)
+  ])
 
 
 if __name__ == '__main__':
