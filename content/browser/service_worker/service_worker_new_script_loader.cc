dff6bd46b1780c06c072e7dfeb6d801ed1410e52
Ghazale Hosseinabadi <ghazale@google.com>
2021-04-20 17:04:38 +0000

    Remove TODO in ServiceWorkerNewScriptLoader constructor
    Adding service-worker header is needed in
    ServiceWorkerNewScriptLoader constructor and the TODO should be removed.
    
    Bug: 1199892
    Change-Id: I9d4d61888a3ce554891529998c884372f8654988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2836225
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874302}

--- content/browser/service_worker/service_worker_new_script_loader.cc
+++ content/browser/service_worker/service_worker_new_script_loader.cc
@@ -110,9 +110,6 @@ ServiceWorkerNewScriptLoader::ServiceWorkerNewScriptLoader(
     // may be used by ServiceWorkerMainResourceLoader for navigations handled
     // by this service worker.
     options |= network::mojom::kURLLoadOptionSendSSLInfoWithResponse;
-
-    // TODO(crbug.com/1199892): Investigate if we still need to set the header
-    // here.
     resource_request.headers.SetHeader("Service-Worker", "script");
   }
 
