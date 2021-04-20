034e1788613af9114a8081c280611824739827c0
James Vecore <vecore@google.com>
2021-04-20 17:35:10 +0000

    [Nearby] Scope tasks with WILL_BLOCK and use default priority
    Previously, it was possible for tasks to get stuck in the queue with
    multi-threaded executor because tasks were being submitted to the shared
    process thread pool which would allocate threads too slowly (2 threads
    a second for BEST_EFFORT tasks, 4 for default priority).
    
    Two changes make the thread allocation for new tasks much quicker and
    this is verified with a unit-test that allocates 255 threads and ensures
    they can spin up fast enough. The changes are:
    
    1) Add a WILL_BLOCK scope/annotation when the task is executing
    2) Change the default task priority from BEST_EFFORT to the default.
    
    Both changes are necessary for the unit-test to pass without timing out.
    
    Bug: b/185628066
    Change-Id: I3391f9a2e844aa5023a41fdcdac39cf49f36c618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2833283
    Commit-Queue: James Vecore <vecore@google.com>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874318}

--- chrome/services/sharing/nearby/platform/submittable_executor.cc
+++ chrome/services/sharing/nearby/platform/submittable_executor.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/task/post_task.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
+#include "base/threading/scoped_blocking_call.h"
 #include "base/threading/thread_restrictions.h"
 
 namespace location {
@@ -77,6 +78,17 @@ void SubmittableExecutor::Execute(Runnable&& runnable) {
 
 void SubmittableExecutor::RunTask(Runnable&& runnable) {
   {
+    // The Nearby Connections library relies on many long running thread tasks
+    // which do not meet the usual usage pattern of a task on the chrome thread
+    // pool (short lived and non-blocking). Without WILL_BLOCK we end up thread
+    // starving tasks when multiple clients are using Nearby Connections at the
+    // same time because the thread pool by default is not big enough. By
+    // scoping this task as WILL_BLOCK we are letting the thread pool know that
+    // it should allocate an additional thread so this task does not starve
+    // other tasks if it does block and/or is long lived. See b/185628066 for
+    // examples of this starvation happening before this change.
+    base::ScopedBlockingCall blocking_call{FROM_HERE,
+                                           base::BlockingType::WILL_BLOCK};
     // base::ScopedAllowBaseSyncPrimitives is required as code inside the
     // runnable uses blocking primitive, which lives outside Chrome.
     base::ScopedAllowBaseSyncPrimitives allow_wait;
