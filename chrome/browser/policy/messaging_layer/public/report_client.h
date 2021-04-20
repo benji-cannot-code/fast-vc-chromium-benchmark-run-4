d0ddcf8e96739c0885e73c6549ce79e56c733b11
Leonid Baraz <lbaraz@chromium.org>
2021-04-20 05:30:49 +0000

    Handle multiple requests with ReportQueueProvider
    ReportQueueProvider and ReportingClient had several bugs that prevented
    them from handling multiple requests.
    
    1. ReportQueueProvider::InitializationStateTracker
    ::OnLeaderPromotionRequest() did not set
    has_promoted_initializing_context_ to true. This allowed following
    requests to fall through and also become leaders. Thereby also doing all
    initialization steps.
    
    2. ReportQueueProvider::InitializationContext::Complete did not properly
    handle the RESOURCE_EXHAUSTED error. Meaning that on Complete() calls,
    it would attempt to ReleaseLeader() even though the leader had not been
    obtained.
    
    3. ReportingClient::ClientInitializingContext::OnCompleted() called
    ReportQueueProvider::InitializingContext::OnCompleted() which posted a
    task to ReportQueueProvider::InitializingContext::OnCompleted which
    called ReportQueueProvider::InitializingContext::Completed() which
    called ReportQueueProvider::InitializingContext::OnCompleted() which was
    overloaded to ReportingClient::ClientInitializingContext::OnCompleted(),
    same as the first one.
    ReportingClient::ClientInitializingContext::OnCompleted() now calls
    ReportQueueProvider::InitializingContext::Complete() directly, which no
    longer calls OnCompleted().
    Removed UpdateConfigurationCallback, which is no longer needed.
    
    4. GetCloudPolicyClientCallback may be handed over to InitializingContext
    multiple times - changed from OnceCallback to RepeatingCallback.
    
    Bug: chromium:1197758
    Change-Id: Ide081b34c298a093187be707a01536b96132fada
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2817845
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874137}

--- chrome/browser/policy/messaging_layer/public/report_client.h
+++ chrome/browser/policy/messaging_layer/public/report_client.h
@@ -33,7 +33,7 @@ class ReportingClient : public ReportQueueProvider {
   using CreateReportQueueCallback =
       base::OnceCallback<void(CreateReportQueueResponse)>;
 
-  using GetCloudPolicyClientCallback = base::OnceCallback<void(
+  using GetCloudPolicyClientCallback = base::RepeatingCallback<void(
       base::OnceCallback<void(StatusOr<policy::CloudPolicyClient*>)>)>;
 
   class ClientInitializingContext
@@ -42,7 +42,6 @@ class ReportingClient : public ReportQueueProvider {
     ClientInitializingContext(
         GetCloudPolicyClientCallback get_client_cb,
         UploaderInterface::AsyncStartUploaderCb async_start_upload_cb,
-        UpdateConfigurationCallback update_config_cb,
         InitCompleteCallback init_complete_cb,
         ReportingClient* client,
         scoped_refptr<InitializationStateTracker> init_state_tracker);
@@ -84,7 +83,6 @@ class ReportingClient : public ReportQueueProvider {
   };
 
   ReportQueueProvider::InitializingContext* InstantiateInitializingContext(
-      InitializingContext::UpdateConfigurationCallback update_config_cb,
       InitCompleteCallback init_complete_cb,
       scoped_refptr<InitializationStateTracker> init_state_tracker) override;
 
