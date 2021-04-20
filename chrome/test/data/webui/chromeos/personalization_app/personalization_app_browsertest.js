e113ebb5a2d411fee62af8c4ac819d0c79bc3516
Jeffrey Young <cowmoo@google.com>
2021-04-20 18:46:38 +0000

    personalization: disable flaky test
    BUG=b/185905694
    
    Change-Id: I6ea9dd1d4ce1f123f92a701c6bf412f4a1285ee9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2840344
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874353}

--- chrome/test/data/webui/chromeos/personalization_app/personalization_app_browsertest.js
+++ chrome/test/data/webui/chromeos/personalization_app/personalization_app_browsertest.js
@@ -20,4 +20,5 @@ var PersonalizationAppBrowserTest = class extends PolymerTest {
   }
 };
 
-TEST_F('PersonalizationAppBrowserTest', 'All', () => mocha.run());
+// Disable until flaky failures are fixed: b/185905694
+TEST_F('PersonalizationAppBrowserTest', 'DISABLED_All', () => mocha.run());
