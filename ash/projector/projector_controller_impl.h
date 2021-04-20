33f417ee618ad34598c2b7afca0595649243825f
Toby Huang <tobyhuang@chromium.org>
2021-04-20 02:20:47 +0000

    Projector: Add stub button for selfie cam to toolbar
    This CL adds a stub button for the selfie cam to the Projector
    toolbar. This button currently doesn't do anything except for toggle
    between the "on" and "off" icons when clicked.
    
    Bug: 1199396
    Change-Id: I233fafc497ef46dd3d92e4011ad35b56577de537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2832114
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874096}

--- ash/projector/projector_controller_impl.h
+++ ash/projector/projector_controller_impl.h
@@ -62,6 +62,8 @@ class ASH_EXPORT ProjectorControllerImpl : public ProjectorController {
   void OnMarkerPressed();
   // Invoked when clear all markers button is pressed.
   void OnClearAllMarkersPressed();
+  // Invoked when selfie cam button is pressed.
+  void OnSelfieCamPressed(bool enabled);
 
   void SetProjectorUiControllerForTest(
       std::unique_ptr<ProjectorUiController> ui_controller);
