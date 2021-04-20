90088fb5ff072ec0e9aeb9a509ee2e5efb8c653d
Charlene Yan <cyan@chromium.org>
2021-04-20 19:14:32 +0000

    [Tab Scrolling] Remove scroll button flag and put behind scrolling flag.
    Bug: 1116118
    Change-Id: Ib2a933f776e176d417b3385bf0ac2be09f1f228b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2796130
    Commit-Queue: Charlene Yan <cyan@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874365}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1240,9 +1240,6 @@ extern const char kScrollableTabStripFlagId[];
 extern const char kScrollableTabStripName[];
 extern const char kScrollableTabStripDescription[];
 
-extern const char kScrollableTabStripButtonsName[];
-extern const char kScrollableTabStripButtonsDescription[];
-
 extern const char kScrollUnificationName[];
 extern const char kScrollUnificationDescription[];
 
