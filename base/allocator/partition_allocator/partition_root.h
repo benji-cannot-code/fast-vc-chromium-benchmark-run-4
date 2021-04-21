0b29107736fb4a585adc3eff84f5164fb4bd8f00
Lei Zhang <thestig@chromium.org>
2021-04-21 17:47:16 +0000

    Remove excess includes in partition_alloc.h.
    This header is included in Blink's platform/wtf/allocator/allocator.h,
    which in turn is included almost everywhere in Blink. Remove its excess
    includes. Then look at the remaining PartitionAlloc includes inside
    partition_alloc.h, and trim a bit from those as well.
    
    Change-Id: I290c920be7aa7aafbadb9eb639d2b2509d40a05a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2843443
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874756}

--- base/allocator/partition_allocator/partition_root.h
+++ base/allocator/partition_allocator/partition_root.h
@@ -48,11 +48,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_oom.h"
 #include "base/allocator/partition_allocator/partition_page.h"
 #include "base/allocator/partition_allocator/partition_ref_count.h"
-#include "base/allocator/partition_allocator/partition_stats.h"
 #include "base/allocator/partition_allocator/starscan/pcscan.h"
 #include "base/allocator/partition_allocator/thread_cache.h"
 #include "base/bits.h"
-#include "base/optional.h"
 #include "base/partition_alloc_buildflags.h"
 #include "build/build_config.h"
 
@@ -67,6 +65,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
 namespace base {
+
+class PartitionStatsDumper;
 
 namespace internal {
 // Avoid including partition_address_space.h from this .h file, by moving the
