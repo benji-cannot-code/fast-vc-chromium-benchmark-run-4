884b5653a2428390325f77e3ad910afaafb119db
Hiroki Nakagawa <nhiroki@chromium.org>
2021-04-21 01:53:05 +0000

    Prerender: Remove multiple-WebContents arch in favor of MPArch
    This CL contains following changes:
    
    > Flag changes
    
    - removes kPrerender2ImplementationParam.
    - removes IsPrerenderWebContentsEnabled().
    - replaces IsPrerenderMPArchEnabled() with IsPrerender2Enabled().
    
    > Implementation changes
    
    - removes PageHolderInterface and WebContentsPageHolder.
    - renames MPArchPageHolder to PageHolder.
    - removes dead code surrounded with IsPrerenderWebContentsEnabled().
    - removes tests parameterized with kPrerender2ImplementationParam.
    
    Bug: 1154501
    Change-Id: I69be74fbe3662fe61e73bcb9e02689e9a6fc4ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2784523
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874533}

--- content/browser/prerender/prerender_processor_unittest.cc
+++ content/browser/prerender/prerender_processor_unittest.cc
@@ -24,36 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 namespace {
 
-enum PrerenderTestType {
-  kWebContents,
-  kMPArch,
-};
-
-std::string ToString(const testing::TestParamInfo<PrerenderTestType>& info) {
-  switch (info.param) {
-    case PrerenderTestType::kWebContents:
-      return "WebContents";
-    case PrerenderTestType::kMPArch:
-      return "MPArch";
-  }
-}
-
-class PrerenderProcessorTest
-    : public RenderViewHostImplTestHarness,
-      public testing::WithParamInterface<PrerenderTestType> {
+class PrerenderProcessorTest : public RenderViewHostImplTestHarness {
  public:
   PrerenderProcessorTest() {
-    std::map<std::string, std::string> parameters;
-    switch (GetParam()) {
-      case kWebContents:
-        parameters["implementation"] = "webcontents";
-        break;
-      case kMPArch:
-        parameters["implementation"] = "mparch";
-        break;
-    }
-    scoped_feature_list_.InitAndEnableFeatureWithParameters(
-        blink::features::kPrerender2, parameters);
+    scoped_feature_list_.InitAndEnableFeature(blink::features::kPrerender2);
   }
 
   void SetUp() override {
@@ -70,15 +44,6 @@ class PrerenderProcessorTest
     web_contents_.reset();
     browser_context_.reset();
     RenderViewHostImplTestHarness::TearDown();
-  }
-
-  bool IsMPArchActive() const {
-    switch (GetParam()) {
-      case kWebContents:
-        return false;
-      case kMPArch:
-        return true;
-    }
   }
 
   RenderFrameHostImpl* GetRenderFrameHost() {
@@ -117,7 +82,7 @@ class PrerenderProcessorTest
   std::unique_ptr<TestWebContents> web_contents_;
 };
 
-TEST_P(PrerenderProcessorTest, StartCancel) {
+TEST_F(PrerenderProcessorTest, StartCancel) {
   RenderFrameHostImpl* render_frame_host = GetRenderFrameHost();
   PrerenderHostRegistry* registry = GetPrerenderHostRegistry();
 
@@ -142,7 +107,7 @@ TEST_P(PrerenderProcessorTest, StartCancel) {
   EXPECT_FALSE(registry->FindHostByUrlForTesting(kPrerenderingUrl));
 }
 
-TEST_P(PrerenderProcessorTest, StartDisconnect) {
+TEST_F(PrerenderProcessorTest, StartDisconnect) {
   RenderFrameHostImpl* render_frame_host = GetRenderFrameHost();
   PrerenderHostRegistry* registry = GetPrerenderHostRegistry();
 
@@ -168,7 +133,7 @@ TEST_P(PrerenderProcessorTest, StartDisconnect) {
   EXPECT_FALSE(registry->FindHostByUrlForTesting(kPrerenderingUrl));
 }
 
-TEST_P(PrerenderProcessorTest, CancelOnDestruction) {
+TEST_F(PrerenderProcessorTest, CancelOnDestruction) {
   RenderFrameHostImpl* render_frame_host = GetRenderFrameHost();
   PrerenderHostRegistry* registry = GetPrerenderHostRegistry();
 
@@ -205,7 +170,7 @@ TEST_P(PrerenderProcessorTest, CancelOnDestruction) {
   EXPECT_FALSE(registry->FindHostByUrlForTesting(kPrerenderingUrl));
 }
 
-TEST_P(PrerenderProcessorTest, StartTwice) {
+TEST_F(PrerenderProcessorTest, StartTwice) {
   RenderFrameHostImpl* render_frame_host = GetRenderFrameHost();
   PrerenderHostRegistry* registry = GetPrerenderHostRegistry();
 
@@ -243,7 +208,7 @@ TEST_P(PrerenderProcessorTest, StartTwice) {
   EXPECT_EQ(bad_message_error, "PP_START_TWICE");
 }
 
-TEST_P(PrerenderProcessorTest, CancelBeforeStart) {
+TEST_F(PrerenderProcessorTest, CancelBeforeStart) {
   RenderFrameHostImpl* render_frame_host = GetRenderFrameHost();
 
   mojo::Remote<blink::mojom::PrerenderProcessor> remote;
@@ -274,7 +239,7 @@ TEST_P(PrerenderProcessorTest, CancelBeforeStart) {
 // Tests that prerendering a cross-origin URL is aborted. Cross-origin
 // prerendering is not supported for now, but we plan to support it later
 // (https://crbug.com/1176054).
-TEST_P(PrerenderProcessorTest, CrossOrigin) {
+TEST_F(PrerenderProcessorTest, CrossOrigin) {
   RenderFrameHostImpl* render_frame_host = GetRenderFrameHost();
   PrerenderHostRegistry* registry = GetPrerenderHostRegistry();
 
@@ -306,7 +271,7 @@ TEST_P(PrerenderProcessorTest, CrossOrigin) {
 // Tests that prerendering triggered by <link rel=next> is aborted. This trigger
 // is not supported for now, but we may want to support it if NoStatePrefetch
 // re-enables it again. See https://crbug.com/1161545.
-TEST_P(PrerenderProcessorTest, RelTypeNext) {
+TEST_F(PrerenderProcessorTest, RelTypeNext) {
   RenderFrameHostImpl* render_frame_host = GetRenderFrameHost();
   PrerenderHostRegistry* registry = GetPrerenderHostRegistry();
 
@@ -346,7 +311,7 @@ TEST_P(PrerenderProcessorTest, RelTypeNext) {
   EXPECT_TRUE(bad_message_error.empty());
 }
 
-TEST_P(PrerenderProcessorTest, StartAfterNavigation) {
+TEST_F(PrerenderProcessorTest, StartAfterNavigation) {
   RenderFrameHostImpl* render_frame_host = GetRenderFrameHost();
   PrerenderHostRegistry* registry = GetPrerenderHostRegistry();
 
@@ -368,11 +333,6 @@ TEST_P(PrerenderProcessorTest, StartAfterNavigation) {
   remote.FlushForTesting();
   EXPECT_FALSE(registry->FindHostByUrlForTesting(kPrerenderingUrl));
 }
-
-INSTANTIATE_TEST_SUITE_P(All,
-                         PrerenderProcessorTest,
-                         testing::Values(kWebContents, kMPArch),
-                         ToString);
 
 }  // namespace
 }  // namespace content
