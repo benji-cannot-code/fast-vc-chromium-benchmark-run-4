4eb795bf9e9a31f85671eccd6a865aeafe688d61
Hidehiko Abe <hidehiko@chromium.org>
2021-04-21 05:05:09 +0000

    Reset composition when text_input::reset is called.
    Currently, on text_input::reset, InputMethod::OnCaretBoundsChanged
    is called.
    However, it is not the method to reset the input method,
    and as the result, wayland client and server has inconsistent
    state and it causes weird behavior.
    With this CL, it invokes CancelComposition to reset IME state.
    
    BUG=1163948
    TEST=Ran on DUT.
    
    Change-Id: Ia50e0af9a77d0d0114b4410e327a7220297a52c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831239
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874573}

--- components/exo/text_input.cc
+++ components/exo/text_input.cc
@@ -74,6 +74,12 @@ void TextInput::Resync() {
     input_method_->OnCaretBoundsChanged(this);
 }
 
+void TextInput::Reset() {
+  composition_ = ui::CompositionText();
+  if (input_method_)
+    input_method_->CancelComposition(this);
+}
+
 void TextInput::SetSurroundingText(const std::u16string& text,
                                    uint32_t cursor_pos,
                                    uint32_t anchor) {
@@ -123,6 +129,7 @@ uint32_t TextInput::ConfirmCompositionText(bool keep_selection) {
   const uint32_t composition_text_length =
       static_cast<uint32_t>(composition_.text.length());
   delegate_->Commit(composition_.text);
+  composition_ = ui::CompositionText();
   return composition_text_length;
 }
 
