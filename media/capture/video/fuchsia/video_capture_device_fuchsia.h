9a6dac79cf53c006b2472be6509334bce82db222
Sergey Ulanov <sergeyu@chromium.org>
2021-04-21 00:55:14 +0000

    [Fuchsia] Replace SysmemBuffer[Reader|Writer] with VmoBuffer
    SysmemBufferReader and SysmemBufferWriter classes were used to read/write
    from sysmem buffers. Replaced both with VmoBuffer. This reduces amount
    of redundant and duplicated code.
    Also renamd SysmemBufferWriterQueue to VmoBufferWriterQueue. In the
    future it can be used in FuchsiaAudioRenderer (which doesn't use sysmem).
    
    Bug: 1131183, 1195522
    Change-Id: Ife6a601b9b8980a5acf2a786647e0839c315ec3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2803484
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874511}

--- media/capture/video/fuchsia/video_capture_device_fuchsia.h
+++ media/capture/video/fuchsia/video_capture_device_fuchsia.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "media/capture/video/video_capture_device.h"
 #include "media/fuchsia/common/sysmem_buffer_pool.h"
-#include "media/fuchsia/common/sysmem_buffer_reader.h"
+#include "media/fuchsia/common/vmo_buffer.h"
 
 namespace media {
 
@@ -81,8 +81,10 @@ class CAPTURE_EXPORT VideoCaptureDeviceFuchsia : public VideoCaptureDevice {
   // Callback for SysmemBufferPool::Creator.
   void OnBufferCollectionCreated(std::unique_ptr<SysmemBufferPool> collection);
 
-  // Callback for SysmemBufferPool::CreateReader().
-  void OnBufferReaderCreated(std::unique_ptr<SysmemBufferReader> reader);
+  // Callback for SysmemBufferPool::AcquireBuffers().
+  void OnBuffersAcquired(
+      std::vector<VmoBuffer> buffers,
+      const fuchsia::sysmem::SingleBufferSettings& buffer_settings);
 
   // Calls Stream::GetNextFrame() in a loop to receive incoming frames.
   void ReceiveNextFrame();
@@ -99,7 +101,8 @@ class CAPTURE_EXPORT VideoCaptureDeviceFuchsia : public VideoCaptureDevice {
   media::BufferAllocator sysmem_allocator_;
   std::unique_ptr<SysmemBufferPool::Creator> buffer_collection_creator_;
   std::unique_ptr<SysmemBufferPool> buffer_collection_;
-  std::unique_ptr<SysmemBufferReader> buffer_reader_;
+  std::vector<VmoBuffer> buffers_;
+  fuchsia::sysmem::ImageFormatConstraints buffers_format_;
 
   base::Optional<gfx::Size> frame_size_;
   fuchsia::camera3::Orientation orientation_ =
--- media/fuchsia/cdm/fuchsia_stream_decryptor.h
+++ media/fuchsia/cdm/fuchsia_stream_decryptor.h
@@ -14,10 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/decryptor.h"
 #include "media/fuchsia/common/stream_processor_helper.h"
 #include "media/fuchsia/common/sysmem_buffer_pool.h"
-#include "media/fuchsia/common/sysmem_buffer_writer_queue.h"
+#include "media/fuchsia/common/vmo_buffer_writer_queue.h"
 
 namespace media {
-class SysmemBufferReader;
 
 // Base class for media stream decryptor implementations.
 class FuchsiaStreamDecryptorBase : public StreamProcessorHelper::Client {
@@ -43,7 +42,7 @@ class FuchsiaStreamDecryptorBase : public StreamProcessorHelper::Client {
 
   BufferAllocator allocator_;
 
-  SysmemBufferWriterQueue input_writer_queue_;
+  VmoBufferWriterQueue input_writer_queue_;
 
   // Key ID for which we received the last OnNewKey() event.
   std::string last_new_key_id_;
@@ -52,7 +51,9 @@ class FuchsiaStreamDecryptorBase : public StreamProcessorHelper::Client {
 
  private:
   void OnInputBufferPoolCreated(std::unique_ptr<SysmemBufferPool> pool);
-  void OnWriterCreated(std::unique_ptr<SysmemBufferWriter> writer);
+  void OnInputBuffersAcquired(
+      std::vector<VmoBuffer> buffers,
+      const fuchsia::sysmem::SingleBufferSettings& buffer_settings);
   void SendInputPacket(const DecoderBuffer* buffer,
                        StreamProcessorHelper::IoPacket packet);
   void ProcessEndOfStream();
@@ -90,14 +91,15 @@ class FuchsiaClearStreamDecryptor : public FuchsiaStreamDecryptorBase {
   void OnError() final;
 
   void OnOutputBufferPoolCreated(std::unique_ptr<SysmemBufferPool> pool);
-  void OnOutputBufferPoolReaderCreated(
-      std::unique_ptr<SysmemBufferReader> reader);
+  void OnOutputBuffersAcquired(
+      std::vector<VmoBuffer> buffers,
+      const fuchsia::sysmem::SingleBufferSettings& buffer_settings);
 
   Decryptor::DecryptCB decrypt_cb_;
 
   std::unique_ptr<SysmemBufferPool::Creator> output_pool_creator_;
   std::unique_ptr<SysmemBufferPool> output_pool_;
-  std::unique_ptr<SysmemBufferReader> output_reader_;
+  std::vector<VmoBuffer> output_buffers_;
 
   // Used to re-assemble decrypted output that was split between multiple sysmem
   // buffers.
