786dc8b941f1ae79936c0bc50025326531a3eb03
Ramin Halavati <rhalavati@chromium.org>
2021-04-21 18:18:38 +0000

    Set browser profile type for WebView BrowserContext.
    Profile classes created in Chrome browser store their type as user data
    in BrowserContext using profile_metrics::SetBrowserProfileType.
    
    AwBrowserContext is updated to store the same value.
    
    Bug: 1169142, 1201158
    Change-Id: I463fd963e123623dba340a5db8b986e67f2a4b17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842472
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874782}

--- android_webview/browser/aw_browser_context.cc
+++ android_webview/browser/aw_browser_context.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/prefs/pref_service_factory.h"
 #include "components/prefs/segregated_pref_store.h"
+#include "components/profile_metrics/browser_profile_type.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/url_formatter/url_fixer.h"
 #include "components/user_prefs/user_prefs.h"
@@ -151,6 +152,9 @@ AwBrowserContext::AwBrowserContext()
   DCHECK(!g_browser_context);
 
   TRACE_EVENT0("startup", "AwBrowserContext::AwBrowserContext");
+
+  profile_metrics::SetBrowserProfileType(
+      this, profile_metrics::BrowserProfileType::kRegular);
 
   g_web_view_compat_crash_key.Set("0");
 
