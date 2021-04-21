7fe30ca81c40d289d72a3bdeb81364e954f8d1a5
Christopher Cameron <ccameron@chromium.org>
2021-04-21 08:56:00 +0000

    MacPWAs: Dump on creating a zombie window
    Bug: 960904
    Change-Id: I23e9783826227087627c25f6dd66ab32c0d5be93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2841703
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874620}

--- components/remote_cocoa/app_shim/native_widget_mac_nswindow.mm
+++ components/remote_cocoa/app_shim/native_widget_mac_nswindow.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "components/remote_cocoa/app_shim/native_widget_mac_nswindow.h"
 
+#include "base/debug/dump_without_crashing.h"
 #include "base/mac/foundation_util.h"
 #include "base/trace_event/trace_event.h"
 #import "components/remote_cocoa/app_shim/native_widget_ns_window_bridge.h"
@@ -82,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   uint64_t _bridgedNativeWidgetId;
   remote_cocoa::NativeWidgetNSWindowBridge* _bridge;
   BOOL _willUpdateRestorableState;
+  BOOL _isEnforcingNeverMadeVisible;
 }
 @synthesize bridgedNativeWidgetId = _bridgedNativeWidgetId;
 @synthesize bridge = _bridge;
@@ -104,9 +106,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // inserting a symbol on NativeWidgetMacNSWindow and should be kept even if it
 // does nothing.
 - (void)dealloc {
+  if (_isEnforcingNeverMadeVisible) {
+    [self removeObserver:self forKeyPath:@"visible"];
+  }
   _willUpdateRestorableState = YES;
   [NSObject cancelPreviousPerformRequestsWithTarget:self];
   [super dealloc];
+}
+
+- (void)enforceNeverMadeVisible {
+  if (_isEnforcingNeverMadeVisible)
+    return;
+  _isEnforcingNeverMadeVisible = YES;
+  [self addObserver:self forKeyPath:@"visible" options:0 context:nil];
+}
+
+- (void)observeValueForKeyPath:(NSString*)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary*)change
+                       context:(void*)context {
+  DCHECK(_isEnforcingNeverMadeVisible);
+  DCHECK([keyPath isEqual:@"visible"]);
+  DCHECK_EQ(object, self);
+  DCHECK_EQ(context, nil);
+  base::debug::DumpWithoutCrashing();
 }
 
 // Public methods.
