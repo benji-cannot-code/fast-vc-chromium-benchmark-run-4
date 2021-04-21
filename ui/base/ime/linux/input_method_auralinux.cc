45562d33b29da1f6b1be6b3fb863392ce4ceb7aa
Hidehiko Abe <hidehiko@chromium.org>
2021-04-21 11:36:10 +0000

    Fix CTRL+T behavior for Lacros.
    CTRL+T was sent to text input client twice on loading,
    because the client type is none so that peek key event
    is also sent to the client in addition to the main key event.
    This CL removes the client input type check on deciding
    whether the event should be held or not.
    
    BUG=1199385
    TEST=Ran on DUT.
    
    Change-Id: I9c420b528d67f6561c57bd76f4c86c80952f20b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2838513
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874658}

--- ui/base/ime/linux/input_method_auralinux.cc
+++ ui/base/ime/linux/input_method_auralinux.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/bind.h"
 #include "base/environment.h"
+#include "build/chromeos_buildflags.h"
 #include "ui/base/ime/constants.h"
 #include "ui/base/ime/linux/linux_input_method_context_factory.h"
 #include "ui/base/ime/text_input_client.h"
@@ -154,7 +155,17 @@ ui::EventDispatchDetails InputMethodAuraLinux::DispatchKeyEvent(
   // consumed by IME and commit/preedit string update will happen
   // asynchronously. The remaining case is covered in OnCommit and
   // OnPreeditChanged/End.
-  if (filtered && !HasInputMethodResult() && !IsTextInputTypeNone()) {
+  // TODO(crbug.com/1199385): On Lacros CTRL+TAB events are sent twice if
+  // user types it on loading page, because the connected client is considered
+  // None type, and so the peek key event is not held here.
+  // To derisk the regression in other platform, and to prioritize the fix
+  // on Lacros, we conditionally do not check whether the connected client
+  // is None type for Lacros only. We should remove this soon.
+  if (filtered && !HasInputMethodResult()
+#if !BUILDFLAG(IS_CHROMEOS_LACROS)
+      && !IsTextInputTypeNone()
+#endif
+  ) {
     ime_filtered_key_event_ = std::move(*event);
     return ui::EventDispatchDetails();
   }
