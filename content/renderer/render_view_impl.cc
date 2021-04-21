2d931e16cff38a7095cc46f5264930cc1f5c70fa
Eric Lawrence <ericlaw@microsoft.com>
2021-04-21 14:33:31 +0000

    Remove kCloneSessionStorageForNoOpener escape hatch
    When we updated Chromium to stop cloning SessionStorage into new windows
    that specified |noopener|, we added a feature flag in case we needed to
    disable the change for compatibility reasons. We did not need to use
    the flag and it can now be safely removed.
    
    Bug: 1151381
    Change-Id: I6a5c1492cfa31949d66c1c5b73b72c1fa4f9a46f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2840725
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#874692}

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/agent_scheduling_group.h"
 #include "content/renderer/render_frame_proxy.h"
 #include "content/renderer/render_thread_impl.h"
-#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/impression_conversions.h"
 #include "third_party/blink/public/platform/modules/video_capture/web_video_capture_impl_manager.h"
 #include "third_party/blink/public/platform/url_conversion.h"
@@ -306,9 +305,7 @@ WebView* RenderViewImpl::CreateView(
   params->window_container_type = WindowFeaturesToContainerType(features);
 
   params->session_storage_namespace_id = session_storage_namespace_id;
-  if (!features.noopener ||
-      base::FeatureList::IsEnabled(
-          blink::features::kCloneSessionStorageForNoOpener)) {
+  if (!features.noopener) {
     params->clone_from_session_storage_namespace_id =
         GetWebView()->GetSessionStorageNamespaceId();
   }
