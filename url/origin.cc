941e3a824da3be48f9039ab3d994ce3cc3c7a3f6
Alexander Timin <altimin@chromium.org>
2021-04-21 01:34:27 +0000

    [tracing] Rename WriteIntoTracedValue to WriteIntoTrace in url/.
    Follow up on a rename missed in crrev.com/c/2837965.
    
    R=dcheng@chromium.org
    BUG=1137154
    
    Change-Id: Ide5485f1b7519e3a44023441077014585938cb28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842464
    Auto-Submit: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874526}

--- url/origin.cc
+++ url/origin.cc
@@ -374,7 +374,7 @@ base::Optional<Origin> Origin::Deserialize(const std::string& value) {
   return origin;
 }
 
-void Origin::WriteIntoTracedValue(perfetto::TracedValue context) const {
+void Origin::WriteIntoTrace(perfetto::TracedValue context) const {
   std::move(context).WriteString(GetDebugString());
 }
 
