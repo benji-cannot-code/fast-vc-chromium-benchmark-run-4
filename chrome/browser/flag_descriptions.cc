3b116163a9fc135022e8c1a93911fa47e7a828ba
Ben Goldberger <benwgold@google.com>
2021-04-21 18:37:15 +0000

    Add chrome flag for penetrating image selection
    Bug: 1177459
    Change-Id: Id9e2d550f9e3ebb351efb391ce5c63eddbab2bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2838590
    Commit-Queue: Ben Goldberger <benwgold@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874792}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1074,6 +1074,12 @@ const char kEnablePciguardUiDescription[] =
     "Enable toggling Pciguard settings through the Settings App. By default, "
     "this flag is enabled.";
 
+const char kEnablePenetratingImageSelectionName[] =
+    "Penetrating Image Selection";
+const char kEnablePenetratingImageSelectionDescription[] =
+    "Enables image options to be surfaced in the context menu for nodes "
+    "covered by transparent overlays.";
+
 const char kEnablePixelCanvasRecordingName[] = "Enable pixel canvas recording";
 const char kEnablePixelCanvasRecordingDescription[] =
     "Pixel canvas recording allows the compositor to raster contents aligned "
