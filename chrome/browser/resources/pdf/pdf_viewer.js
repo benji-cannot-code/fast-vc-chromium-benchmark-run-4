30b748f99c2415685ce84f92086967a9584dba41
Daniel Hosseinian <dhoss@chromium.org>
2021-04-21 17:11:02 +0000

    [pdf-viewer] Remove viewer-form-warning element
    The element has actually been unused since crrev.com/845969.
    
    Bug: 1169292
    Change-Id: I153db5983e2406dff4d9085f122b54d9e16672ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842627
    Auto-Submit: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874743}

--- chrome/browser/resources/pdf/pdf_viewer.js
+++ chrome/browser/resources/pdf/pdf_viewer.js
@@ -3,13 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// <if expr="chromeos">
+import './elements/viewer-ink-host.js';
+// </if>
 import './elements/viewer-password-dialog.js';
 import './elements/viewer-properties-dialog.js';
 import './elements/shared-vars.js';
-// <if expr="chromeos">
-import './elements/viewer-ink-host.js';
-import './elements/viewer-form-warning.js';
-// </if>
 import './pdf_viewer_shared_style.js';
 import 'chrome://resources/cr_elements/hidden_style_css.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
