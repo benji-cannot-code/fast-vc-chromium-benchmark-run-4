0c6356abcabeab1bbcea7a9506e49c383b91f313
Gayane Petrosyan <gayane@chromium.org>
2021-04-21 20:50:58 +0000

    Use the end pos of the node if incrementing current pos invalidates it
    A crash occurs when PositionIteratorInFlatTree is at last position of a
    shadow tree (example text node associated with an input field).
    Incrementing the iterator invalidates it and causes crash when
    computePosition is called.
    
    Therefore, after incrementing, check that it is still valid.
    
    Bug: 1200945
    Change-Id: I3134722d36897aafad497ebff1cd1358fe0329c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842343
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874848}

--- third_party/blink/renderer/core/editing/range_in_flat_tree.cc
+++ third_party/blink/renderer/core/editing/range_in_flat_tree.cc
@@ -22,7 +22,6 @@ void RangeInFlatTree::SetStart(const PositionInFlatTree& start) {
 }
 
 void RangeInFlatTree::SetEnd(const PositionInFlatTree& end) {
-  LOG(ERROR) << "end " << end;
   end_->SetPosition(ToPositionInDOMTree(end));
 }
 
