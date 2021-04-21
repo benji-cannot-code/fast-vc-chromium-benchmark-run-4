6ded97cdccc152b2c437bb97e88c12abe69bd0b1
Kent Tamura <tkent@chromium.org>
2021-04-21 06:37:40 +0000

    SVG Text NG: Add a stream printer for NGSVGcharacterdata
    It's helpful for debugging.
    This CL has no behavior changes.
    
    Bug: 1179585
    Change-Id: Icded5553a6c6470505994b3748aa78f34ea60792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2841730
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874589}

--- third_party/blink/renderer/core/layout/ng/svg/ng_svg_character_data.h
+++ third_party/blink/renderer/core/layout/ng/svg/ng_svg_character_data.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_SVG_NG_SVG_CHARACTER_DATA_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_SVG_NG_SVG_CHARACTER_DATA_H_
 
+#include <iosfwd>
+
 #include "third_party/blink/renderer/core/layout/svg/svg_character_data.h"
 
 namespace blink {
@@ -15,6 +17,8 @@ namespace blink {
 struct NGSVGCharacterData final : public SVGCharacterData {
   bool anchored_chunk = false;
 };
+
+std::ostream& operator<<(std::ostream& ostream, const NGSVGCharacterData& data);
 
 }  // namespace blink
 
