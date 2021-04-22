7f036af23e2d91f6c266fa61513183d595891b82
Alan Cutter <alancutter@chromium.org>
2021-04-22 05:49:19 +0000

    Unship Declarative Link Capturing origin trial on non-Chrome OS
    The Declarative Link Capturing origin trial was accidentally enabled
    on all desktop platforms. This CL restricts it to Chrome OS only.
    
    Bug: 1176482
    Change-Id: If27cb317cfe86bba85722b8816d232e32c8f9ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2843281
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875027}

--- third_party/blink/renderer/modules/manifest/manifest_parser.cc
+++ third_party/blink/renderer/modules/manifest/manifest_parser.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/feature_list.h"
+#include "build/chromeos_buildflags.h"
 #include "net/base/mime_util.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "third_party/blink/public/common/features.h"
@@ -1283,10 +1284,18 @@ String ManifestParser::ParseGCMSenderID(const JSONObject* object) {
 
 mojom::blink::CaptureLinks ManifestParser::ParseCaptureLinks(
     const JSONObject* object) {
+#if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+  bool origin_trial_enabled =
+      RuntimeEnabledFeatures::WebAppLinkCapturingEnabled(feature_context_);
+#else
+  bool origin_trial_enabled = false;
+  ALLOW_UNUSED_LOCAL(feature_context_);
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+
   // Parse if either the command line flag is passed (for about:flags) or the
   // runtime enabled feature is turned on (for origin trial).
   if (!base::FeatureList::IsEnabled(features::kWebAppEnableLinkCapturing) &&
-      !RuntimeEnabledFeatures::WebAppLinkCapturingEnabled(feature_context_)) {
+      !origin_trial_enabled) {
     return mojom::blink::CaptureLinks::kUndefined;
   }
 
