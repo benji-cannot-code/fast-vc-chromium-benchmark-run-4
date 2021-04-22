f372121ab129a95d193bd3de58f9c6c9af3ac358
Jan Krcal <jkrcal@chromium.org>
2021-04-22 15:21:47 +0000

    [Profile creation] Add js tests for enterprise-profile-welcome
    This CL adds a missing test suite for the enterprise-profile-welcome
    screen which is part of the signed-in profile creation flow.
    
    Bug: 1178494
    Change-Id: I2415b69b654ee2de912390502302a85095c8ea2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827911
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875169}

--- chrome/test/data/webui/signin/profile_switch_test.js
+++ chrome/test/data/webui/signin/profile_switch_test.js
@@ -13,7 +13,7 @@ import {waitBeforeNextRender} from '../test_util.m.js';
 
 import {TestManageProfilesBrowserProxy} from './test_manage_profiles_browser_proxy.js';
 
-suite('ProfilePickerMainViewTest', function() {
+suite('ProfileSwitchTest', function() {
   /** @type {!ProfileSwitchElement} */
   let profileSwitchElement;
 
