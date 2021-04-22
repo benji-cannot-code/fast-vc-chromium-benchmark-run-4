418adf409917a2c177f32a671487aa4e2ac48bd2
evliu <evliu@google.com>
2021-04-22 20:44:55 +0000

    Verify the SODA component architecture during installation
    This CL adds a verification step to the SODA component installer to verify that the architecture of the downloaded component matches that of the current installation of Chrome.
    
    Bug: 1182986
    Change-Id: Ieaecd8bdde569f3c69e3bb869b253a017c4c3f2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2829934
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#875327}

--- chrome/browser/component_updater/soda_component_installer.cc
+++ chrome/browser/component_updater/soda_component_installer.cc
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include <aclapi.h>
 #include <windows.h>
+
+#include "base/metrics/histogram_functions.h"
 #include "sandbox/win/src/sid.h"
 #endif
 
@@ -50,6 +52,21 @@ static_assert(base::size(kSodaPublicKeySHA256) == crypto::kSHA256Length,
               "Wrong hash length");
 
 constexpr char kSodaManifestName[] = "SODA Library";
+
+#if defined(OS_WIN)
+
+constexpr base::FilePath::CharType kSodaIndicatorFile[] =
+#if defined(ARCH_CPU_X86)
+    FILE_PATH_LITERAL("SODAFiles/arch_x86");
+#elif defined(ARCH_CPU_X86_64)
+    FILE_PATH_LITERAL("SODAFiles/arch_x64");
+#else
+    {};
+#endif
+
+static_assert(sizeof(kSodaIndicatorFile) > 0, "Unknown CPU architecture.");
+
+#endif
 
 }  // namespace
 
@@ -140,6 +157,19 @@ void SodaComponentInstallerPolicy::OnCustomUninstall() {}
 bool SodaComponentInstallerPolicy::VerifyInstallation(
     const base::DictionaryValue& manifest,
     const base::FilePath& install_dir) const {
+#ifdef OS_WIN
+  bool missing_indicator_file =
+      !base::PathExists(install_dir.Append(kSodaIndicatorFile));
+
+  base::UmaHistogramBoolean(
+      "Accessibility.LiveCaption.SodaVerificationFailureMissingIndicatorFile",
+      missing_indicator_file);
+
+  if (missing_indicator_file) {
+    return false;
+  }
+#endif
+
   return base::PathExists(install_dir.Append(speech::kSodaBinaryRelativePath));
 }
 
