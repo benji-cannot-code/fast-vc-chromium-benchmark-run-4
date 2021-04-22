418e7b6caa45c1485c86da12d963adc1d9aa7529
Patrick Noland <pnoland@chromium.org>
2021-04-22 21:41:52 +0000

    [Sheriff] Disable testCreateAndEditProfileMultipleTimes
    This test is flaky and disrupts screen orientation for other tests
    
    Bug: 1197105
    Change-Id: Ie9ff30f31fdae630ad2f9aa1618334b0c4305e9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2847082
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Owners-Override: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875358}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantPersonalDataManagerTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantPersonalDataManagerTest.java
@@ -193,6 +193,7 @@ public class AutofillAssistantPersonalDataManagerTest {
      */
     @Test
     @MediumTest
+    @FlakyTest(message = "https://crbug.com/1197105")
     public void testCreateAndEditProfileMultipleTimes() throws Exception {
         ArrayList<ActionProto> list = new ArrayList<>();
         list.add(
