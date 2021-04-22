3100bbc2c7f4369cc20cd172c4ed72e4839345b2
tby <tby@chromium.org>
2021-04-22 04:46:38 +0000

    [Launcher] Create skeleton classes for splitting search controller
    The plan for the category-based search prototype is to leave all
    current codepaths intact, and instead create parallel logic for the
    new behaviour. The SearchController will choose between these two
    options. This CL creates some skeleton classes and adds conditions
    for choosing the two options.
    
    Bug: 1199206
    Change-Id: I4581ebcf4c2e021b6f8d944b6008e1a9cf8f2861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2843144
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875012}

---
+++ chrome/browser/ui/app_list/search/ranking/ranker_delegate.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/app_list/search/ranking/ranker_delegate.h"
+
+namespace app_list {
+
+RankerDelegate::RankerDelegate(Profile* profile,
+                               AppListModelUpdater* model_updater,
+                               SearchController* controller) {}
+
+RankerDelegate::~RankerDelegate() {}
+
+}  // namespace app_list
