ea0872ca3ed387657562e3a2b8a97290645276be
Adam Langley <agl@chromium.org>
2021-04-22 14:23:03 +0000

    webauthn: show back button for caBLE sheet on Windows.
    When the Windows API is active we don't show a back arrow on the caBLE
    sheet. The drop-down menu is still there to select the native API but
    we're inconsistent across platforms about whether the transport
    selection sheet is accessible. It _is_ odd that we have both the TSS and
    the drop-down but, until we get rid of one of them, it's weirder to have
    platform-specific behaviour here.
    
    Change-Id: I6b57592d69e721cfaa5b6d6733f050df302fb3a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2845549
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875150}

--- chrome/browser/ui/webauthn/sheet_models.cc
+++ chrome/browser/ui/webauthn/sheet_models.cc
@@ -562,11 +562,7 @@ AuthenticatorPaaskSheetModel::AuthenticatorPaaskSheetModel(
 AuthenticatorPaaskSheetModel::~AuthenticatorPaaskSheetModel() = default;
 
 bool AuthenticatorPaaskSheetModel::IsBackButtonVisible() const {
-#if defined(OS_WIN)
-  return !base::FeatureList::IsEnabled(device::kWebAuthUseNativeWinApi);
-#else
   return true;
-#endif
 }
 
 bool AuthenticatorPaaskSheetModel::IsActivityIndicatorVisible() const {
@@ -652,11 +648,7 @@ AuthenticatorPaaskV2SheetModel::AuthenticatorPaaskV2SheetModel(
 AuthenticatorPaaskV2SheetModel::~AuthenticatorPaaskV2SheetModel() = default;
 
 bool AuthenticatorPaaskV2SheetModel::IsBackButtonVisible() const {
-#if defined(OS_WIN)
-  return !base::FeatureList::IsEnabled(device::kWebAuthUseNativeWinApi);
-#else
   return true;
-#endif
 }
 
 bool AuthenticatorPaaskV2SheetModel::IsActivityIndicatorVisible() const {
