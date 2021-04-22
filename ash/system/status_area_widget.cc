93cc4ae96450f11adae9b8aa77fd179c6b831b08
Jiaming Cheng <jiamingc@chromium.org>
2021-04-22 02:27:18 +0000

    Fix clock position when there's a time change
    Add a new observer in TimeView to observe the TimeView's layout change.
    When the length of the label text changes, we should update the
    statusAreawidget layout. This new observer will also cover these 2 cases:
    on time format change(12hours/24hours) and on time set change. So the
    ClockObserver is removed from the StatusAreaWidget.
    
    Bug: 1162119
    
    Change-Id: I3ae5f200b8f381d82999143368da949bfa024645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842500
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874983}

--- ash/system/status_area_widget.cc
+++ ash/system/status_area_widget.cc
@@ -159,8 +159,6 @@ void StatusAreaWidget::Initialize() {
 
   Shell::Get()->session_controller()->AddObserver(this);
 
-  Shell::Get()->system_tray_model()->clock()->AddObserver(this);
-
   // NOTE: Container may be hidden depending on login/display state.
   Show();
 
@@ -169,7 +167,6 @@ void StatusAreaWidget::Initialize() {
 
 StatusAreaWidget::~StatusAreaWidget() {
   Shell::Get()->session_controller()->RemoveObserver(this);
-  Shell::Get()->system_tray_model()->clock()->RemoveObserver(this);
 }
 
 // static
@@ -195,18 +192,6 @@ void StatusAreaWidget::SetSystemTrayVisibility(bool visible) {
     tray->CloseBubble();
     Hide();
   }
-}
-
-void StatusAreaWidget::OnDateFormatChanged() {
-  CalculateTargetBounds();
-  UpdateLayout(false /*animate*/);
-  UpdateCollapseState();
-}
-
-void StatusAreaWidget::OnSystemClockTimeUpdated() {
-  CalculateTargetBounds();
-  UpdateLayout(false /*animate*/);
-  UpdateCollapseState();
 }
 
 void StatusAreaWidget::OnSessionStateChanged(
