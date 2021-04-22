69db570453a528869e774609aa01353de554bd43
Morten Stenshorne <mstensho@chromium.org>
2021-04-22 08:52:33 +0000

    Remove NGLayoutResult::IsSingleUse().
    Sole fragments (i.e. when nodes don't break into multiple fragments)
    will now be cached, also when they are inside a block fragmentation
    context. We already had code in place to miss the cache if fragmentainer
    size or fragmentainer offset changes. Added a few unit tests.
    
    This is in preparation for another CL, where the SingleUse flag would no
    longer be conditioned on simply being inside block fragmentation.
    Removing the flag will ensure consistency, and make the logic simpler.
    And as a "bonus" we'll sporadically hit the cache even when
    block-fragmented. :)
    
    Bug: 829028
    Change-Id: Id9b23729ef3e6af4e776d0bfceebf5428755a927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2836147
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875065}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -3275,7 +3275,7 @@ bool LayoutBox::NGPhysicalFragmentList::Contains(
 void LayoutBox::SetCachedLayoutResult(const NGLayoutResult* result) {
   NOT_DESTROYED();
   DCHECK(!result->PhysicalFragment().BreakToken());
-  DCHECK(!result->IsSingleUse());
+  DCHECK(To<NGPhysicalBoxFragment>(result->PhysicalFragment()).IsOnlyForNode());
 
   if (result->GetConstraintSpaceForCaching().CacheSlot() ==
       NGCacheSlot::kMeasure) {
@@ -3431,7 +3431,7 @@ const NGLayoutResult* LayoutBox::GetCachedLayoutResult() const {
     return nullptr;
   // Only return re-usable results.
   const NGLayoutResult* result = layout_results_[0];
-  if (result->IsSingleUse())
+  if (!To<NGPhysicalBoxFragment>(result->PhysicalFragment()).IsOnlyForNode())
     return nullptr;
   DCHECK(!result->PhysicalFragment().IsLayoutObjectDestroyedOrMoved() ||
          BeingDestroyed());
@@ -3444,7 +3444,8 @@ const NGLayoutResult* LayoutBox::GetCachedMeasureResult() const {
   if (!measure_result_)
     return nullptr;
 
-  if (measure_result_->IsSingleUse())
+  if (!To<NGPhysicalBoxFragment>(measure_result_->PhysicalFragment())
+           .IsOnlyForNode())
     return nullptr;
 
   return measure_result_;
