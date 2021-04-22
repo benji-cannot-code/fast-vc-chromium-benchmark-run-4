e33223bfab045a2692eb747ee521427c6fb76042
Boris Sazonov <bsazonov@chromium.org>
2021-04-22 20:39:33 +0000

    Remove redundant include from save_card_manage_cards_bubble_views.cc
    save_card_manage_cards_bubble_views.cc doesn't use anything from
    dice_bubble_sync_promo_view.h, so this include can be safely removed.
    
    Bug: 1198523
    Change-Id: Id7281a8636957b076c0f60e5b596a2629aca8df0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2839771
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875326}

--- chrome/browser/ui/views/autofill/payments/save_card_manage_cards_bubble_views.cc
+++ chrome/browser/ui/views/autofill/payments/save_card_manage_cards_bubble_views.cc
@@ -19,10 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
 
-#if !BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/ui/views/sync/dice_bubble_sync_promo_view.h"
-#endif
-
 namespace autofill {
 
 SaveCardManageCardsBubbleViews::SaveCardManageCardsBubbleViews(
