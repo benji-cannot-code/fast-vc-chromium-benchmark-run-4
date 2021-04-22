abbecbcbe69dc757c94b7169cf8d1113e68316ea
Joon Ahn <joonbug@chromium.org>
2021-04-22 22:40:35 +0000

    shimless: convert PWA to SWA
    Bug: 1198187
    Change-Id: I5a6b9a74763619e17251b7d39dee9f4fde55de35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2824732
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875390}

--- ash/content/shimless_rma/url_constants.cc
+++ ash/content/shimless_rma/url_constants.cc
@@ -8,5 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 const char kChromeUIShimlessRMAHost[] = "shimless-rma";
+const char kChromeUIShimlessRMAUrl[] = "chrome://shimless-rma";
 
 }  // namespace ash
