c83413aec935ba2660068473dd2475b9b7fd56ee
Lei Zhang <thestig@chromium.org>
2021-04-22 17:03:05 +0000

    Do IWYU for alternative_state_name_map_updater.h.
    Some STL headers that should be included directly currently comes from 4
    transitive includes away. Also fix include ordering in nearby
    alternative_state_name_map.h.
    
    Change-Id: I09f7bab8d7646f6ac1b002a77e28560773372705
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2845553
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875221}

--- components/autofill/core/browser/geo/alternative_state_name_map.h
+++ components/autofill/core/browser/geo/alternative_state_name_map.h
@@ -8,15 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "components/autofill/core/browser/proto/states.pb.h"
-
 #include "base/i18n/case_conversion.h"
 #include "base/no_destructor.h"
 #include "base/optional.h"
 #include "base/sequence_checker.h"
 #include "base/types/strong_alias.h"
+#include "components/autofill/core/browser/proto/states.pb.h"
 
 namespace autofill {
+
 // AlternativeStateNameMap encapsulates mappings from state names in the
 // profiles to their localized and the abbreviated names.
 //
