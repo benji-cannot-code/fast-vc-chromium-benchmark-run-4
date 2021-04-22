e3ad9b8872374a2d51b1919ddde391b270c71bd5
Talal Jawaid <jawaid@google.com>
2021-04-22 22:47:37 +0000

    Adding front end components for Store Hours feature
    Bug: 1195055
    Change-Id: I1f1d63a443c2ced8e2f0b6e18cac4e026e2d2ba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2807254
    Commit-Queue: Talal Jawaid <jawaid@google.com>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875392}

---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/StoreTrackingUtilities.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+/**
+ * A class to handle whether store hours feature is enabled.
+ */
+public class StoreTrackingUtilities {
+    /**
+     * @return Whether the show store hours on tabs feature is enabled.
+     */
+    public static boolean isStoreHoursOnTabsEnabled() {
+        // TODO(crbug.com/1198277) This should be enabled when the feature flag is on and the user
+        // is signed in
+        return false;
+    }
+}
