c50806e1b7ad4660e51e5a2c6e5aa2ec7bdbaa95
John Abd-El-Malek <jam@chromium.org>
2021-04-22 00:10:12 +0000

    Fix Pepper APIs that use storage::FileSystemContext when ProcessHostOnUI is enabled.
    storage::FileSystemContext lives on the IO thread and has to keep being used there, so introduce hops when necessary to go from the UI thread (where the Pepper API implementations now live) to the IO thread and back.
    
    This was exposed by the relevant browser tests.
    
    Bug: 904556
    Change-Id: Ibeb9005abb84f10bcf29161058a596e490c88b88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2837470
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#874937}

--- content/browser/renderer_host/pepper/pepper_file_system_browser_host_unittest.cc
+++ content/browser/renderer_host/pepper/pepper_file_system_browser_host_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "content/browser/renderer_host/pepper/browser_ppapi_host_test.h"
+#include "content/public/test/browser_task_environment.h"
 #include "ppapi/c/pp_instance.h"
 #include "ppapi/c/pp_resource.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -38,6 +39,8 @@ class PepperFileSystemBrowserHostTest : public testing::Test,
   }
 
  private:
+  // Needed because |host_| has checks for UI/IO threads.
+  BrowserTaskEnvironment task_environment_;
   std::unique_ptr<PepperFileSystemBrowserHost> host_;
 
   DISALLOW_COPY_AND_ASSIGN(PepperFileSystemBrowserHostTest);
