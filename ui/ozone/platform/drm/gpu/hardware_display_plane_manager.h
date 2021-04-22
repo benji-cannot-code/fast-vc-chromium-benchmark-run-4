dee51cc968f925ac880a20c272637d30f4ff4895
Mark Yacoub <markyacoub@google.com>
2021-04-22 18:58:13 +0000

    drm/ozone: Save all overlay planes framebuffers after modeset.
    No longer assume that we modeset with only 1 primary plane.
    Save all modeset buffers and modeset planes after modesetting rather
    than only the primary plane and buffer.
    This allows us to modeset with more than just 1 plane in the future.
    
    BUG=b:185899132
    TEST=HardwareDisplayControllerTest.MultiplePlanesModeset
    
    Change-Id: Ie81d1da56addb62af109b3e60c28c52c2649304a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2844251
    Commit-Queue: Mark Yacoub <markyacoub@google.com>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875291}

--- ui/ozone/platform/drm/gpu/hardware_display_plane_manager.h
+++ ui/ozone/platform/drm/gpu/hardware_display_plane_manager.h
@@ -83,7 +83,7 @@ class HardwareDisplayPlaneManager {
     CrtcState(CrtcState&&);
 
     drmModeModeInfo mode = {};
-    scoped_refptr<DrmFramebuffer> modeset_framebuffer;
+    std::vector<scoped_refptr<DrmFramebuffer>> modeset_framebuffers;
 
     CrtcProperties properties = {};
 
@@ -188,7 +188,7 @@ class HardwareDisplayPlaneManager {
   // which resources needed to be tracked internally in
   // HardwareDisplayPlaneManager and which should be taken care of by the
   // caller.
-  void ResetModesetBufferOfCrtc(uint32_t crtc_id);
+  void ResetModesetStateForCrtc(uint32_t crtc_id);
 
  protected:
   struct ConnectorProperties {
