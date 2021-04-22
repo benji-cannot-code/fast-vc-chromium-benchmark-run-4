a1bbc4ee9e33059c29dcba315267de0bf74ce571
Bartek Nowierski <bartekn@chromium.org>
2021-04-22 21:36:39 +0000

    [PA][BRP] Set up BackupRefPtrNoEnterprise synthetic field trial
    The aim is to set up a binary A/B in Stable, that enables BackupRefPtr
    for 1% of non-enterprise population and sets aside another 1% for the
    control group.
    
    To test the non-enterprise logic, this CL also sets up
    EnterpriseSynthetic synthetic field trial. The reason why we care about
    avoiding enterprises is because we don't want to violate
    the ChromeVariations policy.
    
    Details of the design are described in:
    https://docs.google.com/document/d/1F1ra13q2Drl6ihuUHyfY3xYVLnBKBM6RMsSxkYG4JCw/edit#
    
    Bug: 1197313
    Change-Id: I5c27966c71276af5aaf364c8b8f64d479620e81a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2805832
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Robert Shield <robertshield@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875351}

--- chrome/browser/google/google_brand.cc
+++ chrome/browser/google/google_brand.cc
@@ -151,6 +151,11 @@ bool IsInternetCafeBrandCode(const std::string& brand) {
   return base::Contains(kBrands, brand);
 }
 
+bool IsEnterprise(const std::string& brand) {
+  return brand == "GGRV" ||
+         base::StartsWith(brand, "GCE", base::CompareCase::SENSITIVE);
+}
+
 // BrandForTesting ------------------------------------------------------------
 
 BrandForTesting::BrandForTesting(const std::string& brand) : brand_(brand) {
