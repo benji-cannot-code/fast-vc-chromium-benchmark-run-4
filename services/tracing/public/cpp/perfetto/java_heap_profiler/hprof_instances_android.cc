2893c602d73faaa5df9b2fb90531aa49e913c8e1
ssid <ssid@chromium.org>
2021-04-22 23:12:52 +0000

    Get root types and registered native size of objects
    Parse the root types which are needed to identify which nodes can be
    reached in the heap graph.
    Find the native size of the objects based on size registered in native
    registry, and add the size to the object's size.
    
    Change-Id: I986bf34244c0ec0ac61b410344481c0aebad014a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809148
    Reviewed-by: Salvador Guerrero <salg@google.com>
    Commit-Queue: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875408}

--- services/tracing/public/cpp/perfetto/java_heap_profiler/hprof_instances_android.cc
+++ services/tracing/public/cpp/perfetto/java_heap_profiler/hprof_instances_android.cc
@@ -5,22 +5,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/tracing/public/cpp/perfetto/java_heap_profiler/hprof_instances_android.h"
 
+#include "services/tracing/public/cpp/perfetto/java_heap_profiler/hprof_buffer_android.h"
 #include "services/tracing/public/cpp/perfetto/java_heap_profiler/hprof_data_type_android.h"
 
 namespace tracing {
 
 Instance::Instance(uint64_t object_id) : object_id(object_id) {}
-Instance::Instance(uint64_t object_id, std::string type_name)
+Instance::Instance(uint64_t object_id, const std::string& type_name)
     : type_name(type_name), object_id(object_id) {}
-Instance::Instance(uint64_t object_id, uint32_t size)
+Instance::Instance(uint64_t object_id, uint64_t size)
     : object_id(object_id), size(size) {}
 
-Instance::Instance(uint64_t object_id, uint32_t size, std::string type_name)
+Instance::Instance(uint64_t object_id,
+                   uint64_t size,
+                   const std::string& type_name)
     : type_name(type_name), object_id(object_id), size(size) {}
 
 Instance::Instance(const Instance& other) = default;
 
-Instance::~Instance() {}
+Instance::~Instance() = default;
 
 void Instance::AddReference(const std::string& name, uint64_t object_id) {
   references.push_back({name, object_id});
@@ -33,13 +36,13 @@ ClassInstance::ClassInstance(uint64_t object_id,
       class_id(class_id),
       temp_data_position(temp_data_position) {}
 
-Field::Field(std::string name, DataType type, uint64_t object_id)
+Field::Field(const std::string& name, DataType type, uint64_t object_id)
     : name(name), type(type), object_id(object_id) {}
 
-ClassObject::ClassObject(uint64_t object_id, std::string type_name)
+ClassObject::ClassObject(uint64_t object_id, const std::string& type_name)
     : base_instance(object_id, type_name) {}
 
-ClassObject::~ClassObject() {}
+ClassObject::~ClassObject() = default;
 
 ObjectArrayInstance::ObjectArrayInstance(uint64_t object_id,
                                          uint64_t class_id,
@@ -53,7 +56,7 @@ ObjectArrayInstance::ObjectArrayInstance(uint64_t object_id,
 
 PrimitiveArrayInstance::PrimitiveArrayInstance(uint64_t object_id,
                                                DataType type,
-                                               std::string type_name,
+                                               const std::string& type_name,
                                                uint64_t size)
     : base_instance(object_id, size, type_name), type(type) {}
 
