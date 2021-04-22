a891692794884211a15a7b0eb2cb6514873621b6
Shakti Sahu <shaktisahu@chromium.org>
2021-04-22 11:03:17 +0000

    [segmentation_platform] Setup initial directory and build targets
    This CL sets up the initial directory structure for segmentation
    platform. Since this is a cross-platform module and will be used by
    other components, we added this to components/segmentation_platform.
    
    Added some build targets and empty interfaces.
    
    Bug: 1199551
    Change-Id: I117d614cbf500569a7bea39df0c5bf1eb617e219
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2829960
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875105}

---
+++ components/segmentation_platform/internal/android/java/src/org/chromium/components/segmentation_platform/SegmentationPlatformServiceImpl.java
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.segmentation_platform;
+
+/**
+ * Java side of the JNI bridge between SegmentationPlatformServiceImpl in Java
+ * and C++. All method calls are delegated to the native C++ class.
+ */
+public class SegmentationPlatformServiceImpl implements SegmentationPlatformService {}
