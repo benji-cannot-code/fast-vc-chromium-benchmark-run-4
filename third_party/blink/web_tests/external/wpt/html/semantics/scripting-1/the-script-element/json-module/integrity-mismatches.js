6b3f8d8b148e1be5bd62341fa26c58eb671c8506
Daniel Clark <daniec@microsoft.com>
2021-04-22 18:42:51 +0000

    Add JSON modules tests for charset, integrity, and MIME type parameters
    Add JSON modules tests for the following:
    - The <script> element's integrity attribute is respected.
    - JSON modules are always decoded as UTF-8 regardless of Content-Type
      or the document's encoding.  The existing coverage we had on this in
      utf8.tenative.html was renamed to charset.html, refactored, and
      expanded.
    - Adding parameters to the MIME type doesn't prevent it from being
      evaluated as a JSON MIME type.
    
    Note, some of the existing MIME type tests for */*+json still fail
    because it seems the spec issue on these at
    https://github.com/whatwg/mimesniff/issues/112 hasn't been fully
    resolved. I'll follow up with that separately.
    
    Bug: 1132413
    Change-Id: I682de01bbb14b53214fcc16f427a4875f8cfed7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2841103
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#875274}

---
+++ third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/json-module/integrity-mismatches.js
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import json "./data.json" assert { type: "json" };
+window.mismatchesLog.push(`integrity-mismatches,json:${json.answer}`);
