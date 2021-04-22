0fbc544f718cac48cb50506c7e66426ef948828f
Matthias Körber <koerber@google.com>
2021-04-22 12:07:58 +0000

    [Autofill] Get profile difference as map keyed by type
    This CL also adds the COMPANY to the list over settings visible types.
    
    Change-Id: I2843289c3e8837814864947a8977dee563bca77d
    Bug: 1175693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2845393
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Auto-Submit: Matthias Körber <koerber@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/master@{#875125}

--- components/autofill/core/browser/data_model/autofill_profile_comparator.h
+++ components/autofill/core/browser/data_model/autofill_profile_comparator.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
+#include "base/containers/flat_map.h"
 #include "base/strings/string_piece.h"
 #include "components/autofill/core/browser/data_model/address.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
@@ -85,6 +86,15 @@ class AutofillProfileComparator {
   GetSettingsVisibleProfileDifference(const AutofillProfile& first_profile,
                                       const AutofillProfile& second_profile,
                                       const std::string& app_locale);
+
+  // Same as `GetSettingsVisibleProfileDifference()` but returns a map that maps
+  // the type to a pair of strings that contain the corresponding value from the
+  // first and second profile.
+  static base::flat_map<ServerFieldType,
+                        std::pair<std::u16string, std::u16string>>
+  GetSettingsVisibleProfileDifferenceMap(const AutofillProfile& first_profile,
+                                         const AutofillProfile& second_profile,
+                                         const std::string& app_locale);
 
   // Returns a copy of |text| with uppercase converted to lowercase and
   // diacritics removed.
