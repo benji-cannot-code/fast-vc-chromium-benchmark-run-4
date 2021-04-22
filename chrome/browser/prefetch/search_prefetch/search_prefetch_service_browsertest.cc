deb3a95a66fe85eefc7c4fa7b73fc3b5e8b8cb81
Ryan Sturm <ryansturm@chromium.org>
2021-04-22 22:32:50 +0000

    Launching Search Prefetch
    This CL launches Search Prefetch based on the experimental params.
    
    Bug: 1135715
    Change-Id: Ibaae02a4bd9523fbda4a4047ed34d2aa5665efe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2847440
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875386}

--- chrome/browser/prefetch/search_prefetch/search_prefetch_service_browsertest.cc
+++ chrome/browser/prefetch/search_prefetch/search_prefetch_service_browsertest.cc
@@ -1017,8 +1017,13 @@ IN_PROC_BROWSER_TEST_P(SearchPrefetchServiceEnabledBrowserTest,
   histogram_tester.ExpectUniqueSample(
       "Omnibox.SearchPrefetch.PrefetchEligibilityReason",
       SearchPrefetchEligibilityReason::kPrefetchStarted, 2);
-  EXPECT_FALSE(search_prefetch_service->MaybePrefetchURL(
+  EXPECT_TRUE(search_prefetch_service->MaybePrefetchURL(
       GetSearchServerQueryURL("prefetch_3")));
+  histogram_tester.ExpectUniqueSample(
+      "Omnibox.SearchPrefetch.PrefetchEligibilityReason",
+      SearchPrefetchEligibilityReason::kPrefetchStarted, 3);
+  EXPECT_FALSE(search_prefetch_service->MaybePrefetchURL(
+      GetSearchServerQueryURL("prefetch_4")));
   histogram_tester.ExpectBucketCount(
       "Omnibox.SearchPrefetch.PrefetchEligibilityReason",
       SearchPrefetchEligibilityReason::kMaxAttemptsReached, 1);
@@ -1035,6 +1040,11 @@ IN_PROC_BROWSER_TEST_P(SearchPrefetchServiceEnabledBrowserTest,
 
   prefetch_status =
       search_prefetch_service->GetSearchPrefetchStatusForTesting(u"prefetch_3");
+  ASSERT_TRUE(prefetch_status.has_value());
+  EXPECT_EQ(SearchPrefetchStatus::kInFlight, prefetch_status.value());
+
+  prefetch_status =
+      search_prefetch_service->GetSearchPrefetchStatusForTesting(u"prefetch_4");
   EXPECT_FALSE(prefetch_status.has_value());
 }
 
@@ -2572,8 +2582,10 @@ IN_PROC_BROWSER_TEST_F(SearchPrefetchServiceZeroCacheTimeBrowserTest,
       GetSearchServerQueryURL("prefetch_1")));
   EXPECT_TRUE(search_prefetch_service->MaybePrefetchURL(
       GetSearchServerQueryURL("prefetch_2")));
-  EXPECT_FALSE(search_prefetch_service->MaybePrefetchURL(
+  EXPECT_TRUE(search_prefetch_service->MaybePrefetchURL(
       GetSearchServerQueryURL("prefetch_3")));
+  EXPECT_FALSE(search_prefetch_service->MaybePrefetchURL(
+      GetSearchServerQueryURL("prefetch_4")));
 
   WaitUntilStatusChangesTo(u"prefetch_1", base::nullopt);
 
