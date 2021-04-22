1233b867b93b20fe2799f8497681fc27e615f727
Alex Keng <shihken@microsoft.com>
2021-04-22 14:32:49 +0000

    EditContext: bind editContext.focus with element.focus
    This CL binds editContext.focus() with element.focus().
    When an element is focused/blurred, its associated editContext,
    if it has any, will automatically get focused/blurred.
    
    If an element's editContext is updated while the element is in focus,
    the old editContext will be blurred, and the new editContext will
    automatically get focused.
    
    This simplifies the model of the activation of the editContext.
    The original model requires two focus() calls, one on element,
    one on editContext, which not only creates confusion, but also
    introduces unnecessary complexity. It appears that there is
    no real user scenario where editContext needs to be activated alone
    without an associated element which provides user-facing view to
    show input text to the users.
    
    With this CL, editContext.focus()/blur() is no longer exposed to JS.
    
    Tests are updated accordingly. Some expected texts in the test are
    changed from lowercase to uppercase to make sure the textupdate event
    handler is executed.
    
    Bug: 999184
    Change-Id: Id99917743f8558e15e3e6c325979d93007247429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2794801
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Anupam Snigdha <snianu@microsoft.com>
    Commit-Queue: Alex Keng <shihken@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#875153}

--- third_party/blink/renderer/core/page/focus_controller.cc
+++ third_party/blink/renderer/core/page/focus_controller.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"  // For firstPositionInOrBeforeNode
 #include "third_party/blink/renderer/core/editing/frame_selection.h"
+#include "third_party/blink/renderer/core/editing/ime/edit_context.h"
 #include "third_party/blink/renderer/core/editing/ime/input_method_controller.h"
 #include "third_party/blink/renderer/core/frame/frame_client.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
@@ -1318,6 +1319,18 @@ bool FocusController::SetFocusedElement(Element* element,
         new_document->SetFocusedElement(element, params);
     if (!successfully_focused)
       return false;
+
+    // EditContext's activation is synced with the associated element being
+    // focused or not. If an element loses focus, its associated EditContext
+    // is deactivated. If getting focus, the EditContext is activated.
+    if (old_focused_element) {
+      if (auto* old_editContext = old_focused_element->editContext())
+        old_editContext->blur();
+    }
+    if (element) {
+      if (auto* editContext = element->editContext())
+        editContext->focus();
+    }
   }
 
   return true;
