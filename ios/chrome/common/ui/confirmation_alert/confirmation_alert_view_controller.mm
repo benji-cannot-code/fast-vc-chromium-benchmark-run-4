738e5f0c88c102b4c5a0bd39f598b79edc64031b
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2021-04-22 18:06:30 +0000

    [iOS][Default Promo] Fix 'iOS' capitalization issue
    Bug: 1201696
    Change-Id: I500786efd2f93005b0cabe64752ddc3c92eef1cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846865
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Javier Flores <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875253}

--- ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.mm
+++ ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.mm
@@ -81,6 +81,7 @@ constexpr CGFloat kSafeAreaMultiplier = 0.8;
     _customSpacingAfterImage = kStackViewSpacingAfterIllustration;
     _showDismissBarButton = YES;
     _dismissBarButtonSystemItem = UIBarButtonSystemItemDone;
+    _capitalizeTitle = YES;
   }
   return self;
 }
@@ -484,7 +485,11 @@ constexpr CGFloat kSafeAreaMultiplier = 0.8;
       [UIFontMetrics metricsForTextStyle:self.titleTextStyle];
   title.font = [fontMetrics scaledFontForFont:font];
   title.textColor = [UIColor colorNamed:kTextPrimaryColor];
-  title.text = self.titleString.capitalizedString;
+  if (self.capitalizeTitle) {
+    title.text = self.titleString.capitalizedString;
+  } else {
+    title.text = self.titleString;
+  }
   title.textAlignment = NSTextAlignmentCenter;
   title.translatesAutoresizingMaskIntoConstraints = NO;
   title.adjustsFontForContentSizeCategory = YES;
