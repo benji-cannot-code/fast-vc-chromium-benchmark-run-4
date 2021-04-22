0a74ecef0f96c07c8fd2a57bae3c1ff8ec165a7e
Benjamin Beaudry <bebeaudr@microsoft.com>
2021-04-22 19:04:24 +0000

    Reland "[Keyboard Tooltip] Add tooltip browser tests"
    This is a reland of 85222714e7101632840b44a232ce3e35a82d46cb
    
    The new browser test we added was failing on Win7. On older Windows
    version, tooltips are displayed with TooltipWin instead of TooltipAura.
    For TooltipAura, a tooltip is displayed using a Widget and a Label and,
    for TooltipWin, it is displayed using a native win32 control. Since the
    observer we use in this class is the AnyWidgetObserver, we don't receive
    any update from non-Widget tooltips. This doesn't mean that no tooltip
    is displayed on older platforms, but that we are unable to execute the
    browser test successfully because the tooltip displayed is not
    displayed using a Widget.
    
    Changes were made to tooltip_browsertest.cc, where I added the function
    SkipTestForOldWinVersion and called it at the beginning of the test.
    
    Original change's description:
    > [Keyboard Tooltip] Add tooltip browser tests
    >
    > In this CL, we add the infrastructure needed to have tooltip browser
    > tests: a tooltip_browsertest.cc, the TooltipStateObserver and the
    > TooltipStateMonitor.
    >
    > Also, you'll find the first browser test to validate that the tooltip
    > shows when triggered using the cursor. In future CLs, we'll add more
    > browser tests similar to this one.
    >
    > Bug: 829352
    > Change-Id: Ifb8a95a6fd26cf2c40b421de3a2a61fa503ef457
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2829226
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    > Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#874735}
    
    Bug: 829352
    Change-Id: I39bebf51dd1059a2cd8b9a49f24b46bc69cd0db9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2843905
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#875294}

--- ui/views/corewm/tooltip_aura.cc
+++ ui/views/corewm/tooltip_aura.cc
@@ -159,6 +159,9 @@ END_METADATA
 namespace views {
 namespace corewm {
 
+// static
+const char TooltipAura::kWidgetName[] = "TooltipAura";
+
 TooltipAura::~TooltipAura() {
   DestroyWidget();
   CHECK(!IsInObserverList());
@@ -247,6 +250,7 @@ void TooltipAura::CreateTooltipWidget(const gfx::Rect& bounds) {
   // Use software compositing to avoid using unnecessary hardware resources
   // which just amount to overkill for this UI.
   params.force_software_compositing = true;
+  params.name = kWidgetName;
   widget_->Init(std::move(params));
 }
 
