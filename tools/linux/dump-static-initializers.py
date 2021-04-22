317b843fbfa21d1774b2f47f81dd94cc20007615
Andrew Grieve <agrieve@chromium.org>
2021-04-22 16:14:46 +0000

    Convert dump-static-initializers.py to python3
    Bug: 1201000
    Change-Id: Ibc36d32f1c08b41eb4e2fca1470a7f63c08844d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842425
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875189}

--- tools/linux/dump-static-initializers.py
+++ tools/linux/dump-static-initializers.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -24,6 +24,7 @@ using objdump, we can disassemble those functions and dump all symbols that
 they reference.
 """
 
+# Needed so pylint does not complain about print('', end='').
 from __future__ import print_function
 
 import optparse
@@ -48,11 +49,13 @@ class Demangler(object):
   def __init__(self, toolchain):
     self.cppfilt = subprocess.Popen([toolchain + 'c++filt'],
                                     stdin=subprocess.PIPE,
-                                    stdout=subprocess.PIPE)
+                                    stdout=subprocess.PIPE,
+                                    universal_newlines=True)
 
   def Demangle(self, sym):
     """Given mangled symbol |sym|, return its demangled form."""
     self.cppfilt.stdin.write(sym + '\n')
+    self.cppfilt.stdin.flush()
     return self.cppfilt.stdout.readline().strip()
 
 
@@ -68,9 +71,10 @@ def QualifyFilenameAsProto(filename):
   if not match:
     return filename
   basename = match.groups(0)
-  gitlsfiles = subprocess.Popen(
-    ['git', 'ls-files', '--', '*/%s.proto' % basename],
-    stdout=subprocess.PIPE)
+  cmd = ['git', 'ls-files', '--', '*/%s.proto' % basename]
+  gitlsfiles = subprocess.Popen(cmd,
+                                stdout=subprocess.PIPE,
+                                universal_newlines=True)
   candidate = filename
   for line in gitlsfiles.stdout:
     if candidate != filename:
@@ -96,9 +100,10 @@ def QualifyFilename(filename, symbol):
   if not match:
     return filename
   symbol = match.group(1)
-  gitgrep = subprocess.Popen(
-    ['git', 'grep', '-l', symbol, '--', '*/%s' % filename],
-    stdout=subprocess.PIPE)
+  cmd = ['git', 'grep', '-l', symbol, '--', '*/' + filename]
+  gitgrep = subprocess.Popen(cmd,
+                             stdout=subprocess.PIPE,
+                             universal_newlines=True)
   candidate = filename
   for line in gitgrep.stdout:
     if candidate != filename:  # More than one candidate; return bare filename.
@@ -162,7 +167,8 @@ def ParseNm(toolchain, binary):
   Given a binary, yield static initializers as (file, start, size, symbol)
   tuples."""
   nm = subprocess.Popen([toolchain + 'nm', '-S', binary],
-                        stdout=subprocess.PIPE)
+                        stdout=subprocess.PIPE,
+                        universal_newlines=True)
   for line in nm.stdout:
     parse = ParseNmLine(line)
     if parse:
@@ -177,14 +183,16 @@ def ExtractSymbolReferences(toolchain, binary, start, end, symbol):
   """Given a span of addresses, returns symbol references from disassembly."""
   cmd = [toolchain + 'objdump', binary, '--disassemble',
          '--start-address=0x%x' % start, '--stop-address=0x%x' % end]
-  objdump = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+  objdump = subprocess.Popen(cmd,
+                             stdout=subprocess.PIPE,
+                             universal_newlines=True)
 
   refs = set()
   for line in objdump.stdout:
     if '__static_initialization_and_destruction' in line:
-      raise RuntimeError, ('code mentions '
-                           '__static_initialization_and_destruction; '
-                           'did you accidentally run this on a Debug binary?')
+      raise RuntimeError('code mentions '
+                         '__static_initialization_and_destruction; '
+                         'did you accidentally run this on a Debug binary?')
     match = disassembly_re.search(line)
     if match:
       (ref,) = match.groups()
