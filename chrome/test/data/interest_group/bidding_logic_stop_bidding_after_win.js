feb311a4cfed5a56975c41c6c126fd3d2d53e557
Russ Hamilton <behamilton@google.com>
2021-04-23 17:28:01 +0000

    Only enable InterestGroupAPI when the privacy sandbox is enabled
    Bug: 1186444
    Change-Id: I5e8da737f9539f7be29d0dc987f1b478c2f5e8b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2812223
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875725}

---
+++ chrome/test/data/interest_group/bidding_logic_stop_bidding_after_win.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function generateBid(
+  interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals,
+  browserSignals) {
+const bid = browserSignals.prevWins.length < 1 ? 2 : 0;
+return {
+  'ad': 'example',
+  'bid': bid,
+  'render': 'https://stop_bidding_after_win.com/render'
+};
+}
+
+function reportWin(
+  auctionSignals, perBuyerSignals, sellerSignals, browserSignals) {
+sendReportTo(
+    browserSignals.interestGroupOwner +
+    '/echoall?report_bidder_stop_bidding_after_win');
+}
