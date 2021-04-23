15e1ad312e06a846fe8d418d8bb75579899f963b
Jérémie Boulic <jboulic@chromium.org>
2021-04-23 04:31:39 +0000

    [Files app] Add tests for moving files to shared drive locations
    Add to the set of test ENTRIES a shared directory within Drive:
    "sharedDirectory", and the nested "sharedDirectoryFile" within it.
    
    The current "sharedDirectory" has the SHARED_WITH_ME Drive attribute,
    indicating that another user shared this directory with me. Rename
    this entry "sharedWithMeDirectory".
    
    Test: browser_tests --gtest_filter=Transfer*
    Bug: 731583
    Change-Id: I87c8d650c4310d2698eb2d4115277e5bd3925860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842390
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875517}

--- ui/file_manager/integration_tests/test_util.js
+++ ui/file_manager/integration_tests/test_util.js
@@ -695,6 +695,28 @@ const ENTRIES = {
     pinned: true
   }),
 
+  sharedDirectory: new TestEntryInfo({
+    type: EntryType.DIRECTORY,
+    targetPath: 'Shared',
+    sharedOption: SharedOption.SHARED,
+    lastModifiedTime: 'Jan 1, 2000, 1:00 AM',
+    nameText: 'Shared',
+    sizeText: '--',
+    typeText: 'Folder'
+  }),
+
+  sharedDirectoryFile: new TestEntryInfo({
+    type: EntryType.FILE,
+    sourceFileName: 'text.txt',
+    targetPath: 'Shared/file.txt',
+    mimeType: 'text/plain',
+    sharedOption: SharedOption.SHARED,
+    lastModifiedTime: 'Jan 1, 2000, 1:00 AM',
+    nameText: 'file.txt',
+    sizeText: '51 bytes',
+    typeText: 'Plain text'
+  }),
+
   newlyAdded: new TestEntryInfo({
     type: EntryType.FILE,
     sourceFileName: 'music.ogg',
@@ -1347,7 +1369,7 @@ const ENTRIES = {
     typeText: 'Plain text'
   }),
 
-  sharedDirectory: new TestEntryInfo({
+  sharedWithMeDirectory: new TestEntryInfo({
     type: EntryType.DIRECTORY,
     targetPath: 'Shared Directory',
     sharedOption: SharedOption.SHARED_WITH_ME,
@@ -1357,7 +1379,7 @@ const ENTRIES = {
     typeText: 'Folder'
   }),
 
-  sharedDirectoryFile: new TestEntryInfo({
+  sharedWithMeDirectoryFile: new TestEntryInfo({
     type: EntryType.FILE,
     sourceFileName: 'text.txt',
     targetPath: 'Shared Directory/file.txt',
