413486263d47343585e9299945f787577692e641
Kuo Jen Wei <inker@google.com>
2021-04-23 10:00:49 +0000

    media_recorder: Pass specified profile/level to open h264
    Bug: b/182238408
    Test: tast -verbose run <scarlet> "camera.CCAUIVideoOptionPerf"
    Test: ./out/Default/blink_unittests "--gtest_filter=*H264Encoder*"
    
    Change-Id: I60b0cc0ab014fd2d6d076d5dbcb7074241fea09c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2753216
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Markus Handell <handellm@google.com>
    Cr-Commit-Position: refs/heads/master@{#875592}

--- third_party/blink/renderer/modules/mediarecorder/h264_encoder.h
+++ third_party/blink/renderer/modules/mediarecorder/h264_encoder.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // Class encapsulating all openh264 interactions for H264 encoding.
-class H264Encoder final : public VideoTrackRecorder::Encoder {
+class MODULES_EXPORT H264Encoder final : public VideoTrackRecorder::Encoder {
  public:
   struct ISVCEncoderDeleter {
     void operator()(ISVCEncoder* codec);
@@ -31,16 +31,24 @@ class H264Encoder final : public VideoTrackRecorder::Encoder {
                               ScopedISVCEncoderPtr encoder);
 
   H264Encoder(const VideoTrackRecorder::OnEncodedVideoCB& on_encoded_video_cb,
+              VideoTrackRecorder::CodecProfile codec_profile,
               int32_t bits_per_second,
               scoped_refptr<base::SequencedTaskRunner> task_runner);
 
  private:
+  friend class H264EncoderFixture;
+
   // VideoTrackRecorder::Encoder implementation.
   ~H264Encoder() override;
   void EncodeOnEncodingTaskRunner(scoped_refptr<media::VideoFrame> frame,
                                   base::TimeTicks capture_timestamp) override;
 
   void ConfigureEncoderOnEncodingTaskRunner(const gfx::Size& size);
+
+  SEncParamExt GetEncoderOptionForTesting();
+
+  // TODO(inker): Move this field into VideoTrackRecorder::Encoder.
+  const VideoTrackRecorder::CodecProfile codec_profile_;
 
   // |openh264_encoder_| is a special scoped pointer to guarantee proper
   // destruction, also when reconfiguring due to parameters change. Only used on
