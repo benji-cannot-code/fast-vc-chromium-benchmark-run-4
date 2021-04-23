5dfffa97db1a8163f8560506035672d2ade9f59c
Charlie Hu <chenleihu@google.com>
2021-04-23 22:34:27 +0000

    [UseCounter] Add reserved feature check in mojom traits
    There are some reserved UseCounterFeatures that should NOT be passed
    through mojom interface.
    
    This CL adds a check in mojom traits to detect them.
    
    Bug: 1194678
    Change-Id: I49706dd16ce08105c47c2c4aa7ed578f37355cb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846201
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875888}

--- third_party/blink/common/use_counter/use_counter_feature_mojom_traits.cc
+++ third_party/blink/common/use_counter/use_counter_feature_mojom_traits.cc
@@ -4,13 +4,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/common/use_counter/use_counter_feature_mojom_traits.h"
+
+#include "third_party/blink/public/mojom/use_counter/css_property_id.mojom-shared.h"
+#include "third_party/blink/public/mojom/web_feature/web_feature.mojom-shared.h"
+
 namespace mojo {
+namespace {
+// There are reserved features that should NOT be passed through mojom
+// interface. Returns true if the feature is reserved.
+bool IsReservedFeature(const blink::UseCounterFeature& feature) {
+  switch (feature.type()) {
+    case blink::mojom::UseCounterFeatureType::kWebFeature:
+      return feature.value() ==
+             static_cast<blink::UseCounterFeature::EnumValue>(
+                 blink::mojom::WebFeature::kPageVisits);
+    case blink::mojom::UseCounterFeatureType::kCssProperty:
+    case blink::mojom::UseCounterFeatureType::kAnimatedCssProperty:
+      return feature.value() ==
+             static_cast<blink::UseCounterFeature::EnumValue>(
+                 blink::mojom::CSSSampleId::kTotalPagesMeasured);
+  }
+}
+}  // namespace
 
 bool StructTraits<
     blink::mojom::UseCounterFeatureDataView,
     blink::UseCounterFeature>::Read(blink::mojom::UseCounterFeatureDataView in,
                                     blink::UseCounterFeature* out) {
-  return out->SetTypeAndValue(in.type(), in.value());
+  return out->SetTypeAndValue(in.type(), in.value()) &&
+         !IsReservedFeature(*out);
 }
 
 }  // namespace mojo
