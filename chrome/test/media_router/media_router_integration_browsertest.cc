61481ac42aaff77734f2a574dea0516daa97e1e0
Clifford Cheng <cliffordcheng@chromium.org>
2021-04-23 18:49:46 +0000

    Remove component extension related code from browser tests.
    Component extension no longer needed for running the tests.
    All the code that are used to install/uninstall, load/unload, interact with background page are all removed.
    media_router_base_browsertest.* is merged into media_router_integration_browsertest.* because it doesn't do much after removing all the extension dependency.
    
    Bug: 1197771, b/185001899
    Change-Id: I60e37d18ac67961eb5ca48ee3075fd407a28f5a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2819027
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: Clifford Cheng <cliffordcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875778}

--- chrome/test/media_router/media_router_integration_browsertest.cc
+++ chrome/test/media_router/media_router_integration_browsertest.cc
@@ -92,14 +92,23 @@ MediaRouterIntegrationBrowserTest::MediaRouterIntegrationBrowserTest() =
 MediaRouterIntegrationBrowserTest::~MediaRouterIntegrationBrowserTest() =
     default;
 
+Browser* MediaRouterIntegrationBrowserTest::browser() {
+  return InProcessBrowserTest::browser();
+}
+
+void MediaRouterIntegrationBrowserTest::SetUp() {
+  ParseCommandLine();
+  InProcessBrowserTest::SetUp();
+}
+
 void MediaRouterIntegrationBrowserTest::TearDownOnMainThread() {
   test_ui_->TearDown();
-  MediaRouterBaseBrowserTest::TearDownOnMainThread();
+  InProcessBrowserTest::TearDownOnMainThread();
   test_navigation_observer_.reset();
 }
 
 void MediaRouterIntegrationBrowserTest::SetUpInProcessBrowserTestFixture() {
-  MediaRouterBaseBrowserTest::SetUpInProcessBrowserTestFixture();
+  InProcessBrowserTest::SetUpInProcessBrowserTestFixture();
   ON_CALL(provider_, IsInitializationComplete(testing::_))
       .WillByDefault(testing::Return(true));
   ON_CALL(provider_, IsFirstPolicyLoadComplete(testing::_))
@@ -123,6 +132,31 @@ void MediaRouterIntegrationBrowserTest::SetUpOnMainThread() {
 
   test_ui_ =
       MediaRouterUiForTest::GetOrCreateForWebContents(GetActiveWebContents());
+}
+
+bool MediaRouterIntegrationBrowserTest::ConditionalWait(
+    base::TimeDelta timeout,
+    base::TimeDelta interval,
+    const base::RepeatingCallback<bool(void)>& callback) {
+  base::ElapsedTimer timer;
+  do {
+    if (callback.Run())
+      return true;
+
+    base::RunLoop run_loop;
+    base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+        FROM_HERE, run_loop.QuitClosure(), interval);
+    run_loop.Run();
+  } while (timer.Elapsed() < timeout);
+
+  return false;
+}
+
+void MediaRouterIntegrationBrowserTest::Wait(base::TimeDelta timeout) {
+  base::RunLoop run_loop;
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, run_loop.QuitClosure(), timeout);
+  run_loop.Run();
 }
 
 void MediaRouterIntegrationBrowserTest::ExecuteJavaScriptAPI(
@@ -307,7 +341,6 @@ bool MediaRouterIntegrationBrowserTest::IsRouteClosedOnUI() {
 }
 
 void MediaRouterIntegrationBrowserTest::ParseCommandLine() {
-  MediaRouterBaseBrowserTest::ParseCommandLine();
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
 
   receiver_ = command_line->GetSwitchValueASCII(kReceiver);
@@ -594,18 +627,6 @@ IN_PROC_BROWSER_TEST_F(MediaRouterIntegrationBrowserTest,
                        Fail_StartCancelledNoSupportedSinks) {
   test_provider_->set_unsupported_media_sources_list();
   StartSessionAndAssertNotFoundError();
-}
-
-void MediaRouterIntegrationIncognitoBrowserTest::InstallAndEnableMRExtension() {
-  const extensions::Extension* extension =
-      LoadExtension(extension_unpacked_, {.allow_in_incognito = true});
-  incognito_extension_id_ = extension->id();
-}
-
-void MediaRouterIntegrationIncognitoBrowserTest::UninstallMRExtension() {
-  if (!incognito_extension_id_.empty()) {
-    UninstallExtension(incognito_extension_id_);
-  }
 }
 
 Browser* MediaRouterIntegrationIncognitoBrowserTest::browser() {
