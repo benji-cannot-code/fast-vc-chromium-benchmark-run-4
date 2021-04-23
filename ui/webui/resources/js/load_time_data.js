64136dea5a4d49d720b3ffd8486122f88c4fb079
Christian Dullweber <dullweber@chromium.org>
2021-04-23 07:28:12 +0000

    Revert "WebUI: Preparation step for migrating chrome://welcome to TypeScript."
    This reverts commit 4159f3b91fd8401010db5061f37fddfef4f9f821.
    
    Reason for revert: Broke the build: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-ozone-rel/150093/blamelist
    
    Original change's description:
    > WebUI: Preparation step for migrating chrome://welcome to TypeScript.
    >
    > Checking-in or auto-generating .d.ts definition files for all
    > ui/webui/resources/ dependencies of chrome://welcome.
    >
    > Note: This is still part of experimentation to uncover remaining tasks,
    > and not a holistic effort to convert WebUI pages to TS.
    >
    > Bug: 1189595
    > Change-Id: I87c869a10e57a078612dd04ce439fcd2391a42ba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846869
    > Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    > Commit-Queue: dpapad <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#875555}
    
    Bug: 1189595
    Change-Id: Idf9eddbb3b0a4d20406883fc120b0e12a164fe04
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2847341
    Owners-Override: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#875557}

--- ui/webui/resources/js/load_time_data.js
+++ ui/webui/resources/js/load_time_data.js
@@ -17,9 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // #import {assert} from './assert.m.js';
 
-/** @type {!LoadTimeData} */
 // eslint-disable-next-line no-var
-/* #export */ var loadTimeData;
+/* #export */ /** @type {!LoadTimeData} */ var loadTimeData;
 
 class LoadTimeData {
   constructor() {
