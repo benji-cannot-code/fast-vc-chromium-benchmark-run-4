9e881f331d26402416e58041c7a35acafc201337
Mitsuru Oshima <oshima@chromium.org>
2021-04-23 00:19:07 +0000

    Add unit test for LockStateController::LockWithoutAnimation
    Bug: 1197829
    Change-Id: Ic6953cb3c8c244295979a7322966f7f0f6adaf5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846163
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875442}

--- ash/system/power/power_button_test_base.h
+++ ash/system/power/power_button_test_base.h
@@ -25,6 +25,8 @@ enum class LoginStatus;
 class PowerButtonTestBase : public AshTestBase {
  public:
   PowerButtonTestBase();
+  explicit PowerButtonTestBase(base::test::TaskEnvironment::TimeSource time);
+
   ~PowerButtonTestBase() override;
 
   using ButtonType = PowerButtonController::ButtonType;
