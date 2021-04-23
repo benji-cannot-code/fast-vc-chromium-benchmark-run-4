f41c2df59de85d6012f7dffbec157d43285c2229
Jan Kopanski <jkopanski@google.com>
2021-04-23 17:22:27 +0000

    Remove wrong information about user activity
    Remove messages "Who has used the device recently and when" or
    "Who has used the device recently" from transparency panel for
    users that are not managed by the same organization as a device.
    
    Bug: 1060100
    Change-Id: Icc89a2b5eb0127c4c919ed70e314ffa3735cba81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2840525
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Jan Kopański <jkopanski@google.com>
    Cr-Commit-Position: refs/heads/master@{#875721}

--- chrome/browser/chromeos/policy/status_collector/device_status_collector_browsertest.cc
+++ chrome/browser/chromeos/policy/status_collector/device_status_collector_browsertest.cc
@@ -1405,6 +1405,9 @@ TEST_F(DeviceStatusCollectorTest, ActivityNoUser) {
   scoped_testing_cros_settings_.device_settings()->SetBoolean(
       chromeos::kReportDeviceUsers, true);
 
+  EXPECT_FALSE(status_collector_->ShouldReportActivityTimes());
+  EXPECT_FALSE(status_collector_->ShouldReportUsers());
+
   status_collector_->Simulate(test_states, 3);
   GetStatus();
   EXPECT_EQ(1, device_status_.active_periods_size());
@@ -1421,6 +1424,9 @@ TEST_F(DeviceStatusCollectorTest, ActivityWithPublicSessionUser) {
   const AccountId public_account_id(
       AccountId::FromUserEmail("public@localhost"));
   user_manager_->CreatePublicAccountUser(public_account_id);
+
+  EXPECT_FALSE(status_collector_->ShouldReportActivityTimes());
+  EXPECT_FALSE(status_collector_->ShouldReportUsers());
 
   status_collector_->Simulate(test_states, 3);
   GetStatus();
@@ -1439,6 +1445,9 @@ TEST_F(DeviceStatusCollectorTest, ActivityWithAffiliatedUser) {
   user_manager_->AddUserWithAffiliationAndType(account_id0, true,
                                                user_manager::USER_TYPE_REGULAR);
 
+  EXPECT_TRUE(status_collector_->ShouldReportActivityTimes());
+  EXPECT_TRUE(status_collector_->ShouldReportUsers());
+
   status_collector_->Simulate(test_states, 3);
   GetStatus();
   EXPECT_EQ(1, device_status_.active_periods_size());
@@ -1448,6 +1457,9 @@ TEST_F(DeviceStatusCollectorTest, ActivityWithAffiliatedUser) {
 
   scoped_testing_cros_settings_.device_settings()->SetBoolean(
       chromeos::kReportDeviceUsers, false);
+
+  EXPECT_TRUE(status_collector_->ShouldReportActivityTimes());
+  EXPECT_FALSE(status_collector_->ShouldReportUsers());
 
   status_collector_->Simulate(test_states, 3);
   GetStatus();
@@ -1466,6 +1478,9 @@ TEST_F(DeviceStatusCollectorTest, ActivityWithNotAffiliatedUser) {
   user_manager_->AddUserWithAffiliationAndType(account_id0, false,
                                                user_manager::USER_TYPE_REGULAR);
 
+  EXPECT_FALSE(status_collector_->ShouldReportActivityTimes());
+  EXPECT_FALSE(status_collector_->ShouldReportUsers());
+
   status_collector_->Simulate(test_states, 3);
   GetStatus();
   EXPECT_EQ(1, device_status_.active_periods_size());
@@ -1474,6 +1489,9 @@ TEST_F(DeviceStatusCollectorTest, ActivityWithNotAffiliatedUser) {
 
   scoped_testing_cros_settings_.device_settings()->SetBoolean(
       chromeos::kReportDeviceUsers, false);
+
+  EXPECT_FALSE(status_collector_->ShouldReportActivityTimes());
+  EXPECT_FALSE(status_collector_->ShouldReportUsers());
 
   status_collector_->Simulate(test_states, 3);
   GetStatus();
