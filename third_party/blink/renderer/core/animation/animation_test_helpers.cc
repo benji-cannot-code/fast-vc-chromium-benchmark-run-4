d08191eb33eebb34ed12cf9cca89c956a2b2dc67
Jochen Eisinger <jochen@chromium.org>
2021-04-23 01:11:47 +0000

    Add microtask scopes to tests
    Without a scope, microtasks are never executed, so making that explicit.
    
    Bug: 728583
    Change-Id: I7c4922c3b2641fe91e10978dea7a76d34debe5d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846517
    Auto-Submit: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875458}

--- third_party/blink/renderer/core/animation/animation_test_helpers.cc
+++ third_party/blink/renderer/core/animation/animation_test_helpers.cc
@@ -26,6 +26,8 @@ void SetV8ObjectPropertyAsString(v8::Isolate* isolate,
                                  v8::Local<v8::Object> object,
                                  const StringView& name,
                                  const StringView& value) {
+  v8::MicrotasksScope microtasks_scope(
+      isolate, v8::MicrotasksScope::kDoNotRunMicrotasks);
   object
       ->Set(isolate->GetCurrentContext(), V8String(isolate, name),
             V8String(isolate, value))
@@ -36,6 +38,8 @@ void SetV8ObjectPropertyAsNumber(v8::Isolate* isolate,
                                  v8::Local<v8::Object> object,
                                  const StringView& name,
                                  double value) {
+  v8::MicrotasksScope microtasks_scope(
+      isolate, v8::MicrotasksScope::kDoNotRunMicrotasks);
   object
       ->Set(isolate->GetCurrentContext(), V8String(isolate, name),
             v8::Number::New(isolate, value))
