d3455b59014e401495f70706509e55eda8125a8d
Daniel Clark <daniec@microsoft.com>
2021-04-23 02:40:17 +0000

    Add JSON module test for load and error events
    Add a test validating that the load event fires when appropriate for
    a <script> element containing a JSON module import. Also check that
    the error event is fired when a JSON module 404s.
    
    These aren't intended to replicate the full suite of corresponding
    tests for JavaScript modules. The purpose is to serve as a check that
    implementers are reusing the codepaths for these events when
    implementing JSON modules.
    
    Bug: 1132413
    Change-Id: I4277411c1f1f758c9ea163f87c2a539eacabe082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2845094
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875488}

---
+++ third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/json-module/load-error-events.py
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import re
+
+def main(request, response):
+    headers = [(b"Content-Type", b"text/javascript")]
+    test = request.GET.first(b'test')
+    assert(re.match(b'^[a-zA-Z0-9_]+$', test))
+
+    status = 200
+    if test.find(b'_load') >= 0:
+      content = b'import "./module.json" assert { type: "json"}; %s.executed = true;' % test
+    else:
+      content = b'import "./not_found.json" assert { type: "json"}; %s.test.step(function() { assert_unreached("404 script should not be executed"); });' % test
+
+    return status, headers, content
