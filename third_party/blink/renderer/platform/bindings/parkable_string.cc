0683e3afef44aa9ab1e51aae1579bb212c0dc01e
Benoit Lize <lizeb@chromium.org>
2021-04-23 11:57:41 +0000

    [blink/bindings] Report OOM crashes in ParkableString::Unpark() as such.
    Bug: 1199704
    Change-Id: I7f028820ced096fe5b9d54faed7d5acb0844a01a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2843469
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875617}

--- third_party/blink/renderer/platform/bindings/parkable_string.cc
+++ third_party/blink/renderer/platform/bindings/parkable_string.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // https://chromium.googlesource.com/chromium/src/+/HEAD/docs/wmax_tokens.md
 #pragma clang max_tokens_here 750000
 
+#include "base/allocator/partition_allocator/oom.h"
 #include "base/allocator/partition_allocator/partition_alloc.h"
 #include "base/bind.h"
 #include "base/check_op.h"
@@ -584,10 +585,13 @@ String ParkableStringImpl::UnparkInternal() {
   //
   // (1) is data corruption, and (2) is OOM. In all cases, we cannot
   // recover the string we need, nothing else to do than to abort.
-  //
-  // Stability sheriffs: If you see this, this is likely an OOM.
-  CHECK(compression::GzipUncompress(compressed_string_piece,
-                                    uncompressed_string_piece));
+  if (!compression::GzipUncompress(compressed_string_piece,
+                                   uncompressed_string_piece)) {
+    // Since this is almost always OOM, report it as such. We don't have
+    // certainty, but memory corruption should be much rarer, and could make us
+    // crash anywhere else.
+    OOM_CRASH(uncompressed_string_piece.size());
+  }
 
   base::TimeDelta elapsed = timer.Elapsed();
   manager.RecordUnparkingTime(elapsed);
