c47be88056c5d0da97136a5cfbb336329690c3de
Yi Gu <yigu@chromium.org>
2021-04-23 19:33:25 +0000

    [Sheriff] Disable testContextMenuEntriesBeforeFirstRun on Android M
    Bug: 1180180
    Change-Id: I453848d0dbf8f0635ccc0996804d3f8203ce0e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2848485
    Owners-Override: Yi Gu <yigu@chromium.org>
    Auto-Submit: Yi Gu <yigu@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875802}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -35,6 +35,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.provider.Browser;
@@ -81,6 +82,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.CriteriaNotSatisfiedException;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
@@ -509,7 +511,10 @@ public class CustomTabActivityTest {
 
     @Test
     @SmallTest
-    public void testContextMenuEntriesBeforeFirstRun() throws Exception {
+    @DisableIf.Build(message = "Fails on Marshmallow, https://crbug.com/1180180",
+            sdk_is_greater_than = VERSION_CODES.LOLLIPOP, sdk_is_less_than = VERSION_CODES.N)
+    public void
+    testContextMenuEntriesBeforeFirstRun() throws Exception {
         mCustomTabActivityTestRule.startCustomTabActivityWithIntent(createMinimalCustomTabIntent());
         // Mark the first run as not completed. This has to be done after we start the intent,
         // otherwise we are going to hit the FRE.
