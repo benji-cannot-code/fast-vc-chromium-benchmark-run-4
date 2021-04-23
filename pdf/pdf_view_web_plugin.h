c1cca6036b9a02c328a3d6f6c148a008c26018a6
Hui Yingst <nigi@chromium.org>
2021-04-23 21:38:30 +0000

    [unseasoned-pdf] Add a wrapper for blink::WebPluginContainer.
    Create the abstraction PdfViewWebPlugin::ContainerWrapper as the plugin
    container that interacts with PdfViewWebPlugin. It has a derived class
    BlinkContainerWrapper, which is a wrapper around
    blink::WebPluginContainer.
    
    Creating abstraction PdfViewWebPlugin::ContainerWrapper will allow us
    to create a fake container class for testing PdfViewWebPlugin's methods
    in the future, without creating a real blink::WebPluginContainer.
    
    Bug: 1199558
    Change-Id: Ie8f8b59d32b396517d196d3a9a9eb75612c1a468
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830790
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875860}

--- pdf/pdf_view_web_plugin.h
+++ pdf/pdf_view_web_plugin.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PDF_PDF_VIEW_WEB_PLUGIN_H_
 #define PDF_PDF_VIEW_WEB_PLUGIN_H_
 
+#include <memory>
+
 #include "base/memory/weak_ptr.h"
 #include "cc/paint/paint_image.h"
 #include "pdf/pdf_view_plugin_base.h"
@@ -30,6 +32,22 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
                                public PostMessageReceiver::Client,
                                public SkiaGraphics::Client {
  public:
+  class ContainerWrapper {
+   public:
+    virtual ~ContainerWrapper() = default;
+
+    // Invalidates the entire web plugin container and schedules a paint of the
+    // page in it.
+    virtual void Invalidate() = 0;
+
+    // Returns the device scale factor.
+    virtual float DeviceScaleFactor() const = 0;
+
+    // Returns the blink web plugin container pointer that's wrapped inside this
+    // object. Returns nullptr if this object is for test only.
+    virtual blink::WebPluginContainer* Container() = 0;
+  };
+
   explicit PdfViewWebPlugin(const blink::WebPluginParams& params);
   PdfViewWebPlugin(const PdfViewWebPlugin& other) = delete;
   PdfViewWebPlugin& operator=(const PdfViewWebPlugin& other) = delete;
@@ -128,6 +146,8 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
   // Call `Destroy()` instead.
   ~PdfViewWebPlugin() override;
 
+  bool InitializeCommon(std::unique_ptr<ContainerWrapper> container_wrapper);
+
   void OnViewportChanged(const gfx::Rect& view_rect, float new_device_scale);
 
   // Invalidates the entire web plugin container and schedules a paint of the
@@ -135,7 +155,8 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
   void InvalidatePluginContainer();
 
   blink::WebPluginParams initial_params_;
-  blink::WebPluginContainer* container_ = nullptr;
+
+  std::unique_ptr<ContainerWrapper> container_wrapper_;
 
   v8::Persistent<v8::Object> scriptable_receiver_;
   PostMessageSender post_message_sender_;
