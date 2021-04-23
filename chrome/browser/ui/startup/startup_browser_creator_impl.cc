c75f5dba355291231d71d73834653be22a6e516c
Anastasiia N <anastasiian@chromium.org>
2021-04-23 10:41:57 +0000

    Reland "Don't show welcome page on Lacros"
    This is a reland of eb602ba7b66195b3442cab29509d1b7b98e818ad
    
    The original CL was reverted because it was depended on
    https://crrev.com/c/2817786 which was reverted. That CL was
    relanded: https://crrev.com/c/2835020.
    
    No changes from the original CL.
    
    Original change's description:
    > Don't show welcome page on Lacros
    >
    > Disable welcome screen when Account Manager is available on Lacros.
    > This will apply only to main Lacros profile, since it already has
    > the primary account set. For Guest / additional regular Lacros profiles
    > the behavior is unchanged.
    >
    > (IsAccountManagerAvailable() already checks for kUseAccountManagerFacade
    > flag, IsMainProfile(), IsGuestSession(), IsOffTheRecord() etc.)
    >
    > Bug: 1117486
    > Change-Id: I8d5e74cd314c448cf4cc06115f6c5c8f6427c5ed
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2807612
    > Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    > Reviewed-by: Kush Sinha <sinhak@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#872551}
    
    Bug: 1117486
    Change-Id: If7f973ba4c1ba7a55a7ba20ff19e3bee41256234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2843471
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875598}

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chrome/browser/lacros/account_manager_util.h"
 #include "chrome/browser/lacros/lacros_prefs.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/lacros/lacros_service.h"
@@ -377,6 +378,11 @@ void StartupBrowserCreatorImpl::DetermineURLsAndLaunch(
   welcome_enabled =
       welcome::IsEnabled(profile_) && welcome::HasModulesToShow(profile_);
 #endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  if (IsAccountManagerAvailable(profile_))
+    welcome_enabled = false;
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 
   bool serve_extensions_page =
       extensions::ShouldShowExtensionsCheckupOnStartup(profile_);
