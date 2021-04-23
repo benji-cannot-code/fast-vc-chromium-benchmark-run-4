718a8cc3f98c883fb2f0d3040da593ae3da89ccd
Hans Wennborg <hans@chromium.org>
2021-04-23 18:35:45 +0000

    [webaudio] Don't rely on SkFloatingPoint.h to include xmmintrin.h / arm_neon.h
    Bug: 242216
    Change-Id: I2611997dc90d469454cb3a51b12aab0df212cd1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846972
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875766}

--- third_party/blink/renderer/modules/webaudio/periodic_wave.cc
+++ third_party/blink/renderer/modules/webaudio/periodic_wave.cc
@@ -40,7 +40,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/exception_messages.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 
-#if defined(CPU_ARM_NEON)
+#if defined(ARCH_CPU_X86_FAMILY)
+#include <xmmintrin.h>
+#elif defined(CPU_ARM_NEON)
 #include <arm_neon.h>
 #endif
 
--- third_party/blink/renderer/modules/webaudio/wave_shaper_dsp_kernel.cc
+++ third_party/blink/renderer/modules/webaudio/wave_shaper_dsp_kernel.cc
@@ -34,6 +34,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/audio/vector_math.h"
 #include "third_party/blink/renderer/platform/wtf/threading.h"
 
+#if defined(ARCH_CPU_X86_FAMILY)
+#include <xmmintrin.h>
+#elif defined(CPU_ARM_NEON)
+#include <arm_neon.h>
+#endif
+
 namespace blink {
 
 WaveShaperDSPKernel::WaveShaperDSPKernel(WaveShaperProcessor* processor)
