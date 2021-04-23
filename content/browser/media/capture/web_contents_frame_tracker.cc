ac211991e82ece0c128bc5962b35ea13edb6c21a
Jordan Bayles <jophba@chromium.org>
2021-04-23 18:44:07 +0000

    [Tab Capture] Honor capture size aspect ratio
    This patch changes the WCVCD preferred size calculation to honor the
    aspect ratio of the capture size. If the requested capture size is
    larger than the view/screen size, it is scaled to fit within the bounds
    of the screen but with the same resolution.
    
    Bug: 1194803
    Change-Id: If74320f382643748983bcff9371b351922238e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2844952
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875773}

--- content/browser/media/capture/web_contents_frame_tracker.cc
+++ content/browser/media/capture/web_contents_frame_tracker.cc
@@ -36,7 +36,7 @@ namespace content {
 
 namespace {
 
-// Note on lifetime: this context should be deleted when the web contents
+// Note on lifetime: this context should be deleted when the WebContents
 // is destroyed.
 class WebContentsContext : public WebContentsFrameTracker::Context {
  public:
@@ -82,7 +82,7 @@ class WebContentsContext : public WebContentsFrameTracker::Context {
 
   base::ScopedClosureRunner capture_handle_;
 
-  // The backing web contents.
+  // The backing WebContents.
   WebContents* contents_;
 };
 
@@ -139,6 +139,11 @@ void WebContentsFrameTracker::DidStopCapturingWebContents() {
 // the first capturer's preferred size is set.
 gfx::Size WebContentsFrameTracker::CalculatePreferredSize(
     const gfx::Size& capture_size) {
+  if (capture_size.IsEmpty()) {
+    // NOTE: An empty preferred size will cause the WebContents to keep its
+    // previous size preference.
+    return {};
+  }
   gfx::Size preferred_size = capture_size;
 
   // If we know the available size of the screen, we don't want to exceed
@@ -146,7 +151,24 @@ gfx::Size WebContentsFrameTracker::CalculatePreferredSize(
   if (context_) {
     const base::Optional<gfx::Rect> screen_bounds = context_->GetScreenBounds();
     if (screen_bounds) {
-      preferred_size.SetToMin(screen_bounds->size());
+      if (screen_bounds->size().IsEmpty()) {
+        return {};
+      }
+
+      // We want to honor the aspect ratio of the capture size request while
+      // also limiting it to the screen bounds of the view.
+      // For motivation, see https://crbug.com/1194803.
+      const double x_ratio = static_cast<double>(capture_size.width()) /
+                             static_cast<double>(screen_bounds->size().width());
+      const double y_ratio =
+          static_cast<double>(capture_size.height()) /
+          static_cast<double>(screen_bounds->size().height());
+
+      const double scale_ratio = std::max(x_ratio, y_ratio);
+      if (scale_ratio > 1.0) {
+        preferred_size = gfx::ScaleToFlooredSize(
+            preferred_size, static_cast<float>(1 / scale_ratio));
+      }
     }
   }
   return preferred_size;
