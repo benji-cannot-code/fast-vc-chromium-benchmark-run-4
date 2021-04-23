bf0a110695f5b21f6d4bc2b2c731e7fbf54324a8
David Bokan <bokan@chromium.org>
2021-04-23 15:34:06 +0000

    Rename content_initiated to synchronously_committed
    |is_content_initiated| is misleading because this parameter is used to
    determine whether the browser's navigation code is aware of the
    navigation: if |is_content_initiated == true| then the browser is not
    involved.
    
    However, |is_content_initiated| can be easily mistaken to mean "was
    initiated by script or page content" which it does *not* represent. For
    example, when one document initiates a navigation in a cross-origin
    document, the navigation will be processed through the browser's
    navigation mechanisms and |is_content_initiated == false|. This led to
    the linked bug.
    
    This CL renames the parameter in all its incarnations to
    |is_synchronously_committed| to more precisely describe its value. That
    is, it is true if a navigation is committed from within Blink rather
    than the browser's navigation mechanism.
    
    See also: https://groups.google.com/a/chromium.org/g/navigation-dev/c/QkqYP0drmwA/m/a74MWY3IAAAJ
    
    Bug: 1198670
    Change-Id: I62e6fbe34af9045a8785c2845c49dde79db4ca44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2837370
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875681}

--- third_party/blink/renderer/core/frame/web_frame_test.cc
+++ third_party/blink/renderer/core/frame/web_frame_test.cc
@@ -7682,7 +7682,7 @@ class TestDidNavigateCommitTypeWebFrameClient
 
   // frame_test_helpers::TestWebFrameClient:
   void DidFinishSameDocumentNavigation(WebHistoryCommitType type,
-                                       bool content_initiated,
+                                       bool is_synchronously_committed,
                                        bool is_history_api_navigation,
                                        bool is_client_redirect) override {
     last_commit_type_ = type;
@@ -7709,7 +7709,7 @@ TEST_F(WebFrameTest, SameDocumentHistoryNavigationCommitType) {
       item->Url(), WebFrameLoadType::kBackForward, item.Get(),
       ClientRedirectPolicy::kNotClientRedirect,
       false /* has_transient_user_activation */, /*initiator_origin=*/nullptr,
-      /*is_content_initiated=*/false,
+      /*is_synchronously_committed=*/false,
       mojom::blink::TriggeringEventInfo::kNotFromEvent,
       nullptr /* extra_data */);
   EXPECT_EQ(kWebBackForwardCommit, client.LastCommitType());
@@ -13306,7 +13306,8 @@ TEST_F(WebFrameTest, RecordSameDocumentNavigationToHistogram) {
       ToKURL("about:blank"), kSameDocumentNavigationHistoryApi, message,
       mojom::blink::ScrollRestorationType::kAuto,
       WebFrameLoadType::kReplaceCurrentItem,
-      frame->DomWindow()->GetSecurityOrigin(), /*is_content_initiated=*/true);
+      frame->DomWindow()->GetSecurityOrigin(),
+      /*is_synchronously_committed=*/true);
   // The bucket index corresponds to the definition of
   // |SinglePageAppNavigationType|.
   tester.ExpectBucketCount(histogramName,
@@ -13315,14 +13316,15 @@ TEST_F(WebFrameTest, RecordSameDocumentNavigationToHistogram) {
       ToKURL("about:blank"), kSameDocumentNavigationDefault, message,
       mojom::blink::ScrollRestorationType::kManual,
       WebFrameLoadType::kBackForward, frame->DomWindow()->GetSecurityOrigin(),
-      /*is_content_initiated=*/true);
+      /*is_synchronously_committed=*/true);
   tester.ExpectBucketCount(histogramName,
                            kSPANavTypeSameDocumentBackwardOrForward, 1);
   document_loader.UpdateForSameDocumentNavigation(
       ToKURL("about:blank"), kSameDocumentNavigationDefault, message,
       mojom::blink::ScrollRestorationType::kManual,
       WebFrameLoadType::kReplaceCurrentItem,
-      frame->DomWindow()->GetSecurityOrigin(), /*is_content_initiated=*/true);
+      frame->DomWindow()->GetSecurityOrigin(),
+      /*is_synchronously_committed=*/true);
   tester.ExpectBucketCount(histogramName, kSPANavTypeOtherFragmentNavigation,
                            1);
   // kSameDocumentNavigationHistoryApi and WebFrameLoadType::kBackForward is an
--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -700,7 +700,7 @@ void DocumentLoader::RunURLAndHistoryUpdateSteps(
   UpdateForSameDocumentNavigation(
       new_url, kSameDocumentNavigationHistoryApi, std::move(data),
       scroll_restoration_type, type, frame_->DomWindow()->GetSecurityOrigin(),
-      /*is_content_initiated=*/true);
+      /*is_synchronously_committed=*/true);
 }
 
 void DocumentLoader::UpdateForSameDocumentNavigation(
@@ -710,7 +710,7 @@ void DocumentLoader::UpdateForSameDocumentNavigation(
     mojom::blink::ScrollRestorationType scroll_restoration_type,
     WebFrameLoadType type,
     const SecurityOrigin* initiator_origin,
-    bool is_content_initiated) {
+    bool is_synchronously_committed) {
   SinglePageAppNavigationType single_page_app_navigation_type =
       CategorizeSinglePageAppNavigation(same_document_navigation_source, type);
   UMA_HISTOGRAM_ENUMERATION(
@@ -755,7 +755,7 @@ void DocumentLoader::UpdateForSameDocumentNavigation(
   if (type == WebFrameLoadType::kStandard ||
       same_document_navigation_source == kSameDocumentNavigationDefault) {
     bool is_browser_initiated = !initiator_origin;
-    DCHECK(!(is_browser_initiated && is_content_initiated));
+    DCHECK(!(is_browser_initiated && is_synchronously_committed));
     has_text_fragment_token_ =
         TextFragmentAnchor::GenerateNewTokenForSameDocument(
             new_url.FragmentIdentifier(), type, is_browser_initiated,
@@ -782,7 +782,7 @@ void DocumentLoader::UpdateForSameDocumentNavigation(
       FrameScheduler::NavigationType::kSameDocument);
 
   GetLocalFrameClient().DidFinishSameDocumentNavigation(
-      history_item_.Get(), commit_type, is_content_initiated,
+      history_item_.Get(), commit_type, is_synchronously_committed,
       is_history_api_navigation, is_client_redirect_);
   probe::DidNavigateWithinDocument(frame_);
   if (!was_loading) {
@@ -1217,7 +1217,7 @@ mojom::CommitResult DocumentLoader::CommitSameDocumentNavigation(
     ClientRedirectPolicy client_redirect_policy,
     bool has_transient_user_activation,
     const SecurityOrigin* initiator_origin,
-    bool is_content_initiated,
+    bool is_synchronously_committed,
     mojom::blink::TriggeringEventInfo triggering_event_info,
     std::unique_ptr<WebDocumentLoader::ExtraData> extra_data) {
   DCHECK(!IsReloadLoadType(frame_load_type));
@@ -1265,10 +1265,10 @@ mojom::CommitResult DocumentLoader::CommitSameDocumentNavigation(
 
   // If the requesting document is cross-origin, perform the navigation
   // asynchronously to minimize the navigator's ability to execute timing
-  // attacks. If |is_content_initiated| is false, the navigation is already
-  // asynchronous since it's coming from another agent so there's no need to
-  // post it again.
-  if (is_content_initiated && initiator_origin &&
+  // attacks. If |is_synchronously_committed| is false, the navigation is
+  // already asynchronous since it's coming from the browser so there's no need
+  // to post it again.
+  if (is_synchronously_committed && initiator_origin &&
       !initiator_origin->CanAccess(frame_->DomWindow()->GetSecurityOrigin())) {
     frame_->GetTaskRunner(TaskType::kInternalLoading)
         ->PostTask(
@@ -1277,13 +1277,15 @@ mojom::CommitResult DocumentLoader::CommitSameDocumentNavigation(
                       WrapWeakPersistent(this), url, frame_load_type,
                       WrapPersistent(history_item), client_redirect_policy,
                       has_transient_user_activation,
-                      WTF::RetainedRef(initiator_origin), is_content_initiated,
-                      triggering_event_info, std::move(extra_data)));
+                      WTF::RetainedRef(initiator_origin),
+                      is_synchronously_committed, triggering_event_info,
+                      std::move(extra_data)));
   } else {
     CommitSameDocumentNavigationInternal(
         url, frame_load_type, history_item, client_redirect_policy,
-        has_transient_user_activation, initiator_origin, is_content_initiated,
-        triggering_event_info, std::move(extra_data));
+        has_transient_user_activation, initiator_origin,
+        is_synchronously_committed, triggering_event_info,
+        std::move(extra_data));
   }
   return mojom::CommitResult::Ok;
 }
@@ -1295,7 +1297,7 @@ void DocumentLoader::CommitSameDocumentNavigationInternal(
     ClientRedirectPolicy client_redirect,
     bool has_transient_user_activation,
     const SecurityOrigin* initiator_origin,
-    bool is_content_initiated,
+    bool is_synchronously_committed,
     mojom::blink::TriggeringEventInfo triggering_event_info,
     std::unique_ptr<WebDocumentLoader::ExtraData> extra_data) {
   // If this function was scheduled to run asynchronously, this DocumentLoader
@@ -1350,7 +1352,7 @@ void DocumentLoader::CommitSameDocumentNavigationInternal(
   UpdateForSameDocumentNavigation(url, kSameDocumentNavigationDefault, nullptr,
                                   mojom::blink::ScrollRestorationType::kAuto,
                                   frame_load_type, initiator_origin,
-                                  is_content_initiated);
+                                  is_synchronously_committed);
 
   initial_scroll_state_.was_scrolled_by_user = false;
 
