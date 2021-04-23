d2f10494cd0f587661eb48f88ceb0cc04be50445
Sammie Quon <sammiequon@chromium.org>
2021-04-23 17:55:41 +0000

    full_restore: Restore minimized chrome apps.
    When restoring apps, the views::Widget show state param gets set as
    SHOW_STATE_MINIMIZED. Then, in the Widget::Show() call, the widget gets
    minimized.
    
    For chrome apps, launching them results in a ShowInactive() followed
    by a Show(). The second Show() does not do anything if the window is
    already visible, skipping the minimize in Widget::Show().
    
    The ShowInactive() was added in crrev.com/c/936142 to handle an edge
    case with ChromeOS splitview. I tried the steps for the bug linked in
    that patch and just having Show() after Fullscreen()/Maximize()/Minimize()
    in AppWindow will pass that edge case, and pass existing unit tests.
    
    Fixed: 1200534
    Test: manual, added test
    Change-Id: Id7e61cf98dd620f8e8a8f7182b62f437a0a5d2ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2838285
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875737}

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash_browsertest.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash_browsertest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.h"
 
+#include "ash/public/cpp/split_view_test_api.h"
+#include "ash/public/cpp/tablet_mode.h"
 #include "ash/public/cpp/test/shell_test_api.h"
 #include "base/macros.h"
 #include "base/run_loop.h"
@@ -389,4 +391,32 @@ IN_PROC_BROWSER_TEST_F(ChromeNativeAppWindowViewsAuraAshBrowserTest,
               app_window->GetNativeWindow()->GetBoundsInScreen());
   }
   CloseAppWindow(app_window);
+}
+
+// Tests that opening a chrome app window when a window is already snapped will
+// snap it as well, even if the window is meant to be created maximized.
+IN_PROC_BROWSER_TEST_F(ChromeNativeAppWindowViewsAuraAshBrowserTest,
+                       OpeningDefaultMaximizedWindowInSplitview) {
+  ash::TabletMode::Get()->SetEnabledForTest(true);
+
+  const extensions::Extension* extension =
+      LoadAndLaunchPlatformApp("launch", "Launched");
+
+  extensions::AppWindow::CreateParams params;
+  extensions::AppWindow* app1_window =
+      CreateAppWindowFromParams(browser()->profile(), extension, params);
+
+  ash::SplitViewTestApi split_view_test_api;
+  split_view_test_api.SnapWindow(app1_window->GetNativeWindow(),
+                                 ash::SplitViewTestApi::SnapPosition::LEFT);
+  ASSERT_EQ(app1_window->GetNativeWindow(),
+            split_view_test_api.GetLeftWindow());
+
+  // Open a second app window that should be created maximized. It should be
+  // snapped.
+  params.state = ui::SHOW_STATE_MAXIMIZED;
+  extensions::AppWindow* app2_window =
+      CreateAppWindowFromParams(browser()->profile(), extension, params);
+  ASSERT_EQ(app2_window->GetNativeWindow(),
+            split_view_test_api.GetRightWindow());
 }
