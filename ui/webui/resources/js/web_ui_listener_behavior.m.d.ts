2eacb1970ba4a60f282a306b6a43b477ed7c865a
dpapad <dpapad@chromium.org>
2021-04-23 07:17:37 +0000

    WebUI: Preparation step for migrating chrome://welcome to TypeScript.
    Checking-in or auto-generating .d.ts definition files for all
    ui/webui/resources/ dependencies of chrome://welcome.
    
    Note: This is still part of experimentation to uncover remaining tasks,
    and not a holistic effort to convert WebUI pages to TS.
    
    Bug: 1189595
    Change-Id: I87c869a10e57a078612dd04ce439fcd2391a42ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846869
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875555}

--- ui/webui/resources/js/web_ui_listener_behavior.m.d.ts
+++ ui/webui/resources/js/web_ui_listener_behavior.m.d.ts
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export {CrContainerShadowBehavior};
+export {WebUIListenerBehavior};
 
-interface CrContainerShadowBehavior {
-  enableShadowBehavior(enable: boolean): void;
-  showDropShadows(): void;
+interface WebUIListenerBehavior {
+  addWebUIListener(eventName: string, callback: Function): void;
 }
 
-declare const CrContainerShadowBehavior: object;
+declare const WebUIListenerBehavior: object;
