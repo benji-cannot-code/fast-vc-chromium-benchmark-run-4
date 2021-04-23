fcd37c838f92b4ea9e0cb5efbcc05e3c7f296f08
Istiaque Ahmed <lazyboy@chromium.org>
2021-04-23 17:33:32 +0000

    [Extensions] Add chrome.test.loadScript to load scripts from test.
    The method returns a promise that is resolved once the script
    is loaded. Note that service workers use importScripts to
    achieve this, whereas background page would use a <script> element
    to load the script.
    
    This CL also updates WebNavigationApiTest.GetFrame to use the
    method.
    
    Bug: None
    Change-Id: I1718e5b10dc7c1f3032a6df3146275dfa3df72df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2845615
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875728}

--- chrome/test/data/extensions/api_test/webnavigation/getFrame/test_getFrame.js
+++ chrome/test/data/extensions/api_test/webnavigation/getFrame/test_getFrame.js
@@ -6,24 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const inServiceWorker = 'ServiceWorkerGlobalScope' in self;
 const scriptUrl = '_test_resources/api_test/webnavigation/framework.js';
 let ready;
+let onScriptLoad = chrome.test.loadScript(scriptUrl);
 
-// Note: Importing scripts is different depending on if this script is
-// executing in a Service Worker context.
 if (inServiceWorker) {
-  importScripts(scriptUrl);
-  ready = Promise.resolve();
+  ready = onScriptLoad;
 } else {
-  let script = document.createElement('script');
-  let onScriptLoad = new Promise((resolve) => {
-    script.onload = resolve;
-  });
-  script.src = scriptUrl;
-  document.body.appendChild(script);
-
   let onWindowLoad = new Promise((resolve) => {
     window.onload = resolve;
   });
-
   ready = Promise.all([onWindowLoad, onScriptLoad]);
 }
 
