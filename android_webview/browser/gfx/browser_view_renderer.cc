7f52f6e3e363b776a904612f2b682a72649e64f6
Bo Liu <boliu@chromium.org>
2021-04-23 15:19:26 +0000

    Avoid copy in webview software draw
    Creating an SkImage from a mutable SkBitmap does an allocation and copy.
    This is not needed here in most cases, since drawing into a software
    canvas is synchronous. Exception is capturePicture, which uses a
    recording canvas.
    
    Switch to using SkPixmap/SkImage directly instead of SkBitmap, and add
    a CHECK that in the software canvas case, the canvas releases the pixmap
    immediately.
    
    Bug: 1197054
    Change-Id: Ia1034a28846885a8daa3cde2360d0f4870e26d0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2843951
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875673}

--- android_webview/browser/gfx/browser_view_renderer.cc
+++ android_webview/browser/gfx/browser_view_renderer.cc
@@ -415,7 +415,7 @@ void BrowserViewRenderer::ReturnUsedResources(
 
 bool BrowserViewRenderer::OnDrawSoftware(SkCanvas* canvas) {
   did_invalidate_since_last_draw_ = false;
-  return CanOnDraw() && CompositeSW(canvas);
+  return CanOnDraw() && CompositeSW(canvas, /*software_canvas=*/true);
 }
 
 bool BrowserViewRenderer::NeedToDrawBackgroundColor() {
@@ -443,7 +443,7 @@ sk_sp<SkPicture> BrowserViewRenderer::CapturePicture(int width,
                                                    gfx::Vector2dF());
       compositor_->DidChangeRootLayerScrollOffset(
           gfx::ScrollOffset(scroll_offset_unscaled_));
-      CompositeSW(rec_canvas);
+      CompositeSW(rec_canvas, /*software_canvas=*/false);
     }
     compositor_->DidChangeRootLayerScrollOffset(
         gfx::ScrollOffset(scroll_offset_unscaled_));
@@ -915,9 +915,9 @@ void BrowserViewRenderer::PostInvalidate(
   client_->PostInvalidate();
 }
 
-bool BrowserViewRenderer::CompositeSW(SkCanvas* canvas) {
+bool BrowserViewRenderer::CompositeSW(SkCanvas* canvas, bool software_canvas) {
   DCHECK(compositor_);
-  return compositor_->DemandDrawSw(canvas);
+  return compositor_->DemandDrawSw(canvas, software_canvas);
 }
 
 std::string BrowserViewRenderer::ToString() const {
