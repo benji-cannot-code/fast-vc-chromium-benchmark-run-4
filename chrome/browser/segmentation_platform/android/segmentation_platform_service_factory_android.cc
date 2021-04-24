4c8b10e6d0713471cdcf2069da36402c9e291713
Shakti Sahu <shaktisahu@chromium.org>
2021-04-24 15:55:09 +0000

    Segmentation Platform : Added factory for native and java
    This CL adds factories for SegmentationPlatformService in native and java.
    The factories were added to the chrome/browser target due to dependency
    on other classes and factories which aren't modularized yet, e.g.
    ProfileAndroid, and optimization guide factory. The Java API keys off a
    profile which is converted to keyed service in native to retrieve the
    service.
    
    Bug: 1200656
    Change-Id: If59b7ec810979c133fdf8c6dab5aea2bb8d145b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2838949
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#875997}

---
+++ chrome/browser/segmentation_platform/android/segmentation_platform_service_factory_android.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/android/scoped_java_ref.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_android.h"
+#include "chrome/browser/segmentation_platform/jni_headers/SegmentationPlatformServiceFactory_jni.h"
+#include "chrome/browser/segmentation_platform/segmentation_platform_service_factory.h"
+
+static base::android::ScopedJavaLocalRef<jobject>
+JNI_SegmentationPlatformServiceFactory_GetForProfile(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jobject>& jprofile) {
+  Profile* profile = ProfileAndroid::FromProfileAndroid(jprofile);
+  DCHECK(profile);
+  // TODO(shaktisahu): Use native factory to get the object and its java
+  // counterpart from user data.
+  return base::android::ScopedJavaLocalRef<jobject>();
+}
