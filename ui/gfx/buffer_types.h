6673f5656da0d87d5caf7ac029b4a6070c9bb4a2
Stephan Hartmann <stha09@googlemail.com>
2021-04-25 18:26:51 +0000

    IWYU: add stdint.h for uint32_t
    Bug: None
    Change-Id: If42685fa7ae6b719fc76fa8ff4a8f25a0e210823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2844864
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876026}

--- ui/gfx/buffer_types.h
+++ ui/gfx/buffer_types.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_BUFFER_TYPES_H_
 #define UI_GFX_BUFFER_TYPES_H_
 
+#include <stdint.h>
+
 #include <tuple>
 
 namespace gfx {
