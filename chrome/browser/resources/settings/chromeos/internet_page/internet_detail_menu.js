99d22a8d87b7959d773fb1379dde6918d9dd8b53
Kyle Horimoto <khorimoto@chromium.org>
2021-04-26 16:39:53 +0000

    [CrOS Cellular] Close rename/remove menu when an item is clicked
    Before this CL, the rename/remove menu would remain open even after it
    was clicked, causing minor UI jank. This CL fixes it and adds tests.
    
    Additionally, this CL fixes misspellings of "triple" in the test suite.
    
    Fixed: 1200493
    Change-Id: I74b49372663003d53bcc88900649ff3488daa70c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2850255
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876175}

--- chrome/browser/resources/settings/chromeos/internet_page/internet_detail_menu.js
+++ chrome/browser/resources/settings/chromeos/internet_page/internet_detail_menu.js
@@ -107,6 +107,7 @@ Polymer({
    * @private
    */
   onRenameESimProfileTap_(e) {
+    this.closeMenu_();
     this.fire(
         'show-esim-profile-rename-dialog', {networkState: this.networkState_});
   },
@@ -116,7 +117,15 @@ Polymer({
    * @private
    */
   onRemoveESimProfileTap_(e) {
+    this.closeMenu_();
     this.fire(
         'show-esim-remove-profile-dialog', {networkState: this.networkState_});
-  }
+  },
+
+  /** @private */
+  closeMenu_() {
+    const actionMenu =
+        /** @type {!CrActionMenuElement} */ (this.$$('cr-action-menu'));
+    actionMenu.close();
+  },
 });
