f4bede96438ebe8f5c4a993fb987c72d22e8e341
Canon Mukai <canonmukai@google.com>
2021-04-26 05:22:34 +0000

    validator: Add a rule to validate extended attributes
    This CL adds a rule to validate extended attributes which is
    applicable to types.
    https://heycam.github.io/webidl/#extended-attributes-applicable-to-types
    Accordingly, it also adds some TargetTypes.
    
    Bug: 1198370
    Change-Id: I9940be76e135c81befdc66f2aab9344e90cd53f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846680
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Canon Mukai <canonmukai@google.com>
    Cr-Commit-Position: refs/heads/master@{#876053}

--- third_party/blink/renderer/bindings/scripts/validator/rules/__init__.py
+++ third_party/blink/renderer/bindings/scripts/validator/rules/__init__.py
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from . import rules_function_like
+from . import rules_extended_attributes
 
 
 def register_all_rules(rule_store):
     rules_function_like.register_rules(rule_store)
+    rules_extended_attributes.register_rules(rule_store)
