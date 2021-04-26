a2b937c15f3ff6f411752fadd2ce2ab173df2afe
Peter Boström <pbos@chromium.org>
2021-04-26 22:37:42 +0000

    Ensure focus-ring contrast for ToolbarButtons
    This is inspired by theming code in TabIcon, so if the default color is
    insufficiently contrasty against the toolbar, we fall back on using the
    ToolbarButton icon color which should already be contrasty enough. This
    also looks really nice.
    
    Bug: 1068929
    Change-Id: Idaa623d63f20654e98fcad58c117d41a8dc3ce2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2851381
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876331}

--- chrome/browser/ui/views/toolbar/toolbar_button.cc
+++ chrome/browser/ui/views/toolbar/toolbar_button.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/feature_list.h"
 #include "base/location.h"
+#include "base/optional.h"
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
@@ -188,7 +189,36 @@ ToolbarButton::ToolbarButton(PressedCallback callback,
   SetFocusBehavior(FocusBehavior::ACCESSIBLE_ONLY);
 }
 
-ToolbarButton::~ToolbarButton() {}
+ToolbarButton::~ToolbarButton() = default;
+
+void ToolbarButton::UpdateFocusRingColor(views::View* host,
+                                         views::FocusRing* focus_ring) {
+  DCHECK(host->GetWidget());
+  DCHECK_EQ(host, focus_ring->parent());
+  const SkColor default_focus_ring_color =
+      host->GetNativeTheme()->GetSystemColor(
+          ui::NativeTheme::kColorId_FocusedBorderColor);
+  const SkColor background =
+      host->GetThemeProvider()->GetColor(ThemeProperties::COLOR_TOOLBAR);
+  const float default_contrast =
+      color_utils::GetContrastRatio(default_focus_ring_color, background);
+  if (default_contrast > color_utils::kMinimumVisibleContrastRatio) {
+    focus_ring->SetColor(base::nullopt);
+    return;
+  }
+  const SkColor fallback_focus_ring_color = host->GetThemeProvider()->GetColor(
+      ThemeProperties::COLOR_TOOLBAR_BUTTON_ICON);
+  // TODO(pbos): Should this fallback_contrast_ratio be a DCHECK of >
+  // color_utils::kMinimumVisibleContrastRatio? Hopefully these are already
+  // contrasty.
+  const float fallback_contrast_ratio =
+      color_utils::GetContrastRatio(fallback_focus_ring_color, background);
+  if (fallback_contrast_ratio > default_contrast) {
+    focus_ring->SetColor(fallback_focus_ring_color);
+    return;
+  }
+  focus_ring->SetColor(base::nullopt);
+}
 
 void ToolbarButton::SetHighlight(const std::u16string& highlight_text,
                                  base::Optional<SkColor> highlight_color) {
@@ -426,6 +456,12 @@ void ToolbarButton::OnThemeChanged() {
   if (installable_ink_drop_)
     installable_ink_drop_->SetConfig(GetToolbarInstallableInkDropConfig(this));
   UpdateIcon();
+
+  // TODO(pbos): Remove calls to OnThemeChanged() where there's no widget.
+  // Afaik this is only done in ToolbarButtonViewsTest.NoDefaultLayoutInsets,
+  // but the test setup should instead add the ToolbarButton to a Widget.
+  if (GetWidget())
+    UpdateFocusRingColor(this, focus_ring());
 
   // Call this after UpdateIcon() to properly reset images.
   LabelButton::OnThemeChanged();
@@ -754,7 +790,7 @@ ToolbarButton::HighlightColorAnimation::HighlightColorAnimation(
   highlight_color_animation_.SetSlideDuration(kHighlightAnimationDuration);
 }
 
-ToolbarButton::HighlightColorAnimation::~HighlightColorAnimation() {}
+ToolbarButton::HighlightColorAnimation::~HighlightColorAnimation() = default;
 
 void ToolbarButton::HighlightColorAnimation::Show(
     base::Optional<SkColor> highlight_color) {
