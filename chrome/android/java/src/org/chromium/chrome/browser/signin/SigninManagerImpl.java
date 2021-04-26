efeff419e65db53781cbd4b37875717fa4f26435
Alice Wang <aliceywang@chromium.org>
2021-04-26 09:34:11 +0000

    [Signin][Android] Add integration tests for account rename and removal
    This CL adds three integration tests to check that the sign-in checks
    work as expected when the primary account is renamed or removed on
    device.
    
    Bug: 1152460, 595893
    Change-Id: I34990f9467ed2f24b4838e95580b94505dbca77e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2821442
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876093}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManagerImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManagerImpl.java
@@ -326,9 +326,6 @@ class SigninManagerImpl
     }
 
     /**
-     * @deprecated use {@link #signinAndEnableSync(int, CoreAccountInfo, SignInCallback)} instead.
-     * TODO(crbug.com/1002056): Remove this version after migrating all callers to CoreAccountInfo.
-     *
      * Starts the sign-in flow, and executes the callback when finished.
      *
      * The sign-in flow goes through the following steps:
@@ -344,7 +341,6 @@ class SigninManagerImpl
      * @param callback Optional callback for when the sign-in process is finished.
      */
     @Override
-    @Deprecated
     public void signinAndEnableSync(@SigninAccessPoint int accessPoint, Account account,
             @Nullable SignInCallback callback) {
         mAccountTrackerService.seedAccountsIfNeeded(() -> {
