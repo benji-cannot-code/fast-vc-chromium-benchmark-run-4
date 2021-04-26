c07cea7e02acf7c496e2a7db04f9c97125b716bc
Michael Checo <michaelcheco@google.com>
2021-04-26 17:35:53 +0000

    Diagnostics: Add skeleton network list element
    - Added network-list element
    - Added test suite for network-list
    - Added network-list to main page gated by flag
    
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: Ic4135704577dce3caf3450741d91aae598b9375c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2837510
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876204}

--- chrome/test/data/webui/chromeos/diagnostics/diagnostics_browsertest.js
+++ chrome/test/data/webui/chromeos/diagnostics/diagnostics_browsertest.js
@@ -59,6 +59,7 @@ const debug_suites_list = [
   'FakeSystemDataProvider',
   'FakeSystemRoutineContoller',
   'MemoryCard',
+  'NetworkList',
   'OverviewCard',
   'PercentBarChart',
   'RealtimeCpuChart',
