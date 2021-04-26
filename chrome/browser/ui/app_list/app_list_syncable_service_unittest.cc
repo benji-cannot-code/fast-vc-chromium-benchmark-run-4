b5a378e60020f7a768f156a84b829dd60d76c954
Guido Urdaneta <guidou@chromium.org>
2021-04-26 10:43:03 +0000

    [Sheriff] Disable some AppListSyncableServiceTest tests
    They are flaking significantly on CrOS.
    
    Bug: 1202295
    Change-Id: I433f2a0c732b1236f1dea94c9be565b9805aafa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2850648
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Owners-Override: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876104}

--- chrome/browser/ui/app_list/app_list_syncable_service_unittest.cc
+++ chrome/browser/ui/app_list/app_list_syncable_service_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/test/scoped_command_line.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/app_list/app_list_model_updater.h"
@@ -320,7 +321,13 @@ class AppListSyncableServiceTest : public AppListTestBase {
       model_updater_factory_scope_;
 };
 
-TEST_F(AppListSyncableServiceTest, OEMFolderForConflictingPos) {
+// Flaky on CrOS. See https://crbug.com/1202295
+#if defined(OS_CHROMEOS)
+#define MAYBE_OEMFolderForConflictingPos DISABLED_OEMFolderForConflictingPos
+#else
+#define MAYBE_OEMFolderForConflictingPos OEMFolderForConflictingPos
+#endif
+TEST_F(AppListSyncableServiceTest, MAYBE_OEMFolderForConflictingPos) {
   // Create a "web store" app.
   const std::string web_store_app_id(extensions::kWebStoreAppId);
   scoped_refptr<extensions::Extension> store =
@@ -400,7 +407,14 @@ TEST_F(AppListSyncableServiceTest, OEMItemIgnoreSyncParent) {
 
 // Verifies that an OEM apps parent ID in sync data is not overridden to the OEM
 // folder.
-TEST_F(AppListSyncableServiceTest, OEMAppParentNotOverridenInSync) {
+// Flaky on CrOS. See https://crbug.com/1202295
+#if defined(OS_CHROMEOS)
+#define MAYBE_OEMAppParentNotOverridenInSync \
+  DISABLED_OEMAppParentNotOverridenInSync
+#else
+#define MAYBE_OEMAppParentNotOverridenInSync OEMAppParentNotOverridenInSync
+#endif
+TEST_F(AppListSyncableServiceTest, MAYBE_OEMAppParentNotOverridenInSync) {
   const std::string oem_app_id = CreateNextAppId(extensions::kWebStoreAppId);
   scoped_refptr<extensions::Extension> oem_app = MakeApp(
       kOemAppName, oem_app_id, extensions::Extension::WAS_INSTALLED_BY_OEM);
