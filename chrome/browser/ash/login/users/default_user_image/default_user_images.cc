05ac4990bd676f8596de686f7dba95680e75d972
Xiqi Ruan <xiqiruan@chromium.org>
2021-04-26 22:47:16 +0000

    Metrics: Add metric to record avatar usage
    Add metric UserImage.LoggedIn2 to replace
    UserImage.LoggedIn(expired after M82) and update
    enum ChromeOSUserImageId's value. The old metric
    might have overlapping enum value. For example:
    value 19 was labeled as "Photo taken". It could
    be a user defined image or a default image under
    the path
    ui/chromeos/resources/default_100_percent/default_user_images/avatar_bicycle.png
    
    Bug: 1174912
    Change-Id: Idaf5dedee81fce9ae8753e1178450679382a9513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2793008
    Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876335}

--- chrome/browser/ash/login/users/default_user_image/default_user_images.cc
+++ chrome/browser/ash/login/users/default_user_image/default_user_images.cc
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace default_user_image {
 
-// Resource IDs of default user images.
+// Resource IDs of default user images. When adding new entries to this list,
+// please also update the enum ChromeOSUserImageId in the
+// tools/metrics/histograms/enums.xml.
 // clang-format off
 const int kDefaultImageResourceIDs[] = {
     IDR_LOGIN_DEFAULT_USER,
@@ -130,13 +132,11 @@ const int kLastRandomDefaultImageIndex = 47;
 
 // The order and the values of these constants are important for histograms
 // of different Chrome OS versions to be merged smoothly.
-const int kHistogramImageFromCamera = 19;
-const int kHistogramImageFromFile = 20;
-const int kHistogramImageOld = 21;
-const int kHistogramImageFromProfile = 22;
-// const int kHistogramVideoFromCamera = 23;  // Unused
-// const int kHistogramVideoFromFile = 24;  // Unused
-const int kHistogramImagesCount = kDefaultImagesCount + 6;
+const int kHistogramImageExternal = 19;
+const int kHistogramImageFromProfile = 20;
+const int kHistogramSpecialImagesCount = 2;
+const int kHistogramImagesCount =
+    kDefaultImagesCount + kHistogramSpecialImagesCount;
 
 namespace {
 
@@ -381,15 +381,6 @@ bool IsInCurrentImageSet(int index) {
   int first, last;
   GetFirstLastIndex(&first, &last);
   return index >= first && index <= last;
-}
-
-int GetDefaultImageHistogramValue(int index) {
-  DCHECK(index >= 0 && index < kDefaultImagesCount);
-  // Create a gap in histogram values for
-  // [kHistogramImageFromCamera..kHistogramImageFromProfile] block to fit.
-  if (index < kHistogramImageFromCamera)
-    return index;
-  return index + 6;
 }
 
 std::unique_ptr<base::ListValue> GetAsDictionary(bool all) {
--- chrome/browser/ash/login/users/default_user_image/default_user_images.h
+++ chrome/browser/ash/login/users/default_user_image/default_user_images.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-
 namespace base {
 class ListValue;
 }
@@ -47,17 +46,14 @@ extern const int kFirstDefaultImageIndex;
 
 /// Histogram values. ////////////////////////////////////////////////////////
 
-// Histogram value for user image taken from file.
-extern const int kHistogramImageFromFile;
-
-// Histogram value for user image taken from camera.
-extern const int kHistogramImageFromCamera;
-
-// Histogram value a previously used image from camera/file.
-extern const int kHistogramImageOld;
+// Histogram value for user image selected from file or photo.
+extern const int kHistogramImageExternal;
 
 // Histogram value for user image from G+ profile.
 extern const int kHistogramImageFromProfile;
+
+// Number of special histogram values for user images.
+extern const int kHistogramSpecialImagesCount;
 
 // Number of possible histogram values for user images.
 extern const int kHistogramImagesCount;
