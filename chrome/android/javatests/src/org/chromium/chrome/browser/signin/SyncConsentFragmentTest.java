f883d5272ee0dc25bd66ee983f6038a10851cc7f
Alice Wang <aliceywang@chromium.org>
2021-04-26 09:10:00 +0000

    [Signin][Android] Use ExternalAuthUtils to check GooglePlayServices
    This CL uses ExternalAuthUtils to check GooglePlayServices and shows a
    modal dialog in case GooglePlayServices is not available instead of
    duplicating the same code in SyncConsentFragmentBase.
    
    Bug: 1192105
    Change-Id: I43837be3d0d0e4ffa21250eeb42384b714ab742d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2826463
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876088}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SyncConsentFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SyncConsentFragmentTest.java
@@ -13,6 +13,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import android.os.Bundle;
@@ -62,6 +63,7 @@ import org.chromium.chrome.test.util.ActivityTestUtils;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.chrome.test.util.browser.signin.AccountManagerTestRule;
 import org.chromium.chrome.test.util.browser.sync.SyncTestUtil;
+import org.chromium.components.externalauth.ExternalAuthUtils;
 import org.chromium.components.signin.ChildAccountStatus;
 import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.metrics.SigninAccessPoint;
@@ -119,10 +121,15 @@ public class SyncConsentFragmentTest {
     @Mock
     private FirstRunPageDelegate mFirstRunPageDelegateMock;
 
+    @Mock
+    private ExternalAuthUtils mExternalAuthUtilsMock;
+
     private SyncConsentActivity mSyncConsentActivity;
 
     @Before
     public void setUp() {
+        when(mExternalAuthUtilsMock.canUseGooglePlayServices(any())).thenReturn(true);
+        ExternalAuthUtils.setInstanceForTesting(mExternalAuthUtilsMock);
         mActivityTestRule.setFinishActivity(true);
         mChromeActivityTestRule.startMainActivityOnBlankPage();
     }
