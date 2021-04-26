84dcf17fcac48716b197bc4ade78dc61ecd5c3af
Ken Rockot <rockot@google.com>
2021-04-26 17:45:59 +0000

    Mojo: Fix [NoInterrupt] sync waits
    NoInterrupt sync waits could still be interrupted when called under the
    following conditions:
    
    - the interface has no associated interfaces
    - caller is on the same thread that binds the pipe (no SharedRemote)
    - one or more pending requests were already in flight on the pipe
    
    This corrects the issue by addressing two details:
    
    - Interfaces which define at least one [NoInterrupt] message will now
      behave similarly to those with associated interfaces, in that they
      will use MultiplexRouter's task queueing behavior to support
      deferred dispatch.
    
    - Exclusive sync waits from the MultiplexRouter's own thread now go
      through a separate path which explicitly defers all task processing
      except for the singular reply being waited upon.
    
    One bonus result is that NoInterrupt can also now make stronger
    guarantees about only dispatching the exact reply we're waiting on.
    Documentation and tests are updated accordingly.
    
    Fixed: 1202144
    Change-Id: I76bf00e78dfc0fc004fcdf524ba09aba633df380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2848751
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Ken Rockot <rockot@google.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876210}

--- mojo/public/tools/mojom/mojom/generate/module.py
+++ mojo/public/tools/mojom/mojom/generate/module.py
@@ -1672,6 +1672,13 @@ def HasSyncMethods(interface):
   return False
 
 
+def HasUninterruptableMethods(interface):
+  for method in interface.methods:
+    if not method.allow_interrupt:
+      return True
+  return False
+
+
 def ContainsHandlesOrInterfaces(kind):
   """Check if the kind contains any handles.
 
