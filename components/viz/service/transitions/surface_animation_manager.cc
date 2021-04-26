014279c00c462c5326210cdc0e7c46ae16cffaa0
Vladimir Levin <vmpstr@chromium.org>
2021-04-26 17:39:31 +0000

    SET: Accumulate opacity for shared elements from the source frame.
    This patch accumulates opacity information, assuming SrcOver, for
    both the saved textures and target render passes.
    
    This fixes a test in
    wpt_internal/document-transition/shared-transition-shapes.manual.html
    
    Specifically, the opacity items no longer draw at full opacity when
    in transition.
    
    R=kylechar@chromium.org, vasilyt@chromium.org
    
    Fixed: 1200926
    Change-Id: I3ba440da64e06fd5224b871d40c0b1e541258d25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2845248
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876206}

--- components/viz/service/transitions/surface_animation_manager.cc
+++ components/viz/service/transitions/surface_animation_manager.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/texture_draw_quad.h"
 #include "components/viz/common/resources/returned_resource.h"
 #include "components/viz/common/resources/transferable_resource.h"
+#include "components/viz/common/transition_utils.h"
 #include "components/viz/service/surfaces/surface.h"
 #include "components/viz/service/surfaces/surface_saved_frame_storage.h"
 #include "ui/gfx/animation/keyframe/keyframed_animation_curve.h"
@@ -207,7 +208,7 @@ bool SurfaceAnimationManager::ProcessAnimateDirective(
   saved_textures_.emplace(
       transferable_resource_tracker_.ImportResources(std::move(saved_frame)));
 
-  CreateRootAnimationCurves(saved_textures_->root.rect.size());
+  CreateRootAnimationCurves(saved_textures_->root.draw_data.rect.size());
   CreateSharedElementCurves();
   TickAnimations(latest_time_);
   state_ = State::kAnimating;
@@ -410,7 +411,10 @@ void SurfaceAnimationManager::CopyAndInterpolateSharedElements(
     // anything special with it.
     auto shared_it = shared_draw_data.find(pass_copy->id);
     if (shared_it != shared_draw_data.end()) {
-      shared_it->second.render_pass = std::move(pass_copy);
+      RenderPassDrawData& data = shared_it->second;
+      data.render_pass = std::move(pass_copy);
+      data.opacity = TransitionUtils::ComputeAccumulatedOpacity(
+          source_passes, data.render_pass->id);
     } else {
       interpolated_frame->render_pass_list.emplace_back(std::move(pass_copy));
     }
@@ -477,7 +481,8 @@ void SurfaceAnimationManager::CopyAndInterpolateSharedElements(
 
     if (src_texture.has_value()) {
       bool y_flipped = !src_texture->resource.is_software;
-      CreateAndAppendSrcTextureQuad(animation_pass, rect, transform, opacity,
+      CreateAndAppendSrcTextureQuad(animation_pass, rect, transform,
+                                    opacity * src_texture->draw_data.opacity,
                                     y_flipped, src_texture->resource.id);
       interpolated_frame->resource_list.push_back(src_texture->resource);
     }
@@ -494,9 +499,9 @@ void SurfaceAnimationManager::CopyAndInterpolateSharedElements(
     // Create an quad for the pass into our animation pass.
     // TODO(vmpstr): This needs to be a more sophisticated blending. See
     // crbug.com/1201251 for details.
-    CreateAndAppendSharedRenderPassDrawQuad(animation_pass, rect, transform,
-                                            1.f - opacity, pass_copy->id,
-                                            *draw_data.draw_quad);
+    CreateAndAppendSharedRenderPassDrawQuad(
+        animation_pass, rect, transform, draw_data.opacity * (1.f - opacity),
+        pass_copy->id, *draw_data.draw_quad);
 
     // Finally, add the pass into the interpolated frame. Make sure this comes
     // after CreateAndAppend* call, because we use a pass id, so we need to
@@ -797,7 +802,7 @@ void SurfaceAnimationManager::CreateSharedElementCurves() {
         gfx::CubicBezierTimingFunction::EaseType::EASE);
 
     gfx::TransformOperations transform_ops;
-    transform_ops.AppendMatrix(shared->target_transform);
+    transform_ops.AppendMatrix(shared->draw_data.target_transform);
 
     auto transform_curve = gfx::KeyframedTransformAnimationCurve::Create();
     transform_curve->set_target(&state);
@@ -811,7 +816,7 @@ void SurfaceAnimationManager::CreateSharedElementCurves() {
         gfx::KeyframeEffect::GetNextKeyframeModelId(),
         AnimationState::kSrcTransform));
 
-    const gfx::Rect& rect = shared->rect;
+    const gfx::Rect& rect = shared->draw_data.rect;
 
     auto rect_curve = gfx::KeyframedRectAnimationCurve::Create();
     rect_curve->set_target(&state);
