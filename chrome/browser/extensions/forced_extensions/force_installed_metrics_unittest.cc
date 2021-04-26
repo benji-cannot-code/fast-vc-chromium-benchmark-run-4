9ba3d5437b1824ff3840fdc70b7749d50378f267
Swapnil <swapnilgupta@google.com>
2021-04-26 13:12:28 +0000

    Report MANIFEST_INVALID error separately for CWS and offstore extensions
    Many extensions fail due to MANIFEST_INVALID failure reason which
    could be due to XML parsing failed, invalid gupdate tag etc. This
    CL reports these errors separately for CWS and offstore extensions
    to get a better idea about these failure reasons.
    
    Following histograms are added:
    Extensions.WebStore_ForceInstalledFailureManifestInvalidErrorDetail2
    Extensions.OffStore_ForceInstalledFailureManifestInvalidErrorDetail2
    
    Bug: b/185742793
    Change-Id: I5d5301943bf0dcb9ac526ddbc1ef92a9f7b1e9fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830935
    Reviewed-by: Saurabh Nijhara <snijhara@google.com>
    Reviewed-by: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Swapnil Gupta <swapnilgupta@google.com>
    Cr-Commit-Position: refs/heads/master@{#876123}

--- chrome/browser/extensions/forced_extensions/force_installed_metrics_unittest.cc
+++ chrome/browser/extensions/forced_extensions/force_installed_metrics_unittest.cc
@@ -108,8 +108,10 @@ constexpr char kPossibleNonMisconfigurationFailures[] =
 constexpr char kDisableReason[] =
     "Extensions.ForceInstalledNotLoadedDisableReason";
 constexpr char kBlocklisted[] = "Extensions.ForceInstalledAndBlackListed";
-constexpr char kExtensionManifestInvalid[] =
-    "Extensions.ForceInstalledFailureManifestInvalidErrorDetail2";
+constexpr char kWebStoreExtensionManifestInvalid[] =
+    "Extensions.WebStore_ForceInstalledFailureManifestInvalidErrorDetail2";
+constexpr char kOffStoreExtensionManifestInvalid[] =
+    "Extensions.OffStore_ForceInstalledFailureManifestInvalidErrorDetail2";
 constexpr char kManifestNoUpdatesInfo[] =
     "Extensions.ForceInstalledFailureNoUpdatesInfo";
 constexpr char kExtensionManifestInvalidAppStatusError[] =
@@ -1140,8 +1142,9 @@ TEST_F(ForceInstalledMetricsTest, ExtensionManifestFetchFailedSelfHosted) {
                                       kFetchTries, 2);
 }
 
-// Errors occurred because the fetched update manifest was invalid.
-TEST_F(ForceInstalledMetricsTest, ExtensionManifestInvalid) {
+// Errors occurred because the fetched update manifest for webstore extension
+// was invalid.
+TEST_F(ForceInstalledMetricsTest, ExtensionManifestInvalidWebStore) {
   SetupForceList(/*is_from_store=*/true);
   scoped_refptr<const Extension> ext1 = CreateNewExtension(
       kExtensionName1, kExtensionId1, ExtensionStatus::kLoaded);
@@ -1153,7 +1156,25 @@ TEST_F(ForceInstalledMetricsTest, ExtensionManifestInvalid) {
   // loaded or failed.
   EXPECT_FALSE(fake_timer_->IsRunning());
   histogram_tester_.ExpectUniqueSample(
-      kExtensionManifestInvalid,
+      kWebStoreExtensionManifestInvalid,
+      ManifestInvalidError::INVALID_PROTOCOL_ON_GUPDATE_TAG, 1);
+}
+
+// Errors occurred because the fetched update manifest for offstore extension
+// was invalid.
+TEST_F(ForceInstalledMetricsTest, ExtensionManifestInvalidOffStore) {
+  SetupForceList(/*is_from_store=*/false);
+  scoped_refptr<const Extension> ext1 = CreateNewExtension(
+      kExtensionName1, kExtensionId1, ExtensionStatus::kLoaded);
+  install_stage_tracker()->ReportManifestInvalidFailure(
+      kExtensionId2,
+      ExtensionDownloaderDelegate::FailureData(
+          ManifestInvalidError::INVALID_PROTOCOL_ON_GUPDATE_TAG));
+  // ForceInstalledMetrics shuts down timer because all extension are either
+  // loaded or failed.
+  EXPECT_FALSE(fake_timer_->IsRunning());
+  histogram_tester_.ExpectUniqueSample(
+      kOffStoreExtensionManifestInvalid,
       ManifestInvalidError::INVALID_PROTOCOL_ON_GUPDATE_TAG, 1);
 }
 
@@ -1171,7 +1192,7 @@ TEST_F(ForceInstalledMetricsTest, ExtensionManifestInvalidAppStatusError) {
   // ForceInstalledMetrics shuts down timer because all extension are either
   // loaded or failed.
   EXPECT_FALSE(fake_timer_->IsRunning());
-  histogram_tester_.ExpectUniqueSample(kExtensionManifestInvalid,
+  histogram_tester_.ExpectUniqueSample(kWebStoreExtensionManifestInvalid,
                                        ManifestInvalidError::BAD_APP_STATUS, 1);
   histogram_tester_.ExpectUniqueSample(
       kExtensionManifestInvalidAppStatusError,
