697f1b59d2deaff1bebb5ea47fdc36fbf22fd3ac
Simon Zünd <szuend@chromium.org>
2021-04-26 09:35:11 +0000

    [devtools] Move Issue format conversion to a separate file
    Inspector issues are converted from Mojo to their respective CDP
    values in the audits agent. To prepare for future changes, this CL
    moves that conversion code in a separate file and introduces a
    a new function.
    
    R=sigurds@chromium.org
    
    Bug: chromium:1202622
    Change-Id: I3b400d4580df15f57b4d6c3826c6527cde36a226
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2844868
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876094}

---
+++ third_party/blink/renderer/core/inspector/inspector_issue_conversion.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_INSPECTOR_INSPECTOR_ISSUE_CONVERSION_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_INSPECTOR_INSPECTOR_ISSUE_CONVERSION_H_
+
+#include "third_party/blink/renderer/core/inspector/protocol/Audits.h"
+
+namespace blink {
+
+class InspectorIssue;
+
+std::unique_ptr<protocol::Audits::InspectorIssue>
+ConvertInspectorIssueToProtocolFormat(InspectorIssue*);
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_INSPECTOR_INSPECTOR_ISSUE_CONVERSION_H_
