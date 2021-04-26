22f7b548514240a862130c5718494cdef2440cf7
Alice Wang <aliceywang@chromium.org>
2021-04-26 09:02:58 +0000

    [Signin][Android] Extract account restriction patterns handling in a class
    This CL extracts the account restriction patterns handling in a
    separate class to simplify the logic in AccountManagerFacadeImpl.
    
    Bug: 1057040
    Change-Id: I8ef18b8463c338804ea4848359f7237daad757d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2826464
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876086}

--- components/signin/public/android/junit/src/org/chromium/components/signin/AccountManagerFacadeImplTest.java
+++ components/signin/public/android/junit/src/org/chromium/components/signin/AccountManagerFacadeImplTest.java
@@ -315,8 +315,7 @@ public class AccountManagerFacadeImplTest {
 
     private void setAccountRestrictionPatterns(String... patterns) {
         Bundle restrictions = new Bundle();
-        restrictions.putStringArray(
-                AccountManagerFacadeImpl.ACCOUNT_RESTRICTION_PATTERNS_KEY, patterns);
+        restrictions.putStringArray("RestrictAccountsToPatterns", patterns);
         mShadowUserManager.setApplicationRestrictions(mContext.getPackageName(), restrictions);
         mContext.sendBroadcast(new Intent(Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED));
     }
