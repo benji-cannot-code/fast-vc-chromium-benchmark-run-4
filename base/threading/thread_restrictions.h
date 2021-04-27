5075bc3c559f339db402615fe40e8e4ca2e3e49d
Yuta Hijikata <ythjkt@google.com>
2021-04-27 16:36:38 +0000

    Migrates browser data if lacros is enabled via flags.
    Currently BrowserDataMigrator only runs during the login process for
    existing users. If a user enables lacros via chrome://flags and restarts
    ash-chrome but do not restart the device, lacros becomes available to
    the user without ever going through the migration. This patch ensures
    that browser data is migrated upon restart after lacros is enabled.
    
    http://go/lacros-data-migration
    
    Test: unit_tests, manual test with DUT
    Bug: 1178702
    Change-Id: I43f85d795cb686ddd32230d30f11d518b11767e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830186
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876626}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -117,6 +117,7 @@ class VizCompositorThreadRunnerWebView;
 }
 namespace ash {
 class MojoUtils;
+class BrowserDataMigrator;
 }
 namespace audio {
 class OutputDevice;
@@ -400,6 +401,7 @@ class BASE_EXPORT ScopedAllowBlocking {
   friend class StackSamplingProfiler;
   friend class android_webview::ScopedAllowInitGLBindings;
   friend class ash::MojoUtils;  // http://crbug.com/1055467
+  friend class ash::BrowserDataMigrator;
   friend class blink::DiskDataAllocator;
   friend class content::BrowserProcessSubThread;
   friend class content::NetworkServiceInstancePrivate;
