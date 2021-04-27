72e6582daa49ba736e4a7bed6daa43e710ddf9f6
David Black <dmblack@google.com>
2021-04-27 19:33:20 +0000

    Add `kArcDownload` holding space item type.
    This CL adds and wires up a new holding space item type to holding
    space UI for downloads originating from ARC. New flavors of existing
    histograms are also added.
    
    Note that this CL does *not* introduce the browser-side code that will
    actually create ARC holding space items in response to ARC download
    added events. As such, this CL introduces no user visible changes.
    
    Design doc: http://shortn/_Wau3SBvUpk
    
    Bug: 1134773
    Change-Id: Ifc863dfdc58c5dcd623b814df5eb7eff9b4ed0cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2852275
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#876708}

--- ash/public/cpp/holding_space/holding_space_item.h
+++ ash/public/cpp/holding_space/holding_space_item.h
@@ -37,7 +37,8 @@ class ASH_PUBLIC_EXPORT HoldingSpaceItem {
     kDownload = 2,
     kNearbyShare = 3,
     kScreenRecording = 4,
-    kMaxValue = kScreenRecording,
+    kArcDownload = 5,
+    kMaxValue = kArcDownload,
   };
 
   HoldingSpaceItem(const HoldingSpaceItem&) = delete;
--- ash/system/holding_space/holding_space_test_api.cc
+++ ash/system/holding_space/holding_space_test_api.cc
@@ -7,12 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/drag_drop/drag_drop_controller.h"
 #include "ash/public/cpp/holding_space/holding_space_constants.h"
+#include "ash/public/cpp/holding_space/holding_space_item.h"
 #include "ash/public/cpp/test/shell_test_api.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/system/holding_space/holding_space_item_chip_view.h"
 #include "ash/system/holding_space/holding_space_item_screen_capture_view.h"
+#include "ash/system/holding_space/holding_space_item_view.h"
 #include "ash/system/holding_space/holding_space_tray.h"
 #include "ash/system/status_area_widget.h"
 #include "ui/aura/window.h"
@@ -94,6 +96,22 @@ bool HoldingSpaceTestApi::IsShowing() {
 
 bool HoldingSpaceTestApi::IsShowingInShelf() {
   return holding_space_tray_ && holding_space_tray_->GetVisible();
+}
+
+const std::string& HoldingSpaceTestApi::GetHoldingSpaceItemId(
+    const views::View* item_view) const {
+  return HoldingSpaceItemView::Cast(item_view)->item_id();
+}
+
+views::View* HoldingSpaceTestApi::GetHoldingSpaceItemView(
+    const std::vector<views::View*>& item_views,
+    const std::string& item_id) {
+  auto it = std::find_if(
+      item_views.begin(), item_views.end(), [&](const views::View* item_view) {
+        return !strcmp(HoldingSpaceItemView::Cast(item_view)->item_id().c_str(),
+                       item_id.c_str());
+      });
+  return it != item_views.end() ? *it : nullptr;
 }
 
 views::View* HoldingSpaceTestApi::GetDownloadsSectionHeader() {
