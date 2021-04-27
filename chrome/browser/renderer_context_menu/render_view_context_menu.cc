a00064fbc99911f1574b44753024bccf451a184f
Travis Skare <skare@chromium.org>
2021-04-27 18:16:12 +0000

    [QRCodeGenerator] Allow in incognito for desktop
    Bug: 1180978
    Change-Id: I0838e84bdba3d89437fad6d49b7d1168009aa9a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2851099
    Commit-Queue: Travis Skare <skare@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876674}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -2841,9 +2841,8 @@ bool RenderViewContextMenu::IsQRCodeGeneratorEnabled() const {
   if (!entry)
     return false;
 
-  bool incognito = browser_context_->IsOffTheRecord();
   return qrcode_generator::QRCodeGeneratorBubbleController::
-      IsGeneratorAvailable(entry->GetURL(), incognito);
+      IsGeneratorAvailable(entry->GetURL());
 }
 
 void RenderViewContextMenu::AppendQRCodeGeneratorItem(bool for_image,
--- chrome/browser/ui/qrcode_generator/qrcode_generator_bubble_controller.cc
+++ chrome/browser/ui/qrcode_generator/qrcode_generator_bubble_controller.cc
@@ -21,11 +21,7 @@ QRCodeGeneratorBubbleController::~QRCodeGeneratorBubbleController() {
 }
 
 // static
-bool QRCodeGeneratorBubbleController::IsGeneratorAvailable(const GURL& url,
-                                                           bool in_incognito) {
-  if (in_incognito)
-    return false;
-
+bool QRCodeGeneratorBubbleController::IsGeneratorAvailable(const GURL& url) {
   if (!base::FeatureList::IsEnabled(kSharingQRCodeGenerator))
     return false;
 
