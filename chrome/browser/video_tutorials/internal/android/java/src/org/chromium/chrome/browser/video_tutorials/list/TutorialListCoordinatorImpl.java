483e273759af67e08a6eb7b5aba6aac5b1537b55
Shakti Sahu <shaktisahu@chromium.org>
2021-04-27 15:41:04 +0000

    Video tutorials : Implementation review comments
    This CL fixes:
    1 - Try now IPH alignment with NTP search box
    2 - Spacing between Try Now, Watch Next, and Change language buttons
    3 - Start/end/top padding for the video list page
    
    Attached screenshots on the bug.
    
    Bug: 1203035
    Change-Id: I4f294fb0a1ea68715516488abb338201f4ec066e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2852445
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876605}

--- chrome/browser/video_tutorials/internal/android/java/src/org/chromium/chrome/browser/video_tutorials/list/TutorialListCoordinatorImpl.java
+++ chrome/browser/video_tutorials/internal/android/java/src/org/chromium/chrome/browser/video_tutorials/list/TutorialListCoordinatorImpl.java
@@ -107,8 +107,8 @@ public class TutorialListCoordinatorImpl implements TutorialListCoordinator {
             int screenWidthDp = resources.getConfiguration().screenWidthDp;
             padding = (int) (((screenWidthDp - UiConfig.WIDE_DISPLAY_STYLE_MIN_WIDTH_DP) / 2.f)
                     * resources.getDisplayMetrics().density);
-            padding = (int) Math.max(
-                    resources.getDimensionPixelSize(R.dimen.promo_compact_padding), padding);
+            padding =
+                    (int) Math.max(resources.getDimensionPixelSize(R.dimen.card_padding), padding);
         }
         return padding;
     }
@@ -119,8 +119,7 @@ public class TutorialListCoordinatorImpl implements TutorialListCoordinator {
 
         public ItemDecorationImpl(Resources resources) {
             mVerticalInterCardPaddingPx = resources.getDimensionPixelOffset(R.dimen.card_padding);
-            mHorizontalStartPaddingPx =
-                    resources.getDimensionPixelOffset(R.dimen.promo_compact_padding);
+            mHorizontalStartPaddingPx = resources.getDimensionPixelOffset(R.dimen.card_padding);
         }
 
         @Override
