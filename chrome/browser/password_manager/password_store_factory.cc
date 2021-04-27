a994f139241d2fcd417a9165d7b23585d1da4281
François Doray <fdoray@chromium.org>
2021-04-27 14:52:03 +0000

    Revert "Affiliation Match Helper creation shouldn't depend only on sync changes."
    This reverts commit b9b443ea5fe5cc5f85e143a98a0655a1edca2475.
    
    Reason for revert: crbug.com/1203265
    
    Original change's description:
    > Affiliation Match Helper creation shouldn't depend only on sync changes.
    >
    > In order to enable filling across affiliated websites we should create
    > Affiliation Match Helper instantly together with password store, not
    > only on sync status changes.
    >
    > Bug: 1202704
    > Change-Id: I89d77bc558e81ee4fc842636e40e83d908d653fd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831631
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    > Cr-Commit-Position: refs/heads/master@{#876520}
    
    Bug: 1202704, 1203265
    Change-Id: I3e1c985bb5cd4fee65e7f4e4e24890fb90601266
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854354
    Auto-Submit: François Doray <fdoray@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Owners-Override: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876583}

--- chrome/browser/password_manager/password_store_factory.cc
+++ chrome/browser/password_manager/password_store_factory.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_store_factory_util.h"
 #include "components/password_manager/core/browser/password_store_impl.h"
 #include "components/password_manager/core/browser/password_store_signin_notifier_impl.h"
-#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
@@ -174,18 +173,6 @@ PasswordStoreFactory::BuildServiceInstanceFor(
           IdentityManagerFactory::GetForProfile(profile));
   ps->SetPasswordStoreSigninNotifier(std::move(notifier));
 #endif
-
-  if (base::FeatureList::IsEnabled(
-          password_manager::features::kFillingAcrossAffiliatedWebsites)) {
-    // Try to create affiliation service without awaiting synced state changes.
-    // TODO(http://crbug.com/1202699): Remove sync service completely after
-    // launching HashAffiliationLookup.
-    password_manager::ToggleAffiliationBasedMatchingBasedOnPasswordSyncedState(
-        ps.get(), /*sync_service=*/nullptr,
-        content::BrowserContext::GetDefaultStoragePartition(profile)
-            ->GetURLLoaderFactoryForBrowserProcess(),
-        content::GetNetworkConnectionTracker(), profile->GetPath());
-  }
 
   return ps;
 }
