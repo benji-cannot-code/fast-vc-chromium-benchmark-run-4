4d9f27070cc21ff954cd2cfbd9ecc662dfbaf863
Danila Kuzmin <dkuzmin@google.com>
2021-04-27 16:13:16 +0000

    OOBE histograms: Time to login with Gaia during OOBE
    Bug: 1155630
    Change-Id: Ib903c551b3e07d1a4bceaa7c29b89fbb6d1f6878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2809400
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876621}

--- chrome/browser/ash/login/saml/saml_browsertest.cc
+++ chrome/browser/ash/login/saml/saml_browsertest.cc
@@ -465,6 +465,7 @@ IN_PROC_BROWSER_TEST_F(SamlTest, CredentialPassingAPI) {
 
   histogram_tester.ExpectUniqueSample("ChromeOS.SAML.APILogin", 1, 1);
   histogram_tester.ExpectUniqueSample("ChromeOS.SAML.Provider", 1, 1);
+  histogram_tester.ExpectTotalCount("OOBE.GaiaLoginTime", 0);
 }
 
 // Tests the sign-in flow when the credentials passing API is used w/o 'confirm'
@@ -501,6 +502,7 @@ IN_PROC_BROWSER_TEST_F(SamlTest, CredentialPassingAPIWithoutConfirm) {
 
   histogram_tester.ExpectUniqueSample("ChromeOS.SAML.APILogin", 1, 1);
   histogram_tester.ExpectUniqueSample("ChromeOS.SAML.Provider", 1, 1);
+  histogram_tester.ExpectTotalCount("OOBE.GaiaLoginTime", 0);
 }
 
 // Tests the single password scraped flow.
@@ -541,6 +543,7 @@ IN_PROC_BROWSER_TEST_F(SamlTest, ScrapedSingle) {
   histogram_tester.ExpectUniqueSample("ChromeOS.SAML.APILogin", 2, 1);
   histogram_tester.ExpectUniqueSample("ChromeOS.SAML.Scraping.PasswordCountAll",
                                       1, 1);
+  histogram_tester.ExpectTotalCount("OOBE.GaiaLoginTime", 0);
 }
 
 // Tests password scraping from a dynamically created password field.
@@ -603,6 +606,7 @@ IN_PROC_BROWSER_TEST_F(SamlTest, ScrapedMultiple) {
   histogram_tester.ExpectUniqueSample("ChromeOS.SAML.APILogin", 2, 1);
   histogram_tester.ExpectUniqueSample("ChromeOS.SAML.Scraping.PasswordCountAll",
                                       2, 1);
+  histogram_tester.ExpectTotalCount("OOBE.GaiaLoginTime", 0);
 }
 
 // Tests the no password scraped flow.
@@ -637,6 +641,7 @@ IN_PROC_BROWSER_TEST_F(SamlTest, ScrapedNone) {
   histogram_tester.ExpectUniqueSample("ChromeOS.SAML.APILogin", 2, 1);
   histogram_tester.ExpectUniqueSample("ChromeOS.SAML.Scraping.PasswordCountAll",
                                       0, 1);
+  histogram_tester.ExpectTotalCount("OOBE.GaiaLoginTime", 0);
 }
 
 // Types the second user e-mail into the GAIA login form but then authenticates
--- chrome/browser/ash/login/webview_login_browsertest.cc
+++ chrome/browser/ash/login/webview_login_browsertest.cc
@@ -292,6 +292,7 @@ class WebviewLoginTest : public OobeBaseTest {
     command_line->AppendSwitch(switches::kOobeSkipPostLogin);
     OobeBaseTest::SetUpCommandLine(command_line);
   }
+  base::HistogramTester histogram_tester;
 
  protected:
   void ExpectIdentifierPage() {
@@ -403,7 +404,6 @@ IN_PROC_BROWSER_TEST_F(WebviewLoginTest, NativeTest) {
 
 // Basic signin with username and password.
 IN_PROC_BROWSER_TEST_F(WebviewLoginTest, Basic) {
-  base::HistogramTester histogram_tester;
   WaitForGaiaPageLoadAndPropertyUpdate();
 
   ExpectIdentifierPage();
@@ -434,6 +434,7 @@ IN_PROC_BROWSER_TEST_F(WebviewLoginTest, Basic) {
   EXPECT_FALSE(LoginDisplayHost::default_host());
 
   histogram_tester.ExpectUniqueSample("ChromeOS.SAML.APILogin", 0, 1);
+  histogram_tester.ExpectTotalCount("OOBE.GaiaLoginTime", 1);
 }
 
 IN_PROC_BROWSER_TEST_F(WebviewLoginTest, BackButton) {
@@ -645,6 +646,7 @@ IN_PROC_BROWSER_TEST_F(ReauthEndpointWebviewLoginOwnerTest, SupervisedUser) {
             reauth_user_.account_id.GetUserEmail());
   EXPECT_EQ(fake_gaia_.fake_gaia()->is_supervised(), "1");
   EXPECT_EQ(fake_gaia_.fake_gaia()->is_device_owner(), "1");
+  histogram_tester.ExpectTotalCount("OOBE.GaiaLoginTime", 0);
 }
 
 IN_PROC_BROWSER_TEST_F(WebviewLoginTest, StoragePartitionHandling) {
