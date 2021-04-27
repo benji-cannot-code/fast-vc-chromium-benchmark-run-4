1f210b598e857032dbeafe0fc0761f54b7bff08f
David Maunder <davidjm@chromium.org>
2021-04-27 23:07:39 +0000

    Roll back WebContentsState changes in CriticalPersistedTabData
    These have been causing crashes. I have only been able to rule out
    various theories so far and have run out of time to investigate further.
    Added a TODO to pick up this investigation later and revert the change
    which uses ByteString.copyFrom(ByteBuffer[] ...) which is causing some
    crashes. These crashes started when we started using
    ByteString.copyFrom(ByteBuffer[] ...).
    
    Bug: 1195550
    Change-Id: Ibebca36f8fd68029b5e0dc4d986c892ef0533d55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854399
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876808}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/state/CriticalPersistedTabDataTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/state/CriticalPersistedTabDataTest.java
@@ -153,7 +153,7 @@ public class CriticalPersistedTabDataTest {
         Assert.assertEquals(mCriticalPersistedTabData.getThemeColor(), THEME_COLOR);
         Assert.assertEquals(
                 mCriticalPersistedTabData.getTabLaunchTypeAtCreation(), LAUNCH_TYPE_AT_CREATION);
-        Assert.assertArrayEquals(getByteArrayFromByteBuffer(
+        Assert.assertArrayEquals(CriticalPersistedTabData.getContentStateByteArray(
                                          mCriticalPersistedTabData.getWebContentsState().buffer()),
                 WEB_CONTENTS_STATE_BYTES);
         Semaphore deleteSemaphore = new Semaphore(0);
@@ -272,7 +272,8 @@ public class CriticalPersistedTabDataTest {
         Assert.assertEquals(THEME_COLOR, deserialized.getThemeColor());
         Assert.assertEquals(LAUNCH_TYPE_AT_CREATION, deserialized.getTabLaunchTypeAtCreation());
         Assert.assertArrayEquals(WEB_CONTENTS_STATE_BYTES,
-                getByteArrayFromByteBuffer(deserialized.getWebContentsState().buffer()));
+                CriticalPersistedTabData.getContentStateByteArray(
+                        deserialized.getWebContentsState().buffer()));
     }
 
     @UiThreadTest
@@ -493,12 +494,5 @@ public class CriticalPersistedTabDataTest {
             if (type == CriticalPersistedTabDataProto.LaunchTypeAtCreation.UNKNOWN) continue;
             CriticalPersistedTabData.getLaunchType(type);
         }
-    }
-
-    private static final byte[] getByteArrayFromByteBuffer(ByteBuffer buffer) {
-        byte[] contentsStateBytes = new byte[buffer.limit()];
-        buffer.rewind();
-        buffer.get(contentsStateBytes);
-        return contentsStateBytes;
     }
 }
