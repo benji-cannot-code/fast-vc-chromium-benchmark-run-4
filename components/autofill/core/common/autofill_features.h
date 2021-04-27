c91eacab79697a7e05fb6411c828658d558a1050
Mohamed Amir Yosef <mamir@chromium.org>
2021-04-27 10:09:37 +0000

    [Autofill] Move address nickname behind a feature flag
    Address nicknames have been moved to V2.
    This CL puts the code that shows the nickname field behind a newly
    introduced feature flag.
    
    Bug: 1167060
    Change-Id: I3dbdc59a07d9235737f354241db17a7f38b04b8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2849105
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876519}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -22,6 +22,8 @@ extern const base::Feature kAutofillAddressEnhancementVotes;
 COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillAddressProfileSavePrompt;
 COMPONENT_EXPORT(AUTOFILL)
+extern const base::Feature kAutofillAddressProfileSavePromptNicknameSupport;
+COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillAllowDuplicateFormSubmissions;
 COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillAllowNonHttpActivation;
