37f1c08f217361e698dc3de14d567fcec10aa8ed
Lily Chen <chlily@chromium.org>
2021-04-27 20:25:06 +0000

    Remove unused function in blink::StorageController
    Change-Id: Ie88be14d3844fa219e6b8e6f7223789509689234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854796
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876732}

--- third_party/blink/renderer/modules/storage/storage_controller.h
+++ third_party/blink/renderer/modules/storage/storage_controller.h
@@ -46,7 +46,7 @@ class StorageNamespace;
 // The LocalStorage StorageNamespace object is owned internally, and
 // StorageController delegates the following methods to that namespace:
 // GetLocalStorageArea, AddLocalStorageInspectorStorageAgent,
-// RemoveLocalStorageInspectorStorageAgent, DidDispatchLocalStorageEvent
+// RemoveLocalStorageInspectorStorageAgent
 class MODULES_EXPORT StorageController : public mojom::blink::DomStorageClient {
   USING_FAST_MALLOC(StorageController);
 
@@ -78,16 +78,12 @@ class MODULES_EXPORT StorageController : public mojom::blink::DomStorageClient {
   // Cleans up unused areas if the total cache size is over the cache limit.
   void ClearAreasIfNeeded();
 
-  // Methods that delegate to the internal SessionNamespace used for
+  // Methods that delegate to the internal StorageNamespace used for
   // LocalStorage:
 
   scoped_refptr<CachedStorageArea> GetLocalStorageArea(const SecurityOrigin*);
   void AddLocalStorageInspectorStorageAgent(InspectorDOMStorageAgent* agent);
   void RemoveLocalStorageInspectorStorageAgent(InspectorDOMStorageAgent* agent);
-  void DidDispatchLocalStorageEvent(const SecurityOrigin* origin,
-                                    const String& key,
-                                    const String& old_value,
-                                    const String& new_value);
 
   mojom::blink::DomStorage* dom_storage() const {
     return dom_storage_remote_.get();
