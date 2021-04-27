67615326b3aa9423f408bd588519f1b04a0903a7
gogerald <gogerald@google.com>
2021-04-27 21:08:53 +0000

    Add user actions for the start surface
    Bug: 1203297
    Change-Id: Icd276aea8f489f475459f99c1ffbbaceb489fcbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853684
    Auto-Submit: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876755}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -751,10 +751,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)configureStartSurfaceIfNeeded {
   SceneState* scene =
       SceneStateBrowserAgent::FromBrowser(self.browser)->GetSceneState();
-  BOOL shouldShowReturnToRecentTabTile =
-      scene.modifytVisibleNTPForStartSurface &&
-      ShouldShowReturnToMostRecentTabForStartSurface();
-  if (shouldShowReturnToRecentTabTile) {
+  if (!scene.modifytVisibleNTPForStartSurface)
+    return;
+
+  if (ShouldShowReturnToMostRecentTabForStartSurface()) {
+    base::RecordAction(
+        base::UserMetricsAction("IOS.StartSurface.ShowReturnToRecentTabTile"));
     web::WebState* most_recent_tab =
         StartSurfaceRecentTabBrowserAgent::FromBrowser(self.browser)
             ->most_recent_tab();
@@ -770,8 +772,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       StartSurfaceRecentTabBrowserAgent::FromBrowser(self.browser)
           ->AddObserver(_startSurfaceObserver.get());
     }
-    scene.modifytVisibleNTPForStartSurface = NO;
   }
+  if (ShouldShrinkLogoForStartSurface()) {
+    base::RecordAction(base::UserMetricsAction("IOS.StartSurface.ShrinkLogo"));
+  }
+  if (ShouldHideShortcutsForStartSurface()) {
+    base::RecordAction(
+        base::UserMetricsAction("IOS.StartSurface.HideShortcuts"));
+  }
+  scene.modifytVisibleNTPForStartSurface = NO;
 }
 
 // Creates, configures and returns a DiscoverFeed ViewController.
