2188f679d923882abb9563844c5283d088662803
Alexis Hetu <sugoi@google.com>
2021-04-27 22:31:53 +0000

    Add new flag to allow using SwANGLE as WebGL fallback
    Currently, Chrome uses legacy SwiftShader GL as the WebGL fallback when
    a hardware GPU isn't usable for the purpuse of rendering WebGL content.
    
    As we're in the process of changing legacy SwiftShader GL to SwANGLE,
    this CL adds a new flag which allows using SwANGLE for WebGL only.
    
    To clarify, current switches with legacy SwiftShader GL:
    Software GL: --use-gl=swiftshader
    Software WebGL only: --use-gl=swiftshader-webgl
    
    With SwANGLE, the new switches will be:
    Software GL: --use-gl=angle --use-angle=swiftshader
    Software WebGL only: --use-gl=angle --use-angle=swiftshader-webgl
    
    Bug: chromium:1060139
    Change-Id: I8d0382ac3a12c1578dfc0e62fc1949a0a9c66bd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2845229
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Jonah Ryan-Davis <jonahr@google.com>
    Cr-Commit-Position: refs/heads/master@{#876794}

--- ui/gl/gl_switches.cc
+++ ui/gl/gl_switches.cc
@@ -35,6 +35,7 @@ const char kANGLEImplementationOpenGLESEGLName[] = "gles-egl";
 const char kANGLEImplementationNullName[] = "null";
 const char kANGLEImplementationVulkanName[] = "vulkan";
 const char kANGLEImplementationSwiftShaderName[] = "swiftshader";
+const char kANGLEImplementationSwiftShaderForWebGLName[] = "swiftshader-webgl";
 const char kANGLEImplementationMetalName[] = "metal";
 const char kANGLEImplementationNoneName[] = "";
 
