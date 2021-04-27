0b6909ef3eb1b91aa84f60238429ef54545aa06d
Peter Boström <pbos@chromium.org>
2021-04-27 16:39:20 +0000

    Make Widget::SetNativeTheme propagate
    Fixes a bug where ToolbarActionView::OnThemeChanged() does not return
    the same GetNativeTheme() as ToolbarButton does. This was because
    ToolbarButton would call OnThemeChanged() as a result of
    BrowserFrame::InitBrowserFrame() before InitBrowserFrame() calls
    SelectNativeTheme(). ToolbarActionView however gets added asynchronously
    after the BrowserFrame is initialized, so they do pick up the correct
    final version of GetNativeTheme().
    
    This fix makes sure that ToolbarButton (and any other items that are
    synchronously initialized as a result of Init() inside BrowserFrame) do
    receive the correct theme after SetNativeTheme() is called. A follow-up
    change would investigate whether it's feasible to SetNativeTheme()
    before the hierarchy is initialized so that this propagated update is
    unnecessary.
    
    This also fixes ToolbarActionView's focus ring so that it displays in a
    contrasty version if the theme toolbar contrasts badly with the focus
    ring. This is how the missing SetNativeTheme() propagation was found.
    
    Bug: 1068929
    Change-Id: I75f964ea197b637527824c04be8beb0c4fc5ee5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2852564
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876628}

--- chrome/browser/ui/views/toolbar/toolbar_action_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_action_view.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/view_ids.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/extensions/extension_context_menu_controller.h"
+#include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_ink_drop_util.h"
 #include "components/sessions/content/session_tab_helper.h"
 #include "content/public/browser/notification_source.h"
@@ -87,6 +88,12 @@ gfx::Rect ToolbarActionView::GetAnchorBoundsInScreen() const {
   gfx::Rect bounds = GetBoundsInScreen();
   bounds.Inset(GetToolbarInkDropInsets(this));
   return bounds;
+}
+
+void ToolbarActionView::OnThemeChanged() {
+  MenuButton::OnThemeChanged();
+
+  ToolbarButton::UpdateFocusRingColor(this, focus_ring());
 }
 
 std::unique_ptr<LabelButtonBorder> ToolbarActionView::CreateDefaultBorder()
@@ -216,6 +223,8 @@ void ToolbarActionView::OnDragDone() {
 
 void ToolbarActionView::AddedToWidget() {
   MenuButton::AddedToWidget();
+
+  ToolbarButton::UpdateFocusRingColor(this, focus_ring());
 
   // This cannot happen until there's a focus controller, which lives on the
   // widget.
--- ui/views/widget/widget.cc
+++ ui/views/widget/widget.cc
@@ -1114,6 +1114,7 @@ void Widget::SetNativeTheme(ui::NativeTheme* native_theme) {
   native_theme_observation_.Reset();
   if (native_theme)
     native_theme_observation_.Observe(native_theme);
+  PropagateNativeThemeChanged();
 }
 
 int Widget::GetX() const {
