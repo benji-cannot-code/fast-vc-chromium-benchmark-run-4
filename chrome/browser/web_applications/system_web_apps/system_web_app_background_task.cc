0a6f82e2a44c4b36fd665a4bf3574dadb6a0ca5c
Dominic Schulz <dominicschulz@google.com>
2021-04-27 03:28:53 +0000

    system-web-apps: Make background page timer optional
    2 things for background processing:
    Make the timer period optional in the case that a user-login
    and nothing else is wanted. Previously, one had to have a super
    long timer period or something if a periodic timer was needed.
    
    Null out the webcontents when window.close() is called. Save the
    memory on smaller devices. This also fixes a bug where the
    background page could still be seen in chrome://inspect#other
    even after it was successfully closed.
    
    Change-Id: Ia2a63374925e838ceed8cdd1053fc613720dc681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846176
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Dominic Schulz <dominicschulz@google.com>
    Cr-Commit-Position: refs/heads/master@{#876404}

--- chrome/browser/web_applications/system_web_apps/system_web_app_background_task.cc
+++ chrome/browser/web_applications/system_web_apps/system_web_app_background_task.cc
@@ -16,6 +16,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app {
 
+SystemAppBackgroundTaskInfo::~SystemAppBackgroundTaskInfo() = default;
+SystemAppBackgroundTaskInfo::SystemAppBackgroundTaskInfo() = default;
+
+SystemAppBackgroundTaskInfo::SystemAppBackgroundTaskInfo(
+    const SystemAppBackgroundTaskInfo& other) = default;
+
+SystemAppBackgroundTaskInfo::SystemAppBackgroundTaskInfo(
+    const base::Optional<base::TimeDelta>& period,
+    const GURL& url,
+    bool open_immediately)
+    : period(period), url(url), open_immediately(open_immediately) {}
+
 SystemAppBackgroundTask::SystemAppBackgroundTask(
     Profile* profile,
     const SystemAppBackgroundTaskInfo& info)
@@ -27,7 +39,8 @@ SystemAppBackgroundTask::SystemAppBackgroundTask(
       period_(info.period),
       opened_count_(0),
       timer_activated_count_(0),
-      open_immediately_(info.open_immediately) {}
+      open_immediately_(info.open_immediately),
+      delegate_(this) {}
 
 SystemAppBackgroundTask::~SystemAppBackgroundTask() = default;
 
@@ -41,8 +54,8 @@ void SystemAppBackgroundTask::StartTask() {
         base::BindOnce(&SystemAppBackgroundTask::MaybeOpenPage,
                        weak_ptr_factory_.GetWeakPtr()));
     state_ = INITIAL_WAIT;
-  } else {
-    timer_->Start(FROM_HERE, period_,
+  } else if (period_) {
+    timer_->Start(FROM_HERE, period_.value(),
                   base::BindOnce(&SystemAppBackgroundTask::MaybeOpenPage,
                                  weak_ptr_factory_.GetWeakPtr()));
     state_ = WAIT_PERIOD;
@@ -70,8 +83,8 @@ void SystemAppBackgroundTask::MaybeOpenPage() {
     // We've gone through some weird clock adjustment (daylight savings?) that's
     // sent us back in time. We don't know what's going on, so zero the polling
     // time and stop polling.
-    if (polling_duration < base::TimeDelta::FromSeconds(0)) {
-      timer_->Start(FROM_HERE, period_,
+    if (polling_duration < base::TimeDelta::FromSeconds(0) && period_) {
+      timer_->Start(FROM_HERE, period_.value(),
                     base::BindOnce(&SystemAppBackgroundTask::MaybeOpenPage,
                                    weak_ptr_factory_.GetWeakPtr()));
       state_ = WAIT_PERIOD;
@@ -87,18 +100,31 @@ void SystemAppBackgroundTask::MaybeOpenPage() {
     return;
   }
 
-  timer_->Start(FROM_HERE, period_,
-                base::BindOnce(&SystemAppBackgroundTask::MaybeOpenPage,
-                               weak_ptr_factory_.GetWeakPtr()));
+  if (period_) {
+    timer_->Start(FROM_HERE, period_.value(),
+                  base::BindOnce(&SystemAppBackgroundTask::MaybeOpenPage,
+                                 weak_ptr_factory_.GetWeakPtr()));
+  }
   polling_since_time_ = base::Time();
   state_ = WAIT_PERIOD;
   NavigateBackgroundPage();
+}
+
+void SystemAppBackgroundTask::CloseDelegate::CloseContents(
+    content::WebContents* contents) {
+  task_->CloseWebContents(contents);
+}
+
+void SystemAppBackgroundTask::CloseWebContents(content::WebContents* contents) {
+  DCHECK(contents == web_contents_.get());
+  web_contents_.reset();
 }
 
 void SystemAppBackgroundTask::NavigateBackgroundPage() {
   if (!web_contents_) {
     web_contents_ = content::WebContents::Create(
         content::WebContents::CreateParams(profile_));
+    web_contents_->SetDelegate(&delegate_);
   }
 
   timer_activated_count_++;
@@ -113,10 +139,12 @@ void SystemAppBackgroundTask::NavigateBackgroundPage() {
 }
 
 void SystemAppBackgroundTask::OnLoaderReady(WebAppUrlLoader::Result result) {
-  web_app_url_loader_->LoadUrl(
-      url_, web_contents_.get(), WebAppUrlLoader::UrlComparison::kExact,
-      base::BindOnce(&SystemAppBackgroundTask::OnPageReady,
-                     weak_ptr_factory_.GetWeakPtr()));
+  if (web_contents_) {
+    web_app_url_loader_->LoadUrl(
+        url_, web_contents_.get(), WebAppUrlLoader::UrlComparison::kExact,
+        base::BindOnce(&SystemAppBackgroundTask::OnPageReady,
+                       weak_ptr_factory_.GetWeakPtr()));
+  }
 }
 
 void SystemAppBackgroundTask::OnPageReady(WebAppUrlLoader::Result result) {
