bbd0468eeae4ab25455c0e442d8a351bf9836b66
Gayane Petrosyan <gayane@chromium.org>
2021-04-27 14:18:23 +0000

    [Webnotes] Add flag and sharing hub button for webnotes stylized.
    Screenshot
    https://screenshot.googleplex.com/4bWXsEG28iXwxJg.png
    
    Bug: 1201782
    Change-Id: I4078f327744e05aa6e0058f89ac30ca3ff4dde55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846803
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876565}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -265,6 +265,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kVoiceButtonInTopToolbar,
     &kVrBrowsingFeedback,
     &kPrefetchNotificationSchedulingIntegration,
+    &kWebNotesStylize,
     &features::kDnsOverHttps,
     &net::features::kSameSiteByDefaultCookies,
     &net::features::kCookiesWithoutSameSiteMustBeSecure,
@@ -747,6 +748,9 @@ const base::Feature kVoiceButtonInTopToolbar{"VoiceButtonInTopToolbar",
 
 const base::Feature kVrBrowsingFeedback{"VrBrowsingFeedback",
                                         base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::Feature kWebNotesStylize{"WebNotesStylize",
+                                     base::FEATURE_DISABLED_BY_DEFAULT};
 
 static jboolean JNI_ChromeFeatureList_IsEnabled(
     JNIEnv* env,
