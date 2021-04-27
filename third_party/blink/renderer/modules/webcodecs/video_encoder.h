89beb005dd15180c6a406de43b8c7dda2cad0fdd
Thomas Guilbert <tguilbert@chromium.org>
2021-04-27 23:16:52 +0000

    Remove EncoderBase::CloneFrame()
    Now that AudioFrame has a clone() method, CloneFrame() can be removed,
    in favor of using the frame methods directly.
    
    Fixes: 1201992
    Change-Id: I98a1be3cce2c576d7c85a97478f9a84551c6c2b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854401
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876816}

--- third_party/blink/renderer/modules/webcodecs/video_encoder.h
+++ third_party/blink/renderer/modules/webcodecs/video_encoder.h
@@ -95,7 +95,6 @@ class MODULES_EXPORT VideoEncoder final
   ParsedConfig* ParseConfig(const VideoEncoderConfig*,
                             ExceptionState&) override;
   bool VerifyCodecSupport(ParsedConfig*, ExceptionState&) override;
-  VideoFrame* CloneFrame(VideoFrame*, ExceptionState&) override;
 
   void ContinueConfigureWithGpuFactories(
       Request* request,
