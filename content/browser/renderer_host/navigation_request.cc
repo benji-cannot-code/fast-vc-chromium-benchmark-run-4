5765ff61d6b03d0ae02713a33b5af0aef3730a46
Harkiran Bolaria <hbolaria@google.com>
2021-04-27 17:44:35 +0000

    [MPArch] NavigationRequest::begin_params() to not return pointer.
    This is to make the accessor consistent with accessors to
    commit_params_ and common_params_.
    
    Change-Id: I06a8a05db2ed9cbcf29f76f63c2771e9dde410ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2851617
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Cr-Commit-Position: refs/heads/master@{#876655}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -5805,11 +5805,11 @@ bool NavigationRequest::WasStartedFromContextMenu() {
 }
 
 const GURL& NavigationRequest::GetSearchableFormURL() {
-  return begin_params()->searchable_form_url;
+  return begin_params().searchable_form_url;
 }
 
 const std::string& NavigationRequest::GetSearchableFormEncoding() {
-  return begin_params()->searchable_form_encoding;
+  return begin_params().searchable_form_encoding;
 }
 
 ReloadType NavigationRequest::GetReloadType() {
@@ -5834,11 +5834,11 @@ bool NavigationRequest::IsDownload() {
 }
 
 bool NavigationRequest::IsFormSubmission() {
-  return begin_params()->is_form_submission;
+  return begin_params().is_form_submission;
 }
 
 bool NavigationRequest::WasInitiatedByLinkClick() {
-  return begin_params()->was_initiated_by_link_click;
+  return begin_params().was_initiated_by_link_click;
 }
 
 const std::string& NavigationRequest::GetHrefTranslate() {
@@ -5846,7 +5846,7 @@ const std::string& NavigationRequest::GetHrefTranslate() {
 }
 
 const base::Optional<blink::Impression>& NavigationRequest::GetImpression() {
-  return begin_params()->impression;
+  return begin_params().impression;
 }
 
 const base::Optional<blink::LocalFrameToken>&
