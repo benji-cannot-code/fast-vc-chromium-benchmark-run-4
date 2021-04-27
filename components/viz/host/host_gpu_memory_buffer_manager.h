374567a6b37aa369e7dba8be142552b0ab1cd0db
John Abd-El-Malek <jam@chromium.org>
2021-04-27 00:06:23 +0000

    Remove the waitable event in HostGpuMemoryBufferManager since it's not used.
    Currently under X11 it's waited on the IO thread for an incoming IPC, which would deadlock. On X11 it is safe to assume GpuHostImpl::DidInitialize (which sets the native buffer configuration) ran before trying to create a GpuMemoryBuffer since GPU acceleration can't be assumed beforehand.
    
    See thread on graphics-dev: https://groups.google.com/a/chromium.org/g/graphics-dev/c/9MPg000oL6Y/m/KzlGE2sEBAAJ
    
    Bug: 904556
    Change-Id: Id94ed20b92cfa5d06b24d40807e08b426e7192a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2850375
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876375}

--- components/viz/host/host_gpu_memory_buffer_manager.h
+++ components/viz/host/host_gpu_memory_buffer_manager.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/unsafe_shared_memory_pool.h"
 #include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
+#include "base/synchronization/atomic_flag.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/trace_event/memory_dump_provider.h"
 #include "components/viz/host/viz_host_export.h"
@@ -71,7 +72,6 @@ class VIZ_HOST_EXPORT HostGpuMemoryBufferManager
       gpu::SurfaceHandle surface_handle,
       base::OnceCallback<void(gfx::GpuMemoryBufferHandle)> callback);
 
-  // This method will block until the initial GPUInfo is received.
   bool IsNativeGpuMemoryBufferConfiguration(gfx::BufferFormat format,
                                             gfx::BufferUsage usage) const;
 
@@ -96,7 +96,6 @@ class VIZ_HOST_EXPORT HostGpuMemoryBufferManager
                     base::trace_event::ProcessMemoryDump* pmd) override;
 
  protected:
-  // Must be called from a thread other than |task_runner_|'s thread.
   void SetNativeConfigurations(
       gpu::GpuMemoryBufferConfigurationSet native_configurations);
 
@@ -154,7 +153,7 @@ class VIZ_HOST_EXPORT HostGpuMemoryBufferManager
   scoped_refptr<base::UnsafeSharedMemoryPool> pool_;
 
   gpu::GpuMemoryBufferConfigurationSet native_configurations_;
-  mutable base::WaitableEvent native_configurations_initialized_;
+  base::AtomicFlag native_configurations_initialized_;
 
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   base::WeakPtr<HostGpuMemoryBufferManager> weak_ptr_;
