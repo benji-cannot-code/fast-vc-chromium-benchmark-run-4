47714595e2865f00cbfcda78bbd66b804867ddea
Koji Ishii <kojii@chromium.org>
2021-04-27 15:18:04 +0000

    Fix CheckIsVisualOverflowComputed in InlineFlowBox::AddToLine
    This code path hits the DCHECK with crrev.com/c/2852597 in:
    external/wpt/mathml/relations/html5-tree/integration-point-4.html
    
    Bug: 1201670
    Change-Id: Ic8b972287544d9f72e347dcf78333a4b554a98d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2852828
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876592}

--- third_party/blink/renderer/core/layout/line/inline_flow_box.cc
+++ third_party/blink/renderer/core/layout/line/inline_flow_box.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/line/inline_text_box.h"
 #include "third_party/blink/renderer/core/layout/line/line_orientation_utils.h"
 #include "third_party/blink/renderer/core/layout/line/root_inline_box.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_ink_overflow.h"
 #include "third_party/blink/renderer/core/paint/box_painter.h"
 #include "third_party/blink/renderer/core/paint/inline_flow_box_painter.h"
 #include "third_party/blink/renderer/core/paint/rounded_border_geometry.h"
@@ -211,6 +212,11 @@ void InlineFlowBox::AddToLine(InlineBox* child) {
         child->ClearKnownToHaveNoOverflow();
     } else if (child->GetLineLayoutItem().IsAtomicInlineLevel()) {
       LineLayoutBox box = LineLayoutBox(child->GetLineLayoutItem());
+#if DCHECK_IS_ON()
+      // We're reading the previous overflow state. Read as no overflow if it
+      // was not computed yet.
+      NGInkOverflow::ReadUnsetAsNoneScope read_unset_as_none;
+#endif
       if (box.HasLayoutOverflow() || box.HasVisualOverflow() ||
           box.HasSelfPaintingLayer())
         child->ClearKnownToHaveNoOverflow();
