86a5083f9c1a0e27f4105f324f715cc2393f4038
Sophie Chang <sophiechang@chromium.org>
2021-04-27 22:56:46 +0000

    Add experiment name param to GetClustersRequest controllable via Finch
    Bug: 1179068
    Change-Id: Ib5de5bcc07223e8abc4692e254364e624b596d44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854406
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876803}

--- components/history_clusters/core/memories_features.cc
+++ components/history_clusters/core/memories_features.cc
@@ -14,6 +14,11 @@ GURL RemoteModelEndpointForDebugging() {
       kRemoteModelForDebugging, "MemoriesRemoteModelEndpoint"));
 }
 
+std::string ExperimentNameForRemoteModelEndpoint() {
+  return base::GetFieldTrialParamValueByFeature(
+      kRemoteModelForDebugging, "MemoriesRemoteModelEndpointExperimentName");
+}
+
 bool StoreVisitsInHistoryDb() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kMemories, "MemoriesStoreVisitsInHistoryDb", false);
