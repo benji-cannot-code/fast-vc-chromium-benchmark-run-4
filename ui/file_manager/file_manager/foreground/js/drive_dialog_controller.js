68d33f36c68bb7b990fb560b00b962d328b5e9d6
Noel Gordon <noel@chromium.org>
2021-04-27 07:00:01 +0000

    [filesapp] DriveDialogController should check the dialog is open
    Closing the device lid: the DriveDialogController.onMountedCompleted_()
    is called and calls close() on its |dialog_| member.
    
    However, if the dialog is not open, a console error appears [1]. To fix
    this, check if the dialog is open before calling |dialog_|.close().
    
    [1] Files App Dialogs, and their cr.ui.Dialog base class, throw if hide
    is called when they are already hidden.
    
    Bug: 1203144
    Change-Id: I21386171f7cb6a928dc81b01a7debef399312524
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853284
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Auto-Submit: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876476}

--- ui/file_manager/file_manager/foreground/js/drive_dialog_controller.js
+++ ui/file_manager/file_manager/foreground/js/drive_dialog_controller.js
@@ -74,7 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             chrome.fileManagerPrivate.MountCompletedEventType.UNMOUNT &&
         event.volumeMetadata.volumeType ===
             chrome.fileManagerPrivate.VolumeType.DRIVE) {
-      this.dialog_.hide();
+      this.open && this.dialog_.hide();
     }
   }
 }
