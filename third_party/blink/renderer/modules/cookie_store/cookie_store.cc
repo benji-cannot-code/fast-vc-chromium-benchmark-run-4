d5be416a01ae9be6dd5eaa5b2570d64493c739c7
Frédéric Wang <fwang@igalia.com>
2021-04-27 08:05:27 +0000

    Allow Cookie Store API on all secure contexts
    In [1], it was clarified that Cookie Store API is restricted to secure
    contexts [2]. However, this restriction is currently implemented using
    SecurityOrigin::IsSecure() which does not exactly correspond to [3].
    This CL moves to network::IsOriginPotentiallyTrustworthy() instead,
    allowing to use Cookie Store API on the following origins:
    
    * Origins with host loopback address.
    * Origins with host a localhost or *.localhost address.
    
    This makes things consistent with how document.cookie behaves. However,
    for the same reason, we ensure the local schemes (i.e. file://)  is
    still excluded, as discussed with the API owners [4].
    
    Tests for localhost are added to verify that both document.cookie and
    the Cookie Store API allows to read, write and delete cookies. The case
    for file:// was verified manually.
    
    See also [5] for the spec issue.
    
    [1] https://github.com/WICG/cookie-store/issues/21
    [2] https://html.spec.whatwg.org/multipage/webappapis.html#secure-context
    [3] https://w3c.github.io/webappsec-secure-contexts/#potentially-trustworthy-url
    [4] https://groups.google.com/a/chromium.org/g/blink-dev/c/ekdeaj09c0w
    [5] https://github.com/WICG/cookie-store/issues/193
    
    Bug: 856362, 1153336
    Change-Id: I60d4deda0394ae75d976f0e0212ad60258755431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831586
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876494}

--- third_party/blink/renderer/modules/cookie_store/cookie_store.cc
+++ third_party/blink/renderer/modules/cookie_store/cookie_store.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/optional.h"
 #include "net/cookies/canonical_cookie.h"
+#include "services/network/public/cpp/is_potentially_trustworthy.h"
 #include "services/network/public/mojom/restricted_cookie_manager.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_throw_dom_exception.h"
@@ -130,9 +131,10 @@ std::unique_ptr<net::CanonicalCookie> ToCanonicalCookie(
   // automatically turn it into a secure cookie without any warning.
   //
   // The Cookie Store API can only set secure cookies, so it is unusable on
-  // insecure origins.
-  // TODO(crbug.com/1153336) Use network::IsUrlPotentiallyTrustworthy().
-  if (!SecurityOrigin::IsSecure(cookie_url)) {
+  // insecure origins. file:// are excluded too for consistency with
+  // document.cookie.
+  if (!network::IsUrlPotentiallyTrustworthy(cookie_url) ||
+      base::Contains(url::GetLocalSchemes(), cookie_url.Protocol().Ascii())) {
     exception_state.ThrowTypeError(
         "Cannot modify a secure cookie on insecure origin");
     return nullptr;
