388fbdb1245b37d326463b7e9b1fb4e28c2ec319
Yoshifumi Inoue <yosin@chromium.org>
2021-04-27 10:37:33 +0000

    [LayoutNGBlockFragmentation] make Selection#modify forward line to work with text spreding into multiple columns
    This patch changes |AbstractInlineBox::NextLine()| to utilize
     * |NGInlineCursor::CursorForMovingAcrossFragmentainer()|
     * |NGInlineCursor::MoveToNextIncludingFragmentainer()|
    to find next line across fragmentainer.
    
    Note: Following patch will make |AbstractInlineBox::PreviousLine()| to do so.
    
    Bug: 829028
    Change-Id: I58a2040bb318917651c52af59c0149b701ef7e54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2851831
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876522}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h
@@ -292,6 +292,10 @@ class CORE_EXPORT NGInlineCursor {
   // has no children, returns an empty cursor.
   NGInlineCursor CursorForDescendants() const;
 
+  // Returns a new |NGInlineCursor| whose root is containing block or multicol
+  // container for traversing fragmentainers in root.
+  NGInlineCursor CursorForMovingAcrossFragmentainer() const;
+
   // If |this| is created by |CursorForDescendants()| to traverse parts of an
   // inline formatting context, expand the traversable range to the containing
   // |LayoutBlockFlow|. Does nothing if |this| is for an inline formatting
@@ -571,6 +575,10 @@ class CORE_EXPORT NGInlineCursor {
   bool SetRoot(const LayoutBlockFlow& block_flow, wtf_size_t fragment_index);
 
   bool TrySetRootFragmentItems();
+
+  // Returns true and move to current position to |fragment_item|, otherwise
+  // returns false.
+  bool TryToMoveTo(const NGFragmentItem& fragment_item);
 
   void MoveToItem(const ItemsSpan::iterator& iter);
 
