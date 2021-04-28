c46db89b69fea9bf2bcc360b998e5d920995a7ad
Olesia Marukhno <olesiamarukhno@google.com>
2021-04-28 11:59:19 +0000

    Remove profile reference from PageInfoMainView
    Removed profile reference from PageInfoMainView to decrease number of
    dependencies of the PageInfoMainView. Dependecies of the
    PageInfoMainView also influence subpage infrasture classes which will be
    added later (PageInfoNavigationHandler, PageInfoViewFactory). It was
    used to check if profile is in the guest mode and in that case, site
    settings button is hidden.
    
    Added ShouldShowSiteSettings method to page_info_ui_delegate that will
    return true if profile isn't in guest mode.
    
    Bug: 1188101
    Change-Id: I21d5063d297ca9fea7417d0b7a7723fdfe3b2c24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2848167
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/master@{#877003}

--- chrome/browser/ui/page_info/chrome_page_info_ui_delegate.h
+++ chrome/browser/ui/page_info/chrome_page_info_ui_delegate.h
@@ -19,6 +19,7 @@ class ChromePageInfoUiDelegate : public PageInfoUiDelegate {
   // PageInfoUiDelegate implementation
 #if !defined(OS_ANDROID)
   bool IsBlockAutoPlayEnabled() override;
+  bool ShouldShowSiteSettings() override;
 #endif
   permissions::PermissionResult GetPermissionStatus(
       ContentSettingsType type) override;
--- chrome/browser/ui/views/page_info/page_info_main_view.h
+++ chrome/browser/ui/views/page_info/page_info_main_view.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "ui/views/view.h"
 
-class Profile;
-
 // The main view of the page info, contains security information, permissions
 // and  site-related settings. This is used in the experimental
 // PageInfoNewBubbleView (under a flag PageInfoV2Desktop).
@@ -29,9 +27,7 @@ class PageInfoMainView : public views::View,
   // The width of the column size for permissions and chosen object icons.
   static constexpr int kIconColumnWidth = 16;
 
-  PageInfoMainView(PageInfo* presenter,
-                   PageInfoUiDelegate* ui_delegate,
-                   Profile* profile);
+  PageInfoMainView(PageInfo* presenter, PageInfoUiDelegate* ui_delegate);
   ~PageInfoMainView() override;
 
   enum PageInfoBubbleViewID {
@@ -120,7 +116,7 @@ class PageInfoMainView : public views::View,
   PageInfoHoverButton* certificate_button_ = nullptr;
 
   // The button that opens up "Site Settings".
-  views::View* site_settings_link = nullptr;
+  views::View* site_settings_link_ = nullptr;
 
   // The view that contains the "Permissions" table of the bubble.
   views::View* permissions_view_ = nullptr;
