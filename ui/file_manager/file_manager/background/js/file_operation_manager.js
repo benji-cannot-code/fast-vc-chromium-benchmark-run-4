f97d9a6f3bcf8bad3862580538d21c1abc9127e9
François Degros <fdegros@chromium.org>
2021-04-28 06:34:45 +0000

    [Files App] Zip files without NaCl
    Reactivate the old ZIP creation code and make it available under the
    Chrome feature flag files-zip-pack for testing and evaluation.
    
    The old code was added in
    https://chromiumcodereview.appspot.com/11309014
    
    BUG=chromium:912236
    
    Change-Id: I73d7d50e39cdf3d0f0257de9a1f064ec8601b278
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2850559
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876937}

--- ui/file_manager/file_manager/background/js/file_operation_manager.js
+++ ui/file_manager/file_manager/background/js/file_operation_manager.js
@@ -622,7 +622,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   /**
-   * TODO(crbug.com/912236) Remove dead code.
    * Creates a zip file for the selection of files.
    *
    * @param {!Array<!Entry>} selectionEntries The selected entries.
