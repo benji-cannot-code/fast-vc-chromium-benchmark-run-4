f3a1236e9094892bcc88beb41e6d9b9fe75f45a4
Tanmoy Mollik <triploblastic@chromium.org>
2021-04-28 15:55:45 +0000

    [Signin][Test] Replace ChildAccountInfoFetcher.initializeForTests() usage
    ChildAccountInfoFetcher.initializeForTests() initializes
    AccountManagerFacade on the java side for native side test code that
    interacts with ChildAccountInfoFetcher.
    
    This cl replaces usage of this method with
    IdentityTestUtils.setUpMockAccountManagerFacade() and
    IdentityTestEnvironment. AccountManagerFacade on the java side is mocked
    during IdentityTestEnvironment creation.
    
    Bug: 1194969
    Change-Id: I97e53228b95b298d489bf6d70696b2a0717e56d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2805727
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877064}

--- components/signin/internal/identity_manager/account_tracker_service_unittest.cc
+++ components/signin/internal/identity_manager/account_tracker_service_unittest.cc
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 #if defined(OS_ANDROID)
-#include "components/signin/internal/identity_manager/child_account_info_fetcher_android.h"
+#include "components/signin/public/identity_manager/identity_test_utils.h"
 #endif
 
 namespace {
@@ -178,7 +178,9 @@ class AccountTrackerServiceTest : public testing::Test {
       : signin_client_(&pref_service_),
         fake_oauth2_token_service_(&pref_service_) {
 #if defined(OS_ANDROID)
-    ChildAccountInfoFetcherAndroid::InitializeForTests();
+    // Mock AccountManagerFacade in java code for tests that require its
+    // initialization.
+    signin::SetUpMockAccountManagerFacade();
 #endif
 
     AccountTrackerService::RegisterPrefs(pref_service_.registry());
