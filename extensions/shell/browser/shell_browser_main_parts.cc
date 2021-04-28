06aa78942fbba85a844f6b88fcb55c6028564015
Azeem Arshad <azeemarshad@chromium.org>
2021-04-28 17:24:59 +0000

    [app_shell] Add Hermes dbus client init in app_shell.
    Hermes is the ChromeOS eSIM management daemon that's used for
    setup and configuration of eSIM networks. Parts of
    //chromeos/network layers that depend on this client are currently
    guarded by feature flag. This CL is in preparation for enabling
    this feature by default.
    
    This CL adds hermes dbus client initialization in app_shell. This
    fixes issues in extensions::ShellApiTest(s), that depend on
    //chromeos/network layers, when the feature is enabled by default.
    
    Bug: 1093185
    Change-Id: I86c73e5cca49baea4bfa6e917cae0a82d4ef05c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2855777
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877110}

--- extensions/shell/browser/shell_browser_main_parts.cc
+++ extensions/shell/browser/shell_browser_main_parts.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "chromeos/dbus/hermes/hermes_clients.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/nacl/common/buildflags.h"
 #include "components/prefs/pref_service.h"
@@ -130,10 +131,12 @@ void ShellBrowserMainParts::PostMainMessageLoopStart() {
   chromeos::DBusThreadManager::Initialize();
   dbus::Bus* bus = chromeos::DBusThreadManager::Get()->GetSystemBus();
   if (bus) {
+    chromeos::hermes_clients::Initialize(bus);
     bluez::BluezDBusManager::Initialize(bus);
     chromeos::CrasAudioClient::Initialize(bus);
     chromeos::PowerManagerClient::Initialize(bus);
   } else {
+    chromeos::hermes_clients::InitializeFakes();
     bluez::BluezDBusManager::InitializeFake();
     chromeos::CrasAudioClient::InitializeFake();
     chromeos::PowerManagerClient::InitializeFake();
