f61b66df2c78112044c7f6bd7c1c3d386535f553
Sigurd Schneider <sigurds@chromium.org>
2021-04-28 08:10:07 +0000

    Update tests to use NetworkTestRunner interface
    ..instead of reaching directly into DevTools internals.
    
    Depends on front-end CL: https://crrev.com/c/2851745
    
    Bug: chromium:1202116
    Change-Id: Iab17d62c674f4d8e166c115e0e72b49b167dda84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853343
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876958}

--- third_party/blink/web_tests/http/tests/devtools/network/har-post.js
+++ third_party/blink/web_tests/http/tests/devtools/network/har-post.js
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     const stream = new TestRunner.StringOutputStream(resolve);
     const progress = new Common.Progress();
     const networkRequests = NetworkTestRunner.networkRequests();
-    await Network.HARWriter.write(stream, networkRequests, progress);
+    await NetworkTestRunner.writeHARLog(stream, networkRequests, progress);
     progress.done();
     stream.close();
   });
--- third_party/blink/web_tests/http/tests/devtools/websocket/har-websocket.js
+++ third_party/blink/web_tests/http/tests/devtools/websocket/har-websocket.js
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   const harString = await new Promise(async resolve => {
     const stream = new TestRunner.StringOutputStream(resolve);
     const progress = new Common.Progress();
-    await Network.HARWriter.write(
+    await NetworkTestRunner.writeHARLog(
         stream, NetworkTestRunner.networkRequests(), progress);
     progress.done();
     stream.close();
