fe6b0f7352ccbd11ad665cc98335d6b3cc6ac8c1
Yunke Zhou <yunkez@google.com>
2021-04-28 10:10:40 +0000

    Fix client id reset issue
    There could be 2 cause of the high churns of client id reset.
    
    1. Pending value is not cleared for managed device.
    2. There is a brief moment for every Chrome start that the
    OwnershipStatus is UNKNOWN.
    
    Because of above, pending value could be re-used after OOBE in IsEnabled()
    which results in reset of client ids.
    
    Both are fixed in this CL. Pending value is cleared for managed device
    after ownership is taken; in UNKNOWN state, value from signed store is
    read instead of pending value.
    
    Bug: b/178696047
    Change-Id: If16f563ead2b6b3f226ebd28ef3402cc92a2320b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853353
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876977}

--- chrome/browser/ash/settings/stats_reporting_controller_unittest.cc
+++ chrome/browser/ash/settings/stats_reporting_controller_unittest.cc
@@ -123,8 +123,10 @@ TEST_F(StatsReportingControllerTest, GetAndSet_OwnershipUnknown) {
 
   std::unique_ptr<TestingProfile> user = CreateUser(no_keys);
   StatsReportingController::Get()->SetEnabled(user.get(), true);
-  // Read from the pending value when ownership is not taken.
-  EXPECT_TRUE(StatsReportingController::Get()->IsEnabled());
+  // A pending value is written in case there is no owner, in which case it will
+  // be written properly when ownership is taken - but we don't read the
+  // pending value, in case there actually is an owner already.
+  EXPECT_FALSE(StatsReportingController::Get()->IsEnabled());
   ExpectThatPendingValueIs(true);
   ExpectThatSignedStoredValueIs(false);
 
@@ -231,8 +233,8 @@ TEST_F(StatsReportingControllerTest, SetBeforeOwnershipTaken) {
   // Before device is owned, setting the value means writing a pending value:
   std::unique_ptr<TestingProfile> pre_ownership_user = CreateUser(no_keys);
   StatsReportingController::Get()->SetEnabled(pre_ownership_user.get(), true);
-  EXPECT_TRUE(StatsReportingController::Get()->IsEnabled());
-  EXPECT_TRUE(value_at_last_notification_);
+  EXPECT_FALSE(StatsReportingController::Get()->IsEnabled());
+  EXPECT_FALSE(value_at_last_notification_);
   ExpectThatPendingValueIs(true);
   ExpectThatSignedStoredValueIs(false);
 
