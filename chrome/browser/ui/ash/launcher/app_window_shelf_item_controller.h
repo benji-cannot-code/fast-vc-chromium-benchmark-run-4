ad61e5f6ad90c8f9b5ec4f2414f234469c481ab5
Sigurdur Asgeirsson <siggi@chromium.org>
2021-04-28 21:36:12 +0000

    Retire ScopedObserver in /chrome/browser/ui/ash/launcher.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=khmel@chromium.org
    
    Bug: 1145565
    Change-Id: I6c1556d2bd5240334589f0e74b076b76274b9e29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2857015
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877244}

--- chrome/browser/ui/ash/launcher/app_window_shelf_item_controller.h
+++ chrome/browser/ui/ash/launcher/app_window_shelf_item_controller.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/shelf_item_delegate.h"
 #include "base/macros.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_multi_source_observation.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
 
@@ -101,7 +101,8 @@ class AppWindowShelfItemController : public ash::ShelfItemDelegate,
   AppWindowBase* last_active_window_ = nullptr;
 
   // Scoped list of observed windows (for removal on destruction)
-  ScopedObserver<aura::Window, aura::WindowObserver> observed_windows_{this};
+  base::ScopedMultiSourceObservation<aura::Window, aura::WindowObserver>
+      observed_windows_{this};
 
   std::unique_ptr<ShelfContextMenu> context_menu_;
 
