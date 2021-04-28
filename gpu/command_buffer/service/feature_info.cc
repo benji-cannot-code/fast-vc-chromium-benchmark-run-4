ec8c1df1f99145fc021193ae912e54ba8400178c
Peng Huang <penghuang@chromium.org>
2021-04-28 22:02:43 +0000

    Remove TextureDefinition which is not used anywhere.
    Bug: none
    Change-Id: Iff20b0df5462c82b23bfa4bd203ad9c0143e05a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2856736
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877256}

--- gpu/command_buffer/service/feature_info.cc
+++ gpu/command_buffer/service/feature_info.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromecast_buildflags.h"
 #include "build/chromeos_buildflags.h"
 #include "gpu/command_buffer/service/gpu_switches.h"
-#include "gpu/command_buffer/service/texture_definition.h"
+#include "gpu/command_buffer/service/native_image_buffer.h"
 #include "gpu/config/gpu_switches.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_fence.h"
@@ -1576,10 +1576,6 @@ void FeatureInfo::InitializeFeatures() {
     validators_.src_blend_factor.AddValue(GL_ONE_MINUS_SRC1_ALPHA_EXT);
     validators_.dst_blend_factor.AddValue(GL_ONE_MINUS_SRC1_ALPHA_EXT);
     validators_.g_l_state.AddValue(GL_MAX_DUAL_SOURCE_DRAW_BUFFERS_EXT);
-  }
-
-  if (workarounds_.avoid_egl_image_target_texture_reuse) {
-    TextureDefinition::AvoidEGLTargetTextureReuse();
   }
 
   if (gl_version_info_->IsLowerThanGL(4, 3)) {
--- gpu/command_buffer/service/shared_image_backing_egl_image.cc
+++ gpu/command_buffer/service/shared_image_backing_egl_image.cc
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/command_buffer/service/shared_image_backing_egl_image.h"
 
+#include "gpu/command_buffer/service/native_image_buffer.h"
 #include "gpu/command_buffer/service/shared_context_state.h"
 #include "gpu/command_buffer/service/shared_image_batch_access_manager.h"
 #include "gpu/command_buffer/service/shared_image_representation.h"
 #include "gpu/command_buffer/service/shared_image_representation_skia_gl.h"
-#include "gpu/command_buffer/service/texture_definition.h"
 #include "gpu/command_buffer/service/texture_manager.h"
 #include "ui/gl/gl_fence_egl.h"
 #include "ui/gl/gl_utils.h"
