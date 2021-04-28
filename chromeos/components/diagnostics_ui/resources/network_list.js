b9fbef245c24f8655c07c7ad21723f7412dc76d5
Michael Checo <michaelcheco@google.com>
2021-04-28 19:00:38 +0000

    Diagnostics: Move connectivity card into network list
    - Moves connectivity_card into network-list
    - Updates network-list test to check for connectivity_card
    
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: Ic2989e13336cc1c774a14c0e2d2a814889fad1d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2843934
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/master@{#877149}

--- chromeos/components/diagnostics_ui/resources/network_list.js
+++ chromeos/components/diagnostics_ui/resources/network_list.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './connectivity_card.js';
 import './diagnostics_fonts_css.js';
 import './diagnostics_shared_css.js';
 
@@ -18,4 +19,12 @@ Polymer({
 
   _template: html`{__html_template__}`,
 
+  properties: {
+    /** @type {boolean} */
+    isTestRunning: {
+      type: Boolean,
+      value: false,
+      notify: true,
+    },
+  },
 });
