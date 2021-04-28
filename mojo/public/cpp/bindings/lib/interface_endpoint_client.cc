426e0bf01de8144d46561e1b1edeaa221a48a293
Ken Rockot <rockot@google.com>
2021-04-28 01:25:25 +0000

    Fix data race in off-thread SharedRemote waits
    This fixes a data race in how SharedRemote implements off-thread sync
    IPC waits. There's no need to use SyncResponseInfo state (which may
    be shared by multiple threads) in order to signal completion of the
    wait. Instead, always use a local stack variable that will only be
    tested (by SyncHandleRegistry::Wait) and set (by the local
    SyncEventWatcher's callback) on a single thread.
    
    The data race is harmless in production but causes lots of test flake in
    TSAN builds.
    
    Fixed: 1203262
    Change-Id: I3a4bb0ed0b137b02dca9afa05573ec700b3766bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854388
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#876860}

--- mojo/public/cpp/bindings/lib/interface_endpoint_client.cc
+++ mojo/public/cpp/bindings/lib/interface_endpoint_client.cc
@@ -81,7 +81,6 @@ class ThreadSafeInterfaceEndpointClientProxy : public ThreadSafeProxy {
     SyncResponseInfo() = default;
 
     Message message;
-    bool cancelled = false;
     bool received = false;
     base::WaitableEvent event{base::WaitableEvent::ResetPolicy::MANUAL,
                               base::WaitableEvent::InitialState::NOT_SIGNALED};
@@ -102,10 +101,8 @@ class ThreadSafeInterfaceEndpointClientProxy : public ThreadSafeProxy {
     ~SyncResponseSignaler() override {
       // If Accept() was not called we must still notify the waiter that the
       // sync call is finished.
-      if (response_) {
-        response_->cancelled = true;
+      if (response_)
         response_->event.Signal();
-      }
     }
 
     bool Accept(Message* message) override {
@@ -389,14 +386,16 @@ void ThreadSafeInterfaceEndpointClientProxy::SendMessageWithResponder(
     // In the common case where interrupts are allowed, we watch cooperatively
     // with other potential endpoints on the same thread.
     SyncCallRestrictions::AssertSyncCallAllowed();
-    SyncEventWatcher watcher(&response->event, base::DoNothing());
-    const bool* stop_flags[] = {&response->received, &response->cancelled};
+    bool signaled = false;
+    auto set_flag = [](bool* flag) { *flag = true; };
+    SyncEventWatcher watcher(&response->event,
+                             base::BindRepeating(set_flag, &signaled));
+    const bool* stop_flags[] = {&signaled};
     watcher.SyncWatch(stop_flags, base::size(stop_flags));
   } else {
     // Else we can wait on the event directly. It will only signal after our
     // reply has been processed or cancelled.
     response->event.Wait();
-    DCHECK(response->received || response->cancelled);
   }
 
   {
