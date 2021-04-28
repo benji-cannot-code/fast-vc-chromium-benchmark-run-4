6d218e90a59b2d21a0c988ccbd77dd1364368a46
Sven Zheng <svenzheng@google.com>
2021-04-28 01:02:23 +0000

    Gtest gold test update ignore key
    The gold binary updated and the ignore key now need to be
    non optional.
    
    Change-Id: Ia77bae22e6656f41f5df870c77314cbbeff5684a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2855780
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Auto-Submit: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876856}

--- ui/base/test/skia_gold_pixel_diff_unittest.cc
+++ ui/base/test/skia_gold_pixel_diff_unittest.cc
@@ -282,11 +282,10 @@ TEST_F(SkiaGoldPixelDiffTest, DoNotMakeGerritCommentHasRetryLeft) {
 
   MockSkiaGoldPixelDiff mock_pixel;
   EXPECT_CALL(mock_pixel, LaunchProcess(_)).Times(AnyNumber());
-  EXPECT_CALL(
-      mock_pixel,
-      LaunchProcess(AllOf(Property(
-          &base::CommandLine::GetCommandLineString,
-          HasSubstr(FILE_PATH_LITERAL("--add-test-optional-key=ignore:1"))))))
+  EXPECT_CALL(mock_pixel,
+              LaunchProcess(AllOf(Property(
+                  &base::CommandLine::GetCommandLineString,
+                  HasSubstr(FILE_PATH_LITERAL("--add-test-key=ignore:1"))))))
       .Times(1);
   mock_pixel.Init("Prefix");
   bool ret = mock_pixel.CompareScreenshot("test", GetTestBitmap());
@@ -301,9 +300,9 @@ TEST_F(SkiaGoldPixelDiffTest, DoNotMakeGerritCommentForCIJob) {
   EXPECT_CALL(mock_pixel, LaunchProcess(_)).Times(AnyNumber());
   EXPECT_CALL(
       mock_pixel,
-      LaunchProcess(AllOf(Property(&base::CommandLine::GetCommandLineString,
-                                   Not(HasSubstr(FILE_PATH_LITERAL(
-                                       "--add-test-optional-key=ignore:1")))))))
+      LaunchProcess(AllOf(Property(
+          &base::CommandLine::GetCommandLineString,
+          Not(HasSubstr(FILE_PATH_LITERAL("--add-test-key=ignore:1")))))))
       .Times(3);
   mock_pixel.Init("Prefix");
   bool ret = mock_pixel.CompareScreenshot("test", GetTestBitmap());
@@ -321,9 +320,9 @@ TEST_F(SkiaGoldPixelDiffTest, MakeGerritCommentNoRetryLeft) {
   EXPECT_CALL(mock_pixel, LaunchProcess(_)).Times(AnyNumber());
   EXPECT_CALL(
       mock_pixel,
-      LaunchProcess(AllOf(Property(&base::CommandLine::GetCommandLineString,
-                                   Not(HasSubstr(FILE_PATH_LITERAL(
-                                       "--add-test-optional-key=ignore:1")))))))
+      LaunchProcess(AllOf(Property(
+          &base::CommandLine::GetCommandLineString,
+          Not(HasSubstr(FILE_PATH_LITERAL("--add-test-key=ignore:1")))))))
       .Times(3);
   mock_pixel.Init("Prefix");
   bool ret = mock_pixel.CompareScreenshot("test", GetTestBitmap());
@@ -341,9 +340,9 @@ TEST_F(SkiaGoldPixelDiffTest, MakeGerritCommentInvalidFlag) {
   EXPECT_CALL(mock_pixel, LaunchProcess(_)).Times(AnyNumber());
   EXPECT_CALL(
       mock_pixel,
-      LaunchProcess(AllOf(Property(&base::CommandLine::GetCommandLineString,
-                                   Not(HasSubstr(FILE_PATH_LITERAL(
-                                       "--add-test-optional-key=ignore:1")))))))
+      LaunchProcess(AllOf(Property(
+          &base::CommandLine::GetCommandLineString,
+          Not(HasSubstr(FILE_PATH_LITERAL("--add-test-key=ignore:1")))))))
       .Times(3);
   mock_pixel.Init("Prefix");
   bool ret = mock_pixel.CompareScreenshot("test", GetTestBitmap());
