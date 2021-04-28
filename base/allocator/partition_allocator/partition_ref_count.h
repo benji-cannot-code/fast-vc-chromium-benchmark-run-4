1fa6cf04c444963baf27e7745aaab93b28e0fe4b
Bartek Nowierski <bartekn@chromium.org>
2021-04-28 12:43:24 +0000

    [BRP] Include partition_alloc_check.h from partition_ref_count.h
    The file uses PA_(D)CHECK and breaks when built with
    USE_BACKUP_REF_PTR. crrev.com/c/2821689 removed partition_cookie.h,
    which transitively included partition_alloc_check.h.
    
    Change-Id: I81b12e07437f7801ba1ec9fadb6b70137daba6cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853917
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877009}

--- base/allocator/partition_allocator/partition_ref_count.h
+++ base/allocator/partition_allocator/partition_ref_count.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <atomic>
 #include <cstdint>
 
+#include "base/allocator/partition_allocator/partition_alloc_check.h"
 #include "base/allocator/partition_allocator/partition_alloc_constants.h"
 #include "base/allocator/partition_allocator/partition_alloc_forward.h"
 #include "base/base_export.h"
