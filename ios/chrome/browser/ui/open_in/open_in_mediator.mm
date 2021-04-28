39b573752f6070386abdf3b984fd8f3b7eeb2989
Sylvain Defresne <sdefresne@chromium.org>
2021-04-28 16:08:18 +0000

    [ios] Simplify OpenInController implementation
    Use free functions to implement the file manipulation operations
    instead of methods / static methods which is easier to call from
    blocks / callbacks without inadvertently capturing state.
    
    Remove OpenInControllerBridge which is not really needed. It is
    simpler to just use a weak pointer to OpenInController captured
    by a block.
    
    Add a boolean to record the fact that -disable was called. This
    is needed to prevent executing the code in the block mentionned
    above when the object has been disabled but not yet deallocated
    (e.g. it could be in an autorelease pool).
    
    Use a sequence checker to check callbacks are executed on the
    correct sequence instead of checking for a named thread (this
    makes code easier to write and test).
    
    Ensure that OpenInController drop its pointer to Browser and to
    WebState when disconnected from the UI or when the WebState is
    destroyed.
    
    Bug: none
    Change-Id: I61d7f7b31704685364c0f747517d3f0ee7dee465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854920
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877074}

--- ios/chrome/browser/ui/open_in/open_in_mediator.mm
+++ ios/chrome/browser/ui/open_in/open_in_mediator.mm
@@ -62,7 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)disableAll {
   for (const auto& element : _openInControllersForWebStates)
-    [element.second disable];
+    [element.second detachFromWebState];
   _openInControllersForWebStates.clear();
 }
 
@@ -97,8 +97,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         initWithBaseViewController:_baseViewController
                   URLLoaderFactory:webState->GetBrowserState()
                                        ->GetSharedURLLoaderFactory()
-                          webState:webState];
-    openInController.browser = _browser;
+                          webState:webState
+                           browser:_browser];
     _openInControllersForWebStates[webState] = openInController;
   }
   OpenInController* controller = _openInControllersForWebStates[webState];
