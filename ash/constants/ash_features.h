4319bc2475ad235ca0e0cb428245e97eaf89850f
Ghazale Hosseinabadi <ghazale@chromium.org>
2021-04-28 18:52:38 +0000

    Revert "[CrOS] Remove login device management disclosure feature"
    This reverts commit 0d760a687b4736deb3110eab6cc5f3017c6027cd.
    
    Reason for revert: Deterministic Linux failure
    
    Original change's description:
    > [CrOS] Remove login device management disclosure feature
    >
    > Bug: 1182595
    > Change-Id: Ie1c37176d384981e46c183cf3b7d713a4cecd808
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854775
    > Auto-Submit: Thomas Tellier <tellier@google.com>
    > Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#877054}
    
    Bug: 1182595
    Change-Id: I1bb32649df03c233502b08c3eeeb86f6a57644b4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854893
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Auto-Submit: Ghazale Hosseinabadi <ghazale@chromium.org>
    Reviewed-by: Thomas Tellier <tellier@google.com>
    Reviewed-by: Salvador Guerrero <salg@google.com>
    Owners-Override: Ghazale Hosseinabadi <ghazale@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877143}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -210,6 +210,8 @@ COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kLacrosPrimary;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kLanguageSettingsUpdate2;
 COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const base::Feature kLoginDeviceManagementDisclosure;
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kMediaAppAnnotation;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kMediaAppDisplayExif;
@@ -363,6 +365,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS) bool IsImeSandboxEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsInstantTetheringBackgroundAdvertisingSupported();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsKerberosSettingsSectionEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLoginDeviceManagementDisclosureEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsMinimumChromeVersionEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsNetworkingInDiagnosticsAppEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsNewOobeLayoutEnabled();
--- ash/login/ui/lock_contents_view_unittest.cc
+++ ash/login/ui/lock_contents_view_unittest.cc
@@ -821,8 +821,22 @@ TEST_F(LockContentsViewUnitTest, ShowStatusIndicatorIfAdbSideloadingEnabled) {
   EXPECT_TRUE(test_api.bottom_status_indicator()->GetVisible());
 }
 
+class LockContentsViewUnitTestWithDeviceDisclosureEnabled
+    : public LockContentsViewUnitTest {
+ public:
+  LockContentsViewUnitTestWithDeviceDisclosureEnabled()
+      : LockContentsViewUnitTest() {
+    feature_list_.InitWithFeatures(
+        {chromeos::features::kLoginDeviceManagementDisclosure}, {});
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
 // Show bottom status indicator if device is enrolled
-TEST_F(LockContentsViewUnitTest, ShowStatusIndicatorIfEnrolledDevice) {
+TEST_F(LockContentsViewUnitTestWithDeviceDisclosureEnabled,
+       ShowStatusIndicatorIfEnrolledDevice) {
   // If the device is enrolled, bottom_status_indicator should be visible.
   Shell::Get()->system_tray_model()->SetEnterpriseDomainInfo("BestCompanyEver",
                                                              false);
@@ -847,7 +861,8 @@ TEST_F(LockContentsViewUnitTest, ShowStatusIndicatorIfEnrolledDevice) {
 }
 
 // Show bottom status indicator if device is enrolled
-TEST_F(LockContentsViewUnitTest, ShowManagementBubbleOnClickIfEnrolledDevice) {
+TEST_F(LockContentsViewUnitTestWithDeviceDisclosureEnabled,
+       ShowManagementBubbleOnClickIfEnrolledDevice) {
   // If the device is enrolled, bottom_status_indicator should be visible.
   Shell::Get()->system_tray_model()->SetEnterpriseDomainInfo("BestCompanyEver",
                                                              false);
@@ -882,7 +897,8 @@ TEST_F(LockContentsViewUnitTest, ShowManagementBubbleOnClickIfEnrolledDevice) {
 
 // Do not show the management bubble on click if ADB sideloading is enabled and
 // device is enrolled.
-TEST_F(LockContentsViewUnitTest, DoNotShowManagementBubbleOnClickIfAdb) {
+TEST_F(LockContentsViewUnitTestWithDeviceDisclosureEnabled,
+       DoNotShowManagementBubbleOnClickIfAdb) {
   // If the device is enrolled, bottom_status_indicator should be visible.
   Shell::Get()->system_tray_model()->SetEnterpriseDomainInfo("BestCompanyEver",
                                                              false);
