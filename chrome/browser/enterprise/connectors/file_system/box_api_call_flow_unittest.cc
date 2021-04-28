7c443422dd2ed029505e60862f8e5e6c8503ddc2
Alice Gong <alicego@google.com>
2021-04-28 00:06:28 +0000

    Part 5.5/5.6 Add BoxChunkedFileUploader::FileChunksHandler.
    Use base::SequenceBound<base::File> and SHA-1 functions in //base/hash/sha1.h to  read and prepare file content chunk by chunk for chunked uploads to Box.
    
    BUG=1192671
    
    Change-Id: I7234e02a51dc6bd38b335673e12ed99673137e25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2786771
    Commit-Queue: Alice Gong <alicego@google.com>
    Auto-Submit: Alice Gong <alicego@google.com>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876833}

--- chrome/browser/enterprise/connectors/file_system/box_api_call_flow_unittest.cc
+++ chrome/browser/enterprise/connectors/file_system/box_api_call_flow_unittest.cc
@@ -753,8 +753,6 @@ class BoxPartFileUploadApiCallFlowTest
   void OnResponse(bool success, int response_code, base::Value) {
     processed_success_ = success;
     response_code_ = response_code;
-    if (quit_closure_)
-      std::move(quit_closure_).Run();
   }
 
   const std::string file_content_;
@@ -763,7 +761,6 @@ class BoxPartFileUploadApiCallFlowTest
 
   base::test::SingleThreadTaskEnvironment task_environment_;
   data_decoder::test::InProcessDataDecoder decoder_;
-  base::OnceClosure quit_closure_;
   base::WeakPtrFactory<BoxPartFileUploadApiCallFlowTest> factory_{this};
 };
 
@@ -881,7 +878,6 @@ class BoxAbortUploadSessionApiCallFlowTest
   }
 
   base::test::SingleThreadTaskEnvironment task_environment_;
-  base::OnceClosure quit_closure_;
   base::WeakPtrFactory<BoxAbortUploadSessionApiCallFlowTest> factory_{this};
 };
 
