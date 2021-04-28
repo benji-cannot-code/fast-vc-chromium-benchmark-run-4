30fd5667ee6c6c96c7d0cec558ac5bde3e038003
Mikel Astiz <mastiz@chromium.org>
2021-04-28 11:02:39 +0000

    [signin] Add browser tests for Google-Accounts-RemoveLocalAccount
    This patch extends FakeGaia with a page that browser tests can visit to
    trigger the HTTP header Google-Accounts-RemoveLocalAccount.
    
    Browser tests are added to leverage this functionality and verify that
    IdentityManager notifies observers appropriately via
    OnAccountsInCookieUpdated().
    
    Change-Id: I9a6ea41cecabbc094f2155d3b6df61ec74941525
    Bug: 1078762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2842011
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876994}

--- google_apis/gaia/fake_gaia.h
+++ google_apis/gaia/fake_gaia.h
@@ -86,6 +86,9 @@ class FakeGaia {
 
     // The e-mail address returned by /ListAccounts.
     std::string email;
+
+    // List of signed out gaia IDs returned by /ListAccounts.
+    std::vector<std::string> signed_out_gaia_ids;
   };
 
   struct SyncTrustedVaultKeys {
@@ -195,6 +198,10 @@ class FakeGaia {
   // Returns the is_device_owner param from the reauth URL if any.
   const std::string& is_device_owner() { return is_device_owner_; }
 
+  // Returns the fake server's URL that browser tests can visit to trigger a
+  // RemoveLocalAccount event.
+  GURL GetDummyRemoveLocalAccountURL(const std::string& gaia_id) const;
+
  protected:
   // HTTP handler for /MergeSession.
   virtual void HandleMergeSession(
@@ -210,6 +217,7 @@ class FakeGaia {
       std::map<std::string, SyncTrustedVaultKeys>;
 
   std::string GetGaiaIdOfEmail(const std::string& email) const;
+  std::string GetEmailOfGaiaId(const std::string& email) const;
 
   void AddGoogleAccountsSigninHeader(
       net::test_server::BasicHttpResponse* http_response,
@@ -299,6 +307,9 @@ class FakeGaia {
   // HTTP handler for /OAuth/Multilogin.
   void HandleMultilogin(const net::test_server::HttpRequest& request,
                         net::test_server::BasicHttpResponse* http_response);
+  void HandleDummyRemoveLocalAccount(
+      const net::test_server::HttpRequest& request,
+      net::test_server::BasicHttpResponse* http_response);
 
   // Returns the access token associated with |auth_token| that matches the
   // given |client_id| and |scope_string|. If |scope_string| is empty, the first
