afa47ddd4d849dd6cea9d8c06c6bbf7c136465d6
Ramin Halavati <rhalavati@chromium.org>
2021-04-28 12:43:14 +0000

    Update IsRegularProfile() to return true ONLY for regular profiles.
    Profile::IsRegularProfile was previously defined as !IsOffTheRecord.
    To make profile type functions simpler, this CL changes it to only cover
    actual regular profiles (not system, Incognito, or Guest).
    
    Use cases which needed update after this change are also updated.
    
    Bug: 1142370
    Change-Id: I348e0d4fe5d20cf57c01ac43e90e4c70bc7caa69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2851610
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877008}

--- chrome/browser/net/profile_network_context_service.cc
+++ chrome/browser/net/profile_network_context_service.cc
@@ -140,7 +140,7 @@ network::mojom::AdditionalCertificatesPtr GetAdditionalCertificates(
 // Tests allowing ambient authentication with default credentials based on the
 // profile type.
 bool IsAmbientAuthAllowedForProfile(Profile* profile) {
-  if (profile->IsRegularProfile() && !profile->IsEphemeralGuestProfile())
+  if (profile->IsRegularProfile())
     return true;
 
   // Non-primary OTR profiles are not used to create browser windows and are
