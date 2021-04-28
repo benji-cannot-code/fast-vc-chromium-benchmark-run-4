6c3e4c185fc68d7fe04227bd801ceecf5daaa047
Maria Kazinova <kazinova@google.com>
2021-04-28 10:21:22 +0000

    Reland "Fix crash in PasswordManager::SetGenerationElementAndTypeForForm."
    This is a reland of 5e71e1d3c5404bca2130ec263ff9ed2866a1467a
    
    Original change's description:
    > Fix crash in PasswordManager::SetGenerationElementAndTypeForForm.
    >
    > Fixing a crash caused by triggering manual generation when the saving
    > an filling is not enabled for the current origin.
    >
    > Bug: 1201732
    > Change-Id: Ie4d8c5df0a94085446ceb099948d1fcb280101ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2847439
    > Commit-Queue: Maria Kazinova <kazinova@google.com>
    > Reviewed-by: Javier Flores <javierrobles@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#876491}
    
    Bug: 1201732
    Change-Id: I71e64aa986a77d0b07d05f1baa58ee0f7cdc7ea5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854635
    Auto-Submit: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Javier Flores <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#876978}

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_password_mediator.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_password_mediator.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/ios/browser/autofill_util.h"
 #import "components/autofill/ios/form_util/form_activity_observer_bridge.h"
 #include "components/autofill/ios/form_util/form_activity_params.h"
+#include "components/password_manager/core/browser/password_manager_client.h"
 #include "components/password_manager/core/browser/password_store.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #import "components/password_manager/ios/password_generation_provider.h"
@@ -101,6 +102,9 @@ BOOL AreCredentialsAtIndexesConnected(
   // Bridge to observe form activity in |_webState|.
   std::unique_ptr<autofill::FormActivityObserverBridge>
       _formActivityObserverBridge;
+
+  // Origin to fetch passwords for.
+  GURL _URL;
 }
 
 - (instancetype)initWithPasswordStore:
@@ -109,6 +113,7 @@ BOOL AreCredentialsAtIndexesConnected(
                         faviconLoader:(FaviconLoader*)faviconLoader
                              webState:(web::WebState*)webState
                           syncService:(SyncSetupService*)syncService
+                                  URL:(const GURL&)URL
                invokedOnPasswordField:(BOOL)invokedOnPasswordField {
   self = [super init];
   if (self) {
@@ -117,6 +122,7 @@ BOOL AreCredentialsAtIndexesConnected(
     _faviconLoader = faviconLoader;
     _webState = webState;
     _syncService = syncService;
+    _URL = URL;
     _activeFieldIsPassword = invokedOnPasswordField;
     _webStateObserverBridge =
         std::make_unique<web::WebStateObserverBridge>(self);
@@ -133,12 +139,12 @@ BOOL AreCredentialsAtIndexesConnected(
   }
 }
 
-- (void)fetchPasswordsForURL:(const GURL&)URL {
+- (void)fetchPasswords {
   self.credentials = @[];
   self.passwordFetcher =
       [[PasswordFetcher alloc] initWithPasswordStore:_passwordStore
                                             delegate:self
-                                                 URL:URL];
+                                                 URL:_URL];
 }
 
 #pragma mark - PasswordFetcherDelegate
@@ -232,9 +238,15 @@ BOOL AreCredentialsAtIndexesConnected(
         [[NSMutableArray alloc] init];
     __weak __typeof(self) weakSelf = self;
 
+    password_manager::PasswordManagerClient* passwordManagerClient =
+        _webState ? PasswordTabHelper::FromWebState(_webState)
+                        ->GetPasswordManagerClient()
+                  : nullptr;
     if (base::FeatureList::IsEnabled(
             password_manager::features::kEnableManualPasswordGeneration) &&
         _syncService && _syncService->IsSyncEnabled() &&
+        passwordManagerClient &&
+        passwordManagerClient->IsSavingAndFillingEnabled(_URL) &&
         _activeFieldIsPassword) {
       NSString* suggestPasswordTitleString = l10n_util::GetNSString(
           IDS_IOS_MANUAL_FALLBACK_SUGGEST_PASSWORD_WITH_DOTS);
