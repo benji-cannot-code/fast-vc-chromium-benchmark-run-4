283e1760d2a463250b4aa8cae7263bff065040bf
Zufeng Wang <zufeng@google.com>
2021-04-29 07:06:31 +0000

    Help app: set delegate on window
    Why: The existing logic looks for the <showoff-app> element and sets the
    delegate on that. But the help app's background page won't have a
    showoff-app element.
    
    So this change sets it on the window object as well. Then the background
    page can access it.
    
    Note the similarity with window.customLaunchData in
    chromeos/components/media_app_ui/resources/js/media_app.externs.js
    
    Bug: b/186181384
    Change-Id: I8d6d31f006bea5802c7290990455a56dd3a18e31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2858159
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Zufeng Wang <zufeng@google.com>
    Cr-Commit-Position: refs/heads/master@{#877382}

--- chromeos/components/help_app_ui/resources/receiver.js
+++ chromeos/components/help_app_ui/resources/receiver.js
@@ -58,6 +58,10 @@ const DELEGATE = {
   },
 };
 
+window.customLaunchData = {
+  delegate: DELEGATE,
+};
+
 /**
  * Returns the help app if it can find it in the DOM.
  * @return {?helpApp.ClientApi}
