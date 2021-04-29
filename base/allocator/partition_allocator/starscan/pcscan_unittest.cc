a5f3277ed835d0b1e6090d6dd19e74aeeee340a8
Anton Bikineev <bikineev@chromium.org>
2021-04-29 19:17:22 +0000

    PCScan: Implement stack scanning from safepoints
    The CL does the following things:
    
    1) Copies over the stack scanning infra from V8;
    2) Adds PCScan specific StackVisitor;
    3) Bails out on reentrancy. This is needed because system functions that
       retrieves the top of the stack can allocate;
    4) Introduces DisableStackScanningScope to allow testing.
    
    Bug: 1129751
    Change-Id: Ib69bd6845456f40df42018a8767beb909c1410ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2831600
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877589}

--- base/allocator/partition_allocator/starscan/pcscan_unittest.cc
+++ base/allocator/partition_allocator/starscan/pcscan_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/allocator/partition_allocator/partition_alloc.h"
 #include "base/allocator/partition_allocator/partition_alloc_features.h"
+#include "base/allocator/partition_allocator/starscan/stack/stack.h"
 #include "base/logging.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -18,6 +19,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 namespace internal {
+
+namespace {
+struct DisableStackScanningScope final {
+  DisableStackScanningScope() {
+    if (PCScan::IsStackScanningEnabled()) {
+      PCScan::DisableStackScanning();
+      changed_ = true;
+    }
+  }
+  ~DisableStackScanningScope() {
+    if (changed_)
+      PCScan::EnableStackScanning();
+  }
+
+ private:
+  bool changed_ = false;
+};
+}  // namespace
 
 class PCScanTest : public testing::Test {
  public:
@@ -478,6 +497,8 @@ TEST_F(PCScanTest, Safepoint) {
   using SourceList = List<64>;
   using ValueList = SourceList;
 
+  DisableStackScanningScope no_stack_scanning;
+
   auto* source = SourceList::Create(root());
   auto* value = ValueList::Create(root());
   source->next = value;
@@ -485,6 +506,50 @@ TEST_F(PCScanTest, Safepoint) {
   TestDanglingReferenceWithSafepoint(*this, source, value);
 }
 #endif  // PCSCAN_DISABLE_SAFEPOINTS
+
+TEST_F(PCScanTest, StackScanning) {
+  using ValueList = List<8>;
+
+  PCScan::EnableStackScanning();
+
+  static void* dangling_reference = nullptr;
+  // Set to nullptr if the test is retried.
+  dangling_reference = nullptr;
+
+  // Create and set dangling reference in the global.
+  [this]() NOINLINE {
+    auto* value = ValueList::Create(root(), nullptr);
+    ValueList::Destroy(root(), value);
+    dangling_reference = value;
+  }();
+
+  [this]() NOINLINE {
+    // Register the top of the stack to be the current pointer.
+    PCScan::NotifyThreadCreated(GetStackPointer());
+    [this]() NOINLINE {
+      // This writes the pointer to the stack.
+      auto* volatile stack_ref = dangling_reference;
+      ALLOW_UNUSED_LOCAL(stack_ref);
+      [this]() NOINLINE {
+        // Schedule PCScan but don't scan.
+        SchedulePCScan();
+        // Enter safepoint and scan from mutator. This will scan the stack.
+        JoinPCScanAsMutator();
+        // Check that the object is still quarantined since it's referenced by
+        // |dangling_reference|.
+        EXPECT_TRUE(IsInQuarantine(dangling_reference));
+        // Check that value is not in the freelist.
+        EXPECT_FALSE(IsInFreeList(
+            root().AdjustPointerForExtrasSubtract(dangling_reference)));
+        // Run sweeper.
+        FinishPCScanAsScanner();
+        // Check that |dangling_reference| still exists.
+        EXPECT_FALSE(IsInFreeList(
+            root().AdjustPointerForExtrasSubtract(dangling_reference)));
+      }();
+    }();
+  }();
+}
 
 }  // namespace internal
 }  // namespace base
