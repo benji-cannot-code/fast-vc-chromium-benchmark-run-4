3af282f3d59db290de2af7c1013e7fc92a5d278b
James Forshaw <forshaw@chromium.org>
2021-04-29 23:29:10 +0000

    [Windows] Add utilities for manipulating SIDs and file security.
    This CL adds a Sid class to base which can be used to create a SID
    based on an SDDL string or a well known type or capability. It also
    adds a utility function to grant a list of SIDs access to a file path.
    
    Follow up CLs will remove duplicate code from the installer and sandbox.
    
    Bug: 1191111
    Change-Id: Icd0a2db96d08d6dafb05ac7e7d5ef46a88e561ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2857397
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877700}

---
+++ base/win/scoped_localalloc.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_WIN_SCOPED_LOCALALLOC_H_
+#define BASE_WIN_SCOPED_LOCALALLOC_H_
+
+#include <memory>
+#include <utility>
+
+#include "base/win/windows_types.h"
+
+namespace base {
+namespace win {
+
+// unique_ptr deleter for LocalAlloc memory.
+struct LocalAllocDeleter {
+  void operator()(void* ptr) const { ::LocalFree(ptr); }
+};
+
+template <typename T>
+using ScopedLocalAllocTyped = std::unique_ptr<T, LocalAllocDeleter>;
+
+using ScopedLocalAlloc = ScopedLocalAllocTyped<void>;
+
+// Make a typed ScopedLocalAlloc class and clear the original pointer.
+template <typename T>
+ScopedLocalAllocTyped<T> TakeLocalAlloc(T*& ptr) {
+  return ScopedLocalAllocTyped<T>(std::exchange(ptr, nullptr));
+}
+
+}  // namespace win
+}  // namespace base
+
+#endif  // BASE_WIN_SCOPED_LOCALALLOC_H_
