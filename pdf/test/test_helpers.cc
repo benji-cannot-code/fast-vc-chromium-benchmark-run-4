6514015bdb9469da8007cbe6bfa43c39cbc9c7ee
Hui Yingst <nigi@chromium.org>
2021-04-29 00:24:03 +0000

    [unseasoned-pdf] Make CreateSourceImage() more useful.
    Modify the existing helper CreateSourceImage() so that it can be
    reused for more unit tests in //pdf/.
    
    - Move CreateSourceImage() into separate files
      //pdf/test/test_helpers.*.
    
    - Change the parameter type from SkISize to gfx::Size to avoid
      implementation-specific types.
    
    - Make CreateSourceImage() accept an extra SkColor parameter so that
      it can create an `Image` with any given color.
    
    - Rename CreateSourceImage() to CreateSkiaImageForTesting() since it
      creates an `Image` in Skia format and it's only used for tests.
    
    Bug: 1199558
    Change-Id: Iad6e8b2f9d7fdc9ff9ab8f2e5a0fa4522acc95d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853839
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877309}

---
+++ pdf/test/test_helpers.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "pdf/test/test_helpers.h"
+
+#include "pdf/ppapi_migration/bitmap.h"
+#include "pdf/ppapi_migration/image.h"
+#include "third_party/skia/include/core/SkBitmap.h"
+#include "third_party/skia/include/core/SkColor.h"
+#include "ui/gfx/skia_util.h"
+
+namespace chrome_pdf {
+
+Image CreateSkiaImageForTesting(const gfx::Size& size, SkColor color) {
+  SkBitmap bitmap = CreateN32PremulSkBitmap(gfx::SizeToSkISize(size));
+  bitmap.eraseColor(color);
+  return Image(bitmap);
+}
+
+}  // namespace chrome_pdf
