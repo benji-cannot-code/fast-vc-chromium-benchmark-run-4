4a1b1d9d74d070982a907a474c2b996fc812aad0
Nancy Wang <nancylingwang@chromium.org>
2021-04-29 00:32:31 +0000

    Add AppPlatformAppMetrics to record the installed app count.
    BUG=1200185
    
    Change-Id: Iddb17ee3ba6ed983aec52c89dc61bc555a6445c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2850234
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877316}

--- chrome/browser/apps/app_service/app_platform_metrics.h
+++ chrome/browser/apps/app_service/app_platform_metrics.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_APPS_APP_SERVICE_APP_PLATFORM_METRICS_H_
 #define CHROME_BROWSER_APPS_APP_SERVICE_APP_PLATFORM_METRICS_H_
 
+#include "components/services/app_service/public/cpp/app_registry_cache.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 
 class Profile;
@@ -14,11 +15,66 @@ namespace apps {
 
 class AppUpdate;
 
+// This is used for logging, so do not remove or reorder existing entries.
+enum class AppTypeName {
+  kUnknown = 0,
+  kArc = 1,
+  kBuiltIn = 2,
+  kCrostini = 3,
+  kChromeApp = 4,
+  kWeb = 5,
+  kMacOs = 6,
+  kPluginVm = 7,
+  kStandaloneBrowser = 8,
+  kRemote = 9,
+  kBorealis = 10,
+  kSystemWeb = 11,
+  kChromeBrowser = 12,
+
+  // Add any new values above this one, and update kMaxValue to the highest
+  // enumerator value.
+  kMaxValue = kChromeBrowser,
+};
+
 // Records metrics when launching apps.
 void RecordAppLaunchMetrics(Profile* profile,
                             const apps::AppUpdate& update,
                             apps::mojom::LaunchSource launch_source,
                             apps::mojom::LaunchContainer container);
+
+class AppPlatformMetrics : public apps::AppRegistryCache::Observer {
+ public:
+  explicit AppPlatformMetrics(Profile* profile,
+                              apps::AppRegistryCache& app_registry_cache);
+  AppPlatformMetrics(const AppPlatformMetrics&) = delete;
+  AppPlatformMetrics& operator=(const AppPlatformMetrics&) = delete;
+  ~AppPlatformMetrics() override;
+
+  // UMA metrics for a snapshot count of recently used apps for a given family
+  // user.
+  static const char* GetAppsCountHistogramNameForTest(
+      AppTypeName app_type_name);
+
+  void OnNewDay();
+
+ private:
+  // AppRegistryCache::Observer:
+  void OnAppTypeInitialized(apps::mojom::AppType app_type) override;
+  void OnAppRegistryCacheWillBeDestroyed(
+      apps::AppRegistryCache* cache) override;
+  void OnAppUpdate(const apps::AppUpdate& update) override;
+
+  // Records the number of apps of the given `app_type` that the family user has
+  // recently used.
+  void RecordAppsCount(apps::mojom::AppType app_type);
+
+  Profile* const profile_ = nullptr;
+
+  apps::AppRegistryCache& app_registry_cache_;
+
+  bool should_record_metrics_on_new_day_ = false;
+  bool first_report_on_current_device_ = false;
+};
 
 }  // namespace apps
 
--- chrome/browser/apps/app_service/app_platform_metrics_service.cc
+++ chrome/browser/apps/app_service/app_platform_metrics_service.cc
@@ -32,7 +32,6 @@ AppPlatformMetricsService::AppPlatformMetricsService(Profile* profile)
 }
 
 AppPlatformMetricsService::~AppPlatformMetricsService() {
-  observers_.Clear();
   timer_.Stop();
 }
 
@@ -47,21 +46,17 @@ int AppPlatformMetricsService::GetDayIdForTesting(base::Time time) {
   return GetDayId(time);
 }
 
-void AppPlatformMetricsService::Start() {
+void AppPlatformMetricsService::Start(
+    apps::AppRegistryCache& app_registry_cache) {
+  app_platform_app_metrics_ =
+      std::make_unique<AppPlatformMetrics>(profile_, app_registry_cache);
+
   day_id_ = profile_->GetPrefs()->GetInteger(kAppPlatformMetricsDayId);
   CheckForNewDay();
 
   // Check for a new day every |kTimerInterval| as well.
   timer_.Start(FROM_HERE, kTimerInterval, this,
                &AppPlatformMetricsService::CheckForNewDay);
-}
-
-void AppPlatformMetricsService::AddObserver(Observer* observer) {
-  observers_.AddObserver(observer);
-}
-
-void AppPlatformMetricsService::RemoveObserver(Observer* observer) {
-  observers_.RemoveObserver(observer);
 }
 
 void AppPlatformMetricsService::CheckForNewDay() {
@@ -71,9 +66,7 @@ void AppPlatformMetricsService::CheckForNewDay() {
   // time zone changes.
   if (day_id_ < GetDayId(now)) {
     day_id_ = GetDayId(now);
-    for (Observer& observer : observers_) {
-      observer.OnNewDay();
-    }
+    app_platform_app_metrics_->OnNewDay();
     profile_->GetPrefs()->SetInteger(kAppPlatformMetricsDayId, day_id_);
   }
 }
