35e75cb2e0a51b9d8627384033341d175498839f
Sean Harrison <harrisonsean@chromium.org>
2021-04-29 17:11:15 +0000

    [WebUI][Settings] Add additional test to site settings page
    Add missing test case and fix ordering of tests.
    
    Bug: 1113642
    Change-Id: I8cb0622ac8c6c7098d56bdc3a439878151125aca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2859925
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Auto-Submit: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#877528}

--- chrome/test/data/webui/settings/site_settings_page_test.js
+++ chrome/test/data/webui/settings/site_settings_page_test.js
@@ -88,16 +88,24 @@ suite('SiteSettingsPage', function() {
     const notificationsLinkRow = /** @type {!CrLinkRowElement} */ (
         page.$$('#basicPermissionsList').$$('#notifications'));
 
-    page.set('prefs.generated.notification.value', NotificationSetting.ASK);
+    page.set('prefs.generated.notification.value', NotificationSetting.BLOCK);
+    await flushTasks();
+    assertEquals(
+        loadTimeData.getString('siteSettingsBlocked'),
+        notificationsLinkRow.subLabel);
+
+    page.set(
+        'prefs.generated.notification.value',
+        NotificationSetting.QUIETER_MESSAGING);
     await flushTasks();
     assertEquals(
         loadTimeData.getString('siteSettingsAskBeforeSending'),
         notificationsLinkRow.subLabel);
 
-    page.set('prefs.generated.notification.value', NotificationSetting.BLOCK);
+    page.set('prefs.generated.notification.value', NotificationSetting.ASK);
     await flushTasks();
     assertEquals(
-        loadTimeData.getString('siteSettingsBlocked'),
+        loadTimeData.getString('siteSettingsAskBeforeSending'),
         notificationsLinkRow.subLabel);
   });
 
@@ -108,6 +116,12 @@ suite('SiteSettingsPage', function() {
 
     const notificationsLinkRow = /** @type {!CrLinkRowElement} */ (
         page.$$('#basicPermissionsList').$$('#notifications'));
+
+    page.set('prefs.generated.notification.value', NotificationSetting.BLOCK);
+    await flushTasks();
+    assertEquals(
+        loadTimeData.getString('siteSettingsNotificationsBlocked'),
+        notificationsLinkRow.subLabel);
 
     page.set(
         'prefs.generated.notification.value',
@@ -121,12 +135,6 @@ suite('SiteSettingsPage', function() {
     await flushTasks();
     assertEquals(
         loadTimeData.getString('siteSettingsNotificationsAllowed'),
-        notificationsLinkRow.subLabel);
-
-    page.set('prefs.generated.notification.value', NotificationSetting.BLOCK);
-    await flushTasks();
-    assertEquals(
-        loadTimeData.getString('siteSettingsNotificationsBlocked'),
         notificationsLinkRow.subLabel);
   });
 
