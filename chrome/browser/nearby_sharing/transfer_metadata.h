8fbe44748afa9d1e0afa69195506010a583dcc61
Fabrice de Gans-Riberi <fdegans@chromium.org>
2021-04-29 18:47:55 +0000

    Revert "[Nearby] Add high-level transfer and connection success/failure metrics"
    This reverts commit d7058b5c43bd691d5910671e74b3bf932b45cf3f.
    
    Reason for revert: Broke testJson3OutputWithArtifacts
    Common CL to first failures:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/491/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.12%20Tests/43399/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.14%20Tests/15129/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/chromeos-amd64-generic-rel/52628/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/chromeos-kevin-rel/33634/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-trusty-rel/23569/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-xenial-rel/9160/overview
    
    Original change's description:
    > [Nearby] Add high-level transfer and connection success/failure metrics
    >
    > In this CL, we record explicit success/failure metrics for
    > * "transfers", which encompasses all aspects of the sharing flow after
    >   device discovery;
    > * "transfers" sliced by Critical User Journey (CUJ) scenarios, for
    >   example, sending to a phone using contact-based sharing (see
    >   go/cros-nearby-cujs-sheet);
    > * establishing an outgoing connection with a receiving device, which
    >   is the first step after discovery (note: this step is also captured
    >   in the "transfer" metrics.)
    >
    > We currently have the metrics "Nearby.Share.Transfer.FinalStatus[...]"
    > and "Nearby.Share.Connection.EstablishOutgoingConnectionStatus". These
    > metrics log specific result enums that can be used for diagnostics
    > and more effective debugging. In this CL, we add explicit
    > success/failure metrics to avoid ad hoc determinations during data
    > analysis of what constitutes a success or failure.
    >
    > Manually verified metrics via chrome://histograms.
    >
    > Bug: 1202908, 1105579
    > Change-Id: Ia18605b8c00bc52549cb4a630a4024ab2510cf4c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853620
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: James Vecore <vecore@google.com>
    > Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    > Commit-Queue: Josh Nohle <nohle@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#877498}
    
    Bug: 1202908
    Bug: 1105579
    Change-Id: Ic484ead4f1ebe1a290f7a04ddb1159482c18f550
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2860822
    Auto-Submit: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Owners-Override: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877570}

--- chrome/browser/nearby_sharing/transfer_metadata.h
+++ chrome/browser/nearby_sharing/transfer_metadata.h
@@ -49,16 +49,7 @@ class TransferMetadata {
     kMaxValue = kUnexpectedDisconnection
   };
 
-  enum class Result {
-    kIndeterminate,
-    kSuccess,
-    kFailure,
-    kMaxValue = kFailure
-  };
-
   static bool IsFinalStatus(Status status);
-
-  static Result ToResult(Status status);
 
   static std::string StatusToString(TransferMetadata::Status status);
 
