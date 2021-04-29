dbd62729dad151bced64e0c130e20ec5baf41214
tby <tby@chromium.org>
2021-04-29 03:14:36 +0000

    [Launcher] Add new logic for search providers to update results
    The old callback used by the SearchProvider and SearchController
    doesn't send enough information for the new ranking system, so this
    implements a new SetResults method used only when categorical search
    is enabled.
    
    The SearchController now stores a map of provider types to results. The
    'provider type' is currently just AppListSearchResultType, as that's
    what SearchProvider::ResultType returns. We might need to update this
    to another enum in future, but for now it's okay.
    
    Bug: 1199206
    Change-Id: I248a09eb0b9c9d37151e8c7f1bb2f3e3eceeac61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2855145
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877339}

--- chrome/browser/ui/app_list/search/search_provider.h
+++ chrome/browser/ui/app_list/search/search_provider.h
@@ -19,6 +19,7 @@ class ChromeSearchResult;
 namespace app_list {
 
 enum class RankingItemType;
+class SearchController;
 
 class SearchProvider {
  public:
@@ -42,6 +43,10 @@ class SearchProvider {
   virtual void AppListShown() {}
   // Returns the main result type created by this provider.
   virtual ash::AppListSearchResultType ResultType() = 0;
+
+  void set_controller(SearchController* controller) {
+    search_controller_ = controller;
+  }
 
   void set_result_changed_callback(ResultChangedCallback callback) {
     result_changed_callback_ = std::move(callback);
@@ -68,6 +73,7 @@ class SearchProvider {
   void FireResultChanged();
 
   ResultChangedCallback result_changed_callback_;
+  SearchController* search_controller_;
   Results results_;
 
   DISALLOW_COPY_AND_ASSIGN(SearchProvider);
