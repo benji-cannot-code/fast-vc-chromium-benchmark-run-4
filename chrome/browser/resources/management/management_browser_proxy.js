a1b9be03241a0cd7c550e03f8a28d48b12c8a295
Marc Grimme <marcgrimme@chromium.org>
2021-04-29 08:10:19 +0000

    Add DLP reporting to management disclosure page
    This change adds an entry to the disclosure page (chrome://management)
    if dlp reporting is generally enabled by policy
    (DlpRulesFactory::IsReportingEnabled).
    
    Extract MockDlpRulesManager to a dedicated class because its used in two
    different tests now.
    
    Bug: 1199786
    Change-Id: I73b56a63415f3e212516e8b176cfffb6f6390725
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2843627
    Commit-Queue: Marc Grimme <marcgrimme@chromium.org>
    Reviewed-by: Thiemo Nagel <tnagel@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877396}

--- chrome/browser/resources/management/management_browser_proxy.js
+++ chrome/browser/resources/management/management_browser_proxy.js
@@ -79,6 +79,7 @@ export const DeviceReportingType = {
   LOGS: 'logs',
   PRINT: 'print',
   PRINT_JOBS: 'print jobs',
+  DLP_EVENTS: 'dlp events',
   CROSTINI: 'crostini',
   USERNAME: 'username',
   EXTENSION: 'extension',
