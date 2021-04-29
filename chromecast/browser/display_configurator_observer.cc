e010e312f1e95799c17e417889eb8d29d6d1bca6
Shiv Sakhuja <shivsak@google.com>
2021-04-29 22:32:30 +0000

    Add display change observer.
    Centralize the logic to force a repaint post display changes in an
    observer. This way the complexity of refreshing the display is
    handled in the observer rather than in every piece of code that
    needs to update the display state.
    
    Second attempt at landing this change, after the first one broke
    some bot tests.
    
    Bug: b/180040068
    Test: Ran cast_shell on the desktop to verify there are no crashes
    Change-Id: I6c346f11134dadeacb4f9ccaf7ecef6c7abf1711
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2860464
    Commit-Queue: Shiv Sakhuja <shivsak@google.com>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877669}

---
+++ chromecast/browser/display_configurator_observer.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "display_configurator_observer.h"
+
+namespace chromecast {
+
+DisplayConfiguratorObserver::DisplayConfiguratorObserver(
+    chromecast::shell::CastDisplayConfigurator* display_configurator,
+    chromecast::CastWindowManagerAura* manager)
+    : display_configurator_(display_configurator), window_manager_(manager) {
+  display_configurator_->AddObserver(this);
+}
+
+DisplayConfiguratorObserver::~DisplayConfiguratorObserver() {
+  display_configurator_->RemoveObserver(this);
+}
+
+void DisplayConfiguratorObserver::OnDisplayStateChanged() {
+  window_manager_->GetRootWindow()
+      ->GetHost()
+      ->compositor()
+      ->ScheduleFullRedraw();
+}
+
+}  // namespace chromecast
