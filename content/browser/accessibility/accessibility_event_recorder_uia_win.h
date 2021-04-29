5348ccef69597bafb59e653a25af75275bd08160
Joshua Pawlicki <waffles@chromium.org>
2021-04-29 21:18:19 +0000

    Revert "Improve filtering duplicate events in AccessibilityEventRecorderUia"
    This reverts commit 18f64a30d9da6fdf889a0684b8c7326297994b01.
    
    Reason for revert: Appears to break win-asan: https://ci.chromium.org/ui/p/chromium/builders/ci/win-asan/18459/overview
    
    Original change's description:
    > Improve filtering duplicate events in AccessibilityEventRecorderUia
    >
    > There is an bug in Windows where some events are raised exactly twice
    > for any in-proc off-thread event listeners.
    > Once from each UIAutomationCore.dll and RPCRT4.dll.
    >
    > This CL aims to simplify the event de-duplicate logic, which currently
    > filters after aggregating events based on which thread the event was
    > received on. This can be unreliable, since RCPRT4.dll may send events
    > from more than one thread, and the filtering only works if there are
    > exactly 2 threads.
    >
    > With this CL, events are now filtered by which module (dll) the event
    > was sent from (UIAutomationCore.dll vs RPCRT4.dll).
    > Since this approach actively filters events, rather than after they
    > have been accumulated, the following CL will be able to periodically
    > synchronize events from AccessibilityEventRecorderUia::Thread to the
    > ui::AXEventRecorder, allowing @UIA-WIN-RUN-UNTIL-EVENT to work :
    > https://chromium-review.googlesource.com/c/chromium/src/+/2803575
    >
    > Win7 and Win10 however appear to have slightly different behavior :
    > * On Win10: Most events are from UIAutomationCore.dll
    > * On Win10: Duplicate events are from RPCRT4.dll
    > * On Win7: Most events are from RPCRT.dll
    > * On Win7: Duplicate events are from UIAutomationCore.dll
    >
    > Because of this difference :
    > * Win7 will observe RPCRT4.dll
    > * Win10 will observe UIAutomationCore.dll
    >
    > AX-Relnotes: n/a.
    >
    > Bug: 1197666
    > Change-Id: Ia88d3eb9486b276e6554f23bff186da8f0e3caed
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2818946
    > Reviewed-by: Ian Prest <iapres@microsoft.com>
    > Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#877146}
    
    Bug: 1197666
    Change-Id: I9f5e53135927046a052cbb5ea339ecaaeb94b2ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2860882
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#877647}

--- content/browser/accessibility/accessibility_event_recorder_uia_win.h
+++ content/browser/accessibility/accessibility_event_recorder_uia_win.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <uiautomation.h>
 #include <wrl/client.h>
+#include <map>
 #include <string>
-#include <utility>
 #include <vector>
 
 #include "base/atomicops.h"
@@ -68,11 +68,12 @@ class AccessibilityEventRecorderUia : public AccessibilityEventRecorder {
     base::OnceClosure initialization_complete_;
     base::OnceClosure shutdown_complete_;
     base::WaitableEvent shutdown_signal_;
+    bool shutdown_sentinel_received_ = false;
 
     // Thread-specific wrapper for OnEvent to handle necessary locking
     void OnEvent(const std::string& event);
     base::Lock on_event_lock_;
-    std::vector<std::string> event_logs_;
+    std::map<base::PlatformThreadId, std::vector<std::string>> event_logs_;
 
     // An implementation of various UIA interfaces that forward event
     // notifications to the owning event recorder.
@@ -119,9 +120,6 @@ class AccessibilityEventRecorderUia : public AccessibilityEventRecorder {
       AccessibilityEventRecorderUia::Thread* owner_ = nullptr;
 
      private:
-      std::pair<uintptr_t, uintptr_t> allowed_module_address_range_;
-      bool IsCallerFromAllowedModule(void* return_address);
-
       std::string GetSenderInfo(IUIAutomationElement* sender);
 
       Microsoft::WRL::ComPtr<IUIAutomationElement> root_;
