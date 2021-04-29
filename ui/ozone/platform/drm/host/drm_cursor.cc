b0fe5471e7926e067962e89d07fcc03936ae7f34
Henrique Ferreiro <hferreiro@igalia.com>
2021-04-29 07:04:20 +0000

    Rename BitmapCursorOzone variables to cursor_
    Both WaylandWindow and DrmCursor have a BitmapCursorOzone member called
    bitmap_. This CL renames it to the more obvious name cursor_.
    
    Change-Id: I1588e388af49483ffea34b350282f2a1b6f4f46e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2811698
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#877378}

--- ui/ozone/platform/drm/host/drm_cursor.cc
+++ ui/ozone/platform/drm/host/drm_cursor.cc
@@ -73,7 +73,7 @@ void DrmCursor::ResetDrmCursorProxy() {
 }
 
 gfx::Point DrmCursor::GetBitmapLocationLocked() {
-  return gfx::ToFlooredPoint(location_) - bitmap_->hotspot().OffsetFromOrigin();
+  return gfx::ToFlooredPoint(location_) - cursor_->hotspot().OffsetFromOrigin();
 }
 
 void DrmCursor::SetCursor(gfx::AcceleratedWidget window,
@@ -85,10 +85,10 @@ void DrmCursor::SetCursor(gfx::AcceleratedWidget window,
 
   base::AutoLock lock(lock_);
 
-  if (window_ != window || bitmap_ == platform_cursor)
+  if (window_ != window || cursor_ == platform_cursor)
     return;
 
-  bitmap_ = platform_cursor;
+  cursor_ = platform_cursor;
 
   SendCursorShowLocked();
 }
@@ -210,7 +210,7 @@ void DrmCursor::MoveCursor(const gfx::Vector2dF& delta) {
 
 bool DrmCursor::IsCursorVisible() {
   base::AutoLock lock(lock_);
-  return bitmap_ != nullptr && bitmap_->type() != CursorType::kNone;
+  return cursor_ != nullptr && cursor_->type() != CursorType::kNone;
 }
 
 gfx::PointF DrmCursor::GetLocation() {
@@ -243,13 +243,13 @@ void DrmCursor::SetCursorLocationLocked(const gfx::PointF& location) {
 }
 
 void DrmCursor::SendCursorShowLocked() {
-  if (!bitmap_ || bitmap_->type() == CursorType::kNone) {
+  if (!cursor_ || cursor_->type() == CursorType::kNone) {
     SendCursorHideLocked();
     return;
   }
 
-  CursorSetLockTested(window_, bitmap_->bitmaps(), GetBitmapLocationLocked(),
-                      bitmap_->frame_delay());
+  CursorSetLockTested(window_, cursor_->bitmaps(), GetBitmapLocationLocked(),
+                      cursor_->frame_delay());
 }
 
 void DrmCursor::SendCursorHideLocked() {
@@ -258,7 +258,7 @@ void DrmCursor::SendCursorHideLocked() {
 }
 
 void DrmCursor::SendCursorMoveLocked() {
-  if (!bitmap_ || bitmap_->type() == CursorType::kNone)
+  if (!cursor_ || cursor_->type() == CursorType::kNone)
     return;
 
   MoveLockTested(window_, GetBitmapLocationLocked());
