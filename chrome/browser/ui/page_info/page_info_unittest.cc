4353fc367c6ad19138c42ea2c7a3b4d8759d0d43
Colin Blundell <blundell@chromium.org>
2021-04-29 16:25:51 +0000

    [Infobars] Eliminate last dep on MockInfoBarService and remove it
    I'm working toward removing //chrome's InfoBarService (see the linked
    bug for details). One blocker is the existence of MockInfoBarService.
    This CL removes the last usage of that mock by adding the necessary
    setup for the actual class to be used in its place. It also removes the
    mock itself.
    
    Bug: 1199686
    Change-Id: Ide62d9244aef29c4e815a6dc87bd8f1481f44fd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853561
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877505}

--- chrome/browser/ui/page_info/page_info_unittest.cc
+++ chrome/browser/ui/page_info/page_info_unittest.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/content_settings/page_specific_content_settings_delegate.h"
-#include "chrome/browser/infobars/mock_infobar_service.h"
+#include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/ssl/stateful_ssl_host_state_delegate_factory.h"
 #include "chrome/browser/ssl/tls_deprecation_test_utils.h"
 #include "chrome/browser/subresource_filter/subresource_filter_profile_context_factory.h"
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/android_theme_resources.h"
 #else
 #include "base/test/scoped_feature_list.h"
+#include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "media/base/media_switches.h"
@@ -139,7 +140,7 @@ class PageInfoTest : public ChromeRenderViewHostTestHarness {
         net::ImportCertFromFile(net::GetTestCertsDirectory(), "ok_cert.pem");
     ASSERT_TRUE(cert_);
 
-    MockInfoBarService::CreateForWebContents(web_contents());
+    InfoBarService::CreateForWebContents(web_contents());
     content_settings::PageSpecificContentSettings::CreateForWebContents(
         web_contents(),
         std::make_unique<chrome::PageSpecificContentSettingsDelegate>(
@@ -271,6 +272,10 @@ class PageInfoTest : public ChromeRenderViewHostTestHarness {
   std::unique_ptr<content::WebContents> incognito_web_contents_;
   std::unique_ptr<PageInfo> incognito_page_info_;
   std::unique_ptr<MockPageInfoUI> incognito_mock_ui_;
+
+#if !defined(OS_ANDROID)
+  ChromeLayoutProvider layout_provider_;
+#endif
 
   scoped_refptr<net::X509Certificate> cert_;
   GURL url_;
