eca4b384de95676cf6aa3a9ad1dcd633760b7357
Reilly Grant <reillyg@chromium.org>
2021-04-29 21:04:43 +0000

    bluetooth: Add test for background permission requests
    This change adds a test for sites which attempt to show the Web
    Bluetooth permission prompt while they are in the background. This
    scenario exposed a use-after-free which was fixed without a
    corresponding test in r829523.
    
    This exposed a more general issue that in this scenario the Promise
    returned by requestDevice() would never resolve. Fixing this requires
    making sure that the chooser event callback is executed when the
    controller is destroyed and fixing the resulting reentrancy issues. To
    make this simpler the success and failure callbacks passed to
    BluetoothDeviceChooserController have been merged.
    
    Bug: 730593,1149692
    Change-Id: I9042e2c636322ad3ab2dff04050b45a5c3ec2fbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2858256
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877638}

--- chrome/browser/ui/bluetooth/bluetooth_chooser_controller.cc
+++ chrome/browser/ui/bluetooth/bluetooth_chooser_controller.cc
@@ -67,7 +67,12 @@ BluetoothChooserController::BluetoothChooserController(
   }
 }
 
-BluetoothChooserController::~BluetoothChooserController() {}
+BluetoothChooserController::~BluetoothChooserController() {
+  if (event_handler_) {
+    event_handler_.Run(content::BluetoothChooserEvent::CANCELLED,
+                       std::string());
+  }
+}
 
 bool BluetoothChooserController::ShouldShowIconBeforeText() const {
   return true;
@@ -171,6 +176,7 @@ void BluetoothChooserController::Select(const std::vector<size_t>& indices) {
   DCHECK_LT(index, devices_.size());
   event_handler_.Run(content::BluetoothChooserEvent::SELECTED,
                      devices_[index].id);
+  event_handler_.Reset();
 }
 
 void BluetoothChooserController::Cancel() {
@@ -178,6 +184,7 @@ void BluetoothChooserController::Cancel() {
   if (event_handler_.is_null())
     return;
   event_handler_.Run(content::BluetoothChooserEvent::CANCELLED, std::string());
+  event_handler_.Reset();
 }
 
 void BluetoothChooserController::Close() {
@@ -185,6 +192,7 @@ void BluetoothChooserController::Close() {
   if (event_handler_.is_null())
     return;
   event_handler_.Run(content::BluetoothChooserEvent::CANCELLED, std::string());
+  event_handler_.Reset();
 }
 
 void BluetoothChooserController::OpenHelpCenterUrl() const {
