85b023590bbdd7b5ef7c2e6ca7d599396ba119c3
Lei Zhang <thestig@chromium.org>
2021-04-29 20:11:16 +0000

    Reduce build cost for third_party/blink/renderer/core/page/page.h.
    One big header that page.h includes is frame.mojom-blink.h. However, it
    only needs TextAutosizerPageInfo from that header. So split
    TextAutosizerPageInfo out of frame.mojom and into its own .mojom file.
    
    A commonly used header that includes page.h is layout_box.h. Here, move
    the implementation of LayoutBox::UsesOverlayScrollbars() into
    layout_box.cc, and remove the page.h include. Then do IWYU for other
    files that should have included page.h.
    
    Together, these two changes reduce the amount of data necessary to build
    the chrome target by 615 MB.
    
    Change-Id: I0ce1f950cfdc6093247094d13d2f67680f158975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2858266
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877616}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -92,6 +92,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/mathml/mathml_space_element.h"
 #include "third_party/blink/renderer/core/mathml_names.h"
 #include "third_party/blink/renderer/core/media_type_names.h"
+#include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/page/scrolling/snap_coordinator.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/core/style/style_initial_data.h"
--- third_party/blink/renderer/core/paint/highlight_painting_utils.cc
+++ third_party/blink/renderer/core/paint/highlight_painting_utils.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/layout/layout_theme.h"
 #include "third_party/blink/renderer/core/page/focus_controller.h"
+#include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/paint/paint_info.h"
 #include "third_party/blink/renderer/core/paint/text_paint_style.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
