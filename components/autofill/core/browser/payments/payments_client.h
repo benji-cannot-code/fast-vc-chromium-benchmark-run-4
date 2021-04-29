6b697571dcc753cd5aebf3fc3bd9789277ac6399
Lei Zhang <thestig@chromium.org>
2021-04-29 18:12:20 +0000

    Fix nits in payments_client.h.
    - No need to explicitly initialize base::Optional to base::nullopt.
    - Include what you use for base/ and STL headers.
    
    Change-Id: Ia89cde536d1ef160dee341f10fb4b753ddf979c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2858118
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877552}

--- components/autofill/core/browser/payments/payments_client.h
+++ components/autofill/core/browser/payments/payments_client.h
@@ -9,11 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
+#include "base/values.h"
 #include "components/autofill/core/browser/autofill_client.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/credit_card.h"
@@ -84,7 +87,7 @@ class PaymentsClient {
     bool offer_fido_opt_in = false;
     // Public Key Credential Request Options required for authentication.
     // https://www.w3.org/TR/webauthn/#dictdef-publickeycredentialrequestoptions
-    base::Optional<base::Value> fido_request_options = base::nullopt;
+    base::Optional<base::Value> fido_request_options;
     // Set of credit cards ids that are eligible for FIDO Authentication.
     std::set<std::string> fido_eligible_card_ids;
   };
@@ -101,7 +104,7 @@ class PaymentsClient {
     CreditCard card;
     std::string risk_data;
     CardUnmaskDelegate::UserProvidedUnmaskDetails user_response;
-    base::Optional<base::Value> fido_assertion_info = base::nullopt;
+    base::Optional<base::Value> fido_assertion_info;
   };
 
   // Information retrieved from an UnmaskRequest.
@@ -125,10 +128,10 @@ class PaymentsClient {
     std::string dcvv;
     // Challenge required for enrolling user into FIDO authentication for future
     // card unmasking.
-    base::Optional<base::Value> fido_creation_options = base::nullopt;
+    base::Optional<base::Value> fido_creation_options;
     // Challenge required for authorizing user for FIDO authentication for
     // future card unmasking.
-    base::Optional<base::Value> fido_request_options = base::nullopt;
+    base::Optional<base::Value> fido_request_options;
     // An opaque token used to logically chain consecutive UnmaskCard and
     // OptChange calls together.
     std::string card_authorization_token = std::string();
@@ -160,7 +163,7 @@ class PaymentsClient {
     Reason reason;
     // Signature required for enrolling user into FIDO authentication for future
     // card unmasking.
-    base::Optional<base::Value> fido_authenticator_response = base::nullopt;
+    base::Optional<base::Value> fido_authenticator_response;
     // An opaque token used to logically chain consecutive UnmaskCard and
     // OptChange calls together.
     std::string card_authorization_token = std::string();
@@ -177,10 +180,10 @@ class PaymentsClient {
     base::Optional<bool> user_is_opted_in;
     // Challenge required for enrolling user into FIDO authentication for future
     // card unmasking.
-    base::Optional<base::Value> fido_creation_options = base::nullopt;
+    base::Optional<base::Value> fido_creation_options;
     // Challenge required for authorizing user for FIDO authentication for
     // future card unmasking.
-    base::Optional<base::Value> fido_request_options = base::nullopt;
+    base::Optional<base::Value> fido_request_options;
   };
 
   // A collection of the information required to make a credit card upload
