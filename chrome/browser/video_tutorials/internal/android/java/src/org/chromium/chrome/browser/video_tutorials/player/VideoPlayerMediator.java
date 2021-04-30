01af99d73d89b5c8ac9123e4489deb2d563318ed
Shakti Sahu <shaktisahu@chromium.org>
2021-04-30 21:48:58 +0000

    Video Tutorials : Show play button at the end of the video
    This CL adds a play button to the video player when the video has ended.
    This will enable the users to rewatch the video.
    
    Bug: 1203600
    Change-Id: Ic30a2a3d9c751ec98833758b9d6aeafcb40ef179
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2856357
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878113}

--- chrome/browser/video_tutorials/internal/android/java/src/org/chromium/chrome/browser/video_tutorials/player/VideoPlayerMediator.java
+++ chrome/browser/video_tutorials/internal/android/java/src/org/chromium/chrome/browser/video_tutorials/player/VideoPlayerMediator.java
@@ -65,6 +65,7 @@ class VideoPlayerMediator implements PlaybackStateObserver.Observer {
         mModel.set(VideoPlayerProperties.CALLBACK_TRY_NOW, this::tryNow);
         mModel.set(VideoPlayerProperties.CALLBACK_SHARE, this::share);
         mModel.set(VideoPlayerProperties.CALLBACK_CLOSE, this::close);
+        mModel.set(VideoPlayerProperties.CALLBACK_PLAY_BUTTON, () -> startVideo(mTutorial));
     }
 
     /** Called when the player is getting destroyed. */
@@ -131,6 +132,7 @@ class VideoPlayerMediator implements PlaybackStateObserver.Observer {
         mModel.set(VideoPlayerProperties.WATCH_STATE_FOR_TRY_NOW, State.PAUSED);
         mModel.set(VideoPlayerProperties.SHOW_SHARE, enableShare());
         mModel.set(VideoPlayerProperties.SHOW_CLOSE, true);
+        mModel.set(VideoPlayerProperties.SHOW_PLAY_BUTTON, false);
     }
 
     @Override
@@ -143,6 +145,7 @@ class VideoPlayerMediator implements PlaybackStateObserver.Observer {
         mModel.set(VideoPlayerProperties.WATCH_STATE_FOR_TRY_NOW, State.ENDED);
         mModel.set(VideoPlayerProperties.SHOW_SHARE, enableShare());
         mModel.set(VideoPlayerProperties.SHOW_CLOSE, true);
+        mModel.set(VideoPlayerProperties.SHOW_PLAY_BUTTON, true);
     }
 
     @Override
@@ -214,6 +217,7 @@ class VideoPlayerMediator implements PlaybackStateObserver.Observer {
         mModel.set(VideoPlayerProperties.SHOW_CHANGE_LANGUAGE, false);
         mModel.set(VideoPlayerProperties.SHOW_SHARE, enableShare());
         mModel.set(VideoPlayerProperties.SHOW_CLOSE, true);
+        mModel.set(VideoPlayerProperties.SHOW_PLAY_BUTTON, false);
     }
 
     private boolean enableShare() {
