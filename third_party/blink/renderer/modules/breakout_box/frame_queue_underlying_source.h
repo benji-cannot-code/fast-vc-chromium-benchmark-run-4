60027a67ee8fbc899e6410db617f79b8c94e5504
Omer Katz <omerkatz@chromium.org>
2021-04-30 15:05:26 +0000

    Remove forward declarations from FrameQueueUnderlyingSource
    Some local builds broke because of these incomplete types.
    
    Change-Id: Ic48f6c76e1a2e6844e78be01b1895f6ce584aae9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2862726
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877920}

--- third_party/blink/renderer/modules/breakout_box/frame_queue_underlying_source.h
+++ third_party/blink/renderer/modules/breakout_box/frame_queue_underlying_source.h
@@ -7,13 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_BREAKOUT_BOX_FRAME_QUEUE_UNDERLYING_SOURCE_H_
 
 #include "base/threading/thread_checker.h"
+#include "media/base/audio_buffer.h"
+#include "media/base/video_frame.h"
 #include "third_party/blink/renderer/core/streams/underlying_source_base.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/wtf/deque.h"
-
-namespace media {
-class AudioBuffer;
-}
 
 namespace blink {
 
