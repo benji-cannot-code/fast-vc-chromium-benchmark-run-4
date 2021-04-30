18c54d050f2a57738c8b6cca22f843cad7a7dc0c
Clark DuVall <cduvall@chromium.org>
2021-04-30 20:04:30 +0000

    Reland "[WebLayer] Flush cookies after a delay when SetCookie is called"
    This is a reland of 5fe600122b18e3ff1640ab2e5c1b54b40c6384e0
    
    Original change's description:
    > [WebLayer] Flush cookies after a delay when SetCookie is called
    >
    > This adds a 1 second delay after a SetCookie call to persist the cookie
    > changes. All updates within the 1 second interval will be persisted at
    > once to prevent too many disk writes.
    >
    > Bug: 1203791
    > Change-Id: Ie97266faf769c56f4c37b5f2a6fea1e1ec2107ab
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2857780
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#877670}
    
    Bug: 1203791
    Change-Id: I9a817d0cea5d74422f6f90326296a0c21e021b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2861449
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878054}

--- weblayer/browser/cookie_manager_impl.cc
+++ weblayer/browser/cookie_manager_impl.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace weblayer {
 namespace {
+constexpr base::TimeDelta kCookieFlushDelay = base::TimeDelta::FromSeconds(1);
 
 void GetCookieComplete(CookieManager::GetCookieCallback callback,
                        const net::CookieAccessResultList& cookies,
@@ -137,6 +138,17 @@ void CookieManagerImpl::RemoveCookieChangedCallback(JNIEnv* env, int id) {
 }
 #endif
 
+bool CookieManagerImpl::FireFlushTimerForTesting() {
+  if (!flush_timer_)
+    return false;
+
+  flush_run_loop_for_testing_ = std::make_unique<base::RunLoop>();
+  flush_timer_->FireNow();
+  flush_run_loop_for_testing_->Run();
+  flush_run_loop_for_testing_ = nullptr;
+  return true;
+}
+
 bool CookieManagerImpl::SetCookieInternal(const GURL& url,
                                           const std::string& value,
                                           SetCookieCallback callback) {
@@ -150,7 +162,9 @@ bool CookieManagerImpl::SetCookieInternal(const GURL& url,
       ->GetCookieManagerForBrowserProcess()
       ->SetCanonicalCookie(
           *cc, url, net::CookieOptions::MakeAllInclusive(),
-          net::cookie_util::AdaptCookieAccessResultToBool(std::move(callback)));
+          net::cookie_util::AdaptCookieAccessResultToBool(
+              base::BindOnce(&CookieManagerImpl::OnCookieSet,
+                             weak_factory_.GetWeakPtr(), std::move(callback))));
   return true;
 }
 
@@ -175,6 +189,25 @@ int CookieManagerImpl::AddCookieChangedCallbackInternal(
 
 void CookieManagerImpl::RemoveCookieChangedCallbackInternal(int id) {
   cookie_change_receivers_.Remove(id);
+}
+
+void CookieManagerImpl::OnCookieSet(SetCookieCallback callback, bool success) {
+  std::move(callback).Run(success);
+  if (!flush_timer_) {
+    flush_timer_ = std::make_unique<base::OneShotTimer>();
+    flush_timer_->Start(FROM_HERE, kCookieFlushDelay,
+                        base::BindOnce(&CookieManagerImpl::OnFlushTimerFired,
+                                       weak_factory_.GetWeakPtr()));
+  }
+}
+
+void CookieManagerImpl::OnFlushTimerFired() {
+  browser_context_->GetDefaultStoragePartition()
+      ->GetCookieManagerForBrowserProcess()
+      ->FlushCookieStore(flush_run_loop_for_testing_
+                             ? flush_run_loop_for_testing_->QuitClosure()
+                             : base::DoNothing());
+  flush_timer_ = nullptr;
 }
 
 }  // namespace weblayer
