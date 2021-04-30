4a70fd8aadee179455512530f4c0d6b0f4c00134
Akihiro Ota <akihiroota@chromium.org>
2021-04-30 21:57:38 +0000

    ChromeVox: Polish phonetic readings for Japanese.
    ChromeVox now omits the character type when the same character type
    continues. For example:
    
    Text: あいうえお
    Previous behavior: ひらがなのアサヒ ノ ア, ひらがなのイチゴ ノ イ, ...
    New behavior: ひらがな あいうえお
    
    Bug: 1195393
    AX-Relnotes: ChromeVox gives more concise phonetic readings in Japanese.
    Change-Id: Ib8683ed1fee497ea69300313ffbd853cc3c83e30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2807393
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878119}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/background.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/background.js
@@ -107,7 +107,6 @@ export class Background extends ChromeVoxState {
     CommandHandler.init();
     FindHandler.init();
     DownloadHandler.init();
-    PhoneticData.init();
 
     chrome.accessibilityPrivate.onAnnounceForAccessibility.addListener(
         (announceText) => {
