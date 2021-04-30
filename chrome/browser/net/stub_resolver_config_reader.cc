f97fc2b8655b73e4ae92c5e2ef3ba8e0c524ab26
Eric Orth <ericorth@chromium.org>
2021-04-30 17:43:59 +0000

    Add policy and pref for additional DNS query types
    Does nothing so far.  Will pipe to the DNS stack in a subsequent CL.
    
    Also, while poking at policy_templates.json, took ownership of DNS-
    related policies from people no longer working on Chrome.
    
    Bug: 1203427
    Change-Id: I0b4d24d79f5ce8d8b7bfead460eb06d129ab7687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2860222
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877977}

--- chrome/browser/net/stub_resolver_config_reader.cc
+++ chrome/browser/net/stub_resolver_config_reader.cc
@@ -171,6 +171,9 @@ StubResolverConfigReader::StubResolverConfigReader(PrefService* local_state,
   pref_change_registrar_.Add(prefs::kDnsOverHttpsMode, pref_callback);
   pref_change_registrar_.Add(prefs::kDnsOverHttpsTemplates, pref_callback);
 
+  // TODO(crbug.com/1203427): Watch for `prefs::kAdditionalDnsQueryTypesEnabled`
+  // changes.
+
   parental_controls_delay_timer_.Start(
       FROM_HERE, kParentalControlsCheckDelay,
       base::BindOnce(&StubResolverConfigReader::OnParentalControlsDelayTimer,
@@ -197,6 +200,7 @@ void StubResolverConfigReader::RegisterPrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kBuiltInDnsClientEnabled, false);
   registry->RegisterStringPref(prefs::kDnsOverHttpsMode, std::string());
   registry->RegisterStringPref(prefs::kDnsOverHttpsTemplates, std::string());
+  registry->RegisterBooleanPref(prefs::kAdditionalDnsQueryTypesEnabled, true);
 }
 
 SecureDnsConfig StubResolverConfigReader::GetSecureDnsConfiguration(
@@ -383,6 +387,8 @@ SecureDnsConfig StubResolverConfigReader::GetAndUpdateConfiguration(
   }
 
   if (update_network_service) {
+    // TODO(crbug.com/1203427): Configure for
+    // `prefs::kAdditionalDnsQueryTypesEnabled` value.
     content::GetNetworkService()->ConfigureStubHostResolver(
         GetInsecureStubResolverEnabled(), secure_dns_mode,
         std::move(servers_mojo));
