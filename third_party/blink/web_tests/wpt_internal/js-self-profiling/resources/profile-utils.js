acc400230c8902f226ca511aefcc47783a010083
Andrew Comminos <acomminos@fb.com>
2021-04-30 21:41:23 +0000

    [profiler] Reintroduce context filtering in blink
    Provide a non-null |v8::Context| param to filter profiles by originating
    context, now that context filtering has been reintroduced in blink.
    Modify existing WPTs to be compliant with this, as well as use a more
    realistic test for filtering.
    
    Bug: 956688
    Change-Id: I60c23fdac29af9f452d21f57496fd2f8fc20826e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2852074
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Commit-Queue: Andrew Comminos <acomminos@fb.com>
    Cr-Commit-Position: refs/heads/master@{#878111}

--- third_party/blink/web_tests/wpt_internal/js-self-profiling/resources/profile-utils.js
+++ third_party/blink/web_tests/wpt_internal/js-self-profiling/resources/profile-utils.js
@@ -84,6 +84,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
            (expected.column === undefined || expected.column === actual.column);
   }
 
+  function forceSampleFrame(frame) {
+    const channel = new MessageChannel();
+    const replyPromise = new Promise(res => {
+      channel.port1.onmessage = res;
+    });
+    frame.postMessage('', '*', [channel.port2]);
+    return replyPromise;
+  }
+
+  window.addEventListener('message', message => {
+    // Force sample in response to messages received.
+    (function sampleFromMessage() {
+      ProfileUtils.forceSample();
+      message.ports[0].postMessage('');
+    })();
+  });
+
   global.ProfileUtils = {
     // Capturing
     profileFunction,
@@ -93,6 +110,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     containsFrame,
     containsSubstack,
     containsResource,
+
+    // Cross-frame sampling
+    forceSampleFrame,
 
     // Assertions
     testFunction,
