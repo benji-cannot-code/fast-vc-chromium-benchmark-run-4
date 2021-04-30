0cefd2a0b9f943009ddd101e76235ea014c60de8
Mohamed Amir Yosef <mamir@chromium.org>
2021-04-30 19:15:10 +0000

    [Autofill] Populate the update address prompt
    This CL populates the main content in the update address prompt.
    The update prompt should show all updated types, together with their
    old and new values.
    
    Mocks:
    https://docs.google.com/presentation/d/1Gs3Sf6u34yGVLNyCSVAGmdJ_GD_bgc87IPJ4VJwHigY/edit#slide=id.gc47b915084_0_19
    
    Screenshots:
    One value update
    https://screenshot.googleplex.com/8AdQ49D8b9Cfk4r.png
    Multiple values update:
    https://screenshot.googleplex.com/35LRBQi7QcEZMvN.png
    
    Bug: 1167060
    Change-Id: Ifae5a5d750174facb3f3bc0d905ab428f37fe253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2850922
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878027}

--- chrome/browser/ui/views/autofill/update_address_profile_view.h
+++ chrome/browser/ui/views/autofill/update_address_profile_view.h
@@ -43,9 +43,6 @@ class UpdateAddressProfileView : public AutofillBubbleBase,
   // AutofillBubbleBase:
   void Hide() override;
 
-  // View:
-  void AddedToWidget() override;
-
  private:
   SaveUpdateAddressProfileBubbleController* controller_;
 };
