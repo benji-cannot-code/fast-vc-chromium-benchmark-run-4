7dcf46922e1c7ec82dda98e12e394409cbb16477
Josh Nohle <nohle@google.com>
2021-04-30 20:38:52 +0000

    Reland "[Nearby] Add high-level transfer and connection success/failure metrics"
    This is a reland of d7058b5c43bd691d5910671e74b3bf932b45cf3f
    
    Original change's description:
    > [Nearby] Add high-level transfer and connection success/failure metrics
    >
    > In this CL, we record explicit success/failure metrics for
    > * "transfers", which encompasses all aspects of the sharing flow after
    >   device discovery;
    > * "transfers" sliced by Critical User Journey (CUJ) scenarios, for
    >   example, sending to a phone using contact-based sharing (see
    >   go/cros-nearby-cujs-sheet);
    > * establishing an outgoing connection with a receiving device, which
    >   is the first step after discovery (note: this step is also captured
    >   in the "transfer" metrics.)
    >
    > We currently have the metrics "Nearby.Share.Transfer.FinalStatus[...]"
    > and "Nearby.Share.Connection.EstablishOutgoingConnectionStatus". These
    > metrics log specific result enums that can be used for diagnostics
    > and more effective debugging. In this CL, we add explicit
    > success/failure metrics to avoid ad hoc determinations during data
    > analysis of what constitutes a success or failure.
    >
    > Manually verified metrics via chrome://histograms.
    >
    > Bug: 1202908, 1105579
    > Change-Id: Ia18605b8c00bc52549cb4a630a4024ab2510cf4c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853620
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: James Vecore <vecore@google.com>
    > Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    > Commit-Queue: Josh Nohle <nohle@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#877498}
    
    Bug: 1202908
    Bug: 1105579
    Change-Id: I8f31f998d27de184743d580e60747ca6c1e0a10c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2863185
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Auto-Submit: Josh Nohle <nohle@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878070}

--- chrome/browser/nearby_sharing/transfer_metadata.h
+++ chrome/browser/nearby_sharing/transfer_metadata.h
@@ -49,7 +49,16 @@ class TransferMetadata {
     kMaxValue = kUnexpectedDisconnection
   };
 
+  enum class Result {
+    kIndeterminate,
+    kSuccess,
+    kFailure,
+    kMaxValue = kFailure
+  };
+
   static bool IsFinalStatus(Status status);
+
+  static Result ToResult(Status status);
 
   static std::string StatusToString(TransferMetadata::Status status);
 
