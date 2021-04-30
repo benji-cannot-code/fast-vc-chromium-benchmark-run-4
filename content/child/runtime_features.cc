2c32a930b628bd686fe1f1bcb57838a657241e1a
Christoph Schwering <schwering@google.com>
2021-04-30 13:10:19 +0000

    [Permissions Policy] [Autofill] Added token shared-autofill.
    This CL adds an experimental policy token "shared-autofill"
    intended for cross-frame autofilling. It is enabled with the
    autofill::features::kAutofillAcrossIframes.
    
    The token is needed for Autofill to decide whether or not to
    fill into a frame, which is being implemented in a follow-up CL
    (crrev.com/c/2531999).
    
    Bug: 1201849, 1187842
    Change-Id: Ic0b01ae0711fd787c63f8bee64231c2b097f9943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2844839
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877888}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "cc/base/features.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "content/common/content_navigation_policy.h"
 #include "content/common/content_switches_internal.h"
 #include "content/public/common/content_features.h"
@@ -293,6 +294,7 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
      features::kSendBeaconThrowForBlobWithNonSimpleType},
     {wf::EnableSharedArrayBufferOnDesktop,
      features::kSharedArrayBufferOnDesktop},
+    {wf::EnableSharedAutofill, autofill::features::kAutofillAcrossIframes},
     {wf::EnableSignedExchangePrefetchCacheForNavigations,
      features::kSignedExchangePrefetchCacheForNavigations},
     {wf::EnableSignedExchangeSubresourcePrefetch,
