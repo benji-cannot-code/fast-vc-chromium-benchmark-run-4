e97b0af126000ec5474e47337e4a5f97802023e3
Evan Stade <estade@chromium.org>
2021-04-30 16:19:32 +0000

    [Code health] Fix gn check issues for //c/b/safety_check/android
    Some C++ code depended on the //chrome/browser target, but was
    missing the correct gn deps. Those dependencies are removed by
    introduction of identity_manager_provider, which provides a
    way for modularized code to grab an IdentityManager from a
    BrowserContext.
    
    Bug: 1158915
    
    Change-Id: Id8d8d210f2f70e887a950004cce7d03d6d653916
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2824129
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877952}

--- chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckBridge.java
+++ chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckBridge.java
@@ -7,6 +7,8 @@ package org.chromium.chrome.browser.safety_check;
 
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.NativeMethods;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.components.embedder_support.browser_context.BrowserContextHandle;
 
 /**
  * Provides access to the C++ multi-platform Safety check code in
@@ -48,15 +50,15 @@ public class SafetyCheckBridge {
      * Returns whether the user is signed in for the purposes of password check.
      */
     boolean userSignedIn() {
-        return SafetyCheckBridgeJni.get().userSignedIn();
+        return SafetyCheckBridgeJni.get().userSignedIn(Profile.getLastUsedRegularProfile());
     }
 
     /**
      * Triggers the Safe Browsing check on the C++ side.
      */
     void checkSafeBrowsing() {
-        SafetyCheckBridgeJni.get().checkSafeBrowsing(
-                mNativeSafetyCheckBridge, SafetyCheckBridge.this);
+        SafetyCheckBridgeJni.get().checkSafeBrowsing(mNativeSafetyCheckBridge,
+                SafetyCheckBridge.this, Profile.getLastUsedRegularProfile());
     }
 
     /**
@@ -74,8 +76,9 @@ public class SafetyCheckBridge {
     @NativeMethods
     interface Natives {
         long init(SafetyCheckBridge safetyCheckBridge, SafetyCheckCommonObserver observer);
-        boolean userSignedIn();
-        void checkSafeBrowsing(long nativeSafetyCheckBridge, SafetyCheckBridge safetyCheckBridge);
+        boolean userSignedIn(BrowserContextHandle browserContext);
+        void checkSafeBrowsing(long nativeSafetyCheckBridge, SafetyCheckBridge safetyCheckBridge,
+                BrowserContextHandle browserContext);
         void destroy(long nativeSafetyCheckBridge, SafetyCheckBridge safetyCheckBridge);
     }
 }
--- chrome/browser/signin/identity_manager_factory.cc
+++ chrome/browser/signin/identity_manager_factory.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/account_consistency_mode_manager.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
+#include "chrome/browser/signin/identity_manager_provider.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/pref_registry/pref_registry_syncable.h"
@@ -61,6 +62,11 @@ IdentityManagerFactory::IdentityManagerFactory()
   DependsOn(WebDataServiceFactory::GetInstance());
 #endif
   DependsOn(ChromeSigninClientFactory::GetInstance());
+
+  signin::SetIdentityManagerProvider(
+      base::BindRepeating([](content::BrowserContext* context) {
+        return GetForProfile(Profile::FromBrowserContext(context));
+      }));
 }
 
 IdentityManagerFactory::~IdentityManagerFactory() {}
