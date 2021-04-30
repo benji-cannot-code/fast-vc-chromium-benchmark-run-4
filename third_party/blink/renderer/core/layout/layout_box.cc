b4bfc38b3d47bc71cabab08018fea7882609d69d
Ana SollanoKim <ansollan@microsoft.com>
2021-04-30 05:52:17 +0000

    [GridNG] Compute the correct offset for out of flow items that start or end in the middle of a range
    Previously, if an out of flow item happened to be in the middle of a
    range, its offset and size were computed incorrectly. In this change,
    helpers are added to compute the extra offset needed. Two web tests were
    added to validate this behaviour.
    
    Additionally, we now store range related data instead of the set indices
    for out of flow items, as in this case we need the first and last
    spanned range, as well as the offsets of the tracks. With this
    information, we can just compute the set indices on demand.
    
    Bug: 1045599
    Change-Id: I26c8e1027d89c601de8e81d3f31850e061423844
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2757833
    Commit-Queue: Ana Sollano Kim <ansollan@microsoft.com>
    Reviewed-by: Ethan Jimenez <ethavar@microsoft.com>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877798}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -80,7 +80,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/custom/layout_worklet.h"
 #include "third_party/blink/renderer/core/layout/ng/custom/layout_worklet_global_scope_proxy.h"
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_box_strut.h"
-#include "third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h"
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_fieldset.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_box_fragment.h"
