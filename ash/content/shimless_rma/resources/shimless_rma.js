3e84a4b6ec7b5d950d71d5c012c52178bc1784d8
Joon Ahn <joonbug@chromium.org>
2021-04-30 17:31:24 +0000

    shimless: add shimless rma container skeleton
    Bug: 1198187
    Change-Id: I871a65ab1aec5f4072c97fc173a821111115d665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2857259
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#877974}

--- ash/content/shimless_rma/resources/shimless_rma.js
+++ ash/content/shimless_rma/resources/shimless_rma.js
@@ -3,7 +3,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import './shimless_rma_shared_css.js';
+
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+/**
+ * @typedef {{
+ *  componentIs: string,
+ *  btnNext: string,
+ *  btnNextLabel: string,
+ *  btnCancel: string,
+ *  btnBack: string,
+ * }}
+ */
+let PageInfo;
+
+/**
+ * // TODO(joonbug): update type to <RmaState, PageInfo> using a Map.
+ * @type {!Object<number, !PageInfo>}
+ */
+const StateComponentMapping = {
+  // TODO(joonbug): Update with enum and actual componentId.
+  0: {componentIs: 'component1'},
+};
 
 /**
  * @fileoverview
@@ -18,14 +41,65 @@ export class ShimlessRmaElement extends PolymerElement {
     return html`{__html_template__}`;
   }
 
+  static get properties() {
+    return {
+      /**
+       * Current PageInfo based on current state
+       * @protected
+       * @type {PageInfo}
+       */
+      currentPage_: {
+        type: Object,
+        value: {},
+      },
+    };
+  }
+
   /** @override */
   ready() {
     super.ready();
-    this.$.header.textContent = 'hello world';  // remove
+    this.fetchState_().then((state) => this.loadState_(state));
   }
 
-  close_() {
-    window.close();
+  /** @private */
+  fetchState_() {
+    // TODO(joonbug): fetch from fake
+    return Promise.resolve(0);
+  }
+
+  /** @private */
+  loadState_(state) {
+    const pageInfo = StateComponentMapping[state];
+    this.currentPage_ = pageInfo;
+    // TODO(joonbug): Load component
+  }
+
+  /** @protected */
+  isBtnHidden_(btn) {
+    return btn === 'hidden';
+  }
+
+  /** @protected */
+  isBtnDisabled_(btn) {
+    return btn === 'disabled';
+  }
+
+  /** @protected */
+  onBackBtnClicked_() {
+    // TODO(joonbug): fill with action
+    return;
+  }
+
+  /** @protected */
+  onNextBtnClicked_() {
+    // TODO(joonbug): fill with action
+    return;
+  }
+
+  /** @protected */
+  onCancelBtnClicked_() {
+    // TODO(joonbug): fill with action
+    return;
   }
 };
 
