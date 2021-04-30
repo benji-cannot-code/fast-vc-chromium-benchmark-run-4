ea1666c0cfb022c8c18d76bc72a19eea6c230908
Abigail Klein <abigailbklein@google.com>
2021-04-30 02:49:28 +0000

    [Live Caption] Make SpeechRecognitionRecognizerImpl and CrosSpeechRecognitionRecognizerImpl share the same enable_soda_ flag.
    This is a no-op cleanup.
    
    Bug: 1196105
    Change-Id: I9bd0c44b24af57da4fc8122de7ae14c32fd9f195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2857818
    Reviewed-by: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#877762}

--- chrome/services/speech/speech_recognition_recognizer_impl.h
+++ chrome/services/speech/speech_recognition_recognizer_impl.h
@@ -84,6 +84,8 @@ class SpeechRecognitionRecognizerImpl
       const std::string& language,
       const media::mojom::ConfidenceLevel confidence_level);
 
+  const bool enable_soda_;
+
  private:
   void OnCaptionBubbleClosed() final;
 
@@ -100,7 +102,6 @@ class SpeechRecognitionRecognizerImpl
   // the speech recognition service back to the renderer.
   mojo::Remote<media::mojom::SpeechRecognitionRecognizerClient> client_remote_;
 
-  bool enable_soda_ = false;
   std::unique_ptr<soda::SodaClient> soda_client_;
 
   std::unique_ptr<CloudSpeechRecognitionClient> cloud_client_;
