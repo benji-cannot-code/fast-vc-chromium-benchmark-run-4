bba0d90626cb112b7c15406985b178138f09791b
Alex Turner <alexmt@chromium.org>
2021-04-30 20:55:02 +0000

    Remove SubresourceFilterAgent::HasDocumentLoader()
    No uses remain
    
    Change-Id: I7c580b84ab6fc255009f65e14296f5188cf3a390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2860472
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878081}

--- components/subresource_filter/content/renderer/subresource_filter_agent.h
+++ components/subresource_filter/content/renderer/subresource_filter_agent.h
@@ -64,8 +64,6 @@ class SubresourceFilterAgent
   virtual bool IsProvisional();
   virtual bool IsSubframeCreatedByAdScript();
 
-  virtual bool HasDocumentLoader();
-
   // Injects the provided subresource |filter| into the DocumentLoader
   // orchestrating the most recently created document.
   virtual void SetSubresourceFilterForCurrentDocument(
