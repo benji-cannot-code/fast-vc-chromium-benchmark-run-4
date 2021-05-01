f000960e0e03108692ee885e926962f7c6da0d0d
Alex Moshchuk <alexmos@chromium.org>
2021-05-01 01:48:47 +0000

    Introduce Site Isolation for OAuth sites.
    This CL hooks up site isolation to the OAuth login detection mechanism
    (in chrome/browser/login_detection) that has been recently implemented
    for use with image compression.  When that mechanism discovers that a
    user has just logged in on a site via one of the supported OAuth flows
    (i.e., via redirects or popup), we start dynamically isolating that
    site via SiteInstance::StartIsolatingSite().  Logged-in sites are
    already stored in user prefs that are managed within the
    login_detection logic (see kOAuthSignedInSitesPref, which keeps at
    most 100 most recent entries), so to ensure that stored login sites
    get site-isolated on startup, we add another callout to read and
    isolate those sites at profile initialization time.  All of this is
    gated behind a new feature,
    site_isolation::features::kSiteIsolationForOAuthSites, and is intended
    to be used on Android, where it will complement site isolation for
    sites where users have typed a password.  To work, this also requires
    the login detection feature (login_detection::kLoginDetection), which
    is currently enabled by default on Android.
    
    Because the logic detection code currently lives in
    chrome/browser/login_detection, this isolation for login sites will
    not currently apply in WebLayer (which otherwise tries to match
    Android's site isolation policies).  To address this, we should move
    OAuth detection logic into a separate component in //components, which
    would then be used by both image compression and the site_isolation
    component.  This will happen in followup work.
    
    Bug: 960888
    Change-Id: I030a452bce26b38f5c200f766a36ffa0ee4cbcef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2848687
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878184}

--- chrome/browser/login_detection/login_detection_tab_helper.cc
+++ chrome/browser/login_detection/login_detection_tab_helper.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/login_detection/login_detection_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/prefs/pref_service.h"
+#include "components/site_isolation/site_isolation_policy.h"
 #include "components/ukm/content/source_url_recorder.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_entry.h"
@@ -89,8 +90,7 @@ void LoginDetectionTabHelper::DidFinishNavigation(
   // the time of login will be updated.
   if (auto signedin_site = oauth_login_detector_->GetSuccessfulLoginFlowSite(
           prev_navigation_url, navigation_handle->GetRedirectChain())) {
-    prefs::SaveSiteToOAuthSignedInList(GetPrefs(web_contents()),
-                                       *signedin_site);
+    ProcessNewSignedInSite(*signedin_site);
     RecordLoginDetectionMetrics(LoginDetectionType::kOauthFirstTimeLoginFlow,
                                 navigation_handle->GetNextPageUkmSourceId());
     return;
@@ -131,13 +131,19 @@ void LoginDetectionTabHelper::DidOpenAsPopUp(
 
 void LoginDetectionTabHelper::WebContentsDestroyed() {
   if (auto signedin_site = oauth_login_detector_->GetPopUpLoginFlowSite()) {
+    ProcessNewSignedInSite(*signedin_site);
     RecordLoginDetectionMetrics(
         LoginDetectionType::kOauthPopUpFirstTimeLoginFlow,
         ukm::GetSourceIdForWebContentsDocument(web_contents()));
-    prefs::SaveSiteToOAuthSignedInList(GetPrefs(web_contents()),
-                                       *signedin_site);
   }
   oauth_login_detector_.reset();
+}
+
+void LoginDetectionTabHelper::ProcessNewSignedInSite(
+    const GURL& signedin_site) {
+  prefs::SaveSiteToOAuthSignedInList(GetPrefs(web_contents()), signedin_site);
+  site_isolation::SiteIsolationPolicy::IsolateNewOAuthURL(
+      web_contents()->GetBrowserContext(), signedin_site);
 }
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(LoginDetectionTabHelper)
--- components/site_isolation/site_isolation_policy.cc
+++ components/site_isolation/site_isolation_policy.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_prefs/user_prefs.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/child_process_security_policy.h"
+#include "content/public/browser/site_instance.h"
 #include "content/public/browser/site_isolation_policy.h"
 
 namespace site_isolation {
@@ -45,6 +46,32 @@ bool SiteIsolationPolicy::IsIsolationForPasswordSitesEnabled() {
   // activates the field trial and assigns the client either to a control or an
   // experiment group - such assignment should be final.
   return base::FeatureList::IsEnabled(features::kSiteIsolationForPasswordSites);
+}
+
+// static
+bool SiteIsolationPolicy::IsIsolationForOAuthSitesEnabled() {
+  // If the user has explicitly enabled site isolation for OAuth sites from the
+  // command line, honor this regardless of policies that may disable site
+  // isolation.
+  if (base::FeatureList::GetInstance()->IsFeatureOverriddenFromCommandLine(
+          features::kSiteIsolationForOAuthSites.name,
+          base::FeatureList::OVERRIDE_ENABLE_FEATURE)) {
+    return true;
+  }
+
+  // Don't isolate anything when site isolation is turned off by the user or
+  // policy. This includes things like the switches::kDisableSiteIsolation
+  // command-line switch, the corresponding "Disable site isolation" entry in
+  // chrome://flags, enterprise policy controlled via
+  // switches::kDisableSiteIsolationForPolicy, and memory threshold checks in
+  // ShouldDisableSiteIsolationDueToMemoryThreshold().
+  if (!content::SiteIsolationPolicy::AreDynamicIsolatedOriginsEnabled())
+    return false;
+
+  // The feature needs to be checked last, because checking the feature
+  // activates the field trial and assigns the client either to a control or an
+  // experiment group - such assignment should be final.
+  return base::FeatureList::IsEnabled(features::kSiteIsolationForOAuthSites);
 }
 
 // static
@@ -129,6 +156,35 @@ void SiteIsolationPolicy::ApplyPersistedIsolatedOrigins(
 
   UMA_HISTOGRAM_COUNTS_1000(
       "SiteIsolation.SavedUserTriggeredIsolatedOrigins.Size", origins.size());
+}
+
+// static
+void SiteIsolationPolicy::IsolateStoredOAuthSites(
+    content::BrowserContext* browser_context,
+    const std::vector<url::Origin>& logged_in_sites) {
+  // Only isolate logged-in sites if the corresponding feature is enabled and
+  // other isolation requirements (such as memory threshold) are satisfied.
+  // Note that we don't clear logged-in sites from prefs if site isolation is
+  // disabled so that they can be used if isolation is re-enabled later.
+  if (!IsIsolationForOAuthSitesEnabled())
+    return;
+
+  auto* policy = content::ChildProcessSecurityPolicy::GetInstance();
+  policy->AddFutureIsolatedOrigins(
+      logged_in_sites,
+      content::ChildProcessSecurityPolicy::IsolatedOriginSource::USER_TRIGGERED,
+      browser_context);
+}
+
+// static
+void SiteIsolationPolicy::IsolateNewOAuthURL(
+    content::BrowserContext* browser_context,
+    const GURL& signed_in_url) {
+  if (!IsIsolationForOAuthSitesEnabled())
+    return;
+
+  content::SiteInstance::StartIsolatingSite(browser_context, signed_in_url,
+                                            false /* should_persist */);
 }
 
 // static
