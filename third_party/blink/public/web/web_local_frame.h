5fc33c1ef3e07a4a17a336711e779eccff2ca85b
Alex Turner <alexmt@chromium.org>
2021-05-01 01:44:52 +0000

    Propagate blink::FrameAdEvidence to blink::LocalFrames
    This cl propagates FrameAdEvidence from the browser to the appropriate
    blink::LocalFrame on each navigation. The one exception is a synchronous
    initial navigation to about:blank, where the FrameAdEvidence is
    populated by the renderer itself. This propagation ensures that blink::
    LocalFrames do not lose ad state (e.g. whether they were created by ad
    script) on cross-process navigations. The evidence will also soon be
    used to report the cause of AdTagging to the DevTools frontend.
    
    Note that this cl builds upon an preparatory cl (crrev.com/c/2704782).
    Follow-up cls will clean up some duplicate information by replacing
    existing variables in blink::LocalFrames with their equivalents on the
    frame's FrameAdEvidence (e.g. ad_frame_type_ and
    is_subframe_created_by_ad_script_).
    
    Bug: 1174237
    Change-Id: I3c36fddf7bff69c149b4217dc99097606cf8943a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2737859
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878178}

--- third_party/blink/public/web/web_local_frame.h
+++ third_party/blink/public/web/web_local_frame.h
@@ -19,11 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/web_sandbox_flags.mojom-shared.h"
 #include "third_party/blink/public/common/context_menu_data/untrustworthy_context_menu_params.h"
 #include "third_party/blink/public/common/css/page_size_type.h"
+#include "third_party/blink/public/common/frame/frame_ad_evidence.h"
 #include "third_party/blink/public/common/frame/user_activation_update_source.h"
 #include "third_party/blink/public/common/messaging/transferable_message.h"
 #include "third_party/blink/public/common/permissions_policy/permissions_policy_features.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
-#include "third_party/blink/public/mojom/ad_tagging/ad_frame.mojom-shared.h"
 #include "third_party/blink/public/mojom/blob/blob_url_store.mojom-shared.h"
 #include "third_party/blink/public/mojom/commit_result/commit_result.mojom-shared.h"
 #include "third_party/blink/public/mojom/devtools/devtools_agent.mojom-shared.h"
@@ -766,8 +766,11 @@ class WebLocalFrame : public WebFrame {
   // advertising purposes.
   bool IsAdSubframe() const override = 0;
 
-  // See blink::LocalFrame::SetIsAdSubframe()
-  virtual void SetIsAdSubframe(blink::mojom::AdFrameType ad_frame_type) = 0;
+  // See blink::LocalFrame::SetAdEvidence()
+  virtual void SetAdEvidence(const blink::FrameAdEvidence& ad_evidence) = 0;
+
+  // See blink::LocalFrame::AdEvidence()
+  virtual const base::Optional<blink::FrameAdEvidence>& AdEvidence() = 0;
 
   // True iff a script tagged as an ad was on the v8 stack when the frame was
   // created and the frame is a subframe. This is not currently propagated when
