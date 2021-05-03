276b9c4a0ed71e607b42f5fc504bd5221115e24a
Min Qin <qinmin@chromium.org>
2021-05-03 21:08:00 +0000

    Fix an issue that toast is not shown when removing a download
    BUG=1204399
    
    Change-Id: If614daf28661ae15c8494397bef71a0c758b77d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2863318
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878572}

--- chrome/test/data/webui/downloads/manager_tests.js
+++ chrome/test/data/webui/downloads/manager_tests.js
@@ -119,6 +119,7 @@ suite('manager tests', function() {
     flush();
     const list = manager.$$('iron-list');
     assertTrue(list.hidden);
+    assertTrue(toastManager.isToastOpen);
   });
 
   test('toolbar hasClearableDownloads set correctly', async () => {
