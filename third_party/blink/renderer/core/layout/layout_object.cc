410407e6c949e484acb3d4298e45823b59f381f4
Xianzhu Wang <wangxianzhu@chromium.org>
2021-05-03 22:36:06 +0000

    Remove partial paint invalidation
    It was used for non-composited plugins and canvases which are rare.
    We haven't seen any regression since we used full invalidation instead
    of partial invalidation for these elements.
    
    Bug: 1161155
    Change-Id: I54e7c33546163f0f9e69787dcc818330d931267a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2868750
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878618}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -4503,7 +4503,6 @@ void LayoutObject::ClearPaintInvalidationFlags() {
 #if DCHECK_IS_ON()
   DCHECK(!ShouldCheckForPaintInvalidation() || PaintInvalidationStateIsDirty());
 #endif
-  fragment_->SetPartialInvalidationLocalRect(PhysicalRect());
   if (!ShouldDelayFullPaintInvalidation()) {
     full_paint_invalidation_reason_ = PaintInvalidationReason::kNone;
     bitfields_.SetBackgroundNeedsFullPaintInvalidation(false);
@@ -4526,8 +4525,7 @@ bool LayoutObject::PaintInvalidationStateIsDirty() const {
          DescendantShouldCheckGeometryForPaintInvalidation() ||
          ShouldDoFullPaintInvalidation() ||
          SubtreeShouldDoFullPaintInvalidation() ||
-         MayNeedPaintInvalidationAnimatedBackgroundImage() ||
-         !fragment_->PartialInvalidationLocalRect().IsEmpty();
+         MayNeedPaintInvalidationAnimatedBackgroundImage();
 }
 #endif
 
