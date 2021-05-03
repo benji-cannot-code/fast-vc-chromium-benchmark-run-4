e0398b21503b78d8794452aa9b3285ea83331686
Guillaume Jenkins <gujen@google.com>
2021-05-03 16:06:39 +0000

    [iOS FRE] Small UI tweaks to base FRE screen view controller
    Changes the shared first run screen view controller so that the action
    buttons are now truncated on the right rather than in the middle
    (before: "Ex...mple", after: "Exampl..."). Also makes the screens
    non-dismissable by default on iOS 13+, since the default presentation
    mode of navigation screen view controllers allow users to dismiss them
    by swiping them down.
    
    Bug: 1186762
    Change-Id: I2c15561e4ea17d0e0742bdab3a096aa70f1886ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2862885
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#878429}

--- ios/chrome/browser/ui/first_run/first_run_screen_view_controller.mm
+++ ios/chrome/browser/ui/first_run/first_run_screen_view_controller.mm
@@ -63,6 +63,10 @@ constexpr CGFloat kPreviousContentVisibleOnScroll = 0.15;
 - (void)viewDidLoad {
   [super viewDidLoad];
 
+  if (@available(iOS 13.4, *)) {
+    self.modalInPresentation = !self.canDismissScreen;
+  }
+
   self.view.backgroundColor = [UIColor colorNamed:kBackgroundColor];
 
   // Create a layout guide for the margin between the subtitle and the screen-
@@ -384,6 +388,7 @@ constexpr CGFloat kPreviousContentVisibleOnScroll = 0.15;
         kFirstRunPrimaryActionAccessibilityIdentifier;
     _primaryActionButton.titleEdgeInsets =
         UIEdgeInsetsMake(0, kMoreArrowMargin, 0, kMoreArrowMargin);
+    _primaryActionButton.titleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
     [_primaryActionButton addTarget:self
                              action:@selector(didTapPrimaryActionButton)
                    forControlEvents:UIControlEventTouchUpInside];
@@ -434,6 +439,7 @@ constexpr CGFloat kPreviousContentVisibleOnScroll = 0.15;
   button.translatesAutoresizingMaskIntoConstraints = NO;
   button.titleLabel.adjustsFontForContentSizeCategory = YES;
   button.accessibilityIdentifier = accessibilityIdentifier;
+  button.titleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
 
   if (@available(iOS 13.4, *)) {
     button.pointerInteractionEnabled = YES;
