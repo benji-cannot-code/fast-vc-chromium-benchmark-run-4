d0991ca3c57c024a34868bdbf2b753efe70613dd
Jochen Eisinger <jochen@chromium.org>
2021-05-03 09:16:39 +0000

    Add missing microtasks scopes to pepper's v8 var converter
    Bug: 728583
    Change-Id: If1ae386a885f809ce2fb714bcc0275ef815c5db5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2862848
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878354}

--- content/renderer/pepper/v8_var_converter.cc
+++ content/renderer/pepper/v8_var_converter.cc
@@ -324,6 +324,8 @@ bool V8VarConverter::ToV8Value(const PP_Var& var,
   v8::Context::Scope context_scope(context);
   v8::Isolate* isolate = context->GetIsolate();
   v8::EscapableHandleScope handle_scope(isolate);
+  v8::MicrotasksScope microtasks_scope(
+      isolate, v8::MicrotasksScope::kDoNotRunMicrotasks);
 
   VarHandleMap visited_ids;
   ParentVarSet parent_ids;
@@ -476,7 +478,10 @@ bool V8VarConverter::FromV8ValueInternal(
     v8::Local<v8::Context> context,
     ppapi::ScopedPPVar* result_var) {
   v8::Context::Scope context_scope(context);
-  v8::HandleScope handle_scope(context->GetIsolate());
+  v8::Isolate* isolate = context->GetIsolate();
+  v8::HandleScope handle_scope(isolate);
+  v8::MicrotasksScope microtasks_scope(
+      isolate, v8::MicrotasksScope::kDoNotRunMicrotasks);
 
   HandleVarMap visited_handles;
   ParentHandleSet parent_handles;
@@ -574,8 +579,7 @@ bool V8VarConverter::FromV8ValueInternal(
 
         // Extend this test to cover more types as necessary and if sensible.
         if (!key->IsString() && !key->IsNumber()) {
-          NOTREACHED() << "Key \""
-                       << *v8::String::Utf8Value(context->GetIsolate(), key)
+          NOTREACHED() << "Key \"" << *v8::String::Utf8Value(isolate, key)
                        << "\" "
                           "is neither a string nor a number";
           return false;
@@ -589,7 +593,7 @@ bool V8VarConverter::FromV8ValueInternal(
           continue;
         }
 
-        v8::String::Utf8Value name_utf8(context->GetIsolate(), key_string);
+        v8::String::Utf8Value name_utf8(isolate, key_string);
 
         v8::Local<v8::Value> child_v8;
         if (!v8_object->Get(context, key).ToLocal(&child_v8))
