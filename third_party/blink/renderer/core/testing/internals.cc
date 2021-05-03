f98615e679cc304a41855c9eac752c8ef9055532
Rune Lillesveen <futhark@chromium.org>
2021-05-03 21:47:43 +0000

    Remove SelectRuleFeatureSet
    Left-over from Shadow DOM v0.
    
    Bug: 937746
    Change-Id: Ic6042dacfbb1800b4d95942581f4f7018f4f66d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2867534
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878592}

--- third_party/blink/renderer/core/testing/internals.cc
+++ third_party/blink/renderer/core/testing/internals.cc
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/document_timeline.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/css/properties/css_unresolved_property.h"
-#include "third_party/blink/renderer/core/css/select_rule_feature_set.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/dom_node_ids.h"
