31b78c83bf80de6e80b3fb3b58c718915c363a72
Sinan Sahin <sinansahin@google.com>
2021-05-03 21:12:39 +0000

    🤠 Disable MerchantTrustSignalsCoordinatorTest on tablets
    Bug: 1205108
    Change-Id: If21f6d16083b7536a31b43e9544040ad0d799d8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2866738
    Owners-Override: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878574}

--- chrome/browser/commerce/merchant_viewer/android/javatests/src/org/chromium/chrome/browser/merchant_viewer/MerchantTrustSignalsCoordinatorTest.java
+++ chrome/browser/commerce/merchant_viewer/android/javatests/src/org/chromium/chrome/browser/merchant_viewer/MerchantTrustSignalsCoordinatorTest.java
@@ -56,6 +56,7 @@ import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.display.DisplayAndroid;
 import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.test.util.UiDisableIf;
 import org.chromium.url.GURL;
 
 import java.util.concurrent.TimeUnit;
@@ -72,6 +73,7 @@ import java.util.concurrent.TimeUnit;
 @CommandLineFlags.
 Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE, "force-fieldtrials=Study/Group"})
 @DisableIf.Build(sdk_is_less_than = Build.VERSION_CODES.M)
+@DisableIf.Device(type = {UiDisableIf.TABLET})
 public class MerchantTrustSignalsCoordinatorTest {
     @Rule
     public final ChromeBrowserTestRule mBrowserTestRule = new ChromeBrowserTestRule();
