9b7028ea8cc3fd3587b46cf415278f6fd8df805c
Trent Apted <tapted@chromium.org>
2021-05-03 07:18:57 +0000

    Fix broken linux-chromeos-chrome build.
    https://crrev.com/c/2815129 migrated ChromeSessionManagerRlzTest from
    chrome:: to ash:: namespace
    
    The change passed the CQ because the usage happens to be behind the
    ENABLE_RLZ buildflag.
    
    r878095 fixed up some of the compile errors, but there are more.
    
    Bug: 1164001, 1204573
    Change-Id: I6682f70575362073fe665722e2eaacaf68adf8b4
    Fixed: 1204573
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2866525
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Owners-Override: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878331}

--- chrome/browser/ash/login/session/chrome_session_manager_browsertest.cc
+++ chrome/browser/ash/login/session/chrome_session_manager_browsertest.cc
@@ -36,6 +36,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
+namespace system {
+namespace {
+// TODO(https://crbug.com/1164001): remove when moved to ash::
+using ::chromeos::system::kRlzBrandCodeKey;
+using ::chromeos::system::ScopedFakeStatisticsProvider;
+}  // namespace
+}  // namespace system
 namespace {
 
 // TODO(https://crbug.com/1164001): remove when moved to ash::
