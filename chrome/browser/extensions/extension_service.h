5beda6b3e8aa2f0714bfab690f10be877d18aeb1
Xinghui Lu <xinghuilu@chromium.org>
2021-05-03 22:13:29 +0000

    Add BlocklistStatesInteractionUnitTest.
    Since we break out the blocklist handling into SafeBrowsingVerdictHandler
    and OmahaAttributesHandler, create a dedicated test suite to verify
    that the interaction between Safe Browsing blocklist, Omaha attribute
    blocklist and user action is working as intended.
    
    Currently only BLOCKLISTED_MALWARE state is verified in the test suite.
    More tests will be added after we start to consume other blocklist
    states from Omaha attributes.
    
    Bug: 1193695
    Change-Id: I8b320b3ca31fb6de1adbcb747ec09ba0115ea408
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846128
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878602}

--- chrome/browser/extensions/extension_service.h
+++ chrome/browser/extensions/extension_service.h
@@ -770,6 +770,7 @@ class ExtensionService : public ExtensionServiceInterface,
                            AcknowledgedStateBackFilled);
   friend class ::BlocklistedExtensionSyncServiceTest;
   friend class SafeBrowsingVerdictHandlerUnitTest;
+  friend class BlocklistStatesInteractionUnitTest;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionService);
 };
