dbcb524901df94605c21676a5a0ebebe344e05de
Vincent Boisselle <vincb@google.com>
2021-05-03 18:36:46 +0000

    [iOS]Remove calling #applicationDidBecomeActive when exiting safemode
    There is a bug where calling #applicationDidBecomeActive when getting out
    of safemode triggers a nil reference error, see crbug.com/1204496.
    
    This is only for iOS 12.
    
    Why calling #applicationDidBecomeActive when exiting safemode isn't
    required? See crrev.com/c/2857816/3/ios/chrome/app/application_delegate/app_state.mm#b387
    where we analyze the code and its usefulness.
      > Handling intents isn't needed for safemode.
      > Open a new tab if needed is already handled by the scene controller.
      > Opening the helper bubble is already handle on
        BrowserViewController#viewDidAppear.
      > Logging the startup duration metric isn't needed out of safemode. In
        fact, it should not happen.
      > This is only for iOS 12 + Post SafeMode, which is really an edgy case.
    
    
    Bug: 1204496, 1198957
    Change-Id: Ice9de258ad39baa0382fbd72f098d6f826031d1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2862289
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878485}

--- ios/chrome/app/application_delegate/app_state_unittest.mm
+++ ios/chrome/app/application_delegate/app_state_unittest.mm
@@ -528,9 +528,6 @@ TEST_F(AppStateTest, requiresHandlingAfterLaunchWithOptionsForegroundSafeMode) {
   [[appStateObserverMock expect] appStateDidExitSafeMode:appState];
   [[browserLauncherMock expect]
       startUpBrowserToStage:INITIALIZATION_STAGE_FOREGROUND];
-  id applicationDelegateMock = getApplicationDelegateMock();
-  [[applicationDelegateMock expect]
-      applicationDidBecomeActive:[UIApplication sharedApplication]];
 
   swizzleSafeModeShouldStart(YES);
 
@@ -554,7 +551,6 @@ TEST_F(AppStateTest, requiresHandlingAfterLaunchWithOptionsForegroundSafeMode) {
   EXPECT_OCMOCK_VERIFY(windowMock);
   EXPECT_OCMOCK_VERIFY(browserLauncherMock);
   EXPECT_OCMOCK_VERIFY(appStateObserverMock);
-  EXPECT_OCMOCK_VERIFY(applicationDelegateMock);
 
   EXPECT_EQ(InitStageFinal, appState.initStage);
 }
@@ -580,10 +576,6 @@ TEST_F(AppStateTest, requiresHandlingAfterLaunchWithOptionsForeground) {
                                      InitStageStart, InitStageFinal);
 
   [appState addObserver:appStateObserverMock];
-
-  id applicationDelegateMock = getApplicationDelegateMock();
-  [[applicationDelegateMock expect]
-      applicationDidBecomeActive:[UIApplication sharedApplication]];
 
   id browserLauncherMock = getBrowserLauncherMock();
   BrowserInitializationStageType stageForeground =
