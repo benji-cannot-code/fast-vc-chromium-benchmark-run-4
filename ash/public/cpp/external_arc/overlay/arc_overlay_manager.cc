2bec5529e325eae420a997a9a6b1a03807749e81
Lloyd Pique <lpique@chromium.org>
2021-05-03 22:26:12 +0000

    ARC overlays: Disable animations on overlay windows
    When the shell surface is created, it starts with an window creation
    animation, which includes a non-identity window transform.
    
    While the transform was being reset to identity after reattaching the
    window, native_view_host_aura was making a copy of the window transform.
    When the window was resized, it would then reapply that copy.
    
    The net effect was that the overlay content was scaled down slightly so
    it no longer covered the entire browser window as intended.
    
    This change simply disables animations during the creation process.
    
    Test: Peanut types test app
    Bug: 1198621
    
    Change-Id: I45ace68089aff5ccefe2c6896784dbd8a9693e0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2847722
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Lloyd Pique <lpique@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878614}

--- ash/public/cpp/external_arc/overlay/arc_overlay_manager.cc
+++ ash/public/cpp/external_arc/overlay/arc_overlay_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/shell_surface_base.h"
 #include "components/exo/shell_surface_util.h"
 #include "components/exo/surface.h"
+#include "ui/aura/client/aura_constants.h"
 #include "ui/aura/env.h"
 
 namespace {
@@ -18,6 +19,21 @@ namespace {
 ash::ArcOverlayManager* singleton = nullptr;
 
 const char* kBillingIdPrefix = "billing_id:";
+
+base::Optional<std::string> GetOverlayTokenForArcWindow(aura::Window* window) {
+  auto* shell_surface_base = exo::GetShellSurfaceBaseForWindow(window);
+  DCHECK(shell_surface_base);
+  auto* shell_root_surface = shell_surface_base->root_surface();
+  DCHECK(shell_root_surface);
+
+  // If the client_surface_id doesn't have a particular prefix, it is not an
+  // overlay candidate.
+  std::string client_surface_id = shell_root_surface->GetClientSurfaceId();
+  if (!base::StartsWith(client_surface_id, kBillingIdPrefix))
+    return {};
+
+  return client_surface_id.substr(strlen(kBillingIdPrefix));
+}
 
 }  // namespace
 
@@ -72,6 +88,15 @@ void ArcOverlayManager::OnWindowInitialized(aura::Window* window) {
   if (!window->delegate() || !ash::IsArcWindow(window))
     return;
 
+  // See if a potentially valid overlay token is set on the window, to confirm
+  // that it is intended to be an overlay window.
+  base::Optional<std::string> token = GetOverlayTokenForArcWindow(window);
+  if (!token)
+    return;
+
+  // Disable animations on overlay windows.
+  window->SetProperty(aura::client::kAnimationsDisabledKey, true);
+
   window_observations_.AddObservation(window);
 }
 
@@ -92,22 +117,14 @@ void ArcOverlayManager::OnWindowVisibilityChanged(aura::Window* window,
   // We do not need to keep observing the window.
   window_observations_.RemoveObservation(window);
 
-  auto* shell_surface_base = exo::GetShellSurfaceBaseForWindow(window);
-  DCHECK(shell_surface_base);
-  auto* shell_root_surface = shell_surface_base->root_surface();
-  DCHECK(shell_root_surface);
-
-  // If the client_surface_id doesn't have a particular prefix, it is not an
-  // overlay candidate.
-  std::string client_surface_id = shell_root_surface->GetClientSurfaceId();
-  if (!base::StartsWith(client_surface_id, kBillingIdPrefix))
+  // Get the overlay token.
+  base::Optional<std::string> token = GetOverlayTokenForArcWindow(window);
+  if (!token)
     return;
 
-  std::string overlay_token =
-      client_surface_id.substr(strlen(kBillingIdPrefix));
-
   // Find and attach the overlay to the host window.
-  RegisterOverlayWindow(std::move(overlay_token), shell_surface_base);
+  auto* shell_surface_base = exo::GetShellSurfaceBaseForWindow(window);
+  RegisterOverlayWindow(std::move(token).value(), shell_surface_base);
 }
 
 void ArcOverlayManager::RegisterOverlayWindow(
