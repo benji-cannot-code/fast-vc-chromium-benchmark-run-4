0e6d1e4a3c20361327bd22c09fba82e29030c43b
Michael Checo <michaelcheco@google.com>
2021-05-04 21:43:51 +0000

    Diagnostics: Wire up NetworkStateObserver in network-info
    Bug: 1125150
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I5f71570bc28cc64f106f46aa22a587f546dd0319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2860565
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879083}

--- chrome/test/data/webui/chromeos/diagnostics/network_info_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/network_info_test.js
@@ -5,6 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://diagnostics/network_info.js';
 
+import {Network} from 'chrome://diagnostics/diagnostics_types.js';
+import {fakeNetworkGuidInfoList, fakeWifiNetwork} from 'chrome://diagnostics/fake_data.js';
+import {FakeNetworkHealthProvider} from 'chrome://diagnostics/fake_network_health_provider.js';
+import {setNetworkHealthProviderForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
+
 import {assertFalse, assertTrue} from '../../chai_assert.js';
 import {flushTasks} from '../../test_util.m.js';
 
@@ -14,6 +19,14 @@ export function networkInfoTestSuite() {
   /** @type {?NetworkInfoElement} */
   let networkInfoElement = null;
 
+  /** @type {?FakeNetworkHealthProvider} */
+  let provider = null;
+
+  suiteSetup(() => {
+    provider = new FakeNetworkHealthProvider();
+    setNetworkHealthProviderForTesting(provider);
+  });
+
   setup(() => {
     document.body.innerHTML = '';
   });
@@ -21,45 +34,32 @@ export function networkInfoTestSuite() {
   teardown(() => {
     networkInfoElement.remove();
     networkInfoElement = null;
+    provider.reset();
   });
 
-  function initializeNetworkInfo() {
+  /**
+   * @param {string} guid
+   * @param {!Array<!Network>} networkStateList
+   */
+  function initializeNetworkInfo(guid, networkStateList) {
     assertFalse(!!networkInfoElement);
+    provider.setFakeNetworkGuidInfo(fakeNetworkGuidInfoList);
+    provider.setFakeNetworkState(guid, networkStateList);
 
     // Add the network info to the DOM.
     networkInfoElement = /** @type {!NetworkInfoElement} */ (
         document.createElement('network-info'));
     assertTrue(!!networkInfoElement);
+    networkInfoElement.guid = guid;
     document.body.appendChild(networkInfoElement);
 
     return flushTasks();
   }
 
-  test('NetworkInfoPopulated', () => {
-    return initializeNetworkInfo().then(() => {
+  test('NetworkStatePopulated', () => {
+    return initializeNetworkInfo('wifiGuid', [fakeWifiNetwork]).then(() => {
       dx_utils.assertElementContainsText(
-          networkInfoElement.$$('#cardTitle'), 'Network');
-    });
-  });
-
-  test('WifiInfoPresent', () => {
-    return initializeNetworkInfo().then(() => {
-      const wifiInfoElement = networkInfoElement.$$('wifi-info');
-      assertTrue(!!wifiInfoElement);
-    });
-  });
-
-  test('EthernetInfoPresent', () => {
-    return initializeNetworkInfo().then(() => {
-      const ethernetInfoElement = networkInfoElement.$$('ethernet-info');
-      assertTrue(!!ethernetInfoElement);
-    });
-  });
-
-  test('CellularInfoPresent', () => {
-    return initializeNetworkInfo().then(() => {
-      const cellularInfoElement = networkInfoElement.$$('cellular-info');
-      assertTrue(!!cellularInfoElement);
+          networkInfoElement.$$('#guid'), fakeWifiNetwork.guid);
     });
   });
 }
