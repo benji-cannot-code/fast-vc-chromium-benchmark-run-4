219894070da040d1a2876c0ad6dc50a6de034a29
Nate Chapin <japhet@chromium.org>
2021-05-04 21:47:07 +0000

    Centralize navigate event firing logic in AppHistory
    Change-Id: Ic114099a4e9bc85ddb432413792a0d0fd194779e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2857157
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879084}

--- third_party/blink/renderer/core/app_history/app_history.h
+++ third_party/blink/renderer/core/app_history/app_history.h
@@ -71,8 +71,6 @@ class CORE_EXPORT AppHistory final : public EventTargetWithInlineData,
                              UserNavigationInvolvement,
                              SerializedScriptValue* = nullptr);
 
-  ScriptPromise GetUnresolvingPromise(ScriptState*);
-
   // EventTargetWithInlineData overrides:
   const AtomicString& InterfaceName() const final;
   ExecutionContext* GetExecutionContext() const final {
