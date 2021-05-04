6b1ca558f5fdece738b5b266c070c09ae3a0279d
Kevin Marshall <kmarshall@chromium.org>
2021-05-04 20:18:07 +0000

    Reland "[fuchsia] Use InputMethodFuchsia::CancelComposition() for VK dismissal."
    This is a reland of b3ecf4347bf0c6c95064bddeabdbf7ebd46c677b
    
    Fixes a racy flake by deterministically prolonging test execution until
    a specific GMock mock expectation is hit.
    
    Test: ran tests locally with gtest_repeat=1000, no issues.
    
    Original change's description:
    > [fuchsia] Use InputMethodFuchsia::CancelComposition() for VK dismissal.
    >
    > Uses the InputMethod::CancelComposition() event for triggering
    > virtual keyboard dismissal.
    >
    > Removes Fuchsia-specific code from RenderWidgetHostViewAura for
    > dismissing the IME.
    >
    > Bug: 1166965
    > Change-Id: I988e2b3f61a50268e917a4f63474984ec4906488
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2816016
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Reviewed-by: Sharon Yang <yangsharon@chromium.org>
    > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#877679}
    
    Bug: 1166965
    Change-Id: Ib16b94234a19a841fe16c7075bae8ec01cd1d3c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2863923
    Reviewed-by: Sharon Yang <yangsharon@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879031}

--- ui/base/ime/fuchsia/virtual_keyboard_controller_fuchsia.cc
+++ ui/base/ime/fuchsia/virtual_keyboard_controller_fuchsia.cc
@@ -16,27 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 namespace {
 
-// Returns the FIDL enum representation of the current InputMode.
-fuchsia::input::virtualkeyboard::TextType ConvertTextInputMode(
-    ui::TextInputMode mode) {
-  switch (mode) {
-    case TEXT_INPUT_MODE_NUMERIC:
-    case TEXT_INPUT_MODE_DECIMAL:
-      return fuchsia::input::virtualkeyboard::TextType::NUMERIC;
-
-    case TEXT_INPUT_MODE_TEL:
-      return fuchsia::input::virtualkeyboard::TextType::PHONE;
-
-    case TEXT_INPUT_MODE_DEFAULT:
-    case TEXT_INPUT_MODE_NONE:
-    case TEXT_INPUT_MODE_TEXT:
-    case TEXT_INPUT_MODE_URL:
-    case TEXT_INPUT_MODE_EMAIL:
-    case TEXT_INPUT_MODE_SEARCH:
-      return fuchsia::input::virtualkeyboard::TextType::ALPHANUMERIC;
-  }
-}
-
 }  // namespace
 
 VirtualKeyboardControllerFuchsia::VirtualKeyboardControllerFuchsia(
@@ -48,13 +27,11 @@ VirtualKeyboardControllerFuchsia::VirtualKeyboardControllerFuchsia(
   base::ComponentContextForProcess()
       ->svc()
       ->Connect<fuchsia::input::virtualkeyboard::ControllerCreator>()
-      ->Create(std::move(view_ref),
-               ConvertTextInputMode(input_method_->GetTextInputMode()),
+      ->Create(std::move(view_ref), requested_type_,
                controller_service_.NewRequest());
 
-  controller_service_.set_error_handler([this](zx_status_t status) {
+  controller_service_.set_error_handler([](zx_status_t status) {
     ZX_LOG(ERROR, status) << "virtualkeyboard::Controller disconnected";
-    keyboard_visible_ = false;
   });
 
   WatchVisibility();
@@ -63,22 +40,29 @@ VirtualKeyboardControllerFuchsia::VirtualKeyboardControllerFuchsia(
 VirtualKeyboardControllerFuchsia::~VirtualKeyboardControllerFuchsia() = default;
 
 bool VirtualKeyboardControllerFuchsia::DisplayVirtualKeyboard() {
+  DVLOG(1) << "DisplayVirtualKeyboard (visible= " << keyboard_visible_ << ")";
+
   if (!controller_service_)
     return false;
 
-  controller_service_->SetTextType(
-      ConvertTextInputMode(input_method_->GetTextInputMode()));
+  UpdateTextType();
 
-  if (!keyboard_visible_)
-    controller_service_->RequestShow();
+  requested_visible_ = true;
+  controller_service_->RequestShow();
 
   return true;
 }
 
 void VirtualKeyboardControllerFuchsia::DismissVirtualKeyboard() {
+  DVLOG(1) << "DismissVirtualKeyboard (visible= " << keyboard_visible_ << ")";
+
   if (!controller_service_)
     return;
 
+  if (!requested_visible_ && !keyboard_visible_)
+    return;
+
+  requested_visible_ = false;
   controller_service_->RequestHide();
 }
 
@@ -101,8 +85,40 @@ void VirtualKeyboardControllerFuchsia::WatchVisibility() {
 }
 
 void VirtualKeyboardControllerFuchsia::OnVisibilityChange(bool is_visible) {
+  DVLOG(1) << "OnVisibilityChange " << is_visible;
   keyboard_visible_ = is_visible;
   WatchVisibility();
+}
+
+// Returns the FIDL enum representation of the current InputMode.
+fuchsia::input::virtualkeyboard::TextType
+VirtualKeyboardControllerFuchsia::GetFocusedTextType() const {
+  switch (input_method_->GetTextInputMode()) {
+    case TEXT_INPUT_MODE_NUMERIC:
+    case TEXT_INPUT_MODE_DECIMAL:
+      return fuchsia::input::virtualkeyboard::TextType::NUMERIC;
+
+    case TEXT_INPUT_MODE_TEL:
+      return fuchsia::input::virtualkeyboard::TextType::PHONE;
+
+    case TEXT_INPUT_MODE_DEFAULT:
+    case TEXT_INPUT_MODE_NONE:
+    case TEXT_INPUT_MODE_TEXT:
+    case TEXT_INPUT_MODE_URL:
+    case TEXT_INPUT_MODE_EMAIL:
+    case TEXT_INPUT_MODE_SEARCH:
+      return fuchsia::input::virtualkeyboard::TextType::ALPHANUMERIC;
+  }
+}
+
+void VirtualKeyboardControllerFuchsia::UpdateTextType() {
+  // Only send updates if the type has changed.
+  auto new_type = GetFocusedTextType();
+  if (new_type != requested_type_) {
+    DVLOG(1) << "SetTextType " << static_cast<int>(new_type);
+    controller_service_->SetTextType(new_type);
+    requested_type_ = new_type;
+  }
 }
 
 }  // namespace ui
