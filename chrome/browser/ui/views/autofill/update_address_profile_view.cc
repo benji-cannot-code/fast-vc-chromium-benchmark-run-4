c1b16f1c9785eeceee2e930d0d1ffcb39c965e1a
Mohamed Amir Yosef <mamir@chromium.org>
2021-05-04 19:16:12 +0000

    [Autofill] Add Update Address Prompt Subtitle
    This CL adds the subtitle for the update prompt.
    The subtitle contains a description of the profile to be updated if the
    user accepts the prompt.
    
    This CL also removes a TODO to support dark mode since all used UI
    components in this view have built-in dark mode support.
    
    Screenshot:
    https://screenshot.googleplex.com/cBpykoWVkMjJGMn.png
    
    Bug: 1167060
    Change-Id: I12512445bff478810904f9bb555bfbc580be2ce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2862781
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878996}

--- chrome/browser/ui/views/autofill/update_address_profile_view.cc
+++ chrome/browser/ui/views/autofill/update_address_profile_view.cc
@@ -169,6 +169,9 @@ UpdateAddressProfileView::UpdateAddressProfileView(
   // would have been a save prompt.
   DCHECK(controller_->GetOriginalProfile());
 
+  set_fixed_width(views::LayoutProvider::Get()->GetDistanceMetric(
+      views::DISTANCE_BUBBLE_PREFERRED_WIDTH));
+
   SetAcceptCallback(base::BindOnce(
       &SaveUpdateAddressProfileBubbleController::OnUserDecision,
       base::Unretained(controller_),
@@ -177,6 +180,28 @@ UpdateAddressProfileView::UpdateAddressProfileView(
       &SaveUpdateAddressProfileBubbleController::OnUserDecision,
       base::Unretained(controller_),
       AutofillClient::SaveAddressProfileOfferUserDecision::kDeclined));
+
+  SetLayoutManager(std::make_unique<views::FlexLayout>())
+      ->SetOrientation(views::LayoutOrientation::kVertical)
+      .SetCrossAxisAlignment(views::LayoutAlignment::kStretch)
+      .SetIgnoreDefaultMainAxisMargins(true)
+      .SetCollapseMargins(true)
+      .SetDefault(
+          views::kMarginsKey,
+          gfx::Insets(
+              /*vertical=*/ChromeLayoutProvider::Get()->GetDistanceMetric(
+                  DISTANCE_CONTROL_LIST_VERTICAL),
+              /*horizontal=*/0));
+
+  views::Label* subtitle_label = AddChildView(std::make_unique<views::Label>(
+      GetDescriptionForProfileToUpdate(
+          *controller_->GetOriginalProfile(),
+          g_browser_process->GetApplicationLocale()),
+      views::style::CONTEXT_LABEL, views::style::STYLE_SECONDARY));
+  subtitle_label->SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_LEFT);
+
+  views::View* main_content_view =
+      AddChildView(std::make_unique<views::View>());
 
   base::flat_map<ServerFieldType, std::pair<std::u16string, std::u16string>>
       profile_diff_map =
@@ -189,8 +214,8 @@ UpdateAddressProfileView::UpdateAddressProfileView(
       HasNonEmptySecondValues(profile_diff_map);
 
   // Build the GridLayout column set.
-  views::GridLayout* layout =
-      SetLayoutManager(std::make_unique<views::GridLayout>());
+  views::GridLayout* layout = main_content_view->SetLayoutManager(
+      std::make_unique<views::GridLayout>());
   views::ColumnSet* column_set = layout->AddColumnSet(kColumnSetId);
   const int column_divider = ChromeLayoutProvider::Get()->GetDistanceMetric(
       views::DISTANCE_RELATED_CONTROL_HORIZONTAL);
@@ -232,8 +257,6 @@ UpdateAddressProfileView::UpdateAddressProfileView(
     AddValuesRow(layout, profile_diff_map, /*show_row_label=*/true,
                  /*edit_button_callback=*/{});
   }
-
-  // TODO(crbug.com/1167060): Add support for dark mode.
 }
 
 bool UpdateAddressProfileView::ShouldShowCloseButton() const {
