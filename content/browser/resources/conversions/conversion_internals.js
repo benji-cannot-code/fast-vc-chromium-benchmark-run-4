5c4eeff4f2cd0154f019954e8613bbfad683766a
Andrew Paseltiner <apaseltiner@chromium.org>
2021-05-04 01:06:41 +0000

    [conversions] Improve conversion internals UI
    Derived from the suggestions in https://crbug.com/1144087.
    
    Screenshot of changes in https://crbug.com/1144087#c8.
    
    Bug: 1144087
    Change-Id: Ie56cda6b4c71052dc9d868b42537172f34e1f229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2860305
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878689}

--- content/browser/resources/conversions/conversion_internals.js
+++ content/browser/resources/conversions/conversion_internals.js
@@ -160,6 +160,7 @@ function updatePageData() {
   pageHandler.isMeasurementEnabled().then((response) => {
     $('feature-status-content').innerText =
         response.enabled ? 'enabled' : 'disabled';
+    $('feature-status-content').classList.toggle('disabled', !response.enabled);
 
     const htmlString = 'The #conversion-measurement-debug-mode flag is ' +
         '<strong>enabled</strong>, ' +
