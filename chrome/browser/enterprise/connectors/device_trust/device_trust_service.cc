dedd13f9dc5f9cc5a343718f97c01e66efd22733
Martin Rodriguez <rodmartin@google.com>
2021-05-04 13:28:03 +0000

    [Device Trust] Generate challenge response
    Chrome gets the challenge from the Idp, then it create the challenge
    response and send it back in the proper header.
    
    This CL includes
    - Handle the HTTP response from the Idp which contains the challenge so
    the client can generate the challenge response.
    - Signing message method using the key pair created by client.
    - Challenge response creation with the signature using the method
    mentioned above.
    - Use of VA protos for the handshake that were only part of Chrome OS.
    
    
    This CL doesn't contain the full logic for the challenge response
    creation, items that are missing:
    - Encrypted key info in the challenge response.
    - Verification of the challenge.
    
    Following CLs are going to be created to implement those logic. I
    decided to separated since those items need lot of code for the crypto
    part.
    
    Change-Id: Ie1d90f1007bb08ef48b0b168a200e22342530048
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827913
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Martin Rodriguez <rodmartin@google.com>
    Cr-Commit-Position: refs/heads/master@{#878847}

--- chrome/browser/enterprise/connectors/device_trust/device_trust_service.cc
+++ chrome/browser/enterprise/connectors/device_trust/device_trust_service.cc
@@ -5,9 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/enterprise/connectors/device_trust/device_trust_service.h"
 
+#include "base/base64.h"
+#include "base/base64url.h"
+#include "base/json/json_reader.h"
+#include "base/json/json_writer.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/enterprise/connectors/connectors_prefs.h"
+#include "chrome/browser/enterprise/connectors/device_trust/attestation_ca.pb.h"
+#include "chrome/browser/enterprise/connectors/device_trust/interface.pb.h"
 #include "chrome/browser/enterprise/connectors/device_trust/signal_reporter.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
@@ -26,6 +32,7 @@ DeviceTrustService::DeviceTrustService(Profile* profile)
   key_pair_ = std::make_unique<DeviceTrustKeyPair>();
 #endif  // defined(OS_LINUX) || defined(OS_WIN) || defined(OS_MAC)
 
+  attestation_service_ = std::make_unique<attestation::AttestationService>();
   pref_observer_.Init(prefs_);
   pref_observer_.Add(kContextAwareAccessSignalsAllowlistPref,
                      base::BindRepeating(&DeviceTrustService::OnPolicyUpdated,
@@ -125,5 +132,18 @@ std::string DeviceTrustService::GetAttestationCredentialForTesting() const {
   return key_pair_->ExportPEMPublicKey();
 }
 #endif  // defined(OS_LINUX) || defined(OS_WIN) || defined(OS_MAC)
+
+std::string DeviceTrustService::BuildChallengeResponse(
+    const std::string& challenge) {
+  ::attestation::SignEnterpriseChallengeRequest request;
+  ::attestation::SignEnterpriseChallengeReply result;
+  // Get the challenge from the SignedData json and create request.
+  request.set_challenge(
+      attestation_service_->JsonChallengeToProtobufChallenge(challenge));
+  attestation_service_->SignEnterpriseChallenge(request, &result);
+
+  return attestation_service_->ProtobufChallengeToJsonChallenge(
+      result.challenge_response());
+}
 
 }  // namespace enterprise_connectors
