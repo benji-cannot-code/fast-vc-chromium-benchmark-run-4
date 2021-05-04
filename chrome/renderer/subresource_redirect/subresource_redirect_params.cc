17a2c32b82cee8e47a4994204fd9b8bf992fd4d3
rajendrant <rajendrant@chromium.org>
2021-05-04 20:03:40 +0000

    Increase default robots fetch timeout
    Increases default timeout from 10ms to 2 seconds.
    Also changes SrcVideo compression to take a param for robots fetch timeout
    
    Change-Id: I5d336db9cd623ce34c627d4ee53f99e95820e1e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2871546
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879017}

--- chrome/renderer/subresource_redirect/subresource_redirect_params.cc
+++ chrome/renderer/subresource_redirect/subresource_redirect_params.cc
@@ -42,10 +42,16 @@ int64_t GetHintsReceiveTimeout() {
 }
 
 base::TimeDelta GetRobotsRulesReceiveTimeout() {
+  if (base::FeatureList::IsEnabled(blink::features::kSubresourceRedirect)) {
+    return base::TimeDelta::FromMilliseconds(
+        base::GetFieldTrialParamByFeatureAsInt(
+            blink::features::kSubresourceRedirect,
+            "robots_rules_receive_timeout_ms", 2000));
+  }
   return base::TimeDelta::FromMilliseconds(
       base::GetFieldTrialParamByFeatureAsInt(
-          blink::features::kSubresourceRedirect, "robots_rules_receive_timeout",
-          10));
+          blink::features::kSubresourceRedirectSrcVideo,
+          "robots_rules_receive_timeout_ms", 2000));
 }
 
 size_t GetFirstKSubresourceLimit() {
@@ -54,10 +60,16 @@ size_t GetFirstKSubresourceLimit() {
 }
 
 base::TimeDelta GetRobotsRulesReceiveFirstKSubresourceTimeout() {
+  if (base::FeatureList::IsEnabled(blink::features::kSubresourceRedirect)) {
+    return base::TimeDelta::FromMilliseconds(
+        base::GetFieldTrialParamByFeatureAsInt(
+            blink::features::kSubresourceRedirect,
+            "robots_rules_receive_first_k_timeout_ms", 2000));
+  }
   return base::TimeDelta::FromMilliseconds(
       base::GetFieldTrialParamByFeatureAsInt(
-          blink::features::kSubresourceRedirect,
-          "robots_rules_receive_first_k_timeout_ms", 10));
+          blink::features::kSubresourceRedirectSrcVideo,
+          "robots_rules_receive_first_k_timeout_ms", 2000));
 }
 
 int MaxRobotsRulesParsersCacheSize() {
