2f7410baec5a2ae988282cec348eac724a4cdc8c
Gavin Dodd <gavindodd@google.com>
2021-05-04 21:26:54 +0000

    shimless: Update RmadClient to use new proto
     - Implement GetCurrentState, TransitionNextState,
       TransitionPreviousState and AbortRma
     - Implement FakeRmadClient
     - Add unit tests.
    
    BUG=b:182242749
    TEST=Run unit tests
    
    Change-Id: Ia405050bf3f926e456686735e7660896b9bf05e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2870006
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879077}

--- chromeos/dbus/rmad/rmad_client.h
+++ chromeos/dbus/rmad/rmad_client.h
@@ -8,19 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "chromeos/dbus/dbus_method_call_status.h"
+#include "chromeos/dbus/rmad/rmad.pb.h"
 
 namespace dbus {
 class Bus;
 }
-
-// Temporary to allow code to compile while prototype rmad.proto is replaced.
-namespace rmad {
-class GetStateReply {
- public:
-  int state() { return 0; }
-};
-
-}  // namespace rmad
 
 namespace chromeos {
 
@@ -44,9 +36,27 @@ class COMPONENT_EXPORT(RMAD) RmadClient {
   static RmadClient* Get();
 
   // Asynchronously gets the current RMA state.
-  // The state contains an error code and the current state of the RMA process.
+  // The response contains an error code and the current state of the RMA
+  // process.
   virtual void GetCurrentState(
       DBusMethodCallback<rmad::GetStateReply> callback) = 0;
+  // Asynchronously attempts to transition to the next RMA state.
+  // The response contains an error code and the current state of the RMA
+  // process.
+  virtual void TransitionNextState(
+      const rmad::RmadState& state,
+      DBusMethodCallback<rmad::GetStateReply> callback) = 0;
+  // Asynchronously attempts to transition to the previous RMA state.
+  // The response contains an error code and the current state of the RMA
+  // process.
+  virtual void TransitionPreviousState(
+      DBusMethodCallback<rmad::GetStateReply> callback) = 0;
+
+  // Request the RMA process be cancelled.
+  // There is no guarantee the callback is called if abort is successful because
+  // the device will reboot.
+  // Returns RMAD_ERROR_OK on success or an error code.
+  virtual void AbortRma(DBusMethodCallback<rmad::AbortRmaReply> callback) = 0;
 
  protected:
   // Initialize/Shutdown should be used instead.
