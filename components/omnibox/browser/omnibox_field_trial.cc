5a8e748f2d8e5a518ac56a6ea55e9413e11e1c7b
Timothy Loh <timloh@chromium.org>
2021-05-04 05:34:25 +0000

    Revert "[omnibox] Clean up #omnibox-keyword-space-triggering feature"
    This reverts commit a52e9d6a57b61d9bf9c24070931f4c45c32ebd70.
    
    Reason for revert: The change prior to this broke tests, need to revert this so that can also be reverted.
    
    The other revert: https://chromium-review.googlesource.com/c/chromium/src/+/2870512
    
    Original change's description:
    > [omnibox] Clean up #omnibox-keyword-space-triggering feature
    >
    > This CL removes the keyword space triggering feature flag.
    > Double-space triggering is being deprecated and the option to disable
    > space triggering is being replaced with a setting in the manage
    > search engines page, gated by the #omnibox-keyword-trigger-setting flag
    > (crrev.com/c/2848754).
    >
    > Bug: 1186107
    > Change-Id: Id85775581cd20f0b85f01387598dd8f2035b0b45
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2847993
    > Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    > Reviewed-by: Orin Jaworski <orinj@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#878698}
    
    Bug: 1186107
    Change-Id: I7a5393fcb8dc5dfd9fd45f1b0b01b549ef3366aa
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2870907
    Auto-Submit: Timothy Loh <timloh@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Owners-Override: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878752}

--- components/omnibox/browser/omnibox_field_trial.cc
+++ components/omnibox/browser/omnibox_field_trial.cc
@@ -869,6 +869,18 @@ bool OmniboxFieldTrial::ShouldDisableCGIParamMatching() {
   return base::FeatureList::IsEnabled(omnibox::kDisableCGIParamMatching);
 }
 
+OmniboxFieldTrial::KeywordSpaceTrigger
+OmniboxFieldTrial::GetKeywordSpaceTrigger() {
+  if (base::GetFieldTrialParamByFeatureAsBool(
+          omnibox::kKeywordSpaceTriggering,
+          kKeywordSpaceTriggeringDoubleSpaceParam, false))
+    return DOUBLE_SPACE_TRIGGERS_KEYWORD;
+  else if (base::FeatureList::IsEnabled(omnibox::kKeywordSpaceTriggering))
+    return SINGLE_SPACE_TRIGGERS_KEYWORD;
+  else
+    return SPACE_TRIGGERING_DISABLED;
+}
+
 const char OmniboxFieldTrial::kBundledExperimentFieldTrialName[] =
     "OmniboxBundledExperimentV1";
 const char OmniboxFieldTrial::kDisableProvidersRule[] = "DisableProviders";
@@ -1022,6 +1034,9 @@ extern const char OmniboxFieldTrial::kBookmarkPathsUiAppendAfterTitle[] =
     "OmniboxBookmarkPathsUiAppendAfterTitle";
 extern const char OmniboxFieldTrial::kBookmarkPathsUiDynamicReplaceUrl[] =
     "OmniboxBookmarkPathsUiDynamicReplaceUrl";
+
+extern const char OmniboxFieldTrial::kKeywordSpaceTriggeringDoubleSpaceParam[] =
+    "KeywordSpaceTriggeringDoubleSpace";
 
 std::string OmniboxFieldTrial::internal::GetValueForRuleInContext(
     const std::string& rule,
