9ba434dcb30444a1ffd423c1ed9f434ab27b6cef
Zufeng Wang <zufeng@google.com>
2021-05-04 05:27:04 +0000

    Help app: Sort results by relevance in search handler
    Also move the result list resize into the for loop.
    
    Bug: b/182855408
    Change-Id: I6f51da58bb64e3054682c0d84162b1d213112d7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2862382
    Reviewed-by: dstockwell <dstockwell@google.com>
    Commit-Queue: Zufeng Wang <zufeng@google.com>
    Cr-Commit-Position: refs/heads/master@{#878751}

--- chromeos/components/help_app_ui/search/search_handler.cc
+++ chromeos/components/help_app_ui/search/search_handler.cc
@@ -14,6 +14,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 namespace help_app {
+namespace {
+
+// Order search results by relevance score. Higher relevance first.
+bool CompareSearchResults(const mojom::SearchResultPtr& first,
+                          const mojom::SearchResultPtr& second) {
+  return first->relevance_score > second->relevance_score;
+}
+
+}  // namespace
 
 SearchHandler::SearchHandler(
     SearchTagRegistry* search_tag_registry,
@@ -74,16 +83,15 @@ std::vector<mojom::SearchResultPtr> SearchHandler::GenerateSearchResultsArray(
     uint32_t max_num_results) const {
   std::vector<mojom::SearchResultPtr> search_results;
   for (const auto& result : local_search_service_results) {
+    if (search_results.size() == max_num_results) {
+      break;
+    }
     mojom::SearchResultPtr result_ptr = ResultToSearchResult(result);
     if (result_ptr)
       search_results.push_back(std::move(result_ptr));
   }
-  // TODO(b/182855408): Sort the search results.
+  std::sort(search_results.begin(), search_results.end(), CompareSearchResults);
 
-  // Now that the results have been sorted, limit the size of to
-  // |max_num_results|.
-  search_results.resize(
-      std::min(static_cast<size_t>(max_num_results), search_results.size()));
   return search_results;
 }
 
