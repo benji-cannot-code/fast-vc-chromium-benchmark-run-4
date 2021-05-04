f7e462f71258dcef23d2e25b708bd781cf96819d
Gordon Seto <gordonseto@google.com>
2021-05-04 21:17:41 +0000

    [CrOS Cellular] Add a11y label to unactivated pSIM network list items.
    Add a11y label to <network-list-item> for items that contain a pending
    activation pSIM network.
    
    Fixed: 1202979
    Change-Id: Ic7b0d0c7b540c4a8834b038fe77b95a2199b0af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872540
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/master@{#879071}

--- ui/webui/resources/cr_components/chromeos/network/network_list_item.js
+++ ui/webui/resources/cr_components/chromeos/network/network_list_item.js
@@ -68,7 +68,8 @@ Polymer({
     rowLabel: {
       type: String,
       notify: true,
-      computed: 'getRowLabel_(item, networkState, subtitle_)',
+      computed:
+          'getRowLabel_(item, networkState, subtitle_, isPSimPendingActivationNetwork_)',
     },
 
     buttonLabel: {
@@ -398,6 +399,12 @@ Polymer({
               this.getItemName_(), this.item.typeState.cellular.signalStrength);
         }
         if (status) {
+          if (this.isPSimPendingActivationNetwork_) {
+            return this.i18n(
+                'networkListItemLabelCellularUnactivatedWithConnectionStatus',
+                index, total, this.getItemName_(), status,
+                this.item.typeState.cellular.signalStrength);
+          }
           if (this.subtitle_) {
             return this.i18n(
                 'networkListItemLabelCellularWithConnectionStatusAndProviderName',
@@ -408,6 +415,12 @@ Polymer({
               'networkListItemLabelCellularWithConnectionStatus', index, total,
               this.getItemName_(), status,
               this.item.typeState.cellular.signalStrength);
+        }
+
+        if (this.isPSimPendingActivationNetwork_) {
+          return this.i18n(
+              'networkListItemLabelCellularUnactivated', index, total,
+              this.getItemName_(), this.item.typeState.cellular.signalStrength);
         }
 
         if (this.subtitle_) {
