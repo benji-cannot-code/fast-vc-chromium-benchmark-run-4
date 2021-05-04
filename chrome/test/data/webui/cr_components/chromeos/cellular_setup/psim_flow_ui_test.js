836d4de14736745a655a2fc46f8b55213089d63a
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-05-04 22:06:38 +0000

    [CrOS cellular] Focus on cancel button by default in pSIM flow
    This CL adds default focus to cancel button in pSIM sub flow and also
    adds aria live to description of setup loading page and base
    page. pSIM flow description changes based on the current page state,
    adding aria live makes sure that the new descriptions are read out
    each time it is updated.
    
    
    Video:
    https://drive.google.com/file/d/1dA98Kg98iHen6Xw7ddLFrUbIvZ635ud2/view?usp=sharing
    
    Bug: 1202990
    Change-Id: Ia2ec130e51933fdfe878d5781583038a8bee2a16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872739
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879092}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/psim_flow_ui_test.js
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/psim_flow_ui_test.js
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {FakeCellularSetupDelegate} from './fake_cellular_setup_delegate.m.js';
 // #import {FakeCarrierPortalHandlerRemote, FakeCellularSetupRemote} from './fake_cellular_setup_remote.m.js';
 // #import {MockMetricsPrivate} from './mock_metrics_private.m.js';
+// #import {eventToPromise, flushTasks} from 'chrome://test/test_util.m.js';
 // clang-format on
 
 suite('CrComponentsPsimFlowUiTest', function() {
@@ -68,7 +69,7 @@ suite('CrComponentsPsimFlowUiTest', function() {
         0);
   }
 
-  setup(function() {
+  setup(async function() {
     cellularCarrierHandler =
         new cellular_setup.FakeCarrierPortalHandlerRemote();
     cellularSetupRemote =
@@ -82,7 +83,11 @@ suite('CrComponentsPsimFlowUiTest', function() {
       timeoutFunction = fn;
       return 1;
     });
+
+    const focusNextButtonPromise =
+        test_util.eventToPromise('focus-default-button', pSimPage);
     pSimPage.initSubflow();
+    await focusNextButtonPromise;
     document.body.appendChild(pSimPage);
     Polymer.dom.flush();
   });
