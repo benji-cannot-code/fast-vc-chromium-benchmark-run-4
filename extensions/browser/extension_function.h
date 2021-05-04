7d7b6bf066f6115d450a0057f4b32160342203a0
Colin Blundell <blundell@chromium.org>
2021-05-04 16:51:02 +0000

    [base::Bind conversion] Update comment in extension_function.h
    This CL updates a reference to base::Bind() in a comment in
    extension_function.h and further tightens the exceptions in
    //PRESUBMIT.py. State of the world as of this CL:
    
    blundell:src(callback_in_extensions) $  git grep -l -P 'base::(Bind\(|(Cancelable)?(Callback[<:]|Closure))'
    PRESUBMIT.py
    docs/callback.md
    docs/process/lsc/large_scale_changes.md
    extensions/browser/api/webcam_private/visca_webcam.cc
    extensions/browser/api/webcam_private/visca_webcam.h
    extensions/browser/api/webcam_private/visca_webcam_unittest.cc
    extensions/browser/api/webcam_private/webcam.h
    extensions/browser/api/webcam_private/webcam_private_api.h
    extensions/browser/api/webcam_private/webcam_private_api_chromeos.cc
    third_party/blink/PRESUBMIT_test.py
    third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
    tools/clang/base_bind_rewriters/BaseBindRewriters.cpp
    tools/clang/base_bind_rewriters/tests/callback-temporary-converted-to-once-callback-expected.cc
    tools/clang/base_bind_rewriters/tests/callback-temporary-converted-to-once-callback-original.cc
    tools/clang/rewrite_raw_ptr_fields/RewriteRawPtrFields.cpp
    tools/gdb/gdb_chrome.py
    
    Bug: 1007786
    Change-Id: I7b9efb5ff46f42aa8afa90a73a64e0e2a7b6d623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2866547
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878888}

--- extensions/browser/extension_function.h
+++ extensions/browser/extension_function.h
@@ -417,7 +417,7 @@ class ExtensionFunction : public base::RefCountedThreadSafe<
   // this return value in those cases.
   //
   // FooExtensionFunction::Run() {
-  //   Helper::FetchResults(..., base::Bind(&Success));
+  //   Helper::FetchResults(..., base::BindOnce(&Success));
   //   if (did_respond()) return AlreadyResponded();
   //   return RespondLater();
   // }
@@ -425,9 +425,9 @@ class ExtensionFunction : public base::RefCountedThreadSafe<
   //   Respond(...);
   // }
   //
-  // Helper::FetchResults(..., callback) {
+  // Helper::FetchResults(..., base::OnceCallback callback) {
   //   if (...)
-  //     callback.Run(..);  // Synchronously call |callback|.
+  //     std::move(callback).Run(..);  // Synchronously call |callback|.
   //   else
   //     // Asynchronously call |callback|.
   // }
