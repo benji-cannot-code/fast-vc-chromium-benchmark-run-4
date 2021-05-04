8d3a968f4affced403f4f1516ee91473d420858c
Gayane Petrosyan <gayane@chromium.org>
2021-05-04 21:23:51 +0000

    [Webnotes] Add the main dialog with the top bar.
    Add the main dialog with the top bar, where only the close button is
    functional.
    
    Bug: 1201782
    Change-Id: I38e0e774737d9b3e7c51ee99cd44ce48de948ec3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2856735
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879075}

---
+++ chrome/browser/content_creation/notes/internal/android/java/src/org/chromium/chrome/browser/content_creation/notes/NoteCreationCoordinatorFactory.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.content_creation.notes;
+
+/**
+ * Factory for creating instances of the NoteCreationCoordinatorImpl.
+ */
+public class NoteCreationCoordinatorFactory {
+    /**
+     * @return a NoteCreationCoordinator instance.
+     */
+    public static NoteCreationCoordinator create() {
+        return new NoteCreationCoordinatorImpl();
+    }
+}
---
+++ chrome/browser/content_creation/notes/internal/android/java/src/org/chromium/chrome/browser/content_creation/notes/top_bar/TopBarCoordinator.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package org.chromium.chrome.browser.content_creation.notes.top_bar;
+
+import android.content.Context;
+import android.view.View;
+
+import org.chromium.chrome.browser.content_creation.internal.R;
+
+/**
+ * Coordinator for the TopBar.
+ */
+public class TopBarCoordinator {
+    public TopBarCoordinator(Context context, View parentView, Runnable closeButtonRunnable) {
+        TopBarView topBarView = (TopBarView) parentView.findViewById(R.id.top_bar);
+        topBarView.setOnCloseListener(closeButtonRunnable);
+    }
+}
