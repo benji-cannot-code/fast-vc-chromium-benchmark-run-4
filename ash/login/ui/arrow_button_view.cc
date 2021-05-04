a07cf103315fa6c241d5b898aecd55e9bb229600
Thomas Tellier <tellier@google.com>
2021-05-04 15:34:09 +0000

    [CrOS] Add accessible names for public account arrows on login screen
    Bug: 961930
    Change-Id: If75b6eb3a2e713510b511616f4e2e85d5441dae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2867649
    Auto-Submit: Thomas Tellier <tellier@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Thomas Tellier <tellier@google.com>
    Cr-Commit-Position: refs/heads/master@{#878873}

--- ash/login/ui/arrow_button_view.cc
+++ ash/login/ui/arrow_button_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_provider.h"
 #include "base/time/time.h"
 #include "cc/paint/paint_flags.h"
-#include "ui/accessibility/ax_node_data.h"
 #include "ui/compositor/layer.h"
 #include "ui/gfx/animation/multi_animation.h"
 #include "ui/gfx/animation/tween.h"
@@ -93,13 +92,6 @@ void ArrowButtonView::PaintButtonContents(gfx::Canvas* canvas) {
   // Draw the arc of the loading animation.
   if (loading_animation_)
     PaintLoadingArc(canvas, rect, loading_animation_->GetCurrentValue());
-}
-
-void ArrowButtonView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
-  LoginButton::GetAccessibleNodeData(node_data);
-  // TODO(tbarzic): Fix this - https://crbug.com/961930.
-  if (GetAccessibleName().empty())
-    node_data->SetNameExplicitlyEmpty();
 }
 
 void ArrowButtonView::EnableLoadingAnimation(bool enabled) {
