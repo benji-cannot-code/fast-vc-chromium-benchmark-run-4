7e21d4591e1c4ccca02ef6180f2cd8cb1899dedc
Tibor Goldschwendt <tiborg@chromium.org>
2021-05-04 00:38:44 +0000

    [ntp][modules] Info dialog for Drive module
    The Drive info dialog is similar to the task module info dialog. Thus,
    this CL creates a new info dialog element that is shared by both
    modules. This new dialog element also fixes two outstanding bugs with
    styling and behavior.
    
    Drive info dialog: screenshot/9c9sCCcLAkMZ4Pa
    Task module info dialog: screenshot/8awEsMDcfRZ9FL9
    
    Bug: 1200452
    Change-Id: Ib1683801e3327003cca1c1ea05e64e2800d181ce
    Fixed: 1203828, 1151122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2858119
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878679}

--- chrome/browser/resources/new_tab_page/modules/drive/module.js
+++ chrome/browser/resources/new_tab_page/modules/drive/module.js
@@ -4,11 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import '../module_header.js';
+import 'chrome://resources/cr_elements/cr_lazy_render/cr_lazy_render.m.js';
 
 import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
 import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {loadTimeData} from '../../i18n_setup.js';
+import {InfoDialogElement} from '../info_dialog.js';
 import {ModuleDescriptor} from '../module_descriptor.js';
 
 import {DriveProxy} from './drive_module_proxy.js';
@@ -34,6 +36,12 @@ class DriveModuleElement extends mixinBehaviors
       /** @type {Array<!drive.mojom.File>} */
       files: Array,
     };
+  }
+
+  /** @private */
+  onInfoButtonClick_() {
+    /** @type {InfoDialogElement} */ (this.$.infoDialogRender.get())
+        .showModal();
   }
 
   /** @private */
