f3ac7f6f17b31bbfc98aee4e36122398602081c4
dpapad <dpapad@chromium.org>
2021-05-04 18:20:22 +0000

    Settings: Make PRIVACY the parent route of Clear Browsing Data dialog.
    The CBD dialog appears after clicking on a row within the "Privacy"
    section, but it was configured as if it was a child of the top-level
    chrome://settings/ route.
    
    While this was not particularly problematic so far, it becomes a
    problem with upcoming changes to only show a single Settings section at
    a time.
    
    Fixed: 1204958
    Change-Id: I94d43a4240e605c748c50f4b1d98ff52a33ebe44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2867145
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878971}

--- chrome/browser/resources/settings/router.js
+++ chrome/browser/resources/settings/router.js
@@ -31,7 +31,7 @@ import './i18n_setup.js';
 
       /**
        * @type {boolean} Whether this route corresponds to a navigable
-       *     dialog. Those routes don't belong to a "section".
+       *     dialog. Those routes don't always belong to a "section".
        */
       this.isNavigableDialog = false;
 
@@ -104,7 +104,7 @@ import './i18n_setup.js';
      * @return {boolean}
      */
     isSubpage() {
-      return !!this.parent && !!this.section &&
+      return !this.isNavigableDialog && !!this.parent && !!this.section &&
           this.parent.section === this.section;
     }
   }
