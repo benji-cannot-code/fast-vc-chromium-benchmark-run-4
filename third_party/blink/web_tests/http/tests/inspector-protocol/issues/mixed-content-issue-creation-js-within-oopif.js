b0c10f2df88ec835ed00be5ee374864a668e5d15
Wolfgang Beyer <wolfi@chromium.org>
2021-05-04 07:26:08 +0000

    [DevTools] Fix flaky mixed content in OOPIF inspector protocol test
    Fixed: 1201348
    Change-Id: Idbfbf815cdfe459d5a7687a61e09f1f668a5fec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2867625
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#878770}

--- third_party/blink/web_tests/http/tests/inspector-protocol/issues/mixed-content-issue-creation-js-within-oopif.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/mixed-content-issue-creation-js-within-oopif.js
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   dp.Target.onAttachedToTarget(async e => {
     const dp = session.createChild(e.params.sessionId).protocol;
-    await dp.Network.enable();
-    await dp.Audits.enable();
 
     // We expect to receive two issues, one for a speculative prefetch and another for the actual fetch.
     dp.Audits.onIssueAdded(issue => {
@@ -33,6 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         eventReceived();
       }
     });
+
+    await dp.Network.enable();
+    await dp.Audits.enable();
   });
 
   await page.navigate('https://devtools.test:8443/inspector-protocol/resources/mixed-content-within-oopif.html');
