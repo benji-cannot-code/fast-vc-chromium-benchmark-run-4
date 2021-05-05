1ffa3d70cbacba72c9cc34af8ccb0b2732728095
Steven Bingler <bingler@chromium.org>
2021-05-05 17:30:59 +0000

    Create mojo serialization for StorageKey
    Create the appropriate struct traits and mojom files in order to use
    StorageKey with mojo.
    
    This supports the ability to send/receive StorageKeys between processes
    which is required for the refactor of the StorageAPIs to use
    StorageKey as their persistent storage key.
    
    This allows us to:
    * Rein in the different persistent store keys of the Storage APIs
    (not all of which are compatible which each other) into a single
    abstracted type.
    * Leverage StorageKey for upcoming storage partitioning work which
    will change what the serialization of the key looks like.
    
    This required pulling StorageKey out into its own component in order
    to avoid some circular dependency issues.
    
    Bug: 1199077
    Change-Id: Iaa6b30858742057fb20dd702eb456aaf2369ded9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2864063
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879431}

--- components/services/storage/public/cpp/storage_key.h
+++ components/services/storage/public/cpp/storage_key.h
@@ -8,12 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/component_export.h"
 #include "url/origin.h"
 
 namespace storage {
 
 // A class representing the key that Storage APIs use to key their storage on.
-class COMPONENT_EXPORT(STORAGE_SERVICE_PUBLIC) StorageKey {
+class COMPONENT_EXPORT(STORAGE_SERVICE_STORAGE_KEY_SUPPORT) StorageKey {
  public:
   StorageKey() = default;
   explicit StorageKey(const url::Origin& origin) : origin_(origin) {}
@@ -42,15 +43,15 @@ class COMPONENT_EXPORT(STORAGE_SERVICE_PUBLIC) StorageKey {
   const url::Origin& origin() const { return origin_; }
 
  private:
-  COMPONENT_EXPORT(STORAGE_SERVICE_PUBLIC)
+  COMPONENT_EXPORT(STORAGE_SERVICE_STORAGE_KEY_SUPPORT)
   friend bool operator==(const StorageKey& lhs, const StorageKey& rhs);
 
-  COMPONENT_EXPORT(STORAGE_SERVICE_PUBLIC)
+  COMPONENT_EXPORT(STORAGE_SERVICE_STORAGE_KEY_SUPPORT)
   friend bool operator!=(const StorageKey& lhs, const StorageKey& rhs);
 
   // Allows StorageKey to be used as a key in STL (for example, a std::set or
   // std::map).
-  COMPONENT_EXPORT(STORAGE_SERVICE_PUBLIC)
+  COMPONENT_EXPORT(STORAGE_SERVICE_STORAGE_KEY_SUPPORT)
   friend bool operator<(const StorageKey& lhs, const StorageKey& rhs);
 
   url::Origin origin_;
