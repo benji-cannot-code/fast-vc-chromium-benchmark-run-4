c9d5a38e38a27127b18a020941800b548a7470c9
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2021-05-05 20:00:07 +0000

    Tweak ChromeCleaner tests for DestroyProfileOnBrowserClose
    This test holds a ScopedKeepAlive to prevent BrowserProcess teardown.
    Add a ScopedProfileKeepAlive to prevent Profile* deletion when it
    closes all browser windows.
    
    Bug: 1191460
    Change-Id: I1330b43a0d8439658a3abeb40f0c0afe73989f10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2873025
    Auto-Submit: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879507}

--- chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_dialog_controller_impl_browsertest_win.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_dialog_controller_impl_browsertest_win.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
+#include "chrome/browser/profiles/profile_keep_alive_types.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/profiles/scoped_profile_keep_alive.h"
 #include "chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_win.h"
 #include "chrome/browser/safe_browsing/chrome_cleaner/mock_chrome_cleaner_controller_win.h"
 #include "chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.h"
@@ -125,9 +127,10 @@ IN_PROC_BROWSER_TEST_P(ChromeCleanerPromptUserTest,
 }
 
 IN_PROC_BROWSER_TEST_P(ChromeCleanerPromptUserTest, AllBrowsersClosed) {
-  std::unique_ptr<ScopedKeepAlive> keep_alive =
-      std::make_unique<ScopedKeepAlive>(KeepAliveOrigin::BROWSER,
-                                        KeepAliveRestartOption::DISABLED);
+  auto keep_alive = std::make_unique<ScopedKeepAlive>(
+      KeepAliveOrigin::BROWSER, KeepAliveRestartOption::DISABLED);
+  auto profile_keep_alive = std::make_unique<ScopedProfileKeepAlive>(
+      browser()->profile(), ProfileKeepAliveOrigin::kBrowserWindow);
 
   CloseAllBrowsers();
   base::RunLoop().RunUntilIdle();
