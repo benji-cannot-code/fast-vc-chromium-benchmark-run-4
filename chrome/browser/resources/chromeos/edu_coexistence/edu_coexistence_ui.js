55b4570a9fc4939c96c31a2944a5a011cd860de6
Danan S <danan@chromium.org>
2021-05-05 16:50:03 +0000

    Fix vertical placement of EDU Coexistence buttons
    Bug: 1205639
    Change-Id: I44b678cb832973c6ad6caca67351b5d82cce37bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872436
    Commit-Queue: Dan S <danan@chromium.org>
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879404}

--- chrome/browser/resources/chromeos/edu_coexistence/edu_coexistence_ui.js
+++ chrome/browser/resources/chromeos/edu_coexistence/edu_coexistence_ui.js
@@ -127,8 +127,8 @@ Polymer({
    */
   configureUiForGaiaFlow() {
     var currentUrl = new URL(this.webview_.src);
-    var contentContainer =
-        this.$$('edu-coexistence-template').$$('div.content-container');
+    var template = this.$$('edu-coexistence-template');
+    var contentContainer = template.$$('div.content-container');
 
     if (currentUrl.hostname !== this.controller_.getFlowOriginHostname()) {
       this.$$('edu-coexistence-button').newOobeStyleEnabled =
@@ -140,12 +140,11 @@ Polymer({
       this.showGaiaButtons_ = true;
       // Shrink the content-container so that the buttons line up more closely
       // with the server rendered buttons.
-      contentContainer.style.height = 'calc(100% - 245px)';
+      contentContainer.style.height = 'calc(100% - 90px)';
 
       // Don't show the "Next" button if the EDU authentication got forwarded to
       // a non-Google SSO page.
       this.showGaiaNextButton_ = currentUrl.hostname.endsWith('.google.com');
-
     } else {
       // Hide the GAIA Buttons.
       this.showGaiaButtons_ = false;
@@ -156,6 +155,8 @@ Polymer({
       // Restore the content container div to 100%
       contentContainer.style.height = '100%';
     }
+
+    template.showButtonFooter(this.showGaiaButtons_);
   },
 
   /** @override */
