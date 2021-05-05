b8138bc99323824226ee2f33fa06ce3ab0e9e3ec
Sunny Sachanandani <sunnyps@chromium.org>
2021-05-05 19:59:00 +0000

    Reland "gpu: Migrate D3D11VideoDecoder from EGLStream to EGLImage"
    This is a reland of 05832e084119614cb594baf8cbca02010fdfc55a
    
    Original change's description:
    > gpu: Migrate D3D11VideoDecoder from EGLStream to EGLImage
    >
    > This CL makes D3D11VideoDecoder allocate shared images the same way as
    > other clients using EGLImage with the details hidden inside the backing.
    >
    > It also adds factory functions to create d3d shared image backings for
    > common cases, and adds a new way of creating shared images from planar
    > textures that share keyed mutex and shared handle state.  This enables
    > the decoder to create NV12 textures with resource sharing so Dawn can
    > access them as one texture and GL/ANGLE can access them as two textures
    > with the correct keyed mutex synchronization done by the backing.  Also
    > includes unit tests for the new functionality.
    >
    > Also fixes the pixel format used for the P010 RGB10A2 copy fallback path.
    >
    > Bug: 1116101
    > Change-Id: I17c3aad49f08a43daf25203ff9fa9f1df5021171
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2630265
    > Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    > Reviewed-by: Frank Liberato <liberato@chromium.org>
    > Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#875331}
    
    Bug: 1116101, 1202029
    Change-Id: I5447a0434fe7c868f186f08274c1d5d6c94bbe4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872608
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879505}

--- ui/gl/swap_chain_presenter.h
+++ ui/gl/swap_chain_presenter.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gl {
 class DCLayerTree;
-class GLImageDXGI;
 class GLImageMemory;
 
 // SwapChainPresenter holds a swap chain, direct composition visuals, and other
@@ -162,14 +161,20 @@ class SwapChainPresenter : public base::PowerStateObserver {
   // Try presenting to a decode swap chain based on various conditions such as
   // global state (e.g. finch, NV12 support), texture flags, and transform.
   // Returns true on success.  See PresentToDecodeSwapChain() for more info.
-  bool TryPresentToDecodeSwapChain(GLImageDXGI* nv12_image,
-                                   const gfx::Rect& content_rect,
-                                   const gfx::Size& swap_chain_size);
+  bool TryPresentToDecodeSwapChain(
+      Microsoft::WRL::ComPtr<ID3D11Texture2D> texture,
+      unsigned array_slice,
+      const gfx::ColorSpace& color_space,
+      const gfx::Rect& content_rect,
+      const gfx::Size& swap_chain_size,
+      DXGI_FORMAT swap_chain_format);
 
   // Present to a decode swap chain created from compatible video decoder
   // buffers using given |nv12_image| with destination size |swap_chain_size|.
   // Returns true on success.
-  bool PresentToDecodeSwapChain(GLImageDXGI* nv12_image,
+  bool PresentToDecodeSwapChain(Microsoft::WRL::ComPtr<ID3D11Texture2D> texture,
+                                unsigned array_slice,
+                                const gfx::ColorSpace& color_space,
                                 const gfx::Rect& content_rect,
                                 const gfx::Size& swap_chain_size);
 
