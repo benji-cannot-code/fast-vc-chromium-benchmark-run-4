cb55165671dd7dab65c688c11a4211d04db383f2
Francois Doray <fdoray@chromium.org>
2021-05-05 20:01:13 +0000

    [net] Remove expired histograms DiskCache.BlockLoad* and DiskCache.Blocks*
    Bug: 1190838
    Change-Id: If0ce6925fd3711b70c97ee8d2d779d95f19d5883
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2873026
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879509}

--- net/disk_cache/blockfile/backend_impl.cc
+++ net/disk_cache/blockfile/backend_impl.cc
@@ -1998,9 +1998,6 @@ void BackendImpl::ReportStats() {
 
   stats_.ResetRatios();
   stats_.SetCounter(Stats::TRIM_ENTRY, 0);
-
-  if (GetCacheType() == net::DISK_CACHE)
-    block_files_.ReportStats();
 }
 
 void BackendImpl::UpgradeTo2_1() {
