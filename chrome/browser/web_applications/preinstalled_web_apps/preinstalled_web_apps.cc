f9a683bc781724a2600404f39501886f2af2929b
Alan Cutter <alancutter@chromium.org>
2021-05-05 06:39:32 +0000

    Add web app preinstall configs for Google Chat on Chrome OS
    This CL adds configs to preinstall Google Chat on Chrome OS devices
    for all new users (unmanaged, managed and child accounts).
    This behavior is guarded by the DefaultChatWebApp feature flag.
    
    Bug: 1203138
    Change-Id: I19f4ed5d344795602555aa14eebd5f2e9eaefc6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2870413
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879246}

--- chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_apps.cc
+++ chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_apps.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/web_applications/preinstalled_web_apps/google_calendar.h"
+#include "chrome/browser/web_applications/preinstalled_web_apps/google_chat.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
@@ -68,6 +69,7 @@ std::vector<ExternalInstallOptions> GetPreinstalledWebApps() {
       GetConfigForYouTube(),
 #if BUILDFLAG(IS_CHROMEOS_ASH)
       GetConfigForGoogleCalendar(),
+      GetConfigForGoogleChat(),
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
       // clang-format on
   };
--- chrome/browser/web_applications/preinstalled_web_apps_browsertest.cc
+++ chrome/browser/web_applications/preinstalled_web_apps_browsertest.cc
@@ -42,11 +42,11 @@ IN_PROC_BROWSER_TEST_F(PreinstalledWebAppsBrowserTest, CheckInstalledFields) {
 
   auto& provider = *WebAppProvider::Get(browser()->profile());
 
-  struct Expectation {
+  struct OfflineOnlyExpectation {
     const char* app_id;
     const char* install_url;
     const char* launch_url;
-  } kExpectations[] = {
+  } kOfflineOnlyExpectations[] = {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 #if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
     {
@@ -87,7 +87,22 @@ IN_PROC_BROWSER_TEST_F(PreinstalledWebAppsBrowserTest, CheckInstalledFields) {
     },
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
   };
-  size_t kExpectedCount = sizeof(kExpectations) / sizeof(kExpectations[0]);
+  size_t kOfflineOnlyExpectedCount =
+      sizeof(kOfflineOnlyExpectations) / sizeof(kOfflineOnlyExpectations[0]);
+
+  struct OnlineOnlyExpectation {
+    const char* install_url;
+  } kOnlineOnlyExpectations[] = {
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+    {
+        "https://mail.google.com/chat/download?usp=chrome_default",
+    },
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  };
+  size_t kOnlineOnlyExpectedCount =
+      sizeof(kOnlineOnlyExpectations) / sizeof(kOnlineOnlyExpectations[0]);
 
   base::RunLoop run_loop;
   provider.preinstalled_web_app_manager().LoadAndSynchronizeForTesting(
@@ -95,11 +110,17 @@ IN_PROC_BROWSER_TEST_F(PreinstalledWebAppsBrowserTest, CheckInstalledFields) {
           [&](std::map<GURL, ExternallyManagedAppManager::InstallResult>
                   install_results,
               std::map<GURL, bool> uninstall_results) {
-            EXPECT_EQ(install_results.size(), kExpectedCount);
+            EXPECT_EQ(install_results.size(),
+                      kOfflineOnlyExpectedCount + kOnlineOnlyExpectedCount);
 
-            for (const Expectation& expectation : kExpectations) {
+            for (const auto& expectation : kOfflineOnlyExpectations) {
               EXPECT_EQ(install_results[GURL(expectation.install_url)].code,
                         InstallResultCode::kSuccessOfflineOnlyInstall);
+            }
+
+            for (const auto& expectation : kOnlineOnlyExpectations) {
+              EXPECT_EQ(install_results[GURL(expectation.install_url)].code,
+                        InstallResultCode::kInstallURLLoadFailed);
             }
 
             EXPECT_EQ(uninstall_results.size(), 0u);
@@ -108,7 +129,7 @@ IN_PROC_BROWSER_TEST_F(PreinstalledWebAppsBrowserTest, CheckInstalledFields) {
           }));
   run_loop.Run();
 
-  for (const Expectation& expectation : kExpectations) {
+  for (const auto& expectation : kOfflineOnlyExpectations) {
     EXPECT_EQ(provider.registrar().GetAppLaunchUrl(expectation.app_id),
               GURL(expectation.launch_url));
   }
