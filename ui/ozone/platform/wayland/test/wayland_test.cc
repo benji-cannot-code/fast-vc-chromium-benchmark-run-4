3cf48d4c762385cd9764c34b1e0ea837b95bdff2
Nick Diego Yamane <nickdiego@igalia.com>
2021-05-05 17:33:05 +0000

    ozone/wayland: Switch to ServerConfig as test parameter
    Add an initial version of ServerConfig struct, which will enable more
    flexible per test suite configuration of the test Wayland server, so
    that tests can emulate more permutations of scenarios, when needed.
    
    Follow-up CLs will add real functionality, this one just adds it with a
    temporary implicit constructor to keep compatibility with current tests,
    which should get removed as soon as tests switch ServerConfig param.
    
    R=tonikitoo@igalia.com
    
    Bug: 1204670
    Test: No functional changes for now.
    Change-Id: I6aa121c8f87ab684d1e564a0f80f2b19908bd703
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2864066
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#879432}

--- ui/ozone/platform/wayland/test/wayland_test.cc
+++ ui/ozone/platform/wayland/test/wayland_test.cc
@@ -134,7 +134,7 @@ void WaylandTest::SendConfigureEvent(wl::MockXdgSurface* xdg_surface,
   // surfaces send other data like states, heights and widths.
   // Please note that toplevel surfaces may not exist if the surface was created
   // for the popup role.
-  if (GetParam() == kXdgShellV6) {
+  if (GetParam().shell_version == wl::ShellVersion::kV6) {
     if (xdg_surface->xdg_toplevel()) {
       zxdg_toplevel_v6_send_configure(xdg_surface->xdg_toplevel()->resource(),
                                       width, height, states);
