daf81e49ca8764d07386e9391789316da24a58af
Maksim Moskvitin <mmoskvitin@google.com>
2021-05-05 17:12:24 +0000

    Avoid base::Unretained() misuse in CaptionController
    CaptionController binds own method with base::Unretained() when passing
    callback to BrowserAccessibilityState. This doesn't look safe, since
    BrowserAccessibilityState can outlive the profile.
    
    This CL adapts WeakPtr to avoid use after free. This change should
    deflake *ClientSessionsWithDestroyProfileSyncTest on msan and reenables
    them.
    
    AX-Relnotes: n/a.
    Bug: 1185289, 1205894
    Change-Id: Ic3bf07e9df4cc028e61c64cf93391b4ec2da819d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874293
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#879420}

--- chrome/browser/accessibility/caption_controller.h
+++ chrome/browser/accessibility/caption_controller.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/memory/weak_ptr.h"
 #include "chrome/common/caption.mojom.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/soda/constants.h"
@@ -114,6 +115,8 @@ class CaptionController : public KeyedService,
   // feature being enabled--we wait for SODA to download first. This flag
   // ensures that the UI is not constructed or deconstructed twice.
   bool is_ui_constructed_ = false;
+
+  base::WeakPtrFactory<CaptionController> weak_ptr_factory_{this};
 };
 
 }  // namespace captions
