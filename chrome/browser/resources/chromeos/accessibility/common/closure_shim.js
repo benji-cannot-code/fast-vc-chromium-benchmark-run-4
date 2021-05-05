922bac397df99ddf0efd51a39c240f8f1cad49f5
David Tseng <dtseng@google.com>
2021-05-05 02:31:39 +0000

    Revive ChromeVoxEditableTextUnitTest
    These unit tests come from ChromeVox classic
    (ui/accessibility/extensions/chromevoxclassic).
    
    This is a subset of tests that depend only on
    ChromeVoxEditableTextBase. Other minor changes including ES6 class
    conversion was made.
    
    R=akihiroota@chromium.org
    
    Bug: none
    Test: this
    Change-Id: I3fdd1e432d38ee631a0d9e0c1fc0fe98c3652ed9
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2870935
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/master@{#879191}

--- chrome/browser/resources/chromeos/accessibility/common/closure_shim.js
+++ chrome/browser/resources/chromeos/accessibility/common/closure_shim.js
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var goog = {};
 
+goog.addDependency = function() {};
+
 goog.provide = function(n) {
   globalThis[n] = {};
 };
