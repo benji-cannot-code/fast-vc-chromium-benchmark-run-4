f2d744bfd2ee35a202d051090df9fb6496b92d0d
Kyle Milka <kmilka@chromium.org>
2021-05-05 00:49:36 +0000

    [SharedHighlighting] Fix resharing an existing highlight
    When resharing an existing highlight retrieve the selector from the
    document instead of copying the page url. There are cases where the
    website strips the fragments from the URL. Also when an existing
    highlight overlaps a new selection we should generate a link for the
    selection.
    
    Bug: 1184922
    Change-Id: If5a0b0aa4abbb46c78b574c1d1c38364147feca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2847391
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879150}

--- chrome/browser/renderer_context_menu/link_to_text_menu_observer.cc
+++ chrome/browser/renderer_context_menu/link_to_text_menu_observer.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/shared_highlighting/core/common/disabled_sites.h"
 #include "components/shared_highlighting/core/common/shared_highlighting_features.h"
 #include "components/shared_highlighting/core/common/shared_highlighting_metrics.h"
+#include "components/shared_highlighting/core/common/text_fragments_utils.h"
 #include "content/public/browser/context_menu_params.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
@@ -52,7 +53,7 @@ LinkToTextMenuObserver::~LinkToTextMenuObserver() = default;
 
 void LinkToTextMenuObserver::InitMenu(
     const content::ContextMenuParams& params) {
-  highlight_exists_ = params.opened_from_highlight;
+  link_needs_generation_ = !params.selection_text.empty();
   raw_url_ = params.page_url;
   if (params.page_url.has_ref()) {
     GURL::Replacements replacements;
@@ -98,8 +99,8 @@ void LinkToTextMenuObserver::ExecuteCommand(int command_id) {
   DCHECK(IsCommandIdSupported(command_id));
 
   if (command_id == IDC_CONTENT_CONTEXT_COPYLINKTOTEXT) {
-    if (highlight_exists_) {
-      CopyPageURLToClipboard();
+    if (!link_needs_generation_) {
+      ReshareLink();
     } else {
       if (ShouldPreemptivelyGenerateLink()) {
         CopyLinkToClipboard();
@@ -141,7 +142,7 @@ void LinkToTextMenuObserver::OverrideGeneratedSelectorForTesting(
 bool LinkToTextMenuObserver::ShouldPreemptivelyGenerateLink() {
   return base::FeatureList::IsEnabled(
              shared_highlighting::kPreemptiveLinkToTextGeneration) &&
-         !highlight_exists_;
+         link_needs_generation_;
 }
 
 void LinkToTextMenuObserver::RequestLinkGeneration() {
@@ -210,7 +211,14 @@ void LinkToTextMenuObserver::Timeout() {
   OnRequestLinkGenerationCompleted(std::string());
 }
 
-void LinkToTextMenuObserver::CopyPageURLToClipboard() {
+void LinkToTextMenuObserver::ReshareLink() {
+  GetRemote()->GetExistingSelectors(
+      base::BindOnce(&LinkToTextMenuObserver::OnGetExistingSelectorsComplete,
+                     weak_ptr_factory_.GetWeakPtr()));
+}
+
+void LinkToTextMenuObserver::OnGetExistingSelectorsComplete(
+    const std::vector<std::string>& selectors) {
   content::RenderFrameHost* main_frame =
       proxy_->GetWebContents()->GetMainFrame();
 
@@ -221,8 +229,11 @@ void LinkToTextMenuObserver::CopyPageURLToClipboard() {
 
   ui::ScopedClipboardWriter scw(ui::ClipboardBuffer::kCopyPaste,
                                 std::move(data_transfer_endpoint));
-  scw.WriteText(
-      base::UTF8ToUTF16(raw_url_.is_empty() ? std::string() : raw_url_.spec()));
+
+  GURL url_to_share = shared_highlighting::RemoveTextFragments(url_);
+  url_to_share = shared_highlighting::AppendSelectors(url_to_share, selectors);
+
+  scw.WriteText(base::UTF8ToUTF16(url_to_share.spec()));
 }
 
 void LinkToTextMenuObserver::RemoveHighlight() {
