9eab2ebddf3300f79bcc96ea02c919ca523c7e36
Xianzhu Wang <wangxianzhu@chromium.org>
2021-05-05 02:30:10 +0000

    Proactively update descendant cull rects when needed
    When a PaintLayer updates cull rect and the cull rect is expanded and
    is different from the old one, descendants should proactively update
    cull rects (i.e. avoid the CullRect::ChangedEnough logic).
    
    See crbug.com/1201004#c9 for more details.
    
    Bug: 1201004
    Change-Id: Iad28fd2740ea0022ab43b22973de6fe664c6e10a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2868769
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879189}

--- third_party/blink/renderer/platform/graphics/paint/cull_rect.cc
+++ third_party/blink/renderer/platform/graphics/paint/cull_rect.cc
@@ -197,7 +197,7 @@ bool CullRect::ApplyPaintPropertiesWithoutExpansion(
   return true;
 }
 
-void CullRect::ApplyPaintProperties(
+bool CullRect::ApplyPaintProperties(
     const PropertyTreeState& root,
     const PropertyTreeState& source,
     const PropertyTreeState& destination,
@@ -236,8 +236,7 @@ void CullRect::ApplyPaintProperties(
     // Either the transform or the clip of |source| is not an ancestor of
     // |destination|. Map infinite rect from the root.
     *this = Infinite();
-    ApplyPaintProperties(root, root, destination, old_cull_rect);
-    return;
+    return ApplyPaintProperties(root, root, destination, old_cull_rect);
   }
 
   // These are either the source transform/clip or the last scroll
@@ -264,7 +263,7 @@ void CullRect::ApplyPaintProperties(
             PropertyTreeState(*last_transform, *last_clip, effect_root),
             PropertyTreeState(*scroll_translation->UnaliasedParent(), *clip,
                               effect_root)))
-      return;
+      return false;
 
     last_scroll_translation_result =
         ApplyScrollTranslation(root.Transform(), *scroll_translation);
@@ -276,7 +275,7 @@ void CullRect::ApplyPaintProperties(
   if (!ApplyPaintPropertiesWithoutExpansion(
           PropertyTreeState(*last_transform, *last_clip, effect_root),
           destination))
-    return;
+    return false;
 
   // Since the cull rect mapping above can produce extremely large numbers in
   // cases of perspective, try our best to "normalize" the result by ensuring
@@ -324,6 +323,8 @@ void CullRect::ApplyPaintProperties(
   if (expanded && old_cull_rect &&
       !ChangedEnough(*old_cull_rect, expansion_bounds))
     rect_ = old_cull_rect->Rect();
+
+  return expanded;
 }
 
 bool CullRect::ChangedEnough(const CullRect& old_cull_rect,
