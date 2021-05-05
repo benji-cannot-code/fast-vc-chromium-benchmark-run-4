6420a2b1b3ed17a5390b4b4cc71cbb7b9108be31
Austin Sullivan <asully@chromium.org>
2021-05-05 08:10:53 +0000

    [Code Health] Remove uses of Value::CreateDeepCopy()
    This CL was uploaded by git cl split.
    
    R=victorvianna@google.com
    
    Bug: 1187013
    Change-Id: I4cf7b758d06fdb1ff49d609947df500f7cc45c57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2873358
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#879263}

--- chrome/browser/sync/test/integration/preferences_helper.cc
+++ chrome/browser/sync/test/integration/preferences_helper.cc
@@ -62,7 +62,7 @@ void ChangeListPref(int index,
   ListPrefUpdate update(GetPrefs(index), pref_name);
   base::ListValue* list = update.Get();
   for (const auto& it : new_value) {
-    list->Append(it.CreateDeepCopy());
+    list->Append(it.Clone());
   }
 }
 
