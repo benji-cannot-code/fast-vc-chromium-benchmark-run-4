85a947be139309f20e5864ce23e0291d78dd4bae
adamta <adamta@google.com>
2021-05-05 18:34:48 +0000

    [iOS] More checks before adding header section to content suggestions
    Ensures that Discover feed header item is properly created before
    setting it as the section's header. Also checks the flag to make sure
    that the feed is enabled.
    
    There is no footer in the content suggestions when the feed is enabled,
    so checks the flag there too.
    
    Bug: 1145106
    Change-Id: Ie346073c04ee42ba18295557a8043a99ce31a137
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2867888
    Commit-Queue: Adam Trudeau-Arcaro <adamta@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879463}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_updater.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_updater.mm
@@ -663,8 +663,10 @@ addSuggestionsToModel:(NSArray<CSCollectionViewItem*>*)suggestions
   NSString* footerTitle = sectionInfo.footerTitle;
 
   __weak ContentSuggestionsCollectionUpdater* weakSelf = self;
-  if (footerTitle && ![self.collectionViewController.collectionViewModel
-                         footerForSectionWithIdentifier:sectionIdentifier]) {
+  if (footerTitle &&
+      ![self.collectionViewController.collectionViewModel
+          footerForSectionWithIdentifier:sectionIdentifier] &&
+      !IsDiscoverFeedEnabled()) {
     ContentSuggestionsFooterItem* footer = [[ContentSuggestionsFooterItem alloc]
         initWithType:ItemTypeFooter
                title:sectionInfo.footerTitle
@@ -698,13 +700,18 @@ addSuggestionsToModel:(NSArray<CSCollectionViewItem*>*)suggestions
 
   NSInteger section = [model sectionForSectionIdentifier:sectionIdentifier];
   if ([self isDiscoverSection:section]) {
-    [model setHeader:[self headerForSectionInfo:sectionInfo]
-        forSectionWithIdentifier:sectionIdentifier];
+    CollectionViewItem* discoverSectionHeader =
+        [self headerForSectionInfo:sectionInfo];
+    // TODO(crbug.com/1145106): Potential fix for crash where cellClass is nil.
+    if ([discoverSectionHeader cellClass]) {
+      [model setHeader:discoverSectionHeader
+          forSectionWithIdentifier:sectionIdentifier];
+    }
     return;
   }
 
   if (![model headerForSectionWithIdentifier:sectionIdentifier] &&
-      sectionInfo.title) {
+      sectionInfo.title && !IsDiscoverFeedEnabled()) {
     DCHECK(IsFromContentSuggestionsService(sectionIdentifier));
     if ([self.sectionIdentifiersFromContentSuggestions
             containsObject:@(sectionIdentifier)]) {
