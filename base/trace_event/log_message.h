4602e177a808ac1c40e020d3bcfd90991e6922f7
Lei Zhang <thestig@chromium.org>
2021-05-05 22:22:53 +0000

    Clean up some nits in base/trace_event/.
    - Remove some unused includes and forward declarations.
    - Mark a comment mentioning std::string in backticks just to help make
      sure it does not set off cpplint.py.
    - Mark TraceID single-param ctors as explicit. Then fix build failures.
    
    Change-Id: Ib42380cb2809ccb928d7a509bbed2233fd650b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2843274
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879588}

--- base/trace_event/log_message.h
+++ base/trace_event/log_message.h
@@ -8,16 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include <memory>
 #include <string>
-#include <vector>
 
 #include "base/strings/string_piece.h"
 #include "base/trace_event/trace_event_impl.h"
 
 namespace base {
-
-class Value;
 
 namespace trace_event {
 
