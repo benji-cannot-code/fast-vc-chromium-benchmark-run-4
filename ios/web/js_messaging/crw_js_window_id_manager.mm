e877b573109321490089040ceed23de95535aada
Mike Dougherty <michaeldo@chromium.org>
2021-05-05 17:04:02 +0000

    Reland "[iOS] Disable window id injection failure DCHECK when JS is not allowed"
    This is a reland of c2b7b084cca299ffcc394713323a7bce95e8a5d3
    
    Original change's description:
    > [iOS] Disable window id injection failure DCHECK when JS is not allowed
    >
    > When JavaScript injection is not allowed in the currently loaded
    > webpage, the returned error has code WKErrorJavaScriptExceptionOccurred.
    > While this DCHECK should usually crash on JavaScript exceptions, this
    > particular error (when JavaScript is not allowed on the current page) is
    > an expected state and should be ignored.
    >
    > In order to differentiate this error, the script exception message must
    > be compared for this failure.
    >
    > Bug: 1202707
    > Change-Id: I1440613e113365787d04092605ff0c51e6d313bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2861839
    > Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    > Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#878132}
    
    Bug: 1202707
    Change-Id: I1a45749341719b0fd0adb2bad462f361c9b01381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2869045
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879412}

--- ios/web/js_messaging/crw_js_window_id_manager.mm
+++ ios/web/js_messaging/crw_js_window_id_manager.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web/js_messaging/crw_js_window_id_manager.h"
 
+#include "base/dcheck_is_on.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/sys_string_conversions.h"
@@ -19,6 +20,17 @@ namespace {
 // Number of random bytes in unique key for window ID. The length of the
 // window ID will be twice this number, as it is hexadecimal encoded.
 const size_t kUniqueKeyLength = 16;
+
+#if DCHECK_IS_ON()
+// Returns whether |error| represents a failure to execute JavaScript due to
+// JavaScript execution being disallowed.
+bool IsJavaScriptExecutionProhibitedError(NSError* error) {
+  return error.code == WKErrorJavaScriptExceptionOccurred &&
+         [@"Cannot execute JavaScript in this document"
+             isEqualToString:error.userInfo[@"WKJavaScriptExceptionMessage"]];
+}
+#endif
+
 }  // namespace
 
 @interface CRWJSWindowIDManager () {
@@ -62,10 +74,13 @@ const size_t kUniqueKeyLength = 16;
   [_webView evaluateJavaScript:scriptWithResult
              completionHandler:^(id result, NSError* error) {
                if (error) {
+#if DCHECK_IS_ON()
                  DCHECK(error.code == WKErrorWebViewInvalidated ||
-                        error.code == WKErrorWebContentProcessTerminated)
+                        error.code == WKErrorWebContentProcessTerminated ||
+                        IsJavaScriptExecutionProhibitedError(error))
                      << scriptWithResult << " failed with error "
                      << base::SysNSStringToUTF8(error.description);
+#endif
                  return;
                }
 
