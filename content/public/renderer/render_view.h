52c4b5c82ae606b68dfab685138d8f90d7d4af97
Dave Tapuska <dtapuska@chromium.org>
2021-05-05 16:00:03 +0000

    Remove RenderView being an IPC::Sender.
    RenderView is no longer used to send legacy IPC messages. Remove
    the IPC::Sender interface from the definition of the interface.
    
    BUG=1155202
    
    Change-Id: I6d91de7de27cdbb74bd384a2c3f9c4600854a0e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2869185
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879376}

--- content/public/renderer/render_view.h
+++ content/public/renderer/render_view.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "content/common/content_export.h"
-#include "ipc/ipc_sender.h"
 #include "ui/gfx/native_widget_types.h"
 
 namespace blink {
@@ -36,7 +35,7 @@ class RenderViewVisitor;
 // agnostic of frames and document content or structure. For more context,
 // please see https://crbug.com/467770 and
 // https://www.chromium.org/developers/design-documents/site-isolation.
-class CONTENT_EXPORT RenderView : public IPC::Sender {
+class CONTENT_EXPORT RenderView {
  public:
   // Returns the RenderView containing the given WebView.
   static RenderView* FromWebView(blink::WebView* webview);
@@ -58,7 +57,7 @@ class CONTENT_EXPORT RenderView : public IPC::Sender {
   virtual blink::WebView* GetWebView() = 0;
 
  protected:
-  ~RenderView() override {}
+  virtual ~RenderView() {}
 
  private:
   // This interface should only be implemented inside content.
