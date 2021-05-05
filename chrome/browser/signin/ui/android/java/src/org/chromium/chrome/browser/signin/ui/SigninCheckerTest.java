52237bc1af413a5fe73a0cf7df51d7ce10bd3f98
Alice Wang <aliceywang@chromium.org>
2021-05-05 14:03:32 +0000

    [Android][Test] Reduce test flakiness of SigninCheckerTest
    This CL reduces the test flakiness of SigninCheckerTest by replacing
    the problematic removeAccountAndWaitForSeeding() with removeAccount(),
    as waiting for seeding is not necessary here.
    
    Bug: 1205346
    Change-Id: I171b7537814779306fc437f41e54fd26d5810e28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874666
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879338}

--- chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/SigninCheckerTest.java
+++ chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/SigninCheckerTest.java
@@ -75,7 +75,7 @@ public class SigninCheckerTest {
         final CoreAccountInfo expectedPrimaryAccount =
                 mAccountManagerTestRule.addAccount(newAccountEmail);
 
-        mAccountManagerTestRule.removeAccountAndWaitForSeeding(oldAccount.getEmail());
+        mAccountManagerTestRule.removeAccount(oldAccount.getEmail());
 
         CriteriaHelper.pollUiThread(() -> {
             return expectedPrimaryAccount.equals(
@@ -93,7 +93,7 @@ public class SigninCheckerTest {
         when(mAccountRenameCheckerDelegateMock.getNewNameOfRenamedAccount(oldAccount.getEmail()))
                 .thenReturn(newAccountEmail);
 
-        mAccountManagerTestRule.removeAccountAndWaitForSeeding(oldAccount.getEmail());
+        mAccountManagerTestRule.removeAccount(oldAccount.getEmail());
 
         CriteriaHelper.pollUiThread(() -> {
             return !IdentityServicesProvider.get()
@@ -110,7 +110,7 @@ public class SigninCheckerTest {
         final CoreAccountInfo oldAccount =
                 mAccountManagerTestRule.addTestAccountThenSigninAndEnableSync();
 
-        mAccountManagerTestRule.removeAccountAndWaitForSeeding(oldAccount.getEmail());
+        mAccountManagerTestRule.removeAccount(oldAccount.getEmail());
 
         CriteriaHelper.pollUiThread(() -> {
             return !IdentityServicesProvider.get()
