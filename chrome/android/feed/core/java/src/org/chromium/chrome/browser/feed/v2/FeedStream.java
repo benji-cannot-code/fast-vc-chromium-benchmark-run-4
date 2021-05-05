2c22eee5363936392842dfab63b45393f22250a6
Cathy Li <chili@chromium.org>
2021-05-05 21:20:54 +0000

    Fix NPT caused by action handlers resetting to null when feed is hidden.
    This fix dismisses the bottom sheet when the feed is turned off.
    
    Bug: 1202587
    Change-Id: I271537cc07cc09a9cb9312d3efdf7ef8d58112ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874978
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879550}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedStream.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedStream.java
@@ -574,6 +574,9 @@ public class FeedStream implements Stream {
             notifyContentChange();
         }
 
+        // Dismiss bottomsheet if any is shown.
+        dismissBottomSheet();
+
         mContentManager.setHandlers(new HashMap<>());
         mContentManager = null;
 
