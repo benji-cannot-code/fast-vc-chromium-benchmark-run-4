83ecf0a5e119a0fb548f844dc0b304e633e8ca1d
Tommy Steimel <steimel@chromium.org>
2021-05-05 17:02:50 +0000

    Remove unnecessary include in ash/shell
    Bug: 1203845
    Change-Id: I2b327fa27350a23c0769c8743530fd098eaf9e77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872919
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879411}

--- ash/shell.cc
+++ ash/shell.cc
@@ -189,7 +189,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/viz/host/host_frame_sink_manager.h"
 #include "dbus/bus.h"
-#include "media/base/media_switches.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/env.h"
 #include "ui/aura/layout_manager.h"
