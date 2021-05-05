9a49f74d57065c74c55889047ba821859dc9107f
Austin Sullivan <asully@chromium.org>
2021-05-05 20:25:15 +0000

    [Code Health] Remove uses of Value::CreateDeepCopy()
    This CL was uploaded by git cl split.
    
    R=afakhry@chromium.org
    
    Bug: 1187013
    Change-Id: I3ead3b71310cad58239dcc273c83d579696890bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872993
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879522}

--- chrome/browser/ui/task_manager/task_manager_table_model.cc
+++ chrome/browser/ui/task_manager/task_manager_table_model.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/values.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/task_manager/task_manager_interface.h"
@@ -866,7 +867,8 @@ void TaskManagerTableModel::StoreColumnsSettings() {
 
   base::DictionaryValue::Iterator it(*columns_settings_);
   while (!it.IsAtEnd()) {
-    dict_update->Set(it.key(), it.value().CreateDeepCopy());
+    dict_update->Set(it.key(),
+                     base::Value::ToUniquePtrValue(it.value().Clone()));
     it.Advance();
   }
 
