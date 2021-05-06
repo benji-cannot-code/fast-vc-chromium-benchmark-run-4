42b89ce12031632b7b749b369d6147e1be47a292
Jan Kopanski <jkopanski@google.com>
2021-05-06 16:48:58 +0000

    Report queue check for DLP printing browsertests
    Bug: 1192610
    Change-Id: I502f9d26878cb06decafc28eea433b15031081e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2867449
    Commit-Queue: Jan Kopański <jkopanski@google.com>
    Auto-Submit: Jan Kopański <jkopanski@google.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879877}

--- chrome/browser/chromeos/policy/dlp/dlp_reporting_manager_test_helper.h
+++ chrome/browser/chromeos/policy/dlp/dlp_reporting_manager_test_helper.h
@@ -10,8 +10,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class DlpPolicyEvent;
 
+namespace policy {
+class DlpReportingManager;
+}  // namespace policy
+
 ::testing::Matcher<const DlpPolicyEvent&> IsDlpPolicyEvent(
     const DlpPolicyEvent& event);
 DlpPolicyEvent CreatePrintingRestrictedDlpEvent(const std::string& src_pattern);
+
+// Sets MockReportQueue for DlpReportingManager. Whenever AddRecord function of
+// MockReportQueue is called (a DLP restriction is triggered) a new
+// DlpPolicyEvent is pushed to |events|.
+void SetReportQueueForReportingManager(policy::DlpReportingManager* manager,
+                                       std::vector<DlpPolicyEvent>& events);
 
 #endif  // CHROME_BROWSER_CHROMEOS_POLICY_DLP_DLP_REPORTING_MANAGER_TEST_HELPER_H_
