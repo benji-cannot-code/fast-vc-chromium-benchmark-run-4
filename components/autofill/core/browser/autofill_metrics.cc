79f2add8c1bfedd040068573974bd1426726974e
Siyu An <siyua@chromium.org>
2021-05-06 20:52:09 +0000

    [VCN] Add metrics for virtual card metadata backend components.
    Add one metric to track the number of cards with virtual card metadata
    when profile starts.
    
    Add another one to log whenever new virtual card metadata is synced down.
    
    Design doc: https://docs.google.com/document/d/1VAWrXfJ6DPlKNOP5G72Lo5_v6BZvDoPS3xvdpCk_UxA/edit?resourcekey=0-vpqJ7wYdXLuI1E0F3-Rcug#bookmark=id.1t2nrfzawlii
    
    Bug: 1196021
    Change-Id: Icb31832a63111f55c93871364f385e2fe3569a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2868316
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880062}

--- components/autofill/core/browser/autofill_metrics.cc
+++ components/autofill/core/browser/autofill_metrics.cc
@@ -1926,6 +1926,16 @@ void AutofillMetrics::LogStoredCreditCardMetrics(
         "Autofill.StoredCreditCardDisusedCount.Server.Unmasked",
         num_disused_unmasked_cards);
   }
+
+  // Log the number of server cards that are enrolled with virtual cards.
+  size_t virtual_card_enabled_card_count = base::ranges::count_if(
+      server_cards, [](const std::unique_ptr<CreditCard>& card) {
+        return card->virtual_card_enrollment_state() ==
+               CreditCard::VirtualCardEnrollmentState::ENROLLED;
+      });
+  base::UmaHistogramCounts1000(
+      "Autofill.StoredCreditCardCount.Server.WithVirtualCardMetadata",
+      virtual_card_enabled_card_count);
 }
 
 // static
@@ -2667,6 +2677,12 @@ void AutofillMetrics::LogProfileUpdateEditedType(ServerFieldType edited_type) {
 void AutofillMetrics::LogNewProfileEditedType(ServerFieldType edited_type) {
   base::UmaHistogramEnumeration("Autofill.ProfileImport.NewProfileEditedType",
                                 ConvertEditedFieldTypeForMetrics(edited_type));
+}
+
+// static
+void AutofillMetrics::LogVirtualCardMetadataSynced(bool existing_card) {
+  base::UmaHistogramBoolean("Autofill.VirtualCard.MetadataSynced",
+                            existing_card);
 }
 
 }  // namespace autofill
