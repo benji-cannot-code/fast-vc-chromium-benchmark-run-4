d9831a199026645c0c5246baa55ad9658e7d50e4
Vyshu <vyshu@google.com>
2021-05-06 16:22:47 +0000

    Close notification when CloseRelaunchNotification is called
    Previously, closing these notifications was not supported despite
    having the function for it. Change the update state to enable
    hiding these notifications.
    
    BUG=1205409, b:183423224
    TEST=deploy to DUT, enable repeated updates and verify that
    notification is removed when starting the next update and then
    reshown
    
    Change-Id: If433f9eca02393a9a013973c37132f640d4dccf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872085
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Vyshu Khota <vyshu@google.com>
    Cr-Commit-Position: refs/heads/master@{#879858}

--- ash/system/model/system_tray_model.cc
+++ ash/system/model/system_tray_model.cc
@@ -101,6 +101,10 @@ void SystemTrayModel::SetUpdateNotificationState(
                                              notification_body);
 }
 
+void SystemTrayModel::ResetUpdateState() {
+  update_model()->ResetUpdateAvailable();
+}
+
 void SystemTrayModel::SetUpdateOverCellularAvailableIconVisible(bool visible) {
   update_model()->SetUpdateOverCellularAvailable(visible);
 }
