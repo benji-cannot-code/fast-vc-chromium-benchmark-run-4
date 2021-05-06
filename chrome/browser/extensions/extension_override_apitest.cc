c5200a6c025f87df747d14921c13be539f3ce02f
David Bertoni <dbertoni@chromium.org>
2021-05-06 23:59:21 +0000

    [CodeHealth] Remove uses of ListValue::begin()/end() in /chrome/browser/extensions.
    This CL removes calls to these deprecated member functions. It also
    converts to the newer "for" loop syntax when possible, along with
    other minor cleanup.
    
    This CL was uploaded by git cl split.
    
    R=finnur@chromium.org
    
    Bug: 1187107
    Change-Id: I22b3992f745b5d404de4849ae9a8d8eb97c52e1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2877225
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880149}

--- chrome/browser/extensions/extension_override_apitest.cc
+++ chrome/browser/extensions/extension_override_apitest.cc
@@ -57,7 +57,7 @@ class ExtensionOverrideTest : public ExtensionApiTest {
       return false;
 
     std::set<std::string> seen_overrides;
-    for (const auto& val : *values) {
+    for (const auto& val : values->GetList()) {
       const base::DictionaryValue* dict = nullptr;
       std::string entry;
       if (!val.GetAsDictionary(&dict) || !dict->GetString("entry", &entry) ||
