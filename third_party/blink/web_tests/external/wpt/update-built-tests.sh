56f3ec06cc842ee28ee43131afd55262a875d7e9
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-05-06 14:29:27 +0000

    Import wpt@6a96f5c7fe3c2fd837f1814fef4fac93ade8344e
    Using wpt-import in Chromium edf588d46a8e14390d6156c39796c0aad6a1a7f1.
    With Chromium commits locally applied on WPT:
    df3cf2af98 "Implement <popup> focus, autofocus, delegatesfocus"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org:
      external/wpt/fullscreen
    iclelland@chromium.org:
      external/wpt/permissions-policy
    npm@chromium.org:
      external/wpt/event-timing
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: I35b4b7869fab3a9e999323169b1435d990c17ef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2876577
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Commit-Queue: Jason Chase <chasej@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879819}

--- third_party/blink/web_tests/external/wpt/update-built-tests.sh
+++ third_party/blink/web_tests/external/wpt/update-built-tests.sh
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env sh
 set -ex
 
+# This list should be kept in sync with tools/ci/jobs.py.
+conformance-checkers/tools/build.sh
 html/canvas/tools/build.sh
 infrastructure/assumptions/tools/build.sh
 html/tools/build.sh
