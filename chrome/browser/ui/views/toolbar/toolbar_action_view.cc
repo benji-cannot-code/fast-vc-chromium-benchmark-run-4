86a86431cd9fd91ab0fcba6b9c6401c9d0a29646
Devlin Cronin <rdevlin.cronin@chromium.org>
2021-05-06 23:30:15 +0000

    [Extensions UI] Disable extension dragging in incognito
    Incognito windows should not persist user preferences. Disable extension
    dragging in incognito. This way, incognito windows can stay in-sync with
    on-the-record windows (modulo extensions that are not enabled), but have
    "read-only" access.
    
    This is inline with our current behavior for pinned extensions.
    
    Bug: 1203833
    Change-Id: Ic27bce56fdbd73a0bf2a2f5ddfc5e193bcedd0f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872987
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880133}

--- chrome/browser/ui/views/toolbar/toolbar_action_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_action_view.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_ink_drop_util.h"
 #include "components/sessions/content/session_tab_helper.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/notification_source.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -67,6 +68,14 @@ ToolbarActionView::ToolbarActionView(
   view_controller_->SetDelegate(this);
   SetHorizontalAlignment(gfx::ALIGN_CENTER);
   set_drag_controller(delegate_);
+  // Normally, the notify action is determined by whether a view is draggable
+  // (and is set to press for non-draggable and release for draggable views).
+  // However, ToolbarActionViews may be draggable or non-draggable depending on
+  // whether they are shown in an incognito window. We want to preserve the same
+  // trigger event to keep the UX (more) consistent. Set all ToolbarActionViews
+  // to trigger on mouse release.
+  button_controller()->set_notify_action(
+      views::ButtonController::NotifyAction::kOnRelease);
 
   context_menu_controller_ =
       std::make_unique<ExtensionContextMenuController>(view_controller);
@@ -147,6 +156,10 @@ void ToolbarActionView::UpdateState() {
 
 gfx::ImageSkia ToolbarActionView::GetIconForTest() {
   return GetImage(views::Button::STATE_NORMAL);
+}
+
+int ToolbarActionView::GetDragOperationsForTest(const gfx::Point& point) {
+  return views::View::GetDragOperations(point);
 }
 
 gfx::Size ToolbarActionView::CalculatePreferredSize() const {
