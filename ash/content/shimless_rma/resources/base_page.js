00a7c1ab7600e972deb6280352f3c95249729872
Joon Ahn <joonbug@chromium.org>
2021-05-06 19:34:24 +0000

    shimless: base page component skeleton
    Bug: 1198187
    Change-Id: Ied39d9c140d6dff9c8a6608f393946beafa420b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872737
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880006}

---
+++ ash/content/shimless_rma/resources/base_page.js
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './shimless_rma_shared_css.js';
+
+import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+/**
+ * @fileoverview
+ * 'base-page' is the main page for the shimless rma process modal dialog.
+ */
+export class BasePageElement extends PolymerElement {
+  static get is() {
+    return 'base-page';
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
+  }
+
+  static get properties() {
+    return {
+      /**
+       * Orientation of the page, either row or column.
+       * @type {string}
+       */
+      orientation: {
+        type: String,
+        value: "row",
+      },
+    };
+  }
+
+  /** @override */
+  ready() {
+    super.ready();
+  }
+
+  /** @protected */
+  headerClass_() {
+    return `header-${this.orientation}`;
+  }
+
+  /** @protected */
+  bodyClass_() {
+    return `body-${this.orientation}`;
+  }
+};
+
+customElements.define(BasePageElement.is, BasePageElement);
