e9043f91be14dda96846041d7172624ba2bb1b50
Shakti Sahu <shaktisahu@chromium.org>
2021-05-06 01:55:54 +0000

    Revert "Download controller : Refactored permission related methods"
    This reverts commit dd5bfd95877a53c152405db599e7dff34c9b6aad.
    
    Reason for revert: This refactor gives rise to crbug/1191286. We
    must use the PermissionDelegate provided by the ChromeActivity, since
    only the activity gets notified about the permission result from
    android through #onRequestPermissionsResult(). Will look for a better
    fix for the refactor later.
    
    Original change's description:
    > Download controller : Refactored permission related methods
    >
    > This CL refactors permission related methods from DownloadController to
    > remove ChromeActivity reference.
    >
    > Bug: 1155357
    > Change-Id: Ice0d18a5b508c0b2917adcf3d39296fc0a196662
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2745859
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#863679}
    
    Bug: 1155357, 1191286
    Change-Id: I456eae685e9cb0da1fe26ce1a8593134db00cf95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2873827
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879660}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadController.java
@@ -16,15 +16,14 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.app.ChromeActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.components.download.DownloadCollectionBridge;
 import org.chromium.components.permissions.AndroidPermissionRequester;
 import org.chromium.content_public.browser.BrowserStartupController;
-import org.chromium.ui.base.ActivityAndroidPermissionDelegate;
 import org.chromium.ui.base.AndroidPermissionDelegate;
 import org.chromium.ui.base.PermissionCallback;
-
-import java.lang.ref.WeakReference;
+import org.chromium.ui.base.WindowAndroid;
 
 /**
  * Java counterpart of android DownloadController. Owned by native.
@@ -61,7 +60,6 @@ public class DownloadController {
     }
 
     private static Observer sObserver;
-    private static AndroidPermissionDelegate sAndroidPermissionDelegateForTesting;
 
     public static void setDownloadNotificationService(Observer observer) {
         if (ChromeFeatureList.isEnabled(ChromeFeatureList.DOWNLOAD_OFFLINE_CONTENT_PROVIDER)) {
@@ -124,10 +122,12 @@ public class DownloadController {
     private static boolean hasFileAccess() {
         if (DownloadCollectionBridge.supportsDownloadCollection()) return true;
         Activity activity = ApplicationStatus.getLastTrackedFocusedActivity();
-        AndroidPermissionDelegate permissionDelegate = sAndroidPermissionDelegateForTesting == null
-                ? new ActivityAndroidPermissionDelegate(new WeakReference<>(activity))
-                : sAndroidPermissionDelegateForTesting;
-        return permissionDelegate.hasPermission(permission.WRITE_EXTERNAL_STORAGE);
+        if (activity instanceof ChromeActivity) {
+            return ((ChromeActivity) activity)
+                    .getWindowAndroid()
+                    .hasPermission(permission.WRITE_EXTERNAL_STORAGE);
+        }
+        return false;
     }
 
     /**
@@ -164,10 +164,21 @@ public class DownloadController {
 
     private static void requestFileAccessPermissionHelper(
             final Callback<Pair<Boolean, String>> callback) {
+        AndroidPermissionDelegate delegate = null;
         Activity activity = ApplicationStatus.getLastTrackedFocusedActivity();
-        AndroidPermissionDelegate delegate = sAndroidPermissionDelegateForTesting == null
-                ? new ActivityAndroidPermissionDelegate(new WeakReference<>(activity))
-                : sAndroidPermissionDelegateForTesting;
+        if (activity instanceof ChromeActivity) {
+            WindowAndroid windowAndroid = ((ChromeActivity) activity).getWindowAndroid();
+            if (windowAndroid != null) {
+                delegate = windowAndroid;
+            }
+        } else if (activity instanceof DownloadActivity) {
+            delegate = ((DownloadActivity) activity).getAndroidPermissionDelegate();
+        }
+
+        if (delegate == null) {
+            callback.onResult(Pair.create(false, null));
+            return;
+        }
 
         if (delegate.hasPermission(permission.WRITE_EXTERNAL_STORAGE)) {
             callback.onResult(Pair.create(true, null));
@@ -195,11 +206,6 @@ public class DownloadController {
                                 new String[] {permission.WRITE_EXTERNAL_STORAGE},
                                 permissionCallback),
                 callback.bind(Pair.create(false, null)));
-    }
-
-    /** For testing only. */
-    public static void setAndroidPermissionDelegateForTesting(AndroidPermissionDelegate delegate) {
-        sAndroidPermissionDelegateForTesting = delegate;
     }
 
     /**
