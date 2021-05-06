045c9e60cfffaf57c5c7814e2cf073687c85065c
Orin Jaworski <orinj@google.com>
2021-05-06 17:27:40 +0000

    [omnibox] Add feature and flag for OmniboxPedalsBatch2NonEnglish
    This CL adds the feature and flag to allow batch 2 Pedals in
    non-English locales. It is only applicable when the
    OmniboxPedalsBatch2 flag is enabled.
    
    Bug: 893183
    Change-Id: I737c6ceac85ee064bbf9af985dcaa67b2d2308f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2875957
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879902}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2110,6 +2110,9 @@ extern const char kOmniboxPedalSuggestionsDescription[];
 extern const char kOmniboxPedalsBatch2Name[];
 extern const char kOmniboxPedalsBatch2Description[];
 
+extern const char kOmniboxPedalsBatch2NonEnglishName[];
+extern const char kOmniboxPedalsBatch2NonEnglishDescription[];
+
 extern const char kOmniboxPedalsDefaultIconColoredName[];
 extern const char kOmniboxPedalsDefaultIconColoredDescription[];
 
