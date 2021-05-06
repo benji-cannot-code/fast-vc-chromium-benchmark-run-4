467471584620e7266e5862fe61582227c2a97ca7
Dominic Mazzoni <dmazzoni@chromium.org>
2021-05-06 21:32:27 +0000

    Add all html attributes to AssistData
    Bug: 1175288
    Change-Id: I920a920ed072c989242196ab726558962f600097
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2870490
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880078}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/TestViewStructureInterface.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/TestViewStructureInterface.java
@@ -43,4 +43,9 @@ interface TestViewStructureInterface {
      * Dump HTML tag names in toString.
      */
     public void dumpHtmlTags();
+
+    /**
+     * Get the Android View-style class name.
+     */
+    public String getClassName();
 }
