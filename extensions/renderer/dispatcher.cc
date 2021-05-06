6b42997687f50beacdc4132ce79be33fea3f7b45
David Bertoni <dbertoni@chromium.org>
2021-05-06 18:07:15 +0000

    [CodeHealth] Remove uses of ListValue::begin()/end() in /extensions/renderer.
    This CL removes calls to these deprecated member functions. It also
    converts to the newer "for" loop syntax when possible, along with
    other minor cleanup.
    
    This CL was uploaded by git cl split.
    
    R=jochen@chromium.org
    
    Bug: 1187107
    Change-Id: I36d0ca8e4fb91a53f9607de58f99e36b3900f479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2877366
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879932}

--- extensions/renderer/dispatcher.cc
+++ extensions/renderer/dispatcher.cc
@@ -152,7 +152,7 @@ void CallModuleMethod(const std::string& module_name,
       content::V8ValueConverter::Create();
 
   std::vector<v8::Local<v8::Value>> arguments;
-  for (const auto& arg : *args) {
+  for (const auto& arg : args->GetList()) {
     arguments.push_back(converter->ToV8Value(&arg, context->v8_context()));
   }
 
