f4c4027f86436626cba8df7f1d0971ad0ddcd824
David Bertoni <dbertoni@chromium.org>
2021-05-06 18:37:47 +0000

    [CodeHealth] Remove uses of ListValue::begin()/end() in /components/payments.
    This CL removes calls to these deprecated member functions. It also
    converts to the newer "for" loop syntax when possible, along with
    other minor cleanup.
    
    This CL was uploaded by git cl split.
    
    R=maxlg@chromium.org
    
    Bug: 1187107
    Change-Id: Ie0aa0d28490da1eb9daa805967c2234787e11fa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2877359
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879961}

--- components/payments/content/utility/payment_manifest_parser.cc
+++ components/payments/content/utility/payment_manifest_parser.cc
@@ -194,7 +194,7 @@ void ParseIcons(const base::DictionaryValue& dict,
     return;
   }
 
-  for (const auto& icon : *icons_list) {
+  for (const auto& icon : icons_list->GetList()) {
     if (!icon.is_dict()) {
       log.Warn(base::StringPrintf(
           "Each item in the list \"%s\" should be a dictionary.",
@@ -583,7 +583,7 @@ bool PaymentManifestParser::ParseWebAppInstallationInfoIntoStructs(
             kMaximumNumberOfSupportedDelegations));
         return false;
       }
-      for (const auto& delegation_item : *delegation_list) {
+      for (const auto& delegation_item : delegation_list->GetList()) {
         std::string delegation_name = delegation_item.GetString();
         if (delegation_name == "shippingAddress") {
           installation_info->supported_delegations.shipping_address = true;
