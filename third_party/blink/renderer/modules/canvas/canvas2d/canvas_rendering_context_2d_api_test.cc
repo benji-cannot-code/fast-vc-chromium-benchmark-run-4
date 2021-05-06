7b7cc87f5450af9986fb0adf1d838a7ecf426bb9
Christopher Cameron <ccameron@chromium.org>
2021-05-06 20:49:38 +0000

    Canvas P3 Color Space Support: Un-guard color space from feature flag
    Rename CanvasColorSpace enum to PredefinedColorSpace.
    
    Un-guard colorSpace from behind feature flag.
    
    Un-guard ImageDataSettings from behind feature flag. Use the default
    argument syntax from the spec, and remove redundant functions.
    
    Bug: 1083693
    Change-Id: I5f52e83d98ed08edb4db3c7e786a1bd8762af474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2871229
    Reviewed-by: Alex Russell <slightlyoff@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880058}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_api_test.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_api_test.cc
@@ -231,8 +231,9 @@ TEST_F(CanvasRenderingContext2DAPITest, CreateImageData) {
   NonThrowableExceptionState exception_state;
 
   // create a 100x50 imagedata and fill it with white pixels
+  ImageDataSettings* settings = ImageDataSettings::Create();
   ImageData* image_data =
-      Context2D()->createImageData(100, 50, exception_state);
+      Context2D()->createImageData(100, 50, settings, exception_state);
   EXPECT_FALSE(exception_state.HadException());
   EXPECT_EQ(100, image_data->width());
   EXPECT_EQ(50, image_data->height());
@@ -258,13 +259,17 @@ TEST_F(CanvasRenderingContext2DAPITest, CreateImageData) {
   // createImageData(width, height) takes the absolute magnitude of the size
   // arguments
 
-  ImageData* imgdata1 = Context2D()->createImageData(10, 20, exception_state);
+  ImageData* imgdata1 =
+      Context2D()->createImageData(10, 20, settings, exception_state);
   EXPECT_FALSE(exception_state.HadException());
-  ImageData* imgdata2 = Context2D()->createImageData(-10, 20, exception_state);
+  ImageData* imgdata2 =
+      Context2D()->createImageData(-10, 20, settings, exception_state);
   EXPECT_FALSE(exception_state.HadException());
-  ImageData* imgdata3 = Context2D()->createImageData(10, -20, exception_state);
+  ImageData* imgdata3 =
+      Context2D()->createImageData(10, -20, settings, exception_state);
   EXPECT_FALSE(exception_state.HadException());
-  ImageData* imgdata4 = Context2D()->createImageData(-10, -20, exception_state);
+  ImageData* imgdata4 =
+      Context2D()->createImageData(-10, -20, settings, exception_state);
   EXPECT_FALSE(exception_state.HadException());
 
   EXPECT_EQ(800u, imgdata1->data().GetAsUint8ClampedArray()->length());
@@ -276,8 +281,9 @@ TEST_F(CanvasRenderingContext2DAPITest, CreateImageData) {
 TEST_F(CanvasRenderingContext2DAPITest, CreateImageDataTooBig) {
   CreateContext(kNonOpaque);
   DummyExceptionStateForTesting exception_state;
+  ImageDataSettings* settings = ImageDataSettings::Create();
   ImageData* too_big_image_data =
-      Context2D()->createImageData(1000000, 1000000, exception_state);
+      Context2D()->createImageData(1000000, 1000000, settings, exception_state);
   EXPECT_EQ(nullptr, too_big_image_data);
   EXPECT_TRUE(exception_state.HadException());
   EXPECT_EQ(ESErrorType::kRangeError, exception_state.CodeAs<ESErrorType>());
@@ -286,8 +292,9 @@ TEST_F(CanvasRenderingContext2DAPITest, CreateImageDataTooBig) {
 TEST_F(CanvasRenderingContext2DAPITest, GetImageDataTooBig) {
   CreateContext(kNonOpaque);
   DummyExceptionStateForTesting exception_state;
-  ImageData* image_data =
-      Context2D()->getImageData(0, 0, 1000000, 1000000, exception_state);
+  ImageDataSettings* settings = ImageDataSettings::Create();
+  ImageData* image_data = Context2D()->getImageData(0, 0, 1000000, 1000000,
+                                                    settings, exception_state);
   EXPECT_EQ(nullptr, image_data);
   EXPECT_TRUE(exception_state.HadException());
   EXPECT_EQ(ESErrorType::kRangeError, exception_state.CodeAs<ESErrorType>());
@@ -297,15 +304,16 @@ TEST_F(CanvasRenderingContext2DAPITest,
        GetImageDataIntegerOverflowNegativeParams) {
   CreateContext(kNonOpaque);
   DummyExceptionStateForTesting exception_state;
+  ImageDataSettings* settings = ImageDataSettings::Create();
   ImageData* image_data = Context2D()->getImageData(
-      1, -2147483647, 1, -2147483647, exception_state);
+      1, -2147483647, 1, -2147483647, settings, exception_state);
   EXPECT_EQ(nullptr, image_data);
   EXPECT_TRUE(exception_state.HadException());
   EXPECT_EQ(ESErrorType::kRangeError, exception_state.CodeAs<ESErrorType>());
 
   exception_state.ClearException();
   image_data = Context2D()->getImageData(-2147483647, 1, -2147483647, 1,
-                                         exception_state);
+                                         settings, exception_state);
   EXPECT_EQ(nullptr, image_data);
   EXPECT_TRUE(exception_state.HadException());
   EXPECT_EQ(ESErrorType::kRangeError, exception_state.CodeAs<ESErrorType>());
