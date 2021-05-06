2d7d98181a25d4590ef18a5f424cd73c07459b1b
Stephan Hartmann <stha09@googlemail.com>
2021-05-06 16:42:56 +0000

    IWYU: add includes for std::numeric_limits and UCHAR
    Bug: None
    Change-Id: I81d2af603f362312d60ad1d150c4ccb7a2befd2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874319
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#879874}

--- base/strings/string_piece.cc
+++ base/strings/string_piece.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 
 #include <algorithm>
+#include <climits>
 #include <limits>
 #include <ostream>
 #include <string>
--- base/strings/string_piece.h
+++ base/strings/string_piece.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <iosfwd>
+#include <limits>
 #include <ostream>
 #include <string>
 #include <type_traits>
