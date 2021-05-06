6512af3f3bbe29d4285fb73de4a50e896e7ad87b
Dan Harrington <harringtond@chromium.org>
2021-05-06 16:38:51 +0000

    webfeed: Use compound drawable for unread indicator
    The primary goal of this CL is to fix the off-center active tab
    indicator provided by TabLayout. The use of ImageView affected
    TabLayout's behavior. Setting ImageView to GONE led to problems where
    the layout would not be updated after switching back to VISIBLE.
    Further, trying to work around this issue by updating the custom view
    after any changes led to more problems as setCustomView() doesn't work
    well when called multiple times.
    
    Finally, I tried a compound drawable instead of ImageView. This works
    better, as the layout is updated when the compound drawable is
    modified.
    
    Bug: b/187105312
    Change-Id: I9c8a332d8c035e724539c2f450af348101047848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874968
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879871}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
@@ -396,7 +396,7 @@ public class FeedSurfaceMediator
             mFeedMenuModel = buildMenuItems();
 
             addHeaderAndStream(
-                    getInterestFeedHeaderText(suggestionsVisible), mCoordinator.getStream());
+                    getInterestFeedHeaderText(suggestionsVisible), null, mCoordinator.getStream());
 
             mCoordinator.initializeIph();
             mSigninManager.getIdentityManager().addObserver(this);
@@ -438,15 +438,20 @@ public class FeedSurfaceMediator
         MemoryPressureListener.addCallback(mMemoryPressureCallback);
     }
 
-    private void addHeaderAndStream(String headerText, Stream stream) {
+    private void addHeaderAndStream(
+            String headerText, @Nullable String accessibilityTextUnreadContent, Stream stream) {
         PropertyModel headerModel = SectionHeaderProperties.createSectionHeader(headerText);
         mSectionHeaderModel.get(SectionHeaderListProperties.SECTION_HEADERS_KEY).add(headerModel);
         int tabId =
                 mSectionHeaderModel.get(SectionHeaderListProperties.SECTION_HEADERS_KEY).size() - 1;
 
-        // Update UNREAD_CONTENT_KEY now, and any time hasUnreadContent() changes.
-        Callback<Boolean> callback = hasUnreadContent
-                -> headerModel.set(SectionHeaderProperties.UNREAD_CONTENT_KEY, hasUnreadContent);
+        // Update UNREAD_CONTENT_KEY and HEADER_ACCESSIBILITY_TEXT_KEY now, and any time
+        // hasUnreadContent() changes.
+        Callback<Boolean> callback = hasUnreadContent -> {
+            headerModel.set(SectionHeaderProperties.UNREAD_CONTENT_KEY, hasUnreadContent);
+            headerModel.set(SectionHeaderProperties.HEADER_ACCESSIBILITY_TEXT_KEY,
+                    hasUnreadContent ? accessibilityTextUnreadContent : null);
+        };
         callback.onResult(stream.hasUnreadContent().addObserver(callback));
 
         mTabToStreamMap.put(tabId, stream);
@@ -474,6 +479,9 @@ public class FeedSurfaceMediator
         if (hasWebFeedTab == shouldHaveWebFeedTab) return;
         if (shouldHaveWebFeedTab) {
             addHeaderAndStream(mContext.getResources().getString(R.string.ntp_following),
+                    mContext.getResources().getString(
+                            R.string.accessibility_ntp_following_unread_content),
+
                     mCoordinator.createFeedStream(/* isInterestFeed = */ false));
         } else {
             if (mCurrentStream != null && mCurrentStream.getSectionType() == SectionType.WEB_FEED) {
