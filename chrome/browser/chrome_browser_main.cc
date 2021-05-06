daba437e2797eef2e735ced52678f3b732b6309f
David Bertoni <dbertoni@chromium.org>
2021-05-06 17:42:11 +0000

    [CodeHealth] Remove uses of ListValue::begin()/end() in /chrome/browser.
    This CL removes calls to these deprecated member functions. It also
    converts to the newer "for" loop syntax when possible, along with
    other minor cleanup.
    
    This CL was uploaded by git cl split.
    
    R=tedchoc@chromium.org
    
    Bug: 1187107
    Change-Id: I1e68c954bc8d51b940faca973b846495638b3143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2877199
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879915}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -605,7 +605,7 @@ void ChromeBrowserMainParts::SetupOriginTrialsCommandLine(
     if (override_disabled_feature_list) {
       std::vector<base::StringPiece> disabled_features;
       base::StringPiece disabled_feature;
-      for (const auto& item : *override_disabled_feature_list) {
+      for (const auto& item : override_disabled_feature_list->GetList()) {
         if (item.GetAsString(&disabled_feature)) {
           disabled_features.push_back(disabled_feature);
         }
@@ -625,7 +625,7 @@ void ChromeBrowserMainParts::SetupOriginTrialsCommandLine(
     if (disabled_token_list) {
       std::vector<base::StringPiece> disabled_tokens;
       base::StringPiece disabled_token;
-      for (const auto& item : *disabled_token_list) {
+      for (const auto& item : disabled_token_list->GetList()) {
         if (item.GetAsString(&disabled_token)) {
           disabled_tokens.push_back(disabled_token);
         }
