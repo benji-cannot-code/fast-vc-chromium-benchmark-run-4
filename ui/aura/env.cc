a228b2e2f06b3712c78041c0910437693f193133
Henrique Ferreiro <hferreiro@igalia.com>
2021-05-06 20:06:14 +0000

    Initialize the CursorFactory on headless builds
    This fixes a bug in electron:
    https://github.com/electron/electron/issues/28994.
    
    Change-Id: I7d8b49af41465b2a5c5df6161b5a5418ee8cd28a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2875249
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#880026}

--- ui/aura/env.cc
+++ ui/aura/env.cc
@@ -35,10 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/x/x11_cursor_factory.h"
 #endif
 
-#if defined(OS_WIN) || defined(USE_X11)
-#include "ui/gfx/switches.h"
-#endif
-
 namespace aura {
 
 namespace {
@@ -211,17 +207,13 @@ Env::Env()
     : env_controller_(std::make_unique<EnvInputStateController>(this)),
       gesture_recognizer_(std::make_unique<ui::GestureRecognizerImpl>()),
       input_state_lookup_(InputStateLookup::Create()) {
-#if defined(OS_WIN) || defined(USE_X11)
-  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(switches::kHeadless)) {
 #if defined(USE_X11)
-    // In Ozone/X11, the cursor factory is initialized by the platform
-    // initialization code.
-    if (!features::IsUsingOzonePlatform())
-      cursor_factory_ = std::make_unique<ui::X11CursorFactory>();
-#else
-    cursor_factory_ = std::make_unique<ui::WinCursorFactory>();
-#endif
-  }
+  // In Ozone/X11, the cursor factory is initialized by the platform
+  // initialization code.
+  if (!features::IsUsingOzonePlatform())
+    cursor_factory_ = std::make_unique<ui::X11CursorFactory>();
+#elif defined(OS_WIN)
+  cursor_factory_ = std::make_unique<ui::WinCursorFactory>();
 #endif
 }
 
