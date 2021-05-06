17c0de76b4668ec065bcc37172c28972e9d8fc68
Samuel Tang <samtan@microsoft.com>
2021-05-06 22:05:29 +0000

    Add the single web app permission dialog case
    Add the single web app permission dialog case for web app protocol
    handler intent picker dialog. This cl adds the startup launch logic to
    show the dialog with the right conditions.
    
    This cl does not remember the choices the user makes so they will see
    the dialog every time we launch from protocol.
    
    This cl also removes the multi app picker dialog from the code.
    The decision to do this is following our new design to support
    multi-profile protocol handling for web applications. Design doc link
    below.
    
    One-pager: https://docs.google.com/document/d/1ksfUr31h1YYNdsAswwQ5_-567RukXP_i3uQ2c4kJYYo/edit?usp=sharing&resourcekey=0-Lve5NUPuyZJ_ZPcE8g0HEw
    
    New screenshots of the dialog can be seen at this patchset:
    https://chromium-review.googlesource.com/c/chromium/src/+/2800473/20
    
    To test this change:
    - Launch the browser on Windows/Linux/Mac with
    --enable-blink-features=ParseUrlProtocolHandler
    - Install http://fabiorocha.github.io/pwa
    - From cmd run "start web+github://testvalue"
    - Observe the dialog and select "Allow".
    - Use the DevTools to verify that the PWA was launched with the fully
    translated URL: https://fabiorocha.github.io/pwa/?github=web%2Bgithub%3A%2F%2Ftestvalue%2F
    
    Bug: 1105257
    Change-Id: I2802da85e0adee1e8565e88708b54ae543f2a3e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2800473
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Samuel Tang <samtan@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#880092}

--- chrome/browser/ui/browser_dialogs.h
+++ chrome/browser/ui/browser_dialogs.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/ui/bookmarks/bookmark_editor.h"
+#include "chrome/browser/web_applications/components/web_app_id.h"
 #include "chrome/common/buildflags.h"
 #include "content/public/browser/content_browser_client.h"
 #include "extensions/buildflags/buildflags.h"
@@ -34,7 +35,6 @@ class SettingsOverriddenDialogController;
 #endif
 
 namespace base {
-class CommandLine;
 class FilePath;
 }
 
@@ -123,13 +123,19 @@ void ShowWebAppInstallDialog(content::WebContents* web_contents,
                              AppInstallationAcceptanceCallback callback);
 
 #if defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX)
+// Callback used to indicate whether a user has accepted the launch of a
+// web app. The boolean parameter is true when the user accepts the dialog.
+using WebAppProtocolHandlerAcceptanceCallback =
+    base::OnceCallback<void(bool accepted)>;
+
 // Shows the Web App Protocol Handler Intent Picker view.
-// |close_callback| may be null.
+// |profile| is kept alive throughout the processing and running of
+// |close_callback|. |close_callback| may be null.
 void ShowWebAppProtocolHandlerIntentPicker(
     const GURL& url,
     Profile* profile,
-    const base::CommandLine& command_line,
-    base::OnceCallback<void(bool accepted)> close_callback);
+    const web_app::AppId& app_id,
+    WebAppProtocolHandlerAcceptanceCallback close_callback);
 #endif
 
 // Sets whether |ShowWebAppDialog| should accept immediately without any
