db7c85a2f3f2abf632f5c0a8021f4b149391a0c6
yjliu <yjliu@chromium.org>
2021-05-06 18:47:47 +0000

    Unthrottle frame sinks that are being captured.
    Ash's frame throttling mechanism should not affect frame sinks that are
    being captured by FrameSinkVideoCapturer.
    
    Manually tested to make sure the functionalities using
    FrameSinkVideoCapturer, including ChromeOS screen recording, ChromeCast,
    meet presentation (tab and full screen share), are working as desired
    (no throttling when capture is on).
    
    Bug: 1103245
    Change-Id: I043295d831bff8aedb400a17761e4f5589d971b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2855986
    Auto-Submit: Jun Liu <yjliu@chromium.org>
    Commit-Queue: Jun Liu <yjliu@chromium.org>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#879969}

--- components/viz/service/frame_sinks/video_capture/frame_sink_video_capturer_impl.cc
+++ components/viz/service/frame_sinks/video_capture/frame_sink_video_capturer_impl.cc
@@ -248,8 +248,13 @@ void FrameSinkVideoCapturerImpl::Start(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(consumer);
 
-  Stop();
+  if (video_capture_started_)
+    Stop();
+
   video_capture_started_ = true;
+
+  if (resolved_target_)
+    resolved_target_->OnClientCaptureStarted();
 
   consumer_.Bind(std::move(consumer));
   // In the future, if the connection to the consumer is lost before a call to
@@ -260,6 +265,9 @@ void FrameSinkVideoCapturerImpl::Start(
 }
 
 void FrameSinkVideoCapturerImpl::Stop() {
+  if (!video_capture_started_)
+    return;
+
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   refresh_frame_retry_timer_->Stop();
@@ -276,6 +284,9 @@ void FrameSinkVideoCapturerImpl::Stop() {
     consumer_->OnStopped();
     consumer_.reset();
   }
+
+  if (resolved_target_)
+    resolved_target_->OnClientCaptureStopped();
 
   video_capture_started_ = false;
 }
