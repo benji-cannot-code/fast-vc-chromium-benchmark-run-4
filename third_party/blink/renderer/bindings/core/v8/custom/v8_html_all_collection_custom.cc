560112958b635a5e9c26a29364b6ff78616deb44
Canon Mukai <canonmukai@google.com>
2021-05-07 05:46:27 +0000

    Delete an unnecessary include file
    v8_binding_for_core.h is no longer used in
    v8_html_all_collection_custom.cc.
    
    Change-Id: I9b59a57905cecd01328248f8ae5765c82a794b31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879634
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Canon Mukai <canonmukai@google.com>
    Cr-Commit-Position: refs/heads/master@{#880263}

--- third_party/blink/renderer/bindings/core/v8/custom/v8_html_all_collection_custom.cc
+++ third_party/blink/renderer/bindings/core/v8/custom/v8_html_all_collection_custom.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_html_all_collection.h"
 
 #include "third_party/blink/renderer/bindings/core/v8/html_collection_or_element.h"
-#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
 #include "third_party/blink/renderer/platform/bindings/v8_set_return_value.h"
 
 namespace blink {
