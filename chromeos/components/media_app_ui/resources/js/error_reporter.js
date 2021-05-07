80696a6091f73a9766cd6d2508fcb48256d42050
Trent Apted <tapted@chromium.org>
2021-05-07 02:00:03 +0000

    Improve logging in the chrome://media-app trusted-context error reporter.
     - Ensure everything has a stack
     - Handle objects with loops
     - Emit the class name of the error, as well as the .name/.message properties
     - Append ", (from console)" if it was from console.error rather than window.onerror
    
    Bug: b/177186639
    Change-Id: I8347ddf0dedcdf1cb5ce09d079534dfc34508900
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874916
    Reviewed-by: dstockwell <dstockwell@google.com>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880198}

--- chromeos/components/media_app_ui/resources/js/error_reporter.js
+++ chromeos/components/media_app_ui/resources/js/error_reporter.js
@@ -23,8 +23,14 @@ function reportCrashError(...errors) {
     if (errorArg !== undefined) {
       if (errorArg instanceof Error) {
         message += `\n${errorArg.name}: ${errorArg.message}`;
+      } else if (typeof errorArg === 'string') {
+        message += ', ' + errorArg;
       } else {
-        message += '\n' + JSON.stringify(errorArg);
+        try {
+          message += '\n' + JSON.stringify(errorArg);
+        } catch (e) {
+          message += '<object loop?>';
+        }
       }
     }
   }
@@ -46,6 +52,10 @@ function reportCrashError(...errors) {
   if (firstError instanceof PromiseRejectionEvent) {
     prefix = 'Unhandled rejection: ';
     firstError = firstError.reason;
+  } else if (
+      typeof firstError === 'object' && firstError !== null &&
+      firstError.constructor) {
+    prefix = firstError.constructor.name + ': ';
   }
 
   if (firstError instanceof Error || firstError instanceof ErrorEvent ||
@@ -53,12 +63,20 @@ function reportCrashError(...errors) {
     // Note: `ErrorEvent` doesn't have a name field, `DOMException`s are routed
     // through 'onerror' and treated as `ErrorEvents`, we can also have
     // `DOMExceptions` inside a unhandled rejection.
-    errorMessage =
-        `Error: ${firstError.name || 'ErrorEvent'}: ${firstError.message}`;
+    errorMessage = `[${firstError.name || ''}] ${firstError.message}`;
     errorObject = firstError;
+
+    // Events and exceptions won't have stacks. Make one.
+    if (!errorObject.stack) {
+      errorObject.stack = new Error().stack;
+    }
   } else {
     // Should just be a regular object.
-    errorMessage = `Unexpected: ${JSON.stringify(firstError)}`;
+    try {
+      errorMessage = `Unexpected: ${JSON.stringify(firstError)}`;
+    } catch (e) {
+      errorMessage = `Unexpected: <object loop?>`;
+    }
   }
 
   if (!errorObject) {
@@ -96,7 +114,7 @@ function captureConsoleErrors(realConsoleError, report) {
     // Still call real console.error.
     realConsoleError(...errors);
     // Send to error reporter.
-    report(...errors);
+    report(...errors, '(from console)');
   };
 }
 
