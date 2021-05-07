591b8c87a76a19254ab995fa9e28d10c076bfe41
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-07 02:20:23 +0000

    CodeHealth: Remove use of DictionaryValue::size()
    This change removes the use of DictionaryValue::size()
    and replaces with Value::DictSize()
    
    Bug: 1187019
    Change-Id: Iee9e7f6ab9068e9087fc914396d544e01d87ded5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874763
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Fangzhen Song <songfangzhen@bytedance.com>
    Cr-Commit-Position: refs/heads/master@{#880210}

--- chrome/browser/safe_browsing/chrome_password_protection_service_browsertest.cc
+++ chrome/browser/safe_browsing/chrome_password_protection_service_browsertest.cc
@@ -512,7 +512,7 @@ IN_PROC_BROWSER_TEST_F(ChromePasswordProtectionServiceBrowserTest,
   EXPECT_EQ(1u,
             profile->GetPrefs()
                 ->GetDictionary(prefs::kSafeBrowsingUnhandledGaiaPasswordReuses)
-                ->size());
+                ->DictSize());
 
   // Opens a new browser window.
   Browser* browser2 = CreateBrowser(profile);
@@ -530,7 +530,7 @@ IN_PROC_BROWSER_TEST_F(ChromePasswordProtectionServiceBrowserTest,
   EXPECT_EQ(2u,
             profile->GetPrefs()
                 ->GetDictionary(prefs::kSafeBrowsingUnhandledGaiaPasswordReuses)
-                ->size());
+                ->DictSize());
 
   // Simulates a Gaia password change.
   SimulateGaiaPasswordChanged(service, user_manager::kStubUserEmail,
@@ -539,7 +539,7 @@ IN_PROC_BROWSER_TEST_F(ChromePasswordProtectionServiceBrowserTest,
   EXPECT_EQ(0u,
             profile->GetPrefs()
                 ->GetDictionary(prefs::kSafeBrowsingUnhandledGaiaPasswordReuses)
-                ->size());
+                ->DictSize());
   EXPECT_THAT(histograms.GetAllSamples(kGaiaPasswordChangeHistogramName),
               testing::ElementsAre(base::Bucket(2, 1)));
 }
@@ -577,7 +577,7 @@ IN_PROC_BROWSER_TEST_F(ChromePasswordProtectionServiceBrowserTest,
   EXPECT_EQ(1u,
             profile->GetPrefs()
                 ->GetDictionary(prefs::kSafeBrowsingUnhandledGaiaPasswordReuses)
-                ->size());
+                ->DictSize());
 
   // Save the same password will not trigger OnGaiaPasswordChanged(), thus no
   // change to size of unhandled_password_reuses().
@@ -586,14 +586,14 @@ IN_PROC_BROWSER_TEST_F(ChromePasswordProtectionServiceBrowserTest,
   EXPECT_EQ(1u,
             profile->GetPrefs()
                 ->GetDictionary(prefs::kSafeBrowsingUnhandledGaiaPasswordReuses)
-                ->size());
+                ->DictSize());
   // Save a different password will clear unhandled_password_reuses().
   SimulateGaiaPasswordChange("password_2");
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(0u,
             profile->GetPrefs()
                 ->GetDictionary(prefs::kSafeBrowsingUnhandledGaiaPasswordReuses)
-                ->size());
+                ->DictSize());
 }
 
 IN_PROC_BROWSER_TEST_F(ChromePasswordProtectionServiceBrowserTest,
