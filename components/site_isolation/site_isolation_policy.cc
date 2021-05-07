ef66908518d5cc326fe60640e78acd957faa7e92
David Bertoni <dbertoni@chromium.org>
2021-05-07 02:09:19 +0000

    [CodeHealth] Remove uses of ListValue::begin()/end() in /components/site_isolation.
    This CL removes calls to these deprecated member functions. It also
    converts to the newer "for" loop syntax when possible, along with
    other minor cleanup.
    
    This CL was uploaded by git cl split.
    
    R=nasko@chromium.org
    
    Bug: 1187107
    Change-Id: I5b7b1294b4262316c34f0709a6a6e79d4a08664a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2877398
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880203}

--- components/site_isolation/site_isolation_policy.cc
+++ components/site_isolation/site_isolation_policy.cc
@@ -140,9 +140,9 @@ void SiteIsolationPolicy::ApplyPersistedIsolatedOrigins(
     return;
 
   std::vector<url::Origin> origins;
-  for (const auto& value :
-       *user_prefs::UserPrefs::Get(browser_context)
-            ->GetList(prefs::kUserTriggeredIsolatedOrigins)) {
+  for (const auto& value : user_prefs::UserPrefs::Get(browser_context)
+                               ->GetList(prefs::kUserTriggeredIsolatedOrigins)
+                               ->GetList()) {
     origins.push_back(url::Origin::Create(GURL(value.GetString())));
   }
 
