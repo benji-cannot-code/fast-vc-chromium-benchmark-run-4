a488ae8abc7f63aa14654a9b3d3d91f19a7c0ffe
David Bienvenu <davidbienvenu@chromium.org>
2021-05-07 18:06:47 +0000

    tests: make ScopedFeatureEntries restore feature entries
    Adds a test for the test infra.
    
    Bug: 956719
    Change-Id: I1bd48ba03780f0ef42280a142f37d5f5e5c79894
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2846864
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880481}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -7276,6 +7276,11 @@ class FlagsStateSingleton : public flags_ui::FlagsState::Delegate {
     flags_state_ = std::make_unique<flags_ui::FlagsState>(entries, this);
   }
 
+  void RestoreDefaultState() {
+    flags_state_ =
+        std::make_unique<flags_ui::FlagsState>(kFeatureEntries, this);
+  }
+
  private:
   // flags_ui::FlagsState::Delegate:
   bool ShouldExcludeFlag(const flags_ui::FlagsStorage* storage,
@@ -7488,8 +7493,7 @@ ScopedFeatureEntries::ScopedFeatureEntries(
 ScopedFeatureEntries::~ScopedFeatureEntries() {
   GetEntriesForTesting()->clear();  // IN-TEST
   // Restore the flag state to the production flags.
-  FlagsStateSingleton::GetInstance()->RebuildState(
-      *GetEntriesForTesting());  // IN-TEST
+  FlagsStateSingleton::GetInstance()->RestoreDefaultState();
 }
 
 const FeatureEntry* GetFeatureEntries(size_t* count) {
