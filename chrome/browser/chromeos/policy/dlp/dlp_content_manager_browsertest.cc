332c4d90e488d53c029b47ad4e744ce3dfff9a2b
Aya ElAttar <ayaelattar@chromium.org>
2021-05-07 14:46:44 +0000

    DLP: Get URL patterns for destinations
    Bug: 1199790,1199798
    Change-Id: If255cf639df1cde0379d3b3d90123ac075279ba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880275
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880386}

--- chrome/browser/chromeos/policy/dlp/dlp_content_manager_browsertest.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_content_manager_browsertest.cc
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/gfx/geometry/rect.h"
 
+using testing::_;
+
 namespace policy {
 
 namespace {
@@ -381,7 +383,7 @@ IN_PROC_BROWSER_TEST_F(DlpContentManagerBrowserTest, PrintingRestricted) {
   DlpContentManager* manager = helper_.GetContentManager();
 
   SetupDlpRulesManager();
-  EXPECT_CALL(*mock_rules_manager_, GetSourceUrlPattern)
+  EXPECT_CALL(*mock_rules_manager_, GetSourceUrlPattern(_, _, _))
       .Times(2)
       .WillRepeatedly(testing::Return(src_pattern));
 
@@ -423,7 +425,7 @@ IN_PROC_BROWSER_TEST_F(DlpContentManagerBrowserTest, PrintingNotRestricted) {
       browser()->tab_strip_model()->GetActiveWebContents();
 
   SetupDlpRulesManager();
-  EXPECT_CALL(*mock_rules_manager_, GetSourceUrlPattern).Times(0);
+  EXPECT_CALL(*mock_rules_manager_, GetSourceUrlPattern(_, _, _)).Times(0);
 
   std::vector<DlpPolicyEvent> events;
   SetReportQueueForReportingManager(helper_.GetReportingManager(), events);
