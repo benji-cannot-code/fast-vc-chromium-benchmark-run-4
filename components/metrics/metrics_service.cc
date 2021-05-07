cccc6048b654f6b5237387e42b603cf9d2c6e2d3
Caitlin Fischer <caitlinfischer@google.com>
2021-05-07 17:22:37 +0000

    Stop setting kStabilityExitedCleanly to true in InitializeMetricsState.
    InitializeMetricsState() sets kStabilityExitedCleanly to true with
    WriteBeaconValue(true) if Chrome failed to exit cleanly in the previous
    session. However, setting this pref to true means that any crash between
    WriteBeaconValue(true) in InitializeMetricsState() and a later call to
    WriteBeaconValue(false) is ignored by variations-safe-mode-related code
    in the next session.
    
    AFAICT, the only consequence of this change is that we may detect more
    crashes between the status quo calls to WriteBeaconValue(true) in
    InitializeMetricsState() and WriteBeaconValue(false). The only point at
    which we GetBoolean(prefs::kStabilityExitedCleanly) is in
    CleanExitBeacon(), and we create a CleanExitBeacon before creating the
    MetricsService, i.e. before we can even call InitializeMetricsState().
    
    Bug: 1205645
    Change-Id: I0c9b94e23eeba81e91a04a4f18fb535b90321bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2875666
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/master@{#880458}

--- components/metrics/metrics_service.cc
+++ components/metrics/metrics_service.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //------------------------------------------------------------------------------
 // Description of the life cycle of a instance of MetricsService.
 //
-//  OVERVIEW
+// OVERVIEW
 //
 // A MetricsService instance is typically created at application startup.  It is
 // the central controller for the acquisition of log data, and the automatic
@@ -489,9 +489,6 @@ void MetricsService::InitializeMetricsState() {
   if (!state_manager_->clean_exit_beacon()->exited_cleanly()) {
     provider.LogCrash(
         state_manager_->clean_exit_beacon()->browser_last_live_timestamp());
-    // Reset flag, and wait until we call LogNeedForCleanShutdown() before
-    // monitoring.
-    state_manager_->clean_exit_beacon()->WriteBeaconValue(true);
   }
 
   // HasPreviousSessionData is called first to ensure it is never bypassed.
