f3449a2a40b69f51dcd4cf36d6575a5465d89974
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-07 02:23:34 +0000

    CodeHealth: Remove use of DictionaryValue::size()
    This change removes the use of DictionaryValue::size()
    and replaces with Value::DictSize()
    
    Bug: 1187019
    Change-Id: Ib38d92a527ffbf39441da31727915e8c00e8586c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2875323
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Fangzhen Song <songfangzhen@bytedance.com>
    Cr-Commit-Position: refs/heads/master@{#880215}

--- chrome/browser/chromeos/extensions/external_cache_impl_unittest.cc
+++ chrome/browser/chromeos/extensions/external_cache_impl_unittest.cc
@@ -162,7 +162,7 @@ TEST_F(ExternalCacheImplTest, Basic) {
   content::RunAllTasksUntilIdle();
 
   ASSERT_TRUE(provided_prefs());
-  EXPECT_EQ(provided_prefs()->size(), 2ul);
+  EXPECT_EQ(provided_prefs()->DictSize(), 2ul);
 
   // File in cache from Webstore.
   const base::DictionaryValue* entry1 = NULL;
@@ -202,7 +202,7 @@ TEST_F(ExternalCacheImplTest, Basic) {
       extensions::ExtensionDownloaderDelegate::InstallCallback());
 
   content::RunAllTasksUntilIdle();
-  EXPECT_EQ(provided_prefs()->size(), 3ul);
+  EXPECT_EQ(provided_prefs()->DictSize(), 3ul);
 
   const base::DictionaryValue* entry2 = NULL;
   ASSERT_TRUE(provided_prefs()->GetDictionary(kTestExtensionId2, &entry2));
@@ -233,7 +233,7 @@ TEST_F(ExternalCacheImplTest, Basic) {
   }
 
   content::RunAllTasksUntilIdle();
-  EXPECT_EQ(provided_prefs()->size(), 4ul);
+  EXPECT_EQ(provided_prefs()->DictSize(), 4ul);
 
   const base::DictionaryValue* entry4 = NULL;
   ASSERT_TRUE(provided_prefs()->GetDictionary(kTestExtensionId4, &entry4));
@@ -252,7 +252,7 @@ TEST_F(ExternalCacheImplTest, Basic) {
   external_cache.OnDamagedFileDetected(
       GetExtensionFile(cache_dir, kTestExtensionId2, "2"));
   content::RunAllTasksUntilIdle();
-  EXPECT_EQ(3ul, provided_prefs()->size());
+  EXPECT_EQ(3ul, provided_prefs()->DictSize());
   EXPECT_FALSE(
       base::PathExists(GetExtensionFile(cache_dir, kTestExtensionId2, "2")));
   EXPECT_EQ(1ul, deleted_extension_files().size());
@@ -264,7 +264,7 @@ TEST_F(ExternalCacheImplTest, Basic) {
       base::BindOnce(&ExternalCacheImplTest::OnExtensionListsUpdated,
                      base::Unretained(this), base::Unretained(empty.get())));
   content::RunAllTasksUntilIdle();
-  EXPECT_EQ(provided_prefs()->size(), 0ul);
+  EXPECT_EQ(provided_prefs()->DictSize(), 0ul);
 
   // After Shutdown directory shouldn't be touched.
   external_cache.OnDamagedFileDetected(
@@ -289,7 +289,7 @@ TEST_F(ExternalCacheImplTest, PreserveExternalCrx) {
   content::RunAllTasksUntilIdle();
 
   ASSERT_TRUE(provided_prefs());
-  EXPECT_EQ(provided_prefs()->size(), 1ul);
+  EXPECT_EQ(provided_prefs()->DictSize(), 1ul);
 
   // Extensions downloaded from update url will only be visible in the provided
   // prefs once the download of the .crx has finished. Extensions that are
