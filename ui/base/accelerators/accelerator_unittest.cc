cd17060fff5149a281c02e5820ed07d3bef78fbf
Zentaro Kavanagh <zentaro@chromium.org>
2021-05-07 23:38:09 +0000

    Relax constraint on DomCode in accelerator_map
    - Previously the map DCHECK'd when registering an accelerator
      with a DomCode
    - Accelerators created from KeyEvents include the DomCode and that
      is required during the lookup path
    - Change the map to set the DomCode to NONE when storing
    
    Bug: 1174326
    Test: ash_unittests --gtest_filter="Accelerator*"
    Change-Id: I9a0b4dea57fa16b138c888cbaf6aa4c52c2e278a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880646
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880626}

--- ui/base/accelerators/accelerator_unittest.cc
+++ ui/base/accelerators/accelerator_unittest.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/events/event.h"
 
 namespace ui {
@@ -74,5 +76,36 @@ TEST(AcceleratorTest, ShortcutTextForUnknownKey) {
   const Accelerator accelerator(VKEY_UNKNOWN, EF_NONE);
   EXPECT_EQ(std::u16string(), accelerator.GetShortcutText());
 }
+
+TEST(AcceleratorTest, ConversionFromKeyEvent) {
+  ui::KeyEvent key_event(ui::ET_KEY_PRESSED, ui::VKEY_F,
+                         ui::EF_ALT_DOWN | ui::EF_CONTROL_DOWN);
+  Accelerator accelerator(key_event);
+
+  EXPECT_EQ(accelerator.key_code(), ui::VKEY_F);
+  EXPECT_EQ(accelerator.modifiers(), ui::EF_ALT_DOWN | ui::EF_CONTROL_DOWN);
+}
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+TEST(AcceleratorTest, ConversionFromKeyEvent_Ash) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndEnableFeature(
+      ::features::kImprovedKeyboardShortcuts);
+
+  ui::KeyEvent key_event(ui::ET_KEY_PRESSED, ui::VKEY_F,
+                         ui::EF_ALT_DOWN | ui::EF_CONTROL_DOWN);
+  Accelerator accelerator(key_event);
+
+  EXPECT_EQ(accelerator.key_code(), ui::VKEY_F);
+  EXPECT_EQ(accelerator.modifiers(), ui::EF_ALT_DOWN | ui::EF_CONTROL_DOWN);
+
+  // Code is set when converting from a KeyEvent.
+  EXPECT_EQ(accelerator.code(), DomCode::US_F);
+
+  // Test resetting code.
+  accelerator.reset_code();
+  EXPECT_EQ(accelerator.code(), DomCode::NONE);
+}
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 }  // namespace ui
