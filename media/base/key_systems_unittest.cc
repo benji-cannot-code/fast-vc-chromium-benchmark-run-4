16b2d0ea2fd473805dafa95d2a2b49ab08634cca
Jeffrey Kardatzke <jkardatzke@google.com>
2021-05-07 00:01:04 +0000

    media: Handle disjoint HW secure and identifier case
    This fixes a problem where if you have 2 key systems registered, and
    one of them satisfies the HW secure codec requirement but the other one
    can satisfy a non-codec requirement like no distinctive identifier; that
    we will incorrectly report there is a supported key system. A proper fix
    will be done with a larger refactor. For now, we are just going to pass
    in a tri-state value to GetRobustnessConfigRule so the specific key
    system can see if there are HW secure requirements already when
    indicating the rule for the key system in general.
    
    BUG=1204284,b:185926023
    TEST=Netflix falls back to SWDRM w/ DI blocked on a HWDRM system,
         unit tests pass
    
    Change-Id: Ife48bbb921ba0559d4e15bf98e22dd9da30254d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2870185
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/master@{#880152}

--- media/base/key_systems_unittest.cc
+++ media/base/key_systems_unittest.cc
@@ -77,7 +77,8 @@ class TestKeySystemPropertiesBase : public KeySystemProperties {
 
   EmeConfigRule GetRobustnessConfigRule(
       EmeMediaType media_type,
-      const std::string& requested_robustness) const override {
+      const std::string& requested_robustness,
+      const bool* /*hw_secure_requirement*/) const override {
     return requested_robustness.empty() ? EmeConfigRule::SUPPORTED
                                         : EmeConfigRule::NOT_SUPPORTED;
   }
@@ -141,7 +142,8 @@ class ExternalKeySystemProperties : public TestKeySystemPropertiesBase {
 
   EmeConfigRule GetRobustnessConfigRule(
       EmeMediaType media_type,
-      const std::string& requested_robustness) const override {
+      const std::string& requested_robustness,
+      const bool* /*hw_secure_requirement*/) const override {
     if (requested_robustness == kRobustnessSupported)
       return EmeConfigRule::SUPPORTED;
     else if (requested_robustness == kRobustnessSecureCodecsRequired)
@@ -207,7 +209,7 @@ bool IsSupportedKeySystem(const std::string& key_system) {
 
 EmeConfigRule GetRobustnessConfigRule(const std::string& requested_robustness) {
   return KeySystems::GetInstance()->GetRobustnessConfigRule(
-      kExternal, EmeMediaType::VIDEO, requested_robustness);
+      kExternal, EmeMediaType::VIDEO, requested_robustness, nullptr);
 }
 
 // Adds test container and codec masks.
