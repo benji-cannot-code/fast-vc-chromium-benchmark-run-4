1192fcbe5bf442e1027554840222526f35675fc9
Yoshifumi Inoue <yosin@chromium.org>
2021-05-07 09:43:52 +0000

    Add missing explicit keyword to NG{Block,Column,Fieldset}LayoutAlgorithm constructors
    This patch adds |explicit| missing |explicit| keywords to
      * NGBlockLayoutAlgorithm
      * NGColumnLayoutAlgorithm
      * NGFieldsetLayoutAlgorithm
    for improving code health.
    
    Change-Id: I4f961cab31b550b36df6db6ba4e11cf9fece376e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879642
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880311}

--- third_party/blink/renderer/core/layout/ng/ng_fieldset_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/ng_fieldset_layout_algorithm.h
@@ -22,7 +22,7 @@ class CORE_EXPORT NGFieldsetLayoutAlgorithm
                                NGBoxFragmentBuilder,
                                NGBlockBreakToken> {
  public:
-  NGFieldsetLayoutAlgorithm(const NGLayoutAlgorithmParams& params);
+  explicit NGFieldsetLayoutAlgorithm(const NGLayoutAlgorithmParams& params);
 
   const NGLayoutResult* Layout() override;
 
