7b1be92f7423d5554d64319328499203c9726615
Tom <tluk@chromium.org>
2021-05-07 15:50:34 +0000

    Update side panel contents when the target extension host has loaded
    This CL updates the ExtensionSidePanelController to reload the
    contents hosted in the left aligned side panel when the extension has
    been loaded.
    
    This allows the extension to work as expected when it is first
    installed or reloaded without having to restart the browser or open a
    new window as is necessary in the current state.
    
    Bug: 1197555
    Change-Id: I82f8e1c433d8ce0f76f0a1357314d8c7ce03feb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2873802
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880414}

--- chrome/browser/ui/views/extensions/extensions_side_panel_controller.h
+++ chrome/browser/ui/views/extensions/extensions_side_panel_controller.h
@@ -8,8 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/scoped_observation.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "extensions/browser/extension_registry.h"
+#include "extensions/browser/extension_registry_observer.h"
 #include "extensions/common/extension_id.h"
 
 namespace extensions {
@@ -27,8 +30,10 @@ class ToolbarButton;
 // A class that manages hosting the extension WebContents in the left aligned
 // side panel of the browser window.
 // TODO(crbug.com/1197555): Remove this once the experiment has concluded.
-class ExtensionsSidePanelController : public content::WebContentsObserver,
-                                      public content::WebContentsDelegate {
+class ExtensionsSidePanelController
+    : public content::WebContentsObserver,
+      public content::WebContentsDelegate,
+      public extensions::ExtensionRegistryObserver {
  public:
   ExtensionsSidePanelController(SidePanel* side_panel,
                                 BrowserView* browser_view);
@@ -38,11 +43,22 @@ class ExtensionsSidePanelController : public content::WebContentsObserver,
   ~ExtensionsSidePanelController() override;
 
   std::unique_ptr<ToolbarButton> CreateToolbarButton();
+  void ResetWebContents();
+  void SetNewWebContents();
 
   // content::WebContentsDelegate:
   content::WebContents* OpenURLFromTab(
       content::WebContents* source,
       const content::OpenURLParams& params) override;
+
+  // extensions::ExtensionRegistryObserver:
+  void OnExtensionLoaded(content::BrowserContext* browser_context,
+                         const extensions::Extension* extension) override;
+  void OnExtensionUnloaded(content::BrowserContext* browser_context,
+                           const extensions::Extension* extension,
+                           extensions::UnloadedExtensionReason reason) override;
+
+  views::WebView* get_web_view_for_testing() { return web_view_; }
 
  private:
   // content::WebContentsObserver:
@@ -57,6 +73,11 @@ class ExtensionsSidePanelController : public content::WebContentsObserver,
   SidePanel* side_panel_;
   BrowserView* browser_view_;
   views::WebView* web_view_;
+  std::unique_ptr<content::WebContents> web_contents_;
+
+  base::ScopedObservation<extensions::ExtensionRegistry,
+                          extensions::ExtensionRegistryObserver>
+      registry_observation_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_SIDE_PANEL_CONTROLLER_H_
