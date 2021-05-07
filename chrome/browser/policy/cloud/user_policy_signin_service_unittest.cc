53bdc946c0464b070af3dd42b98ba3e6c0e174db
Igor <igorcov@chromium.org>
2021-05-07 07:52:55 +0000

    Remove unused check for lacros_chrome_service
    BUG=1114069
    TEST=unit tests
    
    Change-Id: I4f5e56338efc271a0ecf2fde205ad77ed7b5f534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2871475
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Commit-Queue: Igor <igorcov@chromium.org>
    Auto-Submit: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880290}

--- chrome/browser/policy/cloud/user_policy_signin_service_unittest.cc
+++ chrome/browser/policy/cloud/user_policy_signin_service_unittest.cc
@@ -52,6 +52,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chromeos/lacros/lacros_test_helper.h"
+#endif
+
 #if defined(OS_ANDROID)
 #include "chrome/browser/policy/cloud/user_policy_signin_service_mobile.h"
 #else
@@ -298,6 +302,9 @@ class UserPolicySigninServiceTest : public testing::Test {
 
   std::unique_ptr<TestingPrefServiceSimple> local_state_;
   network::TestURLLoaderFactory test_url_loader_factory_;
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  chromeos::ScopedLacrosServiceTestHelper test_helper;
+#endif
 };
 
 class UserPolicySigninServiceSignedInTest : public UserPolicySigninServiceTest {
