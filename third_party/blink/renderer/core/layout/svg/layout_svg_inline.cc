3e2c981f76bdd025e83ebc0e351b21aa7bcadd40
Kent Tamura <tkent@chromium.org>
2021-05-07 15:02:10 +0000

    SVG Text NG: Fix ObjectBoundingBox() for rotated characters
    Before this CL, an AffineTransform for scaled geometry was applied to
    unscaled rectangle, and there were cases where characters were clipped
    by a wrong visual rect.
    
    This CL adds a method to NGFragmentItem, and it returns a rectangle
    which is transformed and unscaled correctly.
    
    Bug: 1179585
    Change-Id: I1a7cebd71aaebd75f056736d27a03e8e1f224584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879823
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880393}

--- third_party/blink/renderer/core/layout/svg/layout_svg_inline.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_inline.cc
@@ -83,12 +83,8 @@ FloatRect LayoutSVGInline::ObjectBoundingBox() const {
     for (cursor.MoveToIncludingCulledInline(*this); cursor;
          cursor.MoveToNextForSameLayoutObject()) {
       const NGFragmentItem& item = *cursor.CurrentItem();
-      if (item.Type() != NGFragmentItem::kSVGText)
-        continue;
-      FloatRect item_rect = item.FloatRectInContainerFragment();
-      if (item.HasSVGTransformForBoundingBox())
-        item_rect = item.BuildSVGTransformForBoundingBox().MapRect(item_rect);
-      bounds.Unite(item_rect);
+      if (item.Type() == NGFragmentItem::kSVGText)
+        bounds.Unite(item.ObjectBoundingBox());
     }
     return bounds;
   }
@@ -135,7 +131,7 @@ void LayoutSVGInline::AbsoluteQuads(Vector<FloatQuad>& quads,
       if (item.Type() == NGFragmentItem::kSVGText) {
         quads.push_back(
             LocalToAbsoluteQuad(SVGLayoutSupport::ExtendTextBBoxWithStroke(
-                                    *this, item.FloatRectInContainerFragment()),
+                                    *this, item.ObjectBoundingBox()),
                                 mode));
       }
     }
