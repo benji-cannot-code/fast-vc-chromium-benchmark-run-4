3595c1c61bcc5aac2be76660127eaf1130836a72
Collin Baker <collinbaker@chromium.org>
2021-05-07 23:44:32 +0000

    Show and start tutorials from chrome://internals/user-education/
    Wires up the WebUI page to tutorials infrastructure. Currently
    starting a tutorial will do nothing since FeatureTutorialService is a
    stub.
    
    Bug: 1194751
    Change-Id: I32b9df5028195cbe692ae94a1475bcf6cd75da22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2875644
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880629}

--- chrome/browser/ui/webui/internals/user_education/user_education_internals_page_handler_impl.h
+++ chrome/browser/ui/webui/internals/user_education/user_education_internals_page_handler_impl.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/internals/user_education/user_education_internals.mojom.h"
 #include "content/public/browser/web_ui_data_source.h"
 
+class FeatureTutorialService;
+
 class UserEducationInternalsPageHandlerImpl
     : public mojom::user_education_internals::
           UserEducationInternalsPageHandler {
@@ -22,8 +24,12 @@ class UserEducationInternalsPageHandlerImpl
   UserEducationInternalsPageHandlerImpl& operator=(
       const UserEducationInternalsPageHandlerImpl&) = delete;
 
+  // mojom::user_education_internals::UserEducationInternalsPageHandler:
+  void GetTutorials(GetTutorialsCallback callback) override;
+  void StartTutorial(const std::string& tutorial_id) override;
+
  private:
-  // Profile* profile_ = nullptr;
+  FeatureTutorialService* const tutorial_service_;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_INTERNALS_USER_EDUCATION_USER_EDUCATION_INTERNALS_PAGE_HANDLER_IMPL_H_
