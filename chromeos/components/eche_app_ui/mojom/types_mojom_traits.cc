879c6101967e5e0479728c7a055bc514ec800121
Guanru Lee <guanrulee@chromium.org>
2021-05-07 12:58:52 +0000

    Adds mojo interface to detect screen backlight state on EcheApp
    Detects the screen is off through ScreenBacklightObserver and update
    to Javascript side of EcheApp for closing app stream proactively.
    
    Bug: b/186204165
    Change-Id: If6c421a9428da6d5f48bd4f03a6678edf075ea53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853363
    Commit-Queue: Guanru Lee <guanrulee@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880361}

---
+++ chromeos/components/eche_app_ui/mojom/types_mojom_traits.cc
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/eche_app_ui/mojom/types_mojom_traits.h"
+
+namespace mojo {
+
+// static
+chromeos::eche_app::mojom::ScreenBacklightState
+EnumTraits<chromeos::eche_app::mojom::ScreenBacklightState,
+           ash::ScreenState>::ToMojom(ash::ScreenState input) {
+  switch (input) {
+    case ash::ScreenState::ON:
+      return chromeos::eche_app::mojom::ScreenBacklightState::ON;
+    case ash::ScreenState::OFF:
+      return chromeos::eche_app::mojom::ScreenBacklightState::OFF;
+    case ash::ScreenState::OFF_AUTO:
+      return chromeos::eche_app::mojom::ScreenBacklightState::OFF_AUTO;
+  }
+
+  NOTREACHED();
+}
+
+// static
+bool EnumTraits<chromeos::eche_app::mojom::ScreenBacklightState,
+                ash::ScreenState>::
+    FromMojom(chromeos::eche_app::mojom::ScreenBacklightState input,
+              ash::ScreenState* output) {
+  switch (input) {
+    case chromeos::eche_app::mojom::ScreenBacklightState::ON:
+      *output = ash::ScreenState::ON;
+      return true;
+    case chromeos::eche_app::mojom::ScreenBacklightState::OFF:
+      *output = ash::ScreenState::OFF;
+      return true;
+    case chromeos::eche_app::mojom::ScreenBacklightState::OFF_AUTO:
+      *output = ash::ScreenState::OFF_AUTO;
+      return true;
+  }
+  NOTREACHED();
+  return false;
+}
+
+}  // namespace mojo
---
+++ chromeos/components/eche_app_ui/mojom/types_mojom_traits.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_COMPONENTS_ECHE_APP_UI_MOJOM_TYPES_MOJOM_TRAITS_H_
+#define CHROMEOS_COMPONENTS_ECHE_APP_UI_MOJOM_TYPES_MOJOM_TRAITS_H_
+
+#include "ash/public/cpp/screen_backlight.h"
+#include "chromeos/components/eche_app_ui/mojom/eche_app.mojom-shared.h"
+#include "mojo/public/cpp/bindings/struct_traits.h"
+
+namespace mojo {
+
+template <>
+struct EnumTraits<chromeos::eche_app::mojom::ScreenBacklightState,
+                  ash::ScreenState> {
+  static chromeos::eche_app::mojom::ScreenBacklightState ToMojom(
+      ash::ScreenState input);
+  static bool FromMojom(chromeos::eche_app::mojom::ScreenBacklightState input,
+                        ash::ScreenState* output);
+};
+
+}  // namespace mojo
+
+#endif  // CHROMEOS_COMPONENTS_ECHE_APP_UI_MOJOM_TYPES_MOJOM_TRAITS_H_
