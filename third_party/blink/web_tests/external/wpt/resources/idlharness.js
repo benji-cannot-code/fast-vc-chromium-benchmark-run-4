54472e9f93ea2ae1ecb5de1c5350fcb2462d9d80
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-05-08 14:25:47 +0000

    Import wpt@c435fae819e593723c4db853c76019e8122f2183
    Using wpt-import in Chromium 01b29b81568a63826bcea1e3ca8c01435137bf10.
    With Chromium commits locally applied on WPT:
    df3cf2af98 "Implement <popup> focus, autofocus, delegatesfocus"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/resources
    smcgruer@chromium.org:
      external/wpt/css/css-position
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Id85c5506575a60680449893855365025dd8add64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880916
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Commit-Queue: Jason Chase <chasej@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880716}

--- third_party/blink/web_tests/external/wpt/resources/idlharness.js
+++ third_party/blink/web_tests/external/wpt/resources/idlharness.js
@@ -2627,9 +2627,6 @@ IdlInterface.prototype.test_object = function(desc)
     {
         // Result of [[IsHTMLDDA]] slot
         expected_typeof = "undefined";
-    } else if (this.members.some(function(member) { return member.legacycaller; }))
-    {
-        expected_typeof = "function";
     }
     else
     {
