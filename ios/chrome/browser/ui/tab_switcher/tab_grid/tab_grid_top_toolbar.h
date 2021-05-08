406d2860cf50523cfefcda1b9316c5e866c67f1a
Mohammad Refaat <mrefaat@google.com>
2021-05-08 00:28:01 +0000

    Add select button to the tab grid
    Add select button resources and add it to the tab grid top tool bar.
    
    ipad screenshot: https://screenshot.googleplex.com/3k9aXWHW2G9VCZL
    iphone screenshot: https://screenshot.googleplex.com/BsHqcASTqP6NsaY
    
    Bug: 1196931
    Change-Id: Id554158fb9ed95f33f091ada13dfa2f5b73263a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2877859
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880637}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_top_toolbar.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_top_toolbar.h
@@ -26,7 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setNewTabButtonTarget:(id)target action:(SEL)action;
 // Set |enabled| on the new tab button.
 - (void)setNewTabButtonEnabled:(BOOL)enabled;
-
+// Sets target/action for tapping event on select tabs button.
+- (void)setSelectTabButtonTarget:(id)target action:(SEL)action;
+// Set |enabled| on the select tabs button.
+- (void)setSelectTabsButtonEnabled:(BOOL)enabled;
 // Hides components and uses a black background color for tab grid transition
 // animation.
 - (void)hide;
--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_top_toolbar.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_top_toolbar.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_top_toolbar.h"
 
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/features.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_constants.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_page_control.h"
 #import "ios/chrome/browser/ui/thumb_strip/thumb_strip_feature.h"
@@ -27,7 +28,8 @@ const int kNewTabButtonTrailingSpace = 20;
   UIBarButtonItem* _centralItem;
   UIBarButtonItem* _spaceItem;
   UIBarButtonItem* _newTabButton;
-  UIBarButtonItem* _newTabButtonTrailingSpaceItem;
+  UIBarButtonItem* _fixedTrailingSpaceItem;
+  UIBarButtonItem* _selectTabsButton;
 }
 
 - (void)hide {
@@ -47,6 +49,15 @@ const int kNewTabButtonTrailingSpace = 20;
 
 - (void)setNewTabButtonEnabled:(BOOL)enabled {
   _newTabButton.enabled = enabled;
+}
+
+- (void)setSelectTabButtonTarget:(id)target action:(SEL)action {
+  _selectTabsButton.target = target;
+  _selectTabsButton.action = action;
+}
+
+- (void)setSelectTabsButtonEnabled:(BOOL)enabled {
+  _selectTabsButton.enabled = enabled;
 }
 
 #pragma mark - UIView
@@ -80,21 +91,35 @@ const int kNewTabButtonTrailingSpace = 20;
 - (void)setItemsForTraitCollection:(UITraitCollection*)traitCollection {
   if (traitCollection.verticalSizeClass == UIUserInterfaceSizeClassRegular &&
       traitCollection.horizontalSizeClass == UIUserInterfaceSizeClassCompact) {
-    [self setItems:@[ _spaceItem, _centralItem, _spaceItem ]];
-  } else {
-    // The new tab button is only used if the thumb strip is enabled. In other
-    // cases, there is a floating new tab button on the bottom.
-    if (ShowThumbStripInTraitCollection(traitCollection)) {
+    if (IsTabsBulkActionsEnabled()) {
       [self setItems:@[
-        _leadingButton, _spaceItem, _centralItem, _spaceItem, _newTabButton,
-        _newTabButtonTrailingSpaceItem, _trailingButton
+        _spaceItem, _spaceItem, _centralItem, _spaceItem, _selectTabsButton
       ]];
     } else {
-      [self setItems:@[
-        _leadingButton, _spaceItem, _centralItem, _spaceItem, _trailingButton
-      ]];
+      [self setItems:@[ _spaceItem, _centralItem, _spaceItem ]];
     }
+    return;
   }
+
+  // The new tab button is only used if the thumb strip is enabled. In other
+  // cases, there is a floating new tab button on the bottom.
+  if (ShowThumbStripInTraitCollection(traitCollection)) {
+    [self setItems:@[
+      _leadingButton, _spaceItem, _centralItem, _spaceItem, _newTabButton,
+      _fixedTrailingSpaceItem, _trailingButton
+    ]];
+    return;
+  }
+  if (IsTabsBulkActionsEnabled()) {
+    [self setItems:@[
+      _leadingButton, _spaceItem, _centralItem, _spaceItem, _selectTabsButton,
+      _fixedTrailingSpaceItem, _trailingButton
+    ]];
+    return;
+  }
+  [self setItems:@[
+    _leadingButton, _spaceItem, _centralItem, _spaceItem, _trailingButton
+  ]];
 }
 
 - (void)setupViews {
@@ -107,6 +132,10 @@ const int kNewTabButtonTrailingSpace = 20;
 
   _leadingButton = [[UIBarButtonItem alloc] init];
   _leadingButton.tintColor = UIColorFromRGB(kTabGridToolbarTextButtonColor);
+
+  _selectTabsButton = [[UIBarButtonItem alloc] init];
+  _selectTabsButton.image = [UIImage imageNamed:@"select_tabs_toolbar_button"];
+  _selectTabsButton.tintColor = UIColorFromRGB(kTabGridToolbarTextButtonColor);
 
   // The segmented control has an intrinsic size.
   _pageControl = [[TabGridPageControl alloc] init];
@@ -123,11 +152,11 @@ const int kNewTabButtonTrailingSpace = 20;
                            action:nil];
   _newTabButton.tintColor = UIColorFromRGB(kTabGridToolbarTextButtonColor);
 
-  _newTabButtonTrailingSpaceItem = [[UIBarButtonItem alloc]
+  _fixedTrailingSpaceItem = [[UIBarButtonItem alloc]
       initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace
                            target:nil
                            action:nil];
-  _newTabButtonTrailingSpaceItem.width = kNewTabButtonTrailingSpace;
+  _fixedTrailingSpaceItem.width = kNewTabButtonTrailingSpace;
 
   _spaceItem = [[UIBarButtonItem alloc]
       initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace
