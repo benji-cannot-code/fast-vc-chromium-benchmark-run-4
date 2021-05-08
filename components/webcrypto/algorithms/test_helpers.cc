7bd68cfa798b3127a3cc898cc3c133ecdaeca688
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-08 23:47:12 +0000

    CodeHealth: Remove use of DictionaryValue::empty in /components/webcrypto
    This change removes the use of DictionaryValue::empty()
    and replaces with Value::DictEmpty()
    
    This CL was uploaded by git cl split.
    
    R=rsleevi@chromium.org
    
    Bug: 1187020
    Change-Id: I501a3aa7a7732a8dd5d2d1ce236769ef5fbbce2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2881601
    Commit-Queue: Ryan Sleevi <rsleevi@chromium.org>
    Auto-Submit: Fangzhen Song <songfangzhen@bytedance.com>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880734}

--- components/webcrypto/algorithms/test_helpers.cc
+++ components/webcrypto/algorithms/test_helpers.cc
@@ -453,7 +453,7 @@ base::Optional<base::DictionaryValue> GetJwkDictionary(
     const std::string& k_expected_hex,
     blink::WebCryptoKeyUsageMask use_mask_expected) {
   base::Optional<base::DictionaryValue> dict = GetJwkDictionary(json);
-  if (!dict.has_value() || dict.value().empty())
+  if (!dict.has_value() || dict.value().DictEmpty())
     return ::testing::AssertionFailure() << "JSON parsing failed";
 
   // ---- k
@@ -480,7 +480,7 @@ base::Optional<base::DictionaryValue> GetJwkDictionary(
     const std::string& e_expected_hex,
     blink::WebCryptoKeyUsageMask use_mask_expected) {
   base::Optional<base::DictionaryValue> dict = GetJwkDictionary(json);
-  if (!dict.has_value() || dict.value().empty())
+  if (!dict.has_value() || dict.value().DictEmpty())
     return ::testing::AssertionFailure() << "JSON parsing failed";
 
   // ---- n
