3beb18cbd2b82abae0b4aeb69585604b7f3d5cee
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-08 03:38:51 +0000

    CodeHealth: Remove use of DictionaryValue::size()
    This change removes the use of DictionaryValue::size()
    and replaces with Value::DictSize()
    
    Bug: 1187019
    Change-Id: I6efc494d8044b42aec55bf6a10f8dc97585faaba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2878266
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Fangzhen Song <songfangzhen@bytedance.com>
    Cr-Commit-Position: refs/heads/master@{#880671}

--- net/reporting/reporting_delivery_agent_unittest.cc
+++ net/reporting/reporting_delivery_agent_unittest.cc
@@ -118,7 +118,7 @@ TEST_F(ReportingDeliveryAgentTest, SuccessfulImmediateUpload) {
 
     base::DictionaryValue* report;
     ASSERT_TRUE(list->GetDictionary(0, &report));
-    EXPECT_EQ(5u, report->size());
+    EXPECT_EQ(5u, report->DictSize());
 
     ExpectDictIntegerValue(0, *report, "age");
     ExpectDictStringValue(kType_, *report, "type");
@@ -164,7 +164,7 @@ TEST_F(ReportingDeliveryAgentTest, SuccessfulImmediateSubdomainUpload) {
 
     base::DictionaryValue* report;
     ASSERT_TRUE(list->GetDictionary(0, &report));
-    EXPECT_EQ(5u, report->size());
+    EXPECT_EQ(5u, report->DictSize());
 
     ExpectDictIntegerValue(0, *report, "age");
     ExpectDictStringValue(kType_, *report, "type");
@@ -244,7 +244,7 @@ TEST_F(ReportingDeliveryAgentTest, SuccessfulDelayedUpload) {
 
     base::DictionaryValue* report;
     ASSERT_TRUE(list->GetDictionary(0, &report));
-    EXPECT_EQ(5u, report->size());
+    EXPECT_EQ(5u, report->DictSize());
 
     ExpectDictIntegerValue(0, *report, "age");
     ExpectDictStringValue(kType_, *report, "type");
