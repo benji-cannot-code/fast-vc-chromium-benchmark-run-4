4a372ad31129955014c522f67223f40074fd2cd8
Hui Yingst <nigi@chromium.org>
2021-05-08 03:55:32 +0000

    [unseasoned-pdf] Implement PdfViewWebPlugin::CanEditText().
    Implement PdfViewWebPlugin::CanEditText(). This requires adding a const
    getter PdfViewPluginBase::engine().
    
    This CL also marks PDFEngine::CanEditText() const to match the functions
    with the same name better.
    
    Bug: 1204729
    Change-Id: Ie648b86de33dc1816e4c2c2b9bbe24dde3898d2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874975
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880677}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -2211,7 +2211,7 @@ std::string PDFiumEngine::GetSelectedText() {
   return base::UTF16ToUTF8(result);
 }
 
-bool PDFiumEngine::CanEditText() {
+bool PDFiumEngine::CanEditText() const {
   return editable_form_text_area_;
 }
 
