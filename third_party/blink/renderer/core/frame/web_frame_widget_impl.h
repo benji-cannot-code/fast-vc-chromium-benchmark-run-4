b179cdc288f060a3fb6a769c5d1df5ee306d9c84
Hongbo Song <hbsong@google.com>
2021-05-10 22:57:20 +0000

    Fix performance.eventCounts() for touchstart, touchend and pointerevent for EventTiming..
    Currently performance.eventCounts() is broken especially for pointerdown, touchstart.
    There are two main reasons:
    1. A touchstart can be dropped because of HitTest() for touch event. In this case, following pointerdown will not be created. On the
    other hand, in touch event model only touch starts can set the target and after that the touch event always goes to that
    target. So when touchstart is dropped， the following touchend will not be dispatched.
    
    2. In pointer_event_manager, when there's no pointerevent listener, a pointerevent will not be dispatched.
    
    This CL is trying to count all pointerevent and touchevents for EventTiming invoked by user interactions.
    
    Bug: 1123967
    Change-Id: I415dca8cebfc0b8d50b80010eb6673a14ec6f534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2758031
    Commit-Queue: Hongbo Song <hbsong@google.com>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881289}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.h
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.h
@@ -665,7 +665,7 @@ class CORE_EXPORT WebFrameWidgetImpl
                          cc::PaintBenchmarkResult& result) override;
   KURL GetURLForDebugTrace() override;
   float GetTestingDeviceScaleFactorOverride() override;
-
+  void CountDroppedPointerDownForEventTiming(unsigned count) override;
   // mojom::blink::FrameWidget overrides.
   void DragTargetDragEnter(const WebDragData&,
                            const gfx::PointF& point_in_viewport,
