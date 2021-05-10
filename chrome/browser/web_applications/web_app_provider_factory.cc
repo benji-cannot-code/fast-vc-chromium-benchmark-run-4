f13b063ee54fc9fde4be41a8911b4d346b94ea24
Phillis Tang <phillis@chromium.org>
2021-05-10 23:05:44 +0000

    reland "DPWA: update file handlers on permission change"
    This is a reland with a fix to declare the WebAppProviderFactory
    to depend on HostContentSettingsMap so that the WebAppProvider system
    is shutdown before HostContentSettingsMap.
    Original CL description:
    > Unregister file handlers when file handling permission is changed to BLOCK
    > by the user, and re-register file handlers when the permission settings is
    > changed to ASK/ALLOW. Make WebAppInstallFinalizer observe file handling
    > permission changes and update app db state as well as OS state. Make
    > both install and update task checks file handling permission during
    > |WebAppInstallFinalizer::SetWebAppManifestFieldsAndWriteData|.
    >
    > Bug: 1194163
    > Change-Id: Ia24030018243571b6ed020de205cf346742c0169
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2868925
    > Commit-Queue: Phillis Tang <phillis@chromium.org>
    > Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#880576}
    
    Change-Id: I30cdc19e6d3327004e45df3b9372bc2708829e08
    Bug: 1194163
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2885422
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881294}

--- chrome/browser/web_applications/web_app_provider_factory.cc
+++ chrome/browser/web_applications/web_app_provider_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/web_applications/web_app_provider_factory.h"
 
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/metrics/ukm_background_recorder_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/model_type_store_service_factory.h"
@@ -34,6 +35,9 @@ WebAppProviderFactory::WebAppProviderFactory()
   DependsOnExtensionsSystem();
   DependsOn(ModelTypeStoreServiceFactory::GetInstance());
   DependsOn(ukm::UkmBackgroundRecorderFactory::GetInstance());
+  // Required to listen to file handling settings change in
+  // `WebAppInstallFinalizer::OnContentSettingChanged()`
+  DependsOn(HostContentSettingsMapFactory::GetInstance());
 }
 
 WebAppProviderFactory::~WebAppProviderFactory() {
