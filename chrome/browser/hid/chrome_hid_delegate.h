4b2e5361703d9f01056a7fabbbf828222bf62968
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-10 19:18:57 +0000

    Retire ScopedObserver in /chrome/browser/hid.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=reillyg@chromium.org
    
    Bug: 1145565
    Change-Id: Ie2b55b41cf47c1719d827031f0fa8fbbb87bf94a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883699
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881152}

--- chrome/browser/hid/chrome_hid_delegate.h
+++ chrome/browser/hid/chrome_hid_delegate.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/observer_list.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/hid/hid_chooser_context.h"
 #include "components/permissions/object_permission_context_base.h"
 #include "content/public/browser/hid_delegate.h"
@@ -53,14 +53,15 @@ class ChromeHidDelegate
   void OnHidChooserContextShutdown() override;
 
  private:
-  ScopedObserver<HidChooserContext,
-                 HidChooserContext::DeviceObserver,
-                 &HidChooserContext::AddDeviceObserver,
-                 &HidChooserContext::RemoveDeviceObserver>
-      device_observer_{this};
-  ScopedObserver<permissions::ObjectPermissionContextBase,
-                 permissions::ObjectPermissionContextBase::PermissionObserver>
-      permission_observer_{this};
+  base::ScopedObservation<HidChooserContext,
+                          HidChooserContext::DeviceObserver,
+                          &HidChooserContext::AddDeviceObserver,
+                          &HidChooserContext::RemoveDeviceObserver>
+      device_observation_{this};
+  base::ScopedObservation<
+      permissions::ObjectPermissionContextBase,
+      permissions::ObjectPermissionContextBase::PermissionObserver>
+      permission_observation_{this};
   base::ObserverList<content::HidDelegate::Observer> observer_list_;
 };
 
