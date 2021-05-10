ebffb8671c0c97914735b1889ea5e0cd6c43d813
Eric Willigers <ericwilligers@chromium.org>
2021-05-10 04:56:20 +0000

    [Lacros] WebAppsPublisherHost tracks web app modifications
    When a web app manifest is updated, WebAppsPublisherHost forwards
    the updates to the Ash browser.
    
    Similar updates are sent when a web app is launched, or when the
    'locally installed' status for a web app changes.
    
    Bug: 1194709
    Change-Id: I027aafdb12da43c734d88706ae41a17812a74f05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879828
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880847}

--- chrome/browser/apps/app_service/web_apps_publisher_host.h
+++ chrome/browser/apps/app_service/web_apps_publisher_host.h
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
+#include "base/strings/string_piece.h"
+#include "base/time/time.h"
+#include "chrome/browser/apps/app_service/icon_key_util.h"
 #include "chrome/browser/web_applications/components/app_registrar.h"
 #include "chrome/browser/web_applications/components/app_registrar_observer.h"
 #include "chrome/browser/web_applications/components/web_app_id.h"
@@ -47,13 +50,15 @@ class WebAppsPublisherHost : public web_app::AppRegistrarObserver {
 
   // web_app::AppRegistrarObserver:
   void OnWebAppInstalled(const web_app::AppId& app_id) override;
+  void OnWebAppManifestUpdated(const web_app::AppId& app_id,
+                               base::StringPiece old_name) override;
   void OnWebAppWillBeUninstalled(const web_app::AppId& app_id) override;
   void OnAppRegistrarDestroyed() override;
-
-  // TODO(crbug.com/1194709): override
-  // - OnWebAppLastLaunchTimeChanged
-  // - OnWebAppLocallyInstalledStateChanged
-  // - OnWebAppManifestUpdated
+  void OnWebAppLocallyInstalledStateChanged(const web_app::AppId& app_id,
+                                            bool is_locally_installed) override;
+  void OnWebAppLastLaunchTimeChanged(
+      const std::string& app_id,
+      const base::Time& last_launch_time) override;
 
   // TODO(crbug.com/1194709): inherit from content_settings::Observer and
   // override:
@@ -63,13 +68,15 @@ class WebAppsPublisherHost : public web_app::AppRegistrarObserver {
 
   const web_app::WebApp* GetWebApp(const web_app::AppId& app_id) const;
   apps::mojom::AppPtr Convert(const web_app::WebApp* web_app,
-                              apps::mojom::Readiness readiness) const;
+                              apps::mojom::Readiness readiness);
   void Publish(apps::mojom::AppPtr app);
 
   static crosapi::mojom::AppPublisher* publisher_for_testing_;
 
   Profile* const profile_;
   web_app::WebAppProvider* const provider_;
+
+  apps_util::IncrementingIconKeyFactory icon_key_factory_;
 
   base::ScopedObservation<web_app::AppRegistrar, web_app::AppRegistrarObserver>
       registrar_observation_{this};
