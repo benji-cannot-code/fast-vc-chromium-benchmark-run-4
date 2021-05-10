04fdbdd10f81e79513ed612c8bd8e906762d6ada
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-10 17:09:34 +0000

    Retire ScopedObserver in /chrome/browser/subresource_filter.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=alexmt@chromium.org
    
    Bug: 1145565
    Change-Id: I6e26c67b323b672fbe628caeed3f1fec4d861983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883941
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881058}

--- chrome/browser/subresource_filter/subresource_filter_history_observer.h
+++ chrome/browser/subresource_filter/subresource_filter_history_observer.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SUBRESOURCE_FILTER_SUBRESOURCE_FILTER_HISTORY_OBSERVER_H_
 #define CHROME_BROWSER_SUBRESOURCE_FILTER_SUBRESOURCE_FILTER_HISTORY_OBSERVER_H_
 
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/history_service_observer.h"
 #include "components/subresource_filter/content/browser/subresource_filter_profile_context.h"
@@ -41,8 +41,9 @@ class SubresourceFilterHistoryObserver
   void OnURLsDeleted(history::HistoryService* history_service,
                      const history::DeletionInfo& deletion_info) override;
 
-  ScopedObserver<history::HistoryService, history::HistoryServiceObserver>
-      history_observer_{this};
+  base::ScopedObservation<history::HistoryService,
+                          history::HistoryServiceObserver>
+      history_observation_{this};
 
   // Outlives this object.
   subresource_filter::SubresourceFilterContentSettingsManager*
