f6496531b4b07e882ce556846bb9f60868751c3a
Mei Liang <meiliang@chromium.org>
2021-05-10 22:25:45 +0000

    [RBD] Add a worker for fetching discounts
    This CL adds the worker for fetching discounts in the background. This
    worker is designed to do background work after calling
    FetchDiscountWorker#Start and continue until Chrome is finished. It will
    post a delayed task to perform the background work after it finished a
    task.
    
    Change-Id: If06dec617d6b51d0d4d009c2f3ccc018f8530b7d
    Bug: 1196024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827399
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Yusuf Ozuysal
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881271}

---
+++ chrome/browser/cart/cart_discount_fetcher.h
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef CHROME_BROWSER_CART_CART_DISCOUNT_FETCHER_H_
+#define CHROME_BROWSER_CART_CART_DISCOUNT_FETCHER_H_
+
+#include <memory>
+
+#include "base/memory/scoped_refptr.h"
+#include "chrome/browser/endpoint_fetcher/endpoint_fetcher.h"
+
+namespace network {
+class PendingSharedURLLoaderFactory;
+}  // namespace network
+
+class CartDiscountFetcher {
+ public:
+  using CartDiscountFetcherCallback = base::OnceCallback<void(void)>;
+
+  virtual ~CartDiscountFetcher();
+
+  virtual void Fetch(
+      std::unique_ptr<network::PendingSharedURLLoaderFactory> pending_factory,
+      CartDiscountFetcherCallback callback);
+
+ private:
+  // TODO(meiliang): Add param a list of carts to fetch.
+  static void fetchForDiscounts(
+      std::unique_ptr<network::PendingSharedURLLoaderFactory> pending_factory,
+      CartDiscountFetcherCallback callback);
+  static void OnDiscountsAvailable(
+      std::unique_ptr<EndpointFetcher> endpoint_fetcher,
+      CartDiscountFetcherCallback callback,
+      std::unique_ptr<EndpointResponse> responses);
+  static std::unique_ptr<EndpointFetcher> CreateEndpointFetcher(
+      std::unique_ptr<network::PendingSharedURLLoaderFactory> pending_factory);
+};
+
+class CartDiscountFetcherFactory {
+ public:
+  virtual ~CartDiscountFetcherFactory();
+  virtual std::unique_ptr<CartDiscountFetcher> createFetcher();
+};
+
+#endif  // CHROME_BROWSER_CART_CART_DISCOUNT_FETCHER_H_
