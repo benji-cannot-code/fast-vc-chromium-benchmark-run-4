a57f0f9c4a58f4b4fa2a225bbf18a85407488ec7
Sebastien <seblalancette@chromium.org>
2021-05-10 19:28:00 +0000

    [Stylize] Create NoteCreationMediator
    This mediator will be used by the NoteCreationCoordinator to kick-off
    the retrieval of note templates, along with the resolution of resources
    (e.g. fonts).
    
    Bug: 1194168
    Change-Id: Id3db64742465b9aafdd119e871957a8fe7ce941a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2860859
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881159}

---
+++ chrome/browser/content_creation/notes/internal/android/java/src/org/chromium/chrome/browser/content_creation/notes/NoteProperties.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.content_creation.notes;
+
+import org.chromium.components.content_creation.notes.models.NoteTemplate;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
+
+class NoteProperties {
+    /** The view type used by the recycler view to show the notes. */
+    public static final int NOTE_VIEW_TYPE = 1;
+
+    /** The template definition.*/
+    static final WritableObjectPropertyKey<NoteTemplate> TEMPLATE =
+            new WritableObjectPropertyKey<>();
+
+    static final PropertyKey[] ALL_KEYS = new PropertyKey[] {TEMPLATE};
+}
