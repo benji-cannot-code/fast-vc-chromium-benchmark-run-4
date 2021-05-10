50e78a06b78031b77496aca8852e570ea898c777
Sky Malice <skym@chromium.org>
2021-05-10 19:34:18 +0000

    Fix NPE in FRE policy load code.
    OneshotSupplierImpl posts its notifications to an Android Handler, to
    avoid reentrancy issues. However this allows events to be interleaved
    and notifications to be called after tear down. This problem can be
    avoided by wrapping subscriptions to OneshotSupplierImpl with a
    CallbackControler.
    
    It is possible, depending on how Android handles fragment/activity
    lifecycle, that this change is not sufficient and we'll need to follow
    up with an explicit guard that getActivity()/getPageDelegate() is still
    valid on any incoming signal.
    
    Bug: 1201400
    Change-Id: I74b2bd8c87b04d6196e7b8130560bdf107021c40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883318
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881165}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/PolicyLoadListener.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/PolicyLoadListener.java
@@ -72,7 +72,7 @@ public class PolicyLoadListener implements OneshotSupplier<Boolean> {
 
     @Override
     public Boolean onAvailable(Callback<Boolean> callback) {
-        return mMightHavePoliciesSupplier.onAvailable(callback);
+        return mMightHavePoliciesSupplier.onAvailable(mCallbackController.makeCancelable(callback));
     }
 
     @Override
