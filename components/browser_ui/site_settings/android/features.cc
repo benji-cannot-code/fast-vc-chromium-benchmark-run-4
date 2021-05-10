7059297d7f43a32821b1976797a91e75aa74f90a
Christian Dullweber <dullweber@chromium.org>
2021-05-10 12:27:53 +0000

    PageInfo: Enable ActionableContentSettings by default
    Enable feature by default.
    
    Bug: 1157989
    Change-Id: I806b142a97c11bd8e6a4c8dd5bada8093118b164
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2882767
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880932}

--- components/browser_ui/site_settings/android/features.cc
+++ components/browser_ui/site_settings/android/features.cc
@@ -11,6 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace browser_ui {
 
 const base::Feature kActionableContentSettings{
-    "ActionableContentSettings", base::FEATURE_DISABLED_BY_DEFAULT};
+    "ActionableContentSettings", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace browser_ui
