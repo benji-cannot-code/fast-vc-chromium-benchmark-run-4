7b7f514f798faca4ee0198b2effda64c37eb000b
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-10 19:13:02 +0000

    Retire ScopedObserver in /chrome/browser/background.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=rdevlin.cronin@chromium.org
    
    Bug: 1145565
    Change-Id: I4a962097c4711960521c4562f5a5517a774ad730
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883691
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881147}

--- chrome/browser/background/background_contents_service.h
+++ chrome/browser/background/background_contents_service.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/background/background_contents.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/notification_observer.h"
@@ -263,9 +263,9 @@ class BackgroundContentsService : private content::NotificationObserver,
       ComponentExtensionBackoffEntryMap;
   ComponentExtensionBackoffEntryMap component_backoff_map_;
 
-  ScopedObserver<extensions::ExtensionRegistry,
-                 extensions::ExtensionRegistryObserver>
-      extension_registry_observer_{this};
+  base::ScopedObservation<extensions::ExtensionRegistry,
+                          extensions::ExtensionRegistryObserver>
+      extension_registry_observation_{this};
 
   base::WeakPtrFactory<BackgroundContentsService> weak_ptr_factory_{this};
 
