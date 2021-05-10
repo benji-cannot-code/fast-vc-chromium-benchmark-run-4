e9673ffa4b3f788e3c620209ab59b8e1cbdb0b55
John Williams <jrw@google.com>
2021-05-10 18:22:56 +0000

    Completed functionality of Cast feedback dialog.
    - Adds a default-disabled feature kCastFeedbackDialog to show the Cast feedback WebU
    - Launches new WebUI from Cast toolbar icon menu based on feature
    - Hooks up WebUI to chrome.feedbackPrivate to actually submit feedback
    
    Bug: 1173633
    Change-Id: I74bb9606800e13eb42f46e1f807bc07c50a4b840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2856709
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: John Williams <jrw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881106}

--- chrome/browser/ui/webui/media_router/cast_feedback_ui.h
+++ chrome/browser/ui/webui/media_router/cast_feedback_ui.h
@@ -7,25 +7,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_MEDIA_ROUTER_CAST_FEEDBACK_UI_H_
 
 #include "base/macros.h"
-#include "ui/webui/mojo_web_ui_controller.h"
+#include "content/public/browser/web_ui_controller.h"
+
+class Profile;
+
+namespace base {
+class ListValue;
+}
 
 namespace content {
 class WebContents;
 class WebUI;
 }  // namespace content
 
-class Profile;
-
 namespace media_router {
 
 // The main object controlling the Cast feedback
 // (chrome://cast-feedback) page.
-class CastFeedbackUI : public ui::MojoWebUIController {
+class CastFeedbackUI : public content::WebUIController {
  public:
   explicit CastFeedbackUI(content::WebUI* web_ui);
   ~CastFeedbackUI() override;
 
  private:
+  void OnCloseMessage(const base::ListValue*);
+
   Profile* const profile_;
   content::WebContents* const web_contents_;
 
