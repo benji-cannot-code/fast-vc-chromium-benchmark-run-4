743115e00bab672e1fc8696df8860e8fa3dc6c35
Yuki Shiino <yukishiino@chromium.org>
2021-05-10 12:16:44 +0000

    bind-gen: Remove unnecessary header inclusion (IWYU)
    Bug: 839389
    Change-Id: Iccdee93ec6c022ac605b3dab2a1e5980e773b975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883336
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880930}

--- third_party/blink/renderer/platform/bindings/union_base.h
+++ third_party/blink/renderer/platform/bindings/union_base.h
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_UNION_BASE_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_UNION_BASE_H_
 
-#include "base/containers/span.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
-#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "v8/include/v8.h"
 
 namespace blink {
