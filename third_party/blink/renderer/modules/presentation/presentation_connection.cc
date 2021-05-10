8532af07171e18b45c29083c37915e60d5c7b925
Fredrik Söderqvist <fs@opera.com>
2021-05-10 18:33:49 +0000

    Use EventTarget::EnqueueEvent in PresentationConnection
    Rather than reimplementing pretty much the same functionality.
    
    Change-Id: Iceba36eb479bfd2d777806666340848d61fc5c42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2878971
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#881113}

--- third_party/blink/renderer/modules/presentation/presentation_connection.cc
+++ third_party/blink/renderer/modules/presentation/presentation_connection.cc
@@ -199,12 +199,14 @@ void PresentationConnection::DidChangeState(
     case mojom::blink::PresentationConnectionState::CONNECTING:
       return;
     case mojom::blink::PresentationConnectionState::CONNECTED:
-      DispatchStateChangeEvent(Event::Create(event_type_names::kConnect));
+      EnqueueEvent(*Event::Create(event_type_names::kConnect),
+                   TaskType::kPresentation);
       return;
     case mojom::blink::PresentationConnectionState::CLOSED:
       return;
     case mojom::blink::PresentationConnectionState::TERMINATED:
-      DispatchStateChangeEvent(Event::Create(event_type_names::kTerminate));
+      EnqueueEvent(*Event::Create(event_type_names::kTerminate),
+                   TaskType::kPresentation);
       return;
   }
   NOTREACHED();
@@ -245,14 +247,9 @@ ControllerPresentationConnection* ControllerPresentationConnection::Take(
   controller->RegisterConnection(connection);
 
   // Fire onconnectionavailable event asynchronously.
-  auto* event = PresentationConnectionAvailableEvent::Create(
-      event_type_names::kConnectionavailable, connection);
-  request->GetExecutionContext()
-      ->GetTaskRunner(TaskType::kPresentation)
-      ->PostTask(FROM_HERE,
-                 WTF::Bind(&PresentationConnection::DispatchEventAsync,
-                           WrapPersistent(request), WrapPersistent(event)));
-
+  request->EnqueueEvent(*PresentationConnectionAvailableEvent::Create(
+                            event_type_names::kConnectionavailable, connection),
+                        TaskType::kPresentation);
   return connection;
 }
 
@@ -637,9 +634,10 @@ void PresentationConnection::DidClose(
   }
 
   state_ = mojom::blink::PresentationConnectionState::CLOSED;
-  DispatchStateChangeEvent(PresentationConnectionCloseEvent::Create(
-      event_type_names::kClose, ConnectionCloseReasonToString(reason),
-      message));
+  EnqueueEvent(*PresentationConnectionCloseEvent::Create(
+                   event_type_names::kClose,
+                   ConnectionCloseReasonToString(reason), message),
+               TaskType::kPresentation);
 }
 
 void PresentationConnection::DidFinishLoadingBlob(DOMArrayBuffer* buffer) {
@@ -671,22 +669,6 @@ void PresentationConnection::DidFailLoadingBlob(FileErrorCode error_code) {
   messages_.pop_front();
   blob_loader_.Clear();
   HandleMessageQueue();
-}
-
-void PresentationConnection::DispatchStateChangeEvent(Event* event) {
-  GetExecutionContext()
-      ->GetTaskRunner(TaskType::kPresentation)
-      ->PostTask(FROM_HERE,
-                 WTF::Bind(&PresentationConnection::DispatchEventAsync,
-                           WrapPersistent(this), WrapPersistent(event)));
-}
-
-// static
-void PresentationConnection::DispatchEventAsync(EventTarget* target,
-                                                Event* event) {
-  DCHECK(target);
-  DCHECK(event);
-  target->DispatchEvent(*event);
 }
 
 void PresentationConnection::TearDown() {
