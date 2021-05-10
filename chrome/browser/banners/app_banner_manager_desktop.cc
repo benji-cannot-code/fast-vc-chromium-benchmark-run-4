aee9d02aa33ae3704ab17187ef4ac12f22ace141
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-10 19:08:01 +0000

    Retire ScopedObserver in /chrome/browser/banners.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=msw@chromium.org
    
    Bug: 1145565
    Change-Id: I6677e1a3d4a5735cac75368ca6ff81a5078a71e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883692
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881143}

--- chrome/browser/banners/app_banner_manager_desktop.cc
+++ chrome/browser/banners/app_banner_manager_desktop.cc
@@ -87,7 +87,7 @@ AppBannerManagerDesktop::AppBannerManagerDesktop(
   auto* provider = web_app::WebAppProviderBase::GetProviderBase(profile);
   // May be null in unit tests e.g. TabDesktopMediaListTest.*.
   if (provider)
-    registrar_observer_.Add(&provider->registrar());
+    registrar_observation_.Observe(&provider->registrar());
 }
 
 AppBannerManagerDesktop::~AppBannerManagerDesktop() { }
@@ -206,7 +206,7 @@ void AppBannerManagerDesktop::OnWebAppInstalled(
 }
 
 void AppBannerManagerDesktop::OnAppRegistrarDestroyed() {
-  registrar_observer_.RemoveAll();
+  registrar_observation_.Reset();
 }
 
 void AppBannerManagerDesktop::CreateWebApp(WebappInstallSource install_source) {
--- chrome/browser/banners/app_banner_manager_desktop.h
+++ chrome/browser/banners/app_banner_manager_desktop.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/web_applications/components/app_registrar.h"
 #include "chrome/browser/web_applications/components/app_registrar_observer.h"
 #include "chrome/browser/web_applications/components/web_app_id.h"
@@ -94,8 +94,8 @@ class AppBannerManagerDesktop
 
   extensions::ExtensionRegistry* extension_registry_;
 
-  ScopedObserver<web_app::AppRegistrar, web_app::AppRegistrarObserver>
-      registrar_observer_{this};
+  base::ScopedObservation<web_app::AppRegistrar, web_app::AppRegistrarObserver>
+      registrar_observation_{this};
 
   base::WeakPtrFactory<AppBannerManagerDesktop> weak_factory_{this};
 
