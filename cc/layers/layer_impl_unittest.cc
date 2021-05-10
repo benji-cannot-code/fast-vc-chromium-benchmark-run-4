7e7162bead1901936b93f7a99c3e06c90df77a97
Oriol Brufau <obrufau@igalia.com>
2021-05-10 21:23:09 +0000

    [cc] Remove uses of GetIdealContentsScaleKey() in tests
    r872117 made GetIdealContentsScale() 2D, but as a fallback for code that
    doesn't use 2D scales yet, it added GetIdealContentsScaleKey(), which
    just returns the maximum component of the 2D scale.
    
    Unit tests were just changed to use the 1D method, but checking the full
    2D scale is quite straightforward, so this patch just does that.
    
    Bug: 1196414
    Change-Id: I663cd8e4d65978fa4d90b0382f1b0ff3fddcd832
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2885039
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#881236}

--- cc/layers/layer_impl_unittest.cc
+++ cc/layers/layer_impl_unittest.cc
@@ -266,7 +266,7 @@ TEST_F(LayerImplTest, PerspectiveTransformHasReasonableScale) {
     layer->draw_properties().screen_space_transform = transform;
 
     ASSERT_TRUE(layer->ScreenSpaceTransform().HasPerspective());
-    EXPECT_FLOAT_EQ(15.f, layer->GetIdealContentsScaleKey());
+    EXPECT_EQ(gfx::Vector2dF(15.f, 15.f), layer->GetIdealContentsScale());
   }
   // Ensure that we don't fall below the device scale factor.
   {
@@ -276,7 +276,7 @@ TEST_F(LayerImplTest, PerspectiveTransformHasReasonableScale) {
     layer->draw_properties().screen_space_transform = transform;
 
     ASSERT_TRUE(layer->ScreenSpaceTransform().HasPerspective());
-    EXPECT_FLOAT_EQ(1.f, layer->GetIdealContentsScaleKey());
+    EXPECT_EQ(gfx::Vector2dF(1.f, 1.f), layer->GetIdealContentsScale());
   }
   // Ensure that large scales don't end up extremely large.
   {
@@ -286,7 +286,7 @@ TEST_F(LayerImplTest, PerspectiveTransformHasReasonableScale) {
     layer->draw_properties().screen_space_transform = transform;
 
     ASSERT_TRUE(layer->ScreenSpaceTransform().HasPerspective());
-    EXPECT_FLOAT_EQ(127.f, layer->GetIdealContentsScaleKey());
+    EXPECT_EQ(gfx::Vector2dF(127.f, 127.f), layer->GetIdealContentsScale());
   }
   // Test case from crbug.com/766021.
   {
@@ -297,7 +297,7 @@ TEST_F(LayerImplTest, PerspectiveTransformHasReasonableScale) {
     layer->draw_properties().screen_space_transform = transform;
 
     ASSERT_TRUE(layer->ScreenSpaceTransform().HasPerspective());
-    EXPECT_FLOAT_EQ(1.f, layer->GetIdealContentsScaleKey());
+    EXPECT_EQ(gfx::Vector2dF(1.f, 1.f), layer->GetIdealContentsScale());
   }
 }
 
