2e58ec27be043550176b22bfe80d9ee515e8606e
Viktor Semeniuk <vsemeniuk@google.com>
2021-05-10 06:42:20 +0000

    Not including muted credentials to the count of remaining issues
    This changes doesn't include muted credentials to list of remaining
    issues on PostSaveCompromisedBubble.
    
    Bug: 1198911
    Change-Id: Ibcddc0c5a337a3584995aebaf812fa5c92fe117c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879024
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#880862}

--- components/password_manager/core/browser/ui/post_save_compromised_helper.cc
+++ components/password_manager/core/browser/ui/post_save_compromised_helper.cc
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/ui/post_save_compromised_helper.h"
 
 #include "base/containers/contains.h"
+#include "base/feature_list.h"
 #include "components/password_manager/core/browser/password_store.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_service.h"
 
@@ -62,6 +64,13 @@ void PostSaveCompromisedHelper::OnGetAllInsecureCredentials(
     std::vector<InsecureCredential> insecure_credentials) {
   const bool compromised_password_changed =
       current_leak_ && !base::Contains(insecure_credentials, *current_leak_);
+  if (base::FeatureList::IsEnabled(features::kMutingCompromisedCredentials)) {
+    // We want to show bubble even if updated insecure credentials was muted,
+    // this is why muted credentials are erased after computing
+    // 'compromised_password_changed';
+    base::EraseIf(insecure_credentials,
+                  [](const auto& credential) { return credential.is_muted; });
+  }
   if (compromised_password_changed) {
     bubble_type_ = insecure_credentials.empty()
                        ? BubbleType::kPasswordUpdatedSafeState
