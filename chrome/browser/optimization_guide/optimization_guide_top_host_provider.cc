a736fe8f7f35f259d2e16fa42d73bbf7a8bd53de
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-10 17:07:24 +0000

    CodeHealth: Remove use of DictionaryValue::empty in /chrome/browser/optimization_guide
    This change removes the use of DictionaryValue::empty()
    and replaces with Value::DictEmpty()
    
    This CL was uploaded by git cl split.
    
    R=robertogden@chromium.org
    
    Bug: 1187020
    Change-Id: If1a6da661f3f4806f3363087b500c8e02160d065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2881596
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Auto-Submit: Fangzhen Song <songfangzhen@bytedance.com>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881055}

--- chrome/browser/optimization_guide/optimization_guide_top_host_provider.cc
+++ chrome/browser/optimization_guide/optimization_guide_top_host_provider.cc
@@ -131,7 +131,7 @@ void OptimizationGuideTopHostProvider::
   DCHECK(pref_service_
              ->GetDictionary(
                  optimization_guide::prefs::kHintsFetcherTopHostBlocklist)
-             ->empty());
+             ->DictEmpty());
 
   Profile* profile = Profile::FromBrowserContext(browser_context_);
   auto* engagement_service =
@@ -238,7 +238,7 @@ void OptimizationGuideTopHostProvider::MaybeUpdateTopHostBlocklist(
   }
   blocklist_pref->RemovePath(optimization_guide::HashHostForDictionary(
       navigation_handle->GetURL().host()));
-  if (blocklist_pref->empty()) {
+  if (blocklist_pref->DictEmpty()) {
     blocklist_pref->Clear();
     pref_service->SetInteger(
         optimization_guide::prefs::kHintsFetcherTopHostBlocklistState,
