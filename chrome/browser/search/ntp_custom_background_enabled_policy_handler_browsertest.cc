128cbe98d302101d12382e6e48779909907428fe
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-10 17:07:59 +0000

    CodeHealth: Remove use of DictionaryValue::empty in /chrome/browser/search
    This change removes the use of DictionaryValue::empty()
    and replaces with Value::DictEmpty()
    
    This CL was uploaded by git cl split.
    
    R=gayane@chromium.org
    
    Bug: 1187020
    Change-Id: Ia77e4d180e4ddeec6206e264df4a5e6db1c4805e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2881498
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Auto-Submit: Fangzhen Song <songfangzhen@bytedance.com>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881056}

--- chrome/browser/search/ntp_custom_background_enabled_policy_handler_browsertest.cc
+++ chrome/browser/search/ntp_custom_background_enabled_policy_handler_browsertest.cc
@@ -44,7 +44,7 @@ IN_PROC_BROWSER_TEST_F(NtpCustomBackgroundEnabledPolicyHandlerTest, Override) {
   PrefService* prefs = browser()->profile()->GetPrefs();
 
   // Check initial states.
-  EXPECT_FALSE(prefs->GetDictionary(prefs::kNtpCustomBackgroundDict)->empty());
+  EXPECT_FALSE(prefs->GetDictionary(prefs::kNtpCustomBackgroundDict)->DictEmpty());
   EXPECT_FALSE(prefs->IsManagedPreference(prefs::kNtpCustomBackgroundDict));
 
   // Check if updated policy is reflected.
@@ -54,7 +54,7 @@ IN_PROC_BROWSER_TEST_F(NtpCustomBackgroundEnabledPolicyHandlerTest, Override) {
                policy::POLICY_SOURCE_CLOUD, base::Value(false), nullptr);
   policy_provider_.UpdateChromePolicy(policies);
 
-  EXPECT_TRUE(prefs->GetDictionary(prefs::kNtpCustomBackgroundDict)->empty());
+  EXPECT_TRUE(prefs->GetDictionary(prefs::kNtpCustomBackgroundDict)->DictEmpty());
   EXPECT_TRUE(prefs->IsManagedPreference(prefs::kNtpCustomBackgroundDict));
 
   // Flip the value, and check again.
@@ -63,11 +63,11 @@ IN_PROC_BROWSER_TEST_F(NtpCustomBackgroundEnabledPolicyHandlerTest, Override) {
                policy::POLICY_SOURCE_CLOUD, base::Value(true), nullptr);
   policy_provider_.UpdateChromePolicy(policies);
 
-  EXPECT_FALSE(prefs->GetDictionary(prefs::kNtpCustomBackgroundDict)->empty());
+  EXPECT_FALSE(prefs->GetDictionary(prefs::kNtpCustomBackgroundDict)->DictEmpty());
   EXPECT_FALSE(prefs->IsManagedPreference(prefs::kNtpCustomBackgroundDict));
 
   policy_provider_.UpdateChromePolicy(policy::PolicyMap());
 
-  EXPECT_FALSE(prefs->GetDictionary(prefs::kNtpCustomBackgroundDict)->empty());
+  EXPECT_FALSE(prefs->GetDictionary(prefs::kNtpCustomBackgroundDict)->DictEmpty());
   EXPECT_FALSE(prefs->IsManagedPreference(prefs::kNtpCustomBackgroundDict));
 }
