c0e646c459908fb7f24b560a00f57b2e031f4a1a
Alan Cutter <alancutter@chromium.org>
2021-05-10 09:23:12 +0000

    Add chrome://flags/#allow-default-web-app-migration-for-chrome-os-managed-users
    This CL adds the existing AllowDefaultWebAppMigrationForChromeOsManagedUsers
    feature flag to chrome://flags.
    
    Bug: 1207237
    Change-Id: I4de51f2de39d7c48536afae0ca1bc6e3708eeb92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2881512
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#880889}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2873,6 +2873,10 @@ extern const char kWifiSyncAndroidDescription[];
 #endif  // #if BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+extern const char kAllowDefaultWebAppMigrationForChromeOsManagedUsersName[];
+extern const char
+    kAllowDefaultWebAppMigrationForChromeOsManagedUsersDescription[];
+
 extern const char kDefaultChatWebAppName[];
 extern const char kDefaultChatWebAppDescription[];
 
