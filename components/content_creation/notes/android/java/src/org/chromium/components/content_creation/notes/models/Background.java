01c0e53b6895993fefef998d21acc64ad2dc374c
Sebastien <seblalancette@chromium.org>
2021-05-10 23:34:03 +0000

    [Stylize] Extend NoteTemplate JNI Conversion Logic
    Adding further conversion logic to handle the properties that were
    added to the C++ NoteTemplate model class.
    
    Bug: 1194168
    Change-Id: I3bacaa3e6cc12f14a60c9055f64505ef608b6d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2877697
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881307}

---
+++ components/content_creation/notes/android/java/src/org/chromium/components/content_creation/notes/models/Background.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.content_creation.notes.models;
+
+import androidx.annotation.ColorInt;
+
+/**
+ * Model class for a template's background.
+ */
+public class Background {
+    public final @ColorInt int color;
+
+    /** Constructor. */
+    public Background(@ColorInt int color) {
+        this.color = color;
+    }
+}
---
+++ components/content_creation/notes/android/java/src/org/chromium/components/content_creation/notes/models/TextStyle.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.content_creation.notes.models;
+
+import androidx.annotation.ColorInt;
+
+/**
+ * Model class for a template's text style.
+ */
+public class TextStyle {
+    public final String fontName;
+    public final @ColorInt int fontColor;
+    public final boolean allCaps;
+
+    /** Constructor. */
+    public TextStyle(String fontName, @ColorInt int fontColor, boolean allCaps) {
+        this.fontName = fontName;
+        this.fontColor = fontColor;
+        this.allCaps = allCaps;
+    }
+}
