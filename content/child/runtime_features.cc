97064d0668c860a2dbf49ff5bbb927b5c524f6be
Ian Vollick <vollick@chromium.org>
2021-05-10 16:46:01 +0000

    Feature Control for Document Transitions
    With this CL, we decouple blink/viz control over shared/document
    element transitions in preparation for origin trial. Specifically,
    the feature becomes controlled by blink and, from viz's point of
    view, the feature is always enabled.
    
    Based heavily on jbroman@'s crrev.com/c/2773722
    
    Bug: 1204520
    Change-Id: Icfd05969686b129fbc807837e50aeb26dab02dde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880567
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881040}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -242,7 +242,6 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
     {wf::EnableCooperativeScheduling, features::kCooperativeScheduling},
     {wf::EnableDocumentPolicy, features::kDocumentPolicy},
     {wf::EnableDocumentPolicyNegotiation, features::kDocumentPolicyNegotiation},
-    {wf::EnableDocumentTransition, features::kDocumentTransition},
     {wf::EnableExpensiveBackgroundTimerThrottling,
      features::kExpensiveBackgroundTimerThrottling},
     {wf::EnableFencedFrames, blink::features::kFencedFrames,
