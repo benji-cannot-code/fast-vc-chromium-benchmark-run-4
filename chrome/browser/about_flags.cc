5faec63f31041425024f77e94a8f5ea4b03f0fab
Tommy Li <tommycli@chromium.org>
2021-05-10 21:48:14 +0000

    [memories] Add Context Signal Persistence to chrome://flags
    This adds an option under the main Memories option:
    chrome://flags/#memories  - "Enabled Persist Context"
    
    This turns on saving the context signals of visits to the History
    database, as well as access to the chrome://memories UI.
    
    This flag is meant for fishfood first, then Canary Dogfood.
    
    Notably, it also turns on access to chrome://memories UI. That's a
    side effect of our persistence fishfood/dogfood, but I think it's
    harmless.
    
    Bug: 1184879, 1171352
    Change-Id: Ice3269afda1edfb5adb9d994aa212625c0304853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2885419
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881253}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -923,6 +923,14 @@ const FeatureEntry::Choice kMemlogSamplingRateChoices[] = {
      heap_profiling::kMemlogSamplingRate5MB},
 };
 
+const FeatureEntry::FeatureVariation kMemoriesVariations[] = {{
+    "Persist Context",
+    (FeatureEntry::FeatureParam[]){
+        {"MemoriesPersistContextAnnotationsInHistoryDb", "true"}},
+    1,
+    nullptr,
+}};
+
 #if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_MAC) || \
     defined(OS_WIN)
 const FeatureEntry::FeatureParam kOmniboxDocumentProviderServerScoring[] = {
@@ -4462,7 +4470,9 @@ const FeatureEntry kFeatureEntries[] = {
 
     {"memories", flag_descriptions::kMemoriesName,
      flag_descriptions::kMemoriesDescription, kOsAll,
-     FEATURE_VALUE_TYPE(history_clusters::kMemories)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(history_clusters::kMemories,
+                                    kMemoriesVariations,
+                                    "Memories")},
 
     {"memories-debug", flag_descriptions::kMemoriesDebugName,
      flag_descriptions::kMemoriesDebugDescription, kOsDesktop,
