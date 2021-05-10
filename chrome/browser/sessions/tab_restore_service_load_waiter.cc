20d39159df1f910936ca9419fbf41bdfcac14ade
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-10 20:20:06 +0000

    Retire ScopedObserver in /chrome/browser/sessions.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 1145565
    Change-Id: I1de91640ba89222864b1c5053ccf3d9595ffa55c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883961
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881202}

--- chrome/browser/sessions/tab_restore_service_load_waiter.cc
+++ chrome/browser/sessions/tab_restore_service_load_waiter.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 TabRestoreServiceLoadWaiter::TabRestoreServiceLoadWaiter(
     sessions::TabRestoreService* service)
     : service_(service) {
-  observer_.Add(service_);
+  observation_.Observe(service_);
 }
 
 TabRestoreServiceLoadWaiter::~TabRestoreServiceLoadWaiter() = default;
