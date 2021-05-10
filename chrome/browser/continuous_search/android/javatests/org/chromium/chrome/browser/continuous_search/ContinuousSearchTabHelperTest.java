64bd1fad16a5298a2100c57d1b08bdfe8faf1ff1
ckitagawa <ckitagawa@chromium.org>
2021-05-10 19:20:49 +0000

    [CSN] Disable Integration Test
    Disable integration test it is flaking only on the official builder for
    some reason (looks like the tab is failing to load).
    
    Bug: 1176268
    Change-Id: I523b2fea47f46c29e5c8b425a815efa58549f1b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2884859
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Yashar Dabiran <yashard@chromium.org>
    Reviewed-by: Yashar Dabiran <yashard@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881154}

--- chrome/browser/continuous_search/android/javatests/org/chromium/chrome/browser/continuous_search/ContinuousSearchTabHelperTest.java
+++ chrome/browser/continuous_search/android/javatests/org/chromium/chrome/browser/continuous_search/ContinuousSearchTabHelperTest.java
@@ -22,6 +22,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
@@ -208,6 +209,7 @@ public class ContinuousSearchTabHelperTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/1176268")
     public void testContinuousSearchFakeResults() throws TimeoutException {
         WaitableContinuousNavigationUserDataObserver observer =
                 new WaitableContinuousNavigationUserDataObserver();
