8c2970615af57479bdf09c4ace17210276c012a4
Tommy Steimel <steimel@chromium.org>
2021-05-10 21:27:45 +0000

    Fix enum naming to use k prefixes in MediaSessionUmaHelper
    Change-Id: If821b2bcaf03d1aa0e8a33e904fd9f82c23b5307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880904
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881240}

--- content/browser/media/session/media_session_impl_uma_unittest.cc
+++ content/browser/media/session/media_session_impl_uma_unittest.cc
@@ -78,13 +78,14 @@ struct ActionMappingEntry {
 };
 
 ActionMappingEntry kActionMappings[] = {
-    {MediaSessionAction::kPlay, MediaSessionUserAction::Play},
-    {MediaSessionAction::kPause, MediaSessionUserAction::Pause},
-    {MediaSessionAction::kPreviousTrack, MediaSessionUserAction::PreviousTrack},
-    {MediaSessionAction::kNextTrack, MediaSessionUserAction::NextTrack},
-    {MediaSessionAction::kSeekBackward, MediaSessionUserAction::SeekBackward},
-    {MediaSessionAction::kSeekForward, MediaSessionUserAction::SeekForward},
-    {MediaSessionAction::kSkipAd, MediaSessionUserAction::SkipAd},
+    {MediaSessionAction::kPlay, MediaSessionUserAction::kPlay},
+    {MediaSessionAction::kPause, MediaSessionUserAction::kPause},
+    {MediaSessionAction::kPreviousTrack,
+     MediaSessionUserAction::kPreviousTrack},
+    {MediaSessionAction::kNextTrack, MediaSessionUserAction::kNextTrack},
+    {MediaSessionAction::kSeekBackward, MediaSessionUserAction::kSeekBackward},
+    {MediaSessionAction::kSeekForward, MediaSessionUserAction::kSeekForward},
+    {MediaSessionAction::kSkipAd, MediaSessionUserAction::kSkipAd},
 };
 
 }  // anonymous namespace
@@ -136,7 +137,7 @@ TEST_F(MediaSessionImplUmaTest, RecordPauseDefaultOnUISuspend) {
       GetHistogramSamplesSinceTestStart("Media.Session.UserAction"));
   EXPECT_EQ(1, samples->TotalCount());
   EXPECT_EQ(1, samples->GetCount(static_cast<base::HistogramBase::Sample>(
-                   MediaSessionUserAction::PauseDefault)));
+                   MediaSessionUserAction::kPauseDefault)));
 }
 
 TEST_F(MediaSessionImplUmaTest, RecordPauseDefaultOnUISuspendWithAction) {
@@ -149,7 +150,7 @@ TEST_F(MediaSessionImplUmaTest, RecordPauseDefaultOnUISuspendWithAction) {
       GetHistogramSamplesSinceTestStart("Media.Session.UserAction"));
   EXPECT_EQ(1, samples->TotalCount());
   EXPECT_EQ(1, samples->GetCount(static_cast<base::HistogramBase::Sample>(
-                   MediaSessionUserAction::Pause)));
+                   MediaSessionUserAction::kPause)));
 }
 
 TEST_F(MediaSessionImplUmaTest, RecordPauseDefaultOnSystemSuspend) {
@@ -173,7 +174,7 @@ TEST_F(MediaSessionImplUmaTest, RecordPauseDefaultOnUIResume) {
       GetHistogramSamplesSinceTestStart("Media.Session.UserAction"));
   EXPECT_EQ(1, samples->TotalCount());
   EXPECT_EQ(1, samples->GetCount(static_cast<base::HistogramBase::Sample>(
-                   MediaSessionUserAction::PlayDefault)));
+                   MediaSessionUserAction::kPlayDefault)));
 }
 
 TEST_F(MediaSessionImplUmaTest, RecordPauseDefaultOnUIResumeWithAction) {
@@ -187,7 +188,7 @@ TEST_F(MediaSessionImplUmaTest, RecordPauseDefaultOnUIResumeWithAction) {
       GetHistogramSamplesSinceTestStart("Media.Session.UserAction"));
   EXPECT_EQ(1, samples->TotalCount());
   EXPECT_EQ(1, samples->GetCount(static_cast<base::HistogramBase::Sample>(
-                   MediaSessionUserAction::Play)));
+                   MediaSessionUserAction::kPlay)));
 }
 
 TEST_F(MediaSessionImplUmaTest, RecordPauseDefaultOnSystemResume) {
@@ -213,7 +214,7 @@ TEST_F(MediaSessionImplUmaTest, RecordPauseDefaultOnUIStop) {
       GetHistogramSamplesSinceTestStart("Media.Session.UserAction"));
   EXPECT_EQ(1, samples->TotalCount());
   EXPECT_EQ(1, samples->GetCount(static_cast<base::HistogramBase::Sample>(
-                   MediaSessionUserAction::StopDefault)));
+                   MediaSessionUserAction::kStopDefault)));
 }
 
 // This should never happen but just check this to be safe.
