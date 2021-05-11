c5a46e23b3894139f4edd62cf99f2531917c8b9f
Michael Thiessen <mthiesse@chromium.org>
2021-05-11 19:00:39 +0000

    Verify InputEvents for Attribution Intents on R+
    Android R introduces InputManager#verifyInputEvent, which we use to
    ensure that users of the Attribution Intent API actually received a real
    input event from a user.
    
    A followup change will prevent re-using the same InputEvent for multiple
    attribution Intents.
    
    Bug: 1198308
    Change-Id: I25d92203fe3701fe991290ccc474b90bd6d76953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2877014
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881628}

--- chrome/browser/attribution_reporting/android/internal/java/src/org/chromium/chrome/browser/attribution_reporting/AttributionIntentHandlerFactory.java
+++ chrome/browser/attribution_reporting/android/internal/java/src/org/chromium/chrome/browser/attribution_reporting/AttributionIntentHandlerFactory.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.attribution_reporting;
 
+import android.view.InputEvent;
+
+import org.chromium.base.Predicate;
 import org.chromium.chrome.browser.flags.CachedFeatureFlags;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 
@@ -13,14 +16,22 @@ import org.chromium.chrome.browser.flags.ChromeFeatureList;
  * module.
  */
 public class AttributionIntentHandlerFactory {
+    // Static to preserve state across Chrome launches. Preserving state across process kills would
+    // also be correct, but probably overkill as we only allow recent InputEvents.
+    private static Predicate<InputEvent> sValidator = new InputEventValidator();
+
     /**
      * @return a AttributionIntentHandler instance.
      */
     public static AttributionIntentHandler create() {
         if (CachedFeatureFlags.isEnabled(ChromeFeatureList.APP_TO_WEB_ATTRIBUTION)) {
-            return new AttributionIntentHandlerImpl();
+            return new AttributionIntentHandlerImpl(sValidator);
         } else {
             return new NoopAttributionIntentHandler();
         }
+    }
+
+    public static void setInputEventValidatorForTesting(Predicate<InputEvent> validator) {
+        sValidator = validator;
     }
 }
