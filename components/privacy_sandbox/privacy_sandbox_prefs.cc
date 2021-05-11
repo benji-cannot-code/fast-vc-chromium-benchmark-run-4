e07b33728f2ede7ebb21a31321e1d3a9c0066d0f
sauski <sauski@google.com>
2021-05-11 19:28:44 +0000

    Privacy Sandbox Settings: Add dedicated FLoC preference
    As part of Kartoffel 2, FLoC will be broken out into an independent
    control. This CL simply lands an additional preference for FLoC control
    and updates the comments associated with the existing preferences to
    better reflect what will be their updated meaning.
    
    Bug: 1206250
    Change-Id: I8c7a112bc84e56a3ee99c6be75d8a196800330ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886419
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881645}

--- components/privacy_sandbox/privacy_sandbox_prefs.cc
+++ components/privacy_sandbox/privacy_sandbox_prefs.cc
@@ -24,6 +24,8 @@ const char kPrivacySandboxPageViewed[] = "privacy_sandbox.page_viewed";
 const char kPrivacySandboxFlocDataAccessibleSince[] =
     "privacy_sandbox.floc_data_accessible_since";
 
+extern const char kPrivacySandboxFlocEnabled[] = "privacy_sandbox.floc_enabled";
+
 }  // namespace prefs
 
 namespace privacy_sandbox {
@@ -40,6 +42,9 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kPrivacySandboxPageViewed, false);
   registry->RegisterTimePref(prefs::kPrivacySandboxFlocDataAccessibleSince,
                              base::Time());
+  registry->RegisterBooleanPref(
+      prefs::kPrivacySandboxFlocEnabled, true,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
 }
 
 }  // namespace privacy_sandbox
