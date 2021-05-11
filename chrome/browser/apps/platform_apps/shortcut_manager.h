7eb102159d0cd413c70b0431eb1bc4a5a2319b9a
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-11 01:14:20 +0000

    Retire ScopedObserver in /chrome/browser/apps.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=benwells@chromium.org
    
    Bug: 1145565
    Change-Id: I0df5afc08a98ee835933dfd33216b86ba59a8f61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883689
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881349}

--- chrome/browser/apps/platform_apps/shortcut_manager.h
+++ chrome/browser/apps/platform_apps/shortcut_manager.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "extensions/browser/extension_registry.h"
@@ -63,11 +63,12 @@ class AppShortcutManager : public KeyedService,
   void DeleteApplicationShortcuts(const extensions::Extension* extension);
 
   Profile* profile_;
-  ScopedObserver<ProfileAttributesStorage, ProfileAttributesStorage::Observer>
-      profile_storage_observer_{this};
-  ScopedObserver<extensions::ExtensionRegistry,
-                 extensions::ExtensionRegistryObserver>
-      extension_registry_observer_{this};
+  base::ScopedObservation<ProfileAttributesStorage,
+                          ProfileAttributesStorage::Observer>
+      profile_storage_observation_{this};
+  base::ScopedObservation<extensions::ExtensionRegistry,
+                          extensions::ExtensionRegistryObserver>
+      extension_registry_observation_{this};
 
   base::WeakPtrFactory<AppShortcutManager> weak_ptr_factory_{this};
 };
