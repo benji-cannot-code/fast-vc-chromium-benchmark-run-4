ca311fafa74541a15b602f68cd58288dc3d4ab4c
Jimmy Gong <jimmyxgong@google.com>
2021-05-11 03:56:25 +0000

    Add base navigation-selector element
    This basic navigation-selector element hosts a list of menu
    items that users are able to click. A follow up CL will implement
    displaying the navigated page and add additional styling to the menu
    items.
    
    Screenshots:
    Navigation + collapsed entry:
    https://screenshot.googleplex.com/4xj6F59AvQqdk2r
    
    Navigation + expanded entry:
    https://screenshot.googleplex.com/8tC9aQwrLzmjjsr
    
    Bug: 1205539
    Test: browser_tests
    Change-Id: I883f32db2d73286ed87795c6003fadc39285ed84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872740
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881397}

--- chrome/test/data/webui/chromeos/ash_common/ash_common_unified_test.js
+++ chrome/test/data/webui/chromeos/ash_common/ash_common_unified_test.js
@@ -8,6 +8,7 @@ import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
 
 import {fakeMethodResolverTestSuite} from './fake_method_resolver_test.js';
 import {fakeObservablesTestSuite} from './fake_observables_test.js';
+import {navigationSelectorTestSuite} from './navigation_selector_test.js';
 
 window.test_suites_list = [];
 
@@ -18,3 +19,4 @@ function runSuite(suiteName, testFn) {
 
 runSuite('FakeObservables', fakeObservablesTestSuite);
 runSuite('FakeMethodResolver', fakeMethodResolverTestSuite);
+runSuite('NavigationSelector', navigationSelectorTestSuite);
