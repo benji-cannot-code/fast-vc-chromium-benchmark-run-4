e0e3ba41ed9c0b4d8d875f1a107ec03e17b5a5e6
Tommy Li <tommycli@chromium.org>
2021-05-11 17:02:07 +0000

    [memories] Stop HistoryBackend deleting stray annotation rows & Add Test
    This CL does two things:
    
    1. Stops HistoryBackend from attempting to delete stray annotation rows.
       That's no longer necessary because ExpireHistoryBackend takes care
       of that.
    
    2. Also adds a test to ExpireHistoryBackend's unit tests to verify that
       the context annotation rows are indeed expired along with the visit
       rows.
    
    Bug: 1184879, 1184875, 1171352
    Change-Id: I6c4205bf441ef98d812ef1bd7ac9430128747678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854390
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881577}

--- components/history/core/browser/history_backend.cc
+++ components/history/core/browser/history_backend.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/memory_pressure_listener.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
+#include "base/notreached.h"
 #include "base/rand_util.h"
 #include "base/sequenced_task_runner.h"
 #include "base/single_thread_task_runner.h"
@@ -1437,7 +1438,6 @@ std::vector<AnnotatedVisit> HistoryBackend::GetAnnotatedVisits(
   TRACE_EVENT0("browser", "HistoryBackend::GetAnnotatedVisits");
   if (!db_)
     return {};
-  bool deleted_any_visits = false;
   std::vector<AnnotatedVisit> annotated_visits;
   for (const auto& row : db_->GetAnnotatedVisits(max_results)) {
     URLRow url_row;
@@ -1447,12 +1447,12 @@ std::vector<AnnotatedVisit> HistoryBackend::GetAnnotatedVisits(
       annotated_visits.push_back(
           {url_row, visit_row, row.context_annotations, {}});
     } else {
-      db_->DeleteAnnotationsForVisit(row.visit_id);
-      deleted_any_visits = true;
+      // Ignore corrupt data but do not crash, as user DBs can be in bad states.
+      DVLOG(0) << "HistoryBackend: AnnotatedVisit found with missing associated"
+                  "URL or visit. visit_id = "
+               << row.visit_id;
     }
   }
-  if (deleted_any_visits)
-    ScheduleCommit();
   return annotated_visits;
 }
 
