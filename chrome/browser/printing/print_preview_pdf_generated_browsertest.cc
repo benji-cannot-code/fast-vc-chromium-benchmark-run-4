1a321b3fcd79a3421f0fccb2a4915d360b89245c
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-11 00:23:48 +0000

    CodeHealth: Remove use of DictionaryValue::empty in print*
    This change removes the use of DictionaryValue::empty()
    and replaces with Value::DictEmpty()
    
    This CL was uploaded by git cl split.
    
    R=rbpotter@chromium.org
    
    Bug: 1187020
    Change-Id: I2ca1480033f27fc5b7ba3eabd9fa5506cbdcb26e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879650
    Auto-Submit: Fangzhen Song <songfangzhen@bytedance.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881328}

--- chrome/browser/printing/print_preview_pdf_generated_browsertest.cc
+++ chrome/browser/printing/print_preview_pdf_generated_browsertest.cc
@@ -208,7 +208,7 @@ class PrintPreviewDelegate : printing::PrintPreviewUI::TestDelegate {
       return;
     }
 
-    ASSERT_FALSE(script_argument.empty());
+    ASSERT_FALSE(script_argument.DictEmpty());
     GetUI()->SendManipulateSettingsForTest(script_argument);
   }
 
