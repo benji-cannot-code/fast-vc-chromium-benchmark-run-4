f3f5a73021c5a735580450f811f1c5cd9ab31c83
Tibor Goldschwendt <tiborg@chromium.org>
2021-05-11 02:03:26 +0000

    [ntp][drive] Add fake data variation
    Fake data ensures that the module shows up. This makes development and
    launch reviews easier.
    
    Fixed: 1206107
    Change-Id: Iab6cc89b63d46fa109c32156e87c92b383884b05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2878565
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881371}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1497,6 +1497,13 @@ const FeatureEntry::FeatureVariation kNtpShoppingTasksModuleVariations[] = {
      "t3329139" /* variation_id */},
 };
 
+const FeatureEntry::FeatureParam kNtpDriveModuleFakeData[] = {
+    {ntp_features::kNtpDriveModuleDataParam, "fake"}};
+const FeatureEntry::FeatureVariation kNtpDriveModuleVariations[] = {
+    {"- Fake Data", kNtpDriveModuleFakeData,
+     base::size(kNtpDriveModuleFakeData), nullptr},
+};
+
 const FeatureEntry::FeatureParam kNtpRepeatableQueriesInsertPositionStart[] = {
     {ntp_features::kNtpRepeatableQueriesInsertPositionParam, "start"}};
 const FeatureEntry::FeatureParam kNtpRepeatableQueriesInsertPositionEnd[] = {
@@ -4651,7 +4658,9 @@ const FeatureEntry kFeatureEntries[] = {
 
     {"ntp-drive-module", flag_descriptions::kNtpDriveModuleName,
      flag_descriptions::kNtpDriveModuleDescription, kOsDesktop,
-     FEATURE_VALUE_TYPE(ntp_features::kNtpDriveModule)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(ntp_features::kNtpDriveModule,
+                                    kNtpDriveModuleVariations,
+                                    "DesktopNtpModules")},
 
     {"ntp-recipe-tasks-module", flag_descriptions::kNtpRecipeTasksModuleName,
      flag_descriptions::kNtpRecipeTasksModuleDescription, kOsDesktop,
