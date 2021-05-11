e73934bb4cb746c87477cfaffd2ccf456530d6eb
John Palmer <jopalmer@chromium.org>
2021-05-11 05:48:31 +0000

    [Emoji picker] fix emoji picker focusing on first emoji variant when variants are opened
    #SIMPLE_REVIEW
    
    Bug: b/187231489
    Change-Id: Iafb8689228af629540a4112d264bd1412f5cc6ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879254
    Auto-Submit: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881414}

--- chrome/browser/resources/chromeos/emoji_picker/emoji_variants.js
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_variants.js
@@ -97,9 +97,7 @@ export class EmojiVariants extends PolymerElement {
   }
 
   connectedCallback() {
-    beforeNextRender(
-        this,
-        () => this.shadowRoot.querySelector('emoji-button').focusButton());
+    beforeNextRender(this, () => this.$['fake-focus-target'].focus());
   }
 
   computeVariantRowLengths(variants) {
