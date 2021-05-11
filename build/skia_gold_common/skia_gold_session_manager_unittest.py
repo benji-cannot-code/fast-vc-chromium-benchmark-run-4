45335f618932cb056e14172a7ab0a2d4a5888a5f
Brian Sheedy <bsheedy@chromium.org>
2021-05-11 17:12:54 +0000

    Make common Gold code Python 3-compatible
    Makes the code in //build/skia_gold_common/ compatible with both Python
    2 and 3. Also changes the shebangs of the unittests to use vpython3
    instead of vpython, which causes presubmit to run the tests with both
    Python 2 and 3.
    
    Bug: 1198237
    Change-Id: I544ba573971fc0084fe7b11b70e2328ab7bc7267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886199
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881581}

--- build/skia_gold_common/skia_gold_session_manager_unittest.py
+++ build/skia_gold_common/skia_gold_session_manager_unittest.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env vpython
+#!/usr/bin/env vpython3
 # Copyright 2020 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -8,10 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import json
 import os
+import sys
 import tempfile
 import unittest
 
-import mock
+if sys.version_info[0] == 2:
+  import mock
+else:
+  import unittest.mock as mock
 
 from pyfakefs import fake_filesystem_unittest
 
