bfafb331a009896d8357f897c2bb009550eeac58
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-11 20:28:40 +0000

    CodeHealth: Remove use of DictionaryValue::empty in /chrome/browser/safe_browsing
    This change removes the use of DictionaryValue::empty()
    and replaces with Value::DictEmpty()
    
    This CL was uploaded by git cl split.
    
    R=bdea@chromium.org
    
    Bug: 1187020
    Change-Id: Ia4aa550b75713d453a952d4372c761b8ca8a7f03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880319
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Auto-Submit: Fangzhen Song <songfangzhen@bytedance.com>
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881684}

--- chrome/browser/safe_browsing/chrome_password_protection_service_browsertest.cc
+++ chrome/browser/safe_browsing/chrome_password_protection_service_browsertest.cc
@@ -494,7 +494,7 @@ IN_PROC_BROWSER_TEST_F(ChromePasswordProtectionServiceBrowserTest,
   ASSERT_TRUE(
       profile->GetPrefs()
           ->GetDictionary(prefs::kSafeBrowsingUnhandledGaiaPasswordReuses)
-          ->empty());
+          ->DictEmpty());
 
   base::HistogramTester histograms;
   // Shows modal dialog on current web_contents.
