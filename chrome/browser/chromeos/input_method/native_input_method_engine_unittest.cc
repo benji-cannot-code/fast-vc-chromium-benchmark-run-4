476481d47f33706527256f4313f25815e6fe7744
Darren Shen <shend@chromium.org>
2021-05-11 23:59:51 +0000

    ime: Enable IME service flag in test.
    Needed as we're disabling IME service flag temporarily.
    
    Bug: 1186911
    Change-Id: Icec353935905db2aa9667459f8d524e04a8cfea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2890309
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Reviewed-by: Leo Zhang <googleo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881782}

--- chrome/browser/chromeos/input_method/native_input_method_engine_unittest.cc
+++ chrome/browser/chromeos/input_method/native_input_method_engine_unittest.cc
@@ -104,6 +104,7 @@ class NativeInputMethodEngineTest : public ::testing::Test {
                               features::kAssistPersonalInfoEmail,
                               features::kAssistPersonalInfoName,
                               features::kEmojiSuggestAddition,
+                              features::kImeMojoDecoder,
                               features::kSystemLatinPhysicalTyping},
         /*disabled_features=*/{});
 
