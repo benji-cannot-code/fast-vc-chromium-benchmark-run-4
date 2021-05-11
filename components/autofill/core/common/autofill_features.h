efd8305bce2330dafd01d922f7603812b7c05751
Dominic Battre <battre@chromium.org>
2021-05-11 08:17:00 +0000

    Implement AutofillAblationStudy
    This CL introduces a class AutofillAblationStudy whose purpose is to
    decide whether a given [Site * Day * Browsing session] should be exposed
    to an ablated autofill experience.
    
    Bug: 1205771
    Change-Id: I9af3ef1293b9e0c1102995f78d13036a6d112137
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2878847
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/master@{#881449}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -133,8 +133,6 @@ COMPONENT_EXPORT(AUTOFILL)
 extern const base::FeatureParam<bool>
     kAutofillAblationStudyEnabledForPaymentsParam;
 COMPONENT_EXPORT(AUTOFILL)
-extern const base::FeatureParam<int> kAutofillAblationStudyTotalWeightParam;
-COMPONENT_EXPORT(AUTOFILL)
 extern const base::FeatureParam<int>
     kAutofillAblationStudyAblationWeightPerMilleParam;
 
