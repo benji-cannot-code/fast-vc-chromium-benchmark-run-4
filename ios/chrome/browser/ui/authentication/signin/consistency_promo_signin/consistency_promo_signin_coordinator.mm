f057d50c487597f03064f355ec1c278ee5585705
Nohemi Fernandez <fernandex@chromium.org>
2021-05-11 13:23:01 +0000

    [iOS][MICE] Suppress web sign-in when no accounts are on the device.
    Do not display the web sign-in promo when there are no accounts on the
    device either when the promo is first shown or if the account list
    changes during sign-in.
    
    Stops tracking identity changes after the view controller is dismissed.
    
    Bug: 1151288
    Change-Id: I51d36fd746bedd7bab4d07b1b405a0eb49b2e157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879028
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881513}

--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
@@ -169,6 +169,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                          SigninCoordinatorResultCanceledByUser
                                                identity:nil];
                          }];
+  _identityManagerObserverBridge.reset();
 }
 
 // Calls the sign-in completion block.
