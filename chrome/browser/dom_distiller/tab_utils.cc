dd99236f009944f63f31940d42ff1fcfd36fcc51
Katie Dektar <katie@chromium.org>
2021-05-11 02:59:35 +0000

    Fix AddressSanitizer use-after-free crash.
    Log time on previous web contents before navigating away
    from and destroying those web contents.
    
    Bug: 1203674
    Change-Id: Ic904864a1a729ee22870745e29f87c42c9886922
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872906
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881384}

--- chrome/browser/dom_distiller/tab_utils.cc
+++ chrome/browser/dom_distiller/tab_utils.cc
@@ -166,6 +166,12 @@ void DistillCurrentPageAndView(content::WebContents* old_web_contents) {
   new_web_contents->GetController().CopyStateFrom(
       &old_web_contents->GetController(), /* needs_reload */ true);
 
+#if !defined(OS_ANDROID)
+  // Use the old_web_contents to log time on the distillable page before
+  // navigating away from these contents.
+  dom_distiller::UMAHelper::LogTimeOnDistillablePage(old_web_contents);
+#endif
+
   // StartNavigationToDistillerViewer must come before swapping the tab contents
   // to avoid triggering a reload of the page.  This reloadmakes it very
   // difficult to distinguish between the intermediate reload and a user hitting
@@ -181,10 +187,6 @@ void DistillCurrentPageAndView(content::WebContents* old_web_contents) {
       new SourcePageHandleWebContents(old_web_contents_owned.release(), true));
 
   MaybeStartDistillation(std::move(source_page_handle));
-
-#if !defined(OS_ANDROID)
-  dom_distiller::UMAHelper::LogTimeOnDistillablePage(old_web_contents);
-#endif
 }
 
 void DistillCurrentPage(content::WebContents* source_web_contents) {
