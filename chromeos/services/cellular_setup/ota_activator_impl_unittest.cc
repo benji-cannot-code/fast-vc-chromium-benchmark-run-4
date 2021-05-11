4d971159755dfa2ad93f2c294cd763d405f25286
Azeem Arshad <azeemarshad@chromium.org>
2021-05-11 00:29:52 +0000

    [CrOS Cellular] Fix network connect issue in OtaActivator.
    This CL fixes issue with OtaActivator issuing multiple connect requests
    back to back. This was caused because OtaActivator attempts to make
    network connect with ON_STARTED mode. This causes OtaActivator's
    connect request to complete as soon as connect is acknowledged. A
    subsequent change in network properties triggers a second network
    connect request.
    
    Fixed this by changing callback mode to ON_COMPLETED.
    
    NOTE: The user will still receive error notifications if the connect
    fails due to a real shill error. But retry will be attempted with
    exponential back-off and max out at 3 attempts.
    
    Fixed: 1204363
    Change-Id: I82e065aa05ddf5b3e4fd16d614a3fe2171606682
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2885947
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881330}

--- chromeos/services/cellular_setup/ota_activator_impl_unittest.cc
+++ chromeos/services/cellular_setup/ota_activator_impl_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_simple_task_runner.h"
 #include "chromeos/network/fake_network_activation_handler.h"
 #include "chromeos/network/fake_network_connection_handler.h"
+#include "chromeos/network/network_connection_handler.h"
 #include "chromeos/network/network_state_handler.h"
 #include "chromeos/network/network_state_test_helper.h"
 #include "chromeos/services/cellular_setup/public/cpp/fake_activation_delegate.h"
@@ -203,6 +204,8 @@ class CellularSetupOtaActivatorImplTest : public testing::Test {
 
     // A connection should have been requested by |ota_activator_|.
     EXPECT_EQ(kTestCellularServicePath, connect_calls.back().service_path());
+    EXPECT_EQ(ConnectCallbackMode::ON_COMPLETED,
+              connect_calls.back().connect_callback_mode());
     connect_calls.back().InvokeErrorCallback("fake_error", nullptr);
     base::RunLoop().RunUntilIdle();
   }
