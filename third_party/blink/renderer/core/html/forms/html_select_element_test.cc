f241ce0c42e324560bbe9cc0d6bfe0629196278c
Rune Lillesveen <futhark@chromium.org>
2021-05-11 12:22:12 +0000

    Fix style recalc root update for select rendering change
    SubtreeModified() was called with select as the parent, but the select
    is invoked with DetachLayoutTree() which clears the dirty bits and is
    typically what happens on removed children for SubtreeModified(), which
    caused DCHECKs in SubtreeModified(). Instead call SubtreeModified() with
    the select's parent which better model the typical input for
    SubtreeModified().
    
    Bug: 1207287
    Change-Id: Ife55912256574c84896f21431155eeac1ffa15e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883806
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881498}

--- third_party/blink/renderer/core/html/forms/html_select_element_test.cc
+++ third_party/blink/renderer/core/html/forms/html_select_element_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
@@ -621,6 +622,47 @@ TEST_F(HTMLSelectElementTest, ChangeRenderingCrash) {
   // Changing the size attribute changes the rendering. This should not trigger
   // a DCHECK failure updating the style recalc root.
   GetElementById("sel")->setAttribute(html_names::kSizeAttr, AtomicString("2"));
+}
+
+TEST_F(HTMLSelectElementTest, ChangeRenderingCrash2) {
+  SetHtmlInnerHTML(R"HTML(
+    <select id="sel">
+      <optgroup id="grp">
+        <option id="opt"></option>
+      </optgroup>
+    </select>
+  )HTML");
+  GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kTest);
+  // Make the select UA slot the style recalc root.
+  GetElementById("opt")->SetInlineStyleProperty(CSSPropertyID::kColor, "green");
+  GetElementById("grp")->SetInlineStyleProperty(CSSPropertyID::kColor, "green");
+  // Changing the multiple attribute changes the rendering. This should not
+  // trigger a DCHECK failure updating the style recalc root.
+  GetElementById("sel")->setAttribute(html_names::kMultipleAttr,
+                                      AtomicString("true"));
+}
+
+TEST_F(HTMLSelectElementTest, ChangeRenderingSelectRoot) {
+  // This test exercises the path in StyleEngine::ChangeRenderingForHTMLSelect()
+  // where the select does not have a GetStyleRecalcParent().
+  SetHtmlInnerHTML(R"HTML(
+    <select id="sel">
+      <option></option>
+    </select>
+  )HTML");
+
+  auto* select = GetElementById("sel");
+
+  // Make the select the root element.
+  select->remove();
+  GetDocument().documentElement()->remove();
+  GetDocument().appendChild(select);
+  GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kTest);
+
+  // Changing the multiple attribute changes the rendering.
+  select->setAttribute(html_names::kMultipleAttr, AtomicString("true"));
+  EXPECT_TRUE(GetDocument().GetStyleEngine().NeedsStyleRecalc());
+  EXPECT_TRUE(select->NeedsStyleRecalc());
 }
 
 }  // namespace blink
