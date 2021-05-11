aefe5671d9e57dc194e0679bad6540acad0ebfa8
Alison Maher <almaher@microsoft.com>
2021-05-11 04:48:22 +0000

    [LayoutNG] Multicol overflow
    An empty child is not appended when calculating the overflow rect.
    In the case of a multicol, an empty column may result in an overflow
    if there is a non-zero column gap. To fix this, append an empty
    child to the overflow rect if it is a fragmentainer.
    
    Bug: 829028
    Change-Id: I4f25dee9121a5e6f30a8be15e629699fdd7b1c3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2884799
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881404}

--- third_party/blink/renderer/core/layout/ng/ng_layout_overflow_calculator.h
+++ third_party/blink/renderer/core/layout/ng/ng_layout_overflow_calculator.h
@@ -42,7 +42,7 @@ class CORE_EXPORT NGLayoutOverflowCalculator {
                 PhysicalOffset offset) {
     PhysicalRect child_overflow = LayoutOverflowForPropagation(child_fragment);
     child_overflow.offset += offset;
-    AddOverflow(child_overflow);
+    AddOverflow(child_overflow, child_fragment.IsFragmentainerBox());
   }
 
   // Adds layout-overflow from fragment-items.
@@ -62,11 +62,14 @@ class CORE_EXPORT NGLayoutOverflowCalculator {
   PhysicalRect LayoutOverflowForPropagation(
       const NGPhysicalBoxFragment& child_fragment);
 
-  void AddOverflow(PhysicalRect child_overflow) {
+  void AddOverflow(PhysicalRect child_overflow,
+                   bool child_is_fragmentainer = false) {
     if (is_scroll_container_)
       child_overflow = AdjustOverflowForScrollOrigin(child_overflow);
 
-    if (!child_overflow.IsEmpty())
+    // A fragmentainer may result in an overflow, even if it is empty. For
+    // example, an overflow as a result of a non-zero column gap.
+    if (!child_overflow.IsEmpty() || child_is_fragmentainer)
       layout_overflow_.UniteEvenIfEmpty(child_overflow);
   }
 
