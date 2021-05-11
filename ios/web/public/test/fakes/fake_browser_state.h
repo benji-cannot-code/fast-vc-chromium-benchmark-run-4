a2abf69f97a7802e771e9f723c162d57507c641d
Sylvain Defresne <sdefresne@chromium.org>
2021-05-11 14:31:51 +0000

    [ios] Mark BrowserState implementation classes as final
    The BrowserState implementation classes must not be sub-classed,
    so mark them as `final` to allow the compiler to catch incorrect
    sub-classing.
    
    Bug: none
    Change-Id: I03e4be165675bd1e629561ab2d0a2079d215226b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883706
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881531}

--- ios/web/public/test/fakes/fake_browser_state.h
+++ ios/web/public/test/fakes/fake_browser_state.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 namespace web {
-class FakeBrowserState : public BrowserState {
+class FakeBrowserState final : public BrowserState {
  public:
   static const char kCorsExemptTestHeaderName[];
 
