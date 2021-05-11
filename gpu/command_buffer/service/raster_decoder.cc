d5c0eecd4fcf14745bcb99a0e75e3cfa043179a1
Jonah Chin <jochin@microsoft.com>
2021-05-11 00:52:08 +0000

    Make swap chain mode compatible with Canvas OOPR on Windows
    Updates CanvasResource(Provider)SwapChain to conditionally use OOPR
    raster APIs. This is required for low latency canvas to work with
    Canvas OOPR.
    
    Bug: 1093147
    Change-Id: Ic85ff2dc3998e92b434e6cafd7b9ef27e62a74b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880910
    Commit-Queue: Jonah Chin <jochin@microsoft.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881338}

--- gpu/command_buffer/service/raster_decoder.cc
+++ gpu/command_buffer/service/raster_decoder.cc
@@ -83,7 +83,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(ENABLE_VULKAN)
 #include "components/viz/common/gpu/vulkan_context_provider.h"
 #include "gpu/vulkan/vulkan_device_queue.h"
-#endif
+#endif  // BUILDFLAG(ENABLE_VULKAN)
+
+#if defined(OS_WIN)
+#include "gpu/command_buffer/service/shared_image_backing_factory_d3d.h"
+#endif  // OS_WIN
 
 // Local versions of the SET_GL_ERROR macros
 #define LOCAL_SET_GL_ERROR(error, function_name, msg) \
@@ -1163,6 +1167,10 @@ Capabilities RasterDecoderImpl::GetCapabilities() {
     caps.texture_half_float_linear =
         feature_info()->feature_flags().enable_texture_half_float_linear;
   }
+#if defined(OS_WIN)
+  caps.shared_image_swap_chain =
+      SharedImageBackingFactoryD3D::IsSwapChainSupported();
+#endif  // OS_WIN
   return caps;
 }
 
