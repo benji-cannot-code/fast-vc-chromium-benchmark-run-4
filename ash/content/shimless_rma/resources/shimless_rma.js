74f193c4fe2af976de59523fa768182965a318a0
Joon Ahn <joonbug@chromium.org>
2021-05-11 20:21:26 +0000

    shimless: load component from shimless container
    Bug: 1198187
    Test: browser_tests --gtest_filter=Shimless*
    Change-Id: If1147ed5b7e1116dbf83812341ebb0da7148a366
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2872552
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Auto-Submit: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881679}

--- ash/content/shimless_rma/resources/shimless_rma.js
+++ ash/content/shimless_rma/resources/shimless_rma.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import './onboarding_landing_page.js';
 import './shimless_rma_shared_css.js';
 
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -25,7 +26,7 @@ let PageInfo;
  */
 const StateComponentMapping = {
   // TODO(joonbug): Update with enum and actual componentId.
-  0: {componentIs: 'component1'},
+  0: {componentIs: 'onboarding-landing-page'},
 };
 
 /**
@@ -71,7 +72,49 @@ export class ShimlessRmaElement extends PolymerElement {
   loadState_(state) {
     const pageInfo = StateComponentMapping[state];
     this.currentPage_ = pageInfo;
-    // TODO(joonbug): Load component
+    this.showComponent_(pageInfo.componentIs);
+  }
+
+  /**
+   * @param {string} componentIs
+   * @private
+   */
+  showComponent_(componentIs) {
+    let component = this.shadowRoot.querySelector(`#${componentIs}`);
+    if (component === null) {
+      component = this.loadComponent_(componentIs);
+    }
+
+    this.hideAllComponents_();
+    component.hidden = false;
+  }
+
+  /**
+   * Utility method to bulk hide all contents.
+   */
+  hideAllComponents_() {
+    const components = this.shadowRoot.querySelectorAll('.shimless-content');
+    Array.from(components).map((c) => c.hidden = true);
+  }
+
+  /**
+   * @param {string} componentIs
+   * @private
+   */
+  loadComponent_(componentIs) {
+    const shimlessBody = this.shadowRoot.querySelector('#contentContainer');
+
+    let component = document.createElement(componentIs);
+    component.setAttribute('id', componentIs);
+    component.setAttribute('class', 'shimless-content');
+    component.hidden = true;
+
+    if (!component) {
+      console.error('Failed to create ' + componentIs);
+    }
+
+    shimlessBody.appendChild(component);
+    return component;
   }
 
   /** @protected */
--- chrome/test/data/webui/chromeos/shimless_rma/shimless_rma_app_test.js
+++ chrome/test/data/webui/chromeos/shimless_rma/shimless_rma_app_test.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {ShimlessRmaElement} from 'chrome://shimless-rma/shimless_rma.js';
 
-import {assertTrue} from '../../chai_assert.js';
+import {assertFalse, assertTrue} from '../../chai_assert.js';
 
 export function shimlessRMAAppTest() {
   /** @type {?ShimlessRmaElement} */
