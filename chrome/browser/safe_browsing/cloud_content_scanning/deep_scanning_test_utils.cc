ed9e5106ccac181055f28c16d4e14b7e2a120553
Anastasiia N <anastasiian@chromium.org>
2021-05-11 17:04:35 +0000

    CodeHealth: Remove use of ListValue::empty in /chrome/browser/safe_browsing/cloud_content_scanning
    This change removes the use of `ListValue::empty()`
    and replaces with `Value::GetList().empty()`.
    
    This CL was uploaded by git cl split.
    
    R=drubery@chromium.org
    
    Bug: 1187065
    Change-Id: Ib8854f7a922c75c18255ae66d8678cd8eb2bc8c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2885126
    Auto-Submit: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881578}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_test_utils.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_test_utils.cc
@@ -406,7 +406,7 @@ void SetAnalysisConnector(PrefService* prefs,
                           bool machine_scope) {
   ListPrefUpdate settings_list(prefs, ConnectorPref(connector));
   DCHECK(settings_list.Get());
-  if (!settings_list->empty())
+  if (!settings_list->GetList().empty())
     settings_list->Clear();
 
   settings_list->Append(*base::JSONReader::Read(pref_value));
@@ -424,7 +424,7 @@ void SetOnSecurityEventReporting(
                                enterprise_connectors::kOnSecurityEventPref);
   DCHECK(settings_list.Get());
   if (enabled) {
-    if (settings_list->empty()) {
+    if (settings_list->GetList().empty()) {
       base::Value settings(base::Value::Type::DICTIONARY);
 
       settings.SetKey(enterprise_connectors::kKeyServiceProvider,
