6dad786e315fee2f06ea2fa4fefdb7c17274437a
Olivier Li <olivierli@chromium.org>
2021-05-12 03:27:49 +0000

    Adapt /chrome/browser/devtools to move-only thread options.
    This change will let PlatformThread::Delegate::Options::delegate be a unique_ptr.
    This CL was uploaded by git cl split.
    
    Bug:1208166
    
    R=caseq@chromium.org
    
    Change-Id: Ia170296ac5439297224a4e0ba030e9ddddbd67b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2889376
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881882}

--- chrome/browser/devtools/device/android_device_manager.cc
+++ chrome/browser/devtools/device/android_device_manager.cc
@@ -521,7 +521,7 @@ AndroidDeviceManager::HandlerThread::HandlerThread() {
   thread_ = new base::Thread(kDevToolsAdbBridgeThreadName);
   base::Thread::Options options;
   options.message_pump_type = base::MessagePumpType::IO;
-  if (!thread_->StartWithOptions(options)) {
+  if (!thread_->StartWithOptions(std::move(options))) {
     delete thread_;
     thread_ = nullptr;
   }
