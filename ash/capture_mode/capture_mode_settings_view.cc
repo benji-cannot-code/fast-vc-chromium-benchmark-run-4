2b8d67f0d7a07d2721ed863f7d8c6dd35a3503af
Sammie Quon <sammiequon@chromium.org>
2021-05-12 18:41:49 +0000

    capture_mode: Fix spoken feedback for settings toggle button.
    Similar to CaptureModeSessionFocusCycler::HighlightableView::
    PseudoFocus, we need to manually alert as the widget which contains
    these views is not activatable.
    
    Fixed: 1197257
    Test: manual
    Change-Id: I6a6f6ea1d090deb3144d967eeb8ea0a2d1417798
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2888323
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882195}

--- ash/capture_mode/capture_mode_settings_view.cc
+++ ash/capture_mode/capture_mode_settings_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
 #include "base/bind.h"
+#include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/compositor/layer.h"
@@ -79,6 +80,11 @@ void CaptureModeSettingsView::OnMicrophoneChanged(bool microphone_enabled) {
   microphone_view_->toggle_button_view()->SetIsOn(microphone_enabled);
   microphone_view_->SetIcon(microphone_enabled ? kCaptureModeMicIcon
                                                : kCaptureModeMicOffIcon);
+
+  // This view's widget is not activatable, so `this` will not get true focus.
+  // For spoken feedback to say the correct thing, we need to manually notify.
+  microphone_view_->toggle_button_view()->NotifyAccessibilityEvent(
+      ax::mojom::Event::kCheckedStateChanged, true);
 }
 
 void CaptureModeSettingsView::OnMicrophoneToggled() {
