13c8459bba51568bd24b859f45d067e1d0d9392e
Og Astorga <ogastorga@chromium.org>
2021-05-12 21:01:50 +0000

    Fix use-after-free allocating bt allocating memory for strings
    Reassignation of base::StringPiece in a for loop to later usage is
    causing a use-after-free security issue. This CL fixes that by using a
    std::vector<std::string> instead of std::vector<base::StringPiece>.
    
    Bug: 1191778
    Change-Id: Ia0ffc25acbdf631d3645b88d6bdf219a8eabd18e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2892958
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Og Astorga <ogastorga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882274}

--- components/policy/core/browser/url_blocklist_policy_handler.cc
+++ components/policy/core/browser/url_blocklist_policy_handler.cc
@@ -50,7 +50,7 @@ bool URLBlocklistPolicyHandler::CheckPolicySettings(const PolicyMap& policies,
 
   bool type_error = false;
   std::string policy;
-  std::vector<base::StringPiece> invalid_policies;
+  std::vector<std::string> invalid_policies;
   for (const auto& policy_iter : url_blocklist->GetList()) {
     if (!policy_iter.is_string()) {
       type_error = true;
