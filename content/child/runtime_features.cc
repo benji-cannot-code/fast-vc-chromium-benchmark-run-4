0f54648bf9b5ecbc8ea073336a54a19656302d93
François Beaufort <beaufort.francois@gmail.com>
2021-05-12 09:33:52 +0000

    [Web Bluetooth] Ship manufacturer data filter on supported platforms
    This CL enables the new manufacturer data filter in Web Bluetooth on
    Android, Chrome OS, macOS, and Windows 10.
    
    Intent to ship: https://groups.google.com/a/chromium.org/g/blink-dev/c/5Id2LANtFko
    
    Bug: 707635
    Change-Id: Ib645fcaa4a9aab6b097a53c9affedb179f9960f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2876186
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881966}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -68,6 +68,7 @@ void SetRuntimeFeatureDefaultsForPlatform(
     WebRuntimeFeatures::EnableWebBluetooth(true);
     WebRuntimeFeatures::EnableWebBluetoothRemoteCharacteristicNewWriteValue(
         true);
+    WebRuntimeFeatures::EnableWebBluetoothManufacturerDataFilter(true);
   }
 #endif
 
