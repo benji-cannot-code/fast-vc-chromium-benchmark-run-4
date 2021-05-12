99085edbeffd0f6e8432649431d10ef152392efb
Akira Baruah <akirabaruah@google.com>
2021-05-12 20:12:24 +0000

    Remove HTTP err coercion for lower-layer net errs
    Logs the actual NET_ERROR value returned by the URLLoader, rather than
    assuming all non-SSL network errors are HTTP bad request (400) errors.
    
    Fixed: b/180941306
    Test: Build, deploy, disconnect network, and observe non-HTTP error logs.
    Test: cast_net_unittests
    Change-Id: Icb3d9372c4adb8010f0ab207e2a22b3521961b0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2744289
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Junbo Ke <juke@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Auto-Submit: Akira Baruah <akirabaruah@google.com>
    Cr-Commit-Position: refs/heads/master@{#882240}

--- chromecast/net/connectivity_checker_impl.h
+++ chromecast/net/connectivity_checker_impl.h
@@ -12,8 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "chromecast/base/metrics/cast_metrics_helper.h"
 #include "chromecast/net/connectivity_checker.h"
 #include "chromecast/net/time_sync_tracker.h"
+#include "net/http/http_status_code.h"
 #include "services/network/public/cpp/network_connection_tracker.h"
 
 class GURL;
@@ -33,6 +35,18 @@ class SimpleURLLoader;
 
 namespace chromecast {
 
+// Default (HTTPS) url for connectivity checking.
+constexpr char kDefaultConnectivityCheckUrl[] =
+    "https://connectivitycheck.gstatic.com/generate_204";
+
+// HTTP url for connectivity checking.
+constexpr char kHttpConnectivityCheckUrl[] =
+    "http://connectivitycheck.gstatic.com/generate_204";
+
+// The default URLs above are expected to respond with HTTP 204 (no content).
+constexpr net::HttpStatusCode kConnectivitySuccessStatusCode =
+    net::HTTP_NO_CONTENT;
+
 // Simple class to check network connectivity by sending a HEAD http request
 // to given url.
 class ConnectivityCheckerImpl
@@ -40,6 +54,18 @@ class ConnectivityCheckerImpl
       public network::NetworkConnectionTracker::NetworkConnectionObserver,
       public TimeSyncTracker::Observer {
  public:
+  // Types of errors that can occur when making a network URL request.
+  enum class ErrorType {
+    BAD_HTTP_STATUS = 1,
+    SSL_CERTIFICATE_ERROR = 2,
+    REQUEST_TIMEOUT = 3,
+
+    // A network error not captured by the ones above. See
+    // net/base/net_error_list.h for full set of errors that might fall under
+    // this category.
+    NET_ERROR = 4,
+  };
+
   // Connectivity checking and initialization will run on task_runner.
   static scoped_refptr<ConnectivityCheckerImpl> Create(
       scoped_refptr<base::SingleThreadTaskRunner> task_runner,
@@ -51,6 +77,9 @@ class ConnectivityCheckerImpl
   // ConnectivityChecker implementation:
   bool Connected() const override;
   void Check() override;
+
+  void SetCastMetricsHelperForTesting(
+      metrics::CastMetricsHelper* cast_metrics_helper);
 
  protected:
   explicit ConnectivityCheckerImpl(
@@ -82,12 +111,6 @@ class ConnectivityCheckerImpl
   // Sets connectivity and alerts observers if it has changed
   void SetConnected(bool connected);
 
-  enum class ErrorType {
-    BAD_HTTP_STATUS = 1,
-    SSL_CERTIFICATE_ERROR = 2,
-    REQUEST_TIMEOUT = 3,
-  };
-
   // Called when URL request failed.
   void OnUrlRequestError(ErrorType type);
 
@@ -102,6 +125,7 @@ class ConnectivityCheckerImpl
   const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   network::NetworkConnectionTracker* const network_connection_tracker_;
   TimeSyncTracker* const time_sync_tracker_;
+  metrics::CastMetricsHelper* cast_metrics_helper_;
 
   // connected_lock_ protects access to connected_ which is shared across
   // threads.
