206d90abb83bc174b152ba60de087534418755ca
Tsuyoshi Horo <horo@chromium.org>
2021-05-12 11:34:57 +0000

    Use the same SessionStorageNamespace for prerendering
    Currently there is an issue that the Session Storage is not carried over
    to the prerendering page. This is because a new Session Storage
    Namespace is used for the prerendering page.
    
    To fix this issue, this CL changes PrerenderHost::PageHolder to copy the
    Session Storage Namespace from the initiator page to the prerendering
    page.
    
    We don’t want the Session Storage state in the storage service be
    updated by the prerendering page. And we want to synchronize the Session
    Storage state of the prerendering page with the initiator page when the
    prerendering page is activated. So this CL introduces a flag
    |is_session_storage_for_prerendering_| in CachedStorageArea, and make
    CachedStorageArea not to send the changes of the Session Storage state
    to the storage service, and make StorageArea recreate |cached_area_|
    when the prerendering page is activated.
    
    This is the "clone & swap" mechanism for session storage in prerendering
    described in https://github.com/whatwg/storage/issues/119.
    
    This CL still has an issue that when the initial renderer process is
    reused after the back navigation from a prerendered page, the Session
    Storage state is not correctly propagated to the initial renderer
    process. This issue will be fixed in the next CL.
    https://crrev.com/c/2849654
    
    Bug: 1197383
    Change-Id: Ib43386ccf75f8c867bcddb4b77b333ee0d5b5d79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2850242
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881985}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -8080,6 +8080,12 @@ const Node* Document::GetFindInPageActiveMatchNode() const {
   return find_in_page_active_match_node_;
 }
 
+void Document::RegisterWillDispatchPrerenderchangeCallback(
+    base::OnceClosure closure) {
+  DCHECK(is_prerendering_);
+  will_dispatch_prerenderchange_callbacks_.push_back(std::move(closure));
+}
+
 void Document::ActivateForPrerendering() {
   DCHECK(RuntimeEnabledFeatures::Prerender2Enabled());
 
@@ -8097,6 +8103,12 @@ void Document::ActivateForPrerendering() {
 
   if (DocumentLoader* loader = Loader())
     loader->NotifyPrerenderingDocumentActivated();
+
+  Vector<base::OnceClosure> callbacks;
+  callbacks.swap(will_dispatch_prerenderchange_callbacks_);
+  for (auto& callback : callbacks) {
+    std::move(callback).Run();
+  }
 
   DispatchEvent(*Event::Create(event_type_names::kPrerenderingchange));
 
--- third_party/blink/renderer/modules/storage/storage_area.cc
+++ third_party/blink/renderer/modules/storage/storage_area.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/storage/storage_namespace.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
+#include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
@@ -74,6 +75,11 @@ StorageArea::StorageArea(LocalDOMWindow* window,
   DCHECK(window);
   DCHECK(cached_area_);
   cached_area_->RegisterSource(this);
+  if (cached_area_->is_session_storage_for_prerendering()) {
+    DomWindow()->document()->RegisterWillDispatchPrerenderchangeCallback(
+        WTF::Bind(&StorageArea::OnDocumentActivatedForPrerendering,
+                  WrapWeakPersistent(this)));
+  }
 }
 
 unsigned StorageArea::length(ExceptionState& exception_state) const {
@@ -245,6 +251,20 @@ blink::WebScopedVirtualTimePauser StorageArea::CreateWebScopedVirtualTimePauser(
       ->GetFrame()
       ->GetFrameScheduler()
       ->CreateWebScopedVirtualTimePauser(name, duration);
+}
+
+void StorageArea::OnDocumentActivatedForPrerendering() {
+  StorageNamespace* storage_namespace =
+      StorageNamespace::From(DomWindow()->GetFrame()->GetPage());
+  if (!storage_namespace)
+    return;
+
+  // Swap out the session storage state used within prerendering, and replace it
+  // with the normal session storage state. For more details:
+  // https://docs.google.com/document/d/1I5Hr8I20-C1GBr4tAXdm0U8a1RDUKHt4n7WcH4fxiSE/edit?usp=sharing
+  cached_area_ =
+      storage_namespace->GetCachedArea(DomWindow()->GetSecurityOrigin());
+  cached_area_->RegisterSource(this);
 }
 
 }  // namespace blink
