87f53be9f4ac686e08ed08cfa59170565bf5354c
sauski <sauski@google.com>
2021-05-12 13:54:02 +0000

    Privacy Sandbox Settings: Add basic layout for FLoC card
    Added a basic layout (not aligned with UX specs) for the new FLoC card
    on the Privacy Sandbox settings page. This includes new strings, some
    of which are placeholder, as well as the piping of the FLoC specific
    pref through to settings.
    
    User visible changes are behind the privacy-sandbox-settings-2 flag.
    
    Bug: 1206250
    Change-Id: Iae1c69496cb7ab1860c116a7ef6cd6b50cde7331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2887146
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882027}

--- chrome/browser/resources/settings/privacy_sandbox/app.js
+++ chrome/browser/resources/settings/privacy_sandbox/app.js
@@ -23,6 +23,14 @@ Polymer({
     PrefsBehavior,
   ],
 
+  properties: {
+    /** @private */
+    privacySandboxSettings2Enabled_: {
+      type: Boolean,
+      value: () => loadTimeData.getBoolean('privacySandboxSettings2Enabled'),
+    },
+  },
+
   /** @private {?MetricsBrowserProxy} */
   metricsBrowserProxy_: null,
 
