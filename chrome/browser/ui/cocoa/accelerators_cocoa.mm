79dc883d9dbae94e10c480099be72abf2fee60e7
Leonard Grey <lgrey@chromium.org>
2021-05-12 21:40:40 +0000

    Reland "Mac: add keyboard shortcut for Report an Issue"
    This is a reland of 7c344c0ad7ee54c60244f609a483e0158825312b
    
    Fix is in patchset 2
    
    Original change's description:
    > Mac: add keyboard shortcut for Report an Issue
    >
    > Bug: 1187777
    > Change-Id: I25baf77de9e746d4801d92f8da68dd568d39f9d0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2821636
    > Auto-Submit: Leonard Grey <lgrey@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Commit-Queue: Robert Sesek <rsesek@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#882086}
    
    Bug: 1187777
    Change-Id: I7952f23c281bf0f1957d5d45a5bba05300e529ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2892624
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Auto-Submit: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882302}

--- chrome/browser/ui/cocoa/accelerators_cocoa.mm
+++ chrome/browser/ui/cocoa/accelerators_cocoa.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/singleton.h"
 #include "base/stl_util.h"
+#include "build/branding_buildflags.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/commander/commander.h"
 #include "printing/buildflags/buildflags.h"
@@ -105,6 +106,10 @@ const struct AcceleratorMapping {
      ui::VKEY_OEM_2},
     {IDC_TOGGLE_FULLSCREEN_TOOLBAR, ui::EF_COMMAND_DOWN | ui::EF_SHIFT_DOWN,
      ui::VKEY_F},
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+    {IDC_FEEDBACK, ui::EF_COMMAND_DOWN | ui::EF_ALT_DOWN | ui::EF_SHIFT_DOWN,
+     ui::VKEY_I},
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 };
 
 }  // namespace
