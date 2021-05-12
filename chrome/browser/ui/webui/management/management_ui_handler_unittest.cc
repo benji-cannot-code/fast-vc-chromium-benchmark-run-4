e8f23b3440d1878d70a12d9dc16e319dc6261bd5
Michael Ershov <miersh@google.com>
2021-05-12 14:16:11 +0000

    [Code Health] ManagementUI: Replace deprecated Value::GetAsDictionary
    Bug: 1187011
    Test: *ManagementUIHandlerTests* unittest
    Change-Id: I69e1fc749c1e1bb8d78fa71e1ad7346d4d83dd25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2892195
    Commit-Queue: Michael Ershov <miersh@google.com>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Auto-Submit: Michael Ershov <miersh@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882044}

--- chrome/browser/ui/webui/management/management_ui_handler_unittest.cc
+++ chrome/browser/ui/webui/management/management_ui_handler_unittest.cc
@@ -1285,11 +1285,12 @@ TEST_F(ManagementUIHandlerTests, ThreatReportingInfo) {
   EXPECT_CALL(policy_service_, GetPolicies(chrome_policies_namespace))
       .WillRepeatedly(ReturnRef(chrome_policies));
 
-  base::DictionaryValue* threat_protection_info = nullptr;
+  const base::DictionaryValue* threat_protection_info = nullptr;
 
   // When no policies are set, nothing to report.
   auto info = handler_.GetThreatProtectionInfo(profile_no_domain.get());
-  info.GetAsDictionary(&threat_protection_info);
+  ASSERT_TRUE(info.is_dict());
+  threat_protection_info = &base::Value::AsDictionaryValue(info);
   EXPECT_TRUE(threat_protection_info->FindListKey("info")->GetList().empty());
   EXPECT_EQ(
       l10n_util::GetStringUTF16(IDS_MANAGEMENT_THREAT_PROTECTION_DESCRIPTION),
@@ -1308,7 +1309,8 @@ TEST_F(ManagementUIHandlerTests, ThreatReportingInfo) {
       prefs::kSafeBrowsingEnterpriseRealTimeUrlCheckMode, 0);
 
   info = handler_.GetThreatProtectionInfo(profile_no_domain.get());
-  info.GetAsDictionary(&threat_protection_info);
+  ASSERT_TRUE(info.is_dict());
+  threat_protection_info = &base::Value::AsDictionaryValue(info);
   EXPECT_TRUE(threat_protection_info->FindListKey("info")->GetList().empty());
   EXPECT_EQ(
       l10n_util::GetStringUTF16(IDS_MANAGEMENT_THREAT_PROTECTION_DESCRIPTION),
@@ -1335,7 +1337,8 @@ TEST_F(ManagementUIHandlerTests, ThreatReportingInfo) {
       policy::POLICY_SCOPE_MACHINE);
 
   info = handler_.GetThreatProtectionInfo(profile_no_domain.get());
-  info.GetAsDictionary(&threat_protection_info);
+  ASSERT_TRUE(info.is_dict());
+  threat_protection_info = &base::Value::AsDictionaryValue(info);
   EXPECT_TRUE(threat_protection_info->FindListKey("info")->GetList().empty());
   EXPECT_EQ(
       l10n_util::GetStringUTF16(IDS_MANAGEMENT_THREAT_PROTECTION_DESCRIPTION),
@@ -1347,7 +1350,8 @@ TEST_F(ManagementUIHandlerTests, ThreatReportingInfo) {
       policy::DMToken::CreateValidTokenForTesting("fake-token"));
 
   info = handler_.GetThreatProtectionInfo(profile_no_domain.get());
-  info.GetAsDictionary(&threat_protection_info);
+  ASSERT_TRUE(info.is_dict());
+  threat_protection_info = &base::Value::AsDictionaryValue(info);
   EXPECT_EQ(5u, threat_protection_info->FindListKey("info")->GetList().size());
   EXPECT_EQ(
       l10n_util::GetStringUTF16(IDS_MANAGEMENT_THREAT_PROTECTION_DESCRIPTION),
