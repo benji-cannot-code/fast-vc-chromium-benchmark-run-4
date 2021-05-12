d25dc0e5497ce21dc13b626b09c2a334dd3bada9
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2021-05-12 18:01:26 +0000

    Mark parts of TabGroupUiTest as flaky.
    Bug: 1208386
    Change-Id: I7c7b1eae5ec3a695b61672d46ea4e6a22ec3bf1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2891793
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Auto-Submit: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882157}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiTest.java
@@ -55,6 +55,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.compositor.layouts.Layout;
@@ -127,6 +128,7 @@ public class TabGroupUiTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
+    @FlakyTest(message = "https://crbug.com/1208386")
     public void testRenderStrip_Select5thTabIn10Tabs() throws IOException {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         AtomicReference<RecyclerView> recyclerViewReference = new AtomicReference<>();
@@ -151,6 +153,7 @@ public class TabGroupUiTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
+    @FlakyTest(message = "https://crbug.com/1208386")
     public void testRenderStrip_Select10thTabIn10Tabs() throws IOException {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         AtomicReference<RecyclerView> recyclerViewReference = new AtomicReference<>();
@@ -175,6 +178,7 @@ public class TabGroupUiTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
+    @FlakyTest(message = "https://crbug.com/1208386")
     public void testRenderStrip_AddTab() throws IOException {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         AtomicReference<RecyclerView> recyclerViewReference = new AtomicReference<>();
