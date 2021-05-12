5393597d5952b39ffe515a60b5577d293441d1ba
Jinsuk Kim <jinsukkim@chromium.org>
2021-05-12 21:44:54 +0000

    Reland "Simplify ActivityTabProvider implementation"
    Relanding after fixing the issue that caused revert at
    https://crrev.com/c/2887147
    
    This reverts commit 2c836523324f1cd01e5206dcd14a2685fb80a132.
    
    Bug: 1054072, 1207862
    Change-Id: I548d4c08ce906b403ae9e771971c64f274e9f910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2891182
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882306}

--- chrome/android/javatests/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerTest.java
@@ -52,7 +52,6 @@ import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
-import org.chromium.chrome.browser.ActivityTabProvider;
 import org.chromium.chrome.browser.accessibility_tab_switcher.OverviewListLayout;
 import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.flags.CachedFeatureFlags;
@@ -96,9 +95,6 @@ public class LayoutManagerTest implements MockTabModelDelegate {
 
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
-
-    @Mock
-    private ActivityTabProvider mTabSupplier;
 
     @Mock
     private TopUiThemeColorProvider mTopUiThemeColorProvider;
