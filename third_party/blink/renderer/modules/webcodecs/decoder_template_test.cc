f3b60bdddd8d6d1ca4445ca01f179f4377f36812
Dan Sanders <sandersd@chromium.org>
2021-05-12 23:56:49 +0000

    [webcodecs] Add WPT H.264 VideoDecoder tests.
    Bug: 1117643, 1201299
    Change-Id: I1e98643716de1192db34efe803e86f6ecedc6a18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879161
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882378}

--- third_party/blink/renderer/modules/webcodecs/decoder_template_test.cc
+++ third_party/blink/renderer/modules/webcodecs/decoder_template_test.cc
@@ -159,7 +159,7 @@ TYPED_TEST(DecoderTemplateTest, ResetDuringFlush) {
 
     ScriptPromiseTester tester(v8_scope.GetScriptState(), promise);
     tester.WaitUntilSettled();
-    ASSERT_TRUE(tester.IsFulfilled());
+    ASSERT_TRUE(tester.IsRejected());
   }
 }
 
