782e74187ee0bb5b1fcda92b6868f4392d0ab2cc
Kristi Park <kristipark@chromium.org>
2021-05-12 20:34:37 +0000

    [SharingHub] Add icons for sharing hub omnibox and save page action
    Screenshot: http://screen/8dnJLfokH7B6eWC.png
    Icon source:
    - http://icons/?selected=Google%20Material%20Icons:send (edited to
      16dip)
    - http://icons/?selected=Google%20Material%20Icons:save_alt
    
    Bug: 1186843
    Change-Id: I7dba1de1f2fbb1723bd1e65ab8051cf876972299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883323
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882256}

--- chrome/browser/ui/views/sharing_hub/sharing_hub_icon_view.cc
+++ chrome/browser/ui/views/sharing_hub/sharing_hub_icon_view.cc
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/sharing_hub/sharing_hub_icon_view.h"
 
 #include "chrome/app/chrome_command_ids.h"
-#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ui/browser_command_controller.h"
 #include "chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller.h"
 #include "chrome/browser/ui/views/sharing_hub/sharing_hub_bubble_view_impl.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/omnibox/browser/omnibox_view.h"
+#include "components/omnibox/browser/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 
@@ -58,8 +58,7 @@ void SharingHubIconView::OnExecuting(
     PageActionIconView::ExecuteSource execute_source) {}
 
 const gfx::VectorIcon& SharingHubIconView::GetVectorIcon() const {
-  // TODO(1186843): Add Share icon.
-  return kAddIcon;
+  return omnibox::kSendIcon;
 }
 
 bool SharingHubIconView::ShouldShowLabel() const {
