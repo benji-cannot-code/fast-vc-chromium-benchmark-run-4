34a2f6d780319adbffcc3a230df4cd7ea2056737
Olivier Li <olivierli@chromium.org>
2021-05-12 05:12:57 +0000

    Adapt /mojo/core to move-only thread options.
    This change will let PlatformThread::Delegate::Options::delegate be a unique_ptr.
    This CL was uploaded by git cl split.
    
    Bug:1208166
    
    R=rockot@google.com
    
    Change-Id: Ia3b1495f0218d740b1705513917b1ffe39552dfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2889836
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Auto-Submit: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881910}

--- mojo/core/channel_unittest.cc
+++ mojo/core/channel_unittest.cc
@@ -452,7 +452,7 @@ TEST(ChannelTest, PeerStressTest) {
   base::Thread::Options thread_options;
   thread_options.message_pump_type = base::MessagePumpType::IO;
   base::Thread peer_thread("peer_b_io");
-  peer_thread.StartWithOptions(thread_options);
+  peer_thread.StartWithOptions(std::move(thread_options));
 
   // Create two channels that run on separate threads.
   PlatformChannel platform_channel;
@@ -607,7 +607,7 @@ TEST(ChannelTest, SendToDeadMachPortName) {
   base::Thread::Options thread_options;
   thread_options.message_pump_type = base::MessagePumpType::IO;
   base::Thread peer_thread("channel_b_io");
-  peer_thread.StartWithOptions(thread_options);
+  peer_thread.StartWithOptions(std::move(thread_options));
 
   // Create a PlatformChannel send/receive right pair.
   PlatformChannel platform_channel;
