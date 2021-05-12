52a29ce39252716797a7e3ff1e30cce1e2dbbb53
Roman Arora <romanarora@chromium.org>
2021-05-12 23:00:46 +0000

    [CrOS Settings] Check in Polymer3 version of Guest OS
    Bug: 1180979
    Change-Id: I29628f6be897e3cbb2b8d3d58aa5c2e6311f71ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2889739
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882345}

--- chrome/browser/resources/settings/chromeos/guest_os/guest_os_shared_paths.js
+++ chrome/browser/resources/settings/chromeos/guest_os/guest_os_shared_paths.js
@@ -8,7 +8,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * 'guest-os-shared-paths' is the settings shared paths subpage for guest OSes.
  */
 
+import '//resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
+import '//resources/polymer/v3_0/iron-list/iron-list.js';
+import '../../settings_shared_css.js';
+
+import {assert} from '//resources/js/assert.m.js';
+import {I18nBehavior} from '//resources/js/i18n_behavior.m.js';
+import {html, Polymer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {recordSettingChange} from '../metrics_recorder.m.js';
+
+import {GuestOsBrowserProxy, GuestOsBrowserProxyImpl} from './guest_os_browser_proxy.js';
+
 Polymer({
+  _template: html`{__html_template__}`,
   is: 'settings-guest-os-shared-paths',
 
   behaviors: [I18nBehavior],
@@ -59,7 +72,7 @@ Polymer({
         vmPaths.push(path);
       }
     }
-    settings.GuestOsBrowserProxyImpl.getInstance()
+    GuestOsBrowserProxyImpl.getInstance()
         .getGuestOsSharedPathsDisplayText(vmPaths)
         .then(text => {
           this.sharedPaths_ = vmPaths.map(
@@ -73,14 +86,14 @@ Polymer({
    */
   removeSharedPath_(path) {
     this.sharedPathWhichFailedRemoval_ = null;
-    settings.GuestOsBrowserProxyImpl.getInstance()
+    GuestOsBrowserProxyImpl.getInstance()
         .removeGuestOsSharedPath(this.vmName_(), path)
         .then(success => {
           if (!success) {
             this.sharedPathWhichFailedRemoval_ = path;
           }
         });
-    settings.recordSettingChange();
+    recordSettingChange();
   },
 
   /**
