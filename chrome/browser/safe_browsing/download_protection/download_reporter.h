bbb708dc0662d6dccd9dffcfc49f42e4439b01cb
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-12 23:17:08 +0000

    Retire ScopedObserver in /chrome/browser/safe_browsing/download_protection.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=vakh@chromium.org
    
    Bug: 1145565
    Change-Id: I461e3da3d2d13f3819b9d8ff194ef5e3bb5ddb10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883892
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882362}

--- chrome/browser/safe_browsing/download_protection/download_reporter.h
+++ chrome/browser/safe_browsing/download_protection/download_reporter.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SAFE_BROWSING_DOWNLOAD_PROTECTION_DOWNLOAD_REPORTER_H_
 
 #include "base/macros.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_multi_source_observation.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager_observer.h"
 #include "chrome/browser/profiles/profile_observer.h"
@@ -46,11 +46,14 @@ class DownloadReporter
  private:
   base::flat_map<download::DownloadItem*, download::DownloadDangerType>
       danger_types_;
-  ScopedObserver<Profile, ProfileObserver> observed_profiles_{this};
-  ScopedObserver<download::SimpleDownloadManagerCoordinator,
-                 download::SimpleDownloadManagerCoordinator::Observer>
+  base::ScopedMultiSourceObservation<Profile, ProfileObserver>
+      observed_profiles_{this};
+  base::ScopedMultiSourceObservation<
+      download::SimpleDownloadManagerCoordinator,
+      download::SimpleDownloadManagerCoordinator::Observer>
       observed_coordinators_{this};
-  ScopedObserver<download::DownloadItem, download::DownloadItem::Observer>
+  base::ScopedMultiSourceObservation<download::DownloadItem,
+                                     download::DownloadItem::Observer>
       observed_downloads_{this};
 
   void AddBypassEventToPref(download::DownloadItem* download);
