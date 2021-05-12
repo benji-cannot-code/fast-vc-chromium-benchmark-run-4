a1ad25f18bf36a3aa20ffa33fe990a4fb621f1a3
Christian Dullweber <dullweber@chromium.org>
2021-05-12 20:37:31 +0000

    ClearOnExit: Continue unfinished deletions on startup
    Sometimes Chrome is shut down before deletions for session-only
    data are finished. This adds an additional cleanup step at startup
    if it is detected that the deletion at shutdown did not finish.
    This new behavior is behind a default-enabled Feature in case this is
    causing any issues.
    
    Bug: 637822, 1204022
    Change-Id: Iebbf41a8f7f0b194563964f02df6ab4e0a824e66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2867585
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882258}

--- chrome/browser/sessions/session_data_service_factory.cc
+++ chrome/browser/sessions/session_data_service_factory.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "chrome/browser/content_settings/cookie_settings_factory.h"
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/session_data_deleter.h"
 #include "chrome/browser/sessions/session_data_service.h"
@@ -25,7 +27,10 @@ SessionDataServiceFactory* SessionDataServiceFactory::GetInstance() {
 SessionDataServiceFactory::SessionDataServiceFactory()
     : BrowserContextKeyedServiceFactory(
           "SessionDataService",
-          BrowserContextDependencyManager::GetInstance()) {}
+          BrowserContextDependencyManager::GetInstance()) {
+  DependsOn(HostContentSettingsMapFactory::GetInstance());
+  DependsOn(CookieSettingsFactory::GetInstance());
+}
 
 SessionDataServiceFactory::~SessionDataServiceFactory() = default;
 
---
+++ chrome/browser/sessions/sessions_features.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SESSIONS_SESSIONS_FEATURES_H_
+#define CHROME_BROWSER_SESSIONS_SESSIONS_FEATURES_H_
+
+#include "base/feature_list.h"
+
+extern const base::Feature kDeleteSessionOnlyDataOnStartup;
+
+#endif  // CHROME_BROWSER_SESSIONS_SESSIONS_FEATURES_H_
