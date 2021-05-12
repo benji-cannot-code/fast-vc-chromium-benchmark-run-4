c73eb94123dc5032e3246ed64a7c81e2b728ae3f
John Abd-El-Malek <jam@chromium.org>
2021-05-12 16:52:12 +0000

    Fix threading bugs with ProcessHostOnUI and Wayland.
    GpuHostImpl::InitOzone() runs on the UI thread with that flag, so update
    the implementations of GpuPlatformSupportHost::OnGpuServiceLaunched().
    In particular WaylandBufferManagerConnector::OnGpuServiceLaunched can't
    post a task from the UI thread to the UI thread or else we get errors
    like:
    
    ERROR:wayland_buffer_manager_gpu.cc(100)] WaylandBufferManagerGpu is not
    initialized. Can't register a surface.
    
    in content_browsertests (e.g.
    see https://chromium-swarm.appspot.com/task?id=5376ce41d12c8510).
    
    Bug: 904556
    Change-Id: I8503ded571b7a199894d9b43a4ca2fb46d89e659
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886218
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882131}

--- ui/ozone/platform/drm/host/host_drm_device.h
+++ ui/ozone/platform/drm/host/host_drm_device.h
@@ -39,7 +39,7 @@ class HostDrmDevice : public base::RefCountedThreadSafe<HostDrmDevice>,
 
   void SetDisplayManager(DrmDisplayHostManager* display_manager);
 
-  void OnGpuServiceLaunchedOnIOThread(
+  void OnGpuServiceLaunchedOnProcessThread(
       mojo::PendingRemote<ui::ozone::mojom::DrmDevice> drm_device,
       scoped_refptr<base::SingleThreadTaskRunner> ui_runner);
 
