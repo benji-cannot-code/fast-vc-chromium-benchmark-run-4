79028a13a692e2372d6b33a41a45e658108ef374
Mikel Astiz <mastiz@chromium.org>
2021-05-12 13:50:49 +0000

    [sync] Add metrics to BookmarkModelMerger
    There are still a number of unexplainable metrics, which suggest
    many initial updates get ignored, without contributing to existing
    metrics that represent error cases. To understand this better, this
    patch adds a few additional metrics in BookmarkModelMerger.
    
    Change-Id: If3e9644b79600ef4853755dae038e133622f3242
    Bug: 1177798
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880287
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#882022}

--- components/sync_bookmarks/bookmark_model_merger_unittest.cc
+++ components/sync_bookmarks/bookmark_model_merger_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 using testing::_;
+using testing::ElementsAre;
 using testing::Eq;
 using testing::IsNull;
 using testing::Ne;
@@ -35,6 +36,10 @@ using testing::UnorderedElementsAre;
 namespace sync_bookmarks {
 
 namespace {
+
+MATCHER_P(HasTitle, title, "") {
+  return arg->GetTitle() == title;
+}
 
 // Copy of BookmarksGUIDDuplicates.
 enum class ExpectedBookmarksGUIDDuplicates {
@@ -55,7 +60,8 @@ enum class ExpectedRemoteBookmarkUpdateError {
   kMissingParentEntity = 4,
   kUnexpectedGuid = 9,
   kParentNotFolder = 10,
-  kMaxValue = kParentNotFolder,
+  kUnsupportedPermanentFolder = 13,
+  kMaxValue = kUnsupportedPermanentFolder,
 };
 
 // |*arg| must be of type std::vector<std::unique_ptr<bookmarks::BookmarkNode>>.
@@ -1758,15 +1764,38 @@ TEST(BookmarkModelMergerTest, ShouldLogMetricsForChildrenOfOrphanUpdates) {
 
   base::HistogramTester histogram_tester;
   Merge(std::move(updates), bookmark_model.get());
+  EXPECT_THAT(histogram_tester.GetAllSamples(
+                  "Sync.BookmarkModelMerger.ValidInputUpdates"),
+              ElementsAre(base::Bucket(/*min=*/2, /*count=*/1)));
   histogram_tester.ExpectUniqueSample(
       "Sync.ProblematicServerSideBookmarksDuringMerge",
       /*sample=*/ExpectedRemoteBookmarkUpdateError::kMissingParentEntity,
+      /*count=*/1);
+  EXPECT_THAT(histogram_tester.GetAllSamples(
+                  "Sync.BookmarkModelMerger.ReachableInputUpdates"),
+              ElementsAre(base::Bucket(/*min=*/1, /*count=*/1)));
+}
+
+TEST(BookmarkModelMergerTest, ShouldLogMetricsForUnsupportedServerTag) {
+  std::unique_ptr<bookmarks::BookmarkModel> bookmark_model =
+      bookmarks::TestBookmarkClient::CreateModel();
+
+  syncer::UpdateResponseDataList updates;
+  updates.push_back(CreateBookmarkBarNodeUpdateData());
+  updates.back().entity.server_defined_unique_tag = "someunknowntag";
+
+  base::HistogramTester histogram_tester;
+  Merge(std::move(updates), bookmark_model.get());
+  histogram_tester.ExpectUniqueSample(
+      "Sync.ProblematicServerSideBookmarksDuringMerge",
+      /*sample=*/ExpectedRemoteBookmarkUpdateError::kUnsupportedPermanentFolder,
       /*count=*/1);
 }
 
 TEST(BookmarkModelMergerTest, ShouldRemoveMatchingDuplicatesByGUID) {
   const std::string kTitle1 = "Title 1";
   const std::string kTitle2 = "Title 2";
+  const std::string kTitle3 = "Title 3";
   const std::string kUrl = "http://www.url.com/";
 
   const base::GUID kUrlGUID = base::GUID::GenerateRandomV4();
@@ -1778,6 +1807,7 @@ TEST(BookmarkModelMergerTest, ShouldRemoveMatchingDuplicatesByGUID) {
   // bookmark_bar
   //  |- url1(http://www.url.com, UrlGUID)
   //  |- url2(http://www.url.com, UrlGUID)
+  //  |- url3(http://www.url.com, <other-guid>)
   std::unique_ptr<bookmarks::BookmarkModel> bookmark_model =
       bookmarks::TestBookmarkClient::CreateModel();
 
@@ -1795,18 +1825,31 @@ TEST(BookmarkModelMergerTest, ShouldRemoveMatchingDuplicatesByGUID) {
       /*url=*/kUrl,
       /*is_folder=*/false, /*unique_position=*/MakeRandomPosition(), kUrlGUID));
   updates.back().entity.creation_time = base::Time::Now();
+  updates.push_back(CreateUpdateResponseData(
+      /*server_id=*/"Id3", /*parent_id=*/kBookmarkBarId, kTitle3,
+      /*url=*/kUrl,
+      /*is_folder=*/false, /*unique_position=*/MakeRandomPosition(),
+      base::GUID::GenerateRandomV4()));
+  updates.back().entity.creation_time = base::Time::Now();
 
   base::HistogramTester histogram_tester;
   std::unique_ptr<SyncedBookmarkTracker> tracker =
       Merge(std::move(updates), bookmark_model.get());
   const bookmarks::BookmarkNode* bookmark_bar_node =
       bookmark_model->bookmark_bar_node();
-  ASSERT_THAT(bookmark_bar_node->children().size(), Eq(1u));
+  EXPECT_THAT(bookmark_bar_node->children(),
+              UnorderedElementsAre(HasTitle(base::UTF8ToUTF16(kTitle2)),
+                                   HasTitle(base::UTF8ToUTF16(kTitle3))));
+
+  EXPECT_THAT(histogram_tester.GetAllSamples(
+                  "Sync.BookmarkModelMerger.ValidInputUpdates"),
+              ElementsAre(base::Bucket(/*min=*/4, /*count=*/1)));
   histogram_tester.ExpectBucketCount(
       "Sync.BookmarksGUIDDuplicates",
       /*sample=*/ExpectedBookmarksGUIDDuplicates::kMatchingUrls, /*count=*/1);
-  EXPECT_EQ(bookmark_bar_node->children().front()->GetTitle(),
-            base::UTF8ToUTF16(kTitle2));
+  EXPECT_THAT(histogram_tester.GetAllSamples(
+                  "Sync.BookmarkModelMerger.ReachableInputUpdates"),
+              ElementsAre(base::Bucket(/*min=*/3, /*count=*/1)));
 }
 
 TEST(BookmarkModelMergerTest, ShouldRemoveDifferentDuplicatesByGUID) {
@@ -1847,12 +1890,11 @@ TEST(BookmarkModelMergerTest, ShouldRemoveDifferentDuplicatesByGUID) {
       Merge(std::move(updates), bookmark_model.get());
   const bookmarks::BookmarkNode* bookmark_bar_node =
       bookmark_model->bookmark_bar_node();
-  ASSERT_THAT(bookmark_bar_node->children().size(), Eq(1u));
+  EXPECT_THAT(bookmark_bar_node->children(),
+              ElementsAre(HasTitle(base::UTF8ToUTF16(kTitle1))));
   histogram_tester.ExpectBucketCount(
       "Sync.BookmarksGUIDDuplicates",
       /*sample=*/ExpectedBookmarksGUIDDuplicates::kDifferentUrls, /*count=*/1);
-  EXPECT_EQ(bookmark_bar_node->children().front()->GetTitle(),
-            base::UTF8ToUTF16(kTitle1));
 }
 
 TEST(BookmarkModelMergerTest, ShouldRemoveMatchingFolderDuplicatesByGUID) {
