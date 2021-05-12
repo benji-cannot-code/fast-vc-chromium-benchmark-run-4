98202ca4d6b707597a4daff9534837196299b5a6
Lei Zhang <thestig@chromium.org>
2021-05-12 07:25:06 +0000

    Remove Print Preview code from PrintManager.
    PrintManager is the base class that implements the PrintManagerHost
    interface. Since Print Preview features are implemented by derived
    classes, remove knowledge of Print Preview from PrintManager. With this
    removal, HeadlessPrintManager now needs to implement the required Print
    Preview methods. Since it does not actually use any of them,
    HeadlessPrintManager can implement the method with calls to
    mojo::ReportBadMessage(). Similarly, TestPrintViewManager can just
    implement the methods with NOTREACHED().
    
    Change-Id: Ife931427af57b40d1b38fa7ef364c3707a0d2b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2888590
    Reviewed-by: Jeongeun Kim <je_julie.kim@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881941}

--- chrome/browser/printing/print_view_manager_unittest.cc
+++ chrome/browser/printing/print_view_manager_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/bind.h"
+#include "base/notreached.h"
 #include "base/run_loop.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -92,6 +93,20 @@ class TestPrintViewManager : public PrintViewManagerBase {
     print_job_->SetSource(PrintJob::Source::PRINT_PREVIEW, /*source_id=*/"");
 #endif  // defined(OS_CHROMEOS)
     return true;
+  }
+  void SetupScriptedPrintPreview(
+      SetupScriptedPrintPreviewCallback callback) override {
+    NOTREACHED();
+  }
+  void ShowScriptedPrintPreview(bool is_modifiable) override { NOTREACHED(); }
+  void RequestPrintPreview(
+      mojom::RequestPrintPreviewParamsPtr params) override {
+    NOTREACHED();
+  }
+  void CheckForCancel(int32_t preview_ui_id,
+                      int32_t request_id,
+                      CheckForCancelCallback callback) override {
+    NOTREACHED();
   }
 
  private:
