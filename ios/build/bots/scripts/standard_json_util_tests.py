81ee9db2c6d2ecffa819d3b6a98e4f4d1c83e084
Zhaoyang Li <zhaoyangli@chromium.org>
2021-05-12 20:11:56 +0000

    [iOS][test runner] Report all test logs as artifacts to result sink.
    As requested by result sink owners. The CL also refactored |post| method
    of |ResultSinkClient| for simplicity.
    
    Bug: 1205191
    Change-Id: I8621bb832b5471df9ec938a814884c30be98783a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880898
    Reviewed-by: Lindsay Pasricha <lindsayw@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882239}

--- ios/build/bots/scripts/standard_json_util_tests.py
+++ ios/build/bots/scripts/standard_json_util_tests.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Unittests for standard_json_util.py."""
 
 import collections
+import mock
 import os
 import unittest
 
@@ -114,6 +115,34 @@ class UnitTest(unittest.TestCase):
     output.mark_passed(test)
     self.assertEqual(output.tests['f']['shard'],
                      os.getenv('GTEST_SHARD_INDEX', 0))
+
+  @mock.patch('result_sink_util.ResultSinkClient')
+  def test_post_to_result_sink(self, mock_result_sink_class):
+    result_sink = mock_result_sink_class.return_value
+    passed_test = 'passed_test'
+    failed_test = 'failed_test'
+    skipped_test = 'skipped_test'
+    timeout_test = 'timeout_test'
+
+    output = sju.StdJson()
+    output.mark_passed('passed_test')
+    result_sink.post.assert_called_with('passed_test', 'PASS', True)
+    output.mark_failed('failed_test', 'logs')
+    result_sink.post.assert_called_with(
+        'failed_test', 'FAIL', False, test_log='logs')
+    output.mark_skipped('skipped_test')
+    result_sink.post.assert_called_with(
+        'skipped_test', 'SKIP', True, tags=[('disabled_test', 'true')])
+    output.mark_timeout('timeout_test')
+    timeout_log = (
+        'The test is compiled in test target but was unexpectedly not'
+        ' run or not finished.')
+    result_sink.post.assert_called_with(
+        'timeout_test',
+        'SKIP',
+        False,
+        test_log=timeout_log,
+        tags=[('disabled_test', 'false')])
 
 
 if __name__ == '__main__':
