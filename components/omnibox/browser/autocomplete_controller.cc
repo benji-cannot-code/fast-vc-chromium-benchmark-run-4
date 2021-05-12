d360c430cd834cc33a1ca2f56337b0362fdfdc4b
Tomasz Wiszkowski <ender@google.com>
2021-05-12 00:17:25 +0000

    Propagate additional AutocompleteMatch updates from C++ to Java
    This change propagates updates to Answer in Suggest and Description
    for AutocompleteMatch objects from C++ to Java.
    
    Bug: 1138587
    Change-Id: I174c44f63f3445e7381f06597ddbf3853c0e6619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879173
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#881797}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -812,6 +812,9 @@ void AutocompleteController::UpdateAssociatedKeywords(
       match->associated_keyword = std::make_unique<AutocompleteMatch>(
           keyword_provider_->CreateVerbatimMatch(exact_keyword, exact_keyword,
                                                  input_));
+#if defined(OS_ANDROID)
+      match->UpdateJavaAnswer();
+#endif
       continue;
     }
 
@@ -876,6 +879,10 @@ void AutocompleteController::UpdateKeywordDescriptions(
           i->description_class.push_back(
               ACMatchClassification(0, ACMatchClassification::DIM));
         }
+#if defined(OS_ANDROID)
+        i->UpdateJavaDescription();
+#endif
+
         last_keyword = i->keyword;
       }
     } else {
