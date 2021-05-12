7633bd802a517cded766ac9e433e6d9366dbbed2
Stephan Hartmann <stha09@googlemail.com>
2021-05-12 16:45:33 +0000

    IWYU: include atomic for std::atomic
    Bug: None
    Change-Id: Iff3e6f51f036bf0264d873add7c957dce16e3eb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880543
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#882125}

--- base/threading/platform_thread.cc
+++ base/threading/platform_thread.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/threading/platform_thread.h"
 
+#include <atomic>
 #include <memory>
 
 #include "base/feature_list.h"
