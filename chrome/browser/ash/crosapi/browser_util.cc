1d09a56a7e14022ce302a2f48c78e0ae74b04529
Erik Chen <erikchen@chromium.org>
2021-05-12 18:11:43 +0000

    lacros: Update startup infobar to include a learn more link.
    This CL also updates the warning message to be more nuanced, depending
    on the whether the user is on a managed account, a Google internal
    account, and whether Lacros is the primary browser.
    
    Change-Id: I061804d52fdd75ae8d1d16cf1613b7353492c4d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880844
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882175}

--- chrome/browser/ash/crosapi/browser_util.cc
+++ chrome/browser/ash/crosapi/browser_util.cc
@@ -539,6 +539,7 @@ mojom::BrowserInitParamsPtr GetBrowserInitParams(
 
   params->web_apps_enabled =
       base::FeatureList::IsEnabled(features::kWebAppsCrosapi);
+  params->standalone_browser_is_primary = IsLacrosPrimaryBrowser();
 
   return params;
 }
