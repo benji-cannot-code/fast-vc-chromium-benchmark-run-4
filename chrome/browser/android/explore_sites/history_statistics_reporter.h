55452f3a2017abdeea53fddae95eda54abe9a1f5
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-13 19:20:45 +0000

    Retire ScopedObserver in /chrome/browser/android/explore_sites.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=freedjm@chromium.org
    
    Bug: 1145565
    Change-Id: I4e4c03cca49c845b3d7fa914f2c16ee40d734939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883684
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882596}

--- chrome/browser/android/explore_sites/history_statistics_reporter.h
+++ chrome/browser/android/explore_sites/history_statistics_reporter.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/history_service_observer.h"
@@ -49,8 +49,9 @@ class HistoryStatisticsReporter : public history::HistoryServiceObserver {
   PrefService* prefs_;
 
   base::CancelableTaskTracker cancelable_task_tracker_;
-  ScopedObserver<history::HistoryService, history::HistoryServiceObserver>
-      history_service_observer_;
+  base::ScopedObservation<history::HistoryService,
+                          history::HistoryServiceObserver>
+      history_service_observation_{this};
   bool attempted_to_report_once_ = false;
   base::WeakPtrFactory<HistoryStatisticsReporter> weak_ptr_factory_{this};
 
