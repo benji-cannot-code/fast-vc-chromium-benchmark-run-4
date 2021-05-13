c0dfa0d2ccd1ec6513e383fc570b3e0cf10859a8
Richard Knoll <knollr@chromium.org>
2021-05-13 19:28:20 +0000

    Cleanup RemoteCopy feature flags
    - kRemoteCopyReceiver: Enabled by default
    - kRemoteCopyAllowedOrigins: Hardcoded the only used origin
    - kRemoteCopyImageNotification: Disabled and removed
    - kRemoteCopyPersistentNotification: Disabled and removed
    - kRemoteCopyProgressNotification: Disabled and removed
    
    Bug: 1165186
    Change-Id: I3234cebbeffb6319540148967426eeaf4f4cb46c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886891
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882610}

--- chrome/browser/sharing/shared_clipboard/feature_flags.cc
+++ chrome/browser/sharing/shared_clipboard/feature_flags.cc
@@ -5,27 +5,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/sharing/shared_clipboard/feature_flags.h"
 
-#include "base/metrics/field_trial_params.h"
-
 const base::Feature kSharedClipboardUI{"SharedClipboardUI",
                                        base::FEATURE_DISABLED_BY_DEFAULT};
-
-#if defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) || \
-    defined(OS_CHROMEOS)
-const base::Feature kRemoteCopyReceiver{"RemoteCopyReceiver",
-                                        base::FEATURE_ENABLED_BY_DEFAULT};
-
-const base::FeatureParam<std::string> kRemoteCopyAllowedOrigins = {
-    &kRemoteCopyReceiver, "RemoteCopyAllowedOrigins",
-    "https://googleusercontent.com"};
-
-const base::Feature kRemoteCopyImageNotification{
-    "RemoteCopyImageNotification", base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kRemoteCopyPersistentNotification{
-    "RemoteCopyPersistentNotification", base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kRemoteCopyProgressNotification{
-    "RemoteCopyProgressNotification", base::FEATURE_DISABLED_BY_DEFAULT};
-#endif  // defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) ||
-        // defined(OS_CHROMEOS)
--- chrome/browser/sharing/shared_clipboard/feature_flags.h
+++ chrome/browser/sharing/shared_clipboard/feature_flags.h
@@ -6,32 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SHARING_SHARED_CLIPBOARD_FEATURE_FLAGS_H_
 #define CHROME_BROWSER_SHARING_SHARED_CLIPBOARD_FEATURE_FLAGS_H_
 
-#include <string>
-
 #include "base/feature_list.h"
-#include "base/metrics/field_trial_params.h"
-#include "build/build_config.h"
 
 // Feature to allow shared clipboard gets processed.
 extern const base::Feature kSharedClipboardUI;
-
-#if defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) || \
-    defined(OS_CHROMEOS)
-// Feature to enable handling remote copy messages.
-extern const base::Feature kRemoteCopyReceiver;
-
-// List of allowed origins to fetch images from, comma separated.
-extern const base::FeatureParam<std::string> kRemoteCopyAllowedOrigins;
-
-// Feature to enable image notifications for remote copy messages.
-extern const base::Feature kRemoteCopyImageNotification;
-
-// Feature to enable persistent notifications for remote copy messages.
-extern const base::Feature kRemoteCopyPersistentNotification;
-
-// Feature to enable progress notifications for remote copy messages.
-extern const base::Feature kRemoteCopyProgressNotification;
-#endif  // defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) ||
-        // defined(OS_CHROMEOS)
 
 #endif  // CHROME_BROWSER_SHARING_SHARED_CLIPBOARD_FEATURE_FLAGS_H_
