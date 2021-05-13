69e190f3beede2b6a7d1bf864a04bc263aa0025f
yilkal <yilkal@chromium.org>
2021-05-13 19:53:01 +0000

    Removes projector checkbox from DesktopMediaPickerViews.
    The projector presenter tools checkbox is no longer part of
    the MVP product requirement. Therefore, cleaning up the entry
    point here.
    
    Bug: 1203444
    Change-Id: I65b6cbad3e8a7fa960e525be31ecd0376b3df099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2888309
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Toby Huang <tobyhuang@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Auto-Submit: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882654}

--- ash/projector/projector_controller_impl.cc
+++ ash/projector/projector_controller_impl.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/projector/projector_session.h"
 #include "ash/shell.h"
 #include "base/strings/utf_string_conversions.h"
-#include "ui/aura/window.h"
 
 namespace ash {
 
@@ -62,11 +61,6 @@ void ProjectorControllerImpl::SetProjectorToolsVisible(bool is_visible) {
     ui_controller_->ShowToolbar();
   else
     ui_controller_->CloseToolbar();
-}
-
-void ProjectorControllerImpl::StartProjectorSession(SourceType scope,
-                                                    aura::Window* window) {
-  // TODO(https://crbug.com/1185262): Start projector session.
 }
 
 bool ProjectorControllerImpl::IsEligible() const {
--- ash/public/cpp/projector/projector_controller.h
+++ ash/public/cpp/projector/projector_controller.h
@@ -11,13 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/ash_public_export.h"
 #include "base/time/time.h"
 
-namespace aura {
-class Window;
-}
-
 namespace ash {
 
-enum class SourceType;
 class ProjectorClient;
 
 // Interface to control projector in ash.
@@ -47,11 +42,6 @@ class ASH_PUBLIC_EXPORT ProjectorController {
 
   // Sets projector toolbar visibility.
   virtual void SetProjectorToolsVisible(bool is_visible) = 0;
-
-  // Starts a projector session. If scope is SourceType::kUnset, window may be
-  // null.
-  virtual void StartProjectorSession(SourceType scope,
-                                     aura::Window* window) = 0;
 
   // Returns true if Projector is eligible to start a new session.
   virtual bool IsEligible() const = 0;
