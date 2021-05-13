e52688a81e181f0e0c1cf56dfc34229e37c39f14
Og Astorga <ogastorga@chromium.org>
2021-05-13 16:16:22 +0000

    Device Policy Box in chrome://policy for iOS
    When you open chrome://policy in chrome, chromium,
    clank, and chromeOS. If and only if the device
    is enrolled. There is going to be a box that
    describes enrollment information like machine name,
    enrollment token, device Id, fetch interval, etcetera.
    If and only if the device is enrolled.
    
    This CL extends that behaviour into our iOS codebase.
    
    Bug: 1185893
    Change-Id: Ib309539992c332c7d8ea0b3eecf255efc723bca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2781135
    Commit-Queue: Og Astorga <ogastorga@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882537}

--- chrome/browser/ui/webui/policy/policy_ui_handler.h
+++ chrome/browser/ui/webui/policy/policy_ui_handler.h
@@ -32,12 +32,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_registry_observer.h"
 #endif
 
-class PolicyStatusProvider;
 class PrefChangeRegistrar;
 struct GoogleUpdatePoliciesAndState;
 
 namespace policy {
 class PolicyMap;
+class PolicyStatusProvider;
 }
 
 // The JavaScript message handler for the chrome://policy page.
@@ -130,10 +130,10 @@ class PolicyUIHandler : public content::WebUIMessageHandler,
   // Providers that supply status dictionaries for user and device policy,
   // respectively. These are created on initialization time as appropriate for
   // the platform (Chrome OS / desktop) and type of policy that is in effect.
-  std::unique_ptr<PolicyStatusProvider> user_status_provider_;
-  std::unique_ptr<PolicyStatusProvider> device_status_provider_;
-  std::unique_ptr<PolicyStatusProvider> machine_status_provider_;
-  std::unique_ptr<PolicyStatusProvider> updater_status_provider_;
+  std::unique_ptr<policy::PolicyStatusProvider> user_status_provider_;
+  std::unique_ptr<policy::PolicyStatusProvider> device_status_provider_;
+  std::unique_ptr<policy::PolicyStatusProvider> machine_status_provider_;
+  std::unique_ptr<policy::PolicyStatusProvider> updater_status_provider_;
 
 #if defined(OS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
   std::unique_ptr<policy::PolicyMap> updater_policies_;
--- ios/chrome/browser/ui/webui/policy/policy_ui_handler.h
+++ ios/chrome/browser/ui/webui/policy/policy_ui_handler.h
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_WEBUI_POLICY_POLICY_UI_HANDLER_H_
 #define IOS_CHROME_BROWSER_UI_WEBUI_POLICY_POLICY_UI_HANDLER_H_
 
+#include <memory>
+
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
+#include "components/policy/core/browser/webui/policy_status_provider.h"
 #include "components/policy/core/common/policy_service.h"
 #include "components/policy/core/common/schema_registry.h"
 #include "ios/web/public/webui/web_ui_ios.h"
@@ -62,11 +65,16 @@ class PolicyUIHandler : public web::WebUIIOSMessageHandler,
   // metadata are sent.
   void SendPolicies();
 
+  void SendStatus();
+
   // The callback invoked by PolicyService::RefreshPolicies().
   void OnRefreshPoliciesDone();
 
   // Returns the PolicyService associated with this WebUI's BrowserState.
   policy::PolicyService* GetPolicyService() const;
+
+  // Provider that supply status dictionary for machine policy,
+  std::unique_ptr<policy::PolicyStatusProvider> machine_status_provider_;
 
   // Vends WeakPtrs for this object.
   base::WeakPtrFactory<PolicyUIHandler> weak_factory_{this};
