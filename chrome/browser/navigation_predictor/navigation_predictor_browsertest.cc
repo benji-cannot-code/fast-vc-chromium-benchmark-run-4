b5b6d18a3475e75e2f6a222fc50cb7ad5535e342
Adithya Srinivasan <adithyas@chromium.org>
2021-05-13 16:14:07 +0000

    Sheriff: Disable PageWithIframe
    Bug: 1208143
    Change-Id: Ie59cd06b3c50c4579dc39453ecf45cba862ff5d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2894505
    Owners-Override: Adithya Srinivasan <adithyas@chromium.org>
    Auto-Submit: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882533}

--- chrome/browser/navigation_predictor/navigation_predictor_browsertest.cc
+++ chrome/browser/navigation_predictor/navigation_predictor_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/navigation_predictor/navigation_predictor.h"
 #include "chrome/browser/navigation_predictor/navigation_predictor_keyed_service.h"
@@ -296,7 +297,13 @@ IN_PROC_BROWSER_TEST_F(NavigationPredictorBrowserTest, MultipleNavigations) {
 }
 
 // Tests that anchors from iframes are reported.
-IN_PROC_BROWSER_TEST_F(NavigationPredictorBrowserTest, PageWithIframe) {
+// TODO(crbug.com/1208143): Test is flaky.
+#if defined(OS_LINUX) || defined(OS_WIN) || defined(OS_CHROMEOS)
+#define MAYBE_PageWithIframe DISABLED_PageWithIframe
+#else
+#define MAYBE_PageWithIframe PageWithIframe
+#endif
+IN_PROC_BROWSER_TEST_F(NavigationPredictorBrowserTest, MAYBE_PageWithIframe) {
   auto test_ukm_recorder = std::make_unique<ukm::TestAutoSetUkmRecorder>();
   ResetUKM();
 
