5d4917017419f3c71905e74a5003c74734ed328d
Tetsui Ohkubo <tetsui@chromium.org>
2021-05-13 01:17:09 +0000

    imageWriterPrivate: Use magic for identifying format
    When using the "Use local image" feature in Chrome OS Recovery Utility,
    there is no guarantee that the user will pass a compressed image file
    with a correct extension. (This is particularly problematic for .tar.xz
    as its mime type "application/x-xz" is non-standard and is not supported
    by Chrome right now). Given that, it is more reliable to use a magic
    number in the header to identify the file format.
    
    Design doc: http://go/tar-xz-cros-recovery
    
    TEST=ExtractorTest.Is*File
    BUG=b:153027505
    
    Change-Id: I2ac93d069422fc3e19abe51c3bb36dd4fa886140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886722
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882398}

--- chrome/browser/extensions/api/image_writer_private/extractor_unittest.cc
+++ chrome/browser/extensions/api/image_writer_private/extractor_unittest.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/image_writer_private/extraction_properties.h"
 #include "chrome/browser/extensions/api/image_writer_private/tar_extractor.h"
 #include "chrome/browser/extensions/api/image_writer_private/test_utils.h"
+#include "chrome/browser/extensions/api/image_writer_private/xz_extractor.h"
+#include "chrome/browser/extensions/api/image_writer_private/zip_extractor.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace extensions {
@@ -104,6 +106,62 @@ TEST_F(ExtractorTest, ExtractNonExistentTar) {
       .WillOnce(base::test::RunClosure(run_loop.QuitClosure()));
   TarExtractor::Extract(std::move(properties_));
   run_loop.Run();
+}
+
+TEST_F(ExtractorTest, IsZipFile) {
+  base::FilePath test_data_dir;
+  ASSERT_TRUE(GetTestDataDirectory(&test_data_dir));
+
+  EXPECT_TRUE(ZipExtractor::IsZipFile(test_data_dir.AppendASCII("test.zip")));
+  EXPECT_FALSE(ZipExtractor::IsZipFile(test_data_dir.AppendASCII("test.tar")));
+  EXPECT_FALSE(
+      ZipExtractor::IsZipFile(test_data_dir.AppendASCII("test.tar.xz")));
+
+  // Tests files with incorrect extensions.
+  const base::FilePath temp_dir = temp_dir_.GetPath();
+  EXPECT_TRUE(base::CopyFile(test_data_dir.AppendASCII("test.zip"),
+                             temp_dir.AppendASCII("test.tar")));
+  EXPECT_TRUE(base::CopyFile(test_data_dir.AppendASCII("test.tar"),
+                             temp_dir.AppendASCII("test.zip")));
+  EXPECT_FALSE(ZipExtractor::IsZipFile(temp_dir.AppendASCII("test.zip")));
+  EXPECT_TRUE(ZipExtractor::IsZipFile(temp_dir.AppendASCII("test.tar")));
+}
+
+TEST_F(ExtractorTest, IsTarFile) {
+  base::FilePath test_data_dir;
+  ASSERT_TRUE(GetTestDataDirectory(&test_data_dir));
+
+  EXPECT_TRUE(TarExtractor::IsTarFile(test_data_dir.AppendASCII("test.tar")));
+  EXPECT_FALSE(
+      TarExtractor::IsTarFile(test_data_dir.AppendASCII("test.tar.xz")));
+  EXPECT_FALSE(TarExtractor::IsTarFile(test_data_dir.AppendASCII("test.zip")));
+
+  // Tests files with incorrect extensions.
+  const base::FilePath temp_dir = temp_dir_.GetPath();
+  EXPECT_TRUE(base::CopyFile(test_data_dir.AppendASCII("test.tar"),
+                             temp_dir.AppendASCII("test.tar.xz")));
+  EXPECT_TRUE(base::CopyFile(test_data_dir.AppendASCII("test.tar.xz"),
+                             temp_dir.AppendASCII("test.tar")));
+  EXPECT_FALSE(TarExtractor::IsTarFile(temp_dir.AppendASCII("test.tar")));
+  EXPECT_TRUE(TarExtractor::IsTarFile(temp_dir.AppendASCII("test.tar.xz")));
+}
+
+TEST_F(ExtractorTest, IsXzFile) {
+  base::FilePath test_data_dir;
+  ASSERT_TRUE(GetTestDataDirectory(&test_data_dir));
+
+  EXPECT_TRUE(XzExtractor::IsXzFile(test_data_dir.AppendASCII("test.tar.xz")));
+  EXPECT_FALSE(XzExtractor::IsXzFile(test_data_dir.AppendASCII("test.tar")));
+  EXPECT_FALSE(XzExtractor::IsXzFile(test_data_dir.AppendASCII("test.zip")));
+
+  // Tests files with incorrect extensions.
+  const base::FilePath temp_dir = temp_dir_.GetPath();
+  EXPECT_TRUE(base::CopyFile(test_data_dir.AppendASCII("test.tar.xz"),
+                             temp_dir.AppendASCII("test.tar")));
+  EXPECT_TRUE(base::CopyFile(test_data_dir.AppendASCII("test.tar"),
+                             temp_dir.AppendASCII("test.tar.xz")));
+  EXPECT_FALSE(XzExtractor::IsXzFile(temp_dir.AppendASCII("test.tar.xz")));
+  EXPECT_TRUE(XzExtractor::IsXzFile(temp_dir.AppendASCII("test.tar")));
 }
 
 // TODO(tetsui): Add a test of passing a non-tar file to TarExtractor.
