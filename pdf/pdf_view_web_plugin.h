ac9c23c0ff05f76c7e62306f18ba5931d8e099ec
Hui Yingst <nigi@chromium.org>
2021-05-13 01:15:58 +0000

    [unseasoned-pdf] Implement PdfViewWebPlugin::ExecuteEditCommand().
    Implement PdfViewWebPlugin::ExecuteEditCommand() which is the
    Pepper-free equivalents for PepperWebPluginImpl::ExecuteEditCommand().
    
    Bug: 1204729
    Change-Id: Idad14e734efab16797187bc746e820c1d15813f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874874
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882395}

--- pdf/pdf_view_web_plugin.h
+++ pdf/pdf_view_web_plugin.h
@@ -110,6 +110,8 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
   bool HasEditableText() const override;
   bool CanUndo() const override;
   bool CanRedo() const override;
+  bool ExecuteEditCommand(const blink::WebString& name,
+                          const blink::WebString& value) override;
   blink::WebTextInputType GetPluginTextInputType() override;
 
   // PdfViewPluginBase:
@@ -198,6 +200,13 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
   // Invalidates the entire web plugin container and schedules a paint of the
   // page in it.
   void InvalidatePluginContainer();
+
+  // Text editing methods.
+  bool SelectAll();
+  bool Cut();
+  bool Paste(const blink::WebString& value);
+  bool Undo();
+  bool Redo();
 
   blink::WebString selected_text_;
 
