7fea4ef4688cf6000b863c89663022c82e36caf0
Sorin Jianu <sorin@chromium.org>
2021-05-14 23:20:12 +0000

    Refactor tag parsing in the updater.
    This changes moves the tag parsing code from
    AppInstall to App, so that the data from the tag can
    be used as input to determine the updater scope.
    
    The updater scope is typically obtained by calling the function
    App::updater_scope(), which is part of the public interface of
    App.
    
    Bug: 1209316
    Change-Id: If7b3a253598b8adbc1310be4115c254b6b7e33eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2898156
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883200}

--- chrome/updater/app/app_install.cc
+++ chrome/updater/app/app_install.cc
@@ -10,9 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/check.h"
+#include "base/check_op.h"
 #include "base/command_line.h"
 #include "base/i18n/icu_util.h"
 #include "base/logging.h"
+#include "base/optional.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/threading/sequenced_task_runner_handle.h"
@@ -143,27 +145,18 @@ void AppInstall::WakeCandidate() {
 }
 
 void AppInstall::MaybeInstallApp() {
-  const std::string app_id = []() {
-    // Returns the app id parsed from the tag, if the --tag is specified, or
-    // the switch value of the --app-id command line argument.
-    // Otherwise, returns an empty string.
-    base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-    const std::string tag = command_line->GetSwitchValueASCII(kTagSwitch);
-    if (!tag.empty()) {
-      tagging::TagArgs tag_args;
-      tagging::ErrorCode error = tagging::Parse(tag, base::nullopt, &tag_args);
-      if (error == tagging::ErrorCode::kSuccess) {
-        // TODO(crbug.com/1128631): support bundles. For now, assume one app.
-        DCHECK_EQ(tag_args.apps.size(), size_t{1});
-        const std::string& app_id = tag_args.apps.front().app_id;
-        if (!app_id.empty()) {
-          return app_id;
-        }
-      } else {
-        VLOG(1) << "Tag parsing returned " << error << ".";
+  const std::string app_id = [this]() {
+    base::Optional<tagging::TagArgs> tag_args = this->tag_args();
+    if (tag_args && !tag_args->apps.empty()) {
+      // TODO(crbug.com/1128631): support bundles. For now, assume one app.
+      DCHECK_EQ(tag_args->apps.size(), size_t{1});
+      const std::string& app_id = tag_args->apps.front().app_id;
+      if (!app_id.empty()) {
+        return app_id;
       }
     }
-    return command_line->GetSwitchValueASCII(kAppIdSwitch);
+    return base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+        kAppIdSwitch);
   }();
 
   if (app_id.empty()) {
