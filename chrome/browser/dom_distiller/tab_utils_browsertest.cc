e35977b71d3c67288b1f8e821fbb7afc0a4056b9
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-14 02:01:16 +0000

    Retire ScopedObserver in /chrome/browser/dom_distiller.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=wychen@chromium.org
    
    Bug: 1145565
    Change-Id: I6f2446f263c597c1b7de0f9c25815ea3666424c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883697
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882813}

--- chrome/browser/dom_distiller/tab_utils_browsertest.cc
+++ chrome/browser/dom_distiller/tab_utils_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/run_loop.h"
+#include "base/scoped_observation.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -87,7 +88,7 @@ std::unique_ptr<content::WebContents> NewContentsWithSameParamsAs(
 class FaviconUpdateWaiter : public favicon::FaviconDriverObserver {
  public:
   explicit FaviconUpdateWaiter(content::WebContents* web_contents) {
-    scoped_observer_.Add(
+    scoped_observation_.Observe(
         favicon::ContentFaviconDriver::FromWebContents(web_contents));
   }
   ~FaviconUpdateWaiter() override = default;
@@ -101,7 +102,7 @@ class FaviconUpdateWaiter : public favicon::FaviconDriverObserver {
     run_loop.Run();
   }
 
-  void StopObserving() { scoped_observer_.RemoveAll(); }
+  void StopObserving() { scoped_observation_.Reset(); }
 
  private:
   void OnFaviconUpdated(favicon::FaviconDriver* favicon_driver,
@@ -115,8 +116,9 @@ class FaviconUpdateWaiter : public favicon::FaviconDriverObserver {
   }
 
   bool updated_ = false;
-  ScopedObserver<favicon::FaviconDriver, favicon::FaviconDriverObserver>
-      scoped_observer_{this};
+  base::ScopedObservation<favicon::FaviconDriver,
+                          favicon::FaviconDriverObserver>
+      scoped_observation_{this};
   base::OnceClosure quit_closure_;
 
   DISALLOW_COPY_AND_ASSIGN(FaviconUpdateWaiter);
