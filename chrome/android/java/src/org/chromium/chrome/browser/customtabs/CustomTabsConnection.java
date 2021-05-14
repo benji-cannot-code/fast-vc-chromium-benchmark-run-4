a4cb03ea303b350fc338d8bb0af6d983916a3f57
Scott Violet <sky@chromium.org>
2021-05-14 16:35:04 +0000

    cct: calls setClientDataHeaderForNewTab for hidden tabs
    This adds a call to setClientDataHeaderForNewTab() for tabs created
    when mayLaunchUrl is called. Without this, the right header is not
    added if mayLaunchUrl() was called.
    
    This also adds AppHooksImpl to the list of files that has multiple
    paths, and changes the presubmit checking to prefer the public repo
    in the case of multiple paths matching. This ensures that includes to
    AppHooksImpl don't need to add DEPS to clank.
    
    
    BUG=1207675
    TEST=testMayLaunchUrlAddsClientDataHeader
    
    Change-Id: I2ebbcb1b54ffae50203e0843f71eea5eb40b9fc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2891644
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883000}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabsConnection.java
@@ -515,6 +515,11 @@ public class CustomTabsConnection {
         return true;
     }
 
+    @VisibleForTesting
+    public Tab getHiddenTab() {
+        return mHiddenTabHolder != null ? mHiddenTabHolder.getHiddenTab() : null;
+    }
+
     private boolean preconnectUrls(List<Bundle> likelyBundles) {
         boolean atLeastOneUrl = false;
         if (likelyBundles == null) return false;
@@ -1495,7 +1500,10 @@ public class CustomTabsConnection {
     private void launchUrlInHiddenTab(
             CustomTabsSessionToken session, String url, @Nullable Bundle extras) {
         ThreadUtils.assertOnUiThread();
-        mHiddenTabHolder.launchUrlInHiddenTab(session, mClientManager, url, extras);
+        mHiddenTabHolder.launchUrlInHiddenTab(
+                (Tab tab)
+                        -> setClientDataHeaderForNewTab(session, tab.getWebContents()),
+                session, mClientManager, url, extras);
     }
 
     @VisibleForTesting
