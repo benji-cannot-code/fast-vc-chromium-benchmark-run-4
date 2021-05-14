4ac3856fb3759cc7f5a66c275682e189a9bf7c3a
Robert Liao <robliao@chromium.org>
2021-05-14 21:08:42 +0000

    Fix Windows Display File Names for the WebUI Downloads Bar
    Mojo File Paths may marshal their strings as either a string or a
    utf16 array. This is generally inappropriate for JS as it forces the
    need to decode an array into a string.
    
    The solution here is to convert on the C++ side where the encoding
    is known.
    
    BUG=1182529
    
    Change-Id: I3ded07ebd701b6cb59c465d5bbe2e3acfe8bf495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2895451
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883124}

--- chrome/test/data/webui/download_shelf/download_list_test.js
+++ chrome/test/data/webui/download_shelf/download_list_test.js
@@ -31,7 +31,7 @@ suite('DownloadListTest', function() {
       isDangerous: false,
       isMalicious: false,
       id,
-      fileNameToReportUser: {path: 'test.exe'},
+      fileNameDisplayString: 'test.exe',
       isPaused,
       mixedContentStatus: MixedContentStatus.kSafe,
       mode: DownloadMode.kNormal,
