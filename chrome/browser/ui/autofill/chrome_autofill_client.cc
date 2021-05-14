7752d5f181cbbd8862f0261ee6859f7c75328258
Siyu An <siyua@chromium.org>
2021-05-14 00:33:53 +0000

    [VCN] Add triggering logic for manual fallback bubble
    Show the bubble when the virtual card is fetched.
    
    Bug: 1196021
    Change-Id: Iad986b36ec0ad432084fbb133886e5ac42726de6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2885099
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882773}

--- chrome/browser/ui/autofill/chrome_autofill_client.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client.cc
@@ -98,6 +98,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl.h"
 #include "chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.h"
 #include "chrome/browser/ui/autofill/payments/save_upi_bubble_controller_impl.h"
+#include "chrome/browser/ui/autofill/payments/virtual_card_manual_fallback_bubble_controller_impl.h"
 #include "chrome/browser/ui/autofill/payments/virtual_card_selection_dialog_controller_impl.h"
 #include "chrome/browser/ui/autofill/payments/webauthn_dialog_controller_impl.h"
 #include "chrome/browser/ui/autofill/payments/webauthn_dialog_state.h"
@@ -681,6 +682,19 @@ void ChromeAutofillClient::ShowOfferNotificationIfApplicable(
   OfferNotificationBubbleControllerImpl* controller =
       OfferNotificationBubbleControllerImpl::FromWebContents(web_contents());
   controller->ShowOfferNotificationIfApplicable(offer, card);
+#endif
+}
+
+void ChromeAutofillClient::ShowVirtualCardManualFallbackBubble(
+    const CreditCard* credit_card,
+    const std::u16string& cvc) {
+#if !defined(OS_ANDROID)  // Desktop only
+  VirtualCardManualFallbackBubbleControllerImpl::CreateForWebContents(
+      web_contents());
+  VirtualCardManualFallbackBubbleControllerImpl* controller =
+      VirtualCardManualFallbackBubbleControllerImpl::FromWebContents(
+          web_contents());
+  controller->ShowBubble(credit_card, cvc);
 #endif
 }
 
