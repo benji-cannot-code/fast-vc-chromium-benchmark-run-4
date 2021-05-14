a26c1b97459a291cc8a9b8c51a675df18f62737b
Daniel Cheng <dcheng@chromium.org>
2021-05-14 08:09:17 +0000

    [cleanup] Reuse on-stack WeakPtr<AutofillPopupControllerImpl>
    There's no need to create a new WeakPtr for `this` to pass to
    `base::Bind()` since there is already one on the tsack.
    
    Change-Id: I385e8d6ac852231fea0d997e9b839abddd724330
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2893600
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882906}

--- chrome/browser/ui/autofill/autofill_popup_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_popup_controller_impl.cc
@@ -140,7 +140,7 @@ void AutofillPopupControllerImpl::Show(
              const content::NativeWebKeyboardEvent& event) {
             return weak_this && weak_this->HandleKeyPressEvent(event);
           },
-          GetWeakPtr()));
+          weak_this));
 
   delegate_->OnPopupShown();
 }
