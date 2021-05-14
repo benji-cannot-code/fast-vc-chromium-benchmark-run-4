31704b0d8b3c5d73ec450cffb407bb1ed552b5e6
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-14 02:56:47 +0000

    Retire ScopedObserver in /chrome/browser/safe_browsing/incident_reporting.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=caitkp@chromium.org
    
    Bug: 1145565
    Change-Id: I9026d61d8623c4b2a8232a58e91e810fef50ca5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883900
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882838}

--- chrome/browser/safe_browsing/incident_reporting/last_download_finder.h
+++ chrome/browser/safe_browsing/incident_reporting/last_download_finder.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_multi_source_observation.h"
 #include "chrome/browser/profiles/profile_manager_observer.h"
 #include "chrome/browser/profiles/profile_observer.h"
 #include "chrome/browser/safe_browsing/incident_reporting/download_metadata_manager.h"
@@ -135,8 +135,9 @@ class LastDownloadFinder : public ProfileManagerObserver,
   history::DownloadRow most_recent_non_binary_row_;
 
   // HistoryServiceObserver
-  ScopedObserver<history::HistoryService, history::HistoryServiceObserver>
-      history_service_observer_{this};
+  base::ScopedMultiSourceObservation<history::HistoryService,
+                                     history::HistoryServiceObserver>
+      history_service_observations_{this};
 
   // A factory for asynchronous operations on profiles' HistoryService.
   base::WeakPtrFactory<LastDownloadFinder> weak_ptr_factory_{this};
