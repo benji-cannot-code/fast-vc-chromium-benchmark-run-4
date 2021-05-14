93acdfcbc1112b228a675b32146b8361a68808b0
sauski <sauski@google.com>
2021-05-14 13:52:23 +0000

    Privacy Sandbox Settings: Add Handler + Proxy
    Add Handler + Proxy pair for the Privacy Sandbox JS code to call to the
    PrivacySandboxSettings service to retrieve the FLoC ID display string,
    as well as reset the FLoC ID.
    
    Bug: 1206250
    Change-Id: I3c004b4b34a8f559dabcbd39292acdb6ca79085e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883802
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#882959}

--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/metrics_reporting_handler.h"
 #include "chrome/browser/ui/webui/settings/on_startup_handler.h"
 #include "chrome/browser/ui/webui/settings/people_handler.h"
+#include "chrome/browser/ui/webui/settings/privacy_sandbox_handler.h"
 #include "chrome/browser/ui/webui/settings/profile_info_handler.h"
 #include "chrome/browser/ui/webui/settings/protocol_handlers_handler.h"
 #include "chrome/browser/ui/webui/settings/reset_settings_handler.h"
@@ -219,6 +220,10 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
   AddSettingsPageUIHandler(std::make_unique<PeopleHandler>(profile));
   AddSettingsPageUIHandler(std::make_unique<ProfileInfoHandler>(profile));
   AddSettingsPageUIHandler(std::make_unique<ProtocolHandlersHandler>());
+  if (PrivacySandboxSettingsFactory::GetForProfile(profile)
+          ->PrivacySandboxSettingsFunctional()) {
+    AddSettingsPageUIHandler(std::make_unique<PrivacySandboxHandler>());
+  }
   AddSettingsPageUIHandler(std::make_unique<SearchEnginesHandler>(profile));
   AddSettingsPageUIHandler(std::make_unique<SecureDnsHandler>());
   AddSettingsPageUIHandler(std::make_unique<SiteSettingsHandler>(
--- chrome/test/data/webui/settings/privacy_sandbox_test.js
+++ chrome/test/data/webui/settings/privacy_sandbox_test.js
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://settings/privacy_sandbox/app.js';
 
+import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PrivacySandboxBrowserProxy, PrivacySandboxBrowserProxyImpl} from 'chrome://settings/privacy_sandbox/privacy_sandbox_browser_proxy.js';
 import {CrSettingsPrefs, HatsBrowserProxyImpl, loadTimeData, MetricsBrowserProxyImpl, OpenWindowProxyImpl} from 'chrome://settings/settings.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
+import {TestBrowserProxy} from '../test_browser_proxy.m.js';
 import {flushTasks, isChildVisible} from '../test_util.m.js';
 
 import {TestHatsBrowserProxy} from './test_hats_browser_proxy.js';
@@ -113,6 +116,12 @@ suite('PrivacySandbox_PrivacySandboxSettings2Enabled', function() {
   /** @type {!PrivacySandboxAppElement} */
   let page;
 
+  /**
+   * @implements {PrivacySandboxBrowserProxy}
+   * @extends {TestBrowserProxy}
+   */
+  let testPrivacySandboxBrowserProxy;
+
   suiteSetup(function() {
     loadTimeData.overrideValues({
       privacySandboxSettings2Enabled: true,
@@ -121,6 +130,14 @@ suite('PrivacySandbox_PrivacySandboxSettings2Enabled', function() {
 
   setup(function() {
     document.body.innerHTML = '';
+
+    testPrivacySandboxBrowserProxy =
+        TestBrowserProxy.fromClass(PrivacySandboxBrowserProxy);
+    PrivacySandboxBrowserProxyImpl.instance_ = testPrivacySandboxBrowserProxy;
+
+    testPrivacySandboxBrowserProxy.setResultFor(
+        'getFlocId', Promise.resolve('test-id'));
+
     page = /** @type {!PrivacySandboxAppElement} */
         (document.createElement('privacy-sandbox-app'));
     document.body.appendChild(page);
@@ -130,5 +147,19 @@ suite('PrivacySandbox_PrivacySandboxSettings2Enabled', function() {
 
   test('flocCardVisibility', function() {
     assertTrue(isChildVisible(page, '#flocCard'));
+  });
+
+  test('flocId', async function() {
+    await testPrivacySandboxBrowserProxy.whenCalled('getFlocId');
+    assertEquals('test-id', page.$$('#flocId').textContent);
+
+    webUIListenerCallback('floc-id-changed', 'new-test-id');
+    await flushTasks();
+    assertEquals('new-test-id', page.$$('#flocId').textContent);
+  });
+
+  test('resetFlocId', function() {
+    page.$$('#resetFlocIdButton').click();
+    return testPrivacySandboxBrowserProxy.whenCalled('resetFlocId');
   });
 });
