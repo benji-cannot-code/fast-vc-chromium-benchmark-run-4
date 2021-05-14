24bf328336ee38b271fac8c90ef28bf3c42a3096
Anastasia Helfinstein <anastasi@google.com>
2021-05-14 20:12:06 +0000

    [Switch Access] Setup guide: Add screen to choose switch count
    This change adds another page to the Switch Access setup guide,
    currently under development behnd a flag. The new page contains a set
    of radio buttons, for the user to select how many switches they use.
    
    Googlers can see the UI change at
    https://screenshot.googleplex.com/BAXwZULixEerRVB
    
    AX-Relnotes: n/a.
    Bug: 1174726
    Change-Id: Ia4360bfb2c7a3a7f50c71417386f2a710d5faea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2866741
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#883096}

--- chrome/browser/resources/settings/chromeos/os_a11y_page/switch_access_setup_guide_dialog.js
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/switch_access_setup_guide_dialog.js
@@ -17,9 +17,10 @@ const SASetupElement = {
   BLUETOOTH_BUTTON: 'bluetooth',
   EXIT_BUTTON: 'exit',
   NEXT_BUTTON: 'next',
-  PREIVOUS_BUTTON: 'previous',
+  PREVIOUS_BUTTON: 'previous',
   INTRO_CONTENT: 'intro',
   ASSIGN_SELECT_CONTENT: 'assign-select',
+  CHOOSE_SWITCH_COUNT_CONTENT: 'choose-switch-count',
 };
 
 /**
@@ -62,8 +63,15 @@ SASetupPageList[SASetupPageId.INTRO] = {
 SASetupPageList[SASetupPageId.ASSIGN_SELECT] = {
   titleId: 'switchAccessSetupIntroTitle',
   visibleElements: [
-    SASetupElement.EXIT_BUTTON, SASetupElement.PREIVOUS_BUTTON,
+    SASetupElement.NEXT_BUTTON, SASetupElement.PREVIOUS_BUTTON,
     SASetupElement.ASSIGN_SELECT_CONTENT
+  ]
+};
+SASetupPageList[SASetupPageId.CHOOSE_SWITCH_COUNT] = {
+  titleId: 'switchAccessSetupChooseSwitchCountTitle',
+  visibleElements: [
+    SASetupElement.EXIT_BUTTON, SASetupElement.PREVIOUS_BUTTON,
+    SASetupElement.CHOOSE_SWITCH_COUNT_CONTENT
   ]
 };
 
@@ -103,7 +111,13 @@ Polymer({
    * @private
    */
   getNextPageId_() {
-    return SASetupPageId.ASSIGN_SELECT;
+    switch (this.currentPageId_) {
+      case SASetupPageId.INTRO:
+        return SASetupPageId.ASSIGN_SELECT;
+      case SASetupPageId.ASSIGN_SELECT:
+      default:
+        return SASetupPageId.CHOOSE_SWITCH_COUNT;
+    }
   },
 
   /**
@@ -112,7 +126,13 @@ Polymer({
    * @private
    */
   getPreviousPageId_() {
-    return SASetupPageId.INTRO;
+    switch (this.currentPageId_) {
+      case SASetupPageId.ASSIGN_SELECT:
+        return SASetupPageId.INTRO;
+      case SASetupPageId.CHOOSE_SWITCH_COUNT:
+      default:
+        return SASetupPageId.ASSIGN_SELECT;
+    }
   },
 
   /** @private */
