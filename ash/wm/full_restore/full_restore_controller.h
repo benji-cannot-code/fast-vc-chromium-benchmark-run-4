02ee94966b6a48cbbdf31a2d1fd516790ea44b2d
chinsenj <chinsenj@chromium.org>
2021-05-14 03:42:54 +0000

    Reland "full_restore: Restore window state for arc windows."
    This is a reland of a60f9a76e5d628245049d304496df29220cf8abb
    
    Original change's description:
    > full_restore: Restore window state for arc windows.
    >
    > Currently arc windows restored by Full Restore don't have their window
    > state type restored. This CL fixes this by restoring arc window's state
    > type once their geometry is ready.
    >
    > Test: modified existing
    > Bug: 1190711
    > Change-Id: I4fe9cd9c941fae7781f3c3180af27c76a523b3a7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880475
    > Commit-Queue: Jeremy Chinsen <chinsenj@chromium.org>
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#882501}
    
    Bug: 1190711
    Change-Id: I702e93eeae4fddbdc59c50403890474867fb33ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2894904
    Auto-Submit: Jeremy Chinsen <chinsenj@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882846}

--- ash/wm/full_restore/full_restore_controller.h
+++ ash/wm/full_restore/full_restore_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/session/session_observer.h"
 #include "ash/public/cpp/tablet_mode_observer.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
+#include "base/containers/flat_set.h"
 #include "base/scoped_multi_source_observation.h"
 #include "base/scoped_observation.h"
 #include "components/full_restore/full_restore_info.h"
@@ -109,6 +110,11 @@ class ASH_EXPORT FullRestoreController
   void SaveWindowImpl(WindowState* window_state,
                       base::Optional<int> activation_index);
 
+  // Retrieves the saved `WindowInfo` of `window` and restores its
+  // `WindowStateType`. Also creates a post task to clear `window`s
+  // `full_restore::kLaunchedFromFullRestoreKey`.
+  void RestoreStateTypeAndClearLaunchedKey(aura::Window* window);
+
   // Sets a callback for testing that will be read from in
   // `OnWidgetInitialized()`.
   void SetReadWindowCallbackForTesting(ReadWindowCallback callback);
@@ -123,6 +129,10 @@ class ASH_EXPORT FullRestoreController
 
   // True whenever we are stacking windows to match saved activation order.
   bool is_stacking_ = false;
+
+  // The set of windows that have had their widgets initialized and will be
+  // shown later.
+  base::flat_set<aura::Window*> to_be_shown_windows_;
 
   ScopedSessionObserver scoped_session_observer_{this};
 
