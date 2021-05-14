0774362aed764e7e144a69f2bbe3ff0a3812029d
Chris Thompson <cthomp@chromium.org>
2021-05-14 19:35:29 +0000

    Remove TransportSecurityPersister::DeserializeObsoleteData()
    This removes DeserializeObsoleteData() and the related logic for
    handling the old data format now that it has been obsoleted for a while,
    and cleans up the unittests.
    
    Bug: 1086975
    Change-Id: I1f7ea9b9d696185d1901f3a22ad5cf14753163a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2893276
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883077}

--- net/http/transport_security_persister.h
+++ net/http/transport_security_persister.h
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 class SequencedTaskRunner;
-class Value;
 }
 
 namespace net {
@@ -108,27 +107,13 @@ class NET_EXPORT TransportSecurityPersister
 
   // Clears any existing non-static entries, and then re-populates
   // |transport_security_state_|.
-  //
-  // Sets |*data_in_old_format| to true if the loaded data is in an older format
-  // and should be overwritten with data in the newest format.
-  bool LoadEntries(const std::string& serialized, bool* data_in_old_format);
+  bool LoadEntries(const std::string& serialized);
 
  private:
   // Populates |state| from the JSON string |serialized|. Returns true if
   // all entries were parsed and deserialized correctly.
-  //
-  // Sets |*data_in_old_format| to true if the old data is in the old file
-  // format and needs to be overwritten with data in the newer format; false
-  // otherwise.
   static bool Deserialize(const std::string& serialized,
-                          bool* data_in_old_format,
                           TransportSecurityState* state);
-
-  // Used internally by Deserialize() to handle older dictionaries.
-  // TODO(https://crbug.com/1086975): This should be removed in Chrome 88.
-  static bool DeserializeObsoleteData(const base::Value& value,
-                                      bool* dirty,
-                                      TransportSecurityState* state);
 
   void CompleteLoad(const std::string& state);
   void OnWriteFinished(base::OnceClosure callback);
