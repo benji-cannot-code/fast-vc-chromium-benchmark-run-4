5ae2bf438e10e7ee93e3b5ce1fc62ee832a17974
Curt Clemens <cclem@google.com>
2021-05-14 21:12:51 +0000

    [Nearby] Migrate IceConfigFetcher to use Tachyon API
    The IceConfigFetcher was calling directly to the Network Traversal API
    using Unido's API key, which was causing quota issues. This CL adds a
    TachyonIceConfigFetcher class to allow Nearby to get ICE configuration
    from Tachyon instead.
    
    Fixed: 1189942, 1124392
    Change-Id: I0a0c320841dde8debe5d5b575021c530154c47d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2857447
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Curt Clemens <cclem@google.com>
    Cr-Commit-Position: refs/heads/master@{#883126}

--- chrome/browser/sharing/webrtc/network_traversal_ice_config_fetcher.h
+++ chrome/browser/sharing/webrtc/network_traversal_ice_config_fetcher.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_SHARING_WEBRTC_ICE_CONFIG_FETCHER_H_
-#define CHROME_BROWSER_SHARING_WEBRTC_ICE_CONFIG_FETCHER_H_
+#ifndef CHROME_BROWSER_SHARING_WEBRTC_NETWORK_TRAVERSAL_ICE_CONFIG_FETCHER_H_
+#define CHROME_BROWSER_SHARING_WEBRTC_NETWORK_TRAVERSAL_ICE_CONFIG_FETCHER_H_
 
 #include "base/callback_forward.h"
 #include "base/memory/scoped_refptr.h"
@@ -17,14 +17,17 @@ namespace network {
 class SharedURLLoaderFactory;
 }  // namespace network
 
-class IceConfigFetcher : public sharing::mojom::IceConfigFetcher {
+class NetworkTraversalIceConfigFetcher
+    : public sharing::mojom::IceConfigFetcher {
  public:
-  explicit IceConfigFetcher(
+  explicit NetworkTraversalIceConfigFetcher(
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
-  ~IceConfigFetcher() override;
+  ~NetworkTraversalIceConfigFetcher() override;
 
-  IceConfigFetcher(const IceConfigFetcher& other) = delete;
-  IceConfigFetcher& operator=(const IceConfigFetcher& other) = delete;
+  NetworkTraversalIceConfigFetcher(
+      const NetworkTraversalIceConfigFetcher& other) = delete;
+  NetworkTraversalIceConfigFetcher& operator=(
+      const NetworkTraversalIceConfigFetcher& other) = delete;
 
   // TODO(crbug.com/1124392) - Cache configs fetched from server.
   void GetIceServers(GetIceServersCallback callback) override;
@@ -33,4 +36,4 @@ class IceConfigFetcher : public sharing::mojom::IceConfigFetcher {
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
 };
 
-#endif  // CHROME_BROWSER_SHARING_WEBRTC_ICE_CONFIG_FETCHER_H_
+#endif  // CHROME_BROWSER_SHARING_WEBRTC_NETWORK_TRAVERSAL_ICE_CONFIG_FETCHER_H_
--- chrome/browser/sharing/webrtc/network_traversal_ice_config_fetcher_unittest.cc
+++ chrome/browser/sharing/webrtc/network_traversal_ice_config_fetcher_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/sharing/webrtc/ice_config_fetcher.h"
+#include "chrome/browser/sharing/webrtc/network_traversal_ice_config_fetcher.h"
 
 #include "base/files/file_util.h"
 #include "base/path_service.h"
@@ -20,14 +20,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-class IceConfigFetcherTest : public testing::Test {
+class NetworkTraversalIceConfigFetcherTest : public testing::Test {
  public:
-  IceConfigFetcherTest()
+  NetworkTraversalIceConfigFetcherTest()
       : test_shared_loader_factory_(
             base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
                 &test_url_loader_factory_)),
         ice_config_fetcher_(test_shared_loader_factory_) {}
-  ~IceConfigFetcherTest() override = default;
+  ~NetworkTraversalIceConfigFetcherTest() override = default;
 
   std::string GetApiUrl() const {
     return base::StrCat(
@@ -63,11 +63,11 @@ class IceConfigFetcherTest : public testing::Test {
   base::test::SingleThreadTaskEnvironment task_environment_;
   network::TestURLLoaderFactory test_url_loader_factory_;
   scoped_refptr<network::SharedURLLoaderFactory> test_shared_loader_factory_;
-  IceConfigFetcher ice_config_fetcher_;
+  NetworkTraversalIceConfigFetcher ice_config_fetcher_;
   base::HistogramTester histogram_tester_;
 };
 
-TEST_F(IceConfigFetcherTest, ResponseSuccessful) {
+TEST_F(NetworkTraversalIceConfigFetcherTest, ResponseSuccessful) {
   base::RunLoop run_loop;
   ice_config_fetcher_.GetIceServers(base::BindLambdaForTesting(
       [&](std::vector<sharing::mojom::IceServerPtr> ice_servers) {
@@ -90,7 +90,7 @@ TEST_F(IceConfigFetcherTest, ResponseSuccessful) {
   histogram_tester_.ExpectBucketCount(metric_name, 2, 1);
 }
 
-TEST_F(IceConfigFetcherTest, ResponseError) {
+TEST_F(NetworkTraversalIceConfigFetcherTest, ResponseError) {
   base::RunLoop run_loop;
   ice_config_fetcher_.GetIceServers(base::BindLambdaForTesting(
       [&](std::vector<sharing::mojom::IceServerPtr> ice_servers) {
@@ -112,7 +112,7 @@ TEST_F(IceConfigFetcherTest, ResponseError) {
   histogram_tester_.ExpectBucketCount(metric_name, 0, 1);
 }
 
-TEST_F(IceConfigFetcherTest, OverlappingCalls) {
+TEST_F(NetworkTraversalIceConfigFetcherTest, OverlappingCalls) {
   base::RunLoop run_loop;
   int counter = 2;
   auto callback = [&](std::vector<sharing::mojom::IceServerPtr> ice_servers) {
