efda65ee9f07dc04cf28ae152bc5c1dd22ca12f1
Lei Zhang <thestig@chromium.org>
2021-05-14 18:45:08 +0000

    Remove base::DictionaryValue::begin()/end().
    This removes these deprecated member function and converts all callers
    to use base::Value::DictItems(). This reduces an external dependency on
    base::Value::LegacyDictStorage and makes it possible for base::Value to
    store dictionaries with base::Value::DictStorage.
    
    Bug: 1187057
    Change-Id: I2a37eb0a459372c6b8f57c807ecc4652b2ad8c81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2896224
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883049}

--- chrome/browser/enterprise/reporting/profile_report_generator_desktop.cc
+++ chrome/browser/enterprise/reporting/profile_report_generator_desktop.cc
@@ -87,7 +87,7 @@ void ProfileReportGeneratorDesktop::GetExtensionRequest(
       extension_urls::GetDefaultWebstoreUpdateUrl().spec();
 
   int number_of_requests = 0;
-  for (const auto& it : *pending_requests) {
+  for (const auto& it : pending_requests->DictItems()) {
     if (!ExtensionRequestReportGenerator::ShouldUploadExtensionRequest(
             it.first, webstore_update_url, extension_management)) {
       continue;
@@ -102,7 +102,7 @@ void ProfileReportGeneratorDesktop::GetExtensionRequest(
     auto* request = report->add_extension_requests();
     request->set_id(it.first);
     base::Optional<base::Time> timestamp = ::util::ValueToTime(
-        it.second->FindKey(extension_misc::kExtensionRequestTimestamp));
+        it.second.FindKey(extension_misc::kExtensionRequestTimestamp));
     if (timestamp)
       request->set_request_timestamp(timestamp->ToJavaTime());
   }
