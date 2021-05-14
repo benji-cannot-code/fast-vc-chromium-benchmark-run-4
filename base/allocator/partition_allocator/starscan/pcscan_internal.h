e0acf69afe442eb73313d23f76bdc78857cd2e67
Stephan Hartmann <stha09@googlemail.com>
2021-05-14 20:09:26 +0000

    IWYU: include mutex for std::mutex
    Bug: None
    Change-Id: I80646bff4affcb36263bebf717898f006f45159b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2897218
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883095}

--- base/allocator/partition_allocator/starscan/pcscan_internal.h
+++ base/allocator/partition_allocator/starscan/pcscan_internal.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_STARSCAN_PCSCAN_INTERNAL_H_
 
 #include <array>
+#include <mutex>
 #include <unordered_map>
 
 #include "base/allocator/partition_allocator/starscan/metadata_allocator.h"
