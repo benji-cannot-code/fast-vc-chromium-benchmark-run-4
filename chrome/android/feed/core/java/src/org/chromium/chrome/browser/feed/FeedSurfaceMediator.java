d5a98c11cec3a62145d2bcd6df70aad6db951599
Ian Wells <iwells@chromium.org>
2021-05-14 17:37:26 +0000

    Feed: add command line switch to scroll to the top of the feed on bind
    This change is meant to help with scrolling the feed fully into view when taking screenshots for diffing. The result is that the top edge of the first card appears just under the omnibox.
    
    Bug: 1152187
    Change-Id: I4d840c0377c48f0b5155cf5509e37441b92d1b56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2895651
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883024}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
@@ -538,6 +538,11 @@ public class FeedSurfaceMediator
         }
         mCurrentStream = stream;
         mCurrentStream.addOnContentChangedListener(mStreamContentChangedListener);
+
+        if (FeedFeatures.isAutoScrollToTopEnabled() && mRestoreScrollState == null) {
+            mRestoreScrollState = getScrollStateForAutoScrollToTop();
+        }
+
         mCurrentStream.bind(mCoordinator.getRecyclerView(), mCoordinator.getContentManager(),
                 mRestoreScrollState, mCoordinator.getSurfaceScope(),
                 mCoordinator.getHybridListRenderer());
@@ -1016,6 +1021,12 @@ public class FeedSurfaceMediator
         StartSurfaceConfiguration.recordHistogram(FEED_CONTENT_FIRST_LOADED_TIME_MS_UMA,
                 mContentFirstAvailableTimeMs - mActivityCreationTimeMs, mIsInstantStart);
         return true;
+    }
+
+    private ScrollState getScrollStateForAutoScrollToTop() {
+        ScrollState state = new ScrollState();
+        state.position = 1;
+        return state;
     }
 
     // Detects animation finishes in RecyclerView.
