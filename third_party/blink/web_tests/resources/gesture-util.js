5f25d1da7fa7d10f974203ed953f1adacc74bda1
Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
2021-05-14 21:33:56 +0000

    Percent based scrolling test cases failures fixes - Part 3.
    The CL fixes some mouse wheel scrolling related test cases by changing
    expected elements' scroll values and related deltas when the Percent
    based scrolling feature flag is turned on.
    
    Overscroll events related tests remain unchanged due to the bug.
    
    Bug: 1204176
    Bug: 1199989
    Change-Id: Ia336b72e1028c6286808362bce82fb6a55b243e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2832125
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#883140}

--- third_party/blink/web_tests/resources/gesture-util.js
+++ third_party/blink/web_tests/resources/gesture-util.js
@@ -170,7 +170,7 @@ function waitForScrollEvent(eventTarget, timeoutMs = 1000) {
   return waitForEvent(eventTarget, 'scroll', timeoutMs);
 }
 
-function waitForScrollendEvent(eventTarget, timeoutMs = 1000) {
+function waitForScrollendEvent(eventTarget, timeoutMs = 2000) {
   return waitForEvent(eventTarget, 'scrollend', timeoutMs);
 }
 
