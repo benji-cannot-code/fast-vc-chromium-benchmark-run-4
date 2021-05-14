125676d78df324487b14e66f33b619349f8d3ac2
Joey Arhar <jarhar@chromium.org>
2021-05-14 18:12:38 +0000

    Reland #2 "Safe accessibility tree management during slot reassignment"
    Original patch: http://crrev.com/c/2867729
    Revert: http://crrev.com/c/2881294
    Reland #1: http://crrev.com/c/2885943
    Revert #2: http://crrev.com/c/2891310
    
    Relative to Reland #1, this patch fixes the ASAN use-after-poison issue
    by retaining the stricter HTMLSlotElement check in accessibility code.
    I filed a bug to follow up on this: crbug.com/1209216
    
    The leak test issue was also fixed by crrev.com/c/2896606, so I don't
    need the LeakExpectations entry anymore.
    
    Bug: 1204683, 1205770, 1206404
    Change-Id: I79854301176978b3b734df5001790c42b310ab19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2894725
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883039}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_frame_owner_element.h"
 #include "third_party/blink/renderer/core/html/html_head_element.h"
 #include "third_party/blink/renderer/core/html/html_script_element.h"
+#include "third_party/blink/renderer/core/html/html_slot_element.h"
 #include "third_party/blink/renderer/core/html/html_style_element.h"
 #include "third_party/blink/renderer/core/html/html_table_cell_element.h"
 #include "third_party/blink/renderer/core/html/html_table_element.h"
@@ -4234,6 +4235,11 @@ void AXObject::ClearChildren() const {
                                    << ToString(true, true);
 
   for (const auto& child : children_) {
+    // Check parent first, as the child might be several levels down if there
+    // are unincluded nodes in between, in which case the cached parent will
+    // also be a descendant (unlike children_, parent_ does not skip levels).
+    // Another case where the parent is not the same is when the child has been
+    // reparented using aria-owns.
     if (child->CachedParentObject() == this)
       child->DetachFromParent();
   }
@@ -4242,6 +4248,13 @@ void AXObject::ClearChildren() const {
 
   if (!GetNode())
     return;
+
+  if (GetDocument()->IsFlatTreeTraversalForbidden() ||
+      GetDocument()->IsSlotAssignmentRecalcForbidden()) {
+    // Cannot use layout tree builder traversal now, will have to rely on
+    // RepairParent() at a later point.
+    return;
+  }
 
   // <slot> content is always included in the tree, so there is no need to
   // iterate through the nodes. This also protects us against slot use "after
@@ -4257,9 +4270,16 @@ void AXObject::ClearChildren() const {
   // information with their original location in the DOM. Therefore, we need to
   // ensure that in the accessibility tree no remnant information from the
   // unflattened DOM tree remains, such as the cached parent.
-  if (IsA<HTMLSlotElement>(GetNode()))
+
+  // TODO(crbug.com/1209216): Figure out why removing this causes a
+  // use-after-poison and possibly replace it with a better check.
+  HTMLSlotElement* slot = DynamicTo<HTMLSlotElement>(GetNode());
+  if (slot && slot->SupportsAssignment())
     return;
 
+  // Detach children that were not cleared from first loop.
+  // These must have been an unincluded node who's parent is this,
+  // although it may now be included since the children were last updated.
   for (Node* child_node = LayoutTreeBuilderTraversal::FirstChild(*GetNode());
        child_node;
        child_node = LayoutTreeBuilderTraversal::NextSibling(*child_node)) {
@@ -4267,9 +4287,6 @@ void AXObject::ClearChildren() const {
     AXObject* ax_child_from_node = AXObjectCache().Get(child_node);
     if (ax_child_from_node &&
         ax_child_from_node->CachedParentObject() == this) {
-      // Child was not cleared from first loop.
-      // It must have been an unincluded node who's parent is this,
-      // although it may now be included since the children were last updated.
       // Check current parent first. It may be owned by another node.
       ax_child_from_node->DetachFromParent();
     }
