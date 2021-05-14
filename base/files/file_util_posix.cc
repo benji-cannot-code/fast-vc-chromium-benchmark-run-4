58feadd7df957999832b456ccbd936dea7b90f86
Lei Zhang <thestig@chromium.org>
2021-05-14 21:21:54 +0000

    Reverse delegation between base::WriteFileDescriptor() variants.
    Have the StringPiece variant delegate to the span variant. This looks
    cleaner and does not require a reinterpret_cast. Do the same for
    AppendToFile().
    
    Change-Id: I024e1d7294f837abac3c25a2e24ad0b4dc54f23e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2897423
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883132}

--- base/files/file_util_posix.cc
+++ base/files/file_util_posix.cc
@@ -891,11 +891,6 @@ int WriteFile(const FilePath& filename, const char* data, int size) {
 }
 
 bool WriteFileDescriptor(int fd, span<const uint8_t> data) {
-  return WriteFileDescriptor(
-      fd, StringPiece(reinterpret_cast<const char*>(data.data()), data.size()));
-}
-
-bool WriteFileDescriptor(int fd, StringPiece data) {
   // Allow for partial writes.
   ssize_t bytes_written_total = 0;
   ssize_t size = checked_cast<ssize_t>(data.size());
@@ -908,6 +903,10 @@ bool WriteFileDescriptor(int fd, StringPiece data) {
   }
 
   return true;
+}
+
+bool WriteFileDescriptor(int fd, StringPiece data) {
+  return WriteFileDescriptor(fd, as_bytes(make_span(data)));
 }
 
 bool AllocateFileRegion(File* file, int64_t offset, size_t size) {
@@ -985,12 +984,6 @@ bool AllocateFileRegion(File* file, int64_t offset, size_t size) {
 #if !defined(OS_NACL_NONSFI)
 
 bool AppendToFile(const FilePath& filename, span<const uint8_t> data) {
-  return AppendToFile(
-      filename,
-      StringPiece(reinterpret_cast<const char*>(data.data()), data.size()));
-}
-
-bool AppendToFile(const FilePath& filename, StringPiece data) {
   ScopedBlockingCall scoped_blocking_call(FROM_HERE, BlockingType::MAY_BLOCK);
   bool ret = true;
   int fd = HANDLE_EINTR(open(filename.value().c_str(), O_WRONLY | O_APPEND));
@@ -1011,6 +1004,10 @@ bool AppendToFile(const FilePath& filename, StringPiece data) {
   }
 
   return ret;
+}
+
+bool AppendToFile(const FilePath& filename, StringPiece data) {
+  return AppendToFile(filename, as_bytes(make_span(data)));
 }
 
 bool GetCurrentDirectory(FilePath* dir) {
