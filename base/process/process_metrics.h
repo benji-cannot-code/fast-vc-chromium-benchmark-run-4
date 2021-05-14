73a08d6d0172af01cdb0ad191990539b6fc92205
Lei Zhang <thestig@chromium.org>
2021-05-14 20:23:36 +0000

    Clean up headers in base/process/process_metrics.h.
    - Remove unneeded base/optional.h.
    - Mark a bunch of includes as platform-specific with a #if.
    
    Change-Id: I5ea5e31846406199a02f3545c49797b3beece779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2897391
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883103}

--- base/process/process_metrics.h
+++ base/process/process_metrics.h
@@ -13,17 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
-#include <string>
-#include <utility>
-#include <vector>
 
 #include "base/base_export.h"
-#include "base/cpu.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
-#include "base/optional.h"
 #include "base/process/process_handle.h"
-#include "base/threading/platform_thread.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -43,6 +37,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include "base/win/scoped_handle.h"
 #include "base/win/windows_types.h"
+#endif
+
+#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_ANDROID) || \
+    defined(OS_AIX)
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "base/cpu.h"
+#include "base/threading/platform_thread.h"
 #endif
 
 namespace base {
