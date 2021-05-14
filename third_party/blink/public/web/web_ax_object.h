fd2d61aa16f7252d0a7becd62c05d52135d3d8b5
Dominic Mazzoni <dmazzoni@chromium.org>
2021-05-14 16:17:13 +0000

    Only compute HasAriaAttribute when kScreenReader is enabled.
    This is a more obscure attribute, we can skip computing it when we
    don't think "real" assistive technology is running.
    
    This also moves the computation from content to blink, helping with
    onion soup.
    
    Bug: 1208912, 1068668
    Change-Id: I5cc5818df2fa7f57cbf54ad8e484bbe738f78410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2895111
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882989}

--- third_party/blink/public/web/web_ax_object.h
+++ third_party/blink/public/web/web_ax_object.h
@@ -150,7 +150,6 @@ class WebAXObject {
   BLINK_EXPORT bool IsSelectedOptionActive() const;
   BLINK_EXPORT bool IsVisited() const;
 
-  BLINK_EXPORT bool HasAriaAttribute() const;
   BLINK_EXPORT WebString AccessKey() const;
   BLINK_EXPORT bool CanPress() const;
   BLINK_EXPORT bool CanSetValueAttribute() const;
