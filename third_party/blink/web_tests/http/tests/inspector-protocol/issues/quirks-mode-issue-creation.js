ff654681457effe96ea485d422a138de626ccea3
Sigurd Schneider <sigurds@chromium.org>
2021-05-14 08:52:21 +0000

    Fix flaky test quirks-mode-issue-creation.js
    The test didn't wait for the issues to arrive,
    and would sometimes miss the last issue.
    
    Fixed: chromium:1208547
    Change-Id: Ie2e2f2c0fb545c355ec7fde2c8c155562cc77a46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2896305
    Auto-Submit: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882914}

--- third_party/blink/web_tests/http/tests/inspector-protocol/issues/quirks-mode-issue-creation.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/quirks-mode-issue-creation.js
@@ -4,18 +4,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   await dp.Audits.enable();
 
+  let expectedIssues = 3;
   dp.Audits.onIssueAdded(issue => {
     testRunner.log(issue.params, "Inspector issue: ", ['documentNodeId', 'frameId', 'loaderId']);
+    if (--expectedIssues === 0) {
+      testRunner.completeTest();
+    }
   });
-
-  await page.navigate('../resources/quirks-mode.html');
 
   // Test that going to a no-quirks page does not report QuirksMode issues.
   await page.navigate('../resources/inspector-protocol-page.html');
 
+  await page.navigate('../resources/quirks-mode.html');
+
   await page.navigate('../resources/limited-quirks-mode.html');
 
   await page.navigate('../resources/quirks-mode-in-iframe.html');
-
-  testRunner.completeTest();
 });
