e285fd84db893ca6de949cd829a932b2559fb58c
Kramer Ge <fangzhoug@chromium.org>
2021-05-14 02:45:00 +0000

    Revert "[ozone/wayland] Dispatch MouseWheelEvent instance to actual wheel devices"
    This reverts commit c0aad7f432a0139cc7dccb45d50c1aa3523a14d0.
    
    Reason for revert: Causing crbug.com/1208960, some dropdowns disappear as soon as cursor moves.
    
    Original change's description:
    > [ozone/wayland] Dispatch MouseWheelEvent instance to actual wheel devices
    >
    > Currently, MouseWheelEvent's are being fired for both mouse wheel
    > and two-fingers swipe scroll.
    >
    > BUG=1204222
    > R=​nickdiego@igalia.com
    >
    > Change-Id: I4aa859fea444ccf975d91a8fe5f948c4baf64138
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2884281
    > Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    > Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#881687}
    
    Bug: 1204222, 1208960
    Change-Id: Ia6d11275a0ffb7b399fa24070a8f84e48cf693aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2896045
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882834}

--- ui/ozone/platform/wayland/host/wayland_event_source.cc
+++ ui/ozone/platform/wayland/host/wayland_event_source.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/pointer_details.h"
 #include "ui/events/types/event_type.h"
 #include "ui/gfx/geometry/point_f.h"
+#include "ui/gfx/geometry/vector2d.h"
 #include "ui/gfx/geometry/vector2d_f.h"
 #include "ui/ozone/platform/wayland/host/wayland_connection.h"
 #include "ui/ozone/platform/wayland/host/wayland_event_watcher.h"
@@ -222,18 +223,10 @@ void WaylandEventSource::OnPointerFrameEvent() {
     DispatchEvent(&event);
     recent_pointer_frames_.clear();
   } else {
-    if (current_pointer_frame_.axis_source == WL_POINTER_AXIS_SOURCE_WHEEL) {
-      MouseWheelEvent event(
-          gfx::Vector2d(current_pointer_frame_.dx, current_pointer_frame_.dy),
-          pointer_location_, pointer_location_, EventTimeForNow(), flags, 0);
-      DispatchEvent(&event);
-    } else {
-      ScrollEvent event(ET_SCROLL, pointer_location_, pointer_location_,
-                        EventTimeForNow(), flags, current_pointer_frame_.dx,
-                        current_pointer_frame_.dy, current_pointer_frame_.dx,
-                        current_pointer_frame_.dy, kGestureScrollFingerCount);
-      DispatchEvent(&event);
-    }
+    MouseWheelEvent event(
+        gfx::Vector2d(current_pointer_frame_.dx, current_pointer_frame_.dy),
+        pointer_location_, pointer_location_, EventTimeForNow(), flags, 0);
+    DispatchEvent(&event);
 
     if (recent_pointer_frames_.size() + 1 > kRecentPointerFrameMaxSize)
       recent_pointer_frames_.pop_back();
