b5d1a4ab455f01f5a9061aa05f52bbe6ec660413
Alexey Baskakov <loyso@chromium.org>
2021-05-14 09:26:41 +0000

    dpwa: Download Web App monochrome icons and serialize them.
    Download Web App monochrome icons and save them to
    Manifest Resources directory. Our WebAppIconManager::Read* API
    supports them automatically.
    
    This is primary app icons only.
    
    Monochrome icons are still unused in UI. Will be used in
    Web App Notifications: that will require a browser test.
    
    A support for monochrome icons in Shortcut Menus (Jump Lists)
    will require a separate CL.
    
    Bug: 1114638
    Change-Id: Ia8c01869d26e1329332e9929f1af87cca024ae70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2891231
    Auto-Submit: Alexey Baskakov <loyso@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#882922}

--- chrome/browser/web_applications/web_app_icon_manager.cc
+++ chrome/browser/web_applications/web_app_icon_manager.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/web_applications/components/web_app_utils.h"
+#include "chrome/browser/web_applications/components/web_application_info.h"
 #include "chrome/browser/web_applications/file_utils_wrapper.h"
 #include "chrome/browser/web_applications/web_app.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
@@ -228,18 +229,15 @@ bool WriteDataBlocking(const std::unique_ptr<FileUtilsWrapper>& utils,
     return false;
   }
 
-  if (!WriteIcons(
-          utils.get(),
-          GetAppIconsDirectory(app_temp_dir.GetPath(), IconPurpose::ANY),
-          icon_bitmaps.any)) {
-    return false;
-  }
-  // TODO (crbug.com/1114638): Write Monochrome icons here.
-  if (!WriteIcons(
-          utils.get(),
-          GetAppIconsDirectory(app_temp_dir.GetPath(), IconPurpose::MASKABLE),
-          icon_bitmaps.maskable)) {
-    return false;
+  // Iterates over each icon purpose.
+  for (int p = static_cast<int>(IconPurpose::kMinValue);
+       p <= static_cast<int>(IconPurpose::kMaxValue); ++p) {
+    auto purpose = static_cast<IconPurpose>(p);
+    if (!WriteIcons(utils.get(),
+                    GetAppIconsDirectory(app_temp_dir.GetPath(), purpose),
+                    icon_bitmaps.GetBitmapsForPurpose(purpose))) {
+      return false;
+    }
   }
 
   base::FilePath manifest_resources_directory =
