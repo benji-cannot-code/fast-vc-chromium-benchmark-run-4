b0579301bc9d37dcc0027926c915f6dad9a442c5
Fabrice de Gans <fdegans@chromium.org>
2021-05-15 00:09:31 +0000

    [cast_streaming] Handle non-zero initial timestamps
    Some Senders do not set timestamp 0 for the initial Frame. This
    works around that issue by computing a delay from the initial Frame
    timestamp.
    
    Change-Id: Idc805de350a5b0e7e4487dc4601e92d4edb76fc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2894670
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883218}

--- components/cast_streaming/browser/stream_consumer.h
+++ components/cast_streaming/browser/stream_consumer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_CAST_STREAMING_BROWSER_STREAM_CONSUMER_H_
 
 #include "base/callback.h"
+#include "base/time/time.h"
 #include "media/mojo/mojom/media_types.mojom.h"
 #include "mojo/public/cpp/system/data_pipe.h"
 #include "mojo/public/cpp/system/simple_watcher.h"
@@ -71,6 +72,9 @@ class StreamConsumer : public openscreen::cast::Receiver::Consumer {
 
   // Remaining bytes to write from |pending_buffer_| to |data_pipe_|.
   size_t pending_buffer_remaining_bytes_ = 0;
+
+  // Offset for frames playout time. This is initialized by the first frame.
+  base::TimeDelta playout_offset_ = base::TimeDelta::Max();
 
   // Closure called on every new frame.
   base::RepeatingClosure on_new_frame_;
