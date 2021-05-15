cc80fd4c11c91f660df63b7182e6f0d89904f8d3
Jian Li <jianli@google.com>
2021-05-15 01:02:59 +0000

    Expose API in ProcessScopeDependencyProvider to get API key
    Bug: 1185304
    Change-Id: I9dbe5d5a6ee87ea6793bde383662d85216075918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2888315
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883233}

--- chrome/browser/xsurface/android/java/src/org/chromium/chrome/browser/xsurface/ProcessScopeDependencyProvider.java
+++ chrome/browser/xsurface/android/java/src/org/chromium/chrome/browser/xsurface/ProcessScopeDependencyProvider.java
@@ -91,6 +91,7 @@ public interface ProcessScopeDependencyProvider {
         return false;
     }
 
+    @Deprecated
     default boolean isStableChannel() {
         return false;
     }
@@ -98,5 +99,10 @@ public interface ProcessScopeDependencyProvider {
     /** Returns the reliability logging id. */
     default long getReliabilityLoggingId() {
         return 0L;
+    }
+
+    /** Returns the google API key. */
+    default String getGoogleApiKey() {
+        return null;
     }
 }
