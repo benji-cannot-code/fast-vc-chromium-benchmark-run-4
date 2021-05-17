6d83e1d71771071b57a461401f740346d425716c
Xiyuan Xia <xiyuan@google.com>
2021-05-17 18:33:10 +0000

    Move apps::CreateStandardIconImage off UI thread
    Move apps::CreateStandardIconImage off UI thread for OnAppImageUpdate
    call stack. The call could take more than 100ms so it should not run
    on UI thread. The OnAppImageUpdate stack is the most common jank stack
    of apps::CreateStandardIconImage calls.
    
    Bug: 1205681
    Change-Id: Id2c52584da429b19d1ce0f5fc254b1a4489e7b04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2897431
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883538}

--- chrome/browser/ui/ash/shelf/chrome_shelf_controller.cc
+++ chrome/browser/ui/ash/shelf/chrome_shelf_controller.cc
@@ -26,9 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/sequenced_task_runner.h"
 #include "base/strings/pattern.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/task/post_task.h"
+#include "base/task/thread_pool.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
@@ -121,6 +124,14 @@ void SelectItemWithSource(ash::ShelfItemDelegate* delegate,
 // Returns true if the given |item| has a pinned shelf item type.
 bool ItemTypeIsPinned(const ash::ShelfItem& item) {
   return ash::IsPinnedShelfItemType(item.type);
+}
+
+// Invoked on a worker thread to create standard icon image.
+gfx::ImageSkia CreateStandardImageOnWorkerThread(const gfx::ImageSkia& image) {
+  gfx::ImageSkia standard_image = apps::CreateStandardIconImage(image);
+  if (!standard_image.isNull())
+    standard_image.MakeThreadSafe();
+  return standard_image;
 }
 
 }  // namespace
@@ -991,6 +1002,34 @@ void ChromeShelfController::OnAppImageUpdated(const std::string& app_id,
   if (!AppServiceAppIconLoader::CanLoadImage(latest_active_profile_, app_id))
     is_standard_icon = false;
 
+  if (is_standard_icon) {
+    UpdateAppImage(app_id, image);
+    return;
+  }
+
+  if (!standard_icon_task_runner_) {
+    standard_icon_task_runner_ = base::ThreadPool::CreateSequencedTaskRunner(
+        {base::TaskPriority::USER_VISIBLE,
+         base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
+  }
+
+  gfx::ImageSkia copy;
+  if (image.IsThreadSafe()) {
+    copy = image;
+  } else {
+    image.EnsureRepsForSupportedScales();
+    copy = image.DeepCopy();
+  }
+
+  base::PostTaskAndReplyWithResult(
+      standard_icon_task_runner_.get(), FROM_HERE,
+      base::BindOnce(&CreateStandardImageOnWorkerThread, copy),
+      base::BindOnce(&ChromeShelfController::UpdateAppImage,
+                     weak_ptr_factory_.GetWeakPtr(), app_id));
+}
+
+void ChromeShelfController::UpdateAppImage(const std::string& app_id,
+                                           const gfx::ImageSkia& image) {
   // TODO: need to get this working for shortcuts.
   for (int index = 0; index < model_->item_count(); ++index) {
     ash::ShelfItem item = model_->items()[index];
@@ -999,8 +1038,7 @@ void ChromeShelfController::OnAppImageUpdated(const std::string& app_id,
         item.id.app_id != app_id) {
       continue;
     }
-    item.image =
-        is_standard_icon ? image : apps::CreateStandardIconImage(image);
+    item.image = image;
     shelf_spinner_controller_->MaybeApplySpinningEffect(app_id, &item.image);
     item.notification_badge_color =
         ash::NotificationBadgeColorCache::GetInstance().GetBadgeColorForApp(
