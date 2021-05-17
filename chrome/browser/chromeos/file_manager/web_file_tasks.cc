f244b06b5895232b1cffae3bab761e81cf8ede4d
Patti <patricialor@chromium.org>
2021-05-17 02:26:47 +0000

    Media App: Conditionally remove media app as PDF handler based on flag.
    https://crrev.com/c/2822099 originally made the media app the default
    PDF handler based on the MediaAppHandlesPdf flag, but it assumed SWAs
    would be reinstalled after every flag flip, which only happens on
    developer builds (when restarting the Ozone build). In practice, SWA
    reinstallation only happens on a Chrome upgrade. To fix, tweak the
    implementation to always add PDF as a handler for PDF but skip it in the
    file task handler if the flag is off instead.
    
    Bug: b/173655642
    Change-Id: I34c527344ec1e6d70df9ad01a37fbfe7289bf04c
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2895978
    Auto-Submit: Patti <patricialor@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883351}

--- chrome/browser/chromeos/file_manager/web_file_tasks.cc
+++ chrome/browser/chromeos/file_manager/web_file_tasks.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "ash/constants/ash_features.h"
 #include "base/feature_list.h"
 #include "base/strings/strcat.h"
 #include "chrome/browser/apps/app_service/app_launch_params.h"
@@ -40,16 +41,20 @@ void FindWebTasks(Profile* profile,
   DCHECK(!entries.empty());
   DCHECK(result_list);
 
-  // WebApps only have full support files backed by inodes, so tasks provided by
-  // most Web Apps will be skipped if any non-native files are present. "System"
-  // Web Apps are an exception: we have more control over what they can do, so
-  // tasks provided by System Web Apps are the only ones permitted at present.
-  // See https://crbug.com/1079065.
-  bool has_special_file = false;
+  // WebApps only have full support for files backed by inodes, so tasks
+  // provided by most Web Apps will be skipped if any non-native files are
+  // present. "System" Web Apps are an exception: we have more control over what
+  // they can do, so tasks provided by System Web Apps are the only ones
+  // permitted at present. See https://crbug.com/1079065.
+  bool has_non_native_file = false;
+  bool has_pdf_file = false;
   for (const auto& entry : entries) {
-    if (util::IsUnderNonNativeLocalPath(profile, entry.path)) {
-      has_special_file = true;
-      break;
+    if (!has_non_native_file &&
+        util::IsUnderNonNativeLocalPath(profile, entry.path)) {
+      has_non_native_file = true;
+    }
+    if (!has_pdf_file && entry.path.Extension() == ".pdf") {
+      has_pdf_file = true;
     }
   }
 
@@ -61,7 +66,7 @@ void FindWebTasks(Profile* profile,
 
   std::vector<web_app::AppId> app_ids = registrar.GetAppIds();
   for (const auto& app_id : app_ids) {
-    if (has_special_file && app_id != web_app::kMediaAppId)
+    if (has_non_native_file && app_id != web_app::kMediaAppId)
       continue;
 
     if (!os_integration_manager.IsFileHandlingAPIAvailable(app_id))
@@ -79,6 +84,14 @@ void FindWebTasks(Profile* profile,
 
     if (matches.empty())
       continue;
+
+    // "Hide" the media app task (i.e. skip the rest of this loop which would
+    // add it as a handler) when the flag to handle PDF is off.
+    if (app_id == web_app::kMediaAppId &&
+        !base::FeatureList::IsEnabled(ash::features::kMediaAppHandlesPdf) &&
+        has_pdf_file) {
+      continue;
+    }
 
     // WebApps only support "open with", so find the first good file handler
     // match (or the first match, if there is no good match).
