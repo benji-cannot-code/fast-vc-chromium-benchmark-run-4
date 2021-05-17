f3f8d2466f6c7bd26e21867a2bf1de531d11c9a3
Xianzhu Wang <wangxianzhu@chromium.org>
2021-05-17 19:56:28 +0000

    Work around flaky crash of VideoPaintPreviewTest.*
    It seems related to cached SVGImage created when CAP is enabled and
    used when CAP is disabled, but I don't have enough time to fix the
    root cause. The workaround works and will reduce run time of
    blink_unittests by avoiding retries of the tests.
    
    Bug: 1203406
    Change-Id: I70edb3fab038b05ae33e304861f9f0bfa620034f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2895649
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883579}

--- third_party/blink/renderer/core/svg/graphics/svg_image.cc
+++ third_party/blink/renderer/core/svg/graphics/svg_image.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/layout/intrinsic_sizing_info.h"
+#include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/layout/svg/layout_svg_root.h"
 #include "third_party/blink/renderer/core/loader/frame_load_request.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
@@ -552,6 +553,11 @@ sk_sp<PaintRecord> SVGImage::PaintRecordForCurrentFrame(
     view->UpdateAllLifecyclePhases(DocumentUpdateReason::kSVGImage);
     return view->GetPaintRecord();
   }
+
+  // TODO(crbug.com/1203406): This works around the bug. We may want to find
+  // and fix the root cause, or do nothing until pre-CAP code is removed.
+  if (!view->GetLayoutView() || !view->GetLayoutView()->Compositor())
+    return nullptr;
 
   view->UpdateAllLifecyclePhasesExceptPaint(DocumentUpdateReason::kSVGImage);
   PaintRecordBuilder builder(*paint_controller_);
