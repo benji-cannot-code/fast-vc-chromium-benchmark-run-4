220e3c973c9265185270e3f7104d7ca722828b6b
Olivier Li <olivierli@chromium.org>
2021-05-17 22:19:43 +0000

    Prevent reuse of a moved-from base::Thread::Option instance
    Bug: 1208166
    Change-Id: I4fd716df7e07444f5d9c342e8f330245ae7c95db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2892340
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883664}

--- base/threading/thread.cc
+++ base/threading/thread.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <type_traits>
+#include <utility>
 
 #include "base/bind.h"
 #include "base/callback_helpers.h"
@@ -109,7 +110,17 @@ Thread::Options::Options() = default;
 Thread::Options::Options(MessagePumpType type, size_t size)
     : message_pump_type(type), stack_size(size) {}
 
-Thread::Options::Options(Options&& other) = default;
+Thread::Options::Options(Options&& other)
+    : message_pump_type(std::move(other.message_pump_type)),
+      delegate(std::move(other.delegate)),
+      timer_slack(std::move(other.timer_slack)),
+      task_queue_time_domain(std::move(other.task_queue_time_domain)),
+      message_pump_factory(std::move(other.message_pump_factory)),
+      stack_size(std::move(other.stack_size)),
+      priority(std::move(other.priority)),
+      joinable(std::move(other.joinable)) {
+  other.moved_from_ = true;
+}
 
 Thread::Options::~Options() = default;
 
@@ -142,6 +153,7 @@ bool Thread::Start() {
 }
 
 bool Thread::StartWithOptions(const Options& options) {
+  DCHECK(options.IsValid());
   DCHECK(owning_sequence_checker_.CalledOnValidSequence());
   DCHECK(!delegate_);
   DCHECK(!IsRunning());
