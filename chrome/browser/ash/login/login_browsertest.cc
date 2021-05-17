3e92e05104aeb237d6c0afa74aa91cbeedb1b993
Danila Kuzmin <dkuzmin@google.com>
2021-05-17 12:03:17 +0000

    OfflineLogin: fix flaky browsertest for back button
    Bug: 1197090
    Change-Id: I0f81e7e6760625cb8a82d39b8ddab68978bf4d67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2891580
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/master@{#883436}

--- chrome/browser/ash/login/login_browsertest.cc
+++ chrome/browser/ash/login/login_browsertest.cc
@@ -332,7 +332,7 @@ IN_PROC_BROWSER_TEST_F(LoginOfflineManagedTest, FullEmailDontMatchProvided) {
   TestSystemTrayIsVisible();
 }
 
-IN_PROC_BROWSER_TEST_F(LoginOfflineManagedTest, DISABLED_BackButtonTest) {
+IN_PROC_BROWSER_TEST_F(LoginOfflineManagedTest, BackButtonTest) {
   std::string domain = gaia::ExtractDomainName(managed_user_id_.GetUserEmail());
 
   ConfigurePolicy(domain);
@@ -343,6 +343,9 @@ IN_PROC_BROWSER_TEST_F(LoginOfflineManagedTest, DISABLED_BackButtonTest) {
                                              LoginManagerTest::kPassword);
 
   test::OobeJS().ClickOnPath(kOfflineLoginBackButton);
+  OobeScreenWaiter(GaiaView::kScreenId).Wait();
+  // Send network state update once again.
+  offline_login_test_mixin_.GoOffline();
   OobeScreenWaiter(ErrorScreenView::kScreenId).Wait();
   EXPECT_TRUE(ash::LoginScreenTestApi::IsOobeDialogVisible());
 }
