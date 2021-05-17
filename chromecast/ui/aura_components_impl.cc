095680e7c432c62965c8871e8ad4b5823f40872a
Lei Zhang <thestig@chromium.org>
2021-05-17 20:11:09 +0000

    Avoid a C++98 incompatibility warning in chromecast code.
    Not sure why this is not showing up on bots, but it happened in my local
    build.
    
    Change-Id: Idda5633c8753260573b1710edc03dd4ba8d8d958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2898480
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883585}

--- chromecast/ui/aura_components_impl.cc
+++ chromecast/ui/aura_components_impl.cc
@@ -18,7 +18,7 @@ AuraComponentsImpl::AuraComponentsImpl(CastWindowManager* cast_window_manager)
 #endif
 }
 
-AuraComponentsImpl::~AuraComponentsImpl(){};
+AuraComponentsImpl::~AuraComponentsImpl() = default;
 
 // static
 std::unique_ptr<AuraComponents> AuraComponents::Create(
