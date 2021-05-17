d86e2abbd65bbfdfc73cbc783c76b0fb295ea079
Xianzhu Wang <wangxianzhu@chromium.org>
2021-05-17 22:00:28 +0000

    Improve cached subsequence by using memcpy/memset
    Now instead of moving each display item in a loop, use memcpy/memset
    to move all display items in the subsequence.
    
    This improves performance of subsequence moving by 7%:
    https://ct.skia.org/results/cluster-telemetry/tasks/chromium_perf_runs/wangxianzhu-ChromiumPerf-5809/html/index.html
    record_time measures forced paint with all subsequences cached, so it
    measures the performance of moving the top-level subsequence.
    
    The improvement for other record_time_* benchmarks is much smaller
    because subsequence movement is only a small part of the total cost,
    but the impact of this change will be bigger if we reduce other cost.
    
    Bug: 917911
    Change-Id: Ib9ff7d3067363deb2a3cca0096a3718d3e42d285
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2881123
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883646}

--- third_party/blink/renderer/platform/graphics/paint/paint_controller.h
+++ third_party/blink/renderer/platform/graphics/paint/paint_controller.h
@@ -287,7 +287,6 @@ class PLATFORM_EXPORT PaintController {
   void ProcessNewItem(DisplayItem&);
 
   void CheckNewItem(DisplayItem&);
-  DisplayItem& MoveItemFromCurrentListToNewList(wtf_size_t);
   void CheckNewChunk();
 
   struct IdAsHashKey {
@@ -344,7 +343,6 @@ class PLATFORM_EXPORT PaintController {
                                  wtf_size_t subsequence_index,
                                  wtf_size_t start_chunk_index,
                                  wtf_size_t end_chunk_index);
-  void AppendChunkByMoving(PaintChunk&&);
 
   // Resets the indices (e.g. next_item_to_match_) of
   // current_paint_artifact_.GetDisplayItemList() to their initial values. This
