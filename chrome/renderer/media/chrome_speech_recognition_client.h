4846a0c72cbf9d0aec6dbe3793b4ff3f6ea4308f
evliu <evliu@google.com>
2021-05-17 22:07:38 +0000

    Prevent the ChromeSpeechRecognitionClient from initializing after the destruction of the RenderFrame
    The ChromeSpeechRecognitionClient is failing to initialize due to a crash when using the render frame to bind the interfaces for ~0.004% of streams. The comment in content/public/renderer/render_frame_observer.h says that the RenderFrameObserver must always null-check each call to render_frame() because the RenderFrame can go away at any time, suggesting that the cause of the crash is due to the RenderFrame being destroyed before the ChromeSpeechRecognitionClient is initialized. This CL updates the ChromeSpeechRecognitionClient to only initialize if the render frame has not been destroyed.
    
    Alternatives considered:
    - Bind interfaces in the constructor: Binding the interface in the constructor hasn't caused any crashes, but the ChromeSpeechRecognitionClient is created for all media streams and only a small percentage of them eventually enable the Live Caption feature.
    
    - Delete the ChromeSpeechRecognitionClient on the destruction of the RenderFrame: the ChromeSpeechRecognitionClient only needs the RenderFrame to bind the mojo pipes, so it might not be necesssary to directly tie the lifetime of the client to the RenderFrame. The ChromeSpeechRecognitionClient is owned via unique ptr by the DefaultRendererFactory.
    
    Bug: 1206043
    Change-Id: Ia1803598539910b8725e377aca129cdbcee284eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2900921
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#883654}

--- chrome/renderer/media/chrome_speech_recognition_client.h
+++ chrome/renderer/media/chrome_speech_recognition_client.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_set.h"
 #include "base/memory/weak_ptr.h"
+#include "content/public/renderer/render_frame_observer.h"
 #include "media/base/audio_buffer.h"
 #include "media/base/speech_recognition_client.h"
 #include "media/mojo/common/audio_data_s16_converter.h"
@@ -23,7 +24,8 @@ class RenderFrame;
 }  // namespace content
 
 class ChromeSpeechRecognitionClient
-    : public media::SpeechRecognitionClient,
+    : public content::RenderFrameObserver,
+      public media::SpeechRecognitionClient,
       public media::mojom::SpeechRecognitionBrowserObserver,
       public media::AudioDataS16Converter {
  public:
@@ -38,6 +40,9 @@ class ChromeSpeechRecognitionClient
   ChromeSpeechRecognitionClient& operator=(
       const ChromeSpeechRecognitionClient&) = delete;
   ~ChromeSpeechRecognitionClient() override;
+
+  // content::RenderFrameObserver
+  void OnDestruct() override;
 
   // media::SpeechRecognitionClient
   void AddAudio(scoped_refptr<media::AudioBuffer> buffer) override;
@@ -76,8 +81,6 @@ class ChromeSpeechRecognitionClient
   // recognizer is disconnected. Sends an error message to the UI and halts
   // future transcriptions.
   void OnRecognizerDisconnected();
-
-  content::RenderFrame* render_frame_;
 
   ChromeSpeechRecognitionClient::InitializeCallback initialize_callback_;
 
