a30c74c610f662e167d405ff738ab6424167bd0e
Callistus <callistus@google.com>
2021-05-17 23:47:58 +0000

    Create new chrome-untrusted:// source for the Help app magazine.
    To create the source, I do the following:-
    - Add help_app_kids_magazine_bundle_resources.grd to help app CIPD
    - Add "chrome-untrusted://help-app-kids-magazine" to URL constants
    - Add GRIT definition for help_app_kids_magazine_bundle_resources
    - Add mock index.html and grd for help app kid's magazine.
    
    Bug: b/188006047
    Change-Id: Ib63b8b12213052d27c62da4094209a20cadd3fb5
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2893526
    Reviewed-by: dstockwell <dstockwell@google.com>
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Cr-Commit-Position: refs/heads/master@{#883711}

--- chromeos/components/help_app_ui/url_constants.h
+++ chromeos/components/help_app_ui/url_constants.h
@@ -11,6 +11,7 @@ namespace chromeos {
 extern const char kChromeUIHelpAppHost[];
 extern const char kChromeUIHelpAppURL[];
 extern const char kChromeUIHelpAppUntrustedURL[];
+extern const char kChromeUIHelpAppKidsMagazineUntrustedURL[];
 
 }  // namespace chromeos
 
