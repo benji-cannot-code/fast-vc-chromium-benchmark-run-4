c16d140a2f09b06b92bfe8006563b17825df4bdc
Austin Sullivan <asully@chromium.org>
2021-05-17 19:58:37 +0000

    Clipboard: Re-enable bitmap tests for Android
    It is unclear what the root cause was, but these tests no longer fail
    on Android.
    
    Bug: 1056650
    Change-Id: I6ffe42fba46dc77ee154b04cd8328db9caf9f156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2897418
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883580}

--- ui/base/clipboard/clipboard_test_template.h
+++ ui/base/clipboard/clipboard_test_template.h
@@ -577,10 +577,6 @@ static void TestBitmapWrite(Clipboard* clipboard,
   }
 }
 
-#if !defined(OS_ANDROID)
-// TODO(https://crbug.com/1056650): Re-enable these tests after fixing the root
-// cause. This test only fails on Android.
-
 // Only kN32_SkColorType bitmaps are allowed in the clipboard to prevent
 // surprising buffer overflows due to bits-per-pixel assumptions.
 TYPED_TEST(ClipboardTest, Bitmap_F16_Premul) {
@@ -616,7 +612,6 @@ TYPED_TEST(ClipboardTest, Bitmap_N32_Premul_2x7) {
   };
   TestBitmapWrite(&this->clipboard(), SkImageInfo::MakeN32Premul(2, 7), b, b);
 }
-#endif  // !defined(OS_ANDROID)
 
 }  // namespace
 
