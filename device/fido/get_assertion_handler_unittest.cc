932389a367b06bd32d4dbd30f61e5017afdb9aac
Adam Langley <agl@chromium.org>
2021-05-17 20:49:15 +0000

    Revert "webauthn: (nearly) always probe."
    This reverts commit b381f0fbaa2123c843dff4b57981cb7d0e33f52a.
    
    I'm not comfortable enough with this change for M92. We'll try again later.
    
    Original change's description:
    > webauthn: (nearly) always probe.
    >
    > Previously assertions where the credential ID list would fit into a
    > single batch didn't probe. But we want to learn quickly whether any USB
    > devices have responsive credentials so that we can show a better UI.
    >
    > Therefore (nearly) always probe. The only exceptions are devices that
    > don't support probing, which is currently just caBLE devices.
    >
    > Change-Id: I464d2d69942d5be285b44016886f26e1b65fb9d5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2798759
    > Auto-Submit: Adam Langley <agl@chromium.org>
    > Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    > Reviewed-by: Shengfa Lin <shengfa@google.com>
    > Commit-Queue: Adam Langley <agl@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#874462}
    
    Change-Id: I78961e2c3a190a3b19eddeb4bfdd270f36666b1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2900762
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Shengfa Lin <shengfa@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883606}

--- device/fido/get_assertion_handler_unittest.cc
+++ device/fido/get_assertion_handler_unittest.cc
@@ -211,7 +211,7 @@ TEST_F(FidoGetAssertionHandlerTest, CtapRequestOnSingleDevice) {
   auto request_handler = CreateGetAssertionHandlerCtap();
   discovery()->WaitForCallToStartAndSimulateSuccess();
   auto device = MockFidoDevice::MakeCtapWithGetInfoExpectation();
-  device->ExpectCtap2CommandAndRespondRepeatedlyWith(
+  device->ExpectCtap2CommandAndRespondWith(
       CtapRequestCommand::kAuthenticatorGetAssertion,
       test_data::kTestGetAssertionResponse);
 
@@ -336,7 +336,7 @@ TEST_F(FidoGetAssertionHandlerTest, ValidEmptyCredential) {
   // Resident Keys must be disabled, otherwise allow list check is skipped.
   auto device = MockFidoDevice::MakeCtapWithGetInfoExpectation(
       test_data::kTestGetInfoResponseWithoutResidentKeySupport);
-  device->ExpectCtap2CommandAndRespondRepeatedlyWith(
+  device->ExpectCtap2CommandAndRespondWith(
       CtapRequestCommand::kAuthenticatorGetAssertion,
       test_data::kTestGetAssertionResponseWithEmptyCredential);
   discovery()->AddDevice(std::move(device));
@@ -363,7 +363,7 @@ TEST_F(FidoGetAssertionHandlerTest, TruncatedUTF8) {
   discovery()->WaitForCallToStartAndSimulateSuccess();
   auto device = MockFidoDevice::MakeCtapWithGetInfoExpectation(
       test_data::kTestCtap2OnlyAuthenticatorGetInfoResponse);
-  device->ExpectCtap2CommandAndRespondRepeatedlyWith(
+  device->ExpectCtap2CommandAndRespondWith(
       CtapRequestCommand::kAuthenticatorGetAssertion,
       test_data::kTestGetAssertionResponseWithTruncatedUTF8);
   discovery()->AddDevice(std::move(device));
@@ -518,7 +518,7 @@ TEST_F(FidoGetAssertionHandlerTest, SuccessWithOnlyUsbTransportAllowed) {
       CreateGetAssertionHandlerWithRequest(std::move(request));
 
   auto device = MockFidoDevice::MakeCtapWithGetInfoExpectation();
-  device->ExpectCtap2CommandAndRespondRepeatedlyWith(
+  device->ExpectCtap2CommandAndRespondWith(
       CtapRequestCommand::kAuthenticatorGetAssertion,
       test_data::kTestGetAssertionResponse);
   discovery()->WaitForCallToStartAndSimulateSuccess();
@@ -551,7 +551,7 @@ TEST_F(FidoGetAssertionHandlerTest, SuccessWithOnlyNfcTransportAllowed) {
 
   auto device = MockFidoDevice::MakeCtapWithGetInfoExpectation();
   device->SetDeviceTransport(FidoTransportProtocol::kNearFieldCommunication);
-  device->ExpectCtap2CommandAndRespondRepeatedlyWith(
+  device->ExpectCtap2CommandAndRespondWith(
       CtapRequestCommand::kAuthenticatorGetAssertion,
       test_data::kTestGetAssertionResponse);
   nfc_discovery()->WaitForCallToStartAndSimulateSuccess();
@@ -589,7 +589,7 @@ TEST_F(FidoGetAssertionHandlerTest, SuccessWithOnlyInternalTransportAllowed) {
   device->ExpectCtap2CommandAndRespondWith(
       CtapRequestCommand::kAuthenticatorGetInfo,
       test_data::kTestGetInfoResponsePlatformDevice);
-  device->ExpectCtap2CommandAndRespondRepeatedlyWith(
+  device->ExpectCtap2CommandAndRespondWith(
       CtapRequestCommand::kAuthenticatorGetAssertion,
       test_data::kTestGetAssertionResponse);
   platform_discovery()->WaitForCallToStartAndSimulateSuccess();
@@ -697,7 +697,7 @@ TEST_F(FidoGetAssertionHandlerTest, DeviceFailsImmediately) {
 
                 auto working_device =
                     MockFidoDevice::MakeCtapWithGetInfoExpectation();
-                working_device->ExpectCtap2CommandAndRespondRepeatedlyWith(
+                working_device->ExpectCtap2CommandAndRespondWith(
                     CtapRequestCommand::kAuthenticatorGetAssertion,
                     test_data::kTestGetAssertionResponse);
                 discovery()->AddDevice(std::move(working_device));
--- third_party/blink/web_tests/http/tests/inspector-protocol/webauthn/webauthn-get-credentials.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/webauthn/webauthn-get-credentials.js
@@ -51,8 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     transports: ["usb", "ble", "nfc"],
   })`));
 
-  // Sign count should be increased by two for |nonResidentCredential|: once
-  // for the probe request and once for the actual request.
+  // Sign count should be increased by one for |nonResidentCredential|.
   credentials = (await dp.WebAuthn.getCredentials({authenticatorId})).result.credentials;
   testRunner.log(credentials.find(
       cred => cred.credentialId === nonResidentCredential.credentialId).signCount);
