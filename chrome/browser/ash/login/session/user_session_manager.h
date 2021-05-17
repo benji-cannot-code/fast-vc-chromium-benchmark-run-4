01af13c3631a8fd494934e5d7eaa66a87bc7b718
Pavol Marko <pmarko@google.com>
2021-05-17 22:02:02 +0000

    Explicitly trigger loading shill profile using LoadShillProfile
    If a network configuration mandates re-using the login password of the
    user, the user shill profile should only be loaded after the login
    password has been saved in SessionManager (b/183084821).
    
    CL:2891561 / CL:2891562 introduced a SessionManager D-Bus call to
    trigger loading of the shill profile.
    This CL also adds a browsertest to ensure that this call is now invoked
    for both managed (with and without a relevant network policy) and
    unmanaged users.
    
    This change will become relevant when CL:2891563 lands which will
    actually tie loading of the shill user profile to the new signal
    (triggered through LoadShillProfile) instead of being a side effect of
    the user session start.
    
    Bug: b:183084821
    Test: browser_tests and manual with the CL:2891563 - login and check \
    that user shill profile has been loaded
    
    Change-Id: I7d65d3788db3020946c2a33c686483af1b91deae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2891583
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883648}

--- chrome/browser/ash/login/session/user_session_manager.h
+++ chrome/browser/ash/login/session/user_session_manager.h
@@ -542,6 +542,10 @@ class UserSessionManager
 
   void OnUserEligibleForOnboardingSurvey(Profile* profile);
 
+  // Triggers loading of the shill profile for |account_id|. This should only be
+  // called for the primary user session.
+  void LoadShillProfile(const AccountId& account_id);
+
   UserSessionManagerDelegate* delegate_;
 
   // Used to listen to network changes.
