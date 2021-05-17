d17a142c7279e14951288e1a529908eca026979e
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-05-17 09:32:02 +0000

    Remove shallow functions in ios/c/b/ui/settings/sync/utils/sync_util.h
    These functions don't provide any useful abstraction. Inline them.
    No behavior is changed.
    
    R=droger@chromium.org
    
    Bug: None
    Change-Id: I076938c5ab461a2787fa1c5d6024c6606110c445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2891653
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883406}

--- ios/chrome/browser/ui/settings/sync/utils/sync_error_infobar_delegate.mm
+++ ios/chrome/browser/ui/settings/sync/utils/sync_error_infobar_delegate.mm
@@ -94,7 +94,7 @@ gfx::Image SyncErrorInfoBarDelegate::GetIcon() const {
 }
 
 bool SyncErrorInfoBarDelegate::Accept() {
-  if (ShouldShowSyncSignin(error_state_)) {
+  if (error_state_ == SyncSetupService::kSyncServiceSignInNeedsUpdate) {
     [presenter_ showReauthenticateSignin];
   } else if (ShouldShowSyncSettings(error_state_)) {
     if (signin::IsMobileIdentityConsistencyEnabled()) {
@@ -102,9 +102,10 @@ bool SyncErrorInfoBarDelegate::Accept() {
     } else {
       [presenter_ showGoogleServicesSettings];
     }
-  } else if (ShouldShowSyncPassphraseSettings(error_state_)) {
+  } else if (error_state_ == SyncSetupService::kSyncServiceNeedsPassphrase) {
     [presenter_ showSyncPassphraseSettings];
-  } else if (ShouldShowTrustedVaultReauthentication(error_state_)) {
+  } else if (error_state_ ==
+             SyncSetupService::kSyncServiceNeedsTrustedVaultKey) {
     [presenter_ showTrustedVaultReauthenticationWithRetrievalTrigger:
                     syncer::KeyRetrievalTriggerForUMA::kNewTabPageInfobar];
   }
