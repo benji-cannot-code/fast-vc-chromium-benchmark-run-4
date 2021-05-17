d9b3a393bb275fcb8b461c7bf4dd0372338b6c24
dpapad <dpapad@chromium.org>
2021-05-17 21:17:08 +0000

    Typescript: Copy checked-in ui/webui/resources/ .d.ts  files into gen/.
    This makes it easier for TSC to locate such files, matching where
    auto-generated .d.ts files are located, and requiring one less path
    mapping in tsconfig.json.
    
    Allows .d.ts files to hold relative paths, instead of being forced to
    use chrome://resources/ absolute paths.
    
    Allows checked-in d.ts files to be picked up when auto-generating
    other .d.ts files, which fixes weird cases of incorrect stale files
    like crbug.com/1209703.
    
    It also results in fewer errors surfacing during migration, when some
    ui/webui/resources/ deps don't have .d.ts files yet, by not throwing
    bogus errors for transitive deps that already have .d.ts.
    
    Bug: 1189595,1209703
    Change-Id: I9df3dadce4cfce0e58f320240f612b4178ca12a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2897013
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883617}

--- ui/webui/resources/js/cr/ui/focus_row_behavior.m.d.ts
+++ ui/webui/resources/js/cr/ui/focus_row_behavior.m.d.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {FocusRow} from 'chrome://resources/js/cr/ui/focus_row.m.js';
+import {FocusRow} from './focus_row.m.js';
 
 export interface FocusRowBehaviorInterface {
   id: string|null|undefined;
