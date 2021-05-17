b502b6f74071dab4c104778143c23e8796836e6b
sandromaggi <sandromaggi@google.com>
2021-05-17 17:09:22 +0000

    [Autofill Assistant] Add option comparison for RequiredField
    This adds option comparison attribute and value expression to the
    RequiredField. This is used if defined, if not it behaves as before.
    
    Note: The default is a "contains" match (JS: RegExp.test).
    
    Bug: b/186190903
    Change-Id: I0860dd62892a03896633d29d0643b31c3b8ba25c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2851755
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#883502}

--- components/autofill_assistant/browser/actions/fallback_handler/required_field_unittest.cc
+++ components/autofill_assistant/browser/actions/fallback_handler/required_field_unittest.cc
@@ -16,10 +16,27 @@ class RequiredFieldTest : public testing::Test {
   void SetUp() override {}
 };
 
+TEST_F(RequiredFieldTest, HasValue) {
+  RequiredField required_field;
+  EXPECT_FALSE(required_field.HasValue());
+
+  required_field.proto.mutable_value_expression()->add_chunk()->set_text(
+      "value");
+  EXPECT_TRUE(required_field.HasValue());
+
+  required_field.proto.mutable_value_expression()->clear_chunk();
+  ValueExpressionRegexp value_expression_re2;
+  value_expression_re2.mutable_value_expression()->add_chunk()->set_text("^$");
+  *required_field.proto.mutable_option_comparison_value_expression_re2() =
+      value_expression_re2;
+  EXPECT_TRUE(required_field.HasValue());
+}
+
 TEST_F(RequiredFieldTest, ShouldFallbackForNotEmpty) {
   RequiredField required_field;
   required_field.status = RequiredField::NOT_EMPTY;
-  required_field.value_expression.add_chunk()->set_text("value");
+  required_field.proto.mutable_value_expression()->add_chunk()->set_text(
+      "value");
 
   EXPECT_FALSE(required_field.ShouldFallback(true));
   EXPECT_FALSE(required_field.ShouldFallback(false));
@@ -36,7 +53,8 @@ TEST_F(RequiredFieldTest, ShouldFallbackForNotEmptyToBeCleared) {
 TEST_F(RequiredFieldTest, ShouldFallbackForEmpty) {
   RequiredField required_field;
   required_field.status = RequiredField::EMPTY;
-  required_field.value_expression.add_chunk()->set_text("value");
+  required_field.proto.mutable_value_expression()->add_chunk()->set_text(
+      "value");
 
   EXPECT_TRUE(required_field.ShouldFallback(true));
   EXPECT_TRUE(required_field.ShouldFallback(false));
@@ -44,9 +62,10 @@ TEST_F(RequiredFieldTest, ShouldFallbackForEmpty) {
 
 TEST_F(RequiredFieldTest, ShouldFallbackForNotEmptyForced) {
   RequiredField required_field;
-  required_field.forced = true;
+  required_field.proto.set_forced(true);
   required_field.status = RequiredField::NOT_EMPTY;
-  required_field.value_expression.add_chunk()->set_text("value");
+  required_field.proto.mutable_value_expression()->add_chunk()->set_text(
+      "value");
 
   EXPECT_TRUE(required_field.ShouldFallback(true));
   EXPECT_FALSE(required_field.ShouldFallback(false));
@@ -55,7 +74,8 @@ TEST_F(RequiredFieldTest, ShouldFallbackForNotEmptyForced) {
 TEST_F(RequiredFieldTest, ShouldFallbackForEmptyWithClick) {
   RequiredField required_field;
   required_field.status = RequiredField::EMPTY;
-  required_field.fallback_click_element = Selector({"#element"});
+  *required_field.proto.mutable_option_element_to_click() =
+      ToSelectorProto("#element");
 
   EXPECT_TRUE(required_field.ShouldFallback(true));
   EXPECT_FALSE(required_field.ShouldFallback(false));
@@ -63,11 +83,36 @@ TEST_F(RequiredFieldTest, ShouldFallbackForEmptyWithClick) {
 
 TEST_F(RequiredFieldTest, ShouldFallbackForEmptyOptional) {
   RequiredField required_field;
-  required_field.optional = true;
+  required_field.proto.set_is_optional(true);
   required_field.status = RequiredField::EMPTY;
-  required_field.value_expression.add_chunk()->set_text("value");
+  required_field.proto.mutable_value_expression()->add_chunk()->set_text(
+      "value");
 
   EXPECT_TRUE(required_field.ShouldFallback(true));
+  EXPECT_FALSE(required_field.ShouldFallback(false));
+}
+
+TEST_F(RequiredFieldTest, ShouldFallbackForEmptyWithOptionComparison) {
+  RequiredField required_field;
+  required_field.status = RequiredField::EMPTY;
+  ValueExpressionRegexp value_expression_re2;
+  value_expression_re2.mutable_value_expression()->add_chunk()->set_text("^$");
+  *required_field.proto.mutable_option_comparison_value_expression_re2() =
+      value_expression_re2;
+
+  EXPECT_TRUE(required_field.ShouldFallback(true));
+  EXPECT_TRUE(required_field.ShouldFallback(false));
+}
+
+TEST_F(RequiredFieldTest, ShouldFallbackForNotEmptyWithOptionComparison) {
+  RequiredField required_field;
+  required_field.status = RequiredField::NOT_EMPTY;
+  ValueExpressionRegexp value_expression_re2;
+  value_expression_re2.mutable_value_expression()->add_chunk()->set_text("^$");
+  *required_field.proto.mutable_option_comparison_value_expression_re2() =
+      value_expression_re2;
+
+  EXPECT_FALSE(required_field.ShouldFallback(true));
   EXPECT_FALSE(required_field.ShouldFallback(false));
 }
 
