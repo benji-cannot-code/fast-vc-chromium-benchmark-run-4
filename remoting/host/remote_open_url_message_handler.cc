4417a265ab39b7a58acba4d197519759aa959d44
Yuwei Huang <yuweih@chromium.org>
2021-05-17 22:41:03 +0000

    [remoting][linux host] Allow client to enable/disable URL forwarder
    This CL makes the host enable the URL forwarder when the data channel is
    connected, and disable the URL forwarder when it is disconnected, which
    are done by making the host execute the setup script.
    
    Companion client CL: cl/373475255
    
    Bug: b:183135000
    Change-Id: Id16558e3cbe797734878c18bee95b398e3dfe51a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2893599
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883678}

--- remoting/host/remote_open_url_message_handler.cc
+++ remoting/host/remote_open_url_message_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/logging.h"
 #include "remoting/base/compound_buffer.h"
+#include "remoting/host/scoped_url_forwarder.h"
 #include "remoting/protocol/message_serialization.h"
 #include "url/gurl.h"
 
@@ -51,6 +52,7 @@ RemoteOpenUrlMessageHandler::~RemoteOpenUrlMessageHandler() {
 void RemoteOpenUrlMessageHandler::OnConnected() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
+  scoped_url_forwarder_ = ScopedUrlForwarder::Create();
   ipc_server_.StartServer();
 }
 
@@ -79,6 +81,7 @@ void RemoteOpenUrlMessageHandler::OnIncomingMessage(
 void RemoteOpenUrlMessageHandler::OnDisconnecting() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
+  scoped_url_forwarder_.reset();
   ipc_server_.StopServer();
 
   // The remote connection is going away, so inform all IPC clients to open the
--- remoting/host/remote_open_url_message_handler.h
+++ remoting/host/remote_open_url_message_handler.h
@@ -20,6 +20,8 @@ class GURL;
 
 namespace remoting {
 
+class ScopedUrlForwarder;
+
 class RemoteOpenUrlMessageHandler final
     : public mojom::RemoteUrlOpener,
       public protocol::NamedMessagePipeHandler {
@@ -47,6 +49,8 @@ class RemoteOpenUrlMessageHandler final
 
   MojoIpcServer<mojom::RemoteUrlOpener> ipc_server_{
       GetRemoteOpenUrlIpcChannelName(), 0, this};
+
+  std::unique_ptr<ScopedUrlForwarder> scoped_url_forwarder_;
 
   static_assert(
       std::is_same<
