ed1fb988aeec49945662c060bbe126adfb4795ef
Marc Treib <treib@chromium.org>
2021-05-17 12:17:17 +0000

    Revert "Delay load event waiting for object/embed to trigger fetch"
    This reverts commit 1df1a4b91a4e8cf4e0ed15b0baca738520f137a5.
    
    Reason for revert: Suspected of breaking external/wpt/css/css-backgrounds/background-image-first-[letter,line].html, see crbug.com/1209795
    
    Original change's description:
    > Delay load event waiting for object/embed to trigger fetch
    >
    > The HTML object and embed elements should fetch resources depending on
    > "being rendered" and delay the load event for the document. Since we do
    > not know if they are being rendered until we do an
    > UpdateStyleAndLayoutTree, we need to delay the load event until an
    > UpdateStyleAndLayoutTree has been invoked when an object/embed is
    > inserted into the document.
    >
    > This has been handled by doing a call to UpdateStyleAndLayoutTree when
    > the parsing finishes in Document. However, that meant updating
    > style/layout tree based on incomplete styling information when waiting
    > for styles to load.
    >
    > This CL skips the forced style and layout tree updates if we are waiting
    > for render blocking stylesheets on parsing finished, and block the load
    > event until an UpdateStyleAndLayoutTree has been run as part of the
    > lifecycle update or a forced update via scripts.
    >
    > Bug: 332189
    > Change-Id: I76dc7b38817af44da6afe16aac6661ae8364edb4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2871469
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#883260}
    
    Bug: 332189, 1209795
    Change-Id: Ifafc3377e4c7f4837aba8efb66671a9d3c6563a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2897158
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Owners-Override: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883440}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -2152,8 +2152,6 @@ void Document::UpdateStyleAndLayoutTreeForThisDocument() {
   // Make sure that document.fonts.ready fires, if appropriate.
   FontFaceSetDocument::DidLayout(*this);
 
-  UnblockLoadEventAfterLayoutTreeUpdate();
-
   TRACE_EVENT_END1("blink,devtools.timeline", "UpdateLayoutTree",
                    "elementCount", element_count);
 
@@ -3480,8 +3478,7 @@ void Document::ImplicitClose() {
 
   fetcher_->ScheduleWarnUnusedPreloads();
 
-  if (GetStyleEngine().HaveRenderBlockingStylesheetsLoaded())
-    UpdateStyleAndLayout(DocumentUpdateReason::kUnknown);
+  UpdateStyleAndLayout(DocumentUpdateReason::kUnknown);
 
   load_event_progress_ = kLoadEventCompleted;
 
@@ -6646,10 +6643,8 @@ void Document::FinishedParsing() {
     // the window load event too early.  To avoid this we force the styles to be
     // up to date before calling FrameLoader::finishedParsing().  See
     // https://bugs.webkit.org/show_bug.cgi?id=36864 starting around comment 35.
-    if (!is_initial_empty_document_ &&
-        GetStyleEngine().HaveRenderBlockingStylesheetsLoaded()) {
+    if (!is_initial_empty_document_)
       UpdateStyleAndLayoutTree();
-    }
 
     BeginLifecycleUpdatesIfRenderingReady();
 
@@ -8218,20 +8213,6 @@ void Document::RunPostPrerenderingActivationSteps() {
 bool Document::InStyleRecalc() const {
   return lifecycle_.GetState() == DocumentLifecycle::kInStyleRecalc ||
          style_engine_->InContainerQueryStyleRecalc();
-}
-
-void Document::DelayLoadEventUntilLayoutTreeUpdate() {
-  if (delay_load_event_until_layout_tree_update_)
-    return;
-  delay_load_event_until_layout_tree_update_ = true;
-  IncrementLoadEventDelayCount();
-}
-
-void Document::UnblockLoadEventAfterLayoutTreeUpdate() {
-  if (delay_load_event_until_layout_tree_update_) {
-    delay_load_event_until_layout_tree_update_ = false;
-    DecrementLoadEventDelayCount();
-  }
 }
 
 Document::PaintPreviewScope::PaintPreviewScope(Document& document,
