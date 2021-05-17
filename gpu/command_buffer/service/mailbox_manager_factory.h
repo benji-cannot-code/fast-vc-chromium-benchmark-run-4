7638897725eaa02addf7a14f08452b31bdb4dd8e
Stephan Hartmann <stha09@googlemail.com>
2021-05-17 20:43:19 +0000

    IWYU: include memory for std::unique_ptr
    Bug: None
    Change-Id: I818c15370a7a4f7ed1df805e3c543fb13f4ce1d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2896972
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#883602}

--- gpu/command_buffer/service/mailbox_manager_factory.h
+++ gpu/command_buffer/service/mailbox_manager_factory.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_COMMAND_BUFFER_SERVICE_MAILBOX_MANAGER_FACTORY_H_
 #define GPU_COMMAND_BUFFER_SERVICE_MAILBOX_MANAGER_FACTORY_H_
 
+#include <memory>
+
 #include "gpu/command_buffer/service/mailbox_manager.h"
 #include "gpu/gpu_gles2_export.h"
 
