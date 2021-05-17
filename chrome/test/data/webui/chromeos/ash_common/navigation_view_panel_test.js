cc8b8713d9b58cbc2ca016fa274d7485262dfebd
Jimmy Gong <jimmyxgong@google.com>
2021-05-17 23:24:33 +0000

    [navigation_view] Add support for page notification events
    Child pages that are interested in being notified of the navigation
    changes can implement an optional function "onNavigationPageChanged()"
    to be notified of the event.
    
    To send events between child pages, the parent of
    <navigation-view-element> can call on "notifyEvent()" to notify all
    child pages of an event.
    
    Bug: 1205539
    Change-Id: If4c5e4fa0ea7343afe56eb7e217937e3acd5cbcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2895655
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883699}

--- chrome/test/data/webui/chromeos/ash_common/navigation_view_panel_test.js
+++ chrome/test/data/webui/chromeos/ash_common/navigation_view_panel_test.js
@@ -6,12 +6,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {SelectorItem} from 'chrome://resources/ash/common/navigation_selector.js';
 import {NavigationViewPanelElement} from 'chrome://resources/ash/common/navigation_view_panel.js';
 
-import {assertFalse, assertTrue} from '../../chai_assert.js';
+import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
 import {waitAfterNextRender} from '../../test_util.m.js';
 
 export function navigationViewPanelTestSuite() {
   /** @type {?NavigationViewPanelElement} */
   let viewElement = null;
+
+  /** @type {number} */
+  let eventCount = 0;
+  /** @type {!Object} */
+  let eventDetail = {};
+  /** @type {number} */
+  let numPageChangedCount = 0;
+  /** @type {string} */
+  let previousPage = '';
+  /** @type {string} */
+  let currentPage = '';
 
   setup(() => {
     viewElement =
@@ -23,7 +34,29 @@ export function navigationViewPanelTestSuite() {
   teardown(() => {
     viewElement.remove();
     viewElement = null;
+    eventCount = 0;
+    eventDetail = {};
+    numPageChangedCount = 0;
+    previousPage = '';
+    currentPage = '';
   });
+
+  /**
+   * @param {!Object} e
+   */
+  function handleEvent(e) {
+    eventDetail = e.detail;
+    eventCount++;
+  }
+
+  /**
+   * @param {!CustomEvent} e
+   */
+  function onNavigationPageChanged(e) {
+    previousPage = e.detail.previous;
+    currentPage = e.detail.current;
+    numPageChangedCount++;
+  }
 
   test('oneEntry', async () => {
     const dummyPage1 = 'dummy-page1';
@@ -44,6 +77,7 @@ export function navigationViewPanelTestSuite() {
     const dummyElement1 =
         viewElement.shadowRoot.querySelector(`#${dummyPage1}`);
     assertFalse(dummyElement1.hidden);
+    dummyElement1['onNavigationPageChanged'] = onNavigationPageChanged;
 
     // Click the second menu item. Expect that the dummyPage2 to be created and
     // not hidden. dummyPage1 should be hidden now.
@@ -51,8 +85,14 @@ export function navigationViewPanelTestSuite() {
     await waitAfterNextRender(viewElement);
     const dummyElement2 =
         viewElement.shadowRoot.querySelector(`#${dummyPage2}`);
+    dummyElement2['onNavigationPageChanged'] = onNavigationPageChanged;
     assertFalse(dummyElement2.hidden);
     assertTrue(dummyElement1.hidden);
+    // Only one page has implemented "onNavigationPageChanged" by the second
+    // navigation click, expect only one client to be notified.
+    assertEquals(1, numPageChangedCount);
+    assertEquals(dummyPage1, previousPage);
+    assertEquals(dummyPage2, currentPage);
 
     // Click the first menu item. Expect that dummyPage2 is now hidden and
     // dummyPage1 is not hidden.
@@ -60,6 +100,37 @@ export function navigationViewPanelTestSuite() {
     await waitAfterNextRender(viewElement);
     assertTrue(dummyElement2.hidden);
     assertFalse(dummyElement1.hidden);
+    // Now that both dummy pages have implemented "onNavigationPageChanged",
+    // the navigation click will trigger both page's methods.
+    assertEquals(3, numPageChangedCount);
+    assertEquals(dummyPage2, previousPage);
+    assertEquals(dummyPage1, currentPage);
+  });
+
+  test('notifyEvent', async () => {
+    const dummyPage1 = 'dummy-page1';
+
+    viewElement.addSelector('dummyPage1', dummyPage1);
+
+    await waitAfterNextRender(viewElement);
+
+    const sideNav = viewElement.shadowRoot.querySelector('navigation-selector');
+    const navElements = sideNav.shadowRoot.querySelectorAll('.navigation-item');
+
+    // Create the element.
+    navElements[0].click();
+    await waitAfterNextRender(viewElement);
+    const dummyElement = viewElement.shadowRoot.querySelector(`#${dummyPage1}`);
+
+    const functionName = 'onEventReceived';
+    const expectedDetail = 'test';
+    // Set the function handler for the element.
+    dummyElement[functionName] = handleEvent;
+    // Trigger notifyEvent and expect |dummyElement| to capture the event.
+    viewElement.notifyEvent(functionName, {detail: expectedDetail});
+
+    assertEquals(1, eventCount);
+    assertEquals(expectedDetail, eventDetail.detail);
   });
 
   test('defaultPage', async () => {
