09b4a352bb2c984548688b73030843020e2b70c4
Stephan Hartmann <stha09@googlemail.com>
2021-05-17 22:55:10 +0000

    IWYU: add vector for std::vector
    Bug: None
    Change-Id: Ia17ebedfadb5a46249c947346bdce17fb6dee08e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2896890
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#883687}

--- net/url_request/redirect_util.h
+++ net/url_request/redirect_util.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_URL_REQUEST_REDIRECT_UTIL_H_
 
 #include <string>
+#include <vector>
 
 #include "net/base/net_export.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
