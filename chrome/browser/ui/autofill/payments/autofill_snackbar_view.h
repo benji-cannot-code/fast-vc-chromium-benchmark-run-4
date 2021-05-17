e6d39a9c579f56ffd50b25f821c041b914e145e6
siashah <siashah@chromium.org>
2021-05-17 22:17:14 +0000

    [VirtualCards] Add snackbar implementation for showing after form fill
    This CL includes the wiring and the implementation needed to show a
    snackbar on Clank.
    Follow-ups:
    * Add triggering logic after form fill to instantiate the controller
    and show the snackbar.
    * Trigger manual fallback bottom sheet after snackbar is tapped.
    * Dismiss the snackbar if current tab leaves focus.
    * Add histograms for action clicks and dismisses.
    * Re-use the controller to show the snackbar on desktop.
    * Add tests for the controller.
    
    Implementation: https://screenshot.googleplex.com/4QuSGnkKLg5dxpG.png
    
    Bug: 1196021
    Change-Id: I865738c73ae014cf152638312a07db8786ec7b99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2885651
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883662}

---
+++ chrome/browser/ui/autofill/payments/autofill_snackbar_view.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_AUTOFILL_SNACKBAR_VIEW_H_
+#define CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_AUTOFILL_SNACKBAR_VIEW_H_
+
+#include "base/macros.h"
+
+namespace autofill {
+
+// The UI interface which shows a snackbar after a form is autofilled on
+// Android.
+class AutofillSnackbarView {
+ public:
+  virtual void Show() = 0;
+  virtual void Dismiss() = 0;
+
+  // Factory function for creating the view.
+  static AutofillSnackbarView* Create(AutofillSnackbarController* controller);
+
+ protected:
+  virtual ~AutofillSnackbarView() = default;
+};
+
+}  // namespace autofill
+
+#endif  // CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_AUTOFILL_SNACKBAR_VIEW_H_
