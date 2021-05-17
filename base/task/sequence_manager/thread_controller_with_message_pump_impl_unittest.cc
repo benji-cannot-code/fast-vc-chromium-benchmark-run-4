7b35c8f834b31bcf054058e3e02eb6b70a4f2ec6
Olivier Li <olivierli@chromium.org>
2021-05-17 18:31:47 +0000

    Support hang watching nested loops
    Bug: 1034046
    Change-Id: Iab597a55519310d1e2a90bb597b4c9a32e45ae11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2857195
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883537}

--- base/task/sequence_manager/thread_controller_with_message_pump_impl_unittest.cc
+++ base/task/sequence_manager/thread_controller_with_message_pump_impl_unittest.cc
@@ -50,8 +50,8 @@ class ThreadControllerForTest
   using ThreadControllerWithMessagePumpImpl::DoIdleWork;
   using ThreadControllerWithMessagePumpImpl::DoWork;
   using ThreadControllerWithMessagePumpImpl::EnsureWorkScheduled;
-  using ThreadControllerWithMessagePumpImpl::OnBeginNativeWork;
-  using ThreadControllerWithMessagePumpImpl::OnEndNativeWork;
+  using ThreadControllerWithMessagePumpImpl::OnBeginWorkItem;
+  using ThreadControllerWithMessagePumpImpl::OnEndWorkItem;
   using ThreadControllerWithMessagePumpImpl::Quit;
   using ThreadControllerWithMessagePumpImpl::Run;
 
@@ -989,15 +989,15 @@ TEST_F(ThreadControllerWithMessagePumpTest,
           thread_controller_.SetTaskExecutionAllowed(true);
           // i.e. simulate that something runs code within the scope of a
           // ScopedAllowApplicationTasksInNativeNestedLoop and ends up entering
-          // a nested native loop which would invoke OnBeginNativeWork()
+          // a nested native loop which would invoke OnBeginWorkItem()
 
           // D:
           EXPECT_CALL(*thread_controller_.trace_observer,
                       OnThreadControllerActiveBegin);
-          thread_controller_.OnBeginNativeWork();
+          thread_controller_.OnBeginWorkItem();
           testing::Mock::VerifyAndClearExpectations(
               &*thread_controller_.trace_observer);
-          thread_controller_.OnEndNativeWork();
+          thread_controller_.OnEndWorkItem();
 
           // E:
           EXPECT_CALL(tasks[1], Run());
@@ -1016,10 +1016,10 @@ TEST_F(ThreadControllerWithMessagePumpTest,
           // G:
           EXPECT_CALL(*thread_controller_.trace_observer,
                       OnThreadControllerActiveBegin);
-          thread_controller_.OnBeginNativeWork();
+          thread_controller_.OnBeginWorkItem();
           testing::Mock::VerifyAndClearExpectations(
               &*thread_controller_.trace_observer);
-          thread_controller_.OnEndNativeWork();
+          thread_controller_.OnEndWorkItem();
 
           // H:
           EXPECT_CALL(*thread_controller_.trace_observer,
@@ -1130,10 +1130,10 @@ TEST_F(ThreadControllerWithMessagePumpTest,
           // D:
           EXPECT_CALL(*thread_controller_.trace_observer,
                       OnThreadControllerActiveBegin);
-          thread_controller_.OnBeginNativeWork();
+          thread_controller_.OnBeginWorkItem();
           testing::Mock::VerifyAndClearExpectations(
               &*thread_controller_.trace_observer);
-          thread_controller_.OnEndNativeWork();
+          thread_controller_.OnEndWorkItem();
 
           // E:
           EXPECT_CALL(*thread_controller_.trace_observer,
@@ -1200,10 +1200,10 @@ TEST_F(ThreadControllerWithMessagePumpTest,
               EXPECT_CALL(*thread_controller_.trace_observer,
                           OnThreadControllerActiveBegin);
             }
-            thread_controller_.OnBeginNativeWork();
+            thread_controller_.OnBeginWorkItem();
             testing::Mock::VerifyAndClearExpectations(
                 &*thread_controller_.trace_observer);
-            thread_controller_.OnEndNativeWork();
+            thread_controller_.OnEndWorkItem();
 
             // E & H:
             thread_controller_.SetTaskExecutionAllowed(false);
@@ -1276,10 +1276,10 @@ TEST_F(ThreadControllerWithMessagePumpTest,
           // D:
           EXPECT_CALL(*thread_controller_.trace_observer,
                       OnThreadControllerActiveBegin);
-          thread_controller_.OnBeginNativeWork();
+          thread_controller_.OnBeginWorkItem();
           testing::Mock::VerifyAndClearExpectations(
               &*thread_controller_.trace_observer);
-          thread_controller_.OnEndNativeWork();
+          thread_controller_.OnEndWorkItem();
 
           // E:
           EXPECT_CALL(*thread_controller_.trace_observer,
@@ -1291,10 +1291,10 @@ TEST_F(ThreadControllerWithMessagePumpTest,
           // F:
           EXPECT_CALL(*thread_controller_.trace_observer,
                       OnThreadControllerActiveBegin);
-          thread_controller_.OnBeginNativeWork();
+          thread_controller_.OnBeginWorkItem();
           testing::Mock::VerifyAndClearExpectations(
               &*thread_controller_.trace_observer);
-          thread_controller_.OnEndNativeWork();
+          thread_controller_.OnEndWorkItem();
 
           // G:
           thread_controller_.SetTaskExecutionAllowed(false);
@@ -1445,12 +1445,12 @@ TEST_F(ThreadControllerWithMessagePumpTest,
             }));
 
         // B:
-        thread_controller_.OnBeginNativeWork();
+        thread_controller_.OnBeginWorkItem();
 
         // E:
         EXPECT_CALL(*thread_controller_.trace_observer,
                     OnThreadControllerActiveEnd);
-        thread_controller_.OnEndNativeWork();
+        thread_controller_.OnEndWorkItem();
 
         // F:
         EXPECT_CALL(*thread_controller_.trace_observer,
@@ -1530,12 +1530,12 @@ TEST_F(ThreadControllerWithMessagePumpTest,
             }));
 
         // B:
-        thread_controller_.OnBeginNativeWork();
+        thread_controller_.OnBeginWorkItem();
 
         // G:
         EXPECT_CALL(*thread_controller_.trace_observer,
                     OnThreadControllerActiveEnd);
-        thread_controller_.OnEndNativeWork();
+        thread_controller_.OnEndWorkItem();
 
         // H:
         EXPECT_CALL(*thread_controller_.trace_observer,
