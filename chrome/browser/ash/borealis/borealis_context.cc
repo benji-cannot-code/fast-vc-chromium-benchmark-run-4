b0303dcca6381488ed38a05130292c866d322f2b
danielng <danielng@google.com>
2021-05-17 08:49:35 +0000

    borealis: Implementing GetDiskInfo in the disk manager
    Implementing the GetDiskInfo function for the borealis disk manager.
    This provides the functionality to get information about the borealis
    disk and about the host device (in terms of available space).
    Usage is done at CL:2862302.
    
    Cq-Depend: 2859610
    Bug: b/174592560
    Change-Id: I20fa53e33c08d95ea292b997348e9c79a9c5749f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2859453
    Commit-Queue: Daniel Ng <danielng@google.com>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/master@{#883395}

--- chrome/browser/ash/borealis/borealis_context.cc
+++ chrome/browser/ash/borealis/borealis_context.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "base/scoped_observation.h"
+#include "chrome/browser/ash/borealis/borealis_disk_manager_impl.h"
 #include "chrome/browser/ash/borealis/borealis_engagement_metrics.h"
 #include "chrome/browser/ash/borealis/borealis_game_mode_controller.h"
 #include "chrome/browser/ash/borealis/borealis_metrics.h"
@@ -62,8 +63,8 @@ BorealisContext::BorealisContext(Profile* profile)
           std::make_unique<guest_os::GuestOsStabilityMonitor>(
               kBorealisStabilityHistogram)),
       game_mode_controller_(std::make_unique<BorealisGameModeController>()),
-      engagement_metrics_(
-          std::make_unique<BorealisEngagementMetrics>(profile)) {}
+      engagement_metrics_(std::make_unique<BorealisEngagementMetrics>(profile)),
+      disk_manager_(std::make_unique<BorealisDiskManagerImpl>(this)) {}
 
 std::unique_ptr<BorealisContext>
 BorealisContext::CreateBorealisContextForTesting(Profile* profile) {
