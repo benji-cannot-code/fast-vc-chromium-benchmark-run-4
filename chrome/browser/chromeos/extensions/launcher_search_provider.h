2b48df3b3531aa674e4accaa145b7f5bf4476bad
Rachel Wong <wrong@chromium.org>
2021-05-17 23:43:58 +0000

    Delete launcher search provider. [1/2]
    This is the first CL to deleting the launcher search provider, which is
    unused and should be removed for lacros. We will also delete the
    extension behind it in a follow-up CL.
    
    Bug: 1208731
    Change-Id: I995084d1a5505fb33512953f200c2cb452bda27b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2893886
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883709}

--- chrome/browser/chromeos/extensions/launcher_search_provider.h
+++ chrome/browser/chromeos/extensions/launcher_search_provider.h
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 // Implements chrome.launcherSearchProvider.setSearchResults method.
+// TODO(crbug.com/1208731): This is unused but needs to be defined while the
+// launcher files extension still exists. It should be removed along with the
+// extension.
 class LauncherSearchProviderSetSearchResultsFunction
     : public ExtensionFunction {
  public:
