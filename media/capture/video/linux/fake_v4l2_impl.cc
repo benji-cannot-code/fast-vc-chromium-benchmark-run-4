aa3e8427a78475584b0cea26278242f861a3c6a3
Ilya Nikolaevskiy <ilnik@chromium.org>
2021-05-17 08:34:41 +0000

    Add locks and empty string checks to FakeV4L2Impl
    FakeV4L2Impl is crashed by fuzzer with some weird ASAN errors, which
    turned out to be a threading issue.
    
    Bug: 1205059,1196302
    Change-Id: Ieb3a917c9a4549b655862e69214774e183a70bc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883613
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883390}

--- media/capture/video/linux/fake_v4l2_impl.cc
+++ media/capture/video/linux/fake_v4l2_impl.cc
@@ -381,10 +381,16 @@ FakeV4L2Impl::~FakeV4L2Impl() = default;
 
 void FakeV4L2Impl::AddDevice(const std::string& device_name,
                              const FakeV4L2DeviceConfig& config) {
+  base::AutoLock lock(lock_);
   device_configs_.emplace(device_name, config);
 }
 
 int FakeV4L2Impl::open(const char* device_name, int flags) {
+  if (!device_name)
+    return kInvalidId;
+
+  base::AutoLock lock(lock_);
+
   std::string device_name_as_string(device_name);
   auto device_configs_iter = device_configs_.find(device_name_as_string);
   if (device_configs_iter == device_configs_.end())
@@ -404,6 +410,7 @@ int FakeV4L2Impl::open(const char* device_name, int flags) {
 }
 
 int FakeV4L2Impl::close(int fd) {
+  base::AutoLock lock(lock_);
   auto device_iter = opened_devices_.find(fd);
   if (device_iter == opened_devices_.end())
     return kErrorReturnValue;
@@ -413,6 +420,7 @@ int FakeV4L2Impl::close(int fd) {
 }
 
 int FakeV4L2Impl::ioctl(int fd, int request, void* argp) {
+  base::AutoLock lock(lock_);
   auto device_iter = opened_devices_.find(fd);
   if (device_iter == opened_devices_.end())
     return EBADF;
@@ -519,6 +527,7 @@ void* FakeV4L2Impl::mmap(void* /*start*/,
                          int flags,
                          int fd,
                          off_t offset) {
+  base::AutoLock lock(lock_);
   if (flags & MAP_FIXED) {
     errno = EINVAL;
     return MAP_FAILED;
@@ -544,10 +553,12 @@ void* FakeV4L2Impl::mmap(void* /*start*/,
 }
 
 int FakeV4L2Impl::munmap(void* start, size_t length) {
+  base::AutoLock lock(lock_);
   return kSuccessReturnValue;
 }
 
 int FakeV4L2Impl::poll(struct pollfd* ufds, unsigned int nfds, int timeout) {
+  base::AutoLock lock(lock_);
   if (nfds != 1) {
     // We only support polling of a single device.
     errno = EINVAL;
