164279f7134371330a572cee25c1c3c200c42480
Tina Wang <tinazwang@google.com>
2021-05-17 16:54:37 +0000

    🐻 [Pandora] Fix "Sync setup not complete in FRE"
    Turn on FirstSetupComplete flag after the authentication service has
    granted user consent to start Sync.
    
    Bug: 1207926
    Change-Id: I4cb3e9939e3fea16293c16a9c88859870d2472c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2899570
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883494}

--- ios/chrome/browser/ui/first_run/sync/sync_screen_mediator.mm
+++ ios/chrome/browser/ui/first_run/sync/sync_screen_mediator.mm
@@ -73,6 +73,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.consentAuditor->RecordSyncConsent(coreAccountId, syncConsent);
   self.authenticationService->GrantSyncConsent(identity);
 
+  // Turn on FirstSetupComplete flag after the authentication service has
+  // granted user consent to start Sync.
+  self.syncSetupService->SetFirstSetupComplete(
+      syncer::SyncFirstSetupCompleteSource::BASIC_FLOW);
+
   self.syncSetupService->CommitSyncChanges();
 }
 
