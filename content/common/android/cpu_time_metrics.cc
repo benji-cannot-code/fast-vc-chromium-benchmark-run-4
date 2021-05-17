2c4644847c222fa7ca7877cf8f7c1f60ac966643
Eric Seckler <eseckler@chromium.org>
2021-05-17 12:38:18 +0000

    content/android: Reduce CPU time metric collection frequency
    Split collection of detailed CPU time breakdowns (per-thread and
    per-core-type) from collection of higher level process-wide CPU metrics.
    
    This allows us to avoid collecting the detailed stats when PowerMode or
    process visibility change.
    
    This change also:
    - Increases the periodic collection intervals to reduce overhead
    caused by sampling these metrics too often. In an active process, this
    results in collections every 5-10 seconds. In an inactive process, it
    can stretch to minutes.
    - Simplifies the reporter's logic to no longer check if collection
    is already in progress when posting collection tasks.
    - Moves observing of PowerMode changes onto the thread pool sequence.
    
    Fixed: 1209163
    Change-Id: I8378ea54efdca1211b9ef4d69706e67a836c7dd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2896967
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883443}

---
+++ content/common/android/cpu_time_metrics.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/common/android/cpu_time_metrics.h"
+
+#include "content/common/android/cpu_time_metrics_internal.h"
+
+namespace content {
+
+void SetupCpuTimeMetrics() {
+  internal::ProcessCpuTimeMetrics::GetInstance();
+}
+
+}  // namespace content
--- content/common/android/cpu_time_metrics_unittest.cc
+++ content/common/android/cpu_time_metrics_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/common/android/cpu_time_metrics.h"
+#include "content/common/android/cpu_time_metrics_internal.h"
 
 #include "base/metrics/persistent_histogram_allocator.h"
 #include "base/synchronization/waitable_event.h"
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace content {
+namespace internal {
 namespace {
 
 void WorkForOneCpuSec(base::WaitableEvent* event) {
@@ -40,8 +41,9 @@ TEST(CpuTimeMetricsTest, RecordsMetricsForeground) {
 
   // Create the ProcessCpuTimeTaskObserver instance and register it
   // as the process visibility observer.
-  SetIgnoreHistogramAllocatorForTesting(true);
-  SetupCpuTimeMetrics();
+  ProcessCpuTimeMetrics::SetIgnoreHistogramAllocatorForTesting(true);
+  std::unique_ptr<ProcessCpuTimeMetrics> metrics =
+      ProcessCpuTimeMetrics::CreateForTesting();
 
   // Start out in the foreground and spend one CPU second there.
   // This will also set the current power mode to 'idle'.
@@ -53,9 +55,10 @@ TEST(CpuTimeMetricsTest, RecordsMetricsForeground) {
   // Wait until the thread has consumed one second of CPU time.
   event.Wait();
 
-  // Update the state to background to trigger the collection.
+  // Update the state to background to trigger the collection of high level
+  // metrics.
   ProcessVisibilityTracker::GetInstance()->OnProcessVisibilityChanged(false);
-  WaitForCpuTimeMetricsForTesting();
+  metrics->WaitForCollectionForTesting();
 
   // The test process has no process-type command line flag, so is recognized as
   // the browser process. The thread created above is named like a sampling
@@ -78,14 +81,23 @@ TEST(CpuTimeMetricsTest, RecordsMetricsForeground) {
                                 power_scheduler::PowerMode::kIdle);
   EXPECT_GE(browser_cpu_seconds_power_mode_idle, 1);
 
+  // Thread breakdown requires periodic collection.
   int thread_cpu_seconds =
+      histograms.GetBucketCount("Power.CpuTimeSecondsPerThreadType.Browser",
+                                kSamplingProfilerThreadBucket);
+  EXPECT_EQ(thread_cpu_seconds, 0);
+
+  metrics->PerformFullCollectionForTesting();
+  metrics->WaitForCollectionForTesting();
+
+  thread_cpu_seconds =
       histograms.GetBucketCount("Power.CpuTimeSecondsPerThreadType.Browser",
                                 kSamplingProfilerThreadBucket);
   EXPECT_GE(thread_cpu_seconds, 1);
 
   thread1.Stop();
 
-  SetIgnoreHistogramAllocatorForTesting(false);
+  ProcessCpuTimeMetrics::SetIgnoreHistogramAllocatorForTesting(false);
 }
 
 TEST(CpuTimeMetricsTest, RecordsMetricsBackground) {
@@ -101,8 +113,9 @@ TEST(CpuTimeMetricsTest, RecordsMetricsBackground) {
 
   // Create the ProcessCpuTimeTaskObserver instance and register it
   // as the process visibility observer.
-  SetIgnoreHistogramAllocatorForTesting(true);
-  SetupCpuTimeMetrics();
+  ProcessCpuTimeMetrics::SetIgnoreHistogramAllocatorForTesting(true);
+  std::unique_ptr<ProcessCpuTimeMetrics> metrics =
+      ProcessCpuTimeMetrics::CreateForTesting();
 
   // Start out in the background and spend one CPU second there.
   ProcessVisibilityTracker::GetInstance()->OnProcessVisibilityChanged(false);
@@ -113,9 +126,10 @@ TEST(CpuTimeMetricsTest, RecordsMetricsBackground) {
   // Wait until the thread has consumed one second of CPU time.
   event.Wait();
 
-  // Update the state to foreground to trigger the collection.
+  // Update the state to foreground to trigger the collection of high level
+  // metrics.
   ProcessVisibilityTracker::GetInstance()->OnProcessVisibilityChanged(true);
-  WaitForCpuTimeMetricsForTesting();
+  metrics->WaitForCollectionForTesting();
 
   // The test process has no process-type command line flag, so is recognized as
   // the browser process. The thread created above is named like a sampling
@@ -133,15 +147,25 @@ TEST(CpuTimeMetricsTest, RecordsMetricsBackground) {
       "Power.CpuTimeSecondsPerProcessType.Background", kBrowserProcessBucket);
   EXPECT_GE(browser_cpu_seconds_background, 1);
 
+  // Thread breakdown requires periodic collection.
   int thread_cpu_seconds =
+      histograms.GetBucketCount("Power.CpuTimeSecondsPerThreadType.Browser",
+                                kSamplingProfilerThreadBucket);
+  EXPECT_EQ(thread_cpu_seconds, 0);
+
+  metrics->PerformFullCollectionForTesting();
+  metrics->WaitForCollectionForTesting();
+
+  thread_cpu_seconds =
       histograms.GetBucketCount("Power.CpuTimeSecondsPerThreadType.Browser",
                                 kSamplingProfilerThreadBucket);
   EXPECT_GE(thread_cpu_seconds, 1);
 
   thread1.Stop();
 
-  SetIgnoreHistogramAllocatorForTesting(false);
+  ProcessCpuTimeMetrics::SetIgnoreHistogramAllocatorForTesting(false);
 }
 
 }  // namespace
+}  // namespace internal
 }  // namespace content
