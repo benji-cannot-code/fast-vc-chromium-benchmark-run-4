7ea2aa5675f4a0305106eb28a1229153fecdf2a7
Jinsuk Kim <jinsukkim@chromium.org>
2021-05-17 00:11:30 +0000

    Android: Improve 'move to other window' support
    On some supported platforms, This CL lets Chrome show
    'new window' menu item when a single instance is running.
    Context menu shows 'open in other window' also when Chrome
    can get itself into multi-window mode.
    
    Bug: 1206268
    Change-Id: Iee3657e7894c0644d0972c4f839ea4677b357c3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2874740
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883344}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -209,6 +209,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kInstantStart,
     &kKitKatSupported,
     &kLensCameraAssistedSearch,
+    &kNewWindowAppMenu,
     &kNotificationSuspender,
     &kOfflineIndicatorV2,
     &kOfflineMeasurementsBackgroundTask,
@@ -575,6 +576,9 @@ const base::Feature kSearchEnginePromoExistingDevice{
 
 const base::Feature kSearchEnginePromoNewDevice{
     "SearchEnginePromo.NewDevice", base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::Feature kNewWindowAppMenu{"NewWindowAppMenu",
+                                      base::FEATURE_ENABLED_BY_DEFAULT};
 
 // TODO(knollr): This is a temporary kill switch, it can be removed once we feel
 // okay about leaving it on.
