7467e6f2e46bc356fc8e0308aa356734d1d801ba
rajendrant <rajendrant@chromium.org>
2021-05-17 18:51:00 +0000

    SubresourceRedirect: Allow first k images to be disallowed
    This CL disallows compression of first k images, where the k can be
    experimentally controlled. This allows experimenting with disabling
    compression for images that affect FCP/LCP more.
    
    Fixed: 1209263
    Change-Id: I2190fd5d8473b80cc116eb902b3b38e6c4a70058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2898173
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883545}

--- chrome/renderer/subresource_redirect/public_image_hints_decider_agent.cc
+++ chrome/renderer/subresource_redirect/public_image_hints_decider_agent.cc
@@ -167,6 +167,7 @@ void PublicImageHintsDeciderAgent::RecordMetrics(
     case SubresourceRedirectResult::kIneligibleRobotsDisallowed:
     case SubresourceRedirectResult::kIneligibleRobotsTimeout:
     case SubresourceRedirectResult::kIneligibleLoginDetected:
+    case SubresourceRedirectResult::kIneligibleFirstKDisableSubresourceRedirect:
       NOTREACHED();
   }
   mojo::PendingRemote<ukm::mojom::UkmRecorderInterface> recorder;
