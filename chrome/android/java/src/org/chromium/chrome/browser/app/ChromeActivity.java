9e00c2a4b267925417205e60299bae8f077116f9
Brandon Wylie <wylieb@chromium.org>
2021-05-17 18:18:48 +0000

    [ChromeActivity] Fix small contextual search panel height
    Passing through the dimension in dp and converting it in
    OverlayPanelBase fixed the issue.
    
    Bug: 1207321
    Change-Id: I59b69399e85eeada60fc22b85a82a9fefaa70e9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2885889
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883531}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -1997,11 +1997,10 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
 
         if (mContextualSearchManager != null) {
             mContextualSearchManager.initialize(contentContainer, layoutManager,
-                    mRootUiCoordinator.getBottomSheetController(), getCompositorViewHolder(),
+                    mRootUiCoordinator.getBottomSheetController(), compositorViewHolder,
                     getControlContainerHeightResource() == NO_CONTROL_CONTAINER
-                            ? 0
-                            : getResources().getDimensionPixelSize(
-                                    getControlContainerHeightResource()),
+                            ? 0f
+                            : getResources().getDimension(getControlContainerHeightResource()),
                     getToolbarManager(), getActivityType());
         }
     }
