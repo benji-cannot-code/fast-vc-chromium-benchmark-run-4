7c0361b40b4bb34c6580b500f39d6a667d38bf67
Andrew Grieve <agrieve@chromium.org>
2021-05-17 20:31:51 +0000

    AccessedByNative -> DoNotInline for mRetainedObjects
    Doing this to match the approach used in AwServiceWorkerController.
    @DoNotInline is more appropriate since it still allows obfuscation.
    
    Change-Id: Id5ee93a70caed70aa7618770cf9c7f752aa51a83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2896849
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883596}

--- components/thin_webview/internal/java/src/org/chromium/components/thinwebview/internal/ThinWebViewImpl.java
+++ components/thin_webview/internal/java/src/org/chromium/components/thinwebview/internal/ThinWebViewImpl.java
@@ -13,6 +13,7 @@ import android.widget.FrameLayout;
 import androidx.annotation.Nullable;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.base.annotations.DoNotInline;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.components.embedder_support.delegate.WebContentsDelegateAndroid;
@@ -32,8 +33,11 @@ public class ThinWebViewImpl extends FrameLayout implements ThinWebView {
     private WindowAndroid mWindowAndroid;
     private long mNativeThinWebViewImpl;
     private WebContents mWebContents;
-    private WebContentsDelegateAndroid mWebContentsDelegate;
     private View mContentView;
+    // Passed to native and stored as a weak reference, so ensure this strong
+    // reference is not optimized away by R8.
+    @DoNotInline
+    private WebContentsDelegateAndroid mWebContentsDelegate;
 
     /**
      * Creates a {@link ThinWebViewImpl} backed by a {@link Surface}.
@@ -73,8 +77,8 @@ public class ThinWebViewImpl extends FrameLayout implements ThinWebView {
         mWebContentsDelegate = delegate;
         setContentView(contentView);
         ThinWebViewImplJni.get().setWebContents(
-                mNativeThinWebViewImpl, ThinWebViewImpl.this, mWebContents, mWebContentsDelegate);
-        mWebContents.onShow();
+                mNativeThinWebViewImpl, ThinWebViewImpl.this, webContents, delegate);
+        webContents.onShow();
     }
 
     @Override
