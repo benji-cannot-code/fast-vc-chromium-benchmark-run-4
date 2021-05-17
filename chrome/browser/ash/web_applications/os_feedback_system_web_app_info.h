bc5f7eeade15b9de847b25ca5cc4cc8c8221694e
Xiangdong Kong <Xiangdongkong@google.com>
2021-05-17 23:27:59 +0000

    feedback: convert the new os feedback tool WebUI into a SWA
    Bug: b/185624798
    Change-Id: Ib9b2ff2d091deac166286fbe185de1534b33a4f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2893497
    Reviewed-by: Miriam Zimmerman <mutexlox@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/master@{#883703}

---
+++ chrome/browser/ash/web_applications/os_feedback_system_web_app_info.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_WEB_APPLICATIONS_OS_FEEDBACK_SYSTEM_WEB_APP_INFO_H_
+#define CHROME_BROWSER_ASH_WEB_APPLICATIONS_OS_FEEDBACK_SYSTEM_WEB_APP_INFO_H_
+
+#include <memory>
+
+#include "ui/gfx/geometry/rect.h"
+
+class Browser;
+struct WebApplicationInfo;
+
+// Returns a WebApplicationInfo used to install the app.
+std::unique_ptr<WebApplicationInfo> CreateWebAppInfoForOSFeedbackSystemWebApp();
+
+// Returns the default bounds.
+gfx::Rect GetDefaultBoundsForOSFeedbackApp(Browser*);
+
+#endif  // CHROME_BROWSER_ASH_WEB_APPLICATIONS_OS_FEEDBACK_SYSTEM_WEB_APP_INFO_H_
--- chrome/browser/web_applications/system_web_apps/system_web_app_manager.cc
+++ chrome/browser/web_applications/system_web_apps/system_web_app_manager.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_features.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/components/os_feedback_ui/url_constants.h"
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/content/shimless_rma/url_constants.h"
@@ -68,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/web_applications/eche_app_info.h"
 #include "chrome/browser/ash/web_applications/help_app/help_app_web_app_info.h"
 #include "chrome/browser/ash/web_applications/media_app/media_web_app_info.h"
+#include "chrome/browser/ash/web_applications/os_feedback_system_web_app_info.h"
 #include "chrome/browser/ash/web_applications/os_settings_web_app_info.h"
 #include "chrome/browser/ash/web_applications/personalization_app_info.h"
 #include "chrome/browser/ash/web_applications/print_management_web_app_info.h"
@@ -294,6 +296,23 @@ base::flat_map<SystemAppType, SystemAppInfo> CreateSystemWebApps(
         base::BindRepeating(&GetDefaultBoundsForEche);
   }
 
+  if (SystemWebAppManager::IsAppEnabled(SystemAppType::OS_FEEDBACK)) {
+    infos.emplace(
+        SystemAppType::OS_FEEDBACK,
+        SystemAppInfo(
+            "OSFeedback", GURL(ash::kChromeUIOSFeedbackUrl),
+            base::BindRepeating(&CreateWebAppInfoForOSFeedbackSystemWebApp)));
+    auto& config = infos.at(SystemAppType::OS_FEEDBACK);
+
+    config.allow_scripts_to_close_windows = true;
+    config.capture_navigations = true;
+    // Only allow users to minimize or close the App.
+    config.is_maximizable = false;
+    config.is_resizeable = false;
+    config.get_default_bounds =
+        base::BindRepeating(&GetDefaultBoundsForOSFeedbackApp);
+  }
+
   if (SystemWebAppManager::IsAppEnabled(SystemAppType::PERSONALIZATION)) {
     infos.emplace(
         SystemAppType::PERSONALIZATION,
@@ -471,6 +490,8 @@ bool SystemWebAppManager::IsAppEnabled(SystemAppType type) {
       return features::IsShortcutCustomizationAppEnabled();
     case SystemAppType::DEMO_MODE:
       return chromeos::features::IsDemoModeSWAEnabled();
+    case SystemAppType::OS_FEEDBACK:
+      return base::FeatureList::IsEnabled(ash::features::kOsFeedback);
   }
 #else
   return false;
