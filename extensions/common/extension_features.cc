49cccc8b1776c0c42cdd7d461454c2d4db101029
Xinghui Lu <xinghuilu@chromium.org>
2021-05-18 23:47:31 +0000

    Disable extensions marked as "_policy_violation" in Omaha attributes.
    If the Omaha attributes have the key "_policy_violation" set to true,
    the extension will be added to the Omaha attribute greylist and
    automatically disabled. The user is able to re-enable it.
    
    Add a flag DisablePolicyViolationExtensionRemotely to control the
    "_policy_violation" attribute. This flag is fine-grained, because
    the plan to consume other greylist attributes (e.g. _potentially_uws)
    may be different.
    
    Bug: 1180996
    Change-Id: Ifadf9e1fc2a696fd0b4e11861389bbe48377c066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2882234
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884225}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "extensions/common/extension_features.h"
+#include "base/feature_list.h"
 
 namespace extensions_features {
 
@@ -16,6 +17,12 @@ const base::Feature kExtensionsCheckup{"ExtensionsCheckup",
 // Controls whether we disable extensions for malware.
 const base::Feature kDisableMalwareExtensionsRemotely{
     "DisableMalwareExtensionsRemotely", base::FEATURE_ENABLED_BY_DEFAULT};
+
+// Controls whether we disable extensions that are marked as policy violation
+// by the Omaha attribute.
+const base::Feature kDisablePolicyViolationExtensionsRemotely{
+    "DisablePolicyViolationExtensionsRemotely",
+    base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Controls whether we show an install friction dialog when an Enhanced Safe
 // Browsing user tries to install an extension that is not included in the
--- extensions/common/extension_features.h
+++ extensions/common/extension_features.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions_features {
 
 extern const base::Feature kDisableMalwareExtensionsRemotely;
+extern const base::Feature kDisablePolicyViolationExtensionsRemotely;
 extern const base::Feature kSafeBrowsingCrxAllowlistShowWarnings;
 extern const base::Feature kSafeBrowsingCrxAllowlistAutoDisable;
 
