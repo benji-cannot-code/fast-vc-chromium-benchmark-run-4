d8fad1e9f3ecaf0a560b945b0ba6e6ec5f2c15d7
Ioana Pandele <ioanap@chromium.org>
2021-05-18 11:52:03 +0000

    [BioAuthPwdFill] Trigger auth when filling from the all passwords list
    The re-auth flow should only be triggered for passwords.
    
    Bug: 1209161
    Change-Id: I09f2170a5135e004612a2c2cb55309384917d373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2900360
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883902}

--- chrome/browser/password_manager/android/all_passwords_bottom_sheet_controller.h
+++ chrome/browser/password_manager/android/all_passwords_bottom_sheet_controller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/types/pass_key.h"
 #include "components/autofill/core/common/mojom/autofill_types.mojom-forward.h"
+#include "components/password_manager/core/browser/biometric_authenticator.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
 #include "ui/gfx/native_widget_types.h"
 #include "url/gurl.h"
@@ -72,6 +73,13 @@ class AllPasswordsBottomSheetController
   const GURL& GetFrameUrl();
 
  private:
+  // Called when the biometric re-auth completes. |password| is the password
+  // to be filled and |auth_succeded| is the authentication result.
+  void OnReauthCompleted(const std::u16string& password, bool auth_succeeded);
+
+  // Fills the password into the focused field.
+  void FillPassword(const std::u16string& password);
+
   // The controller takes |view_| ownership.
   std::unique_ptr<AllPasswordsBottomSheetView> view_;
 
@@ -89,6 +97,9 @@ class AllPasswordsBottomSheetController
   // Either |driver_| is created and owned by this controller or received in
   // constructor specified for tests.
   base::WeakPtr<password_manager::PasswordManagerDriver> driver_;
+
+  // Authenticator used to trigger a biometric re-auth before password filling.
+  scoped_refptr<password_manager::BiometricAuthenticator> authenticator_;
 
   // The type of field on which the user is focused, e.g. PASSWORD.
   autofill::mojom::FocusedFieldType focused_field_type_;
