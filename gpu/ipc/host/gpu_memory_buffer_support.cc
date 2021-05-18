7c2b0621e27977a90aed343e250151da0ef90952
Lei Zhang <thestig@chromium.org>
2021-05-18 04:44:32 +0000

    Do IWYU for base/containers/contains.h in various directories.
    Include base/containers/contains.h in various files that use
    base::Contains().
    
    Bug: 970209
    Change-Id: I8ed8e70509155058bd3d1b2c4878129f266fbf22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2901022
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883799}

--- gpu/ipc/host/gpu_memory_buffer_support.cc
+++ gpu/ipc/host/gpu_memory_buffer_support.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/ipc/host/gpu_memory_buffer_support.h"
 
+#include "base/containers/contains.h"
 #include "build/build_config.h"
 #include "gpu/command_buffer/common/gpu_memory_buffer_support.h"
 #include "gpu/ipc/common/gpu_memory_buffer_support.h"
