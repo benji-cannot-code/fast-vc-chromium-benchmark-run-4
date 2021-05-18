f7d161b31dafd1c66785d23cc9109061f0c79f84
Josh Simmons <simmonsjosh@google.com>
2021-05-18 03:39:51 +0000

    Files app: Removed unused Chrome Web Store widget dependency
    The Chrome Web Store widget was used to display compatible apps and
    extensions that could be installed in Files.app. This functionality has
    been removed and these files are no longer required.
    
    Bug: 1178660
    Test: browser_tests --gtest_filter="*FilesAppBrowser*"
    Change-Id: Ieb7de610a57c3652dfdad3ec5c01ec6903611ddc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2881626
    Commit-Queue: Josh Simmons <simmonsjosh@google.com>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883778}

--- ui/file_manager/file_manager/foreground/js/launch_param.js
+++ ui/file_manager/file_manager/foreground/js/launch_param.js
@@ -8,14 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {DialogType} from './dialog_type.m.js';
 // clang-format on
 
-/**
- * @typedef {{
- *   overrideCwsContainerUrlForTest: (string|undefined),
- *   overrideCwsContainerOriginForTest: (string|undefined)
- * }}
- */
-/* #export */ let SuggestAppDialogState;
-
 /* #export */ class LaunchParam {
   /**
    * @param {!Object} unformatted Unformatted option.
--- ui/file_manager/file_manager/foreground/js/main_scripts.js
+++ ui/file_manager/file_manager/foreground/js/main_scripts.js
@@ -94,7 +94,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // <include src="constants.js">
 // <include src="crossover_search_utils.js">
-// <include src="web_store_utils.js">
 // <include src="ui/multi_menu.js">
 // <include src="ui/multi_menu_button.js">
 // <include src="ui/combobutton.js">
@@ -103,11 +102,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="metadata/metadata_cache_set.js">
 // <include src="metadata/metadata_provider.js">
 // <include src="metadata/metadata_request.js">
-//
-// <include src="../../cws_widget/app_installer.js">
-// <include src="../../cws_widget/cws_webview_client.js">
-// <include src="../../cws_widget/cws_widget_container.js">
-// <include src="../../cws_widget/cws_widget_container_error_dialog.js">
 //
 // <include src="actions_controller.js">
 // <include src="actions_model.js">
