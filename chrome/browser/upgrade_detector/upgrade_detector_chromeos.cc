1242d9752bb45e1b747a8dcac8e503dda15e6503
Saurabh Nijhara <snijhara@google.com>
2021-05-18 18:42:44 +0000

    Use RelaunchWindow policy to adjust relaunch deadline for ChromeOS
    This CL reads the RelaunchWindow policy value in UpgradeDetector and
    adjusts the deadline to fall within the window as specified in policy.
    If policy is not set or set incorrectly, default window is used.
    The code to adjust deadline has been moved to base class UpgradeDetector
    so that it can be used by all platform specific implementations.
    
    Next steps in follow-up CLs -
    1) Observe RelaunchWindow policy change in UpgradeDetector and handle in
    platform specific implementations to re-adjust the deadline.
    2) Adjust deadline for Chrome in UpgradeDetectorImpl.
    
    Bug: 915107
    Change-Id: Ib7091edb8fb86c8a93fe4db7e658844d81d30a4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2878956
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Saurabh Nijhara <snijhara@google.com>
    Cr-Commit-Position: refs/heads/master@{#884040}

--- chrome/browser/upgrade_detector/upgrade_detector_chromeos.cc
+++ chrome/browser/upgrade_detector/upgrade_detector_chromeos.cc
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/no_destructor.h"
-#include "base/rand_util.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/clock.h"
 #include "base/time/default_clock.h"
 #include "base/time/default_tick_clock.h"
+#include "base/time/time.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/upgrade_detector/build_state.h"
 #include "chrome/common/pref_names.h"
@@ -168,51 +168,6 @@ base::TimeDelta UpgradeDetectorChromeos::GetRelaunchHeadsUpPeriod() {
   return base::TimeDelta::FromMilliseconds(value);
 }
 
-// static
-base::TimeDelta UpgradeDetectorChromeos::GenRandomTimeDelta(
-    base::TimeDelta max) {
-  return max * base::RandDouble();
-}
-
-// static
-base::Time UpgradeDetectorChromeos::AdjustDeadline(base::Time deadline) {
-  // Compute the offset applied to GMT to get local time at |deadline|.
-  const icu::TimeZone& time_zone =
-      chromeos::system::TimezoneSettings::GetInstance()->GetTimezone();
-  UErrorCode status = U_ZERO_ERROR;
-  int32_t raw_offset, dst_offset;
-  time_zone.getOffset(deadline.ToDoubleT() * base::Time::kMillisecondsPerSecond,
-                      true /* local */, raw_offset, dst_offset, status);
-  base::TimeDelta time_zone_offset;
-  if (U_FAILURE(status)) {
-    LOG(ERROR) << "Failed to get time zone offset, error code: " << status;
-    // The fallback case is to get the raw timezone offset ignoring the daylight
-    // saving time.
-    time_zone_offset =
-        base::TimeDelta::FromMilliseconds(time_zone.getRawOffset());
-  } else {
-    time_zone_offset =
-        base::TimeDelta::FromMilliseconds(raw_offset + dst_offset);
-  }
-
-  // To get local midnight add timezone offset to deadline and treat this time
-  // as UTC based to use UTCMidnight(), then subtract timezone offset.
-  auto midnight =
-      (deadline + time_zone_offset).UTCMidnight() - time_zone_offset;
-  const auto day_time = deadline - midnight;
-  // Return the exact deadline if it naturally falls between 2am and 4am.
-  if (day_time >= base::TimeDelta::FromHours(2) &&
-      day_time <= base::TimeDelta::FromHours(4)) {
-    return deadline;
-  }
-  // Advance to the next day if the deadline falls after 4am.
-  if (day_time > base::TimeDelta::FromHours(4))
-    midnight += base::TimeDelta::FromDays(1);
-
-  return midnight + base::TimeDelta::FromHours(2) +
-         GenRandomTimeDelta(base::TimeDelta::FromHours(2));
-}
-
 void UpgradeDetectorChromeos::CalculateDeadlines() {
   base::TimeDelta notification_period = GetRelaunchNotificationPeriod();
   if (notification_period.is_zero())
@@ -358,4 +313,11 @@ base::TimeDelta UpgradeDetector::GetDefaultHighAnnoyanceThreshold() {
 // static
 base::TimeDelta UpgradeDetector::GetDefaultElevatedAnnoyanceThreshold() {
   return kDefaultElevatedThreshold;
+}
+
+// static
+UpgradeDetector::RelaunchWindow UpgradeDetector::GetDefaultRelaunchWindow() {
+  // Two hours starting at 2am.
+  return RelaunchWindow(/*start_hour=*/2, /*start_minute=*/0,
+                        base::TimeDelta::FromHours(2));
 }
