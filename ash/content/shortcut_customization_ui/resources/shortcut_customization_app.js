4d1b485cb1462025c8b89c0dc90d63410f4b1c39
Jimmy Gong <jimmyxgong@google.com>
2021-05-18 23:27:07 +0000

    shortcuts:Add android and accessibility base pages
    Added pages serves only as skeleton pages.
    
    Screenshot: https://screenshot.googleplex.com/98P4eruBBEL3DZw
    
    Bug: 1060690
    Change-Id: I9fe9aa2f75aa3b34d2b1444c3a3538bde8449273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2901743
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884215}

--- ash/content/shortcut_customization_ui/resources/shortcut_customization_app.js
+++ ash/content/shortcut_customization_ui/resources/shortcut_customization_app.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './shortcut_input.js';
 import './browser_shortcuts_page.js'
 import './chromeos_shortcuts_page.js'
+import './android_shortcuts_page.js'
+import './accessibility_shortcuts_page.js'
 
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import 'chrome://resources/ash/common/navigation_view_panel.js';
@@ -29,6 +31,9 @@ export class ShortcutCustomizationAppElement extends PolymerElement {
     this.$.navigationPanel.addSelector('Chrome OS',
                                        'chromeos-shortcuts-page');
     this.$.navigationPanel.addSelector('Browser', 'browser-shortcuts-page');
+    this.$.navigationPanel.addSelector('Android', 'android-shortcuts-page');
+    this.$.navigationPanel.addSelector('Accessibility',
+                                       'accessibility-shortcuts-page');
   }
 }
 
