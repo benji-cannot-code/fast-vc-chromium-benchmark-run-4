8c9e7dc71ae547c6589870510e90980fa3edcae7
Jing Wang <jiwan@chromium.org>
2021-05-18 05:46:17 +0000

    Implement Grammar Manager.
    Sends grammar check request to real ml service.
    Upon receiving the grammar check result, call renderer api to render
    underline.
    
    Bug: 1132699
    Change-Id: I2b6571f6e394780a76f2e81677d9adc615f65b80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2845629
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883812}

--- chrome/browser/chromeos/input_method/grammar_service_client.h
+++ chrome/browser/chromeos/input_method/grammar_service_client.h
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/services/machine_learning/public/mojom/grammar_checker.mojom.h"
 #include "mojo/public/cpp/bindings/remote.h"
-
-struct SpellCheckResult;
+#include "ui/base/ime/grammar_fragment.h"
 
 namespace chromeos {
 
@@ -48,17 +47,17 @@ namespace chromeos {
 class GrammarServiceClient {
  public:
   GrammarServiceClient();
-  ~GrammarServiceClient();
+  virtual ~GrammarServiceClient();
 
   using TextCheckCompleteCallback = base::OnceCallback<void(
       bool /* success */,
-      const std::vector<SpellCheckResult>& /* results */)>;
+      const std::vector<ui::GrammarFragment>& /* results */)>;
 
   // Sends grammar check request to ML service, parses the reponse
   // and calls a provided callback method.
-  bool RequestTextCheck(Profile* profile,
-                        const std::u16string& text,
-                        TextCheckCompleteCallback callback) const;
+  virtual bool RequestTextCheck(Profile* profile,
+                                const std::u16string& text,
+                                TextCheckCompleteCallback callback) const;
 
  private:
   // Parse the result returned from grammar check service.
