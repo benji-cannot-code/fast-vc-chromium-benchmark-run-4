f53124ec5999b1f83c8416fbe15c6dde9ea93754
Vidhan Jain <vidhanj@google.com>
2021-05-18 18:51:28 +0000

    [iOS][Autofill][SavePrompts] Add Edit Address Profile Modal Screen
    With this CL, |AutofillProfileFieldDisplayInfo/AutofillFieldDisplayInfo|
    and |kProfileFieldsToDisplay/kFieldsToDisplay| has been moved to
    autofill_ui_type_util so that they can be used in the Edit Profile modal
    too.
    
    Now, adding a field to address settings UI and the Edit Address
    Profile Modal UI can be done by adding a row to the
    |AutofillProfileFieldDisplayInfo/AutofillFieldDisplayInfo|.
    
    Mocks: https://docs.google.com/presentation/d/1PpvRjfkq9d7MES7gb8yfWRFVJW7slel7Hs5Dpxjkgjc/edit?ts=609131d1#slide=id.gd344556173_3_10
    
    Bug: 1167062
    Change-Id: I8ff838f0800c97c9a76d9921129f1e7cd6993afd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2883396
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884055}

---
+++ ios/chrome/browser/ui/infobars/modals/infobar_edit_address_profile_table_view_controller.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_INFOBAR_EDIT_ADDRESS_PROFILE_TABLE_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_INFOBAR_EDIT_ADDRESS_PROFILE_TABLE_VIEW_CONTROLLER_H_
+
+#import "ios/chrome/browser/ui/table_view/chrome_table_view_controller.h"
+
+@protocol InfobarSaveAddressProfileModalDelegate;
+
+// The TableView for an Autofill save/update address edit menu.
+@interface InfobarEditAddressProfileTableViewController
+    : ChromeTableViewController
+
+- (instancetype)initWithModalDelegate:
+    (id<InfobarSaveAddressProfileModalDelegate>)delegate;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_INFOBAR_EDIT_ADDRESS_PROFILE_TABLE_VIEW_CONTROLLER_H_
