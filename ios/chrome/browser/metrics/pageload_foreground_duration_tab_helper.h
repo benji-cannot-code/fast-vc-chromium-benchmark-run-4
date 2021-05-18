f221aada5937c37f2414d3143983399adf766bf3
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-18 18:29:22 +0000

    Retire ScopedObserver in /ios/chrome/browser/metrics.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=olivierrobin@chromium.org
    
    Bug: 1145565
    Change-Id: I92b67b17563a680c8dd3cfb8011eee5bc0660563
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2903299
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884015}

--- ios/chrome/browser/metrics/pageload_foreground_duration_tab_helper.h
+++ ios/chrome/browser/metrics/pageload_foreground_duration_tab_helper.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_METRICS_PAGELOAD_FOREGROUND_DURATION_TAB_HELPER_H_
 #define IOS_CHROME_BROWSER_METRICS_PAGELOAD_FOREGROUND_DURATION_TAB_HELPER_H_
 
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "base/time/time.h"
 #import "ios/web/public/web_state.h"
 #import "ios/web/public/web_state_observer.h"
@@ -49,7 +49,8 @@ class PageloadForegroundDurationTabHelper
   // WebState reference.
   web::WebState* web_state_ = nullptr;
   // Scoped observer that facilitates observing the WebState.
-  ScopedObserver<web::WebState, WebStateObserver> scoped_observer_;
+  base::ScopedObservation<web::WebState, WebStateObserver> scoped_observation_{
+      this};
   // Holds references to background NSNotification callback observer.
   id foreground_notification_observer_;
   // Holds references to foreground NSNotification callback observer.
