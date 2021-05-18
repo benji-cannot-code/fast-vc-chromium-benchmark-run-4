4090da29861940331df1273af0b0e362297ef8cf
Kent Tamura <tkent@chromium.org>
2021-05-18 07:48:41 +0000

    LayoutNG: Move NGInlineItemsData out from ng_inline_item.{h,cc}
    It was a little difficult to remember that NGInlineItemsData was in
    ng_inline_item.{h,cc}.
    
    This CL has no behavior changes.
    
    Change-Id: Id20bdf1a51ab4a3908dc5ac3f144b314dbed1083
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2902167
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883846}

---
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_items_data.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_items_data.h"
+
+namespace blink {
+
+void NGInlineItemsData::GetOpenTagItems(wtf_size_t size,
+                                        OpenTagItems* open_items) const {
+  DCHECK_LE(size, items.size());
+  for (const NGInlineItem& item : base::make_span(items.data(), size)) {
+    if (item.Type() == NGInlineItem::kOpenTag)
+      open_items->push_back(&item);
+    else if (item.Type() == NGInlineItem::kCloseTag)
+      open_items->pop_back();
+  }
+}
+
+}  // namespace blink
