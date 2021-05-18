a449c5a5d067ae2df81001171eadc758a1f0148c
Mei Liang <meiliang@chromium.org>
2021-05-18 21:11:59 +0000

    [RBD] Cache used discount
    This CL does the following:
      1. Add a profile prefs to store used rule IDs.
      2. Exclude discounts from used rule IDs.
      3. Add CacheUsedDiscounts and CleanUpDiscounts API.
    
    Change-Id: I825c186176ca17f57b640547240af2349a120319
    Bug: 1210380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2901615
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884141}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -1135,6 +1135,7 @@ extern const char kCartModuleHidden[];
 extern const char kCartModuleWelcomeSurfaceShownTimes[];
 extern const char kCartDiscountAcknowledged[];
 extern const char kCartDiscountEnabled[];
+extern const char kCartUsedDiscounts[];
 #endif
 
 #if defined(OS_ANDROID)
