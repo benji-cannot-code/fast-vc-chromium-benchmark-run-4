a9f1b7654d85f14a274b54fe3184996b6e293b38
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-18 23:50:22 +0000

    Retire ScopedObserver in /ios/chrome/browser/ui/recent_tabs.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=sczs@chromium.org
    
    Bug: 1145565
    Change-Id: I6384c5020792a3cc8a0126ac5cb1d47acea66361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2904187
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884228}

--- ios/chrome/browser/ui/recent_tabs/synced_sessions_bridge.h
+++ ios/chrome/browser/ui/recent_tabs/synced_sessions_bridge.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/macros.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 
 class ChromeBrowserState;
@@ -46,8 +46,9 @@ class SyncedSessionsObserverBridge : public signin::IdentityManager::Observer {
 
   __weak id<SyncedSessionsObserver> owner_ = nil;
   signin::IdentityManager* identity_manager_ = nullptr;
-  ScopedObserver<signin::IdentityManager, signin::IdentityManager::Observer>
-      identity_manager_observer_;
+  base::ScopedObservation<signin::IdentityManager,
+                          signin::IdentityManager::Observer>
+      identity_manager_observation_{this};
   base::CallbackListSubscription foreign_session_updated_subscription_;
 
   DISALLOW_COPY_AND_ASSIGN(SyncedSessionsObserverBridge);
