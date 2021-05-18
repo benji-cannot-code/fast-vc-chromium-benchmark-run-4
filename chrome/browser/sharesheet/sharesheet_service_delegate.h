68e98ba263fce99185d08044807a4afa4ef877e0
Melissa Zhang <melzhang@chromium.org>
2021-05-18 04:25:45 +0000

    [Sharesheet] Ensure sharesheet is cleaned up.
    Views do their own clean up, so let sharesheet_bubble_view be a
    raw pointer and it can clean itself up rather than being dereferenced
    and left in memory.
    
    Bug: 1188938
    Change-Id: Ibef168b6d8eca237c2eb3148db5562c3fe5491e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2899828
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883794}

--- chrome/browser/sharesheet/sharesheet_service_delegate.h
+++ chrome/browser/sharesheet/sharesheet_service_delegate.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SHARESHEET_SHARESHEET_SERVICE_DELEGATE_H_
 #define CHROME_BROWSER_SHARESHEET_SHARESHEET_SERVICE_DELEGATE_H_
 
-#include <memory>
 #include <string>
 
 #include "base/callback.h"
@@ -78,8 +77,8 @@ class SharesheetServiceDelegate : public SharesheetController {
   gfx::NativeWindow native_window_;
 
   std::u16string active_action_;
-  std::unique_ptr<ash::sharesheet::SharesheetBubbleView>
-      sharesheet_bubble_view_;
+  // Owned by views.
+  ash::sharesheet::SharesheetBubbleView* sharesheet_bubble_view_;
   SharesheetService* sharesheet_service_;
 };
 
