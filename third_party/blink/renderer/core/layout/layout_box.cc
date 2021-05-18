c3ba9fdff5b2b03e22e9c57275fb8966632b0d53
Ian Kilpatrick <ikilpatrick@chromium.org>
2021-05-18 02:51:43 +0000

    Mark inline subtree as requiring full paint invalidation for kVerticalRl
    As above. This was required as paint invalidation wasn't triggering
    due to the text being in the same logical position as before.
    
    Bug: 1206914
    Change-Id: I60aec8d4f4ff8c70ead1ff9b0989902abe12fa82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2900889
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883771}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -2790,8 +2790,12 @@ void LayoutBox::SizeChanged() {
     SetShouldCheckForPaintInvalidation();
   // In flipped blocks writing mode, our children can change physical location,
   // but their flipped location remains the same.
-  if (HasFlippedBlocksWritingMode())
-    SetSubtreeShouldCheckForPaintInvalidation();
+  if (HasFlippedBlocksWritingMode()) {
+    if (ChildrenInline())
+      SetSubtreeShouldDoFullPaintInvalidation();
+    else
+      SetSubtreeShouldCheckForPaintInvalidation();
+  }
 }
 
 bool LayoutBox::IntersectsVisibleViewport() const {
