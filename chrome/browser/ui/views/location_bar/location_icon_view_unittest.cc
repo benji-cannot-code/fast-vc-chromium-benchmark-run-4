7e725ea6ac14767253d7a7fc0ef38977406f53d0
meacer <meacer@chromium.org>
2021-05-18 18:34:49 +0000

    Implement feature promo for updated connection security indicators
    This CL adds an in-product-help bubble for the updated connection
    security indicators. The bubble is only shown once when the user first
    visits a main frame URL with a valid https connection.
    
    Bug: 1204293
    Change-Id: Ib73954e30c10e48e4b25f0f4cc0e3fd566dd7912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2881185
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884025}

--- chrome/browser/ui/views/location_bar/location_icon_view_unittest.cc
+++ chrome/browser/ui/views/location_bar/location_icon_view_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
+#include "chrome/test/base/testing_profile.h"
 #include "chrome/test/views/chrome_views_test_base.h"
 #include "components/omnibox/browser/location_bar_model.h"
 #include "components/omnibox/browser/test_location_bar_model.h"
@@ -63,6 +64,8 @@ class LocationIconViewTest : public ChromeViewsTestBase {
   // ChromeViewsTestBase:
   void SetUp() override {
     ChromeViewsTestBase::SetUp();
+    profile_ = std::make_unique<TestingProfile>();
+
     gfx::FontList font_list;
 
     widget_ = CreateTestWidget();
@@ -71,8 +74,8 @@ class LocationIconViewTest : public ChromeViewsTestBase {
     delegate_ =
         std::make_unique<TestLocationIconDelegate>(location_bar_model());
 
-    auto view =
-        std::make_unique<LocationIconView>(font_list, delegate(), delegate());
+    auto view = std::make_unique<LocationIconView>(font_list, delegate(),
+                                                   delegate(), profile_.get());
     view->SetBoundsRect(gfx::Rect(0, 0, 24, 24));
     view_ = widget_->SetContentsView(std::move(view));
 
@@ -81,6 +84,7 @@ class LocationIconViewTest : public ChromeViewsTestBase {
 
   void TearDown() override {
     widget_.reset();
+    profile_.reset();
     ChromeViewsTestBase::TearDown();
   }
 
@@ -107,6 +111,7 @@ class LocationIconViewTest : public ChromeViewsTestBase {
   std::unique_ptr<TestLocationIconDelegate> delegate_;
   LocationIconView* view_;
   std::unique_ptr<views::Widget> widget_;
+  std::unique_ptr<TestingProfile> profile_;
 };
 
 TEST_F(LocationIconViewTest, ShouldNotAnimateWhenSuppressingAnimations) {
