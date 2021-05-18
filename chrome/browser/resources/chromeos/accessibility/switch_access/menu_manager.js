fe8bcbe1b5f052b40635e0dc432c0f635e99877e
Josiah Krutz <josiahk@google.com>
2021-05-18 19:42:10 +0000

    Revert "[Switch Access] Prevent item highlight in point scanning mode"
    This reverts commit 797e495573ea7cf957eebc600418219919e1373a.
    
    Fixed: 1206042
    
    Original change's description:
    > [Switch Access] Prevent item highlight in point scanning mode
    >
    > This change prevents an item scanning highlight from showing up while
    > the user is navigating with point scanning, with the exception of
    > within the Switch Access action menu.
    >
    > AX-Relnotes: n/a.
    > Bug: 1202164
    > Change-Id: I59aea74d2facb19ab24f91453aff15ed36b415cd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2848529
    > Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    > Reviewed-by: Josiah Krutz <josiahk@google.com>
    > Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    > Cr-Commit-Position: refs/heads/master@{#875874}
    
    Bug: 1202164
    Change-Id: I2a27d33e942806170ca846fc0a03cd66881c26fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2901639
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/master@{#884090}

--- chrome/browser/resources/chromeos/accessibility/switch_access/menu_manager.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/menu_manager.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {ActionManager} from './action_manager.js';
-import {FocusRingManager} from './focus_ring_manager.js';
 import {Navigator} from './navigator.js';
 import {SwitchAccess} from './switch_access.js';
 import {SwitchAccessMenuAction} from './switch_access_constants.js';
@@ -69,7 +68,6 @@ export class MenuManager {
 
   /** Exits the menu. */
   static close() {
-    FocusRingManager.clearAll();
     MenuManager.instance.isMenuOpen_ = false;
     MenuManager.instance.actionNode_ = null;
     MenuManager.instance.displayedActions_ = null;
--- chrome/browser/resources/chromeos/accessibility/switch_access/point_scan_manager.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/point_scan_manager.js
@@ -30,8 +30,8 @@ export class PointScanManager extends PointNavigatorInterface {
 
   /** @override */
   start() {
-    FocusRingManager.clearAll();
     SwitchAccess.mode = SAConstants.Mode.POINT_SCAN;
+    FocusRingManager.clearAll();
     chrome.accessibilityPrivate.onPointScanSet.addListener(this.pointListener_);
     chrome.accessibilityPrivate.setPointScanState(PointScanState.START);
   }
