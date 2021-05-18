eb2b8e2c7996efeab033030a0bff2be2cc6a6124
Yue Zhang <yuezhanggg@chromium.org>
2021-05-18 23:07:29 +0000

    [RBD] Fetch discount URL for partner merchants
    Bug: 1210166
    Change-Id: I4c7e4c1af952e4388422e6365761c4195743dd24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2900799
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884205}

--- chrome/browser/cart/cart_service.h
+++ chrome/browser/cart/cart_service.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/values.h"
 #include "chrome/browser/cart/cart_db.h"
+#include "chrome/browser/cart/cart_db_content.pb.h"
+#include "chrome/browser/cart/cart_discount_link_fetcher.h"
 #include "chrome/browser/cart/cart_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/history/core/browser/history_service.h"
@@ -101,6 +103,7 @@ class CartService : public history::HistoryServiceObserver,
  private:
   friend class CartServiceFactory;
   friend class CartServiceTest;
+  friend class CartServiceDiscountTest;
   FRIEND_TEST_ALL_PREFIXES(CartHandlerNtpModuleFakeDataTest,
                            TestEnableFakeData);
 
@@ -143,11 +146,24 @@ class CartService : public history::HistoryServiceObserver,
 
   // Gets called when users has enabled the rule-based discount feature.
   void StartGettingDiscount();
+  // A callback to fetch discount URL.
+  void OnGetDiscountURL(const GURL& default_cart_url,
+                        base::OnceCallback<void(const ::GURL&)> callback,
+                        bool success,
+                        std::vector<CartDB::KeyAndValue> proto_pairs);
+  // A callback to return discount URL when it is fetched.
+  void OnDiscountURLFetched(const GURL& default_cart_url,
+                            base::OnceCallback<void(const ::GURL&)> callback,
+                            const cart_db::ChromeCartContentProto& cart_proto,
+                            const GURL& discount_url);
 
   // A callback to decide if there are partner carts.
   void HasPartnerCarts(base::OnceCallback<void(bool)> callback,
                        bool success,
                        std::vector<CartDB::KeyAndValue> proto_pairs);
+  // Set discount_link_fetcher_ for testing purpose.
+  void SetCartDiscountLinkFetcherForTesting(
+      std::unique_ptr<CartDiscountLinkFetcher> discount_link_fetcher);
 
   void CacheUsedDiscounts(const cart_db::ChromeCartContentProto& proto);
   void CleanUpDiscounts(cart_db::ChromeCartContentProto proto);
@@ -160,6 +176,7 @@ class CartService : public history::HistoryServiceObserver,
   absl::optional<base::Value> domain_name_mapping_;
   absl::optional<base::Value> domain_cart_url_mapping_;
   std::unique_ptr<FetchDiscountWorker> fetch_discount_worker_;
+  std::unique_ptr<CartDiscountLinkFetcher> discount_link_fetcher_;
   base::WeakPtrFactory<CartService> weak_ptr_factory_{this};
 };
 
