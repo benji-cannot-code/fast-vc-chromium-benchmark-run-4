e3bf9513bd85d33fc75601b9dc1316fec697c1b3
Clemens Arbesser <arbesser@google.com>
2021-05-18 14:14:07 +0000

    Reland "[Autofill Assistant] Forward trigger_ui_type to regular script startup"
    This is a reland of 7f3e0ef1906c784768643f0e78bebe99d95b5c08
    
    An intermittent change from base::nullopt (and others) to absl::nullopt
    was not properly reflected in the rebase and caused a compile error.
    
    Original change's description:
    > [Autofill Assistant] Forward trigger_ui_type to regular script startup
    >
    > This will let the backend know which type of trigger script the user
    > saw and accepted.
    >
    > Bug: b/173495422
    > Change-Id: If477bc562eba332bc412e513011e81e78a955966
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879983
    > Commit-Queue: Clemens Arbesser <arbesser@google.com>
    > Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#883860}
    
    Bug: b/173495422
    Change-Id: I22e128db5a9a82682ecf8490d8cf716c7bb807cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2902965
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#883946}

--- components/autofill_assistant/browser/trigger_context_unittest.cc
+++ components/autofill_assistant/browser/trigger_context_unittest.cc
@@ -45,9 +45,12 @@ TEST(TriggerContextTest, Create) {
   EXPECT_TRUE(context.GetDirectAction());
   EXPECT_EQ(context.GetInitialUrl(), "https://www.example.com");
   EXPECT_TRUE(context.GetInChromeTriggered());
+  EXPECT_EQ(context.GetTriggerUIType(), UNSPECIFIED_TRIGGER_UI_TYPE);
 
   context.SetOnboardingShown(false);
   EXPECT_FALSE(context.GetOnboardingShown());
+  context.SetTriggerUIType(CART_FIRST_TIME_USER);
+  EXPECT_EQ(context.GetTriggerUIType(), CART_FIRST_TIME_USER);
 }
 
 TEST(TriggerContextTest, MergeEmpty) {
@@ -59,6 +62,7 @@ TEST(TriggerContextTest, MergeEmpty) {
   EXPECT_FALSE(merged.GetOnboardingShown());
   EXPECT_FALSE(merged.GetDirectAction());
   EXPECT_FALSE(merged.GetInChromeTriggered());
+  EXPECT_EQ(merged.GetTriggerUIType(), UNSPECIFIED_TRIGGER_UI_TYPE);
 }
 
 TEST(TriggerContextTest, MergeEmptyWithNonEmpty) {
@@ -77,6 +81,8 @@ TEST(TriggerContextTest, MergeEmptyWithNonEmpty) {
   EXPECT_FALSE(merged.GetCCT());
   EXPECT_FALSE(merged.GetOnboardingShown());
   EXPECT_FALSE(merged.GetDirectAction());
+  EXPECT_FALSE(merged.GetInChromeTriggered());
+  EXPECT_EQ(merged.GetTriggerUIType(), UNSPECIFIED_TRIGGER_UI_TYPE);
 }
 
 TEST(TriggerContextTest, MergeNonEmptyWithNonEmpty) {
@@ -95,6 +101,7 @@ TEST(TriggerContextTest, MergeNonEmptyWithNonEmpty) {
       /* is_direct_action = */ true,
       /* initial_url = */ "https://www.example.com",
       /* is_in_chrome_triggered = */ true};
+  context2.SetTriggerUIType(CHECKOUT_FIRST_TIME_USER);
 
   // Adding empty to make sure empty contexts are properly skipped.
   TriggerContext empty;
@@ -108,6 +115,7 @@ TEST(TriggerContextTest, MergeNonEmptyWithNonEmpty) {
   EXPECT_TRUE(merged.GetDirectAction());
   EXPECT_EQ(merged.GetInitialUrl(), "https://www.example.com");
   EXPECT_TRUE(merged.GetInChromeTriggered());
+  EXPECT_EQ(merged.GetTriggerUIType(), CHECKOUT_FIRST_TIME_USER);
 }
 
 TEST(TriggerContextTest, HasExperimentId) {
