6f2e186cc6b4e56efcb872f5ff60b5bf6fa19a89
Takuto Ikuta <tikuta@chromium.org>
2021-05-18 07:52:53 +0000

    build: run nocompile_test in python3
    Fixed: 1210296
    Change-Id: I29c5f660dc268115ca0ee8b56627c50249376650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2901682
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883848}

--- tools/nocompile_driver.py
+++ tools/nocompile_driver.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -16,8 +16,8 @@ For more info, see:
 
 from __future__ import print_function
 
-import StringIO
 import ast
+import io
 import os
 import re
 import select
@@ -226,8 +226,8 @@ def StartTest(compiler, sourcefile_path, tempfile_dir, cflags, config):
     cmdline.append('-D%s' % name)
   cmdline.extend(['-o', '/dev/null', '-c', '-x', 'c++',
                   sourcefile_path])
-  test_stdout = tempfile.TemporaryFile(dir=tempfile_dir)
-  test_stderr = tempfile.TemporaryFile(dir=tempfile_dir)
+  test_stdout = tempfile.TemporaryFile(dir=tempfile_dir, mode='w+')
+  test_stderr = tempfile.TemporaryFile(dir=tempfile_dir, mode='w+')
 
   process = subprocess.Popen(cmdline, stdout=test_stdout, stderr=test_stderr)
   now = time.time()
@@ -449,8 +449,8 @@ def main():
   test_configs = ExtractTestConfigs(sourcefile_path, suite_name)
   timings['extract_done'] = time.time()
 
-  resultfile = StringIO.StringIO()
-  resultlog = StringIO.StringIO()
+  resultfile = io.StringIO()
+  resultlog = io.StringIO()
   resultfile.write(RESULT_FILE_HEADER % sourcefile_path)
 
   # Run the no-compile tests, but ensure we do not run more than |parallelism|
