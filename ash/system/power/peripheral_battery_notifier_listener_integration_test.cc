cc8b422cf01535f4e84876ace34db80147178b56
Kenneth Albanowski <kenalba@google.com>
2021-05-18 18:46:06 +0000

    [stylus] Fix leaked observer
    PeripheralBatteryListener was causing crashes in a
    variety of AshTestBase derived unit tests by leaving
    itself as an observer on ui::DDM after its destruction.
    
    Fixes that, and removes adjustments that had been
    needed for several unit tests.
    
    Bug: 1113228
    Test: Verified tests "*WindowCycle*", "PeripheralBattery*Test.*" and operation on dedede.
    Change-Id: Ie442a58cebc4cd45f0049ef977d13f2fce0727e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2901662
    Auto-Submit: Kenneth Albanowski <kenalba@google.com>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Harry Cutts <hcutts@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884046}

--- ash/system/power/peripheral_battery_notifier_listener_integration_test.cc
+++ ash/system/power/peripheral_battery_notifier_listener_integration_test.cc
@@ -62,8 +62,6 @@ class PeripheralBatteryNotifierListenerTest : public AshTestBase {
   ~PeripheralBatteryNotifierListenerTest() override = default;
 
   void SetUp() override {
-    ui::DeviceDataManager::CreateInstance();
-    chromeos::PowerManagerClient::InitializeFake();
     AshTestBase::SetUp();
     ASSERT_TRUE(ui::DeviceDataManager::HasInstance());
 
@@ -90,11 +88,9 @@ class PeripheralBatteryNotifierListenerTest : public AshTestBase {
   }
 
   void TearDown() override {
-    AshTestBase::TearDown();
     battery_notifier_.reset();
     battery_listener_.reset();
-    chromeos::PowerManagerClient::Shutdown();
-    ui::DeviceDataManager::DeleteInstance();
+    AshTestBase::TearDown();
   }
 
   void SendBatteryUpdate(const std::string& path,
