fd9957128f6b6c0891a60c84caa173ce9bc9851f
Anton Bikineev <bikineev@chromium.org>
2021-05-18 12:29:22 +0000

    Deprecate base/optional.h and fix remaining mentions.
    Docs are still to be updated (in a followup).
    
    Bug: 1202909
    Change-Id: I4a28240be2a8d9358c286ee1cf992c1f529c3369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2902810
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Owners-Override: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883910}

--- ios/chrome/browser/link_to_text/link_to_text_tab_helper.mm
+++ ios/chrome/browser/link_to_text/link_to_text_tab_helper.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/bind.h"
 #import "base/metrics/histogram_functions.h"
-#import "base/optional.h"
 #import "base/timer/elapsed_timer.h"
 #import "base/values.h"
 #import "components/shared_highlighting/core/common/disabled_sites.h"
@@ -16,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/js_messaging/web_frames_manager.h"
 #import "ios/web/public/ui/crw_web_view_proxy.h"
 #import "ios/web/public/ui/crw_web_view_scroll_view_proxy.h"
+#import "third_party/abseil-cpp/absl/types/optional.h"
 #import "url/gurl.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
--- ios/chrome/browser/link_to_text/link_to_text_utils.h
+++ ios/chrome/browser/link_to_text/link_to_text_utils.h
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#import "base/optional.h"
 #import "components/shared_highlighting/core/common/shared_highlighting_metrics.h"
 #import "ios/chrome/browser/link_to_text/link_generation_outcome.h"
+#import "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace base {
 class TimeDelta;
