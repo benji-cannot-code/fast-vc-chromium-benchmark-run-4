23304792b6cbf53ed16d55aefc663e607260e90d
Patrick Monette <pmonette@chromium.org>
2021-05-18 19:08:54 +0000

    Notify the select file dialog when SearchTabHelper is destroyed.
    Bug: 1201032
    Change-Id: I444f9f691db1acaf21cee30a297d74d11ba7a2eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2896565
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884075}

--- chrome/browser/ui/search/search_tab_helper.cc
+++ chrome/browser/ui/search/search_tab_helper.cc
@@ -146,6 +146,8 @@ SearchTabHelper::~SearchTabHelper() {
     instant_service_->RemoveObserver(this);
   if (auto* helper = OmniboxTabHelper::FromWebContents(web_contents_))
     helper->RemoveObserver(this);
+  if (select_file_dialog_)
+    select_file_dialog_->ListenerDestroyed();
 }
 
 void SearchTabHelper::OnTabActivated() {
