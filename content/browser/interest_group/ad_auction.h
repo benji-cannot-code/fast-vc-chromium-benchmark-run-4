978d0d9b2217979ffe89de1e0bed3a1bf1684c15
Matt Menke <mmenke@chromium.org>
2021-05-18 22:40:13 +0000

    FLEDGE: Make one AuctionURLLoaderFactoryProxy per worklet.
    Each proxy object now only allows requests from a particular worklet
    for the scripts it needs, rather than having a shared URLLoaderFactory.
    
    Once each worklets is in its own process, this will prevent process
    compromises from letting worklets download each other's scripts.
    
    This CL also moves AuctionURLLoaderFactoryProxy creation and lifetime
    management from AdAuction to AuctionRunner.
    
    Bug: 1205517
    Change-Id: I3cf34b23ae850ebcafc58d86ff2f13a80914f807
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2894548
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884191}

--- content/browser/interest_group/ad_auction.h
+++ content/browser/interest_group/ad_auction.h
@@ -22,7 +22,6 @@ namespace content {
 
 class AdAuctionServiceImpl;
 class AuctionRunner;
-class AuctionURLLoaderFactoryProxy;
 
 // Class for running a single FLEDGE auction.
 class AdAuction {
@@ -90,9 +89,6 @@ class AdAuction {
   // Buyers from `config->interest_group_buyers` that have yet to be retrieved
   // from interest group storage.
   std::vector<url::Origin> pending_buyers_;
-
-  // Proxy used for requests from the worklet process.
-  std::unique_ptr<AuctionURLLoaderFactoryProxy> url_loader_factory_proxy_;
 
   std::unique_ptr<AuctionRunner> auction_runner_;
 
