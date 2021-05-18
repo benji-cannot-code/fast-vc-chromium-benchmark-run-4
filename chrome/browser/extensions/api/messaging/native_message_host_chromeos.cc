59f741736824e28ecaa48ea62b4225bf42a5ba88
Andrii Mostovyi <amostovyi@google.com>
2021-05-18 15:04:00 +0000

    Add native message host for Crostini SK Forwarding
    This built-in in native message host in Chrome OS will
    allow Crostini VMs to forward Security Key requests to
    SK Chrome Extension. The extension will then forward requests
    to gnubbyd app/extension.
    
    BUG=b:179251496
    TEST=manual tests with all depending CLs on ChromeOS device
    
    Change-Id: Ic06953ee7ec395056d9de160c6401b6f474a0ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2871588
    Commit-Queue: Andrii Mostovyi <amostovyi@google.com>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883966}

--- chrome/browser/extensions/api/messaging/native_message_host_chromeos.cc
+++ chrome/browser/extensions/api/messaging/native_message_host_chromeos.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "chrome/browser/ash/arc/extensions/arc_support_message_host.h"
 #include "chrome/browser/ash/drive/drivefs_native_message_host.h"
+#include "chrome/browser/ash/guest_os/vm_sk_forwarding_native_message_host.h"
 #include "chrome/browser/ash/wilco_dtc_supportd/wilco_dtc_supportd_messaging.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/api/messaging/native_message_built_in_host.h"
@@ -50,6 +51,10 @@ const NativeMessageBuiltInHost kBuiltInHosts[] = {
      drive::kDriveFsNativeMessageHostOrigins,
      drive::kDriveFsNativeMessageHostOriginsSize,
      &drive::CreateDriveFsNativeMessageHost},
+    {ash::guest_os::VmSKForwardingNativeMessageHost::kHostName,
+     ash::guest_os::VmSKForwardingNativeMessageHost::kOrigins,
+     ash::guest_os::VmSKForwardingNativeMessageHost::kOriginCount,
+     &ash::guest_os::VmSKForwardingNativeMessageHost::CreateFromExtension},
 };
 
 const size_t kBuiltInHostsCount = base::size(kBuiltInHosts);
