1c013224dbed1f48f72821276dba763d1540638b
Luca Hunkeler <hluca@google.com>
2021-05-18 14:56:57 +0000

    [Autofill Assistant] Access selected profile from generic UI
    A follow up cl will do the equivalent change for selected
    credit cards
    
    Bug: b/180494462, b/182893140
    Change-Id: I072d3e7f561efd8c10e9412ef550cf53d377cd26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2900480
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#883963}

--- components/autofill_assistant/browser/basic_interactions.cc
+++ components/autofill_assistant/browser/basic_interactions.cc
@@ -188,8 +188,7 @@ bool ValueToString(UserModel* user_model,
           DVLOG(2) << "Error evaluating " << __func__ << ": pattern not set";
           return false;
         }
-        auto* profile =
-            user_model->GetProfile(value->profiles().values(i).guid());
+        auto* profile = user_model->GetProfile(value->profiles().values(i));
         if (!profile) {
           DVLOG(2) << "Error evaluating " << __func__ << ": profile not found";
           return false;
