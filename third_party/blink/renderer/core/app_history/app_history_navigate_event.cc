b4c4c4cd4607b0a0aaa5a92746fdc56309cf2323
Yuki Shiino <yukishiino@chromium.org>
2021-05-18 09:06:44 +0000

    bind-gen: Fix file listing about the new IDL dictionaries
    v8_dict.h should be included instead of dict.h.
    Also removes unused files from *.gni.
    
    Bug: 839389
    Change-Id: I93396b303f35f8bc80db8695a3b344d57de66bff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2900016
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#883869}

--- third_party/blink/renderer/core/app_history/app_history_navigate_event.cc
+++ third_party/blink/renderer/core/app_history/app_history_navigate_event.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/app_history/app_history_navigate_event.h"
 
-#include "third_party/blink/renderer/core/app_history/app_history_navigate_event_init.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_app_history_navigate_event_init.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/event_interface_names.h"
 #include "third_party/blink/renderer/core/event_type_names.h"
