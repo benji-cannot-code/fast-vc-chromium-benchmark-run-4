707bf580a9e17e74d351d175d68e81cc571f07bf
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-18 18:56:04 +0000

    Retire ScopedObserver in /ios/chrome/browser/snapshots.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=justincohen@chromium.org
    
    Bug: 1145565
    Change-Id: I5629c4ef074273c70a301eb92b1df25983c979d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2904166
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884064}

--- ios/chrome/browser/snapshots/snapshot_tab_helper.h
+++ ios/chrome/browser/snapshots/snapshot_tab_helper.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "ios/web/public/web_state_observer.h"
 #import "ios/web/public/web_state_user_data.h"
 
@@ -96,7 +96,8 @@ class SnapshotTabHelper : public web::WebStateObserver,
   SnapshotGenerator* snapshot_generator_ = nil;
 
   // Manages this object as an observer of |web_state_|.
-  ScopedObserver<web::WebState, web::WebStateObserver> web_state_observer_;
+  base::ScopedObservation<web::WebState, web::WebStateObserver>
+      web_state_observation_{this};
 
   bool ignore_next_load_ = false;
 
