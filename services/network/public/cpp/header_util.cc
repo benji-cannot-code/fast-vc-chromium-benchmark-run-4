30d953735ba0ff9ea7a1886c5d9c04b456f3a55b
Andrey Kosyakov <caseq@chromium.org>
2021-05-19 05:51:12 +0000

    Make mime type sniffing in DevToolsURLLoaderInterceptor consistent with real URLLoader
    - extract ShouldSniffContent() from url_loader.cc to header_util.{h,cc}
    - reuse it in DevToolsURLLoaderInterceptor
    
    Bug: b/187745024
    Change-Id: I0cd6904e7912c372aa744b0abb038d867c9195d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2905075
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884328}

--- services/network/public/cpp/header_util.cc
+++ services/network/public/cpp/header_util.cc
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/header_util.h"
 
 #include "base/strings/string_util.h"
+#include "net/base/mime_sniffer.h"
 #include "net/http/http_request_headers.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
+#include "url/gurl.h"
 
 namespace network {
 
@@ -92,6 +95,21 @@ bool AreRequestHeadersSafe(const net::HttpRequestHeaders& request_headers) {
   }
 
   return true;
+}
+
+bool ShouldSniffContent(const GURL& url,
+                        const mojom::URLResponseHead& response) {
+  std::string content_type_options;
+  if (response.headers) {
+    response.headers->GetNormalizedHeader("x-content-type-options",
+                                          &content_type_options);
+  }
+  bool sniffing_blocked =
+      base::LowerCaseEqualsASCII(content_type_options, "nosniff");
+  bool we_would_like_to_sniff =
+      net::ShouldSniffMimeType(url, response.mime_type);
+
+  return !sniffing_blocked && we_would_like_to_sniff;
 }
 
 }  // namespace network
