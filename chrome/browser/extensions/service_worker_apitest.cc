9d9755de9d863261a9b240f7efa0d31f3d9a3d89
Emilia Paz <emiliapaz@chromium.org>
2021-05-19 20:16:47 +0000

    [Extensions] chrome.storage: Add session namespace for Storage API
    This cl has 4 main changes:
    a) Introduce `session` namespace in chrome.storage schema and set its
       channel to "canary" so we can test SettingsFunctions before
       `session` is on production.
    b) Add `session` to renderer in SessionStorageArea class
    c) Add RunInSession() in SettingsFunction for `session` namespace.
    d) Implement Set and Get RunInSession() to test session namespace is
       properly introduced.
    a) Update current browser tests. These will run in unknown channel
       while `session` namespace is running in canary.
    
    Bug: 1185226
    Change-Id: Id7b8d912adcd269e2a0703a2c1326165e690e211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2760538
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884655}

--- chrome/browser/extensions/service_worker_apitest.cc
+++ chrome/browser/extensions/service_worker_apitest.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/gcm_driver/fake_gcm_profile_service.h"
 #include "components/gcm_driver/instance_id/fake_gcm_driver_for_instance_id.h"
+#include "components/version_info/channel.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/console_message.h"
 #include "content/public/browser/navigation_controller.h"
@@ -69,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/service_worker_task_queue.h"
 #include "extensions/common/api/test.h"
 #include "extensions/common/extensions_client.h"
+#include "extensions/common/features/feature_channel.h"
 #include "extensions/common/manifest_handlers/background_info.h"
 #include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/value_builder.h"
@@ -290,6 +292,24 @@ class ServiceWorkerBasedBackgroundTest : public ServiceWorkerTest {
   DISALLOW_COPY_AND_ASSIGN(ServiceWorkerBasedBackgroundTest);
 };
 
+// A specialization of ExtensionSettingsApiTest that pretends it's running
+// on version_info::Channel::UNKNOWN.
+class ServiceWorkerBasedBackgroundTrunkTest
+    : public ServiceWorkerBasedBackgroundTest {
+ public:
+  ServiceWorkerBasedBackgroundTrunkTest() = default;
+  ~ServiceWorkerBasedBackgroundTrunkTest() override = default;
+  ServiceWorkerBasedBackgroundTrunkTest(
+      const ServiceWorkerBasedBackgroundTrunkTest& other) = delete;
+  ServiceWorkerBasedBackgroundTrunkTest& operator=(
+      const ServiceWorkerBasedBackgroundTrunkTest& other) = delete;
+
+ private:
+  // TODO(crbug.com/1185226): Remove unknown channel when chrome.storage.session
+  // is released in stable.
+  ScopedCurrentChannel current_channel_{version_info::Channel::UNKNOWN};
+};
+
 class ServiceWorkerBasedBackgroundTestWithNotification
     : public ServiceWorkerBasedBackgroundTest {
  public:
@@ -426,14 +446,22 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerBasedBackgroundTest, RuntimeMisc) {
 }
 
 // Tests chrome.storage APIs.
-IN_PROC_BROWSER_TEST_F(ServiceWorkerBasedBackgroundTest, StorageSetAndGet) {
+// TODO(crbug.com/1185226): Change parent class to
+// `ServiceWorkerBasedBackgroundTest` when chrome.storage.session is released in
+// stable.
+IN_PROC_BROWSER_TEST_F(ServiceWorkerBasedBackgroundTrunkTest,
+                       StorageSetAndGet) {
   ASSERT_TRUE(
       RunExtensionTest("service_worker/worker_based_background/storage"))
       << message_;
 }
 
-// Tests chrome.storage.local and chrome.storage.local APIs.
-IN_PROC_BROWSER_TEST_F(ServiceWorkerBasedBackgroundTest, StorageNoPermissions) {
+// Tests chrome.storage APIs are only enabled with permission.
+// TODO(crbug.com/1185226): Change parent class to
+// `ServiceWorkerBasedBackgroundTest` when chrome.storage.session is released in
+// stable.
+IN_PROC_BROWSER_TEST_F(ServiceWorkerBasedBackgroundTrunkTest,
+                       StorageNoPermissions) {
   ASSERT_TRUE(RunExtensionTest(
       "service_worker/worker_based_background/storage_no_permissions"))
       << message_;
--- extensions/browser/api/storage/storage_api.h
+++ extensions/browser/api/storage/storage_api.h
@@ -30,6 +30,10 @@ class SettingsFunction : public ExtensionFunction {
   // The StorageFrontend makes sure this is posted to the appropriate thread.
   virtual ResponseValue RunWithStorage(ValueStore* storage) = 0;
 
+  // Extension settings function implementations in `session` namespace should
+  // do their work here.
+  virtual ResponseValue RunInSession() = 0;
+
   // Convert the |result| of a read function to the appropriate response value.
   // - If the |result| succeeded this will return a response object argument.
   // - If the |result| failed will return an error object.
@@ -68,6 +72,7 @@ class StorageStorageAreaGetFunction : public SettingsFunction {
 
   // SettingsFunction:
   ResponseValue RunWithStorage(ValueStore* storage) override;
+  ResponseValue RunInSession() override;
 };
 
 class StorageStorageAreaSetFunction : public SettingsFunction {
@@ -79,6 +84,7 @@ class StorageStorageAreaSetFunction : public SettingsFunction {
 
   // SettingsFunction:
   ResponseValue RunWithStorage(ValueStore* storage) override;
+  ResponseValue RunInSession() override;
 
   // ExtensionFunction:
   void GetQuotaLimitHeuristics(QuotaLimitHeuristics* heuristics) const override;
@@ -93,6 +99,7 @@ class StorageStorageAreaRemoveFunction : public SettingsFunction {
 
   // SettingsFunction:
   ResponseValue RunWithStorage(ValueStore* storage) override;
+  ResponseValue RunInSession() override;
 
   // ExtensionFunction:
   void GetQuotaLimitHeuristics(QuotaLimitHeuristics* heuristics) const override;
@@ -107,6 +114,7 @@ class StorageStorageAreaClearFunction : public SettingsFunction {
 
   // SettingsFunction:
   ResponseValue RunWithStorage(ValueStore* storage) override;
+  ResponseValue RunInSession() override;
 
   // ExtensionFunction:
   void GetQuotaLimitHeuristics(QuotaLimitHeuristics* heuristics) const override;
@@ -121,6 +129,7 @@ class StorageStorageAreaGetBytesInUseFunction : public SettingsFunction {
 
   // SettingsFunction:
   ResponseValue RunWithStorage(ValueStore* storage) override;
+  ResponseValue RunInSession() override;
 };
 
 }  // namespace extensions
