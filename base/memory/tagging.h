7d4e6ce7f3852ec7f7e57c9c30087b79c7d838ad
Gabor Kertesz <gabor.kertesz@arm.com>
2021-05-19 11:32:52 +0000

    Call mallopt for tagging mode on Android
    This patch creates new API for Android, setting tagging mode for
    all threads per process by mallopt, instead of per thread by prctl.
    Also as mallopt is published by NDK API level 26 and the supported
    NDK API level of Chromium is 21, we call mallopt on Android by
    dynamic linker.
    
    Bug: 1137393
    Change-Id: I2680216cf83cad81f0c8e3213674ae253a48b629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2879976
    Commit-Queue: Richard Townsend <richard.townsend@arm.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884398}

--- base/memory/tagging.h
+++ base/memory/tagging.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file contains method definitions to support Armv8.5-A's memory tagging
 // extension.
 #include "base/base_export.h"
+#include "build/build_config.h"
 
 namespace base {
 namespace memory {
@@ -23,9 +24,15 @@ enum class TagViolationReportingMode {
   kAsynchronous,
 };
 
+//#if defined(OS_ANDROID)
+// Changes the memory tagging mode for all threads in the current process.
+BASE_EXPORT void ChangeMemoryTaggingModeForAllThreadsPerProcess(
+    TagViolationReportingMode);
+//#else
 // Changes the memory tagging mode for the calling thread.
 BASE_EXPORT void ChangeMemoryTaggingModeForCurrentThread(
     TagViolationReportingMode);
+//#endif  // defined(OS_ANDROID)
 
 // Gets the memory tagging mode for the calling thread.
 BASE_EXPORT TagViolationReportingMode GetMemoryTaggingModeForCurrentThread();
