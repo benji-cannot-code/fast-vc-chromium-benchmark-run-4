e19d6e62102f6ca47cdb0b42cebbf06c3d27ee0e
Sigurdur Asgeirsson <siggi@chromium.org>
2021-05-19 14:37:33 +0000

    Retire ScopedObserver in /ios/chrome/browser/web.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    This CL was uploaded by git cl split.
    
    R=gambard@chromium.org
    
    Bug: 1145565
    Change-Id: Ic71ab203ea19bce916930e4e77236758c3bcdfa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2904172
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884468}

--- ios/chrome/browser/web/blocked_popup_tab_helper.h
+++ ios/chrome/browser/web/blocked_popup_tab_helper.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_observation.h"
 #include "components/infobars/core/infobar_manager.h"
 #include "ios/web/public/navigation/referrer.h"
 #import "ios/web/public/web_state_user_data.h"
@@ -83,8 +83,9 @@ class BlockedPopupTabHelper
   // For management of infobars::InfoBarManager::Observer registration.  This
   // object will not start observing the InfoBarManager until ShowInfoBars() is
   // called.
-  ScopedObserver<infobars::InfoBarManager, infobars::InfoBarManager::Observer>
-      scoped_observer_;
+  base::ScopedObservation<infobars::InfoBarManager,
+                          infobars::InfoBarManager::Observer>
+      scoped_observation_{this};
 
   WEB_STATE_USER_DATA_KEY_DECL();
 
