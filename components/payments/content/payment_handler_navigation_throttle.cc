0730837210265e2fc816c65527576443a66df2ab
Liquan (Max) Gu <maxlg@chromium.org>
2021-05-19 20:50:58 +0000

    PaymentHandlerNavigationThrottle null-checks pointers before use
    Crashes happened in
    PaymentHandlerNavigationThrottle::MaybeCreateThrottleFor(). It's
    possible that NavigationHandle or its WebContents was null. This is a
    tentative fix based on this hypothesis.
    
    Bug: 1193740
    Change-Id: I76974ee0c7ffc5dcc3f1990fb67bb084aedb6f9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2904182
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884668}

--- components/payments/content/payment_handler_navigation_throttle.cc
+++ components/payments/content/payment_handler_navigation_throttle.cc
@@ -41,7 +41,8 @@ void PaymentHandlerNavigationThrottle::MarkPaymentHandlerWebContents(
 std::unique_ptr<PaymentHandlerNavigationThrottle>
 PaymentHandlerNavigationThrottle::MaybeCreateThrottleFor(
     content::NavigationHandle* handle) {
-  if (!handle->GetWebContents()->GetUserData(
+  if (!handle || !handle->GetWebContents() ||
+      !handle->GetWebContents()->GetUserData(
           kPaymentHandlerWebContentsUserDataKey)) {
     return nullptr;
   }
@@ -50,6 +51,8 @@ PaymentHandlerNavigationThrottle::MaybeCreateThrottleFor(
 
 content::NavigationThrottle::ThrottleCheckResult
 PaymentHandlerNavigationThrottle::WillProcessResponse() {
+  if (!navigation_handle())
+    return PROCEED;
   const net::HttpResponseHeaders* response_headers =
       navigation_handle()->GetResponseHeaders();
   if (!response_headers)
