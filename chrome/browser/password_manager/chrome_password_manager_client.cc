9fab331a6e8e36d84568e700027c05c7c39adfe0
Viktor Semeniuk <vsemeniuk@google.com>
2021-05-19 16:44:19 +0000

    Revert "Strenthen the security check for a compromised renderer in"
    This reverts commit 37ad1850f77f9fde1d47383f305040a9bc0eb292.
    
    Reason for revert: Causes crashes on Chrome OS e.g. https://crash.corp.google.com/browse?stbtiq=1adb15bb3d211ed0
    Bug: 1208994
    
    Original change's description:
    > Strenthen the security check for a compromised renderer in
    > ContentPasswordManagerDriver.
    >
    > - Some new methods were not checking FormData::url coming from renderer.
    > - Additionally now they also check FormData::full_url.
    >
    > Bug: 1206049
    > Change-Id: Ic09f35df1aa13e54479e3ee6fda86b46ef778f4a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2878213
    > Reviewed-by: Vadym Doroshenko  <dvadym@chromium.org>
    > Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#880309}
    
    Bug: 1206049
    Change-Id: I8a8ba74e67d3f402c5b5177391cb39c2d324d2b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906053
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#884527}

--- chrome/browser/password_manager/chrome_password_manager_client.cc
+++ chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -933,9 +933,9 @@ FieldInfoManager* ChromePasswordManagerClient::GetFieldInfoManager() const {
 
 void ChromePasswordManagerClient::AutomaticGenerationAvailable(
     const autofill::password_generation::PasswordGenerationUIData& ui_data) {
-  if (!password_manager::bad_message::CheckChildProcessSecurityPolicy(
+  if (!password_manager::bad_message::CheckChildProcessSecurityPolicyForURL(
           password_generation_driver_receivers_.GetCurrentTargetFrame(),
-          base::make_span(&ui_data.form_data, 1),
+          ui_data.form_data.url,
           BadMessageReason::
               CPMD_BAD_ORIGIN_AUTOMATIC_GENERATION_STATUS_CHANGED))
     return;
@@ -981,9 +981,9 @@ void ChromePasswordManagerClient::ShowPasswordEditingPopup(
     const autofill::FormData& form_data,
     autofill::FieldRendererId field_renderer_id,
     const std::u16string& password_value) {
-  if (!password_manager::bad_message::CheckChildProcessSecurityPolicy(
+  if (!password_manager::bad_message::CheckChildProcessSecurityPolicyForURL(
           password_generation_driver_receivers_.GetCurrentTargetFrame(),
-          base::make_span(&form_data, 1),
+          form_data.url,
           BadMessageReason::CPMD_BAD_ORIGIN_SHOW_PASSWORD_EDITING_POPUP))
     return;
   auto* driver = driver_factory_->GetDriverForFrame(
@@ -1014,9 +1014,9 @@ void ChromePasswordManagerClient::PasswordGenerationRejectedByTyping() {
 void ChromePasswordManagerClient::PresaveGeneratedPassword(
     const autofill::FormData& form_data,
     const std::u16string& password_value) {
-  if (!password_manager::bad_message::CheckChildProcessSecurityPolicy(
+  if (!password_manager::bad_message::CheckChildProcessSecurityPolicyForURL(
           password_generation_driver_receivers_.GetCurrentTargetFrame(),
-          base::make_span(&form_data, 1),
+          form_data.url,
           BadMessageReason::CPMD_BAD_ORIGIN_PRESAVE_GENERATED_PASSWORD)) {
     return;
   }
@@ -1031,9 +1031,9 @@ void ChromePasswordManagerClient::PresaveGeneratedPassword(
 
 void ChromePasswordManagerClient::PasswordNoLongerGenerated(
     const autofill::FormData& form_data) {
-  if (!password_manager::bad_message::CheckChildProcessSecurityPolicy(
+  if (!password_manager::bad_message::CheckChildProcessSecurityPolicyForURL(
           password_generation_driver_receivers_.GetCurrentTargetFrame(),
-          base::make_span(&form_data, 1),
+          form_data.url,
           BadMessageReason::CPMD_BAD_ORIGIN_PASSWORD_NO_LONGER_GENERATED)) {
     return;
   }
