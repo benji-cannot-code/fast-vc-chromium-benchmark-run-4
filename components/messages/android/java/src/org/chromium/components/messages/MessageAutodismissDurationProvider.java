833443182d0c2fe6c43eb17afa319d553c22c341
Lijin Shen <lazzzis@google.com>
2021-05-19 21:25:18 +0000

    [Messages] Allow clients to extend dismiss duration
    This CL introduces a property to allow clients to custom the duration.
    
    The value will be applied in both a11y and non-a11y mode.
    
    Bug: 1190532
    Change-Id: I424a329f4c2222aa212562635a65d438d0127c48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2877854
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/master@{#884683}

---
+++ components/messages/android/java/src/org/chromium/components/messages/MessageAutodismissDurationProvider.java
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.messages;
+
+/**
+ * Interface to provide a duration time for message.
+ */
+public interface MessageAutodismissDurationProvider {
+    /**
+     * Provide a duration time based on given custom duration and whether a11y mode is on.
+     * @param customDuration customDuration in milliseconds in non-a11y mode.
+     *        Set 0 to get default duration.
+     * @return The expected duration for the message.
+     */
+    long get(long customDuration);
+}
--- components/messages/android/java/src/org/chromium/components/messages/MessageBannerProperties.java
+++ components/messages/android/java/src/org/chromium/components/messages/MessageBannerProperties.java
@@ -15,6 +15,7 @@ import org.chromium.base.Callback;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableFloatPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableIntPropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableLongPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
 
 /**
@@ -53,6 +54,9 @@ public class MessageBannerProperties {
             new WritableObjectPropertyKey<>();
     public static final WritableObjectPropertyKey<String> SECONDARY_ICON_CONTENT_DESCRIPTION =
             new WritableObjectPropertyKey<>();
+    // Unit: milliseconds.
+    public static final WritableLongPropertyKey DISMISSAL_DURATION_EXTEND =
+            new WritableLongPropertyKey();
     /**
      * The callback invoked when the message is dismissed. DismissReason is passed through the
      * callback's parameter.
@@ -74,6 +78,7 @@ public class MessageBannerProperties {
     public static final PropertyKey[] ALL_KEYS = new PropertyKey[] {PRIMARY_BUTTON_TEXT,
             PRIMARY_BUTTON_CLICK_LISTENER, TITLE, DESCRIPTION, ICON, ICON_RESOURCE_ID,
             ICON_TINT_COLOR, SECONDARY_ICON, SECONDARY_ICON_RESOURCE_ID, SECONDARY_BUTTON_MENU_TEXT,
-            SECONDARY_ICON_CONTENT_DESCRIPTION, TRANSLATION_X, TRANSLATION_Y, ALPHA,
-            ON_TOUCH_RUNNABLE, ON_PRIMARY_ACTION, ON_SECONDARY_ACTION, ON_DISMISSED};
+            SECONDARY_ICON_CONTENT_DESCRIPTION, DISMISSAL_DURATION_EXTEND, TRANSLATION_X,
+            TRANSLATION_Y, ALPHA, ON_TOUCH_RUNNABLE, ON_PRIMARY_ACTION, ON_SECONDARY_ACTION,
+            ON_DISMISSED};
 }
