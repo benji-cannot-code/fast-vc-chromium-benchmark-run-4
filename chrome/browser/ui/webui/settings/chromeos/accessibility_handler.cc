dd3958278fbed1111af3433c347eb4dea6475ab3
Akihiro Ota <akihiroota@chromium.org>
2021-05-19 01:12:12 +0000

    Perform SODA download logic after a11y settings page is ready.
    The attached bug details a crash where we were calling
    FireWebUIListener before JavaScript was allowed. This change fixes
    the crash by performing SODA download logic after the webUI has
    been setup, as opposed to inside the constructor.
    
    Fixed: 1210254
    AX-Relnotes: N/A
    Change-Id: I8d90f3d3d90f5a8ac26a2ee8603a839c19df4867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2904573
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884259}

--- chrome/browser/ui/webui/settings/chromeos/accessibility_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/accessibility_handler.cc
@@ -37,14 +37,7 @@ void RecordShowShelfNavigationButtonsValueChange(bool enabled) {
 }  // namespace
 
 AccessibilityHandler::AccessibilityHandler(Profile* profile)
-    : profile_(profile) {
-  if (::switches::IsExperimentalAccessibilityDictationOfflineEnabled()) {
-    if (speech::SodaInstaller::GetInstance()->IsSodaInstalled())
-      OnSodaInstalled();
-    else
-      speech::SodaInstaller::GetInstance()->AddObserver(this);
-  }
-}
+    : profile_(profile) {}
 
 AccessibilityHandler::~AccessibilityHandler() {
   if (a11y_nav_buttons_toggle_metrics_reporter_timer_.IsRunning())
@@ -121,6 +114,8 @@ void AccessibilityHandler::HandleManageA11yPageReady(
   FireWebUIListener(
       "initial-data-ready",
       base::Value(AccessibilityManager::Get()->GetStartupSoundEnabled()));
+
+  MaybeAddSodaInstallerObserver();
 }
 
 void AccessibilityHandler::HandleShowChromeVoxTutorial(
@@ -137,6 +132,15 @@ void AccessibilityHandler::OpenExtensionOptionsPage(const char extension_id[]) {
   extensions::ExtensionTabUtil::OpenOptionsPage(
       extension,
       chrome::FindBrowserWithWebContents(web_ui()->GetWebContents()));
+}
+
+void AccessibilityHandler::MaybeAddSodaInstallerObserver() {
+  if (::switches::IsExperimentalAccessibilityDictationOfflineEnabled()) {
+    if (speech::SodaInstaller::GetInstance()->IsSodaInstalled())
+      OnSodaInstalled();
+    else
+      speech::SodaInstaller::GetInstance()->AddObserver(this);
+  }
 }
 
 // SodaInstaller::Observer:
