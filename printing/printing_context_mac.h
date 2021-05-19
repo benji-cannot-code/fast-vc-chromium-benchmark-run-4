a6d92b03f900f72295adc83db9cee577e488a952
Daniel Hosseinian <dhoss@chromium.org>
2021-05-19 21:46:55 +0000

    [printing] Set all known PPD color values of macOS CUPS IPP jobs
    Even when interfacing with printer settings using CUPS IPP, the print
    job may still expect PPD color values if the printer was added to the
    system with a PPD. To avoid parsing PPDs (which is the point of using
    CUPS IPP), set every single known PPD color setting and hope that one of
    them sticks.
    
    The fix is a little hacky, but it gets the job done.
    
    Bug: 1203412
    Change-Id: Ie10ace9d3b5c279cd9e23c9992b6fbe66740c099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2905295
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884697}

--- printing/printing_context_mac.h
+++ printing/printing_context_mac.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/mac/scoped_nsobject.h"
+#include "base/strings/string_piece.h"
 #include "printing/mojom/print.mojom.h"
 #include "printing/print_job_constants.h"
 #include "printing/printing_context.h"
@@ -94,6 +95,10 @@ class COMPONENT_EXPORT(PRINTING) PrintingContextMac : public PrintingContext {
   // Sets resolution in PMPrintSettings.
   // Returns true if resolution is set.
   bool SetResolution(const gfx::Size& dpi_size);
+
+  // Sets key-value pair in PMPrintSettings.
+  // Returns true is the pair is set.
+  bool SetKeyValue(base::StringPiece key, base::StringPiece value);
 
   // The native print info object.
   base::scoped_nsobject<NSPrintInfo> print_info_;
